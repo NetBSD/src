61f282557f0bc41c0b762c629a2f4c14be8b7591
cgd <cgd@NetBSD.org>
1993-03-21 09:45:37 +0000

    initial import of 386bsd-0.1 sources

---
+++ gnu/usr.bin/lex/lib/libmain.c
@@ -0,0 +1,13 @@
+/* libmain - flex run-time support library "main" function */
+
+/* $Header: /cvsroot/src/gnu/usr.bin/lex/lib/Attic/libmain.c,v 1.1.1.1 1993/03/21 09:45:37 cgd Exp $ */
+
+extern int yylex();
+
+int main( argc, argv )
+int argc;
+char *argv[];
+
+    {
+    return yylex();
+    }
