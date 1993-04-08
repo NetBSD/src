9b082a69f06aa0152bc72b5a4b8f566d945beb23
cgd <cgd@NetBSD.org>
1993-04-08 01:07:16 +0000

    added ed, from Andrew Moore, alm@netcom.com

---
+++ bin/ed/test/mkscripts.sh
@@ -0,0 +1,37 @@
+#!/bin/sh -
+# This script generates ed test scripts (.ed) from .t files
+
+PATH="/bin:/usr/bin:/usr/local/bin/:."
+[ X"$ED" = X ] && ED="../ed"
+
+for i in *.t; do
+#	base=${i%.*}
+	base=`echo $i | sed 's/\..*/'`
+	(
+	echo "#!/bin/sh -" 
+	echo "$ED - <<\EOT" 
+	echo "r \\$base.d" 
+	cat $i 
+	echo "w \\$base.o" 
+	echo EOT
+	) >$base.ed
+	chmod +x $base.ed
+# The following is pretty ugly and not appropriate use of ed
+# but the point is that it can be done...
+#	base=`$ED - <<-EOF
+#	r !echo $i
+#	s/\..*
+#	EOF`
+#	$ED - <<-EOF
+#	a
+#	#!/bin/sh -
+#	$ED - <<\EOT
+#	r \\$base.d
+#	w \\$base.o
+#	EOT
+#	.
+#	-2r \\$i
+#	w \\$base.ed
+#	!chmod +x $base.ed
+#	EOF
+done
