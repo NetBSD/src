9194d5f4d44ce6ddbedf0c5cde1b6015aab807d0
cgd <cgd>
1993-04-08 14:10:30 +0000

    fixed typos...

--- share/mk/bsd.man.mk
+++ share/mk/bsd.man.mk
@@ -14,8 +14,8 @@ MINSTALL=	install -c -o ${MANOWN} -g ${MANGRP} -m ${MANMODE}
 
 maninstall:
 .if defined(MAN1) && !empty(MAN1)
-        @if [ ! -d ${DESTDIR}${MANDIR}1${MANSUBDIR} ]; then \
-                /bin/rm ${DESTDIR}${MANDIR}1${MANSUBDIR} ; \
+	@if [ ! -d ${DESTDIR}${MANDIR}1${MANSUBDIR} ]; then \
+                /bin/rm -f ${DESTDIR}${MANDIR}1${MANSUBDIR} ; \
                 mkdir -p ${DESTDIR}${MANDIR}1${MANSUBDIR} ; \
                 chown root.wheel ${DESTDIR}${MANDIR}1${MANSUBDIR} ; \
                 chmod 755 ${DESTDIR}${MANDIR}1${MANSUBDIR} ; \
@@ -25,8 +25,8 @@ maninstall:
 	${MINSTALL} ${MAN1} ${DESTDIR}${MANDIR}1${MANSUBDIR}
 .endif
 .if defined(MAN2) && !empty(MAN2)
-        @if [ ! -d ${DESTDIR}${MANDIR}2${MANSUBDIR} ]; then \
-                /bin/rm ${DESTDIR}${MANDIR}2${MANSUBDIR} ; \
+	@if [ ! -d ${DESTDIR}${MANDIR}2${MANSUBDIR} ]; then \
+                /bin/rm -f ${DESTDIR}${MANDIR}2${MANSUBDIR} ; \
                 mkdir -p ${DESTDIR}${MANDIR}2${MANSUBDIR} ; \
                 chown root.wheel ${DESTDIR}${MANDIR}2${MANSUBDIR} ; \
                 chmod 755 ${DESTDIR}${MANDIR}2${MANSUBDIR} ; \
@@ -36,8 +36,8 @@ maninstall:
 	${MINSTALL} ${MAN2} ${DESTDIR}${MANDIR}2${MANSUBDIR}
 .endif
 .if defined(MAN3) && !empty(MAN3)
-        @if [ ! -d ${DESTDIR}${MANDIR}3${MANSUBDIR} ]; then \
-                /bin/rm ${DESTDIR}${MANDIR}3${MANSUBDIR} ; \
+	@if [ ! -d ${DESTDIR}${MANDIR}3${MANSUBDIR} ]; then \
+                /bin/rm -f ${DESTDIR}${MANDIR}3${MANSUBDIR} ; \
                 mkdir -p ${DESTDIR}${MANDIR}3${MANSUBDIR} ; \
                 chown root.wheel ${DESTDIR}${MANDIR}3${MANSUBDIR} ; \
                 chmod 755 ${DESTDIR}${MANDIR}3${MANSUBDIR} ; \
@@ -47,8 +47,8 @@ maninstall:
 	${MINSTALL} ${MAN3} ${DESTDIR}${MANDIR}3${MANSUBDIR}
 .endif
 .if defined(MAN3F) && !empty(MAN3F)
-        @if [ ! -d ${DESTDIR}${MANDIR}3f${MANSUBDIR} ]; then \
-                /bin/rm ${DESTDIR}${MANDIR}3f${MANSUBDIR} ; \
+	@if [ ! -d ${DESTDIR}${MANDIR}3f${MANSUBDIR} ]; then \
+                /bin/rm -f ${DESTDIR}${MANDIR}3f${MANSUBDIR} ; \
                 mkdir -p ${DESTDIR}${MANDIR}3f${MANSUBDIR} ; \
                 chown root.wheel ${DESTDIR}${MANDIR}3f${MANSUBDIR} ; \
                 chmod 755 ${DESTDIR}${MANDIR}3f${MANSUBDIR} ; \
@@ -58,8 +58,8 @@ maninstall:
 	${MINSTALL} ${MAN3F} ${DESTDIR}${MANDIR}3f${MANSUBDIR}
 .endif
 .if defined(MAN4) && !empty(MAN4)
-        @if [ ! -d ${DESTDIR}${MANDIR}4${MANSUBDIR} ]; then \
-                /bin/rm ${DESTDIR}${MANDIR}4${MANSUBDIR} ; \
+	@if [ ! -d ${DESTDIR}${MANDIR}4${MANSUBDIR} ]; then \
+                /bin/rm -f ${DESTDIR}${MANDIR}4${MANSUBDIR} ; \
                 mkdir -p ${DESTDIR}${MANDIR}4${MANSUBDIR} ; \
                 chown root.wheel ${DESTDIR}${MANDIR}4${MANSUBDIR} ; \
                 chmod 755 ${DESTDIR}${MANDIR}4${MANSUBDIR} ; \
@@ -69,8 +69,8 @@ maninstall:
 	${MINSTALL} ${MAN4} ${DESTDIR}${MANDIR}4${MANSUBDIR}
 .endif
 .if defined(MAN5) && !empty(MAN5)
-        @if [ ! -d ${DESTDIR}${MANDIR}5${MANSUBDIR} ]; then \
-                /bin/rm ${DESTDIR}${MANDIR}5${MANSUBDIR} ; \
+	@if [ ! -d ${DESTDIR}${MANDIR}5${MANSUBDIR} ]; then \
+                /bin/rm -f ${DESTDIR}${MANDIR}5${MANSUBDIR} ; \
                 mkdir -p ${DESTDIR}${MANDIR}5${MANSUBDIR} ; \
                 chown root.wheel ${DESTDIR}${MANDIR}5${MANSUBDIR} ; \
                 chmod 755 ${DESTDIR}${MANDIR}5${MANSUBDIR} ; \
@@ -80,8 +80,8 @@ maninstall:
 	${MINSTALL} ${MAN5} ${DESTDIR}${MANDIR}5${MANSUBDIR}
 .endif
 .if defined(MAN6) && !empty(MAN6)
-        @if [ ! -d ${DESTDIR}${MANDIR}6${MANSUBDIR} ]; then \
-                /bin/rm ${DESTDIR}${MANDIR}6${MANSUBDIR} ; \
+	@if [ ! -d ${DESTDIR}${MANDIR}6${MANSUBDIR} ]; then \
+                /bin/rm -f ${DESTDIR}${MANDIR}6${MANSUBDIR} ; \
                 mkdir -p ${DESTDIR}${MANDIR}6${MANSUBDIR} ; \
                 chown root.wheel ${DESTDIR}${MANDIR}6${MANSUBDIR} ; \
                 chmod 755 ${DESTDIR}${MANDIR}6${MANSUBDIR} ; \
@@ -91,8 +91,8 @@ maninstall:
 	${MINSTALL} ${MAN6} ${DESTDIR}${MANDIR}6${MANSUBDIR}
 .endif
 .if defined(MAN7) && !empty(MAN7)
-        @if [ ! -d ${DESTDIR}${MANDIR}7${MANSUBDIR} ]; then \
-                /bin/rm ${DESTDIR}${MANDIR}7${MANSUBDIR} ; \
+	@if [ ! -d ${DESTDIR}${MANDIR}7${MANSUBDIR} ]; then \
+                /bin/rm -f ${DESTDIR}${MANDIR}7${MANSUBDIR} ; \
                 mkdir -p ${DESTDIR}${MANDIR}7${MANSUBDIR} ; \
                 chown root.wheel ${DESTDIR}${MANDIR}7${MANSUBDIR} ; \
                 chmod 755 ${DESTDIR}${MANDIR}7${MANSUBDIR} ; \
@@ -102,8 +102,8 @@ maninstall:
 	${MINSTALL} ${MAN7} ${DESTDIR}${MANDIR}7${MANSUBDIR}
 .endif
 .if defined(MAN8) && !empty(MAN8)
-        @if [ ! -d ${DESTDIR}${MANDIR}8${MANSUBDIR} ]; then \
-                /bin/rm ${DESTDIR}${MANDIR}8${MANSUBDIR} ; \
+	@if [ ! -d ${DESTDIR}${MANDIR}8${MANSUBDIR} ]; then \
+                /bin/rm -f ${DESTDIR}${MANDIR}8${MANSUBDIR} ; \
                 mkdir -p ${DESTDIR}${MANDIR}8${MANSUBDIR} ; \
                 chown root.wheel ${DESTDIR}${MANDIR}8${MANSUBDIR} ; \
                 chmod 755 ${DESTDIR}${MANDIR}8${MANSUBDIR} ; \
