f4e4c57db08a21a123f242ccbb1a012a5fa5085c
cgd <cgd@NetBSD.org>
1993-04-09 15:54:56 +0000

    from sean eric fagan: it seems to keep the vm system from deadlocking the system when it runs out of swap + physical memory. prevents the system from giving the last page(s) to anything but the referenced "processes" (especially important is the pager process, which should never have to wait for a free page).

--- sys/vm/vm_map.c
+++ sys/vm/vm_map.c
@@ -272,10 +272,10 @@ vm_map_entry_t vm_map_entry_create(map)
 	vm_map_t	map;
 {
 	vm_map_entry_t	entry;
-	extern vm_map_t		kernel_map, kmem_map, mb_map, buffer_map;
+	extern vm_map_t		kernel_map, kmem_map, mb_map, buffer_map, pager_map;
 
 	if (map == kernel_map || map == kmem_map || map == mb_map
-		|| map == buffer_map) {
+		|| map == buffer_map || map == pager_map) {
 		if (entry = kentry_free)
 			kentry_free = kentry_free->next;
 	} else
@@ -296,10 +296,10 @@ void vm_map_entry_dispose(map, entry)
 	vm_map_t	map;
 	vm_map_entry_t	entry;
 {
-	extern vm_map_t		kernel_map, kmem_map, mb_map, buffer_map;
+	extern vm_map_t		kernel_map, kmem_map, mb_map, buffer_map, pager_map;
 
 	if (map == kernel_map || map == kmem_map || map == mb_map
-		|| map == buffer_map) {
+		|| map == buffer_map || map == pager_map) {
 		entry->next = kentry_free;
 		kentry_free = entry;
 	} else
