a86141d84e0ddc6133d0bb3b1e1b5ad5588b3e52
cgd <cgd@NetBSD.org>
1993-04-09 12:00:07 +0000

    added netccitt, netiso, and netns support, from (currently alpha) patch 126

---
+++ sys/netiso/xebec/procs.h
@@ -0,0 +1,5 @@
+/* $Header: /cvsroot/src/sys/netiso/xebec/Attic/procs.h,v 1.1 1993/04/09 12:02:14 cgd Exp $ */
+/* $Source: /cvsroot/src/sys/netiso/xebec/Attic/procs.h,v $ */
+
+extern char *stash();
+extern struct Object *SameState;
