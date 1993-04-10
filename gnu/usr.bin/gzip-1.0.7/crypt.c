f470c83340c303566e337f5ac667f7aa3232d392
cgd <cgd>
1993-04-10 15:55:19 +0000

    gnu zip.  necessary to unpack the distribution

---
+++ gnu/usr.bin/gzip-1.0.7/crypt.c
@@ -0,0 +1,6 @@
+/* crypt.c (dummy version) -- do not perform encrytion
+ * Hardly worth copyrighting :-)
+ */
+#ifndef lint
+static char rcsid[] = "$Id: crypt.c,v 1.1 1993/04/10 15:55:38 cgd Exp $";
+#endif
