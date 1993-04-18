dd7cb2ff117a654189c69b2c93597e54e16fb360
mycroft <mycroft@NetBSD.org>
1993-04-18 20:18:21 +0000

    Define default byte order for i386.

--- include/arpa/nameser.h
+++ include/arpa/nameser.h
@@ -136,8 +136,8 @@
 #define	BIG_ENDIAN	4321	/* most-significant byte first (IBM, net) */
 #define	PDP_ENDIAN	3412	/* LSB first in word, MSW first in long (pdp) */
 
-#if defined(vax) || defined(ns32000) || defined(sun386) || defined(MIPSEL) || \
-    defined(BIT_ZERO_ON_RIGHT)
+#if defined(vax) || defined(ns32000) || defined(sun386) || defined(i386) || \
+    defined(MIPSEL) || defined(BIT_ZERO_ON_RIGHT)
 #define BYTE_ORDER	LITTLE_ENDIAN
 
 #endif
