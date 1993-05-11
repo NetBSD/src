c7cc64aa4aa05b4c2a2b4857af8a1228f42392f2
deraadt <deraadt@NetBSD.org>
1993-05-11 23:19:51 +0000

    didn't work as advertized when not given an argument. (hello, i've come for an argument. <core dumped>

--- usr.bin/size/size.c
+++ usr.bin/size/size.c
@@ -46,43 +46,48 @@ static char sccsid[] = "@(#)size.c	4.7 (Berkeley) 6/1/90";
 #include <a.out.h>
 #include <stdio.h>
 
-main(argc, argv)
-	int argc;
-	char **argv;
+int exval;
+
+main(int argc, char *argv[])
 {
+	if (argc < 2) {
+		sizeit("a.out", 0);
+		exit(exval);
+	}
+
+	for (++argv; *argv; ++argv)
+		sizeit(*argv, argc > 2);
+	exit(exval);
+}
+
+sizeit(char *name, int doname)
+{
+	static int first = 1;
 	struct exec head;
 	u_long total;
-	int exval, fd, first;
+	int fd;
 
-	if (!*argv[1])
-		*argv = "a.out";
-	else
-		++argv;
-	for (first = 1, exval = 0; *argv; ++argv) {
-		if ((fd = open(*argv, O_RDONLY, 0)) < 0) {
-			fprintf(stderr, "size: ");
-			perror(*argv);
-			exval = 1;
-			continue;
-		}
-		if (read(fd, (char *)&head, sizeof(head)) != sizeof(head) ||
-		    N_BADMAG(head)) {
-			fprintf(stderr, "size: %s: not in a.out format.\n",
-			    *argv);
-			exval = 1;
-			continue;
-		}
-		(void)close(fd);
-		if (first) {
-			first = 0;
-			printf("text\tdata\tbss\tdec\thex\n");
-		}
-		total = head.a_text + head.a_data + head.a_bss;
-		printf("%lu\t%lu\t%lu\t%lu\t%lx", head.a_text, head.a_data,
-		    head.a_bss, total, total);
-		if (argc > 2)
-			printf("\t%s", *argv);
-		printf("\n");
+	if ((fd = open(name, O_RDONLY, 0)) < 0) {
+		fprintf(stderr, "size: ");
+		perror(name);
+		exval = 1;
+		return;
 	}
-	exit(exval);
+	if (read(fd, (char *)&head, sizeof(head)) != sizeof(head) ||
+	    N_BADMAG(head)) {
+		fprintf(stderr, "size: %s: not in a.out format.\n", name);
+		exval = 1;
+		return;
+	}
+	(void)close(fd);
+	if (first) {
+		first = 0;
+		printf("text\tdata\tbss\tdec\thex\n");
+	}
+	total = head.a_text + head.a_data + head.a_bss;
+	printf("%lu\t%lu\t%lu\t%lu\t%lx", head.a_text, head.a_data,
+	    head.a_bss, total, total);
+	if (doname)
+		printf("\t%s", name);
+	printf("\n");
 }
