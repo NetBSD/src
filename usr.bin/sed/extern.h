152053b06a307a4faea0f60688eb15864568508d
mrg <mrg@NetBSD.org>
1993-06-06 23:13:45 +0000

    4.4BSD-Lite2

--- usr.bin/sed/extern.h
+++ usr.bin/sed/extern.h
@@ -1,7 +1,7 @@
 /*-
  * Copyright (c) 1992 Diomidis Spinellis.
- * Copyright (c) 1992 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1992, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Diomidis Spinellis of Imperial College, University of London.
@@ -34,7 +34,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)extern.h	5.5 (Berkeley) 8/30/92
+ *	@(#)extern.h	8.1 (Berkeley) 6/6/93
  */
 
 extern struct s_command *prog;
@@ -47,7 +47,9 @@ extern int lastline;
 extern int aflag, eflag, nflag;
 extern char *fname;
 
+void	 cfclose __P((struct s_command *, struct s_command *));
 void	 compile __P((void));
+void	 cspace __P((SPACE *, char *, size_t, enum e_spflag));
 char	*cu_fgets __P((char *, int));
 void	 err __P((int, const char *, ...));
 int	 mf_fgets __P((SPACE *, enum e_spflag));
@@ -55,5 +57,3 @@ void	 process __P((void));
 char	*strregerror __P((int, regex_t *));
 void	*xmalloc __P((u_int));
 void	*xrealloc __P((void *, u_int));
-void	 cfclose __P((struct s_command *, struct s_command *));
-void	 cspace __P((SPACE *, char *, size_t, enum e_spflag));
