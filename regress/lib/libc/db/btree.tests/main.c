0c067af3ee3255b34b4882927087f8a8f7a6e801
cgd <cgd@NetBSD.org>
1993-06-14 23:52:39 +0000

    update db code to v1.6 from vangogh, and put it in "db" rather than "DB"

--- regress/lib/libc/db/btree.tests/main.c
+++ regress/lib/libc/db/btree.tests/main.c
@@ -1,6 +1,6 @@
 /*-
- * Copyright (c) 1990 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1990, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Mike Olson.
@@ -35,7 +35,7 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)main.c	5.4 (Berkeley) 6/23/92";
+static char sccsid[] = "@(#)main.c	8.1 (Berkeley) 6/4/93";
 #endif /* LIBC_SCCS and not lint */
 
 #include <sys/param.h>
