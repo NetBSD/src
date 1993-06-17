ad175e69335f6aa26e93286bed59ff1c96a867f8
mycroft <mycroft>
1993-06-17 13:04:04 +0000

    Stupid cpp!

--- lib/libc/i386/sys/sigsuspend.s
+++ lib/libc/i386/sys/sigsuspend.s
@@ -47,7 +47,7 @@ ENTRY(sigsuspend)
 	movl	4(%esp),%eax		# fetch mask arg
 	movl	(%eax),%eax		# indirect to mask arg
 	movl	%eax,4(%esp)
-	movl	$SYS_sigsuspend ,%eax
+	movl	$/**/SYS_sigsuspend ,%eax
 	LCALL(0x7,0)
 	jb	err
 	xorl	%eax,%eax		# shouldn t happen
