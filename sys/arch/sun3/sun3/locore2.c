e1fec749525c69f81ba8e424b81f2f2d0af68fa4
glass <glass@NetBSD.org>
1993-06-27 00:46:09 +0000

    clock.c: now compiles even if address is bogus :) conf.c: compiled, until prom addition :) cons.c: add entry for prom driver lib.s: remove some includes; we're being included by locore.s locore.s: include asm.h, syscall.h, add icode m68k.s: now included by locore.s added signal.s; contains signal support sun3_startup.c: fixed compile bug in vector table initialization; fixed mon 	usage swapgeneric.c: doesn't refer to drivers we don't have

--- sys/arch/sun3/sun3/locore2.c
+++ sys/arch/sun3/sun3/locore2.c
@@ -12,7 +12,7 @@ static void initialize_vector_table()
     old_vector_table = getvbr();
     for (i = 0; i < NVECTORS; i++) {
 	if (vector_table[i] == COPY_ENTRY)
-	    vector_table[i] = old_vector[i];
+	    vector_table[i] = old_vector_table[i];
     }
     setvbr(vector_table);
 }
@@ -26,7 +26,7 @@ void bootstrap()
     for (i=0; i < 11; i++) {
 	mon_putchar(hello[i]);
     }
-    mon_printf("\nPROM Version: %d\n", romp->v_romvec_version);
+    mon_printf("\nPROM Version: %d\n", romp->romvecVersion);
     initialize_vector_table();
     
     mon_exit_to_mon();
