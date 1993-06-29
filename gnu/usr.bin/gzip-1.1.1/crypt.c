f9695d77a31e426a5ac785e1bf82b6fbf14643fd
brezak <brezak@NetBSD.org>
1993-06-29 14:49:14 +0000

    Gzip 1.1.1

---
+++ gnu/usr.bin/gzip-1.1.1/crypt.c
@@ -0,0 +1,6 @@
+/* crypt.c (dummy version) -- do not perform encryption
+ * Hardly worth copyrighting :-)
+ */
+#ifndef lint
+static char rcsid[] = "$Id: crypt.c,v 1.1 1993/06/29 14:49:26 brezak Exp $";
+#endif
