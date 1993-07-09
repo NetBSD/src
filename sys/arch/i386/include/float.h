084f12ef7f58bc82696b33fc00bd807f00e9f300
cgd <cgd@NetBSD.org>
1993-07-09 09:39:13 +0000

    update for better FP routines, from AT&T & elsewhere

--- sys/arch/i386/include/float.h
+++ sys/arch/i386/include/float.h
@@ -31,7 +31,7 @@
  * SUCH DAMAGE.
  *
  *	from: @(#)float.h	7.1 (Berkeley) 5/8/90
- *	$Id: float.h,v 1.3 1993/05/22 08:00:18 cgd Exp $
+ *	$Id: float.h,v 1.4 1993/07/09 09:39:32 cgd Exp $
  */
 
 #define FLT_RADIX	2		/* b */
@@ -40,9 +40,9 @@
 #define FLT_MANT_DIG	24		/* p */
 #define FLT_EPSILON	1.19209290E-07F	/* b**(1-p) */
 #define FLT_DIG		6		/* floor((p-1)*log10(b))+(b == 10) */
-#define FLT_MIN_EXP	-125		/* emin */
+#define FLT_MIN_EXP	(-125)		/* emin */
 #define FLT_MIN		1.17549435E-38F	/* b**(emin-1) */
-#define FLT_MIN_10_EXP	-37		/* ceil(log10(b**(emin-1))) */
+#define FLT_MIN_10_EXP	(-37)		/* ceil(log10(b**(emin-1))) */
 #define FLT_MAX_EXP	128		/* emax */
 #define FLT_MAX		3.40282347E+38F	/* (1-b**(-p))*b**emax */
 #define FLT_MAX_10_EXP	38		/* floor(log10((1-b**(-p))*b**emax)) */
@@ -50,11 +50,11 @@
 #define DBL_MANT_DIG	53
 #define DBL_EPSILON	2.2204460492503131E-16
 #define DBL_DIG		15
-#define DBL_MIN_EXP	-1021
-#define DBL_MIN		2.225073858507201E-308
-#define DBL_MIN_10_EXP	-307
+#define DBL_MIN_EXP	(-1021)
+#define DBL_MIN		2.2250738585072014E-308
+#define DBL_MIN_10_EXP	(-307)
 #define DBL_MAX_EXP	1024
-#define DBL_MAX		1.797693134862315E+308
+#define DBL_MAX		1.7976931348623157E+308
 #define DBL_MAX_10_EXP	308
 
 #define LDBL_MANT_DIG	DBL_MANT_DIG
