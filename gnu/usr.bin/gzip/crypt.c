212e0edfbc5bd4a6e23e23c5ff586b42548fa7c6
jtc <jtc@NetBSD.org>
1993-07-09 15:47:36 +0000

    GNU Gzip 1.2.2

---
+++ gnu/usr.bin/gzip/crypt.c
@@ -0,0 +1,6 @@
+/* crypt.c (dummy version) -- do not perform encryption
+ * Hardly worth copyrighting :-)
+ */
+#ifndef lint
+static char rcsid[] = "$Id: crypt.c,v 1.1.1.1 1993/07/09 15:47:38 jtc Exp $";
+#endif
