84034798dfd03ab9868c4bc39dc4ec6cfa3244bc
brezak <brezak@NetBSD.org>
1993-07-10 03:56:10 +0000

    Allow SYS_INCLUDES={copies,symlinks} choice in bsd.own.mk.

--- share/mk/bsd.own.mk
+++ share/mk/bsd.own.mk
@@ -12,3 +12,8 @@ MANDIR?=	/usr/share/man/cat
 MANGRP?=	bin
 MANOWN?=	bin
 MANMODE?=	444
+
+# Define SYS_INCLUDE to indicate whether you want symbolic links to the system
+# source (``symlinks''), or a separate copy (``copies''); (latter useful
+# in environments where it's not possible to keep /sys publicly readable)
+#SYS_INCLUDE= 	symlinks
