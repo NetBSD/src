940764f44f951a4320ef07aeaed7c3b319274b4b
cgd <cgd@NetBSD.org>
1993-07-18 11:46:53 +0000

    kill version number on directory...  strange hacks to get this to work...

--- gnu/usr.bin/tar/getoldopt.c
+++ gnu/usr.bin/tar/getoldopt.c
@@ -5,7 +5,7 @@ This file is part of GNU Tar.
 
 GNU Tar is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 1, or (at your option)
+the Free Software Foundation; either version 2, or (at your option)
 any later version.
 
 GNU Tar is distributed in the hope that it will be useful,
@@ -23,66 +23,74 @@ the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
  * otherwise, it uses the old rules used by tar, dump, and ps.
  *
  * Written 25 August 1985 by John Gilmore (ihnp4!hoptoad!gnu)
- *
- * @(#)getoldopt.c 1.4 2/4/86 - gnu
  */
 
 #include <stdio.h>
 #include "getopt.h"
+#include "tar.h"		/* For msg() declaration if STDC_MSG. */
+#include <sys/types.h>
+#include "port.h"
 
 int
-getoldopt(argc, argv, optstring, long_options, opt_index)
-	int	argc;
-	char	**argv;
-	char	*optstring;
-	struct option *long_options;
-	int 	*opt_index;
+getoldopt (argc, argv, optstring, long_options, opt_index)
+     int argc;
+     char **argv;
+     char *optstring;
+     struct option *long_options;
+     int *opt_index;
 {
-	extern char	*optarg;	/* Points to next arg */
-	extern int	optind;		/* Global argv index */
-	static char	*key;		/* Points to next keyletter */
-	static char	use_getopt;	/* !=0 if argv[1][0] was '-' */
-	extern char	*index();
-	char		c;
-	char		*place;
+  extern char *optarg;		/* Points to next arg */
+  extern int optind;		/* Global argv index */
+  static char *key;		/* Points to next keyletter */
+  static char use_getopt;	/* !=0 if argv[1][0] was '-' */
+  char c;
+  char *place;
 
-	optarg = NULL;
-	
-	if (key == NULL) {		/* First time */
-		if (argc < 2) return EOF;
-		key = argv[1];
-		if ((*key == '-') || (*key == '+'))
-			use_getopt++;
-		else
-			optind = 2;
+  optarg = NULL;
+
+  if (key == NULL)
+    {				/* First time */
+      if (argc < 2)
+	return EOF;
+      key = argv[1];
+      if ((*key == '-') || (*key == '+'))
+	use_getopt++;
+      else
+	optind = 2;
+    }
+
+  if (use_getopt)
+    return getopt_long (argc, argv, optstring,
+			long_options, opt_index);
+
+  c = *key++;
+  if (c == '\0')
+    {
+      key--;
+      return EOF;
+    }
+  place = index (optstring, c);
+
+  if (place == NULL || c == ':')
+    {
+      msg ("unknown option %c", c);
+      return ('?');
+    }
+
+  place++;
+  if (*place == ':')
+    {
+      if (optind < argc)
+	{
+	  optarg = argv[optind];
+	  optind++;
 	}
-
-	if (use_getopt)
-		return getopt_long(argc, argv, optstring, 
-				   long_options, opt_index);
-
-	c = *key++;
-	if (c == '\0') {
-		key--;
-		return EOF;
+      else
+	{
+	  msg ("%c argument missing", c);
+	  return ('?');
 	}
-	place = index(optstring, c);
+    }
 
-	if (place == NULL || c == ':') {
-		msg("unknown option %c", c);
-		return('?');
-	}
-
-	place++;
-	if (*place == ':') {
-		if (optind < argc) {
-			optarg = argv[optind];
-			optind++;
-		} else {
-			msg("%c argument missing", c);
-			return('?');
-		}
-	}
-
-	return(c);
+  return (c);
 }
