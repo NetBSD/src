aa381d0e73e6411527affbb4402a2579f9cc4463
andrew <andrew@NetBSD.org>
1993-07-18 08:19:34 +0000

    vm_protect() of the user area is no longer required for i386 machines; the protection is implemented by adjusting the bounds of the user segment descriptors.

--- sys/vm/vm_glue.c
+++ sys/vm/vm_glue.c
@@ -34,7 +34,7 @@
  * SUCH DAMAGE.
  *
  *	from: @(#)vm_glue.c	7.8 (Berkeley) 5/15/91
- *	$Id: vm_glue.c,v 1.8 1993/07/15 15:42:17 cgd Exp $
+ *	$Id: vm_glue.c,v 1.9 1993/07/18 08:19:34 andrew Exp $
  *
  *
  * Copyright (c) 1987, 1990 Carnegie-Mellon University.
@@ -236,12 +236,6 @@ vm_fork(p1, p2, isvfork)
 	/* ream out old pagetables and kernel stack */
 	(void)vm_deallocate(vp, addr, UPT_MAX_ADDRESS - addr);
 	(void)vm_allocate(vp, &addr, UPT_MAX_ADDRESS - addr, FALSE);
-
-	/* protect from the user area from user accesses. :-)
-	   addr -> addr + UPAGES*NBPG don't seem to be protected without
-	   this; the rest seems to be OK, and doesn't like being protected
-	   - andrew@werple.apana.org.au */
-	vm_protect(vp, addr, UPAGES*NBPG, FALSE, VM_PROT_NONE);
 	}
 #endif
 	/*
