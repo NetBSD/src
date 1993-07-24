428a09d4b90e24a7fa987b5047cee5ef10e34477
cgd <cgd>
1993-07-24 20:26:40 +0000

    clean the nfsnode's lockf field after getting a new vnode; this probably explains some strange NFS-related lockf crashes on pain, and UFS does it, so it can't hurt.

--- sys/nfs/nfs_node.c
+++ sys/nfs/nfs_node.c
@@ -34,7 +34,7 @@
  * SUCH DAMAGE.
  *
  *	from: @(#)nfs_node.c	7.34 (Berkeley) 5/15/91
- *	$Id: nfs_node.c,v 1.2 1993/05/20 03:18:42 cgd Exp $
+ *	$Id: nfs_node.c,v 1.2.2.1 1993/07/24 20:26:40 cgd Exp $
  */
 
 #include "param.h"
@@ -160,6 +160,7 @@ loop:
 	np->n_sillyrename = (struct sillyrename *)0;
 	np->n_size = 0;
 	np->n_mtime = 0;
+	np->n_lockf = 0;
 	*npp = np;
 	return (0);
 }
