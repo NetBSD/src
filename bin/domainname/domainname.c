970b8a1dfdd54baa2909994c874250915130caa0
mycroft <mycroft@NetBSD.org>
1993-08-02 17:15:10 +0000

    domainname.c

--- bin/domainname/domainname.c
+++ bin/domainname/domainname.c
@@ -1,3 +1,7 @@
+#ifndef lint
+static char rcsid[] = "$Id: domainname.c,v 1.2 1993/08/02 17:15:10 mycroft Exp $";
+#endif /* not lint */
+
 #include <sys/types.h>
 #include <sys/param.h>
 #include <netdb.h>
