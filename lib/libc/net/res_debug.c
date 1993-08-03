10f81d7b52424f3dece04bf6f6df795662d51148
mycroft <mycroft@NetBSD.org>
1993-08-03 21:55:48 +0000

    Add RCS identifier.

--- lib/libc/net/res_debug.c
+++ lib/libc/net/res_debug.c
@@ -29,13 +29,11 @@
  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
- *
- *	from: @(#)res_debug.c	5.36 (Berkeley) 3/6/91
- *	$Id: res_debug.c,v 1.2 1993/08/01 18:39:25 mycroft Exp $
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)res_debug.c	5.36 (Berkeley) 3/6/91";
+/*static char sccsid[] = "from: @(#)res_debug.c	5.36 (Berkeley) 3/6/91";*/
+static char rcsid[] = "$Id: res_debug.c,v 1.3 1993/08/03 21:55:48 mycroft Exp $";
 #endif /* LIBC_SCCS and not lint */
 
 #include <sys/param.h>
