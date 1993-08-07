3a9e663d0a845c5985186a7aa891283e51c4f12a
mycroft <mycroft>
1993-08-07 04:35:30 +0000

    New version from uunet.  Minor changes.

--- usr.bin/size/size.c
+++ usr.bin/size/size.c
@@ -38,30 +38,53 @@ char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-/*static char sccsid[] = "from: @(#)size.c	4.7 (Berkeley) 6/1/90";*/
-static char rcsid[] = "$Id: size.c,v 1.3 1993/08/01 18:08:26 mycroft Exp $";
+/*static char sccsid[] = "from: @(#)size.c	5.1 (Berkeley) 3/2/92";*/
+static char rcsid[] = "$Id: size.c,v 1.4 1993/08/07 04:35:31 mycroft Exp $";
 #endif /* not lint */
 
 #include <sys/param.h>
 #include <sys/file.h>
+#include <errno.h>
 #include <a.out.h>
+#include <unistd.h>
+#include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
-int exval;
+void	err __P((const char *, ...));
+int	show __P((int, char *));
+void	usage __P((void));
 
-main(int argc, char *argv[])
+int
+main(argc, argv)
+	int argc;
+	char *argv[];
 {
-	if (argc < 2) {
-		sizeit("a.out", 0);
-		exit(exval);
-	}
+	int ch, eval;
 
-	for (++argv; *argv; ++argv)
-		sizeit(*argv, argc > 2);
-	exit(exval);
+	while ((ch = getopt(argc, argv, "")) != EOF)
+		switch(ch) {
+		case '?':
+		default:
+			usage();
+		}
+	argc -= optind;
+	argv += optind;
+
+	eval = 0;
+	if (*argv)
+		do {
+			eval |= show(argc, *argv);
+		} while (*++argv);
+	else
+		eval |= show(1, "a.out");
+	exit(eval);
 }
 
-sizeit(char *name, int doname)
+int
+show(count, name)
+	int count;
+	char *name;
 {
 	static int first = 1;
 	struct exec head;
@@ -69,26 +92,58 @@ sizeit(char *name, int doname)
 	int fd;
 
 	if ((fd = open(name, O_RDONLY, 0)) < 0) {
-		fprintf(stderr, "size: ");
-		perror(name);
-		exval = 1;
-		return;
+		err("%s: %s", name, strerror(errno));
+		return (1);
 	}
-	if (read(fd, (char *)&head, sizeof(head)) != sizeof(head) ||
-	    N_BADMAG(head)) {
-		fprintf(stderr, "size: %s: not in a.out format.\n", name);
-		exval = 1;
-		return;
+	if (read(fd, &head, sizeof(head)) != sizeof(head) || N_BADMAG(head)) {
+		err("%s: not in a.out format", name);
+		return (1);
 	}
 	(void)close(fd);
+
 	if (first) {
 		first = 0;
-		printf("text\tdata\tbss\tdec\thex\n");
+		(void)printf("text\tdata\tbss\tdec\thex\n");
 	}
 	total = head.a_text + head.a_data + head.a_bss;
-	printf("%lu\t%lu\t%lu\t%lu\t%lx", head.a_text, head.a_data,
+	(void)printf("%lu\t%lu\t%lu\t%lu\t%lx", head.a_text, head.a_data,
 	    head.a_bss, total, total);
-	if (doname)
-		printf("\t%s", name);
-	printf("\n");
+	if (count > 1)
+		(void)printf("\t%s", name);
+	(void)printf("\n");
+	return (0);
+}
+
+void
+usage()
+{
+	(void)fprintf(stderr, "usage: size [file ...]\n");
+	exit(1);
+}
+
+#if __STDC__
+#include <stdarg.h>
+#else
+#include <varargs.h>
+#endif
+
+void
+#if __STDC__
+err(const char *fmt, ...)
+#else
+err(fmt, va_alist)
+	char *fmt;
+        va_dcl
+#endif
+{
+	va_list ap;
+#if __STDC__
+	va_start(ap, fmt);
+#else
+	va_start(ap);
+#endif
+	(void)fprintf(stderr, "size: ");
+	(void)vfprintf(stderr, fmt, ap);
+	va_end(ap);
+	(void)fprintf(stderr, "\n");
 }
