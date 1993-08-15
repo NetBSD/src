21e34b6f8caf343a892d2408658975bbbf298a95
mycroft <mycroft@NetBSD.org>
1993-08-15 19:26:05 +0000

    Create target directories with `install -d', and move `obj' targets into one file.

--- share/mk/bsd.lib.mk
+++ share/mk/bsd.lib.mk
@@ -124,14 +124,7 @@ depend: .depend
 .if !target(install)
 .if !target(beforeinstall)
 beforeinstall:
-	@if [ ! -d "${DESTDIR}${LIBDIR}" ]; then \
-		/bin/rm -f ${DESTDIR}${LIBDIR} ; \
-		mkdir -p ${DESTDIR}${LIBDIR} ; \
-		chown root.wheel ${DESTDIR}${LIBDIR} ; \
-		chmod 755 ${DESTDIR}${LIBDIR} ; \
-	else \
-		true ; \
-	fi
+	@install -d -o root -g wheel -m 755 ${DESTDIR}${LIBDIR}
 .endif
 
 realinstall:
@@ -180,28 +173,4 @@ tags: ${SRCS}
 .include <bsd.man.mk>
 .endif
 
-.if !target(obj)
-.if defined(NOOBJ)
-obj:
-.else
-obj:
-	@cd ${.CURDIR}; rm -f obj > /dev/null 2>&1 || true; \
-	here=`pwd`; subdir=`echo $$here | sed 's,^/usr/src/,,'`; \
-	if test $$here != $$subdir ; then \
-		dest=/usr/obj/$$subdir ; \
-		echo "$$here -> $$dest"; ln -s $$dest obj; \
-		if test -d /usr/obj -a ! -d $$dest; then \
-			mkdir -p $$dest; \
-		else \
-			true; \
-		fi; \
-	else \
-		true ; \
-		dest=$$here/obj ; \
-		if test ! -d obj ; then \
-			echo "making $$dest" ; \
-			mkdir $$dest; \
-		fi; \
-	fi;
-.endif
-.endif
+.include <bsd.obj.mk>
