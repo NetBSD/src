3ab8c699dad6020b9bf88ebbb8cf5b4f80a54f05
brezak <brezak>
1993-08-23 14:36:07 +0000

    Cleanup port to NetBSD

--- usr.sbin/pppd/pppd/pathnames.h
+++ usr.sbin/pppd/pppd/pathnames.h
@@ -5,7 +5,6 @@
 
 #define PPPHOSTS	"/etc/ppp/hosts"
 
-#define _PATH_DEBUG	"/usr/tmp/"
 #define _PATH_PIDFILE 	"/etc/ppp"
 #define _PATH_UPAPFILE 	"/etc/ppp/upap"
 #define _PATH_CHAPFILE 	"/etc/ppp/chap"
