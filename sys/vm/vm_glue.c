8dba736a4396fe28546ee1fe97a1c34195ec5da2
deraadt <deraadt@NetBSD.org>
1993-09-01 08:49:45 +0000

    #ifdef pc532 in same places as #ifdef i386

--- sys/vm/vm_glue.c
+++ sys/vm/vm_glue.c
@@ -187,7 +187,7 @@ vm_fork(p1, p2, isvfork)
 	register struct user *up;
 	vm_offset_t addr;
 
-#ifdef i386
+#if defined(i386) || defined(pc532)
 	/*
 	 * avoid copying any of the parent's pagetables or other per-process
 	 * objects that reside in the map by marking all of them non-inheritable
@@ -230,7 +230,7 @@ vm_fork(p1, p2, isvfork)
 	    ((caddr_t)&up->u_stats.pstat_endcopy -
 	     (caddr_t)&up->u_stats.pstat_startcopy));
 
-#ifdef i386
+#if defined(i386) || defined(pc532)
 	{ u_int addr = UPT_MIN_ADDRESS - UPAGES*NBPG; struct vm_map *vp;
 
 	vp = &p2->p_vmspace->vm_map;
