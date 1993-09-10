6f189b8827f8537e7da53a5848f661d781df216e
jtc <jtc@NetBSD.org>
1993-09-10 01:24:45 +0000

    Include appropriate header files to bring prototypes into scope. Cleaned up man page.

--- bin/domainname/domainname.c
+++ bin/domainname/domainname.c
@@ -1,34 +1,45 @@
 #ifndef lint
-static char rcsid[] = "$Id: domainname.c,v 1.2 1993/08/02 17:15:10 mycroft Exp $";
+static char rcsid[] = "$Id: domainname.c,v 1.3 1993/09/10 01:24:46 jtc Exp $";
 #endif /* not lint */
 
-#include <sys/types.h>
-#include <sys/param.h>
-#include <netdb.h>
 #include <stdio.h>
-#include <errno.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/param.h>
+
+static void usage __P((void));
 
 main(argc, argv)
-char **argv;
+	int argc;
+	char **argv;
 {
 	char dom[MAXHOSTNAMELEN];
 
 	if( argc>2 ) {
-		fprintf(stderr, "usage: domainname [name]\n");
-		exit(1);
+		usage ();
+		/* NOTREACHED */
 	}
+
 	if( argc==2 ) {
 		if( setdomainname(argv[1], strlen(argv[1])+1) == -1) {
 			perror("setdomainname");
 			exit(1);
 		}
-		exit(0);
+	} else {
+		if( getdomainname(dom, sizeof(dom)) == -1) {
+			perror("getdomainname");
+			exit(1);
+		}
+		printf("%s\n", dom);
 	}
-	if( getdomainname(dom, sizeof dom) == -1) {
-		perror("getdomainname");
-		exit(1);
-	}
-	printf("%s\n", dom);
+
 	exit(0);
 }
 
+static void
+usage ()
+{
+	(void)fprintf(stderr, "usage: domainname [name-of-domain]\n");
+	exit(1);
+}
