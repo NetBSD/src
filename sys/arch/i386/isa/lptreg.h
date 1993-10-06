988c967006eb62cf7e49a4f453bbaefc01e346de
mycroft <mycroft@NetBSD.org>
1993-10-06 12:11:16 +0000

    Give #defines more mnemonic names, and fix comment for LPC_IENABLE.

--- sys/arch/i386/isa/lptreg.h
+++ sys/arch/i386/isa/lptreg.h
@@ -34,7 +34,7 @@
  * SUCH DAMAGE.
  *
  *      from: @(#)lptreg.h	1.1 (Berkeley) 12/19/90
- *	$Id: lptreg.h,v 1.2.4.1 1993/09/29 05:20:31 mycroft Exp $
+ *	$Id: lptreg.h,v 1.2.4.2 1993/10/06 12:11:16 mycroft Exp $
  */
 
 /*
@@ -48,16 +48,16 @@
 
 #define lpt_status	1	/* Status of printer (R) */
 #define	LPS_NERR		0x08	/* printer no error */
-#define	LPS_SEL			0x10	/* printer selected */
-#define	LPS_OUT			0x20	/* printer out of paper */
+#define	LPS_SELECT		0x10	/* printer selected */
+#define	LPS_NOPAPER		0x20	/* printer out of paper */
 #define	LPS_NACK		0x40	/* printer no ack of data */
 #define	LPS_NBSY		0x80	/* printer no ack of data */
 
 #define lpt_control	2	/* Control printer (R/W) */
-#define	LPC_STB			0x01	/* strobe data to printer */
-#define	LPC_AUTOL		0x02	/* automatic linefeed */
+#define	LPC_STROBE		0x01	/* strobe data to printer */
+#define	LPC_AUTOLF		0x02	/* automatic linefeed */
 #define	LPC_NINIT		0x04	/* initialize printer */
-#define	LPC_SEL			0x08	/* printer selected */
-#define	LPC_ENA			0x10	/* printer out of paper */
+#define	LPC_SELECT		0x08	/* printer selected */
+#define	LPC_IENABLE		0x10	/* interrupt enable */
 
 #define	LPT_NPORTS	8
