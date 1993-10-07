699490d7985dbfb7d8372f74f2f6fa39ebd1d7f2
cgd <cgd@NetBSD.org>
1993-10-07 01:43:14 +0000

    print the warning exactly once per function per program invocation.

--- lib/libcrypt/crypt_dummy.c
+++ lib/libcrypt/crypt_dummy.c
@@ -36,7 +36,7 @@
 
 #if defined(LIBC_SCCS) && !defined(lint)
 /*static char *sccsid = "from: @(#)crypt.c	5.11 (Berkeley) 6/25/91";*/
-static char *rcsid = "$Id: crypt_dummy.c,v 1.1 1993/10/07 01:36:24 cgd Exp $";
+static char *rcsid = "$Id: crypt_dummy.c,v 1.2 1993/10/07 01:43:14 cgd Exp $";
 #endif /* LIBC_SCCS and not lint */
 
 #include <unistd.h>
@@ -56,7 +56,12 @@ crypt(key, setting)
 	register const char *key;
 	register const char *setting;
 {
-	fprintf(stderr, "WARNING!  crypt(3) not present in the system!\n");
+	static int warned;
+
+	if (!warned) {
+		fprintf(stderr, "WARNING!  crypt(3) not present in the system!\n");
+		warned = 1;
+	}
 	strncpy(cryptresult, key, sizeof cryptresult);
 	cryptresult[sizeof cryptresult - 1] = '\0';
 	return(cryptresult);
@@ -66,7 +71,12 @@ crypt(key, setting)
 des_setkey(key)
 	register const char *key;
 {
-        fprintf(stderr, "WARNING!  des_setkey(3) not present in the system!\n");
+	static int warned;
+
+	if (!warned) {
+        	fprintf(stderr, "WARNING!  des_setkey(3) not present in the system!\n");
+		warned = 1;
+	}
 	return(0);
 }
 
@@ -76,7 +86,12 @@ des_cipher(in, out, salt, num_iter)
 	long salt;
 	int num_iter;
 {
-        fprintf(stderr, "WARNING!  des_cipher(3) not present in the system!\n");
+	static int warned;
+
+	if (!warned) {
+        	fprintf(stderr, "WARNING!  des_cipher(3) not present in the system!\n");
+		warned = 1;
+	}
 	bcopy(in, out, 8);
 	return(0);
 }
@@ -84,7 +99,12 @@ des_cipher(in, out, salt, num_iter)
 setkey(key)
 	register const char *key;
 {
-        fprintf(stderr, "WARNING!  setkey(3) not present in the system!\n");
+	static int warned;
+
+	if (!warned) {
+        	fprintf(stderr, "WARNING!  setkey(3) not present in the system!\n");
+		warned = 1;
+	}
 	return(0);
 }
 
@@ -92,7 +112,12 @@ encrypt(block, flag)
 	register char *block;
 	int flag;
 {
-        fprintf(stderr, "WARNING!  encrypt(3) not present in the system!\n");
+	static int warned;
+
+	if (!warned) {
+	        fprintf(stderr, "WARNING!  encrypt(3) not present in the system!\n");
+		warned = 1;
+	}
 	return(0);
 }
 
