f85e8405ef5860c38dce52260b8f82ba4db8ed91
mycroft <mycroft@NetBSD.org>
1993-10-09 08:50:36 +0000

    Majorly rewritten to deal with interrupt sharing.  Unused fast vector cruft removed.

--- sys/arch/i386/isa/vector.s
+++ sys/arch/i386/isa/vector.s
@@ -1,10 +1,7 @@
-/*	$Id: vector.s,v 1.10.2.1 1993/09/14 17:32:51 mycroft Exp $ */
+/*	$Id: vector.s,v 1.10.2.2 1993/10/09 08:50:36 mycroft Exp $ */
 
 #include "i386/isa/icu.h"
 #include "i386/isa/isa.h"
-#include "vector.h"
-
-#define	ICU_EOI			0x20	/* XXX - define elsewhere */
 
 #define	IRQ_BIT(irq_num)	(1 << ((irq_num) % 8))
 #define	IRQ_BYTE(irq_num)	((irq_num) / 8)
@@ -70,188 +67,104 @@
  *
  * Use (%reg) instead of 0(%reg) - gas generates larger code for the latter.
  *
- * If the interrupt frame is made more flexible,  INTR can push %eax first
- * and decide the ipending case with less overhead, e.g., by avoiding
- * loading segregs.
+ * We may not need to load the segment registers to check ipending.
  */
 
-#define	FAST_INTR(unit, irq_num, id_num, handler, enable_icus) \
-	pushl	%eax ;		/* save only call-used registers */ \
-	pushl	%ecx ; \
-	pushl	%edx ; \
-	pushl	%ds ; \
-	pushl	%es ; \
-	movl	$KDSEL,%eax ; \
-	movl	%ax,%ds ; \
-	movl	%ax,%es ; \
-	SHOW_CLI ;		/* although it interferes with "ASAP" */ \
-	pushl	$unit ; \
-	call	handler ;	/* do the work ASAP */ \
-	enable_icus ;		/* (re)enable ASAP (helps edge trigger?) */ \
-	addl	$4,%esp ; \
-	incl	_cnt+V_INTR ;	/* book-keeping can wait */ \
-	COUNT_INTR(_intrcnt_actv, id_num) ; \
-	SHOW_STI ; \
-	popl	%es ; \
-	popl	%ds ; \
-	popl	%edx; \
-	popl	%ecx; \
-	popl	%eax; \
-	iret
+	.globl	_isa_strayintr
 
-#define	INTR(unit, irq_num, id_num, mask, handler, icu, enable_icus, reg, stray) \
+#define	INTR(irq_num, icu, enable_icus) \
 	pushl	$0 ;		/* dummy error code */ \
 	pushl	$T_ASTFLT ; \
-	pushal ; \
 	pushl	%ds ; 		/* save our data and extra segments ... */ \
 	pushl	%es ; \
+	pushl	%eax ; \
 	movl	$KDSEL,%eax ;	/* ... and reload with kernel's own ... */ \
 	movl	%ax,%ds ; 	/* ... early in case SHOW_A_LOT is on */ \
 	movl	%ax,%es ; \
-	SHOW_CLI ;		/* interrupt did an implicit cli */ \
-	movb	_imen + IRQ_BYTE(irq_num),%al ; \
+	movb	_imen + IRQ_BYTE(irq_num),%al ; /* mask interrupt in hw */ \
 	orb	$IRQ_BIT(irq_num),%al ; \
 	movb	%al,_imen + IRQ_BYTE(irq_num) ; \
-	SHOW_IMEN ; \
-	FASTER_NOP ; \
 	outb	%al,$icu+1 ; \
-	enable_icus ; \
-	incl	_cnt+V_INTR ;	/* tally interrupts */ \
-	movl	_cpl,%eax ; \
-	testb	$IRQ_BIT(irq_num),%reg ; \
+	enable_icus ; 		/* reenable hw interrupts */ \
+				/* check if intr masked */ \
+	testb	$IRQ_BIT(irq_num),_cpl + IRQ_BYTE(irq_num) ; \
 	jne	2f ; \
 1: ; \
-	COUNT_INTR(_intrcnt_actv, id_num) ; \
-	movl	_cpl,%eax ; \
+	popl	%eax ; 		/* fill out trap frame */ \
+	pushal ; \
+	incl	_cnt+V_INTR ; 	/* increment statistical counters */ \
+	COUNT_INTR(_intrcnt_actv, irq_num) ; \
+	movl	_cpl,%eax ; 	/* finish interrupt frame */ \
 	pushl	%eax ; \
-	pushl	$unit ; \
-	orl	mask,%eax ; \
+	movl	_intrmask + 4*irq_num,%ebx ; /* add interrupt's spl mask */ \
+	orl	(%ebx),%eax ; \
 	movl	%eax,_cpl ; \
-	SHOW_CPL ; \
-	SHOW_STI ; \
 	sti ; \
-	call	handler ; \
+	xorl	%ecx,%ecx ; 	/* clear stray flag */ \
+	movl	_intrhand + 4*irq_num,%ebx ; /* head of handler chain */ \
+	testl	%ebx,%ebx ; 	/* exit loop if no handlers */ \
+	jz	3f ; \
+	movl	(%ebx),%eax ; 	/* push argument or pointer to frame */ \
+	jnz	4f ; \
+	movl	%esp,%eax ; \
+4: ; \
+	pushl	%eax ; \
+	movl	4(%ebx),%eax ; 	/* call handler */ \
+	call	%eax ; \
+	addl	$4,%esp	;	/* pop argument */ \
+	orl	%eax,%ecx ; 	/* set stray flag */ \
+	movl	8(%ebx),%eax ; 	/* next handler in chain */ \
+	testl	%ebx,%ebx ; 	/* exit loop if no more */ \
+	jz	3f ; \
+	movl	(%ebx),%eax ; 	/* push argument or pointer to frame */ \
+	jnz	4b ; \
+	movl	%esp,%eax ; \
+	jmp	4b ; \
+; \
+	ALIGN_TEXT ; \
+3: ; \
+	testl	%ecx,%ecx ; 	/* check for stray interrupt */ \
+	jnz	5f ; \
+	pushl	$irq_num ; \
+	call	_isa_strayintr ; \
+5: ; \
+	cli ; 			/* unmask interrupt in hw */ \
 	movb	_imen + IRQ_BYTE(irq_num),%al ; \
 	andb	$~IRQ_BIT(irq_num),%al ; \
 	movb	%al,_imen + IRQ_BYTE(irq_num) ; \
-	SHOW_IMEN ; \
-	FASTER_NOP ; \
+	sti ; \
 	outb	%al,$icu+1 ; \
-	jmp	doreti ; \
+	jmp	doreti ; 	/* finish up */ \
 ; \
 	ALIGN_TEXT ; \
 2: ; \
-	COUNT_EVENT(_intrcnt_pend, id_num) ; \
-	movl	$1b,%eax ;	/* register resume address */ \
-				/* XXX - someday do it at attach time */ \
-	movl	%eax,Vresume + (irq_num) * 4 ;	\
+	COUNT_EVENT(_intrcnt_pend, irq_num) ; \
+	movl	$1b,%eax ; 	/* save resume address; set pending flag */ \
+	movl	%eax,Vresume + 4*irq_num ; \
 	orb	$IRQ_BIT(irq_num),_ipending + IRQ_BYTE(irq_num) ; \
-	SHOW_IPENDING ; \
+	popl	%eax ; \
 	popl	%es ; \
 	popl	%ds ; \
-	popal ; \
-	addl	$4+4,%esp ; \
+	addl	$8,%esp ; \
 	iret
 
-/*
- * vector.h has defined a macro 'BUILD_VECTORS' containing a big list of info
- * about vectors, including a submacro 'BUILD_VECTOR' that operates on the
- * info about each vector.  We redefine 'BUILD_VECTOR' to expand the info
- * in different ways.  Here we expand it to a list of interrupt handlers.
- * This order is of course unimportant.  Elsewhere we expand it to inline
- * linear search code for which the order is a little more important and
- * concatenating the code with no holes is very important.
- *
- * XXX - now there is BUILD_FAST_VECTOR as well as BUILD_VECTOR.
- *
- * The info consists of the following items for each vector:
- *
- *	name (identifier):	name of the vector; used to build labels
- *	unit (expression):	unit number to call the device driver with
- *	irq_num (number):	number of the IRQ to handled (0-15)
- *	id_num (number):	uniq numeric id for handler (assigned by config)
- *	mask (blank-ident):	priority mask used
- *	handler (blank-ident):	interrupt handler to call
- *	icu_num (number):	(1 + irq_num / 8) converted for label building
- *	icu_enables (number):	1 for icu_num == 1, 1_AND_2 for icu_num == 2
- *	reg (blank-ident):	al for icu_num == 1, ah for icu_num == 2
- *
- * 'irq_num' is converted in several ways at config time to get around
- * limitations in cpp.  The macros have blanks after commas iff they would
- * not mess up identifiers and numbers.
- */
+INTR(0, IO_ICU1, ENABLE_ICU1)
+INTR(1, IO_ICU1, ENABLE_ICU1)
+INTR(2, IO_ICU1, ENABLE_ICU1)
+INTR(3, IO_ICU1, ENABLE_ICU1)
+INTR(4, IO_ICU1, ENABLE_ICU1)
+INTR(5, IO_ICU1, ENABLE_ICU1)
+INTR(6, IO_ICU1, ENABLE_ICU1)
+INTR(7, IO_ICU1, ENABLE_ICU1)
+INTR(8, IO_ICU2, ENABLE_ICU1_AND_2)
+INTR(9, IO_ICU2, ENABLE_ICU1_AND_2)
+INTR(10, IO_ICU2, ENABLE_ICU1_AND_2)
+INTR(11, IO_ICU2, ENABLE_ICU1_AND_2)
+INTR(12, IO_ICU2, ENABLE_ICU1_AND_2)
+INTR(13, IO_ICU2, ENABLE_ICU1_AND_2)
+INTR(14, IO_ICU2, ENABLE_ICU1_AND_2)
+INTR(15, IO_ICU2, ENABLE_ICU1_AND_2)
 
-#undef BUILD_FAST_VECTOR
-#define	BUILD_FAST_VECTOR(name, unit, irq_num, id_num, mask, handler, \
-			  icu_num, icu_enables, reg) \
-	.globl	handler ; \
-	.text ; \
-	.globl	_X/**/name ; \
-	SUPERALIGN_TEXT ; \
-_X/**/name: ; \
-	FAST_INTR(unit, irq_num, id_num, handler, ENABLE_ICU/**/icu_enables)
-
-#undef BUILD_VECTOR
-#define	BUILD_VECTOR(name, unit, irq_num, id_num, mask, handler, \
-		     icu_num, icu_enables, reg) \
-	.globl	handler ; \
-	.text ; \
-	.globl	_X/**/name ; \
-	SUPERALIGN_TEXT ; \
-_X/**/name: ; \
-	INTR(unit,irq_num,id_num, mask, handler, IO_ICU/**/icu_num, \
-	     ENABLE_ICU/**/icu_enables, reg,)
-
-	BUILD_VECTORS
-
-	/* hardware interrupt catcher (IDT 32 - 47) */
-	.globl	_isa_strayintr
-
-#define	STRAYINTR(irq_num, icu_num, icu_enables, reg) \
-IDTVEC(intr/**/irq_num) ; \
-	INTR(irq_num,irq_num,irq_num, _highmask,  _isa_strayintr, \
-		  IO_ICU/**/icu_num, ENABLE_ICU/**/icu_enables, reg,stray)
-
-/*
- * XXX - the mask (1 << 2) == IRQ_SLAVE will be generated for IRQ 2, instead
- * of the mask IRQ2 (defined as IRQ9 == (1 << 9)).  But IRQ 2 "can't happen".
- * In fact, all stray interrupts "can't happen" except for bugs.  The
- * "stray" IRQ 7 is documented behaviour of the 8259.  It happens when there
- * is a glitch on any of its interrupt inputs.  Does it really interrupt when
- * IRQ 7 is masked?
- *
- * XXX - unpend doesn't work for these, it sends them to the real handler.
- *
- * XXX - the race bug during initialization may be because I changed the
- * order of switching from the stray to the real interrupt handler to before
- * enabling interrupts.  The old order looked unsafe but maybe it is OK with
- * the stray interrupt handler installed.  But these handlers only reduce
- * the window of vulnerability - it is still open at the end of
- * isa_configure().
- *
- * XXX - many comments are stale.
- */
-
-	STRAYINTR(0,1,1, al)
-	STRAYINTR(1,1,1, al)
-	STRAYINTR(2,1,1, al)
-	STRAYINTR(3,1,1, al)
-	STRAYINTR(4,1,1, al)
-	STRAYINTR(5,1,1, al)
-	STRAYINTR(6,1,1, al)
-	STRAYINTR(8,2,1_AND_2, ah)
-	STRAYINTR(9,2,1_AND_2, ah)
-	STRAYINTR(10,2,1_AND_2, ah)
-	STRAYINTR(11,2,1_AND_2, ah)
-	STRAYINTR(12,2,1_AND_2, ah)
-	STRAYINTR(13,2,1_AND_2, ah)
-	STRAYINTR(14,2,1_AND_2, ah)
-	STRAYINTR(15,2,1_AND_2, ah)
-IDTVEC(intrdefault)
-	STRAYINTR(7,1,1, al)	/* XXX */
-#if 0
-	INTRSTRAY(255, _highmask, 255) ; call	_isa_strayintr ; INTREXIT2
-#endif
 /*
  * These are the interrupt counters, I moved them here from icu.s so that
  * they are with the name table.  rgrimes
@@ -264,63 +177,53 @@ IDTVEC(intrdefault)
 Vresume:	.space	16 * 4	/* where to resume intr handler after unpend */
 	.globl	_intrcnt
 _intrcnt:			/* used by vmstat to calc size of table */
-	.globl	_intrcnt_bad7
-_intrcnt_bad7:	.space	4	/* glitches on irq 7 */
-	.globl	_intrcnt_bad15
-_intrcnt_bad15:	.space	4	/* glitches on irq 15 */
 	.globl	_intrcnt_stray
 _intrcnt_stray:	.space	4	/* total count of stray interrupts */
 	.globl	_intrcnt_actv
-_intrcnt_actv:	.space	NR_REAL_INT_HANDLERS * 4	/* active interrupts */
-
-#ifdef INTR_DEBUG
+_intrcnt_actv:	.space	16 * 4	/* active interrupts */
 	.globl	_intrcnt_pend
-_intrcnt_pend:	.space	NR_REAL_INT_HANDLERS * 4	/* pending interrupts */
-	.globl	_intrcnt_spl
-_intrcnt_spl:	.space	32 * 4	/* XXX 32 should not be hard coded ? */
-	.globl	_intrcnt_show
-_intrcnt_show:	.space	8 * 4	/* XXX 16 should not be hard coded ? */
-#endif /* INTR_DEBUG */
-
+_intrcnt_pend:	.space	16 * 4	/* pending interrupts */
 	.globl	_eintrcnt
 _eintrcnt:			/* used by vmstat to calc size of table */
-
-/*
- * Build the interrupt name table for vmstat
- */
-
-#undef BUILD_FAST_VECTOR
-#define BUILD_FAST_VECTOR	BUILD_VECTOR
-
-#undef BUILD_VECTOR
-#define	BUILD_VECTOR(name, unit, irq_num, id_num, mask, handler, \
-		     icu_num, icu_enables, reg) \
-	.ascii	"name irq" ; \
-	.asciz	"irq_num"
-/*
- * XXX - use the STRING and CONCAT macros from <sys/cdefs.h> to stringize
- * and concatenate names above and elsewhere.
- */
 
 	.text
 	.globl	_intrnames, _eintrnames
 _intrnames:
-	BUILD_VECTOR(bad,,7,,,,,,)
-	BUILD_VECTOR(bad,,15,,,,,,)
-	BUILD_VECTOR(stray,,,,,,,,)
-	BUILD_VECTORS
+	.asciz	"stray"
+	.asciz	"irq0"
+	.asciz	"irq1"
+	.asciz	"irq2"
+	.asciz	"irq3"
+	.asciz	"irq4"
+	.asciz	"irq5"
+	.asciz	"irq6"
+	.asciz	"irq7"
+	.asciz	"irq8"
+	.asciz	"irq9"
+	.asciz	"irq10"
+	.asciz	"irq11"
+	.asciz	"irq12"
+	.asciz	"irq13"
+	.asciz	"irq14"
+	.asciz	"irq15"
 
 #ifdef INTR_DEBUG
-
-#undef BUILD_FAST_VECTOR
-#define BUILD_FAST_VECTOR	BUILD_VECTOR
-
-#undef BUILD_VECTOR
-#define	BUILD_VECTOR(name, unit, irq_num, id_num, mask, handler, \
-		     icu_num, icu_enables, reg) \
-	.asciz	"name pend"
-
-	BUILD_VECTORS
+	.asciz	"irq0 pend"
+	.asciz	"irq1 pend"
+	.asciz	"irq2 pend"
+	.asciz	"irq3 pend"
+	.asciz	"irq4 pend"
+	.asciz	"irq5 pend"
+	.asciz	"irq6 pend"
+	.asciz	"irq7 pend"
+	.asciz	"irq8 pend"
+	.asciz	"irq9 pend"
+	.asciz	"irq10 pend"
+	.asciz	"irq11 pend"
+	.asciz	"irq12 pend"
+	.asciz	"irq13 pend"
+	.asciz	"irq14 pend"
+	.asciz	"irq15 pend"
 
 /*
  * now the spl names
