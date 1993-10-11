a851f1b5f136dba5940000fe62baf74fa899bbc3
deraadt <deraadt@NetBSD.org>
1993-10-11 02:45:49 +0000

    bwtwo.c, cgthree.c: sys-fbio.h -> machine/fbio.h if_le.c: #ifdef MULTICAST some things, and do arp the old way

--- sys/arch/sparc/dev/bwtwo.c
+++ sys/arch/sparc/dev/bwtwo.c
@@ -42,7 +42,7 @@
  *	@(#)bwtwo.c	8.1 (Berkeley) 6/11/93
  *
  * from: Header: bwtwo.c,v 1.14 92/11/26 02:28:04 torek Exp 
- * $Id: bwtwo.c,v 1.1 1993/10/02 10:23:45 deraadt Exp $
+ * $Id: bwtwo.c,v 1.2 1993/10/11 02:45:49 deraadt Exp $
  */
 
 /*
@@ -53,12 +53,12 @@
 
 #include <sys/param.h>
 #include <sys/device.h>
-#include <sys/fbio.h>
 #include <sys/ioctl.h>
 #include <sys/malloc.h>
 #include <sys/mman.h>
 #include <sys/tty.h>
 
+#include <machine/fbio.h>
 #include <machine/autoconf.h>
 #include <machine/pmap.h>
 #include <machine/fbvar.h>
