096a5220d7a04582e2b6578c50cfe13e732f08ff
jtc <jtc@NetBSD.org>
1993-10-12 21:58:49 +0000

    Moved _POSIX2_VERSION macro from <limits.h>.

--- sys/sys/unistd.h
+++ sys/sys/unistd.h
@@ -31,7 +31,7 @@
  * SUCH DAMAGE.
  *
  *	from: @(#)unistd.h	5.14 (Berkeley) 4/1/91
- *	$Id: unistd.h,v 1.4 1993/09/21 21:50:37 jtc Exp $
+ *	$Id: unistd.h,v 1.5 1993/10/12 21:58:49 jtc Exp $
  */
 
 #ifndef _SYS_UNISTD_H_
@@ -42,7 +42,9 @@
 #ifdef _NOTYET
 #define	_POSIX_SAVED_IDS	/* saved set-user-ID and set-group-ID */
 #endif
+
 #define	_POSIX_VERSION		198808L
+#define _POSIX2_VERSION         199209L
 
 /* execution-time symbolic constants */
 #define	_POSIX_CHOWN_RESTRICTED	/* chown requires appropriate privileges */
@@ -87,23 +89,26 @@
 #define	_SC_OPEN_MAX		5
 #define	_SC_JOB_CONTROL		6
 #define	_SC_SAVED_IDS		7
-#define	_SC_VERSION		8
-#define _SC_BC_BASE_MAX		9
-#define _SC_BC_DIM_MAX		10
-#define _SC_BC_SCALE_MAX	11
-#define _SC_BC_STRING_MAX	12
-#define _SC_COLL_WEIGHTS_MAX	13
-#define _SC_EXPR_NEST_MAX	14
-#define _SC_LINE_MAX		15
-#define _SC_RE_DUP_MAX		16
-#define _SC_2_VERSION		17
-#define _SC_2_C_DEV		18
-#define _SC_2_CHAR_TERM		19
-#define _SC_2_FORT_DEV		20
-#define _SC_2_FORT_RUN		21
-#define _SC_2_LOCALEDEF		22
-#define _SC_2_SW_DEV		23
-#define _SC_2_UPE		24
+#define _SC_STREAM_MAX		8
+#define _SC_TZNAME_MAX		9
+#define	_SC_VERSION		10
+
+#define _SC_BC_BASE_MAX		11
+#define _SC_BC_DIM_MAX		12
+#define _SC_BC_SCALE_MAX	13
+#define _SC_BC_STRING_MAX	14
+#define _SC_COLL_WEIGHTS_MAX	15
+#define _SC_EXPR_NEST_MAX	16
+#define _SC_LINE_MAX		17
+#define _SC_RE_DUP_MAX		18
+#define _SC_2_VERSION		19
+#define _SC_2_C_DEV		20
+#define _SC_2_CHAR_TERM		21
+#define _SC_2_FORT_DEV		22
+#define _SC_2_FORT_RUN		23
+#define _SC_2_LOCALEDEF		24
+#define _SC_2_SW_DEV		25
+#define _SC_2_UPE		26
 
 #define _CS_PATH		1
 
