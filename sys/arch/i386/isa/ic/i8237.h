4acf63d660e9099a6de200671fc7bf0b80dc4007
mycroft <mycroft@NetBSD.org>
1993-10-14 05:19:13 +0000

    Add some bits from the `single mask' register.

--- sys/arch/i386/isa/ic/i8237.h
+++ sys/arch/i386/isa/ic/i8237.h
@@ -1,7 +1,7 @@
 /*
  * Intel 8237 DMA Controller
  *
- *	$Id: i8237.h,v 1.2 1993/05/22 08:01:56 cgd Exp $
+ *	$Id: i8237.h,v 1.2.4.1 1993/10/14 05:19:13 mycroft Exp $
  */
 
 #define	DMA37MD_SINGLE	0x40	/* single pass mode */
@@ -9,3 +9,5 @@
 #define	DMA37MD_WRITE	0x04	/* read the device, write memory operation */
 #define	DMA37MD_READ	0x08	/* write the device, read memory operation */
 	
+#define	DMA37SM_CLEAR	0x00	/* clear mask bit */
+#define	DMA37SM_SET	0x04	/* set mask bit */
