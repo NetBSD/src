48227fa692c9dd62ae4e0b9fac7b02338ce367b3
mycroft <mycroft@NetBSD.org>
1993-10-14 05:22:09 +0000

    Remove DMA gunk.  Rewrite IDT initialization to use a table from locore. Remove some defunct configuration code.  Don't splhigh() in sysbeep() and sysbeepstop().

--- sys/arch/i386/isa/isa.c
+++ sys/arch/i386/isa/isa.c
@@ -1,4 +1,5 @@
 /*-
+ * Copyright (c) 1993 Charles Hannum.
  * Copyright (c) 1991 The Regents of the University of California.
  * All rights reserved.
  *
@@ -34,27 +35,18 @@
  * SUCH DAMAGE.
  *
  *	from: @(#)isa.c	7.2 (Berkeley) 5/13/91
- *	$Id: isa.c,v 1.28.2.6 1993/10/11 01:51:26 mycroft Exp $
+ *	$Id: isa.c,v 1.28.2.7 1993/10/14 05:22:09 mycroft Exp $
  */
 
 /*
  * code to manage AT bus
- *
- * 92/08/18  Frank P. MacLachlan (fpm@crash.cts.com):
- * Fixed uninitialized variable problem and added code to deal
- * with DMA page boundaries in isa_dmarangecheck().  Fixed word
- * mode DMA count compution and reorganized DMA setup code in
- * isa_dmastart()
  */
 
 #include "param.h"
 #include "systm.h"
 #include "conf.h"
 #include "file.h"
-#include "buf.h"
-#include "uio.h"
 #include "syslog.h"
-#include "malloc.h"
 #include "machine/cpu.h"
 #include "machine/pio.h"
 #include "sys/device.h"
@@ -62,26 +54,8 @@
 #include "i386/isa/isa.h"
 #include "i386/isa/isavar.h"
 #include "i386/isa/icu.h"
-#include "i386/isa/ic/i8237.h"
-#include "i386/isa/ic/i8042.h"
 #include "i386/isa/timerreg.h"
 #include "i386/isa/spkr_reg.h"
-
-/*
-**  Register definitions for DMA controller 1 (channels 0..3):
-*/
-#define	DMA1_CHN(c)	(IO_DMA1 + 1*(2*(c)))	/* addr reg for channel c */
-#define	DMA1_SMSK	(IO_DMA1 + 1*10)	/* single mask register */
-#define	DMA1_MODE	(IO_DMA1 + 1*11)	/* mode register */
-#define	DMA1_FFC	(IO_DMA1 + 1*12)	/* clear first/last FF */
-
-/*
-**  Register definitions for DMA controller 2 (channels 4..7):
-*/
-#define	DMA2_CHN(c)	(IO_DMA1 + 2*(2*(c)))	/* addr reg for channel c */
-#define	DMA2_SMSK	(IO_DMA2 + 2*10)	/* single mask register */
-#define	DMA2_MODE	(IO_DMA2 + 2*11)	/* mode register */
-#define	DMA2_FFC	(IO_DMA2 + 2*12)	/* clear first/last FF */
 
 isa_type isa_bustype;				/* type of bus */
 
@@ -120,7 +94,7 @@ isaattach(parent, self, aux)
 	void *aux;
 {
 
-	/* should print ISA/EISA & bus clock frequency and anything else
+	/* XXX should print ISA/EISA & bus clock frequency and anything else
 	   we can figure out? */
 	printf("\n");
 
@@ -224,26 +198,16 @@ isaprint(aux, isaname)
 		printf(" iomem 0x%x", ia->ia_maddr);
 	if (ia->ia_msize > 1)
 		printf("-0x%x", ia->ia_maddr + ia->ia_msize - 1);
-	/* XXXX need to print flags */
+	/* XXXX print flags */
 	return QUIET;	/* actually, our return value is irrelevant. */
 }
 
 
 #define	IDTVEC(name)	__CONCAT(X,name)
 /* default interrupt vector table entries */
-extern	IDTVEC(intr0), IDTVEC(intr1), IDTVEC(intr2), IDTVEC(intr3),
-	IDTVEC(intr4), IDTVEC(intr5), IDTVEC(intr6), IDTVEC(intr7),
-	IDTVEC(intr8), IDTVEC(intr9), IDTVEC(intr10), IDTVEC(intr11),
-	IDTVEC(intr12), IDTVEC(intr13), IDTVEC(intr14), IDTVEC(intr15);
-
-static *defvec[16] = {
-	&IDTVEC(intr0), &IDTVEC(intr1), &IDTVEC(intr2), &IDTVEC(intr3),
-	&IDTVEC(intr4), &IDTVEC(intr5), &IDTVEC(intr6), &IDTVEC(intr7),
-	&IDTVEC(intr8), &IDTVEC(intr9), &IDTVEC(intr10), &IDTVEC(intr11),
-	&IDTVEC(intr12), &IDTVEC(intr13), &IDTVEC(intr14), &IDTVEC(intr15) };
-
+extern	*IDTVEC(intr)[NIRQ];
 /* out of range default interrupt vector gate entry */
-extern	IDTVEC(intrdefault);
+extern	IDTVEC(stray);
 
 /*
  * Fill in default interrupt table, and mask all interrupts.
@@ -255,11 +219,11 @@ isa_defaultirq()
 
 	/* icu vectors */
 	for (i = ICU_OFFSET; i < ICU_OFFSET + ICU_LEN ; i++)
-		setidt(i, defvec[i],  SDT_SYS386IGT, SEL_KPL);
+		setidt(i, IDTVEC(intr)[i],  SDT_SYS386IGT, SEL_KPL);
   
 	/* out of range vectors */
 	for (; i < NIDT; i++)
-		setidt(i, &IDTVEC(intrdefault), SDT_SYS386IGT, SEL_KPL);
+		setidt(i, &IDTVEC(stray), SDT_SYS386IGT, SEL_KPL);
 
 	/* initialize 8259's */
 	outb(IO_ICU1, 0x11);		/* reset; program device, four bytes */
@@ -288,245 +252,18 @@ isa_defaultirq()
 	outb(IO_ICU2, 0x0a);		/* default to IRR on read */
 }
 
-
-/* XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX */
-
-int
-config_isadev(isdp, mp)
-	struct isa_device *isdp;
-	u_int *mp;
-{
-	if (isdp->id_irq) {
-		int intrno;
-
-		intrno = ffs(isdp->id_irq)-1;
-		setidt(ICU_OFFSET+intrno, isdp->id_intr,
-			 SDT_SYS386IGT, SEL_KPL);
-		if(mp)
-			INTRMASK(*mp,isdp->id_irq);
-		INTREN(isdp->id_irq);
-	}
-}
-
-
-/* region of physical memory known to be contiguous */
-vm_offset_t isaphysmem;
-static caddr_t bouncebuf[8];		/* XXX */
-static caddr_t bounced[8];		/* XXX */
-static vm_size_t bouncesize[8];		/* XXX */
-#define MAXDMASZ 512			/* XXX */
-
-/* high byte of address is stored in this port for i-th dma channel */
-static short dmapageport[8] =
-	{ 0x87, 0x83, 0x81, 0x82, 0x8f, 0x8b, 0x89, 0x8a };
-
-/*
- * isa_dmacascade(): program 8237 DMA controller channel to accept
- * external dma control by a board.
- */
-void
-at_dma_cascade(chan)
-	unsigned chan;
-{
-#ifdef DEBUG
-	if (chan > 7)
-		panic("at_dma_cascade: impossible request"); 
-#endif
-
-	/* set dma channel mode, and set dma channel mode */
-	if ((chan & 4) == 0) {
-		outb(DMA1_MODE, DMA37MD_CASCADE | chan);
-		outb(DMA1_SMSK, chan);
-	} else {
-		outb(DMA2_MODE, DMA37MD_CASCADE | (chan & 3));
-		outb(DMA2_SMSK, chan & 3);
-	}
-}
-
-/*
- * at_dma(): program 8237 DMA controller channel, avoid page alignment
- * problems by using a bounce buffer.
- */
-void
-at_dma(flags, addr, nbytes, chan)
-	int read;
-	caddr_t addr;
-	vm_size_t nbytes;
-	unsigned chan;
-{
-	vm_offset_t phys;
-	int waport;
-	caddr_t newaddr;
-
-ppp	if (    chan > 7
-	    || (chan < 4 && nbytes > (1<<16))
-	    || (chan >= 4 && (nbytes > (1<<17) || (u_int)addr & 1)))
-		panic("isa_dmastart: impossible request"); 
-
-	if (at_dma_rangecheck(addr, nbytes, chan)) {
-		panic("bounce buffers don't work yet\n");
-		/* XXX totally braindead; NBPG is not enough */
-		if (bouncebuf[chan] == 0)
-			bouncebuf[chan] =
-				/*(caddr_t)malloc(MAXDMASZ, M_TEMP, M_WAITOK);*/
-				(caddr_t) isaphysmem + NBPG*chan;
-		bouncesize[chan] = nbytes;
-		newaddr = bouncebuf[chan];
-		/* copy bounce buffer on write */
-		if (!read)
-			bcopy(addr, newaddr, nbytes);
-		else
-			bounced[chan] = addr;
-		addr = newaddr;
-	}
-
-	/* translate to physical */
-	phys = pmap_extract(pmap_kernel(), (vm_offset_t)addr);
-
-	if ((chan & 4) == 0) {
-		/*
-		 * Program one of DMA channels 0..3.  These are
-		 * byte mode channels.
-		 */
-		/* set dma channel mode, and reset address ff */
-		if (read)
-			outb(DMA1_MODE, DMA37MD_SINGLE|DMA37MD_WRITE|chan);
-		else
-			outb(DMA1_MODE, DMA37MD_SINGLE|DMA37MD_READ|chan);
-		outb(DMA1_FFC, 0);
-
-		/* send start address */
-		waport =  DMA1_CHN(chan);
-		outb(waport, phys);
-		outb(waport, phys>>8);
-		outb(dmapageport[chan], phys>>16);
-
-		/* send count */
-		outb(waport + 1, --nbytes);
-		outb(waport + 1, nbytes>>8);
-
-		/* unmask channel */
-		outb(DMA1_SMSK, chan);
-	} else {
-		/*
-		 * Program one of DMA channels 4..7.  These are
-		 * word mode channels.
-		 */
-		/* set dma channel mode, and reset address ff */
-		if (read)
-			outb(DMA2_MODE, DMA37MD_SINGLE|DMA37MD_WRITE|(chan&3));
-		else
-			outb(DMA2_MODE, DMA37MD_SINGLE|DMA37MD_READ|(chan&3));
-		outb(DMA2_FFC, 0);
-
-		/* send start address */
-		waport = DMA2_CHN(chan - 4);
-		outb(waport, phys>>1);
-		outb(waport, phys>>9);
-		outb(dmapageport[chan], phys>>16);
-
-		/* send count */
-		nbytes >>= 1;
-		outb(waport + 2, --nbytes);
-		outb(waport + 2, nbytes>>8);
-
-		/* unmask channel */
-		outb(DMA2_SMSK, chan & 3);
-	}
-}
-
-void
-at_dma_terminate(int flags, caddr_t addr, int nbytes, int chan)
-{
-
-	if (bounced[chan]) {
-		bcopy(bouncebuf[chan], bounced[chan], bouncesize[chan]);
-		bounced[chan] = 0;
-	}
-}
-
-/*
- * Check for problems with the address range of a DMA transfer
- * (non-contiguous physical pages, outside of bus address space,
- * crossing DMA page boundaries).
- * Return true if special handling needed.
- */
-
-int
-at_dma_rangecheck(caddr_t va, unsigned length, unsigned chan) {
-	vm_offset_t phys, priorpage = 0, endva;
-	u_int dma_pgmsk = (chan & 4) ?  ~(128*1024-1) : ~(64*1024-1);
-
-	endva = (vm_offset_t)round_page(va + length);
-	for (; va < (caddr_t) endva ; va += NBPG) {
-		phys = trunc_page(pmap_extract(pmap_kernel(), (vm_offset_t)va));
-		if (phys == 0)
-			panic("isa_dmacheck: no physical page present");
-		if (phys > physmem) 
-			return (1);
-		if (priorpage) {
-			if (priorpage + NBPG != phys)
-				return (1);
-			/* check if crossing a DMA page boundary */
-			if (((u_int)priorpage ^ (u_int)phys) & dma_pgmsk)
-				return (1);
-		}
-		priorpage = phys;
-	}
-	return (0);
-}
-
-/* head of queue waiting for physmem to become available */
-struct buf isa_physmemq;
-
-/* blocked waiting for resource to become free for exclusive use */
-static isaphysmemflag;
-/* if waited for and call requested when free (B_CALL) */
-static void (*isaphysmemunblock)(); /* XXX needs to be a list */
-
-/*
- * Allocate contiguous physical memory for transfer, returning
- * a *virtual* address to region. May block waiting for resource.
- * (assumed to be called at splbio())
- */
-caddr_t
-isa_allocphysmem(caddr_t va, unsigned length, void (*func)()) {
-	
-	isaphysmemunblock = func;
-	while (isaphysmemflag & B_BUSY) {
-		isaphysmemflag |= B_WANTED;
-		sleep((caddr_t)&isaphysmemflag, PRIBIO);
-	}
-	isaphysmemflag |= B_BUSY;
-
-	return((caddr_t)isaphysmem);
-}
-
-/*
- * Free contiguous physical memory used for transfer.
- * (assumed to be called at splbio())
- */
-void
-isa_freephysmem(caddr_t va, unsigned length) {
-
-	isaphysmemflag &= ~B_BUSY;
-	if (isaphysmemflag & B_WANTED) {
-		isaphysmemflag &= B_WANTED;
-		wakeup((caddr_t)&isaphysmemflag);
-		if (isaphysmemunblock)
-			(*isaphysmemunblock)();
-	}
-}
-	
 /*
  * Handle a NMI, possibly a machine check.
  * return true to panic system, false to ignore.
+ *
+ * This implementation is hist[oe]rical.
  */
 int
-isa_nmi(cd) {
+isa_nmi()
+{
 
-	log(LOG_CRIT, "\nNMI port 61 %x, port 70 %x\n", inb(0x61), inb(0x70));
-	return(0);
+	log(LOG_CRIT, "NMI port 61 %x, port 70 %x\n", inb(0x61), inb(0x70));
+	return 0;
 }
 
 /*
@@ -536,26 +273,32 @@ void
 isa_strayintr(d)
 	int d;
 {
+	extern u_long intrcnt_stray;
 
 	/*
 	 * Stray level 7 interrupts occur when someone raises an interrupt
 	 * and then drops it before the CPU acknowledges it.  This means
-	 * either the device is screwed or something is cli'ing too long.
+	 * either the device is screwed or something is cli'ing too long
+	 * and it's timing out.
+	 *
+	 * -1 is passed by the generic handler for out of range exceptions,
+	 * since we don't really want 208 little vectors.  (It wouldn't be
+	 * all that much code, but why bother?)
 	 */
-	extern u_long intrcnt_stray;
-
-	intrcnt_stray++;
-	if (intrcnt_stray <= 5)
-		log(LOG_ERR, "stray interrupt %d\n", d);
+	if (intrcnt_stray++ < 5)
+		if (d == -1)
+			log(LOG_ERR, "wild interrupt\n");
+		else
+			log(LOG_ERR, "stray interrupt %d\n", d);
 	if (intrcnt_stray == 5)
-		log(LOG_CRIT,"too many stray interrupts; stopped logging\n");
+		log(LOG_ERR, "too many stray interrupts; stopped logging\n");
 }
 
-static beeping;
+static int beeping;
+
 static void
 sysbeepstop(int f)
 {
-	int s = splhigh();
 
 	/* disable counter 2 */
 	disable_intr();
@@ -565,14 +308,11 @@ sysbeepstop(int f)
 		timeout((timeout_t)sysbeepstop, (caddr_t)0, f);
 	else
 		beeping = 0;
-
-	splx(s);
 }
 
 void
 sysbeep(int pitch, int period)
 {
-	int s = splhigh();
 	static int last_pitch, last_period;
 
 	if (beeping) {
@@ -581,73 +321,16 @@ sysbeep(int pitch, int period)
 	}
 	if (!beeping || last_pitch != pitch) {
 		/*
-	 	* XXX - move timer stuff to clock.c.
-	 	*/
+		 * XXX - move timer stuff to clock.c.
+		 */
 		disable_intr();
 		outb(TIMER_MODE, TIMER_SEL2|TIMER_16BIT|TIMER_SQWAVE);
-		outb(TIMER_CNTR2, TIMER_DIV(pitch)%256);
-		outb(TIMER_CNTR2, TIMER_DIV(pitch)/256);
+		outb(TIMER_CNTR2, TIMER_DIV(pitch));
+		outb(TIMER_CNTR2, TIMER_DIV(pitch)>>8);
 		outb(PITAUX_PORT, inb(PITAUX_PORT) | PIT_SPKR);	/* enable counter 2 */
 		enable_intr();
 	}
 	last_pitch = pitch;
 	beeping = last_period = period;
 	timeout((timeout_t)sysbeepstop, (caddr_t)(period/2), period);
-
-	splx(s);
-}
-
-/*
- * Pass command to keyboard controller (8042)
- */
-unsigned
-kbc_8042cmd(int val)
-{
-	while (inb(KBSTATP)&KBS_IBF);
-	if (val) outb(KBCMDP, val);
-	while (inb(KBSTATP)&KBS_IBF);
-	return (inb(KBDATAP));
-}
-
-/*
- * find an ISA device in a given isa_devtab_* table, given
- * the table to search, the expected id_driver entry, and the unit number.
- *
- * this function is defined in isa_device.h, and this location is debatable;
- * i put it there because it's useless w/o, and directly operates on
- * the other stuff in that file.
- *
- */
-
-struct isa_device *find_isadev(table, driverp, unit)
-	struct isa_device *table;
-	struct isa_driver *driverp;
-	int unit;
-{
-	if (driverp == NULL) /* sanity check */
-		return NULL;
-
-	while ((table->id_driver != driverp) || (table->id_unit != unit)) {
-		if (table->id_driver == 0)
-			return NULL;
-    
-		table++;
-        }
-
-	return table;
-}
-
-/*
- * Return nonzero if a (masked) irq is pending for a given device.
- */
-int
-isa_irq_pending(dvp)
-	struct isa_device *dvp;
-{
-	unsigned id_irq;
-
-	id_irq = (unsigned short) dvp->id_irq;	/* XXX silly type in struct */
-	if (id_irq & 0xff)
-		return (inb(IO_ICU1) & id_irq);
-	return (inb(IO_ICU2) & (id_irq >> 8));
 }
