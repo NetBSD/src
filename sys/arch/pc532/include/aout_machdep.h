3ebec8fc4b4998ea4c5c07cb0faedb10e6523945
pk <pk@NetBSD.org>
1993-10-18 09:46:56 +0000

    Put definition of __LDPGSZ and `struct relocation_info' where they belong: in <machine/exec.h>.

--- sys/arch/pc532/include/aout_machdep.h
+++ sys/arch/pc532/include/aout_machdep.h
@@ -1,5 +1,49 @@
-/* This is an empty file!   Phil Nelson.
+/*
+ * Copyright (c) 1993 Paul Kranenburg
+ * All rights reserved.
  *
- *	$Id: aout_machdep.h,v 1.1.1.1 1993/09/09 23:53:47 phil Exp $
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software withough specific prior written permission
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *	$Id: aout_machdep.h,v 1.2 1993/10/18 09:47:34 pk Exp $
  */
 
+#ifndef _PC532_EXEC_H_
+#define _PC532_EXEC_H_
+
+#define __LDPGSZ	4096
+
+/* Relocation format. */
+struct relocation_info_pc532 {
+	int r_address;			/* offset in text or data segment */
+	unsigned int r_symbolnum : 24,	/* ordinal number of add symbol */
+			 r_pcrel :  1,	/* 1 if value should be pc-relative */
+			r_length :  2,	/* log base 2 of value's width */
+			r_extern :  1,	/* 1 if need to add symbol to value */
+		       r_baserel :  1,	/* linkage table relative */
+		      r_jmptable :  1,	/* relocate to jump table */
+		      r_relative :  1,	/* load address relative */
+			  r_copy :  1;	/* run time copy */
+};
+#define relocation_info	relocation_info_pc532
+
+#endif  /* _PC532_EXEC_H_ */
--- sys/arch/sun3/include/exec.h
+++ sys/arch/sun3/include/exec.h
@@ -45,4 +45,20 @@
  */
 #define cpu_exec_checkmid(mid) (mid == MID_SUN3)
 
+#define __LDPGSZ	8192
+
+/* Relocation format. */
+struct relocation_info_sun3 {
+	int r_address;			/* offset in text or data segment */
+	unsigned int r_symbolnum : 24,	/* ordinal number of add symbol */
+			 r_pcrel :  1,	/* 1 if value should be pc-relative */
+			r_length :  2,	/* log base 2 of value's width */
+			r_extern :  1,	/* 1 if need to add symbol to value */
+		       r_baserel :  1,	/* linkage table relative */
+		      r_jmptable :  1,	/* relocate to jump table */
+		      r_relative :  1,	/* load address relative */
+			  r_copy :  1;	/* run time copy */
+};
+#define relocation_info	relocation_info_sun3
+
 #endif  /* _SUN3_EXEC_H_ */
