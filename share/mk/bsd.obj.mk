5f2e8a5a97b1ccc4dcbfef02b679cefa12a282c2
cgd <cgd@NetBSD.org>
1993-10-21 05:27:40 +0000

    'obj' was missing in echo which informed of symlink creation

--- share/mk/bsd.obj.mk
+++ share/mk/bsd.obj.mk
@@ -1,4 +1,4 @@
-#	$Id: bsd.obj.mk,v 1.1 1993/08/15 19:26:09 mycroft Exp $
+#	$Id: bsd.obj.mk,v 1.2 1993/10/21 05:27:40 cgd Exp $
 
 .if !target(obj)
 .if defined(NOOBJ)
@@ -9,7 +9,7 @@ obj:
 	here=`pwd`; subdir=`echo $$here | sed 's,^/usr/src/,,'`; \
 	if test $$here != $$subdir ; then \
 		dest=/usr/obj/$$subdir ; \
-		echo "$$here -> $$dest"; ln -s $$dest obj; \
+		echo "$$here/obj -> $$dest"; ln -s $$dest obj; \
 		if test -d /usr/obj -a ! -d $$dest; then \
 			mkdir -p $$dest; \
 		else \
