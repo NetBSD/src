bdcfd1a1c6d27f2970f1fe8dee1f16b1d0a72061
phil <phil@NetBSD.org>
1993-10-25 06:18:47 +0000

    Adding the proper stuff that I forgot.  (POSIX/ANSI)

--- sys/arch/pc532/include/limits.h
+++ sys/arch/pc532/include/limits.h
@@ -46,22 +46,24 @@
 
 #define	UCHAR_MAX	0xff		/* max value for an unsigned char */
 #define	CHAR_MAX	0x7f		/* max value for a char */
-#define	CHAR_MIN	0x80		/* min value for a char */
+#define	CHAR_MIN	(-0x7f-1)	/* min value for a char */
 
 #define	USHRT_MAX	0xffff		/* max value for an unsigned short */
 #define	SHRT_MAX	0x7fff		/* max value for a short */
-#define	SHRT_MIN	0x8000		/* min value for a short */
+#define SHRT_MIN        (-0x7fff-1)     /* min value for a short */
 
 #define	UINT_MAX	0xffffffff	/* max value for an unsigned int */
 #define	INT_MAX		0x7fffffff	/* max value for an int */
-#define	INT_MIN		0x80000000	/* min value for an int */
+#define	INT_MIN		(-0x7fffffff-1)	/* min value for an int */
 
 #define	ULONG_MAX	0xffffffff	/* max value for an unsigned long */
 #define	LONG_MAX	0x7fffffff	/* max value for a long */
-#define	LONG_MIN	0x80000000	/* min value for a long */
+#define	LONG_MIN	(-0x7fffffff-1)	/* min value for a long */
 
-#define UQUAD_MAX	0xffffffffffffffffLL	/* max value, unsigned quad */
-#define QUAD_MAX	0x7fffffffffffffffLL	/* max value, quad */
-#define QUAD_MIN	0x8000000000000000LL	/* min value, quad */
+#if !defined(_ANSI_SOURCE) && !defined(_POSIX_SOURCE)
+#define UQUAD_MAX	0xffffffffffffffffLL	   /* max unsigned quad */
+#define QUAD_MAX	0x7fffffffffffffffLL	   /* max quad */
+#define QUAD_MIN	(-0x7fffffffffffffffLL-1)  /* min quad */
+#endif
 
 #endif
