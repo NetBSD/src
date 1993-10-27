429bd3d47d08ae911db156aa1e5e859164f60c33
deraadt <deraadt@NetBSD.org>
1993-10-27 18:12:47 +0000

    back out of Torek's cache enable code since it makes 1/1+-style esp chips blow chunks.

--- sys/arch/sparc/sparc/machdep.c
+++ sys/arch/sparc/sparc/machdep.c
@@ -42,7 +42,7 @@
  *	@(#)machdep.c	8.1 (Berkeley) 6/11/93
  *
  * from: Header: machdep.c,v 1.41 93/05/27 04:39:05 torek Exp 
- * $Id: machdep.c,v 1.8 1993/10/15 06:13:17 deraadt Exp $
+ * $Id: machdep.c,v 1.9 1993/10/27 18:12:49 deraadt Exp $
  */
 
 #include <sys/param.h>
@@ -238,6 +238,12 @@ cpu_startup()
 	 * Configure the system.  The cpu code will turn on the cache.
 	 */
 	configure();
+
+	/*
+	 * Turn on the cache (do after configuration due to a bug in
+	 * some versions of the SPARC chips -- this info from Gilmore).
+	 */
+	cache_enable();
 }
 
 /*
