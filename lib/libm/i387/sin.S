6b3622b0070f87b63c4eff58fe5b5780d04d5122
jtc <jtc@NetBSD.org>
1993-10-28 19:02:53 +0000

    More work on i387 specific math library

--- lib/libm/i387/sin.S
+++ lib/libm/i387/sin.S
@@ -36,12 +36,13 @@
 #include <machine/asm.h>
 
 ENTRY(sin)
-	fldl	4(%esp)
-	fsin
-	fstsw	%ax
+	fldpi				/* Pi */
+	fadd	%st(0),%st		/* 2 Pi */
+	fldl	4(%esp)			/* Theta */
+1:	fprem1
+	fstswl	%ax
 	sahf
-	jp	1f
-
-	/* handle infinity, range reduction, etc here. */
-
-1:	ret
+	jp	1b
+	fstpl	%st(1)
+	fsin
+	ret
