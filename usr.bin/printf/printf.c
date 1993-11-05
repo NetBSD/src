14e76f48d8ce006e018842f2570daea29940ea09
jtc <jtc@NetBSD.org>
1993-11-05 20:12:38 +0000

    Changes required to make printf utility POSIX.2 compliant:  * Escape characters in the string needed to be processed as they were    encountered, otherwise a "\000" octal constant would prematurely    terminate the formatting string.  * Implemented the %b, SysV echo(1) compatibility, formatting directive.

--- usr.bin/printf/printf.c
+++ usr.bin/printf/printf.c
@@ -39,12 +39,33 @@ char copyright[] =
 
 #ifndef lint
 /*static char sccsid[] = "from: @(#)printf.c	5.9 (Berkeley) 6/1/90";*/
-static char rcsid[] = "$Id: printf.c,v 1.3 1993/08/01 18:10:03 mycroft Exp $";
+static char rcsid[] = "$Id: printf.c,v 1.4 1993/11/05 20:12:40 jtc Exp $";
 #endif /* not lint */
 
-#include <sys/types.h>
+#include <ctype.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <limits.h>
+#include <locale.h>
+#include <err.h>
+
+void	do_printf();
+void	print_escape_str();
+int	print_escape();
+
+int	getchr();
+int	getint();
+long	getlong();
+double	getdouble();
+char   *getstr();
+     
+int	rval;
+char  **gargv;
+
+#define isodigit(c)	((c) >= '0' && (c) <= '7')
+#define octtobin(c)	((c) - '0')
+#define hextobin(c)	((c) >= 'A' && 'c' <= 'F' ? c - 'A' + 10 : (c) >= 'a' && (c) <= 'f' ? c - 'a' + 10 : c - '0')
 
 #define PF(f, func) { \
 	if (fieldwidth) \
@@ -58,23 +79,38 @@ static char rcsid[] = "$Id: printf.c,v 1.3 1993/08/01 18:10:03 mycroft Exp $";
 		(void)printf(f, func); \
 }
 
-char **gargv;
-
+int
 main(argc, argv)
 	int argc;
 	char **argv;
 {
-	static char *skip1, *skip2;
-	register char *format, *fmt, *start;
-	register int end, fieldwidth, precision;
-	char convch, nextch, *getstr(), *mklong();
-	double getdouble();
-	long getlong();
+	char *format;
+
+	setlocale (LC_ALL, "");
 
 	if (argc < 2) {
 		fprintf(stderr, "usage: printf format [arg ...]\n");
 		exit(1);
 	}
+
+	format = argv[1];
+	gargv  = argv + 2;
+
+	do {
+		do_printf(format);
+	} while (*gargv);
+
+	exit (rval);
+}
+
+void
+do_printf(format)
+	char *format;
+{
+	static char *skip1, *skip2;
+	register char *fmt, *start;
+	register int fieldwidth, precision;
+	char convch, nextch, *mklong();
 
 	/*
 	 * Basic algorithm is to scan the format string for conversion
@@ -87,99 +123,233 @@ main(argc, argv)
 	skip1 = "#-+ 0";
 	skip2 = "*0123456789";
 
-	escape(fmt = format = *++argv);		/* backslash interpretation */
-	gargv = ++argv;
-	for (;;) {
-		end = 0;
-		/* find next format specification */
-next:		for (start = fmt;; ++fmt) {
-			if (!*fmt) {
-				/* avoid infinite loop */
-				if (end == 1) {
-					fprintf(stderr,
-					    "printf: missing format character.\n");
-					exit(1);
-				}
-				end = 1;
-				if (fmt > start)
-					(void)printf("%s", start);
-				if (!*gargv)
-					exit(0);
-				fmt = format;
-				goto next;
-			}
-			/* %% prints a % */
+	/* find next format specification */
+	for (fmt = format; *fmt; fmt++) {
+		switch (*fmt) {
+		case '%':
+			start = fmt++;
+
 			if (*fmt == '%') {
-				if (*++fmt != '%')
-					break;
-				*fmt++ = '\0';
-				(void)printf("%s", start);
-				goto next;
+				putchar ('%');
+				break;
+			} else if (*fmt == 'b') {
+				char *p = getstr();
+				print_escape_str(p);
+				break;
 			}
-		}
 
-		/* skip to field width */
-		for (; index(skip1, *fmt); ++fmt);
-		fieldwidth = *fmt == '*' ? getint() : 0;
+			/* skip to field width */
+			for (; index(skip1, *fmt); ++fmt);
+			fieldwidth = *fmt == '*' ? getint() : 0;
 
-		/* skip to possible '.', get following precision */
-		for (; index(skip2, *fmt); ++fmt);
-		if (*fmt == '.')
-			++fmt;
-		precision = *fmt == '*' ? getint() : 0;
+			/* skip to possible '.', get following precision */
+			for (; index(skip2, *fmt); ++fmt);
+			if (*fmt == '.')
+				++fmt;
+			precision = *fmt == '*' ? getint() : 0;
 
-		/* skip to conversion char */
-		for (; index(skip2, *fmt); ++fmt);
-		if (!*fmt) {
-			fprintf(stderr, "printf: missing format character.\n");
-			exit(1);
-		}
+			for (; index(skip2, *fmt); ++fmt);
+			if (!*fmt) {
+			        warnx ("missing format character");
+				rval = 1;
+				return;
+			}
 
-		convch = *fmt;
-		nextch = *++fmt;
-		*fmt = '\0';
-		switch(convch) {
-		case 'c': {
-			char p = getchr();
-			PF(start, p);
+			convch = *fmt;
+			nextch = *(fmt + 1);
+			*(fmt + 1) = '\0';
+			switch(convch) {
+			case 'c': {
+				char p = getchr();
+				PF(start, p);
+				break;
+			}
+			case 's': {
+				char *p = getstr();
+				PF(start, p);
+				break;
+			}
+			case 'd': case 'i': case 'o': case 'u': case 'x': case 'X': {
+				char *f = mklong(start, convch);
+				long p = getlong();
+				PF(f, p);
+				free(f);
+				break;
+			}
+			case 'e': case 'E': case 'f': case 'g': case 'G': {
+				double p = getdouble();
+				PF(start, p);
+				break;
+			}
+			default:
+			        warnx ("%s: invalid directive", start);
+			        rval = 1;
+			}
+			*(fmt + 1) = nextch;
 			break;
-		}
-		case 's': {
-			char *p = getstr();
-			PF(start, p);
+
+		case '\\':
+			fmt += print_escape(fmt);
 			break;
-		}
-		case 'd': case 'i': case 'o': case 'u': case 'x': case 'X': {
-			char *f = mklong(start, convch);
-			long p = getlong();
-			PF(f, p);
-			break;
-		}
-		case 'e': case 'E': case 'f': case 'g': case 'G': {
-			double p = getdouble();
-			PF(start, p);
-			break;
-		}
+
 		default:
-			fprintf(stderr, "printf: illegal format character.\n");
-			exit(1);
+			putchar (*fmt);
+			break;
 		}
-		*fmt = nextch;
 	}
-	/* NOTREACHED */
 }
+
+
+/*
+ * Print SysV echo(1) style escape string 
+ */
+void
+print_escape_str(str)
+	register char *str;
+{
+	int value;
+	int c;
+
+	while (*str) {
+		if (*str == '\\') {
+			str++;
+			/* 
+			 * %b string octal constants are not like those in C.
+			 * They start with a \0, and are followed by 0, 1, 2, 
+			 * or 3 octal digits. 
+			 */
+			if (*str == '0') {
+				str++;
+				for (c = 3, value = 0; c-- && isodigit(*str); str++) {
+					value <<= 3;
+					value += octtobin(*str);
+				}
+				putchar (value);
+				str--;
+			} else if (*str == 'c') {
+				exit (rval);
+			} else {
+				str--;			
+				str += print_escape(str);
+			}
+		} else {
+			putchar (*str);
+		}
+		str++;
+	}
+}
+
+/*
+ * Print "standard" escape characters 
+ */
+int
+print_escape(str)
+	register char *str;
+{
+	char *start = str;
+	int c;
+	int value;
+
+	str++;
+
+	switch (*str) {
+	case '0': case '1': case '2': case '3':
+	case '4': case '5': case '6': case '7':
+		for (c = 3, value = 0; c-- && isodigit(*str); str++) {
+			value <<= 3;
+			value += octtobin(*str);
+		}
+		putchar(value);
+		return str - start - 1;
+		/* NOTREACHED */
+
+	case 'x':
+		str++;
+		for (value = 0; isxdigit(*str); str++) {
+			value <<= 4;
+			value += hextobin(*str);
+		}
+		if (value > UCHAR_MAX) {
+			warnx ("escape sequence out of range for character");
+			rval = 1;
+		}
+		putchar (value);
+		return str - start - 1;
+		/* NOTREACHED */
+
+	case '\\':			/* backslash */
+		putchar('\\');
+		break;
+
+	case '\'':			/* single quote */
+		putchar('\'');
+		break;
+
+	case '"':			/* double quote */
+		putchar('"');
+		break;
+
+	case 'a':			/* alert */
+#ifdef __STDC__
+		putchar('\a');
+#else
+		putchar(007);
+#endif
+		break;
+
+	case 'b':			/* backspace */
+		putchar('\b');
+		break;
+
+	case 'e':			/* escape */
+#ifdef __GNUC__
+		putchar('\e');
+#else
+		putchar(033);
+#endif
+		break;
+
+	case 'f':			/* form-feed */
+		putchar('\f');
+		break;
+
+	case 'n':			/* newline */
+		putchar('\n');
+		break;
+
+	case 'r':			/* carriage-return */
+		putchar('\r');
+		break;
+
+	case 't':			/* tab */
+		putchar('\t');
+		break;
+
+	case 'v':			/* vertical-tab */
+		putchar('\v');
+		break;
+
+	default:
+		putchar(*str);
+		warnx("unknown escape sequence `\\%c'", *str);
+		rval = 1;
+	}
+
+	return 1;
+}
+
 
 char *
 mklong(str, ch)
 	char *str, ch;
 {
 	int len;
-	char *copy, *malloc();
+	char *copy;
 
 	len = strlen(str) + 2;
-	if (!(copy = malloc((u_int)len))) {	/* never freed; XXX */
-		fprintf(stderr, "printf: out of memory.\n");
-		exit(1);
+	if (!(copy = malloc((unsigned int)len))) {
+	        err(1, NULL);
+		/* NOTREACHED */
 	}
 	bcopy(str, copy, len - 3);
 	copy[len - 3] = 'l';
@@ -188,66 +358,7 @@ mklong(str, ch)
 	return(copy);
 }
 
-escape(fmt)
-	register char *fmt;
-{
-	register char *store;
-	register int value, c;
-
-	for (store = fmt; c = *fmt; ++fmt, ++store) {
-		if (c != '\\') {
-			*store = c;
-			continue;
-		}
-		switch (*++fmt) {
-		case '\0':		/* EOS, user error */
-			*store = '\\';
-			*++store = '\0';
-			return;
-		case '\\':		/* backslash */
-		case '\'':		/* single quote */
-			*store = *fmt;
-			break;
-		case 'a':		/* bell/alert */
-			*store = '\7';
-			break;
-		case 'b':		/* backspace */
-			*store = '\b';
-			break;
-		case 'f':		/* form-feed */
-			*store = '\f';
-			break;
-		case 'n':		/* newline */
-			*store = '\n';
-			break;
-		case 'r':		/* carriage-return */
-			*store = '\r';
-			break;
-		case 't':		/* horizontal tab */
-			*store = '\t';
-			break;
-		case 'v':		/* vertical tab */
-			*store = '\13';
-			break;
-					/* octal constant */
-		case '0': case '1': case '2': case '3':
-		case '4': case '5': case '6': case '7':
-			for (c = 3, value = 0;
-			    c-- && *fmt >= '0' && *fmt <= '7'; ++fmt) {
-				value <<= 3;
-				value += *fmt - '0';
-			}
-			--fmt;
-			*store = value;
-			break;
-		default:
-			*store = *fmt;
-			break;
-		}
-	}
-	*store = '\0';
-}
-
+int
 getchr()
 {
 	if (!*gargv)
@@ -264,46 +375,64 @@ getstr()
 }
 
 static char *number = "+-.0123456789";
+int
 getint()
 {
 	if (!*gargv)
 		return(0);
+
 	if (index(number, **gargv))
 		return(atoi(*gargv++));
-	return(asciicode());
+
+	return 0;
 }
 
 long
 getlong()
 {
-	long atol();
+	long val;
+	char *ep;
 
 	if (!*gargv)
-		return((long)0);
-	if (index(number, **gargv))
-		return(strtol(*gargv++, (char **)NULL, 0));
-	return((long)asciicode());
+		return(0L);
+
+	if (**gargv == '\"' || **gargv == '\'') {
+		val = gargv[0][1];
+		gargv++;
+		return val;
+	}
+
+	val = strtol (*gargv, &ep, 0);
+	if (*ep) {
+		warnx ("incompletely converted argument: %s", *gargv);
+		rval = 1;
+	}
+
+	gargv++;
+	return val;
 }
 
 double
 getdouble()
 {
-	double atof();
+	double val;
+	char *ep;
 
 	if (!*gargv)
-		return((double)0);
-	if (index(number, **gargv))
-		return(atof(*gargv++));
-	return((double)asciicode());
-}
+		return(0.0);
 
-asciicode()
-{
-	register char ch;
+	if (**gargv == '\"' || **gargv == '\'') {
+		val = gargv[0][1];
+		gargv++;
+		return val;
+	}
 
-	ch = **gargv;
-	if (ch == '\'' || ch == '"')
-		ch = (*gargv)[1];
-	++gargv;
-	return(ch);
+	val = strtod (*gargv, &ep);
+	if (*ep) {
+		warnx ("incompletely converted argument: %s", *gargv);
+		rval = 1;
+	}
+
+	gargv++;
+	return val;
 }
