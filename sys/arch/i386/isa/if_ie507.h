5bfdb674d8b12f9b6f997b4b746c654d31c6d34e
mycroft <mycroft@NetBSD.org>
1993-11-08 20:15:32 +0000

    A slightly unfinished i82586 driver, based on Garrett Wollman's.

---
+++ sys/arch/i386/isa/if_ie507.h
@@ -0,0 +1,13 @@
+/*
+ * $Id: if_ie507.h,v 1.1.2.2 1993/11/08 20:15:58 mycroft Exp $
+ * Definitions for 3C507
+ */
+
+#define	IE507_CONTROL	6	/* control port */
+#define IE507_ATTN 	11	/* any write here sends a Chan attn */
+#define	IE507_MADDR	14	/* shared memory configuration */
+#define	IE507_IRQ	15	/* IRQ configuration */
+
+#define	EL_CTRL_WIDTH	0x10	/* bus width; clear = 8-bit, set = 16-bit */
+#define	EL_CTRL_ONLINE	0x80	/* turn off to reset */
+
