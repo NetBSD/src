356fb280750af1a8bf693551ff80831aeb1fb239
proven <proven@NetBSD.org>
1993-11-15 10:02:18 +0000

    OK one more try at getting it right ...

--- lib/libpthread/arch/i386/machdep.h
+++ lib/libpthread/arch/i386/machdep.h
@@ -4,9 +4,6 @@
  */
 
 #include <pthread/copyright.h>
-#include <unistd.h>
-#include <setjmp.h>
-#include <sys/time.h>
 
 /*
  * The first machine dependent functions are the SEMAPHORES
@@ -28,52 +25,7 @@ temp;                                   \
 #define SEMAPHORE_RESET(lock)           *lock = SEMAPHORE_CLEAR
 
 /*
- * New types
- */
-typedef long    semaphore;
-
-#define SIGMAX	31
-
-/*
- * New Strutures
- */
-struct machdep_pthread {
-    void        		*(*start_routine)(void *);
-    void        		*start_argument;
-    void        		*machdep_stack;
-	struct itimerval	machdep_timer;
-    jmp_buf     		machdep_state;
-};
-
-/*
- * Static machdep_pthread initialization values.
- * For initial thread only.
- */
-#define MACHDEP_PTHREAD_INIT    \
-{ NULL, NULL, NULL, { { 0, 0 }, { 0, 0 } }, 0 }
-
-/*
  * Minimum stack size
  */
 #define PTHREAD_STACK_MIN	1024
 
-/*
- * Some fd flag defines that are necessary to distinguish between posix
- * behavior and bsd4.3 behavior.
- */
-#define __FD_NONBLOCK 		O_NONBLOCK
-
-/*
- * New functions
- */
-
-__BEGIN_DECLS
-
-#if defined(PTHREAD_KERNEL)
-
-int semaphore_text_and_set  __P((semaphore *));
-int machdep_save_state      __P((void));
-
-#endif
-
-__END_DECLS
