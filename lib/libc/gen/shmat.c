c270510914314e9388ec0cd9a4f8c634ebd6c97e
mycroft <mycroft>
1993-11-19 03:18:24 +0000

    Clean up after Brezak.

--- lib/libc/gen/shmat.c
+++ lib/libc/gen/shmat.c
@@ -1,5 +1,5 @@
 #if defined(LIBC_SCCS) && !defined(lint)
-static char *rcsid = "$Id: shmat.c,v 1.2 1993/08/26 15:26:18 brezak Exp $";
+static char *rcsid = "$Id: shmat.c,v 1.3 1993/11/19 03:18:24 mycroft Exp $";
 #endif /* LIBC_SCCS and not lint */
 
 #include <sys/types.h>
@@ -7,11 +7,11 @@ static char *rcsid = "$Id: shmat.c,v 1.2 1993/08/26 15:26:18 brezak Exp $";
 #include <sys/shm.h>
 
 #if __STDC__
-int shmat(int shmid, caddr_t shmaddr, int shmflg)
+int shmat(int shmid, void *shmaddr, int shmflg)
 #else
 int shmat(shmid, shmaddr, shmflg)
 	int shmid;
-	caddr_t shmaddr;
+	void *shmaddr;
 	int shmflg;
 #endif
 {
