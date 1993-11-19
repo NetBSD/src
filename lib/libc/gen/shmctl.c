ce351a47d2ad4ecb1bb2c4950b1e3aea80ed48d6
mycroft <mycroft@NetBSD.org>
1993-11-19 03:18:24 +0000

    Clean up after Brezak.

--- lib/libc/gen/shmctl.c
+++ lib/libc/gen/shmctl.c
@@ -1,5 +1,5 @@
 #if defined(LIBC_SCCS) && !defined(lint)
-static char *rcsid = "$Id: shmctl.c,v 1.2 1993/08/26 15:26:19 brezak Exp $";
+static char *rcsid = "$Id: shmctl.c,v 1.3 1993/11/19 03:18:29 mycroft Exp $";
 #endif /* LIBC_SCCS and not lint */
 
 #include <sys/types.h>
@@ -7,12 +7,12 @@ static char *rcsid = "$Id: shmctl.c,v 1.2 1993/08/26 15:26:19 brezak Exp $";
 #include <sys/shm.h>
 
 #if __STDC__
-int shmctl(int shmid, int cmd, caddr_t buf)
+int shmctl(int shmid, int cmd, struct shmid_ds *buf)
 #else
 int shmctl(shmid, cmd, buf)
 	int shmid;
 	int cmd;
-	caddr_t buf;
+	struct shmid_ds *buf;
 #endif
 {
 	return (shmsys(1, shmid, cmd, buf));
