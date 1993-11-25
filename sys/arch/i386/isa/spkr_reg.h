3a5be743846407376bada8a4b60c5c70d67a9050
mycroft <mycroft@NetBSD.org>
1993-11-25 20:16:30 +0000

    Conversion almost finished.

--- sys/arch/i386/isa/spkr_reg.h
+++ sys/arch/i386/isa/spkr_reg.h
@@ -1,7 +1,7 @@
 /*
  * PIT port addresses and speaker control values
  *
- *	$Id: spkr_reg.h,v 1.2 1993/08/02 17:52:36 mycroft Exp $
+ *	$Id: spkr_reg.h,v 1.2.2.1 1993/11/25 20:16:32 mycroft Exp $
  */
 
 #define PITAUX_PORT	0x61	/* port of Programmable Peripheral Interface */
@@ -9,3 +9,4 @@
 #define PIT_SPKRDATA	0x02	/* Direct to speaker */
 
 #define PIT_SPKR	(PIT_ENABLETMR2|PIT_SPKRDATA)
+#define	PIT_MODE	(TIMER_SEL2|TIMER_MSB|TIMER_SQWAVE)
