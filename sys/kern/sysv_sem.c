f3f11afc0442888580591e3f25fe64116776dc31
mycroft <mycroft@NetBSD.org>
1993-11-27 19:28:28 +0000

    Remove remaining sleep()s.

--- sys/kern/sysv_sem.c
+++ sys/kern/sysv_sem.c
@@ -60,7 +60,7 @@ semsys(p, uap, retval)
 {
 	while ( semlock_holder != NULL && semlock_holder != p ) {
 	    /* printf("semaphore facility locked - sleeping ...\n"); */
-	    sleep( (caddr_t)&semlock_holder, (PZERO - 4) );
+	    tsleep( (caddr_t)&semlock_holder, (PZERO - 4), "semsys", 0 );
 	}
 
 	if (uap->which >= sizeof(semcalls)/sizeof(semcalls[0]))
@@ -1002,7 +1002,7 @@ semexit(p)
 #ifdef SEM_DEBUG
 	printf("semaphore facility locked - sleeping ...\n");
 #endif
-	sleep( (caddr_t)&semlock_holder, (PZERO - 4) );
+	tsleep( (caddr_t)&semlock_holder, (PZERO - 4), "semexit", 0 );
     }
 
     did_something = 0;
