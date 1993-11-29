309cf908cdb12b75c1256b07570663f8524adf7c
mycroft <mycroft@NetBSD.org>
1993-11-29 04:57:43 +0000

    Fix screwy indentation.

--- sys/sys/chio.h
+++ sys/sys/chio.h
@@ -31,7 +31,7 @@
  * SUCH DAMAGE.
  *
  *	from: @(#)mtio.h	7.6 (Berkeley) 2/5/91
- *	$Id: chio.h,v 1.3 1993/05/20 16:21:55 cgd Exp $
+ *	$Id: chio.h,v 1.3.4.1 1993/11/29 04:57:43 mycroft Exp $
  */
 
 /* This is a "converted" mtio.h from 386BSD 
@@ -49,62 +49,61 @@
 #define CH_ADDR_MASK		0xffff
 struct chop {
 	short	ch_op;		/* operations defined below */
-	short	result;		/* The result		    */
+	short	result;		/* the result */
 	union {
-	   struct {
-		int chm;		/* Transport element */
-		int from;
-		int to;
-	   } move;
-	   struct {
-		int chm;		/* Transport element */
-		int to;
-	   } position; 
-	   struct {
-	        short   chmo;                   /* Offset of first CHM */
-	        short   chms;                   /* No. of CHM */
-	        short   slots;                  /* No. of Storage Elements */
-                short   sloto;                  /* Offset of first SE */
-                short   imexs;                  /* No. of Import/Export Slots */
-                short   imexo;                  /* Offset of first IM/EX */
-                short   drives;                 /* No. of CTS */
-                short   driveo;                 /* Offset of first CTS */
-                short   rot;                    /* CHM can rotate */
-	   } getparam;
-	   struct {
-		int type;
+		struct {
+			int chm;		/* Transport element */
+			int from;
+			int to;
+		} move;
+		struct {
+			int chm;		/* Transport element */
+			int to;
+		} position; 
+		struct {
+			short   chmo;		/* Offset of first CHM */
+			short   chms;		/* No. of CHM */
+			short   slots;		/* No. of Storage Elements */
+			short   sloto;		/* Offset of first SE */
+			short   imexs;		/* No. of Import/Export Slots */
+			short   imexo;		/* Offset of first IM/EX */
+			short   drives;		/* No. of CTS */
+			short   driveo;		/* Offset of first CTS */
+			short   rot;		/* CHM can rotate */
+		} getparam;
+		struct {
+			int type;
 #define CH_CHM	1
 #define CH_STOR	2
 #define CH_IMEX	3
 #define CH_CTS	4
-		int from;
-		struct {
-			u_char elema_1;
-			u_char elema_0;
-			u_char full:1;
-			u_char rsvd:1;
-			u_char except:1;
-			u_char :5;
-			u_char rsvd2;
-			union {
-				struct {
-				u_char add_sense_code;
-				u_char add_sense_code_qualifier;
-				} specs;
-				short add_sense;
+			int from;
+			struct {
+				u_char elema_1;
+				u_char elema_0;
+				u_char full:1;
+				u_char rsvd:1;
+				u_char except:1;
+				u_char :5;
+				u_char rsvd2;
+				union {
+					struct {
+						u_char add_sense_code;
+						u_char add_sense_code_qualifier;
+					} specs;
+					short add_sense;
 /* WARINING LSB only */
 #define CH_CHOLDER	0x0290	/* Cartridge holder is missing */
 #define CH_STATUSQ	0x0390	/* Status is questionable */
 #define CH_CTS_CLOSED	0x0490	/* CTS door is closed */
-
-			} ch_add_sense;
-			u_char rsvd3[3];
-			u_char :6;
-			u_char invert:1;
-			u_char svalid:1;
-			u_char source_1;
-			u_char source_0;
-			u_char rsvd4[4];
+				} ch_add_sense;
+				u_char rsvd3[3];
+				u_char :6;
+				u_char invert:1;
+				u_char svalid:1;
+				u_char source_1;
+				u_char source_0;
+				u_char rsvd4[4];
 			} elem_data;
 		} get_elem_stat;
 	} u;
