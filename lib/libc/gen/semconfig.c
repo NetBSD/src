e8e41c4205eba68f49f96dc9f8efcafc35691b30
cgd <cgd@NetBSD.org>
1993-12-01 02:26:50 +0000

    fix typo; from Louis A. Mamakos <louie@TransSys.COM>

--- lib/libc/gen/semconfig.c
+++ lib/libc/gen/semconfig.c
@@ -1,5 +1,5 @@
 /*
- *	$Id: semconfig.c,v 1.1 1993/11/14 12:40:30 cgd Exp $
+ *	$Id: semconfig.c,v 1.2 1993/12/01 02:26:50 cgd Exp $
  */     
 
 #include <sys/types.h>
@@ -9,7 +9,7 @@
 #if __STDC__
 int semconfig(int cmd, int p1, int p2, int p3)
 #else
-int semctl(cmd, p1, p2, p3)
+int semconfig(cmd, p1, p2, p3)
 	int cmd, p1, p2, p3;
 #endif
 {
