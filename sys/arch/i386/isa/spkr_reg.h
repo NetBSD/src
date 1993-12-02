4a7f06fe704daeddd2415ed772d5cde3750b4095
mycroft <mycroft@NetBSD.org>
1993-12-02 05:09:49 +0000

    Change TIMER_MSB to TIMER_16BIT in PIT_MODE.

--- sys/arch/i386/isa/spkr_reg.h
+++ sys/arch/i386/isa/spkr_reg.h
@@ -1,7 +1,7 @@
 /*
  * PIT port addresses and speaker control values
  *
- *	$Id: spkr_reg.h,v 1.2.2.1 1993/11/25 20:16:32 mycroft Exp $
+ *	$Id: spkr_reg.h,v 1.2.2.2 1993/12/02 05:09:49 mycroft Exp $
  */
 
 #define PITAUX_PORT	0x61	/* port of Programmable Peripheral Interface */
@@ -9,4 +9,4 @@
 #define PIT_SPKRDATA	0x02	/* Direct to speaker */
 
 #define PIT_SPKR	(PIT_ENABLETMR2|PIT_SPKRDATA)
-#define	PIT_MODE	(TIMER_SEL2|TIMER_MSB|TIMER_SQWAVE)
+#define	PIT_MODE	(TIMER_SEL2|TIMER_16BIT|TIMER_SQWAVE)
