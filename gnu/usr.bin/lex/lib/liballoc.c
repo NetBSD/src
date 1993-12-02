e00306a5bb9e4c795378c6f07c5c5c425346b234
jtc <jtc@NetBSD.org>
1993-12-02 19:14:27 +0000

    Lex library for Flex 2.4.2.

---
+++ gnu/usr.bin/lex/lib/liballoc.c
@@ -0,0 +1,53 @@
+/* liballoc - flex run-time memory allocation */
+
+/* $Header: /cvsroot/src/gnu/usr.bin/lex/lib/Attic/liballoc.c,v 1.1 1993/12/02 19:14:28 jtc Exp $ */
+
+#include <stdio.h>
+
+#ifdef STDC_HEADERS
+
+#include <stdlib.h>
+
+#else /* not STDC_HEADERS */
+
+void *malloc();
+void *realloc();
+void free();
+
+#endif /* not STDC_HEADERS */
+
+
+void *yy_flex_alloc( size )
+int size;
+	{
+	return (void *) malloc( (unsigned) size );
+	}
+
+void *yy_flex_realloc( ptr, size )
+void *ptr;
+int size;
+	{
+	return (void *) realloc( ptr, (unsigned) size );
+	}
+
+void yy_flex_free( ptr )
+void *ptr;
+	{
+	free( ptr );
+	}
+
+/* The following is only used by bison/alloca. */
+void *yy_flex_xmalloc( size )
+int size;
+	{
+	void *result = yy_flex_alloc( size );
+
+	if ( ! result  )
+		{
+		fprintf( stderr,
+			"flex memory allocation failed in yy_flex_xmalloc()" );
+		exit( 1 );
+		}
+
+	return result;
+	}
---
+++ gnu/usr.bin/lex/lib/libyywrap.c
@@ -0,0 +1,8 @@
+/* libyywrap - flex run-time support library "yywrap" function */
+
+/* $Header: /cvsroot/src/gnu/usr.bin/lex/lib/Attic/libyywrap.c,v 1.1 1993/12/02 19:14:33 jtc Exp $ */
+
+int yywrap()
+	{
+	return 1;
+	}
