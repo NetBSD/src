eea9fe5a834761fe0037b67b96cc0604cf604892
mycroft <mycroft@NetBSD.org>
1993-12-02 09:50:34 +0000

    Add `const's to sys_siglist and sys_signame decls.

--- lib/libc/gen/signame.c
+++ lib/libc/gen/signame.c
@@ -33,13 +33,13 @@
 
 #if defined(LIBC_SCCS) && !defined(lint)
 /*static char *sccsid = "from: @(#)siglist.c	5.6 (Berkeley) 2/23/91";*/
-static char *rcsid = "$Id: signame.c,v 1.3 1993/09/08 18:43:05 jtc Exp $";
+static char *rcsid = "$Id: signame.c,v 1.4 1993/12/02 09:53:33 mycroft Exp $";
 #endif /* LIBC_SCCS and not lint */
 
 #include <signal.h>
 #include <unistd.h>
 
-char	*sys_signame[NSIG] = {
+const char *const sys_signame[NSIG] = {
 	"Signal 0",
 	"HUP",		/* SIGHUP */
 	"INT",		/* SIGINT */
