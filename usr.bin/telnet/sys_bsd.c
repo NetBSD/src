c89fb186fbd5c30fd0705c63669cafdc95973e65
mycroft <mycroft@NetBSD.org>
1993-12-02 22:58:58 +0000

    Patch from Frank van der Linden <vdlinden@fwi.uva.nl> to handle 57600 and 115200 baud.  Baud rate checking code disabled completely when using termios.

--- usr.bin/telnet/sys_bsd.c
+++ usr.bin/telnet/sys_bsd.c
@@ -33,7 +33,7 @@
 
 #ifndef lint
 /*static char sccsid[] = "from: @(#)sys_bsd.c	5.2 (Berkeley) 3/1/91";*/
-static char rcsid[] = "$Id: sys_bsd.c,v 1.3 1993/08/01 18:07:20 mycroft Exp $";
+static char rcsid[] = "$Id: sys_bsd.c,v 1.4 1993/12/02 22:58:58 mycroft Exp $";
 #endif /* not lint */
 
 /*
@@ -690,12 +690,31 @@ TerminalNewMode(f)
 
 }
 
+#if defined(USE_TERMIO) && !defined(SYSV_TERMIO)
+    void
+TerminalSpeeds(ispeed, ospeed)
+    long *ispeed;
+    long *ospeed;
+{
+
+    *ispeed = cfgetispeed(&old_tc);
+    *ospeed = cfgetospeed(&old_tc);
+}
+#else /* USE_TERMIO && !SYSV_TERMIO */
 #ifndef	B19200
 # define B19200 B9600
 #endif
 
 #ifndef	B38400
 # define B38400 B19200
+#endif
+
+#ifndef B57600
+# define B57600 B38400
+#endif
+
+#ifndef B115200
+# define B115200 B57600
 #endif
 
 /*
@@ -707,12 +726,13 @@ struct termspeeds {
 	long speed;
 	long value;
 } termspeeds[] = {
-	{ 0,     B0 },     { 50,    B50 },   { 75,    B75 },
-	{ 110,   B110 },   { 134,   B134 },  { 150,   B150 },
-	{ 200,   B200 },   { 300,   B300 },  { 600,   B600 },
-	{ 1200,  B1200 },  { 1800,  B1800 }, { 2400,  B2400 },
-	{ 4800,  B4800 },  { 9600,  B9600 }, { 19200, B19200 },
-	{ 38400, B38400 }, { -1,    B38400 }
+	{ 0,      B0 },      { 50,     B50 },    { 75,     B75 },
+	{ 110,    B110 },    { 134,    B134 },   { 150,    B150 },
+	{ 200,    B200 },    { 300,    B300 },   { 600,    B600 },
+	{ 1200,   B1200 },   { 1800,   B1800 },  { 2400,   B2400 },
+	{ 4800,   B4800 },   { 9600,   B9600 },  { 19200,  B19200 },
+	{ 38400,  B38400 },  { 57600,  B57600 }, { 115200, B115200 },
+	{ -1,     B115200 }
 };
 
     void
@@ -738,6 +758,7 @@ TerminalSpeeds(ispeed, ospeed)
 	tp++;
     *ospeed = tp->speed;
 }
+#endif /* USE_TERMIO && !SYSV_TERMIO */
 
     int
 TerminalWindowSize(rows, cols)
