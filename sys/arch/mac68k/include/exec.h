40366bfe66d684f9d35a5297cdccf983d718df26
briggs <briggs@NetBSD.org>
1993-12-03 22:29:48 +0000

    Added struct relocation_info_mac68k.  Copied from hp300.

--- sys/arch/mac68k/include/exec.h
+++ sys/arch/mac68k/include/exec.h
@@ -41,4 +41,18 @@
 
 /* #define cpu_exec_makecmds(p,epp)	ENOEXEC */
 
+/* Relocation format. */
+struct relocation_info_mac68k {
+	int r_address;			/* offset in text or data segment */
+	unsigned int r_symbolnum : 24,	/* ordinal number of add symbol */
+			 r_pcrel :  1,	/* 1 if value should be pc-relative */
+			r_length :  2,	/* log base 2 of value's width */
+			r_extern :  1,	/* 1 if need to add symbol to value */
+		       r_baserel :  1,	/* linkage table relative */
+		      r_jmptable :  1,	/* relocate to jump table */
+		      r_relative :  1,	/* load address relative */
+			  r_copy :  1;	/* run time copy */
+};
+#define relocation_info	relocation_info_mac68k
+
 #endif
