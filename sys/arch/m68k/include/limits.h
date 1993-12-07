72d0365bf44e3093d7ebbbeb93b2bdf5d22580b1
mycroft <mycroft@NetBSD.org>
1993-12-07 20:44:28 +0000

    Make sure min values are correct when used at a larger size.

--- sys/arch/m68k/include/limits.h
+++ sys/arch/m68k/include/limits.h
@@ -31,30 +31,30 @@
  * SUCH DAMAGE.
  *
  *	from: @(#)limits.h	7.2 (Berkeley) 6/28/90
- *	$Id: limits.h,v 1.4 1993/12/06 07:35:56 mycroft Exp $
+ *	$Id: limits.h,v 1.5 1993/12/07 20:44:28 mycroft Exp $
  */
 
 #define	CHAR_BIT	8		/* number of bits in a char */
 #define	MB_LEN_MAX	1		/* no multibyte characters */
 
-#define	SCHAR_MIN	0x80		/* max value for a signed char */
+#define	SCHAR_MIN	(-0x7f-1)	/* max value for a signed char */
 #define	SCHAR_MAX	0x7f		/* min value for a signed char */
 
 #define	UCHAR_MAX	0xff		/* max value for an unsigned char */
 #define	CHAR_MAX	0x7f		/* max value for a char */
-#define	CHAR_MIN	0x80		/* min value for a char */
+#define	CHAR_MIN	(-0x7f-1)	/* min value for a char */
 
 #define	USHRT_MAX	0xffff		/* max value for an unsigned short */
 #define	SHRT_MAX	0x7fff		/* max value for a short */
-#define	SHRT_MIN	0x8000		/* min value for a short */
+#define	SHRT_MIN	(-0x7fff-1)	/* min value for a short */
 
 #define	UINT_MAX	0xffffffff	/* max value for an unsigned int */
 #define	INT_MAX		0x7fffffff	/* max value for an int */
-#define	INT_MIN		0x80000000	/* min value for an int */
+#define	INT_MIN		(-0x7fffffff-1)	/* min value for an int */
 
 #define	ULONG_MAX	0xffffffff	/* max value for an unsigned long */
 #define	LONG_MAX	0x7fffffff	/* max value for a long */
-#define	LONG_MIN	0x80000000	/* min value for a long */
+#define	LONG_MIN	(-0x7fffffff-1)	/* min value for a long */
 
 #if !defined(_ANSI_SOURCE) && !defined(_POSIX_SOURCE)
 #define	UQUAD_MAX	0xffffffffffffffffLL		/* max unsigned quad */
