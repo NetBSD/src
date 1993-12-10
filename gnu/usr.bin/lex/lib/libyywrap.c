d81b8687c59e8ba820a04d3f5355066f6f6b1d1b
mycroft <mycroft@NetBSD.org>
1993-12-10 16:39:02 +0000

    Clean up deleted files.

--- gnu/usr.bin/lex/lib/libyywrap.c
+++
@@ -1,8 +0,0 @@
-/* libyywrap - flex run-time support library "yywrap" function */
-
-/* $Header: /cvsroot/src/gnu/usr.bin/lex/lib/Attic/libyywrap.c,v 1.2 1993/12/06 19:26:06 jtc Exp $ */
-
-int yywrap()
-	{
-	return 1;
-	}
