36a8337735c6c48357f6c39ec292a1479272961d
cgd <cgd@NetBSD.org>
1993-12-23 07:36:06 +0000

    include <machine/cpu.h> rather than <machine/mtpr.h> -- if the latter exists at all, it's supposed to be included by <machine/cpu.h>

--- sys/net/if_ppp.c
+++ sys/net/if_ppp.c
@@ -69,7 +69,7 @@
  * Paul Mackerras (paulus@cs.anu.edu.au).
  */
 
-/* $Id: if_ppp.c,v 1.5 1993/12/18 00:40:54 mycroft Exp $ */
+/* $Id: if_ppp.c,v 1.6 1993/12/23 07:36:06 cgd Exp $ */
 /* from if_sl.c,v 1.11 84/10/04 12:54:47 rick Exp */
 
 #include "ppp.h"
@@ -134,7 +134,7 @@
 #endif
 
 #include "if_ppp.h"
-#include <machine/mtpr.h>
+#include <machine/cpu.h>
 
 struct ppp_softc ppp_softc[NPPP];
 int ppp_async_out_debug = 0;
