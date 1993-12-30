f10170aac9beba09524cca448e32bf9eed1b8037
jtc <jtc>
1993-12-30 21:15:18 +0000

    Merged our bugfixes with the 4.4BSD find from uunet.

--- usr.bin/find/misc.c
+++ usr.bin/find/misc.c
@@ -1,6 +1,6 @@
 /*-
- * Copyright (c) 1990 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1990, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Cimarron D. Taylor of the University of California, Berkeley.
@@ -35,16 +35,20 @@
  */
 
 #ifndef lint
-/*static char sccsid[] = "from: @(#)misc.c	5.8 (Berkeley) 5/24/91";*/
-static char rcsid[] = "$Id: misc.c,v 1.2 1993/08/01 18:16:12 mycroft Exp $";
+/*static char sccsid[] = "from: @(#)misc.c	8.1 (Berkeley) 6/6/93";*/
+static char rcsid[] = "$Id: misc.c,v 1.3 1993/12/30 21:15:28 jtc Exp $";
 #endif /* not lint */
 
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <sys/errno.h>
+
+#include <err.h>
+#include <errno.h>
+#include <fts.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 #include "find.h"
  
 /*
@@ -64,8 +68,8 @@ brace_subst(orig, store, path, len)
 		if (ch == '{' && orig[1] == '}') {
 			while ((p - *store) + plen > len)
 				if (!(*store = realloc(*store, len *= 2)))
-					err("%s", strerror(errno));
-			bcopy(path, p, plen);
+					err(1, NULL);
+			memmove(p, path, plen);
 			p += plen;
 			++orig;
 		} else
@@ -78,6 +82,7 @@ brace_subst(orig, store, path, len)
  *	print a message to standard error and then read input from standard
  *	input. If the input is 'y' then 1 is returned.
  */
+int
 queryuser(argv)
 	register char **argv;
 {
@@ -104,7 +109,7 @@ queryuser(argv)
 		(void)fprintf(stderr, "\n");
 		(void)fflush(stderr);
 	}
-        return(first == 'y');
+        return (first == 'y');
 }
  
 /*
@@ -118,36 +123,6 @@ emalloc(len)
 	void *p;
 
 	if (p = malloc(len))
-		return(p);
-	err("%s", strerror(errno));
-	/* NOTREACHED */
-}
-
-#if __STDC__
-#include <stdarg.h>
-#else
-#include <varargs.h>
-#endif
-
-void
-#if __STDC__
-err(const char *fmt, ...)
-#else
-err(fmt, va_alist)
-	char *fmt;
-        va_dcl
-#endif
-{
-	va_list ap;
-#if __STDC__
-	va_start(ap, fmt);
-#else
-	va_start(ap);
-#endif
-	(void)fprintf(stderr, "find: ");
-	(void)vfprintf(stderr, fmt, ap);
-	va_end(ap);
-	(void)fprintf(stderr, "\n");
-	exit(1);
-	/* NOTREACHED */
+		return (p);
+	err(1, NULL);
 }
