58f143a59940906d3c2d9e349cbf0923c8b0abfd
jtc <jtc@NetBSD.org>
1993-12-31 19:13:47 +0000

    Add explicit return types to function definitions.

--- lib/libc/stdio/fgetpos.c
+++ lib/libc/stdio/fgetpos.c
@@ -36,11 +36,12 @@
 
 #if defined(LIBC_SCCS) && !defined(lint)
 /*static char *sccsid = "from: @(#)fgetpos.c	5.1 (Berkeley) 1/20/91";*/
-static char *rcsid = "$Id: fgetpos.c,v 1.4 1993/08/26 00:46:42 jtc Exp $";
+static char *rcsid = "$Id: fgetpos.c,v 1.5 1993/12/31 19:14:08 jtc Exp $";
 #endif /* LIBC_SCCS and not lint */
 
 #include <stdio.h>
 
+int
 fgetpos(fp, pos)
 	FILE *fp;
 	fpos_t *pos;
