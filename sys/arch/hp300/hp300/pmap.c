0bcf3ba6bd855907c41dc781574a774135fe5a6a
mycroft <mycroft@NetBSD.org>
1994-01-04 14:11:05 +0000

    Change ptpage to flag, for new VM code.

--- sys/arch/hp300/hp300/pmap.c
+++ sys/arch/hp300/hp300/pmap.c
@@ -35,7 +35,7 @@
  * SUCH DAMAGE.
  *
  *	from: @(#)pmap.c	7.5 (Berkeley) 5/10/91
- *	$Id: pmap.c,v 1.3 1993/08/14 14:29:10 mycroft Exp $
+ *	$Id: pmap.c,v 1.4 1994/01/04 14:11:05 mycroft Exp $
  */
 
 /*
@@ -2050,7 +2050,7 @@ pmap_enter_ptpage(pmap, va)
 			panic("pmap_enter: vm_fault failed");
 		ptpa = pmap_extract(kernel_pmap, va);
 #ifdef DEBUG
-		PHYS_TO_VM_PAGE(ptpa)->ptpage = TRUE;
+		PHYS_TO_VM_PAGE(ptpa)->flags |= PG_PTPAGE;
 #endif
 	}
 
