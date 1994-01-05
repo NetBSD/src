b6ea5411055a0cd66b4e6d1693be315635c6b554
cgd <cgd@NetBSD.org>
1994-01-05 09:00:57 +0000

    update with latest fdesc file system from jsp@sequent.com

--- sys/miscfs/fdesc/fdesc.h
+++ sys/miscfs/fdesc/fdesc.h
@@ -1,8 +1,9 @@
 /*
+ * Copyright (c) 1992 The Regents of the University of California
  * Copyright (c) 1990, 1992 Jan-Simon Pendry
  * All rights reserved.
  *
- * This code is derived from software contributed to Berkeley by
+ * This code is derived from software donated to Berkeley by
  * Jan-Simon Pendry.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -15,8 +16,8 @@
  *    documentation and/or other materials provided with the distribution.
  * 3. All advertising materials mentioning features or use of this software
  *    must display the following acknowledgement:
- *      This product includes software developed by the University of
- *      California, Berkeley and its contributors.
+ *	This product includes software developed by the University of
+ *	California, Berkeley and its contributors.
  * 4. Neither the name of the University nor the names of its contributors
  *    may be used to endorse or promote products derived from this software
  *    without specific prior written permission.
@@ -33,7 +34,10 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	$Id: fdesc.h,v 1.2 1993/03/25 06:00:17 cgd Exp $
+ * From:
+ *	Id: fdesc.h,v 4.1 1993/12/17 10:47:45 jsp Rel
+ *
+ *	$Id: fdesc.h,v 1.3 1994/01/05 09:00:57 cgd Exp $
  */
 
 #ifdef KERNEL
@@ -41,14 +45,35 @@ struct fdescmount {
 	struct vnode	*f_root;	/* Root node */
 };
 
+#define FD_ROOT		2
+#define FD_DEVFD	3
+#define FD_STDIN	4
+#define FD_STDOUT	5
+#define FD_STDERR	6
+#define FD_CTTY		7
+#define FD_DESC		8
+#define FD_MAX		12
+
+typedef enum {
+	Froot,
+	Fdevfd,
+	Fdesc,
+	Flink,
+	Fctty
+} fdntype;
+
 struct fdescnode {
-	unsigned	f_fd;			/* Fd to be dup'ed */
-	/*int		f_isroot;*/		/* Is this the root */
+	fdntype		fd_type;	/* Type of this node */
+	unsigned	fd_fd;		/* Fd to be dup'ed */
+	char		*fd_link;	/* Link to fd/n */
+	int		fd_ix;		/* filesystem index */
 };
 
 #define VFSTOFDESC(mp)	((struct fdescmount *)((mp)->mnt_data))
 #define	VTOFDESC(vp) ((struct fdescnode *)(vp)->v_data)
 
+extern dev_t devctty;
+extern int fdesc_allocvp __P((fdntype, int, struct mount *, struct vnode **));
 extern struct vnodeops fdesc_vnodeops;
 extern struct vfsops fdesc_vfsops;
 #endif /* KERNEL */
