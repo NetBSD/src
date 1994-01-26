4bf656cb5356253ea95804a36e18de5c49a84af2
mw <mw@NetBSD.org>
1994-01-26 21:35:37 +0000

    Add support for 68040 frames and flag bits.

--- sys/arch/m68k/include/frame.h
+++ sys/arch/m68k/include/frame.h
@@ -37,7 +37,7 @@
  *
  *	from: Utah Hdr: frame.h 1.1 90/07/09
  *	from: @(#)frame.h	7.2 (Berkeley) 11/2/90
- *	$Id: frame.h,v 1.3 1993/08/01 19:25:03 mycroft Exp $
+ *	$Id: frame.h,v 1.4 1994/01/26 21:35:37 mw Exp $
  */
 
 struct frame {
@@ -51,6 +51,28 @@ struct frame {
 		struct fmt2 {
 			u_int	f_iaddr;
 		} F_fmt2;
+
+		struct fmt3 {
+			u_int	f_iaddr;
+		} F_fmt3;
+
+		struct fmt7 {
+			u_int	f_ea;
+			u_short	f_ssw;
+			u_short	f_wb3s;
+			u_short	f_wb2s;
+			u_short	f_wb1s;
+			u_int	f_fa;
+			u_int	f_wb3a;
+			u_int	f_wb3d;
+			u_int	f_wb2a;
+			u_int	f_wb2d;
+			u_int	f_wb1a;
+			u_int	f_wb1d;
+			u_int	f_pd1;
+			u_int	f_pd2;
+			u_int	f_pd3;
+		} F_fmt7;
 
 		struct fmt9 {
 			u_int	f_iaddr;
@@ -87,6 +109,8 @@ struct frame {
 };
 
 #define	f_fmt2		F_u.F_fmt2
+#define f_fmt3		F_u.F_fmt3
+#define	f_fmt7		F_u.F_fmt7
 #define	f_fmtA		F_u.F_fmtA
 #define	f_fmtB		F_u.F_fmtB
 
@@ -97,6 +121,8 @@ struct frame {
 #define	FMT0		0x0
 #define	FMT1		0x1
 #define	FMT2		0x2
+#define	FMT3		0x3
+#define	FMT7		0x7
 #define	FMT9		0x9
 #define	FMTA		0xA
 #define	FMTB		0xB
@@ -105,6 +131,8 @@ struct frame {
 #define	FMT0SIZE	0
 #define	FMT1SIZE	0
 #define	FMT2SIZE	sizeof(struct fmt2)
+#define	FMT3SIZE	sizeof(struct fmt3)
+#define	FMT7SIZE	sizeof(struct fmt7)
 #define	FMT9SIZE	sizeof(struct fmt9)
 #define	FMTASIZE	sizeof(struct fmtA)
 #define	FMTBSIZE	sizeof(struct fmtB)
@@ -113,6 +141,57 @@ struct frame {
 #define	V_ADDRERR	0x00C
 #define	V_TRAP1		0x084
 
+/* 68040 fault frame */
+#define SSW_CP		0x8000		/* Continuation - Floating-Point Post */
+#define SSW_CU		0x4000		/* Continuation - Unimpl. FP */
+#define SSW_CT		0x2000		/* Continuation - Trace */
+#define SSW_CM		0x1000		/* Continuation - MOVEM */
+#define SSW_MA		0x0800		/* Misaligned access */
+#define SSW_ATC		0x0400		/* ATC fault */
+#define SSW_LK		0x0200		/* Locked transfer */
+#define SSW_RW040	0x0100		/* Read/Write */
+#define SSW_SZMASK	0x0060		/* Transfer size */
+#define SSW_TTMASK	0x0018		/* Transfer type */
+#define SSW_TMMASK	0x0007		/* Transfer modifier */
+
+#define WBS_TMMASK	0x0007
+#define WBS_TTMASK	0x0018
+#define WBS_SZMASK	0x0060
+#define WBS_VALID	0x0080
+
+#define WBS_SIZE_BYTE	0x0020
+#define WBS_SIZE_WORD	0x0040
+#define WBS_SIZE_LONG	0x0000
+#define WBS_SIZE_LINE	0x0060
+
+#define WBS_TT_NORMAL	0x0000
+#define WBS_TT_MOVE16	0x0008
+#define WBS_TT_ALTFC	0x0010
+#define WBS_TT_ACK	0x0018
+
+#define WBS_TM_PUSH	0x0000
+#define WBS_TM_UDATA	0x0001
+#define WBS_TM_UCODE	0x0002
+#define WBS_TM_MMUTD	0x0003
+#define WBS_TM_MMUTC	0x0004
+#define WBS_TM_SDATA	0x0005
+#define WBS_TM_SCODE	0x0006
+#define WBS_TM_RESV	0x0007
+
+#define	MMUSR_PA_MASK	0xfffff000
+#define MMUSR_B		0x00000800
+#define MMUSR_G		0x00000400
+#define MMUSR_U1	0x00000200
+#define MMUSR_U0	0x00000100
+#define MMUSR_S		0x00000080
+#define MMUSR_CM	0x00000060
+#define MMUSR_M		0x00000010
+#define MMUSR_0		0x00000008
+#define MMUSR_W		0x00000004
+#define MMUSR_T		0x00000002
+#define MMUSR_R		0x00000001
+
+/* 68020/68030 fault frame */
 #define	SSW_RC		0x2000
 #define	SSW_RB		0x1000
 #define	SSW_DF		0x0100
@@ -142,6 +221,7 @@ struct fpframe {
 		struct fpbusy {
 			u_int	fpf_iregs[53];
 		} FPF_busy;
+
 	} FPF_u2;
 	u_int	fpf_regs[8*3];
 	u_int	fpf_fpcr;
