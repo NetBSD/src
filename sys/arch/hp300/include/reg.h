9da7c995ca5e8b78b30313b507cf55bf6eba12c2
cgd <cgd>
1994-01-27 02:12:44 +0000

    HP300 -> MACHINE

--- sys/arch/hp300/include/reg.h
+++ sys/arch/hp300/include/reg.h
@@ -1,11 +1,10 @@
 /*
- *	$Id: reg.h,v 1.7 1994/01/08 12:41:53 cgd Exp $
+ *	$Id: reg.h,v 1.8 1994/01/27 02:12:44 cgd Exp $
  */
 
-#ifndef _HP300_REG_H_
-#define	_HP300_REG_H_
+#ifndef _MACHINE_REG_H_
+#define	_MACHINE_REG_H_
 
-/* Just use the common m68k definition */
 #include <m68k/reg.h>
 
-#endif /* _HP300_REG_H_ */
+#endif /* _MACHINE_REG_H_ */
