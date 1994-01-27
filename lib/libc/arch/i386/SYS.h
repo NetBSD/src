ef980f76fe45bf0f95ae357583a2c9a3884a2133
mycroft <mycroft@NetBSD.org>
1994-01-27 06:53:25 +0000

    One last tweak.

--- lib/libc/arch/i386/SYS.h
+++ lib/libc/arch/i386/SYS.h
@@ -34,17 +34,17 @@
  * SUCH DAMAGE.
  *
  *	from: @(#)SYS.h	5.5 (Berkeley) 5/7/91
- *	$Id: SYS.h,v 1.4 1994/01/27 04:16:50 mycroft Exp $
+ *	$Id: SYS.h,v 1.5 1994/01/27 06:53:25 mycroft Exp $
  */
 
 #include <machine/asm.h>
 #include <sys/syscall.h>
 
-#define	SYSCALL(x)	2: jmp PIC_PLT(cerror); ENTRY(x); movl $(SYS_/**/x),%eax; LCALL(7,0); jb 2b
+#define	SYSCALL(x)	2: jmp PIC_PLT(cerror); ENTRY(x); movl $(SYS_/**/x),%eax; LCALL(7,0); jc 2b
 #define	RSYSCALL(x)	SYSCALL(x); ret
 #define	PSEUDO(x,y)	ENTRY(x); movl $(SYS_/**/y),%eax; LCALL(7,0); ret
 #define	CALL(x,y)	call PIC_PLT(_/**/y); addl $4*x,%esp
 /* gas fucks up offset -- although we don't currently need it, do for BCS */
-#define	LCALL(x,y)	.byte 0x9a ; .long y; .word x
+#define	LCALL(x,y)	.byte 0x9a; .long y; .word x
 
 	.globl	cerror
