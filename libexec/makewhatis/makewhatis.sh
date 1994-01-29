eee1c6ddd19c6f1efa74ba7769779fbf3e068143
jtc <jtc>
1994-01-29 01:43:03 +0000

    Fix spelling error in Copyright notice

--- libexec/makewhatis/makewhatis.sh
+++ libexec/makewhatis/makewhatis.sh
@@ -14,7 +14,7 @@
 #    must display the following acknowledgement:
 #      This product includes software developed by Winning Strategies, Inc.
 # 4. The name of the author may not be used to endorse or promote products
-#    derived from this software withough specific prior written permission
+#    derived from this software without specific prior written permission
 #
 # THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
 # IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
@@ -27,7 +27,7 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
 # THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
-# $Header: /cvsroot/src/libexec/makewhatis/Attic/makewhatis.sh,v 1.1 1993/12/21 03:28:04 cgd Exp $
+# $Header: /cvsroot/src/libexec/makewhatis/Attic/makewhatis.sh,v 1.2 1994/01/29 01:49:52 jtc Exp $
 #
 
 trap "rm -f /tmp/whatis$$; exit 1" 1 2 15
