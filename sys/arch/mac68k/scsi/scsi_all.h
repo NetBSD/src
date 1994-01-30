404b8fce6baa529cd91716702c3c2ee61e4ea206
briggs <briggs@NetBSD.org>
1994-01-30 01:21:12 +0000

    Update to magnum scsi system.  Also added a function to sd.c to map a scsi target to a disk unit for setroot().  If anyone knows a better way to do this, please let me know.  The only other way I could think of would be to add a field in disk.h:struct dkdevice to reference the scsi target.  Ideally, the correct boot device would be passed in from the booter.  As we only have one scsi bus (until we support NuBUS scsi cards), it makes sense to be backward-compatible (especially since mac weenies seem to need help tying their shoes).

--- sys/arch/mac68k/scsi/scsi_all.h
+++ sys/arch/mac68k/scsi/scsi_all.h
@@ -18,7 +18,7 @@
  *
  * Ported to run under 386BSD by Julian Elischer (julian@tfs.com) Sept 1992
  *
- *	$Id: scsi_all.h,v 1.2 1993/11/29 00:45:10 briggs Exp $
+ *	$Id: scsi_all.h,v 1.3 1994/01/30 01:21:20 briggs Exp $
  */
 
 #ifndef	_SCSI_SCSI_ALL_H
