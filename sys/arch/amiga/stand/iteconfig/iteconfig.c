142f0cced9d2d01bd3a7eacb623e423211d9a859
chopps <chopps@NetBSD.org>
1994-02-01 12:13:17 +0000

    performed facelift.

--- sys/arch/amiga/stand/iteconfig/iteconfig.c
+++ sys/arch/amiga/stand/iteconfig/iteconfig.c
@@ -1,21 +1,33 @@
 /* iteconfig.c 
- * Copyright (C) 1993 Christian E. Hopps 
+ * Copyright (c) 1994 Christian E. Hopps
+ * All rights reserved.
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *      This product includes software developed by Christian E. Hopps
+ * 4. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission
  *
- * Author: Christian E. Hopps  email: chopps@emunix.emich.edu | chopps@ro-chp.UUCP
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *	$Id: iteconfig.c,v 1.2 1994/02/01 12:13:17 chopps Exp $
  */
 
 
@@ -28,7 +40,6 @@
 #include <stdlib.h>
 #include <limits.h>
 #include <errno.h>
-#include "getopt.h"
 #include "grf/grf_types.h"
 #include "grf/grf_colormap.h"
 #include "viewioctl.h"
@@ -38,31 +49,11 @@
 int version = 1;
 int revision = 0;
 
-
-struct option lopts[] = {
-    { "left", required_argument, NULL, 'X' },
-    { "top", required_argument, NULL, 'Y' },
-    { "width", required_argument, NULL, 'W' },
-    { "height", required_argument, NULL, 'H' },
-    { "depth", required_argument, NULL, 'D' },
-    { "volume", required_argument, NULL, 'V' },
-    { "time", required_argument, NULL, 'T' },
-    { "period", required_argument, NULL, 'P' },
-    { "info", no_argument, NULL, 'i' },
-    { "help", no_argument, NULL, 'h' },
-    { "version", no_argument, NULL, 'v' },
-    { NULL, }
-};
-
 char *program_name;
 char *optstr = "W:H:D:V:T:P:X:Y:hiv";
 char *help_string = "options: \n"
     "   [-vih] [-D DEPTH] [-H HEIGHT] [-P PERIOD] [-T COUNT]\n"
     "   [-V VOLUME] [-W WIDTH] [-X OFFSET] [-Y OFFSET]\n"
-    "   [--depth=NUM_PLANES] [--height=PIXELS] [--help]\n"
-    "   [--info] [--left=OFFSET] [--period=PERIOD]\n"
-    "   [--time=COUNT] [--top=OFFSET] [--version]\n"
-    "   [--volume=VOL] [--width=PIXELS]\n\n"
     "   ColorX is one of 0xRRGGBB, 0xGG or 0xM";
 
 void xioctl (int fdesc, int cmd, void *addr);
@@ -78,7 +69,7 @@ main (int argc, char **argv)
     struct ite_window_size is, newis;
     struct ite_bell_values ib, newib;
     struct winsize ws;
-    int longind = 0, opt, cfd;
+    int opt, cfd;
     int opt_info = 0, i, max_colors;
     long val;
     
@@ -96,7 +87,7 @@ main (int argc, char **argv)
     bcopy (&is, &newis, sizeof (is));
     bcopy (&ib, &newib, sizeof (ib));
 
-    while (EOF != (opt = getopt_long (argc, argv, optstr, lopts, &longind))) {
+    while (EOF != (opt = getopt (argc, argv, optstr))) {
 	switch (opt) {
 	  case 'i':
 	    opt_info = 1;
