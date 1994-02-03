6678df0d7a87458fb8c3f8353da4a96c7e6f1db1
mycroft <mycroft@NetBSD.org>
1994-02-03 23:22:55 +0000

    Update...

--- sys/arch/i386/boot/version.c
+++ sys/arch/i386/boot/version.c
@@ -1,9 +1,13 @@
 /*
- *	$Id: version.c,v 1.16 1994/02/03 07:42:35 cgd Exp $
+ *	$Id: version.c,v 1.17 1994/02/03 23:22:55 mycroft Exp $
  */
 
 /*
  *	NOTE ANY CHANGES YOU MAKE TO THE BOOTBLOCKS HERE.
+ *
+ *	1.16 -> 1.17
+ *		removed with prejudice the extra buffer for xread(), changes
+ *		to make the code smaller, and general cleanup. (mycroft)
  *
  *	1.15 -> 1.16
  *		reduce BUFSIZE to 4k, because that's fixed the
@@ -16,4 +20,4 @@
  *		look in boot.c revision logs
  */
 
-char *version = "$Revision: 1.16 $";
+char *version = "$Revision: 1.17 $";
