3f3bb74a85b9ff27244bba2b15978b44515ba12d
cgd <cgd@NetBSD.org>
1994-02-03 23:44:40 +0000

    RCS id's

--- usr.bin/sed/defs.h
+++ usr.bin/sed/defs.h
@@ -34,7 +34,8 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)defs.h	8.1 (Berkeley) 6/6/93
+ *	from: @(#)defs.h	8.1 (Berkeley) 6/6/93
+ *	$Id: defs.h,v 1.5 1994/02/03 23:44:51 cgd Exp $
  */
 
 /*
