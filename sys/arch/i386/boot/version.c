df93c292ca3676470dc0ea30cfab7b0bab266792
mycroft <mycroft@NetBSD.org>
1994-02-04 16:08:53 +0000

    Don't need to frob IDT if !BDB.

---
+++ sys/arch/i386/boot/version.c
@@ -0,0 +1,26 @@
+/*
+ *	$Id: version.c,v 1.18.2.2 1994/02/04 16:08:56 mycroft Exp $
+ */
+
+/*
+ *	NOTE ANY CHANGES YOU MAKE TO THE BOOTBLOCKS HERE.
+ *
+ *	1.17 -> 1.18
+ *		removed some more code we don't need for BDB. (mycroft)
+ *
+ *	1.16 -> 1.17
+ *		removed with prejudice the extra buffer for xread(), changes
+ *		to make the code smaller, and general cleanup. (mycroft)
+ *
+ *	1.15 -> 1.16
+ *		reduce BUFSIZE to 4k, because that's fixed the
+ *		boot problems, for some. (cgd)
+ *
+ *	1.14 -> 1.15
+ *		seperated 'version' out from boot.c (cgd)
+ *
+ *	1.1 -> 1.14
+ *		look in boot.c revision logs
+ */
+
+char *version = "$Revision: 1.18.2.2 $";
