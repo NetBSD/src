1c3caff3ffe4d0356760d3a3d5e6abd1881cc775
proven <proven@NetBSD.org>
1994-02-07 22:04:13 +0000

    RCS ids added. pthread_once() moved to separate file. pthread_detach(), and pthread_join() routines added, and sleep now sleeps only the running thread not the process.

--- lib/libpthread/pthreads/fd_pipe.c
+++ lib/libpthread/pthreads/fd_pipe.c
@@ -1,5 +1,5 @@
 /* ==== fd_pipe.c ============================================================
- * Copyright (c) 1993 by Chris Provenzano, proven@mit.edu
+ * Copyright (c) 1993, 1994 by Chris Provenzano, proven@mit.edu
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -37,6 +37,10 @@
  *	1.01 93/11/13 proven
  *		-The functions readv() and writev() added.
  */
+
+#ifndef lint
+static const char rcsid[] = "$Id: fd_pipe.c,v 1.3 1994/02/07 22:04:17 proven Exp $ $provenid: fd_pipe.c,v 1.16 1994/02/07 02:18:52 proven Exp $";
+#endif
 
 #include <pthread.h>
 #include <pthread/fd_pipe.h>
--- lib/libpthread/pthreads/mutex.c
+++ lib/libpthread/pthreads/mutex.c
@@ -1,5 +1,5 @@
 /* ==== mutex.c ============================================================
- * Copyright (c) 1993 by Chris Provenzano, proven@mit.edu
+ * Copyright (c) 1993, 1994 by Chris Provenzano, proven@mit.edu
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -35,7 +35,11 @@
  *      -Started coding this file.
  */
 
-#include "pthread.h"
+#ifndef lint
+static const char rcsid[] = "$Id: mutex.c,v 1.3 1994/02/07 22:04:21 proven Exp $ $provenid: mutex.c,v 1.16 1994/02/07 02:19:03 proven Exp $";
+#endif
+
+#include <pthread.h>
 #include <errno.h>
 
 /*
