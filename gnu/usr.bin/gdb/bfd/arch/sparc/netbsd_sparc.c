fae2950e23941b826508fa291196f4dc7c3b26f6
pk <pk@NetBSD.org>
1994-02-10 20:31:11 +0000

    Sparc executables have 8k page granularity.

--- gnu/usr.bin/gdb/bfd/arch/sparc/netbsd_sparc.c
+++ gnu/usr.bin/gdb/bfd/arch/sparc/netbsd_sparc.c
@@ -1,13 +1,13 @@
-/*	$Id: netbsd_sparc.c,v 1.1 1994/01/28 12:38:30 pk Exp $ */
+/*	$Id: netbsd_sparc.c,v 1.2 1994/02/10 20:31:11 pk Exp $ */
 
 #define TARGET_IS_BIG_ENDIAN_P
 #define HOST_BIG_ENDIAN_P
 #define	BYTES_IN_WORD	4
 #define	ARCH	32
 
-#define	PAGE_SIZE	4096
-#define	SEGMENT_SIZE	PAGE_SIZE
 #define __LDPGSZ	8192
+#define	PAGE_SIZE	__LDPGSZ
+#define	SEGMENT_SIZE	__LDPGSZ
 #define MID_SPARC	138
 #define	DEFAULT_ARCH	bfd_arch_sparc
 
