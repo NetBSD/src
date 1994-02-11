13618394b2eda518465dcc44aa6ca304028742b9
jtc <jtc@NetBSD.org>
1994-02-11 17:52:17 +0000

    Fdlibm 5.1

---
+++ lib/libm/src/s_finite.c
@@ -0,0 +1,38 @@
+
+/* @(#)s_finite.c 5.1 93/09/24 */
+/*
+ * ====================================================
+ * Copyright (C) 1993 by Sun Microsystems, Inc. All rights reserved.
+ *
+ * Developed at SunPro, a Sun Microsystems, Inc. business.
+ * Permission to use, copy, modify, and distribute this
+ * software is freely granted, provided that this notice 
+ * is preserved.
+ * ====================================================
+ */
+
+/*
+ * finite(x) returns 1 is x is finite, else 0;
+ * no branching!
+ */
+
+#include "fdlibm.h"
+
+#ifdef __STDC__
+static const double one = 1.0;
+#else
+static double one = 1.0;
+#endif
+
+#ifdef __STDC__
+	int finite(double x)
+#else
+	int finite(x)
+	double x;
+#endif
+{
+	int n0,hx; 
+	n0 = ((*(int*)&one)>>29)^1;
+	hx = *(n0+(int*)&x);
+	return  (unsigned)((hx&0x7fffffff)-0x7ff00000)>>31;
+}
