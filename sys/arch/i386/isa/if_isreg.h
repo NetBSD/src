8e16d89f076afbb606592a40ed0e04dd01282a63
mycroft <mycroft>
1994-02-15 00:46:16 +0000

    Add NE2100 and multicast support, and clean up some formatting.

--- sys/arch/i386/isa/if_isreg.h
+++ sys/arch/i386/isa/if_isreg.h
@@ -2,7 +2,7 @@
  * Isolan AT 4141-0 Ethernet driver header file
  * Isolink 4110 
  *
- * By Paul Richards 
+ * Copyright (c) 1994 Charles Hannum.
  *
  * Copyright (C) 1993, Paul Richards. This software may be used, modified,
  *   copied, distributed, and sold, in both source and binary form provided
@@ -11,18 +11,8 @@
  *   of this software, nor does the author assume any responsibility
  *   for damages incurred with its use.
  *
- *	$Id: if_isreg.h,v 1.5 1993/10/25 03:30:56 paul Exp $
+ *	$Id: if_isreg.h,v 1.6 1994/02/15 00:46:18 mycroft Exp $
  */
-
-/*
- * Initialize multicast address hashing registers to accept
- * all multicasts (only used when in promiscuous mode)
- */
-#if NBPFILTER > 0
-#define MULTI_INIT_ADDR 0xff
-#else
-#define MULTI_INIT_ADDR 0
-#endif
 
 /* Declarations specific to this driver */
 #define NTBUF 2
@@ -30,11 +20,23 @@
 #define NRBUF 8
 #define RLEN 3
 #define BUFSIZE 1518
-#define RAP 0xe
-#define RDP 0xc
+
+/* Board types */
+#define	BICC		1
+#define	BICC_RDP	0xc
+#define	BICC_RAP	0xe
+#define	NE2100		2
+#define	NE2100_RDP	0x10
+#define	NE2100_RAP	0x12
+
+/* Chip types */
+#define	PROBE_MASK	0x0007
+#define	LANCE		1
+#define	LANCE_MASK	0x0007
+#define	PCnet_ISA	2
+#define	PCnet_ISA_MASK	0x0000
 
 /* Control and status register 0 flags */
-
 #define ERR	0x8000
 #define BABL	0x4000
 #define CERR	0x2000
@@ -53,64 +55,57 @@
 #define INIT	0x0001
 
 /* Coontrol and status register 3 flags */
-
 #define BSWP	0x0004
 #define ACON	0x0002
 #define BCON	0x0001
 
 /* Initialisation block (must be on word boundary) */
-
 struct init_block {
-       u_short mode;		/* Mode register 			*/
-       u_char  padr[6];		/* Ethernet address 			*/
-       u_char ladrf[8];		/* Logical address filter (multicast) 	*/
-       u_short rdra;		/* Low order pointer to receive ring 	*/
-       u_short rlen;		/* High order pointer and no. rings 	*/
-       u_short tdra;		/* Low order pointer to transmit ring 	*/
-       u_short tlen;		/* High order pointer and no rings 	*/
-       };
+	u_short mode;		/* Mode register */
+	u_char padr[6];		/* Ethernet address */
+	u_long ladrf[2];	/* Logical address filter (multicast) */
+	u_short rdra;		/* Low order pointer to receive ring */
+	u_short rlen;		/* High order pointer and no. rings */
+	u_short tdra;		/* Low order pointer to transmit ring */
+	u_short tlen;		/* High order pointer and no rings */
+};
 
 /* Mode settings */
-
-#define PROM	0x8000		/* Promiscuous		*/
-#define INTL	0x0040		/* Internal loopback	*/
-#define DRTY	0x0020		/* Disable retry 	*/
-#define COLL	0x0010		/* Force collision 	*/
-#define DTCR	0x0008		/* Disable transmit crc	*/
-#define LOOP	0x0004		/* Loop back 		*/
-#define DTX	0x0002		/* Disable transmitter 	*/
-#define DRX	0x0001		/* Disable receiver 	*/
+#define PROM	0x8000		/* Promiscuous */
+#define INTL	0x0040		/* Internal loopback */
+#define DRTY	0x0020		/* Disable retry */
+#define COLL	0x0010		/* Force collision */
+#define DTCR	0x0008		/* Disable transmit crc */
+#define LOOP	0x0004		/* Loop back */
+#define DTX	0x0002		/* Disable transmitter */
+#define DRX	0x0001		/* Disable receiver */
 
 /* Message descriptor structure */
-
 struct mds {
-       u_short addr;
-       u_short flags;
-       u_short bcnt;
-       u_short mcnt;
-       };
+	u_short addr;
+	u_short flags;
+	u_short bcnt;
+	u_short mcnt;
+};
 
 /* Receive ring status flags */
-
-#define OWN	0x8000		/* Owner bit, 0=host, 1=Lance 	*/
-#define MDERR	0x4000		/* Error 			*/	
-#define FRAM	0x2000		/* Framing error error 		*/
-#define OFLO	0x1000		/* Silo overflow 		*/
-#define CRC	0x0800		/* CRC error 			*/
-#define RBUFF	0x0400		/* Buffer error 		*/
-#define STP	0x0200		/* Start of packet 		*/
-#define ENP	0x0100		/* End of packet 		*/
+#define OWN	0x8000		/* Owner bit, 0=host, 1=Lance */
+#define MDERR	0x4000		/* Error */
+#define FRAM	0x2000		/* Framing error error */
+#define OFLO	0x1000		/* Silo overflow */
+#define CRC	0x0800		/* CRC error */
+#define RBUFF	0x0400		/* Buffer error */
+#define STP	0x0200		/* Start of packet */
+#define ENP	0x0100		/* End of packet */
 
 /* Transmit ring flags */
-
-#define MORE	0x1000		/* More than 1 retry 	*/
-#define ONE	0x0800		/* One retry 		*/
-#define DEF	0x0400		/* Deferred transmit 	*/
+#define MORE	0x1000		/* More than 1 retry */
+#define ONE	0x0800		/* One retry */
+#define DEF	0x0400		/* Deferred transmit */
 
 /* Transmit errors */
-
-#define TBUFF	0x8000		/* Buffer error 	*/
-#define UFLO	0x4000		/* Silo underflow	*/
-#define LCOL	0x1000		/* Late collision 	*/
-#define LCAR	0x0800		/* Loss of carrier 	*/
-#define RTRY	0x0400		/* Tried 16 times 	*/
+#define TBUFF	0x8000		/* Buffer error */
+#define UFLO	0x4000		/* Silo underflow */
+#define LCOL	0x1000		/* Late collision */
+#define LCAR	0x0800		/* Loss of carrier */
+#define RTRY	0x0400		/* Tried 16 times */
