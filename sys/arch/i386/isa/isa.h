1a8d283a97b4294a9c66f084f2ed3f1eb5907a09
mycroft <mycroft@NetBSD.org>
1994-02-16 17:59:11 +0000

    Move elink_reset() into a separate file, and add a generic elink_idseq().

--- sys/arch/i386/isa/isa.h
+++ sys/arch/i386/isa/isa.h
@@ -34,7 +34,7 @@
  * SUCH DAMAGE.
  *
  *	from: @(#)isa.h	5.7 (Berkeley) 5/9/91
- *	$Id: isa.h,v 1.14 1994/02/16 07:26:55 hpeyerl Exp $
+ *	$Id: isa.h,v 1.15 1994/02/16 17:59:24 mycroft Exp $
  */
 
 /*
@@ -208,9 +208,3 @@ int isa_irq_pending __P((struct isa_device *dvp));
 #ifndef LOCORE
 extern vm_offset_t isaphysmem;
 #endif
-
-/*
- * defines for elink_reset()  (See isa.c)
- */
-#define ELINK_ID_PORT	0x100
-#define ELINK_RESET	0xc0
