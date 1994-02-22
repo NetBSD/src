e332e61cb78dc5c1d466f537e35b2238b40606a1
paulus <paulus@NetBSD.org>
1994-02-22 00:11:57 +0000

    Some fixes so that the local IP address can be assigned by the peer if no local IP address is specified on the command line (even though a default local IP address has been determined from the hostname).

--- usr.sbin/pppd/pppd/patchlevel.h
+++ usr.sbin/pppd/pppd/patchlevel.h
@@ -1,5 +1,5 @@
-/* $Id: patchlevel.h,v 1.5 1994/01/25 05:58:12 paulus Exp $ */
-#define	PATCHLEVEL	3
+/* $Id: patchlevel.h,v 1.6 1994/02/22 00:12:03 paulus Exp $ */
+#define	PATCHLEVEL	4
 
 #define VERSION	"2.0"
-#define DATE	"10 Jan 94"
+#define DATE	"9 Feb 94"
