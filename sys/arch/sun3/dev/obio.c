4531f9fd226ecd93cf9cbb90b38e3d9619ff3d43
glass <glass@NetBSD.org>
1994-03-01 08:07:12 +0000

    canonicalize includes.  cleanup.  missing copyrights.

--- sys/arch/sun3/dev/obio.c
+++ sys/arch/sun3/dev/obio.c
@@ -1,12 +1,44 @@
-#include "sys/systm.h"
-#include "sys/device.h"
+/*
+ * Copyright (c) 1993 Adam Glass
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed by Adam Glass.
+ * 4. The name of the Author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY Adam Glass ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL Adam Glass BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * $Header: /cvsroot/src/sys/arch/sun3/dev/Attic/obio.c,v 1.6 1994/03/01 08:07:15 glass Exp $
+ */
+#include <sys/systm.h>
+#include <sys/device.h>
 
-#include "machine/autoconf.h"
-#include "machine/obio.h"
-#include "machine/pte.h"
-#include "machine/param.h"
-#include "machine/mon.h"
-#include "machine/isr.h"
+#include <machine/autoconf.h>
+#include <machine/obio.h>
+#include <machine/pte.h>
+#include <machine/param.h>
+#include <machine/mon.h>
+#include <machine/isr.h>
 
 #define ALL 0xFF
     
