072ee998e1c9268d12c0a2431d15a4cae2145be1
cgd <cgd@NetBSD.org>
1994-03-07 05:05:16 +0000

    update to newest rdist from berkeley (via uunet).  No security fixes, but general cleanup.

--- usr.bin/rdist/pathnames.h
+++ usr.bin/rdist/pathnames.h
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1989 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1989, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -30,8 +30,8 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	from: @(#)pathnames.h	5.4 (Berkeley) 8/27/90
- *	$Id: pathnames.h,v 1.2 1993/08/01 18:09:48 mycroft Exp $
+ *	from: @(#)pathnames.h	8.1 (Berkeley) 6/9/93
+ *	$Id: pathnames.h,v 1.3 1994/03/07 05:05:39 cgd Exp $
  */
 
 #include <paths.h>
