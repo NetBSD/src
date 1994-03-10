1f4627be9a781d2cb5214b0744d9a060135c0c3b
phil <phil@NetBSD.org>
1994-03-10 21:35:48 +0000

    Allowing pseudo_dma and cosmetic changes.

--- sys/arch/pc532/dev/ncr.c
+++ sys/arch/pc532/dev/ncr.c
@@ -30,13 +30,13 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
- * $Id: ncr.c,v 1.3 1994/03/08 19:48:17 phil Exp $
+ * $Id: ncr.c,v 1.4 1994/03/10 21:35:48 phil Exp $
  *
  */
 
 /* Modified for use with the pc532 by Phil Nelson, Feb 94. */
 
-/* #define PSEUDO_DMA 1 */
+#define PSEUDO_DMA 1
 
 static int ncr_debug=1;
 
@@ -458,7 +458,6 @@ ncr5380_send_cmd(struct scsi_xfer *xs)
 		switch (sense) {
 			case 0x02:	/* Check condition */
 /*				printf("check cond. target %d.\n", xs->targ); */
-				spinwait(4);  /* ??? just trying it */
 				s = splbio();
 				scsi_group0(xs->adapter,
 					    xs->targ,
@@ -469,6 +468,7 @@ ncr5380_send_cmd(struct scsi_xfer *xs)
 					    sizeof(struct scsi_sense_data));
 				splx(s);
 				xs->error = XS_SENSE;
+		spinwait(2);
 				return HAD_ERROR;
 			case 0x08:	/* Busy */
 				xs->error = XS_BUSY;
