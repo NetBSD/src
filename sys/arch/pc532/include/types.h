00302ed137ef6f0e678e2caedc07908b78b2d02e
cgd <cgd@NetBSD.org>
1994-03-14 02:11:32 +0000

    add basic integral types (a la sparc port) that new nvi wants.

--- sys/arch/pc532/include/types.h
+++ sys/arch/pc532/include/types.h
@@ -49,4 +49,17 @@ typedef struct label_t {
 typedef	u_long	vm_offset_t;
 typedef	u_long	vm_size_t;
 
+/*
+ * Basic integral types.  Omit the typedef if
+ * not possible for a machine/compiler combination.
+ */
+typedef	signed char		   int8_t;
+typedef	unsigned char		 u_int8_t;
+typedef	short			  int16_t;
+typedef	unsigned short		u_int16_t;
+typedef	int			  int32_t;
+typedef	unsigned int		u_int32_t;
+typedef	long long		  int64_t;
+typedef	unsigned long long	u_int64_t;
+
 #endif	/* _MACHTYPES_H_ */
