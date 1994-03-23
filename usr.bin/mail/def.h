d2131c0c85f157f10762df251a47f77a4443cd33
cgd <cgd@NetBSD.org>
1994-03-23 01:32:50 +0000

    kill perverse syntax

--- usr.bin/mail/def.h
+++ usr.bin/mail/def.h
@@ -31,7 +31,7 @@
  * SUCH DAMAGE.
  *
  *	from: @(#)def.h	5.22 (Berkeley) 6/25/90
- *	$Id: def.h,v 1.3 1994/03/06 03:03:21 cgd Exp $
+ *	$Id: def.h,v 1.4 1994/03/23 01:32:50 cgd Exp $
  */
 
 #include <sys/param.h>		/* includes <sys/types.h> */
@@ -267,10 +267,10 @@ struct ignoretab {
  * useful just before closing an old file that was opened
  * for read/write.
  */
-#define trunc(stream)	do { \
-		fflush(stream); \
-		ftruncate(fileno(stream), (long) ftell(stream)) ; \
-	} while (0)
+#define trunc(stream) {						\
+	(void)fflush(stream);					\
+	(void)ftruncate(fileno(stream), (long)ftell(stream));	\
+}
 
 /*
  * Forward declarations of routine types to keep lint and cc happy.
