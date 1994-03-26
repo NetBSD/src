5bee302b779d22554a43b299464c73528378889d
glass <glass@NetBSD.org>
1994-03-26 04:08:45 +0000

    moved useful contents of netiso/tp_astring.c down

--- usr.bin/netstat/iso.c
+++ usr.bin/netstat/iso.c
@@ -33,7 +33,7 @@
 
 #ifndef lint
 /*static char sccsid[] = "from: @(#)iso.c	5.6 (Berkeley) 4/27/91";*/
-static char rcsid[] = "$Id: iso.c,v 1.5 1993/11/03 06:17:08 mycroft Exp $";
+static char rcsid[] = "$Id: iso.c,v 1.6 1994/03/26 04:08:45 glass Exp $";
 #endif /* not lint */
 
 /*******************************************************************************
@@ -63,6 +63,7 @@ SOFTWARE.
  * ARGO Project, Computer Sciences Dept., University of Wisconsin - Madison
  */
 
+#include <stdio.h>
 #include <sys/param.h>
 #include <sys/mbuf.h>
 #include <sys/time.h>
@@ -82,7 +83,6 @@ SOFTWARE.
 #undef satosiso
 #include <netiso/tp_param.h>
 #include <netiso/tp_states.h>
-#include <netiso/tp_astring.c>
 #include <netiso/tp_pcb.h>
 #include <netiso/tp_stat.h>
 #include <netiso/iso_pcb.h>
@@ -94,6 +94,45 @@ SOFTWARE.
 #include <netiso/cons_pcb.h>
 #include <netdb.h>
 
+char *tp_sstring[] = {
+    "ST_ERROR(0x0)",
+    "TP_CLOSED(0x1)",
+    "TP_CRSENT(0x2)",
+    "TP_AKWAIT(0x3)",
+    "TP_OPEN(0x4)",
+    "TP_CLOSING(0x5)",
+    "TP_REFWAIT(0x6)",
+    "TP_LISTENING(0x7)",
+    "TP_CONFIRMING(0x8)",
+};
+
+char *tp_estring[] = {
+    "TM_inact(0x0)",
+    "TM_retrans(0x1)",
+    "TM_sendack(0x2)",
+    "TM_notused(0x3)",
+    "TM_reference(0x4)",
+    "TM_data_retrans(0x5)",
+    "ER_TPDU(0x6)",
+    "CR_TPDU(0x7)",
+    "DR_TPDU(0x8)",
+    "DC_TPDU(0x9)",
+    "CC_TPDU(0xa)",
+    "AK_TPDU(0xb)",
+    "DT_TPDU(0xc)",
+    "XPD_TPDU(0xd)",
+    "XAK_TPDU(0xe)",
+    "T_CONN_req(0xf)",
+    "T_DISC_req(0x10)",
+    "T_LISTEN_req(0x11)",
+    "T_DATA_req(0x12)",
+    "T_XPD_req(0x13)",
+    "T_USR_rcvd(0x14)",
+    "T_USR_Xrcvd(0x15)",
+    "T_DETACH(0x16)",
+    "T_NETRESET(0x17)",
+    "T_ACPT_req(0x18)",
+};
 
 /*
  *	Dump esis stats
