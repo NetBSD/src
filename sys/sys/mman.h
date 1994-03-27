08c4623c8acfcf09343824610afaed0e14b95006
cgd <cgd@NetBSD.org>
1994-03-27 09:09:44 +0000

    expand uid_t/gid_t/off_t

--- sys/sys/mman.h
+++ sys/sys/mman.h
@@ -31,41 +31,41 @@
  * SUCH DAMAGE.
  *
  *	from: @(#)mman.h	7.5 (Berkeley) 6/27/91
- *	$Id: mman.h,v 1.3 1993/05/20 16:22:41 cgd Exp $
+ *	$Id: mman.h,v 1.4 1994/03/27 09:09:44 cgd Exp $
  */
 
 #ifndef _SYS_MMAN_H_
-#define _SYS_MMAN_H_
+#define	_SYS_MMAN_H_
 
 /*
  * Protections are chosen from these bits, or-ed together
  */
-#define	PROT_READ	0x04	/* pages can be read */
+#define	PROT_READ	0x01	/* pages can be read */
 #define	PROT_WRITE	0x02	/* pages can be written */
-#define	PROT_EXEC	0x01	/* pages can be executed */
+#define	PROT_EXEC	0x04	/* pages can be executed */
 
 /*
- * Flags contain mapping type, sharing type and options.
- * Mapping type; choose one
+ * Flags contain sharing type and options.
+ * Sharing types; choose one.
  */
-#define	MAP_FILE	0x0001	/* mapped from a file or device */
-#define	MAP_ANON	0x0002	/* allocated from memory, swap space */
-#define	MAP_TYPE	0x000f	/* mask for type field */
-
-/*
- * Sharing types; choose one
- */
-#define	MAP_COPY	0x0020	/* "copy" region at mmap time */
-#define	MAP_SHARED	0x0010	/* share changes */
-#define	MAP_PRIVATE	0x0000	/* changes are private */
+#define	MAP_SHARED	0x0001	/* share changes */
+#define	MAP_PRIVATE	0x0002	/* changes are private */
+#define	MAP_COPY	0x0004	/* "copy" region at mmap time */
 
 /*
  * Other flags
  */
-#define	MAP_FIXED	0x0100	/* map addr must be exactly as requested */
-#define	MAP_NOEXTEND	0x0200	/* for MAP_FILE, don't change file size */
-#define	MAP_HASSEMPHORE	0x0400	/* region may contain semaphores */
-#define	MAP_INHERIT	0x0800	/* region is retained after exec */
+#define	MAP_FIXED	 0x0010	/* map addr must be exactly as requested */
+#define	MAP_RENAME	 0x0020	/* Sun: rename private pages to file */
+#define	MAP_NORESERVE	 0x0040	/* Sun: don't reserve needed swap area */
+#define	MAP_INHERIT	 0x0080	/* region is retained after exec */
+#define	MAP_NOEXTEND	 0x0100	/* for MAP_FILE, don't change file size */
+#define	MAP_HASSEMAPHORE 0x0200	/* region may contain semaphores */
+
+/*
+ * Mapping type; default is map from file.
+ */
+#define	MAP_ANON	0x1000	/* allocated from memory, swap space */
 
 /*
  * Advice to madvise
