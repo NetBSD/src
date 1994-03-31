366efd30113bb2d23f9d1fc5fdcee0e112d184ee
cgd <cgd@NetBSD.org>
1994-03-31 06:08:28 +0000

    quiet compiler, too

--- lib/libkvm/kvm.c
+++ lib/libkvm/kvm.c
@@ -35,7 +35,7 @@
 
 #if defined(LIBC_SCCS) && !defined(lint)
 /*static char sccsid[] = "from: @(#)kvm.c	5.18 (Berkeley) 5/7/91";*/
-static char rcsid[] = "$Id: kvm.c,v 1.28 1994/03/31 06:06:00 cgd Exp $";
+static char rcsid[] = "$Id: kvm.c,v 1.29 1994/03/31 06:08:28 cgd Exp $";
 #endif /* LIBC_SCCS and not lint */
 
 #include <sys/param.h>
@@ -696,7 +696,7 @@ kvm_getu(p)
 	 */
 	up = (char *) p->p_addr;
 	for (i = 0; i < UPAGES; i++) {
-		klseek(kmem, (off_t)up, 0);
+		klseek(kmem, (off_t)(long)up, 0);
 		if (read(kmem, user.upages[i], CLBYTES) != CLBYTES) {
 			setsyserr("cant read page %x of u of pid %d from %s",
 			    up, p->p_pid, kmemf);
@@ -992,7 +992,7 @@ kvm_read(loc, buf, len)
 
 	if (kvmfilesopen == 0 && kvm_openfiles(NULL, NULL, NULL) == -1)
 		return (-1);
-	klseek(kmem, (off_t) loc, 0);
+	klseek(kmem, (off_t)(long)loc, 0);
 	if ((n = read(kmem, buf, len)) != len) {
 		if (n == -1)
 			setsyserr("error reading kmem at %#x", loc);
