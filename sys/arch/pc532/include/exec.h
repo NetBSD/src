82130972e518733e152c622bb0ecfe9b0d922464
phil <phil@NetBSD.org>
1994-04-17 07:53:25 +0000

    New format for shared libs.

--- sys/arch/pc532/include/exec.h
+++ sys/arch/pc532/include/exec.h
@@ -35,17 +35,15 @@
 /* Relocation format. */
 struct relocation_info_pc532 {
 	int r_address;			/* offset in text or data segment */
-	unsigned int r_symbolnum : 24,	/* ordinal number of add symbol */
+	unsigned int r_symbolnum : 22,	/* ordinal number of add symbol */
+			  r_copy :  1,	/* run time copy */
+		      r_relative :  1,	/* load address relative */
 			 r_pcrel :  1,	/* 1 if value should be pc-relative */
 			r_length :  2,	/* log base 2 of value's width */
 			r_extern :  1,	/* 1 if need to add symbol to value */
-		       r_baserel :  1,	/* linkage table relative */
 		      r_jmptable :  1,	/* relocate to jump table */
-		      r_relative :  1,	/* load address relative */
-			  r_copy :  1;	/* run time copy */
-	unsigned char      r_bsr :  1,  /* Extra fields for the pc532. */
-			  r_disp :  2,
-			   r_pad :  5;
+			  r_disp :  2,	/* ns32k data type */
+		       r_baserel :  1;	/* linkage table relative */
 };
 #define relocation_info	relocation_info_pc532
 
