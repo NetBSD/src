c32a0cf443062d927b3ffb0008c14f0eb56e756b
deraadt <deraadt@NetBSD.org>
1994-04-18 12:10:54 +0000

    better

--- etc/etc.sparc/install.sh
+++ etc/etc.sparc/install.sh
@@ -1,14 +1,10 @@
 #!/bin/sh
-# $Id: install.sh,v 1.1 1994/04/18 10:48:52 deraadt Exp $
+# $Id: install.sh,v 1.2 1994/04/18 12:10:54 deraadt Exp $
 umask 0
-cat ./dev.cpio.gz | gzip -d | (cd /mnt; cpio -iduv)
 cat ./bin.tar.gz | gzip -d | (cd /mnt; tar xvpf -)
+cat ./dev.cpio.gz | gzip -d | (cd /mnt; cpio -iduv)
 cat ./etc.tar.gz | gzip -d | (cd /mnt; tar xvpf -)
-cat ./var.tar.gz | gzip -d | (cd /mnt; tar xvpf -)
 cat ./sbin.tar.gz | gzip -d | (cd /mnt; tar xvpf -)
-if [ ! -d /mnt/usr ]; then
-	mkdir /mnt/usr
-fi
 cat ./usr.bin.tar.gz | gzip -d | (cd /mnt; tar xvpf -)
 cat ./usr.games.tar.gz | gzip -d | (cd /mnt; tar xvpf -)
 cat ./usr.include.tar.gz | gzip -d | (cd /mnt; tar xvpf -)
@@ -17,6 +13,7 @@ cat ./usr.libexec.tar.gz | gzip -d | (cd /mnt; tar xvpf -)
 cat ./usr.misc.tar.gz | gzip -d | (cd /mnt; tar xvpf -)
 cat ./usr.sbin.tar.gz | gzip -d | (cd /mnt; tar xvpf -)
 cat ./usr.share.tar.gz | gzip -d | (cd /mnt; tar xvpf -)
+cat ./var.tar.gz | gzip -d | (cd /mnt; tar xvpf -)
 cp ./netbsd-sd0 /mnt/netbsd-sd0
 cp ./netbsd-sd1 /mnt/netbsd-sd1
 (cd /mnt; ln -s netbsd-sd0 netbsd)
