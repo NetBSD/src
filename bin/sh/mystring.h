7a55f09a872cac527c062aa77c9b2057651044c5
jtc <jtc@NetBSD.org>
1994-05-04 23:46:28 +0000

    Remove private versions of strchr() & bcopy(); there is nothing wrong with the versions in our C library.

--- bin/sh/mystring.h
+++ bin/sh/mystring.h
@@ -34,37 +34,22 @@
  * SUCH DAMAGE.
  *
  *	from: @(#)mystring.h	5.1 (Berkeley) 3/7/91
- *	$Id: mystring.h,v 1.4 1993/08/01 18:58:39 mycroft Exp $
+ *	$Id: mystring.h,v 1.5 1994/05/04 23:46:32 jtc Exp $
  */
 
-#ifndef SYSV
-#define strchr mystrchr
-#endif
+#include <string.h>
 
 #ifdef __STDC__
 void scopyn(const char *, char *, int);
-char *strchr(const char *, int);
-void mybcopy(const pointer, pointer, int);
 int prefix(const char *, const char *);
 int number(const char *);
 int is_number(const char *);
-int strcmp(const char *, const char *);	/* from C library */
-char *strcpy(char *, const char *);	/* from C library */
-int strlen(const char *);		/* from C library */
-char *strcat(char *, const char *);	/* from C library */
 #else
 void scopyn();
-char *strchr();
-void mybcopy();
 int prefix();
 int number();
 int is_number();
-int strcmp();
-char *strcpy();
-int strlen();
-char *strcat();
 #endif
 
 #define equal(s1, s2)	(strcmp(s1, s2) == 0)
 #define scopy(s1, s2)	((void)strcpy(s2, s1))
-#define bcopy(src, dst, n)	mybcopy((pointer)(src), (pointer)(dst), n)
