d3ce6b917ea57fd4caefebae8b1237393724678e
cgd <cgd@NetBSD.org>
1994-05-09 03:35:55 +0000

    clean up import

--- usr.bin/w/proc_compare.c
+++ usr.bin/w/proc_compare.c
@@ -1,6 +1,6 @@
 /*-
- * Copyright (c) 1990 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1990, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -32,43 +32,41 @@
  */
 
 #ifndef lint
-/*static char sccsid[] = "from: @(#)proc_compare.c	5.4 (Berkeley) 2/7/91";*/
-static char rcsid[] = "$Id: proc_compare.c,v 1.4 1994/05/05 06:38:53 cgd Exp $";
+static char sccsid[] = "@(#)proc_compare.c	8.2 (Berkeley) 9/23/93";
 #endif /* not lint */
-
-/*
- * Returns 1 if p2 is more active than p1
- *
- * The algorithm for picking the "more active" process is thus:
- *
- *	1) Runnable processes are favored over anything
- *	   else.  The runner with the highest cpu
- *	   utilization is picked (p_cpu).  Ties are
- *	   broken by picking the highest pid.
- *	2) Next, the sleeper with the shortest sleep
- *	   time is favored.  With ties, we pick out
- *	   just short-term sleepers (p_pri <= PZERO).
- *	   Further ties are broken by picking the highest
- *	   pid.
- *
- *	NOTE - if you change this, be sure to consider making
- *	   the change in the kernel too (^T in kern/tty.c).
- *
- *	TODO - consider whether pctcpu should be used
- *
- */
 
 #include <sys/param.h>
 #include <sys/time.h>
 #include <sys/proc.h>
 
-#define isrun(p)	(((p)->p_stat == SRUN) || ((p)->p_stat == SIDL))
+#include "extern.h"
 
-#define	TESTAB(a, b)	((a)<<1 | (b))
-#define	ONLYA	2
-#define	ONLYB	1
-#define	BOTH	3
+/*
+ * Returns 1 if p2 is "better" than p1
+ *
+ * The algorithm for picking the "interesting" process is thus:
+ *
+ *	1) Only foreground processes are eligible - implied.
+ *	2) Runnable processes are favored over anything else.  The runner
+ *	   with the highest cpu utilization is picked (p_estcpu).  Ties are
+ *	   broken by picking the highest pid.
+ *	3) The sleeper with the shortest sleep time is next.  With ties,
+ *	   we pick out just "short-term" sleepers (P_SINTR == 0).
+ *	4) Further ties are broken by picking the highest pid.
+ *
+ * If you change this, be sure to consider making the change in the kernel
+ * too (^T in kern/tty.c).
+ *
+ * TODO - consider whether pctcpu should be used.
+ */
 
+#define ISRUN(p)	(((p)->p_stat == SRUN) || ((p)->p_stat == SIDL))
+#define TESTAB(a, b)    ((a)<<1 | (b))
+#define ONLYA   2
+#define ONLYB   1
+#define BOTH    3
+
+int
 proc_compare(p1, p2)
 	register struct proc *p1, *p2;
 {
@@ -78,7 +76,7 @@ proc_compare(p1, p2)
 	/*
 	 * see if at least one of them is runnable
 	 */
-	switch (TESTAB(isrun(p1), isrun(p2))) {
+	switch (TESTAB(ISRUN(p1), ISRUN(p2))) {
 	case ONLYA:
 		return (0);
 	case ONLYB:
@@ -93,9 +91,8 @@ proc_compare(p1, p2)
 			return (0);
 		return (p2->p_pid > p1->p_pid);	/* tie - return highest pid */
 	}
-
 	/*
-	 * weed out zombies
+ 	 * weed out zombies
 	 */
 	switch (TESTAB(p1->p_stat == SZOMB, p2->p_stat == SZOMB)) {
 	case ONLYA:
@@ -103,23 +100,21 @@ proc_compare(p1, p2)
 	case ONLYB:
 		return (0);
 	case BOTH:
-		return (p2->p_pid > p1->p_pid);	/* tie - return highest pid */
+		return (p2->p_pid > p1->p_pid); /* tie - return highest pid */
 	}
-
-	/* 
+	/*
 	 * pick the one with the smallest sleep time
 	 */
 	if (p2->p_slptime > p1->p_slptime)
 		return (0);
 	if (p1->p_slptime > p2->p_slptime)
 		return (1);
-
 	/*
 	 * favor one sleeping in a non-interruptible sleep
 	 */
-	 if (p1->p_flag&P_SINTR && (p2->p_flag&P_SINTR) == 0)
-		 return (1);
-	 if (p2->p_flag&P_SINTR && (p1->p_flag&P_SINTR) == 0)
-		 return (0);
-	return(p2->p_pid > p1->p_pid);		/* tie - return highest pid */
+	if (p1->p_flag & P_SINTR && (p2->p_flag & P_SINTR) == 0)
+		return (1);
+	if (p2->p_flag & P_SINTR && (p1->p_flag & P_SINTR) == 0)
+		return (0);
+	return (p2->p_pid > p1->p_pid);		/* tie - return highest pid */
 }
