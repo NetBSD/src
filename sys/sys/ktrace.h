8fc20c1909a6f22427a43a42ffc27819a9fcf512
cgd <cgd@NetBSD.org>
1994-05-11 07:21:09 +0000

    add rcsid

--- sys/sys/ktrace.h
+++ sys/sys/ktrace.h
@@ -30,7 +30,8 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)ktrace.h	8.1 (Berkeley) 6/2/93
+ *	from: @(#)ktrace.h	8.1 (Berkeley) 6/2/93
+ *	$Id: ktrace.h,v 1.5 1994/05/11 07:21:09 cgd Exp $
  */
 
 /*
