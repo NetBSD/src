2472595134258821fd0a62f1a7acb92d4577413b
mycroft <mycroft>
1994-05-13 08:02:16 +0000

    Clean up import.

--- sbin/routed/trace.c
+++ sbin/routed/trace.c
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1983, 1988 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1983, 1988, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -32,8 +32,8 @@
  */
 
 #ifndef lint
-/*static char sccsid[] = "from: @(#)trace.c	5.11 (Berkeley) 2/28/91";*/
-static char rcsid[] = "$Id: trace.c,v 1.4 1993/08/01 18:24:17 mycroft Exp $";
+/*static char sccsid[] = "from: @(#)trace.c	8.1 (Berkeley) 6/5/93";*/
+static char *rcsid = "$Id: trace.c,v 1.5 1994/05/13 08:04:58 mycroft Exp $";
 #endif /* not lint */
 
 /*
