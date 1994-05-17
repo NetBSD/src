f64158fb28a2b084b470d71533e22a8e23a0039b
cgd <cgd>
1994-05-17 02:12:16 +0000

    (almost) upgrade to latest versions, for phil

--- sys/arch/pc532/include/ansi.h
+++ sys/arch/pc532/include/ansi.h
@@ -1,6 +1,6 @@
 /*-
- * Copyright (c) 1990 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1990, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -30,12 +30,12 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	from: @(#)ansi.h	7.1 (Berkeley) 3/9/91
- *	ansi.h,v 1.1.1.1 1993/09/09 23:53:46 phil Exp
+ *	from: @(#)ansi.h	8.2 (Berkeley) 1/4/94
+ *	$Id: ansi.h,v 1.4 1994/05/17 02:12:16 cgd Exp $
  */
 
-#ifndef	_MACHINE_ANSI_H_
-#define	_MACHINE_ANSI_H_
+#ifndef	_ANSI_H_
+#define	_ANSI_H_
 
 /*
  * Types which are fundamental to the implementation and may appear in
@@ -45,19 +45,29 @@
  *	typedef	_SIZE_T_ size_t;
  *	#undef	_SIZE_T_
  *	#endif
- *
- * Thanks, ANSI!
  */
-
 #define	_CLOCK_T_	unsigned long		/* clock() */
 #define	_PTRDIFF_T_	int			/* ptr1 - ptr2 */
 #define	_SIZE_T_	unsigned int		/* sizeof() */
+#define	_SSIZE_T_	int			/* byte count or error */
 #define	_TIME_T_	long			/* time() */
-#define	_WCHAR_T_	unsigned short		/* wchar_t */
-
-/* va_list is "special" in that it is ONLY defined in stdarg.h AND
-   that _VA_LIST_ is *NOT* undefined as stated in the above comment. */
-
 #define	_VA_LIST_	char *			/* va_list */
+
+/*
+ * Runes (wchar_t) is declared to be an ``int'' instead of the more natural
+ * ``unsigned long'' or ``long''.  Two things are happening here.  It is not
+ * unsigned so that EOF (-1) can be naturally assigned to it and used.  Also,
+ * it looks like 10646 will be a 31 bit standard.  This means that if your
+ * ints cannot hold 32 bits, you will be in trouble.  The reason an int was
+ * chosen over a long is that the is*() and to*() routines take ints (says
+ * ANSI C), but they use _RUNE_T_ instead of int.  By changing it here, you
+ * lose a bit of ANSI conformance, but your programs will still work.
+ *    
+ * Note that _WCHAR_T_ and _RUNE_T_ must be of the same type.  When wchar_t
+ * and rune_t are typedef'd, _WCHAR_T_ will be undef'd, but _RUNE_T remains
+ * defined for ctype.h.
+ */
+#define	_WCHAR_T_	int			/* wchar_t */
+#define	_RUNE_T_	int			/* rune_t */
 
 #endif	/* _ANSI_H_ */
