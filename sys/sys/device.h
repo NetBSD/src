a80a5b5a3077b9b2fca2bc6cb030751fc90fe157
glass <glass@NetBSD.org>
1994-05-20 04:26:12 +0000

    update to 4.4-lite, except for some prototypes in disk.h that would've caused trouble

--- sys/sys/device.h
+++ sys/sys/device.h
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1992 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1992, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This software was developed by the Computer Systems Engineering group
  * at Lawrence Berkeley Laboratory under DARPA contract BG 91-66 and
@@ -11,12 +11,40 @@
  *	This product includes software developed by the University of
  *	California, Lawrence Berkeley Laboratory.
  *
- * %sccs.include.redist.c%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed by the University of
+ *	California, Berkeley and its contributors.
+ * 4. Neither the name of the University nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
  *
- *	%W% (Berkeley) %G%
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
  *
- * from: $Header: /cvsroot/src/sys/sys/device.h,v 1.1 1993/08/13 13:20:07 glass Exp $ (LBL)
+ *	from: @(#)device.h	8.2 (Berkeley) 2/17/94
+ *      $Id: device.h,v 1.2 1994/05/20 04:26:12 glass Exp $
  */
+
+#ifndef _SYS_DEVICE_H_
+#define	_SYS_DEVICE_H_
 
 /*
  * Minimal device structures.
@@ -113,3 +141,5 @@ int config_found __P((struct device *, void *, cfprint_t));
 int config_rootfound __P((char *, void *));
 void config_attach __P((struct device *, struct cfdata *, void *, cfprint_t));
 void evcnt_attach __P((struct device *, const char *, struct evcnt *));
+
+#endif /* !_SYS_DEVICE_H_ */
