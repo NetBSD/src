ae4828905d82c60e0908f1aa77b8f089d195b9ef
cgd <cgd@NetBSD.org>
1994-05-21 07:12:31 +0000

    update to Lite, with local changes

--- sys/sys/syslimits.h
+++ sys/sys/syslimits.h
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1988 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1988, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -30,34 +30,28 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	from: @(#)syslimits.h	7.4 (Berkeley) 2/4/91
- *	$Id: syslimits.h,v 1.8 1994/03/24 01:31:37 jtc Exp $
+ *	from: @(#)syslimits.h	8.1 (Berkeley) 6/2/93
+ *	$Id: syslimits.h,v 1.9 1994/05/21 07:12:31 cgd Exp $
  */
 
-#ifndef _SYS_SYSLIMITS_H_
-#define _SYS_SYSLIMITS_H_
-
 #if !defined(_ANSI_SOURCE)
-#define	ARG_MAX		(256 * 1024)	/* max bytes for an exec function */
-#define	CHILD_MAX	40	/* max simultaneous processes */
-#define	LINK_MAX	32767	/* max file link count */
-#define	MAX_CANON	255	/* max bytes in terminal canonical input line */
-#define	MAX_INPUT	255	/* max bytes in terminal input */
-#define	NAME_MAX	255	/* max number of bytes in a file name */
-#define	NGROUPS_MAX	16	/* max number of supplemental group id's */
-#define	OPEN_MAX	64	/* max open files per process */
-#define	PATH_MAX	1024	/* max number of bytes in pathname */
-#define	PIPE_BUF	512	/* max number of bytes for atomic pipe writes */
-#define SSIZE_MAX	INT_MAX /* max number ssize can return */
+#define	ARG_MAX		 (256 * 1024)	/* max bytes for an exec function */
+#define	CHILD_MAX		   40	/* max simultaneous processes */
+#define	LINK_MAX		32767	/* max file link count */
+#define	MAX_CANON		  255	/* max bytes in term canon input line */
+#define	MAX_INPUT		  255	/* max bytes in terminal input */
+#define	NAME_MAX		  255	/* max bytes in a file name */
+#define	NGROUPS_MAX		   16	/* max supplemental group id's */
+#define	OPEN_MAX		   64	/* max open files per process */
+#define	PATH_MAX		 1024	/* max bytes in pathname */
+#define	PIPE_BUF		  512	/* max bytes for atomic pipe writes */
 
-#define	BC_BASE_MAX	INT_MAX	/* max ibase/obase values allowed by bc(1) */
-#define	BC_DIM_MAX	65535	/* max array elements allowed by bc(1) */
-#define	BC_SCALE_MAX	INT_MAX	/* max scale value allowed by bc(1) */
-#define	BC_STRING_MAX	INT_MAX	/* max const string length allowed by bc(1) */
-#define	COLL_WEIGHTS_MAX 2	/* max weights for order keyword; see locale */
-#define	EXPR_NEST_MAX	32	/* max expressions nested in expr(1) */
-#define	LINE_MAX	2048	/* max length in bytes of an input line */
-#define	RE_DUP_MAX	255	/* max repeated RE's using interval notation */
+#define	BC_BASE_MAX	      INT_MAX	/* max ibase/obase values in bc(1) */
+#define	BC_DIM_MAX		65535	/* max array elements in bc(1) */
+#define	BC_SCALE_MAX	      INT_MAX	/* max scale value in bc(1) */
+#define	BC_STRING_MAX	      INT_MAX	/* max const string length in bc(1) */
+#define	COLL_WEIGHTS_MAX	    2	/* max weights for order keyword */
+#define	EXPR_NEST_MAX		   32	/* max expressions nested in expr(1) */
+#define	LINE_MAX		 2048	/* max bytes in an input line */
+#define	RE_DUP_MAX		  255	/* max RE's in interval notation */
 #endif
-
-#endif /* !_SYS_SYSLIMITS_H_ */
