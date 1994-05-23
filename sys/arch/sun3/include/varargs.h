8c5a079ad7afcae54f6d36c9bbf6865dbcf8901d
gwr <gwr@NetBSD.org>
1994-05-23 18:31:53 +0000

    Contents got doubled somehow - remove 2nd copy.

--- sys/arch/sun3/include/varargs.h
+++ sys/arch/sun3/include/varargs.h
@@ -1,11 +1,5 @@
 /*
- *	$Id: varargs.h,v 1.2 1994/02/23 08:29:24 glass Exp $
- */
-
-/* Just use the common m68k definition */
-#include <m68k/varargs.h>
-/*
- *	$Id: varargs.h,v 1.2 1994/02/23 08:29:24 glass Exp $
+ *	$Id: varargs.h,v 1.3 1994/05/23 18:31:53 gwr Exp $
  */
 
 /* Just use the common m68k definition */
