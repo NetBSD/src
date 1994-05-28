ba3c2c5819f7387c8a5781a09e4aebe589276229
gwr <gwr@NetBSD.org>
1994-05-28 15:45:51 +0000

    New version (from Theo (from 4.4)) - supports multicast.

--- sys/arch/sun3/dev/if_le.h
+++ sys/arch/sun3/dev/if_le.h
@@ -1,19 +1,34 @@
 /*
  * Ethernet software status per interface.
- *
+ */
+
+/* Per interface statistics */
+struct	lestats {
+	long	lexints;	/* transmitter interrupts */
+	long	lerints;	/* receiver interrupts */
+	long	lerbufs;	/* total buffers received during interrupts */
+	long	lerhits;	/* times current rbuf was full */
+	long	lerscans;	/* rbufs scanned before finding first full */
+};
+
+/*
  * Each interface is referenced by a network interface structure,
  * le_if, which the routing code uses to locate the interface.
- * This structure contains the output queue for the interface, its address, ...
+ * This structure contains the output queue for the interface,
+ * its address, ...
  */
+
 struct	le_softc {
-        struct device sc_dev;
-	struct	arpcom sc_ac;	/* common Ethernet structures */
-#define	sc_if	sc_ac.ac_if	/* network-visible interface */
-#define	sc_addr	sc_ac.ac_enaddr	/* hardware Ethernet address */
-	char    *sc_machdep;	/* machine dependent pointer */
-	struct	lereg1 *sc_r1;	/* LANCE registers */
-	struct	lereg2 *sc_r2;	/* dual-port RAM */
-	int	sc_rmd;		/* predicted next rmd to process */
+	struct	device sc_dev;		/* base device */
+	void    *sc_machdep;		/* machine dependent pointer */
+	struct	evcnt sc_intrcnt;	/* # of interrupts, per le */
+	struct	evcnt sc_errcnt;	/* # of errors, per le */
+	struct	arpcom sc_ac;		/* common Ethernet structures */
+#define	sc_if	sc_ac.ac_if		/* network-visible interface */
+#define	sc_addr	sc_ac.ac_enaddr		/* hardware Ethernet address */
+	volatile struct	lereg1 *sc_r1;	/* LANCE registers */
+	volatile struct	lereg2 *sc_r2;	/* dual-port RAM */
+	int	sc_rmd;			/* predicted next rmd to process */
 	int	sc_runt;
 	int	sc_jab;
 	int	sc_merr;
@@ -28,7 +43,5 @@ struct	le_softc {
 	int	sc_txoff;
 	int	sc_busy;
 	short	sc_iflags;
-#if NBPFILTER > 0
-	caddr_t sc_bpf;
-#endif
+	struct	lestats sc_lestats;	/* per interface statistics */
 };
