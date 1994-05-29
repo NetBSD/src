a4843c0e320522a7ec7214b596349e0f1a3da43d
hpeyerl <hpeyerl@NetBSD.org>
1994-05-29 00:35:59 +0000

    (blush). They didn't *ALL* have to be changed.

--- lib/libc/gen/shmget.c
+++ lib/libc/gen/shmget.c
@@ -1,5 +1,5 @@
 #if defined(LIBC_SCCS) && !defined(lint)
-static char *rcsid = "$Id: shmget.c,v 1.3 1994/05/28 23:37:57 hpeyerl Exp $";
+static char *rcsid = "$Id: shmget.c,v 1.4 1994/05/29 00:36:08 hpeyerl Exp $";
 #endif /* LIBC_SCCS and not lint */
 
 #include <sys/types.h>
@@ -7,13 +7,13 @@ static char *rcsid = "$Id: shmget.c,v 1.3 1994/05/28 23:37:57 hpeyerl Exp $";
 #include <sys/shm.h>
 
 #if __STDC__
-void shmget(key_t key, size_t size, int shmflg)
+int shmget(key_t key, int size, int shmflg)
 #else
-void shmget(key, size, shmflg)
+int shmget(key, size, shmflg)
 	key_t key;
-	size_t size;
+	int size;
 	int shmflg;
 #endif
 {
-	return ((void) shmsys(3, key, size, shmflg));
+	return (shmsys(3, key, size, shmflg));
 }
