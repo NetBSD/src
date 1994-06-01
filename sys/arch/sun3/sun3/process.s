528c91763626aab66132df4a8eb318689810e9b3
gwr <gwr@NetBSD.org>
1994-06-01 15:37:03 +0000

    No longer need V_SWTCH (or any other vmstat stuff) in assembly code.

--- sys/arch/sun3/sun3/process.s
+++ sys/arch/sun3/sun3/process.s
@@ -40,7 +40,7 @@
  *
  *	from: @(#)locore.s	7.11 (Berkeley) 5/9/91
  *	locore.s,v 1.2 1993/05/22 07:57:30 cgd Exp
- *	$Id: process.s,v 1.15 1994/05/27 14:58:38 gwr Exp $
+ *	$Id: process.s,v 1.16 1994/06/01 15:37:03 gwr Exp $
  */
 
 /*
@@ -190,7 +190,6 @@ ENTRY(cpu_switch)
 	movl	_curproc,sp@-		| remember last proc running
 #endif
 	clrl	_curproc
-	addql	#1,_cnt+V_SWTCH
 
 Lsw1:
 	/*
@@ -344,43 +343,3 @@ Lsvnofpsave:
 Lsavedone:
 	moveq	#0,d0			| return 0
 	rts
-
-#ifdef notdef
-/*
- * update profiling information for the user
- * addupc(pc, &u.u_prof, ticks)
- */
-ENTRY(addupc)
-	movl	a2,sp@-			| scratch register
-	movl	sp@(12),a2		| get &u.u_prof
-	movl	sp@(8),d0		| get user pc
-	subl	a2@(8),d0		| pc -= pr->pr_off
-	jlt	Lauexit			| less than 0, skip it
-	movl	a2@(12),d1		| get pr->pr_scale
-	lsrl	#1,d0			| pc /= 2
-	lsrl	#1,d1			| scale /= 2
-	mulul	d1,d0			| pc /= scale
-	moveq	#14,d1
-	lsrl	d1,d0			| pc >>= 14
-	bclr	#0,d0			| pc &= ~1
-	cmpl	a2@(4),d0		| too big for buffer?
-	jge	Lauexit			| yes, screw it
-	addl	a2@,d0			| no, add base
-	movl	d0,sp@-			| push address
-	jbsr	_fusword		| grab old value
-	movl	sp@+,a0			| grab address back
-	cmpl	#-1,d0			| access ok
-	jeq	Lauerror		| no, skip out
-	addw	sp@(18),d0		| add tick to current value
-	movl	d0,sp@-			| push value
-	movl	a0,sp@-			| push address
-	jbsr	_susword		| write back new value
-	addql	#8,sp			| pop params
-	tstl	d0			| fault?
-	jeq	Lauexit			| no, all done
-Lauerror:
-	clrl	a2@(12)			| clear scale (turn off prof)
-Lauexit:
-	movl	sp@+,a2			| restore scratch reg
-	rts
-#endif
