cf27f74f25e254844873a58464ca0341d1cefe76
glass <glass@NetBSD.org>
1994-06-02 21:27:50 +0000

    fix a guess...someone tried to boot it on a 240.

--- sys/arch/pmax/pmax/pmaxtype.h
+++ sys/arch/pmax/pmax/pmaxtype.h
@@ -35,7 +35,7 @@
  * SUCH DAMAGE.
  *
  *	from: @(#)pmaxtype.h	8.1 (Berkeley) 6/10/93
- *      $Id: pmaxtype.h,v 1.3 1994/05/27 09:03:58 glass Exp $
+ *      $Id: pmaxtype.h,v 1.4 1994/06/02 21:27:50 glass Exp $
  */
 
 /*
@@ -44,9 +44,9 @@
 #define	DS_PMAX		0x1	/* DECstation 2100/3100 */
 #define	DS_3MAX		0x2	/* DECstation 5000/200 */
 #define	DS_3MIN		0x3	/* DECstation 5000/1xx */
+#define	DS_3MAXPLUS	0x4	/* DECstation 5000/240 */
 #define	DS_LSIS		0x5	/* DECsystem 5800 */
 #define	DS_MIPSFAIR	0x6	/* DECsystem 5400 */
 #define	DS_MAXINE	0x7	/* Personal DECstation 5000/xx */
-#define	DS_3MAXPLUS	0x8	/* DECstation 5000/240 (JUST A GUESS) */
 #define	DS_MIPSFAIR2	0xb	/* DECsystem 5500 */
 #define	DS_MIPSMATE	0xc	/* DECsystem 5100 */
