377e9fc6bbfb80f2a4980b678f4963762031f380
chopps <chopps>
1994-06-04 12:13:49 +0000

    change device name back

--- usr.sbin/iteconfig/pathnames.h
+++ usr.sbin/iteconfig/pathnames.h
@@ -27,7 +27,7 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
- *	$Id: pathnames.h,v 1.1 1994/04/05 04:34:54 cgd Exp $
+ *	$Id: pathnames.h,v 1.2 1994/06/04 12:13:49 chopps Exp $
  */
 
-#define	_PATH_AMIGACONSOLE	"/dev/ite0"
+#define	_PATH_AMIGACONSOLE	"/dev/ttye0"
