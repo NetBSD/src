ee010ff5de695c9a458dddaf21cf78ba6c7eaa0e
mycroft <mycroft@NetBSD.org>
1994-06-13 20:46:42 +0000

    Fix up RCS ids.

--- usr.sbin/amd/amd/host_ops.c
+++ usr.sbin/amd/amd/host_ops.c
@@ -35,10 +35,8 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)host_ops.c	8.1 (Berkeley) 6/6/93
- *
- * $Id: host_ops.c,v 1.2 1994/06/13 19:47:32 mycroft Exp $
- *
+ *	from: @(#)host_ops.c	8.1 (Berkeley) 6/6/93
+ *	$Id: host_ops.c,v 1.3 1994/06/13 20:47:12 mycroft Exp $
  */
 
 #include "am.h"
