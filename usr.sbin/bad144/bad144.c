f7fea23d70bfa6210fa1b875dd21680137f95742
mycroft <mycroft@NetBSD.org>
1994-06-13 22:29:11 +0000

    Merge local changes.

--- usr.sbin/bad144/bad144.c
+++ usr.sbin/bad144/bad144.c
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1980,1986,1988 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1980, 1986, 1988, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -32,14 +32,14 @@
  */
 
 #ifndef lint
-char copyright[] =
-"@(#) Copyright (c) 1980,1986,1988 Regents of the University of California.\n\
- All rights reserved.\n";
+static char copyright[] =
+"@(#) Copyright (c) 1980, 1986, 1988, 1993\n\
+	The Regents of the University of California.  All rights reserved.\n";
 #endif not lint
 
 #ifndef lint
-/*static char sccsid[] = "from: @(#)bad144.c	5.19 (Berkeley) 4/11/91";*/
-static char rcsid[] = "$Id: bad144.c,v 1.5 1994/06/13 20:47:16 cgd Exp $";
+/*static char sccsid[] = "from: @(#)bad144.c	8.1 (Berkeley) 6/6/93";*/
+static char *rcsid = "$Id: bad144.c,v 1.6 1994/06/13 22:29:16 mycroft Exp $";
 #endif not lint
 
 /*
@@ -59,9 +59,9 @@ static char rcsid[] = "$Id: bad144.c,v 1.5 1994/06/13 20:47:16 cgd Exp $";
 #include <sys/param.h>
 #include <sys/dkbad.h>
 #include <sys/ioctl.h>
-#include <ufs/ffs/fs.h>
 #include <sys/file.h>
 #include <sys/disklabel.h>
+#include <ufs/ffs/fs.h>
 
 #include <stdio.h>
 #include <paths.h>
