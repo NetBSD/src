8db09e97d13d9ba9593f0797d5b630193dcf08f0
mycroft <mycroft>
1994-06-13 22:04:11 +0000

    Merge local changes.

--- usr.sbin/quotaon/quotaon.c
+++ usr.sbin/quotaon/quotaon.c
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1980, 1990 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1980, 1990, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Robert Elz at The University of Melbourne.
@@ -35,14 +35,14 @@
  */
 
 #ifndef lint
-char copyright[] =
-"@(#) Copyright (c) 1980, 1990 Regents of the University of California.\n\
- All rights reserved.\n";
+static char copyright[] =
+"@(#) Copyright (c) 1980, 1990, 1993\n\
+	The Regents of the University of California.  All rights reserved.\n";
 #endif /* not lint */
 
 #ifndef lint
-/*static char sccsid[] = "from: @(#)quotaon.c	5.12 (Berkeley) 9/27/90";*/
-static char rcsid[] = "$Id: quotaon.c,v 1.3 1994/06/13 20:54:27 cgd Exp $";
+/*static char sccsid[] = "from: @(#)quotaon.c	8.1 (Berkeley) 6/6/93";*/
+static char *rcsid = "$Id: quotaon.c,v 1.4 1994/06/13 22:04:14 mycroft Exp $";
 #endif /* not lint */
 
 /*
