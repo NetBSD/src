c3e4d7f575573c3d8b57bcb9b69245c519b37f48
mycroft <mycroft>
1994-06-13 20:49:44 +0000

    Fix up RCS ids.

--- usr.sbin/amd/config/os-u3_0.h
+++ usr.sbin/amd/config/os-u3_0.h
@@ -35,9 +35,8 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)os-u3_0.h	8.1 (Berkeley) 6/6/93
- *
- * $Id: os-u3_0.h,v 1.2 1994/06/13 19:49:12 mycroft Exp $
+ *	from: @(#)os-u3_0.h	8.1 (Berkeley) 6/6/93
+ *	$Id: os-u3_0.h,v 1.3 1994/06/13 20:49:45 mycroft Exp $
  *
  * Ultrix 3.0 definitions for Amd (automounter)
  */
