7302a4d7c6fabb88a7f45f3d378cf521f422d2bc
gwr <gwr@NetBSD.org>
1994-06-27 22:21:20 +0000

    New, very simple version that works on SunOS (for cross-development) as well as on NetBSD.  This does sift out debugging symbols, but I think one should use "strip -d" for that.

--- usr.sbin/dbsym/dbsym.c
+++ usr.sbin/dbsym/dbsym.c
@@ -1,251 +1,241 @@
-/* Written by Pace Willisson (pace@blitz.com)
- * and placed in the public domain.
+/*
+ * Copyright (c) 1994 Gordon W. Ross
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *	$Id: dbsym.c,v 1.10 1994/06/27 22:21:20 gwr Exp $
  */
 
-#ifndef lint
-static char rcsid[] = "$Id: dbsym.c,v 1.9 1993/10/13 18:33:55 jtc Exp $";
-#endif /* not lint */
+/* Copy the symbol table into the space reserved for it. */
+
+#include <sys/types.h>
+#include <sys/file.h>
+#include <sys/mman.h>
 
 #include <stdio.h>
-#include <stdlib.h>
 #include <a.out.h>
-#include <stab.h>
-#include <machine/param.h>
-#include <unistd.h>
 
-#define FILE_OFFSET(vadr) (((vadr) - text_adr) - N_DATADDR(hdr) + \
-			   N_DATOFF(hdr) + N_TXTADDR(hdr))
+extern off_t lseek();
 
-u_int text_adr = KERNBASE;
+struct exec head;
+char *file;
 
-struct nlist *old_syms;
-int num_old_syms;
-char *old_strtab;
-int old_strtab_size;
+/* Virtual addresses of the symbols we frob. */
+long db_symtab_va, db_symtabsize_va;
 
-struct nlist *new_syms;
-int num_new_syms;
-int new_syms_bytes;
-char *new_strtab;
-int new_strtab_size;
+/* Offsets relative to start of data segment. */
+long db_symtab_off, db_symtabsize_off;
 
-int db_symtabsize_adr;
-int db_symtab_adr;
+/* value in the location at db_symtabsize_off */
+int db_symtabsize_val;
 
-int avail;
+/* pointers to pieces of mapped file */
+char *dataseg;
+char *symbols;
+char *strings;
+int *strtab;
+/* and lengths */
+int strtab_len;
+int file_len;
+int data_off;
+int data_pgoff;
 
-int force = 0;
-int zap_locals = 0;
-int debugging = 0;
-
-usage ()
+main(argc,argv)
+	char **argv;
 {
-	fprintf (stderr, "usage: dbsym [-fgx] [-T addr] file\n");
-	exit (1);
+	int fd, n;
+	int *ip;
+	char *cp;
+
+	if (argc < 2) {
+		printf("%s: missing file name\n", argv[0]);
+		exit(1);
+	}
+	file = argv[1];
+
+	fd = open(file, O_RDWR);
+	if (fd < 0) {
+		perror(file);
+		exit(1);
+	}
+
+	n = read(fd, &head, sizeof(head));
+	if (n < sizeof(head)) {
+		printf("%s: reading header\n", file);
+		exit(1);
+	}
+	file_len = lseek(fd, (off_t)0, 2);
+
+	if (N_BADMAG(head)) {
+		printf("%s: bad magic number\n");
+		exit(1);
+	}
+
+#ifdef	DEBUG
+	printf(" text:  %9d\n", head.a_text);
+	printf(" data:  %9d\n", head.a_data);
+	printf("  bss:  %9d\n", head.a_bss);
+	printf(" syms:  %9d\n", head.a_syms);
+	printf("entry: 0x%08X\n", head.a_entry);
+	printf("trsiz:  %9d\n", head.a_trsize);
+	printf("drsiz:  %9d\n", head.a_drsize);
+#endif
+
+	if (head.a_syms <= 0) {
+		printf("%s: no symbols\n", file);
+		exit(1);
+	}
+	if (head.a_trsize ||
+		head.a_drsize)
+	{
+		printf("%s: has relocations\n");
+		exit(1);
+	}
+
+	find_db_symtab(file);
+
+	/*
+	 * Map in the file from start of data to EOF.
+	 * The file offset needs to be page aligned.
+	 */
+	data_off = N_DATOFF(head);
+	data_pgoff = N_PAGSIZ(head) - 1;
+	data_pgoff &= data_off;
+	data_off -= data_pgoff;
+	dataseg = mmap(NULL,	/* any address is ok */
+				   file_len - data_off, /* length */
+				   PROT_READ | PROT_WRITE,
+				   MAP_SHARED,
+				   fd, data_off);
+	if ((long)dataseg == -1) {
+		printf("%s: can not map data seg\n", file);
+		perror(file);
+		exit(1);
+	}
+	dataseg += data_pgoff;
+	symbols = dataseg + head.a_data;
+	strings = symbols + head.a_syms;
+	/* Find the string table length (first word after symtab). */
+	strtab_len = *((int*)strings);
+#ifdef	DEBUG
+	printf("strtab_len: %d\n", strtab_len);
+#endif
+
+	/*
+	 * Find value in the location: db_symtabsize
+	 */
+	ip = (int*) (dataseg + db_symtabsize_off);
+	db_symtabsize_val = *ip;
+#ifdef	DEBUG
+	printf("db_symtabsize val: 0x%08X (%d)\n",
+		   db_symtabsize_val, db_symtabsize_val);
+#endif
+	/* Is there room for the symbols + strings? */
+	if (db_symtabsize_val < (head.a_syms + strtab_len)) {
+		printf("%s: symbol space too small (%d < %d)\n", argv[0],
+			   db_symtabsize_val, (head.a_syms + strtab_len));
+		exit(1);
+	}
+	printf("Symbols use %d of %d bytes available (%d%%)\n",
+		   head.a_syms + strtab_len,
+		   db_symtabsize_val,
+		   (head.a_syms + strtab_len) * 100 /
+		   db_symtabsize_val);
+
+	/*
+	 * Copy the symbol table and string table.
+	 */
+#ifdef	DEBUG
+	printf("copying symbol table...\n");
+#endif
+	ip = (int*) (dataseg + db_symtab_off);
+	/* Write symtab length */
+	*ip++ = head.a_syms;
+	memcpy((char*)ip, symbols, head.a_syms + strtab_len);
+
+	msync(dataseg - data_pgoff, file_len - data_off
+#ifdef	sun
+		  ,0
+#endif
+		  );
+
+#ifdef	DEBUG
+	printf("...done\n");
+#endif
+	close(fd);
+	exit(0);
 }
 
-struct exec hdr;
 
-main (argc, argv)
-char **argv;
+/*
+ * Find locations of the symbols to patch.
+ */
+struct nlist wantsyms[] = {
+	{ "_db_symtabsize", 0 },
+	{ "_db_symtab", 0 },
+	{ NULL, 0 },
+};
+
+find_db_symtab(file)
+	char *file;
 {
-	FILE *f;
-	char *name;
-	extern int optind;
-	int c, i;
-	int need;
-	char *buf, *p;
-	struct nlist *nsp, *sp;
-	int len;
+	int data_va;
+	int std_entry;
 
-
-	while ((c = getopt (argc, argv, "fgxT:")) != EOF) {
-		switch (c) {
-		case 'f':
-			force = 1;
-			break;
-		case 'g':
-			debugging = 1;
-			break;
-                case 'x':
-                        zap_locals = 1;
-                        break;
-		case 'T':
-			text_adr = strtoul(optarg, &p, 16);
-			if (*p)
-				err("illegal text address: %s", optarg);
-			break;
-		default:
-			usage ();
-		}
+	if (nlist(file, wantsyms)) {
+		printf("%s: no db_symtab symbols?\n", file);
+		exit(1);
+	}
+	std_entry = N_TXTADDR(head) +
+	    (head.a_entry & (N_PAGSIZ(head)-1));
+	data_va = N_DATADDR(head);
+	if (head.a_entry != std_entry) {
+		printf("%s: warning: non-standard entry point: 0x%08x\n",
+			   file, head.a_entry);
+		printf("\texpecting entry=0x%X\n", std_entry);
+		data_va += (head.a_entry - std_entry);
 	}
 
-	if (optind >= argc)
-		usage ();
+	db_symtabsize_off = wantsyms[0].n_value - data_va;
+	db_symtab_off     = wantsyms[1].n_value - data_va;
+#ifdef	DEBUG
+	printf(".data segment  va: 0x%08X\n", data_va);
+	printf("db_symtabsize  va: 0x%08X\n", wantsyms[0].n_value);
+	printf("db_symtab      va: 0x%08X\n", wantsyms[1].n_value);
+	printf("db_symtabsize off: 0x%08X\n", db_symtabsize_off);
+	printf("db_symtab     off: 0x%08X\n", db_symtab_off);
+#endif
 
-	name = argv[optind++];
-
-	if (optind != argc)
-		usage ();
-
-	if ((f = fopen (name, "r+")) == NULL) {
-		fprintf (stderr, "can't open %s\n", name);
-		exit (1);
+	/*
+	 * Sanity check locations of db_* symbols
+	 */
+	if (db_symtab_off < 0 || db_symtab_off >= head.a_data) {
+		printf("%s: db_symtab not in data segment?\n", file);
+		exit(1);
 	}
-
-	if (fread ((char *)&hdr, sizeof hdr, 1, f) != 1) {
-		fprintf (stderr, "can't read header\n");
-		exit (1);
+	if (db_symtabsize_off < 0 || db_symtabsize_off >= head.a_data) {
+		printf("%s: db_symtabsize not in data segment?\n", file);
+		exit(1);
 	}
-
-	if (N_BADMAG (hdr)) {
-		fprintf (stderr, "bad magic number\n");
-		exit (1);
-	}
-
-	if (hdr.a_syms == 0) {
-		fprintf (stderr, "no symbols\n");
-		exit (1);
-	}
-
-	fseek (f, N_STROFF (hdr), 0);
-	if (fread ((char *)&old_strtab_size, sizeof (int), 1, f) != 1) {
-		fprintf (stderr, "can't read old strtab size\n");
-		exit (1);
-	}
-
-	if ((old_syms = (struct nlist *)malloc (hdr.a_syms)) == NULL
-	    || ((old_strtab = malloc (old_strtab_size)) == NULL)
-	    || ((new_syms = (struct nlist *)malloc (hdr.a_syms)) == NULL)
-	    || ((new_strtab = malloc (old_strtab_size)) == NULL)) {
-		fprintf (stderr, "out of memory\n");
-		exit (1);
-	}
-
-	fseek (f, N_SYMOFF (hdr), 0);
-	if (fread ((char *)old_syms, hdr.a_syms, 1, f) != 1) {
-		fprintf (stderr, "can't read symbols\n");
-		exit (1);
-	}
-
-	fseek (f, N_STROFF (hdr), 0);
-	if (fread ((char *)old_strtab, old_strtab_size, 1, f) != 1) {
-		fprintf (stderr, "can't read string table\n");
-		exit (1);
-	}
-
-	num_old_syms = hdr.a_syms / sizeof (struct nlist);
-
-	new_strtab_size = 4;
-
-	nsp = new_syms;
-	for (i = 0, sp = old_syms; i < num_old_syms; i++, sp++) {
-		if (zap_locals && !(sp->n_type & N_EXT))
-			continue;
-
-		if (sp->n_type & N_STAB)
-			switch (sp->n_type & ~N_EXT) {
-				case N_SLINE:
-					if (debugging)
-						*nsp++ = *sp;
-					continue;
-				case N_FUN:
-				case N_PSYM:
-				case N_SO:
-					if (!debugging)
-						continue;
-					goto skip_tests;
-					break;
-				default:
-					continue;
-			}
-
-		if ((sp->n_type & ~N_EXT) == N_UNDF)
-			continue;
-                
-                if (!debugging && (sp->n_type & ~N_EXT) == N_FN)
-			continue;
-
-		if (sp->n_un.n_strx == 0)
-			continue;
-
-		if (sp->n_value < text_adr)
-			continue;
-
-		if (sp->n_value > (text_adr + hdr.a_text + hdr.a_data +
-				   hdr.a_bss))
-			continue;
-
-		skip_tests:
-
-		name = old_strtab + sp->n_un.n_strx;
-
-		len = strlen (name);
-
-		if (len == 0)
-			continue;
-
-		if (strcmp (name, "gcc_compiled.") == 0)
-			continue;
-
-		if (strcmp (name, "gcc2_compiled.") == 0)
-			continue;
-
-                if (strcmp (name, "___gnu_compiled_c") == 0)
-			continue;
-                        
-		*nsp = *sp;
-
-		nsp->n_un.n_strx = new_strtab_size;
-		strcpy (new_strtab + new_strtab_size, name);
-		new_strtab_size += len + 1;
-		nsp++;
-
-		if (strcmp (name, "_db_symtab") == 0)
-			db_symtab_adr = sp->n_value;
-		if (strcmp (name, "_db_symtabsize") == 0)
-			db_symtabsize_adr = sp->n_value;
-	}
-	
-	if (db_symtab_adr == 0 || db_symtabsize_adr == 0)
-		if (!force) {
-			fprintf (stderr, "couldn't find db_symtab symbols\n");
-			exit (1);
-		} else
-			exit (0);
-
-	*(int *)new_strtab = new_strtab_size;
-	num_new_syms = nsp - new_syms;
-	new_syms_bytes = num_new_syms * sizeof (struct nlist);
-
-	need = sizeof (int)
-		+ num_new_syms * sizeof (struct nlist)
-			+ new_strtab_size;
-
-	fseek (f, FILE_OFFSET (db_symtabsize_adr), 0);
-
-	if (fread ((char *)&avail, sizeof (int), 1, f) != 1) {
-		fprintf (stderr, "can't read symtabsize\n");
-		exit (1);
-	}
-
-	printf ("dbsym: need %d; avail %d\n", need, avail);
-
-	if (need > avail) {
-		fprintf (stderr, "not enough room in db_symtab array\n");
-		exit (1);
-	}
-
-	fseek (f, FILE_OFFSET (db_symtab_adr), 0);
-	fwrite ((char *)&new_syms_bytes, sizeof (int), 1, f);
-	fwrite ((char *)new_syms, new_syms_bytes, 1, f);
-	fwrite (new_strtab, new_strtab_size, 1, f);
-	fflush (f);
-
-	if (feof (f) || ferror (f)) {
-		fprintf (stderr, "write error\n");
-		exit (1);
-	}
-	exit (0);
 }
-
