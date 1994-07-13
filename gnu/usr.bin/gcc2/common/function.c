4c5a1210b254287cc836a5a3b928764a6525c8d4
cgd <cgd@NetBSD.org>
1994-07-13 08:59:19 +0000

    from trunk, per mycroft.

--- gnu/usr.bin/gcc2/common/function.c
+++ gnu/usr.bin/gcc2/common/function.c
@@ -18,7 +18,7 @@ along with GNU CC; see the file COPYING.  If not, write to
 the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
 
 #ifndef lint
-static char rcsid[] = "$Id: function.c,v 1.2 1993/08/02 17:34:34 mycroft Exp $";
+static char rcsid[] = "$Id: function.c,v 1.2.4.1 1994/07/13 08:59:40 cgd Exp $";
 #endif /* not lint */
 
 /* This file handles the generation of rtl code from tree structure
@@ -1011,15 +1011,37 @@ fixup_var_refs_insns (var, promoted_mode, unsignedp, insn, toplevel)
       rtx note;
       if (GET_RTX_CLASS (GET_CODE (insn)) == 'i')
 	{
+	  /* If this is a CLOBBER of VAR, delete it.
+
+	     If it has a REG_LIBCALL note, delete the REG_LIBCALL
+	     and REG_RETVAL notes too.  */
+	  if (GET_CODE (PATTERN (insn)) == CLOBBER
+	      && XEXP (PATTERN (insn), 0) == var)
+	    {
+	      if ((note = find_reg_note (insn, REG_LIBCALL, NULL_RTX)) != 0)
+		/* The REG_LIBCALL note will go away since we are going to
+		   turn INSN into a NOTE, so just delete the
+		   corresponding REG_RETVAL note.  */
+		remove_note (XEXP (note, 0),
+			     find_reg_note (XEXP (note, 0), REG_RETVAL,
+					    NULL_RTX));
+
+	      /* In unoptimized compilation, we shouldn't call delete_insn
+		 except in jump.c doing warnings.  */
+	      PUT_CODE (insn, NOTE);
+	      NOTE_LINE_NUMBER (insn) = NOTE_INSN_DELETED;
+	      NOTE_SOURCE_FILE (insn) = 0;
+	    }
+
 	  /* The insn to load VAR from a home in the arglist
 	     is now a no-op.  When we see it, just delete it.  */
-	  if (toplevel
-	      && GET_CODE (PATTERN (insn)) == SET
-	      && SET_DEST (PATTERN (insn)) == var
-	      /* If this represents the result of an insn group,
-		 don't delete the insn.  */
-	      && find_reg_note (insn, REG_RETVAL, NULL_RTX) == 0
-	      && rtx_equal_p (SET_SRC (PATTERN (insn)), var))
+	  else if (toplevel
+		   && GET_CODE (PATTERN (insn)) == SET
+		   && SET_DEST (PATTERN (insn)) == var
+		   /* If this represents the result of an insn group,
+		      don't delete the insn.  */
+		   && find_reg_note (insn, REG_RETVAL, NULL_RTX) == 0
+		   && rtx_equal_p (SET_SRC (PATTERN (insn)), var))
 	    {
 	      /* In unoptimized compilation, we shouldn't call delete_insn
 		 except in jump.c doing warnings.  */
