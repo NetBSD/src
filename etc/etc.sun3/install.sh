99f9bbf0a1dea6e003b483aee0b031030909521b
gwr <gwr@NetBSD.org>
1994-07-13 20:45:31 +0000

    first cut

---
+++ etc/etc.sun3/install.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# $Id: install.sh,v 1.1 1994/07/13 20:45:36 gwr Exp $
+umask 0
+[ "$TARDIR" ] || { echo "$0: set TARDIR first" ; exit 1; }
+
+while read f
+do
+	gzip -d < $TARDIR/$f | tar xvpf -
+done << \END_LIST
+etc.tar.gz
+dev.tar.gz
+var.tar.gz
+bin.tar.gz
+sbin.tar.gz
+usr.bin.tar.gz
+usr.games.tar.gz
+usr.include.tar.gz
+usr.lib.tar.gz
+usr.libexec.tar.gz
+usr.misc.tar.gz
+usr.sbin.tar.gz
+usr.share.tar.gz
+END_LIST
+
+cp -p $TARDIR/netbsd.* .
