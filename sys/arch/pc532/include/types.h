caa0868e93eb569725ffb74f6ecadbda67f4ba5f
cgd <cgd@NetBSD.org>
1994-07-20 05:44:19 +0000

    define __BIT_TYPES_DEFINED__ for compatibility with things like BIND and nvi

--- sys/arch/pc532/include/types.h
+++ sys/arch/pc532/include/types.h
@@ -53,6 +53,7 @@ typedef	unsigned long	vm_size_t;
  * Basic integral types.  Omit the typedef if
  * not possible for a machine/compiler combination.
  */
+#define	__BIT_TYPES_DEFINED__
 typedef	__signed char		   int8_t;
 typedef	unsigned char		 u_int8_t;
 typedef	short			  int16_t;
