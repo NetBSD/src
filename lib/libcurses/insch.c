63d1eef788f76e608a20073b7dd7fed7e7dcdadb
cgd <cgd>
1994-08-17 21:12:30 +0000

    new libcurses, from new nvi

--- lib/libcurses/insch.c
+++ lib/libcurses/insch.c
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1981 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1981, 1993, 1994
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -32,37 +32,43 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)insch.c	5.4 (Berkeley) 6/1/90";
-#endif /* not lint */
+static char sccsid[] = "@(#)insch.c	8.2 (Berkeley) 5/4/94";
+#endif	/* not lint */
 
-# include	"curses.ext"
+#include <string.h>
+
+#include "curses.h"
 
 /*
- *	This routine performs an insert-char on the line, leaving
- * (_cury,_curx) unchanged.
- *
+ * winsch --
+ *	Do an insert-char on the line, leaving (cury, curx) unchanged.
  */
-winsch(win, c)
-reg WINDOW	*win;
-char		c; {
+int
+winsch(win, ch)
+	register WINDOW *win;
+	int ch;
+{
 
-	reg char	*temp1, *temp2;
-	reg char	*end;
+	register __LDATA *end, *temp1, *temp2;
 
-	end = &win->_y[win->_cury][win->_curx];
-	temp1 = &win->_y[win->_cury][win->_maxx - 1];
+	end = &win->lines[win->cury]->line[win->curx];
+	temp1 = &win->lines[win->cury]->line[win->maxx - 1];
 	temp2 = temp1 - 1;
-	while (temp1 > end)
-		*temp1-- = *temp2--;
-	*temp1 = c;
-	touchline(win, win->_cury, win->_curx, win->_maxx - 1);
-	if (win->_cury == LINES - 1 && win->_y[LINES-1][COLS-1] != ' ')
-		if (win->_scroll) {
+	while (temp1 > end) {
+		(void)memcpy(temp1, temp2, sizeof(__LDATA));
+		temp1--, temp2--;
+	}
+	temp1->ch = ch;
+	temp1->attr &= ~__STANDOUT;
+	__touchline(win, win->cury, win->curx, win->maxx - 1, 0);
+	if (win->cury == LINES - 1 && 
+	    (win->lines[LINES - 1]->line[COLS - 1].ch != ' ' ||
+	    win->lines[LINES -1]->line[COLS - 1].attr != 0))
+		if (win->flags & __SCROLLOK) {
 			wrefresh(win);
 			scroll(win);
-			win->_cury--;
-		}
-		else
-			return ERR;
-	return OK;
+			win->cury--;
+		} else
+			return (ERR);
+	return (OK);
 }
