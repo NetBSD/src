c31abe9b057f9df799aa139ebd3a5a15a2ceb44c
pk <pk>
1994-08-17 20:10:27 +0000

    Cleanup import, merge local changes.

--- libexec/getty/pathnames.h
+++ libexec/getty/pathnames.h
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1989 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1989, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -30,8 +30,8 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	from: @(#)pathnames.h	5.3 (Berkeley) 6/1/90
- *	$Id: pathnames.h,v 1.2 1993/08/01 18:30:27 mycroft Exp $
+ *	from: @(#)pathnames.h	8.1 (Berkeley) 6/4/93
+ *	$Id: pathnames.h,v 1.3 1994/08/17 20:10:39 pk Exp $
  */
 
 #include <paths.h>
