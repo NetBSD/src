0621b0944e0a11f31dae3a5519514ec7323e98e4
cgd <cgd@NetBSD.org>
1994-08-29 15:30:15 +0000

    i386 floppy build tools

---
+++ distrib/i386/floppies/runlist.sh
@@ -0,0 +1,13 @@
+#	$Id: runlist.sh,v 1.1.1.1.2.2 1994/08/29 15:30:15 cgd Exp $
+
+if [ "X$1" = "X-d" ]; then
+	SHELLCMD=cat
+	shift
+else
+	SHELLCMD="sh -e"
+fi
+
+( while [ "X$1" != "X" ]; do
+	cat $1
+	shift
+done ) | awk -f ${TOPDIR}/list2sh.awk | ${SHELLCMD}
