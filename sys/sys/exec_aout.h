ea7307006abb507cafee9adb59abce0523fcb644
deraadt <deraadt@NetBSD.org>
1994-09-13 15:47:03 +0000

    add MID_MIPS (big endian MIPS)

--- sys/sys/exec_aout.h
+++ sys/sys/exec_aout.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: exec_aout.h,v 1.8 1994/08/13 08:36:11 pk Exp $	*/
+/*	$NetBSD: exec_aout.h,v 1.9 1994/09/13 15:47:03 deraadt Exp $	*/
 
 /*
  * Copyright (c) 1993, 1994 Christopher G. Demetriou
@@ -73,6 +73,7 @@ struct exec {
 #define	MID_PMAX	139	/* pmax */
 #define	MID_VAX		140	/* vax */
 #define	MID_ALPHA	141	/* Alpha BSD binary */
+#define	MID_MIPS	142	/* big-endian MIPS */
 #define	MID_HP300	300	/* hp300 (68020+68881) BSD binary */
 #define	MID_HPUX	0x20C	/* hp200/300 HP-UX binary */
 #define	MID_HPUX800     0x20B   /* hp800 HP-UX binary */
