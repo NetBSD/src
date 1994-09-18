6a267eee41170cd8f47817d50e994e72e81ac102
mycroft <mycroft@NetBSD.org>
1994-09-18 02:14:32 +0000

    4.4-Lite version, slightly edited.

--- usr.bin/whereis/whereis.c
+++ usr.bin/whereis/whereis.c
@@ -1,6 +1,6 @@
 /*-
- * Copyright (c) 1980 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -32,356 +32,89 @@
  */
 
 #ifndef lint
-char copyright[] =
-"@(#) Copyright (c) 1980 The Regents of the University of California.\n\
- All rights reserved.\n";
+static char copyright[] =
+"@(#) Copyright (c) 1993\n\
+	The Regents of the University of California.  All rights reserved.\n";
 #endif /* not lint */
 
 #ifndef lint
-/*static char sccsid[] = "from: @(#)whereis.c	5.5 (Berkeley) 4/18/91";*/
-static char rcsid[] = "$Id: whereis.c,v 1.4 1993/12/04 01:08:35 jtc Exp $";
+/*static char sccsid[] = "from: @(#)whereis.c	8.1 (Berkeley) 6/6/93";*/
+static char *rcsid = "$Id: whereis.c,v 1.5 1994/09/18 02:14:34 mycroft Exp $";
 #endif /* not lint */
 
 #include <sys/param.h>
-#include <dirent.h>
+#include <sys/stat.h>
+#include <sys/sysctl.h>
+
+#include <err.h>
+#include <errno.h>
 #include <stdio.h>
-#include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
 
-static char *bindirs[] = {
-	"/bin",
-	"/sbin",
-	"/usr/ucb",
-	"/usr/bin",
-	"/usr/sbin",
-	"/usr/old",
-	"/usr/contrib",
-	"/usr/games",
-	"/usr/local",
-	"/usr/libexec",
-	"/usr/include",
-	"/usr/hosts",
-	"/usr/share", /*?*/
-	"/etc",
-#ifdef notdef
-	/* before reorg */
-	"/etc",
-	"/bin",
-	"/usr/bin",
-	"/usr/games",
-	"/lib",
-	"/usr/ucb",
-	"/usr/lib",
-	"/usr/local",
-	"/usr/new",
-	"/usr/old",
-	"/usr/hosts",
-	"/usr/include",
-#endif
-	0
-};
-/* This needs to be redone - man pages live with sources */
-static char *mandirs[] = {
-	"/usr/man/man1",
-	"/usr/man/man2",
-	"/usr/man/man3",
-	"/usr/man/man4",
-	"/usr/man/man5",
-	"/usr/man/man6",
-	"/usr/man/man7",
-	"/usr/man/man8",
-	"/usr/man/manl",
-	"/usr/man/mann",
-	"/usr/man/mano",
-	0
-};
-static char *srcdirs[]  = {
-	"/usr/src/bin",
-	"/usr/src/sbin",
-	"/usr/src/etc",
-	"/usr/src/pgrm",
-	"/usr/src/usr.bin",
-	"/usr/src/gnu/usr.bin",
-	"/usr/src/usr.sbin",
-	"/usr/src/usr.ucb",
-	"/usr/src/usr.new",
-	"/usr/src/lib",
-	"/usr/src/gnu/lib",
-	"/usr/src/libexec",
-	"/usr/src/gnu/libexec",
-	"/usr/src/libdata",
-	"/usr/src/share",
-	"/usr/src/contrib",
-	"/usr/src/athena",
-	"/usr/src/devel",
-	"/usr/src/games",
-	"/usr/src/gnu/games",
-	"/usr/src/local",
-	"/usr/src/man",
-	"/usr/src/root",
-	"/usr/src/old",
-	"/usr/src/include",
-	/* still need libs */
-#ifdef notdef /* before reorg */
-	"/usr/src/bin",
-	"/usr/src/usr.bin",
-	"/usr/src/etc",
-	"/usr/src/ucb",
-	"/usr/src/games",
-	"/usr/src/usr.lib",
-	"/usr/src/lib",
-	"/usr/src/local",
-	"/usr/src/new",
-	"/usr/src/old",
-	"/usr/src/include",
-	"/usr/src/lib/libc/gen",
-	"/usr/src/lib/libc/stdio",
-	"/usr/src/lib/libc/sys",
-	"/usr/src/lib/libc/net/common",
-	"/usr/src/lib/libc/net/inet",
-	"/usr/src/lib/libc/net/misc",
-	"/usr/src/ucb/pascal",
-	"/usr/src/ucb/pascal/utilities",
-	"/usr/src/undoc",
-#endif
-	0
-};
+void usage __P((void));
 
-char	sflag = 1;
-char	bflag = 1;
-char	mflag = 1;
-char	**Sflag;
-int	Scnt;
-char	**Bflag;
-int	Bcnt;
-char	**Mflag;
-int	Mcnt;
-char	uflag;
-/*
- * whereis name
- * look for source, documentation and binaries
- */
+int
 main(argc, argv)
 	int argc;
 	char *argv[];
 {
+	struct stat sb;
+	size_t len;
+	int ch, sverrno, mib[2];
+	char *p, *t, *std, path[MAXPATHLEN];
 
-	argc--, argv++;
-	if (argc == 0) {
-usage:
-		fprintf(stderr, "whereis [ -sbmu ] [ -SBM dir ... -f ] name...\n");
-		exit(1);
+	while ((ch = getopt(argc, argv, "")) != EOF)
+		switch (ch) {
+		case '?':
+		default:
+			usage();
+		}
+	argc -= optind;
+	argv += optind;
+
+	if (argc == 0)
+		usage();
+
+	/* Retrieve the standard path. */
+	mib[0] = CTL_USER;
+	mib[1] = USER_CS_PATH;
+	if (sysctl(mib, 2, NULL, &len, NULL, 0) == -1)
+		return (-1);
+	if (len == 0)
+		err(1, "user_cs_path: sysctl: zero length\n");
+	if ((std = malloc(len)) == NULL)
+		err(1, NULL);
+	if (sysctl(mib, 2, std, &len, NULL, 0) == -1) {
+		sverrno = errno;
+		free(std);
+		errno = sverrno;
+		err(1, "sysctl: user_cs_path");
 	}
-	do
-		if (argv[0][0] == '-') {
-			register char *cp = argv[0] + 1;
-			while (*cp) switch (*cp++) {
 
-			case 'f':
+	/* For each path, for each program... */
+	for (; *argv; ++argv)
+		for (p = std;; *p++ = ':') {
+			t = p;
+			if ((p = strchr(p, ':')) != NULL) {
+				*p = '\0';
+				if (t == p)
+					t = ".";
+			} else
+				if (strlen(t) == 0)
+					t = ".";
+			(void)snprintf(path, sizeof(path), "%s/%s", t, *argv);
+			if (!stat(path, &sb))
+				(void)printf("%s\n", path);
+			if (p == NULL)
 				break;
-
-			case 'S':
-				getlist(&argc, &argv, &Sflag, &Scnt);
-				break;
-
-			case 'B':
-				getlist(&argc, &argv, &Bflag, &Bcnt);
-				break;
-
-			case 'M':
-				getlist(&argc, &argv, &Mflag, &Mcnt);
-				break;
-
-			case 's':
-				zerof();
-				sflag++;
-				continue;
-
-			case 'u':
-				uflag++;
-				continue;
-
-			case 'b':
-				zerof();
-				bflag++;
-				continue;
-
-			case 'm':
-				zerof();
-				mflag++;
-				continue;
-
-			default:
-				goto usage;
-			}
-			argv++;
-		} else
-			lookup(*argv++);
-	while (--argc > 0);
-	exit(0);
-}
-
-getlist(argcp, argvp, flagp, cntp)
-	char ***argvp;
-	int *argcp;
-	char ***flagp;
-	int *cntp;
-{
-
-	(*argvp)++;
-	*flagp = *argvp;
-	*cntp = 0;
-	for ((*argcp)--; *argcp > 0 && (*argvp)[0][0] != '-'; (*argcp)--)
-		(*cntp)++, (*argvp)++;
-	(*argcp)++;
-	(*argvp)--;
-}
-
-
-zerof()
-{
-
-	if (sflag && bflag && mflag)
-		sflag = bflag = mflag = 0;
-}
-int	count;
-int	print;
-
-
-lookup(cp)
-	register char *cp;
-{
-	register char *dp;
-
-	for (dp = cp; *dp; dp++)
-		continue;
-	for (; dp > cp; dp--) {
-		if (*dp == '.') {
-			*dp = 0;
-			break;
 		}
-	}
-	for (dp = cp; *dp; dp++)
-		if (*dp == '/')
-			cp = dp + 1;
-	if (uflag) {
-		print = 0;
-		count = 0;
-	} else
-		print = 1;
-again:
-	if (print)
-		printf("%s:", cp);
-	if (sflag) {
-		looksrc(cp);
-		if (uflag && print == 0 && count != 1) {
-			print = 1;
-			goto again;
-		}
-	}
-	count = 0;
-	if (bflag) {
-		lookbin(cp);
-		if (uflag && print == 0 && count != 1) {
-			print = 1;
-			goto again;
-		}
-	}
-	count = 0;
-	if (mflag) {
-		lookman(cp);
-		if (uflag && print == 0 && count != 1) {
-			print = 1;
-			goto again;
-		}
-	}
-	if (print)
-		printf("\n");
 }
 
-looksrc(cp)
-	char *cp;
-{
-	if (Sflag == 0) {
-		find(srcdirs, cp);
-	} else
-		findv(Sflag, Scnt, cp);
-}
-
-lookbin(cp)
-	char *cp;
-{
-	if (Bflag == 0)
-		find(bindirs, cp);
-	else
-		findv(Bflag, Bcnt, cp);
-}
-
-lookman(cp)
-	char *cp;
-{
-	if (Mflag == 0) {
-		find(mandirs, cp);
-	} else
-		findv(Mflag, Mcnt, cp);
-}
-
-findv(dirv, dirc, cp)
-	char **dirv;
-	int dirc;
-	char *cp;
+void
+usage()
 {
 
-	while (dirc > 0)
-		findin(*dirv++, cp), dirc--;
-}
-
-find(dirs, cp)
-	char **dirs;
-	char *cp;
-{
-
-	while (*dirs)
-		findin(*dirs++, cp);
-}
-
-findin(dir, cp)
-	char *dir, *cp;
-{
-	DIR *dirp;
-	struct dirent *dp;
-
-	dirp = opendir(dir);
-	if (dirp == NULL)
-		return;
-	while ((dp = readdir(dirp)) != NULL) {
-		if (itsit(cp, dp->d_name)) {
-			count++;
-			if (print)
-				printf(" %s/%s", dir, dp->d_name);
-		}
-	}
-	closedir(dirp);
-}
-
-itsit(cp, dp)
-	register char *cp, *dp;
-{
-	register int i = strlen(dp);
-
-	if (dp[0] == 's' && dp[1] == '.' && itsit(cp, dp+2))
-		return (1);
-	while (*cp && *dp && *cp == *dp)
-		cp++, dp++, i--;
-	if (*cp == 0 && *dp == 0)
-		return (1);
-	while (isdigit(*dp))
-		dp++;
-	if (*cp == 0 && *dp++ == '.') {
-		--i;
-		while (i > 0 && *dp)
-			if (--i, *dp++ == '.')
-				return (*dp++ == 'C' && *dp++ == 0);
-		return (1);
-	}
-	return (0);
+	(void)fprintf(stderr, "usage: whereis program [...]\n");
+	exit (1);
 }
