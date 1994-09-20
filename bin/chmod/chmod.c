233527ec61283d751c6c0bf84b65926e4abb390e
mycroft <mycroft>
1994-09-20 04:07:01 +0000

    Merge with 4.4-Lite.

--- bin/chmod/chmod.c
+++ bin/chmod/chmod.c
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1989 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1989, 1993, 1994
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -32,29 +32,27 @@
  */
 
 #ifndef lint
-char copyright[] =
-"@(#) Copyright (c) 1989 The Regents of the University of California.\n\
- All rights reserved.\n";
+static char copyright[] =
+"@(#) Copyright (c) 1989, 1993, 1994\n\
+	The Regents of the University of California.  All rights reserved.\n";
 #endif /* not lint */
 
 #ifndef lint
-/*static char sccsid[] = "from: @(#)chmod.c	5.21 (Berkeley) 1/27/92";*/
-static char rcsid[] = "$Id: chmod.c,v 1.8 1994/02/11 02:57:24 cgd Exp $";
+/*static char sccsid[] = "from: @(#)chmod.c	8.8 (Berkeley) 4/1/94";*/
+static char *rcsid = "$Id: chmod.c,v 1.9 1994/09/20 04:07:04 mycroft Exp $";
 #endif /* not lint */
 
 #include <sys/types.h>
 #include <sys/stat.h>
+
+#include <err.h>
 #include <errno.h>
 #include <fts.h>
-#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
-int retval;
-
-void err __P((const char *, ...));
-void error __P((char *));
 void usage __P((void));
 
 int
@@ -62,33 +60,57 @@ main(argc, argv)
 	int argc;
 	char *argv[];
 {
-	register FTS *fts;
-	register FTSENT *p;
-	register int oct, omode;
-	struct stat sb;
+	FTS *ftsp;
+	FTSENT *p;
 	mode_t *set;
-	int ch, fflag, rflag;
+	long val;
+	int oct, omode;
+	int Hflag, Lflag, Pflag, Rflag, ch, fflag, fts_options, hflag, rval;
 	char *ep, *mode;
 
-	fflag = rflag = 0;
-	while ((ch = getopt(argc, argv, "Rfrwx")) != EOF)
-		switch((char)ch) {
-		case 'R':
-			rflag = 1;
+	Hflag = Lflag = Pflag = Rflag = fflag = hflag = 0;
+	while ((ch = getopt(argc, argv, "HLPRXfgorstuwx")) != -1)
+		switch (ch) {
+		case 'H':
+			Hflag = 1;
+			Lflag = Pflag = 0;
 			break;
-		case 'f':		/* no longer documented */
+		case 'L':
+			Lflag = 1;
+			Hflag = Pflag = 0;
+			break;
+		case 'P':
+			Pflag = 1;
+			Hflag = Lflag = 0;
+			break;
+		case 'R':
+			Rflag = 1;
+			break;
+		case 'f':		/* XXX: undocumented. */
 			fflag = 1;
 			break;
-		case 'r':		/* "-[rwx]" are valid file modes */
-		case 'w':
-		case 'x':
-		case 'u':		/* as are -[ugo] */
-		case 'g':
-		case 'o':
-		case 'X':		/* -[Xts] valid but ignored later */
-		case 't':
-		case 's':
-			--optind;
+		case 'h':
+			/*
+			 * In System V (and probably POSIX.2) the -h option
+			 * causes chmod to change the mode of the symbolic
+			 * link.  4.4BSD's symbolic links don't have modes,
+			 * so it's an undocumented noop.  Do syntax checking,
+			 * though.
+			 */
+			hflag = 1;
+			break;
+		/*
+		 * XXX
+		 * "-[rwx]" are valid mode commands.  If they are the entire
+		 * argument, getopt has moved past them, so decrement optind.
+		 * Regardless, we're done argument processing.
+		 */
+		case 'g': case 'o': case 'r': case 's':
+		case 't': case 'u': case 'w': case 'X': case 'x':
+			if (argv[optind - 1][0] == '-' &&
+			    argv[optind - 1][1] == ch &&
+			    argv[optind - 1][2] == '\0')
+				--optind;
 			goto done;
 		case '?':
 		default:
@@ -100,95 +122,81 @@ done:	argv += optind;
 	if (argc < 2)
 		usage();
 
+	fts_options = FTS_PHYSICAL;
+	if (Rflag) {
+		if (hflag)
+			errx(1,
+		"the -R and -h options may not be specified together.");
+		if (Hflag)
+			fts_options |= FTS_COMFOLLOW;
+		if (Lflag) {
+			fts_options &= ~FTS_PHYSICAL;
+			fts_options |= FTS_LOGICAL;
+		}
+	}
+
 	mode = *argv;
 	if (*mode >= '0' && *mode <= '7') {
-		omode = (int)strtol(mode, &ep, 8);
-		if (omode < 0 || *ep)
-			err("invalid file mode: %s", mode);
+		errno = 0;
+		val = strtol(mode, &ep, 8);
+		if (val > INT_MAX || val < 0)
+			errno = ERANGE;
+		if (errno)
+			err(1, "invalid file mode: %s", mode);
+		if (*ep)
+			errx(1, "invalid file mode: %s", mode);
+		omode = val;
 		oct = 1;
 	} else {
-		if (!(set = setmode(mode)))
-			err("invalid file mode: %s", mode);
+		if ((set = setmode(mode)) == NULL)
+			errx(1, "invalid file mode: %s", mode);
 		oct = 0;
 	}
 
-	retval = 0;
-	if (rflag) {
-		if ((fts = fts_open(++argv, FTS_PHYSICAL, 0)) == NULL)
-			err("%s", strerror(errno));
-		while (p = fts_read(fts))
-			switch(p->fts_info) {
-			case FTS_D:
-			case FTS_SL:
-				break;
-			case FTS_DNR:
-			case FTS_ERR:
-			case FTS_NS:
-				err("%s: %s", p->fts_path, strerror(errno));
-			default:
-				if (chmod(p->fts_accpath, oct ? omode :
-				    getmode(set, p->fts_statp->st_mode)) &&
-				    !fflag)
-					error(p->fts_path);
-				break;
-			}
-		exit(retval);
-	}
-	while (*++argv) {
-		if (lstat(*argv, &sb) < 0) {
-			if (!fflag)
-				error(*argv);
+	if ((ftsp = fts_open(++argv, fts_options, 0)) == NULL)
+		err(1, NULL);
+	for (rval = 0; (p = fts_read(ftsp)) != NULL;) {
+		switch (p->fts_info) {
+		case FTS_D:
+			if (Rflag)		/* Change it at FTS_DP. */
+				continue;
+			fts_set(ftsp, p, FTS_SKIP);
+			break;
+		case FTS_DNR:			/* Warn, chmod, continue. */
+			warnx("%s: %s", p->fts_path, strerror(p->fts_errno));
+			rval = 1;
+			break;
+		case FTS_ERR:			/* Warn, continue. */
+		case FTS_NS:
+			warnx("%s: %s", p->fts_path, strerror(p->fts_errno));
+			rval = 1;
 			continue;
+		case FTS_SL:			/* Ignore. */
+		case FTS_SLNONE:
+			/*
+			 * The only symlinks that end up here are ones that
+			 * don't point to anything and ones that we found
+			 * doing a physical walk.
+			 */
+			continue;
+		default:
+			break;
 		}
-		if (S_ISLNK(sb.st_mode))
-			continue;
-		if (chmod(*argv, oct ? omode : getmode(set, sb.st_mode)) &&
-		    !fflag)
-			error(*argv);
+		if (chmod(p->fts_accpath, oct ? omode :
+		    getmode(set, p->fts_statp->st_mode)) && !fflag) {
+			warn(p->fts_path);
+			rval = 1;
+		}
 	}
-	exit(retval);
-}
-
-void
-error(name)
-	char *name;
-{
-	(void)fprintf(stderr, "chmod: %s: %s\n", name, strerror(errno));
-	retval = 1;
+	if (errno)
+		err(1, "fts_read");
+	exit(rval);
 }
 
 void
 usage()
 {
-	(void)fprintf(stderr, "usage: chmod [-R] mode file ...\n");
+	(void)fprintf(stderr,
+	    "usage: chmod [-R [-H | -L | -P]] mode file ...\n");
 	exit(1);
-}
-
-#if __STDC__
-#include <stdarg.h>
-#else
-#include <varargs.h>
-#endif
-
-void
-#if __STDC__
-err(const char *fmt, ...)
-#else
-err(fmt, va_alist)
-	char *fmt;
-        va_dcl
-#endif
-{
-	va_list ap;
-#if __STDC__
-	va_start(ap, fmt);
-#else
-	va_start(ap);
-#endif
-	(void)fprintf(stderr, "chmod: ");
-	(void)vfprintf(stderr, fmt, ap);
-	va_end(ap);
-	(void)fprintf(stderr, "\n");
-	exit(1);
-	/* NOTREACHED */
 }
