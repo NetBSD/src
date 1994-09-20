bc6377d61a8336b53024997cdd2d4476e8e8eb48
mycroft <mycroft>
1994-09-20 04:52:02 +0000

    Merge with 4.4-Lite version.

--- bin/stty/gfmt.c
+++ bin/stty/gfmt.c
@@ -1,6 +1,6 @@
 /*-
- * Copyright (c) 1991 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1991, 1993, 1994
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -32,18 +32,28 @@
  */
 
 #ifndef lint
-/*static char sccsid[] = "from: @(#)gfmt.c	5.4 (Berkeley) 6/10/91";*/
-static char rcsid[] = "$Id: gfmt.c,v 1.7 1994/03/23 05:05:30 mycroft Exp $";
+/*static char sccsid[] = "from: @(#)gfmt.c	8.6 (Berkeley) 4/2/94";*/
+static char *rcsid = "$Id: gfmt.c,v 1.8 1994/09/20 04:52:05 mycroft Exp $";
 #endif /* not lint */
 
 #include <sys/types.h>
+
 #include <err.h>
 #include <stdio.h>
 #include <string.h>
+
 #include "stty.h"
 #include "extern.h"
 
-static void gerr __P((char *));
+static void
+gerr(s)
+	char *s;
+{
+	if (s)
+		errx(1, "illegal gfmt1 option -- %s", s);
+	else
+		errx(1, "illegal gfmt1 option");
+}
 
 void
 gprint(tp, wp, ldisc)
@@ -51,7 +61,7 @@ gprint(tp, wp, ldisc)
 	struct winsize *wp;
 	int ldisc;
 {
-	register struct cchar *cp;
+	struct cchar *cp;
 
 	(void)printf("gfmt1:cflag=%x:iflag=%x:lflag=%x:oflag=%x:",
 	    tp->c_cflag, tp->c_iflag, tp->c_lflag, tp->c_oflag);
@@ -62,24 +72,25 @@ gprint(tp, wp, ldisc)
 
 void
 gread(tp, s) 
-	register struct termios *tp;
+	struct termios *tp;
 	char *s;
 {
-	register char *ep, *p;
-	register struct cchar *cp;
+	struct cchar *cp;
+	char *ep, *p;
 	long tmp;
 
-#define	CHK(s)	(*p == s[0] && !strcmp(p, s))
-	if (!(s = index(s, ':')))
+	if ((s = strchr(s, ':')) == NULL)
 		gerr(NULL);
-	for (++s; s;) {
+	for (++s; s != NULL;) {
 		p = strsep(&s, ":\0");
 		if (!p || !*p)
 			break;
-		if (!(ep = index(p, '=')))
+		if (!(ep = strchr(p, '=')))
 			gerr(p);
 		*ep++ = '\0';
 		(void)sscanf(ep, "%lx", &tmp);
+
+#define	CHK(s)	(*p == s[0] && !strcmp(p, s))
 		if (CHK("cflag")) {
 			tp->c_cflag = tmp;
 			continue;
@@ -106,25 +117,14 @@ gread(tp, s)
 			tp->c_ospeed = tmp;
 			continue;
 		}
-		for (cp = cchars1; cp->name; ++cp) {
+		for (cp = cchars1; cp->name != NULL; ++cp)
 			if (CHK(cp->name)) {
 				if (cp->sub == VMIN || cp->sub == VTIME)
 					(void)sscanf(ep, "%ld", &tmp);
 				tp->c_cc[cp->sub] = tmp;
 				break;
 			}
-		}
-		if (!cp->name)
+		if (cp->name == NULL)
 			gerr(p);
 	}
-}
-
-static void
-gerr(s)
-	char *s;
-{
-	if (s)
-		errx(1, "illegal gfmt1 option -- %s", s);
-	else
-		errx(1, "illegal gfmt1 option");
 }
