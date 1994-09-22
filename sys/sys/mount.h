4789b488e0019229457afcafb586503206e0cf1b
mycroft <mycroft@NetBSD.org>
1994-09-22 02:06:10 +0000

    Add a reference count for each VFS.

--- sys/sys/mount.h
+++ sys/sys/mount.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: mount.h,v 1.33 1994/06/29 06:44:41 cgd Exp $	*/
+/*	$NetBSD: mount.h,v 1.34 1994/09/22 02:06:10 mycroft Exp $	*/
 
 /*
  * Copyright (c) 1989, 1991, 1993
@@ -200,6 +200,7 @@ struct vfsops {
 				    int *exflagsp, struct ucred **credanonp));
 	int	(*vfs_vptofh)	__P((struct vnode *vp, struct fid *fhp));
 	int	(*vfs_init)	__P((void));
+	int	vfs_refcount;
 };
 
 #define VFS_MOUNT(MP, PATH, DATA, NDP, P) \
