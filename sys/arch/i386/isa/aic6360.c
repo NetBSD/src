1446daf61014b7ff736a0ecde2e7fa1efaf416bf
cgd <cgd@NetBSD.org>
1994-10-17 00:09:10 +0000

    from trunk

--- sys/arch/i386/isa/aic6360.c
+++ sys/arch/i386/isa/aic6360.c
@@ -11,10 +11,7 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed by Jarle Greipsland
- * 4. The name of the author may not be used to endorse or promote products 
+ * 3. The name of the author may not be used to endorse or promote products 
  *    derived from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
@@ -31,7 +28,7 @@
  */
 
 /*
- * $Id: aic6360.c,v 1.5.2.2 1994/08/08 07:09:56 mycroft Exp $
+ * $Id: aic6360.c,v 1.5.2.3 1994/10/17 00:09:10 cgd Exp $
  *
  * Acknowledgements: Many of the algorithms used in this driver are
  * inspired by the work of Julian Elischer (julian@tfs.com) and
