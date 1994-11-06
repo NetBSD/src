1ca8f508a686a20f1d4ad273f27cf2fd29e95a20
cgd <cgd>
1994-11-06 23:22:08 +0000

    this file was corrupted in the tar files.  make the sup'd source consistent.

--- sys/arch/i386/isa/pcvt/pcvt_vtf.c
+++ sys/arch/i386/isa/pcvt/pcvt_vtf.c
@@ -864,8 +864,7 @@ vt_designate(struct video_state *svsp)
 #endif			
 				break;
 				
-			case 'Q': /* French Canadien */
-#ifdef HAVECSE_FRENCHCANADA
+			case 'Q': /* French CC Supplemental Gf HAVECSE_FRENCHCANADA
 				ctp = cse_frenchcanada;
 #endif			
 				break;
