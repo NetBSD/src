0a732ed30e1783835d054d50d98cf5dd918eb4a1
jtc <jtc>
1994-11-14 05:03:06 +0000

    imported from 4.4lite

--- usr.bin/whois/whois.c
+++ usr.bin/whois/whois.c
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1980 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1980, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -32,13 +32,13 @@
  */
 
 #ifndef lint
-char copyright[] =
-"@(#) Copyright (c) 1980 Regents of the University of California.\n\
- All rights reserved.\n";
+static char copyright[] =
+"@(#) Copyright (c) 1980, 1993\n\
+	The Regents of the University of California.  All rights reserved.\n";
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)whois.c	5.11 (Berkeley) 3/2/91";
+static char sccsid[] = "@(#)whois.c	8.1 (Berkeley) 6/6/93";
 #endif /* not lint */
 
 #include <sys/types.h>
@@ -47,7 +47,7 @@ static char sccsid[] = "@(#)whois.c	5.11 (Berkeley) 3/2/91";
 #include <netdb.h>
 #include <stdio.h>
 
-#define	NICHOST	"nic.ddn.mil"
+#define	NICHOST	"whois.internic.net"
 
 main(argc, argv)
 	int argc;
