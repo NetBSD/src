f9db13b4903ea19e1611d5d343379f0c1adb67f9
jtc <jtc@NetBSD.org>
1994-11-14 04:56:13 +0000

    Merged with 4.4 lite. Changed to conform to NetBSD's new RCS ID convention.

--- usr.bin/yes/yes.c
+++ usr.bin/yes/yes.c
@@ -1,6 +1,8 @@
+/*	$NetBSD: yes.c,v 1.3 1994/11/14 04:56:15 jtc Exp $	*/
+
 /*
- * Copyright (c) 1987 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1987, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -32,14 +34,16 @@
  */
 
 #ifndef lint
-char copyright[] =
-"@(#) Copyright (c) 1987 Regents of the University of California.\n\
- All rights reserved.\n";
+static char copyright[] =
+"@(#) Copyright (c) 1987, 1993\n\
+	The Regents of the University of California.  All rights reserved.\n";
 #endif /* not lint */
 
 #ifndef lint
-/*static char sccsid[] = "from: @(#)yes.c	5.4 (Berkeley) 6/1/90";*/
-static char rcsid[] = "$Id: yes.c,v 1.2 1993/08/01 18:00:33 mycroft Exp $";
+#if 0
+static char sccsid[] = "@(#)yes.c	8.1 (Berkeley) 6/6/93";
+#endif
+static char rcsid[] = "$NetBSD: yes.c,v 1.3 1994/11/14 04:56:15 jtc Exp $";
 #endif /* not lint */
 
 main(argc, argv)
