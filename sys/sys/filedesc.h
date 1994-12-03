3b9b2cf37819f84a8dc960846fa121fd53ff95f5
mycroft <mycroft@NetBSD.org>
1994-12-03 15:45:10 +0000

    Change fd_lastfile and fd_freefile to ints.

--- sys/sys/filedesc.h
+++ sys/sys/filedesc.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: filedesc.h,v 1.9 1994/06/29 06:44:12 cgd Exp $	*/
+/*	$NetBSD: filedesc.h,v 1.10 1994/12/03 15:45:10 mycroft Exp $	*/
 
 /*
  * Copyright (c) 1990, 1993
@@ -58,8 +58,8 @@ struct filedesc {
 	struct	vnode *fd_cdir;		/* current directory */
 	struct	vnode *fd_rdir;		/* root directory */
 	int	fd_nfiles;		/* number of open files allocated */
-	u_short	fd_lastfile;		/* high-water mark of fd_ofiles */
-	u_short	fd_freefile;		/* approx. next free file */
+	int	fd_lastfile;		/* high-water mark of fd_ofiles */
+	int	fd_freefile;		/* approx. next free file */
 	u_short	fd_cmask;		/* mask for file creation */
 	u_short	fd_refcnt;		/* reference count */
 };
