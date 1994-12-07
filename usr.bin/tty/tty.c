34b9f978656ed3fdd9d24b6afe613cbf90c71ff7
jtc <jtc>
1994-12-07 00:46:54 +0000

    Merged with 4.4lite. Changed to conform to NetBSD's new RCS Id convention.

--- usr.bin/tty/tty.c
+++ usr.bin/tty/tty.c
@@ -1,6 +1,8 @@
+/*	$NetBSD: tty.c,v 1.4 1994/12/07 00:46:57 jtc Exp $	*/
+
 /*
- * Copyright (c) 1988 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1988, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -32,14 +34,16 @@
  */
 
 #ifndef lint
-char copyright[] =
-"@(#) Copyright (c) 1988 Regents of the University of California.\n\
- All rights reserved.\n";
+static char copyright[] =
+"@(#) Copyright (c) 1988, 1993\n\
+	The Regents of the University of California.  All rights reserved.\n";
 #endif /* not lint */
 
 #ifndef lint
-/*static char sccsid[] = "from: @(#)tty.c	4.4 (Berkeley) 6/1/90";*/
-static char rcsid[] = "$Id: tty.c,v 1.3 1993/09/20 23:53:24 jtc Exp $";
+#if 0
+static char sccsid[] = "@(#)tty.c	8.1 (Berkeley) 6/6/93";
+#endif
+static char rcsid[] = "$NetBSD: tty.c,v 1.4 1994/12/07 00:46:57 jtc Exp $";
 #endif /* not lint */
 
 #include <stdio.h>
