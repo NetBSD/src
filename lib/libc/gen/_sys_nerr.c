3c98cdcd84ab3cff8469e6d60b750e61885677f1
jtc <jtc@NetBSD.org>
1994-12-12 22:41:50 +0000

    Rework indirect reference support as outlined by my recent message to the tech-userlevel mailing list.

---
+++ lib/libc/gen/_sys_nerr.c
@@ -0,0 +1,8 @@
+/*
+ * Written by J.T. Conklin, December 12, 1994
+ * Public domain.
+ */
+
+#include <sys/cdefs.h>
+
+__weak_reference(_sys_nerr, sys_nerr);
--- lib/libc/gen/initgroups.c
+++ lib/libc/gen/initgroups.c
@@ -33,7 +33,7 @@
 
 #if defined(LIBC_SCCS) && !defined(lint)
 /*static char sccsid[] = "from: @(#)initgroups.c	8.1 (Berkeley) 6/4/93";*/
-static char *rcsid = "$Id: initgroups.c,v 1.6 1994/12/11 20:43:59 christos Exp $";
+static char *rcsid = "$Id: initgroups.c,v 1.7 1994/12/12 22:42:11 jtc Exp $";
 #endif /* LIBC_SCCS and not lint */
 
 #include <sys/param.h>
@@ -49,10 +49,10 @@ initgroups(uname, agroup)
 
 	ngroups = NGROUPS;
 	if (getgrouplist(uname, agroup, groups, &ngroups) < 0)
-		__warnx("%s is in too many groups, using first %d",
+		_warnx("%s is in too many groups, using first %d",
 		    uname, ngroups);
 	if (setgroups(ngroups, groups) < 0) {
-		__warn("setgroups");
+		_warn("setgroups");
 		return (-1);
 	}
 	return (0);
