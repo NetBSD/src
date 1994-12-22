db802bff24ec913c85484443676acc0dbd7a1a2b
jtc <jtc@NetBSD.org>
1994-12-22 21:08:34 +0000

    imported from 44lite

--- usr.bin/lorder/lorder.sh
+++ usr.bin/lorder/lorder.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -
 #
-# Copyright (c) 1990 The Regents of the University of California.
-# All rights reserved.
+# Copyright (c) 1990, 1993
+#	The Regents of the University of California.  All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
@@ -31,7 +31,7 @@
 # OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 # SUCH DAMAGE.
 #
-#	@(#)lorder.sh	5.3 (Berkeley) 4/12/91
+#	@(#)lorder.sh	8.1 (Berkeley) 6/6/93
 #
 
 PATH=/bin:/usr/bin
@@ -70,12 +70,12 @@ nm -go $* | sed "
 		d
 	}
 	/ [TD] / {
-		s/:.* [TD]//
+		s/:.* [TD] / /
 		w $S
 		d
 	}
 	/ U / {
-		s/:.* U//
+		s/:.* U / /
 		w $R
 	}
 	d
