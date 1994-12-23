f9e84509ed78f349411f440103c08687fdafbfdb
jtc <jtc@NetBSD.org>
1994-12-23 06:52:56 +0000

    Merged with 4.4lite. Changed to conform to NetBSD's new RCS Id convention.

--- usr.bin/login/pathnames.h
+++ usr.bin/login/pathnames.h
@@ -1,3 +1,5 @@
+/*	$NetBSD: pathnames.h,v 1.4 1994/12/23 06:53:03 jtc Exp $	*/
+
 /*-
  * Copyright (c) 1989, 1993
  *	The Regents of the University of California.  All rights reserved.
@@ -30,8 +32,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	from: @(#)pathnames.h	8.1 (Berkeley) 6/9/93
- *	$Id $
+ *	@(#)pathnames.h	8.1 (Berkeley) 6/9/93
  */
 
 #include <paths.h>
