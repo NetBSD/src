3925d445a07e800f0c4dba6ac272c53bfcb8e48b
cgd <cgd@NetBSD.org>
1994-12-23 13:01:30 +0000

    more than i thought...

--- lib/libc/nls/catgets.c
+++ lib/libc/nls/catgets.c
@@ -11,14 +11,14 @@ __weak_reference(_catgets,catgets);
 
 #include <nl_types.h>
 
-extern char * _catgets __P((nl_catd, int, int, __const char *));
+extern char * _catgets __P((nl_catd, int, int, char *));
 
 char *
 catgets(catd, set_id, msg_id, s)
 	nl_catd catd;
 	int set_id;
 	int msg_id;
-	__const char *s;
+	char *s;
 {
 	return _catgets(catd, set_id, msg_id, s);
 }
