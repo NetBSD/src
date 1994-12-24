30bbc63b7e453fb664b2e0141144e40e7a9d0e20
cgd <cgd@NetBSD.org>
1994-12-24 17:08:05 +0000

    pull in string.h

--- usr.bin/unexpand/unexpand.c
+++ usr.bin/unexpand/unexpand.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: unexpand.c,v 1.4 1994/12/20 01:30:26 jtc Exp $	*/
+/*	$NetBSD: unexpand.c,v 1.5 1994/12/24 17:08:05 cgd Exp $	*/
 
 /*-
  * Copyright (c) 1980, 1993
@@ -43,13 +43,14 @@ static char copyright[] =
 #if 0
 static char sccsid[] = "@(#)unexpand.c	8.1 (Berkeley) 6/6/93";
 #endif
-static char rcsid[] = "$NetBSD: unexpand.c,v 1.4 1994/12/20 01:30:26 jtc Exp $";
+static char rcsid[] = "$NetBSD: unexpand.c,v 1.5 1994/12/24 17:08:05 cgd Exp $";
 #endif /* not lint */
 
 /*
  * unexpand - put tabs into a file replacing blanks
  */
 #include <stdio.h>
+#include <string.h>
 
 char	genbuf[BUFSIZ];
 char	linebuf[BUFSIZ];
