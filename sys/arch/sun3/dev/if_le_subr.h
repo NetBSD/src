e666fbb7afeed48ad2b378a955cc532416594590
gwr <gwr@NetBSD.org>
1995-01-03 15:43:36 +0000

    New, improved "le" driver from Charles Hannum and Paul Richards.

--- sys/arch/sun3/dev/if_le_subr.h
+++ sys/arch/sun3/dev/if_le_subr.h
@@ -1,8 +1,8 @@
-/*	$NetBSD: if_le_subr.h,v 1.6 1994/12/12 18:59:16 gwr Exp $	*/
+/*	$NetBSD: if_le_subr.h,v 1.7 1995/01/03 15:43:40 gwr Exp $	*/
 
-/* One might also set: LE_C3_ACON | LE_C3_BCON */
-#define	LE_C3_CONFIG LE_C3_BSWP
+/* One might also set: LE_ACON | LE_BCON */
+#define	LE_CONF3 (LE_BSWP)
 
 extern int  le_md_match(struct device *, void *, void *args);
 extern void le_md_attach(struct device *, struct device *, void *);
-extern int  le_intr(void *);
+extern int  leintr(void *);
