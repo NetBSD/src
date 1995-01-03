9e499d41b3f99bcf73ed2f7258689c1d6762355e
mycroft <mycroft@NetBSD.org>
1995-01-03 22:38:54 +0000

    \ These files have been 99% rewritten from the original ast driver.  Add an appropriate copyright notice.

--- sys/arch/i386/isa/boca.c
+++ sys/arch/i386/isa/boca.c
@@ -1,15 +1,35 @@
-/*	$NetBSD: boca.c,v 1.1 1995/01/03 22:30:26 mycroft Exp $	*/
+/*	$NetBSD: boca.c,v 1.2 1995/01/03 22:38:55 mycroft Exp $	*/
 
 /*
- * Multi-port serial card interrupt demuxing support.
- * Roland McGrath 3/20/94
- * The author disclaims copyright and places this file in the public domain.
+ * Copyright (c) 1995 Charles Hannum.  All rights reserved.
  *
- * Modified by: Charles Hannum, 3/22/94
+ * This code is derived from public-domain software written by
+ * Roland McGrath, and information provided by David Muir Sharnoff.
  *
- * The programming details of the BOCA board (i.e. how the interrupt register
- * works) were determined by David Muir Sharnoff.  BOCA Research refused to
- * provide any programming information.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed by Charles Hannum.
+ * 4. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
 #include <sys/param.h>
