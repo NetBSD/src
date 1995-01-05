fd1ee902b0316fe082231b7c7246176b57789b01
jtc <jtc@NetBSD.org>
1995-01-05 19:07:23 +0000

    Import January 4, 1995 version of dtoa from netlib.  No need to merge, since the only change was the result of my CONST change being adopted in the master sources.

--- lib/libc/stdlib/strtod.c
+++ lib/libc/stdlib/strtod.c
@@ -1152,7 +1152,7 @@ ratio
 	return da / db;
 	}
 
- static double
+ static CONST double
 tens[] = {
 		1e0, 1e1, 1e2, 1e3, 1e4, 1e5, 1e6, 1e7, 1e8, 1e9,
 		1e10, 1e11, 1e12, 1e13, 1e14, 1e15, 1e16, 1e17, 1e18, 1e19,
@@ -1162,19 +1162,19 @@ tens[] = {
 #endif
 		};
 
- static double
+ static CONST double
 #ifdef IEEE_Arith
 bigtens[] = { 1e16, 1e32, 1e64, 1e128, 1e256 };
-static double tinytens[] = { 1e-16, 1e-32, 1e-64, 1e-128, 1e-256 };
+static CONST double tinytens[] = { 1e-16, 1e-32, 1e-64, 1e-128, 1e-256 };
 #define n_bigtens 5
 #else
 #ifdef IBM
 bigtens[] = { 1e16, 1e32, 1e64 };
-static double tinytens[] = { 1e-16, 1e-32, 1e-64 };
+static CONST double tinytens[] = { 1e-16, 1e-32, 1e-64 };
 #define n_bigtens 3
 #else
 bigtens[] = { 1e16, 1e32 };
-static double tinytens[] = { 1e-16, 1e-32 };
+static CONST double tinytens[] = { 1e-16, 1e-32 };
 #define n_bigtens 2
 #endif
 #endif
