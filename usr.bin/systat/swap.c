3742eda2996208560a1c2e9000c9d88a9931f1e9
jtc <jtc>
1995-01-20 08:51:48 +0000

    Changed to conform to NetBSD's new RCS Id convention.

--- usr.bin/systat/swap.c
+++ usr.bin/systat/swap.c
@@ -1,3 +1,5 @@
+/*	$NetBSD: swap.c,v 1.2 1995/01/20 08:52:11 jtc Exp $	*/
+
 /*-
  * Copyright (c) 1980, 1992, 1993
  *	The Regents of the University of California.  All rights reserved.
@@ -32,7 +34,10 @@
  */
 
 #ifndef lint
+#if 0
 static char sccsid[] = "@(#)swap.c	8.2 (Berkeley) 2/21/94";
+#endif
+static char rcsid[] = "$NetBSD: swap.c,v 1.2 1995/01/20 08:52:11 jtc Exp $";
 #endif /* not lint */
 
 /*
