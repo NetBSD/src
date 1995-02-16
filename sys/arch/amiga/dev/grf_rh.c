aa25c80755c429be8c8aabc9b79d5fa52efc2112
chopps <chopps@NetBSD.org>
1995-02-16 21:57:31 +0000

    cleanup copyright stuff

--- sys/arch/amiga/dev/grf_rh.c
+++ sys/arch/amiga/dev/grf_rh.c
@@ -1,5 +1,35 @@
-/*	$NetBSD: grf_rh.c,v 1.5 1994/12/28 09:25:09 chopps Exp $	*/
+/*	$NetBSD: grf_rh.c,v 1.6 1995/02/16 21:57:36 chopps Exp $	*/
 
+/*
+ * Copyright (c) 1994 Markus Wild
+ * Copyright (c) 1994 Lutz Vieweg
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *      This product includes software developed by Lutz Vieweg.
+ * 4. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 #include "grfrh.h"
 #if NGRFRH > 0
 
@@ -37,30 +67,22 @@ extern unsigned char kernel_font_8x11_lo, kernel_font_8x11_hi;
 extern unsigned char kernel_font_8x11[];
 #endif
 
-
 /*
- * NOTE: this driver for the MacroSystem Retina board was only possible,
- *       because MacroSystem provided information about the pecularities
- *       of the board. THANKS! Competition in Europe among gfx board
- *       manufacturers is rather tough, so Lutz Vieweg, who wrote the
- *       initial driver, has made an agreement with MS not to document
- *       the driver source (see also his Copyright disclaimer below).
- *       -> ALL comments after
- *       -> "/* -------------- START OF CODE -------------- * /"
- *       -> have been added by myself (mw) from studying the publically
- *       -> available "NCR 77C32BLT" Data Manual
- *
- *       Lutz' original driver source (without any of my comments) is
- *       available on request.
+ * This driver for the MacroSystem Retina board was only possible,
+ * because MacroSystem provided information about the pecularities
+ * of the board. THANKS! Competition in Europe among gfx board
+ * manufacturers is rather tough, so Lutz Vieweg, who wrote the
+ * initial driver, has made an agreement with MS not to document
+ * the driver source (see also his comment below).
+ * -> ALL comments after
+ * -> "/* -------------- START OF CODE -------------- * /"
+ * -> have been added by myself (mw) from studying the publically
+ * -> available "NCR 77C32BLT" Data Manual
  */
-
-/* This code offers low-level routines to access the Retina BLT Z3
+/*
+ * This code offers low-level routines to access the Retina BLT Z3
  * graphics-board manufactured by MS MacroSystem GmbH from within NetBSD
- * for the Amiga.  * No warranties for any kind of function at all - this
- * code may crash your hardware and scratch your harddisk.  Use at your
- * own risk.  Freely distributable.
- *
- * Written by Lutz Vieweg 02/94
+ * for the Amiga.
  *
  * Thanks to MacroSystem for providing me with the neccessary information
  * to create theese routines. The sparse documentation of this code
--- sys/arch/amiga/dev/grf_rhreg.h
+++ sys/arch/amiga/dev/grf_rhreg.h
@@ -1,15 +1,37 @@
-/*	$NetBSD: grf_rhreg.h,v 1.3 1994/12/01 17:25:07 chopps Exp $	*/
-
-#ifndef _GRF_RHREG_H
-#define _GRF_RHREG_H
+/*	$NetBSD: grf_rhreg.h,v 1.4 1995/02/16 21:57:38 chopps Exp $	*/
 
 /*
- * Written & Copyright by Lutz Vieweg. As for missing comments: see 
- * grf_rt.*
+ * Copyright (c) 1994 Markus Wild
+ * Copyright (c) 1994 Lutz Vieweg
+ * All rights reserved.
  *
- * Lutz provided these sources in C++, I tried to keep as much of it when
- * converting to C. -mw-
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *      This product includes software developed by Lutz Vieweg.
+ * 4. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+#ifndef _GRF_RHREG_H
+#define _GRF_RHREG_H
 
 #define EMPTY_ALPHA 0x2010 /* this is the char and the attribute
                               that AlphaErase will fill into the
