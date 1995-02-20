480b98e100a4d08d94e3f40c0c16a99fa13f9af7
mycroft <mycroft@NetBSD.org>
1995-02-20 00:12:09 +0000

    Update to the new RCS id convention.

--- sys/arch/hp300/stand/if_le.c
+++ sys/arch/hp300/stand/if_le.c
@@ -1,3 +1,5 @@
+/*	$NetBSD: if_le.c,v 1.3 1995/02/20 00:12:11 mycroft Exp $	*/
+
 /*
  * Copyright (c) 1993 Adam Glass
  * All rights reserved.
@@ -27,8 +29,6 @@
  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
- *
- * $Header: /cvsroot/src/sys/arch/hp300/stand/Attic/if_le.c,v 1.2 1995/02/19 23:54:18 mycroft Exp $
  */
 
 #include <sys/param.h>
