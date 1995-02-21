4ddd4e966debcc585fe04b096851bf5aeb291882
mycroft <mycroft>
1995-02-21 09:06:13 +0000

    Use the libsa exec().

--- sys/arch/hp300/stand/boot.c
+++ sys/arch/hp300/stand/boot.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: boot.c,v 1.5 1995/02/21 06:39:01 mycroft Exp $	*/
+/*	$NetBSD: boot.c,v 1.6 1995/02/21 09:06:13 mycroft Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1990, 1993
@@ -40,11 +40,6 @@
 #include <a.out.h>
 #include "stand.h"
 
-#ifndef INSECURE
-#include <sys/stat.h>
-struct stat sb;
-#endif
-
 /*
  * Boot program... bits in `howto' determine whether boot stops to
  * ask for system name.	 Boot device is derived from ROM provided
@@ -52,16 +47,15 @@ struct stat sb;
  */
 
 char line[100];
-char *ssym, *esym;
 
-extern	unsigned opendev;
+extern	u_int opendev;
 extern	char *lowram;
 extern	int noconsole;
 extern	int howto, bootdev;
 
 main()
 {
-	int io, retry, type;
+	int retry, type;
 
 	printf("%s CPU\nBoot\n", getmachineid());
 #ifdef JUSTASK
@@ -85,97 +79,23 @@ main()
 			}
 		} else
 			printf(": %s\n", line);
-		io = open(line, 0);
-		if (io >= 0) {
-#ifndef INSECURE
-			(void) fstat(io, &sb);
-			if (sb.st_uid || (sb.st_mode & 2)) {
-				printf("non-secure file, will not load\n");
-				close(io);
-				howto = RB_SINGLE|RB_ASKNAME;
-				continue;
-			}
-#endif
-			copyunix(howto, opendev, io);
-			close(io);
-			howto |= RB_SINGLE|RB_ASKNAME;
-		}
-		if (++retry > 2)
-			howto |= RB_SINGLE|RB_ASKNAME;
+		exec(line, lowram, howto);
+		printf("boot: %s\n", strerror(errno));
+		howto |= RB_SINGLE|RB_ASKNAME;
 	}
 }
 
-/*ARGSUSED*/
-copyunix(howto, devtype, io)
-	register int howto;	/* d7 contains boot flags */
-	register u_int devtype;	/* d6 contains boot device */
-	register int io;
+void
+machdep_start(entry, howto, loadaddr, ssym, esym)
+	char *entry;
+	int howto;
+	char *loadaddr;
+	char *ssym, *esym;
 {
-	struct exec x;
-	int i;
-	register char *load;	/* a5 contains load addr for unix */
-	register char *addr;
 
-	i = read(io, (char *)&x, sizeof(x));
-	if (i != sizeof(x) ||
-	    N_BADMAG(x)) {
-		printf("Bad format\n");
-		return;
-	}
-	printf("%d", x.a_text);
-	if (N_GETMAGIC(x) == ZMAGIC && lseek(io, 0x400, SEEK_SET) == -1)
-		goto shread;
-	load = addr = lowram;
-	if (read(io, (char *)addr, x.a_text) != x.a_text)
-		goto shread;
-	addr += x.a_text;
-	if (N_GETMAGIC(x) == ZMAGIC || N_GETMAGIC(x) == NMAGIC)
-		while ((int)addr & CLOFSET)
-			*addr++ = 0;
-	printf("+%d", x.a_data);
-	if (read(io, addr, x.a_data) != x.a_data)
-		goto shread;
-	addr += x.a_data;
-	printf("+%d", x.a_bss);
-	for (i = 0; i < x.a_bss; i++)
-		*addr++ = 0;
-	ssym = addr;
-	bcopy(&x.a_syms, addr, sizeof(x.a_syms));
-	addr += sizeof(x.a_syms);
-	printf("+[%d", x.a_syms);
-	if (read(io, addr, x.a_syms) != x.a_syms)
-		goto shread;
-	addr += x.a_syms;
-
-	if (read(io, &i, sizeof(int)) != sizeof(int))
-		goto shread;
-
-	bcopy(&i, addr, sizeof(int));
-	if (i) {
-		i -= sizeof(int);
-		addr += sizeof(int);
-		if (read(io, addr, i) != i)
-		    goto shread;
-		addr += i;
-	}
-	printf("+%d] ", i);
-
-#define	round_to_size(x) \
-	(((int)(x) + sizeof(int) - 1) & ~(sizeof(int) - 1))
-	esym = (char *)round_to_size(addr - lowram);
-#undef round_to_size
-
-	x.a_entry += (int)lowram;
-	printf(" start 0x%x\n", x.a_entry);
-#ifdef __GNUC__
-	asm("	movl %0,d7" : : "m" (howto));
-	asm("	movl %0,d6" : : "m" (devtype));
-	asm("	movl %0,a5" : : "a" (load));
-	asm("	movl %0,a4" : : "a" (esym));
-#endif
-	(*((int (*)()) x.a_entry))();
-	return;
-shread:
-	printf("Short read\n");
-	return;
+	asm("movl %0,d7" : : "m" (howto));
+	asm("movl %0,d6" : : "m" (opendev));
+	asm("movl %0,a5" : : "a" (loadaddr));
+	asm("movl %0,a4" : : "a" (esym));
+	(*((int (*)())entry))();
 }
