edfa83365254b6d7c6cdaa0d30b214319daeee7f
cgd <cgd@NetBSD.org>
1995-02-27 17:00:21 +0000

    from Lite

--- lib/libc/quad/anddi3.c
+++ lib/libc/quad/anddi3.c
@@ -36,8 +36,7 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-/*static char *sccsid = "from: @(#)anddi3.c	8.1 (Berkeley) 6/4/93";*/
-static char *rcsid = "$Id: anddi3.c,v 1.1 1993/09/16 06:05:48 mycroft Exp $";
+static char sccsid[] = "@(#)anddi3.c	8.1 (Berkeley) 6/4/93";
 #endif /* LIBC_SCCS and not lint */
 
 #include "quad.h"
--- lib/libc/quad/muldi3.c
+++ lib/libc/quad/muldi3.c
@@ -36,8 +36,7 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-/*static char *sccsid = "from: @(#)muldi3.c	8.1 (Berkeley) 6/4/93";*/
-static char *rcsid = "$Id: muldi3.c,v 1.1 1993/09/16 06:06:05 mycroft Exp $";
+static char sccsid[] = "@(#)muldi3.c	8.1 (Berkeley) 6/4/93";
 #endif /* LIBC_SCCS and not lint */
 
 #include "quad.h"
