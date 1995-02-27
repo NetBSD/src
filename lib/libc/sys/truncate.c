4cd159db0099f5083bfb3b1a6ef168238dec5ac7
cgd <cgd>
1995-02-27 11:22:54 +0000

    clean up merge, fix Id's

--- lib/libc/sys/truncate.c
+++ lib/libc/sys/truncate.c
@@ -1,3 +1,5 @@
+/*	$NetBSD: truncate.c,v 1.3 1995/02/27 11:23:11 cgd Exp $	*/
+
 /*
  * Copyright (c) 1992, 1993
  *	The Regents of the University of California.  All rights reserved.
@@ -32,8 +34,11 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-/* from: static char sccsid[] = "@(#)truncate.c	8.1 (Berkeley) 6/17/93"; */
-static char *rcsid = "$Id: truncate.c,v 1.2 1994/05/06 01:02:59 cgd Exp $";
+#if 0
+static char sccsid[] = "@(#)truncate.c	8.1 (Berkeley) 6/17/93";
+#else
+static char rcsid[] = "$NetBSD: truncate.c,v 1.3 1995/02/27 11:23:11 cgd Exp $";
+#endif
 #endif /* LIBC_SCCS and not lint */
 
 #include <sys/types.h>
