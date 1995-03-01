4d5dfa529ad1ceb7ed6f99fa9f56b70540a916ba
jtc <jtc>
1995-03-01 07:52:23 +0000

    Revert to code before it was merged with 4.4lite. Updated to conform to new NetBSD RCS ID conventions.

--- bin/mkdir/mkdir.c
+++ bin/mkdir/mkdir.c
@@ -1,6 +1,8 @@
+/*	$NetBSD: mkdir.c,v 1.11 1995/03/01 07:52:23 jtc Exp $	*/
+
 /*
- * Copyright (c) 1983, 1992, 1993
- *	The Regents of the University of California.  All rights reserved.
+ * Copyright (c) 1983 Regents of the University of California.
+ * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -32,87 +34,78 @@
  */
 
 #ifndef lint
-static char copyright[] =
-"@(#) Copyright (c) 1983, 1992, 1993\n\
-	The Regents of the University of California.  All rights reserved.\n";
+char copyright[] =
+"@(#) Copyright (c) 1983 Regents of the University of California.\n\
+ All rights reserved.\n";
 #endif /* not lint */
 
 #ifndef lint
-/*static char sccsid[] = "from: @(#)mkdir.c	8.2 (Berkeley) 1/25/94";*/
-static char *rcsid = "$Id: mkdir.c,v 1.10 1994/09/22 09:25:39 mycroft Exp $";
+#if 0
+static char sccsid[] = "@(#)mkdir.c	5.7 (Berkeley) 5/31/90";
+#endif
+static char rcsid[] = "$NetBSD: mkdir.c,v 1.11 1995/03/01 07:52:23 jtc Exp $";
 #endif /* not lint */
 
-#include <sys/types.h>
-#include <sys/stat.h>
-
-#include <err.h>
-#include <errno.h>
-#include <locale.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <locale.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <unistd.h>
+#include <err.h>
 
-int	build __P((char *));
-void	usage __P((void));
-
-int
 main(argc, argv)
 	int argc;
-	char *argv[];
+	char **argv;
 {
-	int ch, exitval, omode, pflag;
-	mode_t *set;
-	char *ep, *mode;
+	int ch, exitval, pflag;
+	void *set;
+	mode_t mode, dir_mode;
 
 	setlocale(LC_ALL, "");
 
+	/* default file mode is a=rwx (777) with selected permissions
+	   removed in accordance with the file mode creation mask.
+	   For intermediate path name components, the mode is the default
+	   modified by u+wx so that the subdirectories can always be 
+	   created. */
+	mode = 0777 & ~umask(0);
+	dir_mode = mode | S_IWUSR | S_IXUSR;
+
 	pflag = 0;
-	mode = NULL;
-	while ((ch = getopt(argc, argv, "m:p")) != -1)
+	while ((ch = getopt(argc, argv, "pm:")) != -1)
 		switch(ch) {
 		case 'p':
 			pflag = 1;
 			break;
 		case 'm':
-			mode = optarg;
+			if ((set = setmode(optarg)) == NULL) {
+				errx(1, "invalid file mode.");
+				/* NOTREACHED */
+			}
+			mode = getmode (set, S_IRWXU | S_IRWXG | S_IRWXO);
 			break;
 		case '?':
 		default:
 			usage();
 		}
 
-	argc -= optind;
-	argv += optind;
-	if (argv[0] == NULL)
+	if (!*(argv += optind))
 		usage();
+	
+	for (exitval = 0; *argv; ++argv) {
+		register char *slash;
 
-	if (mode == NULL) {
-		omode = S_IRWXU | S_IRWXG | S_IRWXO;
-	} else if (*mode >= '0' && *mode <= '7') {
-		omode = (int)strtol(mode, &ep, 8);
-		if (omode < 0 || *ep)
-			errx(1, "invalid file mode: %s", mode);
-	} else {
-		if ((set = setmode(mode)) == NULL)
-			errx(1, "invalid file mode: %s", mode);
-		omode = getmode(set, S_IRWXU | S_IRWXG | S_IRWXO);
-	}
+		/* delete trailing slashes */
+		slash = strrchr(*argv, '\0');
+		while (--slash > *argv && *slash == '/')
+			*slash = '\0';
 
-	for (exitval = 0; *argv != NULL; ++argv) {
-		char *p;
-
-		/* Remove trailing slashes, per POSIX. */
-		p = *argv + strlen(*argv);
-		while (--p > *argv && *p == '/')
-			;
-		*++p = '\0';
-
-		if (pflag && build(*argv)) {
-			exitval = 1;
-			continue;
-		}
-		if (mkdir(*argv, omode) < 0) {
+		if (pflag) {
+			exitval |= mkpath(*argv, mode, dir_mode);
+		} else if (mkdir(*argv, mode) < 0) {
 			warn("%s", *argv);
 			exitval = 1;
 		}
@@ -120,61 +113,54 @@ main(argc, argv)
 	exit(exitval);
 }
 
-int
-build(path)
+/*
+ * mkpath -- create directories.  
+ *	path     - path
+ *	mode     - file mode of terminal directory
+ *	dir_mode - file mode of intermediate directories
+ */
+mkpath(path, mode, dir_mode)
 	char *path;
+	mode_t mode;
+	mode_t dir_mode;
 {
 	struct stat sb;
-	mode_t numask, oumask;
-	int first;
-	char *p;
+	register char *slash;
 
-	p = path;
-	while (p[0] == '/')		/* Skip leading '/'. */
-		++p;
-	for (first = 1;; ++p) {
-		if (p[0] == '\0')
-			break;
-		if (p[0] != '/')
-			continue;
-		*p = '\0';
-		if (first) {
-			/*
-			 * POSIX 1003.2:
-			 * For each dir operand that does not name an existing
-			 * directory, effects equivalent to those cased by the
-			 * following command shall occcur:
-			 *
-			 * mkdir -p -m $(umask -S),u+wx $(dirname dir) &&
-			 *    mkdir [-m mode] dir
-			 *
-			 * We change the user's umask and then restore it,
-			 * instead of doing chmod's.
-			 */
-			oumask = umask(0);
-			numask = oumask & ~(S_IWUSR | S_IXUSR);
-			(void)umask(numask);
-			first = 0;
-		}
+	/* skip leading slashes */
+	slash = path;
+	while (*slash == '/')
+		slash++;
+
+	while ((slash = strchr(slash, '/')) != NULL) {
+		*slash = '\0';
+
 		if (stat(path, &sb)) {
-			if (errno != ENOENT ||
-			    mkdir(path, S_IRWXU | S_IRWXG | S_IRWXO) < 0) {
+			if (errno != ENOENT || mkdir(path, dir_mode)) {
 				warn("%s", path);
-				return (1);
+				return 1;
 			}
+		} else if (!S_ISDIR(sb.st_mode)) {
+		        warnx("%s: %s", path, strerror(ENOTDIR));
+			return 1;
 		}
-		*p = '/';
-		while (p[1] == '/')		/* Skip '/'s. */
-			++p;
+		    
+		/* skip multiple slashes */
+		*slash++ = '/';
+		while (*slash == '/')
+			slash++;
 	}
-	if (!first)
-		(void)umask(oumask);
-	return (0);
+
+	if (mkdir (path, mode)) {
+		warn("%s", path);
+		return 1;
+	}
+
+	return(0);
 }
 
-void
 usage()
 {
-	(void)fprintf(stderr, "usage: mkdir [-p] [-m mode] directory ...\n");
-	exit (1);
+	(void)fprintf(stderr, "usage: mkdir [-p] [-m mode] dirname ...\n");
+	exit(1);
 }
