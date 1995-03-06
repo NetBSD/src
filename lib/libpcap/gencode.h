d25d479043f4daa6a33e033e24a55b73a296b24e
mycroft <mycroft@NetBSD.org>
1995-03-06 11:37:58 +0000

    Port to NetBSD, and add NetBSD RCS tags.

--- lib/libpcap/gencode.h
+++ lib/libpcap/gencode.h
@@ -1,3 +1,5 @@
+/*	$NetBSD: gencode.h,v 1.2 1995/03/06 11:38:24 mycroft Exp $	*/
+
 /*
  * Copyright (c) 1990, 1991, 1992, 1993, 1994
  *	The Regents of the University of California.  All rights reserved.
--- lib/libpcap/pcap-namedb.h
+++ lib/libpcap/pcap-namedb.h
@@ -1,3 +1,5 @@
+/*	$NetBSD: pcap-namedb.h,v 1.2 1995/03/06 11:38:48 mycroft Exp $	*/
+
 /*
  * Copyright (c) 1994
  *	The Regents of the University of California.  All rights reserved.
