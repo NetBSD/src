c1825c5238cda663abb262d9fa968426e51d5f24
cgd <cgd>
1995-03-07 14:44:05 +0000

    from 4.4-Lite

--- usr.sbin/mtree/misc.c
+++ usr.sbin/mtree/misc.c
@@ -1,6 +1,6 @@
 /*-
- * Copyright (c) 1991 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1991, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -30,8 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	from: @(#)misc.c	5.2 (Berkeley) 4/17/92
- *	$Id: misc.c,v 1.1 1993/11/02 07:51:10 cgd Exp $
+ *	@(#)misc.c	8.1 (Berkeley) 6/6/93
  */
 
 #include <sys/types.h>
