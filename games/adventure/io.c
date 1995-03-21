43b952514bcba077e4577d97b0351cbcda56a9c7
cgd <cgd@NetBSD.org>
1995-03-21 12:04:53 +0000

    clean up RCS ids

--- games/adventure/io.c
+++ games/adventure/io.c
@@ -1,3 +1,5 @@
+/*	$NetBSD: io.c,v 1.2 1995/03/21 12:05:05 cgd Exp $	*/
+
 /*-
  * Copyright (c) 1991, 1993
  *	The Regents of the University of California.  All rights reserved.
@@ -37,7 +39,11 @@
  */
 
 #ifndef lint
+#if 0
 static char sccsid[] = "@(#)io.c	8.1 (Berkeley) 5/31/93";
+#else
+static char rcsid[] = "$NetBSD: io.c,v 1.2 1995/03/21 12:05:05 cgd Exp $";
+#endif
 #endif /* not lint */
 
 /*      Re-coding of advent in C: file i/o and user i/o                 */
--- games/adventure/wizard.c
+++ games/adventure/wizard.c
@@ -1,3 +1,5 @@
+/*	$NetBSD: wizard.c,v 1.2 1995/03/21 12:05:15 cgd Exp $	*/
+
 /*-
  * Copyright (c) 1991, 1993
  *	The Regents of the University of California.  All rights reserved.
@@ -37,7 +39,11 @@
  */
 
 #ifndef lint
+#if 0
 static char sccsid[] = "@(#)wizard.c	8.1 (Berkeley) 6/2/93";
+#else
+static char rcsid[] = "$NetBSD: wizard.c,v 1.2 1995/03/21 12:05:15 cgd Exp $";
+#endif
 #endif /* not lint */
 
 /*      Re-coding of advent in C: privileged operations                 */
