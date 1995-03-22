ea0108c8e9b3038a3fef5aac76610210b8acaa15
glass <glass@NetBSD.org>
1995-03-22 17:30:04 +0000

    make grog install properly

--- gnu/usr.bin/groff/grog/grog.sh
+++ gnu/usr.bin/groff/grog/grog.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 # grog -- guess options for groff command
 # Like doctype in Kernighan & Pike, Unix Programming Environment, pp 306-8.
+# $Id: grog.sh,v 1.2 1995/03/22 17:30:15 glass Exp $
 
 soelim=gsoelim
 
