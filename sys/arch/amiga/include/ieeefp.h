a7be0d714c939e7857cb13f9c7fadd9f02ab9fb1
mycroft <mycroft@NetBSD.org>
1995-04-09 21:31:58 +0000

    Move fp_except and fp_rnd definitions to machine/include.

---
+++ sys/arch/amiga/include/ieeefp.h
@@ -0,0 +1,4 @@
+/*	$NetBSD: ieeefp.h,v 1.1 1995/04/09 21:36:53 mycroft Exp $	*/
+
+/* Just use the common m68k definition */
+#include <m68k/ieefp.h>
---
+++ sys/arch/sun3/include/ieeefp.h
@@ -0,0 +1,4 @@
+/*	$NetBSD: ieeefp.h,v 1.1 1995/04/09 21:37:45 mycroft Exp $	*/
+
+/* Just use the common m68k definition */
+#include <m68k/ieefp.h>
