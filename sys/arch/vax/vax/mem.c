355841b8d145dda1ff7662dc8ccddc3cee6ce8b7
mycroft <mycroft@NetBSD.org>
1995-04-10 04:02:53 +0000

    Update to match hp300 port.

--- sys/arch/vax/vax/mem.c
+++ sys/arch/vax/vax/mem.c
@@ -1,8 +1,13 @@
-/*	$NetBSD: mem.c,v 1.4 1995/03/30 21:25:25 ragge Exp $	*/
+/*	$NetBSD: mem.c,v 1.5 1995/04/10 04:02:53 mycroft Exp $	*/
 
 /*
- * Copyright (c) 1994 Ludd, University of Lule}, Sweden.
- * All rights reserved.
+ * Copyright (c) 1988 University of Utah.
+ * Copyright (c) 1982, 1986, 1990, 1993
+ *	The Regents of the University of California.  All rights reserved.
+ *
+ * This code is derived from software contributed to Berkeley by
+ * the Systems Programming Group of the University of Utah Computer
+ * Science Department.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -14,125 +19,173 @@
  *    documentation and/or other materials provided with the distribution.
  * 3. All advertising materials mentioning features or use of this software
  *    must display the following acknowledgement:
- *     This product includes software developed at Ludd, University of Lule}.
- * 4. The name of the author may not be used to endorse or promote products
- *    derived from this software without specific prior written permission
+ *	This product includes software developed by the University of
+ *	California, Berkeley and its contributors.
+ * 4. Neither the name of the University nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
  *
- * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
- * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
- * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
- * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
- * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
- * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ *	@(#)mem.c       8.3 (Berkeley) 1/12/94
  */
 
- /* All bugs are subject to removal without further notice */
-		
-
+/*
+ * Memory special file
+ */
 
 #include <sys/param.h>
+#include <sys/conf.h>
 #include <sys/buf.h>
-#include <sys/types.h>
+#include <sys/systm.h>
 #include <sys/uio.h>
 #include <sys/malloc.h>
-#include <sys/proc.h>
-#include <sys/fcntl.h>
-#include <sys/errno.h>
-#include "lib/libkern/libkern.h"
-#include "vm/vm.h"
-#include "vm/vm_kern.h"
-#include "machine/pte.h"
-#include "machine/mtpr.h"
 
-#define TRUE 1
+#include <machine/pte.h>
+#include <machine/mtpr.h>
 
-extern int *pte_cmap;
-extern unsigned int v_cmap,avail_end;
+#include <vm/vm.h>
 
-mmrw(dev_t dev, struct uio *uio, int flags) {
+extern unsigned int vmmap, avail_end;
+caddr_t zeropage;
 
-  unsigned long  b;
-  unsigned long  c,o;
-	caddr_t zbuf = NULL;
+/*ARGSUSED*/
+int
+mmopen(dev, flag, mode)
+	dev_t dev;
+	int flag, mode;
+{
 
-  int error = 0;
-  register struct iovec *iov;
+	return (0);
+}
 
-  while (uio->uio_resid > 0 && error == 0) {
-    iov = uio->uio_iov;
-    if (iov->iov_len == 0) {
-      uio->uio_iov++;
-      uio->uio_iovcnt--;
-      if (uio->uio_iovcnt < 0) {
-	panic("mmrw");
-      }
-      continue;
-    }
-    switch (minor(dev)) {
-    case 0:   /* Minor dev 0 is physical memory */
-      if((b=uio->uio_offset) > avail_end) {
-	return(EFAULT);
-      }
+/*ARGSUSED*/
+int
+mmclose(dev, flag, mode)
+	dev_t dev;
+	int flag, mode;
+{
 
-      pmap_enter(pmap_kernel(), v_cmap, trunc_page(b),
-		 uio->uio_rw == UIO_READ ?
-		 VM_PROT_READ : VM_PROT_WRITE, TRUE);
-      o = (int)uio->uio_offset & PGOFSET;
-      c = (u_int)(NBPG - ((int)iov->iov_base & PGOFSET));
-      c = min(c, (u_int)(NBPG - o));
-      c = min(c, (u_int)iov->iov_len);
-      error = uiomove((caddr_t)(v_cmap+o), (int)c, uio);
-      pmap_remove(pmap_kernel(), v_cmap, v_cmap+NBPG);
-      continue;
+	return (0);
+}
+
+/*ARGSUSED*/
+int
+mmrw(dev, uio, flags)
+	dev_t dev;
+	struct uio *uio;
+	int flags;
+{
+	register int o;
+	register u_int c, v;
+	register struct iovec *iov;
+	int error = 0;
+	static int physlock;
+
+	if (minor(dev) == 0) {
+		/* lock against other uses of shared vmempage */
+		while (physlock > 0) {
+			physlock++;
+			error = tsleep((caddr_t)&physlock, PZERO | PCATCH,
+			    "mmrw", 0);
+			if (error)
+				return (error);
+		}
+		physlock = 1;
+	}
+	while (uio->uio_resid > 0 && error == 0) {
+		iov = uio->uio_iov;
+		if (iov->iov_len == 0) {
+			uio->uio_iov++;
+			uio->uio_iovcnt--;
+			if (uio->uio_iovcnt < 0)
+				panic("mmrw");
+			continue;
+		}
+		switch (minor(dev)) {
+
+/* minor device 0 is physical memory */
+		case 0:
+			v = uio->uio_offset;
+			if (v < 0 || v >= avail_end) {
+				error = EFAULT;
+				goto unlock;
+			}
+			pmap_enter(kernel_pmap, (vm_offset_t)vmmap,
+			    trunc_page(v), uio->uio_rw == UIO_READ ?
+			    VM_PROT_READ : VM_PROT_WRITE, TRUE);
+			o = (int)uio->uio_offset & PGOFSET;
+			c = min(uio->uio_resid, (u_int)(NBPG - o));
+			error = uiomove((caddr_t)vmmap + o, (int)c, uio);
+			pmap_remove(kernel_pmap, (vm_offset_t)vmmap,
+			    (vm_offset_t)vmmap + NBPG);
+			continue;
 
 /* minor device 1 is kernel memory */
-                case 1:
-                        c = iov->iov_len;
-                        if (!kernacc((caddr_t)(long)uio->uio_offset, c,
-                            uio->uio_rw == UIO_READ ? B_READ : B_WRITE))
-                                return(EFAULT);
-                        error = uiomove((caddr_t)(long)uio->uio_offset,
-                            (int)c, uio);
-                        continue;
+		case 1:
+			v = uio->uio_offset;
+			c = min(iov->iov_len, MAXPHYS);
+			if (!kernacc((caddr_t)v, c,
+			    uio->uio_rw == UIO_READ ? B_READ : B_WRITE))
+				return (EFAULT);
+			error = uiomove(v, (int)c, uio);
+			continue;
 
-
-    case 2:   /* Minor dev 2 is EOF/RATHOLE ,ie /dev/null */
-      if (uio->uio_rw == UIO_READ) {
-	return (0);
-      }
-      c = iov->iov_len;
-      break;
+/* minor device 2 is EOF/RATHOLE */
+		case 2:
+			if (uio->uio_rw == UIO_WRITE)
+				uio->uio_resid = 0;
+			return (0);
 
 /* minor device 12 (/dev/zero) is source of nulls on read, rathole on write */
-                case 12:
-                        if (uio->uio_rw == UIO_WRITE) {
-                                c = iov->iov_len;
-                                break;
-                        }
-                        if (zbuf == NULL) {
-                                zbuf = (caddr_t)
-                                    malloc(CLBYTES, M_TEMP, M_WAITOK);
-                                bzero(zbuf, CLBYTES);
-                        }
-                        c = min(iov->iov_len, CLBYTES);
-                        error = uiomove(zbuf, (int)c, uio);
-                        continue;
+		case 12:
+			if (uio->uio_rw == UIO_WRITE) {
+				c = iov->iov_len;
+				break;
+			}
+			if (zeropage == NULL) {
+				zeropage = (caddr_t)
+				    malloc(CLBYTES, M_TEMP, M_WAITOK);
+				bzero(zeropage, CLBYTES);
+			}
+			c = min(iov->iov_len, CLBYTES);
+			error = uiomove(zeropage, (int)c, uio);
+			continue;
 
-    default:
-      return (ENXIO);
-    }
-    if (error)
-      break;
-    iov->iov_base += c;
-    iov->iov_len -= c;
-    uio->uio_offset += c;
-    uio->uio_resid -= c;
-  }
-	if (zbuf)
-		free(zbuf, M_TEMP);
-  return (error);
+		default:
+			return (ENXIO);
+		}
+		if (error)
+			break;
+		iov->iov_base += c;
+		iov->iov_len -= c;
+		uio->uio_offset += c;
+		uio->uio_resid -= c;
+	}
+	if (minor(dev) == 0) {
+unlock:
+		if (physlock > 1)
+			wakeup((caddr_t)&physlock);
+		physlock = 0;
+	}
+	return (error);
+}
+
+int
+mmmmap(dev, off, prot)
+	dev_t dev;
+	int off, prot;
+{
+
+	return (EOPNOTSUPP);
 }
