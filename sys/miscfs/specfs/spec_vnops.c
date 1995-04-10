7f3a305b34162d828f9ca89bbf173a28408ed654
mycroft <mycroft@NetBSD.org>
1995-04-10 00:48:10 +0000

    Use the new d_type field.  Set VISTTY for vnodes of tty devices.

--- sys/miscfs/specfs/spec_vnops.c
+++ sys/miscfs/specfs/spec_vnops.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: spec_vnops.c,v 1.22 1994/12/14 18:49:38 mycroft Exp $	*/
+/*	$NetBSD: spec_vnops.c,v 1.23 1995/04/10 00:48:10 mycroft Exp $	*/
 
 /*
  * Copyright (c) 1989, 1993
@@ -158,7 +158,7 @@ spec_open(ap)
 			 * When running in very secure mode, do not allow
 			 * opens for writing of any disk character devices.
 			 */
-			if (securelevel >= 2 && isdisk(dev, VCHR))
+			if (securelevel >= 2 && cdevsw[maj].d_type == D_DISK)
 				return (EPERM);
 			/*
 			 * When running in secure mode, do not allow opens
@@ -176,6 +176,8 @@ spec_open(ap)
 					return (EPERM);
 			}
 		}
+		if (cdevsw[maj].d_type == D_TTY)
+			vp->v_flag |= VISTTY;
 		VOP_UNLOCK(vp);
 		error = (*cdevsw[maj].d_open)(dev, ap->a_mode, S_IFCHR, ap->a_p);
 		VOP_LOCK(vp);
@@ -189,7 +191,7 @@ spec_open(ap)
 		 * opens for writing of any disk block devices.
 		 */
 		if (securelevel >= 2 && ap->a_cred != FSCRED &&
-		    (ap->a_mode & FWRITE) && isdisk(dev, VBLK))
+		    (ap->a_mode & FWRITE) && bdevsw[maj].d_type == D_DISK)
 			return (EPERM);
 		/*
 		 * Do not allow opens of block devices that are
@@ -387,20 +389,21 @@ spec_ioctl(ap)
 	} */ *ap;
 {
 	dev_t dev = ap->a_vp->v_rdev;
+	int maj = major(dev);
 
 	switch (ap->a_vp->v_type) {
 
 	case VCHR:
-		return ((*cdevsw[major(dev)].d_ioctl)(dev, ap->a_command, ap->a_data,
+		return ((*cdevsw[maj].d_ioctl)(dev, ap->a_command, ap->a_data,
 		    ap->a_fflag, ap->a_p));
 
 	case VBLK:
 		if (ap->a_command == 0 && (long)ap->a_data == B_TAPE)
-			if (bdevsw[major(dev)].d_flags & B_TAPE)
+			if (bdevsw[maj].d_type == D_TAPE)
 				return (0);
 			else
 				return (1);
-		return ((*bdevsw[major(dev)].d_ioctl)(dev, ap->a_command, ap->a_data,
+		return ((*bdevsw[maj].d_ioctl)(dev, ap->a_command, ap->a_data,
 		   ap->a_fflag, ap->a_p));
 
 	default:
