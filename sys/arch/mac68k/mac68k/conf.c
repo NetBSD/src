91ddb691d1862c33bebe960d216225d6156f9b53
mycroft <mycroft@NetBSD.org>
1995-04-10 10:01:40 +0000

    Minor consistency nits.

--- sys/arch/mac68k/mac68k/conf.c
+++ sys/arch/mac68k/mac68k/conf.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: conf.c,v 1.21 1995/04/10 07:54:45 mycroft Exp $	*/
+/*	$NetBSD: conf.c,v 1.22 1995/04/10 10:02:03 mycroft Exp $	*/
 
 /*
  * Copyright (c) 1990 The Regents of the University of California.
@@ -111,24 +111,24 @@ int	lkmenodev();
 
 struct bdevsw	bdevsw[] =
 {
-	bdev_notdef(),         		/* 0: */
-	bdev_notdef(),			/* 1: */
-	bdev_notdef(),         		/* 2: */
+	bdev_notdef(),         		/* 0 */
+	bdev_notdef(),			/* 1 */
+	bdev_notdef(),         		/* 2 */
 	bdev_swap_init(),		/* 3: swap pseudo-device */
-	bdev_disk_init(NSD,sd),		/* 4: scsi disk */
-	bdev_tape_init(NST,st),		/* 5: scsi tape */
-	bdev_disk_init(NCD,cd),		/* 6: scsi CD driver */
-	bdev_notdef(),        	 	/* 7: */
-	bdev_disk_init(NVND,vnd),	/* 8: vnode disk driver. */
-	bdev_notdef(),			/* 9: */
-	bdev_lkm_dummy(),		/* 10: Empty slot for LKM */
-	bdev_lkm_dummy(),		/* 11: Empty slot for LKM */
-	bdev_lkm_dummy(),		/* 12: Empty slot for LKM */
-	bdev_lkm_dummy(),		/* 13: Empty slot for LKM */
-	bdev_lkm_dummy(),		/* 14: Empty slot for LKM */
-	bdev_lkm_dummy(),		/* 15: Empty slot for LKM */
+	bdev_disk_init(NSD,sd),		/* 4: SCSI disk */
+	bdev_tape_init(NST,st),		/* 5: SCSI tape */
+	bdev_disk_init(NCD,cd),		/* 6: SCSI CD-ROM */
+	bdev_notdef(),        	 	/* 7 */
+	bdev_disk_init(NVND,vnd),	/* 8: vnode disk driver */
+	bdev_notdef(),			/* 9 */
+	bdev_lkm_dummy(),		/* 10 */
+	bdev_lkm_dummy(),		/* 11 */
+	bdev_lkm_dummy(),		/* 12 */
+	bdev_lkm_dummy(),		/* 13 */
+	bdev_lkm_dummy(),		/* 14 */
+	bdev_lkm_dummy(),		/* 15 */
 };
-int	nblkdev = sizeof (bdevsw) / sizeof (bdevsw[0]);
+int	nblkdev = sizeof(bdevsw) / sizeof(bdevsw[0]);
 
 /* open, close, ioctl, select, mmap -- XXX should be a map device */
 #define	cdev_grf_init(c,n) { \
@@ -199,35 +199,34 @@ struct cdevsw	cdevsw[] =
 	cdev_tty_init(NPTY,pts),	/* 4: pseudo-tty slave */
 	cdev_ptc_init(NPTY,ptc),	/* 5: pseudo-tty master */
 	cdev_log_init(1,log),		/* 6: /dev/klog */
-	cdev_notdef(),			/* 7: */
-	cdev_notdef(),			/* 8: */
-	cdev_notdef(),			/* 9: */
+	cdev_notdef(),			/* 7 */
+	cdev_notdef(),			/* 8 */
+	cdev_notdef(),			/* 9 */
 	cdev_grf_init(1,grf),		/* 10: frame buffer */
 	cdev_tty_init(NITE,ite),	/* 11: console terminal emulator */
 	cdev_tty_init(NSER,ser),	/* 12: 2 mac serial ports -- BG*/
-	cdev_disk_init(NSD,sd),		/* 13: scsi disk */
-	cdev_tape_init(NST,st),		/* 14: scsi tape */
-	cdev_disk_init(NCD,cd),		/* 15: scsi compact disc */
-	cdev_notdef(),			/* 16: */
-/*	cdev_disk_init(NCH,ch),		 17: scsi changer device */
+	cdev_disk_init(NSD,sd),		/* 13: SCSI disk */
+	cdev_tape_init(NST,st),		/* 14: SCSI tape */
+	cdev_disk_init(NCD,cd),		/* 15: SCSI CD-ROM */
+	cdev_notdef(),			/* 16 */
+/*	cdev_disk_init(NCH,ch),		 17: SCSI autochanger */
 	cdev_notdef(),			/* 17: until we find chstrategy... */
 	cdev_clock_init(NCLOCK,clock),	/* 18: mapped clock */
-	cdev_disk_init(NVND,vnd),	/* 19: vnode disk */
-	cdev_tape_init(NST,st),		/* 20: exabyte tape */
-	cdev_fd_init(1,fd),		/* 21: file descriptor pseudo-dev */
-	cdev_bpftun_init(NBPFILTER,bpf),/* 22: berkeley packet filter */
+	cdev_disk_init(NVND,vnd),	/* 19: vnode disk driver */
+	cdev_tape_init(NST,st),		/* 20: SCSI tape */
+	cdev_fd_init(1,fd),		/* 21: file descriptor pseudo-device */
+	cdev_bpftun_init(NBPFILTER,bpf),/* 22: Berkeley packet filter */
 	cdev_adb_init(adb),		/* 23: ADB event interface */
 	cdev_bpftun_init(NTUN,tun),	/* 24: network tunnel */
-	cdev_lkm_init(NLKM,lkm),	/* 25: loadable kernel modules pdev */
-	cdev_lkm_dummy(),		/* 26: Empty slot for LKM */
-	cdev_lkm_dummy(),		/* 27: Empty slot for LKM */
-	cdev_lkm_dummy(),		/* 28: Empty slot for LKM */
-	cdev_lkm_dummy(),		/* 29: Empty slot for LKM */
-	cdev_lkm_dummy(),		/* 30: Empty slot for LKM */
-	cdev_lkm_dummy(),		/* 31: Empty slot for LKM */
+	cdev_lkm_init(NLKM,lkm),	/* 25: loadable module driver */
+	cdev_lkm_dummy(),		/* 26 */
+	cdev_lkm_dummy(),		/* 27 */
+	cdev_lkm_dummy(),		/* 28 */
+	cdev_lkm_dummy(),		/* 29 */
+	cdev_lkm_dummy(),		/* 30 */
+	cdev_lkm_dummy(),		/* 31 */
 };
-
-int	nchrdev = sizeof (cdevsw) / sizeof (cdevsw[0]);
+int	nchrdev = sizeof(cdevsw) / sizeof(cdevsw[0]);
 
 int	mem_no = 2; 	/* major device number of memory special file */
 
@@ -242,43 +241,64 @@ int	mem_no = 2; 	/* major device number of memory special file */
  */
 dev_t	swapdev = makedev(3, 0);
 
-iszerodev(dev)
-	dev_t	dev;
-{
-	if (major(dev) == mem_no && minor(dev) == 12)
-		return (1);
-	return 0;
-}
-
 /*
- * return true is memory device (kmem or mem)
+ * Returns true if dev is /dev/mem or /dev/kmem.
  */
 int
 iskmemdev(dev)
 	dev_t	dev;
 {
-	if (major(dev) == mem_no && minor(dev) < 2)
-		return (1);
-	return (0);
+
+	return (major(dev) == mem_no && minor(dev) < 2);
+}
+
+/*
+ * Returns true if dev is /dev/zero.
+ */
+iszerodev(dev)
+	dev_t	dev;
+{
+
+	return (major(dev) == mem_no && minor(dev) == 12);
 }
 
 static int chrtoblktab[] = {
-	/* CHR*/	/* BLK*/	/* CHR*/	/* BLK*/
-	/*  0 */	NODEV,		/*  1 */	NODEV,
-	/*  2 */	NODEV,		/*  3 */	3,
-	/*  4 */	NODEV,		/*  5 */	NODEV,
-	/*  6 */	NODEV,		/*  7 */	NODEV,
-	/*  8 */	NODEV,		/*  9 */	NODEV,
-	/* 10 */	NODEV,		/* 11 */	NODEV,
-	/* 12 */	NODEV,		/* 13 */	4,
-	/* 14 */	5,		/* 15 */	6,
-	/* 16 */	NODEV,		/* 17 */	NODEV,
-	/* 18 */	NODEV,		/* 19 */	8,
-	/* 20 */	NODEV,		/* 21 */	NODEV,
+	/* XXXX This needs to be dynamic for LKMs. */
+	/*VCHR*/	/*VBLK*/
+	/*  0 */	NODEV,
+	/*  1 */	NODEV,
+	/*  2 */	NODEV,
+	/*  3 */	3,
+	/*  4 */	NODEV,
+	/*  5 */	NODEV,
+	/*  6 */	NODEV,
+	/*  7 */	NODEV,
+	/*  8 */	NODEV,
+	/*  9 */	NODEV,
+	/* 10 */	NODEV,
+	/* 11 */	NODEV,
+	/* 12 */	NODEV,
+	/* 13 */	4,
+	/* 14 */	5,
+	/* 15 */	6,
+	/* 16 */	NODEV,
+	/* 17 */	NODEV,
+	/* 18 */	NODEV,
+	/* 19 */	8,
+	/* 20 */	NODEV,
+	/* 21 */	NODEV,
 	/* 22 */	NODEV,
+	/* 23 */	NODEV,
+	/* 24 */	NODEV,
+	/* 25 */	NODEV,
+	/* 26 */	NODEV,
+	/* 27 */	NODEV,
+	/* 28 */	NODEV,
+	/* 29 */	NODEV,
+	/* 30 */	NODEV,
+	/* 31 */	NODEV,
 };
 
-dev_t
 chrtoblk(dev)
 	dev_t	dev;
 {
