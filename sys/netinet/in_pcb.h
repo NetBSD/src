b5b72d26ea8389b3b5a66162aaff6c095be166e5
cgd <cgd@NetBSD.org>
1995-04-13 06:25:36 +0000

    be a bit more careful and explicit with types.  (basically a large no-op.)

--- sys/netinet/in_pcb.h
+++ sys/netinet/in_pcb.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: in_pcb.h,v 1.8 1995/03/26 20:32:24 jtc Exp $	*/
+/*	$NetBSD: in_pcb.h,v 1.9 1995/04/13 06:28:48 cgd Exp $	*/
 
 /*
  * Copyright (c) 1982, 1986, 1990, 1993
@@ -43,21 +43,21 @@
  * control block.
  */
 struct inpcb {
-	struct	inpcb *inp_next,*inp_prev;
+	struct	  inpcb *inp_next,*inp_prev;
 					/* pointers to other pcb's */
-	struct	inpcb *inp_head;	/* pointer back to chain of inpcb's
+	struct	  inpcb *inp_head;	/* pointer back to chain of inpcb's
 					   for this protocol */
-	struct	in_addr inp_faddr;	/* foreign host table entry */
-	u_short	inp_fport;		/* foreign port */
-	struct	in_addr inp_laddr;	/* local host table entry */
-	u_short	inp_lport;		/* local port */
-	struct	socket *inp_socket;	/* back pointer to socket */
-	caddr_t	inp_ppcb;		/* pointer to per-protocol pcb */
-	struct	route inp_route;	/* placeholder for routing entry */
-	int	inp_flags;		/* generic IP/datagram flags */
-	struct	ip inp_ip;		/* header prototype; should have more */
-	struct	mbuf *inp_options;	/* IP options */
-	struct	ip_moptions *inp_moptions; /* IP multicast options */
+	struct	  in_addr inp_faddr;	/* foreign host table entry */
+	u_int16_t inp_fport;		/* foreign port */
+	struct	  in_addr inp_laddr;	/* local host table entry */
+	u_int16_t inp_lport;		/* local port */
+	struct	  socket *inp_socket;	/* back pointer to socket */
+	caddr_t	  inp_ppcb;		/* pointer to per-protocol pcb */
+	struct	  route inp_route;	/* placeholder for routing entry */
+	int	  inp_flags;		/* generic IP/datagram flags */
+	struct	  ip inp_ip;		/* header prototype; should have more */
+	struct	  mbuf *inp_options;	/* IP options */
+	struct	  ip_moptions *inp_moptions; /* IP multicast options */
 };
 
 /* flags in inp_flags: */
