8d31aaf1991ab3db567434df8a8a659646e21a4c
cgd <cgd>
1995-04-15 02:22:37 +0000

    some copyright updates requested by Thorsten Lockert <tholo@SigmaSoft.COM>

--- usr.bin/ipcs/ipcs.c
+++ usr.bin/ipcs/ipcs.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: ipcs.c,v 1.9 1995/03/28 17:24:49 jtc Exp $	*/
+/*	$NetBSD: ipcs.c,v 1.10 1995/04/15 02:22:40 cgd Exp $	*/
 
 /*
  * Copyright (c) 1994 SigmaSoft, Th. Lockert <tholo@sigmasoft.com>
@@ -12,7 +12,10 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. The name of the author may not be used to endorse or promote products
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed by SigmaSoft, Th.  Lockert.
+ * 4. The name of the author may not be used to endorse or promote products
  *    derived from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
