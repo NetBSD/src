68618fcbaab11e28474d0cdec738643628ceeefb
jtc <jtc@NetBSD.org>
1995-04-16 16:47:03 +0000

    Fix typo: ieefp.h -> ieeefp.h.

--- sys/arch/mac68k/include/ieeefp.h
+++ sys/arch/mac68k/include/ieeefp.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: ieeefp.h,v 1.1 1995/04/09 21:37:33 mycroft Exp $	*/
+/*	$NetBSD: ieeefp.h,v 1.2 1995/04/16 16:47:07 jtc Exp $	*/
 
 /* Just use the common m68k definition */
-#include <m68k/ieefp.h>
+#include <m68k/ieeefp.h>
