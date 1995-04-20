a04634e01be19753b464eaffcf7ac7fd1f2b4baa
jtc <jtc@NetBSD.org>
1995-04-20 05:09:34 +0000

    First cut of makefile header that builds and installs message catalogs.

---
+++ share/mk/bsd.nls.mk
@@ -0,0 +1,45 @@
+#	$NetBSD: bsd.nls.mk,v 1.1 1995/04/20 05:09:34 jtc Exp $
+
+.if !target(.MAIN)
+.if exists(${.CURDIR}/../Makefile.inc)
+.include "${.CURDIR}/../Makefile.inc"
+.endif
+
+.MAIN: all
+.endif
+
+.SUFFIXES: .cat .msg
+
+.msg.cat:
+	@rm -f ${.TARGET}
+	gencat ${.TARGET} ${.IMPSRC}
+
+NLSALL= ${NLS:.msg=.cat}
+
+.if !defined(NLSNAME)
+.if defined(PROG)
+NLSNAME=${PROG}
+.else
+NLSNAME=lib${LIB}
+.endif
+.endif
+
+nlsinstall:
+.if defined(NLSALL)
+	@for msg in ${NLSALL}; do \
+		NLSLANG=`basename $$msg .cat`; \
+		dir=${DESTDIR}${NLSDIR}/$${NLSLANG}; \
+		install -d $$dir; \
+		install ${COPY} -o ${NLSOWN} -g ${NLSGRP} -m ${NLSMODE} $$msg $$dir/${NLSNAME}.cat; \
+	done
+.endif
+
+.if defined(NLSALL)
+all: ${NLSALL}
+
+install:  nlsinstall
+
+cleandir: cleannls
+cleannls:
+	rm -f ${NLSALL}
+.endif
