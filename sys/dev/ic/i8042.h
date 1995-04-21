bfab570cda8323b8754d08f6bf401e3ec2930fad
mycroft <mycroft@NetBSD.org>
1995-04-21 09:16:16 +0000

    Add constants for some keyboard controller commands.

--- sys/dev/ic/i8042.h
+++ sys/dev/ic/i8042.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: i8042.h,v 1.5 1994/10/27 04:18:39 cgd Exp $	*/
+/*	$NetBSD: i8042.h,v 1.6 1995/04/21 09:16:16 mycroft Exp $	*/
 
 #define	KBSTATP		0x64	/* kbd controller status port (I) */
 #define	 KBS_DIB	0x01	/* kbd data in buffer */
@@ -11,6 +11,23 @@
 #define	 KBS_PERR	0x80	/* kbd parity error */
 
 #define	KBCMDP		0x64	/* kbd controller port (O) */
+#define	 KBC_RAMREAD	0x20	/* read from RAM */
+#define	 KBC_RAMWRITE	0x60	/* write to RAM */
+#define	 KBC_AUXDISABLE	0xa7	/* disable auxiliary port */
+#define	 KBC_AUXENABLE	0xa8	/* enable auxiliary port */
+#define	 KBC_AUXTEST	0xa9	/* test auxiliary port */
+#define	 KBC_KBDECHO	0xd2	/* echo to keyboard port */
+#define	 KBC_AUXECHO	0xd3	/* echo to auxiliary port */
+#define	 KBC_AUXWRITE	0xd4	/* write to auxiliary port */
+#define	 KBC_SELFTEST	0xaa	/* start self-test */
+#define	 KBC_KBDTEST	0xab	/* test keyboard port */
+#define	 KBC_KBDDISABLE	0xad	/* disable keyboard port */
+#define	 KBC_KBDENABLE	0xae	/* enable keyboard port */
+#define	 KBC_PULSE0	0xfe	/* pulse output bit 0 */
+#define	 KBC_PULSE1	0xfd	/* pulse output bit 1 */
+#define	 KBC_PULSE2	0xfb	/* pulse output bit 2 */
+#define	 KBC_PULSE3	0xf7	/* pulse output bit 3 */
+
 #define	KBDATAP		0x60	/* kbd data port (I) */
 #define	KBOUTP		0x60	/* kbd data port (O) */
 
