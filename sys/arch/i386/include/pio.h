e15782951decb18b73dc68c4d7e39ab70904de7d
mycroft <mycroft@NetBSD.org>
1995-04-22 03:59:59 +0000

    Clean this up a bit: * Abstract the conditional for immediate port numbers, and make it always false if not optimizing. * Add some whitespace to the string functions to make them more readable.

--- sys/arch/i386/include/pio.h
+++ sys/arch/i386/include/pio.h
@@ -1,8 +1,7 @@
-/*	$NetBSD: pio.h,v 1.10 1994/11/20 21:36:44 mycroft Exp $	*/
+/*	$NetBSD: pio.h,v 1.11 1995/04/22 03:59:59 mycroft Exp $	*/
 
 /*
- * Copyright (c) 1993 Charles Hannum.
- * All rights reserved.
+ * Copyright (c) 1993, 1995 Charles M. Hannum.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -14,7 +13,7 @@
  *    documentation and/or other materials provided with the distribution.
  * 3. All advertising materials mentioning features or use of this software
  *    must display the following acknowledgement:
- *      This product includes software developed by Charles Hannum.
+ *      This product includes software developed by Charles M. Hannum.
  * 4. The name of the author may not be used to endorse or promote products
  *    derived from this software without specific prior written permission
  *
@@ -32,11 +31,27 @@
 
 /*
  * Functions to provide access to i386 programmed I/O instructions.
+ *
+ * The in[bwl]() and out[bwl]() functions are split into two varieties: one to
+ * use a small, constant, 8-bit port number, and another to use a large or
+ * variable port number.  The former can be compiled as a smaller instruction.
  */
 
+
+#ifdef __OPTIMIZE__
+
+#define	__use_immediate_port(port) \
+	(__builtin_constant_p((port)) && (port) < 0x100)
+
+#else
+
+#define	__use_immediate_port(port)	0
+
+#endif
+
+
 #define	inb(port) \
-	((__builtin_constant_p((port)) && (port) < 0x100) ? \
-	 __inbc(port) : __inb(port))
+	(__use_immediate_port(port) ? __inbc(port) : __inb(port))
 
 static __inline u_char
 __inbc(int port)
@@ -57,13 +72,14 @@ __inb(int port)
 static __inline void
 insb(int port, void *addr, int cnt)
 {
-	__asm __volatile("cld\n\trepne\n\tinsb" :
-			 : "d" (port), "D" (addr), "c" (cnt) : "%edi", "%ecx", "memory");
+	__asm __volatile("cld\n\trepne\n\tinsb"			:
+								:
+			 "d" (port), "D" (addr), "c" (cnt)	:
+			 "%edi", "%ecx", "memory");
 }
 
 #define	inw(port) \
-	((__builtin_constant_p((port)) && (port) < 0x100) ? \
-	 __inwc(port) : __inw(port))
+	(__use_immediate_port(port) ? __inwc(port) : __inw(port))
 
 static __inline u_short
 __inwc(int port)
@@ -84,13 +100,14 @@ __inw(int port)
 static __inline void
 insw(int port, void *addr, int cnt)
 {
-	__asm __volatile("cld\n\trepne\n\tinsw" :
-			 : "d" (port), "D" (addr), "c" (cnt) : "%edi", "%ecx", "memory");
+	__asm __volatile("cld\n\trepne\n\tinsw"			:
+								:
+			 "d" (port), "D" (addr), "c" (cnt)	:
+			 "%edi", "%ecx", "memory");
 }
 
 #define	inl(port) \
-	((__builtin_constant_p((port)) && (port) < 0x100) ? \
-	 __inlc(port) : __inl(port))
+	(__use_immediate_port(port) ? __inlc(port) : __inl(port))
 
 static __inline u_int
 __inlc(int port)
@@ -111,13 +128,14 @@ __inl(int port)
 static __inline void
 insl(int port, void *addr, int cnt)
 {
-	__asm __volatile("cld\n\trepne\n\tinsl" :
-			 : "d" (port), "D" (addr), "c" (cnt) : "%edi", "%ecx", "memory");
+	__asm __volatile("cld\n\trepne\n\tinsl"			:
+								:
+			 "d" (port), "D" (addr), "c" (cnt)	:
+			 "%edi", "%ecx", "memory");
 }
 
 #define	outb(port, data) \
-	((__builtin_constant_p((port)) && (port) < 0x100) ? \
-	 __outbc(port, data) : __outb(port, data))
+	(__use_immediate_port(port) ? __outbc(port, data) : __outb(port, data))
 
 static __inline void
 __outbc(int port, u_char data)
@@ -134,13 +152,14 @@ __outb(int port, u_char data)
 static __inline void
 outsb(int port, void *addr, int cnt)
 {
-	__asm __volatile("cld\n\trepne\n\toutsb" :
-			 : "d" (port), "S" (addr), "c" (cnt) : "%esi", "%ecx");
+	__asm __volatile("cld\n\trepne\n\toutsb"		:
+								:
+			 "d" (port), "S" (addr), "c" (cnt)	:
+			 "%esi", "%ecx");
 }
 
 #define	outw(port, data) \
-	((__builtin_constant_p((port)) && (port) < 0x100) ? \
-	 __outwc(port, data) : __outw(port, data))
+	(__use_immediate_port(port) ? __outwc(port, data) : __outw(port, data))
 
 static __inline void
 __outwc(int port, u_short data)
@@ -157,13 +176,14 @@ __outw(int port, u_short data)
 static __inline void
 outsw(int port, void *addr, int cnt)
 {
-	__asm __volatile("cld\n\trepne\n\toutsw" :
-			 : "d" (port), "S" (addr), "c" (cnt) : "%esi", "%ecx");
+	__asm __volatile("cld\n\trepne\n\toutsw"		:
+								:
+			 "d" (port), "S" (addr), "c" (cnt)	:
+			 "%esi", "%ecx");
 }
 
 #define	outl(port, data) \
-	((__builtin_constant_p((port)) && (port) < 0x100) ? \
-	 __outlc(port, data) : __outl(port, data))
+	(__use_immediate_port(port) ? __outlc(port, data) : __outl(port, data))
 
 static __inline void
 __outlc(int port, u_int data)
@@ -180,6 +200,8 @@ __outl(int port, u_int data)
 static __inline void
 outsl(int port, void *addr, int cnt)
 {
-	__asm __volatile("cld\n\trepne\n\toutsl" :
-			 : "d" (port), "S" (addr), "c" (cnt) : "%esi", "%ecx");
+	__asm __volatile("cld\n\trepne\n\toutsl"		:
+								:
+			 "d" (port), "S" (addr), "c" (cnt)	:
+			 "%esi", "%ecx");
 }
