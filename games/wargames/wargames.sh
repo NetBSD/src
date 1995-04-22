057dc938cc5d0b483967321e27dbbe2883a7a81a
cgd <cgd@NetBSD.org>
1995-04-22 07:49:41 +0000

    src/games/wargames from Lite

--- games/wargames/wargames.sh
+++ games/wargames/wargames.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -
 #
-# Copyright (c) 1985 The Regents of the University of California.
-# All rights reserved.
+# Copyright (c) 1985, 1993
+#	The Regents of the University of California.  All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
@@ -31,7 +31,7 @@
 # OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 # SUCH DAMAGE.
 #
-#	@(#)wargames.sh	5.4 (Berkeley) 6/1/90
+#	@(#)wargames.sh	8.1 (Berkeley) 5/31/93
 #
 echo -n "Would you like to play a game? "
 read x
