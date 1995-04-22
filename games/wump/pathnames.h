0a295f3729c9419287576f6394068d1fc0d582d7
cgd <cgd>
1995-04-22 08:19:45 +0000

    clean up import.  NetBSD RCS IDs

--- games/wump/pathnames.h
+++ games/wump/pathnames.h
@@ -1,6 +1,8 @@
+/*	$NetBSD: pathnames.h,v 1.3 1995/04/22 08:19:48 cgd Exp $	*/
+
 /*-
- * Copyright (c) 1989 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1989, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -30,8 +32,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	from: @(#)pathnames.h	5.1 (Berkeley) 6/1/90
- *	$Id: pathnames.h,v 1.2 1993/08/01 18:49:22 mycroft Exp $
+ *	@(#)pathnames.h	8.1 (Berkeley) 5/31/93
  */
 
 #define	_PATH_PAGER	"/usr/bin/more"
