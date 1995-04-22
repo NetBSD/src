89cc2ec262a41d1e267a31528e76a34058cbdbf9
cgd <cgd@NetBSD.org>
1995-04-22 10:27:22 +0000

    clean up import, NetBSD RCS Ids

--- games/rogue/curses.c
+++ games/rogue/curses.c
@@ -1,6 +1,8 @@
+/*	$NetBSD: curses.c,v 1.3 1995/04/22 10:27:27 cgd Exp $	*/
+
 /*
- * Copyright (c) 1988 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1988, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Timothy C. Stoehr.
@@ -35,8 +37,11 @@
  */
 
 #ifndef lint
-/*static char sccsid[] = "from: @(#)curses.c	5.3 (Berkeley) 6/1/90";*/
-static char rcsid[] = "$Id: curses.c,v 1.2 1993/08/01 18:52:32 mycroft Exp $";
+#if 0
+static char sccsid[] = "@(#)curses.c	8.1 (Berkeley) 5/31/93";
+#else
+static char rcsid[] = "$NetBSD: curses.c,v 1.3 1995/04/22 10:27:27 cgd Exp $";
+#endif
 #endif /* not lint */
 
 /*
--- games/rogue/pack.c
+++ games/rogue/pack.c
@@ -1,6 +1,8 @@
+/*	$NetBSD: pack.c,v 1.3 1995/04/22 10:27:54 cgd Exp $	*/
+
 /*
- * Copyright (c) 1988 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1988, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Timothy C. Stoehr.
@@ -35,8 +37,11 @@
  */
 
 #ifndef lint
-/*static char sccsid[] = "from: @(#)pack.c	5.3 (Berkeley) 6/1/90";*/
-static char rcsid[] = "$Id: pack.c,v 1.2 1993/08/01 18:52:20 mycroft Exp $";
+#if 0
+static char sccsid[] = "@(#)pack.c	8.1 (Berkeley) 5/31/93";
+#else
+static char rcsid[] = "$NetBSD: pack.c,v 1.3 1995/04/22 10:27:54 cgd Exp $";
+#endif
 #endif /* not lint */
 
 /*
@@ -294,6 +299,7 @@ unsigned short mask;
 
 		if (ch == LIST) {
 			check_message();
+			mask = tmask;
 			inventory(&rogue.pack, mask);
 		} else {
 			break;
