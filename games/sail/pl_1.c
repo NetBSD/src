982615d7df4c94ab869fcf840cf59240e14fd75c
cgd <cgd@NetBSD.org>
1995-04-22 10:36:43 +0000

    clean up import, NetBSD RCS IDs

--- games/sail/pl_1.c
+++ games/sail/pl_1.c
@@ -1,6 +1,8 @@
+/*	$NetBSD: pl_1.c,v 1.3 1995/04/22 10:37:07 cgd Exp $	*/
+
 /*
- * Copyright (c) 1983 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1983, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -32,8 +34,11 @@
  */
 
 #ifndef lint
-/*static char sccsid[] = "from: @(#)pl_1.c	5.5 (Berkeley) 2/28/91";*/
-static char rcsid[] = "$Id: pl_1.c,v 1.2 1993/08/01 18:51:41 mycroft Exp $";
+#if 0
+static char sccsid[] = "@(#)pl_1.c	8.1 (Berkeley) 5/31/93";
+#else
+static char rcsid[] = "$NetBSD: pl_1.c,v 1.3 1995/04/22 10:37:07 cgd Exp $";
+#endif
 #endif /* not lint */
 
 #include "player.h"
--- games/sail/pl_5.c
+++ games/sail/pl_5.c
@@ -1,6 +1,8 @@
+/*	$NetBSD: pl_5.c,v 1.3 1995/04/22 10:37:12 cgd Exp $	*/
+
 /*
- * Copyright (c) 1983 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1983, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -32,8 +34,11 @@
  */
 
 #ifndef lint
-/*static char sccsid[] = "from: @(#)pl_5.c	5.4 (Berkeley) 6/1/90";*/
-static char rcsid[] = "$Id: pl_5.c,v 1.2 1993/08/01 18:51:37 mycroft Exp $";
+#if 0
+static char sccsid[] = "@(#)pl_5.c	8.1 (Berkeley) 5/31/93";
+#else
+static char rcsid[] = "$NetBSD: pl_5.c,v 1.3 1995/04/22 10:37:12 cgd Exp $";
+#endif
 #endif /* not lint */
 
 #include "player.h"
