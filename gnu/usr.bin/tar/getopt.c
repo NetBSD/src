361468809e60c4b739634ed7635e89f0c98f6c13
cgd <cgd@NetBSD.org>
1995-04-29 05:36:24 +0000

    include string.h on NetBSD; we need the proto

--- gnu/usr.bin/tar/getopt.c
+++ gnu/usr.bin/tar/getopt.c
@@ -21,7 +21,7 @@
    Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
 
 #ifndef lint
-static char rcsid[] = "$Id: getopt.c,v 1.3 1993/08/02 17:48:47 mycroft Exp $";
+static char rcsid[] = "$Id: getopt.c,v 1.4 1995/04/29 05:36:24 cgd Exp $";
 #endif /* not lint */
 
 /* NOTE!!!  AIX requires this to be the first thing in the file.
@@ -161,7 +161,7 @@ static enum
   REQUIRE_ORDER, PERMUTE, RETURN_IN_ORDER
 } ordering;
 
-#ifdef	__GNU_LIBRARY__
+#if	defined(__GNU_LIBRARY__) || defined(__NetBSD__)
 /* We want to avoid inclusion of string.h with non-GNU libraries
    because there are many ways it can cause trouble.
    On some systems, it contains special magic macros that don't work
