5f0658984a28906a99f6933edfdaa8dd4ffa9b26
mycroft <mycroft@NetBSD.org>
1995-05-01 08:14:25 +0000

    Remove historical garbage.

--- sys/arch/i386/include/tss.h
+++ sys/arch/i386/include/tss.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: tss.h,v 1.4 1994/10/27 04:16:31 cgd Exp $	*/
+/*	$NetBSD: tss.h,v 1.5 1995/05/01 08:23:05 mycroft Exp $	*/
 
 /*-
  * Copyright (c) 1990 The Regents of the University of California.
@@ -43,38 +43,30 @@
  */
 
 struct i386tss {
-	int	tss_link;	/* actually 16 bits: top 16 bits must be zero */
-	int	tss_esp0; 	/* kernel stack pointer priviledge level 0 */
-#define	tss_ksp	tss_esp0
-	int	tss_ss0;	/* actually 16 bits: top 16 bits must be zero */
-	int	tss_esp1; 	/* kernel stack pointer priviledge level 1 */
-	int	tss_ss1;	/* actually 16 bits: top 16 bits must be zero */
-	int	tss_esp2; 	/* kernel stack pointer priviledge level 2 */
-	int	tss_ss2;	/* actually 16 bits: top 16 bits must be zero */
-	/* struct  ptd *tss_cr3; 	** page table directory */
-	int	tss_cr3; 	/* page table directory */
-#define	tss_ptd	tss_cr3
-	int	tss_eip; 	/* program counter */
-#define	tss_pc	tss_eip
-	int	tss_eflags; 	/* program status longword */
-#define	tss_psl	tss_eflags
-	int	tss_eax; 
-	int	tss_ecx; 
-	int	tss_edx; 
-	int	tss_ebx; 
-	int	tss_esp; 	/* user stack pointer */
-#define	tss_usp	tss_esp
-	int	tss_ebp; 	/* user frame pointer */
-#define	tss_fp	tss_ebp
-	int	tss_esi; 
-	int	tss_edi; 
-	int	tss_es;		/* actually 16 bits: top 16 bits must be zero */
-	int	tss_cs;		/* actually 16 bits: top 16 bits must be zero */
-	int	tss_ss;		/* actually 16 bits: top 16 bits must be zero */
-	int	tss_ds;		/* actually 16 bits: top 16 bits must be zero */
-	int	tss_fs;		/* actually 16 bits: top 16 bits must be zero */
-	int	tss_gs;		/* actually 16 bits: top 16 bits must be zero */
-	int	tss_ldt;	/* actually 16 bits: top 16 bits must be zero */
-	int	tss_ioopt;	/* options & io offset bitmap: currently zero */
-				/* XXX unimplemented .. i/o permission bitmap */
+	int	__tss_link;
+	int	tss_esp0; 	/* kernel stack pointer at privilege level 0 */
+	int	tss_ss0;	/* kernel stack segment at privilege level 0 */
+	int	__tss_esp1;
+	int	__tss_ss1;
+	int	__tss_esp2;
+	int	__tss_ss2;
+	int	tss_cr3;	/* page directory paddr */
+	int	__tss_eip;
+	int	__tss_eflags;
+	int	__tss_eax; 
+	int	__tss_ecx; 
+	int	__tss_edx; 
+	int	__tss_ebx; 
+	int	tss_esp;	/* saved stack pointer */
+	int	tss_ebp;	/* saved frame pointer */
+	int	__tss_esi; 
+	int	__tss_edi; 
+	int	__tss_es;
+	int	__tss_cs;
+	int	__tss_ss;
+	int	__tss_ds;
+	int	tss_fs;		/* saved segment register */
+	int	tss_gs;		/* saved segment register */
+	int	__tss_ldt;
+	int	tss_ioopt;	/* options and I/O permission map offset */
 };
