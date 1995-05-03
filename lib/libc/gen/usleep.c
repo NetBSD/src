1beced457a4f10d4b90ac476f100ff98eb850890
mycroft <mycroft>
1995-05-03 12:52:43 +0000

    Explicitly unblock SIGALRM while waiting.

--- lib/libc/gen/usleep.c
+++ lib/libc/gen/usleep.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: usleep.c,v 1.6 1995/03/21 13:50:32 mycroft Exp $	*/
+/*	$NetBSD: usleep.c,v 1.7 1995/05/03 12:52:44 mycroft Exp $	*/
 
 /*
  * Copyright (c) 1989, 1993
@@ -37,7 +37,7 @@
 #if 0
 static char sccsid[] = "@(#)usleep.c	8.1 (Berkeley) 6/4/93";
 #else
-static char rcsid[] = "$NetBSD: usleep.c,v 1.6 1995/03/21 13:50:32 mycroft Exp $";
+static char rcsid[] = "$NetBSD: usleep.c,v 1.7 1995/05/03 12:52:44 mycroft Exp $";
 #endif
 #endif /* LIBC_SCCS and not lint */
 
@@ -91,7 +91,9 @@ usleep(useconds)
 		}
 	}
 
-	(void) sigsuspend(&oset);
+	set = oset;
+	sigdelset(&set, SIGALRM);
+ 	(void) sigsuspend(&set);
 
 	sigaction(SIGALRM, &oact, NULL);
 	sigprocmask(SIG_SETMASK, &oset, NULL);
