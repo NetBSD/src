d9bfd8810f894104704b728caf672228aee568d9
mellon <mellon@NetBSD.org>
1995-05-03 06:04:54 +0000

    Use Alpha cdefs.h

--- sys/arch/pmax/include/cdefs.h
+++ sys/arch/pmax/include/cdefs.h
@@ -1,31 +1,38 @@
-/*	$NetBSD: cdefs.h,v 1.2 1995/03/23 20:10:43 jtc Exp $	*/
+/*	$NetBSD: cdefs.h,v 1.3 1995/05/03 06:04:54 mellon Exp $	*/
 
 /*
- * Written by J.T. Conklin <jtc@wimsey.com> 01/17/95.
- * Public domain.
+ * Copyright (c) 1995 Carnegie-Mellon University.
+ * All rights reserved.
+ *
+ * Author: Chris G. Demetriou
+ * 
+ * Permission to use, copy, modify and distribute this software and
+ * its documentation is hereby granted, provided that both the copyright
+ * notice and this permission notice appear in all copies of the
+ * software, derivative works or modified versions, and any portions
+ * thereof, and that both notices appear in supporting documentation.
+ * 
+ * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS "AS IS" 
+ * CONDITION.  CARNEGIE MELLON DISCLAIMS ANY LIABILITY OF ANY KIND 
+ * FOR ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.
+ * 
+ * Carnegie Mellon requests users of this software to return to
+ *
+ *  Software Distribution Coordinator  or  Software.Distribution@CS.CMU.EDU
+ *  School of Computer Science
+ *  Carnegie Mellon University
+ *  Pittsburgh PA 15213-3890
+ *
+ * any improvements or extensions that they make and grant Carnegie the
+ * rights to redistribute these changes.
  */
 
-#ifndef	_MACHINE_CDEFS_H_
+#ifndef _MACHINE_CDEFS_H_
 #define	_MACHINE_CDEFS_H_
 
-#define _C_LABEL(x)	_STRING(x)
+#define	_C_LABEL(x)	_STRING(x)
 
-#ifdef __GNUC__
-#ifdef __STDC__
-#define __indr_reference(sym,alias)	\
-	__asm__(".stabs \"_" #alias "\",11,0,0,0");	\
-	__asm__(".stabs \"_" #sym "\",1,0,0,0")
-#define __warn_references(sym,msg)	\
-	__asm__(".stabs \"" msg "\",30,0,0,0");		\
-	__asm__(".stabs \"_" #sym "\",1,0,0,0")
-#else
-#define __indr_reference(sym,alias)	\
-	__asm__(".stabs \"_/**/alias\",11,0,0,0");	\
-	__asm__(".stabs \"_/**/sym\",1,0,0,0")
-#define __warn_references(sym,msg)	\
-	__asm__(".stabs msg,30,0,0,0");			\
-	__asm__(".stabs \"_/**/sym\",1,0,0,0")
-#endif
-#endif
+#define	__indr_references(sym,msg)	/* nothing */
+#define	__warn_references(sym,msg)	/* nothing */
 
 #endif /* !_MACHINE_CDEFS_H_ */
