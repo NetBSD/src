8ba8c160dd7c6a3deb35b1701bffb91976048a23
mycroft <mycroft@NetBSD.org>
1995-05-06 19:19:57 +0000

    Define i386_[gs]et_ldt_args here.

--- sys/arch/i386/include/sysarch.h
+++ sys/arch/i386/include/sysarch.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: sysarch.h,v 1.4 1995/03/28 18:17:23 jtc Exp $	*/
+/*	$NetBSD: sysarch.h,v 1.5 1995/05/06 19:19:57 mycroft Exp $	*/
 
 #ifndef _I386_SYSARCH_H_
 #define _I386_SYSARCH_H_
@@ -8,6 +8,18 @@
  */
 #define I386_GET_LDT	0
 #define I386_SET_LDT	1
+
+struct i386_get_ldt_args {
+	int start;
+	union descriptor *desc;
+	int num;
+};
+
+struct i386_set_ldt_args {
+	int start;
+	union descriptor *desc;
+	int num;
+};
 
 #ifndef _KERNEL
 int i386_get_ldt __P((int, union descriptor *, int));
