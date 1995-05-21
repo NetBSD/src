8465d5c10cb186cb704e4170f931b4484d480770
mycroft <mycroft@NetBSD.org>
1995-05-21 13:38:25 +0000

    Split out some helper functions again.

---
+++ bin/ps/fmt.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <vis.h>
+
+void
+fmt_puts(s, leftp)
+	char *s;
+	int *leftp;
+{
+	static char *v = 0, *nv;
+	static int maxlen = 0;
+	int len;
+
+	if (*leftp == 0)
+		return;
+	len = strlen(s) * 4 + 1;
+	if (len > maxlen) {
+		if (maxlen == 0)
+			maxlen = getpagesize();
+		while (len > maxlen)
+			maxlen *= 2;
+		nv = realloc(v, maxlen);
+		if (nv == 0)
+			return;
+		v = nv;
+	}
+	strvis(v, s, VIS_TAB | VIS_NL | VIS_CSTYLE);
+	if (*leftp != -1) {
+		len = strlen(v);
+		if (len > *leftp) {
+			printf("%.*s", *leftp, v);
+			*leftp = 0;
+		} else {
+			printf("%s", v);
+			*leftp -= len;
+		}
+	} else
+		printf("%s", v);
+}
+
+void
+fmt_putc(c, leftp)
+	int c;
+	int *leftp;
+{
+
+	if (*leftp == 0)
+		return;
+	if (*leftp != -1)
+		*leftp -= 1;
+	putchar(c);
+}
