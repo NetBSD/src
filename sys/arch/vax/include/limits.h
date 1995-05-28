aeda7e39bf1a5c0509472b17686724583f3e7e6b
ragge <ragge@NetBSD.org>
1995-05-28 18:38:30 +0000

    Changed *_MIN to be correct.

--- sys/arch/vax/include/limits.h
+++ sys/arch/vax/include/limits.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: limits.h,v 1.4 1995/04/16 20:10:33 ragge Exp $	*/
+/*	$NetBSD: limits.h,v 1.5 1995/05/28 18:38:30 ragge Exp $	*/
 
 /*
  * Copyright (c) 1988 The Regents of the University of California.
@@ -39,24 +39,24 @@
 /* #define CLK_TCK         60              /* ticks per second */
 #define MB_LEN_MAX      1               /* no multibyte characters */
 
-#define SCHAR_MIN       0x80            /* max value for a signed char */
-#define SCHAR_MAX       0x7f            /* min value for a signed char */
+#define SCHAR_MIN       (-0x7f-1)       /* min value for a signed char */
+#define SCHAR_MAX       0x7f            /* max value for a signed char */
 
 #define UCHAR_MAX       0xff            /* max value for an unsigned char */
 #define CHAR_MAX        0x7f            /* max value for a char */
-#define CHAR_MIN        0x80            /* min value for a char */
+#define CHAR_MIN        (-0x7f-1)       /* min value for a char */
 
 #define USHRT_MAX       0xffff          /* max value for an unsigned short */
 #define SHRT_MAX        0x7fff          /* max value for a short */
-#define SHRT_MIN        0x8000          /* min value for a short */
+#define SHRT_MIN        (-0x7fff-1)     /* min value for a short */
 
-#define UINT_MAX        0xffffffff      /* max value for an unsigned int */
+#define UINT_MAX        0xffffffffU     /* max value for an unsigned int */
 #define INT_MAX         0x7fffffff      /* max value for an int */
-#define INT_MIN         0x80000000      /* min value for an int */
+#define INT_MIN         (-0x7fffffff-1) /* min value for an int */
 
-#define ULONG_MAX       0xffffffff      /* max value for an unsigned long */
-#define LONG_MAX        0x7fffffff      /* max value for a long */
-#define LONG_MIN        0x80000000      /* min value for a long */
+#define ULONG_MAX       0xffffffffUL    /* max value for an unsigned long */
+#define LONG_MAX        0x7fffffffL     /* max value for a long */
+#define LONG_MIN        (-0x7fffffffL-1)/* min value for a long */
 
 #if !defined(_ANSI_SOURCE)
 #define SSIZE_MAX       INT_MAX         /* max value for a ssize_t */
