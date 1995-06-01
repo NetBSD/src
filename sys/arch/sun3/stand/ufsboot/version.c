2d7b65e7b6bbd2ce328756090ed7f0320c593796
gwr <gwr@NetBSD.org>
1995-06-01 20:37:44 +0000

    Booting from ufs works now.

---
+++ sys/arch/sun3/stand/ufsboot/version.c
@@ -0,0 +1,9 @@
+/*	$NetBSD: version.c,v 1.1.1.1 1995/06/01 20:38:08 gwr Exp $ */
+
+/*
+ *	NOTE ANY CHANGES YOU MAKE TO THE BOOTBLOCKS HERE.
+ *
+ *	1.1
+ */
+
+char *version = "$Revision: 1.1.1.1 $";
