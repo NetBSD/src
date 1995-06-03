2de9edd5eaae39ddd85ae5fb96aa065e66e2fb2b
mycroft <mycroft@NetBSD.org>
1995-06-03 02:27:17 +0000

    Don't set the rgid or egid.  Document this behavior.  This is compatible with SunOS and SysV.

--- lib/libc/gen/initgroups.c
+++ lib/libc/gen/initgroups.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: initgroups.c,v 1.10 1995/06/02 21:25:09 mycroft Exp $	*/
+/*	$NetBSD: initgroups.c,v 1.11 1995/06/03 02:27:19 mycroft Exp $	*/
 
 /*
  * Copyright (c) 1983, 1993
@@ -37,7 +37,7 @@
 #if 0
 static char sccsid[] = "@(#)initgroups.c	8.1 (Berkeley) 6/4/93";
 #else
-static char rcsid[] = "$NetBSD: initgroups.c,v 1.10 1995/06/02 21:25:09 mycroft Exp $";
+static char rcsid[] = "$NetBSD: initgroups.c,v 1.11 1995/06/03 02:27:19 mycroft Exp $";
 #endif
 #endif /* LIBC_SCCS and not lint */
 
@@ -51,17 +51,13 @@ initgroups(uname, agroup)
 	const char *uname;
 	int agroup;
 {
-	int groups[NGROUPS + 1], ngroups;
+	int groups[NGROUPS], ngroups;
 
-	ngroups = NGROUPS + 1;
+	ngroups = NGROUPS;
 	if (getgrouplist(uname, agroup, groups, &ngroups) < 0)
 		_warnx("%s is in too many groups, using first %d",
 		    uname, ngroups);
-	if (setgid(agroup) < 0) {
-		_warn("setgid");
-		return (-1);
-	}
-	if (setgroups(ngroups - 1, groups + 1) < 0) {
+	if (setgroups(ngroups, groups) < 0) {
 		_warn("setgroups");
 		return (-1);
 	}
