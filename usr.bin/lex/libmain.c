effe85433f52a015a7fabeb668109fdf92081eb3
pk <pk@NetBSD.org>
1995-06-05 19:44:52 +0000

    Prototypes (PR#1102).

--- usr.bin/lex/libmain.c
+++ usr.bin/lex/libmain.c
@@ -1,12 +1,17 @@
 /* libmain - flex run-time support library "main" function */
 
-/* $Header: /cvsroot/src/usr.bin/lex/Attic/libmain.c,v 1.2 1995/05/05 05:35:31 jtc Exp $ */
+/* $Header: /cvsroot/src/usr.bin/lex/Attic/libmain.c,v 1.3 1995/06/05 19:44:52 pk Exp $ */
 
-extern int yylex();
+#include <sys/cdefs.h>
 
-int main( argc, argv )
+int yylex __P((void));
+int main __P((int, char **, char **));
+
+int
+main( argc, argv, envp )
 int argc;
 char *argv[];
+char *envp[];
 	{
 	return yylex();
 	}
