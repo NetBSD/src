d916f5b381faa0f71eaef483f6da05ec21415249
fvdl <fvdl@NetBSD.org>
1995-06-11 15:15:09 +0000

    Don't forget to acknowledge that all the ELF stuff was taken from Christos' svr4 code.

--- sys/compat/linux/common/linux_exec_aout.c
+++ sys/compat/linux/common/linux_exec_aout.c
@@ -1,7 +1,8 @@
-/*	$NetBSD: linux_exec_aout.c,v 1.6 1995/06/11 14:56:47 fvdl Exp $	*/
+/*	$NetBSD: linux_exec_aout.c,v 1.7 1995/06/11 15:15:09 fvdl Exp $	*/
 
 /*
  * Copyright (c) 1995 Frank van der Linden
+ * Copyright (c) 1994 Christos Zoulas
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -12,11 +13,7 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *      This product includes software developed for the NetBSD Project
- *      by Frank van der Linden
- * 4. The name of the author may not be used to endorse or promote products
+ * 3. The name of the author may not be used to endorse or promote products
  *    derived from this software without specific prior written permission
  *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
@@ -30,7 +27,7 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
- * based on kern/exec_aout.c and compat/sunos/sunos_exec.c
+ * based on exec_aout.c, sunos_exec.c and svr4_exec.c
  */
 
 #include <sys/param.h>
