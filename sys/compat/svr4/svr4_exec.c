741c4a8f909a3ff7e09f64c650ca11a82e8af0d3
fvdl <fvdl@NetBSD.org>
1995-06-22 21:32:57 +0000

    Changed to use generic ELF code from kern/exec_elf.c (which was taken from the svr4 exec code plus the linux compat mods)

--- sys/compat/svr4/svr4_exec.c
+++ sys/compat/svr4/svr4_exec.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: svr4_exec.c,v 1.13 1995/06/11 15:06:11 fvdl Exp $	 */
+/*	$NetBSD: svr4_exec.c,v 1.14 1995/06/22 21:32:57 fvdl Exp $	 */
 
 /*
  * Copyright (c) 1994 Christos Zoulas
@@ -29,23 +29,17 @@
 
 #include <sys/param.h>
 #include <sys/systm.h>
-#include <sys/filedesc.h>
 #include <sys/kernel.h>
 #include <sys/proc.h>
-#include <sys/mount.h>
 #include <sys/malloc.h>
 #include <sys/namei.h>
 #include <sys/vnode.h>
-#include <sys/file.h>
-#include <sys/resourcevar.h>
-#include <sys/wait.h>
+#include <sys/exec_elf.h>
 
 #include <sys/mman.h>
 #include <vm/vm.h>
 #include <vm/vm_param.h>
 #include <vm/vm_map.h>
-#include <vm/vm_kern.h>
-#include <vm/vm_pager.h>
 
 #include <machine/cpu.h>
 #include <machine/reg.h>
@@ -56,41 +50,8 @@
 #include <compat/svr4/svr4_syscall.h>
 #include <compat/svr4/svr4_exec.h>
 
-#define SVR4_ALIGN(a, b) ((a) & ~((b) - 1))
-#define SVR4_PAGESIZE 4096
-#ifdef SVR4_COMPAT_SOLARIS2
-# define SVR4_AUX_ARGSIZ (sizeof(AuxInfo) * 12 / sizeof(char *))
-#else
-# define SVR4_AUX_ARGSIZ (sizeof(AuxInfo) * 8 / sizeof(char *))
-#endif
-
-/*
- * I know this is horrible, but I cannot load the interpreter after
- * the data segment because brk(2) breaks. I have to load it somewhere
- * before. Programs start at 0x08000000 so I load the interpreter far
- * before.
- */
-#define SVR4_INTERP_ADDR	0x01000000
-
-struct svr4_args {
-	u_long	arg_entry;	/* progran entry point */
-	u_long	arg_interp;	/* Interpreter load address */
-	u_long	arg_phaddr;	/* program header address */
-	u_long	arg_phentsize;	/* Size of program header */
-	u_long	arg_phnum;	/* Number of program headers */
-};
-
 static void *svr4_copyargs __P((struct exec_package *, struct ps_strings *,
 			       void *, void *));
-static int svr4_check_header __P((Elf32_Ehdr *, int));
-static void svr4_load_psection	__P((struct exec_package *, struct vnode *,
-		                     Elf32_Phdr *, u_long *, u_long *, int *));
-static int svr4_set_segment __P((struct exec_package *, u_long, u_long, int));
-static int svr4_read_from __P((struct vnode *, u_long, struct proc *, caddr_t,
-			       int));
-static int svr4_load_interp __P((struct proc *, char *, struct exec_package *,
-				 struct svr4_args *, u_long * last));
-
 extern int svr4_error[];
 extern struct sysent svr4_sysent[];
 extern char *svr4_syscallnames[];
@@ -110,50 +71,6 @@ struct emul emul_svr4 = {
 	svr4_esigcode,
 };
 
-#ifdef DEBUG_EXEC_SVR4
-static void
-print_Ehdr(e)
-	Elf32_Ehdr     *e;
-{
-	printf("e_ident %s, ", e->e_ident);
-	printf("e_type %d, ", e->e_type);
-	printf("e_machine %d, ", e->e_machine);
-	printf("e_version %ld, ", e->e_version);
-	printf("e_entry %lx, ", e->e_entry);
-	printf("e_phoff %lx, ", e->e_phoff);
-	printf("e_shoff %lx, ", e->e_shoff);
-	printf("e_flags %lx, ", e->e_flags);
-	printf("e_ehsize %d, ", e->e_ehsize);
-	printf("e_phentsize %d, ", e->e_phentsize);
-	printf("e_phnum %d, ", e->e_phnum);
-	printf("e_shentsize %d, ", e->e_shentsize);
-	printf("e_shnum %d, ", e->e_shnum);
-	printf("e_shstrndx %d\n", e->e_shstrndx);
-}
-
-
-static void
-print_Phdr(p)
-	Elf32_Phdr     *p;
-{
-	static char    *types[] =
-	{
-		"null", "load", "dynamic", "interp",
-		"note", "shlib", "phdr", "entry7"
-	};
-
-	printf("p_type %ld [%s], ", p->p_type, types[p->p_type & 7]);
-	printf("p_offset %lx, ", p->p_offset);
-	printf("p_vaddr %lx, ", p->p_vaddr);
-	printf("p_paddr %lx, ", p->p_paddr);
-	printf("p_filesz %ld, ", p->p_filesz);
-	printf("p_memsz %ld, ", p->p_memsz);
-	printf("p_flags %lx, ", p->p_flags);
-	printf("p_align %ld\n", p->p_align);
-}
-#endif
-
-
 static void *
 svr4_copyargs(pack, arginfo, stack, argp)
 	struct exec_package *pack;
@@ -161,87 +78,13 @@ svr4_copyargs(pack, arginfo, stack, argp)
 	void *stack;
 	void *argp;
 {
-	char **cpp = stack;
-	char *dp, *sp;
-	size_t len;
-	void *nullp = NULL;
-	int argc = arginfo->ps_nargvstr;
-	int envc = arginfo->ps_nenvstr;
 	AuxInfo *a;
-	struct svr4_args *ap;
+	struct elf_args *ap;
 
-	if (copyout(&argc, cpp++, sizeof(argc)))
+	if (!(a = (AuxInfo *) elf_copyargs(pack, arginfo, stack, argp)))
 		return NULL;
-
-	dp = (char *) (cpp + argc + envc + 2 + pack->ep_emul->e_arglen);
-	sp = argp;
-
-	/* XXX don't copy them out, remap them! */
-	arginfo->ps_argvstr = cpp; /* remember location of argv for later */
-
-	for (; --argc >= 0; sp += len, dp += len)
-		if (copyout(&dp, cpp++, sizeof(dp)) ||
-		    copyoutstr(sp, dp, ARG_MAX, &len))
-			return NULL;
-
-	if (copyout(&nullp, cpp++, sizeof(nullp)))
-		return NULL;
-
-	arginfo->ps_envstr = cpp; /* remember location of envp for later */
-
-	for (; --envc >= 0; sp += len, dp += len)
-		if (copyout(&dp, cpp++, sizeof(dp)) ||
-		    copyoutstr(sp, dp, ARG_MAX, &len))
-			return NULL;
-
-	if (copyout(&nullp, cpp++, sizeof(nullp)))
-		return NULL;
-
-	/*
-	 * Push extra arguments on the stack needed by dynamically
-	 * linked binaries
-	 */
-	a = (AuxInfo *) cpp;
-	if ((ap = (struct svr4_args *) pack->ep_emul_arg)) {
-
-		DPRINTF(("phaddr=0x%x, phsize=%d, phnum=%d, interp=0x%x, ",
-			 ap->arg_phaddr, ap->arg_phentsize, ap->arg_phnum,
-			 ap->arg_interp));
-		DPRINTF((" entry=0x%x\n", ap->arg_entry));
-
-		a->au_id = AUX_phdr;
-		a->au_v = ap->arg_phaddr;
-		a++;
-
-		a->au_id = AUX_phent;
-		a->au_v = ap->arg_phentsize;
-		a++;
-
-		a->au_id = AUX_phnum;
-		a->au_v = ap->arg_phnum;
-		a++;
-
-		a->au_id = AUX_pagesz;
-		a->au_v = SVR4_PAGESIZE;
-		a++;
-
-		a->au_id = AUX_base;
-		a->au_v = ap->arg_interp;
-		a++;
-
-		a->au_id = AUX_flags;
-		a->au_v = 0;
-		a++;
-
-		a->au_id = AUX_entry;
-		a->au_v = ap->arg_entry;
-		a++;
-
-		a->au_id = AUX_null;
-		a->au_v = 0;
-		a++;
-
 #ifdef SVR4_COMPAT_SOLARIS2
+	if (pack->ep_emul_arg) {
 		a->au_id = AUX_sun_uid;
 		a->au_v = p->p_ucred->cr_uid;
 		a++;
@@ -257,467 +100,30 @@ svr4_copyargs(pack, arginfo, stack, argp)
 		a->au_id = AUX_sun_rgid;
 		a->au_v = p->p_cred->rgid;
 		a++;
-#endif
-		free((char *) ap, M_TEMP);
 	}
+#endif
 	return a;
 }
 
-
-/*
- * svr4_check_header():
- *
- * Check header for validity; return 0 of ok ENOEXEC if error
- */
-static int
-svr4_check_header(eh, type)
-	Elf32_Ehdr     *eh;
-	int             type;
-{
-#ifdef sparc
-  /* #$%@#$%@#$%! */
-# define memcmp bcmp
-#endif
-	if (memcmp(eh->e_ident, Elf32_e_ident, Elf32_e_siz) != 0) {
-		DPRINTF(("Not an elf file\n"));
-		return ENOEXEC;
-	}
-
-	switch (eh->e_machine) {
-#ifdef i386
-	case Elf32_em_386:
-	case Elf32_em_486:
-#endif
-#ifdef sparc
-	case Elf32_em_sparc:
-#endif
-		break;
-
-	default:
-		DPRINTF(("Unsupported elf machine type %d\n", eh->e_machine));
-		return ENOEXEC;
-	}
-
-	if (eh->e_type != type) {
-		DPRINTF(("Not an elf executable\n"));
-		return ENOEXEC;
-	}
-
-	return 0;
-}
-
-
-/*
- * svr4_load_psection():
- * 
- * Load a psection at the appropriate address
- */
-static void
-svr4_load_psection(epp, vp, ph, addr, size, prot)
-	struct exec_package	*epp;
-	struct vnode		*vp;
-	Elf32_Phdr		*ph;
-	u_long			*addr;
-	u_long			*size;
-	int			*prot;
-{
-	u_long	uaddr;
-	long	diff;
-	long	offset;
-	u_long	msize;
-
-	/*
-         * If the user specified an address, then we load there.
-         */
-	if (*addr != ~0) {
-		uaddr = *addr + ph->p_align;
-		*addr = SVR4_ALIGN(uaddr, ph->p_align);
-		uaddr = SVR4_ALIGN(ph->p_vaddr, ph->p_align);
-		diff = ph->p_vaddr - uaddr;
-	} else {
-		uaddr = ph->p_vaddr;
-		*addr = SVR4_ALIGN(uaddr, ph->p_align);
-		diff = uaddr - *addr;
-	}
-
-	*prot |= (ph->p_flags & Elf32_pf_r) ? VM_PROT_READ : 0;
-	*prot |= (ph->p_flags & Elf32_pf_w) ? VM_PROT_WRITE : 0;
-	*prot |= (ph->p_flags & Elf32_pf_x) ? VM_PROT_EXECUTE : 0;
-
-	offset = ph->p_offset - diff;
-	*size = ph->p_filesz + diff;
-	msize = ph->p_memsz + diff;
-
-	DPRINTF(("Elf Seg@ 0x%x/0x%x sz %d/%d off 0x%x/0x%x[%d] algn 0x%x\n",
-		 ph->p_vaddr, *addr, *size, msize, ph->p_offset, offset,
-		 diff, ph->p_align));
-
-	NEW_VMCMD(&epp->ep_vmcmds, vmcmd_map_readvn, *size,
-		  *addr, vp, offset, *prot);
-
-	/*
-         * Check if we need to extend the size of the segment
-         */
-	{
-		u_long	rm = round_page(*addr + msize);
-		u_long	rf = round_page(*addr + *size);
-		if (rm != rf) {
-			DPRINTF(("zeropad 0x%x-0x%x\n", rf, rm));
-			NEW_VMCMD(&epp->ep_vmcmds, vmcmd_map_zero, rm - rf,
-				  rf, NULLVP, 0, *prot);
-			*size = msize;
-		}
-	}
-}
-
-
-/*
- * svr4_set_segment():
- *
- * Decide if the segment is text or data, depending on the protection
- * and set it appropriately
- */
-static int
-svr4_set_segment(epp, vaddr, size, prot)
-	struct exec_package	*epp;
-	u_long			 vaddr;
-	u_long			 size;
-	int			 prot;
-{
-	/*
-         * Kludge: Unfortunately the current implementation of
-         * exec package assumes a single text and data segment.
-         * In Elf we can have more, but here we limit ourselves
-         * to two and hope :-(
-         * We also assume that the text is r-x, and data is rwx.
-         */
-	switch (prot) {
-	case (VM_PROT_READ | VM_PROT_EXECUTE):
-		if (epp->ep_tsize != ~0) {
-			DPRINTF(("More than one text segment\n"));
-			return ENOEXEC;
-		}
-		epp->ep_taddr = vaddr;
-		epp->ep_tsize = size;
-		DPRINTF(("Elf Text@ 0x%x, size %d\n", vaddr, size));
-		break;
-
-	case (VM_PROT_READ | VM_PROT_WRITE):
-	case (VM_PROT_READ | VM_PROT_WRITE | VM_PROT_EXECUTE):
-		if (epp->ep_dsize != ~0) {
-			DPRINTF(("More than one data segment\n"));
-			return ENOEXEC;
-		}
-		epp->ep_daddr = vaddr;
-		epp->ep_dsize = size;
-
-		DPRINTF(("Elf Data@ 0x%x, size %d\n", vaddr, size));
-		break;
-
-	default:
-		DPRINTF(("Bad protection 0%o\n", prot));
-		return ENOEXEC;
-	}
-	return 0;
-}
-
-
-/*
- * svr4_read_from():
- *
- *	Read from vnode into buffer at offset.
- */
-static int
-svr4_read_from(vp, off, p, buf, size)
-	struct vnode	*vp;
-	u_long		 off;
-	struct proc	*p;
-	caddr_t		 buf;
-	int		 size;
-{
-	int	error;
-	int	resid;
-
-	DPRINTF(("read from 0x%x to 0x%x size %d\n",
-		 off, buf, size));
-	if ((error = vn_rdwr(UIO_READ, vp, buf, size,
-			     off, UIO_SYSSPACE, IO_NODELOCKED, p->p_ucred,
-			     &resid, p)) != 0) {
-		DPRINTF(("Bad read error %d\n", error));
-		return error;
-	}
-	/*
-         * See if we got all of it
-         */
-	if (resid != 0) {
-		DPRINTF(("Incomplete read for header ask=%d, rem=%d\n",
-			 size, resid));
-		return error;
-	}
-	return 0;
-}
-
-
-/*
- * svr4_load_interp():
- *
- * Load an interpreter pointed to by path [stolen from coff_load_shlib()]
- */
-static int
-svr4_load_interp(p, path, epp, ap, last)
-	struct proc		*p;
-	char			*path;
-	struct exec_package	*epp;
-	struct svr4_args	*ap;
-	u_long			*last;
-{
-	int			 error, i;
-	struct nameidata	 nd;
-	Elf32_Ehdr		 eh;
-	Elf32_Phdr		*ph = NULL;
-	u_long			 phsize;
-	char			*bp = NULL;
-	u_long			 addr = *last;
-
-	DPRINTF(("Loading interpreter %s\n", path));
-
-	if ((error = svr4_emul_find(p, NULL, svr4_emul_path, path, &bp)) != 0)
-		bp = NULL;
-	else
-		path = bp;
-	/*
-         * 1. open interp file
-         * 2. read filehdr
-         * 3. map text, data, and bss out of it using VM_*
-         */
-	NDINIT(&nd, LOOKUP, FOLLOW, UIO_SYSSPACE, path, p);
-	/* first get the vnode */
-	if ((error = namei(&nd)) != 0) {
-		if (bp != NULL)
-			free((char *) bp, M_TEMP);
-		return error;
-	}
-	if ((error = svr4_read_from(nd.ni_vp, 0, p, (caddr_t) &eh,
-				    sizeof(eh))) != 0)
-		goto bad;
-
-#ifdef DEBUG_EXEC_SVR4
-	print_Ehdr(&eh);
-#endif
-
-	if ((error = svr4_check_header(&eh, Elf32_et_dyn)) != 0)
-		goto bad;
-
-	phsize = eh.e_phnum * sizeof(Elf32_Phdr);
-	ph = (Elf32_Phdr *) malloc(phsize, M_TEMP, M_WAITOK);
-
-	if ((error = svr4_read_from(nd.ni_vp, eh.e_phoff, p,
-				    (caddr_t) ph, phsize)) != 0)
-		goto bad;
-
-	/*
-         * Load all the necessary sections
-         */
-	for (i = 0; i < eh.e_phnum; i++) {
-		u_long	size = 0;
-		int	prot = 0;
-#ifdef DEBUG_EXEC_SVR4
-		print_Phdr(&ph[i]);
-#endif
-
-		switch (ph[i].p_type) {
-		case Elf32_pt_load:
-			svr4_load_psection(epp, nd.ni_vp, &ph[i], &addr, &size,
-					   &prot);
-			/* Assume that the text segment is r-x only */
-			if ((prot & PROT_WRITE) == 0) {
-				epp->ep_entry = addr + eh.e_entry;
-				ap->arg_interp = addr;
-				DPRINTF(("Interpreter@ 0x%x\n", addr));
-			}
-			addr += size;
-			break;
-
-		case Elf32_pt_dynamic:
-		case Elf32_pt_phdr:
-		case Elf32_pt_note:
-			break;
-
-		default:
-			DPRINTF(("interp: Unexpected program header type %d\n",
-				 ph[i].p_type));
-			break;
-		}
-	}
-
-bad:
-	if (ph != NULL)
-		free((char *) ph, M_TEMP);
-	if (bp != NULL)
-		free((char *) bp, M_TEMP);
-
-	*last = addr;
-	vrele(nd.ni_vp);
-	return error;
-}
-
-
-/*
- * exec_svr4_elf_makecmds(): Prepare an Elf binary's exec package
- *
- * First, set of the various offsets/lengths in the exec package.
- *
- * Then, mark the text image busy (so it can be demand paged) or error
- * out if this is not possible.  Finally, set up vmcmds for the
- * text, data, bss, and stack segments.
- */
 int
-exec_svr4_elf_makecmds(p, epp)
-	struct proc		*p;
-	struct exec_package	*epp;
+svr4_elf_probe(p, epp, itp, pos)
+	struct proc *p;
+	struct exec_package *epp;
+	char *itp;
+	u_long *pos;
 {
-	Elf32_Ehdr     *eh = epp->ep_hdr;
-	Elf32_Phdr     *ph;
-	int             error;
-	int             i;
-	char            interp[MAXPATHLEN];
-	u_long          pos = 0;
-	u_long          phsize;
+	char *bp;
+	int error;
+	size_t len;
 
-#ifdef DEBUG_EXEC_SVR4
-	print_Ehdr(eh);
-#endif
-	if (epp->ep_hdrvalid < sizeof(Elf32_Ehdr))
-		return ENOEXEC;
-
-	if (svr4_check_header(eh, Elf32_et_exec))
-		return ENOEXEC;
-
-	/*
-         * check if vnode is in open for writing, because we want to
-         * demand-page out of it.  if it is, don't do it, for various
-         * reasons
-         */
-	if (epp->ep_vp->v_writecount != 0) {
-#ifdef DIAGNOSTIC
-		if (epp->ep_vp->v_flag & VTEXT)
-			panic("exec: a VTEXT vnode has writecount != 0\n");
-#endif
-		return ETXTBSY;
+	if (itp[0]) {
+		if ((error = svr4_emul_find(p, NULL, svr4_emul_path, itp, &bp)))
+			return error;
+		if ((error = copystr(bp, itp, MAXPATHLEN, &len)))
+			return error;
+		free(bp, M_TEMP);
 	}
-	/*
-         * Allocate space to hold all the program headers, and read them
-         * from the file
-         */
-	phsize = eh->e_phnum * sizeof(Elf32_Phdr);
-	ph = (Elf32_Phdr *) malloc(phsize, M_TEMP, M_WAITOK);
-
-	if ((error = svr4_read_from(epp->ep_vp, eh->e_phoff, p,
-				    (caddr_t) ph, phsize)) != 0)
-		goto bad;
-
-	epp->ep_tsize = ~0;
-	epp->ep_dsize = ~0;
-
-	interp[0] = '\0';
-
-	/*
-         * Load all the necessary sections
-         */
-	for (i = 0; i < eh->e_phnum; i++) {
-		u_long          addr = ~0, size = 0;
-		int             prot = 0;
-		Elf32_Phdr     *pp = &ph[i];
-#ifdef DEBUG_EXEC_SVR4
-		print_Phdr(pp);
-#endif
-
-		switch (ph[i].p_type) {
-		case Elf32_pt_load:
-			svr4_load_psection(epp, epp->ep_vp, &ph[i], &addr,
-					   &size, &prot);
-			if ((error = svr4_set_segment(epp, addr, size,
-						      prot)) != 0)
-				goto bad;
-			break;
-
-		case Elf32_pt_shlib:
-			DPRINTF(("No support for COFF libraries (yet)\n"));
-			error = ENOEXEC;
-			goto bad;
-
-		case Elf32_pt_interp:
-			if (pp->p_filesz >= sizeof(interp)) {
-				DPRINTF(("Interpreter path too long %d\n",
-					 pp->p_filesz));
-				goto bad;
-			}
-			if ((error = svr4_read_from(epp->ep_vp, pp->p_offset, p,
-				      (caddr_t) interp, pp->p_filesz)) != 0)
-				goto bad;
-			break;
-
-		case Elf32_pt_dynamic:
-		case Elf32_pt_phdr:
-		case Elf32_pt_note:
-			break;
-
-		default:
-			/*
-			 * Not fatal, we don't need to understand everything
-			 * :-)
-			 */
-			DPRINTF(("Unsupported program header type %d\n",
-				 pp->p_type));
-			break;
-		}
-	}
-
-	/*
-         * Check if we found a dynamically linked binary and arrange to load
-         * it's interpreter
-         */
-	if (interp[0]) {
-		struct svr4_args *ap;
-		pos = SVR4_INTERP_ADDR;
-
-		ap = (struct svr4_args *) malloc(sizeof(struct svr4_args),
-						 M_TEMP, M_WAITOK);
-		if ((error = svr4_load_interp(p, interp, epp, ap, &pos)) != 0) {
-			free((char *) ap, M_TEMP);
-			goto bad;
-		}
-		/* Arrange to load the program headers. */
-		pos = SVR4_ALIGN(pos + SVR4_PAGESIZE, SVR4_PAGESIZE);
-		DPRINTF(("Program header @0x%x\n", pos));
-		ap->arg_phaddr = pos;
-		NEW_VMCMD(&epp->ep_vmcmds, vmcmd_map_readvn, phsize,
-			  pos, epp->ep_vp, eh->e_phoff,
-			  VM_PROT_READ | VM_PROT_WRITE | VM_PROT_EXECUTE);
-		pos += phsize;
-
-		ap->arg_phentsize = eh->e_phentsize;
-		ap->arg_phnum = eh->e_phnum;
-		ap->arg_entry = eh->e_entry;
-
-		epp->ep_emul_arg = ap;
-	} else
-		epp->ep_entry = eh->e_entry;
-
-	DPRINTF(("taddr 0x%x tsize 0x%x daddr 0x%x dsize 0x%x\n",
-	       epp->ep_taddr, epp->ep_tsize, epp->ep_daddr, epp->ep_dsize));
-
-	free((char *) ph, M_TEMP);
-
-	DPRINTF(("Elf entry@ 0x%x\n", epp->ep_entry));
-	epp->ep_vp->v_flag |= VTEXT;
-
 	epp->ep_emul = &emul_svr4;
-
-	return exec_aout_setup_stack(p, epp);
-
-bad:
-	free((char *) ph, M_TEMP);
-	kill_vmcmds(&epp->ep_vmcmds);
-	return ENOEXEC;
+	*pos = SVR4_INTERP_ADDR;
+	return 0;
 }
--- sys/compat/svr4/svr4_exec.h
+++ sys/compat/svr4/svr4_exec.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: svr4_exec.h,v 1.5 1994/10/29 00:43:17 christos Exp $	 */
+/*	$NetBSD: svr4_exec.h,v 1.6 1995/06/22 21:33:00 fvdl Exp $	 */
 
 /*
  * Copyright (c) 1994 Christos Zoulas
@@ -30,110 +30,21 @@
 #ifndef	_SVR4_EXEC_H_
 #define	_SVR4_EXEC_H_
 
-typedef unsigned long	Elf32_Addr;
-typedef unsigned long	Elf32_Off;
-typedef long		Elf32_Sword;
-typedef long		Elf32_Word;
-typedef unsigned short	Elf32_Half;
+#ifdef SVR4_COMPAT_SOLARIS2
+# define SVR4_AUX_ARGSIZ (sizeof(AuxInfo) * 12 / sizeof(char *))
+#else
+# define SVR4_AUX_ARGSIZ (sizeof(AuxInfo) * 8 / sizeof(char *))
+#endif
 
+/*
+ * I know this is horrible, but I cannot load the interpreter after
+ * the data segment because brk(2) breaks. I have to load it somewhere
+ * before. Programs start at 0x08000000 so I load the interpreter far
+ * before.
+ */
+#define SVR4_INTERP_ADDR	0x01000000
 
-#define ELF_IDSIZE	16
-
-enum Elf32_e_type {
-	Elf32_et_none = 0,
-	Elf32_et_rel,
-	Elf32_et_exec,
-	Elf32_et_dyn,
-	Elf32_et_core,
-	Elf32_et_num
-};
-
-enum Elf32_e_machine {
-	Elf32_em_none = 0,
-	Elf32_em_m32,
-	Elf32_em_sparc,
-	Elf32_em_386,
-	Elf32_em_68k,
-	Elf32_em_88k,
-	Elf32_em_486,
-	Elf32_em_860,
-	Elf32_em_num
-};
-
-typedef struct {
-	unsigned char	e_ident[ELF_IDSIZE];	/* Id bytes */
-	Elf32_Half	e_type;			/* file type */
-	Elf32_Half	e_machine;		/* machine type */
-	Elf32_Word	e_version;		/* version number */
-	Elf32_Addr	e_entry;		/* entry point */
-	Elf32_Off	e_phoff;		/* Program hdr offset */
-	Elf32_Off	e_shoff;		/* Section hdr offset */
-	Elf32_Word	e_flags;		/* Processor flags */
-	Elf32_Half      e_ehsize;		/* sizeof ehdr */
-	Elf32_Half      e_phentsize;		/* Program header entry size */
-	Elf32_Half      e_phnum;		/* Number of program headers */
-	Elf32_Half      e_shentsize;		/* Section header entry size */
-	Elf32_Half      e_shnum;		/* Number of section headers */
-	Elf32_Half      e_shstrndx;		/* String table index */
-} Elf32_Ehdr;
-
-
-enum Elf32_p_pf {
-	Elf32_pf_r = 4,
-	Elf32_pf_w = 2,
-	Elf32_pf_x = 1
-};
-
-enum Elf32_p_pt {
-	Elf32_pt_nulli = 0,		/* Program header table entry unused */
-	Elf32_pt_load = 1,		/* Loadable program segment */
-	Elf32_pt_dynamic = 2,		/* Dynamic linking information */
-	Elf32_pt_interp = 3,		/* Program interpreter */
-	Elf32_pt_note = 4,		/* Auxiliary information */
-	Elf32_pt_shlib = 5,		/* Reserved, unspecified semantics */
-	Elf32_pt_phdr = 6,		/* Entry for header table itself */
-	Elf32_pt_loproc = 0x70000000,	/* Processor-specific */
-	Elf32_pt_hiproc = 0x7FFFFFFF	/* Processor-specific */
-
-};
-
-typedef struct {
-	Elf32_Word	p_type;		/* entry type */
-	Elf32_Off	p_offset;	/* offset */
-	Elf32_Addr	p_vaddr;	/* virtual address */
-	Elf32_Addr	p_paddr;	/* physical address */
-	Elf32_Word	p_filesz;	/* file size */
-	Elf32_Word	p_memsz;	/* memory size */
-	Elf32_Word	p_flags;	/* flags */
-	Elf32_Word	p_align;	/* memory & file alignment */
-} Elf32_Phdr;
-
-#define Elf32_e_ident "\177ELF"
-#define Elf32_e_siz (sizeof(Elf32_e_ident) - 1)
-
-typedef struct {
-	int		au_id;
-	unsigned long	au_v;
-} AuxInfo;
-
-enum AuxID {
-	AUX_null = 0,
-	AUX_ignore = 1,
-	AUX_execfd = 2,
-	AUX_phdr = 3,		/* &phdr[0] */
-	AUX_phent = 4,		/* sizeof(phdr[0]) */
-	AUX_phnum = 5,		/* # phdr entries */
-	AUX_pagesz = 6,		/* PAGESIZE */
-	AUX_base = 7,		/* ld.so base addr */
-	AUX_flags = 8,		/* processor flags */
-	AUX_entry = 9,		/* a.out entry */
-	AUX_sun_uid = 2000,	/* euid */
-	AUX_sun_ruid = 2001,	/* ruid */
-	AUX_sun_gid = 2002,	/* egid */
-	AUX_sun_rgid = 2003	/* rgid */
-};
-
-#define	ELF_HDR_SIZE	(sizeof(Elf32_Ehdr))
-int exec_svr4_elf_makecmds __P((struct proc *, struct exec_package *));
+int svr4_elf_probe __P((struct proc *p, struct exec_package *, char *,
+	u_long *pos));
 
 #endif /* !_SVR4_EXEC_H_ */
