64a04b32ac34e5fa3473d49c0065c22b80b15f70
jtc <jtc>
1995-06-26 23:04:45 +0000

    Strip out unneeded header files

--- lib/libc/termios/tcgetpgrp.c
+++ lib/libc/termios/tcgetpgrp.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: tcgetpgrp.c,v 1.1 1995/04/25 00:04:43 jtc Exp $	*/
+/*	$NetBSD: tcgetpgrp.c,v 1.2 1995/06/26 23:06:00 jtc Exp $	*/
 
 /*-
  * Copyright (c) 1989, 1993
@@ -37,20 +37,13 @@
 #if 0
 static char sccsid[] = "@(#)termios.c	8.2 (Berkeley) 2/21/94";
 #else
-static char rcsid[] = "$NetBSD: tcgetpgrp.c,v 1.1 1995/04/25 00:04:43 jtc Exp $";
+static char rcsid[] = "$NetBSD: tcgetpgrp.c,v 1.2 1995/06/26 23:06:00 jtc Exp $";
 #endif
 #endif /* LIBC_SCCS and not lint */
 
 #include <sys/types.h>
 #include <sys/ioctl.h>
-#include <sys/tty.h>
-#include <sys/time.h>
-#include <sys/fcntl.h>
-
-#include <errno.h>
-#include <stdio.h>
 #include <termios.h>
-#include <unistd.h>
 
 pid_t
 tcgetpgrp(fd)
