6b41e85809c3b58ed1e838e42fc5ae3e0e3fa649
gwr <gwr@NetBSD.org>
1995-06-27 14:38:22 +0000

    Remove temporary __BDEVSW_DUMP_OLD_TYPE  (our dumpsys is safe 8^)

--- sys/arch/sun3/include/types.h
+++ sys/arch/sun3/include/types.h
@@ -1,10 +1,8 @@
-/*	$NetBSD: types.h,v 1.6 1995/06/26 05:13:59 cgd Exp $	*/
+/*	$NetBSD: types.h,v 1.7 1995/06/27 14:38:22 gwr Exp $	*/
 
 #ifndef _MACHINE_TYPES_H_
 #define _MACHINE_TYPES_H_
 
 #include <m68k/types.h>
-
-#define	__BDEVSW_DUMP_OLD_TYPE
 
 #endif
