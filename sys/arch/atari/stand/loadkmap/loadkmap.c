9281c02c0bde1461fbc90dfeb17d37eb29b958b9
leo <leo@NetBSD.org>
1995-07-24 05:47:48 +0000

    Added -f option to reload system-wide keymap (instead of current screen one).

--- sys/arch/atari/stand/loadkmap/loadkmap.c
+++ sys/arch/atari/stand/loadkmap/loadkmap.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: loadkmap.c,v 1.1.1.1 1995/07/03 20:49:40 leo Exp $	*/
+/*	$NetBSD: loadkmap.c,v 1.2 1995/07/24 05:47:48 leo Exp $	*/
 
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -8,7 +8,7 @@
 #include <stdio.h>
 
 
-void load_kmap __P((const char *));
+void load_kmap __P((const char *, int));
 void dump_kmap(); 
 
 int
@@ -16,52 +16,59 @@ main(argc, argv)
      int argc;
      char *argv[];
 {
-  if (argc > 2)
-    {
-      fprintf (stderr, "%s keymap\n", argv[0]);
-      exit (1);
-    }
+	int	set_sysmap = 0;
+	char	*mapfile;
 
-  if (argc == 1)
-    dump_kmap ();
-  else
-    load_kmap (argv[1]);
+	if (argc > 2) {
+		if ((argc == 3) && !strcmp(argv[1], "-f")) {
+			mapfile = argv[2];
+			set_sysmap = 1;
+		}
+		else {
+			fprintf(stderr, "%s [-f] keymap\n", argv[0]);
+			exit(1);
+		}
+	}
+	else mapfile = argv[1];
 
-  exit (0);
+	if (argc == 1)
+		dump_kmap();
+	else load_kmap(mapfile, set_sysmap);
+
+	exit (0);
 }
 
 
 void
-load_kmap (file)
-     const char *file;
+load_kmap(file, set_sysmap)
+const char	*file;
+int		set_sysmap;
 {
-  int fd;
-  char buf[sizeof (struct kbdmap)];
+	int	fd;
+	char	buf[sizeof (struct kbdmap)];
+	int	ioc;
 
-  if ((fd = open (file, 0)) >= 0)
-    {
-      if (read (fd, buf, sizeof (buf)) == sizeof (buf))
-	{
-	  if (ioctl (0, ITEIOCSKMAP, buf) == 0)
-	    return;
-	  else
-	    perror ("ITEIOCSKMAP");
+	ioc = set_sysmap ? ITEIOCSSKMAP : ITEIOCSKMAP;
+	
+	if ((fd = open (file, 0)) >= 0) {
+		if (read (fd, buf, sizeof (buf)) == sizeof (buf)) {
+			if (ioctl (0, ioc, buf) == 0)
+				return;
+			else perror("ITEIOCSKMAP");
+		}
+		else perror("read kmap");
+
+		close(fd);
 	}
-      else
-	perror ("read kmap");
-
-      close (fd);
-    }
-  else
-    perror ("open kmap");
+	else perror("open kmap");
 }
 
 void
 dump_kmap()
 {
-  char buf[sizeof (struct kbdmap)];
-  if (ioctl (0, ITEIOCGKMAP, buf) == 0)
-    write (1, buf, sizeof (buf));
-  else
-    perror ("ITEIOCGKMAP");
+	char buf[sizeof (struct kbdmap)];
+
+	if (ioctl (0, ITEIOCGKMAP, buf) == 0)
+		write (1, buf, sizeof (buf));
+	else perror ("ITEIOCGKMAP");
 }
