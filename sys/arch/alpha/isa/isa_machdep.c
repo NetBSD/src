f7d12d5ecd7b1ac6e1c7c07d3d746c7fa3feb160
cgd <cgd@NetBSD.org>
1995-08-03 01:23:08 +0000

    oops; trim some bogus 1994's from copyrights

--- sys/arch/alpha/isa/isa_machdep.c
+++ sys/arch/alpha/isa/isa_machdep.c
@@ -1,7 +1,7 @@
-/*	$NetBSD: isa_machdep.c,v 1.1 1995/06/28 01:24:59 cgd Exp $	*/
+/*	$NetBSD: isa_machdep.c,v 1.2 1995/08/03 01:23:08 cgd Exp $	*/
 
 /*
- * Copyright (c) 1994, 1995 Carnegie-Mellon University.
+ * Copyright (c) 1995 Carnegie-Mellon University.
  * All rights reserved.
  *
  * Author: Chris G. Demetriou
