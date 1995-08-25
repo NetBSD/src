dc8d8d05f5d3e3095c0d2492fb4bac1c7c63b77d
phil <phil@NetBSD.org>
1995-08-25 07:30:33 +0000

    More changes to the "new" ncr interrupt driven scsi driver (from M. Pfaller) Bring back the old polled driver until a kernel built with the interrupt driven one is stable enough for production use.

--- sys/arch/pc532/dev/ncrreg.h
+++ sys/arch/pc532/dev/ncrreg.h
@@ -1,8 +1,7 @@
-/*	$NetBSD: ncrreg.h,v 1.1 1995/06/09 04:36:26 phil Exp $  */
+/*	$NetBSD: ncrreg.h,v 1.2 1995/08/25 07:30:40 phil Exp $ */
 
 /*
- * Copyright (c) 1995 Leo Weppelman.
- * PC532-Port by Matthias Pfaller.
+ * Copyright (c) 1994 Matthias Pfaller.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -15,7 +14,7 @@
  *    documentation and/or other materials provided with the distribution.
  * 3. All advertising materials mentioning features or use of this software
  *    must display the following acknowledgement:
- *      This product includes software developed by Leo Weppelman.
+ *	This product includes software developed by Matthias Pfaller.
  * 4. The name of the author may not be used to endorse or promote products
  *    derived from this software without specific prior written permission
  *
@@ -29,117 +28,35 @@
  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *	$Id: ncrreg.h,v 1.2 1995/08/25 07:30:40 phil Exp $
  */
 
-#ifndef _NCR5380REG_H
-#define _NCR5380REG_H
+#ifndef _NCRREG_H
+#define _NCRREG_H
 
 #define PDMA_ADDRESS	((volatile u_char *) 0xffe00000)
-#define	SCSI_5380	((volatile struct scsi_5380 *) 0xffd00000)
+#define	NCR5380		((volatile struct ncr5380 *) 0xffd00000)
 
-struct scsi_5380 {
-	volatile u_char	scsi_5380[8];	/* use only the odd bytes	*/
+struct ncr5380 {
+	volatile u_char	regs[8];	/* use only the odd bytes	*/
 };
 
-#define	scsi_data	scsi_5380[0]	/* Data register		*/
-#define	scsi_icom	scsi_5380[1]	/* Initiator command register	*/
-#define	scsi_mode	scsi_5380[2]	/* Mode register		*/
-#define	scsi_tcom	scsi_5380[3]	/* Target command register	*/
-#define	scsi_idstat	scsi_5380[4]	/* Bus status register		*/
-#define	scsi_dmstat	scsi_5380[5]	/* DMA status register		*/
-#define	scsi_trcv	scsi_5380[6]	/* Target receive register	*/
-#define	scsi_ircv	scsi_5380[7]	/* Initiator receive register	*/
+#define	ncr_data	regs[0]	/* Data register		*/
+#define	ncr_icom	regs[1]	/* Initiator command register	*/
+#define	ncr_mode	regs[2]	/* Mode register		*/
+#define	ncr_tcom	regs[3]	/* Target command register	*/
+#define	ncr_idstat	regs[4]	/* Bus status register		*/
+#define	ncr_dmstat	regs[5]	/* DMA status register		*/
+#define	ncr_trcv	regs[6]	/* Target receive register	*/
+#define	ncr_ircv	regs[7]	/* Initiator receive register	*/
 
-/*
- * Definitions for Initiator command register.
- */
-#define	SC_A_RST	0x80	/* RW - Assert RST			*/
-#define	SC_TEST		0x40	/* W  - Test mode			*/
-#define	SC_AIP		0x40	/* R  - Arbitration in progress		*/
-#define	SC_LA		0x20	/* R  - Lost arbitration		*/
-#define	SC_A_ACK	0x10	/* RW - Assert ACK			*/
-#define	SC_A_BSY	0x08	/* RW - Assert BSY			*/
-#define	SC_A_SEL	0x04	/* RW - Assert SEL			*/
-#define	SC_A_ATN	0x02	/* RW - Assert ATN			*/
-#define	SC_ADTB		0x01	/* RW - Assert Data To Bus		*/
+#define	GET_5380_REG(rnum)	NCR5380->regs[rnum]
+#define	SET_5380_REG(rnum,val)	(NCR5380->regs[rnum] = val)
+#define scsi_ienable()		intr_enable(IR_SCSI1)
+#define scsi_idisable()		intr_disable(IR_SCSI1)
+#define	scsi_clr_ipend()	do { \
+		int i = GET_5380_REG(NCR5380_IRCV); \
+	} while (0)
 
-/*
- * Definitions for mode register
- */
-#define	SC_B_DMA	0x80	/* RW - Block mode DMA (not on TT!)	*/
-#define	SC_T_MODE	0x40	/* RW - Target mode			*/
-#define	SC_E_PAR	0x20	/* RW - Enable parity check		*/
-#define	SC_E_PARI	0x10	/* RW - Enable parity interrupt		*/
-#define	SC_E_EOPI	0x08	/* RW - Enable End Of Process Interrupt	*/
-#define	SC_MON_BSY	0x04	/* RW - Monitor BSY			*/
-#define	SC_M_DMA	0x02	/* RW - Set DMA mode			*/
-#define	SC_ARBIT	0x01	/* RW - Arbitrate			*/
-
-/*
- * Definitions for tcom register
- */
-#define	SC_LBS		0x80	/* RW - Last Byte Send (not on TT!)	*/
-#define	SC_A_REQ	0x08	/* RW - Assert REQ			*/
-#define	SC_A_MSG	0x04	/* RW - Assert MSG			*/
-#define	SC_A_CD		0x02	/* RW - Assert C/D			*/
-#define	SC_A_IO		0x01	/* RW - Assert I/O			*/
-
-/*
- * Definitions for idstat register
- */
-#define	SC_S_RST	0x80	/* R  - RST is set			*/
-#define	SC_S_BSY	0x40	/* R  - BSY is set			*/
-#define	SC_S_REQ	0x20	/* R  - REQ is set			*/
-#define	SC_S_MSG	0x10	/* R  - MSG is set			*/
-#define	SC_S_CD		0x08	/* R  - C/D is set			*/
-#define	SC_S_IO		0x04	/* R  - I/O is set			*/
-#define	SC_S_SEL	0x02	/* R  - SEL is set			*/
-#define	SC_S_PAR	0x01	/* R  - Parity bit			*/
-
-/*
- * Definitions for dmastat register
- */
-#define	SC_END_DMA	0x80	/* R  - End of DMA			*/
-#define	SC_DMA_REQ	0x40	/* R  - DMA request			*/
-#define	SC_PAR_ERR	0x20	/* R  - Parity error			*/
-#define	SC_IRQ_SET	0x10	/* R  - IRQ is active			*/
-#define	SC_PHS_MTCH	0x08	/* R  - Phase Match			*/
-#define	SC_BSY_ERR	0x04	/* R  - Busy error			*/
-#define	SC_ATN_STAT	0x02	/* R  - State of ATN line		*/
-#define	SC_ACK_STAT	0x01	/* R  - State of ACK line		*/
-#define	SC_S_SEND	0x00	/* W  - Start DMA output		*/
-
-#define	SC_CLINT	{ 		/* Clear interrupts	*/	\
-			int i = SCSI_5380->scsi_ircv;			\
-			}
-
-
-/*
- * Definition of SCSI-bus phases. The values are determined by signals
- * on the SCSI-bus. DO NOT CHANGE!
- * The values must be used to index the pointers in SCSI-PARMS.
- */
-#define	NR_PHASE	8
-#define	PH_DATAOUT	0
-#define	PH_DATAIN	1
-#define	PH_CMD		2
-#define	PH_STATUS	3
-#define	PH_RES1		4
-#define	PH_RES2		5
-#define	PH_MSGOUT	6
-#define	PH_MSGIN	7
-
-#define	PH_OUT(phase)	(!(phase & 1))	/* TRUE if output phase		*/
-#define	PH_IN(phase)	(phase & 1)	/* TRUE if input phase		*/
-
-/*
- * Id of Host-adapter
- */
-#define SC_HOST_ID	0x80
-
-/*
- * Base setting for 5380 mode register
- */
-#define	IMODE_BASE	SC_E_PAR
-
-#endif /* _NCR5380REG_H */
+#endif /* _NCRREG_H */
