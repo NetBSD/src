288f1c084f771b34054813e05426198580643cc8
phil <phil@NetBSD.org>
1995-08-29 08:03:27 +0000

    Changes from Matthias Pfaller to get gdb to work.

--- gnu/usr.bin/gdb/gdb/arch/ns32k/tm.h
+++ gnu/usr.bin/gdb/gdb/arch/ns32k/tm.h
@@ -17,10 +17,9 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
-	$Id: tm.h,v 1.2 1995/07/28 08:03:29 phil Exp $
+	$Id: tm.h,v 1.3 1995/08/29 08:03:33 phil Exp $
 */
 
-#define FETCH_INFERIOR_REGISTERS
 /* Override number of expected traps from sysv. */
 #define START_INFERIOR_TRAPS_EXPECTED 2
 
@@ -30,6 +29,9 @@ Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
 /* Shared library code */
 #include "solib.h"
+
+/* We define our own fetch and store methods. */
+#define FETCH_INFERIOR_REGISTERS
 
 /* Saved Pc.  Get it from sigcontext if within sigtramp.  */
 
