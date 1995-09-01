cf147bd426a3921e39ced5c4e9feb6384da7b920
jtc <jtc@NetBSD.org>
1995-09-01 01:10:44 +0000

    imported from 44lite2

--- usr.bin/fmt/fmt.c
+++ usr.bin/fmt/fmt.c
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1980 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1980, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -32,13 +32,13 @@
  */
 
 #ifndef lint
-char copyright[] =
-"@(#) Copyright (c) 1980 Regents of the University of California.\n\
- All rights reserved.\n";
+static char copyright[] =
+"@(#) Copyright (c) 1980, 1993\n\
+	The Regents of the University of California.  All rights reserved.\n";
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)fmt.c	5.10 (Berkeley) 6/1/90";
+static char sccsid[] = "@(#)fmt.c	8.1 (Berkeley) 7/20/93";
 #endif /* not lint */
 
 #include <stdio.h>
@@ -239,7 +239,7 @@ prefix(line)
 		oflush();
 	pfx = np;
 	if (h)
-		pack(cp);
+		pack(cp, strlen(cp));
 	else	split(cp);
 	if (h)
 		oflush();
