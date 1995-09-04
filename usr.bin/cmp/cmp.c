f9e53dd324b4f31ec2a7dc4c5808c2b17756661b
tls <tls@NetBSD.org>
1995-09-04 21:07:23 +0000

    imported from 44lite2

--- usr.bin/cmp/cmp.c
+++ usr.bin/cmp/cmp.c
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1987 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1987, 1990, 1993, 1994
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -32,48 +32,50 @@
  */
 
 #ifndef lint
-char copyright[] =
-"@(#) Copyright (c) 1987, 1990 Regents of the University of California.\n\
- All rights reserved.\n";
+static char copyright[] =
+"@(#) Copyright (c) 1987, 1990, 1993, 1994\n\
+	The Regents of the University of California.  All rights reserved.\n";
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)cmp.c	5.3 (Berkeley) 6/1/90";
+static char sccsid[] = "@(#)cmp.c	8.3 (Berkeley) 4/2/94";
 #endif /* not lint */
 
-#include <sys/param.h>
-#include <sys/file.h>
+#include <sys/types.h>
 #include <sys/stat.h>
+
+#include <err.h>
+#include <fcntl.h>
 #include <stdio.h>
-#include <ctype.h>
-#include <errno.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
-#define	EXITNODIFF	0
-#define	EXITDIFF	1
-#define	EXITERR		2
+#include "extern.h"
 
-int	all, fd1, fd2, silent;
-u_char	buf1[MAXBSIZE], buf2[MAXBSIZE];
-char	*file1, *file2;
+int	lflag, sflag;
 
+static void usage __P((void));
+
+int
 main(argc, argv)
 	int argc;
 	char *argv[];
 {
-	extern char *optarg;
-	extern int optind;
-	int ch;
-	u_long otoi();
+	struct stat sb1, sb2;
+	off_t skip1, skip2;
+	int ch, fd1, fd2, special;
+	char *file1, *file2;
 
 	while ((ch = getopt(argc, argv, "-ls")) != EOF)
 		switch (ch) {
 		case 'l':		/* print all differences */
-			all = 1;
+			lflag = 1;
 			break;
 		case 's':		/* silent run */
-			silent = 1;
+			sflag = 1;
 			break;
-		case '-':		/* must be after any flags */
+		case '-':		/* stdin (must be after options) */
 			--optind;
 			goto endargs;
 		case '?':
@@ -84,192 +86,61 @@ endargs:
 	argv += optind;
 	argc -= optind;
 
+	if (lflag && sflag)
+		errx(ERR_EXIT, "only one of -l and -s may be specified");
+
 	if (argc < 2 || argc > 4)
 		usage();
 
-	if (all && silent) {
-		fprintf(stderr,
-		    "cmp: only one of -l and -s may be specified.\n");
-		exit(EXITERR);
-	}
-	if (strcmp(file1 = argv[0], "-") == 0)
+	/* Backward compatibility -- handle "-" meaning stdin. */
+	special = 0;
+	if (strcmp(file1 = argv[0], "-") == 0) {
+		special = 1;
 		fd1 = 0;
+		file1 = "stdin";
+	}
 	else if ((fd1 = open(file1, O_RDONLY, 0)) < 0)
-		error(file1);
-	if (strcmp(file2 = argv[1], "-") == 0)
+		err(ERR_EXIT, "%s", file1);
+	if (strcmp(file2 = argv[1], "-") == 0) {
+		if (special)
+			errx(ERR_EXIT,
+				"standard input may only be specified once");
+		special = 1;
 		fd2 = 0;
+		file2 = "stdin";
+	}
 	else if ((fd2 = open(file2, O_RDONLY, 0)) < 0)
-		error(file2);
-	if (fd1 == fd2) {
-		fprintf(stderr,
-		    "cmp: standard input may only be specified once.\n");
-		exit(EXITERR);
-	}
+		err(ERR_EXIT, "%s", file2);
 
-	/* handle skip arguments */
-	if (argc > 2) {
-		skip(otoi(argv[2]), fd1, file1);
-		if (argc == 4)
-			skip(otoi(argv[3]), fd2, file2);
-	}
-	cmp();
-	/*NOTREACHED*/
-}
+	skip1 = argc > 2 ? strtol(argv[2], NULL, 10) : 0;
+	skip2 = argc == 4 ? strtol(argv[3], NULL, 10) : 0;
 
-/*
- * skip --
- *	skip first part of file
- */
-skip(dist, fd, fname)
-	register u_long dist;
-	register int fd;
-	char *fname;
-{
-	register int rlen, nread;
-
-	for (; dist; dist -= rlen) {
-		rlen = MIN(dist, sizeof(buf1));
-		if ((nread = read(fd, buf1, rlen)) != rlen) {
-			if (nread < 0)
-				error(fname);
-			else
-				endoffile(fname);
+	if (!special) {
+		if (fstat(fd1, &sb1))
+			err(ERR_EXIT, "%s", file1);
+		if (!S_ISREG(sb1.st_mode))
+			special = 1;
+		else {
+			if (fstat(fd2, &sb2))
+				err(ERR_EXIT, "%s", file2);
+			if (!S_ISREG(sb2.st_mode))
+				special = 1;
 		}
 	}
+
+	if (special)
+		c_special(fd1, file1, skip1, fd2, file2, skip2);
+	else
+		c_regular(fd1, file1, skip1, sb1.st_size,
+		    fd2, file2, skip2, sb2.st_size);
+	exit(0);
 }
 
-cmp()
-{
-	register u_char	*p1, *p2;
-	register int cnt, len1, len2;
-	register long byte, line;
-	int dfound = 0;
-
-	for (byte = 0, line = 1; ; ) {
-		switch (len1 = read(fd1, buf1, MAXBSIZE)) {
-		case -1:
-			error(file1);
-		case 0:
-			/*
-			 * read of file 1 just failed, find out
-			 * if there's anything left in file 2
-			 */
-			switch (read(fd2, buf2, 1)) {
-				case -1:
-					error(file2);
-					/* NOTREACHED */
-				case 0:
-					exit(dfound ? EXITDIFF : EXITNODIFF);
-					/* NOTREACHED */
-				default:
-					endoffile(file1);
-					break;
-			}
-		}
-		/*
-		 * file1 might be stdio, which means that a read of less than
-		 * MAXBSIZE might not mean an EOF.  So, read whatever we read
-		 * from file1 from file2.
-		 */
-		if ((len2 = read(fd2, buf2, len1)) == -1)
-			error(file2);
-		if (bcmp(buf1, buf2, len2)) {
-			if (silent)
-				exit(EXITDIFF);
-			if (all) {
-				dfound = 1;
-				for (p1 = buf1, p2 = buf2, cnt = len2; cnt--;
-				    ++p1, ++p2) {
-					++byte;
-					if (*p1 != *p2)
-						printf("%6ld %3o %3o\n",
-						    byte, *p1, *p2);
-				}
-			} else for (p1 = buf1, p2 = buf2; ; ++p1, ++p2) {
-				++byte;
-				if (*p1 != *p2) {
-					printf("%s %s differ: char %ld, line %ld\n", file1, file2, byte, line);
-					exit(EXITDIFF);
-				}
-				if (*p1 == '\n')
-					++line;
-			}
-		} else {
-			byte += len2;
-			/*
-			 * here's the real performance problem, we've got to
-			 * count the stupid lines, which means that -l is a
-			 * *much* faster version, i.e., unless you really
-			 * *want* to know the line number, run -s or -l.
-			 */
-			if (!silent && !all)
-				for (p1 = buf1, cnt = len2; cnt--; )
-					if (*p1++ == '\n')
-						++line;
-		}
-		/*
-		 * couldn't read as much from file2 as from file1; checked
-		 * here because there might be a difference before we got
-		 * to this point, which would have precedence.
-		 */
-		if (len2 < len1)
-			endoffile(file2);
-	}
-}
-
-/*
- * otoi --
- *	octal/decimal string to u_long
- */
-u_long
-otoi(s)
-	register char *s;
-{
-	register u_long val;
-	register int base;
-
-	base = (*s == '0') ? 8 : 10;
-	for (val = 0; isdigit(*s); ++s)
-		val = val * base + *s - '0';
-	return (val);
-}
-
-/*
- * error --
- *	print I/O error message and die
- */
-error(filename)
-	char *filename;
-{
-	extern int errno;
-	char *strerror();
-
-	if (!silent)
-		(void) fprintf(stderr, "cmp: %s: %s\n",
-		    filename, strerror(errno));
-	exit(EXITERR);
-}
-
-/*
- * endoffile --
- *	print end-of-file message and exit indicating the files were different
- */
-endoffile(filename)
-	char *filename;
-{
-	/* 32V put this message on stdout, S5 does it on stderr. */
-	/* POSIX.2 currently does it on stdout-- Hooray! */
-	if (!silent)
-		(void) printf("cmp: EOF on %s\n", filename);
-	exit(EXITDIFF);
-}
-
-/*
- * usage --
- *	print usage and die
- */
+static void
 usage()
 {
-	fputs("usage: cmp [-ls] file1 file2 [skip1] [skip2]\n", stderr);
-	exit(EXITERR);
+
+	(void)fprintf(stderr,
+	    "usage: cmp [-l | s] file1 file2 [skip1 [skip2]]\n");
+	exit(ERR_EXIT);
 }
