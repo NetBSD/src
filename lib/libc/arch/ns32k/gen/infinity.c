d995fcfdb1a1ccf7c2fc519e2d1428d9580a2283
phil <phil@NetBSD.org>
1995-09-20 22:34:03 +0000

    correction from Matthias Pfaller.

--- lib/libc/arch/ns32k/gen/infinity.c
+++ lib/libc/arch/ns32k/gen/infinity.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static char rcsid[] = "$Id: infinity.c,v 1.1 1993/10/07 00:20:19 cgd Exp $";
+static char rcsid[] = "$Id: infinity.c,v 1.2 1995/09/20 22:34:03 phil Exp $";
 #endif /* not lint */
 
 /* infinity.c */
@@ -7,4 +7,4 @@ static char rcsid[] = "$Id: infinity.c,v 1.1 1993/10/07 00:20:19 cgd Exp $";
 #include <math.h>
 
 /* bytes for +Infinity on a ns32k */
-char __infinity[] = { 0x7f, 0xf0, 0, 0, 0, 0, 0, 0 };
+char __infinity[] = { 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xef, 0x7f };
