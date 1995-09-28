c276504da809d7e20386987f6a8d2bec5c54b6a9
tls <tls@NetBSD.org>
1995-09-28 06:57:37 +0000

    Sync with 4.4BSD-Lite2

--- usr.bin/msgs/pathnames.h
+++ usr.bin/msgs/pathnames.h
@@ -1,6 +1,8 @@
+/*	$NetBSD: pathnames.h,v 1.3 1995/09/28 06:57:41 tls Exp $	*/
+
 /*
- * Copyright (c) 1989 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1989, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -30,8 +32,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	from: @(#)pathnames.h	5.3 (Berkeley) 6/1/90
- *	$Id: pathnames.h,v 1.2 1993/08/01 18:10:57 mycroft Exp $
+ *	@(#)pathnames.h	8.1 (Berkeley) 6/6/93
  */
 
 #define	_PATH_MSGS	"/var/msgs"
