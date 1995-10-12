c4f9640a8618eef06d65aedaa2cd5fe677853085
chuck <chuck>
1995-10-12 00:43:38 +0000

    change SUNOS_SYS_sunos_sigreturn to SUNOS_SYS_sigreturn

--- sys/arch/sun3/stand/ufsboot/version.c
+++
@@ -1,9 +0,0 @@
-/*	$NetBSD: version.c,v 1.1.1.1 1995/06/01 20:38:08 gwr Exp $ */
-
-/*
- *	NOTE ANY CHANGES YOU MAKE TO THE BOOTBLOCKS HERE.
- *
- *	1.1
- */
-
-char *version = "$Revision: 1.1.1.1 $";
