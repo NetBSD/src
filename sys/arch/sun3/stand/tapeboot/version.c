fec310d11b8cb6925d053c7e3436c6f6ba59f4f3
gwr <gwr@NetBSD.org>
1995-10-13 21:27:30 +0000

    Boot program to help load tape kernels. (You can even choose a tape segment!)

---
+++ sys/arch/sun3/stand/tapeboot/version.c
@@ -0,0 +1,9 @@
+/*	$NetBSD: version.c,v 1.1.1.1 1995/10/13 21:27:30 gwr Exp $ */
+
+/*
+ *	NOTE ANY CHANGES YOU MAKE TO THE BOOTBLOCKS HERE.
+ *
+ *	1.1
+ */
+
+char *version = "$Revision: 1.1.1.1 $";
