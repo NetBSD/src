749a3d8a024dfe9ebec7c181654b74e1695b39d3
gwr <gwr>
1995-10-17 22:58:15 +0000

    Re-write of tapeboot, using a "rawfs" abstraction to hide the fact that the device only supports block access.  (Even works now!)

---
+++ sys/arch/sun3/stand/tapeboot/version.c
@@ -0,0 +1,10 @@
+/*	$NetBSD: version.c,v 1.3.2.2 1995/10/17 22:58:33 gwr Exp $ */
+
+/*
+ *	NOTE ANY CHANGES YOU MAKE TO THE BOOTBLOCKS HERE.
+ *
+ *	Added "rawfs" to do read buffering and ensure that all
+ *	device access stays strictly on block boundaries.
+ */
+
+char *version = "$Revision: 1.3.2.2 $";
