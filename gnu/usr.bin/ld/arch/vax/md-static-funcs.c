6ac54c7a783559e18a829617a9f85e6447863a9c
ragge <ragge@NetBSD.org>
1995-10-19 13:10:17 +0000

    Vax support for ld, so we (at least) can link static binaries.

---
+++ gnu/usr.bin/ld/arch/vax/md-static-funcs.c
@@ -0,0 +1,16 @@
+/*	$NetBSD: md-static-funcs.c,v 1.1 1995/10/19 13:10:17 ragge Exp $	*/
+/*
+ * Called by ld.so when onanating.
+ * This *must* be a static function, so it is not called through a jmpslot.
+ */
+
+static void
+md_relocate_simple(r, relocation, addr)
+struct relocation_info	*r;
+long			relocation;
+char			*addr;
+{
+if (r->r_relative)
+	*(long *)addr += relocation;
+}
+
