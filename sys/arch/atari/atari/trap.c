958f46a53faf8930defb86e8d3c0c076005f24b3
leo <leo@NetBSD.org>
1995-10-22 21:10:21 +0000

    Don't call trapsignal() when fpu_emulate() successfully emulated the instruction.

--- sys/arch/atari/atari/trap.c
+++ sys/arch/atari/atari/trap.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: trap.c,v 1.9.2.1 1995/10/13 21:04:18 leo Exp $	*/
+/*	$NetBSD: trap.c,v 1.9.2.2 1995/10/22 21:10:21 leo Exp $	*/
 
 /*
  * Copyright (c) 1988 University of Utah.
@@ -559,6 +559,8 @@ trap(type, code, v, frame)
 #ifdef FPU_EMULATE
 		i = fpu_emulate(&frame, &p->p_addr->u_pcb.pcb_fpregs);
 		/* XXX -- deal with tracing? (frame.f_sr & PSL_T) */
+		if (i == 0)
+			userret(p, frame.f_pc, sticks); 
 #else
 		uprintf("pid %d killed: no floating point support.\n",
 			p->p_pid);
