5a59eaa2d30040b9fc61df65598237780b2ec6ae
briggs <briggs@NetBSD.org>
1995-12-03 13:52:50 +0000

    Fix from Niklas Hallqvist / Chuck Cranor / Michael Hitch / OpenBSD to avoid integer overflow in initial pmap setup.  Triggerred by high values for maxusers.

--- sys/arch/mac68k/mac68k/pmap.c
+++ sys/arch/mac68k/mac68k/pmap.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: pmap.c,v 1.21 1995/10/10 04:14:30 briggs Exp $	*/
+/*	$NetBSD: pmap.c,v 1.22 1995/12/03 13:52:50 briggs Exp $	*/
 
 /* 
  * Copyright (c) 1991, 1993
@@ -474,7 +474,8 @@ pmap_init()
 	 * map where we want it.
 	 */
 	addr = MAC_PTBASE;
-	s = min(MAC_PTMAXSIZE, maxproc*MAC_MAX_PTSIZE);
+	s = (MAC_PTMAXSIZE / MAC_MAX_PTSIZE < maxproc) ?
+		MAC_PTMAXSIZE : (maxproc * MAC_MAX_PTSIZE);
 	addr2 = addr + s;
 	rv = vm_map_find(kernel_map, NULL, 0, &addr, s, TRUE);
 	if (rv != KERN_SUCCESS)
