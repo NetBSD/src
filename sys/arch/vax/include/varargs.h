c69c23ea36b6575ffe12a3b8fc3c994d824266e7
mycroft <mycroft@NetBSD.org>
1995-12-25 22:22:02 +0000

    Stylistic changes.

--- sys/arch/vax/include/varargs.h
+++ sys/arch/vax/include/varargs.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: varargs.h,v 1.6 1995/12/25 21:41:18 mycroft Exp $	*/
+/*	$NetBSD: varargs.h,v 1.7 1995/12/25 22:22:12 mycroft Exp $	*/
 
 /*-
  * Copyright (c) 1990 The Regents of the University of California.
@@ -50,6 +50,6 @@
 
 #undef va_start
 #define	va_start(ap) \
-	ap = (char *)&va_alist
+	((ap) = (va_list)&va_alist)
 
 #endif /* !_VAX_VARARGS_H_ */
