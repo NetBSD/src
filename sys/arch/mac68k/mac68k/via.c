9f2629c5baba05f21fa33ba2494659d8ebdb7ae0
briggs <briggs@NetBSD.org>
1996-02-11 01:20:46 +0000

    Remove via_inited, as it is unused.

--- sys/arch/mac68k/mac68k/via.c
+++ sys/arch/mac68k/mac68k/via.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: via.c,v 1.36 1996/02/03 22:50:19 briggs Exp $	*/
+/*	$NetBSD: via.c,v 1.37 1996/02/11 01:20:46 briggs Exp $	*/
 
 /*-
  * Copyright (C) 1993	Allen K. Briggs, Chris P. Caputo,
@@ -84,7 +84,6 @@ void *via2iarg[7] = {
 void		via2_intr(struct frame *);
 void		rbv_intr(struct frame *);
 
-static int	via_inited=0;
 void		(*real_via2_intr)(struct frame *);
 
 /* nubus slot interrupt routines */
@@ -154,7 +153,6 @@ VIA_initialize()
 		real_via2_intr = rbv_intr;
 		via2itab[1] = rbv_nubus_intr;
 	}
-	via_inited = 1;
 }
 
 void
