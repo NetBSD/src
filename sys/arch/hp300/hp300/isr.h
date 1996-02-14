baf573cb0ac800f1b8b9a272b342c70b0777d0b5
thorpej <thorpej@NetBSD.org>
1996-02-14 02:56:38 +0000

    New interrupt handling code.  Highlights: 	- Interrupt service routines now take a pointer to 	  the device's softc, rather than a unit number. 	- Interrupt handling code in locore.s condensed.  Only 	  remaining "special" handlers are lev0 (spurious), 	  lev1 (HIL; don't ask), lev6 (clock), and lev7 (NMI). 	  Only remaining hard-coded device driver reference in 	  locore is HIL.  (clock doesn't count...) 	- "dcafastservice" eliminated.  DCA driver has new mechanism 	  to guarantee prompt interrupt service at any ipl (not 	  just level 5). 	- Interrupt code pulled out of autoconf.c and machdep.c and 	  placed in new file, isr.c

--- sys/arch/hp300/hp300/isr.h
+++ sys/arch/hp300/hp300/isr.h
@@ -1,8 +1,8 @@
-/*	$NetBSD: isr.h,v 1.4 1994/10/26 07:25:37 cgd Exp $	*/
+/*	$NetBSD: isr.h,v 1.5 1996/02/14 02:56:51 thorpej Exp $	*/
 
 /*
- * Copyright (c) 1982, 1993
- *	The Regents of the University of California.  All rights reserved.
+ * Copyright (c) 1995, 1996 Jason R. Thorpe.
+ * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -14,34 +14,49 @@
  *    documentation and/or other materials provided with the distribution.
  * 3. All advertising materials mentioning features or use of this software
  *    must display the following acknowledgement:
- *	This product includes software developed by the University of
- *	California, Berkeley and its contributors.
- * 4. Neither the name of the University nor the names of its contributors
- *    may be used to endorse or promote products derived from this software
- *    without specific prior written permission.
+ *	This product includes software developed for the NetBSD Project
+ *	by Jason R. Thorpe.
+ * 4. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
  *
- * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
- *
- *	@(#)isr.h	8.1 (Berkeley) 6/10/93
  */
 
+#include <sys/queue.h>
+
+/*
+ * The location and size of the autovectored interrupt portion
+ * of the vector table.
+ */
+#define ISRLOC		0x18
+#define NISR		8
+
 struct isr {
-	struct	isr *isr_forw;
-	struct	isr *isr_back;
-	int	(*isr_intr)();
-	int	isr_arg;
-	int	isr_ipl;
+	LIST_ENTRY(isr) isr_link;
+	int		(*isr_func) __P((void *));
+	void		*isr_arg;
+	int		isr_ipl;
+	int		isr_priority;
 };
 
-#define	NISR		3
-#define	ISRIPL(x)	((x) - 3)
+/*
+ * ISR priorities.  These are not the same as interrupt levels.
+ */
+#define ISRPRI_BIO		0
+#define ISRPRI_NET		1
+#define ISRPRI_TTY		2
+#define ISRPRI_TTYNOBUF		3
+
+void	isrinit __P((void));
+void	isrlink __P((int (*)(void *), void *, int, int));
+void	isrdispatch __P((int));
