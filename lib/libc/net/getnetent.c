b76519867cb6695679ee1f7c3d219acd6d864051
mrg <mrg@NetBSD.org>
1996-02-16 00:53:00 +0000

    re-apply patch from 2 revisions ago

--- lib/libc/net/getnetent.c
+++ lib/libc/net/getnetent.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: getnetent.c,v 1.6 1996/02/02 15:22:21 mrg Exp $	*/
+/*	$NetBSD: getnetent.c,v 1.7 1996/02/16 00:53:00 mrg Exp $	*/
 
 /*
  * Copyright (c) 1983, 1993
@@ -37,7 +37,7 @@
 #if 0
 static char sccsid[] = "@(#)getnetent.c	8.1 (Berkeley) 6/4/93";
 #else
-static char rcsid[] = "$NetBSD: getnetent.c,v 1.6 1996/02/02 15:22:21 mrg Exp $";
+static char rcsid[] = "$NetBSD: getnetent.c,v 1.7 1996/02/16 00:53:00 mrg Exp $";
 #endif
 #endif /* LIBC_SCCS and not lint */
 
@@ -109,18 +109,19 @@ again:
 	net.n_net = inet_network(cp);
 	net.n_addrtype = AF_INET;
 	q = net.n_aliases = net_aliases;
-	if (p != NULL) 
+	if (p != NULL)  {
 		cp = p;
-	while (cp && *cp) {
-		if (*cp == ' ' || *cp == '\t') {
-			cp++;
-			continue;
+		while (cp && *cp) {
+			if (*cp == ' ' || *cp == '\t') {
+				cp++;
+				continue;
+			}
+			if (q < &net_aliases[MAXALIASES - 1])
+				*q++ = cp;
+			cp = strpbrk(cp, " \t");
+			if (cp != NULL)
+				*cp++ = '\0';
 		}
-		if (q < &net_aliases[MAXALIASES - 1])
-			*q++ = cp;
-		cp = strpbrk(cp, " \t");
-		if (cp != NULL)
-			*cp++ = '\0';
 	}
 	*q = NULL;
 	return (&net);
