f7b9a78df8cd2b0bccd30e7be591890679ff5f7b
mark <mark@NetBSD.org>
1996-02-16 20:47:54 +0000

    Initial commit of the arm32 architecture dependant libc code.

---
+++ lib/libc/arch/arm32/gen/infinity.c
@@ -0,0 +1,9 @@
+/* $NetBSD: infinity.c,v 1.1 1996/02/16 20:48:18 mark Exp $ */
+
+/*
+ *	$Id: infinity.c,v 1.1 1996/02/16 20:48:18 mark Exp $
+ */
+
+#include <math.h>
+
+char __infinity[] = { 0, 0, 0, 0, 0, 0, 0xf0, 0x7f };
