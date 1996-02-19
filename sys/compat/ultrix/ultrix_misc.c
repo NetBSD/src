1e11d8a6eae5c77b13c755d6aa4852eaaf02ceb8
pk <pk>
1996-02-19 15:41:38 +0000

    Adapt to NFSv3.

--- sys/compat/ultrix/ultrix_misc.c
+++ sys/compat/ultrix/ultrix_misc.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: ultrix_misc.c,v 1.20 1996/01/07 13:38:51 jonathan Exp $	*/
+/*	$NetBSD: ultrix_misc.c,v 1.21 1996/02/19 15:41:38 pk Exp $	*/
 
 /*
  * Copyright (c) 1992, 1993
@@ -90,7 +90,7 @@
 #include <miscfs/specfs/specdev.h>
 
 #include <nfs/rpcv2.h>
-#include <nfs/nfsv2.h>
+#include <nfs/nfsproto.h>
 #include <nfs/nfs.h>
 
 #include <vm/vm.h>
