b34d6f72ec0e0d16308fed74543f1671bf31f191
gwr <gwr@NetBSD.org>
1996-02-22 23:23:23 +0000

    Remove the hack that faked up an "ssp" value.  It is now up to locore.s to put the right sp in the frame.  Also move memory access code into db_memrw.c so it can be taken separately.

--- sys/arch/m68k/m68k/db_trace.c
+++ sys/arch/m68k/m68k/db_trace.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: db_trace.c,v 1.14 1996/02/13 17:34:46 gwr Exp $	*/
+/*	$NetBSD: db_trace.c,v 1.15 1996/02/22 23:23:41 gwr Exp $	*/
 
 /* 
  * Mach Operating System
@@ -41,7 +41,6 @@ extern label_t	*db_recover;
  * Register list
  */
 static int db_var_short(struct db_variable *, db_expr_t *, int);
-extern int ddb_regs_ssp;
 struct db_variable db_regs[] = {
 	{ "d0",	(int *)&ddb_regs.d0,	FCN_NULL },
 	{ "d1",	(int *)&ddb_regs.d1,	FCN_NULL },
@@ -58,8 +57,7 @@ struct db_variable db_regs[] = {
 	{ "a4",	(int *)&ddb_regs.a4,	FCN_NULL },
 	{ "a5",	(int *)&ddb_regs.a5,	FCN_NULL },
 	{ "a6",	(int *)&ddb_regs.a6,	FCN_NULL },
-	{ "ssp",&ddb_regs_ssp,  	FCN_NULL },
-	{ "usp",(int *)&ddb_regs.sp,	FCN_NULL },
+	{ "sp",	(int *)&ddb_regs.sp,	FCN_NULL },
 	{ "pc",	(int *)&ddb_regs.pc,	FCN_NULL },
 	{ "sr",	(int *)&ddb_regs.sr,	db_var_short }
 };
