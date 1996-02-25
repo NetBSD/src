b84b8dbab5364f40b52bf53f933903497693b721
cgd <cgd@NetBSD.org>
1996-02-25 22:56:30 +0000

    don't need isa/isareg.h or pci/pci{reg,var}.h.

--- sys/dev/ic/aic7xxx.c
+++ sys/dev/ic/aic7xxx.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: aic7xxx.c,v 1.2 1996/01/13 02:05:22 thorpej Exp $	*/
+/*	$NetBSD: aic7xxx.c,v 1.3 1996/02/25 22:56:30 cgd Exp $	*/
 
 /*
  * Generic driver for the aic7xxx based adaptec SCSI controllers
@@ -43,10 +43,6 @@
 #include <sys/user.h>
 
 #include <machine/pio.h>
-
-#include <dev/isa/isareg.h>
-#include <dev/pci/pcireg.h>
-#include <dev/pci/pcivar.h>
 
 #include <scsi/scsi_all.h>
 #include <scsi/scsi_debug.h>
