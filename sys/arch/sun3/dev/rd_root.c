efba8a7e10146c36efcae4b877ecfea8a978c360
leo <leo@NetBSD.org>
1996-03-07 10:25:28 +0000

    Allow for a machine dependent match function - rd_match_hook()

--- sys/arch/sun3/dev/rd_root.c
+++ sys/arch/sun3/dev/rd_root.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: rd_root.c,v 1.2 1995/11/17 23:24:56 gwr Exp $	*/
+/*	$NetBSD: rd_root.c,v 1.3 1996/03/07 10:26:17 leo Exp $	*/
 
 /*
  * Copyright (c) 1995 Gordon W. Ross
@@ -50,6 +50,15 @@ char rd_root_image[ROOTBYTES] = "|This is the root ramdisk!\n";
 /*
  * This is called during autoconfig.
  */
+int
+rd_match_hook(parent, self, aux)
+	struct device	*parent;
+	void	*self;
+	void	*aux;
+{
+	return (1);
+}
+
 void
 rd_attach_hook(unit, rd)
 	int unit;
