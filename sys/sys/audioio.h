53877395c4f36ba3f78061c8fa0e542f4c36dc5c
scottr <scottr@NetBSD.org>
1996-03-11 05:12:28 +0000

    XXX - stop-gap fix for use of memset() if we're compiling a kernel; it's       not included in the kernel programming interface.

--- sys/sys/audioio.h
+++ sys/sys/audioio.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: audioio.h,v 1.4 1996/02/17 02:28:56 jtk Exp $	*/
+/*	$NetBSD: audioio.h,v 1.5 1996/03/11 05:12:28 scottr Exp $	*/
 
 /*
  * Copyright (c) 1991-1993 Regents of the University of California.
@@ -77,8 +77,16 @@ struct audio_info {
 };
 typedef struct audio_info audio_info_t;
 
+#ifdef _KERNEL
+#define AUDIO_INITINFO(p)\
+	{ register int n = sizeof(struct audio_info); \
+	  register u_char *q = (u_char *) p; \
+	  while (n-- > 0) *q++ = 0xff; }
+
+#else
 #define AUDIO_INITINFO(p)\
 	(void)memset((void *)(p), 0xff, sizeof(struct audio_info))
+#endif
 
 /*
  * Parameter for the AUDIO_GETDEV ioctl to determine current
