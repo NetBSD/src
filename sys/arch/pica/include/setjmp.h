67eb9a612ddf1356f61ef08d8f14a88c659272d2
jonathan <jonathan@NetBSD.org>
1996-03-13 04:58:04 +0000

    First commit of Per Fogelstrom's port to the Acer pica r4400/isa machine.

---
+++ sys/arch/pica/include/setjmp.h
@@ -0,0 +1,7 @@
+/*	$NetBSD: setjmp.h,v 1.1.1.1 1996/03/13 04:58:08 jonathan Exp $	*/
+
+/*
+ * machine/setjmp.h: machine dependent setjmp-related information.
+ */
+
+#define	_JBLEN	83		/* size, in longs, of a jmp_buf */
---
+++ sys/arch/pica/isa/spkrreg.h
@@ -0,0 +1,11 @@
+/*	$NetBSD: spkrreg.h,v 1.1.1.1 1996/03/13 04:58:09 jonathan Exp $	*/
+
+/*
+ * PIT port addresses and speaker control values
+ */
+
+#define PITAUX_PORT	0x61	/* port of Programmable Peripheral Interface */
+#define PIT_ENABLETMR2	0x01	/* Enable timer/counter 2 */
+#define PIT_SPKRDATA	0x02	/* Direct to speaker */
+
+#define PIT_SPKR	(PIT_ENABLETMR2|PIT_SPKRDATA)
