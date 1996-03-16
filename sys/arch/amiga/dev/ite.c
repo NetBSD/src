8afe2d838e9a4185b93dd142b265ae17ba9e12f2
mhitch <mhitch@NetBSD.org>
1996-03-16 08:19:43 +0000

    Removed Debugger() declaration - the prototype is in sys/systm.h.

--- sys/arch/amiga/dev/ite.c
+++ sys/arch/amiga/dev/ite.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: ite.c,v 1.35 1996/02/24 07:44:06 chopps Exp $	*/
+/*	$NetBSD: ite.c,v 1.36 1996/03/16 08:19:43 mhitch Exp $	*/
 
 /*
  * Copyright (c) 1988 University of Utah.
@@ -906,7 +906,6 @@ ite_filter(c, caller)
 		return;
 #ifdef DDB
 	} else if (key_mod == (KBD_MOD_LALT | KBD_MOD_LMETA) && c == 0x59) {
-		extern int Debugger();
 		Debugger();
 		splx(s);
 		return;
