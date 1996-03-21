cac62b2d656df80f2559dd62c375e095e393f3eb
jtc <jtc@NetBSD.org>
1996-03-21 17:56:15 +0000

    Be pedantic, main() returns int in C.

--- usr.sbin/lpr/pac/pac.c
+++ usr.sbin/lpr/pac/pac.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: pac.c,v 1.6 1996/01/13 23:18:15 pk Exp $	*/
+/*	$NetBSD: pac.c,v 1.7 1996/03/21 18:21:20 jtc Exp $	*/
 /*
  * Copyright (c) 1983, 1993
  *	The Regents of the University of California.  All rights reserved.
@@ -101,7 +101,7 @@ static struct	hent *lookup __P((char []));
 static int	qucmp __P((const void *, const void *));
 static void	rewrite __P((void));
 
-void
+int
 main(argc, argv)
 	int argc;
 	char **argv;
