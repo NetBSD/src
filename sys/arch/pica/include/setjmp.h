850eac35b372cda8767a0fc2ea9e3fa786d95d37
jonathan <jonathan@NetBSD.org>
1996-03-23 03:42:25 +0000

    Replace  the following header files from src/sys/arch/pica/include/ with versions that  include  equivalent files from <mips/include>. (cvs magic copied the previous revisions, with history, to  src/sys/arch/mips/include).
    ansi.h bsd-aout.h cdefs.h ecoff.h elf.h endian.h exec.h float.h ieeefp.h
    kdbparam.h limits.h pcb.h pmap.h proc.h profile.h ptrace.h reg.h regdef.h
    regnum.h reloc.h setjmp.h signal.h stdarg.h types.h varargs.h vmparam.h
    
    NB: Some files (e.g., limits.h reloc.h pmap.h) add pica-specific #defines
    to the  generic mips support.

--- sys/arch/pica/include/setjmp.h
+++ sys/arch/pica/include/setjmp.h
@@ -1,7 +1,3 @@
-/*	$NetBSD: setjmp.h,v 1.1.1.1 1996/03/13 04:58:08 jonathan Exp $	*/
+/*	$NetBSD: setjmp.h,v 1.2 1996/03/23 03:43:08 jonathan Exp $	*/
 
-/*
- * machine/setjmp.h: machine dependent setjmp-related information.
- */
-
-#define	_JBLEN	83		/* size, in longs, of a jmp_buf */
+#include <mips/setjmp.h>
