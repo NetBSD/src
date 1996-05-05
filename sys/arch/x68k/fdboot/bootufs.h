625e92391423066c2e32903b0adf95dc452b1835
oki <oki@NetBSD.org>
1996-05-05 12:17:03 +0000

    NetBSD/x68k, by Masaru Oki.

---
+++ sys/arch/x68k/fdboot/bootufs.h
@@ -0,0 +1,11 @@
+/***************************************************************
+ *
+ *	file: bootufs.h
+ *
+ *	author: chapuni(GBA02750@niftyserve.or.jp)
+ *
+ */
+
+extern void test();
+
+/* eof */
