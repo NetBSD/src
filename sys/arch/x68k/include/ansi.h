1348ec85fcf0a5678dfb279cf93f33e4140fe6f5
oki <oki>
1996-05-05 12:17:03 +0000

    NetBSD/x68k, by Masaru Oki.

---
+++ sys/arch/x68k/include/ansi.h
@@ -0,0 +1,4 @@
+/*	$NetBSD: ansi.h,v 1.1.1.1 1996/05/05 12:17:03 oki Exp $	*/
+
+/* Just use the common m68k definition */
+#include <m68k/ansi.h>
---
+++ sys/arch/x68k/include/ptrace.h
@@ -0,0 +1,4 @@
+/*	$NetBSD: ptrace.h,v 1.1.1.1 1996/05/05 12:17:03 oki Exp $	*/
+
+/* Just use the common m68k definition */
+#include <m68k/ptrace.h>
