e485f69d55490c5393a8bcda6b2595d32f6f309e
oki <oki@NetBSD.org>
1996-05-05 12:17:03 +0000

    NetBSD/x68k, by Masaru Oki.

---
+++ sys/arch/x68k/include/cpufunc.h
@@ -0,0 +1,8 @@
+/*	$NetBSD: cpufunc.h,v 1.1.1.1 1996/05/05 12:17:03 oki Exp $	*/
+
+/*
+ * Functions to provide access to special cpu instructions.
+ */
+#ifndef _MACHINE_CPUFUNC_H_
+#define _MACHINE_CPUFUNC_H_
+#endif  /* !_MACHINE_CPUFUNC_H_ */
