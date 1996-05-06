607e1d2f2afe4976e62a17b6764ea1c3b151ba8a
gwr <gwr@NetBSD.org>
1996-05-06 20:05:24 +0000

    Fix "prototype disagrees about inline" error.

--- sys/arch/m68k/m68k/process_machdep.c
+++ sys/arch/m68k/m68k/process_machdep.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: process_machdep.c,v 1.16 1996/04/29 20:50:31 leo Exp $	*/
+/*	$NetBSD: process_machdep.c,v 1.17 1996/05/06 20:05:24 gwr Exp $	*/
 
 /*
  * Copyright (c) 1993 Christopher G. Demetriou
@@ -65,8 +65,8 @@
 #ifdef cpu_set_init_frame
 extern char kstack[];		/* XXX */
 #endif
-static struct frame   *process_frame __P((struct proc *p));
-static struct fpframe *process_fpframe __P((struct proc *p));
+static inline struct frame   *process_frame __P((struct proc *p));
+static inline struct fpframe *process_fpframe __P((struct proc *p));
 
 static inline struct frame *
 process_frame(p)
