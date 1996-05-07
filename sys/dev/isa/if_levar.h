5c3182e45f4eade63527174cf4a68e3acbe75898
thorpej <thorpej>
1996-05-07 01:50:04 +0000

    Make the MI LANCE driver standalone, and use cfattach to resolve naming conflicts between bus attachments on ports that can have multiple instances of the LANCE.
    Break up ISA LANCE driver into ISA and PCI front-ends.
    
    Changed struct ifnet to have a pointer to the softc of the underlying
    device and a printable "external name" (name + unit number), thus eliminating
    if_name and if_unit.  Updated interface to (*if_watchdog)() and (*if_start)()
    to take a struct ifnet *, rather than a unit number.

--- sys/dev/isa/if_levar.h
+++ sys/dev/isa/if_levar.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: if_levar.h,v 1.4 1996/04/29 20:03:23 christos Exp $	*/
+/*	$NetBSD: if_levar.h,v 1.5 1996/05/07 01:50:07 thorpej Exp $	*/
 
 /*
  * LANCE Ethernet driver header file
@@ -20,7 +20,6 @@
 
 #define	NE2100		2
 #define	PCnet_ISA	4
-#define	PCnet_PCI	5
 #define	NE2100_RDP	0x10
 #define	NE2100_RAP	0x12
 
@@ -45,44 +44,10 @@
  * This structure contains the output queue for the interface, its address, ...
  */
 struct le_softc {
-	struct	device sc_dev;		/* base structure */
-	struct	arpcom sc_arpcom;	/* Ethernet common part */
-
-	void	(*sc_copytodesc)	/* Copy to descriptor */
-			__P((struct le_softc *, void *, int, int));
-	void	(*sc_copyfromdesc)	/* Copy from descriptor */
-			__P((struct le_softc *, void *, int, int));
-	void	(*sc_copytobuf)		/* Copy to buffer */
-			__P((struct le_softc *, void *, int, int));
-	void	(*sc_copyfrombuf)	/* Copy from buffer */
-			__P((struct le_softc *, void *, int, int));
-	void	(*sc_zerobuf)		/* and Zero bytes in buffer */
-			__P((struct le_softc *, int, int));
-
-	u_int16_t sc_conf3;		/* CSR3 value */
-
-	void	*sc_mem;		/* base address of RAM -- CPU's view */
-	u_long	sc_addr;		/* base address of RAM -- LANCE's view */
-	u_long	sc_memsize;		/* size of RAM */
-
-	int	sc_nrbuf;		/* number of receive buffers */
-	int	sc_ntbuf;		/* number of transmit buffers */
-	int	sc_last_rd;
-	int	sc_first_td, sc_last_td, sc_no_td;
-
-	int	sc_initaddr;
-	int	sc_rmdaddr;
-	int	sc_tmdaddr;
-	int	sc_rbufaddr;
-	int	sc_tbufaddr;
-
-#ifdef LEDEBUG
-	int	sc_debug;
-#endif
+	struct	am7990_softc sc_am7990;	/* glue to MI code */
 
 	void	*sc_ih;
 	bus_io_handle_t sc_ioh;
-	void	*sc_sh;
 	int	sc_card;
-	int	sc_rap, sc_rdp;		/* LANCE registers */
+	int	sc_rap, sc_rdp;		/* offsets to LANCE registers */
 };
