273e6cc624d989233c7614745db4831f691d6e99
jtc <jtc@NetBSD.org>
1996-05-07 18:32:22 +0000

    Include appropriate header files to bring prototypes into scope. Removed explicit errno declarations.

--- usr.bin/indent/indent.c
+++ usr.bin/indent/indent.c
@@ -43,7 +43,7 @@ char copyright[] =
 
 #ifndef lint
 /*static char sccsid[] = "from: @(#)indent.c	5.16 (Berkeley) 2/26/91";*/
-static char rcsid[] = "$Id: indent.c,v 1.2 1993/08/01 18:14:33 mycroft Exp $";
+static char rcsid[] = "$Id: indent.c,v 1.3 1996/05/07 18:32:28 jtc Exp $";
 #endif /* not lint */
 
 #include <sys/param.h>
@@ -55,6 +55,7 @@ static char rcsid[] = "$Id: indent.c,v 1.2 1993/08/01 18:14:33 mycroft Exp $";
 #include "indent_globs.h"
 #include "indent_codes.h"
 #include <ctype.h>
+#include <errno.h>
 
 char       *in_name = "Standard Input";	/* will always point to name of input
 					 * file */
@@ -1174,9 +1175,6 @@ bakcopy()
 err(msg)
 	char *msg;
 {
-	extern int errno;
-	char *strerror();
-
 	(void)fprintf(stderr, "indent: %s: %s\n", msg, strerror(errno));
 	exit(1);
 }
