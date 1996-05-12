db7bbcf4ab527776ca6401e4a5e3e6912a34e941
mark <mark@NetBSD.org>
1996-05-12 21:27:52 +0000

    Define FLT_ROUNDS properly. Guard #defines with #ifndef / #endif

--- sys/arch/arm32/include/float.h
+++ sys/arch/arm32/include/float.h
@@ -1,8 +1,17 @@
-/* $NetBSD: float.h,v 1.2 1996/03/14 23:11:13 mark Exp $ */
+/* $NetBSD: float.h,v 1.3 1996/05/12 21:27:52 mark Exp $ */
 
 /*
- * Copyright (c) 1989 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1992, 1993
+ *	The Regents of the University of California.  All rights reserved.
+ *
+ * This software was developed by the Computer Systems Engineering group
+ * at Lawrence Berkeley Laboratory under DARPA contract BG 91-66 and
+ * contributed to Berkeley.
+ *
+ * All advertising materials mentioning features or use of this software
+ * must display the following acknowledgement:
+ *	This product includes software developed by the University of
+ *	California, Lawrence Berkeley Laboratory.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -32,20 +41,27 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)float.h	7.1 (Berkeley) 5/8/90
+ *	@(#)float.h	8.1 (Berkeley) 6/11/93
  */
 
-/*extern int __flt_rounds();*/
+#ifndef _ARM32_FLOAT_H_
+#define _ARM32_FLOAT_H_
+
+#include <sys/cdefs.h>
+
+__BEGIN_DECLS
+extern int __flt_rounds();
+__END_DECLS
+
 #define FLT_RADIX	2		/* b */
-/*#define FLT_ROUNDS	__flt_rounds()*/
-#define FLT_ROUNDS	1
+#define FLT_ROUNDS	__flt_rounds()
 
 #define FLT_MANT_DIG	24		/* p */
 #define FLT_EPSILON	1.19209290E-07F	/* b**(1-p) */
 #define FLT_DIG		6		/* floor((p-1)*log10(b))+(b == 10) */
-#define FLT_MIN_EXP	(-125)		/* emin */
+#define FLT_MIN_EXP	-125		/* emin */
 #define FLT_MIN		1.17549435E-38F	/* b**(emin-1) */
-#define FLT_MIN_10_EXP	(-37)		/* ceil(log10(b**(emin-1))) */
+#define FLT_MIN_10_EXP	-37		/* ceil(log10(b**(emin-1))) */
 #define FLT_MAX_EXP	128		/* emax */
 #define FLT_MAX		3.40282347E+38F	/* (1-b**(-p))*b**emax */
 #define FLT_MAX_10_EXP	38		/* floor(log10((1-b**(-p))*b**emax)) */
@@ -53,9 +69,9 @@
 #define DBL_MANT_DIG	53
 #define DBL_EPSILON	2.2204460492503131E-16
 #define DBL_DIG		15
-#define DBL_MIN_EXP	(-1021)
+#define DBL_MIN_EXP	-1021
 #define DBL_MIN		2.2250738585072014E-308
-#define DBL_MIN_10_EXP	(-307)
+#define DBL_MIN_10_EXP	-307
 #define DBL_MAX_EXP	1024
 #define DBL_MAX		1.7976931348623157E+308
 #define DBL_MAX_10_EXP	308
@@ -69,3 +85,5 @@
 #define LDBL_MAX_EXP	DBL_MAX_EXP
 #define LDBL_MAX	DBL_MAX
 #define LDBL_MAX_10_EXP	DBL_MAX_10_EXP
+
+#endif	/* _ARM32_FLOAT_H_ */
