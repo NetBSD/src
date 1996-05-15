b4ca145026d6b4b7227947b610d87a3a71bc4352
leo <leo@NetBSD.org>
1996-05-15 09:21:29 +0000

    Get in sync with the mac68k version of the 5380 driver. Also increase the maximum values of wait_req_xx() functions so some old (slow) scsi-1 drives will work.

--- sys/arch/atari/dev/ncr5380var.h
+++ sys/arch/atari/dev/ncr5380var.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: ncr5380var.h,v 1.1 1996/02/22 21:07:12 leo Exp $	*/
+/*	$NetBSD: ncr5380var.h,v 1.2 1996/05/15 09:21:54 leo Exp $	*/
 
 /*
  * Copyright (c) 1996 Leo Weppelman.
@@ -32,75 +32,6 @@
 
 #ifndef _NCR5380VAR_H
 #define _NCR5380VAR_H
-
-struct	ncr_softc {
-	struct	device		sc_dev;
-	struct	scsi_link	sc_link;
-
-	/*
-	 * Some (pre-SCSI2) devices don't support select with ATN.
-	 * If the device responds to select with ATN by going into
-	 * command phase (ignoring ATN), then we flag it in the
-	 * following bitmask.
-	 * We also keep track of which devices have been selected
-	 * before.  This allows us to not even try raising ATN if
-	 * the target doesn't respond to it the first time.
-	 */
-	u_int8_t	sc_noselatn;
-	u_int8_t	sc_selected;
-};
-
-/*
- * Max. number of dma-chains per request
- */
-#define	MAXDMAIO	(MAXPHYS/NBPG + 1)
-
-/*
- * Some requests are not contiguous in physical memory. We need to break them
- * up into contiguous parts for DMA.
- */
-struct dma_chain {
-	u_int	dm_count;
-	u_long	dm_addr;
-};
-
-/*
- * Define our issue, free and disconnect queue's.
- */
-typedef struct	req_q {
-    struct req_q	*next;	    /* next in free, issue or discon queue  */
-    struct req_q	*link;	    /* next linked command to execute       */
-    struct scsi_xfer	*xs;	    /* request from high-level driver       */
-    u_short		dr_flag;    /* driver state			    */
-    u_char		phase;	    /* current SCSI phase		    */
-    u_char		msgout;	    /* message to send when requested       */
-    u_char		targ_id;    /* target for command		    */
-    u_char		targ_lun;   /* lun for command			    */
-    u_char		status;	    /* returned status byte		    */
-    u_char		message;    /* returned message byte		    */
-    u_char		*bounceb;   /* allocated bounce buffer		    */
-    u_char		*bouncerp;  /* bounce read-pointer		    */
-    struct dma_chain	dm_chain[MAXDMAIO];
-    struct dma_chain	*dm_cur;    /* current dma-request		    */
-    struct dma_chain	*dm_last;   /* last dma-request			    */
-    long		xdata_len;  /* length of transfer		    */
-    u_char		*xdata_ptr; /* virtual address of transfer	    */
-    struct scsi_generic	xcmd;	    /* command to execute		    */
-} SC_REQ;
-
-/*
- * Values for dr_flag:
- */
-#define	DRIVER_IN_DMA	0x01	/* Non-polled DMA activated		*/
-#define	DRIVER_AUTOSEN	0x02	/* Doing automatic sense		*/
-#define	DRIVER_NOINT	0x04	/* We are booting: no interrupts	*/
-#define	DRIVER_DMAOK	0x08	/* DMA can be used on this request	*/
-#define	DRIVER_BOUNCING	0x10	/* Using the bounce buffer		*/
-#define DRIVER_LINKCHK	0x20	/* Doing the linked command check	*/
-
-static SC_REQ	*issue_q   = NULL;	/* Commands waiting to be issued*/
-static SC_REQ	*discon_q  = NULL;	/* Commands disconnected	*/
-static SC_REQ	*connected = NULL;	/* Command currently connected	*/
 
 /*
  * Various debug definitions
