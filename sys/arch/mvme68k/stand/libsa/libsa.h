62d3ebfd70f79b72e3bf6d46043569708509a448
chuck <chuck@NetBSD.org>
1996-05-17 20:59:53 +0000

    bugdev.c: from Dale Rahn (bug device access) exec_mvme.c: from sun3 port, adapted by me.   I also fixed a bug that 	prevented ZMAGIC kernels from running. libsa.h: new file by me parse_args.c: by Theo de Raadt from OpenBSD with slight revisions from by me 	to make it fit the new format.

---
+++ sys/arch/mvme68k/stand/libsa/libsa.h
@@ -0,0 +1,24 @@
+/*	$NetBSD: libsa.h,v 1.1 1996/05/17 21:00:14 chuck Exp $	*/
+
+/*
+ * libsa prototypes 
+ */
+
+#include "libbug.h"
+
+/* bugdev.c */
+int bugscopen __P((struct open_file *, ...));
+int bugscclose __P((struct open_file *));
+int bugscioctl __P((struct open_file *, u_long, void *));
+int bugscstrategy __P((void *, int, daddr_t, size_t, void *, size_t *));
+
+/* exec_mvme.c */
+void exec_mvme __P((char *, int));
+
+/* parse_args.c */
+void parse_args __P((char **, int *));
+
+#ifndef RB_NOSYM
+#define RB_NOSYM 0x400
+#endif
+
