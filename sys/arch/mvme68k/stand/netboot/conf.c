48715fbaad0fcaa1a7349cabf14d7831d13eebc5
chuck <chuck@NetBSD.org>
1996-05-17 21:08:25 +0000

    Makefile: update from OpenBSD boot.c: update from sun3 port, with changes for my new boot code put in. conf.c: config from openbsd, by Theo de Raadt dev_net.c: update from openbsd (standard dev_net with changes from theo) dev_net.h: proto from openbsd

--- sys/arch/mvme68k/stand/netboot/conf.c
+++ sys/arch/mvme68k/stand/netboot/conf.c
@@ -1,21 +1,32 @@
-/*	$NetBSD: conf.c,v 1.2 1995/10/12 20:39:53 chuck Exp $	*/
+/*	$NetBSD: conf.c,v 1.3 1996/05/17 21:08:38 chuck Exp $ */
 
 #include <sys/types.h>
 #include <netinet/in.h>
+#include <netinet/in_systm.h>
 
-#include "stand.h"
-#include "nfs.h"
-#include "dev_net.h"
+#include <stand.h>
+#include <nfs.h>
+#include <dev_net.h>
 
 struct fs_ops file_system[] = {
 	{ nfs_open, nfs_close, nfs_read, nfs_write, nfs_seek, nfs_stat },
 };
-int nfsys = 1;
+int nfsys = sizeof(file_system) / sizeof(file_system[0]);
 
 struct devsw devsw[] = {
 	{ "net",  net_strategy,  net_open,  net_close,  net_ioctl },
 };
-int	ndevs = 1;
+int	ndevs = sizeof(devsw) / sizeof(devsw[0]);
+
+extern struct netif_driver le_driver;
+extern struct netif_driver ie_driver;
+
+struct netif_driver *netif_drivers[] = {
+	&le_driver,
+	&ie_driver,
+};
+int n_netif_drivers = sizeof(netif_drivers) / sizeof(netif_drivers[0]);
+
 
 /* XXX */
 int netif_debug;
