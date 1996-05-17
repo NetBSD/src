6d472208c026f2b1ffaeaa9a542794c4a5a49d12
chuck <chuck@NetBSD.org>
1996-05-17 21:20:11 +0000

    bump

--- sys/arch/mvme68k/stand/netboot/version.c
+++ sys/arch/mvme68k/stand/netboot/version.c
@@ -1,9 +1,7 @@
-/*	$NetBSD: version.c,v 1.1.1.1 1995/07/25 23:12:26 chuck Exp $ */
+/*	$NetBSD: version.c,v 1.2 1996/05/17 21:20:11 chuck Exp $ */
 
 /*
- *	NOTE ANY CHANGES YOU MAKE TO THE BOOTBLOCKS HERE.
- *
- *	1.1
+ * eap eap eap
  */
 
-char *version = "$Revision: 1.1.1.1 $";
+char *version = "$Revision: 1.2 $";
