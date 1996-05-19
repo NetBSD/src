7469cf4c88ee74d51c7a225e7d0059ee11a9b1e5
jonathan <jonathan@NetBSD.org>
1996-05-19 18:24:33 +0000

    Add port number encodings.

--- sys/arch/pmax/tc/sccvar.h
+++ sys/arch/pmax/tc/sccvar.h
@@ -1,9 +1,26 @@
-/*	$NetBSD: sccvar.h,v 1.1 1995/08/04 00:22:02 jonathan Exp $	*/
+/*	$NetBSD: sccvar.h,v 1.2 1996/05/19 18:24:33 jonathan Exp $	*/
 
 /*
- * external declarations from DECstation scc driver
+ *
+ */
+extern int		sccGetc __P((dev_t));
+extern void		sccPutc __P((dev_t, int));
+extern int		sccparam __P((struct tty *, struct termios *));
+
+/*
+ * Minor device numbers for scc. Weird because B channel comes
+ * first and the A channels are wired for keyboard/mouse and the
+ * B channels for the comm port(s).
+ *
+ * XXX
+ *
+ * Even that is not true on the Personal Decstation, which has
+ * a "desktop bus" for keyboard/mouse, and brings A and B channels
+ * out to the bulkhead.  XXX more thought.
  */
 
-extern int sccGetc __P ((dev_t dev));
-extern int sccparam __P((register struct tty *tp, register struct termios *t));
-extern void sccPutc __P ((dev_t dev, int c));
+#define	SCCCOMM2_PORT	0x0
+#define	SCCMOUSE_PORT	0x1
+#define	SCCCOMM3_PORT	0x2
+#define	SCCKBD_PORT	0x3
+
