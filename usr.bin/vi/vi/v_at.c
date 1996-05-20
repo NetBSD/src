a72e4a6ab6a60976905a0d48e49e1c5088cea0ea
mrg <mrg@NetBSD.org>
1996-05-20 03:47:00 +0000

    merge in nvi 1.66

--- usr.bin/vi/vi/v_at.c
+++ usr.bin/vi/vi/v_at.c
@@ -1,38 +1,16 @@
 /*-
- * Copyright (c) 1991, 1993, 1994
+ * Copyright (c) 1992, 1993, 1994
  *	The Regents of the University of California.  All rights reserved.
+ * Copyright (c) 1992, 1993, 1994, 1995, 1996
+ *	Keith Bostic.  All rights reserved.
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed by the University of
- *	California, Berkeley and its contributors.
- * 4. Neither the name of the University nor the names of its contributors
- *    may be used to endorse or promote products derived from this software
- *    without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
+ * See the LICENSE file for redistribution information.
  */
 
+#include "config.h"
+
 #ifndef lint
-static char sccsid[] = "@(#)v_at.c	8.5 (Berkeley) 3/8/94";
+static const char sccsid[] = "@(#)v_at.c	10.8 (Berkeley) 4/27/96";
 #endif /* not lint */
 
 #include <sys/types.h>
@@ -40,33 +18,92 @@ static char sccsid[] = "@(#)v_at.c	8.5 (Berkeley) 3/8/94";
 #include <sys/time.h>
 
 #include <bitstring.h>
+#include <ctype.h>
 #include <limits.h>
-#include <signal.h>
 #include <stdio.h>
-#include <string.h>
-#include <termios.h>
 
-#include "compat.h"
-#include <db.h>
-#include <regex.h>
-
+#include "../common/common.h"
 #include "vi.h"
-#include "excmd.h"
-#include "vcmd.h"
 
 /*
  * v_at -- @
  *	Execute a buffer.
+ *
+ * PUBLIC: int v_at __P((SCR *, VICMD *));
  */
 int
-v_at(sp, ep, vp)
+v_at(sp, vp)
 	SCR *sp;
-	EXF *ep;
-	VICMDARG *vp;
+	VICMD *vp;
 {
-	EXCMDARG cmd;
+	CB *cbp;
+	CHAR_T name;
+	TEXT *tp;
+	size_t len;
+	char nbuf[20];
 
-        SETCMDARG(cmd, C_AT, 0, OOBLNO, OOBLNO, 0, NULL);
-	cmd.buffer = vp->buffer;
-        return (sp->s_ex_cmd(sp, ep, &cmd, &vp->m_final));
+	/*
+	 * !!!
+	 * Historically, [@*]<carriage-return> and [@*][@*] executed the most
+	 * recently executed buffer in ex mode.  In vi mode, only @@ repeated
+	 * the last buffer.  We change historic practice and make @* work from
+	 * vi mode as well, it's simpler and more consistent.
+	 *
+	 * My intent is that *[buffer] will, in the future, pass the buffer to
+	 * whatever interpreter is loaded.
+	 */
+	name = F_ISSET(vp, VC_BUFFER) ? vp->buffer : '@';
+	if (name == '@' || name == '*') {
+		if (!F_ISSET(sp, SC_AT_SET)) {
+			ex_emsg(sp, NULL, EXM_NOPREVBUF);
+			return (1);
+		}
+		name = sp->at_lbuf;
+	}
+	F_SET(sp, SC_AT_SET);
+
+	CBNAME(sp, cbp, name);
+	if (cbp == NULL) {
+		ex_emsg(sp, KEY_NAME(sp, name), EXM_EMPTYBUF);
+		return (1);
+	}
+
+	/* Save for reuse. */
+	sp->at_lbuf = name;
+
+	/*
+	 * The buffer is executed in vi mode, while in vi mode, so simply
+	 * push it onto the terminal queue and continue.
+	 *
+	 * !!!
+	 * Historic practice is that if the buffer was cut in line mode,
+	 * <newlines> were appended to each line as it was pushed onto
+	 * the stack.  If the buffer was cut in character mode, <newlines>
+	 * were appended to all lines but the last one.
+	 *
+	 * XXX
+	 * Historic practice is that execution of an @ buffer could be
+	 * undone by a single 'u' command, i.e. the changes were grouped
+	 * together.  We don't get this right; I'm waiting for the new DB
+	 * logging code to be available.
+	 */
+	for (tp = cbp->textq.cqh_last;
+	    tp != (void *)&cbp->textq; tp = tp->q.cqe_prev)
+		if ((F_ISSET(cbp, CB_LMODE) ||
+		    tp->q.cqe_next != (void *)&cbp->textq) &&
+		    v_event_push(sp, NULL, "\n", 1, 0) ||
+		    v_event_push(sp, NULL, tp->lb, tp->len, 0))
+			return (1);
+
+	/*
+	 * !!!
+	 * If any count was supplied, it applies to the first command in the
+	 * at buffer.
+	 */
+	if (F_ISSET(vp, VC_C1SET)) {
+		len = snprintf(nbuf, sizeof(nbuf), "%lu", vp->count);
+		if (v_event_push(sp, NULL, nbuf, len, 0))
+			return (1);
+	}
+	return (0);
 }
