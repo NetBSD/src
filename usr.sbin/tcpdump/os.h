d65ddbd4e0610999b09663f82f02c3652455cfb7
fvdl <fvdl@NetBSD.org>
1996-05-20 00:41:00 +0000

    Grok NFSv3 packets. Needs a bit of work but is ok for now. Enable dumping of NFS packets over TCP. Upgrade to 3.0.4 while at it. 3.0.4 fixes previous fix made to util.c too; 'official' fix applied instead (very minor difference).
    Fixes to be sent to maintainers RSN.

--- usr.sbin/tcpdump/os.h
+++ usr.sbin/tcpdump/os.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: os.h,v 1.2 1995/03/06 19:10:56 mycroft Exp $	*/
+/*	$NetBSD: os.h,v 1.3 1996/05/20 00:41:06 fvdl Exp $	*/
 
 /*
  * Copyright (c) 1990, 1991, 1993, 1994
@@ -60,3 +60,10 @@
 #ifndef	RIPCMD_POLLENTRY
 #define	RIPCMD_POLLENTRY 6
 #endif
+
+typedef int64_t         int64;
+typedef u_int64_t       u_int64;
+
+#define   INT64_FORMAT   "%qd"
+#define U_INT64_FORMAT   "%qu"
+#define HEX_INT64_FORMAT "%qx"
