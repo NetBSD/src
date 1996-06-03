8ecb17b8bc54d71196a18e4149499d25494b94b4
mark <mark>
1996-06-03 21:35:10 +0000

    RCS Id police.

--- sys/arch/arm32/vidc/console/fonts/font_bold.h
+++ sys/arch/arm32/vidc/console/fonts/font_bold.h
@@ -1,4 +1,4 @@
-/* $NetBSD: font_bold.h,v 1.1 1996/01/31 23:20:07 mark Exp $ */
+/* $NetBSD: font_bold.h,v 1.2 1996/06/03 21:35:10 mark Exp $ */
 
 /*
  * Copyright (c) 1994 Mark Brinicombe.
@@ -41,11 +41,8 @@
  * Font for physical console driver
  *
  * Created      : 18/09/94
- * Last updated : 15/10/94
  *
  * Based on kate/display/14bold.h
- *
- *    $Id: font_bold.h,v 1.1 1996/01/31 23:20:07 mark Exp $
  */
  
 unsigned char font_terminal_14bold_data[] = {
