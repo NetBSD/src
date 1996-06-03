eae1ac5ded753ef6867053a7d26a81dd738282ed
mark <mark@NetBSD.org>
1996-06-03 21:35:10 +0000

    RCS Id police.

--- sys/arch/arm32/vidc/console/fonts/font_italic.h
+++ sys/arch/arm32/vidc/console/fonts/font_italic.h
@@ -1,4 +1,4 @@
-/* $NetBSD: font_italic.h,v 1.1 1996/01/31 23:20:13 mark Exp $ */
+/* $NetBSD: font_italic.h,v 1.2 1996/06/03 21:35:12 mark Exp $ */
 
 /*
  * Copyright (c) 1994 Mark Brinicombe.
@@ -41,11 +41,8 @@
  * Font for physical console driver
  *
  * Created      : 18/09/94
- * Last updated : 15/10/94
  *
  * Based on kate/display/14norm.h
- *
- *    $Id: font_italic.h,v 1.1 1996/01/31 23:20:13 mark Exp $
  */
  
 unsigned char font_terminal_14italic_data[] = {
--- sys/arch/arm32/vidc/console/fonts/font_wide.h
+++ sys/arch/arm32/vidc/console/fonts/font_wide.h
@@ -1,4 +1,4 @@
-/* $NetBSD: font_wide.h,v 1.1 1996/01/31 23:20:22 mark Exp $ */
+/* $NetBSD: font_wide.h,v 1.2 1996/06/03 21:35:15 mark Exp $ */
 
 /*
  * Copyright (c) 1994 Mark Brinicombe.
@@ -41,11 +41,8 @@
  * Font for physical console driver
  *
  * Created      : 18/09/94
- * Last updated : 15/10/94
  *
  * Based on kate/display/14widen.h
- *
- *    $Id: font_wide.h,v 1.1 1996/01/31 23:20:22 mark Exp $
  */
  
 unsigned char font_terminal_14widen_data[] = {
