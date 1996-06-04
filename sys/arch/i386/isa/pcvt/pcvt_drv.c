0750c59103a3359112ee5778c77c78067cc55d4f
cgd <cgd@NetBSD.org>
1996-06-04 19:47:51 +0000

    don't tty_attach() the mouse emulator tty.

--- sys/arch/i386/isa/pcvt/pcvt_drv.c
+++ sys/arch/i386/isa/pcvt/pcvt_drv.c
@@ -263,9 +263,6 @@ pcattach(struct isa_device *dev)
 #if PCVT_EMU_MOUSE
 #if PCVT_NETBSD
 	pc_tty[totalscreens] = ttymalloc(); /* the mouse emulator tty */
-#if PCVT_NETBSD > 120
-	tty_attach(pc_tty[totalscreens]);
-#endif /* PCVT_NETBSD > 120 */
 #else /* !PCVT_NETBSD */
 	/* the mouse emulator tty */
 	pc_tty[totalscreens] = ttymalloc(pccons[totalscreens]);
