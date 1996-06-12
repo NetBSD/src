bf884fa9be0f64e73fde60cd3a6deb63562ef2a8
pk <pk>
1996-06-12 20:32:46 +0000

    Pull down from trunk: Fixes in CMP_PTE_USER_READ4M macro.

--- sys/arch/sparc/sparc/locore.s
+++ sys/arch/sparc/sparc/locore.s
@@ -1273,17 +1273,23 @@ Lpanic_red:
 #define	PTE_OF_ADDR4M(addr, pte, bad, page_offset) \
 	andn	addr, page_offset, pte
 
+/* note: pmap currently does not use the PPROT_R_R and PPROT_RW_RW cases */
 #define CMP_PTE_USER_READ4M(pte) \
 	or	pte, ASI_SRMMUFP_L3, pte; \
 	lda	[pte] ASI_SRMMUFP, pte; \
-	and	pte, 0x1f, pte; \
-	cmp	pte, (SRMMU_TEPTE | PPROT_RX_RX)
+	and	pte, (SRMMU_TETYPE | SRMMU_PROT_MASK), pte; \
+	cmp	pte, (SRMMU_TEPTE | PPROT_RWX_RWX); \
+	be	8f; nop; \
+	cmp	pte, (SRMMU_TEPTE | PPROT_RX_RX); \
+8:
 
+
+/* note: PTE bit 4 set implies no user writes */
 #define CMP_PTE_USER_WRITE4M(pte) \
 	or	pte, ASI_SRMMUFP_L3, pte; \
 	lda	[pte] ASI_SRMMUFP, pte; \
-	and	pte, 0x1f, pte; \
-	cmp	pte, (SRMMU_TEPTE | PPROT_RWX_RWX)
+	and	pte, (SRMMU_TETYPE | 0x14), pte; \
+	cmp	pte, (SRMMU_TEPTE | PPROT_WRITE)
 #endif /* 4m */
 
 #if defined(SUN4M) && !(defined(SUN4C) || defined(SUN4))
