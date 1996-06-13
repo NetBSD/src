f4f3a1cc7a32ed4fadab772be6b271bac0d3b371
cgd <cgd@NetBSD.org>
1996-06-13 18:35:10 +0000

    pull up from trunk: >clean up copyright notices

--- sys/arch/alpha/tc/tc_3000_500.c
+++ sys/arch/alpha/tc/tc_3000_500.c
@@ -1,7 +1,7 @@
-/*	$NetBSD: tc_3000_500.c,v 1.4.4.2 1996/06/05 00:39:08 cgd Exp $	*/
+/*	$NetBSD: tc_3000_500.c,v 1.4.4.3 1996/06/13 18:35:35 cgd Exp $	*/
 
 /*
- * Copyright (c) 1994, 1995 Carnegie-Mellon University.
+ * Copyright (c) 1994, 1995, 1996 Carnegie-Mellon University.
  * All rights reserved.
  *
  * Author: Chris G. Demetriou
