af1b213133d6de46d49545b2686b60691dc87d1b
cgd <cgd@NetBSD.org>
1996-06-14 20:38:11 +0000

    add a delay before serial console is initialized, to allow any outstanding PROM output to drain.  Move the console-type printfs so that they're only printed if console selection fails.

--- sys/arch/alpha/alpha/dec_kn20aa.c
+++ sys/arch/alpha/alpha/dec_kn20aa.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: dec_kn20aa.c,v 1.7 1996/06/13 04:53:59 cgd Exp $	*/
+/*	$NetBSD: dec_kn20aa.c,v 1.8 1996/06/14 20:38:14 cgd Exp $	*/
 
 /*
  * Copyright (c) 1995, 1996 Carnegie-Mellon University.
@@ -76,9 +76,6 @@ dec_kn20aa_consinit()
 
 	ctb = (struct ctb *)(((caddr_t)hwrpb) + hwrpb->rpb_ctb_off);
 
-	printf("ctb->ctb_term_type = 0x%lx\n", ctb->ctb_term_type);
-	printf("ctb->ctb_turboslot = 0x%lx\n", ctb->ctb_turboslot);
-
 	switch (ctb->ctb_term_type) {
 	case 2: 
 		/* serial console ... */
@@ -93,6 +90,9 @@ dec_kn20aa_consinit()
 			extern void comcnpollc __P((dev_t, int));
 			static struct consdev comcons = { NULL, NULL,
 			    comcngetc, comcnputc, comcnpollc, NODEV, 1 };
+
+			/* Delay to allow PROM putchars to complete */
+			DELAY(10000);
 
 			comconsaddr = 0x3f8;
 			comconsinit = 0;
@@ -117,6 +117,9 @@ dec_kn20aa_consinit()
 		break;
 
 	default:
+		printf("ctb->ctb_term_type = 0x%lx\n", ctb->ctb_term_type);
+		printf("ctb->ctb_turboslot = 0x%lx\n", ctb->ctb_turboslot);
+
 		panic("consinit: unknown console type %d\n",
 		    ctb->ctb_term_type);
 	}
