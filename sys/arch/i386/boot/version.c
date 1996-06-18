998cd551410c6c742e72c889b8906f2b4620be20
mycroft <mycroft@NetBSD.org>
1996-06-18 07:51:26 +0000

    Increment the boot block version.

--- sys/arch/i386/boot/version.c
+++ sys/arch/i386/boot/version.c
@@ -1,7 +1,10 @@
-/*	$NetBSD: version.c,v 1.28 1995/12/23 17:21:23 perry Exp $	*/
+/*	$NetBSD: version.c,v 1.29 1996/06/18 07:51:26 mycroft Exp $	*/
 
 /*
  *	NOTE ANY CHANGES YOU MAKE TO THE BOOTBLOCKS HERE.
+ *
+ *	1.28 -> 1.29
+ *		various code cleanup. (mycroft)
  *
  *	1.27 -> 1.28
  *		fix gets to use real timeout instead of loop and do
@@ -54,4 +57,4 @@
  *		look in boot.c revision logs
  */
 
-char *version = "1.28";
+char *version = "1.29";
