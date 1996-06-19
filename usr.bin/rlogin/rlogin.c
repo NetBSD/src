fe706fa4cd90ce240b9a5c177a8191afb8d2f583
mrg <mrg@NetBSD.org>
1996-06-19 06:02:24 +0000

    use strncat/snprintf

--- usr.bin/rlogin/rlogin.c
+++ usr.bin/rlogin/rlogin.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: rlogin.c,v 1.9 1996/06/13 19:59:31 christos Exp $	*/
+/*	$NetBSD: rlogin.c,v 1.10 1996/06/19 06:02:24 mrg Exp $	*/
 
 /*
  * Copyright (c) 1983, 1990, 1993
@@ -43,7 +43,7 @@ static char copyright[] =
 #if 0
 static char sccsid[] = "@(#)rlogin.c	8.1 (Berkeley) 6/6/93";
 #else
-static char rcsid[] = "$NetBSD: rlogin.c,v 1.9 1996/06/13 19:59:31 christos Exp $";
+static char rcsid[] = "$NetBSD: rlogin.c,v 1.10 1996/06/19 06:02:24 mrg Exp $";
 #endif
 #endif /* not lint */
 
@@ -260,10 +260,14 @@ main(argc, argv)
 		exit(1);
 	}
 
-	(void)strcpy(term, (p = getenv("TERM")) ? p : "network");
+	(void)strncpy(term, (p = getenv("TERM")) ? p : "network",
+	    sizeof(term) - 1);
+	term[sizeof(term) - 1] = '\0';
 	if (tcgetattr(0, &tty) == 0) {
-		(void)strcat(term, "/");
-		(void)sprintf(term + strlen(term), "%d", cfgetospeed(&tty));
+		(void)strncat(term, "/", sizeof(term) - strlen(term));
+		(void)snprintf(term + strlen(term),
+		    sizeof(term) - strlen(term) - 1, "%d", cfgetospeed(&tty));
+		term[sizeof(term) - 1] = '\0';
 	}
 
 	(void)get_window_size(0, &winsize);
