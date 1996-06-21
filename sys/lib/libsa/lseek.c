4d4d7f257394e57922b0d9897358173b781035a5
pk <pk>
1996-06-21 20:09:03 +0000

    Allow seeks on raw devices. A `f_offset' field is added to the file structure which is maintained in lseek(), read() and write(), and passed along to the device's strategy routine.

--- sys/lib/libsa/lseek.c
+++ sys/lib/libsa/lseek.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: lseek.c,v 1.2 1994/10/26 05:44:51 cgd Exp $	*/
+/*	$NetBSD: lseek.c,v 1.3 1996/06/21 20:09:03 pk Exp $	*/
 
 /*-
  * Copyright (c) 1993
@@ -79,10 +79,23 @@ lseek(fd, offset, where)
 		return (-1);
 	}
 
-	/* seek is not supported on raw devices */
 	if (f->f_flags & F_RAW) {
-		errno = EOFFSET;
-		return ((off_t)-1);
+		/*
+		 * On RAW devices, update internal offset.
+		 */
+		switch (where) {
+		case SEEK_SET:
+			f->f_offset = offset;
+			break;
+		case SEEK_CUR:
+			f->f_offset += offset;
+			break;
+		case SEEK_END:
+		default:
+			errno = EOFFSET;
+			return (-1);
+		}
+		return (f->f_offset);
 	}
 
 	return (f->f_ops->seek)(f, offset, where);
