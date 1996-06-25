1bfaceef2170fd372c68a57060a4849de0f86c80
christos <christos>
1996-06-25 16:49:05 +0000

    - Add function callback capability when variables are set. - Add setvarsafe that returns an error instead of longjmp() to the   error code.

--- bin/sh/var.h
+++ bin/sh/var.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: var.h,v 1.9 1995/05/11 21:30:44 christos Exp $	*/
+/*	$NetBSD: var.h,v 1.10 1996/06/25 16:49:07 christos Exp $	*/
 
 /*-
  * Copyright (c) 1991, 1993
@@ -43,26 +43,30 @@
  */
 
 /* flags */
-#define VEXPORT		01	/* variable is exported */
-#define VREADONLY	02	/* variable cannot be modified */
-#define VSTRFIXED	04	/* variable struct is staticly allocated */
-#define VTEXTFIXED	010	/* text is staticly allocated */
-#define VSTACK		020	/* text is allocated on the stack */
-#define VUNSET		040	/* the variable is not set */
+#define VEXPORT		0x01	/* variable is exported */
+#define VREADONLY	0x02	/* variable cannot be modified */
+#define VSTRFIXED	0x04	/* variable struct is staticly allocated */
+#define VTEXTFIXED	0x08	/* text is staticly allocated */
+#define VSTACK		0x10	/* text is allocated on the stack */
+#define VUNSET		0x20	/* the variable is not set */
+#define VNOFUNC		0x40	/* don't call the callback function */
 
 
 struct var {
 	struct var *next;		/* next entry in hash list */
-	int flags;		/* flags are defined above */
-	char *text;		/* name=value */
+	int flags;			/* flags are defined above */
+	char *text;			/* name=value */
+	void (*func) __P((const char *));
+					/* function to be called when  */
+					/* the variable gets set/unset */
 };
 
 
 struct localvar {
-	struct localvar *next;	/* next local variable in list */
-	struct var *vp;		/* the variable that was made local */
-	int flags;		/* saved flags */
-	char *text;		/* saved text */
+	struct localvar *next;		/* next local variable in list */
+	struct var *vp;			/* the variable that was made local */
+	int flags;			/* saved flags */
+	char *text;			/* saved text */
 };
 
 
@@ -80,6 +84,9 @@ extern struct var vps2;
 #if ATTY
 extern struct var vterm;
 #endif
+#ifndef NO_HISTORY
+extern struct var vhistsize;
+#endif
 
 /*
  * The following macros access the values of the above variables.
@@ -95,6 +102,10 @@ extern struct var vterm;
 #define ps2val()	(vps2.text + 4)
 #if ATTY
 #define termval()	(vterm.text + 5)
+#endif
+#define optindval()	(voptind.text + 7)
+#ifndef NO_HISTORY
+#define histsizeval()	(vhistsize.text + 9)
 #endif
 
 #if ATTY
@@ -118,3 +129,4 @@ void mklocal __P((char *));
 void poplocalvars __P((void));
 int setvarcmd __P((int, char **));
 int unsetcmd __P((int, char **));
+int unsetvar __P((char *));
