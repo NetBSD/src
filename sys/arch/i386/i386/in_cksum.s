5cca2214a0c5ce6191f17d1c4f0aeef17bff436b
mycroft <mycroft>
1996-07-03 13:07:17 +0000

    Minor cleanup.

--- sys/arch/i386/i386/in_cksum.s
+++ sys/arch/i386/i386/in_cksum.s
@@ -1,12 +1,55 @@
+/*	$NetBSD: in_cksum.s,v 1.2 1996/07/03 13:07:17 mycroft Exp $	*/
+
+/*-
+ * Copyright (c) 1994, 1995, 1996 Charles M. Hannum.  All rights reserved.
+ * Copyright (c) 1990 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *      This product includes software developed by the University of
+ *      California, Berkeley and its contributors.
+ * 4. Neither the name of the University nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
 #include <machine/asm.h>
 #include "assym.h"
 
-# %eax = sum
-# %ebx = m
-# %cl = rotate
-# %edx = len
-# %esi = mlen
-# %ebp = w
+/*
+ * Checksum routine for Internet Protocol family headers.
+ *
+ * in_cksum(m, len)
+ *
+ * Registers used:
+ * %eax = sum
+ * %ebx = m
+ * %cl = rotation count to unswap
+ * %edx = len
+ * %esi = m->m_len
+ * %ebp = m->m_data
+ */
 
 #define	SWAP \
 	roll	$8, %eax	; \
@@ -29,23 +72,22 @@
 #define	ADDWORD \
 	addw	(%ebp), %ax
 
-#define	REDUCE \
-	movzwl	%ax, %esi	; \
-	shrl	$16, %eax	; \
-	addw	%si, %ax	; \
-	adcw	$0, %ax
-
 #define	ADD(n) \
 	addl	n(%ebp), %eax
 
 #define	ADC(n) \
 	adcl	n(%ebp), %eax
 
-ENTRY(myc2_in_cksum)
+#define	REDUCE \
+	movzwl	%ax, %esi	; \
+	shrl	$16, %eax	; \
+	addw	%si, %ax	; \
+	adcw	$0, %ax
+
+ENTRY(in_cksum)
 	pushl	%ebp
 	pushl	%ebx
 	pushl	%esi
-	/*pushl	%edi*/
 
 	movl	16(%esp), %ebx
 	movl	20(%esp), %edx
@@ -232,7 +274,6 @@ done:
 	notw	%ax
 
 return:
-	/*popl	%edi*/
 	popl	%esi
 	popl	%ebx
 	popl	%ebp
