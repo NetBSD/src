0fde995972c419c567734a98ac726b5068e64ed5
fvdl <fvdl>
1996-07-07 12:25:47 +0000

    Use the right time for v3 setattr operation.

--- sys/nfs/nfs_vnops.c
+++ sys/nfs/nfs_vnops.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: nfs_vnops.c,v 1.62 1996/05/11 18:26:49 mycroft Exp $	*/
+/*	$NetBSD: nfs_vnops.c,v 1.63 1996/07/07 12:25:47 fvdl Exp $	*/
 
 /*
  * Copyright (c) 1989, 1993
@@ -709,7 +709,7 @@ nfs_setattrrpc(vp, vap, cred, procp)
 			if (vap->va_mtime.tv_sec != time.tv_sec) {
 				nfsm_build(tl, u_int32_t *, 3 * NFSX_UNSIGNED);
 				*tl++ = txdr_unsigned(NFSV3SATTRTIME_TOCLIENT);
-				txdr_nfsv3time(&vap->va_atime, tl);
+				txdr_nfsv3time(&vap->va_mtime, tl);
 			} else {
 				nfsm_build(tl, u_int32_t *, NFSX_UNSIGNED);
 				*tl = txdr_unsigned(NFSV3SATTRTIME_TOSERVER);
