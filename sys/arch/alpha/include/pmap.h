40fbe8f5092a121782b5a0d8ee04c62be14ed857
cgd <cgd@NetBSD.org>
1996-08-20 23:02:30 +0000

    OLD_PMAP -> NEW_PMAP

--- sys/arch/alpha/include/pmap.h
+++ sys/arch/alpha/include/pmap.h
@@ -1,6 +1,6 @@
-/*	$NetBSD: pmap.h,v 1.8 1996/07/09 22:22:25 cgd Exp $	*/
+/*	$NetBSD: pmap.h,v 1.9 1996/08/20 23:02:30 cgd Exp $	*/
 
-#ifdef OLD_PMAP
+#ifndef NEW_PMAP
 #include <machine/pmap.old.h>
 #else
 #include <machine/pmap.new.h>
