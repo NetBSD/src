70f9c9f24a6816210e9b29dee8c57bf4b4bb62bd
mrg <mrg@NetBSD.org>
1996-09-14 14:40:20 +0000

    move the packet filter hooks in to a saner location.  while i'm here, rename PACKET_FILTER to PFIL_HOOKS.

--- sys/netinet/in.c
+++ sys/netinet/in.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: in.c,v 1.32 1996/09/09 14:51:09 mycroft Exp $	*/
+/*	$NetBSD: in.c,v 1.33 1996/09/14 14:40:23 mrg Exp $	*/
 
 /*
  * Copyright (c) 1982, 1986, 1991, 1993
@@ -43,14 +43,12 @@
 #include <sys/socketvar.h>
 #include <sys/systm.h>
 #include <sys/proc.h>
-#include <sys/queue.h>
 
 #include <net/if.h>
 #include <net/route.h>
 
 #include <netinet/in_systm.h>
 #include <netinet/in.h>
-#include <netinet/ip.h>
 #include <netinet/in_var.h>
 #include <netinet/if_ether.h>
 #include <netinet/ip_mroute.h>
@@ -64,14 +62,6 @@
 #define	SUBNETSARELOCAL	1
 #endif
 int subnetsarelocal = SUBNETSARELOCAL;
-
-#ifdef PACKET_FILTER
-typedef LIST_HEAD(, packet_filter_hook) pfil_list_t;
-pfil_list_t pfil_in_list;
-pfil_list_t pfil_out_list;
-pfil_list_t pfil_bad_list;
-static int done_pfil_init;
-#endif /* PACKET_FILTER */
 
 /*
  * Return 1 if an internet address is for a ``local'' host
@@ -571,128 +561,3 @@ in_delmulti(inm)
 	splx(s);
 }
 #endif
-
-#ifdef PACKET_FILTER
-void pfil_init __P((void));
-void pfil_list_add(pfil_list_t *,
-    int (*) __P((void *, int, struct ifnet *, int, struct mbuf **)), int);
-void pfil_list_remove(struct packet_filter_hook *,
-    int (*) __P((void *, int, struct ifnet *, int, struct mbuf **)));
-
-void
-pfil_init()
-{
-	LIST_INIT(&pfil_in_list);
-	LIST_INIT(&pfil_out_list);
-	LIST_INIT(&pfil_bad_list);
-	done_pfil_init = 1;
-}
-
-/*
- * pfil_add_hook() adds a function to the packet filter hook.  the
- * flags are:
- *	PFIL_IN		call me on incoming packets
- *	PFIL_OUT	call me on outgoing packets
- *	PFIL_BAD	call me when rejecting a packet (that was
- *			not already reject by in/out filters).
- *	PFIL_ALL	call me on all of the above
- *	PFIL_WAITOK	OK to call malloc with M_WAITOK.
- */
-void
-pfil_add_hook(func, flags)
-	int	(*func) __P((void *, int, struct ifnet *, int,
-			     struct mbuf **));
-	int	flags;
-{
-
-	if (done_pfil_init == 0)
-		pfil_init();
-
-	if (flags & PFIL_IN)
-		pfil_list_add(&pfil_in_list, func, flags);
-	if (flags & PFIL_OUT)
-		pfil_list_add(&pfil_out_list, func, flags);
-	if (flags & PFIL_BAD)
-		pfil_list_add(&pfil_bad_list, func, flags);
-}
-
-void
-pfil_list_add(list, func, flags)
-	pfil_list_t *list;
-	int	(*func) __P((void *, int, struct ifnet *, int,
-			     struct mbuf **));
-	int	flags;
-{
-	struct packet_filter_hook *pfh;
-
-	pfh = (struct packet_filter_hook *)malloc(sizeof(*pfh), M_IFADDR,
-	    flags & PFIL_WAITOK ? M_WAITOK : M_NOWAIT);
-	if (pfh == NULL)
-		panic("no memory for packet filter hook");
-
-	pfh->pfil_func = func;
-	LIST_INSERT_HEAD(list, pfh, pfil_link);
-}
-
-/*
- * pfil_remove_hook removes a specific function from the packet filter
- * hook list.
- */
-void
-pfil_remove_hook(func, flags)
-	int	(*func) __P((void *, int, struct ifnet *, int,
-			     struct mbuf **));
-	int	flags;
-{
-
-	if (done_pfil_init == 0)
-		pfil_init();
-
-	if (flags & PFIL_IN)
-		pfil_list_remove(pfil_in_list.lh_first, func);
-	if (flags & PFIL_OUT)
-		pfil_list_remove(pfil_out_list.lh_first, func);
-	if (flags & PFIL_BAD)
-		pfil_list_remove(pfil_bad_list.lh_first, func);
-}
-
-/*
- * pfil_list_remove is an internal function that takes a function off the
- * specified list.
- */
-void
-pfil_list_remove(list, func)
-	struct packet_filter_hook *list;
-	int	(*func) __P((void *, int, struct ifnet *, int,
-			     struct mbuf **));
-{
-	struct packet_filter_hook *pfh;
-
-	for (pfh = list; pfh; pfh = pfh->pfil_link.le_next)
-		if (pfh->pfil_func == func) {
-			LIST_REMOVE(pfh, pfil_link);
-			free(pfh, M_IFADDR);
-			return;
-		}
-	printf("pfil_list_remove:  no function on list\n");
-#ifdef DIAGNOSTIC
-	panic("pfil_list_remove");
-#endif
-}
-
-struct packet_filter_hook *
-pfil_hook_get(flag)
-	int flag;
-{
-	if (done_pfil_init)
-		switch (flag) {
-		case PFIL_IN:
-			return (pfil_in_list.lh_first);
-		case PFIL_OUT:
-			return (pfil_out_list.lh_first);
-		case PFIL_BAD:
-			return (pfil_bad_list.lh_first);
-		}
-	return NULL;
-}
-#endif /* PACKET_FILTER */
