dd1088afc1c32579302271b3fab502f3108fd416
mycroft <mycroft@NetBSD.org>
1996-09-17 16:43:48 +0000

    Catch up with netinet changes.

--- sys/netiso/tuba_subr.c
+++ sys/netiso/tuba_subr.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: tuba_subr.c,v 1.7 1996/02/13 22:12:32 christos Exp $	*/
+/*	$NetBSD: tuba_subr.c,v 1.8 1996/09/17 16:43:51 mycroft Exp $	*/
 
 /*
  * Copyright (c) 1992, 1993
@@ -101,7 +101,7 @@ tuba_init()
 #define TUBAHDRSIZE (3 /*LLC*/ + 9 /*CLNP Fixed*/ + 42 /*Addresses*/ \
 		     + 6 /*CLNP Segment*/ + 20 /*TCP*/)
 
-	in_pcbinit(&tuba_inpcb, tuba_inpcbhashsize);
+	in_pcbinit(&tuba_inpcb, tuba_inpcbhashsize, tuba_inpcbhashsize);
 	tuba_isopcb.isop_next = tuba_isopcb.isop_prev = &tuba_isopcb;
 	tuba_isopcb.isop_faddr = &tuba_isopcb.isop_sfaddr;
 	tuba_isopcb.isop_laddr = &tuba_isopcb.isop_sladdr;
