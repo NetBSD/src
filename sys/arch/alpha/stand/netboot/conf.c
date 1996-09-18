34baffd82b62b5a64196e5e18d810a0a34ac6d7e
cgd <cgd@NetBSD.org>
1996-09-18 20:03:04 +0000

    start on some network boot blocks.  they work, but: 	(1) the if_prom code needs to be cleaned up quite a bit 	    (right now it hard-codes client's ethernet address), and 	(2) much of this code (esp: getsecs.c, netboot.c, newvers.sh, 	    rpcc.S) should be shared with other Alpha boot blocks, 	    and dev_net.[ch] should maybe go in libsa proper.

---
+++ sys/arch/alpha/stand/netboot/conf.c
@@ -0,0 +1,25 @@
+/*	$NetBSD: conf.c,v 1.1 1996/09/18 20:03:06 cgd Exp $	*/
+
+#include <sys/types.h>
+#include <netinet/in.h>
+
+#include "stand.h"
+#include "nfs.h"
+#include "dev_net.h"
+
+struct fs_ops file_system[] = {
+	{ nfs_open, nfs_close, nfs_read, nfs_write, nfs_seek, nfs_stat },
+};
+int nfsys = 1;
+
+struct devsw devsw[] = {
+	{ "net",  net_strategy,  net_open,  net_close,  net_ioctl },
+};
+int	ndevs = 1;
+
+extern struct netif_driver prom_netif_driver;
+ 
+struct netif_driver *netif_drivers[] = {
+        &prom_netif_driver,
+};    
+int     n_netif_drivers = (sizeof(netif_drivers) / sizeof(netif_drivers[0]));
