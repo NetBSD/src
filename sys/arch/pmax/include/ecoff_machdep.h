f5a8940e5d9bdfe9c1f8cc8502823e811734c1dd
cgd <cgd>
1996-09-26 22:40:53 +0000

    since these just included mips/ecoff.h, they need to be changed to include mips/ecoff_machdep.h.

--- sys/arch/pmax/include/ecoff_machdep.h
+++ sys/arch/pmax/include/ecoff_machdep.h
@@ -1,3 +1,3 @@
-/*	$NetBSD: ecoff_machdep.h,v 1.5 1996/03/19 03:06:28 jonathan Exp $	*/
+/*	$NetBSD: ecoff_machdep.h,v 1.6 1996/09/26 22:40:54 cgd Exp $	*/
 
-#include <mips/ecoff.h>
+#include <mips/ecoff_machdep.h>
