3729419cc8cd4c37fcf921a0cb672c5e9e0b0709
mycroft <mycroft@NetBSD.org>
1996-09-28 03:16:56 +0000

    * Bitbucket extra status bytes rather than barfing. * Add copyright.

--- sys/arch/alpha/tc/esp.c
+++ sys/arch/alpha/tc/esp.c
@@ -1,8 +1,37 @@
-/*	$NetBSD: esp.c,v 1.17 1996/09/28 02:10:55 mycroft Exp $	*/
+/*	$NetBSD: esp.c,v 1.18 1996/09/28 03:16:56 mycroft Exp $	*/
 
 #ifdef __sparc__
 #define	SPARC_DRIVER
 #endif
+
+/*
+ * Copyright (c) 1996 Charles M. Hannum.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed by Charles M. Hannum.
+ * 4. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 /*
  * Copyright (c) 1994 Peter Galbavy
@@ -1885,14 +1914,9 @@ if (sc->sc_flags & ESP_ICCS) printf("[[esp: BUMMER]]");
 						sc->sc_espstep);
 				}
 				if ((ESP_READ_REG(sc, ESP_FFLAG) & ESPFIFO_FF) != 2) {
-					printf("%s: ICCS: expected 2, got %d "
-					      ": [intr %x, stat %x, step %x]\n",
-						sc->sc_dev.dv_xname,
-						ESP_READ_REG(sc, ESP_FFLAG) & ESPFIFO_FF,
-						sc->sc_espintr, sc->sc_espstat,
-						sc->sc_espstep);
-					ESPCMD(sc, ESPCMD_FLUSH);
-					goto reset;
+					int i = (ESP_READ_REG(sc, ESP_FFLAG) & ESPFIFO_FF) - 2;
+					while (i--)
+						(void) ESP_READ_REG(sc, ESP_FIFO);
 				}
 				ecb->stat = ESP_READ_REG(sc, ESP_FIFO);
 				msg = ESP_READ_REG(sc, ESP_FIFO);
