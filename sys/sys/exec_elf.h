6ff3b11e2f18737330a11e15322a6ff98441a047
ws <ws>
1996-09-30 16:34:14 +0000

    PowerPC port

--- sys/sys/exec_elf.h
+++ sys/sys/exec_elf.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: exec_elf.h,v 1.6 1996/09/27 21:21:15 cgd Exp $	*/
+/*	$NetBSD: exec_elf.h,v 1.7 1996/09/30 16:35:12 ws Exp $	*/
 
 /*
  * Copyright (c) 1994 Christos Zoulas
@@ -70,6 +70,7 @@ enum Elf_e_machine {
 	Elf_em_486,
 	Elf_em_860,
 	Elf_em_mips,
+	Elf_em_ppc = 20,
 	Elf_em_alpha=0x9026,
 	Elf_em_num
 };
@@ -189,21 +190,21 @@ typedef struct {
 /*
  * Section Headers
  */
-enum Elf_s_sht {
-	Elf_sht_null = 0,
-	Elf_sht_progbits = 1,
-	Elf_sht_symtab = 2,
-	Elf_sht_strtab = 3,
-	Elf_sht_rela = 4,
-	Elf_sht_hash = 5,
-	Elf_sht_dynamic = 6,
-	Elf_sht_note = 7,
-	Elf_sht_nobits = 8,
-	Elf_sht_rel = 9,
-	Elf_sht_shlib = 10,
-	Elf_sht_dynsym = 11,
-	Elf_sht_loproc = 0x70000000,
-	Elf_sht_hiproc = 0x7FFFFFFF
+enum Elf32_s_sht {
+	Elf32_sht_null = 0,
+	Elf32_sht_progbits = 1,
+	Elf32_sht_symtab = 2,
+	Elf32_sht_strtab = 3,
+	Elf32_sht_rela = 4,
+	Elf32_sht_hash = 5,
+	Elf32_sht_dynamic = 6,
+	Elf32_sht_note = 7,
+	Elf32_sht_nobits = 8,
+	Elf32_sht_rel =	9,
+	Elf32_sht_shlib = 10,
+	Elf32_sht_dynsym = 11,
+	Elf32_sht_loproc = 0x70000000,
+	Elf32_sht_hiproc = 0x7FFFFFFF
 };
 
 typedef struct {
