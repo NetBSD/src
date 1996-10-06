faa821e4d03b54aea023be88051031ad829c9cdf
thorpej <thorpej@NetBSD.org>
1996-10-06 00:15:31 +0000

    Nuke __BDEVSW_DUMP_OLD_TYPE.

--- sys/arch/hp300/include/types.h
+++ sys/arch/hp300/include/types.h
@@ -1,10 +1,8 @@
-/*	$NetBSD: types.h,v 1.5 1995/06/26 05:13:46 cgd Exp $	*/
+/*	$NetBSD: types.h,v 1.6 1996/10/06 00:15:31 thorpej Exp $	*/
 
 #ifndef _MACHINE_TYPES_H_
 #define _MACHINE_TYPES_H_
 
 #include <m68k/types.h>
-
-#define	__BDEVSW_DUMP_OLD_TYPE
 
 #endif
