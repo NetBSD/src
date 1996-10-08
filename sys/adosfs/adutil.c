8ef7e73ff1a709cfa6a227deff23bc2a4d97b248
thorpej <thorpej@NetBSD.org>
1996-10-08 22:18:02 +0000

    Merge netbsd-1-2 branch back into mainline.

--- sys/adosfs/adutil.c
+++ sys/adosfs/adutil.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: adutil.c,v 1.12 1996/08/10 07:28:49 veego Exp $	*/
+/*	$NetBSD: adutil.c,v 1.13 1996/10/08 22:18:03 thorpej Exp $	*/
 
 /*
  * Copyright (c) 1994 Christian E. Hopps
@@ -159,18 +159,18 @@ CapitalChar(ch, inter)
 	return(ch);
 }
 
-long
+u_int32_t
 adoscksum(bp, n)
 	struct buf *bp;
-	long n;
+	int n;
 {
-	long sum, *lp;
+	u_int32_t sum, *lp;
 	
-	lp = (long *)bp->b_data;
+	lp = (u_int32_t *)bp->b_data;
 	sum = 0;
 
 	while (n--)
-		sum += *lp++;
+		sum += ntohl(*lp++);
 	return(sum);
 }
 
@@ -223,7 +223,7 @@ tvtods(tvp, dsp)
 #endif
 
 #ifndef m68k
-long
+u_int32_t
 adoswordn(bp, wn)
 	struct buf *bp;
 	int wn;
@@ -231,6 +231,6 @@ adoswordn(bp, wn)
 	/*
 	 * ados stored in network (big endian) order
 	 */
-	return(ntohl(*((long *)bp->b_data + wn)));
+	return(ntohl(*((u_int32_t *)bp->b_data + wn)));
 }
 #endif
