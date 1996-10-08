ba5d66276a7432a0e5208c306d8a2d4ff5e5a600
cgd <cgd@NetBSD.org>
1996-10-08 13:07:26 +0000

    repoint at shared aout_machdep.h, not exec.h

--- sys/arch/pica/include/aout_machdep.h
+++ sys/arch/pica/include/aout_machdep.h
@@ -1,3 +1,3 @@
-/*	$NetBSD: aout_machdep.h,v 1.2 1996/03/23 03:42:40 jonathan Exp $	*/
+/*	$NetBSD: aout_machdep.h,v 1.3 1996/10/08 13:07:34 cgd Exp $	*/
 
-#include <mips/exec.h>
+#include <mips/aout_machdep.h>
--- sys/arch/x68k/include/aout_machdep.h
+++ sys/arch/x68k/include/aout_machdep.h
@@ -1,8 +1,8 @@
-/*	$NetBSD: aout_machdep.h,v 1.2 1996/09/08 01:00:41 thorpej Exp $	*/
+/*	$NetBSD: aout_machdep.h,v 1.3 1996/10/08 13:07:39 cgd Exp $	*/
 
 #ifndef _MACHINE_EXEC_H_
 #define _MACHINE_EXEC_H_
 
-#include <m68k/exec.h>
+#include <m68k/aout_machdep.h>
 
 #endif  /* _MACHINE_EXEC_H_ */
