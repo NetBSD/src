2fc2166049113c1b98dd8ba7d98e00eb30130d6c
cgd <cgd>
1996-10-23 22:51:08 +0000

    recognize the 'nocoredump' mount option as a standard option (supported by all file system types).

--- sbin/mount_nfs/mntopts.h
+++ sbin/mount_nfs/mntopts.h
@@ -42,6 +42,7 @@ struct mntopt {
 
 /* User-visible MNT_ flags. */
 #define MOPT_ASYNC		{ "async",	0, MNT_ASYNC, 0 }
+#define MOPT_NOCOREDUMP		{ "coredump",	1, MNT_NOCOREDUMP, 0 }
 #define MOPT_NODEV		{ "dev",	1, MNT_NODEV, 0 }
 #define MOPT_NOEXEC		{ "exec",	1, MNT_NOEXEC, 0 }
 #define MOPT_NOSUID		{ "suid",	1, MNT_NOSUID, 0 }
@@ -70,6 +71,7 @@ struct mntopt {
 	MOPT_USERQUOTA,							\
 	MOPT_GROUPQUOTA,						\
 	MOPT_FSTAB_COMPAT,						\
+	MOPT_NOCOREDUMP,						\
 	MOPT_NODEV,							\
 	MOPT_NOEXEC,							\
 	MOPT_NOSUID,							\
