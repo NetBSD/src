d198383cea659917635bd7987029aac8d3019c8b
gwr <gwr@NetBSD.org>
1996-10-27 19:27:54 +0000

    Clarify the purpose of this file (user-level code only).

--- sys/sys/dir.h
+++ sys/sys/dir.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: dir.h,v 1.8 1994/06/29 06:43:52 cgd Exp $	*/
+/*	$NetBSD: dir.h,v 1.9 1996/10/27 19:27:54 gwr Exp $	*/
 
 /*
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -42,6 +42,11 @@
 
 #ifndef _SYS_DIR_H_
 #define	_SYS_DIR_H_
+
+#ifdef	_KERNEL
+/* This file should only be used by old user-level code. */
+#error "Please use <sys/dirent.h> instead"
+#endif
 
 #include <dirent.h>
 
