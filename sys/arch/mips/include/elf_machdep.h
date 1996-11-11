f72a1bb18a97947cfd9238b5e158ebab0967926b
jonathan <jonathan@NetBSD.org>
1996-11-11 20:33:10 +0000

    Elf32 fixes for mips shared libraries:
     * handle interpreters with nonzero virtual address of entry-point:
       subtract p_vaddr from computed entrypoint, as the mips elf exec did.
    
     * Add #ifdef ELF_INTERP_NON_RELOCATABLE/#endif around the code
       that tries to choose a `good' address at which to load an interpreter,
       if none was set by the emul probe  function.
       (the address chosen could be improved to avoid  fragmenting the
        process virtual address space).
    
     * define ELF_INTERP_NON_RELOCATABLE in machine/elf_machdep.h for mips CPUs,
       which currently use a GNU-derived ld.so.
    
    ELF_INTERP_NON_RELOCATABLE is not necessary for native NetBSD/alpha ELF
    binaries. It may be required for GNU-derived ELF dynamic loaders (Linux/i386?)

--- sys/arch/mips/include/elf_machdep.h
+++ sys/arch/mips/include/elf_machdep.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: elf_machdep.h,v 1.1 1996/09/26 21:50:59 cgd Exp $	*/
+/*	$NetBSD: elf_machdep.h,v 1.2 1996/11/11 20:33:12 jonathan Exp $	*/
 
 #define	ELF32_MACHDEP_ID_CASES						\
 		case Elf_em_mips:					\
@@ -6,3 +6,10 @@
 
 #define	ELF64_MACHDEP_ID_CASES						\
 		/* no 64-bit ELF machine types supported */
+/*
+ * Tell the kernel ELF exec code not to try relocating the interpreter
+ * (ld.so) for dynamically-linked ELF binaries
+ */
+#ifdef _KERNEL
+#define ELF_INTERP_NON_RELOCATABLE
+#endif
