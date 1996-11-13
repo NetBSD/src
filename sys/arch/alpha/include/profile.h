abbebd04d08c3aa918345c3511b32d5419b0f566
cgd <cgd@NetBSD.org>
1996-11-13 22:20:54 +0000

    update copyright dates

--- sys/arch/alpha/include/profile.h
+++ sys/arch/alpha/include/profile.h
@@ -1,7 +1,7 @@
-/*	$NetBSD: profile.h,v 1.6 1996/09/15 22:33:28 cgd Exp $	*/
+/*	$NetBSD: profile.h,v 1.7 1996/11/13 22:21:01 cgd Exp $	*/
 
 /*
- * Copyright (c) 1994, 1995 Carnegie-Mellon University.
+ * Copyright (c) 1994, 1995, 1996 Carnegie-Mellon University.
  * All rights reserved.
  *
  * Author: Chris G. Demetriou
