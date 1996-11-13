60662d2721239e6ffee5de429e4c86368a76f4d9
cgd <cgd>
1996-11-13 22:20:54 +0000

    update copyright dates

--- sys/arch/alpha/alpha/support.c
+++ sys/arch/alpha/alpha/support.c
@@ -1,7 +1,7 @@
-/*	$NetBSD: support.c,v 1.3 1996/11/13 21:13:13 cgd Exp $	*/
+/*	$NetBSD: support.c,v 1.4 1996/11/13 22:20:56 cgd Exp $	*/
 
 /*
- * Copyright (c) 1994, 1995 Carnegie-Mellon University.
+ * Copyright (c) 1994, 1995, 1996 Carnegie-Mellon University.
  * All rights reserved.
  *
  * Author: Chris G. Demetriou
