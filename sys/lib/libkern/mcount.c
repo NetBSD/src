6263327d7e1eae95a9ce841552ec85042ea18164
fvdl <fvdl@NetBSD.org>
1996-12-05 10:18:46 +0000

    Remove _MCOUNT_DECL prototype declaration of _mcount() again; move it to <machine/profile.h>

--- sys/lib/libkern/mcount.c
+++ sys/lib/libkern/mcount.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: mcount.c,v 1.6 1996/12/04 00:04:40 fvdl Exp $	*/
+/*	$NetBSD: mcount.c,v 1.7 1996/12/05 10:18:46 fvdl Exp $	*/
 
 /*-
  * Copyright (c) 1983, 1992, 1993
@@ -37,14 +37,12 @@
 #if 0
 static char sccsid[] = "@(#)mcount.c	8.1 (Berkeley) 6/4/93";
 #else
-static char rcsid[] = "$NetBSD: mcount.c,v 1.6 1996/12/04 00:04:40 fvdl Exp $";
+static char rcsid[] = "$NetBSD: mcount.c,v 1.7 1996/12/05 10:18:46 fvdl Exp $";
 #endif
 #endif
 
 #include <sys/param.h>
 #include <sys/gmon.h>
-
-_MCOUNT_DECL __P((u_long, u_long));
 
 /*
  * mcount is called on entry to each function compiled with the profiling
