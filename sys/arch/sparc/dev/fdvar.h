5c6bf9143b699d26143ab04f2a265770f7544cc8
pk <pk@NetBSD.org>
1996-12-08 23:40:32 +0000

    Import formatting code from the i386 version. Revise communication between hw & sw interrupt handler.

--- sys/arch/sparc/dev/fdvar.h
+++ sys/arch/sparc/dev/fdvar.h
@@ -1,5 +1,5 @@
 /*
- *	$NetBSD: fdvar.h,v 1.4 1996/02/01 22:32:29 mycroft Exp $
+ *	$NetBSD: fdvar.h,v 1.5 1996/12/08 23:40:34 pk Exp $
  *
  * Copyright (c) 1995 Paul Kranenburg
  * All rights reserved.
@@ -71,6 +71,53 @@ struct fdcio {
 #define ISTATE_SPURIOUS		1	/* Spurious HW interrupt detected */
 #define ISTATE_SENSEI		2	/* Do SENSEI on next HW interrupt */
 #define ISTATE_DMA		3	/* Pseudo-DMA in progress */
+#define ISTATE_DONE		4	/* Interrupt processing complete */
 
-#define FDIOCEJECT	_IO('f', 24)
 
+#define FD_MAX_NSEC 36		/* highest known number of spt - allow for */
+				/* 2.88 MB drives */
+
+#ifndef _LOCORE
+struct ne7_fd_formb {
+	int cyl, head;
+	int transfer_rate;	/* fdreg.h: FDC_???KBPS */
+
+	union {
+		struct fd_form_data {
+			/*
+			 * DO NOT CHANGE THE LAYOUT OF THIS STRUCTS
+			 * it is hardware-dependant since it exactly
+			 * matches the byte sequence to write to FDC
+			 * during its `format track' operation
+			 */
+			u_char secshift; /* 0 -> 128, ...; usually 2 -> 512 */
+			u_char nsecs;	/* must be <= FD_MAX_NSEC */
+			u_char gaplen;	/* GAP 3 length; usually 84 */
+			u_char fillbyte; /* usually 0xf6 */
+			struct fd_idfield_data {
+				/*
+				 * data to write into id fields;
+				 * for obscure formats, they mustn't match
+				 * the real values (but mostly do)
+				 */
+				u_char cylno;	/* 0 thru 79 (or 39) */
+				u_char headno;	/* 0, or 1 */
+				u_char secno;	/* starting at 1! */
+				u_char secsize;	/* usually 2 */
+			} idfields[FD_MAX_NSEC]; /* 0 <= idx < nsecs used */
+		} structured;
+		u_char raw[1];	/* to have continuous indexed access */
+	} format_info;
+};
+
+/* make life easier */
+#define fd_formb_secshift   format_info.structured.secshift
+#define fd_formb_nsecs      format_info.structured.nsecs
+#define fd_formb_gaplen     format_info.structured.gaplen
+#define fd_formb_fillbyte   format_info.structured.fillbyte
+/* these data must be filled in for(i = 0; i < fd_formb_nsecs; i++) */
+#define fd_formb_cylno(i)   format_info.structured.idfields[i].cylno
+#define fd_formb_headno(i)  format_info.structured.idfields[i].headno
+#define fd_formb_secno(i)   format_info.structured.idfields[i].secno
+#define fd_formb_secsize(i) format_info.structured.idfields[i].secsize
+#endif /* _LOCORE */
