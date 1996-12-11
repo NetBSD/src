5a96c651b394d12f63f892f35133a28fc48dd212
mycroft <mycroft@NetBSD.org>
1996-12-11 10:10:57 +0000

    From trunk: Use sys/types.h and sys/systm.h to bring in prototypes for bzero() and bcopy(), instead of string.h.

--- sys/net/zlib.c
+++ sys/net/zlib.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: zlib.c,v 1.2 1996/03/16 23:55:40 christos Exp $	*/
+/*	$NetBSD: zlib.c,v 1.2.4.1 1996/12/11 10:10:57 mycroft Exp $	*/
 
 /*
  * This file is derived from various .h and .c files from the zlib-0.95
@@ -32,8 +32,10 @@
 
 #include "zlib.h"
 
-#ifdef STDC
-#  include <string.h>
+#ifndef __NetBSD__
+#  ifdef STDC
+#    include <string.h>
+#  endif
 #endif
 
 #ifndef local
@@ -86,6 +88,10 @@ extern char *z_errmsg[]; /* indexed by 1-zlib_error */
          /* functions */
 
 #if defined(KERNEL) || defined(_KERNEL)
+#  ifdef __NetBSD__
+#    include <sys/types.h>
+#    include <sys/systm.h>
+#  endif
 #  define zmemcpy(d, s, n)	bcopy((s), (d), (n))
 #  define zmemzero		bzero
 #else
