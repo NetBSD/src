152d7845d408bf78ef185c6bfb0a85297c72bc51
thorpej <thorpej@NetBSD.org>
1996-12-17 08:11:50 +0000

    Kill __BROKEN_INDIRECT_CONFIG

--- sys/arch/hp300/include/types.h
+++ sys/arch/hp300/include/types.h
@@ -1,10 +1,8 @@
-/*	$NetBSD: types.h,v 1.7 1996/12/05 00:13:51 cgd Exp $	*/
+/*	$NetBSD: types.h,v 1.8 1996/12/17 08:11:50 thorpej Exp $	*/
 
 #ifndef _MACHINE_TYPES_H_
 #define _MACHINE_TYPES_H_
 
 #include <m68k/types.h>
-
-#define	__BROKEN_INDIRECT_CONFIG
 
 #endif
