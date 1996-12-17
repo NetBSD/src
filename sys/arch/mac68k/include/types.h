a7bacbdf6a11dd55b159a7bf8552e3e0e8248743
scottr <scottr>
1996-12-17 07:00:12 +0000

    Remove __BROKEN_INDIRECT_CONFIG, since we are no longer broken.

--- sys/arch/mac68k/include/types.h
+++ sys/arch/mac68k/include/types.h
@@ -1,10 +1,8 @@
-/*	$NetBSD: types.h,v 1.9 1996/12/05 00:13:53 cgd Exp $	*/
+/*	$NetBSD: types.h,v 1.10 1996/12/17 07:00:12 scottr Exp $	*/
 
 #ifndef _TYPES_MACHINE_
 #define _TYPES_MACHINE_
 
 #include <m68k/types.h>
-
-#define	__BROKEN_INDIRECT_CONFIG
 
 #endif /* _TYPES_MACHINE_ */
