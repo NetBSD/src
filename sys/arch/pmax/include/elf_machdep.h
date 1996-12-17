b41ba6765af269ede547324d35cd2846569d9089
jonathan <jonathan>
1996-12-17 03:45:05 +0000

    Configure pmax elf_machdep.h as little-endian elf.

--- sys/arch/pmax/include/elf_machdep.h
+++ sys/arch/pmax/include/elf_machdep.h
@@ -1,3 +1,11 @@
-/*	$NetBSD: elf_machdep.h,v 1.1 1996/09/26 21:51:01 cgd Exp $	*/
+/*	$NetBSD: elf_machdep.h,v 1.2 1996/12/17 03:45:05 jonathan Exp $	*/
 
 #include <mips/elf_machdep.h>
+
+/*
+ * pmaxes are mipsel machines
+ */
+
+#define ELF32_MACHDEP_ENDIANNESS	Elf_ed_2lsb
+
+#define ELF64_MACHDEP_ENDIANNESS	XXX	/* break compilation */
