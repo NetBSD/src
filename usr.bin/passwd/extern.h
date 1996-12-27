73219cb17784b4e20c303970a1b7df7497d73c26
tls <tls@NetBSD.org>
1996-12-27 23:11:35 +0000

    Import of 4.4BSD-Lite2 sources

--- usr.bin/passwd/extern.h
+++ usr.bin/passwd/extern.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1988, 1993, 1994
+ * Copyright (c) 1994
  *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -29,90 +29,9 @@
  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
+ *
+ *	@(#)extern.h	8.1 (Berkeley) 4/2/94
  */
 
-#ifndef lint
-static char copyright[] =
-"@(#) Copyright (c) 1988, 1993, 1994\n\
-	The Regents of the University of California.  All rights reserved.\n";
-#endif /* not lint */
-
-#ifndef lint
-static char sccsid[] = "@(#)passwd.c	8.3 (Berkeley) 4/2/94";
-#endif /* not lint */
-
-#include <err.h>
-#include <errno.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-
-#include "extern.h"
-
-void	usage __P((void));
-
-#ifdef KERBEROS
-int use_kerberos = 1;
-#endif
-
-int
-main(argc, argv)
-	int argc;
-	char **argv;
-{
-	int ch;
-	char *uname;
-
-	while ((ch = getopt(argc, argv, "l")) != EOF)
-		switch (ch) {
-#ifdef KERBEROS
-		case 'l':		/* change local password file */
-			use_kerberos = 0;
-			break;
-#endif
-		default:
-		case '?':
-			usage();
-		}
-
-	argc -= optind;
-	argv += optind;
-
-	if ((uname = getlogin()) == NULL)
-		err(1, "getlogin");
-
-	switch(argc) {
-	case 0:
-		break;
-	case 1:
-#ifdef	KERBEROS
-		if (use_kerberos && strcmp(argv[0], uname))
-			errx(1,"%s\n\t%s\n%s\n",
-		"to change another user's Kerberos password, do",
-		"\"kinit user; passwd; kdestroy\";",
-		"to change a user's local passwd, use \"passwd -l user\"");
-#endif
-		uname = argv[0];
-		break;
-	default:
-		usage();
-	}
-
-#ifdef	KERBEROS
-	if (use_kerberos)
-		exit(krb_passwd());
-#endif
-	exit(local_passwd(uname));
-}
-
-void
-usage()
-{
-
-#ifdef	KERBEROS
-	(void)fprintf(stderr, "usage: passwd [-l] user\n");
-#else
-	(void)fprintf(stderr, "usage: passwd user\n");
-#endif
-	exit(1);
-}
+int	krb_passwd __P((void));
+int	local_passwd __P((char *));
--- usr.bin/passwd/kpasswd_proto.h
+++ usr.bin/passwd/kpasswd_proto.h
@@ -1,5 +1,5 @@
-/*
- * Copyright (c) 1988, 1993, 1994
+/*-
+ * Copyright (c) 1989, 1993
  *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -29,90 +29,26 @@
  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
+ *
+ *	@(#)kpasswd_proto.h	8.1 (Berkeley) 6/6/93
  */
 
-#ifndef lint
-static char copyright[] =
-"@(#) Copyright (c) 1988, 1993, 1994\n\
-	The Regents of the University of California.  All rights reserved.\n";
-#endif /* not lint */
+/*
+ * kpasswd_proto
+ *
+ * definitions for the kpasswd "protocol"
+ * (We hope this to be temporary until a real admin protocol is worked out.)
+ */
 
-#ifndef lint
-static char sccsid[] = "@(#)passwd.c	8.3 (Berkeley) 4/2/94";
-#endif /* not lint */
+struct kpasswd_data {
+	des_cblock random_key;
+	char secure_msg[_PASSWORD_LEN];
+};
 
-#include <err.h>
-#include <errno.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-
-#include "extern.h"
-
-void	usage __P((void));
-
-#ifdef KERBEROS
-int use_kerberos = 1;
-#endif
-
-int
-main(argc, argv)
-	int argc;
-	char **argv;
-{
-	int ch;
-	char *uname;
-
-	while ((ch = getopt(argc, argv, "l")) != EOF)
-		switch (ch) {
-#ifdef KERBEROS
-		case 'l':		/* change local password file */
-			use_kerberos = 0;
-			break;
-#endif
-		default:
-		case '?':
-			usage();
-		}
-
-	argc -= optind;
-	argv += optind;
-
-	if ((uname = getlogin()) == NULL)
-		err(1, "getlogin");
-
-	switch(argc) {
-	case 0:
-		break;
-	case 1:
-#ifdef	KERBEROS
-		if (use_kerberos && strcmp(argv[0], uname))
-			errx(1,"%s\n\t%s\n%s\n",
-		"to change another user's Kerberos password, do",
-		"\"kinit user; passwd; kdestroy\";",
-		"to change a user's local passwd, use \"passwd -l user\"");
-#endif
-		uname = argv[0];
-		break;
-	default:
-		usage();
-	}
-
-#ifdef	KERBEROS
-	if (use_kerberos)
-		exit(krb_passwd());
-#endif
-	exit(local_passwd(uname));
-}
-
-void
-usage()
-{
-
-#ifdef	KERBEROS
-	(void)fprintf(stderr, "usage: passwd [-l] user\n");
-#else
-	(void)fprintf(stderr, "usage: passwd user\n");
-#endif
-	exit(1);
-}
+struct update_data {
+	char pw[_PASSWORD_LEN];
+	char secure_msg[_PASSWORD_LEN];
+};
+#define	SERVICE		"kpasswd"
+#define	SECURE_STRING \
+	"Kerberos password update program -- 12/9/88 UC Berkeley"
