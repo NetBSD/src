ab39e1ee9c5605f56043c58667d853a0e1a21652
mrg <mrg@NetBSD.org>
1997-01-05 13:06:38 +0000

    initial import of darren reed's ip-filter, version 3.1.2.

---
+++ usr.sbin/ipf/ipfstat/kmem.h
@@ -0,0 +1,12 @@
+/*
+ * (C)opyright 1993,1994,1995 by Darren Reed.
+ *
+ * Redistribution and use in source and binary forms are permitted
+ * provided that this notice is preserved and due credit is given
+ * to the original author and the contributors.
+ * $Id: kmem.h,v 1.1.1.1 1997/01/05 13:09:04 mrg Exp $
+ */
+
+extern	int	openkmem();
+extern	int	kmemcpy();
+
