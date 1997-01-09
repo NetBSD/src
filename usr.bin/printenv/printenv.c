796b9df18602b36af5bfd0d8532cb2a885a11d17
tls <tls>
1997-01-09 15:17:30 +0000

    Sync to 4.4BSD-Lite2

--- usr.bin/printenv/printenv.c
+++ usr.bin/printenv/printenv.c
@@ -1,6 +1,8 @@
+/*	$NetBSD: printenv.c,v 1.5 1997/01/09 15:17:32 tls Exp $	*/
+
 /*
- * Copyright (c) 1987 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1987, 1993
+ *    Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -32,19 +34,24 @@
  */
 
 #ifndef lint
-char copyright[] =
-"@(#) Copyright (c) 1987 Regents of the University of California.\n\
- All rights reserved.\n";
+static char copyright[] =
+"@(#) Copyright (c) 1987, 1993\n\
+      The Regents of the University of California.  All rights reserved.\n";
 #endif /* not lint */
 
 #ifndef lint
-/*static char sccsid[] = "from: @(#)printenv.c	5.4 (Berkeley) 6/1/90";*/
-static char rcsid[] = "$Id: printenv.c,v 1.4 1993/12/31 19:28:15 jtc Exp $";
+/*static char sccsid[] = "from: @(#)printenv.c	8.2 (Berkeley) 5/4/95";*/
+static char rcsid[] = "$NetBSD: printenv.c,v 1.5 1997/01/09 15:17:32 tls Exp $";
 #endif /* not lint */
 
+#include <sys/types.h>
+
+#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
+#include <unistd.h>
+
+void  usage __P((void));
 
 /*
  * printenv
@@ -55,25 +62,42 @@ static char rcsid[] = "$Id: printenv.c,v 1.4 1993/12/31 19:28:15 jtc Exp $";
 int
 main(argc, argv)
 	int argc;
-	char **argv;
+	char *argv[];
 {
 	extern char **environ;
-	register char *cp, **ep;
-	register int len;
+	char *cp, **ep;
+	size_t len;
+	int ch;
 
-	if (argc < 2) {
+	while ((ch = getopt(argc, argv, "")) != EOF)
+		switch(ch) {
+		case '?':
+		default:
+			usage();
+		}
+	argc -= optind;
+	argv += optind;
+
+	if (argc == 0) {
 		for (ep = environ; *ep; ep++)
-			puts(*ep);
+			(void)printf("%s\n", *ep);
 		exit(0);
 	}
-	len = strlen(*++argv);
+	len = strlen(*argv);
 	for (ep = environ; *ep; ep++)
-		if (!strncmp(*ep, *argv, len)) {
+		if (!memcmp(*ep, *argv, len)) {
 			cp = *ep + len;
 			if (!*cp || *cp == '=') {
-				puts(*cp ? cp + 1 : cp);
+				(void)printf("%s\n", *cp ? cp + 1 : cp);
 				exit(0);
 			}
 		}
+	exit(1);
+}
+
+void
+usage()
+{
+	(void)fprintf(stderr, "usage: printenv [name]\n");
 	exit(1);
 }
