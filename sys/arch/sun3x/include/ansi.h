9ac70e5f20f4bee091a95b2893e95e9f4b882059
gwr <gwr>
1997-01-14 20:57:02 +0000

    Initial import.  Gets as far as nfs_mountroot.

---
+++ sys/arch/sun3x/include/ansi.h
@@ -0,0 +1,3 @@
+/*	$NetBSD: ansi.h,v 1.1.1.1.2.2 1997/01/14 20:57:05 gwr Exp $	*/
+
+#include <m68k/ansi.h>
---
+++ sys/arch/sun3x/include/endian.h
@@ -0,0 +1,3 @@
+/*	$NetBSD: endian.h,v 1.1.1.1.2.2 1997/01/14 20:57:06 gwr Exp $	*/
+
+#include <m68k/endian.h>
