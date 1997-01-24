0a8b9766bd0a0211de9852a10c8fead779c74a93
cgd <cgd@NetBSD.org>
1997-01-24 00:26:31 +0000

    split iso.h into two parts: iso.h (which now contains _only_ definitions about ISO9660 file system structure), and cd9660_extern.h (which now contains the definitions and data structures used by the kernel). There's lots of other stuff scattered around this code that should go into cd9660_extern.h, but doesn't need to right now.  This changed patterned on ffs, which has 'fs.h' which describes the FS structure, and ffs_extern.h for kernel-internals gunk.  Now libsa can include iso.h without the nasty hack that was there before.
    Also, clean up the isonum_* definitions so that the #ifdefs aren't
    impossibly spread apart.  (Now the #ifdefs are _in_ the functions.  This
    leads to #ifdefs that are only a few lines long, rather than a few _pages_
    long, as well as a reduction of duplication of function headers, etc.)
    Note that isonum_7{21,22,31,32} are currently missing.

--- sys/isofs/cd9660/iso.h
+++ sys/isofs/cd9660/iso.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: iso.h,v 1.15 1996/12/22 10:10:17 cgd Exp $	*/
+/*	$NetBSD: iso.h,v 1.16 1997/01/24 00:26:31 cgd Exp $	*/
 
 /*-
  * Copyright (c) 1994
@@ -38,6 +38,12 @@
  * SUCH DAMAGE.
  *
  *	@(#)iso.h	8.4 (Berkeley) 12/5/94
+ */
+
+/*
+ * Definitions describing ISO9660 file system structure, as well as
+ * the functions necessary to access fields of ISO9660 file system
+ * structures.
  */
 
 #define ISODCL(from, to) (to - from + 1)
@@ -129,68 +135,13 @@ struct iso_extended_attributes {
 	u_char len_au			[ISODCL (247, 250)]; /* 723 */
 };
 
-/* CD-ROM Format type */
-enum ISO_FTYPE  { ISO_FTYPE_DEFAULT, ISO_FTYPE_9660, ISO_FTYPE_RRIP, ISO_FTYPE_ECMA };
-
-#ifndef	ISOFSMNT_ROOT
-#define	ISOFSMNT_ROOT	0
-#endif
-
-struct iso_mnt {
-	int im_flags;
-
-	struct mount *im_mountp;
-	dev_t im_dev;
-	struct vnode *im_devvp;
-
-	int logical_block_size;
-	int im_bshift;
-	int im_bmask;
-	
-	int volume_space_size;
-	struct netexport im_export;
-	
-	char root[ISODCL (157, 190)];
-	int root_extent;
-	int root_size;
-	enum ISO_FTYPE  iso_ftype;
-	
-	int rr_skip;
-	int rr_skip0;
-};
-
-#define VFSTOISOFS(mp)	((struct iso_mnt *)((mp)->mnt_data))
-
-#define blkoff(imp, loc)	((loc) & (imp)->im_bmask)
-#define lblktosize(imp, blk)	((blk) << (imp)->im_bshift)
-#define lblkno(imp, loc)	((loc) >> (imp)->im_bshift)
-#define blksize(imp, ip, lbn)	((imp)->logical_block_size)
-
-int cd9660_mount __P((struct mount *,
-	    const char *, void *, struct nameidata *, struct proc *));
-int cd9660_start __P((struct mount *, int, struct proc *));
-int cd9660_unmount __P((struct mount *, int, struct proc *));
-int cd9660_root __P((struct mount *, struct vnode **));
-int cd9660_quotactl __P((struct mount *, int, uid_t, caddr_t, struct proc *));
-int cd9660_statfs __P((struct mount *, struct statfs *, struct proc *));
-int cd9660_sync __P((struct mount *, int, struct ucred *, struct proc *));
-int cd9660_vget __P((struct mount *, ino_t, struct vnode **));
-int cd9660_fhtovp __P((struct mount *, struct fid *, struct mbuf *,
-	    struct vnode **, int *, struct ucred **));
-int cd9660_vptofh __P((struct vnode *, struct fid *));
-void cd9660_init __P((void));
-
-int cd9660_mountroot __P((void)); 
-
-extern int (**cd9660_vnodeop_p) __P((void *));
-extern int (**cd9660_specop_p) __P((void *));
-#ifdef FIFO
-extern int (**cd9660_fifoop_p) __P((void *));
-#endif
-
 extern __inline int isonum_711 __P((u_char *));
 extern __inline int isonum_712 __P((char *));
+/* XXX __inline int isonum_721 __P((u_char *)); */
+/* XXX __inline int isonum_722 __P((u_char *)); */
 extern __inline int isonum_723 __P((u_char *));
+/* XXX __inline int isonum_731 __P((u_char *)); */
+/* XXX __inline int isonum_732 __P((u_char *)); */
 extern __inline int isonum_733 __P((u_char *));
 
 extern __inline int
@@ -215,7 +166,8 @@ isonum_712(p)
 	return *p;
 }
 
-#ifndef UNALIGNED_ACCESS
+/* XXX extern __inline int isonum_721 __P((u_char *)); */
+/* XXX extern __inline int isonum_722 __P((u_char *)); */
 
 extern __inline int
 #if __STDC__
@@ -225,60 +177,20 @@ isonum_723(p)
 	u_char *p;
 #endif
 {
-	return *p|(p[1] << 8);
-}
-
-extern __inline int
-#if __STDC__
-isonum_733(u_char *p)
-#else
-isonum_733(p)
-	u_char *p;
-#endif
-{
-	return *p|(p[1] << 8)|(p[2] << 16)|(p[3] << 24);
-}
-
-#else /* UNALIGNED_ACCESS */
-
+#if defined(UNALIGNED_ACCESS) && \
+    ((BYTE_ORDER == LITTLE_ENDIAN) || (BYTE_ORDER == BIG_ENDIAN))
 #if BYTE_ORDER == LITTLE_ENDIAN
-
-extern __inline int
-#if __STDC__
-isonum_723(u_char *p)
-#else
-isonum_723(p)
-	u_char *p;
-#endif
-{
 	return *(u_int16t *)p;
-}
-
-extern __inline int
-#if __STDC__
-isonum_733(u_char *p)
 #else
-isonum_733(p)
-	u_char *p;
-#endif
-{
-	return *(u_int32t *)p;
-}
-
-#endif
-
-#if BYTE_ORDER == BIG_ENDIAN
-
-extern __inline int
-#if __STDC__
-isonum_723(u_char *p)
-#else
-isonum_723(p)
-	u_char *p;
-#endif
-{
 	return *(u_int16t *)(p + 2);
+#endif
+#else /* !UNALIGNED_ACCESS or weird byte order */
+	return *p|(p[1] << 8);
+#endif
 }
+
+/* XXX extern __inline int isonum_731 __P((u_char *)); */
+/* XXX extern __inline int isonum_732 __P((u_char *)); */
 
 extern __inline int
 #if __STDC__
@@ -288,16 +200,17 @@ isonum_733(p)
 	u_char *p;
 #endif
 {
+#if defined(UNALIGNED_ACCESS) && \
+    ((BYTE_ORDER == LITTLE_ENDIAN) || (BYTE_ORDER == BIG_ENDIAN))
+#if BYTE_ORDER == LITTLE_ENDIAN
+	return *(u_int32t *)p;
+#else
 	return *(u_int32t *)(p + 4);
-}
-
 #endif
-
-#endif /* UNALIGNED_ACCESS */
-
-int isofncmp __P((const u_char *, int, const u_char *, int));
-void isofntrans __P((u_char *, int, u_char *, u_short *, int, int));
-ino_t isodirino __P((struct iso_directory_record *, struct iso_mnt *));
+#else /* !UNALIGNED_ACCESS or weird byte order */
+	return *p|(p[1] << 8)|(p[2] << 16)|(p[3] << 24);
+#endif
+}
 
 /*
  * Associated files have a leading '='.
