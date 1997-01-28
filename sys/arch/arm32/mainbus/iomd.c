f15caece27f5da34012dd8b5946765b456d9c2fb
mark <mark@NetBSD.org>
1997-01-28 04:08:21 +0000

    Add prototypes for match and attach functions.

--- sys/arch/arm32/mainbus/iomd.c
+++ sys/arch/arm32/mainbus/iomd.c
@@ -1,4 +1,4 @@
-/* $NetBSD: iomd.c,v 1.5 1996/11/23 03:45:52 mark Exp $ */
+/*	$NetBSD: iomd.c,v 1.6 1997/01/28 04:08:21 mark Exp $	*/
 
 /*
  * Copyright (c) 1996 Mark Brinicombe.
@@ -57,6 +57,9 @@
 #if NIOMD != 1
 #error Need at 1 IOMD device configured
 #endif
+
+int iomdmatch __P((struct device *parent, void *match, void *aux));
+void iomdattach __P((struct device *parent, struct device *self, void *aux));
 
 /* Declare prototypes */
 
