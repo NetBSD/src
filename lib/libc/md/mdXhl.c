10cab6bea79488fe6cc66094e1427c329c4c4e3a
thorpej <thorpej@NetBSD.org>
1997-01-30 01:01:39 +0000

    Add MD4/MD5 routines to the C library, taken directly from FreeBSD's "libmd".

---
+++ lib/libc/md/mdXhl.c
@@ -0,0 +1,73 @@
+/*	$NetBSD: mdXhl.c,v 1.1 1997/01/30 01:01:44 thorpej Exp $	*/
+
+/* mdXhl.c
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <phk@login.dkuug.dk> wrote this file.  As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return.   Poul-Henning Kamp
+ * ----------------------------------------------------------------------------
+ *
+ * from FreeBSD Id: mdXhl.c,v 1.8 1996/10/25 06:48:12 bde Exp
+ *
+ */
+
+#include <sys/types.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "mdX.h"
+
+char *
+MDXEnd(MDX_CTX *ctx, char *buf)
+{
+    int i;
+    unsigned char digest[16];
+    static const char hex[]="0123456789abcdef";
+
+    if (!buf)
+        buf = malloc(33);
+    if (!buf)
+	return 0;
+    MDXFinal(digest,ctx);
+    for (i=0;i<16;i++) {
+	buf[i+i] = hex[digest[i] >> 4];
+	buf[i+i+1] = hex[digest[i] & 0x0f];
+    }
+    buf[i+i] = '\0';
+    return buf;
+}
+
+char *
+MDXFile (char *filename, char *buf)
+{
+    unsigned char buffer[BUFSIZ];
+    MDX_CTX ctx;
+    int f,i,j;
+
+    MDXInit(&ctx);
+    f = open(filename,O_RDONLY);
+    if (f < 0) return 0;
+    while ((i = read(f,buffer,sizeof buffer)) > 0) {
+	MDXUpdate(&ctx,buffer,i);
+    }
+    j = errno;
+    close(f);
+    errno = j;
+    if (i < 0) return 0;
+    return MDXEnd(&ctx, buf);
+}
+
+char *
+MDXData (const unsigned char *data, unsigned int len, char *buf)
+{
+    MDX_CTX ctx;
+
+    MDXInit(&ctx);
+    MDXUpdate(&ctx,data,len);
+    return MDXEnd(&ctx, buf);
+}
