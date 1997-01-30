038991c893c00362a9344f0fa763cd229ccaf267
thorpej <thorpej>
1997-01-30 01:10:32 +0000

    Add support for generating MD5 checksums, partially derived from the FreeBSD md5(1) program.  MD5 checksums may be generated by using the "-m" flag to cksum, or by invoking this program as "md5".

--- usr.bin/cksum/cksum.c
+++ usr.bin/cksum/cksum.c
@@ -1,6 +1,7 @@
-/*	$NetBSD: cksum.c,v 1.7 1995/09/02 05:45:18 jtc Exp $	*/
+/*	$NetBSD: cksum.c,v 1.8 1997/01/30 01:10:34 thorpej Exp $	*/
 
 /*-
+ * Copyright (c) 1997 Jason R. Thorpe.  All rights reserved.
  * Copyright (c) 1991, 1993
  *	The Regents of the University of California.  All rights reserved.
  *
@@ -46,7 +47,7 @@ static char copyright[] =
 #if 0
 static char sccsid[] = "@(#)cksum.c	8.2 (Berkeley) 4/28/95";
 #endif
-static char rcsid[] = "$NetBSD: cksum.c,v 1.7 1995/09/02 05:45:18 jtc Exp $";
+static char rcsid[] = "$NetBSD: cksum.c,v 1.8 1997/01/30 01:10:34 thorpej Exp $";
 #endif /* not lint */
 
 #include <sys/cdefs.h>
@@ -55,6 +56,7 @@ static char rcsid[] = "$NetBSD: cksum.c,v 1.7 1995/09/02 05:45:18 jtc Exp $";
 #include <err.h>
 #include <errno.h>
 #include <fcntl.h>
+#include <md5.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -62,6 +64,8 @@ static char rcsid[] = "$NetBSD: cksum.c,v 1.7 1995/09/02 05:45:18 jtc Exp $";
 
 #include "extern.h"
 
+int md5_digest_file __P((char *));
+void requiremd5 __P((const char *));
 void usage __P((void));
 
 int
@@ -69,14 +73,19 @@ main(argc, argv)
 	int argc;
 	char **argv;
 {
-	register int ch, fd, rval;
+	register int ch, fd, rval, domd5, dosum, pflag, nomd5stdin;
 	u_int32_t len, val;
 	char *fn;
 	int (*cfncn) __P((int, u_int32_t *, u_int32_t *));
 	void (*pfncn) __P((char *, u_int32_t, u_int32_t));
 	extern char *__progname;
 
-	if (!strcmp(__progname, "sum")) {
+	dosum = domd5 = pflag = nomd5stdin = 0;
+
+	if (!strcmp(__progname, "md5"))
+		domd5 = 1;
+	else if (!strcmp(__progname, "sum")) {
+		dosum = 1;
 		cfncn = csum1;
 		pfncn = psum1;
 	} else {
@@ -84,9 +93,20 @@ main(argc, argv)
 		pfncn = pcrc;
 	}
 
-	while ((ch = getopt(argc, argv, "o:")) != -1)
+	while ((ch = getopt(argc, argv, "mo:ps:tx")) != -1)
 		switch(ch) {
+		case 'm':
+			if (dosum) {
+				warnx("sum mutually exclusive with md5");
+				usage();
+			}
+			domd5 = 1;
+			break;
 		case 'o':
+			if (domd5) {
+				warnx("md5 mutually exclusive with sum");
+				usage();
+			}
 			if (!strcmp(optarg, "1")) {
 				cfncn = csum1;
 				pfncn = psum1;
@@ -97,6 +117,29 @@ main(argc, argv)
 				warnx("illegal argument to -o option");
 				usage();
 			}
+			break;
+		case 'p':
+			if (!domd5)
+				requiremd5("-p");
+			pflag = 1;
+			break;
+		case 's':
+			if (!domd5)
+				requiremd5("-s");
+			nomd5stdin = 1;
+			MDString(optarg);
+			break;
+		case 't':
+			if (!domd5)
+				requiremd5("-t");
+			MDTimeTrial();
+			nomd5stdin = 1;
+			break;
+		case 'x':
+			if (!domd5)
+				requiremd5("-x");
+			MDTestSuite();
+			nomd5stdin = 1;
 			break;
 		case '?':
 		default:
@@ -111,26 +154,62 @@ main(argc, argv)
 	do {
 		if (*argv) {
 			fn = *argv++;
+			if (domd5) {
+				if (md5_digest_file(fn)) {
+					warn("%s", fn);
+					rval = 1;
+				}
+				continue;
+			}
 			if ((fd = open(fn, O_RDONLY, 0)) < 0) {
 				warn("%s", fn);
 				rval = 1;
 				continue;
 			}
+		} else if (domd5 && !nomd5stdin)
+			MDFilter(pflag);
+
+		if (!domd5) {
+			if (cfncn(fd, &val, &len)) {
+				warn("%s", fn ? fn : "stdin");
+				rval = 1;
+			} else
+				pfncn(fn, val, len);
+			(void)close(fd);
 		}
-		if (cfncn(fd, &val, &len)) {
-			warn("%s", fn ? fn : "stdin");
-			rval = 1;
-		} else
-			pfncn(fn, val, len);
-		(void)close(fd);
 	} while (*argv);
 	exit(rval);
+}
+
+int
+md5_digest_file(fn)
+	char *fn;
+{
+	char buf[33], *cp;
+
+	cp = MD5File(fn, buf);
+	if (cp == NULL)
+		return (1);
+
+	printf("MD5 (%s) = %s\n", fn, cp);
+	return (0);
+}
+
+void
+requiremd5(flg)
+	const char *flg;
+{
+	warnx("%s flag requires `md5' or -m", flg);
+	usage();
 }
 
 void
 usage()
 {
 
-	(void)fprintf(stderr, "usage: cksum [-o 1 | 2] [file ...]\n");
+	(void)fprintf(stderr, "usage: cksum [-m | [-o 1 | 2]] [file ...]\n");
+	(void)fprintf(stderr, "       sum [file ...]\n");
+	(void)fprintf(stderr,
+	    "       md5 [-p | -t | -x | -s string] [file ...]\n");
 	exit(1);
 }
