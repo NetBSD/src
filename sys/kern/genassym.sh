b5a327ca352a94b69927cc83d6c561a11ddd310f
matthias <matthias@NetBSD.org>
1997-01-30 09:37:20 +0000

    Add scripts and documentation for a new method to create assym.h files. With the kern/genassym.sh script HOSTED_CC is no longer used to create assym.h files. This should make crosscompilation easier.

---
+++ sys/kern/genassym.sh
@@ -0,0 +1,134 @@
+#	$NetBSD: genassym.sh,v 1.1 1997/01/30 09:38:07 matthias Exp $
+
+#
+# Copyright (c) 1997 Matthias Pfaller.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+# 3. All advertising materials mentioning features or use of this software
+#    must display the following acknowledgement:
+#	This product includes software developed by Matthias Pfaller.
+# 4. The name of the author may not be used to endorse or promote products
+#    derived from this software without specific prior written permission
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+# OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+# IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+# NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+# THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+
+# If first argument is -c, create a temporary C file,
+# compile it and execute the result.
+
+if [ $1 = '-c' ] ; then
+	shift
+	ccode=1
+else
+	ccode=0
+fi
+echo $ccode
+trap "rm -f /tmp/$$.c /tmp/genassym.$$" 0 1 2 3 15
+awk -v ccode=$ccode '
+function defstart()
+{
+	if (defining == 0) {
+		defining = 1;
+		printf("void f" FNR "(void);\n");
+		printf("void f" FNR "() {\n");
+		if (ccode)
+			call[FNR] = "f" FNR;
+		defining = 1;
+	}
+}
+
+function defend()
+{
+	if (defining != 0) {
+		defining = 0;
+		printf("}\n");
+	}
+}
+
+BEGIN {
+	printf("#ifndef _KERNEL\n#define _KERNEL\n#endif\n");
+	printf("#define	offsetof(type, member) ((size_t)(&((type *)0)->member))\n");
+	matched = 0;
+	defining = 0;
+	ccode = 1;
+}
+
+/^[ \t]*#.*/ {
+	# Just ignore comments
+	matched = 1;
+}
+
+/^include[ \t]/ {
+	defend();
+	printf("#%s\n", $0);
+	matched = 1;
+}
+
+/^if[ \t]/ ||
+/^ifdef[ \t]/ ||
+/^ifndef[ \t]/ ||
+/^else/ ||
+/^elif[ \t]/ ||
+/^endif/ {
+	printf("#%s\n", $0);
+	matched = 1;
+}
+
+/^define[ \t]/ {
+	defstart();
+	value = $0
+	gsub("^define[ \t]+[A-Za-z_][A-Za-z_0-9]*[ \t]+", "", value)
+	if (ccode)
+		printf("printf(\"#define " $2 " %%ld\\n\", (long)" value ");\n");
+	else
+		printf("__asm(\"XYZZY %s %%0\" : : \"n\" (%s));\n", $2, value);
+	matched = 1;
+}
+
+/^quote[ \t]/ {
+	value = $0
+	gsub("^quote[ \t]+", "", value)
+	printf(value);
+	matched = 1;
+}
+
+{
+	if (matched || NF == 0)
+		matched = 0;
+	else {
+		printf("syntax error in line %d\n", FNR) >"/dev/stderr";
+		exit(1);
+	}
+}
+END {
+	defend();
+	if (ccode) {
+		printf("int main(int argc, char **argv) {");
+		for (i in call)
+			printf(call[i] "();");
+		printf("}\n");
+	}
+}
+' >/tmp/$$.c || exit 1
+if [ $ccode = 1 ] ; then
+	"$@" /tmp/$$.c -o /tmp/genassym.$$ && /tmp/genassym.$$
+else
+	"$@" -S /tmp/$$.c -o -| sed -n 's/.*XYZZY/#define/gp'
+fi
