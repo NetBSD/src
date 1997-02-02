3c89d3f8ba1a10d0d517aadf176aa8b27a13a60e
thorpej <thorpej@NetBSD.org>
1997-02-02 08:17:46 +0000

    Garbage-collect extra copypage()

--- sys/arch/mac68k/mac68k/locore.s
+++ sys/arch/mac68k/mac68k/locore.s
@@ -1,4 +1,4 @@
-/*	$NetBSD: locore.s,v 1.73 1997/01/09 07:28:12 scottr Exp $	*/
+/*	$NetBSD: locore.s,v 1.74 1997/02/02 08:17:46 thorpej Exp $	*/
 
 /*
  * Copyright (c) 1988 University of Utah.
@@ -1121,38 +1121,6 @@ _esigcode:
  */ 
 
 #include <m68k/asm.h>
-
-/*
- * copypage(fromaddr, toaddr)
- *
- * Optimized version of bcopy for a single page-aligned NBPG byte copy.
- */
-ENTRY(copypage)
-	movl	sp@(4),a0		| source address
-	movl	sp@(8),a1		| destination address
-	movl	#NBPG/32,d0		| number of 32 byte chunks
-#if defined(M68040)
-	cmpl	#MMU_68040,_mmutype	| 68040?
-	jne	Lmlloop			| no, use movl
-Lm16loop:
-	.long	0xf6209000		| move16 a0@+,a1@+
-	.long	0xf6209000		| move16 a0@+,a1@+
-	subql	#1,d0
-	jne	Lm16loop
-	rts
-#endif
-Lmlloop:
-	movl	a0@+,a1@+
-	movl	a0@+,a1@+
-	movl	a0@+,a1@+
-	movl	a0@+,a1@+
-	movl	a0@+,a1@+
-	movl	a0@+,a1@+
-	movl	a0@+,a1@+
-	movl	a0@+,a1@+
-	subql	#1,d0
-	jne	Lmlloop
-	rts
 
 /*
  * non-local gotos
