d8deae5099c5f45f678db8244db3ed5f2144b89c
scottr <scottr@NetBSD.org>
1997-02-03 17:36:00 +0000

    Convert to bus.h; this removes the need for the ASCBase global.

--- sys/arch/mac68k/obio/asc.c
+++ sys/arch/mac68k/obio/asc.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: asc.c,v 1.15 1996/12/16 16:17:02 scottr Exp $	*/
+/*	$NetBSD: asc.c,v 1.16 1997/02/03 17:36:00 scottr Exp $	*/
 
 /*-
  * Copyright (C) 1993	Allen K. Briggs, Chris P. Caputo,
@@ -47,15 +47,19 @@
 
 #include <machine/autoconf.h>
 #include <machine/cpu.h>
+#include <machine/bus.h>
 
 #include "ascvar.h"
+#include "obiovar.h"
 
-/* Global ASC location */
-volatile unsigned char *ASCBase = (unsigned char *) 0x14000;
-
+#define	MAC68K_ASC_BASE	((caddr_t) 0x50f14000)
+#define	MAC68K_ASC_LEN	0x01000
 
 /* bell support data */
-static int asc_configured = 0;
+static int		asc_configured = 0;
+static bus_space_tag_t	asc_tag = MAC68K_BUS_SPACE_MEM;
+static bus_space_handle_t asc_handle;
+
 static int bell_freq = 1880;
 static int bell_length = 10;
 static int bell_volume = 100;
@@ -78,31 +82,66 @@ ascmatch(parent, cf, aux)
 	struct cfdata *cf;
 	void *aux;
 {
-	if (badbaddr((unsigned char *) ASCBase))
-		return 0;
-	return 1;
+	static int asc_matched = 0;
+	struct obio_attach_args *oa = aux;
+	bus_space_tag_t bst = MAC68K_BUS_SPACE_MEM;
+	bus_space_handle_t bsh;
+	bus_addr_t addr;
+	int rval = 0;
+
+	/* Allow only one instance. */
+	if (asc_matched)
+		return (0);
+	asc_matched = 1;
+
+	addr = (bus_addr_t) (oa->oa_addr ? oa->oa_addr : MAC68K_ASC_BASE);
+
+	if (bus_space_map(bst, addr, MAC68K_ASC_LEN, 0, &bsh))
+		return (0);
+
+	if (bus_probe(bst, bsh, 0, 1))
+		rval = 1;
+	else
+		rval = 0;
+
+	bus_space_unmap(bst, bsh, MAC68K_ASC_LEN);
+
+	return rval;
 }
 
 static void
-ascattach(parent, dev, aux)
-	struct device *parent, *dev;
-	void   *aux;
+ascattach(parent, self, aux)
+	struct device *parent, *self;
+	void *aux;
 {
-	printf(" Apple sound chip.\n");
+	struct obio_attach_args *oa = aux;
+	bus_addr_t addr;
+
+	addr = (bus_addr_t) (oa->oa_addr ? oa->oa_addr : MAC68K_ASC_BASE);
+	if (bus_space_map(asc_tag, addr, MAC68K_ASC_LEN, 0,
+	    &asc_handle)) {
+		printf("%s: can't map memory space\n", self->dv_xname);
+		return;
+	}
+
+	printf(" Apple sound chip\n");
 	asc_configured = 1;
 }
 
 int 
 asc_setbellparams(freq, length, volume)
-    int freq;
-    int length;
-    int volume;
+	int freq;
+	int length;
+	int volume;
 {
-	if (!asc_configured) return (ENODEV);
+	if (!asc_configured)
+		return (ENODEV);
 
-	/* I only perform these checks for sanity. */
-	/* I suppose someone might want a bell that rings */
-	/* all day, but then the can make kernel mods themselves. */
+	/*
+	 * I only perform these checks for sanity.  I suppose
+	 * someone might want a bell that rings all day, but then
+	 * they can make kernel mods themselves.
+	 */
 
 	if (freq < 10 || freq > 40000)
 		return (EINVAL);
@@ -125,7 +164,8 @@ asc_getbellparams(freq, length, volume)
     int *length;
     int *volume;
 {
-	if (!asc_configured) return (ENODEV);
+	if (!asc_configured)
+		return (ENODEV);
 
 	*freq = bell_freq;
 	*length = bell_length;
@@ -139,14 +179,14 @@ void
 asc_bellstop(param)
     int param;
 {
-	if (!asc_configured) return;
+	if (!asc_configured)
+		return;
 
 	if (bell_ringing > 1000 || bell_ringing < 0)
-		panic("bell got out of synch?????");
-	if (--bell_ringing == 0) {
-		ASCBase[0x801] = 0;
-	}
-	/* disable ASC */
+		panic("bell got out of sync?");
+
+	if (--bell_ringing == 0)	/* disable ASC */
+		bus_space_write_1(asc_tag, asc_handle, 0x801, 0);
 }
 
 
@@ -156,23 +196,29 @@ asc_ringbell()
 	int     i;
 	unsigned long freq;
 
-	if (!asc_configured) return (ENODEV);
+	if (!asc_configured)
+		return (ENODEV);
 
 	if (bell_ringing == 0) {
+
 		for (i = 0; i < 0x800; i++)
-			ASCBase[i] = 0;
+			bus_space_write_1(asc_tag, asc_handle, i, 0);
 
 		for (i = 0; i < 256; i++) {
-			ASCBase[i] = i / 4;
-			ASCBase[i + 512] = i / 4;
-			ASCBase[i + 1024] = i / 4;
-			ASCBase[i + 1536] = i / 4;
+			bus_space_write_1(asc_tag, asc_handle, i, i / 4);
+			bus_space_write_1(asc_tag, asc_handle, i + 512, i / 4);
+			bus_space_write_1(asc_tag, asc_handle, i + 1024, i / 4);
+			bus_space_write_1(asc_tag, asc_handle, i + 1536, i / 4);
 		}		/* up part of wave, four voices ? */
 		for (i = 0; i < 256; i++) {
-			ASCBase[i + 256] = 0x3f - (i / 4);
-			ASCBase[i + 768] = 0x3f - (i / 4);
-			ASCBase[i + 1280] = 0x3f - (i / 4);
-			ASCBase[i + 1792] = 0x3f - (i / 4);
+			bus_space_write_1(asc_tag, asc_handle, i + 256,
+			    0x3f - (i / 4));
+			bus_space_write_1(asc_tag, asc_handle, i + 768,
+			    0x3f - (i / 4));
+			bus_space_write_1(asc_tag, asc_handle, i + 1280,
+			    0x3f - (i / 4));
+			bus_space_write_1(asc_tag, asc_handle, i + 1792,
+			    0x3f - (i / 4));
 		}		/* down part of wave, four voices ? */
 
 		/* Fix this.  Need to find exact ASC sampling freq */
@@ -182,22 +228,27 @@ asc_ringbell()
 		 * cur_beep.freq, (freq >> 24) & 0xff, (freq >> 16) & 0xff,
 		 * (freq >> 8) & 0xff, (freq) & 0xff); */
 		for (i = 0; i < 8; i++) {
-			ASCBase[0x814 + 8 * i] = (freq >> 24) & 0xff;
-			ASCBase[0x815 + 8 * i] = (freq >> 16) & 0xff;
-			ASCBase[0x816 + 8 * i] = (freq >> 8) & 0xff;
-			ASCBase[0x817 + 8 * i] = (freq) & 0xff;
+			bus_space_write_1(asc_tag, asc_handle, 0x814 + 8 * i,
+			    (freq >> 24) & 0xff);
+			bus_space_write_1(asc_tag, asc_handle, 0x815 + 8 * i,
+			    (freq >> 16) & 0xff);
+			bus_space_write_1(asc_tag, asc_handle, 0x816 + 8 * i,
+			    (freq >> 8) & 0xff);
+			bus_space_write_1(asc_tag, asc_handle, 0x817 + 8 * i,
+			    (freq) & 0xff);
 		}		/* frequency; should put cur_beep.freq in here
 				 * somewhere. */
 
-		ASCBase[0x807] = 3;	/* 44 ? */
-		ASCBase[0x806] = 255 * bell_volume / 100;
-		ASCBase[0x805] = 0;
-		ASCBase[0x80f] = 0;
-		ASCBase[0x802] = 2;	/* sampled */
-		ASCBase[0x801] = 2;	/* enable sampled */
+		bus_space_write_1(asc_tag, asc_handle, 0x807, 3); /* 44 ? */
+		bus_space_write_1(asc_tag, asc_handle, 0x806,
+		    255 * bell_volume / 100);
+		bus_space_write_1(asc_tag, asc_handle, 0x805, 0);
+		bus_space_write_1(asc_tag, asc_handle, 0x80f, 0);
+		bus_space_write_1(asc_tag, asc_handle, 0x802, 2); /* sampled */
+		bus_space_write_1(asc_tag, asc_handle, 0x801, 2); /* enable sampled */
 	}
 	bell_ringing++;
 	timeout((void *) asc_bellstop, 0, bell_length);
 
-	return 0;
+	return (0);
 }
