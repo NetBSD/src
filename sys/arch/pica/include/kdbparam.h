e0972fb4af15e35d87b393fc95ccdae3e0c5a264
mrg <mrg@NetBSD.org>
1997-02-16 12:00:33 +0000

    first non-working cut of Chris Siebenmann <cks@utcc.utoronto.ca> rshrcmd() kind of reworked, to fit in to the framework required for rcmd(3) to remain as useful as it is, and for backwards compatibility.

--- sys/arch/pica/include/kdbparam.h
+++
@@ -1,3 +0,0 @@
-/*	$NetBSD: kdbparam.h,v 1.2 1996/03/23 03:42:46 jonathan Exp $	*/
-
-#include <mips/kdbparam.h>
--- sys/arch/pmax/include/limits.h
+++
@@ -1,7 +0,0 @@
-/*	$NetBSD: limits.h,v 1.10 1996/03/19 03:06:28 jonathan Exp $	*/
-
-#include <mips/limits.h>
-
-#ifdef _KERNEL
-#define CLK_TCK		60		/* ticks per second */
-#endif
