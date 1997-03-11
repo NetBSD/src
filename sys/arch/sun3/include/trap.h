fef866712a6ac435a79df53ea363828c9545f048
gwr <gwr>
1997-03-11 20:31:20 +0000

    Remove unnecessary differences (T_BRKPT, T_WATCHPOINT gone).

--- sys/arch/sun3/include/trap.h
+++ sys/arch/sun3/include/trap.h
@@ -1,11 +1,3 @@
-/*	$NetBSD: trap.h,v 1.7 1994/11/21 21:34:23 gwr Exp $	*/
+/*	$NetBSD: trap.h,v 1.8 1997/03/11 20:31:20 gwr Exp $	*/
 
 #include <m68k/trap.h>
-
-/*
- * XXX - need documentation.
- * probably should be comment,
- * for compat code
- */
-#define	T_BRKPT		T_TRAP15
-#define	T_WATCHPOINT	16
