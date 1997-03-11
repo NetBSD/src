b1590813c36b7e18e89e0620fe813fb4b31ae100
is <is@NetBSD.org>
1997-03-11 16:29:19 +0000

    Change to new ARP code. print-arp print arbitrary ARP packets now. Needs more work to support ARCnet, Metricom and ATM.

--- usr.sbin/tcpdump/print-bootp.c
+++ usr.sbin/tcpdump/print-bootp.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: print-bootp.c,v 1.2 1995/03/06 19:11:05 mycroft Exp $	*/
+/*	$NetBSD: print-bootp.c,v 1.2.4.1 1997/03/11 16:29:23 is Exp $	*/
 
 /*
  * Copyright (c) 1990, 1991, 1993, 1994
@@ -35,7 +35,12 @@ static char rcsid[] =
 #include <net/if.h>
 
 #include <netinet/in.h>
+#ifdef __NetBSD__
+#include <net/if_ether.h>
+#else
 #include <netinet/if_ether.h>
+#endif
+
 
 #include <ctype.h>
 #include <stdio.h>
