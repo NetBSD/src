1a3205418c71753dfa2a5c05d7afc910514ceae8
cgd <cgd@NetBSD.org>
1997-03-13 04:07:44 +0000

    change microcode array definitions from "unsigned short" to the more correct u_int16_t, and remove bogus casts that the old definition required.

--- sys/dev/pci/isp_pci.c
+++ sys/dev/pci/isp_pci.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: isp_pci.c,v 1.4 1997/03/13 03:33:07 mycroft Exp $	*/
+/*	$NetBSD: isp_pci.c,v 1.5 1997/03/13 04:07:47 cgd Exp $	*/
 
 /*
  * PCI specific probe and attach routines for Qlogic ISP SCSI adapters.
@@ -79,7 +79,7 @@ static struct ispmdvec mdvec = {
 	NULL,
 	NULL,
 	isp_pci_reset1,
-	(const u_int16_t *) ISP_RISC_CODE,
+	ISP_RISC_CODE,
 	ISP_CODE_LENGTH,
 	ISP_CODE_ORG,
 /*	BIU_PCI_CONF1_FIFO_16 | BIU_BURST_ENABLE */ 0
