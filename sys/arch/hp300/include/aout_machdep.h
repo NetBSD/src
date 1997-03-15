0e368d2e40399f817694f3beea3aee2a2860079c
thorpej <thorpej@NetBSD.org>
1997-03-15 21:59:20 +0000

    Don't set __LDPGSZ to 4096; we use 8192 like the rest of m68k-land, now.

--- sys/arch/hp300/include/aout_machdep.h
+++ sys/arch/hp300/include/aout_machdep.h
@@ -1,9 +1,8 @@
-/*	$NetBSD: aout_machdep.h,v 1.12 1996/10/08 13:07:30 cgd Exp $	*/
+/*	$NetBSD: aout_machdep.h,v 1.13 1997/03/15 21:59:20 thorpej Exp $	*/
 
 #ifndef _MACHINE_EXEC_H_
 #define _MACHINE_EXEC_H_
 
-#define __LDPGSZ	4096	/* XXX */
 #include <m68k/aout_machdep.h>
 
 #endif  /* _MACHINE_EXEC_H_ */
