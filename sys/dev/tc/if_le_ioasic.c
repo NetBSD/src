f4f32953defed77023a86083d1fbfc072134e303
cgd <cgd>
1997-03-15 21:06:09 +0000

    fix botches in is's new ARP changes

--- sys/dev/tc/if_le_ioasic.c
+++ sys/dev/tc/if_le_ioasic.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: if_le_ioasic.c,v 1.4 1997/03/15 18:12:05 is Exp $	*/
+/*	$NetBSD: if_le_ioasic.c,v 1.5 1997/03/15 21:06:12 cgd Exp $	*/
 
 /*
  * Copyright (c) 1996 Carnegie-Mellon University.
@@ -39,6 +39,7 @@
 #include <sys/device.h>
 
 #include <net/if.h>
+#include <net/if_ether.h>
 
 #ifdef INET
 #include <netinet/in.h>
