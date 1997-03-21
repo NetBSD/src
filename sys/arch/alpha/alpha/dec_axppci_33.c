ef36d8d12990d689a89ec72d6f4b2be702f21933
cgd <cgd>
1997-03-21 01:26:08 +0000

    fix check for bus number when root dev is behind PPB

--- sys/arch/alpha/alpha/dec_axppci_33.c
+++ sys/arch/alpha/alpha/dec_axppci_33.c
@@ -1,7 +1,7 @@
-/*	$NetBSD: dec_axppci_33.c,v 1.18 1997/01/15 22:13:37 cgd Exp $	*/
+/*	$NetBSD: dec_axppci_33.c,v 1.19 1997/03/21 01:26:10 cgd Exp $	*/
 
 /*
- * Copyright (c) 1995, 1996 Carnegie-Mellon University.
+ * Copyright (c) 1995, 1996, 1997 Carnegie-Mellon University.
  * All rights reserved.
  *
  * Author: Chris G. Demetriou
@@ -167,7 +167,7 @@ dec_axppci_33_device_register(dev, aux)
 		else {
 			struct pcibus_attach_args *pba = aux;
 
-			if (b->bus != pba->pba_bus)
+			if ((b->slot / 1000) != pba->pba_bus)
 				return;
 	
 			pcidev = dev;
@@ -184,7 +184,7 @@ dec_axppci_33_device_register(dev, aux)
 		else {
 			struct pci_attach_args *pa = aux;
 
-			if (b->slot != pa->pa_device)
+			if ((b->slot % 1000) != pa->pa_device)
 				return;
 
 			/* XXX function? */
@@ -239,7 +239,7 @@ dec_axppci_33_device_register(dev, aux)
 		else {
 			struct pci_attach_args *pa = aux;
 
-			if (b->slot != pa->pa_device)
+			if ((b->slot % 1000) != pa->pa_device)
 				return;
 
 			/* XXX function? */
