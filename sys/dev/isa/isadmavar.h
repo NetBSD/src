6e51aa6890dac915f5b187b5eaf7507847ce14bf
mycroft <mycroft@NetBSD.org>
1997-03-21 02:17:11 +0000

    Clean up a bit, and export isa_dmamask() and isa_dmaunmask() interfaces.

--- sys/dev/isa/isadmavar.h
+++ sys/dev/isa/isadmavar.h
@@ -1,9 +1,11 @@
-/*	$NetBSD: isadmavar.h,v 1.5 1997/03/21 00:00:22 mycroft Exp $	*/
+/*	$NetBSD: isadmavar.h,v 1.6 1997/03/21 02:17:13 mycroft Exp $	*/
 
 #define	DMAMODE_WRITE	0
 #define	DMAMODE_READ	1
 #define	DMAMODE_LOOP	2
 
+void isa_dmaunmask __P((int));
+void isa_dmamask __P((int));
 void isa_dmacascade __P((int));
 void isa_dmastart __P((int, caddr_t, vm_size_t, int));
 void isa_dmaabort __P((int));
