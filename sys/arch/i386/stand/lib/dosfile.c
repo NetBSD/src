e07aa5026e0ac940ca4698786912bbcad61f9afd
thorpej <thorpej@NetBSD.org>
1997-03-22 01:48:27 +0000

    Purely cosmetic whitespace and indentation changes (mmm, indent(1))

--- sys/arch/i386/stand/lib/dosfile.c
+++ sys/arch/i386/stand/lib/dosfile.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: dosfile.c,v 1.1.1.1 1997/03/14 02:40:32 perry Exp $	*/
+/*	$NetBSD: dosfile.c,v 1.2 1997/03/22 01:48:31 thorpej Exp $	 */
 
 /*
  * Copyright (c) 1996
@@ -32,191 +32,200 @@
  *
  */
 
-/* DOS filesystem for libsa
- standalone - uses no device, works only with
- DOS running
- needs lowlevel parts from dos_file.S
+/*
+ * DOS filesystem for libsa standalone - uses no device, works only with DOS
+ * running needs lowlevel parts from dos_file.S
  */
 
 #include <lib/libsa/stand.h>
 
 #include "diskbuf.h"
 
-extern int dosopen __P((char*));
+extern int dosopen __P((char *));
 extern void dosclose __P((int));
-extern int dosread __P((int, char*, int));
+extern int dosread __P((int, char *, int));
 extern int dosseek __P((int, int, int));
 
-struct dosfile{
-  int doshandle, off;
+struct dosfile {
+	int             doshandle, off;
 };
 
-extern int doserrno; /* in dos_file.S */
+extern int      doserrno;	/* in dos_file.S */
 
-int dos_open(path, f)
-char *path;
-struct open_file *f;
+int 
+dos_open(path, f)
+	char           *path;
+	struct open_file *f;
 {
-  struct dosfile *df;
+	struct dosfile *df;
 
-  df = (struct dosfile*)alloc(sizeof(*df));
-  if(!df) return(-1);
+	df = (struct dosfile *) alloc(sizeof(*df));
+	if (!df)
+		return (-1);
 
-  df->off = 0;
-  df->doshandle = dosopen(path);
-  if(df->doshandle < 0){
-    printf("DOS error %d\n", doserrno);
-    free(df, sizeof(*df));
-    return(-1);
-  }
-  f->f_fsdata = (void*)df;
-  return(0);
+	df->off = 0;
+	df->doshandle = dosopen(path);
+	if (df->doshandle < 0) {
+		printf("DOS error %d\n", doserrno);
+		free(df, sizeof(*df));
+		return (-1);
+	}
+	f->f_fsdata = (void *) df;
+	return (0);
 }
 
-int dos_read(f, addr, size, resid)
-struct open_file *f;
-char *addr;
-u_int size;
-u_int *resid;	/* out */
+int 
+dos_read(f, addr, size, resid)
+	struct open_file *f;
+	char           *addr;
+	u_int           size;
+	u_int          *resid;	/* out */
 {
-  struct dosfile *df;
-  int got;
-  static int tc = 0;
+	struct dosfile *df;
+	int             got;
+	static int      tc = 0;
 
-  df = (struct dosfile*)f->f_fsdata;
+	df = (struct dosfile *) f->f_fsdata;
 
-  if(!(tc++ % 4)) twiddle();
+	if (!(tc++ % 4))
+		twiddle();
 
-  if((unsigned long)addr >= 0x10000) {
-    u_int lsize = size;
+	if ((unsigned long) addr >= 0x10000) {
+		u_int           lsize = size;
 
-    while(lsize > 0) {
-      u_int tsize;
-      int tgot;
+		while (lsize > 0) {
+			u_int           tsize;
+			int             tgot;
 
-      tsize = lsize;
+			tsize = lsize;
 
-      if(tsize > DISKBUFSIZE)
-	tsize = DISKBUFSIZE;
+			if (tsize > DISKBUFSIZE)
+				tsize = DISKBUFSIZE;
 
-      diskbuf_user = dos_read;
+			diskbuf_user = dos_read;
 
-      tgot = dosread(df->doshandle, diskbuf, tsize);
+			tgot = dosread(df->doshandle, diskbuf, tsize);
 
-      if(tgot < 0) {
-	printf("DOS error %d\n", doserrno);
-	return(-1);
-      }
+			if (tgot < 0) {
+				printf("DOS error %d\n", doserrno);
+				return (-1);
+			}
+			bcopy(diskbuf, addr, tgot);
 
-      bcopy(diskbuf, addr, tgot);
+			addr += tgot;
+			lsize -= tgot;
 
-      addr += tgot;
-      lsize -= tgot;
+			if (tgot != tsize)
+				break;	/* EOF */
+		}
+		got = size - lsize;
+	} else {
+		got = dosread(df->doshandle, addr, size);
 
-      if(tgot != tsize)
-	break;	/* EOF */
-    }
-    got = size - lsize;
-  } else {
-    got = dosread(df->doshandle, addr, size);
+		if (got < 0) {
+			printf("DOS error %d\n", doserrno);
+			return (-1);
+		}
+	}
 
-    if(got < 0){
-      printf("DOS error %d\n", doserrno);
-      return(-1);
-    }
-  }
+	df->off += got;
+	size -= got;
 
-  df->off += got;
-  size -= got;
-
-  if(resid)
-    *resid = size;
-  return(0);
+	if (resid)
+		*resid = size;
+	return (0);
 }
 
-int dos_close(f)
-struct open_file *f;
+int 
+dos_close(f)
+	struct open_file *f;
 {
-  struct dosfile *df;
-  df = (struct dosfile*)f->f_fsdata;
+	struct dosfile *df;
+	df = (struct dosfile *) f->f_fsdata;
 
-  dosclose(df->doshandle);
+	dosclose(df->doshandle);
 
-  if(df) free(df, sizeof(*df));
-  return(0);
+	if (df)
+		free(df, sizeof(*df));
+	return (0);
 }
 
-int dos_write(f, start, size, resid)
-struct open_file *f;
-char *start;
-u_int size;
-u_int *resid;	/* out */
+int 
+dos_write(f, start, size, resid)
+	struct open_file *f;
+	char           *start;
+	u_int           size;
+	u_int          *resid;	/* out */
 {
-  errno = EROFS;
-  return(-1);
+	errno = EROFS;
+	return (-1);
 }
 
-int dos_stat(f, sb)
-struct open_file *f;
-struct stat *sb;
+int 
+dos_stat(f, sb)
+	struct open_file *f;
+	struct stat    *sb;
 {
-  struct dosfile *df;
-  df = (struct dosfile*)f->f_fsdata;
+	struct dosfile *df;
+	df = (struct dosfile *) f->f_fsdata;
 
-  sb->st_mode = 0444;
-  sb->st_nlink = 1;
-  sb->st_uid = 0;
-  sb->st_gid = 0;
-  sb->st_size = -1;
-  return(0);
+	sb->st_mode = 0444;
+	sb->st_nlink = 1;
+	sb->st_uid = 0;
+	sb->st_gid = 0;
+	sb->st_size = -1;
+	return (0);
 }
 
-off_t dos_seek(f, offset, where)
-struct open_file *f;
-off_t offset;
-int where;
+off_t 
+dos_seek(f, offset, where)
+	struct open_file *f;
+	off_t           offset;
+	int             where;
 {
-  struct dosfile *df;
-  int doswhence, res;
+	struct dosfile *df;
+	int             doswhence, res;
 #ifdef DOS_CHECK
-  int checkoffs;
+	int             checkoffs;
 #endif
-  df = (struct dosfile*)f->f_fsdata;
+	df = (struct dosfile *) f->f_fsdata;
 
-  switch(where){
-    case SEEK_SET:
-      doswhence = 0;
+	switch (where) {
+	case SEEK_SET:
+		doswhence = 0;
 #ifdef DOS_CHECK
-      checkoffs = offset; /* don't trust DOS */
+		checkoffs = offset;	/* don't trust DOS */
 #endif
-      break;
-    case SEEK_CUR:
-      doswhence = 1;
+		break;
+	case SEEK_CUR:
+		doswhence = 1;
 #ifdef DOS_CHECK
-      checkoffs = df->off + offset;
+		checkoffs = df->off + offset;
 #endif
-      break;
-    case SEEK_END:
-      doswhence = 2;
+		break;
+	case SEEK_END:
+		doswhence = 2;
 #ifdef DOS_CHECK
-      checkoffs = -1; /* we dont know len */
+		checkoffs = -1;	/* we dont know len */
 #endif
-      break;
-    default: errno=EOFFSET; return(-1);
-  }
-  res = dosseek(df->doshandle, offset, doswhence);
-  if(res == -1){
-    errno = EIO;
-    return(-1);
-  }
+		break;
+	default:
+		errno = EOFFSET;
+		return (-1);
+	}
+	res = dosseek(df->doshandle, offset, doswhence);
+	if (res == -1) {
+		errno = EIO;
+		return (-1);
+	}
 #ifdef DOS_CHECK
-  if((checkoffs !=- 1) && (res != checkoffs)){
-    printf("dosfile: unexpected seek result (%d+%d(%d)=%d)\n",
-	   df->off, offset, where, res);
-    errno = EOFFSET;
-    return(-1);
-  }
+	if ((checkoffs != -1) && (res != checkoffs)) {
+		printf("dosfile: unexpected seek result (%d+%d(%d)=%d)\n",
+		       df->off, offset, where, res);
+		errno = EOFFSET;
+		return (-1);
+	}
 #endif
-  df->off = res;
-  return(res);
+	df->off = res;
+	return (res);
 }
