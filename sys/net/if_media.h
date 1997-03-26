0b894d8ecbc4cbf1a6330da85301e4ebfe4c0ebb
thorpej <thorpej@NetBSD.org>
1997-03-26 01:19:27 +0000

    Back out the previous change (add IFM_10_EXT) after some dicussion w/ BSDI and Matt Thomas.

--- sys/net/if_media.h
+++ sys/net/if_media.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: if_media.h,v 1.2 1997/03/24 06:57:31 thorpej Exp $	*/
+/*	$NetBSD: if_media.h,v 1.3 1997/03/26 01:19:27 thorpej Exp $	*/
 
 /*
  * Copyright (c) 1997
@@ -113,8 +113,6 @@ int	ifmedia_ioctl __P((struct ifnet *ifp, struct ifreq *ifr,
  *	16-19	RFU
  *	20-27	Shared (global) options
  *	28-31	Instance
- *
- * NOTE: If you add a media subtype, always add it to the end!
  */
 
 /*
@@ -129,7 +127,6 @@ int	ifmedia_ioctl __P((struct ifnet *ifp, struct ifreq *ifr,
 #define	IFM_100_T4	8		/* 100BaseT4 - 4 pair cat 3 */
 #define	IFM_100_VG	9		/* 100VG-AnyLAN */
 #define	IFM_100_T2	10		/* 100BaseT2 */
-#define	IFM_10_EXT	11		/* misc. "external" port */
 
 /*
  * Token ring
@@ -220,7 +217,6 @@ struct ifmedia_description {
 	{ IFM_100_T4,	"100baseT4" },					\
 	{ IFM_100_VG,	"100baseVG" },					\
 	{ IFM_100_T2,	"100baseT2" },					\
-	{ IFM_10_EXT,	"external" },					\
 	{ 0, NULL },							\
 }
 
@@ -236,7 +232,6 @@ struct ifmedia_description {
 	{ IFM_100_T4,	"100T4" },					\
 	{ IFM_100_VG,	"100VG" },					\
 	{ IFM_100_T2,	"100T2" },					\
-	{ IFM_10_EXT,	"ext" },					\
 	{ 0, NULL },							\
 }
 
