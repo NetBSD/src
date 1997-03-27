9726850f2adc992721ffee88f11306d33a970689
thorpej <thorpej>
1997-03-27 21:01:52 +0000

    Don't allocate mclrefcnt[]; it's dead and gone.

--- sys/arch/vax/vax/machdep.c
+++ sys/arch/vax/vax/machdep.c
@@ -1,4 +1,4 @@
-/* $NetBSD: machdep.c,v 1.38 1997/03/26 22:43:12 gwr Exp $  */
+/* $NetBSD: machdep.c,v 1.39 1997/03/27 21:01:56 thorpej Exp $  */
 
 /*
  * Copyright (c) 1994 Ludd, University of Lule}, Sweden.
@@ -234,12 +234,8 @@ cpu_startup()
 				 16 * NCARGS, TRUE);
 
 	/*
-	 * Finally, allocate mbuf pool.	 Since mclrefcnt is an off-size we
-	 * use the more space efficient malloc in place of kmem_alloc.
+	 * Finally, allocate mbuf cluster submap.
 	 */
-	mclrefcnt = (char *) malloc(NMBCLUSTERS + CLBYTES / MCLBYTES,
-				    M_MBUF, M_NOWAIT);
-	bzero(mclrefcnt, NMBCLUSTERS + CLBYTES / MCLBYTES);
 	mb_map = kmem_suballoc(kernel_map, (vm_offset_t *) & mbutl, &maxaddr,
 			       VM_MBUF_SIZE, FALSE);
 
