2e24541a8aa60e66dc9fa7bc792818bb96d40a31
jonathan <jonathan@NetBSD.org>
1997-03-31 20:22:19 +0000

    PCnet-PnP front-end driver for isapnp PCnet cards.

---
+++ sys/dev/isapnp/if_levar.h
@@ -0,0 +1,35 @@
+/*	$NetBSD: if_levar.h,v 1.1 1997/03/31 20:22:19 jonathan Exp $	*/
+
+/*
+ * LANCE Ethernet driver header file
+ *
+ * Copyright (c) 1994, 1995 Charles M. Hannum.  All rights reserved.
+ *
+ * Copyright (C) 1993, Paul Richards. This software may be used, modified,
+ *   copied, distributed, and sold, in both source and binary form provided
+ *   that the above copyright and these terms are retained. Under no
+ *   circumstances is the author responsible for the proper functioning
+ *   of this software, nor does the author assume any responsibility
+ *   for damages incurred with its use.
+ */
+
+#define	PCNET_SAPROM	0x00
+#define	PCNET_RDP	0x10
+#define	PCNET_RAP	0x12
+
+
+/*
+ * Ethernet software status per interface.
+ *
+ * Each interface is referenced by a network interface structure,
+ * ethercom.ec_if, which the routing code uses to locate the interface.
+ * This structure contains the output queue for the interface, its address, ...
+ */
+struct le_softc {
+	struct	am7990_softc sc_am7990;	/* glue to MI code */
+
+	void	*sc_ih;
+	bus_space_tag_t sc_iot;		/* space cookie */
+	bus_space_handle_t sc_ioh;	/* bus space handle */
+	int	sc_rap, sc_rdp;		/* offsets to LANCE registers */
+};
