37b44cff94f30e80a1d23233773bc438a6de6fa6
cgd <cgd@NetBSD.org>
1997-04-06 22:31:45 +0000

    clean up NetBSD RCS ID strings, include machine/options.h

--- sys/arch/alpha/tc/sfb.c
+++ sys/arch/alpha/tc/sfb.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: sfb.c,v 1.8 1996/12/08 00:22:23 cgd Exp $	*/
+/* $NetBSD: sfb.c,v 1.9 1997/04/06 22:31:59 cgd Exp $ */
 
 /*
  * Copyright (c) 1995, 1996 Carnegie-Mellon University.
@@ -26,6 +26,8 @@
  * any improvements or extensions that they make and grant Carnegie the
  * rights to redistribute these changes.
  */
+
+#include <machine/options.h>		/* Pull in config options headers */
 
 #include <sys/param.h>
 #include <sys/systm.h>
