752880dc40d87da8f97858d882072cc85554de1d
cgd <cgd@NetBSD.org>
1997-04-07 00:18:51 +0000

    clean up NetBSD RCS ID strings

--- sys/arch/alpha/alpha/clockvar.h
+++ sys/arch/alpha/alpha/clockvar.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: clockvar.h,v 1.2 1996/04/17 22:01:21 cgd Exp $	*/
+/* $NetBSD: clockvar.h,v 1.3 1997/04/07 00:18:51 cgd Exp $ */
 
 /*
  * Copyright (c) 1994, 1995 Carnegie-Mellon University.
