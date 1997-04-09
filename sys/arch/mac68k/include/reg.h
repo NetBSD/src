3dc7ee9482188bb526d3dcba083c90bd205c88bc
thorpej <thorpej@NetBSD.org>
1997-04-09 20:12:44 +0000

    regdump() is prototyped in m68k/cpu.h now

--- sys/arch/mac68k/include/reg.h
+++ sys/arch/mac68k/include/reg.h
@@ -1,14 +1,9 @@
-/*	$NetBSD: reg.h,v 1.10 1996/05/05 06:18:00 briggs Exp $	*/
+/*	$NetBSD: reg.h,v 1.11 1997/04/09 20:12:44 thorpej Exp $	*/
 
 #ifndef _REG_MACHINE_
 #define	_REG_MACHINE_
 
 #include <machine/frame.h>
 #include <m68k/reg.h>
-
-__BEGIN_DECLS
-/* machdep.c */
-void	regdump __P((struct frame *, int));
-__END_DECLS
 
 #endif /* _REG_MACHINE_ */
