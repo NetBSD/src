129091a39b495284222d0a3072ef3907902ad3c8
thorpej <thorpej>
1997-04-13 02:37:15 +0000

    Use common m68k sigcode.

--- sys/arch/sun3x/sun3x/locore.s
+++ sys/arch/sun3x/sun3x/locore.s
@@ -1,4 +1,4 @@
-/*	$NetBSD: locore.s,v 1.15 1997/04/09 20:58:33 thorpej Exp $	*/
+/*	$NetBSD: locore.s,v 1.16 1997/04/13 02:37:27 thorpej Exp $	*/
 
 /*
  * Copyright (c) 1988 University of Utah.
@@ -720,35 +720,11 @@ Ldorte:
  */
 
 /*
- * Signal "trampoline" code (18 bytes).  Invoked from RTE setup by sendsig().
- *
- * Stack looks like:
- *
- *	sp+0 ->	signal number
- *	sp+4	signal specific code
- *	sp+8	pointer to signal context frame (scp)
- *	sp+12	address of handler
- *	sp+16	saved hardware state
- *			.
- *			.
- *	scp+0->	beginning of signal context frame
+ * Use common m68k sigcode.
  */
-	.globl	_sigcode, _esigcode
-	.data
-	.align	2
-_sigcode:	/* Found at address: 0x0DFFffdc */
-	movl	sp@(12),a0		| signal handler addr	(4 bytes)
-	jsr	a0@			| call signal handler	(2 bytes)
-	addql	#4,sp			| pop signo		(2 bytes)
-	trap	#1			| special syscall entry	(2 bytes)
-	movl	d0,sp@(4)		| save errno		(4 bytes)
-	moveq	#1,d0			| syscall == exit	(2 bytes)
-	trap	#0			| exit(errno)		(2 bytes)
-	.align	2
-_esigcode:
-	.text
+#include <m68k/m68k/sigcode.s>
 
-/* XXX - hp300 still has icode here... */
+	.text
 
 /*
  * Primitives
