2b94186fb00780eca89c47cc228785f9789f36c8
thorpej <thorpej@NetBSD.org>
1997-04-16 23:02:34 +0000

    Use new va-ppc.h

--- sys/arch/powerpc/include/varargs.h
+++ sys/arch/powerpc/include/varargs.h
@@ -1,61 +1,17 @@
-/*	$NetBSD: varargs.h,v 1.1 1996/09/30 16:34:37 ws Exp $	*/
+/*	$NetBSD: varargs.h,v 1.2 1997/04/16 23:02:35 thorpej Exp $	*/
 
-/*-
- * Copyright (c) 1990, 1993
- *	The Regents of the University of California.  All rights reserved.
- * (c) UNIX System Laboratories, Inc.
- * All or some portions of this file are derived from material licensed
- * to the University of California by American Telephone and Telegraph
- * Co. or Unix System Laboratories, Inc. and are reproduced herein with
- * the permission of UNIX System Laboratories, Inc.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed by the University of
- *	California, Berkeley and its contributors.
- * 4. Neither the name of the University nor the names of its contributors
- *    may be used to endorse or promote products derived from this software
- *    without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- *
- *	@(#)varargs.h	8.2 (Berkeley) 3/22/94
- */
-
-#ifndef _PPC_VARARGS_H_
+#ifndef	_PPC_VARARGS_H_
 #define	_PPC_VARARGS_H_
 
-#include <machine/stdarg.h>
+#include <machine/ansi.h>
 
-#if __GNUC__ == 1
-#define	__va_ellipsis
-#else
-#define	__va_ellipsis	...
+#ifndef	_VARARGS_H
+#define	_VARARGS_H
 #endif
+#include <machine/va-ppc.h>
 
-#define	va_alist	__builtin_va_alist
-#define	va_dcl		long __builtin_va_alist; __va_ellipsis
+typedef	__gnuc_va_list	va_list;
 
-#undef va_start
-#define	va_start(ap) \
-	((ap) = (va_list)&__builtin_va_alist)
+#undef	_BSD_VA_LIST
 
-#endif /* !_PPC_VARARGS_H_ */
+#endif /* ! _PPC_VARARGS_H_ */
