2747d6c8905313eb62da995bc593f14a653dc8a1
thorpej <thorpej@NetBSD.org>
1997-04-16 23:39:28 +0000

    - Update for no __BROKEN_INDIRECT_CONFIG. - Determine and remember if we are a floppy. - Workaround for what is apparently a firmware bug - ignore the sector   size returned by the device.  On my Firepower's floppy, block-size   is the same as max-transfer, which causes Lossage. - Don't read the disklabel on a floppy; do what the ISA floppy driver   does, which is assign the entire disk to each "partition", although   we do not deal with the density stuff. - FIREPOWERBUGS -> FIRMWORKSBUGS - Some general cleanup.

--- sys/dev/ofw/ofdisk.c
+++ sys/dev/ofw/ofdisk.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: ofdisk.c,v 1.3 1996/10/13 01:38:13 christos Exp $	*/
+/*	$NetBSD: ofdisk.c,v 1.4 1997/04/16 23:39:28 thorpej Exp $	*/
 
 /*
  * Copyright (C) 1995, 1996 Wolfgang Solfrank.
@@ -47,13 +47,17 @@ struct ofd_softc {
 	struct device sc_dev;
 	int sc_phandle;
 	int sc_unit;
+	int sc_flags;
 	struct disk sc_dk;
 	int sc_ihandle;
 	u_long max_transfer;
 	char sc_name[16];
 };
 
-static int ofdprobe __P((struct device *, void *, void *));
+/* sc_flags */
+#define OFDF_ISFLOPPY	0x01		/* we are a floppy drive */
+
+static int ofdprobe __P((struct device *, struct cfdata *, void *));
 static void ofdattach __P((struct device *, struct device *, void *));
 
 struct cfattach ofdisk_ca = {
@@ -71,13 +75,15 @@ struct dkdriver ofdkdriver = { ofdstrategy };
 static int
 ofdprobe(parent, match, aux)
 	struct device *parent;
-	void *match, *aux;
+	struct cfdata *match;
+	void *aux;
 {
 	struct ofprobe *ofp = aux;
 	char type[8];
 	int l;
 	
-	if ((l = OF_getprop(ofp->phandle, "device_type", type, sizeof type - 1)) < 0)
+	if ((l = OF_getprop(ofp->phandle, "device_type", type,
+	    sizeof type - 1)) < 0)
 		return 0;
 	if (l >= sizeof type)
 		return 0;
@@ -92,8 +98,17 @@ ofdattach(parent, self, aux)
 {
 	struct ofd_softc *of = (void *)self;
 	struct ofprobe *ofp = aux;
+	char child[64];
 	int l;
-	
+
+	if ((l = OF_getprop(ofp->phandle, "name", child,
+	    sizeof child - 1)) < 0)
+		panic("device without name?");
+	if (l >= sizeof child)
+		l = sizeof child - 1;
+	child[l] = 0;
+
+	of->sc_flags = 0;
 	of->sc_phandle = ofp->phandle;
 	of->sc_unit = ofp->unit;
 	of->sc_ihandle = 0;
@@ -103,6 +118,9 @@ ofdattach(parent, self, aux)
 	disk_attach(&of->sc_dk);
 	dk_establish(&of->sc_dk, self);				/* XXX */
 	printf("\n");
+
+	if (strcmp(child, "floppy") == 0)
+		of->sc_flags |= OFDF_ISFLOPPY;
 }
 
 int
@@ -116,6 +134,7 @@ ofdopen(dev, flags, fmt, p)
 	struct ofd_softc *of;
 	char path[256];
 	struct disklabel *lp;
+	char *errmes;
 	int l;
 	
 	if (unit >= ofdisk_cd.cd_ndevs)
@@ -124,19 +143,19 @@ ofdopen(dev, flags, fmt, p)
 		return ENXIO;
 
 	if (!of->sc_ihandle) {
-
-		if ((l = OF_package_to_path(of->sc_phandle, path, sizeof path - 3)) < 0)
+		if ((l = OF_package_to_path(of->sc_phandle, path,
+		    sizeof path - 3)) < 0)
 			return ENXIO;
 		if (l >= sizeof path - 3)
 			return ENXIO;
 		path[l] = 0;
 
 		/*
-		 * This is for the benefit of SCSI/IDE disks that don't
-		 * have all their childs in the device tree.
-		 * YES, I DO THINK THIS IS A BUG IN OPENFIRMWARE!!!
-		 * And yes, this is a very gross hack!					XXX
-		 * See also ofscsi.c
+		 * XXX This is for the benefit of SCSI/IDE disks that don't
+		 * XXX have all their childs in the device tree.
+		 * XXX YES, I DO THINK THIS IS A BUG IN OPENFIRMWARE!!!
+		 * XXX And yes, this is a very gross hack!
+		 * XXX See also ofscsi.c
 		 */
 		if (!strcmp(path + l - 4, "disk")) {
 			path[l++] = '@';
@@ -152,21 +171,26 @@ ofdopen(dev, flags, fmt, p)
 		/*
 		 * Try to get characteristics of the disk.
 		 */
-		of->max_transfer = OF_call_method_1("max-transfer", of->sc_ihandle, 0);
+		of->max_transfer = OF_call_method_1("max-transfer",
+		    of->sc_ihandle, 0);
 		if (of->max_transfer > MAXPHYS)
 			of->max_transfer = MAXPHYS;
-		
+
 		lp = of->sc_dk.dk_label;
 		bzero(lp, sizeof *lp);
-		
-		lp->d_secsize = OF_call_method_1("block-size", of->sc_ihandle, 0);
-		if (lp->d_secsize == (u_int32_t)-1 || lp->d_secsize > MAXBSIZE)
-			lp->d_secsize = DEV_BSIZE;
-		
-		lp->d_secperunit = OF_call_method_1("#blocks", of->sc_ihandle, 0);
+
+		/*
+		 * XXX Firmware bug?  Asking for block size gives a
+		 * XXX rediculous number!  So we use what the boot program
+		 * XXX uses.
+		 */
+		lp->d_secsize = DEV_BSIZE;
+
+		lp->d_secperunit = OF_call_method_1("#blocks",
+		    of->sc_ihandle, 0);
 		if (lp->d_secperunit == (u_int32_t)-1)
 			lp->d_secperunit = 0x7fffffff;
-		
+
 		lp->d_secpercyl = 1;
 		lp->d_nsectors = 1;
 		lp->d_ntracks = 1;
@@ -174,9 +198,31 @@ ofdopen(dev, flags, fmt, p)
 			
 		lp->d_partitions[RAW_PART].p_offset = 0;
 		lp->d_partitions[RAW_PART].p_size = lp->d_secperunit;
-		
-		readdisklabel(MAKEDISKDEV(major(dev), unit, RAW_PART), ofdstrategy,
-			      lp, of->sc_dk.dk_cpulabel);
+		lp->d_npartitions = RAW_PART + 1;
+
+		/*
+		 * Don't read the disklabel on a floppy; simply
+		 * assign all partitions the same size/offset as
+		 * RAW_PART.  (This is essentially what the ISA
+		 * floppy driver does, but we don't deal with
+		 * density stuff.)
+		 */
+		if (of->sc_flags & OFDF_ISFLOPPY) {
+			lp->d_npartitions = MAXPARTITIONS;
+			for (l = 0; l < lp->d_npartitions; l++) {
+				if (l == RAW_PART)
+					continue;
+				/* struct copy */
+				lp->d_partitions[l] =
+				    lp->d_partitions[RAW_PART];
+			}
+		} else {
+			errmes = readdisklabel(MAKEDISKDEV(major(dev),
+			    unit, RAW_PART), ofdstrategy, lp,
+			    of->sc_dk.dk_cpulabel);
+			if (errmes != NULL)
+				printf("%s: %s\n", errmes);
+		}
 	}
 
 	switch (fmt) {
@@ -187,7 +233,8 @@ ofdopen(dev, flags, fmt, p)
 		of->sc_dk.dk_bopenmask |= 1 << DISKPART(dev);
 		break;
 	}
-	of->sc_dk.dk_openmask = of->sc_dk.dk_copenmask | of->sc_dk.dk_bopenmask;
+	of->sc_dk.dk_openmask =
+	    of->sc_dk.dk_copenmask | of->sc_dk.dk_bopenmask;
 	
 	return 0;
 }
@@ -211,7 +258,7 @@ ofdclose(dev, flags, fmt, p)
 	}
 	of->sc_dk.dk_openmask = of->sc_dk.dk_copenmask | of->sc_dk.dk_bopenmask;
 	
-#ifdef	FIREPOWERBUGS
+#ifdef	FIRMWORKSBUGS
 	/*
 	 * This is a hack to get the firmware to flush its buffers.
 	 */
@@ -235,7 +282,7 @@ ofdstrategy(bp)
 	int read;
 	int (*OF_io)(int, void *, int);
 	daddr_t blkno = bp->b_blkno;
-	
+
 	bp->b_resid = 0;
 	if (bp->b_bcount == 0)
 		goto done;
@@ -250,7 +297,7 @@ ofdstrategy(bp)
 		p = &of->sc_dk.dk_label->d_partitions[DISKPART(bp->b_dev)];
 		blkno = bp->b_blkno + p->p_offset;
 	}
-	
+
 	disk_busy(&of->sc_dk);
 
 	off = (u_quad_t)blkno * DEV_BSIZE;
@@ -260,6 +307,7 @@ ofdstrategy(bp)
 			break;
 		read = OF_io(of->sc_ihandle, bp->b_data, bp->b_bcount);
 	} while (read == -2);
+
 	if (read < 0) {
 		bp->b_error = EIO;
 		bp->b_flags |= B_ERROR;
@@ -327,14 +375,12 @@ ofdioctl(dev, cmd, data, flag, p)
 			return EBADF;
 		
 		error = setdisklabel(of->sc_dk.dk_label,
-				     (struct disklabel *)data, /*of->sc_dk.dk_openmask */0,
-				     of->sc_dk.dk_cpulabel);
+		    (struct disklabel *)data, /*of->sc_dk.dk_openmask */0,
+		    of->sc_dk.dk_cpulabel);
 		if (error == 0 && cmd == DIOCWDINFO)
 			error = writedisklabel(MAKEDISKDEV(major(dev),
-							   DISKUNIT(dev), RAW_PART),
-					       ofdstrategy,
-					       of->sc_dk.dk_label,
-					       of->sc_dk.dk_cpulabel);
+			    DISKUNIT(dev), RAW_PART), ofdstrategy,
+			    of->sc_dk.dk_label, of->sc_dk.dk_cpulabel);
 
 		return error;
 	default:
