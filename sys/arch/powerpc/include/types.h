bb7d0516bb56db1a7052ebe3dc6185d6269a6df3
thorpej <thorpej@NetBSD.org>
1997-04-16 23:03:16 +0000

    Nuke __BROKEN_INDIRECT_CONFIG

--- sys/arch/powerpc/include/types.h
+++ sys/arch/powerpc/include/types.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: types.h,v 1.2 1996/12/05 00:13:57 cgd Exp $	*/
+/*	$NetBSD: types.h,v 1.3 1997/04/16 23:03:16 thorpej Exp $	*/
 
 /*-
  * Copyright (C) 1995 Wolfgang Solfrank.
@@ -53,7 +53,5 @@ typedef	unsigned long		vm_offset_t;
 
 /* This is only to make some unneeded function declaration happy */
 #define	label_t	void
-
-#define	__BROKEN_INDIRECT_CONFIG
 
 #endif	/* _MACHTYPES_H_ */
