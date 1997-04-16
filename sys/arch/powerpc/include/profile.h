bcdc1208f8357fa824b60468e57f9e5a1ad92f1d
thorpej <thorpej>
1997-04-16 23:00:53 +0000

    Yow, "what happened"?  Anyhow, correct apparerent CVS botch, and fix RCS ID.

--- sys/arch/powerpc/include/profile.h
+++ sys/arch/powerpc/include/profile.h
@@ -1,7 +1,12 @@
-#define	MCOUNT
-/*	$OG: profile.h,v 1.2 1997/02/08 03:55:10 thorpej Exp $	*/
+/*	$NetBSD: profile.h,v 1.3 1997/04/16 23:00:53 thorpej Exp $	*/
 
 #define	_MCOUNT_DECL	void mcount
 
 /* XXX implement XXX */
 
+#define	MCOUNT
+
+#ifdef _KERNEL
+#define	MCOUNT_ENTER
+#define	MCONT_EXIT
+#endif
