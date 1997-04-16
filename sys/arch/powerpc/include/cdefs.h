665fc2e1a9b9c978e33fdd605752e403cd2d6b53
thorpej <thorpej>
1997-04-16 22:52:50 +0000

    Update for Elf.

--- sys/arch/powerpc/include/cdefs.h
+++ sys/arch/powerpc/include/cdefs.h
@@ -1,35 +1,36 @@
-/*	$NetBSD: cdefs.h,v 1.1 1996/09/30 16:34:21 ws Exp $	*/
-
-/*
- * Written by J.T. Conklin <jtc@wimsey.com> 01/17/95.
- * Public domain.
- */
+/*	$NetBSD: cdefs.h,v 1.2 1997/04/16 22:53:27 thorpej Exp $	*/
 
 #ifndef	_MACHINE_CDEFS_H_
 #define	_MACHINE_CDEFS_H_
 
-#ifdef __STDC__
-#define _C_LABEL(x)	_STRING(_ ## x)
-#else
-#define _C_LABEL(x)	_STRING(_/**/x)
+#define _C_LABEL(x)	_STRING(x)
+
+#define	__DO_NOT_DO_WEAK__		/* NO WEAK SYMS IN LIBC YET */
+
+#ifndef __DO_NOT_DO_WEAK__
+#define	__indr_reference(sym, alias)	/* use weak symbols instead */
 #endif
 
-#ifdef __GNUC__
-#ifdef __STDC__
-#define __indr_reference(sym,alias)	\
-	__asm__(".stabs \"_" #alias "\",11,0,0,0");	\
-	__asm__(".stabs \"_" #sym "\",1,0,0,0")
-#define __warn_references(sym,msg)	\
-	__asm__(".stabs \"" msg "\",30,0,0,0");		\
-	__asm__(".stabs \"_" #sym "\",1,0,0,0")
-#else
-#define __indr_reference(sym,alias)	\
-	__asm__(".stabs \"_/**/alias\",11,0,0,0");	\
-	__asm__(".stabs \"_/**/sym\",1,0,0,0")
-#define __warn_references(sym,msg)	\
-	__asm__(".stabs msg,30,0,0,0");			\
-	__asm__(".stabs \"_/**/sym\",1,0,0,0")
+#ifdef	__STDC__
+
+#ifndef	__DO_NOT_DO_WEAK__
+#define	 __weak_alias(alias, sym)			\
+	__asm__(".weak " #alias " ; " #alias " = " #sym)
 #endif
+
+#define	__warn_references(sym, msg)			\
+	__asm__(".section .gnu.warning." #sym " ; .ascii \"" msg "\" ; .text")
+
+#else /* ! __STDC__ */
+
+#ifndef	__DO_NOT_DO_WEAK__
+#define	__weak_alias(alias, sym)			\
+	__asm__(".weak alias ; alias = sym")
 #endif
+
+#define	__warn_references(sym, msg)			\
+	__asm__(".section .gnu.warning.sym ; .ascii msg ; .text")
+
+#endif /* __STDC__ */
 
 #endif /* !_MACHINE_CDEFS_H_ */
