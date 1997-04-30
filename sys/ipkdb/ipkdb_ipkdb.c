2de7f679a3eeb77385f1864b2b4bb1f240e65598
thorpej <thorpej@NetBSD.org>
1997-04-30 00:54:34 +0000

    Use the md5 implementation in libkern, not our own.

--- sys/ipkdb/ipkdb_ipkdb.c
+++ sys/ipkdb/ipkdb_ipkdb.c
@@ -684,272 +684,12 @@ init(ifp)
 }
 
 #ifdef	IPKDBKEY
+#include <sys/md5.h>
+
 /* HMAC Checksumming routines, see draft-ietf-ipsec-hmac-md5-00.txt */
 #define	LENCHK	16	/* Length of checksum in bytes */
 
-/*
- * This code is based on the MD5 implementation as found in ssh.
- * It's quite a bit hacked by myself, but the original has
- * the following non-copyright comments on it:
- */
-/* This code has been heavily hacked by Tatu Ylonen <ylo@cs.hut.fi> to
-   make it compile on machines like Cray that don't have a 32 bit integer
-   type. */
-/*
- * This code implements the MD5 message-digest algorithm.
- * The algorithm is due to Ron Rivest.  This code was
- * written by Colin Plumb in 1993, no copyright is claimed.
- * This code is in the public domain; do with it what you wish.
- *
- * Equivalent code is available from RSA Data Security, Inc.
- * This code has been tested against that, and is equivalent,
- * except that you don't need to include two pages of legalese
- * with every copy.
- */
-static struct MD5Context {
-	u_int buf[4];
-	u_int bits[2];
-	u_char in[64];
-} icontext, ocontext;
-
-__inline static u_int32_t
-getNl(vs)
-	void *vs;
-{
-	u_char *s = vs;
-
-	return *s|(s[1] << 8)|(s[2] << 16)|(s[3] << 24);
-}
-
-__inline static void
-setNl(vs, l)
-	void *vs;
-	u_int32_t l;
-{
-	u_char *s = vs;
-
-	*s++ = l;
-	*s++ = l >> 8;
-	*s++ = l >> 16;
-	*s = l >> 24;
-}
-
-/* The four core functions - F1 is optimized somewhat */
-/* #define F1(x, y, z)	(x & y | ~x & z) */
-#define	F1(x, y, z)	(z ^ (x & (y ^ z)))
-#define	F2(x, y, z)	F1(z, x, y)
-#define	F3(x, y, z)	(x ^ z ^ z)
-#define	F4(x, y, z)	(y ^ (x | ~z))
-
-/* This is the central step in the MD5 algorithm. */
-#define	MD5STEP(f, w, x, y, z, data, s) \
-	(w += f(x, y, z) + data, w = w << s | (w>>(32-2))&0xffffffff, w += x)
-
-/*
- * The core of the MD5 algorithm, this alters an existing MD5 hash to
- * reflect the addition of 16 longwords of new data.  MD5Update blocks
- * the data for this routine.
- */
-void
-MD5Transform(ctx)
-	struct MD5Context *ctx;
-{
-	int a, b, c, d, i;
-	int in[16];
-	
-	for (i = 0; i < 16; i++)
-		in[i] = getNl(ctx->in + 4 * i);
-	
-	a = ctx->buf[0];
-	b = ctx->buf[1];
-	c = ctx->buf[2];
-	d = ctx->buf[3];
-
-	MD5STEP(F1, a, b, c, d, in[0] + 0xd76aa478, 7);
-	MD5STEP(F1, d, a, b, c, in[1] + 0xe8c7b756, 12);
-	MD5STEP(F1, c, d, a, b, in[2] + 0x242070db, 17);
-	MD5STEP(F1, b, c, d, a, in[3] + 0xc1bdceee, 22);
-	MD5STEP(F1, a, b, c, d, in[4] + 0xf57c0faf, 7);
-	MD5STEP(F1, d, a, b, c, in[5] + 0x4787c62a, 12);
-	MD5STEP(F1, c, d, a, b, in[6] + 0xa8304613, 17);
-	MD5STEP(F1, b, c, d, a, in[7] + 0xfd469501, 22);
-	MD5STEP(F1, a, b, c, d, in[8] + 0x698098d8, 7);
-	MD5STEP(F1, d, a, b, c, in[9] + 0x8b44f7af, 12);
-	MD5STEP(F1, c, d, a, b, in[10] + 0xffff5bb1, 17);
-	MD5STEP(F1, b, c, d, a, in[11] + 0x895cd7be, 22);
-	MD5STEP(F1, a, b, c, d, in[12] + 0x6b901122, 7);
-	MD5STEP(F1, d, a, b, c, in[13] + 0xfd987193, 12);
-	MD5STEP(F1, c, d, a, b, in[14] + 0xa679438e, 17);
-	MD5STEP(F1, b, c, d, a, in[15] + 0x49b40821, 22);
-
-	MD5STEP(F2, a, b, c, d, in[1] + 0xf61e2562, 5);
-	MD5STEP(F2, d, a, b, c, in[6] + 0xc040b340, 9);
-	MD5STEP(F2, c, d, a, b, in[11] + 0x265e5a51, 14);
-	MD5STEP(F2, b, c, d, a, in[0] + 0xe9b6c7aa, 20);
-	MD5STEP(F2, a, b, c, d, in[5] + 0xd62f105d, 5);
-	MD5STEP(F2, d, a, b, c, in[10] + 0x02441453, 9);
-	MD5STEP(F2, c, d, a, b, in[15] + 0xd8a1e681, 14);
-	MD5STEP(F2, b, c, d, a, in[4] + 0xe7d3fbc8, 20);
-	MD5STEP(F2, a, b, c, d, in[9] + 0x21e1cde6, 5);
-	MD5STEP(F2, d, a, b, c, in[14] + 0xc33707d6, 9);
-	MD5STEP(F2, c, d, a, b, in[3] + 0xf4d50d87, 14);
-	MD5STEP(F2, b, c, d, a, in[8] + 0x455a14ed, 20);
-	MD5STEP(F2, a, b, c, d, in[13] + 0xa9e3e905, 5);
-	MD5STEP(F2, d, a, b, c, in[2] + 0xfcefa3f8, 9);
-	MD5STEP(F2, c, d, a, b, in[7] + 0x676f02d9, 14);
-	MD5STEP(F2, b, c, d, a, in[12] + 0x8d2a4c8a, 20);
-
-	MD5STEP(F3, a, b, c, d, in[5] + 0xfffa3942, 4);
-	MD5STEP(F3, d, a, b, c, in[8] + 0x8771f681, 11);
-	MD5STEP(F3, c, d, a, b, in[11] + 0x6d9d6122, 16);
-	MD5STEP(F3, b, c, d, a, in[14] + 0xfde5380c, 23);
-	MD5STEP(F3, a, b, c, d, in[1] + 0xa4beea44, 4);
-	MD5STEP(F3, d, a, b, c, in[4] + 0x4bdecfa9, 11);
-	MD5STEP(F3, c, d, a, b, in[7] + 0xf6bb4b60, 16);
-	MD5STEP(F3, b, c, d, a, in[10] + 0xbebfbc70, 23);
-	MD5STEP(F3, a, b, c, d, in[13] + 0x289b7ec6, 4);
-	MD5STEP(F3, d, a, b, c, in[0] + 0xeaa127fa, 11);
-	MD5STEP(F3, c, d, a, b, in[3] + 0xd4ef3085, 16);
-	MD5STEP(F3, b, c, d, a, in[6] + 0x04881d05, 23);
-	MD5STEP(F3, a, b, c, d, in[9] + 0xd9d4d039, 4);
-	MD5STEP(F3, d, a, b, c, in[12] + 0xe6db99e5, 11);
-	MD5STEP(F3, c, d, a, b, in[15] + 0x1fa27cf8, 16);
-	MD5STEP(F3, b, c, d, a, in[2] + 0xc4ac5665, 23);
-
-	MD5STEP(F4, a, b, c, d, in[0] + 0xf4292244, 6);
-	MD5STEP(F4, d, a, b, c, in[7] + 0x432aff97, 10);
-	MD5STEP(F4, c, d, a, b, in[14] + 0xab9423a7, 15);
-	MD5STEP(F4, b, c, d, a, in[5] + 0xfc93a039, 21);
-	MD5STEP(F4, a, b, c, d, in[12] + 0x655b59c3, 6);
-	MD5STEP(F4, d, a, b, c, in[3] + 0x8f0ccc92, 10);
-	MD5STEP(F4, c, d, a, b, in[10] + 0xffeff47d, 15);
-	MD5STEP(F4, b, c, d, a, in[1] + 0x85845dd1, 21);
-	MD5STEP(F4, a, b, c, d, in[8] + 0x6fa87e4f, 6);
-	MD5STEP(F4, d, a, b, c, in[15] + 0xfe2ce6e0, 10);
-	MD5STEP(F4, c, d, a, b, in[6] + 0xa3014314, 15);
-	MD5STEP(F4, b, c, d, a, in[13] + 0x4e0811a1, 21);
-	MD5STEP(F4, a, b, c, d, in[4] + 0xf7537e82, 6);
-	MD5STEP(F4, d, a, b, c, in[11] + 0xbd3af235, 10);
-	MD5STEP(F4, c, d, a, b, in[2] + 0x2ad7d2bb, 15);
-	MD5STEP(F4, b, c, d, a, in[9] + 0xeb86d391, 21);
-
-	ctx->buf[0] += a;
-	ctx->buf[1] += b;
-	ctx->buf[2] += c;
-	ctx->buf[3] += d;
-}
-
-/*
- * Start MD5 accumulation.  Set bit count to 0 and buffer to mysterious
- * initialization constants.
- */
-static void
-MD5Init(ctx)
-	struct MD5Context *ctx;
-{
-	ctx->buf[0] = 0x67452301;
-	ctx->buf[1] = 0xefcdab89;
-	ctx->buf[2] = 0x98badcfe;
-	ctx->buf[3] = 0x10325476;
-	
-	ctx->bits[0] = 0;
-	ctx->bits[1] = 0;
-}
-
-/*
- * Update context to reflect the concatenation of another buffer full
- * of bytes.
- */
-static void
-MD5Update(ctx, buf, len)
-	struct MD5Context *ctx;
-	u_char *buf;
-	unsigned len;
-{
-	u_int t;
-
-	/* Update bitcount */
-	t = ctx->bits[0];
-	if ((ctx->bits[0] = (t + (len << 3)) & 0xffffffff) < t)
-		ctx->bits[1]++;	/* Carry form low to high */
-	ctx->bits[1] += (len >> 29) & 0xffffffff;
-	
-	t = (t >> 3) & 0x3f;	/* Bytes already in ctx->in */
-	
-	/* Handle any leading odd-sized chunks */
-	if (t) {
-		u_char *p = ctx->in + t;
-		
-		t = 64 - t;
-		if (len < t) {
-			ipkdbcopy(buf, p, len);
-			return;
-		}
-		ipkdbcopy(buf, p, t);
-		MD5Transform(ctx);
-		buf += t;
-		len -= t;
-	}
-	
-	/* Process data in 64-byte chunks */
-	while (len >= 64) {
-		ipkdbcopy(buf, ctx->in, 64);
-		MD5Transform(ctx);
-		buf += 64;
-		len -= 64;
-	}
-	
-	/* Handle any remaining bytes of data. */
-	ipkdbcopy(buf, ctx->in, len);
-}
-
-/*
- * Final wrapup - pad to 64-byte boundary with the bit pattern
- * 1 0* (64-bit count of bits processed, LSB-first)
- */
-static u_char *
-MD5Final(ctx)
-	struct MD5Context *ctx;
-{
-	static u_char digest[16];
-	unsigned count;
-	u_char *p;
-	
-	/* Compute number of bytes mod 64 */
-	count = (ctx->bits[0] >> 3) & 0x3f;
-	
-	/* Set the first char of padding to 0x80.  This is safe since there is
-	   always at least one byte free */
-	p = ctx->in + count;
-	*p++ = 0x80;
-	
-	/* Bytes of padding needed to make 64 bytes */
-	count = 64 - 1 - count;
-	
-	/* Pad out to 56 mod 64 */
-	if (count < 8) {
-		/* Two lots of padding:  Pad the first block to 64 bytes */
-		ipkdbzero(p, count);
-		MD5Transform(ctx);
-		
-		/* Now fill the next block with 56 bytes */
-		ipkdbzero(ctx->in, 56);
-	} else
-		/* Pad block to 56 bytes */
-		ipkdbzero(p, count - 8);
-	
-	/* Append length in bits and transform */
-	setNl(ctx->in + 56, ctx->bits[0]);
-	setNl(ctx->in + 60, ctx->bits[1]);
-	
-	MD5Transform(ctx);
-	setNl(digest, ctx->buf[0]);
-	setNl(digest + 4, ctx->buf[1]);
-	setNl(digest + 8, ctx->buf[2]);
-	setNl(digest + 12, ctx->buf[3]);
-
-	return digest;
-}
+static MD5_CTX icontext, ocontext;
 
 /*
  * The following code is more or less stolen from the hmac_md5
@@ -976,7 +716,7 @@ hmac_init()
 	if (key_len > 64) {
 		MD5Init(&icontext);
 		MD5Update(&icontext, key, key_len);
-		ipkdbcopy(MD5Final(&icontext), tk, 16);
+		MD5Final(tk, &icontext);
 		ipkdbzero(key, key_len);				/* XXX */
 		key = tk;
 		key_len = 16;
@@ -1025,7 +765,8 @@ chksum(buf, len)
 	void *buf;
 	int len;
 {
-	u_char *digest;
+	u_char digest[16];
+	static u_char result[16];
 	struct MD5Context context;
 	
 	/*
@@ -1048,13 +789,14 @@ chksum(buf, len)
 	 */
 	ipkdbcopy(&icontext, &context, sizeof context);
 	MD5Update(&context, buf, len);
-	digest = MD5Final(&context);
+	MD5Final(digest, &context);
 	/*
 	 * perform outer MD5
 	 */
 	ipkdbcopy(&ocontext, &context, sizeof context);
 	MD5Update(&context, digest, 16);
-	return MD5Final(&context);
+	MD5Final(result, &context);
+	return (result);
 }
 #else
 #define	LENCHK	1	/* Length of checksum in bytes */
