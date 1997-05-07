42489cfa03c8532c8d5c8754528def05515e8240
mycroft <mycroft@NetBSD.org>
1997-05-07 08:42:18 +0000

    Shuffle some .PHONYs around, to work better with `-t'.

--- share/mk/bsd.own.mk
+++ share/mk/bsd.own.mk
@@ -1,4 +1,4 @@
-#	$NetBSD: bsd.own.mk,v 1.34 1997/05/06 20:54:39 mycroft Exp $
+#	$NetBSD: bsd.own.mk,v 1.35 1997/05/07 08:42:20 mycroft Exp $
 
 .if defined(MAKECONF) && exists(${MAKECONF})
 .include "${MAKECONF}"
@@ -81,4 +81,5 @@ NOPROFILE=
 .endif
 
 TARGETS+=	all clean cleandir depend includes install lint obj realinstall tags
-.PHONY:		all clean cleandir depend includes install lint obj realinstall tags
+.PHONY:		all clean cleandir depend includes install lint obj realinstall tags \
+		beforedepend afterdepend beforeinstall afterinstall
