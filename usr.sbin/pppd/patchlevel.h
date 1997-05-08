4f8dc248059fc188f12783bda8c7f89b88a3936f
christos <christos>
1997-05-08 18:07:28 +0000

    Move pppd in its own directory.

--- usr.sbin/pppd/patchlevel.h
+++
@@ -1,8 +0,0 @@
-/*	$NetBSD: patchlevel.h,v 1.12 1997/03/12 20:18:10 christos Exp $	*/
-
-/* Id: patchlevel.h,v 1.26 1997/03/04 03:42:04 paulus Exp  */
-#define	PATCHLEVEL	0
-
-#define VERSION		"2.3"
-#define IMPLEMENTATION	""
-#define DATE		"4 Mar 97"
