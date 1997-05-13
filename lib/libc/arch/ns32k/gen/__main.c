2d2025b147347db3d3966b6e989edc615f68b030
thorpej <thorpej@NetBSD.org>
1997-05-13 02:18:15 +0000

    Add a list of files changed on the thorpej-bus-dma branch.  PLEASE KEEP THIS LIST UP TO DATE!

--- lib/libc/arch/ns32k/gen/__main.c
+++
@@ -1,7 +0,0 @@
-/*	$NetBSD: __main.c,v 1.2 1997/05/08 13:38:30 matthias Exp $	*/
-
-#if defined(LIBC_SCCS) && !defined(lint)
-static char rcsid[] = "$NetBSD: __main.c,v 1.2 1997/05/08 13:38:30 matthias Exp $";
-#endif /* LIBC_SCCS and not lint */
-
-__main(){}
--- sys/arch/pmax/include/signal.h
+++
@@ -1,3 +0,0 @@
-/*	$NetBSD: signal.h,v 1.8 1996/03/19 04:39:07 jonathan Exp $	*/
-
-#include <mips/signal.h>
