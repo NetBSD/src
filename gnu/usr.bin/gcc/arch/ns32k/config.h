c63d9b34a118de05cf6869d41d51c8dee1cbaeb3
phil <phil@NetBSD.org>
1997-05-14 19:26:32 +0000

    OK, OK!  It is now 2.7.2.2.

--- gnu/usr.bin/gcc/arch/ns32k/config.h
+++ gnu/usr.bin/gcc/arch/ns32k/config.h
@@ -1,4 +1,4 @@
 #define NETBSD_NATIVE
-#define DEFAULT_TARGET_VERSION "2.7.2.1"
+#define DEFAULT_TARGET_VERSION "2.7.2.2"
 #define DEFAULT_TARGET_MACHINE "NetBSD/ns32k"
 #include "ns32k/xm-netbsd.h"
