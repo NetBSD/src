0a3a7dc61bc68c17ae8883555a092b2ccdfc5d52
christos <christos@NetBSD.org>
1997-05-17 22:14:13 +0000

    Resolve conflicts

--- usr.sbin/pppd/pppd/patchlevel.h
+++ usr.sbin/pppd/pppd/patchlevel.h
@@ -1,8 +1,8 @@
-/*	$NetBSD: patchlevel.h,v 1.12 1997/03/12 20:18:10 christos Exp $	*/
+/*	$NetBSD: patchlevel.h,v 1.13 1997/05/17 22:14:25 christos Exp $	*/
 
-/* Id: patchlevel.h,v 1.26 1997/03/04 03:42:04 paulus Exp  */
+/* Id: patchlevel.h,v 1.27 1997/04/30 05:56:02 paulus Exp  */
 #define	PATCHLEVEL	0
 
 #define VERSION		"2.3"
-#define IMPLEMENTATION	""
-#define DATE		"4 Mar 97"
+#define IMPLEMENTATION	"b5"
+#define DATE		"30 Apr 97"
