3ed9c7d7298217dc7a561e8fbdb278959abe2838
lukem <lukem>
1997-05-21 06:55:25 +0000

    Don't return allocated memory in an error condition in a char **outXXX argument (set them to the null pointer instead). Thus, code that doesn't specificallly attempt to clean up allocated memory after an error result is returned from yp_...() won't have an unexpected memory leak (i.e, most 3rd party code)

--- lib/libc/yp/yp_first.c
+++ lib/libc/yp/yp_first.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: yp_first.c,v 1.4 1997/05/20 15:25:38 lukem Exp $	 */
+/*	$NetBSD: yp_first.c,v 1.5 1997/05/21 06:55:25 lukem Exp $	 */
 
 /*
  * Copyright (c) 1992, 1993 Theo de Raadt <deraadt@fsa.ca>
@@ -32,7 +32,7 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char rcsid[] = "$NetBSD: yp_first.c,v 1.4 1997/05/20 15:25:38 lukem Exp $";
+static char rcsid[] = "$NetBSD: yp_first.c,v 1.5 1997/05/21 06:55:25 lukem Exp $";
 #endif
 
 #include <stdlib.h>
@@ -106,6 +106,16 @@ again:
 	}
 	xdr_free(xdr_ypresp_key_val, (char *) &yprkv);
 	_yp_unbind(ysd);
+	if (r != 0) {
+		if (*outkey) {
+			free(*outkey);
+			*outkey = NULL;
+		}
+		if (*outval) {
+			free(*outval);
+			*outval = NULL;
+		}
+	}
 	return r;
 }
 
@@ -176,5 +186,15 @@ again:
 	}
 	xdr_free(xdr_ypresp_key_val, (char *) &yprkv);
 	_yp_unbind(ysd);
+	if (r != 0) {
+		if (*outkey) {
+			free(*outkey);
+			*outkey = NULL;
+		}
+		if (*outval) {
+			free(*outval);
+			*outval = NULL;
+		}
+	}
 	return r;
 }
