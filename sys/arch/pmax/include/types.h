a780b0132aa859854658bf19b7e475c04085fe12
lukem <lukem@NetBSD.org>
1997-05-23 19:58:25 +0000

    Add necessary files for nsswitch, take 2 Remove support for "lookup" methods in resolv.conf Remove prototypes for _ng_sl_*; these were replaced a long time ago

--- sys/arch/pmax/include/types.h
+++
@@ -1,5 +0,0 @@
-/*	$NetBSD: types.h,v 1.13 1996/12/05 00:13:56 cgd Exp $	*/
-
-#include <mips/types.h>
-
-#define	__BROKEN_INDIRECT_CONFIG
--- sys/arch/sun3x/stand/libsa/iommu.h
+++
@@ -1,2 +0,0 @@
-void iommu_init();
-void set_iommupde(u_int32_t va, u_int32_t pa);
