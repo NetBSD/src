70bb963e004f45b12a9a95b19b745771ab26494c
jonathan <jonathan@NetBSD.org>
1997-05-25 05:04:03 +0000

    Prototype for pmap_bootstrap().

--- sys/arch/pmax/include/pmap.h
+++ sys/arch/pmax/include/pmap.h
@@ -1,6 +1,12 @@
-/*	$NetBSD: pmap.h,v 1.10 1996/03/19 04:39:05 jonathan Exp $	*/
+/*	$NetBSD: pmap.h,v 1.11 1997/05/25 05:04:03 jonathan Exp $	*/
 
 #include <mips/pmap.h>
 
 #define pmax_trunc_seg(a) mips_trunc_seg(a)
 #define pmax_round_seg(a) mips_round_seg(a)
+
+/*
+ *	Bootstrap the system enough to run with virtual memory.
+ *	firstaddr is the first unused kseg0 address (not page aligned).
+ */
+void	pmap_bootstrap __P((vm_offset_t firstaddr));
