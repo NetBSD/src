5e2bcd37f770cfc3447dddb4fb56f2e4bbd1ff3d
darrenr <darrenr@NetBSD.org>
1997-05-25 12:40:11 +0000

    fix conflicts

--- sys/netinet/ip_state.h
+++ sys/netinet/ip_state.h
@@ -1,20 +1,26 @@
-/*	$NetBSD: ip_state.h,v 1.3 1997/03/29 00:55:05 thorpej Exp $	*/
+/*	$NetBSD: ip_state.h,v 1.4 1997/05/25 12:40:20 darrenr Exp $	*/
 
 /*
- * (C)opyright 1995 by Darren Reed.
+ * (C)opyright 1995-1997 by Darren Reed.
  *
  * Redistribution and use in source and binary forms are permitted
  * provided that this notice is preserved and due credit is given
  * to the original author and the contributors.
  *
  * @(#)ip_state.h	1.3 1/12/96 (C) 1995 Darren Reed
- * $Id: ip_state.h,v 1.3 1997/03/29 00:55:05 thorpej Exp $
+ * $Id: ip_state.h,v 1.4 1997/05/25 12:40:20 darrenr Exp $
  */
 #ifndef	__IP_STATE_H__
 #define	__IP_STATE_H__
 
 #define	IPSTATE_SIZE	257
 #define	IPSTATE_MAX	2048	/* Maximum number of states held */
+
+#define	PAIRS(s1,d1,s2,d2)	((((s1) == (s2)) && ((d1) == (d2))) ||\
+				 (((s1) == (d2)) && ((d1) == (s2))))
+#define	IPPAIR(s1,d1,s2,d2)	PAIRS((s1).s_addr, (d1).s_addr, \
+				      (s2).s_addr, (d2).s_addr)
+
 
 typedef struct udpstate {
 	u_short	us_sport;
@@ -41,8 +47,8 @@ typedef struct ipstate {
 	struct	ipstate	*is_next;
 	u_long	is_age;
 	u_int	is_pass;
-	u_int	is_pkts;
-	u_int	is_bytes;
+	U_QUAD_T	is_pkts;
+	U_QUAD_T	is_bytes;
 	struct	in_addr	is_src;
 	struct	in_addr	is_dst;
 	u_char	is_p;
@@ -67,6 +73,31 @@ typedef struct ipstate {
 
 #define	TH_OPENING	(TH_SYN|TH_ACK)
 
+
+typedef	struct	ipslog	{
+	struct	timeval	isl_tv;
+	U_QUAD_T	isl_pkts;
+	U_QUAD_T	isl_bytes;
+	struct	in_addr	isl_src;
+	struct	in_addr	isl_dst;
+	u_char	isl_p;
+	u_char	isl_flags;
+	u_short	isl_type;
+	union {
+		u_short	isl_filler[2];
+		u_short	isl_ports[2];
+		u_short	isl_icmp;
+	} isl_ps;
+} ipslog_t;
+
+#define	isl_sport	isl_ps.isl_ports[0]
+#define	isl_dport	isl_ps.isl_ports[1]
+#define	isl_itype	isl_ps.isl_icmp
+
+#define	ISL_NEW		0
+#define	ISL_EXPIRE	0xffff
+
+
 typedef	struct	ips_stat {
 	u_long	iss_hits;
 	u_long	iss_miss;
@@ -78,9 +109,19 @@ typedef	struct	ips_stat {
 	u_long	iss_expire;
 	u_long	iss_fin;
 	u_long	iss_active;
+	u_long	iss_logged;
+	u_long	iss_logfail;
 	ipstate_t **iss_table;
 } ips_stat_t;
 
+
+extern u_long fr_tcpidletimeout;
+extern u_long fr_tcpclosewait;
+extern u_long fr_tcplastack;
+extern u_long fr_tcptimeout;
+extern u_long fr_tcpclosed;
+extern u_long fr_udptimeout;
+extern u_long fr_icmptimeout;
 extern int fr_tcpstate __P((ipstate_t *, fr_info_t *, ip_t *,
 			    tcphdr_t *, u_short));
 extern ips_stat_t *fr_statetstats __P((void));
@@ -89,4 +130,6 @@ extern int fr_checkstate __P((ip_t *, fr_info_t *));
 extern void fr_timeoutstate __P((void));
 extern void fr_tcp_age __P((u_long *, u_char *, ip_t *, fr_info_t *, int));
 extern void fr_stateunload __P((void));
+extern void ipstate_log __P((struct ipstate *, u_short));
+extern int fr_state_ioctl __P((caddr_t, int, int));
 #endif /* __IP_STATE_H__ */
