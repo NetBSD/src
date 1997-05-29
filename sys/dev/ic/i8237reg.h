b245f6857cf0f6ce0f3dfa91d303c7a7f4d6e232
mycroft <mycroft>
1997-05-29 21:42:28 +0000

    Add DMA37MD_DEMAND and DMA37MD_BLOCK.

--- sys/dev/ic/i8237reg.h
+++ sys/dev/ic/i8237reg.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: i8237reg.h,v 1.5 1996/03/01 22:27:09 mycroft Exp $	*/
+/*	$NetBSD: i8237reg.h,v 1.6 1997/05/29 21:42:28 mycroft Exp $	*/
 
 /*
  * Intel 8237 DMA Controller
@@ -7,7 +7,9 @@
 #define	DMA37MD_WRITE	0x04	/* read the device, write memory operation */
 #define	DMA37MD_READ	0x08	/* write the device, read memory operation */
 #define	DMA37MD_LOOP	0x10	/* auto-initialize mode */
-#define	DMA37MD_SINGLE	0x40	/* single pass mode */
+#define	DMA37MD_DEMAND	0x00	/* demand mode */
+#define	DMA37MD_SINGLE	0x40	/* single mode */
+#define	DMA37MD_BLOCK	0x80	/* block mode */
 #define	DMA37MD_CASCADE	0xc0	/* cascade mode */
 	
 #define	DMA37SM_CLEAR	0x00	/* clear mask bit */
