f283789d6416d19dd8316e0170c4d5a14a8a5766
cjs <cjs@NetBSD.org>
1997-05-31 22:30:12 +0000

    List of files in the cjs-build-against-obj-dir branch.

--- sys/arch/sun3x/include/cdefs.h
+++
@@ -1,3 +0,0 @@
-/*	$NetBSD: cdefs.h,v 1.1.1.1 1997/01/14 20:57:04 gwr Exp $	*/
-
-#include <m68k/cdefs.h>
--- sys/arch/sun3x/include/ptrace.h
+++
@@ -1,3 +0,0 @@
-/*	$NetBSD: ptrace.h,v 1.1.1.1 1997/01/14 20:57:06 gwr Exp $	*/
-
-#include <m68k/ptrace.h>
