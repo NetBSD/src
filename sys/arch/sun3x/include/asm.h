d2400cdae43ffd73f33b12d585dba3b6d73b393e
cjs <cjs>
1997-05-31 22:30:12 +0000

    List of files in the cjs-build-against-obj-dir branch.

--- sys/arch/sun3x/include/asm.h
+++
@@ -1,3 +0,0 @@
-/*	$NetBSD: asm.h,v 1.1.1.1 1997/01/14 20:57:04 gwr Exp $	*/
-
-#include <m68k/asm.h>
--- sys/lib/libkern/arch/i386/DEFS.h
+++
@@ -1,3 +0,0 @@
-/*	$Id: DEFS.h,v 1.4 1995/10/07 09:26:59 mycroft Exp $ */
-
-#include <machine/asm.h>
