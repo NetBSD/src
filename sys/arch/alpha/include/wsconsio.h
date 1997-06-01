8154e0f9e6ffc9d31a0b79f7ca8f8ecb802071c1
cgd <cgd>
1997-06-01 05:48:16 +0000

    fix copyright; this file is entirely rewritten (by me), and shares no source with the existing wsconsio.h.

--- sys/arch/alpha/include/wsconsio.h
+++ sys/arch/alpha/include/wsconsio.h
@@ -1,30 +1,13 @@
-/* $NetBSD: wsconsio.h,v 1.1.2.3 1997/06/01 04:12:43 cgd Exp $ */
+/* $NetBSD: wsconsio.h,v 1.1.2.4 1997/06/01 05:48:16 cgd Exp $ */
 
 /*
- * Copyright (c) 1995, 1996 Carnegie-Mellon University.
- * All rights reserved.
+ * Copyright (c) 1996, 1997 Christopher G. Demetriou.  All rights reserved.
  *
- * Author: Chris G. Demetriou
+ * THIS SOURCE CODE AND ANY OBJECT CODE DERIVED FROM IT MAY NOT BE
+ * MODIFIED OR DISTRIBUTED TO ANYONE, FOR ANY REASON, WITHOUT EXPRESS 
+ * WRITTEN PERMISSION OF CHRISTOPHER G. DEMETRIOU.
  *
- * Permission to use, copy, modify and distribute this software and
- * its documentation is hereby granted, provided that both the copyright
- * notice and this permission notice appear in all copies of the
- * software, derivative works or modified versions, and any portions
- * thereof, and that both notices appear in supporting documentation.
- *
- * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS "AS IS"
- * CONDITION.  CARNEGIE MELLON DISCLAIMS ANY LIABILITY OF ANY KIND
- * FOR ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.
- *
- * Carnegie Mellon requests users of this software to return to
- *
- *  Software Distribution Coordinator  or  Software.Distribution@CS.CMU.EDU
- *  School of Computer Science
- *  Carnegie Mellon University
- *  Pittsburgh PA 15213-3890
- *
- * any improvements or extensions that they make and grant Carnegie the
- * rights to redistribute these changes.
+ * (XXX COPYRIGHT NOTICE)
  */
 
 #ifndef __ALPHA_INCLUDE_WSCONSIO_H_
