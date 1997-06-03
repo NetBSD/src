d27f62fe4935bfcdd6bbece32666d7e2662982c8
cgd <cgd>
1997-06-03 16:00:30 +0000

    declare ${DESTDIR}${INCSDIR}/$I .PHONY if UPDATE isn't defined, so that headers will be installed properly in the !UPDATE case.  From Christos.

--- share/mk/bsd.inc.mk
+++ share/mk/bsd.inc.mk
@@ -1,4 +1,4 @@
-#	$NetBSD: bsd.inc.mk,v 1.9 1997/05/31 21:21:56 cjs Exp $
+#	$NetBSD: bsd.inc.mk,v 1.10 1997/06/03 16:00:30 cgd Exp $
 
 .PHONY:		incinstall
 includes:	${INCS} incinstall
@@ -8,6 +8,9 @@ includes:	${INCS} incinstall
 incinstall:: ${DESTDIR}${INCSDIR}/$I
 
 .PRECIOUS: ${DESTDIR}${INCSDIR}/$I
+.if !defined(UPDATE)
+.PHONY: ${DESTDIR}${INCSDIR}/$I
+.endif
 ${DESTDIR}${INCSDIR}/$I: $I
 	@cmp -s ${.ALLSRC} ${.TARGET} > /dev/null 2>&1 || \
 	    (echo "${INSTALL} -c -o ${BINOWN} -g ${BINGRP} -m ${NONBINMODE} \
