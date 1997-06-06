a6bb931fcdfa930591bf8d1ae09795f0826c2360
thorpej <thorpej@NetBSD.org>
1997-06-06 23:28:40 +0000

    Pull thorpej-bus-dma branch into mainline.

--- sys/arch/i386/pci/pci_machdep.h
+++ sys/arch/i386/pci/pci_machdep.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: pci_machdep.h,v 1.6 1996/12/17 01:55:58 thorpej Exp $	*/
+/*	$NetBSD: pci_machdep.h,v 1.7 1997/06/06 23:29:18 thorpej Exp $	*/
 
 /*
  * Copyright (c) 1996 Christopher G. Demetriou.  All rights reserved.
@@ -53,6 +53,8 @@ union i386_pci_tag_u {
 		u_int8_t forward;
 	} mode2;
 };
+
+extern struct i386_bus_dma_tag pci_bus_dma_tag;
 
 /*
  * Types provided to machine-independent PCI code
