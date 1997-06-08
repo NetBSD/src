2d333f8ed0071c7b01ef157031ecac67de3e1d9b
mellon <mellon@NetBSD.org>
1997-06-08 04:53:30 +0000

    970607 snapshot

--- usr.sbin/dhcp/server/db.c
+++ usr.sbin/dhcp/server/db.c
@@ -42,7 +42,7 @@
 
 #ifndef lint
 static char copyright[] =
-"$Id: db.c,v 1.1.1.2 1997/06/03 02:49:55 mellon Exp $ Copyright (c) 1995, 1996 The Internet Software Consortium.  All rights reserved.\n";
+"$Id: db.c,v 1.1.1.3 1997/06/08 04:55:15 mellon Exp $ Copyright (c) 1995, 1996 The Internet Software Consortium.  All rights reserved.\n";
 #endif /* not lint */
 
 #include "dhcpd.h"
@@ -106,13 +106,13 @@ int write_lease (lease)
 	if (lease -> uid_len) {
 		int i;
 		errno = 0;
-		fprintf (db_file, "\n\tuid %x", lease -> uid [0]);
+		fprintf (db_file, "\n\tuid %2.2x", lease -> uid [0]);
 		if (errno) {
 			++errors;
 		}
 		for (i = 1; i < lease -> uid_len; i++) {
 			errno = 0;
-			fprintf (db_file, ":%x", lease -> uid [i]);
+			fprintf (db_file, ":%2.2x", lease -> uid [i]);
 			if (errno) {
 				++errors;
 			}
