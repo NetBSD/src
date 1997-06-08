9703a5d26b12553c732365e36b40e2d64f6cf5a0
veego <veego@NetBSD.org>
1997-06-08 10:35:39 +0000

    Use 0x0001 for MDP_STACKADJ. Thanks Klaus.

--- sys/arch/amiga/include/proc.h
+++ sys/arch/amiga/include/proc.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: proc.h,v 1.11 1997/06/07 08:27:47 veego Exp $	*/
+/*	$NetBSD: proc.h,v 1.12 1997/06/08 10:35:39 veego Exp $	*/
 
 /*
  * Copyright (c) 1991 Regents of the University of California.
@@ -46,7 +46,7 @@ struct mdproc {
 };
 
 /* md_flags */
-#define MDP_STACKADJ	0x0002	/* frame SP adjusted, might have to
+#define MDP_STACKADJ	0x0001	/* frame SP adjusted, might have to
 				   undo when system call returns
 				   ERESTART. */
 #endif /* !_MACHINE_PROC_H_ */
