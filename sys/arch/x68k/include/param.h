28b99eab6c979849872d4d1fefbf80af9cd74cce
veego <veego>
1997-06-10 20:20:06 +0000

    The 'Mach derived conversion macros' are now in <m68k/param.h>

--- sys/arch/x68k/include/param.h
+++ sys/arch/x68k/include/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.4 1997/06/10 07:54:48 veego Exp $	*/
+/*	$NetBSD: param.h,v 1.5 1997/06/10 20:20:06 veego Exp $	*/
 
 /*
  * Copyright (c) 1988 University of Utah.
@@ -54,13 +54,13 @@
 #define	PGSHIFT		12		/* LOG2(NBPG) */
 #define	KERNBASE	0x00000000	/* start of kernel virtual */
 
-#define	UPAGES		3		/* pages of u-area */
-
-#include <m68k/param.h>
-
 #define	SEGSHIFT	22		/* LOG2(NBSEG) */
 #define NBSEG		(1 << SEGSHIFT)	/* bytes/segment */
 #define	SEGOFSET	(NBSEG-1)	/* byte offset into segment */
+
+#define	UPAGES		3		/* pages of u-area */
+
+#include <m68k/param.h>
 
 /*
  * Size of kernel malloc arena in CLBYTES-sized logical pages
@@ -68,14 +68,6 @@
 #ifndef NKMEMCLUSTERS
 # define	NKMEMCLUSTERS	(2048 * 1024 / CLBYTES)
 #endif
-
-/*
- * Mach derived conversion macros
- */
-#define x68k_round_page(x)	((((unsigned)(x)) + PGOFSET) & ~PGOFSET)
-#define x68k_trunc_page(x)	((unsigned)(x) & ~PGOFSET)
-#define x68k_btop(x)		((unsigned)(x) >> PGSHIFT)
-#define x68k_ptob(x)		((unsigned)(x) << PGSHIFT)
 
 /*
  * spl functions; all but spl0 are done in-line
