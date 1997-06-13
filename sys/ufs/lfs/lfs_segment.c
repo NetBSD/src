f97288b7080d89efddbffbaa435c381bfb8ea8a0
pk <pk@NetBSD.org>
1997-06-13 08:59:51 +0000

    TIMESPEC_TO_TIMEVAL => TIMEVAL_TO_TIMESPEC

--- sys/ufs/lfs/lfs_segment.c
+++ sys/ufs/lfs/lfs_segment.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: lfs_segment.c,v 1.8 1997/06/11 10:09:59 bouyer Exp $	*/
+/*	$NetBSD: lfs_segment.c,v 1.9 1997/06/13 08:59:51 pk Exp $	*/
 
 /*
  * Copyright (c) 1991, 1993
@@ -415,7 +415,7 @@ lfs_writeinode(fs, sp, ip)
 	/* Update the inode times and copy the inode onto the inode page. */
 	if (ip->i_flag & IN_MODIFIED)
 		--fs->lfs_uinodes;
-	TIMESPEC_TO_TIMEVAL(&time, &ts);
+	TIMEVAL_TO_TIMESPEC(&time, &ts);
 	FFS_ITIMES(ip, &ts, &ts, &ts);
 	ip->i_flag &= ~(IN_ACCESS | IN_CHANGE | IN_MODIFIED | IN_UPDATE);
 	bp = sp->ibp;
