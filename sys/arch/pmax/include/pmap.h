2d10220f8f203e3b372364d14bfb9a6f58eb40a6
jonathan <jonathan@NetBSD.org>
1997-06-16 07:47:42 +0000

    Yet more mips1/mips3 merging:
    Move mips-specific pmap definitions (PMAP_PREFER for mips3, declaratin
    of pmap_bootstrap() for the system-specific machdep.c) from
    arch/pmax/include/pmap.h to arch/mips/include/pmap.h.

--- sys/arch/pmax/include/pmap.h
+++ sys/arch/pmax/include/pmap.h
@@ -1,17 +1,6 @@
-/*	$NetBSD: pmap.h,v 1.12 1997/06/15 18:02:22 mhitch Exp $	*/
+/*	$NetBSD: pmap.h,v 1.13 1997/06/16 07:47:42 jonathan Exp $	*/
 
 #include <mips/pmap.h>
 
 #define pmax_trunc_seg(a) mips_trunc_seg(a)
 #define pmax_round_seg(a) mips_round_seg(a)
-
-#ifdef MIPS3
-#define PMAP_PREFER(pa, va)             pmap_prefer((pa), (va))
-void	pmap_prefer __P((vm_offset_t, vm_offset_t *));
-#endif
-
-/*
- *	Bootstrap the system enough to run with virtual memory.
- *	firstaddr is the first unused kseg0 address (not page aligned).
- */
-void	pmap_bootstrap __P((vm_offset_t firstaddr));
