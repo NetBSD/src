3b94bab28085b0683d6bd478ed3072bd0ceb9405
jonathan <jonathan@NetBSD.org>
1997-06-23 22:04:34 +0000

    New, Alpha-derived crt0.c with ld.elf_so support.
    Expects additional register-passed arguments from the kernel on
    exec().  If no such args ($a0 is zero), fall back to the old hack of
    adjusting the current $sp with a hard-coded frame offset for __start's
    local frame(!).
    
    Suggested by Jason Thorpe (thorpej@nas.nasa.gov), debugged and
    stackframe offsets updated by Jonathan Stone.

--- lib/csu/mips/crt0.c
+++ lib/csu/mips/crt0.c
@@ -1,8 +1,12 @@
-/*	$NetBSD: crt0.c,v 1.1 1997/03/05 03:45:06 jonathan Exp $	*/
-
 /*
- * Copyright (c) 1993 Paul Kranenburg
+ * Copyright (c) 1995 Christopher G. Demetriou
  * All rights reserved.
+ *
+ * Modifications for NetBSD/mips:
+ *
+ *	Jonathan Stone
+ *	Jason R. Thorpe, Mumerical Aerospace Simulation Facility,
+ *	    NASA Ames Research Center
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -14,7 +18,8 @@
  *    documentation and/or other materials provided with the distribution.
  * 3. All advertising materials mentioning features or use of this software
  *    must display the following acknowledgement:
- *      This product includes software developed by Paul Kranenburg.
+ *      This product includes software developed by Christopher G. Demetriou
+ *	for the NetBSD Project.
  * 4. The name of the author may not be used to endorse or promote products
  *    derived from this software without specific prior written permission
  *
@@ -30,72 +35,272 @@
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-
-#if defined(LIBC_SCCS) && !defined(lint)
-static char rcsid[] = "$NetBSD: crt0.c,v 1.1 1997/03/05 03:45:06 jonathan Exp $";
-#endif /* LIBC_SCCS and not lint */
+#ifdef ECOFF_COMPAT
+#undef DYNAMIC
+#endif
 
 #include <sys/param.h>
+#include <sys/exec.h>
+#include <sys/syscall.h>
+#include <vm/vm.h>
+
 #include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
+#ifdef DYNAMIC
+#include <dlfcn.h>
+#include "rtld.h"
+#else
+typedef void Obj_Entry;
+#endif
 
-#ifndef CRTN
-extern void	start __P((long)) asm("start");
+/*
+ * Lots of the chunks of this file cobbled together from pieces of
+ * other NetBSD crt files, including the common code.
+ */
+
+extern int	__syscall __P((int, ...));
+#define	_exit(v)	__syscall(SYS_exit, (v))
+#define	write(fd, s, n)	__syscall(SYS_write, (fd), (s), (n))
+
+#define _FATAL(str)				\
+	do {					\
+		write(2, str, sizeof(str));	\
+		_exit(1);			\
+	} while (0)
+
+static char	*_strrchr __P((char *, char));
+
+
+char	**environ;
+char	*__progname = "";
+struct ps_strings *__ps_strings = 0;
+
+#ifndef ECOFF_COMPAT
+extern void	__init __P((void));
+extern void	__fini __P((void));
+#endif /* ECOFF_COMPAT */
+
+#ifdef DYNAMIC
+void		rtld_setup __P((void (*)(void), const Obj_Entry *obj));
+
+const Obj_Entry *__mainprog_obj;
+
+/*
+ * Arrange for _DYNAMIC to be weak and undefined (and therefore to show up
+ * as being at address zero, unless something else defines it).  That way,
+ * if we happen to be compiling without -static but with without any
+ * shared libs present, things will still work.
+ */
+asm(".weak _DYNAMIC");
+extern int _DYNAMIC;
+#endif /* DYNAMIC */
+
+#ifdef MCRT0
+extern void	monstartup __P((u_long, u_long));
 extern void	_mcleanup __P((void));
-extern unsigned char    eprol asm ("eprol");
-extern unsigned char    etext;
+extern unsigned char _etext, _eprol;
+#endif /* MCRT0 */
 
-char                    **environ;
-static char		empty[1];
-char			*__progname = empty;
-
+/*
+ *	C start-up.  Assumes kernel (or ld.so) passes the
+ *	following parameters to user-space in registers:
+ *
+ *	a0	stack pointer (0 if setregs didn't fill this in)
+ *	a1	cleanup
+ *	a2	Obj_Entry
+ *	a3	ps_strings
+ *
+ *	XXX Does this violate the ABI?
+ *	as well as the usual registers (pc, sp, and t9 == pc for ABI).
+ */
 
 void
-__start()
+__start(sp, cleanup, obj, ps_strings)
+	register u_long sp;
+	register void (*cleanup) __P((void));	/* from shared loader */
+	register const Obj_Entry *obj;		/* from shared loader */
+	register struct ps_strings *ps_strings;
 {
-	struct kframe {
-		int	kargc;
-		char	*kargv[1];	/* size depends on kargc */
-		char	kargstr[1];	/* size varies */
-		char	kenvstr[1];	/* size varies */
-	};
+	register char **ksp;
+	register char **argv, *namep;
+	register int argc;
 
-	register struct kframe *kfp;
-	register char **argv, *ap;
-	register int	argc;
+	/*
+	 * Grab the argc, argv, and envp set up by the kernel.
+	 * Layout of stuff on the stack:
+	 *
+	 *	[ high ]
+	 *	char	kenvstr[1];	// size varies 
+	 *	char	kargstr[1];	// size varies
+	 *	char	*argv[1];	// varies on argc
+	 *	int	argc;
+	 *	[ low ] 		<--- kernel's SP points here
+	 *	.
+	 *	.
+	 *	.
+	 *	[ current stack pointer ]
+	 *
+	 * WARNING!  There is an implicit sizeof(int) == sizeof(char *) here!
+	 */
 
 #ifndef DYNAMIC
-	asm("	la	$28,_gp");
-	asm("	addiu	%0,$29,32" : "=r" (kfp));
-#else
-	asm("	addiu	%0,$29,48" : "=r" (kfp));
+	__asm __volatile("la $28,_gp");
 #endif
-	/* just above the saved frame pointer
-	kfp = (struct kframe *) (&param-1);*/
-	argc = kfp->kargc;
-	argv = &kfp->kargv[0];
-	environ = argv + kfp->kargc + 1;
 
-	if (ap = argv[0])
-		if ((__progname = strrchr(ap, '/')) == NULL)
-			__progname = ap;
+	ksp = (char**)sp;
+	if (ksp == 0) {
+		/*
+		 * Uh, oh. We're running on a old kernel that passed
+		 * us zero in $a0-$a3.  Try adjusting the current
+		 * $sp for our own stack-frame size and see  what
+		 * we find.
+		 * WARNING!  The constants 56 and 64 below were determined
+		 * by examining GCC assembler output of __start to find the
+		 * frame size.  If you change the code or compiler,
+		 * you _will_ lose!
+		 */
+ 
+#ifndef DYNAMIC
+		/* XXX 56 is compiler and stackframe dependent */
+		__asm __volatile("	addiu	%0,$29,56" : "=r" (ksp));
+#else
+		/* XXX 64 is compiler and stackframe dependent */
+		__asm __volatile("	addiu	%0,$29,64" : "=r" (ksp));
+#endif
+	}
+
+
+	argc = *(int *)ksp;
+	argv = ksp + 1;
+	environ = ksp + 2 + argc;	/* 2: argc + NULL ending argv */
+
+	if ((namep = argv[0]) != NULL) {	/* NULL ptr if argc = 0 */
+		if ((__progname = _strrchr(namep, '/')) == NULL)
+			__progname = namep;
 		else
-			++__progname;
+			__progname++;
+	}
 
-asm("eprol:");
+	/*
+	 * Deal with stuff that is only provided if setregs() did
+	 * did the right thing.
+	 */
+	if (sp != 0) {
+		if (ps_strings != (struct ps_strings *)0)
+			__ps_strings = ps_strings;
+#ifdef DYNAMIC
+		/*
+		 * XXX Old MIPS ld.so didn't do any of this at all.
+		 * XXX If we were loaded by that loader, just abort
+		 * XXX the rtld setup.
+		 */
+		if (&_DYNAMIC != NULL && cleanup != NULL /*&& obj != NULL*/)
+			rtld_setup(cleanup, obj);
+#endif
+	}
 
 #ifdef MCRT0
 	atexit(_mcleanup);
-	monstartup((u_long)&eprol, (u_long)&etext);
-#endif MCRT0
+	monstartup((u_long)&_eprol, (u_long)&_etext);
+#endif
 
-asm ("_callmain:");		/* Defined for the benefit of debuggers */
+	/* XXX __fini and __init are not yet  on a MIPS. */
+#if 0
+#ifndef ECOFF_COMPAT
+	atexit(__fini);
+	__init();
+#endif /* ECOFF_COMPAT */
+#endif
 	exit(main(argc, argv, environ));
 }
 
+
+static char *
+_strrchr(p, ch)
+register char *p, ch;
+{
+	register char *save;
+
+	for (save = NULL; ; ++p) {
+		if (*p == ch)
+			save = (char *)p;
+		if (!*p)
+			return(save);
+	}
+/* NOTREACHED */
+}
+
 #ifdef MCRT0
-asm ("	.text");
+asm ("  .text");
 asm ("_eprol:");
 #endif
+
+#ifdef DYNAMIC
+void
+rtld_setup(cleanup, obj)
+	void (*cleanup) __P((void));
+	const Obj_Entry *obj;
+{
+
+	if ((obj == NULL) || (obj->magic != RTLD_MAGIC))
+		_FATAL("Corrupt Obj_Entry pointer in GOT");
+	if (obj->version != RTLD_VERSION)
+		_FATAL("Dynamic linker version mismatch");
+
+	__mainprog_obj = obj;
+	atexit(cleanup);
+}
+
+void *
+dlopen(name, mode)
+	const char *name;
+	int mode;
+{
+
+	if (__mainprog_obj == NULL)
+		return NULL;
+	return (__mainprog_obj->dlopen)(name, mode);
+}
+
+int
+dlclose(fd)
+	void *fd;
+{
+
+	if (__mainprog_obj == NULL)
+		return -1;
+	return (__mainprog_obj->dlclose)(fd);
+}
+
+void *
+dlsym(fd, name)
+	void *fd;
+	const char *name;
+{
+
+	if (__mainprog_obj == NULL)
+		return NULL;
+	return (__mainprog_obj->dlsym)(fd, name);
+}
+
+#if 0 /* not supported for ELF shlibs, apparently */
+int
+dlctl(fd, cmd, arg)
+	void *fd, *arg;
+	int cmd;
+{
+
+	if (__mainprog_obj == NULL)
+		return -1;
+	return (__mainprog_obj->dlctl)(fd, cmd, arg);
+}
 #endif
+
+char *
+dlerror()
+{
+
+	if (__mainprog_obj == NULL)
+		return NULL;
+	return (__mainprog_obj->dlerror)();
+}
+#endif /* DYNAMIC */
