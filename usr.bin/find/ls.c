b70081bba064d66848353a5810820b452d67339f
jtc <jtc@NetBSD.org>
1997-06-30 21:26:57 +0000

    Changed format directive for printing number of blocks from %4ld to %4qd as the st_blocks field is a int64_t.  Fixes PR 3814.

--- usr.bin/find/ls.c
+++ usr.bin/find/ls.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: ls.c,v 1.7 1997/01/09 20:19:13 tls Exp $	*/
+/*	$NetBSD: ls.c,v 1.8 1997/06/30 21:26:57 jtc Exp $	*/
 
 /*
  * Copyright (c) 1989, 1993
@@ -35,7 +35,7 @@
 
 #ifndef lint
 /*static char sccsid[] = "from: @(#)ls.c	8.1 (Berkeley) 6/6/93";*/
-static char rcsid[] = "$NetBSD: ls.c,v 1.7 1997/01/09 20:19:13 tls Exp $";
+static char rcsid[] = "$NetBSD: ls.c,v 1.8 1997/06/30 21:26:57 jtc Exp $";
 #endif /* not lint */
 
 #include <sys/param.h>
@@ -63,7 +63,7 @@ printlong(name, accpath, sb)
 {
 	char modep[15], *user_from_uid(), *group_from_gid();
 
-	(void)printf("%6lu %4ld ", sb->st_ino, sb->st_blocks);
+	(void)printf("%6lu %4qd ", sb->st_ino, sb->st_blocks);
 	(void)strmode(sb->st_mode, modep);
 	(void)printf("%s %3u %-*s %-*s ", modep, sb->st_nlink, UT_NAMESIZE,
 	    user_from_uid(sb->st_uid, 0), UT_NAMESIZE,
