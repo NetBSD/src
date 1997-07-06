0467342a458d5104152c4e7c936c756511b01c59
pk <pk@NetBSD.org>
1997-07-06 23:57:16 +0000

    Follow lock changes (DEBUG => LOCKDEBUG).

--- sys/arch/sparc/include/pmap.h
+++ sys/arch/sparc/include/pmap.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: pmap.h,v 1.28 1997/07/06 23:53:22 pk Exp $ */
+/*	$NetBSD: pmap.h,v 1.29 1997/07/06 23:57:16 pk Exp $ */
 
 /*
  * Copyright (c) 1996
@@ -138,7 +138,7 @@ TAILQ_HEAD(mmuhd,mmuentry);
 struct pmap {
 	union	ctxinfo *pm_ctx;	/* current context, if any */
 	int	pm_ctxnum;		/* current context's number */
-#if defined(DEBUG) || NCPUS > 1
+#if defined(LOCKDEBUG) || NCPUS > 1
 	simple_lock_data_t pm_lock;	/* spinlock */
 #endif
 	int	pm_refcount;		/* just what it says */
