ee6ada36efcbedbbd6f7c67718227d11683436ed
christos <christos>
1997-07-06 18:25:21 +0000

    Fix compiler warnings.

--- lib/libedit/prompt.c
+++ lib/libedit/prompt.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: prompt.c,v 1.2 1997/01/11 06:48:04 lukem Exp $	*/
+/*	$NetBSD: prompt.c,v 1.3 1997/07/06 18:25:31 christos Exp $	*/
 
 /*-
  * Copyright (c) 1992, 1993
@@ -36,11 +36,12 @@
  * SUCH DAMAGE.
  */
 
+#include <sys/cdefs.h>
 #if !defined(lint) && !defined(SCCSID)
 #if 0
 static char sccsid[] = "@(#)prompt.c	8.1 (Berkeley) 6/4/93";
 #else
-static char rcsid[] = "$NetBSD: prompt.c,v 1.2 1997/01/11 06:48:04 lukem Exp $";
+__RCSID("$NetBSD: prompt.c,v 1.3 1997/07/06 18:25:31 christos Exp $");
 #endif
 #endif /* not lint && not SCCSID */
 
