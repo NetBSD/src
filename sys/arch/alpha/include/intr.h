323a78da1e46ffdead04866910d980724ca083c6
cgd <cgd>
1997-07-07 20:37:34 +0000

    mark prototypes for static inline functions as possibly unused (with __attribute__ ((unused))), to avoid generating warnings when compiling without optimization but with the default warning flags.

--- sys/arch/alpha/include/intr.h
+++ sys/arch/alpha/include/intr.h
@@ -1,4 +1,4 @@
-/* $NetBSD: intr.h,v 1.9 1997/06/05 17:36:26 cgd Exp $ */
+/* $NetBSD: intr.h,v 1.10 1997/07/07 20:37:34 cgd Exp $ */
 
 /*
  * Copyright (c) 1997 Christopher G. Demetriou.  All rights reserved.
@@ -54,7 +54,7 @@
 #define splsoftnet()            splsoft()
 
 /* IPL-raising functions/macros */
-static __inline int _splraise __P((int));
+static __inline int _splraise __P((int)) __attribute__ ((unused));
 static __inline int
 _splraise(s)
 	int s;
