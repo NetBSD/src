9884390cbe6d43e7945025792fc2abf90af0cceb
scottr <scottr@NetBSD.org>
1997-07-12 06:13:08 +0000

    Remove a stowaway that got in on the last commit.

--- sys/arch/mac68k/mac68k/machdep.c
+++ sys/arch/mac68k/mac68k/machdep.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: machdep.c,v 1.154 1997/07/12 06:07:24 scottr Exp $	*/
+/*	$NetBSD: machdep.c,v 1.155 1997/07/12 06:13:08 scottr Exp $	*/
 
 /*
  * Copyright (c) 1996 Jason R. Thorpe.  All rights reserved.
@@ -1984,7 +1984,6 @@ struct {
 	caddr_t	fbbase;
 	u_long	fblen;
 } intvid_info[] =  {
-	{ MACH_MACCLASSICII,	(caddr_t)0xfee09a80,	21888 },
 	{ MACH_MACPB140,	(caddr_t)0xfee00000,	32 * 1024 },
 	{ MACH_MACPB145,	(caddr_t)0xfee00000,	32 * 1024 },
 	{ MACH_MACPB170,	(caddr_t)0xfee00000,	32 * 1024 },
