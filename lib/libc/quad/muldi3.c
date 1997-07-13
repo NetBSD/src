2531ab66a808cd6311ad6f3e51b211571543046a
christos <christos@NetBSD.org>
1997-07-13 20:01:39 +0000

    Fix RCSID's

--- lib/libc/quad/muldi3.c
+++ lib/libc/quad/muldi3.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: muldi3.c,v 1.2 1995/02/27 17:30:39 cgd Exp $	*/
+/*	$NetBSD: muldi3.c,v 1.3 1997/07/13 20:01:51 christos Exp $	*/
 
 /*-
  * Copyright (c) 1992, 1993
@@ -37,11 +37,12 @@
  * SUCH DAMAGE.
  */
 
+#include <sys/cdefs.h>
 #if defined(LIBC_SCCS) && !defined(lint)
 #if 0
 static char sccsid[] = "@(#)muldi3.c	8.1 (Berkeley) 6/4/93";
 #else
-static char rcsid[] = "$NetBSD: muldi3.c,v 1.2 1995/02/27 17:30:39 cgd Exp $";
+__RCSID("$NetBSD: muldi3.c,v 1.3 1997/07/13 20:01:51 christos Exp $");
 #endif
 #endif /* LIBC_SCCS and not lint */
 
