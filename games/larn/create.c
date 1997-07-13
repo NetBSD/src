ab8561ee9a90b7a928a89cb544516c6ed04e5798
christos <christos@NetBSD.org>
1997-07-13 20:21:32 +0000

    gtime -> gltime to avoid conflict in time.h

--- games/larn/create.c
+++ games/larn/create.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static char rcsid[] = "$NetBSD: create.c,v 1.4 1997/03/29 20:42:17 thorpej Exp $";
+static char rcsid[] = "$NetBSD: create.c,v 1.5 1997/07/13 20:21:32 christos Exp $";
 #endif /* not lint */
 
 /*	create.c		Larn is copyrighted 1986 by Noah Morgan. */
@@ -32,7 +32,7 @@ makeplayer()
 		}
 	playerx=rnd(MAXX-2);	playery=rnd(MAXY-2);
 	oldx=0;			oldy=25;
-	gtime=0;			/*	time clock starts at zero	*/
+	gltime=0;			/*	time clock starts at zero	*/
 	cbak[SPELLS] = -50;
 	for (i=0; i<6; i++)  c[i]=12; /* make the attributes, ie str, int, etc.	*/
 	recalc();
