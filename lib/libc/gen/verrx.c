028f9e7afcfe8d811228a02d083ef1c28e4b3273
christos <christos@NetBSD.org>
1997-07-13 19:42:37 +0000

    Fix RCSID's Use "namespace.h"

--- lib/libc/gen/verrx.c
+++ lib/libc/gen/verrx.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: verrx.c,v 1.1 1996/04/15 23:45:36 jtc Exp $	*/
+/*	$NetBSD: verrx.c,v 1.2 1997/07/13 19:42:37 christos Exp $	*/
 
 /*-
  * Copyright (c) 1993
@@ -33,14 +33,16 @@
  * SUCH DAMAGE.
  */
 
+#include <sys/cdefs.h>
 #if defined(LIBC_SCCS) && !defined(lint)
 #if 0
 static char sccsid[] = "@(#)err.c	8.1 (Berkeley) 6/4/93";
 #else
-static char rcsid[] = "$NetBSD: verrx.c,v 1.1 1996/04/15 23:45:36 jtc Exp $";
+__RCSID("$NetBSD: verrx.c,v 1.2 1997/07/13 19:42:37 christos Exp $");
 #endif
 #endif /* LIBC_SCCS and not lint */
 
+#include "namespace.h"
 #include <err.h>
 #include <stdio.h>
 #include <stdlib.h>
