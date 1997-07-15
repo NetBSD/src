aeccb886c0769c062b4f60fd085cf2a7f3efcec1
christos <christos@NetBSD.org>
1997-07-15 17:45:53 +0000

    Fix Sparcworks warnings.

--- bin/sh/parser.c
+++ bin/sh/parser.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: parser.c,v 1.36 1997/07/04 21:02:19 christos Exp $	*/
+/*	$NetBSD: parser.c,v 1.37 1997/07/15 17:45:53 christos Exp $	*/
 
 /*-
  * Copyright (c) 1991, 1993
@@ -41,7 +41,7 @@
 #if 0
 static char sccsid[] = "@(#)parser.c	8.7 (Berkeley) 5/16/95";
 #else
-__RCSID("$NetBSD: parser.c,v 1.36 1997/07/04 21:02:19 christos Exp $");
+__RCSID("$NetBSD: parser.c,v 1.37 1997/07/15 17:45:53 christos Exp $");
 #endif
 #endif /* not lint */
 
@@ -103,7 +103,8 @@ int startlinno;			/* line # where last token started */
 
 #define GDB_HACK 1 /* avoid local declarations which gdb can't handle */
 #ifdef GDB_HACK
-static const char argvars[5] = {CTLVAR, VSNORMAL|VSQUOTE, '@', '=', '\0'};
+static const char argvars[5] = {(char)CTLVAR, (char)(VSNORMAL|VSQUOTE),
+    '@', '=', '\0'};
 static const char types[] = "}-+?=";
 #endif
 
