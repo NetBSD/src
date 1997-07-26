66d046de81ac61825f431bcf2b2bf0fb748940e9
ragge <ragge>
1997-07-26 10:12:40 +0000

    Actually do 'halt' or 'reboot' on MicroVAX.

--- sys/arch/vax/include/ka630.h
+++ sys/arch/vax/include/ka630.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: ka630.h,v 1.1 1996/07/20 17:58:17 ragge Exp $ */
+/*	$NetBSD: ka630.h,v 1.2 1997/07/26 10:12:41 ragge Exp $ */
 /*-
  * Copyright (c) 1986, 1988 The Regents of the University of California.
  * All rights reserved.
@@ -94,6 +94,7 @@ struct uvaxIIcpu {
 #define	KA630CLK_RESTRT	01
 #define	KA630CLK_REBOOT	02
 #define	KA630CLK_HALT	03
+#define	KA630CLK_DOTHIS	040
 /* in progress flags */
 #define	KA630CLK_BOOT	04
 #define	KA630CLK_RSTRT	010
