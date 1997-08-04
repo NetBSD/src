6db4990173e11b33b3ade6976283874a6e453a61
perry <perry@NetBSD.org>
1997-08-04 05:29:20 +0000

    Added CAPS_IS_CONTROL option that switches the caps lock and control keys on a pccons console keyboard. submitted in PR 899 by Alistair G. Crooks Note that I only did this for the US type keyboard maps. This and all other such options should be documented, and perhaps rennamed with consistant PCCONS_ prefixes.

--- sys/arch/i386/isa/pccons.c
+++ sys/arch/i386/isa/pccons.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: pccons.c,v 1.107 1997/08/04 05:05:31 perry Exp $	*/
+/*	$NetBSD: pccons.c,v 1.108 1997/08/04 05:29:20 perry Exp $	*/
 
 /*-
  * Copyright (c) 1993, 1994, 1995 Charles Hannum.  All rights reserved.
@@ -1400,7 +1400,11 @@ static Scan_def	scan_codes[] = {
 	{ ASCII,"[",		"{",		"\033" },	/* 26 [ */
 	{ ASCII,"]",		"}",		"\035" },	/* 27 ] */
 	{ ASCII,"\r",		"\r",		"\n" },		/* 28 return */
+#ifdef CAPS_IS_CONTROL
+	{ CAPS,	"",		"",		"" },		/* 29 caps */
+#else
 	{ CTL,	"",		"",		"" },		/* 29 control */
+#endif
 	{ ASCII,"a",		"A",		"\001" },	/* 30 a */
 	{ ASCII,"s",		"S",		"\023" },	/* 31 s */
 	{ ASCII,"d",		"D",		"\004" },	/* 32 d */
@@ -1429,7 +1433,11 @@ static Scan_def	scan_codes[] = {
 	{ KP,	"*",		"*",		"*" },		/* 55 kp * */
 	{ ALT,	"",		"",		"" },		/* 56 alt */
 	{ ASCII," ",		" ",		"\000" },	/* 57 space */
+#ifdef CAPS_IS_CONTROL
+	{ CTL,	"",		"",		"" },		/* 58 control */
+#else
 	{ CAPS,	"",		"",		"" },		/* 58 caps */
+#endif
 	{ FUNC,	"\033[M",	"\033[Y",	"\033[k" },	/* 59 f1 */
 	{ FUNC,	"\033[N",	"\033[Z",	"\033[l" },	/* 60 f2 */
 	{ FUNC,	"\033[O",	"\033[a",	"\033[m" },	/* 61 f3 */
