63cf0cfa7c4c84331ba2f9731ef031162039c1bb
drochner <drochner@NetBSD.org>
1997-08-05 10:27:57 +0000

    #if -> #ifdef, fixes XMS support from Martin Husemann, pr port-i386/3941

--- sys/arch/i386/stand/lib/exec_fromdos.c
+++ sys/arch/i386/stand/lib/exec_fromdos.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: exec_fromdos.c,v 1.1 1997/06/13 17:49:24 drochner Exp $	 */
+/*	$NetBSD: exec_fromdos.c,v 1.2 1997/08/05 10:27:57 drochner Exp $	 */
 
 /*
  * Copyright (c) 1982, 1986, 1990, 1993
@@ -135,7 +135,7 @@ exec_netbsd(file, loadaddr, boothowto, bootdev, consdev)
 
 	extmem = getextmem();
 
-#if XMS
+#ifdef XMS
 	if ((extmem == 0) && (extmem = checkxms())) {
 	        u_long kernsize;
 
