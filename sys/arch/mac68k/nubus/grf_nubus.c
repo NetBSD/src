2b897a9a7f23b3ec3667ca13f8cd863550012ab1
scottr <scottr@NetBSD.org>
1997-08-14 06:16:46 +0000

    Recognize the Sigma Designs ColorMax card, and handle the interrupt.  Code provided by Brian Foley <bfoley@gpo.iol.ie> via Allen Briggs, updated for bus.h by me.

--- sys/arch/mac68k/nubus/grf_nubus.c
+++ sys/arch/mac68k/nubus/grf_nubus.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: grf_nubus.c,v 1.31 1997/08/11 22:53:31 scottr Exp $	*/
+/*	$NetBSD: grf_nubus.c,v 1.32 1997/08/14 06:16:46 scottr Exp $	*/
 
 /*
  * Copyright (c) 1995 Allen Briggs.  All rights reserved.
@@ -58,6 +58,7 @@ static void	grfmv_intr_radius __P((void *vsc, int slot));
 static void	grfmv_intr_cti __P((void *vsc, int slot));
 static void	grfmv_intr_cb264 __P((void *vsc, int slot));
 static void	grfmv_intr_cb364 __P((void *vsc, int slot));
+static void	grfmv_intr_cmax __P((void *vsc, int slot));
 
 static int	grfmv_mode __P((struct grf_softc *gp, int cmd, void *arg));
 static caddr_t	grfmv_phys __P((struct grf_softc *gp));
@@ -236,6 +237,9 @@ bad:
 		sc->cli_offset = 0xa00000;
 		sc->cli_value = 0;
 		add_nubus_intr(na->slot, grfmv_intr_generic_1, sc);
+		break;
+	case NUBUS_DRHW_COLORMAX:
+		add_nubus_intr(na->slot, grfmv_intr_cmax, sc);
 		break;
 	case NUBUS_DRHW_SE30:
 		/* Do nothing--SE/30 interrupts are disabled */
@@ -530,4 +534,20 @@ grfmv_intr_cb364(vsc, slot)
 			movl	#0x1,a0@(0xfe6014)
 		_cb364_intr_quit:
 		" : : "g" (slotbase) : "a0","d0","d1","d2");
+}
+
+/*
+ * Routine to clear interrupts for the Sigma Designs ColorMax card.
+ */
+/*ARGSUSED*/
+static void
+grfmv_intr_cmax(vsc, slot)
+	void	*vsc;
+	int	slot;
+{
+	struct grfbus_softc *sc = (struct grfbus_softc *)vsc;
+	u_int32_t dummy;
+
+	dummy = bus_space_read_4(sc->sc_tag, sc->sc_handle, 0xf501c);
+	dummy = bus_space_read_4(sc->sc_tag, sc->sc_handle, 0xf5018);
 }
