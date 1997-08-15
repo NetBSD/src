17bfffb9fc3b7cf453286218cbaaec819b16ca4b
briggs <briggs>
1997-08-15 00:15:32 +0000

    Add interrupt handler for SuperMac GFX. Tested by Kai-Yew Lum <kaiyewl@engin.umich.edu> back in May.

--- sys/arch/mac68k/nubus/nubus.h
+++ sys/arch/mac68k/nubus/nubus.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: nubus.h,v 1.29 1997/08/14 06:16:49 scottr Exp $	*/
+/*	$NetBSD: nubus.h,v 1.30 1997/08/15 00:15:33 briggs Exp $	*/
 
 /*
  * Copyright (c) 1995 Allen Briggs.  All rights reserved.
@@ -58,6 +58,7 @@
 #define    NUBUS_DRHW_M2HRVC	0x0013	/* Apple Mac II High-Res Video Card */
 #define    NUBUS_DRHW_PVC	0x0017	/* Apple Mac II Portrait Video Card */
 #define    NUBUS_DRHW_MDC	0x0019	/* Apple Macintosh Display Card */
+#define    NUBUS_DRHW_SUPRGFX	0x0105	/* SuperMac GFX */
 #define    NUBUS_DRHW_CB264	0x013B	/* RasterOps ColorBoard 264 */
 #define    NUBUS_DRHW_MICRON	0x0146
 #define    NUBUS_DRHW_CB364	0x026F	/* RasterOps ColorBoard 364 */
