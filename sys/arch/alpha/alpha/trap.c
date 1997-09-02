c3d423e53aaa68bc4fb186dbcd33526f9bdce5bc
thorpej <thorpej@NetBSD.org>
1997-09-02 13:22:53 +0000

    Pull in FIX_UNALIGNED_VAX_FP option.

--- sys/arch/alpha/alpha/trap.c
+++ sys/arch/alpha/alpha/trap.c
@@ -1,4 +1,4 @@
-/* $NetBSD: trap.c,v 1.26 1997/09/02 13:18:40 thorpej Exp $ */
+/* $NetBSD: trap.c,v 1.27 1997/09/02 13:22:53 thorpej Exp $ */
 
 /*
  * Copyright (c) 1994, 1995, 1996 Carnegie-Mellon University.
@@ -27,9 +27,11 @@
  * rights to redistribute these changes.
  */
 
+#include "opt_fix_unaligned_vax_fp.h"
+
 #include <sys/cdefs.h>			/* RCS ID & Copyright macro defns */
 
-__KERNEL_RCSID(0, "$NetBSD: trap.c,v 1.26 1997/09/02 13:18:40 thorpej Exp $");
+__KERNEL_RCSID(0, "$NetBSD: trap.c,v 1.27 1997/09/02 13:22:53 thorpej Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
