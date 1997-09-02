71645788d2b31f38d6d6ab7fe245c8fc818e5581
thorpej <thorpej>
1997-09-02 13:25:36 +0000

    Pull in DEC_AXPPCI_33 option.

--- sys/arch/alpha/pci/lca.c
+++ sys/arch/alpha/pci/lca.c
@@ -1,4 +1,4 @@
-/* $NetBSD: lca.c,v 1.23 1997/09/02 13:19:30 thorpej Exp $ */
+/* $NetBSD: lca.c,v 1.24 1997/09/02 13:25:36 thorpej Exp $ */
 
 /*
  * Copyright (c) 1995, 1996 Carnegie-Mellon University.
@@ -27,9 +27,11 @@
  * rights to redistribute these changes.
  */
 
+include "opt_dec_axppci_33.h"
+
 #include <sys/cdefs.h>			/* RCS ID & Copyright macro defns */
 
-__KERNEL_RCSID(0, "$NetBSD: lca.c,v 1.23 1997/09/02 13:19:30 thorpej Exp $");
+__KERNEL_RCSID(0, "$NetBSD: lca.c,v 1.24 1997/09/02 13:25:36 thorpej Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
