9d5aae5b5a885d0194034979ab3807ec031fdcc8
cjs <cjs>
1997-09-09 06:35:00 +0000

    Sendmail 8.8.7 merge.

--- usr.sbin/sendmail/contrib/expn.pl
+++ usr.sbin/sendmail/contrib/expn.pl
@@ -13,7 +13,7 @@ $sockaddr = 'S n a4 x8';
 # system requirements:
 # 	must have 'nslookup' and 'hostname' programs.
 
-# Header: /home/muir/bin/RCS/expn,v 3.9 1995/10/02 17:51:35 muir Exp muir 
+# $Header: /cvsroot/src/usr.sbin/sendmail/contrib/Attic/expn.pl,v 1.6 1997/09/09 06:35:30 cjs Exp $
 
 # TODO:
 #	less magic should apply to command-line addresses
