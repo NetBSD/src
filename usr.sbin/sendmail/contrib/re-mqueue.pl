fff1019900d2c07d3b10e0fbb4216189ca0af5da
cjs <cjs@NetBSD.org>
1997-09-09 06:35:00 +0000

    Sendmail 8.8.7 merge.

--- usr.sbin/sendmail/contrib/re-mqueue.pl
+++ usr.sbin/sendmail/contrib/re-mqueue.pl
@@ -84,7 +84,7 @@
 # OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 # SUCH DAMAGE.
 #
-# @(#)Id: re-mqueue,v 1.3 1995/05/25 18:14:53 p-pomes Exp 
+# @(#)$Id: re-mqueue.pl,v 1.3 1997/09/09 06:35:32 cjs Exp $
 
 require "syslog.pl";
 
