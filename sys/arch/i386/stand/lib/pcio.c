93ad743f65fc6e9f9dec3cbbbe9dff28b34e16a7
drochner <drochner>
1997-09-17 17:48:00 +0000

    -extend serial console code to all 4 BIOS known interfaces -optionally, use Martin's direct hardware access to serial lines -optionally, new console autoselection method: select serial console  if a character is seen within 1 second -move awaitkey() here, it can be shared by different bootloaders and  it is used by console autoselection -collect information about console selection in a "bootinfo" defined  structure, for passing to the kernel

--- sys/arch/i386/stand/lib/pcio.c
+++ sys/arch/i386/stand/lib/pcio.c
@@ -1,7 +1,7 @@
-/*	$NetBSD: pcio.c,v 1.4 1997/06/13 13:41:20 drochner Exp $	 */
+/*	$NetBSD: pcio.c,v 1.5 1997/09/17 17:48:00 drochner Exp $	 */
 
 /*
- * Copyright (c) 1996
+ * Copyright (c) 1996, 1997
  *	Matthias Drochner.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -38,68 +38,125 @@
  */
 
 #include <lib/libsa/stand.h>
+#include <lib/libkern/libkern.h>
 
 #include "libi386.h"
+#include "bootinfo.h"
 
 extern void conputc __P((int));
 extern int congetc __P((void));
 extern int coniskey __P((void));
 
+struct btinfo_console btinfo_console;
+
 #ifdef SUPPORT_SERIAL
+static int iodev;
+
+#ifdef DIRECT_SERIAL
+#include "comio_direct.h"
+
+#define cominit cominit_d
+#define computc computc_d
+#define comgetc comgetc_d
+/* comstatus() is different */
+
+#define SERIAL_ARG btinfo_console.addr
+#else
 extern void cominit __P((int));
 extern int computc __P((int, int));
 extern int comgetc __P((int));
 extern int comstatus __P((int));
 
-static int      iodev;
-#endif
+#define SERIAL_ARG (iodev - CONSDEV_COM0)
+#endif /* DIRECT_SERIAL */
 
-static char    *iodevname[] = {
-	"pc",
+static int getcomaddr __P((int));
+#endif /* SUPPORT_SERIAL */
+
+#define POLL_FREQ 10
+
 #ifdef SUPPORT_SERIAL
-	"com0",
-	"com1"
+static int
+getcomaddr(idx)
+int idx;
+{
+	short addr;
+	/* read in BIOS data area */
+	pvbcopy(0x400 + 2 * idx, &addr, 2);
+	return(addr);
+}
 #endif
-};
 
-char           *
+void
 initio(dev)
 	int             dev;
 {
 #ifdef SUPPORT_SERIAL
+	int i;
+
 	switch (dev) {
-	case CONSDEV_AUTO:
-		/*
-		 * serial console must have hardware handshake! check: 1.
-		 * character output without error 2. status bits for modem
-		 * ready set (status seems only useful after character
-		 * output)
-		 */
-		cominit(0);
-		if (!(computc(' ', 0) & 0x80) && (comstatus(0) & 0x00b0))
-			dev = CONSDEV_COM0;
-		else {
-			cominit(1);
-			if (!(computc(' ', 1) & 0x80) && (comstatus(1) & 0x00b0))
-				dev = CONSDEV_COM1;
-			else
-				dev = CONSDEV_PC;
+	    case CONSDEV_AUTO:
+		for(i = 0; i < 3; i++) {
+			iodev = CONSDEV_COM0 + i;
+			btinfo_console.addr = getcomaddr(i);
+			if(!btinfo_console.addr) break;
+			conputc('0' + i); /* to tell user what happens */
+			cominit(SERIAL_ARG);
+#ifdef DIRECT_SERIAL
+			/* check for:
+			 *  1. successful output
+			 *  2. optionally, keypress within 1s
+			 */
+			if(computc(' ', SERIAL_ARG)
+#ifdef COMCONS_KEYPRESS
+			   && awaitkey(1, 0)
+#endif
+			   )
+				goto ok;
+#else
+			/*
+			 * serial console must have hardware handshake!
+			 * check:
+			 *  1. character output without error
+			 *  2. status bits for modem ready set
+			 *     (status seems only useful after character output)
+			 *  3. optionally, keypress within 1s
+			 */
+			if (!(computc(' ', SERIAL_ARG) & 0x80)
+			    && (comstatus(SERIAL_ARG) & 0x00b0)
+#ifdef COMCONS_KEYPRESS
+			    && awaitkey(1, 0)
+#endif
+			    )
+				goto ok;
+#endif
 		}
+		iodev = CONSDEV_PC;
+ok:
 		break;
-	case CONSDEV_COM0:
-		cominit(0);
+	    case CONSDEV_COM0:
+	    case CONSDEV_COM1:
+	    case CONSDEV_COM2:
+	    case CONSDEV_COM3:
+		iodev = dev;
+		btinfo_console.addr = getcomaddr(iodev - CONSDEV_COM0);
+		if(!btinfo_console.addr) goto nocom;
+		cominit(SERIAL_ARG);
 		break;
-	case CONSDEV_COM1:
-		cominit(1);
-		break;
-	default:
-		dev = CONSDEV_PC;
+	    default:
+nocom:
+		iodev = CONSDEV_PC;
 		break;
 	}
-	return (iodevname[iodev = dev]);
+	strncpy(btinfo_console.devname, iodev == CONSDEV_PC ? "pc" : "com", 16);
+#if defined(DIRECT_SERIAL) && defined(CONSPEED)
+	btinfo_console.speed = CONSPEED;
 #else
-	return (iodevname[0]);
+	btinfo_console.speed = 9600;
 #endif
+#else /* !SUPPORT_SERIAL */
+	strncpy(btinfo_console.devname, "pc", 16);
+#endif /* SUPPORT_SERIAL */
 }
 
 static inline void
@@ -108,16 +165,16 @@ internal_putchar(c)
 {
 #ifdef SUPPORT_SERIAL
 	switch (iodev) {
-	case CONSDEV_PC:
+	    case CONSDEV_PC:
 #endif
 		conputc(c);
 #ifdef SUPPORT_SERIAL
 		break;
-	case CONSDEV_COM0:
-		computc(c, 0);
-		break;
-	case CONSDEV_COM1:
-		computc(c, 1);
+	    case CONSDEV_COM0:
+	    case CONSDEV_COM1:
+	    case CONSDEV_COM2:
+	    case CONSDEV_COM3:
+		computc(c, SERIAL_ARG);
 		break;
 	}
 #endif
@@ -137,15 +194,16 @@ getchar()
 {
 #ifdef SUPPORT_SERIAL
 	switch (iodev) {
-		default: /* to make gcc -Wall happy... */
-		case CONSDEV_PC:
+	    default: /* to make gcc -Wall happy... */
+	    case CONSDEV_PC:
 #endif
 		return (congetc());
 #ifdef SUPPORT_SERIAL
-	case CONSDEV_COM0:
-		return (comgetc(0));
-	case CONSDEV_COM1:
-		return (comgetc(1));
+	    case CONSDEV_COM0:
+	    case CONSDEV_COM1:
+	    case CONSDEV_COM2:
+	    case CONSDEV_COM3:
+		return (comgetc(SERIAL_ARG));
 	}
 #endif
 }
@@ -155,15 +213,49 @@ iskey()
 {
 #ifdef SUPPORT_SERIAL
 	switch (iodev) {
-		default: /* to make gcc -Wall happy... */
-		case CONSDEV_PC:
+	    default: /* to make gcc -Wall happy... */
+	    case CONSDEV_PC:
 #endif
 		return (coniskey());
 #ifdef SUPPORT_SERIAL
-	case CONSDEV_COM0:
-		return (!!(comstatus(0) & 0x0100));
-	case CONSDEV_COM1:
-		return (!!(comstatus(1) & 0x0100));
-	}
+	    case CONSDEV_COM0:
+	    case CONSDEV_COM1:
+	    case CONSDEV_COM2:
+	    case CONSDEV_COM3:
+#ifdef DIRECT_SERIAL
+		return(!!comstatus_d(SERIAL_ARG));
+#else
+		return (!!(comstatus(SERIAL_ARG) & 0x0100));
 #endif
+	}
+#endif /* SUPPORT_SERIAL */
+}
+
+char
+awaitkey(timeout, tell)
+	int timeout, tell;
+{
+	int i;
+	char c = 0;
+
+	i = timeout * POLL_FREQ;
+
+	while (i) {
+		if (tell && (i % POLL_FREQ) == 0)
+			printf("%d\b", i/POLL_FREQ);
+		if (iskey()) {
+			/* flush input buffer */
+			while (iskey())
+				c = getchar();
+			goto out; /* XXX what happens if c == 0? */
+		}
+		delay(1000000 / POLL_FREQ);
+		i--;
+	}
+
+out:
+	if (tell)
+		printf("0\n");
+
+	return(c);
 }
