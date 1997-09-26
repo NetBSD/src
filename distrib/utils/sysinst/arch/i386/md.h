af1d23bed394cf16fca0ef2e8fd2788567274718
phil <phil@NetBSD.org>
1997-09-26 23:02:53 +0000

    Import of sysinst.  Not quite ready, but other ports want access for parallel development.  i386 version about 90% complete.  pc532 version just started, other architectures later in coordination with portmasters.
    This is a curses based, menu driven installation program.
    
    Better error checking is needed.

---
+++ distrib/utils/sysinst/arch/i386/md.h
@@ -0,0 +1,72 @@
+/*	$NetBSD: md.h,v 1.1.1.1 1997/09/26 23:02:55 phil Exp $	*/
+
+/*
+ * Copyright 1997 Piermont Information Systems Inc.
+ * All rights reserved.
+ *
+ * Written by Philip A. Nelson for Piermont Information Systems Inc.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *      This product includes software develooped for the NetBSD Project by
+ *      Piermont Information Systems Inc.
+ * 4. The name of Piermont Information Systems Inc. may not be used to endorse
+ *    or promote products derived from this software without specific prior
+ *    written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY PIERMONT INFORMATION SYSTEMS INC. ``AS IS''
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL PIERMONT INFORMATION SYSTEMS INC. BE 
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF 
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+/* md.h -- Machine specific definitions for the i386 */
+
+/* constants and defines */
+
+/* Megs required for a full X installation. */
+#define XNEEDMB 50
+
+/* fdisk partition information dlxxxx => NetBSD disklabel, bxxxx => bios */
+EXTERN int bcyl, bhead, bsec, bsize, bcylsize;
+EXTERN int bstuffset INIT(0);
+
+enum info {ID,SIZE,START,FLAG};
+EXTERN int part[4][4] INIT({{0}});
+EXTERN int activepart;
+EXTERN int bsdpart;
+EXTERN int usefull;
+
+/* Definition of files to retreive from ftp. */
+EXTERN char ftp_prefix[STRSIZE] INIT("/binary/Tarfiles");
+EXTERN char ftp_postfix[STRSIZE] INIT(".tar.gz");
+EXTERN char *ftp_list[]
+#ifdef MAIN
+= { "kern", "base", "comp", "etc", "games", "man", "misc", "text", NULL }
+#endif
+;
+
+/* prototypes */
+
+/* from fdisk.c */
+void	set_fdisk_geom (void);
+void	disp_cur_geom (void);
+int	check_geom (void);
+int 	partsoverlap (int, int);
+void	get_fdisk_info (void);
