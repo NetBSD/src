01429cb0dce4ff39bc21284d9a005468be0fa1f9
phil <phil@NetBSD.org>
1997-09-26 23:02:53 +0000

    Import of sysinst.  Not quite ready, but other ports want access for parallel development.  i386 version about 90% complete.  pc532 version just started, other architectures later in coordination with portmasters.
    This is a curses based, menu driven installation program.
    
    Better error checking is needed.

---
+++ distrib/utils/sysinst/run.c
@@ -0,0 +1,137 @@
+/*	$NetBSD: run.c,v 1.1.1.1 1997/09/26 23:02:54 phil Exp $	*/
+
+/*
+ * Copyright 1997 Piermont Information Systems Inc.
+ * All rights reserved.
+ *
+ * Written by Philip A. Nelson for Piermont Information Systems Inc.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *      This product includes software develooped for the NetBSD Project by
+ *      Piermont Information Systems Inc.
+ * 4. The name of Piermont Information Systems Inc. may not be used to endorse
+ *    or promote products derived from this software without specific prior
+ *    written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY PIERMONT INFORMATION SYSTEMS INC. ``AS IS''
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL PIERMONT INFORMATION SYSTEMS INC. BE 
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF 
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+/* run.c -- routines to interact with other programs. */
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include "defs.h"
+
+#ifdef DEBUG
+#define Xsystem(y) printf ("%s\n", y)
+#else
+#define Xsystem(y) system(y)
+#endif
+
+#define BUFSIZE 4096
+
+int
+collect (int kind, char **buffer, char *name, ...)
+{
+	size_t nbytes;		/* Number of bytes in buffer. */
+	size_t fbytes;		/* Number of bytes in file. */
+	struct stat st;		/* stat information. */
+	int ch;
+	FILE *f;
+	char fileorcmd [STRSIZE];
+	va_list ap;
+
+
+	va_start (ap, name);
+	vsnprintf (fileorcmd, STRSIZE, name, ap);
+	va_end (ap);
+
+	if (kind == T_FILE) {
+		/* Get the file information. */
+		if (stat (fileorcmd, &st)) {
+			*buffer = NULL;
+			return -1;
+		}
+		fbytes = (size_t)st.st_size;
+
+		/* Open the file. */
+		f = fopen (fileorcmd, "r");
+		if (f == NULL) {
+			*buffer = NULL;
+			return -1;
+		}
+
+	} else {
+		/* Open the program. */
+		f = popen (fileorcmd, "r");
+		if (f == NULL) {
+			*buffer = NULL;
+			return -1;
+		}
+		
+		fbytes = BUFSIZE;
+	}
+
+	if (fbytes == 0)
+		fbytes = BUFSIZE;
+	
+	/* Allocate the buffer size. */
+	*buffer = (char *) malloc (fbytes + 1);
+	if (!*buffer) 
+		return -1;
+
+	/* Read the buffer. */
+	nbytes = 0;
+	while (nbytes < fbytes && (ch = fgetc(f)) != EOF)
+		(*buffer)[nbytes++] = ch;
+
+	(*buffer)[nbytes] = 0;
+
+	if (kind == T_FILE)
+		fclose(f);
+	else
+		pclose(f);
+
+	return nbytes;
+}
+
+/* Run a program ... using printf style to format the command! */
+
+int run_prog (char *cmd, ...)
+{
+	char command [STRSIZE];
+	va_list ap;
+	int ret;
+
+	va_start (ap, cmd);
+	(void) vsnprintf (command, STRSIZE, cmd, ap);
+	va_end (ap);
+	/* The following may be more than one function call.  Can't just
+	   "return Xsystem (command);" */
+	ret = Xsystem (command);
+	return ret;
+}
+
---
+++ distrib/utils/sysinst/util.c
@@ -0,0 +1,127 @@
+/*	$NetBSD: util.c,v 1.1.1.1 1997/09/26 23:02:54 phil Exp $	*/
+
+/*
+ * Copyright 1997 Piermont Information Systems Inc.
+ * All rights reserved.
+ *
+ * Written by Philip A. Nelson for Piermont Information Systems Inc.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *      This product includes software develooped for the NetBSD Project by
+ *      Piermont Information Systems Inc.
+ * 4. The name of Piermont Information Systems Inc. may not be used to endorse
+ *    or promote products derived from this software without specific prior
+ *    written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY PIERMONT INFORMATION SYSTEMS INC. ``AS IS''
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL PIERMONT INFORMATION SYSTEMS INC. BE 
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF 
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+/* util.c -- routines that don't really fit anywhere else... */
+
+#include <stdio.h>
+#include <unistd.h>
+#include "defs.h"
+#include "msg_defs.h"
+#include "menu_defs.h"
+
+static int asked = 0;
+
+void ask_sizemult ()
+{
+	if (!asked) {
+		msg_display (MSG_sizechoice, dlcylsize);
+		process_menu (MENU_sizechoice);
+	}
+	asked = 1;
+}
+
+/* Returns 1 for "y" or "Y" and "n" otherwise.  CR => default. */
+int
+ask_ynquestion (char *quest, char def, ...)
+{
+	char line[STRSIZE];
+	va_list ap;
+	char c;
+
+	va_start(ap, def);
+	vsnprintf (line, STRSIZE, quest, ap);
+	va_end(ap);
+
+	printf ("%s [%c]: ", line, def);
+	c = getchar();
+	if (c == '\n')
+		return def == 'y';
+
+	while (getchar() != '\n') /* eat characters */;
+
+	return c == 'y' || c == 'Y';
+}
+
+void
+extract_dist (void)
+{
+	int verbose;
+	int numchar;
+	char *files;
+	char *p;
+
+	msg_display (MSG_verboseextract);
+	process_menu (MENU_noyes);
+	verbose = yesno;
+
+	numchar = collect (T_OUTPUT, &files, "/bin/ls %s/*.tar.gz", dist_dir);
+	if (numchar < 0) {
+		endwin();
+		(void)fprintf (stderr, msg_string(MSG_badls));
+		exit(1);
+	}
+	files[numchar] = '\0';
+
+endwin();
+printf ("Files are %s\n", files);
+getchar();
+
+#ifndef DEBUG
+	if (chdir("/mnt")) {
+		endwin();
+		(void)fprintf(stderr, msg_string(MSG_realdir), "/mnt");
+		exit(1);
+	}
+#else
+	printf ("chdir (%s)\n", "/mnt");
+#endif
+
+	if (verbose)
+		endwin();
+	p = strtok (files, " \n");
+	while (p != NULL) {
+		run_prog ("/usr/bin/tar --unlink -xpz%s -f %s",
+			  verbose ? "v":"", p);
+		p= strtok (NULL, " \n");
+	}
+	if (verbose) {
+		(void)fprintf(stderr, msg_string(MSG_endtar));
+		getchar();
+		puts(CL);
+	}
+}
