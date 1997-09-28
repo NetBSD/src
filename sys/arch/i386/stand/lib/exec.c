943f665a2cd66fb5c9530af81567130df146a089
drochner <drochner@NetBSD.org>
1997-09-28 13:31:45 +0000

    Pass new boot information per default.

--- sys/arch/i386/stand/lib/exec.c
+++ sys/arch/i386/stand/lib/exec.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: exec.c,v 1.4 1997/09/17 18:02:11 drochner Exp $	 */
+/*	$NetBSD: exec.c,v 1.5 1997/09/28 13:31:45 drochner Exp $	 */
 
 /*
  * Copyright (c) 1982, 1986, 1990, 1993
@@ -243,14 +243,13 @@ exec_netbsd(file, loadaddr, boothowto)
 	}
 
 	boot_argv[1] = bootdevnr;
-	boot_argv[2] = 0;	/* cyl offset, unused */
-#else				/* XXX to be specified */
+#else
+	boot_argv[1] = 0;
+#endif
 #ifdef PASS_BIOSGEOM
 	bi_getbiosgeom();
 #endif
-	boot_argv[1] = 0;
-	boot_argv[2] = vtophys(bootinfo);	/* cyl offset */
-#endif
+	boot_argv[2] = vtophys(bootinfo);	/* old cyl offset */
 	/*
 	 * boot_argv[3] = end (set above)
 	 */
