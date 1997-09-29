53cbf130bbdd74e8acff0c033fad06415be3971c
gwr <gwr@NetBSD.org>
1997-09-29 15:37:22 +0000

    Change .o to .c

--- gnu/usr.bin/gdb416/_m68k.mk
+++ gnu/usr.bin/gdb416/_m68k.mk
@@ -1,10 +1,10 @@
-# $NetBSD: _m68k.mk,v 1.1.1.1 1997/09/26 04:37:02 gwr Exp $
+# $NetBSD: _m68k.mk,v 1.2 1997/09/29 15:37:23 gwr Exp $
 
 # From gdb/config/m68k/nbsd.mh
 NM_FILE= config/m68k/nm-nbsd.h
-NDEP_FILES= m68knbsd-nat.o
+NDEP_FILES= m68knbsd-nat.c
 
 # From gdb/config/m68k/nbsd.mt
 TM_FILE= config/m68k/tm-nbsd.h
-TDEP_FILES= m68k-tdep.o solib.o
+TDEP_FILES= m68k-tdep.c solib.c
 
