f029fbb3e108bc82d1d48ec452a3a338b75e8e19
gwr <gwr@NetBSD.org>
1997-09-29 15:37:22 +0000

    Change .o to .c

--- gnu/usr.bin/gdb/_ns32k.mk
+++ gnu/usr.bin/gdb/_ns32k.mk
@@ -1,10 +1,10 @@
-# $NetBSD: _ns32k.mk,v 1.1.1.1 1997/09/26 04:37:02 gwr Exp $
+# $NetBSD: _ns32k.mk,v 1.2 1997/09/29 15:37:24 gwr Exp $
 
 # gdb/config/ns32k/nbsd.mh
 NM_FILE= config/ns32k/nm-nbsd.h
-NDEP_FILES= ns32knbsd-nat.o
+NDEP_FILES= ns32knbsd-nat.c
 
 # gdb/config/ns32k/nbsd.mt
 TM_FILE= config/ns32k/tm-nbsd.h
-TDEP_FILES= ns32k-tdep.o solib.o
+TDEP_FILES= ns32k-tdep.c solib.c
 
--- gnu/usr.bin/gdb416/_i386.mk
+++ gnu/usr.bin/gdb416/_i386.mk
@@ -1,10 +1,10 @@
-# $NetBSD: _i386.mk,v 1.1.1.1 1997/09/26 04:37:02 gwr Exp $
+# $NetBSD: _i386.mk,v 1.2 1997/09/29 15:37:23 gwr Exp $
 
 # From gdb/config/i386/nbsd.mh
 NM_FILE= config/i386/nm-nbsd.h
-NDEP_FILES= i386nbsd-nat.o
+NDEP_FILES= i386nbsd-nat.c
 
 # From gdb/config/i386/nbsd.mt
 TM_FILE= config/i386/tm-nbsd.h
-TDEP_FILES= i386-tdep.o i387-tdep.o solib.o
+TDEP_FILES= i386-tdep.c i387-tdep.c solib.c
 
