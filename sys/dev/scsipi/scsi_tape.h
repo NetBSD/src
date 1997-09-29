d3349ea38553068166d1baf885436d14b7c954e1
mjacob <mjacob@NetBSD.org>
1997-09-29 19:29:02 +0000

    Add SCSI-3 DATA COMPRESSION page structure. Add READ POSITION command structure. Add READ_POSITION and LOCATE opcodes.

--- sys/dev/scsipi/scsi_tape.h
+++ sys/dev/scsipi/scsi_tape.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: scsi_tape.h,v 1.10 1997/08/27 11:26:44 bouyer Exp $	*/
+/*	$NetBSD: scsi_tape.h,v 1.11 1997/09/29 19:29:02 mjacob Exp $	*/
 
 /*
  * Copyright (c) 1994 Charles Hannum.  All rights reserved.
@@ -166,6 +166,25 @@ struct scsi_tape_dev_conf_page {
 	u_int8_t reserved;
 };
 
+/* from SCSI-3: SSC-Rev10 (6/97) */
+struct scsi_tape_dev_compression_page {
+	u_int8_t	pagecode;	/* 0xf */
+	u_int8_t	pagelength;	/* 0xe */
+	u_int8_t	dce_dcc;
+#define	DCP_DCE			0x80	/* enable compression */
+#define	DCP_DCC			0x40	/* compression capable */
+	u_int8_t	dde_red;
+#define	DCP_DDE			0x80	/* enable decompression */
+/* There's a lot of gup about bits 5,6 for reporting exceptions */
+/* in transitions between compressed and uncompressed data- but */
+/* mostly we want the default (0), which is to report a MEDIUM	*/
+/* ERROR when a read transitions into data that can't be de-	*/
+/* compressed */
+	u_int8_t	comp_alg[4];	/* compression algorithm */
+	u_int8_t	decomp_alg[4];	/* de-"" */
+	u_int8_t	reserved[4];
+};
+
 /* defines for the device specific byte in the mode select/sense header */
 #define	SMH_DSP_SPEED		0x0F
 #define	SMH_DSP_BUFF_MODE	0x70
@@ -184,6 +203,16 @@ struct block_desc_cipher {
 #define ST150_SEC		0x01	/* soft error count */
 #define	SR150_AUI		0x02	/* autoload inhibit */
 };
+
+
+#define	READ_POSITION	0x34
+struct scsi_tape_read_position {
+	u_int8_t	opcode;		/* READ_POSITION */
+	u_int8_t	byte1;		/* set LSB to read hardware block pos */
+	u_int8_t	reserved[8];
+};
+
+#define	LOCATE		0x2B
 
 /**********************************************************************
 			from the scsi2 spec
