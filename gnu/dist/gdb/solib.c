f76c2ae303e6dad74d5f65fdc8d3d1a71047fec1
gwr <gwr@NetBSD.org>
1997-09-30 21:38:46 +0000

    Need to include elf/common.h for SVR4_SHARED_LIBS

--- gnu/dist/gdb/solib.c
+++ gnu/dist/gdb/solib.c
@@ -37,6 +37,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
 #include <a.out.h> 
 #else
 #include "elf/external.h"
+#include "elf/common.h"
 #endif
 
 #include <link.h>
