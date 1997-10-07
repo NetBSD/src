4e3b80ee95558d389080352845915978a86a0566
mrg <mrg@NetBSD.org>
1997-10-07 13:44:03 +0000

    WARNS?=1.  RCS ids.

--- libexec/lfs_cleanerd/clean.h
+++ libexec/lfs_cleanerd/clean.h
@@ -1,3 +1,5 @@
+/*	$NetBSD: clean.h,v 1.3 1997/10/07 13:44:03 mrg Exp $	*/
+
 /*-
  * Copyright (c) 1992, 1993
  *	The Regents of the University of California.  All rights reserved.
@@ -31,7 +33,6 @@
  * SUCH DAMAGE.
  *
  *	from: @(#)clean.h	8.1 (Berkeley) 6/4/93
- *	$Id: clean.h,v 1.2 1997/10/07 13:39:56 mrg Exp $
  */
 
 /*
