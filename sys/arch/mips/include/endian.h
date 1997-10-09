2f273a597a9fb838a0cc15f9db39daa86096a6b5
bouyer <bouyer@NetBSD.org>
1997-10-09 15:42:19 +0000

    Add byte-swapping functions (bswap16, bswap32, bswap64) to libkern. Only assembly version for i386 bswap16 and bswap32 for now (bswap64 uses bswap32). Contribution of assembly versions of these are welcome. Add byte-swapping of ext2fs metadata for big-endian systems. Tested on i386 and sparc.

--- sys/arch/mips/include/endian.h
+++ sys/arch/mips/include/endian.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: endian.h,v 1.8 1996/10/13 20:59:02 mhitch Exp $	*/
+/*	$NetBSD: endian.h,v 1.9 1997/10/09 15:42:24 bouyer Exp $	*/
 
 /*
  * Copyright (c) 1987, 1991, 1993
@@ -71,6 +71,9 @@ in_addr_t	htonl __P((in_addr_t));
 in_port_t	htons __P((in_port_t));
 in_addr_t	ntohl __P((in_addr_t));
 in_port_t	ntohs __P((in_port_t));
+u_int16_t	bswap16 __P((u_int16_t));
+u_int32_t	bswap32 __P((u_int32_t));
+u_int64_t	bswap64 __P((u_int64_t));
 __END_DECLS
 
 /*
