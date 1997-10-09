a947a7eff95d4ea862c31f20d65ba5e6fb0f0497
oki <oki@NetBSD.org>
1997-10-09 12:59:23 +0000

    Remove __BDEVSW_DUMP_OLD_TYPE.

--- sys/arch/x68k/include/types.h
+++ sys/arch/x68k/include/types.h
@@ -1,11 +1,10 @@
-/*	$NetBSD: types.h,v 1.2 1996/12/05 00:14:01 cgd Exp $	*/
+/*	$NetBSD: types.h,v 1.3 1997/10/09 12:59:23 oki Exp $	*/
 
 #ifndef _MACHINE_TYPES_H_
 #define _MACHINE_TYPES_H_
 
 #include <m68k/types.h>
 
-#define	__BDEVSW_DUMP_OLD_TYPE
 #define	__BROKEN_INDIRECT_CONFIG
 
 #endif
