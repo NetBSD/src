74f573d85dac036f45f8924a7180044de46ca8e8
mrg <mrg@NetBSD.org>
1997-10-10 05:39:47 +0000

    remove advertising clause from all my licenses.

--- sbin/swapctl/swapctl.c
+++ sbin/swapctl/swapctl.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: swapctl.c,v 1.6 1997/09/15 11:22:00 lukem Exp $	*/
+/*	$NetBSD: swapctl.c,v 1.7 1997/10/10 05:39:53 mrg Exp $	*/
 
 /*
  * Copyright (c) 1996, 1997 Matthew R. Green
@@ -12,11 +12,7 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *      This product includes software developed by Matthew R. Green for
- *      The NetBSD Foundation.
- * 4. The name of the author may not be used to endorse or promote products
+ * 3. The name of the author may not be used to endorse or promote products
  *    derived from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
--- sys/vm/vm_swap.c
+++ sys/vm/vm_swap.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: vm_swap.c,v 1.42 1997/06/17 03:37:07 thorpej Exp $	*/
+/*	$NetBSD: vm_swap.c,v 1.43 1997/10/10 05:40:30 mrg Exp $	*/
 
 /*
  * Copyright (c) 1995, 1996, 1997 Matthew R. Green
@@ -12,11 +12,7 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *      This product includes software developed by Matthew R. Green for
- *      The NetBSD Foundation.
- * 4. The name of the author may not be used to endorse or promote products
+ * 3. The name of the author may not be used to endorse or promote products
  *    derived from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
