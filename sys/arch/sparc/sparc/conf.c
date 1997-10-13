80513cb5ae34f2c5f98367e579888f0547d5aeae
explorer <explorer@NetBSD.org>
1997-10-13 00:46:08 +0000

    o Make usage of /dev/random dependant on   pseudo-device   rnd                     # /dev/random and in-kernel generator   in config files.
    o Add declaration to all architectures.
    
    o Clean up copyright message in rnd.c, rnd.h, and rndpool.c to include
      that this code is derived in part from Ted Tyso's linux code.

--- sys/arch/sparc/sparc/conf.c
+++ sys/arch/sparc/sparc/conf.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: conf.c,v 1.48 1997/01/07 11:35:19 mrg Exp $ */
+/*	$NetBSD: conf.c,v 1.49 1997/10/13 00:46:58 explorer Exp $ */
 
 /*
  * Copyright (c) 1992, 1993
@@ -81,6 +81,7 @@
 #include "xy.h"
 #include "md.h"
 #include "ipfilter.h"
+#include "rnd.h"
 
 struct bdevsw	bdevsw[] =
 {
@@ -233,6 +234,7 @@ struct cdevsw	cdevsw[] =
 	cdev_lkm_dummy(),		/* 116 */
 	cdev_lkm_dummy(),		/* 117 */
 	cdev_lkm_dummy(),		/* 118 */
+	cdev_rnd_init(NRND,rnd),	/* 119: random source pseudo-device */
 };
 int	nchrdev = sizeof(cdevsw) / sizeof(cdevsw[0]);
 
@@ -391,6 +393,7 @@ static int chrtoblktbl[] = {
 	/*116 */	NODEV,
 	/*117 */	NODEV,
 	/*118 */	NODEV,
+	/*119 */	NODEV,
 };
 
 /*
