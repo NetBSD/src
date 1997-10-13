97b09932938e336d5a0604e715ce5f8419547bc7
lukem <lukem@NetBSD.org>
1997-10-13 07:38:09 +0000

    add rcsid

--- usr.sbin/ypserv/yppush/yppush.c
+++ usr.sbin/ypserv/yppush/yppush.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: yppush.c,v 1.5 1997/10/07 14:59:37 lukem Exp $	*/
+/*	$NetBSD: yppush.c,v 1.6 1997/10/13 07:38:09 lukem Exp $	*/
 
 /*
  * Copyright (c) 1995 Mats O Jansson <moj@stacken.kth.se>
@@ -30,6 +30,11 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  */
+
+#include <sys/cdefs.h>
+#ifndef lint
+__RCSID("$NetBSD: yppush.c,v 1.6 1997/10/13 07:38:09 lukem Exp $");
+#endif
 
 #include <sys/types.h>
 #include <sys/stat.h>
