92ff239f67a76ecfd7aea1836459358d778a08f7
mark <mark@NetBSD.org>
1997-10-14 19:00:37 +0000

    arm32 kernel source restructure   - New home for the com driver, (moved from arm32/mainbus) without     and bus specific attachments. (Revision history maintained)

--- sys/arch/arm32/dev/comvar.h
+++ sys/arch/arm32/dev/comvar.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: comvar.h,v 1.2 1997/01/13 00:41:00 mark Exp $	*/
+/*	$NetBSD: comvar.h,v 1.3 1997/10/14 19:00:39 mark Exp $	*/
 
 /*
  * Copyright (c) 1996 Christopher G. Demetriou.  All rights reserved.
@@ -28,15 +28,76 @@
  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *	from: comvar.h,v 1.6 1996/10/21 22:40:34 thorpej Exp
  */
 
-int comprobe1 __P((bus_space_tag_t, bus_space_handle_t, int));
-int comintr __P((void *));
-
-/* For other ports that use 'com' as console (alpha). */
 extern int comconsaddr;
 extern int comconsattached;
 extern bus_space_tag_t comconstag;
-extern bus_space_handle_t comconsbah;
+extern bus_space_handle_t comconsioh;
 extern tcflag_t comconscflag;
-void cominit __P((bus_space_tag_t, bus_space_handle_t, int));
+
+/* Hardware flag masks */
+#define	COM_HW_NOIEN	0x01
+#define	COM_HW_FIFO	0x02
+#define	COM_HW_HAYESP	0x04
+#define	COM_HW_CONSOLE	0x40
+
+/* Buffer size for character buffer */
+#define RXBUFSIZE 2048		/* More than enough.. */
+#define RXBUFMASK (RXBUFSIZE-1)	/* Only iff previous is a power of 2 */
+#define RXHIWAT   (RXBUFSIZE >> 2)
+
+struct com_softc {
+	struct device sc_dev;
+	void *sc_ih;
+	struct tty *sc_tty;
+
+	int sc_overflows;
+	int sc_floods;
+	int sc_errors;
+
+	int sc_iobase;
+
+	bus_space_tag_t sc_iot;
+	bus_space_handle_t sc_ioh;
+	bus_space_handle_t sc_hayespioh;
+
+	u_char sc_hwflags;
+	u_char sc_swflags;
+	int sc_fifolen;
+
+	u_char sc_msr, sc_msr_delta, sc_msr_mask, sc_mcr, sc_mcr_active, sc_lcr,
+	       sc_ier, sc_fifo, sc_dlbl, sc_dlbh;
+	u_char sc_mcr_dtr, sc_mcr_rts, sc_msr_cts, sc_msr_dcd;
+
+	int sc_r_hiwat;
+ 	volatile u_int sc_rbget;
+ 	volatile u_int sc_rbput;
+	volatile u_int sc_rbavail;
+ 	u_char sc_rbuf[RXBUFSIZE];
+	u_char sc_lbuf[RXBUFSIZE];
+
+ 	u_char *sc_tba;
+ 	int sc_tbc,
+	    sc_heldtbc;
+
+	volatile u_char sc_rx_blocked,
+			sc_tx_busy,
+			sc_tx_done,
+			sc_tx_stopped,
+			sc_st_check,
+			sc_rx_ready;
+
+	volatile u_char sc_heldchange;
+};
+
+/* Macros to clear/set/test flags. */
+#define SET(t, f)	(t) |= (f)
+#define CLR(t, f)	(t) &= ~(f)
+#define ISSET(t, f)	((t) & (f))
+
+int comprobe1 __P((bus_space_tag_t, bus_space_handle_t, int));
+int comintr __P((void *));
+void com_attach_subr __P((struct com_softc *));
