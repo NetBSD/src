edf79539db32001dc7be365693368147beb9f638
augustss <augustss@NetBSD.org>
1997-10-15 05:13:31 +0000

    Add another SB AWE64 Value.

--- sys/dev/isapnp/sb_isapnp.c
+++ sys/dev/isapnp/sb_isapnp.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: sb_isapnp.c,v 1.10 1997/09/13 13:37:28 augustss Exp $	*/
+/*	$NetBSD: sb_isapnp.c,v 1.11 1997/10/15 05:13:31 augustss Exp $	*/
 
 /*
  * Copyright (c) 1991-1993 Regents of the University of California.
@@ -87,6 +87,7 @@ sb_isapnp_match(parent, match, aux)
 	    strcmp(ipa->ipa_devlogic, "CTL0041") && /* SB16 PnP (CT4131) */
 	    strcmp(ipa->ipa_devlogic, "CTL0042") && /* SB AWE64 Value */
 	    strcmp(ipa->ipa_devlogic, "CTL0044") && /* SB AWE64 Gold */
+	    strcmp(ipa->ipa_devlogic, "CTL0045") && /* SB AWE64 Value */
 	    strcmp(ipa->ipa_devlogic, "ESS1868") &&
 	    strcmp(ipa->ipa_devcompat, "PNPB000") && /* generic SB 1.5 */
 	    strcmp(ipa->ipa_devcompat, "PNPB001") && /* generic SB 2.0 */
