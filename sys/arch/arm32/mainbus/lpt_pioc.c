2590cd11797be34fc561c6690e1668c8caa9178a
mark <mark@NetBSD.org>
1997-10-16 18:35:53 +0000

    Updated to use the MI dev/ic/lpt.c driver.

--- sys/arch/arm32/mainbus/lpt_pioc.c
+++ sys/arch/arm32/mainbus/lpt_pioc.c
@@ -1,8 +1,8 @@
-/*	$NetBSD: lpt_pioc.c,v 1.1 1997/10/14 19:57:40 mark Exp $	*/
+/*	$NetBSD: lpt_pioc.c,v 1.2 1997/10/16 18:35:53 mark Exp $	*/
 
 /*
- * Copyright (c) 1993, 1994 Charles Hannum.
- * Copyright (c) 1990 William F. Jolitz, TeleMuse
+ * Copyright (c) 1997 Mark Brinicombe
+ * Copyright (c) 1997 Causality Limited
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -13,36 +13,17 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This software is a component of "386BSD" developed by 
- *	William F. Jolitz, TeleMuse.
- * 4. Neither the name of the developer nor the name "386BSD"
- *    may be used to endorse or promote products derived from this software
- *    without specific prior written permission.
+ * 3. The name of the company nor the name of the author may be used to
+ *    endorse or promote products derived from this software without specific
+ *    prior written permission.
  *
- * THIS SOFTWARE IS A COMPONENT OF 386BSD DEVELOPED BY WILLIAM F. JOLITZ 
- * AND IS INTENDED FOR RESEARCH AND EDUCATIONAL PURPOSES ONLY. THIS 
- * SOFTWARE SHOULD NOT BE CONSIDERED TO BE A COMMERCIAL PRODUCT. 
- * THE DEVELOPER URGES THAT USERS WHO REQUIRE A COMMERCIAL PRODUCT 
- * NOT MAKE USE OF THIS WORK.
- *
- * FOR USERS WHO WISH TO UNDERSTAND THE 386BSD SYSTEM DEVELOPED
- * BY WILLIAM F. JOLITZ, WE RECOMMEND THE USER STUDY WRITTEN 
- * REFERENCES SUCH AS THE  "PORTING UNIX TO THE 386" SERIES 
- * (BEGINNING JANUARY 1991 "DR. DOBBS JOURNAL", USA AND BEGINNING 
- * JUNE 1991 "UNIX MAGAZIN", GERMANY) BY WILLIAM F. JOLITZ AND 
- * LYNNE GREER JOLITZ, AS WELL AS OTHER BOOKS ON UNIX AND THE 
- * ON-LINE 386BSD USER MANUAL BEFORE USE. A BOOK DISCUSSING THE INTERNALS 
- * OF 386BSD ENTITLED "386BSD FROM THE INSIDE OUT" WILL BE AVAILABLE LATE 1992.
- *
- * THIS SOFTWARE IS PROVIDED BY THE DEVELOPER ``AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE DEVELOPER BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTERS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
@@ -59,9 +40,9 @@
 #include <sys/device.h>
 #include <machine/bus.h>
 #include <machine/irqhandler.h>
-#include <arm32/dev/lptreg.h>
-#include <arm32/dev/lptvar.h>
 #include <arm32/mainbus/piocvar.h>
+#include <dev/ic/lptreg.h>
+#include <dev/ic/lptvar.h>
 
 #include "locators.h"
 
@@ -75,6 +56,101 @@ static void lpt_pioc_attach __P((struct device *, struct device *, void *));
 struct cfattach lpt_pioc_ca = {
 	sizeof(struct lpt_softc), lpt_pioc_probe, lpt_pioc_attach
 };
+
+/*
+ * Internal routine to lptprobe to do port tests of one byte value.
+ */
+static int
+lpt_port_test(iot, ioh, base, off, data, mask)
+	bus_space_tag_t iot;
+	bus_space_handle_t ioh;
+	bus_addr_t base;
+	bus_size_t off;
+	u_char data, mask;
+{
+	int timeout;
+	u_char temp;
+
+	data &= mask;
+	bus_space_write_1(iot, ioh, off, data);
+	timeout = 1000;
+	do {
+		delay(10);
+		temp = bus_space_read_1(iot, ioh, off) & mask;
+	} while (temp != data && --timeout);
+	return (temp == data);
+}
+
+/*
+ * Logic:
+ *	1) You should be able to write to and read back the same value
+ *	   to the data port.  Do an alternating zeros, alternating ones,
+ *	   walking zero, and walking one test to check for stuck bits.
+ *
+ *	2) You should be able to write to and read back the same value
+ *	   to the control port lower 5 bits, the upper 3 bits are reserved
+ *	   per the IBM PC technical reference manauls and different boards
+ *	   do different things with them.  Do an alternating zeros, alternating
+ *	   ones, walking zero, and walking one test to check for stuck bits.
+ *
+ *	   Some printers drag the strobe line down when the are powered off
+ * 	   so this bit has been masked out of the control port test.
+ *
+ *	   XXX Some printers may not like a fast pulse on init or strobe, I
+ *	   don't know at this point, if that becomes a problem these bits
+ *	   should be turned off in the mask byte for the control port test.
+ *
+ *	3) Set the data and control ports to a value of 0
+ */
+static int
+lptprobe(iot, iobase)
+	bus_space_tag_t iot;
+	u_int iobase;
+{
+	bus_space_handle_t ioh;
+	u_char mask, data;
+	int i, rv;
+#ifdef DEBUG
+#define	ABORT	do {printf("lptprobe: mask %x data %x failed\n", mask, data); \
+		    goto out;} while (0)
+#else
+#define	ABORT	goto out
+#endif
+
+	if (bus_space_map(iot, iobase, LPT_NPORTS, 0, &ioh))
+		return 0;
+	rv = 0;
+	mask = 0xff;
+
+	data = 0x55;				/* Alternating zeros */
+	if (!lpt_port_test(iot, ioh, iobase, lpt_data, data, mask))
+		ABORT;
+
+	data = 0xaa;				/* Alternating ones */
+	if (!lpt_port_test(iot, ioh, iobase, lpt_data, data, mask))
+		ABORT;
+
+	for (i = 0; i < CHAR_BIT; i++) {	/* Walking zero */
+		data = ~(1 << i);
+		if (!lpt_port_test(iot, ioh, iobase, lpt_data, data, mask))
+			ABORT;
+	}
+
+	for (i = 0; i < CHAR_BIT; i++) {	/* Walking one */
+		data = (1 << i);
+		if (!lpt_port_test(iot, ioh, iobase, lpt_data, data, mask))
+			ABORT;
+	}
+
+	bus_space_write_1(iot, ioh, lpt_data, 0);
+	bus_space_write_1(iot, ioh, lpt_control, 0);
+
+	rv = LPT_NPORTS;
+
+out:
+	bus_space_unmap(iot, ioh, LPT_NPORTS);
+	return rv;
+}
 
 /*
  * int lpt_pioc_probe(struct device *parent, struct cfdata *cf, void *aux)
@@ -123,24 +199,21 @@ lpt_pioc_attach(parent, self, aux)
 	struct pioc_attach_args *pa = aux;
 	bus_space_tag_t iot;
 	bus_space_handle_t ioh;
+	u_int iobase;
 
-	if (pa->pa_irq != MAINBUSCF_IRQ_DEFAULT) {
+	if (pa->pa_irq != MAINBUSCF_IRQ_DEFAULT)
 		printf("\n");
-		sc->sc_irq = pa->pa_irq;
-	} else {
+	else
 		printf(": polled\n");
-		sc->sc_irq = LPT_NOIRQ;
-	}
 
-	sc->sc_iobase = pa->pa_iobase + pa->pa_offset;
-	sc->sc_state = 0;
+	iobase = pa->pa_iobase + pa->pa_offset;
 
 	iot = sc->sc_iot = pa->pa_iot;
-	if (bus_space_map(iot, sc->sc_iobase, LPT_NPORTS, 0, &ioh))
+	if (bus_space_map(iot, iobase, LPT_NPORTS, 0, &ioh))
 		panic("lptattach: couldn't map I/O ports");
 	sc->sc_ioh = ioh;
 
-	bus_space_write_1(iot, ioh, lpt_control, LPC_NINIT);
+	lpt_attach_subr(sc);
 
 	if (pa->pa_irq != MAINBUSCF_IRQ_DEFAULT)
 		sc->sc_ih = intr_claim(pa->pa_irq, IPL_TTY, "lpt",
