530857cbc14d17f5decaf1863fda0385701fb2e6
jonathan <jonathan>
1997-10-17 04:43:57 +0000

    Add bi-endian support to mips locore, <mips/endian.h>, and mips_opcode.h. Derived from a change request (PR port-mips/4277) from Tsubai Masanari, (tsubai@iri.co.jp).

--- sys/arch/pmax/include/endian.h
+++ sys/arch/pmax/include/endian.h
@@ -1,3 +1,4 @@
-/*	$NetBSD: endian.h,v 1.5 1996/03/19 03:06:28 jonathan Exp $	*/
+/*	$NetBSD: endian.h,v 1.6 1997/10/17 04:44:22 jonathan Exp $	*/
 
+#define BYTE_ORDER LITTLE_ENDIAN
 #include <mips/endian.h>
