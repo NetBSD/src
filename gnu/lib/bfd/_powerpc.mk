f2bea0bb9f7a1f8dd87a3c1213199cbaf32d46d3
gwr <gwr@NetBSD.org>
1997-10-17 19:18:59 +0000

    Take a stab at this.

---
+++ gnu/lib/bfd/_powerpc.mk
@@ -0,0 +1,14 @@
+# $NetBSD: _powerpc.mk,v 1.1 1997/10/17 19:18:59 gwr Exp $
+
+BFD_MACHINES =	cpu-powerpc.c
+BFD_BACKENDS =	elf32-ppc.c elf32.c elf.c elflink.c
+
+TDEFAULTS = \
+ -DSELECT_ARCHITECTURES='&bfd_powerpc_arch' \
+ -DDEFAULT_VECTOR=bfd_elf32_powerpc_vec \
+ -DSELECT_VECS=' &bfd_elf32_powerpc_vec, &bfd_elf32_powerpcle_vec ' \
+ -DHAVE_bfd_elf32_powerpc_vec \
+ -DHAVE_bfd_elf32_powerpcle_vec \
+ -DNETBSD_CORE
+
+OPCODE_MACHINES =  ppc-dis.c ppc-opc.c
