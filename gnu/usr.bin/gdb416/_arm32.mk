d3d951c9ddb5f25213dd170b960aa44c969b65fc
gwr <gwr@NetBSD.org>
1997-10-18 18:57:59 +0000

    This was temporary - now in ../gdb

--- gnu/usr.bin/gdb416/_arm32.mk
+++
@@ -1,9 +0,0 @@
-# $NetBSD: _arm32.mk,v 1.1 1997/10/17 21:30:12 gwr Exp $
-
-# From gdb/config/arm/arm.mh
-NM_FILE= config/nm-nbsd.h
-NDEP_FILES= arm-xdep.c arm-convert.s
-
-# From gdb/config/arm/arm.mt
-TM_FILE= config/arm/tm-arm.h
-TDEP_FILES= arm-tdep.c solib.c
--- gnu/usr.bin/gdb416/_mips.mk
+++
@@ -1,9 +0,0 @@
-# $NetBSD: _mips.mk,v 1.1 1997/10/17 21:30:07 gwr Exp $
-
-# From gdb/config/mips/decstation.mh
-NM_FILE= config/mips/nm-mips.h
-NDEP_FILES= mips-nat.c
-
-# From gdb/config/mips/decstation.mt
-TM_FILE= config/mips/tm-mips.h
-TDEP_FILES= mips-tdep.c solib.c
