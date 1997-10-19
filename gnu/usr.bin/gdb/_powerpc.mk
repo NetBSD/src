f29ae269ba096383ee280467067d6c597ee8aba1
gwr <gwr@NetBSD.org>
1997-10-19 04:31:55 +0000

    Leave out kcore-nbsd.c on these for now

--- gnu/usr.bin/gdb/_powerpc.mk
+++ gnu/usr.bin/gdb/_powerpc.mk
@@ -1,4 +1,4 @@
-# $NetBSD: _powerpc.mk,v 1.1 1997/10/17 21:30:18 gwr Exp $
+# $NetBSD: _powerpc.mk,v 1.2 1997/10/19 04:31:58 gwr Exp $
 
 # From gdb/config/powerpc/nbsd.mh
 NM_FILE= config/powerpc/nm-nbsd.h
@@ -7,3 +7,6 @@ NDEP_FILES= rs6000-nat.c
 # From gdb/config/powerpc/nbsd.mt
 TM_FILE= config/powerpc/tm-nbsd.h
 TDEP_FILES= rs6000-tdep.c solib.c
+
+# Override this for now, to ommit: kcore-nbsd.c
+NDEP_CMN= infptrace.c inftarg.c fork-child.c corelow.c
