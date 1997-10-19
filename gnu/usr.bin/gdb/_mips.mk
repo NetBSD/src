d282ff3fd40bb511baaef8c7bff2c0313cce706c
gwr <gwr@NetBSD.org>
1997-10-19 04:20:54 +0000

    Use config/mips/?m-nbsd.h

--- gnu/usr.bin/gdb/_mips.mk
+++ gnu/usr.bin/gdb/_mips.mk
@@ -1,9 +1,9 @@
-# $NetBSD: _mips.mk,v 1.1 1997/10/17 21:30:07 gwr Exp $
+# $NetBSD: _mips.mk,v 1.2 1997/10/19 04:20:54 gwr Exp $
 
-# From gdb/config/mips/decstation.mh
-NM_FILE= config/mips/nm-mips.h
+# From gdb/config/mips/nbsd.mh
+NM_FILE= config/mips/nm-nbsd.h
 NDEP_FILES= mips-nat.c
 
-# From gdb/config/mips/decstation.mt
-TM_FILE= config/mips/tm-mips.h
+# From gdb/config/mips/nbsd.mt
+TM_FILE= config/mips/tm-nbsd.h
 TDEP_FILES= mips-tdep.c solib.c
