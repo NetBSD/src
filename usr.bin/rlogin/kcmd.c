578aa0bafc90ac1556365b2cacd88325100f0693
lukem <lukem@NetBSD.org>
1997-10-19 14:10:27 +0000

    WARNSify, fix .Nm usage, deprecate register, getopt returns -1 not EOF

--- usr.bin/rlogin/kcmd.c
+++ usr.bin/rlogin/kcmd.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: kcmd.c,v 1.4 1997/06/28 23:43:37 tls Exp $	*/
+/*	$NetBSD: kcmd.c,v 1.5 1997/10/19 14:10:27 lukem Exp $	*/
 
 /*
  * Copyright (c) 1983, 1993
@@ -38,7 +38,7 @@
 static char Xsccsid[] = "derived from @(#)rcmd.c 5.17 (Berkeley) 6/27/88";
 static char sccsid[] = "@(#)kcmd.c	8.2 (Berkeley) 8/19/93";
 #else
-static char rcsid[] = "$NetBSD: kcmd.c,v 1.4 1997/06/28 23:43:37 tls Exp $";
+__RCSID("$NetBSD: kcmd.c,v 1.5 1997/10/19 14:10:27 lukem Exp $");
 #endif
 #endif /* not lint */
 
