841a599be621619f9940ce99efc76f10a0583120
mrg <mrg@NetBSD.org>
1997-10-19 05:50:26 +0000

    merge lite-2.

--- usr.bin/ranlib/pathnames.h
+++ usr.bin/ranlib/pathnames.h
@@ -1,8 +1,8 @@
-/*	$NetBSD: pathnames.h,v 1.4 1997/01/09 20:20:57 tls Exp $	*/
+/*	$NetBSD: pathnames.h,v 1.5 1997/10/19 05:50:30 mrg Exp $	*/
 
 /*-
- * Copyright (c) 1990 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1990, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -32,8 +32,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	from: @(#)pathnames.h	5.2 (Berkeley) 4/16/91
- *	$NetBSD: pathnames.h,v 1.4 1997/01/09 20:20:57 tls Exp $
+ *	from: @(#)pathnames.h	8.1 (Berkeley) 6/6/93
  */
 
 #define _NAME_RANTMP	"ranlib.XXXXXX"
