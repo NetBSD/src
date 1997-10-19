0abdfcdfd542bad7958913cce57e958ddca60146
gwr <gwr@NetBSD.org>
1997-10-19 04:31:55 +0000

    Leave out kcore-nbsd.c on these for now

--- gnu/usr.bin/gdb/_arm32.mk
+++ gnu/usr.bin/gdb/_arm32.mk
@@ -1,4 +1,4 @@
-# $NetBSD: _arm32.mk,v 1.1 1997/10/17 21:30:12 gwr Exp $
+# $NetBSD: _arm32.mk,v 1.2 1997/10/19 04:31:55 gwr Exp $
 
 # From gdb/config/arm/arm.mh
 NM_FILE= config/nm-nbsd.h
@@ -7,3 +7,6 @@ NDEP_FILES= arm-xdep.c arm-convert.s
 # From gdb/config/arm/arm.mt
 TM_FILE= config/arm/tm-arm.h
 TDEP_FILES= arm-tdep.c solib.c
+
+# Override this for now, to ommit: kcore-nbsd.c
+NDEP_CMN= infptrace.c inftarg.c fork-child.c corelow.c
--- gnu/usr.bin/gdb/_mips.mk
+++ gnu/usr.bin/gdb/_mips.mk
@@ -1,4 +1,4 @@
-# $NetBSD: _mips.mk,v 1.2 1997/10/19 04:20:54 gwr Exp $
+# $NetBSD: _mips.mk,v 1.3 1997/10/19 04:31:57 gwr Exp $
 
 # From gdb/config/mips/nbsd.mh
 NM_FILE= config/mips/nm-nbsd.h
@@ -7,3 +7,6 @@ NDEP_FILES= mips-nat.c
 # From gdb/config/mips/nbsd.mt
 TM_FILE= config/mips/tm-nbsd.h
 TDEP_FILES= mips-tdep.c solib.c
+
+# Override this for now, to ommit: kcore-nbsd.c
+NDEP_CMN= infptrace.c inftarg.c fork-child.c corelow.c
