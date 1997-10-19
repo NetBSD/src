a01c11a505bd59a812f7da3f815649ad708c0f02
jonathan <jonathan@NetBSD.org>
1997-10-19 20:19:07 +0000

    Put back kcore-nsbd.c, it builds. Use our own mipsnbsd-nat.c.

--- gnu/usr.bin/gdb/_mips.mk
+++ gnu/usr.bin/gdb/_mips.mk
@@ -1,12 +1,9 @@
-# $NetBSD: _mips.mk,v 1.3 1997/10/19 04:31:57 gwr Exp $
+# $NetBSD: _mips.mk,v 1.4 1997/10/19 20:19:07 jonathan Exp $
 
 # From gdb/config/mips/nbsd.mh
 NM_FILE= config/mips/nm-nbsd.h
-NDEP_FILES= mips-nat.c
+NDEP_FILES= mipsnbsd-nat.c
 
 # From gdb/config/mips/nbsd.mt
 TM_FILE= config/mips/tm-nbsd.h
 TDEP_FILES= mips-tdep.c solib.c
-
-# Override this for now, to ommit: kcore-nbsd.c
-NDEP_CMN= infptrace.c inftarg.c fork-child.c corelow.c
