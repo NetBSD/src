0111d8d3ef6642618bf421c1da634851aafa2d97
oki <oki@NetBSD.org>
1997-10-19 11:00:51 +0000

    New bootblock code (compressed kernel support).  mostly written by ITOH Yasufumi.

---
+++ sys/arch/x68k/stand/xxboot.h
@@ -0,0 +1,22 @@
+/***************************************************************
+ *
+ *	file: boot.h
+ *
+ *	author: chapuni(GBA02750@niftyserve.or.jp)
+ *
+ */
+
+extern volatile void IPLERR __P((void));
+extern int RAW_READ __P((void *buf, int pos, size_t length));
+extern unsigned B_KEYINP __P((void));
+extern void B_CLR_ST __P((unsigned x));
+extern void B_PUTC __P((unsigned c));
+extern void B_PRINT __P((const unsigned char *p));
+extern unsigned B_COLOR __P((unsigned w));
+extern unsigned B_LOCATE __P((unsigned x, unsigned y));
+extern unsigned JISSFT __P((unsigned c));
+extern unsigned short B_SFTSNS __P((void));
+extern int SYS_STAT __P((int flags));
+extern int getcpu __P((void));
+
+/* eof */
