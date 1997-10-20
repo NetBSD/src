117a0c998a6377137b84bc1ac633af9b32d7d1d1
scottr <scottr>
1997-10-20 08:13:26 +0000

    Convert to MI 8530 SCC driver, from Bill Studenmund.  The new front end also supports DTR/CTS flow control.

--- sys/arch/mac68k/include/z8530var.h
+++ sys/arch/mac68k/include/z8530var.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: z8530var.h,v 1.4 1996/10/13 03:21:32 christos Exp $	*/
+/*	$NetBSD: z8530var.h,v 1.5 1997/10/20 08:14:04 scottr Exp $	*/
 
 /*
  * Copyright (c) 1994 Gordon W. Ross
@@ -45,14 +45,71 @@
  *	@(#)zsvar.h	8.1 (Berkeley) 6/11/93
  */
 
-#include <arch/mac68k/dev/z8530sc.h>
-#include <arch/mac68k/dev/z8530tty.h>
+#include <dev/ic/z8530sc.h>
+
+/*
+ * Clock source info structure, added here so xzs_chanstate works
+ */
+struct zsclksrc {
+	long    clk;    /* clock rate, in MHz, present on signal line */
+	int     flags;  /* Specifies how this source can be used
+			   (RTxC divided, RTxC BRG, PCLK BRG, TRxC divided)
+			   and also if the source is "external" and if it
+			   is changeable (by an ioctl ex.). The
+			   source usage flags are used by the tty
+			   child. The other bits tell zsloadchannelregs
+			   if it should call an md signal source
+			   changing routine. ZSC_VARIABLE says if
+			   an ioctl should be able to cahnge the
+			   clock rate.*/
+};
+#define ZSC_PCLK        0x01
+#define ZSC_RTXBRG      0x02
+#define ZSC_RTXDIV      0x04
+#define ZSC_TRXDIV      0x08
+#define ZSC_VARIABLE    0x40
+#define ZSC_EXTERN      0x80
+
+#define ZSC_BRG         0x03
+#define ZSC_DIV         0x0c
+
+
+/*
+ * These are the machine-dependent (extended) variants of
+ * struct zs_chanstate and struct zsc_softc
+ */
+struct xzs_chanstate {
+	/* machine-independent part (First!)*/
+	struct zs_chanstate xzs_cs;
+	/* machine-dependent extensions */
+	int cs_hwflags;
+	int	cs_chip;		/* type of chip */
+	/* Clock source info... */
+	int	cs_clock_count;		/* how many signal sources available */
+	struct zsclksrc cs_clocks[4];	/* info on available signal sources */
+	long	cs_cclk_flag;		/* flag for current clock source */
+	long	cs_pclk_flag;		/* flag for pending clock source */
+	int	cs_csource;		/* current source # */
+	int	cs_psource;		/* pending source # */
+};
+
+struct zsc_softc {
+	struct	device zsc_dev;		/* required first: base device */
+	struct	zs_chanstate *zsc_cs[2];	/* channel A and B soft state */
+	/* Machine-dependent part follows... */
+	struct xzs_chanstate xzsc_xcs_store[2];
+};
 
 /*
  * Functions to read and write individual registers in a channel.
  * The ZS chip requires a 1.6 uSec. recovery time between accesses,
  * and the Sun3 hardware does NOT take care of this for you.
  * MacII hardware DOES dake care of the delay for us. :-)
+ * XXX - Then these should be inline functions! -gwr
+ * Some clock-chirped macs loose serial ports. It could be that the
+ * hardware delay is tied to the CPU speed, and that the minimum delay
+ * no longer's respected. For them, ZS_DELAY might help.
+ * XXX - no one seems to want to try and check this -wrs
  */
 
 u_char zs_read_reg __P((struct zs_chanstate *cs, u_char reg));
@@ -63,48 +120,15 @@ void  zs_write_reg __P((struct zs_chanstate *cs, u_char reg, u_char val));
 void  zs_write_csr __P((struct zs_chanstate *cs, u_char val));
 void  zs_write_data __P((struct zs_chanstate *cs, u_char val));
 
-/*
- * abort detection on console will now timeout after iterating on a loop
- * the following # of times. Cheep hack. Also, abort detection is turned
- * off after a timeout (i.e. maybe there's not a terminal hooked up).
- */
-#define ZSABORT_DELAY 3000000
-
-/*
- * How to request a "soft" interrupt.
- * This could be a macro if you like.
- */
-void zsc_req_softint __P((struct zsc_softc *zsc));
-
-/* Handle user request to enter kernel debugger. */
-void	zs_abort __P((struct zstty_softc *zst));
+/* XXX - Could define splzs() here instead of in psl.h */
 
 /* Hook for MD ioctl support */
-int	zsmdioctl __P((struct tty *tp, u_long com, caddr_t data, int flag,
-	    struct proc *p));
-
-/* Clean up at end of tty attach */
-void zstty_mdattach __P((struct zsc_softc *zsc, struct zstty_softc *zst,
-	    struct zs_chanstate *cs, struct tty *tp));  
+int	zsmdioctl __P((struct zs_chanstate *cs, u_long cmd, caddr_t data));
+/* XXX - This is a bit gross... */
+#define ZS_MD_IOCTL zsmdioctl(cs, cmd, data)
 
 /* Callback for "external" clock sources */
 void zsmd_setclock  __P((struct zs_chanstate *cs));
+#define ZS_MD_SETCLK(cs) zsmd_setclock(cs)
 
-/*
- * Some warts needed by z8530tty.c -
- */
-#define	ZSTTY_MAJOR 	12		/* XXX */
-#undef	ZSTTY_DEF_CFLAG
-#define	ZSTTY_DEF_CFLAG 	(CREAD | CS8 | HUPCL)
 
-#define	ZSTTY_RAW_CFLAG (CS8 | CREAD | HUPCL )
-#define	ZSTTY_RAW_IFLAG (IXANY | IMAXBEL)
-#define	ZSTTY_RAW_LFLAG (ECHOE|ECHOKE|ECHOCTL)
-#define	ZSTTY_RAW_OFLAG (ONLCR | OXTABS)
-/* Above taken from looking at a tty after a stty raw */
-
-/* Booter flags interface */
-#define ZSMAC_RAW	0x01
-#define ZSMAC_LOCALTALK	0x02
-
-#define zsprintf printf
