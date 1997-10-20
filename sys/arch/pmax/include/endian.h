ccefa935178c7e527719e8590948f7785726695f
jonathan <jonathan@NetBSD.org>
1997-10-20 09:57:05 +0000

    * Use ANSI-clean names  for host-specific byte-order definition   (_BYTE_ORDER, _BIG_ENDIAN, _LITTLE_ENDIAN).   Define old names from the ANSI ones if not _POSIX_SOURCE. * Define _QUAD_HIGHWORD and _QUAD_LOWWORD properly when   _BYTE_ORDER == _BIG_ENDIAN.

--- sys/arch/pmax/include/endian.h
+++ sys/arch/pmax/include/endian.h
@@ -1,4 +1,7 @@
-/*	$NetBSD: endian.h,v 1.6 1997/10/17 04:44:22 jonathan Exp $	*/
+/*	$NetBSD: endian.h,v 1.7 1997/10/20 09:57:17 jonathan Exp $	*/
 
-#define BYTE_ORDER LITTLE_ENDIAN
+/*
+ * Define the order of bytes in a 32-bit word.
+ */
+#define _BYTE_ORDER _LITTLE_ENDIAN
 #include <mips/endian.h>
