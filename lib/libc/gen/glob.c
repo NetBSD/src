922c9b44c22a08bb059b8991fe20843ccef58a7a
thorpej <thorpej>
1997-10-22 06:41:23 +0000

    Pull up from trunk:
    New hacks to make libc work painlessly without bumping the major number:
    use type func(arg1s) asm("emitted_name") gcc mechanism.
    Suggested by Bill Sommerfeld.
    
    Allow the old-name functions to be compiled from the same source file as
    the renamed functions, and add some #error directives to force the
    issue with weak symbols for renamed functions when we support weak
    symbols in the C library.

---
+++ lib/libc/gen/glob.c
@@ -0,0 +1,10 @@
+/*	$NetBSD: glob.c,v 1.8.2.2 1997/10/22 06:41:32 thorpej Exp $	*/
+
+/*
+ * Written by Jason R. Thorpe <thorpej@NetBSD.ORG>, October 21, 1997.
+ * Public domain.
+ */
+
+#define __LIBC12_SOURCE__
+
+#include "__glob13.c"
