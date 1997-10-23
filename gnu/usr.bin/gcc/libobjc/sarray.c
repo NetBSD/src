14f2dd2071a2cc381f4bbddd541b388867e0aa55
mellon <mellon@NetBSD.org>
1997-10-23 00:57:34 +0000

    Pull rev 1.2 up from main trunk

--- gnu/usr.bin/gcc/libobjc/sarray.c
+++ gnu/usr.bin/gcc/libobjc/sarray.c
@@ -24,7 +24,7 @@ Boston, MA 02111-1307, USA.  */
    This exception does not however invalidate any other reasons why
    the executable file might be covered by the GNU General Public License.  */
 
-#include "sarray.h"
+#include "objc/sarray.h"
 #include <stdio.h>
 #include "assert.h"
 
