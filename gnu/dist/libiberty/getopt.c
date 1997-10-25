424aa6f888bdc833e6b77840b29dbf59ceff2f15
thorpej <thorpej@NetBSD.org>
1997-10-25 01:58:20 +0000

    Pull up from trunk:
    Pull in <string.h> to get prototypes so that this builds on the Alpha.

--- gnu/dist/libiberty/getopt.c
+++ gnu/dist/libiberty/getopt.c
@@ -54,6 +54,8 @@ the executable file might be covered by the GNU General Public License. */
 
 #include <stdio.h>
 
+#include <string.h>
+
 /* Comment out all this code if we are using the GNU C Library, and are not
    actually compiling the library itself.  This code is part of the GNU C
    Library, but also included in many other GNU distributions.  Compiling
