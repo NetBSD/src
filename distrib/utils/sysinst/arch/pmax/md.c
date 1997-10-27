0c8d4acbc44fbe831dc7c366f03021bd97ebca7c
jonathan <jonathan@NetBSD.org>
1997-10-27 03:34:32 +0000

    sysinst support for pmax, pass 0.

---
+++ distrib/utils/sysinst/arch/pmax/md.c
@@ -0,0 +1,222 @@
+/*	$NetBSD: md.c,v 1.1 1997/10/27 03:34:33 jonathan Exp $	*/
+
+/*
+ * Copyright 1997 Piermont Information Systems Inc.
+ * All rights reserved.
+ *
+ * Written by Philip A. Nelson for Piermont Information Systems Inc.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *      This product includes software develooped for the NetBSD Project by
+ *      Piermont Information Systems Inc.
+ * 4. The name of Piermont Information Systems Inc. may not be used to endorse
+ *    or promote products derived from this software without specific prior
+ *    written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY PIERMONT INFORMATION SYSTEMS INC. ``AS IS''
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL PIERMONT INFORMATION SYSTEMS INC. BE 
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF 
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+/* md.c -- pc532 machine specific routines */
+
+#include <stdio.h>
+#include <curses.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/disklabel.h>
+#include <sys/ioctl.h>
+#include "defs.h"
+#include "md.h"
+#include "msg_defs.h"
+#include "menu_defs.h"
+
+int	md_get_info (void)
+{	struct disklabel disklabel;
+	int fd;
+	char devname[100];
+
+	snprintf (devname, 100, "/dev/r%sc", diskdev);
+
+	fd = open (devname, O_RDONLY, 0);
+	if (fd < 0) {
+		endwin();
+		fprintf (stderr, "Can't open %s\n", devname);
+		exit(1);
+	}
+	if (ioctl(fd, DIOCGDINFO, &disklabel) == -1) {
+		endwin();
+		fprintf (stderr, "Can't read disklabel on %s.\n", devname);
+		close(fd);
+		exit(1);
+	}
+	close(fd);
+ 
+	dlcyl = disklabel.d_ncylinders;
+	dlhead = disklabel.d_ntracks;
+	dlsec = disklabel.d_nsectors;
+	sectorsize = disklabel.d_secsize;
+	dlcylsize = disklabel.d_secpercyl;
+	dlsize = dlcyl*dlhead*dlsec;
+
+	/* Compute minimum NetBSD partition sizes (in sectors). */
+	minfsdmb = (80 + 4*rammb) * (MEG / sectorsize);
+
+	return 1;
+}
+
+void	md_pre_disklabel (void)
+{
+}
+
+void	md_post_disklabel (void)
+{
+}
+
+void	md_post_newfs (void)
+{
+}
+
+void	md_copy_filesystem (void)
+{
+}
+
+void md_make_bsd_partitions (void)
+{
+	FILE *f;
+	int i;
+
+	/* Ask for layout type -- standard or special */
+	msg_display (MSG_layout,
+			(1.0*fsptsize*sectorsize)/MEG,
+			(1.0*minfsdmb*sectorsize)/MEG,
+			(1.0*minfsdmb*sectorsize)/MEG+rammb+XNEEDMB);
+	process_menu (MENU_layout);
+
+	if (layoutkind == 3) {
+		ask_sizemult();
+	} else
+		sizemult = MEG / sectorsize;
+
+	/* Build standard partitions */
+
+	/* Partitions C and D are predefined. */
+	bsdlabel[C][D_FSTYPE] = T_UNUSED;
+	bsdlabel[C][D_OFFSET] = 0;
+	bsdlabel[C][D_SIZE] = dlsize;
+	
+	/* Standard fstypes */
+	bsdlabel[A][D_FSTYPE] = T_42BSD;
+	bsdlabel[B][D_FSTYPE] = T_SWAP;
+	bsdlabel[E][D_FSTYPE] = T_42BSD;
+	bsdlabel[F][D_FSTYPE] = T_UNUSED;
+	bsdlabel[G][D_FSTYPE] = T_UNUSED;
+	bsdlabel[H][D_FSTYPE] = T_UNUSED;
+
+	switch (layoutkind) {
+	case 1: /* standard: a root, b swap, c/d "unused", e /usr */
+	case 2: /* standard X: a root, b swap (big), c/d "unused", e /usr */
+		partstart = ptstart;
+
+		/* Root */
+		i = NUMSEC(20+2*rammb, MEG/sectorsize, dlcylsize) + partstart;
+		partsize = NUMSEC (i/(MEG/sectorsize)+1, MEG/sectorsize,
+				   dlcylsize) - partstart;
+		bsdlabel[A][D_OFFSET] = partstart;
+		bsdlabel[A][D_SIZE] = partsize;
+		bsdlabel[A][D_BSIZE] = 8192;
+		bsdlabel[A][D_FSIZE] = 1024;
+		strcpy (fsmount[A], "/");
+		partstart += partsize;
+
+		/* swap */
+		i = NUMSEC(layoutkind * 2 * (rammb < 32 ? 32 : rammb),
+			   MEG/sectorsize, dlcylsize) + partstart;
+		partsize = NUMSEC (i/(MEG/sectorsize)+1, MEG/sectorsize,
+			   dlcylsize) - partstart - swapadj;
+		bsdlabel[B][D_OFFSET] = partstart;
+		bsdlabel[B][D_SIZE] = partsize;
+		partstart += partsize;
+
+		/* /usr */
+		partsize = fsdsize - partstart;
+		bsdlabel[D][D_OFFSET] = partstart;
+		bsdlabel[D][D_SIZE] = partsize;
+		bsdlabel[D][D_BSIZE] = 8192;
+		bsdlabel[D][D_FSIZE] = 1024;
+		strcpy (fsmount[E], "/usr");
+
+
+		/* Verify Partitions. */
+		process_menu (MENU_fspartok);
+		break;
+
+	case 3: /* custom: ask user for all sizes */
+		process_menu(MENU_editfsparts);
+		break;
+	}
+
+	/* Disk name */
+	msg_prompt (MSG_packname, "mydisk", bsddiskname, 80);
+
+	/* Create the disktab.preinstall */
+	run_prog ("cp /etc/disktab.preinstall /etc/disktab");
+#ifdef DEBUG
+	f = fopen ("/tmp/disktab", "a");
+#else
+	f = fopen ("/etc/disktab", "a");
+#endif
+	if (f == NULL) {
+		endwin();
+		(void) fprintf (stderr, "Could not open /etc/disktab");
+		exit (1);
+	}
+	(void)fprintf (f, "%s|NetBSD installation generated:\\\n", bsddiskname);
+	(void)fprintf (f, "\t:dt=%s:ty=winchester:\\\n", disktype);
+	(void)fprintf (f, "\t:nc#%d:nt#%d:ns#%d:\\\n", dlcyl, dlhead, dlsec);
+	(void)fprintf (f, "\t:sc#%d:su#%d:\\\n", dlhead*dlsec, dlsize);
+	(void)fprintf (f, "\t:se#%d:%s\\\n", sectorsize, doessf);
+	for (i=0; i<8; i++) {
+		(void)fprintf (f, "\t:p%c#%d:o%c#%d:t%c=%s:",
+			       'a'+i, bsdlabel[i][D_SIZE],
+			       'a'+i, bsdlabel[i][D_OFFSET],
+			       'a'+i, fstype[bsdlabel[i][D_FSTYPE]]);
+		if (bsdlabel[i][D_FSTYPE] == T_42BSD)
+			(void)fprintf (f, "b%c#%d:f%c#%d",
+				       'a'+i, bsdlabel[i][D_BSIZE],
+				       'a'+i, bsdlabel[i][D_FSIZE]);
+		if (i < 7)
+			(void)fprintf (f, "\\\n");
+		else
+			(void)fprintf (f, "\n");
+	}
+	fclose (f);
+
+}
+
+/* update support */
+int
+md_update(void)
+{
+	return 1;
+}
+
