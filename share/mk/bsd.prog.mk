6f77030130b91cc7b83e75d7cbf7db91c7a6d2f8
lukem <lukem@NetBSD.org>
1997-10-28 12:46:30 +0000

    add ${LIBOBJC}

--- share/mk/bsd.prog.mk
+++ share/mk/bsd.prog.mk
@@ -1,4 +1,4 @@
-#	$NetBSD: bsd.prog.mk,v 1.88 1997/10/11 08:16:28 mycroft Exp $
+#	$NetBSD: bsd.prog.mk,v 1.89 1997/10/28 12:46:30 lukem Exp $
 #	@(#)bsd.prog.mk	8.2 (Berkeley) 4/2/94
 
 .if !target(__initialized__)
@@ -46,6 +46,7 @@ LIBL?=		${DESTDIR}/usr/lib/libl.a
 LIBM?=		${DESTDIR}/usr/lib/libm.a
 LIBMP?=		${DESTDIR}/usr/lib/libmp.a
 LIBNTP?=	${DESTDIR}/usr/lib/libntp.a
+LIBOBJC?=	${DESTDIR}/usr/lib/libobjc.a
 LIBPC?=		${DESTDIR}/usr/lib/libpc.a
 LIBPCAP?=	${DESTDIR}/usr/lib/libpcap.a
 LIBPLOT?=	${DESTDIR}/usr/lib/libplot.a
