17ac1824dc4cc32573a2425855e83669c58436f5
mellon <mellon@NetBSD.org>
1997-10-29 04:12:38 +0000

    Pull rev 1.3 up from trunk (phil)

--- gnu/lib/bfd/_ns32k.mk
+++ gnu/lib/bfd/_ns32k.mk
@@ -1,4 +1,4 @@
-# $NetBSD: _ns32k.mk,v 1.2 1997/10/17 20:07:27 gwr Exp $
+# $NetBSD: _ns32k.mk,v 1.2.2.1 1997/10/29 04:12:38 mellon Exp $
 
 BFD_BACKENDS =	ns32knetbsd.c aout-ns32k.c
 BFD_MACHINES =	cpu-ns32k.c
@@ -7,9 +7,9 @@ ARCH_DEFS = -DARCH_ns32k \
  -DSELECT_ARCHITECTURES='&bfd_ns32k_arch'
 
 TDEFAULTS = \
- -DDEFAULT_VECTOR=ns32knetbsd_vec \
- -DSELECT_VECS=' &ns32knetbsd_vec ' \
- -DHAVE_ns32knetbsd_vec \
+ -DDEFAULT_VECTOR=pc532netbsd_vec \
+ -DSELECT_VECS=' &pc532netbsd_vec ' \
+ -DHAVE_pc532netbsd_vec \
  -DNETBSD_CORE
 
 OPCODE_MACHINES =  ns32k-dis.c
