4ba76d86ad82676251837dfc949d2897c74e1e2d
drochner <drochner@NetBSD.org>
1997-11-01 14:21:53 +0000

    Fix a misbehaviour introduced in rev. 1.34 - mountfs() returned in "verbose" mode after printing the exec args. This invalidates most of my comment in the previous commit. (Not all, there are still bogosities with mount_mfs.)

--- sbin/mount/mount.c
+++ sbin/mount/mount.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: mount.c,v 1.36 1997/11/01 13:59:00 drochner Exp $	*/
+/*	$NetBSD: mount.c,v 1.37 1997/11/01 14:21:53 drochner Exp $	*/
 
 /*
  * Copyright (c) 1980, 1989, 1993, 1994
@@ -43,7 +43,7 @@ __COPYRIGHT("@(#) Copyright (c) 1980, 1989, 1993, 1994\n\
 #if 0
 static char sccsid[] = "@(#)mount.c	8.25 (Berkeley) 5/8/95";
 #else
-__RCSID("$NetBSD: mount.c,v 1.36 1997/11/01 13:59:00 drochner Exp $");
+__RCSID("$NetBSD: mount.c,v 1.37 1997/11/01 14:21:53 drochner Exp $");
 #endif
 #endif /* not lint */
 
@@ -380,7 +380,6 @@ mountfs(vfstype, spec, name, flags, options, mntopts, skipmounted)
 		for (i = 0; i < argc; i++)
 			(void)printf(" %s", argv[i]);
 		(void)printf("\n");
-		return (0);
 	}
 
 	switch (pid = vfork()) {
