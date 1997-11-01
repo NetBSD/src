ee3a40521fd34201ba9ba07b65a8cfe13296b1c1
mycroft <mycroft@NetBSD.org>
1997-11-01 15:59:26 +0000

    GC some unused bits. Also, part of the previous was copied from the com driver, so DTRT.

--- sys/dev/ic/z8530tty.c
+++ sys/dev/ic/z8530tty.c
@@ -1,4 +1,34 @@
-/*	$NetBSD: z8530tty.c,v 1.20 1997/11/01 15:51:23 mycroft Exp $	*/
+/*	$NetBSD: z8530tty.c,v 1.21 1997/11/01 15:59:26 mycroft Exp $	*/
+
+/*-
+ * Copyright (c) 1993, 1994, 1995, 1996, 1997
+ *	Charles M. Hannum.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed by Charles M. Hannum.
+ * 4. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 /*
  * Copyright (c) 1994 Gordon W. Ross
@@ -538,9 +568,6 @@ zswrite(dev, uio, flags)
 	tp = zst->zst_tty;
 	return (linesw[tp->t_line].l_write(tp, uio, flags));
 }
-
-#define TIOCFLAG_ALL (TIOCFLAG_SOFTCAR | TIOCFLAG_CLOCAL | \
-                      TIOCFLAG_CRTSCTS | TIOCFLAG_MDMBUF )
 
 int
 zsioctl(dev, cmd, data, flag, p)
