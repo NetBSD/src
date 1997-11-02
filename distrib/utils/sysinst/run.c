8fcdad13f1f43ea7fd86f5374ff5bb9039a8906a
mellon <mellon@NetBSD.org>
1997-11-02 20:44:36 +0000

    Pull rev 1.3 up from trunk (jonathan)

--- distrib/utils/sysinst/run.c
+++ distrib/utils/sysinst/run.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: run.c,v 1.2 1997/09/27 00:09:29 phil Exp $	*/
+/*	$NetBSD: run.c,v 1.2.2.1 1997/11/02 20:44:36 mellon Exp $	*/
 
 /*
  * Copyright 1997 Piermont Information Systems Inc.
@@ -45,11 +45,20 @@
 #include <sys/stat.h>
 #include "defs.h"
 
+#include "msg_defs.h"
+
 #ifdef DEBUG
 #define Xsystem(y) printf ("%s\n", y), 0
 #else
 #define Xsystem(y) system(y)
 #endif
+
+/*
+ * local prototypes 
+ */
+int do_system(const char *cmdstr);
+char* va_prog_cmdstr(char *cmd, va_list ap);
+
 
 #define BUFSIZE 4096
 
@@ -118,20 +127,94 @@ collect (int kind, char **buffer, char *name, ...)
 	return nbytes;
 }
 
-/* Run a program ... using printf style to format the command! */
 
+/*
+ * system(3), but with a debug wrapper.
+ */
+int do_system(const char *execstr)
+{
+	register int ret;
+
+	/* The following may be more than one function call.  Can't just
+	   "return Xsystem (command);" */
+
+	ret = Xsystem (execstr);
+	return ret;
+
+}
+
+/*
+ *  build command tring for do_system() from anonymous args.
+ *  XXX return result is in a static buffer.
+ */
+char* va_prog_cmdstr(char *cmd, va_list ap)
+{
+	static char command [STRSIZE];
+
+	bzero(command, STRSIZE);
+	(void) vsnprintf (command, STRSIZE, cmd, ap);
+	return command;
+}
+  
+
+/*  run a program.  The caller handles any and all errors. */
 int run_prog (char *cmd, ...)
 {
-	char command [STRSIZE];
 	va_list ap;
 	int ret;
 
-	va_start (ap, cmd);
-	(void) vsnprintf (command, STRSIZE, cmd, ap);
-	va_end (ap);
-	/* The following may be more than one function call.  Can't just
-	   "return Xsystem (command);" */
-	ret = Xsystem (command);
+    	va_start (ap, cmd);
+	ret = do_system(va_prog_cmdstr(cmd, ap));
+	va_end(ap);
 	return ret;
 }
 
+/*
+ * run a program where no errors are expected.
+ * Like run_prog, except print a generic warning if the return is nonzero.
+ * Used where sysinst doesn't yet know how to  recover from an error,
+ * but where an error is not completely catastrophic.
+ * Use stderr/stdin since we want the user to see any messages 
+ * from the command before we  go back into curses.
+ */
+int run_prog_or_continue (char *cmd, ...)
+{
+	va_list ap;
+	int ret;
+	const char *execstr;
+
+
+	va_start (ap, cmd);
+	execstr = va_prog_cmdstr(cmd, ap);
+	ret = do_system(execstr);
+	va_end (ap);
+
+	/* Warn copiously about any errors. */
+	if (ret) {
+	  /* XXX use messages instead */ 
+	  printf(msg_string(MSG_anonprogfailed), execstr, strerror(ret));
+	  fgetc(stdin);
+	}
+	return ret;
+}
+
+/*
+ * run a program. Die if there are any errors.
+ */
+    
+void run_prog_or_die (char *cmd, ...)
+{
+	va_list ap;
+	int ret;
+	const char *execstr;
+
+	va_start (ap, cmd);
+	execstr = va_prog_cmdstr(cmd, ap);
+	ret = do_system(execstr);
+
+	if (ret) {
+	  printf(msg_string(MSG_progdie), execstr, strerror(ret));
+	  exit(ret);
+	}
+}
+  
