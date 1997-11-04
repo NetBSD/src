2afc069711e8095d669786c674374154d1688458
thorpej <thorpej@NetBSD.org>
1997-11-04 23:18:41 +0000

    Pull up from trunk: bug fixes and cleaups.

--- sys/arch/m68k/include/cdefs.h
+++ sys/arch/m68k/include/cdefs.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: cdefs.h,v 1.2.18.1 1997/10/22 06:16:31 thorpej Exp $	*/
+/*	$NetBSD: cdefs.h,v 1.2.18.2 1997/11/04 23:18:59 thorpej Exp $	*/
 
 /*
  * Written by J.T. Conklin <jtc@wimsey.com> 01/17/95.
@@ -8,31 +8,27 @@
 #ifndef	_M68K_CDEFS_H_
 #define	_M68K_CDEFS_H_
 
-#ifdef __STDC__
-#define _C_LABEL(x)	_STRING(_ ## x)
-#else
-#define _C_LABEL(x)	_STRING(_/**/x)
-#endif
+#define	_C_LABEL(x)	__CONCAT(_,x)
 
 #ifdef __GNUC__
-#define	__RENAME(x)	__asm__(__CONCAT("_",x))
+#define	__RENAME(x)	__asm__(___STRING(_C_LABEL(x)))
 #endif
 
 #ifdef __GNUC__
 #ifdef __STDC__
 #define __indr_reference(sym,alias)	\
 	__asm__(".stabs \"_" #alias "\",11,0,0,0");	\
-	__asm__(".stabs \"_" #sym "\",1,0,0,0")
+	__asm__(".stabs \"_" #sym "\",1,0,0,0");
 #define __warn_references(sym,msg)	\
 	__asm__(".stabs \"" msg "\",30,0,0,0");		\
-	__asm__(".stabs \"_" #sym "\",1,0,0,0")
+	__asm__(".stabs \"_" #sym "\",1,0,0,0");
 #else
 #define __indr_reference(sym,alias)	\
 	__asm__(".stabs \"_/**/alias\",11,0,0,0");	\
-	__asm__(".stabs \"_/**/sym\",1,0,0,0")
+	__asm__(".stabs \"_/**/sym\",1,0,0,0");
 #define __warn_references(sym,msg)	\
 	__asm__(".stabs msg,30,0,0,0");			\
-	__asm__(".stabs \"_/**/sym\",1,0,0,0")
+	__asm__(".stabs \"_/**/sym\",1,0,0,0");
 #endif
 #endif
 
