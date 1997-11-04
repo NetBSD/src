c864417698f6b2661e143590eabf8bdbbfb1c9e3
mellon <mellon@NetBSD.org>
1997-11-04 23:08:52 +0000

    Pull rev 1.52 and 1.53 up from trunk (mycroft)

--- sys/arch/sparc/dev/zs.c
+++ sys/arch/sparc/dev/zs.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: zs.c,v 1.50.2.1 1997/10/23 01:07:14 mellon Exp $	*/
+/*	$NetBSD: zs.c,v 1.50.2.2 1997/11/04 23:08:52 mellon Exp $	*/
 
 /*-
  * Copyright (c) 1996 The NetBSD Foundation, Inc.
@@ -519,26 +519,26 @@ zs_set_modes(cs, cflag)
 	 * status interrupt to detect CTS changes.
 	 */
 	s = splzs();
-#if 0	/* XXX - See below. */
-	if (cflag & CLOCAL) {
+	if ((cflag & (CLOCAL | MDMBUF)) != 0)
 		cs->cs_rr0_dcd = 0;
-		cs->cs_preg[15] &= ~ZSWR15_DCD_IE;
-	} else {
-		/* XXX - Need to notice DCD change here... */
+	else
 		cs->cs_rr0_dcd = ZSRR0_DCD;
-		cs->cs_preg[15] |= ZSWR15_DCD_IE;
-	}
-#endif	/* XXX */
-	if (cflag & CRTSCTS) {
+	if ((cflag & CRTSCTS) != 0) {
 		cs->cs_wr5_dtr = ZSWR5_DTR;
 		cs->cs_wr5_rts = ZSWR5_RTS;
 		cs->cs_rr0_cts = ZSRR0_CTS;
-		cs->cs_preg[15] |= ZSWR15_CTS_IE;
+	} else if ((cflag & CDTRCTS) != 0) {
+		cs->cs_wr5_dtr = 0;
+		cs->cs_wr5_rts = ZSWR5_DTR;
+		cs->cs_rr0_cts = ZSRR0_CTS;
+	} else if ((cflag & MDMBUF) != 0) {
+		cs->cs_wr5_dtr = 0;
+		cs->cs_wr5_rts = ZSWR5_DTR;
+		cs->cs_rr0_cts = ZSRR0_DCD;
 	} else {
 		cs->cs_wr5_dtr = ZSWR5_DTR | ZSWR5_RTS;
 		cs->cs_wr5_rts = 0;
 		cs->cs_rr0_cts = 0;
-		cs->cs_preg[15] &= ~ZSWR15_CTS_IE;
 	}
 	splx(s);
 
