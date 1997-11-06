f353ff278cf87b9a3a680bf85337a6448b5915f0
cgd <cgd>
1997-11-06 00:50:36 +0000

    lint stubs for functions which ports use assembly versions.  When using an assembly version of a function, add the right lint stub to the Makefile.inc which specifies the source to build the function (see the arch/i386/*/Makefile.inc).  Can't just lint the normal C versions of these functions, because some of them don't _have_ normal C versions and if that were done dependencies would get messed up.

---
+++ lib/libc/gen/Lint_fabs.c
@@ -0,0 +1,16 @@
+/*	$NetBSD: Lint_fabs.c,v 1.1 1997/11/06 00:50:42 cgd Exp $	*/
+
+/*
+ * This file placed in the public domain.
+ * Chris Demetriou, November 5, 1997.
+ */
+
+#include <math.h>
+
+/*ARGSUSED*/
+double
+fabs(x)
+	double x;
+{
+	return (0.0);
+}
