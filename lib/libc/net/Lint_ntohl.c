732b9a1bba04cb05ac09c196aaed9ba250f5f1ba
cgd <cgd@NetBSD.org>
1997-11-06 00:51:04 +0000

    lint stubs for functions which ports use assembly versions.  When using an assembly version of a function, add the right lint stub to the Makefile.inc which specifies the source to build the function (see the arch/i386/*/Makefile.inc).  Can't just lint the normal C versions of these functions, because some of them don't _have_ normal C versions and if that were done dependencies would get messed up.

---
+++ lib/libc/net/Lint_ntohl.c
@@ -0,0 +1,16 @@
+/*	$NetBSD: Lint_ntohl.c,v 1.1 1997/11/06 00:51:25 cgd Exp $	*/
+
+/*
+ * This file placed in the public domain.
+ * Chris Demetriou, November 5, 1997.
+ */
+
+#include <sys/types.h>
+
+/*ARGSUSED*/
+in_addr_t
+ntohl(net32)
+	in_addr_t net32;
+{
+	return (0);
+}
