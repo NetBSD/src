5a658b540fc956f3e254485d5919140b1e74203c
veego <veego@NetBSD.org>
1997-11-09 21:52:14 +0000

    Pull up changes from the trunk (from luke, approved by jason): >WARNSify, KNFify

--- usr.sbin/rpc.pcnfsd/pcnfsd_cache.c
+++ usr.sbin/rpc.pcnfsd/pcnfsd_cache.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: pcnfsd_cache.c,v 1.2 1995/07/25 22:20:37 gwr Exp $	*/
+/*	$NetBSD: pcnfsd_cache.c,v 1.2.4.1 1997/11/09 21:52:17 veego Exp $	*/
 
 /* RE_SID: @(%)/usr/dosnfs/shades_SCCS/unix/pcnfsd/v2/src/SCCS/s.pcnfsd_cache.c 1.1 91/09/03 12:45:14 SMI */
 /*
@@ -7,7 +7,6 @@
 **	@(#)pcnfsd_cache.c	1.1	9/3/91
 **=====================================================================
 */
-#include "common.h"
 /*
 **=====================================================================
 **             I N C L U D E   F I L E   S E C T I O N                *
@@ -17,15 +16,16 @@
 ** exclusion of the files conditional on this.                        *
 **=====================================================================
 */
-#include "pcnfsd.h"
 
-#include <stdio.h>
-#include <pwd.h>
 #include <errno.h>
+#include <pwd.h>
+#include <stdio.h>
 #include <string.h>
+#include <unistd.h>
 
-extern char    *crypt();
-
+#include "common.h"
+#include "pcnfsd.h"
+#include "extern.h"
 
 /*
 **---------------------------------------------------------------------
@@ -33,72 +33,64 @@ extern char    *crypt();
 **---------------------------------------------------------------------
 */
 
-extern int      errno;
-
 #ifdef USER_CACHE
-#define CACHE_SIZE 16		/* keep it small, as linear searches are
-				 * done */
-struct cache 
-       {
-       int   cuid;
-       int   cgid;
-       char  cpw[32];
-       char  cuname[10];	/* keep this even for machines
-				 * with alignment problems */
-       }User_cache[CACHE_SIZE];
-
+#define CACHE_SIZE 16		/* keep it small, as linear searches are done */
+struct cache {
+	int     cuid;
+	int     cgid;
+	char    cpw[32];
+	char    cuname[10];	/* keep this even for machines with alignment
+				 * problems */
+}       User_cache[CACHE_SIZE];
 
 
 /*
 **---------------------------------------------------------------------
-**                 User cache support procedures 
+**                 User cache support procedures
 **---------------------------------------------------------------------
 */
 
 
 int
 check_cache(name, pw, p_uid, p_gid)
-	char           *name;
-   char           *pw;
-   int            *p_uid;
-   int            *p_gid;
+	char   *name;
+	char   *pw;
+	int    *p_uid;
+	int    *p_gid;
 {
-	int             i;
-   int             c1, c2;
+	int     i;
+	int     c1, c2;
 
 	for (i = 0; i < CACHE_SIZE; i++) {
 		if (!strcmp(User_cache[i].cuname, name)) {
-           		c1 = strlen(pw);
-	       		c2 = strlen(User_cache[i].cpw);
-	        	if ((!c1 && !c2) ||
-	  	       	    !(strcmp(User_cache[i].cpw,
-		       	           crypt(pw, User_cache[i].cpw)))) {
-		        	*p_uid = User_cache[i].cuid;
-		        	*p_gid = User_cache[i].cgid;
-		        	return (1);
-		    	}
-		    	User_cache[i].cuname[0] = '\0'; /* nuke entry */
-           		return (0);
-       		}
+			c1 = strlen(pw);
+			c2 = strlen(User_cache[i].cpw);
+			if ((!c1 && !c2) ||
+			    !(strcmp(User_cache[i].cpw,
+				    crypt(pw, User_cache[i].cpw)))) {
+				*p_uid = User_cache[i].cuid;
+				*p_gid = User_cache[i].cgid;
+				return (1);
+			}
+			User_cache[i].cuname[0] = '\0';	/* nuke entry */
+			return (0);
+		}
 	}
 	return (0);
 }
 
 void
 add_cache_entry(p)
-	struct passwd  *p;
+	struct passwd *p;
 {
-	int             i;
+	int     i;
 
 	for (i = CACHE_SIZE - 1; i > 0; i--)
 		User_cache[i] = User_cache[i - 1];
 	User_cache[0].cuid = p->pw_uid;
 	User_cache[0].cgid = p->pw_gid;
-	(void)strcpy(User_cache[0].cpw, p->pw_passwd);
-	(void)strcpy(User_cache[0].cuname, p->pw_name);
+	(void) strcpy(User_cache[0].cpw, p->pw_passwd);
+	(void) strcpy(User_cache[0].cuname, p->pw_name);
 }
 
-
 #endif				/* USER_CACHE */
-
-
