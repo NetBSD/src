21e8e41a316958c3693376c0e9f330625936e527
mrg <mrg@NetBSD.org>
1997-11-11 05:48:03 +0000

    add a "checkportcmd <class>" option that stops ftp bounce attacks.

--- libexec/ftpd/conf.c
+++ libexec/ftpd/conf.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: conf.c,v 1.7 1997/10/19 18:15:23 mycroft Exp $	*/
+/*	$NetBSD: conf.c,v 1.8 1997/11/11 05:48:05 mrg Exp $	*/
 
 /*-
  * Copyright (c) 1997 The NetBSD Foundation, Inc.
@@ -38,7 +38,7 @@
 
 #include <sys/cdefs.h>
 #ifndef lint
-__RCSID("$NetBSD: conf.c,v 1.7 1997/10/19 18:15:23 mycroft Exp $");
+__RCSID("$NetBSD: conf.c,v 1.8 1997/11/11 05:48:05 mrg Exp $");
 #endif /* not lint */
 
 #include <sys/types.h>
@@ -274,6 +274,8 @@ parse_conf(findclass)
 				continue;
 			}
 			curclass.umask = umask;
+		} else if (strcasecmp(word, "checkportcmd") == 0) {
+			curclass.checkportcmd = 1;
 		} else {
 			syslog(LOG_WARNING,
 			    "%s line %d: unknown directive '%s'",
