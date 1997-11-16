29a446d6a0a48c007ea679b39ae7ba4de5344858
christos <christos@NetBSD.org>
1997-11-16 22:50:55 +0000

    From Todd Vierling: - Fix memcntl() - Fixed stat and mknod syscalls to properly disassemble and reassemble   dev_t values to/from emulation dev_t's - Added pty (ptmx) subsystem at svr4net minor number 10 -- /dev/ptmx - Implemented fork1() as fork() (should change if and when threads get   added to the system) - fixed spellings in disclaimer in svr4_net.c; it looks like someone went   and did a s/nd/st/g on the whole file... yuck - changed ptmx device to also halt on ENXIO (for pty files that exist with   no kernel pty to back them) - fixed SVR4 setpgrp() to be a near equivalent to BSD setsid() (old   emulation didn't create a new session)

--- sys/compat/svr4/svr4_misc.c
+++ sys/compat/svr4/svr4_misc.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: svr4_misc.c,v 1.54 1997/11/03 22:03:31 pk Exp $	 */
+/*	$NetBSD: svr4_misc.c,v 1.55 1997/11/16 22:50:55 christos Exp $	 */
 
 /*
  * Copyright (c) 1994 Christos Zoulas
@@ -79,6 +79,7 @@
 #include <compat/svr4/svr4_statvfs.h>
 #include <compat/svr4/svr4_sysconfig.h>
 #include <compat/svr4/svr4_acl.h>
+#include <compat/svr4/svr4_mman.h>
 
 #include <vm/vm.h>
 
@@ -578,7 +579,8 @@ svr4_sys_mknod(p, v, retval)
 {
 	struct svr4_sys_mknod_args *uap = v;
 	return svr4_mknod(p, retval,
-			  SCARG(uap, path), SCARG(uap, mode), SCARG(uap, dev));
+			  SCARG(uap, path), SCARG(uap, mode),
+			  svr4_to_bsd_odev_t(SCARG(uap, dev)));
 }
 
 
@@ -590,7 +592,8 @@ svr4_sys_xmknod(p, v, retval)
 {
 	struct svr4_sys_xmknod_args *uap = v;
 	return svr4_mknod(p, retval,
-			  SCARG(uap, path), SCARG(uap, mode), SCARG(uap, dev));
+			  SCARG(uap, path), SCARG(uap, mode),
+			  svr4_to_bsd_dev_t(SCARG(uap, dev)));
 }
 
 
@@ -985,24 +988,22 @@ svr4_sys_pgrpsys(p, v, retval)
 	register_t *retval;
 {
 	struct svr4_sys_pgrpsys_args *uap = v;
-	int error;
 
 	switch (SCARG(uap, cmd)) {
+	case 1:			/* setpgrp() */
+		/*
+		 * SVR4 setpgrp() (which takes no arguments) has the
+		 * semantics that the session ID is also created anew, so
+		 * in almost every sense, setpgrp() is identical to
+		 * setsid() for SVR4.  (Under BSD, the difference is that
+		 * a setpgid(0,0) will not create a new session.)
+		 */
+		sys_setsid(p, NULL, retval);
+		/*FALLTHROUGH*/
+
 	case 0:			/* getpgrp() */
 		*retval = p->p_pgrp->pg_id;
 		return 0;
-
-	case 1:			/* setpgrp() */
-		{
-			struct sys_setpgid_args sa;
-
-			SCARG(&sa, pid) = 0;
-			SCARG(&sa, pgid) = 0;
-			if ((error = sys_setpgid(p, &sa, retval)) != 0)
-				return error;
-			*retval = p->p_pgrp->pg_id;
-			return 0;
-		}
 
 	case 2:			/* getsid(pid) */
 		if (SCARG(uap, pid) != 0 &&
@@ -1606,14 +1607,35 @@ svr4_sys_memcntl(p, v, retval)
 	register_t *retval;
 {
 	struct svr4_sys_memcntl_args *uap = v;
-	struct sys_mprotect_args ap;
+	switch (SCARG(uap, cmd)) {
+	case SVR4_MC_SYNC:
+		{
+			struct sys___msync13_args msa;
 
-	SCARG(&ap, addr) = SCARG(uap, addr);
-	SCARG(&ap, len) = SCARG(uap, len);
-	SCARG(&ap, prot) = SCARG(uap, attr);
+			SCARG(&msa, addr) = SCARG(uap, addr);
+			SCARG(&msa, len) = SCARG(uap, len);
+			SCARG(&msa, flags) = (int)SCARG(uap, arg);
 
-	/* XXX: no locking, invalidating, or syncing supported */
-	return sys_mprotect(p, &ap, retval);
+			return sys___msync13(p, &msa, retval);
+		}
+	case SVR4_MC_ADVISE:
+		{
+			struct sys_madvise_args maa;
+
+			SCARG(&maa, addr) = SCARG(uap, addr);
+			SCARG(&maa, len) = SCARG(uap, len);
+			SCARG(&maa, behav) = (int)SCARG(uap, arg);
+
+			return sys_madvise(p, &maa, retval);
+		}
+	case SVR4_MC_LOCK:
+	case SVR4_MC_UNLOCK:
+	case SVR4_MC_LOCKAS:
+	case SVR4_MC_UNLOCKAS:
+		return EOPNOTSUPP;
+	default:
+		return ENOSYS;
+	}
 }
 
 
--- sys/compat/svr4/svr4_mman.h
+++ sys/compat/svr4/svr4_mman.h
@@ -1,7 +1,7 @@
-/*	$NetBSD: svr4_types.h,v 1.9 1997/11/16 22:50:59 christos Exp $	 */
+/*	$NetBSD: svr4_mman.h,v 1.1 1997/11/16 22:50:56 christos Exp $	*/
 
 /*
- * Copyright (c) 1994 Christos Zoulas
+ * Copyright (c) 1997 Todd Vierling
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -27,47 +27,20 @@
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifndef	_SVR4_TYPES_H_
-#define	_SVR4_TYPES_H_
+#ifndef _SVR4_MMAN_H_
+#define _SVR4_MMAN_H_
 
-typedef u_quad_t	 svr4_ino64_t;
-typedef quad_t		 svr4_off64_t;
-typedef quad_t		 svr4_blkcnt64_t;
-typedef u_quad_t	 svr4_fsblkcnt64_t;
+/*
+ * Commands and flags passed to memcntl().
+ * Most of them are the same as <sys/mman.h>, but we need the MC_
+ * memcntl command definitions.
+ */
 
-typedef long  		 svr4_off_t;
-typedef u_long		 svr4_dev_t;
-typedef u_long		 svr4_ino_t;
-typedef u_long		 svr4_mode_t;
-typedef u_long		 svr4_nlink_t;
-typedef long		 svr4_uid_t;
-typedef long		 svr4_gid_t;
-typedef long		 svr4_daddr_t;
-typedef long		 svr4_pid_t;
-typedef long		 svr4_time_t;
-typedef long		 svr4_blkcnt_t;
-typedef u_long		 svr4_fsblkcnt_t;
-typedef char		*svr4_caddr_t;
-typedef u_int		 svr4_size_t;
+#define SVR4_MC_SYNC		1
+#define SVR4_MC_LOCK		2
+#define SVR4_MC_UNLOCK		3
+#define SVR4_MC_ADVISE		4
+#define SVR4_MC_LOCKAS		5
+#define SVR4_MC_UNLOCKAS	6
 
-typedef short		 svr4_o_dev_t;
-typedef short		 svr4_o_pid_t;
-typedef u_short		 svr4_o_ino_t;
-typedef u_short		 svr4_o_mode_t;
-typedef short		 svr4_o_nlink_t;
-typedef u_short		 svr4_o_uid_t;
-typedef u_short		 svr4_o_gid_t;
-typedef long		 svr4_clock_t;
-typedef int		 svr4_key_t;
-
-typedef struct timespec  svr4_timestruc_t;
-
-#define svr4_to_bsd_odev_t(d) makedev((((d) & 0x7f00) >> 8), ((d) & 0xff))
-/* XXX: we limit the minor number to 0xff, instead of 0x3ffff */
-#define svr4_to_bsd_dev_t(d) makedev((((d) & 0xfffc0000) >> 18), ((d) & 0xff))
-
-#define bsd_to_svr4_odev_t(d) (((major(d) & 0x7f) << 8)|(minor(d) & 0xff))
-#define bsd_to_svr4_dev_t(d) (((major(d) & 0x3fff) << 18)|(minor(d) & 0x3ffff))
-
-
-#endif /* !_SVR4_TYPES_H_ */
+#endif /* !_SVR4_MMAN_H */
