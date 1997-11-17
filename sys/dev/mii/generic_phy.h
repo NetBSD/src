ace2666bdfe4edbbe8b09072f761e13cc312b59a
thorpej <thorpej@NetBSD.org>
1997-11-17 08:25:56 +0000

    Cosmetic changes.

--- sys/dev/mii/generic_phy.h
+++ sys/dev/mii/generic_phy.h
@@ -1,4 +1,4 @@
-/*  $NetBSD: generic_phy.h,v 1.1 1997/10/17 17:33:51 bouyer Exp $   */
+/*  $NetBSD: generic_phy.h,v 1.2 1997/11/17 08:25:56 thorpej Exp $   */
  
 /*
  * Copyright (c) 1997 Manuel Bouyer.  All rights reserved.
@@ -30,59 +30,59 @@
  */
 
 /* MII commands */
-#define MII_START 0x01
-#define MII_READ  0x02
-#define MII_WRITE 0x01
-#define MII_ACK   0x02
+#define	MII_START	0x01
+#define	MII_READ	0x02
+#define	MII_WRITE	0x01
+#define	MII_ACK		0x02
 
 /* generic phy registers */
-#define PHY_CONTROL	0x00 	/* read/write */
-#	define	CTRL_RESET		0x8000 /* reset */
-#	define	CTRL_LOOPBK		0x4000 /* loopback */
-#	define	CTRL_SPEED		0x2000 /* speed (10/100) select */
+#define	PHY_CONTROL	0x00 	/* read/write */
+#	define	CTRL_RESET	0x8000 /* reset */
+#	define	CTRL_LOOPBK	0x4000 /* loopback */
+#	define	CTRL_SPEED	0x2000 /* speed (10/100) select */
 #	define	CTRL_AUTO_EN	0x1000 /* autonegotiation enable */
-#	define	CTRL_ISO		0x0400 /* isolate */
+#	define	CTRL_ISO	0x0400 /* isolate */
 #	define	CTRL_AUTORST	0x0200 /* restart autonegotiation */
-#	define	CTRL_DUPLEX		0x0100 /* Set duplex mode */
+#	define	CTRL_DUPLEX	0x0100 /* Set duplex mode */
 #	define	CTRL_COL_TEST	0x0080 /* collision test */
-#define PHY_STATUS	0x01	/* Read Only */
-#	define	ST_100bT4		0x8000 /* 100 base T4 capable */
+#define	PHY_STATUS	0x01	/* Read Only */
+#	define	ST_100bT4	0x8000 /* 100 base T4 capable */
 #	define	ST_100bTx_fd	0x4000 /* 100 base Tx full duplex capable */
-#	define	ST_100bTx		0x2000 /* 100 base Tx half duplex capable */
-#	define	ST_10bT_fd		0x1000 /* 10 base T full duplex capable */
-#	define	ST_10bT			0x0800 /* 10 base T half duplex capable */
+#	define	ST_100bTx	0x2000 /* 100 base Tx half duplex capable */
+#	define	ST_10bT_fd	0x1000 /* 10 base T full duplex capable */
+#	define	ST_10bT		0x0800 /* 10 base T half duplex capable */
 #	define	ST_AUTO_DONE	0x0020 /* Autonegotiation complete */
-#	define	ST_RemFault		0x0010 /* Link partner fault */
-#	define	ST_AUTO			0x0008 /* Autonegotiation capable */
-#	define	ST_LINK			0x0004 /* Link status */
-#	define	ST_JABBER		0x0002 /* Jabber detected */
-#	define	ST_Ext			0x0001 /* Extended capability */
-#define PHY_IDH		0x02	/* Read Only */
-#define PHY_IDL		0x03	/* Read Only */
-#define PHY_AN_Adv	0x04	/* Read/write */
-#	define Adv_NP			0x8000 /* Next page */
+#	define	ST_RemFault	0x0010 /* Link partner fault */
+#	define	ST_AUTO		0x0008 /* Autonegotiation capable */
+#	define	ST_LINK		0x0004 /* Link status */
+#	define	ST_JABBER	0x0002 /* Jabber detected */
+#	define	ST_Ext		0x0001 /* Extended capability */
+#define	PHY_IDH		0x02	/* Read Only */
+#define	PHY_IDL		0x03	/* Read Only */
+#define	PHY_AN_Adv	0x04	/* Read/write */
+#	define Adv_NP		0x8000 /* Next page */
 #	define Adv_RemFault	0x2000 /* Remote fault */
-#	define Adv_100bT4		0x0200 /* local device supports 100bT4 */
+#	define Adv_100bT4	0x0200 /* local device supports 100bT4 */
 #	define Adv_100bTx_fd	0x0100 /* local device supports 100bTx FD */
-#	define Adv_100bTx		0x0080 /* local device supports 100bTx */
-#	define Adv_10bT_fd		0x0040 /* local device supports 10bT FD */
-#	define Adv_10bT			0x0020 /* local device supports 10bT */
-#	define Adv_Sel			0x001f /* Autoneg selector field */
-#define PHY_AN_LPA	0x05	/* Read Only */
-#	define LPA_NP			0x8000 /* Next page */
-#	define LPA_RemFault		0x2000 /* remote fault */
-#	define LPA_100bT4		0x0200 /* link partner supports 100bT4 */
+#	define Adv_100bTx	0x0080 /* local device supports 100bTx */
+#	define Adv_10bT_fd	0x0040 /* local device supports 10bT FD */
+#	define Adv_10bT		0x0020 /* local device supports 10bT */
+#	define Adv_Sel		0x001f /* Autoneg selector field */
+#define	PHY_AN_LPA	0x05	/* Read Only */
+#	define LPA_NP		0x8000 /* Next page */
+#	define LPA_RemFault	0x2000 /* remote fault */
+#	define LPA_100bT4	0x0200 /* link partner supports 100bT4 */
 #	define LPA_100bTx_fd	0x0100 /* link partner supports 100bTx FD */
-#	define LPA_100bTx		0x0080 /* link partner supports 100bTx */
-#	define LPA_10bT_fd		0x0040 /* link partner supports 10bT FD */
-#	define LPA_10bT			0x0020 /* link partner supports 10bT */
-#	define LPA_Sel			0x001f /* selector field */
-#define PHY_AN_Epx	0x06	/* Read Only */
-#	define Epx_ParDet		0x0010 /* multiple link detection fault */
-#	define Epx_LP_NP		0x0008 /* link parter next page-able */
-#	define Epx_NP			0x0004 /* next page-able */
-#	define Epx_PRX			0x0002 /* Page received */
-#	define Epx_LP_auto		0x0001 /* link parter autoneg-able */
+#	define LPA_100bTx	0x0080 /* link partner supports 100bTx */
+#	define LPA_10bT_fd	0x0040 /* link partner supports 10bT FD */
+#	define LPA_10bT		0x0020 /* link partner supports 10bT */
+#	define LPA_Sel		0x001f /* selector field */
+#define	PHY_AN_Epx	0x06	/* Read Only */
+#	define Epx_ParDet	0x0010 /* multiple link detection fault */
+#	define Epx_LP_NP	0x0008 /* link parter next page-able */
+#	define Epx_NP		0x0004 /* next page-able */
+#	define Epx_PRX		0x0002 /* Page received */
+#	define Epx_LP_auto	0x0001 /* link parter autoneg-able */
 
 /* generic phy softc */
 struct phy_softc {
@@ -90,9 +90,9 @@ struct phy_softc {
 	mii_phy_t *phy_link;
 }; 
 
-int phy_reset __P((struct phy_softc*));
-int phy_media_probe __P((struct phy_softc*));
-void phy_media_print __P((u_int32_t));
-int phy_media_set_10_100 __P((struct phy_softc*, int media));
-int phy_status __P((int, void*));
-void phy_dumpreg __P((struct phy_softc*));
+int	phy_reset __P((struct phy_softc *));
+int	phy_media_probe __P((struct phy_softc *));
+void	phy_media_print __P((u_int32_t));
+int	phy_media_set_10_100 __P((struct phy_softc *, int media));
+int	phy_status __P((int, void *));
+void	phy_dumpreg __P((struct phy_softc *));
