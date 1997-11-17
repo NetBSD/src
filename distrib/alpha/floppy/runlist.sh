1aefd2100a6b819fda524eac1eca7756efe44704
cjs <cjs>
1997-11-17 09:47:09 +0000

    Add support for bootable floppy for installs.

---
+++ distrib/alpha/floppy/runlist.sh
@@ -0,0 +1,13 @@
+#	$NetBSD: runlist.sh,v 1.1.2.2 1997/11/17 09:47:11 cjs Exp $
+
+if [ "X$1" = "X-d" ]; then
+	SHELLCMD=cat
+	shift
+else
+	SHELLCMD="sh -e"
+fi
+
+( while [ "X$1" != "X" ]; do
+	cat $1
+	shift
+done ) | awk -f ${TOPDIR}/list2sh.awk | ${SHELLCMD}
