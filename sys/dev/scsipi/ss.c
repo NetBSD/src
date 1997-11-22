f3e96e547044d6607539a8bd8c35e62250e93bcb
mellon <mellon>
1997-11-22 04:01:56 +0000

    Pull rev 1.18 up from trunk (augustss)

--- sys/dev/scsipi/ss.c
+++ sys/dev/scsipi/ss.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: ss.c,v 1.17 1997/10/01 01:19:18 enami Exp $	*/
+/*	$NetBSD: ss.c,v 1.17.2.1 1997/11/22 04:01:56 mellon Exp $	*/
 
 /*
  * Copyright (c) 1995 Kenneth Stailey.  All rights reserved.
@@ -101,6 +101,8 @@ struct scsipi_inquiry_pattern ss_patterns[] = {
 	 "HP      ", "C2500A          ", ""},
 	{T_PROCESSOR, T_FIXED,
 	 "HP      ", "C1130A          ", ""},
+	{T_PROCESSOR, T_FIXED,
+	 "HP      ", "C5110A          ", ""},
 };
 
 int
