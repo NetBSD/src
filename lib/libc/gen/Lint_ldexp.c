5822b3639f34b16ddabf8b4a0077228cfeab257b
matthias <matthias@NetBSD.org>
1997-12-07 00:24:50 +0000

    add lint stubs for files that are implemented in assembler on the pc532.

---
+++ lib/libc/gen/Lint_ldexp.c
@@ -0,0 +1,16 @@
+/*	$NetBSD: Lint_ldexp.c,v 1.1 1997/12/07 00:24:50 matthias Exp $	*/
+
+/*
+ * This file placed in the public domain.
+ * Matthias Pfaller, December 6, 1997.
+ */
+
+#include <math.h>
+
+double
+ldexp(val, exp)
+	double val;
+	int exp;
+{
+	return(0);
+}
