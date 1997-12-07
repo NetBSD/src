03b4d8467a5f821584047eb57fe7b7442bbdb58b
matthias <matthias@NetBSD.org>
1997-12-07 00:24:50 +0000

    add lint stubs for files that are implemented in assembler on the pc532.

---
+++ lib/libc/gen/Lint_frexp.c
@@ -0,0 +1,16 @@
+/*	$NetBSD: Lint_frexp.c,v 1.1 1997/12/07 00:24:50 matthias Exp $	*/
+
+/*
+ * This file placed in the public domain.
+ * Matthias Pfaller, December 6, 1997.
+ */
+
+#include <math.h>
+
+double
+frexp(value, eptr)
+	double value;
+	int *eptr;
+{
+	return(0);
+}
