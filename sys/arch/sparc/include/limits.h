bd7386c56a2857d9a09733841b089367f21c9d13
perry <perry>
1998-01-09 22:23:44 +0000

    multiple include protect machine/limits.h, fixes pr 4473 (from Mika Nystrom)

--- sys/arch/sparc/include/limits.h
+++ sys/arch/sparc/include/limits.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: limits.h,v 1.7 1996/01/05 18:10:57 pk Exp $ */
+/*	$NetBSD: limits.h,v 1.8 1998/01/09 22:24:03 perry Exp $ */
 
 /*
  * Copyright (c) 1988 The Regents of the University of California.
@@ -34,6 +34,9 @@
  *
  *	@(#)limits.h	8.3 (Berkeley) 1/4/94
  */
+
+#ifndef	_MACHINE_LIMITS_H_
+#define	_MACHINE_LIMITS_H_
 
 #define	CHAR_BIT	8		/* number of bits in a char */
 #define	MB_LEN_MAX	1		/* no multibyte characters */
@@ -84,3 +87,5 @@
 #define FLT_MAX		3.40282347E+38F
 #define FLT_MIN		1.17549435E-38F
 #endif
+
+#endif /* _MACHINE_LIMITS_H_ */
