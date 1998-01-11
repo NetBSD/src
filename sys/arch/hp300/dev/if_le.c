9cbfb1c65454caf2cef917f2052f0b45c81437ac
thorpej <thorpej@NetBSD.org>
1998-01-11 21:57:02 +0000

    Convert the hp300 LANCE driver to use bus.h

--- sys/arch/hp300/dev/if_le.c
+++ sys/arch/hp300/dev/if_le.c
@@ -1,4 +1,40 @@
-/*	$NetBSD: if_le.c,v 1.43 1997/05/05 21:05:32 thorpej Exp $	*/
+/*	$NetBSD: if_le.c,v 1.44 1998/01/11 21:57:02 thorpej Exp $	*/
+
+/*-
+ * Copyright (c) 1998 The NetBSD Foundation, Inc.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to The NetBSD Foundation
+ * by Jason R. Thorpe.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed by the NetBSD
+ *	Foundation, Inc. and its contributors.
+ * 4. Neither the name of The NetBSD Foundation nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 
 /*-
  * Copyright (c) 1995 Charles M. Hannum.  All rights reserved.
@@ -85,6 +121,10 @@ struct cfattach le_ca = {
 
 int	leintr __P((void *));
 
+hide void le_copytobuf __P((struct am7990_softc *, void *, int, int));
+hide void le_copyfrombuf __P((struct am7990_softc *, void *, int, int));
+hide void le_zerobuf __P((struct am7990_softc *, int, int));
+
 /* offsets for:	   ID,   REGS,    MEM,  NVRAM */
 int	lestd[] = { 0, 0x4000, 0x8000, 0xC008 };
 
@@ -96,15 +136,17 @@ lewrcsr(sc, port, val)
 	struct am7990_softc *sc;
 	u_int16_t port, val;
 {
-	struct lereg0 *ler0 = ((struct le_softc *)sc)->sc_r0;
-	struct lereg1 *ler1 = ((struct le_softc *)sc)->sc_r1;
+	struct le_softc *lesc = (struct le_softc *)sc;
+	bus_space_tag_t bst = lesc->sc_bst;
+	bus_space_handle_t bsh0 = lesc->sc_bsh0;
+	bus_space_handle_t bsh1 = lesc->sc_bsh1;
 
 	do {
-		ler1->ler1_rap = port;
-	} while ((ler0->ler0_status & LE_ACK) == 0);
+		bus_space_write_2(bst, bsh1, LER1_RAP, port);
+	} while ((bus_space_read_1(bst, bsh0, LER0_STATUS) & LE_ACK) == 0);
 	do {
-		ler1->ler1_rdp = val;
-	} while ((ler0->ler0_status & LE_ACK) == 0);
+		bus_space_write_2(bst, bsh1, LER1_RDP, val);
+	} while ((bus_space_read_1(bst, bsh0, LER0_STATUS) & LE_ACK) == 0);
 }
 
 hide u_int16_t
@@ -112,16 +154,19 @@ lerdcsr(sc, port)
 	struct am7990_softc *sc;
 	u_int16_t port;
 {
-	struct lereg0 *ler0 = ((struct le_softc *)sc)->sc_r0;
-	struct lereg1 *ler1 = ((struct le_softc *)sc)->sc_r1;
+	struct le_softc *lesc = (struct le_softc *)sc;
+	bus_space_tag_t bst = lesc->sc_bst;
+	bus_space_handle_t bsh0 = lesc->sc_bsh0;
+	bus_space_handle_t bsh1 = lesc->sc_bsh1;
 	u_int16_t val;
 
 	do {
-		ler1->ler1_rap = port;
-	} while ((ler0->ler0_status & LE_ACK) == 0);
+		bus_space_write_2(bst, bsh1, LER1_RAP, port);
+	} while ((bus_space_read_1(bst, bsh0, LER0_STATUS) & LE_ACK) == 0);
 	do {
-		val = ler1->ler1_rdp;
-	} while ((ler0->ler0_status & LE_ACK) == 0);
+		val = bus_space_read_2(bst, bsh1, LER1_RDP);
+	} while ((bus_space_read_1(bst, bsh0, LER0_STATUS) & LE_ACK) == 0);
+
 	return (val);
 }
 
@@ -148,50 +193,66 @@ leattach(parent, self, aux)
 	struct device *parent, *self;
 	void *aux;
 {
-	struct lereg0 *ler0;
 	struct dio_attach_args *da = aux;
 	struct le_softc *lesc = (struct le_softc *)self;
-	caddr_t addr;
 	struct am7990_softc *sc = &lesc->sc_am7990;
-	char *cp;
+	bus_space_tag_t bst = da->da_bst;
+	bus_space_handle_t bsh0, bsh1, bsh2;
+	bus_size_t offset;
 	int i, ipl;
 
-	addr = iomap(dio_scodetopa(da->da_scode), da->da_size);
-	if (addr == 0) {
+	if (bus_space_map(bst, (bus_addr_t)dio_scodetopa(da->da_scode),
+	    da->da_size, 0, &bsh0)) {
 		printf("\n%s: can't map LANCE registers\n",
 		    sc->sc_dev.dv_xname);
 		return;
 	}
 
-	ler0 = lesc->sc_r0 = (struct lereg0 *)(lestd[0] + (int)addr);
-	ler0->ler0_id = 0xFF;
+	if (bus_space_subregion(bst, bsh0, lestd[1], LER1_SIZE, &bsh1)) {
+		printf("\n%s: can't subregion LANCE space\n",
+		    sc->sc_dev.dv_xname);
+		return;
+	}
+
+	if (bus_space_subregion(bst, bsh0, lestd[2], LE_BUFSIZE, &bsh2)) {
+		printf("\n%s: can't subregion buffer space\n",
+		    sc->sc_dev.dv_xname);
+		return;
+	}
+
+	lesc->sc_bst = bst;
+	lesc->sc_bsh0 = bsh0;
+	lesc->sc_bsh1 = bsh1;
+	lesc->sc_bsh2 = bsh2;
+
+	bus_space_write_1(bst, bsh0, LER0_ID, 0xff);
 	DELAY(100);
 
-	ipl = DIO_IPL(addr);
+	ipl = DIO_IPL((caddr_t)bsh0);			/* XXX */
 	printf(" ipl %d", ipl);
 
-	lesc->sc_r1 = (struct lereg1 *)(lestd[1] + (int)addr);
-	sc->sc_mem = (void *)(lestd[2] + (int)addr);
 	sc->sc_conf3 = LE_C3_BSWP;
 	sc->sc_addr = 0;
-	sc->sc_memsize = 16384;
+	sc->sc_memsize = LE_BUFSIZE;
 
 	/*
 	 * Read the ethernet address off the board, one nibble at a time.
 	 */
-	cp = (char *)(lestd[3] + (int)addr);
+	offset = lestd[3];
 	for (i = 0; i < sizeof(sc->sc_enaddr); i++) {
-		sc->sc_enaddr[i] = (*++cp & 0xF) << 4;
-		cp++;
-		sc->sc_enaddr[i] |= *++cp & 0xF;
-		cp++;
+		sc->sc_enaddr[i] =
+		    (bus_space_read_1(bst, bsh0, ++offset) & 0xf) << 4;
+		offset++;
+		sc->sc_enaddr[i] |=
+		    (bus_space_read_1(bst, bsh0, ++offset) & 0xf);
+		offset++;
 	}
 
-	sc->sc_copytodesc = am7990_copytobuf_contig;
-	sc->sc_copyfromdesc = am7990_copyfrombuf_contig;
-	sc->sc_copytobuf = am7990_copytobuf_contig;
-	sc->sc_copyfrombuf = am7990_copyfrombuf_contig;
-	sc->sc_zerobuf = am7990_zerobuf_contig;
+	sc->sc_copytodesc = le_copytobuf;
+	sc->sc_copyfromdesc = le_copyfrombuf;
+	sc->sc_copytobuf = le_copytobuf;
+	sc->sc_copyfrombuf = le_copyfrombuf;
+	sc->sc_zerobuf = le_zerobuf;
 
 	sc->sc_rdcsr = lerdcsr;
 	sc->sc_wrcsr = lewrcsr;
@@ -201,7 +262,7 @@ leattach(parent, self, aux)
 
 	/* Establish the interrupt handler. */
 	(void) dio_intr_establish(leintr, sc, ipl, IPL_NET);
-	ler0->ler0_status = LE_IE;
+	bus_space_write_1(bst, bsh0, LER0_STATUS, LE_IE);
 }
 
 int
@@ -225,4 +286,36 @@ leintr(arg)
 #endif /* USELEDS */
 
 	return (am7990_intr(sc));
+}
+
+hide void
+le_copytobuf(sc, from, boff, len)
+	struct am7990_softc *sc;
+	void *from;
+	int boff, len;
+{
+	struct le_softc *lesc = (struct le_softc *)sc;
+
+	bus_space_write_region_1(lesc->sc_bst, lesc->sc_bsh2, boff, from, len);
+}
+
+hide void
+le_copyfrombuf(sc, to, boff, len)
+	struct am7990_softc *sc;
+	void *to;
+	int boff, len;
+{
+	struct le_softc *lesc = (struct le_softc *)sc;
+
+	bus_space_read_region_1(lesc->sc_bst, lesc->sc_bsh2, boff, to, len);
+}
+
+hide void
+le_zerobuf(sc, boff, len)
+	struct am7990_softc *sc;
+	int boff, len;
+{
+	struct le_softc *lesc = (struct le_softc *)sc;
+
+	bus_space_set_region_1(lesc->sc_bst, lesc->sc_bsh2, boff, 0, len);
 }
