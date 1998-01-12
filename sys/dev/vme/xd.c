8742b48c3f83308f0d5a5d5168a90a3d69b14172
thorpej <thorpej@NetBSD.org>
1998-01-12 09:54:57 +0000

    Adjust for changes to config.

--- sys/dev/vme/xd.c
+++ sys/dev/vme/xd.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: xd.c,v 1.3 1997/12/01 23:25:39 pk Exp $	*/
+/*	$NetBSD: xd.c,v 1.4 1998/01/12 09:54:58 thorpej Exp $	*/
 
 /*
  *
@@ -36,7 +36,7 @@
  * x d . c   x y l o g i c s   7 5 3 / 7 0 5 3   v m e / s m d   d r i v e r
  *
  * author: Chuck Cranor <chuck@ccrc.wustl.edu>
- * id: $NetBSD: xd.c,v 1.3 1997/12/01 23:25:39 pk Exp $
+ * id: $NetBSD: xd.c,v 1.4 1998/01/12 09:54:58 thorpej Exp $
  * started: 27-Feb-95
  * references: [1] Xylogics Model 753 User's Manual
  *                 part number: 166-753-001, Revision B, May 21, 1988.
@@ -261,7 +261,7 @@ void	md_setup()
 }
 #endif
 /*
- * cfdrivers: device driver interface to autoconfig
+ * cfattach's: device driver interface to autoconfig
  */
 
 struct cfattach xdc_ca = {
@@ -269,17 +269,11 @@ struct cfattach xdc_ca = {
 };
 
 
-struct cfdriver xdc_cd = {
-	NULL, "xdc", DV_DULL
-};
-
 struct cfattach xd_ca = {
 	sizeof(struct xd_softc), xdmatch, xdattach
 };
 
-struct cfdriver xd_cd = {
-	NULL, "xd", DV_DISK
-};
+extern struct cfdriver xd_cd;
 
 struct xdc_attach_args {	/* this is the "aux" args to xdattach */
 	int	driveno;	/* unit number */
