01497ecff0e49ef3c7cf18b16ed029be531e2ea5
perry <perry@NetBSD.org>
1998-01-22 07:47:40 +0000

    import lite-2 onto vendor branch

--- lib/libutil/logwtmp.c
+++ lib/libutil/logwtmp.c
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1988 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1988, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -32,13 +32,15 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)logwtmp.c	5.5 (Berkeley) 6/1/90";
+static char sccsid[] = "@(#)logwtmp.c	8.1 (Berkeley) 6/4/93";
 #endif /* LIBC_SCCS and not lint */
 
 #include <sys/types.h>
 #include <sys/file.h>
 #include <sys/time.h>
 #include <sys/stat.h>
+
+#include <unistd.h>
 #include <utmp.h>
 
 logwtmp(line, name, host)
