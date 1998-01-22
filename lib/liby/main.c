d993f9c62a0f933c0a799ff87f3fc6cdfde8cfdd
perry <perry@NetBSD.org>
1998-01-22 06:45:34 +0000

    import lite-2 onto vendor branch

--- lib/liby/main.c
+++ lib/liby/main.c
@@ -1,6 +1,6 @@
 /*-
- * Copyright (c) 1990 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1990, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -32,7 +32,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)main.c	5.3 (Berkeley) 1/13/91";
+static char sccsid[] = "@(#)main.c	8.1 (Berkeley) 6/4/93";
 #endif /* not lint */
 
 main()
