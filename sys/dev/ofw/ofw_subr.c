2f2419bdb2d892e2e06dd1f250a15644201228f0
cgd <cgd>
1998-01-26 21:48:40 +0000

    new file containing common OFW-related routines.  Right now just contains of_decode_int(), a function which decodes OFW integer properties.

---
+++ sys/dev/ofw/ofw_subr.c
@@ -0,0 +1,51 @@
+/*	$NetBSD: ofw_subr.c,v 1.1 1998/01/26 21:48:40 cgd Exp $	*/
+
+/*
+ * Copyright 1998
+ * Digital Equipment Corporation. All rights reserved.
+ *
+ * This software is furnished under license and may be used and
+ * copied only in accordance with the following terms and conditions.
+ * Subject to these conditions, you may download, copy, install,
+ * use, modify and distribute this software in source and/or binary
+ * form. No title or ownership is transferred hereby.
+ *
+ * 1) Any source code used, modified or distributed must reproduce
+ *    and retain this copyright notice and list of conditions as
+ *    they appear in the source file.
+ *
+ * 2) No right is granted to use any trade name, trademark, or logo of
+ *    Digital Equipment Corporation. Neither the "Digital Equipment
+ *    Corporation" name nor any trademark or logo of Digital Equipment
+ *    Corporation may be used to endorse or promote products derived
+ *    from this software without the prior written permission of
+ *    Digital Equipment Corporation.
+ *
+ * 3) This software is provided "AS-IS" and any express or implied
+ *    warranties, including but not limited to, any implied warranties
+ *    of merchantability, fitness for a particular purpose, or
+ *    non-infringement are disclaimed. In no event shall DIGITAL be
+ *    liable for any damages whatsoever, and in particular, DIGITAL
+ *    shall not be liable for special, indirect, consequential, or
+ *    incidental damages or damages for lost profits, loss of
+ *    revenue or loss of use, whether such damages arise in contract,
+ *    negligence, tort, under statute, in equity, at law or otherwise,
+ *    even if advised of the possibility of such damage.
+ */
+
+/*
+ *  This routine converts OFW encoded-int datums
+ *  into the integer format of the host machine.
+ *
+ *  It is primarily used to convert integer properties
+ *  returned by the OF_getprop routine.
+ */
+int
+of_decode_int(p)
+	const unsigned char *p;
+{
+	unsigned int i = *p++ << 8;
+	i = (i + *p++) << 8;
+	i = (i + *p++) << 8;
+	return (i + *p);
+}
