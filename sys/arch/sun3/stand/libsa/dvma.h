de667f77128f5bc01ba0c5c4e31506493531ae78
gwr <gwr@NetBSD.org>
1998-01-27 02:22:05 +0000

    Define the DVMA functions as pointer dereferences.

--- sys/arch/sun3/stand/libsa/dvma.h
+++ sys/arch/sun3/stand/libsa/dvma.h
@@ -1,11 +1,62 @@
-/*	$NetBSD: dvma.h,v 1.3 1998/01/05 07:03:26 perry Exp $	*/
+/*	$NetBSD: dvma.h,v 1.3.2.1 1998/01/27 02:22:05 gwr Exp $	*/
 
+/*-
+ * Copyright (c) 1998 The NetBSD Foundation, Inc.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to The NetBSD Foundation
+ * by Gordon W. Ross.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *        This product includes software developed by the NetBSD
+ *        Foundation, Inc. and its contributors.
+ * 4. Neither the name of The NetBSD Foundation nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 
-void dvma_init();
+extern char *	(*dvma_alloc_p) __P((int len));
+#define dvma_alloc	(*dvma_alloc_p)
 
-char * dvma_mapin(char *pkt, int len);
-void dvma_mapout(char *dmabuf, int len);
+extern void	(*dvma_free_p) __P((char *dvma, int len));
+#define dvma_free	(*dvma_free_p)
 
-char * dvma_alloc(int len);
-void dvma_free(char *dvma, int len);
+extern char *	(*dvma_mapin_p) __P((char *pkt, int len));
+#define dvma_mapin	(*dvma_mapin_p)
 
+extern void	(*dvma_mapout_p) __P((char *dmabuf, int len));
+#define dvma_mapout	(*dvma_mapout_p)
+
+/*
+ * This stuff is not really DVMA-related,
+ * but is mapping related.  Oh well...
+ */
+extern char *	(*dev_mapin_p) __P((int t, u_long a, int l));
+#define dev_mapin	(*dev_mapin_p)
+
+/*
+ * Called from SRT1.c to set the pointers declared above.
+ */
+void	sun3_init __P((void));
+void	sun3x_init __P((void));
