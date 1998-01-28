f1271f740c80d465e2599a502d31b3a6f6d3c3df
cgd <cgd@NetBSD.org>
1998-01-28 00:01:01 +0000

    clean up comments and includes.  add an of_compatible() function which checks for the presence of any of a set of caller-provided strings in a node's "compatible" property (the _right_ way to determine driver compatibility).

--- sys/dev/ofw/ofw_subr.c
+++ sys/dev/ofw/ofw_subr.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: ofw_subr.c,v 1.1 1998/01/26 21:48:40 cgd Exp $	*/
+/*	$NetBSD: ofw_subr.c,v 1.2 1998/01/28 00:01:01 cgd Exp $	*/
 
 /*
  * Copyright 1998
@@ -33,12 +33,24 @@
  *    even if advised of the possibility of such damage.
  */
 
+#include <sys/param.h>
+#include <sys/systm.h>
+#include <sys/malloc.h>
+#include <dev/ofw/openfirm.h>
+
 /*
  *  This routine converts OFW encoded-int datums
  *  into the integer format of the host machine.
  *
  *  It is primarily used to convert integer properties
  *  returned by the OF_getprop routine.
+ *
+ * Arguments:
+ *	p		pointer to unsigned char array which is an
+ *			OFW-encoded integer.
+ *
+ * Return Value:
+ *	Decoded integer value of argument p.
  */
 int
 of_decode_int(p)
@@ -48,4 +60,71 @@ of_decode_int(p)
 	i = (i + *p++) << 8;
 	i = (i + *p++) << 8;
 	return (i + *p);
+}
+
+/*
+ * This routine checks an OFW node's "compatible" entry to see if
+ * it matches any of the provided strings.
+ *
+ * It should be used when determining whether a driver can drive
+ * a partcular device.
+ *
+ *
+ * Arguments:
+ *	phandle		OFW phandle of device to be checked for
+ *			compatibility.
+ *	strings		Array of containing expected "compatibility"
+ *			property values, presence of any of which
+ *			indicates compatibility.
+ *
+ * Return Value:
+ *	-1 if none of the strings are found in phandle's "compatiblity"
+ *	property, or the index of the string in "strings" of the first
+ *	string found in phandle's "compatibility" property.
+ */
+int
+of_compatible(phandle, strings)
+	int phandle;
+	const char * const *strings;
+{
+	int len, allocated, rv;
+	char *buf;
+	const char *sp, *nsp;
+
+	len = OF_getproplen(phandle, "compatible");
+	if (len <= 0)
+		return (-1);
+
+	if (len > 256) {
+		buf = malloc(len, M_TEMP, M_WAITOK);
+		allocated = 1;
+	} else {
+		buf = alloca(len);
+		allocated = 0;
+	}
+
+	/* 'compatible' size should not change. */
+	if (OF_getprop(phandle, "compatible", buf, len) != len) {
+		rv = -1;
+		goto out;
+	}
+
+	sp = buf;
+	while (len && (nsp = memchr(sp, 0, len)) != NULL) {
+		/* look for a match among the strings provided */
+		for (rv = 0; strings[rv] != NULL; rv++)
+			if (strcmp(sp, strings[rv]) == 0)
+				goto out;
+
+		nsp++;			/* skip over NUL char */
+		len -= (nsp - sp);
+		sp = nsp;
+	}
+	rv = -1;
+
+out:
+	if (allocated)
+		free(buf, M_TEMP);
+	return (rv);
+	
 }
