0432372a327cc81887bb1bd973b3b24ab7db0897
perry <perry@NetBSD.org>
1998-02-02 06:07:48 +0000

    import lite-2

--- lib/libc/compat-43/killpg.c
+++ lib/libc/compat-43/killpg.c
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1989 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1989, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -32,7 +32,7 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)killpg.c	5.3 (Berkeley) 2/24/91";
+static char sccsid[] = "@(#)killpg.c	8.1 (Berkeley) 6/2/93";
 #endif /* LIBC_SCCS and not lint */
 
 #include <sys/types.h>
