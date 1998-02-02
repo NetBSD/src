9f6c3e59aa503f6e473b4086a24dc9c9d194f2b5
perry <perry@NetBSD.org>
1998-02-02 06:26:33 +0000

    merge lite-2

--- lib/libc/compat-43/sigcompat.c
+++ lib/libc/compat-43/sigcompat.c
@@ -1,8 +1,8 @@
-/*	$NetBSD: sigcompat.c,v 1.7 1997/07/13 18:50:14 christos Exp $	*/
+/*	$NetBSD: sigcompat.c,v 1.8 1998/02/02 06:26:43 perry Exp $	*/
 
 /*
- * Copyright (c) 1989 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1989, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -36,9 +36,9 @@
 #include <sys/cdefs.h>
 #if defined(LIBC_SCCS) && !defined(lint)
 #if 0
-static char *sccsid = "@(#)sigcompat.c	5.3 (Berkeley) 2/24/91";
+static char sccsid[] = "@(#)sigcompat.c	8.1 (Berkeley) 6/2/93";
 #else
-__RCSID("$NetBSD: sigcompat.c,v 1.7 1997/07/13 18:50:14 christos Exp $");
+__RCSID("$NetBSD: sigcompat.c,v 1.8 1998/02/02 06:26:43 perry Exp $");
 #endif
 #endif /* LIBC_SCCS and not lint */
 
