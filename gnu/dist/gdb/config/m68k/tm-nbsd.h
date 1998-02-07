838a388357fef872a6cd76c71c6758296e3a0125
mellon <mellon>
1998-02-07 07:55:36 +0000

    Pull up 1.4 (gwr)

--- gnu/dist/gdb/config/m68k/tm-nbsd.h
+++ gnu/dist/gdb/config/m68k/tm-nbsd.h
@@ -24,22 +24,16 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
 #define BPT_VECTOR 0xf
 #define REMOTE_BPT_VECTOR 0xf
 
-/* Address of end of stack space.  */
-#if 0 /* XXX: Need to determine this at run-time! */
-extern CORE_ADDR get_stack_end_addr();
-#define STACK_END_ADDR get_stack_end_addr();
-/* For NetBSD, sigtramp is 32 bytes before STACK_END_ADDR.  */
-#define SIGTRAMP_START (STACK_END_ADDR - 32)
-#define SIGTRAMP_END (STACK_END_ADDR)
-#endif
-
-/* Saved Pc.  Get it from sigcontext if within sigtramp.  */
-
-/* Offset to saved PC in sigcontext, from <machine/signal.h>.  */
-#define SIGCONTEXT_PC_OFFSET 20
+/* For NetBSD, sigtramp is 32 bytes before STACK_END_ADDR,
+   but we don't know where that is until run-time!  */
+extern int nbsd_in_sigtramp(CORE_ADDR);
+#define IN_SIGTRAMP(pc, name) nbsd_in_sigtramp (pc)
 
 /* Generic definitions.  */
 #include "m68k/tm-m68k.h"
 #include "tm-nbsd.h"
+
+/* Offset to saved PC in sigcontext, from <m68k/signal.h>.  */
+#define SIGCONTEXT_PC_OFFSET 20
 
 #endif /* TM_NBSD_H */
