71e1170e857b8c40191bab55dc8253324dc6ca13
chs <chs>
1998-02-07 02:13:00 +0000

    GC unused stuff.

--- sys/kern/kern_fthread.c
+++ sys/kern/kern_fthread.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: kern_fthread.c,v 1.1 1998/02/05 14:58:45 mrg Exp $	*/
+/*	$NetBSD: kern_fthread.c,v 1.2 1998/02/07 02:13:00 chs Exp $	*/
 
 /* 
  * Copyright (c) 1991, 1993
@@ -77,32 +77,6 @@
  * these routines fake thread handling
  */
 
-#if !defined(UVM)
-
-void
-assert_wait(event, ruptible)
-	void *event;
-	boolean_t ruptible;
-{
-#ifdef lint
-	ruptible++;
-#endif
-	curproc->p_thread = event;
-}
-
-void
-thread_block(msg)
-char *msg;
-{
-	int s = splhigh();
-
-	if (curproc->p_thread)
-		tsleep(curproc->p_thread, PVM, msg, 0);
-	splx(s);
-}
-
-#endif
-
 void
 thread_sleep_msg(event, lock, ruptible, msg, timo)
 	void *event;
@@ -116,51 +90,7 @@ thread_sleep_msg(event, lock, ruptible, msg, timo)
 #ifdef lint
 	ruptible++;
 #endif
-	curproc->p_thread = event;
 	simple_unlock(lock);
-	if (curproc->p_thread)
-		tsleep(event, PVM, msg, timo);
+	tsleep(event, PVM, msg, timo);
 	splx(s);
-}
-
-/*
- * DEBUG stuff
- */
-
-int indent = 0;
-
-/*
- * Note that stdarg.h and the ANSI style va_start macro is used for both
- * ANSI and traditional C compilers.  (Same as subr_prf.c does.)
- * XXX: This requires that stdarg.h defines: va_alist, va_dcl
- */
-#include <machine/stdarg.h>
-
-/*ARGSUSED2*/
-void
-#ifdef	__STDC__
-iprintf(void (*pr)(const char *, ...), const char *fmt, ...)
-#else
-iprintf(pr, fmt, va_alist)
-	void (*pr)();
-	const char *fmt;
-	va_dcl
-#endif
-{
-	register int i;
-	va_list ap;
-
-	va_start(ap, fmt);
-	for (i = indent; i >= 8; i -= 8)
-		(*pr)("\t");
-	while (--i >= 0)
-		(*pr)(" ");
-#ifdef __powerpc__				/* XXX */
-	if (pr != printf)			/* XXX */
-		panic("iprintf");		/* XXX */
-	vprintf(fmt, ap);			/* XXX */
-#else						/* XXX */
-	(*pr)("%:", fmt, ap);			/* XXX */
-#endif /* __powerpc__ */			/* XXX */
-	va_end(ap);
 }
