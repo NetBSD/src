f9b8844db8d8d1f3dd2791b950ec6e1d7af3941f
mellon <mellon@NetBSD.org>
1998-02-08 21:50:49 +0000

    Update value of NetBSD constant (we *really* need a better way).

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.39.2.3 1997/12/21 00:03:04 perry Exp $	*/
+/*	$NetBSD: param.h,v 1.39.2.4 1998/02/08 21:50:49 mellon Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -47,7 +47,7 @@
 #define BSD4_3	1
 #define BSD4_4	1
 
-#define NetBSD	199712		/* NetBSD version (year & month). */
+#define NetBSD	199713		/* NetBSD version (year & month). */
 #define NetBSD1_3 1		/* NetBSD 1.3 */
 
 #ifndef NULL
