227e717214e9fc101b22790bb3690243b700890f
gwr <gwr@NetBSD.org>
1998-02-08 05:13:17 +0000

    cosmetic

--- sys/arch/sun3/dev/p4reg.h
+++ sys/arch/sun3/dev/p4reg.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: p4reg.h,v 1.2 1997/10/05 18:25:43 thorpej Exp $	*/
+/*	$NetBSD: p4reg.h,v 1.3 1998/02/08 05:13:17 gwr Exp $	*/
 
 /*-
  * Copyright (c) 1996, 1997 The NetBSD Foundation, Inc.
@@ -67,49 +67,51 @@
  */
 
 /*
- * pfour framebuffer registers.
+ * P4 framebuffer register.  Most Sun framebuffers have a P4
+ * register at the beginning of the physical space occupied
+ * by the framebuffer that identifies its type, size, etc.
  */
 
-/* Offset of bwtwo framebuffer from pfour register */
-#define PFOUR_BW_OFF		0x00100000
+#define P4_REG_DIAG		0x80
+#define P4_REG_READBACKCLR	0x40
+#define P4_REG_VIDEO		0x20
+#define P4_REG_SYNC		0x10
+#define P4_REG_VTRACE		0x08
+#define	P4_REG_INT		0x04	/* r: pending */
+#define	P4_REG_INTCLR		0x04	/* w: clear it */
+#define	P4_REG_INTEN		0x02
+#define P4_REG_FIRSTHALF	0x01
+#define P4_REG_RESET		0x01
+
+#define P4_FBTYPE_MASK	0x7f000000
+#define	P4_FBTYPE(x)		((x) >> 24)
+
+#define	P4_ID_MASK		0xf0
+#define P4_ID(x)		(P4_FBTYPE((x)) == P4_ID_COLOR24 ? \
+				    P4_ID_COLOR24 : \
+				    P4_FBTYPE((x)) & P4_ID_MASK)
+
+#define P4_NOTFOUND		-1	/* no P4 register */
+#define P4_ID_BW		0x00	/* BW2: monochrome */
+#define P4_ID_FASTCOLOR 	0x60	/* CG6: accelerated 8-bit color */
+#define P4_ID_COLOR8P1		0x40	/* CG4: 8-bit color + overlay */
+#define P4_ID_COLOR24		0x45	/* CG8: 24-bit color + overlay */
+
+#define P4_SIZE_MASK		0x0f
+#define P4_SIZE(x)		(P4_FBTYPE((x)) & P4_SIZE_MASK)
+#define P4_SIZE_1600X1280	0x00
+#define P4_SIZE_1152X900	0x01
+#define P4_SIZE_1024X1024	0x02
+#define P4_SIZE_1280X1024	0x03
+#define P4_SIZE_1440X1440	0x04
+#define P4_SIZE_640X480	0x05
+
+/* Offset of bwtwo framebuffer from P4 register */
+#define	P4_BW_OFF		0x00100000
 
 /* Offsets for color framebuffers */
-#define PFOUR_COLOR_OFF_OVERLAY	0x00100000
-#define PFOUR_COLOR_OFF_ENABLE	0x00300000
-#define PFOUR_COLOR_OFF_COLOR	0x00500000
-#define PFOUR_COLOR_OFF_END	0x00700000
-#define PFOUR_COLOR_OFF_CMAP	0xfff00000	/* (-0x00100000) */
-
-#define PFOUR_REG_DIAG		0x80
-#define PFOUR_REG_READBACKCLR	0x40
-#define PFOUR_REG_VIDEO		0x20
-#define PFOUR_REG_SYNC		0x10
-#define PFOUR_REG_VTRACE	0x08
-#define PFOUR_REG_INT		0x04
-#define PFOUR_REG_INTCLR	0x04
-#define PFOUR_REG_INTEN		0x02
-#define PFOUR_REG_FIRSTHALF	0x01
-#define PFOUR_REG_RESET		0x01
-
-#define PFOUR_FBTYPE_MASK	0x7f000000
-#define	PFOUR_FBTYPE(x)		((x) >> 24)
-
-#define	PFOUR_ID_MASK		0xf0
-#define PFOUR_ID(x)		(PFOUR_FBTYPE((x)) == PFOUR_ID_COLOR24 ? \
-				    PFOUR_ID_COLOR24 : \
-				    PFOUR_FBTYPE((x)) & PFOUR_ID_MASK)
-#define PFOUR_ID_BW		0x00	/* monochrome */
-#define PFOUR_ID_FASTCOLOR	0x60	/* accelerated 8-bit color */
-#define PFOUR_ID_COLOR8P1	0x40	/* 8-bit color + overlay */
-#define PFOUR_ID_COLOR24	0x45	/* 24-bit color + overlay */
-
-#define PFOUR_SIZE_MASK		0x0f
-#define PFOUR_SIZE(x)		(PFOUR_FBTYPE((x)) & PFOUR_SIZE_MASK)
-#define PFOUR_SIZE_1152X900	0x01
-#define PFOUR_SIZE_1024X1024	0x02
-#define PFOUR_SIZE_1280X1024	0x03
-#define PFOUR_SIZE_1600X1280	0x00
-#define PFOUR_SIZE_1440X1440	0x04
-#define PFOUR_SIZE_640X480	0x05
-
-#define PFOUR_NOTPFOUR		-1
+#define P4_COLOR_OFF_OVERLAY	0x00100000
+#define P4_COLOR_OFF_ENABLE	0x00300000
+#define P4_COLOR_OFF_COLOR	0x00500000
+#define P4_COLOR_OFF_END	0x00700000
+#define P4_COLOR_OFF_CMAP	0xfff00000	/* (-0x00100000) */
