ed1ff1c6ea685b4fc64a3258d6c4d5dd5dfbf947
thorpej <thorpej@NetBSD.org>
1998-02-19 23:09:30 +0000

    Implement new style crash dumps for NetBSD/mips, lifted from NetBSD/alpha.

--- sys/arch/mips/mips/mips_machdep.c
+++ sys/arch/mips/mips/mips_machdep.c
@@ -1,4 +1,41 @@
-/*	$NetBSD: mips_machdep.c,v 1.21 1997/10/17 09:35:11 jonathan Exp $	*/
+/*	$NetBSD: mips_machdep.c,v 1.22 1998/02/19 23:09:30 thorpej Exp $	*/
+
+/*-
+ * Copyright (c) 1998 The NetBSD Foundation, Inc.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to The NetBSD Foundation
+ * by Jason R. Thorpe of the Numerical Aerospace Simulation Facility,
+ * NASA Ames Research Center and by Chris Demetriou.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed by the NetBSD
+ *	Foundation, Inc. and its contributors.
+ * 4. Neither the name of The NetBSD Foundation nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 
 /*
  * Copyright 1996 The Board of Trustees of The Leland Stanford
@@ -15,7 +52,7 @@
 
 #include <sys/cdefs.h>			/* RCS ID & Copyright macro defns */
 
-__KERNEL_RCSID(0, "$NetBSD: mips_machdep.c,v 1.21 1997/10/17 09:35:11 jonathan Exp $");
+__KERNEL_RCSID(0, "$NetBSD: mips_machdep.c,v 1.22 1998/02/19 23:09:30 thorpej Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -27,15 +64,26 @@ __KERNEL_RCSID(0, "$NetBSD: mips_machdep.c,v 1.21 1997/10/17 09:35:11 jonathan E
 #include <sys/signalvar.h>
 #include <sys/syscallargs.h>
 #include <sys/user.h>
+#include <sys/msgbuf.h>
+#include <sys/conf.h>
+#include <sys/core.h>
+#include <sys/kcore.h>  
+#include <machine/kcore.h>
 
 #include <vm/vm.h>
 
 #include <mips/cpu.h>			/* declaration of of cpu_id */
-#include <mips/regnum.h>			/* symbolic register indices */
+#include <mips/regnum.h>		/* symbolic register indices */
 #include <mips/locore.h>
-#include <mips/vmparam.h>			/* USRSTACK */
+#include <mips/vmparam.h>		/* USRSTACK */
 #include <mips/psl.h>
+#include <mips/pte.h>
 #include <machine/cpu.h>		/* declaration of of cpu_id */
+
+/* Internal routines. */
+int	cpu_dumpsize __P((void));
+u_long	cpu_dump_mempagecnt __P((void));
+int	cpu_dump __P((void));
 
 mips_locore_jumpvec_t mips_locore_jumpvec = {
   NULL, NULL, NULL, NULL,
@@ -629,3 +677,266 @@ sys_sigreturn(p, v, retval)
 	return (EJUSTRETURN);
 }
 
+/*
+ * These are imported from platform-specific code.
+ */
+extern phys_ram_seg_t mem_clusters[];
+extern int mem_cluster_cnt;
+
+/*
+ * These are imported from pmap.c
+ */
+extern pt_entry_t *Sysmap;
+extern u_int Sysmapsize;
+
+/*
+ * These variables are needed by /sbin/savecore.
+ */
+u_long	dumpmag = 0x8fca0101;	/* magic number */
+int	dumpsize = 0;		/* pages */
+long	dumplo = 0;		/* blocks */
+
+struct user dumppcb;		/* Actually, struct pcb would do. */
+
+/*
+ * cpu_dumpsize: calculate size of machine-dependent kernel core dump headers.
+ */
+int
+cpu_dumpsize()
+{
+	int size;
+
+	size = ALIGN(sizeof(kcore_seg_t)) + ALIGN(sizeof(cpu_kcore_hdr_t)) +
+	    ALIGN(mem_cluster_cnt * sizeof(phys_ram_seg_t));
+	if (roundup(size, dbtob(1)) != dbtob(1))
+		return (-1);
+
+	return (1);
+}
+
+/*
+ * cpu_dump_mempagecnt: calculate size of RAM (in pages) to be dumped.
+ */
+u_long
+cpu_dump_mempagecnt()
+{
+	u_long i, n;
+
+	n = 0;
+	for (i = 0; i < mem_cluster_cnt; i++)
+		n += atop(mem_clusters[i].size);
+	return (n);
+}
+
+/*
+ * cpu_dump: dump machine-dependent kernel core dump headers.
+ */
+int
+cpu_dump()
+{
+	int (*dump) __P((dev_t, daddr_t, caddr_t, size_t));
+	char buf[dbtob(1)];
+	kcore_seg_t *segp;
+	cpu_kcore_hdr_t *cpuhdrp;
+	phys_ram_seg_t *memsegp;
+	int i;
+
+	dump = bdevsw[major(dumpdev)].d_dump;
+
+	bzero(buf, sizeof buf);
+	segp = (kcore_seg_t *)buf;
+	cpuhdrp = (cpu_kcore_hdr_t *)&buf[ALIGN(sizeof(*segp))];
+	memsegp = (phys_ram_seg_t *)&buf[ ALIGN(sizeof(*segp)) +
+	    ALIGN(sizeof(*cpuhdrp))];
+
+	/*
+	 * Generate a segment header.
+	 */
+	CORE_SETMAGIC(*segp, KCORE_MAGIC, MID_MACHINE, CORE_CPU);
+	segp->c_size = dbtob(1) - ALIGN(sizeof(*segp));
+
+	/*
+	 * Add the machine-dependent header info.
+	 */
+	if (CPUISMIPS3) {
+		cpuhdrp->archlevel = 3;
+		cpuhdrp->pg_shift  = MIPS3_PG_SHIFT;
+		cpuhdrp->pg_frame  = MIPS3_PG_FRAME;
+		cpuhdrp->pg_v      = MIPS3_PG_V;
+	} else {
+		cpuhdrp->archlevel = 1;
+		cpuhdrp->pg_shift  = MIPS1_PG_SHIFT;
+		cpuhdrp->pg_frame  = MIPS1_PG_FRAME;
+		cpuhdrp->pg_v      = MIPS1_PG_V;
+	}
+	cpuhdrp->sysmappa   = MIPS_KSEG0_TO_PHYS(Sysmap);
+	cpuhdrp->sysmapsize = Sysmapsize;
+	cpuhdrp->nmemsegs   = mem_cluster_cnt;
+
+	/*
+	 * Fill in the memory segment descriptors.
+	 */
+	for (i = 0; i < mem_cluster_cnt; i++) {
+		memsegp[i].start = mem_clusters[i].start;
+		memsegp[i].size = mem_clusters[i].size;
+	}
+
+	return (dump(dumpdev, dumplo, (caddr_t)buf, dbtob(1)));
+}
+
+/*
+ * This is called by main to set dumplo and dumpsize.
+ * Dumps always skip the first CLBYTES of disk space
+ * in case there might be a disk label stored there.
+ * If there is extra space, put dump at the end to
+ * reduce the chance that swapping trashes it.
+ */
+void
+cpu_dumpconf()
+{
+	int nblks, dumpblks;	/* size of dump area */
+	int maj;
+
+	if (dumpdev == NODEV)
+		goto bad;
+	maj = major(dumpdev);
+	if (maj < 0 || maj >= nblkdev)
+		panic("dumpconf: bad dumpdev=0x%x", dumpdev);
+	if (bdevsw[maj].d_psize == NULL)
+		goto bad;
+	nblks = (*bdevsw[maj].d_psize)(dumpdev);
+	if (nblks <= ctod(1)) 
+		goto bad;
+
+	dumpblks = cpu_dumpsize(); 
+	if (dumpblks < 0)
+		goto bad;
+	dumpblks += ctod(cpu_dump_mempagecnt());
+
+	/* If dump won't fit (incl. room for possible label), punt. */
+	if (dumpblks > (nblks - ctod(1)))
+		goto bad;
+
+	/* Put dump at end of partition */
+	dumplo = nblks - dumpblks;
+
+	/* dumpsize is in page units, and doesn't include headers. */
+	dumpsize = cpu_dump_mempagecnt();
+	return;
+
+ bad:
+	dumpsize = 0;
+}
+
+/*
+ * Dump the kernel's image to the swap partition.
+ */
+#define	BYTES_PER_DUMP	NBPG
+
+void
+dumpsys()
+{
+	u_long totalbytesleft, bytes, i, n, memcl;
+	u_long maddr;
+	int psize;
+	daddr_t blkno;
+	int (*dump) __P((dev_t, daddr_t, caddr_t, size_t));
+	int error;
+
+	/* Save registers. */
+	savectx(&dumppcb);
+
+	msgbufenabled = 0;	/* don't record dump msgs in msgbuf */
+	if (dumpdev == NODEV)
+		return;
+
+	/*
+	 * For dumps during autoconfiguration,
+	 * if dump device has already configured...
+	 */
+	if (dumpsize == 0)
+		cpu_dumpconf();
+	if (dumplo <= 0) {
+		printf("\ndump to dev %u,%u not possible\n", major(dumpdev),
+		    minor(dumpdev));
+		return;
+	}
+	printf("\ndumping to dev %u,%u offset %ld\n", major(dumpdev),
+	    minor(dumpdev), dumplo);
+
+	psize = (*bdevsw[major(dumpdev)].d_psize)(dumpdev);
+	printf("dump ");
+	if (psize == -1) {
+		printf("area unavailable\n");
+		return;
+	}
+
+	/* XXX should purge all outstanding keystrokes. */
+
+	if ((error = cpu_dump()) != 0)
+		goto err;
+
+	totalbytesleft = ptoa(cpu_dump_mempagecnt());
+	blkno = dumplo + cpu_dumpsize();
+	dump = bdevsw[major(dumpdev)].d_dump;
+	error = 0;
+
+	for (memcl = 0; memcl < mem_cluster_cnt; memcl++) {
+		maddr = mem_clusters[memcl].start;
+		bytes = mem_clusters[memcl].size;
+
+		for (i = 0; i < bytes; i += n, totalbytesleft -= n) {
+			/* Print out how many MBs we have left to go. */
+			if ((totalbytesleft % (1024*1024)) == 0)
+				printf("%ld ", totalbytesleft / (1024 * 1024));
+
+			/* Limit size for next transfer. */
+			n = bytes - i;
+			if (n > BYTES_PER_DUMP)
+				n = BYTES_PER_DUMP;
+
+			error = (*dump)(dumpdev, blkno,
+			    (caddr_t)MIPS_PHYS_TO_KSEG0(maddr), n);
+			if (error)
+				goto err;
+			maddr += n;
+			blkno += btodb(n);		/* XXX? */
+
+			/* XXX should look for keystrokes, to cancel. */
+		}
+	}
+
+ err:
+	switch (error) {
+
+	case ENXIO:
+		printf("device bad\n");
+		break;
+
+	case EFAULT:
+		printf("device not ready\n");
+		break;
+
+	case EINVAL:
+		printf("area improper\n");
+		break;
+
+	case EIO:
+		printf("i/o error\n");
+		break;
+
+	case EINTR:
+		printf("aborted from console\n");
+		break;
+
+	case 0:
+		printf("succeeded\n");
+		break;
+
+	default:
+		printf("error %d\n", error);
+		break;
+	}
+	printf("\n\n");
+	delay(5000000);		/* 5 seconds */
+}
