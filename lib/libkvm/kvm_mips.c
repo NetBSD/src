5af26b83d75429e6310193578a3c75aa9e7f90e4
thorpej <thorpej@NetBSD.org>
1998-02-19 23:32:35 +0000

    Rewrite NetBSD/mips crash dump address translation, based on NetBSD/alpha's, using new crash dump format.

--- lib/libkvm/kvm_mips.c
+++ lib/libkvm/kvm_mips.c
@@ -1,60 +1,52 @@
-/*	$NetBSD: kvm_mips.c,v 1.9 1998/02/03 19:12:43 perry Exp $	*/
+/*	$NetBSD: kvm_mips.c,v 1.10 1998/02/19 23:32:35 thorpej Exp $	*/
 
-/*-
- * Copyright (c) 1989, 1992, 1993
- *	The Regents of the University of California.  All rights reserved.
+/*
+ * Copyright (c) 1994, 1995 Carnegie-Mellon University.
+ * All rights reserved.
  *
- * This code is derived from software developed by the Computer Systems
- * Engineering group at Lawrence Berkeley Laboratory under DARPA contract
- * BG 91-66 and contributed to Berkeley. Modified for MIPS by Ralph Campbell.
+ * Author: Chris G. Demetriou
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed by the University of
- *	California, Berkeley and its contributors.
- * 4. Neither the name of the University nor the names of its contributors
- *    may be used to endorse or promote products derived from this software
- *    without specific prior written permission.
+ * Permission to use, copy, modify and distribute this software and
+ * its documentation is hereby granted, provided that both the copyright
+ * notice and this permission notice appear in all copies of the
+ * software, derivative works or modified versions, and any portions
+ * thereof, and that both notices appear in supporting documentation.
+ *      
+ * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS "AS IS"
+ * CONDITION.  CARNEGIE MELLON DISCLAIMS ANY LIABILITY OF ANY KIND
+ * FOR ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.
+ *      
+ * Carnegie Mellon requests users of this software to return to 
+ *                      
+ *  Software Distribution Coordinator  or  Software.Distribution@CS.CMU.EDU
+ *  School of Computer Science
+ *  Carnegie Mellon University
+ *  Pittsburgh PA 15213-3890
  *
- * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
+ * any improvements or extensions that they make and grant Carnegie the
+ * rights to redistribute these changes.
+ */     
+
+/*
+ * Modified for NetBSD/mips by Jason R. Thorpe, Numerical Aerospace
+ * Simulation Facility, NASA Ames Research Center.
  */
 
 #include <sys/cdefs.h>
 #if defined(LIBC_SCCS) && !defined(lint)
-#if 0
-static char sccsid[] = "@(#)kvm_mips.c	8.1 (Berkeley) 6/4/93";
-#else
-__RCSID("$NetBSD: kvm_mips.c,v 1.9 1998/02/03 19:12:43 perry Exp $");
-#endif
+__RCSID("$NetBSD: kvm_mips.c,v 1.10 1998/02/19 23:32:35 thorpej Exp $");
 #endif /* LIBC_SCCS and not lint */
 
 /*
- * MIPS machine dependent routines for kvm.  Hopefully, the forthcoming 
- * vm code will one day obsolete this module.
+ * MIPS machine dependent routines for kvm.
  */
 
 #include <sys/param.h>
 #include <sys/user.h>
 #include <sys/proc.h>
 #include <sys/stat.h>
+#include <sys/kcore.h>
+#include <machine/kcore.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <nlist.h>
@@ -69,23 +61,13 @@ __RCSID("$NetBSD: kvm_mips.c,v 1.9 1998/02/03 19:12:43 perry Exp $");
 #include "kvm_private.h"
 
 #include <mips/cpuregs.h>
-#include <mips/mips1_pte.h>
-#include <mips/mips3_pte.h>
-#include <mips/pmap.h>
-
-struct vmstate {
-	/*pt_entry_t*/u_int	*Sysmap;
-	u_int		Sysmapsize;
-	u_int		cpu_arch;
-};
-
-#define KREAD(kd, addr, p)\
-	(kvm_read(kd, addr, (char *)(p), sizeof(*(p))) != sizeof(*(p)))
 
 void
 _kvm_freevtop(kd)
 	kvm_t *kd;
 {
+
+	/* Not actually used for anything right now, but safe. */
 	if (kd->vmst != 0)
 		free(kd->vmst);
 }
@@ -94,35 +76,7 @@ int
 _kvm_initvtop(kd)
 	kvm_t *kd;
 {
-	struct vmstate *vm;
-	struct nlist nlist[4];
 
-	vm = (struct vmstate *)_kvm_malloc(kd, sizeof(*vm));
-	if (vm == 0)
-		return (-1);
-	kd->vmst = vm;
-
-	nlist[0].n_name = "cpu_arch";
-	nlist[1].n_name = "Sysmap";
-	nlist[2].n_name = "Sysmapsize";
-	nlist[3].n_name = 0;
-
-	if (kvm_nlist(kd, nlist) != 0) {
-		_kvm_err(kd, kd->program, "bad namelist");
-		return (-1);
-	}
-	if (KREAD(kd, (u_long)nlist[0].n_value, &vm->cpu_arch)) {
-		_kvm_err(kd, kd->program, "cannot read cpu_arch");
-		return (-1);
-	}
-	if (KREAD(kd, (u_long)nlist[1].n_value, &vm->Sysmap)) {
-		_kvm_err(kd, kd->program, "cannot read Sysmap");
-		return (-1);
-	}
-	if (KREAD(kd, (u_long)nlist[2].n_value, &vm->Sysmapsize)) {
-		_kvm_err(kd, kd->program, "cannot read mmutype");
-		return (-1);
-	}
 	return (0);
 }
 
@@ -135,70 +89,112 @@ _kvm_kvatop(kd, va, pa)
 	u_long va;
 	u_long *pa;
 {
-	struct vmstate *vm;
-	u_long pte, addr, offset;
-
-	u_int pgshift, pg_v, pg_frame;
+	cpu_kcore_hdr_t *cpu_kh;
+	int page_off;
+	u_int pte;
+	u_long pte_pa;
 
 	if (ISALIVE(kd)) {
 		_kvm_err(kd, 0, "vatop called in live kernel!");
 		return((off_t)0);
 	}
 
-	/* Compute TLB offsets for the level of mips cpu we're running on */
-	vm = kd->vmst;
-	switch (vm->cpu_arch) {
-	case 1:
-		pgshift = MIPS1_PG_SHIFT;
-		pg_v = MIPS1_PG_V;
-		pg_frame = MIPS1_PG_FRAME;
-		break;
+	cpu_kh = kd->cpu_data;
+	page_off = va & PGOFSET;
 
-	case 3:
-		pgshift = MIPS3_PG_SHIFT;
-		pg_v = MIPS3_PG_V;
-		pg_frame = MIPS3_PG_FRAME;
-		break;
+	if (va < MIPS_KSEG0_START) {
+		/*
+		 * KUSEG (user virtual address space) - invalid.
+		 */
+		_kvm_err(kd, 0, "invalid kernel virtual address");
+		goto lose;
+	}
 
-	default:
-		_kvm_err(kd, 0, "unknown or unsupported mips CPU family!");
-		return((off_t)0);
-	}
-	
-	offset = va & PGOFSET;
-	/*
-	 * If we are initializing (kernel segment table pointer not yet set)
-	 * then return pa == va to avoid infinite recursion.
-	 */
-	if (vm->Sysmap == 0) {
-		*pa = va;
-		return (NBPG - offset);
-	}
-	if (va < KERNBASE ||
-	    va >= VM_MIN_KERNEL_ADDRESS + vm->Sysmapsize * NBPG)
-		goto invalid;
-	if (va < VM_MIN_KERNEL_ADDRESS) {
+	if (va >= MIPS_KSEG0_START && va < MIPS_KSEG1_START) {
+		/*
+		 * Direct-mapped cached address: just convert it.
+		 */
 		*pa = MIPS_KSEG0_TO_PHYS(va);
-		return (NBPG - offset);
+		return (NBPG - page_off);
 	}
-	addr = (u_long)(vm->Sysmap +
-	    ((va - VM_MIN_KERNEL_ADDRESS) >> pgshift));
+
+	if (va >= MIPS_KSEG1_START && va < MIPS_KSEG2_START) {
+		/*
+		 * Direct-mapped uncached address: just convert it.
+		 */
+		*pa = MIPS_KSEG1_TO_PHYS(va);
+		return (NBPG - page_off);
+	}
 
 	/*
-	 * Can't use KREAD to read kernel segment table entries.
-	 * Fortunately it is 1-to-1 mapped so we don't have to. 
+	 * We now know that we're a KSEG2 (kernel virtually mapped)
+	 * address.  Translate the address using the pmap's kernel
+	 * page table.
 	 */
-	if (lseek(kd->pmfd, (off_t)addr, 0) < 0 ||
-	    read(kd->pmfd, (char *)&pte, sizeof(pte)) < 0)
-		goto invalid;
-	if (!(pte & pg_v))
-		goto invalid;
-	*pa = (pte & pg_frame) | offset;
-	return (NBPG - offset);
 
-invalid:
-	_kvm_err(kd, 0, "invalid address (%x)", va);
+	/*
+	 * Step 1: Make sure the kernel page table has a translation
+	 * for the address.
+	 */
+	if (va >= (MIPS_KSEG2_START + (cpu_kh->sysmapsize * NBPG))) {
+		_kvm_err(kd, 0, "invalid KSEG2 address");
+		goto lose;
+	}
+
+	/*
+	 * Step 2: Locate and read the PTE.
+	 */
+	pte_pa = cpu_kh->sysmappa +
+	    (((va - MIPS_KSEG2_START) >> PGSHIFT) * sizeof(u_int));
+	if (lseek(kd->pmfd, _kvm_pa2off(kd, pte_pa), SEEK_SET) == -1 ||
+	    read(kd->pmfd, (char *)&pte, sizeof(pte)) != sizeof(pte)) {
+		_kvm_syserr(kd, 0, "could not read PTE");
+		goto lose;
+	}
+
+	/*
+	 * Step 3: Validate the PTE and return the physical address.
+	 */
+	if ((pte & cpu_kh->pg_v) == 0) {
+		_kvm_err(kd, 0, "invalid translation (invalid PTE)");
+		goto lose;
+	}
+	*pa = (((pte & cpu_kh->pg_frame) >> cpu_kh->pg_shift) << PGSHIFT) +
+	    page_off;
+	return (NBPG - page_off);
+
+ lose:
+	*pa = -1;
 	return (0);
+}
+
+/*
+ * Translate a physical address to a file-offset in the crash-dump.
+ */
+off_t
+_kvm_pa2off(kd, pa)
+	kvm_t *kd;
+	u_long pa;
+{
+	cpu_kcore_hdr_t *cpu_kh;
+	phys_ram_seg_t *ramsegs;
+	off_t off;
+	int i;
+
+	cpu_kh = kd->cpu_data;
+	ramsegs = (phys_ram_seg_t *)((char *)cpu_kh + ALIGN(sizeof *cpu_kh));
+
+	off = 0;
+	for (i = 0; i < cpu_kh->nmemsegs; i++) {
+		if (pa >= ramsegs[i].start &&
+		    (pa - ramsegs[i].start) < ramsegs[i].size) {
+			off += (pa - ramsegs[i].start);
+			break;
+		}
+		off += ramsegs[i].size;
+	}
+
+	return (kd->dump_off + off);
 }
 
 /*
