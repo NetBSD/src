9568902f9844faf7ccf0cffa172b33fea22d4d4f
thorpej <thorpej@NetBSD.org>
1998-02-25 23:31:03 +0000

    DECstations have one physical memory segment.

--- sys/arch/pmax/include/vmparam.h
+++ sys/arch/pmax/include/vmparam.h
@@ -1,6 +1,11 @@
-/*	$NetBSD: vmparam.h,v 1.6 1996/03/19 03:06:28 jonathan Exp $	*/
+/*	$NetBSD: vmparam.h,v 1.7 1998/02/25 23:31:03 thorpej Exp $	*/
 
 #include <mips/vmparam.h>
+
+/*
+ * DECstation has one physical memory segment.
+ */
+#define	VM_PHYSSEG_MAX		1
 
 /* pcb base */
 /*#define	pcbb(p)		((u_int)(p)->p_addr) */
