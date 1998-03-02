8870ed9b9a68525c8adc468df505960e40945b26
ross <ross@NetBSD.org>
1998-03-02 07:44:19 +0000

    Centralize cdev_decl() for these MI drivers + a12dc in machine/conf.h (cgd) (This does not introduce any config or option dependencies.)

---
+++ sys/arch/alpha/include/conf.h
@@ -0,0 +1,8 @@
+/* $NetBSD: conf.h,v 1.2.2.2 1998/03/02 07:44:19 ross Exp $ */
+
+#include <sys/conf.h>
+#include <machine/cpuconf.h>
+
+cdev_decl(a12dc);
+cdev_decl(scc);
+cdev_decl(prom);			/* XXX */
