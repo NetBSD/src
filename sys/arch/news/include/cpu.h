39eefe5842aec7dee24c6bdcbace14e92952776a
thorpej <thorpej@NetBSD.org>
1998-03-04 22:26:35 +0000

    Rename NetBSD/news to NetBSD/newsmips.

--- sys/arch/news/include/cpu.h
+++
@@ -1,4 +0,0 @@
-/*	$NetBSD: cpu.h,v 1.1 1998/02/18 13:48:16 tsubai Exp $	*/
-
-#include <mips/cpu.h>
-#include <mips/cpuregs.h> /* XXX */
--- sys/arch/news/include/float.h
+++
@@ -1,3 +0,0 @@
-/*	$NetBSD: float.h,v 1.1 1998/02/18 13:48:18 tsubai Exp $	*/
-
-#include <mips/float.h>
