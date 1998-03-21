e5e5bb924da24359ac6d5b8519050d3d448ec40e
pk <pk@NetBSD.org>
1998-03-21 20:10:07 +0000

    Sbus attach arguments and map function prototypes go here.

--- sys/arch/sparc/dev/sbusvar.h
+++ sys/arch/sparc/dev/sbusvar.h
@@ -1,4 +1,40 @@
-/*	$NetBSD: sbusvar.h,v 1.4 1996/04/22 02:35:05 abrown Exp $ */
+/*	$NetBSD: sbusvar.h,v 1.5 1998/03/21 20:10:07 pk Exp $ */
+
+/*-
+ * Copyright (c) 1998 The NetBSD Foundation, Inc.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to The NetBSD Foundation
+ * by Paul Kranenburg.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *        This product includes software developed by the NetBSD
+ *        Foundation, Inc. and its contributors.
+ * 4. Neither the name of The NetBSD Foundation nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 
 /*
  * Copyright (c) 1992, 1993
@@ -44,6 +80,9 @@
  *	@(#)sbusvar.h	8.1 (Berkeley) 6/11/93
  */
 
+#ifndef _SBUS_VAR_H
+#define _SBUS_VAR_H
+
 /*
  * S-bus variables.
  */
@@ -53,25 +92,65 @@ struct sbusdev {
 	void	(*sd_reset) __P((struct device *));
 };
 
-/*
- * Sbus driver attach arguments.
- */
-struct sbus_attach_args {
-	struct	romaux sa_ra;		/* name, node, addr, etc */
-	int	sa_slot;		/* Sbus slot number */
-	int	sa_offset;		/* offset within slot */
-};
-
 /* variables per Sbus */
 struct sbus_softc {
 	struct	device sc_dev;		/* base device */
+	bus_space_tag_t	sc_bustag;
+	bus_dma_tag_t	sc_dmatag;
 	int	sc_clockfreq;		/* clock frequency (in Hz) */
 	struct	sbusdev *sc_sbdev;	/* list of all children */
 	struct	rom_range *sc_range;
 	int	sc_nrange;
 	int	sc_burst;		/* burst transfer sizes supported */
+	int	*sc_intr2ipl;		/* Interrupt level translation */
+	int	*sc_intr_compat;	/* `intr' property to sbus compat */
 };
+
+/*
+ * Sbus driver attach arguments.
+ */
+struct sbus_attach_args {
+	int		sa_placeholder;	/* for obio attach args sharing */
+	bus_space_tag_t	sa_bustag;
+	bus_dma_tag_t	sa_dmatag;
+	char		*sa_name;	/* PROM node name */
+	int		sa_node;	/* PROM handle */
+	int		sa_slot;	/* Sbus slot number */
+	int		sa_offset;	/* Sbus slot offset */
+	int		sa_size;	/* Sbus space size */
+	int		sa_pri;		/* priority (IPL) */
+	void		*sa_promvaddr;	/* PROM virtual address, if any */
+	struct bootpath *sa_bp;		/* used for locating boot device */
+};
+
+/* sbus_attach() is also used from obio.c */
+void	sbus_attach __P((struct sbus_softc *, char *, int, struct bootpath *,
+			 const char * const *));
+int	sbus_print __P((void *, const char *));
 
 int	sbusdev_match __P((struct cfdata *, void *));
 void	sbus_establish __P((struct sbusdev *, struct device *));
-void	sbus_translate __P((struct device *, struct confargs *));
+
+void	*sbus_intr_establish __P((
+		void *,			/*cookie*/
+		int,			/*level*/
+		int,			/*flags*/
+		int (*) __P((void *)),	/*handler*/
+		void *));		/*handler arg*/
+
+int	sbus_setup_attach_args __P((
+		struct sbus_softc *,
+		bus_space_tag_t,
+		bus_dma_tag_t,
+		int,			/*node*/
+		struct bootpath *,
+		struct sbus_attach_args *));
+int	sbus_bus_map __P((
+		bus_space_tag_t,
+		int,			/*slot*/
+		int,			/*offset*/
+		int,			/*size*/
+		int,			/*flags*/
+		vm_offset_t,		/*preferred virtual address */
+		bus_space_handle_t *));
+#endif /* _SBUS_VAR_H */
