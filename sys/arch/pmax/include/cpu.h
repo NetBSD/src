b40a3c98f5ca34471b7b6c2e00d4d5e20fc0110d
jonathan <jonathan@NetBSD.org>
1998-03-25 08:35:39 +0000

    Use an MD name for the FPU-interrupt mask, as in PR # 4399. mips-based Sony news wire the FPU to hard-interrupt 3, rather than 5 as recommended in Kane.

--- sys/arch/pmax/include/cpu.h
+++ sys/arch/pmax/include/cpu.h
@@ -1,4 +1,13 @@
-/*	$NetBSD: cpu.h,v 1.17 1997/06/16 06:17:28 jonathan Exp $	*/
+/*	$NetBSD: cpu.h,v 1.18 1998/03/25 08:35:39 jonathan Exp $	*/
 
+#ifndef __PMAX_CPU_H
+#define __PMAX_CPU_H
+
+/*
+ * pmax uses standard mips1 convention, wiring FPU to hard interupt 5.
+ */
 #include <mips/cpu.h>
 #include <mips/cpuregs.h> /* XXX */
+
+#define MIPS_INT_MASK_FPU	MIPS_INT_MASK_5
+#endif __PMAX_CPU_H
