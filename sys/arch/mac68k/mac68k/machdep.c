e3e82a2bd2e907a55194739ba3691a0be1e99818
scottr <scottr@NetBSD.org>
1998-03-27 18:11:31 +0000

    Move bus_space implementation to bus_space.c.

--- sys/arch/mac68k/mac68k/machdep.c
+++ sys/arch/mac68k/mac68k/machdep.c
@@ -1,41 +1,4 @@
-/*	$NetBSD: machdep.c,v 1.185 1998/02/24 07:06:39 scottr Exp $	*/
-
-/*-
- * Copyright (c) 1996, 1997 The NetBSD Foundation, Inc.
- * All rights reserved.
- *
- * This code is derived from software contributed to The NetBSD Foundation
- * by Jason R. Thorpe of the Numerical Aerospace Simulation Facility,
- * NASA Ames Research Center.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed by the NetBSD
- *	Foundation, Inc. and its contributors.
- * 4. Neither the name of The NetBSD Foundation nor the names of its
- *    contributors may be used to endorse or promote products derived
- *    from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
- * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
- * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS
- * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- */
+/*	$NetBSD: machdep.c,v 1.186 1998/03/27 18:11:32 scottr Exp $	*/
 
 /*
  * Copyright (c) 1988 University of Utah.
@@ -119,7 +82,6 @@
 #include <sys/systm.h>
 #include <sys/signalvar.h>
 #include <sys/kernel.h>
-#include <sys/map.h>
 #include <sys/proc.h>
 #include <sys/buf.h>
 #include <sys/exec.h>
@@ -157,7 +119,6 @@
 #include <machine/reg.h>
 #include <machine/psl.h>
 #include <machine/pte.h>
-#include <machine/bus.h>
 #include <machine/kcore.h>	/* XXX should be pulled in by sys/kcore.h */
 #include <net/netisr.h>
 
@@ -266,9 +227,9 @@ unsigned short	mac68k_schedipl = PSL_S | PSL_IPL3;
  * The extent maps are not static!  Machine-dependent NuBus and on-board
  * I/O routines need access to them for bus address space allocation.
  */
-static	long iomem_ex_storage[EXTENT_FIXED_STORAGE_SIZE(8) / sizeof(long)];
-struct	extent *iomem_ex;
-static	iomem_malloc_safe;
+static long iomem_ex_storage[EXTENT_FIXED_STORAGE_SIZE(8) / sizeof(long)];
+struct extent *iomem_ex;
+int iomem_malloc_safe;
 
 static void	identifycpu __P((void));
 static u_long	get_physical __P((u_int, u_long *));
@@ -278,9 +239,6 @@ int	cpu_dumpsize __P((void));
 int	cpu_dump __P((int (*)(dev_t, daddr_t, caddr_t, size_t), daddr_t *));
 void	cpu_init_kcore_hdr __P((void));
 void	dumpsys __P((void));
-
-int	bus_mem_add_mapping __P((bus_addr_t, bus_size_t,
-	    int, bus_space_handle_t *));
 
 /*
  * Machine-dependent crash dump header info.
@@ -2814,223 +2772,4 @@ mac68k_ring_bell(freq, length, volume)
 		    freq, length, volume));
 	else
 		return (ENXIO);
-}
-
-/*
- * bus.h implementation
- */
-
-int
-bus_space_map(t, bpa, size, flags, bshp)
-	bus_space_tag_t t;
-	bus_addr_t bpa;
-	bus_size_t size;
-	int flags;
-	bus_space_handle_t *bshp;
-{
-	u_long pa, endpa;
-	int error;
-
-	/*
-	 * Before we go any further, let's make sure that this
-	 * region is available.
-	 */
-	error = extent_alloc_region(iomem_ex, bpa, size,
-	    EX_NOWAIT | (iomem_malloc_safe ? EX_MALLOCOK : 0));
-	if (error)
-		return (error);
-
-	pa = m68k_trunc_page(bpa + t);
-	endpa = m68k_round_page((bpa + t + size) - 1);
-
-#ifdef DIAGNOSTIC
-	if (endpa <= pa)
-		panic("bus_space_map: overflow");
-#endif
-
-	error = bus_mem_add_mapping(bpa, size, flags, bshp);
-	if (error) {
-		if (extent_free(iomem_ex, bpa, size, EX_NOWAIT |
-		    (iomem_malloc_safe ? EX_MALLOCOK : 0))) {
-			printf("bus_space_map: pa 0x%lx, size 0x%lx\n",
-			    bpa, size);
-			printf("bus_space_map: can't free region\n");
-		}
-	}
-
-	return (error);
-}
-
-int
-bus_space_alloc(t, rstart, rend, size, alignment, boundary, flags, bpap, bshp)
-	bus_space_tag_t t;
-	bus_addr_t rstart, rend;
-	bus_size_t size, alignment, boundary;
-	int flags;
-	bus_addr_t *bpap;
-	bus_space_handle_t *bshp;
-{
-	u_long bpa;
-	int error;
-
-	/*
-	 * Sanity check the allocation against the extent's boundaries.
-	 */
-	if (rstart < iomem_ex->ex_start || rend > iomem_ex->ex_end)
-		panic("bus_space_alloc: bad region start/end");
-
-	/*
-	 * Do the requested allocation.
-	 */
-	error = extent_alloc_subregion(iomem_ex, rstart, rend, size, alignment,
-	    boundary,
-	    EX_FAST | EX_NOWAIT | (iomem_malloc_safe ?  EX_MALLOCOK : 0),
-	    &bpa);
-
-	if (error)
-		return (error);
-
-	/*
-	 * For memory space, map the bus physical address to
-	 * a kernel virtual address.
-	 */
-	error = bus_mem_add_mapping(bpa, size, flags, bshp);
-	if (error) {
-		if (extent_free(iomem_ex, bpa, size, EX_NOWAIT |
-		    (iomem_malloc_safe ? EX_MALLOCOK : 0))) {
-			printf("bus_space_alloc: pa 0x%lx, size 0x%lx\n",
-			    bpa, size);
-			printf("bus_space_alloc: can't free region\n");
-		}
-	}
-
-	*bpap = bpa;
-
-	return (error);
-}
-
-int
-bus_mem_add_mapping(bpa, size, flags, bshp)
-	bus_addr_t bpa;
-	bus_size_t size;
-	int flags;
-	bus_space_handle_t *bshp;
-{
-	u_long pa, endpa;
-	vm_offset_t va;
-
-	pa = m68k_trunc_page(bpa);
-	endpa = m68k_round_page((bpa + size) - 1);
-
-#ifdef DIAGNOSTIC
-	if (endpa <= pa)
-		panic("bus_mem_add_mapping: overflow");
-#endif
-
-	va = kmem_alloc_pageable(kernel_map, endpa - pa);
-	if (va == 0)
-		return (ENOMEM);
-
-	*bshp = (bus_space_handle_t)(va + (bpa & PGOFSET));
-
-	for (; pa < endpa; pa += NBPG, va += NBPG) {
-		pmap_enter(pmap_kernel(), va, pa,
-		    VM_PROT_READ | VM_PROT_WRITE, TRUE);
-		if (!(flags & BUS_SPACE_MAP_CACHEABLE))
-			pmap_changebit(pa, PG_CI, TRUE);
-	}
- 
-	return 0;
-}
-
-void
-bus_space_unmap(t, bsh, size)
-	bus_space_tag_t t;
-	bus_space_handle_t bsh;
-	bus_size_t size;
-{
-	vm_offset_t	va, endva;
-	bus_addr_t bpa;
-
-	va = m68k_trunc_page(bsh);
-	endva = m68k_round_page((bsh + size) - 1);
-
-#ifdef DIAGNOSTIC
-	if (endva <= va)
-		panic("bus_space_unmap: overflow");
-#endif
-
-	bpa = pmap_extract(pmap_kernel(), va) + (bsh & PGOFSET);
-
-	/*
-	 * Free the kernel virtual mapping.
-	 */
-	kmem_free(kernel_map, va, endva - va);
-
-	if (extent_free(iomem_ex, bpa, size,
-	    EX_NOWAIT | (iomem_malloc_safe ? EX_MALLOCOK : 0))) {
-		printf("bus_space_unmap: pa 0x%lx, size 0x%lx\n",
-		    bpa, size);
-		printf("bus_space_unmap: can't free region\n");
-	}
-}
-
-void    
-bus_space_free(t, bsh, size)
-	bus_space_tag_t t;
-	bus_space_handle_t bsh;
-	bus_size_t size;
-{
-	/* bus_space_unmap() does all that we need to do. */
-	bus_space_unmap(t, bsh, size);
-}
-
-int
-bus_space_subregion(t, bsh, offset, size, nbshp)
-	bus_space_tag_t t;
-	bus_space_handle_t bsh;
-	bus_size_t offset, size;
-	bus_space_handle_t *nbshp;
-{
-
-	*nbshp = bsh + offset;
-	return (0);
-}
-
-label_t	*nofault;
-
-int
-mac68k_bus_space_probe(t, bsh, offset, sz)
-	bus_space_tag_t t;
-	bus_space_handle_t bsh;
-	bus_size_t offset;
-	int sz;
-{
-	int i;
-	label_t faultbuf;
-
-	nofault = &faultbuf;
-	if (setjmp(nofault)) {
-		nofault = (label_t *)0;
-		return (0);
-	}
-
-	switch (sz) {
-	case 1:
-		i = bus_space_read_1(t, bsh, offset);
-		break;
-	case 2:
-		i = bus_space_read_2(t, bsh, offset);
-		break;
-	case 4:
-		i = bus_space_read_4(t, bsh, offset);
-		break;
-	case 8:
-	default:
-		panic("bus_space_probe: unsupported data size %d\n", sz);
-		/* NOTREACHED */
-	}
-
-	nofault = (label_t *)0;
-	return (1);
 }
