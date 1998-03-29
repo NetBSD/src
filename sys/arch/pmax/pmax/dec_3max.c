0e0d7a04aa1cc7f3626d42a28496282d4b0bab7f
jonathan <jonathan@NetBSD.org>
1998-03-29 01:12:14 +0000

    Split 3max (kn02) and 3maxplus (kn03, kn05) memory-error handling out dec_3max.c into a shared file. These models have compatible memory subsystems, the same memory slots, and (apparently) the same error reporting.
    Add markers for where MNN probes for configurations with a mixture of
    RAM sizes and for Prestoserve NVRAM (in normal RAM slots) should go.

--- sys/arch/pmax/pmax/dec_3max.c
+++ sys/arch/pmax/pmax/dec_3max.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: dec_3max.c,v 1.3 1998/03/26 12:46:34 jonathan Exp $	*/
+/*	$NetBSD: dec_3max.c,v 1.4 1998/03/29 01:12:15 jonathan Exp $	*/
 
 /*
  * Copyright (c) 1998 Jonathan Stone.  All rights reserved.
@@ -73,7 +73,7 @@
 
 #include <sys/cdefs.h>			/* RCS ID & Copyright macro defns */
 
-__KERNEL_RCSID(0, "$NetBSD: dec_3max.c,v 1.3 1998/03/26 12:46:34 jonathan Exp $");
+__KERNEL_RCSID(0, "$NetBSD: dec_3max.c,v 1.4 1998/03/29 01:12:15 jonathan Exp $");
 
 #include <sys/types.h>
 #include <sys/systm.h>
@@ -96,7 +96,7 @@ __KERNEL_RCSID(0, "$NetBSD: dec_3max.c,v 1.3 1998/03/26 12:46:34 jonathan Exp $"
 #include <pmax/pmax/machdep.h>		/* XXXjrs replace with vectors */
 
 #include <pmax/pmax/kn02.h>
-
+#include <pmax/pmax/dec_3max_subr.h>
 
 
 /*
@@ -328,49 +328,21 @@ dec_3max_intr(mask, pc, statusReg, causeReg)
 
 
 /*
- * Handle Memory error.
- * 3max has ECC, correct   single-bit error, panic on
- * double-bit error.
+ * Handle Memory error.   3max, 3maxplus has ECC.
+ * Correct single-bit error, panic on  double-bit error.
+ * XXX on double-error on clean user page, mark bad and reload frame?
  */
 static void
 dec_3max_errintr()
 {
-	u_int erradr, chksyn, physadr;
+	u_int erradr, errsyn;
 
+	/* Fetch error address, ECC chk/syn bits, clear interrupt */
 	erradr = *(u_int *)MIPS_PHYS_TO_KSEG1(KN02_SYS_ERRADR);
-	chksyn = *(u_int *)MIPS_PHYS_TO_KSEG1(KN02_SYS_CHKSYN);
+	errsyn = *(u_int *)MIPS_PHYS_TO_KSEG1(KN02_SYS_CHKSYN);
 	*(u_int *)MIPS_PHYS_TO_KSEG1(KN02_SYS_ERRADR) = 0;
 	wbflush();
 
-	if (!(erradr & KN02_ERR_VALID))
-		return;
-	/* extract the physical word address and compensate for pipelining */
-	physadr = erradr & KN02_ERR_ADDRESS;
-	if (!(erradr & KN02_ERR_WRITE))
-		physadr = (physadr & ~0xfff) | ((physadr & 0xfff) - 5);
-	physadr <<= 2;
-	printf("%s memory %s %s error at 0x%08x\n",
-		(erradr & KN02_ERR_CPU) ? "CPU" : "DMA",
-		(erradr & KN02_ERR_WRITE) ? "write" : "read",
-		(erradr & KN02_ERR_ECCERR) ? "ECC" : "timeout",
-		physadr);
-	if (erradr & KN02_ERR_ECCERR) {
-		*(u_int *)MIPS_PHYS_TO_KSEG1(KN02_SYS_CHKSYN) = 0;
-		wbflush();
-		printf("ECC 0x%08x\n", chksyn);
-
-		/* check for a corrected, single bit, read error */
-		if (!(erradr & KN02_ERR_WRITE)) {
-			if (physadr & 0x4) {
-				/* check high word */
-				if (chksyn & KN02_ECC_SNGHI)
-					return;
-			} else {
-				/* check low word */
-				if (chksyn & KN02_ECC_SNGLO)
-					return;
-			}
-		}
-	}
-	panic("Mem error interrupt");
+	/* Send to kn02/kn03 memory subsystem handler */
+	dec_mtasic_err(erradr, errsyn);
 }
