eda73f204f9b7d4522f63c2c94a6153c8bb6c122
drochner <drochner@NetBSD.org>
1998-04-15 20:16:30 +0000

    The "mi" TGA driver. It's still not of much use on anything but alpha because it depends on initialization by the firmware. Further, an mi mmap() is needed...

---
+++ sys/dev/pci/tga_bt463.c
@@ -0,0 +1,50 @@
+/* $NetBSD: tga_bt463.c,v 1.1 1998/04/15 20:16:31 drochner Exp $ */
+
+/*
+ * Copyright (c) 1995, 1996 Carnegie-Mellon University.
+ * All rights reserved.
+ *
+ * Author: Chris G. Demetriou
+ *
+ * Permission to use, copy, modify and distribute this software and
+ * its documentation is hereby granted, provided that both the copyright
+ * notice and this permission notice appear in all copies of the
+ * software, derivative works or modified versions, and any portions
+ * thereof, and that both notices appear in supporting documentation.
+ *
+ * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS "AS IS"
+ * CONDITION.  CARNEGIE MELLON DISCLAIMS ANY LIABILITY OF ANY KIND
+ * FOR ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.
+ *
+ * Carnegie Mellon requests users of this software to return to
+ *
+ *  Software Distribution Coordinator  or  Software.Distribution@CS.CMU.EDU
+ *  School of Computer Science
+ *  Carnegie Mellon University
+ *  Pittsburgh PA 15213-3890
+ *
+ * any improvements or extensions that they make and grant Carnegie the
+ * rights to redistribute these changes.
+ */
+
+#include <sys/param.h>
+#include <sys/device.h>
+
+#include <dev/pci/pcivar.h>
+#include <dev/pci/tgareg.h>
+#include <dev/pci/tgavar.h>
+#include <dev/ic/bt485reg.h>
+
+
+const struct tga_ramdac_conf tga_ramdac_bt463 = {
+	"Bt463",
+#if 0
+	NULL,				/* XXX SET CMAP */
+	NULL,				/* XXX GET CMAP */
+	tga_builtin_set_cursor,
+	tga_builtin_get_cursor,
+	tga_builtin_set_curpos,
+	tga_builtin_get_curpos,
+	tga_builtin_get_curmax,
+#endif
+};
