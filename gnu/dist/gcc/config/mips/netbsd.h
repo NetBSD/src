23cf193c978530a36d5a028b5e3ab48c512e02dd
mrg <mrg>
1998-04-17 06:06:09 +0000

    pull over some things from the old gcc, and clean up.

--- gnu/dist/gcc/config/mips/netbsd.h
+++ gnu/dist/gcc/config/mips/netbsd.h
@@ -18,72 +18,96 @@ along with GNU CC; see the file COPYING.  If not, write to
 the Free Software Foundation, 59 Temple Place - Suite 330,
 Boston, MA 02111-1307, USA.  */
 
-#define DECSTATION
+/* Define default target values. */
 
-/* Look for the include files in the system-defined places.  */
+#define TARGET_ENDIAN_DEFAULT 0
+#define TARGET_DEFAULT MASK_GAS
 
-#ifndef CROSS_COMPILE
-#undef GPLUSPLUS_INCLUDE_DIR
-#define GPLUSPLUS_INCLUDE_DIR "/usr/include/g++"
+/* Get generic mips ELF definitions. */
 
-#undef GCC_INCLUDE_DIR
-#define GCC_INCLUDE_DIR "/usr/include"
+#include <mips/elf.h>
 
-#undef INCLUDE_DEFAULTS
-#define INCLUDE_DEFAULTS		\
-  {					\
-    { GPLUSPLUS_INCLUDE_DIR, 1, 1 },	\
-    { GCC_INCLUDE_DIR, 0, 0 },		\
-    { 0, 0, 0 }				\
-  }
+/* Get generic NetBSD definitions. */
 
-/* Under NetBSD, the normal location of the various *crt*.o files is the
-   /usr/lib directory.  */
+#define NETBSD_ELF
+#include <netbsd.h>
 
-#undef STANDARD_STARTFILE_PREFIX
-#define STANDARD_STARTFILE_PREFIX "/usr/lib/"
-#endif
+/* Define mips-specific netbsd predefines... */
+
+#undef CPP_PREDEFINES
+#define CPP_PREDEFINES \
+ "-D__ANSI_COMPAT -DMIPSEL -DR3000 -DSYSTYPE_BSD -D_SYSTYPE_BSD \
+  -D__NetBSD__ -Dmips -D__NO_LEADING_UNDERSCORES__ -D__GP_SUPPORT__ \
+  -Dunix -D_R3000 -Asystem(unix) -Asystem(NetBSD) -Amachine(mips)"
+
+#undef SUBTARGET_CPP_SPEC
+#define SUBTARGET_CPP_SPEC "%{posix:-D_POSIX_SOURCE}"
+
+#undef MACHINE_TYPE
+#define MACHINE_TYPE "NetBSD/mips"
+
+/* Always uses gas.  */
+
+#undef ASM_SPEC
+#define ASM_SPEC \
+ "%{G*} %{EB} %{EL} %{mips1} %{mips2} %{mips3} %{v} \
+  %{noasmopt:-O0} \
+  %{!noasmopt:%{O:-O2} %{O1:-O2} %{O2:-O2} %{O3:-O3}} \
+  %{g} %{g0} %{g1} %{g2} %{g3} \
+  %{ggdb:-g} %{ggdb0:-g0} %{ggdb1:-g1} %{ggdb2:-g2} %{ggdb3:-g3} \
+  %{gstabs:-g} %{gstabs0:-g0} %{gstabs1:-g1} %{gstabs2:-g2} %{gstabs3:-g3} \
+  %{gstabs+:-g} %{gstabs+0:-g0} %{gstabs+1:-g1} %{gstabs+2:-g2} %{gstabs+3:-g3} \
+  %{gcoff:-g} %{gcoff0:-g0} %{gcoff1:-g1} %{gcoff2:-g2} %{gcoff3:-g3} \
+  %{membedded-pic} %{fPIC:-KPIC}"
 
 /* Provide a LINK_SPEC appropriate for NetBSD.  Here we provide support
    for the special GCC options -static, -assert, and -nostdlib.  */
-
+ 
 #undef LINK_SPEC
 #define LINK_SPEC \
   "%{G*} %{EB} %{EL} %{mips1} %{mips2} %{mips3} \
-   %{!nostartfiles:%{!r*:%{!e*:-e __start}}} -dc -dp %{static:-Bstatic} %{assert*}"
+   %{bestGnum} %{shared} %{non_shared} \
+   %{call_shared} %{no_archive} %{exact_version} \
+   %{!shared: %{!non_shared: %{!call_shared: -non_shared}}} \
+   %{!dynamic-linker:-dynamic-linker /usr/libexec/ld.elf_so} \
+   %{nostdlib:-nostdlib} %{!nostdlib:%{!r*:%{!e*:-e __start}}} -dc -dp \
+   %{static:-Bstatic} %{!static:-Bdynamic} %{assert*}"
+
+/* Provide CC1_SPEC appropriate for NetBSD/mips ELF platforms */
+
+#undef CC1_SPEC
+#define CC1_SPEC \
+ "%{gline:%{!g:%{!g0:%{!g1:%{!g2: -g1}}}}} \
+  %{mips1:-mfp32 -mgp32}%{mips2:-mfp32 -mgp32}\
+  %{mips3:%{!msingle-float:%{!m4650:-mfp64}} -mgp64} \
+  %{mips4:%{!msingle-float:%{!m4650:-mfp64}} -mgp64} \
+  %{mfp64:%{msingle-float:%emay not use both -mfp64 and -msingle-float}} \
+  %{mfp64:%{m4650:%emay not use both -mfp64 and -m4650}} \
+  %{m4650:-mcpu=r4650} \
+  %{G*} %{EB:-meb} %{EL:-mel} %{EB:%{EL:%emay not use both -EB and -EL}} \
+  %{pic-none:   -mno-half-pic} \
+  %{pic-lib:    -mhalf-pic} \
+  %{pic-extern: -mhalf-pic} \
+  %{pic-calls:  -mhalf-pic} \
+  %{save-temps: } \
+  %{!mno-abicalls:    -mabicalls}"
+
+#undef CPP_SPEC
+#define CPP_SPEC \
+ "%{posix:-D_POSIX_SOURCE} \
+  %{mlong64:-D__SIZE_TYPE__=long\\ unsigned\\ int -D__PTRDIFF_TYPE__=long\\ int} \
+  %{!mlong64:-D__SIZE_TYPE__=unsigned\\ int -D__PTRDIFF_TYPE__=int} \
+  %{mips3:-U__mips -D__mips=3 -D__mips64} \
+  %{mgp32:-U__mips64} %{mgp64:-D__mips64}"
 
 /* We have atexit(3).  */
 
 #define HAVE_ATEXIT
 
-/* Implicit library calls should use memcpy, not bcopy, etc.  */
+/* Trampoline code for closures should call _cacheflush()
+    to ensure I-cache consistency after writing trampoline code.  */
 
-#define TARGET_MEM_FUNCTIONS
-
-/* Define mips-specific netbsd predefines... */
-#ifndef CPP_PREDEFINES
-#define CPP_PREDEFINES "-D__ANSI_COMPAT \
--DMIPSEL -DR3000 -DSYSTYPE_BSD -D_SYSTYPE_BSD -D__NetBSD__ -Dmips \
--D__NO_LEADING_UNDERSCORES__ -D__GP_SUPPORT__ \
--Dunix -D_R3000 \
--Asystem(unix) -Asystem(NetBSD) -Amachine(mips)"
-#endif
-
-#ifndef SUBTARGET_CPP_SPEC
-#define SUBTARGET_CPP_SPEC "%{posix:-D_POSIX_SOURCE}"
-#endif
-
-#define LIB_SPEC "%{!p:%{!pg:-lc}}%{p:-lc_p}%{pg:-lc_p}"
-#define STARTFILE_SPEC ""
-
-#ifndef MACHINE_TYPE
-#define MACHINE_TYPE "NetBSD/pmax"
-#endif
-
-#define TARGET_DEFAULT MASK_GAS
-#define PREFERRED_DEBUGGING_TYPE DBX_DEBUG
-
-#include "mips/mips.h"
+#define MIPS_CACHEFLUSH_FUNC "_cacheflush"
 
 /*
  * Some imports from svr4.h in support of shared libraries.
@@ -224,4 +248,3 @@ do {                                                                         \
 
 /* Since gas and gld are standard on NetBSD, we don't need these */
 #undef ASM_FINAL_SPEC
-#undef STARTFILE_SPEC
