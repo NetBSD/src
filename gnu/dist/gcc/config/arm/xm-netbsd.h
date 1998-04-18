b24648c6d94e73ec488b809091a5a9ae7035a1a0
cgd <cgd@NetBSD.org>
1998-04-18 00:37:33 +0000

    define SYS_SIGLIST_DECLARED in a way that's consistent with config.h

--- gnu/dist/gcc/config/arm/xm-netbsd.h
+++ gnu/dist/gcc/config/arm/xm-netbsd.h
@@ -3,5 +3,6 @@
 #include <arm/xm-arm.h>
 
 #ifndef SYS_SIGLIST_DECLARED
-#define SYS_SIGLIST_DECLARED
+/* XXX old def (w/o 1) broke w/NetBSD's configure-gen'd(?) config.h -- cgd */
+#define SYS_SIGLIST_DECLARED 1
 #endif
