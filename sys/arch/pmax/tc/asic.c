c7597ae8b8e50fb2696ef881bf5e783cac5318a1
jonathan <jonathan@NetBSD.org>
1998-04-19 10:18:20 +0000

    Rework, based loosely on suggestions from Tohru Nisimura..

--- sys/arch/pmax/tc/asic.c
+++ sys/arch/pmax/tc/asic.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: asic.c,v 1.26 1998/03/26 06:35:27 thorpej Exp $	*/
+/*	$NetBSD: asic.c,v 1.27 1998/04/19 10:18:20 jonathan Exp $	*/
 
 /*
  * Copyright (c) 1994, 1995 Carnegie-Mellon University.
@@ -55,8 +55,52 @@
 #include <pmax/pmax/kmin.h>
 #include <pmax/pmax/nameglue.h>
 
-extern int systype;
-#endif
+/*
+ * Which system models were configured?
+ */
+#include "opt_dec_3max.h"
+#include "opt_dec_3min.h"
+#include "opt_dec_3maxplus.h"
+#include "opt_dec_maxine.h"
+
+
+
+#define	C(x)	((void*)(x))
+#define	ASIC_NDEVS(x)	(sizeof(x)/sizeof(x[0]))
+#define ARRAY_SIZEOF(x) (sizeof((x)) / sizeof ((x)[0]))
+
+#if defined(DEC_3MAXPLUS) || defined(DEC_3MIN)
+struct ioasicdev_attach_args kn03_ioasic_devs[] = {
+/* 0 */	{ "lance",	0x0C0000, 0, C(KN03_LANCE_SLOT)	},
+/* 1 */	{ "scc",	0x100000, 0, C(KN03_SCC0_SLOT)	},
+/* 2 */	{ "scc",	0x180000, 0, C(KN03_SCC1_SLOT)	},
+/* 3 */	{ "mc146818",	0x200000, 0, C(0), },
+/* 4 */	{ "asc",	0x300000, 0, C(KN03_SCSI_SLOT)	},
+};
+const int nkn03_ioasic_devs  =  ARRAY_SIZEOF(kn03_ioasic_devs);
+#endif /* DEC_3MAXPLUS || DEC_3MIN */
+
+#if defined(DEC_MAXINE)
+struct ioasicdev_attach_args xine_ioasic_devs[] = {
+/* 0 */	{ "lance",	0x0C0000, 0, C(XINE_LANCE_SLOT),	},
+/* 1 */	{ "scc",	0x100000, 0, C(XINE_SCC0_SLOT),	},
+/* 2 */	{ "mc146818",	0x200000, 0, C(0),  },
+/* 3 */	{ "isdn",	0x240000, 0, C(XINE_ISDN_SLOT),	},
+/* 4 */	{ "dtop",	0x280000, 0, C(XINE_DTOP_SLOT),	},
+/* 5 */	{ "fdc",	0x2C0000, 0, C(XINE_FLOPPY_SLOT),	},
+/* 6 */	{ "asc",	0x300000, 0, C(XINE_SCSI_SLOT),	},
+};
+const int nxine_ioasic_devs  =  ARRAY_SIZEOF(xine_ioasic_devs);
+#endif /* DEC_MAXINE */
+
+#if defined(DEC_3MAX)
+struct ioasic_dev kn02_ioasic_devs[] = {
+/* 0 */	{ "dc",		0x200000, 0, C(7), },
+/* 0 */	{ "mc146818",	0x280000, 0, C(0), },
+const int n3max_ioasic_devs  =  ARRAY_SIZEOF(3max_ioasic_devs);
+};
+#endif /* DEC_3MAX */
+#endif /* pmax */
 
 struct asic_softc {
 	struct	device sc_dv;
@@ -64,16 +108,16 @@ struct asic_softc {
 };
 
 /* Definition of the driver for autoconfig. */
-int	asicmatch __P((struct device *, struct cfdata *, void *));
-void	asicattach __P((struct device *, struct device *, void *));
-int     asicprint(void *, const char *);
+int	ioasicmatch __P((struct device *, struct cfdata *, void *));
+void	ioasicattach __P((struct device *, struct device *, void *));
+int     ioasicprint(void *, const char *);
 
 /* Device locators. */
 #include "locators.h"
 #define	ioasiccf_offset	cf_loc[IOASICCF_OFFSET]		/* offset */
 
 struct cfattach ioasic_ca = {
-	sizeof(struct asic_softc), asicmatch, asicattach
+	sizeof(struct asic_softc), ioasicmatch, ioasicattach
 };
 
 void    asic_intr_establish __P((struct confargs *, intr_handler_t,
@@ -81,24 +125,19 @@ void    asic_intr_establish __P((struct confargs *, intr_handler_t,
 void    asic_intr_disestablish __P((struct confargs *));
 caddr_t ioasic_cvtaddr __P((struct confargs *));
 
-#ifndef pmax
-int	asic_intr __P((void *));
-#endif
-
 int	asic_intrnull __P((intr_arg_t));
 
 struct asic_slot {
 	struct confargs	as_ca;
-	u_int		as_bits;
 	intr_handler_t	as_handler;
-	void		*as_val;
+	void		*iada_cookie;
+	u_int		iada_intrbits;
 };
 
 #ifdef	pmax
 /*#define IOASIC_DEBUG*/
 
-struct asic_slot *asic_slots;
-#include "ds-asic-conf.c"
+struct ioasicdev_attach_args *asic_slots;
 
 extern tc_addr_t	ioasic_base;
 tc_addr_t	ioasic_base = 0;
@@ -112,7 +151,7 @@ tc_addr_t	ioasic_base = 0;
 #endif
 
 int
-asicmatch(parent, cf, aux)
+ioasicmatch(parent, cf, aux)
 	struct device *parent;
 	struct cfdata *cf;
 	void *aux;
@@ -145,47 +184,48 @@ asicmatch(parent, cf, aux)
 		return (0);
 gotasic:
 
-	/* See if the unit number is valid. */
-	switch (systype) {
-	case DS_3MIN:
-		if (cf->cf_unit > 0)
-			return (0);
-		asic_slots = kn03_asic_slots;	/* XXX - 3min same as kn03? */
-		break;
-	case DS_MAXINE:
-		if (cf->cf_unit > 0)
-			return (0);
-		asic_slots = xine_asic_slots;
-		break;
-	case DS_3MAX:
-		if (cf->cf_unit > 0)
-			return (0);
-		asic_slots = kn02_asic_slots;
-		break;
-	case DS_3MAXPLUS:
-		if (cf->cf_unit > 0)
-			return (0);
-		asic_slots = kn03_asic_slots;
-		break;
-	default:
-		printf("no ASIC config table for this machine\n");
+	if (cf->cf_unit > 0)
 		return (0);
-	}
 
 	return (1);
 }
 
 void
-asicattach(parent, self, aux)
+ioasicattach(parent, self, aux)
 	struct device *parent, *self;
 	void *aux;
 {
 	struct asic_softc *sc = (struct asic_softc *)self;
 	struct tc_attach_args *ta = aux;
-	struct confargs *nca;
-	struct ioasicdev_attach_args ioasicdev;
-	int i;
-	extern int systype;
+	struct ioasicdev_attach_args idev;
+	int i, nslots;
+
+	/* See if the unit number is valid. */
+	switch (systype) {
+	case DS_3MIN:
+	case DS_3MAXPLUS:
+		/* 3min ioasic addressees are the same as 3maxplus. */
+		asic_slots = kn03_ioasic_devs;
+		nslots = nkn03_ioasic_devs;
+		break;
+
+#ifdef DEC_MAXINE
+	case DS_MAXINE:
+		asic_slots = xine_ioasic_devs;
+		nslots = nxine_ioasic_devs;
+		break;
+#endif
+
+#ifdef DEC_3MAX
+	case DS_3MAX:
+		asic_slots = kn02_ioasic_devs;
+		nslots = nkn02_ioasic_devs;
+		break;
+#endif
+
+	default:
+		break;
+	}
 
 	if (asic_slots == NULL)
 		panic("asicattach: no asic_slot map\n");
@@ -219,34 +259,29 @@ asicattach(parent, self, aux)
 #define ASIC_MAX_NSLOTS 7 /*XXX*/
 
         /* Try to configure each CPU-internal device */
-        for (i = 0; i < ASIC_MAX_NSLOTS; i++) {
+        for (i = 0; i < nslots; i++) {
 
 		IOASIC_DPRINTF(("asicattach: entry %d, base addr %x\n",
 		       i, sc->sc_base));
 
-                nca = &asic_slots[i].as_ca;
-		if (nca == NULL) panic ("bad asic table\n");
-		if (nca->ca_name == NULL || nca->ca_name[0] == 0)
+		/* Compute address at runtime. Leave table readonly. */
+		idev = asic_slots[i];
+		idev.iada_addr =((u_long)sc->sc_base) + idev.iada_offset;
+
+		if (idev.iada_modname == NULL || idev.iada_modname[0] == 0)
 			break;
-		nca->ca_addr = ((u_int)sc->sc_base) + nca->ca_offset;
 
-		IOASIC_DPRINTF((" adding %s subslot %d offset %x addr %x\n",
-		       nca->ca_name, nca->ca_slot, nca->ca_offset,
-		       nca->ca_addr));
+		IOASIC_DPRINTF((" adding %s offset %x addr %x\n",
+		    idev.iada_modname, idev.iada_offset, idev.iada_addr));
 
-		strncpy(ioasicdev.iada_modname, nca->ca_name, TC_ROM_LLEN);
-		ioasicdev.iada_modname[TC_ROM_LLEN] = '\0';
-		ioasicdev.iada_offset = nca->ca_offset;
-		ioasicdev.iada_addr = nca->ca_addr;
-		ioasicdev.iada_cookie = (void *)nca->ca_slotpri;
                 /* Tell the autoconfig machinery we've found the hardware. */
-                config_found(self, &ioasicdev, asicprint);
+                config_found(self, &idev, ioasicprint);
         }
 	IOASIC_DPRINTF(("asicattach: done\n"));
 }
 
 int
-asicprint(aux, pnp)
+ioasicprint(aux, pnp)
 	void *aux;
 	const char *pnp;
 {
@@ -269,77 +304,6 @@ ioasic_submatch(match, d)
 		(match->ioasiccf_offset == IOASICCF_OFFSET_DEFAULT));
 }
 
-/*
- * Save interrupt slotname and enable mask (??)
- * On decstations this isn't useful, as the turbochannel
- * decstations all have incompatible ways of mapping interrupts
- * to IO ASIC or r3000 interrupt bits.
- * Instead of writing "as_bits" directly into an IOASIC interrupt-enable
- * register,  Decstations use a  machine-dependent function that
- * interpret (pseudo-) slot numbers and do ``the right thing'' to enable
- * or disable  interrupts for a specific slot (or pseudo-slot), by
- * slot number, on that given CPU.
- */
-void
-asic_intr_establish(ca, handler, val)
-	struct confargs *ca;
-	intr_handler_t handler;
-        intr_arg_t val;
-{
-
-#if defined(DIAGNOSTIC) && defined(alpha)
-	if (ca->ca_slot == IOASIC_SLOT_RTC)
-		panic("setting clock interrupt incorrectly");
-#endif	/*defined(DIAGNOSTIC) && defined(alpha)*/
-
-	/* XXX SHOULD NOT BE THIS LITERAL */
-	if (asic_slots[ca->ca_slot].as_handler != asic_intrnull)
-		/*panic*/ printf("asic_intr_establish: slot %d twice", ca->ca_slot);
-
-	/*
-	 * XXX  We need to invent a better interface to machine-dependent
-	 * interrupt-enable code, or redo the Decstation configuration
-	 * tables with unused entries, so that slot is always equal
-	 * to "priority" (software pseudo-slot number).  FIXME.
-	 */
-#if defined(IOASIC_DEBUG) && 0
-	printf("asic: %s:  intr for entry %d slot %d pri %d\n", 
-		 ca->ca_name, ca->ca_slot, ca->ca_slotpri,
-		 (int)asic_slots[ca->ca_slot].as_val);
-#endif	/*IOASIC_DEBUG*/
-
-#ifdef pmax
-	tc_enable_interrupt(ca->ca_slotpri, handler, val, 1);
-
-#else	/* Alpha AXP */
-
-	/* Save handler info so it can be enabled later (??) */
-	asic_slots[ca->ca_slot].as_handler = handler;
-	asic_slots[ca->ca_slot].as_val = val;
-#endif	/* Alpha AXP */
-}
-
-void
-asic_intr_disestablish(ca)
-	struct confargs *ca;
-{
-
-#ifdef pmax
-	panic("asic_intr_disestablish: shouldn't ever be called\n");
-#else
-	if (ca->ca_slot == IOASIC_SLOT_RTC)
-	        panic("asic_intr_disestablish: can't do clock interrupt");
-
-	/* XXX SHOULD NOT BE THIS LITERAL */
-	if (asic_slots[ca->ca_slot].as_handler == asic_intrnull)
-		panic("asic_intr_disestablish: slot %d missing intr",
-		    ca->ca_slot);
-
-	asic_slots[ca->ca_slot].as_handler = dsasic_intrnull;
-	asic_slots[ca->ca_slot].as_val = (void *)(long)ca->ca_slot;
-#endif
-}
-
 
 void
 ioasic_intr_establish(dev, cookie, level, handler, val)
@@ -352,44 +316,6 @@ ioasic_intr_establish(dev, cookie, level, handler, val)
 
 	(*tc_enable_interrupt)((int)cookie, handler, val, 1);
 }
-
-#ifdef	alpha
-/*
- * asic_intr --
- *	ASIC interrupt handler.
- */
-int
-asic_intr(val)
-	void *val;
-{
-	register struct asic_softc *sc = val;
-	register int i, ifound;
-	int gifound;
-	u_int32_t sir, junk;
-	volatile u_int32_t *sirp, *junkp;
-
-	sirp = (volatile u_int32_t *)IOASIC_REG_INTR(sc->sc_base);
-
-	gifound = 0;
-	do {
-		ifound = 0;
-		MB();
-		MAGIC_READ;
-		MB();
-
-		sir = *sirp;
-		for (i = 0; i < ASIC_MAX_NSLOTS; i++)
-			if (sir & asic_slots[i].as_bits) {
-				(void)(*asic_slots[i].as_handler)
-				    (asic_slots[i].as_val);
-				ifound = 1;
-			}
-		gifound |= ifound;
-	} while (ifound);
-
-	return (gifound);
-}
-#endif /*!pmax*/
 
 int
 asic_intrnull(val)
@@ -404,7 +330,6 @@ asic_intrnull(val)
 char *
 ioasic_lance_ether_address()
 {
-
 	return (u_char *)IOASIC_SYS_ETHER_ADDRESS(ioasic_base);
 }
 
