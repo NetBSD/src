c0484dc8cf18c1a53aea093a94ff3dfbe14d429e
mycroft <mycroft@NetBSD.org>
1998-04-26 06:03:23 +0000

    Update copyright.

--- sys/dev/ata/wdlink.h
+++ sys/dev/ata/wdlink.h
@@ -1,7 +1,7 @@
-/*	$NetBSD: wdlink.h,v 1.6 1998/04/26 05:28:23 mycroft Exp $	*/
+/*	$NetBSD: wdlink.h,v 1.7 1998/04/26 06:03:24 mycroft Exp $	*/
 
 /*
- * Copyright (c) 1994, 1995 Charles M. Hannum.  All rights reserved.
+ * Copyright (c) 1994, 1995, 1998 Charles M. Hannum.  All rights reserved.
  *
  * DMA and multi-sector PIO handling are derived from code contributed by
  * Onno van der Linden.
