5e67cc8dd4d9c6e51897bc64b0a9b2fc82fef216
christos <christos@NetBSD.org>
1998-05-02 14:26:47 +0000

    pppd 2.3.4 from Paulus

--- usr.sbin/pppd/pppstats/pppstats.c
+++ usr.sbin/pppd/pppstats/pppstats.c
@@ -1,5 +1,3 @@
-/*	$NetBSD: pppstats.c,v 1.1.1.3 1997/09/26 18:53:19 christos Exp $	*/
-
 /*
  * print PPP statistics:
  * 	pppstats [-a|-d] [-v|-r|-z] [-c count] [-w wait] [interface]
@@ -33,13 +31,8 @@
  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
  */
 
-#include <sys/cdefs.h>
 #ifndef lint
-#if 0
-static char rcsid[] = "Id: pppstats.c,v 1.19 1997/04/30 06:00:27 paulus Exp ";
-#else
-__RCSID("$NetBSD: pppstats.c,v 1.1.1.3 1997/09/26 18:53:19 christos Exp $");
-#endif
+static char rcsid[] = "Id: pppstats.c,v 1.22 1998/03/31 23:48:03 paulus Exp ";
 #endif
 
 #include <stdio.h>
@@ -54,19 +47,28 @@ __RCSID("$NetBSD: pppstats.c,v 1.1.1.3 1997/09/26 18:53:19 christos Exp $");
 #include <sys/param.h>
 #include <sys/types.h>
 #include <sys/ioctl.h>
-#include <net/ppp_defs.h>
 
 #ifndef STREAMS
 #include <sys/socket.h>		/* *BSD, Linux, NeXT, Ultrix etc. */
-#include <net/if.h>
 #ifndef _linux_
+#include <net/if.h>
+#include <net/ppp_defs.h>
 #include <net/if_ppp.h>
 #else
-#include <net/if_ppp.h>
+/* Linux */
+#if __GLIBC__ >= 2
+#include <net/if.h>
+#else
+#include <linux/if.h>
 #endif
+#include <linux/types.h>
+#include <linux/ppp_defs.h>
+#include <linux/if_ppp.h>
+#endif /* _linux_ */
 
 #else	/* STREAMS */
 #include <sys/stropts.h>	/* SVR4, Solaris 2, SunOS 4, OSF/1, etc. */
+#include <net/ppp_defs.h>
 #include <net/pppio.h>
 
 #endif	/* STREAMS */
