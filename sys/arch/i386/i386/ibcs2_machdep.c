70b45ff391d465a785881d41b53cd9533d4bc5ea
mycroft <mycroft@NetBSD.org>
1998-05-05 09:52:17 +0000

    Pull up 1.5, per request of scottb.

--- sys/arch/i386/i386/ibcs2_machdep.c
+++ sys/arch/i386/i386/ibcs2_machdep.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: ibcs2_machdep.c,v 1.3.2.1 1998/01/29 11:45:06 mellon Exp $	*/
+/*	$NetBSD: ibcs2_machdep.c,v 1.3.2.2 1998/05/05 09:52:17 mycroft Exp $	*/
 
 /*-
  * Copyright (c) 1997 The NetBSD Foundation, Inc.
@@ -37,14 +37,22 @@
  */
 
 #include <sys/param.h>
+#include <sys/systm.h>
 #include <sys/proc.h>
 #include <sys/exec.h>
 #include <sys/user.h>
+#include <sys/signalvar.h>
+#include <sys/signal.h>
 
+#include <machine/cpu.h>
+#include <machine/cpufunc.h>
+#include <machine/psl.h>
 #include <machine/npx.h>
 #include <machine/reg.h>
 #include <machine/vmparam.h>
 #include <machine/ibcs2_machdep.h>
+
+#include <compat/ibcs2/ibcs2_signal.h>
 
 void
 ibcs2_setregs(p, epp, stack)
@@ -59,4 +67,111 @@ ibcs2_setregs(p, epp, stack)
 	pcb->pcb_savefpu.sv_env.en_cw = __iBCS2_NPXCW__;
 	tf = p->p_md.md_regs;
 	tf->tf_eax = 0x2000000;		/* XXX base of heap */
+}
+
+/*
+ * Send an interrupt to process.
+ *
+ * Stack is set up to allow sigcode stored
+ * in u. to call routine, followed by kcall
+ * to sigreturn routine below.  After sigreturn
+ * resets the signal mask, the stack, and the
+ * frame pointer, it returns to the user
+ * specified pc, psl.
+ */
+void
+ibcs2_sendsig(catcher, sig, mask, code)
+	sig_t catcher;
+	int sig, mask;
+	u_long code;
+{
+	register struct proc *p = curproc;
+	register struct trapframe *tf;
+	struct sigframe *fp, frame;
+	struct sigacts *psp = p->p_sigacts;
+	int oonstack;
+	extern char ibcs2_sigcode[], ibcs2_esigcode[];
+
+	sig = bsd_to_ibcs2_sig[sig];
+
+	/* 
+	 * Build the argument list for the signal handler.
+	 */
+	frame.sf_signum = sig;
+
+	tf = p->p_md.md_regs;
+	oonstack = psp->ps_sigstk.ss_flags & SS_ONSTACK;
+
+	/*
+	 * Allocate space for the signal handler context.
+	 */
+	if ((psp->ps_flags & SAS_ALTSTACK) && !oonstack &&
+	    (psp->ps_sigonstack & sigmask(sig))) {
+		fp = (struct sigframe *)(psp->ps_sigstk.ss_sp +
+		    psp->ps_sigstk.ss_size - sizeof(struct sigframe));
+		psp->ps_sigstk.ss_flags |= SS_ONSTACK;
+	} else {
+		fp = (struct sigframe *)tf->tf_esp - 1;
+	}
+
+	frame.sf_code = code;
+	frame.sf_scp = &fp->sf_sc;
+	frame.sf_handler = catcher;
+
+	/*
+	 * Build the signal context to be used by sigreturn.
+	 */
+	frame.sf_sc.sc_err = tf->tf_err;
+	frame.sf_sc.sc_trapno = tf->tf_trapno;
+	frame.sf_sc.sc_onstack = oonstack;
+	frame.sf_sc.sc_mask = mask;
+#ifdef VM86
+	if (tf->tf_eflags & PSL_VM) {
+		frame.sf_sc.sc_gs = tf->tf_vm86_gs;
+		frame.sf_sc.sc_fs = tf->tf_vm86_fs;
+		frame.sf_sc.sc_es = tf->tf_vm86_es;
+		frame.sf_sc.sc_ds = tf->tf_vm86_ds;
+		frame.sf_sc.sc_eflags = get_vflags(p);
+	} else
+#endif
+	{
+		__asm("movl %%gs,%w0" : "=r" (frame.sf_sc.sc_gs));
+		__asm("movl %%fs,%w0" : "=r" (frame.sf_sc.sc_fs));
+		frame.sf_sc.sc_es = tf->tf_es;
+		frame.sf_sc.sc_ds = tf->tf_ds;
+		frame.sf_sc.sc_eflags = tf->tf_eflags;
+	}
+	frame.sf_sc.sc_edi = tf->tf_edi;
+	frame.sf_sc.sc_esi = tf->tf_esi;
+	frame.sf_sc.sc_ebp = tf->tf_ebp;
+	frame.sf_sc.sc_ebx = tf->tf_ebx;
+	frame.sf_sc.sc_edx = tf->tf_edx;
+	frame.sf_sc.sc_ecx = tf->tf_ecx;
+	frame.sf_sc.sc_eax = tf->tf_eax;
+	frame.sf_sc.sc_eip = tf->tf_eip;
+	frame.sf_sc.sc_cs = tf->tf_cs;
+	frame.sf_sc.sc_esp = tf->tf_esp;
+	frame.sf_sc.sc_ss = tf->tf_ss;
+
+	if (copyout(&frame, fp, sizeof(frame)) != 0) {
+		/*
+		 * Process has trashed its stack; give it an illegal
+		 * instruction to halt it in its tracks.
+		 */
+		sigexit(p, SIGILL);
+		/* NOTREACHED */
+	}
+
+	/*
+	 * Build context to run handler in.
+	 */
+	__asm("movl %w0,%%gs" : : "r" (GSEL(GUDATA_SEL, SEL_UPL)));
+	__asm("movl %w0,%%fs" : : "r" (GSEL(GUDATA_SEL, SEL_UPL)));
+	tf->tf_es = GSEL(GUDATA_SEL, SEL_UPL);
+	tf->tf_ds = GSEL(GUDATA_SEL, SEL_UPL);
+	tf->tf_eip = (int)(((char *)PS_STRINGS) - (ibcs2_esigcode - ibcs2_sigcode));
+	tf->tf_cs = GSEL(GUCODE_SEL, SEL_UPL);
+	tf->tf_eflags &= ~(PSL_T|PSL_VM|PSL_AC);
+	tf->tf_esp = (int)fp;
+	tf->tf_ss = GSEL(GUDATA_SEL, SEL_UPL);
 }
