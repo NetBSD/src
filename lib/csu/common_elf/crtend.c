cbd1bb578abfc44860ecda6bca38e3b6dd63631d
ross <ross@NetBSD.org>
1998-05-06 20:45:54 +0000

    Sigh, don't make global calls in repeated sharedlib components. It blows away the dynamic loader.

--- lib/csu/common_elf/crtend.c
+++ lib/csu/common_elf/crtend.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: crtend.c,v 1.4 1998/05/06 05:32:08 ross Exp $	*/
+/*	$NetBSD: crtend.c,v 1.5 1998/05/06 20:45:54 ross Exp $	*/
 
 #include <sys/cdefs.h>
 #include "crt.h"
@@ -9,13 +9,3 @@ static void (*__CTOR_LIST__[1]) __P((void))
 static void (*__DTOR_LIST__[1]) __P((void))
     __attribute__((__unused__))
     __attribute__((section(".dtors"))) = { (void *)0 };		/* XXX */
-
-/* ARGSUSED */
-void __noconst_i(voidvoid *t)
-{
-}
-
-/* ARGSUSED */
-void __noconst_f(voidvoid *t)
-{
-}
