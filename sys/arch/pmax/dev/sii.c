469658d53398ab41a2dc7b174944334aa4d55bee
simonb <simonb@NetBSD.org>
1998-05-08 00:05:19 +0000

    - Allow all valid SCSI id's to work on the SII and first two ASC   controllers (including SCSI id 7 on the 2100/3100). - On the pmax (2100,3100) set the host SCSI id to 6. - Move disk and tape config for the second ASC controller from the GENERIC   config file to scsi.pmax so all configurations can use the second   controller.

--- sys/arch/pmax/dev/sii.c
+++ sys/arch/pmax/dev/sii.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: sii.c,v 1.31 1998/04/19 01:27:02 jonathan Exp $	*/
+/*	$NetBSD: sii.c,v 1.32 1998/05/08 00:05:19 simonb Exp $	*/
 
 /*-
  * Copyright (c) 1992, 1993
@@ -302,9 +302,12 @@ sii_Reset(sc, reset)
 	 */
 	regs->csr = SII_HPM;
 	/*
-	 * SII is always ID 7.
+	 * Set host adapter ID (6 for PMIN/PMAX, 7 for everything else)
 	 */
-	regs->id = SII_ID_IO | 7;
+	if (systype == DS_PMAX)
+		regs->id = SII_ID_IO | 6;
+	else
+		regs->id = SII_ID_IO | 7;
 	/*
 	 * Enable SII to drive the SCSI bus.
 	 */
