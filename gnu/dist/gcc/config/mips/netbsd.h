6aa0101d2f40573f8e05e9e8f7636a0e33de0c58
thorpej <thorpej@NetBSD.org>
1998-05-14 04:16:40 +0000

    Add -D__ELF__ to CPP_PREDEFINES (per the Other gcc)

--- gnu/dist/gcc/config/mips/netbsd.h
+++ gnu/dist/gcc/config/mips/netbsd.h
@@ -37,7 +37,7 @@ Boston, MA 02111-1307, USA.  */
 #undef CPP_PREDEFINES
 #define CPP_PREDEFINES \
  "-D__ANSI_COMPAT -DMIPSEL -DR3000 -DSYSTYPE_BSD -D_SYSTYPE_BSD \
-  -D__NetBSD__ -Dmips -D__NO_LEADING_UNDERSCORES__ -D__GP_SUPPORT__ \
+  -D__NetBSD__ -D__ELF__ -Dmips -D__NO_LEADING_UNDERSCORES__ -D__GP_SUPPORT__ \
   -Dunix -D_R3000 -Asystem(unix) -Asystem(NetBSD) -Amachine(mips)"
 
 #undef SUBTARGET_CPP_SPEC
