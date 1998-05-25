a97fc2f180e1ac21d2f65b9273a63b092796369a
kleink <kleink@NetBSD.org>
1998-05-25 20:59:01 +0000

    If any of _ANSI_SOURCE, _POSIX_C_SOURCE or _XOPEN_SOURCE are defined, don't provide any identifiers other than sig_atomic_t.

--- sys/arch/pc532/include/signal.h
+++ sys/arch/pc532/include/signal.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: signal.h,v 1.5 1995/01/10 19:01:36 jtc Exp $	*/
+/*	$NetBSD: signal.h,v 1.6 1998/05/25 20:59:03 kleink Exp $	*/
 
 /*
  * Copyright (c) 1982, 1986, 1989, 1991 Regents of the University of California.
@@ -40,7 +40,8 @@
 
 typedef int sig_atomic_t;
 
-#ifndef _ANSI_SOURCE
+#if !defined(_ANSI_SOURCE) && !defined(_POSIX_C_SOURCE) && \
+    !defined(_XOPEN_SOURCE)
 /*
  * Get the "code" values
  */
@@ -64,5 +65,5 @@ struct	sigcontext {
 	int	sc_reg[8];		/* The registers */
 };
 
-#endif	/* !_ANSI_SOURCE */
+#endif	/* !_ANSI_SOURCE && !_POSIX_C_SOURCE && !_XOPEN_SOURCE */
 #endif	/* !_MACHINE_SIGNAL_H_ */
