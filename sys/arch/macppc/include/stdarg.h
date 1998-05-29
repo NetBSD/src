07c905aa61eeef10557a3580d15ddf02ac5c6e8a
tsubai <tsubai@NetBSD.org>
1998-05-29 10:32:49 +0000

    Use arch/powerpc/.

--- sys/arch/macppc/include/stdarg.h
+++ sys/arch/macppc/include/stdarg.h
@@ -1,15 +1,3 @@
-/*	$NetBSD: stdarg.h,v 1.1 1998/05/15 10:15:56 tsubai Exp $	*/
+/*	$NetBSD: stdarg.h,v 1.2 1998/05/29 10:32:55 tsubai Exp $	*/
 
-#ifndef	_PPC_STDARG_H_
-#define	_PPC_STDARG_H_
-
-#include <machine/ansi.h>
-
-#ifndef	_STDARG_H
-#define	_STDARG_H
-#endif
-#include <machine/va-ppc.h>
-
-typedef	__gnuc_va_list	va_list;
-
-#endif /* ! _PPC_STDARG_H_ */
+#include <powerpc/stdarg.h>
