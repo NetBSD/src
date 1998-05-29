76dc75098fad00dc5dd7f8e0d7f0fb20197c3c10
thorpej <thorpej>
1998-05-29 22:55:44 +0000

    Cosmetic changes.

--- sys/sys/sha1.h
+++ sys/sys/sha1.h
@@ -1,5 +1,4 @@
-/*	$NetBSD: sha1.h,v 1.1 1998/05/27 00:45:50 explorer Exp $	*/
-/*	$OpenBSD: sha1.h,v 1.8 1997/07/15 01:54:23 millert Exp $	*/
+/*	$NetBSD: sha1.h,v 1.2 1998/05/29 22:55:44 thorpej Exp $	*/
 
 /*
  * SHA-1 in C
@@ -7,25 +6,25 @@
  * 100% Public Domain
  */
 
-#ifndef _SHA1_H
-#define _SHA1_H
+#ifndef _SYS_SHA1_H_
+#define	_SYS_SHA1_H_
 
 #include <sys/types.h>
 
 typedef struct {
-    u_int32_t state[5];
-    u_int32_t count[2];  
-    u_char buffer[64];
+	u_int32_t state[5];
+	u_int32_t count[2];  
+	u_char buffer[64];
 } SHA1_CTX;
   
-void SHA1Transform __P((u_int32_t state[5], const u_char buffer[64]));
-void SHA1Init __P((SHA1_CTX *context));
-void SHA1Update __P((SHA1_CTX *context, const u_char *data, u_int len));
-void SHA1Final __P((u_char digest[20], SHA1_CTX *context));
+void	SHA1Transform __P((u_int32_t state[5], const u_char buffer[64]));
+void	SHA1Init __P((SHA1_CTX *context));
+void	SHA1Update __P((SHA1_CTX *context, const u_char *data, u_int len));
+void	SHA1Final __P((u_char digest[20], SHA1_CTX *context));
 #ifndef _KERNEL
-char *SHA1End __P((SHA1_CTX *, char *));
-char *SHA1File __P((char *, char *));
-char *SHA1Data __P((const u_char *, size_t, char *));
+char	*SHA1End __P((SHA1_CTX *, char *));
+char	*SHA1File __P((char *, char *));
+char	*SHA1Data __P((const u_char *, size_t, char *));
 #endif /* _KERNEL */
 
-#endif /* _SHA1_H */
+#endif /* _SYS_SHA1_H_ */
