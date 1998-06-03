52450409d615d96a3b32c2658850d882edf4cf74
thorpej <thorpej@NetBSD.org>
1998-06-03 21:52:36 +0000

    Sync w/ i386 port:
    Implement bounce buffers for mbufs.
    
    Also, correct copyright snafus, and a forgot-o left over from a commit
    I made yesterday.

--- sys/arch/bebox/isa/isa_machdep.h
+++ sys/arch/bebox/isa/isa_machdep.h
@@ -1,4 +1,41 @@
-/*	$NetBSD: isa_machdep.h,v 1.3 1998/02/03 04:32:17 sakamoto Exp $	*/
+/*	$NetBSD: isa_machdep.h,v 1.4 1998/06/03 21:52:36 thorpej Exp $	*/
+
+/*-
+ * Copyright (c) 1996, 1997 The NetBSD Foundation, Inc.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to The NetBSD Foundation
+ * by Jason R. Thorpe of the Numerical Aerospace Simulation Facility,
+ * NASA Ames Research Center.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed by the NetBSD
+ *	Foundation, Inc. and its contributors.
+ * 4. Neither the name of The NetBSD Foundation nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 
 /*-
  * Copyright (c) 1990 The Regents of the University of California.
@@ -88,9 +125,15 @@ extern struct bebox_bus_dma_tag isa_bus_dma_tag;
 struct bebox_isa_dma_cookie {
 	int	id_flags;		/* flags; see below */
 
+	/*
+	 * Information about the original buffer used during
+	 * DMA map syncs.  Note that origbuflen is only used
+	 * for ID_BUFTYPE_LINEAR.
+	 */
 	void	*id_origbuf;		/* pointer to orig buffer if
 					   bouncing */
 	bus_size_t id_origbuflen;	/* ...and size */
+	int	id_buftype;		/* type of buffer */
 
 	void	*id_bouncebuf;		/* pointer to the bounce buffer */
 	bus_size_t id_bouncebuflen;	/* ...and size */
@@ -104,6 +147,12 @@ struct bebox_isa_dma_cookie {
 #define	ID_HAS_BOUNCE		0x02	/* map currently has bounce buffers */
 #define	ID_IS_BOUNCING		0x04	/* map is bouncing current xfer */
 
+/* id_buftype */
+#define	ID_BUFTYPE_INVALID	0
+#define	ID_BUFTYPE_LINEAR	1
+#define	ID_BUFTYPE_MBUF		2
+#define	ID_BUFTYPE_UIO		3
+#define	ID_BUFTYPE_RAW		4
 
 /*
  * XXX Various seemingly PC-specific constants, some of which may be
