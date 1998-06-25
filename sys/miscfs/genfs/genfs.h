e93f98e734749bdd708321d70e049f30c6b381b4
thorpej <thorpej>
1998-06-25 22:15:28 +0000

    - Rename nqnfs_vop_lease_check() to genfs_lease_check().  If NFSSERVER is   not in the kernel, genfs_lease_check() is simply a no-op.  This allows   LKM'd file systems to be exported (previously did not work properly   due to a compile-time decision based on -DNFSSERVER). - defopt NFSSERVER

--- sys/miscfs/genfs/genfs.h
+++ sys/miscfs/genfs/genfs.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: genfs.h,v 1.5 1998/03/01 02:22:03 fvdl Exp $	*/
+/*	$NetBSD: genfs.h,v 1.6 1998/06/25 22:15:30 thorpej Exp $	*/
 
 int	genfs_badop	__P((void *));
 int	genfs_nullop	__P((void *));
@@ -13,3 +13,4 @@ int	genfs_fsync	__P((void *));
 int	genfs_seek	__P((void *));
 int	genfs_abortop	__P((void *));
 int	genfs_revoke	__P((void *));
+int	genfs_lease_check __P((void *));
