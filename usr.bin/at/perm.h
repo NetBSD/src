6a26ccb44afebff895dc359631cf38ebb6aec248
christos <christos@NetBSD.org>
1998-06-27 21:15:07 +0000

    Merge in changes from OpenBSD; adds support for at.allow and at.deny, plus fixes a lot of bugs.

--- usr.bin/at/perm.h
+++ usr.bin/at/perm.h
@@ -1,8 +1,8 @@
-/*	$NetBSD: parsetime.h,v 1.3 1998/06/27 21:15:08 christos Exp $	*/
+/*	$NetBSD: perm.h,v 1.1 1998/06/27 21:15:08 christos Exp $	*/
 
-/*
- *  parsetime.h -  header for at(1)
- *  Copyright (c) 1993  Thomas Koenig
+/* 
+ *  perm.h -  header for at(1)
+ *  Copyright (C) 1994  Thomas Koenig
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -23,8 +23,6 @@
  * THEORY OF LIABILITY, WETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- * From: $OpenBSD: parsetime.h,v 1.3 1997/03/01 23:40:11 millert Exp $
  */
 
-time_t parsetime	__P((int, char **));
+int check_permission __P((void));
