35ff2b49e625f00d405914c8f688f7184fb9e271
gwr <gwr@NetBSD.org>
1998-06-29 20:33:03 +0000

    Note recent changes. (and bump CVS revsion here)

--- sys/arch/sun3/stand/libsa/version.c
+++ sys/arch/sun3/stand/libsa/version.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: version.c,v 1.5 1998/02/05 04:57:17 gwr Exp $ */
+/*	$NetBSD: version.c,v 1.6 1998/06/29 20:33:03 gwr Exp $ */
 
 /*
  *	NOTE ANY CHANGES YOU MAKE TO THE BOOTBLOCKS HERE.
@@ -6,9 +6,14 @@
  * 1/98 Common boot programs run on both Sun 3 and 3X
  *      machines, with different default kernel names
  *      on each so shared root images are possible.
+ *
+ * 6/98 Both netboot and ufsboot now look first for
+ *	"netbsd.sun3" or "netbsd.sun3x" and then
+ *	"netbsd" so that boot media can support
+ *	both by providing two kernels.
  */
 
 #include <sys/types.h>
 #include "libsa.h"
 
-char version[] = "$Revision: 1.5 $";
+char version[] = "$Revision: 1.6 $";
