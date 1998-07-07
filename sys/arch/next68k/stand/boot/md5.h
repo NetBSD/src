63232b5781ee7fe0dc5a31afef2343bde0ee7bb5
dbj <dbj>
1998-07-07 00:16:34 +0000

    Removed unnecessary bug-workaround headers. Minor compilation and header tweak.

--- sys/arch/next68k/stand/boot/md5.h
+++
@@ -1,8 +0,0 @@
-/*	$NetBSD: md5.h,v 1.1.1.1 1998/06/09 07:53:06 dbj Exp $	*/
-
-#include <sys/systm.h>
-#include <sys/md5.h>
-
-/* This file is a hack @@@ because md5c.c in libkern is broken.
- * -- darrin
- */
