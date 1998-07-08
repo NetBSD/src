86ee0a40550bd88a7aa1b159c88092082bcb80c2
mjacob <mjacob>
1998-07-08 05:25:40 +0000

    Doh! Jason caught me being really stupid. There isn't necessarily a linear 0..N mapping out of alpha_cpu_whami - which is what I was using to index the new cpu machine check array. This is a quick hack back to just using the first element while I think of a kinder function that will do the function (per-platform) of whami->VIRTUAL CPU ID, which if you think about it, is really necessary anyway.

--- sys/arch/alpha/alpha/dec_kn300.c
+++ sys/arch/alpha/alpha/dec_kn300.c
@@ -1,4 +1,4 @@
-/* $NetBSD: dec_kn300.c,v 1.5 1998/07/08 00:44:32 mjacob Exp $ */
+/* $NetBSD: dec_kn300.c,v 1.6 1998/07/08 05:25:40 mjacob Exp $ */
 
 /*
  * Copyright (c) 1998 by Matthew Jacob
@@ -32,7 +32,7 @@
 
 #include <sys/cdefs.h>			/* RCS ID & Copyright macro defns */
 
-__KERNEL_RCSID(0, "$NetBSD: dec_kn300.c,v 1.5 1998/07/08 00:44:32 mjacob Exp $");
+__KERNEL_RCSID(0, "$NetBSD: dec_kn300.c,v 1.6 1998/07/08 05:25:40 mjacob Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -427,7 +427,11 @@ kn300_mcheck(mces, type, logout, framep)
 	/*
 	 * If we expected a machine check, just go handle it in common code.
 	 */
+#if	0	/* TO BE FIXED */
 	mcp = &mchkinfo[alpha_pal_whami()];
+#else
+	mcp = &mchkinfo[0];
+#endif
 	if (mcp->mc_expected) {
 		machine_check(mces, framep, type, logout);
 		return;
