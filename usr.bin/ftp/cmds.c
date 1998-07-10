6f10c545605e73a23b0869179391515318c7173e
thorpej <thorpej>
1998-07-10 04:39:03 +0000

    Add "sndbuf" and "rcvbuf" commands for setting the socket buffer sizes, which in turn can allow the use of larger TCP windows.  This is a work in progress; there is not yet support for specifying global defaults or user prefrences on a host/network basis.

--- usr.bin/ftp/cmds.c
+++ usr.bin/ftp/cmds.c
@@ -1,4 +1,41 @@
-/*	$NetBSD: cmds.c,v 1.38 1998/06/19 23:01:21 kleink Exp $	*/
+/*	$NetBSD: cmds.c,v 1.39 1998/07/10 04:39:03 thorpej Exp $	*/
+
+/*-
+ * Copyright (c) 1998 The NetBSD Foundation, Inc.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to The NetBSD Foundation
+ * by Jason R. Thorpe of the Numerical Aerospace Simulation Facility,
+ * NASA Ames Research Center.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed by the NetBSD
+ *	Foundation, Inc. and its contributors.
+ * 4. Neither the name of The NetBSD Foundation nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 
 /*
  * Copyright (c) 1985, 1989, 1993, 1994
@@ -38,7 +75,7 @@
 #if 0
 static char sccsid[] = "@(#)cmds.c	8.6 (Berkeley) 10/9/94";
 #else
-__RCSID("$NetBSD: cmds.c,v 1.38 1998/06/19 23:01:21 kleink Exp $");
+__RCSID("$NetBSD: cmds.c,v 1.39 1998/07/10 04:39:03 thorpej Exp $");
 #endif
 #endif /* not lint */
 
@@ -1584,6 +1621,8 @@ disconnect(argc, argv)
 	if (!proxy) {
 		macnum = 0;
 	}
+
+	resetsockbufsize();
 }
 
 void
@@ -2248,4 +2287,62 @@ page(argc, argv)
 	verbose = overbose;
 	if (oldargv1 != argv[1])	/* free up after globulize() */
 		free(argv[1]);
+}
+
+/*
+ * Set the socket send buffer size.
+ */
+void
+sndbuf(argc, argv)
+	int argc;
+	char *argv[];
+{
+	int size;
+
+	if (argc != 2) {
+		printf("usage: %s size\n", argv[0]);
+		code = -1;
+		return;
+	}
+
+	if ((size = getsockbufsize(argv[1])) == -1) {
+		printf("invalid socket buffer size: %s\n", argv[1]);
+		code = -1;
+		return;
+	}
+
+	sndbuf_size = size;
+	if (sndbuf_size)
+		sndbuf_manual = 1;
+	else
+		sndbuf_manual = 0;
+}
+
+/*
+ * Set the socket receive buffer size.
+ */
+void
+rcvbuf(argc, argv)
+	int argc;
+	char *argv[];
+{
+	int size;
+
+	if (argc != 2) {
+		printf("usage: %s size\n", argv[0]);
+		code = -1;
+		return;
+	}
+
+	if ((size = getsockbufsize(argv[1])) == -1) {
+		printf("invalid socket buffer size: %s\n", argv[1]);
+		code = -1;
+		return;
+	}
+
+	rcvbuf_size = size;
+	if (rcvbuf_size)
+		rcvbuf_manual = 1;
+	else
+		rcvbuf_manual = 0;
 }
