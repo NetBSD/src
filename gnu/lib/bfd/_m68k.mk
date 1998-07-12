aacae7ac2db8328f2fdaad611c67ee03a17298b5
thorpej <thorpej@NetBSD.org>
1998-07-12 18:53:31 +0000

    Add elf32-m68k support.

--- gnu/lib/bfd/_m68k.mk
+++ gnu/lib/bfd/_m68k.mk
@@ -1,16 +1,18 @@
-# $NetBSD: _m68k.mk,v 1.2 1997/10/17 20:07:15 gwr Exp $
+# $NetBSD: _m68k.mk,v 1.3 1998/07/12 18:53:31 thorpej Exp $
 
 BFD_MACHINES =	cpu-m68k.c
-BFD_BACKENDS =	m68knetbsd.c m68k4knetbsd.c sunos.c aout32.c
+BFD_BACKENDS =	m68knetbsd.c m68k4knetbsd.c sunos.c aout32.c \
+		elf32-m68k.c elf32.c elf.c elflink.c
 
 ARCH_DEFS = -DARCH_m68k \
  -DSELECT_ARCHITECTURES='&bfd_m68k_arch'
 
 TDEFAULTS = \
  -DDEFAULT_VECTOR=m68knetbsd_vec \
- -DSELECT_VECS=' &m68knetbsd_vec, &m68k4knetbsd_vec, &sunos_big_vec ' \
+ -DSELECT_VECS=' &m68knetbsd_vec, &m68k4knetbsd_vec, &bfd_elf32_m68k_vec, &sunos_big_vec ' \
  -DHAVE_m68knetbsd_vec \
  -DHAVE_m68k4knetbsd_vec \
+ -DHAVE_bfd_elf32_m68k_vec \
  -DHAVE_sunos_big_vec \
  -DNETBSD_CORE
 
