20106c31cac7cf7f365582c1043dbfe68ba7a6c8
augustss <augustss@NetBSD.org>
1998-07-13 10:49:41 +0000

    Add some useful HID ioctl()s.

--- sys/dev/usb/usbdi_util.h
+++ sys/dev/usb/usbdi_util.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: usbdi_util.h,v 1.1 1998/07/12 19:52:01 augustss Exp $	*/
+/*	$NetBSD: usbdi_util.h,v 1.2 1998/07/13 10:49:42 augustss Exp $	*/
 
 /*
  * Copyright (c) 1998 The NetBSD Foundation, Inc.
@@ -57,6 +57,8 @@ usbd_status	usbd_get_report_descriptor
 struct usb_hid_descriptor *usbd_get_hid_descriptor 
 	__P((usbd_interface_handle ifc));
 usbd_status	usbd_set_report 
+	__P((usbd_interface_handle iface, int type, int id, void *data, int len));
+usbd_status	usbd_get_report 
 	__P((usbd_interface_handle iface, int type, int id, void *data, int len));
 usbd_status	usbd_set_idle 
 	__P((usbd_interface_handle iface, int duration, int id));
