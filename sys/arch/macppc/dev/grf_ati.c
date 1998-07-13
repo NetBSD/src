ca67aac118bc1ba42ef4724db27b30f6be313ad3
tsubai <tsubai@NetBSD.org>
1998-07-13 19:31:53 +0000

    Get framebuffer address from Openfirmware.

--- sys/arch/macppc/dev/grf_ati.c
+++ sys/arch/macppc/dev/grf_ati.c
@@ -1,3 +1,36 @@
+/*	$NetBSD: grf_ati.c,v 1.2 1998/07/13 19:31:53 tsubai Exp $	*/
+
+/*-
+ * Copyright (C) 1998	Internet Research Institute, Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed by
+ *	Internet Research Institute, Inc.
+ * 4. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #include <sys/param.h>
 #include <sys/device.h>
 
@@ -85,19 +118,6 @@ grf_ati_attach(parent, self, aux)
 	OF_getprop(node, "assigned-addresses", reg, sizeof(reg));
 
 	regaddr = mapiodev(reg[2] + 0x800000 - 0x400, 0x400);
-	videoaddr = mapiodev(reg[2] + 0x400 , reg[4] - 0x400);
-
-
-	/*
-	 * set character color to rgb:ff/ff/ff
-	 */
-#define DAC_W_INDEX 0xc0
-#define DAC_DATA    0xc1
-
-	out8(regaddr + DAC_W_INDEX, 255);
-	out8(regaddr + DAC_DATA, 255);
-	out8(regaddr + DAC_DATA, 255);
-	out8(regaddr + DAC_DATA, 255);
 
 	ga.ga_name = "grf";
 	ga.ga_mode = ati_mode;
