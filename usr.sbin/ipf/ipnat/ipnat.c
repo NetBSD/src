23f29a380dda188431bbed840f55652af526c39f
mellon <mellon@NetBSD.org>
1998-07-23 00:59:02 +0000

    Pull up 1.19 (veego)

--- usr.sbin/ipf/ipnat/ipnat.c
+++ usr.sbin/ipf/ipnat/ipnat.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: ipnat.c,v 1.9.2.2 1997/11/17 16:27:10 mrg Exp $	*/
+/*	$NetBSD: ipnat.c,v 1.9.2.3 1998/07/23 00:59:02 mellon Exp $	*/
 
 /*
  * Copyright (C) 1993-1997 by Darren Reed.
@@ -21,6 +21,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <fcntl.h>
+#include <errno.h>
 #include <sys/types.h>
 #if !defined(__SVR4) && !defined(__svr4__)
 #include <strings.h>
@@ -54,9 +55,16 @@
 #include "netinet/ip_nat.h"
 #include "kmem.h"
 
+#if	defined(sun) && !SOLARIS2
+# define	STRERROR(x)	sys_errlist[x]
+extern	char	*sys_errlist[];
+#else
+# define	STRERROR(x)	strerror(x)
+#endif
+
 #if !defined(lint)
 static const char sccsid[] ="@(#)ipnat.c	1.9 6/5/96 (C) 1993 Darren Reed";
-static const char rcsid[] = "@(#)Id: ipnat.c,v 2.0.2.21.2.1 1997/11/08 04:55:55 darrenr Exp ";
+static const char rcsid[] = "@(#)Id: ipnat.c,v 2.0.2.21.2.8 1998/06/06 14:39:56 darrenr Exp ";
 #endif
 
 
@@ -67,14 +75,14 @@ static const char rcsid[] = "@(#)Id: ipnat.c,v 2.0.2.21.2.1 1997/11/08 04:55:55 
 extern	char	*optarg;
 
 ipnat_t	*parse __P((char *));
-u_long	hostnum __P((char *, int *));
-u_long	hostmask __P((char *));
+u_32_t	hostnum __P((char *, int *));
+u_32_t	hostmask __P((char *));
 u_short	portnum __P((char *, char *));
 void	dostats __P((int, int)), flushtable __P((int, int));
 void	printnat __P((ipnat_t *, int, void *));
 void	parsefile __P((int, char *, int));
 void	usage __P((char *));
-int	countbits __P((u_long));
+int	countbits __P((u_32_t));
 char	*getnattype __P((ipnat_t *));
 int	main __P((int, char*[]));
 
@@ -135,7 +143,8 @@ char *argv[];
 
 	if (!(opts & OPT_NODO) && ((fd = open(IPL_NAT, O_RDWR)) == -1) &&
 	    ((fd = open(IPL_NAT, O_RDONLY)) == -1)) {
-		perror("open");
+		(void) fprintf(stderr, "%s: open: %s\n", IPL_NAT,
+			STRERROR(errno));
 		exit(-1);
 	}
 
@@ -155,9 +164,9 @@ char *argv[];
  * of bits.
  */
 int	countbits(ip)
-u_long	ip;
+u_32_t	ip;
 {
-	u_long	ipn;
+	u_32_t	ipn;
 	int	cnt = 0, i, j;
 
 	ip = ipn = ntohl(ip);
@@ -235,7 +244,7 @@ void *ptr;
 		else
 			printf("%s", inet_ntoa(np->in_in[1]));
 		printf(" -> %s/", inet_ntoa(np->in_out[0]));
-		bits = countbits(ntohl(np->in_out[1].s_addr));
+		bits = countbits(np->in_out[1].s_addr);
 		if (bits != -1)
 			printf("%d ", bits);
 		else
@@ -362,7 +371,7 @@ int fd, opts;
 					ntohs(nat.nat_outport));
 				printf(" [%s %hu]", inet_ntoa(nat.nat_oip),
 					ntohs(nat.nat_oport));
-				printf(" %ld %hu %lx", nat.nat_age,
+				printf(" %ld %hu %x", nat.nat_age,
 					nat.nat_use, nat.nat_sumd);
 #if SOLARIS
 				printf(" %lx", nat.nat_ipsumd);
@@ -410,18 +419,18 @@ char	*name, *proto;
 }
 
 
-u_long	hostmask(msk)
+u_32_t	hostmask(msk)
 char	*msk;
 {
 	int	bits = -1;
-	u_long	mask;
+	u_32_t	mask;
 
 	if (!isdigit(*msk))
-		return (u_long)-1;
+		return (u_32_t)-1;
 	if (strchr(msk, '.'))
 		return inet_addr(msk);
 	if (strchr(msk, 'x'))
-		return (u_long)strtol(msk, NULL, 0);
+		return (u_32_t)strtol(msk, NULL, 0);
 	/*
 	 * set x most significant bits
 	 */
@@ -438,7 +447,7 @@ char	*msk;
  * returns an ip address as a long var as a result of either a DNS lookup or
  * straight inet_addr() call
  */
-u_long	hostnum(host, resolved)
+u_32_t	hostnum(host, resolved)
 char	*host;
 int	*resolved;
 {
@@ -457,7 +466,7 @@ int	*resolved;
 			fprintf(stderr, "can't resolve hostname: %s\n", host);
 			return 0;
 		}
-		return np->n_net;
+		return htonl(np->n_net);
 	}
 	return *(u_32_t *)hp->h_addr;
 }
@@ -693,6 +702,9 @@ char *line;
 					"missing parameter for \"proxy\"\n");
 				return NULL;
 			}
+		} else {
+			fprintf(stderr, "missing keyword \"port\"\n");
+			return NULL;
 		}
 		if ((proto = index(s, '/'))) {
 			*proto++ = '\0';
@@ -762,7 +774,8 @@ int opts;
 
 	if (strcmp(file, "-")) {
 		if (!(fp = fopen(file, "r"))) {
-			perror(file);
+			(void) fprintf(stderr, "%s: open: %s\n", file,
+				STRERROR(errno));
 			exit(1);
 		}
 	} else
