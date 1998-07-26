982e92002259a37c7be6623c4fb24985b315c4f5
mycroft <mycroft@NetBSD.org>
1998-07-26 14:13:00 +0000

    const poisoning.

--- lib/libc/arch/vax/gen/infinity.c
+++ lib/libc/arch/vax/gen/infinity.c
@@ -1,8 +1,8 @@
-/*	$NetBSD: infinity.c,v 1.3 1997/07/16 14:38:05 christos Exp $	*/
+/*	$NetBSD: infinity.c,v 1.4 1998/07/26 14:14:16 mycroft Exp $	*/
 
 #include <sys/cdefs.h>
 #ifndef lint
-__RCSID("$NetBSD: infinity.c,v 1.3 1997/07/16 14:38:05 christos Exp $");
+__RCSID("$NetBSD: infinity.c,v 1.4 1998/07/26 14:14:16 mycroft Exp $");
 #endif /* not lint */
 /*
  * XXX - THIS IS (probably) COMPLETELY WRONG ON VAX!!!
@@ -13,4 +13,4 @@ __RCSID("$NetBSD: infinity.c,v 1.3 1997/07/16 14:38:05 christos Exp $");
 #include <math.h>
 
 /* bytes for +Infinity on a 387 */
-char __infinity[] = { 0, 0, 0, 0, 0, 0, 0xf0, 0x7f };
+const char __infinity[] = { 0, 0, 0, 0, 0, 0, 0xf0, 0x7f };
