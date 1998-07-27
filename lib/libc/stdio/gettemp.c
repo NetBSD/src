f5cc719b1ad83f20e11ca21c8deee2ea65d6f228
mycroft <mycroft@NetBSD.org>
1998-07-27 13:42:27 +0000

    If AUDIT, do not compile certain functions.

--- lib/libc/stdio/gettemp.c
+++ lib/libc/stdio/gettemp.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: mktemp.c,v 1.13 1998/07/27 13:42:27 mycroft Exp $	*/
+/*	$NetBSD: gettemp.c,v 1.1 1998/07/27 13:42:39 mycroft Exp $	*/
 
 /*
  * Copyright (c) 1987, 1993
@@ -38,28 +38,113 @@
 #if 0
 static char sccsid[] = "@(#)mktemp.c	8.1 (Berkeley) 6/4/93";
 #else
-__RCSID("$NetBSD: mktemp.c,v 1.13 1998/07/27 13:42:27 mycroft Exp $");
+__RCSID("$NetBSD: gettemp.c,v 1.1 1998/07/27 13:42:39 mycroft Exp $");
 #endif
 #endif /* LIBC_SCCS and not lint */
 
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 #include <unistd.h>
+#include <errno.h>
 #include "local.h"
 
-char *
-_mktemp(path)
+int
+__gettemp(path, doopen)
 	char *path;
+	int *doopen;
 {
-	return (__gettemp(path, (int *)NULL) ? path : (char *)NULL);
-}
+	char *start, *trv;
+	struct stat sbuf;
+	u_int pid;
 
-__warn_references(mktemp,
-    "warning: mktemp() possibly used unsafely, consider using mkstemp()")
+	/* To guarantee multiple calls generate unique names even if
+	   the file is not created. 676 different possibilities with 7
+	   or more X's, 26 with 6 or less. */
+	static char xtra[2] = "aa";
+	int xcnt = 0;
 
-char *
-mktemp(path)
-	char *path;
-{
-	return (__gettemp(path, (int *)NULL) ? path : (char *)NULL);
+	pid = getpid();
+
+	/* Move to end of path and count trailing X's. */
+	for (trv = path; *trv; ++trv)
+		if (*trv == 'X')
+			xcnt++;
+		else
+			xcnt = 0;	
+
+	/* Use at least one from xtra.  Use 2 if more than 6 X's. */
+	if (*(trv-1) == 'X')
+		*--trv = xtra[0];
+	if (xcnt > 6 && *(trv-1) == 'X')
+		*--trv = xtra[1];
+
+	/* Set remaining X's to pid digits with 0's to the left. */
+	while (*--trv == 'X') {
+		*trv = (pid % 10) + '0';
+		pid /= 10;
+	}
+
+	/* update xtra for next call. */
+	if (xtra[0] != 'z')
+		xtra[0]++;
+	else {
+		xtra[0] = 'a';
+		if (xtra[1] != 'z')
+			xtra[1]++;
+		else
+			xtra[1] = 'a';
+	}
+
+	/*
+	 * check the target directory; if you have six X's and it
+	 * doesn't exist this runs for a *very* long time.
+	 */
+	for (start = trv + 1;; --trv) {
+		if (trv <= path)
+			break;
+		if (*trv == '/') {
+			*trv = '\0';
+			if (stat(path, &sbuf))
+				return (0);
+			if (!S_ISDIR(sbuf.st_mode)) {
+				errno = ENOTDIR;
+				return (0);
+			}
+			*trv = '/';
+			break;
+		}
+	}
+
+	for (;;) {
+		if (doopen) {
+			if ((*doopen =
+			    open(path, O_CREAT|O_EXCL|O_RDWR, 0600)) >= 0)
+				return (1);
+			if (errno != EEXIST)
+				return (0);
+		}
+		else if (lstat(path, &sbuf))
+			return (errno == ENOENT ? 1 : 0);
+
+		/* tricky little algorithm for backward compatibility */
+		for (trv = start;;) {
+			if (!*trv)
+				return (0);
+			if (*trv == 'z')
+				*trv++ = 'a';
+			else {
+				if (isdigit(*trv))
+					*trv = 'a';
+				else
+					++*trv;
+				break;
+			}
+		}
+	}
+	/*NOTREACHED*/
 }
