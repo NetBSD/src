b20a641f5641358ae8dd7b91e9fed8ecd0857063
perry <perry>
1998-08-04 04:48:16 +0000

    Replace complicated and ugly makefile gook to make memmove,memcpy by -Ding the bcopy.c source (and similarly strchr from index.c, strrchr from rindex.c) with stub .c files for memmove, memcpy, strchr and strrchr that simply #define the appropriate thing and #include the appropriate .c file. Inspired by the way many m-d .S files handle the same thing. This radically simplifies the Makefile.inc for libc/string (and libkern).

---
+++ lib/libc/string/memcpy.c
@@ -0,0 +1,4 @@
+/*	$NetBSD: memcpy.c,v 1.1 1998/08/04 04:48:17 perry Exp $	*/
+
+#define MEMCOPY
+#include "bcopy.c"
---
+++ lib/libc/string/strchr.c
@@ -0,0 +1,4 @@
+/*	$NetBSD: strchr.c,v 1.1 1998/08/04 04:48:17 perry Exp $	*/
+
+#define STRCHR
+#include "index.c"
