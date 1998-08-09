636cea8174c6f8f18658789ba0919a43798cda79
is <is@NetBSD.org>
1998-08-09 22:08:40 +0000

    Actually probe the hardware. This is for HyperCom 3i slaveboards (to be done soon) that might or might not sit on top of a HyperCom 3Z or HyperCom 4 or ISDN Blaster.

--- sys/arch/amiga/dev/com_supio.c
+++ sys/arch/amiga/dev/com_supio.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: com_supio.c,v 1.7 1998/04/11 18:28:34 is Exp $	*/
+/*	$NetBSD: com_supio.c,v 1.8 1998/08/09 22:08:40 is Exp $	*/
 
 /*-
  * Copyright (c) 1993, 1994, 1995, 1996
@@ -90,6 +90,7 @@ com_supio_match(parent, match, aux)
 	void *aux;
 {
 	bus_space_tag_t iot;
+	bus_space_handle_t ioh;
 	int iobase;
 	int rv = 1;
 	struct supio_attach_args *supa = aux;
@@ -102,11 +103,13 @@ com_supio_match(parent, match, aux)
 #if 0
 	/* if it's in use as console, it's there. */
 	if (iobase != comconsaddr || comconsattached) {
+#endif
 		if (bus_space_map(iot, iobase, COM_NPORTS, 0, &ioh)) {
 			return 0;
 		}
 		rv = comprobe1(iot, ioh, iobase);
 		bus_space_unmap(iot, ioh, COM_NPORTS);
+#if 0
 	}
 #endif
 	return (rv);
