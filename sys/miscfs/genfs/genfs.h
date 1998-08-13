46af7c98bc6f9c8f3457e04a3ee3f95955d296d9
kleink <kleink@NetBSD.org>
1998-08-13 09:59:52 +0000

    Add genfs_einval(), which does the obvious thing.

--- sys/miscfs/genfs/genfs.h
+++ sys/miscfs/genfs/genfs.h
@@ -1,8 +1,9 @@
-/*	$NetBSD: genfs.h,v 1.7 1998/08/10 08:11:11 matthias Exp $	*/
+/*	$NetBSD: genfs.h,v 1.8 1998/08/13 09:59:52 kleink Exp $	*/
 
 int	genfs_badop	__P((void *));
 int	genfs_nullop	__P((void *));
 int	genfs_enoioctl	__P((void *));
+int	genfs_einval	__P((void *));
 int	genfs_eopnotsupp __P((void *));
 int	genfs_ebadf	__P((void *));
 int	genfs_nolock	__P((void *));
