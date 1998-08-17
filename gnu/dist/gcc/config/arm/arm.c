78e8c617cc996c1e22c14991e43931c5b4cf34a9
tv <tv@NetBSD.org>
1998-08-17 04:02:10 +0000

    The available system headers are defined by "system.h" - don't try to second-guess them here.

--- gnu/dist/gcc/config/arm/arm.c
+++ gnu/dist/gcc/config/arm/arm.c
@@ -22,8 +22,7 @@ the Free Software Foundation, 59 Temple Place - Suite 330,
 Boston, MA 02111-1307, USA.  */
     
 #include "config.h"
-#include <stdio.h>
-#include <string.h>
+#include "system.h"
 #include "rtl.h"
 #include "regs.h"
 #include "hard-reg-set.h"
