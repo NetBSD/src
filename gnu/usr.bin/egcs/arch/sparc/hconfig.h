18b7cf53f442543aa1450fd9a2259752a465c193
tv <tv@NetBSD.org>
1998-08-17 03:35:58 +0000

    Update to egcs 1.1 snapshot, and add NetBSD rcsids.

--- gnu/usr.bin/egcs/arch/sparc/hconfig.h
+++ gnu/usr.bin/egcs/arch/sparc/hconfig.h
@@ -1,1 +1,4 @@
+/*	$NetBSD: hconfig.h,v 1.2 1998/08/17 03:35:58 tv Exp $	*/
+
+#include "auto-host.h"
 #include "sparc/xm-netbsd.h"
--- gnu/usr.bin/egcs/arch/sparc/tm.h
+++ gnu/usr.bin/egcs/arch/sparc/tm.h
@@ -1,1 +1,3 @@
+/*	$NetBSD: tm.h,v 1.2 1998/08/17 03:35:58 tv Exp $	*/
+
 #include "sparc/netbsd.h"
