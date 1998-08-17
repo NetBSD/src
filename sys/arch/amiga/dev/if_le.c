098ca1b2cc6478723e48b997d1f4fc35f92bd0d3
mycroft <mycroft@NetBSD.org>
1998-08-17 00:26:32 +0000

    Assign my copyrights to TNF.

--- sys/arch/amiga/dev/if_le.c
+++ sys/arch/amiga/dev/if_le.c
@@ -1,12 +1,12 @@
-/*	$NetBSD: if_le.c,v 1.30 1998/07/21 17:36:01 drochner Exp $	*/
+/*	$NetBSD: if_le.c,v 1.31 1998/08/17 00:26:32 mycroft Exp $	*/
 
 /*-
- * Copyright (c) 1997 The NetBSD Foundation, Inc.
+ * Copyright (c) 1997, 1998 The NetBSD Foundation, Inc.
  * All rights reserved.
  *
  * This code is derived from software contributed to The NetBSD Foundation
- * by Jason R. Thorpe of the Numerical Aerospace Simulation Facility,
- * NASA Ames Research Center.
+ * by Charles M. Hannum and by Jason R. Thorpe of the Numerical Aerospace
+ * Simulation Facility, NASA Ames Research Center.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -39,7 +39,6 @@
 
 /*-
  * Copyright (c) 1997 Bernd Ernesti.  All rights reserved.
- * Copyright (c) 1995 Charles M. Hannum.  All rights reserved.
  * Copyright (c) 1992, 1993
  *	The Regents of the University of California.  All rights reserved.
  *
