92955c525b6edfa0e00ed7f00b17193e2543a50b
tv <tv@NetBSD.org>
1998-08-22 19:02:10 +0000

    Revamp the libbfd build process.  Massive cleanup, enable 64 bit support on SPARC, and use includes (or dynamically built ones) from dist.

--- gnu/lib/bfd/_alpha.mk
+++ gnu/lib/bfd/_alpha.mk
@@ -1,20 +1,14 @@
-# $NetBSD: _alpha.mk,v 1.3 1998/08/21 21:11:02 tv Exp $
+#	$NetBSD: _alpha.mk,v 1.4 1998/08/22 19:02:10 tv Exp $
 
-BFD_MACHINES =	cpu-alpha.c
-BFD_BACKENDS =	elf64-alpha.c elf64.c elf.c elflink.c \
-		coff-alpha.c ecoff.c ecofflink.c
+BFD_ARCH_SRCS=	cpu-alpha.c \
+		elf64-alpha.c elf64.c elf.c elflink.c \
+		coff-alpha.c ecoff.c ecofflink.c \
+		alpha-dis.c alpha-opc.c
 
-ARCH_DEFS = -DARCH_alpha \
- -DSELECT_ARCHITECTURES='&bfd_alpha_arch'
-
-TDEFAULTS = \
- -DDEFAULT_VECTOR=bfd_elf64_alpha_vec \
- -DSELECT_VECS=' &bfd_elf64_alpha_vec, &ecoffalpha_little_vec ' \
- -DHAVE_bfd_elf64_alpha_vec \
- -DHAVE_ecoffalpha_little_vec \
- -DNETBSD_CORE
-
-OPCODE_MACHINES =  alpha-dis.c alpha-opc.c
-
-BFD_WORDSIZE=	64
-BFD_64BIT_LONG=	1
+BFD_ARCH_DEFS=	-DARCH_alpha \
+		-DSELECT_ARCHITECTURES='&bfd_alpha_arch' \
+		-DDEFAULT_VECTOR=bfd_elf64_alpha_vec \
+		-DSELECT_VECS='&bfd_elf64_alpha_vec, &ecoffalpha_little_vec' \
+		-DHAVE_bfd_elf64_alpha_vec \
+		-DHAVE_ecoffalpha_little_vec \
+		-DNETBSD_CORE
