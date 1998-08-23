781d14e7806103f0762d100e6c794a328f9d72e0
frueauf <frueauf@NetBSD.org>
1998-08-23 20:57:45 +0000

    Add missing \, pointed out by Frederick Bruckman <fb@enteract.com>.

--- gnu/lib/bfd/_m68k.mk
+++ gnu/lib/bfd/_m68k.mk
@@ -1,4 +1,4 @@
-#	$NetBSD: _m68k.mk,v 1.4 1998/08/22 19:02:10 tv Exp $
+#	$NetBSD: _m68k.mk,v 1.5 1998/08/23 20:57:45 frueauf Exp $
 
 BFD_ARCH_SRCS=	cpu-m68k.c \
 		m68knetbsd.c m68k4knetbsd.c sunos.c aout32.c \
@@ -6,7 +6,7 @@ BFD_ARCH_SRCS=	cpu-m68k.c \
 		m68k-dis.c m68k-opc.c
 
 BFD_ARCH_DEFS=	-DARCH_m68k \
-		-DSELECT_ARCHITECTURES='&bfd_m68k_arch'
+		-DSELECT_ARCHITECTURES='&bfd_m68k_arch' \
 		-DDEFAULT_VECTOR=m68knetbsd_vec \
 		-DSELECT_VECS='&m68knetbsd_vec, &m68k4knetbsd_vec, &bfd_elf32_m68k_vec, &sunos_big_vec' \
 		-DHAVE_m68knetbsd_vec \
