e19c8fc2cc9041302e4073fccf09c93635cfb1dc
pk <pk>
1998-08-29 20:49:35 +0000

    Split `esp' driver and DMA engine into separate front-end files. The Sbus front-ends have all been moved to `sys/dev/sbus', while the `obio' front-ends remain here.

--- sys/arch/sparc/dev/if_le_obio.c
+++ sys/arch/sparc/dev/if_le_obio.c
@@ -1,12 +1,12 @@
-/*	$NetBSD: if_le_obio.c,v 1.4 1998/08/15 10:51:16 mycroft Exp $	*/
+/*	$NetBSD: if_le_obio.c,v 1.5 1998/08/29 20:49:37 pk Exp $	*/
 
 /*-
  * Copyright (c) 1997, 1998 The NetBSD Foundation, Inc.
  * All rights reserved.
  *
  * This code is derived from software contributed to The NetBSD Foundation
- * by Charles M. Hannum and by Jason R. Thorpe of the Numerical Aerospace
- * Simulation Facility, NASA Ames Research Center.
+ * by Charles M. Hannum; Jason R. Thorpe of the Numerical Aerospace
+ * Simulation Facility, NASA Ames Research Center; Paul Kranenburg.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -37,67 +37,18 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
-/*-
- * Copyright (c) 1996
- *	The President and Fellows of Harvard College. All rights reserved.
- * Copyright (c) 1992, 1993
- *	The Regents of the University of California.  All rights reserved.
- *
- * This code is derived from software contributed to Berkeley by
- * Ralph Campbell and Rick Macklem.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed by Aaron Brown and
- *	Harvard University.
- *	This product includes software developed by the University of
- *	California, Berkeley and its contributors.
- * 4. Neither the name of the University nor the names of its contributors
- *    may be used to endorse or promote products derived from this software
- *    without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- *
- *	@(#)if_le.c	8.2 (Berkeley) 11/16/93
- */
-
 #include "opt_inet.h"
 #include "bpfilter.h"
 
 #include <sys/param.h>
 #include <sys/systm.h>
-#include <sys/mbuf.h>
-#include <sys/syslog.h>
-#include <sys/socket.h>
 #include <sys/device.h>
 #include <sys/malloc.h>
+#include <sys/socket.h>
 
 #include <net/if.h>
 #include <net/if_ether.h>
 #include <net/if_media.h>
-
-#ifdef INET
-#include <netinet/in.h>
-#include <netinet/if_inarp.h>
-#endif
 
 #include <machine/autoconf.h>
 #include <machine/cpu.h>
@@ -110,16 +61,14 @@
 /*
  * LANCE registers.
  */
-struct lereg1 {
-	volatile u_int16_t	ler1_rdp;	/* data port */
-	volatile u_int16_t	ler1_rap;	/* register select port */
-};
+#define LEREG1_RDP	0	/* Register Data port */
+#define LEREG1_RAP	2	/* Register Address port */
 
 struct	le_softc {
-	struct	am7990_softc sc_am7990;	/* glue to MI code */
-	bus_space_tag_t	sc_bustag;
-	bus_dma_tag_t	sc_dmatag;
-	struct	lereg1 *sc_r1;		/* LANCE registers */
+	struct	am7990_softc	sc_am7990;	/* glue to MI code */
+	bus_space_tag_t		sc_bustag;
+	bus_dma_tag_t		sc_dmatag;
+	bus_space_handle_t	sc_reg;		/* LANCE registers */
 };
 
 #define MEMSIZE 0x4000		/* LANCE memory size */
@@ -161,10 +110,10 @@ lewrcsr(sc, port, val)
 	struct lance_softc *sc;
 	u_int16_t port, val;
 {
-	struct lereg1 *ler1 = ((struct le_softc *)sc)->sc_r1;
+	struct le_softc *lesc = (struct le_softc *)sc;
 
-	ler1->ler1_rap = port;
-	ler1->ler1_rdp = val;
+	bus_space_write_2(lesc->sc_bustag, lesc->sc_reg, LEREG1_RAP, port);
+	bus_space_write_2(lesc->sc_bustag, lesc->sc_reg, LEREG1_RDP, val);
 }
 
 static u_int16_t
@@ -172,12 +121,10 @@ lerdcsr(sc, port)
 	struct lance_softc *sc;
 	u_int16_t port;
 {
-	struct lereg1 *ler1 = ((struct le_softc *)sc)->sc_r1;
-	u_int16_t val;
+	struct le_softc *lesc = (struct le_softc *)sc;
 
-	ler1->ler1_rap = port;
-	val = ler1->ler1_rdp;
-	return (val);
+	bus_space_write_2(lesc->sc_bustag, lesc->sc_reg, LEREG1_RAP, port);
+	return (bus_space_read_2(lesc->sc_bustag, lesc->sc_reg, LEREG1_RDP));
 }
 
 int
@@ -209,7 +156,6 @@ leattach_obio(parent, self, aux)
 	struct obio4_attach_args *oba = &uoba->uoba_oba4;
 	struct le_softc *lesc = (struct le_softc *)self;
 	struct lance_softc *sc = &lesc->sc_am7990.lsc;
-	bus_space_handle_t bh;
 	bus_dma_segment_t seg;
 	int rseg;
 	/* XXX the following declarations should be elsewhere */
@@ -219,13 +165,12 @@ leattach_obio(parent, self, aux)
 	lesc->sc_dmatag = oba->oba_dmatag;
 
 	if (obio_bus_map(oba->oba_bustag, oba->oba_paddr,
-			 0, sizeof(struct lereg1),
+			 0, 2 * sizeof(u_int16_t),
 			 0, 0,
-			 &bh) != 0) {
+			 &lesc->sc_reg) != 0) {
 		printf("%s @ obio: cannot map registers\n", self->dv_xname);
 		return;
 	}
-	lesc->sc_r1 = (struct lereg1 *)bh;
 
 	if (oba->oba_bp != NULL &&
 	    strcmp(oba->oba_bp->name, le_cd.cd_name) == 0 &&
