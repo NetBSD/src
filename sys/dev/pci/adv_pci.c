e5433094f0f300cb17a65ca007c8e21fd809f008
dante <dante@NetBSD.org>
1998-08-29 13:54:50 +0000

    Adjust copyright justification

--- sys/dev/pci/adv_pci.c
+++ sys/dev/pci/adv_pci.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: adv_pci.c,v 1.2 1998/08/29 13:44:49 dante Exp $	*/
+/*	$NetBSD: adv_pci.c,v 1.3 1998/08/29 13:54:50 dante Exp $	*/
 
 /*
  * Copyright (c) 1998 The NetBSD Foundation, Inc. All rights reserved.
@@ -6,18 +6,20 @@
  * Author: Baldassare Dante Profeta <dante@mclink.it>
  * 
  * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are
- * met: 1. Redistributions of source code must retain the above copyright
- * notice, this list of conditions and the following disclaimer. 2.
- * Redistributions in binary form must reproduce the above copyright notice,
- * this list of conditions and the following disclaimer in the documentation
- * and/or other materials provided with the distribution. 3. All advertising
- * materials mentioning features or use of this software must display the
- * following acknowledgement: This product includes software developed by the
- * NetBSD Foundation, Inc. and its contributors. 4. Neither the name of The
- * NetBSD Foundation nor the names of its contributors may be used to endorse
- * or promote products derived from this software without specific prior
- * written permission.
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *    This product includes software developed by the NetBSD
+ *    Foundation, Inc. and its contributors.
+ * 4. Neither the name of The NetBSD Foundation nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
  * 
  * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
  * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
