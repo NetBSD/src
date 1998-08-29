edda33e00c2cbfb4e0e1c5d50068525d2695db32
mrg <mrg@NetBSD.org>
1998-08-29 17:01:14 +0000

    move <vm/vm_swap.h> to <sys/swap.h>. <vm/vm_swap.h> still works for now (goes away later)

--- sys/uvm/uvm_swap.c
+++ sys/uvm/uvm_swap.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: uvm_swap.c,v 1.16 1998/08/29 13:27:51 mrg Exp $	*/
+/*	$NetBSD: uvm_swap.c,v 1.17 1998/08/29 17:01:14 mrg Exp $	*/
 
 /*
  * Copyright (c) 1995, 1996, 1997 Matthew R. Green
@@ -50,9 +50,9 @@
 #include <sys/mount.h>
 #include <sys/pool.h>
 #include <sys/syscallargs.h>
+#include <sys/swap.h>
 
 #include <vm/vm.h>
-#include <vm/vm_swap.h>
 #include <vm/vm_conf.h>
 
 #include <uvm/uvm.h>
