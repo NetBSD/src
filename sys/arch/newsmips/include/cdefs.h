46eaa72b3dc25895bbc5a5a4ebf5a7a5ce032797
ross <ross>
1998-08-30 23:02:50 +0000

    This is a merge *on a branch* of all NetBSD ncr.c changes into the FreeBSD-current (1.123) ncr.c.  It didn't actually solve my '875 problems; we will decide whether to actually use this work later.
    	ross-ncr-fb1-123-base		the branch point
    	ross-ncr-fb1-123-merge		the branch

--- sys/arch/newsmips/include/cdefs.h
+++
@@ -1,3 +0,0 @@
-/*	$NetBSD: cdefs.h,v 1.1 1998/02/18 13:48:16 tsubai Exp $	*/
-
-#include <mips/cdefs.h>
--- sys/arch/ofppc/include/pcb.h
+++
@@ -1,3 +0,0 @@
-/*	$NetBSD: pcb.h,v 1.1 1998/05/28 08:12:15 sakamoto Exp $	*/
-
-#include <powerpc/pcb.h>
