ba8b8a651bd0bf8e85610f18fa45035ba2aece16
thorpej <thorpej@NetBSD.org>
1998-09-08 23:57:58 +0000

    Implement a new kernel thread, the "reaper", which performs the task of freeing the VM resources once a process has exited.  A valid thread must do this work, as doing so may block in a multi-processor environment.

--- sys/kern/kern_exit.c
+++ sys/kern/kern_exit.c
@@ -1,4 +1,41 @@
-/*	$NetBSD: kern_exit.c,v 1.55 1998/09/01 01:02:33 thorpej Exp $	*/
+/*	$NetBSD: kern_exit.c,v 1.56 1998/09/08 23:57:58 thorpej Exp $	*/
+
+/*-
+ * Copyright (c) 1998 The NetBSD Foundation, Inc.
+ * All rights reserved.
+ *  
+ * This code is derived from software contributed to The NetBSD Foundation
+ * by Jason R. Thorpe of the Numerical Aerospace Simulation Facility,
+ * NASA Ames Research Center.  
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright 
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *      This product includes software developed by the NetBSD
+ *      Foundation, Inc. and its contributors.
+ * 4. Neither the name of The NetBSD Foundation nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *      
+ * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 
 /*
  * Copyright (c) 1982, 1986, 1989, 1991, 1993
@@ -215,11 +252,14 @@ exit1(p, rv)
 #endif
 	/*
 	 * Remove proc from allproc queue and pidhash chain.
-	 * Place onto zombproc.  Unlink from parent's child list.
+	 * Unlink from parent's child list.
 	 */
 	LIST_REMOVE(p, p_list);
-	LIST_INSERT_HEAD(&zombproc, p, p_list);
 	p->p_stat = SZOMB;
+
+	/*
+	 * NOTE: WE ARE NO LONGER ALLOWED TO SLEEP!
+	 */
 
 	LIST_REMOVE(p, p_hash);
 
@@ -249,13 +289,6 @@ exit1(p, rv)
 	ruadd(p->p_ru, &p->p_stats->p_cru);
 
 	/*
-	 * Notify parent that we're gone.
-	 */
-	if ((p->p_flag & P_FSTRACE) == 0)
-		psignal(p->p_pptr, SIGCHLD);
-	wakeup((caddr_t)p->p_pptr);
-
-	/*
 	 * Clear curproc after we've done all operations
 	 * that could block, and before tearing down the rest
 	 * of the process state that might be used from clock, etc.
@@ -270,15 +303,71 @@ exit1(p, rv)
 		pool_put(&plimit_pool, p->p_limit);
 
 	/*
-	 * Finally, call machine-dependent code to release the remaining
-	 * resources including address space, the kernel stack and pcb.
-	 * The address space is released by "vmspace_free(p->p_vmspace)";
-	 * This is machine-dependent, as we may have to change stacks
-	 * or ensure that the current one isn't reallocated before we
-	 * finish.  cpu_exit will end with a call to cpu_swtch(), finishing
-	 * our execution (pun intended).
+	 * Finally, call machine-dependent code to switch to a new
+	 * context (possibly the idle context).  Once we are no longer
+	 * using the dead process's vmspace and stack, exit2() will be
+	 * called to schedule those resources to be released by the
+	 * reaper thread.
+	 *
+	 * Note that cpu_exit() will end with a call equivalent to
+	 * cpu_switch(), finishing our execution (pun intended).
 	 */
 	cpu_exit(p);
+}
+
+/*
+ * We are called from cpu_exit() once it is safe to schedule the
+ * dead process's resources to be freed.
+ */
+void
+exit2(p)
+	struct proc *p;
+{
+
+	LIST_INSERT_HEAD(&deadproc, p, p_list);
+	wakeup(&deadproc);
+}
+
+/*
+ * Process reaper.  This is run by a kernel thread to free the resources
+ * of a dead process.  Once the resources are free, the process becomes
+ * a zombie, and the parent is allowed to read the undead's status.
+ */
+void
+reaper()
+{
+	struct proc *p;
+
+	for (;;) {
+		p = LIST_FIRST(&deadproc);
+		if (p == NULL) {
+			/* No work for us; go to sleep until someone exits. */
+			(void) tsleep(&deadproc, PVM, "reaper", 0);
+			continue;
+		}
+
+		/* Remove us from the deadproc list. */
+		LIST_REMOVE(p, p_list);
+
+		/*
+		 * Free the VM resources we're still holding on to.
+		 * We must do this from a valid thread because doing
+		 * so may block.
+		 */
+#if defined(UVM)
+		uvm_exit(p);
+#else
+		vm_exit(p);
+#endif
+
+		/* Process is now a true zombie. */
+		LIST_INSERT_HEAD(&zombproc, p, p_list);
+
+		/* Wake up the parent so it can get exit satus. */
+		if ((p->p_flag & P_FSTRACE) == 0)
+			psignal(p->p_pptr, SIGCHLD);
+		wakeup((caddr_t)p->p_pptr);
+	}
 }
 
 int
@@ -353,7 +442,9 @@ loop:
 			 * Unlink it from its process group and free it.
 			 */
 			leavepgrp(p);
+
 			LIST_REMOVE(p, p_list);	/* off zombproc */
+
 			LIST_REMOVE(p, p_sibling);
 
 			/*
