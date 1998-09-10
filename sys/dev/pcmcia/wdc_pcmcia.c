537261f6c515ad917b46fc050bd6818cf0267a9e
kenh <kenh@NetBSD.org>
1998-09-10 19:55:30 +0000

    Reorganize the probe routine and add a quirk table.  Much of this code comes from Enami Tsugutomo (enami@but-b.or.jp).  Also add a couple more devices to the pcmcia devices table.

--- sys/dev/pcmcia/wdc_pcmcia.c
+++ sys/dev/pcmcia/wdc_pcmcia.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: wdc_pcmcia.c,v 1.8 1998/08/15 10:10:55 mycroft Exp $ */
+/*	$NetBSD: wdc_pcmcia.c,v 1.9 1998/09/10 19:55:31 kenh Exp $ */
 
 /*-
  * Copyright (c) 1998 The NetBSD Foundation, Inc.
@@ -59,11 +59,13 @@
 #include <machine/bus.h>
 
 #include <dev/pcmcia/pcmciavar.h>
+#include <dev/pcmcia/pcmciadevs.h>
+
 #include <dev/isa/isavar.h>
 #include <dev/ic/wdcvar.h>
 
 #define WDC_PCMCIA_REG_NPORTS      8
-#define WDC_PCMCIA_AUXREG_OFFSET   0x206
+#define WDC_PCMCIA_AUXREG_OFFSET   (WDC_PCMCIA_REG_NPORTS + 6)
 #define WDC_PCMCIA_AUXREG_NPORTS   2
 
 struct wdc_pcmcia_softc {
@@ -83,6 +85,73 @@ struct cfattach wdc_pcmcia_ca = {
 	sizeof(struct wdc_pcmcia_softc), wdc_pcmcia_match, wdc_pcmcia_attach
 };
 
+struct wdc_pcmcia_product {
+	u_int32_t	wpp_vendor;	/* vendor ID */
+	u_int32_t	wpp_product;	/* product ID */
+	int		wpp_quirk_flag;	/* Quirk flags */
+#define WDC_PCMCIA_FORCE_16BIT_IO	0x01 /* Don't use PCMCIA_WIDTH_AUTO */
+#define WDC_PCMCIA_NO_EXTRA_RESETS	0x02 /* Only reset ctrl once */
+	const char	*wpp_cis_info[4];	/* XXX necessary? */
+	const char	*wpp_name;	/* product name */
+} wdc_pcmcia_products[] = {
+
+	{ /* PCMCIA_VENDOR_DIGITAL XXX */ 0x0100,
+	  PCMCIA_PRODUCT_DIGITAL_MOBILE_MEDIA_CDROM,
+	  0, { NULL, "Digital Mobile Media CD-ROM", NULL, NULL },
+	  PCMCIA_STR_DIGITAL_MOBILE_MEDIA_CDROM },
+
+	{ PCMCIA_VENDOR_HAGIWARASYSCOM,
+	  -1,			/* XXX */
+	  WDC_PCMCIA_FORCE_16BIT_IO,
+	  { NULL, NULL, NULL, NULL },
+	 "Hagiwara SYS-COM ComactFlash Card" },
+
+	/* CANON FC-8M is also matches.  */
+	{ PCMCIA_VENDOR_SANDISK,
+	  PCMCIA_PRODUCT_SANDISK_SDCFB,
+	  PCMCIA_WIDTH_IO16 /* _AUTO also works */, { NULL, NULL, NULL, NULL },
+	  PCMCIA_STR_SANDISK_SDCFB },
+
+	/* The TEAC IDE/Card II is used on the Sony Vaio */
+	{ PCMCIA_VENDOR_TEAC,
+	  PCMCIA_PRODUCT_TEAC_IDECARDII,
+	  WDC_PCMCIA_NO_EXTRA_RESETS,
+	  PCMCIA_CIS_TEAC_IDECARDII,
+	  PCMCIA_STR_TEAC_IDECARDII },
+
+	{ 0, 0, 0, { NULL, NULL, NULL, NULL}, NULL }
+};
+
+struct wdc_pcmcia_product *
+	wdc_pcmcia_lookup __P((struct pcmcia_attach_args *));
+
+struct wdc_pcmcia_product *
+wdc_pcmcia_lookup(pa)
+	struct pcmcia_attach_args *pa;
+{
+	struct wdc_pcmcia_product *wpp;
+	int i, cis_match;
+
+	for (wpp = wdc_pcmcia_products; wpp->wpp_name != NULL; wpp++)
+		if ((wpp->wpp_vendor == -1 ||
+		     pa->manufacturer == wpp->wpp_vendor) &&
+		    (wpp->wpp_product == -1 ||
+		     pa->product == wpp->wpp_product)) {
+			cis_match = 1;
+			for (i = 0; i < 4; i++) {
+				if (!(wpp->wpp_cis_info[i] == NULL ||
+				      (pa->card->cis1_info[i] != NULL &&
+				       strcmp(pa->card->cis1_info[i],
+					      wpp->wpp_cis_info[i]) == 0)))
+					cis_match = 0;
+			}
+			if (cis_match)
+				return(wpp);
+		}
+
+	return (NULL);
+}
+
 static int
 wdc_pcmcia_match(parent, match, aux)
 	struct device *parent;
@@ -91,14 +160,10 @@ wdc_pcmcia_match(parent, match, aux)
 {
 	struct pcmcia_attach_args *pa = aux;
 
-	if (pa->card->manufacturer == 0x100 && pa->card->product == 0xd00) {
-		if (pa->card->cis1_info[1] != NULL &&
-		    strcmp(pa->card->cis1_info[1], "Digital Mobile Media CD-ROM") == 0)
-			return 1;
-	}
+	if (wdc_pcmcia_lookup(pa) != NULL)
+		return (1);
 
-
-	return 0;
+	return (0);
 }
 
 static void
@@ -110,26 +175,32 @@ wdc_pcmcia_attach(parent, self, aux)
 	struct wdc_pcmcia_softc *sc = (void *)self;
 	struct pcmcia_attach_args *pa = aux;
 	struct pcmcia_config_entry *cfe;
+	struct wdc_pcmcia_product *wpp;
 
 	for (cfe = SIMPLEQ_FIRST(&pa->pf->cfe_head); cfe != NULL;
 	    cfe = SIMPLEQ_NEXT(cfe, cfe_list)) {
-		if (pcmcia_io_alloc(pa->pf, cfe->iospace[0].start,
-		    cfe->iospace[0].length, 0, &sc->sc_pioh))
+		if (cfe->num_iospace != 1 && cfe->num_iospace != 2)
 			continue;
-		if (cfe->num_iospace > 1) {
+
+		if (pcmcia_io_alloc(pa->pf, cfe->iospace[0].start,
+		    cfe->iospace[0].length,
+		    cfe->iospace[0].start == 0 ? cfe->iospace[0].length : 0,
+		    &sc->sc_pioh))
+			continue;
+
+		if (cfe->num_iospace == 2) {
 			if (!pcmcia_io_alloc(pa->pf, cfe->iospace[1].start,
-			    cfe->iospace[1].length, 0, &sc->sc_auxpioh)) {
+			    cfe->iospace[1].length, 0, &sc->sc_auxpioh))
 				break;
-			}
-		} else {
+		} else /* num_iospace == 1 */ {
 			sc->sc_auxpioh.iot = sc->sc_pioh.iot;
-			if (!bus_space_subregion(sc->sc_pioh.iot, sc->sc_pioh.ioh,
-			    WDC_PCMCIA_REG_NPORTS,
-			    cfe->iospace[0].length - WDC_PCMCIA_REG_NPORTS,
-			    &sc->sc_auxpioh.ioh))
+			if (!bus_space_subregion(sc->sc_pioh.iot,
+			    sc->sc_pioh.ioh, WDC_PCMCIA_AUXREG_OFFSET,
+			    WDC_PCMCIA_AUXREG_NPORTS, &sc->sc_auxpioh.ioh))
 				break;
 		}
-		pcmcia_chip_io_free(pa->pf->sc->pct, pa->pf->sc->pch, &sc->sc_pioh);
+		pcmcia_chip_io_free(pa->pf->sc->pct, pa->pf->sc->pch,
+		    &sc->sc_pioh);
 	}
 
 	if (cfe == NULL) {
@@ -149,15 +220,33 @@ wdc_pcmcia_attach(parent, self, aux)
 		return;
 	}
 
-	if (pcmcia_io_map(pa->pf, PCMCIA_WIDTH_AUTO, 0,
-			  sc->sc_pioh.size, &sc->sc_pioh,
-			  &sc->sc_iowindow)) {
+	/*
+	 * XXX  DEC Mobile Media CDROM is not yet tested whether it works
+	 * XXX  with PCMCIA_WIDTH_IO16.  HAGIWARA SYS-COM HPC-CF32 doesn't
+	 * XXX  work with PCMCIA_WIDTH_AUTO.
+	 * XXX  CANON FC-8M (SANDISK SDCFB 8M) works for both _AUTO and IO16.
+	 * XXX  So, here is temporary work around.
+	 */
+	wpp = wdc_pcmcia_lookup(pa);
+	if (wpp == NULL)
+		panic("wdc_pcmcia_attach: impossible");
+
+	if (pcmcia_io_map(pa->pf,
+			  wpp->wpp_quirk_flag & WDC_PCMCIA_FORCE_16BIT_IO ?
+			  PCMCIA_WIDTH_IO16 : PCMCIA_WIDTH_AUTO, 0,
+			  sc->sc_pioh.size, &sc->sc_pioh, &sc->sc_iowindow)) {
 		printf(": can't map first I/O space\n");
 		return;
 	} 
-	if (pcmcia_io_map(pa->pf, PCMCIA_WIDTH_AUTO, 0,
-			  sc->sc_auxpioh.size, &sc->sc_auxpioh,
-			  &sc->sc_auxiowindow)) {
+
+	/*
+	 * Currently, # of iospace is 1 except DIGITAL Mobile Media CD-ROM.
+	 * So whether the work around like above is necessary or not
+	 * is unknown.  XXX.
+	 */
+	if (cfe->num_iospace > 1 &&
+	    pcmcia_io_map(pa->pf, PCMCIA_WIDTH_AUTO, 0,
+	    sc->sc_auxpioh.size, &sc->sc_auxpioh, &sc->sc_auxiowindow)) {
 		printf(": can't map second I/O space\n");
 		return;
 	}
@@ -169,6 +258,9 @@ wdc_pcmcia_attach(parent, self, aux)
 		printf("%s: couldn't establish interrupt\n", self->dv_xname);
 		return;
 	}
+
+	if (wpp->wpp_quirk_flag & WDC_PCMCIA_NO_EXTRA_RESETS)
+		sc->sc_ad.flags |= WDC_NO_EXTRA_RESETS;
 
 	wdcattach(&sc->sc_wdcdev, &sc->sc_ad);
 }
