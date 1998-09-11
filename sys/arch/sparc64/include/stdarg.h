06d8c571014c77605a5cfb96838da5d90da6e745
eeh <eeh>
1998-09-11 00:12:41 +0000

    A (slightly modified) version of tv's fix for va_args for kern_printf.

--- sys/arch/sparc64/include/stdarg.h
+++ sys/arch/sparc64/include/stdarg.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: stdarg.h,v 1.2 1998/07/27 13:55:35 mycroft Exp $ */
+/*	$NetBSD: stdarg.h,v 1.3 1998/09/11 00:12:41 eeh Exp $ */
 
 /*
  * Copyright (c) 1992, 1993
@@ -44,20 +44,16 @@
  *	from: @(#)stdarg.h	8.2 (Berkeley) 9/27/93
  */
 
-#ifndef _SPARC_STDARG_H_
-#define	_SPARC_STDARG_H_
+#ifndef _SPARC64_STDARG_H_
+#define	_SPARC64_STDARG_H_
 
 #include <machine/ansi.h>
 
 #ifdef __lint__
-#define	__extension__(x)		(0)
 #define	__builtin_classify_type(t)	(0)
 #endif
 
 typedef _BSD_VA_LIST_	va_list;
-
-#define	__va_size(type) \
-	(((sizeof(type) + sizeof(long) - 1) / sizeof(long)) * sizeof(long))
 
 #if __GNUC__ > 2 || __GNUC_MINOR__ >= 6
 #define	va_start(ap, last) \
@@ -67,45 +63,26 @@ typedef _BSD_VA_LIST_	va_list;
 	(__builtin_next_arg(), (ap) = (va_list)__builtin_saveregs())
 #endif
 
-/*
- * va_arg picks up the next argument of type `type'.  Appending an
- * asterisk to `type' must produce a pointer to `type' (i.e., `type'
- * may not be, e.g., `int (*)()').
- *
- * Gcc-2.x tries to use ldd/std for double and quad_t values, but Sun's
- * brain-damaged calling convention does not quad-align these.  Thus, for
- * 8-byte arguments, we have to pick up the actual value four bytes at a
- * time, and use type punning (i.e., a union) to produce the result.
- * (We could also do this with a libc function, actually, by returning
- * 8 byte integers in %o0+%o1 and the same 8 bytes as a double in %f0+%f1.)
- *
- * Note: We don't declare __d with type `type', since in C++ the type might
- * have a constructor.
- */
-#if __GNUC__ == 1
-#define	__extension__
-#endif
-
-#define	__va_8byte(ap, type) \
-	__extension__ ({						\
-		union { char __d[sizeof(type)]; int __i[2]; } __va_u;	\
-		__va_u.__i[0] = ((int *)(void *)(ap))[0];		\
-		__va_u.__i[1] = ((int *)(void *)(ap))[1];		\
-		(ap) += 8; *(type *)(va_list)__va_u.__d;		\
-	})
-
 #define	__va_arg(ap, type) \
-	(*(type *)((ap) += __va_size(type),			\
-		   (ap) - (sizeof(type) < sizeof(long) &&	\
-			   sizeof(type) != __va_size(type) ?	\
-			   sizeof(type) : __va_size(type))))
+	(*(type *)((ap) += 8, (ap) - 8))
 
+/* Like __va_arg(), except when the type must be 16-byte aligned. */
+#define	__va_arg16(ap, type) \
+	(*(type *)((__alignof__(type) == 16 ?				\
+		    (ap) = (va_list)(((unsigned long)(ap) + 31) & -16) :\
+			(ap) += 16), (ap) - 16))
+
+#define	__REAL_TYPE_CLASS	8
 #define	__RECORD_TYPE_CLASS	12
 #define va_arg(ap, type) \
-	(__builtin_classify_type(*(type *)0) >= __RECORD_TYPE_CLASS ?	\
-	 *__va_arg(ap, type *) : __va_size(type) == 8 ?			\
-	 __va_8byte(ap, type) : __va_arg(ap, type))
+	(__builtin_classify_type(*(type *)0) == __REAL_TYPE_CLASS ?	\
+	 __va_arg16(ap, type) :						\
+	 (__builtin_classify_type(*(type *)0) < __RECORD_TYPE_CLASS ?	\
+	  __va_arg(ap, type) :						\
+	  (sizeof(type) <= 8 ? __va_arg(ap, type) :			\
+	   (sizeof(type) <= 16 ? __va_arg16(ap, type) :			\
+	    *__va_arg(ap, type *)))))
 
 #define va_end(ap)	
 
-#endif /* !_SPARC_STDARG_H_ */
+#endif /* !_SPARC64_STDARG_H_ */
