f9ba0db19b08f9b8fdf89f9dfc1196199ea20bca
christos <christos>
1998-09-11 14:20:30 +0000

    PR/5793: Fix core dump caused by input strings more than 127 characters in length when readline is used (rl_len was a *char*!!!). Undo disabling of readline (this fix was like ``finger hurts, cut hand'') I guess PR/5793 is already closed...

--- gnu/usr.bin/bc/config.h
+++ gnu/usr.bin/bc/config.h
@@ -39,7 +39,7 @@
 /* #undef BC_MATH_FILE */
 
 /* Define to use the readline library. */
-/* #undef READLINE */
+#define READLINE 1
 
 /* Define to `size_t' if <sys/types.h> and <stddef.h> don't define.  */
 /* #undef ptrdiff_t */
