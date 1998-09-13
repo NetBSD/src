90054fd5027831ceab38543908d5bfe6753a8433
pk <pk@NetBSD.org>
1998-09-13 11:41:02 +0000

    New sigcontext.

--- sys/arch/sparc/include/signal.h
+++ sys/arch/sparc/include/signal.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: signal.h,v 1.5 1998/05/25 20:59:03 kleink Exp $ */
+/*	$NetBSD: signal.h,v 1.6 1998/09/13 11:41:02 pk Exp $ */
 
 /*
  * Copyright (c) 1992, 1993
@@ -64,7 +64,8 @@ typedef int sig_atomic_t;
  *
  * All machines must have an sc_onstack and sc_mask.
  */
-struct sigcontext {
+#if defined(__LIBC12_SOURCE__) || (defined(_KERNEL) && defined(COMPAT_13))
+struct sigcontext13 {
 	int	sc_onstack;		/* sigstack state to restore */
 	int	sc_mask;		/* signal mask to restore */
 	/* begin machine dependent portion */
@@ -74,6 +75,19 @@ struct sigcontext {
 	int	sc_psr;			/* psr to restore */
 	int	sc_g1;			/* %g1 to restore */
 	int	sc_o0;			/* %o0 to restore */
+};
+#endif /* __LIBC12_SOURCE__ || (_KERNEL && COMPAT_13) */
+struct sigcontext {
+	int		sc_onstack;	/* sigstack state to restore */
+	int		__sc_mask13;	/* signal mask to restore (old style) */
+	/* begin machine dependent portion */
+	int		sc_sp;		/* %sp to restore */
+	int		sc_pc;		/* pc to restore */
+	int		sc_npc;		/* npc to restore */
+	int		sc_psr;		/* psr to restore */
+	int		sc_g1;		/* %g1 to restore */
+	int		sc_o0;		/* %o0 to restore */
+	sigset_t	sc_mask;	/* signal mask to restore (new style) */
 };
 #else /* _LOCORE */
 #define	SC_SP_OFFSET	8
