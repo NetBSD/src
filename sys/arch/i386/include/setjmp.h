028b4a01705f52a859de291d2678c705908653cb
thorpej <thorpej@NetBSD.org>
1998-09-14 21:31:52 +0000

    Need 13 longs for jmp_buf; signal mask is 128 bits now.
    XXX Actually, libc appears to use only 7 of the previous 10, so increasing
    the size isn't actually necessary!  But there was a gap at the end before,
    so we'll keep it.

--- sys/arch/i386/include/setjmp.h
+++ sys/arch/i386/include/setjmp.h
@@ -1,7 +1,7 @@
-/*	$NetBSD: setjmp.h,v 1.1 1994/12/20 10:36:43 cgd Exp $	*/
+/*	$NetBSD: setjmp.h,v 1.2 1998/09/14 21:31:52 thorpej Exp $	*/
 
 /*
  * machine/setjmp.h: machine dependent setjmp-related information.
  */
 
-#define	_JBLEN	10		/* size, in longs, of a jmp_buf */
+#define	_JBLEN	13		/* size, in longs, of a jmp_buf */
