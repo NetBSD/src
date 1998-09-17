bf2966b5b501eeafe03a1558fa12cc5b457c3f4c
drochner <drochner@NetBSD.org>
1998-09-17 19:59:30 +0000

    Add definition for highest legal LK201 keycode.

--- sys/dev/dec/wskbdmap_lk201.h
+++ sys/dev/dec/wskbdmap_lk201.h
@@ -1,5 +1,6 @@
-/* $NetBSD: wskbdmap_lk201.h,v 1.1 1998/09/17 18:29:54 drochner Exp $ */
+/* $NetBSD: wskbdmap_lk201.h,v 1.2 1998/09/17 19:59:30 drochner Exp $ */
 
 #define MIN_LK201_KEY            86
+#define MAX_LK201_KEY           251
 
 extern const struct wscons_keydesc zskbd_keydesctab[];
