46ca4772b977d81063d33b40df1280c2b22b0857
kleink <kleink@NetBSD.org>
1998-09-27 16:41:20 +0000

    Frob internal state keeping to make strftime() reentrant.

--- lib/libc/time/strftime.c
+++ lib/libc/time/strftime.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: strftime.c,v 1.4 1997/07/21 14:09:22 jtc Exp $	*/
+/*	$NetBSD: strftime.c,v 1.5 1998/09/27 16:41:20 kleink Exp $	*/
 
 /*
  * Copyright (c) 1989 The Regents of the University of California.
@@ -38,7 +38,7 @@
 #if 0
 static char *sccsid = "@(#)strftime.c	5.11 (Berkeley) 2/24/91";
 #else
-__RCSID("$NetBSD: strftime.c,v 1.4 1997/07/21 14:09:22 jtc Exp $");
+__RCSID("$NetBSD: strftime.c,v 1.5 1998/09/27 16:41:20 kleink Exp $");
 #endif
 #endif /* LIBC_SCCS and not lint */
 
@@ -49,13 +49,11 @@ __RCSID("$NetBSD: strftime.c,v 1.4 1997/07/21 14:09:22 jtc Exp $");
 #include <tzfile.h>
 #include <time.h>
 
-static size_t gsize;
-static char *pt;
-
-static	int _add __P((const char *));
-static	int _conv __P((int, int, char));
-static	int _secs __P((const struct tm *));
-static	size_t _fmt __P((const char *, const struct tm *));
+static	int _add __P((const char *, char **, const char *));
+static	int _conv __P((int, int, char, char **, const char *));
+static	int _secs __P((const struct tm *, char **, const char *));
+static	size_t _fmt __P((const char *, const struct tm *, char **,
+	    const char *));
 
 size_t
 strftime(s, maxsize, format, t)
@@ -64,26 +62,31 @@ strftime(s, maxsize, format, t)
 	const char *format;
 	const struct tm *t;
 {
+	char *pt;
+
 	tzset();
+	if (maxsize < 1)
+		return (0);
 
 	pt = s;
-	if ((gsize = maxsize) < 1)
-		return (0);
-	if (_fmt(format, t)) {
+	if (_fmt(format, t, &pt, s + maxsize)) {
 		*pt = '\0';
-		return (maxsize - gsize);
-	}
-	return (0);
+		return (pt - s);
+	} else
+		return (0);
 }
 
 #define SUN_WEEK(t)	(((t)->tm_yday + 7 - \
 				((t)->tm_wday)) / 7)
 #define MON_WEEK(t)	(((t)->tm_yday + 7 - \
 				((t)->tm_wday ? (t)->tm_wday - 1 : 6)) / 7)
+
 static size_t
-_fmt(format, t)
-	register const char *format;
+_fmt(format, t, pt, ptlim)
+	const char *format;
 	const struct tm *t;
+	char **pt;
+	const char * const ptlim;
 {
 	for (; *format; ++format) {
 		if (*format == '%') {
@@ -102,117 +105,126 @@ _fmt(format, t)
 			case 'A':
 				if (t->tm_wday < 0 || t->tm_wday > 6)
 					return (0);
-				if (!_add(_CurrentTimeLocale->day[t->tm_wday]))
+				if (!_add(_CurrentTimeLocale->day[t->tm_wday],
+				    pt, ptlim))
 					return (0);
 				continue;
+
 			case 'a':
 				if (t->tm_wday < 0 || t->tm_wday > 6)
 					return (0);
-				if (!_add(_CurrentTimeLocale->abday[t->tm_wday]))
+				if (!_add(_CurrentTimeLocale->abday[t->tm_wday],
+				    pt, ptlim))
 					return (0);
 				continue;
 			case 'B':
 				if (t->tm_mon < 0 || t->tm_mon > 11)
 					return (0);
-				if (!_add(_CurrentTimeLocale->mon[t->tm_mon]))
+				if (!_add(_CurrentTimeLocale->mon[t->tm_mon],
+				    pt, ptlim))
 					return (0);
 				continue;
 			case 'b':
 			case 'h':
 				if (t->tm_mon < 0 || t->tm_mon > 11)
 					return (0);
-				if (!_add(_CurrentTimeLocale->abmon[t->tm_mon]))
+				if (!_add(_CurrentTimeLocale->abmon[t->tm_mon],
+				    pt, ptlim))
 					return (0);
 				continue;
 			case 'C':
 				if (!_conv((t->tm_year + TM_YEAR_BASE) / 100,
-				    2, '0'))
+				    2, '0', pt, ptlim))
 					return (0);
 				continue;
 			case 'c':
-				if (!_fmt(_CurrentTimeLocale->d_t_fmt, t))
+				if (!_fmt(_CurrentTimeLocale->d_t_fmt, t, pt,
+				    ptlim))
 					return (0);
 				continue;
 			case 'D':
-				if (!_fmt("%m/%d/%y", t))
+				if (!_fmt("%m/%d/%y", t, pt, ptlim))
 					return (0);
 				continue;
 			case 'd':
-				if (!_conv(t->tm_mday, 2, '0'))
+				if (!_conv(t->tm_mday, 2, '0', pt, ptlim))
 					return (0);
 				continue;
 			case 'e':
-				if (!_conv(t->tm_mday, 2, ' '))
+				if (!_conv(t->tm_mday, 2, ' ', pt, ptlim))
 					return (0);
 				continue;
 			case 'H':
-				if (!_conv(t->tm_hour, 2, '0'))
+				if (!_conv(t->tm_hour, 2, '0', pt, ptlim))
 					return (0);
 				continue;
 			case 'I':
 				if (!_conv(t->tm_hour % 12 ?
-				    t->tm_hour % 12 : 12, 2, '0'))
+				    t->tm_hour % 12 : 12, 2, '0', pt, ptlim))
 					return (0);
 				continue;
 			case 'j':
-				if (!_conv(t->tm_yday + 1, 3, '0'))
+				if (!_conv(t->tm_yday + 1, 3, '0', pt, ptlim))
 					return (0);
 				continue;
 			case 'k':
-				if (!_conv(t->tm_hour, 2, ' '))
+				if (!_conv(t->tm_hour, 2, ' ', pt, ptlim))
 					return (0);
 				continue;
 			case 'l':
 				if (!_conv(t->tm_hour % 12 ?
-				    t->tm_hour % 12: 12, 2, ' '))
+				    t->tm_hour % 12: 12, 2, ' ', pt, ptlim))
 					return (0);
 				continue;
 			case 'M':
-				if (!_conv(t->tm_min, 2, '0'))
+				if (!_conv(t->tm_min, 2, '0', pt, ptlim))
 					return (0);
 				continue;
 			case 'm':
-				if (!_conv(t->tm_mon + 1, 2, '0'))
+				if (!_conv(t->tm_mon + 1, 2, '0', pt, ptlim))
 					return (0);
 				continue;
 			case 'n':
-				if (!_add("\n"))
+				if (!_add("\n", pt, ptlim))
 					return (0);
 				continue;
 			case 'p':
-				if (!_add(_CurrentTimeLocale->am_pm[t->tm_hour >= 12]))
+				if (!_add(_CurrentTimeLocale->am_pm[t->tm_hour
+				    >= 12], pt, ptlim))
 					return (0);
 				continue;
 			case 'R':
-				if (!_fmt("%H:%M", t))
+				if (!_fmt("%H:%M", t, pt, ptlim))
 					return (0);
 				continue;
 			case 'r':
-				if (!_fmt(_CurrentTimeLocale->t_fmt_ampm, t))
+				if (!_fmt(_CurrentTimeLocale->t_fmt_ampm, t, pt,
+				    ptlim))
 					return (0);
 				continue;
 			case 'S':
-				if (!_conv(t->tm_sec, 2, '0'))
+				if (!_conv(t->tm_sec, 2, '0', pt, ptlim))
 					return (0);
 				continue;
 			case 's':
-				if (!_secs(t))
+				if (!_secs(t, pt, ptlim))
 					return (0);
 				continue;
 			case 'T':
-				if (!_fmt("%H:%M:%S", t))
+				if (!_fmt("%H:%M:%S", t, pt, ptlim))
 					return (0);
 				continue;
 			case 't':
-				if (!_add("\t"))
+				if (!_add("\t", pt, ptlim))
 					return (0);
 				continue;
 			case 'U':
-				if (!_conv(SUN_WEEK(t), 2, '0'))
+				if (!_conv(SUN_WEEK(t), 2, '0', pt, ptlim))
 					return (0);
 				continue;
 			case 'u':
-				if (!_conv(t->tm_wday ? t->tm_wday : 7, 1, '0'))
+				if (!_conv(t->tm_wday ? t->tm_wday : 7, 1, '0',
+				    pt, ptlim))
 					return (0);
 				continue;
 			case 'V':
@@ -237,38 +249,42 @@ _fmt(format, t)
 					week = 53;
 				}
 
-				if (!_conv(week, 2, '0'))
+				if (!_conv(week, 2, '0', pt, ptlim))
 					return (0);
 				continue;
 				}
 			case 'W':
-				if (!_conv(MON_WEEK(t), 2, '0'))
+				if (!_conv(MON_WEEK(t), 2, '0', pt, ptlim))
 					return (0);
 				continue;
 			case 'w':
-				if (!_conv(t->tm_wday, 1, '0'))
+				if (!_conv(t->tm_wday, 1, '0', pt, ptlim))
 					return (0);
 				continue;
 			case 'x':
-				if (!_fmt(_CurrentTimeLocale->d_fmt, t))
+				if (!_fmt(_CurrentTimeLocale->d_fmt, t, pt,
+				    ptlim))
 					return (0);
 				continue;
 			case 'X':
-				if (!_fmt(_CurrentTimeLocale->t_fmt, t))
+				if (!_fmt(_CurrentTimeLocale->t_fmt, t, pt,
+				    ptlim))
 					return (0);
 				continue;
 			case 'y':
 				if (!_conv((t->tm_year + TM_YEAR_BASE) % 100,
-				    2, '0'))
+				    2, '0', pt, ptlim))
 					return (0);
 				continue;
 			case 'Y':
-				if (!_conv((t->tm_year + TM_YEAR_BASE), 4, '0'))
+				if (!_conv((t->tm_year + TM_YEAR_BASE), 4, '0',
+				    pt, ptlim))
 					return (0);
 				continue;
 			case 'Z':
 				if (tzname[t->tm_isdst ? 1 : 0] &&
-				    !_add(tzname[t->tm_isdst ? 1 : 0]))
+				    !_add(tzname[t->tm_isdst ? 1 : 0], pt,
+				    ptlim))
 					return (0);
 				continue;
 			case '%':
@@ -281,20 +297,22 @@ _fmt(format, t)
 				break;
 			}
 		}
-		if (!gsize--)
+		if (*pt == ptlim)
 			return (0);
-		*pt++ = *format;
+		*(*pt)++ = *format;
 	}
-	return (gsize);
+	return (ptlim - *pt);
 }
 
 static int
-_secs(t)
+_secs(t, pt, ptlim)
 	const struct tm *t;
+	char **pt;
+	const char * const ptlim;
 {
-	static char buf[15];
-	register time_t s;
-	register char *p;
+	char buf[15];
+	time_t s;
+	char *p;
 	struct tm tmp;
 
 	/* Make a copy, mktime(3) modifies the tm struct. */
@@ -302,32 +320,38 @@ _secs(t)
 	s = mktime(&tmp);
 	for (p = buf + sizeof(buf) - 2; s > 0 && p > buf; s /= 10)
 		*p-- = s % 10 + '0';
-	return (_add(++p));
+	return (_add(++p, pt, ptlim));
 }
 
 static int
-_conv(n, digits, pad)
+_conv(n, digits, pad, pt, ptlim)
 	int n, digits;
 	char pad;
+	char **pt;
+	const char * const ptlim;
 {
-	static char buf[10];
-	register char *p;
+	char buf[10];
+	char *p;
 
+	buf[sizeof (buf) - 1] = '\0';
 	for (p = buf + sizeof(buf) - 2; n > 0 && p > buf; n /= 10, --digits)
 		*p-- = n % 10 + '0';
 	while (p > buf && digits-- > 0)
 		*p-- = pad;
-	return (_add(++p));
+	return (_add(++p, pt, ptlim));
 }
 
 static int
-_add(str)
-	register const char *str;
+_add(str, pt, ptlim)
+	const char *str;
+	char **pt;
+	const char * const ptlim;
 {
-	for (;; ++pt, --gsize) {
-		if (!gsize)
+
+	for (;; ++(*pt)) {
+		if (*pt == ptlim)
 			return (0);
-		if (!(*pt = *str++))
+		if ((**pt = *str++) == '\0')
 			return (1);
 	}
 }
