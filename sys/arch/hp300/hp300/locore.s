2f4f990848985b859d0413567ee23a42c2d2777e
thorpej <thorpej>
1998-09-30 23:47:33 +0000

    Garbage collect trap #1 and trap #2 handlers; they're moved to trap_subr.s

--- sys/arch/hp300/hp300/locore.s
+++ sys/arch/hp300/hp300/locore.s
@@ -1,4 +1,4 @@
-/*	$NetBSD: locore.s,v 1.89 1998/09/30 23:01:29 thorpej Exp $	*/
+/*	$NetBSD: locore.s,v 1.90 1998/09/30 23:47:34 thorpej Exp $	*/
 
 /*
  * Copyright (c) 1994, 1995 Gordon W. Ross
@@ -834,18 +834,6 @@ Ltrap1:
 	moveml	sp@+,#0x7FFF		| restore most registers
 	addql	#8,sp			| pop SP and stack adjust
 	rte
-
-/*
- * Trap 1 - sigreturn
- */
-ENTRY_NOPROFILE(trap1)
-	jra	_ASM_LABEL(sigreturn)
-
-/*
- * Trap 2 - trace trap
- */
-ENTRY_NOPROFILE(trap2)
-	jra	_C_LABEL(trace)
 
 /*
  * Trap 12 is the entry point for the cachectl "syscall" (both HPUX & BSD)
