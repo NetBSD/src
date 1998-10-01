22edc1a3bca1c01c9438c257fc8d505307df7918
erh <erh@NetBSD.org>
1998-10-01 03:48:31 +0000

    Move IPC headers to common.  The msg, sem and shm files, in addition to other common structures and to avoid creating several more machine dependent header files, include the syscall arguments for several system calls which don't exist on all architechtures.

--- sys/compat/linux/common/linux_ipc.h
+++ sys/compat/linux/common/linux_ipc.h
@@ -1,4 +1,40 @@
-/*	$NetBSD: linux_ipc.h,v 1.1 1995/02/28 23:25:47 fvdl Exp $	*/
+/*	$NetBSD: linux_ipc.h,v 1.2 1998/10/01 03:48:32 erh Exp $	*/
+
+/*-
+ * Copyright (c) 1998 The NetBSD Foundation, Inc.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to The NetBSD Foundation
+ * by Eric Haszlakiewicz.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed by the NetBSD
+ *	Foundation, Inc. and its contributors.
+ * 4. Neither the name of The NetBSD Foundation nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 
 /*
  * Copyright (c) 1995 Frank van der Linden
@@ -31,8 +67,10 @@
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifndef _LINUX_IPC_H
-#define _LINUX_IPC_H
+#ifndef _COMMON_LINUX_IPC_H
+#define _COMMON_LINUX_IPC_H
+
+#include <sys/ipc.h>
 
 /*
  * Structs and values to handle the SYSV ipc/shm/msg calls implemented
@@ -61,4 +99,15 @@ struct linux_ipc_perm {
 #define LINUX_IPC_STAT	2
 #define LINUX_IPC_INFO	3
 
-#endif /* _LINUX_IPC_H */
+#if defined (SYSVSEM) || defined(SYSVSHM) || defined(SYSVMSG)
+#ifdef _KERNEL
+__BEGIN_DECLS
+void linux_to_bsd_ipc_perm __P((struct linux_ipc_perm *,
+				       struct ipc_perm *));
+void bsd_to_linux_ipc_perm __P((struct ipc_perm *,
+				       struct linux_ipc_perm *));
+__END_DECLS
+#endif	/* !_KERNEL */
+#endif	/* !SYSVSEM, !SYSVSHM, !SYSVMSG */
+
+#endif /* !_COMMON_LINUX_IPC_H */
