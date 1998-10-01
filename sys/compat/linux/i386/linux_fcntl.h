c3af96efd4c39e78b936d7f5dc66053d510ce89f
erh <erh@NetBSD.org>
1998-10-01 01:16:42 +0000

    Split linux_fcntl.h into i386 and common parts.

--- sys/compat/linux/i386/linux_fcntl.h
+++ sys/compat/linux/i386/linux_fcntl.h
@@ -1,4 +1,40 @@
-/*	$NetBSD: linux_fcntl.h,v 1.1 1995/02/28 23:25:40 fvdl Exp $	*/
+/*	$NetBSD: linux_fcntl.h,v 1.2 1998/10/01 01:16:43 erh Exp $	*/
+
+/*-
+ * Copyright (c) 1998 The NetBSD Foundation, Inc.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to The NetBSD Foundation
+ * by Eric Haszlakiewicz.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed by the NetBSD
+ *	Foundation, Inc. and its contributors.
+ * 4. Neither the name of The NetBSD Foundation nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 
 /*
  * Copyright (c) 1995 Frank van der Linden
@@ -35,14 +71,10 @@
  * Various flag values used in Linux for open(2) and fcntl(2).
  */
 
-#ifndef _LINUX_FCNTL_H
-#define _LINUX_FCNTL_H
+#ifndef _I386_LINUX_FCNTL_H
+#define _I386_LINUX_FCNTL_H
 
-/* read/write mode for open(2) (as usual) */
-#define LINUX_O_RDONLY		0x0000
-#define LINUX_O_WRONLY		0x0001
-#define LINUX_O_RDWR		0x0002
-#define LINUX_O_ACCMODE		0x0003
+/* read/write mode for open(2) defined in common/linux_fcntl.h */
 
 /* flags used in open(2) */
 #define LINUX_O_CREAT		0x0040
@@ -74,17 +106,4 @@
 #define LINUX_LOCK_EX		4
 #define LINUX_LOCK_SH		8
 
-/*
- * The arguments in the flock structure have a different order from the
- * BSD structure.
- */
-
-struct linux_flock {
-	short       l_type;
-	short       l_whence;
-	linux_off_t l_start;
-	linux_off_t l_len;
-	linux_pid_t l_pid;
-};
-
-#endif /* _LINUX_FCNTL_H */
+#endif /* !_I386_LINUX_FCNTL_H */
