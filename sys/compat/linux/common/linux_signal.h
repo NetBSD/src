e64d77d9891115dcb6704ad1e67165f152e95c7e
erh <erh@NetBSD.org>
1998-10-01 00:57:29 +0000

    Split compat/linux/linux_signal.h into i386 and common parts.

--- sys/compat/linux/common/linux_signal.h
+++ sys/compat/linux/common/linux_signal.h
@@ -1,6 +1,7 @@
-/* 	$NetBSD: linux_signal.h,v 1.5 1998/09/11 12:50:09 mycroft Exp $	*/
+/* 	$NetBSD: linux_signal.h,v 1.6 1998/10/01 00:57:30 erh Exp $	*/
 
 /*
+ * Copyright (c) 1998 Eric Haszlakiewicz
  * Copyright (c) 1995 Frank van der Linden
  * All rights reserved.
  *
@@ -31,69 +32,40 @@
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifndef _LINUX_SIGNAL_H
-#define _LINUX_SIGNAL_H
+/* Note: Machine dependant portions of this file exist. */
 
-#define LINUX_SIGHUP	 1
-#define LINUX_SIGINT	 2
-#define LINUX_SIGQUIT	 3
-#define LINUX_SIGILL	 4
-#define LINUX_SIGTRAP	 5
-#define LINUX_SIGABRT	 6
-#define LINUX_SIGIOT	 6
-#define LINUX_SIGBUS	 7
-#define LINUX_SIGFPE	 8
-#define LINUX_SIGKILL	 9
-#define LINUX_SIGUSR1	10
-#define LINUX_SIGSEGV	11
-#define LINUX_SIGUSR2	12
-#define LINUX_SIGPIPE	13
-#define LINUX_SIGALRM	14
-#define LINUX_SIGTERM	15
-#define LINUX_SIGSTKFLT	16
-#define LINUX_SIGCHLD	17
-#define LINUX_SIGCONT	18
-#define LINUX_SIGSTOP	19
-#define LINUX_SIGTSTP	20
-#define LINUX_SIGTTIN	21
-#define LINUX_SIGTTOU	22
-#define LINUX_SIGURG	23
-#define LINUX_SIGXCPU	24
-#define LINUX_SIGXFSZ	25
-#define LINUX_SIGVTALRM	26
-#define LINUX_SIGPROF	27
-#define LINUX_SIGWINCH	28
-#define LINUX_SIGIO	29
-#define LINUX_SIGPWR	30
-#define LINUX_SIGUNUSED	31
-#define LINUX_NSIG	32
+#ifndef _COMMON_LINUX_SIGNAL_H
+#define _COMMON_LINUX_SIGNAL_H
 
-#define LINUX_SIG_BLOCK		0
-#define LINUX_SIG_UNBLOCK	1
-#define LINUX_SIG_SETMASK	2
-
-typedef u_long	linux_sigset_t;
-typedef void	(*linux_handler_t) __P((int));
-
-struct linux_sigaction {
-	linux_handler_t	sa_handler;
-	linux_sigset_t	sa_mask;
-	u_long		sa_flags;
-	void		(*sa_restorer) __P((void));
-};
-
-/* sa_flags */
-#define LINUX_SA_NOCLDSTOP	0x00000001
-#define LINUX_SA_ONSTACK	0x08000000
-#define LINUX_SA_RESTART	0x10000000
-#define LINUX_SA_INTERRUPT	0x20000000
-#define LINUX_SA_NOMASK		0x40000000
-#define LINUX_SA_ONESHOT	0x80000000
-#define	LINUX_SA_ALLBITS	0xf8000001
-
+#ifdef _KERNEL
 extern int native_to_linux_sig[];
 extern int linux_to_native_sig[];
+__BEGIN_DECLS
+void linux_old_to_native_sigset __P((const linux_old_sigset_t *, sigset_t *));
+void native_to_linux_old_sigset __P((const sigset_t *, linux_old_sigset_t *));
+
+#if 0
+/* XXX Need these if sizeof(linux_old_sigset_t) != sizeof(linux_sigset_t) */
 void linux_to_native_sigset __P((const linux_sigset_t *, sigset_t *));
 void native_to_linux_sigset __P((const sigset_t *, linux_sigset_t *));
+#endif
 
-#endif /* !_LINUX_SIGNAL_H */
+#define linux_to_native_sigset(x,y) \
+			linux_old_to_native_sigset((linux_old_sigset_t *)x, y)
+#define native_to_linux_sigset(x,y) \
+			native_to_linux_old_sigset(x, (linux_old_sigset_t *)y)
+
+void linux_old_to_native_sigaction __P((struct linux_old_sigaction *, \
+							struct sigaction *));
+void native_to_linux_old_sigaction __P((struct sigaction *, \
+						struct linux_old_sigaction *));
+
+void linux_to_native_sigaction __P((struct linux_sigaction *, \
+							struct sigaction *));
+void native_to_linux_sigaction __P((struct sigaction *, \
+						struct linux_sigaction *));
+
+__END_DECLS
+#endif /* !_KERNEL */
+
+#endif /* !_COMMON_LINUX_SIGNAL_H */
