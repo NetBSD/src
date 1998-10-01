4ab27f44f9c195e2124c592a503518b2a1d988d9
erh <erh@NetBSD.org>
1998-10-01 03:29:45 +0000

    Move linux/compat/linux_ipccall.h to multiarch.  Defines for the IPC multiplexerand associated functions.

--- sys/compat/linux/common/linux_ipccall.h
+++ sys/compat/linux/common/linux_ipccall.h
@@ -1,4 +1,40 @@
-/*	$NetBSD: linux_ipccall.h,v 1.2 1995/08/15 21:14:33 fvdl Exp $	*/
+/*	$NetBSD: linux_ipccall.h,v 1.3 1998/10/01 03:29:46 erh Exp $	*/
+
+/*-
+ * Copyright (c) 1998 The NetBSD Foundation, Inc.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to The NetBSD Foundation
+ * by Eric Haszlakiewicz.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed by the NetBSD
+ *	Foundation, Inc. and its contributors.
+ * 4. Neither the name of The NetBSD Foundation nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 
 /*
  * Copyright (c) 1995 Frank van der Linden
@@ -31,8 +67,11 @@
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifndef _LINUX_IPC_CALL_H
-#define _LINUX_IPC_CALL_H
+#ifndef _MULTIARCH_LINUX_IPCCALL_H
+#define _MULTIARCH_LINUX_IPCCALL_H
+
+/* Used on: arm, i386, m68k, mips, ppc, sparc, sparc64 */
+/* Not used on: alpha */
 
 /*
  * Defines for the numbers passes as the first argument to the
@@ -51,4 +90,30 @@
 #define LINUX_SYS_shmget	23
 #define LINUX_SYS_shmctl	24
 
-#endif /* _LINUX_IPC_CALL_H */
+
+#ifdef SYSVSEM
+inline int linux_semop __P((struct proc *, struct linux_sys_ipc_args *,
+				register_t *));
+inline int linux_semget __P((struct proc *, struct linux_sys_ipc_args *,
+				register_t *));
+#endif
+
+
+#ifdef SYSVMSG
+inline int linux_msgsnd __P((struct proc *, struct linux_sys_ipc_args *,
+				register_t *));
+inline int linux_msgrcv __P((struct proc *, struct linux_sys_ipc_args *,
+				register_t *));
+inline int linux_msgget __P((struct proc *, struct linux_sys_ipc_args *,
+				register_t *));
+#endif
+
+
+#ifdef SYSVSHM
+inline int linux_shmdt __P((struct proc *, struct linux_sys_ipc_args *,
+				register_t *));
+inline int linux_shmget __P((struct proc *, struct linux_sys_ipc_args *,
+				register_t *));
+#endif
+
+#endif /* !_MULTIARCH_LINUX_IPCCALL_H */
