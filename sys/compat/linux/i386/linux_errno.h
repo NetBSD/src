57c0abf6ac575d246b9f094e825fb52f4ad0acc5
erh <erh@NetBSD.org>
1998-10-01 01:03:37 +0000

    Split compat/linux/linux_errno.h into i386 and common parts.

--- sys/compat/linux/i386/linux_errno.h
+++ sys/compat/linux/i386/linux_errno.h
@@ -1,4 +1,41 @@
-/* 	$NetBSD: linux_errno.h,v 1.1 1995/02/28 23:25:34 fvdl Exp $	*/
+/* 	$NetBSD: linux_errno.h,v 1.2 1998/10/01 01:03:38 erh Exp $	*/
+
+/*-
+ * Copyright (c) 1998 The NetBSD Foundation, Inc.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to The NetBSD Foundation
+ * by Eric Haszlakiewicz.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed by the NetBSD
+ *	Foundation, Inc. and its contributors.
+ * 4. Neither the name of The NetBSD Foundation nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
 /*
  * Copyright (c) 1995 Frank van der Linden
  * All rights reserved.
@@ -30,44 +67,11 @@
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifndef _LINUX_ERRNO_H
-#define _LINUX_ERRNO_H
+#ifndef _I386_LINUX_ERRNO_H
+#define _I386_LINUX_ERRNO_H
 
-#define LINUX_EPERM		 1
-#define LINUX_ENOENT		 2
-#define LINUX_ESRCH		 3
-#define LINUX_EINTR		 4
-#define LINUX_EIO		 5
-#define LINUX_ENXIO		 6
-#define LINUX_E2BIG		 7
-#define LINUX_ENOEXEC		 8
-#define LINUX_EBADF		 9
-#define LINUX_ECHILD		10
-#define LINUX_EAGAIN		11
-#define LINUX_ENOMEM		12
-#define LINUX_EACCES		13
-#define LINUX_EFAULT		14
-#define LINUX_ENOTBLK		15
-#define LINUX_EBUSY		16
-#define LINUX_EEXIST		17
-#define LINUX_EXDEV		18
-#define LINUX_ENODEV		19
-#define LINUX_ENOTDIR		20
-#define LINUX_EISDIR		21
-#define LINUX_EINVAL		22
-#define LINUX_ENFILE		23
-#define LINUX_EMFILE		24
-#define LINUX_ENOTTY		25
-#define LINUX_ETXTBSY		26
-#define LINUX_EFBIG		27
-#define LINUX_ENOSPC		28
-#define LINUX_ESPIPE		29
-#define LINUX_EROFS		30
-#define LINUX_EMLINK		31
-#define LINUX_EPIPE		32
-#define LINUX_EDOM		33
-#define LINUX_ERANGE		34
-#define LINUX_EDEADLK		35
+/* Use common/linux_errno.h for #1-35 */
+
 #define LINUX_ENAMETOOLONG	36
 #define LINUX_ENOLCK		37
 #define LINUX_ENOSYS		38
@@ -155,4 +159,4 @@
 #define LINUX_EREMOTEIO		121
 #define LINUX_EDQUOT		122
 
-#endif /* !_LINUX_ERRNO_H */
+#endif /* !_I386_LINUX_ERRNO_H */
