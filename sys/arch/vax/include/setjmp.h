8f92d99c201a9f7a00761ad46c25147abe7b15db
christos <christos>
1998-10-01 19:00:06 +0000

    Need more room for signal mask

--- sys/arch/vax/include/setjmp.h
+++ sys/arch/vax/include/setjmp.h
@@ -1,7 +1,7 @@
-/*	$NetBSD: setjmp.h,v 1.1 1994/12/20 10:37:18 cgd Exp $	*/
+/*	$NetBSD: setjmp.h,v 1.2 1998/10/01 19:00:06 christos Exp $	*/
 
 /*
  * machine/setjmp.h: machine dependent setjmp-related information.
  */
 
-#define	_JBLEN	10		/* size, in longs, of a jmp_buf */
+#define	_JBLEN	14		/* size, in longs, of a jmp_buf */
