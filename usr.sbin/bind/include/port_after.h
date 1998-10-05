2a98f0507ae0b36fd7f48b8217a0f6d6a8909588
tron <tron@NetBSD.org>
1998-10-05 18:01:57 +0000

    Import BIND 8.1.2.

---
+++ usr.sbin/bind/include/port_after.h
@@ -0,0 +1,41 @@
+/*	$NetBSD: port_after.h,v 1.1.1.1 1998/10/05 18:01:58 tron Exp $	*/
+
+#define CAN_RECONNECT
+#define USE_POSIX
+#define POSIX_SIGNALS
+#define USE_UTIME
+#define USE_WAITPID
+#define HAVE_GETRUSAGE
+#define HAVE_FCHMOD
+#define NEED_PSELECT
+#define SETGRENT_VOID
+#define SETPWENT_VOID
+#define HAVE_SA_LEN
+#define HAVE_MINIMUM_IFREQ
+#define USE_LOG_CONS
+#define RLIMIT_TYPE rlim_t
+#define RLIMIT_LONGLONG
+#define RLIMIT_FILE_INFINITY
+#define HAVE_CHROOT
+#define CAN_CHANGE_ID
+
+#define _TIMEZONE timezone
+
+#define PORT_NONBLOCK	O_NONBLOCK
+#define PORT_WOULDBLK	EWOULDBLOCK
+#define WAIT_T		int
+#define KSYMS		"/netbsd"
+#define KMEM		"/dev/kmem"
+#define UDPSUM		"udpcksum"
+
+/*
+ * We need to know the IPv6 address family number even on IPv4-only systems.
+ * Note that this is NOT a protocol constant, and that if the system has its
+ * own AF_INET6, different from ours below, all of BIND's libraries and
+ * executables will need to be recompiled after the system <sys/socket.h>
+ * has had this type added.  The type number below is correct on most BSD-
+ * derived systems for which AF_INET6 is defined.
+ */
+#ifndef AF_INET6
+#define AF_INET6	24
+#endif
