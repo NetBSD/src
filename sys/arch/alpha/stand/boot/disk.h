9c29657a672bcf051c172e6b4d6a763d45028eec
ross <ross@NetBSD.org>
1998-10-15 00:49:34 +0000

    Extern def for raw prom disk file descriptor.

--- sys/arch/alpha/stand/boot/disk.h
+++ sys/arch/alpha/stand/boot/disk.h
@@ -1,7 +1,9 @@
-/* $NetBSD: disk.h,v 1.2 1997/04/06 08:40:34 cgd Exp $ */
+/* $NetBSD: disk.h,v 1.3 1998/10/15 00:49:34 ross Exp $ */
 
 int	diskstrategy __P((void *, int, daddr_t, size_t, void *, size_t *));
 /* int     diskopen __P((struct open_file *, int, int, int)); */
 int     diskclose __P((struct open_file *));
 
 #define	diskioctl	noioctl
+
+int	booted_dev_fd;
