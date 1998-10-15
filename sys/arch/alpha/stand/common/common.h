ae6435890c8bbdc77d437a55443bdd2965e2f8c1
ross <ross>
1998-10-15 01:00:56 +0000

    Delete bogus prom_open() prototype.

--- sys/arch/alpha/stand/common/common.h
+++ sys/arch/alpha/stand/common/common.h
@@ -1,7 +1,6 @@
-/*	$NetBSD: common.h,v 1.2 1998/01/05 07:02:48 perry Exp $	*/
+/*	$NetBSD: common.h,v 1.3 1998/10/15 01:00:56 ross Exp $	*/
 
 void init_prom_calls __P((void));
-int prom_open __P((char*, int));
 void OSFpal __P((void));
 void halt __P((void));
 u_int64_t prom_dispatch __P((int, ...));
