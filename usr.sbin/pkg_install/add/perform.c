d903dbb5a956e9005e0ffc02be1f68c4b8ca0e84
agc <agc@NetBSD.org>
1998-10-26 17:39:20 +0000

    Modify pkg_create to recognise Build Information and Version files, when creating packages, and modify pkg_add to copy the files when a binary package is added. This preserves the fine-grained version information in a binary package, and also the options with which the binary package was built.

--- usr.sbin/pkg_install/add/perform.c
+++ usr.sbin/pkg_install/add/perform.c
@@ -1,11 +1,11 @@
-/*	$NetBSD: perform.c,v 1.26 1998/10/12 12:03:25 agc Exp $	*/
+/*	$NetBSD: perform.c,v 1.27 1998/10/26 17:39:20 agc Exp $	*/
 
 #include <sys/cdefs.h>
 #ifndef lint
 #if 0
 static const char *rcsid = "from FreeBSD Id: perform.c,v 1.44 1997/10/13 15:03:46 jkh Exp";
 #else
-__RCSID("$NetBSD: perform.c,v 1.26 1998/10/12 12:03:25 agc Exp $");
+__RCSID("$NetBSD: perform.c,v 1.27 1998/10/26 17:39:20 agc Exp $");
 #endif
 #endif
 
@@ -488,6 +488,8 @@ pkg_do(char *pkg)
 	fclose(cfile);
 	move_file(".", DESC_FNAME, LogDir);
 	move_file(".", COMMENT_FNAME, LogDir);
+	move_file(".", BUILD_VERSION_FNAME, LogDir);
+	move_file(".", BUILD_INFO_FNAME, LogDir);
 	if (fexists(DISPLAY_FNAME))
 	    move_file(".", DISPLAY_FNAME, LogDir);
 
