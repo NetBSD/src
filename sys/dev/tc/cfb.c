ac5fd4ae1d3cc67dad64a2d9a00d1980abff4f56
nisimura <nisimura@NetBSD.org>
1998-10-30 00:53:12 +0000

    - Assign copyright terms.

--- sys/dev/tc/cfb.c
+++ sys/dev/tc/cfb.c
@@ -1,8 +1,38 @@
-/* $NetBSD: cfb.c,v 1.1 1998/10/29 12:24:24 nisimura Exp $ */
+/* $NetBSD: cfb.c,v 1.2 1998/10/30 00:53:12 nisimura Exp $ */
+
+/*
+ * Copyright (c) 1996, 1998 Tohru Nishimura.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *      This product includes software developed by Tohru Nishimura
+ *	for the NetBSD Project.
+ * 4. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 #include <sys/cdefs.h>			/* RCS ID & Copyright macro defns */
 
-__KERNEL_RCSID(0, "$NetBSD: cfb.c,v 1.1 1998/10/29 12:24:24 nisimura Exp $");
+__KERNEL_RCSID(0, "$NetBSD: cfb.c,v 1.2 1998/10/30 00:53:12 nisimura Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -46,8 +76,6 @@ __KERNEL_RCSID(0, "$NetBSD: cfb.c,v 1.1 1998/10/29 12:24:24 nisimura Exp $");
 #define MACHINE_KSEG0_TO_PHYS(x) ALPHA_K0SEG_TO_PHYS(x)
 #endif
 
-/* XXX XXX XXX */
-
 /*
  * N.B., Bt459 registers are 8bit width.  Some of TC framebuffers have
  * weird register layout such as each of 2nd and 3rd Bt459 registers
@@ -70,6 +98,8 @@ struct bt459reg {
         u_int32_t       bt_reg;
         u_int32_t       bt_cmap;
 };
+
+/* XXX XXX XXX */
 
 struct fb_devconfig {
 	vaddr_t dc_vaddr;		/* memory space virtual base address */
