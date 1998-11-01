e65489558aeae129f21f64de6c8f7802e0f11f3c
lukem <lukem@NetBSD.org>
1998-11-01 03:46:28 +0000

    reimplement rev 1.30,1.31 using YPREFIX and LPREFIX instead

--- share/mk/bsd.sys.mk
+++ share/mk/bsd.sys.mk
@@ -1,4 +1,4 @@
-#	$NetBSD: bsd.sys.mk,v 1.32 1998/10/31 17:17:56 veego Exp $
+#	$NetBSD: bsd.sys.mk,v 1.33 1998/11/01 03:46:28 lukem Exp $
 #
 # Overrides used for NetBSD source tree builds.
 
@@ -46,35 +46,41 @@ STRIPPROG?=	strip
 	${HOST_COMPILE.c} -o ${.TARGET} ${.IMPSRC}
 
 
-.if defined(PARALLEL)
+.if defined(PARALLEL) || defined(LPREFIX)
+LPREFIX?=yy
+LFLAGS+=-P${LPREFIX}
 # Lex
 .l:
-	${LEX.l} -o${.TARGET:R}.yy.c ${.IMPSRC}
-	${LINK.c} -o ${.TARGET} ${.TARGET:R}.yy.c ${LDLIBS} -ll
-	rm -f ${.TARGET:R}.yy.c
+	${LEX.l} -o${.TARGET:R}.${LPREFIX}.c ${.IMPSRC}
+	${LINK.c} -o ${.TARGET} ${.TARGET:R}.${LPREFIX}.c ${LDLIBS} -ll
+	rm -f ${.TARGET:R}.${LPREFIX}.c
 .l.c:
 	${LEX.l} -o${.TARGET} ${.IMPSRC}
 .l.o:
-	${LEX.l} -o${.TARGET:R}.yy.c ${.IMPSRC}
-	${COMPILE.c} -o ${.TARGET} ${.TARGET:R}.yy.c 
-	rm -f ${.TARGET:R}.yy.c
+	${LEX.l} -o${.TARGET:R}.${LPREFIX}.c ${.IMPSRC}
+	${COMPILE.c} -o ${.TARGET} ${.TARGET:R}.${LPREFIX}.c 
+	rm -f ${.TARGET:R}.${LPREFIX}.c
 .l.lo:
-	${LEX.l} -o${.TARGET:R}.yy.c ${.IMPSRC}
-	${HOST_COMPILE.c} -o ${.TARGET} ${.TARGET:R}.yy.c 
-	rm -f ${.TARGET:R}.yy.c
+	${LEX.l} -o${.TARGET:R}.${LPREFIX}.c ${.IMPSRC}
+	${HOST_COMPILE.c} -o ${.TARGET} ${.TARGET:R}.${LPREFIX}.c 
+	rm -f ${.TARGET:R}.${LPREFIX}.c
 .endif
 
 # Yacc
+.if defined(YHEADER) || defined(YPREFIX)
+.if defined(YPREFIX)
+YFLAGS+=-p${YPREFIX}
+.endif
 .if defined(YHEADER)
 YFLAGS+=-d
+.endif
 .y:
 	${YACC.y} -b ${.TARGET:R} ${.IMPSRC}
 	${LINK.c} -o ${.TARGET} ${.TARGET:R}.tab.c ${LDLIBS}
 	rm -f ${.TARGET:R}.tab.c ${.TARGET:R}.tab.h
-.y.h .y.c:
-	${YACC.y} -b ${.TARGET:R} ${.IMPSRC}
-	mv ${.TARGET:R}.tab.c ${.TARGET:R}.c
-	mv ${.TARGET:R}.tab.h ${.TARGET:R}.h
+.y.h: ${.TARGET:R}.c
+.y.c:
+	${YACC.y} -o ${.TARGET} ${.IMPSRC}
 .y.o:
 	${YACC.y} -b ${.TARGET:R} ${.IMPSRC}
 	${COMPILE.c} -o ${.TARGET} ${.TARGET:R}.tab.c
@@ -89,8 +95,7 @@ YFLAGS+=-d
 	${LINK.c} -o ${.TARGET} ${.TARGET:R}.tab.c ${LDLIBS}
 	rm -f ${.TARGET:R}.tab.c
 .y.c:
-	${YACC.y} -b ${.TARGET:R} ${.IMPSRC}
-	mv ${.TARGET:R}.tab.c ${.TARGET}
+	${YACC.y} -o ${.TARGET} ${.IMPSRC}
 .y.o:
 	${YACC.y} -b ${.TARGET:R} ${.IMPSRC}
 	${COMPILE.c} -o ${.TARGET} ${.TARGET:R}.tab.c
