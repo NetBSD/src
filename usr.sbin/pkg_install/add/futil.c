9b7c1f7864351861082c332bcc8a7583d271d5ca
cgd <cgd>
1998-11-06 20:40:32 +0000

    pull up rev(s) 1.6 from trunk.  (agc)

--- usr.sbin/pkg_install/add/futil.c
+++ usr.sbin/pkg_install/add/futil.c
@@ -1,11 +1,11 @@
-/*	$NetBSD: futil.c,v 1.5.2.1 1998/08/29 03:31:20 mellon Exp $	*/
+/*	$NetBSD: futil.c,v 1.5.2.2 1998/11/06 20:40:32 cgd Exp $	*/
 
 #include <sys/cdefs.h>
 #ifndef lint
 #if 0
 static const char *rcsid = "from FreeBSD Id: futil.c,v 1.7 1997/10/08 07:45:39 charnier Exp";
 #else
-__RCSID("$NetBSD: futil.c,v 1.5.2.1 1998/08/29 03:31:20 mellon Exp $");
+__RCSID("$NetBSD: futil.c,v 1.5.2.2 1998/11/06 20:40:32 cgd Exp $");
 #endif
 #endif
 
