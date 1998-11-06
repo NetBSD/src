07ac7cac028cf2527b17bb0390bea620ad338b46
cgd <cgd@NetBSD.org>
1998-11-06 20:40:44 +0000

    pull up rev(s) 1.6-1.11 from trunk.  (agc)

--- usr.sbin/pkg_install/create/main.c
+++ usr.sbin/pkg_install/create/main.c
@@ -1,11 +1,11 @@
-/*	$NetBSD: main.c,v 1.5.2.1 1998/08/29 03:33:15 mellon Exp $	*/
+/*	$NetBSD: main.c,v 1.5.2.2 1998/11/06 20:40:44 cgd Exp $	*/
 
 #include <sys/cdefs.h>
 #ifndef lint
 #if 0
 static const char *rcsid = "from FreeBSD Id: main.c,v 1.17 1997/10/08 07:46:23 charnier Exp";
 #else
-__RCSID("$NetBSD: main.c,v 1.5.2.1 1998/08/29 03:33:15 mellon Exp $");
+__RCSID("$NetBSD: main.c,v 1.5.2.2 1998/11/06 20:40:44 cgd Exp $");
 #endif
 #endif
 
@@ -24,7 +24,7 @@ __RCSID("$NetBSD: main.c,v 1.5.2.1 1998/08/29 03:33:15 mellon Exp $");
 #include "lib.h"
 #include "create.h"
 
-static char Options[] = "YNOhvf:p:P:C:c:d:i:k:r:t:X:D:m:s:";
+static char Options[] = "ORhlvf:p:P:C:c:d:i:k:r:t:X:D:m:s:b:B:";
 
 char	*Prefix		= NULL;
 char	*Comment	= NULL;
@@ -39,11 +39,26 @@ char	*ExcludeFrom	= NULL;
 char	*Mtree		= NULL;
 char	*Pkgdeps	= NULL;
 char	*Pkgcfl		= NULL;
+char	*BuildVersion	= NULL;
+char	*BuildInfo	= NULL;
 char	PlayPen[FILENAME_MAX];
+size_t	PlayPenSize	= sizeof(PlayPen);
 int	Dereference	= 0;
 int	PlistOnly	= 0;
+int	RelativeLinks	= 0;
+int	ReorderDirs	= 0;
 
-static void usage __P((void));
+static void
+usage(void)
+{
+    fprintf(stderr, "%s\n%s\n%s\n%s\n%s\n",
+"usage: pkg_create [-ORhlv] [-P dpkgs] [-C cpkgs] [-p prefix] [-f contents]",
+"                  [-i iscript] [-k dscript] [-r rscript] [-t template]",
+"                  [-X excludefile] [-D displayfile] [-m mtreefile]",
+"                  [-b build-version-file] [-B build-info-file]",
+"                  -c comment -d description -f packlist pkg-name");
+    exit(1);
+}
 
 int
 main(int argc, char **argv)
@@ -58,16 +73,12 @@ main(int argc, char **argv)
 	    Verbose = TRUE;
 	    break;
 
-	case 'N':
-	    AutoAnswer = NO;
-	    break;
-
-	case 'Y':
-	    AutoAnswer = YES;
-	    break;
-
 	case 'O':
-	    PlistOnly = YES;
+	    PlistOnly = 1;
+	    break;
+
+	case 'R':
+	    ReorderDirs = 1;
 	    break;
 
 	case 'p':
@@ -97,6 +108,10 @@ main(int argc, char **argv)
 	case 'k':
 	    DeInstall = optarg;
 	    break;
+
+	case 'l':
+		RelativeLinks = 1;
+		break;
 
 	case 'r':
 	    Require = optarg;
@@ -130,6 +145,14 @@ main(int argc, char **argv)
 		Pkgcfl = optarg;
 		break;
 
+	case 'b':
+		BuildVersion = optarg;
+		break;
+
+	case 'B':
+		BuildInfo = optarg;
+		break;
+
 	case '?':
 	default:
 	    usage();
@@ -157,15 +180,4 @@ main(int argc, char **argv)
     }
     else
 	return 0;
-}
-
-static void
-usage()
-{
-    fprintf(stderr, "%s\n%s\n%s\n%s\n",
-"usage: pkg_create [-YNOhv] [-P dpkgs] [-C cpkgs] [-p prefix] [-f contents]",
-"                  [-i iscript] [-k dscript] [-r rscript] [-t template]",
-"                  [-X excludefile] [-D displayfile] [-m mtreefile]",
-"                  -c comment -d description -f packlist pkg-name");
-    exit(1);
 }
