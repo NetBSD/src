e13439505dd19aa7a0d53eb005bc88631e31c58c
cgd <cgd>
1998-11-06 20:40:47 +0000

    pull up rev(s) 1.7-1.14 from trunk.  (agc)

--- usr.sbin/pkg_install/create/perform.c
+++ usr.sbin/pkg_install/create/perform.c
@@ -1,11 +1,11 @@
-/*	$NetBSD: perform.c,v 1.6.2.1 1998/08/29 03:34:26 mellon Exp $	*/
+/*	$NetBSD: perform.c,v 1.6.2.2 1998/11/06 20:40:47 cgd Exp $	*/
 
 #include <sys/cdefs.h>
 #ifndef lint
 #if 0
 static const char *rcsid = "from FreeBSD Id: perform.c,v 1.38 1997/10/13 15:03:51 jkh Exp";
 #else
-__RCSID("$NetBSD: perform.c,v 1.6.2.1 1998/08/29 03:34:26 mellon Exp $");
+__RCSID("$NetBSD: perform.c,v 1.6.2.2 1998/11/06 20:40:47 cgd Exp $");
 #endif
 #endif
 
@@ -38,10 +38,153 @@ __RCSID("$NetBSD: perform.c,v 1.6.2.1 1998/08/29 03:34:26 mellon Exp $");
 #include <sys/wait.h>
 #include <unistd.h>
 
-static void sanity_check(void);
-static void make_dist(char *, char *, char *, Package *);
-
 static char *home;
+
+static void
+make_dist(char *home, char *pkg, char *suffix, package_t *plist)
+{
+    char tball[FILENAME_MAX];
+    plist_t *p;
+    int ret;
+    char *args[50];	/* Much more than enough. */
+    int nargs = 0;
+    int pipefds[2];
+    FILE *totar;
+    pid_t pid;
+
+    args[nargs++] = "tar";	/* argv[0] */
+
+    if (*pkg == '/')
+	snprintf(tball, FILENAME_MAX, "%s.%s", pkg, suffix);
+    else
+	snprintf(tball, FILENAME_MAX, "%s/%s.%s", home, pkg, suffix);
+
+    args[nargs++] = "-c";
+    args[nargs++] = "-f";
+    args[nargs++] = tball;
+    if (strchr(suffix, 'z'))	/* Compress/gzip? */
+	args[nargs++] = "-z";
+    if (Dereference)
+	args[nargs++] = "-h";
+    if (ExcludeFrom) {
+	args[nargs++] = "-X";
+	args[nargs++] = ExcludeFrom;
+    }
+    args[nargs++] = "-T";	/* Take filenames from file instead of args. */
+    args[nargs++] = "-";	/* Use stdin for the file. */
+    args[nargs] = NULL;
+
+    if (Verbose)
+	printf("Creating gzip'd tar ball in '%s'\n", tball);
+
+    /* Set up a pipe for passing the filenames, and fork off a tar process. */
+    if (pipe(pipefds) == -1) {
+	cleanup(0);
+	errx(2, "cannot create pipe");
+    }
+    if ((pid = fork()) == -1) {
+	cleanup(0);
+	errx(2, "cannot fork process for tar");
+    }
+    if (pid == 0) {	/* The child */
+	dup2(pipefds[0], 0);
+	close(pipefds[0]);
+	close(pipefds[1]);
+	execv("/usr/bin/tar", args);
+	cleanup(0);
+	errx(2, "failed to execute tar command");
+    }
+
+    /* Meanwhile, back in the parent process ... */
+    close(pipefds[0]);
+    if ((totar = fdopen(pipefds[1], "w")) == NULL) {
+	cleanup(0);
+	errx(2, "fdopen failed");
+    }
+
+    fprintf(totar, "%s\n", CONTENTS_FNAME);
+    fprintf(totar, "%s\n", COMMENT_FNAME);
+    fprintf(totar, "%s\n", DESC_FNAME);
+
+    if (Install) {
+	fprintf(totar, "%s\n", INSTALL_FNAME);
+    }
+    if (DeInstall) {
+	fprintf(totar, "%s\n", DEINSTALL_FNAME);
+    }
+    if (Require) {
+	fprintf(totar, "%s\n", REQUIRE_FNAME);
+    }
+    if (Display) {
+	fprintf(totar, "%s\n", DISPLAY_FNAME);
+    }
+    if (Mtree) {
+	fprintf(totar, "%s\n", MTREE_FNAME);
+    }
+	if (BuildVersion) {
+		(void) fprintf(totar, "%s\n", BUILD_VERSION_FNAME);
+	}
+	if (BuildInfo) {
+		(void) fprintf(totar, "%s\n", BUILD_INFO_FNAME);
+	}
+
+    for (p = plist->head; p; p = p->next) {
+	if (p->type == PLIST_FILE)
+	    fprintf(totar, "%s\n", p->name);
+	else if (p->type == PLIST_CWD || p->type == PLIST_SRC)
+	    fprintf(totar, "-C\n%s\n", p->name);
+	else if (p->type == PLIST_IGNORE)
+	     p = p->next;
+    }
+
+    fclose(totar);
+    wait(&ret);
+    /* assume either signal or bad exit is enough for us */
+    if (ret) {
+	cleanup(0);
+	errx(2, "tar command failed with code %d", ret);
+    }
+}
+
+static void
+sanity_check(void)
+{
+    if (!Comment) {
+	cleanup(0);
+	errx(2, "required package comment string is missing (-c comment)");
+    }
+    if (!Desc) {
+	cleanup(0);
+	errx(2, "required package description string is missing (-d desc)");
+    }
+    if (!Contents) {
+	cleanup(0);
+	errx(2, "required package contents list is missing (-f [-]file)");
+    }
+}
+
+
+/* Clean up those things that would otherwise hang around */
+void
+cleanup(int sig)
+{
+    static int	alreadyCleaning;
+    void (*oldint)(int);
+    void (*oldhup)(int);
+    oldint = signal(SIGINT, SIG_IGN);
+    oldhup = signal(SIGHUP, SIG_IGN);
+
+    if (!alreadyCleaning) {
+    	alreadyCleaning = 1;
+	if (sig)
+	    printf("Signal %d received, cleaning up.\n", sig);
+	leave_playpen(home);
+	if (sig)
+	    exit(1);
+    }
+    signal(SIGINT, oldint);
+    signal(SIGHUP, oldhup);
+}
 
 int
 pkg_perform(char **pkgs)
@@ -49,7 +192,7 @@ pkg_perform(char **pkgs)
     char *pkg = *pkgs;		/* Only one arg to create */
     char *cp;
     FILE *pkg_in, *fp;
-    Package plist;
+    package_t plist;
     char *suffix;  /* What we tack on to the end of the finished package */
 
     /* Preliminary setup */
@@ -62,9 +205,10 @@ pkg_perform(char **pkgs)
 	pkg_in = stdin;
     else {
 	pkg_in = fopen(Contents, "r");
-	if (!pkg_in)
-	    cleanup(0), errx(2, "unable to open contents file '%s' for input",
-				Contents);
+	if (!pkg_in) {
+	    cleanup(0);
+	    errx(2, "unable to open contents file '%s' for input", Contents);
+	}
     }
     plist.head = plist.tail = NULL;
 
@@ -127,8 +271,9 @@ pkg_perform(char **pkgs)
      * Run down the list and see if we've named it, if not stick in a name
      * at the top.
      */
-    if (find_plist(&plist, PLIST_NAME) == NULL)
+    if (find_plist(&plist, PLIST_NAME) == NULL) {
 	add_plist_top(&plist, PLIST_NAME, basename_of(pkg));
+    }
 
     /*
      * We're just here for to dump out a revised plist for the FreeBSD ports
@@ -141,7 +286,7 @@ pkg_perform(char **pkgs)
     }
 
     /* Make a directory to stomp around in */
-    home = make_playpen(PlayPen, 0);
+    home = make_playpen(PlayPen, PlayPenSize, 0);
     signal(SIGINT, cleanup);
     signal(SIGHUP, cleanup);
 
@@ -188,14 +333,28 @@ pkg_perform(char **pkgs)
 	add_plist(&plist, PLIST_FILE, MTREE_FNAME);
 	add_plist(&plist, PLIST_MTREE, MTREE_FNAME);
     }
+    if (BuildVersion) {
+	copy_file(home, BuildVersion, BUILD_VERSION_FNAME);
+	add_plist(&plist, PLIST_IGNORE, NULL);
+	add_plist(&plist, PLIST_FILE, BUILD_VERSION_FNAME);
+    }
+    if (BuildInfo) {
+	copy_file(home, BuildInfo, BUILD_INFO_FNAME);
+	add_plist(&plist, PLIST_IGNORE, NULL);
+	add_plist(&plist, PLIST_FILE, BUILD_INFO_FNAME);
+    }
 
     /* Finally, write out the packing list */
     fp = fopen(CONTENTS_FNAME, "w");
-    if (!fp)
-	cleanup(0), errx(2, "can't open file %s for writing", CONTENTS_FNAME);
+    if (!fp) {
+	cleanup(0);
+	errx(2, "can't open file %s for writing", CONTENTS_FNAME);
+    }
     write_plist(&plist, fp);
-    if (fclose(fp))
-	cleanup(0), errx(2, "error while closing %s", CONTENTS_FNAME);
+    if (fclose(fp)) {
+	cleanup(0);
+	errx(2, "error while closing %s", CONTENTS_FNAME);
+    }
 
     /* And stick it into a tar ball */
     make_dist(home, pkg, suffix, &plist);
@@ -206,128 +365,4 @@ pkg_perform(char **pkgs)
     free_plist(&plist);
     leave_playpen(home);
     return TRUE;	/* Success */
-}
-
-static void
-make_dist(char *home, char *pkg, char *suffix, Package *plist)
-{
-    char tball[FILENAME_MAX];
-    PackingList p;
-    int ret;
-    char *args[50];	/* Much more than enough. */
-    int nargs = 0;
-    int pipefds[2];
-    FILE *totar;
-    pid_t pid;
-
-    args[nargs++] = "tar";	/* argv[0] */
-
-    if (*pkg == '/')
-	snprintf(tball, FILENAME_MAX, "%s.%s", pkg, suffix);
-    else
-	snprintf(tball, FILENAME_MAX, "%s/%s.%s", home, pkg, suffix);
-
-    args[nargs++] = "-c";
-    args[nargs++] = "-f";
-    args[nargs++] = tball;
-    if (strchr(suffix, 'z'))	/* Compress/gzip? */
-	args[nargs++] = "-z";
-    if (Dereference)
-	args[nargs++] = "-h";
-    if (ExcludeFrom) {
-	args[nargs++] = "-X";
-	args[nargs++] = ExcludeFrom;
-    }
-    args[nargs++] = "-T";	/* Take filenames from file instead of args. */
-    args[nargs++] = "-";	/* Use stdin for the file. */
-    args[nargs] = NULL;
-
-    if (Verbose)
-	printf("Creating gzip'd tar ball in '%s'\n", tball);
-
-    /* Set up a pipe for passing the filenames, and fork off a tar process. */
-    if (pipe(pipefds) == -1)
-	cleanup(0), errx(2, "cannot create pipe");
-    if ((pid = fork()) == -1)
-	cleanup(0), errx(2, "cannot fork process for tar");
-    if (pid == 0) {	/* The child */
-	dup2(pipefds[0], 0);
-	close(pipefds[0]);
-	close(pipefds[1]);
-	execv("/usr/bin/tar", args);
-	cleanup(0);
-	errx(2, "failed to execute tar command");
-    }
-
-    /* Meanwhile, back in the parent process ... */
-    close(pipefds[0]);
-    if ((totar = fdopen(pipefds[1], "w")) == NULL)
-	cleanup(0), errx(2, "fdopen failed");
-
-    fprintf(totar, "%s\n", CONTENTS_FNAME);
-    fprintf(totar, "%s\n", COMMENT_FNAME);
-    fprintf(totar, "%s\n", DESC_FNAME);
-
-    if (Install)
-	fprintf(totar, "%s\n", INSTALL_FNAME);
-    if (DeInstall)
-	fprintf(totar, "%s\n", DEINSTALL_FNAME);
-    if (Require)
-	fprintf(totar, "%s\n", REQUIRE_FNAME);
-    if (Display)
-	fprintf(totar, "%s\n", DISPLAY_FNAME);
-    if (Mtree)
-	fprintf(totar, "%s\n", MTREE_FNAME);
-
-    for (p = plist->head; p; p = p->next) {
-	if (p->type == PLIST_FILE)
-	    fprintf(totar, "%s\n", p->name);
-	else if (p->type == PLIST_CWD || p->type == PLIST_SRC)
-	    fprintf(totar, "-C\n%s\n", p->name);
-	else if (p->type == PLIST_IGNORE)
-	     p = p->next;
-    }
-
-    fclose(totar);
-    wait(&ret);
-    /* assume either signal or bad exit is enough for us */
-    if (ret)
-	cleanup(0), errx(2, "tar command failed with code %d", ret);
-}
-
-static void
-sanity_check()
-{
-    if (!Comment)
-	cleanup(0), errx(2,
-		"required package comment string is missing (-c comment)");
-    if (!Desc)
-	cleanup(0), errx(2,
-		"required package description string is missing (-d desc)");
-    if (!Contents)
-	cleanup(0), errx(2,
-		"required package contents list is missing (-f [-]file)");
-}
-
-
-/* Clean up those things that would otherwise hang around */
-void
-cleanup(int sig)
-{
-    static int	alreadyCleaning;
-    void (*oldint)(int);
-    void (*oldhup)(int);
-    oldint = signal(SIGINT, SIG_IGN);
-    oldhup = signal(SIGHUP, SIG_IGN);
-
-    if (!alreadyCleaning) {
-    	alreadyCleaning = 1;
-	if (sig)
-	    printf("Signal %d received, cleaning up.\n", sig);
-	leave_playpen(home);
-	if (sig)
-	    exit(1);
-    }
-    signal(SIGINT, oldint);
-    signal(SIGHUP, oldhup);
 }
