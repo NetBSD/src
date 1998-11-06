8f181ff9eaa7adccf4d1806be638b410c16675b0
cgd <cgd@NetBSD.org>
1998-11-06 20:41:03 +0000

    pull up rev(s) 1.3-1.6 from trunk.  (agc)

--- usr.sbin/pkg_install/info/info.h
+++ usr.sbin/pkg_install/info/info.h
@@ -1,4 +1,4 @@
-/* $NetBSD: info.h,v 1.2 1997/06/05 12:59:38 agc Exp $ */
+/* $NetBSD: info.h,v 1.2.2.1 1998/11/06 20:41:03 cgd Exp $ */
 
 /* from FreeBSD Id: info.h,v 1.10 1997/02/22 16:09:40 peter Exp */
 
@@ -33,29 +33,32 @@
 #define MAXNAMESIZE  20
 #endif
 
-#define	SHOW_COMMENT	0x0001
-#define SHOW_DESC	0x0002
-#define SHOW_PLIST	0x0004
-#define SHOW_INSTALL	0x0008
-#define SHOW_DEINSTALL	0x0010
-#define SHOW_REQUIRE	0x0020
-#define SHOW_PREFIX	0x0040
-#define SHOW_INDEX	0x0080
-#define SHOW_FILES	0x0100
-#define SHOW_DISPLAY	0x0200
-#define SHOW_REQBY	0x0400
-#define SHOW_MTREE	0x0800
+#define	SHOW_COMMENT		0x0001
+#define SHOW_DESC		0x0002
+#define SHOW_PLIST		0x0004
+#define SHOW_INSTALL		0x0008
+#define SHOW_DEINSTALL		0x0010
+#define SHOW_REQUIRE		0x0020
+#define SHOW_PREFIX		0x0040
+#define SHOW_INDEX		0x0080
+#define SHOW_FILES		0x0100
+#define SHOW_DISPLAY		0x0200
+#define SHOW_REQBY		0x0400
+#define SHOW_MTREE		0x0800
+#define SHOW_BUILD_VERSION	0x1000
+#define SHOW_BUILD_INFO		0x2000
 
 extern int Flags;
 extern Boolean AllInstalled;
 extern Boolean Quiet;
 extern char *InfoPrefix;
 extern char PlayPen[];
+extern size_t PlayPenSize;
 extern char *CheckPkg;
 
 extern void	show_file(char *, char *);
-extern void	show_plist(char *, Package *, plist_t);
-extern void	show_files(char *, Package *);
+extern void	show_plist(char *, package_t *, pl_ent_t);
+extern void	show_files(char *, package_t *);
 extern void	show_index(char *, char *);
 
 #endif	/* _INST_INFO_H_INCLUDE */
