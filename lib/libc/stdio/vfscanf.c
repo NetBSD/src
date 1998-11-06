0f47580494058fc0749e5b63b29064e2c19cd203
cgd <cgd@NetBSD.org>
1998-11-06 21:45:25 +0000

    pull up rev 1.21 from trunk (kleink)

--- lib/libc/stdio/vfscanf.c
+++ lib/libc/stdio/vfscanf.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: vfscanf.c,v 1.16 1997/07/13 20:15:35 christos Exp $	*/
+/*	$NetBSD: vfscanf.c,v 1.16.2.1 1998/11/06 21:45:25 cgd Exp $	*/
 
 /*-
  * Copyright (c) 1990, 1993
@@ -41,7 +41,7 @@
 #if 0
 static char sccsid[] = "@(#)vfscanf.c	8.1 (Berkeley) 6/4/93";
 #else
-__RCSID("$NetBSD: vfscanf.c,v 1.16 1997/07/13 20:15:35 christos Exp $");
+__RCSID("$NetBSD: vfscanf.c,v 1.16.2.1 1998/11/06 21:45:25 cgd Exp $");
 #endif
 #endif /* LIBC_SCCS and not lint */
 
@@ -138,13 +138,9 @@ __svfscanf(fp, fmt0, ap)
 		if (c == 0)
 			return (nassigned);
 		if (isspace(c)) {
-			for (;;) {
-				if (fp->_r <= 0 && __srefill(fp))
-					return (nassigned);
-				if (!isspace(*fp->_p))
-					break;
+			while ((fp->_r > 0 || __srefill(fp) == 0) &&
+			    isspace(*fp->_p))
 				nread++, fp->_r--, fp->_p++;
-			}
 			continue;
 		}
 		if (c != '%')
