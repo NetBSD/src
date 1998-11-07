a9bb753f8d088677635bdb67651409b264a5d8cc
cgd <cgd@NetBSD.org>
1998-11-07 00:19:27 +0000

    pull up rev 1.2 from trunk (jonathan)

--- lib/csu/mips/crtend.c
+++ lib/csu/mips/crtend.c
@@ -1,12 +1,10 @@
-/*	$NetBSD: crtend.c,v 1.1 1997/03/05 03:45:06 jonathan Exp $	*/
-
-#ifndef ECOFF_COMPAT
+/*	$NetBSD: crtend.c,v 1.1.2.1 1998/11/07 00:19:27 cgd Exp $	*/
 
 #include <sys/cdefs.h>
 
 static void (*__CTOR_LIST__[1]) __P((void))
+    __attribute__((__unused__))
     __attribute__((section(".ctors"))) = { (void *)0 };		/* XXX */
 static void (*__DTOR_LIST__[1]) __P((void))
+    __attribute__((__unused__))
     __attribute__((section(".dtors"))) = { (void *)0 };		/* XXX */
-
-#endif /* !ECOFF_COMPAT */
