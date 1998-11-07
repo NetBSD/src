e3a4cfebcae60915196ddc65f92465a176beacc4
cgd <cgd@NetBSD.org>
1998-11-07 00:56:32 +0000

    pull up revs 1.1-1.2 from trunk (new file).  (jonathan)

---
+++ gnu/usr.bin/ld.new/ldemul-list.h
@@ -0,0 +1,8 @@
+/*	$NetBSD: ldemul-list.h,v 1.2.2.2 1998/11/07 00:56:32 cgd Exp $	*/
+
+/* alpha emulations */
+extern ld_emulation_xfer_type ld_elf64alpha_emulation;
+
+/* mips emulations */
+extern ld_emulation_xfer_type ld_elf32lmip_emulation;
+extern ld_emulation_xfer_type ld_elf32bmip_emulation;
