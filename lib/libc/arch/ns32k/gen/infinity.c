f7441a1f6c11355d1ee4aa1b352dc76de5d9f0e3
christos <christos>
1998-11-14 19:31:01 +0000

    delint

--- lib/libc/arch/ns32k/gen/infinity.c
+++ lib/libc/arch/ns32k/gen/infinity.c
@@ -1,11 +1,11 @@
-/*	$NetBSD: infinity.c,v 1.6 1998/07/26 14:14:15 mycroft Exp $	*/
+/*	$NetBSD: infinity.c,v 1.7 1998/11/14 19:31:02 christos Exp $	*/
 
 #include <sys/cdefs.h>
 #if defined(LIBC_SCCS) && !defined(lint)
-__RCSID("$NetBSD: infinity.c,v 1.6 1998/07/26 14:14:15 mycroft Exp $");
+__RCSID("$NetBSD: infinity.c,v 1.7 1998/11/14 19:31:02 christos Exp $");
 #endif /* LIBC_SCCS and not lint */
 
 #include <math.h>
 
 /* bytes for +Infinity on a ns32k */
-const char __infinity[] = { 0, 0, 0, 0, 0, 0, 0xf0, 0x7f };
+const char __infinity[] = { 0, 0, 0, 0, 0, 0, (char)0xf0, 0x7f };
