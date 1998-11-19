e2a88cb69c03f810597bbff014ba1ddca8c6f60c
kenh <kenh@NetBSD.org>
1998-11-19 23:41:09 +0000

    Rename wdcio->ataio.h, as per ICB cabal discussion.

--- sys/sys/ataio.h
+++ sys/sys/ataio.h
@@ -1,12 +1,12 @@
-/*	$NetBSD: wdcio.h,v 1.1 1998/11/19 19:44:34 kenh Exp $	*/
+/*	$NetBSD: ataio.h,v 1.1 1998/11/19 23:41:09 kenh Exp $	*/
 
-#ifndef _SYS_WDCIO_H_
-#define _SYS_WDCIO_H_
+#ifndef _SYS_ATAIO_H_
+#define _SYS_ATAIO_H_
 
 #include <sys/types.h>
 #include <sys/ioctl.h>
 
-typedef struct	wdcreq {
+typedef struct	atareq {
 	u_long	flags;		/* info about the request status and type */
 	u_char	command;	/* command code */
 	u_char	features;	/* feature modifier bits for command */
@@ -20,18 +20,18 @@ typedef struct	wdcreq {
 	int	timeout;	/* Command timeout */
 	u_char	retsts;		/* the return status for the command */
 	u_char	error;		/* error bits */
-} wdcreq_t;
+} atareq_t;
 
 /* bit defintions for flags */
-#define WDCCMD_READ		0x00000001
-#define WDCCMD_WRITE		0x00000002
+#define ATACMD_READ		0x00000001
+#define ATACMD_WRITE		0x00000002
 
 /* definitions for the return status (retsts) */
-#define WDCCMD_OK	0x00
-#define WDCCMD_TIMEOUT	0x01
-#define WDCCMD_ERROR	0x02
-#define WDCCMD_DF	0x03
+#define ATACMD_OK	0x00
+#define ATACMD_TIMEOUT	0x01
+#define ATACMD_ERROR	0x02
+#define ATACMD_DF	0x03
 
-#define WDCIOCCOMMAND	_IOWR('Q', 8, wdcreq_t)
+#define ATAIOCCOMMAND	_IOWR('Q', 8, atareq_t)
 
-#endif /* _SYS_WDCIO_H_ */
+#endif /* _SYS_ATAIO_H_ */
