25562cb07cf06dc6832faa0ca4545a8fd442873b
kristerw <kristerw>
1998-11-21 23:43:48 +0000

    Prevent gdb from getting stuck in _PROCEDURE_LINKAGE_TABLE_ () Closes my PR bin/6323.

--- gnu/dist/gdb/config/tm-nbsd.h
+++ gnu/dist/gdb/config/tm-nbsd.h
@@ -23,6 +23,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
 /* Return non-zero if we are in a shared library trampoline code stub. */
 
 #define IN_SOLIB_CALL_TRAMPOLINE(pc, name) \
-  (name && !strcmp(name, "_DYNAMIC"))
+  (name && \
+   (!strcmp(name, "_DYNAMIC") || !strcmp(name, "_PROCEDURE_LINKAGE_TABLE_")))
 
 #endif /* !SVR4_SHARED_LIBS */
