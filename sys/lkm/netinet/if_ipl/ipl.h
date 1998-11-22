0273b7b1383a03be97e60fd9d1203bc81b688979
mrg <mrg@NetBSD.org>
1998-11-22 14:41:29 +0000

    merge ipf 3.2.10

--- sys/lkm/netinet/if_ipl/ipl.h
+++ sys/lkm/netinet/if_ipl/ipl.h
@@ -1,7 +1,7 @@
-/*	$NetBSD: ipl.h,v 1.12 1998/07/12 15:11:50 veego Exp $	*/
+/*	$NetBSD: ipl.h,v 1.13 1998/11/22 14:41:29 mrg Exp $	*/
 
 /*
- * Copyright (C) 1993-1997 by Darren Reed.
+ * Copyright (C) 1993-1998 by Darren Reed.
  *
  * Redistribution and use in source and binary forms are permitted
  * provided that this notice is preserved and due credit is given
@@ -13,6 +13,6 @@
 #ifndef	__IPL_H__
 #define	__IPL_H__
 
-#define	IPL_VERSION	"IP Filter v3.2.9"
+#define	IPL_VERSION	"IP Filter v3.2.10"
 
 #endif
