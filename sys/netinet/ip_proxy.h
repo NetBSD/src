7b0923d9dc131675abe784b5a12cda648b993e69
cgd <cgd@NetBSD.org>
1998-11-24 07:20:14 +0000

    pull up rev(s) 1.12 from trunk (ipfilter 3.2.10).  (mrg)

--- sys/netinet/ip_proxy.h
+++ sys/netinet/ip_proxy.h
@@ -1,13 +1,13 @@
-/*	$NetBSD: ip_proxy.h,v 1.7.2.3 1998/07/22 23:43:30 mellon Exp $	*/
+/*	$NetBSD: ip_proxy.h,v 1.7.2.4 1998/11/24 07:20:14 cgd Exp $	*/
 
 /*
- * Copyright (C) 1997 by Darren Reed.
+ * Copyright (C) 1997-1998 by Darren Reed.
  *
  * Redistribution and use in source and binary forms are permitted
  * provided that this notice is preserved and due credit is given
  * to the original author and the contributors.
  *
- * Id: ip_proxy.h,v 2.0.2.10.2.1 1997/11/27 09:33:27 darrenr Exp 
+ * Id: ip_proxy.h,v 2.0.2.10.2.4 1998/11/22 01:50:30 darrenr Exp
  */
 
 #ifndef _NETINET_IP_PROXY_H_
@@ -28,9 +28,11 @@ struct	ipnat;
 typedef	struct	ap_tcp {
 	u_short	apt_sport;	/* source port */
 	u_short	apt_dport;	/* destination port */
-	short	apt_sel;	/* seqoff/after set selector */
+	short	apt_sel[2];	/* {seq,ack}{off,min} set selector */
 	short	apt_seqoff[2];	/* sequence # difference */
-	tcp_seq	apt_after[2];	/* don't change seq-off until after this */
+	tcp_seq	apt_seqmin[2];	/* don't change seq-off until after this */
+	short	apt_ackoff[2];	/* sequence # difference */
+	tcp_seq	apt_ackmin[2];	/* don't change seq-off until after this */
 	u_char	apt_state[2];	/* connection state */
 } ap_tcp_t;
 
@@ -48,12 +50,14 @@ typedef	struct ap_session {
 		struct	ap_tcp	apu_tcp;
 		struct	ap_udp	apu_udp;
 	} aps_un;
+	u_int	aps_hv;
 	u_int	aps_flags;
-	QUAD_T	aps_bytes;	/* bytes sent */
-	QUAD_T	aps_pkts;	/* packets sent */
-	u_long	aps_tout;	/* time left before expiring */
+	U_QUAD_T aps_bytes;	/* bytes sent */
+	U_QUAD_T aps_pkts;	/* packets sent */
+	void	*aps_nat;	/* pointer back to nat struct */
 	void	*aps_data;	/* private data */
 	int	aps_psiz;	/* size of private data */
+	struct	ap_session	*aps_hnext;
 	struct	ap_session	*aps_next;
 } ap_session_t ;
 
@@ -61,8 +65,10 @@ typedef	struct ap_session {
 #define	aps_dport	aps_un.apu_tcp.apt_dport
 #define	aps_sel		aps_un.apu_tcp.apt_sel
 #define	aps_seqoff	aps_un.apu_tcp.apt_seqoff
-#define	aps_after	aps_un.apu_tcp.apt_after
+#define	aps_seqmin	aps_un.apu_tcp.apt_seqmin
 #define	aps_state	aps_un.apu_tcp.apt_state
+#define	aps_ackoff	aps_un.apu_tcp.apt_ackoff
+#define	aps_ackmin	aps_un.apu_tcp.apt_ackmin
 
 
 typedef	struct	aproxy	{
@@ -70,11 +76,11 @@ typedef	struct	aproxy	{
 	u_char	apr_p;		/* protocol */
 	int	apr_ref;	/* +1 per rule referencing it */
 	int	apr_flags;
-	int	(* apr_init) __P((fr_info_t *, ip_t *, tcphdr_t *,
+	int	(* apr_init) __P((fr_info_t *, ip_t *,
 				   ap_session_t *, struct nat *));
-	int	(* apr_inpkt) __P((fr_info_t *, ip_t *, tcphdr_t *,
+	int	(* apr_inpkt) __P((fr_info_t *, ip_t *,
 				   ap_session_t *, struct nat *));
-	int	(* apr_outpkt) __P((fr_info_t *, ip_t *, tcphdr_t *,
+	int	(* apr_outpkt) __P((fr_info_t *, ip_t *,
 				    ap_session_t *, struct nat *));
 } aproxy_t;
 
@@ -82,14 +88,13 @@ typedef	struct	aproxy	{
 
 
 extern	ap_session_t	*ap_sess_tab[AP_SESS_SIZE];
+extern	ap_session_t	*ap_sess_list;
 extern	aproxy_t	ap_proxies[];
 
 extern	int	ap_ok __P((ip_t *, tcphdr_t *, struct ipnat *));
-extern	void	ap_unload __P((void));
 extern	void	ap_free __P((aproxy_t *));
 extern	void	aps_free __P((ap_session_t *));
-extern	int	ap_check __P((ip_t *, tcphdr_t *, fr_info_t *, struct nat *));
+extern	int	ap_check __P((ip_t *, fr_info_t *, struct nat *));
 extern	aproxy_t	*ap_match __P((u_char, char *));
-extern	void	ap_expire __P((void));
 
 #endif /* _NETINET_IP_PROXY_H_ */
