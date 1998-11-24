d27ecfe6d1d0c1a050469bd44f75f0192c31abd8
cgd <cgd>
1998-11-24 07:21:47 +0000

    pull up rev(s) 1.8 from trunk (ipfilter 3.2.10).  (mrg)

--- usr.sbin/ipf/ipsend/ip.c
+++ usr.sbin/ipf/ipsend/ip.c
@@ -1,7 +1,7 @@
-/*	$NetBSD: ip.c,v 1.5.2.2 1998/07/23 01:29:10 mellon Exp $	*/
+/*	$NetBSD: ip.c,v 1.5.2.3 1998/11/24 07:21:47 cgd Exp $	*/
 
 /*
- * ip.c (C) 1995-1997 Darren Reed
+ * ip.c (C) 1995-1998 Darren Reed
  *
  * Redistribution and use in source and binary forms are permitted
  * provided that this notice is preserved and due credit is given
@@ -9,7 +9,7 @@
  */
 #if !defined(lint)
 static const char sccsid[] = "%W% %G% (C)1995";
-static const char rcsid[] = "@(#)Id: ip.c,v 2.0.2.11.2.3 1997/12/21 12:17:37 darrenr Exp ";
+static const char rcsid[] = "@(#)Id: ip.c,v 2.0.2.11.2.4 1998/11/22 01:51:15 darrenr Exp ";
 #endif
 #include <errno.h>
 #include <stdio.h>
