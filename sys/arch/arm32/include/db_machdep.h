f04bfd7a5fb1e86b69417b9c92707ce1a5beebd8
mycroft <mycroft@NetBSD.org>
1998-11-25 06:41:26 +0000

    First whack at making single-stepping work through switch and return statements.

--- sys/arch/arm32/include/db_machdep.h
+++ sys/arch/arm32/include/db_machdep.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: db_machdep.h,v 1.9 1998/08/15 02:57:28 mark Exp $	*/
+/*	$NetBSD: db_machdep.h,v 1.10 1998/11/25 06:41:27 mycroft Exp $	*/
 
 /*
  * Copyright (c) 1996 Scott K Stevens
@@ -66,18 +66,31 @@ db_regs_t		ddb_regs;	/* register state */
 #define	IS_BREAKPOINT_TRAP(type, code)	((type) == T_BREAKPOINT)
 #define IS_WATCHPOINT_TRAP(type, code)	(0)
 
-#define	inst_trap_return(ins)	((ins)&0)
-#define	inst_return(ins)	((ins)&0)
+#define	inst_trap_return(ins)	(0)
+/* ldmxx reg, {..., pc}
+					    01800000  stack mode
+					    000f0000  register
+					    0000ffff  register list */
+/* mov pc, reg
+					    0000000f  register */
+#define	inst_return(ins)	(((ins) & 0x0e108000) == 0x08108000 || \
+				 ((ins) & 0x0ff0fff0) == 0x01a0f000)
+/* bl ...
+					    00ffffff  offset>>2 */
 #define	inst_call(ins)		(((ins) & 0x0f000000) == 0x0b000000)
-#define	inst_branch(ins)	(((ins) & 0x0f000000) == 0x0a000000)
-#define inst_load(ins)		0
-#define inst_store(ins)		0
+/* b ...
+					    00ffffff  offset>>2 */
+/* ldr pc, [pc, reg, lsl #2]
+					    0000000f  register */
+#define	inst_branch(ins)	(((ins) & 0x0f000000) == 0x0a000000 || \
+				 ((ins) & 0x0fdffff0) == 0x079ff100)
+#define inst_load(ins)		(0)
+#define inst_store(ins)		(0)
 #define inst_unconditional_flow_transfer(ins)	\
-	((((ins) & INSN_COND_MASK) == INSN_COND_AL) && \
-	(inst_call(ins) | inst_branch(ins)))
+	((((ins) & INSN_COND_MASK) == INSN_COND_AL) && inst_branch(ins))
 
 #define getreg_val			(0)
-#define next_instr_address(pc, bd)	((bd) ? (pc) : (pc + INSN_SIZE))
+#define next_instr_address(pc, bd)	((bd) ? (pc) : ((pc) + INSN_SIZE))
 
 #define DB_MACHINE_COMMANDS
 
