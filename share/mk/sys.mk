0caacce512f4878bb5ddf12dea429e6e950361e3
mycroft <mycroft@NetBSD.org>
1998-11-26 23:06:09 +0000

    Use -O2 on SPARC.

--- share/mk/sys.mk
+++ share/mk/sys.mk
@@ -1,4 +1,4 @@
-#	$NetBSD: sys.mk,v 1.44 1998/11/22 13:48:34 mycroft Exp $
+#	$NetBSD: sys.mk,v 1.45 1998/11/26 23:06:09 mycroft Exp $
 #	@(#)sys.mk	8.2 (Berkeley) 3/21/94
 
 unix?=		We run NetBSD.
@@ -19,7 +19,8 @@ COMPILE.S?=	${CC} ${AFLAGS} ${CPPFLAGS} -c -traditional-cpp
 LINK.S?=	${CC} ${AFLAGS} ${CPPFLAGS} ${LDFLAGS}
 
 CC?=		cc
-.if (${MACHINE_ARCH} == "i386")
+.if (${MACHINE_ARCH} == "i386") || \
+    (${MACHINE_ARCH} == "sparc")
 CFLAGS?=	-O2
 .else
 CFLAGS?=	-O
