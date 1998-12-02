b7ddfc3d2cda4772e336bc28d02807aa8c1b61e2
christos <christos>
1998-12-02 09:47:20 +0000

    PR/6251: ``TheMan'': Delint.

--- lib/libc/gen/Lint_frexp.c
+++ lib/libc/gen/Lint_frexp.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: Lint_frexp.c,v 1.1 1997/12/07 00:24:50 matthias Exp $	*/
+/*	$NetBSD: Lint_frexp.c,v 1.2 1998/12/02 09:47:20 christos Exp $	*/
 
 /*
  * This file placed in the public domain.
@@ -7,6 +7,7 @@
 
 #include <math.h>
 
+/*ARGSUSED*/
 double
 frexp(value, eptr)
 	double value;
