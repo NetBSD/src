f2125afd0e9f50ff8b51216b23e4da34c214d16c
kristerw <kristerw@NetBSD.org>
1998-12-12 21:57:19 +0000

    Add sparc and i386

--- gnu/usr.bin/ld.new/ldemul-list.h
+++ gnu/usr.bin/ld.new/ldemul-list.h
@@ -1,8 +1,14 @@
-/*	$NetBSD: ldemul-list.h,v 1.2 1998/09/30 16:47:48 jonathan Exp $	*/
+/*	$NetBSD: ldemul-list.h,v 1.3 1998/12/12 21:57:19 kristerw Exp $	*/
 
 /* alpha emulations */
 extern ld_emulation_xfer_type ld_elf64alpha_emulation;
 
+/* i386 emulations */
+extern ld_emulation_xfer_type ld_i386nbsd_emulation;
+
 /* mips emulations */
 extern ld_emulation_xfer_type ld_elf32lmip_emulation;
 extern ld_emulation_xfer_type ld_elf32bmip_emulation;
+
+/* sparc emulations */
+extern ld_emulation_xfer_type ld_sparcnbsd_emulation;
