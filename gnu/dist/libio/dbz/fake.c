33e12ac59256b24e227d30168c56e873e96c986c
tv <tv@NetBSD.org>
1998-12-13 00:15:30 +0000

    Import egcs 1.1.1 (release).

--- gnu/dist/libio/dbz/fake.c
+++ gnu/dist/libio/dbz/fake.c
@@ -13,7 +13,7 @@
 #define	STREQ(a, b)	(*(a) == *(b) && strcmp((a), (b)) == 0)
 
 #ifndef lint
-static char RCSid[] = "$Header: /cvsroot/src/gnu/dist/libio/dbz/Attic/fake.c,v 1.1.1.4 1998/10/14 14:35:59 tv Exp $";
+static char RCSid[] = "$Header: /cvsroot/src/gnu/dist/libio/dbz/Attic/fake.c,v 1.1.1.5 1998/12/13 00:15:54 tv Exp $";
 #endif
 
 int midonly = 0;		/* just message ids, rest not realistic */
