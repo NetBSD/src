50d818dbed203e5057d67b8426dd5713765efedb
lukem <lukem@NetBSD.org>
1998-12-14 15:39:12 +0000

    * add support for /CHAOS and /HS suffixes to primary & secondary   directives, and add appropriate directives to the zone {} blocks * change comment delimiter from '//' to '#' (which is more intuitive   for unix config files) * KNF formatting (single space indenting is hard to read)

--- usr.sbin/bind/named/named-bootconf.sh
+++ usr.sbin/bind/named/named-bootconf.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# $NetBSD: named-bootconf.sh,v 1.3 1998/10/05 21:37:18 tron Exp $
+# $NetBSD: named-bootconf.sh,v 1.4 1998/12/14 15:39:12 lukem Exp $
 #
 # Copyright (c) 1995, 1998 The NetBSD Foundation, Inc.
 # All rights reserved.
@@ -36,188 +36,188 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
-if [ ${OPTIONFILE-X} = X ]
-then
- OPTIONFILE=/tmp/.options.`date +%s`.$$
- ZONEFILE=/tmp/.zones.`date +%s`.$$
- export OPTIONFILE ZONEFILE
- touch $OPTIONFILE $ZONEFILE
- DUMP=1
+if [ ${OPTIONFILE-X} = X ]; then
+	OPTIONFILE=/tmp/.options.`date +%s`.$$
+	ZONEFILE=/tmp/.zones.`date +%s`.$$
+	export OPTIONFILE ZONEFILE
+	touch $OPTIONFILE $ZONEFILE
+	DUMP=1
 else
- DUMP=0
+	DUMP=0
 fi
 
-while read CMD ARGS
-do
- case $CMD in
- \; )
-  echo // $ARGS
-  ;;
- cache )
-  set - X $ARGS
-  shift
-  if [ $# -eq 2 ]
-  then
-   (echo ""
-    echo "zone \"$1\" {"
-    echo "	type hint;"
-    echo "	file \"$2\";"
-    echo "};") >>$ZONEFILE
-  fi
-  ;;
- directory )
-  set - X $ARGS
-  shift
-  if [ $# -eq 1 -a -d $1 ]
-  then
-   echo "	directory \"$1\";" >>$OPTIONFILE
-   DIRECTORY=$1
-   export DIRECTORY
-  fi
-  ;; 
- forwarders )
-  (echo "	forwarders {"
-   for ARG in $ARGS
-   do
-    echo "		$ARG;"
-   done
-   echo "	};") >>$OPTIONFILE
-  ;;
- include )
-  if [ "$ARGS" != "" ]
-  then
-   (cd ${DIRECTORY-.}; cat $ARGS) | $0
-  fi
-  ;;
- limit )
-  set - X $ARGS
-  shift
-  if [ $# -eq 2 ]
-  then
-   case $1 in
-    datasize | files | transfers-in | transfers-per-ns )
-    echo "	$1 $2;" >>$OPTIONFILE
-    ;;
-   esac
-  fi
-  ;;
- options )
-  for ARG in $ARGS
-  do
-   case $ARG in
-   fake-iquery )
-    echo "	fake-iquery yes;" >>$OPTIONFILE
-    ;;
-   forward-only )
-    echo "	forward only;" >>$OPTIONFILE
-    ;;
-   no-fetch-glue )
-    echo "	fetch-glue no;" >>$OPTIONFILE
-    ;;
-   no-recursion )
-    echo "	recursion no;" >>$OPTIONFILE
-    ;;
-   esac
-  done
-  ;;
- primary )
-  set - X $ARGS
-  shift
-  if [ $# -eq 2 ]
-  then
-   (echo ""
-    echo "zone \"$1\" {"
-    echo "	type master;"
-    echo "	file \"$2\";"
-    echo "};") >>$ZONEFILE
-  fi
-  ;;
- secondary )
-  set - X $ARGS
-  shift
-  if [ $# -gt 2 ]
-  then
-   ZONE=$1
-   shift
-   PRIMARIES=$1
-   while [ $# -gt 2 ]
-   do
-    shift
-    PRIMARIES="$PRIMARIES $1"
-   done
-   (echo ""
-    echo "zone \"$ZONE\" {"
-    echo "	type slave;"
-    echo "	file \"$2\";"
-    echo "	masters {"
-    for PRIMARY in $PRIMARIES
-    do
-     echo "		$PRIMARY;"
-    done
-    echo "	};"
-    echo "};") >>$ZONEFILE
-  fi
-  ;;
- slave )
-  echo "	forward only;" >>$OPTIONFILE
-  ;;
- sortlist )
-  (echo "	topology {"
-   for ARG in $ARGS
-   do
-    case $ARG in
-    *.0.0.0 )
-     echo "		$ARG/8;"
-     ;;
-    *.0.0 )
-     echo "		$ARG/16;"
-     ;;
-    *.0 )
-     echo "		$ARG/24;"
-     ;;
-    * )
-     echo "		$ARG;"
-     ;;
-    esac
-   done
-   echo "	};") >>$OPTIONFILE
-  ;;
- tcplist | xfrnets )
-  (echo "	allow-transfer {"
-   for ARG in $ARGS
-   do
-    case $ARG in
-    *.0.0.0 )
-     echo "		$ARG/8;"
-     ;;
-    *.0.0 )
-     echo "		$ARG/16;"
-     ;;
-    *.0 )
-     echo "		$ARG/24;"
-     ;;
-    * )
-     echo "		$ARG;"
-     ;;
-    esac
-   done
-   echo "	};") >>$OPTIONFILE
-  ;;
- esac
+while read CMD ARGS; do
+	case $CMD in
+	\; )
+		echo \# $ARGS
+		;;
+	cache )
+		set - X $ARGS
+		shift
+		if [ $# -eq 2 ]; then
+			(echo ""
+			echo "zone \"$1\" {"
+			echo "	type hint;"
+			echo "	file \"$2\";"
+			echo "};") >>$ZONEFILE
+		fi
+		;;
+	directory )
+		set - X $ARGS
+		shift
+		if [ $# -eq 1 -a -d $1 ]; then
+			echo "	directory \"$1\";" >>$OPTIONFILE
+			DIRECTORY=$1
+			export DIRECTORY
+		fi
+		;; 
+	forwarders )
+		(echo "	forwarders {"
+		for ARG in $ARGS; do
+			echo "		$ARG;"
+		done
+		echo "	};") >>$OPTIONFILE
+		;;
+	include )
+		if [ "$ARGS" != "" ]; then
+			(cd ${DIRECTORY-.}; cat $ARGS) | $0
+		fi
+		;;
+	limit )
+		set - X $ARGS
+		shift
+		if [ $# -eq 2 ]; then
+			case $1 in
+			datasize | files | transfers-in | transfers-per-ns )
+				echo "	$1 $2;" >>$OPTIONFILE
+				;;
+			esac
+		fi
+		;;
+	options )
+		for ARG in $ARGS; do
+			case $ARG in
+			fake-iquery )
+				echo "	fake-iquery yes;" >>$OPTIONFILE
+				;;
+			forward-only )
+				echo "	forward only;" >>$OPTIONFILE
+				;;
+			no-fetch-glue )
+				echo "	fetch-glue no;" >>$OPTIONFILE
+				;;
+			no-recursion )
+				echo "	recursion no;" >>$OPTIONFILE
+				;;
+			esac
+		done
+		;;
+	primary|primary/* )
+		case $CMD in
+		primary/CHAOS )
+			class="chaos "
+			;;
+		primary/HS )
+			class="hesiod "
+			;;
+		esac
+		set - X $ARGS
+		shift
+		if [ $# -eq 2 ]; then
+			(echo ""
+			echo "zone \"$1\" ${class}{"
+			echo "	type master;"
+			echo "	file \"$2\";"
+			echo "};") >>$ZONEFILE
+		fi
+		;;
+	secondary|secondary/* )
+		case $CMD in
+		secondary/CHAOS )
+			class="chaos "
+			;;
+		secondary/HS )
+			class="hesiod "
+			;;
+		esac
+		set - X $ARGS
+		shift
+		if [ $# -gt 2 ]; then
+			ZONE=$1
+			shift
+			PRIMARIES=$1
+			while [ $# -gt 2 ]; do
+				shift
+				PRIMARIES="$PRIMARIES $1"
+			done
+			(echo ""
+			echo "zone \"$ZONE\" ${class}{"
+			echo "	type slave;"
+			echo "	file \"$2\";"
+			echo "	masters {"
+			for PRIMARY in $PRIMARIES; do
+				echo "		$PRIMARY;"
+			done
+			echo "	};"
+			echo "};") >>$ZONEFILE
+		fi
+		;;
+	slave )
+		echo "	forward only;" >>$OPTIONFILE
+		;;
+	sortlist )
+		(echo "	topology {"
+		for ARG in $ARGS; do
+			case $ARG in
+			*.0.0.0 )
+				echo "		$ARG/8;"
+				;;
+			*.0.0 )
+				echo "		$ARG/16;"
+				;;
+			*.0 )
+				echo "		$ARG/24;"
+				;;
+			* )
+				echo "		$ARG;"
+				;;
+			esac
+		done
+		echo "	};") >>$OPTIONFILE
+		;;
+	tcplist | xfrnets )
+		(echo "	allow-transfer {"
+		for ARG in $ARGS; do
+			case $ARG in
+			*.0.0.0 )
+				echo "		$ARG/8;"
+				;;
+			*.0.0 )
+				echo "		$ARG/16;"
+				;;
+			*.0 )
+				echo "		$ARG/24;"
+				;;
+			* )
+				echo "		$ARG;"
+				;;
+			esac
+		done
+		echo "	};") >>$OPTIONFILE
+		;;
+	esac
 done
 
-if [ $DUMP -eq 1 ]
-then
- if [ -s $OPTIONFILE ]
- then
-  echo ""
-  echo "options {"
-  cat $OPTIONFILE
-  echo "};"
- fi
- cat $ZONEFILE
+if [ $DUMP -eq 1 ]; then
+	if [ -s $OPTIONFILE ]; then
+		echo ""
+		echo "options {"
+		cat $OPTIONFILE
+		echo "};"
+	fi
+	cat $ZONEFILE
 
- rm -f $OPTIONFILE $ZONEFILE
+	rm -f $OPTIONFILE $ZONEFILE
 fi
 
 exit 0
