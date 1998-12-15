f75800fa7f80dfc3ba2c82505d49ceed09a362e8
pk <pk>
1998-12-15 23:16:14 +0000

    Export the common shared library search functions in its own header file.

--- libexec/ld.aout_so/shlib.c
+++ libexec/ld.aout_so/shlib.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: shlib.c,v 1.15 1998/09/15 12:38:55 pk Exp $	*/
+/*	$NetBSD: shlib.c,v 1.16 1998/12/15 23:16:14 pk Exp $	*/
 
 /*-
  * Copyright (c) 1998 The NetBSD Foundation, Inc.
@@ -46,7 +46,6 @@ int	isdigit();
 #include <sys/stat.h>
 #include <sys/file.h>
 #include <sys/time.h>
-#include <a.out.h>
 #include <ctype.h>
 #include <dirent.h>
 #include <err.h>
@@ -54,8 +53,9 @@ int	isdigit();
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <link.h>
 
-#include "ld.h"
+#include "shlib.h"
 
 /*
  * Standard directories to search for files specified by -l.
@@ -74,7 +74,6 @@ int	n_search_dirs;
 char	*standard_search_dirs[] = {
 	STANDARD_SEARCH_DIRS
 };
-
 
 void
 add_search_dir(name)
@@ -324,3 +323,64 @@ int	do_dot_a;
 
 	return path;
 }
+
+
+/*
+ * Utility functions shared with others.
+ */
+
+
+/*
+ * Like malloc but get fatal error if memory is exhausted.
+ */
+void *
+xmalloc(size)
+	size_t size;
+{
+	void	*result = (void *)malloc(size);
+
+	if (!result)
+		errx(1, "virtual memory exhausted");
+
+	return (result);
+}
+
+/*
+ * Like realloc but get fatal error if memory is exhausted.
+ */
+void *
+xrealloc(ptr, size)
+	void *ptr;
+	size_t size;
+{
+	void	*result;
+
+	result = (ptr == NULL) ? malloc(size) : realloc(ptr, size);
+	if (result == NULL)
+		errx(1, "virtual memory exhausted");
+
+	return (result);
+}
+
+/*
+ * Return a newly-allocated string whose contents concatenate
+ * the strings S1, S2, S3.
+ */
+char *
+concat(s1, s2, s3)
+	const char *s1, *s2, *s3;
+{
+	int	len1 = strlen(s1),
+		len2 = strlen(s2),
+		len3 = strlen(s3);
+
+	char *result = (char *)xmalloc(len1 + len2 + len3 + 1);
+
+	strcpy(result, s1);
+	strcpy(result + len1, s2);
+	strcpy(result + len1 + len2, s3);
+	result[len1 + len2 + len3] = 0;
+
+	return (result);
+}
+
