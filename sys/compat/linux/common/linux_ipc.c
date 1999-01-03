dce4704f9b7ba22f2766663b6acd227b8d757206
erh <erh@NetBSD.org>
1999-01-03 05:18:01 +0000

    Undo previous: Put back opt_sysv.h

--- sys/compat/linux/common/linux_ipc.c
+++ sys/compat/linux/common/linux_ipc.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: linux_ipc.c,v 1.18 1999/01/03 03:54:45 erh Exp $	*/
+/*	$NetBSD: linux_ipc.c,v 1.19 1999/01/03 05:18:01 erh Exp $	*/
 
 /*-
  * Copyright (c) 1995, 1998 The NetBSD Foundation, Inc.
@@ -35,6 +35,8 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
+
+#include "opt_sysv.h"
 
 #include <sys/types.h>
 #include <sys/param.h>
