ab0b7593963ba0ba7a8ad35d8904f1c508780418
tron <tron@NetBSD.org>
1999-01-04 00:04:04 +0000

    Avoid problems when trying to link BIND 8 statically, fixes PR bin/6717 by gnatios Souvatzis.

--- usr.sbin/bind/include/port_before.h
+++ usr.sbin/bind/include/port_before.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: port_before.h,v 1.1.1.1 1998/10/05 18:01:58 tron Exp $	*/
+/*	$NetBSD: port_before.h,v 1.2 1999/01/04 00:04:05 tron Exp $	*/
 
 /*
 #define WANT_IRS_NIS
@@ -6,3 +6,5 @@
 #define WANT_IRS_GR
 */
 #define SIG_FN void
+
+#include "namespace.h"
