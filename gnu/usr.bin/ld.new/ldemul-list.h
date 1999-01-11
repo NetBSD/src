98df777c2d25a91d5fbac08118cb5781816a6151
christos <christos@NetBSD.org>
1999-01-11 09:53:26 +0000

    Add ELF emulation, and make it default if OBJECT_FMT == ELF

--- gnu/usr.bin/ld.new/ldemul-list.h
+++ gnu/usr.bin/ld.new/ldemul-list.h
@@ -1,10 +1,11 @@
-/*	$NetBSD: ldemul-list.h,v 1.4 1998/12/17 13:59:05 mrg Exp $	*/
+/*	$NetBSD: ldemul-list.h,v 1.5 1999/01/11 09:53:26 christos Exp $	*/
 
 /* alpha emulations */
 extern ld_emulation_xfer_type ld_elf64alpha_emulation;
 
 /* i386 emulations */
 extern ld_emulation_xfer_type ld_i386nbsd_emulation;
+extern ld_emulation_xfer_type ld_elf_i386_emulation;
 
 /* mips emulations */
 extern ld_emulation_xfer_type ld_elf32lmip_emulation;
