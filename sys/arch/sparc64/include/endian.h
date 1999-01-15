83d7c85af4e44f99db2f567b4db4f87da0d72f21
bouyer <bouyer@NetBSD.org>
1999-01-15 13:31:15 +0000

    Move the bswap functions from libutil to libc (this bups the minor of libc and the major of libutil). For little-endian architectures merge the bnswap() assembly versions with nto* and hton* using symbols aliasing. Use symbol renaming for the bswap function in this case to avoid namespace pollution. Declare bswap* in machine/bswap.h, not machine/endian.h. For little-endian machines, common code for inline macros go in machine/byte_swap.h Sync libkern with libc. Adjust #include in kernel sources for machine/bswap.h.

--- sys/arch/sparc64/include/endian.h
+++ sys/arch/sparc64/include/endian.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: endian.h,v 1.2 1998/07/31 15:07:42 mycroft Exp $ */
+/*	$NetBSD: endian.h,v 1.3 1999/01/15 13:31:27 bouyer Exp $ */
 
 /*
  * Copyright (c) 1987, 1991 Regents of the University of California.
@@ -61,9 +61,6 @@ in_addr_t htonl __P((in_addr_t));
 in_port_t htons __P((in_port_t));
 in_addr_t ntohl __P((in_addr_t));
 in_port_t ntohs __P((in_port_t));
-u_int16_t	bswap16 __P((u_int16_t));
-u_int32_t	bswap32 __P((u_int32_t));
-u_int64_t	bswap64 __P((u_int64_t));
 __END_DECLS
 
 /*
--- sys/lib/libkern/bswap64.c
+++ sys/lib/libkern/bswap64.c
@@ -1,27 +1,28 @@
-/*	$NetBSD: bswap64.c,v 1.1 1997/10/09 15:42:33 bouyer Exp $	*/
+/*  $NetBSD: bswap64.c,v 1.2 1999/01/15 13:31:28 bouyer Exp $    */
 
 /*
  * Written by Manuel Bouyer <bouyer@netbsd.org>.
  * Public domain.
  */
 
+#include <sys/cdefs.h>
 #if defined(LIBC_SCCS) && !defined(lint)
-static char *rcsid = "$NetBSD: bswap64.c,v 1.1 1997/10/09 15:42:33 bouyer Exp $";
-#endif
+__RCSID("$NetBSD: bswap64.c,v 1.2 1999/01/15 13:31:28 bouyer Exp $");
+#endif /* LIBC_SCCS and not lint */
 
 #include <sys/types.h>
+#include <machine/bswap.h>
 
 #undef bswap64
 
 u_int64_t
 bswap64(x)
-    u_int64_t x;
-{  
+	u_int64_t x;
+{
 	u_int32_t *p = (u_int32_t*)&x;
 	u_int32_t t;
 	t = bswap32(p[0]);
 	p[0] = bswap32(p[1]);
 	p[1] = t;
 	return x;
-}   
-
+}
