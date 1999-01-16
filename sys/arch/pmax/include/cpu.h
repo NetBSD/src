56563146ae4627c1d452aeae3a7ecb608533e656
nisimura <nisimura@NetBSD.org>
1999-01-16 02:36:01 +0000

    - Fixup for recent change in arch/mips.

--- sys/arch/pmax/include/cpu.h
+++ sys/arch/pmax/include/cpu.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: cpu.h,v 1.18 1998/03/25 08:35:39 jonathan Exp $	*/
+/*	$NetBSD: cpu.h,v 1.19 1999/01/16 02:36:01 nisimura Exp $	*/
 
 #ifndef __PMAX_CPU_H
 #define __PMAX_CPU_H
@@ -7,7 +7,11 @@
  * pmax uses standard mips1 convention, wiring FPU to hard interupt 5.
  */
 #include <mips/cpu.h>
-#include <mips/cpuregs.h> /* XXX */
+#include <mips/cpuregs.h>
 
 #define MIPS_INT_MASK_FPU	MIPS_INT_MASK_5
+
+#define	INT_MASK_REAL_DEV	(MIPS_HARD_INT_MASK &~ MIPS_INT_MASK_5)
+#define	INT_MASK_FPU_DEAL	MIPS_INT_MASK_5
+
 #endif __PMAX_CPU_H
