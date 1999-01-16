6c669c430b6e47e633a15c6569340bcebfe8e7a2
nisimura <nisimura>
1999-01-16 08:26:24 +0000

    - Add two macros to deal with a recent change in mips/trap.c.

--- sys/arch/newsmips/include/cpu.h
+++ sys/arch/newsmips/include/cpu.h
@@ -1,6 +1,14 @@
-/*	$NetBSD: cpu.h,v 1.2 1998/03/26 13:15:01 tsubai Exp $	*/
+/*	$NetBSD: cpu.h,v 1.3 1999/01/16 08:26:24 nisimura Exp $	*/
+
+#ifndef	_MACHINE_CPU_H_
+#define	_MACHINE_CPU_H_
 
 #include <mips/cpu.h>
-#include <mips/cpuregs.h> /* XXX */
+#include <mips/cpuregs.h>
 
 #define MIPS_INT_MASK_FPU	MIPS_INT_MASK_3
+
+#define	INT_MASK_REAL_DEV	(MIPS_HARD_INT_MASK &~ MIPS_INT_MASK_3)
+#define	INT_MASK_FPU_DEAL	MIPS_INT_MASK_3
+
+#endif	_MACHINE_CPU_H_
