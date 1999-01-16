9e5dea5ee62cc2548e43dde5a174bc401aa5e0a5
simonb <simonb@NetBSD.org>
1999-01-16 05:25:06 +0000

    Apply previous NetBSD patch: 	wouldn't compile due to clash with type of (undeclared) strlen 	on alpha.  Fixed by including string.h.

--- gnu/dist/bc/lib/getopt.c
+++ gnu/dist/bc/lib/getopt.c
@@ -159,13 +159,14 @@ static enum
 /* Value of POSIXLY_CORRECT environment variable.  */
 static char *posixly_correct;
 
-#ifdef	__GNU_LIBRARY__
+#if defined(__GNU_LIBRARY__)  || defined(__NetBSD__)
 /* We want to avoid inclusion of string.h with non-GNU libraries
    because there are many ways it can cause trouble.
    On some systems, it contains special magic macros that don't work
    in GCC.  */
 #include <string.h>
 #define	my_index	strchr
+#include <stdlib.h>
 #else
 
 /* Avoid depending on library functions or files
