129838ea42d21b9dbc838693c9bed192f38d2b7e
thorpej <thorpej@NetBSD.org>
1999-01-22 02:36:12 +0000

    Throw out the old ypserv access control list code.  Instead, use libwrap for access control.  Add a -l option, to enable logging of all requests. Also, just use syslog(3) for all logging, not home-grown logging code.

--- usr.sbin/ypserv/ypserv/ypserv.c
+++ usr.sbin/ypserv/ypserv/ypserv.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: ypserv.c,v 1.7 1997/10/15 05:01:24 lukem Exp $	*/
+/*	$NetBSD: ypserv.c,v 1.8 1999/01/22 02:36:13 thorpej Exp $	*/
 
 /*
  * Copyright (c) 1994 Mats O Jansson <moj@stacken.kth.se>
@@ -33,7 +33,7 @@
 
 #include <sys/cdefs.h>
 #ifndef lint
-__RCSID("$NetBSD: ypserv.c,v 1.7 1997/10/15 05:01:24 lukem Exp $");
+__RCSID("$NetBSD: ypserv.c,v 1.8 1999/01/22 02:36:13 thorpej Exp $");
 #endif
 
 #include <sys/types.h>
@@ -57,10 +57,19 @@ __RCSID("$NetBSD: ypserv.c,v 1.7 1997/10/15 05:01:24 lukem Exp $");
 
 #include <rpcsvc/yp_prot.h>
 
-#include "acl.h"
-#include "yplog.h"
 #include "ypdef.h"
 #include "ypserv.h"
+
+#ifdef LIBWRAP
+#include <tcpd.h>
+
+int allow_severity = LOG_DAEMON | LOG_INFO;
+int deny_severity = LOG_DAEMON | LOG_WARNING;
+
+/* XXX For ypserv_proc.c -- NOT THREAD SAFE!  (like any of this code is) */
+const char *clientstr;
+const char *svcname;
+#endif /* LIBWRAP */
 
 #ifdef __STDC__
 #define SIG_PF void(*)(int)
@@ -76,7 +85,9 @@ static int _rpcfdtype;		/* Whether Stream or Datagram ? */
 static int _rpcsvcdirty;	/* Still serving ? */
 
 int	usedns;
-char	*aclfile;
+#ifdef LIBWRAP
+int	lflag;
+#endif
 
 extern	char *__progname;	/* from crt0.s */
 
@@ -140,79 +151,105 @@ ypprog_2(struct svc_req *rqstp, SVCXPRT *transp)
 	char *result;
 	xdrproc_t xdr_argument, xdr_result;
 	void *(*local) __P((void *, struct svc_req *));
+#ifdef LIBWRAP
+	struct request_info req;
+	struct sockaddr_in *caller;
+#define	SVCNAME(x)	svcname = x
+#else
+#define	SVCNAME(x)	/* nothing */
+#endif
 
 	_rpcsvcdirty = 1;
+
+#ifdef LIBWRAP
+	caller = svc_getcaller(transp);
+	request_init(&req, RQ_DAEMON, __progname, RQ_CLIENT_SIN, caller, NULL);
+	sock_methods(&req);
+#endif
+
 	switch (rqstp->rq_proc) {
 	case YPPROC_NULL:
 		xdr_argument = xdr_void;
 		xdr_result = xdr_void;
 		local = ypproc_null_2_svc;
+		SVCNAME("null_2");
 		break;
 
 	case YPPROC_DOMAIN:
 		xdr_argument = xdr_ypdomain_wrap_string;
 		xdr_result = xdr_bool;
 		local = ypproc_domain_2_svc;
+		SVCNAME("domain_2");
 		break;
 
 	case YPPROC_DOMAIN_NONACK:
 		xdr_argument = xdr_ypdomain_wrap_string;
 		xdr_result = xdr_bool;
 		local = ypproc_domain_nonack_2_svc;
+		SVCNAME("domain_nonack_2");
 		break;
 
 	case YPPROC_MATCH:
 		xdr_argument = xdr_ypreq_key;
 		xdr_result = xdr_ypresp_val;
 		local = ypproc_match_2_svc;
+		SVCNAME("match_2");
 		break;
 
 	case YPPROC_FIRST:
 		xdr_argument = xdr_ypreq_nokey;
 		xdr_result = xdr_ypresp_key_val;
 		local = ypproc_first_2_svc;
+		SVCNAME("first_2");
 		break;
 
 	case YPPROC_NEXT:
 		xdr_argument = xdr_ypreq_key;
 		xdr_result = xdr_ypresp_key_val;
 		local = ypproc_next_2_svc;
+		SVCNAME("next_2");
 		break;
 
 	case YPPROC_XFR:
 		xdr_argument = xdr_ypreq_xfr;
 		xdr_result = xdr_ypresp_xfr;
 		local = ypproc_xfr_2_svc;
+		SVCNAME("xfer_2");
 		break;
 
 	case YPPROC_CLEAR:
 		xdr_argument = xdr_void;
 		xdr_result = xdr_void;
 		local = ypproc_clear_2_svc;
+		SVCNAME("clear_2");
 		break;
 
 	case YPPROC_ALL:
 		xdr_argument = xdr_ypreq_nokey;
 		xdr_result = xdr_ypresp_all;
 		local = ypproc_all_2_svc;
+		SVCNAME("all_2");
 		break;
 
 	case YPPROC_MASTER:
 		xdr_argument = xdr_ypreq_nokey;
 		xdr_result = xdr_ypresp_master;
 		local = ypproc_master_2_svc;
+		SVCNAME("master_2");
 		break;
 
 	case YPPROC_ORDER:
 		xdr_argument = xdr_ypreq_nokey;
 		xdr_result = xdr_ypresp_order;
 		local = ypproc_order_2_svc;
+		SVCNAME("order_2");
 		break;
 
 	case YPPROC_MAPLIST:
 		xdr_argument = xdr_ypdomain_wrap_string;
 		xdr_result = xdr_ypresp_maplist;
 		local = ypproc_maplist_2_svc;
+		SVCNAME("maplist_2");
 		break;
 
 	default:
@@ -220,6 +257,19 @@ ypprog_2(struct svc_req *rqstp, SVCXPRT *transp)
 		_rpcsvcdirty = 0;
 		return;
 	}
+
+#ifdef LIBWRAP
+	clientstr = eval_client(&req);
+
+	if (hosts_access(&req) == 0) {
+		syslog(deny_severity,
+		    "%s: refused request from %.500s", svcname, clientstr);
+		svcerr_auth(transp, AUTH_FAILED);
+		_rpcsvcdirty = 0;
+		return;
+	}
+#endif
+
 	(void) memset((char *)&argument, 0, sizeof (argument));
 	if (!svc_getargs(transp, xdr_argument, (caddr_t) &argument)) {
 		svcerr_decode(transp);
@@ -268,46 +318,46 @@ main(argc, argv)
 	SVCXPRT *transp;
 	int sock, proto;
 	struct sigaction sa;
-	int xflag = 0;
 	int ch;
 
 	transp = NULL;		/* XXX gcc -Wuninitialized */
 	proto = 0;		/* XXX gcc -Wuninitialized */
 
-	while ((ch = getopt(argc, argv, "a:dx")) != -1) {
-		switch (ch) {
-		case 'a':
-			aclfile = optarg;
-			break;
+#ifdef LIBWRAP
+#define	GETOPTSTR	"dl"
+#else
+#define	GETOPTSTR	"d"
+#endif
 
+	while ((ch = getopt(argc, argv, GETOPTSTR)) != -1) {
+		switch (ch) {
 		case 'd':
 			usedns = 1;
 			break;
 
-		case 'x':
-			xflag = 1;
+#ifdef LIBWRAP
+		case 'l':
+			lflag = 1;
 			break;
-
+#endif
 		default:
 			usage();
 		}
 	}
 
+#undef GETOPTSTR
+
 	/* This program must be run by root. */
 	if (geteuid() != 0)
 		errx(1, "must run as root");
 
-	/* Deal with the acl file. */
-	acl_parse(aclfile);
-
-	if (xflag)
-		exit(1);
-
 #ifndef RPC_SVC_FG
 	if (daemon(0, 0))
 		err(1, "can't detach");
-	openlog("ypserv", LOG_PID, LOG_DAEMON);
 #endif
+
+	openlog(__progname, LOG_PID, LOG_DAEMON);
+	syslog(LOG_INFO, "starting");
 
 	{
 		FILE *pidfile = fopen(YPSERV_PID_PATH, "w");
@@ -315,23 +365,28 @@ main(argc, argv)
 		if (pidfile != NULL) {
 			fprintf(pidfile, "%d\n", getpid());
 			fclose(pidfile);
-		} else
-			err(1, "can't write PID file");
+		} else {
+			_msgout("can't write PID file: %m");
+			exit(1);
+		}
 	}
 
 	sock = RPC_ANYSOCK;
 	(void) pmap_unset(YPPROG, YPVERS);
 	(void) pmap_unset(YPPROG, YPVERS_ORIG);
 
-	ypopenlog();	/* open log file */
 	ypdb_init();	/* init db stuff */
 
 	sa.sa_handler = sighandler;
 	sa.sa_flags = 0;
-	if (sigemptyset(&sa.sa_mask))
-		err(1, "sigemptyset");
-	if (sigaction(SIGCHLD, &sa, NULL) || sigaction(SIGHUP, &sa, NULL))
-		err(1, "sigaction");
+	if (sigemptyset(&sa.sa_mask)) {
+		_msgout("sigemptyset: %m");
+		exit(1);
+	}
+	if (sigaction(SIGCHLD, &sa, NULL)) {
+		_msgout("sigaction: %m");
+		exit(1);
+	}
 
 	if ((_rpcfdtype == 0) || (_rpcfdtype == SOCK_DGRAM)) {
 		transp = svcudp_create(sock);
@@ -403,13 +458,6 @@ sighandler(sig)
 	int sig;
 {
 
-	if (sig == SIGHUP) {
-		acl_reset();
-		yplog("reread %s", aclfile ? aclfile : YP_SECURENET_FILE);
-		acl_parse(aclfile);
-		return;
-	}
-
 	/* SIGCHLD */
 	while (wait3((int *)NULL, WNOHANG, (struct rusage *)NULL) > 0);
 }
@@ -418,8 +466,16 @@ void
 usage()
 {
 
-	fprintf(stderr, "usage: %s [-a aclfile] [-d] [-x]\n", __progname);
+#ifdef LIBWRAP
+#define	USAGESTR	"usage: %s [-d] [-l]\n"
+#else
+#define	USAGESTR	"usage: %s [-d]\n"
+#endif
+
+	fprintf(stderr, USAGESTR, __progname);
 	exit(1);
+
+#undef USAGESTR
 }
 
 
