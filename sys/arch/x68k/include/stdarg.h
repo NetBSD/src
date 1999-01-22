940fa927abbf521c4071c956a8f2df18454042d7
mycroft <mycroft@NetBSD.org>
1999-01-22 14:12:07 +0000

    Standardize format.

--- sys/arch/x68k/include/stdarg.h
+++ sys/arch/x68k/include/stdarg.h
@@ -1,4 +1,3 @@
-/*	$NetBSD: stdarg.h,v 1.1.1.1 1996/05/05 12:17:03 oki Exp $	*/
+/*	$NetBSD: stdarg.h,v 1.2 1999/01/22 14:12:07 mycroft Exp $	*/
 
-/* Just use the common m68k definition */
 #include <m68k/stdarg.h>
