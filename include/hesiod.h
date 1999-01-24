65fe79dd7b339eb48742962b9da4d3db937183d9
lukem <lukem>
1999-01-24 23:53:18 +0000

    merge in stuff from hesiod3.0.2 distribution

--- include/hesiod.h
+++ include/hesiod.h
@@ -1,34 +1,63 @@
-/*	$NetBSD: hesiod.h,v 1.2 1999/01/15 12:28:26 lukem Exp $	*/
+/*	$NetBSD: hesiod.h,v 1.3 1999/01/24 23:53:18 lukem Exp $	*/
 
-/* This file contains definitions for use by the Hesiod name service and
- * applications.
+/*-
+ * Copyright (c) 1997, 1998, 1999 The NetBSD Foundation, Inc.
+ * All rights reserved.
  *
- *  Copyright (C) 1989 by the Massachusetts Institute of Technology
- * 
- *    Export of software employing encryption from the United States of
- *    America is assumed to require a specific license from the United
- *    States Government.  It is the responsibility of any person or
- *    organization contemplating export to obtain such a license before
- *    exporting.
- * 
- * WITHIN THAT CONSTRAINT, permission to use, copy, modify, and
- * distribute this software and its documentation for any purpose and
- * without fee is hereby granted, provided that the above copyright
- * notice appear in all copies and that both that copyright notice and
- * this permission notice appear in supporting documentation, and that
- * the name of M.I.T. not be used in advertising or publicity pertaining
- * to distribution of the software without specific, written prior
- * permission.  M.I.T. makes no representations about the suitability of
- * this software for any purpose.  It is provided "as is" without express
- * or implied warranty.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *        This product includes software developed by the NetBSD
+ *        Foundation, Inc. and its contributors.
+ * 4. Neither the name of The NetBSD Foundation nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
  *
- * Original version by Steve Dyer, IBM/Project Athena.
- *
+ * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
  */
+
+/*
+ * Copyright (c) 1996 by Internet Software Consortium.
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND INTERNET SOFTWARE CONSORTIUM DISCLAIMS
+ * ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL INTERNET SOFTWARE
+ * CONSORTIUM BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL
+ * DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR
+ * PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS
+ * ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS
+ * SOFTWARE.
+ */
+
 #ifndef _HESIOD_H_
 #define _HESIOD_H_
 
-/* Configuration information. */
+	/* Application-visible indication that we have the new interfaces */
+
+#define HESIOD_INTERFACES
+
+	/* Configuration information. */
 
 #ifndef _PATH_HESIOD_CONF			/* Configuration file. */
 #define _PATH_HESIOD_CONF	"/etc/hesiod.conf"
@@ -37,7 +66,7 @@
 #define DEF_RHS		""			/* Defaults if HESIOD_CONF */
 #define DEF_LHS		""			/*    file is not present. */
 
-/* Error codes. */
+	/* Error codes (for backwards compatibility) */
 
 #define	HES_ER_UNINIT	-1	/* uninitialized */
 #define	HES_ER_OK	0	/* no error */
@@ -45,14 +74,23 @@
 #define HES_ER_CONFIG	2	/* local problem (no config file?) */
 #define HES_ER_NET	3	/* network problem */
 
-/* Declaration of routines */
+	/* Declaration of routines */
+
 #include <sys/cdefs.h>
 
 __BEGIN_DECLS
-char	 *hes_to_bind	__P((char *, char *));
-char	**hes_resolve	__P((char *, char *));
-int	  hes_error	__P((void));
-void	  hes_free	__P((char **));
+int	hesiod_init	__P((void **));
+char  **hesiod_resolve	__P((void *, const char *, const char *));
+void	hesiod_free_list __P((void *, char **));
+char   *hesiod_to_bind	__P((void *, const char *, const char *));
+void	hesiod_end	__P((void *));
+
+				/* backwards compatibility */
+int	hes_init	__P((void));
+char   *hes_to_bind	__P((const char *, const char *));
+char  **hes_resolve	__P((const char *, const char *));
+int	hes_error	__P((void));
+void	hes_free	__P((char **));
 __END_DECLS
 
 #endif /* ! _HESIOD_H_ */
