357baa58c75ea5918ee81a6b3b0e8c9765199fd2
christos <christos>
1999-01-31 21:16:46 +0000

    Default to elf if the object format says so.

--- gnu/usr.bin/ld.new/_sparc.mk
+++ gnu/usr.bin/ld.new/_sparc.mk
@@ -1,4 +1,8 @@
-#	$NetBSD: _sparc.mk,v 1.2 1998/12/17 13:59:05 mrg Exp $
+#	$NetBSD: _sparc.mk,v 1.3 1999/01/31 21:16:46 christos Exp $
 
 EMULS=		sparcnbsd elf32_sparc sun4
+.if ${OBJECT_FMT} == "ELF"
+DEFAULT_EMUL=	elf32_sparc
+.else
 DEFAULT_EMUL=	sparcnbsd
+.endif
