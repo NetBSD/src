83d09787dea140639c0163144f1ba1b1c2360b12
christos <christos@NetBSD.org>
1999-01-31 21:45:40 +0000

    Choose the elf template if DEFAULT_ELF is defined.

--- gnu/usr.bin/egcs/arch/sparc/tm.h
+++ gnu/usr.bin/egcs/arch/sparc/tm.h
@@ -1,3 +1,7 @@
-/*	$NetBSD: tm.h,v 1.2 1998/08/17 03:35:58 tv Exp $	*/
+/*	$NetBSD: tm.h,v 1.3 1999/01/31 21:45:40 christos Exp $	*/
 
+#ifdef DEFAULT_ELF
 #include "sparc/netbsd.h"
+#else
+#include "sparc/netbsd.h"
+#endif
