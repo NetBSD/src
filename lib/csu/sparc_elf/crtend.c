dec420218e2157ff4e631da7dc99ae6bda0fdb96
christos <christos@NetBSD.org>
1999-01-31 21:15:31 +0000

    Elf startup for sparc.

---
+++ lib/csu/sparc_elf/crtend.c
@@ -0,0 +1,10 @@
+/*	$NetBSD: crtend.c,v 1.1 1999/01/31 21:15:31 christos Exp $	*/
+
+#include <sys/cdefs.h>
+
+static void (*__CTOR_LIST__[1]) __P((void))
+    __attribute__((__unused__))
+    __attribute__((section(".ctors"))) = { (void *)0 };		/* XXX */
+static void (*__DTOR_LIST__[1]) __P((void))
+    __attribute__((__unused__))
+    __attribute__((section(".dtors"))) = { (void *)0 };		/* XXX */
