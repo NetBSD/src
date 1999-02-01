9ec936af1b21376d2169ac0351cbdb4e5b29bb4c
cgd <cgd>
1999-02-01 07:00:28 +0000

    pull up revs 1.15-1.16 from trunk (PR#6862).  (mycroft)

--- sys/arch/m68k/include/stdarg.h
+++ sys/arch/m68k/include/stdarg.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: stdarg.h,v 1.14 1995/12/25 23:15:33 mycroft Exp $	*/
+/*	$NetBSD: stdarg.h,v 1.14.16.1 1999/02/01 07:00:28 cgd Exp $	*/
 
 /*-
  * Copyright (c) 1991, 1993
@@ -46,14 +46,14 @@ typedef _BSD_VA_LIST_	va_list;
 	(((sizeof(type) + sizeof(long) - 1) / sizeof(long)) * sizeof(long))
 
 #define	va_start(ap, last) \
-	((ap) = (va_list)&(last) + __va_size(last))
+	((ap) = (va_list)__builtin_next_arg(last))
 
 #define	va_arg(ap, type) \
-	(*(type *)((ap) += __va_size(type),			\
-		   (ap) - (sizeof(type) < sizeof(long) &&	\
-			   sizeof(type) != __va_size(type) ?	\
-			   sizeof(type) : __va_size(type))))
+	(*(type *)(void *)((ap) += __va_size(type),			\
+			   (ap) - (sizeof(type) < sizeof(long) &&	\
+				   sizeof(type) != __va_size(type) ?	\
+				   sizeof(type) : __va_size(type))))
 
-#define	va_end(ap)	((void)0)
+#define	va_end(ap)	
 
 #endif /* !_M68K_STDARG_H_ */
