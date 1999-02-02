ab1544559b3df095a5022f561209b8cdcce93ff4
fair <fair@NetBSD.org>
1999-02-02 04:57:11 +0000

    Bracket calls to Debugger() with #ifdef DDB (won't compile otherwise; the prototype isn't there without DDB).

--- sys/arch/sun3/dev/si.c
+++ sys/arch/sun3/dev/si.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: si.c,v 1.42 1998/12/23 04:53:44 fair Exp $	*/
+/*	$NetBSD: si.c,v 1.43 1999/02/02 04:57:11 fair Exp $	*/
 
 /*-
  * Copyright (c) 1996 The NetBSD Foundation, Inc.
@@ -221,7 +221,9 @@ si_minphys(struct buf *bp)
 #ifdef	DEBUG
 		if (si_debug) {
 			printf("si_minphys len = 0x%lx.\n", bp->b_bcount);
+#ifdef DDB
 			Debugger();
+#endif
 		}
 #endif
 		bp->b_bcount = MAX_DMA_LEN;
@@ -269,7 +271,9 @@ si_intr(void *arg)
 		if (!claimed) {
 			printf("si_intr: spurious from SBC\n");
 			if (si_debug & 4) {
+#ifdef	DDB
 				Debugger();	/* XXX */
+#endif
 			}
 		}
 #endif
@@ -327,7 +331,9 @@ si_dma_alloc(ncr_sc)
 	 */
 	if (xlen > MAX_DMA_LEN) {
 		printf("si_dma_alloc: excessive xlen=0x%x\n", xlen);
+#ifdef	DDB
 		Debugger();
+#endif
 		ncr_sc->sc_datalen = xlen = MAX_DMA_LEN;
 	}
 
@@ -455,4 +461,3 @@ si_dma_poll(ncr_sc)
 	}
 #endif
 }
-
