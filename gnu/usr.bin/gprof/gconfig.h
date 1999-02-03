74505a7712f89e92791d6f7370c4aadb21b8fe39
tv <tv@NetBSD.org>
1999-02-03 00:15:09 +0000

    Update to 2.9.1.

---
+++ gnu/usr.bin/gprof/gconfig.h
@@ -0,0 +1,16 @@
+/* gconfig.h.  Generated automatically by configure.  */
+/* gconfig.in.  Generated automatically from configure.in by autoheader.  */
+
+/* Define if you need to in order for stat and other things to work.  */
+/* #undef _POSIX_SOURCE */
+
+/* Name of package.  */
+#define PACKAGE "gprof"
+
+/* Version of package.  */
+#define VERSION "2.9.1"
+
+/* Whether this system uses the BSD 4.4 gmon.out format.  */
+#ifndef __ELF__
+#define BSD44_FORMAT 1
+#endif
