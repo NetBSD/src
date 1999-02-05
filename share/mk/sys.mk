9dc5e7f5a7cd8b082f76ec8d1d59d44440d6fac7
simonb <simonb@NetBSD.org>
1999-02-05 13:50:09 +0000

    _Don't_ use -O2 on mips, libc/stdlib/strtod.c breaks

--- share/mk/sys.mk
+++ share/mk/sys.mk
@@ -1,4 +1,4 @@
-#	$NetBSD: sys.mk,v 1.49 1999/01/21 09:44:19 mycroft Exp $
+#	$NetBSD: sys.mk,v 1.50 1999/02/05 13:50:09 simonb Exp $
 #	@(#)sys.mk	8.2 (Berkeley) 3/21/94
 
 unix?=		We run NetBSD.
@@ -22,8 +22,7 @@ CC?=		cc
 .if (${MACHINE_ARCH} == "arm32") || \
     (${MACHINE_ARCH} == "i386") || \
     (${MACHINE_ARCH} == "m68k") || \
-    (${MACHINE_ARCH} == "sparc") || \
-    (${MACHINE_ARCH} == "mips")
+    (${MACHINE_ARCH} == "sparc")
 CFLAGS?=	-O2
 .else
 CFLAGS?=	-O
