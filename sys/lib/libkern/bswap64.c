8abbf33e2783818cc55d1f6609453d1caa1743fa
bouyer <bouyer@NetBSD.org>
1999-02-05 16:34:13 +0000

    Sync with libc version.

--- sys/lib/libkern/bswap64.c
+++ sys/lib/libkern/bswap64.c
@@ -1,4 +1,4 @@
-/*  $NetBSD: bswap64.c,v 1.2 1999/01/15 13:31:28 bouyer Exp $    */
+/*  $NetBSD: bswap64.c,v 1.3 1999/02/05 16:34:13 bouyer Exp $    */
 
 /*
  * Written by Manuel Bouyer <bouyer@netbsd.org>.
@@ -7,7 +7,7 @@
 
 #include <sys/cdefs.h>
 #if defined(LIBC_SCCS) && !defined(lint)
-__RCSID("$NetBSD: bswap64.c,v 1.2 1999/01/15 13:31:28 bouyer Exp $");
+__RCSID("$NetBSD: bswap64.c,v 1.3 1999/02/05 16:34:13 bouyer Exp $");
 #endif /* LIBC_SCCS and not lint */
 
 #include <sys/types.h>
@@ -19,7 +19,7 @@ u_int64_t
 bswap64(x)
 	u_int64_t x;
 {
-	u_int32_t *p = (u_int32_t*)&x;
+	u_int32_t *p = (u_int32_t*)(void *)&x;
 	u_int32_t t;
 	t = bswap32(p[0]);
 	p[0] = bswap32(p[1]);
