6827e6b579756e54b48fd29b3b8a3a921601ee32
simonb <simonb@NetBSD.org>
1999-02-09 12:38:36 +0000

    Import GNU grep 2.2f

---
+++ gnu/dist/grep/src/btowc.c
@@ -0,0 +1,52 @@
+/* Copyright (C) 1996, 1997, 1998 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+   Contributed by Ulrich Drepper, <drepper@gnu.ai.mit.edu>
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public License as
+   published by the Free Software Foundation; either version 2 of the
+   License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public
+   License along with the GNU C Library; see the file COPYING.LIB.  If not,
+   write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.  */
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <stdio.h>
+
+#if ! defined(HAVE_WCHAR_H) || defined(__CYGWIN__)
+typedef unsigned int wint_t;
+# undef WEOF
+# define WEOF ((wint_t)-1)
+#else
+#include <wchar.h>
+#endif
+
+#ifndef weak_alias
+#  define __btowc btowc
+#endif
+
+/* We use UTF8 encoding for multibyte strings and therefore a valid
+   one byte multibyte string only can have a value from 0 to 0x7f.  */
+wint_t
+__btowc (c)
+     int c;
+{
+  if (WEOF != (wint_t) EOF || c < 0 || c > 0x7f)
+    return WEOF;
+  else
+    return (wint_t) c;
+}
+
+#ifdef weak_alias
+weak_alias (__btowc, btowc)
+#endif
---
+++ gnu/dist/grep/src/grepmat.c
@@ -0,0 +1,4 @@
+#include <config.h>
+#include "system.h"
+#include "grep.h"
+char const default_matcher[] = "grep";
