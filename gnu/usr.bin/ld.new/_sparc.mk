fcf8fdfecab190a1e8633f67f60ed6ad435f1e24
tv <tv@NetBSD.org>
1999-02-09 17:39:31 +0000

    - Use the stuff in src/gnu/lib/libbfd - Descend into ld.new on all systems to build ldscripts - Build ld.new if (i386 || sparc) && ELF - Merge separate _*.mk files into one Makefile - Depend ldemul-list.h on Makefile

--- gnu/usr.bin/ld.new/_sparc.mk
+++
@@ -1,8 +0,0 @@
-#	$NetBSD: _sparc.mk,v 1.3 1999/01/31 21:16:46 christos Exp $
-
-EMULS=		sparcnbsd elf32_sparc sun4
-.if ${OBJECT_FMT} == "ELF"
-DEFAULT_EMUL=	elf32_sparc
-.else
-DEFAULT_EMUL=	sparcnbsd
-.endif
