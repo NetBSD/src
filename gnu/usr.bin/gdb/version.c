60ede05d9b8ddd9f0c22fbbfe131051b310ca099
tv <tv@NetBSD.org>
1999-02-10 22:04:51 +0000

    Update build to 4.17.

--- gnu/usr.bin/gdb/version.c
+++ gnu/usr.bin/gdb/version.c
@@ -1,5 +1,5 @@
-/*	$NetBSD: version.c,v 1.1.1.1 1997/09/26 04:37:02 gwr Exp $	*/
+/*	$NetBSD: version.c,v 1.2 1999/02/10 22:04:54 tv Exp $	*/
 
-char *version = "4.16";
+char *version = "4.17";
 char *host_name = HOST_NAME;
 char *target_name = TARGET_NAME;
