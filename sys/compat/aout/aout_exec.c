5392ccd0fa051b647e9ca79c00a3b242d9dea476
christos <christos@NetBSD.org>
1999-02-11 09:41:34 +0000

    A.out compatibility mode; basically /emul/aout/ checking for syscalls that use pathnames.

---
+++ sys/compat/aout/aout_exec.c
@@ -0,0 +1,82 @@
+/*	$NetBSD: aout_exec.c,v 1.1 1999/02/11 09:41:34 christos Exp $	*/
+
+/*-
+ * Copyright (c) 1998 The NetBSD Foundation, Inc.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to The NetBSD Foundation
+ * by Christos Zoulas.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *        This product includes software developed by the NetBSD
+ *        Foundation, Inc. and its contributors.
+ * 4. Neither the name of The NetBSD Foundation nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <sys/param.h>
+#include <sys/systm.h>
+#include <sys/proc.h>
+#include <sys/exec.h>
+#include <sys/signalvar.h>
+
+#include <compat/aout/aout_exec.h>
+#include <compat/aout/aout_syscall.h>
+ 
+const char aout_emul_path[] = "/emul/aout";
+extern struct sysent aout_sysent[];
+#ifdef SYSCALL_DEBUG
+extern char *aout_syscallnames[];
+#endif
+extern char sigcode[], esigcode[];
+
+
+struct emul emul_netbsd_aout = {
+	"netbsd",
+	NULL,
+	sendsig,
+	AOUT_SYS_syscall,
+	AOUT_SYS_MAXSYSCALL,
+	aout_sysent,
+#ifdef SYSCALL_DEBUG
+	aout_syscallnames,
+#else
+	NULL,
+#endif
+	0,
+	copyargs,
+	setregs,
+	sigcode,
+	esigcode,
+};
+
+int
+exec_aoutcompat_makecmds(p, epp)
+	struct proc *p;
+	struct exec_package *epp;
+{
+	epp->ep_emul = &emul_netbsd_aout;
+	return exec_aout_makecmds(p, epp);
+}
