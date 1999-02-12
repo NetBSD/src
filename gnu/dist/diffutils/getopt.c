5a0825daa7fc7e2bb983bf971964aff297624fde
tv <tv@NetBSD.org>
1999-02-12 12:44:58 +0000

    Add some patches, delete Infodoc, and delete files not used for a NetBSD build.

--- gnu/dist/diffutils/getopt.c
+++ gnu/dist/diffutils/getopt.c
@@ -39,6 +39,8 @@
 #endif
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /* Comment out all this code if we are using the GNU C Library, and are not
    actually compiling the library itself.  This code is part of the GNU C
