26475619ed15fdce4c7b6ad4ef91f3c76d283dc8
christos <christos@NetBSD.org>
1999-03-01 16:40:07 +0000

    KNF

--- libexec/ld.elf_so/rtld.h
+++ libexec/ld.elf_so/rtld.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: rtld.h,v 1.7 1999/02/24 18:31:00 christos Exp $	*/
+/*	$NetBSD: rtld.h,v 1.8 1999/03/01 16:40:07 christos Exp $	 */
 
 /*
  * Copyright 1996 John D. Polstra.
@@ -31,8 +31,8 @@
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifndef RTLD_H /* { */
-#define RTLD_H 1
+#ifndef RTLD_H
+#define RTLD_H
 
 #include <stddef.h>
 #include <sys/param.h>
@@ -57,6 +57,7 @@
 #  define PAGESIZE	8192	/* NPBG is not constant! */
 # endif
 #endif
+
 #define round_down(x)	((x) & ~(PAGESIZE-1))
 #define round_up(x)	round_down((x) + PAGESIZE - 1)
 
@@ -74,16 +75,16 @@ typedef unsigned char bool;
 struct Struct_Obj_Entry;
 
 typedef struct Struct_Needed_Entry {
-    struct Struct_Needed_Entry *next;
-    struct Struct_Obj_Entry *obj;
-    unsigned long name;		/* Offset of name in string table */
-} Needed_Entry;
+	struct Struct_Needed_Entry *next;
+	struct Struct_Obj_Entry *obj;
+	unsigned long   name;	/* Offset of name in string table */
+}               Needed_Entry;
 
 typedef struct _rtld_search_path_t {
-    struct _rtld_search_path_t *sp_next;
-    const char *sp_path;
-    size_t sp_pathlen;
-} Search_Path;
+	struct _rtld_search_path_t *sp_next;
+	const char     *sp_path;
+	size_t          sp_pathlen;
+}               Search_Path;
 
 /*
  * Shared object descriptor.
@@ -96,68 +97,72 @@ typedef struct _rtld_search_path_t {
 #define RTLD_VERSION	1
 
 typedef struct Struct_Obj_Entry {
-    Elf32_Word magic;		/* Magic number (sanity check) */
-    Elf32_Word version;		/* Version number of struct format */
+	Elf32_Word      magic;		/* Magic number (sanity check) */
+	Elf32_Word      version;	/* Version number of struct format */
 
-    struct Struct_Obj_Entry *next;
-    char *path;			/* Pathname of underlying file (%) */
-    int refcount;
-    int dl_refcount;		/* Number of times loaded by dlopen */
+	struct Struct_Obj_Entry *next;
+	char           *path;		/* Pathname of underlying file (%) */
+	int             refcount;
+	int             dl_refcount;	/* Number of times loaded by dlopen */
 
-    /* These items are computed by map_object() or by digest_phdr(). */
-    caddr_t mapbase;		/* Base address of mapped region */
-    size_t mapsize;		/* Size of mapped region in bytes */
-    size_t textsize;		/* Size of text segment in bytes */
-    Elf_Addr vaddrbase;		/* Base address in shared object file */
-    caddr_t relocbase;		/* Relocation constant = mapbase - vaddrbase */
-    Elf_Dyn *dynamic;		/* Dynamic section */
-    caddr_t entry;		/* Entry point */
-    const Elf_Phdr *phdr;	/* Program header if it is mapped, else NULL */
-    size_t phsize;		/* Size of program header in bytes */
+	/* These items are computed by map_object() or by digest_phdr(). */
+	caddr_t         mapbase;	/* Base address of mapped region */
+	size_t          mapsize;	/* Size of mapped region in bytes */
+	size_t          textsize;	/* Size of text segment in bytes */
+	Elf_Addr        vaddrbase;	/* Base address in shared object file */
+	caddr_t         relocbase;	/* Reloc const = mapbase - *vaddrbase */
+	Elf_Dyn        *dynamic;	/* Dynamic section */
+	caddr_t         entry;		/* Entry point */
+	const Elf_Phdr *phdr;		/* Program header if mapped, ow NULL */
+	size_t          phsize;		/* Size of program header in bytes */
 
-    /* Items from the dynamic section. */
-    Elf_Addr *pltgot;		/* PLTGOT table */
-    const Elf_Rel *rel;		/* Relocation entries */
-    const Elf_Rel *rellim;	/* Limit of Relocation entries */
-    const Elf_RelA *rela;	/* Relocation entries */
-    const Elf_RelA *relalim;	/* Limit of Relocation entries */
-    const Elf_Rel *pltrel;	/* PLT relocation entries */
-    const Elf_Rel *pltrellim;	/* Limit of PLT relocation entries */
-    const Elf_RelA *pltrela;	/* PLT relocation entries */
-    const Elf_RelA *pltrelalim;	/* Limit of PLT relocation entries */
-    const Elf_Sym *symtab;	/* Symbol table */
-    const char *strtab;		/* String table */
-    unsigned long strsize;	/* Size in bytes of string table */
+	/* Items from the dynamic section. */
+	Elf_Addr       *pltgot;		/* PLTGOT table */
+	const Elf_Rel  *rel;		/* Relocation entries */
+	const Elf_Rel  *rellim;		/* Limit of Relocation entries */
+	const Elf_RelA *rela;		/* Relocation entries */
+	const Elf_RelA *relalim;	/* Limit of Relocation entries */
+	const Elf_Rel  *pltrel;		/* PLT relocation entries */
+	const Elf_Rel  *pltrellim;	/* Limit of PLT relocation entries */
+	const Elf_RelA *pltrela;	/* PLT relocation entries */
+	const Elf_RelA *pltrelalim;	/* Limit of PLT relocation entries */
+	const Elf_Sym  *symtab;		/* Symbol table */
+	const char     *strtab;		/* String table */
+	unsigned long   strsize;	/* Size in bytes of string table */
 #if defined(__mips__)
-    Elf_Word local_gotno;	/* Number of local GOT entries */
-    Elf_Word symtabno;		/* Number of dynamic symbols */
-    Elf_Word gotsym;		/* First dynamic symbol in GOT */
+	Elf_Word        local_gotno;	/* Number of local GOT entries */
+	Elf_Word        symtabno;	/* Number of dynamic symbols */
+	Elf_Word        gotsym;		/* First dynamic symbol in GOT */
 #endif
 
-    const Elf_Word *buckets;	/* Hash table buckets array */
-    unsigned long nbuckets;	/* Number of buckets */
-    const Elf_Word *chains;	/* Hash table chain array */
-    unsigned long nchains;	/* Number of chains */
+	const Elf_Word *buckets;	/* Hash table buckets array */
+	unsigned long   nbuckets;	/* Number of buckets */
+	const Elf_Word *chains;		/* Hash table chain array */
+	unsigned long   nchains;	/* Number of chains */
 
-    Search_Path *rpaths;	/* Search path specified in object */
-    Needed_Entry *needed;	/* Shared objects needed by this one (%) */
+	Search_Path    *rpaths;		/* Search path specified in object */
+	Needed_Entry   *needed;		/* Shared objects needed by this (%) */
 
-    void (*init)(void);		/* Initialization function to call */
-    void (*fini)(void);		/* Termination function to call */
+	void            (*init) 	/* Initialization function to call */
+	    __P((void));
+	void            (*fini)		/* Termination function to call */
+	    __P((void));
 
-    /* Entry points for dlopen() and friends. */
-    void *(*dlopen)(const char *, int);
-    void *(*dlsym)(void *, const char *);
-    char *(*dlerror)(void);
-    int (*dlclose)(void *);
+	/* Entry points for dlopen() and friends. */
+	void           *(*dlopen) __P((const char *, int));
+	void           *(*dlsym) __P((void *, const char *));
+	char           *(*dlerror) __P((void));
+	int             (*dlclose) __P((void *));
 
-    int mainprog : 1;		/* True if this is the main program */
-    int rtld : 1;		/* True if this is the dynamic linker */
-    int textrel : 1;		/* True if there are relocations to text seg */
-    int symbolic : 1;		/* True if generated with "-Bsymbolic" */
-    int printed : 1;		/* True if ldd has printed it */
+	int             mainprog:1;	/* True if this is the main program */
+	int             rtld:1;		/* True if this is the dynamic linker */
+	int             textrel:1;	/* True if there are relocations to
+					 * text seg */
+	int             symbolic:1;	/* True if generated with
+					 * "-Bsymbolic" */
+	int             printed:1;	/* True if ldd has printed it */
 
-    struct link_map linkmap;	/* for GDB */
+	struct link_map linkmap;	/* for GDB */
 } Obj_Entry;
 
 extern struct r_debug _rtld_debug;
@@ -169,71 +174,63 @@ extern bool _rtld_trust;
 extern const char *_rtld_error_message;
 
 /* rtld_start.S */
-extern void _rtld_bind_start(void);
+void _rtld_bind_start __P((void));
 
 /* rtld.c */
-
-extern void _rtld_error(const char *, ...);
-extern void _rtld_die(void);
-
-extern char *_rtld_dlerror(void);
-extern void *_rtld_dlopen(const char *, int);
-extern void *_rtld_dlsym(void *, const char *);
-extern int _rtld_dlclose(void *);
-
-extern void _rtld_debug_state(void);
-
-extern void _rtld_linkmap_add(Obj_Entry *);
-extern void _rtld_linkmap_delete(Obj_Entry *);
+void _rtld_error __P((const char *, ...));
+void _rtld_die __P((void));
+char *_rtld_dlerror __P((void));
+void *_rtld_dlopen __P((const char *, int));
+void *_rtld_dlsym __P((void *, const char *));
+int _rtld_dlclose __P((void *));
+void _rtld_debug_state __P((void));
+void _rtld_linkmap_add __P((Obj_Entry *));
+void _rtld_linkmap_delete __P((Obj_Entry *));
 
 /* headers.c */
-
-extern void _rtld_digest_dynamic(Obj_Entry *);
-extern Obj_Entry *_rtld_digest_phdr(const Elf_Phdr *, int, caddr_t);
+void _rtld_digest_dynamic __P((Obj_Entry *));
+Obj_Entry *_rtld_digest_phdr __P((const Elf_Phdr *, int, caddr_t));
 
 /* load.c */
-
-extern Obj_Entry *_rtld_load_object(char *path, bool);
-extern int _rtld_load_needed_objects(Obj_Entry *);
+Obj_Entry *_rtld_load_object __P((char *, bool));
+int _rtld_load_needed_objects __P((Obj_Entry *));
 
 /* path.c */
-
-extern void _rtld_add_paths(Search_Path **, const char *, bool);
+void _rtld_add_paths __P((Search_Path **, const char *, bool));
 
 /* reloc.c */
-extern int _rtld_do_copy_relocations(const Obj_Entry *, bool);
-extern caddr_t _rtld_bind(const Obj_Entry *, Elf_Word);
-extern int _rtld_relocate_objects(Obj_Entry *, bool, bool);
-extern int _rtld_relocate_nonplt_object(const Obj_Entry *, const Elf_RelA *,
-    bool);
-extern int _rtld_relocate_plt_object(const Obj_Entry *, const Elf_RelA *,
-    caddr_t *, bool, bool);
+int _rtld_do_copy_relocations __P((const Obj_Entry *, bool));
+caddr_t _rtld_bind __P((const Obj_Entry *, Elf_Word));
+int _rtld_relocate_objects __P((Obj_Entry *, bool, bool));
+int _rtld_relocate_nonplt_object __P((const Obj_Entry *,
+    const Elf_RelA *, bool));
+int _rtld_relocate_plt_object __P((const Obj_Entry *, const Elf_RelA *,
+    caddr_t *, bool, bool));
 
 /* search.c */
-
-extern char *_rtld_find_library(const char *, const Obj_Entry *);
+char *_rtld_find_library __P((const char *, const Obj_Entry *));
 
 /* symbol.c */
-extern unsigned long _rtld_elf_hash(const char *);
-extern const Elf_Sym *_rtld_symlook_obj(const char *, unsigned long,
-    const Obj_Entry *, bool);
-extern const Elf_Sym *_rtld_find_symdef(const Obj_Entry *, Elf_Word,
-    const char *, const Obj_Entry *, const Obj_Entry **, bool);
+unsigned long _rtld_elf_hash __P((const char *));
+const Elf_Sym *_rtld_symlook_obj __P((const char *, unsigned long,
+    const Obj_Entry *, bool));
+const Elf_Sym *_rtld_find_symdef __P((const Obj_Entry *, Elf_Word,
+    const char *, const Obj_Entry *, const Obj_Entry **, bool));
 
 /* map_object.c */
-extern Obj_Entry *_rtld_map_object(const char *, int);
+Obj_Entry *_rtld_map_object __P((const char *, int));
 
 #if defined(__mips__)
 /* mips_reloc.c */
-extern void _rtld_relocate_mips_got(Obj_Entry *);
-extern caddr_t _rtld_bind_mips (Elf_Word, Elf_Addr, Elf_Addr, Elf_Addr);
+void _rtld_relocate_mips_got __P((Obj_Entry *));
+caddr_t _rtld_bind_mips __P((Elf_Word, Elf_Addr, Elf_Addr, Elf_Addr));
 #endif
 
 #if defined(__powerpc__)
 /* ppc_reloc.c */
-extern caddr_t _rtld_bind_powerpc(const Obj_Entry *, Elf_Word);
-extern int _rtld_reloc_powerpc_plt(const Obj_Entry *, const Elf_RelA *, bool);
-extern void _rtld_setup_powerpc_plt(const Obj_Entry *);
+caddr_t _rtld_bind_powerpc __P((const Obj_Entry *, Elf_Word));
+int _rtld_reloc_powerpc_plt __P((const Obj_Entry *, const Elf_RelA *, bool));
+void _rtld_setup_powerpc_plt __P((const Obj_Entry *));
 #endif
 
-#endif /* } */
+#endif
