ac3620b27527c51644d09cac47998a14c65c92a2
ross <ross>
1999-03-01 07:58:56 +0000

    Make this build on alpha. Still doesn't work well, though, and now it can't read corefiles.

--- gnu/dist/gdb/config/alpha/nm-nbsd.h
+++ gnu/dist/gdb/config/alpha/nm-nbsd.h
@@ -19,7 +19,9 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
 
 /* Need to define this before including the common nm-nbsd.h. */
-#define SVR4_SHARED_LIBS
+#ifndef	SVR4_SHARED_LIBS
+#define	SVR4_SHARED_LIBS
+#endif	SVR4_SHARED_LIBS
 
 /* Get generic NetBSD native definitions. */
 #include "nm-nbsd.h"
