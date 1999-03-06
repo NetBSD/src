b84d3c2c85bed1e93e713902328f31ba7bdff4fb
mycroft <mycroft>
1999-03-06 00:19:07 +0000

    Add ARGSUSED for main().

--- usr.bin/lex/libmain.c
+++ usr.bin/lex/libmain.c
@@ -1,12 +1,13 @@
 /* libmain - flex run-time support library "main" function */
 
-/* $NetBSD: libmain.c,v 1.5 1998/01/05 05:15:53 perry Exp $ */
+/* $NetBSD: libmain.c,v 1.6 1999/03/06 00:19:07 mycroft Exp $ */
 
 #include <sys/cdefs.h>
 
 int yylex __P((void));
 int main __P((int, char **, char **));
 
+/*ARGSUSED*/
 int
 main( argc, argv, envp )
 int argc;
