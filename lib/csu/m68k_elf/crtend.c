f23c8fc16a3b4a4a4804509d5bfcb7273d935a66
kleink <kleink@NetBSD.org>
1999-03-08 11:03:43 +0000

    Snapshot.

---
+++ lib/csu/m68k_elf/crtend.c
@@ -0,0 +1,10 @@
+/*	$NetBSD: crtend.c,v 1.1 1999/03/08 11:03:43 kleink Exp $	*/
+
+#include <sys/cdefs.h>
+
+static void (*__CTOR_LIST__[1]) __P((void))
+    __attribute__((__unused__))
+    __attribute__((section(".ctors"))) = { (void *)0 };		/* XXX */
+static void (*__DTOR_LIST__[1]) __P((void))
+    __attribute__((__unused__))
+    __attribute__((section(".dtors"))) = { (void *)0 };		/* XXX */
