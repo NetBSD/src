e4ab71a76091d3da1342d921a6b1e0bf1db59db5
perseant <perseant@NetBSD.org>
1999-03-10 00:20:00 +0000

    New sources should leave the LFS in a more-or-less working state.  Changes include:
    	- DIROP segregation is enabled, and greater care is taken
    	  to make sure that a checkpoint completes.  Fsck is not
    	  needed to remount the filesystem.
    	- Several checks to make sure that the LFS subsystem does not
    	  overuse various resources (memory, in particular).
    	- The cleaner routines, lfs_markv in particular, are completely
    	  rewritten.  A buffer overflow is removed.  Greater care is taken
    	  to ensure that inodes come from where lfs_cleanerd say they come
    	  from (so we know nothing has changed since lfs_bmapv was called).
    	- Fragment allocation is fixed, so that writes beyond end-of-file
    	  do the right thing.

--- sys/ufs/lfs/lfs_balloc.c
+++ sys/ufs/lfs/lfs_balloc.c
@@ -1,5 +1,40 @@
-/*	$NetBSD: lfs_balloc.c,v 1.10 1998/11/09 01:18:34 mycroft Exp $	*/
+/*	$NetBSD: lfs_balloc.c,v 1.11 1999/03/10 00:20:00 perseant Exp $	*/
 
+/*-
+ * Copyright (c) 1999 The NetBSD Foundation, Inc.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to The NetBSD Foundation
+ * by Konrad E. Schroder <perseant@hhhh.org>.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *      This product includes software developed by the NetBSD
+ *      Foundation, Inc. and its contributors.
+ * 4. Neither the name of The NetBSD Foundation nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 /*
  * Copyright (c) 1989, 1991, 1993
  *	The Regents of the University of California.  All rights reserved.
@@ -37,6 +72,7 @@
 
 #if defined(_KERNEL) && !defined(_LKM)
 #include "opt_quota.h"
+#include "opt_uvm.h"
 #endif
 
 #include <sys/param.h>
@@ -76,37 +112,43 @@ lfs_balloc(vp, offset, iosize, lbn, bpp)
 	struct lfs *fs;
 	struct indir indirs[NIADDR+2];
 	ufs_daddr_t	daddr, lastblock;
- 	int bb;		/* number of disk blocks in a block disk blocks */
- 	int error, frags, i, nsize, osize, num;
-
+	int bb;		/* number of disk blocks in a block disk blocks */
+	int error, frags, i, nsize, osize, num;
+	
 	ip = VTOI(vp);
 	fs = ip->i_lfs;
-
+	
+#ifdef DEBUG
+	if(!VOP_ISLOCKED(vp)) {
+		printf("lfs_balloc: warning: ino %d not locked\n",ip->i_number);
+	}
+#endif
+	
 	/* 
 	 * Three cases: it's a block beyond the end of file, it's a block in
 	 * the file that may or may not have been assigned a disk address or
 	 * we're writing an entire block.  Note, if the daddr is unassigned,
 	 * the block might still have existed in the cache (if it was read
-	 * or written earlier).  If it did, make sure we don't count it as a
-	 * new block or zero out its contents.  If it did not, make sure
+	 * or written earlier).	 If it did, make sure we don't count it as a
+	 * new block or zero out its contents.	If it did not, make sure
 	 * we allocate any necessary indirect blocks.
 	 * If we are writing a block beyond the end of the file, we need to
-	 * check if the old last block was a fragment.  If it was, we need
+	 * check if the old last block was a fragment.	If it was, we need
 	 * to rewrite it.
 	 */
-
+	
 	*bpp = NULL;
 	error = ufs_bmaparray(vp, lbn, &daddr, &indirs[0], &num, NULL );
 	if (error)
 		return (error);
-
+	
 	/* Check for block beyond end of file and fragment extension needed. */
 	lastblock = lblkno(fs, ip->i_ffs_size);
 	if (lastblock < NDADDR && lastblock < lbn) {
 		osize = blksize(fs, ip, lastblock);
 		if (osize < fs->lfs_bsize && osize > 0) {
 			if ((error = lfs_fragextend(vp, osize, fs->lfs_bsize,
-			    lastblock, &bp)))
+						    lastblock, &bp)))
 				return(error);
 			ip->i_ffs_size = (lastblock + 1) * fs->lfs_bsize;
 #if defined(UVM)
@@ -118,14 +160,14 @@ lfs_balloc(vp, offset, iosize, lbn, bpp)
 			VOP_BWRITE(bp);
 		}
 	}
-
+	
 	bb = VFSTOUFS(vp->v_mount)->um_seqinc;
 	if (daddr == UNASSIGNED)
 		/* May need to allocate indirect blocks */
 		for (i = 1; i < num; ++i)
 			if (!indirs[i].in_exists) {
 				ibp = getblk(vp, indirs[i].in_lbn, fs->lfs_bsize,
-				    0, 0);
+					     0, 0);
 				if ((ibp->b_flags & (B_DONE | B_DELWRI))) 
 					panic ("Indirect block should not exist");
 
@@ -141,7 +183,7 @@ lfs_balloc(vp, offset, iosize, lbn, bpp)
 						return(error);
 				}
 			}
-
+	
 	/*
 	 * If the block we are writing is a direct block, it's the last
 	 * block in the file, and offset + iosize is less than a full
@@ -150,38 +192,45 @@ lfs_balloc(vp, offset, iosize, lbn, bpp)
 	 * size or it already exists and contains some fragments and
 	 * may need to extend it.
 	 */
-	if (lbn < NDADDR && lblkno(fs, ip->i_ffs_size) == lbn) {
+	if (lbn < NDADDR && lblkno(fs, ip->i_ffs_size) <= lbn) {
+		osize = blksize(fs, ip, lbn);
 		nsize = fragroundup(fs, offset + iosize);
 		frags = numfrags(fs, nsize);
 		bb = fragstodb(fs, frags);
-		if (lblktosize(fs, lbn) == ip->i_ffs_size)
+		if (lblktosize(fs, lbn) >= ip->i_ffs_size)
 			/* Brand new block or fragment */
 			*bpp = bp = getblk(vp, lbn, nsize, 0, 0);
 		else {
-			/* Extend existing block */
-			if ((error =
-			    lfs_fragextend(vp, (int)blksize(fs, ip, lbn), 
-			    nsize, lbn, &bp)))
-				return(error);
+			if (nsize <= osize) {
+				/* No need to extend */
+				/* XXX KS - Are we wasting space? */
+				if ((error = bread(vp, lbn, osize, NOCRED, &bp)))
+					return error;
+			} else {
+				/* Extend existing block */
+				if ((error =
+				     lfs_fragextend(vp, osize, nsize, lbn, &bp)))
+					return(error);
+			}
 			*bpp = bp;
 		}
 	} else {
 		/*
 		 * Get the existing block from the cache either because the
-		 * block is 1) not a direct block or because it's not the last
+		 * block 1) is not a direct block or 2) is not the last
 		 * block in the file.
 		 */
 		frags = dbtofrags(fs, bb);
 		*bpp = bp = getblk(vp, lbn, blksize(fs, ip, lbn), 0, 0);
 	}
-
+	
 	/* 
 	 * The block we are writing may be a brand new block
 	 * in which case we need to do accounting (i.e. check
 	 * for free space and update the inode number of blocks.
 	 */
 	if (!(bp->b_flags & (B_DONE | B_DELWRI))) {
-		if (daddr == UNASSIGNED) 
+		if (daddr == UNASSIGNED)
 			if (!ISSPACE(fs, bb, curproc->p_ucred)) {
 				bp->b_flags |= B_INVAL;
 				brelse(bp);
@@ -206,6 +255,7 @@ lfs_balloc(vp, offset, iosize, lbn, bpp)
 			return(biowait(bp));
 		}
 	}
+	
 	return (0);
 }
 
@@ -221,14 +271,16 @@ lfs_fragextend(vp, osize, nsize, lbn, bpp)
 	struct lfs *fs;
 	long bb;
 	int error;
+	extern long locked_queue_bytes;
 
 	ip = VTOI(vp);
 	fs = ip->i_lfs;
+	
 	bb = (long)fragstodb(fs, numfrags(fs, nsize - osize));
+ top:
 	if (!ISSPACE(fs, bb, curproc->p_ucred)) {
 		return(ENOSPC);
 	}
-
 	if ((error = bread(vp, lbn, osize, NOCRED, bpp))) {
 		brelse(*bpp);
 		return(error);
@@ -239,10 +291,23 @@ lfs_fragextend(vp, osize, nsize, lbn, bpp)
 		return (error);
 	}
 #endif
+	/*
+	 * XXX - KS - Don't change size while we're gathered, as we could
+	 * then overlap another buffer in lfs_writeseg.
+	 */
+	if((*bpp)->b_flags & B_GATHERED) {
+		(*bpp)->b_flags |= B_NEEDCOMMIT; /* XXX KS - what flag to use? */
+		brelse(*bpp);
+		tsleep(*bpp, (PRIBIO+1), "lfs_fragextend", 0);
+		goto top;
+	}
 	ip->i_ffs_blocks += bb;
 	ip->i_flag |= IN_CHANGE | IN_UPDATE;
 	fs->lfs_bfree -= fragstodb(fs, numfrags(fs, (nsize - osize)));
+	if((*bpp)->b_flags & B_LOCKED)
+		locked_queue_bytes += (nsize - osize);
 	allocbuf(*bpp, nsize);
 	bzero((char *)((*bpp)->b_data) + osize, (u_int)(nsize - osize));
+	
 	return(0);
 }
--- sys/ufs/lfs/lfs_extern.h
+++ sys/ufs/lfs/lfs_extern.h
@@ -1,5 +1,40 @@
-/*	$NetBSD: lfs_extern.h,v 1.12 1999/02/26 23:44:49 wrstuden Exp $	*/
+/*	$NetBSD: lfs_extern.h,v 1.13 1999/03/10 00:20:00 perseant Exp $	*/
 
+/*-
+ * Copyright (c) 1999 The NetBSD Foundation, Inc.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to The NetBSD Foundation
+ * by Konrad E. Schroder <perseant@hhhh.org>.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *      This product includes software developed by the NetBSD
+ *      Foundation, Inc. and its contributors.
+ * 4. Neither the name of The NetBSD Foundation nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 /*-
  * Copyright (c) 1991, 1993, 1994
  *	The Regents of the University of California.  All rights reserved.
@@ -35,6 +70,23 @@
  *	@(#)lfs_extern.h	8.6 (Berkeley) 5/8/95
  */
 
+/*
+ * Sysctl values for LFS.
+ */
+#define LFS_WRITEINDIR	 1 /* flush indirect blocks on non-checkpoint writes */
+#define LFS_CLEAN_VNHEAD 2 /* put prev unrefed cleaned vnodes on head of free list */
+#define LFS_DOSTATS      3
+#define LFS_STATS        4
+#define LFS_MAXID	 5
+
+#define LFS_NAMES { \
+	{ 0, 0 }, \
+	{ "flushindir", CTLTYPE_INT }, \
+	{ "clean_vnhead", CTLTYPE_INT }, \
+	{ "dostats", CTLTYPE_INT }, \
+	{ "stats", CTLTYPE_STRUCT }, \
+}
+
 struct fid;
 struct mount;
 struct nameidata;
@@ -62,8 +114,11 @@ int lfs_vcreate __P((struct mount *, ino_t, struct vnode **));
 int lfs_balloc __P((struct vnode *, int, u_long, ufs_daddr_t, struct buf **));
 
 /* lfs_bio.c */
-void lfs_flush __P((void));
-int lfs_check __P((struct vnode *, ufs_daddr_t));
+int lfs_bwrite_ext __P((struct buf *, int));
+void lfs_flush __P((struct lfs *, int));
+int lfs_check __P((struct vnode *, ufs_daddr_t, int));
+void lfs_freebuf __P((struct buf *));
+void lfs_countlocked __P((int *, long *));
 
 /* lfs_cksum.c */
 u_long cksum __P((void *, size_t));
@@ -73,25 +128,28 @@ u_long lfs_sb_cksum __P((struct dlfs *));
 #ifdef DEBUG
 void lfs_dump_super __P((struct lfs *));
 void lfs_dump_dinode __P((struct dinode *));
-#endif
+void lfs_check_bpp __P((struct lfs *, struct segment *, char *, int));
+void lfs_check_segsum __P((struct lfs *, struct segment *, char *, int));
+#endif /* DEBUG */
 
 /* lfs_inode.c */
 void lfs_init __P((void));
 struct dinode *lfs_ifind __P((struct lfs *, ino_t, struct dinode *));
 
 /* lfs_segment.c */
+void lfs_imtime __P((struct lfs *));
 int lfs_vflush __P((struct vnode *));
-void lfs_writevnodes __P((struct lfs *, struct mount *, struct segment *, int));
+int lfs_writevnodes __P((struct lfs *, struct mount *, struct segment *, int));
 int lfs_segwrite __P((struct mount *, int));
 void lfs_writefile __P((struct lfs *, struct segment *, struct vnode *));
 int lfs_writeinode __P((struct lfs *, struct segment *, struct inode *));
 int lfs_gatherblock __P((struct segment *, struct buf *, int *));
-void lfs_gather __P((struct lfs *, struct segment *, struct vnode *, int (*match )__P ((struct lfs *, struct buf *))));
+int lfs_gather __P((struct lfs *, struct segment *, struct vnode *, int (*match )__P ((struct lfs *, struct buf *))));
 void lfs_updatemeta __P((struct segment *));
 int lfs_initseg __P((struct lfs *));
 void lfs_newseg __P((struct lfs *));
 int lfs_writeseg __P((struct lfs *, struct segment *));
-void lfs_writesuper __P((struct lfs *));
+void lfs_writesuper __P((struct lfs *, daddr_t));
 int lfs_match_data __P((struct lfs *, struct buf *));
 int lfs_match_indir __P((struct lfs *, struct buf *));
 int lfs_match_dindir __P((struct lfs *, struct buf *));
@@ -102,13 +160,14 @@ void lfs_supercallback __P((struct buf *));
 void lfs_shellsort __P((struct buf **, ufs_daddr_t *, int));
 int lfs_vref __P((struct vnode *));
 void lfs_vunref __P((struct vnode *));
+void lfs_vunref_head __P((struct vnode *));
 
 /* lfs_subr.c */
 void lfs_seglock __P((struct lfs *, unsigned long));
 void lfs_segunlock __P((struct lfs *));
 
 /* lfs_syscalls.c */
-int lfs_fastvget __P((struct mount *, ino_t, ufs_daddr_t, struct vnode **, struct dinode *));
+int lfs_fastvget __P((struct mount *, ino_t, ufs_daddr_t, struct vnode **, struct dinode *, int *));
 struct buf *lfs_fakebuf __P((struct vnode *, int, size_t, caddr_t));
 
 /* lfs_vfsops.c */
@@ -146,10 +205,10 @@ int lfs_close		__P((void *));
 int lfs_inactive	__P((void *));
 int lfs_reclaim		__P((void *));
 int lfs_write		__P((void *));
+int lfs_whiteout	__P((void *));
 
 __END_DECLS
 extern int lfs_mount_type;
 extern int (**lfs_vnodeop_p) __P((void *));
 extern int (**lfs_specop_p) __P((void *));
 extern int (**lfs_fifoop_p) __P((void *));
-
