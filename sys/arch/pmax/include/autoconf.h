ab108682abc2d66f58ce4f5de4226f67e3a4879b
nisimura <nisimura@NetBSD.org>
1999-03-12 07:13:14 +0000

    - Nuke old leftover bogocities, round one; externs for non-existent functions, less-intuitive #define's in inappropriate header files.

--- sys/arch/pmax/include/autoconf.h
+++ sys/arch/pmax/include/autoconf.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: autoconf.h,v 1.9 1998/10/06 20:50:17 thorpej Exp $	*/
+/*	$NetBSD: autoconf.h,v 1.10 1999/03/12 07:13:15 nisimura Exp $	*/
 
 /*
  * Copyright (c) 1994, 1995 Carnegie-Mellon University.
@@ -35,29 +35,10 @@
 
 struct confargs;
 
-
 /* Handle device interrupt for  given unit of a driver */
 
-typedef void* intr_arg_t;		/* pointer to some softc */
+typedef void *intr_arg_t;		/* pointer to some softc */
 typedef int (*intr_handler_t) __P((intr_arg_t));
-/*
- * XXX Establish interrupt on an arbitrary decstation/decsystem bus.
- */
-extern void
-generic_intr_establish __P(( void * parent, void * cookie,
-			 int level,
-			 intr_handler_t handler, intr_arg_t arg));
-
-
-#define KN02_ASIC_NAME "KN02    "	/* ROM name in 3max system slot */
-	
-#define	INTR_ESTABLISH(parent, cookie, level, handler, val)			\
-    generic_intr_establish((parent), (cookie), (level), (handler), (val))
-
-#define	BUS_INTR_ESTABLISH(ca,  handler, val)			\
-    generic_intr_establish( ((struct device*)(val))->dv_parent, \
-    			   (void*)(ca)->ca_slotpri, 0, (handler), (val))
-
 
 struct confargs {
 	char	*ca_name;		/* Device name. */
@@ -67,11 +48,5 @@ struct confargs {
 	int	ca_slotpri;		/* Device interrupt "priority" */
 };
 
-extern caddr_t baseboard_cvtaddr __P((struct confargs *)); /*XXX*/
-
-#ifndef pmax
-void	set_clockintr __P((void (*)(struct clockframe *)));
-#endif
-void	set_iointr __P((void (*)(void *, int)));
-int	badaddr			__P((void *, u_int));
+int	badaddr	__P((void *, u_int));
 void	makebootdev __P((char *cp));
