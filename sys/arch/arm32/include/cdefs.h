3ddd340d8f2d0a27cf114ab8a92f1856117a5220
thorpej <thorpej@NetBSD.org>
1999-03-20 01:40:25 +0000

    Garbage-collect.

--- sys/arch/arm32/include/cdefs.h
+++ sys/arch/arm32/include/cdefs.h
@@ -1,37 +1,8 @@
-/*	$NetBSD: cdefs.h,v 1.6 1998/06/25 21:54:35 mark Exp $	*/
-
-/*
- * Written by J.T. Conklin <jtc@wimsey.com> 01/17/95.
- * Public domain.
- */
+/*	$NetBSD: cdefs.h,v 1.7 1999/03/20 01:40:26 thorpej Exp $	*/
 
 #ifndef	_MACHINE_CDEFS_H_
 #define	_MACHINE_CDEFS_H_
 
-#define	_C_LABEL(x)	__CONCAT(_,x)
-
-#ifdef __GNUC__
-#define	__RENAME(x)	__asm__(___STRING(_C_LABEL(x)))
-#endif
-
-#ifdef __GNUC__
-#ifdef __STDC__
-#define __indr_reference(sym,alias)	\
-	__asm__(".stabs \"_" #alias "\",11,0,0,0");	\
-	__asm__(".stabs \"_" #sym "\",1,0,0,0");
-#define __warn_references(sym,msg)	\
-	__asm__(".stabs \"" msg "\",30,0,0,0");		\
-	__asm__(".stabs \"_" #sym "\",1,0,0,0");
-#else /* __STDC__ */
-#define __indr_reference(sym,alias)	\
-	__asm__(".stabs \"_/**/alias\",11,0,0,0");	\
-	__asm__(".stabs \"_/**/sym\",1,0,0,0");
-#define __warn_references(sym,msg)	\
-	__asm__(".stabs msg,30,0,0,0");			\
-	__asm__(".stabs \"_/**/sym\",1,0,0,0");
-#endif /* __STDC__ */
-#else /* __GNUC__ */
-#define	__warn_references(sym,msg)
-#endif /* __GNUC__ */
+/* No arch-specific cdefs. */
 
 #endif /* !_MACHINE_CDEFS_H_ */
