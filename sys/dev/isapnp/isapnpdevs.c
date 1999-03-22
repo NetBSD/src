86e5f2abc90671fe7b29bd5327140a02fec76e04
mycroft <mycroft@NetBSD.org>
1999-03-22 09:41:58 +0000

    Regen.

--- sys/dev/isapnp/isapnpdevs.c
+++ sys/dev/isapnp/isapnpdevs.c
@@ -1,14 +1,14 @@
-/*	$NetBSD: isapnpdevs.c,v 1.24 1999/03/18 17:07:51 drochner Exp $	*/
+/*	$NetBSD: isapnpdevs.c,v 1.25 1999/03/22 09:41:58 mycroft Exp $	*/
 
 /*
  * THIS FILE AUTOMATICALLY GENERATED.  DO NOT EDIT.
  *
  * generated from:
- *	NetBSD: isapnpdevs,v 1.21 1999/03/18 17:07:09 drochner Exp 
+ *	NetBSD: isapnpdevs,v 1.22 1999/03/22 09:41:10 mycroft Exp 
  */
 
 /*-
- * Copyright (c) 1998 The NetBSD Foundation, Inc.
+ * Copyright (c) 1998, 1999 The NetBSD Foundation, Inc.
  * All rights reserved.
  *
  * This code is derived from software contributed to The NetBSD Foundation
@@ -47,218 +47,202 @@
 
 
 /* Adaptec SCSI */
-static const char *isapnp_aha_devlogic[] = {
-	"ADP1542",	/* Adaptec AHA-1542CP */
-	NULL
+static const struct isapnp_matchinfo isapnp_aha_devlogic[] = {
+	{"ADP1542", 0},	/* Adaptec AHA-1542CP */
 };
-static const char *isapnp_aha_devcompat[] = {
-	"PNP00A0",	/* Adaptec AHA-1542CP */
-	NULL
+static const struct isapnp_matchinfo isapnp_aha_devcompat[] = {
+	{"PNP00A0", 0},	/* Adaptec AHA-1542CP */
 };
 const struct isapnp_devinfo isapnp_aha_devinfo = {
-	isapnp_aha_devlogic, isapnp_aha_devcompat
+	isapnp_aha_devlogic, 1,
+	isapnp_aha_devcompat, 1,
 };
 
 /* Adaptec SCSI */
-static const char *isapnp_aic_devlogic[] = {
-	"ADP1520",	/* Adaptec AHA-1520B */
-	"ADP1505",	/* Adaptec AVA-1505A */
-	NULL
+static const struct isapnp_matchinfo isapnp_aic_devlogic[] = {
+	{"ADP1520", 0},	/* Adaptec AHA-1520B */
+	{"ADP1505", 0},	/* Adaptec AVA-1505A */
 };
-static const char *isapnp_aic_devcompat[] = {
-	"ADP1530",	/* (Adaptec AVA-1505A) */
-	NULL
+static const struct isapnp_matchinfo isapnp_aic_devcompat[] = {
+	{"ADP1530", 0},	/* (Adaptec AVA-1505A) */
 };
 const struct isapnp_devinfo isapnp_aic_devinfo = {
-	isapnp_aic_devlogic, isapnp_aic_devcompat
+	isapnp_aic_devlogic, 2,
+	isapnp_aic_devcompat, 1,
 };
 
 /* National Semiconductor Serial */
-static const char *isapnp_com_devlogic[] = {
-	"BDP3336",	/* Best Data Prods. 336F */
-	"OZO8039",	/* Zoom 56k flex */
-	"BRI1400",	/* Boca 33.6 PnP */
-	"BRIB400",	/* Boca 56k PnP */
-	"ROK0010",	/* Rockwell ? */
-	"USR0004",	/* USR Sportster 14.4k */
-	"USR0006",	/* USR Sportster 33.6k */
-	"USR2070",	/* USR Sportster 56k */
-	"USR3031",	/* USR 56k Faxmodem */
-	"ZTIF761",	/* Zoom ComStar 33.6 */
-	"CIR3000",	/* Cirrus Logic V43 */
-	"MOT0000",	/* Motorolla ModemSurfr */
-	NULL
+static const struct isapnp_matchinfo isapnp_com_devlogic[] = {
+	{"BDP3336", 0},	/* Best Data Prods. 336F */
+	{"OZO8039", 0},	/* Zoom 56k flex */
+	{"BRI1400", 0},	/* Boca 33.6 PnP */
+	{"BRIB400", 0},	/* Boca 56k PnP */
+	{"ROK0010", 0},	/* Rockwell ? */
+	{"USR0004", 0},	/* USR Sportster 14.4k */
+	{"USR0006", 0},	/* USR Sportster 33.6k */
+	{"USR2070", 0},	/* USR Sportster 56k */
+	{"USR3031", 0},	/* USR 56k Faxmodem */
+	{"ZTIF761", 0},	/* Zoom ComStar 33.6 */
+	{"CIR3000", 0},	/* Cirrus Logic V43 */
+	{"MOT0000", 0},	/* Motorola ModemSurfr */
 };
-static const char *isapnp_com_devcompat[] = {
-	"PNP0500",	/* Generic 8250/16450 */
-	"PNP0501",	/* Generic 16550A */
-	NULL
+static const struct isapnp_matchinfo isapnp_com_devcompat[] = {
+	{"PNP0500", 0},	/* Generic 8250/16450 */
+	{"PNP0501", 0},	/* Generic 16550A */
 };
 const struct isapnp_devinfo isapnp_com_devinfo = {
-	isapnp_com_devlogic, isapnp_com_devcompat
+	isapnp_com_devlogic, 12,
+	isapnp_com_devcompat, 2,
 };
 
 /* 3Com 3CXXX Ethernet */
-static const char *isapnp_ep_devlogic[] = {
-	"TCM5090",	/* 3Com 3c509B */
-	"TCM5091",	/* 3Com 3c509B-1 */
-	"TCM5094",	/* 3Com 3c509B-4 */
-	"TCM5095",	/* 3Com 3c509B-5 */
-	"TCM5098",	/* 3Com 3c509B-8 */
-	NULL
-};
-static const char *isapnp_ep_devcompat[] = {
-	NULL
+static const struct isapnp_matchinfo isapnp_ep_devlogic[] = {
+	{"TCM5090", 0},	/* 3Com 3c509B */
+	{"TCM5091", 0},	/* 3Com 3c509B-1 */
+	{"TCM5094", 0},	/* 3Com 3c509B-4 */
+	{"TCM5095", 0},	/* 3Com 3c509B-5 */
+	{"TCM5098", 0},	/* 3Com 3c509B-8 */
 };
 const struct isapnp_devinfo isapnp_ep_devinfo = {
-	isapnp_ep_devlogic, isapnp_ep_devcompat
+	isapnp_ep_devlogic, 5,
+	NULL, 0,
 };
 
 /* ESS Audio Drive */
-static const char *isapnp_ess_devlogic[] = {
-	NULL
-};
-static const char *isapnp_ess_devcompat[] = {
-	NULL
-};
 const struct isapnp_devinfo isapnp_ess_devinfo = {
-	isapnp_ess_devlogic, isapnp_ess_devcompat
+	NULL, 0,
+	NULL, 0,
 };
 
 /* Generic Joystick */
-static const char *isapnp_joy_devlogic[] = {
-	"CSC0001",	/* CS4235 */
-	"CSCA801",	/* Terratec EWS64XL */
-	"CTL7002",	/* Creative Vibra16CL */
-	"ESS0001",	/* ESS1868 */
-	"OPT0001",	/* OPTi Audio 16 */
-	"PNPB02F",	/* XXX broken GUS PnP */
-	"ASB16FD",	/* AdLib NSC 16 PNP */
-	NULL
+static const struct isapnp_matchinfo isapnp_joy_devlogic[] = {
+	{"AZT0003", 0},	/* Aztech AZT2320 GAME PORT */
+	{"CSC0001", 0},	/* CS4235 */
+	{"CSCA801", 0},	/* Terratec EWS64XL */
+	{"CTL7002", 0},	/* Creative Vibra16CL */
+	{"ESS0001", 0},	/* ESS1868 */
+	{"OPT0001", 0},	/* OPTi Audio 16 */
+	{"PNPB02F", 0},	/* XXX broken GUS PnP */
+	{"ASB16FD", 0},	/* AdLib NSC 16 PNP */
 };
-static const char *isapnp_joy_devcompat[] = {
-	"PNPB02F",	/* generic */
-	NULL
+static const struct isapnp_matchinfo isapnp_joy_devcompat[] = {
+	{"PNPB02F", 0},	/* generic */
 };
 const struct isapnp_devinfo isapnp_joy_devinfo = {
-	isapnp_joy_devlogic, isapnp_joy_devcompat
+	isapnp_joy_devlogic, 8,
+	isapnp_joy_devcompat, 1,
 };
 
 /* Gravis Ultrasound */
-static const char *isapnp_gus_devlogic[] = {
-	"GRV0000",	/* Gravis Ultrasound */
-	NULL
-};
-static const char *isapnp_gus_devcompat[] = {
-	NULL
+static const struct isapnp_matchinfo isapnp_gus_devlogic[] = {
+	{"GRV0000", 0},	/* Gravis Ultrasound */
 };
 const struct isapnp_devinfo isapnp_gus_devinfo = {
-	isapnp_gus_devlogic, isapnp_gus_devcompat
+	isapnp_gus_devlogic, 1,
+	NULL, 0,
 };
 
 /* Lance Ethernet */
-static const char *isapnp_le_devlogic[] = {
-	"TKN0010",	/* Lance Ethernet on TEKNOR board */
-	NULL
-};
-static const char *isapnp_le_devcompat[] = {
-	NULL
+static const struct isapnp_matchinfo isapnp_le_devlogic[] = {
+	{"TKN0010", 0},	/* Lance Ethernet on TEKNOR board */
 };
 const struct isapnp_devinfo isapnp_le_devinfo = {
-	isapnp_le_devlogic, isapnp_le_devcompat
+	isapnp_le_devlogic, 1,
+	NULL, 0,
+};
+
+/* MPU-401 MIDI UART */
+static const struct isapnp_matchinfo isapnp_mpu_devlogic[] = {
+	{"AZT0002", 0},	/* Aztech AZT2320 MPU401 MIDI */
+};
+const struct isapnp_devinfo isapnp_mpu_devinfo = {
+	isapnp_mpu_devlogic, 1,
+	NULL, 0,
 };
 
 /* NE2000 Ethernet */
-static const char *isapnp_ne_devlogic[] = {
-	"@@@1980",	/* OvisLink LE-8019R */
-	NULL
+static const struct isapnp_matchinfo isapnp_ne_devlogic[] = {
+	{"@@@1980", 0},	/* OvisLink LE-8019R */
 };
-static const char *isapnp_ne_devcompat[] = {
-	"PNP80D6",	/* Digital DE305 ISAPnP */
-	NULL
+static const struct isapnp_matchinfo isapnp_ne_devcompat[] = {
+	{"PNP80D6", 0},	/* Digital DE305 ISAPnP */
 };
 const struct isapnp_devinfo isapnp_ne_devinfo = {
-	isapnp_ne_devlogic, isapnp_ne_devcompat
+	isapnp_ne_devlogic, 1,
+	isapnp_ne_devcompat, 1,
 };
 
 /* PCMCIA bridge */
-static const char *isapnp_pcic_devlogic[] = {
-	"SCM0469",	/* SCM SwapBox Plug and Play */
-	"AEI0218",	/* Actiontec PnP PCMCIA Adapter */
-	NULL
+static const struct isapnp_matchinfo isapnp_pcic_devlogic[] = {
+	{"SCM0469", 0},	/* SCM SwapBox Plug and Play */
+	{"AEI0218", 0},	/* Actiontec PnP PCMCIA Adapter */
 };
-static const char *isapnp_pcic_devcompat[] = {
-	"PNP0E00",	/* PCIC Compatible PCMCIA Bridge */
-	NULL
+static const struct isapnp_matchinfo isapnp_pcic_devcompat[] = {
+	{"PNP0E00", 0},	/* PCIC Compatible PCMCIA Bridge */
 };
 const struct isapnp_devinfo isapnp_pcic_devinfo = {
-	isapnp_pcic_devlogic, isapnp_pcic_devcompat
+	isapnp_pcic_devlogic, 2,
+	isapnp_pcic_devcompat, 1,
 };
 
 /* Creative Soundblaster */
-static const char *isapnp_sb_devlogic[] = {
-	"ADS7150",	/* AD1815 */
-	"ADS7180",	/* AD1816 */
-	"CTL0001",	/* SB */
-	"CTL0031",	/* SB AWE32 */
-	"CTL0041",	/* SB16 PnP (CT4131) */
-	"CTL0043",	/* SB16 PnP (CT4170) */
-	"CTL0042",	/* SB AWE64 Value */
-	"CTL0044",	/* SB AWE64 Gold */
-	"CTL0045",	/* SB AWE64 Value */
-	"OPT9250",	/* Televideo card, Opti */
-	"@X@0001",	/* CMI8330. Audio Adapter */
-	"ESS1868",	/* ESS1868 */
-	"ESS1869",	/* ESS1869 */
-	NULL
+static const struct isapnp_matchinfo isapnp_sb_devlogic[] = {
+	{"ADS7150", 0},	/* AD1815 */
+	{"ADS7180", 0},	/* AD1816 */
+	{"CTL0001", 0},	/* SB */
+	{"CTL0031", 0},	/* SB AWE32 */
+	{"CTL0041", 0},	/* SB16 PnP (CT4131) */
+	{"CTL0043", 0},	/* SB16 PnP (CT4170) */
+	{"CTL0042", 0},	/* SB AWE64 Value */
+	{"CTL0044", 0},	/* SB AWE64 Gold */
+	{"CTL0045", 0},	/* SB AWE64 Value */
+	{"OPT9250", 0},	/* Televideo card, Opti */
+	{"@X@0001", 0},	/* CMI8330. Audio Adapter */
+	{"ESS1868", 0},	/* ESS1868 */
+	{"ESS1869", 0},	/* ESS1869 */
 };
-static const char *isapnp_sb_devcompat[] = {
-	"PNPB000",	/* Generic SB 1.5 */
-	"PNPB001",	/* Generic SB 2.0 */
-	"PNPB002",	/* Generic SB Pro */
-	"PNPB003",	/* Generic SB 16 */
-	NULL
+static const struct isapnp_matchinfo isapnp_sb_devcompat[] = {
+	{"PNPB000", 0},	/* Generic SB 1.5 */
+	{"PNPB001", 0},	/* Generic SB 2.0 */
+	{"PNPB002", 0},	/* Generic SB Pro */
+	{"PNPB003", 0},	/* Generic SB 16 */
 };
 const struct isapnp_devinfo isapnp_sb_devinfo = {
-	isapnp_sb_devlogic, isapnp_sb_devcompat
+	isapnp_sb_devlogic, 13,
+	isapnp_sb_devcompat, 4,
 };
 
 /* Western Digital Disk Controller */
-static const char *isapnp_wdc_devlogic[] = {
-	"OPT0007",	/* OPTi Audio 16 IDE controller */
-	NULL
+static const struct isapnp_matchinfo isapnp_wdc_devlogic[] = {
+	{"AZT0000", 0},	/* Aztech AZT2320 IDE CDROM */
+	{"OPT0007", 0},	/* OPTi Audio 16 IDE controller */
 };
-static const char *isapnp_wdc_devcompat[] = {
-	"PNP0600",	/* Western Digital Compatible Controller */
-	NULL
+static const struct isapnp_matchinfo isapnp_wdc_devcompat[] = {
+	{"PNP0600", 0},	/* Western Digital Compatible Controller */
 };
 const struct isapnp_devinfo isapnp_wdc_devinfo = {
-	isapnp_wdc_devlogic, isapnp_wdc_devcompat
+	isapnp_wdc_devlogic, 2,
+	isapnp_wdc_devcompat, 1,
 };
 
 /* Microsoft Sound System */
-static const char *isapnp_wss_devlogic[] = {
-	"CSC0000",	/* Windows Sound System */
-	"CSCA800",	/* Terratec EWS64 CoDec */
-	"ASB1611",	/* AdLib NSC 16 PNP */
-	NULL
-};
-static const char *isapnp_wss_devcompat[] = {
-	NULL
+static const struct isapnp_matchinfo isapnp_wss_devlogic[] = {
+	{"AZT0001", 1},	/* Aztech AZT2320 AUDIO */
+	{"CSC0000", 0},	/* Windows Sound System */
+	{"CSCA800", 0},	/* Terratec EWS64 CoDec */
+	{"ASB1611", 0},	/* AdLib NSC 16 PNP */
 };
 const struct isapnp_devinfo isapnp_wss_devinfo = {
-	isapnp_wss_devlogic, isapnp_wss_devcompat
+	isapnp_wss_devlogic, 4,
+	NULL, 0,
 };
 
 /* Yamaha Sound */
-static const char *isapnp_ym_devlogic[] = {
-	"YMH0021",	/* OPL3-SA2, OPL3-SA3 */
-	NULL
-};
-static const char *isapnp_ym_devcompat[] = {
-	NULL
+static const struct isapnp_matchinfo isapnp_ym_devlogic[] = {
+	{"YMH0021", 0},	/* OPL3-SA2, OPL3-SA3 */
 };
 const struct isapnp_devinfo isapnp_ym_devinfo = {
-	isapnp_ym_devlogic, isapnp_ym_devcompat
+	isapnp_ym_devlogic, 1,
+	NULL, 0,
 };
 
