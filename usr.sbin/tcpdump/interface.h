d1b189e80191fa4f60b6b13901ee05bac158a67e
bad <bad@NetBSD.org>
1999-03-23 00:02:24 +0000

    Make this understand Token-Ring. From Onno van der Linden.

--- usr.sbin/tcpdump/interface.h
+++ usr.sbin/tcpdump/interface.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: interface.h,v 1.6 1998/09/25 19:10:25 hwr Exp $	*/
+/*	$NetBSD: interface.h,v 1.7 1999/03/23 00:02:24 bad Exp $	*/
 
 /*
  * Copyright (c) 1988, 1989, 1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997
@@ -238,6 +238,8 @@ extern void snmp_print(const u_char *, u_int);
 extern void sunrpcrequest_print(const u_char *, u_int, const u_char *);
 extern void tcp_print(const u_char *, u_int, const u_char *);
 extern void tftp_print(const u_char *, u_int);
+extern void token_if_print(u_char *, const struct pcap_pkthdr *,
+	const u_char *);
 extern void udp_print(const u_char *, u_int, const u_char *);
 extern void wb_print(const void *, u_int);
 extern void hdlc_if_print(u_char *, const struct pcap_pkthdr *,
