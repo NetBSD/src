9abc6877266d0654f07e36571eae3c9b4da483e6
tv <tv@NetBSD.org>
1999-03-24 21:48:36 +0000

    Fix compile warning about redefinition of SVR4_SHARED_LIBS (which must be definable on the cc command line so that a.out/ELF switchable platforms work).

--- gnu/dist/gdb/config/mips/nm-nbsd.h
+++ gnu/dist/gdb/config/mips/nm-nbsd.h
@@ -19,7 +19,9 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
 
 /* Need to define this before including the common nm-nbsd.h. */
+#ifndef SVR4_SHARED_LIBS
 #define SVR4_SHARED_LIBS
+#endif
 
 /* Get generic NetBSD native definitions. */
 #include "nm-nbsd.h"
