f75ec26a426b9f8946ae57fbb373da40c2d7988c
tsubai <tsubai@NetBSD.org>
1999-03-26 07:03:01 +0000

    Define DB_ELF_SYMBOLS to compile. (currently /boot doesn't preserve symbols.)

--- sys/arch/newsmips/include/db_machdep.h
+++ sys/arch/newsmips/include/db_machdep.h
@@ -1,3 +1,6 @@
-/*	$NetBSD: db_machdep.h,v 1.1 1998/02/18 13:48:17 tsubai Exp $	*/
+/*	$NetBSD: db_machdep.h,v 1.2 1999/03/26 07:03:01 tsubai Exp $	*/
+
+#define DB_ELF_SYMBOLS
+#define DB_ELFSIZE 32
 
 #include <mips/db_machdep.h>
