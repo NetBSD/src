164141309de760f92f41598613801da3f8ef2269
ross <ross>
1999-03-27 09:01:27 +0000

    Bug fix for the bug fix. Be sure to prom_close() in bootxx and boot, but not in netboot, and not in diskclose(), but after trying all kernelnames[].

--- sys/arch/alpha/stand/common/common.h
+++ sys/arch/alpha/stand/common/common.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: common.h,v 1.3 1998/10/15 01:00:56 ross Exp $	*/
+/*	$NetBSD: common.h,v 1.4 1999/03/27 09:01:28 ross Exp $	*/
 
 void init_prom_calls __P((void));
 void OSFpal __P((void));
@@ -6,3 +6,4 @@ void halt __P((void));
 u_int64_t prom_dispatch __P((int, ...));
 int cpu_number __P((void));
 void switch_palcode __P((void));
+void close_primary_device __P((int));
