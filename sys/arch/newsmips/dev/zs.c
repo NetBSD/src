e2cefae5c16763aad92e173801907be7219c6009
wrstuden <wrstuden@NetBSD.org>
1999-03-27 01:21:36 +0000

    Add pps support. Only enable pps if CLOCAL & !MDMBUF.

--- sys/arch/newsmips/dev/zs.c
+++ sys/arch/newsmips/dev/zs.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: zs.c,v 1.6 1999/02/11 15:28:05 mycroft Exp $	*/
+/*	$NetBSD: zs.c,v 1.7 1999/03/27 01:21:36 wrstuden Exp $	*/
 
 /*-
  * Copyright (c) 1996 The NetBSD Foundation, Inc.
@@ -466,9 +466,12 @@ zs_set_modes(cs, cflag)
 	 * status interrupt to detect CTS changes.
 	 */
 	s = splzs();
-	if ((cflag & (CLOCAL | MDMBUF)) != 0)
+	cs->cs_rr0_pps = 0;
+	if ((cflag & (CLOCAL | MDMBUF)) != 0) {
 		cs->cs_rr0_dcd = 0;
-	else
+		if ((cflag & MDMBUF) == 0)
+			cs->cs_rr0_pps = ZSRR0_DCD;
+	} else
 		cs->cs_rr0_dcd = ZSRR0_DCD;
 	if ((cflag & CRTSCTS) != 0) {
 		cs->cs_wr5_dtr = ZSWR5_DTR;
