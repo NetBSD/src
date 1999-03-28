836564ee21310f49bec060e3ddedbc79fde4c687
is <is@NetBSD.org>
1999-03-28 20:09:09 +0000

    Add back the "810" id string.

--- sys/arch/amiga/dev/siop2.c
+++ sys/arch/amiga/dev/siop2.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: siop2.c,v 1.7 1999/03/28 19:39:24 is Exp $	*/
+/*	$NetBSD: siop2.c,v 1.8 1999/03/28 20:09:09 is Exp $	*/
 
 /*
  * Copyright (c) 1994,1998 Michael L. Hitch
@@ -150,7 +150,7 @@ void siopng_dump_trace __P((void));
 
 static char *siopng_chips[] = {
 	"720", "720SE", "770", "0x3",
-	"0x4", "0x5", "0x6", "0x7",
+	"810A", "0x5", "0x6", "0x7",
 	"0x8", "0x9", "0xA", "0xB",
 	"0xC", "0xD", "0xE", "0xF",
 };
