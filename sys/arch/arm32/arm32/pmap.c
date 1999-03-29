70951588165ff2ea89d9414636bbd1dc72e7e96a
mycroft <mycroft@NetBSD.org>
1999-03-29 07:15:06 +0000

    Nuke the contents of pmap_pageable().  It accomplishes nothing on this port, and it's potentially a serious bug.

--- sys/arch/arm32/arm32/pmap.c
+++ sys/arch/arm32/arm32/pmap.c
@@ -1,4 +1,40 @@
-/*	$NetBSD: pmap.c,v 1.55 1999/03/28 22:01:06 mycroft Exp $	*/
+/*	$NetBSD: pmap.c,v 1.56 1999/03/29 07:15:06 mycroft Exp $	*/
+
+/*-
+ * Copyright (c) 1999 The NetBSD Foundation, Inc.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to The NetBSD Foundation
+ * by Charles M. Hannum.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *        This product includes software developed by the NetBSD
+ *        Foundation, Inc. and its contributors.
+ * 4. Neither the name of The NetBSD Foundation nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 
 /*
  * Copyright (c) 1994-1998 Mark Brinicombe.
@@ -1295,36 +1331,6 @@ pmap_pageable(pmap, sva, eva, pageable)
 	vm_offset_t eva;
 	boolean_t pageable;
 {
-	/*
-	 * Ok we can only make the specified pages pageable under the
-	 * following conditions.
-	 * 1. pageable == TRUE
-	 * 2. eva = sva + NBPG
-	 * 3. the pmap is the kernel_pmap ??? - got this from
-	 *    i386/pmap.c ??
-	 *
-	 * right this will get called when making pagetables pageable
-	 */
- 
-	PDEBUG(5, printf("pmap_pageable: pmap=%p sva=%08lx eva=%08lx p=%d\n",
-	    pmap, sva, eva, pageable));
-
-	if (pmap == kernel_pmap && pageable && eva == (sva + NBPG)) {
-		vm_offset_t pa;
-		pt_entry_t *pte;
-
-		pte = pmap_pte(pmap, sva);
-		if (!pte)
-			return;
-		if (!pmap_pte_v(pte))
-			return;
-		pa = pmap_pte_pa(pte);
-
-		/*
-		 * Mark it unmodified to avoid pageout
-		 */
-		pmap_clear_modify(pa);
-	}
 }
 
 /*
