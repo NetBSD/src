c36e9e43e94ba4f75e9d693175418ce32a6811f9
mellon <mellon@NetBSD.org>
1999-03-30 03:12:28 +0000

    Merge patchlevel 22.

--- usr.sbin/dhcp/server/dhcpd.c
+++ usr.sbin/dhcp/server/dhcpd.c
@@ -42,13 +42,13 @@
 
 #ifndef lint
 static char ocopyright[] =
-"$Id: dhcpd.c,v 1.10 1999/03/30 01:53:58 mellon Exp $ Copyright 1995, 1996, 1997, 1998, 1999 The Internet Software Consortium.";
+"$Id: dhcpd.c,v 1.11 1999/03/30 03:12:28 mellon Exp $ Copyright 1995, 1996, 1997, 1998, 1999 The Internet Software Consortium.";
 #endif
 
 static char copyright[] =
 "Copyright 1995, 1996, 1997, 1998, 1999 The Internet Software Consortium.";
 static char arr [] = "All rights reserved.";
-static char message [] = "Internet Software Consortium DHCP Server V2.0b1pl21";
+static char message [] = "Internet Software Consortium DHCP Server V2.0b1pl20";
 static char contrib [] = "Please contribute if you find this software useful.";
 static char url [] = "For info, please visit http://www.isc.org/dhcp-contrib.html";
 
