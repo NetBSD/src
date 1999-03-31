a87086deafffcd693ff8cbe95a6967bbcf66f79f
cgd <cgd@NetBSD.org>
1999-03-31 03:04:21 +0000

    GC cpu_number() and turn alpha_pal_imb() into inline assembler.

--- sys/arch/alpha/stand/common/common.h
+++ sys/arch/alpha/stand/common/common.h
@@ -1,9 +1,10 @@
-/*	$NetBSD: common.h,v 1.4 1999/03/27 09:01:28 ross Exp $	*/
+/*	$NetBSD: common.h,v 1.5 1999/03/31 03:04:21 cgd Exp $	*/
+
+#define	alpha_pal_imb()	__asm__("imb")
 
 void init_prom_calls __P((void));
 void OSFpal __P((void));
 void halt __P((void));
 u_int64_t prom_dispatch __P((int, ...));
-int cpu_number __P((void));
 void switch_palcode __P((void));
 void close_primary_device __P((int));
