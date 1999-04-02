94624b7cd745d77f879433143b21c087ffa47163
perry <perry@NetBSD.org>
1999-04-02 03:02:19 +0000

    update branch to NetBSD 1.4, (NetBSD constant to 1.4_ALPHA date)

--- sys/arch/x68k/include/cpufunc.h
+++ sys/arch/x68k/include/cpufunc.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: cpufunc.h,v 1.1 1996/05/05 12:17:15 oki Exp $	*/
+/*	$NetBSD: cpufunc.h,v 1.1.1.1 1996/05/05 12:17:03 oki Exp $	*/
 
 /*
  * Functions to provide access to special cpu instructions.
