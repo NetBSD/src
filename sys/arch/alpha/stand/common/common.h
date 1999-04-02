b32376e177c57cf75ae91d665d9879d9ac093ca4
perry <perry>
1999-04-02 03:02:19 +0000

    update branch to NetBSD 1.4, (NetBSD constant to 1.4_ALPHA date)

--- sys/arch/alpha/stand/common/common.h
+++
@@ -1,9 +0,0 @@
-/*	$NetBSD: common.h,v 1.6 1999/04/01 11:08:39 ross Exp $	*/
-
-void init_prom_calls __P((void));
-void OSFpal __P((void));
-void halt __P((void));
-u_int64_t prom_dispatch __P((int, ...));
-int cpu_number __P((void));
-void switch_palcode __P((void));
-void close_primary_device __P((int));
