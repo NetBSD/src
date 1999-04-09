0a5391ccfd7934e44def28c6173649d668c0b4b5
minoura <minoura@NetBSD.org>
1999-04-09 15:41:15 +0000

    Fix the startup message. attach_hook is now called without "mdX: " prefix.

--- sys/arch/x68k/dev/md_root.c
+++ sys/arch/x68k/dev/md_root.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: md_root.c,v 1.2 1999/03/17 18:59:23 sommerfe Exp $	*/
+/*	$NetBSD: md_root.c,v 1.3 1999/04/09 15:41:15 minoura Exp $	*/
 
 /*-
  * Copyright (c) 1996 The NetBSD Foundation, Inc.
@@ -72,7 +72,7 @@ md_attach_hook(unit, md)
 		md->md_addr = (caddr_t) md_root_image;
 		md->md_size = (size_t)  md_root_size;
 		md->md_type = MD_KMEM_FIXED;
-		printf(" fixed, %d blocks", MINIROOTSIZE);
+		printf("md0: fixed, %d blocks\n", MINIROOTSIZE);
 	}
 }
 
