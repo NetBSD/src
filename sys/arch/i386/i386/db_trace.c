7c595d5bfe9a25d5f21a2b845aa8d5aa5ffadeca
pk <pk>
1999-04-12 20:38:17 +0000

    Quote "AS IS" as in the majority of Carnegy Mellon notices.

--- sys/arch/i386/i386/db_trace.c
+++ sys/arch/i386/i386/db_trace.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: db_trace.c,v 1.23 1999/04/06 20:09:19 pk Exp $	*/
+/*	$NetBSD: db_trace.c,v 1.24 1999/04/12 20:38:19 pk Exp $	*/
 
 /* 
  * Mach Operating System
@@ -11,7 +11,7 @@
  * software, derivative works or modified versions, and any portions
  * thereof, and that both notices appear in supporting documentation.
  * 
- * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS ``AS IS''
+ * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS "AS IS"
  * CONDITION.  CARNEGIE MELLON DISCLAIMS ANY LIABILITY OF ANY KIND FOR
  * ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.
  * 
--- sys/arch/pmax/dev/mfb.c
+++ sys/arch/pmax/dev/mfb.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: mfb.c,v 1.35 1999/04/06 20:09:19 pk Exp $	*/
+/*	$NetBSD: mfb.c,v 1.36 1999/04/12 20:38:19 pk Exp $	*/
 
 /*-
  * Copyright (c) 1992, 1993
@@ -49,7 +49,7 @@
  * software, derivative works or modified versions, and any portions
  * thereof, and that both notices appear in supporting documentation.
  * 
- * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS ``AS IS''
+ * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS "AS IS"
  * CONDITION.  CARNEGIE MELLON DISCLAIMS ANY LIABILITY OF ANY KIND FOR
  * ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.
  * 
@@ -81,7 +81,7 @@
  */
 
 #include <sys/cdefs.h>			/* RCS ID & Copyright macro defns */
-__KERNEL_RCSID(0, "$NetBSD: mfb.c,v 1.35 1999/04/06 20:09:19 pk Exp $");
+__KERNEL_RCSID(0, "$NetBSD: mfb.c,v 1.36 1999/04/12 20:38:19 pk Exp $");
 
 #include "fb.h"
 #include "mfb.h"
