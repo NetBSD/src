63feb9dcd27958cd3f4dbc7738356b0f9f727ed8
ad <ad@NetBSD.org>
1999-04-13 00:24:05 +0000

    Initial import of wsfont, an in-kernel font table for wscons/rcons.

---
+++ sys/dev/wsfont/wsfont.h
@@ -0,0 +1,78 @@
+/* $NetBSD: wsfont.h,v 1.1 1999/04/13 00:24:16 ad Exp $ */
+
+/*-
+ * Copyright (c) 1999 The NetBSD Foundation, Inc.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to The NetBSD Foundation
+ * by Andy Doran <ad@NetBSD.org>.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *        This product includes software developed by the NetBSD
+ *        Foundation, Inc. and its contributors.
+ * 4. Neither the name of The NetBSD Foundation nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef _WSFONT_H_
+#define _WSFONT_H_ 1
+
+/*
+ * wsfont_find() can be called with any of the parameters as 0, meaning we
+ * don't care about that aspect of the font. It returns a cookie which
+ * we can use with the other functions. When more flexibility is required,
+ * wsfont_enum() should be used. The last two parameters to wsfont_lock()
+ * are the bit order and byte order required (WSFONT_LITTLE or WSFONT_BIG).
+ *
+ * Example:
+ *
+ *	int cookie;
+ *	struct wsdisplay_font *font;
+ *
+ *	if ((cookie = wsfont_find(NULL, 8, 16, 0, 0)) < 0)
+ *		panic("unable to get 8x16 font");
+ *
+ *	if (wsfont_lock(cookie, &font, WSFONT_LITTLE, WSFONT_BIG) < 0)
+ *		panic("unable to lock font");
+ *
+ *	... do stuff ...
+ *
+ *	wsfont_unlock(cookie);
+ */
+struct wsdisplay_font;
+
+#define WSFONT_LITTLE	(0)
+#define WSFONT_BIG	(1)
+
+void	wsfont_init __P((void));
+int	wsfont_find __P((char *name, int width, int height, int stride));
+int	wsfont_add __P((struct wsdisplay_font *font, int copy));
+int	wsfont_remove __P((int cookie));
+void	wsfont_enum __P((void (*func) __P((char *n, int w, int h, int s))));
+int	wsfont_lock __P((int cookie, struct wsdisplay_font **font, int, int));
+int	wsfont_unlock __P((int cookie));
+int	wsfont_getflg __P((int cookie, int *flg, int *lockcount));
+
+#endif /* _WSFONT_H_ */
