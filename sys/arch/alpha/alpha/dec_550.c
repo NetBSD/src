9c5ed58b9de9b1af2f39acee1bc83a350f0e6a3d
thorpej <thorpej@NetBSD.org>
1999-04-16 23:12:39 +0000

    Pull up 1.3 -> 1.4.

--- sys/arch/alpha/alpha/dec_550.c
+++ sys/arch/alpha/alpha/dec_550.c
@@ -1,4 +1,4 @@
-/* $NetBSD: dec_550.c,v 1.3 1999/02/13 02:41:40 thorpej Exp $ */
+/* $NetBSD: dec_550.c,v 1.3.2.1 1999/04/16 23:12:39 thorpej Exp $ */
 
 /*
  * Copyright (c) 1995, 1996, 1997 Carnegie-Mellon University.
@@ -32,7 +32,7 @@
 
 #include <sys/cdefs.h>			/* RCS ID & Copyright macro defns */
 
-__KERNEL_RCSID(0, "$NetBSD: dec_550.c,v 1.3 1999/02/13 02:41:40 thorpej Exp $");
+__KERNEL_RCSID(0, "$NetBSD: dec_550.c,v 1.3.2.1 1999/04/16 23:12:39 thorpej Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -125,17 +125,18 @@ dec_550_cons_init()
 		}
 
 	case 3:
-#if	NPCKBD > 0
+#if NPCKBD > 0
 		/* display console ... */
 		/* XXX */
 		(void) pckbc_cnattach(&ccp->cc_iot, PCKBC_KBD_SLOT);
 
-		if ((ctb->ctb_turboslot & 0xffff) == 0)
+		if (CTB_TURBOSLOT_TYPE(ctb->ctb_turboslot) ==
+		    CTB_TURBOSLOT_TYPE_ISA)
 			isa_display_console(&ccp->cc_iot, &ccp->cc_memt);
 		else
 			pci_display_console(&ccp->cc_iot, &ccp->cc_memt,
-			    &ccp->cc_pc, (ctb->ctb_turboslot >> 8) & 0xff,
-			    ctb->ctb_turboslot & 0xff, 0);
+			    &ccp->cc_pc, CTB_TURBOSLOT_BUS(ctb->ctb_turboslot),
+			    CTB_TURBOSLOT_SLOT(ctb->ctb_turboslot), 0);
 #else
 		panic("not configured to use display && keyboard console");
 #endif
