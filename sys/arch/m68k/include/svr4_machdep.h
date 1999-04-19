ed069ef2750fbc98fc983d9c889dab59d594bc70
kleink <kleink@NetBSD.org>
1999-04-19 21:22:56 +0000

    Add COMPAT_SVR4 for m68k.

--- sys/arch/m68k/include/svr4_machdep.h
+++ sys/arch/m68k/include/svr4_machdep.h
@@ -1,11 +1,11 @@
-/*	$NetBSD: svr4_exec.h,v 1.12 1999/04/19 21:22:56 kleink Exp $	 */
+/*	$NetBSD: svr4_machdep.h,v 1.1 1999/04/19 21:22:58 kleink Exp $	*/
 
 /*-
- * Copyright (c) 1994 The NetBSD Foundation, Inc.
+ * Copyright (c) 1999 The NetBSD Foundation, Inc.
  * All rights reserved.
  *
  * This code is derived from software contributed to The NetBSD Foundation
- * by Christos Zoulas.
+ * by Klaus Klein.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -36,49 +36,84 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifndef	_SVR4_EXEC_H_
-#define	_SVR4_EXEC_H_
+#ifndef _M68K_SVR4_MACHDEP_H_
+#define _M68K_SVR4_MACHDEP_H_
 
-#ifdef SVR4_COMPAT_SOLARIS2
-# define SVR4_AUX_ARGSIZ (sizeof(AuxInfo) * 12 / sizeof(char *))
-#else
-# define SVR4_AUX_ARGSIZ (sizeof(AuxInfo) * 8 / sizeof(char *))
+#include <compat/svr4/svr4_types.h>
+
+#ifdef _KERNEL
+#include <m68k/frame.h>
 #endif
 
 /*
- * The following is horrible; there must be a better way. I need to
- * play with brk(2) a bit more.
+ * Machine-dependent portions [M68K].
  */
-#ifdef i386
+
+#define SVR4_M68K_D0		0
+#define SVR4_M68K_D1		1
+#define SVR4_M68K_D2		2
+#define SVR4_M68K_D3		3
+#define SVR4_M68K_D4		4
+#define SVR4_M68K_D5		5
+#define SVR4_M68K_D6		6
+#define SVR4_M68K_D7		7
+#define SVR4_M68K_A0		8
+#define SVR4_M68K_A1		9
+#define SVR4_M68K_A2		10
+#define SVR4_M68K_A3		11
+#define SVR4_M68K_A4		12
+#define SVR4_M68K_A5		13
+#define SVR4_M68K_A6		14
+#define SVR4_M68K_A7		15
+#define SVR4_M68K_PC		16
+#define SVR4_M68K_PS		17
+#define SVR4_M68K_MAXREG	18
+
+#define SVR4_M68K_FP		SVR4_M68K_A6
+#define SVR4_M68K_SP		SVR4_M68K_A7
+
+typedef int		svr4_greg_t;
+typedef svr4_greg_t	svr4_gregset_t[SVR4_M68K_MAXREG];
+
+typedef struct svr4_fpregset {
+	int	f_pcr;
+	int	f_psr;
+	int	f_piaddr;
+	int	f_fpregs[8][3];
+} svr4_fpregset_t;
+
+typedef struct svr4_mcontext {
+	svr4_gregset_t	gregs;
+	svr4_fpregset_t	fpregs;
+	union {
+		long		mc_state[202];
+#ifdef _KERNEL
+		struct {
+			/* Rest of exception frame. */
+			unsigned int	format;
+			unsigned int	vector;
+			union F_u	exframe;
+			/* Rest of FPU frame. */
+			union FPF_u1	fpf_u1;
+			union FPF_u2	fpf_u2;
+		} frame;
+#endif
+	} mc_pad;
+} svr4_mcontext_t;
+
+#define mc_state	mc_pad.mc_state
+
+/* No padding in ucontext_t; we need all the `padding' in mcontext_t. */
+#undef SVR4_UC_MACHINE_PAD
+
 /*
- * I cannot load the interpreter after the data segment because brk(2)
- * breaks. I have to load it somewhere before. Programs start at
- * 0x08000000 so I load the interpreter far before.
+ * sysm68k() commands
  */
-#define SVR4_INTERP_ADDR	0x01000000
-#endif
+#define SVR4_SYSARCH_SETNAME		56
+#define SVR4_SYSARCH_BIURW		92
+#define SVR4_SYSARCH_EXOP		93
+#define SVR4_SYSARCH_AB_STKFRM		94
+#define SVR4_SYSARCH_LIMUSER		95
+#define SVR4_SYSARCH_KERNADDR		99
 
-#ifdef __m68k__			/* XXX need -D${MACHINE_ARCH} */
-/*
- * Here programs load at 0x80000000, so I load the interpreter far before.
- */
-#define SVR4_INTERP_ADDR	0x01000000
-#endif
-
-#ifdef sparc
-/*
- * Here programs load at 0x00010000, so I load the interpreter far after
- * the end of the data segment.
- */
-#define SVR4_INTERP_ADDR	0x10000000
-#endif
-
-#ifndef SVR4_INTERP_ADDR
-# define SVR4_INTERP_ADDR	0
-#endif
-
-void svr4_setregs __P((struct proc *, struct exec_package *, u_long));
-int svr4_elf32_probe __P((struct proc *, struct exec_package *, Elf32_Ehdr *,
-    char *, Elf32_Addr *));
-
-#endif /* !_SVR4_EXEC_H_ */
+#endif /* !_M68K_SVR4_MACHDEP_H_ */
