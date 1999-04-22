c22ef980b50ce55c26bf72b6f9a76139a18755f6
drochner <drochner>
1999-04-22 10:27:16 +0000

    remove an "unmodified" so that the copyright notice will hopefully match pk's "BSD-style (clauses: 2; no acknowledgements)" pattern

--- sys/arch/i386/stand/lib/netif/3c90xb.c
+++ sys/arch/i386/stand/lib/netif/3c90xb.c
@@ -1,4 +1,4 @@
-/* $NetBSD: 3c90xb.c,v 1.2 1999/03/10 10:37:38 drochner Exp $ */
+/* $NetBSD: 3c90xb.c,v 1.3 1999/04/22 10:27:16 drochner Exp $ */
 
 /*
  * Copyright (c) 1999
@@ -8,8 +8,7 @@
  * modification, are permitted provided that the following conditions
  * are met:
  * 1. Redistributions of source code must retain the above copyright
- *    notice unmodified, this list of conditions, and the following
- *    disclaimer.
+ *    notice, this list of conditions, and the following disclaimer.
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
