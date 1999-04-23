9bb7f1249580abd60ecff27cdba44e8443a5d945
cgd <cgd@NetBSD.org>
1999-04-23 18:01:05 +0000

    clean up multiple inclusion protection def'ns

--- sys/compat/osf1/osf1_util.h
+++ sys/compat/osf1/osf1_util.h
@@ -1,7 +1,7 @@
-/*	$NetBSD: osf1_util.h,v 1.4 1999/04/23 05:55:13 cgd Exp $	*/
+/*	$NetBSD: osf1_util.h,v 1.5 1999/04/23 18:01:05 cgd Exp $	*/
 
-#ifndef _OSF1_OSF1_UTIL_H_
-#define _OSF1_OSF1_UTIL_H_
+#ifndef _COMPAT_OSF1_OSF1_UTIL_H_
+#define _COMPAT_OSF1_OSF1_UTIL_H_
 
 #include <compat/common/compat_util.h>
 
@@ -13,4 +13,4 @@ extern const char osf1_emul_path[];
 #define OSF1_CHECK_ALT_CREAT(p, sgp, path) \
     CHECK_ALT_CREAT(p, sgp, osf1_emul_path, path)
 
-#endif /* _OSF1_OSF1_UTIL_H_ */
+#endif /* _COMPAT_OSF1_OSF1_UTIL_H_ */
