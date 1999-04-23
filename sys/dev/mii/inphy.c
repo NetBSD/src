f8cd62e7f80da32d3d30b34c40d0b4a16f1d7fa1
perry <perry>
1999-04-23 15:39:09 +0000

    pullup 1.10->1.11 (thorpej): Fix PR7361

--- sys/dev/mii/inphy.c
+++ sys/dev/mii/inphy.c
@@ -1,7 +1,7 @@
-/*	$NetBSD: inphy.c,v 1.10 1998/11/05 04:08:02 thorpej Exp $	*/
+/*	$NetBSD: inphy.c,v 1.10.6.1 1999/04/23 15:39:09 perry Exp $	*/
 
 /*-
- * Copyright (c) 1998 The NetBSD Foundation, Inc.
+ * Copyright (c) 1998, 1999 The NetBSD Foundation, Inc.
  * All rights reserved.
  *
  * This code is derived from software contributed to The NetBSD Foundation
@@ -77,6 +77,7 @@
 #include <sys/device.h>
 #include <sys/malloc.h>
 #include <sys/socket.h>
+#include <sys/errno.h>
 
 #include <net/if.h>
 #include <net/if_media.h>
@@ -201,7 +202,7 @@ inphy_service(sc, mii, cmd)
 			 */
 			if (PHY_READ(sc, MII_BMCR) & BMCR_AUTOEN)
 				return (0);
-			(void) mii_phy_auto(sc);
+			(void) mii_phy_auto(sc, 1);
 			break;
 		case IFM_100_T4:
 			/*
@@ -255,7 +256,8 @@ inphy_service(sc, mii, cmd)
 
 		sc->mii_ticks = 0;
 		mii_phy_reset(sc);
-		(void) mii_phy_auto(sc);
+		if (mii_phy_auto(sc, 0) == EJUSTRETURN)
+			return (0);
 		break;
 	}
 
