920a28382a82f61cf75ff8d922a13c226efadf33
perry <perry@NetBSD.org>
1999-04-23 15:43:27 +0000

    pullup 1.1->1.2 (Mark Brinicombe)

--- sys/arch/arm32/footbridge/footbridge_clock.c
+++ sys/arch/arm32/footbridge/footbridge_clock.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: footbridge_clock.c,v 1.1 1998/09/06 02:20:34 mark Exp $	*/
+/*	$NetBSD: footbridge_clock.c,v 1.1.6.1 1999/04/23 15:43:27 perry Exp $	*/
 
 /*
  * Copyright (c) 1997 Mark Brinicombe.
@@ -241,6 +241,9 @@ microtime(tvp)
 	int tm;
 	int deltatm;
 	static struct timeval oldtv;
+
+	if (clock_sc == NULL || clock_sc->sc_clock_count == 0)
+		return;
 
 	s = splhigh();
 
