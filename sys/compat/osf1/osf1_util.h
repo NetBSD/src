5cafad620c5649368ba6ea2d3d130f6ba0e083f2
cgd <cgd@NetBSD.org>
1999-04-23 05:55:13 +0000

    add an OSF1_CHECK_ALT_CREAT, similar to OSF1_CHECK_ALT_EXIST

--- sys/compat/osf1/osf1_util.h
+++ sys/compat/osf1/osf1_util.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: osf1_util.h,v 1.3 1998/05/20 16:34:29 chs Exp $	*/
+/*	$NetBSD: osf1_util.h,v 1.4 1999/04/23 05:55:13 cgd Exp $	*/
 
 #ifndef _OSF1_OSF1_UTIL_H_
 #define _OSF1_OSF1_UTIL_H_
@@ -9,5 +9,8 @@ extern const char osf1_emul_path[];
 
 #define OSF1_CHECK_ALT_EXIST(p, sgp, path) \
     CHECK_ALT_EXIST(p, sgp, osf1_emul_path, path)
+
+#define OSF1_CHECK_ALT_CREAT(p, sgp, path) \
+    CHECK_ALT_CREAT(p, sgp, osf1_emul_path, path)
 
 #endif /* _OSF1_OSF1_UTIL_H_ */
