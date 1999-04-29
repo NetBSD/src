b08ed5d372daad286b474e039c99a4e9a96dd6aa
kleink <kleink@NetBSD.org>
1999-04-29 14:38:39 +0000

    Pull in the right generic m68k header.  (Where was my mind?)

--- sys/arch/hp300/include/svr4_machdep.h
+++ sys/arch/hp300/include/svr4_machdep.h
@@ -1,8 +1,8 @@
-/*	$NetBSD: svr4_machdep.h,v 1.1 1999/04/19 21:22:58 kleink Exp $	*/
+/*	$NetBSD: svr4_machdep.h,v 1.2 1999/04/29 14:38:39 kleink Exp $	*/
 
 #ifndef _MACHINE_SVR4_MACHDEP_H_
 #define _MACHINE_SVR4_MACHDEP_H_
 
-#include <m68k/frame.h>
+#include <m68k/svr4_machdep.h>
 
 #endif
