0847df9065af0717ecf6b7fc27b575e048e1b73e
ross <ross@NetBSD.org>
1999-04-29 02:55:50 +0000

    #ifdef _KERNEL, define the machine FPCR bits and a rounding mode macro

--- sys/arch/alpha/include/ieeefp.h
+++ sys/arch/alpha/include/ieeefp.h
@@ -1,4 +1,4 @@
-/* $NetBSD: ieeefp.h,v 1.2 1997/04/06 08:47:28 cgd Exp $ */
+/* $NetBSD: ieeefp.h,v 1.3 1999/04/29 02:55:50 ross Exp $ */
 
 /* 
  * Written by J.T. Conklin, Apr 28, 1995
@@ -22,5 +22,24 @@ typedef enum {
     FP_RN=2,			/* round to nearest representable number */
     FP_RP=3			/* round toward positive infinity */
 } fp_rnd;
+
+#ifdef _KERNEL
+#define	FPCR_SUM	(1UL << 63)
+#define	FPCR_INED	(1UL << 62)
+#define	FPCR_UNFD	(1UL << 61)
+#define	FPCR_UNDZ	(1UL << 60)
+#define	FPCR_DYN(rm)	((unsigned long)(rm) << 58)
+#define	FPCR_IOV	(1UL << 57)
+#define	FPCR_INE	(1UL << 56)
+#define	FPCR_UNF	(1UL << 55)
+#define	FPCR_OVF	(1UL << 54)
+#define	FPCR_DZE	(1UL << 53)
+#define	FPCR_INV	(1UL << 52)
+#define	FPCR_OVFD	(1UL << 51)
+#define	FPCR_DZED	(1UL << 50)
+#define	FPCR_INVD	(1UL << 49)
+#define	FPCR_DNZ	(1UL << 48)
+#define	FPCR_DNOD	(1UL << 47)
+#endif
 
 #endif /* _ALPHA_IEEEFP_H_ */
