d8466c19e49a6592c1385e4a8e1c0004c984a193
tsubai <tsubai@NetBSD.org>
1999-05-03 10:27:23 +0000

    Support NetBSD/powerpc.

--- gnu/dist/gdb/config/powerpc/nm-nbsd.h
+++ gnu/dist/gdb/config/powerpc/nm-nbsd.h
@@ -19,7 +19,7 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
 
 /* Need to define this before including the common nm-nbsd.h. */
-#define SVR4_SHARED_LIBS
+#define SVR4_SHARED_LIBS 1
 
 /* Get generic NetBSD native definitions. */
 #include "nm-nbsd.h"
