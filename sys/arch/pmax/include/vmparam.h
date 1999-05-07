30bf32e7f05ea8b2d5e68058c68d2671fa803375
thorpej <thorpej@NetBSD.org>
1999-05-07 18:04:36 +0000

    Add a second memory free list, which holds the first 8M of RAM.  This is so that devices which must allocate DMA memory in this range (e.g. PixelStamp graphics boards) have a better chance of doing so, without an awful hack.

--- sys/arch/pmax/include/vmparam.h
+++ sys/arch/pmax/include/vmparam.h
@@ -1,14 +1,17 @@
-/*	$NetBSD: vmparam.h,v 1.8 1998/07/08 04:43:19 thorpej Exp $	*/
+/*	$NetBSD: vmparam.h,v 1.9 1999/05/07 18:04:37 thorpej Exp $	*/
 
 #include <mips/vmparam.h>
 
 /*
  * DECstation has one physical memory segment.
  */
-#define	VM_PHYSSEG_MAX		1
+#define	VM_PHYSSEG_MAX		2	/* 2 free lists */
+#define	VM_PHYSSEG_STRAT	VM_PSTRAT_BSEARCH
+#define	VM_PHYSSEG_NOADD		/* can't add RAM after vm_mem_init */
 
-#define	VM_NFREELIST		1
+#define	VM_NFREELIST		2
 #define	VM_FREELIST_DEFAULT	0
+#define	VM_FREELIST_FIRST8	1
 
 /* pcb base */
 /*#define	pcbb(p)		((u_int)(p)->p_addr) */
