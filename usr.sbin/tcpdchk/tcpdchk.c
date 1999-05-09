3927f600aaf5b521717a198beb3ca5c72adf17f8
christos <christos@NetBSD.org>
1999-05-09 16:07:20 +0000

    Pass NULL for checking only

--- usr.sbin/tcpdchk/tcpdchk.c
+++ usr.sbin/tcpdchk/tcpdchk.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: tcpdchk.c,v 1.6 1999/05/09 16:05:35 christos Exp $	*/
+/*	$NetBSD: tcpdchk.c,v 1.7 1999/05/09 16:07:20 christos Exp $	*/
 
  /*
   * tcpdchk - examine all tcpd access control rules and inetd.conf entries
@@ -21,7 +21,7 @@
 #if 0
 static char sccsid[] = "@(#) tcpdchk.c 1.7 96/02/11 17:01:34";
 #else
-__RCSID("$NetBSD: tcpdchk.c,v 1.6 1999/05/09 16:05:35 christos Exp $");
+__RCSID("$NetBSD: tcpdchk.c,v 1.7 1999/05/09 16:07:20 christos Exp $");
 #endif
 #endif
 
@@ -421,7 +421,6 @@ char   *pat;
 {
     char   *mask;
     int     addr_count = 1;
-    struct in_addr dummy;
 
     if (pat[0] == '@') {			/* @netgroup */
 #ifdef NO_NETGRENT
@@ -441,8 +440,8 @@ char   *pat;
 #endif
 #endif
     } else if ((mask = split_at(pat, '/')) != NULL) {	/* network/netmask */
-	if (dot_quad_addr(pat, &dummy) != 0
-	    || dot_quad_addr(mask, &dummy) != 0)
+	if (dot_quad_addr(pat, NULL) != 0
+	    || dot_quad_addr(mask, NULL) != 0)
 	    tcpd_warn("%s/%s: bad net/mask pattern", pat, mask);
     } else if (STR_EQ(pat, "FAIL")) {		/* obsolete */
 	tcpd_warn("FAIL is no longer recognized");
