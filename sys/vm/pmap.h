bf781486e5ed4b883dd86c40b7ad8b78db6f83d7
thorpej <thorpej>
1999-05-12 19:11:23 +0000

    Add an optional pmap hook, pmap_fork(), to be called at the end of uvmspace_fork().
    pmap_fork() is used to "fork a pmap", that is copy data from one pmap
    to the other that is NOT related to actual mappings in the pmap, but is
    otherwise logically coupled to the address space.

--- sys/vm/pmap.h
+++ sys/vm/pmap.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: pmap.h,v 1.29 1999/03/26 21:58:39 mycroft Exp $	*/
+/*	$NetBSD: pmap.h,v 1.30 1999/05/12 19:11:24 thorpej Exp $	*/
 
 /* 
  * Copyright (c) 1991, 1993
@@ -185,10 +185,14 @@ void		 pmap_update __P((void));
 void		 pmap_zero_page __P((paddr_t));
 
 #if defined(PMAP_STEAL_MEMORY)
-vaddr_t	 pmap_steal_memory __P((vsize_t, paddr_t *,
+vaddr_t		 pmap_steal_memory __P((vsize_t, paddr_t *,
 		    paddr_t *));
 #else
 void		 pmap_virtual_space __P((vaddr_t *, vaddr_t *));
+#endif
+
+#if defined(PMAP_FORK)
+void		pmap_fork __P((pmap_t, pmap_t));
 #endif
 __END_DECLS
 #endif	/* kernel*/
