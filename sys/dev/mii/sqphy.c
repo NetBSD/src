23ddff5688452d717654efc4e3db2c6975ae5166
drochner <drochner@NetBSD.org>
1999-05-14 11:40:28 +0000

    use the new "xx" prefixed OUIs because the mapping to the ID register bits differs from the MII_OUI() way

--- sys/dev/mii/sqphy.c
+++ sys/dev/mii/sqphy.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: sqphy.c,v 1.9 1999/04/23 04:24:32 thorpej Exp $	*/
+/*	$NetBSD: sqphy.c,v 1.10 1999/05/14 11:40:28 drochner Exp $	*/
 
 /*-
  * Copyright (c) 1998, 1999 The NetBSD Foundation, Inc.
@@ -106,8 +106,8 @@ sqphymatch(parent, match, aux)
 {
 	struct mii_attach_args *ma = aux;
 
-	if (MII_OUI(ma->mii_id1, ma->mii_id2) == MII_OUI_SEEQ &&
-	    MII_MODEL(ma->mii_id2) == MII_MODEL_SEEQ_80220)
+	if (MII_OUI(ma->mii_id1, ma->mii_id2) == MII_OUI_xxSEEQ &&
+	    MII_MODEL(ma->mii_id2) == MII_MODEL_xxSEEQ_80220)
 		return (10);
 
 	return (0);
@@ -122,7 +122,7 @@ sqphyattach(parent, self, aux)
 	struct mii_attach_args *ma = aux;
 	struct mii_data *mii = ma->mii_data;
 
-	printf(": %s, rev. %d\n", MII_STR_SEEQ_80220,
+	printf(": %s, rev. %d\n", MII_STR_xxSEEQ_80220,
 	    MII_REV(ma->mii_id2));
 
 	sc->mii_inst = mii->mii_instance;
