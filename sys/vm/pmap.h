8362b905e2dad4540019c84ddcb6c49612c81016
thorpej <thorpej@NetBSD.org>
1999-05-20 23:25:42 +0000

    Make a slight modification of pmap_growkernel() -- it now returns the end of the mappable kernel virtual address space.  Previously, it would get called more often than necessary, because the caller only new what was requested.
    Also, export uvm_maxkaddr so that uvm_pageboot_alloc() can grow the
    kernel pmap if necessary, as well.  Note that pmap_growkernel() must
    now be able to handle being called before pmap_init().

--- sys/vm/pmap.h
+++ sys/vm/pmap.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: pmap.h,v 1.30 1999/05/12 19:11:24 thorpej Exp $	*/
+/*	$NetBSD: pmap.h,v 1.31 1999/05/20 23:25:42 thorpej Exp $	*/
 
 /* 
  * Copyright (c) 1991, 1993
@@ -146,7 +146,7 @@ void		 pmap_enter __P((pmap_t,
 		    vaddr_t, paddr_t, vm_prot_t, boolean_t, vm_prot_t));
 paddr_t		pmap_extract __P((pmap_t, vaddr_t));
 #if defined(PMAP_NEW) && defined(PMAP_GROWKERNEL)
-void		 pmap_growkernel __P((vaddr_t));
+vaddr_t		 pmap_growkernel __P((vaddr_t));
 #endif
 
 void		 pmap_init __P((void));
