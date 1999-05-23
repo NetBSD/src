080560c8c661d4f7b8a016913c017470349fb3f2
ad <ad@NetBSD.org>
1999-05-23 17:59:39 +0000

    - Setting initial position of cursor is up to caller. - Set cursor position properly for SPARC. From Juergen Hannken-Illjes.

--- sys/arch/sparc64/dev/fb.c
+++ sys/arch/sparc64/dev/fb.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: fb.c,v 1.14 1999/05/23 02:45:19 eeh Exp $ */
+/*	$NetBSD: fb.c,v 1.15 1999/05/23 17:59:39 ad Exp $ */
 
 /*
  * Copyright (c) 1992, 1993
@@ -282,6 +282,8 @@ fbrcons_init(fb)
 	rc->rc_maxrow = ri->ri_rows;
 	rc->rc_width = ri->ri_emuwidth;
 	rc->rc_height = ri->ri_emuheight;
+	rc->rc_row = 0;
+	rc->rc_col = 0;
 	rcons_init(rc, 0);
 
 	/* Hook up virtual console */
