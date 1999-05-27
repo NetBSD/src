8889ad40fa6d6fe26f279c2658bf56d760932045
kleink <kleink@NetBSD.org>
1999-05-27 14:50:27 +0000

    m68k/ELF target configuration.

--- gnu/usr.bin/egcs/arch/m68k/tm.h
+++ gnu/usr.bin/egcs/arch/m68k/tm.h
@@ -1,3 +1,7 @@
-/*	$NetBSD: tm.h,v 1.2 1998/08/17 20:26:07 tv Exp $	*/
+/*	$NetBSD: tm.h,v 1.3 1999/05/27 14:50:27 kleink Exp $	*/
 
+#ifdef DEFAULT_ELF
+#include "m68k/netbsd-elf.h"
+#else
 #include "m68k/netbsd.h"
+#endif
