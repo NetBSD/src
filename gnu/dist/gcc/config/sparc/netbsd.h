0d8a37e4760dba1aa3d3824844e803d5ea8e8f24
pk <pk@NetBSD.org>
1999-06-05 12:56:28 +0000

    Sync with <machine/ansi.h>: `size_t' is now a `long'.

--- gnu/dist/gcc/config/sparc/netbsd.h
+++ gnu/dist/gcc/config/sparc/netbsd.h
@@ -12,10 +12,10 @@
 /* Make gcc agree with <machine/ansi.h> */
 
 #undef SIZE_TYPE
-#define SIZE_TYPE "unsigned int"
+#define SIZE_TYPE "long unsigned int"
 
 #undef PTRDIFF_TYPE
-#define PTRDIFF_TYPE "int"
+#define PTRDIFF_TYPE "long int"
 
 #undef WCHAR_TYPE
 #define WCHAR_TYPE "int"
