d13c322492fcdbfb716a6458860d3dd7e7ee9ebf
tsubai <tsubai@NetBSD.org>
1999-06-20 05:34:59 +0000

    More initialization by calling Open Firmware.

--- sys/arch/macppc/dev/if_bm.c
+++ sys/arch/macppc/dev/if_bm.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: if_bm.c,v 1.2 1999/05/18 23:52:53 thorpej Exp $	*/
+/*	$NetBSD: if_bm.c,v 1.3 1999/06/20 05:34:59 tsubai Exp $	*/
 
 /*-
  * Copyright (C) 1998, 1999 Tsubai Masanari.  All rights reserved.
@@ -174,8 +174,14 @@ bmac_attach(parent, self, aux)
 	int i;
 
 	sc->sc_flags =0;
-	if (strcmp(ca->ca_name, "ethernet") == 0)
+	if (strcmp(ca->ca_name, "ethernet") == 0) {
+		char name[64];
+
+		bzero(name, 64);
+		OF_package_to_path(ca->ca_node, name, sizeof(name));
+		OF_open(name);
 		sc->sc_flags |= BMAC_BMACPLUS;
+	}
 
 	ca->ca_reg[0] += ca->ca_baseaddr;
 	ca->ca_reg[2] += ca->ca_baseaddr;
