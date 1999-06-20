a94b9f885ac37a2609ef8b3f560d5ca291ef6e86
cgd <cgd@NetBSD.org>
1999-06-20 06:08:13 +0000

    copyright notice typo fixups, as noticed in PR#7248.  This software was developed, not develooped.

--- distrib/utils/sysinst/disks.c
+++ distrib/utils/sysinst/disks.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: disks.c,v 1.26 1999/04/13 20:17:47 bouyer Exp $ */
+/*	$NetBSD: disks.c,v 1.27 1999/06/20 06:08:14 cgd Exp $ */
 
 /*
  * Copyright 1997 Piermont Information Systems Inc.
@@ -16,7 +16,7 @@
  *    documentation and/or other materials provided with the distribution.
  * 3. All advertising materials mentioning features or use of this software
  *    must display the following acknowledgement:
- *      This product includes software develooped for the NetBSD Project by
+ *      This product includes software developed for the NetBSD Project by
  *      Piermont Information Systems Inc.
  * 4. The name of Piermont Information Systems Inc. may not be used to endorse
  *    or promote products derived from this software without specific prior
