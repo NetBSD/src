99c2dc9e47fb28374f8b55df69ede13886df810e
is <is>
1999-06-22 21:12:00 +0000

    Align adresses better

--- sys/arch/amiga/dev/zbus.c
+++ sys/arch/amiga/dev/zbus.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: zbus.c,v 1.39 1999/03/31 02:47:38 mhitch Exp $	*/
+/*	$NetBSD: zbus.c,v 1.40 1999/06/22 21:12:00 is Exp $	*/
 
 /*
  * Copyright (c) 1994 Christian E. Hopps
@@ -332,7 +332,7 @@ zbusprint(auxp, pnp)
 		if (zap->manid == -1)
 			rv = UNSUPP;
 	}
-	printf(" pa %p man/pro %d/%d", zap->pa, zap->manid, zap->prodid);
+	printf(" pa %8p man/pro %d/%d", zap->pa, zap->manid, zap->prodid);
 	return(rv);
 }
 
