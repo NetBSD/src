1345bb9cd5f3c58b7fbd865aeada3c384fe2c4f2
cgd <cgd@NetBSD.org>
1999-06-24 22:52:38 +0000

    pull up rev(s) 1.17 from trunk. (cgd)

--- distrib/utils/sysinst/arch/pc532/md.c
+++ distrib/utils/sysinst/arch/pc532/md.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: md.c,v 1.14.2.2 1999/04/19 15:19:30 perry Exp $	*/
+/*	$NetBSD: md.c,v 1.14.2.3 1999/06/24 22:52:38 cgd Exp $	*/
 
 /*
  * Copyright 1997 Piermont Information Systems Inc.
@@ -16,7 +16,7 @@
  *    documentation and/or other materials provided with the distribution.
  * 3. All advertising materials mentioning features or use of this software
  *    must display the following acknowledgement:
- *      This product includes software develooped for the NetBSD Project by
+ *      This product includes software developed for the NetBSD Project by
  *      Piermont Information Systems Inc.
  * 4. The name of Piermont Information Systems Inc. may not be used to endorse
  *    or promote products derived from this software without specific prior
