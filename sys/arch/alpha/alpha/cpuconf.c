3714f3754f8f5cc26eba45298e95578a5e576324
ross <ross@NetBSD.org>
1999-06-29 06:50:41 +0000

    Turn on dec_6600_init, "DEC_6600".

--- sys/arch/alpha/alpha/cpuconf.c
+++ sys/arch/alpha/alpha/cpuconf.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: cpuconf.c,v 1.21 1999/04/10 01:21:37 cgd Exp $	*/
+/*	$NetBSD: cpuconf.c,v 1.22 1999/06/29 06:50:41 ross Exp $	*/
 
 /*
  * Copyright (c) 1996 Christopher G. Demetriou.  All rights reserved.
@@ -135,6 +135,13 @@ extern void dec_eb66_init __P((void));
 #define	dec_eb66_init		platform_not_configured
 #endif
 
+#include "opt_dec_6600.h"
+#ifdef DEC_6600
+extern void dec_6600_init __P((void));
+#else
+#define	dec_6600_init		platform_not_configured
+#endif
+
 struct cpuinit cpuinit[] = {
 	cpu_notsupp("???"),			     /*  0: ??? */
 	cpu_notsupp("ST_ADU"),			     /*  1: ST_ADU */
@@ -170,7 +177,7 @@ struct cpuinit cpuinit[] = {
 	cpu_notsupp("???"),			     /* 31: ??? */
 	cpu_notsupp("ST_DEC_EV56_PBP"),		     /* 32: ST_DEC_EV56_PBP */
 	cpu_notsupp("ST_DEC_ALPHAVME_320"),	   /* 33: ST_DEC_ALPHAVME_320 */
-	cpu_notsupp("ST_DEC_6600"),	   	   /* 34: ST_DEC_6600 */
+	cpu_init(dec_6600_init,"DEC_6600"),	     /* 34: ST_6600 */
 };
 int ncpuinit = (sizeof(cpuinit) / sizeof(cpuinit[0]));
 
