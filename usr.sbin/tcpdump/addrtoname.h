31ecc81247d8f8c0b0a2d0867b27d6e84c2ecc2a
itojun <itojun@NetBSD.org>
1999-07-02 11:31:28 +0000

    Pv6, IPsec, ospf6, ripng, dhcp and other protocols support.

--- usr.sbin/tcpdump/addrtoname.h
+++ usr.sbin/tcpdump/addrtoname.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: addrtoname.h,v 1.5 1998/02/01 13:48:08 christos Exp $	*/
+/*	$NetBSD: addrtoname.h,v 1.6 1999/07/02 11:31:28 itojun Exp $	*/
 
 /*
  * Copyright (c) 1990, 1992, 1993, 1994, 1995, 1996, 1997
@@ -39,9 +39,18 @@ extern char *etherproto_string(u_short);
 extern char *tcpport_string(u_short);
 extern char *udpport_string(u_short);
 extern char *getname(const u_char *);
+#ifdef INET6
+extern char *getname6(const u_char *);
+#endif
 extern char *intoa(u_int32_t);
 
 extern void init_addrtoname(u_int32_t, u_int32_t);
 extern struct hnamemem *newhnamemem(void);
+#ifdef INET6
+extern struct h6namemem *newh6namemem(void);
+#endif
 
 #define ipaddr_string(p) getname((const u_char *)(p))
+#ifdef INET6
+#define ip6addr_string(p) getname6((const u_char *)(p))
+#endif
