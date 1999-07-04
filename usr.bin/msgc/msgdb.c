65be9e4af8ae3e91535643b6bd4a24eff0a2282b
cgd <cgd@NetBSD.org>
1999-07-04 07:40:51 +0000

    provide APIs (currently mostly undocumented) to print pre-formatted (table) text.

--- usr.bin/msgc/msgdb.c
+++ usr.bin/msgc/msgdb.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: msgdb.c,v 1.5 1999/06/22 15:00:37 cgd Exp $	*/
+/*	$NetBSD: msgdb.c,v 1.6 1999/07/04 07:40:51 cgd Exp $	*/
 
 /*
  * Copyright 1997 Piermont Information Systems Inc.
@@ -150,7 +150,7 @@ write_msg_file ()
 		"void msg_standend(void);\n"
 		"void msg_display(int msg_no,...);\n"
 		"void msg_display_add(int msg_no,...);\n"
-		"int  msg_vprintf (char *fmt, va_list ap);\n"
+		"int  msg_vprintf (int auto_fill, char *fmt, va_list ap);\n"
 		"int  msg_printf (char *fmt, ...);\n"
 		"int  msg_printf_add (char *fmt, ...);\n"
 		"void msg_prompt_str (char *msg, char *def, char *val,"
@@ -163,6 +163,8 @@ write_msg_file ()
 			" int max_chars, ...);\n"
 		"void msg_echo (void);\n"
 		"void msg_noecho (void);\n"
+		"void msg_table(int msg_no,...);\n"
+		"void msg_table_add(int msg_no,...);\n"
 		"\n"
 		"/* Message names */\n"
 	      );
