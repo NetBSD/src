d55776d21b2f4d380621efb9a443f5c151869034
thorpej <thorpej>
1999-07-06 21:51:22 +0000

    Make the kthread API a bit friendlier to loadable kernel modules.

--- sys/dev/raidframe/rf_threadstuff.h
+++ sys/dev/raidframe/rf_threadstuff.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: rf_threadstuff.h,v 1.4 1999/03/14 21:53:31 oster Exp $	*/
+/*	$NetBSD: rf_threadstuff.h,v 1.5 1999/07/06 21:51:22 thorpej Exp $	*/
 /*
  * Copyright (c) 1995 Carnegie-Mellon University.
  * All rights reserved.
@@ -97,7 +97,7 @@ typedef void *RF_ThreadArg_t;
 #define RF_SIGNAL_COND(_c_)            wakeup(&(_c_))
 #define RF_BROADCAST_COND(_c_)         wakeup(&(_c_))
 #define	RF_CREATE_THREAD(_handle_, _func_, _arg_) \
-	kthread_create((void (*) __P((void *)))(_func_), (void *)(_arg_), \
+	kthread_create1((void (*) __P((void *)))(_func_), (void *)(_arg_), \
 	    (struct proc **)&(_handle_), "raid")
 
 struct RF_ThreadGroup_s {
