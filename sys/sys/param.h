5cd69f33aead59c5503bb9acb3c5bb3603cd42a5
wrstuden <wrstuden@NetBSD.org>
1999-07-15 22:00:40 +0000

    We're now at 1.4G due to changes in vnode flags.

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.65 1999/07/12 20:55:32 kleink Exp $	*/
+/*	$NetBSD: param.h,v 1.66 1999/07/15 22:00:40 wrstuden Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -46,7 +46,7 @@
 /*
  * Historic BSD #defines -- probably will remain untouched for all time.
  */
-#define	BSD	199506		/* System version (year & month). */
+#define	BSD	199507		/* System version (year & month). */
 #define BSD4_3	1
 #define BSD4_4	1
 
@@ -65,7 +65,7 @@
  *
  */
 
-#define __NetBSD_Version__  104060000	/* NetBSD 1.4F */
+#define __NetBSD_Version__  104070000	/* NetBSD 1.4G */
 
 /*
  * Historical NetBSD #define
