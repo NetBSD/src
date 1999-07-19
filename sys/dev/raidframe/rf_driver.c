d0cae0de8e097c91809291da962fc8327869cb46
oster <oster@NetBSD.org>
1999-07-19 01:36:07 +0000

    rf_UnconfigureVnodes() needs to be called in the DO_RAID_FAIL macro.

--- sys/dev/raidframe/rf_driver.c
+++ sys/dev/raidframe/rf_driver.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: rf_driver.c,v 1.11 1999/03/14 21:53:31 oster Exp $	*/
+/*	$NetBSD: rf_driver.c,v 1.12 1999/07/19 01:36:07 oster Exp $	*/
 /*-
  * Copyright (c) 1999 The NetBSD Foundation, Inc.
  * All rights reserved.
@@ -376,6 +376,7 @@ rf_UnconfigureVnodes( raidPtr )
 }
 
 #define DO_RAID_FAIL() { \
+	rf_UnconfigureVnodes(raidPtr); \
 	rf_ShutdownList(&raidPtr->shutdownList); \
 	rf_UnconfigureArray(); \
 }
