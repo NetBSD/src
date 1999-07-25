29eac230458e8d1d83d48321c5f839bb9251c4bb
thorpej <thorpej>
1999-07-25 06:24:22 +0000

    Add a spin lock mode to the lock manager.  Provides a read/write spin lock facility.  Some code and ideas from Ross Harvey.

--- sys/sys/lock.h
+++ sys/sys/lock.h
@@ -1,4 +1,44 @@
-/*	$NetBSD: lock.h,v 1.18 1999/04/06 23:08:48 thorpej Exp $	*/
+/*	$NetBSD: lock.h,v 1.19 1999/07/25 06:24:22 thorpej Exp $	*/
+
+/*-
+ * Copyright (c) 1999 The NetBSD Foundation, Inc.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to The NetBSD Foundation
+ * by Jason R. Thorpe of the Numerical Aerospace Simulation Facility,
+ * NASA Ames Research Center.
+ *
+ * This code is derived from software contributed to The NetBSD Foundation
+ * by Ross Harvey.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed by the NetBSD
+ *	Foundation, Inc. and its contributors.
+ * 4. Neither the name of The NetBSD Foundation nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 
 /* 
  * Copyright (c) 1995
@@ -86,13 +126,41 @@ struct lock {
 	struct	simplelock lk_interlock; /* lock on remaining fields */
 	u_int	lk_flags;		/* see below */
 	int	lk_sharecount;		/* # of accepted shared locks */
-	int	lk_waitcount;		/* # of processes sleeping for lock */
 	short	lk_exclusivecount;	/* # of recursive exclusive locks */
-	short	lk_prio;		/* priority at which to sleep */
-	const char *lk_wmesg;		/* resource sleeping (for tsleep) */
-	int	lk_timo;		/* maximum sleep time (for tsleep) */
-	pid_t	lk_lockholder;		/* pid of exclusive lock holder */
 	short	lk_recurselevel;	/* lvl above which recursion ok */
+
+	/*
+	 * This is the sleep message for sleep locks, and a simple name
+	 * for spin locks.
+	 */
+	const char *lk_wmesg;
+
+	union {
+		struct {
+			/* pid of exclusive lock holder */
+			pid_t lk_sleep_lockholder;
+
+			/* # of processes sleeping for lock */
+			int lk_sleep_waitcount;
+
+			/* priority at which to sleep */
+			int lk_sleep_prio;
+
+			/* maximum sleep time (for tsleep) */
+			int lk_sleep_timo;
+		} lk_un_sleep;
+		struct {
+			/* CPU ID of exclusive lock holder */
+			u_long lk_spin_cpu;
+		} lk_un_spin;
+	} lk_un;
+
+#define	lk_lockholder	lk_un.lk_un_sleep.lk_sleep_lockholder
+#define	lk_waitcount	lk_un.lk_un_sleep.lk_sleep_waitcount
+#define	lk_prio		lk_un.lk_un_sleep.lk_sleep_prio
+#define	lk_timo		lk_un.lk_un_sleep.lk_sleep_timo
+
+#define	lk_cpu		lk_un.lk_un_spin.lk_spin_cpu
 };
 /*
  * Lock request types:
@@ -127,14 +195,14 @@ struct lock {
  *
  * These are flags that are passed to the lockmgr routine.
  */
-#define LK_TYPE_MASK	0x0000000f	/* type of lock sought */
-#define LK_SHARED	0x00000001	/* shared lock */
-#define LK_EXCLUSIVE	0x00000002	/* exclusive lock */
-#define LK_UPGRADE	0x00000003	/* shared-to-exclusive upgrade */
-#define LK_EXCLUPGRADE	0x00000004	/* first shared-to-exclusive upgrade */
-#define LK_DOWNGRADE	0x00000005	/* exclusive-to-shared downgrade */
-#define LK_RELEASE	0x00000006	/* release any type of lock */
-#define LK_DRAIN	0x00000007	/* wait for all lock activity to end */
+#define	LK_TYPE_MASK	0x0000000f	/* type of lock sought */
+#define	LK_SHARED	0x00000001	/* shared lock */
+#define	LK_EXCLUSIVE	0x00000002	/* exclusive lock */
+#define	LK_UPGRADE	0x00000003	/* shared-to-exclusive upgrade */
+#define	LK_EXCLUPGRADE	0x00000004	/* first shared-to-exclusive upgrade */
+#define	LK_DOWNGRADE	0x00000005	/* exclusive-to-shared downgrade */
+#define	LK_RELEASE	0x00000006	/* release any type of lock */
+#define	LK_DRAIN	0x00000007	/* wait for all lock activity to end */
 /*
  * External lock flags.
  *
@@ -142,32 +210,33 @@ struct lock {
  * or passed in as arguments to the lock manager. The LK_REENABLE flag may be
  * set only at the release of a lock obtained by drain.
  */
-#define LK_EXTFLG_MASK	0x00300070	/* mask of external flags */
-#define LK_NOWAIT	0x00000010	/* do not sleep to await lock */
-#define LK_SLEEPFAIL	0x00000020	/* sleep, then return failure */
-#define LK_CANRECURSE	0x00000040	/* this may be recursive lock attempt */
-#define LK_REENABLE	0x00000080	/* lock is be reenabled after drain */
-#define LK_SETRECURSE	0x00100000	/* other locks while we have it OK */
-#define LK_RECURSEFAIL  0x00200000	/* attempt at recursive lock fails */
+#define	LK_EXTFLG_MASK	0x00700070	/* mask of external flags */
+#define	LK_NOWAIT	0x00000010	/* do not sleep to await lock */
+#define	LK_SLEEPFAIL	0x00000020	/* sleep, then return failure */
+#define	LK_CANRECURSE	0x00000040	/* this may be recursive lock attempt */
+#define	LK_REENABLE	0x00000080	/* lock is be reenabled after drain */
+#define	LK_SETRECURSE	0x00100000	/* other locks while we have it OK */
+#define	LK_RECURSEFAIL  0x00200000	/* attempt at recursive lock fails */
+#define	LK_SPIN		0x00400000	/* lock spins instead of sleeps */
 /*
  * Internal lock flags.
  *
  * These flags are used internally to the lock manager.
  */
-#define LK_WANT_UPGRADE	0x00000100	/* waiting for share-to-excl upgrade */
-#define LK_WANT_EXCL	0x00000200	/* exclusive lock sought */
-#define LK_HAVE_EXCL	0x00000400	/* exclusive lock obtained */
-#define LK_WAITDRAIN	0x00000800	/* process waiting for lock to drain */
-#define LK_DRAINING	0x00004000	/* lock is being drained */
-#define LK_DRAINED	0x00008000	/* lock has been decommissioned */
+#define	LK_WANT_UPGRADE	0x00000100	/* waiting for share-to-excl upgrade */
+#define	LK_WANT_EXCL	0x00000200	/* exclusive lock sought */
+#define	LK_HAVE_EXCL	0x00000400	/* exclusive lock obtained */
+#define	LK_WAITDRAIN	0x00000800	/* process waiting for lock to drain */
+#define	LK_DRAINING	0x00004000	/* lock is being drained */
+#define	LK_DRAINED	0x00008000	/* lock has been decommissioned */
 /*
  * Control flags
  *
  * Non-persistent external flags.
  */
-#define LK_INTERLOCK	0x00010000	/* unlock passed simple lock after
+#define	LK_INTERLOCK	0x00010000	/* unlock passed simple lock after
 					   getting lk_interlock */
-#define LK_RETRY	0x00020000	/* vn_lock: retry until locked */
+#define	LK_RETRY	0x00020000	/* vn_lock: retry until locked */
 
 /*
  * Lock return status.
@@ -187,10 +256,11 @@ struct lock {
  */
 
 /*
- * Indicator that no process holds exclusive lock
+ * Indicator that no process/cpu holds exclusive lock
  */
-#define LK_KERNPROC ((pid_t) -2)
-#define LK_NOPROC ((pid_t) -1)
+#define	LK_KERNPROC	((pid_t) -2)
+#define	LK_NOPROC	((pid_t) -1)
+#define	LK_NOCPU	((u_long) -1)
 
 struct proc;
 
@@ -199,7 +269,13 @@ void	lockinit __P((struct lock *, int prio, const char *wmesg, int timo,
 int	lockmgr __P((__volatile struct lock *, u_int flags,
 			struct simplelock *));
 int	lockstatus __P((struct lock *));
-void	lockmgr_printinfo __P((struct lock *));
+void	lockmgr_printinfo __P((__volatile struct lock *));
+
+#define	spinlockinit(lkp, name, flags)					\
+	lockinit((lkp), 0, (name), 0, (flags) | LK_SPIN)
+
+#define	spinlockmgr(lkp, flags, intrlk)					\
+	lockmgr((lkp), (flags) | LK_SPIN, (intrlk))
 
 #if defined(LOCKDEBUG) && !defined(MULTIPROCESSOR)
 void _simple_unlock __P((__volatile struct simplelock *alp, const char *, int));
