420d34ff13c6da80780eb1fbe3a1a00207c0d41f
hubertf <hubertf@NetBSD.org>
1999-07-25 00:06:13 +0000

    Hardcore const poisoning Patch submitted by Joseph Myers <jsm28@cam.ac.uk> in PR 8057

--- games/backgammon/teachgammon/ttext1.c
+++ games/backgammon/teachgammon/ttext1.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: ttext1.c,v 1.5 1999/02/10 12:29:48 hubertf Exp $	*/
+/*	$NetBSD: ttext1.c,v 1.6 1999/07/25 00:06:13 hubertf Exp $	*/
 
 /*
  * Copyright (c) 1980, 1993
@@ -38,15 +38,15 @@
 #if 0
 static char sccsid[] = "@(#)ttext1.c	8.1 (Berkeley) 5/31/93";
 #else
-__RCSID("$NetBSD: ttext1.c,v 1.5 1999/02/10 12:29:48 hubertf Exp $");
+__RCSID("$NetBSD: ttext1.c,v 1.6 1999/07/25 00:06:13 hubertf Exp $");
 #endif
 #endif				/* not lint */
 
 #include "back.h"
 #include "tutor.h"
 
-const char   *opts = " QIMRHEDSPT";
-const char   *prompt = "-->";
+const char   *const opts = " QIMRHEDSPT";
+const char   *const prompt = "-->";
 
 const char   *const list[] = {
 	"\n\n\tI\tIntroduction to Backgammon",
