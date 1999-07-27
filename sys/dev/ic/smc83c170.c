079015017a5652dfd1685e6a801d1c795460f85f
thorpej <thorpej>
1999-07-27 00:55:34 +0000

    Make this "should work" on big endian systems.

--- sys/dev/ic/smc83c170.c
+++ sys/dev/ic/smc83c170.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: smc83c170.c,v 1.17 1999/06/18 17:44:06 thorpej Exp $	*/
+/*	$NetBSD: smc83c170.c,v 1.18 1999/07/27 00:55:34 thorpej Exp $	*/
 
 /*-
  * Copyright (c) 1998, 1999 The NetBSD Foundation, Inc.
@@ -936,6 +936,9 @@ epic_init(sc)
 	 *	- automatic advance to next receive frame
 	 */
 	genctl = GENCTL_RX_FIFO_THRESH0 | GENCTL_ONECOPY;
+#if BYTE_ORDER == BIG_ENDIAN
+	genctl |= GENCTL_BIG_ENDIAN;
+#endif
 	bus_space_write_4(st, sh, EPIC_GENCTL, genctl);
 
 	/*
