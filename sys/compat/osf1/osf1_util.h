d3cc06965dbca4b4e750199b54cf64701c75b21e
drochner <drochner@NetBSD.org>
1999-08-05 13:04:29 +0000

    back out errornous commit to old release branch

--- sys/compat/osf1/osf1_util.h
+++ sys/compat/osf1/osf1_util.h
@@ -1,9 +1,1 @@
 #include <compat/common/compat_util.h>
-
-extern const char osf1_emul_path[];
-
-#define OSF1_CHECK_ALT_EXIST(p, sgp, path) \
-    CHECK_ALT_EXIST(p, sgp, osf1_emul_path, path)
-
-#define OSF1_CHECK_ALT_CREAT(p, sgp, path) \
-    CHECK_ALT_CREAT(p, sgp, osf1_emul_path, path)
