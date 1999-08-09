356180b8e831a12e92d7e354f898433120b10247
drochner <drochner@NetBSD.org>
1999-08-09 12:53:53 +0000

    sync to head rev. 1.15 (pmap_extract() interface change)

--- sys/arch/pmax/pmax/bus_dma.c
+++ sys/arch/pmax/pmax/bus_dma.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: bus_dma.c,v 1.9.2.4 1999/06/11 00:53:34 nisimura Exp $	*/
+/*	$NetBSD: bus_dma.c,v 1.9.2.5 1999/08/09 12:53:53 drochner Exp $	*/
 
 /*-
  * Copyright (c) 1997, 1998 The NetBSD Foundation, Inc.
@@ -178,8 +178,8 @@ _bus_dmamap_load_buffer(map, buf, buflen, p, flags,
 		 * Get the physical address for this segment.
 		 */
 		if (p != NULL)
-			curaddr = pmap_extract(p->p_vmspace->vm_map.pmap,
-			    vaddr);
+			(void) pmap_extract(p->p_vmspace->vm_map.pmap,
+			    vaddr, &curaddr);
 		else
 			curaddr = kvtophys(vaddr);
 
