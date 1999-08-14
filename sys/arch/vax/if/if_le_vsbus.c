8b08f0b5d79dff525b33c7ac785d63d42b486183
ragge <ragge@NetBSD.org>
1999-08-14 18:40:23 +0000

    Support for on-board LANCE on MicroVAX 3300/3400. Also use MI LANCE code for VAXstations again; though not functioning correctly on all types of machines.  (Will fix that later).

--- sys/arch/vax/if/if_le_vsbus.c
+++ sys/arch/vax/if/if_le_vsbus.c
@@ -1,12 +1,11 @@
-/*	$NetBSD: if_le.c,v 1.14 1999/08/14 18:40:23 ragge Exp $	*/
+/*	$NetBSD: if_le_vsbus.c,v 1.1 1999/08/14 18:40:23 ragge Exp $	*/
 
 /*-
- * Copyright (c) 1997, 1998 The NetBSD Foundation, Inc.
+ * Copyright (c) 1998 The NetBSD Foundation, Inc.
  * All rights reserved.
  *
  * This code is derived from software contributed to The NetBSD Foundation
- * by Charles M. Hannum and by Jason R. Thorpe of the Numerical Aerospace
- * Simulation Facility, NASA Ames Research Center.
+ * by Charles M. Hannum.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -97,9 +96,9 @@
 #endif
 
 #include <machine/cpu.h>
-#include <machine/nexus.h>
+#include <machine/bus.h>
 #include <machine/rpb.h>
-#include <machine/scb.h>
+#include <machine/vsbus.h>
 
 #include <dev/ic/lancereg.h>
 #include <dev/ic/lancevar.h>
@@ -108,25 +107,20 @@
 
 #include "ioconf.h"
 
-#define	LEVEC	0xd4	/* Interrupt vector on 3300/3400 */
-
 struct le_softc {
 	struct	am7990_softc sc_am7990; /* Must be first */
 	volatile u_short *sc_rap;
 	volatile u_short *sc_rdp;
 };
 
-int	le_ibus_match __P((struct device *, struct cfdata *, void *));
-void	le_ibus_attach __P((struct device *, struct device *, void *));
-void	lewrcsr __P((struct lance_softc *, u_int16_t, u_int16_t));
-u_int16_t lerdcsr __P((struct lance_softc *, u_int16_t));
-void	lance_copytobuf_gap2 __P((struct lance_softc *, void *, int, int));
-void	lance_copyfrombuf_gap2 __P((struct lance_softc *, void *, int, int));
-void	lance_zerobuf_gap2 __P((struct lance_softc *, int, int));
-void	leintr __P((int));
+int	le_vsbus_match __P((struct device *, struct cfdata *, void *));
+void	le_vsbus_attach __P((struct device *, struct device *, void *));
+static	void	lewrcsr __P((struct lance_softc *, u_int16_t, u_int16_t));
+static	u_int16_t lerdcsr __P((struct lance_softc *, u_int16_t));
+static	void	leintr __P((int));
 
-struct cfattach le_ibus_ca = {
-	sizeof(struct le_softc), le_ibus_match, le_ibus_attach
+struct cfattach le_vsbus_ca = {
+	sizeof(struct le_softc), le_vsbus_match, le_vsbus_attach
 };
 
 void
@@ -152,45 +146,50 @@ lerdcsr(ls, port)
 }
 
 int
-le_ibus_match(parent, cf, aux)
+le_vsbus_match(parent, cf, aux)
 	struct device *parent;
 	struct cfdata *cf;
 	void *aux;
 {
-	struct bp_conf *bp = aux;
+	struct  vsbus_attach_args *va = aux;
+	volatile short *rdp, *rap;
 
-	if (strcmp("lance", bp->type))
-		return 0;
-	return 1;
+	rdp = (short *)va->va_addr;
+	rap = rdp + 2;
+
+	/* Make sure the chip is stopped. */
+	*rap = LE_CSR0;
+	*rdp = LE_C0_STOP;
+	DELAY(100);
+	*rdp = LE_C0_INIT|LE_C0_INEA;
+
+	/* Wait for initialization to finish. */
+	DELAY(100000);
+	va->va_ivec = leintr;
+
+	/* Should have interrupted by now */
+	if (*rdp & LE_C0_IDON)
+		return 1;
+	return 0;
 }
 
 void
-le_ibus_attach(parent, self, aux)
+le_vsbus_attach(parent, self, aux)
 	struct device *parent, *self;
 	void *aux;
 {
+	struct  vsbus_attach_args *va = aux;
 	struct le_softc *sc = (void *)self;
+	bus_dma_segment_t seg;
 	int *lance_addr;
-	int i, vec, br;
+	int i, err, rseg;
 
-	sc->sc_rdp = (short *)vax_map_physmem(0x20084400, 1);
+	sc->sc_rdp = (short *)vax_map_physmem(NI_BASE, 1);
 	sc->sc_rap = sc->sc_rdp + 2;
 
-	/*
-	 * Set interrupt vector, by forcing an interrupt.
-	 */
-	scb_vecref(0, 0); /* Clear vector ref */
-	*sc->sc_rap = LE_CSR0;
-	*sc->sc_rdp = LE_C0_STOP;
-	DELAY(100);
-	*sc->sc_rdp = LE_C0_INIT|LE_C0_INEA;
-	DELAY(100000);
-	i = scb_vecref(&vec, &br);
-	if (i == 0 || vec == 0)
-		return;
-	scb_vecalloc(vec, leintr, self->dv_unit, SCB_ISTACK);
+	/* Prettier printout */
+	printf("\n%s", self->dv_xname);
 
-	printf(": vec %o ipl %x\n%s", vec, br, self->dv_xname);
 	/*
 	 * MD functions.
 	 */
@@ -198,28 +197,39 @@ le_ibus_attach(parent, self, aux)
 	sc->sc_am7990.lsc.sc_wrcsr = lewrcsr;
 	sc->sc_am7990.lsc.sc_nocarrier = NULL;
 
-	sc->sc_am7990.lsc.sc_mem =
-	    (void *)uvm_km_valloc(kernel_map, (128 * 1024));
-	if (sc->sc_am7990.lsc.sc_mem == 0)
-		return;
+        /*
+         * Allocate a (DMA-safe) block for all descriptors and buffers.
+         */
 
-	ioaccess((vaddr_t)sc->sc_am7990.lsc.sc_mem, 0x20120000,
-	    (128 * 1024) >> VAX_PGSHIFT);
+#define ALLOCSIZ (64 * 1024)
+        err = bus_dmamem_alloc(va->va_dmat, ALLOCSIZ, NBPG, 0, 
+            &seg, 1, &rseg, BUS_DMA_NOWAIT);
+        if (err) {
+                printf(": unable to alloc buffer block: err %d\n", err);
+                return;
+        }
+        err = bus_dmamem_map(va->va_dmat, &seg, rseg, ALLOCSIZ, 
+            (caddr_t *)&sc->sc_am7990.lsc.sc_mem,
+	    BUS_DMA_NOWAIT|BUS_DMA_COHERENT);
+        if (err) {
+                printf(": unable to map buffer block: err %d\n", err);
+                bus_dmamem_free(va->va_dmat, &seg, rseg);
+                return;
+        }
+	sc->sc_am7990.lsc.sc_addr =
+	    (paddr_t)sc->sc_am7990.lsc.sc_mem & 0xffffff;
+	sc->sc_am7990.lsc.sc_memsize = ALLOCSIZ;
 
-	
-	sc->sc_am7990.lsc.sc_addr = 0;
-	sc->sc_am7990.lsc.sc_memsize = (64 * 1024);
-
-	sc->sc_am7990.lsc.sc_copytodesc = lance_copytobuf_gap2;
-	sc->sc_am7990.lsc.sc_copyfromdesc = lance_copyfrombuf_gap2;
-	sc->sc_am7990.lsc.sc_copytobuf = lance_copytobuf_gap2;
-	sc->sc_am7990.lsc.sc_copyfrombuf = lance_copyfrombuf_gap2;
-	sc->sc_am7990.lsc.sc_zerobuf = lance_zerobuf_gap2;
+	sc->sc_am7990.lsc.sc_copytodesc = lance_copytobuf_contig;
+	sc->sc_am7990.lsc.sc_copyfromdesc = lance_copyfrombuf_contig;
+	sc->sc_am7990.lsc.sc_copytobuf = lance_copytobuf_contig;
+	sc->sc_am7990.lsc.sc_copyfrombuf = lance_copyfrombuf_contig;
+	sc->sc_am7990.lsc.sc_zerobuf = lance_zerobuf_contig;
 
 	/*
 	 * Get the ethernet address out of rom
 	 */
-	lance_addr = (int *)vax_map_physmem(0x20084200, 1);
+	lance_addr = (int *)vax_map_physmem(NI_ADDR, 1);
 	for (i = 0; i < 6; i++)
 		sc->sc_am7990.lsc.sc_enaddr[i] = (u_char)lance_addr[i];
 	vax_unmap_physmem((vaddr_t)lance_addr, 1);
@@ -235,94 +245,6 @@ le_ibus_attach(parent, self, aux)
 	 */
 	if (B_TYPE(bootdev) == BDEV_LE)
 		booted_from = self;
-}
-
-/*
- * gap2: two bytes of data followed by two bytes of pad.
- *
- * Buffers must be 4-byte aligned.  The code doesn't worry about
- * doing an extra byte.
- */
-
-void
-lance_copytobuf_gap2(sc, fromv, boff, len)
-	struct lance_softc *sc;
-	void *fromv;
-	int boff;
-	register int len;
-{
-	volatile caddr_t buf = sc->sc_mem;
-	register caddr_t from = fromv;
-	register volatile u_int16_t *bptr;
-
-	if (boff & 0x1) {
-		/* handle unaligned first byte */
-		bptr = ((volatile u_int16_t *)buf) + (boff - 1);
-		*bptr = (*from++ << 8) | (*bptr & 0xff);
-		bptr += 2;
-		len--;
-	} else
-		bptr = ((volatile u_int16_t *)buf) + boff;
-	while (len > 1) {
-		*bptr = (from[1] << 8) | (from[0] & 0xff);
-		bptr += 2;
-		from += 2;
-		len -= 2;
-	}
-	if (len == 1)
-		*bptr = (u_int16_t)*from;
-}
-
-void
-lance_copyfrombuf_gap2(sc, tov, boff, len)
-	struct lance_softc *sc;
-	void *tov;
-	int boff, len;
-{
-	volatile caddr_t buf = sc->sc_mem;
-	register caddr_t to = tov;
-	register volatile u_int16_t *bptr;
-	register u_int16_t tmp;
-
-	if (boff & 0x1) {
-		/* handle unaligned first byte */
-		bptr = ((volatile u_int16_t *)buf) + (boff - 1);
-		*to++ = (*bptr >> 8) & 0xff;
-		bptr += 2;
-		len--;
-	} else
-		bptr = ((volatile u_int16_t *)buf) + boff;
-	while (len > 1) {
-		tmp = *bptr;
-		*to++ = tmp & 0xff;
-		*to++ = (tmp >> 8) & 0xff;
-		bptr += 2;
-		len -= 2;
-	}
-	if (len == 1)
-		*to = *bptr & 0xff;
-}
-
-void
-lance_zerobuf_gap2(sc, boff, len)
-	struct lance_softc *sc;
-	int boff, len;
-{
-	volatile caddr_t buf = sc->sc_mem;
-	register volatile u_int16_t *bptr;
-
-	if ((unsigned)boff & 0x1) {
-		bptr = ((volatile u_int16_t *)buf) + (boff - 1);
-		*bptr &= 0xff;
-		bptr += 2;
-		len--;
-	} else
-		bptr = ((volatile u_int16_t *)buf) + boff;
-	while (len > 0) {
-		*bptr = 0;
-		bptr += 2;
-		len -= 2;
-	}
 }
 
 void
