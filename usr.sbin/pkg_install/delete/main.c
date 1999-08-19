c388b2d43c3c1efeefc1bf525647e48d91fd51ab
hubertf <hubertf@NetBSD.org>
1999-08-19 19:37:20 +0000

    Allow specifying a package name without a version.
    If no (installed) package is found with the given name, "-[0-9]*" is
    appended, and the search retried. Withs with any options to pkg_delete
    and (esp.) pkg_info:
    
    	pkg_info kde
    	pkg_info -e emacs
    	pkg_info -qL xlock
    	pkg_delete -v emacs

--- usr.sbin/pkg_install/delete/main.c
+++ usr.sbin/pkg_install/delete/main.c
@@ -1,11 +1,11 @@
-/*	$NetBSD: main.c,v 1.11 1999/03/22 05:02:40 hubertf Exp $	*/
+/*	$NetBSD: main.c,v 1.12 1999/08/19 19:37:21 hubertf Exp $	*/
 
 #include <sys/cdefs.h>
 #ifndef lint
 #if 0
 static char *rcsid = "from FreeBSD Id: main.c,v 1.11 1997/10/08 07:46:48 charnier Exp";
 #else
-__RCSID("$NetBSD: main.c,v 1.11 1999/03/22 05:02:40 hubertf Exp $");
+__RCSID("$NetBSD: main.c,v 1.12 1999/08/19 19:37:21 hubertf Exp $");
 #endif
 #endif
 
@@ -54,7 +54,7 @@ usage(void)
     exit(1);
 }
 
-static int
+int
 find_fn(const char *pkg, char *data)
 {
     lpkg_t *lpp;
