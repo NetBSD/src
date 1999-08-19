07ced192f4b8a2227272fdb7c3fa2031c49d5237
itojun <itojun>
1999-08-19 17:31:05 +0000

    sync with recent KAME code. - do not use s6_addr{8,16,32}, which are nonstandard. - SIGHUP handling now works correctly.

--- usr.sbin/pim6dd/kern.c
+++ usr.sbin/pim6dd/kern.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: kern.c,v 1.1 1999/07/17 14:06:25 itojun Exp $	*/
+/*	$NetBSD: kern.c,v 1.2 1999/08/19 17:31:06 itojun Exp $	*/
 
 /*
  *  Copyright (c) 1998 by the University of Southern California.
@@ -36,7 +36,7 @@
  *  Questions concerning this software should be directed to 
  *  Pavlin Ivanov Radoslavov (pavlin@catarina.usc.edu)
  *
- *  KAME Id: kern.c,v 1.2 1998/06/01 22:27:14 kurtw Exp
+ *  KAME Id: kern.c,v 1.1.1.1 1999/08/08 23:30:52 itojun Exp
  */
 /*
  * Part of this program has been derived from mrouted.
