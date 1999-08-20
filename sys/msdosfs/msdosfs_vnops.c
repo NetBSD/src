941f9dadbbf0f1b15b13aa1d2adb6fa5eb9ae5ef
cgd <cgd@NetBSD.org>
1999-08-20 05:39:36 +0000

    pull up rev 1.83 from trunk. (cgd)

--- sys/msdosfs/msdosfs_vnops.c
+++ sys/msdosfs/msdosfs_vnops.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: msdosfs_vnops.c,v 1.79.2.2 1999/06/21 14:39:46 perry Exp $	*/
+/*	$NetBSD: msdosfs_vnops.c,v 1.79.2.3 1999/08/20 05:39:36 cgd Exp $	*/
 
 /*-
  * Copyright (C) 1994, 1995, 1997 Wolfgang Solfrank.
@@ -373,7 +373,7 @@ msdosfs_setattr(v)
 		printf("    va_type %d, va_nlink %x, va_fsid %lx, va_fileid %lx\n",
 		    vap->va_type, vap->va_nlink, vap->va_fsid, vap->va_fileid);
 		printf("    va_blocksize %lx, va_rdev %x, va_bytes %qx, va_gen %lx\n",
-		    vap->va_blocksize, vap->va_rdev, vap->va_bytes, vap->va_gen);
+		    vap->va_blocksize, vap->va_rdev, (long long)vap->va_bytes, vap->va_gen);
 		printf("    va_uid %x, va_gid %x\n",
 		    vap->va_uid, vap->va_gid);
 #endif
