95816b95dfe9847a8a19a2d9ba6bc670da1e72c2
simonb <simonb@NetBSD.org>
1999-08-21 05:39:51 +0000

    Include <sys/bswap.h> for function prototypes.  i386, pc532 and vax still include <machine/byte_swap.h> and define macros for some of the bswap*() functions.

--- sys/arch/x68k/include/bswap.h
+++ sys/arch/x68k/include/bswap.h
@@ -1,8 +1,3 @@
-/*     $NetBSD: bswap.h,v 1.2 1999/03/16 16:30:21 minoura Exp $   */
-
-#ifndef _MACHINE_BSWAP_H_
-#define _MACHINE_BSWAP_H_
+/*     $NetBSD: bswap.h,v 1.3 1999/08/21 05:39:55 simonb Exp $   */
 
 #include <m68k/bswap.h>
-
-#endif
