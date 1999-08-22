5a018aea673adc48c8df10b48b0ea685eaf19bc3
he <he@NetBSD.org>
1999-08-22 17:05:54 +0000

    Pullup eisadevs 1.16 and if_ep_eisa.c 1.20-1.21 to recognize a 3Com 3c509 Combo configured in EISA mode, fixing PR8234.  From tron.

--- sys/dev/eisa/eisadevs.h
+++ sys/dev/eisa/eisadevs.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: eisadevs.h,v 1.17 1998/07/01 21:59:20 kleink Exp $	*/
+/*	$NetBSD: eisadevs.h,v 1.17.8.1 1999/08/22 17:05:54 he Exp $	*/
 
 /*
  * THIS FILE AUTOMATICALLY GENERATED.  DO NOT EDIT.
@@ -84,6 +84,7 @@
 #define	EISA_PRODUCT_TCM5091	"3Com 3C509 Ethernet"
 #define	EISA_PRODUCT_TCM5092	"3Com 3C579-TP Ethernet"
 #define	EISA_PRODUCT_TCM5093	"3Com 3C579 Ethernet"
+#define	EISA_PRODUCT_TCM5094	"3Com 3C509 Ethernet Combo"
 #define	EISA_PRODUCT_TCM5920	"3Com 3C592 Etherlink III"
 #define	EISA_PRODUCT_TCM5970	"3Com 3C597 Fast Etherlink TX"
 #define	EISA_PRODUCT_TCM5971	"3Com 3C597 Fast Etherlink T4"
