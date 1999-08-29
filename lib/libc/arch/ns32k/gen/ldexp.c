de4ce471dbbb23303ca0ae440a549540cb269bf7
mycroft <mycroft@NetBSD.org>
1999-08-29 22:45:32 +0000

    ldexp(denormal, exp>1023) would generate the wrong result in all non-overflow cases.  Totally rework this code to fix this bug *and* make it faster.

--- lib/libc/arch/ns32k/gen/ldexp.c
+++ lib/libc/arch/ns32k/gen/ldexp.c
@@ -1,7 +1,7 @@
-/*	$NetBSD: ldexp.c,v 1.1 1999/08/29 19:08:44 mycroft Exp $	*/
+/*	$NetBSD: ldexp.c,v 1.2 1999/08/29 22:45:34 mycroft Exp $	*/
 
 /*-
- * Copyright (c) 1998 The NetBSD Foundation, Inc.
+ * Copyright (c) 1999 The NetBSD Foundation, Inc.
  * All rights reserved.
  *
  * This code is derived from software contributed to The NetBSD Foundation
@@ -38,31 +38,117 @@
 
 #include <sys/cdefs.h>
 #if defined(LIBC_SCCS) && !defined(lint)
-__RCSID("$NetBSD: ldexp.c,v 1.1 1999/08/29 19:08:44 mycroft Exp $");
+__RCSID("$NetBSD: ldexp.c,v 1.2 1999/08/29 22:45:34 mycroft Exp $");
 #endif /* LIBC_SCCS and not lint */
 
 #include <sys/types.h>
 #include <machine/ieee.h>
+#include <errno.h>
 #include <math.h>
 
 /*
- * ldexp(value, exp): return value * (2 ** exp).
+ * Multiply the given value by 2^exp.
  */
 double
-ldexp(value, exp)
-	double value;
+ldexp(val, exp)
+	double val;
 	int exp;
 {
-	double temp;
+	register int oldexp, newexp, mulexp;
+	union {
+		double v;
+		struct ieee_double s;
+	} u, mul;
 
-#if __GNUC__ >= 2
-	__asm ("fscale"
-		: "=t" (temp)
-		: "0" (value), "u" ((double)exp));
-#else
-	__asm ("fscale; fstp %%st(1)"
-		: "=f" (temp)
-		: "f" (value), "0" ((double)exp));
-#endif
-	return (temp);
+	u.v = val;
+	oldexp = u.s.dbl_exp;
+
+	/*
+	 * If input is zero, Inf or NaN, just return it.
+	 */
+	if (u.v == 0.0 || oldexp == DBL_EXP_INFNAN)
+		return (val);
+
+	if (oldexp == 0) {
+		/*
+		 * u.v is denormal.  We must adjust it so that the exponent
+		 * arithmetic below will work.
+		 */
+		if (exp <= DBL_EXP_BIAS) {
+			/*
+			 * Optimization: if the scaling can be done in a single
+			 * multiply, or underflows, just do it now.
+			 */
+			if (exp <= -DBL_FRACBITS) {
+				errno = ERANGE;
+				return (0.0);
+			}
+			mul.v = 1.0;
+			mul.s.dbl_exp = exp + DBL_EXP_BIAS;
+			u.v *= mul.v;
+			if (u.v == 0.0) {
+				errno = ERANGE;
+				return (0.0);
+			}
+			return (u.v);
+		} else {
+			/*
+			 * We know that exp is very large, and therefore the
+			 * result cannot be denormal (though it may be Inf).
+			 * Shift u.v by just enough to make it normal.
+			 */
+			mul.v = 1.0;
+			mul.s.dbl_exp = DBL_FRACBITS + DBL_EXP_BIAS;
+			u.v *= mul.v;
+			exp -= DBL_FRACBITS;
+			oldexp = u.s.dbl_exp;
+		}
+	}
+
+	/*
+	 * u.v is now normalized and oldexp has been adjusted if necessary.
+	 * Calculate the new exponent and check for underflow and overflow.
+	 */
+	newexp = oldexp + exp;
+
+	if (newexp >= DBL_EXP_INFNAN) {
+		/*
+		 * The result overflowed; return +/-Inf.
+		 */
+		u.s.dbl_exp = DBL_EXP_INFNAN;
+		u.s.dbl_frach = 0;
+		u.s.dbl_fracl = 0;
+		errno = ERANGE;
+		return (u.v);
+	} else if (newexp <= 0) {
+		/*
+		 * The output number is either denormal or underflows (see
+		 * comments in machine/ieee.h).
+		 */
+		if (newexp <= -DBL_FRACBITS) {
+			errno = ERANGE;
+			return (0.0);
+		}
+		/*
+		 * Denormalize the result.  We do this with a multiply.  If exp
+		 * is very large, it won't fit in a double, so we have to
+		 * adjust the exponent first.  This is safe because we know
+		 * that u.v is normal at this point.
+		 */
+		if (exp <= -DBL_EXP_BIAS) {
+			u.s.dbl_exp = 1;
+			exp += oldexp - 1;
+		}
+		mul.v = 1.0;
+		mul.s.dbl_exp = exp + DBL_EXP_BIAS;
+		u.v *= mul.v;
+		return (u.v);
+	} else {
+		/*
+		 * The result is normal; just replace the old exponent with the
+		 * new one.
+		 */
+		u.s.dbl_exp = newexp;
+		return (u.v);
+	}
 }
