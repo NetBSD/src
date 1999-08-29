bf5666e7ffec1ca30000b159228f71fd4a9099f9
he <he@NetBSD.org>
1999-08-29 20:59:05 +0000

    Pull up revision 1.7:   Recognize 3c905C-TX card.  (bouyer)

--- sys/dev/pci/if_ex_pci.c
+++ sys/dev/pci/if_ex_pci.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: if_ex_pci.c,v 1.5.2.1 1999/06/22 17:14:52 perry Exp $	*/
+/*	$NetBSD: if_ex_pci.c,v 1.5.2.2 1999/08/29 20:59:05 he Exp $	*/
 
 /*-
  * Copyright (c) 1998 The NetBSD Foundation, Inc.
@@ -137,6 +137,8 @@ const struct ex_pci_product {
 	/* XXX Internal PHY? */
 	{ PCI_PRODUCT_3COM_3C980SRV,	EX_CONF_90XB|EX_CONF_MII,
 	  "3c980 Server Adapter 10/100 Ethernet" },
+	{ PCI_PRODUCT_3COM_3C905CTX,	EX_CONF_90XB|EX_CONF_MII,
+	  "3c905C-TX 10/100 Ethernet with mngmt" },
 
 	{ 0,				0,
 	  NULL },
