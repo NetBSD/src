c0963d489e765d6ed9780c6e09613fccee5cbcf8
perseant <perseant@NetBSD.org>
1999-08-31 21:03:43 +0000

    Rudimentary support for LFS under UBC:
    - LFS-specific VOP_BALLOC and VOP_PUTPAGES vnode ops.
    
    - getblk VREG panic #ifdef'd out (can be reinstated when Ifile is
      internalized and Ifile can be made another type from VREG)
    
    - interface to VOP_PUTPAGES changed to pass all pager flags, not
      just sync.  FS putpages routines must know about the pager flags.
    
    - new LFS magic disk address, -2 ("unwritten"), meaning accounted for
      but not assigned to a fixed disk location (since LFS does these two
      things separately, and the previous accounting method using buffer
      headers no longer will work).  Changed references to (foo == (daddr_t)-1)
      to (foo < 0).  Since disk drivers reject all addresses < 0, this should
      not present a problem for other FSs.

--- sys/ufs/lfs/lfs_vnops.c
+++ sys/ufs/lfs/lfs_vnops.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: lfs_vnops.c,v 1.25.2.1.2.4 1999/07/11 05:52:10 chs Exp $	*/
+/*	$NetBSD: lfs_vnops.c,v 1.25.2.1.2.5 1999/08/31 21:03:46 perseant Exp $	*/
 
 /*-
  * Copyright (c) 1999 The NetBSD Foundation, Inc.
@@ -88,6 +88,7 @@
 
 #include <vm/vm.h>
 #include <uvm/uvm.h>
+#include <uvm/uvm_pager.h> /* For PGO_* flags */
 
 #include <miscfs/fifofs/fifo.h>
 #include <miscfs/genfs/genfs.h>
@@ -145,10 +146,13 @@ struct vnodeopv_entry_desc lfs_vnodeop_entries[] = {
 	{ &vop_advlock_desc, ufs_advlock },		/* advlock */
 	{ &vop_blkatoff_desc, lfs_blkatoff },		/* blkatoff */
 	{ &vop_valloc_desc, lfs_valloc },		/* valloc */
+	{ &vop_balloc_desc, lfs_balloc },		/* balloc */
 	{ &vop_vfree_desc, lfs_vfree },			/* vfree */
 	{ &vop_truncate_desc, lfs_truncate },		/* truncate */
 	{ &vop_update_desc, lfs_update },		/* update */
 	{ &vop_bwrite_desc, lfs_bwrite },		/* bwrite */
+	{ &vop_getpages_desc, lfs_getpages },		/* getpages */
+	{ &vop_putpages_desc, lfs_putpages },		/* putpages */
 	{ (struct vnodeop_desc*)NULL, (int(*) __P((void *)))NULL }
 };
 struct vnodeopv_desc lfs_vnodeop_opv_desc =
@@ -249,8 +253,6 @@ struct vnodeopv_entry_desc lfs_fifoop_entries[] = {
 	{ &vop_truncate_desc, fifo_truncate },		/* truncate */
 	{ &vop_update_desc, lfs_update },		/* update */
 	{ &vop_bwrite_desc, lfs_bwrite },		/* bwrite */
-	{ &vop_getpages_desc, lfs_getpages },		/* getpages */
-	{ &vop_putpages_desc, lfs_putpages },		/* putpages */
 	{ (struct vnodeop_desc*)NULL, (int(*) __P((void *)))NULL }
 };
 struct vnodeopv_desc lfs_fifoop_opv_desc =
@@ -634,4 +636,277 @@ lfs_reclaim(v)
 	pool_put(&lfs_inode_pool, vp->v_data);
 	vp->v_data = NULL;
 	return (0);
+}
+
+#define BUFHEAD_THROTTLE 128
+static unsigned long bufhead_count, bufhead_rem;
+
+static void
+lfs_bufcallback(struct buf *bp)
+{
+	int s;
+
+	s = splbio();
+	if(bp->b_vp)
+		brelvp(bp);
+	--bufhead_count;
+	splx(s);
+
+	uvm_aio_biodone1(bp); /* maybe set mbp->b_iodone = aio_aiodone */
+	return;
+}
+
+/*
+ * LFS-specific VM putpages routine.
+ * Encapsulate the pages in buffer headers, and call lfs_segwrite to write
+ * them out.
+ *
+ * Mostly taken from genfs_putpages.
+ * 
+ * XXX Assumes that fs block size is <= page size, in order to avoid
+ * XXX having to read partial blocks in from disk.
+ *
+ * Ideally we would use pagemove() and not have this requirement
+ * (or just call lfs_segwrite with some notation of which pages
+ * were busy).
+ */
+
+#define SET_FLUSHING(fs,vp) (fs)->lfs_flushvp = (vp)
+#define IS_FLUSHING(fs,vp)  ((fs)->lfs_flushvp == (vp))
+#define CLR_FLUSHING(fs,vp) (fs)->lfs_flushvp = NULL
+
+/* Statistics Counters */
+extern int lfs_dostats;
+extern struct lfs_stats lfs_stats;
+
+/*
+ * Utility function for flushing a single vnode, in a case where
+ * lfs_segwrite might deadlock.  Assumes that we hold the seglock.
+ * Excerpted from lfs_vflush. (XXX should be combined)
+ */
+static inline void
+lfs_writevnode(struct lfs *fs, struct vnode *vp)
+{
+	struct segment *sp;
+	struct inode *ip;
+
+	ip = VTOI(vp);
+	sp = fs->lfs_sp;
+
+	do {
+		do {
+			if (vp->v_dirtyblkhd.lh_first != NULL)
+				lfs_writefile(fs, sp, vp);
+		} while (lfs_writeinode(fs, sp, ip));
+	} while (lfs_writeseg(fs, sp) && ip->i_number == LFS_IFILE_INUM);
+	
+	if(lfs_dostats) {
+		++lfs_stats.nwrites;
+		if (sp->seg_flags & SEGM_SYNC)
+			++lfs_stats.nsync_writes;
+		if (sp->seg_flags & SEGM_CKP)
+			++lfs_stats.ncheckpoints;
+	}
+}
+
+int
+lfs_putpages(v)
+	void *v;
+{
+	struct vop_putpages_args /* {
+		struct vnode *a_vp;
+		struct vm_page **a_m;
+		int a_count;
+		int a_flags;
+		int *a_rtvals;
+	} */ *ap = v;
+
+	int s, error, npages, bshift, dev_bshift, dev_bsize;
+	char * kva;
+	off_t offset, startoffset;
+	size_t bytes, iobytes, skipbytes;
+	daddr_t lbn;
+	struct vm_page *pg;
+#if 0
+	struct vm_page *vmpp, *vmpp2;
+#endif
+	struct buf *mbp, *bp;
+	struct vnode *vp = ap->a_vp;
+	struct lfs *fs;
+	struct inode *ip;
+
+	UVMHIST_FUNC("lfs_putpages"); UVMHIST_CALLED(ubchist);
+
+	ip = VTOI(vp);
+	fs = ip->i_lfs;
+
+	lfs_seglock(fs,((ap->a_flags & PGO_SYNCIO) ? SEGM_SYNC : 0));
+
+	/* If we're flushing, segwrite needs to know not to wait */
+	if(ap->a_flags & PGO_ALLPAGES) {
+		SET_FLUSHING(fs,vp);
+	}
+
+	error = 0;
+	npages = ap->a_count;
+	bshift = vp->v_mount->mnt_fs_bshift;
+	dev_bshift = vp->v_mount->mnt_dev_bshift;
+	dev_bsize = 1 << dev_bshift;
+
+	pg = ap->a_m[0];
+	startoffset = pg->offset;
+	bytes = npages << PAGE_SHIFT;
+	skipbytes = 0;
+
+	if (bytes == 0) {
+		panic("lfs_putpages: bytes == 0??? vp %p", vp);
+	}
+
+#if 0
+	/*
+	 * Fix start/end addresses to write entire blocks/fragments
+	 * XXX This can deadlock us; can the pager do this klustering
+	 * for us?
+	 */
+	lbn = startoffset >> bshift;
+	if(startoffset != (lbn << bshift)) {
+		printf("Reading %d bytes at beginning of putpages\n",
+			startoffset - (lbn<<bshift));
+		/* Make sure the request is valid - XXX deadlock */
+		VOP_GETPAGES(vp, (lbn<<bshift), &vmpp, (startoffset - (lbn<<bshift)) >> pgshift, &newpages,
+			0, VM_PROT_DEFAULT, 0, 0);
+		bytes += (startoffset - (lbn << bshift));
+	        startoffset = lbn << bshift;
+		pg = vmpp[0];
+		printf("Done reading beginning.\n");
+	}
+	if(bytes & fs->lfs_bmask) {
+		new_lastsize = blksize(fs, ip, startoffset + bytes);
+		diff = new_lastsize - (bytes & fs->lfs_bmask);
+		if(diff) {
+			printf("Reading %d bytes at end of putpages\n", diff);
+			/* Make sure request is valid - XXX deadlock */
+			VOP_GETPAGES(vp, startoffset+bytes, &vmpp2, diff, &newpages, 0,
+				VM_PROT_DEFAULT, 0, 0);
+			bytes += diff;
+			printf("Done reading end.\n");
+		}
+	}
+#endif
+
+	kva = (void *)uvm_pagermapin(ap->a_m, npages, M_WAITOK);
+
+	s = splbio();
+	vp->v_numoutput++;
+	mbp = pool_get(&bufpool, PR_WAITOK);
+	UVMHIST_LOG(ubchist, "master vp %p bp %p num now %d",
+		    vp, mbp, vp->v_numoutput, 0);
+	splx(s);
+	mbp->b_bufsize = npages << PAGE_SHIFT;
+	mbp->b_data = kva;
+	mbp->b_resid = mbp->b_bcount = bytes;
+	mbp->b_flags = B_BUSY|B_WRITE|(ap->a_flags & PGO_SYNCIO ? 0 : B_CALL)|
+		(curproc == uvm.pagedaemon_proc ? B_PDAEMON : 0);
+	mbp->b_iodone = uvm_aio_biodone;
+	mbp->b_vp = vp;
+
+	bp = NULL;
+
+	offset = startoffset;
+
+	for (; bytes > 0; offset += iobytes, bytes -= iobytes) {
+		lbn = offset >> bshift;
+		iobytes = min((1<<bshift), bytes);
+
+		if(bufhead_rem > BUFHEAD_THROTTLE) {
+			if(ap->a_flags & PGO_ALLPAGES)
+				lfs_writevnode(fs, vp);
+			else
+				lfs_segwrite(vp->v_mount, 0);
+			bufhead_rem = 0;
+			/* Don't sleep here, there's no point XXX ? */
+		}
+
+		++bufhead_rem;
+		s = splbio();
+		++bufhead_count;
+		/* vp->v_numoutput++; */
+		bp = pool_get(&bufpool, PR_WAITOK);
+		UVMHIST_LOG(ubchist, "vp %p bp %p num now %d",
+			    vp, bp, vp->v_numoutput, 0);
+		memset(bp,0,sizeof(*bp));
+		bgetvp(vp, bp);
+		splx(s);
+
+		bp->b_data = kva + offset - pg->offset;
+		bp->b_resid = bp->b_bcount = bp->b_bufsize = iobytes;
+		bp->b_flags = B_BUSY|B_WRITE|B_CALL;
+		bp->b_iodone = lfs_bufcallback /* uvm_aio_biodone1 */ ;
+		bp->b_lblkno = bp->b_blkno = lbn;
+		bp->b_private = mbp;
+
+#ifdef DIAGNOSTIC
+		if(offset != (lbn<<bshift)) {
+			printf("off=0x%lx, lbn<<bshift=0x%lx\n",
+				(unsigned long)offset,
+				(unsigned long)(lbn<<bshift));
+			panic("Oops, fs bsize is %u but page size is apparently %u\n", (unsigned)1<<bshift, (unsigned)bp->b_bcount);
+		}
+		if(iobytes != (1<<bshift)) {
+			printf("iobytes=0x%lx, 1<<bshift=0x%lx\n",
+				(unsigned long)iobytes,
+				(unsigned long)(1<<bshift));
+			panic("Oops, fs bsize is %u but page size is apparently %u\n", (unsigned)1<<bshift, (unsigned)bp->b_bcount);
+		}
+#endif
+		VOP_BWRITE(bp);
+	}
+
+	/*
+	 * The pager wants these buffers to be written right now (async may
+	 * be okay, but definitely start right away).
+	 *
+	 * Call segwrite to start the write.
+	 */
+	if (skipbytes) {
+		s = splbio();
+		mbp->b_resid -= skipbytes;
+		if (mbp->b_resid == 0)
+			biodone(mbp);
+		splx(s);
+	}
+	if(bufhead_rem) {
+		if(ap->a_flags & PGO_ALLPAGES)
+			lfs_writevnode(fs, vp);
+		else
+			lfs_segwrite(vp->v_mount,
+				     ((ap->a_flags & PGO_SYNCIO) ?
+				      SEGM_SYNC : 0));
+		bufhead_rem = 0;
+		if (!(ap->a_flags & PGO_SYNCIO)) {
+			CLR_FLUSHING(fs,vp);
+			lfs_segunlock(fs);
+			return EINPROGRESS;
+		}
+	}
+
+	/*
+	 * By the time we reach this point, all buffers should be recovered,
+	 * and the mbp should be waiting for aiodoned to reclaim it.  We're writing
+	 * synchronously, so we have to wait for this to happen.
+	 */
+ /* errout: */
+	CLR_FLUSHING(fs,vp);
+	lfs_segunlock(fs);
+
+	if (bufhead_count) {
+		error = biowait(mbp);
+	}
+	s = splbio();
+	pool_put(&bufpool, mbp);
+	splx(s);
+
+	uvm_pagermapout((vaddr_t)kva, npages);
+	UVMHIST_LOG(ubchist, "returning, error %d", error,0,0,0);
+	return error;
 }
