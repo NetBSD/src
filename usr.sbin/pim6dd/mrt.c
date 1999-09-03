56448bbc4f409f659c712b184850bc021b844647
itojun <itojun@NetBSD.org>
1999-09-03 04:49:23 +0000

    sync with recent KAME. - added more friendly debugging code - fix to make assert looser correctly stop forwarding - fixed SIGHUP support

--- usr.sbin/pim6dd/mrt.c
+++ usr.sbin/pim6dd/mrt.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: mrt.c,v 1.2 1999/08/19 17:31:07 itojun Exp $	*/
+/*	$NetBSD: mrt.c,v 1.3 1999/09/03 04:49:24 itojun Exp $	*/
 
 /*
  *  Copyright (c) 1998 by the University of Oregon.
@@ -35,7 +35,7 @@
  *  Questions concerning this software should be directed to 
  *  Kurt Windisch (kurtw@antc.uoregon.edu)
  *
- *  KAME Id: mrt.c,v 1.1.1.1 1999/08/08 23:30:52 itojun Exp
+ *  KAME Id: mrt.c,v 1.2 1999/08/24 10:04:56 jinmei Exp
  */
 /*
  * Part of this program has been derived from PIM sparse-mode pimd.
@@ -614,6 +614,7 @@ alloc_mrtentry(srcentry_ptr, grpentry_ptr)
     IF_ZERO(&mrtentry_ptr->pruned_oifs);
     IF_ZERO(&mrtentry_ptr->oifs);
     IF_ZERO(&mrtentry_ptr->filter_oifs);
+    IF_ZERO(&mrtentry_ptr->asserted_oifs);
     mrtentry_ptr->upstream = (pim_nbr_entry_t *)NULL;
     mrtentry_ptr->metric = 0;
     mrtentry_ptr->preference = 0;
