67cb63112e7c8a41ca9d1258e8e66259f9af2cf6
jdolecek <jdolecek@NetBSD.org>
1999-09-05 14:26:32 +0000

    Adapt to cache_lookup() changes.
    Tested by: jdolecek
    Rewieved by: wrstuden

--- sys/filecorefs/filecore_lookup.c
+++ sys/filecorefs/filecore_lookup.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: filecore_lookup.c,v 1.7 1999/08/18 22:01:53 mark Exp $	*/
+/*	$NetBSD: filecore_lookup.c,v 1.8 1999/09/05 14:26:33 jdolecek Exp $	*/
 
 /*-
  * Copyright (c) 1998 Andrew McMurry
@@ -143,63 +143,8 @@ filecore_lookup(v)
 	 * check the name cache to see if the directory/name pair
 	 * we are looking for is known already.
 	 */
-	if ((error = cache_lookup(vdp, vpp, cnp)) != 0) {
-		int vpid;	/* capability number of vnode */
-
-		if (error == ENOENT)
-			return (error);
-#ifdef PARANOID
-		if ((vdp->v_flag & VROOT) && (flags & ISDOTDOT))
-			panic("filecore_lookup: .. through root");
-#endif
-		/*
-		 * Get the next vnode in the path.
-		 * See comment below starting `Step through' for
-		 * an explaination of the locking protocol.
-		 */
-		pdp = vdp;
-		dp = VTOI(*vpp);
-		vdp = *vpp;
-		vpid = vdp->v_id;
-		if (pdp == vdp) {
-			VREF(vdp);
-			error = 0;
-		} else if (flags & ISDOTDOT) {
-			VOP_UNLOCK(pdp, 0);
-			cnp->cn_flags |= PDIRUNLOCK;
-			error = vget(vdp, LK_EXCLUSIVE);
-			if (!error && lockparent && (flags & ISLASTCN)) {
-				error = vn_lock(pdp, LK_EXCLUSIVE);
-				if (error == 0)
-					cnp->cn_flags &= ~PDIRUNLOCK;
-			}
-		} else {
-			error = vget(vdp, LK_EXCLUSIVE);
-			if (!lockparent || error || !(flags & ISLASTCN)) {
-				VOP_UNLOCK(pdp, 0);
-				cnp->cn_flags |= PDIRUNLOCK;
-			}
-		}
-		/*
-		 * Check that the capability number did not change
-		 * while we were waiting for the lock.
-		 */
-		if (!error) {
-			if (vpid == vdp->v_id)
-				return (0);
-			vput(vdp);
-			if (lockparent && pdp != vdp && (flags & ISLASTCN)) {
-				VOP_UNLOCK(pdp, 0);
-				cnp->cn_flags |= PDIRUNLOCK;
-			}
-		}
-		if ((error = vn_lock(pdp, LK_EXCLUSIVE)) != 0)
-			return (error);
-		cnp->cn_flags &= ~PDIRUNLOCK;
-		vdp = pdp;
-		dp = VTOI(pdp);
-		*vpp = NULL;
-	}
+	if ((error = cache_lookup(vdp, vpp, cnp)) >= 0)
+		return (error);
 	
 	name = cnp->cn_nameptr;
 	namelen = cnp->cn_namelen;
