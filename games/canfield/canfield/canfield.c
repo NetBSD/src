bf93429da578455c1c842d18bc0031530c01da5d
jsm <jsm@NetBSD.org>
1999-09-09 17:30:17 +0000

    Include <time.h> in various places in the games where time() or time_t are used.

--- games/canfield/canfield/canfield.c
+++ games/canfield/canfield/canfield.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: canfield.c,v 1.13 1999/09/08 21:45:26 jsm Exp $	*/
+/*	$NetBSD: canfield.c,v 1.14 1999/09/09 17:30:19 jsm Exp $	*/
 
 /*
  * Copyright (c) 1980, 1993
@@ -43,7 +43,7 @@ __COPYRIGHT("@(#) Copyright (c) 1980, 1993\n\
 #if 0
 static char sccsid[] = "@(#)canfield.c	8.1 (Berkeley) 5/31/93";
 #else
-__RCSID("$NetBSD: canfield.c,v 1.13 1999/09/08 21:45:26 jsm Exp $");
+__RCSID("$NetBSD: canfield.c,v 1.14 1999/09/09 17:30:19 jsm Exp $");
 #endif
 #endif /* not lint */
 
@@ -67,7 +67,7 @@ __RCSID("$NetBSD: canfield.c,v 1.13 1999/09/08 21:45:26 jsm Exp $");
 #include <stdlib.h>
 #include <string.h>
 #include <termios.h>
-#include <termios.h>
+#include <time.h>
 #include <unistd.h>
 
 #include "pathnames.h"
