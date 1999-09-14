a7451aadcfd7c7587cb846fc57a5c2a51c2086ab
tsubai <tsubai@NetBSD.org>
1999-09-14 11:20:52 +0000

    Sync with -current. XXX root on nfs don't work, though root on wd0 works well.

--- sys/arch/mmeye/mmeye/machdep.c
+++ sys/arch/mmeye/mmeye/machdep.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: machdep.c,v 1.1 1999/09/13 10:31:03 itojun Exp $	*/
+/*	$NetBSD: machdep.c,v 1.2 1999/09/14 11:20:54 tsubai Exp $	*/
 
 /*-
  * Copyright (c) 1996, 1997, 1998 The NetBSD Foundation, Inc.
@@ -75,10 +75,8 @@
  *	@(#)machdep.c	7.4 (Berkeley) 6/3/91
  */
 
-#include "opt_bufcache.h"
 #include "opt_compat_netbsd.h"
 #include "opt_ddb.h"
-#include "opt_pmap_new.h"
 #include "opt_sysv.h"
 
 #include <sys/param.h>
@@ -102,15 +100,6 @@
 #include <sys/device.h>
 #include <sys/extent.h>
 #include <sys/syscallargs.h>
-#ifdef SYSVMSG
-#include <sys/msg.h>
-#endif
-#ifdef SYSVSEM
-#include <sys/sem.h>
-#endif
-#ifdef SYSVSHM
-#include <sys/shm.h>
-#endif
 
 #ifdef KGDB
 #include <sys/kgdb.h>
@@ -155,26 +144,6 @@ char cpu_model[120];
 
 char bootinfo[BOOTINFO_MAXSIZE];
 
-/*
- * Declare these as initialized data so we can patch them.
- */
-int	nswbuf = 0;
-#ifdef	NBUF
-int	nbuf = NBUF;
-#else
-int	nbuf = 0;
-#endif
-#ifdef	BUFPAGES
-int	bufpages = BUFPAGES;
-#else
-int	bufpages = 0;
-#endif
-#ifdef BUFCACHE
-int	bufcache = BUFCACHE;	/* % of RAM to use for buffer cache */
-#else
-int	bufcache = 0;		/* fallback to old algorithm */
-#endif
-
 int physmem;
 int dumpmem_low;
 int dumpmem_high;
@@ -212,7 +181,6 @@ struct	extent *iomem_ex;
 static	int ioport_malloc_safe;
 
 void setup_bootinfo __P((void));
-caddr_t	allocsys __P((caddr_t));
 void dumpsys __P((void));
 void identifycpu __P((void));
 void initSH3 __P((void *));
@@ -246,21 +214,23 @@ cpu_startup()
 	vaddr_t minaddr, maxaddr;
 	vsize_t size;
 	struct pcb *pcb;
+	char pbuf[9];
 
 	printf(version);
 
 	sprintf(cpu_model, "Hitachi SH3");
 
-	printf("real mem  = %d\n", ctob(physmem));
+	format_bytes(pbuf, sizeof(pbuf), ctob(physmem));
+	printf("total memory = %s\n", pbuf);
 
 	/*
 	 * Find out how much space we need, allocate it,
 	 * and then give everything true virtual addresses.
 	 */
-	sz = (int)allocsys((caddr_t)0);
+	sz = (int)allocsys(NULL, NULL);
 	if ((v = (caddr_t)uvm_km_zalloc(kernel_map, round_page(sz))) == 0)
 		panic("startup: no room for tables");
-	if (allocsys(v) - v != sz)
+	if (allocsys(v, NULL) - v != sz)
 		panic("startup: table size inconsistency");
 
 	/*
@@ -297,22 +267,12 @@ cpu_startup()
 		curbufsize = CLBYTES * ((i < residual) ? (base+1) : base);
 
 		while (curbufsize) {
-			/*
-			 * Attempt to allocate buffers from the first
-			 * 16M of RAM to avoid bouncing file system
-			 * transfers.
-			 */
 			pg = uvm_pagealloc(NULL, 0, NULL, 0);
 			if (pg == NULL)
 				panic("cpu_startup: not enough memory for "
 				    "buffer cache");
-#if defined(PMAP_NEW)
-			pmap_kenter_pgs(curbuf, &pg, 1);
-#else
-			pmap_enter(kernel_map->pmap, curbuf,
-				   VM_PAGE_TO_PHYS(pg), VM_PROT_ALL, TRUE,
-				   VM_PROT_ALL);
-#endif
+			pmap_kenter_pa(curbuf, VM_PAGE_TO_PHYS(pg),
+					VM_PROT_READ|VM_PROT_WRITE);
 			curbuf += PAGE_SIZE;
 			curbufsize -= PAGE_SIZE;
 		}
@@ -344,9 +304,10 @@ cpu_startup()
 	for (i = 1; i < ncallout; i++)
 		callout[i-1].c_next = &callout[i];
 
-	printf("avail mem = %ld\n", ptoa(uvmexp.free));
-	printf("using %d buffers containing %d bytes of memory\n",
-		nbuf, bufpages * CLBYTES);
+	format_bytes(pbuf, sizeof(pbuf), ptoa(uvmexp.free));
+	printf("avail memory = %s\n", pbuf);
+	format_bytes(pbuf, sizeof(pbuf), bufpages * CLBYTES);
+	printf("using %d buffers containing %s of memory\n", nbuf, pbuf);
 
 	/*
 	 * Set up buffers, so they can be used to read disk labels.
@@ -371,102 +332,11 @@ cpu_startup()
 }
 
 /*
- * Allocate space for system data structures.  We are given
- * a starting virtual address and we return a final virtual
- * address; along the way we set each data structure pointer.
- *
- * We call allocsys() with 0 to find out how much space we want,
- * allocate that much and fill it with zeroes, and then call
- * allocsys() again with the correct base virtual address.
- */
-caddr_t
-allocsys(v)
-	caddr_t v;
-{
-
-#define	valloc(name, type, num) \
-	    v = (caddr_t)(((name) = (type *)v) + (num))
-#ifdef REAL_CLISTS
-	valloc(cfree, struct cblock, nclist);
-#endif
-	valloc(callout, struct callout, ncallout);
-#ifdef SYSVSHM
-	valloc(shmsegs, struct shmid_ds, shminfo.shmmni);
-#endif
-#ifdef SYSVSEM
-	valloc(sema, struct semid_ds, seminfo.semmni);
-	valloc(sem, struct sem, seminfo.semmns);
-	/* This is pretty disgusting! */
-	valloc(semu, int, (seminfo.semmnu * seminfo.semusz) / sizeof(int));
-#endif
-#ifdef SYSVMSG
-	valloc(msgpool, char, msginfo.msgmax);
-	valloc(msgmaps, struct msgmap, msginfo.msgseg);
-	valloc(msghdrs, struct msg, msginfo.msgtql);
-	valloc(msqids, struct msqid_ds, msginfo.msgmni);
-#endif
-
-	/*
-	 * If necessary, determine the number of pages to use for the
-	 * buffer cache.  We allocate 1/2 as many swap buffer headers
-	 * as file I/O buffers.
-	 */
-	if (bufpages == 0) {
-		if (bufcache == 0) {		/* use old algorithm */
-			/*
-			 * Determine how many buffers to allocate. We use 10%
-			 * of the first 2MB of memory, and 5% of the rest, with
-			 * a minimum of 16 buffers.
-			 */
-			if (physmem < btoc(2 * 1024 * 1024))
-				bufpages = physmem / (10 * CLSIZE);
-			else
-				bufpages = (btoc(2 * 1024 * 1024) + physmem) /
-				    (20 * CLSIZE);
-		} else {
-			/*
-			 * Set size of buffer cache to physmem/bufcache * 100
-			 * (i.e., bufcache % of physmem).
-			 */
-			if (bufcache < 5 || bufcache > 95) {
-				printf(
-		"warning: unable to set bufcache to %d%% of RAM, using 10%%",
-				    bufcache);
-				bufcache = 10;
-			}
-			bufpages= physmem / (CLSIZE * 100) * bufcache;
-		}
-	}
-	if (nbuf == 0) {
-		nbuf = bufpages;
-		if (nbuf < 16)
-			nbuf = 16;
-	}
-
-	/*
-	 * XXX stopgap measure to prevent wasting too much KVM on
-	 * the sparsely filled buffer cache.
-	 */
-	if (nbuf * MAXBSIZE > VM_MAX_KERNEL_BUF)
-		nbuf = VM_MAX_KERNEL_BUF / MAXBSIZE;
-
-	if (nswbuf == 0) {
-		nswbuf = (nbuf / 2) &~ 1;	/* force even */
-		if (nswbuf > 256)
-			nswbuf = 256;		/* sanity */
-	}
-	valloc(buf, struct buf, nbuf);
-	return v;
-}
-
-/*
  * Info for CTL_HW
  */
 extern	char version[];
 
-
 #define CPUDEBUG
-
 
 /*
  * machine dependent system variables.
@@ -1065,11 +935,7 @@ initSH3(pc)
 	/* Install a PDE recursively mapping page directory as a page table! */
 	pte = (u_int)pagedir;
 	pte |= PG_V | PG_4K | PG_KW | PG_M | PG_N;
-#if defined(PMAP_NEW)
 	pagedir[PDSLOT_PTE] = pte;
-#else
-	pagedir[PTDPTDI] = pte;
-#endif
 
 	/* set PageDirReg */
 	SHREG_TTB = (u_int)pagedir;
@@ -1115,10 +981,9 @@ initSH3(pc)
 	/* MMEYE_LED = 0x01; */
 
 	proc0.p_addr = proc0paddr; /* page dir address */
-#if defined(PMAP_NEW)
+
 	/* XXX: PMAP_NEW requires valid curpcb.   also init'd in cpu_startup */
 	curpcb = &proc0.p_addr->u_pcb;
-#endif
 
 	/*
 	 * Initialize the I/O port and I/O mem extent maps.
