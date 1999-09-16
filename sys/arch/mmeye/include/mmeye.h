f263c98a1e9d9adbff8bc0fb861f1e46e34affc1
msaitoh <msaitoh@NetBSD.org>
1999-09-16 13:32:24 +0000

    remove sh3/include/mmeye.h

--- sys/arch/mmeye/include/mmeye.h
+++ sys/arch/mmeye/include/mmeye.h
@@ -1,3 +1,26 @@
-/*	$NetBSD: mmeye.h,v 1.1 1999/09/13 10:30:57 itojun Exp $	*/
+/* $NetBSD: mmeye.h,v 1.2 1999/09/16 13:33:04 msaitoh Exp $ */
 
-#include <sh3/mmeye.h>
+/*
+ * Brains mmEye specific register definition
+ */
+
+#ifndef _MMEYE_MMEYE_H_
+#define _MMEYE_H_
+
+/* IRQ mask register */
+#ifdef MMEYE_NEW_INT /* for new mmEye */
+#define	MMTA_IMASK	(*(volatile unsigned short  *)0xb000000E)
+#else /* for old mmEye */
+#define	MMTA_IMASK	(*(volatile unsigned short  *)0xb0000010)
+#endif
+
+#define MMEYE_LED       (*(volatile unsigned short *)0xb0000008)
+
+/*
+ * SCI bitrate
+ * 9600bps, 11 = 3750000/(32*9600) -1, Pcyc = 3.75MHz
+ */
+
+#define SCI_BITRATE 11
+
+#endif /* !_MMEYE_MMEYE_H_ */
