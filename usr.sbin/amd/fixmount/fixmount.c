39a38daf0aaa15834f790083b6bd61151772f605
cgd <cgd>
1999-09-21 04:57:11 +0000

    pull up rev 1.7 from trunk (requested by christos):   Upgrade amd(8) and related software to fix expoitable stack overflows   in amq(8), as reported in BUGTRAQ and elsewhere.

--- usr.sbin/amd/fixmount/fixmount.c
+++ usr.sbin/amd/fixmount/fixmount.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: fixmount.c,v 1.6 1999/02/01 19:05:12 christos Exp $	*/
+/*	$NetBSD: fixmount.c,v 1.6.2.1 1999/09/21 04:57:11 cgd Exp $	*/
 
 /*
  * Copyright (c) 1997-1999 Erez Zadok
@@ -40,7 +40,7 @@
  *
  *      %W% (Berkeley) %G%
  *
- * Id: fixmount.c,v 1.3 1999/01/10 21:54:26 ezk Exp 
+ * Id: fixmount.c,v 1.4 1999/02/04 07:24:42 ezk Exp 
  *
  */
 
@@ -88,15 +88,6 @@ void fix_rmtab(CLIENT *, char *, mountlist, int, int);
 void print_dump(mountlist);
 void usage(void);
 
-/* dummy variables */
-#if 0
-char *progname;
-char hostname[MAXHOSTNAMELEN];
-pid_t mypid;
-serv_state amd_state;
-int foreground, orig_umask;
-int debug_flags;
-#endif
 
 void
 usage(void)
