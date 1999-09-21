40d1ab9c49c64b53b58b12d573ebf89b5093d0d8
cgd <cgd@NetBSD.org>
1999-09-21 04:58:45 +0000

    pull up rev 1.1.1.3 from trunk (requested by christos):   Upgrade amd(8) and related software to fix expoitable stack overflows   in amq(8), as reported in BUGTRAQ and elsewhere.

--- usr.sbin/amd/pawd/pawd.c
+++ usr.sbin/amd/pawd/pawd.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: pawd.c,v 1.1.1.2 1999/02/01 18:46:57 christos Exp $	*/
+/*	$NetBSD: pawd.c,v 1.1.1.2.2.1 1999/09/21 04:58:45 cgd Exp $	*/
 
 /*
  * Copyright (c) 1997-1999 Erez Zadok
@@ -40,7 +40,7 @@
  *
  *      %W% (Berkeley) %G%
  *
- * Id: pawd.c,v 1.3 1999/01/10 21:54:00 ezk Exp 
+ * Id: pawd.c,v 1.4 1999/02/04 07:24:20 ezk Exp 
  *
  */
 
@@ -59,16 +59,6 @@
 #endif /* HAVE_CONFIG_H */
 #include <am_defs.h>
 #include <amq.h>
-
-/* dummy variables */
-#if 0
-char *progname;
-char hostname[MAXHOSTNAMELEN];
-pid_t mypid;
-serv_state amd_state;
-int foreground, orig_umask;
-int debug_flags;
-#endif
 
 /* statics */
 static char *localhost="localhost";
