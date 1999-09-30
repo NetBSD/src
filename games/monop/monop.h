a3a42d061c36a65cfa9bb783eec0bc11fe7ac58f
jsm <jsm@NetBSD.org>
1999-09-30 18:01:31 +0000

    Fix uses of namespaces reserved by ISO C or POSIX.1.

--- games/monop/monop.h
+++ games/monop/monop.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: monop.h,v 1.9 1999/09/09 17:27:59 jsm Exp $	*/
+/*	$NetBSD: monop.h,v 1.10 1999/09/30 18:01:32 jsm Exp $	*/
 
 /*
  * Copyright (c) 1980, 1993
@@ -170,7 +170,7 @@ void next_play __P((void));
 int get_int __P((const char *));
 void set_ownlist __P((int));
 void is_monop __P((MON *, int));
-void isnot_monop __P((MON *));
+void is_not_monop __P((MON *));
 void list __P((void));
 void list_all __P((void));
 void quit __P((void));
