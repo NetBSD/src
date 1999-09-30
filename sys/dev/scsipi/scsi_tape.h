0c8fe16723d2e2b16ee2d17271900af654c03e8e
soren <soren@NetBSD.org>
1999-09-30 12:12:41 +0000

    Remove outdated list of density codes. mt(1) has helpful hints and the SCSI specification has the real, current list.

--- sys/dev/scsipi/scsi_tape.h
+++ sys/dev/scsipi/scsi_tape.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: scsi_tape.h,v 1.17 1998/08/17 00:49:01 mycroft Exp $	*/
+/*	$NetBSD: scsi_tape.h,v 1.18 1999/09/30 12:12:41 soren Exp $	*/
 
 /*-
  * Copyright (c) 1998 The NetBSD Foundation, Inc.
@@ -226,54 +226,6 @@ struct scsi_tape_locate {
 	u_int8_t partition;
 	u_int8_t control;
 };
-
-/**********************************************************************
-			from the scsi2 spec
-                Value Tracks Density(bpi) Code Type  Reference     Note
-                0x1     9       800       NRZI  R    X3.22-1983    2
-                0x2     9      1600       PE    R    X3.39-1986    2
-                0x3     9      6250       GCR   R    X3.54-1986    2
-                0x5    4/9     8000       GCR   C    X3.136-1986   1
-                0x6     9      3200       PE    R    X3.157-1987   2
-                0x7     4      6400       IMFM  C    X3.116-1986   1
-                0x8     4      8000       GCR   CS   X3.158-1986   1
-                0x9    18     37871       GCR   C    X3B5/87-099   2
-                0xA    22      6667       MFM   C    X3B5/86-199   1
-                0xB     4      1600       PE    C    X3.56-1986    1
-                0xC    24     12690       GCR   C    HI-TC1        1,5
-                0xD    24     25380       GCR   C    HI-TC2        1,5
-                0xF    15     10000       GCR   C    QIC-120       1,5
-                0x10   18     10000       GCR   C    QIC-150       1,5
-                0x11   26     16000       GCR   C    QIC-320(525?) 1,5
-                0x12   30     51667       RLL   C    QIC-1350      1,5
-                0x13    1     61000       DDS   CS    X3B5/88-185A 4
-                0x14    1     43245       RLL   CS    X3.202-1991  4
-                0x15    1     45434       RLL   CS    ECMA TC17    4
-                0x16   48     10000       MFM   C     X3.193-1990  1
-                0x17   48     42500       MFM   C     X3B5/91-174  1
-		0x45   73     67733	  RLL	C     QIC3095
-
-                where Code means:
-                NRZI Non Return to Zero, change on ones
-                GCR  Group Code Recording
-                PE   Phase Encoded
-                IMFM Inverted Modified Frequency Modulation
-                MFM  Modified Frequency Modulation
-                DDS  Dat Data Storage
-                RLL  Run Length Encoding
-
-                where Type means:
-                R    Reel-to-Reel
-                C    Cartridge
-                CS   cassette
-
-                where Notes means:
-                1    Serial Recorded
-                2    Parallel Recorded
-                3    Old format know as QIC-11
-                4    Helical Scan
-                5    Not ANSI standard, rather industry standard.
-********************************************************************/
 
 #define	HALFINCH_800	0x01
 #define	HALFINCH_1600	0x02
