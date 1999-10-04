18f8d751e34f0fc95ec45e0d42b803e4a48c563f
lukem <lukem@NetBSD.org>
1999-10-04 23:21:18 +0000

    update post change to return value of tputs() third argument

--- lib/libcurses/putchar.c
+++ lib/libcurses/putchar.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: putchar.c,v 1.8 1999/04/13 14:08:18 mrg Exp $	*/
+/*	$NetBSD: putchar.c,v 1.9 1999/10/04 23:21:19 lukem Exp $	*/
 
 /*
  * Copyright (c) 1981, 1993, 1994
@@ -38,13 +38,13 @@
 #if 0
 static char sccsid[] = "@(#)putchar.c	8.2 (Berkeley) 5/4/94";
 #else
-__RCSID("$NetBSD: putchar.c,v 1.8 1999/04/13 14:08:18 mrg Exp $");
+__RCSID("$NetBSD: putchar.c,v 1.9 1999/10/04 23:21:19 lukem Exp $");
 #endif
 #endif				/* not lint */
 
 #include "curses.h"
 
-void
+int
 __cputchar(ch)
 	int	ch;
 {
@@ -52,5 +52,5 @@ __cputchar(ch)
 #ifdef DEBUG
 	__CTRACE("__cputchar: %d\n", unctrl(ch));
 #endif
-	(void)putchar(ch);
+	return (putchar(ch));
 }
