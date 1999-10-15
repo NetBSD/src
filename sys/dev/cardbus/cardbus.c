4eee111127d8ec2bdd77c0da46739e2b42323187
haya <haya@NetBSD.org>
1999-10-15 06:07:17 +0000

    This is the first check-in of CardBus driver.  CardBus driver contains CardBus bus stub, YENTA PCI-CardBus bridge (cbb), 3Com 3C575TX driver (ex) and Intel fxp driver.
    TODO:
      o Conform to the KNF more strictly.
      o Be unified with pcmcia code as much as possible.
      o Add more drivers for CardBus card, such as APA-1480 or USB card.
    
    The affected files are listed below.
    
    	sys/arch/i386/conf/files.i386
    	sys/arch/macppc/conf/files.macppc
    	sys/conf/files
    	sys/dev/ic/elinkxl.c
    	sys/dev/ic/elinkxlvar.h
    	sys/dev/ic/i82365.c
    	sys/dev/ic/i82365var.h
    	sys/dev/isa/i82365_isasubr.c
    	sys/dev/pci/files.pci
    	sys/dev/pcmcia/pcmcia.c
    	sys/dev/pcmcia/pcmciachip.h
    
    The added files are listed below.
    
    	sys/arch/i386/conf/CARDBUS
    	sys/arch/i386/include/rbus_machdep.h
    	sys/arch/i386/i386/rbus_machdep.c
    	sys/arch/macppc/include/rbus_machdep.h
    	sys/arch/macppc/macppc/rbus_machdep.c
    	sys/dev/cardbus/if_ex_cardbus.c
    	sys/dev/cardbus/Makefile.cardbusdevs
    	sys/dev/cardbus/cardbus.c
    	sys/dev/cardbus/cardbus_map.c
    	sys/dev/cardbus/cardbusdevs
    	sys/dev/cardbus/cardbusdevs.h
    	sys/dev/cardbus/cardbusdevs_data.h
    	sys/dev/cardbus/cardbusvar.h
    	sys/dev/cardbus/cardslot.c
    	sys/dev/cardbus/cardslotvar.h
    	sys/dev/cardbus/devlist2h.awk
    	sys/dev/cardbus/files.cardbus
    	sys/dev/cardbus/if_fxp_cardbus.c
    	sys/dev/cardbus/pccardcis.h
    	sys/dev/cardbus/rbus.c
    	sys/dev/cardbus/rbus.h
    	sys/dev/pci/pccbb.c
    	sys/dev/pci/pccbbreg.h
    	sys/dev/pci/pccbbvar.h

---
+++ sys/dev/cardbus/cardbus.c
@@ -0,0 +1,609 @@
+/*	$Id: cardbus.c,v 1.1 1999/10/15 06:07:17 haya Exp $	*/
+
+/*
+ * Copyright (c) 1997 and 1998 HAYAKAWA Koichi.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed by HAYAKAWA Koichi.
+ * 4. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/* #define CARDBUS_DEBUG */
+
+#include <sys/types.h>
+#include <sys/param.h>
+#include <sys/systm.h>
+#include <sys/device.h>
+#include <sys/malloc.h>
+#include <sys/kernel.h>
+#include <sys/syslog.h>
+
+#include <machine/bus.h>
+
+#if pccard
+#include <dev/pccard/cardbusvar.h>
+#include <dev/pccard/pccardcis.h>
+#else
+#include <dev/cardbus/cardbusvar.h>
+#include <dev/cardbus/pccardcis.h>
+#endif
+
+#include <dev/pci/pcivar.h>	/* XXX */
+#include <dev/pci/pcireg.h>	/* XXX */
+
+#if defined CARDBUS_DEBUG
+#define STATIC
+#define DPRINTF(a) printf a
+#define DDELAY(x) delay((x)*1000*1000)
+#else
+#define STATIC static
+#define DPRINTF(a)
+#endif
+
+
+
+STATIC void cardbusattach __P((struct device *, struct device *, void *));
+/* STATIC int cardbusprint __P((void *, const char *)); */
+int cardbus_attach_card __P((struct cardbus_softc *));
+
+#if !defined __BROKEN_INDIRECT_CONFIG
+STATIC int cardbusmatch __P((struct device *, struct cfdata *, void *));
+static int cardbussubmatch __P((struct device *, struct cfdata *, void *));
+#else
+STATIC int cardbusmatch __P((struct device *, void *, void *));
+static int cardbussubmatch __P((struct device *, void *, void *));
+#endif
+static int cardbusprint __P((void *, const char *));
+
+static int decode_tuples __P((u_int8_t *, int));
+
+
+struct cfattach cardbus_ca = {
+	sizeof(struct cardbus_softc), cardbusmatch, cardbusattach
+};
+
+#ifndef __NetBSD_Version__
+struct cfdriver cardbus_cd = {
+	NULL, "cardbus", DV_DULL
+};
+#endif
+
+
+STATIC int
+#if defined __BROKEN_INDIRECT_CONFIG
+cardbusmatch(parent, match, aux)
+     struct device *parent;
+     void *match;
+     void *aux;
+#else
+cardbusmatch(parent, cf, aux)
+     struct device *parent;
+     struct cfdata *cf;
+     void *aux;
+#endif
+{
+#if defined __BROKEN_INDIRECT_CONFIG
+  struct cfdata *cf = match;
+#endif
+  struct cbslot_attach_args *cba = aux;
+
+  if (strcmp(cba->cba_busname, cf->cf_driver->cd_name)) {
+    DPRINTF(("cardbusmatch: busname differs %s <=> %s\n",
+	     cba->cba_busname, cf->cf_driver->cd_name));
+    return 0;
+  }
+
+  /* which function? */
+  if (cf->cbslotcf_func != CBSLOT_UNK_FUNC &&
+      cf->cbslotcf_func != cba->cba_function) {
+    DPRINTF(("pccardmatch: function differs %d <=> %d\n",
+	     cf->cbslotcf_func, cba->cba_function));
+    return 0;
+  }
+
+  if (cba->cba_function < 0 || cba->cba_function > 255) {
+    return 0;
+  }
+
+  return 1;
+}
+
+
+
+STATIC void
+cardbusattach(parent, self, aux)
+     struct device *parent;
+     struct device *self;
+     void *aux;
+{
+  struct cardbus_softc *sc = (void *)self;
+  struct cbslot_attach_args *cba = aux;
+  int cdstatus;
+
+  sc->sc_bus = cba->cba_bus;
+  sc->sc_device = cba->cba_function;
+  sc->sc_intrline = cba->cba_intrline;
+  sc->sc_cacheline = cba->cba_cacheline;
+  sc->sc_lattimer = cba->cba_lattimer;
+
+  printf(" bus %d device %d\n", sc->sc_bus, sc->sc_device);
+  printf("cacheline 0x%x, lattimer 0x%x", sc->sc_cacheline,sc->sc_lattimer);
+
+  sc->sc_iot = cba->cba_iot;	/* CardBus I/O space tag */
+  sc->sc_memt = cba->cba_memt;	/* CardBus MEM space tag */
+  sc->sc_dmat = cba->cba_dmat;	/* DMA tag */
+  sc->sc_cc = cba->cba_cc;
+  sc->sc_cf = cba->cba_cf;
+
+#if rbus
+  sc->sc_rbus_iot = cba->cba_rbus_iot;
+  sc->sc_rbus_memt = cba->cba_rbus_memt;
+#endif
+
+  sc->sc_funcs = NULL;
+
+  cdstatus = 0;
+}
+
+
+
+
+/*
+ * int cardbus_attach_card(struct cardbus_softc *sc)
+ *
+ *    This function attaches the card on the slot: turns on power,
+ *    reads and analyses tuple, sets consifuration index.
+ *
+ *    This function returns the number of recognised device functions.
+ *    If no functions are recognised, return 0.
+ */
+int
+cardbus_attach_card(sc)
+     struct cardbus_softc *sc;
+{
+  cardbus_chipset_tag_t cc;
+  cardbus_function_tag_t cf;
+  int cdstatus;
+  cardbustag_t tag;
+  cardbusreg_t id, class, cis_ptr;
+  cardbusreg_t bhlc;
+  u_int8_t tuple[2048];
+  int function, nfunction;
+  struct cardbus_devfunc **previous_next = &(sc->sc_funcs);
+  struct device *csc;
+  int no_work_funcs = 0;
+  cardbus_devfunc_t ct;
+
+  cc = sc->sc_cc;
+  cf = sc->sc_cf;
+
+  DPRINTF(("cardbus_attach_card: cb%d start\n", sc->sc_dev.dv_unit));
+
+  /* inspect initial voltage */
+  if (0 == (cdstatus = (cf->cardbus_ctrl)(cc, CARDBUS_CD))) {
+    DPRINTF(("cardbusattach: no CardBus card on cb%d\n", sc->sc_dev.dv_unit));
+    return 0;
+  }
+
+  if (cdstatus & CARDBUS_3V_CARD) {
+    cf->cardbus_power(cc, CARDBUS_VCC_3V);
+    sc->sc_poweron_func = 1;	/* function 0 on */
+  }
+
+  (cf->cardbus_ctrl)(cc, CARDBUS_RESET);
+
+  function = 0;
+
+  tag = cardbus_make_tag(cc, cf, sc->sc_bus, sc->sc_device, function);
+
+  /*
+   * Wait until power comes up.  Maxmum 500 ms.
+   */
+  {
+    int i;
+    for (i = 0; i < 5; ++i) {
+      id = cardbus_conf_read(cc, cf, tag, CARDBUS_ID_REG);
+      if (id != 0xffffffff && id != 0) {
+	break;
+      }
+      delay(100*1000);		/* or tsleep */
+    }
+    if (i == 5) {
+      return 0;
+    }
+  }
+  
+  bhlc = cardbus_conf_read(cc, cf, tag, CARDBUS_BHLC_REG);
+  if (CARDBUS_LATTIMER(bhlc) < 0x10) {
+    bhlc &= (CARDBUS_LATTIMER_MASK << CARDBUS_LATTIMER_SHIFT);
+    bhlc |= (0x10 << CARDBUS_LATTIMER_SHIFT);
+    cardbus_conf_write(cc, cf, tag, CARDBUS_BHLC_REG, bhlc);
+  }
+
+  nfunction = CARDBUS_HDRTYPE_MULTIFN(bhlc) ? 8 : 1;
+
+  /*
+   *           XXX multi-function card
+   *
+   * I don't know how to process CIS information for
+   * multi-function cards.
+   */
+
+  id = cardbus_conf_read(cc, cf, tag, CARDBUS_ID_REG);
+  class = cardbus_conf_read(cc, cf, tag, CARDBUS_CLASS_REG);
+  cis_ptr = cardbus_conf_read(cc, cf, tag, CARDBUS_CIS_REG);
+
+  DPRINTF(("cardbus_attach_card: Vendor 0x%x, Product 0x%x, CIS 0x%x\n",
+	   CARDBUS_VENDOR(id), CARDBUS_PRODUCT(id), cis_ptr));
+
+  bzero(tuple, 2048);
+
+  if (CARDBUS_CIS_ASI_TUPLE == (CARDBUS_CIS_ASI(cis_ptr))) {
+				/* Tuple is in Cardbus config space */
+    int i = cis_ptr & CARDBUS_CIS_ADDRMASK;
+    int j = 0;
+
+    for (; i < 0xff; i += 4) {
+      u_int32_t e = (cf->cardbus_conf_read)(cc, tag, i);
+      tuple[j] = 0xff & e;
+      e >>= 8;
+      tuple[j + 1] = 0xff & e;
+      e >>= 8;
+      tuple[j + 2] = 0xff & e;
+      e >>= 8;
+      tuple[j + 3] = 0xff & e;
+      j += 4;
+    }
+  } else if (CARDBUS_CIS_ASI(cis_ptr) <= CARDBUS_CIS_ASI_BAR5) {
+    /*    int bar = CARDBUS_CIS_ASI_BAR(cis_ptr);*/
+  }
+
+
+  decode_tuples(tuple, 2048);
+
+  {
+    struct cardbus_attach_args ca;
+
+    if (NULL == (ct = (cardbus_devfunc_t)malloc(sizeof(struct cardbus_devfunc),
+						M_DEVBUF, M_NOWAIT))) {
+      panic("no room for cardbus_tag");
+    }
+
+    ct->ct_cc = sc->sc_cc;
+    ct->ct_cf = sc->sc_cf;
+    ct->ct_bus = sc->sc_bus;
+    ct->ct_dev = sc->sc_device;
+    ct->ct_func = function;
+    ct->ct_sc = sc;
+    ct->ct_next = NULL;
+    *previous_next = ct;
+
+    ca.ca_unit = sc->sc_dev.dv_unit;
+    ca.ca_ct = ct;
+
+    ca.ca_iot = sc->sc_iot;
+    ca.ca_memt = sc->sc_memt;
+    ca.ca_dmat = sc->sc_dmat;
+
+    ca.ca_tag = tag;
+    ca.ca_device = sc->sc_device;
+    ca.ca_function = function;
+    ca.ca_id = id;
+    ca.ca_class = class;
+
+    ca.ca_intrline = sc->sc_intrline;
+
+    if (NULL == (csc = config_found_sm((void *)sc, &ca, cardbusprint, cardbussubmatch))) {
+      /* do not match */
+      cf->cardbus_power(cc, CARDBUS_VCC_0V);
+      sc->sc_poweron_func = 0;	/* no functions on */
+      free(cc, M_DEVBUF);
+    } else {
+      /* found */
+      previous_next = &(ct->ct_next);
+      ct->ct_device = csc;
+      ++no_work_funcs;
+    }
+  }
+
+  return no_work_funcs;
+}
+
+
+static int
+#ifdef __BROKEN_INDIRECT_CONFIG
+cardbussubmatch(parent, match, aux)
+#else
+cardbussubmatch(parent, cf, aux)
+#endif
+     struct device *parent;
+#ifdef __BROKEN_INDIRECT_CONFIG
+     void *match;
+#else
+     struct cfdata *cf;
+#endif
+     void *aux;
+{
+#ifdef __BROKEN_INDIRECT_CONFIG
+  struct cfdata *cf = match;
+#endif
+  struct cardbus_attach_args *ca = aux;
+
+  if (cf->cardbuscf_dev != CARDBUS_UNK_DEV &&
+      cf->cardbuscf_dev != ca->ca_unit) {
+    return 0;
+  }
+  if (cf->cardbuscf_function != CARDBUS_UNK_FUNCTION &&
+      cf->cardbuscf_function != ca->ca_function) {
+    return 0;
+  }
+
+  return ((*cf->cf_attach->ca_match)(parent, cf, aux));
+}
+
+
+
+static int
+cardbusprint(aux, pnp)
+     void *aux;
+     const char *pnp;
+{
+  register struct cardbus_attach_args *ca = aux;
+  char devinfo[256];
+
+  if (pnp) {
+    pci_devinfo(ca->ca_id, ca->ca_class, 1, devinfo);
+    printf("%s at %s", devinfo, pnp);
+  }
+  printf(" dev %d function %d", ca->ca_device, ca->ca_function);
+
+  return UNCONF;
+}
+
+
+
+
+
+
+/*
+ * void *cardbus_intr_establish(cc, cf, irq, level, func, arg)
+ *   Interrupt handler of pccard.
+ *  args:
+ *   cardbus_chipset_tag_t *cc
+ *   int irq: 
+ */
+void *
+cardbus_intr_establish(cc, cf, irq, level, func, arg)
+     cardbus_chipset_tag_t cc;
+     cardbus_function_tag_t cf;
+     cardbus_intr_handle_t irq;
+     int level;
+     int (*func) __P((void *));
+     void *arg;
+{
+  DPRINTF(("- cardbus_intr_establish: irq %d\n", irq));
+
+  return (*cf->cardbus_intr_establish)(cc, irq, level, func, arg);
+}
+
+
+
+/*
+ * void cardbus_intr_disestablish(cc, cf, handler)
+ *   Interrupt handler of pccard.
+ *  args:
+ *   cardbus_chipset_tag_t *cc
+ */
+void
+cardbus_intr_disestablish(cc, cf, handler)
+     cardbus_chipset_tag_t cc;
+     cardbus_function_tag_t cf;
+     void *handler;
+{
+  DPRINTF(("- pccard_intr_disestablish\n"));
+
+ (*cf->cardbus_intr_disestablish)(cc, handler);
+  return;
+}
+
+
+
+/*
+ * int cardbus_function_enable(cardbus_devfunc_t ct)
+ *
+ *   This function enables a function on a card.  When no power is
+ *  applied on the card, power will be applied on it.
+ */
+int
+cardbus_function_enable(ct)
+     cardbus_devfunc_t ct;
+{
+  struct cardbus_softc *sc = ct->ct_sc;
+  int func = ct->ct_func;
+  cardbus_chipset_tag_t cc = sc->sc_cc;
+  cardbus_function_tag_t cf = sc->sc_cf;
+  cardbusreg_t command;
+  cardbustag_t tag;
+
+  DPRINTF(("entering cardbus_function_enable...  "));
+
+  /* entering critical area */
+
+  if (sc->sc_poweron_func == 0) {
+    /* no functions are enabled */
+    (*cf->cardbus_power)(cc, CARDBUS_VCC_3V); /* XXX: sc_vold should be used */
+    (*cf->cardbus_ctrl)(cc, CARDBUS_RESET);
+  }
+
+  sc->sc_poweron_func |= (1 << func);
+
+  /* exiting critical area */
+
+  tag = Cardbus_make_tag(ct);
+  command = cardbus_conf_read(cc, cf, tag, CARDBUS_COMMAND_STATUS_REG);
+  command |= (CARDBUS_COMMAND_MEM_ENABLE | CARDBUS_COMMAND_IO_ENABLE | CARDBUS_COMMAND_MASTER_ENABLE); /* XXX: good guess needed */
+  cardbus_conf_write(cc, cf, tag, CARDBUS_COMMAND_STATUS_REG, command);
+  Cardbus_free_tag(ct, tag);
+
+
+  DPRINTF(("%x\n", sc->sc_poweron_func));
+
+  return 0;
+}
+
+
+/*
+ * int cardbus_function_disable(cardbus_devfunc_t ct)
+ *
+ *   This function disable a function on a card.  When no functions are
+ *  enabled, it turns off the power.
+ */
+int
+cardbus_function_disable(ct)
+     cardbus_devfunc_t ct;
+{
+  struct cardbus_softc *sc = ct->ct_sc;
+  int func = ct->ct_func;
+  cardbus_chipset_tag_t cc = sc->sc_cc;
+  cardbus_function_tag_t cf = sc->sc_cf;
+
+  DPRINTF(("entering cardbus_enable_disable...  "));
+
+  sc->sc_poweron_func &= ~(1 << func);
+
+  DPRINTF(("%x\n", sc->sc_poweron_func));
+
+  if (sc->sc_poweron_func == 0) {
+    /* power-off because no functions are enabled */
+    (*cf->cardbus_power)(cc, CARDBUS_VCC_0V);
+  }
+
+  return 0;
+}
+
+
+
+
+
+
+
+/*
+ * below this line, there are some functions for decoding tuples.
+ * They should go out from this file.
+ */
+
+static u_int8_t *decode_tuple __P((u_int8_t *));
+static char *tuple_name __P((int));
+
+static int
+decode_tuples(tuple, buflen)
+     u_int8_t *tuple;
+     int buflen;
+{
+  u_int8_t *tp = tuple;
+
+  if (CISTPL_LINKTARGET != *tuple) {
+    DPRINTF(("WRONG TUPLE: 0x%x\n", *tuple));
+    return 0;
+  }
+
+  while (NULL != (tp = decode_tuple(tp))) {
+    if (tuple + buflen < tp) {
+      break;
+    }
+  }
+  
+  return 1;
+}
+
+
+static u_int8_t *
+decode_tuple(tuple)
+     u_int8_t *tuple;
+{
+  u_int8_t type;
+  u_int8_t len;
+  int i;
+
+  type = tuple[0];
+  len = tuple[1] + 2;
+
+  printf("tuple: %s len %d\n", tuple_name(type), len);
+  if (CISTPL_END == type) {
+    return NULL;
+  }
+
+  for (i = 0; i < len; ++i) {
+    if (i % 16 == 0) {
+      printf("  0x%2x:", i);
+    }
+    printf(" %x",tuple[i]);
+    if (i % 16 == 15) {
+      printf("\n");
+    }
+  }
+  if (i % 16 != 0) {
+    printf("\n");
+  }
+
+  return tuple + len;
+}
+
+
+static char *
+tuple_name(type)
+     int type;
+{
+  static char *tuple_name_s [] = {
+    "TPL_NULL", "TPL_DEVICE", "Reserved", "Reserved", /* 0-3 */
+    "CONFIG_CB", "CFTABLE_ENTRY_CB", "Reserved", "BAR", /* 4-7 */
+    "Reserved", "Reserved", "Reserved", "Reserved", /* 8-B */
+    "Reserved", "Reserved", "Reserved", "Reserved", /* C-F */
+    "CHECKSUM", "LONGLINK_A", "LONGLINK_C", "LINKTARGET",	/* 10-13 */
+    "NO_LINK", "VERS_1", "ALTSTR", "DEVICE_A",
+    "JEDEC_C", "JEDEC_A", "CONFIG", "CFTABLE_ENTRY",
+    "DEVICE_OC", "DEVICE_OA", "DEVICE_GEO", "DEVICE_GEO_A",
+    "MANFID", "FUNCID", "FUNCE", "SWIL", /* 20-23 */
+    "Reserved", "Reserved", "Reserved", "Reserved", /* 24-27 */
+    "Reserved", "Reserved", "Reserved", "Reserved", /* 28-2B */
+    "Reserved", "Reserved", "Reserved", "Reserved", /* 2C-2F */
+    "Reserved", "Reserved", "Reserved", "Reserved", /* 30-33 */
+    "Reserved", "Reserved", "Reserved", "Reserved", /* 34-37 */
+    "Reserved", "Reserved", "Reserved", "Reserved", /* 38-3B */
+    "Reserved", "Reserved", "Reserved", "Reserved", /* 3C-3F */
+    "VERS_2", "FORMAT", "GEOMETRY", "BYTEORDER",
+    "DATE", "BATTERY", "ORG"
+  };
+#define NAME_LEN(x) (sizeof x / sizeof(x[0]))
+
+  if (type > 0 && type < NAME_LEN(tuple_name_s)) {
+    return tuple_name_s[type];
+  } else if (0xff == type) {
+    return "END";
+  } else {
+    return "Reserved";
+  }
+}
---
+++ sys/dev/cardbus/if_fxp_cardbus.c
@@ -0,0 +1,281 @@
+/*	$Id: if_fxp_cardbus.c,v 1.1 1999/10/15 06:07:24 haya Exp $	*/
+
+/*
+ * Copyright (c) 1999
+ *       Johan Danielsson <joda@pdc.kth.se>.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed by the author.
+ * 4. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+
+#include "opt_inet.h"
+#include "opt_ns.h"
+#include "bpfilter.h"
+#include "rnd.h"
+
+#include <sys/param.h>
+#include <sys/systm.h>
+#include <sys/mbuf.h>
+#include <sys/malloc.h>
+#include <sys/kernel.h>
+#include <sys/socket.h>
+#include <sys/ioctl.h>
+#include <sys/errno.h>
+#include <sys/device.h>
+
+#if NRND > 0
+#include <sys/rnd.h>
+#endif
+
+#include <net/if.h>
+#include <net/if_dl.h>
+#include <net/if_media.h>
+#include <net/if_ether.h>
+
+#if NBPFILTER > 0
+#include <net/bpf.h>
+#endif
+
+#ifdef INET
+#include <netinet/in.h>
+#include <netinet/if_inarp.h>
+#endif
+
+#ifdef NS
+#include <netns/ns.h>
+#include <netns/ns_if.h>
+#endif
+
+#include <machine/bus.h>
+#include <machine/intr.h>
+
+#include <dev/mii/miivar.h>
+
+#include <dev/pci/if_fxpreg.h>
+#include <dev/pci/if_fxpvar.h>
+
+#include <dev/pci/pcivar.h>
+#include <dev/pci/pcireg.h>
+#include <dev/pci/pcidevs.h>
+
+#include <dev/cardbus/cardbusvar.h>
+#include <dev/cardbus/cardbusdevs.h>
+
+static int fxp_cardbus_match __P((struct device *, struct cfdata *, void *));
+static void fxp_cardbus_attach __P((struct device *, struct device *, void *));
+
+struct fxp_cardbus_softc {
+    struct fxp_softc sc;
+    cardbustag_t tag;
+    pcireg_t base1_reg;
+};
+
+struct cfattach fxp_cardbus_ca = {
+    sizeof(struct fxp_cardbus_softc), fxp_cardbus_match, fxp_cardbus_attach
+};
+
+#ifdef CBB_DEBUG
+#define DPRINTF(X) printf X
+#else
+#define DPRINTF(X)
+#endif
+
+/*
+ * Check if a device is an 82557.
+ */
+static int
+fxp_cardbus_match(parent, match, aux)
+     struct device *parent;
+     struct cfdata *match;
+     void *aux;
+{
+    /* should check CIS */
+    struct cardbus_attach_args *ca = aux;
+    
+    if (CARDBUS_VENDOR(ca->ca_id) != CARDBUS_VENDOR_INTEL)
+	return (0);
+
+    switch (CARDBUS_PRODUCT(ca->ca_id)) {
+    case CARDBUS_PRODUCT_INTEL_82557:
+	return (1);
+    }
+
+    return (0);
+}
+
+int
+fxp_enable(struct fxp_softc *sc);
+
+static int
+fxp_cardbus_enable(struct fxp_softc *sc);
+static void
+fxp_cardbus_disable(struct fxp_softc *sc);
+
+static void
+fxp_cardbus_setup(struct fxp_softc *sc);
+
+static void
+fxp_cardbus_attach(parent, self, aux)
+     struct device *parent, *self;
+     void *aux;
+{
+    struct fxp_softc *sc = (struct fxp_softc*)self;
+    struct fxp_cardbus_softc *csc = (struct fxp_cardbus_softc*)self;
+    struct cardbus_attach_args *ca = aux;
+    cardbus_function_tag_t cf = ca->ca_cf;
+    bus_space_handle_t ioh, memh;
+	
+    bus_addr_t adr;
+
+    /*
+     * Map control/status registers.
+     */
+#if rbus
+    cardbus_function_tag_t cf = ct->ct_cf;
+
+    if (cardbus_mapreg_map(ct, CARDBUS_BASE1_REG, CARDBUS_MAPREG_TYPE_IO, 0,
+			   &(sc->sc_iot), &ioh, &adr, NULL)) {
+      panic("io alloc in ex_attach_cardbus\n");
+    }
+    csc->base1_reg = adr | 1;
+    sc->sc_sh = ioh;
+
+#elif unibus
+    printf("fxp_cardbus_attach: uni %p\n", ca->ca_uni);
+    if ((*cf->cf_ub.unibus_space_alloc)(ca->ca_uni, ca->bar_info[1].tag, 0, 
+					ca->bar_info[1].size,
+					ca->bar_info[1].size - 1,
+					ca->bar_info[1].size,
+					0, 0, &adr, &ioh))
+	panic("io alloc");
+
+
+    csc->base1_reg = adr | 1;
+    sc->sc_st = ca->bar_info[1].tag;
+    sc->sc_sh = ioh;
+
+    if (0) {
+	/*
+	 * Map control/status registers.
+	 */
+	if ((*cf->cf_ub.unibus_space_alloc)(ca->ca_uni, ca->bar_info[0].tag, 0, 
+					    ca->bar_info[0].size,
+					    ca->bar_info[0].size - 1,
+					    ca->bar_info[0].size,
+					    0, 0, &adr, &memh))
+	    panic("mem alloc");
+
+
+	sc->sc_st = ca->bar_info[0].tag;
+	sc->sc_sh = memh;
+    }
+#endif
+
+    csc->tag = ca->ca_tag;
+
+    sc->sc_dmat = ca->ca_dmat;
+    sc->enable = fxp_cardbus_enable;
+    sc->disable = fxp_cardbus_disable;
+    sc->enabled = 0;
+    
+    fxp_cardbus_setup(sc);
+    printf(": Intel EtherExpress Pro 10+/100B Ethernet\n");
+    
+    fxp_attach_common(sc);
+}
+
+
+static void
+fxp_cardbus_setup(struct fxp_softc *sc)
+{
+    struct fxp_cardbus_softc *csc = (struct fxp_cardbus_softc*)sc;
+    struct cardbus_softc *psc = (struct cardbus_softc *)sc->sc_dev.dv_parent;
+    cardbus_chipset_tag_t cc = psc->sc_cc;
+    cardbus_function_tag_t cf = psc->sc_cf;
+    pcireg_t command;
+
+    printf("fxp_cardbus_setup\n");
+
+    cardbus_conf_write(cc, cf, csc->tag, CARDBUS_BASE1_REG, csc->base1_reg);
+
+    (cf->cardbus_ctrl)(cc, CARDBUS_IO_ENABLE);
+
+    /* enable the card */
+    command = cardbus_conf_read(cc, cf, csc->tag, CARDBUS_COMMAND_STATUS_REG);
+    command |= (CARDBUS_COMMAND_IO_ENABLE | CARDBUS_COMMAND_MASTER_ENABLE);
+    cardbus_conf_write(cc, cf, csc->tag, CARDBUS_COMMAND_STATUS_REG, command);
+#if 0
+    cardbus_conf_write(cc, cf, ca->tag, CARDBUS_BASE0_REG, adr);
+
+    (ca->ca_cf->cardbus_ctrl)(cc, CARDBUS_MEM_ENABLE);
+
+    /* enable card mem */
+    command = cardbus_conf_read(cc, cf, ca->tag, CARDBUS_COMMAND_STATUS_REG);
+    command |= CARDBUS_COMMAND_MEM_ENABLE;
+    cardbus_conf_write(cc, cf, ca->tag, CARDBUS_COMMAND_STATUS_REG, command);
+#endif
+}
+
+static int
+fxp_cardbus_enable(struct fxp_softc *sc)
+{
+    struct fxp_cardbus_softc *csc = (struct fxp_cardbus_softc*)sc;
+    struct cardbus_softc *psc = (struct cardbus_softc *)sc->sc_dev.dv_parent;
+    cardbus_chipset_tag_t cc = psc->sc_cc;
+    cardbus_function_tag_t cf = psc->sc_cf;
+
+    cardbus_function_enable(psc, csc->tag);
+
+    fxp_cardbus_setup(sc);
+
+    /* Map and establish the interrupt. */
+
+    sc->sc_ih = cardbus_intr_establish(cc, cf, psc->sc_intrline, IPL_NET, 
+				       fxp_intr, sc);
+    if (NULL == sc->sc_ih) {
+	printf("%s: couldn't establish interrupt\n", sc->sc_dev.dv_xname);
+	return 1;
+    }
+
+    printf("%s: interrupting at %d\n", sc->sc_dev.dv_xname, psc->sc_intrline);
+
+    return 0;
+}
+
+static void
+fxp_cardbus_disable(struct fxp_softc *sc)
+{
+    struct cardbus_softc *psc = (struct cardbus_softc *)sc->sc_dev.dv_parent;
+    cardbus_chipset_tag_t cc = psc->sc_cc;
+    cardbus_function_tag_t cf = psc->sc_cf;
+
+    fxp_stop(sc);
+    cardbus_intr_disestablish(cc, cf, sc->sc_ih); /* remove intr handler */
+    
+    cardbus_function_disable(psc);
+}
