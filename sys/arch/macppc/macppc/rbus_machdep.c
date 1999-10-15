17bb8c3ae1d150573b002175b7e77f9eead2b7c0
haya <haya@NetBSD.org>
1999-10-15 06:07:17 +0000

    This is the first check-in of CardBus driver.  CardBus driver contains CardBus bus stub, YENTA PCI-CardBus bridge (cbb), 3Com 3C575TX driver (ex) and Intel fxp driver.
    TODO:
      o Conform to the KNF more strictly.
      o Be unified with pcmcia code as much as possible.
      o Add more drivers for CardBus card, such as APA-1480 or USB card.
    
    The affected files are listed below.
    
    	sys/arch/i386/conf/files.i386
    	sys/arch/macppc/conf/files.macppc
    	sys/conf/files
    	sys/dev/ic/elinkxl.c
    	sys/dev/ic/elinkxlvar.h
    	sys/dev/ic/i82365.c
    	sys/dev/ic/i82365var.h
    	sys/dev/isa/i82365_isasubr.c
    	sys/dev/pci/files.pci
    	sys/dev/pcmcia/pcmcia.c
    	sys/dev/pcmcia/pcmciachip.h
    
    The added files are listed below.
    
    	sys/arch/i386/conf/CARDBUS
    	sys/arch/i386/include/rbus_machdep.h
    	sys/arch/i386/i386/rbus_machdep.c
    	sys/arch/macppc/include/rbus_machdep.h
    	sys/arch/macppc/macppc/rbus_machdep.c
    	sys/dev/cardbus/if_ex_cardbus.c
    	sys/dev/cardbus/Makefile.cardbusdevs
    	sys/dev/cardbus/cardbus.c
    	sys/dev/cardbus/cardbus_map.c
    	sys/dev/cardbus/cardbusdevs
    	sys/dev/cardbus/cardbusdevs.h
    	sys/dev/cardbus/cardbusdevs_data.h
    	sys/dev/cardbus/cardbusvar.h
    	sys/dev/cardbus/cardslot.c
    	sys/dev/cardbus/cardslotvar.h
    	sys/dev/cardbus/devlist2h.awk
    	sys/dev/cardbus/files.cardbus
    	sys/dev/cardbus/if_fxp_cardbus.c
    	sys/dev/cardbus/pccardcis.h
    	sys/dev/cardbus/rbus.c
    	sys/dev/cardbus/rbus.h
    	sys/dev/pci/pccbb.c
    	sys/dev/pci/pccbbreg.h
    	sys/dev/pci/pccbbvar.h

---
+++ sys/arch/macppc/macppc/rbus_machdep.c
@@ -0,0 +1,153 @@
+/*
+ * Copyright (c) 1999
+ *     TSUBAI Masanari.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/* $Id: rbus_machdep.c,v 1.1 1999/10/15 06:12:17 haya Exp $ */
+
+#include <sys/param.h>
+#include <sys/device.h>
+#include <sys/systm.h>
+
+#include <vm/vm.h>
+#include <vm/vm_kern.h>
+#include <vm/vm_page.h>
+
+#include <uvm/uvm_extern.h>
+
+#include <machine/bat.h>
+#include <machine/bus.h>
+
+#include <dev/pci/pcivar.h>
+#include <dev/cardbus/rbus.h>
+
+static void macppc_cardbus_init __P((pci_chipset_tag_t, pcitag_t));
+
+#define DEBUG_ALLOC
+
+#ifdef DEBUG_ALLOC
+# define DPRINTF printf
+#else
+# define DPRINTF while (0) printf
+#endif
+
+int
+md_space_map(t, bpa, size, flags, bshp)
+	bus_space_tag_t t;
+	bus_addr_t bpa;
+	bus_size_t size;
+	int flags;
+	bus_space_handle_t *bshp;
+{
+	DPRINTF("md_space_map: 0x%x, 0x%x, 0x%x\n", t, bpa, size);
+
+	/* XXX */
+	*bshp = t + bpa;
+	return 0;
+}
+
+void
+md_space_unmap(t, bsh, size, adrp)
+	bus_space_tag_t t;
+	bus_space_handle_t bsh;
+	bus_size_t size;
+	bus_addr_t *adrp;
+{
+	DPRINTF("md_space_unmap: 0x%x 0x%x\n", t, bsh);
+
+	/* XXX */
+	if (adrp)
+		*adrp = bsh - t;
+}
+
+rbus_tag_t
+rbus_pccbb_parent_mem(pa)
+	struct pci_attach_args *pa;
+{
+	bus_addr_t start;
+	bus_size_t size;
+	int node, reg[5];
+
+	macppc_cardbus_init(pa->pa_pc, pa->pa_tag);
+
+	node = pcidev_to_ofdev(pa->pa_pc, pa->pa_tag);
+	OF_getprop(node, "assigned-addresses", reg, sizeof(reg));
+
+	start = reg[2];
+	size = reg[4];
+
+	/* XXX PowerBook G3 */
+	if (size < 0x10000) {
+		start = 0x90000000;
+		size  = 0x10000000;
+	}
+
+	battable[start >> 28].batl = BATL(start, BAT_I);
+	battable[start >> 28].batu = BATU(start);
+
+	return rbus_new_root_delegate(pa->pa_memt, start, size, 0);
+}
+
+rbus_tag_t
+rbus_pccbb_parent_io(pa)
+	struct pci_attach_args *pa;
+{
+	bus_addr_t start = 0x2000;
+	bus_size_t size  = 0x0800;
+
+	start += pa->pa_function * size;
+	return rbus_new_root_delegate(pa->pa_iot, start, size, 0);
+
+	/* XXX we should use ``offset''? */
+}
+
+void
+macppc_cardbus_init(pc, tag)
+	pci_chipset_tag_t pc;
+	pcitag_t tag;
+{
+	u_int x;
+	static int initted = 0;
+
+	if (initted)
+		return;
+	initted = 1;
+
+	if (pc == PCI_CHIPSET_MPC106) {
+		/* For CardBus card. */
+		pci_conf_write(pc, tag, 0x18, 0x10010100);
+
+		/* Route INTA to MFUNC0 */
+		x = pci_conf_read(pc, tag, 0x8c);
+		x |= 0x02;
+		pci_conf_write(pc, tag, 0x8c, x);
+
+		/* Set Subordinate bus number to 1 */
+		tag = pci_make_tag(pc, 0, 0, 0);
+		x = pci_conf_read(pc, tag, 0x40);
+		x |= 1 << 8;
+		pci_conf_write(pc, tag, 0x40, x);
+	}
+}
