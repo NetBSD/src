cbd2a2e299732786432de62abc930b7d49dcd145
augustss <augustss@NetBSD.org>
1999-10-15 11:52:45 +0000

    Add USB OHCI attachment to cardbus. XXX There seems to be some interrupt problem.

---
+++ sys/dev/cardbus/ohci_cardbus.c
@@ -0,0 +1,216 @@
+/*	$NetBSD: ohci_cardbus.c,v 1.1 1999/10/15 11:52:45 augustss Exp $	*/
+
+/*
+ * Copyright (c) 1998 The NetBSD Foundation, Inc.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to The NetBSD Foundation
+ * by Lennart Augustsson (augustss@carlstedt.se) at
+ * Carlstedt Research & Technology.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *        This product includes software developed by the NetBSD
+ *        Foundation, Inc. and its contributors.
+ * 4. Neither the name of The NetBSD Foundation nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/*
+ * USB Open Host Controller driver.
+ *
+ * OHCI spec: http://www.intel.com/design/usb/ohci11d.pdf
+ * USB spec: http://www.teleport.com/cgi-bin/mailmerge.cgi/~usb/cgiform.tpl
+ */
+
+#include <sys/param.h>
+#include <sys/systm.h>
+#include <sys/kernel.h>
+#include <sys/device.h>
+#include <sys/proc.h>
+#include <sys/queue.h>
+
+#include <machine/bus.h>
+
+#if defined pciinc
+#include <dev/pci/pcidevs.h>
+#endif
+#if pccard
+#include <dev/pccard/cardbusvar.h>
+#include <dev/pccard/pccardcis.h>
+#else
+#include <dev/cardbus/cardbusvar.h>
+#include <dev/cardbus/cardbusdevs.h>
+#endif
+
+#include <dev/usb/usb.h>
+#include <dev/usb/usbdi.h>
+#include <dev/usb/usbdivar.h>
+#include <dev/usb/usb_mem.h>
+
+#include <dev/usb/ohcireg.h>
+#include <dev/usb/ohcivar.h>
+
+int	ohci_cardbus_match __P((struct device *, struct cfdata *, void *));
+void	ohci_cardbus_attach __P((struct device *, struct device *, void *));
+int	ohci_cardbus_detach __P((device_ptr_t, int));
+
+struct ohci_cardbus_softc {
+	ohci_softc_t		sc;
+	cardbus_chipset_tag_t	sc_cc;
+	cardbus_function_tag_t	sc_cf;
+	bus_size_t		sc_size;
+	void 			*sc_ih;		/* interrupt vectoring */
+};
+
+struct cfattach ohci_cardbus_ca = {
+	sizeof(struct ohci_cardbus_softc), ohci_cardbus_match,
+	ohci_cardbus_attach, ohci_cardbus_detach, ohci_activate
+};
+
+#define CARDBUS_INTERFACE_OHCI PCI_INTERFACE_OHCI
+#define CARDBUS_CBMEM PCI_CBMEM
+#define cardbus_findvendor pci_findvendor
+#define cardbus_devinfo pci_devinfo
+
+int
+ohci_cardbus_match(parent, match, aux)
+	struct device *parent;
+	struct cfdata *match;
+	void *aux;
+{
+	struct cardbus_attach_args *ca = (struct cardbus_attach_args *) aux;
+
+	if (CARDBUS_CLASS(ca->ca_class) == CARDBUS_CLASS_SERIALBUS &&
+	    CARDBUS_SUBCLASS(ca->ca_class) == CARDBUS_SUBCLASS_SERIALBUS_USB &&
+	    CARDBUS_INTERFACE(ca->ca_class) == CARDBUS_INTERFACE_OHCI)
+		return (1);
+ 
+	return (0);
+}
+
+void
+ohci_cardbus_attach(parent, self, aux)
+	struct device *parent;
+	struct device *self;
+	void *aux;
+{
+	struct ohci_cardbus_softc *sc = (struct ohci_cardbus_softc *)self;
+	struct cardbus_attach_args *ca = aux;
+	cardbus_devfunc_t ct = ca->ca_ct;
+	cardbus_chipset_tag_t cc = ct->ct_cc;
+	cardbus_function_tag_t cf = ct->ct_cf;
+	cardbusreg_t csr;
+	char devinfo[256];
+	usbd_status r;
+	char *vendor;
+	char *devname = sc->sc.sc_bus.bdev.dv_xname;
+
+	cardbus_devinfo(ca->ca_id, ca->ca_class, 0, devinfo);
+	printf(": %s (rev. 0x%02x)\n", devinfo,
+	       CARDBUS_REVISION(ca->ca_class));
+
+	/* Map I/O registers */
+	if (cardbus_mapreg_map(ct, CARDBUS_CBMEM, CARDBUS_MAPREG_TYPE_MEM, 0,
+			   &sc->sc.iot, &sc->sc.ioh, NULL, &sc->sc_size)) {
+		printf("%s: can't map mem space\n", devname);
+		return;
+	}
+
+	/* Disable interrupts, so we don't can any spurious ones. */
+	bus_space_write_4(sc->sc.iot, sc->sc.ioh, OHCI_INTERRUPT_DISABLE,
+			  OHCI_ALL_INTRS);
+
+	sc->sc_cc = cc;
+	sc->sc_cf = cf;
+	sc->sc.sc_bus.dmatag = ca->ca_dmat;
+
+#if rbus
+#else
+XXX	(ct->ct_cf->cardbus_mem_open)(cc, 0, iob, iob + 0x40);
+#endif
+	(ct->ct_cf->cardbus_ctrl)(cc, CARDBUS_MEM_ENABLE);
+
+	/* Enable the device. */
+	csr = cardbus_conf_read(cc, cf, ca->ca_tag,
+				CARDBUS_COMMAND_STATUS_REG);
+	cardbus_conf_write(cc, cf, ca->ca_tag, CARDBUS_COMMAND_STATUS_REG,
+		       csr | CARDBUS_COMMAND_MASTER_ENABLE
+			   | CARDBUS_COMMAND_MEM_ENABLE);
+
+	sc->sc_ih = cardbus_intr_establish(cc, cf, ca->ca_intrline,
+					   IPL_USB, ohci_intr, sc);
+	if (sc->sc_ih == NULL) {
+		printf("%s: couldn't establish interrupt\n", devname);
+		return;
+	}
+	printf("%s: interrupting at %d\n", devname, ca->ca_intrline);
+
+	/* Figure out vendor for root hub descriptor. */
+	vendor = cardbus_findvendor(ca->ca_id);
+	sc->sc.sc_id_vendor = CARDBUS_VENDOR(ca->ca_id);
+	if (vendor)
+		strncpy(sc->sc.sc_vendor, vendor,
+			sizeof(sc->sc.sc_vendor) - 1);
+	else
+		sprintf(sc->sc.sc_vendor, "vendor 0x%04x", 
+			CARDBUS_VENDOR(ca->ca_id));
+	
+	r = ohci_init(&sc->sc);
+	if (r != USBD_NORMAL_COMPLETION) {
+		printf("%s: init failed, error=%d\n", devname, r);
+
+		/* Avoid spurious interrupts. */
+		cardbus_intr_disestablish(sc->sc_cc, sc->sc_cf, sc->sc_ih);
+		sc->sc_ih = 0;
+
+		return;
+	}
+
+	/* Attach usb device. */
+	sc->sc.sc_child = config_found((void *)sc, &sc->sc.sc_bus,
+				       usbctlprint);
+}
+
+int
+ohci_cardbus_detach(self, flags)
+	device_ptr_t self;
+	int flags;
+{
+	struct ohci_cardbus_softc *sc = (struct ohci_cardbus_softc *)self;
+	int rv;
+
+	rv = ohci_detach(&sc->sc, flags);
+	if (rv)
+		return (rv);
+	if (sc->sc_ih) {
+		cardbus_intr_disestablish(sc->sc_cc, sc->sc_cf, sc->sc_ih);
+		sc->sc_ih = 0;
+	}
+	if (sc->sc_size) {
+		bus_space_unmap(sc->sc.iot, sc->sc.ioh, sc->sc_size);
+		sc->sc_size = 0;
+	}
+	return (0);
+}
