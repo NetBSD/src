435c094ab0bb99ea65ef70ea42fa4369448204cc
enami <enami>
1999-10-20 14:32:09 +0000

    Check if the type of device node isn't VBAD before touching v_specinfo.  If the device vnode is revoked, the field is NULL and touching it causes null pointer derefercence.

--- sys/filecorefs/filecore_vfsops.c
+++ sys/filecorefs/filecore_vfsops.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: filecore_vfsops.c,v 1.8 1999/10/16 23:53:27 wrstuden Exp $	*/
+/*	$NetBSD: filecore_vfsops.c,v 1.9 1999/10/20 14:32:10 enami Exp $	*/
 
 /*-
  * Copyright (c) 1998 Andrew McMurry
@@ -393,7 +393,8 @@ filecore_unmount(mp, mntflags, p)
 
 	fcmp = VFSTOFILECORE(mp);
 
-	fcmp->fc_devvp->v_specflags &= ~SI_MOUNTEDON;
+	if (fcmp->fc_devvp->v_type != VBAD)
+		fcmp->fc_devvp->v_specflags &= ~SI_MOUNTEDON;
 	vn_lock(fcmp->fc_devvp, LK_EXCLUSIVE | LK_RETRY);
 	error = VOP_CLOSE(fcmp->fc_devvp, FREAD, NOCRED, p);
 	vput(fcmp->fc_devvp);
