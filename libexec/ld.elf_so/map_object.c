175049b3d280cef186fb30bd3a1464bc1bbecbbe
erh <erh@NetBSD.org>
1999-10-21 21:15:04 +0000

    Remove ELFNAME macros which are already defines in exec_elf.h

--- libexec/ld.elf_so/map_object.c
+++ libexec/ld.elf_so/map_object.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: map_object.c,v 1.5 1999/08/06 22:33:49 thorpej Exp $	 */
+/*	$NetBSD: map_object.c,v 1.6 1999/10/21 21:15:04 erh Exp $	 */
 
 /*
  * Copyright 1996 John D. Polstra.
@@ -39,12 +39,6 @@
 #include <sys/mman.h>
 
 #include "rtld.h"
-
-#define CONCAT(x,y)     __CONCAT(x,y)
-#define ELFNAME(x)      CONCAT(elf,CONCAT(ELFSIZE,CONCAT(_,x)))
-#define ELFNAME2(x,y)   CONCAT(x,CONCAT(_elf,CONCAT(ELFSIZE,CONCAT(_,y))))
-#define ELFNAMEEND(x)   CONCAT(x,CONCAT(_elf,ELFSIZE))
-#define ELFDEFNNAME(x)  CONCAT(ELF,CONCAT(ELFSIZE,CONCAT(_,x)))
 
 static int protflags __P((int));	/* Elf flags -> mmap protection */
 
