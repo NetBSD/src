56162883f3f28d65f0f0ee09d31176c2262a9edd
jdolecek <jdolecek@NetBSD.org>
1999-10-27 13:17:46 +0000

    minor cleanup of previous - avoid goto and code duplication

--- sys/kern/uipc_syscalls.c
+++ sys/kern/uipc_syscalls.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: uipc_syscalls.c,v 1.46 1999/10/27 11:54:56 darrenr Exp $	*/
+/*	$NetBSD: uipc_syscalls.c,v 1.47 1999/10/27 13:17:46 jdolecek Exp $	*/
 
 /*
  * Copyright (c) 1982, 1986, 1989, 1990, 1993
@@ -244,31 +244,25 @@ sys_accept(p, v, retval)
 	fp->f_data = (caddr_t)so;
 	FILE_UNUSE(fp, p);
 	nam = m_get(M_WAIT, MT_SONAME);
-	if ((error = soaccept(so, nam)))
-		goto freeit;
-	if (SCARG(uap, name)) {
+	if ((error = soaccept(so, nam)) == 0 && SCARG(uap, name)) {
 		if (namelen > nam->m_len)
 			namelen = nam->m_len;
 		/* SHOULD COPY OUT A CHAIN HERE */
 		if ((error = copyout(mtod(nam, caddr_t),
-				     (caddr_t)SCARG(uap, name),
-				     namelen)))
-			goto freeit;
-		if ((error = copyout((caddr_t)&namelen,
+			     (caddr_t)SCARG(uap, name), namelen)) == 0) {
+			error = copyout((caddr_t)&namelen,
 				     (caddr_t)SCARG(uap, anamelen),
-				     sizeof(*SCARG(uap, anamelen)))))
-			goto freeit;
+				     sizeof(*SCARG(uap, anamelen)));
+		}
 	}
+
+	/* if an error occured, free the file descriptor */
+	if (error)
+		ffree(fp);
+
 	m_freem(nam);
 	splx(s);
 	return (error);
-
- freeit:
-	ffree(fp);
-	m_freem(nam);
-	splx(s);
-	return (error);
-
 }
 
 /* ARGSUSED */
