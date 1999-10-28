96dfe68e81bc4e2d8c741ccf49d16d00ff1be740
lukem <lukem@NetBSD.org>
1999-10-28 06:37:32 +0000

    - change db_cmd_list() to list commands vertically rather than horizontally - sort entries in the various command tables, so that the `help' lists   are easier to use. this included hacking db_machine_commands_install()   to search for the "machine" entry to change the `more cmds' pointer,   rather than assuming it was the first entry - add a `sync' command, which is effectively `reboot 0x100'. - remove db_help_cmd(); it was unused (and was almost a duplicate of   db_cmd_list()). - move some extern decls to db_output.h, since they're used in more than one   place now - rename NEXT_TAB to DB_NEXT_TAB and move to db_output.h

--- sys/ddb/db_command.h
+++ sys/ddb/db_command.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: db_command.h,v 1.13 1999/05/10 21:13:05 thorpej Exp $	*/
+/*	$NetBSD: db_command.h,v 1.14 1999/10/28 06:37:32 lukem Exp $	*/
 
 /* 
  * Mach Operating System
@@ -42,11 +42,11 @@ void db_object_print_cmd __P((db_expr_t, int, db_expr_t, char *));
 void db_page_print_cmd __P((db_expr_t, int, db_expr_t, char *));
 void db_pool_print_cmd __P((db_expr_t, int, db_expr_t, char *));
 void db_machine_commands_install __P((struct db_command *));
-void db_help_cmd __P((void));
 void db_command_loop __P((void));
 void db_error __P((char *));
 void db_fncall __P((db_expr_t, int, db_expr_t, char *));
 void db_reboot_cmd __P((db_expr_t, int, db_expr_t, char *));
+void db_sync_cmd __P((db_expr_t, int, db_expr_t, char *));
 
 db_addr_t	db_dot;		/* current location */
 db_addr_t	db_last_addr;	/* last explicit address typed */
