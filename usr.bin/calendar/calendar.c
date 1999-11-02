9a6714e6de359ea0f9d48000c32f9ffcea8399f3
lukem <lukem@NetBSD.org>
1999-11-02 10:52:49 +0000

    remove superfluous '-' in getopt string

--- usr.bin/calendar/calendar.c
+++ usr.bin/calendar/calendar.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: calendar.c,v 1.18 1998/11/06 22:56:38 christos Exp $	*/
+/*	$NetBSD: calendar.c,v 1.19 1999/11/02 10:52:49 lukem Exp $	*/
 
 /*
  * Copyright (c) 1989, 1993, 1994
@@ -43,7 +43,7 @@ __COPYRIGHT("@(#) Copyright (c) 1989, 1993\n\
 #if 0
 static char sccsid[] = "@(#)calendar.c	8.4 (Berkeley) 1/7/95";
 #endif
-__RCSID("$NetBSD: calendar.c,v 1.18 1998/11/06 22:56:38 christos Exp $");
+__RCSID("$NetBSD: calendar.c,v 1.19 1999/11/02 10:52:49 lukem Exp $");
 #endif /* not lint */
 
 #include <sys/param.h>
@@ -102,7 +102,7 @@ main(argc, argv)
 	int ch;
 	const char *caldir;
 
-	while ((ch = getopt(argc, argv, "-ad:f:l:w:")) != -1)
+	while ((ch = getopt(argc, argv, "ad:f:l:w:")) != -1)
 		switch (ch) {
 		case '-':		/* backward contemptible */
 		case 'a':
