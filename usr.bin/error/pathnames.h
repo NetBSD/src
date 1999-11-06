d84c01f091938b9a5f1d7b67bc4c679f4261755c
kleink <kleink@NetBSD.org>
1999-11-06 15:11:46 +0000

    For temporary files' pathnames, check $TMPDIR before falling back to _PATH_TMP.

--- usr.bin/error/pathnames.h
+++ usr.bin/error/pathnames.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: pathnames.h,v 1.3 1995/09/02 06:15:40 jtc Exp $	*/
+/*	$NetBSD: pathnames.h,v 1.4 1999/11/06 15:11:46 kleink Exp $	*/
 
 /*
  * Copyright (c) 1989, 1993
@@ -41,5 +41,4 @@
 #define	IG_FILE2	"llib-port"
 #define	IG_FILE3	"/usr/lib/llib-lc"
 #define	IG_FILE4	"/usr/lib/llib-port"
-#undef _PATH_TMP
-#define	_PATH_TMP	"/tmp/ErrorXXXXXX"
+#define	TMPFILE		"ErrorXXXXXX"
