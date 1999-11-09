24c3a8c92fe6cdba004cddea39253293a6919e79
kleink <kleink@NetBSD.org>
1999-11-09 21:34:03 +0000

    Rip protection against multiple inclusion from wrapper headers.

--- sys/arch/x68k/include/svr4_machdep.h
+++ sys/arch/x68k/include/svr4_machdep.h
@@ -1,8 +1,3 @@
-/*	$NetBSD: svr4_machdep.h,v 1.2 1999/04/29 14:38:40 kleink Exp $	*/
-
-#ifndef _MACHINE_SVR4_MACHDEP_H_
-#define _MACHINE_SVR4_MACHDEP_H_
+/*	$NetBSD: svr4_machdep.h,v 1.3 1999/11/09 21:34:05 kleink Exp $	*/
 
 #include <m68k/svr4_machdep.h>
-
-#endif
