2dce5db3ea326b6304ab16a84e659e8e331a75ad
thorpej <thorpej@NetBSD.org>
1999-11-11 20:23:16 +0000

    Update for the improvements to libsa, and don't reference libkern.h.

--- sys/lib/libsa/nfs.c
+++ sys/lib/libsa/nfs.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: nfs.c,v 1.25 1999/03/31 01:50:25 cgd Exp $	*/
+/*	$NetBSD: nfs.c,v 1.26 1999/11/11 20:23:17 thorpej Exp $	*/
 
 /*-
  *  Copyright (c) 1993 John Brezak
@@ -42,11 +42,6 @@
 #include <sys/time.h>
 #include <sys/socket.h>
 #include <sys/stat.h>
-#ifdef _STANDALONE
-#include <lib/libkern/libkern.h>
-#else
-#include <string.h>
-#endif
 
 #include <netinet/in.h>
 #include <netinet/in_systm.h>
