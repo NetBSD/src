a24c82a65f74bd32dd79884a682d639b9b142d02
simonb <simonb@NetBSD.org>
1999-11-12 13:11:40 +0000

    Don't include <lib/libkern/libkern.h> for prototypes that are also in <lib/libsa/stand.h>.  The alpha still needs libkern for the division and remainder support.

--- sys/arch/alpha/stand/common/booted_dev.c
+++ sys/arch/alpha/stand/common/booted_dev.c
@@ -1,4 +1,4 @@
-/* $NetBSD: booted_dev.c,v 1.1 1999/04/02 03:19:08 cgd Exp $ */
+/* $NetBSD: booted_dev.c,v 1.2 1999/11/12 13:11:40 simonb Exp $ */
 
 /*
  * Copyright (c) 1999 Christopher G. Demetriou.  All rights reserved.
@@ -31,7 +31,6 @@
  */
 
 #include <lib/libsa/stand.h>
-#include <lib/libkern/libkern.h>
 
 #include <machine/prom.h>
 #include "common.h"
