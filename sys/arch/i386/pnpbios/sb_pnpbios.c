dd1e06523e19425a08a2eaaaf2b3341cc1ab0eb4
drochner <drochner@NetBSD.org>
1999-11-12 18:36:46 +0000

    "pnpbios" pseudo-bus. Reads out device IDs and ressource usage from the BIOS and attaches devices to it. While it is potentially able to deal with all the motherboard ISA devices, it is only used in hairy cases for now - laptop stuff in particular.

---
+++ sys/arch/i386/pnpbios/sb_pnpbios.c
@@ -0,0 +1,116 @@
+/* $NetBSD: sb_pnpbios.c,v 1.1 1999/11/12 18:36:46 drochner Exp $ */
+/*
+ * Copyright (c) 1999
+ * 	Matthias Drochner.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions, and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <sys/param.h>
+#include <sys/systm.h>
+#include <sys/errno.h>
+#include <sys/ioctl.h>
+#include <sys/syslog.h>
+#include <sys/device.h>
+#include <sys/proc.h>
+
+#include <machine/bus.h>
+
+#include <sys/audioio.h>
+#include <dev/audio_if.h>
+#include <dev/midi_if.h>
+#include <dev/mulaw.h>
+
+#include <dev/isa/isavar.h>
+#include <dev/isa/isadmavar.h>
+
+#include <i386/pnpbios/pnpbiosvar.h>
+
+#include <dev/isa/sbreg.h>
+#include <dev/isa/sbvar.h>
+
+#include <dev/isa/sbdspvar.h>
+
+int sb_pnpbios_match __P((struct device *, struct cfdata *, void *));
+void sb_pnpbios_attach __P((struct device *, struct device *, void *));
+
+struct cfattach sb_pnpbios_ca = {
+	sizeof(struct sbdsp_softc), sb_pnpbios_match, sb_pnpbios_attach
+};
+
+int
+sb_pnpbios_match(parent, match, aux)
+	struct device *parent;
+	struct cfdata *match;
+	void *aux;
+{
+	struct pnpbiosdev_attach_args *aa = aux;
+
+	if (strcmp(aa->idstr, "NMX2210"))
+		return (0);
+
+	return (1);
+}
+
+void
+sb_pnpbios_attach(parent, self, aux)
+	struct device *parent, *self;
+	void *aux;
+{
+	struct sbdsp_softc *sc = (void *)self;
+	struct pnpbiosdev_attach_args *aa = aux;
+
+	if (pnpbios_io_map(aa->pbt, aa->resc, 0, &sc->sc_iot, &sc->sc_ioh)) {
+		printf(": can't map i/o space\n");
+		return;
+	}
+
+	sc->sc_ic = aa->ic;
+
+	/* XXX These are only for setting chip configuration registers. */
+	pnpbios_getiobase(aa->pbt, aa->resc, 0, 0, &sc->sc_iobase);
+
+	if (pnpbios_getirqnum(aa->pbt, aa->resc, 0, &sc->sc_irq)) {
+		printf(": can't get IRQ\n");
+		return;
+	}
+
+	if (pnpbios_getdmachan(aa->pbt, aa->resc, 0, &sc->sc_drq8)) {
+		printf(": can't get DMA channel\n");
+		return;
+	}
+	if (pnpbios_getdmachan(aa->pbt, aa->resc, 1, &sc->sc_drq16))
+		sc->sc_drq16 = -1;
+
+	printf("\n%s", self->dv_xname);
+
+	if (!sbmatch(sc)) {
+		printf("%s: sbmatch failed\n", sc->sc_dev.dv_xname);
+		return;
+	}
+
+	sc->sc_ih = pnpbios_intr_establish(aa->pbt, aa->resc, 0, IPL_AUDIO,
+					   sbdsp_intr, sc);
+
+	sbattach(sc);
+}
+
