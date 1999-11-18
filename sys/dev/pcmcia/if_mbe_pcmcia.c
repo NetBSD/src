b3ecbcc0b43c1b2e76b7a315d3c0234b897b51bf
enami <enami>
1999-11-18 14:25:28 +0000

    Cope with new return value of pcmcia_scan_cis().  (Actually, it's not so new but at least newer when I wrote these code).

--- sys/dev/pcmcia/if_mbe_pcmcia.c
+++ sys/dev/pcmcia/if_mbe_pcmcia.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: if_mbe_pcmcia.c,v 1.9 1999/10/27 19:10:02 is Exp $	*/
+/*	$NetBSD: if_mbe_pcmcia.c,v 1.10 1999/11/18 14:25:28 enami Exp $	*/
 
 /*-
  * Copyright (c) 1998 The NetBSD Foundation, Inc.
@@ -84,7 +84,6 @@ int	mbe_pcmcia_enable __P((struct mb86960_softc *));
 void	mbe_pcmcia_disable __P((struct mb86960_softc *));
 
 struct mbe_pcmcia_get_enaddr_args {
-	int got_enaddr;
 	u_int8_t enaddr[ETHER_ADDR_LEN];
 };
 int	mbe_pcmcia_get_enaddr __P((struct pcmcia_tuple *, void *));
@@ -165,6 +164,7 @@ mbe_pcmcia_attach(parent, self, aux)
 	struct pcmcia_config_entry *cfe;
 	struct mbe_pcmcia_get_enaddr_args pgea;
 	const struct mbe_pcmcia_product *mpp;
+	int rv;
 
 	mpp = mbe_pcmcia_lookup(pa);
 	if (mpp == NULL) {
@@ -209,22 +209,27 @@ mbe_pcmcia_attach(parent, self, aux)
 
 	printf(": %s\n", mpp->mpp_name);
 
-	/* Read station address. */
-	pgea.got_enaddr = 0;
-	if (pcmcia_scan_cis(parent, mbe_pcmcia_get_enaddr, &pgea) == -1) {
+	/* Read station address from CIS. */
+	rv = pcmcia_scan_cis(parent, mbe_pcmcia_get_enaddr, &pgea);
+	if (rv == -1) {
 		printf("%s: Couldn't read CIS to get ethernet address\n",
 		    sc->sc_dev.dv_xname);
 		return;
-	} else if (!pgea.got_enaddr) {
+	} else if (rv == 0) {
 		printf("%s: Couldn't get ethernet address from CIS\n",
 		    sc->sc_dev.dv_xname);
 		return;
-	} else
+	}
+
 #ifdef DIAGNOSTIC
-		printf("%s: Ethernet address from CIS: %s\n",
-		    sc->sc_dev.dv_xname, ether_sprintf(pgea.enaddr))
+	if (rv != 1) {
+		printf("%s: pcmcia_scan_cis returns %d\n", sc->sc_dev.dv_xname,
+		    rv);
+		panic("mbe_pcmcia_attach");
+	}
+	printf("%s: Ethernet address from CIS: %s\n",
+	    sc->sc_dev.dv_xname, ether_sprintf(pgea.enaddr));
 #endif
-		;
 
 	/* Perform generic initialization. */
 	mb86960_attach(sc, MB86960_TYPE_86965, pgea.enaddr);
@@ -307,7 +312,6 @@ mbe_pcmcia_get_enaddr(tuple, arg)
 
 		for (i = 0; i < ETHER_ADDR_LEN; i++)
 			p->enaddr[i] = pcmcia_tuple_read_1(tuple, i + 2);
-		p->got_enaddr = 1;
 		return (1);
 	}
 	return (0);
