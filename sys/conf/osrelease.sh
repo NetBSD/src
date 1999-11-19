89e847a90fd3bc1a02b0489ea60e51b582d4a154
bouyer <bouyer@NetBSD.org>
1999-11-19 13:05:06 +0000

    kernel interface changed, bup version number (what will come after 1.4Z ? :)

--- sys/conf/osrelease.sh
+++ sys/conf/osrelease.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-#	$NetBSD: osrelease.sh,v 1.30 1999/11/15 18:49:08 fvdl Exp $
+#	$NetBSD: osrelease.sh,v 1.31 1999/11/19 13:05:06 bouyer Exp $
 #
 # Copyright (c) 1997 The NetBSD Foundation, Inc.
 # All rights reserved.
@@ -38,7 +38,7 @@
 #
 
 # Release number to use
-release=1.4O
+release=1.4P
 
 
 case $1 in
