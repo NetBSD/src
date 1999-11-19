011f86b97b952d161259a9e0184d087bce6fcfaf
simonb <simonb@NetBSD.org>
1999-11-19 02:35:40 +0000

    Don't need extern with function declaration.

--- sys/arch/pmax/dev/cfbvar.h
+++ sys/arch/pmax/dev/cfbvar.h
@@ -1,8 +1,7 @@
-/*	$NetBSD: cfbvar.h,v 1.2 1999/04/24 08:01:02 simonb Exp $	*/
+/*	$NetBSD: cfbvar.h,v 1.3 1999/11/19 02:35:40 simonb Exp $	*/
 
 /*
  * Initialize a Turbochannel CFB  dumb 2-d framebuffer,
  * so it can be used as a bitmapped glass-tty console device.
  */
-extern int
-cfbinit __P((struct fbinfo *fi, caddr_t base, int unit, int silent));
+int cfbinit __P((struct fbinfo *fi, caddr_t base, int unit, int silent));
