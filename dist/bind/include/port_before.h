42823f7dac8c4764b6bfcc35323f089a64c39c98
veego <veego@NetBSD.org>
1999-11-20 19:14:30 +0000

    Disable IRS and include namespace.h.

--- dist/bind/include/port_before.h
+++ dist/bind/include/port_before.h
@@ -1,8 +1,10 @@
-/*	$NetBSD: port_before.h,v 1.1.1.1 1999/11/20 18:54:03 veego Exp $	*/
+/*	$NetBSD: port_before.h,v 1.2 1999/11/20 19:14:30 veego Exp $	*/
 
+/*
 #define WANT_IRS_NIS
 #define WANT_IRS_PW
 #define WANT_IRS_GR
+*/
 #define SIG_FN void
 #if defined(HAS_PTHREADS) && defined(_REENTRANT)
 #define DO_PTHREADS
@@ -10,3 +12,5 @@
 
 #define SETGRENT_VOID
 #define SETPWENT_VOID
+
+#include "namespace.h"
