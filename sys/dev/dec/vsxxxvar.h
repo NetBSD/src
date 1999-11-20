e740272468688a2f4ce8b09508101782eae00cf3
nisimura <nisimura@NetBSD.org>
1999-11-20 04:55:30 +0000

    Introduce wscons vsxxx mice (and possible tablet) routines; changes of existing codes (lk201_ws.c, zskbd.c, and zsms.c) will follow.

---
+++ sys/dev/dec/vsxxxvar.h
@@ -0,0 +1,55 @@
+/* $NetBSD: vsxxxvar.h,v 1.1 1999/11/20 04:55:30 nisimura Exp $ */
+
+/*
+ * Copyright (c) 1999 Tohru Nishimura.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *      This product includes software developed by Tohru Nishimura.
+ *	for the NetBSD Project.
+ * 4. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+struct vsxxx_softc {
+	struct device sc_dev;
+	struct device *sc_wsmousedev;
+	unsigned sc_nbyte;
+	union {
+		u_int8_t raw[4];
+		struct {
+			u_int8_t data;
+			u_int8_t xpos;
+			u_int8_t ypos;
+		} ms;
+#if 0
+		struct {
+			u_int8_t data;
+			u_int8_t xloc[2];
+			u_int8_t yloc[2];
+		} tb;
+#endif
+	} sc_report;
+};
+
+extern struct wsmouse_accessops vsxxx_accessops;
+void vsxxx_input __P((int));
