c409168ecce4d5baa85aeb29845e9efca48df700
he <he>
1999-11-21 15:44:19 +0000

    Pull up part of revision 1.51 (via patch, requested by itohy):   Fix the problem where single-step tracing of a trap instruction   makes the system fall into kernel debugger.

--- sys/arch/x68k/x68k/locore.s
+++ sys/arch/x68k/x68k/locore.s
@@ -1,4 +1,4 @@
-/*	$NetBSD: locore.s,v 1.41.2.3 1999/06/24 15:58:16 perry Exp $	*/
+/*	$NetBSD: locore.s,v 1.41.2.4 1999/11/21 15:44:19 he Exp $	*/
 
 /*
  * Copyright (c) 1988 University of Utah.
@@ -409,9 +409,17 @@ ENTRY_NOPROFILE(trace)
 	clrl	sp@-			| stack adjust count
 	moveml	#0xFFFF,sp@-
 	moveq	#T_TRACE,d0
+
+	| Check PSW and see what happen.
+	|   T=0 S=0	(should not happen)
+	|   T=1 S=0	trace trap from user mode
+	|   T=0 S=1	trace trap on a trap instruction
+	|   T=1 S=1	trace trap from system mode (kernel breakpoint)
+
 	movw	sp@(FR_HW),d1		| get PSW
-	andw	#PSL_S,d1		| from system mode?
-	jne	Lkbrkpt			| yes, kernel breakpoint
+	notw	d1			| XXX no support for T0 on 680[234]0
+	andw	#PSL_TS,d1		| from system mode (T=1, S=1)?
+	jeq	Lkbrkpt			| yes, kernel breakpoint
 	jra	fault			| no, user-mode fault
 
 /*
