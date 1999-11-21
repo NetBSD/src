7b02919c0659689ff9600578e91382d7f4de0251
uch <uch@NetBSD.org>
1999-11-21 06:50:26 +0000

    PLUM2(TC6358TB) TX3922 companion chip

---
+++ sys/arch/hpcmips/dev/plumreg.h
@@ -0,0 +1,45 @@
+/*	$NetBSD: plumreg.h,v 1.1 1999/11/21 06:50:26 uch Exp $ */
+
+/*
+ * Copyright (c) 1999, by UCHIYAMA Yasushi
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. The name of the developer may NOT be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ */
+/*
+ *	TC6358TB(Plum2) TOSHIBA COMPANION CHIP FOR TMPR3922U
+ */
+
+/*
+ * Revision ID register
+ */
+#define	PLUM_ID_REGBASE			0xb0000
+#define	PLUM_ID_REGSIZE			0x4
+#define	PLUM_ID_REG			0x0
+#define	PLUM2_1				0x0002
+#define	PLUM2_2				0x0102
+
+/*
+ * Bus contorller
+ */
+#define PLUM_BUS_REGBASE	0x0000
+#define PLUM_BUS_REGSIZE	0x1000
