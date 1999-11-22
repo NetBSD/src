c2e142fb0a7a9a6cfece9054c5bdd152f55ce252
jdolecek <jdolecek@NetBSD.org>
1999-11-22 19:05:32 +0000

    don't prototype Debugger() here, including <sys/sytm.h> is enough

--- sys/arch/sun3/sun3/kgdb_machdep.c
+++ sys/arch/sun3/sun3/kgdb_machdep.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: kgdb_machdep.c,v 1.3 1998/02/05 04:57:37 gwr Exp $	*/
+/*	$NetBSD: kgdb_machdep.c,v 1.4 1999/11/22 19:05:32 jdolecek Exp $	*/
 
 /*
  * Copyright (c) 1990, 1993
@@ -51,8 +51,6 @@
 #include <sys/param.h>
 #include <sys/systm.h>
 #include <sys/kgdb.h>
-
-extern void Debugger __P((void));
 
 /*
  * Determine if the memory at va..(va+len) is valid.
