d1b7be06ced4ae983d25e622110dabc85d8861c1
jdolecek <jdolecek@NetBSD.org>
1999-11-22 16:53:59 +0000

    sun3 has cpu_Debugger() and hence Debugger() available even in non-DDB case

--- sys/sys/systm.h
+++ sys/sys/systm.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: systm.h,v 1.97 1999/10/14 18:42:16 jdolecek Exp $	*/
+/*	$NetBSD: systm.h,v 1.98 1999/11/22 16:53:59 jdolecek Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1988, 1991, 1993
@@ -306,9 +306,13 @@ void	kmstartup __P((void));
 #endif
 
 #ifdef _KERNEL
-#ifdef DDB
+#if defined(DDB) || defined(_SUN3_)
+/* note that cpu_Debugger() is always available on sun3 */
 void	cpu_Debugger __P((void));
 #define Debugger	cpu_Debugger
+#endif
+
+#ifdef DDB
 /*
  * Enter debugger(s) from console attention if enabled
  */
