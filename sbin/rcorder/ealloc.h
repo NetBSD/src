71393aa5670dc05b85d898a97772c114acf7f3e4
mrg <mrg>
1999-11-23 05:28:15 +0000

    move rcorder to sbin.

--- sbin/rcorder/ealloc.h
+++ sbin/rcorder/ealloc.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: ealloc.h,v 1.1.1.1 1999/11/19 04:30:56 mrg Exp $	*/
+/*	$NetBSD: ealloc.h,v 1.1 1999/11/23 05:28:20 mrg Exp $	*/
 
 void	*emalloc __P((size_t len));
 char	*estrdup __P((const char *str));
