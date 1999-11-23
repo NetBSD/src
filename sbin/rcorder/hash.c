99bf244e1e345c46f593a2a19c2187c9cb25f48d
mrg <mrg@NetBSD.org>
1999-11-23 05:28:15 +0000

    move rcorder to sbin.

--- sbin/rcorder/hash.c
+++ sbin/rcorder/hash.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: hash.c,v 1.1.1.1 1999/11/19 04:30:56 mrg Exp $	*/
+/*	$NetBSD: hash.c,v 1.1 1999/11/23 05:28:20 mrg Exp $	*/
 
 /*
  * Copyright (c) 1988, 1989, 1990 The Regents of the University of California.
@@ -39,14 +39,14 @@
  */
 
 #ifdef MAKE_BOOTSTRAP
-static char rcsid[] = "$NetBSD: hash.c,v 1.1.1.1 1999/11/19 04:30:56 mrg Exp $";
+static char rcsid[] = "$NetBSD: hash.c,v 1.1 1999/11/23 05:28:20 mrg Exp $";
 #else
 #include <sys/cdefs.h>
 #ifndef lint
 #if 0
 static char sccsid[] = "@(#)hash.c	8.1 (Berkeley) 6/6/93";
 #else
-__RCSID("$NetBSD: hash.c,v 1.1.1.1 1999/11/19 04:30:56 mrg Exp $");
+__RCSID("$NetBSD: hash.c,v 1.1 1999/11/23 05:28:20 mrg Exp $");
 #endif
 #endif /* not lint */
 #endif
