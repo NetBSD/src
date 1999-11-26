c8cdcc1bfd61e6b30db43e865dd59e6abc7bea15
nisimura <nisimura@NetBSD.org>
1999-11-26 07:17:51 +0000

    Make more realistic this mockup; still no clockwork inside.

--- sys/arch/pmax/tc/dtop.c
+++ sys/arch/pmax/tc/dtop.c
@@ -1,4 +1,4 @@
-/* $NetBSD: dtop.c,v 1.1.2.6 1999/11/19 08:56:48 nisimura Exp $ */
+/* $NetBSD: dtop.c,v 1.1.2.7 1999/11/26 07:17:51 nisimura Exp $ */
 
 /*
  * Copyright (c) 1998, 1999 Tohru Nishimura.  All rights reserved.
@@ -55,174 +55,13 @@
 #include <pmax/pmax/maxine.h>
 
 #include <dev/dec/lk201reg.h>
+#include <dev/dec/lk201var.h>
+#include <dev/dec/vsxxxvar.h>
 
-struct dtmessage {
-	u_int8_t src;
-	u_int8_t ctl;
-	u_int8_t body[1];
-};
-
-struct lk501_state {
-#define LK_KLL 8
-	int down_keys_list[LK_KLL];
-	int bellvol;
-	int leds_state;
-	u_int8_t msg[256];
-};
-
-struct dti_softc {
-	struct device	sc_dv;
-	struct lk501_state *lk501_ks;
-
-	bus_space_tag_t	sc_bst;
-	bus_space_handle_t sc_bsh;
-
-	struct device	*sc_wskbddev;
-	struct device	*sc_wsmousedev;
-
-	/* xmit/recv msg buffer pool management here */
-};
-
-static int  dtimatch __P((struct device *, struct cfdata *, void *));
-static void dtiattach __P((struct device *, struct device *, void *));
-
-const struct cfattach dtop_ca = {
-	sizeof(struct dti_softc), dtimatch, dtiattach
-};
-
-static int lk501_init __P((struct lk501_state *));
-static int lk501_decode __P((struct lk501_state *,
-				struct dtmessage *, u_int *, int *));
-static int lk501_null __P((void));
-
-const struct wskbd_accessops lk501_accessops = {
-	(void *)lk501_null,
-	(void *)lk501_null,
-	(void *)lk501_null,
-};
-
-const struct wskbd_mapdata lk501_keymapdata = {
-	zskbd_keydesctab,	/* XXX mis-no-ner XXX */
-	KB_US | KB_LK401,
-};
-
-void dtikbd_cnattach __P((void));
-static void dtikbd_cngetc __P((void *, u_int *, int *));
-static void dtikbd_cnpollc __P((void *, int));
-void dtikbd_input __P((struct dti_softc *, struct dtmessage *));
-
-const struct wskbd_consops dtikbd_consops = {
-	dtikbd_cngetc,
-	dtikbd_cnpollc,
-};
-
-static int  dtims_enable __P((void *));
-static int  dtims_ioctl __P((void *, u_long, caddr_t, int, struct proc *));
-static void dtims_disable __P((void *));
-void dtims_input __P((struct dti_softc *, struct dtmessage *));
-
-static const struct wsmouse_accessops dtims_accessops = { 
-	dtims_enable,
-	dtims_ioctl,
-	dtims_disable,
-};
-
-static int wscons_dti;
-static struct lk501_state dti_private;
-
-
-static int
-dtimatch(parent, match, aux)
-	struct device *parent;
-	struct cfdata *match;
-	void *aux;
-{
-	struct ioasicdev_attach_args *d = aux;
-
-	if (strcmp(d->iada_modname, "dtop") != 0)
-		return 0;
-
-	return 1;
-}
-
-static void
-dtiattach(parent, self, aux)
-	struct device *parent, *self;
-	void *aux;
-{
-	struct dti_softc *sc = (void *)self;
-	struct lk501_state *dti;
-#if 0
-	struct ioasicdev_attach_args *d = aux;
-#endif
-
-	printf(": unsable\n");
-
-	if (wscons_dti)
-		dti = &dti_private;
-	else {
-		dti = malloc(sizeof(struct lk501_state), M_DEVBUF, M_NOWAIT);
-		lk501_init(dti);
-	}
-	sc->lk501_ks = dti;
-
-	sc->sc_bst = ((struct ioasic_softc *)parent)->sc_bst;
-	sc->sc_bsh = ((struct ioasic_softc *)parent)->sc_bsh;
-
-#if 0
-	Send a query and wait for a while any response
-
-	foreach responder
-	switch whoitis {
-	case 0x6c: /* LK501 keyboard */ {
-		struct wskbddev_attach_args a;
-		a.console = wscons_dti;
-		a.keymap = &lk501_keymapdata;
-		a.accessops = &dtikbd_accessops;
-		a.accesscookie = (void *)sc;
-		sc->sc_wskbddev = config_found(self, &a, wskbddevprint);
-	    	break;
-		}
-	case 0x6a: /* VSXXX-GB mouse */ {
-		struct wsmousedev_attach_args b;
-		b.accessops = &dtims_accessops;
-		b.accesscookie = (void *)sc;
-		sc->sc_wsmousedev = config_found(self, &b, wsmousedevprint);
-		break;
-		}
-	}
-
-	ioasic_intr_establish(parent, d->iada_cookie, IPL_TTY, dtiintr, sc);
-#endif
-}
-
-/* EXPORT */ void
-dtikbd_cnattach()
-{
-	struct lk501_state *dti;
-
-	dti = &dti_private;
-	lk501_init(dti);
-
-	wskbd_cnattach(&dtikbd_consops, dti, &lk501_keymapdata);
-	wscons_dti = 1;
-}
-
-static void
-dtikbd_cngetc(v, type, data)
-	void *v;
-	u_int *type;
-	int *data;
-{
-#if 0
-	struct lk501_state *lks = v;
-	struct dtmessage *pkt = lks->msg;
-
-	do {
-		do {
-			dtimsgpoll(lks, pkt);
-		} while (pkt->src != 0x6c);
-	} while (!lk501_decode(lks, &msg, type, data));
+expand: illegal option -- a
+usage: expand [-t tablist] [file ...]
+		dtimsgpoll(lks, pkt);
+	} while (pkt->src != 0x6c || !lk501_decode(lks, &msg, type, data));
 #endif
 }
 
@@ -313,9 +152,10 @@ dtims_input(sc, pkt)
 
 	buttons = pkt->body[0] << 8 | pkt->body[1];
 	/* adjust button bit order; MRL -> LMR */
+	buttons &= 07;
 	if (buttons & 01)
 		buttons |= 010;
-	buttons = (buttons >> 1) & 03;
+	buttons >>= 1;
 
 	x = pkt->body[2] << 8 | pkt->body[3];
 	y = pkt->body[4] << 8 | pkt->body[5];
