812b534067b51d40c6063f509923e8682218fb94
he <he>
1999-11-27 15:49:14 +0000

    Pull up revision 1.18 (requested by is):   Fix i-cache/d-cache synchronization for M68040 cpus (see M68040   UM p.4-10).  Makes regress/sys/kern/sigtramp work even on M68040.

--- sys/arch/x68k/x68k/sys_machdep.c
+++ sys/arch/x68k/x68k/sys_machdep.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: sys_machdep.c,v 1.16 1999/03/24 14:07:39 minoura Exp $	*/
+/*	$NetBSD: sys_machdep.c,v 1.16.2.1 1999/11/27 15:49:14 he Exp $	*/
 
 /*
  * Copyright (c) 1982, 1986, 1993
@@ -154,6 +154,13 @@ cachectl1(req, addr, len, p)
 #endif
 
 		if (addr == 0 ||
+#if defined(M68040)
+#if defined(M68060)
+		    (cputype == CPU_68040 && req & CC_IPURGE) ||
+#else
+		    (req && CC_IPURGE) ||
+#endif
+#endif
 		    ((req & ~CC_EXTPURGE) != CC_PURGE && len > 2*NBPG))
 			doall = 1;
 
