bd2188ffc177bc440306efcfeae7dfb3a89d84bd
takemura <takemura>
1999-11-27 13:17:44 +0000

    Fix frame buffer type for NEC MC-R520 and -R520A. Add a menu item for IBM WorkPad z50.

--- sys/arch/hpcmips/stand/pbsdboot/main.c
+++ sys/arch/hpcmips/stand/pbsdboot/main.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: main.c,v 1.9 1999/11/21 07:58:50 takemura Exp $	*/
+/*	$NetBSD: main.c,v 1.10 1999/11/27 13:17:44 takemura Exp $	*/
 
 /*-
  * Copyright (c) 1999 Shin Takemura.
@@ -130,11 +130,11 @@ struct fb_setting fb_settings[] = {
 	{ TEXT("NEC MC-R510(15bit color)"), BIFB_D16_0000,
 		640, 240, 1600, 0xa000000,
 		PLATID_CPU_MIPS_VR_4121, PLATID_MACH_NEC_MCR_510 },
-	{ TEXT("MobileGearII MC-R520"), BIFB_D8_00,
-		640, 240, 1024, 0xa000000,
+	{ TEXT("MobileGearII MC-R520"), BIFB_D16_0000,
+		640, 240, 1600, 0xa000000,
 		PLATID_CPU_MIPS_VR_4121, PLATID_MACH_NEC_MCR_520 },
-	{ TEXT("Mobile Pro 770"), BIFB_D8_00,
-		640, 240, 1024, 0xa000000,
+	{ TEXT("Mobile Pro 770"), BIFB_D16_0000,
+		640, 240, 1600, 0xa000000,
 		PLATID_CPU_MIPS_VR_4121, PLATID_MACH_NEC_MCR_520A },
 	{ TEXT("MobileGearII MC-R700"), BIFB_D16_0000,
 		800, 600, 1600, 0xa000000,
@@ -163,6 +163,9 @@ struct fb_setting fb_settings[] = {
 	{ TEXT("INTERTOP CX300"), BIFB_D8_FF,
 		640, 480, 640, 0xa000000,
 		PLATID_CPU_MIPS_VR_4121, PLATID_MACH_FUJITSU_INTERTOP_IT300 },
+	{ TEXT("IBM WorkPad z50"), BIFB_D16_0000,
+		640, 480, 1280, 0xa000000,
+		PLATID_CPU_MIPS_VR_4121, PLATID_MACH_IBM_WORKPAD_Z50 },
 	{ TEXT("Philips Nino 312"), BIFB_D2_M2L_0,
 		240, 320, 0, 0,
 		PLATID_CPU_MIPS_TX_3912, PLATID_MACH_PHILIPS_NINO_312 },
@@ -416,7 +419,7 @@ BOOL CALLBACK DlgProc2(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 	case WM_INITDIALOG:
 		SetDlgItemText(hWnd, IDC_ABOUT_EDIT,
 			       TEXT("PocketBSD boot loader\r\n")
-			       TEXT("Version 1.7.3 1999.11.21\r\n")
+			       TEXT("Version 1.7.4 1999.11.27\r\n")
 			       TEXT("\r\n")
 			       TEXT("Copyright(C) 1999 Shin Takemura,\r\n")
 			       TEXT("All rights reserved.\r\n")
