e86c7b395df099590942631b3850983276662904
simonb <simonb@NetBSD.org>
1999-11-28 03:38:10 +0000

    Use installboot(8) to install the bootblocks. XXX: Need "disklabel -W" for now to enable writing to the label area.

--- distrib/utils/sysinst/arch/pmax/md.c
+++ distrib/utils/sysinst/arch/pmax/md.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: md.c,v 1.36 1999/08/16 08:29:06 abs Exp $	*/
+/*	$NetBSD: md.c,v 1.37 1999/11/28 03:38:10 simonb Exp $	*/
 
 /*
  * Copyright 1997 Piermont Information Systems Inc.
@@ -152,8 +152,11 @@ md_post_newfs(void)
 	}
 
 	printf(msg_string(MSG_dobootblks), diskdev);
-	run_prog(0, 1, NULL, "/sbin/disklabel -B %s /dev/r%sc",
-			"-b /usr/mdec/rzboot -s /usr/mdec/bootrz", diskdev);
+	cp_to_target("/usr/mdec/boot.pmax", "/boot.pmax");
+	/* XXX need to enable writing to disklabel first at the moment */
+	run_prog(0, 1, NULL, "/sbin/disklabel -W /dev/r%sc", diskdev);
+	run_prog(0, 1, "Warning: disk is probably not bootable",
+	    "/usr/mdec/installboot /dev/r%sc /usr/mdec/bootxx_ffs", diskdev);
 	return 0;
 }
 
