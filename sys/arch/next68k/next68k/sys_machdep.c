f4171993d6079b514a311f439f5c7c5c460f9883
is <is>
1999-11-28 20:30:57 +0000

    Fix typo (&& -> &).

--- sys/arch/next68k/next68k/sys_machdep.c
+++ sys/arch/next68k/next68k/sys_machdep.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: sys_machdep.c,v 1.9 1999/11/25 20:30:40 is Exp $ */
+/*	$NetBSD: sys_machdep.c,v 1.10 1999/11/28 20:31:00 is Exp $ */
 
 /*
  * This file was taken from mvme68k/mvme68k/sys_machdep.c
@@ -167,7 +167,7 @@ cachectl1(req, addr, len, p)
 #if defined(M68060)
 		    (cputype == CPU_68040 && req & CC_IPURGE) ||
 #else
-		    (req && CC_IPURGE) ||
+		    (req & CC_IPURGE) ||
 #endif
 #endif
 		    (req & ~CC_EXTPURGE) != CC_PURGE && len > 2*NBPG)
