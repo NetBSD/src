29ae1d238bed9b2f9f9f0479e5f181231e7c2252
itojun <itojun@NetBSD.org>
1999-11-29 12:53:59 +0000

    improve cnw driver, looking at bsdi/freebsd-pao cnw driver by wide+prb@bsdi. - implement ioctls compatible with bsdi/freebsd-pao.  stat type is   changed from u_int to u_quad_t to avoid overflows.  now it is possible   to switch domain/encryption key from the userland, using cnwctl(8). - implement multicast/promiscuous mode.  tested with IPv6.

--- sys/dev/pcmcia/if_cnwioctl.h
+++ sys/dev/pcmcia/if_cnwioctl.h
@@ -1,25 +1,81 @@
-/*	PAO2 Id: if_cnwioctl.h,v 1.1.8.1 1998/12/05 22:47:11 itojun Exp */
+/*	$NetBSD: if_cnwioctl.h,v 1.2 1999/11/29 12:54:00 itojun Exp $	*/
+
+/*
+ * Copyright (c) 1996, 1997 Berkeley Software Design, Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that this notice is retained,
+ * the conditions in the following notices are met, and terms applying
+ * to contributors in the following notices also apply to Berkeley
+ * Software Design, Inc.
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *      This product includes software developed by
+ *	Berkeley Software Design, Inc.
+ * 4. Neither the name of the Berkeley Software Design, Inc. nor the names
+ *    of its contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY BERKELEY SOFTWARE DESIGN, INC. ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL BERKELEY SOFTWARE DESIGN, INC. BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ *	PAO2 Id: if_cnwioctl.h,v 1.1.8.1 1998/12/05 22:47:11 itojun Exp
+ *
+ * Paul Borman, December 1996
+ *
+ * This driver is derived from a generic frame work which is
+ * Copyright(c) 1994,1995,1996
+ * Yoichi Shinoda, Yoshitaka Tokugawa, WIDE Project, Wildboar Project
+ * and Foretune.  All rights reserved.
+ *
+ * A linux driver was used as the "hardware reference manual" (i.e.,
+ * to determine registers and a general outline of how the card works)
+ * That driver is publically available and copyright
+ *
+ * John Markus Bj,Ax(Brndalen
+ * Department of Computer Science
+ * University of Troms,Ax(B
+ * Norway             
+ * johnm@staff.cs.uit.no, http://www.cs.uit.no/~johnm/
+ */
+
 struct cnwstatus {
 	struct ifreq	ifr;
 	u_char		data[0x100];
 };
 
 struct cnwstats {
-	u_int	nws_rx;
-	u_int	nws_rxerr;
-	u_int	nws_rxoverflow;
-	u_int	nws_rxoverrun;
-	u_int	nws_rxcrcerror;
-	u_int	nws_rxframe;
-	u_int	nws_rxerrors;
-	u_int	nws_rxavail;
-	u_int	nws_rxone;
-	u_int	nws_tx;
-	u_int	nws_txokay;
-	u_int	nws_txabort;
-	u_int	nws_txlostcd;
-	u_int	nws_txerrors;
-	u_int	nws_txretries[16];
+	u_quad_t nws_rx;
+	u_quad_t nws_rxerr;
+	u_quad_t nws_rxoverflow;
+	u_quad_t nws_rxoverrun;
+	u_quad_t nws_rxcrcerror;
+	u_quad_t nws_rxframe;
+	u_quad_t nws_rxerrors;
+	u_quad_t nws_rxavail;
+	u_quad_t nws_rxone;
+	u_quad_t nws_tx;
+	u_quad_t nws_txokay;
+	u_quad_t nws_txabort;
+	u_quad_t nws_txlostcd;
+	u_quad_t nws_txerrors;
+	u_quad_t nws_txretries[16];
 };
 
 struct cnwistats {
