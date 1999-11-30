0e0384db38786d998ebab938bcb2e3d301241753
veego <veego@NetBSD.org>
1999-11-30 20:18:11 +0000

    ndc is now a binary so we don't need the 'exec' anymore.

--- usr.sbin/bind/reload/named.reload.sh
+++ usr.sbin/bind/reload/named.reload.sh
@@ -1,8 +1,8 @@
 #!/bin/sh -
-#	$NetBSD: named.reload.sh,v 1.1.1.1 1998/10/05 18:02:00 tron Exp $
+#	$NetBSD: named.reload.sh,v 1.2 1999/11/30 20:18:11 veego Exp $
 #
 #	from named.reload	5.2 (Berkeley) 6/27/89
 #	from: Id: named.reload.sh,v 8.1 1994/12/15 06:24:14 vixie Exp 
 #
 
-exec %DESTSBIN%/%INDOT%ndc reload
+%DESTSBIN%/%INDOT%ndc reload
