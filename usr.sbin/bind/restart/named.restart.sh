626fb4bb79066434a7c945136951d67b642f1ebc
veego <veego@NetBSD.org>
1999-11-30 20:18:11 +0000

    ndc is now a binary so we don't need the 'exec' anymore.

--- usr.sbin/bind/restart/named.restart.sh
+++ usr.sbin/bind/restart/named.restart.sh
@@ -1,8 +1,8 @@
 #!/bin/sh -
-#	$NetBSD: named.restart.sh,v 1.1.1.1 1998/10/05 18:02:00 tron Exp $
+#	$NetBSD: named.restart.sh,v 1.2 1999/11/30 20:18:12 veego Exp $
 #
 #	from named.restart	5.4 (Berkeley) 6/27/89
 #	from: Id: named.restart.sh,v 8.1 1994/12/15 06:24:14 vixie Exp 
 #
 
-exec %DESTSBIN%/%INDOT%ndc restart
+%DESTSBIN%/%INDOT%ndc restart
