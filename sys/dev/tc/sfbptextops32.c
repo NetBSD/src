e999dc9f3cdb7ca1249fd3e7777499def0a2f53e
nisimura <nisimura@NetBSD.org>
1999-12-03 09:50:53 +0000

    The first order approximation of PMAGD SFB+ framebuffer.  Proven not working with 32bit PMAGD TURBOchannel option card (I got rainbow colour virtical stripes on screen).

---
+++ sys/dev/tc/sfbptextops32.c
@@ -0,0 +1,19 @@
+
+#include <sys/cdefs.h>			/* RCS ID & Copyright macro defns */
+
+__KERNEL_RCSID(0, "$NetBSD: sfbptextops32.c,v 1.1 1999/12/03 09:50:54 nisimura Exp $");
+
+#include <sys/param.h>
+#include <sys/systm.h>
+#include <sys/device.h>
+
+#include <dev/rcons/raster.h>
+#include <dev/wscons/wsconsio.h>
+#include <dev/wscons/wscons_raster.h>
+#include <dev/wscons/wsdisplayvar.h>
+
+#include <dev/tc/sfbreg.h>
+#include <dev/pci/tgareg.h>
+
+#define SFBBPP 32
+#include "sfbtextops.i"
