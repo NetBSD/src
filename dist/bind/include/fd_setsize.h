1547b766b08e29c50997bcb21a8d68f0a3dc0eaa
he <he@NetBSD.org>
1999-12-04 17:07:34 +0000

    Pull up revision 1.1 (new) (requested by christos and veego):   Update to BIND 8.2.2-P5.

---
+++ dist/bind/include/fd_setsize.h
@@ -0,0 +1,11 @@
+/*	$NetBSD: fd_setsize.h,v 1.1.1.1.2.2 1999/12/04 17:08:21 he Exp $	*/
+
+#ifndef _FD_SETSIZE_H
+#define _FD_SETSIZE_H
+
+/*
+ * If you need a bigger FD_SETSIZE, this is NOT the place to set it.
+ * This file is a fallback for BIND ports which don't specify their own.
+ */
+
+#endif /* _FD_SETSIZE_H */
