eef5e6026adfa00aca1692a4d4490cccb7c4ddd9
nisimura <nisimura@NetBSD.org>
1999-12-06 09:37:14 +0000

    Sync w/ recent changes in main trunk.

--- sys/arch/pmax/include/endian.h
+++ sys/arch/pmax/include/endian.h
@@ -1,7 +1,4 @@
-/*	$NetBSD: endian.h,v 1.7 1997/10/20 09:57:17 jonathan Exp $	*/
+/*	$NetBSD: endian.h,v 1.7.6.1 1999/12/06 09:38:16 nisimura Exp $	*/
 
-/*
- * Define the order of bytes in a 32-bit word.
- */
 #define _BYTE_ORDER _LITTLE_ENDIAN
 #include <mips/endian.h>
