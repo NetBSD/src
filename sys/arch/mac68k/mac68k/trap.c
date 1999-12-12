04ab4c4afc322c24f832c75cd326d49996b096c5
scottr <scottr>
1999-12-12 08:18:48 +0000

    Replace references to PGOFSET with m68k_trunc_page() and m68k_page_offset(), as appropriate.

--- sys/arch/mac68k/mac68k/trap.c
+++ sys/arch/mac68k/mac68k/trap.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: trap.c,v 1.77 1999/12/05 11:56:32 ragge Exp $	*/
+/*	$NetBSD: trap.c,v 1.78 1999/12/12 08:18:49 scottr Exp $	*/
 
 /*
  * Copyright (c) 1988 University of Utah.
@@ -727,7 +727,7 @@ writeback(fp, docachepush)
 			pmap_enter(pmap_kernel(), (vaddr_t)vmmap,
 			    trunc_page(f->f_fa), VM_PROT_WRITE,
 			    VM_PROT_WRITE|PMAP_WIRED);
-			fa = (u_int)&vmmap[(f->f_fa & PGOFSET) & ~0xF];
+			fa = (u_int)&vmmap[m68k_page_offset(f->f_fa) & ~0xF];
 			bcopy((caddr_t)&f->f_pd0, (caddr_t)fa, 16);
 			(void) pmap_extract(pmap_kernel(), (vaddr_t)fa, &pa);
 			DCFL(pa);
