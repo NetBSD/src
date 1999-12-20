b4a28a13b9e2d9a62a35bdfed581ab7b7bf255ab
he <he@NetBSD.org>
1999-12-20 21:35:50 +0000

    Declare NetBSD 1.4.2_ALPHA.

--- sys/conf/osrelease.sh
+++ sys/conf/osrelease.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-#	$NetBSD: osrelease.sh,v 1.15.2.4 1999/07/14 14:23:01 perry Exp $
+#	$NetBSD: osrelease.sh,v 1.15.2.5 1999/12/20 21:35:50 he Exp $
 #
 # Copyright (c) 1997 The NetBSD Foundation, Inc.
 # All rights reserved.
@@ -38,7 +38,7 @@
 #
 
 # Release number to use
-release=1.4.1
+release=1.4.2_ALPHA
 
 
 case $1 in
