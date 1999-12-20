d568b47f719a7415450ceb4e7315de843b3cf6f1
jwise <jwise@NetBSD.org>
1999-12-20 19:31:47 +0000

    TNF copyright -- I will do the paperwork to give these over asap.

--- usr.bin/systat/globalcmds.c
+++ usr.bin/systat/globalcmds.c
@@ -1,7 +1,8 @@
-/*	$NetBSD: globalcmds.c,v 1.3 1999/12/16 06:16:17 jwise Exp $ */
+/*	$NetBSD: globalcmds.c,v 1.4 1999/12/20 19:31:47 jwise Exp $ */
 
 /*-
- * Copyright (c) 1980, 1992, 1993 Jim Wise
+ * Copyright (c) 1999
+ *      The NetBSD Foundation, Inc.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -11,6 +12,12 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *      This product includes software developed by the NetBSD Foundation.
+ * 4. Neither the name of the Foundation nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
--- usr.bin/systat/ps.c
+++ usr.bin/systat/ps.c
@@ -1,4 +1,4 @@
-/*      $NetBSD: ps.c,v 1.10 1999/12/20 17:08:53 jwise Exp $  */
+/*      $NetBSD: ps.c,v 1.11 1999/12/20 19:31:47 jwise Exp $  */
 
 /*-
  * Copyright (c) 1999
@@ -14,9 +14,8 @@
  *    documentation and/or other materials provided with the distribution.
  * 3. All advertising materials mentioning features or use of this software
  *    must display the following acknowledgement:
- *      This product includes software developed by the University of
- *      California, Berkeley and its contributors.
- * 4. Neither the name of the University nor the names of its contributors
+ *      This product includes software developed by the NetBSD Foundation.
+ * 4. Neither the name of the Foundation nor the names of its contributors
  *    may be used to endorse or promote products derived from this software
  *    without specific prior written permission.
  *
@@ -46,7 +45,7 @@
 
 #include <sys/cdefs.h>
 #ifndef lint
-__RCSID("$NetBSD: ps.c,v 1.10 1999/12/20 17:08:53 jwise Exp $");
+__RCSID("$NetBSD: ps.c,v 1.11 1999/12/20 19:31:47 jwise Exp $");
 #endif /* not lint */
 
 #include <sys/param.h>
