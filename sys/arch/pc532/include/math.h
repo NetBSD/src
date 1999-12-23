19f51ab86148aaa3ba673421dfec95f6208c2c63
kleink <kleink@NetBSD.org>
1999-12-23 10:15:05 +0000

    C99: Define a NAN macro in <math.h> which evaulates to a constant expression of a single-precision quiet NaN; only to be defined on platforms that do support this value.

---
+++ sys/arch/pc532/include/math.h
@@ -0,0 +1,10 @@
+/*	$NetBSD: math.h,v 1.1 1999/12/23 10:15:14 kleink Exp $	*/
+
+/*
+ * ISO C99
+ */
+#if !defined(_ANSI_SOURCE) && !defined(_POSIX_C_SOURCE) && \
+    !defined(_XOPEN_SOURCE) || defined(_ISOC99_SOURCE)
+extern const char	__nanf[];
+#define	NAN		(*(const float *)(const void *)__nanf)
+#endif
