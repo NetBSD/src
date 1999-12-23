315864b5fd9385edc74d77bc56b13dfdd3824a0c
ad <ad>
1999-12-23 15:27:14 +0000

    mouseInput() should not be declared here.

--- sys/arch/pmax/dev/lk201var.h
+++ sys/arch/pmax/dev/lk201var.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: lk201var.h,v 1.5 1999/11/29 15:02:38 ad Exp $	*/
+/*	$NetBSD: lk201var.h,v 1.6 1999/12/23 15:27:14 ad Exp $	*/
 
 #ifndef _LK201VAR_H_
 #define _LK201VAR_H_
@@ -8,7 +8,6 @@
 char	*kbdMapChar __P((int, int *));
 void	KBDReset __P((dev_t, void (*)(dev_t, int)));
 void	MouseInit __P((dev_t, void (*)(dev_t, int), int (*)(dev_t)));
-void	mouseInput __P((int cc));
 
 int	LKgetc __P((dev_t dev));
 void	lk_divert __P((int (*getfn) __P ((dev_t dev)), dev_t in_dev));
