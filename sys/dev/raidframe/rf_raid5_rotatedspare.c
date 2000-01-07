1b5eeaab8e4bfe72c91b7ccc0f43e7c75466ae43
oster <oster@NetBSD.org>
2000-01-07 03:40:56 +0000

    Nuke: #include "rf_threadid.h".

--- sys/dev/raidframe/rf_raid5_rotatedspare.c
+++ sys/dev/raidframe/rf_raid5_rotatedspare.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: rf_raid5_rotatedspare.c,v 1.3 1999/02/05 00:06:16 oster Exp $	*/
+/*	$NetBSD: rf_raid5_rotatedspare.c,v 1.4 2000/01/07 03:41:03 oster Exp $	*/
 /*
  * Copyright (c) 1995 Carnegie-Mellon University.
  * All rights reserved.
@@ -37,7 +37,6 @@
 #include "rf_dag.h"
 #include "rf_dagutils.h"
 #include "rf_dagfuncs.h"
-#include "rf_threadid.h"
 #include "rf_general.h"
 #include "rf_utils.h"
 #include "rf_raid5_rotatedspare.h"
