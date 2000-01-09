ace49518e25c6d9c4c4a84d0eaa001a85a43956c
hubertf <hubertf@NetBSD.org>
2000-01-09 17:21:53 +0000

    Also move the +SIZE files into place, if a pkg comes with some.

--- usr.sbin/pkg_install/add/perform.c
+++ usr.sbin/pkg_install/add/perform.c
@@ -1,11 +1,11 @@
-/*	$NetBSD: perform.c,v 1.42 1999/12/01 15:15:29 hubertf Exp $	*/
+/*	$NetBSD: perform.c,v 1.43 2000/01/09 17:21:53 hubertf Exp $	*/
 
 #include <sys/cdefs.h>
 #ifndef lint
 #if 0
 static const char *rcsid = "from FreeBSD Id: perform.c,v 1.44 1997/10/13 15:03:46 jkh Exp";
 #else
-__RCSID("$NetBSD: perform.c,v 1.42 1999/12/01 15:15:29 hubertf Exp $");
+__RCSID("$NetBSD: perform.c,v 1.43 2000/01/09 17:21:53 hubertf Exp $");
 #endif
 #endif
 
@@ -490,6 +490,10 @@ pkg_do(char *pkg)
 			move_file(".", DEINSTALL_FNAME, LogDir);
 		if (fexists(REQUIRE_FNAME))
 			move_file(".", REQUIRE_FNAME, LogDir);
+		if (fexists(SIZE_PKG_FNAME))
+			move_file(".", SIZE_PKG_FNAME, LogDir);
+		if (fexists(SIZE_ALL_FNAME))
+			move_file(".", SIZE_ALL_FNAME, LogDir);
 		(void) snprintf(contents, sizeof(contents), "%s/%s", LogDir, CONTENTS_FNAME);
 		cfile = fopen(contents, "w");
 		if (!cfile) {
