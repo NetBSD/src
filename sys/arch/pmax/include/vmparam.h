6bac03e89c008173a01451171aee264bc7e5d74a
ad <ad@NetBSD.org>
2000-01-09 14:28:54 +0000

    - Protect from multiple includes. - Fix and beef up comments. - G/C unused def.

--- sys/arch/pmax/include/vmparam.h
+++ sys/arch/pmax/include/vmparam.h
@@ -1,17 +1,27 @@
-/*	$NetBSD: vmparam.h,v 1.9 1999/05/07 18:04:37 thorpej Exp $	*/
+/*	$NetBSD: vmparam.h,v 1.10 2000/01/09 14:28:54 ad Exp $	*/
+
+#ifndef _PMAX_VMPARAM_H_
+#define _PMAX_VMPARAM_H_	1
 
 #include <mips/vmparam.h>
 
 /*
- * DECstation has one physical memory segment.
+ * VM_PHYSSEG_MAX
+ *   We have two freelists. The first 8M of RAM goes onto a lower-priority
+ *   free list, since some TC boards (e.g. PixelStamp boards) are only able 
+ *   to DMA into this region, and we want them to have a fighting chance of
+ *   allocating their DMA memory during autoconfiguration.
+ *
+ * VM_PHYSSEG_NOADD
+ *   We can't add RAM after vm_mem_init.
  */
-#define	VM_PHYSSEG_MAX		2	/* 2 free lists */
+ 
+#define	VM_PHYSSEG_MAX		2
 #define	VM_PHYSSEG_STRAT	VM_PSTRAT_BSEARCH
-#define	VM_PHYSSEG_NOADD		/* can't add RAM after vm_mem_init */
+#define	VM_PHYSSEG_NOADD	1
 
 #define	VM_NFREELIST		2
 #define	VM_FREELIST_DEFAULT	0
 #define	VM_FREELIST_FIRST8	1
 
-/* pcb base */
-/*#define	pcbb(p)		((u_int)(p)->p_addr) */
+#endif	/* !_PMAX_VMPARAM_H_ */
