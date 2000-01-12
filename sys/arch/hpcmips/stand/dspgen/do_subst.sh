4d9156018ae40ddaa663c43dc109c1494fc160ba
cgd <cgd@NetBSD.org>
2000-01-12 02:33:54 +0000

    taking evil to the next level: config files, templates, and scripts to automatically generate project (.dsp) and workspace (.dsw) files from configuration scripts for the bits that have to be built on windows.  you build by saying 'make winfiles' here in stand, then check in (on UNIX).  The versions that windows sees have to have CR-NL line termination, so for proper use with samba the generated files contain CRs and must be checked in on UNIX (NOT from Windows CVS).

---
+++ sys/arch/hpcmips/stand/dspgen/do_subst.sh
@@ -0,0 +1,158 @@
+# $NetBSD: do_subst.sh,v 1.1 2000/01/12 02:33:54 cgd Exp $
+#
+# Copyright (c) 1999, 2000 Christopher G. Demetriou.  All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+# 3. All advertising materials mentioning features or use of this software
+#    must display the following acknowledgement:
+#      This product includes software developed by Christopher G. Demetriou
+#      for the NetBSD Project.
+# 4. The name of the author may not be used to endorse or promote products
+#    derived from this software without specific prior written permission
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+# OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+# IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+# NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+# THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+if [ "X$1" = "X--show-libdeps" ]; then
+	if ! expr "X$TYPE" : 'Xconsole_program.*' > /dev/null 2>&1 &&
+	   ! expr "X$TYPE" : 'Xapplication.*' > /dev/null 2>&1; then
+		exit
+	fi
+	( for lib in $LIBDEP_LIST; do
+		echo $lib
+	done ) | sort
+	exit
+fi
+
+AWK=awk
+if [ `uname` = SunOS ]; then
+	AWK=nawk
+fi
+
+STD_CPPDEF_LIST=''
+STD_INCDIR_LIST=''
+STD_LIBRARY_LIST=''
+
+export NAME
+export SRCFILE_LIST
+export CPPDEF_LIST STD_CPPDEF_LIST
+export INCDIR_LIST STD_INCDIR_LIST 
+export LIBDEP_LIST
+export LIBRARY_LIST STD_LIBRARY_LIST
+
+${AWK} '
+BEGIN {
+	NAME=ENVIRON["NAME"]
+
+	SRCFILES=""
+	sz = split(ENVIRON["SRCFILE_LIST"], a);
+	for (i = 1; i <= sz; i++) {
+		if (SRCFILES != "") {
+			SRCFILES=SRCFILES "\n"
+		}
+		SRCFILES=SRCFILES "# Begin Source File\n"
+		SRCFILES=SRCFILES "\n"
+		SRCFILES=SRCFILES "SOURCE=.\\" a[i] "\n"
+		SRCFILES=SRCFILES "# End Source File"
+	}
+
+	CPPDEFS=""
+	sz = split(ENVIRON["STD_CPPDEF_LIST"], a);
+	for (i = 1; i <= sz; i++) {
+		if (CPPDEFS != "") {
+			CPPDEFS=CPPDEFS " "
+		}
+		CPPDEFS=CPPDEFS "/D \"" a[i] "\""
+	}
+	sz = split(ENVIRON["CPPDEF_LIST"], a);
+	for (i = 1; i <= sz; i++) {
+		if (CPPDEFS != "") {
+			CPPDEFS=CPPDEFS " "
+		}
+		CPPDEFS=CPPDEFS "/D \"" a[i] "\""
+	}
+
+	INCDIRS=""
+	sz = split(ENVIRON["STD_INCDIR_LIST"], a);
+	for (i = 1; i <= sz; i++) {
+		if (INCDIRS != "") {
+			INCDIRS=INCDIRS " "
+		}
+		INCDIRS=INCDIRS "/I \"" a[i] "\""
+	}
+	sz = split(ENVIRON["INCDIR_LIST"], a);
+	for (i = 1; i <= sz; i++) {
+		if (INCDIRS != "") {
+			INCDIRS=INCDIRS " "
+		}
+		INCDIRS=INCDIRS "/I \"" a[i] "\""
+	}
+	sz = split(ENVIRON["LIBDEP_LIST"], a);
+	for (i = 1; i <= sz; i++) {
+		if (INCDIRS != "") {
+			INCDIRS=INCDIRS " "
+		}
+		INCDIRS=INCDIRS "/I \"..\\" a[i] "\""
+        }
+
+	LIBRARIES=""
+	sz = split(ENVIRON["STD_LIBRARY_LIST"], a);
+	for (i = 1; i <= sz; i++) {
+		if (LIBRARIES != "") {
+			LIBRARIES=LIBRARIES " "
+		}
+		LIBRARIES=LIBRARIES a[i] ".lib"
+	}
+	sz = split(ENVIRON["LIBRARY_LIST"], a);
+	for (i = 1; i <= sz; i++) {
+		if (LIBRARIES != "") {
+			LIBRARIES=LIBRARIES " "
+		}
+		LIBRARIES=LIBRARIES a[i] ".lib"
+        }
+	sz = split(ENVIRON["LIBDEP_LIST"], a);
+	for (i = 1; i <= sz; i++) {
+		if (LIBRARIES != "") {
+			LIBRARIES=LIBRARIES " "
+		}
+		LIBRARIES=LIBRARIES a[i] ".lib"
+	}
+
+	sz = split(ENVIRON["LIBDEP_LIST"], a);
+	DEBUG_LIBPATH=""
+	RELEASE_LIBPATH=""
+	for (i = 1; i <= sz; i++) {
+		if (i > 1) {
+			DEBUG_LIBPATH=DEBUG_LIBPATH " "
+			RELEASE_LIBPATH=RELEASE_LIBPATH " "
+		}
+		DEBUG_LIBPATH=DEBUG_LIBPATH "/libpath:\"..\\" a[i] "\\WMIPSDbg\""
+		RELEASE_LIBPATH=RELEASE_LIBPATH "/libpath:\"..\\" a[i] "\\WMIPSRel\""
+	}
+}
+{
+	gsub("%%% NAME %%%", NAME)
+	gsub("%%% SRCFILES %%%", SRCFILES)
+	gsub("%%% CPPDEFS %%%", CPPDEFS)
+	gsub("%%% INCDIRS %%%", INCDIRS)
+	gsub("%%% LIBRARIES %%%", LIBRARIES)
+	gsub("%%% DEBUG_LIBPATH %%%", DEBUG_LIBPATH)
+	gsub("%%% RELEASE_LIBPATH %%%", RELEASE_LIBPATH)
+	print $0
+}
+' ../dspgen/${TYPE}.tmpl | awk ' { printf "%s\r\n", $0 }' > ${NAME}.dsp
