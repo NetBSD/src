51d81984e39f887cbff68d4b631f0069c2296e50
augustss <augustss@NetBSD.org>
2000-01-13 20:16:49 +0000

    Regen.

--- sys/dev/mii/miidevs.h
+++ sys/dev/mii/miidevs.h
@@ -1,10 +1,10 @@
-/*	$NetBSD: miidevs.h,v 1.9 1999/11/19 18:26:22 thorpej Exp $	*/
+/*	$NetBSD: miidevs.h,v 1.10 2000/01/13 20:16:49 augustss Exp $	*/
 
 /*
  * THIS FILE AUTOMATICALLY GENERATED.  DO NOT EDIT.
  *
  * generated from:
- *	NetBSD: miidevs,v 1.9 1999/11/19 18:26:09 thorpej Exp 
+ *	NetBSD: miidevs,v 1.10 2000/01/13 20:16:08 augustss Exp 
  */
 
 /*-
@@ -56,6 +56,7 @@
  */
 
 #define	MII_OUI_AMD	0x00001a	/* Advanced Micro Devices */
+#define	MII_OUI_BROADCOM	0x001018	/* Broadcom */
 #define	MII_OUI_ENABLESEMI	0x0010dd	/* Enable Semiconductor */
 #define	MII_OUI_DAVICOM	0x00606e	/* Davicom Semiconductor */
 #define	MII_OUI_ICS	0x00a0be	/* Integrated Circuit Systems */
@@ -98,6 +99,10 @@
 #define	MII_STR_xxAMD_79C873	"Am79C873 10/100 media interface"
 #define	MII_MODEL_AMD_79c973phy	0x0036
 #define	MII_STR_AMD_79c973phy	"Am79c973 internal PHY"
+
+/* Broadcom PHYs */
+#define	MII_MODEL_BROADCOM_BCM5201	0x0021
+#define	MII_STR_BROADCOM_BCM5201	"BCM5201 10/100 media interface"
 
 /* Davicom Semiconductor PHYs */
 #define	MII_MODEL_xxDAVICOM_DM9101	0x0000
