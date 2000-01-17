8d9e12e91a49f98e75d5343284d1b467f651245f
kleink <kleink@NetBSD.org>
2000-01-17 16:21:36 +0000

    Adjust for little endian environments (confirmed by msaitoh).

--- lib/libc/arch/sh3/gen/infinity.c
+++ lib/libc/arch/sh3/gen/infinity.c
@@ -1,13 +1,18 @@
-/*	$NetBSD: infinity.c,v 1.1 2000/01/05 14:07:33 msaitoh Exp $	*/
+/*	$NetBSD: infinity.c,v 1.2 2000/01/17 16:21:36 kleink Exp $	*/
 
 #include <sys/cdefs.h>
 #ifndef lint
-__RCSID("$NetBSD: infinity.c,v 1.1 2000/01/05 14:07:33 msaitoh Exp $");
+__RCSID("$NetBSD: infinity.c,v 1.2 2000/01/17 16:21:36 kleink Exp $");
 #endif /* not lint */
 
 /* infinity.c */
 
 #include <math.h>
+#include <machine/endian.h>
 
 /* bytes for +Infinity on a SH3 */
+#if BYTE_ORDER == LITTLE_ENDIAN
+const char __infinity[] = { 0, 0, 0, 0, 0, 0, (char)0xf0, 0x7f };
+#else
 const char __infinity[] = { 0x7f, (char)0xf0, 0, 0, 0, 0, 0, 0 };
+#endif
