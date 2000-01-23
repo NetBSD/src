9b9da34be3bad23e7d60d4aa320de76168b6c8d8
he <he@NetBSD.org>
2000-01-23 12:01:34 +0000

    Pull up revision 1.6 (requested by mycroft):   Always do the errno indirection hack, so that non-threaded   libraries get the right errno value when linked with -lpthread.   This means "always include <errno.h> and never declare errno   yourself".

--- lib/libwrap/workarounds.c
+++ lib/libwrap/workarounds.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: workarounds.c,v 1.2 1997/10/09 21:20:58 christos Exp $	*/
+/*	$NetBSD: workarounds.c,v 1.2.4.1 2000/01/23 12:01:34 he Exp $	*/
 
  /*
   * Workarounds for known system software bugs. This module provides wrappers
@@ -14,7 +14,7 @@
 #if 0
 char    sccsid[] = "@(#) workarounds.c 1.6 96/03/19 16:22:25";
 #else
-__RCSID("$NetBSD: workarounds.c,v 1.2 1997/10/09 21:20:58 christos Exp $");
+__RCSID("$NetBSD: workarounds.c,v 1.2.4.1 2000/01/23 12:01:34 he Exp $");
 #endif
 #endif
 
@@ -28,8 +28,6 @@ __RCSID("$NetBSD: workarounds.c,v 1.2 1997/10/09 21:20:58 christos Exp $");
 #include <stdio.h>
 #include <syslog.h>
 #include <string.h>
-
-extern int errno;
 
 #include "tcpd.h"
 
