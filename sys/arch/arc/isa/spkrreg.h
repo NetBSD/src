615ec1b3482fd7b98b7db78befe9ae9d5286b1d6
soda <soda@NetBSD.org>
2000-01-23 20:08:42 +0000

    Moved from /cvsroot/syssrc/sys/arch/pica/isa/spkrreg.h,v

---
+++ sys/arch/arc/isa/spkrreg.h
@@ -0,0 +1,11 @@
+/*	$NetBSD: spkrreg.h,v 1.1.1.2 2000/01/23 20:08:43 soda Exp $	*/
+
+/*
+ * PIT port addresses and speaker control values
+ */
+
+#define PITAUX_PORT	0x61	/* port of Programmable Peripheral Interface */
+#define PIT_ENABLETMR2	0x01	/* Enable timer/counter 2 */
+#define PIT_SPKRDATA	0x02	/* Direct to speaker */
+
+#define PIT_SPKR	(PIT_ENABLETMR2|PIT_SPKRDATA)
