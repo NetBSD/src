ddeb2c49e06eed828633c8520c58bfd9f84bb627
soda <soda@NetBSD.org>
2000-01-27 07:49:13 +0000

    make "make includes" work

--- sys/arch/arc/include/pmap.h
+++ sys/arch/arc/include/pmap.h
@@ -1,7 +1,7 @@
-/*	$NetBSD: pmap.h,v 1.4 2000/01/23 21:01:58 soda Exp $	*/
+/*	$NetBSD: pmap.h,v 1.5 2000/01/27 07:49:13 soda Exp $	*/
 /*      $OpenBSD: pmap.h,v 1.3 1997/04/19 17:19:58 pefo Exp $ */
 
-#include <mips/include/pmap.h>
+#include <mips/pmap.h>
 
 #define pmap_resident_count(pmap)       ((pmap)->pm_stats.resident_count)
 #define PMAP_PREFER(pa, va)             pmap_prefer((pa), (va))
