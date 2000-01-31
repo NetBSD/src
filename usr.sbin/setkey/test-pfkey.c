6cd729217db92ccd7220dd1e4822d029c768ea57
itojun <itojun@NetBSD.org>
2000-01-31 14:22:41 +0000

    catch up with latest libipsec/kernel.

--- usr.sbin/setkey/test-pfkey.c
+++ usr.sbin/setkey/test-pfkey.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: test-pfkey.c,v 1.2 1999/07/06 13:13:03 itojun Exp $	*/
+/*	$NetBSD: test-pfkey.c,v 1.3 2000/01/31 14:22:44 itojun Exp $	*/
 
 /*
  * Copyright (C) 1995, 1996, 1997, 1998, and 1999 WIDE Project.
@@ -28,7 +28,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  */
-/* KAME Id: test-pfkey.c,v 1.1.4.3 1999/05/28 19:44:38 sakane Exp */
+/* KAME Id: test-pfkey.c,v 1.2 1999/10/26 08:09:17 itojun Exp */
 
 #include <sys/types.h>
 #include <sys/param.h>
@@ -477,7 +477,8 @@ key_setsadbaddr(ext, af, str)
 	a->sa_family = af;
 	_INPORTBYSA(a) = 
 		(ext == SADB_EXT_ADDRESS_PROXY ? 0 : htons(0x1234));
-	inet_pton(af, str, _INADDRBYSA(a));
+	if (inet_pton(af, str, _INADDRBYSA(a)) != 1)
+		; /* XXX do something */
 	
 	len = sizeof(struct sadb_address) + PFKEY_ALIGN8(a->sa_len);
 	m_addr.sadb_address_len = PFKEY_UNIT64(len);
