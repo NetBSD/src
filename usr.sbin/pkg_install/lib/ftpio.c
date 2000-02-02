10339dbfe5667786010533c5a223a9d308855c8d
hubertf <hubertf@NetBSD.org>
2000-02-02 14:09:40 +0000

    add copyright and (more important!) _RCS_ID()

--- usr.sbin/pkg_install/lib/ftpio.c
+++ usr.sbin/pkg_install/lib/ftpio.c
@@ -1,11 +1,37 @@
-/*	$NetBSD: ftpio.c,v 1.12 2000/02/01 10:46:55 agc Exp $	*/
-/*	 Id: foo2.c,v 1.12 1999/12/17 02:31:57 feyrer Exp feyrer 	*/
+/*	$NetBSD: ftpio.c,v 1.13 2000/02/02 14:09:40 hubertf Exp $	*/
+
+#include <sys/cdefs.h>
+#ifndef lint
+__RCSID("$NetBSD: ftpio.c,v 1.13 2000/02/02 14:09:40 hubertf Exp $");
+#endif
 
 /*
- * Work to implement wildcard dependency processing via FTP for the
- * NetBSD pkg_* package tools. 
+ * Copyright (c) 1999 Hubert Feyrer.  All rights reserved.
  *
- * Copyright (c) 1999 Hubert Feyrer <hubertf@netbsd.org>
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *      This product includes software developed by Hubert Feyrer.
+ * 4. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
 #include <sys/types.h>
