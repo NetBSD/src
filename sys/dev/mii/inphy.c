9914adc7c5d5a52cb89a5f0c03b9ab90ce6c5d84
thorpej <thorpej>
2000-02-02 23:34:56 +0000

    Let mii_phy_setmedia() handle the IFM_AUTO case, too.

--- sys/dev/mii/inphy.c
+++ sys/dev/mii/inphy.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: inphy.c,v 1.17 2000/02/02 17:50:44 thorpej Exp $	*/
+/*	$NetBSD: inphy.c,v 1.18 2000/02/02 23:34:56 thorpej Exp $	*/
 
 /*-
  * Copyright (c) 1998, 1999 The NetBSD Foundation, Inc.
@@ -183,18 +183,7 @@ inphy_service(sc, mii, cmd)
 		if ((mii->mii_ifp->if_flags & IFF_UP) == 0)
 			break;
 
-		switch (IFM_SUBTYPE(ife->ifm_media)) {
-		case IFM_AUTO:
-			/*
-			 * If we're already in auto mode, just return.
-			 */
-			if (PHY_READ(sc, MII_BMCR) & BMCR_AUTOEN)
-				return (0);
-			(void) mii_phy_auto(sc, 1);
-			break;
-		default:
-			mii_phy_setmedia(sc);
-		}
+		mii_phy_setmedia(sc);
 		break;
 
 	case MII_TICK:
