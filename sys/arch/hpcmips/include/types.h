4407430964806e2c65919bf400aa7c0d262b9713
cgd <cgd@NetBSD.org>
2000-02-05 00:13:22 +0000

    add __BROKEN_CONFIG_UNIT_USAGE #define, becuase these ports do very wrong things with device configuration data "cf_unit" information.

--- sys/arch/hpcmips/include/types.h
+++ sys/arch/hpcmips/include/types.h
@@ -1,3 +1,6 @@
-/*	$NetBSD: types.h,v 1.1.1.1 1999/09/16 12:23:24 takemura Exp $	*/
+/*	$NetBSD: types.h,v 1.2 2000/02/05 00:13:23 cgd Exp $	*/
 
 #include <mips/types.h>
+
+#define __BROKEN_CONFIG_UNIT_USAGE
+
