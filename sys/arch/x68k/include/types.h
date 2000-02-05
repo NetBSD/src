c31e0ae67057db1960598fc6b438828978784b95
cgd <cgd@NetBSD.org>
2000-02-05 00:13:22 +0000

    add __BROKEN_CONFIG_UNIT_USAGE #define, becuase these ports do very wrong things with device configuration data "cf_unit" information.

--- sys/arch/x68k/include/types.h
+++ sys/arch/x68k/include/types.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: types.h,v 1.5 2000/02/01 04:01:23 danw Exp $	*/
+/*	$NetBSD: types.h,v 1.6 2000/02/05 00:13:26 cgd Exp $	*/
 
 #ifndef _MACHINE_TYPES_H_
 #define _MACHINE_TYPES_H_
@@ -6,5 +6,7 @@
 #include <m68k/types.h>
 
 #define __HAVE_DEVICE_REGISTER
+
+#define __BROKEN_CONFIG_UNIT_USAGE
 
 #endif
