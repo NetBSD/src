6968a454bdef6cf63f0489e4a962ea69efe9cdbd
tsutsui <tsutsui@NetBSD.org>
2000-02-08 16:17:28 +0000

    Add news1200 support. Based on a patch from Koichi NISHIWAKI <k-nis@ba2.so-net.ne.jp>

--- sys/arch/news68k/include/autoconf.h
+++ sys/arch/news68k/include/autoconf.h
@@ -1,9 +1,7 @@
-/*	$NetBSD: autoconf.h,v 1.1 1999/12/09 14:53:07 tsutsui Exp $	*/
+/*	$NetBSD: autoconf.h,v 1.2 2000/02/08 16:17:33 tsutsui Exp $	*/
 
-/*
- *
- * Copyright (c) 1996 Charles D. Cranor
- * All rights reserved.
+/*-
+ * Copyright (C) 2000 Izumi Tsutsui.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -13,10 +11,7 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *      This product includes software developed by Charles D. Cranor.
- * 4. The name of the author may not be used to endorse or promote products
+ * 3. The name of the author may not be used to endorse or promote products
  *    derived from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
@@ -27,12 +22,16 @@
  * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
- * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-/*
- * autoconf.h
- */
+/* Shorthand for locators. */
+#include "locators.h"
+#define cf_systype cf_loc[MAINBUSCF_SYSTYPE]
 
-/* the following are from the prom/bootblocks */
+struct mainbus_attach_args {
+	char	*ma_name;	/* name of bus */
+	int	ma_systype;	/* system type */
+};
+
