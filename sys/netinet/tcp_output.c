1eacad43b797914264ae6fe8bf83fec055c54ed5
itojun <itojun@NetBSD.org>
2000-02-09 00:50:40 +0000

    optimize mbuf allocation for ip/tcp/tcpopt part.

--- sys/netinet/tcp_output.c
+++ sys/netinet/tcp_output.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: tcp_output.c,v 1.53 1999/12/13 15:17:20 itojun Exp $	*/
+/*	$NetBSD: tcp_output.c,v 1.54 2000/02/09 00:50:40 itojun Exp $	*/
 
 /*
 %%% portions-copyright-nrl-95
@@ -682,7 +682,9 @@ send:
 		m->m_data -= hdrlen;
 #else
 		MGETHDR(m, M_DONTWAIT, MT_HEADER);
-		if (m != NULL) {
+		if (m != NULL &&
+		    (max_linkhdr + hdrlen > MHLEN ||
+		     max_linkhdr + hdrlen + len <= MCLBYTES)) {
 			MCLGET(m, M_DONTWAIT);
 			if ((m->m_flags & M_EXT) == 0) {
 				m_freem(m);
@@ -695,7 +697,7 @@ send:
 		}
 		m->m_data += max_linkhdr;
 		m->m_len = hdrlen;
-		if (len <= MCLBYTES - hdrlen - max_linkhdr) {
+		if (len <= M_TRAILINGSPACE(m)) {
 			m_copydata(so->so_snd.sb_mb, off, (int) len,
 			    mtod(m, caddr_t) + hdrlen);
 			m->m_len += len;
@@ -727,7 +729,7 @@ send:
 			tcpstat.tcps_sndwinup++;
 
 		MGETHDR(m, M_DONTWAIT, MT_HEADER);
-		if (m != NULL) {
+		if (m != NULL && max_linkhdr + hdrlen > MHLEN) {
 			MCLGET(m, M_DONTWAIT);
 			if ((m->m_flags & M_EXT) == 0) {
 				m_freem(m);
