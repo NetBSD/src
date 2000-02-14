278a6ad2370367ab2fab48643e0c8da4b10bfee0
tsubai <tsubai>
2000-02-14 12:45:52 +0000

    Add minimal support of openpic.

--- sys/arch/macppc/macppc/extintr.c
+++ sys/arch/macppc/macppc/extintr.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: extintr.c,v 1.11 2000/02/11 13:15:43 tsubai Exp $	*/
+/*	$NetBSD: extintr.c,v 1.12 2000/02/14 12:45:52 tsubai Exp $	*/
 
 /*-
  * Copyright (c) 1995 Per Fogelstrom
@@ -39,6 +39,7 @@
  *
  *	@(#)isa.c	7.2 (Berkeley) 5/12/91
  */
+
 #include <sys/param.h>
 #include <sys/malloc.h>
 #include <sys/kernel.h>
@@ -46,45 +47,60 @@
 #include <vm/vm.h>
 #include <vm/vm_kern.h>
 
+#include <machine/autoconf.h>
 #include <machine/intr.h>
 #include <machine/psl.h>
 #include <machine/pio.h>
 
-unsigned int imen = 0xffffffff;
-volatile int cpl, ipending, astpending, tickspending;
-int imask[NIPL];
-u_char *interrupt_reg;
+#include <macppc/macppc/openpicreg.h>
 
 #define NIRQ 32
+#define HWIRQ_MAX (NIRQ - 4 - 1)
+#define HWIRQ_MASK 0x0fffffff
 
 void intr_calculatemasks __P((void));
 char *intr_typename __P((int));
 int fakeintr __P((void *));
-
-int intrtype[NIRQ], intrmask[NIRQ], intrlevel[NIRQ];
-struct intrhand *intrhand[NIRQ];
-
-extern u_int *heathrow_FCR;
 
 static __inline int cntlzw __P((int));
 static __inline int read_irq __P((void));
 static __inline int mapirq __P((int));
 static void enable_irq __P((int));
 
-static int hwirq[NIRQ], virq[ICU_LEN];
+static __inline u_int openpic_read __P((int));
+static __inline void openpic_write __P((int, u_int));
+void openpic_enable_irq __P((int));
+void openpic_disable_irq __P((int));
+void openpic_set_priority __P((int, int));
+static __inline int openpic_read_irq __P((int));
+static __inline void openpic_eoi __P((int));
+
+unsigned int imen = 0xffffffff;
+volatile int cpl, ipending, astpending, tickspending;
+int imask[NIPL];
+
+int intrtype[NIRQ], intrmask[NIRQ], intrlevel[NIRQ];
+struct intrhand *intrhand[NIRQ];
+
+static u_char hwirq[NIRQ], virq[ICU_LEN];
 static int virq_max = 0;
 
-#define HWIRQ_MAX 27
-#define HWIRQ_MASK 0x0fffffff
+static u_char *obio_base, *openpic_base;
 
-#define INT_STATE_REG0  (interrupt_reg + 0x20)
-#define INT_ENABLE_REG0 (interrupt_reg + 0x24)
-#define INT_CLEAR_REG0  (interrupt_reg + 0x28)
-#define INT_LEVEL_REG0  (interrupt_reg + 0x2c)
-#define INT_STATE_REG1  (INT_STATE_REG0  - 0x10)
-#define INT_ENABLE_REG1 (INT_ENABLE_REG0 - 0x10)
-#define INT_CLEAR_REG1  (INT_CLEAR_REG0  - 0x10)
-#define INT_LEVEL_REG1  (INT_LEVEL_REG0  - 0x10)
+extern u_int *heathrow_FCR;
+
+#define interrupt_reg	(obio_base + 0x10)
+
+#define INT_STATE_REG_H  (interrupt_reg + 0x00)
+#define INT_ENABLE_REG_H (interrupt_reg + 0x04)
+#define INT_CLEAR_REG_H  (interrupt_reg + 0x08)
+#define INT_LEVEL_REG_H  (interrupt_reg + 0x0c)
+#define INT_STATE_REG_L  (interrupt_reg + 0x10)
+#define INT_ENABLE_REG_L (interrupt_reg + 0x14)
+#define INT_CLEAR_REG_L  (interrupt_reg + 0x18)
+#define INT_LEVEL_REG_L  (interrupt_reg + 0x1c)
+
+#define have_openpic	(openpic_base != NULL)
 
 /*
  * Map 64 irqs into 32 (bits).
@@ -129,28 +145,28 @@ int
 read_irq()
 {
 	int rv = 0;
-	int state0, state1, p;
+	int lo, hi, p;
 
-	state0 = in32rb(INT_STATE_REG0);
-	if (state0)
-		out32rb(INT_CLEAR_REG0, state0);
-	while (state0) {
-		p = 31 - cntlzw(state0);
+	lo = in32rb(INT_STATE_REG_L);
+	if (lo)
+		out32rb(INT_CLEAR_REG_L, lo);
+	while (lo) {
+		p = 31 - cntlzw(lo);
 		rv |= 1 << virq[p];
-		state0 &= ~(1 << p);
+		lo &= ~(1 << p);
 	}
 
 	if (heathrow_FCR)			/* has heathrow? */
-		state1 = in32rb(INT_STATE_REG1);
+		hi = in32rb(INT_STATE_REG_H);
 	else
-		state1 = 0;
+		hi = 0;
 
-	if (state1)
-		out32rb(INT_CLEAR_REG1, state1);
-	while (state1) {
-		p = 31 - cntlzw(state1);
+	if (hi)
+		out32rb(INT_CLEAR_REG_H, hi);
+	while (hi) {
+		p = 31 - cntlzw(hi);
 		rv |= 1 << virq[p + 32];
-		state1 &= ~(1 << p);
+		hi &= ~(1 << p);
 	}
 
 	/* 1 << 0 is invalid. */
@@ -161,25 +177,93 @@ void
 enable_irq(x)
 	int x;
 {
-	int state0, state1, v;
+	int lo, hi, v;
 	int irq;
 
 	x &= HWIRQ_MASK;	/* XXX Higher bits are software interrupts. */
 
-	state0 = state1 = 0;
+	lo = hi = 0;
 	while (x) {
 		v = 31 - cntlzw(x);
 		irq = hwirq[v];
 		if (irq < 32)
-			state0 |= 1 << irq;
+			lo |= 1 << irq;
 		else
-			state1 |= 1 << (irq - 32);
+			hi |= 1 << (irq - 32);
 		x &= ~(1 << v);
 	}
 
-	out32rb(INT_ENABLE_REG0, state0);
+	out32rb(INT_ENABLE_REG_L, lo);
 	if (heathrow_FCR)
-		out32rb(INT_ENABLE_REG1, state1);
+		out32rb(INT_ENABLE_REG_H, hi);
+}
+
+u_int
+openpic_read(reg)
+	int reg;
+{
+	char *addr = openpic_base + reg;
+
+	return in32rb(addr);
+}
+
+void
+openpic_write(reg, val)
+	int reg;
+	u_int val;
+{
+	char *addr = openpic_base + reg;
+
+	out32rb(addr, val);
+}
+
+void
+openpic_enable_irq(irq)
+	int irq;
+{
+	u_int x;
+
+	x = openpic_read(OPENPIC_SRC_VECTOR(irq));
+	x &= ~OPENPIC_IMASK;
+	openpic_write(OPENPIC_SRC_VECTOR(irq), x);
+}
+
+void
+openpic_disable_irq(irq)
+	int irq;
+{
+	u_int x;
+
+	x = openpic_read(OPENPIC_SRC_VECTOR(irq));
+	x |= OPENPIC_IMASK;
+	openpic_write(OPENPIC_SRC_VECTOR(irq), x);
+}
+
+void
+openpic_set_priority(cpu, pri)
+	int cpu, pri;
+{
+	u_int x;
+
+	x = openpic_read(OPENPIC_CPU_PRIORITY(cpu));
+	x &= ~OPENPIC_CPU_PRIORITY_MASK;
+	x |= pri;
+	openpic_write(OPENPIC_CPU_PRIORITY(cpu), x);
+}
+
+int
+openpic_read_irq(cpu)
+	int cpu;
+{
+	return openpic_read(OPENPIC_IACK(cpu)) & OPENPIC_VECTOR_MASK;
+}
+
+void
+openpic_eoi(cpu)
+	int cpu;
+{
+	openpic_write(OPENPIC_EOI(cpu), 0);
+	openpic_read(OPENPIC_EOI(cpu));
 }
 
 /*
@@ -192,6 +276,7 @@ void
 intr_calculatemasks()
 {
 	int irq, level;
+	int irqs = 0;
 	struct intrhand *q;
 
 	/* First, figure out which levels each IRQ uses. */
@@ -275,11 +360,18 @@ intr_calculatemasks()
 	}
 
 	/* Lastly, determine which IRQs are actually in use. */
-	{
-		register int irqs = 0;
-		for (irq = 0; irq < NIRQ; irq++)
-			if (intrhand[irq])
-				irqs |= 1 << irq;
+	for (irq = 0; irq < NIRQ; irq++)
+		if (intrhand[irq])
+			irqs |= 1 << irq;
+
+	if (have_openpic) {
+		for (irq = 0; irq < NIRQ; irq++) {
+			if (irqs & (1 << irq))
+				openpic_enable_irq(hwirq[irq]);
+			else
+				openpic_disable_irq(hwirq[irq]);
+		}
+	} else {
 		imen = ~irqs;
 		enable_irq(~imen);
 	}
@@ -427,7 +519,7 @@ intr_disestablish(arg)
 void
 ext_intr()
 {
-	int i, irq = 0;
+	int irq;
 	int o_imen, r_imen;
 	int pcpl;
 	struct intrhand *ih;
@@ -459,8 +551,56 @@ start:
 		uvmexp.intrs++;
 		intrcnt[hwirq[irq]]++;
 	}
+
 	int_state &= ~r_imen;
 	if (int_state)
+		goto start;
+
+out:
+	splx(pcpl);	/* Process pendings. */
+}
+
+void
+ext_intr_openpic()
+{
+	int irq, realirq;
+	int r_imen;
+	int pcpl;
+	struct intrhand *ih;
+
+	pcpl = splhigh();	/* Turn off all */
+
+	realirq = openpic_read_irq(0);
+	if (realirq == 255) {
+		printf("sprious interrupt\n");
+		goto out;
+	}
+
+start:
+	irq = virq[realirq];
+
+	/* XXX check range */
+
+	r_imen = 1 << irq;
+
+	if ((pcpl & r_imen) != 0) {
+		ipending |= r_imen;	/* Masked! Mark this as pending */
+		openpic_disable_irq(realirq);
+	} else {
+		ih = intrhand[irq];
+		while (ih) {
+			(*ih->ih_fun)(ih->ih_arg);
+			ih = ih->ih_next;
+		}
+
+		uvmexp.intrs++;
+		intrcnt[hwirq[irq]]++;
+	}
+
+	openpic_eoi(0);
+
+	realirq = openpic_read_irq(0);
+	if (realirq != 255)
 		goto start;
 
 out:
@@ -487,8 +627,10 @@ do_pending_int()
 
 	pcpl = splhigh();		/* Turn off all */
 	hwpend = ipending & ~pcpl;	/* Do now unmasked pendings */
-	imen &= ~hwpend;
-	enable_irq(~imen);
+	if (!have_openpic) {
+		imen &= ~hwpend;
+		enable_irq(~imen);
+	}
 	hwpend &= HWIRQ_MASK;
 	while (hwpend) {
 		irq = 31 - cntlzw(hwpend);
@@ -500,6 +642,8 @@ do_pending_int()
 		}
 
 		intrcnt[hwirq[irq]]++;
+		if (have_openpic)
+			openpic_enable_irq(hwirq[irq]);
 	}
 
 	/*out32rb(INT_ENABLE_REG, ~imen);*/
@@ -523,4 +667,119 @@ do_pending_int()
 	cpl = pcpl;	/* Don't use splx... we are here already! */
 	asm volatile("mtmsr %0" :: "r"(emsr));
 	processing = 0;
+}
+
+void
+openpic_init()
+{
+	int irq;
+	u_int x;
+
+	/* disable all interrupts */
+	for (irq = 0; irq < 256; irq++)
+		openpic_write(OPENPIC_SRC_VECTOR(irq), OPENPIC_IMASK);
+
+	openpic_set_priority(0, 15);
+
+	/* we don't need 8259 pass through mode */
+	x = openpic_read(OPENPIC_CONFIG);
+	x |= OPENPIC_CONFIG_8259_PASSTHRU_DISABLE;
+	openpic_write(OPENPIC_CONFIG, x);
+
+	/* send all interrupts to cpu 0 */
+	for (irq = 0; irq < ICU_LEN; irq++)
+		openpic_write(OPENPIC_IDEST(irq), 1 << 0);
+
+	for (irq = 0; irq < ICU_LEN; irq++) {
+		x = irq;
+		x |= OPENPIC_IMASK;
+		x |= OPENPIC_POLARITY_POSITIVE;
+		x |= OPENPIC_SENSE_LEVEL;
+		x |= 8 << OPENPIC_PRIORITY_SHIFT;
+		openpic_write(OPENPIC_SRC_VECTOR(irq), x);
+	}
+
+	/* XXX set spurious intr vector */
+
+	openpic_set_priority(0, 0);
+
+	/* clear all pending interrunts */
+	for (irq = 0; irq < 256; irq++) {
+		openpic_read_irq(0);
+		openpic_eoi(0);
+	}
+
+	for (irq = 0; irq < ICU_LEN; irq++)
+		openpic_disable_irq(irq);
+
+	install_extint(ext_intr_openpic);
+}
+
+void
+legacy_int_init()
+{
+	out32rb(INT_ENABLE_REG_L, 0);		/* disable all intr. */
+	out32rb(INT_CLEAR_REG_L, 0xffffffff);	/* clear pending intr. */
+
+	install_extint(ext_intr);
+}
+
+#define HEATHROW_FCR_OFFSET	0x38		/* XXX should not here */
+#define GC_OBIO_BASE		0xf3000000
+
+void
+init_interrupt()
+{
+	int chosen;
+	int mac_io, reg[5];
+	int32_t ictlr;
+	char type[32];
+
+	openpic_base = NULL;
+
+	mac_io = OF_finddevice("mac-io");
+	if (mac_io == -1)
+		mac_io = OF_finddevice("/pci/mac-io");
+
+	if (mac_io == -1) {
+		/*
+		 * No mac-io.  Assume Grand-Central or OHare.
+		 */
+		obio_base = (void *)GC_OBIO_BASE;
+		legacy_int_init();
+		return;
+	}
+
+	if (OF_getprop(mac_io, "assigned-addresses", reg, sizeof(reg)) < 20)
+		goto failed;
+
+	obio_base = (void *)reg[2];
+	heathrow_FCR = (void *)(obio_base + HEATHROW_FCR_OFFSET);
+
+	bzero(type, sizeof(type));
+	chosen = OF_finddevice("/chosen");
+	if (OF_getprop(chosen, "interrupt-controller", &ictlr, 4) == 4)
+		OF_getprop(ictlr, "device_type", type, sizeof(type));
+
+	if (strcmp(type, "open-pic") != 0) {
+		/*
+		 * Not an open-pic.  Must be a Heathrow (compatible).
+		 */
+		legacy_int_init();
+		return;
+	} else {
+		/*
+		 * We have an Open PIC.
+		 */
+		if (OF_getprop(ictlr, "reg", reg, sizeof(reg)) < 8)
+			goto failed;
+
+		openpic_base = (void *)(obio_base + reg[0]);
+		openpic_init();
+		return;
+	}
+
+	printf("unknown interrupt controller\n");
+failed:
+	panic("init_interrupt: failed to initialize interrupt controller");
 }
