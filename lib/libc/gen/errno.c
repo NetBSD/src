ea22ccf4d057b2400150bbe1ff5a0442e1e81c23
he <he@NetBSD.org>
2000-02-15 10:05:06 +0000

    Pull up revision 1.3 (requested by kleink):   Arrange for a linker warning to be displayed when the errno datum   is accessed directly (not using <errno.h>).

--- lib/libc/gen/errno.c
+++ lib/libc/gen/errno.c
@@ -1,3 +1,8 @@
-/*	$NetBSD: errno.c,v 1.2 1995/02/27 03:42:51 cgd Exp $	*/
+/*	$NetBSD: errno.c,v 1.2.12.1 2000/02/15 10:05:06 he Exp $	*/
+
+#include <sys/cdefs.h>
+
+__warn_references(errno,
+    "warning: reference to deprecated errno; include <errno.h> for correct reference")
 
 int errno;
