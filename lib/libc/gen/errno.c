979eb0ea011c664174e33c5eef0f7b67388f788b
he <he@NetBSD.org>
2000-02-18 20:32:34 +0000

    Undo pull-up of revision 1.3 (requested by abs and kleink):   There's still ``legal'' references to errno, so don't complain.

--- lib/libc/gen/errno.c
+++ lib/libc/gen/errno.c
@@ -1,8 +1,3 @@
-/*	$NetBSD: errno.c,v 1.2.12.1 2000/02/15 10:05:06 he Exp $	*/
-
-#include <sys/cdefs.h>
-
-__warn_references(errno,
-    "warning: reference to deprecated errno; include <errno.h> for correct reference")
+/*	$NetBSD: errno.c,v 1.2.12.2 2000/02/18 20:32:34 he Exp $	*/
 
 int errno;
