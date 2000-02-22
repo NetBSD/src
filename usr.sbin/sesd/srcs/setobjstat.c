6ee2e28cd854abb49c279d23ed486d71d5ff19c4
mjacob <mjacob>
2000-02-22 06:06:06 +0000

    Make it so this will build with -DWARN. It's a pity that erh can't be bothered to contact the author first. Typical NetBSD bullshit.

--- usr.sbin/sesd/srcs/setobjstat.c
+++ usr.sbin/sesd/srcs/setobjstat.c
@@ -1,4 +1,4 @@
-/* $NetBSD: setobjstat.c,v 1.1 2000/02/21 08:10:30 mjacob Exp $ */ 
+/* $NetBSD: setobjstat.c,v 1.2 2000/02/22 06:06:10 mjacob Exp $ */ 
 /* $FreeBSD: $ */
 /* $OpenBSD: $ */
 /*
@@ -40,6 +40,8 @@
 #include <fcntl.h>
 #include <sys/ioctl.h>
 #include SESINC
+
+int main __P((int, char **));
 
 int
 main(a, v)
