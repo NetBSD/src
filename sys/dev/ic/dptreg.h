17107cafb1c8f6608caf8c6e9cfc71de7c05f3db
ad <ad>
2000-02-24 18:47:55 +0000

    - Don't use bitfields. - Cosmetic changes. - Shut down all HBAs "simultaneously", not individually. - Don't bother grokking HA_ST_MORE. It causes problems. - Other bits and pieces.

--- sys/dev/ic/dptreg.h
+++ sys/dev/ic/dptreg.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: dptreg.h,v 1.6 2000/01/18 16:50:38 ad Exp $	*/
+/*	$NetBSD: dptreg.h,v 1.7 2000/02/24 18:47:55 ad Exp $	*/
 
 /*
  * Copyright (c) 1999 Andy Doran <ad@NetBSD.org>
@@ -27,8 +27,8 @@
  *
  */
 
-#ifndef _IC_DPTREG_H_
-#define _IC_DPTREG_H_ 1
+#ifndef	_IC_DPTREG_H_
+#define	_IC_DPTREG_H_	1
 
 /* Hardware limits */
 #define DPT_MAX_TARGETS		16
@@ -41,6 +41,7 @@
 #define DPT_SG_SIZE        	64
 #define DPT_ABORT_TIMEOUT	2000	/* milliseconds */
 #define DPT_MORE_TIMEOUT	1000	/* microseconds */
+#define DPT_SCRATCH_SIZE	256	/* bytes */
 
 #ifdef _KERNEL
 
@@ -111,64 +112,63 @@
  * Structure of an EATA command packet.
  */
 struct eata_cp {
-	u_int8_t	cp_scsireset	:1;	/* cause a bus reset */
-	u_int8_t	cp_hbainit	:1;	/* cause HBA to reinitialize */
-	u_int8_t	cp_autosense	:1;	/* auto request sense on err */
-	u_int8_t	cp_scatter      :1;	/* doing SG I/O */
-	u_int8_t	cp_quick	:1;	/* return no status packet */
-	u_int8_t	cp_interpret	:1;	/* HBA interprets SCSI CDB */
-	u_int8_t	cp_dataout	:1;	/* data out phase */
-	u_int8_t	cp_datain	:1;	/* data in phase */
-	u_int8_t	cp_senselen;		/* request sense length */
-	u_int8_t	cp_unused0[3];		/* unused */
-	u_int8_t	cp_tophys	:1;	/* send to RAID component */
-	u_int8_t	cp_unused1	:7;	/* unused */
-	u_int8_t	cp_physunit	:1;	/* phys unit on mirrored pair */
-	u_int8_t	cp_noat		:1;	/* no address translation */
-	u_int8_t	cp_nocache	:1;	/* no HBA caching */
-	u_int8_t	cp_unused2	:5;	/* unused */
-	u_int8_t	cp_id		:5;	/* SCSI device id of target */
-	u_int8_t	cp_channel	:3;	/* SCSI channel id */
-	u_int8_t	cp_lun		:3;	/* SCSI LUN id */
-	u_int8_t	cp_unused3	:2;	/* unused */
-	u_int8_t	cp_luntar	:1;	/* CP is for target ROUTINE */
-	u_int8_t	cp_dispri	:1;	/* give disconnect privilege */
-	u_int8_t	cp_identify	:1;	/* always true */
-	u_int8_t	cp_msg[3];		/* message bytes 0-3 */
-
-	/* Partial SCSI CDB ref */
-	u_int8_t	cp_scsi_cmd;
-	u_int8_t	cp_extent	:1;
-	u_int8_t	cp_bytchk	:1;
-	u_int8_t	cp_reladr	:1;
-	u_int8_t	cp_cmplst	:1;
-	u_int8_t	cp_fmtdata	:1;
-	u_int8_t	cp_cdblun	:3;
-	u_int8_t	cp_page;
-	u_int8_t	cp_unused4;
-	u_int8_t	cp_len;
-	u_int8_t	cp_link		:1;
-	u_int8_t	cp_flag		:1;
-	u_int8_t	cp_unused5	:4;
-	u_int8_t	cp_vendor	:2;
-	u_int8_t	cp_cdbmore[6];
-
-	u_int32_t	cp_datalen;	/* length in bytes of data/SG list */
-	u_int32_t	cp_ccbid;	/* ID of software CCB */
-	u_int32_t	cp_dataaddr;	/* address of data/SG list */
-	u_int32_t	cp_stataddr;	/* addr for status packet */
-	u_int32_t	cp_senseaddr;	/* addr of req. sense (err only) */
+	u_int8_t	cp_ctl0;		/* Control flags 0 */
+	u_int8_t	cp_senselen;		/* Request sense length */
+	u_int8_t	cp_unused0[3];		/* Unused */
+	u_int8_t	cp_ctl1;		/* Control flags 1 */
+	u_int8_t	cp_ctl2;		/* Control flags 2 */
+	u_int8_t	cp_ctl3;		/* Control flags 3 */
+	u_int8_t	cp_ctl4;		/* Control flags 4 */
+	u_int8_t	cp_msg[3];		/* Message bytes 0-3 */
+	u_int8_t	cp_cdb_cmd;		/* SCSI CDB */
+	u_int8_t	cp_cdb_more0[3];	/* SCSI CDB */
+	u_int8_t	cp_cdb_len;		/* SCSI CDB */
+	u_int8_t	cp_cdb_more1[7];	/* SCSI CDB */
+	u_int32_t	cp_datalen;		/* Bytes of data/SG list */
+	u_int32_t	cp_ccbid;		/* ID of software CCB */
+	u_int32_t	cp_dataaddr;		/* Addr of data/SG list */
+	u_int32_t	cp_stataddr;		/* Addr of status packet */
+	u_int32_t	cp_senseaddr;		/* Addr of req. sense */
 };
+
+#define CP_C0_SCSI_RESET	0x01	/* Cause a bus reset */
+#define CP_C0_HBA_INIT		0x02	/* Reinitialize HBA */
+#define CP_C0_AUTO_SENSE	0x04	/* Auto request sense on error */
+#define CP_C0_SCATTER		0x08	/* Do scatter/gather I/O */
+#define CP_C0_QUICK		0x10	/* Return no status packet */
+#define CP_C0_INTERPRET		0x20	/* HBA interprets SCSI CDB */
+#define CP_C0_DATA_OUT		0x40	/* Data out phase */
+#define CP_C0_DATA_IN		0x80	/* Data in phase */
+
+#define CP_C1_TO_PHYS		0x01	/* Send to RAID component */
+#define CP_C1_RESERVED		0xfe
+
+#define CP_C2_PHYS_UNIT		0x01	/* Physical unit on mirrored pair */
+#define CP_C2_NO_AT		0x02	/* No address translation */
+#define CP_C2_NO_CACHE		0x04	/* No HBA caching */
+#define CP_C2_RESERVED		0xf8
+
+#define CP_C3_ID_MASK		0x1f	/* Target ID */
+#define CP_C3_ID_SHIFT		0
+#define CP_C3_CHANNEL_MASK	0xe0	/* Target channel */
+#define CP_C3_CHANNEL_SHIFT	5
+
+#define CP_C4_LUN_MASK		0x07	/* Target LUN */
+#define CP_C4_LUN_SHIFT		0
+#define CP_C4_RESERVED		0x18
+#define CP_C4_LUN_TAR		0x20	/* CP is for target ROUTINE */
+#define CP_C4_DIS_PRI		0x40	/* Give disconnect privilege */
+#define CP_C4_IDENTIFY		0x80	/* Always true */
 
 /*
  * EATA status packet as returned by controller upon command completion. It 
  * contains status, message info and a handle on the initiating CCB. 
  */
 struct eata_sp {
-	u_int8_t	sp_hba_status;		/* host adapter status */
+	u_int8_t	sp_hba_status;		/* Host adapter status */
 	u_int8_t	sp_scsi_status;		/* SCSI bus status */
-	u_int8_t	sp_reserved[2];		/* reserved */
-	u_int32_t	sp_inv_residue;		/* bytes not transfered */
+	u_int8_t	sp_reserved[2];		/* Reserved */
+	u_int32_t	sp_inv_residue;		/* Bytes not transfered */
 	u_int32_t	sp_ccbid;		/* ID of software CCB */
 	u_int8_t	sp_id_message;
 	u_int8_t	sp_que_message;
@@ -179,29 +179,29 @@ struct eata_sp {
 /* 
  * HBA status as returned by status packet. Bit 7 signals end of command. 
  */
-#define HA_NO_ERROR             0x00    /* No error on command */
-#define HA_ERROR_SEL_TO         0x01    /* Device selection timeout */
-#define HA_ERROR_CMD_TO         0x02    /* Device command timeout */
-#define HA_ERROR_RESET          0x03    /* SCSI bus was reset */
-#define HA_INIT_POWERUP         0x04    /* Initial controller power up */
-#define HA_UNX_BUSPHASE         0x05    /* Unexpected bus phase */
-#define HA_UNX_BUS_FREE         0x06    /* Unexpected bus free */
-#define HA_BUS_PARITY           0x07    /* SCSI bus parity error */
-#define HA_SCSI_HUNG            0x08    /* SCSI bus hung */
-#define HA_UNX_MSGRJCT          0x09    /* Unexpected message reject */
-#define HA_RESET_STUCK          0x0A    /* SCSI bus reset stuck */
-#define HA_RSENSE_FAIL          0x0B    /* Auto-request sense failed */
-#define HA_PARITY               0x0C    /* HBA memory parity error */
-#define HA_ABORT_NA             0x0D    /* CP aborted - not on bus */
-#define HA_ABORTED              0x0E    /* CP aborted - was on bus */
-#define HA_RESET_NA             0x0F    /* CP reset - not on bus */
-#define HA_RESET                0x10    /* CP reset - was on bus */
-#define HA_ECC                  0x11    /* HBA memory ECC error */
-#define HA_PCI_PARITY           0x12    /* PCI parity error */
-#define HA_PCI_MASTER           0x13    /* PCI master abort */
-#define HA_PCI_TARGET           0x14    /* PCI target abort */
-#define HA_PCI_SIGNAL_TARGET    0x15    /* PCI signalled target abort */
-#define HA_ABORT                0x20    /* Software abort (too many retries) */
+#define SP_HBA_NO_ERROR		0x00    /* No error on command */
+#define SP_HBA_ERROR_SEL_TO	0x01    /* Device selection timeout */
+#define SP_HBA_ERROR_CMD_TO	0x02    /* Device command timeout */
+#define SP_HBA_ERROR_RESET	0x03    /* SCSI bus was reset */
+#define SP_HBA_INIT_POWERUP	0x04    /* Initial controller power up */
+#define SP_HBA_UNX_BUSPHASE	0x05    /* Unexpected bus phase */
+#define SP_HBA_UNX_BUS_FREE	0x06    /* Unexpected bus free */
+#define SP_HBA_BUS_PARITY	0x07    /* SCSI bus parity error */
+#define SP_HBA_SCSI_HUNG	0x08    /* SCSI bus hung */
+#define SP_HBA_UNX_MSGRJCT	0x09    /* Unexpected message reject */
+#define SP_HBA_RESET_STUCK	0x0a    /* SCSI bus reset stuck */
+#define SP_HBA_RSENSE_FAIL	0x0b    /* Auto-request sense failed */
+#define SP_HBA_PARITY		0x0c    /* HBA memory parity error */
+#define SP_HBA_ABORT_NA		0x0d    /* CP aborted - not on bus */
+#define SP_HBA_ABORTED		0x0e    /* CP aborted - was on bus */
+#define SP_HBA_RESET_NA		0x0f    /* CP reset - not on bus */
+#define SP_HBA_RESET		0x10    /* CP reset - was on bus */
+#define SP_HBA_ECC		0x11    /* HBA memory ECC error */
+#define SP_HBA_PCI_PARITY	0x12    /* PCI parity error */
+#define SP_HBA_PCI_MASTER	0x13    /* PCI master abort */
+#define SP_HBA_PCI_TARGET	0x14    /* PCI target abort */
+#define SP_HBA_PCI_SIG_TARGET	0x15    /* PCI signalled target abort */
+#define SP_HBA_ABORT		0x20    /* Software abort (too many retries) */
 
 /*
  * Scatter-gather list element.
@@ -212,53 +212,71 @@ struct eata_sg {
 };
 
 /*
- * EATA configuration data as returned by HBA. XXX this is bogus, some fields
- * don't *seem* to be filled on my SmartCache III. Also, it doesn't sync up 
- * with the structure FreeBSD uses. [ad]
+ * EATA configuration data as returned by HBA. XXX this is bogus - it
+ * doesn't sync up with the structure FreeBSD uses. [ad]
  */
 struct eata_cfg {
-        u_int8_t  ec_devtype;
-        u_int8_t  ec_pagecode;
-        u_int8_t  ec_reserved0;
-        u_int8_t  ec_cfglen;		/* Length in bytes after this field */
-        u_int8_t  ec_eatasig[4];	/* EATA signature  */
-        u_int8_t  ec_eataversion;	/* EATA version number */
-	u_int8_t  ec_overlapcmds : 1;	/* Overlapped cmds supported */
-	u_int8_t  ec_targetmode : 1;	/* Target mode supported */
-	u_int8_t  ec_trunnotrec : 1;	/* Truncate cmd not supported */
-	u_int8_t  ec_moresupported:1;	/* More cmd supported */
-	u_int8_t  ec_dmasupported : 1;	/* DMA mode supported */
-	u_int8_t  ec_dmanumvalid : 1;	/* DMA channel field is valid */
-	u_int8_t  ec_atadev : 1;	/* This is an ATA device */
-	u_int8_t  ec_hbavalid : 1;	/* HBA field is valid */
-        u_int8_t  ec_padlength[2];	/* Pad bytes for PIO cmds */
-        u_int8_t  ec_hba[4];		/* Host adapter SCSI IDs */
-        u_int8_t  ec_cplen[4];		/* Command packet length */
-        u_int8_t  ec_splen[4];		/* Status packet length */
-        u_int8_t  ec_queuedepth[2];	/* Controller queue depth */
-        u_int8_t  ec_reserved1[2];
-        u_int8_t  ec_sglen[2];		/* Maximum scatter gather list size */
-        u_int8_t  ec_irqnum : 4;	/* IRQ number */
-        u_int8_t  ec_irqtrigger : 1;	/* IRQ trigger: 0 = edge, 1 = level */
-        u_int8_t  ec_secondary : 1;	/* Controller not at address 0x170 */
-        u_int8_t  ec_dmanum : 2; 	/* DMA channel index for ISA */
-        u_int8_t  ec_irq;		/* IRQ address */
-	u_int8_t  ec_iodisable : 1;	/* ISA I/O address disabled */
-	u_int8_t  ec_forceaddr : 1;	/* PCI forced to an EISA/ISA addr */
-	u_int8_t  ec_sg64k : 1;		/* 64K of SG space */
-	u_int8_t  ec_sgunaligned : 1;	/* Can do unaligned SG, otherwise 4 */
-	u_int8_t  ec_reserved2 : 4;	/* Reserved */
-        u_int8_t  ec_maxtarget : 5;	/* Maximun SCSI target ID supported */
-        u_int8_t  ec_maxchannel : 3;	/* Maximun channel number supported */
-        u_int8_t  ec_maxlun;		/* Maximum LUN supported */
-	u_int8_t  ec_reserved3 : 3;	/* Reserved field */
-	u_int8_t  ec_autoterm : 1;	/* Support auto term (low byte) */
-	u_int8_t  ec_pcim1 : 1;		/* PCI M1 chipset */
-	u_int8_t  ec_bogusraidid : 1;	/* Raid ID may be questionable  */
-	u_int8_t  ec_pci : 1;		/* PCI adapter */
-	u_int8_t  ec_eisa : 1;		/* EISA adapter */
-        u_int8_t  ec_raidnum;		/* RAID host adapter humber */
+	u_int8_t	ec_devtype;
+	u_int8_t	ec_pagecode;
+	u_int8_t	ec_reserved0;
+	u_int8_t	ec_cfglen;		/* Length in bytes past here */
+	u_int8_t	ec_eatasig[4];		/* EATA signature */
+	u_int8_t	ec_eataversion;		/* EATA version number */
+	u_int8_t	ec_feat0;		/* First feature byte */
+	u_int8_t	ec_padlength[2];	/* Pad bytes for PIO cmds */
+	u_int8_t	ec_hba[4];		/* Host adapter SCSI IDs */
+	u_int8_t	ec_cplen[4];		/* Command packet length */
+	u_int8_t	ec_splen[4];		/* Status packet length */
+	u_int8_t	ec_queuedepth[2];	/* Controller queue depth */
+	u_int8_t	ec_reserved1[2];
+	u_int8_t	ec_sglen[2];		/* Maximum s/g list size */
+	u_int8_t	ec_feat1;		/* 2nd feature byte */
+	u_int8_t	ec_irq;			/* IRQ address */
+	u_int8_t	ec_feat2;		/* 3rd feature byte */
+	u_int8_t	ec_feat3;		/* 4th feature byte */
+	u_int8_t	ec_maxlun;		/* Maximum LUN supported */
+	u_int8_t	ec_feat4;		/* 5th feature byte */
+	u_int8_t	ec_raidnum;		/* RAID host adapter humber */
 };
+
+#define EC_F0_OVERLAP_CMDS	0x01	/* Overlapped cmds supported */
+#define EC_F0_TARGET_MODE	0x02	/* Target mode supported */
+#define EC_F0_TRUNC_NOT_REC	0x04	/* Truncate cmd not supported */
+#define EC_F0_MORE_SUPPORTED	0x08	/* More cmd supported */
+#define EC_F0_DMA_SUPPORTED	0x10	/* DMA mode supported */
+#define EC_F0_DMA_NUM_VALID	0x20	/* DMA channel field is valid */
+#define EC_F0_ATA_DEV		0x40	/* This is an ATA device */
+#define EC_F0_HBA_VALID		0x80	/* HBA field is valid */
+
+#define EC_F1_IRQ_NUM_MASK	0x0f	/* IRQ number mask */
+#define EC_F1_IRQ_NUM_SHIFT	0
+#define EC_F1_IRQ_TRIGGER	0x10	/* IRQ trigger: 0 = edge, 1 = level */
+#define EC_F1_SECONDARY		0x20	/* Controller not at address 0x170 */
+#define EC_F1_DMA_NUM_MASK	0xc0 	/* DMA channel *index* for ISA */
+#define EC_F1_DMA_NUM_SHIFT	6
+
+#define EC_F2_ISA_IO_DISABLE	0x01	/* ISA I/O address disabled */
+#define EC_F2_FORCE_ADDR	0x02	/* HBA forced to EISA/ISA address */
+#define EC_F2_SG_64K		0x04	/* 64kB of scatter/gather space */
+#define EC_F2_SG_UNALIGNED	0x08	/* Can do unaligned scatter/gather */
+#define EC_F2_RESERVED0		0x10	/* Reserved */
+#define EC_F2_RESERVED1		0x20	/* Reserved */
+#define EC_F2_RESERVED2		0x40	/* Reserved */
+#define EC_F2_RESERVED3		0x40	/* Reserved */
+
+#define EC_F3_MAX_TARGET_MASK	0x1f	/* Maximum target ID supported */
+#define EC_F3_MAX_TARGET_SHIFT	0
+#define EC_F3_MAX_CHANNEL_MASK	0xe0	/* Maximum channel ID supported */
+#define EC_F3_MAX_CHANNEL_SHIFT	5
+
+#define EC_F4_RESERVED0		0x01	/* Reserved */
+#define EC_F4_RESERVED1		0x02	/* Reserved */
+#define EC_F4_RESERVED2		0x04	/* Reserved */
+#define EC_F4_AUTO_TERM		0x08	/* Supports auto termination */
+#define EC_F4_PCIM1		0x10	/* PCI M1 chipset */
+#define EC_F4_BOGUS_RAID_ID	0x20	/* RAID ID may be questionable  */
+#define EC_F4_HBA_PCI		0x40	/* PCI adapter */
+#define EC_F4_HBA_EISA		0x80	/* EISA adapter */
 
 /*
  * How SCSI inquiry data breaks down for EATA boards.
@@ -271,11 +289,11 @@ struct eata_inquiry_data {
 	u_int8_t 	ei_additional_length;
 	u_int8_t 	ei_unused[2];
 	u_int8_t	ei_flags;	
-	char		ei_vendor[8];	/* Vendor, e.g: DPT, NEC */
-	char		ei_model[7];	/* Model number */
-	char		ei_suffix[9];	/* Model number suffix */
-	char		ei_fw[3];	/* Firmware */
-	char		ei_fwrev[1];	/* Firmware revision */
+	char		ei_vendor[8];		/* Vendor, e.g: DPT, NEC */
+	char		ei_model[7];		/* Model number */
+	char		ei_suffix[9];		/* Model number suffix */
+	char		ei_fw[3];		/* Firmware */
+	char		ei_fwrev[1];		/* Firmware revision */
 	u_int8_t	ei_extra[8];
 };
 
