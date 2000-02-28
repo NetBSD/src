d788e0dbad0a87038d631020785c6c601a96c416
thorpej <thorpej@NetBSD.org>
2000-02-28 17:44:20 +0000

    Make sure to initialize xs->req_sense_length (to 0).  This appears to be a historical leftover, and very few drivers actually look at it.  But the PCI `ncr' driver does, and if this field is not initialized to its default value, that driver will fall over flat on its face when it attempts to get sense data from a target.

--- sys/dev/scsipi/scsipi_base.h
+++ sys/dev/scsipi/scsipi_base.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: scsipi_base.h,v 1.9 1998/12/08 00:20:16 thorpej Exp $	*/
+/*	$NetBSD: scsipi_base.h,v 1.10 2000/02/28 17:44:20 thorpej Exp $	*/
 
 /*-
  * Copyright (c) 1998 The NetBSD Foundation, Inc.
@@ -79,6 +79,7 @@ scsipi_make_xs(sc_link, scsipi_cmd, cmdlen, data_addr, datalen,
 	xs->retries = retries;
 	xs->timeout = timeout;
 	xs->bp = bp;
+	xs->req_sense_length = 0;	/* XXX field is not really useful */
 
 	return (xs);
 }
