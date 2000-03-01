092e9c373a81d4cd9fc9ada6a08503fe67a2a0b2
itojun <itojun@NetBSD.org>
2000-03-01 12:52:11 +0000

    increase kernel version to 1.4U (5 more to 1.4Z!)

--- sys/conf/osrelease.sh
+++ sys/conf/osrelease.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-#	$NetBSD: osrelease.sh,v 1.35 2000/02/17 11:41:59 darrenr Exp $
+#	$NetBSD: osrelease.sh,v 1.36 2000/03/01 12:52:11 itojun Exp $
 #
 # Copyright (c) 1997 The NetBSD Foundation, Inc.
 # All rights reserved.
@@ -38,7 +38,7 @@
 #
 
 # Release number to use
-release=1.4T
+release=1.4U
 
 
 case $1 in
