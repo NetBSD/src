8979ffcddf17b46564a53f314a11dae67a668a48
elric <elric>
2000-03-04 10:27:59 +0000

    Added basic TGA2 support to the TGA driver.  Abstracted the RAMDAC code out into ../ic/bt485.c.  Disabled tga_bt463.c, which we don't currently support.

--- sys/dev/pci/tgavar.h
+++ sys/dev/pci/tgavar.h
@@ -1,4 +1,4 @@
-/* $NetBSD: tgavar.h,v 1.4 1998/08/18 08:23:46 thorpej Exp $ */
+/* $NetBSD: tgavar.h,v 1.5 2000/03/04 10:28:00 elric Exp $ */
 
 /*
  * Copyright (c) 1995, 1996 Carnegie-Mellon University.
@@ -27,6 +27,7 @@
  * rights to redistribute these changes.
  */
 
+#include <dev/ic/ramdac.h>
 #include <dev/pci/tgareg.h>
 #include <dev/rcons/raster.h>
 #include <dev/wscons/wsconsio.h>
@@ -37,39 +38,9 @@ struct fbcmap;
 struct fbcursor;
 struct fbcurpos;
 
-struct tga_ramdac_conf {
-	char	*tgar_name;
-	void	(*tgar_init) __P((struct tga_devconfig *, int));
-	int	(*tgar_intr) __P((void *));
-	int	(*tgar_set_cmap) __P((struct tga_devconfig *,
-		    struct wsdisplay_cmap *));
-	int	(*tgar_get_cmap) __P((struct tga_devconfig *,
-		    struct wsdisplay_cmap *));
-	int	(*tgar_set_cursor) __P((struct tga_devconfig *,
-		    struct wsdisplay_cursor *));
-	int	(*tgar_get_cursor) __P((struct tga_devconfig *,
-		    struct wsdisplay_cursor *));
-	int	(*tgar_set_curpos) __P((struct tga_devconfig *,
-		    struct wsdisplay_curpos *));
-	int	(*tgar_get_curpos) __P((struct tga_devconfig *,
-		    struct wsdisplay_curpos *));
-	int	(*tgar_get_curmax) __P((struct tga_devconfig *,
-		    struct wsdisplay_curpos *));
-
-	/* Only called from the TGA built-in cursor handling code. */
-	int	(*tgar_check_curcmap) __P((struct tga_devconfig *,
-		    struct wsdisplay_cursor *));
-	void	(*tgar_set_curcmap) __P((struct tga_devconfig *,
-		    struct wsdisplay_cursor *));
-	int	(*tgar_get_curcmap) __P((struct tga_devconfig *,
-		    struct wsdisplay_cursor *));
-};
-
 struct tga_conf {
 	char	    *tgac_name;		/* name for this board type */
 
-	const struct tga_ramdac_conf
-		    *tgac_ramdac;	/* the RAMDAC type; see above */
 	int	    tgac_phys_depth;	/* physical frame buffer depth */
 	vsize_t   tgac_cspace_size;	/* core space size */
 	vsize_t   tgac_vvbr_units;	/* what '1' in the VVBR means */
@@ -95,6 +66,11 @@ struct tga_devconfig {
 	int	    dc_tga_type;	/* the device type; see below */
 	const struct tga_conf *dc_tgaconf; /* device buffer configuration */
 
+	struct ramdac_funcs
+		    *dc_ramdac_funcs;	/* The RAMDAC functions */
+	struct ramdac_cookie
+		    *dc_ramdac_cookie;	/* the RAMDAC type; see above */
+
 	vaddr_t dc_vaddr;		/* memory space virtual base address */
 	paddr_t dc_paddr;		/* memory space physical base address */
 
@@ -109,6 +85,8 @@ struct tga_devconfig {
 
 	int	    dc_blanked;		/* currently had video disabled */
 	void	    *dc_ramdac_private; /* RAMDAC private storage */
+
+	void	    (*dc_ramdac_intr) __P((void *));
 };
 	
 struct tga_softc {
@@ -139,9 +117,6 @@ int tga_cnattach __P((bus_space_tag_t, bus_space_tag_t, pci_chipset_tag_t,
 
 int	tga_identify __P((tga_reg_t *));
 const struct tga_conf *tga_getconf __P((int));
-
-extern const struct tga_ramdac_conf tga_ramdac_bt463;
-extern const struct tga_ramdac_conf tga_ramdac_bt485;
 
 int     tga_builtin_set_cursor __P((struct tga_devconfig *,
 	    struct wsdisplay_cursor *));
