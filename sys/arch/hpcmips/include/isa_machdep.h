704b419e4219684b53988d1c0a89da0cb12c935b
takemura <takemura@NetBSD.org>
2000-03-05 04:34:06 +0000

    Add ISA DMA support entries. (It's dummy. I don't know if it work or not.)

--- sys/arch/hpcmips/include/isa_machdep.h
+++ sys/arch/hpcmips/include/isa_machdep.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: isa_machdep.h,v 1.1.1.1 1999/09/16 12:23:22 takemura Exp $	*/
+/*	$NetBSD: isa_machdep.h,v 1.2 2000/03/05 04:34:06 takemura Exp $	*/
 
 /*
  * Copyright (c) 1999, by UCHIYAMA Yasushi
@@ -25,11 +25,19 @@
  * SUCH DAMAGE.
  *
  */
-typedef void *isa_chipset_tag_t;
+#include <dev/isa/isadmavar.h>
+
+struct hpcmips_isa_chipset {
+	struct isa_dma_state ic_dmastate;
+	void *ic_sc;
+};
+typedef struct hpcmips_isa_chipset *isa_chipset_tag_t;
+
 #define VR_ISA_PORT_BASE 0x14000000
 #define VR_ISA_PORT_SIZE 0x4000000
 #define VR_ISA_MEM_BASE 0x10000000
 #define VR_ISA_MEM_SIZE 0x4000000
+
 /*
  * Functions provided to machine-independent ISA code.
  */
@@ -37,3 +45,46 @@ void isa_attach_hook __P((struct device*, struct device*, struct isabus_attach_a
 int isa_intr_alloc __P((isa_chipset_tag_t, int, int, int*));
 void *isa_intr_establish __P((isa_chipset_tag_t, int, int, int, int (*)(void *), void*));
 void isa_intr_disestablish __P((isa_chipset_tag_t, void*));
+
+#define	isa_dmainit(ic, bst, dmat, d)					\
+	_isa_dmainit(&(ic)->ic_dmastate, (bst), (dmat), (d))
+#define	isa_dmacascade(ic, c)						\
+	_isa_dmacascade(&(ic)->ic_dmastate, (c))
+#define	isa_dmamaxsize(ic, c)						\
+	_isa_dmamaxsize(&(ic)->ic_dmastate, (c))
+#define	isa_dmamap_create(ic, c, s, f)					\
+	_isa_dmamap_create(&(ic)->ic_dmastate, (c), (s), (f))
+#define	isa_dmamap_destroy(ic, c)					\
+	_isa_dmamap_destroy(&(ic)->ic_dmastate, (c))
+#define	isa_dmastart(ic, c, a, n, p, f, bf)				\
+	_isa_dmastart(&(ic)->ic_dmastate, (c), (a), (n), (p), (f), (bf))
+#define	isa_dmaabort(ic, c)						\
+	_isa_dmaabort(&(ic)->ic_dmastate, (c))
+#define	isa_dmacount(ic, c)						\
+	_isa_dmacount(&(ic)->ic_dmastate, (c))
+#define	isa_dmafinished(ic, c)						\
+	_isa_dmafinished(&(ic)->ic_dmastate, (c))
+#define	isa_dmadone(ic, c)						\
+	_isa_dmadone(&(ic)->ic_dmastate, (c))
+#define	isa_dmafreeze(ic)						\
+	_isa_dmafreeze(&(ic)->ic_dmastate)
+#define	isa_dmathaw(ic)							\
+	_isa_dmathaw(&(ic)->ic_dmastate)
+#define	isa_dmamem_alloc(ic, c, s, ap, f)				\
+	_isa_dmamem_alloc(&(ic)->ic_dmastate, (c), (s), (ap), (f))
+#define	isa_dmamem_free(ic, c, a, s)					\
+	_isa_dmamem_free(&(ic)->ic_dmastate, (c), (a), (s))
+#define	isa_dmamem_map(ic, c, a, s, kp, f)				\
+	_isa_dmamem_map(&(ic)->ic_dmastate, (c), (a), (s), (kp), (f))
+#define	isa_dmamem_unmap(ic, c, k, s)					\
+	_isa_dmamem_unmap(&(ic)->ic_dmastate, (c), (k), (s))
+#define	isa_dmamem_mmap(ic, c, a, s, o, p, f)				\
+	_isa_dmamem_mmap(&(ic)->ic_dmastate, (c), (a), (s), (o), (p), (f))
+#define	isa_drq_isfree(ic, c)						\
+	_isa_drq_isfree(&(ic)->ic_dmastate, (c))
+#define	isa_malloc(ic, c, s, p, f)					\
+	_isa_malloc(&(ic)->ic_dmastate, (c), (s), (p), (f))
+#define	isa_free(a, p)							\
+	_isa_free((a), (p))
+#define	isa_mappage(m, o, p)						\
+	_isa_mappage((m), (o), (p))
