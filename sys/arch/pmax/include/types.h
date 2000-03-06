0993165c9361a270b9a4928a2d7a49b02f6a76b1
mhitch <mhitch@NetBSD.org>
2000-03-06 03:11:05 +0000

    Define __HAVE_DEVICE_REGISTER, since pmax now has device_register().

--- sys/arch/pmax/include/types.h
+++ sys/arch/pmax/include/types.h
@@ -1,5 +1,7 @@
-/*	$NetBSD: types.h,v 1.15 2000/02/05 00:13:24 cgd Exp $	*/
+/*	$NetBSD: types.h,v 1.16 2000/03/06 03:11:05 mhitch Exp $	*/
 
 #include <mips/types.h>
 
 #define __BROKEN_CONFIG_UNIT_USAGE
+
+#define __HAVE_DEVICE_REGISTER
