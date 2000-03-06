08e4362cd8c57653422b327f9da4f2b953a6962f
augustss <augustss>
2000-03-06 20:58:18 +0000

    Use macros from usb.h instead masking out bit explicitely.

--- sys/dev/usb/if_cue.c
+++ sys/dev/usb/if_cue.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: if_cue.c,v 1.10 2000/03/02 12:37:50 augustss Exp $	*/
+/*	$NetBSD: if_cue.c,v 1.11 2000/03/06 20:58:18 augustss Exp $	*/
 /*
  * Copyright (c) 1997, 1998, 1999, 2000
  *	Bill Paul <wpaul@ee.columbia.edu>.  All rights reserved.
@@ -565,13 +565,13 @@ USB_ATTACH(cue)
 			USB_ATTACH_ERROR_RETURN;
 		}
 		if (UE_GET_DIR(ed->bEndpointAddress) == UE_DIR_IN &&
-		    (ed->bmAttributes & UE_XFERTYPE) == UE_BULK) {
+		    UE_GET_XFERTYPE(ed->bmAttributes) == UE_BULK) {
 			sc->cue_ed[CUE_ENDPT_RX] = ed->bEndpointAddress;
 		} else if (UE_GET_DIR(ed->bEndpointAddress) == UE_DIR_OUT &&
-		    (ed->bmAttributes & UE_XFERTYPE) == UE_BULK) {
+			   UE_GET_XFERTYPE(ed->bmAttributes) == UE_BULK) {
 			sc->cue_ed[CUE_ENDPT_TX] = ed->bEndpointAddress;
 		} else if (UE_GET_DIR(ed->bEndpointAddress) == UE_DIR_IN &&
-		    (ed->bmAttributes & UE_XFERTYPE) == UE_INTERRUPT) {
+			   UE_GET_XFERTYPE(ed->bmAttributes) == UE_INTERRUPT) {
 			sc->cue_ed[CUE_ENDPT_INTR] = ed->bEndpointAddress;
 		}
 	}
