0bdb07d940454dd3058d609c690efb7c74833227
kleink <kleink@NetBSD.org>
2000-03-06 18:32:22 +0000

    Add ISO C99 long long integer general utility interfaces; partially addresses PR standards/9482.

---
+++ lib/libc/stdlib/Lint_llabs.c
@@ -0,0 +1,17 @@
+/*	$NetBSD: Lint_llabs.c,v 1.1 2000/03/06 18:32:28 kleink Exp $	*/
+
+/*
+ * This file placed in the public domain.
+ * Klaus Klein, March 6, 2000.
+ */
+
+#include <stdlib.h>
+
+/* ARGSUSED */ /* LONGLONG */
+long long int
+llabs(j)
+	/* LONGLONG */
+	long long int j;
+{
+	return (0);
+}
