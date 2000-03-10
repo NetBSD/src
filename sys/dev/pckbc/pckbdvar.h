f76ce3b4755f6e995e7bc76389567284bdc7eb25
thorpej <thorpej@NetBSD.org>
2000-03-10 06:10:34 +0000

    Only poll for bell completion if we're calling in from cnbell().  Fixes a problem reported by Lennart Augustsson.

--- sys/dev/pckbc/pckbdvar.h
+++ sys/dev/pckbc/pckbdvar.h
@@ -1,5 +1,5 @@
-/* $NetBSD: pckbdvar.h,v 1.2 2000/03/06 21:40:08 thorpej Exp $ */
+/* $NetBSD: pckbdvar.h,v 1.3 2000/03/10 06:10:35 thorpej Exp $ */
 
 int	pckbd_cnattach __P((pckbc_tag_t, int));
-void	pckbd_hookup_bell __P((void (*fn)(void *, u_int, u_int, u_int),
+void	pckbd_hookup_bell __P((void (*fn)(void *, u_int, u_int, u_int, int),
 	    void *));
