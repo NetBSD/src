33a8ce4a93e1b4f2c0d43d0df6ff0a10d8cbc8b2
itojun <itojun@NetBSD.org>
2000-03-10 14:47:12 +0000

    do not touch radix_node with RNF_ROOT on route_output().  this can cause kernel panic (by non-root invocation of route(8)) on certain routing table setup. KAME PR: 217

--- sys/net/rtsock.c
+++ sys/net/rtsock.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: rtsock.c,v 1.36 2000/03/06 20:49:00 thorpej Exp $	*/
+/*	$NetBSD: rtsock.c,v 1.37 2000/03/10 14:47:13 itojun Exp $	*/
 
 /*
  * Copyright (C) 1995, 1996, 1997, and 1998 WIDE Project.
@@ -203,6 +203,7 @@ route_output(m, va_alist)
 #endif
 {
 	register struct rt_msghdr *rtm = 0;
+	register struct radix_node *rn;
 	register struct rtentry *rt = 0;
 	struct rtentry *saved_nrt = 0;
 	struct radix_node_head *rnh;
@@ -217,8 +218,8 @@ route_output(m, va_alist)
 	so = va_arg(ap, struct socket *);
 	va_end(ap);
 
-
-#define senderr(e) { error = e; goto flush;}
+	bzero(&info, sizeof(info));
+#define senderr(e) do { error = e; goto flush;} while (0)
 	if (m == 0 || ((m->m_len < sizeof(int32_t)) &&
 	   (m = m_pullup(m, sizeof(int32_t))) == 0))
 		return (ENOBUFS);
@@ -293,11 +294,14 @@ route_output(m, va_alist)
 	case RTM_LOCK:
 		if ((rnh = rt_tables[dst->sa_family]) == 0) {
 			senderr(EAFNOSUPPORT);
-		} else if ((rt = (struct rtentry *)
-		    rnh->rnh_lookup(dst, netmask, rnh)) != NULL)
-			rt->rt_refcnt++;
-		else
+		}
+		rn = rnh->rnh_lookup(dst, netmask, rnh);
+		if (rn == NULL || (rn->rn_flags & RNF_ROOT) != 0) {
 			senderr(ESRCH);
+		}
+		rt = (struct rtentry *)rn;
+		rt->rt_refcnt++;
+
 		switch(rtm->rtm_type) {
 
 		case RTM_GET:
@@ -318,7 +322,7 @@ route_output(m, va_alist)
 				} else {
 					ifpaddr = 0;
 					ifaaddr = 0;
-			    }
+				}
 			}
 			(void)rt_msg2(rtm->rtm_type, &info, (caddr_t)0,
 			    (struct walkarg *)0, &len);
