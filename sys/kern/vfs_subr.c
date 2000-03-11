81c3fff63c94460dd437ef48e1a773d4d59b7fb7
perseant <perseant@NetBSD.org>
2000-03-11 05:00:18 +0000

    Move vinvalbuf's check for dirty blocks into ffs_fsync, to ensure that mode and ownership bits are flushed to disk before the vnode is reclaimed.
    The check, introduced in the softdep merge, assumes that if no blocks
    are dirty, no file data *or metadata* needs to be flushed to disk.  This
    is true of ffs, but is not true of lfs, and may not be true of other
    filesystems.
    
    Tested by myself and Bill Squier <groo@cs.stevens-tech.edu>.

--- sys/kern/vfs_subr.c
+++ sys/kern/vfs_subr.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: vfs_subr.c,v 1.118 2000/03/03 05:21:03 mycroft Exp $	*/
+/*	$NetBSD: vfs_subr.c,v 1.119 2000/03/11 05:00:18 perseant Exp $	*/
 
 /*-
  * Copyright (c) 1997, 1998 The NetBSD Foundation, Inc.
@@ -614,15 +614,12 @@ vinvalbuf(vp, flags, cred, p, slpflag, slptimeo)
 			tsleep((caddr_t)&vp->v_numoutput, PRIBIO + 1,
 			    "vbwait", 0);
 		}
-		if (vp->v_dirtyblkhd.lh_first != NULL) {
-			splx(s);
-			if ((error = VOP_FSYNC(vp, cred, FSYNC_WAIT, p)) != 0)
-				return (error);
-			s = splbio();
-			if (vp->v_numoutput > 0 ||
-			    vp->v_dirtyblkhd.lh_first != NULL)
-				panic("vinvalbuf: dirty bufs");
-		}
+		splx(s);
+		if ((error = VOP_FSYNC(vp, cred, FSYNC_WAIT, p)) != 0)
+		        return (error);
+		s = splbio();
+		if (vp->v_numoutput > 0 || vp->v_dirtyblkhd.lh_first != NULL)
+		        panic("vinvalbuf: dirty bufs");
 		splx(s);
 	}
 
