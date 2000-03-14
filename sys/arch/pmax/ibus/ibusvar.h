9a77ffa20d8269ce5dc09ef95eab5de5881b9b23
nisimura <nisimura@NetBSD.org>
2000-03-14 10:12:42 +0000

    Synchronize with the efforts made lately in main trunk, filling gaps.

--- sys/arch/pmax/ibus/ibusvar.h
+++ sys/arch/pmax/ibus/ibusvar.h
@@ -1,7 +1,7 @@
-/* $NetBSD: ibusvar.h,v 1.2.2.4 2000/02/03 09:46:50 nisimura Exp $ */
+/*	$NetBSD: ibusvar.h,v 1.2.2.5 2000/03/14 10:12:42 nisimura Exp $	*/
 
-#ifndef _IBUSVAR_H_
-#define _IBUSVAR_H_ 1
+#ifndef _PMAX_IBUS_IBUSVAR_H_
+#define _PMAX_IBUS_IBUSVAR_H_
 
 #include <machine/bus.h>
 
@@ -9,11 +9,6 @@ struct ibus_attach_args;
 
 struct ibus_softc {
 	struct device	sc_dev;
-	bus_space_tag_t	sc_bst;
-
-	void	(*sc_intr_establish) __P((struct device *, void *,
-					int, int (*)(void *), void *));
-	void	(*sc_intr_disestablish) __P((struct device *, void *));
 };
 
 /*
@@ -25,34 +20,21 @@ struct ibus_dev_attach_args {
 
 	int	ida_ndevs;
 	struct ibus_attach_args	*ida_devs;
-	void	(*ida_establish) __P((struct device *, void *,
-					int, int (*)(void *), void *));
-	void	(*ida_disestablish) __P((struct device *, void *));
 };
 
 /*
  * Arguments used to attach devices to an ibus
  */
 struct ibus_attach_args {
-	char	*ia_name;		/* Device name. */
-	u_int32_t ia_addr;		/* Device address. */
-	void	*ia_cookie;		/* Device cookie. */
+	const char *ia_name;		/* device name */
+	int	ia_cookie;		/* device cookie */
+	u_int32_t ia_addr;		/* device address (KSEG1) */
+	int	ia_basz;		/* badaddr() size */
 };
 
-void ibusattach __P((struct device *, struct device *, void *));
-int  ibusprint __P((void *, const char *));
-void ibus_intr_establish __P((struct device *, void *,
-		int, int (*)(void *), void *));
-void ibus_intr_disestablish __P((struct device *, void *));
+void	ibusattach __P((struct device *, struct device *, void *));
+int	ibusprint __P((void *, const char *));
+void	ibus_intr_establish __P((struct device *, void * cookie, int level,
+	    int (*handler)(void *), void *arg));
 
-void dec_3100_intr_establish __P((struct device *, void *,
-		int, int (*)(void *), void *));
-void dec_3100_intr_disestablish __P((struct device *, void *));
-
-void dec_5100_intr_establish __P((struct device *, void *,
-		int, int (*)(void *), void *));
-void dec_5100_intr_disestablish __P((struct device *, void *));
-
-int  badaddr __P((void *, u_int));
-
-#endif /* _IBUSVAR_H_ */
+#endif	/* !_PMAX_IBUS_IBUSVAR_H_ */
