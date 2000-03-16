6307ed697b52a30a39f7ee0df83081d83d051148
mycroft <mycroft>
2000-03-16 15:09:34 +0000

    Foolish consistency.  Mainly, always use underscores and sys/endian.h.

--- sys/arch/evbsh3/include/endian.h
+++ sys/arch/evbsh3/include/endian.h
@@ -1,13 +1,8 @@
-/*	$NetBSD: endian.h,v 1.1 1999/09/13 10:30:32 itojun Exp $	*/
-#ifndef _EVBSH3_ENDIAN_H_
-#define _EVBSH3_ENDIAN_H_
+/*	$NetBSD: endian.h,v 1.2 2000/03/16 15:09:35 mycroft Exp $	*/
 
 #if 1
-#define _BYTE_ORDER	BIG_ENDIAN
+#define _BYTE_ORDER _BIG_ENDIAN
 #else
-#define _BYTE_ORDER	LITTLE_ENDIAN
+#define _BYTE_ORDER _LITTLE_ENDIAN
 #endif
-
 #include <sh3/endian.h>
-
-#endif /* _EVBSH3_ENDIAN_H_ */
