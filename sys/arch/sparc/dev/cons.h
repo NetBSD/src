ddeb149e551a27fabfc9e1d4339c2e50f974b621
pk <pk@NetBSD.org>
2000-03-19 13:25:10 +0000

    Declare the prom stdio nodes and arguments.

--- sys/arch/sparc/dev/cons.h
+++ sys/arch/sparc/dev/cons.h
@@ -1,17 +1,17 @@
-/*	$NetBSD: cons.h,v 1.3 2000/02/12 12:51:03 pk Exp $	*/
+/*	$NetBSD: cons.h,v 1.4 2000/03/19 13:25:10 pk Exp $	*/
 
 
-struct consdev;
-struct zs_chanstate;
-
-extern void *zs_conschan;
-
-extern void nullcnprobe __P((struct consdev *));
-
-extern int  zs_getc __P((void *arg));
-extern void zs_putc __P((void *arg, int c));
+/*
+ * PROM I/O nodes and arguments are prepared by consinit().
+ * Drivers can examine these when looking for a console device match.
+ */
+extern int prom_stdin_node;
+extern int prom_stdout_node;
+extern char prom_stdin_args[];
+extern char prom_stdout_args[];
 
 #ifdef	KGDB
+struct zs_chanstate;
 void zs_kgdb_init __P((void));
 void zskgdb __P((struct zs_chanstate *));
 #endif
