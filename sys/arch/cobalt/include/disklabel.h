73eaaa60f1c4ce0c92621763a816f768dbae19af
soren <soren@NetBSD.org>
2000-03-22 20:38:05 +0000

    Revert to the same scheme as the other MBR-using ports.

--- sys/arch/cobalt/include/disklabel.h
+++ sys/arch/cobalt/include/disklabel.h
@@ -1,23 +1,17 @@
-/*	$NetBSD: disklabel.h,v 1.1 2000/03/19 23:07:46 soren Exp $	*/
+/*	$NetBSD: disklabel.h,v 1.2 2000/03/22 20:38:05 soren Exp $	*/
 
+#define LABELSECTOR	1		/* sector containing label */
+#define LABELOFFSET	0		/* offset of label in sector */
 #define MAXPARTITIONS	16
 #define RAW_PART	3
 
 /* Pull in MBR partition definitions. */
 #include <sys/disklabel_mbr.h>
  
-#if 0
-struct cpu_disklabel {
-        struct mbr_partition dosparts[NMBRPART];
-};
-#endif
-
-/* XXX */
-
 #ifndef __ASSEMBLER__ 
 #include <sys/dkbad.h>                
 struct cpu_disklabel {
-        struct mbr_partition mbrparts[NMBRPART];
+        struct mbr_partition dosparts[NMBRPART];
         struct dkbad bad;
 };
 #endif
