8354a620f6cf31719a703fdc49c72acd15faa740
soren <soren>
2000-03-24 21:30:58 +0000

    Move sysctl definitions from arch/mips to arch/foo.

--- sys/arch/cobalt/include/cpu.h
+++ sys/arch/cobalt/include/cpu.h
@@ -1,11 +1,19 @@
-/*	$NetBSD: cpu.h,v 1.1 2000/03/19 23:07:46 soren Exp $	*/
+/*	$NetBSD: cpu.h,v 1.2 2000/03/24 21:30:59 soren Exp $	*/
+
+#define MIPS3_INTERNAL_TIMER_INTERRUPT
 
 #include <mips/cpu.h>
 #include <mips/cpuregs.h>
 
-#define MIPS3_INTERNAL_TIMER_INTERRUPT
-#if 0
-#define MIPS_INT_MASK_CLOCK	MIPS_INT_MASK_5
-#endif
-
 #define INT_MASK_REAL_DEV	MIPS_HARD_INT_MASK
+
+/*
+ * CTL_MACHDEP definitions.
+ */
+#define CPU_BOOTSTRING	1		/* Command given to the firmware. */
+#define CPU_MAXID	2
+
+#define CTL_MACHDEP_NAMES { \  
+	{ 0, 0 }, \
+	{ "bootstring", CTLTYPE_STRING }, \
+}
