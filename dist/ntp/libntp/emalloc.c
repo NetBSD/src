b495bdad38776831486c4792c96acc3bd73d3580
simonb <simonb@NetBSD.org>
2000-03-29 12:38:44 +0000

    Import ntp 4.0.99g

---
+++ dist/ntp/libntp/emalloc.c
@@ -0,0 +1,50 @@
+/*	$NetBSD: emalloc.c,v 1.1.1.1 2000/03/29 12:38:49 simonb Exp $	*/
+
+/*
+ * emalloc - return new memory obtained from the system.  Belch if none.
+ */
+#include "ntp_types.h"
+#include "ntp_malloc.h"
+#include "ntp_stdlib.h"
+#include "ntp_syslog.h"
+
+#if defined SYS_WINNT && defined DEBUG
+#include <crtdbg.h>
+#endif
+
+#if defined SYS_WINNT && defined DEBUG
+
+void *
+debug_emalloc(
+	u_int size,
+	char *filename,
+	int line
+	)
+{
+	char *mem;
+
+	if ((mem = (char *)_malloc_dbg(size, _NORMAL_BLOCK, filename, line)) == 0) {
+		msyslog(LOG_ERR, "No more memory!");
+		exit(1);
+	}
+	return mem;
+}
+
+#else
+
+void *
+emalloc(
+	u_int size
+	)
+{
+	char *mem;
+
+	if ((mem = (char *)malloc(size)) == 0) {
+		msyslog(LOG_ERR, "No more memory!");
+		exit(1);
+	}
+	return mem;
+}
+
+
+#endif
