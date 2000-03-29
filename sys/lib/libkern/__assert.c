b3009743c0d3afb87572a8c55ab7376d4cf41d55
simonb <simonb@NetBSD.org>
2000-03-29 03:19:52 +0000

    Don't prototype __assert() if not STANDALONE - <sys/systm.h> has already included <lib/libkern/libkern.h> to get the prototype by this stage.
    XXX: should we just change <sys/systm.h> from
    	#ifdef _KERNEL
    	#include <sys/systm.h>
    	#endif
         to
    	#if defined(_KERNEL) || defined(_STANDALONE)
    	#include <sys/systm.h>
    	#endif
         anyways?

--- sys/lib/libkern/__assert.c
+++ sys/lib/libkern/__assert.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: __assert.c,v 1.4 1999/04/13 19:00:30 drochner Exp $	*/
+/*	$NetBSD: __assert.c,v 1.5 2000/03/29 03:19:52 simonb Exp $	*/
 
 /*
  * Copyright (c) 1996 Christopher G. Demetriou
@@ -36,9 +36,6 @@
 
 #ifdef _STANDALONE
 #include <lib/libkern/libkern.h>
-#else
-void __assert __P((const char *, const char *, int, const char *))
-	__attribute__((__noreturn__)); /* XXX */
 #endif
 
 void
