8b454845ad6fc159f88108df975183f85ddf235c
he <he>
2000-04-02 19:35:52 +0000

    Pull up revision 1.15 (requested by simonb):   Include -r flag in usage message.

--- usr.bin/du/du.c
+++ usr.bin/du/du.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: du.c,v 1.14 1998/02/15 17:08:18 kleink Exp $	*/
+/*	$NetBSD: du.c,v 1.14.2.1 2000/04/02 19:35:52 he Exp $	*/
 
 /*
  * Copyright (c) 1989, 1993, 1994
@@ -46,7 +46,7 @@ __COPYRIGHT("@(#) Copyright (c) 1989, 1993, 1994\n\
 #if 0
 static char sccsid[] = "@(#)du.c	8.5 (Berkeley) 5/4/95";
 #else
-__RCSID("$NetBSD: du.c,v 1.14 1998/02/15 17:08:18 kleink Exp $");
+__RCSID("$NetBSD: du.c,v 1.14.2.1 2000/04/02 19:35:52 he Exp $");
 #endif
 #endif /* not lint */
 
@@ -250,6 +250,6 @@ usage()
 {
 
 	(void)fprintf(stderr,
-		"usage: du [-H | -L | -P] [-a | -s] [-ckx] [file ...]\n");
+		"usage: du [-H | -L | -P] [-a | -s] [-ckrx] [file ...]\n");
 	exit(1);
 }
