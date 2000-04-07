4044b5a41c6d4c88709956851433f7e976b38bd8
thorpej <thorpej@NetBSD.org>
2000-04-07 16:58:53 +0000

    Use separate callouts for motor-on and motor-off.  Fixes a condition where the floppy driver would wedge because a motor-on timeout would be cancelled by another I/O operation cancelling a motor-off timeout.
    From enami tsugutomo <enami@sm.sony.co.jp>.

--- sys/arch/x68k/dev/fd.c
+++ sys/arch/x68k/dev/fd.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: fd.c,v 1.32 2000/03/27 15:49:34 minoura Exp $	*/
+/*	$NetBSD: fd.c,v 1.33 2000/04/07 16:58:56 thorpej Exp $	*/
 
 /*-
  * Copyright (c) 1998 The NetBSD Foundation, Inc.
@@ -225,7 +225,8 @@ struct fd_softc {
 	struct fd_type *sc_deftype;	/* default type descriptor */
 	struct fd_type *sc_type;	/* current type descriptor */
 
-	struct callout sc_motor_ch;
+	struct callout sc_motoron_ch;
+	struct callout sc_motoroff_ch;
 
 	daddr_t	sc_blkno;	/* starting block number */
 	int sc_bcount;		/* byte count left */
@@ -589,7 +590,8 @@ fdattach(parent, self, aux)
 	struct fd_type *type = &fd_types[0];	/* XXX 1.2MB */
 	int drive = fa->fa_drive;
 
-	callout_init(&fd->sc_motor_ch);
+	callout_init(&fd->sc_motoron_ch);
+	callout_init(&fd->sc_motoroff_ch);
 
 	fd->sc_flags = 0;
 
@@ -692,7 +694,7 @@ fdstrategy(bp)
 	/* Queue transfer on drive, activate drive and controller if idle. */
 	s = splbio();
 	disksort_cylinder(&fd->sc_q, bp);
-	callout_stop(&fd->sc_motor_ch);		/* a good idea */
+	callout_stop(&fd->sc_motoroff_ch);		/* a good idea */
 	if (fd->sc_active == 0)
 		fdstart(fd);
 #ifdef DIAGNOSTIC
@@ -761,7 +763,7 @@ fdfinish(fd, bp)
 
 	biodone(bp);
 	/* turn off motor 5s from now */
-	callout_reset(&fd->sc_motor_ch, 5 * hz, fd_motor_off, fd);
+	callout_reset(&fd->sc_motoroff_ch, 5 * hz, fd_motor_off, fd);
 	fdc->sc_state = DEVIDLE;
 }
 
@@ -1109,7 +1111,7 @@ loop:
 		fd->sc_skip = 0;
 		fd->sc_bcount = bp->b_bcount;
 		fd->sc_blkno = bp->b_blkno / (FDC_BSIZE / DEV_BSIZE);
-		callout_stop(&fd->sc_motor_ch);
+		callout_stop(&fd->sc_motoroff_ch);
 		if ((fd->sc_flags & FD_MOTOR_WAIT) != 0) {
 			fdc->sc_state = MOTORWAIT;
 			return 1;
@@ -1120,7 +1122,7 @@ loop:
 			for (i = 0; i < 4; i++) {
 				struct fd_softc *ofd = fdc->sc_fd[i];
 				if (ofd && ofd->sc_flags & FD_MOTOR) {
-					callout_stop(&ofd->sc_motor_ch);
+					callout_stop(&ofd->sc_motoroff_ch);
 					ofd->sc_flags &= ~(FD_MOTOR | FD_MOTOR_WAIT);
 					break;
 				}
@@ -1129,7 +1131,7 @@ loop:
 			fd_set_motor(fdc, 0);
 			fdc->sc_state = MOTORWAIT;
 			/* allow .5s for motor to stabilize */
-			callout_reset(&fd->sc_motor_ch, hz / 2,
+			callout_reset(&fd->sc_motoron_ch, hz / 2,
 			    fd_motor_on, fd);
 			return 1;
 		}
