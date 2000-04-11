3dd5742b63a55d40aa34e5eb9604a46e91a1a1f6
nisimura <nisimura@NetBSD.org>
2000-04-11 02:43:51 +0000

    Change to have cpu_intr() peculiar to DECstations; have ssir global variable and cpu_intr() body in machdep.c.  Reorder and rename model specific interrupt handler arguments.  Fixup machine/intr.h and machine/cpu.h appropriately.

--- sys/arch/pmax/include/cpu.h
+++ sys/arch/pmax/include/cpu.h
@@ -1,17 +1,9 @@
-/*	$NetBSD: cpu.h,v 1.22 2000/03/24 23:06:05 soren Exp $	*/
+/*	$NetBSD: cpu.h,v 1.23 2000/04/11 02:43:51 nisimura Exp $	*/
 
 #ifndef _PMAX_CPU_H_
 #define _PMAX_CPU_H_
 
-/*
- * pmax uses standard mips1 convention, wiring FPU to hard interupt 5.
- */
 #include <mips/cpu.h>
 #include <mips/cpuregs.h>
-
-#define MIPS_INT_MASK_FPU	MIPS_INT_MASK_5
-
-#define	INT_MASK_REAL_DEV	(MIPS_HARD_INT_MASK &~ MIPS_INT_MASK_5)
-#define	INT_MASK_FPU_DEAL	MIPS_INT_MASK_5
 
 #endif	/* !_PMAX_CPU_H_ */
