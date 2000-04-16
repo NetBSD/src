981a0b3b817bde562526e97bfdd41156e9b1f442
itojun <itojun>
2000-04-16 15:00:56 +0000

    better sync with latest kame (cosmetic only).

--- sys/netinet6/nd6.h
+++ sys/netinet6/nd6.h
@@ -1,10 +1,10 @@
-/*	$NetBSD: nd6.h,v 1.10 2000/03/23 07:03:31 thorpej Exp $	*/
-/*	$KAME: nd6.h,v 1.16 2000/02/24 16:34:51 itojun Exp $	*/
+/*	$NetBSD: nd6.h,v 1.11 2000/04/16 15:00:57 itojun Exp $	*/
+/*	$KAME: nd6.h,v 1.19 2000/03/25 07:23:57 sumikawa Exp $	*/
 
 /*
  * Copyright (C) 1995, 1996, 1997, and 1998 WIDE Project.
  * All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
@@ -16,7 +16,7 @@
  * 3. Neither the name of the project nor the names of its contributors
  *    may be used to endorse or promote products derived from this software
  *    without specific prior written permission.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE PROJECT AND CONTRIBUTORS ``AS IS'' AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
