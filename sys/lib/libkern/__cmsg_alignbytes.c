b579e4d6434eef181eac14a7e9e0f192776f9b19
itojun <itojun@NetBSD.org>
2000-04-19 15:16:02 +0000

    change copyright notice from 3-clause TNF -> 4-clause TNF. not sure if which we prefer, but the point is to use exactly the same copyright.  From: matthew green <mrg@eterna.com.au>

--- sys/lib/libkern/__cmsg_alignbytes.c
+++ sys/lib/libkern/__cmsg_alignbytes.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: __cmsg_alignbytes.c,v 1.1 2000/03/02 07:41:51 itojun Exp $	*/
+/*	$NetBSD: __cmsg_alignbytes.c,v 1.2 2000/04/19 15:16:03 itojun Exp $	*/
 
 /*-
  * Copyright (c) 2000 The NetBSD Foundation, Inc.
@@ -15,7 +15,11 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. Neither the name of The NetBSD Foundation nor the names of its
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *        This product includes software developed by the NetBSD
+ *        Foundation, Inc. and its contributors.
+ * 4. Neither the name of The NetBSD Foundation nor the names of its
  *    contributors may be used to endorse or promote products derived
  *    from this software without specific prior written permission.
  *
