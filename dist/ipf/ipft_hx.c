bd61b9ceecd9b31b6e5d2af46412f31c79e2ccdf
veego <veego>
2000-05-03 10:55:27 +0000

    Import IP Filter 3.4.1

--- dist/ipf/ipft_hx.c
+++ dist/ipf/ipft_hx.c
@@ -1,7 +1,7 @@
-/*	$NetBSD: ipft_hx.c,v 1.1.1.1 1999/12/11 22:23:58 veego Exp $	*/
+/*	$NetBSD: ipft_hx.c,v 1.1.1.2 2000/05/03 10:55:47 veego Exp $	*/
 
 /*
- * Copyright (C) 1995-1998 by Darren Reed.
+ * Copyright (C) 1995-2000 by Darren Reed.
  *
  * Redistribution and use in source and binary forms are permitted
  * provided that this notice is preserved and due credit is given
@@ -44,7 +44,7 @@
 
 #if !defined(lint)
 static const char sccsid[] = "@(#)ipft_hx.c	1.1 3/9/96 (C) 1996 Darren Reed";
-static const char rcsid[] = "@(#)Id: ipft_hx.c,v 2.1 1999/08/04 17:30:03 darrenr Exp";
+static const char rcsid[] = "@(#)Id: ipft_hx.c,v 2.2 2000/03/13 22:10:24 darrenr Exp";
 #endif
 
 extern	int	opts;
