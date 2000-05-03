0953b3f63e1b314d338cf7f34811a75011f8d073
veego <veego>
2000-05-03 11:12:03 +0000

    Resolve conflicts.

--- sys/netinet/ipl.h
+++ sys/netinet/ipl.h
@@ -1,18 +1,19 @@
-/*	$NetBSD: ipl.h,v 1.3 2000/02/01 21:29:16 veego Exp $	*/
+/*	$NetBSD: ipl.h,v 1.4 2000/05/03 11:12:14 veego Exp $	*/
 
 /*
- * Copyright (C) 1993-1999 by Darren Reed.
+ * Copyright (C) 1993-2000 by Darren Reed.
  *
  * Redistribution and use in source and binary forms are permitted
  * provided that this notice is preserved and due credit is given
  * to the original author and the contributors.
  *
  * @(#)ipl.h	1.21 6/5/96
+ * Id: ipl.h,v 2.15.2.2 2000/04/30 05:13:54 darrenr Exp
  */
 
 #ifndef	__IPL_H__
 #define	__IPL_H__
 
-#define	IPL_VERSION	"IP Filter: v3.3.8"
+#define	IPL_VERSION	"IP Filter: v3.4.1"
 
 #endif
