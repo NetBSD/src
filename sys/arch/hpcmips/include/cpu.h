484902d2e2cee7d24dffe6ea04946d9beea4535b
soren <soren@NetBSD.org>
2000-05-05 03:18:25 +0000

    MIPS_INT_MASK_CLOCK and INT_MASK_REAL_DEV are deprecated.

--- sys/arch/hpcmips/include/cpu.h
+++ sys/arch/hpcmips/include/cpu.h
@@ -1,20 +1,9 @@
-/*	$NetBSD: cpu.h,v 1.5 2000/04/15 22:05:52 soda Exp $	*/
+/*	$NetBSD: cpu.h,v 1.6 2000/05/05 03:18:26 soren Exp $	*/
 
 #ifndef __HPCMIPS_CPU_H
 #define __HPCMIPS_CPU_H
 
-/*
- *  VR4100: Internal timer causes hard interrupt 5.
- */
-#define MIPS_INT_MASK_CLOCK	MIPS_INT_MASK_5
-
 #include <mips/cpu.h>
-#include <mips/cpuregs.h> /* XXX */
-
-#ifdef ENABLE_MIPS_TX3900
-#define	INT_MASK_REAL_DEV	MIPS_HARD_INT_MASK
-#else
-#define	INT_MASK_REAL_DEV	MIPS3_HARD_INT_MASK	/* XXX */
-#endif
+#include <mips/cpuregs.h>
 
 #endif __HPCMIPS_CPU_H
