dc4401e9b570317ea66520227962521b9b283b2b
sjg <sjg@NetBSD.org>
2000-05-07 01:20:47 +0000

    correct description of KERNSRCDIR and others

--- share/mk/bsd.kernobj.mk
+++ share/mk/bsd.kernobj.mk
@@ -1,17 +1,25 @@
-#	$NetBSD: bsd.kernobj.mk,v 1.3 2000/05/06 07:41:59 sjg Exp $
+#	$NetBSD: bsd.kernobj.mk,v 1.4 2000/05/07 01:20:47 sjg Exp $
 
-#   KERNSRCDIR points to kernel source; it is set by default to ../sys,
-#	but can be overridden.
-#   KERNARCHDIR is the directory under kernel source that holds the md
-#	stuff.  Default is arch/${MACHINE}.
-#   KERNOBJDIR is the kernel build directory, it defaults to
-#	${MAKEOBJDIRPREFIX}${KERNSRCDIR}/${KERNARCHDIR}/compile/KERNELNAME
-#	if ${MAKEOBJDIRPREFIX}${KERNSRCDIR}/${KERNARCHDIR}/compile
-#	exists otherwise it defaults to
-#	${KERNSRCDIR}/${KERNARCHDIR}/compile/KERNELNAME, but can be
-#	overridden.  Or
-#   KERNCONFDIR is where the configuration files for kernels are found;
-#	default is ${KERNSRCDIR}/${KERNARCHDIR}/conf but can be overridden.
+# KERNSRCDIR	Is the location of the top of the kernel src.
+# 		It defaults to ${BSDSRCDIR}/sys, but the top-level
+# 		Makefile.inc sets it to ${ABSTOP}/sys (ABSTOP is the
+# 		absolute path to the directory where the top-level
+# 		Makefile.inc was found.
+# 
+# KERNARCHDIR	Is the location of the machine dependent kernel
+# 		sources.  It defaults to arch/${MACHINE}
+# 		
+# KERNCONFDIR	Is where the configuration files for kernels are
+# 		found; default is ${KERNSRCDIR}/${KERNARCHDIR}/conf.
+# 
+# KERNOBJDIR	Is the kernel build directory.  The kernel GENERIC for
+# 		instance will be compiled in ${KERNOBJDIR}/GENERIC.
+# 		The default value is
+# 		${MAKEOBJDIRPREFIX}${KERNSRCDIR}/${KERNARCHDIR}/compile
+# 		if it exists or the target 'obj' is being made.
+# 		Otherwise the default is
+# 		${KERNSRCDIR}/${KERNARCHDIR}/compile.
+# 
 
 KERNSRCDIR?=	${BSDSRCDIR}/sys
 # just incase ${MACHINE} is not always correct
