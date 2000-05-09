22e6011ebbe17aec88b120591271192522feea33
jhawk <jhawk>
2000-05-09 17:59:39 +0000

    Back out rev 1.7 of i82365_isa.c; 0x300-0x3ff, or even 0x330-0x3ff is not a reasonable place to allocate 12-bit pcmcia iospace. It seems to cause conflicts on a large number of modern laptops, necesitating hand-patching of the kernel (mostly due to video devices in the upper range of that space).
    This will presumably cause problems on the NEC Versa 6030X,
    however that appears to be a vastly less common case than
    the laptops that are inconvenienced by the current state.
    
    12-bit iospace now uses 0x400-0xfff.

--- sys/dev/isa/i82365_isasubr.c
+++ sys/dev/isa/i82365_isasubr.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: i82365_isasubr.c,v 1.20 2000/02/28 05:30:19 enami Exp $	*/
+/*	$NetBSD: i82365_isasubr.c,v 1.21 2000/05/09 17:59:39 jhawk Exp $	*/
 
 #define	PCICISADEBUG
 
@@ -429,11 +429,6 @@ void pcic_isa_bus_width_probe (sc, iot, ioh, base, length)
 	bus_space_free(iot, ioh_high, length);
 
 	/*
-	 * XXX mycroft recommends I/O space range 0x400-0xfff .  I should put
-	 * this in a header somewhere
-	 */
-
-	/*
 	 * XXX some hardware doesn't seem to grok addresses in 0x400 range--
 	 * apparently missing a bit or more of address lines. (e.g.
 	 * CIRRUS_PD672X with Linksys EthernetCard ne2000 clone in TI
@@ -447,22 +442,8 @@ void pcic_isa_bus_width_probe (sc, iot, ioh, base, length)
 		sc->iobase = 0x300;
 		sc->iosize = 0x0ff;
 	} else {
-#if 0
-		/*
-		 * This is what we'd like to use, but...
-		 */
 		sc->iobase = 0x400;
 		sc->iosize = 0xbff;
-#else
-		/*
-		 * ...the above bus width probe doesn't always work.
-		 * So, experimentation has shown the following range
-		 * to not lose on systems that 0x300-0x3ff loses on
-		 * (e.g. the NEC Versa 6030X).
-		 */
-		sc->iobase = 0x330;
-		sc->iosize = 0x0cf;
-#endif
 	}
 
 	DPRINTF(("%s: bus_space_alloc range 0x%04lx-0x%04lx (probed)\n",
