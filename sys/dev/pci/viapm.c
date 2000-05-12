74cfbe4003f76d8d72dc234fa9c38e9ac18bc49c
thorpej <thorpej@NetBSD.org>
2000-05-12 16:42:41 +0000

    Apply indent(1).

--- sys/dev/pci/viapm.c
+++ sys/dev/pci/viapm.c
@@ -1,19 +1,19 @@
-/* $NetBSD: viapm.c,v 1.1 2000/05/08 16:40:43 joda Exp $ */
+/*	$NetBSD: viapm.c,v 1.2 2000/05/12 16:42:42 thorpej Exp $	*/
 
 /*
  * Copyright (c) 2000 Johan Danielsson
  * All rights reserved.
  *
- * Redistribution and use in source and binary forms, with or without 
- * modification, are permitted provided that the following conditions 
- * are met: 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
  *
- * 1. Redistributions of source code must retain the above copyright 
- *    notice, this list of conditions and the following disclaimer. 
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
  *
- * 2. Redistributions in binary form must reproduce the above copyright 
- *    notice, this list of conditions and the following disclaimer in the 
- *    documentation and/or other materials provided with the distribution. 
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
  *
  * 3. Neither the name of author nor the names of any contributors may
  *    be used to endorse or promote products derived from this
@@ -32,10 +32,12 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
-/* frontend for the power management device in the VIA VT82C686a South
-   Bridge. It (the chip) has three functions, power management,
-   hardware monitoring, and an SMBus controller. A driver for the
-   hardware monitoring is provided by the viaenv driver */
+/*
+ * frontend for the power management device in the VIA VT82C686a South
+ * Bridge. It (the chip) has three functions, power management, hardware
+ * monitoring, and an SMBus controller. A driver for the hardware monitoring
+ * is provided by the viaenv driver
+ */
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -49,99 +51,103 @@
 #include <dev/pci/viapmvar.h>
 
 struct viapm_softc {
-    struct device	sc_dev;
+	struct device sc_dev;
 
-    pci_chipset_tag_t	sc_pc;
-    pcitag_t		sc_tag;
-    bus_space_tag_t	sc_iot;
+	pci_chipset_tag_t sc_pc;
+	pcitag_t sc_tag;
+	bus_space_tag_t sc_iot;
 };
 
 static int
-viapm_match(struct device *parent, struct cfdata *match, void *aux)
+viapm_match(struct device * parent, struct cfdata * match, void *aux)
 {
-    struct pci_attach_args *pa = aux;
-    
-    if (PCI_VENDOR(pa->pa_id) == PCI_VENDOR_VIATECH &&
-	PCI_PRODUCT(pa->pa_id) == PCI_PRODUCT_VIATECH_VT82C686A_SMB)
-	return 1;
-    return 0;
+	struct pci_attach_args *pa = aux;
+
+	if (PCI_VENDOR(pa->pa_id) == PCI_VENDOR_VIATECH &&
+	    PCI_PRODUCT(pa->pa_id) == PCI_PRODUCT_VIATECH_VT82C686A_SMB)
+		return 1;
+	return 0;
 }
 
 static const char *
 viapm_device_name(enum vapm_devtype type)
 {
-    switch(type) {
-    case VIAPM_POWER:
-	return "power management";
-    case VIAPM_HWMON:
-	return "hardware monitor";
-    case VIAPM_SMBUS:
-	return "SMBus controller";
-    default:
-	panic("viapm_device_name: unknown type");
-    }
+
+	switch (type) {
+		case VIAPM_POWER:
+		return "power management";
+	case VIAPM_HWMON:
+		return "hardware monitor";
+	case VIAPM_SMBUS:
+		return "SMBus controller";
+	default:
+		panic("viapm_device_name: unknown type");
+	}
 }
 
 static int
 viapm_print(void *aux, const char *pnp)
 {
-    struct viapm_attach_args *vaa = aux;
-    if(pnp)
-	printf("%s at %s", viapm_device_name(vaa->va_type), pnp);
-    return UNCONF;
+	struct viapm_attach_args *vaa = aux;
+
+	if (pnp)
+		printf("%s at %s", viapm_device_name(vaa->va_type), pnp);
+	return UNCONF;
 }
 
 static int
-viapm_submatch(struct device *parent, struct cfdata *cf, void *aux)
+viapm_submatch(struct device * parent, struct cfdata * cf, void *aux)
 {
-    return (*cf->cf_attach->ca_match)(parent, cf, aux);
+
+	return (*cf->cf_attach->ca_match) (parent, cf, aux);
 }
 
 static void
-viapm_config_defer(struct device *self)
+viapm_config_defer(struct device * self)
 {
-    struct viapm_softc *sc = (struct viapm_softc*)self;
-    struct viapm_attach_args vaa;
-  
-    vaa.va_pc = sc->sc_pc;
-    vaa.va_tag = sc->sc_tag;
-    vaa.va_iot = sc->sc_iot;
+	struct viapm_softc *sc = (struct viapm_softc *) self;
+	struct viapm_attach_args vaa;
+
+	vaa.va_pc = sc->sc_pc;
+	vaa.va_tag = sc->sc_tag;
+	vaa.va_iot = sc->sc_iot;
 
 #if 0
-    /* no point in confusing people with these until there are drivers
-       for them */
-    vaa.va_type = VIAPM_POWER;
-    vaa.va_offset = 0x40;
-    config_found_sm(self, &vaa, viapm_print, viapm_submatch);
+	/*
+	 * no point in confusing people with these until there are drivers for
+	 * them
+	 */
+	vaa.va_type = VIAPM_POWER;
+	vaa.va_offset = 0x40;
+	config_found_sm(self, &vaa, viapm_print, viapm_submatch);
 #endif
 
-    vaa.va_type = VIAPM_HWMON;
-    vaa.va_offset = 0x70;
-    config_found_sm(self, &vaa, viapm_print, viapm_submatch);
+	vaa.va_type = VIAPM_HWMON;
+	vaa.va_offset = 0x70;
+	config_found_sm(self, &vaa, viapm_print, viapm_submatch);
 
 #if 0
-    vaa.va_type = VIAPM_SMBUS;
-    vaa.va_offset = 0x93;
-    config_found_sm(self, &vaa, viapm_print, viapm_submatch);
+	vaa.va_type = VIAPM_SMBUS;
+	vaa.va_offset = 0x93;
+	config_found_sm(self, &vaa, viapm_print, viapm_submatch);
 #endif
 }
 
 static void
-viapm_attach(struct device *parent, struct device *self, void *aux)
+viapm_attach(struct device * parent, struct device * self, void *aux)
 {
-    struct viapm_softc *sc = (struct viapm_softc*)self;
-    struct pci_attach_args *pa = aux;
+	struct viapm_softc *sc = (struct viapm_softc *) self;
+	struct pci_attach_args *pa = aux;
 
-    printf("\n");
+	printf("\n");
 
-    sc->sc_pc = pa->pa_pc;
-    sc->sc_tag = pa->pa_tag;
-    sc->sc_iot = pa->pa_iot;
+	sc->sc_pc = pa->pa_pc;
+	sc->sc_tag = pa->pa_tag;
+	sc->sc_iot = pa->pa_iot;
 
-    config_defer(self, viapm_config_defer);
+	config_defer(self, viapm_config_defer);
 }
 
 struct cfattach viapm_ca = {
-    sizeof(struct viapm_softc), viapm_match, viapm_attach
+	sizeof(struct viapm_softc), viapm_match, viapm_attach
 };
-
