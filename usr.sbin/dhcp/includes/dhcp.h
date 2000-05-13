65785c67355439e621cbe2240bf11194e0806e6e
thorpej <thorpej@NetBSD.org>
2000-05-13 04:00:48 +0000

    Define the PXE DHCP option codes.

--- usr.sbin/dhcp/includes/dhcp.h
+++ usr.sbin/dhcp/includes/dhcp.h
@@ -158,6 +158,14 @@ struct dhcp_packet {
 #define DHO_NWIP_SUBOPTIONS		63
 #define DHO_USER_CLASS			77
 #define DHO_DHCP_AGENT_OPTIONS		82
+
+/* Options 93, 94, and 97 are define in the Intel Preboot Execution
+   Environment (PXE) specification, version 2.1, September 20, 1999,
+   page 19. */
+#define DHO_PXE_CLIENT_ARCH_ID		93
+#define	DHO_PXE_CLIENT_NETIF_ID		94
+#define	DHO_PXE_UUID			97
+
 /* The DHO_AUTHENTICATE option is not a standard yet, so I've
    allocated an option out of the "local" option space for it on a
    temporary basis.  Once an option code number is assigned, I will
