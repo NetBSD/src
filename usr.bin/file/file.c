ac08392d733188b4bccfbd8234faf4d563fe514c
christos <christos@NetBSD.org>
2000-05-14 22:53:37 +0000

    Resolve conflicts

--- usr.bin/file/file.c
+++ usr.bin/file/file.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: file.c,v 1.15 1999/11/01 17:39:26 christos Exp $	*/
+/*	$NetBSD: file.c,v 1.16 2000/05/14 22:53:38 christos Exp $	*/
 
 /*
  * file - find type of a file or files - main program.
@@ -27,9 +27,6 @@
  * 4. This notice may not be removed or altered.
  */
 #include "file.h"
-#ifdef __CYGWIN__
-#include <errno.h>
-#endif
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -51,6 +48,9 @@
 #ifdef HAVE_UNISTD_H
 #include <unistd.h>	/* for read() */
 #endif
+#ifdef HAVE_LOCALE_H
+#include <locale.h>
+#endif
 
 #include <netinet/in.h>		/* for byte swapping */
 
@@ -58,17 +58,17 @@
 
 #ifndef	lint
 #if 0
-FILE_RCSID("@(#)Id: file.c,v 1.42 1998/09/12 13:17:52 christos Exp ")
+FILE_RCSID("@(#)Id: file.c,v 1.51 2000/05/14 17:58:36 christos Exp ")
 #else
-__RCSID("$NetBSD: file.c,v 1.15 1999/11/01 17:39:26 christos Exp $");
+__RCSID("$NetBSD: file.c,v 1.16 2000/05/14 22:53:38 christos Exp $");
 #endif
 #endif	/* lint */
 
 
 #ifdef S_IFLNK
-# define USAGE  "Usage: %s [-bcnvzL] [-f namefile] [-m magicfiles] file...\n"
+# define USAGE  "Usage: %s [-bciknvzL] [-f namefile] [-m magicfiles] file...\n"
 #else
-# define USAGE  "Usage: %s [-bcnvz] [-f namefile] [-m magicfiles] file...\n"
+# define USAGE  "Usage: %s [-bciknvz] [-f namefile] [-m magicfiles] file...\n"
 #endif
 
 #ifndef MAGIC
@@ -85,13 +85,17 @@ int 			/* Global command-line options 		*/
 	bflag = 0,	/* brief output format	 		*/
 	zflag = 0,	/* follow (uncompress) compressed files */
 	sflag = 0,	/* read block special files		*/
-	nobuffer = 0;   /* Do not buffer stdout */
+	iflag = 0,
+	nobuffer = 0,   /* Do not buffer stdout */
+	kflag = 0;	/* Keep going after the first match	*/
+
 int			/* Misc globals				*/
 	nmagic = 0;	/* number of valid magic[]s 		*/
 
 struct  magic *magic;	/* array of magic entries		*/
 
 const char *magicfile;	/* where magic be found 		*/
+const char *default_magicfile = MAGIC;
 
 char *progname;		/* used throughout 			*/
 int lineno;		/* line number in the magic file	*/
@@ -115,6 +119,11 @@ main(argc, argv)
 {
 	int c;
 	int check = 0, didsomefiles = 0, errflg = 0, ret = 0, app = 0;
+	char *mime;
+
+#ifdef LC_CTYPE
+	setlocale(LC_CTYPE, ""); /* makes islower etc work for other langs */
+#endif
 
 	if ((progname = strrchr(argv[0], '/')) != NULL)
 		progname++;
@@ -122,24 +131,15 @@ main(argc, argv)
 		progname = argv[0];
 
 	if (!(magicfile = getenv("MAGIC")))
-		magicfile = MAGIC;
+		magicfile = default_magicfile;
 
-	while ((c = getopt(argc, argv, "bcdnf:m:svzL")) != EOF)
+	while ((c = getopt(argc, argv, "bcdf:ikm:nsvzL")) != EOF)
 		switch (c) {
-		case 'v':
-			(void) fprintf(stdout, "%s-%d.%d\n", progname,
-				       FILE_VERSION_MAJOR, patchlevel);
-			(void) fprintf(stdout, "magic file from %s\n",
-				       magicfile);
-			return 1;
 		case 'b':
 			++bflag;
 			break;
 		case 'c':
 			++check;
-			break;
-		case 'n':
-			++nobuffer;
 			break;
 		case 'd':
 			++debug;
@@ -154,20 +154,40 @@ main(argc, argv)
 			unwrap(optarg);
 			++didsomefiles;
 			break;
+		case 'i':
+			iflag++;
+			if ((mime = malloc(strlen(magicfile) + 5)) != NULL) {
+				(void)strcpy(mime, magicfile);
+				(void)strcat(mime, ".mime");
+				magicfile = mime;
+			}
+			break;
+		case 'k':
+			kflag = 1;
+			break;
+		case 'm':
+			magicfile = optarg;
+			break;
+		case 'n':
+			++nobuffer;
+			break;
+		case 's':
+			sflag++;
+			break;
+		case 'v':
+			(void) fprintf(stdout, "%s-%d.%d\n", progname,
+				       FILE_VERSION_MAJOR, patchlevel);
+			(void) fprintf(stdout, "magic file from %s\n",
+				       magicfile);
+			return 1;
+		case 'z':
+			zflag++;
+			break;
 #ifdef S_IFLNK
 		case 'L':
 			++lflag;
 			break;
 #endif
-		case 'm':
-			magicfile = optarg;
-			break;
-		case 'z':
-			zflag++;
-			break;
-		case 's':
-			sflag++;
-			break;
 		case '?':
 		default:
 			errflg++;
@@ -369,7 +389,7 @@ int wid;
 	}
 
 	if (nbytes == 0)
-		ckfputs("empty", stdout);
+		ckfputs(iflag ? "application/x-empty" : "empty", stdout);
 	else {
 		buf[nbytes++] = '\0';	/* null-terminate it */
 		match = tryit(buf, nbytes, zflag);
--- usr.bin/file/softmagic.c
+++ usr.bin/file/softmagic.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: softmagic.c,v 1.17 1999/11/01 17:39:27 christos Exp $	*/
+/*	$NetBSD: softmagic.c,v 1.18 2000/05/14 22:53:38 christos Exp $	*/
 
 /*
  * softmagic - interpret variable magic from MAGIC
@@ -29,6 +29,7 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 #include <stdlib.h>
 #include <time.h>
 #include <sys/types.h>
@@ -38,9 +39,9 @@
 #include <sys/cdefs.h>
 #ifndef	lint
 #if 0
-FILE_RCSID("@(#)Id: softmagic.c,v 1.38 1998/09/12 13:21:01 christos Exp ")
+FILE_RCSID("@(#)Id: softmagic.c,v 1.41 2000/05/14 17:58:36 christos Exp ")
 #else
-__RCSID("$NetBSD: softmagic.c,v 1.17 1999/11/01 17:39:27 christos Exp $");
+__RCSID("$NetBSD: softmagic.c,v 1.18 2000/05/14 22:53:38 christos Exp $");
 #endif
 #endif	/* lint */
 
@@ -108,6 +109,9 @@ int nbytes;
 	static int32 *tmpoff = NULL;
 	static size_t tmplen = 0;
 	int32 oldoff = 0;
+	int returnval = 0; /* if a match is found it is set to 1*/
+	extern int kflag;
+	int firstline = 1; /* a flag to print X\n  X\n- X */
 
 	if (tmpoff == NULL)
 		if ((tmpoff = (int32 *) malloc(tmplen = 20)) == NULL)
@@ -125,6 +129,11 @@ int nbytes;
 			    	   magic[magindex + 1].cont_level != 0)
 			    	   magindex++;
 			    continue;
+		}
+
+		if (! firstline) { /* we found another match */
+			/* put a newline and '-' to do some simple formatting*/
+			printf("\n- ");
 		}
 
 		tmpoff[cont_level] = mprint(&p, &magic[magindex]);
@@ -190,9 +199,13 @@ int nbytes;
 				}
 			}
 		}
-		return 1;		/* all through */
+		firstline = 0;
+		returnval = 1;
+		if (!kflag) {
+			return 1; /* don't keep searching */
+		}			
 	}
-	return 0;			/* no match at all */
+	return returnval;  /* This is hit if -k is set or there is no match */
 }
 
 static int32
@@ -435,30 +448,58 @@ struct magic *m;
 		v = p->l;
 		break;
 
-	case STRING:
-		l = 0;
-		/* What we want here is:
+	case STRING: {
+		/*
+		 * What we want here is:
 		 * v = strncmp(m->value.s, p->s, m->vallen);
 		 * but ignoring any nulls.  bcmp doesn't give -/+/0
 		 * and isn't universally available anyway.
 		 */
+		register unsigned char *a = (unsigned char*)m->value.s;
+		register unsigned char *b = (unsigned char*)p->s;
+		register int len = m->vallen;
+		l = 0;
 		v = 0;
-		{
-			register unsigned char *a = (unsigned char*)m->value.s;
-			register unsigned char *b = (unsigned char*)p->s;
-			register int len = m->vallen;
-
+		if (0L == m->mask) { /* normal string: do it fast */
 			while (--len >= 0)
 				if ((v = *b++ - *a++) != '\0')
-					break;
+					break; 
+		} else { /* combine the others */
+			while (--len >= 0) {
+				if ((m->mask & STRING_IGNORE_LOWERCASE) &&
+				    islower(*a)) {
+					if ((v = tolower(*b++) - *a++) != '\0')
+						break;
+				} else if ((m->mask & STRING_COMPACT_BLANK) && 
+				    isspace(*a)) { 
+					a++;
+					if (isspace(*b++)) {
+						while (isspace(*b))
+							b++;
+					} else {
+						v = 1;
+						break;
+					}
+				} else if (isspace(*a) &&
+				    (m->mask & STRING_COMPACT_OPTIONAL_BLANK)) {
+					a++;
+					while (isspace(*b))
+						b++;
+				} else {
+					if ((v = *b++ - *a++) != '\0')
+						break;
+				}
+			}
 		}
 		break;
+	}
 	default:
 		error("invalid type %d in mcheck().\n", m->type);
 		return 0;/*NOTREACHED*/
 	}
 
-	v = signextend(m, v) & m->mask;
+	if(m->type != STRING)
+		v = signextend(m, v) & m->mask;
 
 	switch (m->reln) {
 	case 'x':
