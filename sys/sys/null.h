50c5231bb4d22ded312c89d3640fddb0bc9a787b
thorpej <thorpej@NetBSD.org>
2000-05-19 04:37:20 +0000

    If not GNU C++, if __AUDIT__ is defined, define NULL as (void *)0.

--- sys/sys/null.h
+++ sys/sys/null.h
@@ -1,8 +1,12 @@
-/*	$NetBSD: null.h,v 1.1 2000/01/10 16:58:39 kleink Exp $	*/
+/*	$NetBSD: null.h,v 1.2 2000/05/19 04:37:20 thorpej Exp $	*/
 
 #ifndef	NULL
 #if !defined(__GNUG__) || __GNUG__ < 2 || (__GNUG__ == 2 && __GNUC_MINOR__ < 90)
+#ifdef __AUDIT__
+#define	NULL	(void *)0
+#else
 #define	NULL	0
+#endif
 #else
 #define	NULL	__null
 #endif
