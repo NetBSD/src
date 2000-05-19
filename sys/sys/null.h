851ae24347e8a162f10b102c2ebf957bab08711d
thorpej <thorpej@NetBSD.org>
2000-05-19 05:04:32 +0000

    Comment previous, suggested by Chris Demetriou.

--- sys/sys/null.h
+++ sys/sys/null.h
@@ -1,8 +1,12 @@
-/*	$NetBSD: null.h,v 1.2 2000/05/19 04:37:20 thorpej Exp $	*/
+/*	$NetBSD: null.h,v 1.3 2000/05/19 05:04:32 thorpej Exp $	*/
 
 #ifndef	NULL
 #if !defined(__GNUG__) || __GNUG__ < 2 || (__GNUG__ == 2 && __GNUC_MINOR__ < 90)
 #ifdef __AUDIT__
+/*
+ * This will cause GCC to emit a warning if you attempt to use NULL
+ * with some non-pointer object, return value, etc.
+ */
 #define	NULL	(void *)0
 #else
 #define	NULL	0
