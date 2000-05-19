e2ed1ac34008962785d5f198a33702b9a2a28f46
thorpej <thorpej@NetBSD.org>
2000-05-19 18:57:48 +0000

    Handle non-GNU C++ as well.  Pointed out by Klaus Klein.

--- sys/sys/null.h
+++ sys/sys/null.h
@@ -1,8 +1,8 @@
-/*	$NetBSD: null.h,v 1.3 2000/05/19 05:04:32 thorpej Exp $	*/
+/*	$NetBSD: null.h,v 1.4 2000/05/19 18:57:48 thorpej Exp $	*/
 
 #ifndef	NULL
 #if !defined(__GNUG__) || __GNUG__ < 2 || (__GNUG__ == 2 && __GNUC_MINOR__ < 90)
-#ifdef __AUDIT__
+#if defined(__AUDIT__) && !defined(__cplusplus)
 /*
  * This will cause GCC to emit a warning if you attempt to use NULL
  * with some non-pointer object, return value, etc.
@@ -10,7 +10,7 @@
 #define	NULL	(void *)0
 #else
 #define	NULL	0
-#endif
+#endif /* __AUDIT__ && !__cplusplus */
 #else
 #define	NULL	__null
 #endif
