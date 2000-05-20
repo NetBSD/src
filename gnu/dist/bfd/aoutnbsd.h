42443ff902f26f13381b66184d243e48c1ff62d5
kristerw <kristerw>
2000-05-20 17:56:54 +0000

    Moved inclusion of netbsd.h and the _get_section_contents #define to aoutnbsd.h

--- gnu/dist/bfd/aoutnbsd.h
+++ gnu/dist/bfd/aoutnbsd.h
@@ -19,6 +19,9 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
 
+#define MY_get_section_contents _bfd_generic_get_section_contents
+#include "netbsd.h"
+
 /*
 SECTION
 	a.out backends
