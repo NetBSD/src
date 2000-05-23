28a6373e0e79d426183e00ebf6aed4b583a227c1
tsubai <tsubai@NetBSD.org>
2000-05-23 13:27:16 +0000

    Remove __BROKEN_DK_ESTABLISH.

--- sys/arch/macppc/include/types.h
+++ sys/arch/macppc/include/types.h
@@ -1,6 +1,5 @@
-/*	$NetBSD: types.h,v 1.4 2000/05/16 05:45:48 thorpej Exp $	*/
+/*	$NetBSD: types.h,v 1.5 2000/05/23 13:27:16 tsubai Exp $	*/
 
 #include <powerpc/types.h>
 
 #define __HAVE_DEVICE_REGISTER
-#define	__BROKEN_DK_ESTABLISH
