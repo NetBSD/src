e4f333c9d9b0ed3fd722a6822323c8df18e69560
is <is@NetBSD.org>
2000-05-25 22:11:57 +0000

    Initial AmigaPPC commit.
    Lots of the work was done by Adam Ciarcinsky.
    
    Currently, this only supports CyberPPC boards by Phase 5. Blizzard PPC
    expected later.
    
    The kernel is useless but for demonstrating that it starts... especially
    interupts, and most of MMU support, is not in yet. Builtin console works,
    however, and you can look at the kernel startup messages.

---
+++ sys/arch/amigappc/include/fpu.h
@@ -0,0 +1,3 @@
+/*	$NetBSD: fpu.h,v 1.1 2000/05/25 22:12:00 is Exp $ */
+
+#include <powerpc/fpu.h>
---
+++ sys/arch/amigappc/include/stdarg.h
@@ -0,0 +1,3 @@
+/*	$NetBSD: stdarg.h,v 1.1 2000/05/25 22:12:01 is Exp $ */
+
+#include <powerpc/stdarg.h>
