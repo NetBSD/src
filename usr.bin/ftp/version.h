d9f81a6a16fb02926e73d13b4855c6aacb0958ec
lukem <lukem@NetBSD.org>
2000-05-28 07:53:30 +0000

    Change `ls' to use the `LIST' and not `NLST' FTP protocol command. Now that after many years on not caring we find certain popular ftp servers are starting to obey RFC959 to the letter of the law and will only return a list of filenames (not directories or other filetypes) in the output of `NLST', then `LIST' is more useful in this case. (Note that the aforementioned pedanticness means that filename completion isn't as useful as it could be...) Fixes [bin/8937] by David A. Gatwood <dgatwood@deepspace.mklinux.org>

--- usr.bin/ftp/version.h
+++ usr.bin/ftp/version.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: version.h,v 1.8 2000/05/01 09:44:55 lukem Exp $	*/
+/*	$NetBSD: version.h,v 1.9 2000/05/28 07:53:31 lukem Exp $	*/
 /*-
  * Copyright (c) 1999, 2000 The NetBSD Foundation, Inc.
  * All rights reserved.
@@ -40,5 +40,5 @@
 #endif
 
 #ifndef FTP_VERSION
-#define	FTP_VERSION	"20000501"
+#define	FTP_VERSION	"20000528"
 #endif
