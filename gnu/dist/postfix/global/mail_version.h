4bc2b8027f400c183db16e41b626cfd85014a987
perry <perry@NetBSD.org>
2000-05-31 15:18:05 +0000

    import patchlevel 8. bugfixes, plus adds the ability to filter on regular expressions applied to message bodies.

--- gnu/dist/postfix/global/mail_version.h
+++ gnu/dist/postfix/global/mail_version.h
@@ -15,7 +15,7 @@
   * Version of this program.
   */
 #define VAR_MAIL_VERSION	"mail_version"
-#define DEF_MAIL_VERSION	"Postfix-19991231-pl07"
+#define DEF_MAIL_VERSION	"Postfix-19991231-pl08"
 extern char *var_mail_version;
 
 /* LICENSE
