1f335bacf94b7ff2259b3fd57842baef56177ed1
tsubai <tsubai@NetBSD.org>
2000-06-04 09:30:43 +0000

    Add PT_GETFPREGS and PT_SETFPREGS support.

--- sys/arch/powerpc/include/reg.h
+++ sys/arch/powerpc/include/reg.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: reg.h,v 1.3 1999/05/03 10:02:19 tsubai Exp $	*/
+/*	$NetBSD: reg.h,v 1.4 2000/06/04 09:30:44 tsubai Exp $	*/
 
 struct reg {
 	register_t fixreg[32];
@@ -7,4 +7,9 @@ struct reg {
 	int xer;
 	register_t ctr;
 	register_t pc;
+};
+
+struct fpreg {
+	double fpreg[32];
+	double fpscr;
 };
