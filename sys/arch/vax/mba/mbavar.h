60ae4e3121cd9fd7f0d466517dd70184a953b8c2
ragge <ragge@NetBSD.org>
2000-06-04 18:04:38 +0000

    Remove home-grown interrupt vector handler. Bus_space'ify; needed to get RH11 working. Still much more to do, though.

--- sys/arch/vax/mba/mbavar.h
+++ sys/arch/vax/mba/mbavar.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: mbavar.h,v 1.6 2000/06/04 06:16:55 matt Exp $ */
+/*	$NetBSD: mbavar.h,v 1.7 2000/06/04 18:04:39 ragge Exp $ */
 /*
  * Copyright (c) 1994 Ludd, University of Lule}, Sweden
  * All rights reserved.
@@ -78,10 +78,12 @@ enum	xfer_action {
  * Info passed do unit device driver during autoconfig.
  */
 struct	mba_attach_args {
-	int	unit;
-        int	type;
-	char	*name;
-	enum	mb_devices devtyp;
+	int	ma_unit;
+        int	ma_type;
+	char	*ma_name;
+	enum	mb_devices ma_devtyp;
+	bus_space_tag_t ma_iot;
+	bus_space_handle_t ma_ioh;
 };
 
 /*
@@ -91,11 +93,11 @@ struct	mba_attach_args {
 struct	mba_device {
 	struct	mba_device *md_back;	/* linked list of runnable devices */
 	    /* Start routine to be called by mbastart. */
-	void	(*md_start) __P((struct mba_device *));
+	void	(*md_start)(struct mba_device *);
 	    /* Routine to be called after attn intr */
-	int	(*md_attn)__P((struct mba_device *));
+	int	(*md_attn)(struct mba_device *);
 	    /* Call after xfer finish */
-	enum	xfer_action (*md_finish) __P((struct mba_device *, int, int *));
+	enum	xfer_action (*md_finish)(struct mba_device *, int, int *);
 	void	*md_softc;	/* Backpointer to this units softc. */
 	struct	mba_softc *md_mba;
 	struct	buf_queue md_q;	/* queue of I/O requests */
@@ -103,12 +105,11 @@ struct	mba_device {
 
 struct	mba_softc {
 	struct  device sc_dev;
+	bus_space_tag_t sc_iot;
+	bus_space_handle_t sc_ioh;
 	struct	evcnt sc_intrcnt;
-	struct  ivec_dsp sc_dsp;	/* Interrupt catch routine */
-	struct  mba_regs *sc_mbareg;
 	struct	mba_device *sc_first, *sc_last;
 	enum    sc_state sc_state;
-	int	sc_physnr;		/* Physical number of this mba */
 	struct	mba_device *sc_md[MAXMBADEV];
 };
 
@@ -119,5 +120,5 @@ struct  mbaunit {
 };
 
 /* Common prototypes */
-void	mbaqueue __P((struct mba_device *));
+void	mbaqueue(struct mba_device *);
 
