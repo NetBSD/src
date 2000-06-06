d8e5d1fa7d81d2b462edc2585e324582aed4be83
soren <soren@NetBSD.org>
2000-06-06 02:24:00 +0000

    Add rnd(4) glue for the MIPS3 cycle counter.

---
+++ sys/arch/arc/include/rnd.h
@@ -0,0 +1,3 @@
+/*	$NetBSD: rnd.h,v 1.1 2000/06/06 02:24:00 soren Exp $	*/
+
+#include <mips/rnd.h>
---
+++ sys/arch/pmax/include/rnd.h
@@ -0,0 +1,3 @@
+/*	$NetBSD: rnd.h,v 1.1 2000/06/06 02:24:02 soren Exp $	*/
+
+#include <mips/rnd.h>
