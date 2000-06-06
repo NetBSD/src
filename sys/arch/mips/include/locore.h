0318266282daf0c620e201d7405357d8825876f1
soren <soren@NetBSD.org>
2000-06-06 17:41:07 +0000

    Rename RM5200 cache ops to mips3_*_2way in anticipation of using them for other CPUs with 2-way set associative L1 caches as well.

--- sys/arch/mips/include/locore.h
+++ sys/arch/mips/include/locore.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: locore.h,v 1.33 2000/05/23 04:21:40 soren Exp $	*/
+/*	$NetBSD: locore.h,v 1.34 2000/06/06 17:41:07 soren Exp $	*/
 
 /*
  * Copyright 1996 The Board of Trustees of The Leland Stanford
@@ -56,7 +56,7 @@ void mips1_TBIA __P((int));
 void mips1_TBIAP __P((int));
 void mips1_TBIS __P((vaddr_t));
 void mips1_TBRPL __P((vaddr_t, vaddr_t, paddr_t));
-int mips1_TLBUpdate __P((u_int, /*pt_entry_t*/ u_int));
+int mips1_TLBUpdate __P((u_int, u_int));
 
 void mips1_wbflush __P((void));
 void mips1_proc_trampoline __P((void));
@@ -73,23 +73,18 @@ void mips3_TBIA __P((int));
 void mips3_TBIAP __P((int));
 void mips3_TBIS __P((vaddr_t));
 void mips3_TBRPL __P((vaddr_t, vaddr_t, paddr_t));
-int mips3_TLBUpdate __P((u_int, /*pt_entry_t*/ u_int));
+int mips3_TLBUpdate __P((u_int, u_int));
 struct tlb;
 void mips3_TLBRead __P((int, struct tlb *));
-#if 0
-void mips3_TLBWriteIndexedVPS __P((u_int index, struct tlb *tlb));
-void mips3_TLBWriteIndexed __P((u_int index, u_int high,
-				u_int lo0, u_int lo1));
-#endif
 void mips3_SetWIRED __P((int));
 void mips3_wbflush __P((void));
 void mips3_proc_trampoline __P((void));
 void mips3_cpu_switch_resume __P((void));
 
-void mips5200_FlushCache  __P((void));
-void mips5200_FlushDCache __P((vaddr_t addr, vaddr_t len));
-void mips5200_HitFlushDCache __P((vaddr_t, int));
-void mips5200_FlushICache __P((vaddr_t addr, vaddr_t len));
+void mips3_FlushCache_2way  __P((void));
+void mips3_FlushDCache_2way __P((vaddr_t addr, vaddr_t len));
+void mips3_HitFlushDCache_2way __P((vaddr_t, int));
+void mips3_FlushICache_2way __P((vaddr_t addr, vaddr_t len));
 
 u_int32_t mips3_cycle_count __P((void));
 u_int32_t mips3_write_count __P((u_int32_t));
@@ -136,11 +131,11 @@ extern mips_locore_jumpvec_t r4000_locore_vec;
 extern long *mips_locoresw[];
 
 #if defined(MIPS3) && !defined (MIPS1)
-#if	defined(MIPS3_L2CACHE_ABSENT) && defined(MIPS3_5200)
-#define MachFlushCache		mips5200_FlushCache
-#define MachFlushDCache		mips5200_FlushDCache
-#define MachHitFlushDCache	mips5200_HitFlushDCache
-#define MachFlushICache		mips5200_FlushICache
+#if	defined(MIPS3_5200)
+#define MachFlushCache		mips3_FlushCache_2way
+#define MachFlushDCache		mips3_FlushDCache_2way
+#define MachHitFlushDCache	mips3_HitFlushDCache_2way
+#define MachFlushICache		mips3_FlushICache_2way
 #else
 #define MachFlushCache		mips3_FlushCache
 #if	defined(MIPS3_L2CACHE_ABSENT) && defined(MIPS3_4100)
