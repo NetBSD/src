b124cdf163b4440548c73c297deba9c226ef5a5a
mycroft <mycroft@NetBSD.org>
2000-06-06 09:53:29 +0000

    Abuse .USE some more.

--- share/mk/bsd.kmod.mk
+++ share/mk/bsd.kmod.mk
@@ -1,4 +1,4 @@
-#	$NetBSD: bsd.kmod.mk,v 1.37 2000/06/06 09:22:01 mycroft Exp $
+#	$NetBSD: bsd.kmod.mk,v 1.38 2000/06/06 09:53:30 mycroft Exp $
 
 .if !target(__initialized__)
 __initialized__:
@@ -65,12 +65,14 @@ kmodinstall:: ${DESTDIR}${KMODDIR}/${PROG}
 .PHONY: ${DESTDIR}${KMODDIR}/${PROG}
 .endif
 
+__kmodinstall: .USE
+	${INSTALL} ${RENAME} ${PRESERVE} ${COPY} ${INSTPRIV} -o ${KMODOWN} \
+	    -g ${KMODGRP} -m ${KMODMODE} ${.ALLSRC} ${.TARGET}
+
 .if !defined(BUILD) && !make(all) && !make(${PROG})
 ${DESTDIR}${KMODDIR}/${PROG}: .MADE
 .endif
-${DESTDIR}${KMODDIR}/${PROG}: ${PROG}
-	${INSTALL} ${RENAME} ${PRESERVE} ${COPY} ${INSTPRIV} -o ${KMODOWN} \
-	    -g ${KMODGRP} -m ${KMODMODE} ${.ALLSRC} ${.TARGET}
+${DESTDIR}${KMODDIR}/${PROG}: ${PROG} __kmodinstall
 .endif
 
 lint: ${LOBJS}
