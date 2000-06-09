ce1bea8493fe462274abb99a5be00c7fdbe211be
kleink <kleink>
2000-06-09 14:05:47 +0000

    Need <sys/systm.h> for declaration, and make argument declarations match.

--- sys/arch/powerpc/powerpc/subyte.c
+++ sys/arch/powerpc/powerpc/subyte.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: subyte.c,v 1.1 1996/09/30 16:34:54 ws Exp $	*/
+/*	$NetBSD: subyte.c,v 1.2 2000/06/09 14:05:47 kleink Exp $	*/
 
 /*-
  * Copyright (C) 1993 Wolfgang Solfrank.
@@ -31,13 +31,16 @@
  * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+#include <sys/systm.h>
+
 /*
  * Emulate subyte.
  */
+
 int
 subyte(addr,c)
-char *addr;
-unsigned char c;
+	void *addr;
+	unsigned char c;
 {
 	if (copyout(&c,addr,sizeof(c)))
 		return -1;
