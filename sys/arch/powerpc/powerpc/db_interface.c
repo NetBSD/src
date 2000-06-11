806d7036af05de3db77970ca1ce65e9282fd80be
tsubai <tsubai@NetBSD.org>
2000-06-11 10:02:15 +0000

    Make this compile again.

--- sys/arch/powerpc/powerpc/db_interface.c
+++ sys/arch/powerpc/powerpc/db_interface.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: db_interface.c,v 1.6 2000/06/09 14:09:45 kleink Exp $ */
+/*	$NetBSD: db_interface.c,v 1.7 2000/06/11 10:02:15 tsubai Exp $ */
 /*	$OpenBSD: db_interface.c,v 1.2 1996/12/28 06:21:50 rahnds Exp $	*/
 
 #include "opt_ddb.h"
@@ -7,15 +7,15 @@
 #include <sys/proc.h>
 #include <sys/systm.h>
 
+#include <machine/db_machdep.h>
+#include <machine/frame.h>
+
 #include <ddb/db_sym.h>
 #include <ddb/db_command.h>
 #include <ddb/db_extern.h>
 #include <ddb/db_access.h>
 #include <ddb/db_output.h>
 #include <ddb/ddbvar.h>
-
-#include <machine/frame.h>
-#include <machine/db_machdep.h>
 
 void
 cpu_Debugger()
