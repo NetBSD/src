cb0489b10b31649554b2172e55b5784c6b59bccf
veego <veego@NetBSD.org>
2000-06-12 13:55:07 +0000

    Bump to 1.4ZD after IP Filter was updated to 3.4.6.

--- sys/conf/osrelease.sh
+++ sys/conf/osrelease.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-#	$NetBSD: osrelease.sh,v 1.45 2000/06/11 07:40:45 assar Exp $
+#	$NetBSD: osrelease.sh,v 1.46 2000/06/12 13:55:12 veego Exp $
 #
 # Copyright (c) 1997 The NetBSD Foundation, Inc.
 # All rights reserved.
@@ -39,7 +39,7 @@
 
 # Release number to use
 # Also check __NetBSD_Version__ in sys/sys/param.h if you change this!
-release=1.4ZC
+release=1.4ZD
 
 
 case $1 in
