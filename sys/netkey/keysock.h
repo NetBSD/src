0f5cac7c0447a29ee593edfc2f755c2a05e60dc7
itojun <itojun@NetBSD.org>
2000-06-12 10:40:37 +0000

    sync with almost-latest KAME IPsec.  full changelog would be too big to mention here.  notable changes are like below.
    kernel:
    - make PF_KEY kernel interface more robust against broken input stream.
      it includes complete internal structure change in sys/netkey/key.c.
    - remove non-RFC compliant change in PF_KEY API, in particular,
      in struct sadb_msg.  we cannot just change these standard structs.
      sadb_x_sa2 is introduced instead.
    - remove prototypes for pfkey_xx functions from /usr/include/net/pfkeyv2.h.
      these functions are not supplied in /usr/lib.
    
    setkey(8):
    - get/delete does not require "-m mode" (ignored with warning, if you
      specify it)
    - spddelete takes direction specification

--- sys/netkey/keysock.h
+++ sys/netkey/keysock.h
@@ -1,9 +1,10 @@
-/*	$NetBSD: keysock.h,v 1.5 2000/01/31 14:19:13 itojun Exp $	*/
+/*	$NetBSD: keysock.h,v 1.6 2000/06/12 10:40:48 itojun Exp $	*/
+/*	$KAME: keysock.h,v 1.8 2000/03/27 05:11:06 sumikawa Exp $	*/
 
 /*
  * Copyright (C) 1995, 1996, 1997, and 1998 WIDE Project.
  * All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
@@ -15,7 +16,7 @@
  * 3. Neither the name of the project nor the names of its contributors
  *    may be used to endorse or promote products derived from this software
  *    without specific prior written permission.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE PROJECT AND CONTRIBUTORS ``AS IS'' AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -28,8 +29,6 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  */
-
-/* KAME Id: keysock.h,v 1.5 2000/01/29 06:21:03 itojun Exp */
 
 #ifndef _NETKEY_KEYSOCK_H_
 #define _NETKEY_KEYSOCK_H_
