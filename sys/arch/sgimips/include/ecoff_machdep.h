e8f17b8ee41c61e434fcb81b25ab28fb34d9a833
soren <soren@NetBSD.org>
2000-06-14 15:39:55 +0000

    Initial commit.

---
+++ sys/arch/sgimips/include/ecoff_machdep.h
@@ -0,0 +1,3 @@
+/*	$NetBSD: ecoff_machdep.h,v 1.1 2000/06/14 15:39:57 soren Exp $	*/
+
+#include <mips/ecoff_machdep.h>
---
+++ sys/arch/sgimips/include/endian_machdep.h
@@ -0,0 +1,4 @@
+/*	$NetBSD: endian_machdep.h,v 1.1 2000/06/14 15:39:57 soren Exp $	*/
+
+#define _BYTE_ORDER _BIG_ENDIAN
+#include <mips/endian_machdep.h>
