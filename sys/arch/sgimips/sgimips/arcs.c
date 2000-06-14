2b22809c14c04413bbee15b19b714eecf72dbf68
soren <soren>
2000-06-14 16:02:38 +0000

    Initial commit. Needs work.

---
+++ sys/arch/sgimips/sgimips/arcs.c
@@ -0,0 +1,84 @@
+/*	$NetBSD: arcs.c,v 1.1 2000/06/14 16:02:38 soren Exp $	*/
+
+/*
+ * Copyright (c) 2000 Soren S. Jorvang
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *          This product includes software developed for the
+ *          NetBSD Project.  See http://www.netbsd.org/ for
+ *          information about NetBSD.
+ * 4. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <sys/param.h> 
+#include <sys/systm.h>
+#include <sys/ioctl.h>
+#include <sys/select.h>
+#include <sys/tty.h>
+#include <sys/proc.h>
+#include <sys/user.h>
+#include <sys/file.h>
+#include <sys/uio.h> 
+#include <sys/kernel.h>
+#include <sys/syslog.h>                  
+#include <sys/types.h>
+#include <sys/device.h>
+
+#include <mips/cpuregs.h>
+#include <machine/arcs.h>
+
+extern int db_max_line;
+
+const struct arcs_fv *ARCS;
+
+void arcsinit(void);
+void arcssig(void);
+
+void
+arcsinit(void)
+{
+	struct arcs_spb *spb;
+int i;
+
+	spb = (struct arcs_spb *)MIPS_PHYS_TO_KSEG0(0x00001000);
+	ARCS = spb->FirmwareVector;
+
+	/*
+	 * Default textport windows are only 20 lines high.
+	 */
+	db_max_line = 20;
+
+	for (i = 0; i < 32; i++) {
+#if 0
+printf("installing sig\n");
+#endif
+	}
+}
+
+void
+arcssig(void)
+{
+	printf("signal\n");
+}
---
+++ sys/arch/sgimips/sgimips/autoconf.c
@@ -0,0 +1,85 @@
+/*	$NetBSD: autoconf.c,v 1.1 2000/06/14 16:02:41 soren Exp $	*/
+
+/*
+ * Copyright (c) 2000 Soren S. Jorvang
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *          This product includes software developed for the
+ *          NetBSD Project.  See http://www.netbsd.org/ for
+ *          information about NetBSD.
+ * 4. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "opt_ddb.h"
+
+#include <sys/param.h>
+#include <sys/systm.h>
+#include <sys/buf.h>
+#include <sys/conf.h>
+#include <sys/device.h>
+
+#include <machine/cpu.h>
+
+static void	findroot(struct device **, int *);
+
+void
+cpu_configure()
+{
+	int s;
+
+	s = splhigh();
+
+	if (config_rootfound("mainbus", "mainbus") == NULL)
+		panic("no mainbus found");
+
+	printf("biomask %02x netmask %02x ttymask %02x clockmask %02x\n",
+	    biomask >> 8, netmask >> 8, ttymask >> 8, clockmask >> 8);
+
+	_splnone();
+}
+
+void
+cpu_rootconf()
+{
+	struct device *booted_device;
+	int booted_partition;
+
+	findroot(&booted_device, &booted_partition);
+
+	printf("boot device: %s\n",
+		booted_device ? booted_device->dv_xname : "<unknown>");
+
+	setroot(booted_device, booted_partition);
+}
+
+dev_t	bootdev = 0;
+
+static void
+findroot(devpp, partp)
+	struct device **devpp;
+	int *partp;
+{
+	return;
+}
