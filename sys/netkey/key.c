5ca9622243d04affecd6ea7912f0b91c8ca7c5a7
itojun <itojun@NetBSD.org>
2000-06-15 13:44:22 +0000

    correct compilation without IPSEC_ESP. From: Matthias Drochner <M.Drochner@fz-juelich.de>

--- sys/netkey/key.c
+++ sys/netkey/key.c
@@ -1,5 +1,5 @@
-/*	$NetBSD: key.c,v 1.22 2000/06/15 12:37:07 itojun Exp $	*/
-/*	$KAME: key.c,v 1.131 2000/06/15 12:20:50 sakane Exp $	*/
+/*	$NetBSD: key.c,v 1.23 2000/06/15 13:44:22 itojun Exp $	*/
+/*	$KAME: key.c,v 1.132 2000/06/15 13:41:49 itojun Exp $	*/
 
 /*
  * Copyright (C) 1995, 1996, 1997, and 1998 WIDE Project.
@@ -396,7 +396,9 @@ static int key_delete __P((struct socket *, struct mbuf *,
 static int key_get __P((struct socket *, struct mbuf *,
 	const struct sadb_msghdr *));
 
+#ifdef IPSEC_ESP
 static struct mbuf *key_getcomb_esp __P((void));
+#endif
 static struct mbuf *key_getcomb_ah __P((void));
 static struct mbuf *key_getprop __P((const struct secasindex *));
 
@@ -2629,7 +2631,9 @@ key_setsaval(sav, m, mhp)
 	struct mbuf *m;
 	const struct sadb_msghdr *mhp;
 {
+#ifdef IPSEC_ESP
 	const struct esp_algorithm *algo;
+#endif
 	int error = 0;
 
 	/* sanity check */
@@ -5279,6 +5283,7 @@ key_get(so, m, mhp)
     }
 }
 
+#ifdef IPSEC_ESP
 /*
  * XXX reorder combinations by preference
  * XXX no idea if the user wants ESP authentication or not
@@ -5357,6 +5362,7 @@ key_getcomb_esp()
 		m_freem(result);
 	return NULL;
 }
+#endif
 
 /*
  * XXX reorder combinations by preference
@@ -5429,9 +5435,11 @@ key_getprop(saidx)
 	int totlen;
 
 	switch (saidx->proto)  {
+#ifdef IPSEC_ESP
 	case IPPROTO_ESP:
 		m = key_getcomb_esp();
 		break;
+#endif
 	case IPPROTO_AH:
 		m = key_getcomb_ah();
 		break;
