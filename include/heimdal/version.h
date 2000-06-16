d0631efd563582c4a7289c984b208162dcf1f6fb
thorpej <thorpej@NetBSD.org>
2000-06-16 19:18:30 +0000

    Import Heimdal build-helper includes from cryptosrc-intl.

---
+++ include/heimdal/version.h
@@ -0,0 +1,5 @@
+char *heimdal_long_version = "@(#)$Version: heimdal-0.2t by root on blaha.blahopp (i386-unknown-netbsdelf1.4X) Thu May 18 05:00:28 CEST 2000 $";
+char *heimdal_version = "heimdal-0.2t";
+
+char *krb4_long_version = "@(#)$Version: krb4-1.0.1 by root on pezen.pdc.kth.se (i386-unknown-netbsd1.4Y) Fri Jun  2 02:39:36 CEST 2000 $";
+char *krb4_version = "krb4-1.0.1";
