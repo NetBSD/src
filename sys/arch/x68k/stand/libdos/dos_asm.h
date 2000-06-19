db3aed16df093673e714db9f64509dec5e6aaa03
itohy <itohy@NetBSD.org>
2000-06-19 03:42:31 +0000

    Fix weird macro definitions. They are internal use only and the typos are no harm, you know. :)

--- sys/arch/x68k/stand/libdos/dos_asm.h
+++ sys/arch/x68k/stand/libdos/dos_asm.h
@@ -4,14 +4,14 @@
  *	written by Yasha (ITOH Yasufumi)
  *	public domain
  *
- *	$NetBSD: dos_asm.h,v 1.1 1999/11/11 08:14:43 itohy Exp $
+ *	$NetBSD: dos_asm.h,v 1.2 2000/06/19 03:42:31 itohy Exp $
  */
 
 #include <machine/asm.h>
 
 #ifdef __ELF__
-#define CERROR		__DOS_PRCERROR
-#define PRCERROR	__DOS_CERROR
+#define CERROR		__DOS_CERROR
+#define PRCERROR	__DOS_PRCERROR
 #else
 #define CERROR		DOS_CERROR
 #define PRCERROR	DOS_PRCERROR
