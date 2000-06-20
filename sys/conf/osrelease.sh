ca9a295751c83183400631dc50b7a32120f99440
fvdl <fvdl>
2000-06-20 12:29:40 +0000

    Set release to 1.5_ALPHA

--- sys/conf/osrelease.sh
+++ sys/conf/osrelease.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-#	$NetBSD: osrelease.sh,v 1.46 2000/06/12 13:55:12 veego Exp $
+#	$NetBSD: osrelease.sh,v 1.46.2.1 2000/06/20 12:29:40 fvdl Exp $
 #
 # Copyright (c) 1997 The NetBSD Foundation, Inc.
 # All rights reserved.
@@ -39,7 +39,7 @@
 
 # Release number to use
 # Also check __NetBSD_Version__ in sys/sys/param.h if you change this!
-release=1.4ZD
+release=1.5_ALPHA
 
 
 case $1 in
