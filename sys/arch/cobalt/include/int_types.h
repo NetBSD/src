e641e5132bc79dac27893e70703011f7ae397268
kleink <kleink@NetBSD.org>
2000-06-26 15:42:16 +0000

    Add <machine/int_types.h>, which provides namespace-pure definitions of exact-width integer types.

---
+++ sys/arch/cobalt/include/int_types.h
@@ -0,0 +1,3 @@
+/*	$NetBSD: int_types.h,v 1.1 2000/06/26 15:42:31 kleink Exp $	*/
+
+#include <mips/int_types.h>
---
+++ sys/arch/pmax/include/int_types.h
@@ -0,0 +1,3 @@
+/*	$NetBSD: int_types.h,v 1.1 2000/06/26 15:43:04 kleink Exp $	*/
+
+#include <mips/int_types.h>
