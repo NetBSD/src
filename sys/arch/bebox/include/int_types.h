5b25003200f0a895a22c8fcabb4e06cbe0aaa8a3
kleink <kleink@NetBSD.org>
2000-06-26 15:42:16 +0000

    Add <machine/int_types.h>, which provides namespace-pure definitions of exact-width integer types.

---
+++ sys/arch/bebox/include/int_types.h
@@ -0,0 +1,3 @@
+/*	$NetBSD: int_types.h,v 1.1 2000/06/26 15:42:28 kleink Exp $	*/
+
+#include <powerpc/int_types.h>
---
+++ sys/arch/sh3/include/int_types.h
@@ -0,0 +1,55 @@
+/*	$NetBSD: int_types.h,v 1.1 2000/06/26 15:43:11 kleink Exp $	*/
+
+/*-
+ * Copyright (c) 1990 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed by the University of
+ *	California, Berkeley and its contributors.
+ * 4. Neither the name of the University nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ *	from: @(#)types.h	7.5 (Berkeley) 3/9/91
+ */
+
+#ifndef	_SH3_INT_TYPES_H_
+#define	_SH3_INT_TYPES_H_
+
+/*
+ * 7.18.1.1 Exact-width integer types
+ */
+typedef	__signed char		 __int8_t;
+typedef	unsigned char		__uint8_t;
+typedef	short			__int16_t;
+typedef	unsigned short	       __uint16_t;
+typedef	int			__int32_t;
+typedef	unsigned int	       __uint32_t;
+/* LONGLONG */
+typedef	long long		__int64_t;
+/* LONGLONG */
+typedef	unsigned long long     __uint64_t;
+
+#endif	/* !_SH3_INT_TYPES_H_ */
