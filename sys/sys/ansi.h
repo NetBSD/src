65fa23e925927ae15706d9b6a30cd38a87d179fe
kleink <kleink@NetBSD.org>
2000-06-26 16:53:20 +0000

    Define __mode_t.

--- sys/sys/ansi.h
+++ sys/sys/ansi.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: ansi.h,v 1.3 2000/06/26 15:51:15 kleink Exp $	*/
+/*	$NetBSD: ansi.h,v 1.4 2000/06/26 16:53:20 kleink Exp $	*/
 
 /*-
  * Copyright (c) 2000 The NetBSD Foundation, Inc.
@@ -41,6 +41,7 @@
 
 #include <machine/int_types.h>
 
+typedef __uint32_t	__mode_t;
 typedef __int64_t	__off_t;
 typedef __uint8_t	__sa_family_t;
 typedef unsigned int	__socklen_t;
