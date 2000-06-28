445d18ed286f1c4f5942e3628b43e78f04da53a5
thorpej <thorpej@NetBSD.org>
2000-06-28 01:13:35 +0000

    Implement FNM_CASEFOLD, for matching the pattern in a case-insensitive way.  Flag name taken from glibc.

--- include/fnmatch.h
+++ include/fnmatch.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: fnmatch.h,v 1.6 1998/01/13 12:45:02 kleink Exp $	*/
+/*	$NetBSD: fnmatch.h,v 1.7 2000/06/28 01:13:35 thorpej Exp $	*/
 
 /*-
  * Copyright (c) 1992, 1993
@@ -38,12 +38,17 @@
 #ifndef	_FNMATCH_H_
 #define	_FNMATCH_H_
 
+#include <sys/featuretest.h>
+
 #define	FNM_NOMATCH	1	/* Match failed. */
 #define	FNM_NOSYS	2	/* Function not implemented. */
 
 #define	FNM_NOESCAPE	0x01	/* Disable backslash escaping. */
 #define	FNM_PATHNAME	0x02	/* Slash must be matched by slash. */
 #define	FNM_PERIOD	0x04	/* Period must be matched by period. */
+#if !defined(_POSIX_C_SOURCE)
+#define	FNM_CASEFOLD	0x08	/* Pattern is matched case-insensitive */
+#endif
 
 #include <sys/cdefs.h>
 
