71e6f203f65bf0789f2b0016c5c6f04c2b4728ee
veego <veego>
2000-06-30 07:44:05 +0000

    We are now at 1.5B because of the removal of <vm/*>.

--- sys/conf/osrelease.sh
+++ sys/conf/osrelease.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-#	$NetBSD: osrelease.sh,v 1.47 2000/06/20 12:41:12 fvdl Exp $
+#	$NetBSD: osrelease.sh,v 1.48 2000/06/30 07:44:05 veego Exp $
 #
 # Copyright (c) 1997 The NetBSD Foundation, Inc.
 # All rights reserved.
@@ -39,7 +39,7 @@
 
 # Release number to use
 # Also check __NetBSD_Version__ in sys/sys/param.h if you change this!
-release=1.5A
+release=1.5B
 
 
 case $1 in
