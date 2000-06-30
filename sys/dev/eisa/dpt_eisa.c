f6d2fef2819febbe28c3fa71736414740be4be86
ad <ad@NetBSD.org>
2000-06-30 13:36:47 +0000

    Use my proper name in copyrights.

--- sys/dev/eisa/dpt_eisa.c
+++ sys/dev/eisa/dpt_eisa.c
@@ -1,7 +1,7 @@
-/*	$NetBSD: dpt_eisa.c,v 1.3 2000/01/05 16:28:41 ad Exp $	*/
+/*	$NetBSD: dpt_eisa.c,v 1.4 2000/06/30 13:36:49 ad Exp $	*/
 
 /*
- * Copyright (c) 1999 Andy Doran <ad@NetBSD.org>
+ * Copyright (c) 1999 Andrew Doran <ad@NetBSD.org>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -32,7 +32,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: dpt_eisa.c,v 1.3 2000/01/05 16:28:41 ad Exp $");
+__KERNEL_RCSID(0, "$NetBSD: dpt_eisa.c,v 1.4 2000/06/30 13:36:49 ad Exp $");
 
 #include <sys/types.h>
 #include <sys/param.h>
