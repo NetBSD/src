0c308caac89f7573e16167ff3b01fe4e8bbf6e91
thorpej <thorpej>
2000-07-02 17:41:17 +0000

    Pull up rev. 1.14: - Avoid an integer overflow when checking if we have exceeded our   rlimit in sbrk.  Slightly modified from a patch from Artur Grabowski. - Rearrange code slightly, partially from Artur Grabowski. - Only adjust vm_dsize if the grow or shrink actually succeeds.

--- sys/uvm/uvm_unix.c
+++ sys/uvm/uvm_unix.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: uvm_unix.c,v 1.12 2000/03/30 12:31:50 augustss Exp $	*/
+/*	$NetBSD: uvm_unix.c,v 1.12.4.1 2000/07/02 17:41:17 thorpej Exp $	*/
 
 /*
  * Copyright (c) 1997 Charles D. Cranor and Washington University.
@@ -79,46 +79,44 @@ sys_obreak(p, v, retval)
 	} */ *uap = v;
 	struct vmspace *vm = p->p_vmspace;
 	vaddr_t new, old;
+	ssize_t diff;
 	int rv;
-	int diff;
 
 	old = (vaddr_t)vm->vm_daddr;
 	new = round_page((vaddr_t)SCARG(uap, nsize));
-	if ((int)(new - old) > p->p_rlimit[RLIMIT_DATA].rlim_cur)
-		return(ENOMEM);
+	if ((new - old) > p->p_rlimit[RLIMIT_DATA].rlim_cur)
+		return (ENOMEM);
 
-	old = round_page(old + ctob(vm->vm_dsize));
+	old = round_page(old + ptoa(vm->vm_dsize));
 	diff = new - old;
+
+	if (diff == 0)
+		return (0);
 
 	/*
 	 * grow or shrink?
 	 */
-
 	if (diff > 0) {
-
 		rv = uvm_map(&vm->vm_map, &old, diff, NULL, UVM_UNKNOWN_OFFSET,
 		    UVM_MAPFLAG(UVM_PROT_ALL, UVM_PROT_ALL, UVM_INH_COPY,
 		    UVM_ADV_NORMAL, UVM_FLAG_AMAPPAD|UVM_FLAG_FIXED|
 		    UVM_FLAG_OVERLAY|UVM_FLAG_COPYONW)); 
-
-		if (rv != KERN_SUCCESS) {
-			uprintf("sbrk: grow failed, return = %d\n", rv);
-			return(ENOMEM);
+		if (rv == KERN_SUCCESS) {
+			vm->vm_dsize += atop(diff);
+			return (0);
 		}
-		vm->vm_dsize += btoc(diff);
-
-	} else if (diff < 0) {
-
-		diff = -diff;
-		rv = uvm_deallocate(&vm->vm_map, new, diff);
-		if (rv != KERN_SUCCESS) {
-			uprintf("sbrk: shrink failed, return = %d\n", rv);
-			return(ENOMEM);
+	} else {
+		rv = uvm_deallocate(&vm->vm_map, new, -diff);
+		if (rv == KERN_SUCCESS) {
+			vm->vm_dsize -= atop(-diff);
+			return (0);
 		}
-		vm->vm_dsize -= btoc(diff);
-
 	}
-	return(0);
+
+	uprintf("sbrk: %s %ld failed, return = %d\n",
+	    diff > 0 ? "grow" : "shrink",
+	    (long)(diff > 0 ? diff : -diff), rv);
+	return (ENOMEM);
 }
 
 /*
