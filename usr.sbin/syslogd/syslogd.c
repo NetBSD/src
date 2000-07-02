fcd1b1a49edffd1833ed714cf750f2391da25369
jwise <jwise>
2000-07-02 02:15:34 +0000

    Fix minor thinko in previous.  Make sure we close and keep closed all sockets in the situation where all of the following are true:
    	* /etc/syslogd.conf contained forwarding actions when we were
    	  started up or when we last received a HUP
    
    	* /etc/syslogd.conf has had all forwarding actions removed
    
    	* we are running with -s
    
    and we receive a HUP.

--- usr.sbin/syslogd/syslogd.c
+++ usr.sbin/syslogd/syslogd.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: syslogd.c,v 1.38 2000/06/30 18:45:04 jwise Exp $	*/
+/*	$NetBSD: syslogd.c,v 1.39 2000/07/02 02:15:34 jwise Exp $	*/
 
 /*
  * Copyright (c) 1983, 1988, 1993, 1994
@@ -43,7 +43,7 @@ __COPYRIGHT("@(#) Copyright (c) 1983, 1988, 1993, 1994\n\
 #if 0
 static char sccsid[] = "@(#)syslogd.c	8.3 (Berkeley) 4/4/94";
 #else
-__RCSID("$NetBSD: syslogd.c,v 1.38 2000/06/30 18:45:04 jwise Exp $");
+__RCSID("$NetBSD: syslogd.c,v 1.39 2000/07/02 02:15:34 jwise Exp $");
 #endif
 #endif /* not lint */
 
@@ -1073,6 +1073,12 @@ init(signo)
 			}
 		}
 	}
+
+	/*
+	 *  Reset counter of forwarding actions
+	 */
+
+	NumForwards=0;
 
 	/* open the configuration file */
 	if ((cf = fopen(ConfFile, "r")) == NULL) {
