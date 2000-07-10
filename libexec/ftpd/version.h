2476980772700a4a48c6524194bac301a8f65cf2
lukem <lukem>
2000-07-10 22:41:17 +0000

    base64_encode(): separate out the special case for the last 2 bytes, and be a bit safer with signed chars. per discussion with kre.

--- libexec/ftpd/version.h
+++ libexec/ftpd/version.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: version.h,v 1.14 2000/07/09 14:26:34 lukem Exp $	*/
+/*	$NetBSD: version.h,v 1.15 2000/07/10 22:41:18 lukem Exp $	*/
 /*-
  * Copyright (c) 1999, 2000 The NetBSD Foundation, Inc.
  * All rights reserved.
@@ -36,5 +36,5 @@
  */
 
 #ifndef FTPD_VERSION
-#define	FTPD_VERSION	"NetBSD-ftpd 20000710"
+#define	FTPD_VERSION	"NetBSD-ftpd 20000711"
 #endif
