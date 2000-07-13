83334b6ac89b4e166310c7d600a0e32d4ff72300
matt <matt@NetBSD.org>
2000-07-13 03:18:22 +0000

    Reflect the new reality.

--- sys/arch/vax/include/elf_machdep.h
+++ sys/arch/vax/include/elf_machdep.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: elf_machdep.h,v 1.3 2000/04/02 15:35:51 minoura Exp $	*/
+/*	$NetBSD: elf_machdep.h,v 1.4 2000/07/13 03:18:22 matt Exp $	*/
 
 #define	ELF32_MACHDEP_ENDIANNESS	ELFDATA2LSB
 #define	ELF32_MACHDEP_ID_CASES						\
@@ -13,17 +13,15 @@
 
 /* VAX relocations */
 #define	R_VAX_NONE	0
-#define	R_VAX_8		1	/* S + A */
+#define	R_VAX_32	1	/* S + A */
 #define	R_VAX_16	2
-#define	R_VAX_32	3
-#define R_VAX_RELATIVE	4
-#define	R_VAX_PC8	5	/* S + A - P */
-#define	R_VAX_PC16	6
-#define	R_VAX_PC32	7
-#define	R_VAX_COPY	8
-#define	R_VAX_REL32	9	/* S + A + D */
-#define	R_VAX_CALL_SLOT	10
-#define	R_VAX_JMP_SLOT	11
-#define	R_VAX_GLOB_DAT	12
+#define	R_VAX_8		3
+#define	R_VAX_PC32	4	/* S + A - PC */
+#define	R_VAX_PC16	5
+#define	R_VAX_PC8	6
+#define	R_VAX_COPY	19
+#define	R_VAX_GLOB_DAT	20
+#define	R_VAX_JMP_SLOT	21
+#define R_VAX_RELATIVE	22	/* S + A + D */
 
 #define	R_TYPE(name)	__CONCAT(R_VAX_,name)
