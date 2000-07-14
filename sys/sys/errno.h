9aca23d162de35efc3fade3c670e79f5d62785ee
tv <tv@NetBSD.org>
2000-07-14 00:52:02 +0000

    Add _XOPEN_SOURCE protections.  The need for these is questionable, advised by kleink about the E* predefined namespace; we'll look into that.
    Also add ENOTSUP from SUSv2 as an alias for EOPNOTSUPP.  It's used by the
    SUSv2 pthreads interface, has the same meaning, and pthreads does not use
    EOPNOTSUPP.

--- sys/sys/errno.h
+++ sys/sys/errno.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: errno.h,v 1.24 2000/03/14 19:16:03 kleink Exp $	*/
+/*	$NetBSD: errno.h,v 1.25 2000/07/14 00:52:02 tv Exp $	*/
 
 /*
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -58,9 +58,9 @@
 #define	ENOMEM		12		/* Cannot allocate memory */
 #define	EACCES		13		/* Permission denied */
 #define	EFAULT		14		/* Bad address */
-#ifndef _POSIX_SOURCE
+#if !defined(_POSIX_SOURCE) && !defined(_XOPEN_SOURCE)
 #define	ENOTBLK		15		/* Block device required */
-#endif
+#endif /* !_POSIX_SOURCE && !_XOPEN_SOURCE */
 #define	EBUSY		16		/* Device busy */
 #define	EEXIST		17		/* File exists */
 #define	EXDEV		18		/* Cross-device link */
@@ -73,7 +73,7 @@
 #define	ENOTTY		25		/* Inappropriate ioctl for device */
 #ifndef _POSIX_SOURCE
 #define	ETXTBSY		26		/* Text file busy */
-#endif
+#endif /* !_POSIX_SOURCE */
 #define	EFBIG		27		/* File too large */
 #define	ENOSPC		28		/* No space left on device */
 #define	ESPIPE		29		/* Illegal seek */
@@ -99,9 +99,14 @@
 #define	EPROTOTYPE	41		/* Protocol wrong type for socket */
 #define	ENOPROTOOPT	42		/* Protocol not available */
 #define	EPROTONOSUPPORT	43		/* Protocol not supported */
+#ifndef _XOPEN_SOURCE
 #define	ESOCKTNOSUPPORT	44		/* Socket type not supported */
+#endif /* !_XOPEN_SOURCE */
 #define	EOPNOTSUPP	45		/* Operation not supported */
+#define	ENOTSUP		EOPNOTSUPP	/* Operation not supported (pthreads) */
+#ifndef _XOPEN_SOURCE
 #define	EPFNOSUPPORT	46		/* Protocol family not supported */
+#endif /* !_XOPEN_SOURCE */
 #define	EAFNOSUPPORT	47		/* Address family not supported by protocol family */
 #define	EADDRINUSE	48		/* Address already in use */
 #define	EADDRNOTAVAIL	49		/* Can't assign requested address */
@@ -109,57 +114,69 @@
 /* ipc/network software -- operational errors */
 #define	ENETDOWN	50		/* Network is down */
 #define	ENETUNREACH	51		/* Network is unreachable */
+#ifndef _XOPEN_SOURCE
 #define	ENETRESET	52		/* Network dropped connection on reset */
+#endif /* !_XOPEN_SOURCE */
 #define	ECONNABORTED	53		/* Software caused connection abort */
 #define	ECONNRESET	54		/* Connection reset by peer */
 #define	ENOBUFS		55		/* No buffer space available */
 #define	EISCONN		56		/* Socket is already connected */
 #define	ENOTCONN	57		/* Socket is not connected */
+#ifndef _XOPEN_SOURCE
 #define	ESHUTDOWN	58		/* Can't send after socket shutdown */
 #define	ETOOMANYREFS	59		/* Too many references: can't splice */
+#endif /* !_XOPEN_SOURCE */
 #define	ETIMEDOUT	60		/* Operation timed out */
 #define	ECONNREFUSED	61		/* Connection refused */
 
 #define	ELOOP		62		/* Too many levels of symbolic links */
-#endif /* _POSIX_SOURCE */
+#endif /* !_POSIX_SOURCE */
 #define	ENAMETOOLONG	63		/* File name too long */
 
 /* should be rearranged */
 #ifndef _POSIX_SOURCE
+#ifndef _XOPEN_SOURCE
 #define	EHOSTDOWN	64		/* Host is down */
+#endif /* !_XOPEN_SOURCE */
 #define	EHOSTUNREACH	65		/* No route to host */
-#endif /* _POSIX_SOURCE */
+#endif /* !_POSIX_SOURCE */
 #define	ENOTEMPTY	66		/* Directory not empty */
 
 /* quotas & mush */
 #ifndef _POSIX_SOURCE
+#ifndef _XOPEN_SOURCE
 #define	EPROCLIM	67		/* Too many processes */
 #define	EUSERS		68		/* Too many users */
+#endif /* !_XOPEN_SOURCE */
 #define	EDQUOT		69		/* Disc quota exceeded */
 
 /* Network File System */
 #define	ESTALE		70		/* Stale NFS file handle */
+#ifndef _XOPEN_SOURCE
 #define	EREMOTE		71		/* Too many levels of remote in path */
 #define	EBADRPC		72		/* RPC struct is bad */
 #define	ERPCMISMATCH	73		/* RPC version wrong */
 #define	EPROGUNAVAIL	74		/* RPC prog. not avail */
 #define	EPROGMISMATCH	75		/* Program version wrong */
 #define	EPROCUNAVAIL	76		/* Bad procedure for program */
-#endif /* _POSIX_SOURCE */
+#endif /* !_XOPEN_SOURCE */
+#endif /* !_POSIX_SOURCE */
 
 #define	ENOLCK		77		/* No locks available */
 #define	ENOSYS		78		/* Function not implemented */
 
-#ifndef _POSIX_SOURCE
+#if !defined(_POSIX_SOURCE) && !defined(_XOPEN_SOURCE)
 #define	EFTYPE		79		/* Inappropriate file type or format */
 #define	EAUTH		80		/* Authentication error */
 #define	ENEEDAUTH	81		/* Need authenticator */
+#endif /* !_POSIX_SOURCE && !_XOPEN_SOURCE */
 
 /* SystemV IPC */
+#ifndef _POSIX_SOURCE
 #define	EIDRM		82		/* Identifier removed */
 #define	ENOMSG		83		/* No message of desired type */
 #define	EOVERFLOW	84		/* Value too large to be stored in data type */
-#endif /* _POSIX_SOURCE */
+#endif /* !_POSIX_SOURCE */
 
 /* Wide/multibyte-character handling, ISO/IEC 9899/AMD1:1995 */
 #define	EILSEQ		85		/* Illegal byte sequence */
