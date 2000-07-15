f5d897bc0ff5e7d1bf1c31b0b81435b3447bab3a
thorpej <thorpej@NetBSD.org>
2000-07-15 00:43:44 +0000

    Pull up rev. 1.3: If __KRB4_VERSION is defined, export Kerberos IV version strings, otherwise export only Heimdal Kerberos V version strings.

--- include/heimdal/version.h
+++ include/heimdal/version.h
@@ -1,5 +1,7 @@
+#ifndef __KRB4_VERSION
 const char *heimdal_long_version = "@(#)$Version: heimdal-0.2t (NetBSD) $";
 const char *heimdal_version = "heimdal-0.2t";
-
+#else
 const char *krb4_long_version = "@(#)$Version: krb4-1.0.1 (NetBSD) $";
 const char *krb4_version = "krb4-1.0.1";
+#endif /* ! __KRB4_VERSION */
