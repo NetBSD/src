b011ac8667ae17d756d129c9c4df409632500167
tron <tron@NetBSD.org>
2000-07-15 20:57:24 +0000

    Avoid printing duplicate colon when card is waked up.

--- sys/dev/pci/if_tlp_pci.c
+++ sys/dev/pci/if_tlp_pci.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: if_tlp_pci.c,v 1.41 2000/07/15 10:32:37 tron Exp $	*/
+/*	$NetBSD: if_tlp_pci.c,v 1.42 2000/07/15 20:57:24 tron Exp $	*/
 
 /*-
  * Copyright (c) 1998, 1999, 2000 The NetBSD Foundation, Inc.
@@ -492,7 +492,7 @@ tlp_pci_attach(parent, self, aux)
 			return;
 		}
 		if (reg != 0) {
-			printf(": waking up from power state D%d\n%s: ",
+			printf(": waking up from power state D%d\n%s",
 			    reg, sc->sc_dev.dv_xname);
 			pci_conf_write(pc, pa->pa_tag, pmreg + 4, 0);
 		}
