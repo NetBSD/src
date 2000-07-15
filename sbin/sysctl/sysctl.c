398ca5dfc8de2a512d5f831d474731697fdfe4b6
itojun <itojun>
2000-07-15 02:01:28 +0000

    pullup 1.38 -> 1.39 (requested by releng-1-5) sync with sys/sysctl.h change (constify)

--- sbin/sysctl/sysctl.c
+++ sbin/sysctl/sysctl.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: sysctl.c,v 1.37 2000/06/07 04:40:47 itojun Exp $	*/
+/*	$NetBSD: sysctl.c,v 1.37.2.1 2000/07/15 02:01:28 itojun Exp $	*/
 
 /*
  * Copyright (c) 1993
@@ -44,7 +44,7 @@ __COPYRIGHT(
 #if 0
 static char sccsid[] = "@(#)sysctl.c	8.1 (Berkeley) 6/6/93";
 #else
-__RCSID("$NetBSD: sysctl.c,v 1.37 2000/06/07 04:40:47 itojun Exp $");
+__RCSID("$NetBSD: sysctl.c,v 1.37.2.1 2000/07/15 02:01:28 itojun Exp $");
 #endif
 #endif /* not lint */
 
@@ -167,7 +167,7 @@ int	Aflag, aflag, nflag, wflag;
 
 int main __P((int, char *[]));
 
-static void listall __P((char *, struct list *));
+static void listall __P((const char *, struct list *));
 static void parse __P((char *, int));
 static void debuginit __P((void));
 static int sysctl_inet __P((char *, char **, int[], int, int *));
@@ -259,7 +259,7 @@ main(argc, argv)
  */
 static void
 listall(prefix, lp)
-	char *prefix;
+	const char *prefix;
 	struct list *lp;
 {
 	int lvl2;
