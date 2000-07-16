e92098fb7b53c21b98905085f754e0bd5e99426a
tron <tron@NetBSD.org>
2000-07-16 20:29:08 +0000

    Sync with NetBSD-current (pull up approved by thorpej):
    syssrc/sys/dev/pci/if_epic_pci.c, 1.12
    syssrc/sys/dev/pci/if_tlp_pci.c, 1.43
    
    If card is in power state D3 put it into power state D0 so that it will
    at least work after the next reboot.

--- sys/dev/pci/if_epic_pci.c
+++ sys/dev/pci/if_epic_pci.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: if_epic_pci.c,v 1.7.12.3 2000/07/15 22:48:01 tron Exp $	*/
+/*	$NetBSD: if_epic_pci.c,v 1.7.12.4 2000/07/16 20:29:08 tron Exp $	*/
 
 /*-
  * Copyright (c) 1998, 1999 The NetBSD Foundation, Inc.
@@ -167,19 +167,23 @@ epic_pci_attach(parent, self, aux)
 	int pmreg, ioh_valid, memh_valid;
 
 	if (pci_get_capability(pc, pa->pa_tag, PCI_CAP_PWRMGMT, &pmreg, 0)) {
-		reg = pci_conf_read(pc, pa->pa_tag, pmreg + 4) & 0x3;
-		if (reg == 3) {
-			/*
-			 * The card has lost all configuration data in
-			 * this state, so punt.
-			 */
-			printf(": unable to wake up from power state D3\n");
-			return;
-		}
-		if (reg != 0) {
+		reg = pci_conf_read(pc, pa->pa_tag, pmreg + 4);
+		switch (reg & PCI_PMCSR_STATE_MASK) {
+		case PCI_PMCSR_STATE_D1:
+		case PCI_PMCSR_STATE_D2:
 			printf(": waking up from power state D%d\n%s",
-			    reg, sc->sc_dev.dv_xname);
+			    reg & PCI_PMCSR_STATE_MASK, sc->sc_dev.dv_xname);
 			pci_conf_write(pc, pa->pa_tag, pmreg + 4, 0);
+			break;
+		case PCI_PMCSR_STATE_D3:
+			/*
+			 * IO and MEM are disabled. We can't enable
+			 * the card because the BARs might be invalid.
+			 */
+			printf(": unable to wake up from power state D3, "
+			       "reboot required.\n");
+			pci_conf_write(pc, pa->pa_tag, pmreg + 4, 0);
+			return;
 		}
 	}
 
