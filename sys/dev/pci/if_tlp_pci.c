6682dbef82c6524f23ea690ab576ae75adabab15
tron <tron@NetBSD.org>
2000-07-17 17:53:44 +0000

    Don't clobber bits from 2 to 31 when trying to change the power state. Problem noted by John Hawkinson.

--- sys/dev/pci/if_tlp_pci.c
+++ sys/dev/pci/if_tlp_pci.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: if_tlp_pci.c,v 1.43 2000/07/16 20:18:48 tron Exp $	*/
+/*	$NetBSD: if_tlp_pci.c,v 1.44 2000/07/17 17:53:44 tron Exp $	*/
 
 /*-
  * Copyright (c) 1998, 1999, 2000 The NetBSD Foundation, Inc.
@@ -488,7 +488,8 @@ tlp_pci_attach(parent, self, aux)
 		case PCI_PMCSR_STATE_D2:
 			printf(": waking up from power state D%d\n%s",
 			    reg & PCI_PMCSR_STATE_MASK, sc->sc_dev.dv_xname);
-			pci_conf_write(pc, pa->pa_tag, pmreg + 4, 0);
+			pci_conf_write(pc, pa->pa_tag, pmreg + 4,
+				       reg & ~PCI_PMCSR_STATE_MASK);
 			break;
 		case PCI_PMCSR_STATE_D3:
 			/*
@@ -497,7 +498,8 @@ tlp_pci_attach(parent, self, aux)
 			 */
 			printf(": unable to wake up from power state D3, "
 			       "reboot required.\n");
-			pci_conf_write(pc, pa->pa_tag, pmreg + 4, 0);
+			pci_conf_write(pc, pa->pa_tag, pmreg + 4,
+				       reg & ~PCI_PMCSR_STATE_MASK);
 			return;
 		}
 	}
