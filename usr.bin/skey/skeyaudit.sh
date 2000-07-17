7185dc16310f021eeefab481412cde236cbad3f6
mjl <mjl@NetBSD.org>
2000-07-17 19:55:50 +0000

    Pull up S/Key overhaul (RFC2289 compliance, new hash types, ctype clean up) from current. Approved by thorpej.

--- usr.bin/skey/skeyaudit.sh
+++ usr.bin/skey/skeyaudit.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 #
-#	$NetBSD: skeyaudit.sh,v 1.2 1996/09/19 19:44:34 thorpej Exp $
+#	$NetBSD: skeyaudit.sh,v 1.2.12.1 2000/07/17 19:55:54 mjl Exp $
 #
 # This script will look thru the skeykeys file for
-# people with sequence numbers less then LOWLIMIT=12
+# people with sequence numbers less than LOWLIMIT=12
 # and send them an e-mail reminder to use skeyinit soon
 # 
 
@@ -25,28 +25,34 @@ fi
 
 # an skeykeys entry looks like
 #   jsw 0076 la13079          ba20a75528de9d3a
-# the sequence number is the second entry
+#   #oot md5 0005 aspa26398        9432d570ff4421f0  Jul 07,2000 01:36:43
+#   mjl sha1 0099 alpha2           459a5dac23d20a90  Jul 07,2000 02:14:17
+# the sequence number is the second (or third) entry
 #
 
-for i in `$AWK '{print $1}' $KEYDB`
-do
-SEQ=`$GREP "^$i[ 	]" $KEYDB | $AWK '{print $2}'`
-if [ $SEQ -lt $LOWLIMIT ]
-then
-  KEY=`$GREP "^$i[ 	]" $KEYDB | $AWK '{print $3}'`
-  if [ $SEQ -lt  3 ]
-  then
-  SUBJECT="IMPORTANT action required"
+SKEYS=`$AWK '/^#/ {next} {if($2 ~ /^[0-9]+$/) print $1,$2,$3; else print $1,$3,$4; }' $KEYDB`
+
+set -- ${SKEYS}
+
+while [ "X$1" != "X" ]; do
+  USER=$1
+  SEQ=$2
+  KEY=$3
+  shift 3
+echo "$USER -- $SEQ -- $KEY"
+  if [ $SEQ -lt $LOWLIMIT ]; then
+    if [ $SEQ -lt  3 ]; then
+      SUBJECT="IMPORTANT action required"
+    fi
+    (
+    $ECHO "You are nearing the end of your current S/Key sequence for account $i"
+    $ECHO "on system $HOST."
+    $ECHO ""
+    $ECHO "Your S/key sequence number is now $SEQ.  When it reaches zero you"
+    $ECHO "will no longer be able to use S/Key to login into the system.  "
+    $ECHO " "
+    $ECHO "Use \"skeyinit -s\" to reinitialize your sequence number."
+    $ECHO ""
+    ) | /usr/bin/Mail -s "$SUBJECT"  $USER $ADMIN
   fi
-  (
-  $ECHO "You are nearing the end of your current S/Key sequence for account $i"
-  $ECHO "on system $HOST."
-  $ECHO ""
-  $ECHO "Your S/key sequence number is now $SEQ.  When it reaches zero you"
-  $ECHO "will no longer be able to use S/Key to login into the system.  "
-  $ECHO " "
-  $ECHO "Type \"skeyinit -s\" to reinitialize your sequence number."
-  $ECHO ""
-  ) | /usr/bin/Mail -s "$SUBJECT"  $i  $ADMIN
-fi
 done
