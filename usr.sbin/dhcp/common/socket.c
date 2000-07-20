8e7171f5ab23343853d72808d167b6bc6a9212a9
mellon <mellon@NetBSD.org>
2000-07-20 05:49:35 +0000

    Final (I hope) DHCP client security fix.

--- usr.sbin/dhcp/common/socket.c
+++ usr.sbin/dhcp/common/socket.c
@@ -51,7 +51,7 @@
 
 #ifndef lint
 static char copyright[] =
-"$Id: socket.c,v 1.1.1.10 2000/06/10 18:04:51 mellon Exp $ Copyright (c) 1995, 1996, 1997, 1998, 1999 The Internet Software Consortium.  All rights reserved.\n";
+"$Id: socket.c,v 1.1.1.11 2000/07/20 05:49:35 mellon Exp $ Copyright (c) 1995, 1996, 1997, 1998, 1999 The Internet Software Consortium.  All rights reserved.\n";
 #endif /* not lint */
 
 #include "dhcpd.h"
@@ -137,8 +137,14 @@ int if_register_socket (info)
 		log_fatal ("Can't set SO_BROADCAST option on dhcp socket: %m");
 
 	/* Bind the socket to this interface's IP address. */
-	if (bind (sock, (struct sockaddr *)&name, sizeof name) < 0)
-		log_fatal ("Can't bind to dhcp address: %m");
+	if (bind (sock, (struct sockaddr *)&name, sizeof name) < 0) {
+		log_error ("Can't bind to dhcp address: %m");
+		log_error ("Please make sure there is no other dhcp server");
+		log_error ("running and that there's no entry for dhcp or");
+		log_error ("bootp in /etc/inetd.conf.   Also make sure you");
+		log_error ("are not running HP JetAdmin software, which");
+		log_fatal ("includes a bootp server.");
+	}
 
 #if defined (HAVE_SO_BINDTODEVICE)
 	/* Bind this socket to this interface. */
