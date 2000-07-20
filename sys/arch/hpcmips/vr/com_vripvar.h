46f62ab52c66aa5f823e26ccc217c04acf45b9c3
jeffs <jeffs@NetBSD.org>
2000-07-20 21:03:36 +0000

    kgdb support for the vr hpcmips machines. From Ethan Solomita (ethan@geocast.com).

--- sys/arch/hpcmips/vr/com_vripvar.h
+++ sys/arch/hpcmips/vr/com_vripvar.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: com_vripvar.h,v 1.1.1.1 1999/09/16 12:23:32 takemura Exp $	*/
+/*	$NetBSD: com_vripvar.h,v 1.2 2000/07/20 21:03:38 jeffs Exp $	*/
 
 /*-
  * Copyright (c) 1999 SASAKI Takesi. All rights reserved.
@@ -34,4 +34,4 @@
  *
  */
 
-int com_vrip_cnattach __P((bus_space_tag_t, int, int, int, tcflag_t));
+int com_vrip_cndb_attach __P((bus_space_tag_t, int, int, int, tcflag_t, int));
