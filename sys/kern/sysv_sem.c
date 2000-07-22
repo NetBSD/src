b701c3bc509f6d18fb97f3a873b14969c103b71e
simonb <simonb@NetBSD.org>
2000-07-22 16:11:02 +0000

    Delete a couple of <uvm/uvm_extern.h> includes that were for <sys/sysctl.h> only.

--- sys/kern/sysv_sem.c
+++ sys/kern/sysv_sem.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: sysv_sem.c,v 1.39 2000/06/27 17:41:38 mrg Exp $	*/
+/*	$NetBSD: sysv_sem.c,v 1.40 2000/07/22 16:11:02 simonb Exp $	*/
 
 /*-
  * Copyright (c) 1999 The NetBSD Foundation, Inc.
@@ -50,7 +50,6 @@
 #include <sys/param.h>
 #include <sys/kernel.h>
 #include <sys/sem.h>
-#include <uvm/uvm_extern.h>	/* XXX for <sys/sysctl.h> */
 #include <sys/sysctl.h>
 #include <sys/mount.h>		/* XXX for <sys/syscallargs.h> */
 #include <sys/syscallargs.h>
