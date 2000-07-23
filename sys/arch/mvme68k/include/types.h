ea427751998384f8803996dc2e9e69b97d5d1698
scw <scw@NetBSD.org>
2000-07-23 20:51:18 +0000

    __BROKEN_CONFIG_UNIT_USAGE is no longer required for mvme68k.

--- sys/arch/mvme68k/include/types.h
+++ sys/arch/mvme68k/include/types.h
@@ -1,11 +1,10 @@
-/*	$NetBSD: types.h,v 1.5 2000/05/16 05:45:48 thorpej Exp $	*/
+/*	$NetBSD: types.h,v 1.6 2000/07/23 20:51:18 scw Exp $	*/
 
 #ifndef _MACHINE_TYPES_H_
 #define _MACHINE_TYPES_H_
 
 #include <m68k/types.h>
 
-#define __BROKEN_CONFIG_UNIT_USAGE
 #define	__BROKEN_DK_ESTABLISH
 
 #endif
