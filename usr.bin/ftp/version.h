7ebd2bf482e19144603bd8a2c22dbed5dd3540ac
lukem <lukem@NetBSD.org>
2000-07-28 12:01:40 +0000

    rename "opts" to "remopts", so people used to "o host" don't get bitten...

--- usr.bin/ftp/version.h
+++ usr.bin/ftp/version.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: version.h,v 1.15 2000/07/18 07:16:57 lukem Exp $	*/
+/*	$NetBSD: version.h,v 1.16 2000/07/28 12:01:40 lukem Exp $	*/
 /*-
  * Copyright (c) 1999, 2000 The NetBSD Foundation, Inc.
  * All rights reserved.
@@ -40,5 +40,5 @@
 #endif
 
 #ifndef FTP_VERSION
-#define	FTP_VERSION	"20000718"
+#define	FTP_VERSION	"20000728"
 #endif
