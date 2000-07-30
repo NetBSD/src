241987dc61683a3310d5c2dab142a7ed0c10952c
lukem <lukem@NetBSD.org>
2000-07-30 04:42:37 +0000

    clean up NO_QUAD support: create helper #defines and use as appropriate: 	#define		NOQUAD		! NOQUAD 	-------		------		- ------ 	QUADF		"%ld"		"%lld" 	QUADFP(x)	"%" x "ld"	"%" x "lld" 	QUADT		long		long long 	STRTOL(x,y,z)	strtol(x,y,z)	strtoll(x,y,z)

--- usr.bin/ftp/version.h
+++ usr.bin/ftp/version.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: version.h,v 1.16 2000/07/28 12:01:40 lukem Exp $	*/
+/*	$NetBSD: version.h,v 1.17 2000/07/30 04:42:38 lukem Exp $	*/
 /*-
  * Copyright (c) 1999, 2000 The NetBSD Foundation, Inc.
  * All rights reserved.
@@ -40,5 +40,5 @@
 #endif
 
 #ifndef FTP_VERSION
-#define	FTP_VERSION	"20000728"
+#define	FTP_VERSION	"20000730"
 #endif
