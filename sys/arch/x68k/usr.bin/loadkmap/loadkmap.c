d19870970ed51522bc48dfcb792941621407d930
minoura <minoura@NetBSD.org>
2000-07-31 23:40:02 +0000

    include <sys/types.h> to define u_int.

--- sys/arch/x68k/usr.bin/loadkmap/loadkmap.c
+++ sys/arch/x68k/usr.bin/loadkmap/loadkmap.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: loadkmap.c,v 1.3 1998/08/06 14:08:55 minoura Exp $	*/
+/*	$NetBSD: loadkmap.c,v 1.4 2000/07/31 23:40:02 minoura Exp $	*/
 /*
  * loadkmap - load keyboard map (for NetBSD/X680x0)
  * from: amiga/stand/loadkmap/loadkmap.c
@@ -6,6 +6,7 @@
  */
 
 #include <stdio.h>
+#include <sys/types.h>
 #include <sys/ioctl.h>
 #define ITEKANJI 1 /* XXX */
 #include <machine/iteioctl.h>
