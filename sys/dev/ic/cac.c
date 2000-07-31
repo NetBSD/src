822b5ac05b046dc469d1117e4e182c54e781a9aa
ad <ad@NetBSD.org>
2000-07-31 13:16:34 +0000

    Previous would have misbehaved if polled commands overlapped.

--- sys/dev/ic/cac.c
+++ sys/dev/ic/cac.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: cac.c,v 1.8 2000/07/24 12:28:31 ad Exp $	*/
+/*	$NetBSD: cac.c,v 1.9 2000/07/31 13:16:34 ad Exp $	*/
 
 /*-
  * Copyright (c) 2000 The NetBSD Foundation, Inc.
@@ -41,7 +41,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: cac.c,v 1.8 2000/07/24 12:28:31 ad Exp $");
+__KERNEL_RCSID(0, "$NetBSD: cac.c,v 1.9 2000/07/31 13:16:34 ad Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -354,9 +354,10 @@ cac_ccb_poll(sc, ccb, timo)
 	int off;
 	
 	ccb_done = NULL;
+	timo *= 10;
 
 	for (;;) {
-		for (timo *= 10, completed = 0; timo != 0; timo--) {
+		for (completed = 0; timo != 0; timo--) {
 			if ((completed = sc->sc_cl->cl_completed(sc)) != 0)
 				break;
 			DELAY(100);
