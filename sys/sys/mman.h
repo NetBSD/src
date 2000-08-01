22c202a882cf46f9fc719bd90ca75b6adf951bcd
wiz <wiz@NetBSD.org>
2000-08-01 00:53:07 +0000

    Rename VM_INHERIT_* to MAP_INHERIT_* and move them to sys/sys/mman.h as discussed on tech-kern. Retire sys/uvm/uvm_inherit.h, update man page for minherit(2).

--- sys/sys/mman.h
+++ sys/sys/mman.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: mman.h,v 1.26 2000/07/28 09:33:28 kleink Exp $	*/
+/*	$NetBSD: mman.h,v 1.27 2000/08/01 00:53:08 wiz Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1993
@@ -128,6 +128,15 @@ typedef	__off_t		off_t;		/* file offset */
 #define	MADV_DONTNEED	4	/* dont need these pages */
 #define	MADV_SPACEAVAIL	5	/* insure that resources are reserved */
 #define	MADV_FREE	6	/* pages are empty, free them */
+/*
+ * Flags to minherit
+ */
+#define	MAP_INHERIT_SHARE	0	/* share with child */
+#define	MAP_INHERIT_COPY	1	/* copy into child */
+#define	MAP_INHERIT_NONE	2	/* absent from child */
+#define	MAP_INHERIT_DONATE_COPY	3	/* copy and delete -- not
+					   implemented in UVM */
+#define	MAP_INHERIT_DEFAULT	MAP_INHERIT_COPY
 #endif
 
 #ifndef _KERNEL
