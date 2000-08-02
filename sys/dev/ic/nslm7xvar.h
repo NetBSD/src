40385e06c2cc889758c03faf14d46ffae8869de9
bouyer <bouyer>
2000-08-02 17:09:49 +0000

    Pull up (approved by thorpej): sys/dev/ic/nslm7x.c 1.4->1.7 sys/dev/ic/nslm7xvar.h 1.3->1.5 share/man/man4/lm.4 1.5->1.6
    Add support for a few winbond hardware monitors.

--- sys/dev/ic/nslm7xvar.h
+++ sys/dev/ic/nslm7xvar.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: nslm7xvar.h,v 1.2.4.1 2000/07/30 17:54:14 bouyer Exp $ */
+/*	$NetBSD: nslm7xvar.h,v 1.2.4.2 2000/08/02 17:09:49 bouyer Exp $ */
 
 /*-
  * Copyright (c) 2000 The NetBSD Foundation, Inc.
@@ -67,6 +67,50 @@
 #define LM_ID_LM79	0xC0
 #define LM_ID_MASK	0xFE
 
+/*
+ * additionnal registers for the Winbond chips:
+ * WB83781D: mostly lm7x compatible; extra temp sensors in bank1 & 2
+ * WB83782D & WB83627HF: voltage sensors needs different handling, more FAN
+ *                       dividers; mode voltage sensors, more temp sensors.
+ */
+#define WB_PIN		0x4B	/* pin & fan3 divider */
+#define WB_BANKSEL	0x4E	/* banck select register */
+#define WB_BANKSEL_B0	0x00	/* select bank 0 */
+#define WB_BANKSEL_B1	0x01	/* select bank 1 */
+#define WB_BANKSEL_B2	0x02	/* select bank 2 */
+#define WB_BANKSEL_B3	0x03	/* select bank 3 */
+#define WB_BANKSEL_B4	0x04	/* select bank 4 */
+#define WB_BANKSEL_B5	0x05	/* select bank 5 */
+#define WB_BANKSEL_HBAC	0x80	/* hight byte access */
+
+#define WB_VENDID	0x4F	/* vendor ID register */
+#define WB_VENDID_WINBOND 0x5CA3
+/* Bank0 regs */
+#define WB_BANK0_CHIPID	0x58
+#define WB_CHIPID_83781		0x10
+#define WB_CHIPID_83782		0x30
+#define WB_CHIPID_83627		0x21
+#define WB_BANK0_FANBAT	0x5D
+/* Bank1 regs */
+#define WB_BANK1_T2H	0x50
+#define WB_BANK1_T2L	0x51
+
+/* Bank2 regs */
+#define WB_BANK2_T3H	0x50
+#define WB_BANK2_T3L	0x51
+
+/* Bank4 regs 83782/83627 only */
+#define WB_BANK4_T1OFF	0x54
+#define WB_BANK4_T2OFF	0x55
+#define WB_BANK4_T3OFF	0x56
+
+/* Bank5 regs 83782/83627 only */
+#define WB_BANK5_5VSB	0x50
+#define WB_BANK5_VBAT	0x51
+
+#define WB83781_NUM_SENSORS	13
+#define WB_NUM_SENSORS	15
+
 struct lm_softc {
 	struct	device sc_dev;
 
@@ -76,8 +120,10 @@ struct lm_softc {
 
 	int	sc_flags;
 	struct	timeval lastread; /* only allow reads every 1.5 seconds */
-	struct	envsys_tre_data sensors[LM_NUM_SENSORS];
-	struct	envsys_basic_info info[LM_NUM_SENSORS];
+	struct	envsys_tre_data sensors[WB_NUM_SENSORS];
+	struct	envsys_basic_info info[WB_NUM_SENSORS];
+	int numsensors;
+	void (*refresh_sensor_data) __P((struct lm_softc *));
 
 	struct sysmon_envsys sc_sysmon;
 };
