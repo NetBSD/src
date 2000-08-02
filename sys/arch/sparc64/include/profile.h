0b2f9843438c497c7f1b861cfcccf4ef98549e43
eeh <eeh>
2000-08-02 22:24:38 +0000

    Get kernel profiling to work.  Now if only gprof worked....

--- sys/arch/sparc64/include/profile.h
+++ sys/arch/sparc64/include/profile.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: profile.h,v 1.3 1998/10/08 02:31:40 eeh Exp $ */
+/*	$NetBSD: profile.h,v 1.4 2000/08/02 22:24:39 eeh Exp $ */
 
 /*
  * Copyright (c) 1992, 1993
@@ -45,16 +45,16 @@
  */
 
 #if	defined(ELF)||defined(__ELF__)
-#define _MCOUNT_SYM "_mcount"
-#else
 #define _MCOUNT_SYM "__mcount"
+#else
+#define _MCOUNT_SYM "___mcount"
 #endif
 
 #ifdef PIC
 /* Inline expansion of PICCY_SET() (see <machine/asm.h>). */
 #define MCOUNT \
-	__asm__(".global mcount");\
-	__asm__("mcount:");\
+	__asm__(".global _mcount");\
+	__asm__("_mcount:");\
 	__asm__("add %o7, 8, %o1");\
 	__asm__("1: rd %pc, %o2");\
 	__asm__("add %o2," _MCOUNT_SYM "-1b, %o2");\
@@ -63,15 +63,15 @@
 	__asm__("add %i7, 8, %o0");
 #else
 #define MCOUNT \
-	__asm__(".global mcount");\
-	__asm__("mcount:");\
+	__asm__(".global _mcount");\
+	__asm__("_mcount:");\
 	__asm__("add %i7, 8, %o0");\
 	__asm__("sethi %hi(" _MCOUNT_SYM "), %o2");\
 	__asm__("jmpl %o2 + %lo(" _MCOUNT_SYM "), %g0");\
 	__asm__("add %o7, 8, %o1");
 #endif
 
-#define	_MCOUNT_DECL	static void _mcount
+#define	_MCOUNT_DECL	static void __mcount
 
 #ifdef _KERNEL
 /*
