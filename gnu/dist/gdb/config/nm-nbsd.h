ae6215ef40a81bad52c99a00817918bbd1369cf8
msaitoh <msaitoh@NetBSD.org>
2000-08-03 05:04:02 +0000

    fix typo

--- gnu/dist/gdb/config/nm-nbsd.h
+++ gnu/dist/gdb/config/nm-nbsd.h
@@ -37,7 +37,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
 
 #define PTRACE_ARG3_TYPE char*
 
-#if !defind(NO_SOLIB)
+#if !defined(NO_SOLIB)
 #include "solib.h"      /* Support for shared libraries. */
 #endif
 
