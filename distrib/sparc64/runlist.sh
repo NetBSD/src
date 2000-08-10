aab46526283e0a6f6c45ab89e78e74a79fb51672
mrg <mrg@NetBSD.org>
2000-08-10 14:38:51 +0000

    first cut sysinst installer.

---
+++ distrib/sparc64/runlist.sh
@@ -0,0 +1,13 @@
+#	$NetBSD: runlist.sh,v 1.1 2000/08/10 14:38:52 mrg Exp $
+
+if [ "X$1" = "X-d" ]; then
+	SHELLCMD=cat
+	shift
+else
+	SHELLCMD="sh -e"
+fi
+
+( while [ "X$1" != "X" ]; do
+	cat $1
+	shift
+done ) | awk -f ${TOPDIR}/list2sh.awk | ${SHELLCMD}
