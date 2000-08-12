079465ff550609d510955368e292cd0f09a8c82d
wdk <wdk>
2000-08-12 22:57:55 +0000

    Initial commit of port to MIPS Computer Systems RC3xxx systems. Currently supports the RC3230

---
+++ sys/arch/mipsco/include/int_types.h
@@ -0,0 +1,3 @@
+/*	$NetBSD: int_types.h,v 1.1 2000/08/12 22:58:22 wdk Exp $	*/
+
+#include <mips/int_types.h>
---
+++ sys/arch/mipsco/include/pcb.h
@@ -0,0 +1,3 @@
+/*	$NetBSD: pcb.h,v 1.1 2000/08/12 22:58:31 wdk Exp $	*/
+
+#include <mips/pcb.h>
