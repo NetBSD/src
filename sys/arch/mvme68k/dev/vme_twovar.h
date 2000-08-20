5a034ed0033384ddf8972452ddc9503113b82e21
scw <scw@NetBSD.org>
2000-08-20 17:07:41 +0000

    Checkpoint of code to add VMEbus slave support using vme_dmamap* and vme_dmamem*.
    This is still a work in progress, but seems to DTRT on mvme167 so far.
    
    TODO:
    	. Get VMEbus slave mode going on mvme147. This should be easy.
    	. Fix up the A16 slave mappings.
    	. Bounce buffer support. (Messy, but pretty much a `must have'.)
    	. Figure out how to deal with `location monitor' interrupts
    	  within the framework. (Useful for Busnet, among other things.)
    	. It would be nice to make use of the VMEchip2's DMA facilities...

--- sys/arch/mvme68k/dev/vme_twovar.h
+++ sys/arch/mvme68k/dev/vme_twovar.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: vme_twovar.h,v 1.5 2000/08/13 17:00:53 scw Exp $ */
+/*	$NetBSD: vme_twovar.h,v 1.6 2000/08/20 17:07:42 scw Exp $ */
 
 /*-
  * Copyright (c) 1999 The NetBSD Foundation, Inc.
@@ -43,8 +43,11 @@
 #define	VME2_MASTER_FIXED_A24	1
 #define	VME2_MASTER_FIXED_A32	2
 #define VME2_MASTER_PROG_START	3
+#define VME2_SLAVE_A16		0
+#define VME2_SLAVE_PROG_START	1
 
 #define VME2_NMASTERS		(VME2_MASTER_PROG_START + VME2_MASTER_WINDOWS)
+#define VME2_NSLAVES		(VME2_SLAVE_PROG_START+(VME2_SLAVE_WINDOWS * 2))
 
 struct vmetwo_softc {
 	struct mvmebus_softc	sc_mvmebus;
@@ -52,6 +55,7 @@ struct vmetwo_softc {
 	bus_space_handle_t	sc_lcrh;
 	bus_space_handle_t	sc_gcrh;
 	struct mvmebus_range	sc_master[VME2_NMASTERS];
+	struct mvmebus_range	sc_slave[VME2_NSLAVES];
 };
 
 #endif /* _MVME68K_VME_TWOVAR_H */
