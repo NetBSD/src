d12bf7ac30e1786724796f89288165dda27b987a
enami <enami@NetBSD.org>
2000-08-22 02:12:47 +0000

    Regen.

--- sys/dev/pci/pcidevs.h
+++ sys/dev/pci/pcidevs.h
@@ -1,10 +1,10 @@
-/*	$NetBSD: pcidevs.h,v 1.242.2.5 2000/07/23 05:21:39 jonathan Exp $	*/
+/*	$NetBSD: pcidevs.h,v 1.242.2.6 2000/08/22 02:12:47 enami Exp $	*/
 
 /*
  * THIS FILE AUTOMATICALLY GENERATED.  DO NOT EDIT.
  *
  * generated from:
- *	NetBSD: pcidevs,v 1.236.2.5 2000/07/23 05:19:19 jonathan Exp 
+ *	NetBSD: pcidevs,v 1.236.2.6 2000/08/22 02:08:40 enami Exp 
  */
 
 /*
@@ -1280,6 +1280,7 @@
 #define	PCI_PRODUCT_PROMISE_ULTRA33	0x4d33		/* Ultra33/ATA Bus Master IDE Accelerator */
 #define	PCI_PRODUCT_PROMISE_ULTRA66	0x4d38		/* Ultra66/ATA Bus Master IDE Accelerator */
 #define	PCI_PRODUCT_PROMISE_ULTRA100	0x4d30		/* Ultra100/ATA Bus Master IDE Accelerator */
+#define	PCI_PRODUCT_PROMISE_ULTRA100X	0x0d30		/* Ultra100/ATA Bus Master IDE Accelerator */
 
 /* QLogic products */
 #define	PCI_PRODUCT_QLOGIC_ISP1020	0x1020		/* ISP1020 */
