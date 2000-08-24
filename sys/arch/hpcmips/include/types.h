d380c4bb5e8996e65e73942490e9420ccaadef80
uch <uch@NetBSD.org>
2000-08-24 11:19:19 +0000

    don't refer cf_unit. undef __BROKEN_CONFIG_UNIT_USAGE

--- sys/arch/hpcmips/include/types.h
+++ sys/arch/hpcmips/include/types.h
@@ -1,6 +1,4 @@
-/*	$NetBSD: types.h,v 1.2 2000/02/05 00:13:23 cgd Exp $	*/
+/*	$NetBSD: types.h,v 1.3 2000/08/24 11:19:20 uch Exp $	*/
 
 #include <mips/types.h>
-
-#define __BROKEN_CONFIG_UNIT_USAGE
 
