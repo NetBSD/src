2b320c03d0228ba16222c63498fd733a7efa1866
matt <matt@NetBSD.org>
2000-08-26 00:54:59 +0000

    Pull up dd.c 1.17, extern.h 1.10, position 1.9.  approved by thorpej: >fix conv=swap when ibs!=obs >block SIGINFO when writing

--- bin/dd/position.c
+++ bin/dd/position.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: position.c,v 1.8 1999/10/09 00:43:32 jtk Exp $	*/
+/*	$NetBSD: position.c,v 1.8.6.1 2000/08/26 00:54:59 matt Exp $	*/
 
 /*-
  * Copyright (c) 1991, 1993, 1994
@@ -42,7 +42,7 @@
 #if 0
 static char sccsid[] = "@(#)position.c	8.3 (Berkeley) 4/2/94";
 #else
-__RCSID("$NetBSD: position.c,v 1.8 1999/10/09 00:43:32 jtk Exp $");
+__RCSID("$NetBSD: position.c,v 1.8.6.1 2000/08/26 00:54:59 matt Exp $");
 #endif
 #endif /* not lint */
 
@@ -167,7 +167,7 @@ pos_out()
 			err(1, "%s", out.name);
 
 		while (cnt++ < out.offset)
-			if ((n = write(out.fd, out.db, out.dbsz)) != out.dbsz)
+			if ((n = bwrite(out.fd, out.db, out.dbsz)) != out.dbsz)
 				err(1, "%s", out.name);
 		break;
 	}
