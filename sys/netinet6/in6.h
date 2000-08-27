21e5c1c4287eb1b876304f556104078511e30c6a
itojun <itojun>
2000-08-27 01:02:48 +0000

    add a warning on IPv6 setsockopt number space (*BSD shares the number space so consult KAME for number allocation)

--- sys/netinet6/in6.h
+++ sys/netinet6/in6.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: in6.h,v 1.19 2000/08/26 11:03:46 itojun Exp $	*/
+/*	$NetBSD: in6.h,v 1.20 2000/08/27 01:02:48 itojun Exp $	*/
 /*	$KAME: in6.h,v 1.57 2000/08/26 10:00:45 itojun Exp $	*/
 
 /*
@@ -404,6 +404,7 @@ struct route_in6 {
 #define IPV6_IPSEC_POLICY	28 /* struct; get/set security policy */
 #endif
 #define IPV6_FAITH		29 /* bool; accept FAITH'ed connections */
+/* to define items, should talk with KAME guys first, for *BSD compatibility */
 
 #define IPV6_RTHDR_LOOSE     0 /* this hop need not be a neighbor. XXX old spec */
 #define IPV6_RTHDR_STRICT    1 /* this hop must be a neighbor. XXX old spec */
