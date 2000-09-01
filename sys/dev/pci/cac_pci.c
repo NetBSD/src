70f91159f1fb6f611a9d367edf223ac0ed1b6c29
ad <ad@NetBSD.org>
2000-09-01 12:13:08 +0000

    - Interface cleanup. static, const in places. - cac_pci_l0 has been moved to cac.c, since it's useful for EISA boards. - Model-specific linkage now is responsible for DMA synchronization. - If we don't recognise the board, print the subsystem ID. - Add support for RAID LC2 and Smart Array 431. - Start firmware background tasks on controllers that need it.

--- sys/dev/pci/cac_pci.c
+++ sys/dev/pci/cac_pci.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: cac_pci.c,v 1.4 2000/06/13 13:36:52 ad Exp $	*/
+/*	$NetBSD: cac_pci.c,v 1.5 2000/09/01 12:13:08 ad Exp $	*/
 
 /*-
  * Copyright (c) 2000 The NetBSD Foundation, Inc.
@@ -40,9 +40,6 @@
  * PCI front-end for cac(4) driver.
  */
 
-#include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: cac_pci.c,v 1.4 2000/06/13 13:36:52 ad Exp $");
-
 #include <sys/param.h>
 #include <sys/systm.h>
 #include <sys/kernel.h>
@@ -63,74 +60,55 @@ __KERNEL_RCSID(0, "$NetBSD: cac_pci.c,v 1.4 2000/06/13 13:36:52 ad Exp $");
 #define	PCI_CBIO	0x10	/* Configuration base I/O address */
 #define	PCI_CBMA	0x14	/* Configuration base memory address */
 
-static int	cac_pci_match __P((struct device *, struct cfdata *, void *));
-static void	cac_pci_attach __P((struct device *, struct device *, void *));
+static void	cac_pci_attach(struct device *, struct device *, void *);
+static int	cac_pci_match(struct device *, struct cfdata *, void *);
 
-static void	cac_pci_l0_submit __P((struct cac_softc *, paddr_t));
-static paddr_t	cac_pci_l0_completed __P((struct cac_softc *));
-static int	cac_pci_l0_intr_pending __P((struct cac_softc *));
-static void	cac_pci_l0_intr_enable __P((struct cac_softc *, int));
-static int	cac_pci_l0_fifo_full __P((struct cac_softc *));
-
-static void	cac_pci_l1_submit __P((struct cac_softc *, paddr_t));
-static paddr_t	cac_pci_l1_completed __P((struct cac_softc *));
-static int	cac_pci_l1_intr_pending __P((struct cac_softc *));
-static void	cac_pci_l1_intr_enable __P((struct cac_softc *, int));
-static int	cac_pci_l1_fifo_full __P((struct cac_softc *));
+static struct	cac_ccb *cac_pci_l0_completed(struct cac_softc *);
+static int	cac_pci_l0_fifo_full(struct cac_softc *);
+static void	cac_pci_l0_intr_enable(struct cac_softc *, int);
+static int	cac_pci_l0_intr_pending(struct cac_softc *);
+static void	cac_pci_l0_submit(struct cac_softc *, struct cac_ccb *);
 
 struct cfattach cac_pci_ca = {
 	sizeof(struct cac_softc), cac_pci_match, cac_pci_attach
 };
 
 static struct cac_linkage cac_pci_l0 = {
-	cac_pci_l0_submit,
 	cac_pci_l0_completed,
-	cac_pci_l0_intr_pending,
+	cac_pci_l0_fifo_full,
 	cac_pci_l0_intr_enable,
-	cac_pci_l0_fifo_full
+	cac_pci_l0_intr_pending,
+	cac_pci_l0_submit
 };
 
-static struct cac_linkage cac_pci_l1 = {
-	cac_pci_l1_submit,
-	cac_pci_l1_completed,
-	cac_pci_l1_intr_pending,
-	cac_pci_l1_intr_enable,
-	cac_pci_l1_fifo_full
-};
+#define	CT_IOMAP	0x01	/* Use I/O port access */
+#define CT_STARTFW	0x02	/* Need to start controller firmware */
 
-/* This block of code inspired by Compaq's Linux driver. */
-struct cac_type {
-	int	ct_subsysid;			/* PCI subsystem ID */
-	int	ct_mmreg;			/* Memory mapped registers */
-	struct	cac_linkage *ct_linkage;	/* Command interface */
-	char	*ct_typestr;			/* Textual description */
-} static cac_type[] = {
-#ifdef notdef
-	{ 0x0040110e, 0, &cac_pci_lX, "IDA" },
-	{ 0x0140110e, 0, &cac_pci_lX, "IDA 2" },
-	{ 0x1040110e, 0, &cac_pci_lX, "IAES" },
-	{ 0x2040110e, 0, &cac_pci_lX, "SMART" },
-#endif
-	{ 0x3040110e, 0, &cac_pci_l0, "SMART-2/E" },
-	{ 0x40300e11, 1, &cac_pci_l0, "SMART-2/P" },
-	{ 0x40310e11, 1, &cac_pci_l0, "SMART-2SL" },
-	{ 0x40320e11, 1, &cac_pci_l0, "Smart Array 3200" },
-	{ 0x40330e11, 1, &cac_pci_l0, "Smart Array 3100ES" },
-	{ 0x40340e11, 1, &cac_pci_l0, "Smart Array 221" },
-	{ 0x40400e11, 1, &cac_pci_l1, "Integrated Array" },
-	{ 0x40500e11, 1, &cac_pci_l1, "Smart Array 4200" },
-	{ 0x40510e11, 1, &cac_pci_l1, "Smart Array 4200ES" },
-	{ -1, 1, &cac_pci_l0, "array controller (unknown type)" },
+struct cac_pci_type {
+	int	ct_subsysid;
+	int	ct_flags;
+	struct	cac_linkage *ct_linkage;
+	char	*ct_typestr;
+} static cac_pci_type[] = {
+	{ 0x3040110e,	CT_IOMAP,	&cac_l0,	"SMART-2/E" },
+	{ 0x40300e11,	0, 		&cac_l0,	"SMART-2/P" },
+	{ 0x40310e11,	0, 		&cac_l0, 	"SMART-2SL" },
+	{ 0x40320e11,	0, 		&cac_l0,	"Smart Array 3200" },
+	{ 0x40330e11,	0, 		&cac_l0,	"Smart Array 3100ES" },
+	{ 0x40340e11,	0, 		&cac_l0,	"Smart Array 221" },
+	{ 0x40400e11,	CT_STARTFW, 	&cac_pci_l0,	"Integrated Array" },
+	{ 0x40480e11,	CT_STARTFW,	&cac_pci_l0,	"RAID LC2" },
+	{ 0x40500e11,	0,	 	&cac_pci_l0,	"Smart Array 4200" },
+	{ 0x40510e11,	0, 		&cac_pci_l0,	"Smart Array 4200ES" },
+	{ 0x40580e11,	0,		&cac_pci_l0,	"Smart Array 431" },
+	{ 0,		0,		&cac_l0,	NULL },
 };
 
 static int
-cac_pci_match(parent, match, aux)
-	struct device *parent;
-	struct cfdata *match;
-	void *aux;
+cac_pci_match(struct device *parent, struct cfdata *match, void *aux)
 {
 	struct pci_attach_args *pa;
-	
+
 	pa = (struct pci_attach_args *)aux;
 
 	if (PCI_VENDOR(pa->pa_id) == PCI_VENDOR_COMPAQ && 
@@ -145,45 +123,39 @@ cac_pci_match(parent, match, aux)
 }
 
 static void
-cac_pci_attach(parent, self, aux)
-	struct device *parent;
-	struct device *self;
-	void *aux;
+cac_pci_attach(struct device *parent, struct device *self, void *aux)
 {
 	struct pci_attach_args *pa;
-	struct cac_type *ct;
+	struct cac_pci_type *ct;
 	struct cac_softc *sc;
 	pci_chipset_tag_t pc;
 	pci_intr_handle_t ih;
 	const char *intrstr;
 	pcireg_t csr, subsysid;
-	int mmreg;
-	
+	int flags;
+
 	sc = (struct cac_softc *)self;
 	pa = (struct pci_attach_args *)aux;
 	pc = pa->pa_pc;
-	mmreg = 0;
-	
-	printf(": ");
 
 	subsysid = pci_conf_read(pa->pa_pc, pa->pa_tag, PCI_SUBSYS_ID_REG);
 
-	for (ct = cac_type; ct->ct_subsysid != -1; ct++)
+	for (ct = cac_pci_type; ct->ct_subsysid != 0; ct++)
 		if (subsysid == ct->ct_subsysid)
 			break;
 
-	if ((mmreg = ct->ct_mmreg) != 0)
+	if (((flags = ct->ct_flags) & CT_IOMAP) == 0)
 		if (pci_mapreg_map(pa, PCI_CBMA, PCI_MAPREG_TYPE_MEM, 0,
 		    &sc->sc_iot, &sc->sc_ioh, NULL, NULL))
-		    	mmreg = 0;
-	
-	if (mmreg == 0)
+		    	flags |= CT_IOMAP;
+
+	if ((flags & CT_IOMAP) != 0)
 		if (pci_mapreg_map(pa, PCI_CBIO, PCI_MAPREG_TYPE_IO, 0,
 		    &sc->sc_iot, &sc->sc_ioh, NULL, NULL)) {
 			printf("can't map i/o space\n");
 			return;
 		}
-	
+
 	sc->sc_dmat = pa->pa_dmat;
 
 	/* Enable the device. */
@@ -207,97 +179,63 @@ cac_pci_attach(parent, self, aux)
 		return;
 	}
 
-	/* Now attach to the bus-independent code */
-	sc->sc_typestr = ct->ct_typestr;
+	printf(": Compaq ");
+	if (ct->ct_typestr == NULL) {
+		printf("array controller\n%s: unknown subsystem ID: 0x%08x\n",
+		    sc->sc_dv.dv_xname, (u_int)subsysid);
+	} else
+		printf("%s\n", ct->ct_typestr);
+
+	/* Now attach to the bus-independent code. */
 	sc->sc_cl = ct->ct_linkage;
-	cac_init(sc, intrstr);
+	cac_init(sc, intrstr, (flags & CT_STARTFW) != 0);
 }
 
 static void
-cac_pci_l0_submit(sc, addr)
-	struct cac_softc *sc;
-	paddr_t addr;
+cac_pci_l0_submit(struct cac_softc *sc, struct cac_ccb *ccb)
 {
 
-	cac_outl(sc, CAC_REG_CMD_FIFO, addr);
+	bus_dmamap_sync(sc->sc_dmat, sc->sc_dmamap, (caddr_t)ccb - sc->sc_ccbs,
+	    sizeof(struct cac_ccb), BUS_DMASYNC_PREWRITE | BUS_DMASYNC_PREREAD);
+	cac_outl(sc, CAC_42REG_CMD_FIFO, ccb->ccb_paddr);
 }
 
-static paddr_t
-cac_pci_l0_completed(sc)
-	struct cac_softc *sc;
+static struct cac_ccb *
+cac_pci_l0_completed(struct cac_softc *sc)
 {
+	struct cac_ccb *ccb;
+	u_int32_t off;
 
-	return (cac_inl(sc, CAC_REG_DONE_FIFO));
-}
-
-static int
-cac_pci_l0_intr_pending(sc)
-	struct cac_softc *sc;
-{
-
-	return (cac_inl(sc, CAC_REG_INT_PENDING));
-}
-
-static void
-cac_pci_l0_intr_enable(sc, state)
-	struct cac_softc *sc;
-	int state;
-{
-
-	cac_outl(sc, CAC_REG_INT_MASK, state);
-}
-
-static int
-cac_pci_l0_fifo_full(sc)
-	struct cac_softc *sc;
-{
-
-	return (cac_inl(sc, CAC_REG_CMD_FIFO) == 0);
-}
-
-static void
-cac_pci_l1_submit(sc, addr)
-	struct cac_softc *sc;
-	paddr_t addr;
-{
-
-	cac_outl(sc, CAC_42REG_CMD_FIFO, addr);
-}
-
-static paddr_t
-cac_pci_l1_completed(sc)
-	struct cac_softc *sc;
-{
-	int32_t val;
-
-	if ((val = cac_inl(sc, CAC_42REG_DONE_FIFO)) == -1)
+	if ((off = cac_inl(sc, CAC_42REG_DONE_FIFO)) == 0xffffffffU)
 		return (0);
-	
+
 	cac_outl(sc, CAC_42REG_DONE_FIFO, 0);	
-	return ((paddr_t)val);
+	off = (off & ~3) - sc->sc_ccbs_paddr;
+	ccb = (struct cac_ccb *)(sc->sc_ccbs + off);
+
+	bus_dmamap_sync(sc->sc_dmat, sc->sc_dmamap, off, sizeof(struct cac_ccb),
+	    BUS_DMASYNC_PREWRITE | BUS_DMASYNC_PREREAD);
+
+	return (ccb);
 }
 
 static int
-cac_pci_l1_intr_pending(sc)
-	struct cac_softc *sc;
+cac_pci_l0_intr_pending(struct cac_softc *sc)
 {
 
-	return (cac_inl(sc, CAC_42REG_INT_PENDING) & 
+	return (cac_inl(sc, CAC_42REG_INTR_PENDING) & 
 	    cac_inl(sc, CAC_42REG_STATUS));
 }
 
 static void
-cac_pci_l1_intr_enable(sc, state)
-	struct cac_softc *sc;
-	int state;
+cac_pci_l0_intr_enable(struct cac_softc *sc, int state)
 {
 
-	cac_outl(sc, CAC_REG_INT_MASK, (state ? 0 : 8));	/* XXX */
+	cac_outl(sc, CAC_REG_INTR_MASK, (state ? 0 : 8));	/* XXX */
 }
 
 static int
-cac_pci_l1_fifo_full(sc)
-	struct cac_softc *sc;
+cac_pci_l0_fifo_full(struct cac_softc *sc)
 {
 
 	return (~cac_inl(sc, CAC_42REG_CMD_FIFO));
