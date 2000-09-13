c65b8a8ceb3acd1c15bb7eadda715bc89424bd4b
nisimura <nisimura@NetBSD.org>
2000-09-13 05:23:37 +0000

    Nuke unused obscure #define's.

--- sys/arch/pmax/include/pmap.h
+++ sys/arch/pmax/include/pmap.h
@@ -1,11 +1,3 @@
-/*	$NetBSD: pmap.h,v 1.14 2000/01/09 15:34:43 ad Exp $	*/
-
-#ifndef _PMAX_PMAP_H_
-#define _PMAX_PMAP_H_
+/*	$NetBSD: pmap.h,v 1.15 2000/09/13 05:23:38 nisimura Exp $	*/
 
 #include <mips/pmap.h>
-
-#define pmax_trunc_seg(a)	mips_trunc_seg(a)
-#define pmax_round_seg(a)	mips_round_seg(a)
-
-#endif	/* !_PMAX_PMAP_H_ */
