969473e580a88373237eb8d0c5488d4fde2a7d05
thorpej <thorpej>
2000-09-13 04:47:00 +0000

    Add support for using the 586- and 686-class performance counters.

--- sys/arch/i386/include/sysarch.h
+++ sys/arch/i386/include/sysarch.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: sysarch.h,v 1.9 1998/02/25 21:27:05 perry Exp $	*/
+/*	$NetBSD: sysarch.h,v 1.10 2000/09/13 04:47:01 thorpej Exp $	*/
 
 #ifndef _I386_SYSARCH_H_
 #define _I386_SYSARCH_H_
@@ -12,6 +12,9 @@
 #define	I386_GET_IOPERM	3
 #define	I386_SET_IOPERM	4
 #define	I386_VM86	5
+#define	I386_PMC_INFO	8
+#define	I386_PMC_STARTSTOP 9
+#define	I386_PMC_READ	10
 
 struct i386_get_ldt_args {
 	int start;
@@ -37,12 +40,48 @@ struct i386_set_ioperm_args {
 	u_long *iomap;
 };
 
+struct i386_pmc_info_args {
+	int	type;
+	int	flags;
+};
+
+#define	PMC_TYPE_NONE		0
+#define	PMC_TYPE_I586		1
+#define	PMC_TYPE_I686		2
+
+#define	PMC_INFO_HASTSC		0x01
+
+#define	PMC_NCOUNTERS		2
+
+struct i386_pmc_startstop_args {
+	int counter;
+	u_int64_t val;
+	u_int8_t event;
+	u_int8_t unit;
+	u_int8_t compare;
+	u_int8_t flags;
+};
+
+#define	PMC_SETUP_KERNEL	0x01
+#define	PMC_SETUP_USER		0x02
+#define	PMC_SETUP_EDGE		0x04
+#define	PMC_SETUP_INV		0x08
+
+struct i386_pmc_read_args {
+	int counter;
+	u_int64_t val;
+	u_int64_t time;
+};
+
 #ifndef _KERNEL
 int i386_get_ldt __P((int, union descriptor *, int));
 int i386_set_ldt __P((int, union descriptor *, int));
 int i386_iopl __P((int));
 int i386_get_ioperm __P((u_long *));
 int i386_set_ioperm __P((u_long *));
+int i386_pmc_info __P((struct i386_pmc_info_args *));
+int i386_pmc_startstop __P((struct i386_pmc_startstop_args *));
+int i386_pmc_read __P((struct i386_pmc_read_args *));
 int sysarch __P((int, void *));
 #endif
 
