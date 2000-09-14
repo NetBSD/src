7e7e6afaa840d47315c39fca176c5f6b94dec382
scottr <scottr@NetBSD.org>
2000-09-14 06:35:16 +0000

    Pull up revs 1.69 and 1.70 from scottr:   The last change causes garbage data to be sent.  Revert and   implement a different solution.

--- sys/arch/mac68k/dev/if_ae.c
+++ sys/arch/mac68k/dev/if_ae.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: if_ae.c,v 1.67.26.1 2000/08/06 02:08:01 briggs Exp $	*/
+/*	$NetBSD: if_ae.c,v 1.67.26.2 2000/09/14 06:35:16 scottr Exp $	*/
 
 /*
  * Device driver for National Semiconductor DS8390/WD83C690 based ethernet
@@ -113,11 +113,8 @@ ae_write_mbuf(sc, m, buf)
 	int buf;
 {
 	u_char *data, savebyte[2];
-	u_int16_t *pshort;
 	int len, wantbyte;
 	u_short totlen = 0;
-
-	pshort = (u_int16_t *) &savebyte[0];
 
 	wantbyte = 0;
 
@@ -130,7 +127,7 @@ ae_write_mbuf(sc, m, buf)
 			if (wantbyte) {
 				savebyte[1] = *data;
 				bus_space_write_region_2(sc->sc_buft,
-				    sc->sc_bufh, buf, pshort, 1);
+				    sc->sc_bufh, buf, (u_int16_t *)savebyte, 1);
 				buf += 2;
 				data++;
 				len--;
@@ -138,8 +135,9 @@ ae_write_mbuf(sc, m, buf)
 			}
 			/* Output contiguous words. */
 			if (len > 1) {
-				bus_space_write_region_2(sc->sc_buft,
-				    sc->sc_bufh, buf, pshort, len >> 1);
+				bus_space_write_region_2(
+				    sc->sc_buft, sc->sc_bufh,
+				    buf, (u_int16_t *)data, len >> 1);
 				buf += len & ~1;
 				data += len & ~1;
 				len &= 1;
@@ -155,7 +153,7 @@ ae_write_mbuf(sc, m, buf)
 	if (wantbyte) {
 		savebyte[1] = 0;
 		bus_space_write_region_2(sc->sc_buft, sc->sc_bufh,
-		    buf, pshort, 1);
+		    buf, (u_int16_t *)savebyte, 1);
 	}
 	return (totlen);
 }
