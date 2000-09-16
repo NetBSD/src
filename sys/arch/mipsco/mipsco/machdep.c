511b1ad73a5fd725fbc3e1953fab5b2fb044a4f7
wdk <wdk@NetBSD.org>
2000-09-16 09:18:01 +0000

    Add missing align argument to uvm_map() which was missed by thorpej during original commit.

--- sys/arch/mipsco/mipsco/machdep.c
+++ sys/arch/mipsco/mipsco/machdep.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: machdep.c,v 1.9 2000/09/16 08:34:26 wdk Exp $	*/
+/*	$NetBSD: machdep.c,v 1.10 2000/09/16 09:18:01 wdk Exp $	*/
 
 /*
  * Copyright (c) 1988 University of Utah.
@@ -43,7 +43,7 @@
 
 #include <sys/cdefs.h>			/* RCS ID & Copyright macro defns */
 
-__KERNEL_RCSID(0, "$NetBSD: machdep.c,v 1.9 2000/09/16 08:34:26 wdk Exp $");
+__KERNEL_RCSID(0, "$NetBSD: machdep.c,v 1.10 2000/09/16 09:18:01 wdk Exp $");
 
 /* from: Utah Hdr: machdep.c 1.63 91/04/24 */
 
@@ -417,7 +417,7 @@ cpu_startup()
 	 */
 	size = MAXBSIZE * nbuf;
 	if (uvm_map(kernel_map, (vaddr_t *)&buffers, round_page(size),
-		    NULL, UVM_UNKNOWN_OFFSET,
+		    NULL, UVM_UNKNOWN_OFFSET, 0,
 		    UVM_MAPFLAG(UVM_PROT_NONE, UVM_PROT_NONE, UVM_INH_NONE,
 				UVM_ADV_NORMAL, 0)) != KERN_SUCCESS)
 		panic("startup: cannot allocate VM for buffers");
