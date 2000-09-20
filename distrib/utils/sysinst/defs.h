eb5649b98dc3ad41eeab727d282ffedf59c40b16
hubertf <hubertf>
2000-09-20 20:13:14 +0000

    Pull up to netbsd-1-5 branch, OK'd by thorpej.
    Revs:
     > cvs rdiff -r1.56 -r1.57 basesrc/distrib/utils/sysinst/defs.h
     > cvs rdiff -r1.23 -r1.24 basesrc/distrib/utils/sysinst/target.c
    
    Log:
     >  * make target-routines (target_expand(), and whatnot) work if no root
     >    disk was selected. With this, sysinst can now be used on a "normal"
     >    system to adjust the system's timezone. Use the entry in the "Utilities"
     >    menu for that.
    
    (The test install that I did was actually with a 1.5 system's sources ;-)

--- distrib/utils/sysinst/defs.h
+++ distrib/utils/sysinst/defs.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: defs.h,v 1.53.2.2 2000/09/08 23:57:19 hubertf Exp $	*/
+/*	$NetBSD: defs.h,v 1.53.2.3 2000/09/20 20:13:14 hubertf Exp $	*/
 
 /*
  * Copyright 1997 Piermont Information Systems Inc.
@@ -140,7 +140,7 @@ EXTERN struct disk_desc *disk;
 EXTERN int sectorsize;
 
 /* Actual name of the disk. */
-EXTERN char diskdev[SSTRSIZE];
+EXTERN char diskdev[SSTRSIZE] INIT("");
 EXTERN char disknames[STRSIZE];
 EXTERN int  numdisks;
 EXTERN char *disktype;		/* ST506, SCSI, ... */
