89ce33836b6dfd33f8617a0c3f99f36f756edee3
hubertf <hubertf>
2000-09-20 21:50:47 +0000

    Pull up to netbsd-1-5 branch, OK'd by thorpej:
    Revisions:
     > cvs rdiff -r1.20 -r1.21 basesrc/distrib/utils/sysinst/install.c
     > cvs rdiff -r1.68 -r1.69 basesrc/distrib/utils/sysinst/msg.mi.en
    
    Log:
     > Doesn't it make you nervous to see a "lastchance" warning, telling you
     > that "your hard disk" is about to get nuked, and you are no longer sure
     > which of your ten disks you told sysinst to wipe?
     >
     > Change this to tell you:
     > ``Ok, we are now ready to install NetBSD on your hard disk (wd0). Nothing ...''

--- distrib/utils/sysinst/install.c
+++ distrib/utils/sysinst/install.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: install.c,v 1.19.10.1 2000/08/15 02:12:52 hubertf Exp $	*/
+/*	$NetBSD: install.c,v 1.19.10.2 2000/09/20 21:50:47 hubertf Exp $	*/
 
 /*
  * Copyright 1997 Piermont Information Systems Inc.
@@ -87,7 +87,7 @@ do_install()
 	/* Last chance ... do you really want to do this? */
 	clear();
 	refresh();
-	msg_display(MSG_lastchance);
+	msg_display(MSG_lastchance, diskdev);
 	process_menu(MENU_noyes);
 	if (!yesno)
 		return;
