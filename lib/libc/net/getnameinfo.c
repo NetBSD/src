2607e617731dd23dbf0cb11d7062a658538abfb7
itojun <itojun>
2000-09-25 23:07:28 +0000

    off-by-1 error in string length validation. From: Pavel Kankovsky <peak@argo.troja.mff.cuni.cz>

--- lib/libc/net/getnameinfo.c
+++ lib/libc/net/getnameinfo.c
@@ -1,5 +1,5 @@
-/*	$NetBSD: getnameinfo.c,v 1.21 2000/08/07 16:18:45 itojun Exp $	*/
-/*	$KAME: getnameinfo.c,v 1.44 2000/08/06 16:43:17 itojun Exp $	*/
+/*	$NetBSD: getnameinfo.c,v 1.22 2000/09/25 23:07:28 itojun Exp $	*/
+/*	$KAME: getnameinfo.c,v 1.45 2000/09/25 22:43:56 itojun Exp $	*/
 
 /*
  * Copyright (C) 1995, 1996, 1997, and 1998 WIDE Project.
@@ -44,7 +44,7 @@
 
 #include <sys/cdefs.h>
 #if defined(LIBC_SCCS) && !defined(lint)
-__RCSID("$NetBSD: getnameinfo.c,v 1.21 2000/08/07 16:18:45 itojun Exp $");
+__RCSID("$NetBSD: getnameinfo.c,v 1.22 2000/09/25 23:07:28 itojun Exp $");
 #endif /* LIBC_SCCS and not lint */
 
 #include "namespace.h"
@@ -163,12 +163,12 @@ getnameinfo(sa, salen, host, hostlen, serv, servlen, flags)
 				(flags & NI_DGRAM) ? "udp" : "tcp");
 		}
 		if (sp) {
-			if (strlen(sp->s_name) > servlen)
+			if (strlen(sp->s_name) + 1 > servlen)
 				return ENI_MEMORY;
 			strcpy(serv, sp->s_name);
 		} else {
 			snprintf(numserv, sizeof(numserv), "%d", ntohs(port));
-			if (strlen(numserv) > servlen)
+			if (strlen(numserv) + 1 > servlen)
 				return ENI_MEMORY;
 			strcpy(serv, numserv);
 		}
@@ -263,7 +263,10 @@ getnameinfo(sa, salen, host, hostlen, serv, servlen, flags)
 					*p = '\0';
 			}
 #endif
-			if (strlen(hp->h_name) > hostlen) {
+			if (strlen(hp->h_name) + 1 > hostlen) {
+#ifdef USE_GETIPNODEBY
+				freehostent(hp);
+#endif
 				return ENI_MEMORY;
 			}
 			strcpy(host, hp->h_name);
