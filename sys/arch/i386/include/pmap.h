f41561e9de39f18500c3bc71330dbaa519d39604
is <is@NetBSD.org>
2000-09-28 13:09:10 +0000

    We do wired page accounting. Tell UVM about it.

--- sys/arch/i386/include/pmap.h
+++ sys/arch/i386/include/pmap.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: pmap.h,v 1.49 2000/09/21 17:46:05 thorpej Exp $	*/
+/*	$NetBSD: pmap.h,v 1.50 2000/09/28 13:17:54 is Exp $	*/
 
 /*
  *
@@ -353,6 +353,7 @@ extern int pmap_pg_g;			/* do we support PG_G? */
 
 #define	pmap_kernel()			(&kernel_pmap_store)
 #define	pmap_resident_count(pmap)	((pmap)->pm_stats.resident_count)
+#define	pmap_wired_count(pmap)		((pmap)->pm_stats.wired_count)
 #define	pmap_update()			tlbflush()
 
 #define pmap_clear_modify(pg)		pmap_change_attrs(pg, 0, PG_M)
