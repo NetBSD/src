772e8b73a3e72f48379f97c4fcdb5b429cec3c83
itohy <itohy@NetBSD.org>
2000-09-28 03:41:50 +0000

    Fix the type of pmap_zero_page_uncached from void (paddr_t) to boolean_t (paddr_t) to make it compile again.

--- sys/arch/mvme68k/mvme68k/pmap.c
+++ sys/arch/mvme68k/mvme68k/pmap.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: pmap.c,v 1.46 2000/09/22 08:27:00 scw Exp $        */
+/*	$NetBSD: pmap.c,v 1.47 2000/09/28 03:49:28 itohy Exp $        */
 
 /*-
  * Copyright (c) 1999 The NetBSD Foundation, Inc.
@@ -1659,7 +1659,7 @@ pmap_zero_page(phys)
  *	through PMAP_PAGEIDLEZERO macro.  Returns TRUE if the page
  *	was zero'd, FALSE if we aborted.
  */
-void
+boolean_t
 pmap_zero_page_uncached(phys)
 	paddr_t phys;
 {
