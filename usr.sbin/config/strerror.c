9b15926f7d0fa4983d06194ada6d4586960510e4
cgd <cgd@NetBSD.org>
2000-10-02 19:48:35 +0000

    nuke __P, and convert all functions to use ANSI-style declarations. Also, do a few trivial KNF cleanups (e.g. newline at start of fn if no locals).  Verified to have no effect via diff on new and old compiled binaries.

---
+++ usr.sbin/config/strerror.c
@@ -0,0 +1,22 @@
+/*	$NetBSD: strerror.c,v 1.3.2.2 2000/10/02 19:48:36 cgd Exp $	*/
+
+/*
+ * strerror() - for those systems that don't have it yet.
+ */
+
+/* These are part of the C library. (See perror.3) */
+extern char *sys_errlist[];
+extern int sys_nerr;
+
+static char errmsg[80];
+
+char *
+strerror(int en)
+{
+
+    if ((0 <= en) && (en < sys_nerr))
+	return sys_errlist[en];
+
+    sprintf(errmsg, "Error %d", en);
+    return errmsg;
+}
