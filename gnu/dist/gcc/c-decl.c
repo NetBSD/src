008bb3b0843791f1570fb848066d4c2a4ee3172f
sommerfeld <sommerfeld@NetBSD.org>
2000-10-05 13:08:57 +0000

    Enhanced format checking, mk II: New option -Wno-format-extra-args : suppress warning for: 	printf("foo", bar); since that isn't interesting from the POV of this audit, and it's more common than most in some code.
    also, do format args checking for
    	printf(Aflag ? "format 1: %s" : "format 2: %s", bar);
    albeit in a half-hearted way for now (just check the "then" case)

--- gnu/dist/gcc/c-decl.c
+++ gnu/dist/gcc/c-decl.c
@@ -552,6 +552,7 @@ int warn_nested_externs = 0;
 
 int warn_format;
 int warn_format_y2k;
+int warn_format_extra_args;
 
 /* Warn about a subscript that has type char.  */
 
@@ -756,9 +757,11 @@ c_decode_option (argc, argv)
   else if (!strcmp (p, "-Wnetbsd-format-audit"))
     warn_format = 2;
   else if (!strcmp (p, "-Wformat"))
-    warn_format = warn_format_y2k = 1;
+    warn_format = warn_format_y2k = warn_format_extra_args = 1;
   else if (!strcmp (p, "-Wno-format"))
     warn_format = warn_format_y2k = 0;
+  else if (!strcmp (p, "-Wno-format-extra-args"))
+    warn_format_extra_args = 0;
   else if (!strcmp (p, "-Wno-format-y2k"))
     warn_format_y2k = 0;
   else if (!strcmp (p, "-Wchar-subscripts"))
