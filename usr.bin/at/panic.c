1492702b6e2f01218bb9c79972f708226fe228a2
kleink <kleink@NetBSD.org>
2000-10-15 14:51:14 +0000

    Add support for the 1003.2-92 -t option to at(1) using which the time may be specified in a language-neutral way.  For symmetry, extend this to the batch(1) front-end as well.  Addresses PR standards/11205.

--- usr.bin/at/panic.c
+++ usr.bin/at/panic.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: panic.c,v 1.8 2000/10/04 19:24:59 mjl Exp $	*/
+/*	$NetBSD: panic.c,v 1.9 2000/10/15 14:51:14 kleink Exp $	*/
 
 /*
  * panic.c - terminate fast in case of error
@@ -45,7 +45,7 @@
 #if 0
 static char rcsid[] = "$OpenBSD: panic.c,v 1.4 1997/03/01 23:40:09 millert Exp $";
 #else
-__RCSID("$NetBSD: panic.c,v 1.8 2000/10/04 19:24:59 mjl Exp $");
+__RCSID("$NetBSD: panic.c,v 1.9 2000/10/15 14:51:14 kleink Exp $");
 #endif
 #endif
 
@@ -99,10 +99,12 @@ usage(void)
 {
 
 	/* Print usage and exit.  */
-	(void)fprintf(stderr,   "Usage: at [-V] [-q x] [-f file] [-m] time\n"
+	(void)fprintf(stderr,   "Usage: at [-V] [-q x] [-f file] [-m] -t [[CC]YY]MMDDhhmm[.SS]\n"
+				"       at [-V] [-q x] [-f file] [-m] timespec\n"
 				"       at [-V] -c job [job ...]\n"
 				"       atq [-V] [-q x] [-v]\n"
 				"       atrm [-V] job [job ...]\n"
-				"       batch [-V] [-f file] [-m]\n");
+				"       batch [-V] [-f file] [-m] [-t [[CC]YY]MMDDhhmm[.SS]]\n"
+				"       batch [-V] [-f file] [-m] [timespec]\n");
 	exit(EXIT_FAILURE);
 }
