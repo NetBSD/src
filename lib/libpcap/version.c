5bf8379bd2278d3c75ed1811231bad13ddc483c6
tv <tv@NetBSD.org>
2000-10-16 22:46:04 +0000

    Pullup 1.3 [itojun]: we already have all fixes toward 0.4 (v0.4 Sat Jul 25 12:40:09 PDT 1998 - note that there are MULTIPLE 0.4), so update version identification.

--- lib/libpcap/version.c
+++ lib/libpcap/version.c
@@ -1,2 +1,2 @@
-/*	$NetBSD: version.c,v 1.1.12.1 2000/07/03 22:32:37 thorpej Exp $	*/
-char pcap_version[] = "0.4a3";
+/*	$NetBSD: version.c,v 1.1.12.2 2000/10/16 22:46:04 tv Exp $	*/
+char pcap_version[] = "0.4";
