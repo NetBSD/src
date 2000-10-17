2b1d7fd29dd3e0197d58464fc3064f5144c86faf
thorpej <thorpej@NetBSD.org>
2000-10-17 02:57:01 +0000

    Add an IP_MTUDISC flag to the flags that can be passed to ip_output().  This flag, if set, causes ip_output() to set DF in the IP header if the MTU in the route is not locked.
    This allows a bunch of redundant code, which I was never
    really all that happy about adding in the first place, to
    be eliminated.
    
    Inspired by a similar change made by provos@openbsd.org when
    he integrated NetBSD's Path MTU Discovery code into OpenBSD.

--- sys/netinet/ip_output.c
+++ sys/netinet/ip_output.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: ip_output.c,v 1.75 2000/06/28 02:59:32 mrg Exp $	*/
+/*	$NetBSD: ip_output.c,v 1.76 2000/10/17 02:57:01 thorpej Exp $	*/
 
 /*
  * Copyright (C) 1995, 1996, 1997, and 1998 WIDE Project.
@@ -415,6 +415,14 @@ ip_output(m0, va_alist)
 		m->m_flags &= ~M_BCAST;
 
 sendit:
+	/*
+	 * If we're doing Path MTU Discovery, we need to set DF unless
+	 * the route's MTU is locked.
+	 */
+	if ((flags & IP_MTUDISC) != 0 && ro->ro_rt != NULL &&
+	    (ro->ro_rt->rt_rmx.rmx_locks & RTV_MTU) == 0)
+		ip->ip_off |= IP_DF;
+
 #ifdef PFIL_HOOKS
 	/*
 	 * Run through list of hooks for output packets.
