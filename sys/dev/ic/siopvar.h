e89eba33623454d2ca2e9baec8cfea1983ab0061
bouyer <bouyer@NetBSD.org>
2000-10-18 17:06:52 +0000

    Adapt for new lun switch script. Implement SCBUSACCEL ioctl. Snapshot of work in progress on tagged queuing: we can send/receive queue tag messages. Infrastructure to manage multiple commands per devices not here yet.

--- sys/dev/ic/siopvar.h
+++ sys/dev/ic/siopvar.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: siopvar.h,v 1.10 2000/10/06 16:39:04 bouyer Exp $	*/
+/*	$NetBSD: siopvar.h,v 1.11 2000/10/18 17:06:52 bouyer Exp $	*/
 
 /*
  * Copyright (c) 2000 Manuel Bouyer.
@@ -34,6 +34,7 @@
 
 TAILQ_HEAD(cmd_list, siop_cmd);
 TAILQ_HEAD(cbd_list, siop_cbd);
+TAILQ_HEAD(lunsw_list, siop_lunsw);
 
 /* Driver internal state */
 struct siop_softc {
@@ -55,15 +56,16 @@ struct siop_softc {
 	bus_dma_tag_t sc_dmat;		/* bus DMA tag */
 	void (*sc_reset) __P((struct siop_softc*)); /* reset callback */
 	bus_dmamap_t  sc_scriptdma;	/* DMA map for script */
-	bus_dmamap_t  sc_scheddma;	/* DMA map for sched and resel script */
+	bus_dmamap_t  sc_scheddma;	/* DMA map for sched script */
 	u_int32_t *sc_script;		/* script location in memory */
 	u_int32_t *sc_sched;		/* script scheduler location in mem */
-	u_int32_t *sc_resel;		/* reselect script location in mem */
 	int sc_nschedslots;		/* number of scheduler slots */
 	int sc_currschedslot;		/* current scheduler slot */
-	int sc_nreselslots;		/* number of reselect slots */
 	struct cbd_list cmds;		/* list of command block descriptors */
 	struct cmd_list free_list;	/* cmd descr free list */
+	struct cmd_list ready_list;	/* cmd descr ready list */
+	struct lunsw_list lunsw_list;	/* lunsw free list */
+	u_int32_t ram_free;		/* free ram offset from sc_scriptaddr */
 	struct siop_target *targets[16]; /* per-target states */
 	u_int32_t sc_flags;
 };
