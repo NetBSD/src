7da8c5f2d5a849df58cbecc762c3d27d8291d403
tv <tv@NetBSD.org>
2000-10-18 00:15:17 +0000

    Pullup 1.10 [he]: Use %lx instead of %x with _kvm_err when arg is u_long.

--- lib/libkvm/kvm_m68k_cmn.c
+++ lib/libkvm/kvm_m68k_cmn.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: kvm_m68k_cmn.c,v 1.9 1999/07/02 15:28:50 simonb Exp $	*/
+/*	$NetBSD: kvm_m68k_cmn.c,v 1.9.8.1 2000/10/18 00:15:17 tv Exp $	*/
 
 /*-
  * Copyright (c) 1997 Jason R. Thorpe.  All rights reserved.
@@ -43,7 +43,7 @@
 #if 0
 static char sccsid[] = "@(#)kvm_hp300.c	8.1 (Berkeley) 6/4/93";
 #else
-__RCSID("$NetBSD: kvm_m68k_cmn.c,v 1.9 1999/07/02 15:28:50 simonb Exp $");
+__RCSID("$NetBSD: kvm_m68k_cmn.c,v 1.9.8.1 2000/10/18 00:15:17 tv Exp $");
 #endif
 #endif /* LIBC_SCCS and not lint */
 
@@ -222,7 +222,7 @@ vatop_030(kd, stpa, va, pa)
 
 	return (h->page_size - offset);
 invalid:
-	_kvm_err(kd, 0, "invalid address (%x)", va);
+	_kvm_err(kd, 0, "invalid address (%lx)", va);
 	return (0);
 }
 
@@ -307,6 +307,6 @@ vatop_040(kd, stpa, va, pa)
 	return (h->page_size - offset);
 
 invalid:
-	_kvm_err(kd, 0, "invalid address (%x)", va);
+	_kvm_err(kd, 0, "invalid address (%lx)", va);
 	return (0);
 }
