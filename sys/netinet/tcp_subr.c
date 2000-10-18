ca1bfcaa8ee02d405b3197daf4c932d1c6c8b884
itojun <itojun@NetBSD.org>
2000-10-18 07:21:10 +0000

    move tcp syn cache parameters from in_proto.c to tcp_subr.c. it makes more sense and helps INET6-only (INET-less) build.

--- sys/netinet/tcp_subr.c
+++ sys/netinet/tcp_subr.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: tcp_subr.c,v 1.96 2000/10/17 03:06:43 itojun Exp $	*/
+/*	$NetBSD: tcp_subr.c,v 1.97 2000/10/18 07:21:10 itojun Exp $	*/
 
 /*
  * Copyright (C) 1995, 1996, 1997, and 1998 WIDE Project.
@@ -173,11 +173,22 @@ int	tcp_compat_42 = 1;
 #else
 int	tcp_compat_42 = 0;
 #endif
+int	tcp_rst_ppslim = 100;	/* 100pps */
 
+/* tcb hash */
 #ifndef TCBHASHSIZE
 #define	TCBHASHSIZE	128
 #endif
 int	tcbhashsize = TCBHASHSIZE;
+
+/* syn hash parameters */
+#define	TCP_SYN_HASH_SIZE	293
+#define	TCP_SYN_BUCKET_SIZE	35
+int	tcp_syn_cache_size = TCP_SYN_HASH_SIZE;
+int	tcp_syn_cache_limit = TCP_SYN_HASH_SIZE*TCP_SYN_BUCKET_SIZE;
+int	tcp_syn_bucket_limit = 3*TCP_SYN_BUCKET_SIZE;
+struct	syn_cache_head tcp_syn_cache[TCP_SYN_HASH_SIZE];
+int	tcp_syn_cache_interval = 1;	/* runs timer twice a second */
 
 int	tcp_freeq __P((struct tcpcb *));
 
