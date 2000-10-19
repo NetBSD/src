87f9d4cee94b708b39363b32eecaab181183f45b
he <he@NetBSD.org>
2000-10-19 17:05:04 +0000

    Pull up revision 1.4 (requested by he):   Format string cleanup.

--- usr.sbin/bootp/common/report.h
+++ usr.sbin/bootp/common/report.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: report.h,v 1.3 1998/07/26 15:33:34 mycroft Exp $	*/
+/*	$NetBSD: report.h,v 1.3.2.1 2000/10/19 17:05:04 he Exp $	*/
 
 /* report.h */
 
@@ -9,7 +9,8 @@
 #endif
 
 extern void report_init P((int nolog));
-extern void report P((int, char *, ...));
+extern void report P((int, const char *, ...))
+     __attribute__((__format__(__printf__, 2, 3)));
 extern const char *get_errmsg P((void));
 
 #undef P
