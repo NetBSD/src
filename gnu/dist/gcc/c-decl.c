e745db48855c91c3e9d7964284cbc7b9dfb96a48
he <he@NetBSD.org>
2000-10-19 15:58:39 +0000

    Pull up revisions 1.7-1.9 (requested by is):   Implement enhanced format string checking.  New options:   -Wnetbsd-format-audit and -Wno-format-extra-args.

--- gnu/dist/gcc/c-decl.c
+++ gnu/dist/gcc/c-decl.c
@@ -552,6 +552,7 @@ int warn_nested_externs = 0;
 
 int warn_format;
 int warn_format_y2k;
+int warn_format_extra_args;
 
 /* Warn about a subscript that has type char.  */
 
@@ -753,10 +754,17 @@ c_decode_option (argc, argv)
     warn_traditional = 1;
   else if (!strcmp (p, "-Wno-traditional"))
     warn_traditional = 0;
+  else if (!strcmp (p, "-Wnetbsd-format-audit"))
+    warn_format = MAX(warn_format, 2);
   else if (!strcmp (p, "-Wformat"))
-    warn_format = warn_format_y2k = 1;
+    {
+      warn_format_y2k = warn_format_extra_args = 1;
+      warn_format = MAX(warn_format, 1);
+    }
   else if (!strcmp (p, "-Wno-format"))
     warn_format = warn_format_y2k = 0;
+  else if (!strcmp (p, "-Wno-format-extra-args"))
+    warn_format_extra_args = 0;
   else if (!strcmp (p, "-Wno-format-y2k"))
     warn_format_y2k = 0;
   else if (!strcmp (p, "-Wchar-subscripts"))
@@ -832,7 +840,8 @@ c_decode_option (argc, argv)
       warn_return_type = 1;
       warn_unused = 1;
       warn_switch = 1;
-      warn_format = warn_format_y2k = 1;
+      warn_format_y2k = 1;
+      warn_format = MAX(warn_format, 1);
       warn_char_subscripts = 1;
       warn_parentheses = 1;
       warn_missing_braces = 1;
