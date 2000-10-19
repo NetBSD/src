3d4d2a7c5c166232cfba8da991c0ae4db7286e98
he <he@NetBSD.org>
2000-10-19 18:23:01 +0000

    Pull up revision 1.8 (requested by he):   Fix pointer/format problem, cast to u_long, print with %lx.

--- usr.bin/systat/fetch.c
+++ usr.bin/systat/fetch.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: fetch.c,v 1.4.2.1 1999/09/26 13:35:57 he Exp $	*/
+/*	$NetBSD: fetch.c,v 1.4.2.2 2000/10/19 18:23:01 he Exp $	*/
 
 /*-
  * Copyright (c) 1980, 1992, 1993
@@ -38,7 +38,7 @@
 #if 0
 static char sccsid[] = "@(#)fetch.c	8.1 (Berkeley) 6/6/93";
 #endif
-__RCSID("$NetBSD: fetch.c,v 1.4.2.1 1999/09/26 13:35:57 he Exp $");
+__RCSID("$NetBSD: fetch.c,v 1.4.2.2 2000/10/19 18:23:01 he Exp $");
 #endif /* not lint */
 
 #include <sys/types.h>
@@ -52,7 +52,8 @@ kvm_ckread(a, b, l)
 {
 	if (kvm_read(kd, (u_long)a, b, l) != l) {
 		if (verbose)
-			error("error reading kmem at %x\n", a);
+			error("error reading kmem at %lx\n",
+			   (u_long) a);
 		return (0);
 	} else
 		return (1);
