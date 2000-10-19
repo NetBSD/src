38ed56a0f369eb7d45cfe6a08d03e5f31edd1081
he <he@NetBSD.org>
2000-10-19 16:32:56 +0000

    Pull up revision 1.4 (requested by he):   Format string cleanup.

--- usr.bin/top/display.h
+++ usr.bin/top/display.h
@@ -27,7 +27,8 @@ void i_process __P((int, char *));
 void u_process __P((int, char *));
 void u_endscreen __P((int));
 void display_header __P((int));
-void new_message __P((int, const char *, ...));
+void new_message __P((int, const char *, ...))
+     __attribute__((__format__(__printf__, 2, 3)));
 void clear_message __P((void));
 int readline __P((char *, int, int));
 char *printable __P((char *));
