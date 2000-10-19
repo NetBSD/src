38b4a02544d873850c918a26c977c8012b326907
he <he@NetBSD.org>
2000-10-19 17:05:51 +0000

    Pull up revision 1.2 (requested by he):   Format string cleanup.

--- usr.sbin/tcpdchk/percent_m.h
+++ usr.sbin/tcpdchk/percent_m.h
@@ -1,3 +1,4 @@
-/*	$NetBSD: percent_m.h,v 1.1 1997/10/11 21:41:41 christos Exp $	*/
+/*	$NetBSD: percent_m.h,v 1.1.4.1 2000/10/19 17:05:51 he Exp $	*/
 
-char *percent_m __P((char *, const char *));
+char *percent_m __P((char *, const char *))
+	__attribute__((__format_arg__(2)));
