fc947ec5d9833a1ae1dc632fe51cd6d01b2a382e
uch <uch>
2000-10-22 10:33:01 +0000

    reverse color support.

--- sys/arch/hpcmips/dev/video_subr.h
+++ sys/arch/hpcmips/dev/video_subr.h
@@ -1,7 +1,11 @@
-/*	$NetBSD: video_subr.h,v 1.3 2000/05/22 17:17:44 uch Exp $	*/
+/*	$NetBSD: video_subr.h,v 1.4 2000/10/22 10:33:01 uch Exp $	*/
 
 /*-
- * Copyright (c) 2000 UCHIYAMA Yasushi.  All rights reserved.
+ * Copyright (c) 2000 The NetBSD Foundation, Inc.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to The NetBSD Foundation
+ * by UCHIYAMA Yasushi.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -11,19 +15,25 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. The name of the author may not be used to endorse or promote products
- *    derived from this software without specific prior written permission.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *        This product includes software developed by the NetBSD
+ *        Foundation, Inc. and its contributors.
+ * 4. Neither the name of The NetBSD Foundation nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
  *
- * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
- * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
- * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
- * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
- * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
  */
 
 #define LEGAL_CLUT_INDEX(x)	((x) >= 0 && (x) <= 255)
@@ -31,12 +41,10 @@
 				 (((g) << 8) & 0x0000ff00) |		\
 				 (((b)) & 0x000000ff))
 
-int cmap_work_alloc __P((u_int8_t **, u_int8_t **, u_int8_t **, u_int32_t **,
-			 int));
-void cmap_work_free __P((u_int8_t *, u_int8_t *, u_int8_t *, u_int32_t *));
-void rgb24_compose __P((u_int32_t *, u_int8_t *, u_int8_t *, u_int8_t *, int)); 
-void rgb24_decompose __P((u_int32_t *, u_int8_t *, u_int8_t *, u_int8_t *,
-			  int));
+int cmap_work_alloc(u_int8_t **, u_int8_t **, u_int8_t **, u_int32_t **, int);
+void cmap_work_free(u_int8_t *, u_int8_t *, u_int8_t *, u_int32_t *);
+void rgb24_compose(u_int32_t *, u_int8_t *, u_int8_t *, u_int8_t *, int); 
+void rgb24_decompose(u_int32_t *, u_int8_t *, u_int8_t *, u_int8_t *, int);
 
 /* debug function for TX. VR will need `line bytes' */
 struct video_chip;
@@ -50,11 +58,12 @@ struct video_chip {
 	int vc_fbwidth;
 	int vc_fbheight;
 
-	void (*vc_drawline) __P((struct video_chip *, int, int, int, int));
-	void (*vc_drawdot) __P((struct video_chip *, int, int));
+	void (*vc_drawline)(struct video_chip *, int, int, int, int);
+	void (*vc_drawdot)(struct video_chip *, int, int);
 };
 
-void video_attach_drawfunc __P((struct video_chip *));
-void video_line __P((struct video_chip *, int, int, int, int));
-void video_dot __P((struct video_chip *, int, int));
-void video_calibration_pattern __P((struct video_chip *));
+void video_attach_drawfunc(struct video_chip *);
+void video_line(struct video_chip *, int, int, int, int);
+void video_dot(struct video_chip *, int, int);
+void video_calibration_pattern(struct video_chip *);
+int video_reverse_color(void);
