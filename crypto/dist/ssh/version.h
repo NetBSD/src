5f344262c9528fc7df6a3dcc7ec13e41444e6000
itojun <itojun@NetBSD.org>
2000-10-28 13:41:55 +0000

    make version identification string conform to SSH version string format. version format must be like: 	SSH-[0-9]*.[0-9]*-[^-]*( .*)? and previous string did not conform to the requirement (too many hyphens). based on comment from markus@openbsd (openssh maintainer)

--- crypto/dist/ssh/version.h
+++ crypto/dist/ssh/version.h
@@ -1,3 +1,10 @@
-/*	$NetBSD: version.h,v 1.2 2000/10/03 09:56:38 lukem Exp $	*/
+/*	$NetBSD: version.h,v 1.3 2000/10/28 13:41:55 itojun Exp $	*/
 
-#define SSH_VERSION	"NetBSD_Secure_Shell-20001003"
+#define __OPENSSH_VERSION	"OpenSSH_2.2.0"
+#define __NETBSDSSH_VERSION	"NetBSD_Secure_Shell-20001003"
+
+/*
+ * it is important to retain OpenSSH version identification part, it is
+ * used for bug compatibility operation.  present NetBSD SSH version as comment.
+ */
+#define SSH_VERSION	(__OPENSSH_VERSION " " __NETBSDSSH_VERSION)
