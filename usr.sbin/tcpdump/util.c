2f253cca577154b25bf003c15589501c23c7b4c0
he <he@NetBSD.org>
2000-10-31 13:54:15 +0000

    Pull up revision 1.8 (requested by he):   Change from sprintf() to snprintf() where we may print externally   supplied data of unknown length.

--- usr.sbin/tcpdump/util.c
+++ usr.sbin/tcpdump/util.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: util.c,v 1.6 1997/10/03 19:56:03 christos Exp $	*/
+/*	$NetBSD: util.c,v 1.6.4.1 2000/10/31 13:54:15 he Exp $	*/
 
 /*
  * Copyright (c) 1990, 1991, 1993, 1994, 1995, 1996, 1997
@@ -27,7 +27,7 @@
 static const char rcsid[] =
     "@(#) Header: util.c,v 1.58 97/05/09 14:52:17 leres Exp  (LBL)";
 #else
-__RCSID("$NetBSD: util.c,v 1.6 1997/10/03 19:56:03 christos Exp $");
+__RCSID("$NetBSD: util.c,v 1.6.4.1 2000/10/31 13:54:15 he Exp $");
 #endif
 #endif
 
@@ -161,7 +161,7 @@ tok2str(register const struct tok *lp, register const char *fmt,
 	}
 	if (fmt == NULL)
 		fmt = "#%d";
-	(void)sprintf(buf, fmt, v);
+	(void)snprintf(buf, sizeof(buf), fmt, v);
 	return (buf);
 }
 
