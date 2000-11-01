f36461d48aa31afa35b53dcfa824ec8178bb9a5d
wiz <wiz@NetBSD.org>
2000-11-01 14:34:14 +0000

    Use NetBSD's getopt{,_long}.

--- gnu/dist/diffutils/diff.c
+++ gnu/dist/diffutils/diff.c
@@ -23,7 +23,7 @@ the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
 #define GDIFF_MAIN
 #include "diff.h"
 #include <signal.h>
-#include "getopt.h"
+#include <getopt.h>
 #include "fnmatch.h"
 
 #ifndef DEFAULT_WIDTH
