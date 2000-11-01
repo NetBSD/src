251543770c8cd8c6f9e08f5685b88676a521f0c1
tv <tv@NetBSD.org>
2000-11-01 16:13:48 +0000

    Pullup 1.82 [tsubai, mycroft]: Enable console in initppc().

--- sys/arch/macppc/macppc/machdep.c
+++ sys/arch/macppc/macppc/machdep.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: machdep.c,v 1.74 2000/06/09 10:54:48 tsubai Exp $	*/
+/*	$NetBSD: machdep.c,v 1.74.2.1 2000/11/01 16:13:48 tv Exp $	*/
 
 /*
  * Copyright (C) 1995, 1996 Wolfgang Solfrank.
@@ -231,15 +231,6 @@ initppc(startkernel, endkernel, args)
 
 	curpm = curpcb->pcb_pmreal = curpcb->pcb_pm = pmap_kernel();
 
-#if 0
-	/*
-	 * i386 port says, that this shouldn't be here,
-	 * but I really think the console should be initialized
-	 * as early as possible.
-	 */
-	consinit();
-#endif
-
 #ifdef	__notyet__		/* Needs some rethinking regarding real/virtual OFW */
 	OF_set_callback(callback);
 #endif
@@ -305,6 +296,13 @@ initppc(startkernel, endkernel, args)
 		      : "=r"(scratch) : "K"(PSL_IR|PSL_DR|PSL_ME|PSL_RI));
 
 	ofmsr &= ~PSL_IP;
+
+	/*
+	 * i386 port says, that this shouldn't be here,
+	 * but I really think the console should be initialized
+	 * as early as possible.
+	 */
+	consinit();
 
 	/*
 	 * Parse arg string.
