ef14cfbe4bf371e39f2948c1d68d32654b92b4b1
itojun <itojun@NetBSD.org>
2000-11-02 01:09:01 +0000

    build framework for GNU gettext.  will be overridden whenever Todd's BSD licensed gettext implementation comes in.

---
+++ gnu/usr.bin/gettext/include/libgettext.h
@@ -0,0 +1,7 @@
+#include <libintl.h>
+
+#define gettext__	gettext
+#define dgettext__	dgettext
+#define dcgettext__	dcgettext
+#define textdomain__	textdomain
+#define bindtextdomain__	bindtextdomain
