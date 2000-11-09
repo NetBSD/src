c76730e045fff185b646c22d5dd08f3aebe9eacf
tv <tv>
2000-11-09 23:16:23 +0000

    Pullup 1.37 [thorpej]: One more fix to multicast hash computation.  Submitter confirms that with this fix, multicast does indeed work properly on the EPIC.
    From Zdenek Salvet <salvet@ics.muni.cz>.

--- sys/dev/ic/smc83c170.c
+++ sys/dev/ic/smc83c170.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: smc83c170.c,v 1.32 2000/05/26 00:14:41 tsutsui Exp $	*/
+/*	$NetBSD: smc83c170.c,v 1.32.4.1 2000/11/09 23:16:23 tv Exp $	*/
 
 /*-
  * Copyright (c) 1998, 1999 The NetBSD Foundation, Inc.
@@ -1361,7 +1361,8 @@ epic_set_mchash(sc)
 			goto allmulti;
 		}
 
-		hash = ether_crc32_le(enm->enm_addrlo, ETHER_ADDR_LEN) & 0x3f;
+		hash = ether_crc32_be(enm->enm_addrlo, ETHER_ADDR_LEN);
+		hash >>= 26;
 
 		/* Set the corresponding bit in the hash table. */
 		mchash[hash >> 4] |= 1 << (hash & 0xf);
