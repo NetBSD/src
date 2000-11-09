cfe8f47d6ceeafd3dd05dcbebd01624cdf6b8763
tsutsui <tsutsui>
2000-11-09 14:38:44 +0000

    On sun3x, cpu_reboot() calls sunmon_abort() for PROM bug work-around, but sunmon_abort() does not restore mon_crp so it causes problems. Instead, call romVectorPtr->abortEntry (via trap14) from sunmon_halt() and call it from cpu_reboot().
    Now PROM boot command works properly even after halt on my 3/80.

--- sys/arch/sun3/sun3/sunmon.c
+++ sys/arch/sun3/sun3/sunmon.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: sunmon.c,v 1.10 2000/09/24 12:32:39 jdolecek Exp $	*/
+/*	$NetBSD: sunmon.c,v 1.11 2000/11/09 14:38:44 tsutsui Exp $	*/
 
 /*-
  * Copyright (c) 1996 The NetBSD Foundation, Inc.
@@ -117,6 +117,12 @@ void sunmon_halt()
 	*romVectorPtr->vector_cmd = sunmon_vcmd;
 #ifdef	_SUN3X_
 	loadcrp(&mon_crp);
+	/*
+	 * The PROM monitor "exit_to_mon" function appears to have problems...
+	 * SunOS uses the "abort" function when you halt (bug work-around?)
+	 * so we might as well do the same.
+	 */
+	asm(" trap #14"); /* mon_exit_to_mon() provokes PROM monitor bug */
 #endif
 	mon_exit_to_mon();
 	/*NOTREACHED*/
