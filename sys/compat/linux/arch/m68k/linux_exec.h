dd0ea4681702a9a1bcae5b462bb2efa76a4f2c8f
erh <erh@NetBSD.org>
2000-11-17 03:55:17 +0000

    Make the linux compatibilty code work on the alpha.  (horay!) (at last!) Two main changes: 	Create a linux_elf64_copyargs that uses the linux specific LinuxAuxInfo 	structure.  This is only used on the alpha.  i386 and m68k use the 	standard elf copyargs function.
    	Since linux's approach to binary compatibilty is to look as much
    	like osf1 as possible, add all the osf1 syscalls that we have
    	implemented to the linux syscall table.  This includes get/setsysinfo,
    	ported from FreeBSD.
    
    In order for linux compat to work you must have COMPAT_OSF1, COMPAT_43,
    COMPAT_09, COMPAT_12 and COMPAT_13 on also.

--- sys/compat/linux/arch/m68k/linux_exec.h
+++ sys/compat/linux/arch/m68k/linux_exec.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: linux_exec.h,v 1.1 1998/12/15 19:25:40 itohy Exp $	*/
+/*	$NetBSD: linux_exec.h,v 1.2 2000/11/17 03:55:18 erh Exp $	*/
 
 /*-
  * Copyright (c) 1998 The NetBSD Foundation, Inc.
@@ -46,5 +46,9 @@
  * Linux Elf32 format parameters
  */
 #define LINUX_GCC_SIGNATURE	1
+
+#define LINUX_ELF_AUX_ARGSIZ \
+	(howmany(ELF_AUX_ENTRIES * sizeof(AuxInfo), sizeof(Elf_Addr)))
+#define LINUX_COPYARGS_FUNCTION	ELFNAME(copyargs)
 
 #endif /* !_M68K_LINUX_EXEC_H */
