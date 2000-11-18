21681d0265215de3aaac7cdec6645fbef339852e
mrg <mrg@NetBSD.org>
2000-11-18 03:51:14 +0000

    struct pcb's lastcall is const char *

--- sys/arch/sparc64/include/pcb.h
+++ sys/arch/sparc64/include/pcb.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: pcb.h,v 1.5 2000/06/24 20:48:39 eeh Exp $ */
+/*	$NetBSD: pcb.h,v 1.6 2000/11/18 03:51:14 mrg Exp $ */
 
 /*
  * Copyright (c) 1992, 1993
@@ -135,7 +135,7 @@ struct pcb {
 	char	pcb_cwp;	/* %cwp when switch() was called */
 	char	pcb_pil;	/* %pil when switch() was called -- prolly not needed */
 
-	char	*lastcall;	/* DEBUG -- name of last system call */
+	const char *lastcall;	/* DEBUG -- name of last system call */
 	/* the following MUST be aligned on a 64-bit boundary */
 	struct	rwindow64 pcb_rw[PCB_MAXWIN];	/* saved windows */
 };
