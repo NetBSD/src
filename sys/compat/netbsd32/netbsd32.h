e4f5ca29a9dea2818e34d339464592480d591af8
mrg <mrg@NetBSD.org>
2000-11-18 02:00:50 +0000

    kill gcc warnings.

--- sys/compat/netbsd32/netbsd32.h
+++ sys/compat/netbsd32/netbsd32.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: netbsd32.h,v 1.12 2000/08/22 03:35:14 mrg Exp $	*/
+/*	$NetBSD: netbsd32.h,v 1.13 2000/11/18 02:00:50 mrg Exp $	*/
 
 /*
  * Copyright (c) 1998 Matthew R. Green
@@ -257,7 +257,7 @@ struct netbsd32_semid_ds {
 };
 
 struct netbsd32_semid_ds14 {
-	struct netbsd32_ipc_perm	sem_perm;/* operation permission struct */
+	struct netbsd32_ipc_perm14	sem_perm;/* operation permission struct */
 	netbsd32_semp_t	sem_base;	/* pointer to first semaphore in set */
 	unsigned short	sem_nsems;	/* number of sems in set */
 	netbsd32_time_t	sem_otime;		/* last operation time */
