74fad97832f5353f57a8581b344e705dfcd992fe
bouyer <bouyer@NetBSD.org>
2000-11-20 22:35:40 +0000

    Update thorpej_scsipi to -current as of a month ago A i386 GENERIC kernel compiles without the siop, ahc and bha drivers (will be updated later). i386 IDE/ATAPI and ncr work, as well as sparc/esp_sbus. alpha should work as well (untested yet). siop, ahc and bha will be updated once I've updated the branch to current -current, as well as machine-dependant code.

---
+++ sys/arch/newsmips/stand/boot/promdev.h
@@ -0,0 +1,12 @@
+struct romdev {
+	int fd;
+	int devtype;
+};
+
+#define DT_BLOCK	1
+#define DT_NET		2
+
+int net_open __P((struct romdev *));
+int net_close __P((struct romdev *));
+int net_mountroot __P((void));
+int prom_getether __P((struct romdev *, u_char *));
