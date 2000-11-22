534e7d44544b1adf076010942ac6ce1fe2e448b1
thorpej <thorpej@NetBSD.org>
2000-11-22 08:39:46 +0000

    Several changes, which get us generally further along with multiprocessor support: - Implement MP-safe halt. - Make the FPU saving code more like Bill's on the i386 MP branch.   XXX This code will no doubt be revisited again. - Pass the cpu_info and trapframe to IPI handlers, saving some work   in the handlers themselves, and also making it possible for the   "pause" handler to reference register state for DDB. - Add "machine cpu" to DDB, making it possible to reference other   CPUs registers (and thus get e.g. a traceback) from whichever   CPU is actually running the debugger. - Garbage-collect "machine halt" and "machine reboot" DDB commands.   They don't have a prayer of working properly in multiprocessor   kernels, and didn't really work all that well in uniprocessor kernels.

--- sys/arch/alpha/alpha/compat_13_machdep.c
+++ sys/arch/alpha/alpha/compat_13_machdep.c
@@ -1,4 +1,4 @@
-/* $NetBSD: compat_13_machdep.c,v 1.7 2000/08/15 22:16:17 thorpej Exp $ */
+/* $NetBSD: compat_13_machdep.c,v 1.8 2000/11/22 08:39:46 thorpej Exp $ */
 
 /*
  * Copyright (c) 1994, 1995, 1996 Carnegie-Mellon University.
@@ -29,7 +29,7 @@
 
 #include <sys/cdefs.h>			/* RCS ID & Copyright macro defns */
 
-__KERNEL_RCSID(0, "$NetBSD: compat_13_machdep.c,v 1.7 2000/08/15 22:16:17 thorpej Exp $");
+__KERNEL_RCSID(0, "$NetBSD: compat_13_machdep.c,v 1.8 2000/11/22 08:39:46 thorpej Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -93,7 +93,7 @@ compat_13_sys_sigreturn(p, v, retval)
 
 	/* XXX ksc.sc_ownedfp ? */
 	if (p->p_addr->u_pcb.pcb_fpcpu != NULL)
-		synchronize_fpstate(p, 0);
+		fpusave_proc(p, 0);
 	bcopy((struct fpreg *)ksc.sc_fpregs, &p->p_addr->u_pcb.pcb_fp,
 	    sizeof(struct fpreg));
 	/* XXX ksc.sc_fp_control ? */
