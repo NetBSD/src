8e4328dba61c459916b4cac5ccca4ce3c4e37b74
nathanw <nathanw@NetBSD.org>
2000-11-30 20:56:10 +0000

    Don't set the value of doreallocblks here; it's defined over in vfs_cluster.c In fact, doreallocblks isn't used here at all. Delete the declaration.

--- sys/ufs/ffs/ffs_alloc.c
+++ sys/ufs/ffs/ffs_alloc.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: ffs_alloc.c,v 1.38 2000/11/30 19:46:02 jdolecek Exp $	*/
+/*	$NetBSD: ffs_alloc.c,v 1.39 2000/11/30 20:56:10 nathanw Exp $	*/
 
 /*
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -394,7 +394,6 @@ struct ctldebug debug15 = { "prtrealloc", &prtrealloc };
 #endif
 
 int doasyncfree = 1;
-int doreallocblks = 1;
 
 int
 ffs_reallocblks(v)
