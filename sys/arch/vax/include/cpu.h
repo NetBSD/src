a81d92ef6929da059c3ec50b79bba95ede088ba2
ragge <ragge@NetBSD.org>
2000-12-02 17:08:32 +0000

    Add prototype.

--- sys/arch/vax/include/cpu.h
+++ sys/arch/vax/include/cpu.h
@@ -1,4 +1,4 @@
-/*      $NetBSD: cpu.h,v 1.54 2000/08/25 01:04:11 thorpej Exp $      */
+/*      $NetBSD: cpu.h,v 1.55 2000/12/02 17:08:32 ragge Exp $      */
 
 /*
  * Copyright (c) 1994 Ludd, University of Lule}, Sweden
@@ -168,6 +168,7 @@ void	ioaccess(vaddr_t, paddr_t, int);
 void	iounaccess(vaddr_t, int);
 void	findcpu(void);
 void	child_return(void *);
+int	getmajor(void *);
 #ifdef DDB
 int	kdbrint(int);
 #endif
