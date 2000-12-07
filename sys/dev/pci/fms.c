c38470f8f63fb168cfcafb49abfef3cc2c1823c6
abs <abs@NetBSD.org>
2000-12-07 14:12:30 +0000

    Wrap call to mpu_intr in NMPU - nowbuildswithout 'mpu at fms' in config

--- sys/dev/pci/fms.c
+++ sys/dev/pci/fms.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: fms.c,v 1.7 2000/11/14 18:42:55 thorpej Exp $	*/
+/*	$NetBSD: fms.c,v 1.8 2000/12/07 14:12:30 abs Exp $	*/
 
 /*-
  * Copyright (c) 1999 The NetBSD Foundation, Inc.
@@ -39,6 +39,8 @@
 /*
  * Forte Media FM801 Audio Device Driver
  */
+
+#include "mpu.h"
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -476,8 +478,10 @@ fms_intr(arg)
 			printf("unexpected rec intr\n");
 	}
 	
+#if NMPU > 0
 	if (istat & FM_INTSTATUS_MPU)
 		mpu_intr(sc->sc_mpu_dev);
+#endif
 
 	bus_space_write_2(sc->sc_iot, sc->sc_ioh, FM_INTSTATUS, 
 			  istat & (FM_INTSTATUS_PLAY | FM_INTSTATUS_REC));
