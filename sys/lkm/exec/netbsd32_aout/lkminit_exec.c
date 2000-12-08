413bdf22a1ac95a287fceb0efa8ac3788dfa8f64
jdolecek <jdolecek@NetBSD.org>
2000-12-08 23:05:33 +0000

    add build glue for exec LKMs for now, only i386 linux_elf is actually installed, since the others have not been tested yet

---
+++ sys/lkm/exec/netbsd32_aout/lkminit_exec.c
@@ -0,0 +1,74 @@
+/* $NetBSD: lkminit_exec.c,v 1.1 2000/12/08 23:05:43 jdolecek Exp $ */
+
+/*-
+ * Copyright (c) 1996 The NetBSD Foundation, Inc.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to The NetBSD Foundation
+ * by Michael Graff <explorer@flame.org>.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *        This product includes software developed by the NetBSD
+ *        Foundation, Inc. and its contributors.
+ * 4. Neither the name of The NetBSD Foundation nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <sys/param.h>
+#include <sys/systm.h>
+#include <sys/conf.h>
+#include <sys/exec.h>
+#include <sys/proc.h>
+#include <sys/lkm.h>
+
+#include <sys/exec_aout.h>
+#include <compat/netbsd32/netbsd32_exec.h>
+
+int exec_netbsd32_aout_lkmentry __P((struct lkm_table *, int, int));
+
+static struct execsw exec_netbsd32_aout =
+	{ sizeof(struct netbsd32_exec), exec_netbsd32_makecmds, { NULL },
+	  NULL, EXECSW_PRIO_FIRST,
+	  0, netbsd32_copyargs, netbsd32_setregs }; /* sparc 32 bit */
+
+/*
+ * declare the exec
+ */
+MOD_EXEC("exec_netbsd32_aout", -1, &exec_netbsd32_aout, "netbsd32");
+
+/*
+ * entry point
+ */
+int
+exec_netbsd32_aout_lkmentry(lkmtp, cmd, ver)
+	struct lkm_table *lkmtp;	
+	int cmd;
+	int ver;
+{
+	DISPATCH(lkmtp, cmd, ver,
+		 lkm_nofunc,
+		 lkm_nofunc,
+		 lkm_nofunc);
+}
