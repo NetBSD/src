8c4b0f545ed55172b50a1465e1986833edbf558e
itojun <itojun@NetBSD.org>
2000-12-09 01:29:45 +0000

    update icmp6 too big validation.  the change is necessary since pmtud is mandatory for IPv6 (so we can't just validate by using connected pcb - we need to allow traffic from unconnected pcb to do pmtud). - if the traffic is validated by xx_ctlinput, allow up to "hiwat" pmtud   route entries. - if the traffic was not validated by xx_ctlinput, allow up to "lowat" pmtud   route entries (there's upper limit, so bad guys cannot blow up our routing   table). sync with kame
    XXX need to think again about default hiwat/lowat value.
    XXX victim selection to help starvation case

--- sys/net/route.h
+++ sys/net/route.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: route.h,v 1.22 2000/05/04 17:33:03 ragge Exp $	*/
+/*	$NetBSD: route.h,v 1.23 2000/12/09 01:29:45 itojun Exp $	*/
 
 /*
  * Copyright (c) 1980, 1986, 1993
@@ -255,6 +255,7 @@ struct rttimer {
 
 struct rttimer_queue {
 	long				rtq_timeout;
+	unsigned long			rtq_count;
 	TAILQ_HEAD(, rttimer)		rtq_head;
 	LIST_ENTRY(rttimer_queue)	rtq_link;
 };
@@ -297,6 +298,7 @@ struct rttimer_queue *
 void	 rt_timer_queue_change __P((struct rttimer_queue *, long));
 void	 rt_timer_queue_destroy __P((struct rttimer_queue *, int));
 void	 rt_timer_remove_all __P((struct rtentry *));
+unsigned long	rt_timer_count __P((struct rttimer_queue *));
 void	 rt_timer_timer __P((void *));
 void	 rtable_init __P((void **));
 void	 rtalloc __P((struct route *));
