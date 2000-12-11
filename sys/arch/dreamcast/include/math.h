e8e7b75b1bf2ef039bb450e586886e5122d594ab
marcus <marcus@NetBSD.org>
2000-12-11 18:19:12 +0000

    Initial import of port "dreamcast" (cloned from port "evbsh3").

---
+++ sys/arch/dreamcast/include/math.h
@@ -0,0 +1,3 @@
+/*	$NetBSD: math.h,v 1.1 2000/12/11 18:19:15 marcus Exp $	*/
+
+#include <sh3/math.h>
---
+++ sys/arch/dreamcast/include/reg.h
@@ -0,0 +1,7 @@
+/*	$NetBSD: reg.h,v 1.1 2000/12/11 18:19:16 marcus Exp $	*/
+#ifndef _DREAMCAST_REG_H_
+#define _DREAMCAST_REG_H_
+
+#include <sh3/reg.h>
+
+#endif /* _DREAMCAST_REG_H_ */
