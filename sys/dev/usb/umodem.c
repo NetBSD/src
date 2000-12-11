48ffd35313a8ebfa3656951b3703ae66a7e639e4
augustss <augustss@NetBSD.org>
2000-12-11 05:41:00 +0000

    Get rid of UQ_ASSUME_CM_OVER_DATA quirk; it's handled differently now.

--- sys/dev/usb/umodem.c
+++ sys/dev/usb/umodem.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: umodem.c,v 1.33 2000/11/10 14:08:32 augustss Exp $	*/
+/*	$NetBSD: umodem.c,v 1.34 2000/12/11 05:41:00 augustss Exp $	*/
 
 /*
  * Copyright (c) 1998 The NetBSD Foundation, Inc.
@@ -258,22 +258,18 @@ USB_ATTACH(umodem)
 		goto bad;
 	}
 
-	if (usbd_get_quirks(sc->sc_udev)->uq_flags & UQ_ASSUME_CM_OVER_DATA) {
-		sc->sc_cm_over_data = 1;
-	} else {
-		if (sc->sc_cm_cap & USB_CDC_CM_OVER_DATA) {
-			if (sc->sc_acm_cap & USB_CDC_ACM_HAS_FEATURE)
-				err = umodem_set_comm_feature(sc,
-				    UCDC_ABSTRACT_STATE, UCDC_DATA_MULTIPLEXED);
-			else
-				err = 0;
-			if (err) {
-				printf("%s: could not set data multiplex mode\n",
-				       USBDEVNAME(sc->sc_dev));
-				goto bad;
-			}
-			sc->sc_cm_over_data = 1;
+	if (sc->sc_cm_cap & USB_CDC_CM_OVER_DATA) {
+		if (sc->sc_acm_cap & USB_CDC_ACM_HAS_FEATURE)
+			err = umodem_set_comm_feature(sc,
+			    UCDC_ABSTRACT_STATE, UCDC_DATA_MULTIPLEXED);
+		else
+			err = 0;
+		if (err) {
+			printf("%s: could not set data multiplex mode\n",
+			       USBDEVNAME(sc->sc_dev));
+			goto bad;
 		}
+		sc->sc_cm_over_data = 1;
 	}
 	sc->sc_dtr = -1;
 
