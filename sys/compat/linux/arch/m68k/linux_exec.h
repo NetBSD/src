0cc9b367ab7172af23e8aac3cbd952c9e65f49cc
itohy <itohy@NetBSD.org>
2000-12-13 03:49:51 +0000

    sync with i386, so that exec_conf.c compiles again

--- sys/compat/linux/arch/m68k/linux_exec.h
+++ sys/compat/linux/arch/m68k/linux_exec.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: linux_exec.h,v 1.2 2000/11/17 03:55:18 erh Exp $	*/
+/*	$NetBSD: linux_exec.h,v 1.3 2000/12/13 03:49:51 itohy Exp $	*/
 
 /*-
  * Copyright (c) 1998 The NetBSD Foundation, Inc.
@@ -48,7 +48,11 @@
 #define LINUX_GCC_SIGNATURE	1
 
 #define LINUX_ELF_AUX_ARGSIZ \
-	(howmany(ELF_AUX_ENTRIES * sizeof(AuxInfo), sizeof(Elf_Addr)))
+	(howmany(ELF_AUX_ENTRIES * sizeof(Aux32Info), sizeof(Elf32_Addr)))
+#ifdef ELF32NAME
+#define LINUX_COPYARGS_FUNCTION	ELF32NAME(copyargs)
+#else
 #define LINUX_COPYARGS_FUNCTION	ELFNAME(copyargs)
+#endif
 
 #endif /* !_M68K_LINUX_EXEC_H */
