fcb8751887feca88ef2f7c10d9750e760891989f
scw <scw@NetBSD.org>
2000-12-13 21:58:56 +0000

    Declare _nsyylineno as extern since it belongs in the data section (due to being initialised in nslexer.[cy]).

--- include/nsswitch.h
+++ include/nsswitch.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: nsswitch.h,v 1.9 2000/06/13 01:21:53 simonb Exp $	*/
+/*	$NetBSD: nsswitch.h,v 1.10 2000/12/13 21:58:56 scw Exp $	*/
 
 /*-
  * Copyright (c) 1997, 1998, 1999 The NetBSD Foundation, Inc.
@@ -182,7 +182,7 @@ const ns_dbt	*_nsdbtget __P((const char *));
 int		 _nsdbtput __P((const ns_dbt *));
 void		 _nsyyerror __P((const char *));
 int		 _nsyylex __P((void));
-int		 _nsyylineno;
+extern int	 _nsyylineno;
 #endif /* _NS_PRIVATE */
 
 __END_DECLS
