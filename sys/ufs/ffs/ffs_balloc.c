ce1db4f75a1a9d257858096e1ae018752b60a35d
he <he@NetBSD.org>
2000-12-14 23:36:34 +0000

    Pull up revision 1.22 (requested by fvdl):   Improve NFS performance, possibly with as much as 100% in   throughput.  Please note: this implies a kernel interface change,   VOP_FSYNC gains two arguments.

--- sys/ufs/ffs/ffs_balloc.c
+++ sys/ufs/ffs/ffs_balloc.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: ffs_balloc.c,v 1.20 2000/05/29 16:28:27 mycroft Exp $	*/
+/*	$NetBSD: ffs_balloc.c,v 1.20.2.1 2000/12/14 23:36:34 he Exp $	*/
 
 /*
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -360,7 +360,7 @@ fail:
 	 * occurence. The error return from fsync is ignored as we already
 	 * have an error to return to the user.
 	 */
-	(void) VOP_FSYNC(vp, cred, FSYNC_WAIT, curproc);
+	(void) VOP_FSYNC(vp, cred, FSYNC_WAIT, 0, 0, curproc);
 	for (deallocated = 0, blkp = allociblk; blkp < allocblk; blkp++) {
 		ffs_blkfree(ip, *blkp, fs->fs_bsize);
 		deallocated += fs->fs_bsize;
@@ -402,6 +402,6 @@ fail:
 		ip->i_ffs_blocks -= btodb(deallocated);
 		ip->i_flag |= IN_CHANGE | IN_UPDATE;
 	}
-	(void) VOP_FSYNC(vp, cred, FSYNC_WAIT, curproc);
+	(void) VOP_FSYNC(vp, cred, FSYNC_WAIT, 0, 0, curproc);
 	return (error);
 }
