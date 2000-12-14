b3a7c93d2e48d957a4c14fd49e2c7545fd74af74
mycroft <mycroft@NetBSD.org>
2000-12-14 00:38:20 +0000

    Wrap mdproc in #ifdef _KERNEL.

--- sys/arch/alpha/include/proc.h
+++ sys/arch/alpha/include/proc.h
@@ -1,4 +1,4 @@
-/* $NetBSD: proc.h,v 1.6 2000/12/13 03:16:39 mycroft Exp $ */
+/* $NetBSD: proc.h,v 1.7 2000/12/14 00:38:20 mycroft Exp $ */
 
 /*
  * Copyright (c) 1994, 1995 Carnegie-Mellon University.
@@ -27,10 +27,10 @@
  * rights to redistribute these changes.
  */
 
+#ifdef _KERNEL
 /*
  * Machine-dependent part of the proc struct for the Alpha.
  */
-
 struct mdproc {
 	u_long	md_flags;
 	struct	trapframe *md_tf;	/* trap/syscall registers */
@@ -39,3 +39,4 @@ struct mdproc {
 };
 
 #define	MDP_FPUSED	0x0001		/* Process used the FPU */
+#endif
