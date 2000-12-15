37f2940ecb5a4f69865d69ec4f12d87f257807b3
oster <oster>
2000-12-15 02:12:58 +0000

    For the RF_ASSERT() and RF_PANIC() macros, call a function to do the sprintf, instead of doing the sprintf in the macros.  This means just 1 copy of each of the error messages, chopping about about 16K off the size of an i386 kernel.  Thanks to Simon Burge and Enami Tsugutomo for providing the inspiration to do this.

--- sys/dev/raidframe/rf_general.h
+++ sys/dev/raidframe/rf_general.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: rf_general.h,v 1.5 2000/03/03 02:04:48 oster Exp $	*/
+/*	$NetBSD: rf_general.h,v 1.6 2000/12/15 02:12:58 oster Exp $	*/
 /*
  * Copyright (c) 1995 Carnegie-Mellon University.
  * All rights reserved.
@@ -46,8 +46,11 @@
 #define RF_ERRORMSG2(s,a,b)       printf((s),(a),(b))
 #define RF_ERRORMSG3(s,a,b,c)     printf((s),(a),(b),(c))
 
+void rf_print_panic_message(int, char *);
+void rf_print_assert_panic_message(int, char *, char *);
+
 extern char rf_panicbuf[];
-#define RF_PANIC() {sprintf(rf_panicbuf,"raidframe error at line %d file %s",__LINE__,__FILE__); panic(rf_panicbuf);}
+#define RF_PANIC() {rf_print_panic_message(__LINE__,__FILE__); panic(rf_panicbuf);}
 
 #ifdef _KERNEL
 #ifdef RF_ASSERT
@@ -56,9 +59,7 @@ extern char rf_panicbuf[];
 #ifndef NOASSERT
 #define RF_ASSERT(_x_) { \
   if (!(_x_)) { \
-    sprintf(rf_panicbuf, \
-        "raidframe error at line %d file %s (failed asserting %s)\n", \
-        __LINE__, __FILE__, #_x_); \
+    rf_print_assert_panic_message(__LINE__, __FILE__, #_x_); \
     panic(rf_panicbuf); \
   } \
 }
