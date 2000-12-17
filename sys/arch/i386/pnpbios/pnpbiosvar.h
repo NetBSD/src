a37c060b3ce6e5dc581d66595754b9e2756009bf
jmc <jmc>
2000-12-17 07:59:14 +0000

    Add pnpbios_getiosize

--- sys/arch/i386/pnpbios/pnpbiosvar.h
+++ sys/arch/i386/pnpbios/pnpbiosvar.h
@@ -1,4 +1,4 @@
-/* $NetBSD: pnpbiosvar.h,v 1.4 2000/04/22 06:38:24 thorpej Exp $ */
+/* $NetBSD: pnpbiosvar.h,v 1.5 2000/12/17 07:59:14 jmc Exp $ */
 /*
  * Copyright (c) 1999
  * 	Matthias Drochner.  All rights reserved.
@@ -94,6 +94,7 @@ void *pnpbios_intr_establish __P((pnpbios_tag_t, struct pnpresources *, int,
 
 int pnpbios_getiobase __P((pnpbios_tag_t, struct pnpresources *, int,
 			   bus_space_tag_t *, int *));
+int pnpbios_getiosize __P((pnpbios_tag_t, struct pnpresources *, int, int *));
 int pnpbios_getirqnum __P((pnpbios_tag_t, struct pnpresources *, int, int *,
 			   int *));
 int pnpbios_getdmachan __P((pnpbios_tag_t, struct pnpresources *, int, int *));
