325f1514740080743f5b2e34b6f54025bd1bf7d7
bjh21 <bjh21@NetBSD.org>
2000-12-20 23:08:44 +0000

    Looks like I got the state of the link-detect bit inverted.  Fixing that gets me working auto-media selection, which is nice.

--- sys/arch/arm26/podulebus/if_ehreg.h
+++ sys/arch/arm26/podulebus/if_ehreg.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: if_ehreg.h,v 1.3 2000/12/17 22:29:26 bjh21 Exp $	*/
+/*	$NetBSD: if_ehreg.h,v 1.4 2000/12/20 23:08:44 bjh21 Exp $	*/
 
 /*
  * This file is in the public domain
@@ -9,6 +9,25 @@
 
 /*
  * Register definitions for i-cubed EtherLan 100-, 200-, and 500-series cards.
+ */
+
+/*
+ * Addr		r/w	Descr
+ * SYNC+0x0000	r	ROM data
+ * SYNC+0x0000	w	ROM page latch
+ * FAST+0x0800	r/w	AUX1
+ * FAST+0x2800	r/w	AUX2
+ * MEMC+0x0000	r/w	MX98902A registers
+ * MEMC+0x0200	r/w	MX98902A data
+ *
+ * AUX1 bits:
+ * 0 r Interrupt status
+ * 0 w Interrupt enable
+ * 1 r Link beat (0 = link, 1 = no link)
+ * 1 w Media select (0 = 10b2, 1 = 10bT)
+ *
+ * AUX2 bits:
+ * 0 r Media sense (0 = 10bT, 1 = 10b2)
  */
 
 /* Loosely derived from Linux drivers/acorn/net/etherh.c. */
@@ -23,7 +42,7 @@
 #define EH_CTRL_IE	0x01	/* Interrupt enable (W) */
 #define EH_CTRL_IS	0x01	/* Interrupt status (R) */
 #define EH_CTRL_MEDIA	0x02	/* Media select (0 = 10b2, 1 = 10bT) (W) */
-#define EH_CTRL_LINK	0x02	/* Link beat detect (R) */
+#define EH_CTRL_NOLINK	0x02	/* Link beat detect (R) */
 
 /* Bits of control register 2 */
 #define EH_CTRL2_10B2	0x01	/* Media sense (0 = 10bT, 1 = 10b2) (R) */
