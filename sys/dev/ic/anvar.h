975b9a9f998356475ee777d2f7e89ee0ad9b7a74
onoe <onoe@NetBSD.org>
2000-12-20 23:30:36 +0000

    move definition of AN_INTRS from anreg.h to anvar.h strip off AN_EV_CMD from AN_INTRS because it causes severe performance problem.

--- sys/dev/ic/anvar.h
+++ sys/dev/ic/anvar.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: anvar.h,v 1.4 2000/12/19 08:00:56 onoe Exp $	*/
+/*	$NetBSD: anvar.h,v 1.5 2000/12/20 23:30:36 onoe Exp $	*/
 /*
  * Copyright (c) 1997, 1998, 1999
  *	Bill Paul <wpaul@ctr.columbia.edu>.  All rights reserved.
@@ -61,6 +61,10 @@
 #define AN_DEFAULT_NODENAME	"NetBSD"
 
 #define AN_DEFAULT_IBSS		"NetBSD IBSS"
+
+/* The interrupts we will handle */
+#define AN_INTRS	\
+	(AN_EV_RX|AN_EV_TX|AN_EV_TX_EXC|AN_EV_ALLOC|AN_EV_LINKSTAT)
 
 /*
  * register space access macros
