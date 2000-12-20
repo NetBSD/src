118e5c499d5b668d3950ef9ce3385f1fe8605020
christos <christos@NetBSD.org>
2000-12-20 23:08:51 +0000

    oops fix previous typos.

--- lib/libc/rpc/clnt_dg.c
+++ lib/libc/rpc/clnt_dg.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: clnt_dg.c,v 1.5 2000/12/20 20:52:50 christos Exp $	*/
+/*	$NetBSD: clnt_dg.c,v 1.6 2000/12/20 23:08:51 christos Exp $	*/
 
 /*
  * Sun RPC is a product of Sun Microsystems, Inc. and is provided for
@@ -351,7 +351,7 @@ call_again:
 	 * the transaction is the first thing in the out buffer
 	 */
 	(*(u_int32_t *)(void *)(cu->cu_outbuf))++;
-	if ((! XDR_PUTINT32(xdrs, (int32p)&proc)) ||
+	if ((! XDR_PUTINT32(xdrs, (int32_t *)&proc)) ||
 	    (! AUTH_MARSHALL(cl->cl_auth, xdrs)) ||
 	    (! (*xargs)(xdrs, argsp))) {
 		release_fd_lock(cu->cu_fd, mask);
