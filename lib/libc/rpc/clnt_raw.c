032b842beb1762d95ce71e92f49786ed247494df
christos <christos@NetBSD.org>
2000-12-20 23:08:51 +0000

    oops fix previous typos.

--- lib/libc/rpc/clnt_raw.c
+++ lib/libc/rpc/clnt_raw.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: clnt_raw.c,v 1.21 2000/12/20 20:52:50 christos Exp $	*/
+/*	$NetBSD: clnt_raw.c,v 1.22 2000/12/20 23:08:51 christos Exp $	*/
 
 /*
  * Sun RPC is a product of Sun Microsystems, Inc. and is provided for
@@ -35,7 +35,7 @@
 static char *sccsid = "@(#)clnt_raw.c 1.22 87/08/11 Copyr 1984 Sun Micro";
 static char *sccsid = "@(#)clnt_raw.c	2.2 88/08/01 4.0 RPCSRC";
 #else
-__RCSID("$NetBSD: clnt_raw.c,v 1.21 2000/12/20 20:52:50 christos Exp $");
+__RCSID("$NetBSD: clnt_raw.c,v 1.22 2000/12/20 23:08:51 christos Exp $");
 #endif
 #endif
 
@@ -182,7 +182,7 @@ call_again:
 	XDR_SETPOS(xdrs, 0);
 	clp->u.mashl_rpcmsg.rm_xid ++ ;
 	if ((! XDR_PUTBYTES(xdrs, clp->u.mashl_callmsg, clp->mcnt)) ||
-	    (! XDR_PUTINT32(xdrs, (int32p)&proc)) ||
+	    (! XDR_PUTINT32(xdrs, (int32_t *)&proc)) ||
 	    (! AUTH_MARSHALL(h->cl_auth, xdrs)) ||
 	    (! (*xargs)(xdrs, argsp))) {
 		return (RPC_CANTENCODEARGS);
