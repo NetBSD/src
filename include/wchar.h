93ab97e9c6ad8606f5f1a12beaea9aee6b6de321
itojun <itojun@NetBSD.org>
2000-12-20 14:53:23 +0000

    add wmem* functions, from citrus.  bump libc shlib minor.

--- include/wchar.h
+++ include/wchar.h
@@ -1,4 +1,30 @@
-/*	$NetBSD: wchar.h,v 1.1 2000/04/20 09:56:37 kleink Exp $	*/
+/*	$NetBSD: wchar.h,v 1.2 2000/12/20 14:53:23 itojun Exp $	*/
+
+/*-
+ * Copyright (c)1999 Citrus Project,
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
 
 /*-
  * Copyright (c) 1999, 2000 The NetBSD Foundation, Inc.
@@ -39,6 +65,7 @@
 #ifndef _WCHAR_H_
 #define _WCHAR_H_
 
+#include <sys/cdefs.h>
 #include <machine/ansi.h>
 
 #ifdef	_BSD_WCHAR_T_
@@ -55,5 +82,13 @@ typedef	_BSD_WINT_T_	wint_t;
 typedef	_BSD_SIZE_T_	size_t;
 #undef	_BSD_SIZE_T_
 #endif
+
+__BEGIN_DECLS
+wchar_t	*wmemchr __P((const wchar_t *, wchar_t, size_t));
+int	wmemcmp __P((const wchar_t *, const wchar_t *, size_t));
+wchar_t	*wmemcpy __P((wchar_t *, const wchar_t *, size_t));
+wchar_t	*wmemmove __P((wchar_t *, const wchar_t *, size_t));
+wchar_t	*wmemset __P((wchar_t *, wchar_t, size_t));
+__END_DECLS
 
 #endif /* !_WCHAR_H_ */
