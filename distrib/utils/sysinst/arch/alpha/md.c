0ea7dee8d9f572978226a1832858dc2a044a5ac1
jdc <jdc@NetBSD.org>
2000-12-21 21:42:00 +0000

    Don't output the termcap `cl' sequence. XXX: do we really need to home the cursor here? XXX: should we do clearok() instead or in addition?

--- distrib/utils/sysinst/arch/alpha/md.c
+++ distrib/utils/sysinst/arch/alpha/md.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: md.c,v 1.22 2000/10/11 23:47:57 fvdl Exp $	*/
+/*	$NetBSD: md.c,v 1.23 2000/12/21 21:42:00 jdc Exp $	*/
 
 /*
  * Copyright 1997 Piermont Information Systems Inc.
@@ -339,7 +339,7 @@ md_update(void)
 	endwin();
 	md_copy_filesystem ();
 	md_post_newfs();
-	puts(CL);		/* XXX */
+	wmove(stdscr, 0, 0);
 	wclear(stdscr);
 	wrefresh(stdscr);
 	return 1;
