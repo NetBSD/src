ef94d95ca66679107755f8854c8410c8b524934a
itojun <itojun@NetBSD.org>
2000-12-21 18:20:03 +0000

    find shlib/func by findshlib() and findfunc().  this nukes DLRUNE_AOUT, and removes conditionals for library naming. XXX if we mix a.out library and elf library in /usr/lib/runemodule...

--- lib/libc/locale/setrunelocale.c
+++ lib/libc/locale/setrunelocale.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: setrunelocale.c,v 1.2 2000/12/21 11:29:48 itojun Exp $	*/
+/*	$NetBSD: setrunelocale.c,v 1.3 2000/12/21 18:20:03 itojun Exp $	*/
 
 /*-
  * Copyright (c)1999 Citrus Project,
@@ -64,7 +64,7 @@
 
 #include <sys/cdefs.h>
 #if defined(LIBC_SCCS) && !defined(lint)
-__RCSID("$NetBSD: setrunelocale.c,v 1.2 2000/12/21 11:29:48 itojun Exp $");
+__RCSID("$NetBSD: setrunelocale.c,v 1.3 2000/12/21 18:20:03 itojun Exp $");
 #endif /* LIBC_SCCS and not lint */
 
 #include "rune.h"
@@ -79,6 +79,13 @@ __RCSID("$NetBSD: setrunelocale.c,v 1.2 2000/12/21 11:29:48 itojun Exp $");
 #include "rune_local.h"
 
 #ifdef DLRUNE
+#include <sys/types.h>
+#include <dirent.h>
+
+static int getdewey __P((int [], char *));
+static int cmpndewey __P((int [], int, int [], int));
+static const char *findshlib __P((char *, int *, int *));
+static void *findfunc __P((void *, const char *));
 static int loadrunemodule __P((_RuneLocale *, void **));
 #endif
 
@@ -105,24 +112,188 @@ static struct localetable localetable;
 #ifdef DLRUNE
 static char *_PathModule = NULL;
 
+/* from libexec/ld.aout_so/shlib.c */
+#undef major
+#undef minor
+#define MAXDEWEY	3	/*ELF*/
+
+static int
+getdewey(dewey, cp)
+int	dewey[];
+char	*cp;
+{
+	int	i, n;
+
+	for (n = 0, i = 0; i < MAXDEWEY; i++) {
+		if (*cp == '\0')
+			break;
+
+		if (*cp == '.') cp++;
+		if (*cp < '0' || '9' < *cp)
+			return 0;
+
+		dewey[n++] = strtol(cp, &cp, 10);
+	}
+
+	return n;
+}
+
+/*
+ * Compare two dewey arrays.
+ * Return -1 if `d1' represents a smaller value than `d2'.
+ * Return  1 if `d1' represents a greater value than `d2'.
+ * Return  0 if equal.
+ */
+static int
+cmpndewey(d1, n1, d2, n2)
+int	d1[], d2[];
+int	n1, n2;
+{
+	register int	i;
+
+	for (i = 0; i < n1 && i < n2; i++) {
+		if (d1[i] < d2[i])
+			return -1;
+		if (d1[i] > d2[i])
+			return 1;
+	}
+
+	if (n1 == n2)
+		return 0;
+
+	if (i == n1)
+		return -1;
+
+	if (i == n2)
+		return 1;
+
+	/* XXX cannot happen */
+	return 0;
+}
+
+static const char *
+findshlib(name, majorp, minorp)
+char	*name;
+int	*majorp, *minorp;
+{
+	int		dewey[MAXDEWEY];
+	int		ndewey;
+	int		tmp[MAXDEWEY];
+	int		i;
+	int		len;
+	char		*lname;
+	static char	path[PATH_MAX];
+	int		major = *majorp, minor = *minorp;
+	const char	*search_dirs[1];
+	const int	n_search_dirs = 1;
+
+	path[0] = '\0';
+	search_dirs[0] = _PathModule;
+	len = strlen(name);
+	lname = name;
+
+	ndewey = 0;
+
+	for (i = 0; i < n_search_dirs; i++) {
+		DIR		*dd = opendir(search_dirs[i]);
+		struct dirent	*dp;
+		int		found_dot_a = 0;
+		int		found_dot_so = 0;
+
+		if (dd == NULL)
+			continue;
+
+		while ((dp = readdir(dd)) != NULL) {
+			int	n;
+
+			if (dp->d_namlen < len + 4)
+				continue;
+			if (strncmp(dp->d_name, lname, len) != 0)
+				continue;
+			if (strncmp(dp->d_name+len, ".so.", 4) != 0)
+				continue;
+
+			if ((n = getdewey(tmp, dp->d_name+len+4)) == 0)
+				continue;
+
+			if (major != -1 && found_dot_a)
+				found_dot_a = 0;
+
+			/* XXX should verify the library is a.out/ELF? */
+
+			if (major == -1 && minor == -1) {
+				goto compare_version;
+			} else if (major != -1 && minor == -1) {
+				if (tmp[0] == major)
+					goto compare_version;
+			} else if (major != -1 && minor != -1) {
+				if (tmp[0] == major) {
+					if (n == 1 || tmp[1] >= minor)
+						goto compare_version;
+				}
+			}
+
+			/* else, this file does not qualify */
+			continue;
+
+		compare_version:
+			if (cmpndewey(tmp, n, dewey, ndewey) <= 0)
+				continue;
+
+			/* We have a better version */
+			found_dot_so = 1;
+			snprintf(path, sizeof(path), "%s/%s", search_dirs[i],
+			    dp->d_name);
+			found_dot_a = 0;
+			bcopy(tmp, dewey, sizeof(dewey));
+			ndewey = n;
+			*majorp = dewey[0];
+			*minorp = dewey[1];
+		}
+		closedir(dd);
+
+		if (found_dot_a || found_dot_so)
+			/*
+			 * There's a lib in this dir; take it.
+			 */
+			return path[0] ? path : NULL;
+	}
+
+	return path[0] ? path : NULL;
+}
+
+static void *
+findfunc(handle, sym)
+	void *handle;
+	const char *sym;
+{
+	char name[PATH_MAX];
+	void *p;
+
+	p = dlsym(handle, sym);
+	if (!p) {
+		/* a.out case */
+		snprintf(name, sizeof(name), "_%s", sym);
+		p = dlsym(handle, sym);
+	}
+	return p;
+}
+
 static int
 loadrunemodule(_RuneLocale *rl, void **rhandle)
 {
-	char name[PATH_MAX];
+	const char *p;
+	char namebase[PATH_MAX], name[PATH_MAX];
+	int maj, min;
 	int ret;
         int (*init) __P((_RuneLocale *));
         int (*initstream) __P((_RuneLocale *));
 
 	if (_PathModule == NULL) {
-		char *p = getenv("PATH_LOCALEMODULE");
-#ifndef RUNEMOD_MAJOR
-		/* "/libencoding.so" */
-		const size_t offset = 4 + sizeof(rl->__encoding) + 3;
-#else
 		/* "/libencoding.so.###" */
 		const size_t offset = 4 + sizeof(rl->__encoding) + 7;
-#endif
 
+		p = getenv("PATH_LOCALEMODULE");
 		if (p != NULL && !issetugid()) {
 			if (strlen(p) + offset + 1 >= PATH_MAX)
 				return EFAULT;
@@ -133,22 +304,18 @@ loadrunemodule(_RuneLocale *rl, void **rhandle)
 			_PathModule = _PATH_LOCALEMODULE;
 	}
 	/* Range checking not needed, encoding length already checked above */
-#ifdef RUNEMOD_MAJOR
-        (void) snprintf(name, sizeof(name), "%s/lib%s.so.%d",
-                       _PathModule, rl->__encoding, RUNEMOD_MAJOR);
-#else
-        (void) snprintf(name, sizeof(name), "%s/lib%s.so",
-                       _PathModule, rl->__encoding);
-#endif
-	*rhandle = dlopen(name, RTLD_LAZY);
+	(void)snprintf(namebase, sizeof(namebase), "lib%s", rl->__encoding);
+	maj = RUNEMOD_MAJOR;
+	min = -1;
+	p = findshlib(namebase, &maj, &min);
+	if (!p)
+		return (EINVAL);
+	*rhandle = dlopen(p, RTLD_LAZY);
 	if (!*rhandle)
 		return(EINVAL);
-#ifdef DLRUNE_AOUT
-	snprintf(name, sizeof(name), "__%s_init", rl->__encoding);
-#else
 	snprintf(name, sizeof(name), "_%s_init", rl->__encoding);
-#endif
-	init = (int (*)__P((_RuneLocale *))) dlsym(*rhandle, name);
+	init = (int (*)__P((_RuneLocale *))) findfunc(*rhandle, name);
+	dlsym(*rhandle, name);
 	if (!init) {
 		ret = EINVAL;
 		goto fail;
@@ -156,12 +323,8 @@ loadrunemodule(_RuneLocale *rl, void **rhandle)
 	ret = (*init)(rl);
 	if (ret)
 		goto fail;
-#ifdef DLRUNE_AOUT
-	snprintf(name, sizeof(name), "__%s_init_stream", rl->__encoding);
-#else
 	snprintf(name, sizeof(name), "_%s_init_stream", rl->__encoding);
-#endif
-	initstream = (int (*)__P((_RuneLocale *))) dlsym(*rhandle, name);
+	initstream = (int (*)__P((_RuneLocale *))) findfunc(*rhandle, name);
 	if (initstream) {
 		ret = (*initstream)(rl);
 		if (ret)
