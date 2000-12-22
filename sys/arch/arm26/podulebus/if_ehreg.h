baa03c3b7d93df1b365285b57cbedf9f972a2219
bjh21 <bjh21>
2000-12-22 22:21:37 +0000

    Support for identifying MAUs attached to EtherLan 200-series cards.

--- sys/arch/arm26/podulebus/if_ehreg.h
+++ sys/arch/arm26/podulebus/if_ehreg.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: if_ehreg.h,v 1.4 2000/12/20 23:08:44 bjh21 Exp $	*/
+/*	$NetBSD: if_ehreg.h,v 1.5 2000/12/22 22:21:37 bjh21 Exp $	*/
 
 /*
  * This file is in the public domain
@@ -23,8 +23,10 @@
  * AUX1 bits:
  * 0 r Interrupt status
  * 0 w Interrupt enable
- * 1 r Link beat (0 = link, 1 = no link)
- * 1 w Media select (0 = 10b2, 1 = 10bT)
+ * 1 r (E100) Link beat (0 = link, 1 = no link)
+ * 1 w (E100) Media select (0 = 10b2, 1 = 10bT)
+ * 1 r (E200) MAU ID input (from MAU)
+ * 1 w (E200) MAU ID output (to MAU)
  *
  * AUX2 bits:
  * 0 r Media sense (0 = 10bT, 1 = 10b2)
@@ -44,7 +46,17 @@
 #define EH_CTRL_MEDIA	0x02	/* Media select (0 = 10b2, 1 = 10bT) (W) */
 #define EH_CTRL_NOLINK	0x02	/* Link beat detect (R) */
 
+/*
+ * The EtherLan 200 is strange.  A write to AUX1 following another write has
+ * the effect described above, but a write following a read affects this:
+ */
+#define EH200_CTRL_MAU	0x02	/* MAU input/output */
+
 /* Bits of control register 2 */
 #define EH_CTRL2_10B2	0x01	/* Media sense (0 = 10bT, 1 = 10b2) (R) */
 
+/* EtherLan 200 MAU types */
+#define EH200_MAUID_10_T	2
+#define EH200_MAUID_10_2	3
+#define EH200_MAUID_10_5	4
 #endif
