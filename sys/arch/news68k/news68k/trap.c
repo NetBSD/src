de31133f1d0c3f23c0b6c31d344c6b47bd6405e3
jdolecek <jdolecek@NetBSD.org>
2000-12-23 09:35:51 +0000

    split off thread specific stuff from struct sigacts to struct sigctx, leaving     only signal handler array sharable between threads move other random signal stuff from struct proc to struct sigctx
    This addresses kern/10981 by Matthew Orgass.
    
    XXX I wish m68k ports would share trap.c

--- sys/arch/news68k/news68k/trap.c
+++ sys/arch/news68k/news68k/trap.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: trap.c,v 1.10 2000/12/19 21:09:57 scw Exp $	*/
+/*	$NetBSD: trap.c,v 1.11 2000/12/23 09:35:52 jdolecek Exp $	*/
 
 /*
  * Copyright (c) 1988 University of Utah.
@@ -354,10 +354,10 @@ trap(type, code, v, frame)
 		printf("pid %d: kernel %s exception\n", p->p_pid,
 		       type==T_COPERR ? "coprocessor" : "format");
 		type |= T_USER;
-		p->p_sigacts->ps_sigact[SIGILL].sa_handler = SIG_DFL;
-		sigdelset(&p->p_sigignore, SIGILL);
-		sigdelset(&p->p_sigcatch, SIGILL);
-		sigdelset(&p->p_sigmask, SIGILL);
+		SIGACTION(p, SIGILL).sa_handler = SIG_DFL;
+		sigdelset(&p->p_sigctx.ps_sigignore, SIGILL);
+		sigdelset(&p->p_sigctx.ps_sigcatch, SIGILL);
+		sigdelset(&p->p_sigctx.ps_sigmask, SIGILL);
 		i = SIGILL;
 		ucode = frame.f_format;	/* XXX was ILL_RESAD_FAULT */
 		break;
