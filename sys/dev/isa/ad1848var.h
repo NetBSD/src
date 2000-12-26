2a5ad1daf8b3d80b83933f295b98175e54ec268a
jhawk <jhawk@NetBSD.org>
2000-12-26 15:47:36 +0000

    Pull up revision 1.35 (requested by thorpej):   Support full-duplex audio on 1848 wss devices by using   seperate interrupt handlers for play and record. Fix from   Juergen Weiss <weiss@uni-mainz.de> by way of PR#11178.

--- sys/dev/isa/ad1848var.h
+++ sys/dev/isa/ad1848var.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: ad1848var.h,v 1.32.4.1 2000/06/30 16:27:47 simonb Exp $	*/
+/*	$NetBSD: ad1848var.h,v 1.32.4.2 2000/12/26 15:47:36 jhawk Exp $	*/
 
 /*-
  * Copyright (c) 1999 The NetBSD Foundation, Inc.
@@ -89,8 +89,10 @@ struct ad1848_isa_softc {
 	bus_size_t sc_rec_maxsize;	/* record/capture DMA size */
 	
 	u_long	sc_interrupts;		/* number of interrupts taken */
-	void	(*sc_intr)(void *);	/* dma completion intr handler */
-	void	*sc_arg;		/* arg for sc_intr() */
+	void	(*sc_pintr)(void *);	/* play dma completion intr handler */
+	void	*sc_parg;		/* arg for sc_pintr() */
+	void	(*sc_rintr)(void *);	/* rec. dma completion intr handler */
+	void	*sc_rarg;		/* arg for sc_rintr() */
 
 	/* Only used by pss XXX */
 	int	sc_iobase;
