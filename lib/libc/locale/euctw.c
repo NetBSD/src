6ade44cb29c0e40e38fcf618bd12fcf9e6c06640
itojun <itojun@NetBSD.org>
2000-12-28 05:27:35 +0000

    sync citrus Id part with citrus repository.

--- lib/libc/locale/euctw.c
+++ lib/libc/locale/euctw.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: euctw.c,v 1.4 2000/12/28 05:22:27 itojun Exp $	*/
+/*	$NetBSD: euctw.c,v 1.5 2000/12/28 05:27:35 itojun Exp $	*/
 
 /*-
  * Copyright (c)1999 Citrus Project,
@@ -25,12 +25,12 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	citrus Id: euctw.c,v 1.6 2000/12/21 07:15:25 itojun Exp
+ *	citrus Id: euctw.c,v 1.9 2000/12/28 02:30:29 itojun Exp
  */
 
 #include <sys/cdefs.h>
 #if defined(LIBC_SCCS) && !defined(lint)
-__RCSID("$NetBSD: euctw.c,v 1.4 2000/12/28 05:22:27 itojun Exp $");
+__RCSID("$NetBSD: euctw.c,v 1.5 2000/12/28 05:27:35 itojun Exp $");
 #endif /* LIBC_SCCS and not lint */
 
 #include <sys/types.h>
