c07a5a07dad43c0e717ce8487ab4ef4c9d53e32c
matt <matt>
2000-12-28 03:55:14 +0000

    Make identical to arm32 version. (add same parens, use __P, etc.)

--- sys/arch/arm26/include/float.h
+++ sys/arch/arm26/include/float.h
@@ -1,4 +1,4 @@
-/* $NetBSD: float.h,v 1.1 2000/05/09 21:55:59 bjh21 Exp $ */
+/* $NetBSD: float.h,v 1.2 2000/12/28 03:55:14 matt Exp $ */
 
 /*
  * Copyright (c) 1992, 1993
@@ -44,24 +44,24 @@
  *	@(#)float.h	8.1 (Berkeley) 6/11/93
  */
 
-#ifndef _ARM32_FLOAT_H_
-#define _ARM32_FLOAT_H_
+#ifndef _ARM26_FLOAT_H_
+#define _ARM26_FLOAT_H_
 
 #include <sys/cdefs.h>
 
 __BEGIN_DECLS
-extern int __flt_rounds();
+extern int __flt_rounds __P((void));
 __END_DECLS
 
 #define FLT_RADIX	2		/* b */
 #define FLT_ROUNDS	__flt_rounds()
 
 #define FLT_MANT_DIG	24		/* p */
-#define FLT_EPSILON	1.19209290E-07F	/* b**(1-p) */
+#define FLT_EPSILON	1.19209290E-7F	/* b**(1-p) */
 #define FLT_DIG		6		/* floor((p-1)*log10(b))+(b == 10) */
-#define FLT_MIN_EXP	-125		/* emin */
+#define FLT_MIN_EXP	(-125)		/* emin */
 #define FLT_MIN		1.17549435E-38F	/* b**(emin-1) */
-#define FLT_MIN_10_EXP	-37		/* ceil(log10(b**(emin-1))) */
+#define FLT_MIN_10_EXP	(-37)		/* ceil(log10(b**(emin-1))) */
 #define FLT_MAX_EXP	128		/* emax */
 #define FLT_MAX		3.40282347E+38F	/* (1-b**(-p))*b**emax */
 #define FLT_MAX_10_EXP	38		/* floor(log10((1-b**(-p))*b**emax)) */
@@ -69,9 +69,9 @@ __END_DECLS
 #define DBL_MANT_DIG	53
 #define DBL_EPSILON	2.2204460492503131E-16
 #define DBL_DIG		15
-#define DBL_MIN_EXP	-1021
+#define DBL_MIN_EXP	(-1021)
 #define DBL_MIN		2.2250738585072014E-308
-#define DBL_MIN_10_EXP	-307
+#define DBL_MIN_10_EXP	(-307)
 #define DBL_MAX_EXP	1024
 #define DBL_MAX		1.7976931348623157E+308
 #define DBL_MAX_10_EXP	308
@@ -86,4 +86,4 @@ __END_DECLS
 #define LDBL_MAX	DBL_MAX
 #define LDBL_MAX_10_EXP	DBL_MAX_10_EXP
 
-#endif	/* _ARM32_FLOAT_H_ */
+#endif	/* _ARM26_FLOAT_H_ */
