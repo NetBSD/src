1fb988a3ee10d5eca8fa7fb386ec57fb31346daa
matt <matt@NetBSD.org>
2000-12-29 21:34:11 +0000

    Massive changes to include PIC support including a mostly revamp machine description.

--- gnu/dist/toolchain/gcc/config/vax/netbsd.h
+++ gnu/dist/toolchain/gcc/config/vax/netbsd.h
@@ -24,3 +24,51 @@
 
 #undef TARGET_DEFAULT
 #define TARGET_DEFAULT MASK_HALFPIC	/* default to half-pic mode */
+
+/* Function CSE screws up PLT .vs. GOT usage.
+ */
+#define NO_FUNCTION_CSE
+
+/* This makes use of a hook in varasm.c to mark all external functions
+   for us.  We use this to make sure that external functions are correctly
+   referenced from the PLT.  */
+
+#define	NO_EXTERNAL_INDIRECT_ADDRESS
+
+/* Define this macro if references to a symbol must be treated
+   differently depending on something about the variable or
+   function named by the symbol (such as what section it is in).
+
+   On the VAX, if using PIC, mark a SYMBOL_REF for a non-global
+   symbol so that we may use indirect accesses with it.  */
+
+#define ENCODE_SECTION_INFO(DECL)				\
+do								\
+  {								\
+    if ((flag_pic | TARGET_HALFPIC))				\
+      {								\
+	rtx rtl = (TREE_CODE_CLASS (TREE_CODE (DECL)) != 'd'	\
+		   ? TREE_CST_RTL (DECL) : DECL_RTL (DECL));	\
+								\
+	if (GET_CODE (rtl) == MEM)				\
+	  {							\
+	    SYMBOL_REF_FLAG (XEXP (rtl, 0))			\
+	      = (TREE_CODE_CLASS (TREE_CODE (DECL)) != 'd'	\
+		 || ! TREE_PUBLIC (DECL));			\
+	  }							\
+      }								\
+  }								\
+while (0)
+
+/* Put relocations in the constant pool in the writable data section.  */
+#undef  SELECT_RTX_SECTION
+#define SELECT_RTX_SECTION(MODE,RTX)		\
+{						\
+  if ((flag_pic || TARGET_HALFPIC)		\
+      && vax_symbolic_operand ((RTX), (MODE)))	\
+    data_section ();				\
+  else						\
+    readonly_data_section ();			\
+}
+
+/* Use sjlj exceptions. */
