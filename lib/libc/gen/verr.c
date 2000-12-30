12a9c7e898a23f1c5863731320fbe3a355620e21
martin <martin>
2000-12-30 16:27:33 +0000

    Be consistent with argument types vs. prototype; use _BSD_VA_LIST_ as the argument, instead of va_list. Might not make a difference on most ports, depending on toolchain/MD typedefs. Does make a difference on sparc64, where it wouldn't compile otherwise.

--- lib/libc/gen/verr.c
+++ lib/libc/gen/verr.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: verr.c,v 1.4 1999/08/17 03:47:40 mycroft Exp $	*/
+/*	$NetBSD: verr.c,v 1.5 2000/12/30 16:27:33 martin Exp $	*/
 
 /*-
  * Copyright (c) 1993
@@ -38,7 +38,7 @@
 #if 0
 static char sccsid[] = "@(#)err.c	8.1 (Berkeley) 6/4/93";
 #else
-__RCSID("$NetBSD: verr.c,v 1.4 1999/08/17 03:47:40 mycroft Exp $");
+__RCSID("$NetBSD: verr.c,v 1.5 2000/12/30 16:27:33 martin Exp $");
 #endif
 #endif /* LIBC_SCCS and not lint */
 
@@ -67,7 +67,7 @@ __dead void
 _verr(eval, fmt, ap)
 	int eval;
 	const char *fmt;
-	va_list ap;
+	_BSD_VA_LIST_ ap;
 {
 	int sverrno;
 
