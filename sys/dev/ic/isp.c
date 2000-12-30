cac5d093efd0f51985dbc33e64470c5d3f43d456
mjacob <mjacob@NetBSD.org>
2000-12-30 19:30:09 +0000

    There's a compiler bug for sparc64 that makes the construct:
    	char *foo = "XXXX";
    	...
    	foo[1] = 'Y';
    
    blow up (in the kernel) with the 2nd assignment. Work around it here-
    it's probably just as well- I was spending more in cpu instructions doing
    the assignment than I was saving in string space (it would have been
    cheap on a pdp11 or a 68k- but the address loads and assignments on something
    like sparc or alpha way outweigh the savings in space. Tsk.).

--- sys/dev/ic/isp.c
+++ sys/dev/ic/isp.c
@@ -1,4 +1,4 @@
-/* $NetBSD: isp.c,v 1.66 2000/12/28 22:27:46 mjacob Exp $ */
+/* $NetBSD: isp.c,v 1.67 2000/12/30 19:30:09 mjacob Exp $ */
 /*
  * This driver, which is contained in NetBSD in the files:
  *
@@ -235,13 +235,12 @@ isp_reset(isp)
 	ISP_WRITE(isp, HCCR, HCCR_CMD_PAUSE);
 
 	if (IS_FC(isp)) {
-		revname = "2X00";
 		switch (isp->isp_type) {
 		case ISP_HA_FC_2100:
-			revname[1] = '1';
+			revname = "2100";
 			break;
 		case ISP_HA_FC_2200:
-			revname[1] = '2';
+			revname = "2200";
 			break;
 		default:
 			break;
