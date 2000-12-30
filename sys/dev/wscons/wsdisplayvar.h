6bc94b0b38657da6277d6542be1bb8ec62ee6807
sato <sato>
2000-12-30 01:41:51 +0000

    notify console polling mode or not to lower level framebuffer driver. notify EMUL mode or not to lower level framebuffer driver. these may use framebuffer level hardware acceleration/software optimization.

--- sys/dev/wscons/wsdisplayvar.h
+++ sys/dev/wscons/wsdisplayvar.h
@@ -1,4 +1,4 @@
-/* $NetBSD: wsdisplayvar.h,v 1.17 2000/09/10 11:44:13 lukem Exp $ */
+/* $NetBSD: wsdisplayvar.h,v 1.18 2000/12/30 01:41:52 sato Exp $ */
 
 /*
  * Copyright (c) 1996, 1997 Christopher G. Demetriou.  All rights reserved.
@@ -110,6 +110,7 @@ struct wsdisplay_accessops {
 	int	(*show_screen) __P((void *, void *, int,
 				    void (*) (void *, int, int), void *));
 	int	(*load_font) __P((void *, void *, struct wsdisplay_font *));
+	void	(*pollc) __P((void *, int));
 };
 
 /*
