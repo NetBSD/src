5e8a2eac0cf97581f424228e3903c8158d5ce6f6
takemura <takemura>
2001-01-03 10:08:55 +0000

    replace 'long long' with int64_t to compile stand alone program with compiler other than GCC.

--- sys/arch/i386/include/ansi.h
+++ sys/arch/i386/include/ansi.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: ansi.h,v 1.14 2000/12/26 10:35:29 itojun Exp $	*/
+/*	$NetBSD: ansi.h,v 1.15 2001/01/03 10:08:59 takemura Exp $	*/
 
 /*-
  * Copyright (c) 1990, 1993
@@ -39,6 +39,8 @@
 #define	_ANSI_H_
 
 #include <sys/cdefs.h>
+
+#include <machine/int_types.h>
 
 /*
  * Types which are fundamental to the implementation and may appear in
@@ -91,7 +93,7 @@
  */
 typedef union {
 	char __mbstate8[128];
-	long long __mbstateL;	/* for alignment */
+	__int64_t __mbstateL;	/* for alignment */
 } __mbstate_t;
 #define	_BSD_MBSTATE_T_		__mbstate_t	/* mbstate_t */
 
--- sys/arch/pc532/include/types.h
+++ sys/arch/pc532/include/types.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: types.h,v 1.23 2000/05/16 05:45:49 thorpej Exp $	*/
+/*	$NetBSD: types.h,v 1.24 2001/01/03 10:09:01 takemura Exp $	*/
 
 /*-
  * Copyright (c) 1990 The Regents of the University of California.
@@ -39,6 +39,7 @@
 #define	_MACHINE_TYPES_H_
 
 #include <sys/cdefs.h>
+#include <machine/int_types.h>
 
 #if defined(_KERNEL)
 typedef struct label_t {
@@ -59,16 +60,14 @@ typedef unsigned long	vsize_t;
  * not possible for a machine/compiler combination.
  */
 #define	__BIT_TYPES_DEFINED__
-typedef	__signed char		   int8_t;
-typedef	unsigned char		 u_int8_t;
-typedef	short			  int16_t;
-typedef	unsigned short		u_int16_t;
-typedef	int			  int32_t;
-typedef	unsigned int		u_int32_t;
-/* LONGLONG */
-typedef	long long		  int64_t;
-/* LONGLONG */
-typedef	unsigned long long	u_int64_t;
+typedef	__int8_t	   int8_t;
+typedef	__uint8_t	 u_int8_t;
+typedef	__int16_t	  int16_t;
+typedef	__uint16_t	u_int16_t;
+typedef	__int32_t	  int32_t;
+typedef	__uint32_t	u_int32_t;
+typedef	__int64_t	  int64_t;
+typedef	__uint64_t	u_int64_t;
 
 typedef int32_t			register_t;
 
