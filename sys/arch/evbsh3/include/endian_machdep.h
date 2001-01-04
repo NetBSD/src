62cf4d65eed1b7eb6187c10a5c387bba6d9a4f6d
itojun <itojun@NetBSD.org>
2001-01-04 00:51:15 +0000

    look at __LITTLE_ENDIAN__ (gcc symbol) to determine which endian we are in. makes the file bi-endian.

--- sys/arch/evbsh3/include/endian_machdep.h
+++ sys/arch/evbsh3/include/endian_machdep.h
@@ -1,8 +1,9 @@
-/*	$NetBSD: endian_machdep.h,v 1.1 2000/03/17 00:09:20 mycroft Exp $	*/
+/*	$NetBSD: endian_machdep.h,v 1.2 2001/01/04 00:51:15 itojun Exp $	*/
 
-#if 1
-#define _BYTE_ORDER _BIG_ENDIAN
-#else
+/* XXX relying upon gcc symbol here... */
+#ifdef __LITTLE_ENDIAN__
 #define _BYTE_ORDER _LITTLE_ENDIAN
+#else
+#define _BYTE_ORDER _BIG_ENDIAN
 #endif
 #include <sh3/endian_machdep.h>
