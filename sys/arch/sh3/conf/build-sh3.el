3d7d29123fbb287093398f94dfc0dfb3fbb34881
itojun <itojun>
2001-01-04 07:09:47 +0000

    nuke obsolete Makefile templates. split build-sh3 into two, for sh3e[bl].

--- sys/arch/sh3/conf/build-sh3.el
+++ sys/arch/sh3/conf/build-sh3.el
@@ -2,13 +2,14 @@
 
 # just helping for cross compilation.
 
-MACHINE=sh3
-MACHINE_ARCH=sh3
+MACHINE=evbsh3
+MACHINE_ARCH=sh3eb
 # just in case you forgot to specify this when you built gcc...
 CFLAGS="-D__NetBSD__ -v"
 export MACHINE MACHINE_ARCH CFLAGS
-TARGET=sh-hitachi-coff
+TARGET=/usr/pkg/cross/sh-netbsdcoff
 
 make AR=$TARGET-ar AS=$TARGET-as CC=$TARGET-gcc LD=$TARGET-ld NM=$TARGET-nm \
-	RANLIB=$TARGET-ranlib SIZE=$TARGET-size STRIP=$TARGET-strip \
+	RANLIB=$TARGET-ranlib SIZE=$TARGET-size \
+	STRIP=$TARGET-strip OBJCOPY=$TARGET-objcopy \
 	CPP=$TARGET-cpp CXX=$TARGET-c++ $*
