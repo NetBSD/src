601ad4cd74934218c9c0b4aec4e054efc5feafca
jwise <jwise@NetBSD.org>
2001-01-04 05:06:15 +0000

    Use License templates from localsrc for license on new files.

--- games/sail/restart.h
+++ games/sail/restart.h
@@ -1,8 +1,11 @@
-/*	$NetBSD: restart.h,v 1.1 2001/01/04 03:51:24 jwise Exp $	*/
+/*	$NetBSD: restart.h,v 1.2 2001/01/04 05:06:15 jwise Exp $	*/
 
 /*-
  * Copyright (c) 2001 The NetBSD Foundation, Inc.
  * All rights reserved.
+ *
+ * This code is derived from software contributed to The NetBSD Foundation
+ * by 
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -12,7 +15,11 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. Neither the name of The NetBSD Foundation nor the names of its
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *        This product includes software developed by the NetBSD
+ *        Foundation, Inc. and its contributors.
+ * 4. Neither the name of The NetBSD Foundation nor the names of its
  *    contributors may be used to endorse or promote products derived
  *    from this software without specific prior written permission.
  *
