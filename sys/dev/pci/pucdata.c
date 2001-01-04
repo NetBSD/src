6d2b86bfccd5974c399ed1ae5b6d2b7f1c7b2e42
bouyer <bouyer@NetBSD.org>
2001-01-04 11:34:27 +0000

    Ops, add COM_FREQ to a few PUC_PORT_TYPE_COM I missed. Thanks to Bernd Ernesti for pointing this out.

--- sys/dev/pci/pucdata.c
+++ sys/dev/pci/pucdata.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: pucdata.c,v 1.13 2001/01/04 08:49:31 veego Exp $	*/
+/*	$NetBSD: pucdata.c,v 1.14 2001/01/04 11:34:27 bouyer Exp $	*/
 
 /*
  * Copyright (c) 1998, 1999 Christopher G. Demetriou.  All rights reserved.
@@ -556,7 +556,7 @@ const struct puc_device_description puc_devices[] = {
 	    {	0x1033,	0x007d,	0x1033,	0x8012	},
 	    {	0xffff,	0xffff,	0xffff,	0xffff	},
 	    {
-		{ PUC_PORT_TYPE_COM, 0x10, 0x00 },
+		{ PUC_PORT_TYPE_COM, 0x10, 0x00, COM_FREQ},
 	    },
 	},
 
@@ -574,8 +574,8 @@ const struct puc_device_description puc_devices[] = {
 	    {	0x1407,	0x0100,	0,	0	},
 	    {	0xffff,	0xfffc,	0,	0	},
 	    {
-		{ PUC_PORT_TYPE_COM, 0x10, 0x00 },
-		{ PUC_PORT_TYPE_COM, 0x14, 0x00 },
+		{ PUC_PORT_TYPE_COM, 0x10, 0x00, COM_FREQ },
+		{ PUC_PORT_TYPE_COM, 0x14, 0x00, COM_FREQ },
 	    },
 	},
 
@@ -584,7 +584,7 @@ const struct puc_device_description puc_devices[] = {
 	    {	0x12b9,	0x1008,	0,	0	},
 	    {	0xffff,	0xffff,	0,	0	},
 	    {
-		{ PUC_PORT_TYPE_COM, 0x10, 0x00 },
+		{ PUC_PORT_TYPE_COM, 0x10, 0x00, COM_FREQ },
 	    },
 	},
 
@@ -593,7 +593,7 @@ const struct puc_device_description puc_devices[] = {
 	    {	0x11c1,	0x0480,	0x0, 	0x0	},
 	    {	0xffff,	0xffff,	0x0,	0x0	},
 	    {
-		{ PUC_PORT_TYPE_COM,	0x14,	0x00	},
+		{ PUC_PORT_TYPE_COM,	0x14,	0x00, COM_FREQ },
 	    },
 	},
 
