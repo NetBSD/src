8e02cc4746bc4c4e7bea9cf5dd902f0cbb7ce040
martin <martin>
2001-01-05 13:09:15 +0000

    Enable the imported ISDN4BSD based ISDN subsystem.
    This is the kernel part (userland to follow soon) of the latest (and
    very probably last) release (version 0.96) of ISDN4BSD. ISDN4BSD has a
    homepage at http://www.freebsd-support.de/i4b/.
    
    It gives the user various ways to use the isdn connection: raw data (via
    the i4brbch "raw b-channel" device), ppp (via the isp "isdn PPP" device),
    voice/answering machine (the i4btel "telephone" device) and ip over isdn
    (the ipr device, "IP over raw ISDN").
    
    Supported are a bunch of common and older cards, more to be added soon
    after some cleanup. Currently only the european E-DSS1 variant of the
    ISDN D channel protocol is supported.

--- sys/arch/amiga/amiga/conf.c
+++ sys/arch/amiga/amiga/conf.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: conf.c,v 1.57 2000/01/31 22:49:12 mhitch Exp $	*/
+/*	$NetBSD: conf.c,v 1.58 2001/01/05 13:09:16 martin Exp $	*/
 
 /*-
  * Copyright (c) 1991 The Regents of the University of California.
@@ -113,41 +113,40 @@ dev_decl(filedesc,open);
 
 cdev_decl(wd);
 
-#ifdef __I4B_IS_INTEGRATED
 /* open, close, ioctl */
 #define cdev_i4bctl_init(c,n) { \
-      dev_init(c,n,open), dev_init(c,n,close), (dev_type_read((*))) enodev, \
-      (dev_type_write((*))) enodev, dev_init(c,n,ioctl), \
-      (dev_type_stop((*))) enodev, 0, seltrue, \
-      (dev_type_mmap((*))) enodev }
+	dev_init(c,n,open), dev_init(c,n,close), (dev_type_read((*))) enodev, \
+	(dev_type_write((*))) enodev, dev_init(c,n,ioctl), \
+	(dev_type_stop((*))) enodev, 0, seltrue, \
+	(dev_type_mmap((*))) enodev }
 
 /* open, close, read, write */
-#define       cdev_i4brbch_init(c,n) { \
-      dev_init(c,n,open), dev_init(c,n,close), dev_init(c,n,read), \
-      dev_init(c,n,write), (dev_type_ioctl((*))) enodev, \
-      (dev_type_stop((*))) enodev, \
-      0, (dev_type_poll((*))) enodev, (dev_type_mmap((*))) enodev }
+#define	cdev_i4brbch_init(c,n) { \
+	dev_init(c,n,open), dev_init(c,n,close), dev_init(c,n,read), \
+	dev_init(c,n,write), (dev_type_ioctl((*))) enodev, \
+	(dev_type_stop((*))) enodev, \
+	0, dev_init(c,n,poll), (dev_type_mmap((*))) enodev }
 
 /* open, close, read, write */
 #define	cdev_i4btel_init(c,n) { \
-      dev_init(c,n,open), dev_init(c,n,close), dev_init(c,n,read), \
-      dev_init(c,n,write), (dev_type_ioctl((*))) enodev, \
-      (dev_type_stop((*))) enodev, 0, (dev_type_poll((*))) enodev, \
-      (dev_type_mmap((*))) enodev, D_TTY }
+	dev_init(c,n,open), dev_init(c,n,close), dev_init(c,n,read), \
+	dev_init(c,n,write), (dev_type_ioctl((*))) enodev, \
+	(dev_type_stop((*))) enodev, \
+	0, dev_init(c,n,poll), (dev_type_mmap((*))) enodev, D_TTY }
 
 /* open, close, read, ioctl */
 #define cdev_i4btrc_init(c,n) { \
-      dev_init(c,n,open), dev_init(c,n,close), dev_init(c,n,read), \
-      (dev_type_write((*))) enodev, dev_init(c,n,ioctl), \
-      (dev_type_stop((*))) enodev, 0, (dev_type_poll((*))) enodev, \
-      (dev_type_mmap((*))) enodev }
+	dev_init(c,n,open), dev_init(c,n,close), dev_init(c,n,read), \
+	(dev_type_write((*))) enodev, dev_init(c,n,ioctl), \
+	(dev_type_stop((*))) enodev, 0, (dev_type_poll((*))) enodev, \
+	(dev_type_mmap((*))) enodev }
 
 /* open, close, read, poll, ioctl */
 #define cdev_i4b_init(c,n) { \
-      dev_init(c,n,open), dev_init(c,n,close), dev_init(c,n,read), \
-      (dev_type_write((*))) enodev, dev_init(c,n,ioctl), \
-      (dev_type_stop((*))) enodev, 0, dev_init(c,n,poll), \
-      (dev_type_mmap((*))) enodev }
+	dev_init(c,n,open), dev_init(c,n,close), dev_init(c,n,read), \
+	(dev_type_write((*))) enodev, dev_init(c,n,ioctl), \
+	(dev_type_stop((*))) enodev, 0, dev_init(c,n,poll), \
+	(dev_type_mmap((*))) enodev }	
       
 
 #include "i4b.h"
@@ -160,7 +159,6 @@ cdev_decl(i4bctl);
 cdev_decl(i4btrc);
 cdev_decl(i4brbch);
 cdev_decl(i4btel);
-#endif	/* __I4B_IS_INTEGRATED */
 
 struct cdevsw	cdevsw[] =
 {
@@ -209,20 +207,11 @@ struct cdevsw	cdevsw[] =
 	cdev_rnd_init(NRND,rnd),	/* 42: random source pseudo-device */
 	cdev_disk_init(NMD,md),		/* 43: memory disk */
 	cdev_scsibus_init(NSCSIBUS,scsibus), /* 44: SCSI bus */
-#ifdef __I4B_IS_INTEGRATED
- 	/* i4b character devices */
 	cdev_i4b_init(NI4B, i4b),		/* 45: i4b main device */
 	cdev_i4bctl_init(NI4BCTL, i4bctl),	/* 46: i4b control device */
 	cdev_i4brbch_init(NI4BRBCH, i4brbch),	/* 47: i4b raw b-channel */
 	cdev_i4btrc_init(NI4BTRC, i4btrc),	/* 48: i4b trace device */
 	cdev_i4btel_init(NI4BTEL, i4btel),	/* 49: i4b phone device */
-#else
-	cdev_lkm_dummy(),		/* 45 */
-	cdev_lkm_dummy(),		/* 46 */
-	cdev_lkm_dummy(),		/* 47 */
-	cdev_lkm_dummy(),		/* 48 */
-	cdev_lkm_dummy(),		/* 49 */
-#endif
 	cdev_disk_init(NRAID,raid),	/* 50: RAIDframe disk driver */
 	cdev_svr4_net_init(NSVR4_NET,svr4_net), /* 51: svr4 net pseudo-device */
 	cdev_disk_init(NWD,wd),		/* 52: IDE disk */
