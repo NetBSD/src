6b82e7fe4292830be607b8c3d8847a8fefcc3c1a
martin <martin@NetBSD.org>
2001-01-07 21:47:24 +0000

    64bit police. Rumors say there are archs without ISA busses, so avoid including (uneccesarily) isa bus headers in MI files. XXX this is the minimal solution, layer interface calls will have XXX to be revisited later

--- sys/netisdn/i4b_trace.c
+++ sys/netisdn/i4b_trace.c
@@ -27,7 +27,7 @@
  *	i4btrc - device driver for trace data read device
  *	---------------------------------------------------
  *
- *	$Id: i4b_trace.c,v 1.1.1.1 2001/01/05 12:50:00 martin Exp $
+ *	$Id: i4b_trace.c,v 1.2 2001/01/07 21:47:29 martin Exp $
  *
  *	last edit-date: [Fri Jan  5 11:33:47 2001]
  *
@@ -518,7 +518,7 @@ i4btrcioctl(dev_t dev, int cmd, caddr_t data, int flag, struct proc *p)
 		case I4B_TRC_SET:
 			if(cno < 0)
 				return ENOTTY;
-			(*ctrl_desc[cno].N_MGMT_COMMAND)(ctrl_desc[cno].unit, CMR_SETTRACE, (void *)*(unsigned int *)data);
+			(*ctrl_desc[cno].N_MGMT_COMMAND)(ctrl_desc[cno].unit, CMR_SETTRACE, (void *)*(unsigned long *)data);
 			break;
 
 		case I4B_TRC_SETA:
@@ -547,8 +547,8 @@ i4btrcioctl(dev_t dev, int cmd, caddr_t data, int flag, struct proc *p)
 					
 				outunit = unit;
 				analyzemode = 1;
-				(*ctrl_desc[cno].N_MGMT_COMMAND)(rxunit, CMR_SETTRACE, (int *)(tsa->rxflags & (TRACE_I | TRACE_D_RX | TRACE_B_RX)));
-				(*ctrl_desc[cno].N_MGMT_COMMAND)(txunit, CMR_SETTRACE, (int *)(tsa->txflags & (TRACE_I | TRACE_D_RX | TRACE_B_RX)));
+				(*ctrl_desc[cno].N_MGMT_COMMAND)(rxunit, CMR_SETTRACE, (void *)(unsigned long)(tsa->rxflags & (TRACE_I | TRACE_D_RX | TRACE_B_RX)));
+				(*ctrl_desc[cno].N_MGMT_COMMAND)(txunit, CMR_SETTRACE, (void *)(unsigned long)(tsa->txflags & (TRACE_I | TRACE_D_RX | TRACE_B_RX)));
 			}
 			break;
 
