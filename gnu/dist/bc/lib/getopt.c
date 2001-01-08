6dbdc6772f32887c609baed488c6704fd2718532
itojun <itojun>
2001-01-08 08:06:37 +0000

    need string.h for mem* prototypes. XXX not sure if we need to compile getopt{,1}.c in gnu/usr.bin/{bc,dc}...

--- gnu/dist/bc/lib/getopt.c
+++ gnu/dist/bc/lib/getopt.c
@@ -43,6 +43,7 @@ Cambridge, MA 02139, USA.  */
 #endif
 
 #include <stdio.h>
+#include <string.h>
 
 /* Comment out all this code if we are using the GNU C Library, and are not
    actually compiling the library itself.  This code is part of the GNU C
