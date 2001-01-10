cabb7d6479648564cff8ff4c6349e008ef08d8ec
thorpej <thorpej@NetBSD.org>
2001-01-10 15:50:20 +0000

    Add the 53c1510d and 53c895a.  1510d confirmed working by Mark Davies <mark@MCS.VUW.AC.NZ>.

--- sys/dev/pci/siop_pci_common.c
+++ sys/dev/pci/siop_pci_common.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: siop_pci_common.c,v 1.5 2000/12/28 22:59:15 sommerfeld Exp $	*/
+/*	$NetBSD: siop_pci_common.c,v 1.6 2001/01/10 15:50:20 thorpej Exp $	*/
 
 /*
  * Copyright (c) 2000 Manuel Bouyer.
@@ -151,6 +151,24 @@ const struct siop_product_desc siop_products[] = {
 	SF_CHIP_LS | SF_CHIP_10REGS |
 	SF_BUS_ULTRA2 | SF_BUS_WIDE,
 	7, 31, 7, 62, 8192
+	},
+	{ PCI_PRODUCT_SYMBIOS_895A,
+	0x00,
+	"Symbios Logic 53c895a (ultra2-wide scsi)",
+	SF_PCI_RL | SF_PCI_CLS | SF_PCI_WRI | SF_PCI_RM |
+	SF_CHIP_FIFO | SF_CHIP_PF | SF_CHIP_RAM | SF_CHIP_QUAD |
+	SF_CHIP_LS | SF_CHIP_10REGS |
+	SF_BUS_ULTRA2 | SF_BUS_WIDE,
+	7, 31, 7, 62, 8192
+	},
+	{ PCI_PRODUCT_SYMBIOS_1510D,
+	0x00,
+	"Symbios Logic 53c1510d (ultra2-wide scsi)",
+	SF_PCI_RL | SF_PCI_CLS | SF_PCI_WRI | SF_PCI_RM |
+	SF_CHIP_FIFO | SF_CHIP_PF | SF_CHIP_RAM | SF_CHIP_QUAD |
+	SF_CHIP_LS | SF_CHIP_10REGS |
+	SF_BUS_ULTRA2 | SF_BUS_WIDE,
+	7, 31, 7, 62, 4096
 	},
 	{ 0,
 	0x00,
