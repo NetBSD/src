492fd827a24164400050b6c9b39db6f7e2ce3c69
bjh21 <bjh21@NetBSD.org>
2001-01-10 19:02:05 +0000

    Header files which don't differ between arm32 and arm26 and can thus be shared.

---
+++ sys/arch/arm/include/cdefs.h
@@ -0,0 +1,8 @@
+/*	$NetBSD: cdefs.h,v 1.1 2001/01/10 19:02:05 bjh21 Exp $	*/
+
+#ifndef	_MACHINE_CDEFS_H_
+#define	_MACHINE_CDEFS_H_
+
+/* No arch-specific cdefs. */
+
+#endif /* !_MACHINE_CDEFS_H_ */
