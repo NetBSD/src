a2dacbe06db71e99c32f3b9e7e41a86786ade10c
bjh21 <bjh21@NetBSD.org>
2001-01-11 22:07:53 +0000

    Switch over to using the generic arm header files for everything that arm26 and arm32 agree on.

--- sys/arch/arm32/include/math.h
+++ sys/arch/arm32/include/math.h
@@ -1,11 +1,3 @@
-/*	$NetBSD: math.h,v 1.3 2000/02/05 14:04:37 kleink Exp $	*/
+/*	$NetBSD: math.h,v 1.4 2001/01/11 22:07:59 bjh21 Exp $	*/
 
-/*
- * ISO C99
- */
-#if !defined(_ANSI_SOURCE) && \
-    (!defined(_POSIX_C_SOURCE) && !defined(_XOPEN_SOURCE) || \
-     defined(_ISOC99_SOURCE) || (__STDC_VERSION__ - 0) >= 199901L)
-extern __const char	__nanf[];
-#define	NAN		(*(__const float *)(__const void *)__nanf)
-#endif
+#include <arm/math.h>
