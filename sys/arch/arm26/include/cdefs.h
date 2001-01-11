debadad9bdb404a56bbf5eea535e096b5430434e
bjh21 <bjh21@NetBSD.org>
2001-01-11 22:07:53 +0000

    Switch over to using the generic arm header files for everything that arm26 and arm32 agree on.

--- sys/arch/arm26/include/cdefs.h
+++ sys/arch/arm26/include/cdefs.h
@@ -1,8 +1,3 @@
-/*	$NetBSD: cdefs.h,v 1.2 2000/12/29 22:42:23 bjh21 Exp $	*/
+/*	$NetBSD: cdefs.h,v 1.3 2001/01/11 22:07:54 bjh21 Exp $	*/
 
-#ifndef	_MACHINE_CDEFS_H_
-#define	_MACHINE_CDEFS_H_
-
-/* No arch-specific cdefs. */
-
-#endif /* !_MACHINE_CDEFS_H_ */
+#include <arm/cdefs.h>
