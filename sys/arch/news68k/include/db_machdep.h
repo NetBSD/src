91e54eae8b21b8e860aac7f3259f2e092a21cb10
tsutsui <tsutsui@NetBSD.org>
2001-01-12 17:11:50 +0000

    Enable ELF symbol support to migrate. DB_AOUT_SYMBOLS is still defined.

--- sys/arch/news68k/include/db_machdep.h
+++ sys/arch/news68k/include/db_machdep.h
@@ -1,4 +1,7 @@
-/*	$NetBSD: db_machdep.h,v 1.1 1999/12/09 14:53:10 tsutsui Exp $	*/
+/*	$NetBSD: db_machdep.h,v 1.2 2001/01/12 17:11:50 tsutsui Exp $	*/
 
 /* Just use the common m68k definition */
 #include <m68k/db_machdep.h>
+
+#define	DB_ELF_SYMBOLS
+#define	DB_ELFSIZE	32
