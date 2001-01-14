7c6d6350f341153f0b842174ece882ab64bc676c
martin <martin@NetBSD.org>
2001-01-14 11:17:28 +0000

    The i4b raw b-channel devices actually do have an ioctl routine, so better add this to the cdevsw entry. Noted by Bruno Achauer.

--- sys/arch/arm32/arm32/conf.c
+++ sys/arch/arm32/arm32/conf.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: conf.c,v 1.46 2001/01/09 20:51:04 martin Exp $	*/
+/*	$NetBSD: conf.c,v 1.47 2001/01/14 11:17:29 martin Exp $	*/
 
 /*
  * Copyright (c) 1994-1998 Mark Brinicombe.
@@ -156,7 +156,7 @@ int nblkdev = sizeof(bdevsw) / sizeof(bdevsw[0]);
 /* open, close, read, write, poll */
 #define	cdev_i4brbch_init(c,n) { \
 	dev_init(c,n,open), dev_init(c,n,close), dev_init(c,n,read), \
-	dev_init(c,n,write), (dev_type_ioctl((*))) enodev, \
+	dev_init(c,n,write), dev_init(c,n,ioctl), \
 	(dev_type_stop((*))) enodev, \
 	0, dev_init(c,n,poll), (dev_type_mmap((*))) enodev }
 
