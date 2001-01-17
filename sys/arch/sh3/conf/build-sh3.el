9f6727bab110465ae11744b423e429433bf11447
itojun <itojun@NetBSD.org>
2001-01-17 06:03:54 +0000

    it looks that gas configured for little endian output runs as big endian assembler, sometimes.  explicitly have -little to specify little endian output.

--- sys/arch/sh3/conf/build-sh3.el
+++ sys/arch/sh3/conf/build-sh3.el
@@ -10,7 +10,7 @@ export MACHINE MACHINE_ARCH CFLAGS
 TARGET=/usr/pkg/cross/bin/shel-netbsdcoff
 CPP=`$TARGET-gcc -print-prog-name=cpp`
 
-make AR=$TARGET-ar AS=$TARGET-as CC=$TARGET-gcc LD=$TARGET-ld NM=$TARGET-nm \
+make AR=$TARGET-ar AS="$TARGET-as -little" CC=$TARGET-gcc LD=$TARGET-ld NM=$TARGET-nm \
 	RANLIB=$TARGET-ranlib SIZE=$TARGET-size \
 	STRIP=$TARGET-strip OBJCOPY=$TARGET-objcopy \
 	CXX=$TARGET-c++ CPP=$CPP $*
