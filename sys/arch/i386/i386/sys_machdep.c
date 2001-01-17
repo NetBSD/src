832e5e82e7a18c9b07c5812db3c5c51ae52247c0
tv <tv@NetBSD.org>
2001-01-17 01:44:52 +0000

    Pullup sys/arch/i386/i386/sys_machdep.c 1.55 [sommerfeld]: Validate gate targets. Fix by myself and Charles Hannum.

--- sys/arch/i386/i386/sys_machdep.c
+++ sys/arch/i386/i386/sys_machdep.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: sys_machdep.c,v 1.48 1999/05/12 19:28:29 thorpej Exp $	*/
+/*	$NetBSD: sys_machdep.c,v 1.48.14.1 2001/01/17 01:44:52 tv Exp $	*/
 
 /*-
  * Copyright (c) 1998 The NetBSD Foundation, Inc.
@@ -317,6 +317,17 @@ i386_set_ldt(p, args, retval)
 			break;
 		case SDT_SYS286CGT:
 		case SDT_SYS386CGT:
+			/*
+			 * Only allow call gates targeting a segment
+			 * in the LDT or a user segment in the fixed
+			 * part of the gdt.  Segments in the LDT are
+			 * constrained (below) to be user segments.
+			 */
+			if (desc.gd.gd_p != 0 && !ISLDT(desc.gd.gd_selector) &&
+			    ((IDXSEL(desc.gd.gd_selector) >= NGDT) ||
+			     (gdt[IDXSEL(desc.gd.gd_selector)].sd.sd_dpl !=
+				 SEL_UPL)))
+				return (EACCES);
 			/* Can't replace in use descriptor with gate. */
 			if (n == fsslot || n == gsslot)
 				return (EBUSY);
