5421c9c5892265256fc50d36dfcde8e6983136e6
manu <manu@NetBSD.org>
2001-01-19 01:44:46 +0000

    Added support for powerpc Linux compatibility

--- sys/compat/linux/linux_syscallargs.h
+++ sys/compat/linux/linux_syscallargs.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: linux_syscallargs.h,v 1.35 1998/12/15 19:31:30 itohy Exp $	*/
+/*	$NetBSD: linux_syscallargs.h,v 1.36 2001/01/19 01:46:23 manu Exp $	*/
 
 /*-
  * Copyright (c) 1998 The NetBSD Foundation, Inc.
@@ -45,6 +45,8 @@
 #include <compat/linux/arch/m68k/linux_syscallargs.h>
 #elif defined(__alpha__)
 #include <compat/linux/arch/alpha/linux_syscallargs.h>
+#elif defined(__powerpc__)
+#include <compat/linux/arch/powerpc/linux_syscallargs.h>
 #else
 #error Undefined linux_syscallargs.h machine type.
 #endif
