02787447a99c80d5c0d804c1036b68a1b4fffcce
thorpej <thorpej>
2001-01-20 02:15:01 +0000

    Clean up the PCI attachment a bit, and adjust the other pieces to suit.

--- sys/dev/pci/cy_pci.c
+++ sys/dev/pci/cy_pci.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: cy_pci.c,v 1.10 2000/12/28 22:59:11 sommerfeld Exp $	*/
+/*	$NetBSD: cy_pci.c,v 1.11 2001/01/20 02:15:02 thorpej Exp $	*/
 
 /*
  * cy_pci.c
@@ -7,8 +7,8 @@
  * (currently not tested with Cyclom-32 cards)
  *
  * Timo Rossi, 1996
- *
  */
+
 #include <sys/param.h>
 #include <sys/systm.h>
 #include <sys/device.h>
@@ -24,182 +24,147 @@
 #include <dev/ic/cyreg.h>
 #include <dev/ic/cyvar.h>
 
-static int cy_match_pci __P((struct device *, struct cfdata *, void *));
-static void cy_attach_pci  __P((struct device *, struct device *, void *));
-static int cy_map_pci __P((struct pci_attach_args *, bus_space_tag_t *,
-    bus_space_handle_t *, bus_size_t  *, bus_space_tag_t *,
-    bus_space_handle_t *, bus_size_t *));
-static void cy_unmap_pci __P((bus_space_tag_t, bus_space_handle_t,
-    bus_size_t, bus_space_tag_t, bus_space_handle_t, bus_size_t));
+struct cy_pci_softc {
+	struct cy_softc sc_cy;		/* real cy softc */
 
-struct cfattach cy_pci_ca = {
-	sizeof(struct cy_softc), cy_match_pci, cy_attach_pci
+	bus_space_tag_t sc_iot;		/* PLX runtime i/o tag */
+	bus_space_handle_t sc_ioh;	/* PLX runtime i/o handle */
 };
 
-static int
-cy_map_pci(pap, iotp, iohp, iosizep, memtp, memhp, memsizep)
-	struct pci_attach_args	*pap;
-	bus_space_tag_t		*iotp, *memtp;
-	bus_space_handle_t	*iohp, *memhp;
-	bus_size_t		*iosizep, *memsizep;
+int	cy_pci_match(struct device *, struct cfdata *, void *);
+void	cy_pci_attach(struct device *, struct device *, void *);
+
+struct cfattach cy_pci_ca = {
+	sizeof(struct cy_pci_softc), cy_pci_match, cy_pci_attach
+};
+
+static const struct cy_pci_product {
+	pci_product_id_t cp_product;	/* product ID */
+	pcireg_t cp_memtype;		/* memory type */
+} cy_pci_products[] = {
+	{ PCI_PRODUCT_CYCLADES_CYCLOMY_1,
+	  PCI_MAPREG_TYPE_MEM|PCI_MAPREG_MEM_TYPE_32BIT_1M },
+	{ PCI_PRODUCT_CYCLADES_CYCLOM4Y_1,
+	  PCI_MAPREG_TYPE_MEM|PCI_MAPREG_MEM_TYPE_32BIT_1M },
+	{ PCI_PRODUCT_CYCLADES_CYCLOM8Y_1,
+	  PCI_MAPREG_TYPE_MEM|PCI_MAPREG_MEM_TYPE_32BIT_1M },
+
+	{ PCI_PRODUCT_CYCLADES_CYCLOMY_2,
+	  PCI_MAPREG_TYPE_MEM|PCI_MAPREG_MEM_TYPE_32BIT },
+	{ PCI_PRODUCT_CYCLADES_CYCLOM4Y_2,
+	  PCI_MAPREG_TYPE_MEM|PCI_MAPREG_MEM_TYPE_32BIT },
+	{ PCI_PRODUCT_CYCLADES_CYCLOM8Y_2,
+	  PCI_MAPREG_TYPE_MEM|PCI_MAPREG_MEM_TYPE_32BIT },
+
+	{ 0,
+	  0 },
+};
+static const int cy_pci_nproducts =
+    sizeof(cy_pci_products) / sizeof(cy_pci_products[0]);
+
+static const struct cy_pci_product *
+cy_pci_lookup(const struct pci_attach_args *pa)
 {
-	int ioh_valid, memh_valid;
-	pcireg_t expected;
+	const struct cy_pci_product *cp;
+	int i;
 
-	/* Map I/O (if possible). */
-	ioh_valid = (pci_mapreg_map(pap, 0x14, PCI_MAPREG_TYPE_IO, 0,
-	    iotp, iohp, NULL, iosizep) == 0);
+	if (PCI_VENDOR(pa->pa_id) != PCI_VENDOR_CYCLADES)
+		return (NULL);
 
-	/* Map mem (if possible).  Expected mem type depends on board ID.  */
-	expected = PCI_MAPREG_TYPE_MEM;
-	switch (PCI_PRODUCT(pap->pa_id)) {
-	case PCI_PRODUCT_CYCLADES_CYCLOMY_1:
-	case PCI_PRODUCT_CYCLADES_CYCLOM4Y_1:
-	case PCI_PRODUCT_CYCLADES_CYCLOM8Y_1:
-		expected |= PCI_MAPREG_MEM_TYPE_32BIT_1M;
-		break;
-	default:
-		expected |= PCI_MAPREG_MEM_TYPE_32BIT;
+	for (i = 0; i < cy_pci_nproducts; i++) {
+		cp = &cy_pci_products[i];
+		if (PCI_PRODUCT(pa->pa_id) == cp->cp_product)
+			return (cp);
 	}
-	memh_valid = (pci_mapreg_map(pap, 0x18, expected, 0,
-	    memtp, memhp, NULL, memsizep) == 0);
-
-	if (ioh_valid && memh_valid)
-		return (1);
-
-	if (ioh_valid)
-		bus_space_unmap(*iotp, *iohp, *iosizep);
-	if (memh_valid)
-		bus_space_unmap(*memtp, *memhp, *memsizep);
-	return (0);
+	return (NULL);
 }
 
-static void
-cy_unmap_pci(iot, ioh, iosize, memt, memh, memsize)
-	bus_space_tag_t iot, memt;
-	bus_space_handle_t ioh, memh;
-	bus_size_t iosize, memsize;
-
+int
+cy_pci_match(struct device *parent, struct cfdata *match, void *aux)
 {
-	bus_space_unmap(iot, ioh, iosize);
-	bus_space_unmap(memt, memh, memsize);
+	struct pci_attach_args *pa = aux;
+
+	return (cy_pci_lookup(pa) != NULL);
 }
 
-static int
-cy_match_pci(parent, match, aux)
-	struct device	*parent;
-	struct cfdata	*match;
-	void		*aux;
+void
+cy_pci_attach(struct device *parent, struct device *self, void *aux)
 {
-	struct pci_attach_args *pap = aux;
-	bus_space_tag_t iot, memt;
-	bus_space_handle_t ioh, memh;
-	bus_size_t iosize, memsize;
-
-	if (PCI_VENDOR(pap->pa_id) != PCI_VENDOR_CYCLADES)
-		return 0;
-
-	switch (PCI_PRODUCT(pap->pa_id)) {
-	case PCI_PRODUCT_CYCLADES_CYCLOMY_1:
-	case PCI_PRODUCT_CYCLADES_CYCLOMY_2:
-	case PCI_PRODUCT_CYCLADES_CYCLOM4Y_1:
-	case PCI_PRODUCT_CYCLADES_CYCLOM4Y_2:
-	case PCI_PRODUCT_CYCLADES_CYCLOM8Y_1:
-	case PCI_PRODUCT_CYCLADES_CYCLOM8Y_2:
-		break;
-	default:
-		return 0;
-	}
-
-#ifdef CY_DEBUG
-	printf("cy: Found Cyclades PCI device, id = 0x%x\n", pap->pa_id);
-#endif
-
-	if (cy_map_pci(pap, &iot, &ioh, &iosize, &memt, &memh, &memsize) == 0)
-		return (0);
-
-#if 0
-	XXX probably should do something like this, but this code is just
-	XXX too broken.
-
-#ifdef CY_DEBUG
-	printf("%s: pci mapped mem 0x%lx (size %d), io 0x%x (size %d)\n",
-	    sc.sc_dev.dv_xname, memaddr, memsize, iobase, iosize);
-#endif
-
-	if ((rv = cy_find(&sc)) == 0)
-		printf("%s: PCI Cyclom card with no CD1400s!?\n",
-		    sc.sc_dev.dv_xname);
-#endif /* 0 */
-
-	cy_unmap_pci(iot, ioh, iosize, memt, memh, memsize);
-	return (1);
-}
-
-
-static void
-cy_attach_pci(parent, self, aux)
-	struct device  *parent, *self;
-	void *aux;
-{
-	struct cy_softc *sc = (void *) self;
-	struct pci_attach_args *pap = aux;
-	pci_intr_handle_t intrhandle;
-	bus_space_tag_t iot, memt;
-	bus_space_handle_t ioh, memh;
-	bus_size_t iosize, memsize;
+	struct cy_pci_softc *psc = (void *) self;
+	struct cy_softc *sc = (void *) &psc->sc_cy;
+	struct pci_attach_args *pa = aux;
+	pci_intr_handle_t ih;
+	const struct cy_pci_product *cp;
 	const char *intrstr;
 	int plx_ver;
 
 	sc->sc_bustype = CY_BUSTYPE_PCI;
 
-	if (cy_map_pci(pap, &iot, &ioh, &iosize, &memt, &memh,
-	    &memsize) == 0) {
-		printf(": unable to map device registers\n");
+	cp = cy_pci_lookup(pa);
+	if (cp == NULL)
+		panic("cy_pci_attach: impossible");
+
+	printf(": Cyclades-Y multiport serial\n");
+
+	if (pci_mapreg_map(pa, 0x14, PCI_MAPREG_TYPE_IO, 0,
+	    &psc->sc_iot, &psc->sc_ioh, NULL, NULL) != 0) {
+		printf("%s: unable to map PLX registers\n",
+		    sc->sc_dev.dv_xname);
 		return;
 	}
 
-	sc->sc_memt = memt;
-	sc->sc_bsh = memh;
+	if (pci_mapreg_map(pa, 0x18, cp->cp_memtype, 0,
+	    &sc->sc_memt, &sc->sc_bsh, NULL, NULL) != 0) {
+		printf("%s: unable to map device registers\n",
+		    sc->sc_dev.dv_xname);
+		return;
+	}
 
 	if (cy_find(sc) == 0) {
-		printf(": cannot find CD1400s\n");
+		printf("%s: unable to find CD1400s\n", sc->sc_dev.dv_xname);
 		return;
 	}
 
-	/* Set up interrupt handler. */
-	if (pci_intr_map(pap, &intrhandle) != 0) {
-		printf(": couldn't map PCI interrupt\n");
+	/*
+	 * XXX Like the Cyclades-Z, we should really check the EEPROM to
+	 * determine the "poll or interrupt" setting.  For now, we always
+	 * map the interrupt and enable it in the PLX.
+	 */
+
+	/* Map and establish the interrupt. */
+	if (pci_intr_map(pa, &ih) != 0) {
+		printf(": unable to map interrupt\n");
 		return;
 	}
-	intrstr = pci_intr_string(pap->pa_pc, intrhandle);
-	sc->sc_ih = pci_intr_establish(pap->pa_pc, intrhandle, IPL_TTY,
-	    cy_intr, sc);
+	intrstr = pci_intr_string(pa->pa_pc, ih);
+	sc->sc_ih = pci_intr_establish(pa->pa_pc, ih, IPL_TTY, cy_intr, sc);
 	if (sc->sc_ih == NULL) {
-		printf(": couldn't establish interrupt");
+		printf(": unable to establish interrupt");
 		if (intrstr != NULL)
 			printf(" at %s", intrstr);
 		printf("\n");
 		return;
 	}
-	if (intrstr != NULL)
-		printf(": interrupting at %s\n%s", intrstr, self->dv_xname);
+	printf("%s: interrupting at %s\n", sc->sc_dev.dv_xname, intrstr);
 
 	/* attach the hardware */
 	cy_attach(parent, self, aux);
 
-	plx_ver = bus_space_read_1(memt, memh, CY_PLX_VER) & 0x0f;
+	plx_ver = bus_space_read_1(sc->sc_memt, sc->sc_bsh, CY_PLX_VER) & 0x0f;
 
 	/* Enable PCI card interrupts */
 	switch (plx_ver) {
 	case CY_PLX_9050:
-		bus_space_write_2(iot, ioh, CY_PCI_INTENA_9050,
-		    bus_space_read_2(iot, ioh, CY_PCI_INTENA_9050) | 0x40);
+		bus_space_write_2(psc->sc_iot, psc->sc_ioh, CY_PCI_INTENA_9050,
+		    bus_space_read_2(psc->sc_iot, psc->sc_ioh,
+		    CY_PCI_INTENA_9050) | 0x40);
 		break;
+
 	case CY_PLX_9060:
 	case CY_PLX_9080:
 	default:
-		bus_space_write_2(iot, ioh, CY_PCI_INTENA,
-		    bus_space_read_2(iot, ioh, CY_PCI_INTENA) | 0x900);
+		bus_space_write_2(psc->sc_iot, psc->sc_ioh, CY_PCI_INTENA,
+		    bus_space_read_2(psc->sc_iot, psc->sc_ioh,
+		    CY_PCI_INTENA) | 0x900);
 	}
 }
