1ac55bd9d6eee8f9f9e8cd094aaa31c1272d4195
uch <uch@NetBSD.org>
2001-01-21 18:33:53 +0000

    hpcsh original device configuration.

--- sys/arch/hpcsh/hpcsh/conf.c
+++ sys/arch/hpcsh/hpcsh/conf.c
@@ -1,7 +1,8 @@
-/*	$NetBSD: conf.c,v 1.1 2001/01/17 05:21:49 itojun Exp $	*/
+/*	$NetBSD: conf.c,v 1.2 2001/01/21 18:33:53 uch Exp $	*/
 
-/*
- * Copyright (c) 1994, 1995 Charles M. Hannum.  All rights reserved.
+/*-
+ * Copyright (c) 2001 The NetBSD Foundation, Inc.
+ * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -13,20 +14,23 @@
  *    documentation and/or other materials provided with the distribution.
  * 3. All advertising materials mentioning features or use of this software
  *    must display the following acknowledgement:
- *	This product includes software developed by Charles Hannum.
- * 4. The name of the author may not be used to endorse or promote products
- *    derived from this software without specific prior written permission.
+ *        This product includes software developed by the NetBSD
+ *        Foundation, Inc. and its contributors.
+ * 4. Neither the name of The NetBSD Foundation nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
  *
- * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
- * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
- * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
- * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
- * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
- * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
  */
 
 #include <sys/param.h>
@@ -37,11 +41,14 @@
 #include <sys/conf.h>
 #include <sys/vnode.h>
 
-#include <machine/conf.h>
+#include <dev/cons.h>
 
+/*
+ * Block devices.
+ */
+bdev_decl(sw);
 #include "wd.h"
 bdev_decl(wd);
-bdev_decl(sw);
 #include "sd.h"
 bdev_decl(sd);
 #include "st.h"
@@ -50,215 +57,168 @@ bdev_decl(st);
 bdev_decl(cd);
 #include "vnd.h"
 bdev_decl(vnd);
-#include "ccd.h"
-bdev_decl(ccd);
 #include "raid.h"
 bdev_decl(raid);
+#include "ccd.h"
+bdev_decl(ccd);
 #include "md.h"
 bdev_decl(md);
 
-struct bdevsw	bdevsw[] =
-{
-	bdev_disk_init(NWD,wd),		/* 0: ST506/ESDI/IDE disk */
-	bdev_swap_init(1,sw),		/* 1: swap pseudo-device */
-	bdev_notdef(),			/* 2 (was floppy diskette) */
-	bdev_notdef(),			/* 3 (was QIC-02/QIC-36 tape) */
-	bdev_disk_init(NSD,sd),		/* 4: SCSI disk */
-	bdev_tape_init(NST,st),		/* 5: SCSI tape */
-	bdev_disk_init(NCD,cd),		/* 6: SCSI CD-ROM */
-	bdev_notdef(),			/* 7 (was Mitsumi CD-ROM) */
-	bdev_lkm_dummy(),		/* 8 */
-	bdev_lkm_dummy(),		/* 9 */
-	bdev_lkm_dummy(),		/* 10 */
-	bdev_lkm_dummy(),		/* 11 */
-	bdev_lkm_dummy(),		/* 12 */
-	bdev_lkm_dummy(),		/* 13 */
-	bdev_disk_init(NVND,vnd),	/* 14: vnode disk driver */
-	bdev_notdef(),			/* 15 (was Sony CD-ROM) */
-	bdev_disk_init(NCCD,ccd),	/* 16: concatenated disk driver */
-	bdev_disk_init(NMD,md),		/* 17: memory disk driver */
-	bdev_disk_init(NRAID,raid),	/* 18: RAIDframe disk driver */
-};
-int	nblkdev = sizeof(bdevsw) / sizeof(bdevsw[0]);
-
+/*
+ * Character devices.
+ */
+/* System */
 cdev_decl(cn);
 cdev_decl(ctty);
 #define	mmread	mmrw
 #define	mmwrite	mmrw
+dev_type_read(mmrw);
 cdev_decl(mm);
-#include "wdog.h"
-cdev_decl(wdog);
-cdev_decl(wd);
 cdev_decl(sw);
 #include "pty.h"
-#define	ptstty		ptytty
-#define	ptsioctl	ptyioctl
+#define ptstty ptytty
+#define ptsioctl ptyioctl
 cdev_decl(pts);
-#define	ptctty		ptytty
-#define	ptcioctl	ptyioctl
+#define ptctty ptytty
+#define ptcioctl ptyioctl
 cdev_decl(ptc);
 cdev_decl(log);
-#include "com.h"
-cdev_decl(com);
-#include "sci.h"
-cdev_decl(sci);
-#include "scif.h"
-cdev_decl(scif);
-cdev_decl(fd);
+
+/* Disk */
+cdev_decl(wd);
 cdev_decl(sd);
 cdev_decl(st);
+cdev_decl(cd);
+cdev_decl(md);
+cdev_decl(raid);
+cdev_decl(vnd);
+cdev_decl(ccd);
+
+/* Network */
+#include "tun.h"
+cdev_decl(tun);
+#include "bpfilter.h"
+cdev_decl(bpf);
+#include "ipfilter.h"
+cdev_decl(ipl);
+
+/* SCSI misc */
+#include "scsibus.h"
+cdev_decl(scsibus);
+#include "ch.h"
+cdev_decl(ch);
 #include "ss.h"
 cdev_decl(ss);
 #include "uk.h"
 cdev_decl(uk);
-cdev_decl(cd);
-#include "ch.h"
-cdev_decl(ch);
-dev_decl(filedesc,open);
-#include "bpfilter.h"
-cdev_decl(bpf);
-cdev_decl(md);
-#include "cy.h"
-cdev_decl(cy);
-#include "tun.h"
-cdev_decl(tun);
-cdev_decl(vnd);
-#include "audio.h"
-cdev_decl(audio);
-cdev_decl(svr4_net);
-cdev_decl(ccd);
-cdev_decl(raid);
-#include "vcoda.h"
-cdev_decl(vc_nb_);
 
-#include "esh.h"
-cdev_decl(esh_fp);
-#include "scsibus.h"
-cdev_decl(scsibus);
+/* wscons */
+#include "wsdisplay.h"
+cdev_decl(wsdisplay);
+#include "wskbd.h"
+cdev_decl(wskbd);
+#include "wsmouse.h"
+cdev_decl(wsmouse);
+#include "wsmux.h"
+cdev_decl(wsmux);
 
-#include "ipfilter.h"
+/* misc */
 #include "rnd.h"
 
-struct cdevsw	cdevsw[] =
+/* SH specific */
+#define scicnpollc	nullcnpollc
+#define scifcnpollc	nullcnpollc
+cons_decl(sci);
+cons_decl(scif);
+cons_decl(com);
+
+#include "sci.h"
+cdev_decl(sci);
+#include "scif.h"
+cdev_decl(scif);
+
+struct bdevsw bdevsw[] =
 {
-	cdev_cn_init(1,cn),		/* 0: virtual console */
-	cdev_ctty_init(1,ctty),		/* 1: controlling terminal */
-	cdev_mm_init(1,mm),		/* 2: /dev/{null,mem,kmem,...} */
-	cdev_disk_init(NWD,wd),		/* 3: ST506/ESDI/IDE disk */
-	cdev_swap_init(1,sw),		/* 4: /dev/drum (swap pseudo-device) */
-	cdev_tty_init(NPTY,pts),	/* 5: pseudo-tty slave */
-	cdev_ptc_init(NPTY,ptc),	/* 6: pseudo-tty master */
-	cdev_log_init(1,log),		/* 7: /dev/klog */
-	cdev_tty_init(NCOM,com),	/* 8: serial port */
-	cdev_notdef(),			/* 9 (was floppy disk) */
-	cdev_tty_init(NSCIF,scif),	/* 10: serial with FIFO */
-	cdev_tty_init(NSCI,sci),	/* 11: serial */
-	cdev_notdef(),			/* 12: */
-	cdev_disk_init(NSD,sd),		/* 13: SCSI disk */
-	cdev_tape_init(NST,st),		/* 14: SCSI tape */
-	cdev_disk_init(NCD,cd),		/* 15: SCSI CD-ROM */
-	cdev_notdef(),			/* 16: */
-	cdev_ch_init(NCH,ch),		/* 17: SCSI autochanger */
-	cdev_disk_init(NCCD,ccd),	/* 18: concatenated disk driver */
-	cdev_scanner_init(NSS,ss),	/* 19: SCSI scanner */
-	cdev_uk_init(NUK,uk),		/* 20: SCSI unknown */
-	cdev_notdef(),			/* 21 (was APM) */
-	cdev_fd_init(1,filedesc),	/* 22: file descriptor pseudo-device */
-	cdev_bpftun_init(NBPFILTER,bpf),/* 23: Berkeley packet filter */
-	cdev_disk_init(NMD,md),		/* 24: memory disk driver */
-	cdev_notdef(),			/* 25 */
-	cdev_notdef(),			/* 26 (was joystick) */
-	cdev_notdef(),			/* 27 (was PC speaker) */
-	cdev_lkm_init(NLKM,lkm),	/* 28: loadable module driver */
-	cdev_lkm_dummy(),		/* 29 */
-	cdev_lkm_dummy(),		/* 30 */
-	cdev_lkm_dummy(),		/* 31 */
-	cdev_lkm_dummy(),		/* 32 */
-	cdev_lkm_dummy(),		/* 33 */
-	cdev_lkm_dummy(),		/* 34 */
-	cdev_notdef(),			/* 35 (was Microsoft mouse) */
-	cdev_notdef(),			/* 36 (was Logitech mouse) */
-	cdev_notdef(),			/* 37 (was PS/2 mouse) */
-	cdev_notdef(),			/* 38 (was Cyclom serial port) */
-	cdev_notdef(),			/* 39 (was Mitsumi CD-ROM) */
-	cdev_bpftun_init(NTUN,tun),	/* 40: network tunnel */
-	cdev_disk_init(NVND,vnd),	/* 41: vnode disk driver */
-	cdev_audio_init(NAUDIO,audio),	/* 42: generic audio I/O */
-	cdev_notdef(),			/* 43 */
-	cdev_ipf_init(NIPFILTER,ipl),	/* 44: ip-filter device */
-	cdev_notdef(),			/* 45 (was planetconnect satlink) */
-	cdev_rnd_init(NRND,rnd),	/* 46: random source pseudo-device */
-	cdev_vc_nb_init(NVCODA,vc_nb_),	/* 47: coda file system psdev */
-	cdev_scsibus_init(NSCSIBUS,scsibus), /* 48: SCSI bus */
-	cdev_disk_init(NRAID,raid),	/* 49: RAIDframe disk driver */
-	cdev_esh_init(NESH, esh_fp),	/* 50: HIPPI (esh) raw device */
-	cdev_wdog_init(NWDOG,wdog),	/* 51: watchdog timer */
+	bdev_swap_init(1,sw),		/* 0: swap pseudo-device */
+	bdev_disk_init(NWD,wd),		/* 1: IDE disk driver */
+	bdev_notdef(),			/* 2: reserve */
+	bdev_disk_init(NSD,sd),		/* 3: SCSI disk driver */
+	bdev_tape_init(NST,st),		/* 4: SCSI tape */
+	bdev_disk_init(NCD,cd),		/* 5: SCSI CD-ROM */
+	bdev_disk_init(NMD,md),		/* 6: memory disk driver */
+	bdev_disk_init(NCCD,ccd),	/* 7: concatenated disk driver */
+	bdev_disk_init(NVND,vnd),	/* 8: vnode disk driver */
+	bdev_disk_init(NRAID,raid),	/* 9: RAIDframe disk driver */
 };
-int	nchrdev = sizeof(cdevsw) / sizeof(cdevsw[0]);
 
-int	mem_no = 2; 	/* major device number of memory special file */
-
-/*
- * Swapdev is a fake device implemented
- * in sw.c used only internally to get to swstrategy.
- * It cannot be provided to the users, because the
- * swstrategy routine munches the b_dev and b_blkno entries
- * before calling the appropriate driver.  This would horribly
- * confuse, e.g. the hashing routines. Instead, /dev/drum is
- * provided as a character (raw) device.
- */
-dev_t	swapdev = makedev(1, 0);
-
-/*
- * Returns true if dev is /dev/mem or /dev/kmem.
- */
-int
-iskmemdev(dev)
-	dev_t dev;
+struct cdevsw cdevsw[] =
 {
+	cdev_mm_init(1, mm),            /*  0: /dev/{null,mem,kmem,...} */
+	cdev_swap_init(1, sw),          /*  1: /dev/drum(swap pseudo-device) */
+	cdev_cn_init(1, cn),            /*  2: virtual console */
+	cdev_ctty_init(1,ctty),         /*  3: controlling terminal */
+	cdev_fd_init(1,filedesc),	/*  4: file descriptor pseudo-dev */
+	cdev_log_init(1,log),           /*  5: /dev/klog */
+	cdev_ptc_init(NPTY,ptc),        /*  6: pseudo-tty master */
+	cdev_tty_init(NPTY,pts),        /*  7: pseudo-tty slave */
+	cdev_notdef(),			/*  8: (reserved) NS16550 compatible */
+	cdev_notdef(),		        /*  9: (reserved) parallel printer*/
+	cdev_disk_init(NWD, wd),        /* 10: ST506/ESDI/IDE disk */
+	cdev_notdef(),			/* 11: (reserved) floppy diskette */
+	cdev_disk_init(NMD, md),        /* 12: memory disk driver */
+	cdev_disk_init(NCCD,ccd),	/* 13: concatenated disk driver */
+	cdev_disk_init(NVND,vnd),       /* 14: vnode disk driver */
+	cdev_disk_init(NRAID,raid),	/* 15: RAIDframe disk driver */
+	cdev_scsibus_init(NSCSIBUS,
+	    scsibus),			/* 16: SCSI bus */
+	cdev_disk_init(NSD,sd),		/* 17: SCSI disk */
+	cdev_tape_init(NST,st),		/* 18: SCSI tape */
+	cdev_disk_init(NCD,cd),		/* 19: SCSI CD-ROM */
+	cdev_ch_init(NCH,ch),	 	/* 20: SCSI autochanger */
+	cdev_uk_init(NUK,uk),	 	/* 21: SCSI unknown */
+	cdev_scanner_init(NSS,ss),	/* 22: SCSI scanner */
+	cdev_ipf_init(NIPFILTER,ipl),	/* 23: ip-filter device */
+	cdev_bpftun_init(NTUN,tun),	/* 24: network tunnel */
+	cdev_bpftun_init(NBPFILTER,bpf),/* 25: Berkeley packet filter */
+	cdev_wsdisplay_init(NWSDISPLAY,
+	    wsdisplay),			/* 26: ws displays. */
+	cdev_mouse_init(NWSKBD, wskbd),	/* 27: ws keyboards */
+	cdev_mouse_init(NWSMOUSE,
+	    wsmouse),			/* 28: ws mice */
+	cdev_mouse_init(NWSMUX,	wsmux), /* 29: ws multiplexor */
+	cdev_rnd_init(NRND,rnd),	/* 30: random source pseudo-device */
+	cdev_tty_init(NSCIF,scif),	/* 31: SH internal serial with FIFO */
+	cdev_tty_init(NSCI,sci),	/* 32: SH internal serial */
+};
 
-	return (major(dev) == mem_no && (minor(dev) < 2 || minor(dev) == 14));
-}
-
-/*
- * Returns true if dev is /dev/zero.
- */
-int
-iszerodev(dev)
-	dev_t dev;
-{
-
-	return (major(dev) == mem_no && minor(dev) == 12);
-}
-
-static int chrtoblktbl[] = {
+static int chrtoblktbl[] =  {
 	/* XXXX This needs to be dynamic for LKMs. */
 	/*VCHR*/	/*VBLK*/
 	/*  0 */	NODEV,
 	/*  1 */	NODEV,
 	/*  2 */	NODEV,
-	/*  3 */	0,
+	/*  3 */	NODEV,
 	/*  4 */	NODEV,
 	/*  5 */	NODEV,
 	/*  6 */	NODEV,
 	/*  7 */	NODEV,
 	/*  8 */	NODEV,
 	/*  9 */	NODEV,
-	/* 10 */	NODEV,
-	/* 11 */	NODEV,
-	/* 12 */	NODEV,
-	/* 13 */	4,
-	/* 14 */	5,
-	/* 15 */	6,
+	/* 10 */	1, /* wd */
+	/* 11 */	NODEV, /* reserve */
+	/* 12 */	6, /* md */
+	/* 13 */	7, /* ccd */
+	/* 14 */	8, /* vnd */
+	/* 15 */	9, /* raid */
 	/* 16 */	NODEV,
-	/* 17 */	NODEV,
-	/* 18 */	16,
-	/* 19 */	NODEV,
+	/* 17 */	3, /* sd */
+	/* 18 */	4, /* st */
+	/* 19 */	5, /* cd */
 	/* 20 */	NODEV,
 	/* 21 */	NODEV,
 	/* 22 */	NODEV,
 	/* 23 */	NODEV,
-	/* 24 */	17,
+	/* 24 */	NODEV,
 	/* 25 */	NODEV,
 	/* 26 */	NODEV,
 	/* 27 */	NODEV,
@@ -275,42 +235,7 @@ static int chrtoblktbl[] = {
 	/* 38 */	NODEV,
 	/* 39 */	NODEV,
 	/* 40 */	NODEV,
-	/* 41 */	14,
-	/* 42 */	NODEV,
-	/* 43 */	NODEV,
-	/* 44 */	NODEV,
-	/* 45 */	NODEV,
-	/* 46 */	NODEV,
-	/* 47 */	NODEV,
-	/* 48 */	NODEV,
-	/* 49 */	NODEV,
-	/* 50 */	NODEV,
 };
-
-/*
- * Convert a character device number to a block device number.
- */
-dev_t
-chrtoblk(dev)
-	dev_t dev;
-{
-	int blkmaj;
-
-	if (major(dev) >= nchrdev)
-		return (NODEV);
-	blkmaj = chrtoblktbl[major(dev)];
-	if (blkmaj == NODEV)
-		return (NODEV);
-	return (makedev(blkmaj, minor(dev)));
-}
-
-#include <dev/cons.h>
-
-#define scicnpollc	nullcnpollc
-#define scifcnpollc	nullcnpollc
-cons_decl(sci);
-cons_decl(scif);
-cons_decl(com);
 
 struct consdev constab[] = {
 #if NSCI > 0
@@ -324,3 +249,50 @@ struct consdev constab[] = {
 #endif
 	{ 0 },
 };
+
+/*
+ * Swapdev is a fake block device implemented  in sw.c and only used 
+ * internally to get to swstrategy.  It cannot be provided to the
+ * users, because the swstrategy routine munches the b_dev and b_blkno
+ * entries before calling the appropriate driver.  This would horribly
+ * confuse, e.g. the hashing routines.   User access (e.g., for libkvm
+ * and ps) is provided through the /dev/drum character (raw) device.
+ */
+dev_t	swapdev = makedev(0, 0);
+int	mem_no = 0; 	/* major device number of memory special file */
+int	nblkdev = sizeof(bdevsw) / sizeof(bdevsw[0]);
+int	nchrdev = sizeof(cdevsw) / sizeof(cdevsw[0]);
+
+/*
+ * Routine that identifies /dev/mem and /dev/kmem.
+ */
+int
+iskmemdev(dev_t dev)
+{
+	return (major(dev) == mem_no && (minor(dev) < 2 || minor(dev) == 14));
+}
+
+/*
+ * Returns true if dev is /dev/zero.
+ */
+int
+iszerodev(dev_t dev)
+{
+	return (major(dev) == mem_no && minor(dev) == 12);
+}
+
+/*
+ * Routine to convert from character to block device number.
+ */
+dev_t
+chrtoblk(dev_t dev)
+{
+	int blkmaj;
+
+	if (major(dev) >= nchrdev)
+		return (NODEV);
+	blkmaj = chrtoblktbl[major(dev)];
+	if (blkmaj == NODEV)
+		return (NODEV);
+	return (makedev(blkmaj, minor(dev)));
+}
