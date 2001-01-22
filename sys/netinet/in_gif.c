9496b5df3daa119bf652f6d62340f0d08fce5c4b
itojun <itojun@NetBSD.org>
2001-01-22 07:51:01 +0000

    make it possible to turn off ingress filter on gif/stf tunnel egress, by using IFF_LINK2.  (part of) PR 11163 from Ken Raeburn.

--- sys/netinet/in_gif.c
+++ sys/netinet/in_gif.c
@@ -1,5 +1,5 @@
-/*	$NetBSD: in_gif.c,v 1.15 2000/07/05 21:01:38 thorpej Exp $	*/
-/*	$KAME: in_gif.c,v 1.39 2000/04/26 05:33:31 itojun Exp $	*/
+/*	$NetBSD: in_gif.c,v 1.16 2001/01/22 07:51:01 itojun Exp $	*/
+/*	$KAME: in_gif.c,v 1.50 2001/01/22 07:27:16 itojun Exp $	*/
 
 /*
  * Copyright (C) 1995, 1996, 1997, and 1998 WIDE Project.
@@ -54,6 +54,7 @@
 #if !defined(__FreeBSD__) || __FreeBSD__ < 3
 #include <sys/ioctl.h>
 #endif
+#include <sys/syslog.h>
 
 #if defined(__FreeBSD__) && __FreeBSD__ >= 3
 #include <sys/malloc.h>
@@ -107,7 +108,7 @@ in_gif_output(ifp, family, m, rt)
 	struct mbuf	*m;
 	struct rtentry *rt;
 {
-	register struct gif_softc *sc = (struct gif_softc*)ifp;
+	struct gif_softc *sc = (struct gif_softc*)ifp;
 	struct sockaddr_in *dst = (struct sockaddr_in *)&sc->gif_ro.ro_dst;
 	struct sockaddr_in *sin_src = (struct sockaddr_in *)sc->gif_psrc;
 	struct sockaddr_in *sin_dst = (struct sockaddr_in *)sc->gif_pdst;
@@ -121,8 +122,6 @@ in_gif_output(ifp, family, m, rt)
 		m_freem(m);
 		return EAFNOSUPPORT;
 	}
-
-	bzero(&iphdr, sizeof(iphdr));
 
 	switch (family) {
 #ifdef INET
@@ -168,6 +167,7 @@ in_gif_output(ifp, family, m, rt)
 		return EAFNOSUPPORT;
 	}
 
+	bzero(&iphdr, sizeof(iphdr));
 	iphdr.ip_src = sin_src->sin_addr;
 	if (ifp->if_flags & IFF_LINK0) {
 		/* multi-destination mode */
@@ -398,7 +398,8 @@ gif_encapcheck4(m, off, proto, arg)
 	}
 
 	/* ingress filters on outer source */
-	if ((m->m_flags & M_PKTHDR) != 0 && m->m_pkthdr.rcvif) {
+	if ((sc->gif_if.if_flags & IFF_LINK2) == 0 &&
+	    (m->m_flags & M_PKTHDR) != 0 && m->m_pkthdr.rcvif) {
 		struct sockaddr_in sin;
 		struct rtentry *rt;
 
@@ -411,10 +412,14 @@ gif_encapcheck4(m, off, proto, arg)
 #else
 		rt = rtalloc1((struct sockaddr *)&sin, 0);
 #endif
-		if (!rt)
-			return 0;
-		if (rt->rt_ifp != m->m_pkthdr.rcvif) {
-			rtfree(rt);
+		if (!rt || rt->rt_ifp != m->m_pkthdr.rcvif) {
+#if 0
+			log(LOG_WARNING, "%s: packet from 0x%x dropped "
+			    "due to ingress filter\n", if_name(&sc->gif_if),
+			    (u_int32_t)ntohl(sin.sin_addr.s_addr));
+#endif
+			if (rt)
+				rtfree(rt);
 			return 0;
 		}
 		rtfree(rt);
