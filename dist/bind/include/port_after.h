ce705f16fa33e0e4db7b710a0e7f7b5996857727
he <he@NetBSD.org>
2001-01-28 17:09:42 +0000

    Pull up revisions 1.2-1.3 (requested by itojun):   Upgrade to BIND version 8.2.3.

--- dist/bind/include/port_after.h
+++ dist/bind/include/port_after.h
@@ -1,8 +1,7 @@
-/*	$NetBSD: port_after.h,v 1.1.1.1.2.2 1999/12/04 17:08:36 he Exp $	*/
+/*	$NetBSD: port_after.h,v 1.1.1.1.2.3 2001/01/28 17:09:42 he Exp $	*/
 
 #ifndef	PORT_AFTER_H
 #define	PORT_AFTER_H
-#define CAN_RECONNECT
 #define USE_POSIX
 #define POSIX_SIGNALS
 #define USE_UTIME
@@ -14,7 +13,7 @@
 #define SETPWENT_VOID
 #define HAVE_SA_LEN
 #define HAVE_MINIMUM_IFREQ
-#define USE_LOG_CONS
+#undef USE_LOG_CONS
 #define RLIMIT_TYPE rlim_t
 #define RLIMIT_LONGLONG
 #define RLIMIT_FILE_INFINITY
@@ -77,4 +76,8 @@ struct sockaddr_in6 {
 	u_int32_t	sin6_scope_id;	/* set of interfaces for a scope */
 };
 #endif	/* HAS_INET6_STRUCTS */
+#ifndef ISC_FACILITY
+#define ISC_FACILITY LOG_DAEMON
+#endif
+
 #endif	/* ! PORT_AFTER_H */
