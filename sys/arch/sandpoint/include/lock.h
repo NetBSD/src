fbeb623b6db1412df367890bc94cb8b2d52b5a94
briggs <briggs>
2001-02-04 18:32:10 +0000

    Support for the Motorola Sandpoint reference platform using the MPC8240 processor module.  See the README for more information.

---
+++ sys/arch/sandpoint/include/lock.h
@@ -0,0 +1,4 @@
+/*	$NetBSD: lock.h,v 1.1 2001/02/04 18:32:14 briggs Exp $	*/
+
+/* Just use the common PowerPC definition */
+#include <powerpc/lock.h>
---
+++ sys/arch/sandpoint/include/signal.h
@@ -0,0 +1,3 @@
+/*	$NetBSD: signal.h,v 1.1 2001/02/04 18:32:15 briggs Exp $	*/
+
+#include <powerpc/signal.h>
