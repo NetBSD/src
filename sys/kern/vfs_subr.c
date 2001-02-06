eaaa3051a3d5c21184739637bff180680b3c2eb1
chs <chs@NetBSD.org>
2001-02-06 10:58:55 +0000

    in vtruncbuf(), pass 0 (meaning everything at or past the start of the range) instead of the vnode's size to pgo_flush() since there can be pages past EOF. in the same call, cast "lbn" to voff_t to avoid overflow.

--- sys/kern/vfs_subr.c
+++ sys/kern/vfs_subr.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: vfs_subr.c,v 1.144 2001/01/08 07:05:47 chs Exp $	*/
+/*	$NetBSD: vfs_subr.c,v 1.145 2001/02/06 10:58:55 chs Exp $	*/
 
 /*-
  * Copyright (c) 1997, 1998 The NetBSD Foundation, Inc.
@@ -747,8 +747,8 @@ vtruncbuf(vp, lbn, slpflag, slptimeo)
 	if (vp->v_type == VREG) {
 		simple_lock(&uobj->vmobjlock);
 		rv = (uobj->pgops->pgo_flush)(uobj,
-		    round_page(lbn << vp->v_mount->mnt_fs_bshift),
-		    vp->v_uvm.u_size, PGO_FREE|PGO_SYNCIO);
+		    round_page((voff_t)lbn << vp->v_mount->mnt_fs_bshift), 0,
+		    PGO_FREE|PGO_SYNCIO);
 		simple_unlock(&uobj->vmobjlock);
 		if (!rv) {
 			splx(s);
