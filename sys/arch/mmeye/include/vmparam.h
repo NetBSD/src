c435e17ae6095d909656455206c7c735744c3179
uch <uch@NetBSD.org>
2001-02-07 14:53:09 +0000

    move VM_PHYSSEG_MAX, VM_NFREELIST, VM_FREELIST_DEFAULT defines to machine/vmparam.h

--- sys/arch/mmeye/include/vmparam.h
+++ sys/arch/mmeye/include/vmparam.h
@@ -1,3 +1,8 @@
-/*	$NetBSD: vmparam.h,v 1.1 1999/09/13 10:31:00 itojun Exp $	*/
+/*	$NetBSD: vmparam.h,v 1.2 2001/02/07 14:53:09 uch Exp $	*/
 
 #include <sh3/vmparam.h>
+
+#define VM_PHYSSEG_MAX		1
+
+#define VM_NFREELIST		1
+#define VM_FREELIST_DEFAULT	0
