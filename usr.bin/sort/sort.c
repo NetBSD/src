beb41b2fd78c403c36eefa3137e7bc97529180b8
jdolecek <jdolecek@NetBSD.org>
2001-02-07 20:31:44 +0000

    Since -T is used to select directory for temporary files in other sort implementations, we should avoid using it for something else. Use (new) flag -w for setting record delimiter, make -T noop.

--- usr.bin/sort/sort.c
+++ usr.bin/sort/sort.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: sort.c,v 1.19 2001/02/07 19:47:44 jdolecek Exp $	*/
+/*	$NetBSD: sort.c,v 1.20 2001/02/07 20:31:44 jdolecek Exp $	*/
 
 /*-
  * Copyright (c) 1993
@@ -51,7 +51,7 @@ __COPYRIGHT("@(#) Copyright (c) 1993\n\
 #endif /* not lint */
 
 #ifndef lint
-__RCSID("$NetBSD: sort.c,v 1.19 2001/02/07 19:47:44 jdolecek Exp $");
+__RCSID("$NetBSD: sort.c,v 1.20 2001/02/07 20:31:44 jdolecek Exp $");
 __SCCSID("@(#)sort.c	8.1 (Berkeley) 6/6/93");
 #endif /* not lint */
 
@@ -112,7 +112,7 @@ main(argc, argv)
 
 	fixit(&argc, argv);
 
-	while ((ch = getopt(argc, argv, "bcdfik:mHno:rsSt:T:ux")) != -1) {
+	while ((ch = getopt(argc, argv, "bcdfik:mHno:rsSt:T:uw:x")) != -1) {
 		switch (ch) {
 		case 'b':
 			fldtab->flags |= BI | BT;
@@ -160,6 +160,9 @@ main(argc, argv)
 				errx(2, "record/field delimiter clash");
 			break;
 		case 'T':
+			/* -T tmpdir, noop (not supported) */
+			break;
+		case 'w':
 			if (REC_D != '\n')
 				usage("multiple record delimiters");
 			if ('\n' == (REC_D = *optarg))
@@ -297,6 +300,6 @@ usage(msg)
 	if (msg != NULL)
 		(void)fprintf(stderr, "sort: %s\n", msg);
 	(void)fprintf(stderr, "usage: [-o output] [-cmubdfinrsS] [-t char] ");
-	(void)fprintf(stderr, "[-T char] [-k keydef] ... [files]\n");
+	(void)fprintf(stderr, "[-w char] [-k keydef] ... [files]\n");
 	exit(2);
 }
