bc10b7e208d4dd3989b6ca8ad63be0bcac3cbbf5
briggs <briggs>
2001-02-09 14:33:15 +0000

    Add an option (defopt) PCI_NETBSD_CONFIGURE that provides PCI bus configuration (assignment of bus numbers, BARs, timer values, interrupt lines, etc.). The interface must be called from m.d. code prior to probing the bus. It is meant to be called once for each primary (bus == 0) PCI bus in the system.  It will configure any busses behind PCI-PCI bridges. Section 9 man page for pci_configure_bus() will come soon. In the meantime, sample usage is in arch/sandpoint/sandpoint/mainbus.c. [ Reviewed by thorpej ]

---
+++ sys/dev/pci/pciconf.h
@@ -0,0 +1,49 @@
+/*	$NetBSD: pciconf.h,v 1.1 2001/02/09 14:33:15 briggs Exp $	*/
+
+/*
+ * Copyright 2001 Wasabi Systems, Inc.
+ * All rights reserved.
+ *
+ * Written by Allen Briggs for Wasabi Systems, Inc.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *      This product includes software developed for the NetBSD Project by
+ *      Wasabi Systems, Inc.
+ * 4. The name of Wasabi Systems, Inc. may not be used to endorse
+ *    or promote products derived from this software without specific prior
+ *    written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY WASABI SYSTEMS, INC. ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL WASABI SYSTEMS, INC
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/*
+ * args: pci_chipset_tag_t, io_extent, mem_extent, pmem_extent
+ * where pmem_extent is "pre-fetchable" memory -- if NULL, mem_extent will
+ * be used for both
+ */
+int	pci_configure_bus __P((pci_chipset_tag_t, struct extent *,
+	    struct extent *, struct extent *));
+
+/* Defined in machdep code.  Returns the interrupt line to set */
+/* args: chipset_tag, bus, dev, function, ptr to interrupt line */
+void	pci_conf_interrupt __P((pci_chipset_tag_t, int, int, int, int, int *));
+
