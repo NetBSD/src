9173eae73b436a06eb66eb285783e849e39c4a28
uch <uch@NetBSD.org>
2001-02-09 18:34:01 +0000

    bootloader for SH3, SA-1100, TX39, VR41 based Windows CE(2.00 or later)

---
+++ sys/arch/hpc/stand/include/machine/cdefs.h
@@ -0,0 +1,8 @@
+/*	$NetBSD: cdefs.h,v 1.1 2001/02/09 18:35:24 uch Exp $	*/
+
+/* Windows CE architecture */
+
+#define __BEGIN_MACRO	do {
+#define __END_MACRO	} while (/*CONSTCOND*/0)
+
+#include <sys/cdefs.h>
