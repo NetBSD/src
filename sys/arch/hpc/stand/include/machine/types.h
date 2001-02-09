5e10cd367b210074516c39991a198730e1c7f1b7
uch <uch@NetBSD.org>
2001-02-09 18:34:01 +0000

    bootloader for SH3, SA-1100, TX39, VR41 based Windows CE(2.00 or later)

---
+++ sys/arch/hpc/stand/include/machine/types.h
@@ -0,0 +1,38 @@
+/*	$NetBSD: types.h,v 1.1 2001/02/09 18:35:33 uch Exp $	*/
+
+/* Windows CE architecture */
+
+#ifndef	_MACHTYPES_H_
+#define	_MACHTYPES_H_
+
+#include <sys/cdefs.h>
+#include <machine/int_types.h>
+
+typedef unsigned char		u_int8_t;
+typedef unsigned short		u_int16_t;
+typedef unsigned int		u_int32_t;
+typedef unsigned __int64	u_int64_t;
+
+typedef signed char		int8_t;	
+typedef signed short		int16_t;
+typedef signed int		int32_t;
+typedef signed __int64		int64_t;
+
+typedef u_int32_t		off_t;
+#define off_t			u_int32_t
+#define _TIME_T_DEFINED
+typedef unsigned long		time_t;
+typedef unsigned int		size_t;
+
+/* Windows CE virtual address */
+typedef u_int32_t		vaddr_t;
+typedef u_int32_t		vsize_t;
+/* Physical address */
+typedef u_int32_t		paddr_t;
+typedef u_int32_t		psize_t;
+
+/* kernel virtual address */
+typedef u_int32_t		kaddr_t;
+typedef u_int32_t		ksize_t;
+
+#endif /* _MACHTYPES_H_ */
