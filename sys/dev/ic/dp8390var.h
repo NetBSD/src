3b581a7b414b26d0d2e752e357020c6df639d334
thorpej <thorpej>
2001-02-12 18:49:03 +0000

    Adjust the way that media is initialized on DP8390-compatible chips.  The dp8390_softc now has media_init and media_fini function pointers that do the work.

--- sys/dev/ic/dp8390var.h
+++ sys/dev/ic/dp8390var.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: dp8390var.h,v 1.21 2001/02/10 17:18:02 thorpej Exp $	*/
+/*	$NetBSD: dp8390var.h,v 1.22 2001/02/12 18:49:04 thorpej Exp $	*/
 
 /*
  * Device driver for National Semiconductor DS8390/WD83C690 based ethernet
@@ -87,6 +87,9 @@ struct dp8390_softc {
 	int	(*sc_enable) __P((struct dp8390_softc *));
 	void	(*sc_disable) __P((struct dp8390_softc *));
 
+	void	(*sc_media_init) __P((struct dp8390_softc *));
+	void	(*sc_media_fini) __P((struct dp8390_softc *));
+
 	int	(*sc_mediachange) __P((struct dp8390_softc *));
 	void	(*sc_mediastatus) __P((struct dp8390_softc *,
 		    struct ifmediareq *));
@@ -151,7 +154,7 @@ struct dp8390_softc {
 #define	NIC_BARRIER(t, h)	bus_space_barrier(t, h, 0, 0x10,	\
 			    BUS_SPACE_BARRIER_READ | BUS_SPACE_BARRIER_WRITE)
 
-int	dp8390_config __P((struct dp8390_softc *, int *, int, int));
+int	dp8390_config __P((struct dp8390_softc *));
 int	dp8390_intr __P((void *));
 int	dp8390_ioctl __P((struct ifnet *, u_long, caddr_t));
 void	dp8390_start __P((struct ifnet *));
@@ -172,6 +175,11 @@ void	dp8390_disable __P((struct dp8390_softc *));
 int	dp8390_activate __P((struct device *, enum devact));
 
 int	dp8390_detach __P((struct dp8390_softc *, int));
+
+int	dp8390_mediachange __P((struct ifnet *));
+void	dp8390_mediastatus __P((struct ifnet *, struct ifmediareq *));
+
+void	dp8390_media_init __P((struct dp8390_softc *));
 
 #ifdef IPKDB_DP8390
 int	dp8390_ipkdb_attach __P((struct ipkdb_if *));
