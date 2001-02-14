a11f5d50915989ff6bebc82d6a217e24be8e51ab
itojun <itojun@NetBSD.org>
2001-02-14 01:22:02 +0000

    update import date.

--- crypto/dist/ssh/version.h
+++ crypto/dist/ssh/version.h
@@ -1,7 +1,7 @@
 /* $OpenBSD: version.h,v 1.17 2001/02/08 18:15:22 markus Exp $ */
 
 #define __OPENSSH_VERSION	"OpenSSH_2.3.2"
-#define __NETBSDSSH_VERSION	"NetBSD_Secure_Shell-20010209"
+#define __NETBSDSSH_VERSION	"NetBSD_Secure_Shell-20010214"
 
 /*
  * it is important to retain OpenSSH version identification part, it is
