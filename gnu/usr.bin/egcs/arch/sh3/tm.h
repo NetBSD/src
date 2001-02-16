24f60db5d315943469a84d761948b7dfba591b9f
msaitoh <msaitoh@NetBSD.org>
2001-02-16 21:11:12 +0000

    for little endian

--- gnu/usr.bin/egcs/arch/sh3/tm.h
+++ gnu/usr.bin/egcs/arch/sh3/tm.h
@@ -1,7 +1,15 @@
-/*	$NetBSD: tm.h,v 1.2 1999/11/26 07:36:48 msaitoh Exp $	*/
+/*	$NetBSD: tm.h,v 1.3 2001/02/16 21:11:13 msaitoh Exp $	*/
 
 #ifdef DEFAULT_ELF
+#ifdef TARGET_LITTLE_ENDIAN_DEFAULT
+#include "sh/netbsdelelf.h"
+#else
 #include "sh/netbsdelf.h"
+#endif
+#else
+#ifdef TARGET_LITTLE_ENDIAN_DEFAULT
+#include "sh/netbsdelcoff.h"
 #else
 #include "sh/netbsdcoff.h"
+#endif
 #endif
