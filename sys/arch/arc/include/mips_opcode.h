c12a409b5715059cf89953a0534f98a9836c00d0
tsutsui <tsutsui@NetBSD.org>
2001-02-17 04:31:25 +0000

    Fix include path. (mips/include/mips_opcode.h -> mips/mips_opcode.h)

--- sys/arch/arc/include/mips_opcode.h
+++ sys/arch/arc/include/mips_opcode.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: mips_opcode.h,v 1.4 2000/01/23 21:01:57 soda Exp $	*/
+/*	$NetBSD: mips_opcode.h,v 1.5 2001/02/17 04:31:25 tsutsui Exp $	*/
 /*      $OpenBSD: mips_opcode.h,v 1.1.1.1 1996/06/24 09:07:17 pefo Exp $	*/
 
-#include <mips/include/mips_opcode.h>
+#include <mips/mips_opcode.h>
