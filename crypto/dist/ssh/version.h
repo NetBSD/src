1317273faeb966c99c45bb40f8d79a4385fdea6e
itojun <itojun@NetBSD.org>
2001-02-19 12:13:04 +0000

    sync up with 2.5.1. this fixes backward compatibility breakage against 1.2.18 - 1.2.22.

--- crypto/dist/ssh/version.h
+++ crypto/dist/ssh/version.h
@@ -1,7 +1,7 @@
-/* $OpenBSD: version.h,v 1.18 2001/02/16 14:26:57 markus Exp $ */
+/* $OpenBSD: version.h,v 1.19 2001/02/19 10:35:23 markus Exp $ */
 
-#define __OPENSSH_VERSION	"OpenSSH_2.5.0"
-#define __NETBSDSSH_VERSION	"NetBSD_Secure_Shell-20010217"
+#define __OPENSSH_VERSION	"OpenSSH_2.5.1"
+#define __NETBSDSSH_VERSION	"NetBSD_Secure_Shell-20010219"
 
 /*
  * it is important to retain OpenSSH version identification part, it is
