df98929e3b7dea517e6306ca34dbef7e426da841
uch <uch@NetBSD.org>
2001-02-21 16:01:52 +0000

    more diagnostic message. (SH3) add framebuffer information (ARM) fix boot parameter bug. (-b=sd0)

---
+++ sys/arch/hpc/stand/hpcboot/sh3/hd64461.h
@@ -0,0 +1,6 @@
+/*	$NetBSD: hd64461.h,v 1.1 2001/02/21 16:01:54 uch Exp $	*/
+
+#include "../../../../hpcsh/dev/hd64461/hd64461reg.h"
+#include "../../../../hpcsh/dev/hd64461/hd64461intcreg.h"
+#include "../../../../hpcsh/dev/hd64461/hd64461pcmciareg.h"
+#include "../../../../hpcsh/dev/hd64461/hd64461gpioreg.h"
