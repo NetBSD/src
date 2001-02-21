862ed6c4aa2dc91bf9396140410a83d9546c85d4
minoura <minoura@NetBSD.org>
2001-02-21 12:39:16 +0000

    Use m68k_page_offset and m68k_trunc_page macros, instead of using PGOFSET directly.

--- sys/arch/x68k/x68k/mem.c
+++ sys/arch/x68k/x68k/mem.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: mem.c,v 1.23 2001/02/21 05:53:35 minoura Exp $	*/
+/*	$NetBSD: mem.c,v 1.24 2001/02/21 12:39:17 minoura Exp $	*/
 
 /*
  * Copyright (c) 1988 University of Utah.
@@ -137,7 +137,7 @@ mmrw(dev, uio, flags)
 			    VM_PROT_WRITE;
 			pmap_enter(pmap_kernel(), (vaddr_t)vmmap,
 			    trunc_page(v), prot, prot|PMAP_WIRED);
-			o = uio->uio_offset & PGOFSET;
+			o = m68k_page_offset(uio->uio_offset);
 			c = min(uio->uio_resid, (int)(NBPG - o));
 			error = uiomove((caddr_t)vmmap + o, c, uio);
 			pmap_remove(pmap_kernel(), (vaddr_t)vmmap,
