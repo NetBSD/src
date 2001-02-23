1e49deb5f9eb3b72f43661813d0c8290f3cff185
bjh21 <bjh21>
2001-02-23 22:02:43 +0000

    __HAVE_NWSCONS and __HAVE_DEVICE_REGISTER are MACHINE-dependent.

--- sys/arch/hpcarm/include/types.h
+++ sys/arch/hpcarm/include/types.h
@@ -1,3 +1,11 @@
-/* $NetBSD: types.h,v 1.2 2001/02/23 21:23:53 reinoud Exp $ */
+/* $NetBSD: types.h,v 1.3 2001/02/23 22:02:44 bjh21 Exp $ */
+
+#ifndef _HPCARM_TYPES_H_
+#define _HPCARM_TYPES_H_
 
 #include <arm/types.h>
+
+#define __HAVE_DEVICE_REGISTER
+#define __HAVE_NWSCONS
+
+#endif
