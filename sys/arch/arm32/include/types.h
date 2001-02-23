a137cd0c212a9301b9847ef8f53ba3bc24fc546f
bjh21 <bjh21@NetBSD.org>
2001-02-23 22:02:43 +0000

    __HAVE_NWSCONS and __HAVE_DEVICE_REGISTER are MACHINE-dependent.

--- sys/arch/arm32/include/types.h
+++ sys/arch/arm32/include/types.h
@@ -1,3 +1,11 @@
-/* $NetBSD: types.h,v 1.14 2001/02/23 21:23:51 reinoud Exp $ */
+/* $NetBSD: types.h,v 1.15 2001/02/23 22:02:44 bjh21 Exp $ */
+
+#ifndef _ARM32_TYPES_H_
+#define _ARM32_TYPES_H_
 
 #include <arm/types.h>
+
+#define __HAVE_DEVICE_REGISTER
+#define __HAVE_NWSCONS
+
+#endif
