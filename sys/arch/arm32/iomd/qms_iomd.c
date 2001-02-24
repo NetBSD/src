7535275b7c39d76bb87c3e87b7972eb05c75e5ca
reinoud <reinoud@NetBSD.org>
2001-02-24 21:29:38 +0000

    Fixed the `slow printing' bug that occured with debugged kernels on RiscPC's. When the IOMD isnt mapped and all is mapped 1:1 a read gives garbage and thus the obscure feature that slows down printing (or halts it!) when the mouse button is pressed during output to the vidcconsole gets allways activated.
    Included a simple flag that signals if the IOMD is mapped; the delay routine
    won't check the mouse if this flag is still clear.

--- sys/arch/arm32/iomd/qms_iomd.c
+++ sys/arch/arm32/iomd/qms_iomd.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: qms_iomd.c,v 1.1 1997/10/14 11:16:39 mark Exp $	*/
+/*	$NetBSD: qms_iomd.c,v 1.2 2001/02/24 21:29:38 reinoud Exp $	*/
 
 /*
  * Copyright (c) Scott Stevens 1995 All rights reserved
@@ -48,6 +48,14 @@
 #include <machine/irqhandler.h>
 #include <arm32/dev/qmsvar.h>
 #include <arm32/iomd/iomdvar.h>
+
+/*
+ * needs iomd_mapped flag to detect if the IOMD is allready connected
+ * otherwise the vidcconsole qms_console_freeze will go wrong for it
+ * can't read the IOMD when it isnt initialised (!)
+ */
+extern int iomd_mapped;
+
 
 #define TIMER1_COUNT 40000		/* 50Hz */
 
@@ -121,6 +129,8 @@ qms_iomd_intenable(sc, enable)
 void
 qms_console_freeze()
 {
+	if (!iomd_mapped) return;
+
 	/* Middle mouse button freezes the display while active */
 	while ((ReadByte(IO_MOUSE_BUTTONS) & MOUSE_BUTTON_MIDDLE) == 0);
 
