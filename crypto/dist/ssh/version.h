5257939651558f554f24cfb3e34334238ab60789
he <he@NetBSD.org>
2001-02-26 20:27:09 +0000

    Pull up revisions 1.4-1.10 (requested by itojun):   Update SSH to version found on trunk as of 26 Feb 2001.

--- crypto/dist/ssh/version.h
+++ crypto/dist/ssh/version.h
@@ -1,10 +1,10 @@
-/*	$NetBSD: version.h,v 1.1.1.1.2.3 2000/10/30 22:53:12 tv Exp $	*/
+/* $OpenBSD: version.h,v 1.19 2001/02/19 10:35:23 markus Exp $ */
 
-#define __OPENSSH_VERSION	"OpenSSH_2.2.0"
-#define __NETBSDSSH_VERSION	"NetBSD_Secure_Shell-20001003"
+#define __OPENSSH_VERSION	"OpenSSH_2.5.1"
+#define __NETBSDSSH_VERSION	"NetBSD_Secure_Shell-20010219"
 
 /*
  * it is important to retain OpenSSH version identification part, it is
- * used for bug compatibility operation.  present NetBSD SSH version as comment.
+ * used for bug compatibility operation.  present NetBSD SSH version as comment
  */
 #define SSH_VERSION	(__OPENSSH_VERSION " " __NETBSDSSH_VERSION)
