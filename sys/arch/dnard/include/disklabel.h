61fa5a1deb57c387c14fd4c7f63edc6cf6c18cd6
matt <matt@NetBSD.org>
2001-02-26 03:09:23 +0000

    More fill-in

---
+++ sys/arch/dnard/include/disklabel.h
@@ -0,0 +1,3 @@
+/*	$NetBSD: disklabel.h,v 1.1 2001/02/26 03:09:23 matt Exp $	*/
+
+#include <arm/disklabel.h>
---
+++ sys/arch/dnard/include/profile.h
@@ -0,0 +1,3 @@
+/* $NetBSD: profile.h,v 1.1 2001/02/26 03:09:23 matt Exp $ */
+
+#include <arm/profile.h>
