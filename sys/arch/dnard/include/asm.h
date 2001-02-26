fcc9dd8619cc7a9bf4e4665c2a66c8f83e954ece
matt <matt@NetBSD.org>
2001-02-26 02:57:48 +0000

    Add some of the common files.

---
+++ sys/arch/dnard/include/asm.h
@@ -0,0 +1,3 @@
+/*	$NetBSD: asm.h,v 1.1 2001/02/26 02:57:48 matt Exp $	*/
+
+#include <arm/asm.h>
---
+++ sys/arch/dnard/include/proc.h
@@ -0,0 +1,3 @@
+/* $NetBSD: proc.h,v 1.1 2001/02/26 02:57:49 matt Exp $ */
+
+#include <arm/proc.h>
