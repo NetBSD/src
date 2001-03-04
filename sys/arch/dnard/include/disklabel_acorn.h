0f6140dbeb3c8565668919c6fc69502625521d9d
matt <matt@NetBSD.org>
2001-03-04 05:52:58 +0000

    More includes for <arm/*.h>

---
+++ sys/arch/dnard/include/disklabel_acorn.h
@@ -0,0 +1,3 @@
+/*	$NetBSD: disklabel_acorn.h,v 1.1 2001/03/04 05:52:58 matt Exp $	*/
+
+#include <arm/disklabel_acorn.h>
---
+++ sys/arch/dnard/include/endian_machdep.h
@@ -0,0 +1,3 @@
+/*	$NetBSD: endian_machdep.h,v 1.1 2001/03/04 05:52:58 matt Exp $	*/
+
+#include <arm/endian_machdep.h>
