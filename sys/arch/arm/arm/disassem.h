dc619dda8fe21f4e607651d3940e55b28c8cf530
matt <matt@NetBSD.org>
2001-03-04 04:15:58 +0000

    s/vm_offset_t/vaddr_t/g

--- sys/arch/arm/arm/disassem.h
+++ sys/arch/arm/arm/disassem.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: disassem.h,v 1.3 2001/01/13 16:52:01 bjh21 Exp $	*/
+/*	$NetBSD: disassem.h,v 1.4 2001/03/04 04:15:58 matt Exp $	*/
 
 /*
  * Copyright (c) 1997 Mark Brinicombe.
@@ -44,5 +44,5 @@ typedef struct {
 
 /* Prototypes for callable functions */
 
-vm_offset_t disasm(const disasm_interface_t *, vm_offset_t, int);
+vaddr_t disasm(const disasm_interface_t *, vaddr_t, int);
 void disassemble(u_int);
