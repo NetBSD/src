332596607eea92a75a12ed5720907aa4a716106e
thorpej <thorpej@NetBSD.org>
2001-03-07 22:42:16 +0000

    Add the BUS_DMA_STREAMING flag.

--- sys/arch/macppc/include/bus.h
+++ sys/arch/macppc/include/bus.h
@@ -1,7 +1,7 @@
-/*	$NetBSD: bus.h,v 1.11 2000/12/21 23:17:32 tsubai Exp $	*/
+/*	$NetBSD: bus.h,v 1.12 2001/03/07 22:42:18 thorpej Exp $	*/
 
 /*-
- * Copyright (c) 1996, 1997, 1998 The NetBSD Foundation, Inc.
+ * Copyright (c) 1996, 1997, 1998, 2001 The NetBSD Foundation, Inc.
  * All rights reserved.
  *
  * This code is derived from software contributed to The NetBSD Foundation
@@ -713,6 +713,7 @@ bus_space_set_region_stream_4(tag, bsh, offset, val, count)
 #define BUS_DMA_NOWAIT		0x01	/* not safe to sleep */
 #define BUS_DMA_ALLOCNOW	0x02	/* perform resource allocation now */
 #define BUS_DMA_COHERENT	0x04	/* hint: map memory DMA coherent */
+#define	BUS_DMA_STREAMING	0x08	/* hint: sequential, unidirectional */
 #define BUS_DMA_BUS1		0x10	/* placeholders for bus functions... */
 #define BUS_DMA_BUS2		0x20
 #define BUS_DMA_BUS3		0x40
