4e46f76577751b79403b0ee1a91bddd482331272
tsutsui <tsutsui@NetBSD.org>
2001-03-09 13:35:50 +0000

    Declare constty extern.

--- sys/kern/subr_prf.c
+++ sys/kern/subr_prf.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: subr_prf.c,v 1.76 2000/08/09 10:22:31 tv Exp $	*/
+/*	$NetBSD: subr_prf.c,v 1.77 2001/03/09 13:35:50 tsutsui Exp $	*/
 
 /*-
  * Copyright (c) 1986, 1988, 1991, 1993
@@ -142,7 +142,7 @@ static void	 klogpri __P((int));
  * globals
  */
 
-struct	tty *constty;	/* pointer to console "window" tty */
+extern	struct tty *constty;	/* pointer to console "window" tty */
 extern	int log_open;	/* subr_log: is /dev/klog open? */
 const	char *panicstr; /* arg to first call to panic (used as a flag
 			   to indicate that panic has already been called). */
