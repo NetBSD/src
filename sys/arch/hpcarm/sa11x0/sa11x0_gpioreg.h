2cb44b8020017e2b6cf87e0a38da6f164e1ac235
ichiro <ichiro@NetBSD.org>
2001-03-11 06:00:42 +0000

    add h3600 and jornada720 gpio port and irq

--- sys/arch/hpcarm/sa11x0/sa11x0_gpioreg.h
+++ sys/arch/hpcarm/sa11x0/sa11x0_gpioreg.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: sa11x0_gpioreg.h,v 1.3 2001/03/10 13:34:35 toshii Exp $	*/
+/*	$NetBSD: sa11x0_gpioreg.h,v 1.4 2001/03/11 06:00:42 ichiro Exp $	*/
 
 /*-
  * Copyright (c) 2001 The NetBSD Foundation, Inc.  All rights reserved.
@@ -65,6 +65,9 @@
 /* GPIO alternate function register */
 #define SAGPIO_AFR	0x1C
 
+/* XXX */
+#define GPIO(x)		(0x00000001 << (x))
+
 /*
  * iPAQ H3600 specific parameter
  */
@@ -93,8 +96,64 @@ port	I/O(Active)	desc
 27	O(L)	Indicates presence of expansion pack inserted
  */
 
+#define GPIO_H3600_POWER_BUTTON	GPIO (0)
+#define GPIO_H3600_PCMCIA_CD0	GPIO (17)
+#define GPIO_H3600_PCMCIA_CD1	GPIO (10)
+#define GPIO_H3600_PCMCIA_IRQ0	GPIO (21)
+#define GPIO_H3600_PCMCIA_IRQ1	GPIO (11)
+#define GPIO_H3600_OPT_LOCK	GPIO (22)
+#define GPIO_H3600_OPT_IRQ	GPIO (24)
+#define GPIO_H3600_OPT_DETECT	GPIO (27)
+
+#define IRQ_H3600_POWER_BUTTON	IRQ_GPIO0
+#define IRQ_H3600_PCMCIA_CD0	IRQ_GPIO17
+#define IRQ_H3600_PCMCIA_CD1	IRQ_GPIO10
+#define IRQ_H3600_PCMCIA_IRQ0	IRQ_GPIO21
+#define IRQ_H3600_PCMCIA_IRQ1	IRQ_GPIO11
+#define IRQ_H3600_OPT_IRQ	IRQ_GPIO24
+#define IRQ_H3600_OPT_DETECT	IRQ_GPIO27
 
 /*
  * JORNADA720 specific parameter
  */
+
+#define JORNADA720_KBD_IRQ	GPIO (0)
+#define JORNADA720_MOUSE_IRQ	GPIO (9)
+
+/* 
+ * IRQ Number of GPIO(x)
+ * GPIO(0..10)  -> IRQ(0..10)
+ * GPIO(11..27) -> IRQ(32..48)
+ */
+#define IRQ_GPIO1(x)		(0 + x)
+#define IRQ_GPIO2(x)		(32 * (x) - 11)
+
+#define IRQ_GPIO0	IRQ_GPIO1(0)
+#define IRQ_GPIO1	IRQ_GPIO1(1)
+#define IRQ_GPIO2	IRQ_GPIO1(2)
+#define IRQ_GPIO3	IRQ_GPIO1(3)
+#define IRQ_GPIO4	IRQ_GPIO1(4)
+#define IRQ_GPIO5	IRQ_GPIO1(5)
+#define IRQ_GPIO6	IRQ_GPIO1(6)
+#define IRQ_GPIO7	IRQ_GPIO1(7)
+#define IRQ_GPIO8	IRQ_GPIO1(8)
+#define IRQ_GPIO9	IRQ_GPIO1(9)
+#define IRQ_GPIO10	IRQ_GPIO1(10)
+#define IRQ_GPIO11	IRQ_GPIO2(11)
+#define IRQ_GPIO12	IRQ_GPIO2(12)
+#define IRQ_GPIO13	IRQ_GPIO2(13)
+#define IRQ_GPIO14	IRQ_GPIO2(14)
+#define IRQ_GPIO15	IRQ_GPIO2(15)
+#define IRQ_GPIO16	IRQ_GPIO2(16)
+#define IRQ_GPIO17	IRQ_GPIO2(17)
+#define IRQ_GPIO18	IRQ_GPIO2(18)
+#define IRQ_GPIO19	IRQ_GPIO2(19)
+#define IRQ_GPIO20	IRQ_GPIO2(20)
+#define IRQ_GPIO21	IRQ_GPIO2(21)
+#define IRQ_GPIO22	IRQ_GPIO2(22)
+#define IRQ_GPIO23	IRQ_GPIO2(23)
+#define IRQ_GPIO24	IRQ_GPIO2(24)
+#define IRQ_GPIO25	IRQ_GPIO2(25)
+#define IRQ_GPIO26	IRQ_GPIO2(26)
+#define IRQ_GPIO27	IRQ_GPIO2(27)
 
