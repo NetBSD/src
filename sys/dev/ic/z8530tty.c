688a84b1b25a922e98452c309809bf9fdd62a7fe
wdk <wdk@NetBSD.org>
2001-03-11 09:06:09 +0000

    Add missing \n during probe if port is dedicated to kgdb

--- sys/dev/ic/z8530tty.c
+++ sys/dev/ic/z8530tty.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: z8530tty.c,v 1.72 2000/11/08 23:13:03 eeh Exp $	*/
+/*	$NetBSD: z8530tty.c,v 1.73 2001/03/11 09:06:09 wdk Exp $	*/
 
 /*-
  * Copyright (c) 1993, 1994, 1995, 1996, 1997, 1998, 1999
@@ -328,7 +328,7 @@ zstty_attach(parent, self, aux)
 		 * Allow kgdb to "take over" this port.  Returns true
 		 * if this serial port is in-use by kgdb.
 		 */
-		printf(" (kgdb)");
+		printf(" (kgdb)\n");
 		/*
 		 * This is the kgdb port (exclusive use)
 		 * so skip the normal attach code.
