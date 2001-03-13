31df339cd35be15f1d89577470c39d9ad3e922f8
ross <ross@NetBSD.org>
2001-03-13 07:49:33 +0000

    Boilerplate, WARNS.

--- regress/lib/libc/ieeefp/testfloat/include/testLoops.h
+++ regress/lib/libc/ieeefp/testfloat/include/testLoops.h
@@ -1,3 +1,42 @@
+/* $NetBSD: testLoops.h,v 1.2 2001/03/13 07:49:34 ross Exp $ */
+
+/* This is a derivative work. */
+
+/*-
+ * Copyright (c) 2001 The NetBSD Foundation, Inc.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to The NetBSD Foundation
+ * by Ross Harvey.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *        This product includes software developed by the NetBSD
+ *        Foundation, Inc. and its contributors.
+ * 4. Neither the name of The NetBSD Foundation nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 
 /*
 ===============================================================================
@@ -27,14 +66,14 @@ this code that are retained.
 
 extern volatile flag stop;
 
-extern char *trueName, *testName;
+extern const char *trueName, *testName;
 extern flag forever, errorStop;
 extern uint32 maxErrorCount;
 extern flag checkNaNs;
 extern int8 *trueFlagsPtr;
 extern int8 ( *testFlagsFunctionPtr )( void );
-extern char *functionName;
-extern char *roundingPrecisionName, *roundingModeName, *tininessModeName;
+extern const char *functionName;
+extern const char *roundingPrecisionName, *roundingModeName, *tininessModeName;
 extern flag anyErrors;
 
 void writeFunctionName( FILE * );
