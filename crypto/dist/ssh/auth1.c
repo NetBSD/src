8e2e268b4a9ff7a8ce7a9f2081f3c03bdfad4ca8
itojun <itojun@NetBSD.org>
2001-03-19 19:42:00 +0000

    OpenSSH 2.5.2 as of 3/19/2001, from openbsd usr.bin/ssh

--- crypto/dist/ssh/auth1.c
+++ crypto/dist/ssh/auth1.c
@@ -10,7 +10,7 @@
  */
 
 #include "includes.h"
-RCSID("$OpenBSD: auth1.c,v 1.17 2001/02/13 22:49:40 markus Exp $");
+RCSID("$OpenBSD: auth1.c,v 1.19 2001/03/08 18:47:12 stevesk Exp $");
 
 #include "xmalloc.h"
 #include "rsa.h"
@@ -23,10 +23,10 @@ RCSID("$OpenBSD: auth1.c,v 1.17 2001/02/13 22:49:40 markus Exp $");
 #include "compat.h"
 #include "auth.h"
 #include "session.h"
+#include "misc.h"
 
 /* import */
 extern ServerOptions options;
-extern char *forced_command;
 
 /*
  * convert ssh auth msg type into description
