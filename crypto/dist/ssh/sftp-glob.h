9fa6ee4ac6b2a206c7a1afd4946db0d5990fe416
itojun <itojun@NetBSD.org>
2001-03-19 19:42:00 +0000

    OpenSSH 2.5.2 as of 3/19/2001, from openbsd usr.bin/ssh

--- crypto/dist/ssh/sftp-glob.h
+++ crypto/dist/ssh/sftp-glob.h
@@ -1,7 +1,7 @@
-/*	$OpenBSD: key.h,v 1.11 2001/03/12 22:02:01 markus Exp $	*/
+/* $OpenBSD: sftp-glob.h,v 1.1 2001/03/13 22:42:54 djm Exp $ */
 
 /*
- * Copyright (c) 2000 Markus Friedl.  All rights reserved.
+ * Copyright (c) 2001 Damien Miller.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -23,61 +23,10 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-#ifndef KEY_H
-#define KEY_H
 
-#include <openssl/rsa.h>
-#include <openssl/dsa.h>
-
-typedef struct Key Key;
-enum types {
-	KEY_RSA1,
-	KEY_RSA,
-	KEY_DSA,
-	KEY_UNSPEC
-};
-enum fp_type {
-	SSH_FP_SHA1,
-	SSH_FP_MD5
-};
-enum fp_rep {
-	SSH_FP_HEX,
-	SSH_FP_BUBBLEBABBLE
-};
-struct Key {
-	int	type;
-	RSA	*rsa;
-	DSA	*dsa;
-};
-
-Key	*key_new(int type);
-Key	*key_new_private(int type);
-void	key_free(Key *k);
-int	key_equal(Key *a, Key *b);
-char	*key_fingerprint(Key *k, enum fp_type dgst_type, enum fp_rep dgst_rep);
-char	*key_type(Key *k);
-int	key_write(Key *key, FILE *f);
-int	key_read(Key *key, char **cpp);
-u_int	key_size(Key *k);
-
-Key	*key_generate(int type, u_int bits);
-Key	*key_from_private(Key *k);
-int	key_type_from_name(char *name);
-
-Key	*key_from_blob(char *blob, int blen);
-int	key_to_blob(Key *key, u_char **blobp, u_int *lenp);
-char	*key_ssh_name(Key *k);
+/* Remote sftp filename globbing */
 
 int
-key_sign(
-    Key *key,
-    u_char **sigp, int *lenp,
-    u_char *data, int datalen);
+remote_glob(int fd_in, int fd_out, const char *pattern, int flags, 
+    const int (*errfunc)(const char *, int), glob_t *pglob);
 
-int
-key_verify(
-    Key *key,
-    u_char *signature, int signaturelen,
-    u_char *data, int datalen);
-
-#endif
