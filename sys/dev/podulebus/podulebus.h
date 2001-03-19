022fdfe1ad86f2bb36e6a9d1ed581f5cc394783b
bjh21 <bjh21@NetBSD.org>
2001-03-19 23:59:33 +0000

    Quick hack to pull in podulebus.h for the current architecture. This should be done better (probably with <machine/podulebus_machdep.h> later.

---
+++ sys/dev/podulebus/podulebus.h
@@ -0,0 +1,7 @@
+/* $NetBSD: podulebus.h,v 1.1 2001/03/19 23:59:33 bjh21 Exp $ */
+
+#ifdef arm26
+#include <arch/arm26/podulebus/podulebus.h>
+#else
+#include <arch/arm32/podulebus/podulebus.h>
+#endif
