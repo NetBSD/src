c29373d60dd95a1e7c4acb15c49789faa50b41ce
toshii <toshii@NetBSD.org>
2001-03-20 18:01:49 +0000

    hpcarm kernel is ELF.

--- sys/arch/hpcarm/include/db_machdep.h
+++ sys/arch/hpcarm/include/db_machdep.h
@@ -1,4 +1,7 @@
-/* $NetBSD: db_machdep.h,v 1.2 2001/02/23 21:23:52 reinoud Exp $ */
+/* $NetBSD: db_machdep.h,v 1.3 2001/03/20 18:01:49 toshii Exp $ */
 
 #include <arm/db_machdep.h>
 
+/* hpcarm uses ELF for kernel */
+#define DB_ELF_SYMBOLS
+#define DB_ELFSIZE      32
