43671fa809ab4cacc8114b761c2df104521540d9
bjh21 <bjh21>
2001-03-20 22:59:39 +0000

    Move machine-dependent podulebus headers to <machine/podulebus_machdep.h>. <dev/podulebus/podulebus.h> is the canonical file to include, and includes <machine/podulebus_machdep.h>. <arch/${MACHINE}/poudulebus/podulebus.h> remains for backward compatibility.

--- sys/dev/podulebus/podulebus.h
+++ sys/dev/podulebus/podulebus.h
@@ -1,7 +1,3 @@
-/* $NetBSD: podulebus.h,v 1.1 2001/03/19 23:59:33 bjh21 Exp $ */
+/* $NetBSD: podulebus.h,v 1.2 2001/03/20 22:59:39 bjh21 Exp $ */
 
-#ifdef arm26
-#include <arch/arm26/podulebus/podulebus.h>
-#else
-#include <arch/arm32/podulebus/podulebus.h>
-#endif
+#include <machine/podulebus_machdep.h>
