9700ece58c3478f4e5acf904e0de28f24eb2c825
soren <soren@NetBSD.org>
2001-03-21 18:40:28 +0000

    s/vm_page_alloc_memory/uvm_pglistalloc/ in panic message.

--- sys/arch/macppc/macppc/bus_dma.c
+++ sys/arch/macppc/macppc/bus_dma.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: bus_dma.c,v 1.16 2000/06/29 08:10:45 mrg Exp $	*/
+/*	$NetBSD: bus_dma.c,v 1.17 2001/03/21 18:40:31 soren Exp $	*/
 
 /*-
  * Copyright (c) 1997, 1998 The NetBSD Foundation, Inc.
@@ -455,7 +455,7 @@ _bus_dmamem_alloc(t, size, alignment, boundary, segs, nsegs, rsegs, flags)
 		curaddr = VM_PAGE_TO_PHYS(m);
 #ifdef DIAGNOSTIC
 		if (curaddr < avail_start || curaddr >= high) {
-			printf("vm_page_alloc_memory returned non-sensical"
+			printf("uvm_pglistalloc returned non-sensical"
 			    " address 0x%lx\n", curaddr);
 			panic("_bus_dmamem_alloc");
 		}
