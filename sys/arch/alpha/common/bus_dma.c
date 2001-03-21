1b74c17a6340a3291e03538a6dbdbe4c7a29ed05
soren <soren>
2001-03-21 18:40:28 +0000

    s/vm_page_alloc_memory/uvm_pglistalloc/ in panic message.

--- sys/arch/alpha/common/bus_dma.c
+++ sys/arch/alpha/common/bus_dma.c
@@ -1,4 +1,4 @@
-/* $NetBSD: bus_dma.c,v 1.43 2001/01/03 21:39:20 thorpej Exp $ */
+/* $NetBSD: bus_dma.c,v 1.44 2001/03/21 18:40:28 soren Exp $ */
 
 /*-
  * Copyright (c) 1997, 1998 The NetBSD Foundation, Inc.
@@ -39,7 +39,7 @@
 
 #include <sys/cdefs.h>			/* RCS ID & Copyright macro defns */
 
-__KERNEL_RCSID(0, "$NetBSD: bus_dma.c,v 1.43 2001/01/03 21:39:20 thorpej Exp $");
+__KERNEL_RCSID(0, "$NetBSD: bus_dma.c,v 1.44 2001/03/21 18:40:28 soren Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -464,7 +464,7 @@ _bus_dmamem_alloc_range(bus_dma_tag_t t, bus_size_t size, bus_size_t alignment,
 		curaddr = VM_PAGE_TO_PHYS(m);
 #ifdef DIAGNOSTIC
 		if (curaddr < avail_start || curaddr >= high) {
-			printf("vm_page_alloc_memory returned non-sensical"
+			printf("uvm_pglistalloc returned non-sensical"
 			    " address 0x%lx\n", curaddr);
 			panic("_bus_dmamem_alloc");
 		}
--- sys/arch/hpcmips/hpcmips/bus_dma.c
+++ sys/arch/hpcmips/hpcmips/bus_dma.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: bus_dma.c,v 1.5 2000/10/22 09:38:23 uch Exp $	*/
+/*	$NetBSD: bus_dma.c,v 1.6 2001/03/21 18:40:30 soren Exp $	*/
 
 /*-
  * Copyright (c) 1997, 1998 The NetBSD Foundation, Inc.
@@ -574,7 +574,7 @@ _bus_dmamem_alloc(t, size, alignment, boundary, segs, nsegs, rsegs, flags)
 		curaddr = VM_PAGE_TO_PHYS(m);
 #ifdef DIAGNOSTIC
 		if (curaddr < avail_start || curaddr >= high) {
-			printf("vm_page_alloc_memory returned non-sensical"
+			printf("uvm_pglistalloc returned non-sensical"
 			    " address 0x%lx\n", curaddr);
 			panic("_bus_dmamem_alloc");
 		}
