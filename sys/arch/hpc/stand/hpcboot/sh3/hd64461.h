df9bd6aaef1477c6ee2d1f631a053b58b1142ee4
uch <uch@NetBSD.org>
2001-03-22 18:27:51 +0000

    add HD64461 UART serial console. add SH3 SCI console method add SH3 TMU register dump use generic serial console when companion chip is unknown.

--- sys/arch/hpc/stand/hpcboot/sh3/hd64461.h
+++ sys/arch/hpc/stand/hpcboot/sh3/hd64461.h
@@ -1,6 +1,31 @@
-/*	$NetBSD: hd64461.h,v 1.1 2001/02/21 16:01:54 uch Exp $	*/
+/*	$NetBSD: hd64461.h,v 1.2 2001/03/22 18:27:51 uch Exp $	*/
 
 #include "../../../../hpcsh/dev/hd64461/hd64461reg.h"
 #include "../../../../hpcsh/dev/hd64461/hd64461intcreg.h"
 #include "../../../../hpcsh/dev/hd64461/hd64461pcmciareg.h"
 #include "../../../../hpcsh/dev/hd64461/hd64461gpioreg.h"
+#include "../../../../hpcsh/dev/hd64461/hd64461uartreg.h"
+
+#define	LSR_TXRDY	0x20	/* Transmitter buffer empty */
+
+#define HD64461COM_TX_BUSY()						\
+	while ((VOLATILE_REF8(HD64461_ULSR_REG8) & LSR_TXRDY) == 0)
+
+#define HD64461COM_PUTC(c)						\
+__BEGIN_MACRO								\
+	HD64461COM_TX_BUSY();						\
+	VOLATILE_REF8(HD64461_UTBR_REG8) = (c);				\
+	HD64461COM_TX_BUSY();						\
+__END_MACRO
+
+#define HD64461COM_PRINT(s)						\
+__BEGIN_MACRO								\
+	char *__s =(char *)(s);						\
+	int __i;							\
+	for (__i = 0; __s[__i] != '\0'; __i++) {			\
+		char __c = __s[__i];					\
+		if (__c == '\n')					\
+			HD64461COM_PUTC('\r');				\
+		HD64461COM_PUTC(__c);					\
+	}								\
+__END_MACRO
