e3c3021f3de0baeea5525a4a1e1c5be37b3dec13
mrg <mrg@NetBSD.org>
2001-03-22 06:21:43 +0000

    sync with sparc64.

--- sys/arch/sparc/include/frame.h
+++ sys/arch/sparc/include/frame.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: frame.h,v 1.2 1994/11/20 20:53:07 deraadt Exp $ */
+/*	$NetBSD: frame.h,v 1.3 2001/03/22 06:23:24 mrg Exp $ */
 
 /*
  * Copyright (c) 1992, 1993
@@ -52,16 +52,83 @@
  * of the frame, you must first force the kernel to write any such
  * windows to the stack.
  */
+#ifndef _LOCORE
+#ifndef SUN4U
 struct frame {
-	int	fr_local[8];	/* space to save locals (%l0..%l7) */
-	int	fr_arg[6];	/* space to save arguments (%i0..%i5) */
+	int32_t	fr_local[8];	/* space to save locals (%l0..%l7) */
+	int32_t	fr_arg[6];	/* space to save arguments (%i0..%i5) */
 	struct	frame *fr_fp;	/* space to save frame pointer (%i6) */
-	int	fr_pc;		/* space to save return pc (%i7) */
+	int32_t	fr_pc;		/* space to save return pc (%i7) */
 	/*
 	 * SunOS reserves another 8 words here; this is pointless
 	 * but we do it for compatibility.
 	 */
-	int	fr_xxx;		/* `structure return pointer' (unused) */
-	int	fr_argd[6];	/* `arg dump area' (lunacy) */
-	int	fr_argx[1];	/* arg extension (args 7..n; variable size) */
+	int32_t	fr_xxx;		/* `structure return pointer' (unused) */
+	int32_t	fr_argd[6];	/* `arg dump area' (lunacy) */
+	int32_t	fr_argx[1];	/* arg extension (args 7..n; variable size) */
 };
+#else
+struct frame32 {
+	int32_t	fr_local[8];	/* space to save locals (%l0..%l7) */
+	int32_t	fr_arg[6];	/* space to save arguments (%i0..%i5) */
+	u_int32_t	fr_fp;	/* space to save frame pointer (%i6) */
+	u_int32_t	fr_pc;	/* space to save return pc (%i7) */
+	/*
+	 * SunOS reserves another 8 words here; this is pointless
+	 * but we do it for compatibility.
+	 */
+	int32_t	fr_xxx;		/* `structure return pointer' (unused) */
+	int32_t	fr_argd[6];	/* `arg dump area' (lunacy) */
+	int32_t	fr_argx[1];	/* arg extension (args 7..n; variable size) */
+};
+#endif
+#endif
+
+/*
+ * CCFSZ (C Compiler Frame SiZe) is the size of a stack frame required if
+ * a function is to call C code.  It should be just 64, but Sun defined
+ * their frame with space to hold arguments 0 through 5 (plus some junk),
+ * and varargs routines (such as kprintf) demand this, and gcc uses this
+ * area at times anyway.
+ */
+#define CCFSZ		96
+
+/*
+ * Sparc v9 stack frame format.
+ *
+ * Note that the contents of each stack frame may be held only in
+ * machine register windows.  In order to get an accurate picture
+ * of the frame, you must first force the kernel to write any such
+ * windows to the stack.
+ *
+ * V9 frames have an odd bias, so you can tall a v9 frame from
+ * a v8 frame by testing the stack pointer's lsb.
+ */
+#if !defined(_LOCORE) && !defined(_LIBC)
+struct frame64 {
+	int64_t	fr_local[8];	/* space to save locals (%l0..%l7) */
+	int64_t	fr_arg[6];	/* space to save arguments (%i0..%i5) */
+	u_int64_t	fr_fp;		/* space to save frame pointer (%i6) */
+	u_int64_t	fr_pc;		/* space to save return pc (%i7) */
+	/*
+	 * SVR4 reserves a bunch of extra stuff.
+	 */
+	int64_t fr_argd[6];	/* `register save area' (lunacy) */
+	int64_t	fr_argx[0];	/* arg extension (args 7..n; variable size) */
+};
+
+#define v9next_frame(f)		((struct frame64*)(f->fr_fp+BIAS))
+#endif
+
+/*
+ * CC64FSZ (C Compiler 64-bit Frame SiZe) is the size of a stack frame used
+ * by the compiler in 64-bit mode.  It is (16)*8; space for 8 ins, 8 outs.
+ */
+#define CC64FSZ		176
+
+/*
+ * v9 stacks all have a bias of 2047 added to the %sp and %fp, so you can easily
+ * detect it by testing the register for an odd value.  Why 2K-1 I don't know.
+ */
+#define BIAS	(2048-1)
+
