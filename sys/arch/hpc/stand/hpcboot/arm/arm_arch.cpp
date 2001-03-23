b8a9f52dc1dd14d06c00611acf4ea93bab9bea62
toshii <toshii@NetBSD.org>
2001-03-23 08:48:12 +0000

    Pass bootargs like usual function calls. Initialize sp register to more useful value.

--- sys/arch/hpc/stand/hpcboot/arm/arm_arch.cpp
+++ sys/arch/hpc/stand/hpcboot/arm/arm_arch.cpp
@@ -1,4 +1,4 @@
-/*	$NetBSD: arm_arch.cpp,v 1.1 2001/02/09 18:34:49 uch Exp $	*/
+/*	$NetBSD: arm_arch.cpp,v 1.2 2001/03/23 08:48:12 toshii Exp $	*/
 
 /*-
  * Copyright (c) 2001 The NetBSD Foundation, Inc.
@@ -128,7 +128,7 @@ ARMArchitecture::jump(paddr_t info, paddr_t pvec)
 
 	// stack for bootloader
 	_mem->getPage(v, p);
-	sp = ptokv(p);
+	sp = ptokv(p) + _mem->getPageSize();
 
 	// writeback whole D-cache
 	WritebackDCache();
