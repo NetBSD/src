1deb61d750755983b363dc89dac19df4abc4752e
ichiro <ichiro@NetBSD.org>
2001-03-26 12:40:39 +0000

    add UPLCOM_DEBUG

--- sys/dev/usb/usb_port.h
+++ sys/dev/usb/usb_port.h
@@ -1,5 +1,5 @@
 /*	$OpenBSD: usb_port.h,v 1.18 2000/09/06 22:42:10 rahnds Exp $ */
-/*	$NetBSD: usb_port.h,v 1.40 2001/01/23 17:03:19 augustss Exp $	*/
+/*	$NetBSD: usb_port.h,v 1.41 2001/03/26 12:40:39 ichiro Exp $	*/
 /*	$FreeBSD: src/sys/dev/usb/usb_port.h,v 1.21 1999/11/17 22:33:47 n_hibma Exp $	*/
 
 /*
@@ -64,6 +64,7 @@
 #define UHUB_DEBUG 1
 #define ULPT_DEBUG 1
 #define UCOM_DEBUG 1
+#define UPLCOM_DEBUG 1
 #define UMODEM_DEBUG 1
 #define UAUDIO_DEBUG 1
 #define AUE_DEBUG 1
