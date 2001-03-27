d85c2b7e1715097bb07a114ef3d443076683e5ba
toshii <toshii@NetBSD.org>
2001-03-27 18:09:31 +0000

    Change place of tuple.{memt,memh} initialization.

--- sys/dev/pcmcia/pcmcia_cis.c
+++ sys/dev/pcmcia/pcmcia_cis.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: pcmcia_cis.c,v 1.21 2001/02/10 23:22:10 martin Exp $	*/
+/*	$NetBSD: pcmcia_cis.c,v 1.22 2001/03/27 18:09:31 toshii Exp $	*/
 
 #define	PCMCIACISDEBUG
 
@@ -128,9 +128,6 @@ pcmcia_scan_cis(dev, fct, arg)
 #endif
 		return -1;
 	}
-	tuple.memt = pcmh.memt;
-	tuple.memh = pcmh.memh;
-
 	/* initialize state for the primary tuple chain */
 	if (pcmcia_chip_mem_map(pct, pch, PCMCIA_MEM_ATTR, 0,
 	    PCMCIA_CIS_SIZE, &pcmh, &tuple.ptr, &window)) {
@@ -141,6 +138,9 @@ pcmcia_scan_cis(dev, fct, arg)
 #endif
 		return -1;
 	}
+	tuple.memt = pcmh.memt;
+	tuple.memh = pcmh.memh;
+
 	DPRINTF(("cis mem map %x\n", (unsigned int) tuple.memh));
 
 	tuple.mult = 2;
