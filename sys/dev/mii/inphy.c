571a9adabf0560ffe76db678b95427abccf6729b
drochner <drochner>
2001-03-28 14:16:49 +0000

    update for miidevs changes

--- sys/dev/mii/inphy.c
+++ sys/dev/mii/inphy.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: inphy.c,v 1.20 2000/07/04 03:28:59 thorpej Exp $	*/
+/*	$NetBSD: inphy.c,v 1.21 2001/03/28 14:16:49 drochner Exp $	*/
 
 /*-
  * Copyright (c) 1998, 1999, 2000 The NetBSD Foundation, Inc.
@@ -111,8 +111,8 @@ inphymatch(parent, match, aux)
 {
 	struct mii_attach_args *ma = aux;
 
-	if (MII_OUI(ma->mii_id1, ma->mii_id2) == MII_OUI_INTEL &&
-	    MII_MODEL(ma->mii_id2) == MII_MODEL_INTEL_I82555)
+	if (MII_OUI(ma->mii_id1, ma->mii_id2) == MII_OUI_yyINTEL &&
+	    MII_MODEL(ma->mii_id2) == MII_MODEL_yyINTEL_I82555)
 		return (10);
 
 	return (0);
@@ -127,7 +127,7 @@ inphyattach(parent, self, aux)
 	struct mii_attach_args *ma = aux;
 	struct mii_data *mii = ma->mii_data;
 
-	printf(": %s, rev. %d\n", MII_STR_INTEL_I82555,
+	printf(": %s, rev. %d\n", MII_STR_yyINTEL_I82555,
 	    MII_REV(ma->mii_id2));
 
 	sc->mii_inst = mii->mii_instance;
