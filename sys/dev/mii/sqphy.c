adf9edcd41c189c74c912c30239625ca0fe434c0
drochner <drochner@NetBSD.org>
2001-03-28 14:16:49 +0000

    update for miidevs changes

--- sys/dev/mii/sqphy.c
+++ sys/dev/mii/sqphy.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: sqphy.c,v 1.19 2000/07/04 03:29:00 thorpej Exp $	*/
+/*	$NetBSD: sqphy.c,v 1.20 2001/03/28 14:16:49 drochner Exp $	*/
 
 /*-
  * Copyright (c) 1998, 1999, 2000 The NetBSD Foundation, Inc.
@@ -111,8 +111,8 @@ sqphymatch(parent, match, aux)
 {
 	struct mii_attach_args *ma = aux;
 
-	if (MII_OUI(ma->mii_id1, ma->mii_id2) == MII_OUI_xxSEEQ &&
-	    MII_MODEL(ma->mii_id2) == MII_MODEL_xxSEEQ_80220)
+	if (MII_OUI(ma->mii_id1, ma->mii_id2) == MII_OUI_SEEQ &&
+	    MII_MODEL(ma->mii_id2) == MII_MODEL_SEEQ_80220)
 		return (10);
 
 	return (0);
@@ -127,7 +127,7 @@ sqphyattach(parent, self, aux)
 	struct mii_attach_args *ma = aux;
 	struct mii_data *mii = ma->mii_data;
 
-	printf(": %s, rev. %d\n", MII_STR_xxSEEQ_80220,
+	printf(": %s, rev. %d\n", MII_STR_SEEQ_80220,
 	    MII_REV(ma->mii_id2));
 
 	sc->mii_inst = mii->mii_instance;
