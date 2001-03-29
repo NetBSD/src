946dd3c4c0ea14949c1b86486d96bd4046120b3e
marcus <marcus@NetBSD.org>
2001-03-29 03:23:33 +0000

    When the endians are little, they are little.

--- sys/arch/sh3/include/elf_machdep.h
+++ sys/arch/sh3/include/elf_machdep.h
@@ -1,6 +1,14 @@
-/*	$NetBSD: elf_machdep.h,v 1.3 2000/04/02 15:35:50 minoura Exp $	*/
+/*	$NetBSD: elf_machdep.h,v 1.4 2001/03/29 03:23:33 marcus Exp $	*/
 
+#ifndef _BYTE_ORDER
+#error Define _BYTE_ORDER!
+#endif
+
+#if _BYTE_ORDER == _LITTLE_ENDIAN
+#define	ELF32_MACHDEP_ENDIANNESS	ELFDATA2LSB
+#else
 #define	ELF32_MACHDEP_ENDIANNESS	ELFDATA2MSB
+#endif
 #define	ELF32_MACHDEP_ID_CASES						\
 		case EM_SH:						\
 			break;
