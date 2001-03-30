89a4d72c9f08715aacb4a0fe23ff8bcb05c64c4d
leo <leo@NetBSD.org>
2001-03-30 20:00:06 +0000

    Optimize bswap16() and bswap32() functions to inline assembly.

---
+++ sys/arch/m68k/include/bswap.h
@@ -0,0 +1,18 @@
+/*      $NetBSD: bswap.h,v 1.3.8.2 2001/03/30 20:00:06 leo Exp $      */
+
+#ifndef _MACHINE_BSWAP_H_
+#define	_MACHINE_BSWAP_H_
+
+#define	__BSWAP_RENAME
+#include <sys/bswap.h>
+
+#ifdef	__GNUC__
+
+#include <m68k/byte_swap.h>
+
+#define bswap16(x)	__byte_swap_word(x)
+#define bswap32(x)	__byte_swap_long(x)
+
+#endif	/* __GNUC__ */
+
+#endif /* !_MACHINE_BSWAP_H_ */
