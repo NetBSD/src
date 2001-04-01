c097d4a92a12acf4f8f2583f0f7b97092ed43eb5
he <he@NetBSD.org>
2001-04-01 16:59:35 +0000

    Pull up revision 1.23 (requested by tsubai):   Fix Keyboard problem of recent PowerBooks (Pismo, PowerBook G4,   etc.).

--- sys/arch/macppc/macppc/autoconf.c
+++ sys/arch/macppc/macppc/autoconf.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: autoconf.c,v 1.21.4.1 2000/11/01 16:25:19 tv Exp $	*/
+/*	$NetBSD: autoconf.c,v 1.21.4.2 2001/04/01 16:59:35 he Exp $	*/
 
 /*
  * Copyright (C) 1995, 1996 Wolfgang Solfrank.
@@ -379,4 +379,33 @@ pcidev_to_ofdev(pc, tag)
 		}
 	}
 	return 0;
+}
+
+int
+getnodebyname(start, target)
+	int start;
+	char *target;
+{
+	int node, next;
+	char name[64];
+
+	if (start == 0)
+		start = OF_peer(0);
+
+	for (node = start; node; node = next) {
+		bzero(name, sizeof name);
+		OF_getprop(node, "name", name, sizeof name - 1);
+		if (strcmp(name, target) == 0)
+			break;
+
+		if ((next = OF_child(node)) != 0)
+			continue;
+		while (node) {
+			if ((next = OF_peer(node)) != 0)
+				break;
+			node = OF_parent(node);
+		}
+	}
+
+	return node;
 }
