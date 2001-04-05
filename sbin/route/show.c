962a0d5b5c05e4caed8ca034ba0ec1720ba73a9c
he <he>
2001-04-05 12:33:54 +0000

    Pull up revision 1.16 (requested by itojun):   Support RTF_BLACKHOLE routes.

--- sbin/route/show.c
+++ sbin/route/show.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: show.c,v 1.14.4.1 2000/10/18 00:39:48 tv Exp $	*/
+/*	$NetBSD: show.c,v 1.14.4.2 2001/04/05 12:33:54 he Exp $	*/
 
 /*
  * Copyright (c) 1983, 1988, 1993
@@ -38,7 +38,7 @@
 #if 0
 static char sccsid[] = "from: @(#)route.c	8.3 (Berkeley) 3/9/94";
 #else
-__RCSID("$NetBSD: show.c,v 1.14.4.1 2000/10/18 00:39:48 tv Exp $");
+__RCSID("$NetBSD: show.c,v 1.14.4.2 2001/04/05 12:33:54 he Exp $");
 #endif
 #endif /* not lint */
 
@@ -89,6 +89,7 @@ static const struct bits bits[] = {
 	{ RTF_XRESOLVE,	'X' },
 	{ RTF_LLINFO,	'L' },
 	{ RTF_STATIC,	'S' },
+	{ RTF_BLACKHOLE, 'B' },
 	{ RTF_PROTO1,	'1' },
 	{ RTF_PROTO2,	'2' },
 	{ 0 }
