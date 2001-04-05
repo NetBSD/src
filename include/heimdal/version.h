6464b2f3c70bd7d48091c3b08f33025f730c83f4
he <he>
2001-04-05 23:25:55 +0000

    Apply patch (requested by assar):   Upgrade Heimdal to version 0.3e.

--- include/heimdal/version.h
+++ include/heimdal/version.h
@@ -1,20 +1,20 @@
-/*	$NetBSD: version.h,v 1.2.2.2 2000/07/15 17:04:27 thorpej Exp $	*/
+/*	$NetBSD: version.h,v 1.2.2.3 2001/04/05 23:25:56 he Exp $	*/
 
-#ifdef __KRB5_VERSION
+#if defined(__KRB5_VERSION) && !defined(__NO_KRB4_VERSION)
 #define	heimdal_long_version	__heimdal_long_version
 #define	heimdal_version		__heimdal_version
 #define	__NO_KRB4_VERSION
 #endif
 
-#ifdef __KRB4_VERSION
+#if defined(__KRB4_VERSION) && !defined(__NO_KRB5_VERSION)
 #define	krb4_long_version	__krb4_long_version
 #define	krb4_version		__krb4_version
 #define	__NO_KRB5_VERSION
 #endif
 
 #ifndef __NO_KRB5_VERSION
-const char *heimdal_long_version = "@(#)$Version: heimdal-0.2t (NetBSD) $";
-const char *heimdal_version = "heimdal-0.2t";
+const char *heimdal_long_version = "@(#)$Version: heimdal-0.3e (NetBSD) $";
+const char *heimdal_version = "heimdal-0.3e";
 #endif
 
 #ifndef __NO_KRB4_VERSION
