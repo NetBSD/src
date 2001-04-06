2270841d5d7151972de2f6ee6bfa7011f24fd706
fredette <fredette@NetBSD.org>
2001-04-06 13:13:01 +0000

    Added.

---
+++ sys/arch/sun2/include/bswap.h
@@ -0,0 +1,8 @@
+/*     $NetBSD: bswap.h,v 1.1 2001/04/06 13:13:02 fredette Exp $   */
+
+#ifndef _MACHINE_BSWAP_H_
+#define	_MACHINE_BSWAP_H_
+
+#include <sys/bswap.h>
+
+#endif /* !_MACHINE_BSWAP_H_ */
