b00399899f42dc3a9930370676f7b9d7778e444c
simonb <simonb@NetBSD.org>
2001-04-06 16:48:11 +0000

    Fix typo - s/dbcst/dcbst/.

--- sys/arch/powerpc/powerpc/db_disasm.c
+++ sys/arch/powerpc/powerpc/db_disasm.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: db_disasm.c,v 1.2 2000/06/09 14:07:13 kleink Exp $	*/
+/*	$NetBSD: db_disasm.c,v 1.3 2001/04/06 16:48:11 simonb Exp $	*/
 /*	$OpenBSD: db_disasm.c,v 1.2 1996/12/28 06:21:48 rahnds Exp $	*/
 
 #include <sys/param.h>
@@ -217,7 +217,7 @@ const struct opcode opcodes_1f[] = {
 	{ "cmpl",	0xfc0007fe, 0x7c000040, Op_crfD | Op_L | Op_A | Op_B },
 	{ "subf",	0xfc0007fe, 0x7c000050, Op_D | Op_A | Op_B | Op_OE | Op_Rc },
 	{ "ldux",	0xfc0007fe, 0x7c00006a, Op_D | Op_A | Op_B },
-	{ "dbcst",	0xfc0007fe, 0x7c00006c, Op_A | Op_B },
+	{ "dcbst",	0xfc0007fe, 0x7c00006c, Op_A | Op_B },
 	{ "lwzux",	0xfc0007fe, 0x7c00006e, Op_D | Op_A | Op_B },
 	{ "cntlzd",	0xfc0007fe, 0x7c000074, Op_S | Op_A | Op_Rc },
 	{ "andc",	0xfc0007fe, 0x7c000078, Op_S | Op_A | Op_B | Op_Rc },
