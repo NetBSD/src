59730aa07c1d2a02974482e6770b455a422f318e
fredette <fredette@NetBSD.org>
2001-04-06 13:13:01 +0000

    Added.

---
+++ sys/arch/sun2/include/ansi.h
@@ -0,0 +1,3 @@
+/*	$NetBSD: ansi.h,v 1.1 2001/04/06 13:13:01 fredette Exp $	*/
+
+#include <m68k/ansi.h>
---
+++ sys/arch/sun2/include/conf.h
@@ -0,0 +1,72 @@
+/*	$NetBSD: conf.h,v 1.1 2001/04/06 13:13:02 fredette Exp $	*/
+
+/*-
+ * Copyright (c) 1994 Adam Glass, Gordon W. Ross
+ * Copyright (c) 1991, 1993
+ *	The Regents of the University of California.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed by the University of
+ *	California, Berkeley and its contributors.
+ * 4. Neither the name of the University nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ *	@(#)conf.c	8.2 (Berkeley) 11/14/93
+ */
+
+#define	mmread	mmrw
+#define	mmwrite	mmrw
+cdev_decl(mm);
+
+cdev_decl(cn);
+
+cdev_decl(zs);
+
+cdev_decl(fb);
+
+cdev_decl(ms);
+
+cdev_decl(kbd);
+cdev_decl(kd);
+
+cdev_decl(bwtwo);
+
+bdev_decl(xd);
+cdev_decl(xd);
+
+bdev_decl(xy);
+cdev_decl(xy);
+
+/* swap device (required) */
+bdev_decl(sw);
+cdev_decl(sw);
+
+bdev_decl(md);
+cdev_decl(md);
+
+bdev_decl(raid);
+cdev_decl(raid);
+
+cdev_decl(scsibus);
