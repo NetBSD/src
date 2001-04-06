dbf552c3c01ef311704d5440a095fc9b6200b1b5
he <he@NetBSD.org>
2001-04-06 00:25:38 +0000

    Pull up revision 1.54 (requested by itojun):   Record IPsec packet history in m_aux structure.  Let ipfilter   look at wire-format packet only (not the decapsulated ones), so   that VPN setting can work with NAT/ipfilter settings.

--- sys/netinet/raw_ip.c
+++ sys/netinet/raw_ip.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: raw_ip.c,v 1.53.4.1 2001/02/26 22:43:55 he Exp $	*/
+/*	$NetBSD: raw_ip.c,v 1.53.4.2 2001/04/06 00:25:38 he Exp $	*/
 
 /*
  * Copyright (C) 1995, 1996, 1997, and 1998 WIDE Project.
@@ -296,7 +296,10 @@ rip_output(m, va_alist)
 		ipstat.ips_rawout++;
 	}
 #ifdef IPSEC
-	ipsec_setsocket(m, inp->inp_socket);
+	if (ipsec_setsocket(m, inp->inp_socket) != 0) {
+		m_freem(m);
+		return ENOBUFS;
+	}
 #endif /*IPSEC*/
 	return (ip_output(m, opts, &inp->inp_route, flags, inp->inp_moptions, &inp->inp_errormtu));
 }
