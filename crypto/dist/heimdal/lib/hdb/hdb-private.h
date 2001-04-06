0cef08088c673ee005c1cff86dd94dd5a2fdb4cf
itojun <itojun>
2001-04-06 08:32:35 +0000

    sync HEAD with v0-3e tag.  it seems that it was left behind when heimdal 0.3e is imported.  without this change, lib/libhdb does not compile. PR12560

--- crypto/dist/heimdal/lib/hdb/hdb-private.h
+++ crypto/dist/heimdal/lib/hdb/hdb-private.h
@@ -13,4 +13,24 @@
 #endif
 #endif
 
+krb5_error_code
+_hdb_fetch __P((
+	krb5_context context,
+	HDB *db,
+	unsigned flags,
+	hdb_entry *entry));
+
+krb5_error_code
+_hdb_remove __P((
+	krb5_context context,
+	HDB *db,
+	hdb_entry *entry));
+
+krb5_error_code
+_hdb_store __P((
+	krb5_context context,
+	HDB *db,
+	unsigned flags,
+	hdb_entry *entry));
+
 #endif /* __hdb_private_h__ */
