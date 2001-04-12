22c746ceb4fa701e6adfa417737df14e6ca0c006
kleink <kleink>
2001-04-12 22:01:18 +0000

    Replace the 'unsigned __COMPILER_INT64__' construct with a new name, __COMPILER_UINT64__, to be supplied - if such a case is made, it shouldn't be assumed that the unsigned type-specifier may be applied to it.

--- sys/arch/mips/include/int_types.h
+++ sys/arch/mips/include/int_types.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: int_types.h,v 1.3 2001/01/03 10:09:01 takemura Exp $	*/
+/*	$NetBSD: int_types.h,v 1.4 2001/04/12 22:01:19 kleink Exp $	*/
 
 /*-
  * Copyright (c) 1992, 1993
@@ -54,8 +54,8 @@ typedef	unsigned short int     __uint16_t;
 typedef	int			__int32_t;
 typedef	unsigned int	       __uint32_t;
 #ifdef __COMPILER_INT64__
-typedef	__COMPILER_INT64__		__int64_t;
-typedef	unsigned __COMPILER_INT64__	__uint64_t;
+typedef	__COMPILER_INT64__	__int64_t;
+typedef	__COMPILER_UINT64__    __uint64_t;
 #else
 /* LONGLONG */
 typedef	long long int		__int64_t;
