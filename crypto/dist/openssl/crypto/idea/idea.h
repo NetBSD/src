2b21767f55a566f7bc6a7a792c0662495af57d23
itojun <itojun@NetBSD.org>
2001-04-12 07:48:03 +0000

    use openssl 0.9.6a.  shlib major # is bumped for libcrypto, libssl and all kerberos libraries.

--- crypto/dist/openssl/crypto/idea/idea.h
+++ crypto/dist/openssl/crypto/idea/idea.h
@@ -59,11 +59,6 @@
 #ifndef HEADER_IDEA_H
 #define HEADER_IDEA_H
 
-#ifdef  __cplusplus
-extern "C" {
-#endif
-
-
 #include <sys/types.h>
 #define	IDEA_INT	u_int32_t
 
@@ -73,22 +68,26 @@ extern "C" {
 #define IDEA_BLOCK	8
 #define IDEA_KEY_LENGTH	16
 
+#ifdef  __cplusplus
+extern "C" {
+#endif
+
 typedef struct idea_key_st
 	{
 	IDEA_INT data[9][6];
 	} IDEA_KEY_SCHEDULE;
 
 const char *idea_options(void);
-void idea_ecb_encrypt(unsigned char *in, unsigned char *out,
+void idea_ecb_encrypt(const unsigned char *in, unsigned char *out,
 	IDEA_KEY_SCHEDULE *ks);
-void idea_set_encrypt_key(unsigned char *key, IDEA_KEY_SCHEDULE *ks);
+void idea_set_encrypt_key(const unsigned char *key, IDEA_KEY_SCHEDULE *ks);
 void idea_set_decrypt_key(IDEA_KEY_SCHEDULE *ek, IDEA_KEY_SCHEDULE *dk);
-void idea_cbc_encrypt(unsigned char *in, unsigned char *out,
+void idea_cbc_encrypt(const unsigned char *in, unsigned char *out,
 	long length, IDEA_KEY_SCHEDULE *ks, unsigned char *iv,int enc);
-void idea_cfb64_encrypt(unsigned char *in, unsigned char *out,
+void idea_cfb64_encrypt(const unsigned char *in, unsigned char *out,
 	long length, IDEA_KEY_SCHEDULE *ks, unsigned char *iv,
 	int *num,int enc);
-void idea_ofb64_encrypt(unsigned char *in, unsigned char *out,
+void idea_ofb64_encrypt(const unsigned char *in, unsigned char *out,
 	long length, IDEA_KEY_SCHEDULE *ks, unsigned char *iv, int *num);
 void idea_encrypt(IDEA_INT *in, IDEA_KEY_SCHEDULE *ks);
 #ifdef  __cplusplus
