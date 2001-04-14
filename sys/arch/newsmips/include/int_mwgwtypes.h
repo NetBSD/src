2929d93609f510c2849870be33c289984ab75efc
kleink <kleink@NetBSD.org>
2001-04-14 12:19:49 +0000

    Add definitions of C99 minimum-width and greatest-width integer types. XXX Fastest minimum-width integer types haven't been decided upon yet.

---
+++ sys/arch/newsmips/include/int_mwgwtypes.h
@@ -0,0 +1,3 @@
+/*	$NetBSD: int_mwgwtypes.h,v 1.1 2001/04/14 12:19:58 kleink Exp $	*/
+
+#include <mips/int_mwgwtypes.h>
---
+++ sys/arch/prep/include/int_mwgwtypes.h
@@ -0,0 +1,3 @@
+/* $NetBSD: int_mwgwtypes.h,v 1.1 2001/04/14 12:20:00 kleink Exp $ */
+
+#include <powerpc/int_mwgwtypes.h>
