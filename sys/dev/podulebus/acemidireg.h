5fd7ee0cc4af51de1de9419dd8af263c9bc87176
bjh21 <bjh21@NetBSD.org>
2001-04-14 20:39:24 +0000

    Add rudimentary support for the ACE Midi-Connect card which abs gave me. Currently it appears as a "com" port, which is less than useful, and occasionally generates stray interrupts.  "stty -f /dev/tty00 31250 raw" does enable it to talk to a nearby MIDI device, though.

---
+++ sys/dev/podulebus/acemidireg.h
@@ -0,0 +1,8 @@
+/* $NetBSD: acemidireg.h,v 1.1.10.2 2001/04/14 20:39:24 bjh21 Exp $ */
+
+/*
+ * This file is in the public domain.
+ */
+
+#define ACEMIDI_16550_BASE	0x2000
+#define ACEMIDI_16550_FREQ	2000000
