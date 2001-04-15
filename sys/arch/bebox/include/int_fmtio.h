0dea4a204a474d495c823e917635d96bb6a7bc9f
kleink <kleink@NetBSD.org>
2001-04-15 17:13:06 +0000

    Add definitions of C99 integer format conversion macros. XXX Fastest minimum-width integer types haven't been decided upon yet.

---
+++ sys/arch/bebox/include/int_fmtio.h
@@ -0,0 +1,3 @@
+/*	$NetBSD: int_fmtio.h,v 1.1.12.2 2001/04/15 17:13:09 kleink Exp $	*/
+
+#include <powerpc/int_fmtio.h>
---
+++ sys/arch/macppc/include/int_fmtio.h
@@ -0,0 +1,3 @@
+/*	$NetBSD: int_fmtio.h,v 1.1.12.2 2001/04/15 17:13:13 kleink Exp $	*/
+
+#include <powerpc/int_fmtio.h>
