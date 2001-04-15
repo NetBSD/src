6423fcbc12aa0232d0faf45942460977f1706feb
kleink <kleink@NetBSD.org>
2001-04-15 15:29:02 +0000

    Add definitions of C99 specified-width integer type limits. XXX Fastest minimum-width integer types haven't been decided upon yet.

---
+++ sys/arch/atari/include/int_limits.h
@@ -0,0 +1,3 @@
+/*	$NetBSD: int_limits.h,v 1.1 2001/04/15 15:29:04 kleink Exp $	*/
+
+#include <m68k/int_limits.h>
---
+++ sys/arch/hpcsh/include/int_limits.h
@@ -0,0 +1,3 @@
+/*	$NetBSD: int_limits.h,v 1.1 2001/04/15 15:29:06 kleink Exp $	*/
+
+#include <sh3/int_limits.h>
