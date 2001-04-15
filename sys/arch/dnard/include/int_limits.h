800780d9f237d9b272f69169d7afd363918865e5
kleink <kleink>
2001-04-15 15:29:02 +0000

    Add definitions of C99 specified-width integer type limits. XXX Fastest minimum-width integer types haven't been decided upon yet.

---
+++ sys/arch/dnard/include/int_limits.h
@@ -0,0 +1,3 @@
+/*	$NetBSD: int_limits.h,v 1.1 2001/04/15 15:29:13 kleink Exp $	*/
+
+#include <arm/int_limits.h>
---
+++ sys/arch/macppc/include/int_limits.h
@@ -0,0 +1,3 @@
+/*	$NetBSD: int_limits.h,v 1.1 2001/04/15 15:29:08 kleink Exp $	*/
+
+#include <powerpc/int_limits.h>
