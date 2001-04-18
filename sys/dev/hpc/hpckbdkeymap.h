da3b13f348cb74cf6251b8e663a8bdf6c1e51b73
takemura <takemura@NetBSD.org>
2001-04-18 14:01:06 +0000

    Add Mobile Pro 780 own keymap. This resolves the arrow key problem on Mobile Pro 780.

--- sys/dev/hpc/hpckbdkeymap.h
+++ sys/dev/hpc/hpckbdkeymap.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: hpckbdkeymap.h,v 1.9 2001/04/09 16:05:53 sato Exp $ */
+/*	$NetBSD: hpckbdkeymap.h,v 1.10 2001/04/18 14:01:06 takemura Exp $ */
 
 /*-
  * Copyright (c) 1999-2001 The NetBSD Foundation, Inc.
@@ -293,8 +293,8 @@ static const keysym_t mccs_cmdmap[] = {
 };
 
 static u_int8_t mobilepro_keytrans[] = {
-/*00	space	]	\	/	-	-	enter	l	*/
-/*08	-	[	'	;	-	-	.	o	*/
+/*00	space	]	\	/	left	down	enter	l	*/
+/*08	-	[	'	;	right	up	.	o	*/
 /*10	-	-	-	Windows	v	c	x	z	*/
 /*18	-	=	\-	`	f	d	s	a	*/
 /*20	8	7	6	5	r	e	w	q	*/
@@ -357,6 +357,35 @@ static u_int8_t mobilepro750c_keytrans[] = {
 /*48*/	IGN,	56,	IGN,	IGN,	125,	87,	65,	61,
 /*50*/	IGN,	IGN,	29,	IGN,	68,	68,	64,	60,
 /*58*/	IGN,	IGN,	IGN,	42,	13,	67,	63,	59,
+};
+
+/* NEC MobilePro 780 */
+static u_int8_t mobilepro780_keytrans[] = {
+/*00	space	]	\	/	left	right	enter	l	*/
+/*08	-	[	'	;	up	down	.	o	*/
+/*10	-	-	-	Windows	v	c	x	z	*/
+/*18	-	=	\-	`	f	d	s	a	*/
+/*20	8	7	6	5	r	e	w	q	*/
+/*28	,	m	n	b	-	-	0	9	*/
+/*30	k	j	h	g	4	3	2	1	*/
+/*38	i	u	y	t	-	caps	del	esc	*/
+/*40	alt_R	-	-	-	BS	p	TAB	Fn	*/
+/*48	-	alt_L	-	-	f12	f11	f10	f9	*/
+/*50	-	-	ctrl	-	f8	f7	f6	f5	*/
+/*58	-	-	-	shift	f4	f3	f2	f1	*/
+/*----------------------------------------------------------------------*/
+/*00*/	 57,	 27,	 43,	 53,	203,	205,	 28,	 38,
+/*08*/	IGN,	 26,	 40,	 39,	200,	208,	 52,	 24,
+/*10*/	IGN,	IGN,	IGN,	221,	 47,	 46,	 45,	 44,
+/*18*/	IGN,	 13,	 12,	 41,	 33,	 32,	 31,	 30,
+/*20*/	  9,	  8,	  7,	  6,	 19,	 18,	 17,	 16,
+/*28*/	 51,	 50,	 49,	 48,	IGN,	IGN,	 11,	 10,
+/*30*/	 37,	 36,	 35,	 34,	  5,	  4,	  3,	  2,
+/*38*/	 23,	 22,	 21,	 20,	IGN,	 58,	 14,	  1,
+/*40*/	184,	IGN,	IGN,	IGN,	 14,	 25,	 15,	IGN,
+/*48*/	IGN,	 56,	IGN,	IGN,	 88,	 87,	 68,	 67,
+/*50*/	IGN,	IGN,	 29,	IGN,	 66,	 65,	 64,	 63,
+/*58*/	IGN,	IGN,	IGN,	 42,	 62,	 61,	 60,	 59,
 };
 
 /* FUJITSU INTERTOP CX300 */
@@ -594,7 +623,7 @@ const struct hpckbd_keymap_table {
 		CMDMAP(mobilepro_cmdmap),
 		KB_US },
 	{	&platid_mask_MACH_NEC_MCR_530A,
-		mobilepro_keytrans,
+		mobilepro780_keytrans,
 		NULL,
 		NULLCMDMAP,
 		KB_US },
