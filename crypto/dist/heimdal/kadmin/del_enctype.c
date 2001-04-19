bf426a7575e732d7c719a406842119a437c9964f
joda <joda@NetBSD.org>
2001-04-19 07:30:04 +0000

    fix argument count check after getarg change; spotted by Mark Davies in bin/12696

--- crypto/dist/heimdal/kadmin/del_enctype.c
+++ crypto/dist/heimdal/kadmin/del_enctype.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1999-2000 Kungliga Tekniska Högskolan
+ * Copyright (c) 1999-2001 Kungliga Tekniska Högskolan
  * (Royal Institute of Technology, Stockholm, Sweden). 
  * All rights reserved. 
  *
@@ -33,7 +33,7 @@
 
 #include "kadmin_locl.h"
 
-RCSID("$Id: del_enctype.c,v 1.1.1.3 2001/02/11 13:51:32 assar Exp $");
+RCSID("$Id: del_enctype.c,v 1.2 2001/04/19 07:30:04 joda Exp $");
 
 /*
  * del_enctype principal enctypes...
@@ -73,7 +73,7 @@ del_enctype(int argc, char **argv)
 	usage ();
 	return 0;
     }
-    if(argc - optind < 3 || help_flag) {
+    if(argc - optind < 2 || help_flag) {
 	usage ();
 	return 0;
     }
