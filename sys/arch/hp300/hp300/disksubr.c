2e84b8057fc3cad97860694f9cc491eb045b423b
he <he>
2001-04-20 16:59:56 +0000

    Pull up revision 1.11 (requested by he):   Correctly set d_npartitions in in-core disklabel to RAW_PART+1.

--- sys/arch/hp300/hp300/disksubr.c
+++ sys/arch/hp300/hp300/disksubr.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: disksubr.c,v 1.9 1997/04/01 03:12:13 scottr Exp $	*/
+/*	$NetBSD: disksubr.c,v 1.9.32.1 2001/04/20 16:59:56 he Exp $	*/
 
 /*
  * Copyright (c) 1982, 1986, 1988, 1993
@@ -68,7 +68,7 @@ readdisklabel(dev, strat, lp, osdep)
 
 	if (lp->d_secperunit == 0)
 		lp->d_secperunit = 0x1fffffff;
-	lp->d_npartitions = 1;
+	lp->d_npartitions = RAW_PART + 1;
 	if (lp->d_partitions[0].p_size == 0)
 		lp->d_partitions[0].p_size = 0x1fffffff;
 	lp->d_partitions[0].p_offset = 0;
