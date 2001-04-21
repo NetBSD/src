99052484f48883ae5c538bd0cb1827ac0b4bed9e
thorpej <thorpej@NetBSD.org>
2001-04-21 21:57:16 +0000

    Fix LP64 printf format problems.

--- dist/ntp/libparse/clk_dcf7000.c
+++ dist/ntp/libparse/clk_dcf7000.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: clk_dcf7000.c,v 1.1.1.1 2000/03/29 12:38:51 simonb Exp $	*/
+/*	$NetBSD: clk_dcf7000.c,v 1.2 2001/04/21 21:57:16 thorpej Exp $	*/
 
 /*
  * /src/NTP/ntp-4/libparse/clk_dcf7000.c,v 4.6 1999/11/28 09:13:49 kardel RELEASE_19991128_A
@@ -142,7 +142,7 @@ inp_dcf7000(
 {
 	unsigned int rtc;
 	
-	parseprintf(DD_PARSE, ("inp_dcf7000(0x%x, 0x%x, ...)\n", (int)parseio, (int)ch));
+	parseprintf(DD_PARSE, ("inp_dcf7000(0x%lx, 0x%x, ...)\n", (unsigned long)parseio, ch));
 	
 	switch (ch)
 	{
