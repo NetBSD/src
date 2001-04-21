be69ffdc73ec46d36b308c943053d3908a3a187f
thorpej <thorpej@NetBSD.org>
2001-04-21 21:57:16 +0000

    Fix LP64 printf format problems.

--- dist/ntp/libparse/clk_schmid.c
+++ dist/ntp/libparse/clk_schmid.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: clk_schmid.c,v 1.1.1.1 2000/03/29 12:38:51 simonb Exp $	*/
+/*	$NetBSD: clk_schmid.c,v 1.2 2001/04/21 21:57:16 thorpej Exp $	*/
 
 /*
  * /src/NTP/ntp-4/libparse/clk_schmid.c,v 4.5 1999/11/28 09:13:51 kardel RELEASE_19991128_A
@@ -189,7 +189,7 @@ inp_schmid(
 {
 	unsigned int rtc;
 	
-	parseprintf(DD_PARSE, ("inp_schmid(0x%x, 0x%x, ...)\n", (int)parseio, (int)ch));
+	parseprintf(DD_PARSE, ("inp_schmid(0x%lx, 0x%x, ...)\n", (unsigned long)parseio, ch));
 	
 	switch (ch)
 	{
