016b9b37c3ce156348f22aef58a18185442dbb05
thorpej <thorpej@NetBSD.org>
2001-04-21 17:13:12 +0000

    Update the comment for pmap_update().

--- sys/arch/arm/include/arm32/pmap.h
+++ sys/arch/arm/include/arm32/pmap.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: pmap.h,v 1.4 2001/03/04 19:05:56 matt Exp $	*/
+/*	$NetBSD: pmap.h,v 1.5 2001/04/21 17:13:12 thorpej Exp $	*/
 
 /*
  * Copyright (c) 1994,1995 Mark Brinicombe.
@@ -147,7 +147,7 @@ extern int		pmap_debug_level; /* Only exists if PMAP_DEBUG */
  * Macros that we need to export
  */
 #define pmap_kernel()			(&kernel_pmap_store)
-#define pmap_update()			/*cpu_tlb_flushID()*/
+#define pmap_update()			/* nothing (yet) */
 #define	pmap_resident_count(pmap)	((pmap)->pm_stats.resident_count)
 #define	pmap_wired_count(pmap)		((pmap)->pm_stats.wired_count)
 
