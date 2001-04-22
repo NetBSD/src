856547f067f5e410f93eb394ee545c875e112651
bjh21 <bjh21@NetBSD.org>
2001-04-22 15:01:25 +0000

    Driver for Archimedes parallel ports.  I haven't got a parallel printer, so this is currently untested.

---
+++ sys/arch/arm26/ioc/arcppreg.h
@@ -0,0 +1,10 @@
+/* $NetBSD: arcppreg.h,v 1.1 2001/04/22 15:01:25 bjh21 Exp $ */
+
+/*
+ * This file is in the public domain.
+ */
+
+/* Archimedes parallel port registers */
+
+/* Offset of data latch from IOC bank base (bus_size_t units) */
+#define ARCPP_DATA	0x04
