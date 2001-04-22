26a8e50b2c27c8b05a12ab52d1dd4b4e6d95a832
thorpej <thorpej@NetBSD.org>
2001-04-22 23:19:26 +0000

    Undo a misguided previous change to the pmap_update() API.

--- sys/arch/amiga/include/pmap.h
+++ sys/arch/amiga/include/pmap.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: pmap.h,v 1.29 2001/04/22 00:34:00 thorpej Exp $	*/
+/*	$NetBSD: pmap.h,v 1.30 2001/04/22 23:19:27 thorpej Exp $	*/
 
 /* 
  * Copyright (c) 1987 Carnegie-Mellon University
@@ -136,7 +136,7 @@ struct pmap	kernel_pmap_store;
 #define	pmap_resident_count(pmap)	((pmap)->pm_stats.resident_count)
 #define	pmap_wired_count(pmap)		((pmap)->pm_stats.wired_count)
 
-#define	pmap_update(pmap)		/* nothing (yet) */
+#define	pmap_update()			/* nothing (yet) */
 
 vaddr_t		pmap_map __P((vaddr_t, paddr_t, paddr_t, int));
 void		pmap_procwr __P((struct proc *, vaddr_t, u_long));
--- sys/arch/hp300/include/pmap.h
+++ sys/arch/hp300/include/pmap.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: pmap.h,v 1.27 2001/04/22 00:34:00 thorpej Exp $	*/
+/*	$NetBSD: pmap.h,v 1.28 2001/04/22 23:19:27 thorpej Exp $	*/
 
 /* 
  * Copyright (c) 1987 Carnegie-Mellon University
@@ -147,7 +147,7 @@ extern struct pv_entry	*pv_table;	/* array of entries, one per page */
 #define	pmap_resident_count(pmap)	((pmap)->pm_stats.resident_count)
 #define	pmap_wired_count(pmap)		((pmap)->pm_stats.wired_count)
 
-#define	pmap_update(pmap)		/* nothing (yet) */
+#define	pmap_update()			/* nothing (yet) */
 
 extern pt_entry_t	*Sysmap;
 extern char		*vmmap;		/* map for mem, dumps, etc. */
