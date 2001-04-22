9bce9305d12ee872c51cc865bcf7d00a90027693
bjh21 <bjh21@NetBSD.org>
2001-04-22 00:26:35 +0000

    Add a skeleton driver for the random latches on the Archimedes, which are shared between the printer port and floppy drive.  Useful facilities will be added when I know what I need.

--- sys/arch/arm26/ioc/latchvar.h
+++ sys/arch/arm26/ioc/latchvar.h
@@ -1,6 +1,7 @@
-/* $NetBSD: latchreg.h,v 1.2 2001/04/22 00:26:36 bjh21 Exp $ */
+/* $NetBSD: latchvar.h,v 1.1 2001/04/22 00:26:36 bjh21 Exp $ */
+
 /*-
- * Copyright (c) 1997, 1998, 2001 Ben Harris
+ * Copyright (c) 2001 Ben Harris
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -25,37 +26,8 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-/* This file is part of NetBSD/arm26 -- a port of NetBSD to ARM2/3 machines. */
-/*
- * latchreg.h - Archimedes internal latches
- */
 
-#ifndef _LATCHREG_H
-#define _LATCHREG_H
-
-/* Register offsets for latches (bus_space units) */
-
-#define LATCH_A		0x10
-#define LATCH_B		0x06
-
-/* Latch A controls some of the floppy disc system (all active-low). */
-#define LATCHA_NSEL0	0x01	/* Floppy drive 0 select */
-#define LATCHA_NSEL1	0x02	/* Floppy drive 1 select */
-#define LATCHA_NSEL2	0x04	/* Floppy drive 2 select */
-#define LATCHA_NSEL3	0x08	/* Floppy drive 3 select */
-#define LATCHA_NSIDE1	0x10	/* Floppy side select. */
-#define LATCHA_NMOTORON	0x20	/* Motor on/off */
-#define LATCHA_NINUSE	0x40	/* 'In Use' */
-/* Bit 7 not used */
-
-/* Latch B does all sorts of random stuff (mostly active-low). */
-/* Bit 0 reserved */
-#define LATCHB_NDDEN	0x02	/* Floppy disc density setting */
-/* Bit 2 reserved */
-#define LATCHB_NFDCR	0x08	/* Floppy controller reset */
-#define LATCHB_NPSTB	0x10	/* Printer strobe line */
-#define LATCHB_AUX1	0x20	/* Spare (VIDC Enhancer?) */
-#define LATCHB_AUX2	0x40	/* Spare */
-/* Bit 7 not used */
-
-#endif /* !_LATCHREG_H */
+#ifndef _LATCHVAR_H_
+#define _LATCHVAR_H_
+extern struct device *the_latches;
+#endif
