cde5ded0822b5b130e54bc8fc2b73aaf7f114a57
he <he@NetBSD.org>
2001-04-23 22:05:29 +0000

    Pull up revisions 1.38,1.43 (requested by hubertf):   Support Accton EN2242 and other AmdTek AN985 compatible cards   with the tlp(4) driver.

--- sys/dev/ic/tulipvar.h
+++ sys/dev/ic/tulipvar.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: tulipvar.h,v 1.36.4.2 2000/10/17 21:44:15 tv Exp $	*/
+/*	$NetBSD: tulipvar.h,v 1.36.4.3 2001/04/23 22:05:29 he Exp $	*/
 
 /*-
  * Copyright (c) 1998, 1999, 2000 The NetBSD Foundation, Inc.
@@ -149,9 +149,11 @@ typedef enum {
 	TULIP_CHIP_DM9102    = 18,	/* Davicom DM9102 */
 	TULIP_CHIP_DM9102A   = 19,	/* Davicom DM9102A */
 	TULIP_CHIP_AL981     = 20,	/* ADMtek AL981 */
-	TULIP_CHIP_AX88140   = 21,	/* ASIX AX88140 */
-	TULIP_CHIP_AX88141   = 22,	/* ASIX AX88141 */
-	TULIP_CHIP_X3201_3   = 23,	/* Xircom X3201-3 */
+	TULIP_CHIP_AN983     = 21,	/* ADMtek AN983 */
+	TULIP_CHIP_AN985     = 22,	/* ADMtek AN985 */
+	TULIP_CHIP_AX88140   = 23,	/* ASIX AX88140 */
+	TULIP_CHIP_AX88141   = 24,	/* ASIX AX88141 */
+	TULIP_CHIP_X3201_3   = 25,	/* Xircom X3201-3 */
 } tulip_chip_t;
 
 #define	TULIP_CHIP_NAMES						\
@@ -177,6 +179,8 @@ typedef enum {
 	"Davicom DM9102",						\
 	"Davicom DM9102A",						\
 	"ADMtek AL981",							\
+	"ADMtek AN983",							\
+	"ADMtek AN985",							\
 	"ASIX AX88140",							\
 	"ASIX AX88141",							\
 	"Xircom X3201-3",						\
@@ -584,6 +588,7 @@ extern const struct tulip_mediasw tlp_sio_mii_mediasw;
 extern const struct tulip_mediasw tlp_pnic_mediasw;
 extern const struct tulip_mediasw tlp_pmac_mediasw;
 extern const struct tulip_mediasw tlp_al981_mediasw;
+extern const struct tulip_mediasw tlp_an985_mediasw;
 extern const struct tulip_mediasw tlp_dm9102_mediasw;
 
 void	tlp_attach __P((struct tulip_softc *, const u_int8_t *));
