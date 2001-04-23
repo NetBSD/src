b1c95a5eb1d980aaf1dc405383966be1b2628571
thorpej <thorpej@NetBSD.org>
2001-04-23 23:50:22 +0000

    Call pmap_bootstrap() before calling uvm_pageboot_alloc(), so that the kernel virtual address space bounds will be set up.

--- sys/arch/hpcmips/hpcmips/machdep.c
+++ sys/arch/hpcmips/hpcmips/machdep.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: machdep.c,v 1.50 2001/04/23 11:31:15 sato Exp $	*/
+/*	$NetBSD: machdep.c,v 1.51 2001/04/23 23:50:23 thorpej Exp $	*/
 
 /*-
  * Copyright (c) 1999 Shin Takemura, All rights reserved.
@@ -72,7 +72,7 @@
 
 #include <sys/cdefs.h>			/* RCS ID & Copyright macro defns */
 
-__KERNEL_RCSID(0, "$NetBSD: machdep.c,v 1.50 2001/04/23 11:31:15 sato Exp $");
+__KERNEL_RCSID(0, "$NetBSD: machdep.c,v 1.51 2001/04/23 23:50:23 thorpej Exp $");
 
 /* from: Utah Hdr: machdep.c 1.63 91/04/24 */
 #include "opt_vr41xx.h"
@@ -494,6 +494,10 @@ mach_init(argc, argv, bi)
 	 */
 	mips_init_msgbuf();
 
+	/*
+	 * Initialize the virtual memory system.
+	 */
+	pmap_bootstrap();
 
 	/*
 	 * Allocate space for system data structures.  These data structures
@@ -505,11 +509,6 @@ mach_init(argc, argv, bi)
 	v = (caddr_t)uvm_pageboot_alloc(size);
 	if ((allocsys(v, NULL) - v) != size)
 		panic("mach_init: table size inconsistency");
-
-	/*
-	 * Initialize the virtual memory system.
-	 */
-	pmap_bootstrap();
 }
 
 
