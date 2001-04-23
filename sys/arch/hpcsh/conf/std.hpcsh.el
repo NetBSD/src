df114b6b66b07b9d986f82c24161433390d4988c
uch <uch@NetBSD.org>
2001-04-23 11:22:55 +0000

    switch to ELF kernel.

--- sys/arch/hpcsh/conf/std.hpcsh.el
+++ sys/arch/hpcsh/conf/std.hpcsh.el
@@ -1,4 +1,4 @@
-# $NetBSD: std.hpcsh.el,v 1.2 2001/03/22 18:35:34 uch Exp $
+# $NetBSD: std.hpcsh.el,v 1.3 2001/04/23 11:22:55 uch Exp $
 #
 # standard, required NetBSD/hpcsh 'options'
  
@@ -7,6 +7,6 @@ machine hpcsh sh3
 options	EXEC_SCRIPT	# exec #! scripts
 
 makeoptions	ENDIAN="-EL"
-makeoptions 	LDSCRIPTBASE="shl-coff.x"	# for COFF kernel
-#makeoptions 	LDSCRIPTBASE="shl-elf.x"	# for ELF kernel
+#makeoptions 	LDSCRIPTBASE="shl-coff.x"	# for COFF kernel
+makeoptions 	LDSCRIPTBASE="shl-elf.x"	# for ELF kernel
 makeoptions 	MACHINE_ARCH=sh3el
