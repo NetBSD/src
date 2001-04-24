f30c0b02f2fcda97a5f63bdd756b24c3758023c6
fredette <fredette@NetBSD.org>
2001-04-24 14:42:33 +0000

    Added these required headers.

---
+++ sys/arch/sun2/include/float.h
@@ -0,0 +1,3 @@
+/*	$NetBSD: float.h,v 1.1.8.2 2001/04/24 14:42:33 fredette Exp $	*/
+
+#include <m68k/float.h>
---
+++ sys/arch/sun2/include/varargs.h
@@ -0,0 +1,3 @@
+/*	$NetBSD: varargs.h,v 1.1.8.2 2001/04/24 14:42:33 fredette Exp $	*/
+
+#include <m68k/varargs.h>
