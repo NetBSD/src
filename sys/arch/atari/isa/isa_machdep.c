0407e47f83c4f2d400c2679f0037ee6fdb3862e7
leo <leo@NetBSD.org>
2001-04-24 06:39:47 +0000

    Split the ISA common attachment stuff and the interrupt handling. There are too many differences between the Hades and Milan in this area.

--- sys/arch/atari/isa/isa_machdep.c
+++ sys/arch/atari/isa/isa_machdep.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: isa_machdep.c,v 1.21 2001/03/09 20:55:47 leo Exp $	*/
+/*	$NetBSD: isa_machdep.c,v 1.22 2001/04/24 06:39:48 leo Exp $	*/
 
 /*
  * Copyright (c) 1997 Leo Weppelman.  All rights reserved.
@@ -33,21 +33,14 @@
 
 #include <sys/types.h>
 #include <sys/param.h>
-#include <sys/time.h>
 #include <sys/systm.h>
-#include <sys/errno.h>
 #include <sys/device.h>
-
-#include <uvm/uvm_extern.h>
 
 #define _ATARI_BUS_DMA_PRIVATE
 #include <machine/bus.h>
 #include <dev/isa/isavar.h>
 #include <dev/isa/isareg.h>
 
-#include <m68k/asm_single.h>
-
-#include <machine/cpu.h>
 #include <machine/iomap.h>
 #include <machine/mfp.h>
 #include <atari/atari/device.h>
@@ -112,6 +105,7 @@ void		*auxp;
 	struct isabus_softc *sc = (struct isabus_softc *)dp;
 	struct isabus_attach_args	iba;
 	extern struct atari_bus_dma_tag isa_bus_dma_tag;
+	extern void isa_bus_init(void);
 
 	iba.iba_busname = "isa";
 	iba.iba_dmat	= &isa_bus_dma_tag;
@@ -125,7 +119,9 @@ void		*auxp;
 	iba.iba_iot->base  = ISA_IOSTART;
 	iba.iba_memt->base = ISA_MEMSTART;
 
-	MFP->mf_aer |= (IO_ISA1|IO_ISA2); /* ISA interrupts: LOW->HIGH */
+	if (machineid & ATARI_HADES)
+	    MFP->mf_aer |= (IO_ISA1|IO_ISA2); /* ISA interrupts: LOW->HIGH */
+	isa_bus_init();
 
 	printf("\n");
 	config_found(dp, &iba, isabusprint);
@@ -148,175 +144,10 @@ isa_attach_hook(parent, self, iba)
 {
 }
 
-/*
- * The interrupt stuff is rather ugly. On the Hades, all interrupt lines
- * for a slot are wired together and connected to either IO3 (slot1) or
- * IO7 (slot2). Since no info can be obtained about the slot position
- * at isa_intr_establish() time, the following assumption is made:
- *   - irq <= 6 -> slot 1
- *   - irq >  6 -> slot 2
- */
-
-#define	SLOTNR(irq)	((irq <= 6) ? 0 : 1)
-
-static isa_intr_info_t iinfo[2] = { { -1 }, { -1 } };
-
-static int	iifun __P((int, int));
-
-static int
-iifun(slot, sr)
-int	slot;
-int	sr;
-{
-	isa_intr_info_t *iinfo_p;
-	int		s;
-
-	iinfo_p = &iinfo[slot];
-
-	/*
-	 * Disable the interrupts
-	 */
-	if (slot == 0) {
-		single_inst_bclr_b(MFP->mf_imrb, IB_ISA1);
-	}
-	else {
-		single_inst_bclr_b(MFP->mf_imra, IA_ISA2);
-	}
-
-	if ((sr & PSL_IPL) >= (iinfo_p->ipl & PSL_IPL)) {
-		/*
-		 * We're running at a too high priority now.
-		 */
-		add_sicallback((si_farg)iifun, (void*)slot, 0);
-	}
-	else {
-		s = splx(iinfo_p->ipl);
-		if (slot == 0) {
-			do {
-				MFP->mf_iprb = (u_int8_t)~IB_ISA1;
-				(void) (iinfo_p->ifunc)(iinfo_p->iarg);
-			} while (MFP->mf_iprb & IB_ISA1);
-			single_inst_bset_b(MFP->mf_imrb, IB_ISA1);
-		}
-		else {
-			do {
-				MFP->mf_ipra = (u_int8_t)~IA_ISA2;
-				(void) (iinfo_p->ifunc)(iinfo_p->iarg);
-			} while (MFP->mf_ipra & IA_ISA2);
-			single_inst_bset_b(MFP->mf_imra, IA_ISA2);
-		}
-		splx(s);
-	}
-	return 1;
-}
-
-
-/*
- * XXXX
- * XXXX Note that this function is not really working yet! The big problem is
- * XXXX to only generate interrupts for the slot the card is in...
- */
-int
-isa_intr_alloc(ic, mask, type, irq)
-	isa_chipset_tag_t ic;
-	int mask;
-	int type;
-	int *irq;
-{
-	isa_intr_info_t *iinfo_p;
-	int		slot, i;
-
-
-	/*
-	 * The Hades only supports edge triggered interrupts!
-	 */
-	if (type != IST_EDGE)
-		return 1;
-
-#define	MAXIRQ		10	/* XXX: Pure fiction	*/
-	for (i = 0; i < MAXIRQ; i++) {
-		if (mask & (1<<i)) {
-		    slot    = SLOTNR(i);
-		    iinfo_p = &iinfo[slot];
-
-		    if (iinfo_p->slot < 0) {
-			*irq = i;
-			printf("WARNING: isa_intr_alloc is not yet ready!\n"
-			       "         make sure the card is in slot %d!\n",
-				slot);
-			return 0;
-		    }
-		}
-	}
-	return (1);
-}
-
 const struct evcnt *
 isa_intr_evcnt(isa_chipset_tag_t ic, int irq)
 {
 
 	/* XXX for now, no evcnt parent reported */
 	return NULL;
-}
-
-void *
-isa_intr_establish(ic, irq, type, level, ih_fun, ih_arg)
-	isa_chipset_tag_t ic;
-	int		  irq, type, level;
-	int		  (*ih_fun) __P((void *));
-	void		  *ih_arg;
-{
-	isa_intr_info_t *iinfo_p;
-	struct intrhand	*ihand;
-	int		slot;
-
-	/*
-	 * The Hades only supports edge triggered interrupts!
-	 */
-	if (type != IST_EDGE)
-		return NULL;
-
-	slot    = SLOTNR(irq);
-	iinfo_p = &iinfo[slot];
-
-	if (iinfo_p->slot >= 0)
-	    panic("isa_intr_establish: interrupt was already established\n");
-
-	ihand = intr_establish((slot == 0) ? 3 : 15, USER_VEC, 0,
-				(hw_ifun_t)iifun, (void *)slot);
-	if (ihand != NULL) {
-		iinfo_p->slot  = slot;
-		iinfo_p->ipl   = level;
-		iinfo_p->ifunc = ih_fun;
-		iinfo_p->iarg  = ih_arg;
-		iinfo_p->ihand = ihand;
-
-		/*
-		 * Enable (unmask) the interrupt
-		 */
-		if (slot == 0) {
-			single_inst_bset_b(MFP->mf_imrb, IB_ISA1);
-			single_inst_bset_b(MFP->mf_ierb, IB_ISA1);
-		}
-		else {
-			single_inst_bset_b(MFP->mf_imra, IA_ISA2);
-			single_inst_bset_b(MFP->mf_iera, IA_ISA2);
-		}
-		return(iinfo_p);
-	}
-	return NULL;
-}
-
-void
-isa_intr_disestablish(ic, handler)
-	isa_chipset_tag_t	ic;
-	void			*handler;
-{
-	isa_intr_info_t *iinfo_p = (isa_intr_info_t *)handler;
-
-	if (iinfo_p->slot < 0)
-	    panic("isa_intr_disestablish: interrupt was not established\n");
-
-	(void) intr_disestablish(iinfo_p->ihand);
-	iinfo_p->slot = -1;
 }
---
+++ sys/arch/atari/isa/isa_milan.c
@@ -0,0 +1,254 @@
+/*	$NetBSD: isa_milan.c,v 1.1 2001/04/24 06:39:48 leo Exp $	*/
+
+/*-
+ * Copyright (c) 2001 The NetBSD Foundation, Inc.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to The NetBSD Foundation
+ * by Leo Weppelman.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed by the NetBSD
+ *	Foundation, Inc. and its contributors.
+ * 4. Neither the name of The NetBSD Foundation nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <sys/types.h>
+#include <sys/param.h>
+#include <sys/systm.h>
+#include <sys/device.h>
+
+#include <dev/isa/isavar.h>
+#include <dev/isa/isareg.h>
+
+#include <machine/iomap.h>
+
+void	isa_bus_init(void);
+#if 0
+static void init_icu(void);
+#endif
+static void set_icus(void);
+static void calc_imask(void);
+
+/*
+ * Bitmask of currently enabled isa interrupts. Used by set_icus().
+ */
+static u_int16_t imask_enable = 0xffff;
+
+#define	IRQ_SLAVE	2
+
+/*
+ * Interrupt routing table
+ */
+#define MILAN_MAX_ISA_INTS	16
+
+static isa_intr_info_t milan_isa_iinfo[MILAN_MAX_ISA_INTS];
+
+void
+isa_bus_init()
+{
+#if 0
+	init_icu();
+#endif
+	set_icus();
+}
+
+#if 0
+static void
+init_icu()
+{
+#define	ICU_OFFSET	0
+
+	u_int8_t	*icu;
+
+	icu = (u_int8_t*)(AD_8259_MASTER);
+
+	icu[0] = 0x11;			/* reset; program device, four bytes */
+	icu[1] = ICU_OFFSET;		/* starting at this vector index */
+	icu[1] = (1 << IRQ_SLAVE);	/* slave on line 2 */
+	icu[1] = 1;			/* 8086 mode */
+	icu[1] = 0xff;			/* leave interrupts masked */
+
+	icu = (u_int8_t*)(AD_8259_SLAVE);
+
+	icu[0] = 0x11;			/* reset; program device, four bytes */
+	icu[1] = ICU_OFFSET + 8;	/* starting at this vector index */
+	icu[1] = IRQ_SLAVE;		/* slave on line 2 */
+	icu[1] = 1;			/* 8086 mode */
+	icu[1] = 0xff;			/* leave interrupts masked */
+}
+#endif
+
+static void
+set_icus()
+{
+	u_int8_t	*icu;
+
+	icu = (u_int8_t*)AD_8259_MASTER;
+	icu[1] = imask_enable & 0xff;
+	icu = (u_int8_t*)AD_8259_SLAVE;
+	icu[1] = (imask_enable >> 8) & 0xff;
+}
+
+static void
+calc_imask()
+{
+	int		irq;
+	u_int16_t	nmask = 0;
+
+	for (irq = 0; irq < MILAN_MAX_ISA_INTS; irq++) {
+		if (milan_isa_iinfo[irq].ifunc != NULL)
+			nmask |= 1 << irq;
+		if (nmask >= 0x100)
+			nmask |= 1 << IRQ_SLAVE;
+	}
+	imask_enable = ~nmask;
+	set_icus();
+}
+
+
+void milan_isa_intr(int);
+void
+milan_isa_intr(vector)
+int	vector;
+{
+	isa_intr_info_t *iinfo_p;
+
+if(vector != 6)
+    printf("milan_isa_intr: vector: %d\n", vector);
+	if (vector >= MILAN_MAX_ISA_INTS) {
+		printf("milan_isa_intr: Bogus vector %d\n", vector);
+		return;
+	}
+/* DEBUGGING LWP */
+if (vector == 1) { /* Keyboard */
+	printf("kbd scancode: 0x%x, 0x%x, 0x%x, 0x%x\n",
+		*((u_char *)(pci_io_addr + 0x60)),
+		*((u_char *)(pci_io_addr + 0x61)),
+		*((u_char *)(pci_io_addr + 0x62)),
+		*((u_char *)(pci_io_addr + 0x63)));
+}
+
+	/* Acknowledge interrupt XXX 0x20 == EOI	*/
+	if (vector > 7)
+		*((u_char *)AD_8259_SLAVE) = 0x20;
+	*((u_char *)AD_8259_MASTER) = 0x20;
+
+	iinfo_p = &milan_isa_iinfo[vector];
+	if (iinfo_p->ifunc == NULL)
+		printf("milan_isa_intr: Stray interrupt: %d (mask:%04x)\n",
+				vector, imask_enable);
+	else (void) (iinfo_p->ifunc)(iinfo_p->iarg);
+}
+
+
+/*
+ * Try to allocate a free interrupt... On the Milan, we have available:
+ * 5, 9, 10, 11, 13. Or in a bitmask: 0x1720.
+ */
+#define	MILAN_AVAIL_ISA_INTS	0x1720
+
+int
+isa_intr_alloc(ic, mask, type, irq)
+	isa_chipset_tag_t ic;
+	int mask;
+	int type;
+	int *irq;
+{
+	int	i;
+
+	/*
+	 * The Hades only supports edge triggered interrupts!
+	 * XXX: Find out about the Milan....
+	 */
+	if (type != IST_EDGE)
+		return 1;
+
+	/*
+	 * Say no to impossible questions...
+	 */
+	if (!(mask &= MILAN_AVAIL_ISA_INTS))
+		return 1;
+
+	for (i = 0; i < MILAN_MAX_ISA_INTS; i++) {
+		if (mask & (1<<i)) {
+		    if (milan_isa_iinfo[i].ifunc == NULL) {
+			*irq = i;
+			return 0;
+		    }
+		}
+	}
+	return (1);
+}
+
+void *
+isa_intr_establish(ic, irq, type, level, ih_fun, ih_arg)
+	isa_chipset_tag_t ic;
+	int		  irq, type, level;
+	int		  (*ih_fun) __P((void *));
+	void		  *ih_arg;
+{
+	isa_intr_info_t *iinfo_p;
+
+	/*
+	 * The Hades only supports edge triggered interrupts!
+	 * XXX: Find out oubout the Milan...
+	 */
+	if (type != IST_EDGE)
+		return NULL;
+
+	iinfo_p = &milan_isa_iinfo[irq];
+
+	if (iinfo_p->ifunc != NULL) {
+		printf("isa_intr_establish: interrupt %d was already "
+			"established\n", irq);
+		return NULL;
+	}
+
+	iinfo_p->slot  = 0;	/* Unused on Milan */
+	iinfo_p->ihand = NULL;	/* Unused on Milan */
+	iinfo_p->ipl   = level;
+	iinfo_p->ifunc = ih_fun;
+	iinfo_p->iarg  = ih_arg;
+printf("Irq %d established\n", irq);
+
+	calc_imask();
+	return(iinfo_p);
+}
+
+void
+isa_intr_disestablish(ic, handler)
+	isa_chipset_tag_t	ic;
+	void			*handler;
+{
+	isa_intr_info_t *iinfo_p = (isa_intr_info_t *)handler;
+
+	if (iinfo_p->ifunc == NULL)
+	    panic("isa_intr_disestablish: interrupt was not established\n");
+
+	iinfo_p->ifunc = NULL;
+	calc_imask();
+}
