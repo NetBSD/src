71163d3dafd91faf13954572917d8afa572b6c90
thorpej <thorpej@NetBSD.org>
2001-04-25 17:54:07 +0000

    Remove some redundant definitions (they're already in <mips/pmap.h>).

--- sys/arch/arc/include/pmap.h
+++ sys/arch/arc/include/pmap.h
@@ -1,7 +1,4 @@
-/*	$NetBSD: pmap.h,v 1.5 2000/01/27 07:49:13 soda Exp $	*/
+/*	$NetBSD: pmap.h,v 1.6 2001/04/25 17:54:07 thorpej Exp $	*/
 /*      $OpenBSD: pmap.h,v 1.3 1997/04/19 17:19:58 pefo Exp $ */
 
 #include <mips/pmap.h>
-
-#define pmap_resident_count(pmap)       ((pmap)->pm_stats.resident_count)
-#define PMAP_PREFER(pa, va)             pmap_prefer((pa), (va))
