55739436f32c5054d7d8ac889aac398bd8652903
uch <uch@NetBSD.org>
2001-04-30 13:41:33 +0000

    add wrapper header for integer types.

---
+++ sys/arch/hpc/stand/include/machine/int_mwgwtypes.h
@@ -0,0 +1,11 @@
+/*	$NetBSD: int_mwgwtypes.h,v 1.1.10.2 2001/04/30 13:41:34 uch Exp $	*/
+
+#if defined ARM
+#include "../../../../arm/include/int_mwgwtypes.h"
+#elif defined MIPS
+#include "../../../../mips/include/int_mwgwtypes.h"
+#elif defined SHx
+#include "../../../../sh3/include/int_mwgwtypes.h"
+#else
+#error "unknown architecture"
+#endif
