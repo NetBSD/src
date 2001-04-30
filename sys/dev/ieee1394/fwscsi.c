75df65042bfb28ae6a555102aa758ac2f2902935
jmc <jmc@NetBSD.org>
2001-04-30 09:06:27 +0000

    Place holder until real fwscsi code is fleshed out.

---
+++ sys/dev/ieee1394/fwscsi.c
@@ -0,0 +1,80 @@
+/*	$NetBSD: fwscsi.c,v 1.1 2001/04/30 09:06:27 jmc Exp $	*/
+
+/*
+ * Copyright (c) 2000 The NetBSD Foundation, Inc.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to The NetBSD Foundation
+ * by James Chacon.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed by the NetBSD
+ *	Foundation, Inc. and its contributors.
+ * 4. Neither the name of The NetBSD Foundation nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <sys/param.h>
+#include <sys/types.h>
+#include <sys/kernel.h>
+#include <sys/device.h>
+#include <sys/systm.h>
+#include <sys/malloc.h>
+
+#include <machine/bus.h>
+
+#include <dev/std/ieee1212reg.h>
+#include <dev/std/sbp2reg.h>
+
+static int fwscsi_match(struct device *, struct cfdata *, void *);
+static void fwscsi_attach(struct device *, struct device *, void *);
+static int fwscsi_detach(struct device *, int);
+
+struct fwscsi_softc {
+    struct device sc_dev;
+};
+
+struct cfattach fwscsi_ca = {
+    sizeof(struct fwscsi_softc), fwscsi_match, fwscsi_attach, fwscsi_detach
+};
+
+/* Place holders for the moment. */
+
+static int
+fwscsi_match(struct device *parent, struct cfdata *match, void *aux)
+{
+    return 0;
+}
+
+static void
+fwscsi_attach(struct device *parent, struct device *self, void *aux)
+{
+}
+
+static int
+fwscsi_detach(struct device *self, int flags)
+{
+    return 0;
+}
