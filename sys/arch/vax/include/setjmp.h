b41d1dc44e66b8ff242ec1277f3a72c31841c31e
matt <matt@NetBSD.org>
2001-05-01 04:47:37 +0000

    Make the jmp_buf bigger under ELF so it can store a full context.

--- sys/arch/vax/include/setjmp.h
+++ sys/arch/vax/include/setjmp.h
@@ -1,7 +1,12 @@
-/*	$NetBSD: setjmp.h,v 1.2 1998/10/01 19:00:06 christos Exp $	*/
+/*	$NetBSD: setjmp.h,v 1.3 2001/05/01 04:47:37 matt Exp $	*/
 
 /*
  * machine/setjmp.h: machine dependent setjmp-related information.
  */
 
+#ifdef __ELF__
+#define	_JBLEN	24		/* size, in longs, of a jmp_buf */
+/* R0..R11,AP,FP,SP,PC  PSL,SSP,ESP + 5 extra */
+#else
 #define	_JBLEN	14		/* size, in longs, of a jmp_buf */
+#endif
