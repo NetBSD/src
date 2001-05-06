d690f9a3960f2960eaf1d4c0b01bd4c9cc6abec9
he <he@NetBSD.org>
2001-05-06 15:08:02 +0000

    Pull up revision 1.123 (requested by he):   Add new powerhook argument values, PWR_SOFTSUSPEND, PWR_SOFTSTANDBY and   PWR_SOFTRESUME.  Apm calls powerhook with the values in normal interrupt   priority level while others are protected with splhigh().

--- sys/sys/systm.h
+++ sys/sys/systm.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: systm.h,v 1.111.2.2 2000/07/14 18:10:51 thorpej Exp $	*/
+/*	$NetBSD: systm.h,v 1.111.2.3 2001/05/06 15:08:02 he Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1988, 1991, 1993
@@ -265,9 +265,12 @@ void	doshutdownhooks __P((void));
 void	*powerhook_establish __P((void (*)(int, void *), void *));
 void	powerhook_disestablish __P((void *));
 void	dopowerhooks __P((int));
-#define PWR_RESUME 0
-#define PWR_SUSPEND 1
-#define PWR_STANDBY 2
+#define PWR_RESUME	0
+#define PWR_SUSPEND	1
+#define PWR_STANDBY	2
+#define PWR_SOFTRESUME	3
+#define PWR_SOFTSUSPEND	4
+#define PWR_SOFTSTANDBY	5
 
 /*
  * Mountroot hooks.  Device drivers establish these to be executed
