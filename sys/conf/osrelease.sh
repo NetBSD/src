0a4b47fc171f29d187625b33be18f5f005d5fdf8
he <he@NetBSD.org>
2001-05-09 22:50:18 +0000

    Apply patch (requested by he):   We are now at NetBSD 1.5.1_BETA2.

--- sys/conf/osrelease.sh
+++ sys/conf/osrelease.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-#	$NetBSD: osrelease.sh,v 1.46.2.7 2001/04/06 01:57:40 he Exp $
+#	$NetBSD: osrelease.sh,v 1.46.2.8 2001/05/09 22:50:18 he Exp $
 #
 # Copyright (c) 1997 The NetBSD Foundation, Inc.
 # All rights reserved.
@@ -39,7 +39,7 @@
 
 # Release number to use
 # Also check __NetBSD_Version__ in sys/sys/param.h if you change this!
-release=1.5.1_BETA
+release=1.5.1_BETA2
 
 
 case $1 in
