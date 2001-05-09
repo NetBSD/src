56f877468c9b6db5665fcadf506f1dde2717fe0f
he <he@NetBSD.org>
2001-05-09 20:41:31 +0000

    Pull up revision 1.13 (requested by kleink):   Restore PSR ICC bits correctly from sigcontext.

--- sys/arch/sparc64/sparc64/netbsd32_machdep.c
+++ sys/arch/sparc64/sparc64/netbsd32_machdep.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: netbsd32_machdep.c,v 1.5.4.3 2000/10/18 16:31:32 tv Exp $	*/
+/*	$NetBSD: netbsd32_machdep.c,v 1.5.4.4 2001/05/09 20:41:31 he Exp $	*/
 
 /*
  * Copyright (c) 1998 Matthew R. Green
@@ -409,7 +409,7 @@ netbsd32___sigreturn14(p, v, retval)
 		return (EINVAL);
 #endif
 	/* take only psr ICC field */
-	tf->tf_tstate = (int64_t)(tf->tf_tstate & ~TSTATE_CCR) | (scp->sc_tstate & TSTATE_CCR);
+	tf->tf_tstate = (int64_t)(tf->tf_tstate & ~TSTATE_CCR) | PSRCC_TO_TSTATE(sc.sc_tstate);
 	tf->tf_pc = (int64_t)scp->sc_pc;
 	tf->tf_npc = (int64_t)scp->sc_npc;
 	tf->tf_global[1] = (int64_t)scp->sc_g1;
