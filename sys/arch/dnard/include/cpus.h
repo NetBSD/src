2722e2a442b94359a8fbcad323848dbb27759287
matt <matt@NetBSD.org>
2001-05-09 15:40:54 +0000

    Populate dnard/include

---
+++ sys/arch/dnard/include/cpus.h
@@ -0,0 +1,4 @@
+/* $NetBSD: cpus.h,v 1.1 2001/05/09 15:40:55 matt Exp $ */
+
+#include <arm/cpus.h>
+
---
+++ sys/arch/dnard/include/psl.h
@@ -0,0 +1,4 @@
+/* $NetBSD: psl.h,v 1.1 2001/05/09 15:40:56 matt Exp $ */
+
+#include <arm/arm32/psl.h>
+
