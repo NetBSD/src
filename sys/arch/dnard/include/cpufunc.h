54ee04c483258fefc8063f5d3d0ec3fe9d573d69
matt <matt@NetBSD.org>
2001-05-09 15:40:54 +0000

    Populate dnard/include

---
+++ sys/arch/dnard/include/cpufunc.h
@@ -0,0 +1,4 @@
+/* $NetBSD: cpufunc.h,v 1.1 2001/05/09 15:40:55 matt Exp $ */
+
+#include <arm/cpufunc.h>
+
---
+++ sys/arch/dnard/include/ofw.h
@@ -0,0 +1,3 @@
+/* $NetBSD: ofw.h,v 1.1 2001/05/09 15:40:56 matt Exp $ */
+
+#include <arm/ofw.h>
