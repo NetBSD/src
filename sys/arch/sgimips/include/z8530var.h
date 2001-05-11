87f3aebd5e75139d63af7ba06b6ad83dd4497939
thorpej <thorpej@NetBSD.org>
2001-05-11 04:26:17 +0000

    New data structures for the new SCC driver.
    From Rafal K. Boni.

--- sys/arch/sgimips/include/z8530var.h
+++ sys/arch/sgimips/include/z8530var.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: z8530var.h,v 1.1 2000/06/14 15:40:02 soren Exp $	*/
+/*	$NetBSD: z8530var.h,v 1.2 2001/05/11 04:26:17 thorpej Exp $	*/
 
 /*
  * Copyright (c) 1994 Gordon W. Ross
@@ -41,31 +41,41 @@
  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
+ *
+ *	@(#)zsvar.h	8.1 (Berkeley) 6/11/93
  */
 
 #include <machine/bus.h>
 #include <dev/ic/z8530sc.h>
 
+struct zs_channel {
+	struct zs_chanstate	cs_zscs; 	/* Required: soft state */
+	bus_space_tag_t		cs_bustag;	/* Machine-dependent */
+	bus_space_handle_t	cs_regs;
+};	
+
 struct zsc_softc {
-	struct device zsc_dev;
-	bus_space_tag_t zsc_st;
-	bus_space_handle_t zsc_sh;
-	struct zs_chanstate *zsc_cs[2];	/* channel A and B soft state */
+	struct	device		zsc_dev;	/* required: base device */
+	struct	zs_chanstate   *zsc_cs[2];	/* channel soft state */
+	struct  zs_channel	zsc_cs_store[2];
 	/* Machine-dependent part follows... */
-	struct zs_chanstate zsc_cs_store[2];
+	bus_space_tag_t		zsc_bustag;	/* Bus type */
+	bus_space_handle_t	zsc_base; 	/* Device base address */
+	void		       *sc_si;		/* Softinterrupt handle */
 };
 
 /*
  * Functions to read and write individual registers in a channel.
- * The ZS chip requires a 1.6 uSec. recovery time between accesses.
- * The delay is now handled inside the chip access functions.
- * These could be inlines, but with the delay, speed is moot.
+ * The SCC chip requires 3-4 PCLK cycles recovery time between accesses
  */
 
-u_int8_t zs_read_reg(struct zs_chanstate *cs, bus_size_t reg);
-u_int8_t zs_read_csr(struct zs_chanstate *cs);
-u_int8_t zs_read_data(struct zs_chanstate *cs);
+u_char zs_read_reg __P((struct zs_chanstate *cs, u_char reg));
+u_char zs_read_csr __P((struct zs_chanstate *cs));
+u_char zs_read_data __P((struct zs_chanstate *cs));
 
-void zs_write_reg(struct zs_chanstate *cs, bus_size_t reg, u_int8_t val);
-void zs_write_csr(struct zs_chanstate *cs, u_int8_t val);
-void zs_write_data(struct zs_chanstate *cs, u_int8_t val);
+void  zs_write_reg __P((struct zs_chanstate *cs, u_char reg, u_char val));
+void  zs_write_csr __P((struct zs_chanstate *cs, u_char val));
+void  zs_write_data __P((struct zs_chanstate *cs, u_char val));
+
+/* Zilog Serial hardware interrupts (level 0) */
+#define splzs()		spltty()
