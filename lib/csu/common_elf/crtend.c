a0736c067acb0856a31f7e726fa82cf2db450d01
ross <ross@NetBSD.org>
2001-05-11 22:44:14 +0000

    Reorganization of crtbegin/crtend. o  Primary mod: alpha and i386 code for .init and .fini section fallthroughs o  Actual fallthru is not yet enabled, see csu/{alpha,i386_elf}/Makefile o  When enabled, should fix bin/12865 & should fix at least one WINE bug,    see http://www.winehq.com/hypermail/wine-devel/2001/05/0021.html o  Also, removed the function pointer hack put in to prevent gcc from    incorrectly attempting to do short calls over section boundaries,    on some architectures, rather, simply simply move ctors() and dtors()    into .init and .fini. Other notes. o  Our crtbegin.c and crtend.c were completely in C, despite    the fact that .init and .fini section fallthroughs require machine code.    These modules implement the gnu ctors/dtors mechanism directly, eliminating    in part the need for the elf .init/.fini section fallthrough. However,    if we ever want to use the gnu-provided constructor support, we need to    do the fallthrough, at least one program (WINE) apparently uses the    feature independently, and other compilers may depend on the mechanism. o  It is amusing that the previous NetBSD _init() and _fini() were put    into the .init and .fini sections, because nothing is gained over    plain .text if they don't fall through. Cargo cult! o  Other elf ports will need to copy the alpha and i386 example mods.

--- lib/csu/common_elf/crtend.c
+++ lib/csu/common_elf/crtend.c
@@ -1,6 +1,7 @@
-/*	$NetBSD: crtend.c,v 1.6 1998/05/06 20:58:34 cgd Exp $	*/
+/*	$NetBSD: crtend.c,v 1.7 2001/05/11 22:44:15 ross Exp $	*/
 
 #include <sys/cdefs.h>
+#include "dot_init.h"
 
 static void (*__CTOR_LIST__[1]) __P((void))
     __attribute__((__unused__))
@@ -8,3 +9,7 @@ static void (*__CTOR_LIST__[1]) __P((void))
 static void (*__DTOR_LIST__[1]) __P((void))
     __attribute__((__unused__))
     __attribute__((section(".dtors"))) = { (void *)0 };		/* XXX */
+
+MD_INIT_SECTION_EPILOGUE;
+
+MD_FINI_SECTION_EPILOGUE;
