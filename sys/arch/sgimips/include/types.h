2a1745e03635018a5c2e14b23c41546a7df50a1c
thorpej <thorpej@NetBSD.org>
2001-05-11 04:42:41 +0000

    We now have generic soft interrupts.

--- sys/arch/sgimips/include/types.h
+++ sys/arch/sgimips/include/types.h
@@ -1,3 +1,5 @@
-/*	$NetBSD: types.h,v 1.1 2000/06/14 15:40:01 soren Exp $	*/
+/*	$NetBSD: types.h,v 1.2 2001/05/11 04:42:41 thorpej Exp $	*/
 
 #include <mips/types.h>
+
+#define	__HAVE_GENERIC_SOFT_INTERRUPTS
