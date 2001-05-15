03b97ed0c65f54b769cea27204204b1981d55d17
lukem <lukem@NetBSD.org>
2001-05-15 15:00:04 +0000

    delint: () protect some macro args

--- sys/dev/isa/pas.c
+++ sys/dev/isa/pas.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: pas.c,v 1.47 2000/04/28 08:51:42 jdolecek Exp $	*/
+/*	$NetBSD: pas.c,v 1.48 2001/05/15 15:00:04 lukem Exp $	*/
 
 /*
  * Copyright (c) 1991-1993 Regents of the University of California.
@@ -166,8 +166,8 @@ static struct audio_device pas_device = {
 };
 
 /*XXX assume default I/O base address */
-#define pasread(p) inb(p)
-#define paswrite(d, p) outb(p, d)
+#define pasread(p) inb((p))
+#define paswrite(d, p) outb((p), (d))
 
 void
 pasconf(model, sbbase, sbirq, sbdrq)
