67c467b3dacbf946b70fdf41bb175593f7b11166
itojun <itojun@NetBSD.org>
2001-05-17 20:45:58 +0000

    BIND 8.2.4

---
+++ dist/bind/lib/dnssafe/binfocsh.h
@@ -0,0 +1,35 @@
+/*	$NetBSD: binfocsh.h,v 1.1.1.1 2001/05/17 20:47:11 itojun Exp $	*/
+
+/* Copyright (C) RSA Data Security, Inc. created 1993, 1996.  This is an
+   unpublished work protected as such under copyright law.  This work
+   contains proprietary, confidential, and trade secret information of
+   RSA Data Security, Inc.  Use, disclosure or reproduction without the
+   express written authorization of RSA Data Security, Inc. is
+   prohibited.
+ */
+
+#ifndef _BINFOCSH_H_
+#define _BINFOCSH_H_ 1
+
+#include "bmempool.h"
+
+typedef struct B_InfoCache {
+  B_MemoryPool memoryPool;                                      /* inherited */
+  struct {
+    unsigned int infoCount;
+    struct {
+      POINTER infoType;
+      POINTER info;
+    } *infos;
+    /* POINTER reserved; */
+  } z;           /* z gives the members that are zeroized by the constructor */
+} B_InfoCache;
+
+void B_InfoCacheConstructor PROTO_LIST ((B_InfoCache *));
+#define B_INFO_CACHE_Destructor(infoCache) \
+  B_MemoryPoolDestructor (&(infoCache)->memoryPool)
+
+int B_InfoCacheAddInfo PROTO_LIST ((B_InfoCache *, POINTER, POINTER));
+int B_InfoCacheFindInfo PROTO_LIST ((B_InfoCache *, POINTER *, POINTER));
+
+#endif
