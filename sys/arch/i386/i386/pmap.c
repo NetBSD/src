ec5ca9e70d1ca1c1569ad753f0451e2933e214a9
sommerfeld <sommerfeld@NetBSD.org>
2001-05-23 04:25:35 +0000

    In kern/12554 workaround in pmap_page_remove, don't forget to unmap (and unlock) the other pmap's pte's before continuing to the next pmap.  Avoids pmap lock leaks (generates LOCKDEBUG warning, deadlocks MULTIPROCESSOR).

--- sys/arch/i386/i386/pmap.c
+++ sys/arch/i386/i386/pmap.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: pmap.c,v 1.123 2001/04/29 04:42:04 thorpej Exp $	*/
+/*	$NetBSD: pmap.c,v 1.124 2001/05/23 04:25:35 sommerfeld Exp $	*/
 
 /*
  *
@@ -2352,6 +2352,7 @@ pmap_page_remove(pg)
 			    VM_PAGE_TO_PHYS(pg), pg->wire_count);
 #endif
 			prevptr = &pve->pv_next;
+			pmap_unmap_ptes(pve->pv_pmap);	/* unlocks pmap */
 			continue;
 		}
 #endif /* kern/12554 */
