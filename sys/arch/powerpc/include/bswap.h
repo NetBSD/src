574400daa551fc57c49c3d6e2380d038bfefa7c0
tsubai <tsubai@NetBSD.org>
2001-05-30 13:08:34 +0000

    Inline bswap{16,32} when the argument is constant.

--- sys/arch/powerpc/include/bswap.h
+++ sys/arch/powerpc/include/bswap.h
@@ -1,8 +1,14 @@
-/*      $NetBSD: bswap.h,v 1.2 1999/08/21 05:39:54 simonb Exp $      */
+/*	$NetBSD: bswap.h,v 1.3 2001/05/30 13:08:34 tsubai Exp $	*/
 
-#ifndef _MACHINE_BSWAP_H_
-#define	_MACHINE_BSWAP_H_
+#ifndef _POWERPC_BSWAP_H_
+#define _POWERPC_BSWAP_H_
 
 #include <sys/bswap.h>
 
-#endif /* !_MACHINE_BSWAP_H_ */
+#ifdef __GNUC__
+#include <powerpc/byte_swap.h>
+#define bswap16(x) __bswap16(x)
+#define bswap32(x) __bswap32(x)
+#endif
+
+#endif /* _POWERPC_BSWAP_H_ */
