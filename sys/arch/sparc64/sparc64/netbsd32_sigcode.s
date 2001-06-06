73f9db4c08ba96c26e845b98d0daa343979c2bf6
mrg <mrg@NetBSD.org>
2001-06-06 21:19:48 +0000

    extract the netbsd32, sunos, 32 bit svr4 and 64 bit svr4 sigcode out into separate files, so that compat lkms can link with them as well.

---
+++ sys/arch/sparc64/sparc64/netbsd32_sigcode.s
@@ -0,0 +1,41 @@
+/*	$NetBSD: netbsd32_sigcode.s,v 1.1 2001/06/06 21:19:50 mrg Exp $	*/
+
+/*
+ * Copyright (c) 2001 Matthew R. Green
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <machine/frame.h>
+#include <machine/trap.h>
+#include <machine/fsr.h>
+#include <compat/netbsd32/netbsd32_syscall.h>
+
+#define SIGCODE_NAME           netbsd32_sigcode
+#define ESIGCODE_NAME          netbsd32_esigcode
+#define SIGRETURN_NAME         netbsd32_SYS_netbsd32___sigreturn14
+#define EXIT_NAME              netbsd32_SYS_netbsd32_exit
+
+#include "sigcode32.s"
