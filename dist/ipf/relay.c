ebf6ffc33b385072f72fc7293fa43538dd6c9360
mrg <mrg@NetBSD.org>
2001-06-07 14:15:39 +0000

    make this program actually work.

--- dist/ipf/relay.c
+++ dist/ipf/relay.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: relay.c,v 1.2 2000/05/03 11:40:17 veego Exp $	*/
+/*	$NetBSD: relay.c,v 1.3 2001/06/07 14:15:39 mrg Exp $	*/
 
 /*
  * Sample program to be used as a transparent proxy.
@@ -109,13 +109,16 @@ char *argv[];
 	int	fd, sl = sizeof(sl), se;
 
 	openlog(argv[0], LOG_PID|LOG_NDELAY, LOG_DAEMON);
-	if ((fd = open("/dev/ipl", O_RDONLY)) == -1) {
+	if ((fd = open("/dev/ipnat", O_RDONLY)) == -1) {
 		se = errno;
 		perror("open");
 		errno = se;
 		syslog(LOG_ERR, "open: %m\n");
 		exit(-1);
 	}
+
+	bzero(&nl, sizeof(nl);
+	nl.nl_flags = IPN_TCP;
 
 	bzero(&sin, sizeof(sin));
 	sin.sin_family = AF_INET;
@@ -153,8 +156,8 @@ char *argv[];
 		exit(-1);
 	}
 
-	sin.sin_port = nl.nl_inport;
-	sin.sin_addr = nl.nl_inip;
+	sin.sin_port = nl.nl_realport;
+	sin.sin_addr = nl.nl_realip;
 	sl = sizeof(sin);
 
 	fd = socket(AF_INET, SOCK_STREAM, 0);
