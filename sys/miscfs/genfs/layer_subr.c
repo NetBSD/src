4c66738cc5f10015751ed3af2fb6833d2164d7ff
wiz <wiz>
2001-06-07 13:32:46 +0000

    Typos in comments (misc/13133 by Michael K. Sanders)

--- sys/miscfs/genfs/layer_subr.c
+++ sys/miscfs/genfs/layer_subr.c
@@ -1,11 +1,11 @@
-/*	$NetBSD: layer_subr.c,v 1.7 2000/11/27 08:39:45 chs Exp $	*/
+/*	$NetBSD: layer_subr.c,v 1.8 2001/06/07 13:32:47 wiz Exp $	*/
 
 /*
  * Copyright (c) 1999 National Aeronautics & Space Administration
  * All rights reserved.
  *
  * This software was written by William Studenmund of the
- * Numerical Aerospace Similation Facility, NASA Ames Research Center.
+ * Numerical Aerospace Simulation Facility, NASA Ames Research Center.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
