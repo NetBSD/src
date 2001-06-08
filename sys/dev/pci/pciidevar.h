e5bd00e48d1d3737d03d48b3a491814369f8e4cc
simonb <simonb@NetBSD.org>
2001-06-08 04:48:54 +0000

    For ports that wire up pciide in compatibility mode, have them define __HAVE_PCIIDE_MACHDEP_COMPAT_INTR_ESTABLISH in pci_machdep.h and pciide_map_compat_intr() only calls pciide_machdep_compat_intr_establish() if that preprocessor define exists.
    Ports that don't need to do this no longer need to supply a
    dummy function.

--- sys/dev/pci/pciidevar.h
+++ sys/dev/pci/pciidevar.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: pciidevar.h,v 1.6 2001/01/12 16:04:00 bouyer Exp $	*/
+/*	$NetBSD: pciidevar.h,v 1.7 2001/06/08 04:48:58 simonb Exp $	*/
 
 /*
  * Copyright (c) 1998 Christopher G. Demetriou.  All rights reserved.
@@ -83,5 +83,7 @@ struct pciide_softc {
  */
 
 /* Attach compat interrupt handler, returning handle or NULL if failed. */
+#ifdef __HAVE_PCIIDE_MACHDEP_COMPAT_INTR_ESTABLISH
 void	*pciide_machdep_compat_intr_establish __P((struct device *,
 	    struct pci_attach_args *, int, int (*)(void *), void *));
+#endif
