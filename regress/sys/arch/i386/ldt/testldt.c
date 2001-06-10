3c570168cae0857262012d126f741c8a38d8dd28
sommerfeld <sommerfeld@NetBSD.org>
2001-06-10 16:27:21 +0000

    Test setting of multiple ldt entries in one syscall. While we're here, it's code cleanup time:  - KNF  - WARNS=2  - add header files for prototypes which matter.  - remove most magic constants in favor of segments.h bitfields  - delinuxify  - use lcall instead of a bunch of .byte's for the gate invocation.  - take a first stab at linting.

--- regress/sys/arch/i386/ldt/testldt.c
+++ regress/sys/arch/i386/ldt/testldt.c
@@ -1,193 +1,221 @@
-/*	$NetBSD: testldt.c,v 1.5 1997/11/01 06:20:50 lukem Exp $	*/
+/*	$NetBSD: testldt.c,v 1.6 2001/06/10 16:27:21 sommerfeld Exp $	*/
 
-#include <stdio.h>
+#include <err.h>
 #include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
 #include <sys/types.h>
 #include <sys/mman.h>
 #include <machine/segments.h>
+#include <machine/sysarch.h>
 
-extern int i386_get_ldt(int, union descriptor *, int);
-extern int i386_set_ldt(int, union descriptor *, int);
-
-int verbose = 0;
-struct sigaction segv_act;
-
-inline void set_fs(unsigned long val)
+static void
+set_fs(unsigned int val)
 {
 	__asm__ __volatile__("mov %0,%%fs"::"r" ((unsigned short) val));
 }
 
-inline unsigned char get_fs_byte(const char * addr)
+static unsigned char
+get_fs_byte(const char *addr)
 {
-	unsigned register char _v;
+	unsigned char _v;
 
 	__asm__ ("movb %%fs:%1,%0":"=q" (_v):"m" (*addr));
 	return _v;
 }
 
-inline unsigned short get_cs(void)
+static unsigned short
+get_cs(void)
 {
-	unsigned register short _v;
+	unsigned short _v;
 
-	__asm__ ("movw %%cs,%0"::"r" ((unsigned short) _v));
+	__asm__ ("movw %%cs,%0": "=r" ((unsigned short) _v));
 	return _v;
 }
 
-int
-check_desc(unsigned int desc)
+static int
+check_desc(int desc)
 {
-	desc = LSEL(desc, SEL_UPL);
-	set_fs(desc);
+	unsigned int sel = LSEL(desc, SEL_UPL);
+	set_fs(sel);
 	return(get_fs_byte((char *) 0));
 }
 
-void
-gated_call()
+static void
+gated_call(void)
 {
 	printf("Called from call gate...");
 	__asm__ __volatile__("popl %ebp");
 	__asm__ __volatile__(".byte 0xcb");
 }
 
-struct segment_descriptor *
-make_sd(unsigned base, unsigned limit, int type, int dpl, int seg32, int inpgs)
+static union descriptor *
+make_sd(void *basep, unsigned limit, int type, int dpl, int seg32, int inpgs)
 {
-	static struct segment_descriptor d;
+	static union descriptor d;
+	unsigned long base = (unsigned long)basep;
 
-	d.sd_lolimit = limit & 0x0000ffff;
-	d.sd_lobase  = base & 0x00ffffff;
-	d.sd_type    = type & 0x01f;
-	d.sd_dpl     = dpl & 0x3;
-	d.sd_p	     = 1;
-	d.sd_hilimit = (limit & 0x00ff0000) >> 16;
-	d.sd_xx	     = 0;
-	d.sd_def32   = seg32?1:0;
-	d.sd_gran    = inpgs?1:0;
-	d.sd_hibase  = (base & 0xff000000) >> 24;
+	d.sd.sd_lolimit = limit & 0x0000ffff;
+	d.sd.sd_lobase  = base & 0x00ffffff;
+	d.sd.sd_type    = type & 0x01f;
+	d.sd.sd_dpl     = dpl & 0x3;
+	d.sd.sd_p	     = 1;
+	d.sd.sd_hilimit = (limit & 0x00ff0000) >> 16;
+	d.sd.sd_xx	     = 0;
+	d.sd.sd_def32   = seg32?1:0;
+	d.sd.sd_gran    = inpgs?1:0;
+	d.sd.sd_hibase  = (base & 0xff000000) >> 24;
 
 	return (&d);
 }
 
-struct gate_descriptor *
-make_gd(unsigned offset, unsigned int sel, unsigned stkcpy, int type, int dpl)
+static union descriptor *
+make_gd(void *func, unsigned int sel, unsigned stkcpy, int type, int dpl)
 {
-	static struct gate_descriptor d;
+	static union descriptor d;
+	unsigned long offset = (unsigned long)func;
 
-	d.gd_looffset = offset & 0x0000ffff;
-	d.gd_selector = sel & 0xffff;
-	d.gd_stkcpy   = stkcpy & 0x1ff;
-	d.gd_type     = type & 0x1ff;
-	d.gd_dpl      = dpl & 0x3;
-	d.gd_p	      = 1;
-	d.gd_hioffset = (offset & 0xffff0000) >> 16;
+	d.gd.gd_looffset = offset & 0x0000ffff;
+	d.gd.gd_selector = sel & 0xffff;
+	d.gd.gd_stkcpy   = stkcpy & 0x1ff;
+	d.gd.gd_type     = type & 0x1ff;
+	d.gd.gd_dpl      = dpl & 0x3;
+	d.gd.gd_p	      = 1;
+	d.gd.gd_hioffset = (offset & 0xffff0000) >> 16;
 
 	return(&d);
 }
 
-void
+static const char *
+seg_type_name[] = {
+	"SYSNULL", "SYS286TSS", "SYSLDT", "SYS286BSY",
+	"SYS286CGT", "SYSTASKGT", "SYS286IGT", "SYS286TGT",
+	"SYSNULL2", "SYS386TSS", "SYSNULL3", "SYS386BSY",
+	"SYS386CGT", "SYSNULL4", "SYS386IGT", "SYS386TGT",
+	"MEMRO", "MEMROA", "MEMRW", "MEMRWA",
+	"MEMROD", "MEMRODA", "MEMRWD", "MEMRWDA",
+	"MEME", "MEMEA", "MEMER", "MEMERA",
+	"MEMEC", "MEMEAC", "MEMERC", "MEMERAC"
+};
+
+static const char *
+segtype(unsigned int type)
+{
+	if (type >= 32)
+		return "Out of range";
+	
+	return seg_type_name[type];
+}
+	
+
+static void
 print_ldt(union descriptor *dp)
 {
-	unsigned long base_addr, limit, offset, selector, stack_copy;
-	int type, dpl, i;
-	unsigned long *lp = (unsigned long *)dp;
+	unsigned long base_addr, limit, offset, stack_copy;
+	unsigned int type, dpl;
+	unsigned long lp[2];
+
+	(void) memcpy(lp, dp, sizeof(lp));
     
-	/* First 32 bits of descriptor */
-	selector = base_addr = (*lp >> 16) & 0x0000FFFF;
-	offset = limit = *lp & 0x0000FFFF;
-	lp++;
+	base_addr = dp->sd.sd_lobase | (dp->sd.sd_hibase << 24);
+	limit = dp->sd.sd_lolimit | (dp->sd.sd_hilimit << 16);
+	offset = dp->gd.gd_looffset | (dp->gd.gd_hioffset << 16);
 	
-	/* First 32 bits of descriptor */
-	base_addr |= (*lp & 0xFF000000) | ((*lp << 16) & 0x00FF0000);
-	limit |= (*lp & 0x000F0000);
 	type = dp->sd.sd_type;
 	dpl = dp->sd.sd_dpl;
 	stack_copy = dp->gd.gd_stkcpy;
-	offset |= (*lp >> 16) & 0x0000FFFF;
-    
+
 	if (type == SDT_SYS386CGT || type == SDT_SYS286CGT)
-		printf("LDT: Gate Off %08.8x, Sel   %05.5x, Stkcpy %d DPL %d, Type %d\n",
-			offset, selector, stack_copy, dpl, type);
+		printf("LDT: Gate Off %8.8lx, Sel   %5.5x, Stkcpy %lu DPL %d,"
+		    " Type %d/%s\n",
+		    offset, dp->gd.gd_selector, stack_copy, dpl,
+		    type, segtype(type));
 	else
-		printf("LDT: Seg Base %08.8x, Limit %05.5x, DPL %d, Type %d\n",
-			base_addr, limit, dpl, type);
+		printf("LDT: Seg Base %8.8lx, Limit %5.5lx, DPL %d, "
+		    "Type %d/%s\n",
+		    base_addr, limit, dpl,
+		    type, segtype(type));
 	printf("	  ");
-	if (*lp & 0x100)
+	if (type & 0x1)
 		printf("Accessed, ");
-	if (*lp & 8000)
+	if (dp->sd.sd_p)
 		printf("Present, ");
 	if (type != SDT_SYS386CGT && type != SDT_SYS286CGT) {
-		if (*lp & 0x100000)
+		if (type & 0x10)
 			printf("User, ");
-		if (*lp & 0x200000)
+		if (type & 0x08)
 			printf("X, ");
-		if (*lp & 0x400000)
-			printf("32, ");
+		if (dp->sd.sd_def32)
+			printf("32-bit, ");
 		else
-			printf("16, ");
-		if (*lp & 0x800000)
+			printf("16-bit, ");
+		if (dp->sd.sd_gran)
 			printf("page limit, ");
 		else
 			printf("byte limit, ");
 	}
 	printf("\n");
-	printf("	  %08.8x %08.8x\n", *(lp), *(lp-1));
+	printf("	  Raw descriptor: %08lx %08lx\n", lp[0], lp[1]);
 }
 
-static void busfault(int signal, int code, struct sigcontext *sc)
+/* ARGSUSED */
+static void
+busfault(int signo, int code, struct sigcontext *sc)
 {
-	fprintf(stderr, "\nbus fault - investigate.\n");
-	exit(1);
+	errx(1, "\nbus fault - investigate.\n");
 }
 
-static void usage(int status)
+static void
+usage(int status)
 {
-	fprintf(stderr, "Usage: testldt [-v]\n");
-        exit(status);
+	errx(status, "Usage: testldt [-v]\n");
 }
 
 #define MAX_USER_LDT 1024
+
+int
 main(int argc, char *argv[])
 {
 	union descriptor ldt[MAX_USER_LDT];
-	int num, n, ch;
+	int n, ch;
+	int num;
 	unsigned int cs = get_cs();
-	char *data;
-	struct segment_descriptor *sd;
-	struct gate_descriptor *gd;
-	
+	unsigned char *data;
+	union descriptor *sd, *gd;
+	unsigned char one = 1, two = 2, val;
+	struct sigaction segv_act;
+	int verbose = 0;
+
 	segv_act.sa_handler = (sig_t) busfault;
-	if (sigaction(SIGBUS, &segv_act, NULL) < 0) {
-		perror("sigaction");
-		exit(1);
-	}
+	if (sigaction(SIGBUS, &segv_act, NULL) < 0)
+		err(1, "sigaction");
 
 	while ((ch = getopt(argc, argv, "v")) != -1) {
 		switch (ch) {
 		case 'v':
-		    verbose++;
-		    break;
+			verbose++;
+			break;
 		default:
-		    usage(1);
-		    break;
+			usage(1);
+			break;
 		}
 	}
 
 	printf("Testing i386_get_ldt...\n");
-	if ((num = i386_get_ldt(0, ldt, MAX_USER_LDT)) < 0) {
-		perror("get_ldt");
-		exit(2);
-	}
-	if (num == 0) {
-	    fprintf(stderr, "ERROR: i386_get_ldt() return 0 default LDT entries.\n");
-	    exit(1);
-	}
+	if ((num = i386_get_ldt(0, ldt, MAX_USER_LDT)) < 0)
+		err(2, "get_ldt");
+
+	if (num == 0)
+		errx(1, "i386_get_ldt() returned empty initial LDT");
 
 	if (verbose) {
-	    printf("Got %d (default) LDTs\n", num);
-	    for (n=0; n < num; n++) {
-		printf("Entry %d: ", n);
-		print_ldt(&ldt[n]);
-	    }
+		printf("Got %d (initial) LDT entries\n", num);
+		for (n=0; n < num; n++) {
+			printf("Entry %d: ", n);
+			print_ldt(&ldt[n]);
+		}
 	}
 	
 	/*
@@ -195,73 +223,115 @@ main(int argc, char *argv[])
 	 */
 	printf("Testing i386_set_ldt...\n");
 	data = (void *) mmap( (char *)0x005f0000, 0x0fff,
-			     PROT_EXEC | PROT_READ | PROT_WRITE,
-			     MAP_FIXED | MAP_PRIVATE | MAP_ANON, -1, 0);
-	if (data == NULL) {
-		perror("mmap");
-		exit(1);
-	}
-	if (verbose) printf("data address: %8.8x\n", data);
+	    PROT_EXEC | PROT_READ | PROT_WRITE,
+	    MAP_FIXED | MAP_PRIVATE | MAP_ANON, -1, (off_t)0);
+	if (data == NULL)
+		err(1, "mmap");
+
+	if (verbose)
+		printf("data address: %p\n", data);
 
 	*data = 0x97;
 
 	/* Get the next free LDT and set it to the allocated data. */
-	sd = make_sd((unsigned)data, 4096, SDT_MEMRW, SEL_UPL, 0, 0);
-	if ((num = i386_set_ldt(6, (union descriptor *)sd, 1)) < 0) {
-		perror("set_ldt");
-		exit(1);
-	}
+	sd = make_sd(data, 4096, SDT_MEMRW, SEL_UPL, 0, 0);
+	if ((num = i386_set_ldt(6, sd, 1)) < 0)
+		err(1, "set_ldt");
+
 	if (verbose) printf("setldt returned: %d\n", num);
-	if ((n = i386_get_ldt(num, ldt, 1)) < 0) {
-		perror("get_ldt");
-		exit(1);
-	}
+	if ((n = i386_get_ldt(num, ldt, 1)) < 0)
+		err(1, "get_ldt");
+
 	if (verbose) {
 		printf("Entry %d: ", num);
 		print_ldt(&ldt[0]);
 	}
 
-	if (verbose) printf("Checking desc (should be 0x97): 0x%x\n", check_desc(num));
-	if (check_desc(num) != 0x97) {
-		fprintf(stderr, "ERROR: descriptor check failed: (should be 0x97): 0x%x\n", check_desc(num));
-		exit(1);
-	}
+	if (verbose)
+		printf("Checking desc (should be 0x97): 0x%x\n",
+		    check_desc(num));
+	if (check_desc(num) != 0x97)
+		errx(1, "ERROR: descriptor check failed; "
+		    "expected 0x97, got 0x%x\n", check_desc(num));
 	
 	/*
 	 * Test a Call Gate
 	 */
-	printf("Testing Call Gate...");
-	gd = make_gd((unsigned)gated_call, cs, 0, SDT_SYS386CGT, SEL_UPL);
-	if ((num = i386_set_ldt(5, (union descriptor *)gd, 1)) < 0) {
-		perror("set_ldt: call gate");
-		exit(1);
-	}
-	if (verbose) printf("setldt returned: %d\n", num);
-	if (verbose) printf("Call gate sel = 0x%x\n", LSEL(num, SEL_UPL));
-	if ((n = i386_get_ldt(num, ldt, 1)) < 0) {
-		perror("get_ldt");
-		exit(1);
-	}
-	if (verbose) printf("Entry %d: ", num);
-	if (verbose) print_ldt(&ldt[0]);
+	printf("Making Call Gate\n");
+	fflush(stdout);
+	
+	gd = make_gd((void *)gated_call, cs, 0, SDT_SYS386CGT, SEL_UPL);
+	if ((num = i386_set_ldt(5, gd, 1)) < 0)
+		err(1, "set_ldt: call gate");
 
-#if 0
-	err = setldt(5,
-		     gated_call,	/* Offset */
-		     0x0001,		/* This selector is for the executable segment descriptor.  It
-					   is the standard linux text descriptor. */
-		     0x00008c00);	/* Descriptor flags (you can't set all, the OS protects some) */
-	printf("setldt returned: %d\n", err);
-#endif
+	if (verbose) {
+		printf("setldt returned: %d\n", num);
+		printf("Call gate sel = 0x%x\n", LSEL(num, SEL_UPL));
+	}
 
-	__asm__ __volatile__(".byte 0x9a"); /* This is a call to a call gate. */
-	__asm__ __volatile__(".byte 0x00"); /* Value is ignored in a call gate but can be used. */
-	__asm__ __volatile__(".byte 0x00"); /* by the called procedure. */
-	__asm__ __volatile__(".byte 0x00");
-	__asm__ __volatile__(".byte 0x00");
-	__asm__ __volatile__(".byte 0x2f"); /* Selector 0x002f.	 This is index = 5 (the call gate), */
-	__asm__ __volatile__(".byte 0x00"); /* and a requestor priveledge level of 3. */
+	if ((n = i386_get_ldt(num, ldt, 1)) < 0)
+		err(1, "get_ldt");
+
+	if (verbose) {
+		printf("Entry %d: ", num);
+		print_ldt(&ldt[0]);
+	}
+
+	printf("Testing call gate...");
+	fflush(stdout);
+	/*
+	 * Long call to call gate.
+	 * Only the selector matters; offset is irrelevant.
+	 */
+	__asm__ __volatile__("lcall $0x2f,$0x0");
 
 	printf("Gated call returned\n");
-	exit (0);
+
+	/*
+	 * Test multiple sets.
+	 */
+
+	printf("Testing multiple descriptors at once.\n");
+
+	sd = (union descriptor *)malloc (sizeof(*sd) * 2);
+	if (sd == NULL)
+		err(1, "can't malloc");
+
+	sd[0] = *make_sd(&one, 1, SDT_MEMRO, SEL_UPL, 1, 0);
+	sd[1] = *make_sd(&two, 1, SDT_MEMRO, SEL_UPL, 1, 0);
+
+	if ((num = i386_set_ldt(6, (union descriptor *)sd, 2)) < 0)
+		err(1, "set_ldt");
+
+	if (verbose) printf("setldt returned: %d\n", num);
+	if ((n = i386_get_ldt(num, ldt, 2)) < 0)
+		err(1, "get_ldt");
+
+	if (verbose) {
+		printf("Entry %d: ", num);
+		print_ldt(&ldt[0]);
+		printf("Entry %d: ", num+1);
+		print_ldt(&ldt[1]);
+	}
+	val = check_desc(num);
+	printf("contents of segment ONE: %x\n", val);
+	if (val != 1) {
+		errx(1, "ONE has unexpected value %x", val);
+	}
+	val = check_desc(num+1);	
+	printf("contents of segment TWO: %x\n", val);
+	if (val != 2)
+		errx(1, "TWO has unexpected value %x", val);
+
+	if ((n = i386_get_ldt(num, ldt, 2)) < 0)
+		err(1, "get_ldt");		
+
+	if (verbose) {
+		printf("Entry %d: ", num);
+		print_ldt(&ldt[0]);
+		printf("Entry %d: ", num+1);
+		print_ldt(&ldt[1]);
+	}
+
+	return 0;
 }
