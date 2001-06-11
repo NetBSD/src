00b430e83bb1eb485bc10b8e114c67461dfe8721
rearnsha <rearnsha@NetBSD.org>
2001-06-11 11:56:57 +0000

    Split consinit functionality out of machdep.c into platform-specific files.  G/c include files for machdep.c
    Only include dev/cninit.c in ofw-based kernels.

---
+++ sys/arch/arm32/vidc/console/consinit.c
@@ -0,0 +1,101 @@
+/*	$NetBSD: consinit.c,v 1.1 2001/06/11 11:56:58 rearnsha Exp $ */
+
+/*
+ * Copyright (c) 1994-1998 Mark Brinicombe.
+ * Copyright (c) 1994 Brini.
+ * All rights reserved.
+ *
+ * This code is derived from software written for Brini by Mark Brinicombe
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed by Mark Brinicombe
+ *	for the NetBSD Project.
+ * 4. The name of the company nor the name of the author may be used to
+ *    endorse or promote products derived from this software without specific
+ *    prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * Console init functions for machines with VIDC
+ *
+ * Created      : 17/09/94
+ * Updated	: 18/04/01 updated for new wscons
+ */
+
+#include <sys/param.h>
+#include <sys/systm.h>
+#include <sys/kernel.h>
+#include <sys/conf.h>
+#include <sys/device.h>
+
+#include <machine/bus.h>
+
+#include <arm32/iomd/iomdreg.h>
+#include <arm32/dev/rpckbdvar.h>
+#include <machine/vidc.h>
+#include <arm32/vidc/vidcvideo.h>
+
+#include "vidcvideo.h"
+#include "rpckbd.h"
+
+#if ((NVIDCVIDEO>0) && (NRPCKBD>0))
+
+extern videomemory_t videomemory;
+extern struct bus_space iomd_bs_tag;
+extern struct rpckbd_softc console_kbd;
+
+#endif
+
+void
+consinit(void)
+{
+#if ((NVIDCVIDEO>0) && (NRPCKBD>0))
+	static struct rpckbd_softc *ksc = &console_kbd;
+#endif
+
+	static int consinit_called = 0;
+
+	if (consinit_called != 0)
+		return;
+	consinit_called = 1;
+
+#ifdef COMCONSOLE
+	comcninit(NULL);
+	return;
+#endif
+
+
+#if ((NVIDCVIDEO>0) && (NRPCKBD>0))
+	/* set up bus variables for attachment */
+	ksc->sc_iot	 = &iomd_bs_tag;
+	ksc->t_isconsole = 1;
+	ksc->data_port	 = IOMD_KBDDAT;
+	ksc->cmd_port	 = IOMD_KBDCR;
+	ksc->sc_enabled	 = 1;
+	bus_space_map(ksc->sc_iot, IOMD_KBDDAT, 8, 0, &(ksc->sc_ioh));
+
+	rpckbd_cnattach((struct device *) ksc);
+	vidcvideo_cnattach(videomemory.vidm_vbase);
+	return;
+#endif
+	panic("No console");	/* Will we ever see this?  */
+}
