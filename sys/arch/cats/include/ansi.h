9ad40921a8b30e8eb7b75826569d43f50d9415b5
matt <matt@NetBSD.org>
2001-06-12 17:37:35 +0000

    Eliminate all files that are just #include <arm.foo.h>.  Update Makefile to refer to cats, not netwinder.

--- sys/arch/cats/include/ansi.h
+++
@@ -1,3 +0,0 @@
-/*	$NetBSD: ansi.h,v 1.1 2001/06/08 22:22:59 chris Exp $	*/
-
-#include <arm/ansi.h>
--- sys/arch/cats/include/cpufunc.h
+++
@@ -1,4 +0,0 @@
-/* $NetBSD: cpufunc.h,v 1.1 2001/06/08 22:23:00 chris Exp $ */
-
-#include <arm/cpufunc.h>
-
