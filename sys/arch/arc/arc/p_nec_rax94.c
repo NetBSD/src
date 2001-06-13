3daeaff174983d67571a38fd379cec4fc2f573c4
soda <soda>
2001-06-13 15:36:04 +0000

    platform description for NEC RISCstation 2200 variants. not tested.

---
+++ sys/arch/arc/arc/p_nec_rax94.c
@@ -0,0 +1,49 @@
+/*	$NetBSD: p_nec_rax94.c,v 1.1 2001/06/13 15:36:04 soda Exp $	*/
+
+/*-
+ * Copyright (C) 2000 Shuichiro URATA.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <sys/param.h>
+
+#include <machine/autoconf.h>
+#include <machine/platform.h>
+
+#include <arc/jazz/jazziovar.h>
+
+struct platform platform_nec_rax94 = {
+	"NEC-RA'94",
+	"NEC W&S",
+	" (RA'94)",
+	"RISCstation 2200",
+	"NEC",
+	200, /* MHz */
+	c_nec_pci_mainbusdevs,
+	platform_generic_match,
+	c_nec_pci_init, /* XXX - not really confirmed */
+	c_nec_pci_cons_init, /* XXX - not really tested */
+	jazzio_reset,
+	c_nec_jazz_set_intr,
+};
