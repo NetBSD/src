3e4cca0a1afc4d53d351e16a81e66aef59cc288b
soda <soda@NetBSD.org>
2001-06-13 15:38:15 +0000

    remove __BROKEN_CONFIG_UNIT_USAGE, finally (sorry to be late, cgd).

--- sys/arch/arc/include/types.h
+++ sys/arch/arc/include/types.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: types.h,v 1.10 2001/06/13 14:36:33 soda Exp $	*/
+/*	$NetBSD: types.h,v 1.11 2001/06/13 15:38:15 soda Exp $	*/
 /*	$OpenBSD: types.h,v 1.2 1997/04/19 17:19:59 pefo Exp $	*/
 /*	NetBSD: types.h,v 1.10 1995/07/06 03:39:43 cgd Exp 	*/
 
@@ -8,5 +8,3 @@
 
 #define __HAVE_DEVICE_REGISTER
 #define __HAVE_NWSCONS
-
-#define __BROKEN_CONFIG_UNIT_USAGE
