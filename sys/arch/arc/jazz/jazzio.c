2db61c009db64c406733defd1283310e809274d8
soda <soda@NetBSD.org>
2001-06-13 15:11:38 +0000

    remove "cputype", introduce "struct jazzio_config *jazzio_conf;" to describe platform-dependent variants. clean up jazz i/o interreupt handling had device configuration information.

--- sys/arch/arc/jazz/jazzio.c
+++ sys/arch/arc/jazz/jazzio.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: jazzio.c,v 1.4 2001/04/30 04:52:53 tsutsui Exp $	*/
+/*	$NetBSD: jazzio.c,v 1.5 2001/06/13 15:11:38 soda Exp $	*/
 /*	$OpenBSD: picabus.c,v 1.11 1999/01/11 05:11:10 millert Exp $	*/
 /*	NetBSD: tc.c,v 1.2 1995/03/08 00:39:05 cgd Exp 	*/
 
@@ -32,24 +32,20 @@
 
 #include <sys/param.h>
 #include <sys/systm.h>
-#include <sys/proc.h>
-#include <sys/user.h>
 #include <sys/device.h>
 
-#include <uvm/uvm_extern.h>
-
 #include <machine/bus.h>
-#include <machine/intr.h>
-#include <machine/cpu.h>
 #include <machine/pio.h>
 #include <machine/autoconf.h>
+#include <machine/platform.h>
 
 #include <arc/jazz/jazziovar.h>
 #include <arc/jazz/pica.h>
-#include <arc/jazz/rd94.h>
-#include <arc/arc/arctype.h>
 #include <arc/jazz/jazzdmatlbreg.h>
 #include <arc/jazz/dma.h>
+#include <arc/jazz/pckbc_jazzioreg.h>
+
+void arc_sysreset __P((bus_addr_t, bus_size_t));
 
 struct jazzio_softc {
 	struct	device sc_dv;
@@ -70,170 +66,43 @@ extern struct cfdriver jazzio_cd;
 
 void	jazzio_intr_establish(int, int (*)(void *), void *);
 void	jazzio_intr_disestablish(int);
-int	pica_iointr(unsigned int, struct clockframe *);
-int	pica_clkintr(unsigned int, struct clockframe *);
-int	rd94_iointr(unsigned int, struct clockframe *);
-int	rd94_clkintr(unsigned int, struct clockframe *);
-
-intr_handler_t pica_clock_handler;
+int	jazzio_intr(unsigned, struct clockframe *);
+int	jazzio_no_handler __P((void *));
 
 /*
- *  Interrupt dispatch table.
+ *  Interrupt dispatch table for jazz i/o bus.
  */
-struct pica_int_desc int_table[] = {
-	{0, pica_intrnull, (void *)NULL, 0 },  /*  0 */
-	{0, pica_intrnull, (void *)NULL, 0 },  /*  1 */
-	{0, pica_intrnull, (void *)NULL, 0 },  /*  2 */
-	{0, pica_intrnull, (void *)NULL, 0 },  /*  3 */
-	{0, pica_intrnull, (void *)NULL, 0 },  /*  4 */
-	{0, pica_intrnull, (void *)NULL, 0 },  /*  5 */
-	{0, pica_intrnull, (void *)NULL, 0 },  /*  6 */
-	{0, pica_intrnull, (void *)NULL, 0 },  /*  7 */
-	{0, pica_intrnull, (void *)NULL, 0 },  /*  8 */
-	{0, pica_intrnull, (void *)NULL, 0 },  /*  9 */
-	{0, pica_intrnull, (void *)NULL, 0 },  /* 10 */
-	{0, pica_intrnull, (void *)NULL, 0 },  /* 11 */
-	{0, pica_intrnull, (void *)NULL, 0 },  /* 12 */
-	{0, pica_intrnull, (void *)NULL, 0 },  /* 13 */
-	{0, pica_intrnull, (void *)NULL, 0 },  /* 14 */
-	{0, pica_intrnull, (void *)NULL, 0 },  /* 15 */
+struct jazzio_intr_registry {
+	intr_handler_t	int_hand;	/* Interrupt handler */
+	void		*param;		/* Parameter to send to handler */
 };
 
-struct pica_dev {
-	struct confargs	ps_ca;
-	u_int		ps_mask;
-	intr_handler_t	ps_handler;
-	caddr_t		ps_base;
+struct jazzio_intr_registry jazzio_intrtab[] = {
+	{ jazzio_no_handler, (void *)NULL, },  /*  0 */
+	{ jazzio_no_handler, (void *)NULL, },  /*  1 */
+	{ jazzio_no_handler, (void *)NULL, },  /*  2 */
+	{ jazzio_no_handler, (void *)NULL, },  /*  3 */
+	{ jazzio_no_handler, (void *)NULL, },  /*  4 */
+	{ jazzio_no_handler, (void *)NULL, },  /*  5 */
+	{ jazzio_no_handler, (void *)NULL, },  /*  6 */
+	{ jazzio_no_handler, (void *)NULL, },  /*  7 */
+	{ jazzio_no_handler, (void *)NULL, },  /*  8 */
+	{ jazzio_no_handler, (void *)NULL, },  /*  9 */
+	{ jazzio_no_handler, (void *)NULL, },  /* 10 */
+	{ jazzio_no_handler, (void *)NULL, },  /* 11 */
+	{ jazzio_no_handler, (void *)NULL, },  /* 12 */
+	{ jazzio_no_handler, (void *)NULL, },  /* 13 */
+	{ jazzio_no_handler, (void *)NULL, },  /* 14 */
+	{ jazzio_no_handler, (void *)NULL, },  /* 15 */
 };
 
-struct pica_dev acer_pica_61_cpu[] = {
-	{{ "dallas_rtc",0, 0, },
-	   0,			 pica_intrnull, (void *)PICA_SYS_CLOCK, },
-	{{ "lpt",	1, 0, },
-	   PICA_SYS_LB_IE_PAR1,	 pica_intrnull, (void *)PICA_SYS_PAR1, },
-	{{ "fdc",	2, 0, },
-	   PICA_SYS_LB_IE_FLOPPY,pica_intrnull, (void *)PICA_SYS_FLOPPY, },
-	{{ NULL,	3, NULL, },
-	   0, pica_intrnull, (void *)NULL, },
-	{{ "vga",	4, NULL, },
-	   0, pica_intrnull, (void *)PICA_V_LOCAL_VIDEO, },
-	{{ "sonic",	5, 0, },
-	   PICA_SYS_LB_IE_SONIC, pica_intrnull, (void *)PICA_SYS_SONIC, },
-	{{ "asc",	6, 0, },
-	   PICA_SYS_LB_IE_SCSI,  pica_intrnull, (void *)PICA_SYS_SCSI, },
-	{{ "pckbd",	7, 0, },
-	   PICA_SYS_LB_IE_KBD,	 pica_intrnull, (void *)PICA_SYS_KBD, },
-	{{ "pms",	8, NULL, },
-	   PICA_SYS_LB_IE_MOUSE, pica_intrnull, (void *)PICA_SYS_KBD, },
-	{{ "com",	9, 0, },
-	   PICA_SYS_LB_IE_COM1,	 pica_intrnull, (void *)PICA_SYS_COM1, },
-	{{ "com",      10, 0, },
-	   PICA_SYS_LB_IE_COM2,	 pica_intrnull, (void *)PICA_SYS_COM2, },
-	{{ NULL,       -1, NULL, },
-	   0, NULL, (void *)NULL, },
-};
 
-struct pica_dev mips_magnum_r4000_cpu[] = {
-	{{ "dallas_rtc",0, 0, },
-	   0,			 pica_intrnull, (void *)PICA_SYS_CLOCK, },
-	{{ "lpt",	1, 0, },
-	   PICA_SYS_LB_IE_PAR1,	 pica_intrnull, (void *)PICA_SYS_PAR1, },
-	{{ "fdc",	2, 0, },
-	   PICA_SYS_LB_IE_FLOPPY,pica_intrnull, (void *)PICA_SYS_FLOPPY, },
-	{{ NULL,	3, NULL, },
-	   0, pica_intrnull, (void *)NULL, },
-	{{ "vxl",       4, 0, },
-	   PICA_SYS_LB_IE_VIDEO, pica_intrnull, (void *)PICA_V_LOCAL_VIDEO, },
-	{{ "sonic",	5, 0, },
-	   PICA_SYS_LB_IE_SONIC, pica_intrnull, (void *)PICA_SYS_SONIC, },
-	{{ "asc",	6, 0, },
-	   PICA_SYS_LB_IE_SCSI,  pica_intrnull, (void *)PICA_SYS_SCSI, },
-	{{ "pckbd",	7, 0, },
-	   PICA_SYS_LB_IE_KBD,	 pica_intrnull, (void *)PICA_SYS_KBD, },
-	{{ "pms",	8, NULL, },
-	   PICA_SYS_LB_IE_MOUSE, pica_intrnull, (void *)PICA_SYS_KBD, },
-	{{ "com",	9, 0, },
-	   PICA_SYS_LB_IE_COM1,	 pica_intrnull, (void *)PICA_SYS_COM1, },
-	{{ "com",      10, 0, },
-	   PICA_SYS_LB_IE_COM2,	 pica_intrnull, (void *)PICA_SYS_COM2, },
-	{{ NULL,       -1, NULL, },
-	   0, NULL, (void *)NULL, },
-};
-
-struct pica_dev nec_rd94_cpu[] = {
-	{{ "dallas_rtc",0, 0, },
-	   0,			 pica_intrnull,	(void *)RD94_SYS_CLOCK, },
-	{{ "lpt",	1, 0, },
-	   RD94_SYS_LB_IE_PAR1,  pica_intrnull,	(void *)RD94_SYS_PAR1, },
-	{{ "fdc",	2, 0, },
-	   RD94_SYS_LB_IE_FLOPPY,pica_intrnull,	(void *)RD94_SYS_FLOPPY, },
-	{{ NULL,	3, NULL, },
-	   0, pica_intrnull, (void *)NULL, },
-	{{ "sonic",	4, 0, },
-	   RD94_SYS_LB_IE_SONIC, pica_intrnull,	(void *)RD94_SYS_SONIC, },
-	{{ NULL,	5, NULL, },
-	   0, pica_intrnull, (void *)NULL, },
-	{{ NULL,	6, NULL, },
-	   0, pica_intrnull, (void *)NULL, },
-	{{ "pckbd",	7, 0, },
-	   RD94_SYS_LB_IE_KBD,	 pica_intrnull,	(void *)RD94_SYS_KBD, },
-	{{ "pms",	8, NULL, },
-	   RD94_SYS_LB_IE_MOUSE, pica_intrnull,	(void *)RD94_SYS_KBD, },
-	{{ "com",	9, 0, },
-	   RD94_SYS_LB_IE_COM1,	 pica_intrnull,	(void *)RD94_SYS_COM1, },
-	{{ "com",      10, 0, },
-	   RD94_SYS_LB_IE_COM2,	 pica_intrnull,	(void *)RD94_SYS_COM2, },
-	{{ NULL,       -1, NULL, },
-	   0, NULL, (void *)NULL, },
-};
-
-struct pica_dev nec_jc94_cpu[] = {
-	{{ "dallas_rtc",0, 0, },
-	   0,			 pica_intrnull,	(void *)RD94_SYS_CLOCK, },
-	{{ "lpt",	1, 0, },
-	   RD94_SYS_LB_IE_PAR1,  pica_intrnull,	(void *)RD94_SYS_PAR1, },
-	{{ "fdc",	2, 0, },
-	   RD94_SYS_LB_IE_FLOPPY,pica_intrnull,	(void *)RD94_SYS_FLOPPY, },
-	{{ NULL,	3, NULL, },
-	   0, pica_intrnull, (void *)NULL, },
-	{{ "sonic",	4, 0, },
-	   RD94_SYS_LB_IE_SONIC, pica_intrnull,	(void *)RD94_SYS_SONIC, },
-	{{ "osiop",	5, 0, },
-	   RD94_SYS_LB_IE_SCSI0, pica_intrnull, (void *)RD94_SYS_SCSI0, },
-	{{ "osiop",	6, 0, },
-	   RD94_SYS_LB_IE_SCSI1, pica_intrnull, (void *)RD94_SYS_SCSI1, },
-	{{ "pckbd",	7, 0, },
-	   RD94_SYS_LB_IE_KBD,	 pica_intrnull,	(void *)RD94_SYS_KBD, },
-	{{ "pms",	8, NULL, },
-	   RD94_SYS_LB_IE_MOUSE, pica_intrnull,	(void *)RD94_SYS_KBD, },
-	{{ "com",	9, 0, },
-	   RD94_SYS_LB_IE_COM1,	 pica_intrnull,	(void *)RD94_SYS_COM1, },
-	{{ "com",      10, 0, },
-	   RD94_SYS_LB_IE_COM2,	 pica_intrnull,	(void *)RD94_SYS_COM2, },
-	{{ NULL,       -1, NULL, },
-	   0, NULL, (void *)NULL, },
-};
-
-struct pica_dev *pica_cpu_devs[] = {
-        NULL,                   /* Unused */
-        acer_pica_61_cpu,       /* Acer PICA */
-	mips_magnum_r4000_cpu,	/* Mips MAGNUM R4000 */
-	nec_rd94_cpu,		/* NEC-R94 */
-	nec_rd94_cpu,		/* NEC-RA'94 */
-	nec_rd94_cpu,		/* NEC-RD94 */
-	nec_rd94_cpu,		/* NEC-R96 */
-	NULL,
-	NULL,
-	NULL,
-	NULL,
-	nec_jc94_cpu,		/* NEC-JC94 */
-};
-int npica_cpu_devs = sizeof pica_cpu_devs / sizeof pica_cpu_devs[0];
-
+struct jazzio_config *jazzio_conf = NULL;
+struct pica_dev *jazzio_devconfig = NULL;
 int jazzio_found = 0;
-int local_int_mask = 0;	/* Local interrupt enable mask */
+int jazzio_int_mask = 0;	/* jazz i/o interrupt enable mask */
 
-extern struct arc_bus_space	pica_bus;
+struct arc_bus_space jazzio_bus;
 
 int
 jazziomatch(parent, match, aux)
@@ -243,14 +112,9 @@ jazziomatch(parent, match, aux)
 {
 	struct confargs *ca = aux;
 
-        /* Make sure that we're looking for a PICA. */
+        /* Make sure that we're looking for a jazzio bus. */
         if (strcmp(ca->ca_name, jazzio_cd.cd_name) != 0)
                 return (0);
-
-        /* Make sure that unit exists. */
-	if (jazzio_found ||
-	    cputype > npica_cpu_devs || pica_cpu_devs[cputype] == NULL)
-		return (0);
 
 	return (1);
 }
@@ -265,27 +129,20 @@ jazzioattach(parent, self, aux)
 	struct jazzio_attach_args ja;
 	int i;
 
-	printf("\n");
+	if (jazzio_conf == NULL)
+		panic("jazzio_conf isn't initialized");
+	if (jazzio_devconfig == NULL)
+		panic("jazzio_devconfig isn't initialized");
 
 	jazzio_found = 1;
 
+	printf("\n");
+
 	/* keep our CPU device description handy */
-	sc->sc_devs = pica_cpu_devs[cputype];
+	sc->sc_devs = jazzio_devconfig;
 
 	/* set up interrupt handlers */
-	switch (cputype) {
-	case ACER_PICA_61:
-	case MAGNUM:
-		set_intr(MIPS_INT_MASK_1, pica_iointr, 2);
-		break;
-	case NEC_R94:
-	case NEC_RAx94:
-	case NEC_RD94:
-	case NEC_R96:
-	case NEC_JC94:
-		set_intr(MIPS_INT_MASK_1, rd94_iointr, 2);
-		break;
-	}
+	(*platform->set_intr)(MIPS_INT_MASK_1, jazzio_intr, 2);
 
 	sc->sc_bus.ab_dv = (struct device *)sc;
 
@@ -295,15 +152,12 @@ jazzioattach(parent, self, aux)
 	/* Create bus_dma_tag */
 	jazz_bus_dma_tag_init(&sc->sc_dmat);
 
-	/* Try to configure each PICA attached device */
-	for (i = 0; sc->sc_devs[i].ps_ca.ca_slot >= 0; i++) {
-
-		if (sc->sc_devs[i].ps_ca.ca_name == NULL)
-			continue; /* Empty slot */
+	/* Try to configure each jazzio attached device */
+	for (i = 0; sc->sc_devs[i].ps_ca.ca_name != NULL; i++) {
 
 		ja.ja_name = sc->sc_devs[i].ps_ca.ca_name;
 		ja.ja_bus = &sc->sc_bus;
-		ja.ja_bust = &pica_bus;
+		ja.ja_bust = &jazzio_bus;
 		ja.ja_dmat = &sc->sc_dmat;
 		ja.ja_addr = (bus_addr_t)sc->sc_devs[i].ps_base;
 		ja.ja_intr = sc->sc_devs[i].ps_ca.ca_slot;
@@ -321,154 +175,73 @@ jazzioprint(aux, pnp)
 {
 	struct jazzio_attach_args *ja = aux;
 
-        if (pnp)
-                printf("%s at %s", ja->ja_name, pnp);
-        printf(" addr 0x%lx intr %d", ja->ja_addr, ja->ja_intr);
-        return (UNCONF);
+	if (pnp)
+		printf("%s at %s", ja->ja_name, pnp);
+	printf(" addr 0x%lx", ja->ja_addr);
+	if (ja->ja_intr != -1)
+		printf(" intr %d", ja->ja_intr);
+	return (UNCONF);
 }
 
 void
-jazzio_intr_establish(slot, handler, val)
-	int slot;
+jazzio_intr_establish(intr, handler, val)
+	int intr;
 	intr_handler_t handler;
 	void *val;
 {
-	struct jazzio_softc *sc = jazzio_cd.cd_devs[0];
-
-	if (slot == 0) {	/* Slot 0 is special, clock */
-		pica_clock_handler = handler;
-		switch (cputype) {
-		case ACER_PICA_61:
-		case MAGNUM:
-			set_intr(MIPS_INT_MASK_4, pica_clkintr, 1);
-			break;
-		case NEC_R94:
-		case NEC_RAx94:
-		case NEC_RD94:
-		case NEC_R96:
-		case NEC_JC94:
-			set_intr(MIPS_INT_MASK_3, rd94_clkintr, 1);
-			break;
-		}
-	}
-
-	if (int_table[slot].int_mask != 0) {
-		panic("pica intr already set");
+	if (intr < 0 ||
+	    intr >= sizeof(jazzio_intrtab)/sizeof(jazzio_intrtab[0])) {
+		panic("jazzio intr %d out of range", intr);
+	} else if (jazzio_intrtab[intr].int_hand != jazzio_no_handler) {
+		panic("jazzio intr %d already set to %p", intr,
+		    jazzio_intrtab[intr].int_hand);
 	} else {
-		int_table[slot].int_mask = sc->sc_devs[slot].ps_mask;
-		local_int_mask |= int_table[slot].int_mask;
-		int_table[slot].int_hand = handler;
-		int_table[slot].param = val;
+		jazzio_int_mask |= 1 << intr;
+		jazzio_intrtab[intr].int_hand = handler;
+		jazzio_intrtab[intr].param = val;
 	}
 
-	switch (cputype) {
-	case ACER_PICA_61:
-	case MAGNUM:
-		out16(PICA_SYS_LB_IE, local_int_mask);
-		break;
-
-	case NEC_R94:
-	case NEC_R96:
-		out16(RD94_SYS_LB_IE2, local_int_mask);
-		break;
-
-	case NEC_RAx94:
-	case NEC_RD94:
-	case NEC_JC94:
-		/* XXX: I don't know why, but firmware does. */
-		if (in32(0xe0000560) != 0)
-			out16(RD94_SYS_LB_IE2, local_int_mask);
-		else
-			out16(RD94_SYS_LB_IE1, local_int_mask);
-		break;
-	}
+	(*jazzio_conf->jc_set_iointr_mask)(jazzio_int_mask);
 }
 
 void
-jazzio_intr_disestablish(slot)
-	int slot;
+jazzio_intr_disestablish(intr)
+	int intr;
 {
-	if (slot != 0)		 {	/* Slot 0 is special, clock */
-		local_int_mask &= ~int_table[slot].int_mask;
-		int_table[slot].int_mask = 0;
-		int_table[slot].int_hand = pica_intrnull;
-		int_table[slot].param = (void *)NULL;
-	}
+	jazzio_int_mask &= ~(1 << intr);
+	jazzio_intrtab[intr].int_hand = jazzio_no_handler;
+	jazzio_intrtab[intr].param = (void *)NULL;
+
+	(*jazzio_conf->jc_set_iointr_mask)(jazzio_int_mask);
 }
 
 int
-pica_intrnull(val)
-	void *val;
+jazzio_no_handler(arg)
+	void *arg;
 {
-	panic("uncaught PICA intr for slot %p", val);
-}
-
-/*
- *   Handle pica i/o interrupt.
- */
-int
-pica_iointr(mask, cf)
-	unsigned mask;
-	struct clockframe *cf;
-{
-	int vector;
-
-	while ((vector = inb(PVIS) >> 2) != 0) {
-		(*int_table[vector].int_hand)(int_table[vector].param);
-	}
-	return (~0);  /* Dont reenable */
+	panic("uncaught jazzio interrupt with arg %p", arg);
 }
 
 /*
- * Handle pica interval clock interrupt.
+ *   Handle jazz i/o interrupt.
  */
 int
-pica_clkintr(mask, cf)
+jazzio_intr(mask, cf)
 	unsigned mask;
 	struct clockframe *cf;
 {
-	int temp;
+	unsigned int vector;
+	struct jazzio_intr_registry *jirp;
 
-	temp = inw(R4030_SYS_IT_STAT);
-	(*pica_clock_handler)(cf);
-
-	/* Re-enable clock interrupts */
-	splx(MIPS_INT_MASK_4 | MIPS_SR_INT_IE);
-
-	return (~MIPS_INT_MASK_4); /* Keep clock interrupts enabled */
-}
-
-/*
- *   Handle NEC-RD94 i/o interrupt.
- */
-int
-rd94_iointr(mask, cf)
-	unsigned mask;
-	struct clockframe *cf;
-{
-	int vector;
-
-	while ((vector = inb(RD94_SYS_INTSTAT1) >> 2) != 0) {
-		(*int_table[vector].int_hand)(int_table[vector].param);
+	while ((vector = inb(jazzio_conf->jc_iointr_status_reg)) != 0) {
+		jirp = &jazzio_intrtab[(vector >> 2) - 1];
+		(*jirp->int_hand)(jirp->param);
 	}
 	return (~0);  /* Dont reenable */
 }
 
-/*
- * Handle NEC-RD94 interval clock interrupt.
- */
-int
-rd94_clkintr(mask, cf)
-	unsigned mask;
-	struct clockframe *cf;
+void
+jazzio_reset()
 {
-	int temp;
-
-	temp = in32(RD94_SYS_INTSTAT3);
-	(*pica_clock_handler)(cf);
-
-	/* Re-enable clock interrupts */
-	splx(MIPS_INT_MASK_3 | MIPS_SR_INT_IE);
-
-	return (~MIPS_INT_MASK_3); /* Keep clock interrupts enabled */
+	arc_sysreset(PICA_SYS_KBD, JAZZIO_KBCMDP);
 }
--- sys/arch/arc/jazz/jazziovar.h
+++ sys/arch/arc/jazz/jazziovar.h
@@ -1,3 +1,31 @@
+/*	$NetBSD: jazziovar.h,v 1.2 2001/06/13 15:11:38 soda Exp $	*/
+
+/*-
+ * Copyright (C) 2000 Shuichiro URATA.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #ifndef __JAZZIOVAR_H
 #define __JAZZIOVAR_H
 
@@ -13,7 +41,25 @@ struct jazzio_attach_args {
 	int		ja_dma;
 };
 
+struct pica_dev {
+	struct confargs	ps_ca;
+	caddr_t		ps_base;
+};
+
+struct jazzio_config {
+	int jc_iointr_status_reg;
+	void (*jc_set_iointr_mask)(int);
+	bus_addr_t jc_dmatlbreg;
+	bus_addr_t jc_fdcdmareg;	/* XXX should be obtained from bios */
+};
+
 void	jazzio_intr_establish(int, int (*)(void *), void *);
 void	jazzio_intr_disestablish(int);
+void	jazzio_reset __P((void));
+
+extern struct jazzio_config *jazzio_conf;
+extern struct pica_dev *jazzio_devconfig;
+
+extern struct arc_bus_space jazzio_bus;
 
 #endif
