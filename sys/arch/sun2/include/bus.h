3e792c8a428c920acde4caae9621ecfb6452a465
fredette <fredette@NetBSD.org>
2001-06-14 13:12:51 +0000

    Now simply include the real header under arch/sun68k.

---
+++ sys/arch/sun2/include/bus.h
@@ -0,0 +1,3 @@
+/*	$NetBSD: bus.h,v 1.3.8.2 2001/06/14 13:16:04 fredette Exp $	*/
+
+#include <sun68k/bus.h>
---
+++ sys/arch/sun2/include/idprom.h
@@ -0,0 +1,3 @@
+/*	$NetBSD: idprom.h,v 1.2.8.2 2001/06/14 13:12:51 fredette Exp $	*/
+
+#include <sun68k/idprom.h>
