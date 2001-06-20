778726071c691b7c8298ba086907ebc714a163cf
nonaka <nonaka>
2001-06-20 14:35:23 +0000

    Seprate model depended code. (based on arc 'struct platform')
    files.prep:
     - add new files entry.
    
    pci_machdep.c:
     - use dev/pci/pciconf.c instead of mine for pci io/mem addr/intr fixup.
    
    cpu.c:
     - separate code
    
    machdep.c:
     - call ident_platform()
    
    mainbus.c:
     - call pci_configure_bus(), if defined PCI_NETBSD_CONFIGURE
    
    ibm_machdep.c:
     - for IBM PReP generic
    
    ibm_6050.c:
     - for IBM Personal Power Series 830/850
    
    ibm_7248.c:
     - for IBM RS6000/43P deppendency

--- sys/arch/prep/prep/platform.c
+++ sys/arch/prep/prep/platform.c
@@ -1,7 +1,7 @@
-/*	$NetBSD: cpu.c,v 1.4 2001/06/20 14:35:25 nonaka Exp $	*/
+/*	$NetBSD: platform.c,v 1.1 2001/06/20 14:35:27 nonaka Exp $	*/
 
 /*-
- * Copyright (c) 2000, 2001 The NetBSD Foundation, Inc.
+ * Copyright (c) 2001 The NetBSD Foundation, Inc.
  * All rights reserved.
  *
  * This code is derived from software contributed to The NetBSD Foundation
@@ -17,8 +17,8 @@
  *    documentation and/or other materials provided with the distribution.
  * 3. All advertising materials mentioning features or use of this software
  *    must display the following acknowledgement:
- *      This product includes software developed by the NetBSD
- *      Foundation, Inc. and its contributors.
+ *	This product includes software developed by the NetBSD
+ *	Foundation, Inc. and its contributors.
  * 4. Neither the name of The NetBSD Foundation nor the names of its
  *    contributors may be used to endorse or promote products derived
  *    from this software without specific prior written permission.
@@ -37,44 +37,78 @@
  */
 
 #include <sys/param.h>
-#include <sys/systm.h>
-#include <sys/device.h>
 
-#include <machine/autoconf.h>
-#include <machine/bus.h>
-#include <machine/cpu.h>
+#include <machine/intr.h>
 #include <machine/platform.h>
+#include <machine/residual.h>
 
-int cpumatch(struct device *, struct cfdata *, void *);
-void cpuattach(struct device *, struct device *, void *);
-
-struct cfattach cpu_ca = {
-	sizeof(struct device), cpumatch, cpuattach
+static struct platform platform_unknown = {
+	NULL,			/* model */
+	platform_generic_match,	/* match */
+	pci_intr_nofixup,	/* pci_intr_fixup */
+	ext_intr,		/* ext_intr */
+	cpu_setup_unknown,	/* cpu_setup */
+	reset_unknown,		/* reset */
 };
 
-extern struct cfdriver cpu_cd;
+static struct plattab plattab_unknown = {
+	NULL,	0
+};
+
+struct platform *platform = &platform_unknown;
 
 int
-cpumatch(parent, cfdata, aux)
-	struct device *parent;
-	struct cfdata *cfdata;
-	void *aux;
+ident_platform(void)
 {
-	struct confargs *ca = aux;
+	struct plattab *p;
+	int matched = -1, match = 0;
+	int i, rv;
 
-	if (strcmp(ca->ca_name, cpu_cd.cd_name) != 0)
-		return (0);
-	return (1);
+	if (res == NULL)
+		return 0;
+
+	if (strncmp(res->VitalProductData.PrintableModel, "IBM", 3) == 0)
+		p = &plattab_ibm;
+	else
+		p = &plattab_unknown;
+
+	for (i = 0; i < p->num; i++) {
+		rv = (*p->platform[i]->match)(p->platform[i]);
+		if (rv > match) {
+			match = rv;
+			matched = i;
+		}
+	}
+	if (match)
+		platform = p->platform[matched];
+	return match;
+}
+
+int
+platform_generic_match(struct platform *p)
+{
+
+	if (p->model == NULL)
+		return 0;
+	if (strcmp(res->VitalProductData.PrintableModel, p->model) == 0)
+    		return 1;
+	return 0;
+}
+
+
+/* ARGUSED */
+void
+pci_intr_nofixup(int busno, int device, int *intr)
+{
+}
+
+/* ARGUSED */
+void
+cpu_setup_unknown(struct device *dev)
+{
 }
 
 void
-cpuattach(parent, dev, aux)
-	struct device *parent;
-	struct device *dev;
-	void *aux;
+reset_unknown(void)
 {
-
-	printf("\n");
-
-	(*platform->cpu_setup)(dev);
 }
