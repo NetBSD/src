d75760a3290bc5fda0045517dd699b8d6a25415a
simonb <simonb@NetBSD.org>
2001-06-20 03:15:42 +0000

    Add some more e_machine values and a reference to the SCO website with ELF ABI information.

--- sys/sys/exec_elf.h
+++ sys/sys/exec_elf.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: exec_elf.h,v 1.49 2001/06/20 01:20:19 fvdl Exp $	*/
+/*	$NetBSD: exec_elf.h,v 1.50 2001/06/20 03:15:42 simonb Exp $	*/
 
 /*-
  * Copyright (c) 1994 The NetBSD Foundation, Inc.
@@ -38,6 +38,14 @@
 
 #ifndef _SYS_EXEC_ELF_H_
 #define	_SYS_EXEC_ELF_H_
+
+/*
+ * The current ELF ABI specification is available at:
+ *	http://www.sco.com/developer/gabi/
+ *
+ * Current header definitions are in:
+ *	http://www.sco.com/developer/gabi/latest/ch4.eheader.html
+ */
 
 #include <machine/int_types.h>
 
@@ -171,6 +179,7 @@ typedef struct {
 #define ELFOSABI_TRU64		10	/* TRU64 UNIX */
 #define ELFOSABI_MODESTO	11	/* Novell Modesto */
 #define ELFOSABI_OPENBSD	12	/* OpenBSD */
+/* Unofficial OSABIs follow */
 #define ELFOSABI_ARM		97	/* ARM */
 #define	ELFOSABI_STANDALONE	255	/* Standalone (embedded) application */
 
@@ -199,6 +208,7 @@ typedef struct {
 #define	EM_MIPS		8	/* MIPS I Architecture */
 #define	EM_S370		9	/* Amdahl UTS on System/370 */
 #define	EM_MIPS_RS3_LE	10	/* MIPS RS3000 Little-endian */
+			/* 11-14 - Reserved */
 #define	EM_RS6000	11	/* IBM RS/6000 XXX reserved */
 #define	EM_PARISC	15	/* Hewlett-Packard PA-RISC */
 #define	EM_NCUBE	16	/* NCube XXX reserved */
@@ -206,6 +216,8 @@ typedef struct {
 #define	EM_SPARC32PLUS	18	/* Enhanced instruction set SPARC */
 #define	EM_960		19	/* Intel 80960 */
 #define	EM_PPC		20	/* PowerPC */
+#define	EM_PPC64	21	/* 64-bit PowerPC */
+			/* 22-35 - Reserved */
 #define	EM_V800		36	/* NEC V800 */
 #define	EM_FR20		37	/* Fujitsu FR20 */
 #define	EM_RH32		38	/* TRW RH-32 */
@@ -224,8 +236,48 @@ typedef struct {
 #define	EM_MIPS_X	51	/* Stanford MIPS-X */
 #define	EM_COLDFIRE	52	/* Motorola Coldfire */
 #define	EM_68HC12	53	/* Motorola MC68HC12 */
-#define EM_X86_64	62	/* AMD x86-64 */
-#define	EM_VAX		75	/* DIGITAL VAX */
+#define	EM_MMA		54	/* Fujitsu MMA Multimedia Accelerator */
+#define	EM_PCP		55	/* Siemens PCP */
+#define	EM_NCPU		56	/* Sony nCPU embedded RISC processor */
+#define	EM_NDR1		57	/* Denso NDR1 microprocessor */
+#define	EM_STARCORE	58	/* Motorola Star*Core processor */
+#define	EM_ME16		59	/* Toyota ME16 processor */
+#define	EM_ST100	60	/* STMicroelectronics ST100 processor */
+#define	EM_TINYJ	61	/* Advanced Logic Corp. TinyJ embedded family processor */
+#define	EM_X86_64	62	/* AMD x86-64 architecture */
+#define	EM_PDSP		63	/* Sony DSP Processor */
+			/* 64-65 - Reserved */
+#define	EM_FX66		66	/* Siemens FX66 microcontroller */
+#define	EM_ST9PLUS	67	/* STMicroelectronics ST9+ 8/16 bit microcontrol
+#define	EM_ST7		68	/* STMicroelectronics ST7 8-bit microcontroller */
+#define	EM_68HC16	69	/* Motorola MC68HC16 Microcontroller */
+#define	EM_68HC11	70	/* Motorola MC68HC11 Microcontroller */
+#define	EM_68HC08	71	/* Motorola MC68HC08 Microcontroller */
+#define	EM_68HC05	72	/* Motorola MC68HC05 Microcontroller */
+#define	EM_SVX		73	/* Silicon Graphics SVx */
+#define	EM_ST19		74	/* STMicroelectronics ST19 8-bit cpu */
+ler */
+#define	EM_VAX		75	/* Digital VAX */
+#define	EM_CRIS		76	/* Axis Communications 32-bit embedded processor */
+#define	EM_JAVELIN	77	/* Infineon Technologies 32-bit embedded cpu */
+#define	EM_FIREPATH	78	/* Element 14 64-bit DSP processor */
+#define	EM_ZSP		79	/* LSI Logic's 16-bit DSP processor */
+#define	EM_MMIX		80	/* Donald Knuth's educational 64-bit processor */
+#define	EM_HUANY	81	/* Harvard's machine-independent format */
+#define	EM_PRISM	82	/* SiTera Prism */
+#define	EM_AVR		83	/* Atmel AVR 8-bit microcontroller */
+#define	EM_FR30		84	/* Fujitsu FR30 */
+#define	EM_D10V		85	/* Mitsubishi D10V */
+#define	EM_D30V		86	/* Mitsubishi D30V */
+#define	EM_V850		87	/* NEC v850 */
+#define	EM_M32R		88	/* Mitsubishi M32R */
+#define	EM_MN10300	89	/* Matsushita MN10300 */
+#define	EM_MN10200	90	/* Matsushita MN10200 */
+#define	EM_PJ		91	/* picoJava */
+#define	EM_OPENRISC	92	/* OpenRISC 32-bit embedded processor */
+#define	EM_ARC_A5	93	/* ARC Cores Tangent-A5 */
+#define	EM_XTENSA	94	/* Tensilica Xtensa Architecture */
+/* Unofficial machine types follow */
 #define	EM_ALPHA_EXP	36902	/* used by NetBSD/alpha; obsolete */
 #define	EM_NUM		36903
 
