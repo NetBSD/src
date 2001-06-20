091b05a2ecef9b8eac83543aa654d0bdd259e9a4
toshii <toshii@NetBSD.org>
2001-06-20 02:34:26 +0000

    Define __HAVE_GENERIC_SOFT_INTERRUPTS, and nuke traditional softserial and softclock.

--- sys/arch/hpcarm/include/types.h
+++ sys/arch/hpcarm/include/types.h
@@ -1,4 +1,4 @@
-/* $NetBSD: types.h,v 1.3 2001/02/23 22:02:44 bjh21 Exp $ */
+/* $NetBSD: types.h,v 1.4 2001/06/20 02:34:26 toshii Exp $ */
 
 #ifndef _HPCARM_TYPES_H_
 #define _HPCARM_TYPES_H_
@@ -7,5 +7,6 @@
 
 #define __HAVE_DEVICE_REGISTER
 #define __HAVE_NWSCONS
+#define __HAVE_GENERIC_SOFT_INTERRUPTS
 
 #endif
