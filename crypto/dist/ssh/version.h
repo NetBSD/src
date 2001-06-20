99dc93cbb9f57574569fb082d074c4cfccbd1bb7
itojun <itojun@NetBSD.org>
2001-06-20 07:49:45 +0000

    bump netbsd-local version number to identify X11 "cookies" fix

--- crypto/dist/ssh/version.h
+++ crypto/dist/ssh/version.h
@@ -1,8 +1,8 @@
-/*	$NetBSD: version.h,v 1.13 2001/05/15 15:26:11 itojun Exp $	*/
+/*	$NetBSD: version.h,v 1.14 2001/06/20 07:49:45 itojun Exp $	*/
 /* $OpenBSD: version.h,v 1.23 2001/04/24 16:43:16 markus Exp $ */
 
 #define __OPENSSH_VERSION	"OpenSSH_2.9"
-#define __NETBSDSSH_VERSION	"NetBSD_Secure_Shell-20010515"
+#define __NETBSDSSH_VERSION	"NetBSD_Secure_Shell-20010614"
 
 /*
  * it is important to retain OpenSSH version identification part, it is
