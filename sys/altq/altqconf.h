4a0339df3834b98cfbf26d9792e1858f88a543c0
nathanw <nathanw@NetBSD.org>
2001-06-21 17:58:41 +0000

    Catch up to -current.

--- sys/altq/altqconf.h
+++ sys/altq/altqconf.h
@@ -1,6 +1,6 @@
-/*	$NetBSD: altqconf.h,v 1.1 2000/12/14 23:50:43 thorpej Exp $	*/
+/*	$NetBSD: altqconf.h,v 1.1.4.1 2001/06/21 18:12:50 nathanw Exp $	*/
 
-#if defined(_KERNEL) && !defined(_LKM)
+#if defined(_KERNEL_OPT)
 #include "opt_altq_enabled.h"
 
 #include <sys/conf.h>
@@ -14,4 +14,4 @@
 cdev_decl(altq);
 
 #define	cdev_altq_init(x,y)	cdev__oci_init(x,y)
-#endif /* _KERNEL && ! _LKM */
+#endif /* _KERNEL_OPT */
