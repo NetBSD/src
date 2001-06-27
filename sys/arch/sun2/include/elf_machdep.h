50b382a15dbe65c8cc5a0b33eb5244a3c8269a60
fredette <fredette@NetBSD.org>
2001-06-27 19:20:22 +0000

    Now define a suitable ELF32_MACHDEP_ID_CASES.

--- sys/arch/sun2/include/elf_machdep.h
+++ sys/arch/sun2/include/elf_machdep.h
@@ -1,8 +1,13 @@
-/*	$NetBSD: elf_machdep.h,v 1.1 2001/04/06 13:13:03 fredette Exp $	*/
+/*	$NetBSD: elf_machdep.h,v 1.2 2001/06/27 19:20:22 fredette Exp $	*/
 
 #ifndef _MACHINE_ELF_MACHDEP_H_
 #define _MACHINE_ELF_MACHDEP_H_
 
 #include <m68k/elf_machdep.h>
+
+#undef	ELF32_MACHDEP_ID_CASES
+#define	ELF32_MACHDEP_ID_CASES						\
+                case EM_68000:						\
+                        break;
 
 #endif
