96dfe8fbfd52927df4804accab6daa1ad20b5224
nisimura <nisimura@NetBSD.org>
2001-06-27 08:20:44 +0000

    Do a straight and simple cpu identification in cpu_attach().

--- sys/arch/newsmips/newsmips/cpu.c
+++ sys/arch/newsmips/newsmips/cpu.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: cpu.c,v 1.3 2000/02/19 03:59:04 mycroft Exp $	*/
+/*	$NetBSD: cpu.c,v 1.4 2001/06/27 08:20:45 nisimura Exp $	*/
 
 /*
  * Copyright (c) 1994, 1995 Carnegie-Mellon University.
@@ -30,6 +30,8 @@
 #include <sys/param.h>
 #include <sys/device.h>
 #include <sys/systm.h>
+
+#include <mips/locore.h>
 
 #include <machine/cpu.h>
 #include <machine/autoconf.h>
@@ -65,7 +67,53 @@ cpuattach(parent, dev, aux)
 	struct device *dev;
 	void *aux;
 {
+	char *cpu_name, *fpu_name;
 
-	printf(": ");
-	cpu_identify();
+	printf("\n");
+	switch (MIPS_PRID_IMPL(cpu_id)) {
+	case MIPS_R3000:
+		cpu_name = "MIPS R3000 CPU";
+		break;
+	case MIPS_R4000:
+		cpu_name = "MIPS R4000 CPU";
+		break;
+	default:
+		cpu_name = "Unknown CPU";
+	}
+	if (MIPS_PRID_IMPL(cpu_id) == MIPS_R4000
+	    && mips_L1ICacheSize == 16384)
+		cpu_name = "MIPS R4400 CPU";
+
+	switch (MIPS_PRID_IMPL(fpu_id)) {
+	case MIPS_R3010:
+		fpu_name = "MIPS R3010 FPA";
+		break;
+	case MIPS_R4010:
+		fpu_name = "MIPS R4010 FPA";
+		break;
+	default:
+		fpu_name = "unknown FPA";
+		break;
+	}
+	printf("%s: %s (0x%04x) with %s (0x%04x)\n",
+	    dev->dv_xname, cpu_name, cpu_id, fpu_name, fpu_id);
+	if (MIPS_PRID_IMPL(cpu_id) != MIPS_R4000) {
+		printf("%s: ", dev->dv_xname);
+		printf("%dKB Instruction, %dKB Data, direct mapped cache\n",
+		    mips_L1ICacheSize/1024, mips_L1DCacheSize/1024);
+	}
+	else {
+		printf("%s: L1 cache: ", dev->dv_xname);
+		printf("%dKB Instruction, %dKB Data, direct mapped\n",
+		    mips_L1ICacheSize/1024, mips_L1DCacheSize/1024);
+		printf("%s: ", dev->dv_xname);
+		
+		if (!mips_L2CachePresent)
+			printf("no L2 cache\n");
+		else
+			printf("L2 cache: %dKB/%dB %s, %s\n",
+			    mips_L2CacheSize/1024, mips_L2CacheLSize,
+			    mips_L2CacheMixed ? "mixed" : "separated",
+			    mips_L2CacheIsSnooping? "snooping" : "no snooping");
+	}
 }
