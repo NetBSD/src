4f21bb2d6ac217a321633609cadf24335e198714
itojun <itojun@NetBSD.org>
2001-07-02 23:02:11 +0000

    typo (should we get rid of this portion?

--- sys/kern/subr_userconf.c
+++ sys/kern/subr_userconf.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: subr_userconf.c,v 1.1 2001/07/01 02:56:20 gmcgarry Exp $	*/
+/*	$NetBSD: subr_userconf.c,v 1.2 2001/07/02 23:02:11 itojun Exp $	*/
 
 /*
  * Copyright (c) 1996 Mats O Jansson <moj@stacken.kth.se>
@@ -428,7 +428,7 @@ userconf_change(devno)
 				l++;
 			}
 
-#ifdef __OpenSBD__
+#ifdef __OpenBSD__
 			if (share) {
 				if (bcmp(cd->cf_loc, lk, sizeof(int) * i))
 					cd->cf_loc = lk;
