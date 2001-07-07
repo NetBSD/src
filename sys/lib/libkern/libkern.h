5439e1a9ff73efa18d35667ecedd946c1380aa23
simonb <simonb@NetBSD.org>
2001-07-07 12:23:43 +0000

    Put the prototypes for bcmp() and bzero() back (ifdef _STANDALONE) so that libsa builds again.

--- sys/lib/libkern/libkern.h
+++ sys/lib/libkern/libkern.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: libkern.h,v 1.35 2001/07/07 05:42:16 perry Exp $	*/
+/*	$NetBSD: libkern.h,v 1.36 2001/07/07 12:23:43 simonb Exp $	*/
 
 /*-
  * Copyright (c) 1992, 1993
@@ -250,6 +250,10 @@ tolower(ch)
 #endif
 
 /* Prototypes for non-quad routines. */
+#ifdef _STANDALONE
+int	 bcmp __P((const void *, const void *, size_t));
+void	 bzero __P((void *, size_t));
+#endif
 void	 __assert __P((const char *, const char *, int, const char *))
 	    __attribute__((__noreturn__));
 int	 ffs __P((int));
