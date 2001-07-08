f35d5da40c1a6581f37c85c594324fb703cdc7e6
gmcgarry <gmcgarry@NetBSD.org>
2001-07-08 05:21:15 +0000

    backout previous; wrong branch

--- bin/date/date.c
+++ bin/date/date.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: date.c,v 1.30.4.2 2001/07/08 05:15:17 gmcgarry Exp $	*/
+/*	$NetBSD: date.c,v 1.30.4.3 2001/07/08 05:21:15 gmcgarry Exp $	*/
 
 /*
  * Copyright (c) 1985, 1987, 1988, 1993
@@ -44,7 +44,7 @@ __COPYRIGHT(
 #if 0
 static char sccsid[] = "@(#)date.c	8.2 (Berkeley) 4/28/95";
 #else
-__RCSID("$NetBSD: date.c,v 1.30.4.2 2001/07/08 05:15:17 gmcgarry Exp $");
+__RCSID("$NetBSD: date.c,v 1.30.4.3 2001/07/08 05:21:15 gmcgarry Exp $");
 #endif
 #endif /* not lint */
 
@@ -131,7 +131,7 @@ main(argc, argv)
 
 #define	ATOI2(s)	((s) += 2, ((s)[-2] - '0') * 10 + ((s)[-1] - '0'))
 
-static void
+void
 setthetime(p)
 	const char *p;
 {
