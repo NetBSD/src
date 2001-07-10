5c3c5aed35049e91a872e05ecdc015fbc52c9b1f
ichiro <ichiro@NetBSD.org>
2001-07-10 16:39:33 +0000

    delete Extended-GPIOs mapping

--- sys/arch/hpcarm/sa11x0/sa11x0.c
+++ sys/arch/hpcarm/sa11x0/sa11x0.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: sa11x0.c,v 1.16 2001/07/08 14:43:52 ichiro Exp $	*/
+/*	$NetBSD: sa11x0.c,v 1.17 2001/07/10 16:39:33 ichiro Exp $	*/
 
 /*-
  * Copyright (c) 2001, The NetBSD Foundation, Inc.  All rights reserved.
@@ -144,20 +144,11 @@ sa11x0_attach(parent, self, aux)
 		panic("%s: Cannot map registers\n", self->dv_xname);
 	saipic_base = sc->sc_ioh;
 
-	/* Map the GPIO and Extended GPIO registers */
+	/* Map the GPIO registers */
 	if (bus_space_map(sc->sc_iot, SAGPIO_BASE, SAGPIO_NPORTS,
 			  0, &sc->sc_gpioh))
 		panic("%s: unable to map GPIO registers\n", self->dv_xname);
 	bus_space_write_4(sc->sc_iot, sc->sc_gpioh, SAGPIO_EDR, 0xffffffff);
-
-#ifdef SAEGPIO_BASE
-	if (bus_space_map(sc->sc_iot, SAEGPIO_BASE, 1, 0, &sc->sc_egpioh))
-		panic("%s: unable to map Extended GPIO registers\n",
-			self->dv_xname);
-# if 0
-	bus_space_write_2(sc->sc_iot, sc->sc_egpioh, 0, EGPIO_INIT);
-# endif
-#endif
 
 	/* Map the PPC registers */
 	if (bus_space_map(sc->sc_iot, SAPPC_BASE, SAPPC_NPORTS,
