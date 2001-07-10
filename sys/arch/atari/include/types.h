ba86e5619b8fefdc83bc6e36e579cd8aad48ae15
leo <leo>
2001-07-10 20:23:36 +0000

    Remove __BROKEN_CONFIG_UNIT_USAGE.

--- sys/arch/atari/include/types.h
+++ sys/arch/atari/include/types.h
@@ -1,10 +1,8 @@
-/*	$NetBSD: types.h,v 1.6 2000/02/05 00:13:23 cgd Exp $	*/
+/*	$NetBSD: types.h,v 1.7 2001/07/10 20:23:36 leo Exp $	*/
 
 #ifndef _MACHINE_TYPES_H_
 #define _MACHINE_TYPES_H_
 
 #include <m68k/types.h>
-
-#define __BROKEN_CONFIG_UNIT_USAGE
 
 #endif
