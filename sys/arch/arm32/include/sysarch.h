274c58ffd667b396a6e2b39ad402348005574c87
matt <matt@NetBSD.org>
2001-07-14 00:23:09 +0000

    Make <sysarch.h> valid for all arm platforms.  Adjust/remove port-specific includes as required.

--- sys/arch/arm32/include/sysarch.h
+++ sys/arch/arm32/include/sysarch.h
@@ -1,4 +1,4 @@
-/* $NetBSD: sysarch.h,v 1.6 2001/02/23 21:23:51 reinoud Exp $ */
+/* $NetBSD: sysarch.h,v 1.7 2001/07/14 00:23:10 matt Exp $ */
 
-#include <arm/arm32/sysarch.h>
+#include <arm/sysarch.h>
 
