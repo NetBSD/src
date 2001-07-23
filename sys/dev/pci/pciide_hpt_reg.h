ecbce129cbe03eafc087d0eee1eb99f83498eb88
bouyer <bouyer@NetBSD.org>
2001-07-23 14:55:26 +0000

    Handle HPT-370A controllers, from skanto@sjk-software.fi (kern/13540) but with some differences to the original patch: don't assume all controllers with rev >= HPT370_REV are HPT370, and explicitely print if we have a chip with a rev the driver does't know.

--- sys/dev/pci/pciide_hpt_reg.h
+++ sys/dev/pci/pciide_hpt_reg.h
@@ -1,4 +1,4 @@
-/*      $NetBSD: pciide_hpt_reg.h,v 1.3 2001/01/05 15:20:53 bouyer Exp $       */
+/*      $NetBSD: pciide_hpt_reg.h,v 1.4 2001/07/23 14:55:27 bouyer Exp $       */
 
 /*
  * Copyright (c) 2000 Manuel Bouyer.
@@ -46,6 +46,7 @@
  */
 #define HPT366_REV 0x01
 #define HPT370_REV 0x03
+#define HPT370A_REV 0x04
 
 #define HPT_IDETIM(chan, drive) (0x40 + ((drive) * 4) + ((chan) * 8))
 #define HPT_IDETIM_BUFEN		0x80000000
