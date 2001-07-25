b2a9cd60be6f747a141bea19da388d85c2adfc57
itojun <itojun>
2001-07-25 07:13:44 +0000

    do not copy rmx_mtu on RTM_ADD/RESOLVE.  the fragment was mistakenly introduced on 1.25, from other *bsd via kame.  from thorpej

--- sys/net/route.c
+++ sys/net/route.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: route.c,v 1.45 2001/07/20 18:52:18 itojun Exp $	*/
+/*	$NetBSD: route.c,v 1.46 2001/07/25 07:13:44 itojun Exp $	*/
 
 /*-
  * Copyright (c) 1998 The NetBSD Foundation, Inc.
@@ -648,13 +648,6 @@ rtrequest1(req, info, ret_nrt)
 			rt->rt_rmx = (*ret_nrt)->rt_rmx; /* copy metrics */
 			rt->rt_parent = *ret_nrt;
 			rt->rt_parent->rt_refcnt++;
-		} else if (rt->rt_rmx.rmx_mtu == 0
-			    && !(rt->rt_rmx.rmx_locks & RTV_MTU)) { /* XXX */
-			if (rt->rt_gwroute != NULL) {
-				rt->rt_rmx.rmx_mtu = rt->rt_gwroute->rt_rmx.rmx_mtu;
-			} else {
-				rt->rt_rmx.rmx_mtu = ifa->ifa_ifp->if_mtu;
-			}
 		}
 		rn = rnh->rnh_addaddr((caddr_t)ndst, (caddr_t)netmask,
 		    rnh, rt->rt_nodes);
