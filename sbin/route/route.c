7bfb790042ff533fc7c39903b33814b88784f0f9
he <he@NetBSD.org>
2001-07-29 19:47:03 +0000

    Pull up revision 1.46 (requested by bad):   Make sure decoded output from ``route monitor'' gets properly   flushed.

--- sbin/route/route.c
+++ sbin/route/route.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: route.c,v 1.38.4.4 2001/04/05 12:41:51 he Exp $	*/
+/*	$NetBSD: route.c,v 1.38.4.5 2001/07/29 19:47:03 he Exp $	*/
 
 /*
  * Copyright (c) 1983, 1989, 1991, 1993
@@ -43,7 +43,7 @@ __COPYRIGHT("@(#) Copyright (c) 1983, 1989, 1991, 1993\n\
 #if 0
 static char sccsid[] = "@(#)route.c	8.6 (Berkeley) 4/28/95";
 #else
-__RCSID("$NetBSD: route.c,v 1.38.4.4 2001/04/05 12:41:51 he Exp $");
+__RCSID("$NetBSD: route.c,v 1.38.4.5 2001/07/29 19:47:03 he Exp $");
 #endif
 #endif /* not lint */
 
@@ -1639,17 +1639,17 @@ pmsg_addrs(cp, addrs)
 	struct sockaddr *sa;
 	int i;
 
-	if (addrs == 0)
-		return;
-	(void) printf("\nsockaddrs: ");
-	bprintf(stdout, addrs, addrnames);
-	(void) putchar('\n');
-	for (i = 1; i; i <<= 1)
-		if (i & addrs) {
-			sa = (struct sockaddr *)cp;
-			(void) printf(" %s", routename(sa));
-			ADVANCE(cp, sa);
-		}
+	if (addrs != 0) {
+		(void) printf("\nsockaddrs: ");
+		bprintf(stdout, addrs, addrnames);
+		(void) putchar('\n');
+		for (i = 1; i; i <<= 1)
+			if (i & addrs) {
+				sa = (struct sockaddr *)cp;
+				(void) printf(" %s", routename(sa));
+				ADVANCE(cp, sa);
+			}
+	}
 	(void) putchar('\n');
 	(void) fflush(stdout);
 }
