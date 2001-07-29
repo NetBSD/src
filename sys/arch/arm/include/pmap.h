0cf7a650bd0c71cbd6ba231351306883c376235a
chris <chris@NetBSD.org>
2001-07-29 09:50:07 +0000

    PR 13581 Add empty pmap.h and vmparam.h to make userland happier.

---
+++ sys/arch/arm/include/pmap.h
@@ -0,0 +1,8 @@
+/* $NetBSD: pmap.h,v 1.1 2001/07/29 09:50:07 chris Exp $ */
+
+/*
+ * Dummy pmap.h file to keep userland happy
+ */
+#ifdef _KERNEL
+#error "userland pmap.h included by the kernel!"
+#endif
