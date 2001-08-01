55d2f0067cab6dc376958b936391363961987a7e
ichiro <ichiro@NetBSD.org>
2001-08-01 06:43:50 +0000

    add register of GPCLK/UART1 (SACOM1_BASE)

--- sys/arch/hpcarm/sa11x0/sa11x0_reg.h
+++ sys/arch/hpcarm/sa11x0/sa11x0_reg.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: sa11x0_reg.h,v 1.7 2001/07/11 15:20:23 ichiro Exp $	*/
+/*	$NetBSD: sa11x0_reg.h,v 1.8 2001/08/01 06:43:50 ichiro Exp $	*/
 
 /*-
  * Copyright (c) 2001 The NetBSD Foundation, Inc.  All rights reserved.
@@ -47,6 +47,7 @@
 #define SAIPIC_BASE		0x90050000	/* Interrupt Controller */
 #define SAPPC_BASE		0x90060000	/* Peripheral Pin Controller */
 #define SAUDC_BASE		0x80000000	/* USB Device Controller*/
+#define SACOM1_BASE		0x80010000	/* GPCLK/UART 1 */
 #define SACOM3_HW_BASE		0x80050000	/* UART 3  */
 #define SAMCP_BASE		0x80060000	/* MCP Controller */
 #define SASSP_BASE		0x80070000	/* Synchronous serial port */
