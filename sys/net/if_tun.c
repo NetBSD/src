993919d18958f96eb52f317cf92716ae0415686d
itojun <itojun@NetBSD.org>
2001-08-02 13:37:51 +0000

    do not break from loop even if m_len == 0.  it's valid to have mbuf with m_len == 0 in mbuf chain.

--- sys/net/if_tun.c
+++ sys/net/if_tun.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: if_tun.c,v 1.43 2001/04/13 23:30:18 thorpej Exp $	*/
+/*	$NetBSD: if_tun.c,v 1.44 2001/08/02 13:37:51 itojun Exp $	*/
 
 /*
  * Copyright (c) 1988, Julian Onions <jpo@cs.nott.ac.uk>
@@ -511,6 +511,11 @@ tunread(dev, uio, ioflag)
 	splx(s);
 
 	while (m0 && uio->uio_resid > 0 && error == 0) {
+		if (m0->m_len == 0) {
+			MFREE(m0, m);
+			m0 = m;
+			continue;
+		}
 		len = min(uio->uio_resid, m0->m_len);
 		if (len == 0)
 			break;
