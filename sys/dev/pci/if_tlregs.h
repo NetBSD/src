7c292e3d22fe6d6d32d91ea4f8db7946588a338b
bouyer <bouyer@NetBSD.org>
2001-08-03 16:53:08 +0000

    - kill the carrier detect code. It's not usefull any more and can give   false informations on a loaded ethernet segement. - convert to bus_dma(9) (better late than never :) - add proper le32toh/htole32 so that it works on big-endian system (tested   on macppc). Close PR kern/10327.

--- sys/dev/pci/if_tlregs.h
+++ sys/dev/pci/if_tlregs.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: if_tlregs.h,v 1.4 1998/08/15 16:58:53 bouyer Exp $	*/
+/*	$NetBSD: if_tlregs.h,v 1.5 2001/08/03 16:53:08 bouyer Exp $	*/
 
 /*
  * Copyright (c) 1997 Manuel Bouyer.  All rights reserved.
@@ -184,16 +184,20 @@ struct tl_Tx_list {
  * lists must start on an 8 bytes boundary.
  */
 
-struct Tx_list {
-	struct mbuf *m; /* mbuf associated with this list */
-	struct Tx_list* next;
-	struct tl_Tx_list hw_list;
-};
-
 struct Rx_list {
 	struct mbuf *m; /* mbuf associated with this list */
+	bus_dmamap_t m_dmamap; /* and it's DMA map */
 	struct Rx_list *next;
-	struct tl_Rx_list hw_list;
+	bus_addr_t hw_listaddr;
+	struct tl_Rx_list *hw_list;
+};
+
+struct Tx_list {
+	struct mbuf *m; /* mbuf associated with this list */
+	bus_dmamap_t m_dmamap; /* and it's DMA map */
+	struct Tx_list *next;
+	bus_addr_t hw_listaddr;
+	struct tl_Tx_list *hw_list;
 };
 
 #endif /* ! _DEV_PCI_IF_TLREGS_H_ */
