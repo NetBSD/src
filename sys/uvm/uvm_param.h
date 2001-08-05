0448824a7f966cd214b795befeba600ac9f81175
matt <matt@NetBSD.org>
2001-08-05 03:33:15 +0000

    Don't include <machine/pmap.h> and <machine/vmparam.h> if _KERNEL isn't defined.  Include them explicitly in the few kvm_arch.c that need them.

--- sys/uvm/uvm_param.h
+++ sys/uvm/uvm_param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: uvm_param.h,v 1.11 2001/07/14 06:36:03 matt Exp $	*/
+/*	$NetBSD: uvm_param.h,v 1.12 2001/08/05 03:33:16 matt Exp $	*/
 
 /*
  * Copyright (c) 1991, 1993
@@ -71,7 +71,9 @@
 #ifndef	_VM_PARAM_
 #define	_VM_PARAM_
 
+#ifdef _KERNEL
 #include <machine/vmparam.h>
+#endif
 
 /*
  * This belongs in types.h, but breaks too many existing programs.
