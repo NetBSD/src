3406fca2ea4692a51afa153eb7ec9e86a4cc0675
chris <chris@NetBSD.org>
2001-08-05 13:09:46 +0000

    Share procfs_machdep.c across arm.

---
+++ sys/arch/arm/arm/procfs_machdep.c
@@ -0,0 +1,20 @@
+/*	$NetBSD: procfs_machdep.c,v 1.1.8.2 2001/08/05 13:09:46 chris Exp $	*/
+
+#include <sys/param.h>
+#include <sys/systm.h>
+#include <sys/mount.h>
+#include <sys/vnode.h>
+#include <miscfs/procfs/procfs.h>
+
+
+/*
+ * Linux-style /proc/cpuinfo.
+ * Only used when procfs is mounted with -o linux.
+ */
+int
+procfs_getcpuinfstr(char *buf, int *len)
+{
+	*len = 0;
+
+	return 0;
+}
