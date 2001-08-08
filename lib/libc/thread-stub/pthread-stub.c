350bec5a9e2622565ab7573a12796abcb9ac7d1c
nathanw <nathanw@NetBSD.org>
2001-08-08 16:23:12 +0000

    Add weak aliases and stub implementations for pthread_once, pthread_cond*, pthread_key_create, pthread_{get,set}specific, pthread_self.
    Disable the pthread_rwlock stubs pending the creation of
    pthread_rwlock_t and pthread_rwlockattr_t.

--- lib/libc/thread-stub/pthread-stub.c
+++ lib/libc/thread-stub/pthread-stub.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: pthread-stub.c,v 1.3 2000/01/22 22:19:21 mycroft Exp $	*/
+/*	$NetBSD: pthread-stub.c,v 1.3.6.1 2001/08/08 16:23:12 nathanw Exp $	*/
 
 /*-
  * Copyright (c) 1999 Michael Graff <explorer@flame.org>.
@@ -40,27 +40,50 @@
 __weak_alias(pthread_mutex_init, _pthread_mutex_init)
 __weak_alias(pthread_mutex_lock, _pthread_mutex_lock)
 __weak_alias(pthread_mutex_unlock, _pthread_mutex_unlock)
-
+#if 0
 __weak_alias(pthread_rwlock_init, _pthread_rwlock_init)
 __weak_alias(pthread_rwlock_rdlock, _pthread_rwlock_rdlock)
 __weak_alias(pthread_rwlock_wrlock, _pthread_rwlock_wrlock)
 __weak_alias(pthread_rwlock_unlock, _pthread_rwlock_unlock)
-
+#endif
 __weak_alias(flockfile, _flockfile)
 __weak_alias(funlockfile, _funlockfile)
+__weak_alias(pthread_once, _pthread_once)
+__weak_alias(pthread_cond_init, _pthread_cond_init)
+__weak_alias(pthread_cond_wait, _pthread_cond_wait)
+__weak_alias(pthread_cond_signal, _pthread_cond_signal)
+__weak_alias(pthread_spinlock, _pthread_spinlock)
+__weak_alias(pthread_spinunlock, _pthread_spinunlock)
+__weak_alias(pthread_key_create, _pthread_key_create)
+__weak_alias(pthread_setspecific, _pthread_setspecific)
+__weak_alias(pthread_getspecific, _pthread_getspecific)
+__weak_alias(pthread_self, _pthread_self)
+__weak_alias(pthread_sigmask, _pthread_sigmask)
 #endif
 
 int _pthread_mutex_init(pthread_mutex_t *, pthread_mutexattr_t *);
 int _pthread_mutex_lock(pthread_mutex_t *);
 int _pthread_mutex_unlock(pthread_mutex_t *);
-
+#if 0
 int _pthread_rwlock_init(pthread_rwlock_t *, pthread_rwlockattr_t *);
 int _pthread_rwlock_rdlock(pthread_rwlock_t *);
 int _pthread_rwlock_wrlock(pthread_rwlock_t *);
 int _pthread_rwlock_unlock(pthread_rwlock_t *);
-
+#endif
 int _flockfile(FILE *);
 int _funlockfile(FILE *);
+int _pthread_once(pthread_once_t *, void (*)(void));
+int _pthread_cond_init(pthread_cond_t *,const pthread_condattr_t *);
+int _pthread_cond_wait(pthread_cond_t *, pthread_mutex_t *);
+int _pthread_cond_signal(pthread_cond_t *);
+void _pthread_spinlock(pthread_t, pt_spin_t *);
+void _pthread_spinunlock(pthread_t, pt_spin_t *);
+int _pthread_key_create(pthread_key_t *, void (*)(void *));
+int _pthread_setspecific(pthread_key_t, const void *);
+void* _pthread_getspecific(pthread_key_t);
+pthread_t _pthread_self(void);
+int _pthread_sigmask(int, const sigset_t *, sigset_t *);
+
 
 int
 _pthread_mutex_init(pthread_mutex_t *m, pthread_mutexattr_t *a)
@@ -86,7 +109,7 @@ _pthread_mutex_unlock(pthread_mutex_t *m)
 
 	return (0);
 }
-
+#if 0
 int
 _pthread_rwlock_init(pthread_rwlock_t *l, pthread_rwlockattr_t *a)
 {
@@ -119,7 +142,7 @@ _pthread_rwlock_unlock(pthread_rwlock_t *l)
 
 	return (0);
 }
-
+#endif
 int
 _flockfile(FILE *fp)
 {
@@ -135,3 +158,94 @@ _funlockfile(FILE *fp)
 
 	return (0);
 }
+
+int
+_pthread_once(pthread_once_t *o, void (*r)(void))
+{
+	(void)o;
+	(void)r;
+
+	return (0);
+}
+
+int
+_pthread_cond_init(pthread_cond_t *c,const pthread_condattr_t *a)
+{
+	(void)c;
+	(void)a;
+
+	return (0);
+}
+
+int
+_pthread_cond_wait(pthread_cond_t *c, pthread_mutex_t *m)
+{
+	(void)c;
+	(void)m;
+
+	return (0);
+}
+
+int
+_pthread_cond_signal(pthread_cond_t *c)
+{
+	(void)c;
+	
+	return (0);
+}
+
+void 
+_pthread_spinlock(pthread_t t, pt_spin_t *l)
+{
+	(void)t;
+	(void)l;
+}
+
+void
+_pthread_spinunlock(pthread_t t, pt_spin_t *l)
+{
+	(void)t;
+	(void)l;
+}
+
+int
+_pthread_key_create(pthread_key_t *k, void (*d)(void *))
+{
+	(void)k;
+	(void)d;
+
+	return (0);
+}
+
+int
+_pthread_setspecific(pthread_key_t k, const void *v)
+{
+	(void)k;
+	(void)v;
+}
+
+void*
+_pthread_getspecific(pthread_key_t k)
+{
+	(void)k;
+	
+	return (0);
+}
+
+pthread_t
+_pthread_self(void)
+{
+
+	return (0);
+}
+
+int
+_pthread_sigmask(int h, const sigset_t *s, sigset_t *o)
+{
+	(void)h;
+	(void)s;
+	(void)0;
+
+	return (0);
+}
+
