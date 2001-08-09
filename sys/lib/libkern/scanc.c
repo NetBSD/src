982b26557ff73a988ba0f756cf6e3401b0a532f9
lukem <lukem@NetBSD.org>
2001-08-09 08:03:34 +0000

    convert to ansi knf, and fix a bug where the last arg was incorrectly 'u_char' instead of 'int' (per the prototype and usage and other scanc implementations)

--- sys/lib/libkern/scanc.c
+++ sys/lib/libkern/scanc.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: scanc.c,v 1.5 2000/03/30 12:19:46 augustss Exp $	*/
+/*	$NetBSD: scanc.c,v 1.6 2001/08/09 08:03:34 lukem Exp $	*/
 
 /*
  * Copyright (c) 1982, 1986, 1989 Regents of the University of California.
@@ -39,10 +39,7 @@
 #include <lib/libkern/libkern.h>
 
 int
-scanc(size, cp, table, mask)
-	u_int size;
-	const u_char *cp, table[];
-	u_char mask;
+scanc(u_int size, const u_char *cp, const u_char table[], int mask)
 {
 	const u_char *end = &cp[size];
 
