2938fa5186b516819557bd9ec9e903cfd3020a93
tv <tv@NetBSD.org>
2001-08-14 09:31:42 +0000

    Simplify some MACHINE_ARCH transforms, and redefine MACHINE_GNU_PLATFORM as the full GNU triplet name.

--- share/mk/bsd.own.mk
+++ share/mk/bsd.own.mk
@@ -1,4 +1,4 @@
-#	$NetBSD: bsd.own.mk,v 1.171 2001/08/14 07:02:13 tv Exp $
+#	$NetBSD: bsd.own.mk,v 1.172 2001/08/14 09:31:42 tv Exp $
 
 .if !defined(_BSD_OWN_MK_)
 _BSD_OWN_MK_=1
@@ -132,26 +132,11 @@ CFLAGS+=-Wno-format -fno-builtin
 SHLIB_VERSION_FILE?= ${.CURDIR}/shlib_version
 
 # GNU sources and packages sometimes see architecture names differently.
-# This table maps an architecture name to its GNU counterpart.
-# Use as so:  ${GNU_ARCH.${TARGET_ARCH}} or ${MACHINE_GNU_ARCH}
-.ifndef MACHINE_GNU_ARCH
-GNU_ARCH.alpha=alpha
 GNU_ARCH.arm26=arm
 GNU_ARCH.arm32=arm
-GNU_ARCH.arm=arm
-GNU_ARCH.i386=i386
-GNU_ARCH.m68k=m68k
-GNU_ARCH.mipseb=mipseb
-GNU_ARCH.mipsel=mipsel
-GNU_ARCH.ns32k=ns32k
-GNU_ARCH.powerpc=powerpc
 GNU_ARCH.sh3eb=sh
 GNU_ARCH.sh3el=sh
-GNU_ARCH.sparc=sparc
-GNU_ARCH.sparc64=sparc64
-GNU_ARCH.vax=vax
-MACHINE_GNU_ARCH=${GNU_ARCH.${MACHINE_ARCH}}
-.endif
+MACHINE_GNU_ARCH=${GNU_ARCH.${MACHINE_ARCH}:U${MACHINE_ARCH}}
 
 # In order to identify NetBSD to GNU packages, we sometimes need
 # an "elf" tag for historically a.out platforms.
@@ -161,22 +146,17 @@ MACHINE_GNU_ARCH=${GNU_ARCH.${MACHINE_ARCH}}
      ${MACHINE_ARCH} == "m68k" || \
      ${MACHINE_ARCH} == "sparc" || \
      ${MACHINE_ARCH} == "vax")
-MACHINE_GNU_PLATFORM?= netbsdelf
+MACHINE_GNU_PLATFORM=${MACHINE_GNU_ARCH}--netbsdelf
 .else
-MACHINE_GNU_PLATFORM?= netbsd
+MACHINE_GNU_PLATFORM=${MACHINE_GNU_ARCH}--netbsd
 .endif
 
 # CPU model, derived from MACHINE_ARCH
 MACHINE_CPU=	${MACHINE_ARCH:C/mipse[bl]/mips/:S/arm26/arm/:S/arm32/arm/:C/sh3e[bl]/sh3/:S/m68000/m68k/}
 
-.if ${MACHINE_ARCH} == "mips"
+.if ${MACHINE_ARCH} == "mips" || ${MACHINE_ARCH} == "sh3"
 .BEGIN:
-	@echo Must set MACHINE_ARCH to one of mipseb or mipsel
-	@false
-.endif
-.if ${MACHINE_ARCH} == "sh3"
-.BEGIN:
-	@echo Must set MACHINE_ARCH to one of sh3eb or sh3el
+	@echo Must set MACHINE_ARCH to one of ${MACHINE_ARCH}eb or ${MACHINE_ARCH}el
 	@false
 .endif
 
