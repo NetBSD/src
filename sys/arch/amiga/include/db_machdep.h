9fa2e6124dea8b257535155b52eaaff787f53ce7
mhitch <mhitch@NetBSD.org>
2001-08-15 01:29:29 +0000

    ELF symbol support for DDB.

--- sys/arch/amiga/include/db_machdep.h
+++ sys/arch/amiga/include/db_machdep.h
@@ -1,8 +1,11 @@
-/*	$NetBSD: db_machdep.h,v 1.4 1994/10/26 02:06:03 cgd Exp $	*/
+/*	$NetBSD: db_machdep.h,v 1.5 2001/08/15 01:29:29 mhitch Exp $	*/
 
 #ifndef _MACHINE_DB_MACHDEP_H_
 #define _MACHINE_DB_MACHDEP_H_
 
 #include <m68k/db_machdep.h>
+
+#define DB_ELF_SYMBOLS
+#define DB_ELFSIZE	32
 
 #endif
