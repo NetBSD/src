e786900d9e40b54a32ddfbb52a31b552d4918e2a
fredette <fredette@NetBSD.org>
2001-08-16 23:45:12 +0000

    Added the crucial "packed" attribute to the type 8 frame structure.

--- sys/arch/m68k/include/frame.h
+++ sys/arch/m68k/include/frame.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: frame.h,v 1.17 2001/03/28 23:08:15 fredette Exp $	*/
+/*	$NetBSD: frame.h,v 1.18 2001/08/16 23:45:12 fredette Exp $	*/
 
 /*
  * Copyright (c) 1988 University of Utah.
@@ -95,7 +95,7 @@ struct frame {
 			u_short	f_irc;
 			u_short	f_maskpc;
 			u_short	f_iregs[15];
-		} F_fmt8;
+		} __attribute__((packed)) F_fmt8;
 
 		struct fmt9 {
 			u_int	f_iaddr;
