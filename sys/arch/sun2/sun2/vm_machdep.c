31e6c4fa484706e1c2ba02706934ed17fc00ddfb
chs <chs@NetBSD.org>
2001-08-19 18:08:30 +0000

    in vunmapbuf(), call pmap_*remove() explicitly since uvm_km_free_wakeup() will soon no longer do it for us.

--- sys/arch/sun2/sun2/vm_machdep.c
+++ sys/arch/sun2/sun2/vm_machdep.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: vm_machdep.c,v 1.3 2001/07/29 23:59:19 chs Exp $	*/
+/*	$NetBSD: vm_machdep.c,v 1.4 2001/08/19 18:08:31 chs Exp $	*/
 
 /*
  * Copyright (c) 1994, 1995 Gordon W. Ross
@@ -417,8 +417,8 @@ vunmapbuf(bp, len)
 	kva = m68k_trunc_page(bp->b_data);
 	off = (vm_offset_t)bp->b_data - kva;
 	len = m68k_round_page(off + len);
-
-	/* This will call pmap_remove() for us. */
+	pmap_remove(vm_map_pmap(kernel_map), kva, kva + len);
+	pmap_update();
 	uvm_km_free_wakeup(kernel_map, kva, len);
 	bp->b_data = bp->b_saveaddr;
 	bp->b_saveaddr = NULL;
