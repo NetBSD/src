ed5e27cc44f51f49245a79dc230a785bccb4b6a9
bjh21 <bjh21@NetBSD.org>
2001-08-25 14:45:59 +0000

    Increase the size of a jmp_buf when using ELF, since this will be our last chance to do so for a while.  We don't use the extra space yet, but VFP support is likely to need it.

--- sys/arch/arm/include/setjmp.h
+++ sys/arch/arm/include/setjmp.h
@@ -1,10 +1,14 @@
-/*	$NetBSD: setjmp.h,v 1.1 2001/01/10 19:02:07 bjh21 Exp $	*/
+/*	$NetBSD: setjmp.h,v 1.2 2001/08/25 14:45:59 bjh21 Exp $	*/
 
 /*
  * machine/setjmp.h: machine dependent setjmp-related information.
  */
 
+#ifdef __ELF__
+#define	_JBLEN	64		/* size, in longs, of a jmp_buf */
+#else
 #define	_JBLEN	29		/* size, in longs, of a jmp_buf */
+#endif
 
 /*
  * NOTE: The internal structure of a jmp_buf is *PRIVATE*
