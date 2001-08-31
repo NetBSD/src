ea2c1149ea0b0bf9a730d2b8d1a88836ee0ada55
simonb <simonb@NetBSD.org>
2001-08-31 03:46:03 +0000

    G/C the unused kernel-only CLK_TCK #define.
    XXX: does include/time.h still need <machine/limits.h>?

--- sys/arch/algor/include/limits.h
+++ sys/arch/algor/include/limits.h
@@ -1,12 +1,3 @@
-/*	$NetBSD: limits.h,v 1.1 2001/05/28 16:22:19 thorpej Exp $	*/
-
-#ifndef	_ALGOR_LIMITS_H_
-#define	_ALGOR_LIMITS_H_
+/*	$NetBSD: limits.h,v 1.2 2001/08/31 03:46:03 simonb Exp $	*/
 
 #include <mips/limits.h>
-
-#ifdef _KERNEL
-#define CLK_TCK		60		/* ticks per second */
-#endif
-
-#endif	/* !_ALGOR_LIMITS_H_ */
