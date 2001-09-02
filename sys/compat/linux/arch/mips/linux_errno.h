33a1d4acd644a52776803fa86b55676080df22d6
manu <manu@NetBSD.org>
2001-09-02 06:51:15 +0000

    Fixed indent problems

--- sys/compat/linux/arch/mips/linux_errno.h
+++ sys/compat/linux/arch/mips/linux_errno.h
@@ -1,4 +1,4 @@
-/* $NetBSD: linux_errno.h,v 1.1 2001/08/26 15:16:42 manu Exp $ */
+/* $NetBSD: linux_errno.h,v 1.2 2001/09/02 06:51:15 manu Exp $ */
 
 /*-
  * Copyright (c) 2001 The NetBSD Foundation, Inc.
@@ -50,74 +50,74 @@
 /* Linux/mips switches EDEADLK and ENOMSG. */
 #undef LINUX_EDEADLK
 #define LINUX_EDEADLK		45		
-#define LINUX_ENOMSG			35
+#define LINUX_ENOMSG		35
 
 /* 
  * From Linux's include/asm-mips/errno.h
  */
-#define LINUX_EIDRM			36
-#define LINUX_ECHRNG			37
+#define LINUX_EIDRM		36
+#define LINUX_ECHRNG		37
 #define LINUX_EL2NSYNC		38
-#define LINUX_EL3HLT			39
-#define LINUX_EL3RST			40
-#define LINUX_ELNRNG			41
+#define LINUX_EL3HLT		39
+#define LINUX_EL3RST		40
+#define LINUX_ELNRNG		41
 #define LINUX_EUNATCH		42
-#define LINUX_ENOCSI			43
-#define LINUX_EL2HLT			44
+#define LINUX_ENOCSI		43
+#define LINUX_EL2HLT		44
 /* LINUX_EDEADLK defined above */
-#define LINUX_ENOLCK			46
-#define LINUX_EBADE			50
-#define LINUX_EBADR			51
-#define LINUX_EXFULL			52
-#define LINUX_ENOANO			53
+#define LINUX_ENOLCK		46
+#define LINUX_EBADE		50
+#define LINUX_EBADR		51
+#define LINUX_EXFULL		52
+#define LINUX_ENOANO		53
 #define LINUX_EBADRQC		54
 #define LINUX_EBADSLT		55
 #define LINUX_EDEADLOCK		56
-#define LINUX_EBFONT			59
-#define LINUX_ENOSTR			60
+#define LINUX_EBFONT		59
+#define LINUX_ENOSTR		60
 #define LINUX_ENODATA		61
-#define LINUX_ETIME			62
-#define LINUX_ENOSR			63
-#define LINUX_ENONET			64
-#define LINUX_ENOPKG			65
+#define LINUX_ETIME		62
+#define LINUX_ENOSR		63
+#define LINUX_ENONET		64
+#define LINUX_ENOPKG		65
 #define LINUX_EREMOTE		66
 #define LINUX_ENOLINK		67
-#define LINUX_EADV			68
-#define LINUX_ESRMNT			69
-#define LINUX_ECOMM			70
-#define LINUX_EPROTO			71
+#define LINUX_EADV		68
+#define LINUX_ESRMNT		69
+#define LINUX_ECOMM		70
+#define LINUX_EPROTO		71
 #define LINUX_EDOTDOT		73
 #define LINUX_EMULTIHOP		74
 #define LINUX_EBADMSG		77
 #define LINUX_ENAMETOOLONG	78
 #define LINUX_EOVERFLOW		79
 #define LINUX_ENOTUNIQ		80
-#define LINUX_EBADFD			81
+#define LINUX_EBADFD		81
 #define LINUX_EREMCHG		82
 #define LINUX_ELIBACC		83
 #define LINUX_ELIBBAD		84
 #define LINUX_ELIBSCN		85
 #define LINUX_ELIBMAX		86
 #define LINUX_ELIBEXEC		87
-#define LINUX_EILSEQ			88
-#define LINUX_ENOSYS			89
-#define LINUX_ELOOP			90
+#define LINUX_EILSEQ		88
+#define LINUX_ENOSYS		89
+#define LINUX_ELOOP		90
 #define LINUX_ERESTART		91
 #define LINUX_ESTRPIPE		92
 #define LINUX_ENOTEMPTY		93
-#define LINUX_EUSERS			94
+#define LINUX_EUSERS		94
 #define LINUX_ENOTSOCK		95
 #define LINUX_EDESTADDRREQ	96
 #define LINUX_EMSGSIZE		97
 #define LINUX_EPROTOTYPE	98
 #define LINUX_ENOPROTOOPT	99
-#define LINUX_EPROTONOSUPPORT		120
-#define LINUX_ESOCKTNOSUPPORT		121
+#define LINUX_EPROTONOSUPPORT	120
+#define LINUX_ESOCKTNOSUPPORT	121
 #define LINUX_EOPNOTSUPP	122
 #define LINUX_EPFNOSUPPORT	123
 #define LINUX_EAFNOSUPPORT	124
 #define LINUX_EADDRINUSE	125
-#define LINUX_EADDRNOTAVAIL		126
+#define LINUX_EADDRNOTAVAIL	126
 #define LINUX_ENETDOWN		127
 #define LINUX_ENETUNREACH	128
 #define LINUX_ENETRESET		129
@@ -129,9 +129,9 @@
 #define LINUX_EUCLEAN		135
 #define LINUX_ENOTNAM		137
 #define LINUX_ENAVAIL		138
-#define LINUX_EISNAM			139
+#define LINUX_EISNAM		139
 #define LINUX_EREMOTEIO		140
-#define LINUX_EINIT			141
+#define LINUX_EINIT		141
 #define LINUX_EREMDEV		142
 #define LINUX_ESHUTDOWN		143
 #define LINUX_ETOOMANYREFS	144
@@ -142,12 +142,12 @@
 #define LINUX_EWOULDBLOCK	LINUX_EAGAIN
 #define LINUX_EALREADY		149
 #define LINUX_EINPROGRESS	150
-#define LINUX_ESTALE			151
+#define LINUX_ESTALE		151
 #define LINUX_ECANCELED		158
 /* linux/include/asm-mips/errno.h states theses are Linux extensions */
 #define LINUX_ENOMEDIUM		159
 #define LINUX_EMEDIUMTYPE	160
-#define LINUX_EDQUOT			1133
+#define LINUX_EDQUOT		1133
 /* Biggest errno */			
 #define LINUX_EMAXERRNO		1133
 			
