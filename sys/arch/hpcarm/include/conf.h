44a5a6e5c1bb1911ea9ab4ab32b16af031c0d237
wiz <wiz@NetBSD.org>
2001-09-04 21:45:19 +0000

    Fix typos (CONS instead of CONF, and specifc instead of specific).

--- sys/arch/hpcarm/include/conf.h
+++ sys/arch/hpcarm/include/conf.h
@@ -2,11 +2,11 @@
 #define	_HPCARM_CONF_H
 
 /*
- * HPCARM specifc device includes go in here
+ * HPCARM specific device includes go in here
  */
-#define	CONS_HAVE_SCSIPI
-#define	CONS_HAVE_WSCONS
-#define	CONS_HAVE_USB
+#define	CONF_HAVE_SCSIPI
+#define	CONF_HAVE_WSCONS
+#define	CONF_HAVE_USB
 
 #include "biconsdev.h"
 
