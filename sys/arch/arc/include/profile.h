6bd60e9dd9658df242a16cea39885ef9dee40787
simonb <simonb>
2001-09-04 07:12:28 +0000

    Remove some more useless RCS ids.

--- sys/arch/arc/include/profile.h
+++ sys/arch/arc/include/profile.h
@@ -1,4 +1,3 @@
-/*	$NetBSD: profile.h,v 1.4 2000/01/23 21:01:58 soda Exp $	*/
-/*      $OpenBSD: profile.h,v 1.1.1.1 1996/06/24 09:07:18 pefo Exp $	*/
+/*	$NetBSD: profile.h,v 1.5 2001/09/04 07:12:28 simonb Exp $	*/
 
 #include <mips/profile.h>
