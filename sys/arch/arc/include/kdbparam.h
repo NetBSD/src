edb93854f985069c7c193f2a294ed4832ca8268d
simonb <simonb@NetBSD.org>
2001-09-04 07:22:03 +0000

    More RCS ID cleanup.

--- sys/arch/arc/include/kdbparam.h
+++ sys/arch/arc/include/kdbparam.h
@@ -1,5 +1,3 @@
-/*	$NetBSD: kdbparam.h,v 1.4 2000/01/23 21:01:57 soda Exp $	*/
-/*	$OpenBSD: kdbparam.h,v 1.1.1.1 1996/06/24 09:07:17 pefo Exp $	*/
-/*	NetBSD: kdbparam.h,v 1.4 1994/10/26 21:09:42 cgd Exp 	*/
+/*	$NetBSD: kdbparam.h,v 1.5 2001/09/04 07:22:03 simonb Exp $	*/
 
 #include <mips/kdbparam.h>
