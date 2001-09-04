3839989a12904766358dbb4a762950f82e244ea6
simonb <simonb@NetBSD.org>
2001-09-04 06:24:54 +0000

    Remove unneeded RCS header.

--- sys/arch/arc/include/ecoff_machdep.h
+++ sys/arch/arc/include/ecoff_machdep.h
@@ -1,4 +1,3 @@
-/*	$NetBSD: ecoff_machdep.h,v 1.5 2000/01/23 21:01:56 soda Exp $	*/
-/*	$OpenBSD: ecoff.h,v 1.3 1996/10/02 21:47:53 pefo Exp $	*/
+/*	$NetBSD: ecoff_machdep.h,v 1.6 2001/09/04 06:24:54 simonb Exp $	*/
 
 #include <mips/ecoff_machdep.h>
