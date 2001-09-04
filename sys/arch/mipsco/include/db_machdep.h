18613b8689d94aa76cb52ffe62530e49bfe61eb0
simonb <simonb@NetBSD.org>
2001-09-04 07:43:29 +0000

    Standardise the format of MIPS' <machine/db_machdep.h>.

--- sys/arch/mipsco/include/db_machdep.h
+++ sys/arch/mipsco/include/db_machdep.h
@@ -1,6 +1,11 @@
-/*	$NetBSD: db_machdep.h,v 1.1 2000/08/12 22:58:12 wdk Exp $	*/
+/*	$NetBSD: db_machdep.h,v 1.2 2001/09/04 07:43:30 simonb Exp $	*/
+
+#ifndef _MIPSCO_DB_MACHDEP_H_
+#define _MIPSCO_DB_MACHDEP_H_
 
 #define DB_ELF_SYMBOLS
-#define DB_ELFSIZE 32
+#define DB_ELFSIZE	32
 
 #include <mips/db_machdep.h>
+
+#endif	/* !_MIPSCO_DB_MACHDEP_H_ */
