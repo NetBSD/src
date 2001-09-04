2742722cfea73e71b84814e89e37221a09fc8dc2
simonb <simonb@NetBSD.org>
2001-09-04 06:26:18 +0000

    Clean up.

--- sys/arch/arc/include/ieeefp.h
+++ sys/arch/arc/include/ieeefp.h
@@ -1,4 +1,3 @@
-/*	$NetBSD: ieeefp.h,v 1.5 2000/01/23 21:01:56 soda Exp $	*/
-/*	$OpenBSD: ieeefp.h,v 1.1.1.1 1996/06/24 09:07:17 pefo Exp $	*/
+/*	$NetBSD: ieeefp.h,v 1.6 2001/09/04 06:26:18 simonb Exp $	*/
 
 #include <mips/ieeefp.h>
