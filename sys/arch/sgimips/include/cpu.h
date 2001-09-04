20dc9237e13b91d742b82d919babd7e066cf5255
simonb <simonb@NetBSD.org>
2001-09-04 06:19:21 +0000

    Centralise struct cpu_info declaration and related info to <mips/cpu.h>.

--- sys/arch/sgimips/include/cpu.h
+++ sys/arch/sgimips/include/cpu.h
@@ -1,31 +1,4 @@
-/*	$NetBSD: cpu.h,v 1.2 2001/05/30 12:28:50 mrg Exp $	*/
-
-#ifndef _SGIMIPS_CPU_H
-#define _SGIMIPS_CPU_H
+/*	$NetBSD: cpu.h,v 1.3 2001/09/04 06:19:23 simonb Exp $	*/
 
 #include <mips/cpu.h>
 #include <mips/cpuregs.h>
-
-#ifndef _LOCORE
-#if defined(_KERNEL_OPT)
-#include "opt_lockdebug.h"
-#endif
-
-#include <sys/sched.h>
-struct cpu_info {
-	struct schedstate_percpu ci_schedstate; /* scheduler state */
-#if defined(DIAGNOSTIC) || defined(LOCKDEBUG)
-	u_long ci_spin_locks;		/* # of spin locks held */
-	u_long ci_simple_locks;		/* # of simple locks held */
-#endif
-};
-
-#ifdef _KERNEL
-extern struct cpu_info cpu_info_store;
-
-#define	cpu_number()	(0)
-#define	curcpu()	(&cpu_info_store)
-#endif
-#endif /* !_LOCORE */
-
-#endif  /* !_SGIMIPS_CPU_H_ */
