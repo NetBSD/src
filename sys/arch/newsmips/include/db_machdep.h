384f2d28ab1a03c1f4f2dd616c076e8ff11b03f9
simonb <simonb>
2001-09-04 07:43:29 +0000

    Standardise the format of MIPS' <machine/db_machdep.h>.

--- sys/arch/newsmips/include/db_machdep.h
+++ sys/arch/newsmips/include/db_machdep.h
@@ -1,6 +1,11 @@
-/*	$NetBSD: db_machdep.h,v 1.2 1999/03/26 07:03:01 tsubai Exp $	*/
+/*	$NetBSD: db_machdep.h,v 1.3 2001/09/04 07:43:30 simonb Exp $	*/
+
+#ifndef _NEWSMIPS_DB_MACHDEP_H_
+#define _NEWSMIPS_DB_MACHDEP_H_
 
 #define DB_ELF_SYMBOLS
-#define DB_ELFSIZE 32
+#define DB_ELFSIZE	32
 
 #include <mips/db_machdep.h>
+
+#endif	/* !_NEWSMIPS_DB_MACHDEP_H_ */
