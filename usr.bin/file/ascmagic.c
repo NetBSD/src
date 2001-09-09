7402f5537a5243c624a138e031d40e3579846204
pooka <pooka>
2001-09-09 10:38:45 +0000

    file 3.37

--- usr.bin/file/ascmagic.c
+++ usr.bin/file/ascmagic.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: ascmagic.c,v 1.1.1.6 2000/11/23 23:07:24 pooka Exp $	*/
+/*	$NetBSD: ascmagic.c,v 1.1.1.7 2001/09/09 10:38:48 pooka Exp $	*/
 
 /*
  * ASCII magic -- file types that we know based on keywords
@@ -48,7 +48,7 @@
 #include "names.h"
 
 #ifndef	lint
-FILE_RCSID("@(#)Id: ascmagic.c,v 1.29 2000/08/05 19:00:11 christos Exp ")
+FILE_RCSID("@(#)Id: ascmagic.c,v 1.30 2001/07/26 13:15:49 christos Exp ")
 #endif	/* lint */
 
 typedef unsigned long unichar;
@@ -107,9 +107,12 @@ ascmagic(buf, nbytes)
 		return 1;
 	}
 
-	/* Undo the NUL-termination kindly provided by process() */
+	/*
+	 * Undo the NUL-termination kindly provided by process()
+	 * but leave at least one byte to look at
+	 */
 
-	while (nbytes > 0 && buf[nbytes - 1] == '\0')
+	while (nbytes > 1 && buf[nbytes - 1] == '\0')
 		nbytes--;
 
 	/*
