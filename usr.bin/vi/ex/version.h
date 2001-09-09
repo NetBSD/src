4fa1ec90e5706e7d1a8ae1a37d466c9cf668d721
aymeric <aymeric@NetBSD.org>
2001-09-09 11:04:23 +0000

    bump to nb2 after fixing PR #10367 and correcting another related bug

--- usr.bin/vi/ex/version.h
+++ usr.bin/vi/ex/version.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: version.h,v 1.4 2001/08/28 07:36:42 aymeric Exp $	*/
+/*	$NetBSD: version.h,v 1.5 2001/09/09 11:04:23 aymeric Exp $	*/
 
 #define	VI_VERSION \
-    "Version 1.79nb1 (10/23/96) The CSRG, University of California, Berkeley."
+    "Version 1.79nb2 (10/23/96) The CSRG, University of California, Berkeley."
