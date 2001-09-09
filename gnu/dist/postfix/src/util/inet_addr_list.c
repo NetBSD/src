2952765cdae72d8451f8d568ebbfaa416aafddfe
itojun <itojun@NetBSD.org>
2001-09-09 02:52:44 +0000

    compare IPv6 address correctly, for net_interfaces directive. KAME PR 382.

--- gnu/dist/postfix/src/util/inet_addr_list.c
+++ gnu/dist/postfix/src/util/inet_addr_list.c
@@ -75,7 +75,7 @@ void    inet_addr_list_init(INET_ADDR_LIST *list)
 #else
     list->addrs = (struct in_addr *)
 #endif
-	mymalloc(sizeof(*list->addrs) * list->size);
+    mymalloc(sizeof(*list->addrs) * list->size);
 }
 
 /* inet_addr_list_append - append address to internet address list */
@@ -129,10 +129,36 @@ void    inet_addr_list_append(INET_ADDR_LIST *list, struct in_addr * addr)
 
 static int inet_addr_list_comp(const void *a, const void *b)
 {
+#ifdef INET6
+    char h1[NI_MAXHOST], h2[NI_MAXHOST];
+#ifdef NI_WITHSCOPEID
+    const int niflags = NI_NUMERICHOST | NI_WITHSCOPEID;
+#else
+    const int niflags = NI_NUMERICHOST;
+#endif
+    int alen, blen;
+
+#ifndef HAS_SA_LEN
+    alen = SA_LEN((struct sockaddr *)a);
+    blen = SA_LEN((struct sockaddr *)b);
+#else
+    alen = ((struct sockaddr *)a)->sa_len;
+    blen = ((struct sockaddr *)b)->sa_len;
+#endif
+
+    if (getnameinfo((struct sockaddr *)a, alen, h1, sizeof(h1), NULL, 0,
+	niflags) == 0 &&
+        getnameinfo((struct sockaddr *)b, blen, h2, sizeof(h2), NULL, 0,
+	niflags) == 0) {
+	return strcmp(h1, h2);
+    } else
+	return 0;	/*error*/
+#else
     const struct in_addr *a_addr = (const struct in_addr *) a;
     const struct in_addr *b_addr = (const struct in_addr *) b;
 
     return (a_addr->s_addr - b_addr->s_addr);
+#endif
 }
 
 /* inet_addr_list_uniq - weed out duplicates */
