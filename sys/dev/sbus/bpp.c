51ac649d65dac44f2b318fe3d89397b7a4911f71
thorpej <thorpej@NetBSD.org>
2001-09-12 17:33:25 +0000

    Add kqueue support (not compiled yet).

--- sys/dev/sbus/bpp.c
+++ sys/dev/sbus/bpp.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: bpp.c,v 1.8 2000/11/01 06:17:32 eeh Exp $ */
+/*	$NetBSD: bpp.c,v 1.8.4.1 2001/09/12 17:33:25 thorpej Exp $ */
 
 /*-
  * Copyright (c) 1998 The NetBSD Foundation, Inc.
@@ -503,6 +503,86 @@ bpppoll(dev, events, p)
 	return (revents);
 }
 
+static void
+filt_bpprdetach(struct knote *kn)
+{
+	struct bpp_softc *sc = (void *) kn->kn_hook;
+	int s;
+
+	s = splbpp();
+	SLIST_REMOVE(&sc->sc_rsel.si_klist, kn, knote, kn_selnext);
+	splx(s);
+}
+
+static int
+filt_bppread(struct knote *kn, long hint)
+{
+	struct bpp_softc *sc = (void *) kn->kn_hook;
+
+	/* XXX Read not yet implemented. */
+	return (0);
+}
+
+static const struct filterops bppread_filtops =
+	{ 1, NULL, filt_bpprdetach, filt_bppread };
+
+static void
+filt_bppwdetach(struct knote *kn)
+{
+	struct bpp_softc *sc = (void *) kn->kn_hook;
+	int s;
+
+	s = splbpp();
+	SLIST_REMOVE(&sc->sc_wsel.si_klist, kn, knote, kn_selnext);
+	splx(s);
+}
+
+static int
+filt_bpfwrite(struct knote *kn, long hint)
+{
+	struct bpp_softc *sc = (void *) kn->kn_hook;
+
+	if (sc->sc_flags & BPP_LOCKED)
+		return (0);
+
+	kn->kn_data = 0;	/* XXXLUKEM (thorpej): what to put here? */
+	return (1);
+}
+
+static const struct filterops bppwrite_filtops =
+	{ 1, NULL, filt_bppwdetach, filt_bpfwrite };
+
+int
+bppkqfilter(dev_t dev, struct knote *kn)
+{
+	struct bpp_softc *sc = bpp_cd.cd_devs[BPPUNIT(dev)];
+	struct klist *klist;
+	int s;
+
+	switch (kn->kn_filter) {
+	case EVFILT_READ:
+		klist = &sc->sc_rsel.si_klist;
+		kn->kn_fop = &bppread_filtops;
+		break;
+
+	case EVFILT_WRITE:
+		klist = &sc->sc_wsel.si_klist;
+		kn->kn_fop = &bppwrite_filtops;
+		break;
+
+	default:
+		return (1);
+	}
+
+	kn->kn_hook = (void *) sc;
+
+	s = splbpp();
+	SLIST_INSERT_HEAD(klist, kn, kn_selnext);
+	splx(s);
+
+	return (0);
+}
+
 int
 bppintr(arg)
 	void *arg;
@@ -531,7 +611,7 @@ bppintr(arg)
 		sc->sc_flags &= ~BPP_WANT;
 		wakeup(sc->sc_buf);
 	} else {
-		selwakeup(&sc->sc_wsel);
+		selnotify(&sc->sc_wsel, 0);
 		if (sc->sc_asyncproc != NULL)
 			psignal(sc->sc_asyncproc, SIGIO);
 	}
