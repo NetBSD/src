e111f0b92edb320933d186bd1a55a7f81276394f
manu <manu@NetBSD.org>
2001-09-16 07:14:15 +0000

    Added prototypes declarations for lower part of root-only time-related system calls. This is the the clockctl pseudosriver

--- sys/sys/time.h
+++ sys/sys/time.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: time.h,v 1.31 2001/04/30 01:13:21 lukem Exp $	*/
+/*	$NetBSD: time.h,v 1.32 2001/09/16 07:14:15 manu Exp $	*/
 
 /*
  * Copyright (c) 1982, 1986, 1993
@@ -165,6 +165,9 @@ void	microtime __P((struct timeval *tv));
 int	settime __P((struct timeval *));
 int	ratecheck __P((struct timeval *, const struct timeval *));
 int	ppsratecheck __P((struct timeval *, int *, int));
+int	settimeofday1 __P((struct timeval *, struct timezone *, struct proc *));
+int	adjtime1 __P((struct timeval *, struct timeval *, struct proc *));
+int	clock_settime1 __P((clockid_t, struct timespec *));
 #else /* !_KERNEL */
 
 #ifndef _STANDALONE
