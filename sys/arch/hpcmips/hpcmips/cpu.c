6a9bbffc4d14925cb9fced059c4ae114f02845ac
uch <uch@NetBSD.org>
2001-09-16 15:45:42 +0000

    rearrange struct platform@sysconf.h suitable for hpcmips. overhaul hpcmips/hpcmips/. no actual changes.

--- sys/arch/hpcmips/hpcmips/cpu.c
+++ sys/arch/hpcmips/hpcmips/cpu.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: cpu.c,v 1.9 2001/09/15 14:08:15 uch Exp $	*/
+/*	$NetBSD: cpu.c,v 1.10 2001/09/16 15:45:43 uch Exp $	*/
 /*-
  * Copyright (c) 1999 Shin Takemura, All rights reserved.
  * Copyright (c) 1999-2001 SATO Kazumi, All rights reserved.
@@ -55,16 +55,12 @@
  * rights to redistribute these changes.
  */
 
-#include "opt_vr41xx.h"
-#include "opt_tx39xx.h"
-
 #include <sys/param.h>
 #include <sys/systm.h>
 
+#include <machine/sysconf.h>
 #include <machine/bus.h>
 #include <machine/autoconf.h>
-#include <machine/platid.h>
-#include <machine/platid_mask.h>
 
 /* Definition of the driver for autoconfig. */
 static int	cpumatch(struct device *, struct cfdata *, void *);
@@ -76,21 +72,13 @@ struct cfattach cpu_ca = {
 
 extern struct cfdriver cpu_cd;
 
-extern void cpu_identify(void);
-extern void vr_idle(void);
-
-
 static int
 cpumatch(struct device *parent, struct cfdata *cf, void *aux)
 {
 	struct mainbus_attach_args *ma = aux;
 
-
 	/* make sure that we're looking for a CPU. */
-	if (strcmp(ma->ma_name, cpu_cd.cd_name) != 0) {
-		return (0);
-	}
-	return (1);
+	return (strcmp(ma->ma_name, cpu_cd.cd_name) != 0 ? 0 : 1);
 }
 
 static void
@@ -101,22 +89,7 @@ cpuattach(struct device *parent, struct device *dev, void *aux)
 
 	cpu_identify();
 
-#ifdef VR41XX
-#ifdef TX39XX
-	if (platid_match(&platid, &platid_mask_CPU_MIPS_VR_41XX))
-#endif /* TX39XX */
-	{
-		printf("cpu0: install VR specific idle routine\n");
-		CPU_IDLE = (long *)vr_idle;
-	}
-#endif
-#ifdef TX39XX
-#ifdef VR41XX
-	if (platid_match(&platid, &platid_mask_CPU_MIPS_TX_3900)
-	    || platid_match(&platid, &platid_mask_CPU_MIPS_TX_3920))
-#endif /* VR41XX */
-	{
-		; /* XXXX: currenty not implemented */
-	}
-#endif
+	/* install CPU specific idle routine if any. */
+	if (platform.cpu_idle != NULL)
+		CPU_IDLE = (long *)platform.cpu_idle;
 }
