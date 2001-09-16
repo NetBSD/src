ad20a42535925592094c4f94008f83e3792351c5
tsutsui <tsutsui>
2001-09-16 03:20:01 +0000

    Wrap some declarations with #ifdef _KERNEL and #ifndef _LOCORE .

--- sys/arch/newsmips/include/cpu.h
+++ sys/arch/newsmips/include/cpu.h
@@ -1,13 +1,17 @@
-/*	$NetBSD: cpu.h,v 1.12 2001/09/04 06:23:17 simonb Exp $	*/
+/*	$NetBSD: cpu.h,v 1.13 2001/09/16 03:20:01 tsutsui Exp $	*/
 
 #ifndef _NEWSMIPS_CPU_H_
 #define _NEWSMIPS_CPU_H_
 
 #include <mips/cpu.h>
 
+#ifdef _KERNEL
+#ifndef _LOCORE
 extern int systype;
 
 #define NEWS3400	1
 #define NEWS5000	2
+#endif
+#endif
 
 #endif /* _NEWSMIPS_CPU_H_ */
