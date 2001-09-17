a7357fecf41feecfe7405bf7899ead4a88ed8b40
jdolecek <jdolecek@NetBSD.org>
2001-09-17 18:59:29 +0000

    soreceive(): do not ignore uiomove() error Problem reported and fix provided by Aaro Koskinen in kern/11692.

--- sys/kern/uipc_socket.c
+++ sys/kern/uipc_socket.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: uipc_socket.c,v 1.56 2001/04/13 23:30:10 thorpej Exp $	*/
+/*	$NetBSD: uipc_socket.c,v 1.57 2001/09/17 18:59:29 jdolecek Exp $	*/
 
 /*
  * Copyright (c) 1982, 1986, 1988, 1990, 1993
@@ -736,6 +736,8 @@ soreceive(struct socket *so, struct mbuf **paddr, struct uio *uio,
 			splx(s);
 			error = uiomove(mtod(m, caddr_t) + moff, (int)len, uio);
 			s = splsoftnet();
+			if (error)
+				goto release;
 		} else
 			uio->uio_resid -= len;
 		if (len == m->m_len - moff) {
