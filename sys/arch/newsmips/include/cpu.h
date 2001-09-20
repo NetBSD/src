f6c576a4b54bb37e3c5a54ed072324ba1e88926d
tsutsui <tsutsui@NetBSD.org>
2001-09-20 12:29:49 +0000

    bootloader requires systype values so change #ifdef _KERNEL -> #if defined(_KERNEL) || defined(_STANDALONE)

---
+++ sys/arch/newsmips/include/cpu.h
@@ -0,0 +1,17 @@
+/*	$NetBSD: cpu.h,v 1.14.4.2 2001/09/20 12:29:49 tsutsui Exp $	*/
+
+#ifndef _NEWSMIPS_CPU_H_
+#define _NEWSMIPS_CPU_H_
+
+#include <mips/cpu.h>
+
+#if defined(_KERNEL) || defined(_STANDALONE)
+#ifndef _LOCORE
+extern int systype;
+
+#define NEWS3400	1
+#define NEWS5000	2
+#endif
+#endif
+
+#endif /* _NEWSMIPS_CPU_H_ */
