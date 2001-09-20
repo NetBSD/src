4621b03c753603aada7eef08cb6d3bcf83827510
bjh21 <bjh21@NetBSD.org>
2001-09-20 21:54:11 +0000

    Re-organise eca(4)'s FIQ handlers.  Now we have a single FIQ handler copied to zero page, and it branches to either the rx or tx handler.  My intention is that the tx handler should switch to the rx one the moment it finishes, without needing a downgrade, which I hope will get rid of the start-of-frame rx overruns I'm seeing.
    While I'm here, move the constants describing the shape of the FIQ state
    structures out of if_ecavar.h and into genassym.cf where they belong.

--- sys/arch/arm26/ioc/if_ecavar.h
+++ sys/arch/arm26/ioc/if_ecavar.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: if_ecavar.h,v 1.2 2001/09/15 17:27:25 bjh21 Exp $	*/
+/*	$NetBSD: if_ecavar.h,v 1.3 2001/09/20 21:54:11 bjh21 Exp $	*/
 
 /*-
  * Copyright (c) 2001 Ben Harris
@@ -31,17 +31,22 @@
 #define _ECAVAR_H_
 
 #ifndef _LOCORE
+#include <sys/device.h>
+#include <sys/socket.h>
+
 #include <net/if.h>
 #include <net/if_eco.h>
 
-struct eca_rxstate {
-	struct mbuf	*erx_curmbuf;
-	u_int32_t	erx_flags;
-	u_int8_t	erx_myaddr;
-};
+#include <machine/bus.h>
+#include <machine/fiq.h>
 
-struct eca_txstate {
-	struct mbuf	*etx_curmbuf;
+struct eca_fiqstate {
+	void		*efs_fiqhandler;
+	struct mbuf	*efs_rx_curmbuf;
+	struct fiq_regs	efs_rx_fiqregs;
+	u_int32_t	efs_rx_flags;
+	u_int8_t	efs_rx_myaddr;
+	struct mbuf	*efs_tx_curmbuf;
 };
 
 struct eca_softc {
@@ -53,27 +58,22 @@ struct eca_softc {
 	u_int8_t	sc_cr4;
 	bus_space_tag_t sc_iot;
 	bus_space_handle_t sc_ioh;
-	struct mbuf	*sc_rcvmbuf;
 	void		*sc_rx_soft;
-	struct eca_rxstate sc_rxstate;
-	u_int8_t	sc_sr2;
+	struct eca_fiqstate sc_fiqstate;
+	struct mbuf	*sc_rcvmbuf;
 	struct mbuf	*sc_txmbuf;
+	u_int8_t	sc_sr2;
 	void		*sc_tx_soft;
-	struct eca_txstate sc_txstate;
 	int		sc_transmitting;
 };
 
-extern char eca_fiqhandler_rx[], eca_efiqhandler_rx[];
-extern char eca_fiqhandler_tx[], eca_efiqhandler_tx[];
+extern char eca_fiqhandler[], eca_efiqhandler[];
+extern char eca_fiqhandler_rx[], eca_fiqhandler_tx[];
 
 #endif
-/* Sync with rxstate above */
-#define ERX_CURMBUF	0
-#define ERX_FLAGS	4
+
+/* Flags for efs_rx_flags. */
 #define ERXF_GOTHDR	0x01
 #define ERXF_FLAGFILL	0x02
-#define ERX_MYADDR	8
-/* Sync with txstate above */
-#define ETX_CURMBUF	0
 
 #endif
