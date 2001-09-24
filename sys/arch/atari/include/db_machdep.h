b122c9d45b6848d2bbec88126b13eafd781ab88e
leo <leo@NetBSD.org>
2001-09-24 09:21:24 +0000

    Enable ELF symbols in ddb.

--- sys/arch/atari/include/db_machdep.h
+++ sys/arch/atari/include/db_machdep.h
@@ -1,8 +1,11 @@
-/*	$NetBSD: db_machdep.h,v 1.1.1.1 1995/03/26 07:12:06 leo Exp $	*/
+/*	$NetBSD: db_machdep.h,v 1.2 2001/09/24 09:21:24 leo Exp $	*/
 
 #ifndef _MACHINE_DB_MACHDEP_H_
 #define _MACHINE_DB_MACHDEP_H_
 
 #include <m68k/db_machdep.h>
+
+#define	DB_ELF_SYMBOLS
+#define	DB_ELFSIZE	32
 
 #endif
