dfa4ab7a70a1cfdeffe9636d062b606612d6a3b4
ad <ad@NetBSD.org>
2001-09-27 18:43:37 +0000

    Make firmware updates work with dpti.

--- sys/dev/i2o/dpti.c
+++ sys/dev/i2o/dpti.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: dpti.c,v 1.1 2001/09/21 23:44:20 ad Exp $	*/
+/*	$NetBSD: dpti.c,v 1.2 2001/09/27 18:43:37 ad Exp $	*/
 
 /*-
  * Copyright (c) 2001 The NetBSD Foundation, Inc.
@@ -76,16 +76,18 @@
 #include <sys/conf.h>
 #include <sys/ioctl.h>
 
+#include <uvm/uvm_extern.h>
+
 #include <machine/bus.h>
+#ifdef i386
+#include <machine/pio.h>
+#endif
 
 #include <dev/i2o/i2o.h>
 #include <dev/i2o/i2odpt.h>
 #include <dev/i2o/iopio.h>
 #include <dev/i2o/iopvar.h>
 #include <dev/i2o/dptivar.h>
-
-#include <compat/linux/common/linux_types.h>
-#include <compat/linux/common/linux_ioctl.h>
 
 #ifdef I2ODEBUG
 #define	DPRINTF(x)		printf x
@@ -94,13 +96,40 @@
 #endif
 
 static struct dpt_sig dpti_sig = {
-	{ 'd', 'P', 't', 'S', 'i', 'G'}, SIG_VERSION, PROC_INTEL,
-	PROC_386 | PROC_486 | PROC_PENTIUM | PROC_SEXIUM, FT_HBADRVR, 0,
-	OEM_DPT, OS_FREE_BSD, CAP_ABOVE16MB, DEV_ALL,
+	{ 'd', 'P', 't', 'S', 'i', 'G'},
+	SIG_VERSION,
+#if defined(i386)
+	PROC_INTEL,
+#elif defined(powerpc)
+	PROC_POWERPC,
+#elif defined(alpha)
+	PROC_ALPHA,
+#elif defined(mips)
+	PROC_MIPS,
+#elif defined(sparc64)
+	PROC_ULTRASPARC,
+#endif
+#if defined(i386)
+	PROC_386 | PROC_486 | PROC_PENTIUM | PROC_SEXIUM,
+#else
+	0,
+#endif
+	FT_HBADRVR,
+	0,
+	OEM_DPT,
+	OS_FREE_BSD,	/* XXX */
+	CAP_ABOVE16MB,
+	DEV_ALL,
 	ADF_ALL_SC5,
-	0, 0, DPTI_VERSION, DPTI_REVISION, DPTI_SUBREVISION,
-	DPTI_MONTH, DPTI_DAY, DPTI_YEAR,
-	""
+	0,
+	0,
+	DPTI_VERSION,
+	DPTI_REVISION,
+	DPTI_SUBREVISION,
+	DPTI_MONTH,
+	DPTI_DAY,
+	DPTI_YEAR,
+	""		/* Will be filled later */
 };
 
 void	dpti_attach(struct device *, struct device *, void *);
@@ -155,7 +184,7 @@ dpti_attach(struct device *parent, struct device *self, void *aux)
 	 * Tell the world what we are.  The description in the signature
 	 * must be no more than 46 bytes long (see dptivar.h).
 	 */
-	printf(": DPT/Adaptec SCSI RAID management interface\n");
+	printf(": DPT/Adaptec RAID management interface\n");
 	sprintf(dpti_sig.dsDescription, "NetBSD %s I2O OSM", osrelease);
 
 	rv = iop_field_get_all(iop, I2O_TID_IOP,
@@ -192,7 +221,7 @@ dptiioctl(dev_t dev, u_long cmd, caddr_t data, int flag, struct proc *p)
 	struct iop_softc *iop;
 	struct dpti_softc *sc;
 	struct ioctl_pt *pt;
-	int i, size;
+	int i, size, rv;
 
 	sc = device_lookup(&dpti_cd, minor(dev));
 	iop = (struct iop_softc *)sc->sc_dv.dv_parent;
@@ -242,12 +271,19 @@ dptiioctl(dev_t dev, u_long cmd, caddr_t data, int flag, struct proc *p)
 		return (EIO);
 
 	case DPT_I2OUSRCMD:
-		return (dpti_passthrough(sc, data, p));
+		if (sc->sc_nactive++ >= 2)
+			tsleep(&sc->sc_nactive, PRIBIO, "dptislp", 0);
+
+		rv = dpti_passthrough(sc, data, p);
+
+		sc->sc_nactive--;
+		wakeup_one(&sc->sc_nactive);
+		return (rv);
 
 	case DPT_I2ORESETCMD:
 		printf("%s: I2ORESETCMD not implemented\n",
 		    sc->sc_dv.dv_xname);
-		return (ENOTTY);
+		return (EOPNOTSUPP);
 
 	case DPT_I2ORESCANCMD:
 		return (iop_reconfigure(iop, 0));
@@ -278,31 +314,30 @@ dpti_blinkled(struct dpti_softc *sc)
 int
 dpti_ctlrinfo(struct dpti_softc *sc, u_long cmd, caddr_t data)
 {
-	struct dpt_ctlrinfo CtlrInfo;
+	struct dpt_ctlrinfo info;
 	struct iop_softc *iop;
 	int rv, i;
 
 	iop = (struct iop_softc *)sc->sc_dv.dv_parent;
 
-	memset(&CtlrInfo, 0, sizeof(CtlrInfo));
+	memset(&info, 0, sizeof(info));
 
-	CtlrInfo.length = sizeof(CtlrInfo) - sizeof(u_int16_t);
-	CtlrInfo.drvrHBAnum = sc->sc_dv.dv_unit;
-	CtlrInfo.baseAddr = iop->sc_memaddr;
+	info.length = sizeof(info) - sizeof(u_int16_t);
+	info.drvrHBAnum = sc->sc_dv.dv_unit;
+	info.baseAddr = iop->sc_memaddr;
 	if ((i = dpti_blinkled(sc)) == -1)
 		i = 0;
-	CtlrInfo.blinkState = i;
-	CtlrInfo.pciBusNum = iop->sc_pcibus;
-	CtlrInfo.pciDeviceNum = iop->sc_pcidev;
-	CtlrInfo.hbaFlags = FLG_OSD_PCI_VALID | FLG_OSD_DMA | FLG_OSD_I2O;
-	CtlrInfo.Interrupt = 10;			/* XXX */
+	info.blinkState = i;
+	info.pciBusNum = iop->sc_pcibus;
+	info.pciDeviceNum = iop->sc_pcidev;
+	info.hbaFlags = FLG_OSD_PCI_VALID | FLG_OSD_DMA | FLG_OSD_I2O;
+	info.Interrupt = 10;			/* XXX */
 
 	if (IOCPARM_LEN(cmd) > sizeof(*data)) {
-		bcopy(&CtlrInfo, data, sizeof(CtlrInfo));
+		memcpy(data, &info, min(sizeof(info), IOCPARM_LEN(cmd)));
 		rv = 0;
 	} else
-		rv = copyout(&CtlrInfo, *(caddr_t *)data,
-		    sizeof(CtlrInfo));
+		rv = copyout(&info, *(caddr_t *)data, sizeof(info));
 
 	return (rv);
 }
@@ -310,14 +345,86 @@ dpti_ctlrinfo(struct dpti_softc *sc, u_long cmd, caddr_t data)
 int
 dpti_sysinfo(struct dpti_softc *sc, u_long cmd, caddr_t data)
 {
+	struct dpt_sysinfo info;
+	int rv;
+#ifdef i386
+	int i, j;
+#endif
+
+	memset(&info, 0, sizeof(info));
+
+#ifdef i386
+	outb (0x70, 0x12);
+	i = inb(0x71);
+	j = i >> 4;
+	if (i == 0x0f) {
+		outb (0x70, 0x19);
+		j = inb (0x71);
+	}
+	info.drive0CMOS = j;
+
+	j = i & 0x0f;
+	if (i == 0x0f) {
+		outb (0x70, 0x1a);
+		j = inb (0x71);
+	}
+	info.drive1CMOS = j;
+	info.processorFamily = dpti_sig.dsProcessorFamily;
 
 	/*
-	 * XXX Not currently implemented, since it involves reading from the
-	 * IBM PC CMOS and grovelling around in ISA expansion card space,
-	 * which is kind of disgusting.  The DPT utilities don't seem to
-	 * mind.  :-)
+	 * Get the conventional memory size from CMOS.
 	 */
-	return (EIO);
+	outb(0x70, 0x16);
+	j = inb(0x71);
+	j <<= 8;
+	outb(0x70, 0x15);
+	j |= inb(0x71);
+	info.conventionalMemSize = j;
+
+	/*
+	 * Get the extended memory size from CMOS.
+	 */
+	outb(0x70, 0x31);
+	j = inb(0x71);
+	j <<= 8;
+	outb(0x70, 0x30);
+	j |= inb(0x71);
+	info.extendedMemSize = j;
+
+	switch (cpu_class) {
+	case CPUCLASS_386:
+		info.processorType = PROC_386;
+		break;
+	case CPUCLASS_486:
+		info.processorType = PROC_486;
+		break;
+	case CPUCLASS_586:
+		info.processorType = PROC_PENTIUM;
+		break;
+	case CPUCLASS_686:
+	default:
+		info.processorType = PROC_SEXIUM;
+		break;
+	}
+
+	info.flags = SI_CMOS_Valid | SI_BusTypeValid |
+	    SI_MemorySizeValid | SI_NO_SmartROM;
+#else
+	info.flags = SI_BusTypeValid | SI_NO_SmartROM;
+#endif
+
+	info.busType = SI_PCI_BUS;
+
+	/*
+	 * Copy out the info structure to the user.
+	 */
+	if (IOCPARM_LEN(cmd) > sizeof(*data)) {
+		memcpy(data, &info, min(sizeof(info), IOCPARM_LEN(cmd)));
+		rv = 0;
+	} else
+		rv = copyout(&info, *(caddr_t *)data, sizeof(info));
+
+	return (rv);
 }
 
 int
@@ -330,10 +437,11 @@ dpti_passthrough(struct dpti_softc *sc, caddr_t data, struct proc *proc)
 	struct dpti_ptbuf bufs[IOP_MAX_MSG_XFERS];
 	u_int32_t mbtmp[IOP_MAX_MSG_SIZE / sizeof(u_int32_t)];
 	u_int32_t rbtmp[IOP_MAX_MSG_SIZE / sizeof(u_int32_t)];
-	int rv, msgsize, repsize, sgoff, i, mapped, nbuf;
-	u_int32_t *p, *pmax;
+	int rv, msgsize, repsize, sgoff, i, mapped, nbuf, nfrag, j, sz;
+	u_int32_t *p, *pmax, *pstart;
 
 	iop = (struct iop_softc *)sc->sc_dv.dv_parent;
+	im = NULL;
 
 	if ((rv = dpti_blinkled(sc)) != -1) {
 		if (rv != 0) {
@@ -362,30 +470,27 @@ dpti_passthrough(struct dpti_softc *sc, caddr_t data, struct proc *proc)
 
 	/*
 	 * Handle special commands.
-	 *
-	 * XXX We'll need to handle all of these later for online firmware
-	 * upgrades.  Swish or what, eh?
 	 */
 	switch (mh.msgfunc >> 24) {
 	case I2O_EXEC_IOP_RESET:
-		printf("dpti%s: I2O_EXEC_IOP_RESET not implemented\n",
+		printf("%s: I2O_EXEC_IOP_RESET not implemented\n",
 		    sc->sc_dv.dv_xname);
-		return (EIO);
+		return (EOPNOTSUPP);
 
 	case I2O_EXEC_OUTBOUND_INIT:
-		printf("dpti%s: I2O_EXEC_OUTBOUND_INIT not implemented\n",
+		printf("%s: I2O_EXEC_OUTBOUND_INIT not implemented\n",
 		    sc->sc_dv.dv_xname);
-		return (EIO);
+		return (EOPNOTSUPP);
 
 	case I2O_EXEC_SYS_TAB_SET:
-		printf("dpti%s: I2O_EXEC_SYS_TAB_SET not implemented\n",
+		printf("%s: I2O_EXEC_SYS_TAB_SET not implemented\n",
 		    sc->sc_dv.dv_xname);
-		return (EIO);
+		return (EOPNOTSUPP);
 
 	case I2O_EXEC_STATUS_GET:
-		iop_status_get(iop, 0);
-		rv = copyout(&iop->sc_status, data + msgsize,
-		    sizeof(iop->sc_status));
+		if ((rv = iop_status_get(iop, 0)) == 0)
+			rv = copyout(&iop->sc_status, data + msgsize,
+			    sizeof(iop->sc_status));
 		return (rv);
 	}
 
@@ -423,7 +528,7 @@ dpti_passthrough(struct dpti_softc *sc, caddr_t data, struct proc *proc)
 	 * If the message has a scatter gather list, it must be comprised of
 	 * simple elements.  If any one transfer contains multiple segments,
 	 * we allocate a temporary buffer for it; otherwise, the buffer will
-	 * be mapped directly.  XXX Lies!  We don't do scatter-gather yet.
+	 * be mapped directly.
 	 */
 	if ((sgoff = ((mh.msgflags >> 4) & 15)) != 0) {
 		if ((sgoff + 2) > (msgsize >> 2)) {
@@ -431,33 +536,38 @@ dpti_passthrough(struct dpti_softc *sc, caddr_t data, struct proc *proc)
 			    sc->sc_dv.dv_xname));
 			return (EINVAL);
 		}
+
+		memset(bufs, 0, sizeof(bufs));
 	} else
 		nbuf = -1;
 
+	rv = EINVAL;
+
 	if (sgoff != 0) {
 		p = mbtmp + sgoff;
-		pmax = mbtmp + (msgsize >> 2);
+		pmax = mbtmp + (msgsize >> 2) - 2;
 
-		for (nbuf = 0; nbuf < IOP_MAX_MSG_XFERS; nbuf++) {
-			if (p > pmax - 2) {
+		for (nbuf = 0; nbuf < IOP_MAX_MSG_XFERS; nbuf++, p += 2) {
+			if (p > pmax) {
 				DPRINTF(("%s: invalid SGL (1)\n",
 				    sc->sc_dv.dv_xname));
-				return (EINVAL);
+				goto bad;
 			}
 
 			if ((p[0] & 0x30000000) != I2O_SGL_SIMPLE) {
 				DPRINTF(("%s: invalid SGL (2)\n",
 				    sc->sc_dv.dv_xname));
-				return (EINVAL);
+				goto bad;
 			}
 
 			bufs[nbuf].db_out = (p[0] & I2O_SGL_DATA_OUT) != 0;
+			bufs[nbuf].db_ptr = NULL;
 
 			if ((p[0] & I2O_SGL_END_BUFFER) != 0) {
 				if ((p[0] & 0x00ffffff) > IOP_MAX_XFER) {
 					DPRINTF(("%s: buffer too large\n",
 					    sc->sc_dv.dv_xname));
-					return (EINVAL);
+					goto bad;
 				}
 
 				bufs[nbuf].db_ptr = (caddr_t)p[1];
@@ -467,19 +577,85 @@ dpti_passthrough(struct dpti_softc *sc, caddr_t data, struct proc *proc)
 				if ((p[0] & I2O_SGL_END) != 0)
 					break;
 
-				p += 2;
 				continue;
 			}
 
-			printf("%s: scatter-gather not implemented\n",
-			    sc->sc_dv.dv_xname);
-			return (EIO);
+			/*
+			 * The buffer has multiple segments.  Determine the
+			 * total size.
+			 */
+			nfrag = 0;
+			sz = 0;
+			for (pstart = p; p <= pmax; p += 2) {
+				if (nfrag == DPTI_MAX_SEGS) {
+					DPRINTF(("%s: too many segments\n",
+					    sc->sc_dv.dv_xname));
+					goto bad;
+				}
+
+				bufs[nbuf].db_frags[nfrag].iov_len =
+				    p[0] & 0x00ffffff;
+				bufs[nbuf].db_frags[nfrag].iov_base = 
+				    (void *)p[1];
+
+				sz += p[0] & 0x00ffffff;
+				nfrag++;
+
+				if ((p[0] & I2O_SGL_END) != 0) {
+					if ((p[0] & I2O_SGL_END_BUFFER) == 0) {
+						DPRINTF((
+						    "%s: invalid SGL (3)\n",
+						    sc->sc_dv.dv_xname));
+						goto bad;
+					}
+					break;
+				}
+				if ((p[0] & I2O_SGL_END_BUFFER) != 0)
+					break;
+			}
+			bufs[nbuf].db_nfrag = nfrag;
+
+			if (p > pmax) {
+				DPRINTF(("%s: invalid SGL (4)\n",
+				    sc->sc_dv.dv_xname));
+				goto bad;
+			}
+
+			if (sz > IOP_MAX_XFER) {
+				DPRINTF(("%s: buffer too large\n",
+				    sc->sc_dv.dv_xname));
+				goto bad;
+			}
+
+			bufs[nbuf].db_size = sz;
+			bufs[nbuf].db_ptr = malloc(sz, M_DEVBUF, M_WAITOK);
+			if (bufs[nbuf].db_ptr == NULL) {
+				DPRINTF(("%s: allocation failure\n",
+				    sc->sc_dv.dv_xname));
+				rv = ENOMEM;
+				goto bad;
+			}
+
+			for (i = 0, sz = 0; i < bufs[nbuf].db_nfrag; i++) {
+				rv = copyin(bufs[nbuf].db_frags[i].iov_base,
+				    bufs[nbuf].db_ptr + sz,
+				    bufs[nbuf].db_frags[i].iov_len);
+				if (rv != 0) {
+					DPRINTF(("%s: frag copyin\n",
+					    sc->sc_dv.dv_xname));
+					goto bad;
+				}
+				sz += bufs[nbuf].db_frags[i].iov_len;
+			}
+
+			if ((p[0] & I2O_SGL_END) != 0)
+				break;
 		}
 
 		if (nbuf == IOP_MAX_MSG_XFERS) {
 			DPRINTF(("%s: too many transfers\n",
 			    sc->sc_dv.dv_xname));
-			return (EINVAL);
+			goto bad;
 		}
 	}
 
@@ -487,12 +663,13 @@ dpti_passthrough(struct dpti_softc *sc, caddr_t data, struct proc *proc)
 	 * Allocate a wrapper, and adjust the message header fields to
 	 * indicate that no scatter-gather list is currently present.
 	 */
+	mapped = 0;
+
 	im = iop_msg_alloc(iop, IM_WAIT | IM_NOSTATUS);
 	im->im_rb = (struct i2o_reply *)rbtmp;
 	mf = (struct i2o_msg *)mbtmp;
 	mf->msgictx = IOP_ICTX;
 	mf->msgtctx = im->im_tctx;
-	mapped = 0;
 
 	if (sgoff != 0)
 		mf->msgflags = (mf->msgflags & 0xff0f) | (sgoff << 16);
@@ -504,8 +681,8 @@ dpti_passthrough(struct dpti_softc *sc, caddr_t data, struct proc *proc)
 		rv = iop_msg_map(iop, im, mbtmp, bufs[i].db_ptr,
 		    bufs[i].db_size, bufs[i].db_out, bufs[i].db_proc);
 		if (rv != 0) {
-			DPRINTF(("%s: msg_map failed\n",
-			    sc->sc_dv.dv_xname));
+			DPRINTF(("%s: msg_map failed, rv = %d\n",
+			    sc->sc_dv.dv_xname, rv));
 			goto bad;
 		}
 		mapped = 1;
@@ -514,9 +691,6 @@ dpti_passthrough(struct dpti_softc *sc, caddr_t data, struct proc *proc)
 	/*
 	 * Start the command and sleep until it completes.
 	 */
-	if (sc->sc_nactive++ >= 2)
-		tsleep(&sc->sc_nactive, PRIBIO, "dptislp", 0);
-
 	if ((rv = iop_msg_post(iop, im, mbtmp, 5*60*1000)) != 0)
 		goto bad;
 
@@ -528,12 +702,33 @@ dpti_passthrough(struct dpti_softc *sc, caddr_t data, struct proc *proc)
 		    sc->sc_dv.dv_xname));
 
  bad:
-	sc->sc_nactive--;
-	wakeup_one(&sc->sc_nactive);
+	/*
+	 * Free resources and return to the caller.
+	 */
+	if (im != NULL) {
+		if (mapped)
+			iop_msg_unmap(iop, im);
+		iop_msg_free(iop, im);
+	}
 
-	if (mapped)
-		iop_msg_unmap(iop, im);
+	for (i = 0; i <= nbuf; i++) {
+		if (bufs[i].db_proc != NULL)
+			continue;
 
-	iop_msg_free(iop, im);
+		if (!bufs[i].db_out && rv == 0) {
+			for (j = 0, sz = 0; j < bufs[i].db_nfrag; j++) {
+				rv = copyout(bufs[i].db_ptr + sz,
+				    bufs[i].db_frags[j].iov_base,
+				    bufs[i].db_frags[j].iov_len);
+				if (rv != 0)
+					break;
+				sz += bufs[i].db_frags[j].iov_len;
+			}
+		}
+
+		if (bufs[i].db_ptr != NULL)
+			free(bufs[i].db_ptr, M_DEVBUF);
+	}
+
 	return (rv);
 }
