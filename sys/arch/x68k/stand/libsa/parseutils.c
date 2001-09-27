fb0a6f158a97326a7becde1d25bb7bc0b77c5b13
minoura <minoura@NetBSD.org>
2001-09-27 10:03:28 +0000

    libsa for the new bootloader of x68k port.

---
+++ sys/arch/x68k/stand/libsa/parseutils.c
@@ -0,0 +1,140 @@
+/*	$NetBSD: parseutils.c,v 1.1.4.2 2001/09/27 10:03:29 minoura Exp $	*/
+
+/*
+ *	from /sys/arch/i386/lib/parseutils.c
+ *	NetBSD: parseutils.c,v 1.3 2000/09/24 12:32:35 jdolecek Exp
+ */
+
+/*
+ * Copyright (c) 1996, 1997
+ * 	Matthias Drochner.  All rights reserved.
+ * Copyright (c) 1996, 1997
+ * 	Perry E. Metzger.  All rights reserved.
+ * Copyright (c) 1997
+ *	Jason R. Thorpe.  All rights reserved
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgements:
+ *	This product includes software developed for the NetBSD Project
+ *	by Matthias Drochner.
+ *	This product includes software developed for the NetBSD Project
+ *	by Perry E. Metzger.
+ * 4. The names of the authors may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <lib/libkern/libkern.h>
+#include <lib/libsa/stand.h>
+#include <sys/boot_flag.h>
+
+#include "libx68k.h"
+
+
+/*
+ * chops the head from the arguments and returns the arguments if any,
+ * or possibly an empty string.
+ */
+char *
+gettrailer(arg)
+	char *arg;
+{
+	char *options;
+
+	if ((options = strchr(arg, ' ')) == NULL)
+		return ("");
+	else
+		*options++ = '\0';
+
+	/* trim leading blanks */
+	while (*options && *options == ' ')
+		options++;
+
+	return (options);
+}
+
+int
+parseopts(opts, howto)
+	const char *opts;
+	int *howto;
+{
+	int r, tmpopt = 0;
+
+	opts++; 	/* skip - */
+	while (*opts && *opts != ' ') {
+		r = 0;
+		BOOT_FLAG(*opts, r);
+		if (r == 0) {
+			printf("-%c: unknown flag\n", *opts);
+			return(0);
+		}
+		tmpopt |= r;
+		opts++;
+	}
+
+	*howto = tmpopt;
+	return(1);
+}
+
+int
+parseboot(arg, filename, howto)
+	char *arg;
+	char **filename;
+	int *howto;
+{
+	char *opts = NULL;
+
+	*filename = 0;
+	*howto = 0;
+
+	/* if there were no arguments */
+	if (!*arg)
+		return(1);
+
+	/* format is... */
+	/* [[xxNx:]filename] [-adqsv] */
+
+	/* check for just args */
+	if (arg[0] == '-')
+		opts = arg;
+	else {
+		/* there's a file name */
+		*filename = arg;
+
+		opts = gettrailer(arg);
+		if (!*opts)
+			opts = NULL;
+		else if (*opts != '-') {
+			printf("invalid arguments\n");
+			return(0);
+		}
+	}
+
+	/* at this point, we have dealt with filenames. */
+
+	/* now, deal with options */
+	if (opts) {
+		if (parseopts(opts, howto) == 0)
+			return(0);
+	}
+
+	return(1);
+}
---
+++ sys/arch/x68k/stand/libsa/sdcdvar.h
@@ -0,0 +1,35 @@
+/*	$NetBSD: sdcdvar.h,v 1.1.4.2 2001/09/27 10:03:29 minoura Exp $	*/
+
+/*
+ * Copyright (c) 2001 Minoura Makoto
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+
+#define BLKBYTES(len)	(1 << ((len) + 8))
+
+struct boot_partinfo {
+	u_int32_t	start;	/* in blocks */
+	u_int32_t	size;	/* in blocks */
+};
