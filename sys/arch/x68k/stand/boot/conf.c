79e21469838eded3f43d4a84c6c11bb9a9f2072c
minoura <minoura@NetBSD.org>
2001-09-27 10:14:49 +0000

    Initial commit of the new bootloader to support ELF kernel. It consists of two programs named boot_ufs and boot; the former reads the latter (an OMAGIC a.out). Boot now utilizes the libsa library to read the actual kernel.
     - LFS code exists, but probobly does not work.
     - Currently the ELF toolchain is required.
     - Many features are missing.

---
+++ sys/arch/x68k/stand/boot/conf.c
@@ -0,0 +1,62 @@
+/*	$NetBSD: conf.c,v 1.1 2001/09/27 10:14:49 minoura Exp $	*/
+
+/*
+ * Copyright (c) 2001 Minoura Makoto
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <sys/param.h>
+#include <lib/libsa/stand.h>
+#include <lib/libsa/ufs.h>
+#include <lib/libsa/lfs.h>
+#include <lib/libsa/cd9660.h>
+
+#include "libx68k.h"
+
+struct devsw devsw[] = {
+	{ "sd",	sdstrategy, sdopen, sdclose, noioctl },
+	{ "cd",	cdstrategy, cdopen, cdclose, noioctl },
+	{ "fd",	fdstrategy, fdopen, fdclose, noioctl },
+	{ 0, 0, 0, 0, 0 }
+};
+
+int ndevs = (sizeof (devsw) / sizeof (devsw[0]));
+
+const struct devspec devspec[] = {
+	{ "sd", 0, 7 },
+	{ "cd", 1, 7 },
+	{ "fd", 2, 3 },
+	{ 0, 0, 0 }
+};
+
+struct fs_ops file_system[] = {
+    { ufs_open, ufs_close, ufs_read, ufs_write, ufs_seek,
+	ufs_stat },
+    { lfs_open, lfs_close, lfs_read, lfs_write, lfs_seek,
+	lfs_stat },
+    { cd9660_open, cd9660_close, cd9660_read, cd9660_write, cd9660_seek,
+	cd9660_stat },
+};
+ 
+int nfsys = sizeof(file_system)/sizeof(struct fs_ops);
---
+++ sys/arch/x68k/stand/boot_ufs/readufs.c
@@ -0,0 +1,325 @@
+/*	$Id: readufs.c,v 1.1 2001/09/27 10:14:50 minoura Exp $	*/
+
+/*
+ * Read UFS (FFS / LFS)
+ *
+ * Written by ITOH, Yasufumi (itohy@netbsd.org).
+ * Public domain.
+ *
+ * Intended to be used for boot programs (first stage).
+ * DON'T ADD ANY FANCY FEATURE.  THIS SHALL BE COMPACT.
+ */
+
+#include <sys/types.h>
+#include <sys/param.h>
+#include <ufs/ufs/dinode.h>
+
+#include "readufs.h"
+
+#define fs	ufs_info
+
+static void raw_read_queue __P((void *buf, ufs_daddr_t blkpos, size_t bytelen));
+static int ufs_read_indirect __P((ufs_daddr_t blk, int level, caddr_t *buf,
+		unsigned *poff, size_t count));
+
+#ifdef DEBUG_WITH_STDIO
+void ufs_list_dir __P((ino_t dirino));
+int main __P((int argc, char *argv[]));
+#endif
+
+#ifdef DEBUG_WITH_STDIO
+int fd;
+
+void
+RAW_READ(buf, blkpos, bytelen)
+	void *buf;
+	u_int32_t blkpos;
+	size_t bytelen;
+{
+	if (pread(fd, buf, bytelen, (off_t)dbtob(blkpos)) != (ssize_t) bytelen)
+		err(1, "pread: buf %p, blk %u, len %u", buf, blkpos, bytelen);
+}
+#endif
+
+struct ufs_info fs;
+
+/*
+ * Read contiguous sectors at once for speedup.
+ */
+static size_t rq_len;
+
+static void
+raw_read_queue(buf, blkpos, bytelen)
+	void *buf;
+	ufs_daddr_t blkpos;
+	size_t bytelen;		/* must be DEV_BSIZE aligned */
+{
+	static ufs_daddr_t rq_start;
+	static char *rq_buf;
+
+	if (rq_len) {
+		if (bytelen && blkpos == rq_start + (ssize_t) btodb(rq_len)
+				&& buf == rq_buf + rq_len) {
+			rq_len += bytelen;
+			return;
+		} else {
+#ifdef DEBUG_WITH_STDIO
+			printf("raw_read_queue: read: buf %p, blk %d, len %d\n",
+				 rq_buf, rq_start, rq_len);
+#endif
+			RAW_READ(rq_buf, (unsigned int) rq_start, rq_len);
+		}
+	}
+	rq_buf = buf;
+	rq_start = blkpos;
+	rq_len = bytelen;
+}
+
+#define RAW_READ_QUEUE_INIT()	(rq_len = 0)
+#define RAW_READ_QUEUE_FLUSH()	\
+		raw_read_queue((void *) 0, (u_int32_t) 0, (size_t) 0)
+
+
+/*
+ * Read a file, specified by dinode.
+ * No support for holes or (short) symbolic links.
+ */
+size_t
+ufs_read(di, buf, off, count)
+	struct dinode *di;
+	void *buf;
+	unsigned off;	/* position in block */
+	size_t count;
+{
+	size_t bsize = fs.bsize;
+	caddr_t b = buf;
+	int i;
+	size_t nread;
+
+#ifdef DEBUG_WITH_STDIO
+	printf("ufs_read: off: %d, count %u\n", off, count);
+#endif
+	if ((size_t) di->di_size < count + off * bsize)
+		count = (size_t) di->di_size - off * bsize;
+
+	/* FS block size alignment. */
+	nread = count;
+	count = (count + bsize - 1) & ~(bsize - 1);
+
+	RAW_READ_QUEUE_INIT();
+
+	/* Read direct blocks. */
+	for ( ; off < NDADDR && count > 0; off++) {
+#if 0
+		printf("ufs_read: read: blk: %d\n",
+			di->di_db[off] << fs.iblkshift);
+#endif
+		raw_read_queue(b, di->di_db[off] << fs.iblkshift, bsize);
+		b += bsize;
+		count -= bsize;
+	}
+	off -= NDADDR;
+
+	/* Read indirect blocks. */
+	for (i = 0; i < NIADDR && count > 0; i++)
+		count = ufs_read_indirect(di->di_ib[i], i, &b, &off, count);
+
+	RAW_READ_QUEUE_FLUSH();
+
+	return (size_t) nread;
+}
+
+static int
+ufs_read_indirect(blk, level, buf, poff, count)
+	ufs_daddr_t blk;
+	int level;
+	caddr_t *buf;
+	unsigned *poff;	/* position in block */
+	size_t count;
+{
+	size_t bsize = fs.bsize;
+	ufs_daddr_t *idbuf = alloca(bsize);
+	unsigned off = *poff;
+	unsigned b;
+
+#ifdef DEBUG_WITH_STDIO
+	printf("ufs_read_indirect: off: %d, count %u\n", off, count);
+#endif
+	if (off) {
+		unsigned subindirsize = 1, indirsize;
+		int i;
+
+		for (i = level; i > 0; i--)
+			subindirsize *= fs.nindir;
+		indirsize = subindirsize * fs.nindir;
+		if (off >= indirsize) {
+			/* no need to read any data */
+			*poff = off - indirsize;
+			return 0;
+		}
+
+		b = off / subindirsize;
+		off -= b * subindirsize;
+		*poff = 0;
+	} else
+		b = 0;
+
+	/* read the indirect block */
+	RAW_READ(idbuf, (unsigned int) blk << fs.iblkshift, bsize);
+
+	for ( ; b < fs.nindir && count > 0; b++) {
+		if (level)
+			count = ufs_read_indirect(idbuf[b], level - 1, buf, &off, count);
+		else {
+#if 0
+			printf("ufs_read: read: blk: %d\n",
+				idbuf[b] << fs.iblkshift);
+#endif
+			raw_read_queue(*buf, idbuf[b] << fs.iblkshift, bsize);
+			*buf += bsize;
+			count -= bsize;
+		}
+	}
+
+	return count;
+}
+
+/*
+ * look-up fn in directory dirino
+ */
+ino_t
+ufs_lookup(dirino, fn)
+	ino_t dirino;
+	const char *fn;
+{
+	struct dinode dirdi;
+	struct direct *pdir;
+	char *p;
+
+	if (ufs_get_inode(dirino, &dirdi))
+		return 0;
+
+	if ((dirdi.di_mode & IFMT) != IFDIR)
+		return 0;			/* Not a directory */
+
+#if 0
+	p = alloca(((size_t) dirdi.di_size + fs.bsize - 1) & ~(fs.bsize - 1));
+#else	/* simplify calculation to reduce code size */
+	p = alloca((size_t) dirdi.di_size + fs.bsize);
+#endif
+	ufs_read(&dirdi, p, 0, (size_t) dirdi.di_size);
+	for ( ; pdir = (void *) p, pdir->d_ino; p += pdir->d_reclen) {
+		if (!strcmp(fn, pdir->d_name))
+			return pdir->d_ino;
+	}
+	return 0;				/* No such file or directory */
+}
+
+/*
+ * look-up a file in absolute pathname from the root directory
+ */
+ino_t
+ufs_lookup_path(path)
+	const char *path;
+{
+	char fn[MAXNAMLEN + 1];
+	char *p;
+	ino_t ino = ROOTINO;
+
+	do {
+		while (*path == '/')
+			path++;
+		for (p = fn; *path && *path != '/'; )
+			*p++ = *path++;
+		*p++ = '\0';
+		ino = ufs_lookup(ino, fn);
+	} while (ino && *path);
+
+	return ino;
+}
+
+#if 0
+size_t
+ufs_load_file(buf, dirino, fn)
+	void *buf;
+	ino_t dirino;
+	const char *fn;
+{
+	size_t cnt;
+	struct dinode dinode;
+
+	if (ufs_fn_inode(dirino, fn, &dinode))
+		return (unsigned) 0;
+	cnt = ufs_read(&dinode, buf, 0, (size_t) dinode.di_size);
+
+	return cnt;
+}
+#endif
+
+int
+ufs_init()
+{
+	return 1
+#ifdef USE_FFS
+		&& try_ffs()
+#endif
+#ifdef USE_LFS
+		&& try_lfs()
+#endif
+		;
+}
+
+#ifdef DEBUG_WITH_STDIO
+void
+ufs_list_dir(dirino)
+	ino_t dirino;
+{
+	struct dinode dirdi;
+	struct direct *pdir;
+	char *p;
+
+	if (ufs_get_inode(dirino, &dirdi))
+		errx(1, "ino = %d: not found", dirino);
+
+	p = alloca(((size_t) dirdi.di_size + fs.bsize - 1) & ~(fs.bsize - 1));
+	ufs_read(&dirdi, p, 0, (size_t) dirdi.di_size);
+	for ( ; pdir = (void *) p, pdir->d_ino; p += pdir->d_reclen) {
+		printf("%6d %s\n", pdir->d_ino, pdir->d_name);
+	}
+}
+#endif
+
+#ifdef DEBUG_WITH_STDIO
+int
+main(argc, argv)
+	int argc __attribute__((unused));
+	char *argv[];
+{
+	struct dinode dinode;
+
+	if ((fd = open(argv[1], O_RDONLY)) < 0)
+		err(1, "open: %s", argv[1]);
+
+	if (ufs_init())
+		errx(1, "%s: unknown fs", argv[1]);
+
+#if 1
+	ufs_list_dir(ROOTINO);
+	{
+		void *p;
+		size_t cnt;
+		ino_t ino;
+
+		if ((ino = ufs_lookup_path(argv[2])) == 0)
+			errx(1, "%s: not found", argv[2]);
+		ufs_get_inode(ino, &dinode);
+		p = malloc(((size_t) dinode.di_size + fs.bsize - 1) & ~(fs.bsize - 1));
+		cnt = ufs_read(&dinode, p, 0, (size_t) dinode.di_size);
+		write(3, p, cnt);
+		free(p);
+	}
+#endif
+
+	return 0;
+}
+#endif
