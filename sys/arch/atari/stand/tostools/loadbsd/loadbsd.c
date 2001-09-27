cc67a8db6c9766d0cbdb42967debf8fd2782e3eb
leo <leo@NetBSD.org>
2001-09-27 14:29:22 +0000

    Add ELF stuff to loader. This is a combined effort from Thomas Gerner and myself.

--- sys/arch/atari/stand/tostools/loadbsd/loadbsd.c
+++ sys/arch/atari/stand/tostools/loadbsd/loadbsd.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: loadbsd.c,v 1.15 1999/06/23 19:26:13 leo Exp $	*/
+/*	$NetBSD: loadbsd.c,v 1.16 2001/09/27 14:29:22 leo Exp $	*/
 
 /*
  * Copyright (c) 1995 L. Weppelman
@@ -34,6 +34,7 @@
  * NetBSD loader for the Atari-TT.
  */
 
+#include "exec_elf.h"
 #include <a_out.h>
 #include <fcntl.h>
 #include <stdio.h>
@@ -55,11 +56,12 @@
 char	*Progname;		/* How are we called		*/
 int	d_flag  = 0;		/* Output debugging output?	*/
 int	h_flag  = 0;		/* show help			*/
+int	N_flag  = 0;		/* No symbols?			*/
 int	s_flag  = 0;		/* St-ram only			*/
 int	t_flag  = 0;		/* Just test, do not execute	*/
 int	v_flag  = 0;		/* show version			*/
 
-const char version[] = "$Revision: 1.15 $";
+const char version[] = "$Revision: 1.16 $";
 
 /*
  * Default name of kernel to boot, large enough to patch
@@ -70,9 +72,13 @@ static struct kparamb kparam;
 
 void help  PROTO((void));
 void usage PROTO((void));
+int  aout_load PROTO((int));
+int  elf_load PROTO((int));
 void get_sys_info PROTO((void));
 void start_kernel PROTO((void));
 
+#define ELFMAGIC	((ELFMAG0 << 24) | (ELFMAG1 << 16) | \
+				(ELFMAG2 << 8) | ELFMAG3)
 int
 main(argc, argv)
 int	argc;
@@ -85,15 +91,13 @@ char	**argv;
 	extern	char	*optarg;
 	int		ch;
 	int		fd;
-	long		textsz, stringsz;
-	struct exec	ehdr;
 	
 	init_toslib(argv[0]);
 	Progname = argv[0];
 
 	kparam.boothowto = RB_SINGLE;
 
-	while ((ch = getopt(argc, argv, "abdhstVwDo:S:T:")) != -1) {
+	while ((ch = getopt(argc, argv, "abdDhNstVwo:S:T:")) != -1) {
 		switch (ch) {
 		case 'a':
 			kparam.boothowto &= ~(RB_SINGLE);
@@ -110,6 +114,9 @@ char	**argv;
 			break;
 		case 'h':
 			h_flag = 1;
+			break;
+		case 'N':
+			N_flag = 1;
 			break;
 		case 'o':
 			redirect_output(optarg);
@@ -156,11 +163,41 @@ char	**argv;
 	 */
 	if ((fd = open(kname, O_RDONLY)) < 0)
 		fatal(-1, "Cannot open kernel '%s'", kname);
+	if (elf_load(fd) == 0)
+		if (aout_load(fd) == 0)
+			fatal(-1, "Not an ELF or NMAGIC file '%s'", kname);
+	close(fd);
+
+	if (d_flag) {
+	    eprintf("\r\nKernel info:\r\n");
+	    eprintf("Kernel loadaddr\t: 0x%08x\r\n", kparam.kp);
+	    eprintf("Kernel size\t: %10d bytes\r\n", kparam.ksize);
+	    eprintf("Kernel entry\t: 0x%08x\r\n", kparam.entry);
+	    eprintf("Kernel esym\t: 0x%08x\r\n", kparam.esym_loc);
+	}
+
+	if (!t_flag)
+		start_kernel();
+		/* NOT REACHED */
+
+	eprintf("Kernel '%s' was loaded OK\r\n", kname);
+	xexit(0);
+	return 0;
+}
+
+int
+aout_load(fd)
+int				fd;
+{
+	long		textsz, stringsz;
+	struct exec	ehdr;
+
+	lseek(fd, (off_t)0, SEEK_SET);
 	if (read(fd, (char *)&ehdr, sizeof(ehdr)) != sizeof(ehdr))
-		fatal(-1, "Cannot read exec-header of '%s'", kname);
+		return 0;
 
 	if (N_MAGIC(ehdr) != NMAGIC)
-		fatal(-1, "Not an NMAGIC file '%s'", kname);
+		return 0;
 
 	/*
 	 * Extract various sizes from the kernel executable
@@ -170,7 +207,7 @@ char	**argv;
 	kparam.ksize    = textsz + ehdr.a_data + ehdr.a_bss;
 	kparam.entry    = ehdr.a_entry;
 
-	if (ehdr.a_syms) {
+	if (!N_flag && ehdr.a_syms) {
 	  if (lseek(fd,ehdr.a_text+ehdr.a_data+ehdr.a_syms+sizeof(ehdr),0)
 									<= 0)
 		fatal(-1, "Cannot seek to string table in '%s'", kname);
@@ -195,7 +232,7 @@ char	**argv;
 	/*
 	 * Read symbol and string table
 	 */
-	if (ehdr.a_syms) {
+	if (!N_flag && ehdr.a_syms) {
 	    long	*p;
 
 	    p = (long *)(kparam.kp + textsz + ehdr.a_data + ehdr.a_bss);
@@ -207,22 +244,147 @@ char	**argv;
 		fatal(-1, "Cannot read string table\n");
 	    kparam.esym_loc = (long)((char *)p-(char *)kparam.kp +stringsz);
 	}
-	close(fd);
+	
+	return 1;
+}
 
-	if (d_flag) {
-	    eprintf("\r\nKernel info:\r\n");
-	    eprintf("Kernel loadaddr\t: 0x%08x\r\n", kparam.kp);
-	    eprintf("Kernel size\t: %10d bytes\r\n", kparam.ksize);
-	    eprintf("Kernel entry\t: 0x%08x\r\n", kparam.entry);
-	    eprintf("Kernel esym\t: 0x%08x\r\n", kparam.esym_loc);
+int
+elf_load(fd)
+int				fd;
+{
+	int		i,j;
+	Elf32_Ehdr	ehdr;
+	Elf32_Phdr	*phdrs;
+	Elf32_Word	symsize, symstart;
+	long		kernsize;
+
+	lseek(fd, (off_t)0, SEEK_SET);
+	if (read(fd, (char *)&ehdr, sizeof(ehdr)) != sizeof(ehdr))
+		return 0;
+
+	if (*((u_int *)ehdr.e_ident) != ELFMAGIC)
+		return 0;
+
+	/*
+	 * calculate highest used address
+	 */
+	i = ehdr.e_phnum * sizeof(Elf32_Phdr);
+	if ((phdrs = (Elf32_Phdr *)malloc(i)) == NULL)
+		fatal(-1, "Cannot malloc Elf phdr storage space");
+	if (read(fd, phdrs, i) != i)
+		fatal(-1, "Cannot read Elf32_Phdrs");
+
+	kernsize = 0;
+	for (i = 0; i < ehdr.e_phnum; i++) {
+		Elf32_Word sum;
+
+		sum = phdrs[i].p_vaddr + phdrs[i].p_memsz;
+		if ((phdrs[i].p_flags & (PF_W|PF_X)) && (sum > kernsize))
+			kernsize = sum;
 	}
 
-	if (!t_flag)
-		start_kernel();
-		/* NOT REACHED */
+	/*
+	 * look for symbols and calculate the size
+	 * XXX: This increases the load time by a factor 2 for gzipped
+	 *      images!
+	 */
+	symsize  = 0;
+	symstart = 0;
+	if (!N_flag) {
+	    i = ehdr.e_shnum + 1;
+	    if (lseek(fd, (off_t)ehdr.e_shoff, SEEK_SET) != ehdr.e_shoff)
+		fatal(-1, "Cannot seek to e_shoff location");
+	    while (--i) {
+	      Elf32_Shdr shdr;
 
-	eprintf("Kernel '%s' was loaded OK\r\n", kname);
-	xexit(0);
+	      if (read(fd, &shdr, sizeof(shdr)) != sizeof(shdr))
+		fatal(-1, "Cannot read Elf32_shdr");
+	      if ((shdr.sh_type == SHT_SYMTAB) || (shdr.sh_type == SHT_STRTAB))
+		symsize += shdr.sh_size;
+	    }
+	}
+
+	if (symsize) {
+	  symstart = kernsize;
+	  kernsize += symsize + sizeof(ehdr) + ehdr.e_shnum*sizeof(Elf32_Shdr);
+	}
+
+	/*
+	 * Extract various sizes from the kernel executable
+	 */
+	kparam.esym_loc = symsize ? kernsize : 0;
+	kparam.ksize    = kernsize;
+	kparam.entry    = ehdr.e_entry;
+
+	if ((kparam.kp = (u_char *)malloc(kparam.ksize)) == NULL)
+		fatal(-1, "Cannot malloc kernel image space");
+
+	/*
+	 * Read text & data, clear bss
+	 */
+	for (i = 0; i < ehdr.e_phnum; i++) {
+	    u_char	*p;
+	    Elf32_Phdr	*php = &phdrs[i];
+
+	    if (php->p_flags & (PF_W|PF_X)) {
+		if (lseek(fd, (off_t)php->p_offset, SEEK_SET) != php->p_offset)
+		    fatal(-1, "Seek error while reading text segment\n");
+		p = (u_char *)(kparam.kp) + php->p_vaddr;
+		if (read(fd, p, php->p_filesz) != php->p_filesz)
+		    fatal(-1, "Read error in text segment\n");
+		if (php->p_memsz > php->p_filesz)
+		    memset(p + php->p_filesz, 0, php->p_memsz - php->p_filesz);
+	    }
+	}
+
+	/*
+	 * Read symbols and strings
+	 */
+	if (symsize) {
+	    u_char	*p, *symtab;
+	    int		nhdrs;
+	    Elf32_Shdr	*shp;
+
+	    symtab = kparam.kp + symstart;
+
+	    p = symtab + sizeof(ehdr);
+	    nhdrs = ehdr.e_shnum;
+	    if (lseek(fd, (off_t)ehdr.e_shoff, SEEK_SET) != ehdr.e_shoff)
+		fatal(-1, "Error seeking to section headers");
+	    if (read(fd, p, nhdrs * sizeof(*shp)) != nhdrs * sizeof(*shp))
+		fatal(-1, "Error reading section headers");
+	    shp = (Elf32_Shdr*)p;
+	    p  += nhdrs * sizeof(*shp);
+	    for (i = 0; i < nhdrs; i++) {
+		if (shp[i].sh_type == SHT_SYMTAB) {
+		    if (shp[i].sh_offset == 0)
+			continue;
+		    /* Got the symbol table. */
+		    if (lseek(fd, (off_t)shp[i].sh_offset, SEEK_SET) !=
+							shp[i].sh_offset)
+		    	fatal(-1, "Error seeking to symbols");
+		    if (read(fd, p, shp[i].sh_size) != shp[i].sh_size)
+			fatal(-1, "Error reading symbols");
+		    shp[i].sh_offset = p - symtab;
+		    /* Find the string table to go with it. */
+		    j = shp[i].sh_link;
+		    if (shp[j].sh_offset == 0)
+			continue;
+		    p += shp[i].sh_size;
+		    if (lseek(fd, (off_t)shp[j].sh_offset, SEEK_SET) !=
+				    			shp[j].sh_offset)
+		    	fatal(-1, "Error seeking to string table");
+		    if (read(fd, p, shp[j].sh_size) != shp[j].sh_size)
+			fatal(-1, "Error reading strings");
+		    shp[j].sh_offset = p - symtab;
+		    /* There should only be one symbol table. */
+		    break;
+		}
+	    }
+	    ehdr.e_shoff = sizeof(ehdr);
+	    bcopy(&ehdr, symtab, sizeof(ehdr));
+	}
+	return 1;
 }
 
 /*
@@ -339,6 +501,7 @@ Description of options:\r
 \t-d  Enter kernel debugger.\r
 \t-D  printout debug information while loading\r
 \t-h  What you're getting right now.\r
+`t-N  No symbols must be loaded.\r
 \t-o  Write output to both <output file> and stdout.\r
 \t-s  Use only ST-compatible RAM\r
 \t-S  Set amount of ST-compatible RAM\r
