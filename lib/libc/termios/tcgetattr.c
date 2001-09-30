3758d48a3f27708312ffe1e0ed3e587b1475f7c8
lukem <lukem@NetBSD.org>
2001-09-30 23:25:38 +0000

    don't bother checking _DIAGASSERT(fd != -1), since the underlying ioctl(2) will check it and return EBADF, and i got sick of the warnings from programs like sshd which occassionally call isatty() with bad fds.

--- lib/libc/termios/tcgetattr.c
+++ lib/libc/termios/tcgetattr.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: tcgetattr.c,v 1.7 2000/01/22 22:19:21 mycroft Exp $	*/
+/*	$NetBSD: tcgetattr.c,v 1.8 2001/09/30 23:25:39 lukem Exp $	*/
 
 /*-
  * Copyright (c) 1989, 1993
@@ -38,7 +38,7 @@
 #if 0
 static char sccsid[] = "@(#)termios.c	8.2 (Berkeley) 2/21/94";
 #else
-__RCSID("$NetBSD: tcgetattr.c,v 1.7 2000/01/22 22:19:21 mycroft Exp $");
+__RCSID("$NetBSD: tcgetattr.c,v 1.8 2001/09/30 23:25:39 lukem Exp $");
 #endif
 #endif /* LIBC_SCCS and not lint */
 
@@ -60,7 +60,6 @@ tcgetattr(fd, t)
 	struct termios *t;
 {
 
-	_DIAGASSERT(fd != -1);
 	_DIAGASSERT(t != NULL);
 
 	return (ioctl(fd, TIOCGETA, t));
