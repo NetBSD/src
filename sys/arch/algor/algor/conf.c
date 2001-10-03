98ab9f5fb6cd1e30de7e56a154b491fc1ce8d641
simonb <simonb@NetBSD.org>
2001-10-03 13:14:02 +0000

    One copy of <sys/conf.h> will be enough.

--- sys/arch/algor/algor/conf.c
+++ sys/arch/algor/algor/conf.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: conf.c,v 1.1 2001/05/28 16:22:15 thorpej Exp $	*/
+/*	$NetBSD: conf.c,v 1.2 2001/10/03 13:14:02 simonb Exp $	*/
 
 /*-
  * Copyright (c) 1991 The Regents of the University of California.
@@ -37,7 +37,7 @@
 
 #include <sys/cdefs.h>			/* RCS ID & Copyright macro defns */
 
-__KERNEL_RCSID(0, "$NetBSD: conf.c,v 1.1 2001/05/28 16:22:15 thorpej Exp $");
+__KERNEL_RCSID(0, "$NetBSD: conf.c,v 1.2 2001/10/03 13:14:02 simonb Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -46,7 +46,6 @@ __KERNEL_RCSID(0, "$NetBSD: conf.c,v 1.1 2001/05/28 16:22:15 thorpej Exp $");
 #include <sys/tty.h>
 #include <sys/conf.h>
 #include <sys/vnode.h>
-#include <sys/conf.h>
 
 #include "fdc.h"
 bdev_decl(fd);
