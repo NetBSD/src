7d18db6f68ebe67220068f07b12a934452294fe7
lukem <lukem@NetBSD.org>
2001-10-05 15:32:57 +0000

    * Implement support for absolute paths in the specfile.  An absolute path   is one that contains a `/' character that is not the first character.   All parent directories referenced in the path must exist. * Add copyright for all the work I've done. * Document the history of various features added since 4.3-Reno.

--- usr.sbin/mtree/spec.c
+++ usr.sbin/mtree/spec.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: spec.c,v 1.30 2001/10/05 13:14:56 lukem Exp $	*/
+/*	$NetBSD: spec.c,v 1.31 2001/10/05 15:32:57 lukem Exp $	*/
 
 /*-
  * Copyright (c) 1989, 1993
@@ -33,12 +33,48 @@
  * SUCH DAMAGE.
  */
 
+/*-
+ * Copyright (c) 2001 The NetBSD Foundation, Inc.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to The NetBSD Foundation
+ * by Luke Mewburn of Wasabi Systems.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *        This product includes software developed by the NetBSD
+ *        Foundation, Inc. and its contributors.
+ * 4. Neither the name of The NetBSD Foundation nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #include <sys/cdefs.h>
 #ifndef lint
 #if 0
 static char sccsid[] = "@(#)spec.c	8.2 (Berkeley) 4/28/95";
 #else
-__RCSID("$NetBSD: spec.c,v 1.30 2001/10/05 13:14:56 lukem Exp $");
+__RCSID("$NetBSD: spec.c,v 1.31 2001/10/05 15:32:57 lukem Exp $");
 #endif
 #endif /* not lint */
 
@@ -67,13 +103,16 @@ static void	 unset(char *, NODE *);
 NODE *
 spec(void)
 {
-	NODE *centry, *last;
-	char *p;
+	NODE *centry, *last, *pathparent, *cur;
+	char *p, *e;
 	NODE ginfo, *root;
-	char *buf;
+	char *buf, *tname;
+	size_t tnamelen, plen;
 
 	root = NULL;
 	centry = last = NULL;
+	tname = NULL;
+	tnamelen = 0;
 	memset(&ginfo, 0, sizeof(ginfo));
 	for (lineno = 0;
 	    (buf = fparseln(stdin, NULL, &lineno, NULL,
@@ -105,12 +144,9 @@ spec(void)
 			continue;
 		}
 
-		if (strchr(p, '/'))
-			mtree_err("slash character in file name");
-
-		if (!strcmp(p, "..")) {
+		if (strcmp(p, "..") == 0) {
 			/* Don't go up, if haven't gone down. */
-			if (!root)
+			if (root == NULL)
 				goto noparent;
 			if (last->type != F_DIR || last->flags & F_DONE) {
 				if (last == root)
@@ -123,19 +159,69 @@ spec(void)
 noparent:		mtree_err("no parent node");
 		}
 
+		plen = strlen(p) + 1;
+		if (plen > tnamelen) {
+			tnamelen = plen;
+			if ((tname = realloc(tname, tnamelen)) == NULL)
+				mtree_err("realloc: %s", strerror(errno));
+		}
+		if (strunvis(tname, p) == -1)
+			mtree_err("strunvis failed on `%s'", p);
+		p = tname;
+
+		pathparent = NULL;
+		if (strchr(p, '/') != NULL) {
+			cur = root;
+			for (; (e = strchr(p, '/')) != NULL; p = e+1) {
+				if (p == e)
+					continue;	/* handle // */
+				*e = '\0';
+				if (strcmp(p, ".") != 0) {
+					while (cur && strcmp(cur->name, p)) {
+						cur = cur->next;
+					}
+				}
+				if (cur == NULL || cur->type != F_DIR) {
+					mtree_err("%s: %s", tname,
+					    strerror(ENOENT));
+				}
+				*e = '/';
+				pathparent = cur;
+				cur = cur->child;
+			}
+			if (*p == '\0')
+				mtree_err("%s: empty leaf element", tname);
+			for (; cur != NULL; cur = cur->next) {
+				if (strcmp(cur->name, p) == 0)
+					mtree_err("%s: %s", p,
+					    strerror(EEXIST));
+			}
+		}
+
 		if ((centry = calloc(1, sizeof(NODE) + strlen(p))) == NULL)
 			mtree_err("%s", strerror(errno));
 		*centry = ginfo;
-                if (strunvis(centry->name, p) == -1)
-			mtree_err("strunvis failed on `%s'", p);
+		strcpy(centry->name, p);
 #define	MAGIC	"?*["
 		if (strpbrk(p, MAGIC))
 			centry->flags |= F_MAGIC;
 		set(NULL, centry);
 
-		if (!root) {
+		if (root == NULL) {
 			last = root = centry;
 			root->parent = root;
+		} else if (pathparent != NULL) {
+			centry->parent = pathparent;
+			cur = pathparent->child;
+			if (cur == NULL)
+				pathparent->child = centry;
+			else {
+				while (cur->next != NULL)
+					cur = cur->next;
+				cur->next = centry;
+				centry->prev = cur;
+			}
+			last = centry;
 		} else if (last->type == F_DIR && !(last->flags & F_DONE)) {
 			centry->parent = last;
 			last = last->child = centry;
@@ -187,7 +273,7 @@ dump_nodes(const char *dir, NODE *root)
 		}
 		if (MATCHFLAG(F_MODE))
 			printf("mode=%#o ", cur->st_mode);
-		if (MATCHFLAG(F_NLINK) /* && cur->st_nlink > 1 */)
+		if (MATCHFLAG(F_NLINK))
 			printf("nlink=%d ", cur->st_nlink);
 		if (MATCHFLAG(F_SLINK))
 			printf("link=%s ", cur->slink);
