e66a7cf6f89226a03b87a92fac09c344eac6483b
mrg <mrg@NetBSD.org>
2001-10-06 14:50:22 +0000

    sync with sparc: add cdtty

--- sys/arch/sparc64/sparc64/conf.c
+++ sys/arch/sparc64/sparc64/conf.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: conf.c,v 1.18 2001/09/15 17:51:02 mrg Exp $ */
+/*	$NetBSD: conf.c,v 1.19 2001/10/06 14:50:22 mrg Exp $ */
 
 /*
  * Copyright (c) 1992, 1993
@@ -88,6 +88,8 @@
 #include "com.h"
 #include "bpp.h"
 #include "magma.h"		/* has NMTTY and NMBPP */
+#include "siosixteen.h"
+cdev_decl(cdtty);
 
 #include "fdc.h"		/* has NFDC and NFD; see files.sparc */
 #include "bwtwo.h"
@@ -276,6 +278,7 @@ struct cdevsw	cdevsw[] =
 	cdev_disk_init(NRAID,raid),	/* 121: RAIDframe disk driver */
 	cdev_tty_init(NPCONS,pcons),	/* 122: PROM console */
 	cdev_pci_init(NPCI,pci),	/* 123: PCI bus access device */
+	cdev_tty_init(NCLCD,cdtty),	/* 124: Cirrus-Logic CD18xx */
 };
 int	nchrdev = sizeof(cdevsw) / sizeof(cdevsw[0]);
 
