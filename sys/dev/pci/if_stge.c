dec77834e2510220b5e83ef1b0115debc72a8242
thorpej <thorpej@NetBSD.org>
2001-10-06 00:14:59 +0000

    Create the Tx DMA maps with a size large enough for a jumbo Ethernet frame.

--- sys/dev/pci/if_stge.c
+++ sys/dev/pci/if_stge.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: if_stge.c,v 1.6 2001/07/30 20:12:10 thorpej Exp $	*/
+/*	$NetBSD: if_stge.c,v 1.7 2001/10/06 00:14:59 thorpej Exp $	*/
 
 /*-
  * Copyright (c) 2001 The NetBSD Foundation, Inc.
@@ -516,8 +516,8 @@ stge_attach(struct device *parent, struct device *self, void *aux)
 	 * such chips to 1.
 	 */
 	for (i = 0; i < STGE_NTXDESC; i++) {
-		if ((error = bus_dmamap_create(sc->sc_dmat, MCLBYTES,
-		    STGE_NTXFRAGS, MCLBYTES, 0, 0,
+		if ((error = bus_dmamap_create(sc->sc_dmat,
+		    ETHER_MAX_LEN_JUMBO, STGE_NTXFRAGS, MCLBYTES, 0, 0,
 		    &sc->sc_txsoft[i].ds_dmamap)) != 0) {
 			printf("%s: unable to create tx DMA map %d, "
 			    "error = %d\n", sc->sc_dev.dv_xname, i, error);
