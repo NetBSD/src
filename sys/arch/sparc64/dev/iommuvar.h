bb99dc52c763f15379ac52971ddf9d9df7eeac67
eeh <eeh@NetBSD.org>
2001-10-07 20:30:40 +0000

    Manage both streaming caches on psycho/psycho+.

--- sys/arch/sparc64/dev/iommuvar.h
+++ sys/arch/sparc64/dev/iommuvar.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: iommuvar.h,v 1.8 2001/09/15 06:55:50 eeh Exp $	*/
+/*	$NetBSD: iommuvar.h,v 1.9 2001/10/07 20:30:41 eeh Exp $	*/
 
 /*
  * Copyright (c) 1999 Matthew R. Green
@@ -44,12 +44,12 @@ struct iommu_state {
 
 	paddr_t			is_flushpa;	/* used to flush the SBUS */
 	/* Needs to be volatile or egcs optimizes away loads */
-	volatile int64_t	is_flush;
+	volatile int64_t	is_flush[2];
 
 	/* copies of our parents state, to allow us to be self contained */
 	bus_space_tag_t		is_bustag;	/* our bus tag */
 	struct iommureg		*is_iommu;	/* IOMMU registers */
-	struct iommu_strbuf	*is_sb;		/* streaming buffer */
+	struct iommu_strbuf	*is_sb[2];	/* streaming buffer(s) */
 };
 
 /* interfaces for PCI/SBUS code */
