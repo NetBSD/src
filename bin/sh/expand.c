9f8faff302543d38951926d0f84ec2bfde21a99a
he <he@NetBSD.org>
2001-10-08 19:50:50 +0000

    Pull up revision 1.52 (requested by itojun):   Do not truncate expr > 10 digits.  Fixes PR#13943.

--- bin/sh/expand.c
+++ bin/sh/expand.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: expand.c,v 1.49 2000/03/13 22:47:19 soren Exp $	*/
+/*	$NetBSD: expand.c,v 1.49.4.1 2001/10/08 19:50:50 he Exp $	*/
 
 /*-
  * Copyright (c) 1991, 1993
@@ -41,7 +41,7 @@
 #if 0
 static char sccsid[] = "@(#)expand.c	8.5 (Berkeley) 5/15/95";
 #else
-__RCSID("$NetBSD: expand.c,v 1.49 2000/03/13 22:47:19 soren Exp $");
+__RCSID("$NetBSD: expand.c,v 1.49.4.1 2001/10/08 19:50:50 he Exp $");
 #endif
 #endif /* not lint */
 
@@ -371,7 +371,10 @@ expari(flag)
 	 * have to rescan starting from the beginning since CTLESC
 	 * characters have to be processed left to right.
 	 */
-	CHECKSTRSPACE(8, expdest);
+#if INT_MAX / 1000000000 >= 10 || INT_MIN / 1000000000 <= -10
+#error "integers with more than 10 digits are not supported"
+#endif
+	CHECKSTRSPACE(12 - 2, expdest);
 	USTPUTC('\0', expdest);
 	start = stackblock();
 	p = expdest - 1;
@@ -393,7 +396,7 @@ expari(flag)
 	if (quotes)
 		rmescapes(p+2);
 	result = arith(p+2);
-	fmtstr(p, 10, "%d", result);
+	fmtstr(p, 12, "%d", result);
 
 	while (*p++)
 		;
