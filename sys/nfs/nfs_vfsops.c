306582265053723604236abbccbaf57a6ab30ad0
chs <chs@NetBSD.org>
2001-10-08 00:22:13 +0000

    revert a change that I accidentally included with ubcperf.

--- sys/nfs/nfs_vfsops.c
+++ sys/nfs/nfs_vfsops.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: nfs_vfsops.c,v 1.109 2001/09/20 08:22:04 chs Exp $	*/
+/*	$NetBSD: nfs_vfsops.c,v 1.110 2001/10/08 00:22:13 chs Exp $	*/
 
 /*
  * Copyright (c) 1989, 1993, 1995
@@ -883,11 +883,11 @@ loop:
 		 */
 		if (vp->v_mount != mp)
 			goto loop;
-		if (waitfor == MNT_LAZY ||
+		if (waitfor == MNT_LAZY || VOP_ISLOCKED(vp) ||
 		    (LIST_EMPTY(&vp->v_dirtyblkhd) &&
 		     vp->v_uobj.uo_npages == 0))
 			continue;
-		if (vget(vp, LK_EXCLUSIVE | LK_NOWAIT))
+		if (vget(vp, LK_EXCLUSIVE))
 			goto loop;
 		error = VOP_FSYNC(vp, cred,
 		    waitfor == MNT_WAIT ? FSYNC_WAIT : 0, 0, 0, p);
