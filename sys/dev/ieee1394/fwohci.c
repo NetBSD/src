7e5aea588a39f9b3dd8b3d55e4ccf7da4dd67c23
haya <haya@NetBSD.org>
2001-10-12 09:27:23 +0000

    Bugfix: re-order double buffer only when second buffer exists.

--- sys/dev/ieee1394/fwohci.c
+++ sys/dev/ieee1394/fwohci.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: fwohci.c,v 1.43 2001/09/18 16:11:00 tsutsui Exp $	*/
+/*	$NetBSD: fwohci.c,v 1.44 2001/10/12 09:27:23 haya Exp $	*/
 
 #define DOUBLEBUF 1
 #define NO_THREAD 1
@@ -938,7 +938,9 @@ fwohci_ctx_alloc(struct fwohci_softc *sc, struct fwohci_ctx **fcp,
 	struct fwohci_ctx *fc;
 	struct fwohci_buf *fb;
 	struct fwohci_desc *fd;
+#if DOUBLEBUF
 	int buf2cnt;
+#endif
 
 	fc = malloc(sizeof(*fc) + sizeof(*fb) * bufcnt, M_DEVBUF, M_WAITOK);
 	memset(fc, 0, sizeof(*fc) + sizeof(*fb) * bufcnt);
@@ -1021,7 +1023,8 @@ fwohci_ctx_free(struct fwohci_softc *sc, struct fwohci_ctx *fc)
 	struct fwohci_handler *fh;
 
 #if DOUBLEBUF
-	if (TAILQ_FIRST(&fc->fc_buf) > TAILQ_FIRST(&fc->fc_buf2)) {
+	if ((fc->fc_type == FWOHCI_CTX_ISO_MULTI) &&
+	    (TAILQ_FIRST(&fc->fc_buf) > TAILQ_FIRST(&fc->fc_buf2))) {
 		struct fwohci_buf_s fctmp;
 
 		fctmp = fc->fc_buf;
