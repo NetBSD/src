8d9894a8225b94d6a778cf93449e6f7c69292877
simonb <simonb@NetBSD.org>
2001-10-13 22:59:16 +0000

    Don't initialise the 5th element of some 4 element arrays.

--- sys/nfs/nfs_socket.c
+++ sys/nfs/nfs_socket.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: nfs_socket.c,v 1.67 2001/05/09 20:02:32 fvdl Exp $	*/
+/*	$NetBSD: nfs_socket.c,v 1.68 2001/10/13 22:59:16 simonb Exp $	*/
 
 /*
  * Copyright (c) 1989, 1991, 1993, 1995
@@ -293,9 +293,9 @@ nfs_connect(nmp, rep)
 
 	/* Initialize other non-zero congestion variables */
 	nmp->nm_srtt[0] = nmp->nm_srtt[1] = nmp->nm_srtt[2] = nmp->nm_srtt[3] =
-		nmp->nm_srtt[4] = (NFS_TIMEO << 3);
+		NFS_TIMEO << 3;
 	nmp->nm_sdrtt[0] = nmp->nm_sdrtt[1] = nmp->nm_sdrtt[2] =
-		nmp->nm_sdrtt[3] = nmp->nm_sdrtt[4] = 0;
+		nmp->nm_sdrtt[3] = 0;
 	nmp->nm_cwnd = NFS_MAXCWND / 2;	    /* Initial send window */
 	nmp->nm_sent = 0;
 	nmp->nm_timeouts = 0;
