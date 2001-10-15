a84ae73f33eb1b950ab3e75c3d425a9b7ae62d7c
kleink <kleink@NetBSD.org>
2001-10-15 19:49:16 +0000

    Unify sparc{,64} headers once again; restore 32-bit sparc64 ABI.

--- sys/arch/sparc/include/int_mwgwtypes.h
+++ sys/arch/sparc/include/int_mwgwtypes.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: int_mwgwtypes.h,v 1.3 2001/10/14 20:11:11 kleink Exp $	*/
+/*	$NetBSD: int_mwgwtypes.h,v 1.4 2001/10/15 19:49:16 kleink Exp $	*/
 
 /*-
  * Copyright (c) 2001 The NetBSD Foundation, Inc.
@@ -68,12 +68,27 @@ typedef	unsigned long long int	uint_least64_t;
 
 /* 7.18.1.3 Fastest minimum-width integer types */
 
+#ifdef __arch64__
+typedef	long int		   int_fast8_t;
+typedef	unsigned char		  uint_fast8_t;
+#else
 typedef	int			   int_fast8_t;
 typedef	unsigned int		  uint_fast8_t;
+#endif
+#ifdef __arch64__
+typedef	long int		  int_fast16_t;
+typedef	unsigned short int	 uint_fast16_t;
+#else
 typedef	int			  int_fast16_t;
 typedef	unsigned int		 uint_fast16_t;
+#endif
+#ifdef __arch64__
+typedef	long int		  int_fast32_t;
+typedef	unsigned int		 uint_fast32_t;
+#else
 typedef	int			  int_fast32_t;
 typedef	unsigned int		 uint_fast32_t;
+#endif
 #ifdef __COMPILER_INT64__
 typedef	__COMPILER_INT64__	  int_fast64_t;
 typedef	__COMPILER_UINT64__	 uint_fast64_t;
