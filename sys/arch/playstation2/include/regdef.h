b92ec2263913e14576f48fd70c5b7e38a0cad616
uch <uch>
2001-10-16 15:38:29 +0000

    playstation2 port

---
+++ sys/arch/playstation2/include/regdef.h
@@ -0,0 +1,3 @@
+/*	$NetBSD: regdef.h,v 1.1 2001/10/16 15:38:50 uch Exp $	*/
+
+#include <mips/regdef.h>
---
+++ sys/arch/playstation2/include/regnum.h
@@ -0,0 +1,3 @@
+/*	$NetBSD: regnum.h,v 1.1 2001/10/16 15:38:50 uch Exp $	*/
+
+#include <mips/regnum.h>
