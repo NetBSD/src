f1d72a84b0109b856b9d91b732fd4a798479416a
mhitch <mhitch@NetBSD.org>
2001-10-18 02:19:54 +0000

    We have device_register().

--- sys/arch/sgimips/include/types.h
+++ sys/arch/sgimips/include/types.h
@@ -1,5 +1,6 @@
-/*	$NetBSD: types.h,v 1.2 2001/05/11 04:42:41 thorpej Exp $	*/
+/*	$NetBSD: types.h,v 1.3 2001/10/18 02:19:54 mhitch Exp $	*/
 
 #include <mips/types.h>
 
+#define	__HAVE_DEVICE_REGISTER
 #define	__HAVE_GENERIC_SOFT_INTERRUPTS
