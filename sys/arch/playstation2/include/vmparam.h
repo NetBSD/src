6d90c3e4d544e64b14e07ce562050a3747689565
shin <shin@NetBSD.org>
2001-10-19 05:47:41 +0000

    cosmetic change.

---
+++ sys/arch/playstation2/include/vmparam.h
@@ -0,0 +1,13 @@
+/*	$NetBSD: vmparam.h,v 1.2.4.2 2001/10/19 05:47:41 shin Exp $	*/
+
+#include <mips/vmparam.h>
+
+/*
+ * PlayStation 2 has one physical memory segment.
+ *	0 ... kernel itself
+ *	1 ... heap (continuous)
+ */
+#define	VM_PHYSSEG_MAX		2
+
+#define	VM_NFREELIST		1
+#define	VM_FREELIST_DEFAULT	0
