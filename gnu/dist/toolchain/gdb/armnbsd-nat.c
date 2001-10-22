ab53fca0eaaa71153e0b708f350b4ec345837029
bjh21 <bjh21>
2001-10-22 21:09:47 +0000

    Replace Mark Brinicombe's NetBSD/arm glue with Richard Earnshaw's, since the latter is likely to be easier to get into the GNU sources.  I believe the only functional change is the loss of a working get_longjmp_target().

--- gnu/dist/toolchain/gdb/armnbsd-nat.c
+++ gnu/dist/toolchain/gdb/armnbsd-nat.c
@@ -1,74 +1,71 @@
-/* Native-dependent code for BSD Unix running on i386's, for GDB.
-   Copyright 1988, 1989, 1991, 1992, 1994, 1996 Free Software Foundation, Inc.
+/* Native-dependent code for BSD Unix running on ARM's, for GDB.
+   Copyright 1988, 1989, 1991, 1992, 1994, 1996, 1999 Free Software Foundation, Inc.
 
-This file is part of GDB.
+   This file is part of GDB.
 
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.  */
 
 #include "defs.h"
 
+#ifdef FETCH_INFERIOR_REGISTERS
 #include <sys/types.h>
-#include <sys/param.h>
 #include <sys/ptrace.h>
 #include <sys/sysctl.h>
-#include <sys/user.h>
 #include <machine/reg.h>
+#include <machine/frame.h>
 #include "inferior.h"
-#include "gdbcore.h"
-#include "target.h"
 
 void
-fetch_inferior_registers(regno)
+fetch_inferior_registers (regno)
      int regno;
 {
   struct reg inferior_registers;
-  struct fpreg fp_registers;
-  int loop;
+  struct fpreg inferior_fpregisters;
 
-  /* integer registers */
-  ptrace(PT_GETREGS, inferior_pid, (PTRACE_ARG3_TYPE) &inferior_registers, 0);
-  memcpy(&registers[REGISTER_BYTE(0)], &inferior_registers, 4*16);
-  memcpy(&registers[REGISTER_BYTE(PS_REGNUM)], &inferior_registers.r_cpsr, 4);
-
-  /* floating point registers */
-  ptrace(PT_GETFPREGS, inferior_pid, (PTRACE_ARG3_TYPE) &fp_registers, 0);
-  memcpy(&registers[REGISTER_BYTE(F0_REGNUM)], &fp_registers.fpr[0], 12*8);
-  memcpy(&registers[REGISTER_BYTE(FPS_REGNUM)], &fp_registers.fpr_fpsr, 4);
-
+  ptrace (PT_GETREGS, inferior_pid, (PTRACE_ARG3_TYPE) &inferior_registers, 0);
+  memcpy (&registers[REGISTER_BYTE (0)], &inferior_registers,
+	  16 * sizeof (unsigned int));
+  memcpy (&registers[REGISTER_BYTE (PS_REGNUM)], &inferior_registers.r_cpsr,
+	  sizeof (unsigned int));
+  ptrace (PT_GETFPREGS, inferior_pid, (PTRACE_ARG3_TYPE) &inferior_fpregisters,
+	  0);
+  memcpy (&registers[REGISTER_BYTE (F0_REGNUM)], &inferior_fpregisters.fpr[0],
+	  8 * sizeof (fp_reg_t));
+  memcpy (&registers[REGISTER_BYTE (FPS_REGNUM)],
+	  &inferior_fpregisters.fpr_fpsr, sizeof (unsigned int));
   registers_fetched ();
 }
 
 void
-store_inferior_registers(regno)
+store_inferior_registers (regno)
      int regno;
 {
   struct reg inferior_registers;
-  struct fpreg fp_registers;
 
-  /* integer registers */
-  memcpy(&inferior_registers.r_cpsr, &registers[REGISTER_BYTE(PS_REGNUM)], 4);
-  memcpy(&inferior_registers, &registers[REGISTER_BYTE(0)], 4*16);
-  ptrace(PT_SETREGS, inferior_pid, (PTRACE_ARG3_TYPE) &inferior_registers, 0);
+  memcpy (&inferior_registers, &registers[REGISTER_BYTE (0)],
+	  16 * sizeof (unsigned int));
+  memcpy (&inferior_registers.r_cpsr, &registers[REGISTER_BYTE (PS_REGNUM)],
+	  sizeof (unsigned int));
+  ptrace (PT_SETREGS, inferior_pid, (PTRACE_ARG3_TYPE) &inferior_registers, 0);
 
-  /* floating point registers */
-  memcpy(&fp_registers.fpr_fpsr, &registers[REGISTER_BYTE(FPS_REGNUM)], 4);
-  memcpy(&fp_registers.fpr[0], &registers[REGISTER_BYTE(F0_REGNUM)], 12*8);
-  ptrace(PT_SETFPREGS, inferior_pid, (PTRACE_ARG3_TYPE) &fp_registers, 0);
+  /* XXX Set FP regs. */
 }
 
-struct md_core {
+struct md_core
+{
   struct reg intreg;
   struct fpreg freg;
 };
@@ -78,53 +75,31 @@ fetch_core_registers (core_reg_sect, core_reg_size, which, ignore)
      char *core_reg_sect;
      unsigned core_reg_size;
      int which;
-     unsigned int ignore;
+     CORE_ADDR ignore;
 {
-  struct md_core *core_reg = (struct md_core *)core_reg_sect;
+  struct md_core *core_reg = (struct md_core *) core_reg_sect;
 
   /* integer registers */
-  memcpy(&registers[REGISTER_BYTE(0)], &core_reg->intreg, 4*16);
-  memcpy(&registers[REGISTER_BYTE(PS_REGNUM)], &core_reg->intreg.r_cpsr, 4);
-  
+  memcpy (&registers[REGISTER_BYTE (0)], &core_reg->intreg,
+	  sizeof (struct reg));
   /* floating point registers */
-  memcpy(&registers[REGISTER_BYTE(F0_REGNUM)], &core_reg->freg.fpr[0], 12*8);
-  memcpy(&registers[REGISTER_BYTE(FPS_REGNUM)], &core_reg->freg.fpr_fpsr, 4);
+  /* XXX */
 }
 
-int
-kernel_u_size ()
+#else
+#error Not FETCH_INFERIOR_REGISTERS 
+#endif /* !FETCH_INFERIOR_REGISTERS */
+
+int 
+get_longjmp_target (CORE_ADDR *addr)
 {
-  return (sizeof (struct user));
+  return 0;
 }
-
-/*
- * NetBSD core stuf should be in netbsd-core.c
- */
-
-static struct core_fns netbsd_core_fns =
-{
-  bfd_target_unknown_flavour,
-  default_check_format,
-  default_core_sniffer,
-  fetch_core_registers,
-  NULL
-};
-
-void
-_initialize_core_netbsd ()
-{
-  add_core_fns (&netbsd_core_fns);
-}
-
-/*
- * Temporary routine to warn folks this code is still experimental
- */
 
 extern int arm_apcs_32;
 
-extern char *target_name;
 void
-_initialize_armnbsd_nat ()
+_initialize_armnbsd_nat (void)
 {
   int mib[2];
   char machine[16];
@@ -137,9 +112,8 @@ _initialize_armnbsd_nat ()
      appropriate. */
   mib[0] = CTL_HW;
   mib[1] = HW_MACHINE;
-  len = sizeof(machine);
-  if (sysctl(mib, 2, machine, &len, NULL, 0) == 0 &&
-      strcmp(machine, "arm26") == 0)
+  len = sizeof (machine);
+  if (sysctl (mib, 2, machine, &len, NULL, 0) == 0 &&
+      strcmp (machine, "arm26") == 0)
     arm_apcs_32 = 0;
 }
-
