664561e76f8db5b0d8814b7ad18223111fc7c9c8
mrg <mrg@NetBSD.org>
2001-10-22 07:31:41 +0000

    implement `options BLINK' for sparc64 for ebus & sbus.  tested on a U2 (sbus) and a U5 (ebus).

--- sys/arch/sparc64/dev/auxio.c
+++ sys/arch/sparc64/dev/auxio.c
@@ -1,7 +1,7 @@
-/*	$NetBSD: auxio.c,v 1.1 2000/04/15 03:08:13 mrg Exp $	*/
+/*	$NetBSD: auxio.c,v 1.2 2001/10/22 07:31:41 mrg Exp $	*/
 
 /*
- * Copyright (c) 2000 Matthew R. Green
+ * Copyright (c) 2000, 2001 Matthew R. Green
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -29,11 +29,14 @@
  */
 
 /*
- * AUXIO registers support on the sbus & ebus2.
+ * AUXIO registers support on the sbus & ebus2, used for the floppy driver
+ * and to control the system LED, for the BLINK option.
  */
 
 #include <sys/param.h>
 #include <sys/systm.h>
+#include <sys/kernel.h>
+#include <sys/callout.h>
 #include <sys/errno.h>
 #include <sys/device.h>
 #include <sys/malloc.h>
@@ -41,29 +44,106 @@
 #include <machine/autoconf.h>
 #include <machine/cpu.h>
 
-#include <sparc64/dev/ebusreg.h>
+#include <dev/ebus/ebusreg.h>
 #include <sparc64/dev/ebusvar.h>
 #include <sparc64/dev/sbusvar.h>
 #include <sparc64/dev/auxioreg.h>
-#include <sparc64/dev/auxiovar.h>
+
+/*
+ * on sun4u, auxio exists with one register (LED) on the sbus, and 5
+ * registers on the ebus2 (pci) (LED, PCIMODE, FREQUENCY, SCSI
+ * OSCILLATOR, and TEMP SENSE.
+ */
+
+struct auxio_softc {
+	struct device		sc_dev;
+
+	/* parent's tag */
+	bus_space_tag_t		sc_tag;
+
+	/* handles to the various auxio regsiter sets */
+	bus_space_handle_t	sc_led;
+	bus_space_handle_t	sc_pci;
+	bus_space_handle_t	sc_freq;
+	bus_space_handle_t	sc_scsi;
+	bus_space_handle_t	sc_temp;
+
+	int			sc_flags;
+#define	AUXIO_LEDONLY		0x1
+#define	AUXIO_EBUS		0x2
+#define	AUXIO_SBUS		0x4
+};
 
 #define	AUXIO_ROM_NAME		"auxio"
 
-/*
- * ebus code.
- */
-int	auxio_ebus_match __P((struct device *, struct cfdata *, void *));
-void	auxio_ebus_attach __P((struct device *, struct device *, void *));
-int	auxio_sbus_match __P((struct device *, struct cfdata *, void *));
-void	auxio_sbus_attach __P((struct device *, struct device *, void *));
+void	auxio_attach_common(struct auxio_softc *);
+int	auxio_ebus_match(struct device *, struct cfdata *, void *);
+void	auxio_ebus_attach(struct device *, struct device *, void *);
+int	auxio_sbus_match(struct device *, struct cfdata *, void *);
+void	auxio_sbus_attach(struct device *, struct device *, void *);
 
 struct cfattach auxio_ebus_ca = {
 	sizeof(struct auxio_softc), auxio_ebus_match, auxio_ebus_attach
 };
-
 struct cfattach auxio_sbus_ca = {
 	sizeof(struct auxio_softc), auxio_sbus_match, auxio_sbus_attach
 };
+
+#ifdef BLINK
+static struct callout blink_ch = CALLOUT_INITIALIZER;
+
+static void auxio_blink(void *);
+
+static void
+auxio_blink(x)
+	void *x;
+{
+	struct auxio_softc *sc = x;
+	int s;
+	u_int32_t led;
+
+	s = splhigh();
+	if (sc->sc_flags & AUXIO_EBUS)
+		led = le32toh(bus_space_read_4(sc->sc_tag, sc->sc_led, 0));
+	else
+		led = bus_space_read_1(sc->sc_tag, sc->sc_led, 0);
+	if (led & AUXIO_LED_LED)
+		led = 0;
+	else
+		led = AUXIO_LED_LED;
+	if (sc->sc_flags & AUXIO_EBUS)
+		bus_space_write_4(sc->sc_tag, sc->sc_led, 0, htole32(led));
+	else
+		bus_space_write_1(sc->sc_tag, sc->sc_led, 0, led);
+	splx(s);
+
+	/*
+	 * Blink rate is:
+	 *	full cycle every second if completely idle (loadav = 0)
+	 *	full cycle every 2 seconds if loadav = 1
+	 *	full cycle every 3 seconds if loadav = 2
+	 * etc.
+	 */
+	s = (((averunnable.ldavg[0] + FSCALE) * hz) >> (FSHIFT + 1));
+	callout_reset(&blink_ch, s, auxio_blink, sc);
+}
+#endif
+
+void
+auxio_attach_common(sc)
+	struct auxio_softc *sc;
+{
+	static int do_once = 1;
+
+#ifdef BLINK
+	/* only start one blinker */
+	if (do_once) {
+		auxio_blink(sc);
+		do_once = 0;
+	}
+#endif
+	printf("\n");
+}
 
 int
 auxio_ebus_match(parent, cf, aux)
@@ -95,17 +175,16 @@ auxio_ebus_attach(parent, self, aux)
 		sc->sc_flags = AUXIO_LEDONLY|AUXIO_EBUS;
 	} else {
 		sc->sc_flags = AUXIO_EBUS;
-		sc->sc_registers.auxio_pci = (u_int32_t *)(u_long)ea->ea_vaddrs[1];
-		sc->sc_registers.auxio_freq = (u_int32_t *)(u_long)ea->ea_vaddrs[2];
-		sc->sc_registers.auxio_scsi = (u_int32_t *)(u_long)ea->ea_vaddrs[3];
-		sc->sc_registers.auxio_temp = (u_int32_t *)(u_long)ea->ea_vaddrs[4];
+		sc->sc_pci = (bus_space_handle_t)(u_long)ea->ea_vaddrs[1];
+		sc->sc_freq = (bus_space_handle_t)(u_long)ea->ea_vaddrs[2];
+		sc->sc_scsi = (bus_space_handle_t)(u_long)ea->ea_vaddrs[3];
+		sc->sc_temp = (bus_space_handle_t)(u_long)ea->ea_vaddrs[4];
 	}
-	sc->sc_registers.auxio_led = (u_int32_t *)(u_long)ea->ea_vaddrs[0];
+	sc->sc_led = (bus_space_handle_t)(u_long)ea->ea_vaddrs[0];
 	
-	if (!auxio_reg)
-		auxio_reg = (u_char *)(u_long)ea->ea_vaddrs[0];
+	sc->sc_tag = ea->ea_bustag;
 
-	printf("\n");
+	auxio_attach_common(sc);
 }
 
 int
@@ -133,31 +212,16 @@ auxio_sbus_attach(parent, self, aux)
 	}
 
 	if (sa->sa_nreg != 1 || sa->sa_npromvaddrs != 1) {
-		printf(": not 1 (%d/%d) registers??", sa->sa_nreg, sa->sa_npromvaddrs);
+		printf(": not 1 (%d/%d) registers??", sa->sa_nreg,
+		    sa->sa_npromvaddrs);
 		return;
 	}
 
 	/* sbus auxio only has one set of registers */
 	sc->sc_flags = AUXIO_LEDONLY|AUXIO_SBUS;
-	sc->sc_registers.auxio_led = (u_int32_t *)(u_long)sa->sa_promvaddr;
+	sc->sc_led = (bus_space_handle_t)(u_long)sa->sa_promvaddr;
 
-	if (!auxio_reg)
-		auxio_reg = (u_char *)(u_long)sa->sa_promvaddr;
+	sc->sc_tag = sa->sa_bustag;
 
-	printf("\n");
-}
-
-/*
- * old interface; used by fd driver for now
- */
-unsigned int
-auxregbisc(bis, bic)
-	int bis, bic;
-{
-	register int v, s = splhigh();
-
-	v = *auxio_reg;
-	*auxio_reg = ((v | bis) & ~bic) | AUXIO_LED_MB1;
-	splx(s);
-	return v;
+	auxio_attach_common(sc);
 }
