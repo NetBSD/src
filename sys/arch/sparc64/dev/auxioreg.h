7fb7a7894314ffe73f2957e90e66256530682bcc
mrg <mrg>
2001-10-22 07:31:41 +0000

    implement `options BLINK' for sparc64 for ebus & sbus.  tested on a U2 (sbus) and a U5 (ebus).

--- sys/arch/sparc64/dev/auxioreg.h
+++ sys/arch/sparc64/dev/auxioreg.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: auxioreg.h,v 1.3 2000/04/15 03:08:13 mrg Exp $	*/
+/*	$NetBSD: auxioreg.h,v 1.4 2001/10/22 07:31:41 mrg Exp $	*/
 
 /*
  * Copyright (c) 2000 Matthew R. Green
@@ -46,17 +46,7 @@
 #define	AUXIO_POWER_COURTESY_OFF	0x1
 
 #define	AUXIO_LED			0x00726000
-#define	AUXIO_LED_LED			0x0
-#define	AUXIO_LED_MB1			0xf0	/* must be set on write */
-/* XXX: these may be useless on Ebus2 auxio! find out! */
-#define	AUXIO_LED_FHD	0x20		/* floppy: high density (unreliable?)*/
-#define	AUXIO_LED_FDC	0x10		/* floppy: diskette was changed */
-#define	AUXIO_LED_FDS	0x08		/* floppy: drive select */
-#define	AUXIO_LED_FTC	0x04		/* floppy: drives Terminal Count pin */
-#define	AUXIO_LED_FEJ	0x02		/* floppy: eject disk */
-#if 0
-#define	AUXIO_LED_LED	0x01		/* front panel LED */
-#endif
+#define	AUXIO_LED_LED			1
 
 #define	AUXIO_PCI			0x00728000
 #define	AUXIO_PCI_SLOT0			0x0	/* two bits each */
