23dbe4a38bb07f14488218eaf979a32b27a9b20e
he <he@NetBSD.org>
2001-10-25 17:59:35 +0000

    Pull up revisions 1.4-1.5 (via patch, requested by ad):   Add Mylex DACC960, CAC-EISA, and I2O block/SCSI drivers.

--- sys/dev/ic/cacreg.h
+++ sys/dev/ic/cacreg.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: cacreg.h,v 1.3 2000/06/13 13:36:47 ad Exp $	*/
+/*	$NetBSD: cacreg.h,v 1.3.2.1 2001/10/25 17:59:35 he Exp $	*/
 
 /*-
  * Copyright (c) 2000 The NetBSD Foundation, Inc.
@@ -60,34 +60,43 @@
  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
- *
- * FreeBSD: src/sys/dev/ida/idareg.h,v 1.2 1999/08/28 00:41:55 peter Exp
  */
 
 #ifndef _IC_CACREG_H_
-#define _IC_CACREG_H_
-
-#define cac_inl(sc, port) \
-	bus_space_read_4((sc)->sc_iot, (sc)->sc_ioh, port)
-#define cac_outl(sc, port, val) \
-	bus_space_write_4((sc)->sc_iot, (sc)->sc_ioh, port, val)
+#define	_IC_CACREG_H_
 
 /* Board register offsets */
-#define CAC_REG_CMD_FIFO	0x04
-#define CAC_REG_DONE_FIFO	0x08
-#define CAC_REG_INT_MASK	0x0C
-#define CAC_REG_STATUS		0x10
-#define CAC_REG_INT_PENDING	0x14
+#define	CAC_REG_CMD_FIFO		0x04
+#define	CAC_REG_DONE_FIFO		0x08
+#define	CAC_REG_INTR_MASK		0x0C
+#define	CAC_REG_STATUS			0x10
+#define	CAC_REG_INTR_PENDING		0x14
 
-#define CAC_42REG_CMD_FIFO	0x40
-#define CAC_42REG_DONE_FIFO	0x44
-#define CAC_42REG_INT_MASK	0x34
-#define CAC_42REG_STATUS	0x30
-#define CAC_42REG_INT_PENDING	0x08
+#define	CAC_42REG_CMD_FIFO		0x40
+#define	CAC_42REG_DONE_FIFO		0x44
+#define	CAC_42REG_INTR_MASK		0x34
+#define	CAC_42REG_STATUS		0x30
+
+#define	CAC_42_EXTINT			0x08
+
+#define	CAC_EISAREG_INTR_MASK		0x01
+#define	CAC_EISAREG_LOCAL_MASK		0x04
+#define	CAC_EISAREG_LOCAL_DOORBELL	0x05
+#define	CAC_EISAREG_SYSTEM_MASK		0x06
+#define	CAC_EISAREG_SYSTEM_DOORBELL	0x07
+#define	CAC_EISAREG_LIST_ADDR		0x08
+#define	CAC_EISAREG_LIST_LEN		0x0c
+#define	CAC_EISAREG_TAG			0x0f
+#define	CAC_EISAREG_COMPLETE_ADDR	0x10
+#define	CAC_EISAREG_LIST_STATUS		0x16
+
+/* EISA channel control */
+#define	CAC_EISA_CHANNEL_BUSY		0x01
+#define	CAC_EISA_CHANNEL_CLEAR		0x02
 
 /* Interrupt mask values */
-#define CAC_INT_DISABLE		0x00
-#define CAC_INT_ENABLE		0x01
+#define	CAC_INTR_DISABLE		0x00
+#define	CAC_INTR_ENABLE			0x01
 
 /* Command types */
 #define	CAC_CMD_GET_LOG_DRV_INFO	0x10
@@ -99,17 +108,18 @@
 #define	CAC_CMD_SENSE_DRV_LEDS		0x17
 #define	CAC_CMD_GET_LOG_DRV_EXT		0x18
 #define	CAC_CMD_GET_CTRL_INFO		0x11
-#define CAC_CMD_READ			0x20
-#define CAC_CMD_WRITE			0x30
-#define CAC_CMD_WRITE_MEDIA		0x31
-#define CAC_CMD_GET_CONFIG		0x50
-#define CAC_CMD_SET_CONFIG		0x51
-#define CAC_CMD_FLUSH_CACHE		0xc2
+#define	CAC_CMD_READ			0x20
+#define	CAC_CMD_WRITE			0x30
+#define	CAC_CMD_WRITE_MEDIA		0x31
+#define	CAC_CMD_GET_CONFIG		0x50
+#define	CAC_CMD_SET_CONFIG		0x51
+#define	CAC_CMD_START_FIRMWARE		0x99
+#define	CAC_CMD_FLUSH_CACHE		0xc2
 
 /* Return status codes */
-#define CAC_RET_SOFT_ERROR		0x02
-#define CAC_RET_HARD_ERROR		0x04
-#define CAC_RET_CMD_REJECTED		0x14
+#define	CAC_RET_SOFT_ERROR		0x02
+#define	CAC_RET_HARD_ERROR		0x04
+#define	CAC_RET_CMD_REJECTED		0x14
 
 struct cac_drive_info {
 	u_int16_t	secsize;
@@ -133,6 +143,37 @@ struct cac_controller_info {
 	u_int8_t	num_drvs;
 	u_int32_t	signature;
 	u_int8_t	firm_rev[4];
+	u_int8_t	rom_rev[4];
+	u_int8_t	hw_rev;
+	u_int32_t	bb_rev;
+	u_int32_t	drv_present_map;
+	u_int32_t	ext_drv_map;
+	u_int32_t	board_id;
+	u_int8_t	cfg_error;
+	u_int32_t	non_disk_bits;
+	u_int8_t	bad_ram_addr;
+	u_int8_t	cpu_rev;
+	u_int8_t	pdpi_rev;
+	u_int8_t	epic_rev;
+	u_int8_t	wcxc_rev;
+	u_int8_t	marketing_rev;
+	u_int8_t	ctlr_flags;
+	u_int8_t	host_flags;
+	u_int8_t	expand_dis;
+	u_int8_t	scsi_chips;
+	u_int32_t	max_req_blocks;
+	u_int32_t	ctlr_clock;
+	u_int8_t	drvs_per_bus;
+	u_int16_t	big_drv_present_map[8];
+	u_int16_t	big_ext_drv_map[8];
+	u_int16_t	big_non_disk_map[8];
+	u_int16_t	task_flags;
+	u_int8_t	icl_bus;
+	u_int8_t	red_modes;
+	u_int8_t	cur_red_mode;
+	u_int8_t	red_ctlr_stat;
+	u_int8_t	red_fail_reason;
+	u_int8_t	reserved[403];
 } __attribute__((__packed__));
 
 struct cac_hdr {
@@ -155,17 +196,5 @@ struct cac_sgb {
 	u_int32_t	length;		/* length of S/G segment */
 	u_int32_t	addr;		/* physical address of block */
 } __attribute__((__packed__));
-
-/*
- * Stupid macros to deal with alignment/endianness issues.
- */
-
-#define CAC_GET1(x)							\
-	(((u_char *)&(x))[0])
-#define CAC_GET2(x)							\
-	(((u_char *)&(x))[0] | (((u_char *)&(x))[1] << 8))
-#define CAC_GET4(x)							\
-	((((u_char *)&(x))[0] | (((u_char *)&(x))[1] << 8)) |		\
-	(((u_char *)&(x))[0] << 16 | (((u_char *)&(x))[1] << 24)))
 	
 #endif	/* !_IC_CACREG_H_ */
