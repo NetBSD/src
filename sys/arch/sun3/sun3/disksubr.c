ed8fcb27168f8e9f5bd09ad4e4dbf6b51c1d35b5
he <he@NetBSD.org>
2001-10-27 18:32:35 +0000

    Pull down revision 1.24.4.1 from the netbsd-1-5 branch: Make sure that an unlabeled device gets at least RAW_PART in the default in-core disklabel, instead of only a single partition, so that applying a new disklabel can work normally.

--- sys/arch/sun3/sun3/disksubr.c
+++ sys/arch/sun3/sun3/disksubr.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: disksubr.c,v 1.27 2001/09/05 14:12:21 tsutsui Exp $	*/
+/*	$NetBSD: disksubr.c,v 1.28 2001/10/27 18:32:35 he Exp $	*/
 
 /*
  * Copyright (c) 1994, 1995 Gordon W. Ross
@@ -87,7 +87,7 @@ readdisklabel(dev, strat, lp, clp)
 	/* minimal requirements for archtypal disk label */
 	if (lp->d_secperunit == 0)
 		lp->d_secperunit = 0x1fffffff;
-	lp->d_npartitions = 1;
+	lp->d_npartitions = RAW_PART + 1;
 	if (lp->d_partitions[0].p_size == 0)
 		lp->d_partitions[0].p_size = 0x1fffffff;
 	lp->d_partitions[0].p_offset = 0;
