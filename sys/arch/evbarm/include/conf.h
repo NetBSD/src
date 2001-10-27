3825268802ac9a9b559ae48c60b0a8ab845da06c
rearnsha <rearnsha@NetBSD.org>
2001-10-27 16:15:50 +0000

    Add SCSI PCI and support for the integrator console.

--- sys/arch/evbarm/include/conf.h
+++ sys/arch/evbarm/include/conf.h
@@ -1,10 +1,15 @@
+/*	$NetBSD: conf.h,v 1.2 2001/10/27 16:15:50 rearnsha Exp $ */
+
 #ifndef _EVBARM_CONF_H
 #define	_EVBARM_CONF_H
 
 /*
  * EVBARM specifc device includes go in here
  */
-#define	CONF_HAVE_SCSIPI
+#include "plcom.h"
+
+#define CONF_HAVE_PCI
+#define CONF_HAVE_SCSIPI
 #define	CONF_HAVE_USB
 #define	CONF_HAVE_WSCONS
 
