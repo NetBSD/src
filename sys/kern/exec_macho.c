91b85de9a7f25da1224492d75064365760a9f4c8
christos <christos>
2001-10-28 04:46:42 +0000

    make a copy of the epp->ep_path, because it is too late to use it when copyargs is called since we've swapped vmspaces.

--- sys/kern/exec_macho.c
+++ sys/kern/exec_macho.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: exec_macho.c,v 1.2 2001/07/14 03:05:31 christos Exp $	*/
+/*	$NetBSD: exec_macho.c,v 1.3 2001/10/28 04:46:42 christos Exp $	*/
 
 /*-
  * Copyright (c) 2001 The NetBSD Foundation, Inc.
@@ -226,7 +226,7 @@ exec_macho_load_dylinker(struct proc *p, struct exec_package *epp,
 static int
 exec_macho_load_dylib(struct proc *p, struct exec_package *epp,
     struct exec_macho_dylib_command *dy) {
-#ifdef notyet
+#ifdef notdef
 	const char *name = ((const char *)dy) + dy->dylib.name.offset;
 	char path[MAXPATHLEN];
 	int error;
@@ -235,7 +235,7 @@ exec_macho_load_dylib(struct proc *p, struct exec_package *epp,
 #ifdef DEBUG_MACHO
 	exec_macho_print_dylib_command(dy);
 #endif
-#ifdef notyet
+#ifdef notdef
 	(void)snprintf(path, sizeof(path), "%s%s",
 	    (const char *)epp->ep_emul_arg, name);
 	DPRINTF(("loading library %s\n", path));
@@ -539,6 +539,16 @@ exec_macho_makecmds(struct proc *p, struct exec_package *epp)
 	    &epp->ep_entry, MACHO_MOH_EXECUTE)) != 0)
 		goto bad;
 
+	/*
+	 * stash a copy of the program name in epp->ep_emul_arg because
+	 * might need it later.
+	 */
+	MALLOC(epp->ep_emul_arg, char *, MAXPATHLEN, M_EXEC, M_WAITOK);
+	if ((error = copyinstr(epp->ep_name, epp->ep_emul_arg,
+	    MAXPATHLEN, NULL)) != 0) {
+		DPRINTF(("Copyinstr %p failed\n", epp->ep_name));
+		goto bad;
+	}
 	vn_marktext(epp->ep_vp);
 	return exec_macho_setup_stack(p, epp);
 bad:
