542dee9053587813853af70d057770af14cd4b2a
thorpej <thorpej@NetBSD.org>
2001-10-29 19:04:24 +0000

    Add/rework infrastructure that will allow us to support native device drivers: - Various native device entries in cdevsw/bdevsw. - Rework the interrupt infrastructure to provide more flexibility to   the platform-dependent back-end.  Rewrite the "ofwgen" simulated   interrupt routines to reflect the changes. - Clear out the BAT registers and set the fixed battable entries before   calling the platform init routine.  The platform init routine is allowed   to set entries in the battable. - Don't call the platform cons_init routine until after translation is   enabled -- we might need translation to work in order to access bus   space.

--- sys/arch/ofppc/ofwgen/ofwgen.c
+++ sys/arch/ofppc/ofwgen/ofwgen.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: ofwgen.c,v 1.1 2001/10/22 23:01:18 thorpej Exp $	*/
+/*	$NetBSD: ofwgen.c,v 1.2 2001/10/29 19:04:26 thorpej Exp $	*/
 
 /*
  * Copyright 2001 Wasabi Systems, Inc.
@@ -63,11 +63,7 @@ ofwgen_init(void)
 	platform.cons_init = ofwgen_cons_init;
 	platform.device_register = ofwgen_device_register;
 
-	/*
-	 * Generic OpenFirmware configurations use software
-	 * simulated interrupts.
-	 */
-	machine_interface = ofwgen_machvec;
+	ofwgen_intr_init();
 }
 
 void
