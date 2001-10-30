747ef9fed7a5204e711eb27bf28a6df04eb05b66
wiz <wiz@NetBSD.org>
2001-10-30 15:17:16 +0000

    Add support for INCSNAME and INCSNAME_<file> to bsd.inc.mk. Same syntax as FILESNAME and FILESNAME_<file>, allows to install includes under a different name. Okay'd by christos.

--- share/mk/bsd.inc.mk
+++ share/mk/bsd.inc.mk
@@ -1,13 +1,13 @@
-#	$NetBSD: bsd.inc.mk,v 1.17 2001/05/08 03:19:52 sommerfeld Exp $
+#	$NetBSD: bsd.inc.mk,v 1.18 2001/10/30 15:17:17 wiz Exp $
 
 .PHONY:		incinstall
 includes:	${INCS} incinstall
 
 .if defined(INCS)
-incinstall:: ${INCS:@I@${DESTDIR}${INCSDIR}/$I@}
-.PRECIOUS: ${INCS:@I@${DESTDIR}${INCSDIR}/$I@}
+incinstall:: ${INCS:@I@${DESTDIR}${INCSDIR_${I}:U${INCSDIR}}/${INCSNAME_${I}:U${INCSNAME:U${I:T}}}@}
+.PRECIOUS: ${INCS:@I@${DESTDIR}${INCSDIR_${I}:U${INCSDIR}}/${INCSNAME_${I}:U${INCSNAME:U${I:T}}}@}
 .if !defined(UPDATE)
-.PHONY: ${INCS:@I@${DESTDIR}${INCSDIR}/$I@}
+.PHONY: ${INCS:@I@${DESTDIR}${INCSDIR_${I}:U${INCSDIR}}/${INCSNAME_${I}:U${INCSNAME:U${I:T}}}@}
 .endif
 
 __incinstall: .USE
@@ -19,7 +19,7 @@ __incinstall: .USE
 		 -g ${BINGRP} -m ${NONBINMODE} ${.ALLSRC} ${.TARGET})
 
 .for I in ${INCS:O:u}
-${DESTDIR}${INCSDIR}/$I: $I __incinstall
+${DESTDIR}${INCSDIR_${I}:U${INCSDIR}}/${INCSNAME_${I}:U${INCSNAME:U${I:T}}}: ${I} __incinstall
 .endfor
 .endif
 
