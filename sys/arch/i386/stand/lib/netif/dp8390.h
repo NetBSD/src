84e61da8529546d34a6a6ee18531d8b5a29ffc4d
yamt <yamt@NetBSD.org>
2001-11-01 09:37:17 +0000

    add ne2000 standalone driver.

--- sys/arch/i386/stand/lib/netif/dp8390.h
+++ sys/arch/i386/stand/lib/netif/dp8390.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: dp8390.h,v 1.3 1998/05/06 13:32:18 drochner Exp $	*/
+/*	$NetBSD: dp8390.h,v 1.4 2001/11/01 09:37:17 yamt Exp $	*/
 
 extern int dp8390_config __P((void));
 extern void dp8390_stop __P((void));
@@ -17,5 +17,17 @@ extern int dp8390_is790;
 #define dp8390_is790 1
 #endif
 #endif
+
+#ifdef SUPPORT_NE2000
+#define dp8390_is790 0
+#define IFNAME "ne"
+#define RX_BUFBASE 0
+#define TX_PAGE_START (dp8390_membase >> ED_PAGE_SHIFT)
+#else
+#define IFNAME "we"
+#define RX_BUFBASE dp8390_membase
+#define TX_PAGE_START 0
+#endif
+
 extern u_int8_t dp8390_cr_proto; /* values always set in CR */
 extern u_int8_t dp8390_dcr_reg; /* override DCR if LS is set */
