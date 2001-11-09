46b03d775b4473965c7f1a1138c11ac75ad07918
thorpej <thorpej@NetBSD.org>
2001-11-09 03:27:51 +0000

    Snapshot of work-in-progress for Intel i80312 Companion I/O chip; just basic Inbound and Outbound window setup is done, PCI configuration space access (not quite working yet), and I/O and Memory space routines so far.

--- sys/arch/arm/xscale/i80312var.h
+++ sys/arch/arm/xscale/i80312var.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: i80312var.h,v 1.2 2001/11/05 23:38:05 thorpej Exp $	*/
+/*	$NetBSD: i80312var.h,v 1.3 2001/11/09 03:27:52 thorpej Exp $	*/
 
 /*
  * Copyright (c) 2001 Wasabi Systems, Inc.
@@ -38,7 +38,132 @@
 #ifndef _ARM_XSCALE_I80312VAR_H_
 #define	_ARM_XSCALE_I80312VAR_H_
 
+#include <machine/bus.h>
+
+#include <dev/pci/pcivar.h>
+
+struct i80312_softc {
+	struct device sc_dev;		/* generic device glue */
+
+	int sc_is_host;			/* indicates if we're a host or
+					   plugged into another host */
+
+	/*
+	 * This is the bus_space and handle used to access the
+	 * i80312 itself.  This is filled in by the board-specific
+	 * front-end.
+	 */
+	bus_space_tag_t sc_st;
+	bus_space_handle_t sc_sh;
+
+	/* Handles for the various subregions. */
+	bus_space_handle_t sc_ppb_sh;
+	bus_space_handle_t sc_atu_sh;
+
+	/*
+	 * Secondary IDSEL Select bits for providing a private
+	 * PCI device space.
+	 */
+	uint16_t sc_sisr;
+
+	/*
+	 * We expect the board-specific front-end to have already mapped
+	 * the PCI I/O spaces .. they're only 64K each, and I/O mappings
+	 * tend to be smaller than a page size, so it's generally more
+	 * efficient to map them all into virtual space in one fell swoop.
+	 */
+	vaddr_t	sc_piow_vaddr;		/* primary I/O window vaddr */
+	vaddr_t sc_siow_vaddr;		/* secondary I/O window vaddr */
+
+	/*
+	 * Variables that define the Primary Inbound window.  The base
+	 * address is configured by a host via BAR #0.  The xlate variable
+	 * defines the start of the local address space that it maps to.
+	 * The size variable defines the byte size.
+	 *
+	 * This window is used for incoming PCI memory read/write cycles
+	 * from a host.
+	 *
+	 * ...unless we're a host, in which case we make the Primary
+	 * Inbound window work like the Secondary Inbound window, so
+	 * that PCI devices on that bus can talk to our local RAM.
+	 */
+	uint32_t sc_pin_base;
+	uint32_t sc_pin_xlate;
+	uint32_t sc_pin_size;
+
+	/*
+	 * Variables that define the Secondary Inbound window.  The
+	 * base variable indicates the PCI base address of the window.
+	 * The xlate variable defines the start of the local address
+	 * space that it maps to.  The size variable defines the byte
+	 * size.
+	 *
+	 * This window is used for DMA with devices on the secondary bus.
+	 */
+	uint32_t sc_sin_base;
+	uint32_t sc_sin_xlate;
+	uint32_t sc_sin_size;
+
+	/*
+	 * This is the PCI address that the Primary Outbound Memory
+	 * window maps to.
+	 */
+	uint32_t sc_pmemout_base;
+	uint32_t sc_pmemout_size;
+
+	/*
+	 * This is the PCI address that the Primary Outbound I/O
+	 * window maps to.
+	 */
+	uint32_t sc_pioout_base;
+	uint32_t sc_pioout_size;
+
+	/*
+	 * This is the PCI address that the Secondary Outbound Memory
+	 * window maps to.
+	 */
+	uint32_t sc_smemout_base;
+	uint32_t sc_smemout_size;
+
+	/*
+	 * This is the PCI address that the Secondary Outbound I/O
+	 * window maps to.
+	 */
+	uint32_t sc_sioout_base;
+	uint32_t sc_sioout_size;
+
+	/*
+	 * This defines the private I/O and Memory spaces on the
+	 * Secondary bus.
+	 */
+	uint32_t sc_privio_base;
+	uint32_t sc_privio_size;
+	uint32_t sc_privmem_base;
+	uint32_t sc_privmem_size;
+
+	uint8_t sc_sder;	/* secondary decode enable register */
+
+	/* Bus space, DMA, and PCI tags for the PCI bus (private devices). */
+	struct bus_space sc_pci_iot;
+	struct bus_space sc_pci_memt;
+	struct arm32_bus_dma_tag sc_pci_dmat;
+	struct arm32_pci_chipset sc_pci_chipset;
+};
+
+extern struct bus_space i80312_bs_tag;
+
 void	i80312_sdram_bounds(bus_space_tag_t, bus_space_handle_t,
 	    paddr_t *, psize_t *);
+
+void	i80312_attach(struct i80312_softc *);
+
+void	i80312_bs_init(bus_space_tag_t, void *);
+void	i80312_io_bs_init(bus_space_tag_t, void *);
+void	i80312_mem_bs_init(bus_space_tag_t, void *);
+
+void	i80312_pci_dma_init(bus_dma_tag_t, void *);
+
+void	i80312_pci_init(pci_chipset_tag_t, void *);
 
 #endif /* _ARM_XSCALE_I80312VAR_H_ */
