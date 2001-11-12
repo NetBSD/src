eb99bafa15009520914c446b0de185d40cab9d48
lukem <lukem>
2001-11-12 23:22:57 +0000

    add RCSIDs

--- sys/lkm/compat/netbsd32/lkminit_emul.c
+++ sys/lkm/compat/netbsd32/lkminit_emul.c
@@ -1,4 +1,4 @@
-/* $NetBSD: lkminit_emul.c,v 1.4 2001/06/06 21:34:23 mrg Exp $ */
+/* $NetBSD: lkminit_emul.c,v 1.5 2001/11/12 23:23:03 lukem Exp $ */
 
 /*-
  * Copyright (c) 1996 The NetBSD Foundation, Inc.
@@ -35,6 +35,9 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
+
+#include <sys/cdefs.h>
+__KERNEL_RCSID(0, "$NetBSD: lkminit_emul.c,v 1.5 2001/11/12 23:23:03 lukem Exp $");
 
 #include <sys/param.h>
 #include <sys/ioctl.h>
--- sys/lkm/vfs/filecorefs/lkminit_vfs.c
+++ sys/lkm/vfs/filecorefs/lkminit_vfs.c
@@ -1,4 +1,4 @@
-/* $NetBSD: lkminit_vfs.c,v 1.1 2001/01/22 13:41:51 jdolecek Exp $ */
+/* $NetBSD: lkminit_vfs.c,v 1.2 2001/11/12 23:23:30 lukem Exp $ */
 
 /*-
  * Copyright (c) 2001 The NetBSD Foundation, Inc.
@@ -35,6 +35,9 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
+
+#include <sys/cdefs.h>
+__KERNEL_RCSID(0, "$NetBSD: lkminit_vfs.c,v 1.2 2001/11/12 23:23:30 lukem Exp $");
 
 #include <sys/param.h>
 #include <sys/ioctl.h>
