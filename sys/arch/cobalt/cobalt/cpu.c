20a6f7c5e57a48b1051041f9aa88571f20e2468d
thorpej <thorpej>
2001-11-13 20:07:04 +0000

    Make the cobalt port build with thorpej-mips-cache.

--- sys/arch/cobalt/cobalt/cpu.c
+++ sys/arch/cobalt/cobalt/cpu.c
@@ -1,92 +1,57 @@
-/*	$NetBSD: cpu.c,v 1.2 2001/06/27 08:44:24 nisimura Exp $	*/
+/*	$NetBSD: cpu.c,v 1.2.6.1 2001/11/13 20:07:04 thorpej Exp $	*/
 
 /*
- * Copyright (c) 2000 Soren S. Jorvang.  All rights reserved.
+ * Copyright (c) 1994, 1995 Carnegie-Mellon University.
+ * All rights reserved.
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions, and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
+ * Author: Chris G. Demetriou
  *
- * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
+ * Permission to use, copy, modify and distribute this software and
+ * its documentation is hereby granted, provided that both the copyright
+ * notice and this permission notice appear in all copies of the
+ * software, derivative works or modified versions, and any portions
+ * thereof, and that both notices appear in supporting documentation.
+ *
+ * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS "AS IS"
+ * CONDITION.  CARNEGIE MELLON DISCLAIMS ANY LIABILITY OF ANY KIND
+ * FOR ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.
+ * 
+ * Carnegie Mellon requests users of this software to return to
+ *
+ *  Software Distribution Coordinator  or  Software.Distribution@CS.CMU.EDU
+ *  School of Computer Science
+ *  Carnegie Mellon University
+ *  Pittsburgh PA 15213-3890
+ *
+ * any improvements or extensions that they make and grant Carnegie the
+ * rights to redistribute these changes.
  */
 
 #include <sys/param.h>
 #include <sys/device.h>
 #include <sys/systm.h>
 
-#include <mips/locore.h>
-
-#include <machine/cpu.h>
 #include <machine/autoconf.h>
 
-static int	cpu_match(struct device *, struct cfdata *, void *);
-static void	cpu_attach(struct device *, struct device *, void *);
+int	cpu_match(struct device *, struct cfdata *, void *);
+void	cpu_attach(struct device *, struct device *, void *);
 
 struct cfattach cpu_ca = {
-	sizeof(struct device), cpu_match, cpu_attach
+	sizeof (struct device), cpu_match, cpu_attach
 };
+extern struct cfdriver cpu_cd;
 
-static int
-cpu_match(parent, match, aux)
-	struct device *parent;
-	struct cfdata *match;
-	void *aux;
+int
+cpu_match(struct device *parent, struct cfdata *cf, void *aux)
 {
-	return 1;
+
+	return (1);
 }
 
-static void
-cpu_attach(parent, self, aux)
-	struct device *parent;
-	struct device *self;
-	void *aux;
+void
+cpu_attach(struct device *parent, struct device *dev, void *aux)
 {
-	char *cpu_name, *fpu_name;
-	extern void rm52xx_idle(void);	/* power saving when idle */
 
-	printf("\n");
-	switch (MIPS_PRID_IMPL(cpu_id)) {
-	case MIPS_RM5200:
-		cpu_name = "QED RM5200 CPU";
-		fpu_name = "builtin FPA";
-		break;
-	default:
-		cpu_name = "Unknown CPU";
-		fpu_name = "Unknown FPA";
-		break;
-	}
-
-	printf("%s: %s (0x%04x) with %s (0x%04x)\n",
-	    self->dv_xname, cpu_name, cpu_id, fpu_name, fpu_id);
-
-	printf("%s: L1 cache: ", self->dv_xname);
-	printf("%dKB Instruction, %dKB Data, 2way set associative\n",
-	    mips_L1ICacheSize/1024, mips_L1DCacheSize/1024);
-	printf("%s: ", self->dv_xname);
-
-	if (!mips_L2CachePresent)
-		printf("no L2 cache\n");
-	else
-		printf("L2 cache: %dKB/%dB %s, %s\n",
-		    mips_L2CacheSize/1024, mips_L2CacheLSize,
-		    mips_L2CacheMixed ? "mixed" : "separated",
-		    mips_L2CacheIsSnooping? "snooping" : "no snooping");
-
-	/* XXX probably should not be here XXX */
-	CPU_IDLE = (long *) rm52xx_idle;
+	printf(": ");
+	cpu_identify();
 }
