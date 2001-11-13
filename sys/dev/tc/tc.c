6a3181d38f8f9567212f02103a5c083863510fbd
lukem <lukem@NetBSD.org>
2001-11-13 06:26:09 +0000

    add/cleanup RCSIDs

--- sys/dev/tc/tc.c
+++ sys/dev/tc/tc.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: tc.c,v 1.28 2000/06/04 19:15:14 cgd Exp $	*/
+/*	$NetBSD: tc.c,v 1.29 2001/11/13 06:26:10 lukem Exp $	*/
 
 /*
  * Copyright (c) 1994, 1995 Carnegie-Mellon University.
@@ -26,6 +26,9 @@
  * any improvements or extensions that they make and grant Carnegie the
  * rights to redistribute these changes.
  */
+
+#include <sys/cdefs.h>
+__KERNEL_RCSID(0, "$NetBSD: tc.c,v 1.29 2001/11/13 06:26:10 lukem Exp $");
 
 #include "opt_tcverbose.h"
 
