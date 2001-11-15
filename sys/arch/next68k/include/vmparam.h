667da20400c92d6a26595b8e641535e199dba797
soren <soren@NetBSD.org>
2001-11-15 18:06:11 +0000

    MAXSLP is defined to be a machine-independent scheduling parameter, so move it into sys/param.h.

--- sys/arch/next68k/include/vmparam.h
+++ sys/arch/next68k/include/vmparam.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: vmparam.h,v 1.12 2001/05/01 02:19:18 thorpej Exp $	*/
+/*	$NetBSD: vmparam.h,v 1.13 2001/11/15 18:06:16 soren Exp $	*/
 
 /*
  * This file was taken from from mvme68k/include/vmparam.h and
@@ -124,17 +124,6 @@
 #ifndef SHMMAXPGS
 #define SHMMAXPGS	1024		/* 4mb */
 #endif
-
-/*
- * The time for a process to be blocked before being very swappable.
- * This is a number of seconds which the system takes as being a non-trivial
- * amount of real time.  You probably shouldn't change this;
- * it is used in subtle ways (fractions and multiples of it are, that is, like
- * half of a ``long time'', almost a long time, etc.)
- * It is related to human patience and other factors which don't really
- * change over time.
- */
-#define	MAXSLP 		20
 
 /*
  * Mach derived constants
