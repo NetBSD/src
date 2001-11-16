419d5d3a6d2d50caaac849d2e24169bcde36c973
thorpej <thorpej@NetBSD.org>
2001-11-16 23:57:25 +0000

    First cut at machine-dependent pthread bits for ARM.

---
+++ lib/libpthread/arch/arm/pthread_md.h
@@ -0,0 +1,62 @@
+/*	$NetBSD: pthread_md.h,v 1.1.2.1 2001/11/16 23:57:26 thorpej Exp $	*/
+
+/*
+ * Copyright (c) 2001 Wasabi Systems, Inc.
+ * All rights reserved.
+ *
+ * Written by Jason R. Thorpe for Wasabi Systems, Inc.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed for the NetBSD Project by
+ *	Wasabi Systems, Inc.
+ * 4. The name of Wasabi Systems, Inc. may not be used to endorse
+ *    or promote products derived from this software without specific prior
+ *    written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY WASABI SYSTEMS, INC. ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL WASABI SYSTEMS, INC
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef _LIB_PTHREAD_ARM_MD_H
+#define	_LIB_PTHREAD_ARM_MD_H
+
+static __inline long
+pthread__sp(void)
+{
+	long ret;
+
+	__asm __volatile("mov %0, sp"
+		: "=r" (ret));
+
+	return (ret);
+}
+
+#define	pthread__uc_sp(ucp)	((ucp)->uc_mcontext.__gregs[_REG_SP])
+#define	pthread__uc_pc(ucp)	((ucp)->uc_mcontext.__gregs[_REG_PC])
+
+/*
+ * Usable stack space below the ucontext_t.
+ *    For a good time, see comments in pthread_switch.S and
+ *    ../i386/pthread_switch.S about STACK_SWITCH.
+ */
+#define	STACKSPACE		(6 * sizeof(long))
+
+#endif /* _LIB_PTHREAD_ARM_MD_H */
