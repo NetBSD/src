827e3c62541768a992978e32cf365152c92c4cce
lukem <lukem@NetBSD.org>
2001-11-20 12:56:17 +0000

    cleanup: 	options SPACE TAB 	makeoptions TAB 	psuedo-device TAB 	remove trailing whitespace 	replace multiple spaces -> tabs 	options "FOO" -> options FOO 	options "FOO=bar" -> options FOO=bar 	options "FOO=\"bar\"" -> options FOO="\"bar\""

--- sys/arch/evbsh3/conf/std.evbsh3.el
+++ sys/arch/evbsh3/conf/std.evbsh3.el
@@ -1,11 +1,11 @@
-# $NetBSD: std.evbsh3.el,v 1.2 2001/01/04 07:03:23 itojun Exp $
+# $NetBSD: std.evbsh3.el,v 1.3 2001/11/20 12:56:26 lukem Exp $
 #
 # standard, required NetBSD/evbsh3 'options'
- 
+
 machine evbsh3 sh3
 
-options	EXEC_SCRIPT	# exec #! scripts
+options 	EXEC_SCRIPT	# exec #! scripts
 
 makeoptions	ENDIAN="-EL"
-makeoptions 	LDSCRIPTBASE="shl.x"	# for little endian
-makeoptions 	MACHINE_ARCH=sh3el
+makeoptions	LDSCRIPTBASE="shl.x"	# for little endian
+makeoptions	MACHINE_ARCH=sh3el
