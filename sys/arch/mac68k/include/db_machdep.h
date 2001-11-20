a093384e0ff7c6cd80815c28199fa410b11173db
chs <chs@NetBSD.org>
2001-11-20 03:19:41 +0000

    prepare for ELF:  - add "%" prefix to register names in assembly code.  - use _C_LABEL() where necessary.  - use packed structures where necessary to match the old ABI.

--- sys/arch/mac68k/include/db_machdep.h
+++ sys/arch/mac68k/include/db_machdep.h
@@ -1,3 +1,5 @@
-/*	$NetBSD: db_machdep.h,v 1.3 1994/10/26 08:46:27 cgd Exp $	*/
+/*	$NetBSD: db_machdep.h,v 1.4 2001/11/20 03:19:42 chs Exp $	*/
 
 #include <m68k/db_machdep.h>
+#define DB_ELF_SYMBOLS
+#define DB_ELFSIZE 32
