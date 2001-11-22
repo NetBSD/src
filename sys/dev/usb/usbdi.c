2da2c0fb6a5d26485e565afaa43582b12ef89441
augustss <augustss@NetBSD.org>
2001-11-22 04:31:01 +0000

    Improve dump routine slightly.

--- sys/dev/usb/usbdi.c
+++ sys/dev/usb/usbdi.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: usbdi.c,v 1.87 2001/11/21 15:37:41 augustss Exp $	*/
+/*	$NetBSD: usbdi.c,v 1.88 2001/11/22 04:31:01 augustss Exp $	*/
 /*	$FreeBSD: src/sys/dev/usb/usbdi.c,v 1.28 1999/11/17 22:33:49 n_hibma Exp $	*/
 
 /*
@@ -39,7 +39,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: usbdi.c,v 1.87 2001/11/21 15:37:41 augustss Exp $");
+__KERNEL_RCSID(0, "$NetBSD: usbdi.c,v 1.88 2001/11/22 04:31:01 augustss Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -171,7 +171,7 @@ usbd_dump_pipe(usbd_pipe_handle pipe)
 	usbd_dump_endpoint(pipe->endpoint);
 	printf(" (usbd_dump_pipe:)\n refcnt=%d running=%d aborting=%d\n",
 	       pipe->refcnt, pipe->running, pipe->aborting);
-	printf("intrxfer=%p, repeat=%d, interval=%d\n",
+	printf(" intrxfer=%p, repeat=%d, interval=%d\n",
 	       pipe->intrxfer, pipe->repeat, pipe->interval);
 }
 #endif
