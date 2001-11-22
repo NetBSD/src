d909ba9e7afd0b1adb8aaec5448f74824ea598f1
lukem <lukem@NetBSD.org>
2001-11-22 02:47:25 +0000

    reference the ufs/[uf]fs headers in ../../sys rather than using those in /usr/include. this should make it easier to make this a host tool

--- usr.sbin/makefs/ffs/mkfs.c
+++ usr.sbin/makefs/ffs/mkfs.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: mkfs.c,v 1.1.1.1 2001/10/26 06:21:57 lukem Exp $	*/
+/*	$NetBSD: mkfs.c,v 1.2 2001/11/22 02:47:26 lukem Exp $	*/
 /* From NetBSD: mkfs.c,v 1.55 2001/09/06 02:16:01 lukem Exp $ */
 
 /*
@@ -39,7 +39,7 @@
 #if 0
 static char sccsid[] = "@(#)mkfs.c	8.11 (Berkeley) 5/3/95";
 #else
-__RCSID("$NetBSD: mkfs.c,v 1.1.1.1 2001/10/26 06:21:57 lukem Exp $");
+__RCSID("$NetBSD: mkfs.c,v 1.2 2001/11/22 02:47:26 lukem Exp $");
 #endif
 #endif /* not lint */
 
@@ -53,10 +53,10 @@ __RCSID("$NetBSD: mkfs.c,v 1.1.1.1 2001/10/26 06:21:57 lukem Exp $");
 #include <string.h>
 #include <unistd.h>
 
-#include <ufs/ufs/dir.h>
-#include <ufs/ufs/inode.h>
-#include <ufs/ufs/ufs_bswap.h>
-#include <ufs/ffs/fs.h>
+#include "ufs/ufs/dir.h"
+#include "ufs/ufs/inode.h"
+#include "ufs/ufs/ufs_bswap.h"
+#include "ufs/ffs/fs.h"
 
 #include "makefs.h"
 
