0e953c78d6fd5f7ec4a7b23378b3353f15383e07
jdolecek <jdolecek>
2001-11-23 18:56:33 +0000

    Two changes to setrlimit(2): * return EINVAL if specified current limit exceeds specified hard limit.   This behaviour is required by SUSv2 (noted by Giles Lean on tech-kern) * return EINVAL if an attempt is made to lower stack size limit below   current usage; this addresses bin/3045 by Jason Thorpe, and conforms to SUSv2

--- sys/kern/kern_resource.c
+++ sys/kern/kern_resource.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: kern_resource.c,v 1.61 2001/11/12 15:25:14 lukem Exp $	*/
+/*	$NetBSD: kern_resource.c,v 1.62 2001/11/23 18:56:33 jdolecek Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1991, 1993
@@ -41,7 +41,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: kern_resource.c,v 1.61 2001/11/12 15:25:14 lukem Exp $");
+__KERNEL_RCSID(0, "$NetBSD: kern_resource.c,v 1.62 2001/11/23 18:56:33 jdolecek Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -263,12 +263,17 @@ dosetrlimit(p, cred, which, limp)
 	    limp->rlim_max == alimp->rlim_max)
 		return 0;
 
-	if (limp->rlim_cur > alimp->rlim_max || 
-	    limp->rlim_max > alimp->rlim_max)
-		if ((error = suser(cred->pc_ucred, &p->p_acflag)) != 0)
+	if (limp->rlim_cur > limp->rlim_max) {
+		/*
+		 * This is programming error. According to SUSv2, we should
+		 * return error in this case.
+		 */
+		return (EINVAL);
+	}
+	if (limp->rlim_max > alimp->rlim_max
+	    && (error = suser(cred->pc_ucred, &p->p_acflag)) != 0)
 			return (error);
-	if (limp->rlim_cur > limp->rlim_max)
-		limp->rlim_cur = limp->rlim_max;
+
 	if (p->p_limit->p_refcnt > 1 &&
 	    (p->p_limit->p_lflags & PL_SHAREMOD) == 0) {
 		newplim = limcopy(p->p_limit);
@@ -291,6 +296,16 @@ dosetrlimit(p, cred, which, limp)
 			limp->rlim_cur = maxsmap;
 		if (limp->rlim_max > maxsmap)
 			limp->rlim_max = maxsmap;
+
+		/*
+		 * Return EINVAL if the new stack size limit is lower than
+		 * current usage. Otherwise, the process would get SIGSEGV the
+		 * moment it would try to access anything on it's current stack.
+		 * This conforms to SUSv2.
+		 */
+		if (limp->rlim_cur < p->p_vmspace->vm_ssize * PAGE_SIZE
+		    || limp->rlim_max < p->p_vmspace->vm_ssize * PAGE_SIZE)
+			return (EINVAL);
 
 		/*
 		 * Stack is allocated to the max at exec time with
