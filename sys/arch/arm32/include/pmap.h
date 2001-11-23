20d1dedb6f4c79833ae427a0a0a63cfe127fd665
thorpej <thorpej>
2001-11-23 17:29:00 +0000

    Extra whitespace removal.

--- sys/arch/arm32/include/pmap.h
+++ sys/arch/arm32/include/pmap.h
@@ -1,4 +1,3 @@
-/* $NetBSD: pmap.h,v 1.19 2001/02/23 21:23:51 reinoud Exp $ */
+/* $NetBSD: pmap.h,v 1.20 2001/11/23 17:29:01 thorpej Exp $ */
 
 #include <arm/arm32/pmap.h>
-
--- sys/arch/hpcarm/include/pmap.h
+++ sys/arch/hpcarm/include/pmap.h
@@ -1,4 +1,3 @@
-/* $NetBSD: pmap.h,v 1.2 2001/02/23 21:23:52 reinoud Exp $ */
+/* $NetBSD: pmap.h,v 1.3 2001/11/23 17:29:02 thorpej Exp $ */
 
 #include <arm/arm32/pmap.h>
-
