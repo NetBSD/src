5ee539e40af82e1e7f2bc325cc53cd21c708f3bc
thorpej <thorpej@NetBSD.org>
2001-11-23 17:39:03 +0000

    - Move more contents of various <machine/vmparam.h> files into   <arm/arm32/vmparam.h> (mostly the stuff that's tied to the pmap   implementation). - Since the MMU definitions in pte.h are specific to ARM processors   that support 32-bit mode, move pte.h to <arm/arm32/pte.h>. - Make the Netwinder startup file build again (use PT_B|PT_C, rather   than PT_CACHEABLE, since the latter expands to a variable these days).

--- sys/arch/netwinder/include/vmparam.h
+++ sys/arch/netwinder/include/vmparam.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: vmparam.h,v 1.6 2001/11/15 18:06:15 soren Exp $	*/
+/*	$NetBSD: vmparam.h,v 1.7 2001/11/23 17:39:05 thorpej Exp $	*/
 
 /*
  * Copyright (c) 1988 The Regents of the University of California.
@@ -36,8 +36,7 @@
 #ifndef	_ARM32_VMPARAM_H_
 #define	_ARM32_VMPARAM_H_
 
-/* for pt_entry_t definition */
-#include <machine/pte.h>
+#ifdef _KERNEL
 
 #include <arm/arm32/vmparam.h>
 
@@ -171,18 +170,6 @@
 #define	VM_FREELIST_DEFAULT	0
 #define	VM_FREELIST_ISADMA	1
 
-/*
- * define structure pmap_physseg: there is one of these structures
- * for each chunk of noncontig RAM you have.
- */
-
-#define	__HAVE_PMAP_PHYSSEG
-
-struct pmap_physseg {
-	struct pv_head *pvhead;		/* pv_entry array */
-	char *attrs;			/* attrs array */
-};
+#endif /* _KERNEL */
 
 #endif	/* _ARM32_VMPARAM_H_ */
-
-/* End of vmparam.h */
