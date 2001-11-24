c9e0b10c682f2b96b71837449e6e232d9011450a
he <he@NetBSD.org>
2001-11-24 21:45:20 +0000

    Pull up revisions 1.2-1.3 (requested by lukem):   Jumbo pullup for newfs:    o convert to ANSI function prototypes    o move external variables to extern.h

--- sbin/newfs/extern.h
+++ sbin/newfs/extern.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: extern.h,v 1.1 1997/06/30 22:20:32 christos Exp $	*/
+/*	$NetBSD: extern.h,v 1.1.12.1 2001/11/24 21:45:20 he Exp $	*/
 
 /*
  * Copyright (c) 1997 Christos Zoulas.  All rights reserved.
@@ -29,4 +29,37 @@
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-void mkfs __P((struct partition *, char *, int, int));
+/* prototypes */
+void mkfs(struct partition *, char *, int, int);
+
+/* * variables set up by front end. */
+extern int	mfs;		/* run as the memory based filesystem */
+extern int	Nflag;		/* run mkfs without writing file system */
+extern int	Oflag;		/* format as an 4.3BSD file system */
+extern int	fssize;		/* file system size */
+extern int	ntracks;	/* # tracks/cylinder */
+extern int	nsectors;	/* # sectors/track */
+extern int	nphyssectors;	/* # sectors/track including spares */
+extern int	secpercyl;	/* sectors per cylinder */
+extern int	sectorsize;	/* bytes/sector */
+extern int	rpm;		/* revolutions/minute of drive */
+extern int	interleave;	/* hardware sector interleave */
+extern int	trackskew;	/* sector 0 skew, per track */
+extern int	headswitch;	/* head switch time, usec */
+extern int	trackseek;	/* track-to-track seek, usec */
+extern int	fsize;		/* fragment size */
+extern int	bsize;		/* block size */
+extern int	cpg;		/* cylinders/cylinder group */
+extern int	cpgflg;		/* cylinders/cylinder group flag was given */
+extern int	minfree;	/* free space threshold */
+extern int	opt;		/* optimization preference (space or time) */
+extern int	density;	/* number of bytes per inode */
+extern int	maxcontig;	/* max contiguous blocks to allocate */
+extern int	rotdelay;	/* rotational delay between blocks */
+extern int	maxbpg;		/* maximum blocks per file in a cyl group */
+extern int	nrpos;		/* # of distinguished rotational positions */
+extern int	bbsize;		/* boot block size */
+extern int	sbsize;		/* superblock size */
+extern u_long	memleft;	/* virtual memory available */
+extern caddr_t	membase;	/* start address of memory based filesystem */
+extern int	needswap;	/* Filesystem not in native byte order */
