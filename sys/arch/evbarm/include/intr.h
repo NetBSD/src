c4ac127d20e83789b1e056f26725e6f078108499
thorpej <thorpej@NetBSD.org>
2001-11-26 20:26:20 +0000

    Remove the NEWINTR stuff; it's not used, incomplete, and I'm trying to detangle the ARM interrupt code.

--- sys/arch/evbarm/include/intr.h
+++ sys/arch/evbarm/include/intr.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: intr.h,v 1.4 2001/11/25 15:56:04 thorpej Exp $	*/
+/*	$NetBSD: intr.h,v 1.5 2001/11/26 20:26:20 thorpej Exp $	*/
 
 /*
  * Copyright (c) 1997 Mark Brinicombe.
@@ -36,112 +36,6 @@
 #ifndef _EVBARM_INTR_H_
 #define _EVBARM_INTR_H_
 
-#ifdef NEWINTR
-/* Define the various Interrupt Priority Levels */
-
-/* Interrupt Priority Levels are mutually exclusive. */
-
-#define	IPL_NONE	0	/* no interrupts blocked */
-#define	IPL_SOFT	1	/* generic soft interrupts */
-#define	IPL_SOFTCLOCK	2	/* clock soft interrupts */
-#define	IPL_SOFTNET	3	/* network soft interrupts */
-#define	IPL_SOFTSERIAL	4	/* serial soft interrupts */
-#define IPL_BIO		5	/* block I/O */
-#define IPL_NET		6	/* network */
-#define IPL_TTY		7	/* terminal */
-#define IPL_IMP		8	/* memory allocation */
-#define IPL_AUDIO	9	/* audio */
-#define IPL_CLOCK	10	/* clock */
-#define IPL_SERIAL	11	/* serial */
-#define IPL_PERF	12	/* peformance monitoring unit */
-#define IPL_HIGH	13	/* blocks all interrupts */
-
-#define IPL_LEVELS	14
-
-#define	IST_UNUSABLE	-1	/* interrupt cannot be used */
-#define	IST_NONE	0	/* none (dummy) */
-#define	IST_PULSE	1	/* pulsed */
-#define	IST_EDGE	2	/* edge-triggered */
-#define	IST_LEVEL	3	/* level-triggered */
-
-#if defined (_KERNEL) && !defined(_LOCORE)
-#include <sys/queue.h>
-#include <sys/device.h>
-
-extern int _splraise(int);
-extern int _spllower(int);
-extern int _splget(int);
-extern int _splset(int);
-extern int _splnone(void);
-extern void _softintrset(int);
-extern int _softintrclr(int);
-
-#define	splsoftclock()		_splraise(IPL_SOFTCLOCK)
-#define	splsoftnet()		_splraise(IPL_SOFTNET)
-#define	splsoftserial()		_splraise(IPL_SOFTSERIAL)
-#define	splbio()		_splraise(IPL_BIO)
-#define	splnet()		_splraise(IPL_NET)
-#define	spltty()		_splraise(IPL_TTY)
-#define	splvm()			_splraise(IPL_IMP)
-#define	splaudio()		_splraise(IPL_AUDIO)
-#define	splclock()		_splraise(IPL_CLOCK)
-#define	splserial()		_splraise(IPL_SERIAL)
-#define	splhigh()		_splraise(IPL_HIGH)
-#define	spl0()			(void) _splnone()
-#define	splx(s)			(void) _splset(s)
-
-#define	spllock()		splhigh()
-#define	splsched()		splclock()
-#define	splstatclock()		splclock()
-
-#define	spllowersoftclock()	_spllower(IPL_SOFTCLOCK)
-
-#define	setsoftclock()		_softintrset(IPL_SOFTCLOCK)
-#define	setsoftnet()		_softintrset(IPL_SOFTNET)
-#define	setsoftserial()		_softintrset(IPL_SOFTSERIAL)
-
-#define	_SPL_0			IPL_NONE
-
-struct intrsource {
-	void *is_cookie;
-	LIST_ENTRY(evbarm_intrsource) is_link;
-	void *(*is_establish)(void *, int, int, int (*)(void *), void *);
-	void (*is_disestablish)(void *, void *);
-
-	void (*is_setmask)(int);
-};
-
-#define	intr_establish(src, irq, type, func, arg) \
-	(((src)->is_establish)((src)->is_cookie, irq, type, func, arg))
-#define	intr_disestablish(src, ih) \
-	(((src)->is_disestablish)((src)->is_cookie, ih))
-
-struct irqhandler {
-	LIST_ENTRY(intrhandler) ih_ipllink;
-	LIST_ENTRY(intrhandler) ih_srclink;
-	int (*ih_func)(void *);
-	void *ih_arg;
-	int ih_flags;
-	int ih_ipl;
-	struct evcnt ih_ev;
-};
-
-struct fiqhandler {
-	void (*fh_func)(void);	/* handler function */
-	size_t fh_size;		/* Size of handler function */
-	register_t fh_r8;	/* FIQ mode r8 */
-	register_t fh_r9;	/* FIQ mode r9 */
-	register_t fh_r10;	/* FIQ mode r10 */
-	register_t fh_r11;	/* FIQ mode r11 */
-	register_t fh_r12;	/* FIQ mode r12 */
-	register_t fh_r13;	/* FIQ mode r13 */
-};
-
-#endif	/* _KERNEL */
-
-#else	/* NEWINTR */
-/* This should go away when we port the Integrator code to use NEWINTR */
-
 /* Define the various Interrupt Priority Levels */
 
 /* Hardware Interrupt Priority Levels are not mutually exclusive. */
@@ -175,7 +69,5 @@ struct fiqhandler {
 
 #include <machine/irqhandler.h>
 #include <machine/psl.h>
-
-#endif	/* NEWINTR */
 
 #endif	/* _EVBARM_INTR_H */
