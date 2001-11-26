0372a1cb46f151ee9124500ab627299bfd58c757
thorpej <thorpej>
2001-11-26 01:03:58 +0000

    Remove dummy includes.

--- sys/arch/arm/include/vmparam.h
+++
@@ -1,8 +0,0 @@
-/* $NetBSD: vmparam.h,v 1.1 2001/07/29 09:50:07 chris Exp $ */
-
-/*
- * Dummy vmparam.h file to keep userland happy
- */
-#ifdef _KERNEL
-#error "userland vmparam.h included by the kernel!"
-#endif
