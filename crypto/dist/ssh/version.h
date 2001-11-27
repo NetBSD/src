2dd3b02a9ad6ed55dd9508da0bdacd8d9677a463
itojun <itojun>
2001-11-27 04:16:08 +0000

    update version date to 20011127

--- crypto/dist/ssh/version.h
+++ crypto/dist/ssh/version.h
@@ -1,8 +1,8 @@
-/*	$NetBSD: version.h,v 1.18 2001/11/19 07:39:57 itojun Exp $	*/
+/*	$NetBSD: version.h,v 1.19 2001/11/27 04:16:08 itojun Exp $	*/
 /* $OpenBSD: version.h,v 1.26 2001/11/13 09:03:57 markus Exp $ */
 
 #define __OPENSSH_VERSION	"OpenSSH_3.0.1"
-#define __NETBSDSSH_VERSION	"NetBSD_Secure_Shell-20011107"
+#define __NETBSDSSH_VERSION	"NetBSD_Secure_Shell-20011127"
 
 /*
  * it is important to retain OpenSSH version identification part, it is
