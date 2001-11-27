b5310620b0aee6109bd9ecf5c704415b81b8c308
jmc <jmc@NetBSD.org>
2001-11-27 05:17:31 +0000

    Back out last. Typo'd some things I need to test again

--- share/mk/bsd.kernobj.mk
+++ share/mk/bsd.kernobj.mk
@@ -1,4 +1,4 @@
-#	$NetBSD: bsd.kernobj.mk,v 1.5 2001/11/27 05:11:41 jmc Exp $
+#	$NetBSD: bsd.kernobj.mk,v 1.6 2001/11/27 05:17:31 jmc Exp $
 
 # KERNSRCDIR	Is the location of the top of the kernel src.
 # 		It defaults to ${BSDSRCDIR}/sys, but the top-level
@@ -15,34 +15,19 @@
 # KERNOBJDIR	Is the kernel build directory.  The kernel GENERIC for
 # 		instance will be compiled in ${KERNOBJDIR}/GENERIC.
 # 		The default value is
-# 		${KERNSRCDIR}/${KERNARCHDIR}/compile
-#
-#		If MAKEOBJDIRPREFIX or _SRC_TOP_OBJ is set than the value will
-#		be either 
-#
-#		${MAKEOBJDIRPREFIX}${KERNSRCDIR}/${KERNARCHDIR}/compile
-#
-#		or
-#
-#		${_SRC_TOP_OBJ_}/sys/${KERNARCHDIR}/compile
-#
-#		with MAKEOBJDIRPREFIX taking priority over _SRC_TOP_OBJ_ 
+# 		${MAKEOBJDIRPREFIX}${KERNSRCDIR}/${KERNARCHDIR}/compile
+# 		if it exists or the target 'obj' is being made.
+# 		Otherwise the default is
+# 		${KERNSRCDIR}/${KERNARCHDIR}/compile.
 # 
-
-.include <bsd.own.mk>
 
 KERNSRCDIR?=	${BSDSRCDIR}/sys
 # just incase ${MACHINE} is not always correct
 KERNARCHDIR?=	arch/${MACHINE}
 
-.if defined(MAKEOBJDIRPREFIX)
-KERNOBJDIR?=    ${MAKEOBJDIRPREFIX}${KERNSRCDIR}/${KERNARCHDIR}/compile
-.else
-.if defined(_SRC_TOP_OBJ_) && ${_SRC_TOP_OBJ_} != ""
-KERNOBJDIR?=	${_SRC_TOP_OBJ_}/sys/${KERNARCHDIR}/compile
+.if make(obj) || exists(${MAKEOBJDIRPREFIX}${KERNSRCDIR}/${KERNARCHDIR}/compile)
+KERNOBJDIR?=	${MAKEOBJDIRPREFIX}${KERNSRCDIR}/${KERNARCHDIR}/compile
 .else
 KERNOBJDIR?=	${KERNSRCDIR}/${KERNARCHDIR}/compile
 .endif
-.endif
-
 KERNCONFDIR?=	${KERNSRCDIR}/${KERNARCHDIR}/conf
