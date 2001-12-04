ef333bea3d8527be82ac19d161b2ed69673e9924
blymn <blymn@NetBSD.org>
2001-12-04 13:12:23 +0000

    Replace deprecated curses call with supported equivalent.

--- games/cribbage/crib.c
+++ games/cribbage/crib.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: crib.c,v 1.14 2001/02/05 00:28:30 christos Exp $	*/
+/*	$NetBSD: crib.c,v 1.15 2001/12/04 13:12:23 blymn Exp $	*/
 
 /*-
  * Copyright (c) 1980, 1993
@@ -43,7 +43,7 @@ __COPYRIGHT("@(#) Copyright (c) 1980, 1993\n\
 #if 0
 static char sccsid[] = "@(#)crib.c	8.1 (Berkeley) 5/31/93";
 #else
-__RCSID("$NetBSD: crib.c,v 1.14 2001/02/05 00:28:30 christos Exp $");
+__RCSID("$NetBSD: crib.c,v 1.15 2001/12/04 13:12:23 blymn Exp $");
 #endif
 #endif /* not lint */
 
@@ -112,7 +112,7 @@ main(argc, argv)
 
 	initscr();
 	(void)signal(SIGINT, rint);
-	crmode();
+	cbreak();
 	noecho();
 
 	Playwin = subwin(stdscr, PLAY_Y, PLAY_X, 0, 0);
