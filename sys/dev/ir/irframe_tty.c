bfa89af2bc58207ad07133e26b27f80a51268395
augustss <augustss@NetBSD.org>
2001-12-05 14:50:13 +0000

    Break out SIR framing to its own file.

--- sys/dev/ir/irframe_tty.c
+++ sys/dev/ir/irframe_tty.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: irframe_tty.c,v 1.8 2001/12/05 04:31:02 augustss Exp $	*/
+/*	$NetBSD: irframe_tty.c,v 1.9 2001/12/05 14:50:14 augustss Exp $	*/
 
 /*
  * TODO
@@ -46,7 +46,7 @@
 
 /*
  * Loosely based on ppp_tty.c.
- * Framing and dongle handing written by Tommy Bohlin.
+ * Framing and dongle handling written by Tommy Bohlin.
  */
 
 #include <sys/param.h>
@@ -63,6 +63,7 @@
 #include <sys/poll.h>
 
 #include <dev/ir/ir.h>
+#include <dev/ir/sir.h>
 #include <dev/ir/irdaio.h>
 #include <dev/ir/irframevar.h>
 
@@ -80,13 +81,6 @@ int irframetdebug = 0;
 #define Static static
 #endif
 
-/* Protocol constants */
-#define SIR_EXTRA_BOF            0xff
-#define SIR_BOF                  0xc0
-#define SIR_CE                   0x7d
-#define SIR_EOF                  0xc1
-
-#define SIR_ESC_BIT              0x20
 /*****/
 
 #define MAX_IRDA_FRAME 5000	/* XXX what is it? */
@@ -152,9 +146,8 @@ Static int	irframet_get_speeds(void *h, int *speeds);
 Static int	irframet_get_turnarounds(void *h, int *times);
 
 /* internal */
-Static int	irt_write_buf(void *h, void *buf, size_t len);
+Static int	irt_write_frame(void *h, u_int8_t *buf, size_t len);
 Static int	irt_putc(int c, struct tty *tp);
-Static int	irt_putesc(int c, struct tty *tp);
 Static void	irt_frame(struct irframet_softc *sc, u_char *buf, u_int len);
 Static void	irt_timeout(void *v);
 
@@ -168,52 +161,6 @@ void
 irframettyattach(int n)
 {
 }
-
-/*
- * CRC computation
- */
-static const u_int16_t fcstab[] = {
-	0x0000, 0x1189, 0x2312, 0x329b, 0x4624, 0x57ad, 0x6536, 0x74bf,
-	0x8c48, 0x9dc1, 0xaf5a, 0xbed3, 0xca6c, 0xdbe5, 0xe97e, 0xf8f7,
-	0x1081, 0x0108, 0x3393, 0x221a, 0x56a5, 0x472c, 0x75b7, 0x643e,
-	0x9cc9, 0x8d40, 0xbfdb, 0xae52, 0xdaed, 0xcb64, 0xf9ff, 0xe876,
-	0x2102, 0x308b, 0x0210, 0x1399, 0x6726, 0x76af, 0x4434, 0x55bd,
-	0xad4a, 0xbcc3, 0x8e58, 0x9fd1, 0xeb6e, 0xfae7, 0xc87c, 0xd9f5,
-	0x3183, 0x200a, 0x1291, 0x0318, 0x77a7, 0x662e, 0x54b5, 0x453c,
-	0xbdcb, 0xac42, 0x9ed9, 0x8f50, 0xfbef, 0xea66, 0xd8fd, 0xc974,
-	0x4204, 0x538d, 0x6116, 0x709f, 0x0420, 0x15a9, 0x2732, 0x36bb,
-	0xce4c, 0xdfc5, 0xed5e, 0xfcd7, 0x8868, 0x99e1, 0xab7a, 0xbaf3,
-	0x5285, 0x430c, 0x7197, 0x601e, 0x14a1, 0x0528, 0x37b3, 0x263a,
-	0xdecd, 0xcf44, 0xfddf, 0xec56, 0x98e9, 0x8960, 0xbbfb, 0xaa72,
-	0x6306, 0x728f, 0x4014, 0x519d, 0x2522, 0x34ab, 0x0630, 0x17b9,
-	0xef4e, 0xfec7, 0xcc5c, 0xddd5, 0xa96a, 0xb8e3, 0x8a78, 0x9bf1,
-	0x7387, 0x620e, 0x5095, 0x411c, 0x35a3, 0x242a, 0x16b1, 0x0738,
-	0xffcf, 0xee46, 0xdcdd, 0xcd54, 0xb9eb, 0xa862, 0x9af9, 0x8b70,
-	0x8408, 0x9581, 0xa71a, 0xb693, 0xc22c, 0xd3a5, 0xe13e, 0xf0b7,
-	0x0840, 0x19c9, 0x2b52, 0x3adb, 0x4e64, 0x5fed, 0x6d76, 0x7cff,
-	0x9489, 0x8500, 0xb79b, 0xa612, 0xd2ad, 0xc324, 0xf1bf, 0xe036,
-	0x18c1, 0x0948, 0x3bd3, 0x2a5a, 0x5ee5, 0x4f6c, 0x7df7, 0x6c7e,
-	0xa50a, 0xb483, 0x8618, 0x9791, 0xe32e, 0xf2a7, 0xc03c, 0xd1b5,
-	0x2942, 0x38cb, 0x0a50, 0x1bd9, 0x6f66, 0x7eef, 0x4c74, 0x5dfd,
-	0xb58b, 0xa402, 0x9699, 0x8710, 0xf3af, 0xe226, 0xd0bd, 0xc134,
-	0x39c3, 0x284a, 0x1ad1, 0x0b58, 0x7fe7, 0x6e6e, 0x5cf5, 0x4d7c,
-	0xc60c, 0xd785, 0xe51e, 0xf497, 0x8028, 0x91a1, 0xa33a, 0xb2b3,
-	0x4a44, 0x5bcd, 0x6956, 0x78df, 0x0c60, 0x1de9, 0x2f72, 0x3efb,
-	0xd68d, 0xc704, 0xf59f, 0xe416, 0x90a9, 0x8120, 0xb3bb, 0xa232,
-	0x5ac5, 0x4b4c, 0x79d7, 0x685e, 0x1ce1, 0x0d68, 0x3ff3, 0x2e7a,
-	0xe70e, 0xf687, 0xc41c, 0xd595, 0xa12a, 0xb0a3, 0x8238, 0x93b1,
-	0x6b46, 0x7acf, 0x4854, 0x59dd, 0x2d62, 0x3ceb, 0x0e70, 0x1ff9,
-	0xf78f, 0xe606, 0xd49d, 0xc514, 0xb1ab, 0xa022, 0x92b9, 0x8330,
-	0x7bc7, 0x6a4e, 0x58d5, 0x495c, 0x3de3, 0x2c6a, 0x1ef1, 0x0f78
-};
-
-#define INITFCS 0xffff
-#define GOODFCS 0xf0b8
-
-static __inline u_int16_t updateFCS(u_int16_t fcs, int c) {
-	return (fcs >> 8) ^ fcstab[(fcs^c) & 0xff];
-} 
-
 
 /*
  * Line specific open routine for async tty devices.
@@ -323,11 +270,7 @@ irframetioctl(struct tty *tp, u_long cmd, caddr_t data, int flag,
 		sc->sc_dongle = *(int *)data;
 		break;
 	default:
-#if 0
-		error = irframeioctl(sc, cmd, data, flag, p);
-#else
 		error = EINVAL;
-#endif
 		break;
 	}
 
@@ -335,9 +278,7 @@ irframetioctl(struct tty *tp, u_long cmd, caddr_t data, int flag,
 }
 
 /*
- * Start output on async tty interface.  If the transmit queue
- * has drained sufficiently, arrange for irframeasyncstart to be
- * called later at splsoftnet.
+ * Start output on async tty interface.
  * Called at spltty or higher.
  */
 int
@@ -347,10 +288,6 @@ irframetstart(struct tty *tp)
 
 	DPRINTF(("%s: tp=%p\n", __FUNCTION__, tp));
 
-	/*
-	 * If there is stuff in the output queue, send it now.
-	 * We are being called in lieu of ttstart and must do what it would.
-	 */
 	if (tp->t_oproc != NULL)
 		(*tp->t_oproc)(tp);
 
@@ -579,6 +516,11 @@ irt_putc(int c, struct tty *tp)
 	int s;
 	int error;
 
+#if IRFRAMET_DEBUG
+	if (irframetdebug > 3)
+		DPRINTF(("%s: tp=%p c=0x%02x cc=%d\n", __FUNCTION__, tp, c,
+			 tp->t_outq.c_cc));
+#endif
 	if (tp->t_outq.c_cc > tp->t_hiwat) {
 		irframetstart(tp);
 		s = spltty();
@@ -605,75 +547,38 @@ irt_putc(int c, struct tty *tp)
 }
 
 int
-irt_putesc(int c, struct tty *tp)
-{
-	int error;
-
-	if (c == SIR_BOF || c == SIR_EOF || c == SIR_CE) {
-		error = irt_putc(SIR_CE, tp);
-		if (!error)
-			error = irt_putc(SIR_ESC_BIT^c, tp);
-	} else {
-		error = irt_putc(c, tp);
-	}
-	return (error);
-}
-
-int
 irframet_write(void *h, struct uio *uio, int flag)
 {
+	struct tty *tp = h;
+	struct irframet_softc *sc = (struct irframet_softc *)tp->t_sc;
 	u_int8_t buf[MAX_IRDA_FRAME];
-	size_t n;
-	int error;
+	int n;
 
 	DPRINTF(("%s: resid=%d, iovcnt=%d, offset=%ld\n", 
 		 __FUNCTION__, uio->uio_resid, uio->uio_iovcnt, 
 		 (long)uio->uio_offset));
 
-	n = uio->uio_resid;
-	if (n > MAX_IRDA_FRAME)
-		return (EINVAL);
-	error = uiomove(buf, n, uio);
-	if (error)
-		return (error);
-	return (irt_write_buf(h, buf, n));
+	n = irda_sir_frame(buf, MAX_IRDA_FRAME, uio, sc->sc_ebofs);
+	if (n < 0)
+		return (-n);
+	return (irt_write_frame(h, buf, n));
 }
 
 int
-irt_write_buf(void *h, void *buf, size_t len)
+irt_write_frame(void *h, u_int8_t *buf, size_t len)
 {
 	struct tty *tp = h;
-	struct irframet_softc *sc = (struct irframet_softc *)tp->t_sc;
-	u_int8_t *cp = buf;
-	int c, error, ofcs, i;
+	int error, i;
 
 	DPRINTF(("%s: tp=%p len=%d\n", __FUNCTION__, tp, len));
 
-	ofcs = INITFCS;
 	error = 0;
-
-	for (i = 0; i < sc->sc_ebofs; i++)
-		irt_putc(SIR_EXTRA_BOF, tp);
-	irt_putc(SIR_BOF, tp);
-
-	while (len-- > 0) {
-		c = *cp++;
-		ofcs = updateFCS(ofcs, c);
-		error = irt_putesc(c, tp);
-		if (error)
-			return (error);
-	}
-
-	ofcs = ~ofcs;
-	error = irt_putesc(ofcs & 0xff, tp);
-	if (!error)
-		error = irt_putesc((ofcs >> 8) & 0xff, tp);
-	if (!error)
-		error = irt_putc(SIR_EOF, tp);
+	for (i = 0; !error && i < len; i++)
+		error = irt_putc(buf[i], tp);
 
 	irframetstart(tp);
 
-	DPRINTF(("%s: done\n", __FUNCTION__));
+	DPRINTF(("%s: done, error=%d\n", __FUNCTION__, error));
 
 	return (error);
 }
@@ -689,9 +594,11 @@ irframet_poll(void *h, int events, struct proc *p)
 	DPRINTF(("%s: sc=%p\n", __FUNCTION__, sc));
 
 	s = splir();
-	/* XXX should check with tty */
+	/* XXX is this a good check? */
 	if (events & (POLLOUT | POLLWRNORM))
-		revents |= events & (POLLOUT | POLLWRNORM);
+		if (tp->t_outq.c_cc <= tp->t_lowat)
+			revents |= events & (POLLOUT | POLLWRNORM);
+
 	if (events & (POLLIN | POLLRDNORM)) {
 		if (sc->sc_nframes > 0) {
 			DPRINTF(("%s: have data\n", __FUNCTION__));
