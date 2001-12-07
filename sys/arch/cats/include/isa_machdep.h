51b04de50399b10b8ff7d81a854000ef061df1bf
chris <chris>
2001-12-07 23:09:32 +0000

    Reintroduce these header files so that cats kernels can build.  Fallout from Jason reverting the includes stuff.

---
+++ sys/arch/cats/include/isa_machdep.h
@@ -0,0 +1,3 @@
+/* $NetBSD: isa_machdep.h,v 1.3 2001/12/07 23:09:32 chris Exp $ */
+
+#include <arm/isa_machdep.h>
---
+++ sys/arch/cats/include/isapnp_machdep.h
@@ -0,0 +1,4 @@
+/* $NetBSD: isapnp_machdep.h,v 1.3 2001/12/07 23:09:32 chris Exp $ */
+
+#include <arm/isapnp_machdep.h>
+
