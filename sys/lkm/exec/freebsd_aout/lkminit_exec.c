165f442f860cee867de49380ae4742367e8edf39
thorpej <thorpej@NetBSD.org>
2001-12-08 00:37:07 +0000

    Update for "coredumps now exec-format/emulation specific".

--- sys/lkm/exec/freebsd_aout/lkminit_exec.c
+++ sys/lkm/exec/freebsd_aout/lkminit_exec.c
@@ -1,4 +1,4 @@
-/* $NetBSD: lkminit_exec.c,v 1.3 2001/11/12 23:23:09 lukem Exp $ */
+/* $NetBSD: lkminit_exec.c,v 1.4 2001/12/08 00:37:09 thorpej Exp $ */
 
 /*-
  * Copyright (c) 1996 The NetBSD Foundation, Inc.
@@ -37,7 +37,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: lkminit_exec.c,v 1.3 2001/11/12 23:23:09 lukem Exp $");
+__KERNEL_RCSID(0, "$NetBSD: lkminit_exec.c,v 1.4 2001/12/08 00:37:09 thorpej Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -47,15 +47,23 @@ __KERNEL_RCSID(0, "$NetBSD: lkminit_exec.c,v 1.3 2001/11/12 23:23:09 lukem Exp $
 #include <sys/exec_elf.h>
 #include <sys/proc.h>
 #include <sys/lkm.h>
+#include <sys/signalvar.h>
 
 #include <compat/freebsd/freebsd_exec.h>
 
 int exec_freebsd_aout_lkmentry __P((struct lkm_table *, int, int));
 
 static struct execsw exec_freebsd_aout =
-	{ FREEBSD_AOUT_HDR_SIZE, exec_freebsd_aout_makecmds, { NULL },
-	  NULL, EXECSW_PRIO_ANY,
-	  0, copyargs };	/* a.out */
+	/* FreeBSD a.out (native word size) */
+	{ FREEBSD_AOUT_HDR_SIZE,
+	  exec_freebsd_aout_makecmds,
+	  { NULL },
+	  NULL,
+	  EXECSW_PRIO_ANY,
+	  0,
+	  copyargs,
+	  NULL,
+	  coredump_netbsd };
 
 
 /*
