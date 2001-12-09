4693a38f3f2fe0ccf3562170a16a72a09581e126
he <he>
2001-12-09 17:22:50 +0000

    Pull up revisions 1.7-1.8 (requested by skrll):   Add init/fini section support in crtbegin and crtend, and introduce   support for DWARF2 exception handling.  Fixes PR#12865, PR#13488,   PR#13489, and PR#13491.  Also fix ld.elf_so to deal appropriately.

--- libexec/ld.elf_so/xprintf.c
+++ libexec/ld.elf_so/xprintf.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: xprintf.c,v 1.6 2000/04/02 23:04:32 soren Exp $	 */
+/*	$NetBSD: xprintf.c,v 1.6.4.1 2001/12/09 17:22:50 he Exp $	 */
 
 /*
  * Copyright 1996 Matt Thomas <matt@3am-software.com>
@@ -30,6 +30,7 @@
 #include <sys/cdefs.h>
 #include "rtldenv.h"
 #include <string.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <errno.h>
 #ifdef __STDC__
@@ -63,7 +64,7 @@ xvsnprintf(buf, buflen, fmt, ap)
 	char *const ep = buf + buflen - 4;
 	int size;
 
-	while (*fmt != NULL && bp < ep) {
+	while (*fmt != '\0' && bp < ep) {
 		switch (*fmt) {
 		case '\\':{
 			if (fmt[1] != '\0')
@@ -94,12 +95,12 @@ xvsnprintf(buf, buflen, fmt, ap)
 				unsigned long long uval;
 				char digits[sizeof(int) * 3], *dp = digits;
 #define	SARG() \
-(size & SZ_SHORT ? va_arg(ap, short) : \
+(size & SZ_SHORT ? (short) va_arg(ap, int) : \
 size & SZ_LONG ? va_arg(ap, long) : \
 size & SZ_QUAD ? va_arg(ap, long long) : \
 va_arg(ap, int))
 #define	UARG() \
-(size & SZ_SHORT ? va_arg(ap, unsigned short) : \
+(size & SZ_SHORT ? (unsigned short) va_arg(ap, unsigned int) : \
 size & SZ_LONG ? va_arg(ap, unsigned long) : \
 size & SZ_QUAD ? va_arg(ap, unsigned long long) : \
 va_arg(ap, unsigned int))
