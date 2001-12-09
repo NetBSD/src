55a7d977b269cc849578b17e881d33f16b50c848
he <he@NetBSD.org>
2001-12-09 17:11:17 +0000

    Pull up revisions 1.7-1.8 (requested by srkll):   Add init/fini section support in crtbegin and crtend, and introduce   support for DWARF2 exception handling.  Fixes PR#12865, PR#13488,   PR#13489, and PR#13491.  Also fix ld.elf_so to deal appropriately.

--- lib/csu/common_elf/crtend.c
+++ lib/csu/common_elf/crtend.c
@@ -1,6 +1,7 @@
-/*	$NetBSD: crtend.c,v 1.6 1998/05/06 20:58:34 cgd Exp $	*/
+/*	$NetBSD: crtend.c,v 1.6.10.1 2001/12/09 17:11:17 he Exp $	*/
 
 #include <sys/cdefs.h>
+#include "dot_init.h"
 
 static void (*__CTOR_LIST__[1]) __P((void))
     __attribute__((__unused__))
@@ -8,3 +9,13 @@ static void (*__CTOR_LIST__[1]) __P((void))
 static void (*__DTOR_LIST__[1]) __P((void))
     __attribute__((__unused__))
     __attribute__((section(".dtors"))) = { (void *)0 };		/* XXX */
+
+#ifdef DWARF2_EH
+static unsigned int __FRAME_END__[]
+    __attribute__((__unused__))
+    __attribute__((section(".eh_frame"))) = { 0 };
+#endif
+
+MD_INIT_SECTION_EPILOGUE;
+
+MD_FINI_SECTION_EPILOGUE;
