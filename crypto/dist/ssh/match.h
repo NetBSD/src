c26aff2d35a5eb4c8e5c91afdac00ce9f7cd94e5
he <he>
2001-12-10 23:53:30 +0000

    Pull up revisions 1.1.1.4-1.1.1.7 (requested by itojun):   Upgrade OpenSSH to version 3.0.2.

--- crypto/dist/ssh/match.h
+++ crypto/dist/ssh/match.h
@@ -1,11 +1,10 @@
-/*	$OpenBSD: match.h,v 1.6 2001/01/29 01:58:17 niklas Exp $	*/
+/*	$NetBSD: match.h,v 1.1.1.1.2.3 2001/12/10 23:53:30 he Exp $	*/
+/*	$OpenBSD: match.h,v 1.10 2001/12/05 16:54:51 markus Exp $	*/
 
 /*
  * Author: Tatu Ylonen <ylo@cs.hut.fi>
  * Copyright (c) 1995 Tatu Ylonen <ylo@cs.hut.fi>, Espoo, Finland
  *                    All rights reserved
- * This file contains various auxiliary functions related to multiple
- * precision integers.
  *
  * As far as I am concerned, the code I have written for this software
  * can be used freely for any purpose.  Any derived versions of this
@@ -16,18 +15,11 @@
 #ifndef MATCH_H
 #define MATCH_H
 
-/*
- * Returns true if the given string matches the pattern (which may contain ?
- * and * as wildcards), and zero if it does not match.
- */
-int     match_pattern(const char *s, const char *pattern);
-
-/*
- * Tries to match the host name (which must be in all lowercase) against the
- * comma-separated sequence of subpatterns (each possibly preceded by ! to
- * indicate negation).  Returns -1 if negation matches, 1 if there is
- * a positive match, 0 if there is no match at all.
- */
-int     match_hostname(const char *host, const char *pattern, u_int len);
+int	 match_pattern(const char *, const char *);
+int	 match_pattern_list(const char *, const char *, u_int, int);
+int	 match_hostname(const char *, const char *, u_int);
+int	 match_host_and_ip(const char *, const char *, const char *);
+int	 match_user(const char *, const char *, const char *, const char *);
+char	*match_list(const char *, const char *, u_int *);
 
 #endif
