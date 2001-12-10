265f7bd40037df1472aba11541895f576c2b5448
he <he>
2001-12-10 23:52:26 +0000

    Pull up revisions 1.1.1.4-1.1.1.5 (requested by itojun):   Upgrade OpenSSH to version 3.0.2.

--- crypto/dist/ssh/auth-options.h
+++ crypto/dist/ssh/auth-options.h
@@ -1,3 +1,4 @@
+/*	$NetBSD: auth-options.h,v 1.1.1.1.2.3 2001/12/10 23:52:26 he Exp $	*/
 /*
  * Author: Tatu Ylonen <ylo@cs.hut.fi>
  * Copyright (c) 1995 Tatu Ylonen <ylo@cs.hut.fi>, Espoo, Finland
@@ -11,7 +12,7 @@
  * called by a name other than "ssh" or "Secure Shell".
  */
 
-/* $OpenBSD: auth-options.h,v 1.8 2001/01/21 19:05:42 markus Exp $ */
+/* $OpenBSD: auth-options.h,v 1.10 2001/06/26 17:27:22 markus Exp $ */
 
 #ifndef AUTH_OPTIONS_H
 #define AUTH_OPTIONS_H
@@ -30,15 +31,7 @@ extern int no_pty_flag;
 extern char *forced_command;
 extern struct envstring *custom_environment;
 
-/*
- * return 1 if access is granted, 0 if not.
- * side effect: sets key option flags
- */
-int
-auth_parse_options(struct passwd *pw, char *options, char *file,
-    u_long linenum);
-
-/* reset options flags */
+int	auth_parse_options(struct passwd *, char *, char *, u_long);
 void	auth_clear_options(void);
 
 #endif
