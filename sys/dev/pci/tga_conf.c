2e8f61aa112c3a10e185a9a6b4fc644bd1bdc8be
elric <elric@NetBSD.org>
2001-12-12 07:50:05 +0000

    Added support for the PowerStorm 4d20, initial support that is. I plan on cleaning a few things up over the next few days, but this appears to be working for me.

--- sys/dev/pci/tga_conf.c
+++ sys/dev/pci/tga_conf.c
@@ -1,4 +1,4 @@
-/* $NetBSD: tga_conf.c,v 1.5 2001/11/13 07:48:49 lukem Exp $ */
+/* $NetBSD: tga_conf.c,v 1.6 2001/12/12 07:50:05 elric Exp $ */
 
 /*
  * Copyright (c) 1995, 1996 Carnegie-Mellon University.
@@ -28,9 +28,10 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: tga_conf.c,v 1.5 2001/11/13 07:48:49 lukem Exp $");
+__KERNEL_RCSID(0, "$NetBSD: tga_conf.c,v 1.6 2001/12/12 07:50:05 elric Exp $");
 
 #include <sys/param.h>
+#include <sys/systm.h>
 #include <sys/device.h>
 
 #include <dev/pci/pcivar.h>
@@ -39,6 +40,7 @@ __KERNEL_RCSID(0, "$NetBSD: tga_conf.c,v 1.5 2001/11/13 07:48:49 lukem Exp $");
 
 #include <dev/ic/bt485var.h>
 #include <dev/ic/bt463var.h>
+#include <dev/ic/ibm561var.h>
 
 #undef KB
 #define KB		* 1024
@@ -116,6 +118,17 @@ static const struct tga_conf tga_configs[TGA_TYPE_UNKNOWN] = {
 		1,	{ 16 MB,    0 },	{ 8 MB,    0 },
 		1,	{ 24 MB,    0 },	{ 8 MB,    0 },
 	},
+	/* TGA_TYPE_POWERSTORM_4D20 */
+	/* XXX: These numbers may be incorrect */
+	{
+		"PS4d20",
+		ibm561_funcs,
+		32,
+		32 MB,
+		16 KB,
+		1,	{ 16 MB,    0 },	{ 8 MB,    0 },
+		1,	{ 24 MB,    0 },	{ 8 MB,    0 },
+	}
 };
 
 #undef KB
@@ -127,14 +140,17 @@ tga_identify(dc)
 {
 	int type;
 	int gder;
+	int grev;
 	int deep, addrmask, wide;
+	int tga2;
 
 	gder = TGARREG(dc, TGA_REG_GDER);
+	grev = TGARREG(dc, TGA_REG_GREV);
 
 	deep = (gder & 0x1) != 0; /* XXX */
 	addrmask = (gder >> 2) & 0x7; /* XXX */
 	wide = (gder & 0x200) == 0; /* XXX */
-
+	tga2 = (grev & 0x20) != 0;
 
 	type = TGA_TYPE_UNKNOWN;
 
@@ -161,6 +177,10 @@ tga_identify(dc)
 		}
 	} else {
 		/* 32bpp frame buffer */
+		if (addrmask == 0x00 && tga2 && wide) {
+			/* My PowerStorm 4d20 shows up this way? */
+			type = TGA_TYPE_POWERSTORM_4D20;
+		}
 
 		if (addrmask == 0x3) {
 			/* 16MB core map; T32-04 or T32-08 */
@@ -172,7 +192,7 @@ tga_identify(dc)
 		} else if (addrmask == 0x7) {
 			/* 32MB core map; T32-88 */
 
-			if (wide)			/* sanity */
+			if (wide && !tga2)			/* sanity */
 				type = TGA_TYPE_T32_88;
 		}
 	}
@@ -190,4 +210,3 @@ tga_getconf(type)
 
 	return (NULL);
 }
-
