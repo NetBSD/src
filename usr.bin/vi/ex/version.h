8547d028613ecb87578e6c0dd2894025175e67d5
aymeric <aymeric@NetBSD.org>
2001-12-13 20:53:47 +0000

    FreeBSD PR#23912 fixed by applying a patch from nvi 1.81.x "underflow of cnt in vs_paint() by O_NUMBER_LENGTH when both leftright and  number options in use"

--- usr.bin/vi/ex/version.h
+++ usr.bin/vi/ex/version.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: version.h,v 1.6 2001/10/20 10:11:41 aymeric Exp $	*/
+/*	$NetBSD: version.h,v 1.7 2001/12/13 20:53:47 aymeric Exp $	*/
 
 #define	VI_VERSION \
-    "Version 1.79nb3 (10/23/96) The CSRG, University of California, Berkeley."
+    "Version 1.79nb4 (10/23/96) The CSRG, University of California, Berkeley."
