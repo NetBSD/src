c840177d628d850891aa48f8cf831cc8e210ba69
tsutsui <tsutsui@NetBSD.org>
2001-12-16 03:53:21 +0000

    Fix comments for ref-count of PT pages.

--- sys/arch/amiga/amiga/pmap.c
+++ sys/arch/amiga/amiga/pmap.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: pmap.c,v 1.95 2001/12/13 04:39:50 chs Exp $	*/
+/*	$NetBSD: pmap.c,v 1.96 2001/12/16 03:53:21 tsutsui Exp $	*/
 
 /*-
  * Copyright (c) 1999 The NetBSD Foundation, Inc.
@@ -2083,7 +2083,7 @@ pmap_remove_mapping(pmap, va, pte, flags)
 			pmap_check_wiring("remove", ptpva);
 #endif
 		/*
-		 * If reference count drops to 1, and we're not instructed
+		 * If reference count drops to 0, and we're not instructed
 		 * to keep it around, free the PT page.
 		 */
 		if (refs == 0 && (flags & PRM_KEEPPTPAGE) == 0) {
