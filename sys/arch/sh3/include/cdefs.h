518a8acbe995ba7d38c3a60703104ae823a3dc08
thorpej <thorpej@NetBSD.org>
2001-12-16 18:11:12 +0000

    Consider __NO_LEADING_UNDERSCORES__ provided by the new ELF toolchain.

--- sys/arch/sh3/include/cdefs.h
+++ sys/arch/sh3/include/cdefs.h
@@ -1,9 +1,15 @@
-/*	$NetBSD: cdefs.h,v 1.2 2001/02/25 15:49:58 msaitoh Exp $	*/
+/*	$NetBSD: cdefs.h,v 1.3 2001/12/16 18:11:12 thorpej Exp $	*/
 
 #ifndef _MACHINE_CDEFS_H_
 #define _MACHINE_CDEFS_H_
 
-#ifdef __ELF__
+/*
+ * The old NetBSD/sh3 ELF toolchain used underscores.  The new
+ * NetBSD/sh3 ELF toolchain does not.  The C pre-processor
+ * defines __NO_LEADING_UNDERSCORES__ for the new ELF toolchain.
+ */
+
+#if defined(__ELF__) && !defined(__NO_LEADING_UNDERSCORES__)
 #define __LEADING_UNDERSCORE
 #endif
 
