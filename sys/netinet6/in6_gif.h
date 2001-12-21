be2041c62bab736a4f9cc0cdf375b0e7c816b09b
itojun <itojun@NetBSD.org>
2001-12-21 06:30:43 +0000

    use radix table for inbound tunnel lookup (would increase performance for machines with a lot of tunnels). update route cache for IPvX-over-IPv6 tunnel on path MTU discovery. snyc with kame

--- sys/netinet6/in6_gif.h
+++ sys/netinet6/in6_gif.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: in6_gif.h,v 1.7 2001/12/21 03:58:15 itojun Exp $	*/
+/*	$NetBSD: in6_gif.h,v 1.8 2001/12/21 06:30:44 itojun Exp $	*/
 /*	$KAME: in6_gif.h,v 1.7 2001/07/26 06:53:16 jinmei Exp $	*/
 
 /*
@@ -37,10 +37,12 @@
 extern int	ip6_gif_hlim;		/* Hop limit for gif encap packet */
 
 struct gif_softc;
+struct sockaddr;
 int in6_gif_input __P((struct mbuf **, int *, int));
 int in6_gif_output __P((struct ifnet *, int, struct mbuf *));
 int gif_encapcheck6 __P((const struct mbuf *, int, int, void *));
 int in6_gif_attach __P((struct gif_softc *));
 int in6_gif_detach __P((struct gif_softc *));
+void in6_gif_ctlinput __P((int, struct sockaddr *, void *));
 
 #endif /* _NETINET6_IN6_GIF_H_ */
