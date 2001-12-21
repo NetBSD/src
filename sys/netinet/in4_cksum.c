e09651bc30ee1fbb262bd4eb7ee55d4030826f4a
itojun <itojun@NetBSD.org>
2001-12-21 02:50:28 +0000

    whitespace.  sync with kame

--- sys/netinet/in4_cksum.c
+++ sys/netinet/in4_cksum.c
@@ -1,9 +1,9 @@
-/*	$NetBSD: in4_cksum.c,v 1.7 2001/11/13 00:32:36 lukem Exp $	*/
+/*	$NetBSD: in4_cksum.c,v 1.8 2001/12/21 02:50:28 itojun Exp $	*/
 
 /*
  * Copyright (C) 1999 WIDE Project.
  * All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
@@ -15,7 +15,7 @@
  * 3. Neither the name of the project nor the names of its contributors
  *    may be used to endorse or promote products derived from this software
  *    without specific prior written permission.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE PROJECT AND CONTRIBUTORS ``AS IS'' AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -65,7 +65,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: in4_cksum.c,v 1.7 2001/11/13 00:32:36 lukem Exp $");
+__KERNEL_RCSID(0, "$NetBSD: in4_cksum.c,v 1.8 2001/12/21 02:50:28 itojun Exp $");
 
 #include <sys/param.h>
 #include <sys/mbuf.h>
@@ -147,7 +147,7 @@ in4_cksum(m, nxt, off, len)
 			 * of a word spanning between this mbuf and the
 			 * last mbuf.
 			 *
-			 * s_util.c[0] is already saved when scanning previous 
+			 * s_util.c[0] is already saved when scanning previous
 			 * mbuf.
 			 */
 			s_util.c[1] = *(u_int8_t *)w;
