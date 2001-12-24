f6488733ab781450ff2a5087f0b303147cd6e0d0
chs <chs>
2001-12-24 17:57:19 +0000

    use ELF instead of a.out symbols now.

--- sys/arch/luna68k/include/db_machdep.h
+++ sys/arch/luna68k/include/db_machdep.h
@@ -1,4 +1,8 @@
-/*	$NetBSD: db_machdep.h,v 1.1 2000/01/05 08:48:57 nisimura Exp $	*/
+/*	$NetBSD: db_machdep.h,v 1.2 2001/12/24 17:57:19 chs Exp $	*/
 
 /* Just use the common m68k definition */
 #include <m68k/db_machdep.h>
+
+#undef DB_AOUT_SYMBOLS
+#define DB_ELF_SYMBOLS
+#define DB_ELFSIZE 32
