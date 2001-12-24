68070a88dc3a37fe888d43f52c1ffa8d8df5bcf5
lukem <lukem@NetBSD.org>
2001-12-24 03:02:34 +0000

    For dump_lfs(8), in getfstab(), only "lfs" (instead of "ufs" or "ffs"). Fix from Minoura Makoto in [bin/10525].

--- sbin/dump/optr.c
+++ sbin/dump/optr.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: optr.c,v 1.23 2001/12/23 12:29:56 lukem Exp $	*/
+/*	$NetBSD: optr.c,v 1.24 2001/12/24 03:02:34 lukem Exp $	*/
 
 /*-
  * Copyright (c) 1980, 1988, 1993
@@ -38,7 +38,7 @@
 #if 0
 static char sccsid[] = "@(#)optr.c	8.2 (Berkeley) 1/6/94";
 #else
-__RCSID("$NetBSD: optr.c,v 1.23 2001/12/23 12:29:56 lukem Exp $");
+__RCSID("$NetBSD: optr.c,v 1.24 2001/12/24 03:02:34 lukem Exp $");
 #endif
 #endif /* not lint */
 
@@ -455,9 +455,14 @@ getfstab(void)
 		    strcmp(fs->fs_type, FSTAB_RO) &&
 		    strcmp(fs->fs_type, FSTAB_RQ))
 			continue;
+#ifdef DUMP_LFS
+		if (strcmp(fs->fs_vfstype, "lfs"))
+			continue;
+#else
 		if (strcmp(fs->fs_vfstype, "ufs") &&
 		    strcmp(fs->fs_vfstype, "ffs"))
 			continue;
+#endif
 		fs = allocfsent(fs);
 		pf = (struct pfstab *)xmalloc(sizeof (*pf));
 		pf->pf_fstab = fs;
