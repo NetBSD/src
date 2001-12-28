a419176fe73c74bc64060e6f141778fbe300fc38
thorpej <thorpej@NetBSD.org>
2001-12-28 07:48:39 +0000

    Add -ffreestanding to CFLAGS.

--- share/mk/bsd.kmod.mk
+++ share/mk/bsd.kmod.mk
@@ -1,4 +1,4 @@
-#	$NetBSD: bsd.kmod.mk,v 1.45 2001/11/28 20:19:08 tv Exp $
+#	$NetBSD: bsd.kmod.mk,v 1.46 2001/12/28 07:48:39 thorpej Exp $
 
 .include <bsd.init.mk>
 
@@ -12,7 +12,7 @@ realinstall:	kmodinstall
 S?=		/sys
 KERN=		$S/kern
 
-CFLAGS+=	${COPTS} -D_KERNEL -D_LKM
+CFLAGS+=	-ffreestanding ${COPTS} -D_KERNEL -D_LKM
 CPPFLAGS+=	-nostdinc -I. -I${.CURDIR} -isystem $S -isystem $S/arch
 
 DPSRCS+=	${SRCS:M*.l:.l=.c} ${SRCS:M*.y:.y=.c}
