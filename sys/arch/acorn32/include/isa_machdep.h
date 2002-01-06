5923c796663aac0909e5a9e78449c916622ac95d
thorpej <thorpej@NetBSD.org>
2002-01-06 20:29:42 +0000

    Acorn systems do not support the ISA bus; remove these files.

--- sys/arch/acorn32/include/isa_machdep.h
+++
@@ -1,3 +0,0 @@
-/* $NetBSD: isa_machdep.h,v 1.1 2001/10/05 22:27:51 reinoud Exp $ */
-
-#include <arm/isa_machdep.h>
--- sys/arch/acorn32/include/isapnp_machdep.h
+++
@@ -1,4 +0,0 @@
-/* $NetBSD: isapnp_machdep.h,v 1.1 2001/10/05 22:27:51 reinoud Exp $ */
-
-#include <arm/isapnp_machdep.h>
-
