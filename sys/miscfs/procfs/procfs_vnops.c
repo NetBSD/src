62ec8f0cef05c60d218fed310f5659e40edd1421
christos <christos@NetBSD.org>
2002-01-12 18:51:56 +0000

    Don't hide the real return code with EPERM.

--- sys/miscfs/procfs/procfs_vnops.c
+++ sys/miscfs/procfs/procfs_vnops.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: procfs_vnops.c,v 1.87 2001/12/06 04:27:42 chs Exp $	*/
+/*	$NetBSD: procfs_vnops.c,v 1.88 2002/01/12 18:52:30 christos Exp $	*/
 
 /*
  * Copyright (c) 1993 Jan-Simon Pendry
@@ -44,7 +44,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: procfs_vnops.c,v 1.87 2001/12/06 04:27:42 chs Exp $");
+__KERNEL_RCSID(0, "$NetBSD: procfs_vnops.c,v 1.88 2002/01/12 18:52:30 christos Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -242,6 +242,7 @@ procfs_open(v)
 	} */ *ap = v;
 	struct pfsnode *pfs = VTOPFS(ap->a_vp);
 	struct proc *p1, *p2;
+	int error;
 
 	p1 = ap->a_p;				/* tracer */
 	p2 = PFIND(pfs->pfs_pid);		/* traced */
@@ -255,8 +256,8 @@ procfs_open(v)
 		    ((pfs->pfs_flags & O_EXCL) && (ap->a_mode & FWRITE)))
 			return (EBUSY);
 
-		if (procfs_checkioperm(p1, p2) != 0)
-			return (EPERM);
+		if ((error = procfs_checkioperm(p1, p2)) != 0)
+			return (error);
 
 		if (ap->a_mode & FWRITE)
 			pfs->pfs_flags = ap->a_mode & (FWRITE|O_EXCL);
