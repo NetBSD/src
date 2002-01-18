7d651083bb605ba32a80b6deee194c58efdd5761
lukem <lukem@NetBSD.org>
2002-01-18 08:20:32 +0000

    don't need ffs/buf.h here

--- usr.sbin/makefs/ffs/ufs_bmap.c
+++ usr.sbin/makefs/ffs/ufs_bmap.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: ufs_bmap.c,v 1.9 2002/01/08 06:00:14 lukem Exp $	*/
+/*	$NetBSD: ufs_bmap.c,v 1.10 2002/01/18 08:20:32 lukem Exp $	*/
 /* From: NetBSD: ufs_bmap.c,v 1.14 2001/11/08 05:00:51 chs Exp */
 
 /*
@@ -43,7 +43,7 @@
 
 #include <sys/cdefs.h>
 #ifndef __lint
-__RCSID("$NetBSD: ufs_bmap.c,v 1.9 2002/01/08 06:00:14 lukem Exp $");
+__RCSID("$NetBSD: ufs_bmap.c,v 1.10 2002/01/18 08:20:32 lukem Exp $");
 #endif	/* !__lint */
 
 #include <sys/param.h>
@@ -59,7 +59,6 @@ __RCSID("$NetBSD: ufs_bmap.c,v 1.9 2002/01/08 06:00:14 lukem Exp $");
 #include <ufs/ufs/ufs_bswap.h>
 #include <ufs/ffs/fs.h>
 
-#include "ffs/buf.h"
 #include "ffs/ufs_inode.h"
 #include "ffs/ffs_extern.h"
 
