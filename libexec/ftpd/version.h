ef93fa931d97e8a3e394e8add5c267adbb9653d4
lukem <lukem@NetBSD.org>
2002-01-21 11:25:20 +0000

    in closedataconn(), only close the passive data fd if the main data descriptor was set by dataconn().  this fixes a problem for clients (such as lynx and netscape) that only sent PASV/EPSV after a transfer (RETR, LIST, STOR) started and returned 150.  certain command sequences could return 550 (etc) before setting up the dataconn(), and would run into this bug. netbsd's ftp didn't hit this bug because it always sends PASV/EPSV before a new transfer command.

--- libexec/ftpd/version.h
+++ libexec/ftpd/version.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: version.h,v 1.38 2001/12/06 02:00:07 lukem Exp $	*/
+/*	$NetBSD: version.h,v 1.39 2002/01/21 11:25:21 lukem Exp $	*/
 /*-
  * Copyright (c) 1999-2001 The NetBSD Foundation, Inc.
  * All rights reserved.
@@ -36,5 +36,5 @@
  */
 
 #ifndef FTPD_VERSION
-#define	FTPD_VERSION	"NetBSD-ftpd 20011206"
+#define	FTPD_VERSION	"NetBSD-ftpd 20020121"
 #endif
