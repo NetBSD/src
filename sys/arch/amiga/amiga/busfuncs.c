d1c534088fb2397a959f83a469355f8353084271
aymeric <aymeric>
2002-01-26 13:24:53 +0000

    - ANSIfy - remove some trailing spaces/tabs - minor style nits

--- sys/arch/amiga/amiga/busfuncs.c
+++ sys/arch/amiga/amiga/busfuncs.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: busfuncs.c,v 1.6 1999/02/16 23:34:12 is Exp $	*/
+/*	$NetBSD: busfuncs.c,v 1.7 2002/01/26 13:24:53 aymeric Exp $	*/
 
 /*-
  * Copyright (c) 1998 The NetBSD Foundation, Inc.
@@ -115,8 +115,8 @@ const struct amiga_bus_space_methods amiga_interleaved_wordaccess_methods = {
 /*
  * Contiguous methods
  * Be sure to only create word accesses.
- * For busses that _need_ split byte accesses, use the interleaved functions. 
- */ 
+ * For busses that _need_ split byte accesses, use the interleaved functions.
+ */
 
 u_int16_t
 amiga_contiguous_read_2(t, h, o)
