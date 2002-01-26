868743df7aaef7f3c438ef6df75c7db4255217d5
ichiro <ichiro>
2002-01-26 16:30:00 +0000

    OCHI -> OHCI

--- sys/dev/pci/fwohci_pci.c
+++ sys/dev/pci/fwohci_pci.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: fwohci_pci.c,v 1.12 2001/11/15 09:48:12 lukem Exp $	*/
+/*	$NetBSD: fwohci_pci.c,v 1.13 2002/01/26 16:30:00 ichiro Exp $	*/
 
 /*-
  * Copyright (c) 2000 The NetBSD Foundation, Inc.
@@ -37,7 +37,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: fwohci_pci.c,v 1.12 2001/11/15 09:48:12 lukem Exp $");
+__KERNEL_RCSID(0, "$NetBSD: fwohci_pci.c,v 1.13 2002/01/26 16:30:00 ichiro Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -103,7 +103,7 @@ fwohci_pci_attach(struct device *parent, struct device *self, void *aux)
 	if (pci_mapreg_map(pa, PCI_OHCI_MAP_REGISTER, PCI_MAPREG_TYPE_MEM, 0,
 	    &psc->psc_sc.sc_memt, &psc->psc_sc.sc_memh,
 	    NULL, &psc->psc_sc.sc_memsize)) {
-		printf("%s: can't map OCHI register space\n", self->dv_xname);
+		printf("%s: can't map OHCI register space\n", self->dv_xname);
 		return;
 	}
 
