ffb46385e73eaf56a031ce8d66f6c27067e76586
thorpej <thorpej>
2002-01-28 21:52:49 +0000

    Just use the stock m68k/elf_machdep.h for now, so that things compile. A patch to handle EF_M68000 will be posted shortly.

--- sys/arch/sun2/include/elf_machdep.h
+++ sys/arch/sun2/include/elf_machdep.h
@@ -1,16 +1,8 @@
-/*	$NetBSD: elf_machdep.h,v 1.3 2001/12/09 23:05:59 thorpej Exp $	*/
+/*	$NetBSD: elf_machdep.h,v 1.4 2002/01/28 21:52:49 thorpej Exp $	*/
 
 #ifndef _MACHINE_ELF_MACHDEP_H_
 #define _MACHINE_ELF_MACHDEP_H_
 
 #include <m68k/elf_machdep.h>
-
-#undef	ELF32_MACHDEP_ID_CASES
-#define	ELF32_MACHDEP_ID_CASES						\
-                case EM_68000:						\
-                        break;
-
-#undef	ELF32_MACHDEP_ID
-#define	ELF32_MACHDEP_ID	EM_68000
 
 #endif
