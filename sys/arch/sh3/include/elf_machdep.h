041f307c151b431144aa054f26fdaa9f1757f269
thorpej <thorpej@NetBSD.org>
2002-01-29 01:42:57 +0000

    Add SuperH ELF header flags.

--- sys/arch/sh3/include/elf_machdep.h
+++ sys/arch/sh3/include/elf_machdep.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: elf_machdep.h,v 1.6 2002/01/29 01:35:36 thorpej Exp $	*/
+/*	$NetBSD: elf_machdep.h,v 1.7 2002/01/29 01:42:57 thorpej Exp $	*/
 
 #ifndef _BYTE_ORDER
 #error Define _BYTE_ORDER!
@@ -20,6 +20,24 @@
 #define	ELF32_MACHDEP_ID	EM_SH
 
 #define ARCH_ELFSIZE		32	/* MD native binary size */
+
+/*
+ * SuperH ELF header flags.
+ */
+#define	EF_SH_MACH_MASK		0x1f
+
+#define	EF_SH_UNKNOWN		0x00
+#define	EF_SH_SH1		0x01
+#define	EF_SH_SH2		0x02
+#define	EF_SH_SH3		0x03
+#define	EF_SH_DSP		0x04
+#define	EF_SH_SH3_DSP		0x05
+#define	EF_SH_SH3E		0x08
+#define	EF_SH_SH4		0x09
+
+#define	EF_SH_HAS_DSP(x)	((x) & EF_SH_DSP)
+#define	EF_SH_HAS_FP(x)		((x) & EF_SH_SH3E)
+
 
 #define	R_SH_NONE		0
 #define	R_SH_DIR32		1
