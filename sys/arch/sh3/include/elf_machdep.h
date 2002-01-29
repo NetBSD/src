7f7889b72b6ffc776f94a8185441641247ec20db
thorpej <thorpej@NetBSD.org>
2002-01-29 01:35:36 +0000

    Add SuperH relocation types.

--- sys/arch/sh3/include/elf_machdep.h
+++ sys/arch/sh3/include/elf_machdep.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: elf_machdep.h,v 1.5 2001/12/09 23:05:58 thorpej Exp $	*/
+/*	$NetBSD: elf_machdep.h,v 1.6 2002/01/29 01:35:36 thorpej Exp $	*/
 
 #ifndef _BYTE_ORDER
 #error Define _BYTE_ORDER!
@@ -20,3 +20,37 @@
 #define	ELF32_MACHDEP_ID	EM_SH
 
 #define ARCH_ELFSIZE		32	/* MD native binary size */
+
+#define	R_SH_NONE		0
+#define	R_SH_DIR32		1
+#define	R_SH_REL32		2
+#define	R_SH_DIR8WPN		3
+#define	R_SH_IND12W		4
+#define	R_SH_DIR8WPL		5
+#define	R_SH_DIR8WPZ		6
+#define	R_SH_DIR8BP		7
+#define	R_SH_DIR8W		8
+#define	R_SH_DIR8L		9
+#define	R_SH_SWITCH16		25
+#define	R_SH_SWITCH32		26
+#define	R_SH_USES		27
+#define	R_SH_COUNT		28
+#define	R_SH_ALIGN		29
+#define	R_SH_CODE		30
+#define	R_SH_DATA		31
+#define	R_SH_LABEL		32
+#define	R_SH_SWITCH8		33
+#define	R_SH_GNU_VTINHERIT	34
+#define	R_SH_GNU_VTENTRY	35
+#define	R_SH_LOOP_START		36
+#define	R_SH_LOOP_END		37
+#define	R_SH_GOT32		160
+#define	R_SH_PLT32		161
+#define	R_SH_COPY		162
+#define	R_SH_GLOB_DAT		163
+#define	R_SH_JMP_SLOT		164
+#define	R_SH_RELATIVE		165
+#define	R_SH_GOTOFF		166
+#define	R_SH_GOTPC		167
+
+#define	R_TYPE(name)	__CONCAT(R_SH_,name)
