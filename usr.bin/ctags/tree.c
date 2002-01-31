9bbcbcba1de6bb78aa9f341febfaae644470ac62
tv <tv@NetBSD.org>
2002-01-31 19:26:34 +0000

    Make this compilable as a host tool.

--- usr.bin/ctags/tree.c
+++ usr.bin/ctags/tree.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: tree.c,v 1.6 1998/11/06 23:06:30 christos Exp $	*/
+/*	$NetBSD: tree.c,v 1.7 2002/01/31 19:26:35 tv Exp $	*/
 
 /*
  * Copyright (c) 1987, 1993, 1994
@@ -34,11 +34,11 @@
  */
 
 #include <sys/cdefs.h>
-#ifndef lint
+#if defined(__RCSID) && !defined(lint)
 #if 0
 static char sccsid[] = "@(#)tree.c	8.3 (Berkeley) 4/2/94";
 #else
-__RCSID("$NetBSD: tree.c,v 1.6 1998/11/06 23:06:30 christos Exp $");
+__RCSID("$NetBSD: tree.c,v 1.7 2002/01/31 19:26:35 tv Exp $");
 #endif
 #endif /* not lint */
 
