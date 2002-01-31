f2bdb121983e8e38746c6a9338290befea30c259
tv <tv@NetBSD.org>
2002-01-31 21:01:15 +0000

    In the generated C file, don't assume that <sys/cdefs.h> exists.  Instead, imply it with the <stdlib.h> include already present in the file, and provide a compatibility definition for __P().

--- usr.bin/yacc/skeleton.c
+++ usr.bin/yacc/skeleton.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: skeleton.c,v 1.22 2002/01/30 20:37:59 tv Exp $	*/
+/*	$NetBSD: skeleton.c,v 1.23 2002/01/31 21:01:15 tv Exp $	*/
 
 /*
  * Copyright (c) 1989 The Regents of the University of California.
@@ -41,7 +41,7 @@
 #if 0
 static char sccsid[] = "@(#)skeleton.c	5.8 (Berkeley) 4/29/95";
 #else
-__RCSID("$NetBSD: skeleton.c,v 1.22 2002/01/30 20:37:59 tv Exp $");
+__RCSID("$NetBSD: skeleton.c,v 1.23 2002/01/31 21:01:15 tv Exp $");
 #endif /* 0 */
 #endif /* not lint */
 
@@ -61,17 +61,23 @@ __RCSID("$NetBSD: skeleton.c,v 1.22 2002/01/30 20:37:59 tv Exp $");
 
 const char * const banner[] =
 {
-    "#include <sys/cdefs.h>",
+    "#include <stdlib.h>",
     "#ifndef lint",
     "#if 0",
     "static char yysccsid[] = \"@(#)yaccpar	1.9 (Berkeley) 02/21/93\";",
     "#else",
     "#if defined(__NetBSD__) && defined(__IDSTRING)",
-    "__IDSTRING(yyrcsid, \"$NetBSD: skeleton.c,v 1.22 2002/01/30 20:37:59 tv Exp $\");",
+    "__IDSTRING(yyrcsid, \"$NetBSD: skeleton.c,v 1.23 2002/01/31 21:01:15 tv Exp $\");",
     "#endif /* __NetBSD__ && __IDSTRING */",
     "#endif /* 0 */",
     "#endif /* lint */",
-    "#include <stdlib.h>",
+    "#ifndef __P",
+    "#ifdef __STDC__",
+    "#define __P(x) x",
+    "#else",
+    "#define __P(x) ()",
+    "#endif",
+    "#endif",
     "#define YYBYACC 1",
     "#define YYMAJOR 1",
     "#define YYMINOR 9",
