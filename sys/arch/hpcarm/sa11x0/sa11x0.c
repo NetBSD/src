e5487fbaf83c8548274b689ec5ee7ceb60bbafc6
chris <chris@NetBSD.org>
2002-02-08 10:37:16 +0000

    Fix location of some headers, IPAQ kernel now manages to make depend.

--- sys/arch/hpcarm/sa11x0/sa11x0.c
+++ sys/arch/hpcarm/sa11x0/sa11x0.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: sa11x0.c,v 1.17 2001/07/10 16:39:33 ichiro Exp $	*/
+/*	$NetBSD: sa11x0.c,v 1.18 2002/02/08 10:37:16 chris Exp $	*/
 
 /*-
  * Copyright (c) 2001, The NetBSD Foundation, Inc.  All rights reserved.
@@ -71,8 +71,8 @@
 #include <hpcarm/sa11x0/sa11x0_ppcreg.h>
 #include <hpcarm/sa11x0/sa11x0_gpioreg.h>
 
-#include <hpc/hpc/config_hook.h>
-#include <hpc/hpc/platid.h>
+#include <hpc/include/config_hook.h>
+#include <hpc/include/platid.h>
 #include <hpc/include/platid_mask.h>
 
 #include "locators.h"
--- sys/arch/hpcarm/sa11x0/sa11x0_com.c
+++ sys/arch/hpcarm/sa11x0/sa11x0_com.c
@@ -1,4 +1,4 @@
-/*      $NetBSD: sa11x0_com.c,v 1.13 2002/01/04 16:36:41 toshii Exp $        */
+/*      $NetBSD: sa11x0_com.c,v 1.14 2002/02/08 10:37:16 chris Exp $        */
 
 /*-
  * Copyright (c) 1998, 1999, 2001 The NetBSD Foundation, Inc.
@@ -104,7 +104,7 @@
 #include <hpcarm/sa11x0/sa11x0_comreg.h>
 #include <hpcarm/sa11x0/sa11x0_comvar.h>
 
-#include <hpc/hpc/platid.h>
+#include <hpc/include/platid.h>
 #include <hpc/include/platid_mask.h>
 
 #include "sacom.h"
