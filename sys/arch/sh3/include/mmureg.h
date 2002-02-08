82581abf03457b5926bd6b7f4c3824788d7a4fdc
uch <uch@NetBSD.org>
2002-02-08 06:11:38 +0000

    add SH3/SH4 namespace free defines.

--- sys/arch/sh3/include/mmureg.h
+++ sys/arch/sh3/include/mmureg.h
@@ -1,7 +1,7 @@
-/* $Id: mmureg.h,v 1.4 2000/09/04 05:48:35 msaitoh Exp $ */
-/* $NetBSD: mmureg.h,v 1.4 2000/09/04 05:48:35 msaitoh Exp $ */
+/*	$NetBSD: mmureg.h,v 1.5 2002/02/08 06:11:38 uch Exp $	*/
 
 /*-
+ * Copyright (C) 2002 UCHIYAMA Yasushi.  All rights reserved.
  * Copyright (C) 1999 SAITOH Masanobu.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -30,6 +30,7 @@
 #ifndef _SH3_MMUREG_H__
 #define _SH3_MMUREG_H__
 
+#ifdef _KERNEL
 /*
  * MMU
  */
@@ -44,7 +45,7 @@
 #define SHREG_TEA	(*(volatile unsigned long *)	0xFFFFFFFC)
 #define SHREG_MMUCR	(*(volatile unsigned long *)	0xFFFFFFE0)
 
-#else
+#else /* !SH4 */
 
 /* SH4 definitions */
 
@@ -55,7 +56,7 @@
 #define SHREG_TEA	(*(volatile unsigned long *)	0xff00000c)
 #define SHREG_MMUCR	(*(volatile unsigned long *)	0xff000010)
 
-#endif
+#endif /* !SH4 */
 
 #if !defined(SH4)
 
@@ -65,7 +66,7 @@
 #define MMUCR_TF	0x0004	/* TLB flush */
 #define MMUCR_SV	0x0100	/* single virtual space mode */
 
-#else
+#else /* !SH4 */
 
 /* SH4 definitions */
 #define MMUCR_AT	0x0001	/* address traslation enable */
@@ -78,11 +79,157 @@
 /* alias */
 #define MMUCR_TF	MMUCR_TI
 
-#endif
+#endif /* !SH4 */
 
-
-#ifdef _KERNEL
 extern int PageDirReg;
-#endif
 
+/*
+ * name-space free version.
+ */
+/*
+ *	SH3
+ */
+/* 128-entry 4-way set-associative */
+#define SH3_MMU_WAY			4
+#define SH3_MMU_ENTRY			32
+
+#define SH3_PTEH			0xfffffff0
+#define   SH3_PTEH_ASID_MASK		  0x0000000f
+#define   SH3_PTEH_VPN_MASK		  0xfffffc00
+#define SH3_MMUCR			0xffffffe0
+#define   SH3_MMUCR_AT			  0x00000001
+#define   SH3_MMUCR_IX			  0x00000002
+#define   SH3_MMUCR_TF			  0x00000004
+#define   SH3_MMUCR_RC			  0x00000030
+#define   SH3_MMUCR_SV			  0x00000100
+
+/* 
+ * memory-mapped TLB 
+ */
+/* Address array */
+#define SH3_MMUAA			0xf2000000
+/* address specification */
+#define   SH3_MMU_VPN_SHIFT		  12
+#define   SH3_MMU_VPN_MASK		  0x0001f000	/* [16:12] */
+#define   SH3_MMU_WAY_SHIFT		  8
+#define   SH3_MMU_WAY_MASK		  0x00000300
+/* data specification */
+#define   SH3_MMU_D_VALID		  0x00000100
+#define   SH3_MMUAA_D_VPN_MASK		  0xfffe0c00	/* [31:17][11:10] */
+#define   SH3_MMUAA_D_ASID_MASK		  0x0000000f
+
+/* Data array */
+#define SH3_MMUDA			0xf3000000
+#define   SH3_MMUDA_D_PPN_MASK		  0xfffffc00
+#define   SH3_MMUDA_D_V			  0x00000100
+#define   SH3_MMUDA_D_PR_SHIFT		  5
+#define   SH3_MMUDA_D_PR_MASK		  0x00000060	/* [6:5] */
+#define   SH3_MMUDA_D_SZ		  0x00000010
+#define   SH3_MMUDA_D_C			  0x00000008
+#define   SH3_MMUDA_D_D			  0x00000004
+#define   SH3_MMUDA_D_SH		  0x00000002
+
+/*
+ *	SH4
+ */
+/* ITLB 4-entry full-associative UTLB 64-entry full-associative */
+#define SH4_PTEH			0xff000000
+#define   SH4_PTEH_ASID_MASK		  0x0000000f
+#define SH4_PTEL			0xff000004
+#define   SH4_PTEL_WT			  0x00000001
+#define   SH4_PTEL_SH			  0x00000002
+#define   SH4_PTEL_D			  0x00000004
+#define   SH4_PTEL_C			  0x00000008
+#define   SH4_PTEL_PR_SHIFT		  5
+#define   SH4_PTEL_PR_MASK		  0x00000060	/* [5:6] */
+#define   SH4_PTEL_SZ_MASK		  0x00000090	/* [4][7] */
+#define     SH4_PTEL_SZ_1K		  0x00000000
+#define     SH4_PTEL_SZ_4K		  0x00000010
+#define     SH4_PTEL_SZ_64K		  0x00000080
+#define     SH4_PTEL_SZ_1M		  0x00000090
+#define   SH4_PTEL_V			  0x00000100
+#define SH4_PTEA			0xff000034
+#define   SH4_PTEA_SA_MASK		  0x00000007
+#define   SH4_PTEA_SA_TC		  0x00000008
+#define SH4_TTB				0xff000008
+#define SH4_TTA				0xff00000c
+#define SH4_MMUCR			0xff000010
+#define   SH4_MMUCR_AT			  0x00000001
+#define   SH4_MMUCR_TI			  0x00000004
+#define   SH4_MMUCR_SV			  0x00000100
+#define   SH4_MMUCR_SQMD		  0x00000200
+#define   SH4_MMUCR_URC_SHIFT		  10
+#define   SH4_MMUCR_URC_MASK		  0x0000fc00	/* [10:15] */
+#define   SH4_MMUCR_URB_SHIFT		  18
+#define   SH4_MMUCR_URB_MASK		  0x00fc0000	/* [18:23] */
+#define   SH4_MMUCR_LRUI_SHIFT		  26
+#define   SH4_MMUCR_LRUT_MASK		  0xfc000000	/* [26:31] */
+/* 
+ * memory-mapped TLB 
+ *	must be access from P2-area program.
+ *	branch to the other area must be maed at least 8 instruction
+ *	after access.
+ */
+/* ITLB */
+#define SH4_ITLB_AA			0xf2000000
+/* address specification (common for address and data array(0,1)) */
+#define   SH4_ITLB_E_SHIFT		  8
+#define   SH4_ITLB_E_MASK		  0x00000300	/* [9:8] */
+/* data specification */
+/* address-array */
+#define   SH4_ITLB_AA_ASID_MASK		  0x000000ff	/* [7:0] */
+#define   SH4_ITLB_AA_V			  0x00000100
+#define   SH4_ITLB_AA_VPN_SHIFT		  10
+#define   SH4_ITLB_AA_VPN_MASK		  0xfffffc00	/* [31:10] */
+/* data-array 1 */
+#define SH4_ITLB_DA1			0xf3000000
+#define   SH4_ITLB_DA1_SH		  0x00000002
+#define   SH4_ITLB_DA1_C		  0x00000008
+#define   SH4_ITLB_DA1_SZ_MASK		  0x00000090	/* [7][4] */
+#define     SH4_ITLB_DA1_SZ_1K		  0x00000000
+#define     SH4_ITLB_DA1_SZ_4K		  0x00000010
+#define     SH4_ITLB_DA1_SZ_64K		  0x00000080
+#define     SH4_ITLB_DA1_SZ_1M		  0x00000090
+#define   SH4_ITLB_DA1_PR		  0x00000040
+#define   SH4_ITLB_DA1_V		  0x00000100
+#define   SH4_ITLB_DA1_PPN_SHIFT	  11
+#define   SH4_ITLB_DA1_PPN_MASK		  0x1ffffc00	/* [28:10] */
+/* data-array 2 */
+#define SH4_ITLB_DA2			0xf3800000
+#define   SH4_ITLB_DA2_SA_MASK		  0x00000003
+#define   SH4_ITLB_DA2_TC		  0x00000004
+
+/* UTLB */
+#define SH4_UTLB_AA			0xf6000000
+/* address specification (common for address and data array(0,1)) */
+#define   SH4_UTLB_E_SHIFT		  8
+#define   SH4_UTLB_E_MASK		  0x00003f00
+/* data specification */
+/* address-array */
+#define   SH4_UTLB_AA_VPN_MASK		  0xfffffc00	/* [31:10] */
+#define   SH4_UTLB_AA_D			  0x00000200
+#define   SH4_UTLB_AA_V			  0x00000100
+#define   SH4_UTLB_AA_ASID_MASK		  0x000000ff	/* [7:0] */
+/* data-array 1 */
+#define SH4_UTLB_DA1			0xf7000000
+#define   SH4_UTLB_DA1_WT		  0x00000001
+#define   SH4_UTLB_DA1_SH		  0x00000002
+#define   SH4_UTLB_DA1_D		  0x00000004
+#define   SH4_UTLB_DA1_C		  0x00000008
+#define   SH4_UTLB_DA1_SZ_MASK		  0x00000090	/* [7][4] */
+#define     SH4_UTLB_DA1_SZ_1K		  0x00000000
+#define     SH4_UTLB_DA1_SZ_4K		  0x00000010
+#define     SH4_UTLB_DA1_SZ_64K		  0x00000080
+#define     SH4_UTLB_DA1_SZ_1M		  0x00000090
+#define   SH4_UTLB_DA1_PR_SHIFT		  5
+#define   SH4_UTLB_DA1_PR_MASK		  0x00000060
+#define   SH4_UTLB_DA1_V		  0x00000100
+#define   SH4_UTLB_DA1_PPN_SHIFT	  11
+#define   SH4_UTLB_DA1_PPN_MASK		  0x1ffffc00	/* [28:10] */
+/* data-array 2 */
+#define SH4_UTLB_DA2			0xf7800000
+#define   SH4_UTLB_DA2_SA_MASK		  0x00000003
+#define   SH4_UTLB_DA2_TC		  0x00000004
+
+#endif /* _KERNEL */
 #endif /* !_SH3_MMUREG_H__ */
