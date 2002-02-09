1f81c546914bc68c07b95769891db0422a3facce
he <he>
2002-02-09 16:55:08 +0000

    Pull up revisions 1.10-1.11 (requested by martti):   Updated IPFilter to 3.4.23

--- sys/netinet/ipl.h
+++ sys/netinet/ipl.h
@@ -1,19 +1,17 @@
-/*	$NetBSD: ipl.h,v 1.8.2.1 2000/08/31 14:49:50 veego Exp $	*/
+/*	$NetBSD: ipl.h,v 1.8.2.2 2002/02/09 16:55:08 he Exp $	*/
 
 /*
- * Copyright (C) 1993-2000 by Darren Reed.
+ * Copyright (C) 1993-2002 by Darren Reed.
  *
- * Redistribution and use in source and binary forms are permitted
- * provided that this notice is preserved and due credit is given
- * to the original author and the contributors.
+ * See the IPFILTER.LICENCE file for details on licencing.
  *
  * @(#)ipl.h	1.21 6/5/96
- * Id: ipl.h,v 2.15.2.10 2000/08/07 15:10:09 darrenr Exp
+ * Id: ipl.h,v 2.15.2.29 2002/01/15 14:36:51 darrenr Exp
  */
 
 #ifndef	__IPL_H__
 #define	__IPL_H__
 
-#define	IPL_VERSION	"IP Filter: v3.4.9"
+#define	IPL_VERSION	"IP Filter: v3.4.23"
 
 #endif
