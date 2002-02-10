8a2f3ea3f7bc82b4fbd35645a913a37869fa5b6c
thorpej <thorpej@NetBSD.org>
2002-02-10 01:28:32 +0000

    Add the Ethertype for 802.3x flow control packets.

--- sys/net/ethertypes.h
+++ sys/net/ethertypes.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: ethertypes.h,v 1.12 2001/10/18 21:29:19 matt Exp $	*/
+/*	$NetBSD: ethertypes.h,v 1.13 2002/02/10 01:28:32 thorpej Exp $	*/
 
 /*
  * Copyright (c) 1982, 1986, 1993
@@ -291,6 +291,7 @@
 #define	ETHERTYPE_TCPCOMP	0x876B	/* TCP/IP Compression (RFC1701) */
 #define	ETHERTYPE_IPAS		0x876C	/* IP Autonomous Systems (RFC1701) */
 #define	ETHERTYPE_SECUREDATA	0x876D	/* Secure Data (RFC1701) */
+#define	ETHERTYPE_FLOWCONTROL	0x8808	/* 802.3x flow control packet */
 #define	ETHERTYPE_PPP		0x880B	/* PPP (obsolete by PPPOE) */
 #define	ETHERTYPE_HITACHI	0x8820	/* Hitachi Cable (Optoelectronic Systems Laboratory) */
 #define	ETHERTYPE_MPLS		0x8847	/* MPLS Unicast */
