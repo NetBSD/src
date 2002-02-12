6b268b9d7c4e3a4153ec0e70e3a61c483ee2f63b
scw <scw>
2002-02-12 20:38:09 +0000

    Separate out devices common to many Motorola mvme boards (68k, 88k and ppc) in preparation for future mvmeppc and mvme88k ports.
    This needs a bit if tidying up to make it trully shareable, which will
    happen as the new mvme ports are added.

--- sys/arch/mvme68k/include/cpu.h
+++ sys/arch/mvme68k/include/cpu.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: cpu.h,v 1.24 2001/07/06 19:00:13 scw Exp $	*/
+/*	$NetBSD: cpu.h,v 1.25 2002/02/12 20:38:35 scw Exp $	*/
 
 /*
  * Copyright (c) 1988 University of Utah.
@@ -148,6 +148,7 @@ extern int astpending;		/* need to trap before returning to user mode */
 /*
  * Associate MVME models with CPU types.
  */
+#define	MVME68K		1	
 
 /*
  * MVME-147; 68030 CPU
