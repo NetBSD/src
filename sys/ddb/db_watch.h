4eaa4d66a825d3a320df04057b60d4290e726694
simonb <simonb@NetBSD.org>
2002-02-15 07:33:48 +0000

    ANSIfy, KNF. Make some variables and functions static when not used outside of a module. Make variables in headers extern. Delete the unused db_find_watchpoint() function.

--- sys/ddb/db_watch.h
+++ sys/ddb/db_watch.h
@@ -1,27 +1,27 @@
-/*	$NetBSD: db_watch.h,v 1.14 2001/06/02 18:09:25 chs Exp $	*/
+/*	$NetBSD: db_watch.h,v 1.15 2002/02/15 07:33:54 simonb Exp $	*/
 
-/* 
+/*
  * Mach Operating System
  * Copyright (c) 1991,1990 Carnegie Mellon University
  * All Rights Reserved.
- * 
+ *
  * Permission to use, copy, modify and distribute this software and its
  * documentation is hereby granted, provided that both the copyright
  * notice and this permission notice appear in all copies of the
  * software, derivative works or modified versions, and any portions
  * thereof, and that both notices appear in supporting documentation.
- * 
+ *
  * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS "AS IS"
  * CONDITION.  CARNEGIE MELLON DISCLAIMS ANY LIABILITY OF ANY KIND FOR
  * ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.
- * 
+ *
  * Carnegie Mellon requests users of this software to return to
- * 
+ *
  *  Software Distribution Coordinator  or  Software.Distribution@CS.CMU.EDU
  *  School of Computer Science
  *  Carnegie Mellon University
  *  Pittsburgh PA 15213-3890
- * 
+ *
  * any improvements or extensions that they make and grant Carnegie the
  * rights to redistribute these changes.
  *
@@ -42,16 +42,10 @@ typedef struct db_watchpoint {
 	struct db_watchpoint *link;	/* link in in-use or free chain */
 } *db_watchpoint_t;
 
-db_watchpoint_t db_watchpoint_alloc __P((void));
-void db_watchpoint_free __P((db_watchpoint_t));
-void db_set_watchpoint __P((struct vm_map *, db_addr_t, vsize_t));
-void db_delete_watchpoint __P((struct vm_map *, db_addr_t));
-void db_list_watchpoints __P((void));
-void db_deletewatch_cmd __P((db_expr_t, int, db_expr_t, char *));
-void db_watchpoint_cmd __P((db_expr_t, int, db_expr_t, char *));
-void db_listwatch_cmd __P((db_expr_t, int, db_expr_t, char *));
-void db_set_watchpoints __P((void));
-void db_clear_watchpoints __P((void));
-boolean_t db_find_watchpoint __P((struct vm_map *, db_addr_t, db_regs_t *));
+void		db_deletewatch_cmd(db_expr_t, int, db_expr_t, char *);
+void		db_watchpoint_cmd(db_expr_t, int, db_expr_t, char *);
+void		db_listwatch_cmd(db_expr_t, int, db_expr_t, char *);
+void		db_set_watchpoints(void);
+void		db_clear_watchpoints(void);
 
 #endif	/* _DDB_DB_WATCH_ */
--- sys/ddb/db_xxx.c
+++ sys/ddb/db_xxx.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: db_xxx.c,v 1.15 2001/11/12 22:54:07 lukem Exp $	*/
+/*	$NetBSD: db_xxx.c,v 1.16 2002/02/15 07:33:54 simonb Exp $	*/
 
 /*
  * Copyright (c) 1982, 1986, 1989, 1991, 1993
@@ -41,7 +41,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: db_xxx.c,v 1.15 2001/11/12 22:54:07 lukem Exp $");
+__KERNEL_RCSID(0, "$NetBSD: db_xxx.c,v 1.16 2002/02/15 07:33:54 simonb Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -64,11 +64,7 @@ __KERNEL_RCSID(0, "$NetBSD: db_xxx.c,v 1.15 2001/11/12 22:54:07 lukem Exp $");
 #include <ddb/db_extern.h>
 
 void
-db_kill_proc(addr, haddr, count, modif)
-	db_expr_t addr;
-	int haddr;
-	db_expr_t count;
-	char *modif;
+db_kill_proc(db_expr_t addr, int haddr, db_expr_t count, char *modif)
 {
 	struct proc *p;
 	db_expr_t pid, sig;
@@ -77,7 +73,7 @@ db_kill_proc(addr, haddr, count, modif)
 	/* What pid? */
 	if (!db_expression(&pid)) {
 		db_error("pid?\n");
-	    /*NOTREACHED*/
+		/*NOTREACHED*/
 	}
 	/* What sig? */
 	t = db_read_token();
@@ -91,31 +87,27 @@ db_kill_proc(addr, haddr, count, modif)
 		sig = 15;
 	}
 	if (db_read_token() != tEOL) {
-	    db_error("?\n");
-	    /*NOTREACHED*/
+		db_error("?\n");
+		/*NOTREACHED*/
 	}
 
 	p = pfind((pid_t)pid);
 	if (p == NULL) {
 		db_error("no such proc\n");
-	    /*NOTREACHED*/
+		/*NOTREACHED*/
 	}
 	psignal(p, (int)sig);
 }
 
 void
-db_show_all_procs(addr, haddr, count, modif)
-	db_expr_t addr;
-	int haddr;
-	db_expr_t count;
-	char *modif;
+db_show_all_procs(db_expr_t addr, int haddr, db_expr_t count, char *modif)
 {
 	int i;
 	char *mode;
 	struct proc *p, *pp;
 	struct timeval tv[2];
 	const struct proclist_desc *pd;
-    
+
 	if (modif[0] == 0)
 		modif[0] = 'n';			/* default == normal mode */
 
@@ -127,9 +119,8 @@ db_show_all_procs(addr, haddr, count, modif)
 		db_printf("\t/w == show process wait/emul info\n");
 		return;
 	}
-	
-	switch (*mode) {
 
+	switch (*mode) {
 	case 'a':
 		db_printf(" PID       %10s %18s %18s %18s\n",
 		    "COMMAND", "STRUCT PROC *", "UAREA *", "VMSPACE/VM_MAP");
@@ -194,14 +185,8 @@ db_show_all_procs(addr, haddr, count, modif)
 }
 
 void
-db_show_callout(addr, haddr, count, modif)
-	db_expr_t addr; 
-	int haddr; 
-	db_expr_t count;
-	char *modif;
+db_show_callout(db_expr_t addr, int haddr, db_expr_t count, char *modif)
 {
-	extern struct callout_queue *callwheel;
-	extern int callwheelsize;
 	uint64_t hint;
 	int i;
 
@@ -231,11 +216,7 @@ db_show_callout(addr, haddr, count, modif)
 }
 
 void
-db_dmesg(addr, haddr, count, modif)
-	db_expr_t addr; 
-	int haddr; 
-	db_expr_t count;
-	char *modif;
+db_dmesg(db_expr_t addr, int haddr, db_expr_t count, char *modif)
 {
 	struct kern_msgbuf *mbp;
 	int ch, newl, skip, i;
