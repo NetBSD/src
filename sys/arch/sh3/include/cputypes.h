c6fdda013726a66e929225b0c96cf8832de70fda
uch <uch@NetBSD.org>
2002-02-17 20:59:33 +0000

    To determine CPU architecture and product, add cpu_arch and cpu_prduct variables. these are initialized by sh_cpu_init()@sh3/sh3_machdep.c (see sh3/include/cputypes.h for detail.) add "options SH3" for SH3 kernel config file.

--- sys/arch/sh3/include/cputypes.h
+++ sys/arch/sh3/include/cputypes.h
@@ -1,7 +1,7 @@
-/*	$NetBSD: cputypes.h,v 1.1 1999/09/13 10:31:16 itojun Exp $	*/
+/*	$NetBSD: cputypes.h,v 1.2 2002/02/17 20:59:33 uch Exp $	*/
 
-/*
- * Copyright (c) 1993 Christopher G. Demetriou
+/*-
+ * Copyright (c) 2002 The NetBSD Foundation, Inc.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -12,49 +12,60 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. The name of the author may not be used to endorse or promote products
- *    derived from this software without specific prior written permission
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *        This product includes software developed by the NetBSD
+ *        Foundation, Inc. and its contributors.
+ * 4. Neither the name of The NetBSD Foundation nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
  *
- * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
- * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
- * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
- * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
- * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
- * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
  */
 
-/*
- * Classes of Processor. CPU identification code depends on
- * this starting at 0, and having an increment of one.
- */
+#ifndef _SH3_CPUTYPES_H_
+#define _SH3_CPUTYPES_H_
+#ifdef _KERNEL
 
-#define CPUCLASS_SH3	0
+#define CPU_ARCH_SH3		3
+#define CPU_ARCH_SH4		4
 
-/*
- * Kinds of Processor. Only the first 1 are used, as they are processors
- * that might not have a cpuid instruction.
- */
+#define CPU_PRODUCT_UNKNOWN	0
+/* SH3 series */
+#define CPU_PRODUCT_7708	1
+#define CPU_PRODUCT_7708S	2
+#define CPU_PRODUCT_7708R	3
+#define CPU_PRODUCT_7709	4
+#define CPU_PRODUCT_7709A	5
+/* SH4 series */
+#define CPU_PRODUCT_7750	6
+#define CPU_PRODUCT_7750S	7
 
-#define CPU_SH3		0	/* Hitachi SH3 */
-
-/*
- * CPU vendors
- */
-
-#define CPUVENDOR_UNKNOWN	-1
-#define CPUVENDOR_HITACHI		0
-
-/*
- * Some other defines, dealing with values returned by cpuid.
- */
-
-#if 0
-#define CPU_MAXMODEL	15	/* Models within family range 0-15 */
-#define CPU_DEFMODEL	16	/* Value for unknown model -> default  */
-#define CPU_MINFAMILY	 4	/* Lowest that cpuid can return (486) */
-#define CPU_MAXFAMILY	 6	/* Highest we know (686) */
+#ifndef _LOCORE
+extern int cpu_arch;
+extern int cpu_product;
+#if defined(SH3) && defined(SH4)
+#define CPU_IS_SH3		(cpu_arch == CPU_ARCH_SH3)
+#define CPU_IS_SH4		(cpu_arch == CPU_ARCH_SH4)
+#elif defined(SH3)
+#define CPU_IS_SH3		1
+#define CPU_IS_SH4		0
+#elif defined(SH4)
+#define CPU_IS_SH3		0
+#define CPU_IS_SH4		1
+#else
+#error "define SH3 and/or SH4"
 #endif
+#endif /* !_LOCORE */
+#endif /* _KERNEL */
+#endif /* !_SH3_CPUTYPES_H_ */
