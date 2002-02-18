554b9e45517056f635c35cf991a5915ee8e5d771
bjh21 <bjh21@NetBSD.org>
2002-02-18 11:40:42 +0000

    Change signature of dma_intr() to match other interrupt handlers.

--- sys/arch/arm/iomd/iomd_dma.c
+++ sys/arch/arm/iomd/iomd_dma.c
@@ -1,4 +1,4 @@
-/* 	$NetBSD: iomd_dma.c,v 1.3 2002/01/25 19:19:26 thorpej Exp $	*/
+/* 	$NetBSD: iomd_dma.c,v 1.4 2002/02/18 11:40:42 bjh21 Exp $	*/
 
 /*
  * Copyright (c) 1995 Scott Stevens
@@ -139,9 +139,10 @@ dma_isintr(dp)
 }
 
 int
-dma_intr(dp)
-	struct dma_ctrl *dp;
+dma_intr(cookie)
+	void *cookie;
 {
+	struct dma_ctrl *dp = cookie;
 	u_char status = (*dp->dc_st) & DMA_ST_MASK;
 	vm_offset_t cur;
 	int len;
