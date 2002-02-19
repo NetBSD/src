dd58383a324459e91aa3d5772048fc646f1429fd
simonb <simonb@NetBSD.org>
2002-02-19 13:08:12 +0000

    Clean up some rampant code duplication wrt ieee number handling:  - Add alignment-safe double and float unions.  - Use the above for the __infinity and __nan constants on all    architectures that use the standard ieee754 representation of    those constants.  - Add a single copy of various ieee754 math functions (frexp, isinf,    isnan, ldexp and modf) that had numerous duplicates among the    arch-specific directories.  - Use the above functions on all architectures where the generic C    versions where used.  Architectures that had local assembly    routines are untouched (for those functions only).

--- sys/arch/m68k/include/math.h
+++ sys/arch/m68k/include/math.h
@@ -1,11 +1,3 @@
-/*	$NetBSD: math.h,v 1.3 2000/02/05 14:04:38 kleink Exp $	*/
+/*	$NetBSD: math.h,v 1.4 2002/02/19 13:08:14 simonb Exp $	*/
 
-/*
- * ISO C99
- */
-#if !defined(_ANSI_SOURCE) && \
-    (!defined(_POSIX_C_SOURCE) && !defined(_XOPEN_SOURCE) || \
-     defined(_ISOC99_SOURCE) || (__STDC_VERSION__ - 0) >= 199901L)
-extern __const char	__nanf[];
-#define	NAN		(*(__const float *)(__const void *)__nanf)
-#endif
+#define	__HAVE_NANF
