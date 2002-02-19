bb6c41e75194f08e506fbb59fdce9757874aea44
thorpej <thorpej@NetBSD.org>
2002-02-19 21:50:01 +0000

    Adjust for changes to __infinity decl.

--- lib/libc/arch/vax/gen/infinity.c
+++ lib/libc/arch/vax/gen/infinity.c
@@ -1,8 +1,8 @@
-/*	$NetBSD: infinity.c,v 1.7 2000/09/13 22:32:27 msaitoh Exp $	*/
+/*	$NetBSD: infinity.c,v 1.8 2002/02/19 21:50:01 thorpej Exp $	*/
 
 #include <sys/cdefs.h>
 #if defined(LIBC_SCCS) && !defined(lint)
-__RCSID("$NetBSD: infinity.c,v 1.7 2000/09/13 22:32:27 msaitoh Exp $");
+__RCSID("$NetBSD: infinity.c,v 1.8 2002/02/19 21:50:01 thorpej Exp $");
 #endif /* LIBC_SCCS and not lint */
 /*
  * XXX - This is not correct, but what can we do about it???
@@ -13,5 +13,5 @@ __RCSID("$NetBSD: infinity.c,v 1.7 2000/09/13 22:32:27 msaitoh Exp $");
 #include <math.h>
 
 /* The highest D float on a vax. */
-const char __infinity[] = { (char)0xff, (char)0x7f, (char)0xff, (char)0xff,
-	(char)0xff, (char)0xff, (char)0xff, (char)0xff };
+const union __double_u __infinity =
+	{ { 0xff, 0x7f, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff } };
