1411311505ec9e1907881f2e78256d963f8c3b50
agc <agc@NetBSD.org>
2002-02-22 10:10:38 +0000

    Bump version date to reflect this change to create/pl.c:
    > revision 1.22
    > date: 2002/02/18 00:57:54;  author: hubertf;  state: Exp;  lines: +10 -3
    > make @cwd work a bit more reliable:
    >
    > When -L is used (as it is by pkgsrc), don't override the directory
    > set by @cwd unconditionally with what was given to -L (srcdir).
    > Instead, use the @cwd directory.

--- usr.sbin/pkg_install/lib/version.h
+++ usr.sbin/pkg_install/lib/version.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: version.h,v 1.8 2002/01/09 18:50:17 agc Exp $	*/
+/*	$NetBSD: version.h,v 1.9 2002/02/22 10:10:38 agc Exp $	*/
 
 /*
  * Copyright (c) 2001 Thomas Klausner.  All rights reserved.
@@ -33,6 +33,6 @@
 #ifndef _INST_LIB_VERSION_H_
 #define _INST_LIB_VERSION_H_
 
-#define PKGTOOLS_VERSION "20020108"
+#define PKGTOOLS_VERSION "20020118"
 
 #endif /* _INST_LIB_VERSION_H_ */
