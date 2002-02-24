3a374c42ab978e233ee979f37c9e8f6ade4e8a52
ad <ad>
2002-02-24 00:39:36 +0000

    Reserve majors 101 and 102 for wsmux and wsfont.

--- sys/arch/pmax/pmax/conf.c
+++ sys/arch/pmax/pmax/conf.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: conf.c,v 1.43 2002/01/12 14:59:52 manu Exp $	*/
+/*	$NetBSD: conf.c,v 1.44 2002/02/24 00:39:36 ad Exp $	*/
 
 /*
  * Copyright (c) 1992, 1993
@@ -278,7 +278,9 @@ struct cdevsw	cdevsw[] =
 	cdev_disk_init(NMD,md),	/* 97: memory disk  driver */
 	cdev_fbm_init(NPX,px),	/* 98: PixelStamp board driver */
 	cdev_audio_init(NAUDIO,audio),  /* 99: generic audio I/O */
-	cdev_clockctl_init(NCLOCKCTL, clockctl),/* 100 clockctl pseudo device */
+	cdev_clockctl_init(NCLOCKCTL, clockctl),/* 100: clockctl pseudo device */
+	cdev_notdef(),		/* 101: reserved for wsmux */
+	cdev_notdef(),		/* 102: reserved for wsfont */
 };
 int	nchrdev = sizeof(cdevsw) / sizeof(cdevsw[0]);
 
@@ -418,6 +420,8 @@ static int chrtoblktbl[] =  {
 	/* 98 */	NODEV,
 	/* 99 */	NODEV,
 	/* 100 */	NODEV,
+	/* 101 */	NODEV,
+	/* 102 */	NODEV,
 };
 
 /*
