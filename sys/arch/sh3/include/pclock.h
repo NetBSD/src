72589177e0d14bc8546003f35f0b79a227a7589a
uch <uch@NetBSD.org>
2002-02-24 18:13:06 +0000

    remove obsolete file.

--- sys/arch/sh3/include/pclock.h
+++
@@ -1,8 +0,0 @@
-/*	$NetBSD: pclock.h,v 1.1 2002/02/01 17:52:56 uch Exp $	*/
-
-#include "opt_pclock.h"
-
-extern int sh3_pclock;	/* defined in sh3/sh3_machdep.c */
-#ifndef PCLOCK
-#define PCLOCK sh3_pclock
-#endif
