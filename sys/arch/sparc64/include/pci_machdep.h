1eb34844258ac1d71c90d86929359ea622a66386
eeh <eeh@NetBSD.org>
2002-02-26 22:39:14 +0000

    Fix PR# port-sparc64/13811: PCITAG_SETNODE() is defined using `x' insted of `t'. from URA Hiroshi

--- sys/arch/sparc64/include/pci_machdep.h
+++ sys/arch/sparc64/include/pci_machdep.h
@@ -1,4 +1,4 @@
-/* $NetBSD: pci_machdep.h,v 1.8 2001/10/18 23:47:16 eeh Exp $ */
+/* $NetBSD: pci_machdep.h,v 1.9 2002/02/26 22:39:14 eeh Exp $ */
 
 /*
  * Copyright (c) 1999 Matthew R. Green
@@ -67,7 +67,7 @@ struct sparc_pci_chipset {
 #define	PCITAG_NODE(x)		(int)(((x)>>32)&0xffffffff)
 #define	PCITAG_OFFSET(x)	((x)&0xffffffff)
 #define	PCITAG_CREATE(n,b,d,f)	(((u_int64_t)(n)<<32)|((b)<<16)|((d)<<11)|((f)<<8))
-#define	PCITAG_SETNODE(t,n)	((x)&0xffffffff)|(((n)<<32)
+#define	PCITAG_SETNODE(t,n)	((t)&0xffffffff)|(((n)<<32)
 typedef u_int64_t pcitag_t; 
 
 
