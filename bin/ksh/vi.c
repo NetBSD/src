ed63958a64ee7396ec67701b7c1ba1d79625d26e
he <he>
2002-02-26 19:51:55 +0000

    Pull up revision 1.6 (requested by dogcow):   Synchronize with current development version:    o fix problem with ``set -x'' causing a core dump; fixes PR#11361    o fix problem with ``set -e'' causing early exit; fixes PR#11542    o fix various misspellings and nested extern declarations

--- bin/ksh/vi.c
+++ bin/ksh/vi.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: vi.c,v 1.5 1999/11/02 22:06:45 jdolecek Exp $	*/
+/*	$NetBSD: vi.c,v 1.5.6.1 2002/02/26 19:51:56 he Exp $	*/
 
 /*
  *	vi command editing
@@ -2146,7 +2146,7 @@ char_len(c)
 	return len;
 }
 
-/* Similar to x_zotc(emacs.c), but no tab wierdness */
+/* Similar to x_zotc(emacs.c), but no tab weirdness */
 static void
 x_vi_zotc(c)
 	int c;
