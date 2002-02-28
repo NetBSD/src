bf88f892a51cba62f5ee4f12773433d9a6ef43b7
simonb <simonb@NetBSD.org>
2002-02-28 03:17:27 +0000

    Use "#define<tab>".

---
+++ sys/arch/dreamcast/include/types.h
@@ -0,0 +1,7 @@
+/*	$NetBSD: types.h,v 1.2.12.2 2002/02/28 03:17:27 simonb Exp $	*/
+#ifndef _DREAMCAST_TYPES_H_
+#define	_DREAMCAST_TYPES_H_
+
+#include <sh3/types.h>
+
+#endif /* _DREAMCAST_TYPES_H_ */
---
+++ sys/arch/hpcsh/include/types.h
@@ -0,0 +1,7 @@
+/*	$NetBSD: types.h,v 1.3.12.2 2002/02/28 03:17:29 simonb Exp $	*/
+#ifndef _HPCSH_TYPES_H_
+#define	_HPCSH_TYPES_H_
+
+#include <sh3/types.h>
+
+#endif /* _HPCSH_TYPES_H_ */
