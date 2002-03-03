cb9975e98d71390c2f6a9aa9ad120740e3565031
uch <uch>
2002-03-03 14:28:48 +0000

    machine dependent sysctl are moved to machine/cpu.h 	dreamcast, hpcsh ... CPU_CONSDEV 	mmeye, evbsh3	 ... CPU_CONSDEV, CPU_LOADANDRESET

--- sys/arch/dreamcast/include/cpu.h
+++ sys/arch/dreamcast/include/cpu.h
@@ -1,7 +1,18 @@
-/*	$NetBSD: cpu.h,v 1.1 2000/12/11 18:19:14 marcus Exp $	*/
+/*	$NetBSD: cpu.h,v 1.2 2002/03/03 14:28:49 uch Exp $	*/
 #ifndef _DREAMCAST_CPU_H_
 #define _DREAMCAST_CPU_H_
 
 #include <sh3/cpu.h>
+
+/*
+ * CTL_MACHDEP definitions.
+ */
+#define	CPU_CONSDEV		1	/* dev_t: console terminal device */
+#define	CPU_MAXID		2	/* number of valid machdep ids */
+
+#define	CTL_MACHDEP_NAMES { \
+	{ 0, 0 }, \
+	{ "console_device", CTLTYPE_STRUCT }, \
+}
 
 #endif /* _DREAMCAST_CPU_H_ */
