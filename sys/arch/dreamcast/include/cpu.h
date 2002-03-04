2f603eaa17b654075031942e89389879774b5cd0
uch <uch@NetBSD.org>
2002-03-04 14:36:13 +0000

    CTL_MACHDEP definitions are integrated into sh3/include/cpu.h again.

--- sys/arch/dreamcast/include/cpu.h
+++ sys/arch/dreamcast/include/cpu.h
@@ -1,18 +1,3 @@
-/*	$NetBSD: cpu.h,v 1.2 2002/03/03 14:28:49 uch Exp $	*/
-#ifndef _DREAMCAST_CPU_H_
-#define _DREAMCAST_CPU_H_
+/*	$NetBSD: cpu.h,v 1.3 2002/03/04 14:36:13 uch Exp $	*/
 
 #include <sh3/cpu.h>
-
-/*
- * CTL_MACHDEP definitions.
- */
-#define	CPU_CONSDEV		1	/* dev_t: console terminal device */
-#define	CPU_MAXID		2	/* number of valid machdep ids */
-
-#define	CTL_MACHDEP_NAMES { \
-	{ 0, 0 }, \
-	{ "console_device", CTLTYPE_STRUCT }, \
-}
-
-#endif /* _DREAMCAST_CPU_H_ */
