26c0fb5b4f26f0e9b4fd994850858698ab798298
simonb <simonb@NetBSD.org>
2002-03-05 09:40:38 +0000

    Don't cast argument to ffs() to long. Per discussion on port-alpha, noticed by Robert Elz.

--- sys/arch/pmax/pmax/disksubr.c
+++ sys/arch/pmax/pmax/disksubr.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: disksubr.c,v 1.35 2002/02/19 17:09:47 wiz Exp $	*/
+/*	$NetBSD: disksubr.c,v 1.36 2002/03/05 09:40:41 simonb Exp $	*/
 
 /*
  * Copyright (c) 1982, 1986, 1988 Regents of the University of California.
@@ -218,7 +218,7 @@ setdisklabel(olp, nlp, openmask, osdep)
 	if (nlp->d_magic != DISKMAGIC || nlp->d_magic2 != DISKMAGIC ||
 	    dkcksum(nlp) != 0)
 		return (EINVAL);
-	while ((i = ffs((long)openmask)) != 0) {
+	while ((i = ffs(openmask)) != 0) {
 		i--;
 		openmask &= ~(1 << i);
 		if (nlp->d_npartitions <= i)
