ddefa423a71f847fdb97ddeb8c24480217531216
simonb <simonb@NetBSD.org>
2002-03-05 16:12:35 +0000

    Provide a L2 cache configuration function.

--- sys/arch/newsmips/include/types.h
+++ sys/arch/newsmips/include/types.h
@@ -1,5 +1,7 @@
-/*	$NetBSD: types.h,v 1.3 2002/02/28 03:17:29 simonb Exp $	*/
+/*	$NetBSD: types.h,v 1.4 2002/03/05 16:13:57 simonb Exp $	*/
 
 #include <mips/types.h>
 
 #define	__BROKEN_CONFIG_UNIT_USAGE
+
+#define	__HAVE_MIPS_MACHDEP_CACHE_CONFIG
--- sys/arch/pmax/include/types.h
+++ sys/arch/pmax/include/types.h
@@ -1,6 +1,8 @@
-/*	$NetBSD: types.h,v 1.19 2002/02/28 03:17:30 simonb Exp $	*/
+/*	$NetBSD: types.h,v 1.20 2002/03/05 16:14:28 simonb Exp $	*/
 
 #include <mips/types.h>
 
 #define	__HAVE_DEVICE_REGISTER
 #define	__HAVE_GENERIC_SOFT_INTERRUPTS
+
+#define	__HAVE_MIPS_MACHDEP_CACHE_CONFIG
