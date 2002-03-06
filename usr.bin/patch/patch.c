9329113792fb1e806a8695630b4757b9ccd8e014
ragge <ragge@NetBSD.org>
2002-03-06 12:01:03 +0000

    License change; patch.c now has a BSD license. From Larry Wall via Niklas Hallqvist, <niklas@appli.se>.

--- usr.bin/patch/patch.c
+++ usr.bin/patch/patch.c
@@ -1,16 +1,31 @@
-/*	$NetBSD: patch.c,v 1.8 2000/10/16 07:05:04 kleink Exp $	*/
+/*	$NetBSD: patch.c,v 1.9 2002/03/06 12:01:04 ragge Exp $	*/
 
 /* patch - a program to apply diffs to original files
  *
  * Copyright 1986, Larry Wall
  *
- * This program may be copied as long as you don't try to make any
- * money off of it, or pretend that you wrote it.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following condition
+ * is met:
+ *  1. Redistributions of source code must retain the above copyright
+ *     notice, this condition and the following disclaimer.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
  */
 
 #include <sys/cdefs.h>
 #ifndef lint
-__RCSID("$NetBSD: patch.c,v 1.8 2000/10/16 07:05:04 kleink Exp $");
+__RCSID("$NetBSD: patch.c,v 1.9 2002/03/06 12:01:04 ragge Exp $");
 #endif /* not lint */
 
 #include "INTERN.h"
