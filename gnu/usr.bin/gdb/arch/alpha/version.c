e44bd33760e3e04597223d7e57717dfcd4d87037
jdolecek <jdolecek@NetBSD.org>
2002-03-06 19:17:09 +0000

    version[] 5.0-nb1 --> 5.0nb1, as other archs use

--- gnu/usr.bin/gdb/arch/alpha/version.c
+++ gnu/usr.bin/gdb/arch/alpha/version.c
@@ -2,6 +2,6 @@
 /* Generated from: 	NetBSD: toolchain2netbsd,v 1.13 2001/12/05 22:37:40 rafal Exp  */
 
 #include "version.h"
-const char version[] = "5.0-nb1";
+const char version[] = "5.0nb1";
 const char host_name[] = "alpha-unknown-netbsd";
 const char target_name[] = "alpha-unknown-netbsd";
