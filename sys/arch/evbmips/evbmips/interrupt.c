f610e064a8fad2b5afa7948111439bad017ba76c
simonb <simonb>
2002-03-07 14:43:56 +0000

    A port to the MIPS Malta evaluation board.  Currently supports the MIPS32 4Kc CPU board, with support for the MIPS64 5Kc and the QED RM5261 CPU boards to follow.
    The cs4281 audio hasn't been tested, there are some interrupt problems
    with onboard the pciide, but all other on-board peripherals work.
    
    The evbmips port will support more MIPS evaluation boards in the future.

---
+++ sys/arch/evbmips/evbmips/interrupt.c
@@ -0,0 +1,218 @@
+/*	$NetBSD: interrupt.c,v 1.1 2002/03/07 14:43:58 simonb Exp $	*/
+
+/*-
+ * Copyright (c) 2001 The NetBSD Foundation, Inc.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to The NetBSD Foundation
+ * by Jason R. Thorpe.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed by the NetBSD
+ *	Foundation, Inc. and its contributors.
+ * 4. Neither the name of The NetBSD Foundation nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <sys/param.h>
+#include <sys/malloc.h>
+#include <sys/device.h>
+
+#include <uvm/uvm_extern.h>
+
+#include <machine/intr.h>
+#include <machine/locore.h>
+
+struct evbmips_soft_intrhand *softnet_intrhand;
+
+/*
+ * This is a mask of bits to clear in the SR when we go to a
+ * given software interrupt priority level.
+ * Hardware ipls are port/board specific.
+ */
+
+const u_int32_t ipl_si_to_sr[_IPL_NSOFT] = {
+	MIPS_SOFT_INT_MASK_0,			/* IPL_SOFT */
+	MIPS_SOFT_INT_MASK_0,			/* IPL_SOFTCLOCK */
+	MIPS_SOFT_INT_MASK_1,			/* IPL_SOFTNET */
+	MIPS_SOFT_INT_MASK_1,			/* IPL_SOFTSERIAL */
+};
+
+struct evbmips_soft_intr evbmips_soft_intrs[_IPL_NSOFT];
+
+struct evcnt mips_int5_evcnt =
+    EVCNT_INITIALIZER(EVCNT_TYPE_INTR, NULL, "mips", "int 5 (clock)");
+
+void
+intr_init(void)
+{
+
+	evbmips_intr_init();	/* board specific stuff */
+
+	softintr_init();
+}
+
+void
+cpu_intr(u_int32_t status, u_int32_t cause, u_int32_t pc, u_int32_t ipending)
+{
+	struct clockframe cf;
+	struct evbmips_soft_intr *si;
+	struct evbmips_soft_intrhand *sih;
+	int i, s;
+
+	uvmexp.intrs++;
+
+	if (ipending & MIPS_INT_MASK_5) {
+		u_int32_t cycles = mips3_cp0_count_read();
+		mips3_cp0_compare_write(cycles + curcpu()->ci_cycles_per_hz);
+
+		cf.pc = pc;
+		cf.sr = status;
+		hardclock(&cf);
+
+		mips_int5_evcnt.ev_count++;
+
+		/* Re-enable clock interrupts. */
+		cause &= ~MIPS_INT_MASK_5;
+		_splset(MIPS_SR_INT_IE |
+		    ((status & ~cause) & MIPS_HARD_INT_MASK));
+	}
+
+	if (ipending & (MIPS_INT_MASK_0|MIPS_INT_MASK_1|MIPS_INT_MASK_2|
+			MIPS_INT_MASK_3|MIPS_INT_MASK_4)) {
+		/* Process I/O and error interrupts. */
+		evbmips_iointr(status, cause, pc, ipending);
+	}
+
+	ipending &= (MIPS_SOFT_INT_MASK_1|MIPS_SOFT_INT_MASK_0);
+	if (ipending == 0)
+		return;
+
+	_clrsoftintr(ipending);
+
+	for (i = _IPL_NSOFT - 1; i >= 0; i--) {
+		if ((ipending & ipl_si_to_sr[i]) == 0)
+			continue;
+
+		si = &evbmips_soft_intrs[i];
+
+		if (TAILQ_FIRST(&si->softintr_q) != NULL)
+			si->softintr_evcnt.ev_count++;
+
+		for (;;) {
+			s = splhigh();
+
+			sih = TAILQ_FIRST(&si->softintr_q);
+			if (sih != NULL) {
+				TAILQ_REMOVE(&si->softintr_q, sih, sih_q);
+				sih->sih_pending = 0;
+			}
+
+			splx(s);
+
+			if (sih == NULL)
+				break;
+
+			uvmexp.softs++;
+			(*sih->sih_fn)(sih->sih_arg);
+		}
+	}
+}
+
+/*
+ * softintr_init:
+ *
+ *	Initialize the software interrupt system.
+ */
+void
+softintr_init(void)
+{
+	static const char *softintr_names[] = IPL_SOFTNAMES;
+	struct evbmips_soft_intr *si;
+	int i;
+
+	for (i = 0; i < _IPL_NSOFT; i++) {
+		si = &evbmips_soft_intrs[i];
+		TAILQ_INIT(&si->softintr_q);
+		si->softintr_ipl = IPL_SOFT + i;
+		evcnt_attach_dynamic(&si->softintr_evcnt, EVCNT_TYPE_INTR,
+		    NULL, "soft", softintr_names[i]);
+	}
+
+	/* XXX Establish legacy soft interrupt handlers. */
+	softnet_intrhand = softintr_establish(IPL_SOFTNET,
+	    (void (*)(void *))netintr, NULL);
+
+	assert(softnet_intrhand != NULL);
+}
+
+/*
+ * softintr_establish:		[interface]
+ *
+ *	Register a software interrupt handler.
+ */
+void *
+softintr_establish(int ipl, void (*func)(void *), void *arg)
+{
+	struct evbmips_soft_intr *si;
+	struct evbmips_soft_intrhand *sih;
+
+	if (__predict_false(ipl >= (IPL_SOFT + _IPL_NSOFT) ||
+			    ipl < IPL_SOFT))
+		panic("softintr_establish");
+
+	si = &evbmips_soft_intrs[ipl - IPL_SOFT];
+
+	sih = malloc(sizeof(*sih), M_DEVBUF, M_NOWAIT);
+	if (__predict_true(sih != NULL)) {
+		sih->sih_intrhead = si;
+		sih->sih_fn = func;
+		sih->sih_arg = arg;
+		sih->sih_pending = 0;
+	}
+	return (sih);
+}
+
+/*
+ * softintr_disestablish:	[interface]
+ *
+ *	Unregister a software interrupt handler.
+ */
+void
+softintr_disestablish(void *arg)
+{
+	struct evbmips_soft_intrhand *sih = arg;
+	struct evbmips_soft_intr *si = sih->sih_intrhead;
+	int s;
+
+	s = splhigh();
+	if (sih->sih_pending) {
+		TAILQ_REMOVE(&si->softintr_q, sih, sih_q);
+		sih->sih_pending = 0;
+	}
+	splx(s);
+
+	free(sih, M_DEVBUF);
+}
---
+++ sys/arch/evbmips/evbmips/yamon.c
@@ -0,0 +1,119 @@
+/*	$NetBSD: yamon.c,v 1.1 2002/03/07 14:43:59 simonb Exp $	*/
+
+/*
+ * Copyright 2002 Wasabi Systems, Inc.
+ * All rights reserved.
+ *
+ * Written by Simon Burge for Wasabi Systems, Inc.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *      This product includes software developed for the NetBSD Project by
+ *      Wasabi Systems, Inc.
+ * 4. The name of Wasabi Systems, Inc. may not be used to endorse
+ *    or promote products derived from this software without specific prior
+ *    written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY WASABI SYSTEMS, INC. ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL WASABI SYSTEMS, INC
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/* XXX move to arch/mips/yamon/yamon.c or similar? */
+
+#include <sys/param.h>
+#include <sys/systm.h>
+#include <sys/device.h>
+
+#include <dev/cons.h>
+
+#include <machine/yamon.h>
+
+static int  yamongetc(dev_t);
+static void yamonputc(dev_t, int);
+
+/*
+ * Default consdev, for errors or warnings before
+ * consinit runs: use the PROM.
+ */
+struct consdev yamon_promcd = {
+	NULL,		/* probe */
+	NULL,		/* init */
+	yamongetc,	/* getc */
+	yamonputc,	/* putc */
+	nullcnpollc,	/* pollc */
+	NULL,		/* bell */
+	makedev(0, 0),
+	CN_DEAD,
+};
+/*
+ * Get character from PROM console.
+ */
+static int
+yamongetc(dev)
+	dev_t dev;
+{
+	char chr;
+
+	while (!YAMON_GETCHAR(&chr))
+		/* nothing */;
+	return chr;
+}
+
+/*
+ * Print a character on PROM console.
+ */
+static void
+yamonputc(dev_t dev, int c)
+{
+	char chr;
+
+	chr = c;
+	YAMON_PRINT_COUNT(&chr, 1);
+}
+
+char *
+yamon_getenv(char *name)
+{
+	yamon_env_var *yev = yamon_envp;
+
+	if (yev == NULL)
+		return (NULL);
+	while (yev->name != NULL) {
+		if (strcmp(yev->name, name) == 0)
+			return (yev->val);
+		yev++;
+	}
+
+	return (NULL);
+}
+
+void
+yamon_print(char *str)
+{
+
+	YAMON_PRINT(str);
+}
+
+void
+yamon_exit(uint32_t rc)
+{
+
+	YAMON_EXIT(rc);
+}
