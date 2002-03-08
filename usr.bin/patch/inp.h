8a18114acad28ace9083862dc003b6a315212b17
kristerw <kristerw>
2002-03-08 21:57:33 +0000

    Ansify Remove 'register' Make local functions static. Remove most '#ifndef lint'

--- usr.bin/patch/inp.h
+++ usr.bin/patch/inp.h
@@ -1,11 +1,11 @@
-/*	$NetBSD: inp.h,v 1.4 1998/02/22 13:33:49 christos Exp $	*/
+/*	$NetBSD: inp.h,v 1.5 2002/03/08 21:57:33 kristerw Exp $	*/
 
 EXT LINENUM input_lines INIT(0);	/* how long is input file in lines */
 EXT LINENUM last_frozen_line INIT(0);	/* how many input lines have been */
 					/* irretractibly output */
-void re_input __P((void));
-void scan_input __P((char *));
-bool plan_a __P((char *));
-void plan_b __P((char *));
-char *ifetch __P((Reg1 LINENUM, int));
-bool rev_in_string __P((char *));
+void re_input(void);
+void scan_input(char *);
+bool plan_a(char *);
+void plan_b(char *);
+char *ifetch(LINENUM, int);
+bool rev_in_string(char *);
