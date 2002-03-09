5b938e0c1e352f5fafe3002be0ec5b96d724448c
he <he>
2002-03-09 16:47:20 +0000

    Apply patch (requested by he):   Fix off by one error in channel number check.   Here: bump the NetBSD date to mark updated version.

--- crypto/dist/ssh/version.h
+++ crypto/dist/ssh/version.h
@@ -1,8 +1,8 @@
-/*	$NetBSD: version.h,v 1.1.1.1.2.6 2001/12/11 00:04:45 he Exp $	*/
+/*	$NetBSD: version.h,v 1.1.1.1.2.7 2002/03/09 16:47:20 he Exp $	*/
 /* $OpenBSD: version.h,v 1.27 2001/12/05 15:04:48 markus Exp $ */
 
 #define __OPENSSH_VERSION	"OpenSSH_3.0.2"
-#define __NETBSDSSH_VERSION	"NetBSD_Secure_Shell-20011206"
+#define __NETBSDSSH_VERSION	"NetBSD_Secure_Shell-20020307"
 
 /*
  * it is important to retain OpenSSH version identification part, it is
