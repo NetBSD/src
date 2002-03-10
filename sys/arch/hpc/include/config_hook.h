3a5800addd523550a797033480f3fb414b0f466d
takemura <takemura@NetBSD.org>
2002-03-10 07:24:52 +0000

    Attached vrkiu at vrc4173bcu.

--- sys/arch/hpc/include/config_hook.h
+++ sys/arch/hpc/include/config_hook.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: config_hook.h,v 1.1 2002/02/01 18:16:04 uch Exp $	*/
+/*	$NetBSD: config_hook.h,v 1.2 2002/03/10 07:24:52 takemura Exp $	*/
 
 /*-
  * Copyright (c) 1999-2001
@@ -276,8 +276,10 @@ int	config_connected_call(config_call_tag, void *);
 #define PCIINTR_ID(bus, dev, func)	CONFIG_HOOK_PCIINTR_ID(bus, dev, func)
 #define PCIINTR_00_00_00	PCIINTR_ID(0,  0, 0)
 #define PCIINTR_00_01_00	PCIINTR_ID(0,  1, 0)
+#define PCIINTR_00_12_00	PCIINTR_ID(0, 12, 0)
 #define PCIINTR_00_12_02	PCIINTR_ID(0, 12, 2)
 #define PCIINTR_00_16_00	PCIINTR_ID(0, 16, 0)
+#define PCIINTR_00_19_00	PCIINTR_ID(0, 19, 0)
 #define PCIINTR_00_19_02	PCIINTR_ID(0, 19, 2)
 #endif /* CONFIG_HOOK_DEFINE_NICKNAME */
 
