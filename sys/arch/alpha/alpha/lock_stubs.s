4ccdc845e6b0074d0ca48b75ea5076cf2dc2178d
thorpej <thorpej@NetBSD.org>
2002-03-11 17:55:36 +0000

    Document a critical section in mutex_exit().  From the comment:
    THIS IS IMPORTANT WHEN WE ADD SUPPORT FOR KERNEL PREEMPTION.
    
    There is a critical section within mutex_exit(); if we are
    preempted, between checking for waiters and releasing the
    lock, then we must check for waiters again.
    
    On some architectures, we could simply disable interrupts
    (and, thus, preemption) for the short duration of this
    critical section.
    
    On others, where disabling interrupts might be too expensive,
    a restartable sequence could be used; in the interrupt handler,
    if the PC is within the critical section, then then PC should
    be reset to the beginning of the critical section so that the
    sequence will be restarted when we are resumed.  NOTE: In this
    case, it is very important that the insn that actually clears
    the lock must NEVER be executed twice.
    
    On the Alpha, it'd be nice to be able to use load-locked/store-conditional,
    but since we're storing zero, that breaks the store-conditional logic.  On
    the alpha, we'll use the restartable sequence method.

--- sys/arch/alpha/alpha/lock_stubs.s
+++ sys/arch/alpha/alpha/lock_stubs.s
@@ -1,4 +1,4 @@
-/* $NetBSD: lock_stubs.s,v 1.1.2.2 2002/03/11 17:21:03 thorpej Exp $ */
+/* $NetBSD: lock_stubs.s,v 1.1.2.3 2002/03/11 17:55:36 thorpej Exp $ */
 
 /*-
  * Copyright (c) 2002 The NetBSD Foundation, Inc.
@@ -36,7 +36,7 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
-__KERNEL_RCSID(7, "$NetBSD: lock_stubs.s,v 1.1.2.2 2002/03/11 17:21:03 thorpej Exp $")
+__KERNEL_RCSID(7, "$NetBSD: lock_stubs.s,v 1.1.2.3 2002/03/11 17:55:36 thorpej Exp $")
 
 /*
  * Assembly language lock stubs.  These handle the common ("easy")
@@ -86,17 +86,46 @@ NESTED_NOPROFILE(mutex_tryenter, 1, 0, ra, 0, 0)
 	jmp	zero, (t0)
 END(mutex_tryenter)
 
+/*
+ * THIS IS IMPORTANT WHEN WE ADD SUPPORT FOR KERNEL PREEMPTION.
+ *
+ * There is a critical section within mutex_exit(); if we are
+ * preempted, between checking for waiters and releasing the
+ * lock, then we must check for waiters again.
+ *
+ * On some architectures, we could simply disable interrupts
+ * (and, thus, preemption) for the short duration of this
+ * critical section.
+ *
+ * On others, where disabling interrupts might be too expensive,
+ * a restartable sequence could be used; in the interrupt handler,
+ * if the PC is within the critical section, then then PC should
+ * be reset to the beginning of the critical section so that the
+ * sequence will be restarted when we are resumed.  NOTE: In this
+ * case, it is very important that the insn that actually clears
+ * the lock must NEVER be executed twice.
+ *
+ * On the Alpha, it'd be nice to be able to use load-locked/store-conditional,
+ * but since we're storing zero, that breaks the store-conditional logic.  On
+ * the alpha, we'll use the restartable sequence method.
+ */
 NESTED_NOPROFILE(mutex_exit, 1, 0, ra, 0, 0)
 	LDGP(pv)
 
 	GET_CURPROC
 	ldq	t0, 0(v0)		/* current thread */
+
+	/* BEGIN CRITICAL SECTION */
+EXPORT(mutex_exit_critical_start)
 	ldq	t1, 0(a0)		/* load lock contents */
 	cmpeq	t0, t1, t0		/* are they the same? */
 	bfalse	t0, 1f			/* no, hard case */
 
 	mb
+EXPORT(mutex_exit_critical_end)
 	stq	zero, 0(a0)		/* release lock */
+	/* END CRITICAL SECTION */
+
 	RET
 
 	/* Do a tail-call to mutex_vector_exit() */
