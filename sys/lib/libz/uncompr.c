ce03556019fd78c86374c6ff819db0adb4633a72
fvdl <fvdl>
2002-03-12 00:42:23 +0000

    Sync with userland (1.1.4)

--- sys/lib/libz/uncompr.c
+++ sys/lib/libz/uncompr.c
@@ -1,11 +1,11 @@
-/* $NetBSD: uncompr.c,v 1.1 1999/04/15 00:48:03 simonb Exp $ */
+/* $NetBSD: uncompr.c,v 1.2 2002/03/12 00:42:24 fvdl Exp $ */
 
 /* uncompr.c -- decompress a memory buffer
- * Copyright (C) 1995-1998 Jean-loup Gailly.
+ * Copyright (C) 1995-2002 Jean-loup Gailly.
  * For conditions of distribution and use, see copyright notice in zlib.h 
  */
 
-/* @(#) Id */
+/* @(#) $Id: uncompr.c,v 1.2 2002/03/12 00:42:24 fvdl Exp $ */
 
 #include "zlib.h"
 
