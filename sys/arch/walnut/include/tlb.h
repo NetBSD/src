819aa3bfcca3bbe0b437a0750877b16adf772a49
eeh <eeh@NetBSD.org>
2002-03-13 19:26:07 +0000

    Add a ppc4xx_tlbflags macro.

--- sys/arch/walnut/include/tlb.h
+++ sys/arch/walnut/include/tlb.h
@@ -1,3 +1,6 @@
-/*	$NetBSD: tlb.h,v 1.1 2001/06/13 06:01:59 simonb Exp $	*/
+/*	$NetBSD: tlb.h,v 1.2 2002/03/13 19:26:07 eeh Exp $	*/
 
 #include <powerpc/ibm4xx/tlb.h>
+
+#define	ppc4xx_tlbflags(va, pa)        (0)
+
