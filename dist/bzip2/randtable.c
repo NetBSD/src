52b993000bbb00bed89ae57e4b24cc9dfcc3190f
mjl <mjl@NetBSD.org>
2002-03-15 01:35:17 +0000

    Import bzip2 1.0.2

--- dist/bzip2/randtable.c
+++ dist/bzip2/randtable.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: randtable.c,v 1.1.1.1 2001/06/03 13:03:06 simonb Exp $	*/
+/*	$NetBSD: randtable.c,v 1.1.1.2 2002/03/15 01:35:29 mjl Exp $	*/
 
 
 /*-------------------------------------------------------------*/
@@ -10,7 +10,7 @@
   This file is a part of bzip2 and/or libbzip2, a program and
   library for lossless, block-sorting data compression.
 
-  Copyright (C) 1996-2000 Julian R Seward.  All rights reserved.
+  Copyright (C) 1996-2002 Julian R Seward.  All rights reserved.
 
   Redistribution and use in source and binary forms, with or without
   modification, are permitted provided that the following conditions
