ff5e54e845883e7c09be7a4133e6a1a260382e75
eeh <eeh@NetBSD.org>
2002-03-20 18:54:46 +0000

    Overhaul bus space.
    bus_space_handle_t now holds an address and two ASIs, one for normal accesses
    and one for streaming accesses.  This allows to map individual handles
    different ways, so some can use MMU bypass accesses and others use virtual
    addresses.  bus_space_map() will now create handles that use bypass accesses
    unles BUS_SPACE_MAP_LINEAR is passed in.  So only pass in BUS_SPACE_MAP_LINEAR
    if you absolutely *need* to use bus_space_vaddr().  This removes at least one
    extra level of indirection and should reduce TLB misses.
    
    32-bit kernels have problems accessing 64-bit addresses, so they always use
    virtual addresses.

--- sys/arch/sparc64/dev/iommuvar.h
+++ sys/arch/sparc64/dev/iommuvar.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: iommuvar.h,v 1.10 2002/02/07 21:35:27 eeh Exp $	*/
+/*	$NetBSD: iommuvar.h,v 1.11 2002/03/20 18:54:47 eeh Exp $	*/
 
 /*
  * Copyright (c) 1999 Matthew R. Green
@@ -47,10 +47,11 @@ struct iommu_state {
 	/* Needs to be volatile or egcs optimizes away loads */
 	volatile int64_t	is_flush[2];
 
-	/* copies of our parents state, to allow us to be self contained */
+	/* copies of our piarents state, to allow us to be self contained */
 	bus_space_tag_t		is_bustag;	/* our bus tag */
-	struct iommureg		*is_iommu;	/* IOMMU registers */
-	struct iommu_strbuf	*is_sb[2];	/* streaming buffer(s) */
+	bus_space_handle_t	is_iommu;	/* IOMMU registers */
+	bus_space_handle_t	is_sb[2];	/* streaming buffer(s) */
+	int			is_sbvalid[2];
 };
 
 /* interfaces for PCI/SBUS code */
