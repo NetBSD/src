53a52c0ad8f5bd588199b6995dd42e9fff8e4cce
itojun <itojun@NetBSD.org>
2002-03-21 04:23:36 +0000

    pfkey statistics was presented in wrong direction.

--- usr.bin/netstat/ipsec.c
+++ usr.bin/netstat/ipsec.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: ipsec.c,v 1.4 2001/08/06 10:25:54 itojun Exp $	*/
+/*	$NetBSD: ipsec.c,v 1.5 2002/03/21 04:23:38 itojun Exp $	*/
 
 /*
  * Copyright (C) 1995, 1996, 1997, 1998, and 1999 WIDE Project.
@@ -68,7 +68,7 @@
 static char sccsid[] = "from: @(#)inet.c	8.4 (Berkeley) 4/20/94";
 #else
 #ifdef __NetBSD__
-__RCSID("$NetBSD: ipsec.c,v 1.4 2001/08/06 10:25:54 itojun Exp $");
+__RCSID("$NetBSD: ipsec.c,v 1.5 2002/03/21 04:23:38 itojun Exp $");
 #endif
 #endif
 #endif /* not lint */
@@ -306,9 +306,9 @@ pfkey_stats(off, name)
 #define	p(f, m) if (pfkeystat.f || sflag <= 1) \
     printf(m, (CAST)pfkeystat.f, plural(pfkeystat.f))
 
-	/* kernel -> userland */
-	p(out_total, "\t" LLU " request%s sent to userland\n");
-	p(out_bytes, "\t" LLU " byte%s sent to userland\n");
+	/* userland -> kernel */
+	p(out_total, "\t" LLU " request%s sent from userland\n");
+	p(out_bytes, "\t" LLU " byte%s sent from userland\n");
 	for (first = 1, type = 0;
 	     type < sizeof(pfkeystat.out_msgtype)/sizeof(pfkeystat.out_msgtype[0]);
 	     type++) {
@@ -331,9 +331,9 @@ pfkey_stats(off, name)
 	p(out_invsatype, "\t" LLU " message%s with invalid sa type\n");
 	p(out_invaddr, "\t" LLU " message%s with invalid address extension\n");
 
-	/* userland -> kernel */
-	p(in_total, "\t" LLU " request%s sent from userland\n");
-	p(in_bytes, "\t" LLU " byte%s sent from userland\n");
+	/* kernel -> userland */
+	p(in_total, "\t" LLU " request%s sent to userland\n");
+	p(in_bytes, "\t" LLU " byte%s sent to userland\n");
 	for (first = 1, type = 0;
 	     type < sizeof(pfkeystat.in_msgtype)/sizeof(pfkeystat.in_msgtype[0]);
 	     type++) {
