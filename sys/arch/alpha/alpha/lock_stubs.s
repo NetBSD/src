808c6246ed338887701bb37fa55b2408902c2751
thorpej <thorpej@NetBSD.org>
2002-03-22 00:18:20 +0000

    * Fix the tail-calls to the slow-path. * Handle MUTEX_DEBUG.

--- sys/arch/alpha/alpha/lock_stubs.s
+++ sys/arch/alpha/alpha/lock_stubs.s
@@ -1,4 +1,4 @@
-/* $NetBSD: lock_stubs.s,v 1.1.2.4 2002/03/12 17:17:32 thorpej Exp $ */
+/* $NetBSD: lock_stubs.s,v 1.1.2.5 2002/03/22 00:18:22 thorpej Exp $ */
 
 /*-
  * Copyright (c) 2002 The NetBSD Foundation, Inc.
@@ -36,12 +36,14 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
-__KERNEL_RCSID(7, "$NetBSD: lock_stubs.s,v 1.1.2.4 2002/03/12 17:17:32 thorpej Exp $")
+__KERNEL_RCSID(7, "$NetBSD: lock_stubs.s,v 1.1.2.5 2002/03/22 00:18:22 thorpej Exp $")
 
 /*
  * Assembly language lock stubs.  These handle the common ("easy")
  * cases for mutexes.
  */
+
+#include "opt_lockdebug.h"
 
 	.text
 inc7:	.stabs	__FILE__,132,0,0,inc7; .loc	1 __LINE__
@@ -57,22 +59,15 @@ NESTED_NOPROFILE(mutex_enter, 1, 0, ra, 0, 0)
 	beq	t0, 2f			/* failed, try again */
 
 	mb				/* succeeded */
+#if defined(MUTEX_DEBUG)
+	stq	ra, MTX_DBG_LOCKED(a0)
+#endif
 	RET
 
 2:	br	1b
 
-#if 1
-3:	lda	sp, -8(sp)
-	stq	ra, 0(sp)
-	CALL(mutex_vector_enter)
-	ldq	ra, 0(sp)
-	lda	sp, 8(sp)
-	RET
-#else
 	/* Do a tail-call to mutex_vector_enter() */
-3:	lda	t0, mutex_vector_enter
-	jmp	zero, (t0)
-#endif
+3:	jmp	zero, mutex_vector_enter
 END(mutex_enter)
 
 NESTED_NOPROFILE(mutex_tryenter, 1, 0, ra, 0, 0)
@@ -86,22 +81,15 @@ NESTED_NOPROFILE(mutex_tryenter, 1, 0, ra, 0, 0)
 	beq	t0, 2f			/* failed, try again */
 
 	mb				/* succeeded */
+#if defined(MUTEX_DEBUG)
+	stq	ra, MTX_DBG_LOCKED(a0)
+#endif
 	RET
 
 2:	br	1b
 
-#if 1
-3:	lda	sp, -8(sp)
-	stq	ra, 0(sp)
-	CALL(mutex_vector_tryenter)
-	ldq	ra, 0(sp)
-	lda	sp, 8(sp)
-	RET
-#else
 	/* Do a tail-call to mutex_vector_tryenter() */
-3:	lda	t0, mutex_vector_tryenter
-	jmp	zero, (t0)
-#endif
+3:	jmp	zero, mutex_vector_tryenter
 END(mutex_tryenter)
 
 /*
@@ -124,37 +112,28 @@ END(mutex_tryenter)
  * the lock must NEVER be executed twice.
  *
  * On the Alpha, we can use load-locked/store-conditional.
- * XXX But we don't, yet.
  */
 NESTED_NOPROFILE(mutex_exit, 1, 0, ra, 0, 0)
 	LDGP(pv)
-
+#if !defined(MUTEX_DEBUG)
 	GET_CURPROC
 	ldq	t0, 0(v0)		/* current thread */
 
 	/* BEGIN CRITICAL SECTION */
-EXPORT(mutex_exit_critical_start)
-	ldq	t1, 0(a0)		/* load lock contents */
+1:	mb
+	mov	zero, t3
+	ldq_l	t1, 0(a0)		/* load lock contents */
 	cmpeq	t0, t1, t0		/* are they the same? */
-	bfalse	t0, 1f			/* no, hard case */
+	bfalse	t0, 3f			/* no, hard case */
 
-	mb
-EXPORT(mutex_exit_critical_end)
-	stq	zero, 0(a0)		/* release lock */
+	stq_c	t3, 0(a0)		/* release lock */
+	beq	t3, 2f
 	/* END CRITICAL SECTION */
 
 	RET
 
-#if 1
-1:	lda	sp, -8(sp)
-	stq	ra, 0(sp)
-	CALL(mutex_vector_exit)
-	ldq	ra, 0(sp)
-	lda	sp, 8(sp)
-	RET
-#else
+2:	br	1b
+#endif /* ! MUTEX_DEBUG */
 	/* Do a tail-call to mutex_vector_exit() */
-1:	lda	t0, mutex_vector_exit
-	jmp	zero, (t0)
-#endif
+3:	jmp	zero, mutex_vector_exit
 END(mutex_exit)
