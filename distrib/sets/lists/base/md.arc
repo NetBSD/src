b79e39575970839b88066f7c1c18687e5897e7ec
eeh <eeh>
2002-03-22 18:24:34 +0000

    Add devprops interfaces.

--- distrib/sets/lists/base/md.arc
+++
@@ -1,2 +0,0 @@
-# $NetBSD: md.arc,v 1.2 2001/01/25 08:29:19 jwise Exp $
-./sbin/fdisk		base-sysutil-root
--- usr.sbin/pppd/pppd/patchlevel.h
+++
@@ -1,8 +0,0 @@
-/*	$NetBSD: patchlevel.h,v 1.20 2000/09/23 22:39:38 christos Exp $	*/
-
-/* Id: patchlevel.h,v 1.50 2000/08/01 01:46:24 paulus Exp  */
-#define	PATCHLEVEL	0
-
-#define VERSION		"2.4"
-#define IMPLEMENTATION	""
-#define DATE		"1 August 2000"
