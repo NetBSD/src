28a056509acd3fab8655d3d1df6eb38eeaa1bc15
scw <scw>
2002-03-24 22:04:04 +0000

    sysinst for mvme68k, based mostly on hp300's sysinst. XXX: French language support needs some attention. XXX: sysinst needs to be taught how to get sets from tape...

---
+++ distrib/utils/sysinst/arch/mvme68k/md.h
@@ -0,0 +1,143 @@
+/*	$NetBSD: md.h,v 1.1 2002/03/24 22:04:05 scw Exp $	*/
+
+/*
+ * Copyright 1997 Piermont Information Systems Inc.
+ * All rights reserved.
+ *
+ * Based on code written by Philip A. Nelson for Piermont Information
+ * Systems Inc.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *      This product includes software developed for the NetBSD Project by
+ *      Piermont Information Systems Inc.
+ * 4. The name of Piermont Information Systems Inc. may not be used to endorse
+ *    or promote products derived from this software without specific prior
+ *    written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY PIERMONT INFORMATION SYSTEMS INC. ``AS IS''
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL PIERMONT INFORMATION SYSTEMS INC. BE 
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF 
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+/* md.h -- Machine specific definitions for the mvme68k */
+
+
+#include <machine/cpu.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+
+/* constants and defines */
+
+/*
+ * Symbolic names for disk partitions.
+ */
+#define PART_ROOT	A
+#define PART_SWAP	B
+#define PART_RAW	C
+#define PART_USR	D	/* Can be after PART_FIRST_FREE */
+#define PART_FIRST_FREE	E
+
+#define DEFSWAPRAM	32	/* Assume at least this RAM for swap calc */
+#define DEFROOTSIZE	20	/* Default root size */
+#define DEFVARSIZE	32	/* Default /var size, if created */
+#define DEFUSRSIZE	70	/* Default /usr size, if /home */
+#define STDNEEDMB	80	/* Min space for non X install */
+#define XNEEDMB		35	/* Extra megs for full X installation */
+
+/*
+ *  Default filesets to fetch and install during installation
+ *  or upgrade. The standard sets are:
+ *      base, etc, comp, games, man, misc, text,
+ *      xbase, xfont, xcontrib, xcomp.
+ *
+ * Third entry is the last extension name in the split sets for loading
+ * from floppy.
+ */
+EXTERN distinfo dist_list[]
+#ifdef MAIN
+= {
+    {"kern-GENERIC",	1, "ac", "Kernel       : "},
+    {"base",		1, "bs", "Base         : "},
+    {"etc",		1, "aa", "System (/etc): "},
+    {"comp",		1, "bj", "Compiler     : "},
+    {"games",		1, "am", "Games        : "},
+    {"man",		1, "ar", "Manuals      : "},
+    {"misc",		1, "aj", "Miscellaneous: "},
+    {"text",		1, "af", "Text tools   : "},
+
+    {"xbase",		1, "ak", "X11 clients  : "},
+    {"xfont",		1, "ay", "X11 fonts    : "},
+#if 0
+    /* no xserver until vmebus graphics cards supported */
+    {"xserver",		1, "ag", "X11 servers  : "},
+#endif
+    {"xcontrib",	1, "aa", "X11 contrib  : "},
+    {"xcomp",		1, "ag", "X programming: "},
+    {"xmisc",		1, NULL, "X11 Misc.    : "},
+    {NULL, 0, NULL, NULL }
+}
+#endif
+;
+
+/*
+ * Disk names accepted as valid targets for a from-scratch installation.
+ *
+ * On  mvme68k, allow "sd" scsi disks only.
+ */
+EXTERN  char *disk_names[]
+#ifdef MAIN
+= {"sd", NULL}
+#endif
+;
+;
+
+/*
+ * Legal start character for a disk for checking input. 
+ * this must return 1 for a character that matches the first
+ * characters of each member of disk_names.
+ *
+ * On mvme68k, that means matching 's' for sd.
+ */
+#define ISDISKSTART(dn) (dn == 's')
+
+
+/*
+ * Machine-specific command to write a new label to a disk.
+ * For example, i386  uses "/sbin/disklabel -w -r", just like i386
+ * miniroot scripts, though this may leave a bogus incore label.
+ * Sun ports should probably use  DISKLABEL_CMD "/sbin/disklabel -w"
+ * to get incore  to ondisk inode translation for the Sun proms.
+ * If not defined, we assume the port does not support disklabels and
+ * hand-edited disklabel will NOT be written by MI code.
+ *
+ * On mvme68k, do what the miniroot install scripts did. 
+ */
+#define DISKLABEL_CMD "disklabel -w -r"
+
+
+/*
+ * Default fileystem type for floppy disks.
+ * On mvme68k, if we had floppies, that would be ffs.
+ */
+EXTERN	char *fdtype INIT("ffs");
