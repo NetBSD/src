e971d6b60f62b5f6cc69c4c95a6085cffaed44f7
jdolecek <jdolecek@NetBSD.org>
2002-03-24 16:30:11 +0000

    msdosfs_mountfs(): drop some excessive braces in the GEMDOS-related section no change in functionality

--- sys/msdosfs/msdosfs_vfsops.c
+++ sys/msdosfs/msdosfs_vfsops.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: msdosfs_vfsops.c,v 1.79 2002/01/08 20:11:00 jdolecek Exp $	*/
+/*	$NetBSD: msdosfs_vfsops.c,v 1.80 2002/03/24 16:30:11 jdolecek Exp $	*/
 
 /*-
  * Copyright (C) 1994, 1995, 1997 Wolfgang Solfrank.
@@ -48,7 +48,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: msdosfs_vfsops.c,v 1.79 2002/01/08 20:11:00 jdolecek Exp $");
+__KERNEL_RCSID(0, "$NetBSD: msdosfs_vfsops.c,v 1.80 2002/03/24 16:30:11 jdolecek Exp $");
 
 #if defined(_KERNEL_OPT)
 #include "opt_quota.h"
@@ -578,9 +578,9 @@ msdosfs_mountfs(devvp, mp, p, argp)
 	pmp->pm_fatsize = pmp->pm_FATsecs * pmp->pm_BytesPerSec;
 
 	if (argp->flags & MSDOSFSMNT_GEMDOSFS) {
-		if ((pmp->pm_nmbrofclusters <= (0xff0 - 2))
-		      && ((dtype == DTYPE_FLOPPY) || ((dtype == DTYPE_VNODE)
-		      && ((pmp->pm_Heads == 1) || (pmp->pm_Heads == 2))))
+		if (pmp->pm_nmbrofclusters <= (0xff0 - 2)
+		      && (dtype == DTYPE_FLOPPY || (dtype == DTYPE_VNODE
+		      && (pmp->pm_Heads == 1 || pmp->pm_Heads == 2)))
 		    ) {
 			pmp->pm_fatmask = FAT12_MASK;
 			pmp->pm_fatmult = 3;
