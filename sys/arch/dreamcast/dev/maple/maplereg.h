51d68a68a6330696c2a09f946bbaf15e5a9b6e4e
uch <uch@NetBSD.org>
2002-03-25 18:59:41 +0000

    ANSIfy, KNF

---
+++ sys/arch/dreamcast/dev/maple/maplereg.h
@@ -0,0 +1,48 @@
+/*	$NetBSD: maplereg.h,v 1.3.6.2 2002/03/25 18:59:41 uch Exp $	*/
+
+/*-
+ * Copyright (c) 2001 Marcus Comstedt
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed by Marcus Comstedt.
+ * 4. Neither the name of The NetBSD Foundation nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#define MAPLE_DMAADDR	(*(volatile paddr_t *)		0xa05f6c04)
+#define MAPLE_RESET2	(*(volatile u_int32_t *)	0xa05f6c10)
+#define MAPLE_ENABLE	(*(volatile u_int32_t *)	0xa05f6c14)
+#define MAPLE_STATE	(*(volatile u_int32_t *)	0xa05f6c18)
+#define MAPLE_SPEED	(*(volatile u_int32_t *)	0xa05f6c80)
+#define MAPLE_RESET	(*(volatile u_int32_t *)	0xa05f6c8c)
+
+#define RESET_MAGIC 0x6155404f
+
+#define SPEED_2MBPS 0
+#define TIMEOUT(n) ((n)<<16)
+
+#define MAPLE_DMABUF_SIZE (MAPLE_PORTS*MAPLE_SUBUNITS*(1024+1024+4+4))
+
