b246378fe7bf0bbded647f3169e74a4f705dc8c2
uch <uch@NetBSD.org>
2002-03-27 10:44:40 +0000

    All the sh3 ports had an identical procfs_machdep.c, so use a common file under arch/sh3/sh3.

---
+++ sys/arch/sh3/sh3/procfs_machdep.c
@@ -0,0 +1,20 @@
+/*	$NetBSD: procfs_machdep.c,v 1.3.8.2 2002/03/27 10:44:40 uch Exp $	*/
+
+#include <sys/param.h>
+#include <sys/systm.h>
+#include <sys/mount.h>
+#include <sys/vnode.h>
+#include <miscfs/procfs/procfs.h>
+
+
+/*
+ * Linux-style /proc/cpuinfo.
+ * Only used when procfs is mounted with -o linux.
+ */
+int
+procfs_getcpuinfstr(char *buf, int *len)
+{
+	*len = 0;
+
+	return 0;
+}
