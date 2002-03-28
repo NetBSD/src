4a7f5de89f2770d534526874286da16750276f22
manu <manu@NetBSD.org>
2002-03-28 18:45:28 +0000

    Fixed the swap block size problem: the swap block size as used in struct swapent, or as seen in userland, is dbtob(1), which turns to be 512 for all arch for now.
    In struct swapdev, there is another field for block size. This value is private
    to uvm_swap.c and is only used for swap I/O on regular files. It is equal to
    the underlying device block size and it is not necessarily 512.

--- sys/compat/irix/irix_swap.c
+++ sys/compat/irix/irix_swap.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: irix_swap.c,v 1.5 2002/03/28 13:14:42 manu Exp $ */
+/*	$NetBSD: irix_swap.c,v 1.6 2002/03/28 18:45:28 manu Exp $ */
 
 /*-
  * Copyright (c) 2002 The NetBSD Foundation, Inc.
@@ -37,7 +37,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: irix_swap.c,v 1.5 2002/03/28 13:14:42 manu Exp $");
+__KERNEL_RCSID(0, "$NetBSD: irix_swap.c,v 1.6 2002/03/28 18:45:28 manu Exp $");
 
 #include <sys/types.h>
 #include <sys/signal.h> 
@@ -61,8 +61,6 @@ __KERNEL_RCSID(0, "$NetBSD: irix_swap.c,v 1.5 2002/03/28 13:14:42 manu Exp $");
 #include <compat/irix/irix_swap.h>
 #include <compat/irix/irix_syscall.h>
 #include <compat/irix/irix_syscallargs.h>
-
-static int get_block_size __P((char *, int *, struct proc *));
 
 int
 irix_sys_swapctl(p, v, retval)
@@ -128,8 +126,8 @@ irix_sys_swapctl(p, v, retval)
 		struct irix_swapent *ise, *uise;
 		int len, ilen, pathlen;
 		int i;
-		size_t blksz;
-		int scale;
+		size_t blksz = dbtob(1); /* One swap block size in bytes */
+		int scale = (PAGE_SIZE / blksz);
 
 		if ((error = copyin(SCARG(uap, arg), &ist, sizeof(ist))) != 0)
 			return error;
@@ -149,10 +147,6 @@ irix_sys_swapctl(p, v, retval)
 		uvm_swap_stats(SWAP_STATS, bse, ist.swt_n, retval);
 
 		for (i = 0; i < ist.swt_n; i++) {
-			if ((error = get_block_size((char *)&(bse[i].se_path), 
-			    &blksz, p)) != 0 || blksz == 0)
-				goto bad;
-			scale = (PAGE_SIZE / blksz);
 
 			pathlen = MIN(strlen(bse[i].se_path), IRIX_PATH_MAX);
 			if (ise[i].ste_path != NULL && 
@@ -190,7 +184,6 @@ bad:
 		int entries;
 		struct swapent *sep;
 		int i, dontcare, sum = 0;
-		int blksz;
 
 		if (!uvm_useracc((caddr_t)SCARG(uap, arg), 
 		    sizeof(sum), B_WRITE))
@@ -207,19 +200,17 @@ bad:
 		uvm_swap_stats(SWAP_STATS, sep, entries, 
 		    (register_t *)&dontcare);
 
-		for (i = 0; i < entries; i++) {
-			if ((error = get_block_size(sep[i].se_path, 
-			    &blksz, p)) != 0 || blksz == 0)
-				return error;
+		if (SCARG(uap, cmd) == IRIX_SC_GETFREESWAP)
+			for (i = 0; i < entries; i++) 
+				sum += (sep[i].se_nblks - sep[i].se_inuse);
 
-			if (SCARG(uap, cmd) == IRIX_SC_GETFREESWAP)
-				sum += (sep[i].se_nblks - sep[i].se_inuse) /
-				    (blksz / IRIX_SWAP_BLKSZ);
+		if (SCARG(uap, cmd) == IRIX_SC_GETSWAPVIRT)
+			for (i = 0; i < entries; i++) 
+				sum += sep[i].se_nblks;
+		
+		/* dbtob(1) is the size in byte of one swap block */
+		sum = sum * IRIX_SWAP_BLKSZ / dbtob(1); 
 
-			if (SCARG(uap, cmd) == IRIX_SC_GETSWAPVIRT)
-				sum += sep[i].se_nblks / 
-				    (blksz / IRIX_SWAP_BLKSZ);
-		}
 		if ((error = copyout(&sum, SCARG(uap, arg), sizeof(sum))) != 0)
 			return error;
 		break;
@@ -232,59 +223,3 @@ bad:
 	return 0;
 }
 
-
-static int
-get_block_size(path, size, p) /* XXX Fix me! */
-	char *path;
-	int *size;
-	struct proc *p;
-{
-	/* 
-	 * XXX The following code returns values that do not match real swap
-	 * usage. We get the correct values by using a swap block size of
-	 * 512. Is the swap block size constant?
-	 */
-#ifdef notyet
-	struct nameidata nd;
-	struct vnode *vp;
-	struct vattr va;
-	int error = 0;
-
-
-#ifdef DEBUG_IRIX
-	printf("get_block_size()\n");
-	printf("get_block_size(): path=%s\n", path);
-#endif
-	NDINIT(&nd, LOOKUP, FOLLOW|LOCKLEAF, UIO_SYSSPACE, path, p);
-	if ((error = namei(&nd)) != 0)
-		return error;
-	vp = nd.ni_vp;
-
-	switch (vp->v_type) {
-	case VBLK:
-		if ((error = VOP_GETATTR(vp, &va, p->p_ucred, p)))
-			goto out;
-		*size = va.va_blocksize; /* XXX Not sure this is right */
-		break;
-	case VREG:
-		*size = vp->v_mount->mnt_stat.f_iosize;
-		break;
-	default:
-		error = ENXIO;
-		break;
-	}
-
-#ifdef DEBUG_IRIX
-	printf("block size = %d\n", *size);
-#endif
-out:
-	vput(vp);
-	return error ;
-
-#else /* notyet */
-	
-	*size = 512; 
-	return 0;
-
-#endif /* notyet */
-}
