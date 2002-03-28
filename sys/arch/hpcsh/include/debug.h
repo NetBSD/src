44d8d3c93341afabb36063080da7aeca36f987d0
uch <uch@NetBSD.org>
2002-03-28 15:24:25 +0000

    INTERRUPT_MONITOR -> HPC_DEBUG_INTERRUPT_MONITOR

---
+++ sys/arch/hpcsh/include/debug.h
@@ -0,0 +1,62 @@
+/*	$NetBSD: debug.h,v 1.3.6.2 2002/03/28 15:24:26 uch Exp $	*/
+
+/*-
+ * Copyright (c) 1999-2002 The NetBSD Foundation, Inc.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to The NetBSD Foundation
+ * by UCHIYAMA Yasushi.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *        This product includes software developed by the NetBSD
+ *        Foundation, Inc. and its contributors.
+ * 4. Neither the name of The NetBSD Foundation nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#define USE_HPC_DPRINTF
+#define __DPRINTF_EXT
+#include <hpc/include/debug.h>
+
+#include "debug_hpc.h"
+/*
+ * interrupt monitor
+ */
+#ifdef HPC_DEBUG_INTERRUPT_MONITOR
+enum heart_beat {
+	HEART_BEAT_BLACK = 0,
+	HEART_BEAT_RED,
+	HEART_BEAT_GREEN,
+	HEART_BEAT_YELLOW,
+	HEART_BEAT_BLUE,
+	HEART_BEAT_MAGENTA,
+	HEART_BEAT_CYAN,
+	HEART_BEAT_WHITE,
+};
+void __dbg_heart_beat(enum heart_beat);
+#else
+#define __dbg_heart_beat(x)	((void)0)
+#endif /* HPC_DEBUG_INTERRUPT_MONITOR */
+
