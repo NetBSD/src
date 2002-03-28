7c686333345fddcdfe280d91b6630fa6aed75589
manu <manu@NetBSD.org>
2002-03-28 13:14:42 +0000

    - now reports accurate swap object size with swapctl(SWAP_STATS) - Added two more swapctl commands: GETFREESWAP and GETSWAPVIRT.
    There is a problem in the way swap block size are found here. See comment
    in get_block_size().

--- sys/compat/irix/irix_swap.c
+++ sys/compat/irix/irix_swap.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: irix_swap.c,v 1.4 2002/03/18 20:34:54 manu Exp $ */
+/*	$NetBSD: irix_swap.c,v 1.5 2002/03/28 13:14:42 manu Exp $ */
 
 /*-
  * Copyright (c) 2002 The NetBSD Foundation, Inc.
@@ -37,7 +37,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: irix_swap.c,v 1.4 2002/03/18 20:34:54 manu Exp $");
+__KERNEL_RCSID(0, "$NetBSD: irix_swap.c,v 1.5 2002/03/28 13:14:42 manu Exp $");
 
 #include <sys/types.h>
 #include <sys/signal.h> 
@@ -47,6 +47,8 @@ __KERNEL_RCSID(0, "$NetBSD: irix_swap.c,v 1.4 2002/03/18 20:34:54 manu Exp $");
 #include <sys/proc.h>
 #include <sys/systm.h>
 #include <sys/swap.h>
+#include <sys/vnode.h>
+#include <sys/namei.h>
 #include <sys/syscallargs.h>
 
 #include <uvm/uvm_page.h>
@@ -59,6 +61,8 @@ __KERNEL_RCSID(0, "$NetBSD: irix_swap.c,v 1.4 2002/03/18 20:34:54 manu Exp $");
 #include <compat/irix/irix_swap.h>
 #include <compat/irix/irix_syscall.h>
 #include <compat/irix/irix_syscallargs.h>
+
+static int get_block_size __P((char *, int *, struct proc *));
 
 int
 irix_sys_swapctl(p, v, retval)
@@ -124,6 +128,8 @@ irix_sys_swapctl(p, v, retval)
 		struct irix_swapent *ise, *uise;
 		int len, ilen, pathlen;
 		int i;
+		size_t blksz;
+		int scale;
 
 		if ((error = copyin(SCARG(uap, arg), &ist, sizeof(ist))) != 0)
 			return error;
@@ -143,22 +149,29 @@ irix_sys_swapctl(p, v, retval)
 		uvm_swap_stats(SWAP_STATS, bse, ist.swt_n, retval);
 
 		for (i = 0; i < ist.swt_n; i++) {
+			if ((error = get_block_size((char *)&(bse[i].se_path), 
+			    &blksz, p)) != 0 || blksz == 0)
+				goto bad;
+			scale = (PAGE_SIZE / blksz);
+
 			pathlen = MIN(strlen(bse[i].se_path), IRIX_PATH_MAX);
 			if (ise[i].ste_path != NULL && 
 			    ((error = copyout(&(bse[i].se_path), 
 			    ise[i].ste_path, pathlen)) != 0))
 				goto bad;
+
 			ise[i].ste_start = 0;
-			ise[i].ste_length = bse[i].se_nblks;
-			ise[i].ste_pages = bse[i].se_nblks;
-			ise[i].ste_free = bse[i].se_nblks - bse[i].se_inuse;
+			ise[i].ste_length = bse[i].se_nblks * blksz;
+			ise[i].ste_pages = bse[i].se_nblks / scale;
+			ise[i].ste_free = (bse[i].se_nblks - bse[i].se_inuse) /
+			    scale;
 
 			ise[i].ste_flags = 0; 
 			if (bse[i].se_flags & SWF_FAKE)
 				ise[i].ste_flags |= IRIX_ST_NOTREADY;
 
-			ise[i].ste_vpages = bse[i].se_inuse;
-			ise[i].ste_maxpages = bse[i].se_nblks;
+			ise[i].ste_vpages = bse[i].se_inuse / scale;
+			ise[i].ste_maxpages = bse[i].se_nblks / scale;
 			ise[i].ste_lswap = 1; /* XXX */
 			ise[i].ste_pri = bse[i].se_priority;
 
@@ -170,8 +183,47 @@ bad:
 		free(ise, M_TEMP);
 		return error;
 		break;
-	}
+	 }
 
+	case IRIX_SC_GETFREESWAP:
+	case IRIX_SC_GETSWAPVIRT: {
+		int entries;
+		struct swapent *sep;
+		int i, dontcare, sum = 0;
+		int blksz;
+
+		if (!uvm_useracc((caddr_t)SCARG(uap, arg), 
+		    sizeof(sum), B_WRITE))
+			return EACCES;
+
+		SCARG(&cup, cmd) = SWAP_NSWAP;
+		SCARG(&cup, arg) = NULL;
+		SCARG(&cup, misc) = 0;
+		if ((error = sys_swapctl(p, &cup, (register_t *)&entries)) != 0)
+			return error;
+
+		sep = (struct swapent *)malloc(
+		    sizeof(struct swapent) * entries, M_TEMP, M_WAITOK);
+		uvm_swap_stats(SWAP_STATS, sep, entries, 
+		    (register_t *)&dontcare);
+
+		for (i = 0; i < entries; i++) {
+			if ((error = get_block_size(sep[i].se_path, 
+			    &blksz, p)) != 0 || blksz == 0)
+				return error;
+
+			if (SCARG(uap, cmd) == IRIX_SC_GETFREESWAP)
+				sum += (sep[i].se_nblks - sep[i].se_inuse) /
+				    (blksz / IRIX_SWAP_BLKSZ);
+
+			if (SCARG(uap, cmd) == IRIX_SC_GETSWAPVIRT)
+				sum += sep[i].se_nblks / 
+				    (blksz / IRIX_SWAP_BLKSZ);
+		}
+		if ((error = copyout(&sum, SCARG(uap, arg), sizeof(sum))) != 0)
+			return error;
+		break;
+	}
 	default:
 		printf("irix_sys_swapctl(): unsupported command %d\n", 
 		    SCARG(uap, cmd));
@@ -181,3 +233,58 @@ bad:
 }
 
 
+static int
+get_block_size(path, size, p) /* XXX Fix me! */
+	char *path;
+	int *size;
+	struct proc *p;
+{
+	/* 
+	 * XXX The following code returns values that do not match real swap
+	 * usage. We get the correct values by using a swap block size of
+	 * 512. Is the swap block size constant?
+	 */
+#ifdef notyet
+	struct nameidata nd;
+	struct vnode *vp;
+	struct vattr va;
+	int error = 0;
+
+
+#ifdef DEBUG_IRIX
+	printf("get_block_size()\n");
+	printf("get_block_size(): path=%s\n", path);
+#endif
+	NDINIT(&nd, LOOKUP, FOLLOW|LOCKLEAF, UIO_SYSSPACE, path, p);
+	if ((error = namei(&nd)) != 0)
+		return error;
+	vp = nd.ni_vp;
+
+	switch (vp->v_type) {
+	case VBLK:
+		if ((error = VOP_GETATTR(vp, &va, p->p_ucred, p)))
+			goto out;
+		*size = va.va_blocksize; /* XXX Not sure this is right */
+		break;
+	case VREG:
+		*size = vp->v_mount->mnt_stat.f_iosize;
+		break;
+	default:
+		error = ENXIO;
+		break;
+	}
+
+#ifdef DEBUG_IRIX
+	printf("block size = %d\n", *size);
+#endif
+out:
+	vput(vp);
+	return error ;
+
+#else /* notyet */
+	
+	*size = 512; 
+	return 0;
+
+#endif /* notyet */
+}
