49867abc2956014fb28203983e045e3903404546
martin <martin@NetBSD.org>
2002-03-30 19:13:44 +0000

    Be a nice citizen, enable interrupts only when we are ready to talk to the ISDN (i.e. when userland has /dev/isdn open).

--- sys/dev/isa/isic_isa.c
+++ sys/dev/isa/isic_isa.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: isic_isa.c,v 1.9 2002/03/24 23:04:27 martin Exp $	*/
+/*	$NetBSD: isic_isa.c,v 1.10 2002/03/30 19:13:45 martin Exp $	*/
 
 /*
  *   Copyright (c) 1997-1999 Martin Husemann. All rights reserved.
@@ -35,7 +35,7 @@
  *	isic_isa.c - ISA bus frontend for i4b_isic driver
  *	--------------------------------------------------
  *
- *	$Id: isic_isa.c,v 1.9 2002/03/24 23:04:27 martin Exp $ 
+ *	$Id: isic_isa.c,v 1.10 2002/03/30 19:13:45 martin Exp $ 
  *
  *      last edit-date: [Tue Jan  9 01:43:45 2001]
  *
@@ -45,7 +45,7 @@
  *---------------------------------------------------------------------------*/
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: isic_isa.c,v 1.9 2002/03/24 23:04:27 martin Exp $");
+__KERNEL_RCSID(0, "$NetBSD: isic_isa.c,v 1.10 2002/03/30 19:13:45 martin Exp $");
 
 #include <sys/param.h>
 #include <sys/errno.h>
@@ -807,8 +807,9 @@ isicattach(int flags, struct isic_softc *sc)
 #endif /* __FreeBSD__ */
 
 	/* init higher protocol layers */
+	isic_enable_intr(sc, 0);
 	isic_attach_bri(sc, drvid, &isic_std_driver);
-	
+
 	return(1);
 #undef PARM
 #undef FLAGS
