15b0cb61a92cf7fe4d9cfb6f73d5a96ae98dbda2
bjh21 <bjh21@NetBSD.org>
2002-04-01 13:30:24 +0000

    Add a default for CAP_MKDB.

--- share/mk/bsd.sys.mk
+++ share/mk/bsd.sys.mk
@@ -1,4 +1,4 @@
-#	$NetBSD: bsd.sys.mk,v 1.72 2002/03/31 15:01:40 bjh21 Exp $
+#	$NetBSD: bsd.sys.mk,v 1.73 2002/04/01 13:30:24 bjh21 Exp $
 #
 # Build definitions used for NetBSD source tree builds.
 
@@ -61,6 +61,7 @@ HOST_LDFLAGS?=
 HOST_AR?=	ar
 HOST_RANLIB?=	ranlib
 
+CAP_MKDB?=	cap_mkdb
 CKSUM?=		cksum
 CONFIG?=	config
 CRUNCHGEN?=	crunchgen
