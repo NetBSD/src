51fe2f23c767cdf6095f6a229b4dd375f032a7f7
nathanw <nathanw@NetBSD.org>
2002-04-01 18:48:04 +0000

    Add a few files missed in the merge.

---
+++ sys/lkm/exec/svr4_32_elf/lkminit_exec.c
@@ -0,0 +1,89 @@
+/* $NetBSD: lkminit_exec.c,v 1.4.8.2 2002/04/01 18:48:13 nathanw Exp $ */
+
+/*-
+ * Copyright (c) 1996 The NetBSD Foundation, Inc.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to The NetBSD Foundation
+ * by Michael Graff <explorer@flame.org>.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *        This product includes software developed by the NetBSD
+ *        Foundation, Inc. and its contributors.
+ * 4. Neither the name of The NetBSD Foundation nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <sys/cdefs.h>
+__KERNEL_RCSID(0, "$NetBSD: lkminit_exec.c,v 1.4.8.2 2002/04/01 18:48:13 nathanw Exp $");
+
+#include <sys/param.h>
+#include <sys/systm.h>
+#include <sys/conf.h>
+#include <sys/exec.h>
+#include <sys/proc.h>
+#include <sys/lkm.h>
+#include <sys/signalvar.h>
+
+#include <machine/elf_machdep.h>
+#define ELFSIZE	32
+#include <sys/exec_elf.h>
+
+#include <compat/svr4_32/svr4_32_types.h>
+#include <compat/svr4_32/svr4_32_exec.h>
+
+int exec_svr4_32_elf_lkmentry __P((struct lkm_table *, int, int));
+
+static struct execsw exec_svr4_32_elf =
+	/* SVR4 Elf32 on 64-bit */
+	{ sizeof (Elf_Ehdr),
+	  exec_elf32_makecmds,
+	  { ELFNAME2(svr4_32,probe) },
+	  NULL,
+	  EXECSW_PRIO_ANY,
+	  SVR4_32_AUX_ARGSIZ,
+	  svr4_32_copyargs,
+	  NULL,
+	  coredump_netbsd };
+
+/*
+ * declare the exec
+ */
+MOD_EXEC("exec_svr4_32_elf", -1, &exec_svr4_32_elf, "svr4_32");
+
+/*
+ * entry point
+ */
+int
+exec_svr4_32_elf_lkmentry(lkmtp, cmd, ver)
+	struct lkm_table *lkmtp;	
+	int cmd;
+	int ver;
+{
+	DISPATCH(lkmtp, cmd, ver,
+		 lkm_nofunc,
+		 lkm_nofunc,
+		 lkm_nofunc);
+}
