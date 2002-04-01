ecd12ab0332a0262db30f09c5dff929d1d5a25ea
thorpej <thorpej@NetBSD.org>
2002-04-01 20:25:50 +0000

    Update DEFAULT_VECTOR and SELECT_VECS for arm--netbsdelf BFD changes.

--- gnu/lib/libbfd/arch/arm/defs.mk
+++ gnu/lib/libbfd/arch/arm/defs.mk
@@ -5,7 +5,7 @@ G_libbfd_la_DEPENDENCIES=elfarm-nabi.lo elf32.lo elf.lo elflink.lo dwarf1.lo arm
 G_libbfd_la_OBJECTS=archive.lo archures.lo bfd.lo cache.lo coffgen.lo  corefile.lo format.lo init.lo libbfd.lo opncls.lo reloc.lo section.lo  syms.lo targets.lo hash.lo linker.lo srec.lo binary.lo tekhex.lo  ihex.lo stabs.lo stab-syms.lo dwarf2.lo
 G_DEFS=-DHAVE_CONFIG_H -I. -I${DIST}/bfd -I.
 G_INCLUDES=-D_GNU_SOURCE  -DNETBSD_CORE   -I. -I${DIST}/bfd -I${DIST}/bfd/../include  -I${DIST}/bfd/../intl -I../intl
-G_TDEFAULTS=-DDEFAULT_VECTOR=bfd_elf32_littlearm_vec -DSELECT_VECS='&bfd_elf32_littlearm_vec,&armnetbsd_vec,&bfd_elf32_little_generic_vec,&bfd_elf32_big_generic_vec' -DSELECT_ARCHITECTURES='&bfd_arm_arch' -DHAVE_bfd_elf32_littlearm_vec -DHAVE_armnetbsd_vec -DHAVE_bfd_elf32_little_generic_vec -DHAVE_bfd_elf32_big_generic_vec
+G_TDEFAULTS=-DDEFAULT_VECTOR=bfd_elf32_littlearm_nbsd_vec -DSELECT_VECS='&bfd_elf32_littlearm_nbsd_vec,&bfd_elf32_littlearm_vec,&bfd_elf32_bigarm_vec,&bfd_elf32_bigarm_nbsd_vec,&armnetbsd_vec,&bfd_elf32_little_generic_vec,&bfd_elf32_big_generic_vec' -DSELECT_ARCHITECTURES='&bfd_arm_arch' -DHAVE_bfd_elf32_littlearm_vec -DHAVE_armnetbsd_vec -DHAVE_bfd_elf32_little_generic_vec -DHAVE_bfd_elf32_big_generic_vec
 G_archdefs=-DARCH_arm
 G_BFD_MACHINES=arm-dis.lo
 G_libopcodes_la_SOURCES=dis-buf.c disassemble.c
