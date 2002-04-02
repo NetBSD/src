079e668baebd45c156fa53a1a42d5b8c1e89f809
jdolecek <jdolecek@NetBSD.org>
2002-04-02 17:46:06 +0000

    Changes to make it less likely to need to be revisited later again: * fdesc_attr(): don't panic for 'unknown' descriptor types, rather use   (*fp->f_ops->fo_stat)() hook, as for DTYPE_SOCKET and DTYPE_PIPE   XXX perhaps use different vnode type than VBAD for these? * fdesc_setattr(): just return 0 regardless of type, rather than paniccing   for 'unknown' descriptor types

--- sys/miscfs/fdesc/fdesc_vnops.c
+++ sys/miscfs/fdesc/fdesc_vnops.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: fdesc_vnops.c,v 1.68 2002/04/02 10:34:54 jmc Exp $	*/
+/*	$NetBSD: fdesc_vnops.c,v 1.69 2002/04/02 17:46:06 jdolecek Exp $	*/
 
 /*
  * Copyright (c) 1992, 1993
@@ -45,7 +45,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: fdesc_vnops.c,v 1.68 2002/04/02 10:34:54 jmc Exp $");
+__KERNEL_RCSID(0, "$NetBSD: fdesc_vnops.c,v 1.69 2002/04/02 17:46:06 jdolecek Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -492,37 +492,42 @@ fdesc_attr(fd, vap, cred, p)
 		}
 		break;
 
-	case DTYPE_SOCKET:
-	case DTYPE_PIPE:
+	default:
 		FILE_USE(fp);
+		memset(&stb, 0, sizeof(stb));
 		error = (*fp->f_ops->fo_stat)(fp, &stb, p);
 		FILE_UNUSE(fp, p);
-		if (error == 0) {
-			vattr_null(vap);
-			if (fp->f_type == DTYPE_SOCKET)
-				vap->va_type = VSOCK;
-			else
-				vap->va_type = VFIFO;
-			vap->va_mode = stb.st_mode;
-			vap->va_nlink = stb.st_nlink;
-			vap->va_uid = stb.st_uid;
-			vap->va_gid = stb.st_gid;
-			vap->va_fsid = stb.st_dev;
-			vap->va_fileid = stb.st_ino;
-			vap->va_size = stb.st_size;
-			vap->va_blocksize = stb.st_blksize;
-			vap->va_atime = stb.st_atimespec;
-			vap->va_mtime = stb.st_mtimespec;
-			vap->va_ctime = stb.st_ctimespec;
-			vap->va_gen = stb.st_gen;
-			vap->va_flags = stb.st_flags;
-			vap->va_rdev = stb.st_rdev;
-			vap->va_bytes = stb.st_blocks * stb.st_blksize;
-		}
-		break;
+		if (error)
+			break;
 
-	default:
-		panic("fdesc attr");
+		vattr_null(vap);
+		switch(fp->f_type) {
+		case DTYPE_SOCKET:
+			vap->va_type = VSOCK;
+			break;
+		case DTYPE_PIPE:
+			vap->va_type = VFIFO;
+			break;
+		default:
+			/* use VNON perhaps? */
+			vap->va_type = VBAD;
+			break;
+		}
+		vap->va_mode = stb.st_mode;
+		vap->va_nlink = stb.st_nlink;
+		vap->va_uid = stb.st_uid;
+		vap->va_gid = stb.st_gid;
+		vap->va_fsid = stb.st_dev;
+		vap->va_fileid = stb.st_ino;
+		vap->va_size = stb.st_size;
+		vap->va_blocksize = stb.st_blksize;
+		vap->va_atime = stb.st_atimespec;
+		vap->va_mtime = stb.st_mtimespec;
+		vap->va_ctime = stb.st_ctimespec;
+		vap->va_gen = stb.st_gen;
+		vap->va_flags = stb.st_flags;
+		vap->va_rdev = stb.st_rdev;
+		vap->va_bytes = stb.st_blocks * stb.st_blksize;
 		break;
 	}
 
@@ -623,7 +628,6 @@ fdesc_setattr(v)
 	struct filedesc *fdp = ap->a_p->p_fd;
 	struct file *fp;
 	unsigned fd;
-	int error;
 
 	/*
 	 * Can't mess with the root vnode
@@ -648,19 +652,7 @@ fdesc_setattr(v)
 	 *      On vnode's this will cause truncation and socket/pipes make
 	 *      no sense.
 	 */
-	switch (fp->f_type) {
-	case DTYPE_VNODE:
-	case DTYPE_SOCKET:
-	case DTYPE_PIPE:
-		error = 0;
-		break;
-
-	default:
-		panic("fdesc setattr");
-		break;
-	}
-
-	return (error);
+	return (0);
 }
 
 #define UIO_MX 32
