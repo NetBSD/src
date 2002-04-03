b8853d5d145fed71b9b8d4b4548b54777b15c0e3
christos <christos@NetBSD.org>
2002-04-03 17:13:32 +0000

    provide missing types for arch's that don't have specific linux_types.h

--- sys/compat/linux/common/linux_types.h
+++ sys/compat/linux/common/linux_types.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: linux_types.h,v 1.13 2002/02/15 16:48:03 christos Exp $	*/
+/*	$NetBSD: linux_types.h,v 1.14 2002/04/03 17:13:32 christos Exp $	*/
 
 /*-
  * Copyright (c) 1995, 1998 The NetBSD Foundation, Inc.
@@ -54,6 +54,9 @@
 #include <compat/linux/arch/m68k/linux_types.h>
 #elif defined(__mips__)
 #include <compat/linux/arch/mips/linux_types.h>
+#else
+typedef unsigned long linux_clock_t;
+typedef unsigned long linux_time_t;
 #endif
 
 /*
