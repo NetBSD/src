2a5adba7334765da2696eb022be71946c090f52c
matt <matt@NetBSD.org>
2002-04-03 00:12:07 +0000

    Clean the icache for pages when they are entered as executable and before they were either not mapped at all or mapped as non-executable.  Round memory regions in pmap_bootstrap.

--- sys/arch/powerpc/include/mpc6xx/pte.h
+++ sys/arch/powerpc/include/mpc6xx/pte.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: pte.h,v 1.7 2002/03/02 21:36:27 kleink Exp $	*/
+/*	$NetBSD: pte.h,v 1.8 2002/04/03 00:12:07 matt Exp $	*/
 
 /*-
  * Copyright (C) 1995, 1996 Wolfgang Solfrank.
@@ -67,6 +67,8 @@ struct pte {
 #define	PTE_BR		0x00000003	/* Both Read Only    (U: RO, S: RO) */
 #define	PTE_RW		PTE_BW
 #define	PTE_RO		PTE_BR
+
+#define	PTE_EXEC	0x00000200	/* pseudo bit in attrs; page is exec */
 
 #ifndef	_LOCORE
 typedef	struct pte pte_t;
