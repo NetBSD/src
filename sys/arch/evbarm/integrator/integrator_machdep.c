991426d348589f5ff22c242470707192d271d5ef
thorpej <thorpej@NetBSD.org>
2002-04-05 16:58:01 +0000

    * Rewrite the 32-bit ARM pte.h based on the ARM architecture manual.   Significant cleanup, here, including better PTE bit names. * Add XScale PTE extensions (ECC enable, write-allocate cache mode). * Mechanical changes everywhere else to update for new pte.h.  While   doing this, two bugs (as a result of typos) were fixed in
    	arm/arm32/bus_dma.c
    	evbarm/integrator/int_bus_dma.c

--- sys/arch/evbarm/integrator/integrator_machdep.c
+++ sys/arch/evbarm/integrator/integrator_machdep.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: integrator_machdep.c,v 1.20 2002/04/03 23:33:32 thorpej Exp $	*/
+/*	$NetBSD: integrator_machdep.c,v 1.21 2002/04/05 16:58:08 thorpej Exp $	*/
 
 /*
  * Copyright (c) 2001 ARM Ltd
@@ -555,12 +555,12 @@ initarm(bootinfo)
 	kernel_l1pt.pv_pa = 0;
 	for (loop = 0; loop <= NUM_KERNEL_PTS; ++loop) {
 		/* Are we 16KB aligned for an L1 ? */
-		if ((physical_freestart & (PD_SIZE - 1)) == 0
+		if ((physical_freestart & (L1_TABLE_SIZE - 1)) == 0
 		    && kernel_l1pt.pv_pa == 0) {
-			valloc_pages(kernel_l1pt, PD_SIZE / NBPG);
+			valloc_pages(kernel_l1pt, L1_TABLE_SIZE / NBPG);
 		} else {
 			alloc_pages(kernel_pt_table[loop1].pv_pa,
-			    PT_SIZE / NBPG);
+			    L2_TABLE_SIZE / NBPG);
 			++loop1;
 			kernel_pt_table[loop1].pv_va =
 			    kernel_pt_table[loop1].pv_pa;
@@ -568,7 +568,7 @@ initarm(bootinfo)
 	}
 
 	/* This should never be able to happen but better confirm that. */
-	if (!kernel_l1pt.pv_pa || (kernel_l1pt.pv_pa & (PD_SIZE-1)) != 0)
+	if (!kernel_l1pt.pv_pa || (kernel_l1pt.pv_pa & (L1_TABLE_SIZE-1)) != 0)
 		panic("initarm: Failed to align the kernel page directory\n");
 
 	/*
@@ -579,7 +579,7 @@ initarm(bootinfo)
 	alloc_pages(systempage.pv_pa, 1);
 
 	/* Allocate a page for the page table to map kernel page tables*/
-	valloc_pages(kernel_ptpt, PT_SIZE / NBPG);
+	valloc_pages(kernel_ptpt, L2_TABLE_SIZE / NBPG);
 
 	/* Allocate stacks for all modes */
 	valloc_pages(irqstack, IRQ_STACK_SIZE);
@@ -682,7 +682,7 @@ initarm(bootinfo)
 	    UPAGES * NBPG, VM_PROT_READ|VM_PROT_WRITE, PTE_CACHE);
 
 	pmap_map_chunk(l1pagetable, kernel_l1pt.pv_va, kernel_l1pt.pv_pa,
-	    PD_SIZE, VM_PROT_READ|VM_PROT_WRITE, PTE_NOCACHE);
+	    L1_TABLE_SIZE, VM_PROT_READ|VM_PROT_WRITE, PTE_NOCACHE);
 
 	/* Map the page table that maps the kernel pages */
 	pmap_map_entry(l1pagetable, kernel_ptpt.pv_va, kernel_ptpt.pv_pa,
@@ -732,7 +732,7 @@ initarm(bootinfo)
 		    l1_sec_table[loop].pa + l1_sec_table[loop].size - 1,
 		    l1_sec_table[loop].va);
 #endif
-		for (sz = 0; sz < l1_sec_table[loop].size; sz += L1_SEC_SIZE)
+		for (sz = 0; sz < l1_sec_table[loop].size; sz += L1_S_SIZE)
 			pmap_map_section(l1pagetable,
 			    l1_sec_table[loop].va + sz,
 			    l1_sec_table[loop].pa + sz,
