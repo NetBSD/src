f1da7005c9f4ecaa83d05788928e753f51d08003
wiz <wiz>
2002-04-08 12:25:22 +0000

    No more arm32.

--- share/mk/sys.mk
+++ share/mk/sys.mk
@@ -1,4 +1,4 @@
-#	$NetBSD: sys.mk,v 1.65 2002/03/27 05:39:00 eeh Exp $
+#	$NetBSD: sys.mk,v 1.66 2002/04/08 12:25:22 wiz Exp $
 #	@(#)sys.mk	8.2 (Berkeley) 3/21/94
 
 unix?=		We run NetBSD.
@@ -21,7 +21,7 @@ LINK.S?=	${CC} ${AFLAGS} ${CPPFLAGS} ${LDFLAGS}
 
 CC?=		cc
 .if ${MACHINE_ARCH} == "alpha" || \
-    ${MACHINE_ARCH} == "arm" ||	${MACHINE_ARCH} == "arm32" || \
+    ${MACHINE_ARCH} == "arm" || \
     ${MACHINE_ARCH} == "i386" || \
     ${MACHINE_ARCH} == "m68k" || \
     ${MACHINE_ARCH} == "mipsel" || ${MACHINE_ARCH} == "mipseb" || \
