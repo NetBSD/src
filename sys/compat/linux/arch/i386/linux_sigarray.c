5351d532cc1d0ad49f7ec0090af3e0e835addd21
itojun <itojun@NetBSD.org>
2002-04-10 09:53:01 +0000

    revert previous

--- sys/compat/linux/arch/i386/linux_sigarray.c
+++ sys/compat/linux/arch/i386/linux_sigarray.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: linux_sigarray.c,v 1.23 2002/04/10 09:37:51 itojun Exp $	*/
+/*	$NetBSD: linux_sigarray.c,v 1.24 2002/04/10 09:53:01 itojun Exp $	*/
 
 /*-
  * Copyright (c) 1995, 1998 The NetBSD Foundation, Inc.
@@ -36,12 +36,7 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include <sys/cdefs.h>
-__KERNEL_RCSID(1, "$NetBSD: linux_sigarray.c,v 1.23 2002/04/10 09:37:51 itojun Exp $");
-
-#include <sys/types.h>
-#include <sys/signal.h>
-#include <compat/linux/arch/i386/linux_signal.h>
+__KERNEL_RCSID(1, "$NetBSD: linux_sigarray.c,v 1.24 2002/04/10 09:53:01 itojun Exp $");
 
 const int linux_to_native_signo[LINUX__NSIG] = {
 	0,		/* 0 */
