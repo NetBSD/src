eaf1f1814667ef382a791cd93f5a4946ae29f016
pooka <pooka@NetBSD.org>
2002-04-15 08:37:04 +0000

    Bump to nb7 after fixing FreeBSD PR #12801 (again) in a way which hopefully doesn't have adverse side-effects.

--- usr.bin/vi/ex/version.h
+++ usr.bin/vi/ex/version.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: version.h,v 1.9 2002/02/11 19:52:00 pooka Exp $	*/
+/*	$NetBSD: version.h,v 1.10 2002/04/15 08:37:04 pooka Exp $	*/
 
 #define	VI_VERSION \
-    "Version 1.79nb6 (10/23/96) The CSRG, University of California, Berkeley."
+    "Version 1.79nb7 (10/23/96) The CSRG, University of California, Berkeley."
