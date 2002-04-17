1ae41687003118dd998f60594aac4c7a2af15696
he <he@NetBSD.org>
2002-04-17 12:06:37 +0000

    Pull up revision 1.31 (requested by groo):   Replace an instance of strncpy() with strlcpy().

--- lib/libc/net/res_debug.c
+++ lib/libc/net/res_debug.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: res_debug.c,v 1.26.4.1 2000/07/27 16:07:03 itojun Exp $	*/
+/*	$NetBSD: res_debug.c,v 1.26.4.2 2002/04/17 12:06:37 he Exp $	*/
 
 /*-
  * Copyright (c) 1985, 1990, 1993
@@ -81,7 +81,7 @@
 static char sccsid[] = "@(#)res_debug.c	8.1 (Berkeley) 6/4/93";
 static char rcsid[] = "Id: res_debug.c,v 8.20 1997/06/01 20:34:37 vixie Exp ";
 #else
-__RCSID("$NetBSD: res_debug.c,v 1.26.4.1 2000/07/27 16:07:03 itojun Exp $");
+__RCSID("$NetBSD: res_debug.c,v 1.26.4.2 2002/04/17 12:06:37 he Exp $");
 #endif
 #endif /* LIBC_SCCS and not lint */
 
@@ -1127,7 +1127,7 @@ __p_time(value)
 	register char *p;
 
 	if (value == 0) {
-		strncpy(nbuf, "0 secs", sizeof(nbuf) - 1);
+		strlcpy(nbuf, "0 secs", sizeof(nbuf));
 		return(nbuf);
 	}
 
