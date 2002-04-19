6f24e8dbfed239ceddc40c18108833829c31d7d8
wiz <wiz@NetBSD.org>
2002-04-19 02:12:00 +0000

    Remove (commented out) pms cdev_decl.

--- sys/arch/mvmeppc/mvmeppc/conf.c
+++ sys/arch/mvmeppc/mvmeppc/conf.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: conf.c,v 1.2 2002/03/16 16:55:57 martin Exp $	*/
+/*	$NetBSD: conf.c,v 1.3 2002/04/19 02:12:00 wiz Exp $	*/
 
 /*-
  * Copyright (c) 1998 The NetBSD Foundation, Inc.
@@ -153,9 +153,7 @@ cdev_decl(spkr);
 /* #include "mms.h"
 cdev_decl(mms);
 #include "lms.h"
-cdev_decl(lms);
-#include "pms.h"
-cdev_decl(pms); */
+cdev_decl(lms); */
 #include "cy.h"
 cdev_decl(cy);
 cdev_decl(mcd);
