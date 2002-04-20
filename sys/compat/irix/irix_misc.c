efe336454aa4fdce0d6b55743437e51ef25aee24
manu <manu@NetBSD.org>
2002-04-20 07:42:32 +0000

    Added missing irix_misc.c

---
+++ sys/compat/irix/irix_misc.c
@@ -0,0 +1,81 @@
+/*	$NetBSD: irix_misc.c,v 1.1 2002/04/20 07:42:32 manu Exp $ */
+
+/*-
+ * Copyright (c) 2002 The NetBSD Foundation, Inc.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to The NetBSD Foundation
+ * by Emmanuel Dreyfus.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed by the NetBSD
+ *	Foundation, Inc. and its contributors.
+ * 4. Neither the name of The NetBSD Foundation nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <sys/cdefs.h>
+__KERNEL_RCSID(0, "$NetBSD: irix_misc.c,v 1.1 2002/04/20 07:42:32 manu Exp $");
+
+#include <sys/types.h>
+#include <sys/signal.h>
+#include <sys/systm.h>
+#include <sys/param.h>
+#include <sys/proc.h>
+#include <sys/mount.h>
+
+#include <sys/syscallargs.h>
+
+#include <compat/irix/irix_types.h>
+#include <compat/irix/irix_signal.h>
+#include <compat/irix/irix_syscallargs.h>
+
+/* 
+ * This is copied from sys/compat/sunos/sunos_misc.c:sunos_sys_setpgrp()
+ * Maybe consider moving this to sys/compat/common/compat_util.c?
+ */
+int
+irix_sys_setpgrp(p, v, retval)
+	struct proc *p; 
+	void *v;
+	register_t *retval;
+{
+	struct irix_sys_setpgrp_args /* { 
+		syscallarg(int) pid;
+		syscallarg(int) pgid;
+	} */ *uap = v;
+ 
+	/*
+	 * difference to our setpgid call is to include backwards  
+	 * compatibility to pre-setsid() binaries. Do setsid()
+	 * instead of setpgid() in those cases where the process
+	 * tries to create a new session the old way.
+	 */
+	if (!SCARG(uap, pgid) &&
+	    (!SCARG(uap, pid) || SCARG(uap, pid) == p->p_pid))
+		return sys_setsid(p, uap, retval);
+	else
+		return sys_setpgid(p, uap, retval);
+}
