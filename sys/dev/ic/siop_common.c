8ba368a0b6cafdeda953a03ec7b39ae940ef1868
bouyer <bouyer@NetBSD.org>
2002-04-22 20:31:49 +0000

    Really, don't set siop_cmd->tag in setup_table()

--- sys/dev/ic/siop_common.c
+++ sys/dev/ic/siop_common.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: siop_common.c,v 1.19 2002/04/22 15:48:56 bouyer Exp $	*/
+/*	$NetBSD: siop_common.c,v 1.20 2002/04/22 20:31:49 bouyer Exp $	*/
 
 /*
  * Copyright (c) 2000 Manuel Bouyer.
@@ -33,7 +33,7 @@
 /* SYM53c7/8xx PCI-SCSI I/O Processors driver */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: siop_common.c,v 1.19 2002/04/22 15:48:56 bouyer Exp $");
+__KERNEL_RCSID(0, "$NetBSD: siop_common.c,v 1.20 2002/04/22 20:31:49 bouyer Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -165,8 +165,7 @@ siop_setuptables(siop_cmd)
 		siop_cmd->siop_tables->msg_out[2] = siop_cmd->tag;
 		siop_cmd->siop_tables->t_msgout.count = htole32(3);
 		msgoffset = 3;
-	} else
-		siop_cmd->tag = 0;
+	}
 	if (sc->targets[target]->status == TARST_ASYNC) {
 		if (sc->targets[target]->flags & TARF_WIDE) {
 			sc->targets[target]->status = TARST_WIDE_NEG;
