1eeb46176aae4d2ff8d381af27b4e4a91751e204
augustss <augustss@NetBSD.org>
2002-04-22 12:48:40 +0000

    Another disk quirk.

--- sys/dev/usb/umass_quirks.c
+++ sys/dev/usb/umass_quirks.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: umass_quirks.c,v 1.12 2002/02/20 22:28:54 augustss Exp $	*/
+/*	$NetBSD: umass_quirks.c,v 1.13 2002/04/22 12:48:40 augustss Exp $	*/
 
 /*
  * Copyright (c) 2001 The NetBSD Foundation, Inc.
@@ -203,6 +203,14 @@ Static const struct umass_quirk umass_quirks[] = {
 	},
 
 	{ { USB_VENDOR_MINOLTA, USB_PRODUCT_MINOLTA_X },
+	  UMASS_WPROTO_UNSPEC, UMASS_CPROTO_UNSPEC,
+	  UMASS_QUIRK_NO_MAX_LUN | UMASS_QUIRK_NO_START_STOP,
+	  0,
+	  UMATCH_DEVCLASS_DEVSUBCLASS_DEVPROTO,
+	  NULL, NULL
+	},
+
+	{ { USB_VENDOR_PEN, USB_PRODUCT_PEN_USBDISK },
 	  UMASS_WPROTO_UNSPEC, UMASS_CPROTO_UNSPEC,
 	  UMASS_QUIRK_NO_MAX_LUN | UMASS_QUIRK_NO_START_STOP,
 	  0,
