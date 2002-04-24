43fa8329082c1a9cfe9043f26e8f87c8fb742386
atatat <atatat@NetBSD.org>
2002-04-24 16:37:08 +0000

    Clean out "dirty" to avoid spurious log entries.

--- lib/libwrap/hosts_access.c
+++ lib/libwrap/hosts_access.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: hosts_access.c,v 1.12 2002/04/04 19:50:27 atatat Exp $	*/
+/*	$NetBSD: hosts_access.c,v 1.13 2002/04/24 16:37:08 atatat Exp $	*/
 
  /*
   * This module implements a simple access control language that is based on
@@ -24,7 +24,7 @@
 #if 0
 static char sccsid[] = "@(#) hosts_access.c 1.21 97/02/12 02:13:22";
 #else
-__RCSID("$NetBSD: hosts_access.c,v 1.12 2002/04/04 19:50:27 atatat Exp $");
+__RCSID("$NetBSD: hosts_access.c,v 1.13 2002/04/24 16:37:08 atatat Exp $");
 #endif
 #endif
 
@@ -475,6 +475,7 @@ char   *string;
 	return (NO);				/* not tcpd_jump() */
     }
 
+    dirty = 0;
     for (i = 0; i < sizeof(addr); i++) {
 	addr.s6_addr[i] &= mask.s6_addr[i];
 	dirty |= (net.s6_addr[i] & ~mask.s6_addr[i]);
