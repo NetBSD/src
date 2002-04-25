1fabb17bf37fd71135a6e07f5c42f527a45abccf
nonaka <nonaka@NetBSD.org>
2002-04-25 20:36:50 +0000

    Separate consinit.

--- sys/arch/prep/prep/machdep.c
+++ sys/arch/prep/prep/machdep.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: machdep.c,v 1.37 2002/04/23 12:41:08 kleink Exp $	*/
+/*	$NetBSD: machdep.c,v 1.38 2002/04/25 20:36:50 nonaka Exp $	*/
 
 /*
  * Copyright (C) 1995, 1996 Wolfgang Solfrank.
@@ -70,31 +70,6 @@
 #include <machine/trap.h>
 
 #include <dev/cons.h>
-
-#include "gten.h"
-#if (NGTEN > 0)
-#include <machine/gtenvar.h>
-#endif
-
-/* Implied by gten support. */
-#if (NGTEN > 0)
-#include <dev/pci/pcivar.h>
-#endif
-
-#include "vga.h"
-#if (NVGA > 0)
-#include <dev/ic/mc6845reg.h>
-#include <dev/ic/pcdisplayvar.h>
-#include <dev/ic/vgareg.h>
-#include <dev/ic/vgavar.h>
-#endif
-
-#include "pckbc.h"
-#if (NPCKBC > 0)
-#include <dev/isa/isareg.h>
-#include <dev/ic/i8042reg.h>
-#include <dev/ic/pckbcvar.h>
-#endif
 
 #include "com.h"
 #if (NCOM > 0)
@@ -582,75 +557,6 @@ lookup_bootinfo(type)
 		(size_t)help < (size_t)bootinfo + sizeof (bootinfo));
 
 	return (NULL);
-}
-
-/*
- * consinit
- * Initialize system console.
- */
-void
-consinit()
-{
-	struct btinfo_console *consinfo;
-	static int initted = 0;
-#if (NGTEN > 0)
-	struct prep_pci_chipset pc;
-#endif
-
-	if (initted)
-		return;
-	initted = 1;
-
-	consinfo = (struct btinfo_console *)lookup_bootinfo(BTINFO_CONSOLE);
-	if (!consinfo)
-		panic("not found console information in bootinfo");
-
-#if (NPFB > 0)
-	if (!strcmp(consinfo->devname, "fb")) {
-		pfb_cnattach(consinfo->addr);
-#if (NPCKBC > 0)
-		pckbc_cnattach(&prep_isa_io_space_tag, IO_KBD, KBCMDP,
-		    PCKBC_KBD_SLOT);
-#endif
-		return;
-	}
-#endif
-
-#if (NVGA > 0) || (NGTEN > 0)
-	if (!strcmp(consinfo->devname, "vga")) {
-#if (NGTEN > 0)
-		(*platform->pci_get_chipset_tag)(&pc);
-#endif
-#if (NGTEN > 0)
-		if (!gten_cnattach(&pc, &prep_mem_space_tag))
-			goto dokbd;
-#endif
-#if (NVGA > 0)
-		if (!vga_cnattach(&prep_io_space_tag, &prep_mem_space_tag,
-				-1, 1))
-			goto dokbd;
-#endif
-dokbd:
-#if (NPCKBC > 0)
-		pckbc_cnattach(&prep_isa_io_space_tag, IO_KBD, KBCMDP,
-		    PCKBC_KBD_SLOT);
-#endif
-		return;
-	}
-#endif /* VGA | GTEN */
-
-#if (NCOM > 0)
-	if (!strcmp(consinfo->devname, "com")) {
-		bus_space_tag_t tag = &prep_isa_io_space_tag;
-
-		if(comcnattach(tag, consinfo->addr, consinfo->speed, COM_FREQ,
-		    ((TTYDEF_CFLAG & ~(CSIZE | CSTOPB | PARENB)) | CS8)))
-			panic("can't init serial console");
-
-		return;
-	}
-#endif
-	panic("invalid console device %s", consinfo->devname);
 }
 
 void
