63a83497e10ee3438bb63fde9856aa2884b9aaff
he <he@NetBSD.org>
2002-04-26 17:01:13 +0000

    Apply patch (requested by he):   Protect against buffer overruns when decoding KerberosIV credentials.   Bump NetBSD part of version string.

--- crypto/dist/ssh/version.h
+++ crypto/dist/ssh/version.h
@@ -1,8 +1,8 @@
-/*	$NetBSD: version.h,v 1.1.1.1.2.7 2002/03/09 16:47:20 he Exp $	*/
+/*	$NetBSD: version.h,v 1.1.1.1.2.8 2002/04/26 17:01:13 he Exp $	*/
 /* $OpenBSD: version.h,v 1.27 2001/12/05 15:04:48 markus Exp $ */
 
 #define __OPENSSH_VERSION	"OpenSSH_3.0.2"
-#define __NETBSDSSH_VERSION	"NetBSD_Secure_Shell-20020307"
+#define __NETBSDSSH_VERSION	"NetBSD_Secure_Shell-20020426"
 
 /*
  * it is important to retain OpenSSH version identification part, it is
