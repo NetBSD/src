554da09823b7c079c57810888923b848a13e486c
he <he@NetBSD.org>
2002-04-26 13:17:23 +0000

    Pull up revision 1.13 (requested by kleink):   Make it possible to build strlcat() and strlcpy() outside the   libc environment.

--- lib/libc/string/strlcat.c
+++ lib/libc/string/strlcat.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: strlcat.c,v 1.5.8.1 2002/04/22 22:03:41 he Exp $	*/
+/*	$NetBSD: strlcat.c,v 1.5.8.2 2002/04/26 13:17:23 he Exp $	*/
 /*	from OpenBSD: strlcat.c,v 1.2 1999/06/17 16:28:58 millert Exp 	*/
 
 /*
@@ -30,10 +30,12 @@
 
 #include <sys/cdefs.h>
 #if defined(LIBC_SCCS) && !defined(lint)
-__RCSID("$NetBSD: strlcat.c,v 1.5.8.1 2002/04/22 22:03:41 he Exp $");
+__RCSID("$NetBSD: strlcat.c,v 1.5.8.2 2002/04/26 13:17:23 he Exp $");
 #endif /* LIBC_SCCS and not lint */
 
+#ifdef _LIBC
 #include "namespace.h"
+#endif
 #include <sys/types.h>
 #include <assert.h>
 #include <string.h>
