55da36c2dcbd06f34f58f692f8bb93ae77ec3a7f
jdolecek <jdolecek@NetBSD.org>
2002-05-05 16:02:24 +0000

    avoid unnecessary use of MAXBLOCKNUM (which is not defined here)

--- sys/arch/sun68k/stand/bootxx/bootxx.c
+++ sys/arch/sun68k/stand/bootxx/bootxx.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: bootxx.c,v 1.3 2002/04/24 01:40:25 lukem Exp $ */
+/*	$NetBSD: bootxx.c,v 1.4 2002/05/05 16:02:24 jdolecek Exp $ */
 
 /*-
  * Copyright (c) 1998 The NetBSD Foundation, Inc.
@@ -68,7 +68,7 @@
 struct sun68k_bbinfo bbinfo = {
 	{ SUN68K_BBINFO_MAGIC },
 	0,
-	MAXBLOCKNUM,
+	0,
 	{ 0 }
 };
 
