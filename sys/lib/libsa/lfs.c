3f4c1c519b8b18e2fc31eac56545cdbde04033b8
simonb <simonb@NetBSD.org>
2002-05-07 02:02:41 +0000

    Remove two shadowed variables, one of which was set to what it was shadowing(!) and the other a temporary variable (as was it's shadow). Code generated with "gcc -O2" is the same before and after.

--- sys/lib/libsa/lfs.c
+++ sys/lib/libsa/lfs.c
@@ -1,4 +1,4 @@
-/* $NetBSD: lfs.c,v 1.6 2000/03/30 12:19:48 augustss Exp $ */
+/* $NetBSD: lfs.c,v 1.7 2002/05/07 02:02:41 simonb Exp $ */
 
 /*-
  * Copyright (c) 1993
@@ -619,9 +619,7 @@ lfs_open(path, f)
 				/*
 				 * Read file for symbolic link
 				 */
-				size_t buf_size;
 				daddr_t	disk_block;
-				struct fs *fs = fp->f_fs;
 
 				if (!buf)
 					buf = alloc(fs->fs_bsize);
