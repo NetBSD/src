f56554013bf7d3c9a3c4fe90f31ddab0ef188e1d
kleink <kleink@NetBSD.org>
2002-05-12 23:04:15 +0000

    Provide local definitions of in_{addr,port}_t in <netinet/in.h> and use them where deemed appropriate by XNS5.2/POSIX-2001.

--- sys/netinet6/in6.h
+++ sys/netinet6/in6.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: in6.h,v 1.33 2001/12/21 08:54:54 itojun Exp $	*/
+/*	$NetBSD: in6.h,v 1.34 2002/05/12 23:04:15 kleink Exp $	*/
 /*	$KAME: in6.h,v 1.83 2001/03/29 02:55:07 jinmei Exp $	*/
 
 /*
@@ -143,7 +143,7 @@ struct in6_addr {
 struct sockaddr_in6 {
 	u_int8_t	sin6_len;	/* length of this struct(sa_family_t)*/
 	sa_family_t	sin6_family;	/* AF_INET6 (sa_family_t) */
-	u_int16_t	sin6_port;	/* Transport layer port # (in_port_t)*/
+	in_port_t	sin6_port;	/* Transport layer port # (in_port_t)*/
 	u_int32_t	sin6_flowinfo;	/* IP6 flow information */
 	struct in6_addr	sin6_addr;	/* IP6 address */
 	u_int32_t	sin6_scope_id;	/* scope zone index */
