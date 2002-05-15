39738d4eb775d0eeaae0d8f22e2eef3d93040f09
thorpej <thorpej>
2002-05-15 19:23:51 +0000

    Let machine-dependent code specify how to enumerate the bus. Currently, everyone uses pci_enumerate_bus_generic().

--- sys/arch/algor/include/pci_machdep.h
+++ sys/arch/algor/include/pci_machdep.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: pci_machdep.h,v 1.3 2001/10/29 23:34:23 thorpej Exp $	*/
+/*	$NetBSD: pci_machdep.h,v 1.4 2002/05/15 19:23:52 thorpej Exp $	*/
 
 /*
  * Copyright (c) 1996 Carnegie-Mellon University.
@@ -104,6 +104,9 @@ struct algor_pci_chipset {
     (*(c)->pc_intr_disestablish)((c)->pc_intr_v, (iv))
 #define	pci_conf_interrupt(c, b, d, p, s, lp)				\
     (*(c)->pc_conf_interrupt)((c)->pc_intr_v, (b), (d), (p), (s), (lp))
+
+#define	pci_enumerate_bus(sc, m, p)					\
+	pci_enumerate_bus_generic((sc), (m), (p))
 
 /*
  * algor-specific PCI functions.
