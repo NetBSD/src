ddc526006ca96a2460db2278497dbe682361813a
gehenna <gehenna@NetBSD.org>
2002-05-16 03:48:06 +0000

    Use devsw APIs for checking validity of major numbers.

--- sys/ufs/ffs/ffs_vfsops.c
+++ sys/ufs/ffs/ffs_vfsops.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: ffs_vfsops.c,v 1.98 2002/04/10 08:05:13 mycroft Exp $	*/
+/*	$NetBSD: ffs_vfsops.c,v 1.98.2.1 2002/05/16 03:48:07 gehenna Exp $	*/
 
 /*
  * Copyright (c) 1989, 1991, 1993, 1994
@@ -36,7 +36,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: ffs_vfsops.c,v 1.98 2002/04/10 08:05:13 mycroft Exp $");
+__KERNEL_RCSID(0, "$NetBSD: ffs_vfsops.c,v 1.98.2.1 2002/05/16 03:48:07 gehenna Exp $");
 
 #if defined(_KERNEL_OPT)
 #include "opt_ffs.h"
@@ -64,6 +64,7 @@ __KERNEL_RCSID(0, "$NetBSD: ffs_vfsops.c,v 1.98 2002/04/10 08:05:13 mycroft Exp 
 #include <sys/pool.h>
 #include <sys/lock.h>
 #include <sys/sysctl.h>
+#include <sys/conf.h>
 
 #include <miscfs/specfs/specdev.h>
 
@@ -227,7 +228,7 @@ ffs_mount(mp, path, data, ndp, p)
 			 */
 			if (devvp->v_type != VBLK)
 				error = ENOTBLK;
-			else if (major(devvp->v_rdev) >= nblkdev)
+			else if (bdevsw_lookup(devvp->v_rdev) == NULL)
 				error = ENXIO;
 		} else {
 			/*
