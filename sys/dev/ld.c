e9cf58bd311e7edb773755854127202e627b622e
gehenna <gehenna>
2002-05-16 04:53:05 +0000

    Add the block/character device switches. Replace the direct-access to devsw table with calling devsw API.

--- sys/dev/ld.c
+++ sys/dev/ld.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: ld.c,v 1.13 2002/05/08 15:49:07 drochner Exp $	*/
+/*	$NetBSD: ld.c,v 1.13.2.1 2002/05/16 04:53:05 gehenna Exp $	*/
 
 /*-
  * Copyright (c) 1998, 2000 The NetBSD Foundation, Inc.
@@ -41,7 +41,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: ld.c,v 1.13 2002/05/08 15:49:07 drochner Exp $");
+__KERNEL_RCSID(0, "$NetBSD: ld.c,v 1.13.2.1 2002/05/16 04:53:05 gehenna Exp $");
 
 #include "rnd.h"
 
@@ -77,6 +77,24 @@ static int	ldstart(struct ld_softc *, struct buf *);
 static void	ldunlock(struct ld_softc *);
 
 extern struct	cfdriver ld_cd;
+
+dev_type_open(ldopen);
+dev_type_close(ldclose);
+dev_type_read(ldread);
+dev_type_write(ldwrite);
+dev_type_ioctl(ldioctl);
+dev_type_strategy(ldstrategy);
+dev_type_dump(lddump);
+dev_type_size(ldsize);
+
+const struct bdevsw ld_bdevsw = {
+	ldopen, ldclose, ldstrategy, ldioctl, lddump, ldsize, D_DISK
+};
+
+const struct cdevsw ld_cdevsw = {
+	ldopen, ldclose, ldread, ldwrite, ldioctl,
+	nostop, notty, nopoll, nommap, D_DISK
+};
 
 static struct	dkdriver lddkdriver = { ldstrategy };
 static void	*ld_sdh;
@@ -185,12 +203,8 @@ ldenddetach(struct ld_softc *sc)
 			printf("%s: not drained\n", sc->sc_dv.dv_xname);
 
 	/* Locate the major numbers. */
-	for (bmaj = 0; bmaj <= nblkdev; bmaj++)
-		if (bdevsw[bmaj].d_open == ldopen)
-			break;
-	for (cmaj = 0; cmaj <= nchrdev; cmaj++)
-		if (cdevsw[cmaj].d_open == ldopen)
-			break;
+	bmaj = bdevsw_lookup_major(&ld_bdevsw);
+	cmaj = cdevsw_lookup_major(&ld_cdevsw);
 
 	/* Kill off any queued buffers. */
 	s = splbio();
