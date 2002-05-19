6f767749808cc1b3111693fd0f3a2a854fcc338c
gehenna <gehenna@NetBSD.org>
2002-05-19 08:31:17 +0000

    Remove port-dependent conf.h.

--- sys/arch/playstation2/include/conf.h
+++
@@ -1,3 +0,0 @@
-/*	$NetBSD: conf.h,v 1.2 2002/02/27 01:19:07 christos Exp $	*/
-
-#include <sys/conf.h>
--- sys/arch/sgimips/include/conf.h
+++
@@ -1,7 +0,0 @@
-/*	$NetBSD: conf.h,v 1.3 2002/02/27 01:19:07 christos Exp $	*/
-
-#include <sys/conf.h>
-
-bdev_decl(sw);
-cdev_decl(sw);
-cdev_decl(scsibus);
