5ba1eacd50a4a4de69558381d0f9c604b6015e67
kleink <kleink@NetBSD.org>
2002-05-22 09:42:37 +0000

    Turn the flags argument to getnameinfo(3) back into an int (from unsigned int); this is consistent with RFC2553, the current draft-ietf-ipngwg- rfc2553bis-05 and the majority of deployed implementations.
    Also, there is an effort under way to have this changed back to int in
    XBD6/XSH6, XNS, and POSIX-2001 TC1.

--- include/netdb.h
+++ include/netdb.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: netdb.h,v 1.25 2002/05/14 14:35:36 kleink Exp $	*/
+/*	$NetBSD: netdb.h,v 1.26 2002/05/22 09:42:37 kleink Exp $	*/
 
 /*
  * Copyright (C) 1995, 1996, 1997, and 1998 WIDE Project.
@@ -319,7 +319,7 @@ void		setprotoent __P((int));
 int		getaddrinfo __P((const char *, const char *,
 				 const struct addrinfo *, struct addrinfo **));
 int		getnameinfo __P((const struct sockaddr *, socklen_t, char *,
-				 socklen_t, char *, socklen_t, unsigned int));
+				 socklen_t, char *, socklen_t, int));
 void		freeaddrinfo __P((struct addrinfo *));
 char		*gai_strerror __P((int));
 #endif
