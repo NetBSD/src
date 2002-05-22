c2c2980f422726a3a379fe8c9d6c75fab6187fda
itojun <itojun@NetBSD.org>
2002-05-22 07:31:40 +0000

    change return type of pidfile(3) to int (-1 on error), to inform caller of errors.  behavior now in sync with openbsd implementation. (see tech-userlevel and bsd-api-discuss)

--- include/util.h
+++ include/util.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: util.h,v 1.21 2001/08/18 19:31:47 ad Exp $	*/
+/*	$NetBSD: util.h,v 1.22 2002/05/22 07:31:40 itojun Exp $	*/
 
 /*-
  * Copyright (c) 1995
@@ -72,7 +72,7 @@ void		logwtmp(const char *, const char *, const char *);
 int		opendisk(const char *, int, char *, size_t, int);
 int		openpty(int *, int *, char *, struct termios *,
 			struct winsize *);
-void		pidfile(const char *);
+int		pidfile(const char *);
 int		pidlock(const char *, int, pid_t *, const char *);
 int		pw_abort(void);
 void		pw_copy(int, int, struct passwd *, struct passwd *);
