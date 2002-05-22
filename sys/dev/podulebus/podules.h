a483c555eff0b0c6593250d7a92d1082404ede7b
bjh21 <bjh21@NetBSD.org>
2002-05-22 22:43:13 +0000

    Substantial overhaul of podule IDs.  Unlike on PCI or USB, podule IDs are assigned by RISCOS Ltd (and were assigned by Acorn) to be unique across all manufacturers.  This means that associating each one with a manufacturer (and checking the manufacturer when attaching) is bogus.  Thus, we don't do that any more.
    This should have the pleasant side-effect of getting APDL IDE interfaces
    working, since they're just ICS ones with a different manufacturer ID.

--- sys/dev/podulebus/podules.h
+++ sys/dev/podulebus/podules.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: podules.h,v 1.7 2002/05/19 22:34:07 bjh21 Exp $	*/
+/*	$NetBSD: podules.h,v 1.8 2002/05/22 22:43:15 bjh21 Exp $	*/
 
 /*
  * THIS FILE AUTOMATICALLY GENERATED.  DO NOT EDIT.
@@ -76,118 +76,78 @@
 #define	MANUFACTURER_EESOX	0x0064		/* EESOX */
 
 /*
- * List of known podules.  Grouped by vendor.
+ * List of known podules.
  */
 
-#define	PODULE_ACORN_ETHER3XXX	0x0000		/* Ether3 (NOROM) */
-#define	PODULE_ACORN_SCSI	0x0002		/* SCSI 1 interface */
-#define	PODULE_ACORN_ETHER1	0x0003		/* ether 1 interface */
-#define	PODULE_ACORN_ROMRAM	0x0005		/* ROM/RAM podule */
-#define	PODULE_ACORN_BBCIO	0x0006		/* BBC IO interface */
-#define	PODULE_ACORN_ST506	0x000b		/* ST506 HD interface */
+#define	PODULE_ACORN_SCSI	0x0002		/* Acorn SCSI interface */
+#define	PODULE_ETHER1	0x0003		/* Ether1 interface */
+#define	PODULE_ROMRAM	0x0005		/* ROM/RAM podule */
+#define	PODULE_BBCIO	0x0006		/* BBC I/O podule */
+#define	PODULE_FAXPACK	0x0007		/* FaxPack modem */
+#define	PODULE_ST506	0x000b		/* ST506 HD interface */
 #define	PODULE_ACORN_MIDI	0x0013		/* MIDI interface */
-#define	PODULE_ACORN_USERMIDI	0x003F		/* User Port/MIDI interface */
-#define	PODULE_ACORN_ETHER2	0x0061		/* ether 2 interface */
-#define	PODULE_ACORN_ETHERI	0x0139		/* EtherI interface */
-
-#define	PODULE_CCONCEPTS_ROMRAM	0x0005		/* ROM/RAM podule */
-#define	PODULE_CCONCEPTS_FAXPACK	0x0007		/* FaxPack modem */
-#define	PODULE_CCONCEPTS_LASERDIRECT	0x0014		/* laser direct (Canon LBP-4) */
-
-#define	PODULE_ARMADILLO_A448	0x0016		/* A448 sound sampler */
-
-/* From an Issue 2.0 ColourCard (others may differ) */
-#define	PODULE_WILDVISION_COLOURCARD	0x0050		/* ColourCard */
-#define	PODULE_WILDVISION_HAWKV9	0x0052		/* hawk v9 mark2 */
-#define	PODULE_WILDVISION_SOUNDSAMPLER	0x0054		/* Sound Sampler */
-#define	PODULE_WILDVISION_CENTRONICS	0x006f		/* Bi-directional Centronics */
-#define	PODULE_WILDVISION_SCANLIGHTV256	0x00cb		/* scanlight video 256 */
-#define	PODULE_WILDVISION_EAGLEM2	0x00cc		/* eagle M2 */
-#define	PODULE_WILDVISION_LARKA16	0x00ce		/* lark A16 */
-#define	PODULE_WILDVISION_MIDIMAX	0x0200		/* MIDI max */
-
-#define	PODULE_HCCS_IDESCSI	0x0022		/* IDE or SCSI interface */
-#define	PODULE_HCCS_ULTIMATE	0x0063		/* Ultimate micropodule carrier */
-
-#define	PODULE_ATOMWIDE_ETHER3	0x00A4		/* ether 3/5 interface */
-
-#define	PODULE_ATOMWIDE2_SERIAL	0x0090		/* multiport serial interface */
-
-#define	PODULE_LINGENUITY_SCSI	0x0095		/* 16 bit SCSI interface */
-#define	PODULE_LINGENUITY_SCSISHARE	0x0096		/* 16 bit SCSIShare interface */
+#define	PODULE_LASERDIRECT	0x0014		/* LaserDirect (Canon LBP-4) */
+#define	PODULE_A448	0x0016		/* A448 sound sampler */
+#define	PODULE_HCCS_IDESCSI	0x0022		/* HCCS IDE or SCSI interface */
+#define	PODULE_CUMANA_SCSI2	0x003a		/* SCSI II interface */
+#define	PODULE_ACORN_USERMIDI	0x003f		/* User Port/MIDI interface */
 #define	PODULE_LINGENUITY_SCSI8	0x0040		/* 8 bit SCSI interface */
-#define	PODULE_LINGENUITY_SCSI8SHARE	0x008c		/* 8 bit SCSIShare interface */
-
-#define	PODULE_IRLAM_24I16	0x00e6		/* 24i16 digitiser */
-#define	PODULE_IRLAM_MMETHERV	0x1234		/* Multi-media/EtherV */
-#define	PODULE_IRLAM_ETHERN	0x5678		/* EtherN interface */
-
+#define	PODULE_ARXE_SCSI	0x0041		/* 16 bit SCSI interface */
+#define	PODULE_COLOURCARD	0x0050		/* ColourCard */
+#define	PODULE_HAWKV9	0x0052		/* Hawk v9 mark2 */
+#define	PODULE_WILDVISION_SOUNDSAMPLER	0x0054		/* Wild Vision Sound Sampler */
+#define	PODULE_DTSOFT_IDE	0x0055		/* IDE interface */
 /* XXX ID 0x0058 is used by Oak ClassNet (EtherO) Ethernet cards */
 #define	PODULE_OAK_SCSI	0x0058		/* 16 bit SCSI interface */
-
-#define	PODULE_BEEBUG_IDE8	0x0097		/* 8 bit IDE */
-
-#define	PODULE_SJ_NEXUS	0x0064		/* Nexus interface (Podule) */
-#define	PODULE_SJ_NEXUSNS	0x008F		/* Nexus interface (A3020 netslot) */
-
+#define	PODULE_ETHER2	0x0061		/* Ether2 interface */
+#define	PODULE_ULTIMATE	0x0063		/* Ultimate micropodule carrier */
+#define	PODULE_WILDVISION_CENTRONICS	0x006f		/* Bi-directional Centronics */
+#define	PODULE_NEXUS	0x0064		/* Nexus interface (Podule) */
 #define	PODULE_MORLEY_SCSI	0x0067		/* SCSI interface */
 #define	PODULE_MORLEY_USERANALOGUE	0x006d		/* User and Analogue ports */
-
-#define	PODULE_HCCS2_USERANALOGUE	0x006e		/* User and Analogue ports */
-
+#define	PODULE_HCCS_USERANALOGUE	0x006e		/* User and Analogue ports */
+#define	PODULE_LINGENUITY_SCSI8SHARE	0x008c		/* 8 bit SCSIShare interface */
 #define	PODULE_VTI_SCSI	0x008d		/* SCSI interface */
-
-#define	PODULE_DTSOFT_IDE	0x0055		/* IDE interface */
-
-#define	PODULE_CUMANA_SCSI2	0x003a		/* SCSI II interface */
+#define	PODULE_NEXUSNS	0x008f		/* Nexus interface (A3020 netslot) */
+#define	PODULE_ATOMWIDE_SERIAL	0x0090		/* multiport serial interface */
+#define	PODULE_LINGENUITY_SCSI	0x0095		/* 16 bit SCSI interface */
+#define	PODULE_LINGENUITY_SCSISHARE	0x0096		/* 16 bit SCSIShare interface */
+#define	PODULE_BEEBUG_IDE8	0x0097		/* 8 bit IDE */
 #define	PODULE_CUMANA_SCSI1	0x00a0		/* SCSI I interface */
-#define	PODULE_CUMANA_SLCD	0x00dd		/* CDFS & SLCD expansion card */
-
+#define	PODULE_ETHER3	0x00a4		/* Ether3/Ether5 interface */
 #define	PODULE_ICS_IDE	0x00ae		/* IDE Interface */
-
 #define	PODULE_SERIALPORT_DUALSERIAL	0x00b9		/* Serial interface */
-
-#define	PODULE_ARXE_SCSI	0x0041		/* 16 bit SCSI interface */
-
-#define	PODULE_ALEPH1_PCCARD	0x00ea		/* PC card */
-
-/* i-cubed's own cards */
-#define	PODULE_ICUBED_ETHERLAN100	0x00cf		/* EtherLan 100-series */
-#define	PODULE_ICUBED_ETHERLAN200	0x00bd		/* EtherLan 200-series */
-#define	PODULE_ICUBED_ETHERLAN500	0x00d4		/* EtherLan 500-series */
-#define	PODULE_ICUBED_ETHERLAN600	0x00ec		/* EtherLan 600-series */
-#define	PODULE_ICUBED_ETHERLAN700	0x012f		/* EtherLan 700-series */
-/* cards made by i-cubed for Acorn */
-#define	PODULE_ICUBED_ETHERLAN100AEH	0x011c		/* AEH77 (EtherLan 102) */
-#define	PODULE_ICUBED_ETHERLAN200AEH	0x011d		/* AEH79 (EtherLan 210) */
-#define	PODULE_ICUBED_ETHERLAN600AEH	0x011e		/* AEH62/78/99 (EtherLan 602) */
-#define	PODULE_ICUBED_ETHERLAN500AEH	0x011f		/* AEH75 (EtherLan 512) */
-#define	PODULE_ICUBED_ETHERLAN700AEH	0x012e		/* AEH98 (EtherLan 700-series) */
-/* XXX Not listed by Design IT. */
-#define	PODULE_ICUBED_ETHERLAN100XXX	0x00c4		/* EtherLan 100??? */
-
-#define	PODULE_BRINI_PORT	0x0000		/* BriniPort intelligent I/O interface */
-#define	PODULE_BRINI_LINK	0x00df		/* BriniLink transputer link adapter */
-
-#define	PODULE_ANT_ETHER3	0x00a4		/* ether 3/5 interface */
-#define	PODULE_ANT_ETHERB	0x00e4		/* ether B network slot interface */
-#define	PODULE_ANT_ETHERM	0x00d8		/* ether M dual interface NIC */
-
-#define	PODULE_CASTLE_SCSI16	0x00f6		/* 8 or 16 bit SCSI2 interface */
+#define	PODULE_ETHERLAN200	0x00bd		/* EtherLan 200-series */
+#define	PODULE_SCANLIGHTV256	0x00cb		/* ScanLight Video 256 */
+#define	PODULE_EAGLEM2	0x00cc		/* Eagle M2 */
+#define	PODULE_LARKA16	0x00ce		/* Lark A16 */
+#define	PODULE_ETHERLAN100	0x00cf		/* EtherLan 100-series */
+#define	PODULE_ETHERLAN500	0x00d4		/* EtherLan 500-series */
+#define	PODULE_ETHERM	0x00d8		/* EtherM dual interface NIC */
+#define	PODULE_CUMANA_SLCD	0x00dd		/* CDFS & SLCD expansion card */
+#define	PODULE_BRINILINK	0x00df		/* BriniLink transputer link adapter */
+#define	PODULE_ETHERB	0x00e4		/* EtherB network slot interface */
+#define	PODULE_24I16	0x00e6		/* 24i16 digitiser */
+#define	PODULE_PCCARD	0x00ea		/* PC card */
+#define	PODULE_ETHERLAN600	0x00ec		/* EtherLan 600-series */
 #define	PODULE_CASTLE_SCSI16SHARE	0x00f3		/* 8 or 16 bit SCSI2Share interface */
-#define	PODULE_CASTLE_ETHERSCSI	0x00f5		/* EtherSCSI */
 #define	PODULE_CASTLE_ETHERSCSISHARE	0x00f4		/* 8 or 16 bit SCSI2Share interface, possibly with Ethernet */
-#define	PODULE_CASTLE_SCSI32	0x012b		/* 32 bit SCSI2 + DMA interface */
-
+#define	PODULE_CASTLE_ETHERSCSI	0x00f5		/* EtherSCSI */
+#define	PODULE_CASTLE_SCSI16	0x00f6		/* 8 or 16 bit SCSI2 interface */
 #define	PODULE_ALSYSTEMS_SCSI	0x0107		/* SCSI II host adapter */
-
+#define	PODULE_RAPIDE	0x0114		/* RapIDE32 interface */
+#define	PODULE_ETHERLAN100AEH	0x011c		/* AEH77 (EtherLan 102) */
+#define	PODULE_ETHERLAN200AEH	0x011d		/* AEH79 (EtherLan 210) */
+#define	PODULE_ETHERLAN600AEH	0x011e		/* AEH62/78/99 (EtherLan 602) */
+#define	PODULE_ETHERLAN500AEH	0x011f		/* AEH75 (EtherLan 512) */
+#define	PODULE_CONNECT32	0x0125		/* Connect32 SCSI II interface */
+#define	PODULE_CASTLE_SCSI32	0x012b		/* 32 bit SCSI2 + DMA interface */
+#define	PODULE_ETHERLAN700AEH	0x012e		/* AEH98 (EtherLan 700-series) */
+#define	PODULE_ETHERLAN700	0x012f		/* EtherLan 700-series */
 #define	PODULE_SIMTEC_IDE8	0x0130		/* 8 bit IDE interface */
 #define	PODULE_SIMTEC_IDE	0x0131		/* 16 bit IDE interface */
-
-#define	PODULE_YES_RAPIDE	0x0114		/* RapIDE32 interface */
-
-/* MCS also call themselves ACE (Acorn Computer Entertainment) */
-#define	PODULE_MCS_SCSI	0x0125		/* Connect32 SCSI II interface */
-#define	PODULE_MCS_MIDICONNECT	0x0133		/* Midi-Connect */
-
-#define	PODULE_EESOX_SCSI	0x008c		/* EESOX SCSI II interface */
+#define	PODULE_MIDICONNECT	0x0133		/* Midi-Connect */
+#define	PODULE_ETHERI	0x0139		/* EtherI interface */
+#define	PODULE_MIDIMAX	0x0200		/* MIDI max */
+#define	PODULE_MMETHERV	0x1234		/* Multi-media/EtherV */
+#define	PODULE_ETHERN	0x5678		/* EtherN interface */
