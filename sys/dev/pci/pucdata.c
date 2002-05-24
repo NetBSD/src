69dab978eea7977c783aa5858f39d7e5c10f4ad6
lukem <lukem>
2002-05-24 12:07:46 +0000

    Support  NetMos NM9845 Quad UART and 1284 Printer port. All 4 serial ports and parallel port seem to work.

--- sys/dev/pci/pucdata.c
+++ sys/dev/pci/pucdata.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: pucdata.c,v 1.27 2002/05/13 09:19:57 hannken Exp $	*/
+/*	$NetBSD: pucdata.c,v 1.28 2002/05/24 12:07:46 lukem Exp $	*/
 
 /*
  * Copyright (c) 1998, 1999 Christopher G. Demetriou.  All rights reserved.
@@ -36,7 +36,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: pucdata.c,v 1.27 2002/05/13 09:19:57 hannken Exp $");
+__KERNEL_RCSID(0, "$NetBSD: pucdata.c,v 1.28 2002/05/24 12:07:46 lukem Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -792,6 +792,19 @@ const struct puc_device_description puc_devices[] = {
 		{ PUC_PORT_TYPE_COM, 0x10, 0x00, COM_FREQ },
 		{ PUC_PORT_TYPE_COM, 0x14, 0x00, COM_FREQ },
 		{ PUC_PORT_TYPE_LPT, 0x18, 0x00, 0x00 },
+	    },
+	},
+
+	/* NetMos 4S1P PCI NM9845 : 4S, 1P */
+	{   "NetMos NM9845 Quad UART and 1284 Printer port",
+	    {	0x9710,	0x9845,	0,	0	},
+	    {	0xffff,	0xffff,	0,	0	},
+	    {
+		{ PUC_PORT_TYPE_COM, 0x10, 0x00, COM_FREQ },
+		{ PUC_PORT_TYPE_COM, 0x14, 0x00, COM_FREQ },
+		{ PUC_PORT_TYPE_COM, 0x18, 0x00, COM_FREQ * 4 },
+		{ PUC_PORT_TYPE_COM, 0x1c, 0x00, COM_FREQ * 4 },
+		{ PUC_PORT_TYPE_LPT, 0x20, 0x00, 0x00 },
 	    },
 	},
 
