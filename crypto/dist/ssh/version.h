6e45c3dc27667787c548835782f021914c7c877a
itojun <itojun@NetBSD.org>
2002-05-27 13:45:40 +0000

    bump date for rhosts auth fix

--- crypto/dist/ssh/version.h
+++ crypto/dist/ssh/version.h
@@ -1,8 +1,8 @@
-/*	$NetBSD: version.h,v 1.23 2002/05/13 02:58:19 itojun Exp $	*/
+/*	$NetBSD: version.h,v 1.24 2002/05/27 13:45:40 itojun Exp $	*/
 /* $OpenBSD: version.h,v 1.30 2002/04/23 12:54:10 markus Exp $ */
 
 #define __OPENSSH_VERSION	"OpenSSH_3.2.1"
-#define __NETBSDSSH_VERSION	"NetBSD_Secure_Shell-20020513"
+#define __NETBSDSSH_VERSION	"NetBSD_Secure_Shell-20020527"
 
 /*
  * it is important to retain OpenSSH version identification part, it is
