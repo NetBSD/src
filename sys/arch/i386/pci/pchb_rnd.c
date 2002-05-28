9884d651018b248699a3783e30936e0e531f0c7e
tron <tron@NetBSD.org>
2002-05-28 17:23:07 +0000

    Add hardware random generator support for Intel i845, i850 and i860 chipsets.

--- sys/arch/i386/pci/pchb_rnd.c
+++ sys/arch/i386/pci/pchb_rnd.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: pchb_rnd.c,v 1.9 2001/11/15 07:03:34 lukem Exp $	*/
+/*	$NetBSD: pchb_rnd.c,v 1.10 2002/05/28 17:23:07 tron Exp $	*/
 
 /*
  * Copyright (c) 2000 Michael Shalayeff
@@ -34,7 +34,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: pchb_rnd.c,v 1.9 2001/11/15 07:03:34 lukem Exp $");
+__KERNEL_RCSID(0, "$NetBSD: pchb_rnd.c,v 1.10 2002/05/28 17:23:07 tron Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -70,6 +70,9 @@ pchb_attach_rnd(struct pchb_softc *sc, struct pci_attach_args *pa)
 		case PCI_PRODUCT_INTEL_82815_FULL_HUB:
 		case PCI_PRODUCT_INTEL_82820_MCH:
 		case PCI_PRODUCT_INTEL_82840_HB:
+		case PCI_PRODUCT_INTEL_82845_HB:
+		case PCI_PRODUCT_INTEL_82850_HB:
+		case PCI_PRODUCT_INTEL_82860_HB:
 			sc->sc_st = pa->pa_memt;
 			if (bus_space_map(sc->sc_st, I82802_IOBASE,
 			    I82802_IOSIZE, 0, &sc->sc_sh) != 0) {
