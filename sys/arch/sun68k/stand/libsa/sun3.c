40e6d23dd55bd5515ea40b3235441f343cf6692d
tv <tv>
2002-05-28 23:11:28 +0000

    Pull up revision 1.3 (requested by nathanw in ticket #52): Deliberately #include the appropriate arch-specific param.h to get the correct value for NBSG, even when building with a different sun68k target.

--- sys/arch/sun68k/stand/libsa/sun3.c
+++ sys/arch/sun68k/stand/libsa/sun3.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: sun3.c,v 1.2 2001/11/30 16:00:27 fredette Exp $	*/
+/*	$NetBSD: sun3.c,v 1.2.10.1 2002/05/28 23:11:28 tv Exp $	*/
 
 /*-
  * Copyright (c) 1998 The NetBSD Foundation, Inc.
@@ -47,6 +47,12 @@
 #define set_pte sun3_set_pte
 #define get_segmap sun3_get_segmap
 #define set_segmap sun3_set_segmap
+
+/* 
+ * We need to get the sun3 NBSG definition, even if we're
+ * building this with a different sun68k target.
+ */
+#include <arch/sun3/include/param.h>
 
 #include <sys/param.h>
 #include <machine/idprom.h>
