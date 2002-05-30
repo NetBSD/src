7d468606ab9231ee2c3b15764c0e933de44d3d63
eeh <eeh@NetBSD.org>
2002-05-30 16:13:06 +0000

    Add sysinst support for the walnut port.

---
+++ distrib/utils/sysinst/arch/walnut/md.c
@@ -0,0 +1,414 @@
+/*	$NetBSD: md.c,v 1.1 2002/05/30 16:13:07 eeh Exp $ */
+
+/*
+ * Copyright 1997,2002 Piermont Information Systems Inc.
+ * All rights reserved.
+ *
+ * Written by Philip A. Nelson for Piermont Information Systems Inc.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *      This product includes software developed for the NetBSD Project by
+ *      Piermont Information Systems Inc.
+ * 4. The name of Piermont Information Systems Inc. may not be used to endorse
+ *    or promote products derived from this software without specific prior
+ *    written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY PIERMONT INFORMATION SYSTEMS INC. ``AS IS''
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL PIERMONT INFORMATION SYSTEMS INC. BE 
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF 
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+/* md.c -- Machine specific code for bebox */
+
+#include <sys/param.h>
+#include <sys/sysctl.h>
+#include <stdio.h>
+#include <util.h>
+#include "defs.h"
+#include "md.h"
+#include "msg_defs.h"
+#include "menu_defs.h"
+
+
+int mbr_present;
+int c1024_resp;
+char mbr[512];
+
+/* prototypes */
+
+
+int md_get_info()
+{
+
+	read_mbr(diskdev, mbr, sizeof mbr);
+	md_bios_info(diskdev);
+	return edit_mbr((struct mbr_partition *) &mbr[MBR_PARTOFF]);
+}
+
+int md_pre_disklabel()
+{
+	printf ("%s", msg_string (MSG_dofdisk));
+
+	/* write edited MBR onto disk. */
+	if (write_mbr(diskdev, mbr, sizeof mbr, 1) != 0) {
+		msg_display(MSG_wmbrfail);
+		process_menu(MENU_ok);
+		return 1;
+	}
+	return 0;
+}
+
+int md_post_disklabel(void)
+{
+	/* Sector forwarding / badblocks ... */
+	if (*doessf) {
+		printf ("%s", msg_string (MSG_dobad144));
+		return run_prog(RUN_DISPLAY, NULL, "/usr/sbin/bad144 %s 0",
+		    diskdev);
+	}
+	return 0;
+}
+
+int md_post_newfs(void)
+{
+/* Walnut don't do bootblocks */
+#if 0
+	/* boot blocks ... */
+	printf (msg_string(MSG_dobootblks), diskdev);
+	run_prog (RUN_DISPLAY, NULL,
+	    "/usr/mdec/installboot -v /usr/mdec/biosboot.sym "
+		  "/dev/r%sa", diskdev);
+#endif
+	return 0;
+}
+
+int md_copy_filesystem (void)
+{
+	if (target_already_root()) {
+		return 0;
+	}
+
+	/* Copy the instbin(s) to the disk */
+	printf ("%s", msg_string(MSG_dotar));
+	if (run_prog (RUN_DISPLAY, NULL, "pax -X -r -w -pe / /mnt") != 0)
+		return 1;
+
+	/* Copy next-stage install profile into target /.profile. */
+	if (cp_to_target ("/tmp/.hdprofile", "/.profile")!= 0)
+		return 1;
+	return cp_to_target ("/usr/share/misc/termcap", "/.termcap");
+}
+
+
+
+int md_make_bsd_partitions (void)
+{
+	FILE *f;
+	int i;
+	int part;
+	int maxpart = getmaxpartitions();
+	int remain;
+	char isize[20];
+
+editlab:
+	/* Ask for layout type -- standard or special */
+	msg_display (MSG_layout,
+			(1.0*fsptsize*sectorsize)/MEG,
+			(1.0*minfsdmb*sectorsize)/MEG,
+			(1.0*minfsdmb*sectorsize)/MEG+rammb+XNEEDMB);
+	process_menu (MENU_layout);
+
+	if (layoutkind == 3) {
+		ask_sizemult(dlcylsize);
+	} else {
+		sizemult = MEG / sectorsize;
+		multname = msg_string(MSG_megname);
+	}
+
+
+	/* Build standard partitions */
+	emptylabel(bsdlabel);
+
+	/* Partitions C and D are predefined. */
+	bsdlabel[C].pi_fstype = FS_UNUSED;
+	bsdlabel[C].pi_offset = ptstart;
+	bsdlabel[C].pi_size = fsptsize;
+	
+	bsdlabel[D].pi_fstype = FS_UNUSED;
+	bsdlabel[D].pi_offset = 0;
+	bsdlabel[D].pi_size = fsdsize;
+
+	/* Standard fstypes */
+	bsdlabel[A].pi_fstype = FS_BSDFFS;
+	bsdlabel[B].pi_fstype = FS_SWAP;
+	bsdlabel[E].pi_fstype = FS_UNUSED;
+	bsdlabel[F].pi_fstype = FS_UNUSED;
+	bsdlabel[G].pi_fstype = FS_UNUSED;
+	bsdlabel[H].pi_fstype = FS_UNUSED;
+
+	switch (layoutkind) {
+	case 1: /* standard: a root, b swap, c/d "unused", e /usr */
+	case 2: /* standard X: a root, b swap (big), c/d "unused", e /usr */
+		partstart = ptstart;
+
+		/* Root */
+		i = NUMSEC(20+2*rammb, MEG/sectorsize, dlcylsize) + partstart;
+		partsize = NUMSEC (i/(MEG/sectorsize)+1, MEG/sectorsize,
+				   dlcylsize) - partstart;
+		bsdlabel[A].pi_offset = partstart;
+		bsdlabel[A].pi_size = partsize;
+		bsdlabel[A].pi_bsize = 8192;
+		bsdlabel[A].pi_fsize = 1024;
+		strcpy (fsmount[A], "/");
+		partstart += partsize;
+
+		/* swap */
+		i = NUMSEC(layoutkind * 2 * (rammb < 16 ? 16 : rammb),
+			   MEG/sectorsize, dlcylsize) + partstart;
+		partsize = NUMSEC (i/(MEG/sectorsize)+1, MEG/sectorsize,
+			   dlcylsize) - partstart;
+		bsdlabel[B].pi_offset = partstart;
+		bsdlabel[B].pi_size = partsize;
+		partstart += partsize;
+
+		/* /usr */
+		partsize = fsptsize - (partstart - ptstart);
+		bsdlabel[E].pi_fstype = FS_BSDFFS;
+		bsdlabel[E].pi_offset = partstart;
+		bsdlabel[E].pi_size = partsize;
+		bsdlabel[E].pi_bsize = 8192;
+		bsdlabel[E].pi_fsize = 1024;
+		strcpy (fsmount[E], "/usr");
+
+		break;
+
+	case 3: /* custom: ask user for all sizes */
+		ask_sizemult(dlcylsize);
+		partstart = ptstart;
+		remain = fsptsize;
+
+		/* root */
+		i = NUMSEC(20+2*rammb, MEG/sectorsize, dlcylsize) + partstart;
+		partsize = NUMSEC (i/(MEG/sectorsize)+1, MEG/sectorsize,
+				   dlcylsize) - partstart;
+		snprintf (isize, 20, "%d", partsize/sizemult);
+		msg_prompt (MSG_askfsroot, isize, isize, 20,
+			    remain/sizemult, multname);
+		partsize = NUMSEC(atoi(isize),sizemult, dlcylsize);
+		bsdlabel[A].pi_offset = partstart;
+		bsdlabel[A].pi_size = partsize;
+		bsdlabel[A].pi_bsize = 8192;
+		bsdlabel[A].pi_fsize = 1024;
+		strcpy (fsmount[A], "/");
+		partstart += partsize;
+		remain -= partsize;
+		
+		/* swap */
+		i = NUMSEC( 2 * (rammb < 16 ? 16 : rammb),
+			   MEG/sectorsize, dlcylsize) + partstart;
+		partsize = NUMSEC (i/(MEG/sectorsize)+1, MEG/sectorsize,
+			   dlcylsize) - partstart;
+		snprintf (isize, 20, "%d", partsize/sizemult);
+		msg_prompt_add (MSG_askfsswap, isize, isize, 20,
+			    remain/sizemult, multname);
+		partsize = NUMSEC(atoi(isize),sizemult, dlcylsize);
+		bsdlabel[B].pi_offset = partstart;
+		bsdlabel[B].pi_size = partsize;
+		partstart += partsize;
+		remain -= partsize;
+		
+		/* Others E, F, G, H */
+		part = E;
+		if (remain > 0)
+			msg_display (MSG_otherparts);
+		while (remain > 0 && part <= H) {
+			partsize = remain;
+			snprintf (isize, 20, "%d", partsize/sizemult);
+			msg_prompt_add (MSG_askfspart, isize, isize, 20,
+					diskdev, partition_name(part),
+					remain/sizemult, multname);
+			partsize = NUMSEC(atoi(isize),sizemult, dlcylsize);
+			if (partsize > 0) {
+				if (remain - partsize < sizemult)
+					partsize = remain;
+				bsdlabel[part].pi_fstype = FS_BSDFFS;
+				bsdlabel[part].pi_offset = partstart;
+				bsdlabel[part].pi_size = partsize;
+				bsdlabel[part].pi_bsize = 8192;
+				bsdlabel[part].pi_fsize = 1024;
+				if (part == E)
+					strcpy (fsmount[E], "/usr");
+				msg_prompt_add (MSG_mountpoint, fsmount[part],
+						fsmount[part], 20);
+				partstart += partsize;
+				remain -= partsize;
+			}
+			part++;
+		}
+		
+		break;
+	}
+
+	/*
+	 * OK, we have a partition table. Give the user the chance to
+	 * edit it and verify it's OK, or abort altogether.
+	 */
+	if (edit_and_check_label(bsdlabel, maxpart, RAW_PART, RAW_PART) == 0) {
+		msg_display(MSG_abort);
+		return 0;
+	}
+
+	if ((bsdlabel[A].pi_offset + bsdlabel[A].pi_bsize) / bcylsize > 1024) {
+		process_menu(MENU_cyl1024);
+		/* XXX UGH! need arguments to process_menu */
+		switch (c1024_resp) {
+		case 1:
+			edit_mbr((struct mbr_partition *) &mbr[MBR_PARTOFF]);
+			/*FALLTHROUGH*/
+		case 2:
+			goto editlab;
+		default:
+			break;
+		}
+	}
+
+	/* Disk name */
+	msg_prompt (MSG_packname, "mydisk", bsddiskname, DISKNAME_SIZE);
+
+	/* Create the disktab.preinstall */
+	run_prog (0, NULL, "cp /etc/disktab.preinstall /etc/disktab");
+#ifdef DEBUG
+	f = fopen ("/tmp/disktab", "a");
+#else
+	f = fopen ("/etc/disktab", "a");
+#endif
+	if (f == NULL) {
+		endwin();
+		(void) fprintf (stderr, "Could not open /etc/disktab");
+		exit (1);
+	}
+	(void)fprintf (f, "%s|NetBSD installation generated:\\\n", bsddiskname);
+	(void)fprintf (f, "\t:dt=%s:ty=winchester:\\\n", disktype);
+	(void)fprintf (f, "\t:nc#%d:nt#%d:ns#%d:\\\n", dlcyl, dlhead, dlsec);
+	(void)fprintf (f, "\t:sc#%d:su#%d:\\\n", dlhead*dlsec, dlsize);
+	(void)fprintf (f, "\t:se#%d:%s\\\n", sectorsize, doessf);
+	for (i=0; i<8; i++) {
+		(void)fprintf (f, "\t:p%c#%d:o%c#%d:t%c=%s:",
+			       'a'+i, bsdlabel[i].pi_size,
+			       'a'+i, bsdlabel[i].pi_offset,
+			       'a'+i, fstypenames[bsdlabel[i].pi_fstype]);
+		if (bsdlabel[i].pi_fstype == FS_BSDFFS)
+			(void)fprintf (f, "b%c#%d:f%c#%d",
+				       'a'+i, bsdlabel[i].pi_bsize,
+				       'a'+i, bsdlabel[i].pi_fsize);
+		if (i < 7)
+			(void)fprintf (f, "\\\n");
+		else
+			(void)fprintf (f, "\n");
+	}
+	fclose (f);
+
+	/* Everything looks OK. */
+	return (1);
+}
+
+
+/* Upgrade support */
+int
+md_update (void)
+{
+	endwin();
+	md_copy_filesystem ();
+	md_post_newfs();
+	wrefresh(curscr);
+	wmove(stdscr, 0, 0);
+	wclear(stdscr);
+	wrefresh(stdscr);
+	return 1;
+}
+
+void
+md_cleanup_install(void)
+{
+	char realfrom[STRSIZE];
+	char realto[STRSIZE];
+	char sedcmd[STRSIZE];
+
+	strncpy(realfrom, target_expand("/etc/rc.conf"), STRSIZE);
+	strncpy(realto, target_expand("/etc/rc.conf.install"), STRSIZE);
+	sprintf(sedcmd, "sed 's/rc_configured=NO/rc_configured=YES/' < %s > %s",
+	    realfrom, realto);
+	scripting_fprintf(log, "%s\n", sedcmd);
+	do_system(sedcmd);
+
+	run_prog(RUN_FATAL, NULL, "mv -f %s %s", realto, realfrom);
+	run_prog(0, NULL, "rm -f %s", target_expand("/sysinst"));
+	run_prog(0, NULL, "rm -f %s", target_expand("/.termcap"));
+	run_prog(0, NULL, "rm -f %s", target_expand("/.profile"));
+}
+
+int
+md_pre_update()
+{
+	return 1;
+}
+
+void
+md_init()
+{
+}
+
+int
+md_bios_info(dev)
+	char *dev;
+{
+	int cyl, head, sec;
+
+	msg_display(MSG_nobiosgeom, dlcyl, dlhead, dlsec);
+	if (guess_biosgeom_from_mbr(mbr, &cyl, &head, &sec) >= 0) {
+		msg_display_add(MSG_biosguess, cyl, head, sec);
+		set_bios_geom(cyl, head, sec);
+	} else
+		set_bios_geom(dlcyl, dlhead, dlsec);
+	bsize = bcyl * bhead * bsec;
+	bcylsize = bhead * bsec;
+	return 0;
+}
+
+
+void
+md_set_sizemultname()
+{
+
+	set_sizemultname_meg();
+}
+
+void
+md_set_no_x()
+{
+
+	toggle_getit (8);
+	toggle_getit (9);
+	toggle_getit (10);
+	toggle_getit (11);
+	toggle_getit (12);
+	toggle_getit (13);
+}
---
+++ distrib/utils/sysinst/arch/walnut/msg.md.pl
@@ -0,0 +1,218 @@
+/*	$NetBSD: msg.md.pl,v 1.1 2002/05/30 16:13:11 eeh Exp $	*/
+/*	Based on english version: */
+/*	NetBSD: msg.md.en,v 1.1 2001/10/24 19:43:40 briggs Exp */
+
+/*
+ * Copyright 1997 Piermont Information Systems Inc.
+ * All rights reserved.
+ *
+ * Written by Philip A. Nelson for Piermont Information Systems Inc.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *      This product includes software developed for the NetBSD Project by
+ *      Piermont Information Systems Inc.
+ * 4. The name of Piermont Information Systems Inc. may not be used to endorse
+ *    or promote products derived from this software without specific prior
+ *    written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY PIERMONT INFORMATION SYSTEMS INC. ``AS IS''
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL PIERMONT INFORMATION SYSTEMS INC. BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+/* MD Message catalog -- Polish, sandpoint version */
+
+message md_hello
+{Jesli uruchomiles komputer z dyskietki, mozesz ja teraz wyciagnac.
+
+}
+
+message diagcyl
+{Niektore stare BIOSy lub programy diagnostyczne moga uzywac ostatniego
+cylindra dysku w celach testowych. Nie powinno to byc problemem w nowoczesnych
+systemach. Jesli masz stary system i nie jestes pewnien czy moze on tak dzialac,
+mozesz wylaczyc ostatni cylinder dysku z uzycia.
+
+Jesli to zrobisz, zarowno geometria rzeczywista jak i BIOS zostanie dopasowana, 
+nie musisz jej ponownie wprowadzac.
+
+Czy chcesz wylaczyc ostatni cylinder z uzycia?
+
+}
+
+message fullpart
+{Zainstalujemy teraz NetBSD na dysku %s. Mozesz wybrac, czy chcesz 
+zainstalowac NetBSD na calym dysku, czy tylko na jego czesci.
+
+Instalacja na czesci dysku, tworzy partycje, lub 'plaster', dla NetBSD
+w tablicy partycji MBR twojego dysku. Instalacja na calym dysku jest
+`zdecydowanie polecana': zabiera ona caly MBR. Spowoduje to calkowita
+utrate danych na dysku. Uniemozliwia ona take pozniejsza instalacje kilku
+systemow na tym dysku (chyba, ze nadpiszesz NetBSD i przeinstalujesz uzywajac
+tylko czesci dysku).
+
+Ktora instalacje chcesz zrobic?
+}
+
+message wdtype
+{Jakim rodzajem dysku jest %s?}
+
+message sectforward
+{Czy twoj dysk przesuwa AUTOMATYCZNIE sektory?}
+
+message dlgeom
+{Wyglada na to, ze twoj dysk, %s, zostal juz skonfigurowany za pomoca
+BSD disklabel i disklabel raportuje, ze geometria jest inna od prawdziwej.
+Te dwie geometrie to:
+
+disklabel:		%d cylindrow, %d glowic, %d sektorow 
+prawdziwa geometria:	%d cylindrow, %d glowic, %d sektorow 
+}
+
+/* the %s's will expand into three character strings */
+message part_header
+{   Calkowity rozmiar dysku %d sektorow.
+
+    Pocz(%3s) Rozm(%3s)  Koniec(%3s)Rodzaj
+   ---------- ---------- ---------- ----
+}
+
+message part_row_start_unused
+{%-1d:                                 }
+
+message part_row_start_used
+{%-1d: %-10d %-10d %-10d}
+
+message part_row_end
+{ %s\n}
+
+message setbiosgeom
+{Zostaniesz poproszony o podanie geometrii. Podaj wartosci jakie chcesz.
+Ilosc cylindrow powinna byc <= 1024 a ilosc sektorow <= 63. Jesli twoj
+BIOS jest ustawiony aby obslugiwac > 1024 cylindry po prostu zmniejsz
+tutaj ta ilosc do 1024; NetBSD rozpozna reszte cylindrow.
+
+}
+
+message confirmbiosgeom
+{Sprawdz czy geometria dysku z BIOS ponizej jest poprawna. Mozliwe ze
+ilosc cylindrow zostala zmniejszona do 1024. Jest to w porzadku o ile
+reszta parametrow jest poprawna; tylko 1024 cylindry moga byc podane
+w MBR, reszta zostanie odnaleziona przez NetBSD w inny sposob.
+
+Jesli poprawiles wartosci, upewnij sie ze sa one poprawne i odpowiadaja
+tym uzywanym przez inne systemy na tym dysku. Wartosci, ktore sa nie poprawne
+moga spowodowac utrate danych.
+
+}
+
+message badgeom
+{Aktualne wartosci dla geometrii twojego dysku to:
+
+}
+
+message realgeom
+{praw. geo: %d cyl, %d glowic, %d sek  (tylko dla porownania)\n}
+
+message biosgeom
+{BIOS geom: %d cyl, %d glowic, %d sek\n}
+
+message reentergeom
+{Wartosci podane dla geometrii sa nieprawidlowe. Sprawdz i podaj
+je jeszcze raz.
+}
+
+message ovrwrite
+{Twoj dysk aktualnie posiada partycje nie-NetBSD. Czy napewno chcesz ja
+nadpisac z NetBSD?
+}
+
+message parttable
+{Aktualnie tablica partycji na twoim dysku wyglada tak:
+}
+
+message editpart
+{Partycja %d wyglada tak:\n\n}
+
+message editparttable
+{Wyedytuj DOSowa tablice partycji. Podswietlona partycja jest aktualnie
+aktywna. Tablica partycji wyglada tak:
+
+}
+
+message reeditpart
+{Partycje MBR sie nakladaja, lub jest wiecej niz jedna partycja NetBSD.
+Powinienes zrekonfigurowac tablice partycji MBR.
+
+Czy chcesz ja przekonfigurowac?
+}
+
+message nobsdpart
+{Nie ma partycji NetBSD w tablicy partycji MBR.}
+
+message multbsdpart
+{W tablicy partycji MBR znajduje sie kilka partycji NetBSD.
+Zostanie uzyta partycja %d.}
+
+message dofdisk
+{Konfigurowanie DOSowej tablicy partycji ...
+}
+
+message dobad144
+{Instalowanie tablicy zlych blokow ...
+}
+
+message dobootblks
+{Instalowanie bootblokow na %s....
+}
+
+message askfsroot
+{Bede pytal o informacje o partycjach.
+
+Najpierw partycja glowna. Masz %d %s wolnego miejsca na dysku.
+
+Rozmiar partycji glownej? }
+
+message askfsswap
+{
+Nastepnie partycja wymiany. Masz %d %s wolnego miejsca na dysku.
+
+Rozmiar partycji wymiany? }
+
+message otherparts
+{Nadal masz wolna przestrzen na dysku. Podaj rozmiary i punkty montazu
+dla ponizszych partycji.
+
+}
+
+message askfspart
+{Nastepna partycja jest /dev/%s%c. Masz %d %s wolnego miejsca na dysku.
+
+Rozmiar partycji? }
+
+message cyl1024
+{Disklabel (zestaw partycji) ktory skonfigurowales ma glowna partycje, ktora
+konczy sie poza 1024 cylindrem BIOS. Aby byc pewnym, ze system bedzie
+mogl sie zawsze uruchomic, cala glowna partycja powinna znajdowac sie ponizej
+tego ograniczenia. Mozesz ponadto: }
+
+message wmbrfail
+{Nadpisanie MBR nie powiodlo sie. Nie moge kontynuowac.}
