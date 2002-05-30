30a71a578b7d1cd98d2424cd56a707175b49e86d
itojun <itojun>
2002-05-30 21:55:14 +0000

    comment

--- share/mk/bsd.endian.mk
+++ share/mk/bsd.endian.mk
@@ -1,9 +1,9 @@
-#	$NetBSD: bsd.endian.mk,v 1.1 2002/05/30 21:40:48 itojun Exp $
+#	$NetBSD: bsd.endian.mk,v 1.2 2002/05/30 21:55:14 itojun Exp $
 
 .include <bsd.init.mk>
 
-# find out endianness of target and set proper flag for pwd_mkdb so that
-# it creates database in same endianness.
+# find out endianness of target and set proper flag for pwd_mkdb and such,
+# so that it creates database in same endianness.
 .if exists(${DESTDIR}/usr/include/sys/endian.h)
 TARGET_ENDIANNESS!= \
 	printf '\#include <sys/endian.h>\n_BYTE_ORDER\n' | \
