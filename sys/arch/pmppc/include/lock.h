feb02d9a1027c4f1f63fd171cf634c0abf48ba1a
augustss <augustss@NetBSD.org>
2002-05-30 08:51:06 +0000

    Add some files for the Artesyn PMPPC board.  This is a PowerPC based Mezzanine PCI board. NOTE: this is not yet all the files needed for the port.

---
+++ sys/arch/pmppc/include/lock.h
@@ -0,0 +1,4 @@
+/*	$NetBSD: lock.h,v 1.1 2002/05/30 08:51:25 augustss Exp $	*/
+
+/* Just use the common PowerPC definition */
+#include <powerpc/lock.h>
---
+++ sys/arch/pmppc/include/pte.h
@@ -0,0 +1,3 @@
+/*	$NetBSD: pte.h,v 1.1 2002/05/30 08:51:30 augustss Exp $	*/
+
+#include <powerpc/pte.h>
