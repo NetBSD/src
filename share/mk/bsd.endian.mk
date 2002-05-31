168d9ddedf460d1a2b2ef5aa9a53b4a43a430b02
tv <tv@NetBSD.org>
2002-05-31 00:03:52 +0000

    Pull up revision 1.2 (requested by itojun in ticket #100): comment

--- share/mk/bsd.endian.mk
+++ share/mk/bsd.endian.mk
@@ -1,9 +1,9 @@
-#	$NetBSD: bsd.endian.mk,v 1.2.2.2 2002/05/30 23:59:29 tv Exp $
+#	$NetBSD: bsd.endian.mk,v 1.2.2.3 2002/05/31 00:03:52 tv Exp $
 
 .include <bsd.init.mk>
 
-# find out endianness of target and set proper flag for pwd_mkdb so that
-# it creates database in same endianness.
+# find out endianness of target and set proper flag for pwd_mkdb and such,
+# so that it creates database in same endianness.
 .if exists(${DESTDIR}/usr/include/sys/endian.h)
 TARGET_ENDIANNESS!= \
 	printf '\#include <sys/endian.h>\n_BYTE_ORDER\n' | \
