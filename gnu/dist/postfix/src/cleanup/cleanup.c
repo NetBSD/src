ab13a2b0d3a1033d95b2a655eca7871b3c3a2f49
kim <kim@NetBSD.org>
2002-06-01 22:55:04 +0000

    Provide a switch to turn off envelope recipient canonicalization. Canonicalizing the recipient is usually undesirable for sites that have local delivery distributed over multiple machines.

--- gnu/dist/postfix/src/cleanup/cleanup.c
+++ gnu/dist/postfix/src/cleanup/cleanup.c
@@ -281,6 +281,7 @@ int     main(int argc, char **argv)
      * Pass control to the single-threaded service skeleton.
      */
     single_server_main(argc, argv, cleanup_service,
+		       MAIL_SERVER_BOOL_TABLE, cleanup_bool_table,
 		       MAIL_SERVER_INT_TABLE, cleanup_int_table,
 		       MAIL_SERVER_STR_TABLE, cleanup_str_table,
 		       MAIL_SERVER_TIME_TABLE, cleanup_time_table,
