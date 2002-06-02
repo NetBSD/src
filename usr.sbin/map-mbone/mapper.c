eacaedbb6cc412a32af1228b31791e65b8195b1a
itojun <itojun@NetBSD.org>
2002-06-02 13:47:02 +0000

    missing memset() for sockaddr.  from deraadt

--- usr.sbin/map-mbone/mapper.c
+++ usr.sbin/map-mbone/mapper.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: mapper.c,v 1.8 2001/09/24 13:22:37 wiz Exp $	*/
+/*	$NetBSD: mapper.c,v 1.9 2002/06/02 13:47:02 itojun Exp $	*/
 
 /* Mapper for connections between MRouteD multicast routers.
  * Written by Pavel Curtis <Pavel@PARC.Xerox.Com>
@@ -921,6 +921,7 @@ int main(argc, argv)
 	struct sockaddr_in addr;
 	int addrlen = sizeof(addr);
 
+	memset(&addr, 0, sizeof(addr));
 	addr.sin_family = AF_INET;
 #if (defined(BSD) && (BSD >= 199103))
 	addr.sin_len = sizeof addr;
