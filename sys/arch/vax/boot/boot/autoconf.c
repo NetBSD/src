eac5a5432aadb4a717b67957ef7b7a27516808e8
tv <tv>
2002-06-02 01:20:51 +0000

    Pull up revision 1.19 (requested by ragge in ticket #125): Add 11/780 and 8600 to list for who to create a RPB.

--- sys/arch/vax/boot/boot/autoconf.c
+++ sys/arch/vax/boot/boot/autoconf.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: autoconf.c,v 1.16 2002/02/24 01:04:24 matt Exp $ */
+/*	$NetBSD: autoconf.c,v 1.16.10.1 2002/06/02 01:20:51 tv Exp $ */
 /*
  * Copyright (c) 1994, 1998 Ludd, University of Lule}, Sweden.
  * All rights reserved.
@@ -78,6 +78,8 @@ autoconf()
 #endif
 	switch (vax_boardtype) {
 
+	case VAX_BTYP_780:
+	case VAX_BTYP_790:
 	case VAX_BTYP_8000:
 	case VAX_BTYP_9CC:
 	case VAX_BTYP_9RR:
