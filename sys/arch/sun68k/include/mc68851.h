b9605a3752052997ad0cd742ed2037507f3fc55a
fredette <fredette@NetBSD.org>
2002-06-03 00:56:55 +0000

    Reverted previous commit.  It was meant to fix a problem that was already fixed by nathanw, had I bothered to check.

---
+++ sys/arch/sun68k/include/mc68851.h
@@ -0,0 +1,9 @@
+/*	$NetBSD: mc68851.h,v 1.3 2002/06/03 00:56:55 fredette Exp $	*/
+
+/*
+ * XXX - this is an awful hack that lets libsa/sun3x.h 
+ * include arch/sun3/include/pte3x.h, which wants to 
+ * include machine/mc68851.h.  NEVER INSTALL THIS FILE.
+ * Better solutions gladly accepted.
+ */
+#include <arch/sun3/include/mc68851.h>
