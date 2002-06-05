acdb08a93e60f7d591e37a86de965fe61cea7067
simonb <simonb@NetBSD.org>
2002-06-05 06:18:34 +0000

    Simplify include files that just include <mips/locore.h>.

--- sys/arch/hpcmips/include/locore.h
+++ sys/arch/hpcmips/include/locore.h
@@ -1,18 +1,3 @@
-/*	$NetBSD: locore.h,v 1.1.1.1 1999/09/16 12:23:23 takemura Exp $	*/
-
-/*
- * Copyright 1996 The Board of Trustees of The Leland Stanford
- * Junior University. All Rights Reserved.
- *
- * Permission to use, copy, modify, and distribute this
- * software and its documentation for any purpose and without
- * fee is hereby granted, provided that the above copyright
- * notice appear in all copies.  Stanford University
- * makes no representations about the suitability of this
- * software for any purpose.  It is provided "as is" without
- * express or implied warranty.
- *
- * This file contributed by Jonathan Stone
- */
+/* $NetBSD: locore.h,v 1.2 2002/06/05 06:18:37 simonb Exp $ */
 
 #include <mips/locore.h>
