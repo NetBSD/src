90c90b33f12420d4e8d242ab61fdc990788ce0bd
scw <scw>
2002-06-05 15:55:51 +0000

    Fix a botched pmap_vsid_bitmap[] index. I'm surprised this worked as long as it did.

--- sys/arch/powerpc/mpc6xx/pmap.c
+++ sys/arch/powerpc/mpc6xx/pmap.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: pmap.c,v 1.44 2002/06/01 23:50:56 lukem Exp $	*/
+/*	$NetBSD: pmap.c,v 1.45 2002/06/05 15:55:51 scw Exp $	*/
 /*-
  * Copyright (c) 2001 The NetBSD Foundation, Inc.
  * All rights reserved.
@@ -897,7 +897,7 @@ pmap_pinit(pmap_t pm)
 				entropy = (pmap_vsidcontext >> 20);
 				continue;
 			}
-			i = ffs(~pmap_vsid_bitmap[i]) - 1;
+			i = ffs(~pmap_vsid_bitmap[n]) - 1;
 			mask = 1 << i;
 			hash &= 0xfffff & ~(VSID_NBPW-1);
 			hash |= i;
