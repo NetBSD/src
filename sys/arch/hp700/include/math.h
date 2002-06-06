315551a83cd0e1df9c20a908d6e2b77912a9d7fe
fredette <fredette@NetBSD.org>
2002-06-06 19:48:01 +0000

    Added hp700-specific files.  Still a work in progress.

---
+++ sys/arch/hp700/include/math.h
@@ -0,0 +1,3 @@
+/*	$NetBSD: math.h,v 1.1 2002/06/06 19:48:09 fredette Exp $	*/
+
+#include <hppa/math.h>
---
+++ sys/arch/hp700/include/psl.h
@@ -0,0 +1,3 @@
+/*	$NetBSD: psl.h,v 1.1 2002/06/06 19:48:10 fredette Exp $	*/
+
+#include <hppa/psl.h>
