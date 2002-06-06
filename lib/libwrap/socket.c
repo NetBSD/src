c3fba53bea0837147d7743b923af834bcc8083fb
itojun <itojun@NetBSD.org>
2002-06-06 21:28:49 +0000

    support scoped IPv6 address properly.  PR 16810.

--- lib/libwrap/socket.c
+++ lib/libwrap/socket.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: socket.c,v 1.10 2002/05/24 05:38:20 itojun Exp $	*/
+/*	$NetBSD: socket.c,v 1.11 2002/06/06 21:28:50 itojun Exp $	*/
 
  /*
   * This module determines the type of socket (datagram, stream), the client
@@ -22,7 +22,7 @@
 #if 0
 static char sccsid[] = "@(#) socket.c 1.15 97/03/21 19:27:24";
 #else
-__RCSID("$NetBSD: socket.c,v 1.10 2002/05/24 05:38:20 itojun Exp $");
+__RCSID("$NetBSD: socket.c,v 1.11 2002/06/06 21:28:50 itojun Exp $");
 #endif
 #endif
 
@@ -105,27 +105,12 @@ void    sock_hostaddr(host)
 struct host_info *host;
 {
     struct sockaddr *sa = host->sin;
-    int alen, af;
-    char *ap;
 
     if (!sa)
 	return;
-    switch (af = sa->sa_family) {
-    case AF_INET:
-	ap = (char *)&((struct sockaddr_in *)sa)->sin_addr;
-	alen = sizeof(struct in_addr);
-	break;
-#ifdef INET6
-    case AF_INET6:
-	ap = (char *)&((struct sockaddr_in6 *)sa)->sin6_addr;
-	alen = sizeof(struct in6_addr);
-	break;
-#endif
-    default:
-	return;
-    }
     host->addr[0] = '\0';
-    inet_ntop(af, ap, host->addr, sizeof(host->addr));
+    getnameinfo(sa, sa->sa_len, host->addr, sizeof(host->addr),
+	NULL, 0, NI_NUMERICHOST);
 }
 
 /* sock_hostname - map endpoint address to host name */
@@ -133,95 +118,84 @@ struct host_info *host;
 void    sock_hostname(host)
 struct host_info *host;
 {
-    struct sockaddr *sinp = host->sin;
-    struct hostent *hp;
-    int     i;
-    int af, alen;
-    char *ap;
-    char hbuf[MAXHOSTNAMELEN];
-
-    /*
-     * On some systems, for example Solaris 2.3, gethostbyaddr(0.0.0.0) does
-     * not fail. Instead it returns "INADDR_ANY". Unfortunately, this does
-     * not work the other way around: gethostbyname("INADDR_ANY") fails. We
-     * have to special-case 0.0.0.0, in order to avoid false alerts from the
-     * host name/address checking code below.
-     */
-    if (!sinp)
-	return;
-    switch (af = sinp->sa_family) {
-    case AF_INET:
-	if (((struct sockaddr_in *)sinp)->sin_addr.s_addr == 0)
-	    return;
-	ap = (char *)&((struct sockaddr_in *)sinp)->sin_addr;
-	alen = sizeof(struct in_addr);
-	break;
+    struct sockaddr *sa = host->sin;
+    char h1[NI_MAXHOST], h2[NI_MAXHOST];
+    struct addrinfo hints, *res, *res0;
 #ifdef INET6
-    case AF_INET6:
-	ap = (char *)&((struct sockaddr_in6 *)sinp)->sin6_addr;
-	alen = sizeof(struct in6_addr);
-	/* special case on reverse lookup: mapped addr.  I hate it */
-	if (IN6_IS_ADDR_V4MAPPED((struct in6_addr *)ap)) {
-	    af = AF_INET;
-	    ap += (sizeof(struct in6_addr) - sizeof(struct in_addr));
-	    alen = sizeof(struct in_addr);
-	}
-	break;
+    struct sockaddr_in tmp;
 #endif
-    default:
+
+    if (!sa)
+	return;
+#ifdef INET6
+    /* special case on reverse lookup: mapped addr.  I hate it */
+    if (sa->sa_family == AF_INET6 &&
+        IN6_IS_ADDR_V4MAPPED(&((struct sockaddr_in6 *)sa)->sin6_addr)) {
+	memset(&tmp, 0, sizeof(tmp));
+	tmp.sin_family = AF_INET;
+	tmp.sin_len = sizeof(struct sockaddr_in);
+	memcpy(&tmp.sin_addr,
+	    &((struct sockaddr_in6 *)sa)->sin6_addr.s6_addr[12], 4);
+	sa = (struct sockaddr *)&tmp;
+    }
+#endif
+    if (getnameinfo(sa, sa->sa_len, h1, sizeof(h1), NULL, 0,
+        NI_NUMERICHOST) != 0) {
 	return;
     }
-    if ((hp = gethostbyaddr(ap, alen, af)) != 0) {
-
-	STRN_CPY(host->name, hp->h_name, sizeof(host->name));
-
+    if (getnameinfo(sa, sa->sa_len, host->name, sizeof(host->name), NULL, 0,
+        NI_NAMEREQD) == 0) {
 	/*
 	 * Verify that the address is a member of the address list returned
-	 * by gethostbyname(hostname).
+	 * by getaddrinfo(hostname).
 	 * 
-	 * Verify also that gethostbyaddr() and gethostbyname() return the same
+	 * Verify also that getnameinfo() and getaddrinfo() return the same
 	 * hostname, or rshd and rlogind may still end up being spoofed.
 	 * 
-	 * On some sites, gethostbyname("localhost") returns "localhost.domain".
+	 * On some sites, getaddrinfo("localhost") returns "localhost.domain".
 	 * This is a DNS artefact. We treat it as a special case. When we
-	 * can't believe the address list from gethostbyname("localhost")
+	 * can't believe the address list from getaddrinfo("localhost")
 	 * we're in big trouble anyway.
 	 */
-
-	if ((hp = gethostbyname2(host->name, af)) == 0) {
-
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family = sa->sa_family;
+	hints.ai_socktype = SOCK_DGRAM;	/*dummy*/
+	hints.ai_flags = AI_CANONNAME;
+	if (getaddrinfo(host->name, "0", &hints, &res0) != 0) {
 	    /*
 	     * Unable to verify that the host name matches the address. This
 	     * may be a transient problem or a botched name server setup.
 	     */
 
-	    tcpd_warn("can't verify hostname: gethostbyname2(%s, %d) failed",
-		      host->name, af);
-
-	} else if (STR_NE(host->name, hp->h_name)
-		   && STR_NE(host->name, "localhost")) {
-
+	    tcpd_warn("can't verify hostname: getaddrinfo(%s, %d) failed",
+	        host->name, hints.ai_family);
+	} else if (res0->ai_canonname &&
+	    STR_NE(host->name, res0->ai_canonname) &&
+	    STR_NE(host->name, "localhost")) {
 	    /*
-	     * The gethostbyaddr() and gethostbyname() calls did not return
+	     * The getnameinfo() and getaddrinfo() calls did not return
 	     * the same hostname. This could be a nameserver configuration
 	     * problem. It could also be that someone is trying to spoof us.
 	     */
 
-	    tcpd_warn("host name/name mismatch: %s != %.*s",
-		      host->name, STRING_LENGTH, hp->h_name);
-
+	    tcpd_warn("host name/name mismatch: %s != %s",
+		host->name, res0->ai_canonname);
+	    freeaddrinfo(res0);
 	} else {
-
 	    /*
 	     * The address should be a member of the address list returned by
-	     * gethostbyname(). We should first verify that the h_addrtype
-	     * field is AF_INET, but this program has already caused too much
-	     * grief on systems with broken library code.
+	     * getaddrinfo().
 	     */
 
-	    for (i = 0; hp->h_addr_list[i]; i++) {
-		if (memcmp(hp->h_addr_list[i], (char *) ap, alen) == 0)
-		    return;			/* name is good, keep it */
+	    for (res = res0; res; res = res->ai_next) {
+		if (getnameinfo(res->ai_addr, res->ai_addrlen, h2, sizeof(h2),
+		    NULL, 0, NI_NUMERICHOST) != 0) {
+		    continue;
+		}
+		if (STR_EQ(h1, h2)) {
+		    freeaddrinfo(res0);
+		    return;
+		}
 	    }
 
 	    /*
@@ -230,9 +204,10 @@ struct host_info *host;
 	     * server.
 	     */
 
-	    tcpd_warn("host name/address mismatch: %s != %.*s",
-		      inet_ntop(af, ap, hbuf, sizeof(hbuf)),
-		      STRING_LENGTH, hp->h_name);
+	    tcpd_warn("host name/address mismatch: %s != %s", h1,
+		res0->ai_canonname ? res0->ai_canonname : "?");
+
+	    freeaddrinfo(res0);
 	}
 	/* name is bad, clobber it */
 	(void)strncpy(host->name, paranoid, sizeof(host->name) - 1);
