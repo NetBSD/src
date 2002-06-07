e4f39ff86fc3bcf5fc870367a9e17d0fda3abe81
itojun <itojun@NetBSD.org>
2002-06-07 23:42:41 +0000

    panic() if NULL is passed to ah_sumsiz_xx.  suggested by sam leffler, sync w/kame

--- sys/netinet6/ah_core.c
+++ sys/netinet6/ah_core.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: ah_core.c,v 1.26 2001/11/13 00:56:55 lukem Exp $	*/
+/*	$NetBSD: ah_core.c,v 1.27 2002/06/07 23:42:41 itojun Exp $	*/
 /*	$KAME: ah_core.c,v 1.45 2001/07/26 06:53:14 jinmei Exp $	*/
 
 /*
@@ -35,7 +35,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: ah_core.c,v 1.26 2001/11/13 00:56:55 lukem Exp $");
+__KERNEL_RCSID(0, "$NetBSD: ah_core.c,v 1.27 2002/06/07 23:42:41 itojun Exp $");
 
 #include "opt_inet.h"
 #include "opt_ipsec.h"
@@ -174,7 +174,7 @@ ah_sumsiz_1216(sav)
 	struct secasvar *sav;
 {
 	if (!sav)
-		return -1;
+		panic("ah_sumsiz_1216: null pointer is passed");
 	if (sav->flags & SADB_X_EXT_OLD)
 		return 16;
 	else
@@ -186,7 +186,7 @@ ah_sumsiz_zero(sav)
 	struct secasvar *sav;
 {
 	if (!sav)
-		return -1;
+		panic("ah_sumsiz_zero: null pointer is passed");
 	return 0;
 }
 
