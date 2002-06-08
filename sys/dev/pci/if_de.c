8679e01cb45d5b04f851748ae59c7e28ada96461
yamt <yamt@NetBSD.org>
2002-06-08 17:10:02 +0000

    make an auto const variable static.

--- sys/dev/pci/if_de.c
+++ sys/dev/pci/if_de.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: if_de.c,v 1.103 2001/11/13 07:48:42 lukem Exp $	*/
+/*	$NetBSD: if_de.c,v 1.104 2002/06/08 17:10:02 yamt Exp $	*/
 
 /*-
  * Copyright (c) 1994-1997 Matt Thomas (matt@3am-software.com)
@@ -37,7 +37,7 @@
  *   board which support 21040, 21041, or 21140 (mostly).
  */
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: if_de.c,v 1.103 2001/11/13 07:48:42 lukem Exp $");
+__KERNEL_RCSID(0, "$NetBSD: if_de.c,v 1.104 2002/06/08 17:10:02 yamt Exp $");
 
 #define	TULIP_HDR_DATA
 
@@ -3971,7 +3971,7 @@ tulip_print_abnormal_interrupt(
     const char * const *msgp = tulip_status_bits;
     const char *sep;
     u_int32_t mask;
-    const char thrsh[] = "72|128\0\0\0" "96|256\0\0\0" "128|512\0\0" "160|1024";
+    static const char thrsh[] = "72|128\0\0\0" "96|256\0\0\0" "128|512\0\0" "160|1024";
 
     csr &= (1 << (sizeof(tulip_status_bits)/sizeof(tulip_status_bits[0]))) - 1;
     printf(TULIP_PRINTF_FMT ": abnormal interrupt:", TULIP_PRINTF_ARGS);
