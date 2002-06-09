e5b9c84ba7662c7b1bc2cc109736a0665a70ccea
itojun <itojun@NetBSD.org>
2002-06-09 02:47:03 +0000

    use strchr

--- usr.sbin/rtadvd/config.c
+++ usr.sbin/rtadvd/config.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: config.c,v 1.15 2002/06/08 18:11:46 itojun Exp $	*/
+/*	$NetBSD: config.c,v 1.16 2002/06/09 02:47:29 itojun Exp $	*/
 /*	$KAME: config.c,v 1.62 2002/05/29 10:13:10 itojun Exp $	*/
 
 /*
@@ -507,7 +507,7 @@ makeentry(buf, len, id, string, add)
 	if (add) {
 		char *cp;
 
-		cp = (char *)index(buf, '\0');
+		cp = (char *)strchr(buf, '\0');
 		snprintf(cp, ep - cp, "%d", id);
 	}
 }
