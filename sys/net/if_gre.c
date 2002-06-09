9c614903cd9709d5cdca7880a4cdab7215db2254
itojun <itojun@NetBSD.org>
2002-06-09 17:59:45 +0000

    no need for if_addrlen be 4.  From: Martin Husemann <martin@duskware.de>

--- sys/net/if_gre.c
+++ sys/net/if_gre.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: if_gre.c,v 1.33 2002/06/09 17:32:54 itojun Exp $ */
+/*	$NetBSD: if_gre.c,v 1.34 2002/06/09 17:59:45 itojun Exp $ */
 
 /*
  * Copyright (c) 1998 The NetBSD Foundation, Inc.
@@ -46,7 +46,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: if_gre.c,v 1.33 2002/06/09 17:32:54 itojun Exp $");
+__KERNEL_RCSID(0, "$NetBSD: if_gre.c,v 1.34 2002/06/09 17:59:45 itojun Exp $");
 
 #include "opt_inet.h"
 #include "opt_ns.h"
@@ -144,7 +144,7 @@ gre_clone_create(ifc, unit)
 	sprintf(sc->sc_if.if_xname, "%s%d", ifc->ifc_name, unit);
 	sc->sc_if.if_softc = sc;
 	sc->sc_if.if_type = IFT_OTHER;
-	sc->sc_if.if_addrlen = 4;
+	sc->sc_if.if_addrlen = 0;
 	sc->sc_if.if_hdrlen = 24; /* IP + GRE */
 	sc->sc_if.if_dlt = DLT_NULL;
 	sc->sc_if.if_mtu = GREMTU;
