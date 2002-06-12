da673ec092f5b2bb4ef8439c2575ab18cd9a3ff4
he <he>
2002-06-12 20:45:09 +0000

    Apply patch (requested by he):   Mark this as NetBSD 1.5.3.

--- sys/conf/osrelease.sh
+++ sys/conf/osrelease.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-#	$NetBSD: osrelease.sh,v 1.46.2.15 2002/04/26 23:13:39 he Exp $
+#	$NetBSD: osrelease.sh,v 1.46.2.16 2002/06/12 20:45:09 he Exp $
 #
 # Copyright (c) 1997 The NetBSD Foundation, Inc.
 # All rights reserved.
@@ -39,7 +39,7 @@
 
 # Release number to use
 # Also check __NetBSD_Version__ in sys/sys/param.h if you change this!
-release=1.5.3_RC2
+release=1.5.3
 
 
 case $1 in
