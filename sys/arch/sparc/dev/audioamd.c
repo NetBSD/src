46e70bf6c21d9303bdf7d0254507203f6b7d9cc5
gmcgarry <gmcgarry>
2002-06-13 21:02:45 +0000

    Make this work with AUDIO_C_HANDLER.

--- sys/arch/sparc/dev/audioamd.c
+++ sys/arch/sparc/dev/audioamd.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: audioamd.c,v 1.8 2002/03/11 16:27:01 pk Exp $	*/
+/*	$NetBSD: audioamd.c,v 1.9 2002/06/13 21:02:45 gmcgarry Exp $	*/
 /*	NetBSD: am7930_sparc.c,v 1.44 1999/03/14 22:29:00 jonathan Exp 	*/
 
 /*
@@ -391,9 +391,10 @@ audioamd_start_input(addr, p, cc, intr, arg)
 
 #ifdef AUDIO_C_HANDLER
 int
-am7930hwintr(sc)
-	struct audioamd_softc *au0;
+am7930hwintr(v)
+	void *v;
 {
+	struct audioamd_softc *sc = v;
 	struct auio *au = &sc->sc_au;
 	u_int8_t *d, *e;
 	int k;
@@ -425,7 +426,7 @@ am7930hwintr(sc)
 		}
 	}
 
-	*(au->au_intrcnt)++;
+	au->au_intrcnt.ev_count++;
 	return (1);
 }
 #endif /* AUDIO_C_HANDLER */
