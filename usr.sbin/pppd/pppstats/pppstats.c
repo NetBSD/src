3834b20369f6a0f520f995ce80a30c6a0189c9f8
itojun <itojun@NetBSD.org>
2002-06-18 22:32:16 +0000

    new copyright notice for VJ.  via openbsd.
      update copyright notice.  mckusick says that Van signed the standard
      Berkeley copyright assignment form, therefore the new license should
      be on these files. I just copied in the notice that Kirk mailed me.

--- usr.sbin/pppd/pppstats/pppstats.c
+++ usr.sbin/pppd/pppstats/pppstats.c
@@ -1,36 +1,50 @@
-/*	$NetBSD: pppstats.c,v 1.24 2000/09/23 22:39:40 christos Exp $	*/
+/*	$NetBSD: pppstats.c,v 1.25 2002/06/18 22:32:17 itojun Exp $	*/
 
 /*
  * print PPP statistics:
  * 	pppstats [-a|-d] [-v|-r|-z] [-c count] [-w wait] [interface]
  *
  *   -a Show absolute values rather than deltas
- *   -d Show data rate (kB/s) rather than bytes
+ *   -d Show data rate (KB/s) rather than bytes
  *   -v Show more stats for VJ TCP header compression
  *   -r Show compression ratio
  *   -z Show compression statistics instead of default display
  *
  * History:
- *      perkins@cps.msu.edu: Added compression statistics and alternate 
+ *      perkins@cps.msu.edu: Added compression statistics and alternate
  *                display. 11/94
- *	Brad Parker (brad@cayman.com) 6/92
+ *      Brad Parker (brad@cayman.com) 6/92
  *
- * from the original "slstats" by Van Jacobson
+ * from the original "slstats" by Van Jacobson:
  *
- * Copyright (c) 1989 Regents of the University of California.
- * All rights reserved.
+ * Contributed by Van Jacobson (van@ee.lbl.gov), Dec 31, 1989.
  *
- * Redistribution and use in source and binary forms are permitted
- * provided that the above copyright notice and this paragraph are
- * duplicated in all such forms and that any documentation,
- * advertising materials, and other materials related to such
- * distribution and use acknowledge that the software was developed
- * by the University of California, Berkeley.  The name of the
- * University may not be used to endorse or promote products derived
- * from this software without specific prior written permission.
- * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
- * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
- * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ * Copyright (c) 1989, 1990, 1991, 1992 Regents of the University of
+ * California. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the University nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
  */
 
 #ifndef __STDC__
@@ -42,7 +56,7 @@
 #if 0
 static const char rcsid[] = "Id: pppstats.c,v 1.28 2000/04/24 02:54:18 masputra Exp ";
 #else
-__RCSID("$NetBSD: pppstats.c,v 1.24 2000/09/23 22:39:40 christos Exp $");
+__RCSID("$NetBSD: pppstats.c,v 1.25 2002/06/18 22:32:17 itojun Exp $");
 #endif
 #endif
 
