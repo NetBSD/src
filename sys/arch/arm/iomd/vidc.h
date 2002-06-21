b70d7c0c1d9f4f14f1721673bd1b33625a8833ba
lukem <lukem@NetBSD.org>
2002-06-21 14:49:57 +0000

    Pull up revision 1.4 (requested by bjh21 in ticket #328): Parenthesise arguments to VIDC_BLUE and VIDC_GREEN correctly.

--- sys/arch/arm/iomd/vidc.h
+++ sys/arch/arm/iomd/vidc.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: vidc.h,v 1.3 2002/03/30 17:10:31 reinoud Exp $	*/
+/*	$NetBSD: vidc.h,v 1.3.4.1 2002/06/21 14:49:57 lukem Exp $	*/
 
 /*
  * Copyright (c) 1994,1995 Mark Brinicombe.
@@ -106,8 +106,8 @@ extern int *vidc_base;
 /* VIDC palette macros */
 
 #define VIDC_RED(r)   (r)
-#define VIDC_GREEN(g) (g << 8)
-#define VIDC_BLUE(b)  (b << 16)
+#define VIDC_GREEN(g) ((g) << 8)
+#define VIDC_BLUE(b)  ((b) << 16)
 #define VIDC_COL(r, g, b) (VIDC_RED(r) | VIDC_GREEN(g) | VIDC_BLUE(b))
 
 
