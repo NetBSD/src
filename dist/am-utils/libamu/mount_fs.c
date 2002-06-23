62fd2769a1a7a1a601eb9e071f8615a9465d2fad
tron <tron@NetBSD.org>
2002-06-23 08:37:36 +0000

    Synchronize last change with what is in the official amd sources now as suggested by Erez Zadok in private e-mail.

--- dist/am-utils/libamu/mount_fs.c
+++ dist/am-utils/libamu/mount_fs.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: mount_fs.c,v 1.4 2002/06/14 15:16:54 tron Exp $	*/
+/*	$NetBSD: mount_fs.c,v 1.5 2002/06/23 08:37:36 tron Exp $	*/
 
 /*
  * Copyright (c) 1997-2001 Erez Zadok
@@ -719,11 +719,6 @@ compute_nfs_args(nfs_args_t *nap, mntent_t *mntp, int genflags, struct sockaddr_
   if (nap->maxgrouplist != NULL)
     nap->flags |= MNT2_NFS_OPT_MAXGRPS;
 #endif /* defined(MNT2_NFS_OPT_MAXGRPS) && defined(MNTTAB_OPT_MAXGROUPS) */
-
-#if defined(MNT2_NFS_OPT_XLATECOOKIE) && defined(MNTTAB_OPT_XLATECOOKIE)
-  if (hasmntopt(mntp, MNTTAB_OPT_XLATECOOKIE) != NULL)
-    nap->flags |= MNT2_NFS_OPT_XLATECOOKIE;
-#endif /* defined(MNT2_NFS_OPT_XLATECOOKIE) && defined(MNTTAB_OPT_XLATECOOKIE) */
 
 #ifdef HAVE_FIELD_NFS_ARGS_T_OPTSTR
   nap->optstr = mntp->mnt_opts;
