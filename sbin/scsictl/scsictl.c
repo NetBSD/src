a269dfefff726b55b43ab13c21b603a9de8f6ff9
mjacob <mjacob>
2002-06-26 16:04:11 +0000

    Add a bunch of stuff from Bob Nestor that fixes format to allow one to specify device logical blocksize.
    Furthermore, it also adds an 'immediate' option, which then sends more modern
    devices into formatting in the background. scsictl still waits around and
    sends a TEST UNIT READY every 10 seconds and, based upon the sense data
    it gets back after the invariable CHECK CONDITION, prints out a progress
    indicator (based upon SKSV progress in the sense data).
    
    While we're at it, add a 'tur', 'reserve', 'release', 'start' and 'stop'
    functions to the device commands.
    
    Fix a long standing bug in scsi_command- the value in retsts is an integral
    value- not a bitmask.

--- sbin/scsictl/scsictl.c
+++ sbin/scsictl/scsictl.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: scsictl.c,v 1.15 2002/04/14 03:24:42 tsutsui Exp $	*/
+/*	$NetBSD: scsictl.c,v 1.16 2002/06/26 16:04:14 mjacob Exp $	*/
 
 /*-
  * Copyright (c) 1998 The NetBSD Foundation, Inc.
@@ -79,13 +79,23 @@ struct	scsi_addr dvaddr;		/* SCSI device's address */
 void	device_format __P((int, char *[]));
 void	device_identify __P((int, char *[]));
 void	device_reassign __P((int, char *[]));
+void	device_release __P((int, char *[]));
+void	device_reserve __P((int, char *[]));
 void	device_reset __P((int, char *[]));
+void	device_start __P((int, char *[]));
+void	device_stop __P((int, char *[]));
+void	device_tur __P((int, char *[]));
 
 struct command device_commands[] = {
-	{ "format",	"",			device_format },
+	{ "format",	"[blocksize [immediate]]", 	device_format },
 	{ "identify",	"",			device_identify },
 	{ "reassign",	"blkno [blkno [...]]",	device_reassign },
+	{ "release",	"",			device_release },
+	{ "reserve",	"",			device_reserve },
 	{ "reset",	"",			device_reset },
+	{ "start",	"",			device_start },
+	{ "stop",	"",			device_stop },
+	{ "tur",	"",			device_tur },
 	{ NULL,		NULL,			NULL },
 };
 
@@ -193,39 +203,193 @@ usage()
  * device_format:
  *
  *	Format a direct access device.
- *
- *	XXX Does not handle defect list management or geometry settings.
  */
 void
 device_format(argc, argv)
 	int argc;
 	char *argv[];
 {
+	u_int32_t blksize;
+	int i, j, immediate;
+#define	PC	(65536/10)
+	static int complete[] = {
+	    PC*1, PC*2, PC*3, PC*4, PC*5, PC*6, PC*7, PC*8, PC*9, 65536
+	};
+	char *cp, buffer[64];
+	struct scsipi_sense_data sense;
 	struct scsi_format_unit cmd;
+	struct {
+		struct scsi_format_unit_defect_list_header header;
+		/* optional initialization pattern */
+		/* optional defect list */
+	} dfl;
 	struct {
 		struct scsipi_mode_header header;
 		struct scsi_blk_desc blk_desc;
 		struct page_disk_format format_page;
-	} data;
+	} mode_page;
+	struct {
+		struct scsipi_mode_header header;
+		struct scsi_blk_desc blk_desc;
+	} data_select;
+	
 
-	/* No arguments. */
-	if (argc != 0)
+	/* Blocksize is an optional argument. */
+	if (argc > 2)
 		usage();
+
+	/*
+	 * Loop doing Request Sense to clear any pending Unit Attention.
+	 *
+	 * Multiple conditions may exist on the drive which are returned
+	 * in priority order.
+	 */
+	for (i = 0; i < 8; i++) {
+		scsi_request_sense(fd, &sense, sizeof (sense));
+		if ((j = sense.flags & SSD_KEY) == SKEY_NO_SENSE)
+			break;
+	}
+	/*
+	 * Make sure we cleared any pending Unit Attention
+	 */
+	if (j != SKEY_NO_SENSE) {
+		cp = scsi_decode_sense((const unsigned char *) &sense, 2,
+		    buffer, sizeof (buffer));
+		errx(1, "failed to clean Unit Attention: %s\n", cp);
+	}
 
 	/*
 	 * Get the DISK FORMAT mode page.  SCSI-2 recommends specifying the
 	 * interleave read from this page in the FORMAT UNIT command.
 	 */
-	scsi_mode_sense(fd, 0x03, 0x00, &data, sizeof(data));
+	scsi_mode_sense(fd, 0x03, 0x00, &mode_page, sizeof(mode_page));
+
+	j = (mode_page.format_page.bytes_s[0] << 8) |
+	    (mode_page.format_page.bytes_s[1]);
+
+	if (j != DEV_BSIZE)
+		printf("current disk sector size: %hd\n", j);
 
 	memset(&cmd, 0, sizeof(cmd));
 
 	cmd.opcode = SCSI_FORMAT_UNIT;
-	memcpy(cmd.interleave, data.format_page.interleave,
+	memcpy(cmd.interleave, mode_page.format_page.interleave,
 	    sizeof(cmd.interleave));
 
-	scsi_command(fd, &cmd, sizeof(cmd), NULL, 0, 6 * 60 * 60 * 1000, 0);
+	/*
+	 * The blocksize on the device is only changed if the user
+	 * specified a new blocksize. If not specified the blocksize
+	 * used for the device will be the Default value in the device.
+	 * We don't specify the number of blocks since the format
+	 * command will always reformat the entire drive.  Also by
+	 * not specifying a block count the drive will reset the
+	 * block count to the maximum available after the format
+	 * completes if the blocksize was changed in the format.
+	 * Finally, the new disk geometry will not but updated on
+	 * the drive in permanent storage until _AFTER_ the format
+	 * completes successfully.
+	 */
+	if (argc > 0) {
+		blksize = strtoul(argv[0], &cp, 10);
+		if (*cp != '\0')
+			errx(1, "invalid block size: %s\n", argv[0]);
 
+		memset(&data_select, 0, sizeof(data_select));
+
+		data_select.header.blk_desc_len = sizeof(struct scsi_blk_desc);
+		/*
+		 * blklen in desc is 3 bytes with a leading reserved byte
+		 */
+		_lto4b(blksize, &data_select.blk_desc.reserved);
+
+		/*
+		 * Issue Mode Select to modify the device blocksize to be
+		 * used on the Format.  The modified device geometry will
+		 * be stored as Current and Saved Page 3 parameters when
+		 * the Format completes.
+		 */
+		scsi_mode_select(fd, 0, &data_select, sizeof(data_select));
+
+		/*
+		 * Since user specified a specific block size make sure it
+		 * gets stored in the device when the format completes.
+		 *
+		 * Also scrub the defect list back to the manufacturers
+		 * original.
+		 */
+		cmd.flags = SFU_CMPLST | SFU_FMTDATA;
+	}
+
+	memset(&dfl, 0, sizeof(dfl));
+
+	if (argc > 1 && strncmp(argv[1], "imm", 3) == 0) {
+		/*
+		 * Signal target for an immediate return from Format.
+		 *
+		 * We'll poll for completion status.
+		 */
+		dfl.header.flags = DLH_IMMED;
+		immediate = 1;
+	} else {
+		immediate = 0;
+	}
+
+	scsi_command(fd, &cmd, sizeof(cmd), &dfl, sizeof(dfl),
+	    8 * 60 * 60 * 1000, 0);
+
+	/*
+	 * Poll device for completion of Format
+	 */
+	if (immediate) {
+		i = 0;
+		printf("formatting.");
+		fflush(stdout);
+		do {
+			scsireq_t req;
+			struct scsipi_test_unit_ready tcmd;
+
+			memset(&tcmd, 0, sizeof(cmd));
+			tcmd.opcode = TEST_UNIT_READY;
+
+			memset(&req, 0, sizeof(req));
+			memcpy(req.cmd, &tcmd, 6);
+			req.cmdlen = 6;
+			req.timeout = 10000;
+			req.senselen = SENSEBUFLEN;
+
+			if (ioctl(fd, SCIOCCOMMAND, &req) == -1) {
+				err(1, "SCIOCCOMMAND");
+			}
+
+			if (req.retsts == SCCMD_OK) {
+				break;
+			} else if (req.retsts == SCCMD_TIMEOUT) {
+				fprintf(stderr, "%s: SCSI command timed out",
+				    dvname);
+				break;
+			} else if (req.retsts == SCCMD_BUSY) {
+				fprintf(stderr, "%s: device is busy",
+				    dvname);
+				break;
+			} else if (req.retsts != SCCMD_SENSE) {
+				fprintf(stderr,
+				    "%s: device had unknown status %x", dvname,
+				    req.retsts);
+				break;
+			}
+			memcpy(&sense, req.sense, SENSEBUFLEN);
+			if (sense.sense_key_spec_1 == SSD_SCS_VALID) {
+				j = (sense.sense_key_spec_2 << 8) |
+				    (sense.sense_key_spec_3);
+				if (j >= complete[i]) {
+					printf(".%d0%%.", ++i);
+					fflush(stdout);
+				}
+			}
+			sleep(10);
+		} while ((sense.flags & SSD_KEY) == SKEY_NOT_READY);
+		printf(".100%%..done.\n");
+	}
 	return;
 }
 
@@ -331,6 +495,66 @@ device_reassign(argc, argv)
 }
 
 /*
+ * device_release:
+ *
+ *      Issue a RELEASE command to a SCSI drevice
+ */
+#ifndef	SCSI_RELEASE
+#define	SCSI_RELEASE	0x17
+#endif
+void
+device_release(argc, argv)
+	int argc;
+	char *argv[];
+{
+	struct scsipi_test_unit_ready cmd;	/* close enough */
+
+	/* No arguments. */
+	if (argc != 0)
+		usage();
+
+	memset(&cmd, 0, sizeof(cmd));
+
+	cmd.opcode = SCSI_RELEASE;
+
+	scsi_command(fd, &cmd, sizeof(cmd), NULL, 0, 10000, 0);
+
+	return;
+}
+
+
+
+/*
+ * device_reserve:
+ *
+ *      Issue a RESERVE command to a SCSI drevice
+ */
+#ifndef	SCSI_RESERVE
+#define	SCSI_RESERVE	0x16
+#endif
+void
+device_reserve(argc, argv)
+	int argc;
+	char *argv[];
+{
+	struct scsipi_test_unit_ready cmd;	/* close enough */
+
+	/* No arguments. */
+	if (argc != 0)
+		usage();
+
+	memset(&cmd, 0, sizeof(cmd));
+
+	cmd.opcode = SCSI_RESERVE;
+
+	scsi_command(fd, &cmd, sizeof(cmd), NULL, 0, 10000, 0);
+
+	return;
+}
+
+
+
+/*
  * device_reset:
  *
  *	Issue a reset to a SCSI device.
@@ -375,6 +599,85 @@ bus_reset(argc, argv)
 
 	return;
 }
+
+/*
+ * device_start:
+ *
+ *      Issue a start to a SCSI device.
+ */
+void
+device_start(argc, argv)
+	int argc;
+	char *argv[];
+{
+	struct scsipi_start_stop cmd;
+
+	/* No arguments. */
+	if (argc != 0)
+		usage();
+
+	memset(&cmd, 0, sizeof(cmd));
+
+	cmd.opcode = START_STOP;
+	cmd.how = SSS_START;
+
+	scsi_command(fd, &cmd, sizeof(cmd), NULL, 0, 10000, 0);
+
+	return;
+}
+
+/*
+ * device_stop:
+ *
+ *      Issue a stop to a SCSI device.
+ */
+void
+device_stop(argc, argv)
+	int argc;
+	char *argv[];
+{
+	struct scsipi_start_stop cmd;
+
+	/* No arguments. */
+	if (argc != 0)
+		usage();
+
+	memset(&cmd, 0, sizeof(cmd));
+
+	cmd.opcode = START_STOP;
+	cmd.how = SSS_STOP;
+
+	scsi_command(fd, &cmd, sizeof(cmd), NULL, 0, 10000, 0);
+
+	return;
+}
+
+/*
+ * device_tur:
+ *
+ *      Issue a TEST UNIT READY to a SCSI drevice
+ */
+void
+device_tur(argc, argv)
+	int argc;
+	char *argv[];
+{
+	struct scsipi_test_unit_ready cmd;
+
+	/* No arguments. */
+	if (argc != 0)
+		usage();
+
+	memset(&cmd, 0, sizeof(cmd));
+
+	cmd.opcode = TEST_UNIT_READY;
+
+	scsi_command(fd, &cmd, sizeof(cmd), NULL, 0, 10000, 0);
+
+	return;
+}
+
+
 
 /*
  * bus_scan:
