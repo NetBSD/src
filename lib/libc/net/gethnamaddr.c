6bd94f51d3236186c74551f1045091945cb3b9c2
lukem <lukem@NetBSD.org>
2002-06-26 11:15:53 +0000

    Pull up revision 1.47 (requested by itojun in ticket #376): correct DIAGASSERT

--- lib/libc/net/gethnamaddr.c
+++ lib/libc/net/gethnamaddr.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: gethnamaddr.c,v 1.42.2.2 2002/06/26 11:11:15 lukem Exp $	*/
+/*	$NetBSD: gethnamaddr.c,v 1.42.2.3 2002/06/26 11:15:53 lukem Exp $	*/
 
 /*
  * ++Copyright++ 1985, 1988, 1993
@@ -61,7 +61,7 @@
 static char sccsid[] = "@(#)gethostnamadr.c	8.1 (Berkeley) 6/4/93";
 static char rcsid[] = "Id: gethnamaddr.c,v 8.21 1997/06/01 20:34:37 vixie Exp ";
 #else
-__RCSID("$NetBSD: gethnamaddr.c,v 1.42.2.2 2002/06/26 11:11:15 lukem Exp $");
+__RCSID("$NetBSD: gethnamaddr.c,v 1.42.2.3 2002/06/26 11:15:53 lukem Exp $");
 #endif
 #endif /* LIBC_SCCS and not lint */
 
@@ -996,7 +996,7 @@ map_v4v6_hostent(hp, bpp, ep)
 
 	_DIAGASSERT(hp != NULL);
 	_DIAGASSERT(bpp != NULL);
-	_DIAGASSERT(lenp != NULL);
+	_DIAGASSERT(ep != NULL);
 
 	if (hp->h_addrtype != AF_INET || hp->h_length != INADDRSZ)
 		return;
