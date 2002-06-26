1c300f5935bfa127b3eba2949554b96b01fe1127
he <he>
2002-06-26 20:00:26 +0000

    Apply patch (requested by he):   Pull in a point patch for the preauth security vulnerability,   taken from http://www.openssh.com/txt/preauth.adv.
      Mark NetBSD part with today's date.

--- crypto/dist/ssh/version.h
+++ crypto/dist/ssh/version.h
@@ -1,8 +1,8 @@
-/*	$NetBSD: version.h,v 1.1.1.1.2.9 2002/06/06 16:47:44 he Exp $	*/
+/*	$NetBSD: version.h,v 1.1.1.1.2.10 2002/06/26 20:00:26 he Exp $	*/
 /* $OpenBSD: version.h,v 1.27 2001/12/05 15:04:48 markus Exp $ */
 
 #define __OPENSSH_VERSION	"OpenSSH_3.0.2"
-#define __NETBSDSSH_VERSION	"NetBSD_Secure_Shell-20020606"
+#define __NETBSDSSH_VERSION	"NetBSD_Secure_Shell-20020626"
 
 /*
  * it is important to retain OpenSSH version identification part, it is
