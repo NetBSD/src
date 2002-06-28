e3d3b308eaad5d74a22ac51a7c05e8255226a689
lukem <lukem>
2002-06-28 11:50:48 +0000

    Pull up revision 1.3 (requested by itojun in ticket #387): Update to BIND 8.3.3.  Fixes buffer overrun in resolver code.

--- dist/bind/lib/irs/irp_p.h
+++ dist/bind/lib/irs/irp_p.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: irp_p.h,v 1.2 2000/10/08 20:03:13 is Exp $	*/
+/*	$NetBSD: irp_p.h,v 1.2.2.1 2002/06/28 11:50:48 lukem Exp $	*/
 
 /*
  * Copyright (c) 1996 by Internet Software Consortium.
@@ -18,7 +18,7 @@
  */
 
 /*
- * Id: irp_p.h,v 8.1 1999/01/18 07:46:54 vixie Exp
+ * Id: irp_p.h,v 8.2 2001/08/10 02:40:52 marka Exp
  */
 
 #ifndef _IRP_P_H_INCLUDED
@@ -55,8 +55,7 @@ char *irs_irp_read_body(struct irp_p *pvt, size_t *size);
 int irs_irp_get_full_response(struct irp_p *pvt, int *code,
 			      char *text, size_t textlen,
 			      char **body, size_t *bodylen);
-int irs_irp_send_command(struct irp_p *pvt, const char *fmt, ...)
-     __attribute__((__format__(__printf__, 2, 3)));
+int irs_irp_send_command(struct irp_p *pvt, const char *fmt, ...) ISC_FORMAT_PRINTF(2, 3);
 
 
 extern int irp_log_errors;
