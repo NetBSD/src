e74b5cd15ab4cc67d190f10ecd8addd530ecf8ba
lukem <lukem@NetBSD.org>
2002-06-28 12:43:15 +0000

    Pull up revisions 1.25-1.26 (requested by taca in ticket #369): - fix buffer size checks - define PKG_PATTEN_MAX and PKG_SUFFIX_MAX and   use them instead of constants like 255. - add asserts and buffer size checks. - constify and make WARNS=2 clean. - Fixes provided by Stoned Elipot <seb@script.jussieu.fr> in PR 17323

--- usr.sbin/pkg_install/admin/main.c
+++ usr.sbin/pkg_install/admin/main.c
@@ -1,8 +1,8 @@
-/*	$NetBSD: main.c,v 1.24 2002/03/05 13:01:19 agc Exp $	*/
+/*	$NetBSD: main.c,v 1.24.2.1 2002/06/28 12:43:15 lukem Exp $	*/
 
 #include <sys/cdefs.h>
 #ifndef lint
-__RCSID("$NetBSD: main.c,v 1.24 2002/03/05 13:01:19 agc Exp $");
+__RCSID("$NetBSD: main.c,v 1.24.2.1 2002/06/28 12:43:15 lukem Exp $");
 #endif
 
 /*
@@ -51,6 +51,8 @@ void    usage(void);
 
 int     filecnt;
 int     pkgcnt;
+
+static int checkpattern_fn(const char *, void *);
 
 /*
  * Assumes CWD is in /var/db/pkg/<pkg>!
@@ -307,7 +309,7 @@ checkall(void)
 }
 
 static int
-checkpattern_fn(const char *pkg, char *data)
+checkpattern_fn(const char *pkg, void *vp)
 {
 	int     rc;
 
@@ -324,8 +326,9 @@ checkpattern_fn(const char *pkg, char *data)
 }
 
 static int
-lspattern_fn(const char *pkg, char *data)
+lspattern_fn(const char *pkg, void *vp)
 {
+	char *data = vp;
 	printf("%s/%s\n", data, pkg);
 	
 	return 0;
@@ -471,8 +474,10 @@ main(int argc, char *argv[])
 
 			cwd = getcwd(NULL, 0);
 			p = findbestmatchingname(cwd, base);
-			if (p)
+			if (p) {
 				printf("%s/%s\n", cwd, p);
+				free(p);
+			}
 			free(cwd);
 			
 			argv++;
