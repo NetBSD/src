dc36a8c72a426d4ed583ae400868e50de1332a22
lukem <lukem>
2002-06-28 12:44:06 +0000

    Pull up revision 1.13 (requested by taca in ticket #369): - fix buffer size checks - define PKG_PATTEN_MAX and PKG_SUFFIX_MAX and   use them instead of constants like 255. - add asserts and buffer size checks. - constify and make WARNS=2 clean. - Fixes provided by Stoned Elipot <seb@script.jussieu.fr> in PR 17323

--- usr.sbin/pkg_install/info/info.h
+++ usr.sbin/pkg_install/info/info.h
@@ -1,4 +1,4 @@
-/* $NetBSD: info.h,v 1.12 1999/11/29 19:48:46 hubertf Exp $ */
+/* $NetBSD: info.h,v 1.12.6.1 2002/06/28 12:44:06 lukem Exp $ */
 
 /* from FreeBSD Id: info.h,v 1.10 1997/02/22 16:09:40 peter Exp */
 
@@ -67,6 +67,5 @@ extern void show_plist(char *, package_t *, pl_ent_t);
 extern void show_files(char *, package_t *);
 extern void show_depends(char *, package_t *);
 extern void show_index(char *, char *);
-extern int find_fn(const char *pkg, char *data);
 
 #endif				/* _INST_INFO_H_INCLUDE */
