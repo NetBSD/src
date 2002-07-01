fbf675007ed2d4afee254733908f8cde15cafe33
he <he@NetBSD.org>
2002-07-01 17:15:52 +0000

    Pull up revisions 1.1.1.1-1.1.1.2 (requested by itojun):   Update BIND to version 8.3.3.

---
+++ dist/bind/lib/bsd/putenv.c
@@ -0,0 +1,27 @@
+/*	$NetBSD: putenv.c,v 1.1.1.1.4.2 2002/07/01 17:15:54 he Exp $	*/
+
+#ifndef LINT
+static const char rcsid[] = "Id: putenv.c,v 8.4 1999/10/13 16:39:21 vixie Exp";
+#endif
+
+#include "port_before.h"
+#include "port_after.h"
+
+/*
+ * To give a little credit to Sun, SGI,
+ * and many vendors in the SysV world.
+ */
+
+#if !defined(NEED_PUTENV)
+int __bindcompat_putenv;
+#else
+int
+putenv(char *str) {
+	char *tmp;
+
+	for (tmp = str; *tmp && (*tmp != '='); tmp++)
+		;
+
+	return (setenv(str, tmp, 1));
+}
+#endif
---
+++ dist/bind/lib/bsd/strdup.c
@@ -0,0 +1,20 @@
+/*	$NetBSD: strdup.c,v 1.1.1.1.4.2 2002/07/01 17:15:56 he Exp $	*/
+
+#include "port_before.h"
+
+#include <stdlib.h>
+
+#include "port_after.h"
+
+#ifndef NEED_STRDUP
+int __bind_strdup_unneeded;
+#else
+char *
+strdup(const char *src) {
+	char *dst = malloc(strlen(src) + 1);
+
+	if (dst)
+		strcpy(dst, src);
+	return (dst);
+}
+#endif
