78aa53999048e9d73f187e1c1e92b33d2f7ad5da
christos <christos@NetBSD.org>
2002-07-01 13:55:35 +0000

    Fix iplog problem on sparc64 [from Tomi Nylund] 	1. size_t is 64 bits, so use a u_32_t for iplused 	2. microtime() and friends expect a struct timeval, 	   passing the first of two unsigned longs will not cut it.

--- sys/netinet/ip_fil.h
+++ sys/netinet/ip_fil.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: ip_fil.h,v 1.44 2002/05/02 17:12:04 martti Exp $	*/
+/*	$NetBSD: ip_fil.h,v 1.45 2002/07/01 13:55:35 christos Exp $	*/
 
 /*
  * Copyright (C) 1993-2002 by Darren Reed.
@@ -416,8 +416,7 @@ typedef	struct frgroup {
 typedef	struct	iplog	{
 	u_32_t	ipl_magic;
 	u_int	ipl_count;
-	u_long	ipl_sec;
-	u_long	ipl_usec;
+	struct timeval	ipl_time;
 	size_t	ipl_dsize;
 	struct	iplog	*ipl_next;
 } iplog_t;
@@ -643,7 +642,7 @@ extern	int	fr_minttllog;
 extern	fr_info_t	frcache[2];
 extern	char	ipfilter_version[];
 extern	iplog_t	**iplh[IPL_LOGMAX+1], *iplt[IPL_LOGMAX+1];
-extern	size_t	iplused[IPL_LOGMAX + 1];
+extern	u_32_t	iplused[IPL_LOGMAX + 1];
 extern	struct frentry *ipfilter[2][2], *ipacct[2][2];
 #ifdef	USE_INET6
 extern	struct frentry *ipfilter6[2][2], *ipacct6[2][2];
