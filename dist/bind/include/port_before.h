2e584350c88f933ff9152f3bd8106f516f25b4aa
he <he>
2002-07-01 17:15:30 +0000

    Pull up revision 1.5 (requested by itojun):   Update BIND to version 8.3.3.

--- dist/bind/include/port_before.h
+++ dist/bind/include/port_before.h
@@ -1,14 +1,24 @@
-/*	$NetBSD: port_before.h,v 1.3.4.1 2001/01/28 15:52:48 he Exp $	*/
+/*	$NetBSD: port_before.h,v 1.3.4.2 2002/07/01 17:15:31 he Exp $	*/
 
 #if 0
 #define WANT_IRS_NIS
 #define WANT_IRS_PW
+#define HAVE_PW_CLASS
 #define WANT_IRS_GR
 #endif
 #define SIG_FN void
+#define ISC_SOCKLEN_T int
 #if defined(HAS_PTHREADS) && defined(_REENTRANT)
 #define DO_PTHREADS
 #endif
 
 #define SETGRENT_VOID
 #define SETPWENT_VOID
+#define GETGROUPLIST_ARGS const char *name, gid_t basegid, gid_t *groups, \
+		      int *ngroups
+#ifdef __GNUC__
+#define ISC_FORMAT_PRINTF(fmt, args) \
+	__attribute__((__format__(__printf__, fmt, args)))
+#else
+#define ISC_FORMAT_PRINTF(fmt, args)
+#endif
