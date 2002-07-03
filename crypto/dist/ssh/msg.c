ad7f894c0e96caf038db8c990734a52e420608f4
itojun <itojun@NetBSD.org>
2002-07-03 10:05:58 +0000

    correct signed/unsigned mixup; openbsd

--- crypto/dist/ssh/msg.c
+++ crypto/dist/ssh/msg.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: msg.c,v 1.6 2002/06/26 14:08:31 itojun Exp $	*/
+/*	$NetBSD: msg.c,v 1.7 2002/07/03 10:05:58 itojun Exp $	*/
 /*
  * Copyright (c) 2002 Markus Friedl.  All rights reserved.
  *
@@ -23,7 +23,7 @@
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 #include "includes.h"
-RCSID("$OpenBSD: msg.c,v 1.3 2002/06/24 15:49:22 itojun Exp $");
+RCSID("$OpenBSD: msg.c,v 1.4 2002/07/01 16:15:25 deraadt Exp $");
 
 #include "buffer.h"
 #include "getput.h"
@@ -64,7 +64,7 @@ msg_recv(int fd, Buffer *m)
 	}
 	msg_len = GET_32BIT(buf);
 	if (msg_len > 256 * 1024)
-		fatal("msg_recv: read: bad msg_len %d", msg_len);
+		fatal("msg_recv: read: bad msg_len %u", msg_len);
 	buffer_clear(m);
 	buffer_append_space(m, msg_len);
 	res = atomic_read(fd, buffer_ptr(m), msg_len);
