1f94dd03c1bf7ec5f45d162a9da44a6abc623478
gehenna <gehenna@NetBSD.org>
2002-07-09 06:04:26 +0000

    catch up with -current.

--- sys/arch/amigappc/amigappc/machdep.c
+++ sys/arch/amigappc/amigappc/machdep.c
@@ -1,4 +1,4 @@
-/* $NetBSD: machdep.c,v 1.18 2002/04/23 12:41:04 kleink Exp $ */
+/* $NetBSD: machdep.c,v 1.18.2.1 2002/07/09 06:04:26 gehenna Exp $ */
 
 /*
  * Copyright (C) 1995, 1996 Wolfgang Solfrank.
@@ -597,7 +597,7 @@ identifycpu()
 {
 	register int pvr, hid1;
 	char *mach, *pup, *cpu;
-	const char pll[] = {10, 10, 70, 0, 20, 65, 25, 45,
+	static const char pll[] = {10, 10, 70, 0, 20, 65, 25, 45,
 			30, 55, 40, 50, 15, 60, 35, 0};
 	const char *p5type_p = (const char *)0xf00010;
 	int cpuclock, busclock;
