e347459d9d0153bd62a0256d9dd2a9f51eb7d5b6
drochner <drochner@NetBSD.org>
2002-07-10 19:15:42 +0000

    get some more info out of the VESA BIOS and attach subdevices for 8-bit pseudo color and text modes still doesn't do anything useful (It would be easy to attach a wsdisplay, but we have to cooperate with the PCI or ISA attached VGA drivers. There are open issues.)

--- sys/arch/i386/bios/vesabios.c
+++ sys/arch/i386/bios/vesabios.c
@@ -1,12 +1,16 @@
-/* $NetBSD: vesabios.c,v 1.1 2002/07/07 12:59:58 drochner Exp $ */
+/* $NetBSD: vesabios.c,v 1.2 2002/07/10 19:15:43 drochner Exp $ */
 
 #include <sys/param.h>
 #include <sys/systm.h>
 #include <sys/device.h>
+#include <sys/malloc.h>
 #include <machine/frame.h>
 #include <machine/kvm86.h>
 
 #include <arch/i386/bios/vesabios.h>
+#include <arch/i386/bios/vesabiosreg.h>
+
+#include "opt_vesabios.h"
 
 struct vbeinfoblock
 {
@@ -21,10 +25,20 @@ struct vbeinfoblock
 	/* data area, in total max 512 bytes for VBE 2.0 */
 } __attribute__ ((packed));
 
+#define FAR2FLATPTR(p) ((p & 0xffff) + ((p >> 12) & 0xffff0))
+
 static int vesabios_match(struct device *, struct cfdata *, void *);
+static void vesabios_attach(struct device *, struct device *, void *);
+static int vesabios_print(void *, const char *);
+
+static int vbegetinfo(struct vbeinfoblock **);
+static void vbefreeinfo(struct vbeinfoblock *);
+#ifdef VESABIOSVERBOSE
+static const char *mm2txt(unsigned int);
+#endif
 
 struct cfattach vesabios_ca = {
-	sizeof(struct device), vesabios_match, 0
+	sizeof(struct device), vesabios_match, vesabios_attach
 };
 
 static int
@@ -39,21 +53,21 @@ vesabios_match(parent, match, aux)
 	if (strcmp(vaa->vaa_busname, "vesabios") != 0)
 		return (0);
 
-	return (0); /* XXX for now */
+	return (1);
 }
 
-int
-vbeprobe()
+static int
+vbegetinfo(vip)
+	struct vbeinfoblock **vip;
 {
 	unsigned char *buf;
 	struct trapframe tf;
-	int res, ok = 0;
-	struct vbeinfoblock *vi;
+	int res, error;
 
 	buf = kvm86_bios_addpage(0x2000);
 	if (!buf) {
-		printf("vbeprobe: kvm86_bios_addpage(0x2000) failed\n");
-		return (0);
+		printf("vbegetinfo: kvm86_bios_addpage(0x2000) failed\n");
+		return (ENOMEM);
 	}
 
 	memcpy(buf, "VBE2", 4);
@@ -64,22 +78,200 @@ vbeprobe()
 	tf.tf_edi = 0x2000; /* buf ptr */
 
 	res = kvm86_bioscall(0x10, &tf);
-
-	printf("vbecall: res=%d, ax=%x\n", res, tf.tf_eax);
-	if (res || tf.tf_eax != 0x004f)
-		goto out;
-
-	vi = (struct vbeinfoblock *)buf;
-	if (memcmp(vi->VbeSignature, "VESA", 4)) {
-		printf("VESA: bad sig\n");
+	if (res || tf.tf_eax != 0x004f) {
+		printf("vbecall: res=%d, ax=%x\n", res, tf.tf_eax);
+		error = ENXIO;
 		goto out;
 	}
-	printf("VESA: version: %x, cap: %x, oem @%x, modes @%x\n",
-	       vi->VbeVersion, vi->Capabilities, vi->OemStringPtr,
-	       vi->VideoModePtr);
-	ok = 1;
+
+	if (memcmp(((struct vbeinfoblock *)buf)->VbeSignature, "VESA", 4)) {
+		error = EIO;
+		goto out;
+	}
+
+	if (vip)
+		*vip = (struct vbeinfoblock *)buf;
+	return (0);
 
 out:
 	kvm86_bios_delpage(0x2000, buf);
-	return (ok);
+	return (error);
+}
+
+static void
+vbefreeinfo(vip)
+	struct vbeinfoblock *vip;
+{
+
+	kvm86_bios_delpage(0x2000, vip);
+}
+
+int
+vbeprobe()
+{
+	struct vbeinfoblock *vi;
+
+	if (vbegetinfo(&vi))
+		return (0);
+	vbefreeinfo(vi);
+	return (1);
+}
+
+#ifdef VESABIOSVERBOSE
+static const char *
+mm2txt(mm)
+	unsigned int mm;
+{
+	static char buf[30];
+	static const char *names[] = {
+		"Text mode",
+		"CGA graphics",
+		"Hercules graphics",
+		"Planar",
+		"Packed pixel",
+		"Non-chain 4, 256 color",
+		"Direct Color",
+		"YUV"
+	};
+
+	if (mm < sizeof(names)/sizeof(names[0]))
+		return (names[mm]);
+	snprintf(buf, sizeof(buf), "unknown memory model %d", mm);
+	return (buf);
+}
+#endif
+
+static void
+vesabios_attach(parent, dev, aux)
+	struct device * parent, *dev;
+	void *aux;
+{
+	struct vbeinfoblock *vi;
+	unsigned char *buf;
+	struct trapframe tf;
+	int res;
+	char name[256];
+#define MAXMODES 60
+	u_int16_t modes[MAXMODES];
+	int raster8modes[MAXMODES];
+	int textmodes[MAXMODES];
+	int nmodes, nraster8modes, ntextmodes, i;
+	u_int32_t modeptr;
+	struct modeinfoblock *mi;
+	struct vesabiosdev_attach_args vbaa;
+
+	if (vbegetinfo(&vi))
+		panic("vesabios_attach: disappeared");
+
+	printf(": version %d.%d", vi->VbeVersion >> 8, vi->VbeVersion & 0xff);
+	
+	res = kvm86_bios_read(FAR2FLATPTR(vi->OemVendorNamePtr),
+			      name, sizeof(name));
+	if (res > 0) {
+		name[res - 1] = 0;
+		printf(", %s", name);
+		res = kvm86_bios_read(FAR2FLATPTR(vi->OemProductNamePtr),
+				      name, sizeof(name));
+		if (res > 0) {
+			name[res - 1] = 0;
+			printf(" %s", name);
+		}
+	}
+	printf("\n");
+
+	nmodes = 0;
+	modeptr = FAR2FLATPTR(vi->VideoModePtr);
+	while (nmodes < MAXMODES) {
+		res = kvm86_bios_read(modeptr, (char *)&modes[nmodes], 2);
+		if (res != 2 || modes[nmodes] == 0xffff)
+			break;
+		nmodes++;
+		modeptr += 2;
+	}
+
+	vbefreeinfo(vi);
+	if (nmodes == 0)
+		return;
+
+	nraster8modes = ntextmodes = 0;
+
+	buf = kvm86_bios_addpage(0x2000);
+	if (!buf) {
+		printf("vesabios_attach: kvm86_bios_addpage(0x2000) failed\n");
+		return;
+	}
+	for (i = 0; i < nmodes; i++) {
+
+		memset(&tf, 0, sizeof(struct trapframe));
+		tf.tf_eax = 0x4f01; /* function code */
+		tf.tf_ecx = modes[i];
+		tf.tf_es = 0;
+		tf.tf_edi = 0x2000; /* buf ptr */
+
+		res = kvm86_bioscall(0x10, &tf);
+		if (res || tf.tf_eax != 0x004f) {
+			printf("vbecall: res=%d, ax=%x\n", res, tf.tf_eax);
+			break;
+		}
+		mi = (struct modeinfoblock *)buf;
+#ifdef VESABIOSVERBOSE
+		printf("VESA mode %04x: attributes %04x",
+		       modes[i], mi->ModeAttributes);
+#endif
+		if (!(mi->ModeAttributes & 1)) {
+#ifdef VESABIOSVERBOSE
+			printf("\n");
+#endif
+			continue;
+		}
+		if (mi->ModeAttributes & 0x10) {
+			/* graphics */
+#ifdef VESABIOSVERBOSE
+			printf(", %dx%d %dbbp %s\n",
+			       mi->XResolution, mi->YResolution,
+			       mi->BitsPerPixel, mm2txt(mi->MemoryModel));
+#endif
+			if ((mi->ModeAttributes & 0x80)
+			    && mi->BitsPerPixel == 8
+			    && mi->MemoryModel == 4) {
+				/* flat buffer, 8bpp packed pixel */
+				raster8modes[nraster8modes++] = modes[i];
+			}
+		} else {
+			/* text */
+#ifdef VESABIOSVERBOSE
+			printf(", text %dx%d\n",
+			       mi->XResolution, mi->YResolution);
+#endif
+			textmodes[ntextmodes++] = modes[i];
+		}
+	}
+	kvm86_bios_delpage(0x2000, buf);
+
+	if (nraster8modes) {
+		vbaa.vbaa_type = "raster8";
+		vbaa.vbaa_modes = raster8modes;
+		vbaa.vbaa_nmodes = nraster8modes;
+
+		config_found(dev, &vbaa, vesabios_print);
+	}
+	if (textmodes) {
+		vbaa.vbaa_type = "text";
+		vbaa.vbaa_modes = textmodes;
+		vbaa.vbaa_nmodes = ntextmodes;
+
+		config_found(dev, &vbaa, vesabios_print);
+	}
+}
+
+static int
+vesabios_print(aux, pnp)
+	void *aux;
+	const char *pnp;
+{
+	struct vesabiosdev_attach_args *vbaa = aux;
+
+	if (pnp)
+		printf("%s at %s", vbaa->vbaa_type, pnp);
+	return (UNCONF);
 }
--- sys/arch/i386/bios/vesabios.h
+++ sys/arch/i386/bios/vesabios.h
@@ -1,7 +1,13 @@
-/* $NetBSD: vesabios.h,v 1.1 2002/07/07 12:59:58 drochner Exp $ */
+/* $NetBSD: vesabios.h,v 1.2 2002/07/10 19:15:43 drochner Exp $ */
 
 int vbeprobe __P((void));
 
 struct vesabios_attach_args {
 	char *vaa_busname;
+};
+
+struct vesabiosdev_attach_args {
+	char *vbaa_type;
+	int *vbaa_modes;
+	int vbaa_nmodes;
 };
