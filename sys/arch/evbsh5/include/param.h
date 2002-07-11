4db85d7e4aa436810414fda3fe31a701c5657f62
scw <scw>
2002-07-11 14:07:41 +0000

    Follow convention for MACHINE_ARCH endian suffix.

--- sys/arch/evbsh5/include/param.h
+++ sys/arch/evbsh5/include/param.h
@@ -1,8 +1,4 @@
-/*	$NetBSD: param.h,v 1.1 2002/07/05 13:31:47 scw Exp $	*/
-
-/*
- * XXX: Not sure if these are actually correct for SH-5
- */
+/*	$NetBSD: param.h,v 1.2 2002/07/11 14:07:41 scw Exp $	*/
 
 #ifndef _EVBSH5_PARAM_H
 #define _EVBSH5_PARAM_H
@@ -10,16 +6,14 @@
 #include <sh5/param.h>
 
 #ifdef __LITTLE_ENDIAN__
-#define	_MACHINE_ARCH	sh5le
-#define	MACHINE_ARCH	"sh5le"
+#define	_MACHINE_ARCH	sh5el
+#define	MACHINE_ARCH	"sh5el"
 #else
-#define	_MACHINE_ARCH	sh5
-#define	MACHINE_ARCH	"sh5"
+#define	_MACHINE_ARCH	sh5eb
+#define	MACHINE_ARCH	"sh5eb"
 #endif
 
 #define	_MACHINE	evbsh5
 #define	MACHINE		"evbsh5"
-
-#define	MID_MACHINE	MID_SH5
 
 #endif /* _EVBSH5_PARAM_H */
