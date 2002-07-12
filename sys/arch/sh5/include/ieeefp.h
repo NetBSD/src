723a7074eda12aa27dd32f08778a9a8f5cb123de
scw <scw@NetBSD.org>
2002-07-12 12:20:24 +0000

    Add IEEE FP mode control functions and definitions for SH5.

--- sys/arch/sh5/include/ieeefp.h
+++ sys/arch/sh5/include/ieeefp.h
@@ -1,7 +1,8 @@
-/*	$NetBSD: ieeefp.h,v 1.1 2002/07/10 10:38:23 scw Exp $	*/
+/*	$NetBSD: ieeefp.h,v 1.2 2002/07/12 12:20:24 scw Exp $	*/
 
 /*
  * Written by J.T. Conklin, Apr 6, 1995
+ * Updated for SH5 by Steve C. Woodford of Wasabi Systems.
  * Public domain.
  */
 
@@ -9,18 +10,17 @@
 #define	_SH5_IEEEFP_H_
 
 typedef int fp_except;
-#define	FP_X_INV	0x01	/* invalid operation exception */
-#define	FP_X_DNML	0x02	/* denormalization exception */
-#define	FP_X_DZ		0x04	/* divide-by-zero exception */
-#define	FP_X_OFL	0x08	/* overflow exception */
-#define	FP_X_UFL	0x10	/* underflow exception */
-#define	FP_X_IMP	0x20	/* imprecise (loss of precision) */
+#define	FP_X_IMP	0x01	/* imprecise (loss of precision) */
+#define	FP_X_UFL	0x02	/* underflow exception */
+#define	FP_X_OFL	0x04	/* overflow exception */
+#define	FP_X_DZ		0x08	/* divide-by-zero exception */
+#define	FP_X_INV	0x10	/* invalid operation exception */
 
 typedef enum {
-	FP_RN=0,		/* round to nearest representable number */
-	FP_RM=1,		/* round toward negative infinity */
-	FP_RP=2,		/* round toward positive infinity */
-	FP_RZ=3			/* round to zero (truncate) */
+	FP_RZ=0		/* round to zero (truncate) */
+	FP_RN=1,	/* round to nearest representable number */
+	FP_RM=2,	/* round toward negative infinity (no h/w support) */
+	FP_RP=3,	/* round toward positive infinity (no h/w support) */
 } fp_rnd;
 
 #endif /* !_SH5_IEEEFP_H_ */
