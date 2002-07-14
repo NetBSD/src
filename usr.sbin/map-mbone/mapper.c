25489cfbb9809f286acc47b9ee1d9b7d4b0f0ca5
wiz <wiz@NetBSD.org>
2002-07-14 15:34:58 +0000

    Unifdef __STDC__. ANSIfy. Remove __P().

--- usr.sbin/map-mbone/mapper.c
+++ usr.sbin/map-mbone/mapper.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: mapper.c,v 1.9 2002/06/02 13:47:02 itojun Exp $	*/
+/*	$NetBSD: mapper.c,v 1.10 2002/07/14 15:34:58 wiz Exp $	*/
 
 /* Mapper for connections between MRouteD multicast routers.
  * Written by Pavel Curtis <Pavel@PARC.Xerox.Com>
@@ -39,11 +39,7 @@
 #include <sys/time.h>
 #include "defs.h"
 #include <arpa/inet.h>
-#ifdef __STDC__
 #include <stdarg.h>
-#else
-#include <varargs.h>
-#endif
 
 #define DEFAULT_TIMEOUT	2	/* How long to wait before retrying requests */
 #define DEFAULT_RETRIES 1	/* How many times to ask each router */
@@ -87,31 +83,27 @@ int	show_names = TRUE;
 vifi_t  numvifs;		/* to keep loader happy */
 				/* (see COPY_TABLES macro called in kern.c) */
 
-Node *			find_node __P((u_int32_t addr, Node **ptr));
-Interface *		find_interface __P((u_int32_t addr, Node *node));
-Neighbor *		find_neighbor __P((u_int32_t addr, Node *node));
-int			main __P((int argc, char *argv[]));
-void			ask __P((u_int32_t dst));
-void			ask2 __P((u_int32_t dst));
-int			retry_requests __P((Node *node));
-char *			inet_name __P((u_int32_t addr));
-void			print_map __P((Node *node));
-char *			graph_name __P((u_int32_t addr, char *buf));
-void			graph_edges __P((Node *node));
-void			elide_aliases __P((Node *node));
-void			graph_map __P((void));
-int			get_number __P((int *var, int deflt, char ***pargv,
-						int *pargc));
-u_int32_t			host_addr __P((char *name));
+Node *			find_node(u_int32_t addr, Node **ptr);
+Interface *		find_interface(u_int32_t addr, Node *node);
+Neighbor *		find_neighbor(u_int32_t addr, Node *node);
+int			main(int argc, char *argv[]);
+void			ask(u_int32_t dst);
+void			ask2(u_int32_t dst);
+int			retry_requests(Node *node);
+char *			inet_name(u_int32_t addr);
+void			print_map(Node *node);
+char *			graph_name(u_int32_t addr, char *buf);
+void			graph_edges(Node *node);
+void			elide_aliases(Node *node);
+void			graph_map(void);
+int			get_number(int *var, int deflt, char ***pargv,
+				   int *pargc);
+u_int32_t		host_addr(char *name);
 
-#ifdef __STDC__
 void log(int severity, int syserr, const char *format, ...)
 	__attribute__((__format__(__printf__, 3, 4)));
-#endif
 
-Node *find_node(addr, ptr)
-    u_int32_t addr;
-    Node **ptr;
+Node *find_node(u_int32_t addr, Node **ptr)
 {
     Node *n = *ptr;
 
@@ -132,9 +124,7 @@ Node *find_node(addr, ptr)
 }
 
 
-Interface *find_interface(addr, node)
-    u_int32_t addr;
-    Node *node;
+Interface *find_interface(u_int32_t addr, Node *node)
 {
     Interface *ifc;
 
@@ -152,9 +142,7 @@ Interface *find_interface(addr, node)
 }
 
 
-Neighbor *find_neighbor(addr, node)
-    u_int32_t addr;
-    Node *node;
+Neighbor *find_neighbor(u_int32_t addr, Node *node)
 {
     Interface *ifc;
 
@@ -175,17 +163,8 @@ Neighbor *find_neighbor(addr, node)
  * message and the current debug level.  For errors of severity LOG_ERR or
  * worse, terminate the program.
  */
-#ifdef __STDC__
 void
 log(int severity, int syserr, const char *format, ...)
-#else
-/*VARARGS3*/
-void 
-log(severity, syserr, format, va_alist)
-	int     severity, syserr;
-	char   *format;
-	va_dcl
-#endif
 {
     va_list ap;
     char    fmt[100];
@@ -200,11 +179,7 @@ log(severity, syserr, format, va_alist)
 		strcat(fmt, "warning - ");
 	    strncat(fmt, format, 80);
 	    format = fmt;
-#ifdef __STDC__
 	    va_start(ap, format);
-#else
-	    va_start(ap);
-#endif
 	    vfprintf(stderr, format, ap);
 	    va_end(ap);
 	    if (syserr == 0)
@@ -221,15 +196,13 @@ log(severity, syserr, format, va_alist)
 /*
  * Send a neighbors-list request.
  */
-void ask(dst)
-    u_int32_t dst;
+void ask(u_int32_t dst)
 {
     send_igmp(our_addr, dst, IGMP_DVMRP, DVMRP_ASK_NEIGHBORS,
 		htonl(MROUTED_LEVEL), 0);
 }
 
-void ask2(dst)
-    u_int32_t dst;
+void ask2(u_int32_t dst)
 {
     send_igmp(our_addr, dst, IGMP_DVMRP, DVMRP_ASK_NEIGHBORS2,
 		htonl(MROUTED_LEVEL), 0);
@@ -239,9 +212,7 @@ void ask2(dst)
 /*
  * Process an incoming group membership report.
  */
-void accept_group_report(src, dst, group, r_type)
-    u_int32_t src, dst, group;
-    int r_type;
+void accept_group_report(u_int32_t src, u_int32_t dst, u_int32_t group, int r_type)
 {
     log(LOG_INFO, 0, "ignoring IGMP group membership report from %s to %s",
 	inet_fmt(src, s1), inet_fmt(dst, s2));
@@ -251,10 +222,8 @@ void accept_group_report(src, dst, group, r_type)
 /*
  * Process an incoming neighbor probe message.
  */
-void accept_probe(src, dst, p, datalen, level)
-    u_int32_t src, dst, level;
-    char *p;
-    int datalen;
+void accept_probe(u_int32_t src, u_int32_t dst, char *p, int datalen,
+		  u_int32_t level)
 {
     log(LOG_INFO, 0, "ignoring DVMRP probe from %s to %s",
 	inet_fmt(src, s1), inet_fmt(dst, s2));
@@ -264,10 +233,8 @@ void accept_probe(src, dst, p, datalen, level)
 /*
  * Process an incoming route report message.
  */
-void accept_report(src, dst, p, datalen, level)
-    u_int32_t src, dst, level;
-    char *p;
-    int datalen;
+void accept_report(u_int32_t src, u_int32_t dst, char *p, int datalen,
+		   u_int32_t level)
 {
     log(LOG_INFO, 0, "ignoring DVMRP routing report from %s to %s",
 	inet_fmt(src, s1), inet_fmt(dst, s2));
@@ -277,8 +244,7 @@ void accept_report(src, dst, p, datalen, level)
 /*
  * Process an incoming neighbor-list request message.
  */
-void accept_neighbor_request(src, dst)
-    u_int32_t src, dst;
+void accept_neighbor_request(u_int32_t src, u_int32_t dst)
 {
     if (src != our_addr)
 	log(LOG_INFO, 0,
@@ -286,8 +252,7 @@ void accept_neighbor_request(src, dst)
 	    inet_fmt(src, s1), inet_fmt(dst, s2));
 }
 
-void accept_neighbor_request2(src, dst)
-    u_int32_t src, dst;
+void accept_neighbor_request2(u_int32_t src, u_int32_t dst)
 {
     if (src != our_addr)
 	log(LOG_INFO, 0,
@@ -299,10 +264,8 @@ void accept_neighbor_request2(src, dst)
 /*
  * Process an incoming neighbor-list message.
  */
-void accept_neighbors(src, dst, p, datalen, level)
-    u_int32_t src, dst, level;
-    u_char *p;
-    int datalen;
+void accept_neighbors(u_int32_t src, u_int32_t dst, u_char *p, int datalen,
+		      u_int32_t level)
 {
     Node       *node = find_node(src, &routers);
 
@@ -460,10 +423,8 @@ void accept_neighbors(src, dst, p, datalen, level)
     }
 }
 
-void accept_neighbors2(src, dst, p, datalen, level)
-    u_int32_t src, dst, level;
-    u_char *p;
-    int datalen;
+void accept_neighbors2(u_int32_t src, u_int32_t dst, u_char *p, int datalen,
+		       u_int32_t level)
 {
     Node       *node = find_node(src, &routers);
     u_int broken_cisco = ((level & 0xffff) == 0x020a); /* 10.2 */
@@ -607,14 +568,13 @@ void accept_neighbors2(src, dst, p, datalen, level)
 }
 
 
-void check_vif_state()
+void check_vif_state(void)
 {
     log(LOG_NOTICE, 0, "network marked down...");
 }
 
 
-int retry_requests(node)
-    Node *node;
+int retry_requests(Node *node)
 {
     int	result;
 
@@ -634,8 +594,7 @@ int retry_requests(node)
 }
 
 
-char *inet_name(addr)
-    u_int32_t addr;
+char *inet_name(u_int32_t addr)
 {
     struct hostent *e;
 
@@ -645,8 +604,7 @@ char *inet_name(addr)
 }
 
 
-void print_map(node)
-    Node *node;
+void print_map(Node *node)
 {
     if (node) {
 	char *name, *addr;
@@ -713,9 +671,7 @@ void print_map(node)
 }
 
 
-char *graph_name(addr, buf)
-    u_int32_t addr;
-    char *buf;
+char *graph_name(u_int32_t addr, char *buf)
 {
     char *name;
 
@@ -728,8 +684,7 @@ char *graph_name(addr, buf)
 }
 
 
-void graph_edges(node)
-    Node *node;
+void graph_edges(Node *node)
 {
     Interface *ifc;
     Neighbor *nb;
@@ -773,8 +728,7 @@ void graph_edges(node)
     }
 }
 
-void elide_aliases(node)
-    Node *node;
+void elide_aliases(Node *node)
 {
     if (node) {
 	elide_aliases(node->left);
@@ -796,7 +750,7 @@ void elide_aliases(node)
     }
 }
 
-void graph_map()
+void graph_map(void)
 {
     time_t now = time(0);
     char *nowstr = ctime(&now);
@@ -810,9 +764,7 @@ void graph_map()
 }
 
 
-int get_number(var, deflt, pargv, pargc)
-    int *var, *pargc, deflt;
-    char ***pargv;
+int get_number(int *var, int deflt, char ***pargv, int *pargc)
 {
     if ((*pargv)[0][2] == '\0') { /* Get the value from the next argument */
 	if (*pargc > 1  &&  isdigit((*pargv)[1][0])) {
@@ -835,8 +787,7 @@ int get_number(var, deflt, pargv, pargc)
 }
 
 
-u_int32_t host_addr(name)
-    char *name;
+u_int32_t host_addr(char *name)
 {
     struct hostent *e = gethostbyname(name);
     int addr;
@@ -853,9 +804,7 @@ u_int32_t host_addr(name)
 }
 
 
-int main(argc, argv)
-    int argc;
-    char *argv[];
+int main(int argc, char **argv)
 {
     int flood = FALSE, graph = FALSE;
     
@@ -998,53 +947,32 @@ int main(argc, argv)
 }
 
 /* dummies */
-void accept_prune(src, dst, p, datalen)
-	u_int32_t src, dst;
-	char *p;
-	int datalen;
+void accept_prune(u_int32_t src, u_int32_t dst, char *p, int datalen)
 {
 }
-void accept_graft(src, dst, p, datalen)
-	u_int32_t src, dst;
-	char *p;
-	int datalen;
+void accept_graft(u_int32_t src, u_int32_t dst, char *p, int datalen)
 {
 }
-void accept_g_ack(src, dst, p, datalen)
-	u_int32_t src, dst;
-	char *p;
-	int datalen;
+void accept_g_ack(u_int32_t src, u_int32_t dst, char *p, int datalen)
 {
 }
-void add_table_entry(origin, mcastgrp)
-	u_int32_t origin, mcastgrp;
+void add_table_entry(u_int32_t origin, u_int32_t mcastgrp)
 {
 }
-void accept_leave_message(src, dst, group)
-	u_int32_t src, dst, group;
+void accept_leave_message(u_int32_t src, u_int32_t dst, u_int32_t group)
 {
 }
-void accept_mtrace(src, dst, group, data, no, datalen)
-	u_int32_t src, dst, group;
-	char *data;
-	u_int no;
-	int datalen;
+void accept_mtrace(u_int32_t src, u_int32_t dst, u_int32_t group, char *data,
+		   u_int no, int datalen)
 {
 }
-void accept_membership_query(src, dst, group, tmo)
-	u_int32_t src, dst, group;
-	int tmo;
+void accept_membership_query(u_int32_t src, u_int32_t dst, u_int32_t group,
+			     int tmo)
 {
 }
-void accept_info_request(src, dst, p, datalen)
-	u_int32_t src, dst;
-	u_char *p;
-	int datalen;
+void accept_info_request(u_int32_t src, u_int32_t dst, u_char *p, int datalen)
 {
 }
-void accept_info_reply(src, dst, p, datalen)
-	u_int32_t src, dst;
-	u_char *p;
-	int datalen;
+void accept_info_reply(u_int32_t src, u_int32_t dst, u_char *p, int datalen)
 {
 }
