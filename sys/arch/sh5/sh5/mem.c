b1a0f46f25bf90f699f56af5a9c8def056b61618
gehenna <gehenna@NetBSD.org>
2002-07-16 01:43:15 +0000

    Add new devsw stuff.

--- sys/arch/sh5/sh5/mem.c
+++ sys/arch/sh5/sh5/mem.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: mem.c,v 1.1.2.2 2002/07/16 00:41:21 gehenna Exp $	*/
+/*	$NetBSD: mem.c,v 1.1.2.3 2002/07/16 01:43:16 gehenna Exp $	*/
 
 /*
  * Copyright (c) 1988 University of Utah.
@@ -44,7 +44,7 @@
 
 #include <sys/cdefs.h>
 
-__KERNEL_RCSID(0, "$NetBSD: mem.c,v 1.1.2.2 2002/07/16 00:41:21 gehenna Exp $");
+__KERNEL_RCSID(0, "$NetBSD: mem.c,v 1.1.2.3 2002/07/16 01:43:16 gehenna Exp $");
 
 #include <sys/param.h>
 #include <sys/conf.h>
@@ -63,31 +63,14 @@ extern vaddr_t vmmap;
 static caddr_t zeropage;
 static volatile int physlock;
 
+dev_type_read(mmrw);
+dev_type_ioctl(mmioctl);
+dev_type_mmap(mmmmap);
 
-/*ARGSUSED*/
-int
-mmopen(dev, flag, mode, p)
-	dev_t dev;
-	int flag, mode;
-	struct proc *p;
-{
-	switch (minor(dev)) {
-	default:
-		break;
-	}
-	return (0);
-}
-
-/*ARGSUSED*/
-int
-mmclose(dev, flag, mode, p)
-	dev_t dev;
-	int flag, mode;
-	struct proc *p;
-{
-
-	return (0);
-}
+const struct cdevsw mem_cdevsw = {
+	nullopen, nullclose, mmrw, mmrw, mmioctl,
+	nostop, notty, nopoll, mmmmap,
+};
 
 /*ARGSUSED*/
 int
