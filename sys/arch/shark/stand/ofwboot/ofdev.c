f70a459bf05cb1ff223696b621a629e2a46b4bad
gehenna <gehenna@NetBSD.org>
2002-07-17 01:41:56 +0000

    catch up with -current.

--- sys/arch/shark/stand/ofwboot/ofdev.c
+++ sys/arch/shark/stand/ofwboot/ofdev.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: ofdev.c,v 1.1 2002/02/10 01:58:18 thorpej Exp $	*/
+/*	$NetBSD: ofdev.c,v 1.1.10.1 2002/07/17 01:43:02 gehenna Exp $	*/
 
 /*
  * Copyright (C) 1995, 1996 Wolfgang Solfrank.
@@ -209,7 +209,7 @@ search_label(devp, off, buf, lp, off0)
 	    || read != DEV_BSIZE)
 		return ERDLAB;
 	
-	if (buf[510] != 0x55 || buf[511] != 0xaa)
+	if (*(u_int16_t *)&buf[MBR_MAGICOFF] != sa_htole16(MBR_MAGIC))
 		return ERDLAB;
 
 	if (recursion++ <= 1)
