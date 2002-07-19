3e899f2541997c5daddf8570c92a4834a6a6dce4
simonb <simonb>
2002-07-19 03:13:55 +0000

    White space nits, add a #endif comment.

--- sys/arch/mips/include/sysarch.h
+++ sys/arch/mips/include/sysarch.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: sysarch.h,v 1.4 2002/03/05 14:12:30 simonb Exp $ */
+/*	$NetBSD: sysarch.h,v 1.5 2002/07/19 03:13:55 simonb Exp $ */
 
 #ifndef _MIPS_SYSARCH_H_
 #define _MIPS_SYSARCH_H_
@@ -8,7 +8,6 @@
  */
 #define MIPS_CACHEFLUSH	0
 #define MIPS_CACHECTL	1
-
 
 struct mips_cacheflush_args {
 	vaddr_t va;
@@ -24,6 +23,5 @@ struct mips_cachectl_args {
 
 #ifndef _KERNEL
 int sysarch(int, void *);
-#endif
-
+#endif /* !_KERNEL */
 #endif /* !_MIPS_SYSARCH_H_ */
