db6157e524b92ace2efa191350a749a181976263
hannken <hannken@NetBSD.org>
2002-07-24 19:34:57 +0000

    Convert to new device buffer queue interface.

--- sys/dev/ic/rrunnervar.h
+++ sys/dev/ic/rrunnervar.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: rrunnervar.h,v 1.6 2000/01/21 23:39:58 thorpej Exp $	*/
+/*	$NetBSD: rrunnervar.h,v 1.7 2002/07/24 19:34:57 hannken Exp $	*/
 
 /* Copyright (c) 1997, 1998 The NetBSD Foundation, Inc.
  * All rights reserved.
@@ -114,7 +114,7 @@ struct esh_send_ring_ctl {
 	struct buf *ec_cur_buf;		/* current buf being processed */
 	struct esh_dmainfo *ec_cur_dmainfo;	
 					/* current dmainfo being processed */
-	struct buf_queue ec_buf_queue;	/* queue of bufs to send */
+	struct bufq_state ec_buf_queue;	/* queue of bufs to send */
 	int ec_error;			/* encountered error? */
 	u_int16_t ec_producer;		/* latest buffer driver produced */
 	u_int16_t ec_consumer;		/* latest buffer runcode consumed */
