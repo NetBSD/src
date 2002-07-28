59d3d6c274c6eaea98ef62fbdd19b4cff7db0ec8
christos <christos@NetBSD.org>
2002-07-28 19:54:47 +0000

    Don't throw away the name and the secret lengths. This eliminates all the strlen() calls, and there was a whole bunch of them.

--- sys/net/if_spppvar.h
+++ sys/net/if_spppvar.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: if_spppvar.h,v 1.4 2002/03/02 16:23:42 martin Exp $	*/
+/*	$NetBSD: if_spppvar.h,v 1.5 2002/07/28 19:54:47 christos Exp $	*/
 
 /*
  * Defines for synchronous PPP/Cisco link level subroutines.
@@ -63,6 +63,8 @@ struct sauth {
 	u_short	flags;
 	char	*name;			/* system identification name */
 	char	*secret;		/* secret password */
+	u_char	name_len;		/* no need to have a bigger size */
+	u_char	secret_len;		/* because proto gives size in a byte */
 	char	challenge[16];		/* random challenge [don't change size! it's realy hardcoded!] */
 };
 
