c875177fef84760c1981de968aff5317184f45f7
simonb <simonb@NetBSD.org>
2002-07-31 03:42:54 +0000

    Add kttcp, sysmon and broadcom/sibyte devices.

--- sys/arch/evbmips/malta/conf.c
+++ sys/arch/evbmips/malta/conf.c
@@ -1,8 +1,8 @@
-/* $NetBSD: conf.c,v 1.5 2002/07/26 03:23:05 simonb Exp $ */
+/* $NetBSD: conf.c,v 1.6 2002/07/31 03:42:55 simonb Exp $ */
 
 #include <sys/cdefs.h>			/* RCS ID & Copyright macro defns */
 
-__KERNEL_RCSID(0, "$NetBSD: conf.c,v 1.5 2002/07/26 03:23:05 simonb Exp $");
+__KERNEL_RCSID(0, "$NetBSD: conf.c,v 1.6 2002/07/31 03:42:55 simonb Exp $");
 
 #define	CONF_HAVE_ISDN
 #define	CONF_HAVE_PCI
@@ -10,7 +10,10 @@ __KERNEL_RCSID(0, "$NetBSD: conf.c,v 1.5 2002/07/26 03:23:05 simonb Exp $");
 #define	CONF_HAVE_USB
 #define	CONF_HAVE_WSCONS
 #define	CONF_HAVE_SPKR
-#define	CONF_HAVE_ISA	/* XXX bad name */
+#define	CONF_HAVE_ISA		/* XXX needs a better name */
+#undef	CONF_HAVE_KTTCP
+#undef	CONF_HAVE_SYSMON
 #undef	CONF_HAVE_ALCHEMY
+#undef	CONF_HAVE_SBMIPS
 
 #include "arch/evbmips/evbmips/conf_common.c"
