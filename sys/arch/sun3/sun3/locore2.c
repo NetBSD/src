3a942c300ce47dbab375b64b4f1ccf7ae45915ed
soren <soren@NetBSD.org>
2002-08-02 18:19:58 +0000

    G/c vestiges of old sun3-specific SYMTAB_SPACE support.

--- sys/arch/sun3/sun3/locore2.c
+++ sys/arch/sun3/sun3/locore2.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: locore2.c,v 1.79 2001/09/05 14:12:21 tsutsui Exp $	*/
+/*	$NetBSD: locore2.c,v 1.80 2002/08/02 18:19:59 soren Exp $	*/
 
 /*-
  * Copyright (c) 1996 The NetBSD Foundation, Inc.
@@ -108,7 +108,7 @@ void _bootstrap __P((void));
 static void _verify_hardware __P((void));
 static void _vm_init __P((void));
 
-#if defined(DDB) && !defined(SYMTAB_SPACE)
+#if defined(DDB)
 static void _save_symtab __P((void));
 
 /*
@@ -167,7 +167,7 @@ _save_symtab()
 	ssym = (char *)ehdr;
 	esym = (char *)maxsym;
 }
-#endif	/* DDB && !SYMTAB_SPACE */
+#endif	/* DDB */
 
 /*
  * This function is called from _bootstrap() to initialize
@@ -189,7 +189,7 @@ _vm_init()
 	 * if DDB is not part of this kernel, ignore the symbols.
 	 */
 	esym = end + 4;
-#if defined(DDB) && !defined(SYMTAB_SPACE)
+#if defined(DDB)
 	/* This will advance esym past the symbols. */
 	_save_symtab();
 #endif
