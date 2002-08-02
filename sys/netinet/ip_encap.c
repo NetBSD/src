17100fe670b01cfcb51ad8a5633dccae31c56856
lukem <lukem@NetBSD.org>
2002-08-02 00:34:13 +0000

    Pull up revision 1.10 (requested by itojun in ticket #593): remove packed attribute as it will cause data be unaligned

--- sys/netinet/ip_encap.c
+++ sys/netinet/ip_encap.c
@@ -70,7 +70,7 @@
 #define USE_RADIX
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: ip_encap.c,v 1.8 2002/03/04 13:24:12 sommerfeld Exp $");
+__KERNEL_RCSID(0, "$NetBSD: ip_encap.c,v 1.8.8.1 2002/08/02 00:34:13 lukem Exp $");
 
 #include "opt_mrouting.h"
 #include "opt_inet.h"
@@ -118,18 +118,18 @@ struct sockaddr_pack {
 	u_int8_t sp_len;
 	u_int8_t sp_family;	/* not really used */
 	/* followed by variable-length data */
-} __attribute__((__packed__));
+};
 
 struct pack4 {
 	struct sockaddr_pack p;
 	struct sockaddr_in mine;
 	struct sockaddr_in yours;
-} __attribute__((__packed__));
+};
 struct pack6 {
 	struct sockaddr_pack p;
 	struct sockaddr_in6 mine;
 	struct sockaddr_in6 yours;
-} __attribute__((__packed__));
+};
 
 enum direction { INBOUND, OUTBOUND };
 
