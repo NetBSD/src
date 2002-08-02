d37fa4fdae17508bc64409020a9fff511395833c
thorpej <thorpej@NetBSD.org>
2002-08-02 00:35:47 +0000

    Add support for the Intel i80321 I/O Processor's Application Accelerator Unit.  The AAU provides block fill, block copy, XOR, and XOR-parity-check operations.  We currently provide dmover(9) functions for "zero", "fill8", and "copy".
    Much of this code can be shared with the i80312 Companion I/O AAU, and
    will be when support for the older chip is implemented.

---
+++ sys/arch/arm/xscale/iopaau.c
@@ -0,0 +1,619 @@
+/*	$NetBSD: iopaau.c,v 1.1 2002/08/02 00:35:48 thorpej Exp $	*/
+
+/*
+ * Copyright (c) 2002 Wasabi Systems, Inc.
+ * All rights reserved.
+ *
+ * Written by Jason R. Thorpe for Wasabi Systems, Inc.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed for the NetBSD Project by
+ *	Wasabi Systems, Inc.
+ * 4. The name of Wasabi Systems, Inc. may not be used to endorse
+ *    or promote products derived from this software without specific prior
+ *    written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY WASABI SYSTEMS, INC. ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL WASABI SYSTEMS, INC
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/*
+ * Common code for XScale-based I/O Processor Application Accelerator
+ * Unit support.
+ *
+ * The AAU provides a back-end for the dmover(9) facility.
+ */
+
+#include <sys/cdefs.h>
+__KERNEL_RCSID(0, "$NetBSD");
+
+#include <sys/param.h>
+#include <sys/pool.h>
+#include <sys/lock.h>
+#include <sys/systm.h>
+#include <sys/device.h>
+#include <sys/uio.h>
+
+#include <uvm/uvm.h>
+
+#include <machine/bus.h>
+
+#include <arm/xscale/iopaaureg.h>
+#include <arm/xscale/iopaauvar.h>
+
+#ifdef AAU_DEBUG
+#define	DPRINTF(x)	printf x
+#else
+#define	DPRINTF(x)	/* nothing */
+#endif
+
+static struct pool aau_desc_4_pool;
+static struct pool_cache aau_desc_4_cache;
+
+/*
+ * iopaau_desc_ctor:
+ *
+ *	Constructor for all types of descriptors.
+ */
+static int
+iopaau_desc_ctor(void *arg, void *object, int flags)
+{
+	struct aau_desc_4 *d = object;
+
+	/*
+	 * Cache the physical address of the hardware portion of
+	 * the descriptor in the software portion of the descriptor
+	 * for quick reference later.
+	 */
+	d->d_pa = vtophys(d) + SYNC_DESC_4_OFFSET;
+	KASSERT((d->d_pa & 31) == 0);
+	return (0);
+}
+
+/*
+ * iopaau_desc_4_free:
+ *
+ *	Free a chain of aau_desc_4 structures.
+ */
+void
+iopaau_desc_4_free(struct iopaau_softc *sc, void *firstdesc)
+{
+	struct aau_desc_4 *d, *next;
+
+	for (d = firstdesc; d != NULL; d = next) {
+		next = d->d_next;
+		pool_cache_put(&aau_desc_4_cache, d);
+	}
+}
+
+/*
+ * iopaau_start:
+ *
+ *	Start an AAU request.  Must be called at splbio().
+ */
+static void
+iopaau_start(struct iopaau_softc *sc)
+{
+	struct dmover_backend *dmb = &sc->sc_dmb;
+	struct dmover_request *dreq;
+	struct iopaau_function *af;
+	int error;
+
+	for (;;) {
+
+		KASSERT(sc->sc_running == NULL);
+
+		dreq = TAILQ_FIRST(&dmb->dmb_pendreqs);
+		if (dreq == NULL)
+			return;
+
+		dmover_backend_remque(dmb, dreq);
+		dreq->dreq_flags |= DMOVER_REQ_RUNNING;
+
+		sc->sc_running = dreq;
+
+		/* XXXUNLOCK */
+
+		af = dreq->dreq_assignment->das_algdesc->dad_data;
+		error = (*af->af_setup)(sc, dreq);
+
+		/* XXXLOCK */
+
+		if (error) {
+			dreq->dreq_flags |= DMOVER_REQ_ERROR;
+			dreq->dreq_error = error;
+			sc->sc_running = NULL;
+			/* XXXUNLOCK */
+			dmover_done(dreq);
+			/* XXXLOCK */
+			continue;
+		}
+
+#ifdef DIAGNOSTIC
+		if (bus_space_read_4(sc->sc_st, sc->sc_sh, AAU_ASR) &
+		    AAU_ASR_AAF)
+			panic("iopaau_start: AAU already active");
+#endif
+
+		DPRINTF(("%s: starting dreq %p\n", sc->sc_dev.dv_xname,
+		    dreq));
+
+		bus_space_write_4(sc->sc_st, sc->sc_sh, AAU_ANDAR,
+		    sc->sc_firstdesc_pa);
+		bus_space_write_4(sc->sc_st, sc->sc_sh, AAU_ACR,
+		    AAU_ACR_AAE);
+
+		break;
+	}
+}
+
+/*
+ * iopaau_finish:
+ *
+ *	Finish the current operation.  AAU must be stopped.
+ */
+static void
+iopaau_finish(struct iopaau_softc *sc)
+{
+	struct dmover_request *dreq = sc->sc_running;
+	struct iopaau_function *af =
+	    dreq->dreq_assignment->das_algdesc->dad_data;
+	void *firstdesc = sc->sc_firstdesc;
+	int i, ninputs = dreq->dreq_assignment->das_algdesc->dad_ninputs;
+
+	sc->sc_running = NULL;
+
+	/* If the function has inputs, unmap them. */
+	for (i = 0; i < ninputs; i++) {
+		bus_dmamap_sync(sc->sc_dmat, sc->sc_map_in[i], 0,
+		    sc->sc_map_in[i]->dm_mapsize, BUS_DMASYNC_POSTREAD);
+		bus_dmamap_unload(sc->sc_dmat, sc->sc_map_in[i]);
+	}
+
+	/* Unload the output buffer DMA map. */
+	bus_dmamap_sync(sc->sc_dmat, sc->sc_map_out, 0,
+	    sc->sc_map_out->dm_mapsize, BUS_DMASYNC_POSTWRITE);
+	bus_dmamap_unload(sc->sc_dmat, sc->sc_map_out);
+
+	/* Get the next transfer started. */
+	iopaau_start(sc);
+
+	/* Now free descriptors for last transfer. */
+	(*af->af_free)(sc, firstdesc);
+
+	dmover_done(dreq);
+}
+
+/*
+ * iopaau_process:
+ *
+ *	Dmover back-end entry point.
+ */
+void
+iopaau_process(struct dmover_backend *dmb)
+{
+	struct iopaau_softc *sc = dmb->dmb_cookie;
+	int s;
+
+	s = splbio();
+	/* XXXLOCK */
+
+	if (sc->sc_running == NULL)
+		iopaau_start(sc);
+
+	/* XXXUNLOCK */
+	splx(s);
+}
+
+/*
+ * iopaau_func_zero_setup:
+ *
+ *	Setup routine for the "zero" function.
+ */
+int
+iopaau_func_zero_setup(struct iopaau_softc *sc, struct dmover_request *dreq)
+{
+	bus_dmamap_t dmamap = sc->sc_map_out;
+	uint32_t *prevpa;
+	struct aau_desc_4 **prevp, *cur;
+	int error, seg;
+
+	switch (dreq->dreq_outbuf_type) {
+	case DMOVER_BUF_LINEAR:
+		error = bus_dmamap_load(sc->sc_dmat, dmamap,
+		    dreq->dreq_outbuf.dmbuf_linear.l_addr,
+		    dreq->dreq_outbuf.dmbuf_linear.l_len, NULL,
+		    BUS_DMA_NOWAIT|BUS_DMA_WRITE|BUS_DMA_STREAMING);
+		break;
+
+	case DMOVER_BUF_UIO:
+	    {
+		struct uio *uio = dreq->dreq_outbuf.dmbuf_uio;
+
+		if (uio->uio_rw != UIO_READ)
+			return (EINVAL);
+
+		error = bus_dmamap_load_uio(sc->sc_dmat, dmamap,
+		    uio, BUS_DMA_NOWAIT|BUS_DMA_WRITE|BUS_DMA_STREAMING);
+		break;
+	    }
+	}
+
+	if (__predict_false(error != 0))
+		return (error);
+
+	bus_dmamap_sync(sc->sc_dmat, dmamap, 0, dmamap->dm_mapsize,
+	    BUS_DMASYNC_PREWRITE);
+
+	prevp = (struct aau_desc_4 **) &sc->sc_firstdesc;
+	prevpa = &sc->sc_firstdesc_pa;
+
+	for (seg = 0; seg < dmamap->dm_nsegs; seg++) {
+		cur = pool_cache_get(&aau_desc_4_cache, PR_NOWAIT);
+		if (cur == NULL) {
+			*prevp = NULL;
+			error = ENOMEM;
+			goto bad;
+		}
+
+		*prevp = cur;
+		*prevpa = cur->d_pa;
+
+		prevp = &cur->d_next;
+		prevpa = &cur->d_nda;
+
+		/*
+		 * We don't actually enforce the page alignment
+		 * constraint, here, because there is only one
+		 * data stream to worry about.
+		 */
+
+		cur->d_sar1 = 0;	/* immediate value */
+		cur->d_dar = dmamap->dm_segs[seg].ds_addr;
+		cur->d_bc = dmamap->dm_segs[seg].ds_len;
+		cur->d_dc = AAU_DC_B1_CC(AAU_DC_CC_FILL) | AAU_DC_DWE;
+		SYNC_DESC_4(cur);
+	}
+
+	*prevp = NULL;
+	*prevpa = 0;
+
+	cur->d_dc |= AAU_DC_IE;
+	SYNC_DESC_4(cur);
+
+	sc->sc_lastdesc = cur;
+
+	return (0);
+
+ bad:
+	iopaau_desc_4_free(sc, sc->sc_firstdesc);
+	bus_dmamap_unload(sc->sc_dmat, sc->sc_map_out);
+	sc->sc_firstdesc = NULL;
+
+	return (error);
+}
+
+/*
+ * iopaau_func_fill8_setup:
+ *
+ *	Setup routine for the "fill8" function.
+ */
+int
+iopaau_func_fill8_setup(struct iopaau_softc *sc, struct dmover_request *dreq)
+{
+	bus_dmamap_t dmamap = sc->sc_map_out;
+	uint32_t *prevpa;
+	struct aau_desc_4 **prevp, *cur;
+	int error, seg;
+	uint32_t immed;
+
+	immed = dreq->dreq_immediate[0] |
+	    (dreq->dreq_immediate[0] << 8) |
+	    (dreq->dreq_immediate[0] << 16) |
+	    (dreq->dreq_immediate[0] << 24);
+
+	switch (dreq->dreq_outbuf_type) {
+	case DMOVER_BUF_LINEAR:
+		error = bus_dmamap_load(sc->sc_dmat, dmamap,
+		    dreq->dreq_outbuf.dmbuf_linear.l_addr,
+		    dreq->dreq_outbuf.dmbuf_linear.l_len, NULL,
+		    BUS_DMA_NOWAIT|BUS_DMA_WRITE|BUS_DMA_STREAMING);
+		break;
+
+	case DMOVER_BUF_UIO:
+	    {
+		struct uio *uio = dreq->dreq_outbuf.dmbuf_uio;
+
+		if (uio->uio_rw != UIO_READ)
+			return (EINVAL);
+
+		error = bus_dmamap_load_uio(sc->sc_dmat, dmamap,
+		    uio, BUS_DMA_NOWAIT|BUS_DMA_WRITE|BUS_DMA_STREAMING);
+		break;
+	    }
+	}
+
+	if (__predict_false(error != 0))
+		return (error);
+
+	bus_dmamap_sync(sc->sc_dmat, dmamap, 0, dmamap->dm_mapsize,
+	    BUS_DMASYNC_PREWRITE);
+
+	prevp = (struct aau_desc_4 **) &sc->sc_firstdesc;
+	prevpa = &sc->sc_firstdesc_pa;
+
+	for (seg = 0; seg < dmamap->dm_nsegs; seg++) {
+		cur = pool_cache_get(&aau_desc_4_cache, PR_NOWAIT);
+		if (cur == NULL) {
+			*prevp = NULL;
+			error = ENOMEM;
+			goto bad;
+		}
+
+		*prevp = cur;
+		*prevpa = cur->d_pa;
+
+		prevp = &cur->d_next;
+		prevpa = &cur->d_nda;
+
+		/*
+		 * We don't actually enforce the page alignment
+		 * constraint, here, because there is only one
+		 * data stream to worry about.
+		 */
+
+		cur->d_sar1 = immed;
+		cur->d_dar = dmamap->dm_segs[seg].ds_addr;
+		cur->d_bc = dmamap->dm_segs[seg].ds_len;
+		cur->d_dc = AAU_DC_B1_CC(AAU_DC_CC_FILL) | AAU_DC_DWE;
+		SYNC_DESC_4(cur);
+	}
+
+	*prevp = NULL;
+	*prevpa = 0;
+
+	cur->d_dc |= AAU_DC_IE;
+	SYNC_DESC_4(cur);
+
+	sc->sc_lastdesc = cur;
+
+	return (0);
+
+ bad:
+	iopaau_desc_4_free(sc, sc->sc_firstdesc);
+	bus_dmamap_unload(sc->sc_dmat, sc->sc_map_out);
+	sc->sc_firstdesc = NULL;
+
+	return (error);
+}
+
+/*
+ * iopaau_func_copy_setup:
+ *
+ *	Setup routine for the "copy" function.
+ */
+int
+iopaau_func_copy_setup(struct iopaau_softc *sc, struct dmover_request *dreq)
+{
+	bus_dmamap_t dmamap = sc->sc_map_out;
+	bus_dmamap_t inmap = sc->sc_map_in[0];
+	uint32_t *prevpa;
+	struct aau_desc_4 **prevp, *cur;
+	int error, seg;
+
+	switch (dreq->dreq_outbuf_type) {
+	case DMOVER_BUF_LINEAR:
+		error = bus_dmamap_load(sc->sc_dmat, dmamap,
+		    dreq->dreq_outbuf.dmbuf_linear.l_addr,
+		    dreq->dreq_outbuf.dmbuf_linear.l_len, NULL,
+		    BUS_DMA_NOWAIT|BUS_DMA_WRITE|BUS_DMA_STREAMING);
+		break;
+
+	case DMOVER_BUF_UIO:
+	    {
+		struct uio *uio = dreq->dreq_outbuf.dmbuf_uio;
+
+		if (uio->uio_rw != UIO_READ)
+			return (EINVAL);
+
+		error = bus_dmamap_load_uio(sc->sc_dmat, dmamap,
+		    uio, BUS_DMA_NOWAIT|BUS_DMA_WRITE|BUS_DMA_STREAMING);
+		break;
+	    }
+	}
+
+	if (__predict_false(error != 0))
+		return (error);
+
+	switch (dreq->dreq_inbuf_type) {
+	case DMOVER_BUF_LINEAR:
+		error = bus_dmamap_load(sc->sc_dmat, inmap,
+		    dreq->dreq_inbuf[0].dmbuf_linear.l_addr,
+		    dreq->dreq_inbuf[0].dmbuf_linear.l_len, NULL,
+		    BUS_DMA_NOWAIT|BUS_DMA_READ|BUS_DMA_STREAMING);
+		break;
+
+	 case DMOVER_BUF_UIO:
+	     {
+		struct uio *uio = dreq->dreq_inbuf[0].dmbuf_uio;
+
+		if (uio->uio_rw != UIO_WRITE) {
+			error = EINVAL;
+			break;
+		}
+
+		error = bus_dmamap_load_uio(sc->sc_dmat, inmap,
+		    uio, BUS_DMA_NOWAIT|BUS_DMA_READ|BUS_DMA_STREAMING);
+		break;
+	    }
+	}
+
+	if (__predict_false(error != 0)) {
+		bus_dmamap_unload(sc->sc_dmat, dmamap);
+		return (error);
+	}
+
+	if (dmamap->dm_nsegs != inmap->dm_nsegs) {
+		bus_dmamap_unload(sc->sc_dmat, dmamap);
+		bus_dmamap_unload(sc->sc_dmat, inmap);
+		return (EFAULT);	/* "address" error, sort of. */
+	}
+
+	bus_dmamap_sync(sc->sc_dmat, dmamap, 0, dmamap->dm_mapsize,
+	    BUS_DMASYNC_PREWRITE);
+	bus_dmamap_sync(sc->sc_dmat, inmap, 0, inmap->dm_mapsize,
+	    BUS_DMASYNC_PREREAD);
+
+	prevp = (struct aau_desc_4 **) &sc->sc_firstdesc;
+	prevpa = &sc->sc_firstdesc_pa;
+
+	for (seg = 0; seg < dmamap->dm_nsegs; seg++) {
+		cur = pool_cache_get(&aau_desc_4_cache, PR_NOWAIT);
+		if (cur == NULL) {
+			*prevp = NULL;
+			error = ENOMEM;
+			goto bad;
+		}
+
+		*prevp = cur;
+		*prevpa = cur->d_pa;
+
+		prevp = &cur->d_next;
+		prevpa = &cur->d_nda;
+
+		if (dmamap->dm_segs[seg].ds_len !=
+		    inmap->dm_segs[seg].ds_len) {
+			*prevp = NULL;
+			error = EFAULT;	/* "address" error, sort of. */
+			goto bad;
+		}
+
+		cur->d_sar1 = inmap->dm_segs[seg].ds_addr;
+		cur->d_dar = dmamap->dm_segs[seg].ds_addr;
+		cur->d_bc = dmamap->dm_segs[seg].ds_len;
+		cur->d_dc = AAU_DC_B1_CC(AAU_DC_CC_DIRECT_FILL) | AAU_DC_DWE;
+		SYNC_DESC_4(cur);
+	}
+
+	*prevp = NULL;
+	*prevpa = 0;
+
+	cur->d_dc |= AAU_DC_IE;
+	SYNC_DESC_4(cur);
+
+	sc->sc_lastdesc = cur;
+
+	return (0);
+
+ bad:
+	iopaau_desc_4_free(sc, sc->sc_firstdesc);
+	bus_dmamap_unload(sc->sc_dmat, sc->sc_map_out);
+	bus_dmamap_unload(sc->sc_dmat, sc->sc_map_in[0]);
+	sc->sc_firstdesc = NULL;
+
+	return (error);
+}
+
+int
+iopaau_intr(void *arg)
+{
+	struct iopaau_softc *sc = arg;
+	struct dmover_request *dreq;
+	uint32_t asr;
+
+	/* Clear the interrupt. */
+	asr = bus_space_read_4(sc->sc_st, sc->sc_sh, AAU_ASR);
+	if (asr == 0)
+		return (0);
+	bus_space_write_4(sc->sc_st, sc->sc_sh, AAU_ASR, asr);
+
+	/* XXX -- why does this happen? */
+	if (sc->sc_running == NULL) {
+		printf("%s: unexpected interrupt, ASR = 0x%08x\n",
+		    sc->sc_dev.dv_xname, asr);
+		return (1);
+	}
+	dreq = sc->sc_running;
+
+	/* Stop the AAU. */
+	bus_space_write_4(sc->sc_st, sc->sc_sh, AAU_ACR, 0);
+
+	DPRINTF(("%s: got interrupt for dreq %p\n", sc->sc_dev.dv_xname,
+	    dreq));
+
+	if (__predict_false((asr & AAU_ASR_ETIF) != 0)) {
+		/*
+		 * We expect to get end-of-chain interrupts, not
+		 * end-of-transfer interrupts, so panic if we get
+		 * one of these.
+		 */
+		panic("aau_intr: got EOT interrupt");
+	}
+
+	if (__predict_false((asr & AAU_ASR_MA) != 0)) {
+		printf("%s: WARNING: got master abort\n", sc->sc_dev.dv_xname);
+		dreq->dreq_flags |= DMOVER_REQ_ERROR;
+		dreq->dreq_error = EFAULT;
+	}
+
+	/* Finish this transfer, start next one. */
+	iopaau_finish(sc);
+
+	return (1);
+}
+
+void
+iopaau_attach(struct iopaau_softc *sc)
+{
+	int error, i;
+
+	error = bus_dmamap_create(sc->sc_dmat, AAU_MAX_XFER, AAU_MAX_SEGS,
+	    AAU_MAX_XFER, AAU_IO_BOUNDARY, 0, &sc->sc_map_out);
+	if (error) {
+		printf("%s: unable to create output DMA map, error = %d\n",
+		    sc->sc_dev.dv_xname, error);
+		return;
+	}
+
+	for (i = 0; i < AAU_MAX_INPUTS; i++) {
+		error = bus_dmamap_create(sc->sc_dmat, AAU_MAX_XFER,
+		    AAU_MAX_SEGS, AAU_MAX_XFER, AAU_IO_BOUNDARY, 0,
+		    &sc->sc_map_in[i]);
+		if (error) {
+			printf("%s: unable to create input %d DMA map, "
+			    "error = %d\n", sc->sc_dev.dv_xname, i, error);
+			return;
+		}
+	}
+
+	/*
+	 * Initialize global resources.  Ok to do here, since there's
+	 * only one AAU.
+	 */
+	pool_init(&aau_desc_4_pool, sizeof(struct aau_desc_4),
+	    8 * 4, offsetof(struct aau_desc_4, d_nda), 0, "aaud4pl",
+	    NULL);
+	pool_cache_init(&aau_desc_4_cache, &aau_desc_4_pool, iopaau_desc_ctor,
+	    NULL, NULL);
+
+	/* Register us with dmover. */
+	dmover_backend_register(&sc->sc_dmb);
+}
