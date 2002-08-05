58556020173c3e294bbd964c729a7afacaf84b5c
itojun <itojun>
2002-08-05 23:37:48 +0000

    based on RFC2529, stf(4) should have 1480 as MTU, not 1280. tron found it, sync w/kame

--- sys/net/if_stf.c
+++ sys/net/if_stf.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: if_stf.c,v 1.26 2002/07/23 06:59:51 tron Exp $	*/
+/*	$NetBSD: if_stf.c,v 1.27 2002/08/05 23:37:48 itojun Exp $	*/
 /*	$KAME: if_stf.c,v 1.62 2001/06/07 22:32:16 itojun Exp $	*/
 
 /*
@@ -75,7 +75,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: if_stf.c,v 1.26 2002/07/23 06:59:51 tron Exp $");
+__KERNEL_RCSID(0, "$NetBSD: if_stf.c,v 1.27 2002/08/05 23:37:48 itojun Exp $");
 
 #include "opt_inet.h"
 
@@ -129,6 +129,8 @@ __KERNEL_RCSID(0, "$NetBSD: if_stf.c,v 1.26 2002/07/23 06:59:51 tron Exp $");
 
 #define IN6_IS_ADDR_6TO4(x)	(ntohs((x)->s6_addr16[0]) == 0x2002)
 #define GET_V4(x)	((struct in_addr *)(&(x)->s6_addr16[1]))
+
+#define STF_MTU		(1500 - 20)
 
 struct stf_softc {
 	struct ifnet	sc_if;	   /* common area */
@@ -210,7 +212,7 @@ stf_clone_create(ifc, unit)
 		return (EIO);	/* XXX */
 	}
 
-	sc->sc_if.if_mtu    = IPV6_MMTU;
+	sc->sc_if.if_mtu    = STF_MTU;
 	sc->sc_if.if_flags  = 0;
 	sc->sc_if.if_ioctl  = stf_ioctl;
 	sc->sc_if.if_output = stf_output;
@@ -692,7 +694,7 @@ stf_rtrequest(cmd, rt, info)
 {
 
 	if (rt)
-		rt->rt_rmx.rmx_mtu = IPV6_MMTU;
+		rt->rt_rmx.rmx_mtu = STF_MTU;
 }
 
 static int
