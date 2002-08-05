9c3da13e12b4616f643588189f55241cabab1995
simonb <simonb@NetBSD.org>
2002-08-05 02:13:14 +0000

    Use a __HAVE_BOOTINFO_H define to check for bootinfo support instead of speading port names in arch-dependant code.

--- sys/arch/newsmips/include/types.h
+++ sys/arch/newsmips/include/types.h
@@ -1,7 +1,9 @@
-/*	$NetBSD: types.h,v 1.4 2002/03/05 16:13:57 simonb Exp $	*/
+/*	$NetBSD: types.h,v 1.5 2002/08/05 02:13:15 simonb Exp $	*/
 
 #include <mips/types.h>
 
 #define	__BROKEN_CONFIG_UNIT_USAGE
 
+/* MIPS specific options */
+#define	__HAVE_BOOTINFO_H
 #define	__HAVE_MIPS_MACHDEP_CACHE_CONFIG
