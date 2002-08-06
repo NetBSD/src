301f1ebf316cd8765133213f1daadb7e260fefc7
chs <chs@NetBSD.org>
2002-08-06 06:14:33 +0000

    move more inlines to cpu.h: mftb(), mftbl() and mfpvr(). (the mftb() in pmap.c only wanted the lower 32 bits, so that's now mftbl()).

--- sys/arch/mvmeppc/mvmeppc/clock.c
+++ sys/arch/mvmeppc/mvmeppc/clock.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: clock.c,v 1.1 2002/02/27 21:02:23 scw Exp $	*/
+/*	$NetBSD: clock.c,v 1.2 2002/08/06 06:14:34 chs Exp $	*/
 /*      $OpenBSD: clock.c,v 1.3 1997/10/13 13:42:53 pefo Exp $  */
 
 /*
@@ -51,7 +51,6 @@ static volatile u_long lasttb;
 static todr_chip_handle_t clock_handle;
 
 void decr_intr __P((struct clockframe *)); /* Called from trap_subr.S */
-static inline u_quad_t mftb __P((void));
 void clock_rtc_config(todr_chip_handle_t);
 
 void
@@ -120,17 +119,6 @@ cpu_initclocks()
 	ticks_per_intr = ticks_per_sec / hz;
 	asm volatile ("mftb %0" : "=r"(lasttb));
 	asm volatile ("mtdec %0" :: "r"(ticks_per_intr));
-}
-
-static inline u_quad_t
-mftb()
-{
-	u_long scratch;
-	u_quad_t tb;
-	
-	asm ("1: mftbu %0; mftb %0+1; mftbu %1; cmpw %0,%1; bne 1b"
-	    : "=r"(tb), "=r"(scratch));
-	return tb;
 }
 
 /*
