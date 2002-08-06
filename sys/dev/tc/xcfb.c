4c1eb146215e0aabe05f696ac0fed0bba68a10c2
itojun <itojun@NetBSD.org>
2002-08-06 22:51:45 +0000

    integer overflow. from silvio@qualys.com

--- sys/dev/tc/xcfb.c
+++ sys/dev/tc/xcfb.c
@@ -1,4 +1,4 @@
-/* $NetBSD: xcfb.c,v 1.28 2002/07/04 14:37:14 junyoung Exp $ */
+/* $NetBSD: xcfb.c,v 1.29 2002/08/06 22:52:53 itojun Exp $ */
 
 /*
  * Copyright (c) 1998, 1999 Tohru Nishimura.  All rights reserved.
@@ -31,7 +31,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: xcfb.c,v 1.28 2002/07/04 14:37:14 junyoung Exp $");
+__KERNEL_RCSID(0, "$NetBSD: xcfb.c,v 1.29 2002/08/06 22:52:53 itojun Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -553,7 +553,7 @@ get_cmap(sc, p)
 {
 	u_int index = p->index, count = p->count;
 
-	if (index >= CMAP_SIZE || (index + count) > CMAP_SIZE)
+	if (index >= CMAP_SIZE || count > CMAP_SIZE - index)
 		return (EINVAL);
 
 	if (!uvm_useracc(p->red, count, B_WRITE) ||
