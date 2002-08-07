33e68631fe528aa35e98598fe59d8797fc29fd91
briggs <briggs>
2002-08-07 05:14:47 +0000

    Implement pmc(9) -- An interface to hardware performance monitoring counters.  These counters do not exist on all CPUs, but where they do exist, can be used for counting events such as dcache misses that would otherwise be difficult or impossible to instrument by code inspection or hardware simulation.
    pmc(9) is meant to be a general interface.  Initially, the Intel XScale
    counters are the only ones supported.

---
+++ sys/arch/dreamcast/include/pmc.h
@@ -0,0 +1,3 @@
+/*	$NetBSD: pmc.h,v 1.1 2002/08/07 05:15:11 briggs Exp $	*/
+
+/* No performance counters */
---
+++ sys/arch/hp700/include/pmc.h
@@ -0,0 +1,3 @@
+/*	$NetBSD: pmc.h,v 1.1 2002/08/07 05:15:20 briggs Exp $	*/
+
+#include <hppa/pmc.h>
