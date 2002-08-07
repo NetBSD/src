3ecef2ccc2b008d8a0a0ef02341f6a28d4f42d05
lukem <lukem@NetBSD.org>
2002-08-07 02:00:32 +0000

    Pull up revision 1.17 (requested by itojun in ticket #616): integer overflow. from silvio@qualys.com

--- sys/arch/hpcmips/dev/ite8181.c
+++ sys/arch/hpcmips/dev/ite8181.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: ite8181.c,v 1.16 2002/03/17 19:40:39 atatat Exp $	*/
+/*	$NetBSD: ite8181.c,v 1.16.6.1 2002/08/07 02:00:32 lukem Exp $	*/
 
 /*-
  * Copyright (c) 2000,2001 SATO Kazumi
@@ -645,7 +645,7 @@ ite8181_ioctl(v, cmd, data, flag, p)
 		if (sc->sc_fbconf.hf_class != HPCFB_CLASS_INDEXCOLOR ||
 		    sc->sc_fbconf.hf_pack_width != 8 ||
 		    256 <= cmap->index ||
-		    256 < (cmap->index + cmap->count))
+		    256 - cmap->index < cmap->count)
 			return (EINVAL);
 
 		if (!uvm_useracc(cmap->red, cmap->count, B_WRITE) ||
