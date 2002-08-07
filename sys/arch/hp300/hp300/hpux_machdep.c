879d9c86eba3d95066c61c3dace33dcee6f5a71b
lukem <lukem@NetBSD.org>
2002-08-07 01:30:56 +0000

    Pull up revision 1.30 (requested by itojun in ticket #616): correct range check, have overflow check, fix type mismatches, for cmap args and some other calls.  from openbsd

--- sys/arch/hp300/hp300/hpux_machdep.c
+++ sys/arch/hp300/hp300/hpux_machdep.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: hpux_machdep.c,v 1.28 2002/03/15 05:55:37 gmcgarry Exp $	*/
+/*	$NetBSD: hpux_machdep.c,v 1.28.6.1 2002/08/07 01:30:56 lukem Exp $	*/
 
 /*-
  * Copyright (c) 1996, 1997, 1998 The NetBSD Foundation, Inc.
@@ -78,7 +78,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: hpux_machdep.c,v 1.28 2002/03/15 05:55:37 gmcgarry Exp $");                                                  
+__KERNEL_RCSID(0, "$NetBSD: hpux_machdep.c,v 1.28.6.1 2002/08/07 01:30:56 lukem Exp $");                                                  
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -279,6 +279,9 @@ hpux_sys_getcontext(p, v, retval)
 	const char *str;
 	int l, i, error = 0;
 	int len; 
+
+	if (SCARG(uap, len) <= 0)
+		return (EINVAL);
 
 	for (i = 0; context_table[i].str != NULL; i++)
 		if (context_table[i].val == fputype)
