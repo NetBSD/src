2db21982af7bee3be1df1310e3aa35c0cac9096c
lukem <lukem@NetBSD.org>
2002-08-07 01:35:25 +0000

    Pull up revision 1.11 (requested by itojun in ticket #616): integer overflow.  reported by silvio@qualys.com

--- sys/arch/amiga/dev/grf_cv3d.c
+++ sys/arch/amiga/dev/grf_cv3d.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: grf_cv3d.c,v 1.10 2002/03/17 19:40:29 atatat Exp $ */
+/*	$NetBSD: grf_cv3d.c,v 1.10.6.1 2002/08/07 01:35:25 lukem Exp $ */
 
 /*
  * Copyright (c) 1995 Michael Teske
@@ -33,7 +33,7 @@
 #include "opt_amigacons.h"
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: grf_cv3d.c,v 1.10 2002/03/17 19:40:29 atatat Exp $");
+__KERNEL_RCSID(0, "$NetBSD: grf_cv3d.c,v 1.10.6.1 2002/08/07 01:35:25 lukem Exp $");
 
 #include "grfcv3d.h"
 #if NGRFCV3D > 0
@@ -962,7 +962,7 @@ cv3d_getcmap(struct grf_softc *gfp, struct grf_colormap *cmap)
 	if (cmap->count == 0 || cmap->index >= 256)
 		return (0);
 
-	if (cmap->index + cmap->count > 256)
+	if (cmap->count > 256 - cmap->index)
 		cmap->count = 256 - cmap->index;
 
 	/* first read colors out of the chip, then copyout to userspace */
