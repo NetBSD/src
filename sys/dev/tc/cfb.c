bac1a3279c038f56d6c72f4346f62763d5d4e2ac
lukem <lukem@NetBSD.org>
2002-08-07 01:57:33 +0000

    Pull up revision 1.34 (requested by itojun in ticket #616): integer overflow. from silvio@qualys.com

--- sys/dev/tc/cfb.c
+++ sys/dev/tc/cfb.c
@@ -1,4 +1,4 @@
-/* $NetBSD: cfb.c,v 1.32 2002/03/17 19:41:02 atatat Exp $ */
+/* $NetBSD: cfb.c,v 1.32.6.1 2002/08/07 01:57:33 lukem Exp $ */
 
 /*
  * Copyright (c) 1998, 1999 Tohru Nishimura.  All rights reserved.
@@ -31,7 +31,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: cfb.c,v 1.32 2002/03/17 19:41:02 atatat Exp $");
+__KERNEL_RCSID(0, "$NetBSD: cfb.c,v 1.32.6.1 2002/08/07 01:57:33 lukem Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -672,7 +672,7 @@ get_cmap(sc, p)
 {
 	u_int index = p->index, count = p->count;
 
-	if (index >= CMAP_SIZE || (index + count) > CMAP_SIZE)
+	if (index >= CMAP_SIZE || count) > CMAP_SIZE - index)
 		return (EINVAL);
 
 	if (!uvm_useracc(p->red, count, B_WRITE) ||
