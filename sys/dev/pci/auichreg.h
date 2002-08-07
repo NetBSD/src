83136f6ae4beb7db63cecddee13f02460ec0dcf7
kent <kent@NetBSD.org>
2002-08-07 10:31:09 +0000

    - Port clock calibration code (auich_calibrate) from ich.c of FreeBSD to solve   kern/16373. - SiS 7012 support from kern/17716 submitted by Quentin Garnier. - ICH4 support - Experimental support for nForce MCP, AMD768 and AMD8111. - Rename the short name for ICH3: "i830M" -> "ICH3"

--- sys/dev/pci/auichreg.h
+++ sys/dev/pci/auichreg.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: auichreg.h,v 1.2 2000/11/28 16:57:16 thorpej Exp $	*/
+/*	$NetBSD: auichreg.h,v 1.3 2002/08/07 10:31:09 kent Exp $	*/
 
 /*
  * Copyright (c) 2000 Michael Shalayeff
@@ -70,6 +70,16 @@
 #define	ICH_MICI	0x20
 
 #define	ICH_GCTRL	0x2c
+#define		ICH_SSM_78	0x40000000 /* S/PDIF slots 7 and 8 */
+#define		ICH_SSM_69	0x80000000 /* S/PDIF slots 6 and 9 */
+#define		ICH_SSM_1011	0xc0000000 /* S/PDIF slots 10 and 11 */
+#define		ICH_POM16	0x000000 /* PCM out precision 16bit */
+#define		ICH_POM20	0x400000 /* PCM out precision 20bit */
+#define		ICH_PCM246_MASK	0x300000
+#define		 ICH_PCM2	0x000000 /* 2ch output */
+#define		 ICH_PCM4	0x100000 /* 4ch output */
+#define		 ICH_PCM6	0x200000 /* 6ch output */
+#define		ICH_S2RIE	0x40	/* int when tertiary codec resume */
 #define		ICH_SRIE	0x20	/* int when 2ndary codec resume */
 #define		ICH_PRIE	0x10	/* int when primary codec resume */
 #define		ICH_ACLSO	0x08	/* aclink shut off */
@@ -77,6 +87,14 @@
 #define		ICH_CRESET	0x02	/* cold reset */
 #define		ICH_GIE		0x01	/* gpi int enable */
 #define	ICH_GSTS	0x30
+#define		ICH_S2RI	0x20000000 /* tertiary resume int */
+#define		ICH_S2CR	0x10000000 /* tertiary codec ready */
+#define		ICH_BCS		0x08000000 /* bit clock stopped */
+#define		ICH_SPINT	0x04000000 /* S/PDIF int */
+#define		ICH_P2INT	0x02000000 /* PCM-In 2 int */
+#define		ICH_M2INT	0x01000000 /* mic 2 int */
+#define		ICH_SAMPLE_CAP	0x00c00000 /* sampling precision capability */
+#define		ICH_CHAN_CAP	0x00300000 /* multi-channel capability */
 #define		ICH_MD3		0x20000	/* pwr-dn semaphore for modem */
 #define		ICH_AD3		0x10000	/* pwr-dn semaphore for audio */
 #define		ICH_RCS		0x08000	/* read completion status */
