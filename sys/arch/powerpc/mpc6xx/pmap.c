3c534e2fa80ae41c52379b840c431078aa8c16e2
nathanw <nathanw@NetBSD.org>
2002-08-08 20:19:57 +0000

    Pull up pmap fix.

--- sys/arch/powerpc/mpc6xx/pmap.c
+++ sys/arch/powerpc/mpc6xx/pmap.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: pmap.c,v 1.28.4.11 2002/08/06 22:47:11 nathanw Exp $	*/
+/*	$NetBSD: pmap.c,v 1.28.4.12 2002/08/08 20:19:57 nathanw Exp $	*/
 /*-
  * Copyright (c) 2001 The NetBSD Foundation, Inc.
  * All rights reserved.
@@ -1384,7 +1384,7 @@ pmap_enter(pmap_t pm, vaddr_t va, paddr_t pa, vm_prot_t prot, int flags)
 	 * If this is a managed page, and it's the first reference to the
 	 * page clear the execness of the page.  Otherwise fetch the execness.
 	 */
-#ifndef MULTIPROCESSOR
+#if !defined(MULTIPROCESSOR) && 0 /* disable for now */
 	/* XXX more is needed for MP */
 	if (pg != NULL)
 		was_exec = pmap_attr_fetch(pg) & PTE_EXEC;
