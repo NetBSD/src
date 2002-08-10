791a4c8c339f095f0028f8e1721b9e8e80aadc4b
itojun <itojun@NetBSD.org>
2002-08-10 00:42:45 +0000

    have a choice of bcrypt passwd.  not sure if we should set ypcipher to something other than "old"...

--- distrib/utils/sysinst/util.c
+++ distrib/utils/sysinst/util.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: util.c,v 1.67.2.3 2002/06/29 23:26:02 lukem Exp $	*/
+/*	$NetBSD: util.c,v 1.67.2.4 2002/08/10 00:42:45 itojun Exp $	*/
 
 /*
  * Copyright 1997 Piermont Information Systems Inc.
@@ -1080,6 +1080,15 @@ set_crypt_type(void)
 		    "default:\n"
 		    "  localcipher = md5\n"
 		    "  ypcipher = md5\n");
+		fclose(pwc);
+		break;
+	case 3:	/* blowfish,7 */
+		rename(fn, target_expand("/etc/passwd.conf.pre-sysinst"));
+		pwc = fopen(fn, "w");
+		fprintf(pwc,
+		    "default:\n"
+		    "  localcipher = blowfish,7\n"
+		    "  ypcipher = blowfish,7\n");
 		fclose(pwc);
 		break;
 	}
