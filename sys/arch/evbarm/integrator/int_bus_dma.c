a7d44c2503ad383fb6a154097c340d1d9f435c63
thorpej <thorpej@NetBSD.org>
2002-08-17 20:46:26 +0000

    Use separate function pointers for dmamap_sync pre- vs post- operations. Change the bus_dmamap_sync() macro to test the ops argument against pre- and post- constants.  The compiler will optimize out dead code because of the constants.  Since post- operations are not needed on ARM (except for ISA bounce buffers), this eliminate a large number of function calls which are noops, each of which cost at least 6 cycles just in the call and return overhead (not to mention whatever other useless work the compiler decides to do in the callee).

--- sys/arch/evbarm/integrator/int_bus_dma.c
+++ sys/arch/evbarm/integrator/int_bus_dma.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: int_bus_dma.c,v 1.10 2002/07/31 17:34:26 thorpej Exp $	*/
+/*	$NetBSD: int_bus_dma.c,v 1.11 2002/08/17 20:46:28 thorpej Exp $	*/
 
 /*
  * Copyright (c) 2002 Wasabi Systems, Inc.
@@ -72,7 +72,8 @@ integrator_pci_dma_init(bus_dma_tag_t dmat)
 	dmat->_dmamap_load_uio = _bus_dmamap_load_uio;
 	dmat->_dmamap_load_raw = _bus_dmamap_load_raw;
 	dmat->_dmamap_unload = _bus_dmamap_unload;
-	dmat->_dmamap_sync = _bus_dmamap_sync;
+	dmat->_dmamap_sync_pre = _bus_dmamap_sync;
+	dmat->_dmamap_sync_post = NULL;
 
 	dmat->_dmamem_alloc = _bus_dmamem_alloc;
 	dmat->_dmamem_free = _bus_dmamem_free;
