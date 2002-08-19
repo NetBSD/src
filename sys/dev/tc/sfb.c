d9c7b74b4092c5610fde51cfa838ef5872c76253
itohy <itohy@NetBSD.org>
2002-08-19 03:52:53 +0000

    Make it compile again. Hi, itojun!

--- sys/dev/tc/sfb.c
+++ sys/dev/tc/sfb.c
@@ -1,4 +1,4 @@
-/* $NetBSD: sfb.c,v 1.53 2002/08/06 22:52:30 itojun Exp $ */
+/* $NetBSD: sfb.c,v 1.54 2002/08/19 03:52:53 itohy Exp $ */
 
 /*
  * Copyright (c) 1998, 1999 Tohru Nishimura.  All rights reserved.
@@ -31,7 +31,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: sfb.c,v 1.53 2002/08/06 22:52:30 itojun Exp $");
+__KERNEL_RCSID(0, "$NetBSD: sfb.c,v 1.54 2002/08/19 03:52:53 itohy Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -702,7 +702,7 @@ get_cmap(sc, p)
 {
 	u_int index = p->index, count = p->count;
 
-	if (index >= CMAP_SIZE || count) > CMAP_SIZE - index)
+	if (index >= CMAP_SIZE || count > CMAP_SIZE - index)
 		return (EINVAL);
 
 	if (!uvm_useracc(p->red, count, B_WRITE) ||
