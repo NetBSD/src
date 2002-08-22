03b3f2575521a42d0d073ee9086592dbd6853e98
lukem <lukem>
2002-08-22 00:00:09 +0000

    Pull up revision 1.24 (requested by itojun in ticket #713): check packet length before fetching ESP crypto checksum.  sync w/kame

--- sys/netinet6/esp_input.c
+++ sys/netinet6/esp_input.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: esp_input.c,v 1.21 2002/03/18 15:30:03 itojun Exp $	*/
+/*	$NetBSD: esp_input.c,v 1.21.6.1 2002/08/22 00:00:09 lukem Exp $	*/
 /*	$KAME: esp_input.c,v 1.60 2001/09/04 08:43:19 itojun Exp $	*/
 
 /*
@@ -35,7 +35,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: esp_input.c,v 1.21 2002/03/18 15:30:03 itojun Exp $");
+__KERNEL_RCSID(0, "$NetBSD: esp_input.c,v 1.21.6.1 2002/08/22 00:00:09 lukem Exp $");
 
 #include "opt_inet.h"
 
@@ -216,6 +216,10 @@ esp4_input(m, va_alist)
 	if (!sumalgo)
 		goto noreplaycheck;
 	siz = (((*sumalgo->sumsiz)(sav) + 3) & ~(4 - 1));
+	if (m->m_pkthdr.len < off + ESPMAXLEN + siz) {
+		ipsecstat.in_inval++;
+		goto bad;
+	}
 	if (AH_MAXSUMSIZE < siz) {
 		ipseclog((LOG_DEBUG,
 		    "internal error: AH_MAXSUMSIZE must be larger than %lu\n",
@@ -630,6 +634,10 @@ esp6_input(mp, offp, proto)
 	if (!sumalgo)
 		goto noreplaycheck;
 	siz = (((*sumalgo->sumsiz)(sav) + 3) & ~(4 - 1));
+	if (m->m_pkthdr.len < off + ESPMAXLEN + siz) {
+		ipsecstat.in_inval++;
+		goto bad;
+	}
 	if (AH_MAXSUMSIZE < siz) {
 		ipseclog((LOG_DEBUG,
 		    "internal error: AH_MAXSUMSIZE must be larger than %lu\n",
