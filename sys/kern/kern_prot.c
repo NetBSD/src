b30ef0f3e3d7ac8abafe081a1c555317a82a782c
thorpej <thorpej@NetBSD.org>
2002-08-25 21:30:40 +0000

    Fix some signed/unsigned comparison warnings from GCC 3.3.

--- sys/kern/kern_prot.c
+++ sys/kern/kern_prot.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: kern_prot.c,v 1.68 2001/12/06 23:11:59 christos Exp $	*/
+/*	$NetBSD: kern_prot.c,v 1.69 2002/08/25 21:30:40 thorpej Exp $	*/
 
 /*
  * Copyright (c) 1982, 1986, 1989, 1990, 1991, 1993
@@ -45,7 +45,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: kern_prot.c,v 1.68 2001/12/06 23:11:59 christos Exp $");
+__KERNEL_RCSID(0, "$NetBSD: kern_prot.c,v 1.69 2002/08/25 21:30:40 thorpej Exp $");
 
 #include "opt_compat_43.h"
 
@@ -249,14 +249,15 @@ sys_getgroups(p, v, retval)
 		syscallarg(gid_t *) gidset;
 	} */ *uap = v;
 	struct pcred *pc = p->p_cred;
-	int ngrp;
+	u_int ngrp;
 	int error;
 
-	ngrp = SCARG(uap, gidsetsize);
-	if (ngrp == 0) {
+	if (SCARG(uap, gidsetsize) == 0) {
 		*retval = pc->pc_ucred->cr_ngroups;
 		return (0);
-	}
+	} else if (SCARG(uap, gidsetsize) < 0)
+		return (EINVAL);
+	ngrp = SCARG(uap, gidsetsize);
 	if (ngrp < pc->pc_ucred->cr_ngroups)
 		return (EINVAL);
 	ngrp = pc->pc_ucred->cr_ngroups;
@@ -616,7 +617,7 @@ sys_setgroups(p, v, retval)
 	/*
 	 * Check if this is a no-op.
 	 */
-	if (pc->pc_ucred->cr_ngroups == ngrp &&
+	if (pc->pc_ucred->cr_ngroups == (u_int) ngrp &&
 	    memcmp(grp, pc->pc_ucred->cr_groups, grsize) == 0)
 		return (0);
 
