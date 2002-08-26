750d298cb0a98c85cbd7fe00407632547afe02aa
fredb <fredb@NetBSD.org>
2002-08-26 22:36:24 +0000

    Follow AMD's recommendations for programming the uncachable/write-combine bits of the K6 cache-control-register: disable the cache; flush the cache; set the bits; re-enable the cache (all much like programming the pentium mtrr's). See reference posted to tech-kern; also review there by thorpej.

--- sys/arch/i386/i386/mtrr_k6.c
+++ sys/arch/i386/i386/mtrr_k6.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: mtrr_k6.c,v 1.2 2001/11/15 07:03:30 lukem Exp $	*/
+/*	$NetBSD: mtrr_k6.c,v 1.3 2002/08/26 22:36:24 fredb Exp $	*/
 
 /*
  * Copyright 2001 Wasabi Systems, Inc.
@@ -40,7 +40,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: mtrr_k6.c,v 1.2 2001/11/15 07:03:30 lukem Exp $");
+__KERNEL_RCSID(0, "$NetBSD: mtrr_k6.c,v 1.3 2002/08/26 22:36:24 fredb Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -102,9 +102,14 @@ static void
 k6_mtrr_reload(void)
 {
 	uint64_t uwccr;
+	uint32_t origcr0, cr0;
 	int i;
 
 	disable_intr();
+
+	origcr0 = cr0 = rcr0();
+	cr0 |= CR0_CD;
+	lcr0(cr0);
 
 	wbinvd();
 
@@ -115,7 +120,7 @@ k6_mtrr_reload(void)
 
 	wrmsr(MSR_K6_UWCCR, uwccr);
 
-	wbinvd();
+	lcr0(origcr0);
 
 	enable_intr();
 }
