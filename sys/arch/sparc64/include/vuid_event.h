d3d2788c00dc3f5ee85695ec9a745db17f1208cb
uwe <uwe@NetBSD.org>
2002-08-27 09:47:41 +0000

    Make sun ports use common keyboard/firm events related includes from dev/sun.

--- sys/arch/sparc64/include/vuid_event.h
+++ sys/arch/sparc64/include/vuid_event.h
@@ -1,3 +1,3 @@
-/*       $NetBSD: vuid_event.h,v 1.2 2002/07/20 11:52:22 mrg Exp $        */
+/*	$NetBSD: vuid_event.h,v 1.3 2002/08/27 09:47:46 uwe Exp $	*/
 
-#include <sparc/vuid_event.h>
+#include <dev/sun/vuid_event.h>
