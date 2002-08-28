2284920498b6d52fd02a51f0c6032b3e43603c98
gmcgarry <gmcgarry@NetBSD.org>
2002-08-28 08:56:59 +0000

    RAS support for m68k.

--- sys/arch/hp300/include/types.h
+++ sys/arch/hp300/include/types.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: types.h,v 1.11 2002/02/28 03:17:33 simonb Exp $	*/
+/*	$NetBSD: types.h,v 1.12 2002/08/28 08:56:59 gmcgarry Exp $	*/
 
 #ifndef _MACHINE_TYPES_H_
 #define	_MACHINE_TYPES_H_
@@ -7,5 +7,6 @@
 
 #define	__HAVE_DEVICE_REGISTER
 #define	__HAVE_GENERIC_SOFT_INTERRUPTS
+#define __HAVE_RAS
 
 #endif
