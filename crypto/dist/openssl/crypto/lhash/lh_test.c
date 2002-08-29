79f3688c4683edd11f566a97be31e9ed6f8e9d2e
he <he@NetBSD.org>
2002-08-29 16:48:03 +0000

    Pull up revisions 1.1.1.2-1.1.1.3 (requested by itojun,he):   Update OpenSSL to version 0.9.6g.  Includes major version bump for   libcrypto and all the dependent libraries.

--- crypto/dist/openssl/crypto/lhash/lh_test.c
+++ crypto/dist/openssl/crypto/lhash/lh_test.c
@@ -75,9 +75,8 @@ main()
 		buf[0]='\0';
 		fgets(buf,256,stdin);
 		if (buf[0] == '\0') break;
-		buf[256]='\0';
 		i=strlen(buf);
-		p=Malloc(i+1);
+		p=OPENSSL_malloc(i+1);
 		memcpy(p,buf,i+1);
 		lh_insert(conf,p);
 		}
