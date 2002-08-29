77e34ef41566f7f4a3840e8b660d78f7bd7a5922
he <he@NetBSD.org>
2002-08-29 16:50:09 +0000

    Pull up revisions 1.3-1.4 (requested by itojun,he):   Update OpenSSL to version 0.9.6g.  Includes major version bump for   libcrypto and all the dependent libraries.

--- crypto/dist/openssl/crypto/mdc2/mdc2.h
+++ crypto/dist/openssl/crypto/mdc2/mdc2.h
@@ -59,11 +59,11 @@
 #ifndef HEADER_MDC2_H
 #define HEADER_MDC2_H
 
+#include <openssl/des.h>
+
 #ifdef  __cplusplus
 extern "C" {
 #endif
-
-#include <openssl/des.h>
 
 
 #define MDC2_BLOCK              8
