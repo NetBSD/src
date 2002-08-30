eda03f8ab2dab28009e652c1f96187800671d49c
scw <scw@NetBSD.org>
2002-08-30 10:50:06 +0000

    Re-arrange the endian/byteswap headers a bit by splitting off the byte swap code into a separate file and renaming them. Fix a typo which prevented ntohX and htonX macros DTRT for little-endian mode.

--- sys/arch/sh5/include/endian_machdep.h
+++ sys/arch/sh5/include/endian_machdep.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: endian_machdep.h,v 1.2 2002/07/12 19:33:27 scw Exp $	*/
+/*	$NetBSD: endian_machdep.h,v 1.3 2002/08/30 10:50:07 scw Exp $	*/
 
 #ifdef __LITTLE_ENDIAN__
 #define _BYTE_ORDER     _LITTLE_ENDIAN
@@ -8,18 +8,13 @@
 
 #ifdef	__GNUC__
 
-#include <sh5/bswap.h>
+#include <sh5/byte_swap.h>
 
-#if _BTYE_ORDER == _LITTLE_ENDIAN
-#define	ntohl(x)	((in_addr_t)bswap32(x))
-#define	ntohs(x)	((in_port_t)bswap16(x))
-#define	htonl(x)	((in_addr_t)bswap32(x))
-#define	htons(x)	((in_port_t)bswap16(x))
-#else
-#define	ntohl(x)	(x)
-#define	ntohs(x)	(x)
-#define	htonl(x)	(x)
-#define	htons(x)	(x)
+#if _BYTE_ORDER == _LITTLE_ENDIAN
+#define	ntohl(x)	((in_addr_t)_sh5_bswap32((in_addr_t)(x)))
+#define	ntohs(x)	((in_port_t)_sh5_bswap16((in_port_t)(x)))
+#define	htonl(x)	((in_addr_t)_sh5_bswap32((in_addr_t)(x)))
+#define	htons(x)	((in_port_t)_sh5_bswap16((in_port_t)(x)))
 #endif
 
 #endif	/* __GNUC__ */
