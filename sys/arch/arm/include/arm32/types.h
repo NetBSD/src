77ca2ce15744b229b6dec273f8c9586f7a53e2ad
thorpej <thorpej@NetBSD.org>
2002-08-31 03:07:32 +0000

    Add machine-dependent bits of RAS for arm32.

--- sys/arch/arm/include/arm32/types.h
+++ sys/arch/arm/include/arm32/types.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: types.h,v 1.1 2001/11/22 17:59:58 thorpej Exp $	*/
+/*	$NetBSD: types.h,v 1.2 2002/08/31 03:07:32 thorpej Exp $	*/
 
 /*
  * Copyright (c) 2001 Wasabi Systems, Inc.
@@ -43,5 +43,7 @@
 #endif
 
 #include <arm/types.h>		/* pull in generic ARM definitions */
+
+#define	__HAVE_RAS
 
 #endif /* _ARM_ARM32_TYPES_H_ */
