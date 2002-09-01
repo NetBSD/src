bf18041e759c68b657eeffdb17a0802cc3277a5f
lukem <lukem@NetBSD.org>
2002-09-01 23:45:43 +0000

    now at RC3.  branch is locked down, ready for release.

--- sys/conf/osrelease.sh
+++ sys/conf/osrelease.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-#	$NetBSD: osrelease.sh,v 1.75.6.8 2002/08/27 12:59:03 lukem Exp $
+#	$NetBSD: osrelease.sh,v 1.75.6.9 2002/09/01 23:45:43 lukem Exp $
 #
 # Copyright (c) 1997 The NetBSD Foundation, Inc.
 # All rights reserved.
@@ -42,7 +42,7 @@
 #	sys/sys/param.h:	__NetBSD_Version__
 #	share/tmac/doc-common:	ds oS
 #
-release=1.6_RC2
+release=1.6_RC3
 
 case $1 in
 -s)
