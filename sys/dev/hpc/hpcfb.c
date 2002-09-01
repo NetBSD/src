6a286a4f2a2f9fb1bcf97fb09acb68d0f9749a5d
takemura <takemura@NetBSD.org>
2002-09-01 11:38:29 +0000

    BUGFIX: WSDISPLAYIO_SVIDEO and WSDISPLAYIO_GVIDEO don't work.

--- sys/dev/hpc/hpcfb.c
+++ sys/dev/hpc/hpcfb.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: hpcfb.c,v 1.21 2002/07/04 14:37:10 junyoung Exp $	*/
+/*	$NetBSD: hpcfb.c,v 1.22 2002/09/01 11:38:29 takemura Exp $	*/
 
 /*-
  * Copyright (c) 1999
@@ -43,13 +43,13 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: hpcfb.c,v 1.21 2002/07/04 14:37:10 junyoung Exp $");
+__KERNEL_RCSID(0, "$NetBSD: hpcfb.c,v 1.22 2002/09/01 11:38:29 takemura Exp $");
 
 #define FBDEBUG
 static const char _copyright[] __attribute__ ((unused)) =
     "Copyright (c) 1999 Shin Takemura.  All rights reserved.";
 static const char _rcsid[] __attribute__ ((unused)) =
-    "$NetBSD: hpcfb.c,v 1.21 2002/07/04 14:37:10 junyoung Exp $";
+    "$NetBSD: hpcfb.c,v 1.22 2002/09/01 11:38:29 takemura Exp $";
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -603,6 +603,8 @@ hpcfb_ioctl(void *v, u_long cmd, caddr_t data, int flag, struct proc *p)
 
 	case WSDISPLAYIO_GETCMAP:
 	case WSDISPLAYIO_PUTCMAP:
+	case WSDISPLAYIO_SVIDEO:
+	case WSDISPLAYIO_GVIDEO:
 	case WSDISPLAYIO_GETPARAM:
 	case WSDISPLAYIO_SETPARAM:
 	case HPCFBIO_GCONF:
