f1e72e0da2697db9ae8c37326a30519fd89d414d
gehenna <gehenna@NetBSD.org>
2002-09-01 12:16:27 +0000

    revive machine dependent conf.c for iskmemdev().

---
+++ sys/arch/sbmips/sbmips/conf.c
@@ -0,0 +1,7 @@
+/* $NetBSD: conf.c,v 1.3.4.7 2002/09/01 12:16:27 gehenna Exp $ */
+
+#include <sys/cdefs.h>			/* RCS ID & Copyright macro defns */
+
+__KERNEL_RCSID(0, "$NetBSD: conf.c,v 1.3.4.7 2002/09/01 12:16:27 gehenna Exp $");
+
+#include "arch/evbmips/evbmips/conf_common.c"
