c850af1408a7051db94196f98874c6abcbd8b73a
oster <oster@NetBSD.org>
2002-09-07 23:11:46 +0000

    Protect rf_PrintDAGList() in a few more places.  Thanks to Monroe Williams on current-users for the patch.

--- sys/dev/raidframe/rf_evenodd.c
+++ sys/dev/raidframe/rf_evenodd.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: rf_evenodd.c,v 1.7 2001/11/13 07:11:14 lukem Exp $	*/
+/*	$NetBSD: rf_evenodd.c,v 1.8 2002/09/07 23:11:46 oster Exp $	*/
 /*
  * Copyright (c) 1995 Carnegie-Mellon University.
  * All rights reserved.
@@ -33,7 +33,7 @@
  ****************************************************************************************/
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: rf_evenodd.c,v 1.7 2001/11/13 07:11:14 lukem Exp $");
+__KERNEL_RCSID(0, "$NetBSD: rf_evenodd.c,v 1.8 2002/09/07 23:11:46 oster Exp $");
 
 #include "rf_archs.h"
 
@@ -433,10 +433,12 @@ rf_VerifyParityEvenOdd(raidPtr, raidAddr, parityPDA, correct_it, flags)
 	memset((char *) &tracerec, 0, sizeof(tracerec));
 	rd_dag_h->tracerec = &tracerec;
 
+#if RF_DEBUG_VALIDATE_DAG
 	if (rf_verifyParityDebug) {
 		printf("Parity verify read dag:\n");
 		rf_PrintDAGList(rd_dag_h);
 	}
+#endif
 	RF_LOCK_MUTEX(mcpair->mutex);
 	mcpair->flag = 0;
 	rf_DispatchDAG(rd_dag_h, (void (*) (void *)) rf_MCPairWakeupFunc,
@@ -493,10 +495,12 @@ rf_VerifyParityEvenOdd(raidPtr, raidAddr, parityPDA, correct_it, flags)
 		wrBlock->succedents[0]->params[3].v = RF_CREATE_PARAM3(RF_IO_NORMAL_PRIORITY, 0, 0, which_ru);
 		memset((char *) &tracerec, 0, sizeof(tracerec));
 		wr_dag_h->tracerec = &tracerec;
+#if RF_DEBUG_VALIDATE_DAG
 		if (rf_verifyParityDebug) {
 			printf("Parity verify write dag:\n");
 			rf_PrintDAGList(wr_dag_h);
 		}
+#endif
 		RF_LOCK_MUTEX(mcpair->mutex);
 		mcpair->flag = 0;
 		rf_DispatchDAG(wr_dag_h, (void (*) (void *)) rf_MCPairWakeupFunc,
@@ -522,10 +526,12 @@ rf_VerifyParityEvenOdd(raidPtr, raidAddr, parityPDA, correct_it, flags)
 		wrBlock->succedents[0]->params[3].v = RF_CREATE_PARAM3(RF_IO_NORMAL_PRIORITY, 0, 0, which_ru);
 		memset((char *) &tracerec, 0, sizeof(tracerec));
 		wr_dag_h->tracerec = &tracerec;
+#if RF_DEBUG_VALIDATE_DAG
 		if (rf_verifyParityDebug) {
 			printf("Dag of write new second redundant information in parity verify :\n");
 			rf_PrintDAGList(wr_dag_h);
 		}
+#endif
 		RF_LOCK_MUTEX(mcpair->mutex);
 		mcpair->flag = 0;
 		rf_DispatchDAG(wr_dag_h, (void (*) (void *)) rf_MCPairWakeupFunc,
