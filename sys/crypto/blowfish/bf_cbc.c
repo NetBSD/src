c242f201c549ee4b4084a91d18945c0cb11de732
elric <elric@NetBSD.org>
2002-09-08 07:52:41 +0000

    Modified to avoid compiler warnings.  The specific warning related to const BF_KEY * vars, and I chose to ``fix'' it in this file rather than elsewhere in the framework because, although the other fix was more appropriate, nothing seems to use the code in this file and hence the risk of disrupting other people was lower.  In the future, the more appropriate change would be to change blowfish.h and bf_enc.c to have functions with signatures:
    BF_encrypt(BF_LONG *, const BF_KEY *);
    BF_decrypt(BF_LONG *, const BF_KEY *);

--- sys/crypto/blowfish/bf_cbc.c
+++ sys/crypto/blowfish/bf_cbc.c
@@ -57,7 +57,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: bf_cbc.c,v 1.7 2002/09/07 23:35:37 elric Exp $");
+__KERNEL_RCSID(0, "$NetBSD: bf_cbc.c,v 1.8 2002/09/08 07:52:41 elric Exp $");
 
 #include <sys/types.h>
 
@@ -85,7 +85,7 @@ void BF_cbc_encrypt(const unsigned char *in, unsigned char *out, long length,
 			tin1^=tout1;
 			tin[0]=tin0;
 			tin[1]=tin1;
-			BF_encrypt(tin,schedule);
+			BF_encrypt(tin,(BF_KEY *)schedule);
 			tout0=tin[0];
 			tout1=tin[1];
 			l2n(tout0,out);
@@ -98,7 +98,7 @@ void BF_cbc_encrypt(const unsigned char *in, unsigned char *out, long length,
 			tin1^=tout1;
 			tin[0]=tin0;
 			tin[1]=tin1;
-			BF_encrypt(tin,schedule);
+			BF_encrypt(tin,(BF_KEY *)schedule);
 			tout0=tin[0];
 			tout1=tin[1];
 			l2n(tout0,out);
@@ -118,7 +118,7 @@ void BF_cbc_encrypt(const unsigned char *in, unsigned char *out, long length,
 			n2l(in,tin1);
 			tin[0]=tin0;
 			tin[1]=tin1;
-			BF_decrypt(tin,schedule);
+			BF_decrypt(tin,(BF_KEY *)schedule);
 			tout0=tin[0]^xor0;
 			tout1=tin[1]^xor1;
 			l2n(tout0,out);
@@ -132,7 +132,7 @@ void BF_cbc_encrypt(const unsigned char *in, unsigned char *out, long length,
 			n2l(in,tin1);
 			tin[0]=tin0;
 			tin[1]=tin1;
-			BF_decrypt(tin,schedule);
+			BF_decrypt(tin,(BF_KEY *)schedule);
 			tout0=tin[0]^xor0;
 			tout1=tin[1]^xor1;
 			l2nn(tout0,tout1,out,l+8);
