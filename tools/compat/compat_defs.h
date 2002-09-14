9b6d4327700ae087c324cf220b8f92d6f52f7980
thorpej <thorpej@NetBSD.org>
2002-09-14 04:30:27 +0000

    * Check for mkstemp() and mkdtemp(), and build them into libnbcompat   if necessary. * mktemp is now a host-tool, so nuke the lame script (which isn't even   properly installed during the tools bootstrap).

--- tools/compat/compat_defs.h
+++ tools/compat/compat_defs.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: compat_defs.h,v 1.17 2002/09/13 23:35:34 bjh21 Exp $	*/
+/*	$NetBSD: compat_defs.h,v 1.18 2002/09/14 04:30:27 thorpej Exp $	*/
 
 #ifndef	__NETBSD_COMPAT_DEFS_H__
 #define	__NETBSD_COMPAT_DEFS_H__
@@ -188,6 +188,19 @@ int issetugid(void);
 
 #define bswap64(x)	(((u_int64_t)bswap32((x)) << 32) | \
 			 ((u_int64_t)bswap32((x) >> 32)))
+#endif
+
+#if !HAVE_MKSTEMP
+int mkstemp(char *);
+#endif
+
+#if !HAVE_MKDTEMP
+char *mkdtemp(char *);
+#endif
+
+#if !HAVE_MKSTEMP || !HAVE_MKDTEMP
+/* This is a prototype for the internal function. */
+int gettemp(char *, int *, int);
 #endif
 
 #if !HAVE_PREAD
