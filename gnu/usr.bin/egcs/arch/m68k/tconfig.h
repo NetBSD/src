71603d5cb85f71cf8259d55cffd1903253b94ec0
thorpej <thorpej@NetBSD.org>
2002-09-16 16:22:15 +0000

    Delete the egcs/binutils 2.9 toolchain.  Remove USE_NEW_TOOLCHAIN conditionals.

--- gnu/usr.bin/egcs/arch/m68k/tconfig.h
+++
@@ -1,3 +0,0 @@
-/*	$NetBSD: tconfig.h,v 1.2 1998/08/17 20:26:06 tv Exp $	*/
-
-#include "m68k/xm-netbsd.h"
--- gnu/usr.bin/gas.new/arch/alpha/targ-cpu.h
+++
@@ -1,3 +0,0 @@
-/*	$NetBSD: targ-cpu.h,v 1.1 1998/08/27 16:09:20 tv Exp $	*/
-
-#include "tc-alpha.h"
