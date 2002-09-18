46f216c4d1561e9a234016b0066c58776c54c3c5
mycroft <mycroft@NetBSD.org>
2002-09-18 23:16:13 +0000

    Fix pluralization in a syslog message.

--- usr.sbin/bootp/bootpd/bootpd.c
+++ usr.sbin/bootp/bootpd/bootpd.c
@@ -22,7 +22,7 @@ SOFTWARE.
 
 #include <sys/cdefs.h>
 #ifndef lint
-__RCSID("$NetBSD: bootpd.c,v 1.15 2002/09/18 23:13:39 mycroft Exp $");
+__RCSID("$NetBSD: bootpd.c,v 1.16 2002/09/18 23:16:13 mycroft Exp $");
 #endif
 
 /*
@@ -510,8 +510,9 @@ main(int argc, char **argv)
 		}
 		if (nfound == 0) {
 			if (debug > 1)
-				report(LOG_INFO, "exiting after %d minutes of inactivity",
-					   actualtimeout / 60000);
+				report(LOG_INFO, "exiting after %d minute%s of inactivity",
+					   actualtimeout / 60000,
+					   actualtimeout == 60000 ? "" : "s");
 			exit(0);
 		}
 		ra_len = sizeof(recv_addr);
