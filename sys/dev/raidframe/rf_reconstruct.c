d497178f261037544a9dd047f7b0d080361b3f69
oster <oster@NetBSD.org>
2002-09-19 22:41:51 +0000

    One signal will do, thanks.

--- sys/dev/raidframe/rf_reconstruct.c
+++ sys/dev/raidframe/rf_reconstruct.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: rf_reconstruct.c,v 1.41 2002/09/17 03:30:33 oster Exp $	*/
+/*	$NetBSD: rf_reconstruct.c,v 1.42 2002/09/19 22:41:51 oster Exp $	*/
 /*
  * Copyright (c) 1995 Carnegie-Mellon University.
  * All rights reserved.
@@ -33,7 +33,7 @@
  ************************************************************/
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: rf_reconstruct.c,v 1.41 2002/09/17 03:30:33 oster Exp $");
+__KERNEL_RCSID(0, "$NetBSD: rf_reconstruct.c,v 1.42 2002/09/19 22:41:51 oster Exp $");
 
 #include <sys/time.h>
 #include <sys/buf.h>
@@ -639,7 +639,6 @@ rf_ReconstructInPlace(raidPtr, row, col)
 	}
 	RF_UNLOCK_MUTEX(raidPtr->mutex);
 	RF_SIGNAL_COND(raidPtr->waitForReconCond);
-	wakeup(&raidPtr->waitForReconCond);	
 	return (rc);
 }
 
