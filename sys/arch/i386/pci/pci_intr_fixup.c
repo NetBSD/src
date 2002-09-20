285ba8bee18d0ccbba0f1655611f96fd322fea95
kanaoka <kanaoka@NetBSD.org>
2002-09-20 14:52:39 +0000

    Add support for "Intel 82801DB LPC".  kern/18348: From Andreas Wrede <andreas@planix.com>.

--- sys/arch/i386/pci/pci_intr_fixup.c
+++ sys/arch/i386/pci/pci_intr_fixup.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: pci_intr_fixup.c,v 1.20 2002/06/01 23:50:55 lukem Exp $	*/
+/*	$NetBSD: pci_intr_fixup.c,v 1.21 2002/09/20 14:52:39 kanaoka Exp $	*/
 
 /*-
  * Copyright (c) 1999 The NetBSD Foundation, Inc.
@@ -67,7 +67,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: pci_intr_fixup.c,v 1.20 2002/06/01 23:50:55 lukem Exp $");
+__KERNEL_RCSID(0, "$NetBSD: pci_intr_fixup.c,v 1.21 2002/09/20 14:52:39 kanaoka Exp $");
 
 #include "opt_pcibios.h"
 
@@ -141,6 +141,8 @@ const struct pciintr_icu_table {
 	{ PCI_VENDOR_INTEL,	PCI_PRODUCT_INTEL_82801BA_LPC,
 	  piix_init },
 	{ PCI_VENDOR_INTEL,	PCI_PRODUCT_INTEL_82801BAM_LPC,
+	  piix_init },
+	{ PCI_VENDOR_INTEL,	PCI_PRODUCT_INTEL_82801DB_LPC,
 	  piix_init },
 
 	{ PCI_VENDOR_OPTI,	PCI_PRODUCT_OPTI_82C558,
