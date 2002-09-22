ecc477674e066730eca4439d5ad0f7554340ffcd
simonb <simonb@NetBSD.org>
2002-09-22 08:30:56 +0000

    Use "#define\t" instead of "#define ".

--- sys/arch/acorn26/include/types.h
+++ sys/arch/acorn26/include/types.h
@@ -1,4 +1,4 @@
-/* $NetBSD: types.h,v 1.2 2002/09/22 04:11:32 gmcgarry Exp $ */
+/* $NetBSD: types.h,v 1.3 2002/09/22 08:30:56 simonb Exp $ */
 
 #ifndef	_ACORN26_TYPES_H_
 #define	_ACORN26_TYPES_H_
@@ -6,6 +6,6 @@
 #include <arm/arm26/types.h>
 
 #define	__HAVE_GENERIC_SOFT_INTERRUPTS
-#define __HAVE_MD_RUNQUEUE
+#define	__HAVE_MD_RUNQUEUE
 
 #endif	/* _ACORN26_TYPES_H_ */
