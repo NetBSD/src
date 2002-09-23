a2b14227a6c43c7ea2aace1663c55c16d9558fda
oster <oster@NetBSD.org>
2002-09-23 04:14:20 +0000

    The 'reconDesc' argument to rf_SignalQuiescenceLock() is a holdover from simulation code. *poof*  Thanks to Simon B.

--- sys/dev/raidframe/rf_driver.h
+++ sys/dev/raidframe/rf_driver.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: rf_driver.h,v 1.7 2002/01/07 01:58:03 oster Exp $	*/
+/*	$NetBSD: rf_driver.h,v 1.8 2002/09/23 04:14:20 oster Exp $	*/
 /*
  * rf_driver.h
  */
@@ -61,8 +61,7 @@ int     rf_SetReconfiguredMode(RF_Raid_t * raidPtr, RF_RowCol_t row,
 #endif
 int     rf_FailDisk(RF_Raid_t * raidPtr, RF_RowCol_t frow, RF_RowCol_t fcol,
 		    int initRecon);
-void    rf_SignalQuiescenceLock(RF_Raid_t * raidPtr, 
-				RF_RaidReconDesc_t * reconDesc);
+void    rf_SignalQuiescenceLock(RF_Raid_t * raidPtr);
 int     rf_SuspendNewRequestsAndWait(RF_Raid_t * raidPtr);
 void    rf_ResumeNewRequests(RF_Raid_t * raidPtr);
 void    rf_StartThroughputStats(RF_Raid_t * raidPtr);
