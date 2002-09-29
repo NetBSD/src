b1c7ac0e6de094187bfb041ee5035beb68e26ad6
wiz <wiz@NetBSD.org>
2002-09-29 23:23:56 +0000

    "definitions" has lots of 'i's, but that's not reason to leave one out.

--- sys/netccitt/pk.h
+++ sys/netccitt/pk.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: pk.h,v 1.8 2000/07/02 17:34:01 sommerfeld Exp $	*/
+/*	$NetBSD: pk.h,v 1.9 2002/09/29 23:24:00 wiz Exp $	*/
 
 /*
  * Copyright (c) 1984 University of British Columbia.
@@ -49,7 +49,7 @@
  *
  */
 
-/* Packet type identifier field defintions. */
+/* Packet type identifier field definitions. */
 
 #define X25_CALL                         11
 #define X25_CALL_ACCEPTED                15   
