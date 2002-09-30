e26f257b65695797d6cdb517828ac9fc67bf56e7
lukem <lukem>
2002-09-30 13:47:00 +0000

    Pull up revision 1.64 (requested by bouyer in ticket #879): In nfsrv_create(), kill an extra PNBUF_PUT() in the NFSv2 mknod case. The pnbuf has already been freed by VOP_MKNOD. This should have been removed in rev 1.60. Should fix PR 18013, OK'd by fvdl.

--- sys/nfs/nfs_serv.c
+++ sys/nfs/nfs_serv.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: nfs_serv.c,v 1.62 2001/11/10 10:59:09 lukem Exp $	*/
+/*	$NetBSD: nfs_serv.c,v 1.62.10.1 2002/09/30 13:47:00 lukem Exp $	*/
 
 /*
  * Copyright (c) 1989, 1993
@@ -59,7 +59,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: nfs_serv.c,v 1.62 2001/11/10 10:59:09 lukem Exp $");
+__KERNEL_RCSID(0, "$NetBSD: nfs_serv.c,v 1.62.10.1 2002/09/30 13:47:00 lukem Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -1415,7 +1415,6 @@ nfsrv_create(nfsd, slp, procp, mrq)
 			if (error) {
 				nfsm_reply(0);
 			}
-			PNBUF_PUT(nd.ni_cnd.cn_pnbuf);
 			if (nd.ni_cnd.cn_flags & ISSYMLINK) {
 				vrele(nd.ni_dvp);
 				vput(nd.ni_vp);
