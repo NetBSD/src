9f6036c7462daabf6991346e4b92aeeb001f96c3
bsh <bsh@NetBSD.org>
2002-10-01 11:02:27 +0000

    add board type for Intel PXA2[15]0 and Samsung S3C2800 based boards.

--- sys/arch/evbarm/include/cpu.h
+++ sys/arch/evbarm/include/cpu.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: cpu.h,v 1.2 2002/08/17 16:42:22 briggs Exp $	*/
+/*	$NetBSD: cpu.h,v 1.3 2002/10/01 11:02:27 bsh Exp $	*/
 
 #if defined(_KERNEL) && !defined(_LKM)
 
@@ -8,6 +8,8 @@
 #define EVBARM_BOARDTYPE_IQ80310	2
 #define EVBARM_BOARDTYPE_I80321		3
 #define EVBARM_BOARDTYPE_IXM1200	4
+#define EVBARM_BOARDTYPE_PXA2X0	  	5   /* PXA2X0 based boards */
+#define EVBARM_BOARDTYPE_S3C2800	6   /* S3C2800 based boards */
 
 #endif
 
