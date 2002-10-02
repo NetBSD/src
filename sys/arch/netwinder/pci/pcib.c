021b694d77be4be20ded1f146490318de026dba8
thorpej <thorpej@NetBSD.org>
2002-10-02 04:40:08 +0000

    Use CFATTACH_DECL().

--- sys/arch/netwinder/pci/pcib.c
+++ sys/arch/netwinder/pci/pcib.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: pcib.c,v 1.3 2002/09/27 20:34:07 thorpej Exp $	*/
+/*	$NetBSD: pcib.c,v 1.4 2002/10/02 04:45:25 thorpej Exp $	*/
 
 /*-
  * Copyright (c) 1996, 1998 The NetBSD Foundation, Inc.
@@ -56,9 +56,8 @@
 int	pcibmatch __P((struct device *, struct cfdata *, void *));
 void	pcibattach __P((struct device *, struct device *, void *));
 
-const struct cfattach pcib_ca = {
-	sizeof(struct device), pcibmatch, pcibattach
-};
+CFATTACH_DECL(pcib, sizeof(struct device),
+    pcibmatch, pcibattach, NULL, NULL);
 
 void	pcib_callback __P((struct device *));
 int	pcib_print __P((void *, const char *));
