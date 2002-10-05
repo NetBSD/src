27aeab62c35fc02ecbc4292787585ef15f42426b
bjh21 <bjh21>
2002-10-05 14:09:15 +0000

    Update URLs again (serves me right for committing an old change without checking it).  Back to sco.com, but with a different path.

--- sys/sys/exec_elf.h
+++ sys/sys/exec_elf.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: exec_elf.h,v 1.63 2002/10/05 13:29:29 bjh21 Exp $	*/
+/*	$NetBSD: exec_elf.h,v 1.64 2002/10/05 14:09:15 bjh21 Exp $	*/
 
 /*-
  * Copyright (c) 1994 The NetBSD Foundation, Inc.
@@ -41,10 +41,10 @@
 
 /*
  * The current ELF ABI specification is available at:
- *	http://www.caldera.com/developers/gabi/
+ *	http://www.sco.com/developers/gabi/
  *
  * Current header definitions are in:
- *	http://www.caldera.com/developers/gabi/latest/ch4.eheader.html
+ *	http://www.sco.com/developers/gabi/latest/ch4.eheader.html
  */
 
 #if defined(_KERNEL) || defined(_STANDALONE)
