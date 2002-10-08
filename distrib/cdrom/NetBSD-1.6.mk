a869f9d2cf50bd1f00cad505b9e67cbc5c95c547
lukem <lukem@NetBSD.org>
2002-10-08 14:18:17 +0000

    gunzip cats/binary/kernel/netbsd.aout-INSTALL.gz to /netbsd.cats

--- distrib/cdrom/NetBSD-1.6.mk
+++ distrib/cdrom/NetBSD-1.6.mk
@@ -1,4 +1,4 @@
-# $NetBSD: NetBSD-1.6.mk,v 1.3 2002/09/29 06:44:36 lukem Exp $
+# $NetBSD: NetBSD-1.6.mk,v 1.4 2002/10/08 14:18:17 lukem Exp $
 #
 # Configuration file for the NetBSD 1.6 binary release.
 
@@ -10,6 +10,9 @@ BOOTFILE.alpha=		${EXTFILEDIR}/alpha.bootxx
 EXTFILES.alpha=		alpha.bootxx:alpha/binary/sets/base.tgz,./usr/mdec/bootxx_cd9660
 INTFILES.alpha=		netbsd.alpha:alpha/installation/instkernel/netbsd.gz,link \
 			boot:alpha/binary/sets/base.tgz,./usr/mdec/boot
+
+# cats needs an a.out kernel to boot from
+INTFILES.cats=		netbsd.cats:cats/binary/kernel/netbsd.aout-INSTALL.gz
 
 # BOOTFILE.i386 is relative to CD staging root
 BOOTFILE.i386=		boot.i386
