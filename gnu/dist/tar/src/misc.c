e076df538c9f02ea9afbc26b49d7497377b07fdc
itojun <itojun@NetBSD.org>
2002-10-09 19:58:35 +0000

    seemingly more correct ".." patch by Solar Designer <solar@openwall.com>, from bugtraq posting

--- gnu/dist/tar/src/misc.c
+++ gnu/dist/tar/src/misc.c
@@ -208,19 +208,12 @@ contains_dot_dot (char const *name)
       if (p[0] == '.' && p[1] == '.' && (ISSLASH (p[2]) || !p[2]))
 	return 1;
 
-      do
+      while (! ISSLASH (*p))
 	{
 	  if (! *p++)
 	    return 0;
 	}
-      while (! ISSLASH (*p));
-
-      do
-	{
-	  if (! *p++)
-	    return 0;
-	}
-      while ( ISSLASH (*p));
+      p++;
     }
 }
 
