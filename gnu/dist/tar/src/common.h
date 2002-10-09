20fc2e8751ce5d4fe29ceb59410f6c5d04a56919
itojun <itojun>
2002-10-09 19:28:14 +0000

    bring in --fast-read code from freebsd.

--- gnu/dist/tar/src/common.h
+++ gnu/dist/tar/src/common.h
@@ -152,6 +152,9 @@ GLOBAL int dereference_option;
 /* Patterns that match file names to be excluded.  */
 GLOBAL struct exclude *excluded;
 
+/* Boolean value.  */
+GLOBAL int fast_read_option;
+
 /* Specified file containing names to work on.  */
 GLOBAL const char *files_from_option;
 
@@ -195,6 +198,9 @@ GLOBAL struct mode_change *mode_option;
 
 /* Boolean value.  */
 GLOBAL int multi_volume_option;
+
+/* Boolean value.  */
+GLOBAL int namelist_freed;
 
 /* The same variable hold the time, whether mtime or ctime.  Just fake a
    non-existing option, for making the code clearer, elsewhere.  */
