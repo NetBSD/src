0f74f047d3f46cfdea8d727c70a83c2b2fcc75d6
chris <chris@NetBSD.org>
2002-10-12 11:53:38 +0000

    Merge isa_machdep.c from netwinder and cats into footbridge/isa (where it joins other machdep files) Saves maintaining multiple copies of the same thing, the only differences were: IRQ line used on the footbridge (made that a define in include/isa_machdep.h) name of a dma_ranges variable contained arch name, so just made it generic.

--- sys/arch/cats/include/isa_machdep.h
+++ sys/arch/cats/include/isa_machdep.h
@@ -1,8 +1,12 @@
-/* $NetBSD: isa_machdep.h,v 1.4 2002/01/07 22:58:08 chris Exp $ */
+/* $NetBSD: isa_machdep.h,v 1.5 2002/10/12 11:53:40 chris Exp $ */
 
 #ifndef _CATS_ISA_MACHDEP_H_
 #define _CATS_ISA_MACHDEP_H_
-void isa_cats_init __P((u_int, u_int));
-
 #include <arm/isa_machdep.h>
-#endif
+
+#ifdef _KERNEL
+#define ISA_FOOTBRIDGE_IRQ IRQ_IN_L2
+void	isa_footbridge_init(u_int, u_int);
+#endif /* _KERNEL */
+
+#endif /* _CATS_ISA_MACHDEP_H_ */
