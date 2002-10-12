a8d15299eaa98ff01171af7df0b129211ad2ed7c
jdolecek <jdolecek@NetBSD.org>
2002-10-12 14:04:45 +0000

    put back the while loop in kernfs_getattr() removed in rev 1.82; it's necessary to get the whole file length e.g. for msgbuf
    this fixes the '/kern/msgbuf & less' problem reported on port-i386
    by Dave Tyson

--- sys/miscfs/kernfs/kernfs_vnops.c
+++ sys/miscfs/kernfs/kernfs_vnops.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: kernfs_vnops.c,v 1.83 2002/08/03 04:52:44 simonb Exp $	*/
+/*	$NetBSD: kernfs_vnops.c,v 1.84 2002/10/12 14:04:45 jdolecek Exp $	*/
 
 /*
  * Copyright (c) 1992, 1993
@@ -43,7 +43,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: kernfs_vnops.c,v 1.83 2002/08/03 04:52:44 simonb Exp $");
+__KERNEL_RCSID(0, "$NetBSD: kernfs_vnops.c,v 1.84 2002/10/12 14:04:45 jdolecek Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -499,7 +499,7 @@ kernfs_getattr(v)
 		vap->va_size = DEV_BSIZE;
 	} else {
 		const struct kern_target *kt = VTOKERN(vp)->kf_kt;
-		size_t total;
+		size_t nread, total;
 #ifdef KERNFS_DIAGNOSTIC
 		printf("kernfs_getattr: stat target %s\n", kt->kt_name);
 #endif
@@ -507,10 +507,14 @@ kernfs_getattr(v)
 		vap->va_mode = kt->kt_mode;
 		vap->va_nlink = 1;
 		vap->va_fileid = 1 + (kt - kern_targets);
-		buf = strbuf;
-		if (0 == (error = kernfs_xread(kt, 0, &buf,
-				sizeof(strbuf), &total)))
-			vap->va_size = total;
+		total = 0;
+		do {
+			buf = strbuf;
+			error = kernfs_xread(kt, total, &buf,
+				sizeof(strbuf), &nread);
+			total += nread;
+		} while (error == 0 && nread != 0);
+		vap->va_size = total;
 	}
 
 #ifdef KERNFS_DIAGNOSTIC
