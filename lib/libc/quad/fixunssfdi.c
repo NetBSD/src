37b34d511dea595d3ba03a661cf3b775038ea5f8
scw <scw@NetBSD.org>
2002-10-20 10:15:47 +0000

    As discussed (briefly) on tech-userlevel, fix our quad support to work correctly on LP64 platforms. This is mostly just s/long/int/ where appropriate.

--- lib/libc/quad/fixunssfdi.c
+++ lib/libc/quad/fixunssfdi.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: fixunssfdi.c,v 1.4 1997/07/13 20:01:45 christos Exp $	*/
+/*	$NetBSD: fixunssfdi.c,v 1.5 2002/10/20 10:15:47 scw Exp $	*/
 
 /*-
  * Copyright (c) 1992, 1993
@@ -42,13 +42,13 @@
 #if 0
 static char sccsid[] = "@(#)fixunssfdi.c	8.1 (Berkeley) 6/4/93";
 #else
-__RCSID("$NetBSD: fixunssfdi.c,v 1.4 1997/07/13 20:01:45 christos Exp $");
+__RCSID("$NetBSD: fixunssfdi.c,v 1.5 2002/10/20 10:15:47 scw Exp $");
 #endif
 #endif /* LIBC_SCCS and not lint */
 
 #include "quad.h"
 
-#define	ONE_FOURTH	((long)1 << (LONG_BITS - 2))
+#define	ONE_FOURTH	((int)1 << (INT_BITS - 2))
 #define	ONE_HALF	(ONE_FOURTH * 2.0)
 #define	ONE		(ONE_FOURTH * 4.0)
 
@@ -88,20 +88,20 @@ __fixunssfdi(float f)
 	 * between x and this is the bottom part (this may introduce
 	 * a few fuzzy bits, but what the heck).  With any luck this
 	 * difference will be nonnegative: x should wind up in the
-	 * range [0..ULONG_MAX].  For paranoia, we assume [LONG_MIN..
-	 * 2*ULONG_MAX] instead.
+	 * range [0..UINT_MAX].  For paranoia, we assume [INT_MIN..
+	 * 2*UINT_MAX] instead.
 	 */
-	t.ul[H] = (unsigned long)toppart;
+	t.ul[H] = (unsigned int)toppart;
 	t.ul[L] = 0;
 	x -= (double)t.uq;
 	if (x < 0) {
 		t.ul[H]--;
-		x += ULONG_MAX;
+		x += UINT_MAX;
 	}
-	if (x > ULONG_MAX) {
+	if (x > UINT_MAX) {
 		t.ul[H]++;
-		x -= ULONG_MAX;
+		x -= UINT_MAX;
 	}
-	t.ul[L] = (u_long)x;
+	t.ul[L] = (u_int)x;
 	return (t.uq);
 }
