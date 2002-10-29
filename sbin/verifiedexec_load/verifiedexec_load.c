feb4bae7ebe1aa40284b49675d95ac03097e056a
blymn <blymn@NetBSD.org>
2002-10-29 13:58:01 +0000

    Added fingerprint loader tool for verified exec.

---
+++ sbin/verifiedexec_load/verifiedexec_load.c
@@ -0,0 +1,62 @@
+/*	$NetBSD: verifiedexec_load.c,v 1.1 2002/10/29 13:58:02 blymn Exp $	*/
+
+/*-
+ * Copyright (c) 1998-1999 Brett Lymn
+ *                         (blymn@baea.com.au, brett_lymn@yahoo.com.au)
+ * All rights reserved.
+ *
+ * This code has been donated to The NetBSD Foundation by the Author.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. The name of the author may not be used to endorse or promote products
+ *    derived from this software withough specific prior written permission
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *
+ */
+
+
+#include <stdio.h>
+#include <fcntl.h>
+
+/* globals */
+int fd;
+extern FILE *yyin;
+int yyparse(void);
+
+int
+main(int argc, char *argv[])
+{
+	if (argv[1] == NULL) {
+		fprintf(stderr, "Usage: verifiedexec_load signature_file\n");
+		exit(1);
+	}
+
+	fd = open("/dev/veriexec", O_WRONLY, 0);
+	if (fd < 0) {
+		fprintf(stderr, "Dev open failed\n");
+		exit(1);
+	}
+
+	if ((yyin = fopen(argv[1], "r")) == NULL) {
+		fprintf(stderr, "Input file open failed\n");
+		exit(1);
+	}
+
+	yyparse();
+        exit(0);
+}
