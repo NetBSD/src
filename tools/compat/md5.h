bfc2021cabcbc3f5ec8255fab378bbb46f4fd068
he <he@NetBSD.org>
2002-11-03 16:18:39 +0000

    Pull up revision 1.1 (new, requested by thorpej in ticket #797):   Include various more functions in libnbcompat, mostly   becuase our tools use features not universally present in   other versions. This applies to glob(3), vis(3), and the   MD2, MD4, RMD160 and SHA1 implementations.   Also add checks for random(3) and termios.h for the Heimdal   host tools.   Update README to indicate the current state of building the   host tools on Solaris 8 (for SPARC).

---
+++ tools/compat/md5.h
@@ -0,0 +1,5 @@
+/*	$NetBSD: md5.h,v 1.1.2.2 2002/11/03 16:18:39 he Exp $	*/
+
+/* We unconditionally use the NetBSD MD5 in libnbcompat. */
+#include "config.h"
+#include "../../include/md5.h"
---
+++ tools/compat/rmd160.h
@@ -0,0 +1,5 @@
+/*	$NetBSD: rmd160.h,v 1.1.2.2 2002/11/03 16:18:40 he Exp $	*/
+
+/* We unconditionally use the NetBSD RMD160 in libnbcompat. */
+#include "config.h"
+#include "../../include/rmd160.h"
