51503c6319d9146a4f93e5c811796a0dd8660d19
he <he>
2002-11-03 19:46:51 +0000

    Pull up revisions 1.18-1.20 (requested by he in ticket #787):   Make these compile again, after the changes in ticket #787.

--- usr.bin/apropos/apropos.c
+++ usr.bin/apropos/apropos.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: apropos.c,v 1.17 2002/03/08 20:23:10 jdolecek Exp $	*/
+/*	$NetBSD: apropos.c,v 1.17.2.1 2002/11/03 19:46:51 he Exp $	*/
 
 /*
  * Copyright (c) 1987, 1993, 1994
@@ -44,7 +44,7 @@ __COPYRIGHT("@(#) Copyright (c) 1987, 1993, 1994\n\
 #if 0
 static char sccsid[] = "@(#)apropos.c	8.8 (Berkeley) 5/4/95";
 #else
-__RCSID("$NetBSD: apropos.c,v 1.17 2002/03/08 20:23:10 jdolecek Exp $");
+__RCSID("$NetBSD: apropos.c,v 1.17.2.1 2002/11/03 19:46:51 he Exp $");
 #endif
 #endif /* not lint */
 
@@ -60,7 +60,7 @@ __RCSID("$NetBSD: apropos.c,v 1.17 2002/03/08 20:23:10 jdolecek Exp $");
 #include <string.h>
 #include <unistd.h>
 
-#include <man/config.h>
+#include <man/manconf.h>
 #include <man/pathnames.h>
 
 static int *found, foundman;
@@ -121,9 +121,8 @@ main(argc, argv)
 		apropos(argv, p_path, 1);
 	else {
 		config(conffile);
-		ep = (tp = getlist("_whatdb")) == NULL ?
-			NULL : TAILQ_FIRST(&tp->list);
-		for (; ep != NULL; ep = TAILQ_NEXT(ep, q)) {
+		tp = getlist("_whatdb", 1);
+		TAILQ_FOREACH(ep, &tp->list, q) {
 			if ((rv = glob(ep->s, GLOB_BRACE | GLOB_NOSORT, NULL,
 			    &pg)) != 0) {
 				if (rv == GLOB_NOMATCH)
