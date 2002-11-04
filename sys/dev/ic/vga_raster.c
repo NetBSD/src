074fd60137cb262b8c4f0052a034b360dbefdf54
junyoung <junyoung@NetBSD.org>
2002-11-04 08:05:52 +0000

    Missing '.'

--- sys/dev/ic/vga_raster.c
+++ sys/dev/ic/vga_raster.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: vga_raster.c,v 1.3 2002/10/31 13:06:50 junyoung Exp $	*/
+/*	$NetBSD: vga_raster.c,v 1.4 2002/11/04 08:05:53 junyoung Exp $	*/
 
 /*
  * Copyright (c) 2001, 2002 Bang Jun-Young
@@ -13,7 +13,7 @@
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
  * 3. The name of the author may not be used to endorse or promote products
- *    derived from this software without specific prior written permission
+ *    derived from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
