6efed11d0b0ede0fb31cd1799ee75e8251c1f08f
manu <manu@NetBSD.org>
2002-11-07 00:22:28 +0000

    Added two sysctl-able flags: proc.curproc.stopfork and proc.curproc.stopexec that can be used to block a process after fork(2) or exec(2) calls. The new process is created in the SSTOP state and is never scheduled for running.
    This feature is designed so that it is esay to attach the process using gdb
    before it has done anything.
    
    It works also with sproc, kthread_create, clone...

--- sys/sys/sysctl.h
+++ sys/sys/sysctl.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: sysctl.h,v 1.78 2002/08/26 13:09:39 augustss Exp $	*/
+/*	$NetBSD: sysctl.h,v 1.79 2002/11/07 00:22:28 manu Exp $	*/
 
 /*
  * Copyright (c) 1989, 1993
@@ -590,12 +590,16 @@ struct kinfo_proc2 {
  */
 #define	PROC_PID_CORENAME	1
 #define	PROC_PID_LIMIT		2
-#define	PROC_PID_MAXID		3
+#define	PROC_PID_STOPFORK	3
+#define	PROC_PID_STOPEXEC	4
+#define	PROC_PID_MAXID		5
 
 #define	PROC_PID_NAMES { \
 	{ 0, 0 }, \
 	{ "corename", CTLTYPE_STRING }, \
 	{ "rlimit", CTLTYPE_NODE }, \
+	{ "stopfork", CTLTYPE_INT }, \
+	{ "stopexec", CTLTYPE_INT }, \
 }
 
 /* Limit types from <sys/resources.h> */
