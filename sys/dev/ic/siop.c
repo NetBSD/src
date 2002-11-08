0c4cba1ddc94c7e326fb50db0c3ca93510c9b5c0
bouyer <bouyer@NetBSD.org>
2002-11-08 22:04:41 +0000

    in siop_reset(), reset sc_ntargets to 0. The number of targets will be computed again in siop_add_reselsw() in siop_reset(), reset the tag reseloff to 0, in addition to the lun reseloff. If siop_add_dev() fails this time we would use the old reseloff, clobbering memory now used for something else.

--- sys/dev/ic/siop.c
+++ sys/dev/ic/siop.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: siop.c,v 1.64 2002/07/26 01:00:43 wiz Exp $	*/
+/*	$NetBSD: siop.c,v 1.65 2002/11/08 22:04:41 bouyer Exp $	*/
 
 /*
  * Copyright (c) 2000 Manuel Bouyer.
@@ -33,7 +33,7 @@
 /* SYM53c7/8xx PCI-SCSI I/O Processors driver */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: siop.c,v 1.64 2002/07/26 01:00:43 wiz Exp $");
+__KERNEL_RCSID(0, "$NetBSD: siop.c,v 1.65 2002/11/08 22:04:41 bouyer Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -252,6 +252,7 @@ siop_reset(sc)
 	}
 	sc->script_free_lo = sizeof(siop_script) / sizeof(siop_script[0]);
 	sc->script_free_hi = sc->sc_c.ram_size / 4;
+	sc->sc_ntargets = 0;
 
 	/* free used and unused lun switches */
 	while((lunsw = TAILQ_FIRST(&sc->lunsw_list)) != NULL) {
@@ -1768,7 +1769,7 @@ siop_add_reselsw(sc, target)
 	struct siop_softc *sc;
 	int target;
 {
-	int i;
+	int i, j;
 	struct siop_target *siop_target;
 	struct siop_lun *siop_lun;
 
@@ -1805,6 +1806,8 @@ siop_add_reselsw(sc, target)
 			continue;
 		if (siop_lun->reseloff > 0) {
 			siop_lun->reseloff = 0;
+			for (j = 0; j < SIOP_NTAG; j++)
+				siop_lun->siop_tag[j].reseloff = 0;
 			siop_add_dev(sc, target, i);
 		}
 	}
