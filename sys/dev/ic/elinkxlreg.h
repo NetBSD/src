545f475e0e0a0dd3ce4d3c23fa422433bc3dfdd1
enami <enami@NetBSD.org>
2002-11-09 11:45:18 +0000

    Fix multicast handling on 3C905B or later card; - Handle IFF_ALLMULTI case correctly.  This is necessary to mrouted working.   Tested by taca@netbsd.org. - Clear unnecessary multicast hash bit.  Otherwise, unnecessary multicast   packet is received.  Tested by me with the 3C905C bought at akihabara.   It was 2980yen.
    The code is written by fvdl@netbsd.org and me.

--- sys/dev/ic/elinkxlreg.h
+++ sys/dev/ic/elinkxlreg.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: elinkxlreg.h,v 1.10 2001/12/28 20:35:47 christos Exp $	*/
+/*	$NetBSD: elinkxlreg.h,v 1.11 2002/11/09 11:45:19 enami Exp $	*/
 
 /*-
  * Copyright (c) 1998 The NetBSD Foundation, Inc.
@@ -164,6 +164,7 @@
 #define ELINK_DNUNSTALL		0x3003
 #define ELINK_TXRECLTHRESH	0xc000
 #define ELINK_TXSTARTTHRESH	0x9800
+#define ELINK_CLEARHASHFILBIT	0xc800
 #define ELINK_SETHASHFILBIT	0xcc00
 
 /*
