296cdd15a7f313eaba8f1a454e56255f07b663ad
itojun <itojun>
2002-11-10 19:52:16 +0000

    always create pmtud timeout queue, as ip_mtudisc can be tweaked via sysctl at runtime.  From lha@stacken.kth.se

--- sys/netinet/ip_input.c
+++ sys/netinet/ip_input.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: ip_input.c,v 1.159 2002/11/02 07:28:12 perry Exp $	*/
+/*	$NetBSD: ip_input.c,v 1.160 2002/11/10 19:52:16 itojun Exp $	*/
 
 /*
  * Copyright (C) 1995, 1996, 1997, and 1998 WIDE Project.
@@ -102,7 +102,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: ip_input.c,v 1.159 2002/11/02 07:28:12 perry Exp $");
+__KERNEL_RCSID(0, "$NetBSD: ip_input.c,v 1.160 2002/11/10 19:52:16 itojun Exp $");
 
 #include "opt_gateway.h"
 #include "opt_pfil_hooks.h"
@@ -338,9 +338,7 @@ ip_init()
 	TAILQ_INIT(&in_ifaddr);
 	in_ifaddrhashtbl = hashinit(IN_IFADDR_HASH_SIZE, HASH_LIST, M_IFADDR,
 	    M_WAITOK, &in_ifaddrhash);
-	if (ip_mtudisc != 0)
-		ip_mtudisc_timeout_q =
-		    rt_timer_queue_create(ip_mtudisc_timeout);
+	ip_mtudisc_timeout_q = rt_timer_queue_create(ip_mtudisc_timeout);
 #ifdef GATEWAY
 	ipflow_init();
 #endif
