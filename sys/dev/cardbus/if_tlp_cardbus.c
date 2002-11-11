d1479e83a1d21591b86834a70cabaa2f02faca9d
onoe <onoe@NetBSD.org>
2002-11-11 12:51:38 +0000

    Add support of AboCom FE2500MX 10/100 Ethernet (Lanneed LD-CBL/TX).

--- sys/dev/cardbus/if_tlp_cardbus.c
+++ sys/dev/cardbus/if_tlp_cardbus.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: if_tlp_cardbus.c,v 1.37 2002/10/02 16:33:43 thorpej Exp $	*/
+/*	$NetBSD: if_tlp_cardbus.c,v 1.38 2002/11/11 12:51:38 onoe Exp $	*/
 
 /*-
  * Copyright (c) 1999, 2000 The NetBSD Foundation, Inc.
@@ -43,7 +43,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: if_tlp_cardbus.c,v 1.37 2002/10/02 16:33:43 thorpej Exp $");
+__KERNEL_RCSID(0, "$NetBSD: if_tlp_cardbus.c,v 1.38 2002/11/11 12:51:38 onoe Exp $");
 
 #include "opt_inet.h"
 #include "opt_ns.h"
@@ -152,6 +152,9 @@ const struct tulip_cardbus_product {
 	  TULIP_CHIP_AN985 },
 
 	{ CARDBUS_VENDOR_ABOCOM,	CARDBUS_PRODUCT_ABOCOM_PCM200,
+	  TULIP_CHIP_AN985 },
+
+	{ CARDBUS_VENDOR_ABOCOM,	CARDBUS_PRODUCT_ABOCOM_FE2500MX,
 	  TULIP_CHIP_AN985 },
 
 	{ CARDBUS_VENDOR_HAWKING,	CARDBUS_PRODUCT_HAWKING_PN672TX,
