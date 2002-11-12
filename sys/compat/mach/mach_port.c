fc2d2ae017eb33f0e09ee2aeab531bcbf390fbb8
manu <manu@NetBSD.org>
2002-11-12 06:14:39 +0000

    In mach_reply_port, don't increment the returned port for now, this confuses the binary on the second launch. In mach_vm_map, hack in a failure so that we fail exactly like Darwin when mapping a page at address 0 Add vm_allocate trap

--- sys/compat/mach/mach_port.c
+++ sys/compat/mach/mach_port.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: mach_port.c,v 1.3 2002/11/11 09:28:00 manu Exp $ */
+/*	$NetBSD: mach_port.c,v 1.4 2002/11/12 06:14:39 manu Exp $ */
 
 /*-
  * Copyright (c) 2002 The NetBSD Foundation, Inc.
@@ -37,7 +37,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: mach_port.c,v 1.3 2002/11/11 09:28:00 manu Exp $");
+__KERNEL_RCSID(0, "$NetBSD: mach_port.c,v 1.4 2002/11/12 06:14:39 manu Exp $");
 
 #include <sys/types.h>
 #include <sys/param.h>
@@ -58,7 +58,7 @@ mach_sys_reply_port(p, v, retval)
 {
 	static int current_port = 0x80b;
 
-	*retval = current_port++; /* XXX */
+	*retval = current_port; /* XXX */
 	return 0;
 }
 
