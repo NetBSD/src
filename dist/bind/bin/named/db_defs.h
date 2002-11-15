cab85f155147dbc886fb2461bc198158eeb31f03
lukem <lukem@NetBSD.org>
2002-11-15 00:48:09 +0000

    Pull up revision 1.5 (requested by itojun in ticket #989): apply http://www.isc.org/products/BIND/patches/bind833.diff to fix recent vulnerabilities: * BIND: Remote Execution of Code (BIND 4 & 8) * BIND: Multiple Denial of Service (BIND 8 only)

--- dist/bind/bin/named/db_defs.h
+++ dist/bind/bin/named/db_defs.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: db_defs.h,v 1.3.2.1 2002/06/28 11:29:29 lukem Exp $	*/
+/*	$NetBSD: db_defs.h,v 1.3.2.2 2002/11/15 00:48:09 lukem Exp $	*/
 
 /*
  *	from db.h	4.16 (Berkeley) 6/1/90
@@ -80,7 +80,7 @@
  */
 
 	/* max length of data in RR data field */
-#define MAXDATA		(2*MAXDNAME + 5*INT32SZ)
+#define MAXDATA		(3*MAXDNAME + 5*INT32SZ)
 
 	/* max length of data in a TXT RR segment */
 #define MAXCHARSTRING 255
