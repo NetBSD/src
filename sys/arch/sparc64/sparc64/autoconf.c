f79f4b77311c7db63cae13c2dbd11c0397fec9a9
lukem <lukem@NetBSD.org>
2002-11-15 00:37:43 +0000

    Pull up revision 1.68 (requested by mrg in ticket #959): redo the previous:  it seems that the 2nd prom address value is ignored/useless (it seems to be always zero), and that the channel & drive are encoded into the first value as "channel * 2 + drive", so, look for this.

--- sys/arch/sparc64/sparc64/autoconf.c
+++ sys/arch/sparc64/sparc64/autoconf.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: autoconf.c,v 1.57.4.2 2002/11/15 00:32:33 lukem Exp $ */
+/*	$NetBSD: autoconf.c,v 1.57.4.3 2002/11/15 00:37:43 lukem Exp $ */
 
 /*
  * Copyright (c) 1996
@@ -1339,8 +1339,13 @@ device_register(dev, aux)
 		/* IDE disks. */
 		struct ata_device *adev = aux;
 
-		if (adev->adev_channel == bp->val[1] &&
-		    adev->adev_drv_data->drive == bp->val[0]) {
+		/*
+		 * The PROM gives you names like "disk@1,0", where the first value
+		 * appears to be both the drive & channel combined (channel * 2 +
+		 * drive), and the second value we don't use (what is it anyway?)
+		 */
+		if ((adev->adev_channel * 2) + adev->adev_drv_data->drive ==
+		    bp->val[0]) {
 			nail_bootdev(dev, bp);
 			DPRINTF(ACDB_BOOTDEV, ("\t-- found wd disk %s\n",
 			    dev->dv_xname));
