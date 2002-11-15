539c77595bc09276c9f8d2fe7eb3b238ec2b5e00
lukem <lukem>
2002-11-15 00:48:19 +0000

    Pull up revision 1.6 (requested by itojun in ticket #989): apply http://www.isc.org/products/BIND/patches/bind833.diff to fix recent vulnerabilities: * BIND: Remote Execution of Code (BIND 4 & 8) * BIND: Multiple Denial of Service (BIND 8 only)

--- dist/bind/bin/named/ns_req.c
+++ dist/bind/bin/named/ns_req.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: ns_req.c,v 1.4.2.1 2002/06/28 11:34:35 lukem Exp $	*/
+/*	$NetBSD: ns_req.c,v 1.4.2.2 2002/11/15 00:48:19 lukem Exp $	*/
 
 #if !defined(lint) && !defined(SABER)
 static const char sccsid[] = "@(#)ns_req.c	4.47 (Berkeley) 7/1/91";
@@ -2197,7 +2197,7 @@ make_rr(const char *name, struct databuf *dp, u_char *buf,
 
 		/* first just copy over the type_covered, algorithm, */
 		/* labels, orig ttl, two timestamps, and the footprint */
-		if ((dp->d_size - 18) > buflen)
+		if (buflen < 18)
 			goto cleanup;  /* out of room! */
 		memcpy(cp, cp1, 18);
 		cp  += 18;
