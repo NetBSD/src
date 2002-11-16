1a0539ff2b53d5914743ec66aa39f32e847219bc
manu <manu@NetBSD.org>
2002-11-16 20:00:29 +0000

    Implment map_fd mach trap Fix host_info(BASIC_INFO) which was broken wrt to the number of field returned.

--- sys/arch/powerpc/powerpc/mach_machdep.c
+++ sys/arch/powerpc/powerpc/mach_machdep.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: mach_machdep.c,v 1.3 2002/11/15 21:48:06 manu Exp $ */
+/*	$NetBSD: mach_machdep.c,v 1.4 2002/11/16 20:00:29 manu Exp $ */
 
 /*-
  * Copyright (c) 2002 The NetBSD Foundation, Inc.
@@ -37,7 +37,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: mach_machdep.c,v 1.3 2002/11/15 21:48:06 manu Exp $");
+__KERNEL_RCSID(0, "$NetBSD: mach_machdep.c,v 1.4 2002/11/16 20:00:29 manu Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -102,9 +102,9 @@ mach_host_basic_info(info)
     struct mach_host_basic_info *info;
 {
 	/* XXX fill this  accurately */
-	info->max_cpus = 1;
-	info->avail_cpus = 1;
-	info->memory_size = uvmexp.active + uvmexp.inactive;
+	info->max_cpus = 1; /* XXX */
+	info->avail_cpus = 1; /* XXX */
+	info->memory_size = (uvmexp.active + uvmexp.inactive) * PAGE_SIZE;
 	info->cpu_type = MACHO_CPU_TYPE_POWERPC;
-	info->cpu_subtype = MACHO_CPU_SUBTYPE_POWERPC_ALL;
+	info->cpu_subtype = MACHO_CPU_SUBTYPE_POWERPC_604; /* XXX */
 }
