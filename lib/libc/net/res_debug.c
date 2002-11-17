32e76e6a117a121311cbfeb360449ef11567d823
itojun <itojun@NetBSD.org>
2002-11-17 20:48:44 +0000

    whitespace

--- lib/libc/net/res_debug.c
+++ lib/libc/net/res_debug.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: res_debug.c,v 1.33 2002/08/16 11:45:55 itojun Exp $	*/
+/*	$NetBSD: res_debug.c,v 1.34 2002/11/17 20:48:44 itojun Exp $	*/
 
 /*-
  * Copyright (c) 1985, 1990, 1993
@@ -81,7 +81,7 @@
 static char sccsid[] = "@(#)res_debug.c	8.1 (Berkeley) 6/4/93";
 static char rcsid[] = "Id: res_debug.c,v 8.20 1997/06/01 20:34:37 vixie Exp ";
 #else
-__RCSID("$NetBSD: res_debug.c,v 1.33 2002/08/16 11:45:55 itojun Exp $");
+__RCSID("$NetBSD: res_debug.c,v 1.34 2002/11/17 20:48:44 itojun Exp $");
 #endif
 #endif /* LIBC_SCCS and not lint */
 
@@ -473,9 +473,9 @@ __p_fqnname(cp, msg, msglen, name, namelen)
 
 	if ((n = dn_expand(msg, cp + msglen, cp, name, namelen)) < 0)
 		return (NULL);
-	newlen = strlen (name);
+	newlen = strlen(name);
 	if (newlen == 0 || name[newlen - 1] != '.') {
-		if (newlen+1 >= namelen)	/* Lack space for final dot */
+		if (newlen + 1 >= namelen)	/* Lack space for final dot */
 			return (NULL);
 		else
 			strcpy(name + newlen, ".");
