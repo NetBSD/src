7c7c49779775cfbff3e98f8c5be12c8b6627a550
he <he>
2002-11-18 01:03:52 +0000

    Pull up revision 1.4 (requested by thoeprj in ticket #667):   Byte-swapping optimizations, enabled if compiling with GCC:    o Byte-swap 16-bit and 32-bit constants at compile-time    o Inline 16-bit and 32-bit variable byte-swaps

--- sys/arch/arm/include/endian_machdep.h
+++ sys/arch/arm/include/endian_machdep.h
@@ -1,8 +1,21 @@
-/* $NetBSD: endian_machdep.h,v 1.3 2001/02/17 14:55:44 bjh21 Exp $ */
+/* $NetBSD: endian_machdep.h,v 1.3.20.1 2002/11/18 01:03:52 he Exp $ */
 
 /* GCC predefines __ARMEB__ when building for big-endian ARM. */
 #ifdef __ARMEB__
 #define _BYTE_ORDER _BIG_ENDIAN
 #else
 #define _BYTE_ORDER _LITTLE_ENDIAN
+#endif
+
+#ifdef __GNUC__
+
+#include <arm/byte_swap.h>
+
+#if _BYTE_ORDER == _LITTLE_ENDIAN
+#define	ntohl(x)	((in_addr_t)__byte_swap_long((in_addr_t)(x)))
+#define	ntohs(x)	((in_port_t)__byte_swap_word((in_port_t)(x)))
+#define	htonl(x)	((in_addr_t)__byte_swap_long((in_addr_t)(x)))
+#define	htons(x)	((in_port_t)__byte_swap_word((in_port_t)(x)))
+#endif
+
 #endif
