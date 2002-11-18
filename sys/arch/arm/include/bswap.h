eff449c4b8a39b20984190bfd3235e6ef8899e04
he <he@NetBSD.org>
2002-11-18 01:03:30 +0000

    Pull up revision 1.2 (requested by thorpej in ticket #667):   Byte-swapping optimizations, enabled if compiling with GCC:    o Byte-swap 16-bit and 32-bit constants at compile-time    o Inline 16-bit and 32-bit variable byte-swaps

--- sys/arch/arm/include/bswap.h
+++ sys/arch/arm/include/bswap.h
@@ -1,9 +1,17 @@
-/*      $NetBSD: bswap.h,v 1.1 2001/01/10 19:02:05 bjh21 Exp $      */
+/*      $NetBSD: bswap.h,v 1.1.20.1 2002/11/18 01:03:30 he Exp $      */
 
 #ifndef _MACHINE_BSWAP_H_
 #define	_MACHINE_BSWAP_H_
 
 #define __BSWAP_RENAME
 #include <sys/bswap.h>
+
+#ifdef __GNUC__
+
+#include <arm/byte_swap.h>
+#define	bswap16(x)	__byte_swap_word(x)
+#define	bswap32(x)	__byte_swap_long(x)
+
+#endif /* __GNUC__ */
 
 #endif /* !_MACHINE_BSWAP_H_ */
