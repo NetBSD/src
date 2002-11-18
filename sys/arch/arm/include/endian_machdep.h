33754767f26177da00b2ff90535f87ba8bfe4b60
he <he@NetBSD.org>
2002-11-18 01:16:48 +0000

    Pull up revision 1.5 (requested by thorpej in ticket #673):   Rename ``word'' -> 16 and ``long'' -> 32.   Replace __byte_swap_32_variable() with a C version that   generates nearly identical assembly.

--- sys/arch/arm/include/endian_machdep.h
+++ sys/arch/arm/include/endian_machdep.h
@@ -1,4 +1,4 @@
-/* $NetBSD: endian_machdep.h,v 1.3.20.1 2002/11/18 01:03:52 he Exp $ */
+/* $NetBSD: endian_machdep.h,v 1.3.20.2 2002/11/18 01:16:48 he Exp $ */
 
 /* GCC predefines __ARMEB__ when building for big-endian ARM. */
 #ifdef __ARMEB__
@@ -12,10 +12,10 @@
 #include <arm/byte_swap.h>
 
 #if _BYTE_ORDER == _LITTLE_ENDIAN
-#define	ntohl(x)	((in_addr_t)__byte_swap_long((in_addr_t)(x)))
-#define	ntohs(x)	((in_port_t)__byte_swap_word((in_port_t)(x)))
-#define	htonl(x)	((in_addr_t)__byte_swap_long((in_addr_t)(x)))
-#define	htons(x)	((in_port_t)__byte_swap_word((in_port_t)(x)))
+#define	ntohl(x)	((in_addr_t)__byte_swap_32((in_addr_t)(x)))
+#define	ntohs(x)	((in_port_t)__byte_swap_16((in_port_t)(x)))
+#define	htonl(x)	((in_addr_t)__byte_swap_32((in_addr_t)(x)))
+#define	htons(x)	((in_port_t)__byte_swap_16((in_port_t)(x)))
 #endif
 
 #endif
