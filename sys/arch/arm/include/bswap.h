91c16262e19548eea9d875aed6be1e40484f21c9
he <he@NetBSD.org>
2002-11-18 01:15:59 +0000

    Pull up revision 1.3 (requested by thorpej in ticket #673):   Rename ``word'' -> 16 and ``long'' -> 32.   Replace __byte_swap_32_variable() with a C version that   generates nearly identical assembly.

--- sys/arch/arm/include/bswap.h
+++ sys/arch/arm/include/bswap.h
@@ -1,4 +1,4 @@
-/*      $NetBSD: bswap.h,v 1.1.20.1 2002/11/18 01:03:30 he Exp $      */
+/*      $NetBSD: bswap.h,v 1.1.20.2 2002/11/18 01:15:59 he Exp $      */
 
 #ifndef _MACHINE_BSWAP_H_
 #define	_MACHINE_BSWAP_H_
@@ -9,8 +9,8 @@
 #ifdef __GNUC__
 
 #include <arm/byte_swap.h>
-#define	bswap16(x)	__byte_swap_word(x)
-#define	bswap32(x)	__byte_swap_long(x)
+#define	bswap16(x)	__byte_swap_16(x)
+#define	bswap32(x)	__byte_swap_32(x)
 
 #endif /* __GNUC__ */
 
