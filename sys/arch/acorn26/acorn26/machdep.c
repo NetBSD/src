343f565c9b76e2e0b28696ec878b05b5c87f3b95
he <he@NetBSD.org>
2002-11-18 02:35:46 +0000

    Pull up revision 1.4 (requested by bjh21 in ticket #689):   New and much improved version of copyin(), copyout(), and   kcopy() which works on both 26-bit and 32-bit machines.

--- sys/arch/acorn26/acorn26/machdep.c
+++ sys/arch/acorn26/acorn26/machdep.c
@@ -1,4 +1,4 @@
-/* $NetBSD: machdep.c,v 1.2.4.1 2002/11/18 02:23:32 he Exp $ */
+/* $NetBSD: machdep.c,v 1.2.4.2 2002/11/18 02:35:46 he Exp $ */
 
 /*-
  * Copyright (c) 1998 Ben Harris
@@ -32,7 +32,7 @@
 
 #include <sys/param.h>
 
-__KERNEL_RCSID(0, "$NetBSD: machdep.c,v 1.2.4.1 2002/11/18 02:23:32 he Exp $");
+__KERNEL_RCSID(0, "$NetBSD: machdep.c,v 1.2.4.2 2002/11/18 02:35:46 he Exp $");
 
 #include <sys/buf.h>
 #include <sys/kernel.h>
@@ -224,6 +224,8 @@ cpu_startup()
 	 * Set up buffers, so they can be used to read disk labels.
 	 */
 	bufinit();
+
+	curpcb = &proc0.p_addr->u_pcb;
 
 #if 0
 	/* Test exception handlers */
