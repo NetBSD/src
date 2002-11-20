fd666408c79cb095eb60ed69ee1a9a09a1a9e684
bsh <bsh@NetBSD.org>
2002-11-20 17:52:48 +0000

    Samsung S3C2800 is a CPU with ARM920T core.

---
+++ sys/arch/arm/s3c2xx0/s3c2800.c
@@ -0,0 +1,184 @@
+/*	$NetBSD: s3c2800.c,v 1.1 2002/11/20 17:52:49 bsh Exp $ */
+
+/*
+ * Copyright (c) 2002 Fujitsu Component Limited
+ * Copyright (c) 2002 Genetec Corporation
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of The Fujitsu Component Limited nor the name of
+ *    Genetec corporation may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY FUJITSU COMPONENT LIMITED AND GENETEC
+ * CORPORATION ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL FUJITSU COMPONENT LIMITED OR GENETEC
+ * CORPORATION BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+
+#include <sys/param.h>
+#include <sys/systm.h>
+#include <sys/device.h>
+#include <sys/kernel.h>
+#include <sys/reboot.h>
+
+#include <machine/cpu.h>
+#include <machine/bus.h>
+
+#include <arm/cpufunc.h>
+#include <arm/mainbus/mainbus.h>
+#include <arm/s3c2xx0/s3c2800reg.h>
+#include <arm/s3c2xx0/s3c2800var.h>
+
+#include "locators.h"
+
+/* prototypes */
+static int	s3c2800_match(struct device *, struct cfdata *, void *);
+static void	s3c2800_attach(struct device *, struct device *, void *);
+static int	s3c2800_search(struct device *, struct cfdata *, void *);
+
+/* attach structures */
+CFATTACH_DECL(ssio, sizeof(struct s3c2800_softc), s3c2800_match, s3c2800_attach,
+    NULL, NULL);
+
+extern struct bus_space s3c2xx0_bs_tag;
+
+struct s3c2xx0_softc *s3c2xx0_softc;
+
+static int
+s3c2800_print(void *aux, const char *name)
+{
+	struct s3c2xx0_attach_args *sa = (struct s3c2xx0_attach_args *) aux;
+
+	if (sa->sa_size)
+		printf(" addr 0x%lx", sa->sa_addr);
+	if (sa->sa_size > 1)
+		printf("-0x%lx", sa->sa_addr + sa->sa_size - 1);
+	if (sa->sa_intr != SSIOCF_INTR_DEFAULT)
+		printf(" intr %d", sa->sa_intr);
+	if (sa->sa_index != SSIOCF_INDEX_DEFAULT)
+		printf(" unit %d", sa->sa_index);
+
+	return (UNCONF);
+}
+
+int
+s3c2800_match(struct device *parent, struct cfdata *match, void *aux)
+{
+	return 1;
+}
+
+void
+s3c2800_attach(struct device *parent, struct device *self, void *aux)
+{
+	struct s3c2800_softc *sc = (struct s3c2800_softc *) self;
+	bus_space_tag_t iot;
+	const char *which_registers;	/* for panic message */
+
+#define FAIL(which)  do { \
+	which_registers=(which); goto abort; }while(/*CONSTCOND*/0)
+
+	s3c2xx0_softc = &(sc->sc_sx);
+	sc->sc_sx.sc_iot = iot = &s3c2xx0_bs_tag;
+
+	if (bus_space_map(iot,
+		S3C2800_INTCTL_BASE, S3C2800_INTCTL_SIZE,
+		BUS_SPACE_MAP_LINEAR, &sc->sc_sx.sc_intctl_ioh))
+		FAIL("intc");
+	/* tell register addresses to interrupt handler */
+	s3c2800_intr_init(sc);
+
+	/* Map the GPIO registers */
+	if (bus_space_map(iot, S3C2800_GPIO_BASE, S3C2800_GPIO_SIZE,
+		0, &sc->sc_sx.sc_gpio_ioh))
+		FAIL("GPIO");
+
+#if 0
+	/* Map the DMA controller registers */
+	if (bus_space_map(iot, S3C2800_DMAC_BASE, S3C2800_DMAC_SIZE,
+		0, &sc->sc_sx.sc_dmach))
+		FAIL("DMAC");
+#endif
+
+	/* Memory controller */
+	if (bus_space_map(iot, S3C2800_MEMCTL_BASE,
+		S3C2800_MEMCTL_SIZE, 0, &sc->sc_sx.sc_memctl_ioh))
+		FAIL("MEMC");
+	/* Clock manager */
+	if (bus_space_map(iot, S3C2800_CLKMAN_BASE,
+		S3C2800_CLKMAN_SIZE, 0, &sc->sc_sx.sc_clkman_ioh))
+		FAIL("CLK");
+
+#if 0
+	/* Real time clock */
+	if (bus_space_map(iot, S3C2800_RTC_BASE,
+		S3C2800_RTC_SIZE, 0, &sc->sc_sx.sc_rtc_ioh))
+		FAIL("RTC");
+#endif
+
+	if (bus_space_map(iot, S3C2800_TIMER0_BASE,
+		S3C2800_TIMER_SIZE, 0, &sc->sc_tmr0_ioh))
+		FAIL("TIMER0");
+
+	if (bus_space_map(iot, S3C2800_TIMER1_BASE,
+		S3C2800_TIMER_SIZE, 0, &sc->sc_tmr1_ioh))
+		FAIL("TIMER0");
+
+
+	printf("\n");
+
+
+	/*
+	 *  Attach devices.
+	 */
+	config_search(s3c2800_search, self, NULL);
+	return;
+
+abort:
+	panic("%s: unable to map %s registers",
+	    self->dv_xname, which_registers);
+
+#undef FAIL
+}
+
+int
+s3c2800_search(struct device * parent, struct cfdata * cf, void *aux)
+{
+	struct s3c2800_softc *sc = (struct s3c2800_softc *) parent;
+	struct s3c2xx0_attach_args aa;
+
+	aa.sa_sc = sc;
+	aa.sa_iot = sc->sc_sx.sc_iot;
+	aa.sa_addr = cf->cf_loc[SSIOCF_ADDR];
+	aa.sa_size = cf->cf_loc[SSIOCF_SIZE];
+	aa.sa_index = cf->cf_loc[SSIOCF_INDEX];
+	aa.sa_intr = cf->cf_loc[SSIOCF_INTR];
+
+	if (config_match(parent, cf, &aa))
+		config_attach(parent, cf, &aa, s3c2800_print);
+
+	return 0;
+}
+
+void
+s3c2800_softreset(void)
+{
+	/* XXX */
+}
---
+++ sys/arch/arm/s3c2xx0/s3c2800_intr.c
@@ -0,0 +1,168 @@
+/* $NetBSD: s3c2800_intr.c,v 1.1 2002/11/20 17:52:50 bsh Exp $ */
+
+/*
+ * Copyright (c) 2002 Fujitsu Component Limited
+ * Copyright (c) 2002 Genetec Corporation
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of The Fujitsu Component Limited nor the name of
+ *    Genetec corporation may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY FUJITSU COMPONENT LIMITED AND GENETEC
+ * CORPORATION ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL FUJITSU COMPONENT LIMITED OR GENETEC
+ * CORPORATION BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+/*
+ * IRQ handler for Samsung S3C2800 processor.
+ * It has integrated interrupt controller.
+ */
+#include <sys/param.h>
+#include <sys/systm.h>
+#include <sys/malloc.h>
+#include <uvm/uvm_extern.h>
+#include <machine/bus.h>
+#include <machine/intr.h>
+#include <arm/cpufunc.h>
+
+#include <arm/s3c2xx0/s3c2800reg.h>
+#include <arm/s3c2xx0/s3c2800var.h>
+#include <arm/s3c2xx0/s3c2xx0_intr.h>
+
+/*
+ * interrupt dispatch table.
+ */
+
+struct s3c2xx0_intr_dispatch handler[ICU_LEN];
+
+__volatile int softint_pending;
+
+__volatile int current_spl_level;
+__volatile int intr_mask;
+
+/* interrupt masks for each level */
+int s3c2xx0_imask[NIPL];
+int s3c2xx0_ilevel[ICU_LEN];
+
+int current_intr_depth;
+
+vaddr_t intctl_base;		/* interrupt controller registers */
+#define icreg(offset) \
+	(*(volatile uint32_t *)(intctl_base+(offset)))
+
+/*
+ * Map a software interrupt queue to an interrupt priority level.
+ */
+static const int si_to_ipl[SI_NQUEUES] = {
+	IPL_SOFT,		/* SI_SOFT */
+	IPL_SOFTCLOCK,		/* SI_SOFTCLOCK */
+	IPL_SOFTNET,		/* SI_SOFTNET */
+	IPL_SOFTSERIAL,		/* SI_SOFTSERIAL */
+};
+/*
+ * called from irq_entry.
+ */
+void s3c2800_irq_handler(struct clockframe *);
+void
+s3c2800_irq_handler(struct clockframe *frame)
+{
+	uint32_t irqbits;
+	int irqno;
+	int saved_spl_level;
+
+	++current_intr_depth;
+	saved_spl_level = current_spl_level;
+
+	/* get pending IRQs */
+	irqbits = icreg(INTCTL_IRQPND) & ICU_INT_HWMASK;
+
+	for (irqno = 0; irqbits; ++irqno) {
+		if ((irqbits & (1 << irqno)) == 0)
+			continue;
+		/* raise spl to stop interrupts of lower priorities */
+		if (saved_spl_level < handler[irqno].level)
+			s3c2xx0_setipl(handler[irqno].level);
+
+		/* clear pending bit */
+		icreg(INTCTL_SRCPND) = 1 << irqno;
+#ifdef notyet
+		/* Enable interrupt */
+#endif
+		(*handler[irqno].func) (
+		    handler[irqno].cookie == 0
+		    ? frame : handler[irqno].cookie);
+#ifdef notyet
+		/* Disable interrupt */
+#endif
+
+		irqbits &= ~(1 << irqno);
+	}
+
+	/* restore spl to that was when this interrupt happen */
+	s3c2xx0_setipl(saved_spl_level);
+
+	if (softint_pending & intr_mask)
+		s3c2xx0_do_pending();
+
+	--current_intr_depth;
+}
+
+
+void *
+s3c2800_intr_establish(int irqno, int level,
+    int (* func) (void *), void *cookie)
+{
+	int save;
+
+	if (irqno < 0 || irqno >= ICU_LEN)
+		panic("intr_establish: bogus irq or type");
+
+	save = disable_interrupts(I32_bit);
+
+	handler[irqno].cookie = cookie;
+	handler[irqno].func = func;
+	handler[irqno].level = level;
+
+	s3c2xx0_update_intr_masks(irqno, level);
+
+	intr_mask = s3c2xx0_imask[current_spl_level];
+	*s3c2xx0_intr_mask_reg = intr_mask;
+
+	restore_interrupts(save);
+
+	return (&handler[irqno]);
+}
+
+
+void
+s3c2800_intr_init(struct s3c2800_softc *sc)
+{
+	intctl_base = (vaddr_t) bus_space_vaddr(sc->sc_sx.sc_iot,
+	    sc->sc_sx.sc_intctl_ioh);
+
+	s3c2xx0_intr_mask_reg = (uint32_t *)(intctl_base + INTCTL_INTMSK);
+
+	/* clear all pending interrupt */
+	icreg(INTCTL_SRCPND) = 0xffffffff;
+
+	s3c2xx0_intr_init(handler, ICU_LEN);
+}
