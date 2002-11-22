7c14b55d7bdc56fde923c907b5713afe2e780e7b
tron <tron@NetBSD.org>
2002-11-22 17:54:31 +0000

    Pull up revision 1.21-1.22 (requested by martin in ticket #948): Adjust residual counter for wide transfers.

--- sys/dev/ic/lsi64854.c
+++ sys/dev/ic/lsi64854.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: lsi64854.c,v 1.20 2001/11/15 09:48:06 lukem Exp $ */
+/*	$NetBSD: lsi64854.c,v 1.20.10.1 2002/11/22 17:54:31 tron Exp $ */
 
 /*-
  * Copyright (c) 1998 The NetBSD Foundation, Inc.
@@ -37,7 +37,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: lsi64854.c,v 1.20 2001/11/15 09:48:06 lukem Exp $");
+__KERNEL_RCSID(0, "$NetBSD: lsi64854.c,v 1.20.10.1 2002/11/22 17:54:31 tron Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -121,9 +121,12 @@ lsi64854_attach(sc)
 		return;
 	}
 
-	printf(": dma rev ");
 	csr = L64854_GCSR(sc);
 	sc->sc_rev = csr & L64854_DEVID;
+	if (sc->sc_rev == DMAREV_HME) {
+		return;
+	}
+	printf(": dma rev ");
 	switch (sc->sc_rev) {
 	case DMAREV_0:
 		printf("0");
@@ -139,9 +142,6 @@ lsi64854_attach(sc)
 		break;
 	case DMAREV_2:
 		printf("2");
-		break;
-	case DMAREV_HME:
-		printf("fas");
 		break;
 	default:
 		printf("unknown (0x%x)", sc->sc_rev);
@@ -438,6 +438,9 @@ lsi64854_scsi_intr(arg)
 	if (!(csr & D_WRITE) &&
 	    (resid = (NCR_READ_REG(nsc, NCR_FFLAG) & NCRFIFO_FF)) != 0) {
 		DPRINTF(LDB_SCSI, ("dmaintr: empty esp FIFO of %d ", resid));
+		if (nsc->sc_rev == NCR_VARIANT_FAS366 &&
+		    (NCR_READ_REG(nsc, NCR_CFG3) & NCRFASCFG3_EWIDE))
+			resid <<= 1;
 	}
 
 	if ((nsc->sc_espstat & NCRSTAT_TC) == 0) {
