955ee6e616afa9e44792a6ef7349eb5c2b2970dc
kristerw <kristerw@NetBSD.org>
2002-11-23 11:04:33 +0000

    Make this identical to the other m68k machine/bswap.h by including m68k/bswap.h instead of sys/bswap.h.
    Solves part of PR toolchain/18314.

--- sys/arch/sun3/include/bswap.h
+++ sys/arch/sun3/include/bswap.h
@@ -1,8 +1,3 @@
-/*     $NetBSD: bswap.h,v 1.2 1999/08/21 05:39:55 simonb Exp $   */
+/*     $NetBSD: bswap.h,v 1.3 2002/11/23 11:04:33 kristerw Exp $   */
 
-#ifndef _MACHINE_BSWAP_H_
-#define	_MACHINE_BSWAP_H_
-
-#include <sys/bswap.h>
-
-#endif /* !_MACHINE_BSWAP_H_ */
+#include <m68k/bswap.h>
