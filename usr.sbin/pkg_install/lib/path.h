9a3f6a4ac524fe67c61b5867177269458088bfa8
tron <tron@NetBSD.org>
2002-11-24 22:22:49 +0000

    Pull up revision 1.2 (requested by jschauma in ticket #1011): Introduce path_prepend_from_pkgname() and path_remove_first() and use in pkg_add to ensure the location of the package being added is checked first for any additional packages.

--- usr.sbin/pkg_install/lib/path.h
+++ usr.sbin/pkg_install/lib/path.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: path.h,v 1.1.2.2 2002/07/21 04:42:55 lukem Exp $	*/
+/*	$NetBSD: path.h,v 1.1.2.3 2002/11/24 22:22:49 tron Exp $	*/
 
 /*-
  * Copyright (c)2002 YAMAMOTO Takashi,
@@ -35,4 +35,6 @@ TAILQ_HEAD(pathhead, path);
 extern struct pathhead PkgPath;
 void path_create(const char *);
 void path_free(void);
+void path_prepend_from_pkgname(const char *);
+void path_remove_first(void);
 void path_setenv(const char *);
