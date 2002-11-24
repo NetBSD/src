801dde03135324a0a0eccdb288e08c417084f3a9
tron <tron@NetBSD.org>
2002-11-24 22:37:39 +0000

    Pull up revision 1.19 (requested by jschauma in ticket #1011): Do not abort incomplete deletion if the cwd was removed.  Instead, warn, stay in no-longer existing dir and continue with removal of db-files; otherwise, the packages contents would be removed, but the package not uninstalled.  Pointed out by Grant in PR pkg/18384.  OK'd by hubertf.

--- usr.sbin/pkg_install/lib/version.h
+++ usr.sbin/pkg_install/lib/version.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: version.h,v 1.13.2.5 2002/11/24 22:37:21 tron Exp $	*/
+/*	$NetBSD: version.h,v 1.13.2.6 2002/11/24 22:37:39 tron Exp $	*/
 
 /*
  * Copyright (c) 2001 Thomas Klausner.  All rights reserved.
@@ -33,6 +33,6 @@
 #ifndef _INST_LIB_VERSION_H_
 #define _INST_LIB_VERSION_H_
 
-#define PKGTOOLS_VERSION "20021114"
+#define PKGTOOLS_VERSION "20021123"
 
 #endif /* _INST_LIB_VERSION_H_ */
