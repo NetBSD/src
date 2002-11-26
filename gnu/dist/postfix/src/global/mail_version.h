02fe6765c533fcb4e7b83898cd08c951e44f3bfd
itojun <itojun@NetBSD.org>
2002-11-26 03:29:46 +0000

    postfix 1.1.12

--- gnu/dist/postfix/src/global/mail_version.h
+++ gnu/dist/postfix/src/global/mail_version.h
@@ -21,14 +21,14 @@
   * release date only, unless they include the same bugfix as a patch release.
   */
 #define VAR_MAIL_VERSION	"mail_version"
-#define DEF_MAIL_VERSION	"1.1.11"
+#define DEF_MAIL_VERSION	"1.1.12"
 extern char *var_mail_version;
 
  /*
   * Release date.
   */
 #define VAR_MAIL_RELEASE	"mail_release_date"
-#define DEF_MAIL_RELEASE	"20020528"
+#define DEF_MAIL_RELEASE	"20021122"
 extern char *var_mail_release;
 
 /* LICENSE
