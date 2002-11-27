2185c952fcf182853f32c99041dd4d998571e63d
christos <christos@NetBSD.org>
2002-11-27 21:59:23 +0000

    sync to current

--- sys/dev/i2o/iopvar.h
+++ sys/dev/i2o/iopvar.h
@@ -1,7 +1,7 @@
-/*	$NetBSD: iopvar.h,v 1.4.2.6 2002/11/11 22:09:13 nathanw Exp $	*/
+/*	$NetBSD: iopvar.h,v 1.4.2.7 2002/11/27 21:59:24 christos Exp $	*/
 
 /*-
- * Copyright (c) 2000, 2001 The NetBSD Foundation, Inc.
+ * Copyright (c) 2000, 2001, 2002 The NetBSD Foundation, Inc.
  * All rights reserved.
  *
  * This code is derived from software contributed to The NetBSD Foundation
@@ -112,8 +112,8 @@ struct iop_softc {
 	bus_space_handle_t sc_ioh;	/* Bus space handle */
 	bus_space_tag_t	sc_iot;		/* Bus space tag */
 	bus_dma_tag_t	sc_dmat;	/* Bus DMA tag */
-	bus_space_handle_t sc_rep_ioh;	/* Bus space handle */
-	bus_space_tag_t	sc_rep_iot;	/* Bus space tag */
+	bus_space_handle_t sc_msg_ioh;	/* Message queue bus space handle */
+	bus_space_tag_t	sc_msg_iot;	/* Message queue bus space tag */
 	void	 	*sc_ih;		/* Interrupt handler cookie */
 
 	struct iop_msg	*sc_ims;	/* Message wrappers */
