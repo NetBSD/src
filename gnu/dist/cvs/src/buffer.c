19a37cf7f26d5a831e1d57f99c1a9d5d0786836a
christos <christos@NetBSD.org>
2002-11-28 22:04:46 +0000

    Better fix for the portability cast problem via Urban Boquist.

--- gnu/dist/cvs/src/buffer.c
+++ gnu/dist/cvs/src/buffer.c
@@ -1247,6 +1247,19 @@ stdio_buffer_initialize (fp, child_pid, input, memory)
 			   (void *) bc);
 }
 
+/* Return the file associated with a stdio buffer. */
+FILE *
+stdio_buffer_get_file (buf)
+   struct buffer *buf;
+{
+   struct stdio_buffer_closure *bc;
+
+   assert(buf->shutdown == stdio_buffer_shutdown);
+
+   bc = (struct stdio_buffer_closure *) buf->closure;
+   return(bc->fp);
+}
+
 /* The buffer input function for a buffer built on a stdio FILE.  */
 
 static int
