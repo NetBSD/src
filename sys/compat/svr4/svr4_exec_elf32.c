a7ed5e188d3b14c328f7c64cbf36fd544888db2a
jdolecek <jdolecek>
2002-11-29 19:13:14 +0000

    Get the dynamic interpreter location using emul_find_interp(), so that it checks both the alternative/emul tree, and the non-emul tree. This makes it possible to run chrooted emulated binaries without need to setup shadow /emul tree within the chroot hierarchy. Only tested for COMPAT_LINUX, changes to other compat modules were mechanical. Fixes kern/19161 by Christian Groessler.

--- sys/compat/svr4/svr4_exec_elf32.c
+++ sys/compat/svr4/svr4_exec_elf32.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: svr4_exec_elf32.c,v 1.4 2002/08/26 21:06:03 christos Exp $	 */
+/*	$NetBSD: svr4_exec_elf32.c,v 1.5 2002/11/29 19:13:15 jdolecek Exp $	 */
 
 /*-
  * Copyright (c) 1994 The NetBSD Foundation, Inc.
@@ -37,7 +37,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: svr4_exec_elf32.c,v 1.4 2002/08/26 21:06:03 christos Exp $");
+__KERNEL_RCSID(0, "$NetBSD: svr4_exec_elf32.c,v 1.5 2002/11/29 19:13:15 jdolecek Exp $");
 
 #define	ELFSIZE		32				/* XXX should die */
 
@@ -69,16 +69,11 @@ svr4_elf32_probe(p, epp, eh, itp, pos)
 	char *itp;
 	vaddr_t *pos;
 {
-	const char *bp;
 	int error;
-	size_t len;
 
 	if (itp[0]) {
-		if ((error = emul_find(p, NULL, epp->ep_esch->es_emul->e_path, itp, &bp, 0)))
+		if ((error = emul_find_interp(p, epp->ep_esch->es_emul->e_path, itp)))
 			return error;
-		if ((error = copystr(bp, itp, MAXPATHLEN, &len)))
-			return error;
-		free((void *)bp, M_TEMP);
 	}
 	*pos = SVR4_INTERP_ADDR;
 	return 0;
