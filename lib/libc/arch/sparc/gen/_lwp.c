1f446387692c316fc1bc4e1a0ad9687245e532d5
uwe <uwe@NetBSD.org>
2002-11-29 15:40:50 +0000

    Clean up remaining sparc64'isms.  Beautify it a bit. XXX: keep the code that sets %i6 and %i7 in the window save area, though I think it's not necessary.

--- lib/libc/arch/sparc/gen/_lwp.c
+++ lib/libc/arch/sparc/gen/_lwp.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: _lwp.c,v 1.1.2.1 2002/11/23 13:55:16 martin Exp $ */
+/*	$NetBSD: _lwp.c,v 1.1.2.2 2002/11/29 15:40:50 uwe Exp $ */
 
 #include <sys/types.h>
 #include <ucontext.h>
@@ -6,9 +6,9 @@
 #include <stdlib.h>
 
 
-void _lwp_makecontext(ucontext_t *u, void (*start)(void *),
-		      void *arg, void *private,
-		      caddr_t stack_base, size_t stack_size)
+void
+_lwp_makecontext(ucontext_t *u, void (*start)(void *), void *arg,
+		 void *private, caddr_t stack_base, size_t stack_size)
 {
 	__greg_t *gr;
 	unsigned long *sp;
@@ -21,21 +21,22 @@ void _lwp_makecontext(ucontext_t *u, void (*start)(void *),
 	u->uc_stack.ss_sp = stack_base;
 	u->uc_stack.ss_size = stack_size;
 
+
 	sp = (ulong *)(stack_base + stack_size);
-	sp = (ulong *)((ulong)sp & ~0x0f);
+	sp = (ulong *)((ulong)sp & ~0x07);
 
-	sp -= 8 + 8 + 6;
+	/* Make room for the fake caller stack frame (CCFSZ, only in words) */
+	sp -= 8 + 8 + 1 + 6 + 1;
 
-	sp[4]  = (ulong)arg;
-	sp[14] = (ulong)sp;
-	sp[15] = (ulong)_lwp_exit - 8;
-
-	sp = (ulong *)((caddr_t)sp);
-
+	/* Entering (*start)(arg), return is to _lwp_exit */
 	gr[_REG_PC] = (ulong) start;
 	gr[_REG_nPC] = (ulong) start + 4;
-
-	gr[_REG_O0] = (ulong) arg;
-	gr[_REG_O6] = (ulong) sp;
+	gr[_REG_O0] = (ulong)arg;
+	gr[_REG_O6] = (ulong)sp;
 	gr[_REG_O7] = (ulong)_lwp_exit - 8;
+
+	/* XXX: uwe: why do we need this? */
+	/* create loopback in the window save area on the stack? */
+	sp[8+6] = (ulong)sp;		/* %i6 */
+	sp[8+7] = (ulong)_lwp_exit - 8;	/* %i7 */
 }
