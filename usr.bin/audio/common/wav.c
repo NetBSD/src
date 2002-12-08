d3c15d65b383b0dc00fb2779c374c0a9ae95e29f
mrg <mrg@NetBSD.org>
2002-12-08 10:49:03 +0000

    remove some #if 0 debug code

--- usr.bin/audio/common/wav.c
+++ usr.bin/audio/common/wav.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: wav.c,v 1.3 2002/01/25 15:33:51 mrg Exp $	*/
+/*	$NetBSD: wav.c,v 1.4 2002/12/08 10:49:03 mrg Exp $	*/
 
 /*
  * Copyright (c) 2002 Matthew R. Green
@@ -127,10 +127,6 @@ audio_wav_parse_hdr(hdr, sz, enc, prec, sample, channels, datasize)
 
 	memcpy(&fmt, (owhere + 8), sizeof fmt);
 
-#if 0
-printf("fmt header is:\n\t%d\ttag\n\t%d\tchannels\n\t%d\tsample rate\n\t%d\tavg_bps\n\t%d\talignment\n\t%d\tbits per sample\n", getle16(fmt.tag), getle16(fmt.channels), getle32(fmt.sample_rate), getle32(fmt.avg_bps), getle16(fmt.alignment), getle16(fmt.bits_per_sample));
-#endif
-
 	switch (getle16(fmt.tag)) {
 	case WAVE_FORMAT_UNKNOWN:
 	case WAVE_FORMAT_ADPCM:
@@ -177,9 +173,6 @@ printf("fmt header is:\n\t%d\ttag\n\t%d\tchannels\n\t%d\tsample rate\n\t%d\tavg_
 
 	do {
 		memcpy(&part, where, sizeof part);
-#if 0
-printf("part `%c%c%c%c' len = %d\n", part.name[0], part.name[1], part.name[2], part.name[3], getle32(part.len));
-#endif
 		owhere = where;
 		where += (getle32(part.len) + 8);
 	} while (where < end && strncmp(part.name, strdata, sizeof strdata));
