6a86b956dfa8c6144f2b56900bdf4aba10c81e12
scw <scw@NetBSD.org>
2002-12-09 12:15:48 +0000

    New umbrella-port for PowerPC-based evaluation boards.
    The first board to be included here is the port to the 405GP-based
    Walnut evaluation board, which up until now lived in arch/walnut.
    
    arch/walnut will go away soon, once all the remaining walnut-isms
    in the tree have been dealt with.

---
+++ sys/arch/evbppc/include/psl.h
@@ -0,0 +1,23 @@
+/*	$NetBSD: psl.h,v 1.1 2002/12/09 12:16:13 scw Exp $	*/
+
+#ifdef _KERNEL_OPT
+#include "opt_ppcarch.h"
+#endif
+
+#include <powerpc/psl.h>
+
+#ifdef PPC_IBM4XX
+/* Apparently we get unexplained machine checks, so disable them. */
+#undef PSL_USERSET
+#define	PSL_USERSET	(PSL_EE | PSL_PR | PSL_IR | PSL_DR | PSL_RI)
+
+/* 
+ * We also need to override the PSL_SE bit.  4xx have completely
+ * different debug register support.
+ *
+ * The SE bit is actually the DWE bit.  We want to set the DE bit
+ * to enable the debug regs instead of the DWE bit.
+ */
+#undef	PSL_SE
+#define	PSL_SE	0x00000200
+#endif
---
+++ sys/arch/evbppc/include/types.h
@@ -0,0 +1,5 @@
+/*	$NetBSD: types.h,v 1.1 2002/12/09 12:16:19 scw Exp $	*/
+
+#include <powerpc/types.h>
+
+#define	__HAVE_NWSCONS
