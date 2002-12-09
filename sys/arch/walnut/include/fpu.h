ce8f3556b3e6c8ed37153c6789d80a065bb62aad
scw <scw@NetBSD.org>
2002-12-09 14:57:47 +0000

    The walnut port has been subsumed by the new evbppc port.

--- sys/arch/walnut/include/fpu.h
+++
@@ -1,3 +0,0 @@
-/*	$NetBSD: fpu.h,v 1.1 2001/06/13 06:01:56 simonb Exp $	*/
-
-#include <powerpc/fpu.h>
--- sys/arch/walnut/include/rnd.h
+++
@@ -1,3 +0,0 @@
-/*	$NetBSD: rnd.h,v 1.1 2001/06/13 06:01:58 simonb Exp $	*/
-
-#include <powerpc/rnd.h>
