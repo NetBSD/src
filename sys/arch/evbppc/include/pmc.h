60e46f59bb2554073ef9abbf25a242d47e954459
scw <scw@NetBSD.org>
2002-12-09 12:15:48 +0000

    New umbrella-port for PowerPC-based evaluation boards.
    The first board to be included here is the port to the 405GP-based
    Walnut evaluation board, which up until now lived in arch/walnut.
    
    arch/walnut will go away soon, once all the remaining walnut-isms
    in the tree have been dealt with.

---
+++ sys/arch/evbppc/include/pmc.h
@@ -0,0 +1,3 @@
+/*	$NetBSD: pmc.h,v 1.1 2002/12/09 12:16:12 scw Exp $	*/
+
+#include <powerpc/pmc.h>
---
+++ sys/arch/evbppc/include/stdarg.h
@@ -0,0 +1,3 @@
+/*	$NetBSD: stdarg.h,v 1.1 2002/12/09 12:16:17 scw Exp $	*/
+
+#include <powerpc/stdarg.h>
