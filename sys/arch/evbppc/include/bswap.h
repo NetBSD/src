6a68b9b5d6d293a9f7f55ce074f2f2e6f7344943
scw <scw>
2002-12-09 12:15:48 +0000

    New umbrella-port for PowerPC-based evaluation boards.
    The first board to be included here is the port to the 405GP-based
    Walnut evaluation board, which up until now lived in arch/walnut.
    
    arch/walnut will go away soon, once all the remaining walnut-isms
    in the tree have been dealt with.

---
+++ sys/arch/evbppc/include/bswap.h
@@ -0,0 +1,4 @@
+/*	$NetBSD: bswap.h,v 1.1 2002/12/09 12:15:58 scw Exp $	*/
+
+#include <powerpc/bswap.h>
+
---
+++ sys/arch/evbppc/include/macho_machdep.h
@@ -0,0 +1,3 @@
+/*	$NetBSD: macho_machdep.h,v 1.1 2002/12/09 12:16:09 scw Exp $	*/
+
+#include <powerpc/macho_machdep.h>
