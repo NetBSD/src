8461b5b85fb7e50d33380af1713cb7e4598974c5
christos <christos@NetBSD.org>
2002-12-10 18:33:26 +0000

    PR/19339: Martin Weber: Tar fails to append on empty files and exits with 0. Revert previous change for PR/18689. We always want to exit with an error if we could not determine the archive format. Instead, treat empty files specially. On list/extract we turn into no/op. On append, we turn into archive.

--- bin/pax/ar_subs.c
+++ bin/pax/ar_subs.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: ar_subs.c,v 1.20 2002/10/18 11:54:22 itojun Exp $	*/
+/*	$NetBSD: ar_subs.c,v 1.21 2002/12/10 18:33:26 christos Exp $	*/
 
 /*-
  * Copyright (c) 1992 Keith Muller.
@@ -42,7 +42,7 @@
 #if 0
 static char sccsid[] = "@(#)ar_subs.c	8.2 (Berkeley) 4/18/94";
 #else
-__RCSID("$NetBSD: ar_subs.c,v 1.20 2002/10/18 11:54:22 itojun Exp $");
+__RCSID("$NetBSD: ar_subs.c,v 1.21 2002/12/10 18:33:26 christos Exp $");
 #endif
 #endif /* not lint */
 
@@ -1267,6 +1267,6 @@ get_arc(void)
 	/*
 	 * we cannot find a header, bow, apologize and quit
 	 */
-	tty_warn(0, "Sorry, unable to determine archive format.");
+	tty_warn(1, "Sorry, unable to determine archive format.");
 	return(-1);
 }
