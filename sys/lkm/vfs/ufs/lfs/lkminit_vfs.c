f0f124f0bf49f5a5dd11d7a6205c0ee6e0941aa3
scw <scw@NetBSD.org>
2002-12-11 12:13:11 +0000

    Avoid strict-alias warnings.

--- sys/lkm/vfs/ufs/lfs/lkminit_vfs.c
+++ sys/lkm/vfs/ufs/lfs/lkminit_vfs.c
@@ -1,4 +1,4 @@
-/* $NetBSD: lkminit_vfs.c,v 1.3 2001/11/12 23:23:40 lukem Exp $ */
+/* $NetBSD: lkminit_vfs.c,v 1.4 2002/12/11 12:13:12 scw Exp $ */
 
 /*-
  * Copyright (c) 2000 The NetBSD Foundation, Inc.
@@ -34,7 +34,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: lkminit_vfs.c,v 1.3 2001/11/12 23:23:40 lukem Exp $");
+__KERNEL_RCSID(0, "$NetBSD: lkminit_vfs.c,v 1.4 2002/12/11 12:13:12 scw Exp $");
 
 #include <sys/param.h>
 #include <sys/ioctl.h>
@@ -107,7 +107,7 @@ lfs_lkmentry(lkmtp, cmd, ver)
 
 	/* do this first, lkmdispatch() expects this to be already filled in */
 	if (cmd == LKM_E_LOAD)
-		lkmtp->private.lkm_any = (struct lkm_any *)&_module;
+		lkmtp->private.lkm_any = (void *)&_module;
 
 	if ((error = lkmdispatch(lkmtp, cmd)))
 		return error;
