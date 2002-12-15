f9fa850a7e426dd84ade74385762e63b8c201fdb
takemura <takemura@NetBSD.org>
2002-12-15 09:17:07 +0000

    Changed SCALE value from (1024*1024) to (1024*256) to support large (1024<) sample values.

--- sys/dev/hpc/tpcalib.c
+++ sys/dev/hpc/tpcalib.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: tpcalib.c,v 1.4 2002/03/17 19:40:56 atatat Exp $	*/
+/*	$NetBSD: tpcalib.c,v 1.5 2002/12/15 09:17:07 takemura Exp $	*/
 
 /*
  * Copyright (c) 1999 Shin Takemura All rights reserved.
@@ -28,7 +28,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: tpcalib.c,v 1.4 2002/03/17 19:40:56 atatat Exp $");
+__KERNEL_RCSID(0, "$NetBSD: tpcalib.c,v 1.5 2002/12/15 09:17:07 takemura Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -49,7 +49,7 @@ int	tpcalib_debug = 0;
 extern int mra_Y_AX1_BX2_C(int *, int, int *, int, int *, int, int, int, int *,
     int *, int *);
 
-#define SCALE	(1024*1024)
+#define SCALE	(1024*256)
 
 int
 tpcalib_init(struct tpcalib_softc *sc)
@@ -118,6 +118,9 @@ tpcalib_ioctl(struct tpcalib_softc *sc, u_long cmd, caddr_t data, int flag,
 				sc->sc_miny = d->miny;
 				sc->sc_maxy = d->maxy;
 				sc->sc_saved = *d;
+				DPRINTF(("tpcalib: x=%d~%d y=%d~%d\n",
+				    sc->sc_minx, sc->sc_maxx,
+				    sc->sc_miny, sc->sc_maxy));
 				DPRINTF(("tpcalib: Ax=%d Bx=%d Cx=%d\n",
 				    sc->sc_ax, sc->sc_bx, sc->sc_cx));
 				DPRINTF(("tpcalib: Ay=%d By=%d Cy=%d\n",
