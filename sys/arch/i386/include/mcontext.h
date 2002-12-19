b6f8a372b1015928af7f811c87c487f61ac92275
thorpej <thorpej@NetBSD.org>
2002-12-19 02:43:49 +0000

    Expose _UC_UCONTEXT_ALIGN.

--- sys/arch/i386/include/mcontext.h
+++ sys/arch/i386/include/mcontext.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: mcontext.h,v 1.1.2.4 2002/09/06 17:49:16 nathanw Exp $	*/
+/*	$NetBSD: mcontext.h,v 1.1.2.5 2002/12/19 02:43:49 thorpej Exp $	*/
 
 /*-
  * Copyright (c) 1999 The NetBSD Foundation, Inc.
@@ -101,9 +101,10 @@ typedef struct {
 
 #define _UC_MACHINE_PAD	5	/* Padding appended to ucontext_t */
 
+#define _UC_UCONTEXT_ALIGN	(~0xf)
+
 #ifdef _KERNEL
 #define _UC_MACHINE_SP(uc)	((uc)->uc_mcontext.__gregs[_REG_UESP])
-#define _UC_UCONTEXT_ALIGN	(~0xf)
 #endif
 
 #endif	/* !_I386_MCONTEXT_H_ */
