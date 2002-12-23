238efe4697f6366cbd89abaf0e48080bbb2c3a61
tsutsui <tsutsui@NetBSD.org>
2002-12-23 02:58:36 +0000

    Replace magic numbers for power management control with PCI_PMCSR* macros.
    XXX Should we use pci_get_powerstate() and pci_set_powerstate() in pci.c?

--- sys/dev/pci/esa.c
+++ sys/dev/pci/esa.c
@@ -1,4 +1,4 @@
-/* $NetBSD: esa.c,v 1.17 2002/10/02 16:51:12 thorpej Exp $ */
+/* $NetBSD: esa.c,v 1.18 2002/12/23 02:58:36 tsutsui Exp $ */
 
 /*
  * Copyright (c) 2001, 2002 Jared D. McNeill <jmcneill@invisible.ca>
@@ -1597,9 +1597,9 @@ esa_power(struct esa_softc *sc, int state)
 	int pmcapreg;
 
 	if (pci_get_capability(pc, tag, PCI_CAP_PWRMGMT, &pmcapreg, 0)) {
-		data = pci_conf_read(pc, tag, pmcapreg + 4);
+		data = pci_conf_read(pc, tag, pmcapreg + PCI_PMCSR);
 		if ((data & PCI_PMCSR_STATE_MASK) != state)
-			pci_conf_write(pc, tag, pmcapreg + 4, state);
+			pci_conf_write(pc, tag, pmcapreg + PCI_PMCSR, state);
 	}
 		
 	return (0);
