a9ded362eee5342ead15a31917baf8d1ff9aa72e
jmcneill <jmcneill>
2002-12-28 17:31:25 +0000

    NACPCA -> NACPI, so isa_dmainit gets called if we have ACPI support too.

--- sys/arch/i386/i386/mainbus.c
+++ sys/arch/i386/i386/mainbus.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: mainbus.c,v 1.44 2002/12/28 17:11:50 matt Exp $	*/
+/*	$NetBSD: mainbus.c,v 1.45 2002/12/28 17:31:25 jmcneill Exp $	*/
 
 /*
  * Copyright (c) 1996 Christopher G. Demetriou.  All rights reserved.
@@ -31,7 +31,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: mainbus.c,v 1.44 2002/12/28 17:11:50 matt Exp $");
+__KERNEL_RCSID(0, "$NetBSD: mainbus.c,v 1.45 2002/12/28 17:31:25 jmcneill Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -189,7 +189,7 @@ mainbus_attach(parent, self, aux)
 	pci_mode = pci_mode_detect();
 #endif
 
-#if NISADMA > 0 && (NACPCI > 0 || NPNPBIOS > 0)
+#if NISADMA > 0 && (NACPI > 0 || NPNPBIOS > 0)
 	/*
 	 * ACPI and PNPBIOS need ISA DMA initialized before they start probing.
 	 */
