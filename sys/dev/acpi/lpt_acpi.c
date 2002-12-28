6be3bfe3a6ba6c75777565da37ccc75afef3f6b6
jmcneill <jmcneill@NetBSD.org>
2002-12-28 08:04:28 +0000

    Add an lpt attachment to acpi(4)

---
+++ sys/dev/acpi/lpt_acpi.c
@@ -0,0 +1,144 @@
+/* $NetBSD: lpt_acpi.c,v 1.1 2002/12/28 08:04:28 jmcneill Exp $ */
+
+/*
+ * Copyright (c) 2002 Jared D. McNeill <jmcneill@invisible.ca>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <sys/cdefs.h>
+__KERNEL_RCSID(0, "$NetBSD: lpt_acpi.c,v 1.1 2002/12/28 08:04:28 jmcneill Exp $");
+
+#include <sys/param.h>
+#include <sys/systm.h>
+#include <sys/errno.h>
+#include <sys/ioctl.h>
+#include <sys/syslog.h>
+#include <sys/device.h>
+#include <sys/proc.h>
+#include <sys/termios.h>
+
+#include <machine/bus.h>
+
+#include <dev/isa/isavar.h>
+#include <dev/isa/isadmavar.h>
+
+#include <dev/acpi/acpica.h>
+#include <dev/acpi/acpireg.h>
+#include <dev/acpi/acpivar.h>
+
+#include <dev/ic/lptvar.h>
+
+int	lpt_acpi_match(struct device *, struct cfdata *, void *);
+void	lpt_acpi_attach(struct device *, struct device *, void *);
+
+struct lpt_acpi_softc {
+	struct lpt_softc sc_lpt;
+};
+
+CFATTACH_DECL(lpt_acpi, sizeof(struct lpt_acpi_softc), lpt_acpi_match,
+    lpt_acpi_attach, NULL, NULL);
+
+/*
+ * Supported device IDs
+ */
+
+static const char * const lpt_acpi_ids[] = {
+	"PNP0400",	/* Standard LPT printer port */
+	"PNP0401",	/* ECP printer port */
+	NULL
+};
+
+/*
+ * lpt_acpi_match: autoconf(9) match routine
+ */
+int
+lpt_acpi_match(struct device *parent, struct cfdata *match, void *aux)
+{
+	struct acpi_attach_args *aa = aux;
+	const char *id;
+	int i;
+
+	if (aa->aa_node->ad_type != ACPI_TYPE_DEVICE)
+		return 0;
+
+	for (i = 0; (id = lpt_acpi_ids[i]) != NULL; ++i) {
+		if (strcmp(aa->aa_node->ad_devinfo.HardwareId, id) == 0)
+			return 1;
+	}
+
+	/* No matches found */
+	return 0;
+}
+
+/*
+ * lpt_acpi_attach: autoconf(9) attach routine
+ */
+void
+lpt_acpi_attach(struct device *parent, struct device *self, void *aux)
+{
+	struct lpt_acpi_softc *asc = (struct lpt_acpi_softc *)self;
+	struct lpt_softc *sc = &asc->sc_lpt;
+	struct acpi_attach_args *aa = aux;
+	struct acpi_resources res;
+	struct acpi_io *io;
+	struct acpi_irq *irq;
+	ACPI_STATUS rv;
+
+	printf("\n");
+
+	/* parse resources */
+	rv = acpi_resource_parse(&sc->sc_dev, aa->aa_node, &res,
+	    &acpi_resource_parse_ops_default);
+	if (rv != AE_OK) {
+		printf("%s: unable to parse resources\n", sc->sc_dev.dv_xname);
+		return;
+	}
+
+	/* find our i/o registers */
+	io = acpi_res_io(&res, 0);
+	if (io == NULL) {
+		printf("%s: unable to find i/o register resource\n",
+		    sc->sc_dev.dv_xname);
+		return;
+	}
+
+	/* find our IRQ */
+	irq = acpi_res_irq(&res, 0);
+	if (irq == NULL) {
+		printf("%s: unable to find irq resource\n",
+		    sc->sc_dev.dv_xname);
+		return;
+	}
+
+	sc->sc_iot = aa->aa_iot;
+	if (bus_space_map(sc->sc_iot, io->ar_base, io->ar_length,
+		    0, &sc->sc_ioh)) {
+		printf("%s: can't map i/o space\n", sc->sc_dev.dv_xname);
+		return;
+	}
+
+	lpt_attach_subr(sc);
+
+	sc->sc_ih = isa_intr_establish(aa->aa_ic, irq->ar_irq, 0,
+				       IPL_TTY, lptintr, sc);
+}
