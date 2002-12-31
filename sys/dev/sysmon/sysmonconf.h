93c08abf37e4572d6ca8004de8279234883251ac
thorpej <thorpej@NetBSD.org>
2002-12-31 22:47:48 +0000

    Nothing cares about NSYSMON anymore; delete it.

--- sys/dev/sysmon/sysmonconf.h
+++ sys/dev/sysmon/sysmonconf.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: sysmonconf.h,v 1.1 2000/11/05 04:06:14 thorpej Exp $	*/
+/*	$NetBSD: sysmonconf.h,v 1.2 2002/12/31 22:47:48 thorpej Exp $	*/
 
 /*-
  * Copyright (c) 2000 Zembu Labs, Inc.
@@ -38,11 +38,5 @@
 
 #include "sysmon_envsys.h"
 #include "sysmon_wdog.h"
-
-#if (NSYSMON_ENVSYS > 0) || (NSYSMON_WDOG > 0)
-#define	NSYSMON	1
-#else
-#define	NSYSMON	0
-#endif
 
 #endif /* _DEV_SYSMON_SYSMONCONF_H_ */
