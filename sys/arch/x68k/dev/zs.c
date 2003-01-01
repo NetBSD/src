04c7c6262d4dfa536769487da392ffed91e45575
thorpej <thorpej>
2003-01-01 02:31:13 +0000

    Use aprint_normal() for cfprint routines.

--- sys/arch/x68k/dev/zs.c
+++ sys/arch/x68k/dev/zs.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: zs.c,v 1.22 2002/10/02 16:02:44 thorpej Exp $	*/
+/*	$NetBSD: zs.c,v 1.23 2003/01/01 02:31:14 thorpej Exp $	*/
 
 /*-
  * Copyright (c) 1998 Minoura Makoto
@@ -302,10 +302,10 @@ zs_print(aux, name)
 	struct zsc_attach_args *args = aux;
 
 	if (name != NULL)
-		printf("%s: ", name);
+		aprint_normal("%s: ", name);
 
 	if (args->channel != -1)
-		printf(" channel %d", args->channel);
+		aprint_normal(" channel %d", args->channel);
 
 	return UNCONF;
 }
