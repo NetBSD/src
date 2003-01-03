2e468a5104b8a054c865574abf288ac622c04857
rafal <rafal>
2003-01-03 10:30:00 +0000

    Cosmetic fix -- move __HAVE_MIPS_MACHDEP_CACHE_CONFIG to "MIPS specific options" section.

--- sys/arch/sgimips/include/types.h
+++ sys/arch/sgimips/include/types.h
@@ -1,10 +1,10 @@
-/*	$NetBSD: types.h,v 1.5 2003/01/03 09:09:22 rafal Exp $	*/
+/*	$NetBSD: types.h,v 1.6 2003/01/03 10:30:00 rafal Exp $	*/
 
 #include <mips/types.h>
 
 #define	__HAVE_DEVICE_REGISTER
 #define	__HAVE_GENERIC_SOFT_INTERRUPTS
-#define	__HAVE_MIPS_MACHDEP_CACHE_CONFIG
 
 /* MIPS specific options */
 #define	__HAVE_BOOTINFO_H
+#define	__HAVE_MIPS_MACHDEP_CACHE_CONFIG
