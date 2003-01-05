bb0176f359862f250bc5b63e867dbec0b426e198
agc <agc@NetBSD.org>
2003-01-05 21:27:20 +0000

    Use EXIT_FAILURE, rather than hardcoding a value of '1' in err(3) and errx(3).
    Plug a memory leak by modifying the signature of
    _pkgdb_getPKGDB_FILE() to take a buffer and its size, and fill in that
    buffer and return it.

--- usr.sbin/pkg_install/info/main.c
+++ usr.sbin/pkg_install/info/main.c
@@ -1,11 +1,11 @@
-/*	$NetBSD: main.c,v 1.31 2002/08/04 19:43:08 hubertf Exp $	*/
+/*	$NetBSD: main.c,v 1.32 2003/01/05 21:27:25 agc Exp $	*/
 
 #include <sys/cdefs.h>
 #ifndef lint
 #if 0
 static char *rcsid = "from FreeBSD Id: main.c,v 1.14 1997/10/08 07:47:26 charnier Exp";
 #else
-__RCSID("$NetBSD: main.c,v 1.31 2002/08/04 19:43:08 hubertf Exp $");
+__RCSID("$NetBSD: main.c,v 1.32 2003/01/05 21:27:25 agc Exp $");
 #endif
 #endif
 
@@ -202,14 +202,14 @@ main(int argc, char **argv)
 		char   *s;
 
 		if (pkgdb_open(1) == -1)
-			err(1, "cannot open pkgdb");
+			err(EXIT_FAILURE, "cannot open pkgdb");
 
 		s = pkgdb_retrieve(CheckPkg);
 
 		if (s) {
 			CheckPkg = strdup(s);
 		} else {
-			errx(1, "No matching pkg for %s.", CheckPkg);
+			errx(EXIT_FAILURE, "No matching pkg for %s.", CheckPkg);
 		}
 
 		pkgdb_close();
@@ -220,7 +220,7 @@ main(int argc, char **argv)
 	/* Get all the remaining package names, if any */
 	if (File2Pkg && !AllInstalled)
 		if (pkgdb_open(1) == -1) {
-			err(1, "cannot open pkgdb");
+			err(EXIT_FAILURE, "cannot open pkgdb");
 		}
 	while (*argv) {
 		/* pkgdb: if -F flag given, don't add pkgnames to the "pkgs"
@@ -235,11 +235,11 @@ main(int argc, char **argv)
 				lpp = alloc_lpkg(s);
 				TAILQ_INSERT_TAIL(&pkgs, lpp, lp_link);
 			} else
-				errx(1, "No matching pkg for %s.", *argv);
+				errx(EXIT_FAILURE, "No matching pkg for %s.", *argv);
 		} else {
 			if (ispkgpattern(*argv)) {
 				if (findmatchingname(_pkgdb_getPKGDB_DIR(), *argv, add_to_list_fn, &pkgs) == 0)
-					errx(1, "No matching pkg for %s.", *argv);
+					errx(EXIT_FAILURE, "No matching pkg for %s.", *argv);
 			} else {
 				lpp = alloc_lpkg(*argv);
 				TAILQ_INSERT_TAIL(&pkgs, lpp, lp_link);
--- usr.sbin/pkg_install/lib/file.c
+++ usr.sbin/pkg_install/lib/file.c
@@ -1,11 +1,11 @@
-/*	$NetBSD: file.c,v 1.56 2002/10/17 03:31:34 christos Exp $	*/
+/*	$NetBSD: file.c,v 1.57 2003/01/05 21:27:27 agc Exp $	*/
 
 #include <sys/cdefs.h>
 #ifndef lint
 #if 0
 static const char *rcsid = "from FreeBSD Id: file.c,v 1.29 1997/10/08 07:47:54 charnier Exp";
 #else
-__RCSID("$NetBSD: file.c,v 1.56 2002/10/17 03:31:34 christos Exp $");
+__RCSID("$NetBSD: file.c,v 1.57 2003/01/05 21:27:27 agc Exp $");
 #endif
 #endif
 
@@ -184,7 +184,7 @@ fileURLHost(const char *fname, char *where, int max)
 	assert(max > 0);
 
 	if ((i = URLlength(fname)) < 0) {	/* invalid URL? */
-		errx(1, "fileURLhost called with a bad URL: `%s'", fname);
+		errx(EXIT_FAILURE, "fileURLhost called with a bad URL: `%s'", fname);
 	}
 	fname += i;
 	/* Do we have a place to stick our work? */
@@ -209,7 +209,7 @@ fileURLFilename(const char *fname, char *where, int max)
 	assert(max > 0);
 
 	if ((i = URLlength(fname)) < 0) {	/* invalid URL? */
-		errx(1, "fileURLFilename called with a bad URL: `%s'", fname);
+		errx(EXIT_FAILURE, "fileURLFilename called with a bad URL: `%s'", fname);
 	}
 	fname += i;
 	/* Do we have a place to stick our work? */
@@ -240,7 +240,7 @@ fileGetURL(const char *spec)
 
 	rp = NULL;
 	if (!IS_URL(spec)) {
-		errx(1, "fileGetURL was called with non-url arg '%s'", spec);
+		errx(EXIT_FAILURE, "fileGetURL was called with non-url arg '%s'", spec);
 	}
 
  	/* Some sanity checks on the URL */
@@ -378,7 +378,7 @@ fileFindByPath(const char *fname)
 		else {
 			char cwdtmp[MAXPATHLEN];
 			if (getcwd(cwdtmp, sizeof(cwdtmp)) == NULL)
-				errx(1, "getcwd");
+				errx(EXIT_FAILURE, "getcwd");
 			snprintf(tmp, sizeof(tmp), "%s/%s/%s", cwdtmp, cp2, fname);
 		}
 		cp = resolvepattern(tmp);
