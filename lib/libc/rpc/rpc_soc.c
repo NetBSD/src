113fc3581e16a3760253b27ac7e15a8a8cad4d70
jmc <jmc@NetBSD.org>
2003-01-06 04:43:52 +0000

    Pull up revisions 1.8-1.9 (requested by fvdl in ticket #1037)  * Adapt include files for rpc_com.h/rpc_internal.h split.  * Implement non-blocking connections.  * Add rpc_control to svc.c

--- lib/libc/rpc/rpc_soc.c
+++ lib/libc/rpc/rpc_soc.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: rpc_soc.c,v 1.8 2001/11/04 13:57:30 lukem Exp $	*/
+/*	$NetBSD: rpc_soc.c,v 1.8.2.1 2003/01/06 04:43:52 jmc Exp $	*/
 
 /*
  * Sun RPC is a product of Sun Microsystems, Inc. and is provided for
@@ -72,7 +72,7 @@ static char sccsid[] = "@(#)rpc_soc.c 1.41 89/05/02 Copyr 1988 Sun Micro";
 #include <syslog.h>
 #include <unistd.h>
 
-#include "rpc_com.h"
+#include "rpc_internal.h"
 
 #ifdef __weak_alias
 __weak_alias(clntudp_bufcreate,_clntudp_bufcreate)
