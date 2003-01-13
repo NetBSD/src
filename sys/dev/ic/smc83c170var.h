565df331cc5864a484a71e1b032aebad64e57fa0
bouyer <bouyer@NetBSD.org>
2003-01-13 17:00:18 +0000

    For short (< ETHER_MIN_LEN) packets, supply a zero'd buffer for the extra bytes to make sure the chip won't send random data.

--- sys/dev/ic/smc83c170var.h
+++ sys/dev/ic/smc83c170var.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: smc83c170var.h,v 1.6 2002/11/07 07:48:36 thorpej Exp $	*/
+/*	$NetBSD: smc83c170var.h,v 1.7 2003/01/13 17:00:18 bouyer Exp $	*/
 
 /*-
  * Copyright (c) 1998, 1999 The NetBSD Foundation, Inc.
@@ -117,6 +117,8 @@ struct epic_softc {
 
 	bus_dmamap_t sc_cddmamap;	/* control data DMA map */
 #define	sc_cddma	sc_cddmamap->dm_segs[0].ds_addr
+	bus_dmamap_t sc_nulldmamap;	/* DMA map for the pad buffer */
+#define sc_nulldma	sc_nulldmamap->dm_segs[0].ds_addr
 
 	/*
 	 * Software state for transmit and receive descriptors.
