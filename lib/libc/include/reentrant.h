01e184ca022ab2723ac79df53212082c59c1e5dd
thorpej <thorpej@NetBSD.org>
2003-01-19 19:25:05 +0000

    Use <threadlib.h>.

--- lib/libc/include/reentrant.h
+++ lib/libc/include/reentrant.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: reentrant.h,v 1.7 2003/01/18 11:23:54 thorpej Exp $	*/
+/*	$NetBSD: reentrant.h,v 1.8 2003/01/19 19:25:05 thorpej Exp $	*/
 
 /*-
  * Copyright (c) 1997, 1998, 2003 The NetBSD Foundation, Inc.
@@ -88,103 +88,17 @@
  *
  * When the thread library is linked in, it provides strong-alias versions
  * of those symbols which dispatch to its own real thread operations.
+ *
+ * [This interface has been exposed to simplify making other libraries
+ * thread-safe.]
  */
 
 #ifdef _REENTRANT
 
-#include <pthread.h>
-#include <signal.h>
-
-#define	mutex_t			pthread_mutex_t
-#define	MUTEX_INITIALIZER	PTHREAD_MUTEX_INITIALIZER
-
-#define	mutexattr_t		pthread_mutexattr_t
-
-#define	cond_t			pthread_cond_t
-#define	COND_INITIALIZER	PTHREAD_COND_INITIALIZER
-
-#define	condattr_t		pthread_condattr_t
-
-#define	rwlock_t		pthread_rwlock_t
-#define	RWLOCK_INITIALIZER	PTHREAD_RWLOCK_INITIALIZER
-
-#define	rwlockattr_t		pthread_rwlockattr_t
-
-#define	thread_key_t		pthread_key_t
-
-#define	thr_t			pthread_t
-
-#define	once_t			pthread_once_t
-#define	ONCE_INITIALIZER	PTHREAD_ONCE_INIT
-
-#ifndef __LIBC_THREAD_STUBS
-
-int	__libc_mutex_init(mutex_t *, const mutexattr_t *);
-int	__libc_mutex_lock(mutex_t *);
-int	__libc_mutex_trylock(mutex_t *);
-int	__libc_mutex_unlock(mutex_t *);
-int	__libc_mutex_destroy(mutex_t *);
-
-#define	mutex_init(m, a)	__libc_mutex_init((m), (a))
-#define	mutex_lock(m)		__libc_mutex_lock((m))
-#define	mutex_trylock(m)	__libc_mutex_trylock((m))
-#define	mutex_unlock(m)		__libc_mutex_unlock((m))
-#define	mutex_destroy(m)	__libc_mutex_destroy((m))
-
-int	__libc_cond_init(cond_t *, const condattr_t *);
-int	__libc_cond_signal(cond_t *);
-int	__libc_cond_broadcast(cond_t *);
-int	__libc_cond_wait(cond_t *, mutex_t *);
-int	__libc_cond_timedwait(cond_t *, mutex_t *, const struct timespec *);
-int	__libc_cond_destroy(cond_t *);
-
-#define	cond_init(c, t, a)     	__libc_cond_init((c), (a))
-#define	cond_signal(c)		__libc_cond_signal((c))
-#define	cond_broadcast(c)	__libc_cond_broadcast((c))
-#define	cond_wait(c, m)		__libc_cond_wait((c), (m))
-#define	cond_timedwait(c, m, t)	__libc_cond_timedwait((c), (m), (t))
-#define	cond_destroy(c)		__libc_cond_destroy((c))
-
-int	__libc_rwlock_init(rwlock_t *, const rwlockattr_t *);
-int	__libc_rwlock_rdlock(rwlock_t *);
-int	__libc_rwlock_wrlock(rwlock_t *);
-int	__libc_rwlock_tryrdlock(rwlock_t *);
-int	__libc_rwlock_trywrlock(rwlock_t *);
-int	__libc_rwlock_unlock(rwlock_t *);
-int	__libc_rwlock_destroy(rwlock_t *);
-
-#define	rwlock_init(l, a)	__libc_rwlock_init((l), (a))
-#define	rwlock_rdlock(l)	__libc_rwlock_rdlock((l))
-#define	rwlock_wrlock(l)	__libc_rwlock_wrlock((l))
-#define	rwlock_tryrdlock(l)	__libc_rwlock_tryrdlock((l))
-#define	rwlock_trywrlock(l)	__libc_rwlock_trywrlock((l))
-#define	rwlock_unlock(l)	__libc_rwlock_unlock((l))
-#define	rwlock_destroy(l)	__libc_rwlock_destroy((l))
-
-int	__libc_thr_keycreate(thread_key_t *, void (*)(void *));
-int	__libc_thr_setspecific(thread_key_t, const void *);
-void	*__libc_thr_getspecific(thread_key_t);
-int	__libc_thr_keydelete(thread_key_t);
-
-#define	thr_keycreate(k, d)	__libc_thr_keycreate((k), (d))
-#define	thr_setspecific(k, p)	__libc_thr_setspecific((k), (p))
-#define	thr_getspecific(k)	__libc_thr_getspecific((k))
-#define	thr_keydelete(k)	__libc_thr_keydelete((k))
-
-int	__libc_thr_once(once_t *, void (*)(void));
-int	__libc_thr_sigsetmask(int, const sigset_t *, sigset_t *);
-thr_t	__libc_thr_self(void);
-int	*__libc_thr_errno(void);
-
-#define	thr_once(o, f)		__libc_thr_once((o), (f))
-#define	thr_sigsetmask(f, n, o)	__libc_thr_sigsetmask((f), (n), (o))
-#define	thr_self()		__libc_thr_self()
-#define	thr_errno()		__libc_thr_errno()
+#include <threadlib.h>
 
 #define	FLOCKFILE(fp)		flockfile(fp)
 #define	FUNLOCKFILE(fp)		funlockfile(fp)
-
-#endif /* __LIBC_THREAD_STUBS */
 
 #else /* _REENTRANT */
 
