c32fa4baa0c170ec40aa9a95d2560c1b7eaaae39
scw <scw@NetBSD.org>
2003-01-19 23:05:01 +0000

    SH5 libc ucontext/lwp support. Untested for now, until I get the libpthread stuff done and an SA userland built.

--- lib/libc/arch/sh5/gen/_lwp.c
+++ lib/libc/arch/sh5/gen/_lwp.c
@@ -1,11 +1,11 @@
-/*	$NetBSD: __sigaction14_sigtramp.c,v 1.3 2003/01/19 23:05:04 scw Exp $	*/
+/*	$NetBSD: _lwp.c,v 1.1 2003/01/19 23:05:01 scw Exp $	*/
 
 /*-
- * Copyright (c) 2002 The NetBSD Foundation, Inc.
+ * Copyright (c) 2001 The NetBSD Foundation, Inc.
  * All rights reserved.
  *
  * This code is derived from software contributed to The NetBSD Foundation
- * by Jason R. Thorpe.
+ * by Nathan J. Williams and Steve C. Woodford.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -17,8 +17,8 @@
  *    documentation and/or other materials provided with the distribution.
  * 3. All advertising materials mentioning features or use of this software
  *    must display the following acknowledgement:
- *	This product includes software developed by the NetBSD
- *	Foundation, Inc. and its contributors.
+ *        This product includes software developed by the NetBSD
+ *        Foundation, Inc. and its contributors.
  * 4. Neither the name of The NetBSD Foundation nor the names of its
  *    contributors may be used to endorse or promote products derived
  *    from this software without specific prior written permission.
@@ -36,25 +36,55 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
-#define	__LIBC12_SOURCE__
-
 #include <sys/types.h>
-#include <signal.h>
+#include <ucontext.h>
+#include <lwp.h>
+#include <stdlib.h>
 
-#include "extern.h"
-
-__weak_alias(__sigaction14, __libc_sigaction14)
-
-int
-__libc_sigaction14(int sig, const struct sigaction *act, struct sigaction *oact)
+void
+_lwp_makecontext(ucontext_t *u, void (*start)(void *),
+    void *arg, void *private, caddr_t stack_base, size_t stack_size)
 {
-	extern void __sigtramp_sigcontext_1(void);
+	__greg_t *gr;
+
+	getcontext(u);
+	gr = u->uc_mcontext.__gregs;
+
+	u->uc_link = NULL;
+
+	u->uc_stack.ss_sp = stack_base;
+	u->uc_stack.ss_size = stack_size;
+
+	gr[_REG_PC] = (register_t)(intptr_t)start;
+	gr[_REG_R(18)] = (register_t)(intptr_t)_lwp_exit;
+	gr[_REG_R(2)] = (register_t)(intptr_t)arg;
+	gr[_REG_SP] = (register_t)((stack_base + stack_size) & ~0x7);
+	gr[_REG_FP] = 0;
+	gr[_REG_USR] = 0x000f;	/* r0-r31 are dirty */
+	gr[_REG_R(24)] = 0x12345678ACEBABE5ULL;	/* magic number */
 
 	/*
-	 * Right here we should select the SA_SIGINFO trampoline
-	 * if SA_SIGINFO is set in the sigaction.
+	 * The new context inherits the global variable/constant pointers
+	 * in r26 and r27.
+	 *
+	 * XXXSCW: I've never actually seen these used anywhere, so this
+	 * may well be unnecessary...
 	 */
+	__asm __volatile("or r26, r63, %0" : "=r"(r));
+	gr[_REG_R(26)] = r;
+	__asm __volatile("or r27, r63, %0" : "=r"(r));
+	gr[_REG_R(27)] = r;
 
-	return (__sigaction_sigtramp(sig, act, oact,
-				     (void *)__sigtramp_sigcontext_1, 1));
+	/*
+	 * Ensure the branch target registers contain sane values
+	 * or else the kernel will refuse to restore the context.
+	 */
+	gr[_REG_TR(0)] = 0;
+	gr[_REG_TR(1)] = 0;
+	gr[_REG_TR(2)] = 0;
+	gr[_REG_TR(3)] = 0;
+	gr[_REG_TR(4)] = 0;
+	gr[_REG_TR(5)] = 0;
+	gr[_REG_TR(6)] = 0;
+	gr[_REG_TR(7)] = 0;
 }
