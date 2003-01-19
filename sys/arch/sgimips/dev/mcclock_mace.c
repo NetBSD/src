69b5daad22c8326f117eeb73d72afea13a3227a5
rafal <rafal@NetBSD.org>
2003-01-19 22:26:38 +0000

    Update to use the full DS1687 register set, therefore allowing the clock to store absolute year rather than an offset -- this means the clock is now consitent across the ARCS PROM, IRIX and NetBSD.
    XXX: This attachment is now a mismoner, since it's a Dallas Semi RTC, not
    a Motorola RTC.  Should be renamed.

--- sys/arch/sgimips/dev/mcclock_mace.c
+++ sys/arch/sgimips/dev/mcclock_mace.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: mcclock_mace.c,v 1.7 2002/10/02 15:52:33 thorpej Exp $	*/
+/*	$NetBSD: mcclock_mace.c,v 1.8 2003/01/19 22:26:38 rafal Exp $	*/
 
 /*
  * Copyright (c) 2001 Antti Kantee.  All Rights Reserved.
@@ -77,17 +77,11 @@
 #include <machine/machtype.h>
 
 #include <dev/clock_subr.h>
-#include <dev/ic/mc146818reg.h>
+#include <dev/ic/ds1687reg.h>
 
 #include <sgimips/dev/macevar.h>
 
 #include <sgimips/sgimips/clockvar.h>
-
-/*
- * Following register acts different in MC146818A, that's why it's
- * not found in mc146818reg.h, and we must define it separately.
- */
-#define MC_REGA_DV1	0x20
 
 struct mcclock_mace_softc {
 	struct device		sc_dev;
@@ -103,8 +97,8 @@ static void	mcclock_mace_init(struct device *);
 static void	mcclock_mace_get(struct device *, struct clock_ymdhms *);
 static void	mcclock_mace_set(struct device *, struct clock_ymdhms *);
 
-unsigned int	mc146818_read(void *arg, unsigned int addr);
-void		mc146818_write(void *arg, unsigned int addr, unsigned int data);
+unsigned int	ds1687_read(void *arg, unsigned int addr);
+void		ds1687_write(void *arg, unsigned int addr, unsigned int data);
 
 const struct clockfns mcclock_mace_clockfns = {
 	mcclock_mace_init, mcclock_mace_get, mcclock_mace_set
@@ -128,21 +122,30 @@ mcclock_mace_attach(struct device *parent, struct device *self, void *aux)
 	sc->sc_st = maa->maa_st;
 	sc->sc_sh = maa->maa_sh;
 
-	mc146818_write(sc, MC_REGA, MC_REGA_DV1);
-	mc146818_write(sc, MC_REGB, MC_REGB_BINARY | MC_REGB_24HR);
-	if (!(mc146818_read(sc, MC_REGD) & MC_REGD_VRT))
-		printf(": lithium cell is dead, RTC unreliable");
-	printf("\n");
+	/* 
+ 	 * We want a fixed format: 24-hour, BCD data, so just force the
+ 	 * RTC to this mode; if there was junk there we'll be able to
+ 	 * fix things up later when we discover the time read back is
+	 * no good.
+	 */
+	ds1687_write(sc, DS1687_CONTROLA, DS1687_DV1 | DS1687_BANK1);
+	ds1687_write(sc, DS1687_CONTROLB, DS1687_24HRS);
 
+	/* XXXrkb: init kickstart/wakeup stuff */
+
+	if (!(ds1687_read(sc, DS1687_CONTROLD) & DS1687_VRT))
+		printf(": lithium cell is dead, RTC unreliable");
+
+	printf("\n");
 	clockattach(&sc->sc_dev, &mcclock_mace_clockfns);
 }
 
 /*
  * We need to use the following two functions from
- * MC146818_PUT/GETTOD(). Hence the names.
+ * DS1687_PUT/GETTOD(). Hence the names.
  */
 unsigned int
-mc146818_read(void *arg, unsigned int addr)
+ds1687_read(void *arg, unsigned int addr)
 {
 	struct mcclock_mace_softc *sc = (struct mcclock_mace_softc *)arg;
 
@@ -150,7 +153,7 @@ mc146818_read(void *arg, unsigned int addr)
 }
 
 void
-mc146818_write(void *arg, unsigned int addr, unsigned int data)
+ds1687_write(void *arg, unsigned int addr, unsigned int data)
 {
 	struct mcclock_mace_softc *sc = (struct mcclock_mace_softc *)arg;
 
@@ -167,40 +170,41 @@ static void
 mcclock_mace_get(struct device *dev, struct clock_ymdhms *dt)
 {
 	struct mcclock_mace_softc *sc = (struct mcclock_mace_softc *)dev;
-	mc_todregs regs;
+	ds1687_todregs regs;
 	int s;
 
 	s = splhigh();
-	MC146818_GETTOD(sc, &regs);
+	DS1687_GETTOD(sc, &regs);
 	splx(s);
 
-	dt->dt_sec = FROMBCD(regs[MC_SEC]);
-	dt->dt_min = FROMBCD(regs[MC_MIN]);
-	dt->dt_hour = FROMBCD(regs[MC_HOUR]);
-	dt->dt_wday = FROMBCD(regs[MC_DOW]);
-	dt->dt_day = FROMBCD(regs[MC_DOM]);
-	dt->dt_mon = FROMBCD(regs[MC_MONTH]);
-	dt->dt_year = FROM_IRIX_YEAR(FROMBCD(regs[MC_YEAR]));
+	dt->dt_sec = FROMBCD(regs[DS1687_SOFT_SEC]);
+	dt->dt_min = FROMBCD(regs[DS1687_SOFT_MIN]);
+	dt->dt_hour = FROMBCD(regs[DS1687_SOFT_HOUR]);
+	dt->dt_wday = FROMBCD(regs[DS1687_SOFT_DOW]);
+	dt->dt_day = FROMBCD(regs[DS1687_SOFT_DOM]);
+	dt->dt_mon = FROMBCD(regs[DS1687_SOFT_MONTH]);
+	dt->dt_year = FROMBCD(regs[DS1687_SOFT_YEAR]) + 
+			(100 * FROMBCD(regs[DS1687_SOFT_CENTURY]));
 }
 
 static void
 mcclock_mace_set(struct device *dev, struct clock_ymdhms *dt)
 {
 	struct mcclock_mace_softc *sc = (struct mcclock_mace_softc *)dev;
-	mc_todregs regs;
+	ds1687_todregs regs;
 	int s;
 
 	memset(&regs, 0, sizeof(regs));
 
-	regs[MC_SEC] = TOBCD(dt->dt_sec);
-	regs[MC_MIN] = TOBCD(dt->dt_min);
-	regs[MC_HOUR] = TOBCD(dt->dt_hour);
-	regs[MC_DOW] = TOBCD(dt->dt_wday);
-	regs[MC_DOM] = TOBCD(dt->dt_day);
-	regs[MC_MONTH] = TOBCD(dt->dt_mon);
-	regs[MC_YEAR] = TOBCD(TO_IRIX_YEAR(dt->dt_year));
-
+	regs[DS1687_SOFT_SEC] = TOBCD(dt->dt_sec);
+	regs[DS1687_SOFT_MIN] = TOBCD(dt->dt_min);
+	regs[DS1687_SOFT_HOUR] = TOBCD(dt->dt_hour);
+	regs[DS1687_SOFT_DOW] = TOBCD(dt->dt_wday);
+	regs[DS1687_SOFT_DOM] = TOBCD(dt->dt_day);
+	regs[DS1687_SOFT_MONTH] = TOBCD(dt->dt_mon);
+	regs[DS1687_SOFT_YEAR] = TOBCD(dt->dt_year % 100);
+	regs[DS1687_SOFT_CENTURY] = TOBCD(dt->dt_year / 100);
 	s = splhigh();
-	MC146818_PUTTOD(sc, &regs);
+	DS1687_PUTTOD(sc, &regs);
 	splx(s);
 }
