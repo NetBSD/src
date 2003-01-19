2992fd0e9c4aedb830b7ea8cc1601cb3d80c6d1e
matt <matt>
2003-01-19 09:23:39 +0000

    Make this reflect reality.

--- sys/arch/powerpc/include/signal.h
+++ sys/arch/powerpc/include/signal.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: signal.h,v 1.7 2003/01/18 06:23:29 thorpej Exp $	*/
+/*	$NetBSD: signal.h,v 1.8 2003/01/19 09:23:39 matt Exp $	*/
 
 /*
  * Copyright (C) 1995, 1996 Wolfgang Solfrank.
@@ -62,14 +62,14 @@ struct sigcontext {
  */
 #define	_MCONTEXT_TO_SIGCONTEXT(uc, sc)					\
 do {									\
-	memcpy((sc)->sc_frame.fixreg, &(uc)->uc_mcontext.__gregs.__r_r0,\
+	memcpy((sc)->sc_frame.fixreg, &(uc)->uc_mcontext.__gregs[0],	\
 	    sizeof((sc)->sc_frame.fixreg));				\
-	(sc)->sc_frame.lr     = (uc)->uc_mcontext.__gregs.__r_lr;	\
-	(sc)->sc_frame.cr     = (uc)->uc_mcontext.__gregs.__r_cr;	\
-	(sc)->sc_frame.xer    = (uc)->uc_mcontext.__gregs.__r_xer;	\
-	(sc)->sc_frame.ctr    = (uc)->uc_mcontext.__gregs.__r_ctr;	\
-	(sc)->sc_frame.srr0   = (uc)->uc_mcontext.__gregs.__r_pc;	\
-	(sc)->sc_frame.srr1   = (uc)->uc_mcontext.__gregs.__r_msr;	\
+	(sc)->sc_frame.cr     = (uc)->uc_mcontext.__gregs[32];		\
+	(sc)->sc_frame.lr     = (uc)->uc_mcontext.__gregs[33];		\
+	(sc)->sc_frame.srr0   = (uc)->uc_mcontext.__gregs[34];		\
+	(sc)->sc_frame.srr1   = (uc)->uc_mcontext.__gregs[35];		\
+	(sc)->sc_frame.xer    = (uc)->uc_mcontext.__gregs[36];		\
+	(sc)->sc_frame.ctr    = (uc)->uc_mcontext.__gregs[37];		\
 	(sc)->sc_frame.dar    = 0;					\
 	(sc)->sc_frame.dsisr  = 0;					\
 	(sc)->sc_frame.exc    = 0;					\
@@ -78,14 +78,14 @@ do {									\
 
 #define	_SIGCONTEXT_TO_MCONTEXT(sc, uc)					\
 do {									\
-	memcpy(&(uc)->uc_mcontext.__gregs.__r_r0, (sc)->sc_frame.fixreg,\
+	memcpy(&(uc)->uc_mcontext.__gregs[0], (sc)->sc_frame.fixreg,	\
 	    sizeof((sc)->sc_frame.fixreg));				\
-	(uc)->uc_mcontext.__gregs.__r_lr  = (sc)->sc_frame.lr;		\
-	(uc)->uc_mcontext.__gregs.__r_cr  = (sc)->sc_frame.cr;		\
-	(uc)->uc_mcontext.__gregs.__r_xer = (sc)->sc_frame.xer;		\
-	(uc)->uc_mcontext.__gregs.__r_ctr = (sc)->sc_frame.ctr;		\
-	(uc)->uc_mcontext.__gregs.__r_pc  = (sc)->sc_frame.srr0;	\
-	(uc)->uc_mcontext.__gregs.__r_msr = (sc)->sc_frame.srr1;	\
+	(uc)->uc_mcontext.__gregs[32] = (sc)->sc_frame.cr;		\
+	(uc)->uc_mcontext.__gregs[33] = (sc)->sc_frame.lr;		\
+	(uc)->uc_mcontext.__gregs[34] = (sc)->sc_frame.srr0;		\
+	(uc)->uc_mcontext.__gregs[35] = (sc)->sc_frame.srr1;		\
+	(uc)->uc_mcontext.__gregs[36] = (sc)->sc_frame.ctr;		\
+	(uc)->uc_mcontext.__gregs[37] = (sc)->sc_frame.xer;		\
 	(uc)->uc_mcontext.__vrf.__vrsave  = (sc)->sc_frame.vrsave;	\
 } while (/*CONSTCOND*/0)
 
