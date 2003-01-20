9971e90eb62d78ec9aa41a4a4de697d0fcf72b9f
aymeric <aymeric@NetBSD.org>
2003-01-20 16:13:26 +0000

    bump version. ":cs add" works when using a database file in the current directory, whichever way we specify its name.

--- usr.bin/vi/ex/version.h
+++ usr.bin/vi/ex/version.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: version.h,v 1.10 2002/04/15 08:37:04 pooka Exp $	*/
+/*	$NetBSD: version.h,v 1.11 2003/01/20 16:13:26 aymeric Exp $	*/
 
 #define	VI_VERSION \
-    "Version 1.79nb7 (10/23/96) The CSRG, University of California, Berkeley."
+    "Version 1.79nb8 (10/23/96) The CSRG, University of California, Berkeley."
