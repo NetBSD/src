17e180f16a9a5758b6e7504986a8147df15384bf
wiz <wiz@NetBSD.org>
2003-01-26 00:43:02 +0000

    Import of diffutils-2.8.1 (converted with to-be-committed diffutils2netbsd script).

---
+++ gnu/dist/diffutils/lib/offtostr.c
@@ -0,0 +1,5 @@
+/*	$NetBSD: offtostr.c,v 1.1.1.1 2003/01/26 00:43:15 wiz Exp $	*/
+
+#define inttostr offtostr
+#define inttype off_t
+#include "inttostr.c"
---
+++ gnu/dist/diffutils/lib/posixver.h
@@ -0,0 +1,3 @@
+/*	$NetBSD: posixver.h,v 1.1.1.1 2003/01/26 00:43:14 wiz Exp $	*/
+
+int posix2_version (void);
