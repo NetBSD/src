27ab2abac87ac59518f7b4c32942854809e2f576
fvdl <fvdl@NetBSD.org>
2003-01-26 14:12:10 +0000

    Rearrange the trapframe and mcontext to put the registers used for argument passing to be in front, in order. Define constants for offsets inside frames, to avoid having to redo asm code should they change again.

--- sys/arch/x86_64/include/frameasm.h
+++ sys/arch/x86_64/include/frameasm.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: frameasm.h,v 1.5 2003/01/26 00:05:37 fvdl Exp $	*/
+/*	$NetBSD: frameasm.h,v 1.6 2003/01/26 14:12:10 fvdl Exp $	*/
 
 #ifndef _X86_64_MACHINE_FRAMEASM_H
 #define _X86_64_MACHINE_FRAMEASM_H
@@ -13,38 +13,38 @@
  */
 #define INTR_SAVE_GPRS \
 	subq	$120,%rsp	; \
-	movq	%r15,0(%rsp)	; \
-	movq	%r14,8(%rsp)	; \
-	movq	%r13,16(%rsp)	; \
-	movq	%r12,24(%rsp)	; \
-	movq	%r11,32(%rsp)	; \
-	movq	%r10,40(%rsp)	; \
-	movq	%r9,48(%rsp)	; \
-	movq	%r8,56(%rsp)	; \
-	movq	%rdi,64(%rsp)	; \
-	movq	%rsi,72(%rsp)	; \
-	movq	%rbp,80(%rsp)	; \
-	movq	%rbx,88(%rsp)	; \
-	movq	%rdx,96(%rsp)	; \
-	movq	%rcx,104(%rsp)	; \
-	movq	%rax,112(%rsp)
+	movq	%r15,TF_R15(%rsp)	; \
+	movq	%r14,TF_R14(%rsp)	; \
+	movq	%r13,TF_R13(%rsp)	; \
+	movq	%r12,TF_R12(%rsp)	; \
+	movq	%r11,TF_R11(%rsp)	; \
+	movq	%r10,TF_R10(%rsp)	; \
+	movq	%r9,TF_R9(%rsp)		; \
+	movq	%r8,TF_R8(%rsp)		; \
+	movq	%rdi,TF_RDI(%rsp)	; \
+	movq	%rsi,TF_RSI(%rsp)	; \
+	movq	%rbp,TF_RBP(%rsp)	; \
+	movq	%rbx,TF_RBX(%rsp)	; \
+	movq	%rdx,TF_RDX(%rsp)	; \
+	movq	%rcx,TF_RCX(%rsp)	; \
+	movq	%rax,TF_RAX(%rsp)
 
 #define	INTR_RESTORE_GPRS \
-	movq	0(%rsp),%r15	; \
-	movq	8(%rsp),%r14	; \
-	movq	16(%rsp),%r13	; \
-	movq	24(%rsp),%r12	; \
-	movq	32(%rsp),%r11	; \
-	movq	40(%rsp),%r10	; \
-	movq	48(%rsp),%r9	; \
-	movq	56(%rsp),%r8	; \
-	movq	64(%rsp),%rdi	; \
-	movq	72(%rsp),%rsi	; \
-	movq	80(%rsp),%rbp	; \
-	movq	88(%rsp),%rbx	; \
-	movq	96(%rsp),%rdx	; \
-	movq	104(%rsp),%rcx	; \
-	movq	112(%rsp),%rax	; \
+	movq	TF_R15(%rsp),%r15	; \
+	movq	TF_R14(%rsp),%r14	; \
+	movq	TF_R13(%rsp),%r13	; \
+	movq	TF_R12(%rsp),%r12	; \
+	movq	TF_R11(%rsp),%r11	; \
+	movq	TF_R10(%rsp),%r10	; \
+	movq	TF_R9(%rsp),%r9		; \
+	movq	TF_R8(%rsp),%r8		; \
+	movq	TF_RDI(%rsp),%rdi	; \
+	movq	TF_RSI(%rsp),%rsi	; \
+	movq	TF_RBP(%rsp),%rbp	; \
+	movq	TF_RBX(%rsp),%rbx	; \
+	movq	TF_RDX(%rsp),%rdx	; \
+	movq	TF_RCX(%rsp),%rcx	; \
+	movq	TF_RAX(%rsp),%rax	; \
 	addq	$120,%rsp
 
 #define	INTRENTRY \
@@ -54,8 +54,8 @@
 	swapgs				; \
 	movw	%gs,0(%rsp)		; \
 	movw	%fs,8(%rsp)		; \
-	movw	%ds,16(%rsp)		; \
-	movw	%es,24(%rsp)		; \
+	movw	%es,16(%rsp)		; \
+	movw	%ds,24(%rsp)		; \
 98: 	INTR_SAVE_GPRS
 
 #define INTRFASTEXIT \
@@ -66,8 +66,8 @@
 	swapgs				; \
 	movw	0(%rsp),%gs		; \
 	movw	8(%rsp),%fs		; \
-	movw	16(%rsp),%ds		; \
-	movw	24(%rsp),%es		; \
+	movw	16(%rsp),%es		; \
+	movw	24(%rsp),%ds		; \
 99:	addq	$48,%rsp		; \
 	iretq
 
