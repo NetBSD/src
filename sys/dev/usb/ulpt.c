1f564c54604542bc73d761a4afc6abc3614b9538
jmc <jmc>
2003-01-27 06:02:35 +0000

    Pullup revisions 1.50-1.51 (requested by bad in ticket #1105)  Move a quirk tests so the message printed about directionality is right.

--- sys/dev/usb/ulpt.c
+++ sys/dev/usb/ulpt.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: ulpt.c,v 1.49 2002/02/25 22:39:01 augustss Exp $	*/
+/*	$NetBSD: ulpt.c,v 1.49.10.1 2003/01/27 06:02:35 jmc Exp $	*/
 /*	$FreeBSD: src/sys/dev/usb/ulpt.c,v 1.24 1999/11/17 22:33:44 n_hibma Exp $	*/
 
 /*
@@ -43,7 +43,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: ulpt.c,v 1.49 2002/02/25 22:39:01 augustss Exp $");
+__KERNEL_RCSID(0, "$NetBSD: ulpt.c,v 1.49.10.1 2003/01/27 06:02:35 jmc Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -276,18 +276,19 @@ USB_ATTACH(ulpt)
 		}
 	}
 	if (sc->sc_out == -1) {
-		printf("%s: could not find bulk endpoint\n",
+		printf("%s: could not find bulk out endpoint\n",
 		    USBDEVNAME(sc->sc_dev));
 		sc->sc_dying = 1;
 		USB_ATTACH_ERROR_RETURN;
 	}
-	printf("%s: using %s-directional mode\n", USBDEVNAME(sc->sc_dev),
-	       sc->sc_in >= 0 ? "bi" : "uni");
 
 	if (usbd_get_quirks(dev)->uq_flags & UQ_BROKEN_BIDIR) {
 		/* This device doesn't handle reading properly. */
 		sc->sc_in = -1;
 	}
+
+	printf("%s: using %s-directional mode\n", USBDEVNAME(sc->sc_dev),
+	       sc->sc_in >= 0 ? "bi" : "uni");
 
 	DPRINTFN(10, ("ulpt_attach: bulk=%d\n", sc->sc_out));
 
