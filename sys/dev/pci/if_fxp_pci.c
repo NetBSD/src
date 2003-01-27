0afbe2441914e64f66802725e4c459c6a699bf16
jmc <jmc@NetBSD.org>
2003-01-27 06:26:57 +0000

    Pullup revisions 1.23-1.24 (requested by masanobu in ticket #1110)  Add support intel C-ICH's embeded Ether MACs.

--- sys/dev/pci/if_fxp_pci.c
+++ sys/dev/pci/if_fxp_pci.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: if_fxp_pci.c,v 1.22.4.3 2002/10/13 22:29:23 lukem Exp $	*/
+/*	$NetBSD: if_fxp_pci.c,v 1.22.4.4 2003/01/27 06:26:57 jmc Exp $	*/
 
 /*-
  * Copyright (c) 1997, 1998, 1999, 2000, 2001 The NetBSD Foundation, Inc.
@@ -43,7 +43,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: if_fxp_pci.c,v 1.22.4.3 2002/10/13 22:29:23 lukem Exp $");
+__KERNEL_RCSID(0, "$NetBSD: if_fxp_pci.c,v 1.22.4.4 2003/01/27 06:26:57 jmc Exp $");
 
 #include "rnd.h"
 
@@ -117,6 +117,10 @@ const struct fxp_pci_product {
 	  "Intel InBusiness Ethernet" },
 	{ PCI_PRODUCT_INTEL_82801BA_LAN,
 	  "Intel i82562 Ethernet" },
+	{ PCI_PRODUCT_INTEL_82801E_LAN_1,
+	  "Intel i82559 Ethernet" },
+	{ PCI_PRODUCT_INTEL_82801E_LAN_2,
+	  "Intel i82559 Ethernet" },
 	{ PCI_PRODUCT_INTEL_PRO_100_VE_0,
 	  "Intel PRO/100 VE Network Controller" },
 	{ PCI_PRODUCT_INTEL_PRO_100_VE_1,
@@ -375,6 +379,15 @@ fxp_pci_attach(parent, self, aux)
 		 */
 		sc->sc_rev = 1;
 		sc->sc_flags |= FXPF_HAS_RESUME_BUG;
+		break;
+	case PCI_PRODUCT_INTEL_82801E_LAN_1:
+	case PCI_PRODUCT_INTEL_82801E_LAN_2:
+		printf(": %s, rev %d\n", fpp->fpp_name, sc->sc_rev);
+
+		/*
+		 *  XXX We have to read the C-ICH's developer's manual
+		 *  in detail
+		 */
 		break;
 	}
 
