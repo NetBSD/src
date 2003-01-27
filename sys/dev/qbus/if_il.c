d6b7a83983485279f62d9449a158e0b3dac81542
jmc <jmc@NetBSD.org>
2003-01-27 05:11:26 +0000

    Pullup revisions 1.8-1.9 (requested by bouyer in ticket #1094)  This chip should autopad, so just remove the check.

--- sys/dev/qbus/if_il.c
+++ sys/dev/qbus/if_il.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: if_il.c,v 1.4 2002/03/05 04:12:59 itojun Exp $	*/
+/*	$NetBSD: if_il.c,v 1.4.8.1 2003/01/27 05:11:26 jmc Exp $	*/
 /*
  * Copyright (c) 1982, 1986 Regents of the University of California.
  * All rights reserved.
@@ -39,7 +39,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: if_il.c,v 1.4 2002/03/05 04:12:59 itojun Exp $");
+__KERNEL_RCSID(0, "$NetBSD: if_il.c,v 1.4.8.1 2003/01/27 05:11:26 jmc Exp $");
 
 #include "opt_inet.h"
 #include "opt_ns.h"
@@ -427,15 +427,6 @@ ilstart(struct ifnet *ifp)
 		goto startcmd;
 	}
 	len = if_wubaput(&sc->sc_ifuba, m);
-	/*
-	 * Ensure minimum packet length.
-	 * This makes the safe assumtion that there are no virtual holes
-	 * after the data.
-	 * For security, it might be wise to zero out the added bytes,
-	 * but we're mainly interested in speed at the moment.
-	 */
-	if (len - sizeof(struct ether_header) < ETHERMIN)
-		len = ETHERMIN + sizeof(struct ether_header);
 #ifdef notdef
 	if (sc->sc_ifuba.ifu_flags & UBA_NEEDBDP)
 		UBAPURGE(is->is_ifuba.ifu_uba, is->is_ifuba.ifu_w.ifrw_bdp);
