00bb791c5a30f447a1f8a4190692e25b221bf4db
jmc <jmc>
2003-01-28 08:45:16 +0000

    Update to 1.6.1_RC1

--- sys/conf/osrelease.sh
+++ sys/conf/osrelease.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-#	$NetBSD: osrelease.sh,v 1.75.6.11 2002/09/30 11:02:39 lukem Exp $
+#	$NetBSD: osrelease.sh,v 1.75.6.12 2003/01/28 08:45:16 jmc Exp $
 #
 # Copyright (c) 1997 The NetBSD Foundation, Inc.
 # All rights reserved.
@@ -42,7 +42,7 @@
 #	sys/sys/param.h:	__NetBSD_Version__
 #	share/tmac/doc-common:	ds oS
 #
-release=1.6_STABLE
+release=1.6.1_RC1
 
 case $1 in
 -s)
