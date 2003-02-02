52152d618c911ddbcb698392bbaf1d8970216122
wiz <wiz@NetBSD.org>
2003-02-02 10:21:13 +0000

    Fix various spelling, grammar, and punctuation problems in comments, from Sergey Svishchev in PR 20164.

--- bin/pax/tables.c
+++ bin/pax/tables.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: tables.c,v 1.19 2003/01/06 20:30:29 wiz Exp $	*/
+/*	$NetBSD: tables.c,v 1.20 2003/02/02 10:21:15 wiz Exp $	*/
 
 /*-
  * Copyright (c) 1992 Keith Muller.
@@ -42,7 +42,7 @@
 #if 0
 static char sccsid[] = "@(#)tables.c	8.1 (Berkeley) 5/31/93";
 #else
-__RCSID("$NetBSD: tables.c,v 1.19 2003/01/06 20:30:29 wiz Exp $");
+__RCSID("$NetBSD: tables.c,v 1.20 2003/02/02 10:21:15 wiz Exp $");
 #endif
 #endif /* not lint */
 
@@ -73,7 +73,7 @@ __RCSID("$NetBSD: tables.c,v 1.19 2003/01/06 20:30:29 wiz Exp $");
  * large archives. These database routines carefully combine memory usage and
  * temporary file storage in ways which will not significantly impact runtime
  * performance while allowing the largest possible archives to be handled.
- * Trying to force the fit to the posix databases routines was not considered
+ * Trying to force the fit to the posix database routines was not considered
  * time well spent.
  */
 
@@ -317,14 +317,14 @@ lnk_end(void)
  * An append with an -u must read the archive and store the modification time
  * for every file on that archive before starting the write phase. It is clear
  * that this is one HUGE database. To save memory space, the actual file names
- * are stored in a scatch file and indexed by an in memory hash table. The
+ * are stored in a scratch file and indexed by an in-memory hash table. The
  * hash table is indexed by hashing the file path. The nodes in the table store
  * the length of the filename and the lseek offset within the scratch file
- * where the actual name is stored. Since there are never any deletions to this
+ * where the actual name is stored. Since there are never any deletions from this
  * table, fragmentation of the scratch file is never a issue. Lookups seem to
  * not exhibit any locality at all (files in the database are rarely
- * looked up more than once...). So caching is just a waste of memory. The
- * only limitation is the amount of scatch file space available to store the
+ * looked up more than once...), so caching is just a waste of memory. The
+ * only limitation is the amount of scratch file space available to store the
  * path names.
  */
 
@@ -890,7 +890,7 @@ map_dev(ARCHD *arcn, u_long dev_mask, u_long ino_mask)
  * directory access/mod time reset table routines (for directories READ by pax)
  *
  * The pax -t flag requires that access times of archive files to be the same
- * before being read by pax. For regular files, access time is restored after
+ * as before being read by pax. For regular files, access time is restored after
  * the file has been copied. This database provides the same functionality for
  * directories read during file tree traversal. Restoring directory access time
  * is more complex than files since directories may be read several times until
@@ -976,7 +976,7 @@ add_atdir(char *fname, dev_t dev, ino_t ino, time_t mtime, time_t atime)
 	 * return (the older entry always has the correct time). The only
 	 * way this will happen is when the same subtree can be traversed by
 	 * different args to pax and the -n option is aborting fts out of a
-	 * subtree before all the post-order visits have been made).
+	 * subtree before all the post-order visits have been made.
 	 */
 	indx = ((unsigned)ino) % A_TAB_SZ;
 	if ((pt = atab[indx]) != NULL) {
