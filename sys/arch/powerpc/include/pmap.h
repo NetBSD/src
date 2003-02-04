52263a8c8087033d16dbe14d001b97d9bfda1f20
matt <matt@NetBSD.org>
2003-02-04 01:31:49 +0000

    LKM's need a pmap_t typedef but there's no PPC_xxx to defined which pmap.h to include.  So just declare the pmap_t and don't do anything else.

--- sys/arch/powerpc/include/pmap.h
+++ sys/arch/powerpc/include/pmap.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: pmap.h,v 1.30 2003/02/03 17:10:01 matt Exp $	*/
+/*	$NetBSD: pmap.h,v 1.31 2003/02/04 01:31:49 matt Exp $	*/
 
 #ifdef _KERNEL_OPT
 #include "opt_ppcarch.h"
@@ -8,4 +8,6 @@
 #include <powerpc/ibm4xx/pmap.h>
 #elif defined(PPC_OEA)
 #include <powerpc/oea/pmap.h>
+#else
+typedef struct pmap *pmap_t;
 #endif
