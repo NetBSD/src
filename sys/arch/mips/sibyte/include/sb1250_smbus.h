8a6b8c3b72150fdb12c10e7534080664c135f24b
cgd <cgd@NetBSD.org>
2003-02-07 17:38:48 +0000

    Update to consistently use Broadcom GPL-compatible license on all SiByte code.

--- sys/arch/mips/sibyte/include/sb1250_smbus.h
+++ sys/arch/mips/sibyte/include/sb1250_smbus.h
@@ -12,7 +12,7 @@
     *  
     *********************************************************************  
     *
-    *  Copyright 2000,2001
+    *  Copyright 2000,2001,2002,2003
     *  Broadcom Corporation. All rights reserved.
     *  
     *  This software is furnished under license and may be used and 
@@ -23,24 +23,23 @@
     *  or ownership is transferred hereby.
     *  
     *  1) Any source code used, modified or distributed must reproduce 
-    *     and retain this copyright notice and list of conditions as 
-    *     they appear in the source file.
+    *     and retain this copyright notice and list of conditions 
+    *     as they appear in the source file.
     *  
     *  2) No right is granted to use any trade name, trademark, or 
-    *     logo of Broadcom Corporation. Neither the "Broadcom 
-    *     Corporation" name nor any trademark or logo of Broadcom 
-    *     Corporation may be used to endorse or promote products 
-    *     derived from this software without the prior written 
-    *     permission of Broadcom Corporation.
+    *     logo of Broadcom Corporation.  The "Broadcom Corporation" 
+    *     name may not be used to endorse or promote products derived 
+    *     from this software without the prior written permission of 
+    *     Broadcom Corporation.
     *  
     *  3) THIS SOFTWARE IS PROVIDED "AS-IS" AND ANY EXPRESS OR
-    *     IMPLIED WARRANTIES, INCLUDING BUT NOT LIMITED TO, ANY IMPLIED 
+    *     IMPLIED WARRANTIES, INCLUDING BUT NOT LIMITED TO, ANY IMPLIED
     *     WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR 
     *     PURPOSE, OR NON-INFRINGEMENT ARE DISCLAIMED. IN NO EVENT 
     *     SHALL BROADCOM BE LIABLE FOR ANY DAMAGES WHATSOEVER, AND IN 
-    *     PARTICULAR, BROADCOM SHALL NOT BE LIABLE FOR DIRECT, INDIRECT, 
+    *     PARTICULAR, BROADCOM SHALL NOT BE LIABLE FOR DIRECT, INDIRECT,
     *     INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES 
-    *     (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE 
+    *     (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
     *     GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
     *     BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY 
     *     OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR 
--- sys/arch/sbmips/include/leds.h
+++ sys/arch/sbmips/include/leds.h
@@ -1,4 +1,4 @@
-/* $NetBSD: leds.h,v 1.2 2002/03/17 06:28:56 simonb Exp $ */
+/* $NetBSD: leds.h,v 1.3 2003/02/07 17:46:12 cgd Exp $ */
 
 /*
  * Copyright 2000
@@ -15,10 +15,9 @@
  *    the source file.
  *
  * 2) No right is granted to use any trade name, trademark, or logo of
- *    Broadcom Corporation. Neither the "Broadcom Corporation" name nor any
- *    trademark or logo of Broadcom Corporation may be used to endorse or
- *    promote products derived from this software without the prior written
- *    permission of Broadcom Corporation.
+ *    Broadcom Corporation.  The "Broadcom Corporation" name may not be
+ *    used to endorse or promote products derived from this software
+ *    without the prior written permission of Broadcom Corporation.
  *
  * 3) THIS SOFTWARE IS PROVIDED "AS-IS" AND ANY EXPRESS OR IMPLIED
  *    WARRANTIES, INCLUDING BUT NOT LIMITED TO, ANY IMPLIED WARRANTIES OF
