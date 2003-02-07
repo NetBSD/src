96a8308b6ed845a9d35c34e40f18d31560d70ad9
tron <tron@NetBSD.org>
2003-02-07 18:14:54 +0000

    Pull up revision 1.49 (requested by martin in ticket #1152): don't access freed memory.

--- sys/net/if_spppsubr.c
+++ sys/net/if_spppsubr.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: if_spppsubr.c,v 1.46.4.7 2003/02/07 18:12:11 tron Exp $	 */
+/*	$NetBSD: if_spppsubr.c,v 1.46.4.8 2003/02/07 18:14:54 tron Exp $	 */
 
 /*
  * Synchronous PPP/Cisco link level subroutines.
@@ -28,7 +28,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: if_spppsubr.c,v 1.46.4.7 2003/02/07 18:12:11 tron Exp $");
+__KERNEL_RCSID(0, "$NetBSD: if_spppsubr.c,v 1.46.4.8 2003/02/07 18:14:54 tron Exp $");
 
 #include "opt_inet.h"
 #include "opt_ipx.h"
@@ -734,9 +734,11 @@ sppp_output(struct ifnet *ifp, struct mbuf *m,
 		 * - we flag TCP packets with src ip 0 as an error
 		 */	
 		if (ip && ip->ip_src.s_addr == INADDR_ANY) {
+			u_int8_t proto = ip->ip_p;
+
 			m_freem(m);
 			splx(s);
-			if (ip->ip_p == IPPROTO_TCP)
+			if (proto == IPPROTO_TCP)
 				return(EADDRNOTAVAIL);
 			else
 				return(0);
