05fa5b5c35586d92da45d6f702646a55542f8d7e
jmc <jmc@NetBSD.org>
2003-02-08 07:48:48 +0000

    Pullup rev 1.35 (requested by abs in ticket #1141)  Catchup to -current as of version 20030202.

--- usr.sbin/pkg_install/create/perform.c
+++ usr.sbin/pkg_install/create/perform.c
@@ -1,11 +1,11 @@
-/*	$NetBSD: perform.c,v 1.30.2.3 2002/11/24 22:33:56 tron Exp $	*/
+/*	$NetBSD: perform.c,v 1.30.2.4 2003/02/08 07:48:48 jmc Exp $	*/
 
 #include <sys/cdefs.h>
 #ifndef lint
 #if 0
 static const char *rcsid = "from FreeBSD Id: perform.c,v 1.38 1997/10/13 15:03:51 jkh Exp";
 #else
-__RCSID("$NetBSD: perform.c,v 1.30.2.3 2002/11/24 22:33:56 tron Exp $");
+__RCSID("$NetBSD: perform.c,v 1.30.2.4 2003/02/08 07:48:48 jmc Exp $");
 #endif
 #endif
 
@@ -137,6 +137,9 @@ make_dist(const char *home, const char *pkg, const char *suffix, const package_t
 	}
 	if (SizeAll) {
 		(void) fprintf(totar, "%s\n", SIZE_ALL_FNAME);
+	}
+	if (Preserve) {
+		(void) fprintf(totar, "%s\n", PRESERVE_FNAME);
 	}
 
 	for (p = plist->head; p; p = p->next) {
@@ -381,6 +384,11 @@ pkg_perform(lpkg_head_t *pkgs)
 		copy_file(Home, SizeAll, SIZE_ALL_FNAME);
 		add_plist(&plist, PLIST_IGNORE, NULL);
 		add_plist(&plist, PLIST_FILE, SIZE_ALL_FNAME);
+	}
+	if (Preserve) {
+		copy_file(Home, Preserve, PRESERVE_FNAME);
+		add_plist(&plist, PLIST_IGNORE, NULL);
+		add_plist(&plist, PLIST_FILE, PRESERVE_FNAME);
 	}
 
 	/* Finally, write out the packing list */
