d4f12ba1c3ce1d468f419db42f60f906a9a20306
jmc <jmc>
2003-02-08 07:53:36 +0000

    Pullup rev 1.23 (requested by abs in ticket #1141)  Catchup to -current as of version 20030202.

--- usr.sbin/pkg_install/lib/pen.c
+++ usr.sbin/pkg_install/lib/pen.c
@@ -1,11 +1,11 @@
-/*	$NetBSD: pen.c,v 1.19.2.2 2002/07/21 05:00:32 lukem Exp $	*/
+/*	$NetBSD: pen.c,v 1.19.2.3 2003/02/08 07:53:36 jmc Exp $	*/
 
 #include <sys/cdefs.h>
 #ifndef lint
 #if 0
 static const char *rcsid = "from FreeBSD Id: pen.c,v 1.25 1997/10/08 07:48:12 charnier Exp";
 #else
-__RCSID("$NetBSD: pen.c,v 1.19.2.2 2002/07/21 05:00:32 lukem Exp $");
+__RCSID("$NetBSD: pen.c,v 1.19.2.3 2003/02/08 07:53:36 jmc Exp $");
 #endif
 #endif
 
@@ -156,7 +156,7 @@ make_playpen(char *pen, size_t pensize, size_t sz)
 		strcpy(Previous, Current);
 	else if (!getcwd(Previous, FILENAME_MAX)) {
 		cleanup(0);
-		err(1, "fatal error during execution: getcwd");
+		err(EXIT_FAILURE, "fatal error during execution: getcwd");
 	}
 	if (chdir(pen) == FAIL) {
 		cleanup(0);
