fde6e61b503bec5af289f7309aac614fc4c2739e
pk <pk@NetBSD.org>
2003-02-09 22:28:40 +0000

    uao_put: release uvm object's lock only after we're done with its page list.

--- sys/uvm/uvm_aobj.c
+++ sys/uvm/uvm_aobj.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: uvm_aobj.c,v 1.54 2003/02/01 06:23:54 thorpej Exp $	*/
+/*	$NetBSD: uvm_aobj.c,v 1.55 2003/02/09 22:28:40 pk Exp $	*/
 
 /*
  * Copyright (c) 1998 Chuck Silvers, Charles D. Cranor and
@@ -43,7 +43,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: uvm_aobj.c,v 1.54 2003/02/01 06:23:54 thorpej Exp $");
+__KERNEL_RCSID(0, "$NetBSD: uvm_aobj.c,v 1.55 2003/02/09 22:28:40 pk Exp $");
 
 #include "opt_uvmhist.h"
 
@@ -916,11 +916,11 @@ uao_put(uobj, start, stop, flags)
 		}
 	}
 	uvm_unlock_pageq();
-	simple_unlock(&uobj->vmobjlock);
 	if (by_list) {
 		TAILQ_REMOVE(&uobj->memq, &endmp, listq);
 		PRELE(curlwp);
 	}
+	simple_unlock(&uobj->vmobjlock);
 	return 0;
 }
 
