afc03f9fe6db7860969a524c481f4359751eff20
cgd <cgd@NetBSD.org>
2003-02-11 23:48:21 +0000

    fix spacing on Broadcom license.

--- sys/arch/sbmips/sbmips/rtc.c
+++ sys/arch/sbmips/sbmips/rtc.c
@@ -1,4 +1,4 @@
-/* $NetBSD: rtc.c,v 1.6 2003/02/07 17:46:12 cgd Exp $ */
+/* $NetBSD: rtc.c,v 1.7 2003/02/11 23:48:21 cgd Exp $ */
 
 /*
  * Copyright 2002 Wasabi Systems, Inc.
@@ -465,24 +465,24 @@ return;	/* XXX XXX */
 /* XXX eville direct-access-to-the-device code follows... */
 
 /*
-    *  Copyright 2000,2001
-    *  Broadcom Corporation. All rights reserved.
-    *  
+ * Copyright 2000,2001
+ * Broadcom Corporation. All rights reserved.
+ *
  * This software is furnished under license and may be used and copied only
  * in accordance with the following terms and conditions.  Subject to these
  * conditions, you may download, copy, install, use, modify and distribute
  * modified or unmodified copies of this software in source and/or binary
  * form. No title or ownership is transferred hereby.
-    *  
+ *
  * 1) Any source code used, modified or distributed must reproduce and
  *    retain this copyright notice and list of conditions as they appear in
  *    the source file.
-    *  
+ *
  * 2) No right is granted to use any trade name, trademark, or logo of
  *    Broadcom Corporation.  The "Broadcom Corporation" name may not be
  *    used to endorse or promote products derived from this software
  *    without the prior written permission of Broadcom Corporation.
-    *  
+ *
  * 3) THIS SOFTWARE IS PROVIDED "AS-IS" AND ANY EXPRESS OR IMPLIED
  *    WARRANTIES, INCLUDING BUT NOT LIMITED TO, ANY IMPLIED WARRANTIES OF
  *    MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, OR
