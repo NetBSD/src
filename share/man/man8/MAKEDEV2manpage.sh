9788993f8501bce240daec8621da03343ca351e3
grant <grant@NetBSD.org>
2003-02-14 15:20:17 +0000

    The correct capitalisation of 'NetBSD.org' is (you guessed it) 'NetBSD.org'.
    some mdoc fixes.

--- share/man/man8/MAKEDEV2manpage.sh
+++ share/man/man8/MAKEDEV2manpage.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 #
-#       $NetBSD: MAKEDEV2manpage.sh,v 1.2 2002/08/02 23:35:06 wiz Exp $
+#       $NetBSD: MAKEDEV2manpage.sh,v 1.3 2003/02/14 15:20:20 grant Exp $
 #
 # Copyright (c) 2002
-#       Dieter Baron <dillo@netbsd.org>.  All rights reserved.
+#       Dieter Baron <dillo@NetBSD.org>.  All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
