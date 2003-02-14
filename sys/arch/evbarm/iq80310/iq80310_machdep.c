9aad4d3314b6e325b45ed4f7d2518823d5c2636e
he <he>
2003-02-14 22:24:05 +0000

    Revert pull-up of revision 1.43 (requested by rearnsha in ticket #1170):   Revert cached memory access to L1 tables, this causes instability   on the release branch.

--- sys/arch/evbarm/iq80310/iq80310_machdep.c
+++ sys/arch/evbarm/iq80310/iq80310_machdep.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: iq80310_machdep.c,v 1.40.4.1 2002/11/21 20:16:59 he Exp $	*/
+/*	$NetBSD: iq80310_machdep.c,v 1.40.4.2 2003/02/14 22:24:05 he Exp $	*/
 
 /*
  * Copyright (c) 2001, 2002 Wasabi Systems, Inc.
@@ -601,7 +601,7 @@ initarm(void *arg)
 	    UPAGES * NBPG, VM_PROT_READ|VM_PROT_WRITE, PTE_CACHE);
 
 	pmap_map_chunk(l1pagetable, kernel_l1pt.pv_va, kernel_l1pt.pv_pa,
-	    L1_TABLE_SIZE, VM_PROT_READ|VM_PROT_WRITE, PTE_CACHE);
+	    L1_TABLE_SIZE, VM_PROT_READ|VM_PROT_WRITE, PTE_NOCACHE);
 
 	/* Map the Mini-Data cache clean area. */
 	xscale_setup_minidata(l1pagetable, minidataclean.pv_va,
