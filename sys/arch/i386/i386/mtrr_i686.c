87df29647def4ecc44bb7d1479c451bbe36534cc
simonb <simonb@NetBSD.org>
2003-02-19 05:38:58 +0000

    Revert previous (in line with <i386/mtrr.h> reversion).

--- sys/arch/i386/i386/mtrr_i686.c
+++ sys/arch/i386/i386/mtrr_i686.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: mtrr_i686.c,v 1.7 2003/02/09 09:38:23 jdolecek Exp $ */
+/*	$NetBSD: mtrr_i686.c,v 1.8 2003/02/19 05:38:58 simonb Exp $ */
 
 /*-
  * Copyright (c) 2000 The NetBSD Foundation, Inc.
@@ -37,7 +37,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: mtrr_i686.c,v 1.7 2003/02/09 09:38:23 jdolecek Exp $");
+__KERNEL_RCSID(0, "$NetBSD: mtrr_i686.c,v 1.8 2003/02/19 05:38:58 simonb Exp $");
 
 #include "opt_multiprocessor.h"
 
@@ -113,7 +113,7 @@ static struct mtrr_state *mtrr_fixed_raw;
 static struct mtrr *mtrr_fixed;
 static struct mtrr *mtrr_var;
 
-const struct mtrr_funcs i686_mtrr_funcs = {
+struct mtrr_funcs i686_mtrr_funcs = {
 	i686_mtrr_init_cpu,
 	i686_mtrr_reload_cpu,
 	i686_mtrr_clean,
