1fbde7ec0c4674e22601357a16e464bde21f8564
tron <tron@NetBSD.org>
2003-02-20 10:02:21 +0000

    Pull up revision 1.304 (requested by jmc in ticket #1178): USE_XF86_4 on macppc.

--- share/mk/bsd.own.mk
+++ share/mk/bsd.own.mk
@@ -1,4 +1,4 @@
-#	$NetBSD: bsd.own.mk,v 1.294.2.7 2002/12/01 21:40:39 he Exp $
+#	$NetBSD: bsd.own.mk,v 1.294.2.8 2003/02/20 10:02:21 tron Exp $
 
 .if !defined(_BSD_OWN_MK_)
 _BSD_OWN_MK_=1
@@ -436,7 +436,8 @@ USE_${var}?= yes
 .endfor
 
 # Use XFree86 4.x as default version on i386 and x86_64.
-.if ${MACHINE_ARCH} == "i386" || ${MACHINE_ARCH} == "x86_64"
+.if ${MACHINE_ARCH} == "i386" || ${MACHINE_ARCH} == "x86_64" || \
+    ${MACHINE_ARCH} == "macppc"
 USE_XF86_4?=	yes
 .endif
 
