9340b40c1bafbc563f58efabb373d6072dc66148
jdolecek <jdolecek>
2003-02-25 09:12:11 +0000

    add __KERNEL_RCSID

--- sys/netsmb/smb_trantcp.c
+++ sys/netsmb/smb_trantcp.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: smb_trantcp.c,v 1.7 2003/02/21 21:12:24 jdolecek Exp $	*/
+/*	$NetBSD: smb_trantcp.c,v 1.8 2003/02/25 09:12:11 jdolecek Exp $	*/
 
 /*
  * Copyright (c) 2000-2001 Boris Popov
@@ -33,6 +33,10 @@
  *
  * FreeBSD: src/sys/netsmb/smb_trantcp.c,v 1.4 2001/12/02 08:47:29 bp Exp
  */
+
+#include <sys/cdefs.h>
+__KERNEL_RCSID(0, "$NetBSD: smb_trantcp.c,v 1.8 2003/02/25 09:12:11 jdolecek Exp $");
+ 
 #include <sys/param.h>
 #include <sys/systm.h>
 #include <sys/kernel.h>
