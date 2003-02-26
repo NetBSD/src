a6927ac3697476b860362398f9a7b81ab783a0de
fvdl <fvdl>
2003-02-26 21:31:12 +0000

    Add various include files for MP; mostly redirects to x86/foo.h

---
+++ sys/arch/x86_64/include/apicvar.h
@@ -0,0 +1,3 @@
+/*	$NetBSD: apicvar.h,v 1.1 2003/02/26 21:31:12 fvdl Exp $	*/
+
+#include <x86/apicvar.h>
---
+++ sys/arch/x86_64/include/i82489reg.h
@@ -0,0 +1,3 @@
+/*	$NetBSD: i82489reg.h,v 1.1 2003/02/26 21:31:13 fvdl Exp $	*/
+
+#include <x86/i82489reg.h>
