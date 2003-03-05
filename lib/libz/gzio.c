971ca9a9a50cda3b3a205b7a27369d93956defdd
jmc <jmc@NetBSD.org>
2003-03-05 19:56:46 +0000

    Pullup rev 1.14-1.15 (requested by groo in tickets #29 and #30)  Fix for potential buffer overflow in snprintf() (from OpenBSD)  Remove the HAS_* ifdefs which select [v]s{,n}printf.  Always choose the  n version.

--- lib/libz/gzio.c
+++ lib/libz/gzio.c
@@ -1,4 +1,4 @@
-/* $NetBSD: gzio.c,v 1.9.8.1 2002/03/20 23:18:16 he Exp $ */
+/* $NetBSD: gzio.c,v 1.9.8.2 2003/03/05 19:56:46 jmc Exp $ */
 
 /* gzio.c -- IO on .gz files
  * Copyright (C) 1995-2002 Jean-loup Gailly.
@@ -7,7 +7,7 @@
  * Compile this file with -DNO_DEFLATE to avoid the compression code.
  */
 
-/* @(#) $Id: gzio.c,v 1.9.8.1 2002/03/20 23:18:16 he Exp $ */
+/* @(#) $Id: gzio.c,v 1.9.8.2 2003/03/05 19:56:46 jmc Exp $ */
 
 #include <stdio.h>
 
@@ -531,14 +531,9 @@ int ZEXPORTVA gzprintf (gzFile file, const char *format, /* args */ ...)
     int len;
 
     va_start(va, format);
-#ifdef HAS_vsnprintf
-    (void)vsnprintf(buf, sizeof(buf), format, va);
-#else
-    (void)vsprintf(buf, format, va);
-#endif
+    len = vsnprintf(buf, sizeof(buf), format, va);
     va_end(va);
-    len = strlen(buf); /* some *sprintf don't return the nb of bytes written */
-    if (len <= 0) return 0;
+    if (len <= 0 || len >= sizeof(buf)) return 0;
 
     return gzwrite(file, buf, (unsigned)len);
 }
@@ -554,15 +549,9 @@ int ZEXPORTVA gzprintf (file, format, a1, a2, a3, a4, a5, a6, a7, a8, a9, a10,
     char buf[Z_PRINTF_BUFSIZE];
     int len;
 
-#ifdef HAS_snprintf
-    snprintf(buf, sizeof(buf), format, a1, a2, a3, a4, a5, a6, a7, a8,
+    len = snprintf(buf, sizeof(buf), format, a1, a2, a3, a4, a5, a6, a7, a8,
 	     a9, a10, a11, a12, a13, a14, a15, a16, a17, a18, a19, a20);
-#else
-    sprintf(buf, format, a1, a2, a3, a4, a5, a6, a7, a8,
-	    a9, a10, a11, a12, a13, a14, a15, a16, a17, a18, a19, a20);
-#endif
-    len = strlen(buf); /* old sprintf doesn't return the nb of bytes written */
-    if (len <= 0) return 0;
+    if (len <= 0 || len >= sizeof(buf)) return 0;
 
     return gzwrite(file, buf, len);
 }
