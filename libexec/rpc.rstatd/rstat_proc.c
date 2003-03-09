a775363fd56b89ec4afcada601822e6e3b21e746
hannken <hannken@NetBSD.org>
2003-03-09 10:33:04 +0000

    Dont call gettimeofday on a rpc timeval. This fails on sparc64 because rpc timeval elements are int and timeval elements are long.

--- libexec/rpc.rstatd/rstat_proc.c
+++ libexec/rpc.rstatd/rstat_proc.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: rstat_proc.c,v 1.40 2002/11/02 01:59:24 mrg Exp $	*/
+/*	$NetBSD: rstat_proc.c,v 1.41 2003/03/09 10:33:04 hannken Exp $	*/
 
 /*
  * Sun RPC is a product of Sun Microsystems, Inc. and is provided for
@@ -35,7 +35,7 @@
 static char sccsid[] = "from: @(#)rpc.rstatd.c 1.1 86/09/25 Copyr 1984 Sun Micro";
 static char sccsid[] = "from: @(#)rstat_proc.c	2.2 88/08/01 4.0 RPCSRC";
 #else
-__RCSID("$NetBSD: rstat_proc.c,v 1.40 2002/11/02 01:59:24 mrg Exp $");
+__RCSID("$NetBSD: rstat_proc.c,v 1.41 2003/03/09 10:33:04 hannken Exp $");
 #endif
 #endif
 
@@ -301,8 +301,8 @@ updatestat(int dummy)
 		stats_all.s3.if_collisions += ifnet.if_data.ifi_collisions;
 		off = (long)ifnet.if_list.tqe_next;
 	}
-	gettimeofday((struct timeval *)&stats_all.s3.curtime,
-		(struct timezone *) 0);
+	stats_all.s3.curtime.tv_sec = tm.tv_sec;
+	stats_all.s3.curtime.tv_usec = tm.tv_usec;
 	alarm(1);
 }
 
