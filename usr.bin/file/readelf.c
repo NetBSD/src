f0266f58c68d4cf8a6b8cc8defacec6f5c2d6677
he <he@NetBSD.org>
2003-03-09 22:39:32 +0000

    Pull up revision 1.17 (requested by pooka in ticket #31):         Don't blindly trust values coming from an ELF file, and fix         a memory allocation issue (compute the correct size to allocate).

--- usr.bin/file/readelf.c
+++ usr.bin/file/readelf.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: readelf.c,v 1.6.4.2 2000/09/30 06:20:46 simonb Exp $	*/
+/*	$NetBSD: readelf.c,v 1.6.4.3 2003/03/09 22:39:32 he Exp $	*/
 
 #include "file.h"
 
@@ -20,7 +20,7 @@
 #if 0
 FILE_RCSID("@(#)Id: readelf.c,v 1.12 2000/04/11 02:32:35 christos Exp ")
 #else
-__RCSID("$NetBSD: readelf.c,v 1.6.4.2 2000/09/30 06:20:46 simonb Exp $");
+__RCSID("$NetBSD: readelf.c,v 1.6.4.3 2003/03/09 22:39:32 he Exp $");
 #endif
 #endif
 
@@ -108,12 +108,18 @@ getu64(swap, value)
 #define sh_addr		(class == ELFCLASS32		\
 			 ? (void *) &sh32		\
 			 : (void *) &sh64)
+#define sh_size		(class == ELFCLASS32		\
+			 ? sizeof sh32			\
+			 : sizeof sh64)
 #define shs_type	(class == ELFCLASS32		\
 			 ? getu32(swap, sh32.sh_type)	\
 			 : getu32(swap, sh64.sh_type))
 #define ph_addr		(class == ELFCLASS32		\
 			 ? (void *) &ph32		\
 			 : (void *) &ph64)
+#define ph_size		(class == ELFCLASS32		\
+			 ? sizeof ph32			\
+			 : sizeof ph64)
 #define ph_type		(class == ELFCLASS32		\
 			 ? getu32(swap, ph32.p_type)	\
 			 : getu32(swap, ph64.p_type))
@@ -148,11 +154,14 @@ doshn(class, swap, fd, off, num, size)
 	Elf32_Shdr sh32;
 	Elf64_Shdr sh64;
 
+	if (size != sh_size)
+		error("corrupted section header size.\n");
+
 	if (lseek(fd, off, SEEK_SET) == -1)
 		error("lseek failed (%s).\n", strerror(errno));
 
 	for ( ; num; num--) {
-		if (read(fd, sh_addr, size) == -1)
+		if (read(fd, sh_addr, sh_size) == -1)
 			error("read failed (%s).\n", strerror(errno));
 		if (shs_type == SHT_SYMTAB /* || shs_type == SHT_DYNSYM */) {
 			(void) printf (", not stripped");
@@ -181,11 +190,13 @@ dophn_exec(class, swap, fd, off, num, size)
 	char *linking_style = "statically";
 	char *shared_libraries = "";
 
+	if (size != ph_size)
+		error("corrupted program header size.\n");
 	if (lseek(fd, off, SEEK_SET) == -1)
 		error("lseek failed (%s).\n", strerror(errno));
 
   	for ( ; num; num--) {
-  		if (read(fd, ph_addr, size) == -1)
+  		if (read(fd, ph_addr, ph_size) == -1)
   			error("read failed (%s).\n", strerror(errno));
 
 		switch (ph_type) {
@@ -257,13 +268,15 @@ dophn_core(class, swap, fd, off, num, size)
 	int bufsize;
 	int is_freebsd;
 
+	if (size != ph_size)
+		error("corrupted program header size.\n");
 	/*
 	 * Loop through all the program headers.
 	 */
 	for ( ; num; num--) {
 		if (lseek(fd, off, SEEK_SET) == -1)
 			error("lseek failed (%s).\n", strerror(errno));
-		if (read(fd, ph_addr, size) == -1)
+		if (read(fd, ph_addr, ph_size) == -1)
 			error("read failed (%s).\n", strerror(errno));
 		off += size;
 		if (ph_type != PT_NOTE)
