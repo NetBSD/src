6e23a90fdb2e52d5652391ca2c86cc2e0f1de930
tshiozak <tshiozak@NetBSD.org>
2003-03-11 09:21:22 +0000

    add wcstoll, wcstoimax, wcstoull and wcstoumax. (lib/20656)   - these are defined in C99 (wcsto[ui]max) and SUSv3.   - these implementations are written by sigsegv@s25.xrea.com,     derived from NetBSD's wcstol/wcstoul.   - I just added some comments.

--- lib/libc/locale/wcstoimax.c
+++ lib/libc/locale/wcstoimax.c
@@ -1,13 +1,14 @@
-/* $NetBSD: wcstol.c,v 1.2 2003/03/11 09:21:23 tshiozak Exp $ */
+/* $NetBSD: wcstoimax.c,v 1.1 2003/03/11 09:21:23 tshiozak Exp $ */
 
 #include <sys/cdefs.h>
 #if defined(LIBC_SCCS) && !defined(lint)
-__RCSID("$NetBSD: wcstol.c,v 1.2 2003/03/11 09:21:23 tshiozak Exp $");
+__RCSID("$NetBSD: wcstoimax.c,v 1.1 2003/03/11 09:21:23 tshiozak Exp $");
 #endif /* LIBC_SCCS and not lint */
 
 #include <assert.h>
 #include <ctype.h>
 #include <errno.h>
+#include <inttypes.h>
 #include <limits.h>
 #include <stdlib.h>
 #include <wchar.h>
@@ -15,9 +16,9 @@ __RCSID("$NetBSD: wcstol.c,v 1.2 2003/03/11 09:21:23 tshiozak Exp $");
 
 #include "__wctoint.h"
 
-#define	_FUNCNAME	wcstol
-#define	__INT		long
-#define	__INT_MIN	LONG_MIN
-#define	__INT_MAX	LONG_MAX
+#define	_FUNCNAME	wcstoimax
+#define	__INT		intmax_t
+#define	__INT_MIN	INTMAX_MIN
+#define	__INT_MAX	INTMAX_MAX
 
 #include "_wcstol.h"
