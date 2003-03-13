3ef733e82c81d68568e1203dee063ed61aa0a47e
drochner <drochner>
2003-03-13 12:15:31 +0000

    make netif_close() return value void fix prototype

--- sys/arch/macppc/stand/ofwboot/netif_of.h
+++ sys/arch/macppc/stand/ofwboot/netif_of.h
@@ -1,4 +1,4 @@
-/* $NetBSD: netif_of.h,v 1.1 2003/03/13 11:35:55 drochner Exp $ */
+/* $NetBSD: netif_of.h,v 1.2 2003/03/13 12:15:31 drochner Exp $ */
 
 int netif_of_open(struct of_dev *);
-void netif_of_close(void);
+void netif_of_close(int);
