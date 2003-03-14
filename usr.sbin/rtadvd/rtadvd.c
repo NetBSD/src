624e079eee456c75bb66871a7714756f2a88fc5b
itojun <itojun@NetBSD.org>
2003-03-14 18:32:42 +0000

    add missing ND option length validation.  from kame

--- usr.sbin/rtadvd/rtadvd.c
+++ usr.sbin/rtadvd/rtadvd.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: rtadvd.c,v 1.26 2002/09/23 12:48:10 mycroft Exp $	*/
+/*	$NetBSD: rtadvd.c,v 1.27 2003/03/14 18:32:42 itojun Exp $	*/
 /*	$KAME: rtadvd.c,v 1.74 2002/09/08 01:25:17 itojun Exp $	*/
 
 /*
@@ -1183,12 +1183,21 @@ nd6_options(struct nd_opt_hdr *hdr, int limit,
 	int optlen = 0;
 
 	for (; limit > 0; limit -= optlen) {
+		if (limit < sizeof(struct nd_opt_hdr *)) {
+			syslog(LOG_INFO, "<%s> short option header", __func__);
+			goto bad;
+		}
+
 		hdr = (struct nd_opt_hdr *)((caddr_t)hdr + optlen);
-		optlen = hdr->nd_opt_len << 3;
 		if (hdr->nd_opt_len == 0) {
 			syslog(LOG_ERR,
 			    "<%s> bad ND option length(0) (type = %d)",
 			    __func__, hdr->nd_opt_type);
+			goto bad;
+		}
+		optlen = hdr->nd_opt_len << 3;
+		if (optlen > limit) {
+			syslog(LOG_INFO, "<%s> short option", __func__);
 			goto bad;
 		}
 
@@ -1205,10 +1214,24 @@ nd6_options(struct nd_opt_hdr *hdr, int limit,
 			continue;
 		}
 
+		/*
+		 * Option length check.  Do it here for all fixed-length
+		 * options.
+		 */
+		if ((hdr->nd_opt_type == ND_OPT_MTU &&
+		    (optlen != sizeof(struct nd_opt_mtu))) ||
+		    ((hdr->nd_opt_type == ND_OPT_PREFIX_INFORMATION &&
+		    optlen != sizeof(struct nd_opt_prefix_info)))) {
+			syslog(LOG_INFO, "<%s> invalid option length",
+			    __func__);
+			continue;
+		}
+
 		switch (hdr->nd_opt_type) {
 		case ND_OPT_SOURCE_LINKADDR:
 		case ND_OPT_TARGET_LINKADDR:
 		case ND_OPT_REDIRECTED_HEADER:
+			break;	/* we don't care about these options */
 		case ND_OPT_MTU:
 			if (ndopts->nd_opt_array[hdr->nd_opt_type]) {
 				syslog(LOG_INFO,
