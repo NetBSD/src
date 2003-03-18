bbff353f89ffc6bc6f75cbbeaa87a6fb5fa99bca
mycroft <mycroft>
2003-03-18 19:33:51 +0000

    Move more stuff into .rodata.

--- sys/lib/libz/infutil.c
+++ sys/lib/libz/infutil.c
@@ -1,4 +1,4 @@
-/* $NetBSD: infutil.c,v 1.4 2002/03/12 00:42:24 fvdl Exp $ */
+/* $NetBSD: infutil.c,v 1.5 2003/03/18 19:33:51 mycroft Exp $ */
 
 /* inflate_util.c -- data and routines common to blocks and codes
  * Copyright (C) 1995-2002 Mark Adler
@@ -14,7 +14,7 @@
 struct inflate_codes_state {int dummy;}; /* for buggy compilers */
 
 /* And'ing with mask[n] masks the lower n bits */
-uInt inflate_mask[17] = {
+const uInt inflate_mask[17] = {
     0x0000,
     0x0001, 0x0003, 0x0007, 0x000f, 0x001f, 0x003f, 0x007f, 0x00ff,
     0x01ff, 0x03ff, 0x07ff, 0x0fff, 0x1fff, 0x3fff, 0x7fff, 0xffff
