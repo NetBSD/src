93da74855e5722bdf6febd9e0c9d31b09e7a65a4
jdolecek <jdolecek@NetBSD.org>
2003-03-24 18:08:51 +0000

    Receive packets in a small pieces (NB_SORECEIVE_CHUNK), so TCP slowstart will get its ACKs faster. From Darwin via FreeBSD.
    XXX I don't have any idea if this is useful or reasonable, but it looks
    XXX sane and the code works. "Remove if found nonuseful."

--- sys/netsmb/smb_trantcp.h
+++ sys/netsmb/smb_trantcp.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: smb_trantcp.h,v 1.3 2003/02/21 20:12:05 jdolecek Exp $	*/
+/*	$NetBSD: smb_trantcp.h,v 1.4 2003/03/24 18:08:51 jdolecek Exp $	*/
 
 /*
  * Copyright (c) 2000-2001, Boris Popov
@@ -31,7 +31,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- * FreeBSD: src/sys/netsmb/smb_trantcp.h,v 1.2 2001/12/10 08:09:49 obrien Exp
+ * FreeBSD: src/sys/netsmb/smb_trantcp.h,v 1.3 2002/09/18 07:38:10 bp Exp
  */
 #ifndef _NETSMB_SMB_TRANTCP_H_
 #define	_NETSMB_SMB_TRANTCP_H_
@@ -83,6 +83,14 @@ struct nbpcb {
  */
 #define NB_SNDTIMEO	(5 * hz)
 #define NB_RCVTIMEO	(5 * hz)
+
+/*
+ * TCP slowstart presents a problem in conjunction with large
+ * reads.  To ensure a steady stream of ACKs while reading using
+ * large transaction sizes, we call soreceive() with a smaller
+ * buffer size.  See nbssn_recv().
+ */
+#define NB_SORECEIVE_CHUNK	(8 * 1024)
 
 extern struct smb_tran_desc smb_tran_nbtcp_desc;
 
