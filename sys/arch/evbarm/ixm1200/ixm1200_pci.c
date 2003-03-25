8c89aafa1248c7e9162d3070234551d09f3372ef
igy <igy@NetBSD.org>
2003-03-25 06:53:15 +0000

    Add __KERNEL_RCSID tags

--- sys/arch/evbarm/ixm1200/ixm1200_pci.c
+++ sys/arch/evbarm/ixm1200/ixm1200_pci.c
@@ -1,4 +1,4 @@
-/*      $NetBSD: ixm1200_pci.c,v 1.3 2003/02/17 20:51:53 ichiro Exp $ */
+/*      $NetBSD: ixm1200_pci.c,v 1.4 2003/03/25 06:53:16 igy Exp $ */
 #define PCI_DEBUG
 /*
  * Copyright (c) 2002, 2003
@@ -32,6 +32,9 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  */
+
+#include <sys/cdefs.h>
+__KERNEL_RCSID(0, "$NetBSD: ixm1200_pci.c,v 1.4 2003/03/25 06:53:16 igy Exp $");
 
 /*
  * IXM1200 PCI interrupt support.
--- sys/arch/evbarm/ixm1200/ixp12x0_mainbus.c
+++ sys/arch/evbarm/ixm1200/ixp12x0_mainbus.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: ixp12x0_mainbus.c,v 1.4 2003/02/17 20:51:53 ichiro Exp $ */
+/*	$NetBSD: ixp12x0_mainbus.c,v 1.5 2003/03/25 06:53:16 igy Exp $ */
 /*
  * Copyright (c) 2002
  *	Ichiro FUKUHARA <ichiro@ichiro.org>.
@@ -31,6 +31,9 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  */
+
+#include <sys/cdefs.h>
+__KERNEL_RCSID(0, "$NetBSD: ixp12x0_mainbus.c,v 1.5 2003/03/25 06:53:16 igy Exp $");
 
 /*
  * front-end for the ixp12x0 I/O Processor.
