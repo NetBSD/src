5a484868846265b84351d7b2182254e9e3423d41
pooka <pooka@NetBSD.org>
2003-03-25 22:29:42 +0000

    file 4.00

---
+++ dist/file/src/magic.h
@@ -0,0 +1,60 @@
+/*	$NetBSD: magic.h,v 1.1.1.1 2003/03/25 22:30:19 pooka Exp $	*/
+
+/*
+ * Copyright (c) Christos Zoulas 2003.
+ * All Rights Reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice immediately at the beginning of the file, without modification,
+ *    this list of conditions, and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *  
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+#ifndef _MAGIC_H
+#define _MAGIC_H
+
+#include <sys/types.h>
+
+#define	MAGIC_NONE	0x00	/* No flags */
+#define	MAGIC_DEBUG	0x01	/* Turn on debugging */
+#define	MAGIC_SYMLINK	0x02	/* Follow symlinks */
+#define	MAGIC_COMPRESS	0x04	/* Check inside compressed files */
+#define	MAGIC_DEVICES	0x08	/* Look at the contents of devices */
+#define	MAGIC_MIME	0x10	/* Return a mime string */
+#define	MAGIC_CONTINUE	0x20	/* Return all matches, not just the first */
+#define	MAGIC_CHECK	0x40	/* Print warnings to stderr */
+
+typedef struct magic_set *magic_t;
+
+magic_t magic_open(int flags);
+void magic_close(magic_t);
+
+const char *magic_file(magic_t, const char *);
+const char *magic_buffer(magic_t, const void *, size_t);
+
+const char *magic_error(magic_t);
+void magic_setflags(magic_t, int);
+
+int magic_load(magic_t, const char *);
+int magic_compile(magic_t, const char *);
+int magic_check(magic_t, const char *);
+
+#endif
