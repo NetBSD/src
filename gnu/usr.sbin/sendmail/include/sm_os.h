66cbb52af5fc5b8f765c81640b208767b2d47978
atatat <atatat@NetBSD.org>
2003-03-25 03:55:38 +0000

    Fix the $NetBSD$ tag.

--- gnu/usr.sbin/sendmail/include/sm_os.h
+++ gnu/usr.sbin/sendmail/include/sm_os.h
@@ -1,4 +1,4 @@
-/* $NetBSD */
+/* $NetBSD: sm_os.h,v 1.2 2003/03/25 03:55:38 atatat Exp $ */
 
 # if defined(__NetBSD__) && defined(__NetBSD_Version__) && __NetBSD_Version__ >= 104170000
 #  define HASSETUSERCONTEXT	1	/* BSDI-style login classes */
