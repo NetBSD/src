cf7cf04d33987ee4f7ab715c72180e1b1c7d46e6
perry <perry@NetBSD.org>
2003-04-02 00:58:56 +0000

    grow #! line length -- patch from cgd, fixes PR kern/20112 from Todd Vierling

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.162 2003/03/19 11:57:04 dsl Exp $	*/
+/*	$NetBSD: param.h,v 1.163 2003/04/02 00:58:56 perry Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -100,7 +100,7 @@
 #include <sys/syslimits.h>
 
 #define	MAXCOMLEN	16		/* max command name remembered */
-#define	MAXINTERP	64		/* max interpreter file name length */
+#define	MAXINTERP	PATH_MAX	/* max interpreter file name length */
 /* DEPRECATED: use LOGIN_NAME_MAX instead. */
 #define	MAXLOGNAME	(LOGIN_NAME_MAX - 1) /* max login name length */
 #define	NCARGS		ARG_MAX		/* max bytes for an exec function */
