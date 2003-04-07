3b364b9b85d4d42906e2e0cd7caa2f4621d2b869
kleink <kleink@NetBSD.org>
2003-04-07 21:04:20 +0000

    Make sure we reference getcontext() by its internal name.

--- lib/libc/arch/sparc64/gen/_lwp.c
+++ lib/libc/arch/sparc64/gen/_lwp.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: _lwp.c,v 1.2 2003/01/18 11:17:45 thorpej Exp $	*/
+/*	$NetBSD: _lwp.c,v 1.3 2003/04/07 21:04:22 kleink Exp $	*/
 
 /*-
  * Copyright (c) 2002 The NetBSD Foundation, Inc.
@@ -33,6 +33,7 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
+#include "namespace.h"
 #include <sys/types.h>
 #include <ucontext.h>
 #include <lwp.h>
