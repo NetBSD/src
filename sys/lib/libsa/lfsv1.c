3f60a78c119a870789d23cf9d04804da3b7d35d5
dsl <dsl@NetBSD.org>
2003-04-11 11:27:06 +0000

    Use common ffs/lfs file ufs.c instead of lfs specific version.

--- sys/lib/libsa/lfsv1.c
+++ sys/lib/libsa/lfsv1.c
@@ -1,4 +1,19 @@
-/* $NetBSD: lfsv1.c,v 1.1 2003/02/23 23:17:42 simonb Exp $ */
+/* $NetBSD: lfsv1.c,v 1.2 2003/04/11 11:27:06 dsl Exp $ */
 
-#define	LIBSA_LFSv1
-#include "lib/libsa/lfs.c"
+#define	LIBSA_LFS
+#define	REQUIRED_LFS_VERSION	1
+
+#define	ufs_open		lfsv1_open
+#define	ufs_close		lfsv1_close
+#define	ufs_read		lfsv1_read
+#define	ufs_write		lfsv1_write
+#define	ufs_seek		lfsv1_seek
+#define	ufs_stat		lfsv1_stat
+
+#define	fs_bsize		lfs_ibsize
+#define	IFILE_Vx		IFILE_V1
+
+#define	FSBTODB(fs, daddr)	(daddr)		/* LFSv1 uses sectors for addresses */
+#define	INOPBx(fs) INOPB(fs)
+
+#include "lib/libsa/ufs.c"
