ba6be90d1b00ca5a59ee9be63f2d78d87bc15a68
itojun <itojun>
2003-04-15 07:31:23 +0000

    10 char is not sufficient here.  sync w/ kame

--- crypto/dist/kame/libipsec/pfkey_dump.c
+++ crypto/dist/kame/libipsec/pfkey_dump.c
@@ -496,8 +496,8 @@ str_prefport(family, pref, port, ulp)
 	u_int family, pref, port, ulp;
 {
 	static char buf[128];
-	char prefbuf[10];
-	char portbuf[10];
+	char prefbuf[20];
+	char portbuf[20];
 	int plen;
 
 	switch (family) {
