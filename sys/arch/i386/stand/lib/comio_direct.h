9738b045f3b7200e560f054fde70a97eeb03c39d
dsl <dsl@NetBSD.org>
2003-04-16 14:58:32 +0000

    Add baud_rate arg to cominit_d

--- sys/arch/i386/stand/lib/comio_direct.h
+++ sys/arch/i386/stand/lib/comio_direct.h
@@ -1,6 +1,6 @@
-/*	$NetBSD: comio_direct.h,v 1.2 1998/01/05 07:02:58 perry Exp $	*/
+/*	$NetBSD: comio_direct.h,v 1.3 2003/04/16 14:58:32 dsl Exp $	*/
 
-void cominit_d __P((int));
+void cominit_d __P((int, int));
 int computc_d __P((int, int));
 int comgetc_d __P((int));
 int comstatus_d __P((int));
