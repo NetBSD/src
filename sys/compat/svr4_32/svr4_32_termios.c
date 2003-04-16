01761b17f440f2ddff79c8ec1d83b42109d542f2
dsl <dsl@NetBSD.org>
2003-04-16 19:20:33 +0000

    Remove unnecessary caddr_t casts

--- sys/compat/svr4_32/svr4_32_termios.c
+++ sys/compat/svr4_32/svr4_32_termios.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: svr4_32_termios.c,v 1.4 2003/03/22 07:54:22 nakayama Exp $	 */
+/*	$NetBSD: svr4_32_termios.c,v 1.5 2003/04/16 19:20:33 dsl Exp $	 */
 
 /*-
  * Copyright (c) 1994 The NetBSD Foundation, Inc.
@@ -37,7 +37,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: svr4_32_termios.c,v 1.4 2003/03/22 07:54:22 nakayama Exp $");
+__KERNEL_RCSID(0, "$NetBSD: svr4_32_termios.c,v 1.5 2003/04/16 19:20:33 dsl Exp $");
 
 #include <sys/param.h>
 #include <sys/proc.h>
@@ -524,7 +524,7 @@ svr4_32_term_ioctl(fp, l, retval, fd, cmd, data)
 	switch (cmd) {
 	case SVR4_TCGETA:
 	case SVR4_TCGETS:
-		if ((error = (*ctl)(fp, TIOCGETA, (caddr_t) &bt, p)) != 0)
+		if ((error = (*ctl)(fp, TIOCGETA,  &bt, p)) != 0)
 			return error;
 
 		memset(&st, 0, sizeof(st));
@@ -551,7 +551,7 @@ svr4_32_term_ioctl(fp, l, retval, fd, cmd, data)
 	case SVR4_TCSETAF:
 	case SVR4_TCSETSF:
 		/* get full BSD termios so we don't lose information */
-		if ((error = (*ctl)(fp, TIOCGETA, (caddr_t) &bt, p)) != 0)
+		if ((error = (*ctl)(fp, TIOCGETA,  &bt, p)) != 0)
 			return error;
 
 		switch (cmd) {
@@ -602,13 +602,13 @@ svr4_32_term_ioctl(fp, l, retval, fd, cmd, data)
 		print_svr4_32_termios(&st);
 #endif /* DEBUG_SVR4 */
 
-		return (*ctl)(fp, cmd, (caddr_t) &bt, p);
+		return (*ctl)(fp, cmd,  &bt, p);
 
 	case SVR4_TIOCGWINSZ:
 		{
 			struct svr4_winsize ws;
 
-			error = (*ctl)(fp, TIOCGWINSZ, (caddr_t) &ws, p);
+			error = (*ctl)(fp, TIOCGWINSZ,  &ws, p);
 			if (error)
 				return error;
 			return copyout(&ws, data, sizeof(ws));
@@ -620,7 +620,7 @@ svr4_32_term_ioctl(fp, l, retval, fd, cmd, data)
 
 			if ((error = copyin(data, &ws, sizeof(ws))) != 0)
 				return error;
-			return (*ctl)(fp, TIOCSWINSZ, (caddr_t) &ws, p);
+			return (*ctl)(fp, TIOCSWINSZ,  &ws, p);
 		}
 
 	default:
