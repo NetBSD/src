ad5b6f6e761c3bb33e6ed40d132c9df6d3067b87
christos <christos@NetBSD.org>
2003-04-19 23:05:28 +0000

    PR/3012: Greg A. Woods: Write all float.h files [except the vax of course] in terms of float_ieee.h

--- sys/arch/dreamcast/include/float.h
+++ sys/arch/dreamcast/include/float.h
@@ -1,7 +1,3 @@
-/*	$NetBSD: float.h,v 1.1 2000/12/11 18:19:14 marcus Exp $	*/
-#ifndef _DREAMCAST_FLOAT_H_
-#define _DREAMCAST_FLOAT_H_
+/*	$NetBSD: float.h,v 1.2 2003/04/19 23:05:29 christos Exp $	*/
 
 #include <sh3/float.h>
-
-#endif /* _DREAMCAST_FLOAT_H_ */
