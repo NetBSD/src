a2dfb1b5700ec5ce19e407f59c43d1b5981f7491
christos <christos@NetBSD.org>
2003-04-19 23:05:28 +0000

    PR/3012: Greg A. Woods: Write all float.h files [except the vax of course] in terms of float_ieee.h

--- sys/arch/evbsh3/include/float.h
+++ sys/arch/evbsh3/include/float.h
@@ -1,7 +1,3 @@
-/*	$NetBSD: float.h,v 1.1 1999/09/13 10:30:33 itojun Exp $	*/
-#ifndef _EVBSH3_FLOAT_H_
-#define _EVBSH3_FLOAT_H_
+/*	$NetBSD: float.h,v 1.2 2003/04/19 23:05:30 christos Exp $	*/
 
 #include <sh3/float.h>
-
-#endif /* _EVBSH3_FLOAT_H_ */
