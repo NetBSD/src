e95552a346478cfca0d765fcbd2c7f6a5040123e
christos <christos@NetBSD.org>
2003-04-21 00:08:24 +0000

    Override LDBL_MIN

--- sys/arch/x86/include/float.h
+++ sys/arch/x86/include/float.h
@@ -1,9 +1,10 @@
-/*	$NetBSD: float.h,v 1.2 2003/04/19 23:05:34 christos Exp $	*/
+/*	$NetBSD: float.h,v 1.3 2003/04/21 00:08:24 christos Exp $	*/
 
 #ifndef _X86_FLOAT_H_
 #define _X86_FLOAT_H_
 
 #define	LDBL_MANT_DIG	64
+#define LDBL_MIN	3.3621031431120935063E-4932L
 #include <sys/float_ieee.h>
 
 #endif	/* _X86_FLOAT_H_ */
