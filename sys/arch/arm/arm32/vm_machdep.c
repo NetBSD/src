23ff0ba8b6eed1e2d3a64e86462028698075a329
thorpej <thorpej@NetBSD.org>
2003-04-22 00:24:48 +0000

    Some ARM32_PMAP_NEW-related cleanup: * Define a new "MMU type", ARM_MMU_SA1.  While the SA-1's MMU is basically   compatible with the generic, the SA-1 cache does not have a write-through   mode, and it is useful to know have an indication of this. * Add a new PMAP_NEEDS_PTE_SYNC indicator, and try to evaluate it at   compile time.  We evaluate it like so:   - If SA-1-style MMU is the only type configured -> 1   - If SA-1-style MMU is not configured -> 0   - Otherwise, defer to a run-time variable.   If PMAP_NEEDS_PTE_SYNC might evaluate to true (SA-1 only or run-time   check), then we also define PMAP_INCLUDE_PTE_SYNC so that e.g. assembly   code can include the necessary run-time support.  PMAP_INCLUDE_PTE_SYNC   largely replaces the ARM32_PMAP_NEEDS_PTE_SYNC manual setting Steve   included with the original new pmap. * In the new pmap, make pmap_pte_init_generic() check to see if the CPU   has a write-back cache.  If so, init the PT cache mode to C=1,B=0 to get   write-through mode.  Otherwise, init the PT cache mode to C=1,B=1. * Add a new pmap_pte_init_arm8().  Old pmap, same as generic.  New pmap,   sets page table cacheability to 0 (ARM8 has a write-back cache, but   flushing it is quite expensive). * In the new pmap, make pmap_pte_init_arm9() reset the PT cache mode to   C=1,B=0, since the write-back check in generic gets it wrong for ARM9,   since we use write-through mode all the time on ARM9 right now.  (What   this really tells me is that the test for write-through cache is less   than perfect, but we can fix that later.) * Add a new pmap_pte_init_sa1().  Old pmap, same as generic.  New pmap,   does generic initialization, then resets page table cache mode to   C=1,B=1, since C=1,B=0 does not produce write-through on the SA-1.

--- sys/arch/arm/arm32/vm_machdep.c
+++ sys/arch/arm/arm32/vm_machdep.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: vm_machdep.c,v 1.26 2003/04/18 11:08:26 scw Exp $	*/
+/*	$NetBSD: vm_machdep.c,v 1.27 2003/04/22 00:24:49 thorpej Exp $	*/
 
 /*
  * Copyright (c) 1994-1998 Mark Brinicombe.
@@ -344,9 +344,6 @@ pagemove(from, to, size)
 		*fpte++ = 0;
 		size -= PAGE_SIZE;
 	}
-#ifndef ARM32_PMAP_NEEDS_PTE_SYNC
-	(void)ptecnt;
-#endif
 	PTE_SYNC_RANGE(vtopte((vaddr_t)from), ptecnt);
 	PTE_SYNC_RANGE(vtopte((vaddr_t)to), ptecnt);
 	//cpu_tlb_flushD();
