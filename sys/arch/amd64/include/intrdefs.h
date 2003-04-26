dc5a7a4f9679fd62299c2587404943f85c47d935
fvdl <fvdl@NetBSD.org>
2003-04-26 18:39:25 +0000

    Rename the x86_64 port to amd64, as this is the actual name used for the processor family now. x86_64 is kept as the MACHINE_ARCH value, since it's already widely used (by e.g. the toolchain, etc), and by other operating systems.

---
+++ sys/arch/amd64/include/intrdefs.h
@@ -0,0 +1,3 @@
+/*	$NetBSD: intrdefs.h,v 1.1 2003/04/26 18:39:43 fvdl Exp $	*/
+
+#include <x86/intrdefs.h>
---
+++ sys/arch/amd64/include/math.h
@@ -0,0 +1,3 @@
+/*	$NetBSD: math.h,v 1.1 2003/04/26 18:39:43 fvdl Exp $	*/
+
+#define	__HAVE_NANF
