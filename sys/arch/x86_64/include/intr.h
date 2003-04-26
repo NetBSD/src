203d8003789075f33458e66ec9f433d95f274d7d
fvdl <fvdl>
2003-04-26 18:40:00 +0000

    Rename the x86_64 port to amd64, as this is the actual name used for the processor family now. x86_64 is kept as the MACHINE_ARCH value, since it's already widely used (by e.g. the toolchain, etc), and by other operating systems.

--- sys/arch/x86_64/include/intr.h
+++
@@ -1,3 +0,0 @@
-/*	$NetBSD: intr.h,v 1.5 2003/02/26 21:29:35 fvdl Exp $	*/
-
-#include <x86/intr.h>
--- sys/arch/x86_64/include/lock.h
+++
@@ -1,3 +0,0 @@
-/*	$NetBSD: lock.h,v 1.2 2003/03/05 23:56:02 fvdl Exp $	*/
-
-#include <x86/lock.h>
