e8ee90b9415c9eea544554fa63238edd649f4b27
fvdl <fvdl@NetBSD.org>
2003-04-26 18:40:00 +0000

    Rename the x86_64 port to amd64, as this is the actual name used for the processor family now. x86_64 is kept as the MACHINE_ARCH value, since it's already widely used (by e.g. the toolchain, etc), and by other operating systems.

--- sys/arch/x86_64/include/i82093reg.h
+++
@@ -1,6 +0,0 @@
-/*	 $NetBSD: i82093reg.h,v 1.2 2003/03/05 23:56:01 fvdl Exp $ */
-
-#include <x86/i82093reg.h>
-
-#define ioapic_asm_ack(num) \
-	movl	$0,(_C_LABEL(local_apic)+LAPIC_EOI)(%rip)
--- sys/arch/x86_64/include/pci_machdep.h
+++
@@ -1,3 +0,0 @@
-/*	$NetBSD: pci_machdep.h,v 1.3 2003/02/26 21:29:35 fvdl Exp $	*/
-
-#include <x86/pci_machdep.h>
