613f437834c090880f7e40c488d9805e0b34f3c6
dsl <dsl@NetBSD.org>
2003-05-08 11:34:19 +0000

    Add -g gid -m mode -u uid so that MAKEDEV can run when chown/chmod are not available. Add -r and -R to do two (different) sensible things if node exists. From a discussion with Lukem.

--- sbin/mknod/mknod.c
+++ sbin/mknod/mknod.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: mknod.c,v 1.25 2003/04/17 04:01:57 lukem Exp $	*/
+/*	$NetBSD: mknod.c,v 1.26 2003/05/08 11:34:19 dsl Exp $	*/
 
 /*-
  * Copyright (c) 1998, 2001 The NetBSD Foundation, Inc.
@@ -39,11 +39,12 @@
 #include <sys/cdefs.h>
 #ifndef lint
 __COPYRIGHT("@(#) Copyright (c) 1998 The NetBSD Foundation, Inc.  All rights reserved.\n");
-__RCSID("$NetBSD: mknod.c,v 1.25 2003/04/17 04:01:57 lukem Exp $");
+__RCSID("$NetBSD: mknod.c,v 1.26 2003/05/08 11:34:19 dsl Exp $");
 #endif /* not lint */
 
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/param.h>
 
 #include <err.h>
 #include <errno.h>
@@ -51,13 +52,17 @@ __RCSID("$NetBSD: mknod.c,v 1.25 2003/04/17 04:01:57 lukem Exp $");
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <pwd.h>
+#include <grp.h>
 #include <string.h>
+#include <ctype.h>
 
 #include "pack_dev.h"
 
+static int gid_name(const char *, gid_t *);
+
 	int	main(int, char *[]);
 static	void	usage(void);
-
 
 #define	MAXARGS	3		/* 3 for bsdos, 2 for rest */
 
@@ -70,19 +75,65 @@ main(int argc, char **argv)
 	pack_t	*pack;
 	u_long	 numbers[MAXARGS];
 	int	 n, ch, fifo, hasformat;
+	int	 r_flag = 0;		/* force: delete existing entry */
+	void	*modes = 0;
+	uid_t	 uid = -1;
+	gid_t	 gid = -1;
+	int	 rval;
 
 	dev = 0;
 	fifo = hasformat = 0;
 	pack = pack_native;
 
-	while ((ch = getopt(argc, argv, "F:")) != -1) {
+	while ((ch = getopt(argc, argv, "rRF:g:m:u:")) != -1) {
 		switch (ch) {
+
+		case 'r':
+			r_flag = 1;
+			break;
+
+		case 'R':
+			r_flag = 2;
+			break;
+
 		case 'F':
 			pack = pack_find(optarg);
 			if (pack == NULL)
 				errx(1, "invalid format: %s", optarg);
 			hasformat++;
 			break;
+
+		case 'g':
+			if (optarg[0] == '#') {
+				gid = strtol(optarg + 1, &p, 10);
+				if (*p == 0)
+					break;
+			}
+			if (gid_name(optarg, &gid) == 0)
+				break;
+			gid = strtol(optarg, &p, 10);
+			if (*p == 0)
+				break;
+			errx(1, "%s: invalid group name", optarg);
+
+		case 'm':
+			modes = setmode(optarg);
+			if (modes == NULL)
+				errx(1, "invalid mode: %s", optarg);
+			break;
+
+		case 'u':
+			if (optarg[0] == '#') {
+				uid = strtol(optarg + 1, &p, 10);
+				if (*p == 0)
+					break;
+			}
+			if (uid_from_user(optarg, &uid) == 0)
+				break;
+			uid = strtol(optarg, &p, 10);
+			if (*p == 0)
+				break;
+			errx(1, "%s: invalid user name", optarg);
 
 		default:
 		case '?':
@@ -99,7 +150,9 @@ main(int argc, char **argv)
 	argc--;
 	argv++;
 
-	mode = S_IRUSR|S_IWUSR|S_IRGRP|S_IWGRP|S_IROTH|S_IWOTH;
+	umask(mode = umask(0));
+	mode = (S_IRUSR|S_IWUSR|S_IRGRP|S_IWGRP|S_IROTH|S_IWOTH) & ~mode;
+
 	if (argv[0][1] != '\0')
 		goto badtype;
 	switch (*argv[0]) {
@@ -114,6 +167,7 @@ main(int argc, char **argv)
 	case 'p':
 		if (hasformat)
 			errx(1, "format is meaningless for fifos");
+		mode |= S_IFIFO;
 		fifo = 1;
 		break;
 
@@ -135,9 +189,9 @@ main(int argc, char **argv)
 	for (n = 0; n < argc; n++) {
 		errno = 0;
 		numbers[n] = strtoul(argv[n], &p, 0);
-		if ((p && *p != '\0') ||
-		    (numbers[n] == ULONG_MAX && errno == ERANGE))
-			errx(1, "invalid number: %s", argv[n]);
+		if (*p == 0 && errno == 0)
+			continue;
+		errx(1, "invalid number: %s", argv[n]);
 	}
 
 	switch (argc) {
@@ -154,14 +208,40 @@ main(int argc, char **argv)
 		break;
 	}
 
+	if (modes != NULL)
+		mode = getmode(modes, mode);
 #if 0
-	printf("name: %s\nmode: %05o\ndev:  %08x\n", name, mode, dev);
-#else
-	if ((fifo ? mkfifo(name, mode) : mknod(name, mode, dev)) < 0)
-		err(1, "%s", name);
+	printf("name: %s\nmode: %05o\ndev:  %08x\nuid: %5d\ngid: %5d\n",
+		name, mode, dev, uid, gid);
 #endif
+	umask(0);
+	rval = fifo ? mkfifo(name, mode) : mknod(name, mode, dev);
+	if (rval < 0 && errno == EEXIST && r_flag) {
+		struct stat sb;
+		if (lstat(name, &sb) != 0 || (!fifo && sb.st_rdev != dev))
+			sb.st_mode = 0;
 
-	return(0);
+		if ((sb.st_mode & S_IFMT) == (mode & S_IFMT)) {
+			if (r_flag == 1)
+				/* Ignore permissions and user/group */
+				return 0;
+			if (sb.st_mode != mode)
+				rval = chmod(name, mode);
+			else
+				rval = 0;
+		} else {
+			unlink(name);
+			rval = fifo ? mkfifo(name, mode)
+				    : mknod(name, mode, dev);
+		}
+	}
+	if (rval < 0)
+		err(1, "%s", name);
+	if ((uid != -1 || gid != -1) && chown(name, uid, gid) == -1)
+		/* XXX Should we unlink the files here? */
+		warn("%s: uid/gid not changed", name);
+
+	return 0;
 }
 
 static void
@@ -170,11 +250,24 @@ usage(void)
 	const char *progname = getprogname();
 
 	(void)fprintf(stderr,
-	    "Usage: %s [-F format] name [b | c] major minor\n", progname);
-	(void)fprintf(stderr,
-	    "       %s [-F format] name [b | c] major unit subunit\n",
+	    "Usage: %s [-rR] [-F format] [-m mode] [-u user] [-g group]\n",
 	    progname);
-	(void)fprintf(stderr, "       %s name [b | c] number\n", progname);
-	(void)fprintf(stderr, "       %s name p\n", progname);
+	(void)fprintf(stderr,
+	    "                   [ name [b | c] major minor\n"
+	    "                   | name [b | c] major unit subunit\n"
+	    "                   | name [b | c] number\n"
+	    "                   | name p ]\n");
 	exit(1);
+}
+
+static int
+gid_name(const char *name, gid_t *gid)
+{
+	struct group *g;
+
+	g = getgrnam(name);
+	if (!g)
+		return -1;
+	*gid = g->gr_gid;
+	return 0;
 }
