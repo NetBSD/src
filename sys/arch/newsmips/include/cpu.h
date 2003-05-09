80d9f32ad971fb124e92749a0909177445a4ab48
tsutsui <tsutsui>
2003-05-09 13:36:39 +0000

    - Define and use struct hb_attach_args rather than struct confargs   for hb devices. - Move declarations for hb functions and macros to dev/hbvar.h. - Define and use hb_badaddr() macro for hb devices. - Declare news3400_badaddr() in machine/cpu.h rather than machine/autoconf.h. - Remove unused stuff from machine/autoconf.h.

--- sys/arch/newsmips/include/cpu.h
+++ sys/arch/newsmips/include/cpu.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: cpu.h,v 1.15 2003/04/26 18:50:18 tsutsui Exp $	*/
+/*	$NetBSD: cpu.h,v 1.16 2003/05/09 13:36:42 tsutsui Exp $	*/
 
 #ifndef _NEWSMIPS_CPU_H_
 #define _NEWSMIPS_CPU_H_
@@ -15,6 +15,7 @@ extern int systype;
 /* System type dependent initializations. */
 #ifdef news3400
 void news3400_init(void);
+int news3400_badaddr(void *, u_int);
 #endif
 #ifdef news5000
 void news5000_init(void);
