34a6cf96263345cabb0a71f46d964f0e9598b693
thorpej <thorpej@NetBSD.org>
2003-05-10 21:12:24 +0000

    Bump version to 1.6T:
    Back out the following chagne:
        http://mail-index.netbsd.org/source-changes/2003/05/08/0068.html
    
    There were some side-effects that I didn't anticipate, and fixing them
    is proving to be more difficult than I thought, do just eject for now.
    Maybe one day we can look at this again.
    
    Fixes PR kern/21517.

--- sys/conf/osrelease.sh
+++ sys/conf/osrelease.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-#	$NetBSD: osrelease.sh,v 1.95 2003/05/08 18:13:30 thorpej Exp $
+#	$NetBSD: osrelease.sh,v 1.96 2003/05/10 21:12:25 thorpej Exp $
 #
 # Copyright (c) 1997 The NetBSD Foundation, Inc.
 # All rights reserved.
@@ -42,7 +42,7 @@
 #	sys/sys/param.h:	__NetBSD_Version__
 #	share/tmac/doc-common:	ds oS
 #
-release=1.6S
+release=1.6T
 
 case $1 in
 -s)
