ef939cd12e6207a671047d09c809fda8933d5961
kleink <kleink@NetBSD.org>
2003-05-12 15:22:53 +0000

    Rename <sys/float_ieee.h> to <sys/float_ieee754.h>, following libc's convention for these.

--- sys/arch/alpha/include/float.h
+++ sys/arch/alpha/include/float.h
@@ -1,3 +1,3 @@
-/* $NetBSD: float.h,v 1.10 2003/04/19 23:05:28 christos Exp $ */
+/* $NetBSD: float.h,v 1.11 2003/05/12 15:22:54 kleink Exp $ */
 
-#include <sys/float_ieee.h>
+#include <sys/float_ieee754.h>
