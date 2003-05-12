3bb5ea659cd95adef37324c8d2e061e81ffefc14
thorpej <thorpej>
2003-05-12 14:48:12 +0000

    Include <sys/disk.h>.

--- sys/arch/pmppc/pmppc/disksubr.c
+++ sys/arch/pmppc/pmppc/disksubr.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: disksubr.c,v 1.4 2003/05/10 23:12:37 thorpej Exp $	*/
+/*	$NetBSD: disksubr.c,v 1.5 2003/05/12 14:48:13 thorpej Exp $	*/
 
 /*
  * Copyright (c) 1982, 1986, 1988 Regents of the University of California.
@@ -39,6 +39,7 @@
 #include <sys/systm.h>
 #include <sys/buf.h>
 #include <sys/disklabel.h>
+#include <sys/disk.h>
 #include <sys/syslog.h>
 
 #include "opt_mbr.h"
