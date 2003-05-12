70a2ff37a3a91b1563ec992c927a8cbaab589207
kleink <kleink>
2003-05-12 15:22:53 +0000

    Rename <sys/float_ieee.h> to <sys/float_ieee754.h>, following libc's convention for these.

--- sys/arch/m68k/include/float.h
+++ sys/arch/m68k/include/float.h
@@ -1,9 +1,9 @@
-/*	$NetBSD: float.h,v 1.15 2003/04/20 12:57:43 bjh21 Exp $	*/
+/*	$NetBSD: float.h,v 1.16 2003/05/12 15:22:54 kleink Exp $	*/
 
 #ifndef _M68K_FLOAT_H_
 #define _M68K_FLOAT_H_
 
 #define LDBL_MANT_DIG	64
-#include <sys/float_ieee.h>
+#include <sys/float_ieee754.h>
 
 #endif	/* !_M68K_FLOAT_H_ */
