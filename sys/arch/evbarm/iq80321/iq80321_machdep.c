0bba8a069b1d6414bbe2f8adeaa6e3e08027a02b
thorpej <thorpej@NetBSD.org>
2003-05-17 23:46:59 +0000

    Decrease the verbosity level unless VERBOSE_INIT_ARM is defiend.

--- sys/arch/evbarm/iq80321/iq80321_machdep.c
+++ sys/arch/evbarm/iq80321/iq80321_machdep.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: iq80321_machdep.c,v 1.23 2003/05/03 18:25:32 thorpej Exp $	*/
+/*	$NetBSD: iq80321_machdep.c,v 1.24 2003/05/17 23:47:01 thorpej Exp $	*/
 
 /*
  * Copyright (c) 2001, 2002, 2003 Wasabi Systems, Inc.
@@ -253,10 +253,6 @@ int kgdb_devmode = KGDB_DEVMODE;
 void
 cpu_reboot(int howto, char *bootstr)
 {
-#ifdef DIAGNOSTIC
-	/* info */
-	printf("boot: howto=%08x curproc=%p\n", howto, curproc);
-#endif
 
 	/*
 	 * If we are still cold then hit the air brakes
@@ -403,8 +399,10 @@ initarm(void *arg)
 	 */
 	consinit();
 
+#ifdef VERBOSE_INIT_ARM
 	/* Talk to the user */
 	printf("\nNetBSD/evbarm (IQ80321) booting ...\n");
+#endif
 
 	/*
 	 * Heads up ... Setup the CPU / MMU / TLB functions
@@ -426,7 +424,9 @@ initarm(void *arg)
 	i80321_sdram_bounds(&obio_bs_tag, VERDE_PMMR_BASE + VERDE_MCU_BASE,
 	    &memstart, &memsize);
 
+#ifdef VERBOSE_INIT_ARM
 	printf("initarm: Configuring system ...\n");
+#endif
 
 	/* Fake bootconfig structure for the benefit of pmap.c */
 	/* XXX must make the memory description h/w independant */
@@ -454,9 +454,11 @@ initarm(void *arg)
 
 	physmem = (physical_end - physical_start) / PAGE_SIZE;
 
+#ifdef VERBOSE_INIT_ARM
 	/* Tell the user about the memory */
 	printf("physmemory: %d pages at 0x%08lx -> 0x%08lx\n", physmem,
 	    physical_start, physical_end - 1);
+#endif
 
 	/*
 	 * Okay, the kernel starts 2MB in from the bottom of physical
@@ -748,7 +750,9 @@ initarm(void *arg)
 	 * Since the ARM stacks use STMFD etc. we must set r13 to the top end
 	 * of the stack memory.
 	 */
+#ifdef VERBOSE_INIT_ARM
 	printf("init subsystems: stacks ");
+#endif
 
 	set_stackptr(PSR_IRQ32_MODE,
 	    irqstack.pv_va + IRQ_STACK_SIZE * PAGE_SIZE);
@@ -766,31 +770,44 @@ initarm(void *arg)
 	 * Initialisation of the vectors will just panic on a data abort.
 	 * This just fills in a slighly better one.
 	 */
+#ifdef VERBOSE_INIT_ARM
 	printf("vectors ");
+#endif
 	data_abort_handler_address = (u_int)data_abort_handler;
 	prefetch_abort_handler_address = (u_int)prefetch_abort_handler;
 	undefined_handler_address = (u_int)undefinedinstruction_bounce;
 
 	/* Initialise the undefined instruction handlers */
+#ifdef VERBOSE_INIT_ARM
 	printf("undefined ");
+#endif
 	undefined_init();
 
 	/* Load memory into UVM. */
+#ifdef VERBOSE_INIT_ARM
 	printf("page ");
+#endif
 	uvm_setpagesize();	/* initialize PAGE_SIZE-dependent variables */
 	uvm_page_physload(atop(physical_freestart), atop(physical_freeend),
 	    atop(physical_freestart), atop(physical_freeend),
 	    VM_FREELIST_DEFAULT);
 
 	/* Boot strap pmap telling it where the kernel page table is */
+#ifdef VERBOSE_INIT_ARM
 	printf("pmap ");
+#endif
 	pmap_bootstrap((pd_entry_t *)kernel_l1pt.pv_va, KERNEL_VM_BASE,
 	    KERNEL_VM_BASE + KERNEL_VM_SIZE);
 
 	/* Setup the IRQ system */
+#ifdef VERBOSE_INIT_ARM
 	printf("irq ");
+#endif
 	i80321_intr_init();
+
+#ifdef VERBOSE_INIT_ARM
 	printf("done.\n");
+#endif
 
 #ifdef BOOTHOWTO
 	boothowto = BOOTHOWTO;
