3ead59370ece7e5db6b18830a57f5d4d7572b180
dsl <dsl>
2003-05-18 18:54:05 +0000

    Use MD menus and msgs for mbr code. Remove a couple of unused variables. Use actual raw partition when looking for disklabel in sparc code

--- distrib/utils/sysinst/arch/atari/md.c
+++ distrib/utils/sysinst/arch/atari/md.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: md.c,v 1.5 2002/12/05 01:17:21 fvdl Exp $ */
+/*	$NetBSD: md.c,v 1.6 2003/05/18 18:54:06 dsl Exp $ */
 
 /*
  * Copyright 1997 Piermont Information Systems Inc.
@@ -51,8 +51,6 @@
 
 int c1024_resp;
 struct disklist *disklist = NULL;
-
-//int defbootselpart, defbootseldisk;
 
 
 /* prototypes */
