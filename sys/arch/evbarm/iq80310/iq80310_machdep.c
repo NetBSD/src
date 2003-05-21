ee405427c6682179def9931fe0d85100f490a71b
thorpej <thorpej@NetBSD.org>
2003-05-21 22:48:20 +0000

    Move KERNEL_VM_BASE inside where it is used (it won't be there for long).

--- sys/arch/evbarm/iq80310/iq80310_machdep.c
+++ sys/arch/evbarm/iq80310/iq80310_machdep.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: iq80310_machdep.c,v 1.55 2003/05/17 23:47:00 thorpej Exp $	*/
+/*	$NetBSD: iq80310_machdep.c,v 1.56 2003/05/21 22:48:22 thorpej Exp $	*/
 
 /*
  * Copyright (c) 2001, 2002, 2003 Wasabi Systems, Inc.
@@ -116,6 +116,7 @@
 
 /* Kernel text starts 2MB in from the bottom of the kernel address space. */
 #define	KERNEL_TEXT_BASE	(KERNEL_BASE + 0x00200000)
+#define	KERNEL_VM_BASE		(KERNEL_BASE + 0x01000000)
 
 /*
  * Address to call from cpu_reset() to reset the machine.
