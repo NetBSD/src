a2445cf7a2b91bbd76cd6924ef125f1234670951
ichiro <ichiro@NetBSD.org>
2003-05-23 00:57:23 +0000

    support IXP425 Intel Network Processor  running on BigEndian

--- sys/arch/arm/include/cpuconf.h
+++ sys/arch/arm/include/cpuconf.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: cpuconf.h,v 1.6 2003/04/22 00:24:49 thorpej Exp $	*/
+/*	$NetBSD: cpuconf.h,v 1.7 2003/05/23 00:57:24 ichiro Exp $	*/
 
 /*
  * Copyright (c) 2002 Wasabi Systems, Inc.
@@ -62,7 +62,8 @@
 			 defined(CPU_IXP12X0) +				\
 			 defined(CPU_XSCALE_80200) +			\
 			 defined(CPU_XSCALE_80321) +			\
-			 defined(CPU_XSCALE_PXA2X0))
+			 defined(CPU_XSCALE_PXA2X0) + 			\
+			 defined(CPU_XSCALE_IXP425))
 #else
 #define	CPU_NTYPES	2
 #endif /* _KERNEL_OPT */
@@ -87,7 +88,7 @@
 #if !defined(_KERNEL_OPT) ||						\
     (defined(CPU_ARM7TDMI) || defined(CPU_ARM8) || defined(CPU_ARM9) ||	\
      defined(CPU_SA110) || defined(CPU_SA1100) || defined(CPU_SA1110) || \
-     defined(CPU_IXP12X0))
+     defined(CPU_IXP12X0) || defined(CPU_XSCALE_IXP425))
 #define	ARM_ARCH_4	1
 #else
 #define	ARM_ARCH_4	0
@@ -146,7 +147,7 @@
 
 #if !defined(_KERNEL_OPT) ||						\
     (defined(CPU_XSCALE_80200) || defined(CPU_XSCALE_80321) ||		\
-     defined(CPU_XSCALE_PXA2X0))
+     defined(CPU_XSCALE_PXA2X0) || defined(CPU_XSCALE_IXP425))
 #define	ARM_MMU_XSCALE		1
 #else
 #define	ARM_MMU_XSCALE		0
