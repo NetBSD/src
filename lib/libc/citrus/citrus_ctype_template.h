2a3f397514cc23b4e6a8c0fda5533c115826b1cf
tron <tron@NetBSD.org>
2003-06-02 15:00:28 +0000

    Pull up revision 1.16 (requested by yamt in ticket #1202): when s==0, wctomb should initialize its internal state and return if encodings have state dependency or not. a patch provided with PR 18269 by wurlitzer. modified by me. fix part of PR 18269.

--- lib/libc/citrus/citrus_ctype_template.h
+++ lib/libc/citrus/citrus_ctype_template.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: citrus_ctype_template.h,v 1.13.2.2 2003/06/02 14:59:39 tron Exp $	*/
+/*	$NetBSD: citrus_ctype_template.h,v 1.13.2.3 2003/06/02 15:00:28 tron Exp $	*/
 
 /*-
  * Copyright (c)2002 Citrus Project,
@@ -602,18 +602,22 @@ static int
 _FUNCNAME(ctype_wctomb)(void * __restrict cl, char * __restrict s, wchar_t wc,
 			int * __restrict nresult)
 {
+	_ENCODING_STATE *psenc;
 	size_t nr;
 	int err = 0;
-	char s0[MB_LEN_MAX];
 
 	_DIAGASSERT(cl != NULL);
 
-	if (s==NULL)
-		s = s0;
+	psenc = &_CEI_TO_STATE(_TO_CEI(cl), wctomb);
+	if (s == NULL) {
+		_FUNCNAME(init_state)(_CEI_TO_EI(_TO_CEI(cl)), psenc);
+		*nresult = _ENCODING_IS_STATE_DEPENDENT;
+		return 0;
+	}
 
 	err = _FUNCNAME(wcrtomb_priv)(cl, s,
 		      _ENCODING_MB_CUR_MAX(_CEI_TO_EI(_TO_CEI(cl))),
-		      wc, &_CEI_TO_STATE(_TO_CEI(cl), wctomb), &nr);
+		      wc, psenc, &nr);
 	*nresult = (int)nr;
 
 	return 0;
