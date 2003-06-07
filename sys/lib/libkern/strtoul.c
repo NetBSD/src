bb107ac7f180a67b5fda6fae687989f9d72a9ece
briggs <briggs>
2003-06-07 02:52:33 +0000

    Provide a strtoul() implementation based on the one in libc instead of the totally different version that was here.  This version, of course, has an BSD license on it while the old one did not.  This one also compiles down to tighter code--the smaller the better for libkern & libsa.

--- sys/lib/libkern/strtoul.c
+++ sys/lib/libkern/strtoul.c
@@ -1,86 +1,147 @@
-/*	$NetBSD: strtoul.c,v 1.5 2000/05/25 01:22:41 msaitoh Exp $	*/
+/*	$NetBSD: strtoul.c,v 1.6 2003/06/07 02:52:33 briggs Exp $	*/
 
+/*
+ * Copyright (c) 1990, 1993
+ *	The Regents of the University of California.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed by the University of
+ *	California, Berkeley and its contributors.
+ * 4. Neither the name of the University nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#if !defined(_KERNEL) && !defined(_STANDALONE)
+#include <sys/cdefs.h>
+#if defined(LIBC_SCCS) && !defined(lint)
+#if 0
+static char sccsid[] = "@(#)strtoul.c	8.1 (Berkeley) 6/4/93";
+#else
+__RCSID("$NetBSD: strtoul.c,v 1.6 2003/06/07 02:52:33 briggs Exp $");
+#endif
+#endif /* LIBC_SCCS and not lint */
+
+#include <assert.h>
+#include <ctype.h>
+#include <errno.h>
+#include <limits.h>
+#include <stdlib.h>
+
+#else
 #include <sys/param.h>
 #include <lib/libkern/libkern.h>
-
-#ifndef ULONG_MAX
-#define	ULONG_MAX	((u_long)(~0L))		/* 0xFFFFFFFF */
+#define _DIAGASSERT(x)	(void)0
+#define isspace(x) ((x) == ' ' || (x) == '\t' || (x) == '\n' || (x) == '\r')
+#define isdigit(x) ((x) >= '0' && (x) <= '9')
+#define isalpha(x) (((x) >= 'a' && (x) <= 'z') || ((x) >= 'A' && (x) <= 'Z'))
+#define toupper(x) ((x) & ~0x20)
 #endif
 
-u_long
-strtoul(s, ptr, base)
-	const char *s;
-	char **ptr;
+/*
+ * Convert a string to an unsigned long integer.
+ *
+ * Ignores `locale' stuff.  Assumes that the upper and lower case
+ * alphabets and digits are each contiguous.
+ */
+unsigned long
+strtoul(nptr, endptr, base)
+	const char *nptr;
+	char **endptr;
 	int base;
 {
-	u_long total = 0;
-	u_int digit;
-	const char *start=s;
-	int did_conversion=0;
-	int overflow = 0;
-	int negate = 0;
-	u_long maxdiv, maxrem;
+	const char *s;
+	unsigned long acc, cutoff;
+	int c;
+	int neg, any, cutlim;
 
-	if (s == NULL) {
-		if (ptr != NULL)
-			*ptr = (char *)start;
-		return 0L;
+	_DIAGASSERT(nptr != NULL);
+	/* endptr may be NULL */
+
+	/*
+	 * See strtol for comments as to the logic used.
+	 */
+	s = nptr;
+	do {
+		c = (unsigned char) *s++;
+	} while (isspace(c));
+	if (c == '-') {
+		neg = 1;
+		c = *s++;
+	} else {
+		neg = 0;
+		if (c == '+')
+			c = *s++;
 	}
-
-	while (*s == ' ')
-		s++;
-	if (*s == '+')
-		s++;
-	else if (*s == '-')
-		s++, negate = 1;
-	if (base==0 || base==16) { /*  the 'base==16' is for handling 0x */
-		int tmp;
-
-      /*
-       * try to infer base from the string
-       */
-		if (*s != '0')
-			tmp = 10;	/* doesn't start with 0 - assume decimal */
-		else if (s[1] == 'X' || s[1] == 'x')
-			tmp = 16, s += 2; /* starts with 0x or 0X - hence hex */
-		else
-			tmp = 8;	/* starts with 0 - hence octal */
-		if (base==0)
-			base = (int)tmp;
+	if ((base == 0 || base == 16) &&
+	    c == '0' && (*s == 'x' || *s == 'X')) {
+		c = s[1];
+		s += 2;
+		base = 16;
 	}
+	if (base == 0)
+		base = c == '0' ? 8 : 10;
 
-	maxdiv = ULONG_MAX / base;
-	maxrem = ULONG_MAX % base;
-
-	while ((digit = *s) != '\0') {
-		if (digit >= '0' && digit < ('0' + min(base, 10)))
-			digit -= '0';
-		else
-			if (base > 10) {
-				if (digit >= 'a' && digit < ('a'+(base-10)))
-					digit = digit - 'a' + 10;
-				else if (digit >= 'A' && digit < ('A'+(base-10)))
-					digit = digit - 'A' + 10;
-				else
-					break;
-			}
-			else
-				break;
-			did_conversion = 1;
-			if (total > maxdiv
-			    || (total == maxdiv && digit > maxrem))
-				overflow = 1;
-			total = (total * base) + digit;
-			s++;
+	cutoff = ULONG_MAX / (unsigned long)base;
+	cutlim = (int)(ULONG_MAX % (unsigned long)base);
+	for (acc = 0, any = 0;; c = (unsigned char) *s++) {
+		if (isdigit(c))
+			c -= '0';
+		else if (isalpha(c)) {
+#if defined(_KERNEL) || defined(_STANDALONE)
+			c = toupper(c) - 'A' + 10;
+#else
+			c -= isupper(c) ? 'A' - 10 : 'a' - 10;
+#endif
+		} else
+			break;
+		if (c >= base)
+			break;
+#if !defined(_KERNEL) && !defined(_STANDALONE)
+		if (any < 0)
+			continue;
+#endif
+		if (acc > cutoff || (acc == cutoff && c > cutlim)) {
+#if defined(_KERNEL) || defined(_STANDALONE)
+			if (endptr)
+				*endptr = (char *) nptr;
+			return ULONG_MAX;
+#else
+			any = -1;
+			acc = ULONG_MAX;
+			errno = ERANGE;
+#endif
+		} else {
+			any = 1;
+			acc *= (unsigned long)base;
+			acc += c;
+		}
 	}
-	if (overflow) {
-		if (ptr != NULL)
-			*ptr = (char *)s;
-		return (ULONG_MAX);
-	}
-	if (ptr != NULL)
-		*ptr = (char *) ((did_conversion) ? (char *)s : (char *)start);
-	return negate ? -total : total;
+	if (neg && any > 0)
+		acc = -acc;
+	if (endptr != NULL)
+		/* LINTED interface specification */
+		*endptr = (char *)(any ? s - 1 : nptr);
+	return (acc);
 }
-
-/* End of strtoul.c */
