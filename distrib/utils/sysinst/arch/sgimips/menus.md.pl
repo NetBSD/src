9842121375aa635cef96f0246feba5b4b512a109
dsl <dsl@NetBSD.org>
2003-06-10 17:47:15 +0000

    Use dynamic menu for final editing of partition stable

--- distrib/utils/sysinst/arch/sgimips/menus.md.pl
+++ distrib/utils/sysinst/arch/sgimips/menus.md.pl
@@ -1,4 +1,4 @@
-/*	$NetBSD: menus.md.pl,v 1.5 2003/06/06 14:31:31 dsl Exp $	*/
+/*	$NetBSD: menus.md.pl,v 1.6 2003/06/10 17:47:23 dsl Exp $	*/
 /* Based on english version: */
 /*	NetBSD: menus.md.en,v 1.2 2001/11/29 23:21:01 thorpej Exp */
 
@@ -41,22 +41,6 @@
 
 /* sgimips machine dependent menus, Polish */
 
-menu editfsparts, y=12, exit;
-	display action  {
-			ask_sizemult(dlcylsize);
-			msg_display(MSG_fspart, multname);
-			disp_cur_fspart(-1, 1);
-		};
-	option "Zmien a", action { editpart = A;}, sub menu edfspart;
-	option "Zmien b", action { editpart = B;}, sub menu edfspart;
-	option "Caly dysk - nie mozna zmienic", action {};
-	option "Partycja bootowalna - nie mozna zmienic", action {};
-	option "Zmien e", action { editpart = E;}, sub menu edfspart;
-	option "Zmien f", action { editpart = F;}, sub menu edfspart;
-	option "Zmien g", action { editpart = G;}, sub menu edfspart;
-	option "Zmien h", action { editpart = H;}, sub menu edfspart;
-	option "Ustaw nowy przydzial rozmiarow", action { reask_sizemult(dlcylsize); };
- 
 menu md_distcustom, x=26, y=5, exit, title "Wybierz";
 	display action { show_cur_distsets (); };
 	option	"Kernel (GENERIC_INDY)", action { toggle_getit (0); };
