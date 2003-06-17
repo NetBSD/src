bbadc027bcfac456f29f530a83a8ee1aed610f6d
msaitoh <msaitoh@NetBSD.org>
2003-06-17 09:27:08 +0000

    Pull up revisions 1.21 (requested by Ben Harris in ticket #1314):  Don't bring down mc(4) interfaces when they underflow.  Fixes PR#12088.

--- sys/arch/mac68k/dev/if_mc.c
+++ sys/arch/mac68k/dev/if_mc.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: if_mc.c,v 1.19 2002/01/16 06:00:37 thorpej Exp $	*/
+/*	$NetBSD: if_mc.c,v 1.19.10.1 2003/06/17 09:27:08 msaitoh Exp $	*/
 
 /*-
  * Copyright (c) 1997 David Huang <khym@bga.com>
@@ -193,7 +193,6 @@ mcioctl(ifp, cmd, data)
 	struct ifreq *ifr;
 
 	int	s = splnet(), err = 0;
-	int	temp;
 
 	switch (cmd) {
 
@@ -252,9 +251,7 @@ mcioctl(ifp, cmd, data)
 			 * reset the interface to pick up any other changes
 			 * in flags
 			 */
-			temp = ifp->if_flags & IFF_UP;
 			mcreset(sc);
-			ifp->if_flags |= temp;
 			mcstart(ifp);
 		}
 		break;
@@ -271,9 +268,7 @@ mcioctl(ifp, cmd, data)
 			 * Multicast list has changed; set the hardware
 			 * filter accordingly. But remember UP flag!
 			 */
-			temp = ifp->if_flags & IFF_UP;
 			mcreset(sc);
-			ifp->if_flags |= temp;
 			err = 0;
 		}
 		break;
@@ -420,7 +415,7 @@ mcstop(sc)
 	DELAY(100);
 
 	sc->sc_if.if_timer = 0;
-	sc->sc_if.if_flags &= ~(IFF_RUNNING | IFF_UP);
+	sc->sc_if.if_flags &= ~IFF_RUNNING;
 
 	splx(s);
 	return (0);
@@ -436,12 +431,9 @@ mcwatchdog(ifp)
 	struct ifnet *ifp;
 {
 	struct mc_softc *sc = ifp->if_softc;
-	int temp;
 
 	printf("mcwatchdog: resetting chip\n");
-	temp = ifp->if_flags & IFF_UP;
 	mcreset(sc);
-	ifp->if_flags |= temp;
 }
 
 /*
