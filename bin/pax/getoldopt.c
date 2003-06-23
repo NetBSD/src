cf56833ae37642adf4fb1148e7336f1e0ba34655
christos <christos@NetBSD.org>
2003-06-23 13:15:15 +0000

    Simplify the way we deal with old argument style. Old way was broken when tar cf - -T dir

--- bin/pax/getoldopt.c
+++ bin/pax/getoldopt.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: getoldopt.c,v 1.16 2002/10/13 00:32:09 mrg Exp $	*/
+/*	$NetBSD: getoldopt.c,v 1.17 2003/06/23 13:15:15 christos Exp $	*/
 
 /*
  * Plug-compatible replacement for getopt() for parsing tar-like
@@ -11,7 +11,7 @@
 
 #include <sys/cdefs.h>
 #if defined(__RCSID) && !defined(lint)
-__RCSID("$NetBSD: getoldopt.c,v 1.16 2002/10/13 00:32:09 mrg Exp $");
+__RCSID("$NetBSD: getoldopt.c,v 1.17 2003/06/23 13:15:15 christos Exp $");
 #endif /* not lint */
 
 #include <getopt.h>
@@ -28,53 +28,20 @@ getoldopt(int argc, char **argv, const char *optstring,
 	struct option *longopts, int *idx)
 {
 	static char	*key;		/* Points to next keyletter */
-	static char	use_getopt;	/* !=0 if argv[1][0] was '-' */
-	char		c;
-	char		*place;
-
-	optarg = NULL;
+	static char	argv1[64];
 
 	if (key == NULL) {		/* First time */
 		if (argc < 2) return -1;
 		key = argv[1];
-		if (*key == '-')
-			use_getopt++;
-		else
-			optind = 2;
+		if (*key != '-')
+			(void)snprintf(argv[1] = argv1, sizeof(argv1), "-%s",
+			    key);
 	}
 
-	if (use_getopt) {
-		if (longopts != NULL) {
-			return getopt_long(argc, argv, optstring,
-			    longopts, idx);
-		} else {
-			return getopt(argc, argv, optstring);
-		}
+	if (longopts != NULL) {
+		return getopt_long(argc, argv, optstring,
+		    longopts, idx);
+	} else {
+		return getopt(argc, argv, optstring);
 	}
-
-	c = *key++;
-	if (c == '\0') {
-		key--;
-		return -1;
-	}
-	place = strchr(optstring, c);
-
-	if (place == NULL || c == ':') {
-		fprintf(stderr, "%s: unknown option %c\n", argv[0], c);
-		return('?');
-	}
-
-	place++;
-	if (*place == ':') {
-		if (optind < argc) {
-			optarg = argv[optind];
-			optind++;
-		} else {
-			fprintf(stderr, "%s: %c argument missing\n",
-				argv[0], c);
-			return('?');
-		}
-	}
-
-	return(c);
 }
