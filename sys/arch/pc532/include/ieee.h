3fa38a4235a4d943e059a2716451bf86b9f7510d
simonb <simonb@NetBSD.org>
2003-06-23 06:59:12 +0000

    Removed "extended precision" definitions left over from when this was originally copied from i386/include/ieee.h.
    Fixes PR port-pc532/21960 from Jason Thorpe.

--- sys/arch/pc532/include/ieee.h
+++ sys/arch/pc532/include/ieee.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: ieee.h,v 1.1 1999/08/29 18:10:33 mycroft Exp $ */
+/*	$NetBSD: ieee.h,v 1.2 2003/06/23 06:59:12 simonb Exp $ */
 
 /*
  * Copyright (c) 1992, 1993
@@ -84,9 +84,6 @@
 #define	DBL_EXPBITS	11
 #define	DBL_FRACBITS	52
 
-#define	EXT_EXPBITS	15
-#define	EXT_FRACBITS	112
-
 struct ieee_single {
 	u_int	sng_frac:23;
 	u_int	sng_exp:8;
@@ -100,15 +97,6 @@ struct ieee_double {
 	u_int	dbl_sign:1;
 };
 
-struct ieee_ext {
-	u_int	ext_fracl;
-	u_int	ext_fraclm;
-	u_int	ext_frachm;
-	u_int	ext_frach:16;
-	u_int	ext_exp:15;
-	u_int	ext_sign:1;
-};
-
 /*
  * Floats whose exponent is in [1..INFNAN) (of whatever type) are
  * `normal'.  Floats whose exponent is INFNAN are either Inf or NaN.
@@ -120,12 +108,10 @@ struct ieee_ext {
  */
 #define	SNG_EXP_INFNAN	255
 #define	DBL_EXP_INFNAN	2047
-#define	EXT_EXP_INFNAN	32767
 
 #if 0
 #define	SNG_QUIETNAN	(1 << 22)
 #define	DBL_QUIETNAN	(1 << 19)
-#define	EXT_QUIETNAN	(1 << 15)
 #endif
 
 /*
@@ -133,4 +119,3 @@ struct ieee_ext {
  */
 #define	SNG_EXP_BIAS	127
 #define	DBL_EXP_BIAS	1023
-#define	EXT_EXP_BIAS	16383
