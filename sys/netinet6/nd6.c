4d1ef5a6d95e9930e99af78d8f91f64fd1e791b0
itojun <itojun@NetBSD.org>
2003-06-24 07:49:03 +0000

    * kame/sys/netinet6/nd6.c (nd6_rtrequest): changed a condition to decide whether to create an empty llinfo stricter so that a user can manually change the link-layer address of an existing neighbor cache. Pointed out by: KIU Shueng Chuan
    from kame

--- sys/netinet6/nd6.c
+++ sys/netinet6/nd6.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: nd6.c,v 1.83 2003/06/24 07:39:26 itojun Exp $	*/
+/*	$NetBSD: nd6.c,v 1.84 2003/06/24 07:49:03 itojun Exp $	*/
 /*	$KAME: nd6.c,v 1.279 2002/06/08 11:16:51 itojun Exp $	*/
 
 /*
@@ -31,7 +31,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: nd6.c,v 1.83 2003/06/24 07:39:26 itojun Exp $");
+__KERNEL_RCSID(0, "$NetBSD: nd6.c,v 1.84 2003/06/24 07:49:03 itojun Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -1008,12 +1008,13 @@ nd6_rtrequest(req, rt, info)
 		 *     SIN(rt_mask(rt))->sin_addr.s_addr != 0xffffffff)
 		 *	   rt->rt_flags |= RTF_CLONING;
 		 */
-		if (rt->rt_flags & (RTF_CLONING | RTF_LLINFO)) {
+		if ((rt->rt_flags & RTF_CLONING) ||
+		    ((rt->rt_flags & RTF_LLINFO) && !ln)) {
 			/*
-			 * Case 1: This route should come from
-			 * a route to interface.  RTF_LLINFO flag is set
-			 * for a host route whose destination should be
-			 * treated as on-link.
+			 * Case 1: This route should come from a route to
+			 * interface (RTF_CLONING case) or the route should be
+			 * treated as on-link but is currently not
+			 * (RTF_LLINFO && !ln case).
 			 */
 			rt_setgate(rt, rt_key(rt),
 				   (struct sockaddr *)&null_sdl);
