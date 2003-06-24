7115805444a96e80709017e350b4423ce775446f
grant <grant@NetBSD.org>
2003-06-24 09:37:21 +0000

    Pull up revision 1.173 (requested by nakayama in ticket #1330):
    Divide `set' instruction in delay slot (from OpenBSD change to rev
    1.30).

--- sys/arch/sparc64/sparc64/locore.s
+++ sys/arch/sparc64/sparc64/locore.s
@@ -1,4 +1,4 @@
-/*	$NetBSD: locore.s,v 1.153.4.4 2003/06/24 09:34:15 grant Exp $	*/
+/*	$NetBSD: locore.s,v 1.153.4.5 2003/06/24 09:37:21 grant Exp $	*/
 
 /*
  * Copyright (c) 1996-2002 Eduardo Horvath
@@ -2854,10 +2854,11 @@ winfixsave:
 	CHKPT(%g2,%g5,0x16)
 
 	/* Did we save a user or kernel window ? */
-!	srax	%g3, 48, %g7				! User or kernel store? (TAG TARGET)
-	sllx	%g3, (64-13), %g7			! User or kernel store? (TAG ACCESS)
-	brnz,pt	%g7, 1f					! User fault -- save windows to pcb
-	 set	(2*NBPG)-8, %g7
+!	srax	%g3, 48, %g5				! User or kernel store? (TAG TARGET)
+	sllx	%g3, (64-13), %g5			! User or kernel store? (TAG ACCESS)
+	sethi	%hi((2*NBPG)-8), %g7
+	brnz,pt	%g5, 1f					! User fault -- save windows to pcb
+	 or	%g7, %lo((2*NBPG)-8), %g7
 
 	and	%g4, CWP, %g4				! %g4 = %cwp of trap
 	wrpr	%g4, 0, %cwp				! Kernel fault -- restore %cwp and force and trap to debugger
