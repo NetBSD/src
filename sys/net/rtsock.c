11ae0e541422cc795c1a26642f512c58b72fa7e6
msaitoh <msaitoh@NetBSD.org>
2003-06-25 09:26:12 +0000

    Pullup reviosion 1.60 (requested by itojun in ticket #48):   recover code that requires exact match on rtm_change/lock (lost in 1.16).   without it "route change X" would change less-specific route by mistake.   reported by jinmei@kame

--- sys/net/rtsock.c
+++ sys/net/rtsock.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: rtsock.c,v 1.40.4.1 2000/10/19 13:38:13 he Exp $	*/
+/*	$NetBSD: rtsock.c,v 1.40.4.2 2003/06/25 09:26:12 msaitoh Exp $	*/
 
 /*
  * Copyright (C) 1995, 1996, 1997, and 1998 WIDE Project.
@@ -304,6 +304,22 @@ route_output(m, va_alist)
 		}
 		rt = (struct rtentry *)rn;
 		rt->rt_refcnt++;
+		if (rtm->rtm_type != RTM_GET) {/* XXX: too grotty */
+			struct radix_node *rn;
+			extern struct radix_node_head *mask_rnhead;
+
+			if (Bcmp(dst, rt_key(rt), dst->sa_len) != 0)
+				senderr(ESRCH);
+			if (netmask && (rn = rn_search(netmask,
+					    mask_rnhead->rnh_treetop)))
+				netmask = (struct sockaddr *)rn->rn_key;
+			for (rn = rt->rt_nodes; rn; rn = rn->rn_dupedkey)
+				if (netmask == (struct sockaddr *)rn->rn_mask)
+					break;
+			if (rn == 0)
+				senderr(ETOOMANYREFS);
+			rt = (struct rtentry *)rn;
+		}
 
 		switch(rtm->rtm_type) {
 
