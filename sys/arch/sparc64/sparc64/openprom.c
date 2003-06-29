82fa6f9a1b251008ed9c80e84ae0e6bc3ce7eb9f
martin <martin>
2003-06-29 10:37:45 +0000

    struct proc * -> struct lwp *

--- sys/arch/sparc64/sparc64/openprom.c
+++ sys/arch/sparc64/sparc64/openprom.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: openprom.c,v 1.9 2003/05/17 01:38:40 nakayama Exp $ */
+/*	$NetBSD: openprom.c,v 1.10 2003/06/29 10:37:46 martin Exp $ */
 
 /*
  * Copyright (c) 1992, 1993
@@ -76,10 +76,10 @@ static int openpromcheckid __P((int, int));
 static int openpromgetstr __P((int, char *, char **));
 
 int
-openpromopen(dev, flags, mode, p)
+openpromopen(dev, flags, mode, l)
 	dev_t dev;
 	int flags, mode;
-	struct proc *p;
+	struct lwp *l;
 {
 #if defined(SUN4)
 	if (cputyp==CPU_SUN4)
@@ -124,12 +124,12 @@ openpromgetstr(len, user, cpp)
 }
 
 int
-openpromioctl(dev, cmd, data, flags, p)
+openpromioctl(dev, cmd, data, flags, l)
 	dev_t dev;
 	u_long cmd;
 	caddr_t data;
 	int flags;
-	struct proc *p;
+	struct lwp *l;
 {
 	struct opiocdesc *op;
 	int node, len, ok, error, s;
