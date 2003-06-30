8d9a41bd39f997acf36abbf9d1d0d4a1a469a207
tshiozak <tshiozak@NetBSD.org>
2003-06-30 12:32:21 +0000

    add #include "config.h" when these files are built as parts of host tools.

--- lib/libc/citrus/citrus_bcs.c
+++ lib/libc/citrus/citrus_bcs.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: citrus_bcs.c,v 1.1 2003/06/25 09:51:26 tshiozak Exp $	*/
+/*	$NetBSD: citrus_bcs.c,v 1.2 2003/06/30 12:32:21 tshiozak Exp $	*/
 
 /*-
  * Copyright (c)2003 Citrus Project,
@@ -26,9 +26,13 @@
  * SUCH DAMAGE.
  */
 
+#if HAVE_CONFIG_H
+#include "config.h"
+#endif
+
 #include <sys/cdefs.h>
 #if defined(LIBC_SCCS) && !defined(lint)
-__RCSID("$NetBSD: citrus_bcs.c,v 1.1 2003/06/25 09:51:26 tshiozak Exp $");
+__RCSID("$NetBSD: citrus_bcs.c,v 1.2 2003/06/30 12:32:21 tshiozak Exp $");
 #endif /* LIBC_SCCS and not lint */
 
 #ifndef HOSTPROG
