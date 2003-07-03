bc1a4f2cf89cd03b321f6596207986d7a02c500a
wrstuden <wrstuden@NetBSD.org>
2003-07-03 01:32:55 +0000

    LWP-ify union fs.
    Note: These changes suffer from the same cnp->cn_lwp issue noted for
    ufs. They will need to get fixed at the same time as ufs. The fix is to
    add struct lwp * as a parameter to some VOPs.
    
    Note also that most of the cn_lwp references used to be cn_proc references,
    so if cnp->cn_lwp is bad to use, unionfs's been naughty for quite some
    time.

--- sys/fs/union/union_vnops.c
+++ sys/fs/union/union_vnops.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: union_vnops.c,v 1.6 2003/06/29 22:31:14 fvdl Exp $	*/
+/*	$NetBSD: union_vnops.c,v 1.6.2.1 2003/07/03 01:32:56 wrstuden Exp $	*/
 
 /*
  * Copyright (c) 1992, 1993, 1994, 1995 Jan-Simon Pendry.
@@ -40,7 +40,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: union_vnops.c,v 1.6 2003/06/29 22:31:14 fvdl Exp $");
+__KERNEL_RCSID(0, "$NetBSD: union_vnops.c,v 1.6.2.1 2003/07/03 01:32:56 wrstuden Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -239,7 +239,7 @@ union_lookup1(udvp, dvpp, vpp, cnp)
 		if (vfs_busy(mp, 0, 0))
 			continue;
 
-		error = VFS_ROOT(mp, &tdvp);
+		error = VFS_ROOT(mp, &tdvp, cnp->cn_lwp);
 		vfs_unbusy(mp);
 		if (error) {
 			vput(dvp);
@@ -359,7 +359,7 @@ union_lookup(v)
 				iswhiteout = 1;
 			} else if (lowerdvp != NULLVP) {
 				lerror = VOP_GETATTR(upperdvp, &va,
-					cnp->cn_cred, cnp->cn_proc);
+					cnp->cn_cred, cnp->cn_lwp);
 				if (lerror == 0 && (va.va_flags & OPAQUE))
 					iswhiteout = 1;
 			}
@@ -506,7 +506,7 @@ union_lookup(v)
 		VOP_UNLOCK(lowervp, 0);
 
 	error = union_allocvp(ap->a_vpp, dvp->v_mount, dvp, upperdvp, cnp,
-			      uppervp, lowervp, 1);
+			      uppervp, lowervp, 1, cnp->cn_lwp);
 
 	if (error) {
 		if (uppervp != NULLVP)
@@ -558,7 +558,7 @@ union_create(v)
 			return (error);
 
 		error = union_allocvp(ap->a_vpp, mp, NULLVP, NULLVP, cnp, vp,
-				NULLVP, 1);
+				NULLVP, 1, cnp->cn_lwp);
 		if (error)
 			vput(vp);
 		return (error);
@@ -617,7 +617,7 @@ union_mknod(v)
 			return (error);
 
 		error = union_allocvp(ap->a_vpp, mp, NULLVP, NULLVP,
-				      cnp, vp, NULLVP, 1);
+				      cnp, vp, NULLVP, 1, cnp->cn_lwp);
 		if (error)
 		    vput(vp);
 		return (error);
@@ -636,13 +636,13 @@ union_open(v)
 		struct vnode *a_vp;
 		int a_mode;
 		struct ucred *a_cred;
-		struct proc *a_p;
+		struct lwp *a_l;
 	} */ *ap = v;
 	struct union_node *un = VTOUNION(ap->a_vp);
 	struct vnode *tvp;
 	int mode = ap->a_mode;
 	struct ucred *cred = ap->a_cred;
-	struct proc *p = ap->a_p;
+	struct lwp *l = ap->a_l;
 	int error;
 
 	/*
@@ -657,9 +657,9 @@ union_open(v)
 		 */
 		tvp = un->un_lowervp;
 		if ((ap->a_mode & FWRITE) && (tvp->v_type == VREG)) {
-			error = union_copyup(un, (mode&O_TRUNC) == 0, cred, p);
+			error = union_copyup(un, (mode&O_TRUNC) == 0, cred, l);
 			if (error == 0)
-				error = VOP_OPEN(un->un_uppervp, mode, cred, p);
+				error = VOP_OPEN(un->un_uppervp, mode, cred, l);
 			return (error);
 		}
 
@@ -671,7 +671,7 @@ union_open(v)
 			return ENXIO;
 		un->un_openl++;
 		vn_lock(tvp, LK_EXCLUSIVE | LK_RETRY);
-		error = VOP_OPEN(tvp, mode, cred, p);
+		error = VOP_OPEN(tvp, mode, cred, l);
 		VOP_UNLOCK(tvp, 0);
 
 		return (error);
@@ -685,7 +685,7 @@ union_open(v)
 
 	FIXUP(un);
 
-	error = VOP_OPEN(tvp, mode, cred, p);
+	error = VOP_OPEN(tvp, mode, cred, l);
 
 	return (error);
 }
@@ -847,7 +847,7 @@ union_getattr(v)
 		if (un->un_flags & UN_LOCKED)
 			FIXUP(un);
 
-		error = VOP_GETATTR(vp, vap, ap->a_cred, ap->a_p);
+		error = VOP_GETATTR(vp, vap, ap->a_cred, ap->a_l);
 		if (error)
 			return (error);
 		union_newsize(ap->a_vp, vap->va_size, VNOVAL);
@@ -864,7 +864,7 @@ union_getattr(v)
 	}
 
 	if (vp != NULLVP) {
-		error = VOP_GETATTR(vp, vap, ap->a_cred, ap->a_p);
+		error = VOP_GETATTR(vp, vap, ap->a_cred, ap->a_l);
 		if (error)
 			return (error);
 		union_newsize(ap->a_vp, VNOVAL, vap->va_size);
@@ -940,7 +940,7 @@ union_setattr(v)
 	    /* assert(un->un_lowervp != NULLVP) */
 	    (un->un_lowervp->v_type == VREG)) {
 		error = union_copyup(un, (vap->va_size != 0),
-						ap->a_cred, ap->a_p);
+						ap->a_cred, ap->a_l);
 		if (error)
 			return (error);
 	}
@@ -952,7 +952,7 @@ union_setattr(v)
 	if (un->un_uppervp != NULLVP) {
 		FIXUP(un);
 		error = VOP_SETATTR(un->un_uppervp, vap,
-					ap->a_cred, ap->a_p);
+					ap->a_cred, ap->a_l);
 		if ((error == 0) && (vap->va_size != VNOVAL))
 			union_newsize(ap->a_vp, vap->va_size, VNOVAL);
 	} else {
@@ -1138,7 +1138,7 @@ union_fsync(v)
 		struct proc *a_p;
 	} */ *ap = v;
 	int error = 0;
-	struct proc *p;
+	struct lwp *l;
 	struct vnode *targetvp;
 
 	/*
@@ -1152,7 +1152,7 @@ union_fsync(v)
 		return 0;
 	
 	targetvp = OTHERVP(ap->a_vp);
-	p = ap->a_p;
+	l = ap->a_l;
 	
 	if (targetvp != NULLVP) {
 		int dolock = (targetvp == LOWERVP(ap->a_vp));
@@ -1162,7 +1162,7 @@ union_fsync(v)
 		else
 			FIXUP(VTOUNION(ap->a_vp));
 		error = VOP_FSYNC(targetvp, ap->a_cred, ap->a_flags,
-			    ap->a_offlo, ap->a_offhi, p);
+			    ap->a_offlo, ap->a_offhi, l);
 		if (dolock)
 			VOP_UNLOCK(targetvp, 0);
 	}
@@ -1216,7 +1216,7 @@ union_remove(v)
 		un->un_flags |= UN_KLOCK;
 		vput(ap->a_vp);
 
-		if (union_dowhiteout(un, cnp->cn_cred, cnp->cn_proc))
+		if (union_dowhiteout(un, cnp->cn_cred, cnp->cn_lwp))
 			cnp->cn_flags |= DOWHITEOUT;
 		error = VOP_REMOVE(dvp, vp, cnp);
 		if (!error)
@@ -1244,7 +1244,7 @@ union_link(v)
 	} */ *ap = v;
 	int error = 0;
 	struct componentname *cnp = ap->a_cnp;
-	struct proc *p = cnp->cn_proc;
+	struct lwp *l = cnp->cn_lwp;
 	struct union_node *dun;
 	struct vnode *vp;
 	struct vnode *dvp;
@@ -1272,7 +1272,7 @@ union_link(v)
 				dun->un_flags &= ~UN_ULOCK;
 				VOP_UNLOCK(dun->un_uppervp, 0);
 			}
-			error = union_copyup(un, 1, cnp->cn_cred, p);
+			error = union_copyup(un, 1, cnp->cn_cred, l);
 			if (dun->un_uppervp == un->un_dirvp) {
 				/*
 				 * During copyup, we dropped the lock on the
@@ -1456,7 +1456,7 @@ union_mkdir(v)
 		}
 
 		error = union_allocvp(ap->a_vpp, ap->a_dvp->v_mount, ap->a_dvp,
-				NULLVP, cnp, vp, NULLVP, 1);
+				NULLVP, cnp, vp, NULLVP, 1, cnp->cn_lwp);
 		vrele(ap->a_dvp);
 		if (error)
 			vput(vp);
@@ -1497,7 +1497,7 @@ union_rmdir(v)
 		un->un_flags |= UN_KLOCK;
 		vput(ap->a_vp);
 
-		if (union_dowhiteout(un, cnp->cn_cred, cnp->cn_proc))
+		if (union_dowhiteout(un, cnp->cn_cred, cnp->cn_lwp))
 			cnp->cn_flags |= DOWHITEOUT;
 		error = VOP_RMDIR(dvp, vp, ap->a_cnp);
 		if (!error)
