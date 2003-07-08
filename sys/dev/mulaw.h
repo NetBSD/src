25673fd3c5cc041f606fd1c156f0e6f09d5b3da8
itojun <itojun@NetBSD.org>
2003-07-08 09:07:15 +0000

    function prototype must not have variable name

--- sys/dev/mulaw.h
+++ sys/dev/mulaw.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: mulaw.h,v 1.15 2003/04/06 18:20:12 wiz Exp $	*/
+/*	$NetBSD: mulaw.h,v 1.16 2003/07/08 09:07:15 itojun Exp $	*/
 
 /*-
  * Copyright (c) 1996 The NetBSD Foundation, Inc.
@@ -37,29 +37,29 @@
  */
 
 /* Convert 8-bit mu-law to 16 bit unsigned linear. */
-extern void mulaw_to_ulinear16_le(void *, u_char *buf, int cnt);
-extern void mulaw_to_ulinear16_be(void *, u_char *buf, int cnt);
+extern void mulaw_to_ulinear16_le(void *, u_char *, int);
+extern void mulaw_to_ulinear16_be(void *, u_char *, int);
 /* Convert 8-bit mu-law from/to 16 bit signed linear. */
-extern void mulaw_to_slinear16_le(void *, u_char *buf, int cnt);
-extern void mulaw_to_slinear16_be(void *, u_char *buf, int cnt);
-extern void slinear16_to_mulaw_le(void *, u_char *buf, int cnt);
+extern void mulaw_to_slinear16_le(void *, u_char *, int);
+extern void mulaw_to_slinear16_be(void *, u_char *, int);
+extern void slinear16_to_mulaw_le(void *, u_char *, int);
 /* Convert 8-bit mu-law to/from 8 bit unsigned linear. */
-extern void mulaw_to_ulinear8(void *, u_char *buf, int cnt);
-extern void ulinear8_to_mulaw(void *, u_char *buf, int cnt);
+extern void mulaw_to_ulinear8(void *, u_char *, int);
+extern void ulinear8_to_mulaw(void *, u_char *, int);
 /* Convert 8-bit mu-law to/from 8 bit signed linear. */
-extern void mulaw_to_slinear8(void *, u_char *buf, int cnt);
-extern void slinear8_to_mulaw(void *, u_char *buf, int cnt);
+extern void mulaw_to_slinear8(void *, u_char *, int);
+extern void slinear8_to_mulaw(void *, u_char *, int);
 /* Convert 8-bit A-law to 16 bit unsigned linear. */
-extern void alaw_to_ulinear16_le(void *, u_char *buf, int cnt);
-extern void alaw_to_ulinear16_be(void *, u_char *buf, int cnt);
+extern void alaw_to_ulinear16_le(void *, u_char *, int);
+extern void alaw_to_ulinear16_be(void *, u_char *, int);
 /* Convert 8-bit A-law to/from 16 bit signed linear. */
-extern void alaw_to_slinear16_le(void *, u_char *buf, int cnt);
-extern void alaw_to_slinear16_be(void *, u_char *buf, int cnt);
-extern void slinear16_to_alaw_le(void *, u_char *buf, int cnt);
-extern void slinear16_to_alaw_be(void *, u_char *buf, int cnt);
+extern void alaw_to_slinear16_le(void *, u_char *, int);
+extern void alaw_to_slinear16_be(void *, u_char *, int);
+extern void slinear16_to_alaw_le(void *, u_char *, int);
+extern void slinear16_to_alaw_be(void *, u_char *, int);
 /* Convert 8-bit A-law to/from 8 bit unsigned linear. */
-extern void alaw_to_ulinear8(void *, u_char *buf, int cnt);
-extern void ulinear8_to_alaw(void *, u_char *buf, int cnt);
+extern void alaw_to_ulinear8(void *, u_char *, int);
+extern void ulinear8_to_alaw(void *, u_char *, int);
 /* Convert 8-bit A-law to/from 8 bit signed linear. */
-extern void alaw_to_slinear8(void *, u_char *buf, int cnt);
-extern void slinear8_to_alaw(void *, u_char *buf, int cnt);
+extern void alaw_to_slinear8(void *, u_char *, int);
+extern void slinear8_to_alaw(void *, u_char *, int);
