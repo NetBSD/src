0e9c277763c563e6718faf38bf51fb4cab558b33
pk <pk>
2003-07-10 09:00:33 +0000

    pmap_map(): round up size to compute # of pages.

--- sys/arch/sparc/stand/common/mmu.c
+++ sys/arch/sparc/stand/common/mmu.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: mmu.c,v 1.2 2003/06/27 21:27:24 uwe Exp $	*/
+/*	$NetBSD: mmu.c,v 1.3 2003/07/10 09:00:33 pk Exp $	*/
 
 /*-
  * Copyright (c) 2003 The NetBSD Foundation, Inc.
@@ -101,7 +101,7 @@ int mmu_init(void)
 
 int pmap_map4(vaddr_t va, paddr_t pa, psize_t size)
 {
-	u_int n = size >> PGSHIFT;
+	u_int n = (size + NBPG - 1) >> PGSHIFT;
 	u_int pte;
 
 	if (sun4_mmu3l)
@@ -147,7 +147,7 @@ int pmap_extract4(vaddr_t va, paddr_t *ppa)
 int pmap_map_srmmu(vaddr_t va, paddr_t pa, psize_t size)
 {
 	char buf[64];
-	u_int n = size >> PGSHIFT;
+	u_int n = (size + NBPG - 1) >> PGSHIFT;
 
 	if (boothowto & AB_VERBOSE)
 		printf("Mapping %lx -> %lx (%d pages)\n", va, pa, n);
