52c38f7f7424063e0c75edb8e4d1ca807aca8f61
lukem <lukem@NetBSD.org>
2003-07-14 22:32:39 +0000

    add __KERNEL_RCSID()

--- sys/arch/x86/x86/cacheinfo.c
+++ sys/arch/x86/x86/cacheinfo.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: cacheinfo.c,v 1.2 2003/05/14 12:22:34 fvdl Exp $	*/
+/*	$NetBSD: cacheinfo.c,v 1.3 2003/07/14 22:32:39 lukem Exp $	*/
 
 /*-
  * Copyright (c) 2000 The NetBSD Foundation, Inc.
@@ -35,6 +35,9 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
+
+#include <sys/cdefs.h>
+__KERNEL_RCSID(0, "$NetBSD: cacheinfo.c,v 1.3 2003/07/14 22:32:39 lukem Exp $");
 
 #include <sys/types.h>
 #include <sys/systm.h>
