be19e96ee359f2c7ce6e5651911f3846911d2f91
itojun <itojun@NetBSD.org>
2003-07-15 11:00:36 +0000

    simplify and update rijndael code.  markus@openbsd

--- sys/crypto/rijndael/rijndael.h
+++ sys/crypto/rijndael/rijndael.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: rijndael.h,v 1.2 2000/10/02 17:19:15 itojun Exp $	*/
-/*	$KAME: rijndael.h,v 1.2 2000/10/02 17:14:27 itojun Exp $	*/
+/*	$NetBSD: rijndael.h,v 1.3 2003/07/15 11:00:43 itojun Exp $	*/
+/*	$KAME: rijndael.h,v 1.3 2003/07/15 10:47:16 itojun Exp $	*/
 
-#include <crypto/rijndael/rijndael-api-fst.h>
+#include <crypto/rijndael/rijndael-alg-fst.h>
