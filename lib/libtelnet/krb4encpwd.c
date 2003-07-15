427af575fe7c81c6be1c0bf7d68e47c94418b0d7
itojun <itojun@NetBSD.org>
2003-07-15 10:14:54 +0000

    use bounded string op.  bump shlib major for libtelnet due to API change.

--- lib/libtelnet/krb4encpwd.c
+++ lib/libtelnet/krb4encpwd.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: krb4encpwd.c,v 1.3 2003/07/15 05:09:35 itojun Exp $	*/
+/*	$NetBSD: krb4encpwd.c,v 1.4 2003/07/15 10:14:55 itojun Exp $	*/
 
 /*-
  * Copyright (c) 1992, 1993
@@ -38,7 +38,7 @@
 #if 0
 static char sccsid[] = "@(#)krb4encpwd.c	8.3 (Berkeley) 5/30/95";
 #else
-__RCSID("$NetBSD: krb4encpwd.c,v 1.3 2003/07/15 05:09:35 itojun Exp $");
+__RCSID("$NetBSD: krb4encpwd.c,v 1.4 2003/07/15 10:14:55 itojun Exp $");
 #endif
 #endif /* not lint */
 
@@ -341,9 +341,10 @@ krb4encpwd_reply(ap, data, cnt)
 }
 
 	int
-krb4encpwd_status(ap, name, level)
+krb4encpwd_status(ap, name, l, level)
 	Authenticator *ap;
 	char *name;
+	size_t l;
 	int level;
 {
 
@@ -351,7 +352,7 @@ krb4encpwd_status(ap, name, level)
 		return(level);
 
 	if (UserNameRequested && passwdok(UserNameRequested, UserPassword)) {
-		strcpy(name, UserNameRequested);
+		strlcpy(name, UserNameRequested, l);
 		return(AUTH_VALID);
 	} else {
 		return(AUTH_USER);
