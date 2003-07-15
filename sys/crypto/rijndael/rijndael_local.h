da48f0d388a88ed726233fe287a7d4ed000c9826
itojun <itojun@NetBSD.org>
2003-07-15 11:00:36 +0000

    simplify and update rijndael code.  markus@openbsd

--- sys/crypto/rijndael/rijndael_local.h
+++ sys/crypto/rijndael/rijndael_local.h
@@ -1,11 +1,11 @@
-/*	$NetBSD: rijndael_local.h,v 1.2 2000/10/02 17:19:15 itojun Exp $	*/
-/*	$KAME: rijndael_local.h,v 1.3 2000/10/02 17:14:27 itojun Exp $	*/
+/*	$NetBSD: rijndael_local.h,v 1.3 2003/07/15 11:00:43 itojun Exp $	*/
+/*	$KAME: rijndael_local.h,v 1.4 2003/07/15 10:47:16 itojun Exp $	*/
 
 /* the file should not be used from outside */
-typedef u_int8_t		BYTE;
-typedef u_int8_t		word8;	
-typedef u_int16_t		word16;	
-typedef u_int32_t		word32;
+typedef u_int8_t		u8;
+typedef u_int16_t		u16;	
+typedef u_int32_t		u32;
 
-#define MAXKC		RIJNDAEL_MAXKC
-#define MAXROUNDS	RIJNDAEL_MAXROUNDS
+#define MAXKC RIJNDAEL_MAXKC	(256/32)
+#define MAXKB RIJNDAEL_MAXKB	(256/8)
+#define MAXNR RIJNDAEL_MAXNR	14
