9b28f9c625d3a905bc0cc893dda1250926c19dd0
cb <cb@NetBSD.org>
2003-07-16 19:42:11 +0000

    Mark __clockctl_fd close-on-exec to avoid unintentionally leaking time setting privileges across an exec.

--- lib/libc/sys/settimeofday.c
+++ lib/libc/sys/settimeofday.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: settimeofday.c,v 1.4 2003/01/18 11:33:13 thorpej Exp $ */
+/*	$NetBSD: settimeofday.c,v 1.5 2003/07/16 19:42:11 cb Exp $ */
 
 /*
  * Copyright (c) 2001 The NetBSD Foundation, Inc.      
@@ -109,6 +109,7 @@ try_syscall:
 		__clockctl_fd = open(_PATH_CLOCKCTL, O_WRONLY, 0);
 		if (__clockctl_fd == -1)
 			return -1;
+		(void) fcntl(__clockctl_fd, F_SETFD, FD_CLOEXEC);
 	}
 
 	/* 
