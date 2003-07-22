dfd29b093b11fa27bc368bff3da1c62cfb93f394
nathanw <nathanw@NetBSD.org>
2003-07-22 21:26:13 +0000

    Add a test that a cancellation registered while cancellation is enabled is not honored while cancellation is disabled.

---
+++ regress/lib/libpthread/cancel2/cancel2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <unistd.h>
+
+int
+main(void)
+{
+	char str1[] = "You should see this.\n";
+	char str2[] = "You shoukd not see this.\n";
+
+	printf("Cancellation test 2: Self-cancellation and disabling.\n");
+
+
+	pthread_cancel(pthread_self());
+
+	pthread_setcancelstate(PTHREAD_CANCEL_DISABLE, NULL);
+
+	write(STDOUT_FILENO, str1, sizeof(str1));
+
+	pthread_setcancelstate(PTHREAD_CANCEL_ENABLE, NULL);
+
+	write(STDOUT_FILENO, str2, sizeof(str2));
+
+	exit(1);
+}
