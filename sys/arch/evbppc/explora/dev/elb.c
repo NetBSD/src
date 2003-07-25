0b0e493effcc15cfd473e7347ee377c00ad354c2
scw <scw@NetBSD.org>
2003-07-25 11:44:18 +0000

    Switch ibm4xx ports to common powerpc bus_space/bus_dma code. XXX: Walnut PCI support is broken. This will be addressed shortly.

--- sys/arch/evbppc/explora/dev/elb.c
+++ sys/arch/evbppc/explora/dev/elb.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: elb.c,v 1.2 2003/07/15 01:37:36 lukem Exp $	*/
+/*	$NetBSD: elb.c,v 1.3 2003/07/25 11:44:20 scw Exp $	*/
 
 /*-
  * Copyright (c) 2003 The NetBSD Foundation, Inc.
@@ -37,15 +37,16 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: elb.c,v 1.2 2003/07/15 01:37:36 lukem Exp $");
+__KERNEL_RCSID(0, "$NetBSD: elb.c,v 1.3 2003/07/25 11:44:20 scw Exp $");
 
 #include <sys/param.h>
 #include <sys/conf.h>
 #include <sys/device.h>
 #include <sys/systm.h>
+#include <sys/extent.h>
 
 #include <machine/explora.h>
-#define _IBM4XX_BUS_DMA_PRIVATE
+#define _POWERPC_BUS_DMA_PRIVATE
 #include <machine/bus.h>
 
 #include <powerpc/ibm4xx/dcr403cgx.h>
@@ -57,19 +58,62 @@ struct elb_dev {
 	int elb_addr;
 	int elb_addr2;
 	int elb_irq;
+	bus_space_tag_t elb_bt;
 };
 
 static int	elb_match(struct device *, struct cfdata *, void *);
 static void	elb_attach(struct device *, struct device *, void *);
 static int	elb_print(void *, const char *);
 
+static struct powerpc_bus_space elb_tag = {
+	_BUS_SPACE_LITTLE_ENDIAN | _BUS_SPACE_MEM_TYPE | 1,	/* stride 1 */
+	0x00000000,
+	BASE_PCKBC,
+	BASE_PCKBC + 0x6ff
+};
+static char elb_ex_storage[EXTENT_FIXED_STORAGE_SIZE(8)]
+    __attribute__((aligned(8)));
+static int elb_tag_init_done;
+
+static struct powerpc_bus_space elb_fb_tag = {
+	_BUS_SPACE_LITTLE_ENDIAN | _BUS_SPACE_MEM_TYPE,
+	0x00000000,
+	BASE_FB,
+	BASE_FB2 + SIZE_FB - 1
+};
+static char elbfb_ex_storage[EXTENT_FIXED_STORAGE_SIZE(8)]
+    __attribute__((aligned(8)));
+static int elbfb_tag_init_done;
+
+/*
+ * DMA struct, nothing special.
+ */
+static struct powerpc_bus_dma_tag elb_bus_dma_tag = {
+	0,			/* _bounce_thresh */
+	_bus_dmamap_create, 
+	_bus_dmamap_destroy,
+	_bus_dmamap_load,
+	_bus_dmamap_load_mbuf,
+	_bus_dmamap_load_uio,
+	_bus_dmamap_load_raw,
+	_bus_dmamap_unload,
+	_bus_dmamap_sync,
+	_bus_dmamem_alloc,
+	_bus_dmamem_free,
+	_bus_dmamem_map,
+	_bus_dmamem_unmap,
+	_bus_dmamem_mmap,
+	_bus_dma_phys_to_bus_mem_generic,
+	_bus_dma_bus_mem_to_phys_generic,
+};
+
 static struct elb_dev elb_devs[] = {
-	{ "cpu",	0,		0,		-1 },
-	{ "pckbc",	BASE_PCKBC,	BASE_PCKBC2,	31 },
-	{ "com",	BASE_COM,	0,		30 },
-	{ "lpt",	BASE_LPT,	0,		-1 },
-	{ "fb",		BASE_FB,	BASE_FB2,	-1 },
-	{ "le",		BASE_LE,	0,		28 },
+	{ "cpu",	0,		0,		-1, NULL },
+	{ "pckbc",	BASE_PCKBC,	BASE_PCKBC2,	31, &elb_tag },
+	{ "com",	BASE_COM,	0,		30, &elb_tag },
+	{ "lpt",	BASE_LPT,	0,		-1, &elb_tag },
+	{ "fb",		BASE_FB,	BASE_FB2,	-1, &elb_fb_tag },
+	{ "le",		BASE_LE,	0,		28, &elb_fb_tag },
 };
 
 CFATTACH_DECL(elb, sizeof(struct device),
@@ -96,8 +140,8 @@ elb_attach(struct device *parent, struct device *self, void *aux)
 	printf("\n");
 	for (i = 0; i < sizeof(elb_devs)/sizeof(elb_devs[0]); i++) {
 		eaa.elb_name = elb_devs[i].elb_name;
-		eaa.elb_bt = MAKE_BUS_TAG(elb_devs[i].elb_addr);
-		eaa.elb_dmat = &ibm4xx_default_bus_dma_tag;
+		eaa.elb_bt = elb_get_bus_space_tag(elb_devs[i].elb_addr);
+		eaa.elb_dmat = &elb_bus_dma_tag;
 		eaa.elb_base = elb_devs[i].elb_addr;
 		eaa.elb_base2 = elb_devs[i].elb_addr2;
 		eaa.elb_irq = elb_devs[i].elb_irq;
@@ -117,4 +161,29 @@ elb_print(void *aux, const char *pnp)
 		aprint_normal(" irq %d", eaa->elb_irq);
 
 	return (UNCONF);
+}
+
+bus_space_tag_t
+elb_get_bus_space_tag(bus_addr_t addr)
+{
+
+	if ((addr & 0xff000000) == 0x74000000) {
+		if (!elb_tag_init_done) {
+			if (bus_space_init(&elb_tag, "elbtag",
+			    elb_ex_storage, sizeof(elb_ex_storage)))
+				panic("elb_get_bus_space_tag: elb_tag");
+
+			elb_tag_init_done = 1;
+		}
+		return (&elb_tag);
+	} else {
+		if (!elbfb_tag_init_done) {
+			if (bus_space_init(&elb_fb_tag, "elbfbtag",
+			    elbfb_ex_storage, sizeof(elbfb_ex_storage)))
+				panic("elb_get_bus_space_tag: elb_fb_tag");
+
+			elbfb_tag_init_done = 1;
+		}
+		return (&elb_fb_tag);
+	}
 }
