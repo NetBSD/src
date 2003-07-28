2ac045df192d0f4c09d822c834d458ad8390efaf
he <he@NetBSD.org>
2003-07-28 17:36:03 +0000

    Pull up revisions 1.16-1.18 (requested by thorpej in ticket #764):   Several changes/updates to scsictl:    o Allow specification of logical blocksize    o Add 'getcache' and 'setcache' commands    o Add 'tur', 'reserve', 'release', 'start', and 'stop' functions    o Add an 'immediate' option to do formatting in the background    o Fix bug in scsi_command -- value in resets is an int not a      bitmask    o Fix usage of errx() / warnx() and remove unneeded trailing \n

--- sbin/scsictl/scsictl.c
+++ sbin/scsictl/scsictl.c
@@ -1,7 +1,7 @@
-/*	$NetBSD: scsictl.c,v 1.15 2002/04/14 03:24:42 tsutsui Exp $	*/
+/*	$NetBSD: scsictl.c,v 1.15.2.1 2003/07/28 17:36:03 he Exp $	*/
 
 /*-
- * Copyright (c) 1998 The NetBSD Foundation, Inc.
+ * Copyright (c) 1998, 2002 The NetBSD Foundation, Inc.
  * All rights reserved.
  *
  * This code is derived from software contributed to The NetBSD Foundation
@@ -79,13 +79,27 @@ struct	scsi_addr dvaddr;		/* SCSI device's address */
 void	device_format __P((int, char *[]));
 void	device_identify __P((int, char *[]));
 void	device_reassign __P((int, char *[]));
+void	device_release __P((int, char *[]));
+void	device_reserve __P((int, char *[]));
 void	device_reset __P((int, char *[]));
+void	device_start __P((int, char *[]));
+void	device_stop __P((int, char *[]));
+void	device_tur __P((int, char *[]));
+void	device_getcache __P((int, char *[]));
+void	device_setcache __P((int, char *[]));
 
 struct command device_commands[] = {
-	{ "format",	"",			device_format },
+	{ "format",	"[blocksize [immediate]]", 	device_format },
 	{ "identify",	"",			device_identify },
 	{ "reassign",	"blkno [blkno [...]]",	device_reassign },
+	{ "release",	"",			device_release },
+	{ "reserve",	"",			device_reserve },
 	{ "reset",	"",			device_reset },
+	{ "start",	"",			device_start },
+	{ "stop",	"",			device_stop },
+	{ "tur",	"",			device_tur },
+	{ "getcache",	"",			device_getcache },
+	{ "setcache",	"none|r|w|rw [save]",	device_setcache },
 	{ NULL,		NULL,			NULL },
 };
 
@@ -193,39 +207,193 @@ usage()
  * device_format:
  *
  *	Format a direct access device.
- *
- *	XXX Does not handle defect list management or geometry settings.
  */
 void
 device_format(argc, argv)
 	int argc;
 	char *argv[];
 {
+	u_int32_t blksize;
+	int i, j, immediate;
+#define	PC	(65536/10)
+	static int complete[] = {
+	    PC*1, PC*2, PC*3, PC*4, PC*5, PC*6, PC*7, PC*8, PC*9, 65536
+	};
+	char *cp, buffer[64];
+	struct scsipi_sense_data sense;
 	struct scsi_format_unit cmd;
+	struct {
+		struct scsi_format_unit_defect_list_header header;
+		/* optional initialization pattern */
+		/* optional defect list */
+	} dfl;
 	struct {
 		struct scsipi_mode_header header;
 		struct scsi_blk_desc blk_desc;
 		struct page_disk_format format_page;
-	} data;
+	} mode_page;
+	struct {
+		struct scsipi_mode_header header;
+		struct scsi_blk_desc blk_desc;
+	} data_select;
+	
 
-	/* No arguments. */
-	if (argc != 0)
+	/* Blocksize is an optional argument. */
+	if (argc > 2)
 		usage();
+
+	/*
+	 * Loop doing Request Sense to clear any pending Unit Attention.
+	 *
+	 * Multiple conditions may exist on the drive which are returned
+	 * in priority order.
+	 */
+	for (i = 0; i < 8; i++) {
+		scsi_request_sense(fd, &sense, sizeof (sense));
+		if ((j = sense.flags & SSD_KEY) == SKEY_NO_SENSE)
+			break;
+	}
+	/*
+	 * Make sure we cleared any pending Unit Attention
+	 */
+	if (j != SKEY_NO_SENSE) {
+		cp = scsi_decode_sense((const unsigned char *) &sense, 2,
+		    buffer, sizeof (buffer));
+		errx(1, "failed to clean Unit Attention: %s", cp);
+	}
 
 	/*
 	 * Get the DISK FORMAT mode page.  SCSI-2 recommends specifying the
 	 * interleave read from this page in the FORMAT UNIT command.
 	 */
-	scsi_mode_sense(fd, 0x03, 0x00, &data, sizeof(data));
+	scsi_mode_sense(fd, 0x03, 0x00, &mode_page, sizeof(mode_page));
+
+	j = (mode_page.format_page.bytes_s[0] << 8) |
+	    (mode_page.format_page.bytes_s[1]);
+
+	if (j != DEV_BSIZE)
+		printf("current disk sector size: %hd\n", j);
 
 	memset(&cmd, 0, sizeof(cmd));
 
 	cmd.opcode = SCSI_FORMAT_UNIT;
-	memcpy(cmd.interleave, data.format_page.interleave,
+	memcpy(cmd.interleave, mode_page.format_page.interleave,
 	    sizeof(cmd.interleave));
 
-	scsi_command(fd, &cmd, sizeof(cmd), NULL, 0, 6 * 60 * 60 * 1000, 0);
+	/*
+	 * The blocksize on the device is only changed if the user
+	 * specified a new blocksize. If not specified the blocksize
+	 * used for the device will be the Default value in the device.
+	 * We don't specify the number of blocks since the format
+	 * command will always reformat the entire drive.  Also by
+	 * not specifying a block count the drive will reset the
+	 * block count to the maximum available after the format
+	 * completes if the blocksize was changed in the format.
+	 * Finally, the new disk geometry will not but updated on
+	 * the drive in permanent storage until _AFTER_ the format
+	 * completes successfully.
+	 */
+	if (argc > 0) {
+		blksize = strtoul(argv[0], &cp, 10);
+		if (*cp != '\0')
+			errx(1, "invalid block size: %s", argv[0]);
 
+		memset(&data_select, 0, sizeof(data_select));
+
+		data_select.header.blk_desc_len = sizeof(struct scsi_blk_desc);
+		/*
+		 * blklen in desc is 3 bytes with a leading reserved byte
+		 */
+		_lto4b(blksize, &data_select.blk_desc.reserved);
+
+		/*
+		 * Issue Mode Select to modify the device blocksize to be
+		 * used on the Format.  The modified device geometry will
+		 * be stored as Current and Saved Page 3 parameters when
+		 * the Format completes.
+		 */
+		scsi_mode_select(fd, 0, &data_select, sizeof(data_select));
+
+		/*
+		 * Since user specified a specific block size make sure it
+		 * gets stored in the device when the format completes.
+		 *
+		 * Also scrub the defect list back to the manufacturers
+		 * original.
+		 */
+		cmd.flags = SFU_CMPLST | SFU_FMTDATA;
+	}
+
+	memset(&dfl, 0, sizeof(dfl));
+
+	if (argc > 1 && strncmp(argv[1], "imm", 3) == 0) {
+		/*
+		 * Signal target for an immediate return from Format.
+		 *
+		 * We'll poll for completion status.
+		 */
+		dfl.header.flags = DLH_IMMED;
+		immediate = 1;
+	} else {
+		immediate = 0;
+	}
+
+	scsi_command(fd, &cmd, sizeof(cmd), &dfl, sizeof(dfl),
+	    8 * 60 * 60 * 1000, 0);
+
+	/*
+	 * Poll device for completion of Format
+	 */
+	if (immediate) {
+		i = 0;
+		printf("formatting.");
+		fflush(stdout);
+		do {
+			scsireq_t req;
+			struct scsipi_test_unit_ready tcmd;
+
+			memset(&tcmd, 0, sizeof(cmd));
+			tcmd.opcode = TEST_UNIT_READY;
+
+			memset(&req, 0, sizeof(req));
+			memcpy(req.cmd, &tcmd, 6);
+			req.cmdlen = 6;
+			req.timeout = 10000;
+			req.senselen = SENSEBUFLEN;
+
+			if (ioctl(fd, SCIOCCOMMAND, &req) == -1) {
+				err(1, "SCIOCCOMMAND");
+			}
+
+			if (req.retsts == SCCMD_OK) {
+				break;
+			} else if (req.retsts == SCCMD_TIMEOUT) {
+				fprintf(stderr, "%s: SCSI command timed out",
+				    dvname);
+				break;
+			} else if (req.retsts == SCCMD_BUSY) {
+				fprintf(stderr, "%s: device is busy",
+				    dvname);
+				break;
+			} else if (req.retsts != SCCMD_SENSE) {
+				fprintf(stderr,
+				    "%s: device had unknown status %x", dvname,
+				    req.retsts);
+				break;
+			}
+			memcpy(&sense, req.sense, SENSEBUFLEN);
+			if (sense.sense_key_spec_1 == SSD_SCS_VALID) {
+				j = (sense.sense_key_spec_2 << 8) |
+				    (sense.sense_key_spec_3);
+				if (j >= complete[i]) {
+					printf(".%d0%%.", ++i);
+					fflush(stdout);
+				}
+			}
+			sleep(10);
+		} while ((sense.flags & SSD_KEY) == SKEY_NOT_READY);
+		printf(".100%%..done.\n");
+	}
 	return;
 }
 
@@ -331,6 +499,64 @@ device_reassign(argc, argv)
 }
 
 /*
+ * device_release:
+ *
+ *      Issue a RELEASE command to a SCSI drevice
+ */
+#ifndef	SCSI_RELEASE
+#define	SCSI_RELEASE	0x17
+#endif
+void
+device_release(argc, argv)
+	int argc;
+	char *argv[];
+{
+	struct scsipi_test_unit_ready cmd;	/* close enough */
+
+	/* No arguments. */
+	if (argc != 0)
+		usage();
+
+	memset(&cmd, 0, sizeof(cmd));
+
+	cmd.opcode = SCSI_RELEASE;
+
+	scsi_command(fd, &cmd, sizeof(cmd), NULL, 0, 10000, 0);
+
+	return;
+}
+
+
+
+/*
+ * device_reserve:
+ *
+ *      Issue a RESERVE command to a SCSI drevice
+ */
+#ifndef	SCSI_RESERVE
+#define	SCSI_RESERVE	0x16
+#endif
+void
+device_reserve(argc, argv)
+	int argc;
+	char *argv[];
+{
+	struct scsipi_test_unit_ready cmd;	/* close enough */
+
+	/* No arguments. */
+	if (argc != 0)
+		usage();
+
+	memset(&cmd, 0, sizeof(cmd));
+
+	cmd.opcode = SCSI_RESERVE;
+
+	scsi_command(fd, &cmd, sizeof(cmd), NULL, 0, 10000, 0);
+
+	return;
+}
+
+/*
  * device_reset:
  *
  *	Issue a reset to a SCSI device.
@@ -347,6 +573,174 @@ device_reset(argc, argv)
 
 	if (ioctl(fd, SCIOCRESET, NULL) != 0)
 		err(1, "SCIOCRESET");
+
+	return;
+}
+
+/*
+ * device_getcache:
+ *
+ *	Get the caching parameters for a SCSI disk.
+ */
+void
+device_getcache(argc, argv)
+	int argc;
+	char *argv[];
+{
+	struct {
+		struct scsipi_mode_header header;
+		struct scsi_blk_desc blk_desc;
+		struct page_caching caching_params;
+	} data;
+
+	/* No arguments. */
+	if (argc != 0)
+		usage();
+
+	scsi_mode_sense(fd, 0x08, 0x00, &data, sizeof(data));
+
+	if ((data.caching_params.flags & (CACHING_RCD|CACHING_WCE)) ==
+	    CACHING_RCD)
+		printf("%s: no caches enabled\n", dvname);
+	else {
+		printf("%s: read cache %senabled\n", dvname,
+		    (data.caching_params.flags & CACHING_RCD) ? "not " : "");
+		printf("%s: write-back cache %senabled\n", dvname,
+		    (data.caching_params.flags & CACHING_WCE) ? "" : "not ");
+	}
+	printf("%s: caching parameters are %ssavable\n", dvname,
+	    (data.caching_params.pg_code & PGCODE_PS) ? "" : "not ");
+}
+
+/*
+ * device_setcache:
+ *
+ *	Set cache enables for a SCSI disk.
+ */
+void
+device_setcache(argc, argv)
+	int argc;
+	char *argv[];
+{
+	struct {
+		struct scsipi_mode_header header;
+		struct scsi_blk_desc blk_desc;
+		struct page_caching caching_params;
+	} data;
+	int dlen;
+	u_int8_t flags, byte2;
+
+	if (argc > 2 || argc == 0)
+		usage();
+
+	if (strcmp(argv[0], "none") == 0)
+		flags = CACHING_RCD;
+	else if (strcmp(argv[0], "r") == 0)
+		flags = 0;
+	else if (strcmp(argv[0], "w") == 0)
+		flags = CACHING_RCD|CACHING_WCE;
+	else if (strcmp(argv[0], "rw") == 0)
+		flags = CACHING_WCE;
+	else
+		usage();
+
+	if (argc == 2) {
+		if (strcmp(argv[1], "save") == 0)
+			byte2 = SMS_SP;
+		else
+			usage();
+	}
+
+	scsi_mode_sense(fd, 0x08, 0x00, &data, sizeof(data));
+
+	data.caching_params.pg_code &= PGCODE_MASK;
+	data.caching_params.flags =
+	    (data.caching_params.flags & ~(CACHING_RCD|CACHING_WCE)) | flags;
+
+	data.caching_params.cache_segment_size[0] = 0;
+	data.caching_params.cache_segment_size[1] = 0;
+
+	data.header.data_length = 0;
+
+	dlen = sizeof(data.header) + sizeof(data.blk_desc) + 2 +
+	    data.caching_params.pg_length;
+
+	scsi_mode_select(fd, byte2, &data, dlen);
+}
+
+/*
+ * device_start:
+ *
+ *      Issue a start to a SCSI device.
+ */
+void
+device_start(argc, argv)
+	int argc;
+	char *argv[];
+{
+	struct scsipi_start_stop cmd;
+
+	/* No arguments. */
+	if (argc != 0)
+		usage();
+
+	memset(&cmd, 0, sizeof(cmd));
+
+	cmd.opcode = START_STOP;
+	cmd.how = SSS_START;
+
+	scsi_command(fd, &cmd, sizeof(cmd), NULL, 0, 10000, 0);
+
+	return;
+}
+
+/*
+ * device_stop:
+ *
+ *      Issue a stop to a SCSI device.
+ */
+void
+device_stop(argc, argv)
+	int argc;
+	char *argv[];
+{
+	struct scsipi_start_stop cmd;
+
+	/* No arguments. */
+	if (argc != 0)
+		usage();
+
+	memset(&cmd, 0, sizeof(cmd));
+
+	cmd.opcode = START_STOP;
+	cmd.how = SSS_STOP;
+
+	scsi_command(fd, &cmd, sizeof(cmd), NULL, 0, 10000, 0);
+
+	return;
+}
+
+/*
+ * device_tur:
+ *
+ *      Issue a TEST UNIT READY to a SCSI drevice
+ */
+void
+device_tur(argc, argv)
+	int argc;
+	char *argv[];
+{
+	struct scsipi_test_unit_ready cmd;
+
+	/* No arguments. */
+	if (argc != 0)
+		usage();
+
+	memset(&cmd, 0, sizeof(cmd));
+
+	cmd.opcode = TEST_UNIT_READY;
+
+	scsi_command(fd, &cmd, sizeof(cmd), NULL, 0, 10000, 0);
 
 	return;
 }
@@ -437,7 +831,7 @@ bus_detach(argc, argv)
 	else {
 		args.sa_target = strtol(argv[0], &cp, 10);
 		if (*cp != '\0' || args.sa_target < 0)
-			errx(1, "invalid target: %s\n", argv[0]);
+			errx(1, "invalid target: %s", argv[0]);
 	}
 
 	if (strcmp(argv[1], "any") == 0 || strcmp(argv[1], "all") == 0)
@@ -445,7 +839,7 @@ bus_detach(argc, argv)
 	else {
 		args.sa_lun = strtol(argv[1], &cp, 10);
 		if (*cp != '\0' || args.sa_lun < 0)
-			errx(1, "invalid lun: %s\n", argv[1]);
+			errx(1, "invalid lun: %s", argv[1]);
 	}
 
 	if (ioctl(fd, SCBUSIODETACH, &args) != 0)
