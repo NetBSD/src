56d71e1a2a53e8a8adf20adeefdb6cec9c41e704
lukem <lukem@NetBSD.org>
2003-07-28 02:38:33 +0000

    Be consistent how "multiple include protection" is implemented.  I.e,
    	.if !defined(_BSD_foo_MK_)
    	_BSD_foo_MK_=1
    
    	# ...
    
    	.endif	# !defined(_BSD_foo_MK_)

--- share/mk/bsd.init.mk
+++ share/mk/bsd.init.mk
@@ -1,11 +1,13 @@
-#	$NetBSD: bsd.init.mk,v 1.1 2001/11/02 05:21:50 tv Exp $
+#	$NetBSD: bsd.init.mk,v 1.2 2003/07/28 02:38:33 lukem Exp $
 
 # <bsd.init.mk> includes Makefile.inc and <bsd.own.mk>; this is used at the
 # top of all <bsd.*.mk> files which actually "build something".
 
-.if !target(__initialized__)
-__initialized__:
+.if !defined(_BSD_INIT_MK_)
+_BSD_INIT_MK_=1
+
 .-include "${.CURDIR}/../Makefile.inc"
 .include <bsd.own.mk>
 .MAIN:		all
-.endif
+
+.endif	# !defined(_BSD_INIT_MK_)
