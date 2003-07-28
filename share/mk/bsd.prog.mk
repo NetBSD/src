f9d69e17f13fdddc0a70d341a7ad8972c4e889c4
lukem <lukem@NetBSD.org>
2003-07-28 08:53:53 +0000

    replace  MKCXXSUPCXX(!=no)  with  USE_LIBSTDCXX(==no)

--- share/mk/bsd.prog.mk
+++ share/mk/bsd.prog.mk
@@ -1,4 +1,4 @@
-#	$NetBSD: bsd.prog.mk,v 1.176 2003/07/28 07:03:44 lukem Exp $
+#	$NetBSD: bsd.prog.mk,v 1.177 2003/07/28 08:53:55 lukem Exp $
 #	@(#)bsd.prog.mk	8.2 (Berkeley) 4/2/94
 
 .ifndef HOSTPROG
@@ -120,7 +120,7 @@ _PROGLDOPTS+=	-Wl,-rpath-link,${DESTDIR}${SHLIBINSTALLDIR}:${DESTDIR}/usr/lib \
 
 .if defined(PROG_CXX)
 _CCLINK=	${CXX}
-.if ${MKCXXSUPCXX} != "no"
+.if ${USE_LIBSTDCXX} == "no"
 _SUPCXX=	-lsupc++ -lm
 .else
 _SUPCXX=	-lstdc++ -lm
