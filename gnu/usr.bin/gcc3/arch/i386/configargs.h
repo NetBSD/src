2c5b77b2d33a948ee0aa7759fb81f9f852c6697d
mrg <mrg@NetBSD.org>
2003-07-30 05:33:25 +0000

    - find gthr-default.h for libgcc3 - adapt for gcc3/libiberty change - regenerate

--- gnu/usr.bin/gcc3/arch/i386/configargs.h
+++ gnu/usr.bin/gcc3/arch/i386/configargs.h
@@ -2,5 +2,5 @@
 /* Generated from: 	NetBSD: mknative-gcc,v 1.3 2003/07/28 02:35:43 mrg Exp  */
 
 /* Generated automatically. */
-static const char configuration_arguments[] = "/home/mrg/src/netbsd/src/tools/gcc/../../gnu/dist/gcc/configure --disable-multilib --enable-threads --build=i386-unknown-netbsdelf --host=i386--netbsdelf --target=i386--netbsdelf : (reconfigured) /home/mrg/src/netbsd/src/tools/gcc/../../gnu/dist/gcc/configure --disable-multilib --enable-threads --build=i386-unknown-netbsdelf1.6U --host=i386--netbsdelf --target=i386--netbsdelf";
+static const char configuration_arguments[] = "/home/mrg/src/netbsd/src/tools/gcc/../../gnu/dist/gcc/configure --disable-multilib --enable-threads --build=i386-unknown-netbsdelf --host=i386--netbsdelf --target=i386--netbsdelf";
 static const char thread_model[] = "posix";
