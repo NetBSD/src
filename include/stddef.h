7724b00ca7855886830cead01e9b6fee54765a1a
agc <agc>
2003-08-07 09:44:09 +0000

    Move UCB-licensed code from 4-clause to 3-clause licence.
    Patches provided by Joel Baker in PR 22270, verified by myself.

--- include/stddef.h
+++ include/stddef.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: stddef.h,v 1.9 2000/11/10 06:35:45 tshiozak Exp $	*/
+/*	$NetBSD: stddef.h,v 1.10 2003/08/07 09:44:11 agc Exp $	*/
 
 /*-
  * Copyright (c) 1990, 1993
@@ -12,11 +12,7 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed by the University of
- *	California, Berkeley and its contributors.
- * 4. Neither the name of the University nor the names of its contributors
+ * 3. Neither the name of the University nor the names of its contributors
  *    may be used to endorse or promote products derived from this software
  *    without specific prior written permission.
  *
