af85b0e3cc387b602a85cc0b31547b69a3a8096a
agc <agc@NetBSD.org>
2003-08-07 09:27:56 +0000

    Move UCB-licensed code from 4-clause to 3-clause licence.
    Patches provided by Joel Baker in PR 22268, verified by myself.

--- distrib/utils/libhack/getcap.c
+++ distrib/utils/libhack/getcap.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: getcap.c,v 1.5 2003/08/06 13:43:31 itojun Exp $	*/
+/*	$NetBSD: getcap.c,v 1.6 2003/08/07 09:27:57 agc Exp $	*/
 
 /*-
  * Copyright (c) 1992, 1993
@@ -15,11 +15,7 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed by the University of
- *	California, Berkeley and its contributors.
- * 4. Neither the name of the University nor the names of its contributors
+ * 3. Neither the name of the University nor the names of its contributors
  *    may be used to endorse or promote products derived from this software
  *    without specific prior written permission.
  *
@@ -46,7 +42,7 @@
 #if 0
 static char sccsid[] = "@(#)getcap.c	8.3 (Berkeley) 3/25/94";
 #else
-__RCSID("$NetBSD: getcap.c,v 1.5 2003/08/06 13:43:31 itojun Exp $");
+__RCSID("$NetBSD: getcap.c,v 1.6 2003/08/07 09:27:57 agc Exp $");
 #endif
 #endif /* LIBC_SCCS and not lint */
 
