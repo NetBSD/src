bba8003938f337bf1195c11b06326274a98698a3
agc <agc>
2003-08-07 09:27:56 +0000

    Move UCB-licensed code from 4-clause to 3-clause licence.
    Patches provided by Joel Baker in PR 22268, verified by myself.

--- distrib/utils/libhack/getnet.c
+++ distrib/utils/libhack/getnet.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: getnet.c,v 1.2 2001/06/15 17:26:51 tsutsui Exp $	*/
+/*	$NetBSD: getnet.c,v 1.3 2003/08/07 09:27:57 agc Exp $	*/
 
 /* Copyright (c) 1993 Carlos Leandro and Rui Salgueiro
  *	Dep. Matematica Universidade de Coimbra, Portugal, Europe
@@ -19,11 +19,7 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed by the University of
- *	California, Berkeley and its contributors.
- * 4. Neither the name of the University nor the names of its contributors
+ * 3. Neither the name of the University nor the names of its contributors
  *    may be used to endorse or promote products derived from this software
  *    without specific prior written permission.
  *
