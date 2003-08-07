58b842d24a0c0fd49319782d7ab20d967d17d402
agc <agc@NetBSD.org>
2003-08-07 09:36:50 +0000

    Move UCB-licensed code from 4-clause to 3-clause licence.
    Patches provided by Joel Baker in PR 22269, verified by myself.

--- games/canfield/canfield/canfield.c
+++ games/canfield/canfield/canfield.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: canfield.c,v 1.17 2000/05/08 07:56:02 mycroft Exp $	*/
+/*	$NetBSD: canfield.c,v 1.18 2003/08/07 09:37:07 agc Exp $	*/
 
 /*
  * Copyright (c) 1980, 1993
@@ -12,11 +12,7 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed by the University of
- *	California, Berkeley and its contributors.
- * 4. Neither the name of the University nor the names of its contributors
+ * 3. Neither the name of the University nor the names of its contributors
  *    may be used to endorse or promote products derived from this software
  *    without specific prior written permission.
  *
@@ -43,7 +39,7 @@ __COPYRIGHT("@(#) Copyright (c) 1980, 1993\n\
 #if 0
 static char sccsid[] = "@(#)canfield.c	8.1 (Berkeley) 5/31/93";
 #else
-__RCSID("$NetBSD: canfield.c,v 1.17 2000/05/08 07:56:02 mycroft Exp $");
+__RCSID("$NetBSD: canfield.c,v 1.18 2003/08/07 09:37:07 agc Exp $");
 #endif
 #endif /* not lint */
 
