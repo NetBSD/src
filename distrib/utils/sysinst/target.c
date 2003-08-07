74b7d6e9f2ef3862080193e82ba2531e60ffd8a4
agc <agc@NetBSD.org>
2003-08-07 09:27:56 +0000

    Move UCB-licensed code from 4-clause to 3-clause licence.
    Patches provided by Joel Baker in PR 22268, verified by myself.

--- distrib/utils/sysinst/target.c
+++ distrib/utils/sysinst/target.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: target.c,v 1.41 2003/08/06 13:56:59 itojun Exp $	*/
+/*	$NetBSD: target.c,v 1.42 2003/08/07 09:28:01 agc Exp $	*/
 
 /*
  * Copyright 1997 Jonathan Stone
@@ -51,11 +51,7 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *      This product includes software developed by the University of
- *      California, Berkeley and its contributors.
- * 4. Neither the name of the University nor the names of its contributors
+ * 3. Neither the name of the University nor the names of its contributors
  *    may be used to endorse or promote products derived from this software
  *    without specific prior written permission. 
  * 
@@ -75,7 +71,7 @@
 
 #include <sys/cdefs.h>
 #if defined(LIBC_SCCS) && !defined(lint)
-__RCSID("$NetBSD: target.c,v 1.41 2003/08/06 13:56:59 itojun Exp $");
+__RCSID("$NetBSD: target.c,v 1.42 2003/08/07 09:28:01 agc Exp $");
 #endif
 
 /*
