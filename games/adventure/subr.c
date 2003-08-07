001f039446bd94ad9b38d324a3e0df32d986f2dc
agc <agc>
2003-08-07 09:36:50 +0000

    Move UCB-licensed code from 4-clause to 3-clause licence.
    Patches provided by Joel Baker in PR 22269, verified by myself.

--- games/adventure/subr.c
+++ games/adventure/subr.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: subr.c,v 1.9 2000/07/03 03:57:39 matt Exp $	*/
+/*	$NetBSD: subr.c,v 1.10 2003/08/07 09:36:51 agc Exp $	*/
 
 /*-
  * Copyright (c) 1991, 1993
@@ -17,11 +17,7 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed by the University of
- *	California, Berkeley and its contributors.
- * 4. Neither the name of the University nor the names of its contributors
+ * 3. Neither the name of the University nor the names of its contributors
  *    may be used to endorse or promote products derived from this software
  *    without specific prior written permission.
  *
@@ -43,7 +39,7 @@
 #if 0
 static char sccsid[] = "@(#)subr.c	8.1 (Berkeley) 5/31/93";
 #else
-__RCSID("$NetBSD: subr.c,v 1.9 2000/07/03 03:57:39 matt Exp $");
+__RCSID("$NetBSD: subr.c,v 1.10 2003/08/07 09:36:51 agc Exp $");
 #endif
 #endif				/* not lint */
 
