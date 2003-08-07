2f6078c925885e00731264e179496de690bb4781
agc <agc>
2003-08-07 09:05:01 +0000

    Move UCB-licensed code from 4-clause to 3-clause licence.
    Patches provided by Joel Baker in PR 22249, verified by myself.

--- bin/rmdir/rmdir.c
+++ bin/rmdir/rmdir.c
@@ -1,4 +1,4 @@
-/* $NetBSD: rmdir.c,v 1.18 2003/08/04 22:31:25 jschauma Exp $ */
+/* $NetBSD: rmdir.c,v 1.19 2003/08/07 09:05:29 agc Exp $ */
 
 /*-
  * Copyright (c) 1992, 1993, 1994
@@ -12,11 +12,7 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed by the University of
- *	California, Berkeley and its contributors.
- * 4. Neither the name of the University nor the names of its contributors
+ * 3. Neither the name of the University nor the names of its contributors
  *    may be used to endorse or promote products derived from this software
  *    without specific prior written permission.
  *
@@ -43,7 +39,7 @@ __COPYRIGHT("@(#) Copyright (c) 1992, 1993, 1994\n\
 #if 0
 static char sccsid[] = "@(#)rmdir.c	8.3 (Berkeley) 4/2/94";
 #else
-__RCSID("$NetBSD: rmdir.c,v 1.18 2003/08/04 22:31:25 jschauma Exp $");
+__RCSID("$NetBSD: rmdir.c,v 1.19 2003/08/07 09:05:29 agc Exp $");
 #endif
 #endif /* not lint */
 
