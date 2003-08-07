bd70d51c73ef5862947d456d15a3f0886aa5d593
agc <agc@NetBSD.org>
2003-08-07 09:20:39 +0000

    Move UCB-licensed code from 4-clause to 3-clause licence.
    Patches provided by Joel Baker in PR 22253, verified by myself.

--- dist/bind/bin/named/ns_init.c
+++ dist/bind/bin/named/ns_init.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: ns_init.c,v 1.5 2003/06/03 07:33:34 itojun Exp $	*/
+/*	$NetBSD: ns_init.c,v 1.6 2003/08/07 09:20:45 agc Exp $	*/
 
 #if !defined(lint) && !defined(SABER)
 static const char sccsid[] = "@(#)ns_init.c	4.38 (Berkeley) 3/21/91";
@@ -17,11 +17,7 @@ static const char rcsid[] = "Id: ns_init.c,v 8.77 2002/08/20 04:27:23 marka Exp"
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- * 	This product includes software developed by the University of
- * 	California, Berkeley and its contributors.
- * 4. Neither the name of the University nor the names of its contributors
+ * 3. Neither the name of the University nor the names of its contributors
  *    may be used to endorse or promote products derived from this software
  *    without specific prior written permission.
  * 
