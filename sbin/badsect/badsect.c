6bc83133a0d713de76626b303928b1c18b9868ad
agc <agc@NetBSD.org>
2003-08-07 10:04:11 +0000

    Move UCB-licensed code from 4-clause to 3-clause licence.
    Patches provided by Joel Baker in PR 22308, verified by myself.

--- sbin/badsect/badsect.c
+++ sbin/badsect/badsect.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: badsect.c,v 1.23 2003/07/13 08:23:39 itojun Exp $	*/
+/*	$NetBSD: badsect.c,v 1.24 2003/08/07 10:04:11 agc Exp $	*/
 
 /*
  * Copyright (c) 1981, 1983, 1993
@@ -12,11 +12,7 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed by the University of
- *	California, Berkeley and its contributors.
- * 4. Neither the name of the University nor the names of its contributors
+ * 3. Neither the name of the University nor the names of its contributors
  *    may be used to endorse or promote products derived from this software
  *    without specific prior written permission.
  *
@@ -43,7 +39,7 @@ __COPYRIGHT("@(#) Copyright (c) 1981, 1983, 1993\n\
 #if 0
 static char sccsid[] = "@(#)badsect.c	8.2 (Berkeley) 5/4/95";
 #else
-__RCSID("$NetBSD: badsect.c,v 1.23 2003/07/13 08:23:39 itojun Exp $");
+__RCSID("$NetBSD: badsect.c,v 1.24 2003/08/07 10:04:11 agc Exp $");
 #endif
 #endif /* not lint */
 
