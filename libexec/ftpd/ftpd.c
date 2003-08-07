d6f3920f10492cf090ef407a0c1ac5842905753f
agc <agc@NetBSD.org>
2003-08-07 09:46:37 +0000

    Move UCB-licensed code from 4-clause to 3-clause licence.
    Patches provided by Joel Baker in PR 22284, verified by myself.

--- libexec/ftpd/ftpd.c
+++ libexec/ftpd/ftpd.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: ftpd.c,v 1.155 2003/06/30 03:06:06 tacha Exp $	*/
+/*	$NetBSD: ftpd.c,v 1.156 2003/08/07 09:46:39 agc Exp $	*/
 
 /*
  * Copyright (c) 1997-2003 The NetBSD Foundation, Inc.
@@ -48,11 +48,7 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed by the University of
- *	California, Berkeley and its contributors.
- * 4. Neither the name of the University nor the names of its contributors
+ * 3. Neither the name of the University nor the names of its contributors
  *    may be used to endorse or promote products derived from this software
  *    without specific prior written permission.
  *
@@ -109,7 +105,7 @@ __COPYRIGHT(
 #if 0
 static char sccsid[] = "@(#)ftpd.c	8.5 (Berkeley) 4/28/95";
 #else
-__RCSID("$NetBSD: ftpd.c,v 1.155 2003/06/30 03:06:06 tacha Exp $");
+__RCSID("$NetBSD: ftpd.c,v 1.156 2003/08/07 09:46:39 agc Exp $");
 #endif
 #endif /* not lint */
 
