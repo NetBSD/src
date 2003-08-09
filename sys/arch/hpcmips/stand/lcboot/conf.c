f8e9de83b80144b2196857f39f63e227f5773058
igy <igy@NetBSD.org>
2003-08-09 08:01:42 +0000

    netboot support

--- sys/arch/hpcmips/stand/lcboot/conf.c
+++ sys/arch/hpcmips/stand/lcboot/conf.c
@@ -1,11 +1,8 @@
-/* $NetBSD: conf.c,v 1.1 2003/06/24 12:27:03 igy Exp $ */
+/* $NetBSD: conf.c,v 1.2 2003/08/09 08:01:43 igy Exp $ */
 
 /*
- * Copyright (c) 2003 The NetBSD Foundation, Inc.
+ * Copyright (c) 2003 Naoto Shimazaki.
  * All rights reserved.
- *
- * This code is derived from software contributed to The NetBSD Foundation
- * by Naoto Shimazaki.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -15,30 +12,26 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *        This product includes software developed by the NetBSD
- *        Foundation, Inc. and its contributors.
- * 4. Neither the name of The NetBSD Foundation nor the names of its
- *    contributors may be used to endorse or promote products derived
- *    from this software without specific prior written permission.
  *
- * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
- * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
- * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS
- * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * THIS SOFTWARE IS PROVIDED BY NAOTO SHIMAZAKI AND CONTRIBUTORS ``AS IS''
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE NAOTO OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
  * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
  * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
  * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
  * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
  */
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: conf.c,v 1.1 2003/06/24 12:27:03 igy Exp $");
+__KERNEL_RCSID(0, "$NetBSD: conf.c,v 1.2 2003/08/09 08:01:43 igy Exp $");
+
+#include <netinet/in.h>
 
 #include <lib/libsa/stand.h>
+#include <lib/libsa/netif.h>
 #include <lib/libsa/tftp.h>
 
 #include "extern.h"
@@ -51,8 +44,29 @@ struct devsw	devsw[] = {
 		.dv_close	= flash_close,
 		.dv_ioctl	= flash_ioctl,
 	},
+	{
+		.dv_name	= "net",
+		.dv_strategy	= net_strategy,
+		.dv_open	= net_open,
+		.dv_close	= net_close,
+		.dv_ioctl	= net_ioctl,
+	},
 };
 int	ndevs = sizeof devsw / sizeof devsw[0];
 
-struct fs_ops	file_system[1];
-int		nfsys = 0;
+struct fs_ops	file_system[] = {
+	{
+		.open		= tftp_open,
+		.close		= tftp_close,
+		.read		= tftp_read,
+		.write		= tftp_write,
+		.seek		= tftp_seek,
+		.stat		= tftp_stat,
+	}
+};
+int	nfsys = sizeof file_system / sizeof file_system[0];
+
+struct netif_driver	*netif_drivers[] = {
+	&cs_driver,
+};
+int n_netif_drivers = sizeof netif_drivers / sizeof netif_drivers[0];
