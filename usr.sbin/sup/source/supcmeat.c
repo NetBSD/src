45d1a5d9aee6f476cf7cd5ec2cb4b34bba96207b
msaitoh <msaitoh@NetBSD.org>
2003-08-11 18:32:37 +0000

    Pull up rev. 1.28 via patch (requested by itojun in ticket #60):  Fix debian bug id "sup 1.8-8, bug #175327" - temporary file symlink race

--- usr.sbin/sup/source/supcmeat.c
+++ usr.sbin/sup/source/supcmeat.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: supcmeat.c,v 1.21 1999/08/02 05:36:05 erh Exp $	*/
+/*	$NetBSD: supcmeat.c,v 1.21.8.1 2003/08/11 18:32:37 msaitoh Exp $	*/
 
 /*
  * Copyright (c) 1992 Carnegie Mellon University
@@ -1137,40 +1137,40 @@ char *from;		/* 0 if reading from network */
 	/* try destination directory */
 		path (to,dpart,fpart);
 		(void) sprintf (tname,"%s/#%d.sup",dpart,thispid);
-		tof = open (tname,(O_WRONLY|O_CREAT|O_TRUNC),0600);
+		tof = open (tname,(O_WRONLY|O_CREAT|O_TRUNC|O_EXCL),0600);
 		if (tof >= 0)  break;
 	/* try sup directory */
 		if (thisC->Cprefix)  (void) chdir (thisC->Cbase);
 		(void) sprintf (tname,"sup/#%d.sup",thispid);
-		tof = open (tname,(O_WRONLY|O_CREAT|O_TRUNC),0600);
+		tof = open (tname,(O_WRONLY|O_CREAT|O_TRUNC|O_EXCL),0600);
 		if (tof >= 0) {
 			if (thisC->Cprefix)  (void) chdir (thisC->Cprefix);
 			break;
 		}
 	/* try base directory */
 		(void) sprintf (tname,"#%d.sup",thispid);
-		tof = open (tname,(O_WRONLY|O_CREAT|O_TRUNC),0600);
+		tof = open (tname,(O_WRONLY|O_CREAT|O_TRUNC|O_EXCL),0600);
 		if (thisC->Cprefix)  (void) chdir (thisC->Cprefix);
 		if (tof >= 0)  break;
 #ifdef	VAR_TMP
 	/* try /var/tmp */
 		(void) sprintf (tname,"/var/tmp/#%d.sup",thispid);
-		tof = open (tname,(O_WRONLY|O_CREAT|O_TRUNC),0600);
+		tof = open (tname,(O_WRONLY|O_CREAT|O_TRUNC|O_EXCL),0600);
 		if (tof >= 0)  break;
 #else
 	/* try /usr/tmp */
 		(void) sprintf (tname,"/usr/tmp/#%d.sup",thispid);
-		tof = open (tname,(O_WRONLY|O_CREAT|O_TRUNC),0600);
+		tof = open (tname,(O_WRONLY|O_CREAT|O_TRUNC|O_EXCL),0600);
 		if (tof >= 0)  break;
 #endif
 	/* try /tmp */
 		(void) sprintf (tname,"/tmp/#%d.sup",thispid);
-		tof = open (tname,(O_WRONLY|O_CREAT|O_TRUNC),0600);
+		tof = open (tname,(O_WRONLY|O_CREAT|O_TRUNC|O_EXCL),0600);
 		if (tof >= 0)  break;
 		istemp = FALSE;
 	/* give up: try to create output file */
 		if (!docompress)
-			tof = open (to,(O_WRONLY|O_CREAT|O_TRUNC),0600);
+			tof = open (to,(O_WRONLY|O_CREAT|O_TRUNC|O_EXCL),0600);
 		if (tof >= 0)  break;
 	/* no luck */
 		notify ("SUP: Can't create %s or temp file for it\n",to);
@@ -1272,7 +1272,7 @@ char *from;		/* 0 if reading from network */
 		av[ac++] = NULL;
 		if ( (infd = open(tname, O_RDONLY)) == -1 ||
 		     unlink(tname) == -1 ||
-		     (outfd = open(tname, O_WRONLY|O_CREAT|O_TRUNC)) == -1 ||
+		     (outfd = open(tname, O_WRONLY|O_CREAT|O_TRUNC|O_EXCL)) == -1 ||
 		     runiofd( av, infd, outfd, 2 ) != 0 ) {
 			notify("SUP: Error in uncompressing file %s (%s)\n",
 				to, tname );
@@ -1299,7 +1299,7 @@ char *from;		/* 0 if reading from network */
 		lockout (FALSE);
 		return (TRUE);
 	}
-	tof = open (to,(O_WRONLY|O_CREAT|O_TRUNC),0600);
+	tof = open (to,(O_WRONLY|O_CREAT|O_TRUNC|O_EXCL),0600);
 	if (tof < 0) {
 		(void) close (fromf);
 		notify ("SUP: Can't create %s from temp file: %s\n",
