739f3262665de07f5c89b2da7b7e61072fead5f6
atatat <atatat@NetBSD.org>
2003-08-13 03:51:15 +0000

    Jobs specified for @reboot are only popped into the job queue at cron startup time ("reboot" of cron, not the system), so okay_to_go() must always say they are good to go.  Otherwise they never will.  All other meta-timetags pertain to regular times, so they are (or were) unaffected.
    Problem noted by tron and successfully assigned to me by perry.

--- usr.sbin/cron/job.c
+++ usr.sbin/cron/job.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: job.c,v 1.4 2002/04/25 14:45:05 atatat Exp $	*/
+/*	$NetBSD: job.c,v 1.5 2003/08/13 03:51:15 atatat Exp $	*/
 
 /* Copyright 1988,1990,1993,1994 by Paul Vixie
  * All rights reserved
@@ -28,7 +28,7 @@
 #if 0
 static char rcsid[] = "Id: job.c,v 1.6 1994/01/15 20:43:43 vixie Exp";
 #else
-__RCSID("$NetBSD: job.c,v 1.4 2002/04/25 14:45:05 atatat Exp $");
+__RCSID("$NetBSD: job.c,v 1.5 2003/08/13 03:51:15 atatat Exp $");
 #endif
 #endif
 
@@ -110,6 +110,9 @@ okay_to_go(j)
 {
 	char *within, *t;
 	int delta;
+
+	if (j->e->flags & WHEN_REBOOT)
+		return (1);
 
 	within = env_get("CRON_WITHIN", j->e->envp);
 	if (within == NULL)
