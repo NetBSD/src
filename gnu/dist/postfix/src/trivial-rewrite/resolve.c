44ae00e1e8696db1429689e2c6e4958856ee8ae5
tron <tron@NetBSD.org>
2003-08-19 20:06:50 +0000

    Apply patch (requested by taca in ticket 1396): Fix remote DoS vulnerability described in CAN-2003-0540.

--- gnu/dist/postfix/src/trivial-rewrite/resolve.c
+++ gnu/dist/postfix/src/trivial-rewrite/resolve.c
@@ -139,6 +139,7 @@ void    resolve_addr(char *addr, VSTRING *channel, VSTRING *nexthop,
 	    if (saved_domain)
 		tok822_free_tree(saved_domain);
 	    saved_domain = domain;
+	    domain = 0;
 	}
 
 	/*
