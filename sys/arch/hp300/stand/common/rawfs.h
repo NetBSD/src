00ce8ed0a049cfbfa51bd01fc9f3676f501689f8
he <he@NetBSD.org>
2003-08-21 23:28:27 +0000

    Adapt to const-ification of first arg of open function in fs_ops.

--- sys/arch/hp300/stand/common/rawfs.h
+++ sys/arch/hp300/stand/common/rawfs.h
@@ -1,11 +1,11 @@
-/*	$NetBSD: rawfs.h,v 1.1 1997/02/04 03:52:47 thorpej Exp $	*/
+/*	$NetBSD: rawfs.h,v 1.2 2003/08/21 23:28:27 he Exp $	*/
 
 /*
  * Raw file system - for stream devices like tapes.
  * No random access, only sequential read allowed.
  */
 
-int	rawfs_open __P((char *path, struct open_file *f));
+int	rawfs_open __P((const char *path, struct open_file *f));
 int	rawfs_close __P((struct open_file *f));
 int	rawfs_read __P((struct open_file *f, void *buf,
 		u_int size, u_int *resid));
