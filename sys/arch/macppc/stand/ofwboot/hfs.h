e70e0d24b12c5da8a0e79249d76418d91d54413e
he <he>
2003-08-22 08:06:19 +0000

    Adapt to const-ification of first arg of open function in fs_ops.

--- sys/arch/macppc/stand/ofwboot/hfs.h
+++ sys/arch/macppc/stand/ofwboot/hfs.h
@@ -1,6 +1,6 @@
-/*	$NetBSD: hfs.h,v 1.1 2000/11/14 11:25:35 tsubai Exp $	*/
+/*	$NetBSD: hfs.h,v 1.2 2003/08/22 08:06:19 he Exp $	*/
 
-int hfs_open(char *, struct open_file *);
+int hfs_open(const char *, struct open_file *);
 int hfs_close(struct open_file *);
 int hfs_read(struct open_file *, void *, size_t, size_t *);
 int hfs_write(struct open_file *, void *, size_t, size_t *);
