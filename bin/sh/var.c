aba1ca99aedf8a6304977b32232c8d3708bdc1a6
jmmv <jmmv@NetBSD.org>
2003-08-26 18:14:24 +0000

    Include strings.h, needed for index's prototype.

--- bin/sh/var.c
+++ bin/sh/var.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: var.c,v 1.33 2003/08/07 09:05:39 agc Exp $	*/
+/*	$NetBSD: var.c,v 1.34 2003/08/26 18:14:24 jmmv Exp $	*/
 
 /*-
  * Copyright (c) 1991, 1993
@@ -37,12 +37,13 @@
 #if 0
 static char sccsid[] = "@(#)var.c	8.3 (Berkeley) 5/4/95";
 #else
-__RCSID("$NetBSD: var.c,v 1.33 2003/08/07 09:05:39 agc Exp $");
+__RCSID("$NetBSD: var.c,v 1.34 2003/08/26 18:14:24 jmmv Exp $");
 #endif
 #endif /* not lint */
 
 #include <unistd.h>
 #include <stdlib.h>
+#include <strings.h>
 #include <paths.h>
 
 /*
