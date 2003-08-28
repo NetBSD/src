0546bc8b1cc591ce2e5660ef435bb7581da97734
dsl <dsl@NetBSD.org>
2003-08-28 05:00:09 +0000

    Bump for inclusion of :set matchars

--- usr.bin/vi/ex/version.h
+++ usr.bin/vi/ex/version.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: version.h,v 1.12 2003/06/06 08:09:38 aymeric Exp $	*/
+/*	$NetBSD: version.h,v 1.13 2003/08/28 05:00:09 dsl Exp $	*/
 
 #define	VI_VERSION \
-    "Version 1.79nb9 (10/23/96) The CSRG, University of California, Berkeley."
+    "Version 1.79nb10 (10/23/96) The CSRG, University of California, Berkeley."
