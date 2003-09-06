314b0d2301f84990c54d00a5c47832fea8ed8a2b
itojun <itojun>
2003-09-06 04:13:50 +0000

    correct comment

--- sys/netinet6/in6_ifattach.c
+++ sys/netinet6/in6_ifattach.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: in6_ifattach.c,v 1.55 2003/07/08 10:20:45 itojun Exp $	*/
+/*	$NetBSD: in6_ifattach.c,v 1.56 2003/09/06 04:13:50 itojun Exp $	*/
 /*	$KAME: in6_ifattach.c,v 1.124 2001/07/18 08:32:51 jinmei Exp $	*/
 
 /*
@@ -31,7 +31,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: in6_ifattach.c,v 1.55 2003/07/08 10:20:45 itojun Exp $");
+__KERNEL_RCSID(0, "$NetBSD: in6_ifattach.c,v 1.56 2003/09/06 04:13:50 itojun Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -580,6 +580,8 @@ in6_ifattach(ifp, altifp)
 	/* some of the interfaces are inherently not IPv6 capable */
 	switch (ifp->if_type) {
 	case IFT_BRIDGE:
+	case IFT_PFLOG:
+	case IFT_PFSYNC:
 		return;
 	}
 
