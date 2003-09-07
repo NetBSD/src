d493f580242f4327b53b2dc3aebd5187f90ade19
isaki <isaki@NetBSD.org>
2003-09-07 04:24:05 +0000

    Remove an advertising clause.

--- sys/arch/x68k/dev/vs.c
+++ sys/arch/x68k/dev/vs.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: vs.c,v 1.22 2003/07/15 01:44:52 lukem Exp $	*/
+/*	$NetBSD: vs.c,v 1.23 2003/09/07 04:24:06 isaki Exp $	*/
 
 /*
  * Copyright (c) 2001 Tetsuya Isaki. All rights reserved.
@@ -11,10 +11,7 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *      This product includes software developed by Tetsuya Isaki.
- * 4. The name of the author may not be used to endorse or promote products
+ * 3. The name of the author may not be used to endorse or promote products
  *    derived from this software without specific prior written permission
  *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
@@ -35,7 +32,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: vs.c,v 1.22 2003/07/15 01:44:52 lukem Exp $");
+__KERNEL_RCSID(0, "$NetBSD: vs.c,v 1.23 2003/09/07 04:24:06 isaki Exp $");
 
 #include "audio.h"
 #include "vs.h"
