566ecab083b856e3c7e2287fa0f9cb69a64becac
isaki <isaki@NetBSD.org>
2003-09-07 04:24:05 +0000

    Remove an advertising clause.

--- sys/dev/ic/msm6258var.h
+++ sys/dev/ic/msm6258var.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: msm6258var.h,v 1.4 2002/04/07 14:51:40 isaki Exp $	*/
+/*	$NetBSD: msm6258var.h,v 1.5 2003/09/07 04:24:07 isaki Exp $	*/
 
 /*
  * Copyright (c) 2001 Tetsuya Isaki. All rights reserved.
@@ -11,10 +11,7 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *      This product includes software developed by Tetsuya Isaki.
- * 4. The name of the author may not be used to endorse or promote products
+ * 3. The name of the author may not be used to endorse or promote products
  *    derived from this software without specific prior written permission
  *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
