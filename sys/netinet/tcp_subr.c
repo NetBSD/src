c3d80b2108854294b129ee6456ffe50caca03093
itojun <itojun@NetBSD.org>
2003-09-08 02:06:34 +0000

    initialize ip_hl for ipsec policy lookup.  PR kern/22715

--- sys/netinet/tcp_subr.c
+++ sys/netinet/tcp_subr.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: tcp_subr.c,v 1.152 2003/09/06 03:36:31 itojun Exp $	*/
+/*	$NetBSD: tcp_subr.c,v 1.153 2003/09/08 02:06:34 itojun Exp $	*/
 
 /*
  * Copyright (C) 1995, 1996, 1997, and 1998 WIDE Project.
@@ -98,7 +98,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: tcp_subr.c,v 1.152 2003/09/06 03:36:31 itojun Exp $");
+__KERNEL_RCSID(0, "$NetBSD: tcp_subr.c,v 1.153 2003/09/08 02:06:34 itojun Exp $");
 
 #include "opt_inet.h"
 #include "opt_ipsec.h"
@@ -455,6 +455,7 @@ tcp_template(tp)
 	    {
 		struct ipovly *ipov;
 		mtod(m, struct ip *)->ip_v = 4;
+		mtod(m, struct ip *)->ip_hl = hlen >> 2;
 		ipov = mtod(m, struct ipovly *);
 		ipov->ih_pr = IPPROTO_TCP;
 		ipov->ih_len = htons(sizeof(struct tcphdr));
