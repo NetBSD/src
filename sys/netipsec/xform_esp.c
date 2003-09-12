23c03403b0df012472524dc1527cced43135509f
itojun <itojun>
2003-09-12 11:20:57 +0000

    merge netipsec/key* into netkey/key*.  no need for both. change confusing filename

--- sys/netipsec/xform_esp.c
+++ sys/netipsec/xform_esp.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: xform_esp.c,v 1.2 2003/08/20 22:33:41 jonathan Exp $	*/
+/*	$NetBSD: xform_esp.c,v 1.3 2003/09/12 11:21:00 itojun Exp $	*/
 /*	$FreeBSD: src/sys/netipsec/xform_esp.c,v 1.2.2.1 2003/01/24 05:11:36 sam Exp $	*/
 /*	$OpenBSD: ip_esp.c,v 1.69 2001/06/26 06:18:59 angelos Exp $ */
 
@@ -39,7 +39,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: xform_esp.c,v 1.2 2003/08/20 22:33:41 jonathan Exp $");
+__KERNEL_RCSID(0, "$NetBSD: xform_esp.c,v 1.3 2003/09/12 11:21:00 itojun Exp $");
 
 #include "opt_inet.h"
 #ifdef __FreeBSD__
@@ -77,8 +77,8 @@ __KERNEL_RCSID(0, "$NetBSD: xform_esp.c,v 1.2 2003/08/20 22:33:41 jonathan Exp $
 #include <netinet6/ip6_ecn.h>
 #endif
 
-#include <netipsec/key.h>
-#include <netipsec/key_debug.h>
+#include <netkey/key.h>
+#include <netkey/key_debug.h>
 
 #include <netipsec/ipsec_osdep.h>
 
