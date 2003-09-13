95aa37118510ed9a9872787f0cc515387f08d319
jdolecek <jdolecek@NetBSD.org>
2003-09-13 12:13:29 +0000

    fix description of Intel PRO/100 VM Network Controller with 82562ET/EZ PHY don't activate any quirks for VM_6 variant problem reported by Lars Heidieker on current-users
    also don't activate any quirks for VM_3 - VM_5 variants, neither
    VE_2 - VE_4, in anticipation that the more modern variants don't need
    the quirks

--- sys/dev/pci/if_fxp_pci.c
+++ sys/dev/pci/if_fxp_pci.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: if_fxp_pci.c,v 1.35 2003/08/14 07:59:39 nonaka Exp $	*/
+/*	$NetBSD: if_fxp_pci.c,v 1.36 2003/09/13 12:13:30 jdolecek Exp $	*/
 
 /*-
  * Copyright (c) 1997, 1998, 1999, 2000, 2001 The NetBSD Foundation, Inc.
@@ -43,7 +43,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: if_fxp_pci.c,v 1.35 2003/08/14 07:59:39 nonaka Exp $");
+__KERNEL_RCSID(0, "$NetBSD: if_fxp_pci.c,v 1.36 2003/09/13 12:13:30 jdolecek Exp $");
 
 #include "rnd.h"
 
@@ -104,7 +104,7 @@ static void	fxp_pci_power __P((int why, void *arg));
 CFATTACH_DECL(fxp_pci, sizeof(struct fxp_pci_softc),
     fxp_pci_match, fxp_pci_attach, NULL, NULL);
 
-const struct fxp_pci_product {
+static const struct fxp_pci_product {
 	u_int32_t	fpp_prodid;	/* PCI product ID */
 	const char	*fpp_name;	/* device name */
 } fxp_pci_products[] = {
@@ -143,7 +143,7 @@ const struct fxp_pci_product {
 	{ PCI_PRODUCT_INTEL_PRO_100_VM_5,
 	  "Intel PRO/100 VM (MOB) Network Controller" },
 	{ PCI_PRODUCT_INTEL_PRO_100_VM_6,
-	  "Intel PRO/100 VM Network Controller with 82562ET PHY" },
+	  "Intel PRO/100 VM Network Controller with 82562ET/EZ PHY" },
 	{ PCI_PRODUCT_INTEL_PRO_100_M,
 	  "Intel PRO/100 M Network Controller" },
 	{ 0,
@@ -381,10 +381,6 @@ fxp_pci_attach(parent, self, aux)
 	case PCI_PRODUCT_INTEL_82562EH_HPNA_1:
 	case PCI_PRODUCT_INTEL_82562EH_HPNA_2:
 	case PCI_PRODUCT_INTEL_PRO_100_VM_2:
-	case PCI_PRODUCT_INTEL_PRO_100_VM_3:
-	case PCI_PRODUCT_INTEL_PRO_100_VM_4:
-	case PCI_PRODUCT_INTEL_PRO_100_VM_5:
-	case PCI_PRODUCT_INTEL_PRO_100_VM_6:
 		aprint_normal(": %s, rev %d\n", fpp->fpp_name, sc->sc_rev);
 
 		/*
@@ -402,6 +398,19 @@ fxp_pci_attach(parent, self, aux)
 		/*
 		 *  XXX We have to read the C-ICH's developer's manual
 		 *  in detail
+		 */
+		break;
+	case PCI_PRODUCT_INTEL_PRO_100_VE_2:
+	case PCI_PRODUCT_INTEL_PRO_100_VE_3:
+	case PCI_PRODUCT_INTEL_PRO_100_VE_4:
+	case PCI_PRODUCT_INTEL_PRO_100_VM_3:
+	case PCI_PRODUCT_INTEL_PRO_100_VM_4:
+	case PCI_PRODUCT_INTEL_PRO_100_VM_5:
+	case PCI_PRODUCT_INTEL_PRO_100_VM_6:
+		aprint_normal(": %s, rev %d\n", fpp->fpp_name, sc->sc_rev);
+		
+		/*
+		 * No particular quirks.
 		 */
 		break;
 	}
