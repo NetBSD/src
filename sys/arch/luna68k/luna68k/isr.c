8887ec19496cb03c4cdece227728b080545b0274
junyoung <junyoung@NetBSD.org>
2003-09-14 12:59:33 +0000

    Remove a duplicate $NetBSD$.

--- sys/arch/luna68k/luna68k/isr.c
+++ sys/arch/luna68k/luna68k/isr.c
@@ -1,5 +1,4 @@
-/* $NetBSD: isr.c,v 1.6 2002/09/27 15:36:12 provos Exp $ */
-/*	$NetBSD: isr.c,v 1.6 2002/09/27 15:36:12 provos Exp $	*/
+/*	$NetBSD: isr.c,v 1.7 2003/09/14 12:59:33 junyoung Exp $	*/
 
 /*-
  * Copyright (c) 1996 The NetBSD Foundation, Inc.
@@ -39,7 +38,7 @@
 
 #include <sys/cdefs.h>			/* RCS ID & Copyright macro defns */
 
-__KERNEL_RCSID(0, "$NetBSD: isr.c,v 1.6 2002/09/27 15:36:12 provos Exp $");
+__KERNEL_RCSID(0, "$NetBSD: isr.c,v 1.7 2003/09/14 12:59:33 junyoung Exp $");
 
 /*
  * Link and dispatch interrupts.
