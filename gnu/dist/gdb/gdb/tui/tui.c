8f226f9bd47d4ce9fc69d8436741a726c3558232
christos <christos>
2003-09-14 22:16:48 +0000

    Add casts so that we can compile with both the readline headers and ours. Of course we cannot link using our libedit yet.

--- gnu/dist/gdb/gdb/tui/tui.c
+++ gnu/dist/gdb/gdb/tui/tui.c
@@ -294,16 +294,16 @@ tui_initialize_readline ()
 
   rl_initialize ();
 
-  rl_add_defun ("tui-switch-mode", tui_rl_switch_mode, -1);
-  rl_add_defun ("gdb-command", tui_rl_command_key, -1);
-  rl_add_defun ("next-keymap", tui_rl_next_keymap, -1);
+  rl_add_defun ("tui-switch-mode", (Function *)tui_rl_switch_mode, -1);
+  rl_add_defun ("gdb-command", (Function *)tui_rl_command_key, -1);
+  rl_add_defun ("next-keymap", (Function *)tui_rl_next_keymap, -1);
 
   tui_keymap = rl_make_bare_keymap ();
   tui_ctlx_keymap = rl_make_bare_keymap ();
   tui_readline_standard_keymap = rl_get_keymap ();
 
   for (i = 0; tui_commands[i].cmd; i++)
-    rl_bind_key_in_map (tui_commands[i].key, tui_rl_command_key, tui_keymap);
+    rl_bind_key_in_map (tui_commands[i].key, (Function *)tui_rl_command_key, tui_keymap);
 
   rl_generic_bind (ISKMAP, "\\C-x", (char*) tui_ctlx_keymap, tui_keymap);
 
@@ -320,22 +320,22 @@ tui_initialize_readline ()
       if (tui_commands[j].cmd)
         continue;
 
-      rl_bind_key_in_map (i, tui_rl_command_mode, tui_keymap);
+      rl_bind_key_in_map (i, (Function *)tui_rl_command_mode, tui_keymap);
     }
 
-  rl_bind_key_in_map ('a', tui_rl_switch_mode, emacs_ctlx_keymap);
-  rl_bind_key_in_map ('a', tui_rl_switch_mode, tui_ctlx_keymap);
-  rl_bind_key_in_map ('A', tui_rl_switch_mode, emacs_ctlx_keymap);
-  rl_bind_key_in_map ('A', tui_rl_switch_mode, tui_ctlx_keymap);
-  rl_bind_key_in_map (CTRL ('A'), tui_rl_switch_mode, emacs_ctlx_keymap);
-  rl_bind_key_in_map (CTRL ('A'), tui_rl_switch_mode, tui_ctlx_keymap);
-  rl_bind_key_in_map ('1', tui_rl_delete_other_windows, emacs_ctlx_keymap);
-  rl_bind_key_in_map ('1', tui_rl_delete_other_windows, tui_ctlx_keymap);
-  rl_bind_key_in_map ('2', tui_rl_change_windows, emacs_ctlx_keymap);
-  rl_bind_key_in_map ('2', tui_rl_change_windows, tui_ctlx_keymap);
-  rl_bind_key_in_map ('q', tui_rl_next_keymap, tui_keymap);
-  rl_bind_key_in_map ('s', tui_rl_next_keymap, emacs_ctlx_keymap);
-  rl_bind_key_in_map ('s', tui_rl_next_keymap, tui_ctlx_keymap);
+  rl_bind_key_in_map ('a', (Function *)tui_rl_switch_mode, emacs_ctlx_keymap);
+  rl_bind_key_in_map ('a', (Function *)tui_rl_switch_mode, tui_ctlx_keymap);
+  rl_bind_key_in_map ('A', (Function *)tui_rl_switch_mode, emacs_ctlx_keymap);
+  rl_bind_key_in_map ('A', (Function *)tui_rl_switch_mode, tui_ctlx_keymap);
+  rl_bind_key_in_map (CTRL ('A'), (Function *)tui_rl_switch_mode, emacs_ctlx_keymap);
+  rl_bind_key_in_map (CTRL ('A'), (Function *)tui_rl_switch_mode, tui_ctlx_keymap);
+  rl_bind_key_in_map ('1', (Function *)tui_rl_delete_other_windows, emacs_ctlx_keymap);
+  rl_bind_key_in_map ('1', (Function *)tui_rl_delete_other_windows, tui_ctlx_keymap);
+  rl_bind_key_in_map ('2', (Function *)tui_rl_change_windows, emacs_ctlx_keymap);
+  rl_bind_key_in_map ('2', (Function *)tui_rl_change_windows, tui_ctlx_keymap);
+  rl_bind_key_in_map ('q', (Function *)tui_rl_next_keymap, tui_keymap);
+  rl_bind_key_in_map ('s', (Function *)tui_rl_next_keymap, emacs_ctlx_keymap);
+  rl_bind_key_in_map ('s', (Function *)tui_rl_next_keymap, tui_ctlx_keymap);
 }
 
 /* Enter in the tui mode (curses).
