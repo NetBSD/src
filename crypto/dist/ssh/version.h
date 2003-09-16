8e7fb4badbfd910c8c89c617e741a09a462f5a7b
grant <grant@NetBSD.org>
2003-09-16 19:33:55 +0000

    Pull up revision 1.29 via patch (requested by tron in ticket #1471):
    crank __NETBSDSSH_VERSION for buffer.c fix

--- crypto/dist/ssh/version.h
+++ crypto/dist/ssh/version.h
@@ -1,8 +1,8 @@
-/*	$NetBSD: version.h,v 1.23.2.2 2002/06/26 16:54:41 tv Exp $	*/
+/*	$NetBSD: version.h,v 1.23.2.3 2003/09/16 19:33:55 grant Exp $	*/
 /* $OpenBSD: version.h,v 1.34 2002/06/26 13:56:27 markus Exp $ */
 
 #define __OPENSSH_VERSION	"OpenSSH_3.4"
-#define __NETBSDSSH_VERSION	"NetBSD_Secure_Shell-20020626"
+#define __NETBSDSSH_VERSION	"NetBSD_Secure_Shell-20030916"
 
 /*
  * it is important to retain OpenSSH version identification part, it is
