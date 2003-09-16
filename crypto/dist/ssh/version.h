b06b2565a8af9b9a5b21533185f903345d84a1d1
lukem <lukem@NetBSD.org>
2003-09-16 17:39:32 +0000

    crank __NETBSDSSH_VERSION for buffer.c fix

--- crypto/dist/ssh/version.h
+++ crypto/dist/ssh/version.h
@@ -1,8 +1,8 @@
-/*	$NetBSD: version.h,v 1.28 2003/04/03 06:21:37 itojun Exp $	*/
+/*	$NetBSD: version.h,v 1.29 2003/09/16 17:39:32 lukem Exp $	*/
 /* $OpenBSD: version.h,v 1.37 2003/04/01 10:56:46 markus Exp $ */
 
 #define __OPENSSH_VERSION	"OpenSSH_3.6.1"
-#define __NETBSDSSH_VERSION	"NetBSD_Secure_Shell-20030403"
+#define __NETBSDSSH_VERSION	"NetBSD_Secure_Shell-20030916"
 
 /*
  * it is important to retain OpenSSH version identification part, it is
