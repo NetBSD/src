983a41b75b1510f6ba20c036c410aa3d4f15931c
itojun <itojun@NetBSD.org>
2003-09-16 00:31:55 +0000

    exp is reserved name under posix

--- sys/netinet/ip_id.c
+++ sys/netinet/ip_id.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: ip_id.c,v 1.1 2003/09/06 03:36:30 itojun Exp $	*/
+/*	$NetBSD: ip_id.c,v 1.2 2003/09/16 00:31:55 itojun Exp $	*/
 /*	$OpenBSD: ip_id.c,v 1.6 2002/03/15 18:19:52 millert Exp $	*/
 
 /*
@@ -57,7 +57,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: ip_id.c,v 1.1 2003/09/06 03:36:30 itojun Exp $");
+__KERNEL_RCSID(0, "$NetBSD: ip_id.c,v 1.2 2003/09/16 00:31:55 itojun Exp $");
 
 #include <sys/types.h>
 #include <sys/param.h>
@@ -100,13 +100,13 @@ static void ip_initid(void);
  */
 
 static u_int16_t
-pmod(u_int16_t gen, u_int16_t exp, u_int16_t mod)
+pmod(u_int16_t gen, u_int16_t expo, u_int16_t mod)
 {
 	u_int16_t s, t, u;
 
 	s = 1;
 	t = gen;
-	u = exp;
+	u = expo;
 
 	while (u) {
 		if (u & 1)
