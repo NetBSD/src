6df8befe7cf880fef5305afa19c38c214942e08d
christos <christos@NetBSD.org>
2003-09-16 23:32:21 +0000

    pull up 3.7 -> 3.7.1 fixes.

--- crypto/dist/ssh/version.h
+++ crypto/dist/ssh/version.h
@@ -1,8 +1,8 @@
-/*	$NetBSD: version.h,v 1.1.1.1.2.11 2003/09/16 21:53:38 christos Exp $	*/
+/*	$NetBSD: version.h,v 1.1.1.1.2.12 2003/09/16 23:32:23 christos Exp $	*/
 /* $OpenBSD: version.h,v 1.27 2001/12/05 15:04:48 markus Exp $ */
 
 #define __OPENSSH_VERSION	"OpenSSH_3.0.2"
-#define __NETBSDSSH_VERSION	"NetBSD_Secure_Shell-20030916"
+#define __NETBSDSSH_VERSION	"NetBSD_Secure_Shell-20030916a"
 
 /*
  * it is important to retain OpenSSH version identification part, it is
