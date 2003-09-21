1e77b606bf867a494355322627383ad1de631e41
matt <matt@NetBSD.org>
2003-09-21 20:16:47 +0000

    Switch MACHINE_ARCH arm to GCC 3.3.1

--- share/mk/bsd.own.mk
+++ share/mk/bsd.own.mk
@@ -1,4 +1,4 @@
-#	$NetBSD: bsd.own.mk,v 1.360 2003/09/19 08:15:46 mrg Exp $
+#	$NetBSD: bsd.own.mk,v 1.361 2003/09/21 20:16:47 matt Exp $
 
 .if !defined(_BSD_OWN_MK_)
 _BSD_OWN_MK_=1
@@ -40,11 +40,11 @@ TOOLCHAIN_MISSING=	no
 #    ${MACHINE_ARCH} == "sh3eb" ||
 #    ${MACHINE_ARCH} == "powerpc" ||
 # mostly working,:
-#    ${MACHINE_ARCH} == "arm" ||
 #    ${MACHINE_ARCH} == "m68k" ||
 #    ${MACHINE_ARCH} == "mipseb" ||
 #    ${MACHINE_ARCH} == "mipsel" ||
 .if ${MACHINE_ARCH} == "alpha" || \
+    ${MACHINE_ARCH} == "arm" ||
     ${MACHINE_ARCH} == "i386" || \
     ${MACHINE_ARCH} == "sparc" || \
     ${MACHINE_ARCH} == "sparc64"
