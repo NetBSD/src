209d90a3fff6a8f48ec8185c64baa469bd3af76b
chs <chs@NetBSD.org>
2003-09-28 17:25:07 +0000

    avoid hanging during autoconf if no mouse is attached.  fixes PR 11111. based on code from Christian Groessler.

--- sys/arch/pmax/tc/scc.c
+++ sys/arch/pmax/tc/scc.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: scc.c,v 1.82 2003/08/07 16:29:16 agc Exp $	*/
+/*	$NetBSD: scc.c,v 1.83 2003/09/28 17:25:07 chs Exp $	*/
 
 /*
  * Copyright (c) 1991,1990,1989,1994,1995,1996 Carnegie Mellon University
@@ -62,7 +62,7 @@
  */
 
 #include <sys/cdefs.h>			/* RCS ID & Copyright macro defns */
-__KERNEL_RCSID(0, "$NetBSD: scc.c,v 1.82 2003/08/07 16:29:16 agc Exp $");
+__KERNEL_RCSID(0, "$NetBSD: scc.c,v 1.83 2003/09/28 17:25:07 chs Exp $");
 
 /*
  * Intel 82530 dual usart chip driver. Supports the serial port(s) on the
@@ -262,6 +262,8 @@ static struct consdev scccons = {
 	NODEV, 0
 };
 
+static boolean_t scc_getc_dotimeout;
+
 void
 scc_cnattach(base, offset)
 	u_int32_t	base;
@@ -414,7 +416,6 @@ sccattach(parent, self, aux)
 	 */
 	sccreset(sc);
 
-
 	/*
 	 * Special handling for consoles.
 	 */
@@ -422,7 +423,7 @@ sccattach(parent, self, aux)
 		/*
 		 * We were using PROM callbacks for console I/O,
 		 * and we just reset the chip under the console.
-		 * Re-wire  this unit up as console ASAP.
+		 * Re-wire this unit up as console ASAP.
 		 */
 		sc->scc_softCAR |= 1 << SCCLINE(cn_tab->cn_dev);
 		scc_tty_init(sc, cn_tab->cn_dev);
@@ -434,7 +435,7 @@ sccattach(parent, self, aux)
 	printf("\n");
 
 
-	/* Wire up any childre, like keyboards or mice. */
+	/* Wire up any children, like keyboards or mice. */
 #if NRASTERCONSOLE > 0
 	if (systype != DS_MAXINE) {
 		int maj;
@@ -532,7 +533,9 @@ scc_mouse_init(sc, dev)
 		goto done;
 
 	DELAY(10000);
+	scc_getc_dotimeout = TRUE;
 	lk_mouseinit(ctty.t_dev, sccPutc, sccGetc);
+	scc_getc_dotimeout = FALSE;
 	DELAY(10000);
 
 done:
@@ -1410,9 +1413,8 @@ sccGetc(dev)
 	dev_t dev;
 {
 	scc_regmap_t *regs;
-	int c, line;
+	int s, c, line, timeout;
 	u_char value;
-	int s;
 
 	line = SCCLINE(dev);
 	if (cold && scc_cons_addr) {
@@ -1427,6 +1429,7 @@ sccGetc(dev)
 		return (0);
 	/*s = spltty(); */	/* XXX  why different spls? */
 	s = splhigh();
+	timeout = 500000;
 	for (;;) {
 		SCC_READ_REG(regs, line, SCC_RR0, value);
 		if (value & ZSRR0_RX_READY) {
@@ -1443,8 +1446,14 @@ sccGetc(dev)
 				splx(s);
 				return (c & 0xff);
 			}
-		} else
+			timeout = 500000;
+		} else {
 			DELAY(10);
+			if (scc_getc_dotimeout && --timeout == 0) {
+				splx(s);
+				return (-1);
+			}
+		}
 	}
 }
 
