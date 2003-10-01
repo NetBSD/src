e969ce2f8d3e7e3ce67cc8e3abfc9a5b5bb022f7
seb <seb@NetBSD.org>
2003-10-01 11:59:06 +0000

    Adjust pathnames to {md{4,5},sha1}.h after their removal from src/include.
    Approved by mrg@

--- tools/compat/sha1.h
+++ tools/compat/sha1.h
@@ -1,5 +1,5 @@
-/*	$NetBSD: sha1.h,v 1.1 2002/09/13 19:07:02 thorpej Exp $	*/
+/*	$NetBSD: sha1.h,v 1.2 2003/10/01 11:59:06 seb Exp $	*/
 
 /* We unconditionally use the NetBSD SHA1 in libnbcompat. */
 #include "config.h"
-#include "../../include/sha1.h"
+#include "../../sys/sys/sha1.h"
