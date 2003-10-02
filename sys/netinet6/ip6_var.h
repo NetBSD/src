f351fcc4afad375462f97f65c66a76641e77dbf0
tron <tron@NetBSD.org>
2003-10-02 20:30:32 +0000

    Pull up revision 1.21 via patch (requested by itojun in ticket #1491): sync with latest KAME in6_ifaddr/prefix/default router manipulation. behavior changes: - two iocts used by ndp(8) are now obsolete (backward compat provided).   use sysctl path instead. - lo0 does not get ::1 automatically.  it will get ::1 when lo0 comes up.

--- sys/netinet6/ip6_var.h
+++ sys/netinet6/ip6_var.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: ip6_var.h,v 1.18 2001/12/21 03:58:15 itojun Exp $	*/
+/*	$NetBSD: ip6_var.h,v 1.18.10.1 2003/10/02 20:30:32 tron Exp $	*/
 /*	$KAME: ip6_var.h,v 1.33 2000/06/11 14:59:20 jinmei Exp $	*/
 
 /*
@@ -201,7 +201,7 @@ struct	ip6stat {
 
 #ifdef _KERNEL
 /* flags passed to ip6_output as last parameter */
-#define	IPV6_DADOUTPUT		0x01	/* DAD */
+#define	IPV6_UNSPECSRC		0x01	/* allow :: as the source address */
 #define	IPV6_FORWARDING		0x02	/* most of IPv6 header exists */
 #define	IPV6_MINMTU		0x04	/* use minimum MTU (IPV6_USE_MIN_MTU) */
 
