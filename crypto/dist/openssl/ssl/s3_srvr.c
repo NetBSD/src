20e1809968cfb5a09fcc10a03f9a4009d8a22aaf
tron <tron@NetBSD.org>
2003-10-02 09:20:16 +0000

    Pull up revision 1.6 (requested by itojun in ticket #1496): more fixes from 0.9.7c, from openbsd

--- crypto/dist/openssl/ssl/s3_srvr.c
+++ crypto/dist/openssl/ssl/s3_srvr.c
@@ -415,8 +415,11 @@ int ssl3_accept(SSL *s)
 			else {
 				/* could be sent for a DH cert, even if we
 				 * have not asked for it :-) */
-				ret=ssl3_get_client_certificate(s);
-				if (ret <= 0) goto end;
+				 if (s->s3->tmp.cert_request)
+					{
+					ret=ssl3_get_client_certificate(s);
+					if (ret <= 0) goto end;
+					}
 				s->init_num=0;
 				s->state=SSL3_ST_SR_KEY_EXCH_A;
 			}
@@ -821,6 +824,9 @@ static int ssl3_get_client_hello(SSL *s)
 		}
 
 	/* TLS does not mind if there is extra stuff */
+#if 0	/* SSL 3.0 does not mind either, so we should disable this test
+	 * (was enabled in 0.9.6d through 0.9.6j and 0.9.7 through 0.9.7b,
+	 * in earlier SSLeay/OpenSSL releases this test existed but was buggy) */
 	if (s->version == SSL3_VERSION)
 		{
 		if (p < (d+n))
@@ -832,6 +838,7 @@ static int ssl3_get_client_hello(SSL *s)
 			goto f_err;
 			}
 		}
+#endif
 
 	/* Given s->session->ciphers and ssl_get_ciphers_by_id(s), we must
 	 * pick a cipher */
@@ -1326,6 +1333,7 @@ static int ssl3_send_certificate_request(SSL *s)
 		s->init_num += 4;
 #endif
 
+		s->state = SSL3_ST_SW_CERT_REQ_B;
 		}
 
 	/* SSL3_ST_SW_CERT_REQ_B */
