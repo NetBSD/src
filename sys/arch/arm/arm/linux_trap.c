15b0afdf7960c344e059d70de3c60359b0bea0a1
matt <matt@NetBSD.org>
2003-10-10 14:44:42 +0000

    Adapt ARM Linux compat code to deal with SIGINFO.

--- sys/arch/arm/arm/linux_trap.c
+++ sys/arch/arm/arm/linux_trap.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: linux_trap.c,v 1.3 2003/07/15 00:24:39 lukem Exp $ */
+/*	$NetBSD: linux_trap.c,v 1.4 2003/10/10 14:44:42 matt Exp $ */
 
 /*-
  * Copyright (c) 2001 The NetBSD Foundation, Inc.
@@ -37,7 +37,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: linux_trap.c,v 1.3 2003/07/15 00:24:39 lukem Exp $");
+__KERNEL_RCSID(0, "$NetBSD: linux_trap.c,v 1.4 2003/10/10 14:44:42 matt Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -51,6 +51,7 @@ __KERNEL_RCSID(0, "$NetBSD: linux_trap.c,v 1.3 2003/07/15 00:24:39 lukem Exp $")
 #include <compat/linux/common/linux_exec.h>
 
 void
-linux_trapsignal(struct lwp *l, int signo, u_long type) {
-	trapsignal(l, signo, type);
+linux_trapsignal(struct lwp *l, const ksiginfo_t *ksi)
+{
+	trapsignal(l, ksi);
 }
