2af734816dbeee942695afcfa1660f2240ba45c6
matt <matt@NetBSD.org>
2003-10-10 18:08:04 +0000

    Adapt to SIGINFO changes.

--- sys/compat/linux/arch/alpha/linux_machdep.h
+++ sys/compat/linux/arch/alpha/linux_machdep.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: linux_machdep.h,v 1.8 2003/09/26 12:02:57 simonb Exp $	*/
+/*	$NetBSD: linux_machdep.h,v 1.9 2003/10/10 18:08:04 matt Exp $	*/
 
 /*-
  * Copyright (c) 1998, 2000 The NetBSD Foundation, Inc.
@@ -97,11 +97,11 @@ struct linux_rt_sigframe {
 
 #ifdef _KERNEL
 __BEGIN_DECLS
-void setup_linux_rt_sigframe __P((struct trapframe *, int, sigset_t *));
-void setup_linux_sigframe __P((struct trapframe *, int, const sigset_t *));
-int linux_restore_sigcontext __P((struct lwp *, struct linux_sigcontext,
-				  sigset_t *));
-void linux_syscall_intern __P((struct proc *));
+void setup_linux_rt_sigframe(struct trapframe *, int, const sigset_t *);
+void setup_linux_sigframe(struct trapframe *, int, const sigset_t *);
+int linux_restore_sigcontext(struct lwp *, struct linux_sigcontext,
+				  sigset_t *);
+void linux_syscall_intern(struct proc *);
 __END_DECLS
 #endif /* !_KERNEL */
 
