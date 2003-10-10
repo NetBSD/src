8ff398492d22b17d5b14523dcb5f41679b3fbf9b
matt <matt@NetBSD.org>
2003-10-10 23:18:57 +0000

    Define SIGINFO variant of netbsd32_sendsig when __HAVE_SIGINFO is defined.

--- sys/compat/netbsd32/netbsd32_exec.h
+++ sys/compat/netbsd32/netbsd32_exec.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: netbsd32_exec.h,v 1.16 2003/09/26 18:10:32 christos Exp $	*/
+/*	$NetBSD: netbsd32_exec.h,v 1.17 2003/10/10 23:18:57 matt Exp $	*/
 
 /*
  * Copyright (c) 1998, 2001 Matthew R. Green
@@ -72,7 +72,11 @@ static __inline int netbsd32_copyargs __P((struct proc *, struct exec_package *,
 
 void netbsd32_setregs (struct lwp *, struct exec_package *, u_long stack);
 int netbsd32_sigreturn (struct proc *, void *, register_t *);
+#ifdef __HAVE_SIGINFO
+void netbsd32_sendsig (const ksiginfo_t *, const sigset_t *);
+#else
 void netbsd32_sendsig (int, const sigset_t *, u_long);
+#endig
 
 extern char netbsd32_esigcode[], netbsd32_sigcode[];
 
