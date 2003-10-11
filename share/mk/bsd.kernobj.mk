fe4dbc22054c8f13efed04e83eb36477106d9cae
lukem <lukem@NetBSD.org>
2003-10-11 08:05:57 +0000

    Only try to  KERNOBJDIR!=  if the required directory exists.

--- share/mk/bsd.kernobj.mk
+++ share/mk/bsd.kernobj.mk
@@ -1,4 +1,4 @@
-#	$NetBSD: bsd.kernobj.mk,v 1.11 2003/07/18 04:04:03 lukem Exp $
+#	$NetBSD: bsd.kernobj.mk,v 1.12 2003/10/11 08:05:57 lukem Exp $
 
 # KERNSRCDIR	Is the location of the top of the kernel src.
 # 		It defaults to `${NETBSDSRCDIR}/sys'.
@@ -21,6 +21,6 @@
 KERNSRCDIR?=	${NETBSDSRCDIR}/sys
 KERNARCHDIR?=	arch/${MACHINE}
 KERNCONFDIR?=	${KERNSRCDIR}/${KERNARCHDIR}/conf
-.if !defined(KERNOBJDIR)
+.if !defined(KERNOBJDIR) && exists(${KERNSRCDIR}/${KERNARCHDIR}/compile)
 KERNOBJDIR!=	cd ${KERNSRCDIR}/${KERNARCHDIR}/compile && ${PRINTOBJDIR}
 .endif
