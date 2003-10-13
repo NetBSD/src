9202a5752f52c701545adaca136108123f47d83f
agc <agc@NetBSD.org>
2003-10-13 14:22:20 +0000

    Move Matt Green's code from a 4-clause to a 3-clause licence by removing the advertising clause. Diffs provided in PR 22396 by Joel Baker, the changes were confirmed to the board by Matt Green.

--- lib/libc/net/rcmd.c
+++ lib/libc/net/rcmd.c
@@ -1,6 +1,7 @@
-/*	$NetBSD: rcmd.c,v 1.53 2003/08/07 16:43:12 agc Exp $	*/
+/*	$NetBSD: rcmd.c,v 1.54 2003/10/13 14:22:20 agc Exp $	*/
 
 /*
+ * Copyright (c) 1997 Matthew R. Green.
  * Copyright (c) 1983, 1993, 1994
  *	The Regents of the University of California.  All rights reserved.
  *
@@ -29,44 +30,12 @@
  * SUCH DAMAGE.
  */
 
-/*
- * Copyright (c) 1997 Matthew R. Green.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed by the University of
- *	California, Berkeley and its contributors.
- * 4. Neither the name of the University nor the names of its contributors
- *    may be used to endorse or promote products derived from this software
- *    without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- */
-
 #include <sys/cdefs.h>
 #if defined(LIBC_SCCS) && !defined(lint)
 #if 0
 static char sccsid[] = "@(#)rcmd.c	8.3 (Berkeley) 3/26/94";
 #else
-__RCSID("$NetBSD: rcmd.c,v 1.53 2003/08/07 16:43:12 agc Exp $");
+__RCSID("$NetBSD: rcmd.c,v 1.54 2003/10/13 14:22:20 agc Exp $");
 #endif
 #endif /* LIBC_SCCS and not lint */
 
