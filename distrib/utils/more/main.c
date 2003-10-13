bf7d04198bb6771cd8ca1d1ea7a2a5fad398cc6c
agc <agc@NetBSD.org>
2003-10-13 14:34:25 +0000

    Move Mark Nudelman's code from a 4-clause to a 3-clause licence by removing the advertising clause. Diffs provided in PR 22549 by Joel Baker, confirmed to the board by Mark Nudelman.

--- distrib/utils/more/main.c
+++ distrib/utils/more/main.c
@@ -1,6 +1,7 @@
-/*	$NetBSD: main.c,v 1.7 2003/08/10 08:26:38 mrg Exp $	*/
+/*	$NetBSD: main.c,v 1.8 2003/10/13 14:34:25 agc Exp $	*/
 
 /*
+ * Copyright (c) 1988 Mark Nudelman
  * Copyright (c) 1988, 1993
  *	Regents of the University of California.  All rights reserved.
  *
@@ -29,42 +30,10 @@
  * SUCH DAMAGE.
  */
 
-/*
- * Copyright (c) 1988 Mark Nudleman
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed by the University of
- *	California, Berkeley and its contributors.
- * 4. Neither the name of the University nor the names of its contributors
- *    may be used to endorse or promote products derived from this software
- *    without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- */
-
 #include <sys/cdefs.h>
 #ifndef lint
 __COPYRIGHT(\
-"@(#) Copyright (c) 1988 Mark Nudleman.\n\
+"@(#) Copyright (c) 1988 Mark Nudelman.\n\
 @(#) Copyright (c) 1988, 1993\n\
 	Regents of the University of California.  All rights reserved.\n");
 #endif /* not lint */
@@ -73,7 +42,7 @@ __COPYRIGHT(\
 #if 0
 static char sccsid[] = "@(#)main.c	8.1 (Berkeley) 6/7/93";
 #else
-__RCSID("$NetBSD: main.c,v 1.7 2003/08/10 08:26:38 mrg Exp $");
+__RCSID("$NetBSD: main.c,v 1.8 2003/10/13 14:34:25 agc Exp $");
 #endif
 #endif /* not lint */
 
