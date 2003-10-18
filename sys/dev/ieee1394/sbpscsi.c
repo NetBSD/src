93847cb0315ba1cb7624421590250b1d12170acf
enami <enami@NetBSD.org>
2003-10-18 12:47:28 +0000

    Don't limit xfer size to SBP2_MAXPHYS.  Its not transfer size limit but just a threshold how to describe transfer.

--- sys/dev/ieee1394/sbpscsi.c
+++ sys/dev/ieee1394/sbpscsi.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: sbpscsi.c,v 1.5 2002/12/19 09:58:24 jmc Exp $	*/
+/*	$NetBSD: sbpscsi.c,v 1.6 2003/10/18 12:47:28 enami Exp $	*/
 
 /*
  * Copyright (c) 2002 The NetBSD Foundation, Inc.
@@ -37,7 +37,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: sbpscsi.c,v 1.5 2002/12/19 09:58:24 jmc Exp $");
+__KERNEL_RCSID(0, "$NetBSD: sbpscsi.c,v 1.6 2003/10/18 12:47:28 enami Exp $");
 
 #include <sys/param.h>
 #include <sys/kernel.h>
@@ -316,7 +316,5 @@ sbpscsi_minphys(bp)
         struct buf *bp;
 {
 
-        if (bp->b_bcount > SBP2_MAXPHYS)
-                bp->b_bcount = SBP2_MAXPHYS;
 	minphys(bp);
 }
