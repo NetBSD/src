c093d01fc1f1b159aeafc9ddb1b365c38ac6721c
mrg <mrg>
2003-10-18 11:03:09 +0000

    initial import of GCC 3.3.2.  this is a bug-fix release that fixes dozens of bugs in both MI and many NetBSD MD parts.
    visit http://gcc.gnu.org/gcc-3.3/changes.html#3.3.2 for more details.

--- gnu/dist/gcc/gcc/config/pa/pa-hpux11.h
+++ gnu/dist/gcc/gcc/config/pa/pa-hpux11.h
@@ -18,6 +18,12 @@ along with GNU CC; see the file COPYING.  If not, write to
 the Free Software Foundation, 59 Temple Place - Suite 330,
 Boston, MA 02111-1307, USA.  */
 
+/* GCC always defines __STDC__.  HP C++ compilers don't define it.  This
+   causes trouble when sys/stdsyms.h is included.  As a work around,
+   we define __STDC_EXT__.  A similar situation exists with respect to
+   the definition of __cplusplus.  We define _INCLUDE_LONGLONG
+   to prevent nlist.h from defining __STDC_32_MODE__ (no longlong
+   support).  */
 #undef TARGET_OS_CPP_BUILTINS
 #define TARGET_OS_CPP_BUILTINS()				\
   do								\
@@ -34,6 +40,7 @@ Boston, MA 02111-1307, USA.  */
 	  {							\
 	    builtin_define ("_HPUX_SOURCE");			\
 	    builtin_define ("_INCLUDE_LONGLONG");		\
+	    builtin_define ("__STDC_EXT__");			\
 	  }							\
 	else							\
 	  {							\
@@ -67,6 +74,13 @@ Boston, MA 02111-1307, USA.  */
     }								\
   while (0)
 
+#undef CPP_SPEC
+#define CPP_SPEC \
+  "%{mt|pthread:-D_REENTRANT -D_THREAD_SAFE -D_POSIX_C_SOURCE=199506L}"
+/* aCC defines also -DRWSTD_MULTI_THREAD, -DRW_MULTI_THREAD.  These
+   affect only aCC's C++ library (Rogue Wave-derived) which we do not
+   use, and they violate the user's name space.  */
+
 /* We can debug dynamically linked executables on hpux11; we also
    want dereferencing of a NULL pointer to cause a SEGV.  */
 #undef LINK_SPEC
@@ -81,17 +95,13 @@ Boston, MA 02111-1307, USA.  */
    %{static:-a archive} %{shared:-b}"
 #endif
 
-/* Like the default, except no -lg.  */
+/* hpux 11 has posix threads.  */
 #undef LIB_SPEC
 #define LIB_SPEC \
   "%{!shared:\
-     %{!p:%{!pg:\
-       %{!threads:-lc %{static:%{!nolibdld:-a shared -ldld -a archive -lc}}}\
-       %{threads:-lcma -lc_r}}}\
-     %{p: -L/lib/libp/ -lc\
-       %{static:%{!nolibdld:-a shared -ldld -a archive -lc}}}\
-     %{pg: -L/lib/libp/ -lc\
-       %{static:%{!nolibdld:-a shared -ldld -a archive -lc}}}}"
+     %{mt|pthread:-lpthread}     \
+     %{p|pg:-L/usr/lib/libp} -lc \
+     %{static:%{!nolibdld:-a shared -ldld -a archive -lc}}}"
 
 /* Under hpux11, the normal location of the `ld' and `as' programs is the
    /usr/ccs/bin directory.  */
