f76a4948daf2431cc7821f33922327d739732b5b
matt <matt@NetBSD.org>
2003-10-19 03:33:49 +0000

    Add IBM Network Station 1000 port.  From John Gordon and updated by me.

---
+++ sys/arch/ibmnws/include/ieeefp.h
@@ -0,0 +1,3 @@
+/*	$NetBSD: ieeefp.h,v 1.1 2003/10/19 03:33:50 matt Exp $	*/
+
+#include <powerpc/ieeefp.h>
---
+++ sys/arch/ibmnws/include/vmparam.h
@@ -0,0 +1,4 @@
+/*	$NetBSD: vmparam.h,v 1.1 2003/10/19 03:33:50 matt Exp $	*/
+
+#define	USER_SR		11
+#include <powerpc/oea/vmparam.h>
