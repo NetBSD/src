f6c9407fe2104a360f8f24dd445ee52fe9370068
uwe <uwe@NetBSD.org>
2003-10-19 02:22:56 +0000

    __KERNEL_RCSID.

--- sys/arch/sh3/dev/adc.c
+++ sys/arch/sh3/dev/adc.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: adc.c,v 1.1 2003/10/18 01:27:18 uwe Exp $ */
+/*	$NetBSD: adc.c,v 1.2 2003/10/19 02:22:56 uwe Exp $ */
 
 /*
  * Copyright (c) 2003 Valeriy E. Ushakov
@@ -26,6 +26,9 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+
+#include <sys/cdefs.h>
+__KERNEL_RCSID(0, "$NetBSD: adc.c,v 1.2 2003/10/19 02:22:56 uwe Exp $");
 
 #include <sys/param.h>
 #include <sys/kernel.h>
