b77dcff254137d7cc27db3dd21aa59767788573e
kleink <kleink>
2003-10-22 21:04:40 +0000

    G/c the local declaration of __nanf; handling of this (conditional on __HAVE_NANF) was moved to <math.h> some time ago.

--- sys/arch/hppa/include/math.h
+++ sys/arch/hppa/include/math.h
@@ -1,13 +1,3 @@
-/*	$NetBSD: math.h,v 1.3 2003/10/06 05:27:19 matt Exp $	*/
-
-#include <sys/featuretest.h>
+/*	$NetBSD: math.h,v 1.4 2003/10/22 21:04:40 kleink Exp $	*/
 
 #define	__HAVE_NANF
-/*
- * ISO C99
- */
-#if !defined(_ANSI_SOURCE) && \
-    (defined(_ISOC99_SOURCE) || (__STDC_VERSION__ - 0) >= 199901L || \
-     defined(_NETBSD_SOURCE))
-extern const union __float_u __nanf;
-#endif
