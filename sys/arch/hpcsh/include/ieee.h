66cd83e29950a79a7557e8dc0ef74f0202ec08af
kleink <kleink@NetBSD.org>
2003-10-23 13:02:52 +0000

    No need for multiple-inclusion protection in these.

--- sys/arch/hpcsh/include/ieee.h
+++ sys/arch/hpcsh/include/ieee.h
@@ -1,7 +1,3 @@
-/*	$NetBSD: ieee.h,v 1.2 2001/01/28 03:31:31 uch Exp $	*/
-#ifndef _HPCSH_IEEE_H_
-#define _HPCSH_IEEE_H_
+/*	$NetBSD: ieee.h,v 1.3 2003/10/23 13:02:52 kleink Exp $	*/
 
 #include <sh3/ieee.h>
-
-#endif /* _HPCSH_IEEE_H_ */
