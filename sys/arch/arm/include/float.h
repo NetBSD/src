422697679eba0a519cc881325dda109fbb50d48d
kleink <kleink@NetBSD.org>
2003-10-23 23:26:06 +0000

    * Move the definitions for types other than single-precision and double-   precision back to machine-dependent headers.  C99 has no strict   requirement which, if any, extended-precision type `long double' must   match, and even between 80-bit formats there are differences in   implementation (m68k vs. x86). * On arm, consider __VFP_FP__.

--- sys/arch/arm/include/float.h
+++ sys/arch/arm/include/float.h
@@ -1,3 +1,31 @@
-/*	$NetBSD: float.h,v 1.3 2003/05/12 15:22:54 kleink Exp $	*/
+/*	$NetBSD: float.h,v 1.4 2003/10/23 23:26:06 kleink Exp $	*/
+
+#ifndef _ARM_FLOAT_H_
+#define _ARM_FLOAT_H_
+
+#ifndef __VFP_FP__
+#define	LDBL_MANT_DIG	113
+#define	LDBL_EPSILON	1.925929944387235853055977942584927319E-34L
+#define	LDBL_DIG	33
+#define	LDBL_MIN_EXP	(-16381)
+#define	LDBL_MIN	3.3621031431120935062626778173217526026E-4932L
+#define	LDBL_MIN_10_EXP	(-4931)
+#define	LDBL_MAX_EXP	16384
+#define	LDBL_MAX	1.1897314953572317650857593266280070162E4932L
+#define	LDBL_MAX_10_EXP	4932
+#endif
 
 #include <sys/float_ieee754.h>
+
+#ifndef __VFP_FP__
+#if !defined(_ANSI_SOURCE) && !defined(_POSIX_C_SOURCE) && \
+    !defined(_XOPEN_SOURCE) || \
+    ((__STDC_VERSION__ - 0) >= 199901L) || \
+    ((_POSIX_C_SOURCE - 0) >= 200112L) || \
+    ((_XOPEN_SOURCE  - 0) >= 600) || \
+    defined(_ISOC99_SOURCE) || defined(_NETBSD_SOURCE)
+#define	DECIMAL_DIG	36
+#endif /* !defined(_ANSI_SOURCE) && ... */
+#endif /* !__VFP_FP__ */
+
+#endif /* !_ARM_FLOAT_H_ */
