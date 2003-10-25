9a956129ff1394753b752c0e7fe0fc0aa6513af6
kleink <kleink@NetBSD.org>
2003-10-25 22:38:19 +0000

    Check HUGE_VALF and HUGE_VALL as well.  Also add some cross checking for floating type promotion and demotion of these.

--- regress/lib/libc/ieeefp/infinity/infinity.c
+++ regress/lib/libc/ieeefp/infinity/infinity.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: infinity.c,v 1.2 2003/10/24 16:35:08 kleink Exp $	*/
+/*	$NetBSD: infinity.c,v 1.3 2003/10/25 22:38:19 kleink Exp $	*/
 
 /*
  * This file is in the Public Domain.
@@ -7,8 +7,9 @@
  */
 
 /*
- * Check that HUGE_VAL (alias __infinity) really is infinite.
- * Alternatively, check that isinf() and _isinfl() minimally work.
+ * Check that HUGE_VAL{,FL} (alias __infinity{,fl}) really is infinite.
+ * Alternatively, check that isinf() and _isinfl() minimally work,
+ * and that floating type promotion respectively demotion works.
  */
 
 #include "namespace.h"
@@ -22,5 +23,14 @@ main(int argc, char **argv)
 	/* HUGE_VAL is meant to be an infinity. */
 	assert(isinf(HUGE_VAL));
 	assert(_isinfl(HUGE_VAL));
+
+	/* HUGE_VALF is the float analog of HUGE_VAL. */
+	assert(isinf(HUGE_VALF));
+	assert(_isinfl(HUGE_VALF));
+
+	/* HUGE_VALL is the float analog of HUGE_VAL. */
+	assert(isinf(HUGE_VALL));
+	assert(_isinfl(HUGE_VALL));
+
 	return 0;
 }
