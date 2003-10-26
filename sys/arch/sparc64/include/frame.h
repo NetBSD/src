9ab20a13e28704a0ae46cd4094e34e31215b02f6
christos <christos@NetBSD.org>
2003-10-26 08:06:56 +0000

    more siginfo glue

--- sys/arch/sparc64/include/frame.h
+++ sys/arch/sparc64/include/frame.h
@@ -1,3 +1,12 @@
-/*       $NetBSD: frame.h,v 1.11 2002/07/20 11:52:21 mrg Exp $        */
+/*       $NetBSD: frame.h,v 1.12 2003/10/26 08:06:56 christos Exp $        */
 
 #include <sparc/frame.h>
+
+#ifndef _LOCORE
+#ifdef COMPAT_16
+void sendsig_sigcontext(const ksiginfo_t *, const sigset_t *);
+#endif
+
+void *getframe(struct lwp *, int, int *);
+void buildcontext(struct lwp *, void *, const void *, void *);
+#endif
