541959d71814ad09ab27e9b91fd480513261fe21
kleink <kleink@NetBSD.org>
2003-10-26 20:55:30 +0000

    Use <sys/ieee754.h> where applicable.

--- sys/arch/hppa/include/ieee.h
+++ sys/arch/hppa/include/ieee.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: ieee.h,v 1.3 2003/10/26 00:36:43 kleink Exp $	*/
+/*	$NetBSD: ieee.h,v 1.4 2003/10/26 20:55:30 kleink Exp $	*/
 
 /*	$OpenBSD: ieee.h,v 1.1 1999/04/20 19:44:04 mickey Exp $	*/
 
@@ -48,55 +48,10 @@
  * mode bits, exceptions, and so forth.
  */
 
-/*
- * Define the number of bits in each fraction and exponent.
- *
- *		     k	         k+1
- * Note that  1.0 x 2  == 0.1 x 2      and that denorms are represented
- *
- *					  (-exp_bias+1)
- * as fractions that look like 0.fffff x 2             .  This means that
- *
- *			 -126
- * the number 0.10000 x 2    , for instance, is the same as the normalized
- *
- *		-127			   -128
- * float 1.0 x 2    .  Thus, to represent 2    , we need one leading zero
- *
- *				  -129
- * in the fraction; to represent 2    , we need two, and so on.  This
- *
- *						     (-exp_bias-fracbits+1)
- * implies that the smallest denormalized number is 2
- *
- * for whichever format we are talking about: for single precision, for
- *
- *						-126		-149
- * instance, we get .00000000000000000000001 x 2    , or 1.0 x 2    , and
- *
- * -149 == -127 - 23 + 1.
- */
-#define	SNG_EXPBITS	8
-#define	SNG_FRACBITS	23
-
-#define	DBL_EXPBITS	11
-#define	DBL_FRACBITS	52
+#include <sys/ieee754.h>
 
 #define	EXT_EXPBITS	15
 #define	EXT_FRACBITS	112
-
-struct ieee_single {
-	u_int	sng_sign:1;
-	u_int	sng_exp:8;
-	u_int	sng_frac:23;
-};
-
-struct ieee_double {
-	u_int	dbl_sign:1;
-	u_int	dbl_exp:11;
-	u_int	dbl_frach:20;
-	u_int	dbl_fracl;
-};
 
 struct ieee_ext {
 	u_int	ext_sign:1;
@@ -116,19 +71,13 @@ struct ieee_ext {
  * A NaN is a `signalling NaN' if its QUIETNAN bit is clear in its
  * high fraction; if the bit is set, it is a `quiet NaN'.
  */
-#define	SNG_EXP_INFNAN	255
-#define	DBL_EXP_INFNAN	2047
 #define	EXT_EXP_INFNAN	32767
 
 #if 0
-#define	SNG_QUIETNAN	(1 << 22)
-#define	DBL_QUIETNAN	(1 << 19)
 #define	EXT_QUIETNAN	(1 << 15)
 #endif
 
 /*
  * Exponent biases.
  */
-#define	SNG_EXP_BIAS	127
-#define	DBL_EXP_BIAS	1023
 #define	EXT_EXP_BIAS	16383
