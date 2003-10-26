37b4ed5f2461eef00cea069cbc09b1318d394b2a
kleink <kleink>
2003-10-26 16:00:17 +0000

    On FPA, switch back `long double' to double-precision.  According to Richard Earnshaw, extended-precision was never supported by the compiler.

--- lib/libc/arch/arm/gen/infinityl.c
+++ lib/libc/arch/arm/gen/infinityl.c
@@ -1,9 +1,9 @@
-/*	$NetBSD: infinityl.c,v 1.1 2003/10/25 22:31:20 kleink Exp $	*/
+/*	$NetBSD: infinityl.c,v 1.2 2003/10/26 16:00:17 kleink Exp $	*/
 
 /*
  * IEEE-compatible infinityl.c -- public domain.
  * For VFP (where long double == double) and
- * for FPA (where an 80-bit format with padding is used).
+ * for FPA (which ia a different layout thereof).
  */
 
 #include <math.h>
@@ -12,10 +12,10 @@
 const union __long_double_u __infinityl =
 #ifdef __VFP_FP__
 #if BYTE_ORDER == BIG_ENDIAN
-	{ { 0x7f, 0xf0, 0, 0, 0, 0,    0,    0             } };
+	{ { 0x7f, 0xf0,    0,    0, 0, 0,    0,    0 } };
 #else
-	{ {    0,    0, 0, 0, 0, 0, 0xf0, 0x7f             } };
+	{ {    0,    0,    0,    0, 0, 0, 0xf0, 0x7f } };
 #endif /* BYTE_ORDER == BIG_ENDIAN */
 #else
-	{ { 0xff, 0x7f, 0, 0, 0, 0,    0,    0, 0, 0, 0, 0 } };
+	{ {    0,    0, 0xf0, 0x7f, 0, 0,    0,    0 } };
 #endif /* __VFP_FP__ */
