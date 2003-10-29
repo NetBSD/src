2853023b7235ad5934ef79d38196d1c7fa2ddd9c
mycroft <mycroft@NetBSD.org>
2003-10-29 01:12:23 +0000

    XXX: gcc

--- sys/dev/ic/esiop.c
+++ sys/dev/ic/esiop.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: esiop.c,v 1.22 2003/10/25 18:35:42 christos Exp $	*/
+/*	$NetBSD: esiop.c,v 1.23 2003/10/29 01:12:23 mycroft Exp $	*/
 
 /*
  * Copyright (c) 2002 Manuel Bouyer.
@@ -33,7 +33,7 @@
 /* SYM53c7/8xx PCI-SCSI I/O Processors driver */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: esiop.c,v 1.22 2003/10/25 18:35:42 christos Exp $");
+__KERNEL_RCSID(0, "$NetBSD: esiop.c,v 1.23 2003/10/29 01:12:23 mycroft Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -421,7 +421,7 @@ esiop_intr(v)
 	struct esiop_cmd *esiop_cmd;
 	struct esiop_lun *esiop_lun;
 	struct scsipi_xfer *xs;
-	int istat, sist, sstat1, dstat = 0;
+	int istat, sist, sstat1, dstat = 0; /* XXX: gcc */
 	u_int32_t irqcode;
 	int need_reset = 0;
 	int offset, target, lun, tag;
