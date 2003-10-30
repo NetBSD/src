ce17fb71843b5f0a3325da9caf7652114ad7d4f3
kent <kent@NetBSD.org>
2003-10-30 19:33:51 +0000

    For ICH4/ICH5, set IOSE flag to make them compatible to old ICHs.

--- sys/dev/pci/auichreg.h
+++ sys/dev/pci/auichreg.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: auichreg.h,v 1.5 2003/05/03 18:11:32 wiz Exp $	*/
+/*	$NetBSD: auichreg.h,v 1.6 2003/10/30 19:33:51 kent Exp $	*/
 
 /*
  * Copyright (c) 2000 Michael Shalayeff
@@ -43,6 +43,8 @@
 #define	ICH_NAMBAR	0x10
 /* 12.1.11 NABMBAR - native audio bus mastering base address register */
 #define	ICH_NABMBAR	0x14
+#define ICH_CFG		0x41
+#define		ICH_CFG_IOSE	0x01
 
 /* table 12-3. native audio bus master control registers */
 #define	ICH_BDBAR	0x00	/* 8-byte aligned address */
