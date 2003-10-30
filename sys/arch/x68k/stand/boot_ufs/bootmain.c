5a3305a7a5bdc6824c95011116f9c40ea48d3914
he <he@NetBSD.org>
2003-10-30 22:27:05 +0000

    Add include of <lib/libsa/stand.h> for memcpy() and memset() prototypes.

--- sys/arch/x68k/stand/boot_ufs/bootmain.c
+++ sys/arch/x68k/stand/boot_ufs/bootmain.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: bootmain.c,v 1.5 2003/04/09 12:57:14 itohy Exp $	*/
+/*	$NetBSD: bootmain.c,v 1.6 2003/10/30 22:27:05 he Exp $	*/
 
 /*-
  * Copyright (c) 1993, 1994 Takumi Nakamura.
@@ -44,6 +44,7 @@
 #ifdef SCSI_ADHOC_BOOTPART
 #include <machine/disklabel.h>
 #endif
+#include <lib/libsa/stand.h>
 
 #include "boot_ufs.h"
 #include "readufs.h"
