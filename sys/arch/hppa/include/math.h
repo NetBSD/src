617742615ce7dff767c97fcd82941ab63b748e0b
kleink <kleink@NetBSD.org>
2003-10-30 22:46:17 +0000

    It turns out the hppa ABI we're using does not support an extended- precision type, so drop the library support for now but leave in place (#ifdef _LP64) the header definitions.

--- sys/arch/hppa/include/math.h
+++ sys/arch/hppa/include/math.h
@@ -1,4 +1,6 @@
-/*	$NetBSD: math.h,v 1.5 2003/10/28 00:55:28 kleink Exp $	*/
+/*	$NetBSD: math.h,v 1.6 2003/10/30 22:46:17 kleink Exp $	*/
 
+#ifdef _LP64
 #define	__HAVE_LONG_DOUBLE
+#endif
 #define	__HAVE_NANF
