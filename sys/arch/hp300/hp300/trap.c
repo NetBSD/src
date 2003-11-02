f6005a39f3eb3ae9f7f36f39d9152f1b8ffd0174
cl <cl>
2003-11-02 16:49:38 +0000

    Extend the #ifdef M68040 so that the `sig' variable declaration is moved in as well, as it's otherwise not used. (from atari/atari/trap.c and x68k/x68k/trap.c)

--- sys/arch/hp300/hp300/trap.c
+++ sys/arch/hp300/hp300/trap.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: trap.c,v 1.105 2003/10/31 16:44:35 cl Exp $	*/
+/*	$NetBSD: trap.c,v 1.106 2003/11/02 16:49:38 cl Exp $	*/
 
 /*
  * Copyright (c) 1982, 1986, 1990, 1993
@@ -77,7 +77,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: trap.c,v 1.105 2003/10/31 16:44:35 cl Exp $");                                                  
+__KERNEL_RCSID(0, "$NetBSD: trap.c,v 1.106 2003/11/02 16:49:38 cl Exp $");                                                  
 
 #include "opt_ddb.h"
 #include "opt_execfmt.h"
@@ -227,8 +227,8 @@ userret(l, fp, oticks, faultaddr, fromtrap)
 	int fromtrap;
 {
 	struct proc *p = l->l_proc;
-	int sig;
 #ifdef M68040
+	int sig;
 	int beenhere = 0;
 
 again:
