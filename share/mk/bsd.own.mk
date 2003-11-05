63cc91d9e8fffc9de99056d44f5a590600c4d779
skrll <skrll@NetBSD.org>
2003-11-05 07:14:31 +0000

    Switch ARM to gdb 5.3.
    XXX 26bit support isn't there at the moment.

--- share/mk/bsd.own.mk
+++ share/mk/bsd.own.mk
@@ -1,4 +1,4 @@
-#	$NetBSD: bsd.own.mk,v 1.389 2003/11/04 07:14:24 mrg Exp $
+#	$NetBSD: bsd.own.mk,v 1.390 2003/11/05 07:14:31 skrll Exp $
 
 .if !defined(_BSD_OWN_MK_)
 _BSD_OWN_MK_=1
@@ -67,11 +67,11 @@ USE_TOOLS_TOOLCHAIN?=yes
 # not ported:
 #    ${MACHINE_ARCH} == "hppa" ||
 # in progress:
-#    ${MACHINE_ARCH} == "arm" ||
 #    ${MACHINE_ARCH} == "armeb" ||
 #    ${MACHINE_ARCH} == "m68000" ||
 # working:
 .if ${MACHINE_ARCH} == "alpha" || \
+    ${MACHINE_ARCH} == "arm" || \
     ${MACHINE_ARCH} == "i386" || \
     ${MACHINE_ARCH} == "m68k" || \
     ${MACHINE_ARCH} == "mipseb" || \
