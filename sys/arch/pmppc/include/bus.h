1d5075fc4df5abe57d9636f7c497e95728c526af
augustss <augustss@NetBSD.org>
2003-11-07 17:00:19 +0000

    Fix various fallout from the big PPC changes early this year. This pmppc port runs again now.

--- sys/arch/pmppc/include/bus.h
+++ sys/arch/pmppc/include/bus.h
@@ -1,18 +1,11 @@
-/*	$NetBSD: bus.h,v 1.3 2003/07/28 07:58:51 scw Exp $	*/
+/*	$NetBSD: bus.h,v 1.4 2003/11/07 17:00:19 augustss Exp $	*/
 
 #ifndef _PMPPC_BUS_H_
 #define _PMPPC_BUS_H_
 
-/*
- * Address conversion as seen from a PCI master.
- */
-#define PHYS_TO_BUS_MEM(t,x)	(x)
-#define BUS_MEM_TO_PHYS(t,x)	(x)
-
 extern struct powerpc_bus_space pmppc_mem_tag;
 extern struct powerpc_bus_space pmppc_pci_io_tag;
 
-void	pmppc_bus_space_init(void);
 void	pmppc_bus_space_mallocok(void);
 
 #include <powerpc/bus.h>
