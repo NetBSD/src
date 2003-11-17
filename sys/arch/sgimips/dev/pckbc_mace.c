41312ec8d4e17d3067ad9dfb72e001096f7c2050
keihan <keihan@NetBSD.org>
2003-11-17 10:07:58 +0000

    www.netbsd.org -> www.NetBSD.org

--- sys/arch/sgimips/dev/pckbc_mace.c
+++ sys/arch/sgimips/dev/pckbc_mace.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: pckbc_mace.c,v 1.8 2003/10/17 12:43:08 sekiya Exp $	*/
+/*	$NetBSD: pckbc_mace.c,v 1.9 2003/11/17 10:07:58 keihan Exp $	*/
 
 /*
  * Copyright (c) 2003 Christopher SEKIYA
@@ -16,7 +16,7 @@
  * 3. All advertising materials mentioning features or use of this software
  *    must display the following acknowledgement:
  *          This product includes software developed for the
- *          NetBSD Project.  See http://www.netbsd.org/ for
+ *          NetBSD Project.  See http://www.NetBSD.org/ for
  *          information about NetBSD.
  * 4. The name of the author may not be used to endorse or promote products
  *    derived from this software without specific prior written permission.
@@ -34,7 +34,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: pckbc_mace.c,v 1.8 2003/10/17 12:43:08 sekiya Exp $");
+__KERNEL_RCSID(0, "$NetBSD: pckbc_mace.c,v 1.9 2003/11/17 10:07:58 keihan Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
--- sys/kern/subr_autoconf.c
+++ sys/kern/subr_autoconf.c
@@ -1,4 +1,4 @@
-/* $NetBSD: subr_autoconf.c,v 1.87 2003/08/07 16:31:52 agc Exp $ */
+/* $NetBSD: subr_autoconf.c,v 1.88 2003/11/17 10:07:58 keihan Exp $ */
 
 /*
  * Copyright (c) 1996, 2000 Christopher G. Demetriou
@@ -15,7 +15,7 @@
  * 3. All advertising materials mentioning features or use of this software
  *    must display the following acknowledgement:
  *          This product includes software developed for the
- *          NetBSD Project.  See http://www.netbsd.org/ for
+ *          NetBSD Project.  See http://www.NetBSD.org/ for
  *          information about NetBSD.
  * 4. The name of the author may not be used to endorse or promote products
  *    derived from this software without specific prior written permission.
@@ -77,7 +77,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: subr_autoconf.c,v 1.87 2003/08/07 16:31:52 agc Exp $");
+__KERNEL_RCSID(0, "$NetBSD: subr_autoconf.c,v 1.88 2003/11/17 10:07:58 keihan Exp $");
 
 #include "opt_ddb.h"
 
