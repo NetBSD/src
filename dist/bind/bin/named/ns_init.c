3fef6a79096f5354002768ec6a0a2513b19297eb
itojun <itojun@NetBSD.org>
2003-11-26 01:33:20 +0000

    BIND 8.3.7

--- dist/bind/bin/named/ns_init.c
+++ dist/bind/bin/named/ns_init.c
@@ -1,8 +1,8 @@
-/*	$NetBSD: ns_init.c,v 1.1.1.5 2003/06/03 07:04:34 itojun Exp $	*/
+/*	$NetBSD: ns_init.c,v 1.1.1.6 2003/11/26 01:33:26 itojun Exp $	*/
 
 #if !defined(lint) && !defined(SABER)
 static const char sccsid[] = "@(#)ns_init.c	4.38 (Berkeley) 3/21/91";
-static const char rcsid[] = "Id: ns_init.c,v 8.77 2002/08/20 04:27:23 marka Exp";
+static const char rcsid[] = "Id: ns_init.c,v 8.77.8.2 2003/09/04 04:09:09 marka Exp";
 #endif /* not lint */
 
 /*
