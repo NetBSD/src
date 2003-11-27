7525482bacf185d8d4891b4492e133479ae35af3
mrg <mrg>
2003-11-27 10:32:25 +0000

    delete GDB 5.0.  it is no longer used by supported platforms (sorry pc532 :-)

--- gnu/dist/toolchain/gdb/config/sparc/linux.mt
+++
@@ -1,3 +0,0 @@
-# Target: Sparcstation, running Linux
-TDEPFILES= sparc-tdep.o solib.o
-TM_FILE= tm-linux.h
--- gnu/dist/toolchain/gdb/testsuite/gdb.base/bar.c
+++
@@ -1,9 +0,0 @@
-static int barx __attribute__ ((section (".data01"))) = 'b' + 'a' + 'r';
-
-int bar (int x)
-{
-  if (x)
-    return barx;
-  else
-    return 0;
-}
