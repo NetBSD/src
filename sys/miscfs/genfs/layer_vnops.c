03e41d98b7e9c7a10ea5da10ba0dda260af9f7a1
wiz <wiz@NetBSD.org>
2003-11-30 20:53:48 +0000

    Typo fixes in comments from jmc@openbsd.

--- sys/miscfs/genfs/layer_vnops.c
+++ sys/miscfs/genfs/layer_vnops.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: layer_vnops.c,v 1.12 2003/11/17 11:16:10 wiz Exp $	*/
+/*	$NetBSD: layer_vnops.c,v 1.13 2003/11/30 20:53:48 wiz Exp $	*/
 
 /*
  * Copyright (c) 1999 National Aeronautics & Space Administration
@@ -67,7 +67,7 @@
  *
  * Ancestors:
  *	@(#)lofs_vnops.c	1.2 (Berkeley) 6/18/92
- *	$Id: layer_vnops.c,v 1.12 2003/11/17 11:16:10 wiz Exp $
+ *	$Id: layer_vnops.c,v 1.13 2003/11/30 20:53:48 wiz Exp $
  *	...and...
  *	@(#)null_vnodeops.c 1.20 92/07/07 UCLA Ficus project
  */
@@ -121,13 +121,13 @@
  * The null layer is the minimum file system layer,
  * simply bypassing all possible operations to the lower layer
  * for processing there.  The majority of its activity centers
- * on the bypass routine, though which nearly all vnode operations
+ * on the bypass routine, through which nearly all vnode operations
  * pass.
  *
  * The bypass routine accepts arbitrary vnode operations for
  * handling by the lower layer.  It begins by examing vnode
  * operation arguments and replacing any layered nodes by their
- * lower-layer equivlants.  It then invokes the operation
+ * lower-layer equivalents.  It then invokes the operation
  * on the lower layer.  Finally, it replaces the layered nodes
  * in the arguments and, if a vnode is return by the operation,
  * stacks a layered node on top of the returned vnode.
@@ -179,7 +179,7 @@
  * are created as a result of vnode operations on
  * this or other null vnode stacks.
  *
- * New vnode stacks come into existance as a result of
+ * New vnode stacks come into existence as a result of
  * an operation which returns a vnode.  
  * The bypass routine stacks a null-node above the new
  * vnode before returning it to the caller.
@@ -219,20 +219,20 @@
  *
  * The first approach is to call the aliasing layer's bypass routine.
  * This method is most suitable when you wish to invoke the operation
- * currently being hanldled on the lower layer.  It has the advantage
+ * currently being handled on the lower layer.  It has the advantage
  * that the bypass routine already must do argument mapping.
  * An example of this is null_getattrs in the null layer.
  *
- * A second approach is to directly invoked vnode operations on
+ * A second approach is to directly invoke vnode operations on
  * the lower layer with the VOP_OPERATIONNAME interface.
  * The advantage of this method is that it is easy to invoke
  * arbitrary operations on the lower layer.  The disadvantage
- * is that vnodes arguments must be manualy mapped.
+ * is that vnodes' arguments must be manually mapped.
  *
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: layer_vnops.c,v 1.12 2003/11/17 11:16:10 wiz Exp $");
+__KERNEL_RCSID(0, "$NetBSD: layer_vnops.c,v 1.13 2003/11/30 20:53:48 wiz Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
