ae63de983ddd34b352d14ae23c3b52d89b02b0ee
mrg <mrg@NetBSD.org>
2003-12-04 09:29:31 +0000

    build lib{opcodes,bfd,iberty} back in gnu/lib instead of gnu/usr.bin/binutils as they are needed earlier than when gnu/usr.bin/binutils is built when "make -j" is used.

--- gnu/lib/libbfd/lib_bfd.c
+++ gnu/lib/libbfd/lib_bfd.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: lib_bfd.c,v 1.1 1999/02/09 15:57:53 tv Exp $	*/
+/*	$NetBSD: lib_bfd.c,v 1.2 2003/12/04 09:29:31 mrg Exp $	*/
 
 /* ELF systems need this to avoid the "wrong" libbfd.so. */
 #include "libbfd.c"
