6281b8fe8a222189e3dcb67911acdc9be6a0ff24
mrg <mrg>
2003-12-04 02:00:07 +0000

    delete binutils 2.13 sources.

--- gnu/dist/toolchain/gas/testsuite/gas/mips/relax.d
+++
@@ -1,8 +0,0 @@
-#objdump: -dr --prefix-addresses -mmips:4000
-#name: MIPS relax
-
-# Test relaxation.
-
-.*: +file format .*mips.*
-
-Disassembly of section .text:
--- gnu/dist/toolchain/ld/testsuite/ld-mmix/sec-7a.s
+++
@@ -1,6 +0,0 @@
- .section .debug_info
- .byte 42
- .rept 32763
- .byte 0
- .endr
- .byte 43
