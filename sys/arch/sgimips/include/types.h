1d0bcfa7eef7e6f4b53019dbda3cf294ee9ea4bd
lonewolf <lonewolf@NetBSD.org>
2003-12-10 00:24:21 +0000

    sgimips lives in the new-style wscons land.

--- sys/arch/sgimips/include/types.h
+++ sys/arch/sgimips/include/types.h
@@ -1,8 +1,9 @@
-/*	$NetBSD: types.h,v 1.6 2003/01/03 10:30:00 rafal Exp $	*/
+/*	$NetBSD: types.h,v 1.7 2003/12/10 00:24:21 lonewolf Exp $	*/
 
 #include <mips/types.h>
 
 #define	__HAVE_DEVICE_REGISTER
+#define	__HAVE_NWSCONS
 #define	__HAVE_GENERIC_SOFT_INTERRUPTS
 
 /* MIPS specific options */
