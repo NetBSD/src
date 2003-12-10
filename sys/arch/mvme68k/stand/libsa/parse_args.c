c6b0a2341081897b9eae4d26b6f8518b802e613c
agc <agc@NetBSD.org>
2003-12-10 12:06:25 +0000

    Modify the licences of code written by Theo De Raadt from a 4-clause to a 2-clause licence (retaining UCB clauses (1) and (2)), per PR 22409 from Joel Baker, approved by Theo de Raadt, and ratified by myself - the only discrepancy being the handling of the original clause 3 in src/usr.sbin/yppoll/yppoll.c.

--- sys/arch/mvme68k/stand/libsa/parse_args.c
+++ sys/arch/mvme68k/stand/libsa/parse_args.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: parse_args.c,v 1.7 2000/09/24 12:32:36 jdolecek Exp $	*/
+/*	$NetBSD: parse_args.c,v 1.8 2003/12/10 12:06:25 agc Exp $	*/
 
 /*-
  * Copyright (c) 1995 Theo de Raadt
@@ -11,12 +11,6 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed under OpenBSD by
- *	Theo de Raadt for Willowglen Singapore.
- * 4. The name of the author may not be used to endorse or promote products
- *    derived from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS
  * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
