614e5d6f0837bc4bbe66f4bb3297da2a99aab3db
uwe <uwe@NetBSD.org>
2003-12-16 18:59:10 +0000

    Add btodb() define.

--- sys/arch/hpc/stand/include/machine/param.h
+++ sys/arch/hpc/stand/include/machine/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.1 2001/02/09 18:35:30 uch Exp $	*/
+/*	$NetBSD: param.h,v 1.2 2003/12/16 18:59:10 uwe Exp $	*/
 
 /* Windows CE architecture */
 
@@ -6,3 +6,6 @@
 #define	DEV_BSHIFT	9		/* log2(DEV_BSIZE) */
 #define BLKDEV_IOSIZE	2048
 #define	MAXPHYS		(64 * 1024)	/* max raw I/O transfer size */
+
+/* bytes to disk blocks */
+#define	btodb(x)	((x) >> DEV_BSHIFT)
