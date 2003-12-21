18649accd0050dd9b0c134fed2d41941f226693c
tron <tron@NetBSD.org>
2003-12-21 08:13:35 +0000

    Pull up revision 1.14 (requested by kleink in ticket #1580): Update to keep file lengths in appropriate data types (off_t, as opposed to u_int32_t).

--- usr.bin/cksum/crc.c
+++ usr.bin/cksum/crc.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: crc.c,v 1.12 2002/03/31 14:43:22 bjh21 Exp $	*/
+/*	$NetBSD: crc.c,v 1.12.2.1 2003/12/21 08:13:35 tron Exp $	*/
 
 /*-
  * Copyright (c) 1991, 1993
@@ -41,7 +41,7 @@
 #if 0
 static char sccsid[] = "@(#)crc.c	8.1 (Berkeley) 6/17/93";
 #else
-__RCSID("$NetBSD: crc.c,v 1.12 2002/03/31 14:43:22 bjh21 Exp $");
+__RCSID("$NetBSD: crc.c,v 1.12.2.1 2003/12/21 08:13:35 tron Exp $");
 #endif
 #endif /* not lint */
 
@@ -114,16 +114,19 @@ static const u_int32_t crctab[] = {
 int
 crc(fd, cval, clen)
 	register int fd;
-	u_int32_t *cval, *clen;
+	u_int32_t *cval;
+	off_t *clen;
 {
 	register u_char *p;
 	register int nr;
-	register u_int32_t thecrc, len;
+	register u_int32_t thecrc;
+	off_t len;
 	u_char buf[16 * 1024];
 
 #define	COMPUTE(var, ch)	(var) = (var) << 8 ^ crctab[(var) >> 24 ^ (ch)]
 
-	thecrc = len = 0;
+	thecrc = 0;
+	len = 0;
 	while ((nr = read(fd, buf, sizeof(buf))) > 0)
 		for (len += nr, p = buf; nr--; ++p) {
 			COMPUTE(thecrc, *p);
