8ec34e13f35f335a9d23fe9d23e1301a213b1f91
wiz <wiz@NetBSD.org>
2003-12-26 19:04:55 +0000

    Niels Provos kindly agreed to drop clauses 3 and 4 from the license -- thanks. Based on OpenBSD commit and hints by itojun.

--- sys/netinet/ip_id.c
+++ sys/netinet/ip_id.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: ip_id.c,v 1.5 2003/12/10 05:22:18 itojun Exp $	*/
+/*	$NetBSD: ip_id.c,v 1.6 2003/12/26 19:04:55 wiz Exp $	*/
 /*	$OpenBSD: ip_id.c,v 1.6 2002/03/15 18:19:52 millert Exp $	*/
 
 /*
@@ -18,11 +18,6 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *      This product includes software developed by Niels Provos.
- * 4. The name of the author may not be used to endorse or promote products
- *    derived from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
@@ -57,7 +52,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: ip_id.c,v 1.5 2003/12/10 05:22:18 itojun Exp $");
+__KERNEL_RCSID(0, "$NetBSD: ip_id.c,v 1.6 2003/12/26 19:04:55 wiz Exp $");
 
 #include "opt_inet.h"
 
