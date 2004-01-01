dc56f602c7319028c89b6f7fed6abbb25314f202
oster <oster@NetBSD.org>
2004-01-01 19:37:17 +0000

    Tidy up the #includes a bit.

--- sys/dev/raidframe/rf_debugprint.c
+++ sys/dev/raidframe/rf_debugprint.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: rf_debugprint.c,v 1.7 2003/12/30 21:59:03 oster Exp $	*/
+/*	$NetBSD: rf_debugprint.c,v 1.8 2004/01/01 19:37:17 oster Exp $	*/
 /*
  * Copyright (c) 1995 Carnegie-Mellon University.
  * All rights reserved.
@@ -34,16 +34,12 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: rf_debugprint.c,v 1.7 2003/12/30 21:59:03 oster Exp $");
+__KERNEL_RCSID(0, "$NetBSD: rf_debugprint.c,v 1.8 2004/01/01 19:37:17 oster Exp $");
 
 #include <dev/raidframe/raidframevar.h>
 
-#include "rf_threadstuff.h"
 #include "rf_debugprint.h"
 #include "rf_general.h"
-#include "rf_options.h"
-
-#include <sys/param.h>
 
 void 
 rf_debug_printf(char *s, void *a1, void *a2, void *a3, void *a4, 
