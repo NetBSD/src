3ed69918d7a8b864e36b0c02789de3d20c9063b3
aymeric <aymeric@NetBSD.org>
2004-01-03 00:09:15 +0000

    fix an obvious typo which prevented the machdep.cacheinfo table from being published. This generated the following warning message at boot time: sysctl_createv: sysctl_create(cacheinfo) returned 17

--- sys/arch/powerpc/powerpc/powerpc_machdep.c
+++ sys/arch/powerpc/powerpc/powerpc_machdep.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: powerpc_machdep.c,v 1.24 2003/12/05 11:00:57 hannken Exp $	*/
+/*	$NetBSD: powerpc_machdep.c,v 1.25 2004/01/03 00:09:15 aymeric Exp $	*/
 
 /*
  * Copyright (C) 1995, 1996 Wolfgang Solfrank.
@@ -32,7 +32,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: powerpc_machdep.c,v 1.24 2003/12/05 11:00:57 hannken Exp $");
+__KERNEL_RCSID(0, "$NetBSD: powerpc_machdep.c,v 1.25 2004/01/03 00:09:15 aymeric Exp $");
 
 #include "opt_altivec.h"
 
@@ -155,7 +155,7 @@ SYSCTL_SETUP(sysctl_machdep_setup, "sysctl machdep subtree setup")
 	sysctl_createv(SYSCTL_PERMANENT,
 		       CTLTYPE_STRUCT, "cacheinfo", NULL,
 		       sysctl_machdep_cacheinfo, 0, NULL, 0,
-		       CTL_MACHDEP, CPU_PRINTFATALTRAPS, CTL_EOL);
+		       CTL_MACHDEP, CPU_CACHEINFO, CTL_EOL);
 #ifdef PPC_OEA
 	sysctl_createv(SYSCTL_PERMANENT|SYSCTL_READWRITE,
 		       CTLTYPE_INT, "powersave", NULL,
