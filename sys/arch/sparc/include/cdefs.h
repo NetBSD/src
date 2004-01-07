5f13a6436e483e6f8e5ae47365900aa12324a41b
martin <martin@NetBSD.org>
2004-01-07 20:02:28 +0000

    A __insn_barrier() is now available for all archs, remove the MD version here. Suggested by Jason Thorpe.

--- sys/arch/sparc/include/cdefs.h
+++ sys/arch/sparc/include/cdefs.h
@@ -1,18 +1,8 @@
-/*	$NetBSD: cdefs.h,v 1.9 2003/11/16 11:07:57 pk Exp $	*/
+/*	$NetBSD: cdefs.h,v 1.10 2004/01/07 20:02:28 martin Exp $	*/
 
 #ifndef	_MACHINE_CDEFS_H_
 #define	_MACHINE_CDEFS_H_
 
-#ifdef __GNUC__
-/*
- * A statement designed to tell the compiler not to re-order instructions
- * across this barrier. It does not generate any code by itself and the
- * harmless `clobber' of %g0 prevents other side-effects such as re-loading
- * registers from memory.
- */
-#define	__insn_barrier()	__asm __volatile("": : : "g0")
-#else
-#define	__insn_barrier()	/*void*/
-#endif
+/* No arch-specific cdefs. */
 
 #endif /* !_MACHINE_CDEFS_H_ */
