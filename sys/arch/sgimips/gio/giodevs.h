721c897f3491f57da534cd3d8aeca04437dc78ef
sekiya <sekiya@NetBSD.org>
2004-01-10 02:26:44 +0000

    Add framework for gio card identification based on product ID.  devlist2h.awk was borrowed from sys/dev/pcmcia.

---
+++ sys/arch/sgimips/gio/giodevs.h
@@ -0,0 +1,28 @@
+/*	$NetBSD: giodevs.h,v 1.1 2004/01/10 02:26:44 sekiya Exp $	*/
+
+/*
+ * THIS FILE AUTOMATICALLY GENERATED.  DO NOT EDIT.
+ */
+
+struct gio_knowndev {
+	int productid;
+	char *product;
+};
+
+struct gio_knowndev gio_knowndevs[] = {
+	{ 0x02, "GTR TokenRing" },
+	{ 0x04, "Synchronous ISDN" },
+	{ 0x06, "Canon Interface" },
+	{ 0x08, "JPEG (Double Wide)" },
+	{ 0x09, "JPEG (Single Wide)" },
+	{ 0x0a, "XPI mez. FDDI device 0" },
+	{ 0x0b, "XPI mez. FDDI device 1" },
+	{ 0x0e, "E-Plex 8-port Ethernet" },
+	{ 0x30, "Lyon Lamb IVAS" },
+	{ 0x36, "Phobos G130 10/100 Ethernet" },
+	{ 0x85, "ATM board" },
+	{ 0x87, "16 bit SCSI Card" },
+	{ 0x8c, "SMPTE 259M Video" },
+	{ 0x8d, "Babblefish Compression" },
+	{ 0, NULL }
+};
