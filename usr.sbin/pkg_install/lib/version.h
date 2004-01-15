5944031e3b7bcd0e2936f9a119081aca06eb9ad4
agc <agc@NetBSD.org>
2004-01-15 09:33:38 +0000

    At package creation time, record the targets of symbolic links which are installed as part of the package.
    Enable the verification of the integrity of any symbolic links
    in the package in "pkg_admin check".
    
    Add a check to pkg_delete to verify the integrity of the symbolic link
    before deletion, and to act in the same way as an MD5 failure if the
    symbolic link has been modified since package creation time.
    
    Bump version number.
    
    Addresses PR 23048 from Klaus Klein.

--- usr.sbin/pkg_install/lib/version.h
+++ usr.sbin/pkg_install/lib/version.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: version.h,v 1.43 2004/01/14 23:32:37 jlam Exp $	*/
+/*	$NetBSD: version.h,v 1.44 2004/01/15 09:33:39 agc Exp $	*/
 
 /*
  * Copyright (c) 2001 Thomas Klausner.  All rights reserved.
@@ -33,6 +33,6 @@
 #ifndef _INST_LIB_VERSION_H_
 #define _INST_LIB_VERSION_H_
 
-#define PKGTOOLS_VERSION "20040114"
+#define PKGTOOLS_VERSION "20040115"
 
 #endif /* _INST_LIB_VERSION_H_ */
