6c78fb6e45bd957828561fe36c8f54c17ebd6d8a
yamt <yamt>
2004-01-18 03:57:30 +0000

    _citrus_NONE_stdenc_wctomb: return E2BIG correctly.

--- lib/libc/citrus/citrus_none.c
+++ lib/libc/citrus/citrus_none.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: citrus_none.c,v 1.11 2003/12/29 17:49:22 yamt Exp $	*/
+/*	$NetBSD: citrus_none.c,v 1.12 2004/01/18 03:57:30 yamt Exp $	*/
 
 /*-
  * Copyright (c)2002 Citrus Project,
@@ -28,7 +28,7 @@
 
 #include <sys/cdefs.h>
 #if defined(LIBC_SCCS) && !defined(lint)
-__RCSID("$NetBSD: citrus_none.c,v 1.11 2003/12/29 17:49:22 yamt Exp $");
+__RCSID("$NetBSD: citrus_none.c,v 1.12 2004/01/18 03:57:30 yamt Exp $");
 #endif /* LIBC_SCCS and not lint */
 
 #include <assert.h>
@@ -455,8 +455,8 @@ _citrus_NONE_stdenc_wctomb(struct _citrus_stdenc * __restrict ce,
 		return (EILSEQ);
 	}
 	if (n==0) {
-		*nresult = (size_t)-2;
-		return (0);
+		*nresult = (size_t)-1;
+		return (E2BIG);
 	}
 
 	*nresult = 1;
