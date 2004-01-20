6ab01af199b21f9e24f71594b8ff89d118b74c86
kleink <kleink@NetBSD.org>
2004-01-20 19:52:40 +0000

    Move FP_ROP and FP_DIRTYZERO from <math.h> to <vax/math.h> (where they would have ended up if I hadn't missed the latter file in the commit).

--- sys/arch/vax/include/math.h
+++ sys/arch/vax/include/math.h
@@ -1,3 +1,14 @@
-/*	$NetBSD: math.h,v 1.2 2003/10/26 00:02:02 kleink Exp $	*/
+/*	$NetBSD: math.h,v 1.3 2004/01/20 19:52:40 kleink Exp $	*/
 
 #define	__INFINITY	1.0E+39F
+
+#if !defined(_ANSI_SOURCE) && !defined(_POSIX_C_SOURCE) && \
+    !defined(_XOPEN_SOURCE) || \
+    ((__STDC_VERSION__ - 0) >= 199901L) || \
+    ((_POSIX_C_SOURCE - 0) >= 200112L) || \
+    ((_XOPEN_SOURCE  - 0) >= 600) || \
+    defined(_ISOC99_SOURCE) || defined(_NETBSD_SOURCE)
+/* 7.12#6 number classification macros; machine-specific classes */
+#define	FP_DIRTYZERO	0x80
+#define	FP_ROP		0x81
+#endif
