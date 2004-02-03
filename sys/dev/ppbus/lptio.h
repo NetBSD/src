0802e692ab29a9c1b3da2e1b4e07b4fe13da2745
jdolecek <jdolecek@NetBSD.org>
2004-02-03 19:57:00 +0000

    overhaul the ioctl interface to be better suited for extensions and to reduce number of separate ioctls - have only 'get mode', 'set mode', 'get flags', 'set flags'

--- sys/dev/ppbus/lptio.h
+++ sys/dev/ppbus/lptio.h
@@ -1,4 +1,4 @@
-/* $NetBSD: lptio.h,v 1.4 2004/01/28 17:35:58 jdolecek Exp $ */
+/* $NetBSD: lptio.h,v 1.5 2004/02/03 19:57:00 jdolecek Exp $ */
 
 /*-
  * Copyright (c) 2004 The NetBSD Foundation, Inc.
@@ -41,31 +41,23 @@
 
 /* Definitions for get status command */
 enum lpt_mode_t {
-	standard = 1,
-	nibble = 2,
-	ps2 = 3,
-	fast = 4,
-	ecp = 5,
-	epp = 6
+	mode_unknown = -1,
+	mode_standard = 1,
+	mode_nibble = 2,
+	mode_ps2 = 3,
+	mode_fast = 4,
+	mode_ecp = 5,
+	mode_epp = 6
 };
 
-typedef struct {
-	u_int16_t mode_status;
-	u_int8_t dma_status;
-	u_int8_t ieee_status;
-} LPT_INFO_T;
-
 /* LPT ioctl commands */
-#define LPTIO_ENABLE_DMA	_IO('L', 0)
-#define LPTIO_DISABLE_DMA	_IO('L', 1)
-#define LPTIO_MODE_STD		_IO('L', 2)
-#define LPTIO_MODE_FAST		_IO('L', 3)
-#define LPTIO_MODE_PS2		_IO('L', 4)
-#define LPTIO_MODE_ECP		_IO('L', 5)
-#define LPTIO_MODE_EPP		_IO('L', 6)
-#define LPTIO_MODE_NIBBLE	_IO('L', 7)
-#define LPTIO_ENABLE_IEEE	_IO('L', 8)
-#define LPTIO_DISABLE_IEEE	_IO('L', 9)
-#define LPTIO_GET_STATUS	_IOR('L', 10, LPT_INFO_T)
+#define	LPTGMODE	_IOR('L', 0, int)
+#define	LPTSMODE	_IOW('L', 1, int)
+#define	LPTGFLAGS	_IOR('L', 2, int)
+#define	LPTSFLAGS	_IOW('L', 3, int)
+
+/* flags for LPT[GS]FLAGS */
+#define	LPT_DMA		0x01	/* enabled DMA */
+#define LPT_IEEE	0x02	/* enabled IEEE 1284 negotiation */
 
 #endif /* __DEV_PPBUS_LPTIO_H_ */
