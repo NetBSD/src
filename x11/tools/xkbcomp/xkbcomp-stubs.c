fcc9c2eaaa3493c9f7eb555c4431ad1268d4be0a
lukem <lukem>
2004-02-08 21:25:09 +0000

    add TNF copyright.  remove unnecessary cruft

--- x11/tools/xkbcomp/xkbcomp-stubs.c
+++ x11/tools/xkbcomp/xkbcomp-stubs.c
@@ -1,4 +1,40 @@
-/*	$NetBSD: xkbcomp-stubs.c,v 1.1.1.1 2003/09/11 18:37:09 lukem Exp $	*/
+/*	$NetBSD: xkbcomp-stubs.c,v 1.2 2004/02/08 21:25:09 lukem Exp $	*/
+
+/*-
+ * Copyright (c) 2003-2004 The NetBSD Foundation, Inc.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to The NetBSD Foundation
+ * by Luke Mewburn.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *        This product includes software developed by the NetBSD
+ *        Foundation, Inc. and its contributors.
+ * 4. Neither the name of The NetBSD Foundation nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 
 #include <stdio.h>
 
@@ -114,35 +150,3 @@ _XRead(Display *dpy, char *data, long size)
 {
 	return 0;
 }
-
-
-
-/*		=========		*/
-#if 0
-
-Status
-XkbGetGeometry(Display *dpy,XkbDescPtr xkb)
-{
-	return BadValue;
-}
-
-Display *
-XkbOpenDisplay(char * name, int * ev_rtrn, int * err_rtrn, int * major_rtrn,
-    int * minor_rtrn, int * reason)
-{
-	return NULL;
-}
-
-Bool
-XkbLibraryVersion(int *libMajorRtrn,int *libMinorRtrn)
-{
-	return True;
-}
-
-int
-XFlush(Display *dpy)
-{
-	return 1;
-}
-
-#endif
