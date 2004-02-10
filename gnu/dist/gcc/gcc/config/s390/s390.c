2d9d64e9bdeb5d9a617c29689d59479be9acad03
mrg <mrg@NetBSD.org>
2004-02-10 12:05:11 +0000

    import GCC 3.3.3-pre20040209.  this fixes dozens of bugs from the GCC database, several logged by netbsd users/developers.  total count in various area: 	3 - Bootstrap failures and issues 	19 - Internal compiler errors (multi-platform) 	13 - C and optimization bugs 	42 - C++ compiler and library 	1 - Java compiler and library 	1 - Objective-C compiler and library 	3 - Fortran compiler and library 	5 - x86-specific (Intel/AMD) 	4 - PowerPC-specific 	3 - SPARC-specific 	1 - ARM-specific 	4 - ia64-specific 	3 - Alpha-specific 	2 - HPPA-specific 	1 - S390-specific 	6 - SH-specific 	5 - Other embedded target specific 	1 - GNU HURD-specific 	1 - Tru64 Unix specific 	2 - AIX-specific 	1 - IRIX-specific 	1 - Solaris-specific 	2 - Testsuite problems (compiler is not affected) 	1 - Miscellaneous
    total:  125

--- gnu/dist/gcc/gcc/config/s390/s390.c
+++ gnu/dist/gcc/gcc/config/s390/s390.c
@@ -2943,6 +2943,30 @@ s390_expand_cmpstr (target, op0, op1, len)
 #endif
 }
 
+/* This is called from dwarf2out.c via ASM_OUTPUT_DWARF_DTPREL.
+   We need to emit DTP-relative relocations.  */
+
+void
+s390_output_dwarf_dtprel (file, size, x)
+     FILE *file;
+     int size;
+     rtx x;
+{
+  switch (size)
+    {
+    case 4:
+      fputs ("\t.long\t", file);
+      break;
+    case 8:
+      fputs ("\t.quad\t", file);
+      break;
+    default:
+      abort ();
+    }
+  output_addr_const (file, x);
+  fputs ("@DTPOFF", file);
+}
+
 /* In the name of slightly smaller debug output, and to cater to
    general assembler losage, recognize various UNSPEC sequences
    and turn them back into a direct symbol reference.  */
@@ -5594,6 +5618,8 @@ s390_function_arg_pass_by_reference (mode, type)
      tree type;
 {
   int size = s390_function_arg_size (mode, type);
+  if (size > 8)
+    return 1;
 
   if (type)
     {
@@ -5601,7 +5627,8 @@ s390_function_arg_pass_by_reference (mode, type)
           size != 1 && size != 2 && size != 4 && size != 8)
         return 1;
 
-      if (TREE_CODE (type) == COMPLEX_TYPE)
+      if (TREE_CODE (type) == COMPLEX_TYPE
+          || TREE_CODE (type) == VECTOR_TYPE)
         return 1;
     }
   return 0;
--- gnu/dist/gcc/gcc/cp/init.c
+++ gnu/dist/gcc/gcc/cp/init.c
@@ -245,15 +245,18 @@ build_zero_init (tree type, tree nelts, bool static_storage_p)
       max_index = nelts ? nelts : array_type_nelts (type);
       my_friendly_assert (TREE_CODE (max_index) == INTEGER_CST, 20030618);
 
-      for (index = size_zero_node;
-	   !tree_int_cst_lt (max_index, index);
-	   index = size_binop (PLUS_EXPR, index, size_one_node))
-	inits = tree_cons (index,
-			   build_zero_init (TREE_TYPE (type),
-					    /*nelts=*/NULL_TREE,
-					    static_storage_p),
-			   inits);
-      CONSTRUCTOR_ELTS (init) = nreverse (inits);
+      /* A zero-sized array, which is accepted as an extension, will
+         have an upper bound of -1.  */
+      if (!tree_int_cst_equal (max_index, integer_minus_one_node))
+        for (index = size_zero_node;
+             !tree_int_cst_lt (max_index, index);
+             index = size_binop (PLUS_EXPR, index, size_one_node))
+          inits = tree_cons (index,
+                             build_zero_init (TREE_TYPE (type),
+                                              /*nelts=*/NULL_TREE,
+                                              static_storage_p),
+                             inits);
+         CONSTRUCTOR_ELTS (init) = nreverse (inits);
     }
   else if (TREE_CODE (type) == REFERENCE_TYPE)
     ;
@@ -544,6 +547,7 @@ sort_mem_initializers (tree t, tree mem_inits)
 	    cp_warning_at ("  `%#D'", subobject);
 	  else
 	    warning ("  base `%T'", subobject);
+	  warning ("  when initialized here");
 	}
 
       /* Look again, from the beginning of the list.  */
@@ -2832,11 +2836,13 @@ build_vec_init (base, maxindex, init, from_array)
 
 	  num_initialized_elts++;
 
+	  current_stmt_tree ()->stmts_are_full_exprs_p = 1;
 	  if (IS_AGGR_TYPE (type) || TREE_CODE (type) == ARRAY_TYPE)
 	    finish_expr_stmt (build_aggr_init (baseref, elt, 0));
 	  else
 	    finish_expr_stmt (build_modify_expr (baseref, NOP_EXPR,
 						 elt));
+	  current_stmt_tree ()->stmts_are_full_exprs_p = 0;
 
 	  finish_expr_stmt (build_unary_op (PREINCREMENT_EXPR, base, 0));
 	  finish_expr_stmt (build_unary_op (PREDECREMENT_EXPR, iterator, 0));
@@ -3092,23 +3098,35 @@ build_delete (type, addr, auto_delete, flags, use_global_delete)
 
   if (TREE_CODE (type) == POINTER_TYPE)
     {
+      bool complete_p = true;
+
       type = TYPE_MAIN_VARIANT (TREE_TYPE (type));
       if (TREE_CODE (type) == ARRAY_TYPE)
 	goto handle_array;
 
-      if (VOID_TYPE_P (type)
-	  /* We don't want to warn about delete of void*, only other
-	     incomplete types.  Deleting other incomplete types
-	     invokes undefined behavior, but it is not ill-formed, so
-	     compile to something that would even do The Right Thing
-	     (TM) should the type have a trivial dtor and no delete
-	     operator.  */
-	  || !complete_type_or_diagnostic (type, addr, 1)
-	  || !IS_AGGR_TYPE (type))
+      /* We don't want to warn about delete of void*, only other
+	  incomplete types.  Deleting other incomplete types
+	  invokes undefined behavior, but it is not ill-formed, so
+	  compile to something that would even do The Right Thing
+	  (TM) should the type have a trivial dtor and no delete
+	  operator.  */
+      if (!VOID_TYPE_P (type))
 	{
-	  /* Call the builtin operator delete.  */
-	  return build_builtin_delete_call (addr);
+	  complete_type (type);
+	  if (!COMPLETE_TYPE_P (type))
+	    {
+	      warning ("possible problem detected in invocation of "
+		       "delete operator:");
+	      cxx_incomplete_type_diagnostic (addr, type, 1);
+	      inform ("neither the destructor nor the class-specific "
+		      "operator delete will be called, even if they are "
+		      "declared when the class is defined.");
+	      complete_p = false;
+	    }
 	}
+      if (VOID_TYPE_P (type) || !complete_p || !IS_AGGR_TYPE (type))
+	/* Call the builtin operator delete.  */
+	return build_builtin_delete_call (addr);
       if (TREE_SIDE_EFFECTS (addr))
 	addr = save_expr (addr);
 
