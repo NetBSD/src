cb50aa8d322d85f8fa35bbcb3027e1accc824854
jmc <jmc@NetBSD.org>
2004-03-15 05:15:54 +0000

    Pullup rev hand patch (requested by wiz in ticket #1620)
    Correct version # reported to correct one

--- gnu/usr.bin/cvs/include/config.h
+++ gnu/usr.bin/cvs/include/config.h
@@ -391,13 +391,13 @@
 #define PACKAGE_NAME "Concurrent Versions System (CVS)"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "Concurrent Versions System (CVS) 1.11.5"
+#define PACKAGE_STRING "Concurrent Versions System (CVS) 1.11.10"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "cvs"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "1.11.5"
+#define PACKAGE_VERSION "1.11.10"
 
 /* Path to the pr utility */
 #define PR_PROGRAM "/usr/bin/pr"
