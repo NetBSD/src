79cd74627c3e5a32314fcad806c367d94a7d50a5
jonathan <jonathan@NetBSD.org>
2004-03-17 00:21:43 +0000

    sys/netinet6/ip6_ecn.h is reportedly a FreeBSD-ism; NetBSD has prototypes for the IPv6 ECN ingress/egress functions in sys/netinet/ip_ecn.h, inside an #ifdef INET6 wrapper.   So, wrap sys/netipsec ocurrences of 	#include <netinet6/ip6_ecn.h> in #ifdef __FreeBSD__/#endif, until both camps can agree on this teensy little piece of namespace. Affects:     ipsec_output.c xform_ah.c xform_esp.c xform_ipip.c

--- sys/netipsec/xform_esp.c
+++ sys/netipsec/xform_esp.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: xform_esp.c,v 1.4 2003/10/06 22:05:15 tls Exp $	*/
+/*	$NetBSD: xform_esp.c,v 1.5 2004/03/17 00:21:43 jonathan Exp $	*/
 /*	$FreeBSD: src/sys/netipsec/xform_esp.c,v 1.2.2.1 2003/01/24 05:11:36 sam Exp $	*/
 /*	$OpenBSD: ip_esp.c,v 1.69 2001/06/26 06:18:59 angelos Exp $ */
 
@@ -39,7 +39,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: xform_esp.c,v 1.4 2003/10/06 22:05:15 tls Exp $");
+__KERNEL_RCSID(0, "$NetBSD: xform_esp.c,v 1.5 2004/03/17 00:21:43 jonathan Exp $");
 
 #include "opt_inet.h"
 #ifdef __FreeBSD__
@@ -74,7 +74,9 @@ __KERNEL_RCSID(0, "$NetBSD: xform_esp.c,v 1.4 2003/10/06 22:05:15 tls Exp $");
 #ifdef INET6
 #include <netinet6/ip6_var.h>
 #include <netipsec/ipsec6.h>
-#include <netinet6/ip6_ecn.h>
+#  ifdef __FreeBSD__
+#  include <netinet6/ip6_ecn.h>
+#  endif
 #endif
 
 #include <netipsec/key.h>
--- sys/netipsec/xform_ipip.c
+++ sys/netipsec/xform_ipip.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: xform_ipip.c,v 1.8 2004/01/16 11:06:27 scw Exp $	*/
+/*	$NetBSD: xform_ipip.c,v 1.9 2004/03/17 00:21:43 jonathan Exp $	*/
 /*	$FreeBSD: src/sys/netipsec/xform_ipip.c,v 1.3.2.1 2003/01/24 05:11:36 sam Exp $	*/
 /*	$OpenBSD: ip_ipip.c,v 1.25 2002/06/10 18:04:55 itojun Exp $ */
 
@@ -39,7 +39,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: xform_ipip.c,v 1.8 2004/01/16 11:06:27 scw Exp $");
+__KERNEL_RCSID(0, "$NetBSD: xform_ipip.c,v 1.9 2004/03/17 00:21:43 jonathan Exp $");
 
 /*
  * IP-inside-IP processing
@@ -86,7 +86,9 @@ __KERNEL_RCSID(0, "$NetBSD: xform_ipip.c,v 1.8 2004/01/16 11:06:27 scw Exp $");
 #ifdef INET6
 #include <netinet/ip6.h>
 #include <netipsec/ipsec6.h>
-#include <netinet6/ip6_ecn.h>
+#  ifdef __FreeBSD__
+#  include <netinet6/ip6_ecn.h>
+#  endif
 #include <netinet6/in6_var.h>
 #include <netinet6/ip6protosw.h>
 #endif
