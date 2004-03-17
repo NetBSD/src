339527b928fda73b43ca746673cb417343ccccd0
matt <matt@NetBSD.org>
2004-03-17 20:16:21 +0000

    Make the assumption that all MACHINE_ARCH'es ending in 'el' are little endian and all ending in 'eb' are big endian.

--- share/mk/bsd.endian.mk
+++ share/mk/bsd.endian.mk
@@ -1,4 +1,4 @@
-#	$NetBSD: bsd.endian.mk,v 1.11 2004/03/17 19:57:49 he Exp $
+#	$NetBSD: bsd.endian.mk,v 1.12 2004/03/17 20:16:21 matt Exp $
 
 .if !defined(_BSD_ENDIAN_MK_)
 _BSD_ENDIAN_MK_=1
@@ -7,24 +7,19 @@ _BSD_ENDIAN_MK_=1
 
 .if ${MACHINE_ARCH} == "alpha" || \
     ${MACHINE_ARCH} == "arm" || \
-    ${MACHINE_ARCH} == "x86_64" || \
     ${MACHINE_ARCH} == "i386" || \
-    ${MACHINE_ARCH} == "mipsel" || \
     ${MACHINE_ARCH} == "ns32k" || \
-    ${MACHINE_ARCH} == "sh3el" || \
-    ${MACHINE_ARCH} == "sh5el" || \
-    ${MACHINE_ARCH} == "vax"
+    ${MACHINE_ARCH} == "vax" || \
+    ${MACHINE_ARCH} == "x86_64" || \
+    ${MACHINE_ARCH:C/^.*el$/el/} == "el"
 TARGET_ENDIANNESS=	1234
-.elif ${MACHINE_ARCH} == "armeb" || \
-      ${MACHINE_ARCH} == "hppa" || \
+.elif ${MACHINE_ARCH} == "hppa" || \
       ${MACHINE_ARCH} == "m68000" || \
       ${MACHINE_ARCH} == "m68k" || \
-      ${MACHINE_ARCH} == "mipseb" || \
       ${MACHINE_ARCH} == "powerpc" || \
-      ${MACHINE_ARCH} == "sh3eb" || \
-      ${MACHINE_ARCH} == "sh5eb" || \
       ${MACHINE_ARCH} == "sparc" || \
-      ${MACHINE_ARCH} == "sparc64"
+      ${MACHINE_ARCH} == "sparc64" || \
+      ${MACHINE_ARCH:C/^.*eb$/eb/} == "eb"
 TARGET_ENDIANNESS=	4321
 .endif
 
