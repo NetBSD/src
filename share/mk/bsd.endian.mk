86171a3a44825e35094e027cc16b21da2eacb0be
he <he@NetBSD.org>
2004-03-17 19:57:49 +0000

    Add sh3el and sh3eb MACHINE_ARCHs.

--- share/mk/bsd.endian.mk
+++ share/mk/bsd.endian.mk
@@ -1,4 +1,4 @@
-#	$NetBSD: bsd.endian.mk,v 1.10 2004/03/16 14:46:44 he Exp $
+#	$NetBSD: bsd.endian.mk,v 1.11 2004/03/17 19:57:49 he Exp $
 
 .if !defined(_BSD_ENDIAN_MK_)
 _BSD_ENDIAN_MK_=1
@@ -11,6 +11,7 @@ _BSD_ENDIAN_MK_=1
     ${MACHINE_ARCH} == "i386" || \
     ${MACHINE_ARCH} == "mipsel" || \
     ${MACHINE_ARCH} == "ns32k" || \
+    ${MACHINE_ARCH} == "sh3el" || \
     ${MACHINE_ARCH} == "sh5el" || \
     ${MACHINE_ARCH} == "vax"
 TARGET_ENDIANNESS=	1234
@@ -20,6 +21,7 @@ TARGET_ENDIANNESS=	1234
       ${MACHINE_ARCH} == "m68k" || \
       ${MACHINE_ARCH} == "mipseb" || \
       ${MACHINE_ARCH} == "powerpc" || \
+      ${MACHINE_ARCH} == "sh3eb" || \
       ${MACHINE_ARCH} == "sh5eb" || \
       ${MACHINE_ARCH} == "sparc" || \
       ${MACHINE_ARCH} == "sparc64"
