51594002c3d59e8515cf9604349bc4174a81b516
mrg <mrg>
2004-03-21 01:22:12 +0000

    also define __sparcv9.  noted by heas@

--- gnu/dist/gcc/gcc/config/sparc/netbsd-elf.h
+++ gnu/dist/gcc/gcc/config/sparc/netbsd-elf.h
@@ -29,6 +29,7 @@ Boston, MA 02111-1307, USA.  */
 	  NETBSD_OS_CPP_BUILTINS_LP64();		\
 	  builtin_define ("__sparc64__");		\
 	  builtin_define ("__sparc_v9__");		\
+	  builtin_define ("__sparcv9");			\
 	}						\
       else						\
 	builtin_define ("__sparc");			\
