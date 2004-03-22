7a4a03f415de4a5774e6124da26c952acab37102
jdolecek <jdolecek@NetBSD.org>
2004-03-22 16:40:48 +0000

    remove apparently meaningless part of one debug printf insmbfs_lookup()

--- sys/fs/smbfs/smbfs_vnops.c
+++ sys/fs/smbfs/smbfs_vnops.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: smbfs_vnops.c,v 1.41 2004/03/21 13:43:09 jdolecek Exp $	*/
+/*	$NetBSD: smbfs_vnops.c,v 1.42 2004/03/22 16:40:48 jdolecek Exp $	*/
 
 /*-
  * Copyright (c) 2003 The NetBSD Foundation, Inc.
@@ -71,7 +71,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: smbfs_vnops.c,v 1.41 2004/03/21 13:43:09 jdolecek Exp $");
+__KERNEL_RCSID(0, "$NetBSD: smbfs_vnops.c,v 1.42 2004/03/22 16:40:48 jdolecek Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -1198,7 +1198,7 @@ smbfs_lookup(v)
 	    (cnp->cn_nameiop == DELETE || cnp->cn_nameiop == RENAME))
 		return (EROFS);
 
-	SMBVDEBUG("%d '%.*s' in '%.*s' id=d\n", nameiop, nmlen, name, 
+	SMBVDEBUG("%d '%.*s' in '%.*s'\n", nameiop, nmlen, name, 
 	    (int) VTOSMB(dvp)->n_nmlen, VTOSMB(dvp)->n_name);
 
 	islastcn = flags & ISLASTCN;
