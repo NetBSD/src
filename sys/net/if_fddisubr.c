54c725070b965f1fac04eed78febb50626bbd6b8
matt <matt@NetBSD.org>
2004-03-22 18:02:12 +0000

    Update my copyright to not include advertising clause.

--- sys/net/if_fddisubr.c
+++ sys/net/if_fddisubr.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: if_fddisubr.c,v 1.51 2003/08/07 16:32:53 agc Exp $	*/
+/*	$NetBSD: if_fddisubr.c,v 1.52 2004/03/22 18:02:12 matt Exp $	*/
 
 /*
  * Copyright (C) 1995, 1996, 1997, and 1998 WIDE Project.
@@ -74,13 +74,9 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed by the University of
- *	California, Berkeley and its contributors.
- * 4. Neither the name of the University nor the names of its contributors
- *    may be used to endorse or promote products derived from this software
- *    without specific prior written permission.
+ * 3. The name of its contributor may not be used to endorse or promote
+ *    products derived from this software without specific prior written
+ *    permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
@@ -100,7 +96,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: if_fddisubr.c,v 1.51 2003/08/07 16:32:53 agc Exp $");
+__KERNEL_RCSID(0, "$NetBSD: if_fddisubr.c,v 1.52 2004/03/22 18:02:12 matt Exp $");
 
 #include "opt_inet.h"
 #include "opt_atalk.h"
