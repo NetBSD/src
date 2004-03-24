2c9d676de09db566aec6df14f02f7f471cbacbb4
drochner <drochner@NetBSD.org>
2004-03-24 16:03:06 +0000

    remove license clauses 3 and 4 from my copyright notice

--- sys/lib/libsa/ufs_ls.c
+++ sys/lib/libsa/ufs_ls.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: ufs_ls.c,v 1.6 2003/08/07 16:32:31 agc Exp $	 */
+/*	$NetBSD: ufs_ls.c,v 1.7 2004/03/24 16:03:06 drochner Exp $	 */
 
 /*
  * Copyright (c) 1993
@@ -41,13 +41,6 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed by the University of
- *	California, Berkeley and its contributors.
- * 4. Neither the name of the University nor the names of its contributors
- *    may be used to endorse or promote products derived from this software
- *    without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
