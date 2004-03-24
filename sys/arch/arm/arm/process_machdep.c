ad278e1d83f3407a9c1b7bacdb0c506c8fb6ce9a
ws <ws@NetBSD.org>
2004-03-24 16:39:52 +0000

    Fix Frank Lancaster's broken license (after consulting with him). Pointed out by wiz.

--- sys/arch/arm/arm/process_machdep.c
+++ sys/arch/arm/arm/process_machdep.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: process_machdep.c,v 1.11 2003/08/07 16:26:52 agc Exp $	*/
+/*	$NetBSD: process_machdep.c,v 1.12 2004/03/24 16:39:52 ws Exp $	*/
 
 /*
  * Copyright (c) 1993 The Regents of the University of California.
@@ -38,6 +38,35 @@
 /*
  * Copyright (c) 1995 Frank Lancaster.  All rights reserved.
  * Copyright (c) 1995 Tools GmbH.  All rights reserved.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *      This product includes software developed by TooLs GmbH.
+ * 4. The name of TooLs GmbH may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY TOOLS GMBH ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL TOOLS GMBH BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/*
  * Copyright (c) 1995 Charles M. Hannum.  All rights reserved.
  * Copyright (c) 1993 Jan-Simon Pendry
  * All rights reserved.
@@ -104,7 +133,7 @@
 
 #include <sys/param.h>
 
-__KERNEL_RCSID(0, "$NetBSD: process_machdep.c,v 1.11 2003/08/07 16:26:52 agc Exp $");
+__KERNEL_RCSID(0, "$NetBSD: process_machdep.c,v 1.12 2004/03/24 16:39:52 ws Exp $");
 
 #include <sys/proc.h>
 #include <sys/ptrace.h>
