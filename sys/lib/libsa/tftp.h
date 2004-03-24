f13fbf7813685d34b9c10dd8a3361397fcbc0149
drochner <drochner@NetBSD.org>
2004-03-24 17:26:53 +0000

    remove license clauses 3 and 4 from my cpoyright notices

--- sys/lib/libsa/tftp.h
+++ sys/lib/libsa/tftp.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: tftp.h,v 1.4 2003/08/18 15:45:29 dsl Exp $	*/
+/*	$NetBSD: tftp.h,v 1.5 2004/03/24 17:29:14 drochner Exp $	*/
 
 /*
  * Copyright (c) 1996
@@ -12,12 +12,6 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed for the NetBSD Project
- *	by Matthias Drochner.
- * 4. The name of the author may not be used to endorse or promote products
- *    derived from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
