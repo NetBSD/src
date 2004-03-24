6fd3ca990cb47f72e7464f2b43b269747e9fc64e
drochner <drochner@NetBSD.org>
2004-03-24 16:34:29 +0000

    remove license clauses 3 and 4 from my cpoyright notices

--- sys/arch/i386/stand/lib/biosdisk.c
+++ sys/arch/i386/stand/lib/biosdisk.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: biosdisk.c,v 1.19 2003/10/31 10:49:25 hannken Exp $	*/
+/*	$NetBSD: biosdisk.c,v 1.20 2004/03/24 16:34:29 drochner Exp $	*/
 
 /*
  * Copyright (c) 1996, 1998
@@ -12,12 +12,6 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed for the NetBSD Project
- *	by Matthias Drochner.
- * 4. The name of the author may not be used to endorse or promote products
- *    derived from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
