d77d671a5923519a727eb9f4e6304ba1eddf00bd
sekiya <sekiya>
2004-03-25 00:53:58 +0000

    The lpt driver doesn't play nicely with the serial ports -- they share the same interrupt, and something isn't quite right with the mace interrupt dispatcher.  Disable for now.
    Pointed out by Tillman Hodgson and confirmed by ozone on port-sgimips.

--- sys/arch/sgimips/mace/lpt_mace.c
+++ sys/arch/sgimips/mace/lpt_mace.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: lpt_mace.c,v 1.2 2004/01/19 10:28:28 sekiya Exp $	*/
+/*	$NetBSD: lpt_mace.c,v 1.3 2004/03/25 00:53:58 sekiya Exp $	*/
 
 /*
  * Copyright (c) 2003 Christopher SEKIYA 
@@ -34,7 +34,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: lpt_mace.c,v 1.2 2004/01/19 10:28:28 sekiya Exp $");
+__KERNEL_RCSID(0, "$NetBSD: lpt_mace.c,v 1.3 2004/03/25 00:53:58 sekiya Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -79,8 +79,8 @@ lpt_mace_match(parent, match, aux)
 	struct cfdata *match;
 	void *aux;
 {
-
-	return 1;
+	/* Not A Real Driver(tm) */
+	return 0;
 }
 
 static void
