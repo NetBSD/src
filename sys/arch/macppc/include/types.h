39ec60aef504a864901de7ad87876fa80c359b4d
matt <matt>
2004-03-25 18:52:25 +0000

    Enable generic software interrupts on macppc.

--- sys/arch/macppc/include/types.h
+++ sys/arch/macppc/include/types.h
@@ -1,5 +1,6 @@
-/*	$NetBSD: types.h,v 1.6 2002/02/28 03:17:34 simonb Exp $	*/
+/*	$NetBSD: types.h,v 1.7 2004/03/25 18:52:25 matt Exp $	*/
 
 #include <powerpc/types.h>
 
 #define	__HAVE_DEVICE_REGISTER
+#define	__HAVE_GENERIC_SOFT_INTERRUPTS
