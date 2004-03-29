28f9347a21a9da5d4a5c76174ca01d70704b847b
christos <christos@NetBSD.org>
2004-03-29 21:44:07 +0000

    include <bsd.sys.mk> because uue files need TOOL_UUDECODE and in the USETOOLS=no case this never gets defined.

--- share/mk/bsd.files.mk
+++ share/mk/bsd.files.mk
@@ -1,9 +1,10 @@
-#	$NetBSD: bsd.files.mk,v 1.33 2004/03/19 06:10:27 jmc Exp $
+#	$NetBSD: bsd.files.mk,v 1.34 2004/03/29 21:44:07 christos Exp $
 
 .if !defined(_BSD_FILES_MK_)
 _BSD_FILES_MK_=1
 
 .include <bsd.init.mk>
+.include <bsd.sys.mk>
 
 .if !target(__fileinstall)
 ##### Basic targets
