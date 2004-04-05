18f2f5c83845001300a9137e63407d7c630803c7
aymeric <aymeric@NetBSD.org>
2004-04-05 07:22:39 +0000

    bump version: failed recovery doesn't prevent edition anymore

--- usr.bin/vi/ex/version.h
+++ usr.bin/vi/ex/version.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: version.h,v 1.13 2003/08/28 05:00:09 dsl Exp $	*/
+/*	$NetBSD: version.h,v 1.14 2004/04/05 07:22:39 aymeric Exp $	*/
 
 #define	VI_VERSION \
-    "Version 1.79nb10 (10/23/96) The CSRG, University of California, Berkeley."
+    "Version 1.79nb11 (10/23/96) The CSRG, University of California, Berkeley."
