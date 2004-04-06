b060dfe9c81cc5da72bf6116a7738d191f82f722
jmc <jmc>
2004-04-06 05:25:04 +0000

    Pullup patch (requested by briggs in ticket #1648)
    Pull in changes to support the Intel 82801EB/ER (ICH5) Controller. PR#25002

--- sys/dev/pci/if_fxp_pci.c
+++ sys/dev/pci/if_fxp_pci.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: if_fxp_pci.c,v 1.22.4.6 2003/08/15 19:29:32 tron Exp $	*/
+/*	$NetBSD: if_fxp_pci.c,v 1.22.4.7 2004/04/06 05:25:04 jmc Exp $	*/
 
 /*-
  * Copyright (c) 1997, 1998, 1999, 2000, 2001 The NetBSD Foundation, Inc.
@@ -43,7 +43,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: if_fxp_pci.c,v 1.22.4.6 2003/08/15 19:29:32 tron Exp $");
+__KERNEL_RCSID(0, "$NetBSD: if_fxp_pci.c,v 1.22.4.7 2004/04/06 05:25:04 jmc Exp $");
 
 #include "rnd.h"
 
@@ -145,6 +145,8 @@ const struct fxp_pci_product {
 	  "Intel PRO/100 VM Network Controller with 82562ET PHY" },
 	{ PCI_PRODUCT_INTEL_PRO_100_M,
 	  "Intel PRO/100 M Network Controller" },
+	{ PCI_PRODUCT_INTEL_82801EB_LAN,
+	  "Intel 82801EB/ER (ICH5) Network Controller" },
 	{ 0,
 	  NULL },
 };
@@ -381,6 +383,7 @@ fxp_pci_attach(parent, self, aux)
 	case PCI_PRODUCT_INTEL_PRO_100_VM_3:
 	case PCI_PRODUCT_INTEL_PRO_100_VM_4:
 	case PCI_PRODUCT_INTEL_PRO_100_VM_6:
+	case PCI_PRODUCT_INTEL_82801EB_LAN:
 		printf(": %s, rev %d\n", fpp->fpp_name, sc->sc_rev);
 
 		/*
