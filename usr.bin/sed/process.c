065e959811d6b143c6159dadec30bcf2dfc5e95f
jmc <jmc@NetBSD.org>
2004-04-07 22:44:06 +0000

    Pullup rev 1.34 (requested by itojun in ticket #108)
    Prevent integer underflow.

--- usr.bin/sed/process.c
+++ usr.bin/sed/process.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: process.c,v 1.26.4.1 2001/10/27 20:06:50 he Exp $	*/
+/*	$NetBSD: process.c,v 1.26.4.2 2004/04/07 22:44:06 jmc Exp $	*/
 
 /*-
  * Copyright (c) 1992 Diomidis Spinellis.
@@ -42,7 +42,7 @@
 #if 0
 static char sccsid[] = "@(#)process.c	8.6 (Berkeley) 4/20/94";
 #else
-__RCSID("$NetBSD: process.c,v 1.26.4.1 2001/10/27 20:06:50 he Exp $");
+__RCSID("$NetBSD: process.c,v 1.26.4.2 2004/04/07 22:44:06 jmc Exp $");
 #endif
 #endif /* not lint */
 
@@ -570,7 +570,7 @@ regsub(sp, string, src)
 	char c, *dst;
 
 #define	NEEDSP(reqlen)							\
-	if (sp->len >= sp->blen - (reqlen) - 1) {			\
+	if (sp->len + (reqlen) + 1 >= sp->blen) {			\
 		sp->blen += (reqlen) + 1024;				\
 		sp->space = sp->back = xrealloc(sp->back, sp->blen);	\
 		dst = sp->space + sp->len;				\
