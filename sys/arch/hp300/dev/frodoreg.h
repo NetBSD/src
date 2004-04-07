e35413c02dbacc2f058f38b74e08650b1d46f108
tsutsui <tsutsui@NetBSD.org>
2004-04-07 13:41:10 +0000

    Adjust FRODO_BASE macro since com_frodo uses bus_space_map(9) which takes offset from intiobase instead of KVA.
    Now com* at frodo? is probed properly.
    (but not tested because I don't have a special cable for apci ports yet)

--- sys/arch/hp300/dev/frodoreg.h
+++ sys/arch/hp300/dev/frodoreg.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: frodoreg.h,v 1.1 1997/05/12 08:03:49 thorpej Exp $	*/
+/*	$NetBSD: frodoreg.h,v 1.2 2004/04/07 13:41:10 tsutsui Exp $	*/
 
 /*
  * Copyright (c) 1997 Michael Smith.  All rights reserved.
@@ -25,8 +25,8 @@
  * SUCH DAMAGE.
  */
 
-/* Base address of the Frodo part */
-#define	FRODO_BASE		(INTIOBASE + 0x1c000)
+/* Base address offset from intiobase of the Frodo part */
+#define	FRODO_BASE		0x1c000
 
 /*
  * Where we find the 8250-like APCI ports, and how far apart they are.
