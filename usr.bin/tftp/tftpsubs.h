1231c6b62e6c8ca6439dc6098e9200c84cb73170
jmc <jmc>
2004-04-07 22:27:20 +0000

    Pullup rev 1.3 (requested by itojun in ticket #96)
    Finish pulling up new options code from trunk

--- usr.bin/tftp/tftpsubs.h
+++ usr.bin/tftp/tftpsubs.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: tftpsubs.h,v 1.2 1994/12/08 09:51:32 jtc Exp $	*/
+/*	$NetBSD: tftpsubs.h,v 1.2.16.1 2004/04/07 22:27:20 jmc Exp $	*/
 
 /*
  * Copyright (c) 1993
@@ -40,10 +40,10 @@
  * server.
  */
 struct tftphdr *r_init __P((void));
-void	read_ahead __P((FILE *, int));
-int	readit __P((FILE *, struct tftphdr **, int));
+void	read_ahead __P((FILE *, int, int));
+int	readit __P((FILE *, struct tftphdr **, int, int));
 
-int	synchnet __P((int));
+int	synchnet __P((int, int));
 
 struct tftphdr *w_init __P((void));
 int	write_behind __P((FILE *, int));
