95d43ff2e629adb1c2abfa3676c972fa1eb68670
jdc <jdc>
2004-04-08 20:06:45 +0000

    Pull up revision 1.89 (requested by atatat in ticket #92)
    Don't copy the size that sysctl gave back directly, copy the size of
    the description.

--- sbin/sysctl/sysctl.c
+++ sbin/sysctl/sysctl.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: sysctl.c,v 1.86.2.2 2004/04/08 19:34:16 jdc Exp $ */
+/*	$NetBSD: sysctl.c,v 1.86.2.3 2004/04/08 20:06:45 jdc Exp $ */
 
 /*-
  * Copyright (c) 2003 The NetBSD Foundation, Inc.
@@ -72,7 +72,7 @@ __COPYRIGHT(
 #if 0
 static char sccsid[] = "@(#)sysctl.c	8.1 (Berkeley) 6/6/93";
 #else
-__RCSID("$NetBSD: sysctl.c,v 1.86.2.2 2004/04/08 19:34:16 jdc Exp $");
+__RCSID("$NetBSD: sysctl.c,v 1.86.2.3 2004/04/08 20:06:45 jdc Exp $");
 #endif
 #endif /* not lint */
 
@@ -1441,7 +1441,7 @@ getdesc1(int *name, u_int namelen, struct sysctlnode *pnode)
 	if (desc == NULL)
 		desc = (char *)-1;
 	if (desc != (char *)-1)
-		memcpy(desc, &d->descr_str[0], sz);
+		memcpy(desc, &d->descr_str[0], d->descr_len);
 	name[namelen - 1] = node.sysctl_num;
 	if (pnode->sysctl_desc != NULL &&
 	    pnode->sysctl_desc != (const char *)-1)
@@ -1502,7 +1502,7 @@ getdesc(int *name, u_int namelen, struct sysctlnode *pnode)
 			if (desc == NULL)
 				desc = (char *)-1;
 			else
-				memcpy(desc, &p->descr_str[0], sz);
+				memcpy(desc, &p->descr_str[0], p->descr_len);
 			node->sysctl_desc = desc;
 		}
 	}
