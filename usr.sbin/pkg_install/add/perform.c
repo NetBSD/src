424d80cba0924f19b495cca06f44e0ab993a20a8
tv <tv>
2004-04-09 18:27:48 +0000

    Re-add umask() setting, and parameterize it so that pkgsrc/bootstrap based pkgsrc platforms can override the value.

--- usr.sbin/pkg_install/add/perform.c
+++ usr.sbin/pkg_install/add/perform.c
@@ -1,11 +1,11 @@
-/*	$NetBSD: perform.c,v 1.95 2004/03/29 21:41:39 tv Exp $	*/
+/*	$NetBSD: perform.c,v 1.96 2004/04/09 18:27:48 tv Exp $	*/
 
 #include <sys/cdefs.h>
 #ifndef lint
 #if 0
 static const char *rcsid = "from FreeBSD Id: perform.c,v 1.44 1997/10/13 15:03:46 jkh Exp";
 #else
-__RCSID("$NetBSD: perform.c,v 1.95 2004/03/29 21:41:39 tv Exp $");
+__RCSID("$NetBSD: perform.c,v 1.96 2004/04/09 18:27:48 tv Exp $");
 #endif
 #endif
 
@@ -704,6 +704,7 @@ ignore_replace_depends_check:
 	if (!NoRecord && !Fake) {
 		char    contents[FILENAME_MAX];
 
+		umask(DEF_UMASK);
 #ifndef __INTERIX
 		if (getuid() != 0)
 			warnx("not running as root - trying to record install anyway");
