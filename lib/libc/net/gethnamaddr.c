f7843ad59332039e2986acb43380d0c35993150c
msaitoh <msaitoh@NetBSD.org>
2004-04-15 08:00:45 +0000

    Pullup rev. 1.58 (requested by itojun in ticket #141):
     Back out rev. 1.56; `pointer points the byte just after the valid
     region' is normal case.  If the parser wants something more, there
     should be another bounds check for it.

--- lib/libc/net/gethnamaddr.c
+++ lib/libc/net/gethnamaddr.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: gethnamaddr.c,v 1.33.4.7 2004/04/07 22:37:51 jmc Exp $	*/
+/*	$NetBSD: gethnamaddr.c,v 1.33.4.8 2004/04/15 08:00:45 msaitoh Exp $	*/
 
 /*
  * ++Copyright++ 1985, 1988, 1993
@@ -61,7 +61,7 @@
 static char sccsid[] = "@(#)gethostnamadr.c	8.1 (Berkeley) 6/4/93";
 static char rcsid[] = "Id: gethnamaddr.c,v 8.21 1997/06/01 20:34:37 vixie Exp ";
 #else
-__RCSID("$NetBSD: gethnamaddr.c,v 1.33.4.7 2004/04/07 22:37:51 jmc Exp $");
+__RCSID("$NetBSD: gethnamaddr.c,v 1.33.4.8 2004/04/15 08:00:45 msaitoh Exp $");
 #endif
 #endif /* LIBC_SCCS and not lint */
 
@@ -203,16 +203,16 @@ dprintf(char *msg, ...)
 
 #define BOUNDED_INCR(x) \
 	do { \
-		if (cp + (x) >= eom) { \
+		cp += (x); \
+		if (cp > eom) { \
 			h_errno = NO_RECOVERY; \
 			return (NULL); \
 		} \
-		cp += (x); \
 	} while (/*CONSTCOND*/0)
 
 #define BOUNDS_CHECK(ptr, count) \
 	do { \
-		if ((ptr) + (count) >= eom) { \
+		if ((ptr) + (count) > eom) { \
 			h_errno = NO_RECOVERY; \
 			return (NULL); \
 		} \
