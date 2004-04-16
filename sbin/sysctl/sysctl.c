7d817825ab72a2d928de7ce0c9fce43c29e1f3c0
tron <tron@NetBSD.org>
2004-04-16 07:54:34 +0000

    Pull up revision 1.91 (requested by atatat in ticket #136): If the node was specifically requested but has no description, print the "no description" message instead of skipping (as happens when recursing into a subtree).  This does not apply to non-node-type nodes.

--- sbin/sysctl/sysctl.c
+++ sbin/sysctl/sysctl.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: sysctl.c,v 1.86.2.6 2004/04/08 20:26:06 jdc Exp $ */
+/*	$NetBSD: sysctl.c,v 1.86.2.7 2004/04/16 07:54:34 tron Exp $ */
 
 /*-
  * Copyright (c) 2003 The NetBSD Foundation, Inc.
@@ -72,7 +72,7 @@ __COPYRIGHT(
 #if 0
 static char sccsid[] = "@(#)sysctl.c	8.1 (Berkeley) 6/6/93";
 #else
-__RCSID("$NetBSD: sysctl.c,v 1.86.2.6 2004/04/08 20:26:06 jdc Exp $");
+__RCSID("$NetBSD: sysctl.c,v 1.86.2.7 2004/04/16 07:54:34 tron Exp $");
 #endif
 #endif /* not lint */
 
@@ -547,7 +547,7 @@ print_tree(int *name, u_int namelen, struct sysctlnode *pnode, u_int type,
 		if (Aflag || type != CTLTYPE_NODE) {
 			if (pnode->sysctl_desc == NULL)
 				getdesc1(name, namelen, pnode);
-			if (Aflag ||
+			if (Aflag || !add ||
 			    (pnode->sysctl_desc != NULL &&
 			     pnode->sysctl_desc != (const char*)-1)) {
 				if (!nflag)
