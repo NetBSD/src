80f6dec1c8e82262980d1729148709b800266178
jmc <jmc@NetBSD.org>
2004-04-29 04:48:40 +0000

    Pullup rev 1.4 (requested by mrg in ticket #223)
    Mega patch fixes several issues & bugs in gzip:
    - stdin bz2/compress support
    - gzip -v pipe support
    - various robustness issues
    - no more .Sh BUGS in gzip.1

--- usr.bin/gzip/unbzip2.c
+++ usr.bin/gzip/unbzip2.c
@@ -1,21 +1,19 @@
-/*	$NetBSD: unbzip2.c,v 1.1.2.2 2004/03/31 09:09:20 grant Exp $	*/
+/*	$NetBSD: unbzip2.c,v 1.1.2.3 2004/04/29 04:48:40 jmc Exp $	*/
 
 /* This file is #included by gzip.c */
 
-#define INBUFSIZE	(64 * 1024)
-#define OUTBUFSIZE	(64 * 1024)
-
 static off_t
-unbzip2(int in, int out)
+unbzip2(int in, int out, char *pre, size_t prelen, off_t *bytes_in)
 {
-	int		n, ret, end_of_file;
+	int		ret, end_of_file;
+	size_t		n;
 	off_t		bytes_out = 0;
 	bz_stream	bzs;
 	static char	*inbuf, *outbuf;
 
-	if (inbuf == NULL && (inbuf = malloc(INBUFSIZE)) == NULL)
+	if (inbuf == NULL && (inbuf = malloc(BUFLEN)) == NULL)
 	        maybe_err(1, "malloc");
-	if (outbuf == NULL && (outbuf = malloc(OUTBUFSIZE)) == NULL)
+	if (outbuf == NULL && (outbuf = malloc(BUFLEN)) == NULL)
 	        maybe_err(1, "malloc");
 
 	bzs.bzalloc = NULL;
@@ -27,22 +25,28 @@ unbzip2(int in, int out)
 	if (ret != BZ_OK)
 	        maybe_errx(1, "bzip2 init");
 
-	bzs.avail_in = 0;
+	/* Prepend. */
+	bzs.avail_in = prelen;
+	bzs.next_in = pre;
+
+	if (bytes_in)
+		*bytes_in = prelen;
 
 	while (ret != BZ_STREAM_END) {
 	        if (bzs.avail_in == 0 && !end_of_file) {
-	                n = read(in, inbuf, INBUFSIZE);
+	                n = read(in, inbuf, BUFLEN);
 	                if (n < 0)
 	                        maybe_err(1, "read");
 	                if (n == 0)
 	                        end_of_file = 1;
 	                bzs.next_in = inbuf;
 	                bzs.avail_in = n;
-	        } else
-	                n = 0;
+			if (bytes_in)
+				*bytes_in += n;
+	        }
 
 	        bzs.next_out = outbuf;
-	        bzs.avail_out = OUTBUFSIZE;
+	        bzs.avail_out = BUFLEN;
 	        ret = BZ2_bzDecompress(&bzs);
 
 	        switch (ret) {
@@ -51,7 +55,7 @@ unbzip2(int in, int out)
 	                if (ret == BZ_OK && end_of_file)
 	                        maybe_err(1, "read");
 	                if (!tflag) {
-	                        n = write(out, outbuf, OUTBUFSIZE - bzs.avail_out);
+	                        n = write(out, outbuf, BUFLEN - bzs.avail_out);
 	                        if (n < 0)
 	                                maybe_err(1, "write");
 	                }
