255f57a8b629c10cd3c4ee1df6b8cce9686e0c67
dyoung <dyoung@NetBSD.org>
2004-04-30 22:57:32 +0000

    Add a malloc(9) type for 802.11 nodes.

--- sys/net80211/ieee80211_node.h
+++ sys/net80211/ieee80211_node.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: ieee80211_node.h,v 1.8 2003/12/14 09:56:53 dyoung Exp $	*/
+/*	$NetBSD: ieee80211_node.h,v 1.9 2004/04/30 22:57:32 dyoung Exp $	*/
 /*-
  * Copyright (c) 2001 Atsushi Onoe
  * Copyright (c) 2002, 2003 Sam Leffler, Errno Consulting
@@ -30,7 +30,7 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
- * $FreeBSD: src/sys/net80211/ieee80211_node.h,v 1.7 2003/10/17 21:41:52 sam Exp $
+ * $FreeBSD: src/sys/net80211/ieee80211_node.h,v 1.10 2004/04/05 22:10:26 sam Exp $
  */
 #ifndef _NET80211_IEEE80211_NODE_H_
 #define _NET80211_IEEE80211_NODE_H_
@@ -165,6 +165,10 @@ ieee80211_unref_node(struct ieee80211_node **ni)
 	mtx_assert(&(_ic)->ic_nodelock, MA_OWNED)
 
 struct ieee80211com;
+
+#ifdef MALLOC_DECLARE
+MALLOC_DECLARE(M_80211_NODE);
+#endif
 
 extern	void ieee80211_node_attach(struct ifnet *);
 extern	void ieee80211_node_lateattach(struct ifnet *);
