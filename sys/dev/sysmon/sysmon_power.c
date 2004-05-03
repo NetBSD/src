2c1ffcf1a6f2b50a584fb81c0055b89c6fb93404
kochi <kochi@NetBSD.org>
2004-05-03 07:43:37 +0000

    Add AC adapter event type.

--- sys/dev/sysmon/sysmon_power.c
+++ sys/dev/sysmon/sysmon_power.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: sysmon_power.c,v 1.8 2003/07/14 15:47:28 lukem Exp $	*/
+/*	$NetBSD: sysmon_power.c,v 1.9 2004/05/03 07:43:37 kochi Exp $	*/
 
 /*
  * Copyright (c) 2003 Wasabi Systems, Inc.
@@ -44,7 +44,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: sysmon_power.c,v 1.8 2003/07/14 15:47:28 lukem Exp $");
+__KERNEL_RCSID(0, "$NetBSD: sysmon_power.c,v 1.9 2004/05/03 07:43:37 kochi Exp $");
 
 #include <sys/param.h>
 #include <sys/reboot.h>
@@ -491,6 +491,24 @@ sysmon_pswitch_event(struct sysmon_pswitch *smpsw, int event)
 		default:
 			printf("%s: unknown lid switch event: %d\n",
 			    smpsw->smpsw_name, event);
+		}
+		break;
+
+	case PSWITCH_TYPE_ACADAPTER:
+		switch (event) {
+		case PSWITCH_EVENT_PRESSED:
+			/*
+			 * Try to enter a power-save state.
+			 */
+			printf("%s: AC adapter offline.\n", smpsw->smpsw_name);
+			break;
+
+		case PSWITCH_EVENT_RELEASED:
+			/*
+			 * Come out of power-save state.
+			 */
+			printf("%s: AC adapter online.\n", smpsw->smpsw_name);
+			break;
 		}
 		break;
 
