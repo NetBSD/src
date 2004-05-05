ac134bc148baa8bbb948e1d1929537c5eade0511
tron <tron>
2004-05-05 15:23:59 +0000

    Pull up revision 1.21 (requested by jonathan in ticket #239): Identify AES-capable chips (7955, 7956) in the driver attach message.

--- sys/dev/pci/hifn7751.c
+++ sys/dev/pci/hifn7751.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: hifn7751.c,v 1.20 2003/11/16 00:22:09 jonathan Exp $	*/
+/*	$NetBSD: hifn7751.c,v 1.20.2.1 2004/05/05 15:23:59 tron Exp $	*/
 /*	$FreeBSD: hifn7751.c,v 1.5.2.7 2003/10/08 23:52:00 sam Exp $ */
 /*	$OpenBSD: hifn7751.c,v 1.140 2003/08/01 17:55:54 deraadt Exp $	*/
 
@@ -48,7 +48,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: hifn7751.c,v 1.20 2003/11/16 00:22:09 jonathan Exp $");
+__KERNEL_RCSID(0, "$NetBSD: hifn7751.c,v 1.20.2.1 2004/05/05 15:23:59 tron Exp $");
 
 #include "rnd.h"
 #include "opencrypto.h"
@@ -847,7 +847,10 @@ report:
 		return ("DES");
 
 	case HIFN_PUSTAT_ENA_2:
-		return ("3DES");
+		if (sc->sc_flags & HIFN_HAS_AES)
+		    return ("3DES/AES");
+		else
+		    return ("3DES");
 
 	default:
 		return ("disabled");
