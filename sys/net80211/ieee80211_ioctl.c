c793aad2aafb3156de18e06ed886d6001d1dc388
dyoung <dyoung@NetBSD.org>
2004-05-06 01:56:08 +0000

    Check if_init error.

--- sys/net80211/ieee80211_ioctl.c
+++ sys/net80211/ieee80211_ioctl.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: ieee80211_ioctl.c,v 1.11 2004/05/05 16:10:29 cube Exp $	*/
+/*	$NetBSD: ieee80211_ioctl.c,v 1.12 2004/05/06 01:56:08 dyoung Exp $	*/
 /*-
  * Copyright (c) 2001 Atsushi Onoe
  * Copyright (c) 2002, 2003 Sam Leffler, Errno Consulting
@@ -35,7 +35,7 @@
 #ifdef __FreeBSD__
 __FBSDID("$FreeBSD: src/sys/net80211/ieee80211_ioctl.c,v 1.13 2004/03/30 22:57:57 sam Exp $");
 #else
-__KERNEL_RCSID(0, "$NetBSD: ieee80211_ioctl.c,v 1.11 2004/05/05 16:10:29 cube Exp $");
+__KERNEL_RCSID(0, "$NetBSD: ieee80211_ioctl.c,v 1.12 2004/05/06 01:56:08 dyoung Exp $");
 #endif
 
 /*
@@ -1375,7 +1375,8 @@ ieee80211_ioctl(struct ifnet *ifp, u_long cmd, caddr_t data)
 		case AF_INET:
 			if ((ifp->if_flags & IFF_UP) == 0) {
 				ifp->if_flags |= IFF_UP;
-				ifp->if_init(ifp);
+				if ((error = ifp->if_init(ifp)) != 0)
+					break;
 			}
 			arp_ifinit(ifp, ifa);
 			break;
