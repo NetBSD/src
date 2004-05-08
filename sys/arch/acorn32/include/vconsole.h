9fec47653d045fc235e981a7a9233168b7c75729
jmc <jmc@NetBSD.org>
2004-05-08 21:04:06 +0000

    Add proper include guards..

--- sys/arch/acorn32/include/vconsole.h
+++ sys/arch/acorn32/include/vconsole.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: vconsole.h,v 1.1 2001/10/05 22:27:53 reinoud Exp $	*/
+/*	$NetBSD: vconsole.h,v 1.2 2004/05/08 21:04:07 jmc Exp $	*/
 
 /*
  * Copyright (c) 1994,1995 Melvyn Tang-Richardson
@@ -46,6 +46,9 @@
  * Hopefully this file has a short life with the new console coming just
  * round the corner
  */
+
+#ifndef _ACORN32_VCONSOLE_H
+#define _ACORN32_VCONSOLE_H
 
 #ifdef _KERNEL
 #define LOSSY           1
@@ -294,4 +297,4 @@ struct console_palette {
 #define CONSOLE_IOCTL_COMPAT_N	_IO( 'n', 107 )
 #define CONSOLE_IOCTL_COMPAT_T	_IO( 't', 107 )
 
-/* End of vconsole.h */
+#endif /* _ACORN32_VCONSOLE_H */
