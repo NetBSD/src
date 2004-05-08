de3fd5d864d54a8bec98c001e0da1d8fcd572b9a
christos <christos@NetBSD.org>
2004-05-08 23:40:01 +0000

    Cleanup and remove suplicate copies of the incorrect power setup code. Thanks to mycroft for suggesting this. XXX: This is a copy of the pci code; another reason cardbus needs to die.

--- sys/dev/cardbus/if_ath_cardbus.c
+++ sys/dev/cardbus/if_ath_cardbus.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: if_ath_cardbus.c,v 1.1 2003/10/14 17:47:03 ichiro Exp $ */
+/*	$NetBSD: if_ath_cardbus.c,v 1.2 2004/05/08 23:40:01 christos Exp $ */
 /*
  * Copyright (c) 2003
  *	Ichiro FUKUHARA <ichiro@ichiro.org>.
@@ -36,7 +36,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: if_ath_cardbus.c,v 1.1 2003/10/14 17:47:03 ichiro Exp $");
+__KERNEL_RCSID(0, "$NetBSD: if_ath_cardbus.c,v 1.2 2004/05/08 23:40:01 christos Exp $");
 
 #include "opt_inet.h"
 #include "opt_ns.h"
@@ -311,29 +311,9 @@ ath_cardbus_setup(struct ath_cardbus_softc *csc)
 	cardbus_chipset_tag_t cc = ct->ct_cc;
 	cardbus_function_tag_t cf = ct->ct_cf;
 	pcireg_t reg;
-	int pmreg;
 
-	if (cardbus_get_capability(cc, cf, csc->sc_tag,
-	    PCI_CAP_PWRMGMT, &pmreg, 0)) {
-		reg = cardbus_conf_read(cc, cf, csc->sc_tag, pmreg + 4);
-#if 1 /* XXX Probably not right for CardBus. */
-		if (reg & 0x03) {
-			/*
-			 * The card has lost all configuration data in
-			 * this state, so punt.
-			 */
-			printf("%s: unable to wake up from power state D3\n",
-			    sc->sc_dev.dv_xname);
-			return;
-		}
-#endif
-		if (reg != 0) {
-			printf("%s: waking up from power state D%d\n",
-			    sc->sc_dev.dv_xname, reg);
-			cardbus_conf_write(cc, cf, csc->sc_tag,
-			    pmreg + 4, 0);
-		}
-	}
+	(void)cardbus_setpowerstate(sc->sc_dev.dv_xname, ct, csc->sc_tag,
+	    PCI_PWR_D0);
 
 	/* Make sure the right access type is on the CardBus bridge. */
 	(*ct->ct_cf->cardbus_ctrl)(cc, CARDBUS_MEM_ENABLE);
