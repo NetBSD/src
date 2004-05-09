3225cc81a4f6a0f991b92be22f89b12110f0a375
jdc <jdc@NetBSD.org>
2004-05-09 08:25:38 +0000

    Pull up revision 1.15 (requested by aymeric in ticket #263)
    bump for latest bug fix: flush the cached line on every DB operation

--- usr.bin/vi/ex/version.h
+++ usr.bin/vi/ex/version.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: version.h,v 1.13.2.1 2004/04/08 21:24:26 jdc Exp $	*/
+/*	$NetBSD: version.h,v 1.13.2.2 2004/05/09 08:25:38 jdc Exp $	*/
 
 #define	VI_VERSION \
-    "Version 1.79nb11 (10/23/96) The CSRG, University of California, Berkeley."
+    "Version 1.79nb12 (10/23/96) The CSRG, University of California, Berkeley."
