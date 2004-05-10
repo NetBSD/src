b74fec71bbf90fdbc6e15ec0ba11b05a960dd874
tron <tron@NetBSD.org>
2004-05-10 15:52:47 +0000

    Pull up revision 1.85 (requested by sjg in ticket #282): Remove use of sh -e when running in compat mode. Its not posix compliant and serves very little purpose. With this change compat and jobs modes are consistent wrt how they treat each line of a script. Add support for the '+' command line prefix as required by posix. Lines prefixed with '+' are executed even when -n is given. [Actually posix says they should also be done for -q and -t] PR: Reviewed by: jmc

--- usr.bin/make/job.c
+++ usr.bin/make/job.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: job.c,v 1.83.2.1 2004/05/10 15:42:23 tron Exp $	*/
+/*	$NetBSD: job.c,v 1.83.2.2 2004/05/10 15:52:47 tron Exp $	*/
 
 /*
  * Copyright (c) 1988, 1989, 1990 The Regents of the University of California.
@@ -70,14 +70,14 @@
  */
 
 #ifndef MAKE_NATIVE
-static char rcsid[] = "$NetBSD: job.c,v 1.83.2.1 2004/05/10 15:42:23 tron Exp $";
+static char rcsid[] = "$NetBSD: job.c,v 1.83.2.2 2004/05/10 15:52:47 tron Exp $";
 #else
 #include <sys/cdefs.h>
 #ifndef lint
 #if 0
 static char sccsid[] = "@(#)job.c	8.2 (Berkeley) 3/19/94";
 #else
-__RCSID("$NetBSD: job.c,v 1.83.2.1 2004/05/10 15:42:23 tron Exp $");
+__RCSID("$NetBSD: job.c,v 1.83.2.2 2004/05/10 15:52:47 tron Exp $");
 #endif
 #endif /* not lint */
 #endif
@@ -704,11 +704,24 @@ JobPrintCommand(ClientData cmdp, ClientData jobp)
     /*
      * Check for leading @' and -'s to control echoing and error checking.
      */
-    while (*cmd == '@' || *cmd == '-') {
-	if (*cmd == '@') {
+    while (*cmd == '@' || *cmd == '-' || (*cmd == '+')) {
+	switch (*cmd) {
+	case '@':
 	    shutUp = TRUE;
-	} else {
+	    break;
+	case '-':
 	    errOff = TRUE;
+	    break;
+	case '+':
+	    if (noSpecials) {
+		/*
+		 * We're not actually executing anything...
+		 * but this one needs to be - use compat mode just for it.
+		 */
+		CompatRunCommand(cmdp, (ClientData)job->node);
+		return 0;
+	    }
+	    break;
 	}
 	cmd++;
     }
