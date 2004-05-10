32ca8dbe5d491b223940ad1bb417bf48fc9fe6ff
tron <tron@NetBSD.org>
2004-05-10 15:02:18 +0000

    Pull up revision 1.13 (requested by jonathan in ticket #280): Redo net.inet.* sysctl subtree for fast-ipsec from scratch. Attach FAST-IPSEC statistics with 64-bit counters to new sysctl MIB. Rework netstat to show FAST_IPSEC statistics, via sysctl,  for netstat -p ipsec. New kernel files: 	sys/netipsec/Makefile		(new file; install *_var.h includes) 	sys/netipsec/ipsec_var.h	(new 64-bit mib counter struct) Changed kernel files: 	sys/Makefile			(recurse into sys/netipsec/) 	sys/netinet/in.h		(fake IP_PROTO name for fast_ipsec 					sysctl subtree.) 	sys/netipsec/ipsec.h		(minimal userspace inclusion) 	sys/netipsec/ipsec_osdep.h	(minimal userspace inclusion) 	sys/netipsec/ipsec_netbsd.c	(redo sysctl subtree from scratch) 	sys/netipsec/key*.c		(fix broken net.key subtree) 	sys/netipsec/ah_var.h		(increase all counters to 64 bits) 	sys/netipsec/esp_var.h		(increase all counters to 64 bits) 	sys/netipsec/ipip_var.h		(increase all counters to 64 bits) 	sys/netipsec/ipcomp_var.h	(increase all counters to 64 bits) 	sys/netipsec/ipsec.c		(add #include netipsec/ipsec_var.h) 	sys/netipsec/ipsec_mbuf.c	(add #include netipsec/ipsec_var.h) 	sys/netipsec/ipsec_output.c	(add #include netipsec/ipsec_var.h) 	sys/netinet/raw_ip.c		(add #include netipsec/ipsec_var.h) 	sys/netinet/tcp_input.c		(add #include netipsec/ipsec_var.h) 	sys/netinet/udp_usrreq.c	(add #include netipsec/ipsec_var.h) Changes to usr.bin/netstat to print the new fast-ipsec sysctl tree for "netstat -s -p ipsec": New file: 	usr.bin/netstat/fast_ipsec.c	(print fast-ipsec counters) Changed files: 	usr.bin/netstat/Makefile	(add fast_ipsec.c) 	usr.bin/netstat/netstat.h	(declarations for fast_ipsec.c) 	usr.bin/netstat/main.c		(call KAME-vs-fast-ipsec dispatcher)

--- sys/netipsec/ipsec_output.c
+++ sys/netipsec/ipsec_output.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: ipsec_output.c,v 1.12 2004/03/17 00:21:43 jonathan Exp $	*/
+/*	$NetBSD: ipsec_output.c,v 1.12.2.1 2004/05/10 15:02:18 tron Exp $	*/
 
 /*-
  * Copyright (c) 2002, 2003 Sam Leffler, Errno Consulting
@@ -29,7 +29,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: ipsec_output.c,v 1.12 2004/03/17 00:21:43 jonathan Exp $");
+__KERNEL_RCSID(0, "$NetBSD: ipsec_output.c,v 1.12.2.1 2004/05/10 15:02:18 tron Exp $");
 
 /*
  * IPsec output processing.
@@ -74,6 +74,7 @@ __KERNEL_RCSID(0, "$NetBSD: ipsec_output.c,v 1.12 2004/03/17 00:21:43 jonathan E
 #endif
 
 #include <netipsec/ipsec.h>
+#include <netipsec/ipsec_var.h>
 #ifdef INET6
 #include <netipsec/ipsec6.h>
 #endif
