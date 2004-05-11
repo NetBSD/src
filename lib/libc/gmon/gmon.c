31872146d650f335233658c4522bf581d8b92c01
tron <tron@NetBSD.org>
2004-05-11 14:51:50 +0000

    Pull up revision 1.22 (requested by thorpej in ticket #308): Make gmon / mcount work properly for threaded applications by keeping thread-specific gmonparam structures and then merging them before dumping the profile buffers. Written by Nathan J. Williams, contributed by Wasabi Systems, Inc.

--- lib/libc/gmon/gmon.c
+++ lib/libc/gmon/gmon.c
@@ -1,4 +1,39 @@
-/*	$NetBSD: gmon.c,v 1.21 2003/08/07 16:43:02 agc Exp $	*/
+/*	$NetBSD: gmon.c,v 1.21.2.1 2004/05/11 14:51:50 tron Exp $	*/
+
+/*
+ * Copyright (c) 2003, 2004 Wasabi Systems, Inc.
+ * All rights reserved.
+ *
+ * Written by Nathan J. Williams for Wasabi Systems, Inc.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed for the NetBSD Project by
+ *	Wasabi Systems, Inc.
+ * 4. The name of Wasabi Systems, Inc. may not be used to endorse
+ *    or promote products derived from this software without specific prior
+ *    written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY WASABI SYSTEMS, INC. ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL WASABI SYSTEMS, INC
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 
 /*-
  * Copyright (c) 1983, 1992, 1993
@@ -34,7 +69,7 @@
 #if 0
 static char sccsid[] = "@(#)gmon.c	8.1 (Berkeley) 6/4/93";
 #else
-__RCSID("$NetBSD: gmon.c,v 1.21 2003/08/07 16:43:02 agc Exp $");
+__RCSID("$NetBSD: gmon.c,v 1.21.2.1 2004/05/11 14:51:50 tron Exp $");
 #endif
 #endif
 
@@ -42,17 +77,28 @@ __RCSID("$NetBSD: gmon.c,v 1.21 2003/08/07 16:43:02 agc Exp $");
 #include <sys/param.h>
 #include <sys/time.h>
 #include <sys/gmon.h>
+#include <sys/mman.h>
 #include <sys/sysctl.h>
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <fcntl.h>
 #include <limits.h>
 #include <unistd.h>
 #include <err.h>
 #include "extern.h"
+#include "reentrant.h"
 
 struct gmonparam _gmonparam = { GMON_PROF_OFF };
+
+#ifdef _REENTRANT
+struct gmonparam *_gmonfree;
+struct gmonparam *_gmoninuse;
+mutex_t _gmonlock = MUTEX_INITIALIZER;
+thread_key_t _gmonkey;
+struct gmonparam _gmondummy;
+#endif
 
 static u_int	s_scale;
 /* see profil(2) where this is describe (incorrectly) */
@@ -65,6 +111,12 @@ void	monstartup __P((u_long, u_long));
 void	_mcleanup __P((void));
 static int hertz __P((void));
 
+#ifdef _REENTRANT
+static void _m_gmon_destructor(void *);
+struct gmonparam *_m_gmon_alloc(void)  __attribute__((__no_instrument_function__));
+static void _m_gmon_merge(void);
+static void _m_gmon_merge_two(struct gmonparam *, struct gmonparam *);
+#endif
 
 void
 monstartup(lowpc, highpc)
@@ -128,8 +180,187 @@ monstartup(lowpc, highpc)
 	} else
 		s_scale = SCALE_1_TO_1;
 
+#ifdef _REENTRANT
+	_gmondummy.state = GMON_PROF_BUSY;
+	thr_keycreate(&_gmonkey, _m_gmon_destructor);
+#endif
 	moncontrol(1);
 }
+
+#ifdef _REENTRANT
+static void
+_m_gmon_destructor(void *arg)
+{
+	struct gmonparam *p = arg, *q, **prev;
+
+	if (p == &_gmondummy)
+		return;
+
+	thr_setspecific(_gmonkey, &_gmondummy);
+
+	mutex_lock(&_gmonlock);
+	/* XXX eww, linear list traversal. */
+	for (q = _gmoninuse, prev = &_gmoninuse;
+	     q != NULL;
+	     prev = (struct gmonparam **)&q->kcount,
+		 q = (struct gmonparam *)(void *)q->kcount) {
+		if (q == p)
+			*prev = (struct gmonparam *)(void *)q->kcount;
+	}
+	p->kcount = (u_short *)(void *)_gmonfree;
+	_gmonfree = p;
+	mutex_unlock(&_gmonlock);
+
+	thr_setspecific(_gmonkey, NULL);
+}
+
+struct gmonparam *
+_m_gmon_alloc(void)
+{
+	struct gmonparam *p;
+	char *cp;
+
+	mutex_lock(&_gmonlock);
+	if (_gmonfree != NULL) {
+		p = _gmonfree;
+		_gmonfree = (struct gmonparam *)(void *)p->kcount;
+		p->kcount = (u_short *)(void *)_gmoninuse;
+		_gmoninuse = p;
+	} else {
+		mutex_unlock(&_gmonlock);
+		cp = mmap(NULL,
+		    (size_t)(sizeof (struct gmonparam) + 
+			_gmonparam.fromssize + _gmonparam.tossize),
+		    PROT_READ|PROT_WRITE, MAP_ANON|MAP_PRIVATE, -1, 0LL);
+		p = (void *)cp;
+		*p = _gmonparam;
+		p->kcount = NULL;
+		cp += sizeof (struct gmonparam);
+		memset(cp, 0, (size_t)(p->fromssize + p->tossize));
+		p->froms = (u_short *)(void *)cp;
+		p->tos = (struct tostruct *)(void *)(cp + p->fromssize);
+		mutex_lock(&_gmonlock);
+		p->kcount = (u_short *)(void *)_gmoninuse;
+		_gmoninuse = p;
+	}
+	mutex_unlock(&_gmonlock);
+	thr_setspecific(_gmonkey, p);
+
+	return p;
+}
+
+static void
+_m_gmon_merge_two(struct gmonparam *p, struct gmonparam *q)
+{
+	u_long fromindex;
+	u_short *frompcindex, qtoindex, toindex;
+	u_long selfpc;
+	int endfrom;
+	long count;
+	struct tostruct *top;
+	
+	endfrom = (int)(q->fromssize / sizeof(*q->froms));
+	for (fromindex = 0; fromindex < endfrom; fromindex++) {
+		if (q->froms[fromindex] == 0)
+			continue;
+		for (qtoindex = q->froms[fromindex]; qtoindex != 0;
+		     qtoindex = q->tos[qtoindex].link) {
+			selfpc = q->tos[qtoindex].selfpc;
+			count = q->tos[qtoindex].count;
+			/* cribbed from mcount */
+			frompcindex = &p->froms[fromindex];
+			toindex = *frompcindex;
+			if (toindex == 0) {
+				/*
+				 *	first time traversing this arc
+				 */
+				toindex = ++p->tos[0].link;
+				if (toindex >= p->tolimit)
+					/* halt further profiling */
+					goto overflow;
+				
+				*frompcindex = (u_short)toindex;
+				top = &p->tos[(size_t)toindex];
+				top->selfpc = selfpc;
+				top->count = count;
+				top->link = 0;
+				goto done;
+			}
+			top = &p->tos[(size_t)toindex];
+			if (top->selfpc == selfpc) {
+				/*
+				 * arc at front of chain; usual case.
+				 */
+				top->count+= count;
+				goto done;
+			}
+			/*
+			 * have to go looking down chain for it.
+			 * top points to what we are looking at,
+			 * we know it is not at the head of the chain.
+			 */
+			for (; /* goto done */; ) {
+				if (top->link == 0) {
+					/*
+					 * top is end of the chain and
+					 * none of the chain had
+					 * top->selfpc == selfpc.  so
+					 * we allocate a new tostruct
+					 * and link it to the head of
+					 * the chain.
+					 */
+					toindex = ++p->tos[0].link;
+					if (toindex >= p->tolimit)
+						goto overflow;
+					
+					top = &p->tos[(size_t)toindex];
+					top->selfpc = selfpc;
+					top->count = count;
+					top->link = *frompcindex;
+					*frompcindex = (u_short)toindex;
+					goto done;
+				}
+				/*
+				 * otherwise, check the next arc on the chain.
+				 */
+				top = &p->tos[top->link];
+				if (top->selfpc == selfpc) {
+					/*
+					 * there it is.
+					 * add to its count.
+					 */
+					top->count += count;
+					goto done;
+				}
+				
+			}
+
+		done: ;
+		}
+
+	}
+ overflow: ;
+ 
+}
+
+static void
+_m_gmon_merge(void)
+{
+	struct gmonparam *q;
+
+	mutex_lock(&_gmonlock);
+
+	for (q = _gmonfree; q != NULL; q = (struct gmonparam *)(void *)q->kcount)
+		_m_gmon_merge_two(&_gmonparam, q);
+
+	for (q = _gmoninuse; q != NULL; q = (struct gmonparam *)(void *)q->kcount) {
+		q->state = GMON_PROF_OFF;
+		_m_gmon_merge_two(&_gmonparam, q);
+	}
+
+	mutex_unlock(&_gmonlock);
+}
+#endif
 
 void
 _mcleanup()
@@ -215,6 +446,9 @@ _mcleanup()
 	len = snprintf(buf2, sizeof buf2, "[mcleanup1] kcount 0x%x ssiz %d\n",
 	    p->kcount, p->kcountsize);
 	write(log, buf2, len);
+#endif
+#ifdef _REENTRANT
+	_m_gmon_merge();
 #endif
 	hdr = (struct gmonhdr *)&gmonhdr;
 	hdr->lpc = p->lowpc;
