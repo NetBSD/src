901973874a844c3535fb98bb8cf6e5133fa74da3
wiz <wiz@NetBSD.org>
2004-05-12 20:43:17 +0000

    Fix typo in comment; from Robert Nagy via OpenBSD.

--- sys/dev/pci/bktr/bktr_audio.c
+++ sys/dev/pci/bktr/bktr_audio.c
@@ -1,6 +1,6 @@
 /* $SourceForge: bktr_audio.c,v 1.6 2003/03/11 23:11:20 thomasklausner Exp $ */
 
-/*	$NetBSD: bktr_audio.c,v 1.12 2003/03/12 00:19:20 wiz Exp $	*/
+/*	$NetBSD: bktr_audio.c,v 1.13 2004/05/12 20:43:17 wiz Exp $	*/
 /* $FreeBSD: src/sys/dev/bktr/bktr_audio.c,v 1.8 2000/10/31 13:09:56 roger Exp$ */
 /*
  * This is part of the Driver for Video Capture Cards (Frame grabbers)
@@ -11,7 +11,7 @@
  * bktr_audio : This deals with controlling the audio on TV cards,
  *                controlling the Audio Multiplexer (audio source selector).
  *                controlling any MSP34xx stereo audio decoders.
- *                controlling any DPL35xx dolby surroud sound audio decoders.
+ *                controlling any DPL35xx dolby surround sound audio decoders.
  *                initialising TDA98xx audio devices.
  *
  */
@@ -71,7 +71,7 @@
 
 #ifdef __NetBSD__
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: bktr_audio.c,v 1.12 2003/03/12 00:19:20 wiz Exp $");
+__KERNEL_RCSID(0, "$NetBSD: bktr_audio.c,v 1.13 2004/05/12 20:43:17 wiz Exp $");
 
 #include <sys/proc.h>
 #include <dev/ic/bt8xx.h>	/* NetBSD location of .h files */
