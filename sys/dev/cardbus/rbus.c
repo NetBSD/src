ab79a2f067c86cc836ea39181a90c1dd5057ca89
skd <skd@NetBSD.org>
2004-05-16 21:32:31 +0000

    fix typo in comment: resister -> register.

--- sys/dev/cardbus/rbus.c
+++ sys/dev/cardbus/rbus.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: rbus.c,v 1.19 2003/11/02 09:56:38 wiz Exp $	*/
+/*	$NetBSD: rbus.c,v 1.20 2004/05/16 21:32:31 skd Exp $	*/
 /*
  * Copyright (c) 1999 and 2000
  *     HAYAKAWA Koichi.  All rights reserved.
@@ -30,7 +30,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: rbus.c,v 1.19 2003/11/02 09:56:38 wiz Exp $");
+__KERNEL_RCSID(0, "$NetBSD: rbus.c,v 1.20 2004/05/16 21:32:31 skd Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -128,7 +128,7 @@ rbus_space_alloc_subregion(rbt, substart, subend, addr, size, mask, align, flags
 				return 1;
 			}
 		} else if (decodesize == 0) {
-			/* maybe, the resister is overflowed. */
+			/* maybe, the register is overflowed. */
       
 			if (extent_alloc_subregion(rbt->rb_ext, addr,
 			    addr + size, size, 1, 0, exflags, &result)) {
