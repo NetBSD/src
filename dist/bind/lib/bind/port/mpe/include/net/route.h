1885fbb90d5923b71262c73ee1d9793c896d22de
christos <christos@NetBSD.org>
2004-05-17 23:43:04 +0000

    Import bind 9.3.0beta3

---
+++ dist/bind/lib/bind/port/mpe/include/net/route.h
@@ -0,0 +1,3 @@
+/*	$NetBSD: route.h,v 1.1.1.1 2004/05/17 23:44:46 christos Exp $	*/
+
+/* Omitted from MPE. */
---
+++ dist/bind/lib/bind/port/solaris/include/paths.h
@@ -0,0 +1,4 @@
+/*	$NetBSD: paths.h,v 1.1.1.1 2004/05/17 23:44:47 christos Exp $	*/
+
+#define _PATH_DEVNULL "/dev/null"
+
