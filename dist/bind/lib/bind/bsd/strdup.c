af5b73d26a83f06fb39fd9ff1a6348c48dd5e914
christos <christos@NetBSD.org>
2004-05-17 23:43:04 +0000

    Import bind 9.3.0beta3

---
+++ dist/bind/lib/bind/bsd/strdup.c
@@ -0,0 +1,20 @@
+/*	$NetBSD: strdup.c,v 1.1.1.1 2004/05/17 23:44:40 christos Exp $	*/
+
+#include "port_before.h"
+
+#include <stdlib.h>
+
+#include "port_after.h"
+
+#ifndef NEED_STRDUP
+int __bind_strdup_unneeded;
+#else
+char *
+strdup(const char *src) {
+	char *dst = malloc(strlen(src) + 1);
+
+	if (dst)
+		strcpy(dst, src);
+	return (dst);
+}
+#endif
---
+++ dist/bind/lib/bind/port/qnx/include/sys/mbuf.h
@@ -0,0 +1,2 @@
+/*	$NetBSD: mbuf.h,v 1.1.1.1 2004/05/17 23:44:46 christos Exp $	*/
+
