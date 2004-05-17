acdfabf216aedba4d56ca916fa9a258fc3da422b
tron <tron@NetBSD.org>
2004-05-17 10:38:03 +0000

    Pull up revision 1.16 (requested by kleink in ticket #349): Don't use tm_gmtoff for %z: a strictly conforming ISO C application won't initialize it.  However, instead of using what is suggested in this code path, attempt get it right for DST as well.  Addresses PR standards/21722 from Richard Earnshaw.

--- lib/libc/time/strftime.c
+++ lib/libc/time/strftime.c
@@ -1,11 +1,11 @@
-/*	$NetBSD: strftime.c,v 1.14.2.1 2004/05/14 06:23:54 jdc Exp $	*/
+/*	$NetBSD: strftime.c,v 1.14.2.2 2004/05/17 10:38:03 tron Exp $	*/
 
 #include <sys/cdefs.h>
 #if defined(LIBC_SCCS) && !defined(lint)
 #if 0
 static char	elsieid[] = "@(#)strftime.c	7.64";
 #else
-__RCSID("$NetBSD: strftime.c,v 1.14.2.1 2004/05/14 06:23:54 jdc Exp $");
+__RCSID("$NetBSD: strftime.c,v 1.14.2.2 2004/05/17 10:38:03 tron Exp $");
 #endif
 #endif /* LIBC_SCCS and not lint */
 
@@ -18,8 +18,15 @@ __RCSID("$NetBSD: strftime.c,v 1.14.2.1 2004/05/14 06:23:54 jdc Exp $");
 
 #include "private.h"
 
+/*
+** We don't use these extensions in strftime operation even when
+** supported by the local tzcode configuration.  A strictly
+** conforming C application may leave them in undefined state.
+*/
+
 #ifdef _LIBC
 #undef TM_ZONE
+#undef TM_GMTOFF
 #endif
 
 /*
@@ -486,6 +493,7 @@ label:
 				** determinable, so output nothing if the
 				** appropriate variables are not available.
 				*/
+#ifndef STD_INSPIRED
 				if (t->tm_isdst == 0)
 #ifdef USG_COMPAT
 					diff = -timezone;
@@ -498,6 +506,35 @@ label:
 #else /* !defined ALTZONE */
 					continue;
 #endif /* !defined ALTZONE */
+#else /* defined STD_INSPIRED */
+				{
+					struct tm tmp;
+					time_t lct, gct;
+
+					/*
+					** Get calendar time from t
+					** being treated as local.
+					*/
+					tmp = *t; /* mktime discards const */
+					lct = mktime(&tmp);
+
+					if (lct == (time_t)-1)
+						continue;
+
+					/*
+					** Get calendar time from t
+					** being treated as GMT.
+					**/
+					tmp = *t; /* mktime discards const */
+					gct = timegm(&tmp);
+
+					if (gct == (time_t)-1)
+						continue;
+
+					/* LINTED difference will fit int */
+					diff = (intmax_t)gct - (intmax_t)lct;
+				}
+#endif /* defined STD_INSPIRED */
 #endif /* !defined TM_GMTOFF */
 				if (diff < 0) {
 					sign = "-";
