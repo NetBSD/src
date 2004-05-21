a4f48a4ba4c8c30ba52c906b1a6723f0920a854f
christos <christos@NetBSD.org>
2004-05-21 16:03:05 +0000

    disable weak aliases for now; the resolver header file adds __ to all the names.

--- lib/libc/resolv/res_query.c
+++ lib/libc/resolv/res_query.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: res_query.c,v 1.4 2004/05/21 15:35:05 christos Exp $	*/
+/*	$NetBSD: res_query.c,v 1.5 2004/05/21 16:03:05 christos Exp $	*/
 
 /*
  * Copyright (c) 1988, 1993
@@ -76,7 +76,7 @@
 static const char sccsid[] = "@(#)res_query.c	8.1 (Berkeley) 6/4/93";
 static const char rcsid[] = "Id: res_query.c,v 1.2.2.3.4.2 2004/03/16 12:34:19 marka Exp";
 #else
-__RCSID("$NetBSD: res_query.c,v 1.4 2004/05/21 15:35:05 christos Exp $");
+__RCSID("$NetBSD: res_query.c,v 1.5 2004/05/21 16:03:05 christos Exp $");
 #endif
 #endif /* LIBC_SCCS and not lint */
 
@@ -97,11 +97,13 @@ __RCSID("$NetBSD: res_query.c,v 1.4 2004/05/21 15:35:05 christos Exp $");
 #include <string.h>
 #include "port_after.h"
 
+#if 0
 #ifdef __weak_alias
 __weak_alias(res_nquery,_res_nquery)
 __weak_alias(res_nsearch,_res_nsearch)
 __weak_alias(res_nquerydomain,__res_nquerydomain)
 __weak_alias(res_hostalias,__res_hostalias)
+#endif
 #endif
 
 /* Options.  Leave them on. */
