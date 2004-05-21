ee34c0d5da0b5dd372ad0066f01f16300a3d3243
kleink <kleink@NetBSD.org>
2004-05-21 22:52:06 +0000

    Change {u,}int_fast{8,16}_t to 32-bit types, as the previous minimum- width implementation was a rather poor choice.  Per discussion with Charles Hannum.
    Note: While this is technically an ABI change I believe it is a
    change that we can afford at this time (and to be pulled up to
    2.0).  The types are not widely used yet, and a survey of pkgsrc
    has not shown uses that would be adversely affected by it.

--- sys/arch/i386/include/int_mwgwtypes.h
+++ sys/arch/i386/include/int_mwgwtypes.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: int_mwgwtypes.h,v 1.2 2001/04/26 16:25:21 kleink Exp $	*/
+/*	$NetBSD: int_mwgwtypes.h,v 1.3 2004/05/21 22:52:06 kleink Exp $	*/
 
 /*-
  * Copyright (c) 2001 The NetBSD Foundation, Inc.
@@ -63,10 +63,10 @@ typedef	unsigned long long int	uint_least64_t;
 
 /* 7.18.1.3 Fastest minimum-width integer types */
 
-typedef	__signed char		   int_fast8_t;
-typedef	unsigned char		  uint_fast8_t;
-typedef	short int		  int_fast16_t;
-typedef	unsigned short int	 uint_fast16_t;
+typedef	int			   int_fast8_t;
+typedef	unsigned int		  uint_fast8_t;
+typedef	int			  int_fast16_t;
+typedef	unsigned int		 uint_fast16_t;
 typedef	int			  int_fast32_t;
 typedef	unsigned int		 uint_fast32_t;
 #ifdef __COMPILER_INT64__
