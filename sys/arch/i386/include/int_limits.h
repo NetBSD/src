e9e9a297a4006218e8fe52b4a0e5a8e23027793b
kleink <kleink>
2004-05-22 14:16:59 +0000

    Bring back {u,}int_fast8_t back to 8 bits, as I had misunderstood Charles.

--- sys/arch/i386/include/int_limits.h
+++ sys/arch/i386/include/int_limits.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: int_limits.h,v 1.4 2004/05/21 22:52:06 kleink Exp $	*/
+/*	$NetBSD: int_limits.h,v 1.5 2004/05/22 14:16:59 kleink Exp $	*/
 
 /*-
  * Copyright (c) 2001 The NetBSD Foundation, Inc.
@@ -86,19 +86,19 @@
 /* 7.18.2.3 Limits of fastest minimum-width integer types */
  
 /* minimum values of fastest minimum-width signed integer types */
-#define	INT_FAST8_MIN	(-0x7fffffff-1)			/* int_fast8_t	  */
+#define	INT_FAST8_MIN	(-0x7f-1)			/* int_fast8_t	  */
 #define	INT_FAST16_MIN	(-0x7fffffff-1)			/* int_fast16_t	  */
 #define	INT_FAST32_MIN	(-0x7fffffff-1)			/* int_fast32_t	  */
 #define	INT_FAST64_MIN	(-0x7fffffffffffffffLL-1)	/* int_fast64_t	  */
 
 /* maximum values of fastest minimum-width signed integer types */
-#define	INT_FAST8_MAX	0x7fffffff			/* int_fast8_t	  */
+#define	INT_FAST8_MAX	0x7f				/* int_fast8_t	  */
 #define	INT_FAST16_MAX	0x7fffffff			/* int_fast16_t	  */
 #define	INT_FAST32_MAX	0x7fffffff			/* int_fast32_t	  */
 #define	INT_FAST64_MAX	0x7fffffffffffffffLL		/* int_fast64_t	  */
 
 /* maximum values of fastest minimum-width unsigned integer types */
-#define	UINT_FAST8_MAX	0xffffffffU			/* uint_fast8_t	  */
+#define	UINT_FAST8_MAX	0xffU				/* uint_fast8_t	  */
 #define	UINT_FAST16_MAX	0xffffffffU			/* uint_fast16_t  */
 #define	UINT_FAST32_MAX	0xffffffffU			/* uint_fast32_t  */
 #define	UINT_FAST64_MAX	0xffffffffffffffffULL		/* uint_fast64_t  */
