a750c9e36f380db35629cce8e10b9eca9d77caec
tron <tron@NetBSD.org>
2004-05-23 10:46:28 +0000

    Pull up revision 1.15 (requested by atatat in ticket #374): Tweak sysctl setup functions (the macros, actually) for use in lkms, and tweak lkminit_*.c (where applicable) to call them, and to call sysctl_teardown() when being unloaded. This consists of (1) making setup functions not be static when being compiled as lkms (change to sys/sysctl.h), (2) making prototypes visible for the various setup functions in header files (changes to various header files), and (3) making simple "load" and "unload" functions in the actual lkminit stuff. linux_sysctl.c also needs its root exposed (ie, made not static) for this (when built as an lkm).

--- sys/compat/linux/common/linux_sysctl.c
+++ sys/compat/linux/common/linux_sysctl.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: linux_sysctl.c,v 1.13.2.1 2004/04/29 04:07:18 jmc Exp $	*/
+/*	$NetBSD: linux_sysctl.c,v 1.13.2.2 2004/05/23 10:46:28 tron Exp $	*/
 
 /*-
  * Copyright (c) 2003 The NetBSD Foundation, Inc.
@@ -41,7 +41,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: linux_sysctl.c,v 1.13.2.1 2004/04/29 04:07:18 jmc Exp $");
+__KERNEL_RCSID(0, "$NetBSD: linux_sysctl.c,v 1.13.2.2 2004/05/23 10:46:28 tron Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -68,7 +68,10 @@ char linux_release[128] = "2.0.38";
 char linux_version[128] = "#0 Sun Nov 11 11:11:11 MET 2000";
 #endif
 
-static struct sysctlnode linux_sysctl_root = {
+#ifndef _LKM
+static
+#endif
+struct sysctlnode linux_sysctl_root = {
 	.sysctl_flags = SYSCTL_VERSION|
 	    CTLFLAG_ROOT|CTLTYPE_NODE|CTLFLAG_READWRITE,
 	.sysctl_num = 0,
