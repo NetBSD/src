fc2f689fcba978ae49fe6cce664a8266a8d64bd2
skrll <skrll@NetBSD.org>
2004-05-25 13:39:35 +0000

    Re-run mknative.

--- gnu/usr.bin/gcc3/arch/sparc/configargs.h
+++ gnu/usr.bin/gcc3/arch/sparc/configargs.h
@@ -2,5 +2,5 @@
 /* Generated from: 	NetBSD: mknative-gcc,v 1.12 2004/02/10 09:37:58 skrll Exp  */
 
 /* Generated automatically. */
-static const char configuration_arguments[] = "/home/nick/work/netbsd/src/tools/gcc/../../gnu/dist/gcc/configure --enable-long-long --disable-multilib --enable-threads --disable-symvers --build=i386-unknown-netbsdelf --host=sparc--netbsdelf --target=sparc--netbsdelf";
+static const char configuration_arguments[] = "/home/nick/work/netbsd/src/tools/gcc/../../gnu/dist/gcc/configure --enable-long-long --disable-multilib --enable-threads --disable-symvers --build=i386-unknown-netbsdelf2.0. --host=sparc--netbsdelf --target=sparc--netbsdelf : (reconfigured) /home/nick/work/netbsd/src/tools/gcc/../../gnu/dist/gcc/configure --enable-long-long --disable-multilib --enable-threads --disable-symvers --build=i386-unknown-netbsdelf2.0. --host=sparc--netbsdelf --target=sparc--netbsdelf";
 static const char thread_model[] = "posix";
