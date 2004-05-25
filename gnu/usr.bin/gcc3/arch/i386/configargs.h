5bee659b41fd5c6cd4654bf6b946890b5b088df5
skrll <skrll@NetBSD.org>
2004-05-25 13:39:35 +0000

    Re-run mknative.

--- gnu/usr.bin/gcc3/arch/i386/configargs.h
+++ gnu/usr.bin/gcc3/arch/i386/configargs.h
@@ -2,5 +2,5 @@
 /* Generated from: 	NetBSD: mknative-gcc,v 1.12 2004/02/10 09:37:58 skrll Exp  */
 
 /* Generated automatically. */
-static const char configuration_arguments[] = "/home/nick/work/netbsd/src/tools/gcc/../../gnu/dist/gcc/configure --enable-long-long --disable-multilib --enable-threads --disable-symvers --build=i386-unknown-netbsdelf --host=i386--netbsdelf --target=i386--netbsdelf";
+static const char configuration_arguments[] = "/home/nick/work/netbsd/src/tools/gcc/../../gnu/dist/gcc/configure --enable-long-long --disable-multilib --enable-threads --disable-symvers --build=i386-unknown-netbsdelf2.0. --host=i386--netbsdelf --target=i386--netbsdelf : (reconfigured) /home/nick/work/netbsd/src/tools/gcc/../../gnu/dist/gcc/configure --enable-long-long --disable-multilib --enable-threads --disable-symvers --build=i386-unknown-netbsdelf2.0. --host=i386--netbsdelf --target=i386--netbsdelf";
 static const char thread_model[] = "posix";
--- gnu/usr.bin/gcc3/arch/powerpc/configargs.h
+++ gnu/usr.bin/gcc3/arch/powerpc/configargs.h
@@ -2,5 +2,5 @@
 /* Generated from: 	NetBSD: mknative-gcc,v 1.12 2004/02/10 09:37:58 skrll Exp  */
 
 /* Generated automatically. */
-static const char configuration_arguments[] = "/home/nick/work/netbsd/src/tools/gcc/../../gnu/dist/gcc/configure --enable-long-long --disable-multilib --enable-threads --disable-symvers --build=i386-unknown-netbsdelf --host=powerpc--netbsd --target=powerpc--netbsd";
+static const char configuration_arguments[] = "/home/nick/work/netbsd/src/tools/gcc/../../gnu/dist/gcc/configure --enable-long-long --disable-multilib --enable-threads --disable-symvers --build=i386-unknown-netbsdelf2.0. --host=powerpc--netbsd --target=powerpc--netbsd : (reconfigured) /home/nick/work/netbsd/src/tools/gcc/../../gnu/dist/gcc/configure --enable-long-long --disable-multilib --enable-threads --disable-symvers --build=i386-unknown-netbsdelf2.0. --host=powerpc--netbsd --target=powerpc--netbsd";
 static const char thread_model[] = "posix";
