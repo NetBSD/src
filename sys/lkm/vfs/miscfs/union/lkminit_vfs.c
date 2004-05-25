d513701dd1a50869c39b88b112a776b952da441c
mhitch <mhitch@NetBSD.org>
2004-05-25 01:36:37 +0000

    Addition of header files for sysctl set function prototypes referenced structs defined in other header files.  Gcc 2.95 on vax doesn't like this. Add appropriate header files to fix vax compile failures.

--- sys/lkm/vfs/miscfs/union/lkminit_vfs.c
+++ sys/lkm/vfs/miscfs/union/lkminit_vfs.c
@@ -1,4 +1,4 @@
-/* $NetBSD: lkminit_vfs.c,v 1.6 2004/05/20 06:34:28 atatat Exp $ */
+/* $NetBSD: lkminit_vfs.c,v 1.7 2004/05/25 01:36:37 mhitch Exp $ */
 
 /*-
  * Copyright (c) 1996 The NetBSD Foundation, Inc.
@@ -37,7 +37,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: lkminit_vfs.c,v 1.6 2004/05/20 06:34:28 atatat Exp $");
+__KERNEL_RCSID(0, "$NetBSD: lkminit_vfs.c,v 1.7 2004/05/25 01:36:37 mhitch Exp $");
 
 #include <sys/param.h>
 #include <sys/sysctl.h>
@@ -49,6 +49,7 @@ __KERNEL_RCSID(0, "$NetBSD: lkminit_vfs.c,v 1.6 2004/05/20 06:34:28 atatat Exp $
 #include <sys/lkm.h>
 #include <sys/file.h>
 #include <sys/errno.h>
+#include <sys/namei.h>
 
 #include <fs/union/union.h>
 
