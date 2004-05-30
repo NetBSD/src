e38943358d34d3267de5915b79bacf09be28ca8a
tron <tron@NetBSD.org>
2004-05-30 15:09:52 +0000

    Pull up revision 1.14 (requested by wrstuden in ticket #424): Since VOP_UPCALL() has been a long time in coming, add this partial fix for layered-file-removal. It will work for the case of accessing and deleting a file through the layered file system. Accessing via the layer and deleting on the underlying still won't work, nor will accessing via complicated structures (like two umap layers over a given file systems). We still need VOP_UPCALL(), but this is better than things were before. This patch has been discussed off & on for a while. This incarnation was tested by hannken at netbsd dot org.

--- sys/miscfs/genfs/layer_extern.h
+++ sys/miscfs/genfs/layer_extern.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: layer_extern.h,v 1.11 2004/01/25 18:06:49 hannken Exp $	*/
+/*	$NetBSD: layer_extern.h,v 1.11.2.1 2004/05/30 15:09:52 tron Exp $	*/
 
 /*
  * Copyright (c) 1999 National Aeronautics & Space Administration
@@ -110,5 +110,6 @@ int	layer_lookup __P((void *));
 int	layer_setattr __P((void *));
 int	layer_access __P((void *));
 int	layer_open __P((void *));
+int	layer_remove __P((void *));
 int	layer_getpages __P((void *));
 int	layer_putpages __P((void *));
