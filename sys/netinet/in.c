37b9c052f7e09f7ddf6bfc80f1671fb2b6146bf8
itojun <itojun>
2004-05-30 06:37:07 +0000

    fix SIOC*LIFADDR for IPv4.  markus friedl

--- sys/netinet/in.c
+++ sys/netinet/in.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: in.c,v 1.94 2004/04/21 18:40:41 itojun Exp $	*/
+/*	$NetBSD: in.c,v 1.95 2004/05/30 06:37:07 itojun Exp $	*/
 
 /*
  * Copyright (C) 1995, 1996, 1997, and 1998 WIDE Project.
@@ -98,7 +98,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: in.c,v 1.94 2004/04/21 18:40:41 itojun Exp $");
+__KERNEL_RCSID(0, "$NetBSD: in.c,v 1.95 2004/05/30 06:37:07 itojun Exp $");
 
 #include "opt_inet.h"
 #include "opt_inet_conf.h"
@@ -648,7 +648,7 @@ in_lifaddr_ioctl(so, cmd, data, ifp, p)
 		if (iflr->flags & IFLR_PREFIX)
 			return EINVAL;
 
-		/* copy args to in_aliasreq, perform ioctl(SIOCAIFADDR_IN6). */
+		/* copy args to in_aliasreq, perform ioctl(SIOCAIFADDR_IN). */
 		bzero(&ifra, sizeof(ifra));
 		bcopy(iflr->iflr_name, ifra.ifra_name,
 			sizeof(ifra.ifra_name));
@@ -704,7 +704,7 @@ in_lifaddr_ioctl(so, cmd, data, ifp, p)
 		}
 
 		TAILQ_FOREACH(ifa, &ifp->if_addrlist, ifa_list) {
-			if (ifa->ifa_addr->sa_family != AF_INET6)
+			if (ifa->ifa_addr->sa_family != AF_INET)
 				continue;
 			if (!cmp)
 				break;
@@ -736,7 +736,7 @@ in_lifaddr_ioctl(so, cmd, data, ifp, p)
 		} else {
 			struct in_aliasreq ifra;
 
-			/* fill in_aliasreq and do ioctl(SIOCDIFADDR_IN6) */
+			/* fill in_aliasreq and do ioctl(SIOCDIFADDR_IN) */
 			bzero(&ifra, sizeof(ifra));
 			bcopy(iflr->iflr_name, ifra.ifra_name,
 				sizeof(ifra.ifra_name));
