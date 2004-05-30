09150b75374bd5232c4c91e32e73dcc83359a629
tron <tron@NetBSD.org>
2004-05-30 15:09:30 +0000

    Pull up revision 1.26 (requested by wrstuden in ticket #424): Since VOP_UPCALL() has been a long time in coming, add this partial fix for layered-file-removal. It will work for the case of accessing and deleting a file through the layered file system. Accessing via the layer and deleting on the underlying still won't work, nor will accessing via complicated structures (like two umap layers over a given file systems). We still need VOP_UPCALL(), but this is better than things were before. This patch has been discussed off & on for a while. This incarnation was tested by hannken at netbsd dot org.

--- sys/miscfs/umapfs/umap_vnops.c
+++ sys/miscfs/umapfs/umap_vnops.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: umap_vnops.c,v 1.24 2004/01/25 18:06:49 hannken Exp $	*/
+/*	$NetBSD: umap_vnops.c,v 1.24.2.1 2004/05/30 15:09:30 tron Exp $	*/
 
 /*
  * Copyright (c) 1992, 1993
@@ -39,7 +39,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: umap_vnops.c,v 1.24 2004/01/25 18:06:49 hannken Exp $");
+__KERNEL_RCSID(0, "$NetBSD: umap_vnops.c,v 1.24.2.1 2004/05/30 15:09:30 tron Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -84,6 +84,7 @@ const struct vnodeopv_entry_desc umap_vnodeop_entries[] = {
 	{ &vop_open_desc,	layer_open },
 	{ &vop_setattr_desc,	layer_setattr },
 	{ &vop_access_desc,	layer_access },
+	{ &vop_remove_desc,	layer_remove },
 
 	{ &vop_bwrite_desc,	layer_bwrite },
 	{ &vop_bmap_desc,	layer_bmap },
