1706f4b9191a5cd72e60309e5fb1f2d5f30f85ff
christos <christos@NetBSD.org>
2004-06-06 07:03:53 +0000

    Include a fake syslog that does only printf; saves 4K.

---
+++ distrib/utils/libhack/syslog.c
@@ -0,0 +1,44 @@
+#include <sys/types.h>
+#include <sys/syslog.h>
+#include <stdio.h>
+#include <stdarg.h>
+
+void
+openlog(const char *path, int opt, int fac)
+{
+}
+
+void
+closelog(void)
+{
+}
+
+void
+syslog(int fac, const char *fmt, ...)
+{
+	va_list ap;
+	va_start(ap, fmt);
+	(void)vfprintf(stderr, fmt, ap);
+	va_end(ap);
+}
+
+void
+vsyslog(int fac, const char *fmt, va_list ap)
+{
+	(void)vfprintf(stderr, fmt, ap);
+}
+
+void
+_syslog(int fac, const char *fmt, ...)
+{
+	va_list ap;
+	va_start(ap, fmt);
+	(void)vfprintf(stderr, fmt, ap);
+	va_end(ap);
+}
+
+void
+_vsyslog(int fac, const char *fmt, va_list ap)
+{
+	(void)vfprintf(stderr, fmt, ap);
+}
