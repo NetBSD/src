a18f132474a060cd4d1636b5bde3e310ed18d015
uch <uch@NetBSD.org>
2004-06-12 12:10:04 +0000

    recompile.

--- sys/arch/hpc/stand/binary/build_number.h
+++ sys/arch/hpc/stand/binary/build_number.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: build_number.h,v 1.9 2004/06/10 16:00:08 uch Exp $	*/
-#define HPCBOOT_BUILD_NUMBER	10
+/*	$NetBSD: build_number.h,v 1.10 2004/06/12 12:10:04 uch Exp $	*/
+#define HPCBOOT_BUILD_NUMBER	11
 
 
