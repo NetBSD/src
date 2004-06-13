9a25f74cfac63d45af1d037792dd52e77b9e1f66
aymeric <aymeric@NetBSD.org>
2004-06-13 10:17:14 +0000

    bump: bin/25849 fixed. "rapid resize can crash vi"

--- usr.bin/vi/ex/version.h
+++ usr.bin/vi/ex/version.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: version.h,v 1.15 2004/05/02 17:01:53 aymeric Exp $	*/
+/*	$NetBSD: version.h,v 1.16 2004/06/13 10:17:14 aymeric Exp $	*/
 
 #define	VI_VERSION \
-    "Version 1.79nb12 (10/23/96) The CSRG, University of California, Berkeley."
+    "Version 1.79nb13 (10/23/96) The CSRG, University of California, Berkeley."
