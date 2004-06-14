7e265eecd91b30a9f66eaa8d8f03c5de049571ab
jmc <jmc@NetBSD.org>
2004-06-14 04:18:00 +0000

    Pullup rev 1.26-1.28 (requested by grant in ticket #494)
    sed(1) fixes for PR#25899, PR#24234, PR#25911

--- usr.bin/sed/compile.c
+++ usr.bin/sed/compile.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: compile.c,v 1.25 2003/08/07 11:15:49 agc Exp $	*/
+/*	$NetBSD: compile.c,v 1.25.2.1 2004/06/14 04:18:00 jmc Exp $	*/
 
 /*-
  * Copyright (c) 1992, 1993
@@ -72,7 +72,7 @@
 #if 0
 static char sccsid[] = "@(#)compile.c	8.2 (Berkeley) 4/28/95";
 #else
-__RCSID("$NetBSD: compile.c,v 1.25 2003/08/07 11:15:49 agc Exp $");
+__RCSID("$NetBSD: compile.c,v 1.25.2.1 2004/06/14 04:18:00 jmc Exp $");
 #endif
 #endif /* not lint */
 
@@ -481,6 +481,7 @@ compile_subst(char *p, struct s_subst *s)
 	static char lbuf[_POSIX2_LINE_MAX + 1];
 	int asize, ref, size;
 	char c, *text, *op, *sp;
+	int sawesc = 0;
 
 	c = *p++;			/* Terminator character */
 	if (c == '\0')
@@ -494,9 +495,29 @@ compile_subst(char *p, struct s_subst *s)
 	do {
 		op = sp = text + size;
 		for (; *p; p++) {
-			if (*p == '\\') {
-				p++;
-				if (strchr("123456789", *p) != NULL) {
+			if (*p == '\\' || sawesc) {
+				/*
+				 * If this is a continuation from the last
+				 * buffer, we won't have a character to
+				 * skip over.
+				 */
+				if (sawesc)
+					sawesc = 0;
+				else
+					p++;
+
+				if (*p == '\0') {
+					/*
+					 * This escaped character is continued
+					 * in the next part of the line.  Note
+					 * this fact, then cause the loop to
+					 * exit w/ normal EOL case and reenter
+					 * above with the new buffer.
+					 */
+					sawesc = 1;
+					p--;
+					continue;
+				} else if (strchr("123456789", *p) != NULL) {
 					*sp++ = '\\';
 					ref = *p - '0';
 					if (s->re != NULL &&
@@ -570,6 +591,7 @@ compile_flags(char *p, struct s_subst *s)
 			gn = 1;
 			/* XXX Check for overflow */
 			s->n = (int)strtol(p, &p, 10);
+			p--;
 			break;
 		case 'w':
 			p++;
@@ -634,7 +656,7 @@ compile_tr(char *p, char **transtab)
 		return (NULL);
 	}
 	/* We assume characters are 8 bits */
-	lt = xmalloc(UCHAR_MAX);
+	lt = xmalloc(UCHAR_MAX+1);
 	for (i = 0; i <= UCHAR_MAX; i++)
 		lt[i] = (char)i;
 	for (op = old, np = new; *op; op++, np++)
