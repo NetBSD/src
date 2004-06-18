ee46800bc23ce9d669f5cdf955b4a73f22f4c905
tron <tron@NetBSD.org>
2004-06-18 09:07:20 +0000

    Pull up revision 1.24 (requested by dillo in ticket #507): do the special symlink treatment with all negative fd's.

--- bin/pax/buf_subs.c
+++ bin/pax/buf_subs.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: buf_subs.c,v 1.23 2003/10/27 00:12:41 lukem Exp $	*/
+/*	$NetBSD: buf_subs.c,v 1.23.2.1 2004/06/18 09:07:20 tron Exp $	*/
 
 /*-
  * Copyright (c) 1992 Keith Muller.
@@ -42,7 +42,7 @@
 #if 0
 static char sccsid[] = "@(#)buf_subs.c	8.2 (Berkeley) 4/18/94";
 #else
-__RCSID("$NetBSD: buf_subs.c,v 1.23 2003/10/27 00:12:41 lukem Exp $");
+__RCSID("$NetBSD: buf_subs.c,v 1.23.2.1 2004/06/18 09:07:20 tron Exp $");
 #endif
 #endif /* not lint */
 
@@ -696,7 +696,7 @@ rd_wrfile(ARCHD *arcn, int ofd, off_t *left)
 	 * pass the blocksize of the file being written to the write routine,
 	 * if the size is zero, use the default MINFBSZ
 	 */
-	if (ofd == -1)
+	if (ofd < 0)
 		sz = PAXPATHLEN+1;
 	else if (fstat(ofd, &sb) == 0) {
 		if (sb.st_blksize > 0)
