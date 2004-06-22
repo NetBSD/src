19d53422ae18c1268d8a7c442a3f33f3a8825a9c
tron <tron>
2004-06-22 07:28:12 +0000

    Pull up revision 1.73 (requested by jmc in ticket #527): Completely rework how tools/compat is done. Purge all uses/references to _NETBSD_SOURCE as this makes cross building from older/newer versions of NetBSD harder, not easier (and also makes the resulting tools 'different') Wrap all required code with the inclusion of nbtool_config.h, attempt to only use POSIX code in all places (or when reasonable test w. configure and provide definitions: ala u_int, etc). Reviewed by lukem. Tested on FreeBSD 4.9, Redhat Linux ES3, NetBSD 1.6.2 x86 NetBSD current (x86 and amd64) and Solaris 9. Fixes PR's: PR#17762 PR#25944

--- usr.sbin/config/mkioconf.c
+++ usr.sbin/config/mkioconf.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: mkioconf.c,v 1.72 2003/08/07 11:25:16 agc Exp $	*/
+/*	$NetBSD: mkioconf.c,v 1.72.2.1 2004/06/22 07:28:12 tron Exp $	*/
 
 /*
  * Copyright (c) 1992, 1993
@@ -39,6 +39,10 @@
  *
  *	from: @(#)mkioconf.c	8.1 (Berkeley) 6/6/93
  */
+
+#if HAVE_NBTOOL_CONFIG_H
+#include "nbtool_config.h"
+#endif
 
 #include <sys/param.h>
 #include <errno.h>
