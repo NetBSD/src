4ce581b629a2e054c5c1238d6b02912128271b68
tron <tron@NetBSD.org>
2004-06-22 07:28:58 +0000

    Pull up revision 1.76 (requested by jmc in ticket #527): Completely rework how tools/compat is done. Purge all uses/references to _NETBSD_SOURCE as this makes cross building from older/newer versions of NetBSD harder, not easier (and also makes the resulting tools 'different') Wrap all required code with the inclusion of nbtool_config.h, attempt to only use POSIX code in all places (or when reasonable test w. configure and provide definitions: ala u_int, etc). Reviewed by lukem. Tested on FreeBSD 4.9, Redhat Linux ES3, NetBSD 1.6.2 x86 NetBSD current (x86 and amd64) and Solaris 9. Fixes PR's: PR#17762 PR#25944

--- bin/pax/options.c
+++ bin/pax/options.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: options.c,v 1.73 2004/02/20 05:16:54 uebayasi Exp $	*/
+/*	$NetBSD: options.c,v 1.73.2.1 2004/06/22 07:28:58 tron Exp $	*/
 
 /*-
  * Copyright (c) 1992 Keith Muller.
@@ -42,7 +42,7 @@
 #if 0
 static char sccsid[] = "@(#)options.c	8.2 (Berkeley) 4/18/94";
 #else
-__RCSID("$NetBSD: options.c,v 1.73 2004/02/20 05:16:54 uebayasi Exp $");
+__RCSID("$NetBSD: options.c,v 1.73.2.1 2004/06/22 07:28:58 tron Exp $");
 #endif
 #endif /* not lint */
 
@@ -53,7 +53,11 @@ __RCSID("$NetBSD: options.c,v 1.73 2004/02/20 05:16:54 uebayasi Exp $");
 #include <sys/param.h>
 #include <ctype.h>
 #include <errno.h>
+#if HAVE_NBTOOL_CONFIG_H
+#include "compat_getopt.h"
+#else
 #include <getopt.h>
+#endif
 #include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
