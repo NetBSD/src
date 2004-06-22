7ce4c01318bf90f9dff1a6448d7671fa03e52098
tron <tron>
2004-06-22 07:30:06 +0000

    Pull up revision 1.43 (requested by jmc in ticket #527): Completely rework how tools/compat is done. Purge all uses/references to _NETBSD_SOURCE as this makes cross building from older/newer versions of NetBSD harder, not easier (and also makes the resulting tools 'different') Wrap all required code with the inclusion of nbtool_config.h, attempt to only use POSIX code in all places (or when reasonable test w. configure and provide definitions: ala u_int, etc). Reviewed by lukem. Tested on FreeBSD 4.9, Redhat Linux ES3, NetBSD 1.6.2 x86 NetBSD current (x86 and amd64) and Solaris 9. Fixes PR's: PR#17762 PR#25944

--- usr.sbin/config/sem.c
+++ usr.sbin/config/sem.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: sem.c,v 1.41 2003/11/24 21:44:37 christos Exp $	*/
+/*	$NetBSD: sem.c,v 1.41.2.1 2004/06/22 07:30:06 tron Exp $	*/
 
 /*
  * Copyright (c) 1992, 1993
@@ -39,6 +39,10 @@
  *
  *	from: @(#)sem.c	8.1 (Berkeley) 6/6/93
  */
+
+#if HAVE_NBTOOL_CONFIG_H
+#include "nbtool_config.h"
+#endif
 
 #include <sys/param.h>
 #include <ctype.h>
