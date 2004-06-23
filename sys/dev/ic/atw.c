17dd70af52c9e336181b956d31e4012c964f3d0c
dyoung <dyoung@NetBSD.org>
2004-06-23 09:27:59 +0000

    After we wait for the MAC's transmit section to idle, drain the transmit queue and cancel the watchdog timer.  This ends the annoying "atw0: transmit timeout" messages that disrupted my WiFi tonight.

--- sys/dev/ic/atw.c
+++ sys/dev/ic/atw.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: atw.c,v 1.35 2004/06/23 09:05:50 dyoung Exp $	*/
+/*	$NetBSD: atw.c,v 1.36 2004/06/23 09:27:59 dyoung Exp $	*/
 
 /*-
  * Copyright (c) 1998, 1999, 2000, 2002, 2003, 2004 The NetBSD Foundation, Inc.
@@ -41,7 +41,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: atw.c,v 1.35 2004/06/23 09:05:50 dyoung Exp $");
+__KERNEL_RCSID(0, "$NetBSD: atw.c,v 1.36 2004/06/23 09:27:59 dyoung Exp $");
 
 #include "bpfilter.h"
 
@@ -184,6 +184,7 @@ void	atw_start(struct ifnet *);
 void	atw_watchdog(struct ifnet *);
 int	atw_ioctl(struct ifnet *, u_long, caddr_t);
 int	atw_init(struct ifnet *);
+void	atw_txdrain(struct atw_softc *);
 void	atw_stop(struct ifnet *, int);
 
 void	atw_reset(struct atw_softc *);
@@ -2507,6 +2508,26 @@ atw_add_rxbuf(struct atw_softc *sc, int idx)
 }
 
 /*
+ * Release any queued transmit buffers.
+ */
+void
+atw_txdrain(struct atw_softc *sc)
+{
+	struct atw_txsoft *txs;
+
+	while ((txs = SIMPLEQ_FIRST(&sc->sc_txdirtyq)) != NULL) {
+		SIMPLEQ_REMOVE_HEAD(&sc->sc_txdirtyq, txs_q);
+		if (txs->txs_mbuf != NULL) {
+			bus_dmamap_unload(sc->sc_dmat, txs->txs_dmamap);
+			m_freem(txs->txs_mbuf);
+			txs->txs_mbuf = NULL;
+		}
+		SIMPLEQ_INSERT_TAIL(&sc->sc_txfreeq, txs, txs_q);
+	}
+	sc->sc_tx_timer = 0;
+}
+
+/*
  * atw_stop:		[ ifnet interface function ]
  *
  *	Stop transmission on the interface.
@@ -2516,7 +2537,6 @@ atw_stop(struct ifnet *ifp, int disable)
 {
 	struct atw_softc *sc = ifp->if_softc;
 	struct ieee80211com *ic = &sc->sc_ic;
-	struct atw_txsoft *txs;
 
 	ieee80211_new_state(ic, IEEE80211_S_INIT, -1);
 
@@ -2530,18 +2550,7 @@ atw_stop(struct ifnet *ifp, int disable)
 	ATW_WRITE(sc, ATW_TDBP, 0);
 	ATW_WRITE(sc, ATW_RDB, 0);
 
-	/*
-	 * Release any queued transmit buffers.
-	 */
-	while ((txs = SIMPLEQ_FIRST(&sc->sc_txdirtyq)) != NULL) {
-		SIMPLEQ_REMOVE_HEAD(&sc->sc_txdirtyq, txs_q);
-		if (txs->txs_mbuf != NULL) {
-			bus_dmamap_unload(sc->sc_dmat, txs->txs_dmamap);
-			m_freem(txs->txs_mbuf);
-			txs->txs_mbuf = NULL;
-		}
-		SIMPLEQ_INSERT_TAIL(&sc->sc_txfreeq, txs, txs_q);
-	}
+	atw_txdrain(sc);
 
 	if (disable) {
 		atw_rxdrain(sc);
@@ -2554,8 +2563,8 @@ atw_stop(struct ifnet *ifp, int disable)
 	ifp->if_flags &= ~(IFF_RUNNING | IFF_OACTIVE);
 	ifp->if_timer = 0;
 
-	/* XXX */
-	atw_reset(sc);
+	if (!disable)
+		atw_reset(sc);
 }
 
 /*
@@ -2878,6 +2887,7 @@ atw_idle(struct atw_softc *sc, u_int32_t bits)
 		    sc->sc_dev.dv_xname, bits, test0, stsr));
 	}
 out:
+	atw_txdrain(sc);
 	splx(s);
 	return;
 }
