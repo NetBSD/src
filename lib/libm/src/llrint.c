d76992e49074610fe8854dddf1e33f61cd1dbb0e
drochner <drochner@NetBSD.org>
2004-06-30 15:08:30 +0000

    Implement (l)lrint(f). (Could get some optimization for LP64; this would need a change to the float access macros in math_private.h.)

---
+++ lib/libm/src/llrint.c
@@ -0,0 +1,8 @@
+/* $NetBSD: llrint.c,v 1.1 2004/06/30 15:08:30 drochner Exp $ */
+
+#define LRINTNAME llrint
+#define RESTYPE long long int
+#define RESTYPE_MIN LLONG_MIN
+#define RESTYPE_MAX LLONG_MAX
+
+#include "lrint.c"
