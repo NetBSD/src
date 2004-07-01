83c79e5249e26383655fdac4bbb56112cd9efdd9
christos <christos@NetBSD.org>
2004-07-01 19:04:14 +0000

    add __UNCONST

--- sys/lib/libkern/strstr.c
+++ sys/lib/libkern/strstr.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: strstr.c,v 1.3 2003/08/07 16:32:12 agc Exp $	*/
+/*	$NetBSD: strstr.c,v 1.4 2004/07/01 19:04:14 christos Exp $	*/
 
 /*-
  * Copyright (c) 1990, 1993
@@ -37,7 +37,7 @@
 #if 0
 static char sccsid[] = "@(#)strstr.c	8.1 (Berkeley) 6/4/93";
 #else
-__RCSID("$NetBSD: strstr.c,v 1.3 2003/08/07 16:32:12 agc Exp $");
+__RCSID("$NetBSD: strstr.c,v 1.4 2004/07/01 19:04:14 christos Exp $");
 #endif
 #endif /* LIBC_SCCS and not lint */
 
@@ -68,6 +68,5 @@ strstr(s, find)
 		} while (strncmp(s, find, len) != 0);
 		s--;
 	}
-	/* LINTED interface specification */
-	return ((char *)s);
+	return __UNCONST(s);
 }
