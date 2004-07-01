147d852dbc82ec7fc792ab6389e6992875a9c973
drochner <drochner@NetBSD.org>
2004-07-01 16:09:21 +0000

    being here, add the (l)lround(f) functions to libm (almost the same like lrint)

---
+++ lib/libm/src/llroundf.c
@@ -0,0 +1,8 @@
+/* $NetBSD: llroundf.c,v 1.1 2004/07/01 16:09:21 drochner Exp $ */
+
+#define LROUNDNAME llroundf
+#define RESTYPE long long int
+#define RESTYPE_MIN LLONG_MIN
+#define RESTYPE_MAX LLONG_MAX
+
+#include "lroundf.c"
