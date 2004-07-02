afbaa13870bb0bf27eeb3c43cf63748613559cd4
he <he@NetBSD.org>
2004-07-02 17:54:52 +0000

    Pull up revision 1.17 (requested by hannken in ticket #575):         Do LAYERFS_REMOVED for vop_rmdir.

--- sys/miscfs/genfs/layer_extern.h
+++ sys/miscfs/genfs/layer_extern.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: layer_extern.h,v 1.11.2.2 2004/06/21 10:05:45 tron Exp $	*/
+/*	$NetBSD: layer_extern.h,v 1.11.2.3 2004/07/02 17:54:52 he Exp $	*/
 
 /*
  * Copyright (c) 1999 National Aeronautics & Space Administration
@@ -112,5 +112,6 @@ int	layer_access __P((void *));
 int	layer_open __P((void *));
 int	layer_remove __P((void *));
 int	layer_rename __P((void *));
+int	layer_rmdir __P((void *));
 int	layer_getpages __P((void *));
 int	layer_putpages __P((void *));
