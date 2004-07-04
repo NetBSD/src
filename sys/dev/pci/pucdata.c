d718974ba7215b4e7f4f60908955a8a2d76ead15
he <he>
2004-07-04 13:19:57 +0000

    Pull up revision 1.37 (requested by mycroft in ticket #597):   Support for the VS-Com PCI-210H 2s/1p card. Fixes PR#18478.

--- sys/dev/pci/pucdata.c
+++ sys/dev/pci/pucdata.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: pucdata.c,v 1.36 2004/02/03 19:51:39 fredb Exp $	*/
+/*	$NetBSD: pucdata.c,v 1.36.2.1 2004/07/04 13:19:57 he Exp $	*/
 
 /*
  * Copyright (c) 1998, 1999 Christopher G. Demetriou.  All rights reserved.
@@ -36,7 +36,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: pucdata.c,v 1.36 2004/02/03 19:51:39 fredb Exp $");
+__KERNEL_RCSID(0, "$NetBSD: pucdata.c,v 1.36.2.1 2004/07/04 13:19:57 he Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -693,6 +693,22 @@ const struct puc_device_description puc_devices[] = {
 		{ PUC_PORT_TYPE_COM, 0x18, 0x28, COM_FREQ * 8 },
 		{ PUC_PORT_TYPE_COM, 0x18, 0x30, COM_FREQ * 8 },
 		{ PUC_PORT_TYPE_COM, 0x18, 0x38, COM_FREQ * 8 },
+	    },
+	},
+
+	{   "Titan PCI-010HV2",
+	    {   0x14d2, 0xe001, 0,      0       },
+	    {   0xffff, 0xffff, 0,      0       },
+	    {
+		{ PUC_PORT_TYPE_LPT, 0x10, 0x00, 0x00 },
+	    },
+	},
+	{   "Titan PCI-200HV2",
+	    {   0x14d2, 0xe020, 0,      0       },
+	    {   0xffff, 0xffff, 0,      0       },
+	    {
+		{ PUC_PORT_TYPE_COM, 0x10, 0x00, COM_FREQ * 8 },
+		{ PUC_PORT_TYPE_COM, 0x14, 0x00, COM_FREQ * 8 },
 	    },
 	},
 
