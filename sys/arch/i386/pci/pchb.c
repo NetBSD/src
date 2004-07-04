86a6b01ed99e1ddc17bdc26b6e6f2cb63e0c4926
mycroft <mycroft@NetBSD.org>
2004-07-04 05:53:55 +0000

    Update Serverworks chip names, and add a few more.

--- sys/arch/i386/pci/pchb.c
+++ sys/arch/i386/pci/pchb.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: pchb.c,v 1.51 2004/07/04 04:55:18 mycroft Exp $	*/
+/*	$NetBSD: pchb.c,v 1.52 2004/07/04 05:53:55 mycroft Exp $	*/
 
 /*-
  * Copyright (c) 1996, 1998, 2000 The NetBSD Foundation, Inc.
@@ -37,7 +37,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: pchb.c,v 1.51 2004/07/04 04:55:18 mycroft Exp $");
+__KERNEL_RCSID(0, "$NetBSD: pchb.c,v 1.52 2004/07/04 05:53:55 mycroft Exp $");
 
 #include <sys/types.h>
 #include <sys/param.h>
@@ -140,12 +140,16 @@ pchbattach(struct device *parent, struct device *self, void *aux)
 		 * Configure it.
 		 */
 		switch (PCI_PRODUCT(pa->pa_id)) {
-		case PCI_PRODUCT_SERVERWORKS_XX5:
-		case PCI_PRODUCT_SERVERWORKS_CIOB20:
-		case PCI_PRODUCT_SERVERWORKS_CNB20HE:
-		case PCI_PRODUCT_SERVERWORKS_CNB20LE:
-		case PCI_PRODUCT_SERVERWORKS_CIOB30:
-		case PCI_PRODUCT_SERVERWORKS_CIOBX2:
+		case PCI_PRODUCT_SERVERWORKS_CNB20_LE_AGP:
+		case PCI_PRODUCT_SERVERWORKS_CNB30_LE_PCI:
+		case PCI_PRODUCT_SERVERWORKS_CNB20_LE_PCI:
+		case PCI_PRODUCT_SERVERWORKS_CNB20_HE_PCI:
+		case PCI_PRODUCT_SERVERWORKS_CNB20_HE_AGP:
+		case PCI_PRODUCT_SERVERWORKS_CIOB_X:
+		case PCI_PRODUCT_SERVERWORKS_CNB30_HE:
+		case PCI_PRODUCT_SERVERWORKS_CNB20_HE_PCI2:
+		case PCI_PRODUCT_SERVERWORKS_CIOB_X2:
+		case PCI_PRODUCT_SERVERWORKS_CIOB_E:
 			doattach = 1;
 			if ((attachflags &
 			    (PCI_FLAGS_IO_ENABLED | PCI_FLAGS_MEM_ENABLED)) ==
