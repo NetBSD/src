09cdb1408b60549d3eb998d1c8b9a39ba546e129
mycroft <mycroft@NetBSD.org>
2004-07-06 07:07:15 +0000

    Fix function calls in the sample server to use the right function name. From PR 4411.

--- usr.bin/rpcgen/rpc_sample.c
+++ usr.bin/rpcgen/rpc_sample.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: rpc_sample.c,v 1.10 2004/06/20 22:20:16 jmc Exp $	*/
+/*	$NetBSD: rpc_sample.c,v 1.11 2004/07/06 07:07:15 mycroft Exp $	*/
 /*
  * Sun RPC is a product of Sun Microsystems, Inc. and is provided for
  * unrestricted use provided that this legend is included on all tape
@@ -38,7 +38,7 @@
 #if 0
 static char sccsid[] = "@(#)rpc_sample.c  1.1  90/08/30  (C) 1987 SMI";
 #else
-__RCSID("$NetBSD: rpc_sample.c,v 1.10 2004/06/20 22:20:16 jmc Exp $");
+__RCSID("$NetBSD: rpc_sample.c,v 1.11 2004/07/06 07:07:15 mycroft Exp $");
 #endif
 #endif
 
@@ -207,10 +207,7 @@ write_sample_server(def)
 				return_type(proc);
 				f_print(fout, "*\n");
 			}
-			if (Cflag)
-				pvname_svc(proc->proc_name, vp->vers_num);
-			else
-				pvname(proc->proc_name, vp->vers_num);
+			pvname_svc(proc->proc_name, vp->vers_num);
 			printarglist(proc, "result", RQSTP, "struct svc_req *");
 
 			f_print(fout, "{\n");
