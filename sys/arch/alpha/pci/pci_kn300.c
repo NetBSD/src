9feb85365b34c1ac9ea5db89ce8adfe9a4c169e5
tron <tron@NetBSD.org>
2004-07-09 18:29:11 +0000

    Pull up revision 1.26 (requested by mhitch in ticket #622): Ooops - buspin is what should be used.

--- sys/arch/alpha/pci/pci_kn300.c
+++ sys/arch/alpha/pci/pci_kn300.c
@@ -1,4 +1,4 @@
-/* $NetBSD: pci_kn300.c,v 1.24.12.1 2004/07/09 18:27:32 tron Exp $ */
+/* $NetBSD: pci_kn300.c,v 1.24.12.2 2004/07/09 18:29:11 tron Exp $ */
 
 /*
  * Copyright (c) 1998 by Matthew Jacob
@@ -32,7 +32,7 @@
 
 #include <sys/cdefs.h>			/* RCS ID & Copyright macro defns */
 
-__KERNEL_RCSID(0, "$NetBSD: pci_kn300.c,v 1.24.12.1 2004/07/09 18:27:32 tron Exp $");
+__KERNEL_RCSID(0, "$NetBSD: pci_kn300.c,v 1.24.12.2 2004/07/09 18:29:11 tron Exp $");
 
 #include <sys/types.h>
 #include <sys/param.h>
@@ -154,7 +154,7 @@ dec_kn300_intr_map(pa, ihp)
 	if (ccp->cc_mid == 5 && device == 1) {
 		mcpcia_irq = 16;
 	} else if (device >= 2 && device <= 5) {
-		mcpcia_irq = (device - 2) * 4 + bus - 1;
+		mcpcia_irq = (device - 2) * 4 + buspin - 1;
 	} else {
 		printf("dec_kn300_intr_map: weird device number %d\n", device);
 		return(1);
