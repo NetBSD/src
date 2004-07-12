4e8ef5a4a4b7776504f76911605d046da630d157
jonathan <jonathan@NetBSD.org>
2004-07-12 20:44:56 +0000

    Commit changes from Heimdal-current, as per discussion with Love (lha@NetBSD.ORG), to incorporate contemporary (last-year-ish) set-password and change-password extensions derived RFC-3244 (Microsoft set-password/change-password extensions), and the subsequent MIT-KRB5 APIs for changing and setting passwords.
    Required for compatibility with recent (2002/2003-ish) open-source
    code which uses the MIT KRB5 APIs for setting passwords, or for
    joining Microsoft domains as a  "computer account".
    
    Modified files (for pullup tracking purposes):
    	lib/libasn1/Makefile
    	crypto/dist/heimdal/lib/asn1/k5.asn1
     	crypto/dist/heimdal/lib/krb5/changepw.c
     	crypto/dist/heimdal/lib/krb5/krb5-protos.h
     	crypto/dist/heimdal/lib/krb5/krb5.h

--- crypto/dist/heimdal/lib/krb5/krb5-protos.h
+++ crypto/dist/heimdal/lib/krb5/krb5-protos.h
@@ -2445,6 +2445,26 @@ krb5_set_ignore_addresses (
 	krb5_context /*context*/,
 	const krb5_addresses */*addresses*/);
 
+krb5_error_code
+krb5_set_password (
+	krb5_context /*context*/,
+	krb5_creds */*creds*/,
+	char */*newpw*/,
+	krb5_principal /*targprinc*/,
+	int */*result_code*/,
+	krb5_data */*result_code_string*/,
+	krb5_data */*result_string*/);
+
+krb5_error_code
+krb5_set_password_using_ccache (
+	krb5_context /*context*/,
+	krb5_ccache /*ccache*/,
+	char */*newpw*/,
+	krb5_principal /*targprinc*/,
+	int */*result_code*/,
+	krb5_data */*result_code_string*/,
+	krb5_data */*result_string*/);
+
 void
 krb5_set_use_admin_kdc (
 	krb5_context /*context*/,
