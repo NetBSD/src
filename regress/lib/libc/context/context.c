ea200fccc13ca3582fb0b49b5534797f56446c4c
chs <chs@NetBSD.org>
2004-07-19 06:36:27 +0000

    check for getcontext() failure.

--- regress/lib/libc/context/context.c
+++ regress/lib/libc/context/context.c
@@ -1,4 +1,4 @@
-/* $NetBSD: context.c,v 1.2 2004/03/01 12:04:18 drochner Exp $ */
+/* $NetBSD: context.c,v 1.3 2004/07/19 06:36:27 chs Exp $ */
 
 #include <malloc.h>
 #include <ucontext.h>
@@ -35,12 +35,14 @@ main()
 	int i, res;
 
 	for (i = 0; i < DEPTH; i++) {
-		getcontext(&uc[i]);
+		res = getcontext(&uc[i]);
+		if (res)
+			err(1, "getcontext");
 		uc[i].uc_stack.ss_sp = malloc(STACKSZ);
 		uc[i].uc_stack.ss_size = STACKSZ;
 		if (i > 0)
 			uc[i].uc_link = &uc[i - 1];
-		makecontext(&uc[i], (void *)run, 10, i, 
+		makecontext(&uc[i], (void *)run, 10, i,
 			0, 1, 2, 3, 4, 5, 6, 7, 8);
 	}
 	res = setcontext(&uc[DEPTH-1]);
