b22d29f8c44c563eca4c265487d00427524b6d03
tron <tron@NetBSD.org>
2004-07-23 07:02:04 +0000

    Pull up revision 1.3 (requested by rumble in ticket #682): RCS Id police

--- sys/arch/sgimips/sgimips/arcemu.c
+++ sys/arch/sgimips/sgimips/arcemu.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: arcemu.c,v 1.6.2.3 2004/07/23 06:59:38 tron Exp $	*/
+/*	$NetBSD: arcemu.c,v 1.6.2.4 2004/07/23 07:02:04 tron Exp $	*/
 
 /*
  * Copyright (c) 2004 Steve Rumble 
@@ -27,6 +27,9 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+
+#include <sys/cdefs.h>
+__KERNEL_RCSID(0, "$NetBSD: arcemu.c,v 1.6.2.4 2004/07/23 07:02:04 tron Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
