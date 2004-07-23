c8bc28344c94c0b841de342bd5ef376ebadba690
martti <martti@NetBSD.org>
2004-07-23 05:33:55 +0000

    Import IPFilter 4.1.3

--- dist/ipf/lib/printactivenat.c
+++ dist/ipf/lib/printactivenat.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: printactivenat.c,v 1.1.1.1 2004/03/28 08:56:19 martti Exp $	*/
+/*	$NetBSD: printactivenat.c,v 1.1.1.2 2004/07/23 05:34:36 martti Exp $	*/
 
 /*
  * Copyright (C) 1993-2001 by Darren Reed.
@@ -12,7 +12,7 @@
 
 
 #if !defined(lint)
-static const char rcsid[] = "@(#)Id: printactivenat.c,v 1.3 2004/01/17 17:31:20 darrenr Exp";
+static const char rcsid[] = "@(#)Id: printactivenat.c,v 1.3.2.4 2004/05/11 16:07:32 darrenr Exp";
 #endif
 
 
@@ -59,12 +59,27 @@ int opts;
 		printf("bytes %lu/%lu pkts %lu/%lu", nat->nat_bytes[0],
 			nat->nat_bytes[1], nat->nat_pkts[0], nat->nat_pkts[1]);
 #endif
-#if SOLARIS
-		printf(" %lx", nat->nat_ipsumd);
-#endif
+		printf(" ipsumd %x", nat->nat_ipsumd);
 	}
 
+	if (opts & OPT_DEBUG) {
+		printf("\n\tnat_next %p _pnext %p _hm %p\n",
+			nat->nat_next, nat->nat_pnext, nat->nat_hm);
+		printf("\t_hnext %p/%p _phnext %p/%p\n",
+			nat->nat_hnext[0], nat->nat_hnext[1],
+			nat->nat_phnext[0], nat->nat_phnext[1]);
+		printf("\t_data %p _me %p _state %p _aps %p\n",
+			nat->nat_data, nat->nat_me, nat->nat_state, nat->nat_aps);
+		printf("\tfr %p ptr %p ifps %p/%p sync %p\n",
+			nat->nat_fr, nat->nat_ptr, nat->nat_ifps[0],
+			nat->nat_ifps[1], nat->nat_sync);
+		printf("\ttqe:pnext %p next %p ifq %p parent %p/%p\n",
+			nat->nat_tqe.tqe_pnext, nat->nat_tqe.tqe_next,
+			nat->nat_tqe.tqe_ifq, nat->nat_tqe.tqe_parent, nat);
+		printf("\ttqe:die %ld touched %ld flags %x state %d/%d\n",
+			nat->nat_tqe.tqe_die, nat->nat_tqe.tqe_touched,
+			nat->nat_tqe.tqe_flags, nat->nat_tqe.tqe_state[0],
+			nat->nat_tqe.tqe_state[1]);
+	}
 	putchar('\n');
-	if (nat->nat_aps)
-		printaps(nat->nat_aps, opts);
 }
