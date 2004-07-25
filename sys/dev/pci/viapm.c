aa906768f37e9f7f7c30d3da3f438b3b02ebcd5a
christos <christos@NetBSD.org>
2004-07-25 16:07:08 +0000

    Match viatech's power controller (Sergio Jimenez)

--- sys/dev/pci/viapm.c
+++ sys/dev/pci/viapm.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: viapm.c,v 1.9 2003/01/01 00:10:23 thorpej Exp $	*/
+/*	$NetBSD: viapm.c,v 1.10 2004/07/25 16:07:08 christos Exp $	*/
 
 /*
  * Copyright (c) 2000 Johan Danielsson
@@ -40,7 +40,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: viapm.c,v 1.9 2003/01/01 00:10:23 thorpej Exp $");
+__KERNEL_RCSID(0, "$NetBSD: viapm.c,v 1.10 2004/07/25 16:07:08 christos Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -66,10 +66,16 @@ viapm_match(struct device * parent, struct cfdata * match, void *aux)
 {
 	struct pci_attach_args *pa = aux;
 
-	if (PCI_VENDOR(pa->pa_id) == PCI_VENDOR_VIATECH &&
-	    PCI_PRODUCT(pa->pa_id) == PCI_PRODUCT_VIATECH_VT82C686A_SMB)
+	if (PCI_VENDOR(pa->pa_id) != PCI_VENDOR_VIATECH)
+		return 0;
+
+	switch (PCI_PRODUCT(pa->pa_id)) {
+	case PCI_PRODUCT_VIATECH_VT82C686A_SMB:
+	case PCI_PRODUCT_VIATECH_VT8231_PWR:
 		return 1;
-	return 0;
+	default:
+		return 0;
+	}
 }
 
 static const char *
