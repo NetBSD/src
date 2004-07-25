b2c962939eec680e033eecc8e073b9a8ee6d6b17
tron <tron@NetBSD.org>
2004-07-25 07:45:01 +0000

    Pull up revision 1.198 (requested by nakayama in ticket #711): Change interrupt pending list end value to -1, to avoid linking an interrupt handler twice. It may fix "sleep forever" bug.

--- sys/arch/sparc64/sparc64/locore.s
+++ sys/arch/sparc64/sparc64/locore.s
@@ -1,4 +1,4 @@
-/*	$NetBSD: locore.s,v 1.196 2004/03/26 23:18:42 petrov Exp $	*/
+/*	$NetBSD: locore.s,v 1.196.2.1 2004/07/25 07:45:01 tron Exp $	*/
 
 /*
  * Copyright (c) 1996-2002 Eduardo Horvath
@@ -4033,7 +4033,7 @@ return_from_syscall:
 	.data
 	.globl	intrpending
 intrpending:
-	.space	16 * 8 * PTRSZ
+	.space	16 * 8 * PTRSZ, -1
 
 #ifdef DEBUG
 #define INTRDEBUG_VECTOR	0x1
@@ -4492,9 +4492,9 @@ sparc_intr_retry:
 1:
 	membar	#StoreLoad		! Make sure any failed casxa insns complete
 	LDPTR	[%l4], %l2		! Check a slot
-	brz,pn	%l2, intrcmplt		! Empty list?
-
-	 clr	%l7
+	cmp	%l2, -1
+	beq,pn	CCCR, intrcmplt		! Empty list?
+	 mov	-1, %l7
 	membar	#LoadStore
 	CASPTR	[%l4] ASI_N, %l2, %l7	! Grab the entire list
 	cmp	%l7, %l2
@@ -4517,7 +4517,8 @@ sparc_intr_retry:
 	stx	%g0, [%l1]		! Clear intr source
 	membar	#Sync			! Should not be needed
 0:
-	brnz,pn	%l7, 2b			! 'Nother?
+	cmp	%l7, -1
+	bne,pn	CCCR, 2b		! 'Nother?
 	 mov	%l7, %l2
 
 #else /* INTRLIST */
