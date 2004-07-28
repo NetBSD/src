9f3cc21f544928f2d967ac0c65b077d2a2026e66
manu <manu@NetBSD.org>
2004-07-28 22:24:06 +0000

    Empty shell for Darwin audit API

--- sys/arch/powerpc/powerpc/darwin_machdep.c
+++ sys/arch/powerpc/powerpc/darwin_machdep.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: darwin_machdep.c,v 1.16 2004/07/07 22:02:12 christos Exp $ */
+/*	$NetBSD: darwin_machdep.c,v 1.17 2004/07/28 22:24:06 manu Exp $ */
 
 /*-
  * Copyright (c) 2002 The NetBSD Foundation, Inc.
@@ -37,7 +37,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: darwin_machdep.c,v 1.16 2004/07/07 22:02:12 christos Exp $");
+__KERNEL_RCSID(0, "$NetBSD: darwin_machdep.c,v 1.17 2004/07/28 22:24:06 manu Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -48,6 +48,7 @@ __KERNEL_RCSID(0, "$NetBSD: darwin_machdep.c,v 1.16 2004/07/07 22:02:12 christos
 #include <compat/mach/mach_types.h>
 #include <compat/mach/mach_vm.h>
 
+#include <compat/darwin/darwin_audit.h>
 #include <compat/darwin/darwin_signal.h>
 #include <compat/darwin/darwin_syscallargs.h>
 
