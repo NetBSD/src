097ab7ad57fcac65c99292fad8964edfcebf7076
manu <manu@NetBSD.org>
2004-07-28 22:24:06 +0000

    Empty shell for Darwin audit API

--- sys/compat/darwin/darwin_sysctl.c
+++ sys/compat/darwin/darwin_sysctl.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: darwin_sysctl.c,v 1.31 2004/07/28 21:30:00 manu Exp $ */
+/*	$NetBSD: darwin_sysctl.c,v 1.32 2004/07/28 22:24:06 manu Exp $ */
 
 /*-
  * Copyright (c) 2002 The NetBSD Foundation, Inc.
@@ -37,7 +37,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: darwin_sysctl.c,v 1.31 2004/07/28 21:30:00 manu Exp $");
+__KERNEL_RCSID(0, "$NetBSD: darwin_sysctl.c,v 1.32 2004/07/28 22:24:06 manu Exp $");
 
 #include <sys/types.h>
 #include <sys/param.h>
@@ -61,6 +61,7 @@ __KERNEL_RCSID(0, "$NetBSD: darwin_sysctl.c,v 1.31 2004/07/28 21:30:00 manu Exp 
 #include <compat/mach/mach_types.h>
 #include <compat/mach/mach_vm.h>
 
+#include <compat/darwin/darwin_audit.h>
 #include <compat/darwin/darwin_types.h>
 #include <compat/darwin/darwin_exec.h>
 #include <compat/darwin/darwin_sysctl.h>
