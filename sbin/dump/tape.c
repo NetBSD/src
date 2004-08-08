35f8a3242f9d286307a34e4565e72e1a19153359
lukem <lukem@NetBSD.org>
2004-08-08 09:46:16 +0000

    Signal handlers should manipulate 'volatile sig_atomic_t' instead of 'int'.

--- sbin/dump/tape.c
+++ sbin/dump/tape.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: tape.c,v 1.43 2004/03/15 01:06:06 lukem Exp $	*/
+/*	$NetBSD: tape.c,v 1.44 2004/08/08 09:46:16 lukem Exp $	*/
 
 /*-
  * Copyright (c) 1980, 1991, 1993
@@ -34,7 +34,7 @@
 #if 0
 static char sccsid[] = "@(#)tape.c	8.4 (Berkeley) 5/1/95";
 #else
-__RCSID("$NetBSD: tape.c,v 1.43 2004/03/15 01:06:06 lukem Exp $");
+__RCSID("$NetBSD: tape.c,v 1.44 2004/08/08 09:46:16 lukem Exp $");
 #endif
 #endif /* not lint */
 
@@ -116,7 +116,7 @@ static int64_t tapea_volume;	/* value of spcl.c_tapea at volume start */
 
 int master;		/* pid of master, for sending error signals */
 int tenths;		/* length of tape used per block written */
-static int caught;	/* have we caught the signal to proceed? */
+static volatile sig_atomic_t caught;	/* have we caught the signal to proceed? */
 
 int
 alloctape(void)
