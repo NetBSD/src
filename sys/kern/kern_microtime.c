564789942734e171e46b7fcd9cf69505215393dd
christos <christos@NetBSD.org>
2004-08-11 16:25:51 +0000

    make the reset time a DIAGNOSTIC, and print the cpu#.

--- sys/kern/kern_microtime.c
+++ sys/kern/kern_microtime.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: kern_microtime.c,v 1.10 2004/06/27 15:21:30 fredb Exp $	*/
+/*	$NetBSD: kern_microtime.c,v 1.11 2004/08/11 16:25:51 christos Exp $	*/
 
 /*-
  * Copyright (c) 2001 The NetBSD Foundation, Inc.
@@ -54,7 +54,7 @@
 
 #include <sys/cdefs.h>			/* RCS ID & Copyright macro defns */
 
-__KERNEL_RCSID(0, "$NetBSD: kern_microtime.c,v 1.10 2004/06/27 15:21:30 fredb Exp $");
+__KERNEL_RCSID(0, "$NetBSD: kern_microtime.c,v 1.11 2004/08/11 16:25:51 christos Exp $");
 
 #include "opt_multiprocessor.h"
 
@@ -202,13 +202,13 @@ cc_microset(struct cpu_info *ci)
 		ci->ci_cc_ms_delta = delta;
 		ci->ci_cc_denom = denom;
 #if 0
-		printf("cc_microset: delta %" PRId64 ", denom %" PRId64 "\n",
-		       delta, denom);
+		printf("cc_microset[%lu]: delta %" PRId64
+		    ", denom %" PRId64 "\n", ci->ci_cpuid, delta, denom);
 #endif
 	} else {
-#if 0
-		printf("cc_microset: delta %" PRId64 ", resetting state\n",
-		       delta);
+#ifdef DIAGNOSTIC
+		printf("cc_microset[%lu]: delta %" PRId64 ", resetting state\n",
+		       ci->ci_cpuid, delta);
 #endif
 		ci->ci_cc_ms_delta = 1000000;
 		ci->ci_cc_denom = cpu_frequency(ci);
