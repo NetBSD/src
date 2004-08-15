e07e2632a2cf8b998a69d9b94c86c5f7611881d0
dsl <dsl>
2004-08-15 21:02:28 +0000

    Add a field to hold the io address of an i386 serial console. Allows a tty that isn't known to the bios to be used. Extend the keymap to 64 chars - I don't think 16 was enough to be useful.

--- sys/sys/bootblock.h
+++ sys/sys/bootblock.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: bootblock.h,v 1.28 2004/07/28 09:17:31 skrll Exp $	*/
+/*	$NetBSD: bootblock.h,v 1.29 2004/08/15 21:02:28 dsl Exp $	*/
 
 /*-
  * Copyright (c) 2002-2004 The NetBSD Foundation, Inc.
@@ -689,7 +689,8 @@ struct x86_boot_params {
 	uint32_t	bp_consdev;
 	uint32_t	bp_conspeed;
 	uint8_t		bp_password[16];	/* md5 hash of password */
-	char		bp_keymap[16];	/* keyboard traslation map */
+	char		bp_keymap[64];	/* keyboard traslation map */
+	uint32_t	bp_consaddr;	/* ioaddr for console */
 };
 
 #endif	/* !defined(__ASSEMBLER__) */				/* } */
