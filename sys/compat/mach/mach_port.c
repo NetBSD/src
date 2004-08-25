e1d10710de7c181c5e68f2cda434ee5ced5f9b13
he <he@NetBSD.org>
2004-08-25 21:27:00 +0000

    Pull up revision 1.54 (requested by hubertf in ticket #787):   Fix copyright notice, as OK'd by the copyright holder.

--- sys/compat/mach/mach_port.c
+++ sys/compat/mach/mach_port.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: mach_port.c,v 1.52 2004/03/24 16:55:07 pooka Exp $ */
+/*	$NetBSD: mach_port.c,v 1.52.2.1 2004/08/25 21:27:00 he Exp $ */
 
 /*-
  * Copyright (c) 2002-2003 The NetBSD Foundation, Inc.
@@ -17,6 +17,7 @@
  *    documentation and/or other materials provided with the distribution.
  * 3. All advertising materials mentioning features or use of this software
  *    must display the following acknowledgement:
+ *        This product includes software developed by the NetBSD
  *        Foundation, Inc. and its contributors.
  * 4. Neither the name of The NetBSD Foundation nor the names of its
  *    contributors may be used to endorse or promote products derived
@@ -38,7 +39,7 @@
 #include "opt_compat_darwin.h"
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: mach_port.c,v 1.52 2004/03/24 16:55:07 pooka Exp $");
+__KERNEL_RCSID(0, "$NetBSD: mach_port.c,v 1.52.2.1 2004/08/25 21:27:00 he Exp $");
 
 #include <sys/types.h>
 #include <sys/param.h>
