f15e478a643c14704982d121c66aa2a24cea0825
jmc <jmc@NetBSD.org>
2004-08-26 05:00:41 +0000

    Pullup rev 1.26-1.30 (requested by he in ticket #158)
    Update to NetBSD ftpd 20040809.  Fixes SA#2004-009.

--- libexec/ftpd/popen.c
+++ libexec/ftpd/popen.c
@@ -1,7 +1,7 @@
-/*	$NetBSD: popen.c,v 1.22.4.1 2001/03/29 14:14:18 lukem Exp $	*/
+/*	$NetBSD: popen.c,v 1.22.4.2 2004/08/26 05:00:41 jmc Exp $	*/
 
 /*-
- * Copyright (c) 1999 The NetBSD Foundation, Inc.
+ * Copyright (c) 1999-2004 The NetBSD Foundation, Inc.
  * All rights reserved.
  *
  * This code is derived from software contributed to The NetBSD Foundation
@@ -51,11 +51,7 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed by the University of
- *	California, Berkeley and its contributors.
- * 4. Neither the name of the University nor the names of its contributors
+ * 3. Neither the name of the University nor the names of its contributors
  *    may be used to endorse or promote products derived from this software
  *    without specific prior written permission.
  *
@@ -78,7 +74,7 @@
 #if 0
 static char sccsid[] = "@(#)popen.c	8.3 (Berkeley) 4/6/94";
 #else
-__RCSID("$NetBSD: popen.c,v 1.22.4.1 2001/03/29 14:14:18 lukem Exp $");
+__RCSID("$NetBSD: popen.c,v 1.22.4.2 2004/08/26 05:00:41 jmc Exp $");
 #endif
 #endif /* not lint */
 
@@ -88,7 +84,6 @@ __RCSID("$NetBSD: popen.c,v 1.22.4.1 2001/03/29 14:14:18 lukem Exp $");
 
 #include <errno.h>
 #include <glob.h>
-#include <setjmp.h>
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -117,7 +112,7 @@ static int fds;
 extern int ls_main(int, char *[]);
 
 FILE *
-ftpd_popen(char *argv[], const char *type, int stderrfd)
+ftpd_popen(char *argv[], const char *ptype, int stderrfd)
 {
 	FILE *iop;
 	int argc, pdes[2], pid, isls;
@@ -126,7 +121,7 @@ ftpd_popen(char *argv[], const char *type, int stderrfd)
 
 	iop = NULL;
 	isls = 0;
-	if ((*type != 'r' && *type != 'w') || type[1])
+	if ((*ptype != 'r' && *ptype != 'w') || ptype[1])
 		return (NULL);
 
 	if (!pids) {
@@ -180,7 +175,7 @@ ftpd_popen(char *argv[], const char *type, int stderrfd)
 		goto pfree;
 		/* NOTREACHED */
 	case 0:				/* child */
-		if (*type == 'r') {
+		if (*ptype == 'r') {
 			if (pdes[1] != STDOUT_FILENO) {
 				dup2(pdes[1], STDOUT_FILENO);
 				(void)close(pdes[1]);
@@ -209,11 +204,11 @@ ftpd_popen(char *argv[], const char *type, int stderrfd)
 		_exit(1);
 	}
 	/* parent; assume fdopen can't fail...  */
-	if (*type == 'r') {
-		iop = fdopen(pdes[0], type);
+	if (*ptype == 'r') {
+		iop = fdopen(pdes[0], ptype);
 		(void)close(pdes[1]);
 	} else {
-		iop = fdopen(pdes[1], type);
+		iop = fdopen(pdes[1], ptype);
 		(void)close(pdes[0]);
 	}
 	pids[fileno(iop)] = pid;
@@ -229,7 +224,7 @@ ftpd_pclose(FILE *iop)
 {
 	int fdes, status;
 	pid_t pid;
-	sigset_t sigset, osigset;
+	sigset_t nsigset, osigset;
 
 	/*
 	 * pclose returns -1 if stream is not associated with a
@@ -238,11 +233,11 @@ ftpd_pclose(FILE *iop)
 	if (pids == 0 || pids[fdes = fileno(iop)] == 0)
 		return (-1);
 	(void)fclose(iop);
-	sigemptyset(&sigset);
-	sigaddset(&sigset, SIGINT);
-	sigaddset(&sigset, SIGQUIT);
-	sigaddset(&sigset, SIGHUP);
-	sigprocmask(SIG_BLOCK, &sigset, &osigset);
+	sigemptyset(&nsigset);
+	sigaddset(&nsigset, SIGINT);
+	sigaddset(&nsigset, SIGQUIT);
+	sigaddset(&nsigset, SIGHUP);
+	sigprocmask(SIG_BLOCK, &nsigset, &osigset);
 	while ((pid = waitpid(pids[fdes], &status, 0)) < 0 && errno == EINTR)
 		continue;
 	sigprocmask(SIG_SETMASK, &osigset, NULL);
