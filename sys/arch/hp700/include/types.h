1fe063acbea74e99d0dc022e8aa212c3d6123423
jkunz <jkunz>
2004-08-26 16:48:06 +0000

    Import STI wscons(4) driver from OpenBSD and add new driver for PS/2 keyboard / mouse pots of LASI.

--- sys/arch/hp700/include/types.h
+++ sys/arch/hp700/include/types.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: types.h,v 1.2 2004/06/15 20:43:41 jkunz Exp $	*/
+/*	$NetBSD: types.h,v 1.3 2004/08/26 16:48:06 jkunz Exp $	*/
 
 #ifndef	_HP700_TYPES_H_
 #define	_HP700_TYPES_H_
@@ -7,5 +7,6 @@
 
 #define	__HAVE_GENERIC_SOFT_INTERRUPTS
 #define	__HAVE_DEVICE_REGISTER
+#define	__HAVE_NWSCONS
 
 #endif	/* _HP700_TYPES_H_ */
