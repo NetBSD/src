a4ee532ba4a01d2162185e701d4d461ca998c663
enami <enami@NetBSD.org>
2004-09-07 01:07:40 +0000

    Adjust caller for net/if_ieee80211subr.c rev 1.11 change; the argument `reassoc' is replaced with testing if the type is ..._REASSOC_REQ.

--- sys/net80211/ieee80211_proto.c
+++ sys/net80211/ieee80211_proto.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: ieee80211_proto.c,v 1.16 2004/08/10 00:57:22 dyoung Exp $	*/
+/*	$NetBSD: ieee80211_proto.c,v 1.17 2004/09/07 01:07:40 enami Exp $	*/
 /*-
  * Copyright (c) 2001 Atsushi Onoe
  * Copyright (c) 2002, 2003 Sam Leffler, Errno Consulting
@@ -35,7 +35,7 @@
 #ifdef __FreeBSD__
 __FBSDID("$FreeBSD: src/sys/net80211/ieee80211_proto.c,v 1.8 2004/04/02 20:22:25 sam Exp $");
 #else
-__KERNEL_RCSID(0, "$NetBSD: ieee80211_proto.c,v 1.16 2004/08/10 00:57:22 dyoung Exp $");
+__KERNEL_RCSID(0, "$NetBSD: ieee80211_proto.c,v 1.17 2004/09/07 01:07:40 enami Exp $");
 #endif
 
 /*
@@ -514,7 +514,7 @@ ieee80211_newstate(struct ieee80211com *ic, enum ieee80211_state nstate, int mgt
 			break;
 		case IEEE80211_S_RUN:
 			IEEE80211_SEND_MGMT(ic, ni,
-			    IEEE80211_FC0_SUBTYPE_ASSOC_REQ, 1);
+			    IEEE80211_FC0_SUBTYPE_REASSOC_REQ, 0);
 			break;
 		}
 		break;
