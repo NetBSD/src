2c2da4ad952a91aec45cb5982e375e83aa940f31
jdolecek <jdolecek@NetBSD.org>
2004-09-08 19:45:21 +0000

    implement basic exit_group(2), which just terminates calling thread; apparently not many apps use thread groups yet, at least not with default SuSE 9.1 libraries, so this is enough for now

--- sys/compat/linux/common/linux_sched.c
+++ sys/compat/linux/common/linux_sched.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: linux_sched.c,v 1.13 2004/09/08 19:41:24 jdolecek Exp $	*/
+/*	$NetBSD: linux_sched.c,v 1.14 2004/09/08 19:45:22 jdolecek Exp $	*/
 
 /*-
  * Copyright (c) 1999 The NetBSD Foundation, Inc.
@@ -42,7 +42,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: linux_sched.c,v 1.13 2004/09/08 19:41:24 jdolecek Exp $");
+__KERNEL_RCSID(0, "$NetBSD: linux_sched.c,v 1.14 2004/09/08 19:45:22 jdolecek Exp $");
 
 #include <sys/param.h>
 #include <sys/mount.h>
@@ -50,6 +50,7 @@ __KERNEL_RCSID(0, "$NetBSD: linux_sched.c,v 1.13 2004/09/08 19:41:24 jdolecek Ex
 #include <sys/systm.h>
 #include <sys/sa.h>
 #include <sys/syscallargs.h>
+#include <sys/wait.h>
 
 #include <machine/cpu.h>
 
@@ -340,3 +341,28 @@ linux_sys_sched_get_priority_min(cl, v, retval)
 	*retval = 0;
 	return 0;
 }
+
+#ifndef __m68k__
+/* Present on everything but m68k */
+int
+linux_sys_exit_group(l, v, retval)
+	struct lwp *l;
+	void *v;
+	register_t *retval;
+{
+	struct linux_sys_exit_group_args /* {
+		syscallarg(int) error_code;
+	} */ *uap = v;
+
+	/*
+	 * XXX The calling thread is supposed to kill all threads
+	 * in the same thread group (i.e. all threads created
+	 * via clone(2) with CLONE_THREAD flag set). This appears
+	 * to not be used yet, so the thread group handling
+	 * is currenly not implemented.
+	 */
+
+	exit1(l, W_EXITCODE(SCARG(uap, error_code), 0));
+	/* NOTREACHED */
+}
+#endif /* !__m68k__ */
