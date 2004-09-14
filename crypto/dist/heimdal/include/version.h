a2a7f80bee8bc93c03e9615f9b38cc2983acc3d4
lha <lha@NetBSD.org>
2004-09-14 07:45:53 +0000

    Import heimdal 0.6.3
    Changes in release 0.6.3
    
     * fix vulnerabilities in ftpd
     * support for linux AFS /proc "syscalls"
     * support for RFC3244 (Windows 2000 Kerberos Change/Set Password) in
       kpasswdd
     * fix possible KDC denial of service
     * bug fixes

--- crypto/dist/heimdal/include/version.h
+++ crypto/dist/heimdal/include/version.h
@@ -1,2 +1,2 @@
-const char *heimdal_long_version = "@(#)$Version: Heimdal 0.6.2 by lha on hummel.it.su.se (i386-unknown-netbsdelf2.0C) Thu May  6 18:14:43 CEST 2004 $";
-const char *heimdal_version = "Heimdal 0.6.2";
+const char *heimdal_long_version = "@(#)$Version: Heimdal 0.6.3 by lha on nutcracker.stacken.kth.se (i386-unknown-netbsdelf2.0G) Mon Sep 13 22:50:55 CEST 2004 $";
+const char *heimdal_version = "Heimdal 0.6.3";
