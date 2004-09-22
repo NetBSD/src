602704146270963b899a772e1cfd5cff45f6544e
skrll <skrll>
2004-09-22 20:27:03 +0000

    Fix.

---
+++ sys/dev/isa/commultiprint.c
@@ -0,0 +1,18 @@
+/* $NetBSD: commultiprint.c,v 1.1.2.4 2004/09/22 20:27:05 skrll Exp $ */
+
+#include <sys/param.h>
+#include <sys/systm.h>
+#include <sys/device.h>
+#include <machine/bus.h>
+#include <dev/isa/com_multi.h>
+
+int
+commultiprint(void *vca, const char *pnp)
+{
+	struct commulti_attach_args *ca = vca;
+
+	if (pnp)
+		aprint_normal("com at %s", pnp);
+	aprint_normal(" slave %d", ca->ca_slave);
+	return (UNCONF);
+}
