24536605818fd2f9fa2d940b2501c7d63de97ccb
jmc <jmc@NetBSD.org>
2004-09-23 02:55:07 +0000

    Welcome to 2.0_RC1

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.188.2.2 2004/03/29 15:31:52 jmc Exp $	*/
+/*	$NetBSD: param.h,v 1.188.2.3 2004/09/23 02:55:07 jmc Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -67,7 +67,7 @@
  * needs to be updated and the changes sent back to the groff maintainers.
  */
 
-#define	__NetBSD_Version__	200000000	/* NetBSD 2.0_BETA */
+#define	__NetBSD_Version__	200000000	/* NetBSD 2.0_RC1 */
 
 /*
  * Historical NetBSD #define
