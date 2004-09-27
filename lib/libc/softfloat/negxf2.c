9ac3a85e48b56f80b7c13c21aabfcec18b0ebd56
he <he@NetBSD.org>
2004-09-27 10:16:24 +0000

    Only compile the floatx80-using functions if the arch in question defines the FLOATX80 macro.  Fixes build problem for arm ports.

--- lib/libc/softfloat/negxf2.c
+++ lib/libc/softfloat/negxf2.c
@@ -1,4 +1,4 @@
-/* $NetBSD: negxf2.c,v 1.1 2004/09/26 21:13:27 jmmv Exp $ */
+/* $NetBSD: negxf2.c,v 1.2 2004/09/27 10:16:24 he Exp $ */
 
 /*
  * Written by Ben Harris, 2000.  This file is in the Public Domain.
@@ -10,8 +10,10 @@
 
 #include <sys/cdefs.h>
 #if defined(LIBC_SCCS) && !defined(lint)
-__RCSID("$NetBSD: negxf2.c,v 1.1 2004/09/26 21:13:27 jmmv Exp $");
+__RCSID("$NetBSD: negxf2.c,v 1.2 2004/09/27 10:16:24 he Exp $");
 #endif /* LIBC_SCCS and not lint */
+
+#ifdef FLOATX80
 
 floatx80 __negxf2(floatx80);
 
@@ -22,3 +24,4 @@ __negxf2(floatx80 a)
 	/* libgcc1.c says -a */
 	return __mulxf3(a,__floatsixf(-1));
 }
+#endif /* FLOATX80 */
