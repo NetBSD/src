346d4f2b483a6b531ed223ea6516bca77027be43
gavan <gavan@NetBSD.org>
2004-10-13 23:28:34 +0000

    Initial import of iyonix port.
    The Iyonix is a desktop machine from Castle Technology, based on a 600MHz
    XScale[tm] 80321 processor.
    
    * Uses the bootloader from NetBSD/acorn32, which is now 32-bit compatible.
    * Currently boots multiuser with a serial console.
    * Device support is not yet complete.
    
    With help from abs.

---
+++ sys/arch/iyonix/include/mcontext.h
@@ -0,0 +1,3 @@
+/*	$NetBSD: mcontext.h,v 1.1 2004/10/13 23:28:36 gavan Exp $	*/
+
+#include <arm/mcontext.h>
---
+++ sys/arch/iyonix/include/types.h
@@ -0,0 +1,12 @@
+/*	$NetBSD: types.h,v 1.1 2004/10/13 23:28:36 gavan Exp $	*/
+
+#ifndef _IYONIX_TYPES_H_
+#define	_IYONIX_TYPES_H_
+
+#include <arm/arm32/types.h>
+
+#define	__HAVE_GENERIC_SOFT_INTERRUPTS
+#define	__HAVE_DEVICE_REGISTER
+#define	__HAVE_NWSCONS
+
+#endif
