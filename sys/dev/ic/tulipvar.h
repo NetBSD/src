ec442b3b0bb17d4bc7f82f2f8b01f5c7821e4816
christos <christos@NetBSD.org>
2004-10-24 00:35:08 +0000

    Change the sc_name buffer size from 16 to 32, since we already oveflow 16 in if_tlp_pci.c... The name code really needs to be fixed so that it does not have a buffer overflow again:
    1. the cardbus and eisa code don't initialize sc_name; it probably should?
    2. tlp_attach() seems to print the name and the ethernet address, but
       the pci front end seems to do the same.
    3. some of the name determination code in the pci front-end does not guarantee
       nul terminated strings.
    
    Really, all this name code is just bloat and another place for bugs to hide.

--- sys/dev/ic/tulipvar.h
+++ sys/dev/ic/tulipvar.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: tulipvar.h,v 1.50 2003/12/18 18:39:36 thorpej Exp $	*/
+/*	$NetBSD: tulipvar.h,v 1.51 2004/10/24 00:35:08 christos Exp $	*/
 
 /*-
  * Copyright (c) 1998, 1999, 2000 The NetBSD Foundation, Inc.
@@ -369,7 +369,7 @@ struct tulip_softc {
 	tulip_chip_t	sc_chip;	/* chip type */
 	int		sc_rev;		/* chip revision */
 	int		sc_flags;	/* misc flags. */
-	char		sc_name[16];	/* board name */
+	char		sc_name[32];	/* board name */
 	u_int32_t	sc_cacheline;	/* cache line size */
 	u_int32_t	sc_maxburst;	/* maximum burst length */
 	int		sc_devno;	/* PCI device # */
