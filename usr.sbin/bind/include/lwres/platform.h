61071abbd73434e97d76d4dbea6f36cff7f868af
christos <christos@NetBSD.org>
2004-11-07 01:13:32 +0000

    Add netbsd id

--- usr.sbin/bind/include/lwres/platform.h
+++ usr.sbin/bind/include/lwres/platform.h
@@ -1,3 +1,5 @@
+/*	$NetBSD: platform.h,v 1.2 2004/11/07 01:13:32 christos Exp $	*/
+
 /*
  * Copyright (C) 2004  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 2000, 2001  Internet Software Consortium.
