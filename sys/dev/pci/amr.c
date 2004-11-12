d6765337edc6f8e6b35b7054eacce562982ab6ec
jmc <jmc@NetBSD.org>
2004-11-12 06:27:33 +0000

    Pullup rev 1.24 (requested by martti in ticket #953)
    Added Dell PERC 4/SC

--- sys/dev/pci/amr.c
+++ sys/dev/pci/amr.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: amr.c,v 1.19.2.2 2004/11/12 06:14:28 jmc Exp $	*/
+/*	$NetBSD: amr.c,v 1.19.2.3 2004/11/12 06:27:33 jmc Exp $	*/
 
 /*-
  * Copyright (c) 2002, 2003 The NetBSD Foundation, Inc.
@@ -71,7 +71,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: amr.c,v 1.19.2.2 2004/11/12 06:14:28 jmc Exp $");
+__KERNEL_RCSID(0, "$NetBSD: amr.c,v 1.19.2.3 2004/11/12 06:27:33 jmc Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -137,6 +137,7 @@ struct amr_pci_type {
 	{ PCI_VENDOR_DELL,  PCI_PRODUCT_DELL_PERC_4DI, AT_QUARTZ },
 	{ PCI_VENDOR_DELL,  PCI_PRODUCT_DELL_PERC_4DI_2, AT_QUARTZ },
 	{ PCI_VENDOR_DELL,  PCI_PRODUCT_DELL_PERC_4ESI, AT_QUARTZ },
+	{ PCI_VENDOR_SYMBIOS,  PCI_PRODUCT_SYMBIOS_PERC_4SC, AT_QUARTZ },
 };
 
 struct amr_typestr {
