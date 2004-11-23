82d6fd98ad536534c38ede2a5f560ebc1a65a1cc
aymeric <aymeric@NetBSD.org>
2004-11-23 14:36:50 +0000

    bump version: coredump fixed when autocompleting a filename wider than screen

--- usr.bin/vi/ex/version.h
+++ usr.bin/vi/ex/version.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: version.h,v 1.16 2004/06/13 10:17:14 aymeric Exp $	*/
+/*	$NetBSD: version.h,v 1.17 2004/11/23 14:36:50 aymeric Exp $	*/
 
 #define	VI_VERSION \
-    "Version 1.79nb13 (10/23/96) The CSRG, University of California, Berkeley."
+    "Version 1.79nb14 (10/23/96) The CSRG, University of California, Berkeley."
