e359f3e29557523dd97449855477337edd37994f
martin <martin@NetBSD.org>
2004-11-24 10:15:54 +0000

    Add BCM 4401 phy (from FreeBSD).

--- sys/dev/mii/bmtphy.c
+++ sys/dev/mii/bmtphy.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: bmtphy.c,v 1.15 2004/08/23 06:16:06 thorpej Exp $	*/
+/*	$NetBSD: bmtphy.c,v 1.16 2004/11/24 10:15:54 martin Exp $	*/
 
 /*-
  * Copyright (c) 1998, 1999, 2000, 2001 The NetBSD Foundation, Inc.
@@ -73,7 +73,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: bmtphy.c,v 1.15 2004/08/23 06:16:06 thorpej Exp $");
+__KERNEL_RCSID(0, "$NetBSD: bmtphy.c,v 1.16 2004/11/24 10:15:54 martin Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -115,6 +115,8 @@ static const struct mii_phydesc bmtphys[] = {
 	  MII_STR_xxBROADCOM_BCM5214 },
 	{ MII_OUI_xxBROADCOM,		MII_MODEL_xxBROADCOM_BCM5221,
 	  MII_STR_xxBROADCOM_BCM5221 },
+	{ MII_OUI_xxBROADCOM,		MII_MODEL_xxBROADCOM_BCM4401,
+	  MII_STR_xxBROADCOM_BCM4401 },
 
 	{ 0,				0,
 	  NULL },
