0e15493f5c4d90cd1b42d3ec95eecfe9a007eb05
jmc <jmc@NetBSD.org>
2004-11-24 22:01:21 +0000

    Retool sgivol into a proper hostool that doesn't depend on disklabel.h. Assume defaults for file images that match what a vnd would have used. Native use will still use the standard NetBSD label to get this data. Fixes PR#25996

--- sys/arch/sgimips/stand/sgivol/sgivol.c
+++ sys/arch/sgimips/stand/sgivol/sgivol.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: sgivol.c,v 1.8 2003/11/08 04:59:00 sekiya Exp $	*/
+/*	$NetBSD: sgivol.c,v 1.9 2004/11/24 22:01:21 jmc Exp $	*/
 
 /*-
  * Copyright (c) 2001 The NetBSD Foundation, Inc.
@@ -36,10 +36,22 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
+#if HAVE_NBTOOL_CONFIG_H
+#include "nbtool_config.h"
+#endif
+
 #include <sys/types.h>
 #include <sys/ioctl.h>
 #include <sys/stat.h>
+
+#if HAVE_NBTOOL_CONFIG_H
+#include "../../../../../sys/sys/bootblock.h"
+/* Ficticious geometry for cross tool usage against a file image */
+#define SGIVOL_NBTOOL_NSECS	32
+#define SGIVOL_NBTOOL_NTRACKS	64
+#else
 #include <sys/disklabel.h>
+#endif
 
 #include <errno.h>
 #include <stdio.h>
@@ -50,74 +62,6 @@
 #include <util.h>
 #include <sys/endian.h>
 
-/*
- * Some IRIX man pages refer to the size being a multiple of whole cylinders.
- * Later ones only refer to the size being "typically" 2MB.  IRIX fx(1)
- * uses a default drive geometry if one can't be determined, suggesting
- * that "whole cylinder" multiples are not required.
- */
-
-#define SGI_SIZE_VOLHDR	3135	/* Can be overridden via -h parameter */
-
-struct local_devparms {
-	u_int8_t        dp_skew;
-	u_int8_t        dp_gap1;
-	u_int8_t        dp_gap2;
-	u_int8_t        dp_spares_cyl;
-	u_int16_t       dp_cyls;
-	u_int16_t       dp_shd0;
-	u_int16_t       dp_trks0;
-	u_int8_t        dp_ctq_depth;
-	u_int8_t        dp_cylshi;
-	u_int16_t       dp_unused;
-	u_int16_t       dp_secs;
-	u_int16_t       dp_secbytes;
-	u_int16_t       dp_interleave;
-	u_int32_t       dp_flags;
-	u_int32_t       dp_datarate;
-	u_int32_t       dp_nretries;
-	u_int32_t       dp_mspw;
-	u_int16_t       dp_xgap1;
-	u_int16_t       dp_xsync;
-	u_int16_t       dp_xrdly;
-	u_int16_t       dp_xgap2;
-	u_int16_t       dp_xrgate;
-	u_int16_t       dp_xwcont;
-}               __attribute__((__packed__));
-
-struct local_sgilabel {
-#define SGILABEL_MAGIC  0xbe5a941
-	u_int32_t       magic;
-	int16_t         root;
-	int16_t         swap;
-	char            bootfile[16];
-	struct local_devparms dp;
-	struct {
-		char            name[8];
-		int32_t         block;
-		int32_t         bytes;
-	}               voldir[15];
-	struct {
-		int32_t         blocks;
-		int32_t         first;
-		int32_t         type;
-	}               partitions[MAXPARTITIONS];
-	int32_t         checksum;
-	int32_t         _pad;
-}               __attribute__((__packed__));
-
-#define SGI_PTYPE_VOLHDR        0
-#define SGI_PTYPE_RAW           3
-#define SGI_PTYPE_BSD           4
-#define SGI_PTYPE_VOLUME        6
-#define SGI_PTYPE_EFS           7
-#define SGI_PTYPE_LVOL          8
-#define SGI_PTYPE_RLVOL         9
-#define SGI_PTYPE_XFS           10
-#define SGI_PTYPE_XFSLOG        11
-#define SGI_PTYPE_XLV           12
-#define SGI_PTYPE_XVM           13
-
 int	fd;
 int	opt_i;			/* Initialize volume header */
 int	opt_r;			/* Read a file from volume header */
@@ -127,14 +71,18 @@ int	opt_p;			/* Modify a partition */
 int	opt_q;			/* quiet mode */
 int	opt_f;			/* Don't ask, just do what you're told */
 int	partno, partfirst, partblocks, parttype;
-struct local_sgilabel *volhdr;
+struct sgi_boot_block *volhdr;
 int32_t	checksum;
-u_int32_t	volhdr_size = SGI_SIZE_VOLHDR;
+u_int32_t	volhdr_size = SGI_BOOT_BLOCK_SIZE_VOLHDR;
 
 const char *vfilename = "";
 const char *ufilename = "";
 
+#if HAVE_NBTOOL_CONFIG_H
+struct stat st;
+#else
 struct disklabel lbl;
+#endif
 
 unsigned char buf[512];
 
@@ -249,6 +197,10 @@ main(int argc, char *argv[])
 	
 	fd = open(argv[0], (opt_i | opt_w | opt_d | opt_p) ? O_RDWR : O_RDONLY);
 	if (fd < 0) {
+#if HAVE_NBTOOL_CONFIG_H
+		perror("File open");
+		exit(1);
+#else
 		sprintf(buf, "/dev/r%s%c", argv[0], 'a' + getrawpartition());
 		fd = open(buf, (opt_i | opt_w | opt_d | opt_p) 
 				? O_RDWR : O_RDONLY);
@@ -257,21 +209,43 @@ main(int argc, char *argv[])
 				argv[0], strerror(errno));
 			exit(1);
 		}
+#endif
 	}
 	if (read(fd, buf, sizeof(buf)) != sizeof(buf)) {
 		perror("read volhdr");
 		exit(1);
 	}
-	if (ioctl(fd, DIOCGDINFO, &lbl) < 0) {
+#if HAVE_NBTOOL_CONFIG_H
+	if (fstat(fd, &st) < 0) {
+		perror("stat error");
+		exit(1);
+	}
+	if (!S_ISREG(st.st_mode)) {
+		printf("Must be regular file\n");
+		exit(1);
+	}
+	if (st.st_size % SGI_BOOT_BLOCK_BLOCKSIZE) {
+		printf("Size must be multiple of %d\n", 
+		    SGI_BOOT_BLOCK_BLOCKSIZE);
+		exit(1);
+	}
+	if (st.st_size < (SGIVOL_NBTOOL_NSECS * SGIVOL_NBTOOL_NTRACKS)) {
+		printf("Minimum size of %d required\n",
+		    SGIVOL_NBTOOL_NSECS * SGIVOL_NBTOOL_NTRACKS);
+		exit(1);
+	}
+#else
+	if (ioctl(fd, DIOCGDINFO, &lbl) < 1) {
 		perror("DIOCGDINFO");
 		exit(1);
 	}
-	volhdr = (struct local_sgilabel *) buf;
+#endif
+	volhdr = (struct sgi_boot_block *) buf;
 	if (opt_i) {
 		init_volhdr();
 		exit(0);
 	}
-	if (be32toh(volhdr->magic) != SGILABEL_MAGIC) {
+	if (be32toh(volhdr->magic) != SGI_BOOT_BLOCK_MAGIC) {
 		printf("No Volume Header found, magic=%x.  Use -i first.\n", 
 		       be32toh(volhdr->magic));
 		exit(1);
@@ -305,7 +279,12 @@ display_vol(void)
 	int32_t *l;
 	int i;
 
+#if HAVE_NBTOOL_CONFIG_H
+	printf("disklabel shows %d sectors\n", 
+	    st.st_size / SGI_BOOT_BLOCK_BLOCKSIZE);
+#else
 	printf("disklabel shows %d sectors\n", lbl.d_secperunit);
+#endif
 	l = (int32_t *)buf;
 	checksum = 0;
 	for (i = 0; i < 512 / 4; ++i)
@@ -318,11 +297,14 @@ display_vol(void)
 	printf("\nVolume header files:\n");
 	for (i = 0; i < 15; ++i)
 		if (volhdr->voldir[i].name[0])
-			printf("%-8s offset %4d blocks, length %8d bytes (%d blocks)\n",
-			       volhdr->voldir[i].name, be32toh(volhdr->voldir[i].block),
-			       be32toh(volhdr->voldir[i].bytes), (be32toh(volhdr->voldir[i].bytes) + 511) / 512);
+			printf("%-8s offset %4d blocks, length %8d bytes "
+			    "(%d blocks)\n",
+			    volhdr->voldir[i].name, 
+                            be32toh(volhdr->voldir[i].block),
+			    be32toh(volhdr->voldir[i].bytes), 
+                            (be32toh(volhdr->voldir[i].bytes) + 511) / 512);
 	printf("\nSGI partitions:\n");
-	for (i = 0; i < MAXPARTITIONS; ++i) {
+	for (i = 0; i < SGI_BOOT_BLOCK_MAXPARTITIONS; ++i) {
 		if (be32toh(volhdr->partitions[i].blocks)) {
 			printf("%2d:%c blocks %8d first %8d type %2d (%s)\n",
 			  i, i + 'a', be32toh(volhdr->partitions[i].blocks),
@@ -338,27 +320,53 @@ void
 init_volhdr(void)
 {
 	memset(buf, 0, sizeof(buf));
-	volhdr->magic = htobe32(SGILABEL_MAGIC);
+	volhdr->magic = htobe32(SGI_BOOT_BLOCK_MAGIC);
 	volhdr->root = htobe16(0);
 	volhdr->swap = htobe16(1);
 	strcpy(volhdr->bootfile, "/netbsd");
+#if HAVE_NBTOOL_CONFIG_H
+	volhdr->dp.dp_skew = 0;
+#else
 	volhdr->dp.dp_skew = lbl.d_trackskew;
+#endif
 	volhdr->dp.dp_gap1 = 1; /* XXX */
 	volhdr->dp.dp_gap2 = 1; /* XXX */
+#if HAVE_NBTOOL_CONFIG_H        
+	volhdr->dp.dp_cyls = 
+	    htobe16(st.st_size / (SGIVOL_NBTOOL_NSECS * SGIVOL_NBTOOL_NTRACKS));
+#else
 	volhdr->dp.dp_cyls = htobe16(lbl.d_ncylinders);
+#endif
 	volhdr->dp.dp_shd0 = 0;
+#if HAVE_NBTOOL_CONFIG_H
+	volhdr->dp.dp_trks0 = htobe16(SGIVOL_NBTOOL_NTRACKS);
+	volhdr->dp.dp_secs = htobe16(SGIVOL_NBTOOL_NSECS);
+	volhdr->dp.dp_secbytes = htobe16(SGI_BOOT_BLOCK_BLOCKSIZE);
+	volhdr->dp.dp_interleave = htobe16(1);
+#else
 	volhdr->dp.dp_trks0 = htobe16(lbl.d_ntracks);
 	volhdr->dp.dp_secs = htobe16(lbl.d_nsectors);
 	volhdr->dp.dp_secbytes = htobe16(lbl.d_secsize);
 	volhdr->dp.dp_interleave = htobe16(lbl.d_interleave);
+#endif
 	volhdr->dp.dp_nretries = htobe32(22);
+#if HAVE_NBTOOL_CONFIG_H
+	volhdr->partitions[10].blocks = 
+	    htobe32(st.st_size / SGI_BOOT_BLOCK_BLOCKSIZE);
+#else
 	volhdr->partitions[10].blocks = htobe32(lbl.d_secperunit);
+#endif
 	volhdr->partitions[10].first = 0;
 	volhdr->partitions[10].type = htobe32(SGI_PTYPE_VOLUME);
 	volhdr->partitions[8].blocks = htobe32(volhdr_size);
 	volhdr->partitions[8].first = 0;
 	volhdr->partitions[8].type = htobe32(SGI_PTYPE_VOLHDR);
+#if HAVE_NBTOOL_CONFIG_H
+	volhdr->partitions[0].blocks = 
+	    htobe32((st.st_size / SGI_BOOT_BLOCK_BLOCKSIZE) - volhdr_size);
+#else
 	volhdr->partitions[0].blocks = htobe32(lbl.d_secperunit - volhdr_size);
+#endif
 	volhdr->partitions[0].first = htobe32(volhdr_size);
 	volhdr->partitions[0].type = htobe32(SGI_PTYPE_BSD);
 	write_volhdr();
@@ -581,7 +589,11 @@ allocate_space(int size)
 		}
 		++n;
 	}
+#if HAVE_NBTOOL_CONFIG_H
+	if (first + blocks > (st.st_size / SGI_BOOT_BLOCK_BLOCKSIZE))
+#else
 	if (first + blocks > lbl.d_secperunit)
+#endif
 		first = -1;
 	/* XXX assumes volume header is partition 8 */
 	/* XXX assumes volume header starts at 0? */
