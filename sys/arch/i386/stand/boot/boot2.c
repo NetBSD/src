0e803a71b24e0daec0fe47ae45f7a647e5249c03
christos <christos@NetBSD.org>
2004-11-25 08:30:52 +0000

    include sys/types.h to make this compile again.

--- sys/arch/i386/stand/boot/boot2.c
+++ sys/arch/i386/stand/boot/boot2.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: boot2.c,v 1.4 2003/10/08 04:25:45 lukem Exp $	*/
+/*	$NetBSD: boot2.c,v 1.5 2004/11/25 08:30:52 christos Exp $	*/
 
 /*
  * Copyright (c) 2003
@@ -41,6 +41,7 @@
 
 /* Based on stand/biosboot/main.c */
 
+#include <sys/types.h>
 #include <sys/reboot.h>
 #include <sys/bootblock.h>
 
