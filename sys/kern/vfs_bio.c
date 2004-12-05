f4ba3402ffc0d619a2886406f17dfd19b4bba339
jrf <jrf>
2004-12-05 06:12:54 +0000

    Fix previous commit, got bufcache and bufmem messages reversed.

--- sys/kern/vfs_bio.c
+++ sys/kern/vfs_bio.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: vfs_bio.c,v 1.138 2004/12/05 06:00:20 jrf Exp $	*/
+/*	$NetBSD: vfs_bio.c,v 1.139 2004/12/05 06:12:54 jrf Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -81,7 +81,7 @@
 #include "opt_softdep.h"
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: vfs_bio.c,v 1.138 2004/12/05 06:00:20 jrf Exp $");
+__KERNEL_RCSID(0, "$NetBSD: vfs_bio.c,v 1.139 2004/12/05 06:12:54 jrf Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -1664,14 +1664,14 @@ SYSCTL_SETUP(sysctl_vm_buf_setup, "sysctl vm.buf* subtree setup")
 	sysctl_createv(clog, 0, NULL, NULL,
 		       CTLFLAG_PERMANENT|CTLFLAG_READWRITE,
 		       CTLTYPE_INT, "bufcache",
-		       SYSCTL_DESCR("Percentage of kernel memory to use for "
+		       SYSCTL_DESCR("Percentage of physical memory to use for "
 				    "buffer cache"),
 		       sysctl_bufvm_update, 0, &bufcache, 0,
 		       CTL_VM, CTL_CREATE, CTL_EOL);
 	sysctl_createv(clog, 0, NULL, NULL,
 		       CTLFLAG_PERMANENT|CTLFLAG_READONLY,
 		       CTLTYPE_INT, "bufmem",
-		       SYSCTL_DESCR("Amount of physical memory used by buffer "
+		       SYSCTL_DESCR("Amount of kernel memory used by buffer "
 				    "cache"),
 		       NULL, 0, &bufmem, 0,
 		       CTL_VM, CTL_CREATE, CTL_EOL);
