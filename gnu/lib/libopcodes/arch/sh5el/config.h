b60c6a1e5703b48dc06af332092586acef9c81ac
mrg <mrg@NetBSD.org>
2004-12-09 06:48:34 +0000

    run mknative-binutils for 2.15 for m68000, sh3eb, sh3el and sh5el.
    XXX: these are not run-time tested but do build fine.

--- gnu/lib/libopcodes/arch/sh5el/config.h
+++ gnu/lib/libopcodes/arch/sh5el/config.h
@@ -29,9 +29,6 @@
 /* Define to `long' if <sys/types.h> doesn't define.  */
 /* #undef off_t */
 
-/* Define if you need to in order for stat and other things to work.  */
-/* #undef _POSIX_SOURCE */
-
 /* Define to `unsigned' if <sys/types.h> doesn't define.  */
 /* #undef size_t */
 
@@ -113,12 +110,6 @@
 /* Define if you have the <sys/param.h> header file.  */
 #define HAVE_SYS_PARAM_H 1
 
-/* Define if you have the <sys/stat.h> header file.  */
-/* #undef HAVE_SYS_STAT_H */
-
-/* Define if you have the <sys/types.h> header file.  */
-/* #undef HAVE_SYS_TYPES_H */
-
 /* Define if you have the <unistd.h> header file.  */
 #define HAVE_UNISTD_H 1
 
@@ -129,7 +120,7 @@
 #define PACKAGE "opcodes"
 
 /* Version number of package */
-#define VERSION "2.14"
+#define VERSION "2.15"
 
 /* Define if you have the stpcpy function */
 /* #undef HAVE_STPCPY */
