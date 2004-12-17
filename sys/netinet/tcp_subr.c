7ac63a03f04fc293f5b91040ed77956451a4d667
christos <christos@NetBSD.org>
2004-12-17 22:54:52 +0000

    Turn checksumming on loopback back on until we fix the bugs in it. Connect over tcp on the loopback is broken:
      4729 amq      0.000007 CALL  connect(4,0x804f2a0,0x1c)
      4729 amq      75.007420 RET   connect -1 errno 60 Connection timed out

--- sys/netinet/tcp_subr.c
+++ sys/netinet/tcp_subr.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: tcp_subr.c,v 1.174 2004/12/15 04:25:20 thorpej Exp $	*/
+/*	$NetBSD: tcp_subr.c,v 1.175 2004/12/17 22:54:52 christos Exp $	*/
 
 /*
  * Copyright (C) 1995, 1996, 1997, and 1998 WIDE Project.
@@ -98,7 +98,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: tcp_subr.c,v 1.174 2004/12/15 04:25:20 thorpej Exp $");
+__KERNEL_RCSID(0, "$NetBSD: tcp_subr.c,v 1.175 2004/12/17 22:54:52 christos Exp $");
 
 #include "opt_inet.h"
 #include "opt_ipsec.h"
@@ -200,7 +200,7 @@ int	tcp_compat_42 = 0;
 #endif
 int	tcp_rst_ppslim = 100;	/* 100pps */
 int	tcp_ackdrop_ppslim = 100;	/* 100pps */
-int	tcp_do_loopback_cksum = 0;
+int	tcp_do_loopback_cksum = 1;
 
 /* tcb hash */
 #ifndef TCBHASHSIZE
