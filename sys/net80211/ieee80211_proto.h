ee4dcf38df3cd50df4d0c7c0ca94a1656b4e2aa9
dyoung <dyoung@NetBSD.org>
2004-12-19 08:08:06 +0000

    Define for more bits in the Service field of the 802.11 PLCP Header.
    For use by the subroutine ieee80211_compute_duration, add struct
    ieee80211_duration, and #define a number of microsecond constants
    used for the transmit timing of 802.11 packets.
    
    Add the subroutine ieee80211_compute_duration, which computes for
    any packet the appropriate 802.11 Duration field, the PLCP Length
    field, as well as the Duration and Length fields for an RTS frame.
    
    atw(4), rtw(4), future drivers, and possibly ath(4) will share
    ieee80211_compute_duration.

--- sys/net80211/ieee80211_proto.h
+++ sys/net80211/ieee80211_proto.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: ieee80211_proto.h,v 1.5 2004/07/28 08:12:49 dyoung Exp $	*/
+/*	$NetBSD: ieee80211_proto.h,v 1.6 2004/12/19 08:08:06 dyoung Exp $	*/
 /*-
  * Copyright (c) 2001 Atsushi Onoe
  * Copyright (c) 2002, 2003 Sam Leffler, Errno Consulting
@@ -78,6 +78,9 @@ extern	void ieee80211_print_essid(u_int8_t *, int);
 extern	void ieee80211_dump_pkt(u_int8_t *, int, int, int);
 extern	int ieee80211_ibss_merge(struct ieee80211com *,
 		struct ieee80211_node *, u_int64_t);
+extern	int ieee80211_compute_duration(struct ieee80211_frame *, int,
+		uint32_t, int, int, struct ieee80211_duration *,
+		struct ieee80211_duration *, int *);
 
 extern	const char *ieee80211_state_name[IEEE80211_S_MAX];
 #endif /* _NET80211_IEEE80211_PROTO_H_ */
