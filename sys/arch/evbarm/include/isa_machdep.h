e8d2af9dd07b66f6f480a98fd5faf4448286d532
joff <joff@NetBSD.org>
2004-12-23 04:36:18 +0000

    Need these files present for ISA support

---
+++ sys/arch/evbarm/include/isa_machdep.h
@@ -0,0 +1,7 @@
+/* $NetBSD: isa_machdep.h,v 1.1 2004/12/23 04:36:18 joff Exp $ */
+
+#ifndef _EVBARM_ISA_MACHDEP_H_
+#define _EVBARM_ISA_MACHDEP_H_
+#include <arm/isa_machdep.h>
+
+#endif /* _EVBARM_ISA_MACHDEP_H_ */
