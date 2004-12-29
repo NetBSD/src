abb0bb99634f1419b14f6f3bc9088a3e733ab7bf
reinoud <reinoud@NetBSD.org>
2004-12-29 17:57:19 +0000

    Remove conditional around label. Its allways used in the code and thus not explicitly only for the protocols indicated by the #if
    Allthough its unlikely a kernel will be build without NET_INET, it will
    fail compilation here when NET_INET is not defined.

--- sys/net/if_tokensubr.c
+++ sys/net/if_tokensubr.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: if_tokensubr.c,v 1.27 2004/12/06 02:59:23 christos Exp $	*/
+/*	$NetBSD: if_tokensubr.c,v 1.28 2004/12/29 17:57:19 reinoud Exp $	*/
 
 /*
  * Copyright (c) 1982, 1989, 1993
@@ -99,7 +99,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: if_tokensubr.c,v 1.27 2004/12/06 02:59:23 christos Exp $");
+__KERNEL_RCSID(0, "$NetBSD: if_tokensubr.c,v 1.28 2004/12/29 17:57:19 reinoud Exp $");
 
 #include "opt_inet.h"
 #include "opt_atalk.h"
@@ -732,9 +732,7 @@ token_input(ifp, m)
 	default:
 		/* printf("token_input: unknown dsap 0x%x\n", l->llc_dsap); */
 		ifp->if_noproto++;
-#if defined(INET) || defined(NS) || defined(DECNET)
 	dropanyway:
-#endif
 		m_freem(m);
 		return;
 	}
