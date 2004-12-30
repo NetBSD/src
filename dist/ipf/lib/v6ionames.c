a23662ca5c9ef34b2bcf5a4e90a94b1293fa630e
darrenr <darrenr@NetBSD.org>
2004-12-30 12:07:07 +0000

    undo this last change, it did match fil.c - bad me.

--- dist/ipf/lib/v6ionames.c
+++ dist/ipf/lib/v6ionames.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: v6ionames.c,v 1.2 2004/12/30 08:29:09 darrenr Exp $	*/
+/*	$NetBSD: v6ionames.c,v 1.3 2004/12/30 12:07:07 darrenr Exp $	*/
 
 /*
  * Copyright (C) 1993-2001 by Darren Reed.
@@ -14,13 +14,13 @@
 
 struct	ipopt_names	v6ionames[] ={
 	{ IPPROTO_HOPOPTS,	0x000001,	0,	"hopopts" },
-	{ IPPROTO_IPV6,		0x000002,	0,	"ipv6" },
-	{ IPPROTO_ROUTING,	0x000004,	0,	"routing" },
-	{ IPPROTO_FRAGMENT,	0x000008,	0,	"frag" },	
-	{ IPPROTO_ESP,		0x000010,	0,	"esp" },
-	{ IPPROTO_AH,		0x000020,	0,	"ah" },
-	{ IPPROTO_NONE,		0x000040,	0,	"none" },	
-	{ IPPROTO_DSTOPTS,	0x000080,	0,	"dstopts" },
+	{ IPPROTO_DSTOPTS,	0x000002,	0,	"dstopts" },
+	{ IPPROTO_ESP,		0x000004,	0,	"esp" },
+	{ IPPROTO_AH,		0x000008,	0,	"ah" },
+	{ IPPROTO_ROUTING,	0x000010,	0,	"routing" },
+	{ IPPROTO_IPV6,		0x000020,	0,	"ipv6" },
+	{ IPPROTO_FRAGMENT,	0x000040,	0,	"frag" },	
+	{ IPPROTO_NONE,		0x000080,	0,	"none" },	
 	{ 0, 			0,		0,	(char *)NULL }
 };
 
