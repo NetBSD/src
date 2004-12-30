712921291505c1d33ae0ebd1b43b0fd4225154d4
rumble <rumble@NetBSD.org>
2004-12-30 02:41:03 +0000

    Replace the hpc0 base address with the definition from hpc/hpcregs.h.

--- sys/arch/sgimips/sgimips/console.c
+++ sys/arch/sgimips/sgimips/console.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: console.c,v 1.28 2004/07/08 10:00:30 sekiya Exp $	*/
+/*	$NetBSD: console.c,v 1.29 2004/12/30 02:41:03 rumble Exp $	*/
 
 /*
  * Copyright (c) 1994, 1995, 1996 Carnegie-Mellon University.
@@ -28,7 +28,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: console.c,v 1.28 2004/07/08 10:00:30 sekiya Exp $");
+__KERNEL_RCSID(0, "$NetBSD: console.c,v 1.29 2004/12/30 02:41:03 rumble Exp $");
 
 #include "opt_kgdb.h"
 
@@ -144,8 +144,9 @@ gio_video_init(char *consdev)
 		case MACH_SGI_IP22:
 #if (NPCKBC > 0)
 			/* XXX Hardcoded iotag, HPC address XXX */
-			pckbc_cnattach(1, 0x1fb80000 + HPC_PBUS_CH6_DEVREGS
-				+ IOC_KB_REGS, KBCMDP, PCKBC_KBD_SLOT);
+			pckbc_cnattach(1, HPC_BASE_ADDRESS_0 +
+			    HPC_PBUS_CH6_DEVREGS + IOC_KB_REGS, KBCMDP,
+			    PCKBC_KBD_SLOT);
 #endif
 			break;
 		}
