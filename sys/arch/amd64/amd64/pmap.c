5fcf4fd31cc59bf1ad8925598917f01907a601b7
yamt <yamt@NetBSD.org>
2005-01-01 21:00:06 +0000

    for in-kernel maps, - allocate kva for vm_map_entry from the map itsself and   remove the static limit, MAX_KMAPENT. - keep merged entries for later splitting to fix allocate-to-free problem.   PR/24039.

--- sys/arch/amd64/amd64/pmap.c
+++ sys/arch/amd64/amd64/pmap.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: pmap.c,v 1.14 2004/08/10 18:57:09 drochner Exp $	*/
+/*	$NetBSD: pmap.c,v 1.15 2005/01/01 21:00:06 yamt Exp $	*/
 
 /*
  *
@@ -108,7 +108,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: pmap.c,v 1.14 2004/08/10 18:57:09 drochner Exp $");
+__KERNEL_RCSID(0, "$NetBSD: pmap.c,v 1.15 2005/01/01 21:00:06 yamt Exp $");
 
 #ifndef __x86_64__
 #include "opt_cputype.h"
@@ -1652,7 +1652,7 @@ pmap_free_pvpage()
 		/* unmap the page */
 		dead_entries = NULL;
 		uvm_unmap_remove(map, (vaddr_t)pvp, ((vaddr_t)pvp) + PAGE_SIZE,
-		    &dead_entries);
+		    &dead_entries, NULL);
 		vm_map_unlock(map);
 
 		if (dead_entries != NULL)
