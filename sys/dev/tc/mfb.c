b603617fbcab0495c82038fb1e704b60c988f489
mhitch <mhitch@NetBSD.org>
2005-01-02 20:41:20 +0000

    Fix and simplify incorrect check for screen blanking in WSDISPLAYIO_SVIDEO.

--- sys/dev/tc/mfb.c
+++ sys/dev/tc/mfb.c
@@ -1,4 +1,4 @@
-/* $NetBSD: mfb.c,v 1.41 2003/12/20 07:10:00 nisimura Exp $ */
+/* $NetBSD: mfb.c,v 1.42 2005/01/02 20:41:20 mhitch Exp $ */
 
 /*
  * Copyright (c) 1998, 1999 Tohru Nishimura.  All rights reserved.
@@ -31,7 +31,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: mfb.c,v 1.41 2003/12/20 07:10:00 nisimura Exp $");
+__KERNEL_RCSID(0, "$NetBSD: mfb.c,v 1.42 2005/01/02 20:41:20 mhitch Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -359,7 +359,7 @@ mfbioctl(v, cmd, data, flag, p)
 
 	case WSDISPLAYIO_SVIDEO:
 		turnoff = *(int *)data == WSDISPLAYIO_VIDEO_OFF;
-		if ((sc->sc_blanked == 0) ^ turnoff) {
+		if (sc->sc_blanked != turnoff) {
 			sc->sc_blanked = turnoff;
 #if 0	/* XXX later XXX */
 	To turn off,
