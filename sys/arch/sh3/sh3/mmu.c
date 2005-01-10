de0b61695871be096203400f5d0b6bded7bbd5f1
jmc <jmc@NetBSD.org>
2005-01-10 18:42:22 +0000

    Pullup rev 1.10 (requested by tsutsui in ticket #1072)
    Protect accesses to PTE/TLB registers with
    _cpu_exception_suspend()/_cpu_exception_resume() pair. Prevents
    spontaneous reboot under heavy load

--- sys/arch/sh3/sh3/mmu.c
+++ sys/arch/sh3/sh3/mmu.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: mmu.c,v 1.9 2003/07/15 03:35:57 lukem Exp $	*/
+/*	$NetBSD: mmu.c,v 1.9.6.1 2005/01/10 18:42:22 jmc Exp $	*/
 
 /*-
  * Copyright (c) 2002 The NetBSD Foundation, Inc.
@@ -37,7 +37,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: mmu.c,v 1.9 2003/07/15 03:35:57 lukem Exp $");
+__KERNEL_RCSID(0, "$NetBSD: mmu.c,v 1.9.6.1 2005/01/10 18:42:22 jmc Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -112,6 +112,9 @@ sh_mmu_information()
 void
 sh_tlb_set_asid(int asid)
 {
+	int s;
 
+	s = _cpu_exception_suspend();
 	_reg_write_4(SH_(PTEH), asid);
+	_cpu_exception_resume(s);
 }
