c726b33d627b65863d17da515af8b7cf016ca234
christos <christos@NetBSD.org>
2005-01-11 22:40:00 +0000

    - remove local UNCONST - add new __gensalt functions

--- lib/libcrypt/crypt.h
+++ lib/libcrypt/crypt.h
@@ -1,5 +1,5 @@
 /*
- * $NetBSD: crypt.h,v 1.1 2004/07/02 00:05:23 sjg Exp $
+ * $NetBSD: crypt.h,v 1.2 2005/01/11 22:40:00 christos Exp $
  */
 char	*__md5crypt(const char *pw, const char *salt);	/* XXX */
 char *__bcrypt(const char *, const char *);	/* XXX */
@@ -8,16 +8,11 @@ unsigned int __crypt_sha1_iterations (unsigned int hint);
 void __hmac_sha1(unsigned char *, size_t, unsigned char *, size_t, unsigned char *);
 void __crypt_to64(char *s, u_int32_t v, int n);
 
+int __gensalt_blowfish(char *salt, size_t saltlen, size_t nrounds);
+int __gensalt_old(char *salt, size_t saltsiz, size_t nrounds);
+int __gensalt_new(char *salt, size_t saltsiz, size_t nrounds);
+int __gensalt_md5(char *salt, size_t saltsiz, size_t nrounds);
+int __gensalt_sha1(char *salt, size_t saltsiz, size_t nrounds);
+
 #define SHA1_MAGIC "$sha1$"
 #define SHA1_SIZE 20
-
-#ifdef __GNUC__
-#define UNCONST(ptr)	({ 		\
-    union __unconst {			\
-	const void *__cp;		\
-	void *__p;			\
-    } __d;				\
-    __d.__cp = ptr, __d.__p; })
-#else
-#define UNCONST(ptr)	(void *)(ptr)
-#endif
