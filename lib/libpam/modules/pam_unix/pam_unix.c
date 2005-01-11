63c47d3d2a8b9ed967fa9b92c4508ae5c36b5ad7
christos <christos@NetBSD.org>
2005-01-11 23:23:33 +0000

    eliminate the third copy of pwd_gensalt.

--- lib/libpam/modules/pam_unix/pam_unix.c
+++ lib/libpam/modules/pam_unix/pam_unix.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: pam_unix.c,v 1.2 2004/12/12 08:17:56 christos Exp $	*/
+/*	$NetBSD: pam_unix.c,v 1.3 2005/01/11 23:23:33 christos Exp $	*/
 
 /*-
  * Copyright 1998 Juniper Networks, Inc.
@@ -40,7 +40,7 @@
 #ifdef __FreeBSD__
 __FBSDID("$FreeBSD: src/lib/libpam/modules/pam_unix/pam_unix.c,v 1.49 2004/02/10 10:13:21 des Exp $");
 #else
-__RCSID("$NetBSD: pam_unix.c,v 1.2 2004/12/12 08:17:56 christos Exp $");
+__RCSID("$NetBSD: pam_unix.c,v 1.3 2005/01/11 23:23:33 christos Exp $");
 #endif
 
 
@@ -210,8 +210,6 @@ pam_sm_acct_mgmt(pam_handle_t *pamh, int flags __unused,
 
 	return (PAM_SUCCESS);
 }
-
-static int pwd_gensalt(char *, int, struct passwd *, char);
 
 #ifdef YP
 /*
@@ -575,11 +573,11 @@ pam_sm_chauthtok(pam_handle_t *pamh, int flags,
 			break;
 		}
 
-		if (pwd_gensalt(salt, _PASSWORD_LEN, pwd,
+		if (pw_gensalt(salt, _PASSWORD_LEN, pwd,
 #ifdef YP
 		    strcmp(passwd_db, "nis") == 0 ? 'y' :
 #endif
-		    'l') == 0) {
+		    'l') == -1) {
 			pam_error(pamh, "Couldn't generate salt.");
 			return (PAM_SERVICE_ERR);
 		}
@@ -603,125 +601,3 @@ pam_sm_chauthtok(pam_handle_t *pamh, int flags,
 }
 
 PAM_MODULE_ENTRY("pam_unix");
-
-/*
- * Copyright 1997 Niels Provos <provos@physnet.uni-hamburg.de>
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *      This product includes software developed by Niels Provos.
- * 4. The name of the author may not be used to endorse or promote products
- *    derived from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
- * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
- * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
- * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
- * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
- * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
-static const unsigned char itoa64[] =	 /* 0 ... 63 => ascii - 64 */
-	"./0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-
-static void
-to64(char *s, uint32_t v, int n)
-{
-
-	
-	while (--n >= 0) {
-		*s++ = itoa64[v & 0x3f];
-		v >>= 6;
-	}
-}
-
-static int
-pwd_gensalt(char *salt, int max, struct passwd *pwd, char type)
-{
-	char option[LINE_MAX], *next, *now, grpkey[LINE_MAX];
-	const char *cipher;
-	int rounds;
-	struct group *grp;
-
-	*salt = '\0';
-
-	switch (type) {
-	case 'y':
-	        cipher = "ypcipher";
-		break;
-	case 'l':
-	default:
-	        cipher = "localcipher";
-		break;
-	}
-
-	pw_getconf(option, sizeof(option), pwd->pw_name, cipher);
-
-	/* Try to find an entry for the group */
-	if (*option == 0) {
-		if ((grp = getgrgid(pwd->pw_gid)) != NULL) {
-			snprintf(grpkey, sizeof(grpkey), ":%s", grp->gr_name);
-			pw_getconf(option, sizeof(option), grpkey, cipher);
-		}
-		if (*option == 0)
-		        pw_getconf(option, sizeof(option), "default", cipher);
-	}
-
-	next = option;
-	now = strsep(&next, ",");
-	if (strcmp(now, "old") == 0) {
-		if (max < 3)
-			return (0);
-		to64(&salt[0], arc4random(), 2);
-		salt[2] = '\0';
-	} else if (strcmp(now, "newsalt") == 0) {
-		rounds = atol(next);
-		if (max < 10)
-			return (0);
-		/* Check rounds, 24 bit is max */
-		if (rounds < 7250)
-			rounds = 7250;
-		else if (rounds > 0xffffff)
-		        rounds = 0xffffff;
-		salt[0] = _PASSWORD_EFMT1;
-		to64(&salt[1], (u_int32_t)rounds, 4);
-		to64(&salt[5], arc4random(), 4);
-		salt[9] = '\0';
-	} else if (strcmp(now, "md5") == 0) {
-		if (max < 13)  /* $1$8salt$\0 */
-			return (0);
-		salt[0] = _PASSWORD_NONDES;
-		salt[1] = '1';
-		salt[2] = '$';
-		to64(&salt[3], arc4random(), 4);
-		to64(&salt[7], arc4random(), 4);
-		salt[11] = '$';
-		salt[12] = '\0';
-	} else if (strcmp(now, "blowfish") == 0) {
-		rounds = atoi(next);
-		if (rounds < 4)
-			rounds = 4;
-		strlcpy(salt, bcrypt_gensalt(rounds), (size_t)max);
-	} else {
-		strlcpy(salt, ":", (size_t)max);
-#if 0
-		/* XXX pam_error()? */
-		warnx("Unknown option %s.", now);
-#endif
-	}
-
-	return (1);
-}
