5b957a0d8d26c4e68fc60705c810dc8a5444f575
jmc <jmc@NetBSD.org>
2005-01-11 06:42:22 +0000

    Pullup rev 1.53 (requested by yamt in ticket #1079)
    	Invalidate cache if filesize is changed besides our activity
    	because it means that were out of sync with the server.

--- sys/nfs/nfsnode.h
+++ sys/nfs/nfsnode.h
@@ -1,4 +1,4 @@
-/*	 $NetBSD: nfsnode.h,v 1.46.2.2.2.1 2005/01/11 06:36:35 jmc Exp $	*/
+/*	 $NetBSD: nfsnode.h,v 1.46.2.2.2.2 2005/01/11 06:42:22 jmc Exp $	*/
 
 /*
  * Copyright (c) 1989, 1993
@@ -205,7 +205,8 @@ LIST_HEAD(nfsnodehashhead, nfsnode);
 #define	NACC		0x0100	/* Special file accessed */
 #define	NUPD		0x0200	/* Special file updated */
 #define	NCHG		0x0400	/* Special file times changed */
-#define	NTRUNCDELAYED	0x1000	/* Should be truncated later */
+#define	NTRUNCDELAYED	0x1000	/* Should be truncated later;
+				   implies stale cache */
 #define	NREMOVED	0x2000	/* Has been removed */
 #define	NUSEOPENCRED	0x4000	/* Try open cred first rather than owner's */
 
