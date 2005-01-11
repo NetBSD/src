c1299a1f5ce76ff0a9b2927c4c1828018dcb33dc
skrll <skrll@NetBSD.org>
2005-01-11 18:24:24 +0000

    iwi(4) - Intel PRO/Wireless 2200AG/2915ABG driver. Written by Damien Bergamini, from http://damien.bergamini.free.fr/ipw/
    Ad-hoc mode and roaming are not fully implemented yet.

---
+++ sys/dev/pci/if_iwivar.h
@@ -0,0 +1,144 @@
+/*	$Id: if_iwivar.h,v 1.1.1.1 2005/01/11 18:24:24 skrll Exp $ */
+
+/*-
+ * Copyright (c) 2004, 2005
+ *      Damien Bergamini <damien.bergamini@free.fr>. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice unmodified, this list of conditions, and the following
+ *    disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+struct iwi_firmware {
+	void	*boot;
+	int	boot_size;
+	void	*ucode;
+	int	ucode_size;
+	void	*main;
+	int	main_size;
+};
+
+struct iwi_rx_radiotap_header {
+	struct ieee80211_radiotap_header wr_ihdr;
+	u_int8_t	wr_flags;
+	u_int8_t	wr_rate;
+	u_int16_t	wr_chan_freq;
+	u_int16_t	wr_chan_flags;
+	u_int8_t	wr_antsignal;
+	u_int8_t	wr_antnoise;
+	u_int8_t	wr_antenna;
+};
+
+#define IWI_RX_RADIOTAP_PRESENT						\
+	((1 << IEEE80211_RADIOTAP_FLAGS) |				\
+	 (1 << IEEE80211_RADIOTAP_RATE) |				\
+	 (1 << IEEE80211_RADIOTAP_CHANNEL) |				\
+	 (1 << IEEE80211_RADIOTAP_DB_ANTSIGNAL) |			\
+	 (1 << IEEE80211_RADIOTAP_DB_ANTNOISE) |			\
+	 (1 << IEEE80211_RADIOTAP_ANTENNA))
+
+struct iwi_tx_radiotap_header {
+	struct ieee80211_radiotap_header wt_ihdr;
+	u_int8_t	wt_flags;
+	u_int16_t	wt_chan_freq;
+	u_int16_t	wt_chan_flags;
+};
+
+#define IWI_TX_RADIOTAP_PRESENT						\
+	((1 << IEEE80211_RADIOTAP_FLAGS) |				\
+	 (1 << IEEE80211_RADIOTAP_CHANNEL))
+
+struct iwi_softc {
+	struct device		sc_dev;
+
+	struct ieee80211com	sc_ic;
+	int			(*sc_newstate)(struct ieee80211com *,
+				    enum ieee80211_state, int);
+
+	struct iwi_firmware	fw;
+	u_int32_t		flags;
+#define IWI_FLAG_FW_CACHED	(1 << 0)
+#define IWI_FLAG_FW_INITED	(1 << 1)
+
+	bus_dma_tag_t		sc_dmat;
+
+	struct iwi_tx_desc	*tx_desc;
+	bus_dmamap_t		tx_ring_map;
+	bus_dma_segment_t	tx_ring_seg;
+
+	struct iwi_tx_buf {
+		bus_dmamap_t		map;
+		struct mbuf		*m;
+		struct ieee80211_node	*ni;
+	} tx_buf[IWI_TX_RING_SIZE];
+
+	int			tx_cur;
+	int			tx_old;
+	int			tx_queued;
+
+	struct iwi_cmd_desc	*cmd_desc;
+	bus_dmamap_t		cmd_ring_map;
+	bus_dma_segment_t	cmd_ring_seg;
+	int			cmd_cur;
+
+	struct iwi_rx_buf {
+		bus_dmamap_t	map;
+		struct mbuf	*m;
+	} rx_buf[IWI_RX_RING_SIZE];
+
+	int			rx_cur;
+
+	struct resource		*irq;
+	struct resource		*mem;
+	bus_space_tag_t		sc_st;
+	bus_space_handle_t	sc_sh;
+	void 			*sc_ih;
+	pci_chipset_tag_t	sc_pct;
+	pcitag_t		sc_pcitag;
+	bus_size_t		sc_sz;
+
+	int			authmode;
+
+	int			sc_tx_timer;
+
+#if NBPFILTER > 0
+	struct bpf_if		*sc_drvbpf;
+
+	union {
+		struct iwi_rx_radiotap_header th;
+		u_int8_t	pad[64];
+	} sc_rxtapu;
+#define sc_rxtap	sc_rxtapu.th
+	int			sc_rxtap_len;
+
+	union {
+		struct iwi_tx_radiotap_header th;
+		u_int8_t	pad[64];
+	} sc_txtapu;
+#define sc_txtap	sc_txtapu.th
+	int			sc_txtap_len;
+#endif
+};
+
+#define SIOCSLOADFW	 _IOW('i', 137, struct ifreq)
+#define SIOCSKILLFW	 _IOW('i', 138, struct ifreq)
+#define SIOCGRADIO	_IOWR('i', 139, struct ifreq)
+#define SIOCGTABLE0	_IOWR('i', 140, struct ifreq)
