57d6cb921290b5e94ef6e3d38036af569aca440e
christos <christos>
2005-01-12 03:32:52 +0000

    - change the gensalt functions to take an optional string instead of   the number of rounds. - make pw_gensalt() more generic, so that it does not depend in libutil.

--- lib/libcrypt/crypt.h
+++ lib/libcrypt/crypt.h
@@ -1,5 +1,5 @@
 /*
- * $NetBSD: crypt.h,v 1.2 2005/01/11 22:40:00 christos Exp $
+ * $NetBSD: crypt.h,v 1.3 2005/01/12 03:32:52 christos Exp $
  */
 char	*__md5crypt(const char *pw, const char *salt);	/* XXX */
 char *__bcrypt(const char *, const char *);	/* XXX */
@@ -8,11 +8,11 @@ unsigned int __crypt_sha1_iterations (unsigned int hint);
 void __hmac_sha1(unsigned char *, size_t, unsigned char *, size_t, unsigned char *);
 void __crypt_to64(char *s, u_int32_t v, int n);
 
-int __gensalt_blowfish(char *salt, size_t saltlen, size_t nrounds);
-int __gensalt_old(char *salt, size_t saltsiz, size_t nrounds);
-int __gensalt_new(char *salt, size_t saltsiz, size_t nrounds);
-int __gensalt_md5(char *salt, size_t saltsiz, size_t nrounds);
-int __gensalt_sha1(char *salt, size_t saltsiz, size_t nrounds);
+int __gensalt_blowfish(char *salt, size_t saltlen, const char *option);
+int __gensalt_old(char *salt, size_t saltsiz, const char *option);
+int __gensalt_new(char *salt, size_t saltsiz, const char *option);
+int __gensalt_md5(char *salt, size_t saltsiz, const char *option);
+int __gensalt_sha1(char *salt, size_t saltsiz, const char *option);
 
 #define SHA1_MAGIC "$sha1$"
 #define SHA1_SIZE 20
