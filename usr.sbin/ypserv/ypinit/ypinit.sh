aecdac1d75d10c24a490bfa7c4431f4809e00149
he <he@NetBSD.org>
2005-01-12 21:24:53 +0000

    Pull up evision 1.12 (requested by tron in ticket #893):   Fix a bug which caused "ypinit" to die with a strange error message   if "/var/yp/binding" does not exist.

--- usr.sbin/ypserv/ypinit/ypinit.sh
+++ usr.sbin/ypserv/ypinit/ypinit.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-#	$NetBSD: ypinit.sh,v 1.11 2003/11/12 13:31:08 grant Exp $
+#	$NetBSD: ypinit.sh,v 1.11.4.1 2005/01/12 21:24:53 he Exp $
 #
 # ypinit.sh - setup a master or slave YP server
 #
@@ -129,6 +129,7 @@ echo ""
 
 binding_dir=${yp_dir}/binding
 if [ ! -d ${binding_dir} ]; then
+	cat 1>&2 << __no_dir
 $progname: The directory ${binding_dir} does not exist.
 	Restore it from the distribution.
 __no_dir
