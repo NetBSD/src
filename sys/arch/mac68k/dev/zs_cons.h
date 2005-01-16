002762a2fe67e8f59dbb83ad36d0f82eb09f9fbe
chs <chs@NetBSD.org>
2005-01-16 00:32:59 +0000

    only match one instance (instead of an unlimited number). simplify by not pretending that mac68k can have more than 1 zsc.

--- sys/arch/mac68k/dev/zs_cons.h
+++ sys/arch/mac68k/dev/zs_cons.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: zs_cons.h,v 1.2 2005/01/15 16:00:59 chs Exp $	*/
+/*	$NetBSD: zs_cons.h,v 1.3 2005/01/16 00:32:59 chs Exp $	*/
 
 extern int	zsinited;
 extern void	*zs_conschan;
@@ -7,7 +7,7 @@ void		zs_init(void);
 int		zs_getc(void *);
 void		zs_putc(void *, int);
 
-struct zschan	*zs_get_chan_addr(int, int);
+struct zschan	*zs_get_chan_addr(int);
 
 #ifdef	KGDB
 void zs_kgdb_init(void);
