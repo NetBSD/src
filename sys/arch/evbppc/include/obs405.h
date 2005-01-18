a80b706b2b6d8b80263d7181b5b75b6fc043a5d5
shige <shige@NetBSD.org>
2005-01-18 17:55:16 +0000

    - Copy evbppc/obs405/machdep.c to evbppc/obs405/md_machdep.c.    (prevent machdep.o from conflict) - Rename function: cpu_startup -> obs405_cpu_startup. - Set md-func-ptr: md_cpu_startup = obs405_cpu_startup.

--- sys/arch/evbppc/include/obs405.h
+++ sys/arch/evbppc/include/obs405.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: obs405.h,v 1.1 2005/01/13 17:07:24 shige Exp $	*/
+/*	$NetBSD: obs405.h,v 1.2 2005/01/18 17:55:16 shige Exp $	*/
 
 /*
  * Copyright 2004 Shigeyuki Fukushima.
@@ -70,6 +70,7 @@
 /*
  * extern variables and functions
  */
+extern void obs405_cpu_startup(void);
 extern void obs405_device_register(struct device *dev, void *aux);
 
 #endif	/* _EVBPPC_OBS405_H_ */
