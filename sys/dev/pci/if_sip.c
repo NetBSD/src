ede3808f749f139cf3ba90bc91fc68c670c1cdc2
he <he@NetBSD.org>
2005-01-24 21:41:52 +0000

    Pull up revision 1.96 (requested by thorpej in ticket #939):   When adding or deleting multicast addresses, only change   the address filter if the interface is marked RUNNING.   Fixes PR#27678.

--- sys/dev/pci/if_sip.c
+++ sys/dev/pci/if_sip.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: if_sip.c,v 1.87.2.1 2004/05/28 07:10:38 tron Exp $	*/
+/*	$NetBSD: if_sip.c,v 1.87.2.1.2.1 2005/01/24 21:41:52 he Exp $	*/
 
 /*-
  * Copyright (c) 2001, 2002 The NetBSD Foundation, Inc.
@@ -80,7 +80,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: if_sip.c,v 1.87.2.1 2004/05/28 07:10:38 tron Exp $");
+__KERNEL_RCSID(0, "$NetBSD: if_sip.c,v 1.87.2.1.2.1 2005/01/24 21:41:52 he Exp $");
 
 #include "bpfilter.h"
 #include "rnd.h"
@@ -1481,7 +1481,8 @@ SIP_DECL(ioctl)(struct ifnet *ifp, u_long cmd, caddr_t data)
 			 * Multicast list has changed; set the hardware filter
 			 * accordingly.
 			 */
-			(*sc->sc_model->sip_variant->sipv_set_filter)(sc);
+			if (ifp->if_flags & IFF_RUNNING)
+			    (*sc->sc_model->sip_variant->sipv_set_filter)(sc);
 			error = 0;
 		}
 		break;
