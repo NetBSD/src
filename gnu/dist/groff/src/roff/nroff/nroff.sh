fd4873058eb7992fb0a4f92d8a3409ff49148bd3
wiz <wiz@NetBSD.org>
2005-01-26 10:25:09 +0000

    From groff CVS: 2005-01-25  Werner LEMBERG         * src/roff/nroff/nroff.sh: Handle `*.ISO8859-1' in locale.  From         NetBSD PR bin/29114.

--- gnu/dist/groff/src/roff/nroff/nroff.sh
+++ gnu/dist/groff/src/roff/nroff/nroff.sh
@@ -16,7 +16,7 @@ case "`exec 2>/dev/null ; locale charmap`" in
     case "${LC_ALL-${LC_CTYPE-${LANG}}}" in
       *.UTF-8)
         T=-Tutf8 ;;
-      iso_8859_1 | *.ISO-8859-1)
+      iso_8859_1 | *.ISO-8859-1 | *.ISO8859-1)
         T=-Tlatin1 ;;
       *.IBM-1047)
         T=-Tcp1047 ;;
