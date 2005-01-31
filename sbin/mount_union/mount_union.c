d31c546ace3aac9a90e718b04d3ef5669568db1e
erh <erh>
2005-01-31 05:19:18 +0000

    Fix PR#3617 and PR#3205: call realpath to convert mount points and paths to device nodes into absolute paths before using them.

--- sbin/mount_union/mount_union.c
+++ sbin/mount_union/mount_union.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: mount_union.c,v 1.12 2003/08/07 10:04:33 agc Exp $	*/
+/*	$NetBSD: mount_union.c,v 1.13 2005/01/31 05:19:19 erh Exp $	*/
 
 /*
  * Copyright (c) 1992, 1993, 1994
@@ -42,7 +42,7 @@ __COPYRIGHT("@(#) Copyright (c) 1992, 1993, 1994\n\
 #if 0
 static char sccsid[] = "@(#)mount_union.c	8.6 (Berkeley) 4/26/95";
 #else
-__RCSID("$NetBSD: mount_union.c,v 1.12 2003/08/07 10:04:33 agc Exp $");
+__RCSID("$NetBSD: mount_union.c,v 1.13 2005/01/31 05:19:19 erh Exp $");
 #endif
 #endif /* not lint */
 
@@ -88,7 +88,8 @@ mount_union(argc, argv)
 {
 	struct union_args args;
 	int ch, mntflags;
-	char target[MAXPATHLEN];
+	char target[MAXPATHLEN], canon_dir[MAXPATHLEN];
+
 
 	mntflags = 0;
 	args.mntflags = UNMNT_ABOVE;
@@ -116,17 +117,28 @@ mount_union(argc, argv)
 	if (argc != 2)
 		usage();
 
-	if (realpath(argv[0], target) == 0)
-		err(1, "%s", target);
+	if (realpath(argv[0], target) == NULL)       /* Check device path */
+		err(1, "realpath %s", argv[0]);
+	if (strncmp(argv[0], target, MAXPATHLEN)) {
+		warnx("\"%s\" is a relative path.", argv[0]);
+		warnx("using \"%s\" instead.", target);
+	}
 
-	if (subdir(target, argv[1]) || subdir(argv[1], target))
+	if (realpath(argv[1], canon_dir) == NULL)    /* Check mounton path */
+		err(1, "realpath %s", argv[1]);
+	if (strncmp(argv[1], canon_dir, MAXPATHLEN)) {
+		warnx("\"%s\" is a relative path.", argv[1]);
+		warnx("using \"%s\" instead.", canon_dir);
+	}
+
+	if (subdir(target, canon_dir) || subdir(canon_dir, target))
 		errx(1, "%s (%s) and %s are not distinct paths",
-		    argv[0], target, argv[1]);
+		    argv[0], target, canon_dir);
 
 	args.target = target;
 
-	if (mount(MOUNT_UNION, argv[1], mntflags, &args))
-		err(1, "%s on %s", target, argv[1]);
+	if (mount(MOUNT_UNION, canon_dir, mntflags, &args))
+		err(1, "%s on %s", target, canon_dir);
 	if (mntflags & MNT_GETARGS) {
 		char buf[1024];
 		(void)snprintb(buf, sizeof(buf), UNMNT_BITS, args.mntflags);
