c4202aad579c112e9e0ecdbd6c32bc692c17737c
thorpej <thorpej@NetBSD.org>
2005-01-31 03:04:25 +0000

    Eliminate use of M_HASFCS.

--- sys/dev/ic/atw.c
+++ sys/dev/ic/atw.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: atw.c,v 1.82 2005/01/04 00:56:51 dyoung Exp $	*/
+/*	$NetBSD: atw.c,v 1.83 2005/01/31 03:06:40 thorpej Exp $	*/
 
 /*-
  * Copyright (c) 1998, 1999, 2000, 2002, 2003, 2004 The NetBSD Foundation, Inc.
@@ -41,7 +41,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: atw.c,v 1.82 2005/01/04 00:56:51 dyoung Exp $");
+__KERNEL_RCSID(0, "$NetBSD: atw.c,v 1.83 2005/01/31 03:06:40 thorpej Exp $");
 
 #include "bpfilter.h"
 
@@ -3091,7 +3091,7 @@ atw_rxintr(struct atw_softc *sc)
 
 		ifp->if_ipackets++;
 		if (sc->sc_opmode & ATW_NAR_PR)
-			m->m_flags |= M_HASFCS;
+			len -= IEEE80211_CRC_LEN;
 		m->m_pkthdr.rcvif = ifp;
 		m->m_pkthdr.len = m->m_len = MIN(m->m_ext.ext_size, len);
 
