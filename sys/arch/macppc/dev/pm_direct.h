eafb2d44b6ad7a39e7b923148022b0163b236ce2
briggs <briggs>
2005-02-01 03:08:16 +0000

    Remove one level of interrupt redirection by switching on the ADB hardware type and registering a Cuda or PMU interrupt handler specifically instead of an intermediate interrupt handler.

--- sys/arch/macppc/dev/pm_direct.h
+++ sys/arch/macppc/dev/pm_direct.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: pm_direct.h,v 1.9 2005/02/01 02:46:00 briggs Exp $	*/
+/*	$NetBSD: pm_direct.h,v 1.10 2005/02/01 03:08:16 briggs Exp $	*/
 
 /*
  * Copyright (C) 1997 Takashi Hamada
@@ -46,6 +46,7 @@ typedef	struct	{
 }	PMData;
 
 int pmgrop __P((PMData *));
+int pm_intr __P((void *));
 void pm_init __P((void));
 void pm_adb_restart __P((void));
 void pm_adb_poweroff __P((void));
