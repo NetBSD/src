b59c0f697a7ba3a88a5ed9bb394a59c94826551f
simonb <simonb@NetBSD.org>
2005-02-05 00:13:34 +0000

    De-register.

--- usr.bin/cksum/cksum.c
+++ usr.bin/cksum/cksum.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: cksum.c,v 1.23 2005/01/20 15:44:59 xtraeme Exp $	*/
+/*	$NetBSD: cksum.c,v 1.24 2005/02/05 00:13:34 simonb Exp $	*/
 
 /*-
  * Copyright (c) 1991, 1993
@@ -81,7 +81,7 @@ __COPYRIGHT("@(#) Copyright (c) 1991, 1993\n\
 #if 0
 static char sccsid[] = "@(#)cksum.c	8.2 (Berkeley) 4/28/95";
 #endif
-__RCSID("$NetBSD: cksum.c,v 1.23 2005/01/20 15:44:59 xtraeme Exp $");
+__RCSID("$NetBSD: cksum.c,v 1.24 2005/02/05 00:13:34 simonb Exp $");
 #endif /* not lint */
 
 #include <sys/cdefs.h>
@@ -145,7 +145,7 @@ void	usage(void);
 int
 main(int argc, char **argv)
 {
-	register int ch, fd, rval, dosum, pflag, nohashstdin;
+	int ch, fd, rval, dosum, pflag, nohashstdin;
 	u_int32_t val;
 	off_t len;
 	char *fn;
--- usr.bin/cksum/crc.c
+++ usr.bin/cksum/crc.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: crc.c,v 1.16 2005/01/12 17:04:35 xtraeme Exp $	*/
+/*	$NetBSD: crc.c,v 1.17 2005/02/05 00:13:34 simonb Exp $	*/
 
 /*-
  * Copyright (c) 1991, 1993
@@ -41,7 +41,7 @@
 #if 0
 static char sccsid[] = "@(#)crc.c	8.1 (Berkeley) 6/17/93";
 #else
-__RCSID("$NetBSD: crc.c,v 1.16 2005/01/12 17:04:35 xtraeme Exp $");
+__RCSID("$NetBSD: crc.c,v 1.17 2005/02/05 00:13:34 simonb Exp $");
 #endif
 #endif /* not lint */
 
@@ -112,11 +112,11 @@ static const u_int32_t crctab[] = {
  * success and 1 on failure.  Errno is set on failure.
  */
 int
-crc(register int fd, u_int32_t *cval, off_t *clen)
+crc(int fd, u_int32_t *cval, off_t *clen)
 {
-	register u_char *p;
-	register int nr;
-	register u_int32_t thecrc;
+	u_char *p;
+	int nr;
+	u_int32_t thecrc;
 	off_t len;
 	u_char buf[16 * 1024];
 
