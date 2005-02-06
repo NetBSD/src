5f6ecd182a3516c26fe8626311a1c1346681445a
jmc <jmc@NetBSD.org>
2005-02-06 07:40:50 +0000

    Pull up revision 1.1 (requested by martti in ticket #1086) Move ipf to sys/dist/ipf and sync w. trunk

--- sys/dist/ipf/netinet/ip_frag.c
+++ sys/dist/ipf/netinet/ip_frag.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: ip_frag.c,v 1.1 2004/10/01 15:26:00 christos Exp $	*/
+/*	$NetBSD: ip_frag.c,v 1.1.10.1 2005/02/06 07:40:50 jmc Exp $	*/
 
 /*
  * Copyright (C) 1993-2003 by Darren Reed.
@@ -103,7 +103,7 @@ extern struct timeout fr_slowtimer_ch;
 #if !defined(lint)
 #if defined(__NetBSD__)
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: ip_frag.c,v 1.1 2004/10/01 15:26:00 christos Exp $");
+__KERNEL_RCSID(0, "$NetBSD: ip_frag.c,v 1.1.10.1 2005/02/06 07:40:50 jmc Exp $");
 #else
 static const char sccsid[] = "@(#)ip_frag.c	1.11 3/24/96 (C) 1993-2000 Darren Reed";
 static const char rcsid[] = "@(#)Id: ip_frag.c,v 2.77 2004/01/27 00:24:54 darrenr Exp";
