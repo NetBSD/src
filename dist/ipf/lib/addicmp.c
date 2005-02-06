328b6c4d2bd7e5db912c92b0ebb857c8b161cb6a
jmc <jmc@NetBSD.org>
2005-02-06 07:43:32 +0000

    Pull up revisions 1.2-1.4 (requested by martti in ticket #1086) Move ipf to sys/dist/ipf and sync w. trunk

--- dist/ipf/lib/addicmp.c
+++ dist/ipf/lib/addicmp.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: addicmp.c,v 1.1.1.1 2004/03/28 08:56:17 martti Exp $	*/
+/*	$NetBSD: addicmp.c,v 1.1.1.1.4.1 2005/02/06 07:43:32 jmc Exp $	*/
 
 /*
  * Copyright (C) 1993-2001 by Darren Reed.
@@ -36,7 +36,7 @@ int     linenum;
 		return -1;
 	if (!fp->fr_proto)	/* to catch lusers */
 		fp->fr_proto = IPPROTO_ICMP;
-	if (isdigit(***cp)) {
+	if (ISDIGIT(***cp)) {
 		if (!ratoi(**cp, &i, 0, 255)) {
 			fprintf(stderr,
 				"%d: Invalid icmp-type (%s) specified\n",
@@ -70,7 +70,7 @@ int     linenum;
 	if (**cp && strcasecmp("code", **cp))
 		return 0;
 	(*cp)++;
-	if (isdigit(***cp)) {
+	if (ISDIGIT(***cp)) {
 		if (!ratoi(**cp, &i, 0, 255)) {
 			fprintf(stderr,
 				"%d: Invalid icmp code (%s) specified\n",
