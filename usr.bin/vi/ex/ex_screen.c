258d729dd2228c5cae33bb89fa9b32a69f87913b
aymeric <aymeric@NetBSD.org>
2005-02-12 12:53:22 +0000

    Fix the RCSID's to be $NetBSD$ instead of $NetBSD

--- usr.bin/vi/ex/ex_screen.c
+++ usr.bin/vi/ex/ex_screen.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: ex_screen.c,v 1.9 2002/04/09 01:47:33 thorpej Exp $	*/
+/*	$NetBSD: ex_screen.c,v 1.10 2005/02/12 12:53:23 aymeric Exp $	*/
 
 /*-
  * Copyright (c) 1993, 1994
@@ -16,7 +16,7 @@
 #if 0
 static const char sccsid[] = "@(#)ex_screen.c	10.11 (Berkeley) 6/29/96";
 #else
-__RCSID("$NetBSD");
+__RCSID("$NetBSD: ex_screen.c,v 1.10 2005/02/12 12:53:23 aymeric Exp $");
 #endif
 #endif /* not lint */
 
