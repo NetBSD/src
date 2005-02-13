1fb6cc413ad4673fe58102ba8f0eccc90d65ad30
yamt <yamt@NetBSD.org>
2005-02-13 10:28:11 +0000

    use new apis.

--- sys/arch/sgimips/sgimips/bus.c
+++ sys/arch/sgimips/sgimips/bus.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: bus.c,v 1.37 2004/12/17 22:59:49 sekiya Exp $	*/
+/*	$NetBSD: bus.c,v 1.37.4.1 2005/02/13 10:36:53 yamt Exp $	*/
 
 /*
  * Copyright (c) 1998 The NetBSD Foundation, Inc.
@@ -38,7 +38,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: bus.c,v 1.37 2004/12/17 22:59:49 sekiya Exp $");
+__KERNEL_RCSID(0, "$NetBSD: bus.c,v 1.37.4.1 2005/02/13 10:36:53 yamt Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -1036,7 +1036,7 @@ _bus_dmamem_map(bus_dma_tag_t t, bus_dma_segment_t *segs, int nsegs,
 
 	size = round_page(size);
 
-	va = uvm_km_valloc(kernel_map, size);
+	va = uvm_km_alloc(kernel_map, size, 0, UVM_KMF_VAONLY);
 
 	if (va == 0)
 		return (ENOMEM);
@@ -1083,7 +1083,9 @@ _bus_dmamem_unmap(bus_dma_tag_t t, caddr_t kva, size_t size)
 		return;
 
 	size = round_page(size);
-	uvm_km_free(kernel_map, (vaddr_t)kva, size);
+	pmap_remove(pmap_kernel(), (vaddr_t)kva, (vaddr_t)kva + size);
+	pmap_update(pmap_kernel());
+	uvm_km_free(kernel_map, (vaddr_t)kva, size, UVM_KMF_VAONLY);
 }
 
 /*
