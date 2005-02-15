a82410ae3def8ba4fdea87d046a9a65cadb733e5
jsm <jsm@NetBSD.org>
2005-02-15 12:58:21 +0000

    Don't presume phantasia's internal bool (char) is the same as that in curses.h: define phbool and use it where necessary to avoid declaring functions with one bool and defining them with the other.
    Reviewed by <hubertf>.

--- games/phantasia/interplayer.c
+++ games/phantasia/interplayer.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: interplayer.c,v 1.8 2004/04/11 13:35:06 he Exp $	*/
+/*	$NetBSD: interplayer.c,v 1.9 2005/02/15 12:58:21 jsm Exp $	*/
 
 /*
  * interplayer.c - player to player routines for Phantasia
@@ -539,7 +539,7 @@ tampered(what, arg1, arg2)
 
 void
 userlist(ingameflag)
-	bool    ingameflag;
+	phbool  ingameflag;
 {
 	int     numusers = 0;	/* number of users on file */
 
