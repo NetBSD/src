58b3287ba1126b63666fb96bb525cb593cd11b5e
jsm <jsm@NetBSD.org>
2005-02-15 12:58:21 +0000

    Don't presume phantasia's internal bool (char) is the same as that in curses.h: define phbool and use it where necessary to avoid declaring functions with one bool and defining them with the other.
    Reviewed by <hubertf>.

--- games/phantasia/io.c
+++ games/phantasia/io.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: io.c,v 1.8 2004/04/11 13:35:06 he Exp $	*/
+/*	$NetBSD: io.c,v 1.9 2005/02/15 12:58:21 jsm Exp $	*/
 
 /*
  * io.c - input/output routines for Phantasia
@@ -156,7 +156,7 @@ interrupt()
 int
 getanswer(choices, def)
 	const char   *choices;
-	bool    def;
+	phbool  def;
 {
 	int     ch;		/* input */
 	volatile int	loop;	/* counter */
