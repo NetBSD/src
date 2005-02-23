196bb31499f3ebb3f88ac04787d23bb799abaeae
he <he@NetBSD.org>
2005-02-23 15:30:24 +0000

    Pull up revision 1.17 (requested by aymeric in ticket #1000):   Move a line of code which was "obviously" misplaced.  Fixes   a core dump when auto-completing file names and at least   one of the file names is larger than the screen width.   Fixes PR#28382.  Bump version.

--- usr.bin/vi/ex/version.h
+++ usr.bin/vi/ex/version.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: version.h,v 1.13.2.3 2004/06/14 20:24:20 tron Exp $	*/
+/*	$NetBSD: version.h,v 1.13.2.3.2.1 2005/02/23 15:30:24 he Exp $	*/
 
 #define	VI_VERSION \
-    "Version 1.79nb13 (10/23/96) The CSRG, University of California, Berkeley."
+    "Version 1.79nb14 (10/23/96) The CSRG, University of California, Berkeley."
