4fa5cb4dafffd953e91ca24be8ee24bbe3b4958c
christos <christos@NetBSD.org>
2005-02-26 20:58:24 +0000

    Resolve conflicts.

--- dist/openpam/lib/openpam_restore_cred.c
+++ dist/openpam/lib/openpam_restore_cred.c
@@ -31,7 +31,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- * $P4: //depot/projects/openpam/lib/openpam_restore_cred.c#10 $
+ * $P4: //depot/projects/openpam/lib/openpam_restore_cred.c#11 $
  */
 
 #include <sys/param.h>
