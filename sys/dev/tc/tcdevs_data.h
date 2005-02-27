65b6e1f57e981c249d14183cd8be89153f28aa87
perry <perry>
2005-02-27 15:34:22 +0000

    regen

--- sys/dev/tc/tcdevs_data.h
+++ sys/dev/tc/tcdevs_data.h
@@ -1,10 +1,10 @@
-/*	$NetBSD: tcdevs_data.h,v 1.20 2003/10/25 16:24:44 chs Exp $	*/
+/*	$NetBSD: tcdevs_data.h,v 1.21 2005/02/27 15:34:22 perry Exp $	*/
 
 /*
  * THIS FILE AUTOMATICALLY GENERATED.  DO NOT EDIT.
  *
  * generated from:
- *	NetBSD: tcdevs,v 1.19 2003/10/25 16:24:30 chs Exp 
+ *	NetBSD: tcdevs,v 1.19 2003/10/25 16:24:30 chs Exp
  */
 
 /*
