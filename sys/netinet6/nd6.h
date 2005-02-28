d2538e8525c956583524851f9c3c487febd8e980
itojun <itojun@NetBSD.org>
2005-02-28 09:27:07 +0000

    make ip6_getpmtu back to static

--- sys/netinet6/nd6.h
+++ sys/netinet6/nd6.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: nd6.h,v 1.38 2004/03/23 18:21:38 martti Exp $	*/
+/*	$NetBSD: nd6.h,v 1.39 2005/02/28 09:27:07 itojun Exp $	*/
 /*	$KAME: nd6.h,v 1.95 2002/06/08 11:31:06 itojun Exp $	*/
 
 /*
@@ -380,8 +380,6 @@ int nd6_storelladdr __P((struct ifnet *, struct rtentry *, struct mbuf *,
 	struct sockaddr *, u_char *));
 int nd6_sysctl __P((int, void *, size_t *, void *, size_t));
 int nd6_need_cache __P((struct ifnet *));
-int ip6_getpmtu __P((struct route_in6 *, struct route_in6 *, struct ifnet *,
-	struct in6_addr *, u_long *, int *));
 
 /* nd6_nbr.c */
 void nd6_na_input __P((struct mbuf *, int, int));
