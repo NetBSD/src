47bfa33f0681d40a8ea358579045039090a54c10
jdolecek <jdolecek@NetBSD.org>
2005-02-28 07:36:48 +0000

    le_mca_intredge(): g/c error semicolon, found by Simon Burge

--- sys/dev/mca/if_le_mca.c
+++ sys/dev/mca/if_le_mca.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: if_le_mca.c,v 1.9 2005/02/27 00:27:21 perry Exp $	*/
+/*	$NetBSD: if_le_mca.c,v 1.10 2005/02/28 07:36:48 jdolecek Exp $	*/
 
 /*-
  * Copyright (c) 2001 The NetBSD Foundation, Inc.
@@ -52,7 +52,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: if_le_mca.c,v 1.9 2005/02/27 00:27:21 perry Exp $");
+__KERNEL_RCSID(0, "$NetBSD: if_le_mca.c,v 1.10 2005/02/28 07:36:48 jdolecek Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -288,7 +288,7 @@ le_mca_intredge(arg)
 	 * at this time anyway.
 	 */
 
-	if (am7990_intr(arg) == 0);
+	if (am7990_intr(arg) == 0)
 		return (0);
 	for(;;)
 		if (am7990_intr(arg) == 0)
