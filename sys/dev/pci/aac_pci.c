802400546dba230138f54abd4aded314a18558e5
briggs <briggs@NetBSD.org>
2005-03-01 03:31:45 +0000

    * Remove aif_get_mailboxstatus() from the aac_interface structure.  Replace   with aif_get_mailbox().  Make it return uint32_t instead of 'int'. * Add an AAC_GET_MAILBOX() macro and change AAC_GET_MAILBOXSTATUS() to use   that. * Update the Dell PERC 2QC quirk code to use AAC_GET_MAILBOX instead of the   StrongARM-specific code.  While StrongARM access is correct for that card,   it's a bad example of how to access the mailbox registers. * Add the GETINFO command and use it to get and display the card's   supported options at a verbose level during attachment.

--- sys/dev/pci/aac_pci.c
+++ sys/dev/pci/aac_pci.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: aac_pci.c,v 1.11 2005/02/27 00:27:32 perry Exp $	*/
+/*	$NetBSD: aac_pci.c,v 1.12 2005/03/01 03:31:45 briggs Exp $	*/
 
 /*-
  * Copyright (c) 2002 The NetBSD Foundation, Inc.
@@ -72,7 +72,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: aac_pci.c,v 1.11 2005/02/27 00:27:32 perry Exp $");
+__KERNEL_RCSID(0, "$NetBSD: aac_pci.c,v 1.12 2005/03/01 03:31:45 briggs Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -103,7 +103,7 @@ int	aac_rx_get_istatus(struct aac_softc *);
 void	aac_rx_clear_istatus(struct aac_softc *, int);
 void	aac_rx_set_mailbox(struct aac_softc *, u_int32_t, u_int32_t,
 			   u_int32_t, u_int32_t, u_int32_t);
-int	aac_rx_get_mailboxstatus(struct aac_softc *);
+uint32_t aac_rx_get_mailbox(struct aac_softc *, int);
 void	aac_rx_set_interrupts(struct aac_softc *, int);
 
 /* StrongARM interface */
@@ -113,7 +113,7 @@ int	aac_sa_get_istatus(struct aac_softc *);
 void	aac_sa_clear_istatus(struct aac_softc *, int);
 void	aac_sa_set_mailbox(struct aac_softc *, u_int32_t, u_int32_t,
 			   u_int32_t, u_int32_t, u_int32_t);
-int	aac_sa_get_mailboxstatus(struct aac_softc *);
+uint32_t aac_sa_get_mailbox(struct aac_softc *, int);
 void	aac_sa_set_interrupts(struct aac_softc *, int);
 
 const struct aac_interface aac_rx_interface = {
@@ -122,7 +122,7 @@ const struct aac_interface aac_rx_interface = {
 	aac_rx_get_istatus,
 	aac_rx_clear_istatus,
 	aac_rx_set_mailbox,
-	aac_rx_get_mailboxstatus,
+	aac_rx_get_mailbox,
 	aac_rx_set_interrupts
 };
 
@@ -132,7 +132,7 @@ const struct aac_interface aac_sa_interface = {
 	aac_sa_get_istatus,
 	aac_sa_clear_istatus,
 	aac_sa_set_mailbox,
-	aac_sa_get_mailboxstatus,
+	aac_sa_get_mailbox,
 	aac_sa_set_interrupts
 };
 
@@ -585,20 +585,20 @@ aac_rx_set_mailbox(struct aac_softc *sc, u_int32_t command,
 }
 
 /*
- * Fetch the immediate command status word
+ * Fetch the specified mailbox
  */
-int
-aac_sa_get_mailboxstatus(struct aac_softc *sc)
+uint32_t
+aac_sa_get_mailbox(struct aac_softc *sc, int mb)
 {
 
-	return (AAC_GETREG4(sc, AAC_SA_MAILBOX));
+	return (AAC_GETREG4(sc, AAC_SA_MAILBOX + (mb * 4)));
 }
 
-int
-aac_rx_get_mailboxstatus(struct aac_softc *sc)
+uint32_t
+aac_rx_get_mailbox(struct aac_softc *sc, int mb)
 {
 
-	return (AAC_GETREG4(sc, AAC_RX_MAILBOX));
+	return (AAC_GETREG4(sc, AAC_RX_MAILBOX + (mb * 4)));
 }
 
 /*
