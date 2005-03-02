1b5ed5a02f8b5f4f9253180a047a93b6bb09b5ce
mycroft <mycroft@NetBSD.org>
2005-03-02 11:37:27 +0000

    Copyright maintenance.

--- sys/dev/usb/uhid.c
+++ sys/dev/usb/uhid.c
@@ -1,7 +1,7 @@
-/*	$NetBSD: uhid.c,v 1.62 2004/06/23 02:30:52 mycroft Exp $	*/
+/*	$NetBSD: uhid.c,v 1.63 2005/03/02 11:37:27 mycroft Exp $	*/
 
 /*
- * Copyright (c) 1998 The NetBSD Foundation, Inc.
+ * Copyright (c) 1998, 2004 The NetBSD Foundation, Inc.
  * All rights reserved.
  *
  * This code is derived from software contributed to The NetBSD Foundation
@@ -42,7 +42,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: uhid.c,v 1.62 2004/06/23 02:30:52 mycroft Exp $");
+__KERNEL_RCSID(0, "$NetBSD: uhid.c,v 1.63 2005/03/02 11:37:27 mycroft Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
