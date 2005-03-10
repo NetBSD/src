e26de33fea6da20c50b29ffbdb5629a335370d5a
bouyer <bouyer@NetBSD.org>
2005-03-10 19:47:08 +0000

    Bump blkif_pdev_t from 16 to 32 bits, to fix the dev_t problem on NetBSD. Remove the padding bits from blkif_extent_t, so that the message size doesn't change. You'll need xentools20-2.0.3nb1 if you upgrade your kernel (the old tools didn't zero out the padding bits, and a new kernel will interpret them as part of the device number).

--- sys/arch/xen/include/xen-public/io/domain_controller.h
+++ sys/arch/xen/include/xen-public/io/domain_controller.h
@@ -1,4 +1,4 @@
-/* $NetBSD: domain_controller.h,v 1.2 2005/03/09 22:39:20 bouyer Exp $ */
+/* $NetBSD: domain_controller.h,v 1.3 2005/03/10 19:47:08 bouyer Exp $ */
 
 /*
  * Copyright (c) 2004, K A Fraser
@@ -104,7 +104,7 @@ typedef struct {
 
 /* These are used by both front-end and back-end drivers. */
 #define blkif_vdev_t   u16
-#define blkif_pdev_t   u16
+#define blkif_pdev_t   u32
 #define blkif_sector_t u64
 
 /*
@@ -206,7 +206,6 @@ typedef struct {
     blkif_sector_t sector_start;   /*  0 */
     blkif_sector_t sector_length;  /*  8 */
     blkif_pdev_t   device;         /* 16 */
-    u16            __pad;          /* 18 */
 } PACKED blkif_extent_t; /* 20 bytes */
 
 /* Non-specific 'okay' return. */
