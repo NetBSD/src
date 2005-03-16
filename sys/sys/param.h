245d3bf344e060bb42883d2fc72faa4d41ba50ef
tron <tron@NetBSD.org>
2005-03-16 15:24:02 +0000

    After thinking about it again "2.0.2_STABLE" isn't the correct name. It will be used for 2.0.2 plus fixes. Use "2.0.2_BETA" instead.

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.188.2.12 2005/03/16 12:21:45 tron Exp $	*/
+/*	$NetBSD: param.h,v 1.188.2.13 2005/03/16 15:24:02 tron Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -63,7 +63,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	200000200	/* NetBSD 2.0.2_STABLE */
+#define	__NetBSD_Version__	200000200	/* NetBSD 2.0.2_BETA */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) >= __NetBSD_Version__)
