b51f293ba0a232b1eb2f2cf9916338e5688defec
kleink <kleink@NetBSD.org>
2005-03-17 20:48:50 +0000

    We have pselect(2) now; the local wrapper is no longer necessary.

--- lib/libc/include/port_before.h
+++ lib/libc/include/port_before.h
@@ -1,6 +1,5 @@
 #include "namespace.h"
 #include <sys/cdefs.h>
-#define NEED_PSELECT
 #define ISC_FORMAT_PRINTF(a,b) __attribute__((__format__(__printf__,a,b)))
 #define ISC_SOCKLEN_T	socklen_t
 #define DE_CONST(c,v)	v = ((c) ? \
