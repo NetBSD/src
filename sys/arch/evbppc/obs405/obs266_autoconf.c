86eb2eb11cc1b7c82d8b646f4414e7a933a9780b
shige <shige>
2005-03-18 14:12:34 +0000

    Rearrange codes for OpenBlockS266 machine.

--- sys/arch/evbppc/obs405/obs266_autoconf.c
+++ sys/arch/evbppc/obs405/obs266_autoconf.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: obs405.h,v 1.5 2005/03/18 14:12:34 shige Exp $	*/
+/*	$NetBSD: obs266_autoconf.c,v 1.1 2005/03/18 14:12:34 shige Exp $	*/
 
 /*
  * Copyright 2004 Shigeyuki Fukushima.
@@ -32,43 +32,16 @@
  * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
  * DAMAGE.
  */
+#include <sys/cdefs.h>
+__KERNEL_RCSID(0, "$NetBSD: obs266_autoconf.c,v 1.1 2005/03/18 14:12:34 shige Exp $");
 
-#ifndef	_EVBPPC_OBS405_H_
-#define	_EVBPPC_OBS405_H_
-
-#include <sys/param.h>
+#include <sys/systm.h>
 #include <sys/device.h>
 
-#include <powerpc/ibm4xx/ibm405gp.h>
+#include <machine/obs266.h>
 
-#include "com.h"
-#if (NCOM > 0)
+void device_register(struct device *dev, void *aux)
+{
 
-#include <sys/termios.h>
-
-#  ifndef CONADDR
-#  define CONADDR	IBM405GP_UART0_BASE
-#  endif
-#  ifndef CONSPEED
-#  define CONSPEED	B9600
-#  endif
-#  ifndef CONMODE
-   /* 8N1 */
-#  define CONMODE	((TTYDEF_CFLAG & ~(CSIZE | CSTOPB | PARENB)) | CS8)
-#  endif
-
-#define OBS405_CONADDR		(CONADDR)
-#define OBS405_CONSPEED		(CONSPEED)
-#define OBS405_CONMODE		(CONMODE)
-
-#endif /* NCOM */
-
-#include <dev/ic/comreg.h>
-
-/*
- * extern variables and functions
- */
-extern void obs405_consinit(int com_freq);
-extern void obs405_device_register(struct device *dev, void *aux, int com_freq);
-
-#endif	/* _EVBPPC_OBS405_H_ */
+	obs405_device_register(dev, aux, OBS266_COM_FREQ);
+}
