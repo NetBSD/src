eee66f6a42ca83fbfb834a56b01ec69c6b00ff02
tron <tron>
2005-03-19 01:44:15 +0000

    Pull up revision 1.30 (requested by bouyer in ticket #1751): Add support for the Ignore Wide Residue SCSI message.

--- sys/dev/ic/esiop.c
+++ sys/dev/ic/esiop.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: esiop.c,v 1.16.2.3 2005/03/17 17:50:08 tron Exp $	*/
+/*	$NetBSD: esiop.c,v 1.16.2.4 2005/03/19 01:44:15 tron Exp $	*/
 
 /*
  * Copyright (c) 2002 Manuel Bouyer.
@@ -33,7 +33,7 @@
 /* SYM53c7/8xx PCI-SCSI I/O Processors driver */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: esiop.c,v 1.16.2.3 2005/03/17 17:50:08 tron Exp $");
+__KERNEL_RCSID(0, "$NetBSD: esiop.c,v 1.16.2.4 2005/03/19 01:44:15 tron Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -921,6 +921,15 @@ scintr:
 				CALL_SCRIPT(Ent_msgin_ack);
 				return 1;
 			}
+			if (msgin == MSG_IGN_WIDE_RESIDUE) {
+			/* use the extmsgdata table to get the second byte */
+				esiop_cmd->cmd_tables->t_extmsgdata.count =
+				    htole32(1);
+				esiop_table_sync(esiop_cmd,
+				    BUS_DMASYNC_PREREAD | BUS_DMASYNC_PREWRITE);
+				CALL_SCRIPT(Ent_get_extmsgdata);
+				return 1;
+			}
 			if (xs)
 				scsipi_printaddr(xs->xs_periph);
 			else
@@ -963,6 +972,29 @@ scintr:
 			printf("\n");
 			}
 #endif
+			if (esiop_cmd->cmd_tables->msg_in[0] ==
+			    MSG_IGN_WIDE_RESIDUE) {
+			/* we got the second byte of MSG_IGN_WIDE_RESIDUE */
+				if (esiop_cmd->cmd_tables->msg_in[3] != 1)
+					printf("MSG_IGN_WIDE_RESIDUE: "
+					     "bad len %d\n",
+					     esiop_cmd->cmd_tables->msg_in[3]);
+				switch (siop_iwr(&esiop_cmd->cmd_c)) {
+				case SIOP_NEG_MSGOUT:
+					esiop_table_sync(esiop_cmd,
+					    BUS_DMASYNC_PREREAD |
+					    BUS_DMASYNC_PREWRITE);
+					CALL_SCRIPT(Ent_send_msgout);
+					return 1;
+				case SIOP_NEG_ACK:
+					CALL_SCRIPT(Ent_msgin_ack);
+					return 1;
+				default:
+					panic("invalid retval from "
+					    "siop_iwr()");
+				}
+				return 1;
+			}
 			if (esiop_cmd->cmd_tables->msg_in[2] == MSG_EXT_PPR) {
 				switch (siop_ppr_neg(&esiop_cmd->cmd_c)) {
 				case SIOP_NEG_MSGOUT:
