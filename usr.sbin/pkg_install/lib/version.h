22ae583943e290d41d97807d7da522b7eb91f472
tron <tron@NetBSD.org>
2005-03-19 13:18:38 +0000

    Pull up revision 1.65 (requested by hubertf in ticket #9): Bump PKGTOOLS_VERSION to today's date to allow telling in which version PR 24744 was fixed (affects lib/ftpio.c).

--- usr.sbin/pkg_install/lib/version.h
+++ usr.sbin/pkg_install/lib/version.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: version.h,v 1.64 2005/02/10 22:52:31 grant Exp $	*/
+/*	$NetBSD: version.h,v 1.64.2.1 2005/03/19 13:18:38 tron Exp $	*/
 
 /*
  * Copyright (c) 2001 Thomas Klausner.  All rights reserved.
@@ -33,6 +33,6 @@
 #ifndef _INST_LIB_VERSION_H_
 #define _INST_LIB_VERSION_H_
 
-#define PKGTOOLS_VERSION "20050210"
+#define PKGTOOLS_VERSION "20050318"
 
 #endif /* _INST_LIB_VERSION_H_ */
