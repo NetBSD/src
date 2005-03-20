b4eaefd64a80ab9517e213b5db528587f3f52c2a
christos <christos@NetBSD.org>
2005-03-20 19:13:23 +0000

    Add a member lockcnt to keep track of locks per uid. Add a uid_find() function similar to the one in OpenBSD.

--- sys/sys/resourcevar.h
+++ sys/sys/resourcevar.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: resourcevar.h,v 1.24 2005/02/03 19:20:02 perry Exp $	*/
+/*	$NetBSD: resourcevar.h,v 1.25 2005/03/20 19:13:23 christos Exp $	*/
 
 /*
  * Copyright (c) 1991, 1993
@@ -93,6 +93,7 @@ struct uidinfo {
 	LIST_ENTRY(uidinfo) ui_hash;
 	uid_t	ui_uid;
 	long	ui_proccnt;	/* Number of processes */
+	long	ui_lockcnt;	/* Number of locks */
 	rlim_t	ui_sbsize;	/* socket buffer size */
 
 };
@@ -101,6 +102,7 @@ extern LIST_HEAD(uihashhead, uidinfo) *uihashtbl;
 extern u_long uihash;		/* size of hash table - 1 */
 int       chgproccnt(uid_t, int);
 int       chgsbsize(uid_t, u_long *, u_long, rlim_t);
+struct uidinfo *uid_find(uid_t);
 
 extern char defcorename[];
 void	 addupc_intr(struct proc *, u_long);
