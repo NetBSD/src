f5c39908f21ec41aefaca8443eec2f368050e97a
kleink <kleink@NetBSD.org>
2005-03-23 08:16:53 +0000

    Avoid lint warning after previous.

--- lib/libc/stdlib/tfind.c
+++ lib/libc/stdlib/tfind.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: tfind.c,v 1.4 2005/03/22 20:13:42 kleink Exp $	*/
+/*	$NetBSD: tfind.c,v 1.5 2005/03/23 08:16:53 kleink Exp $	*/
 
 /*
  * Tree search generalized from Knuth (6.2.2) Algorithm T just like
@@ -13,7 +13,7 @@
 
 #include <sys/cdefs.h>
 #if defined(LIBC_SCCS) && !defined(lint)
-__RCSID("$NetBSD: tfind.c,v 1.4 2005/03/22 20:13:42 kleink Exp $");
+__RCSID("$NetBSD: tfind.c,v 1.5 2005/03/23 08:16:53 kleink Exp $");
 #endif /* LIBC_SCCS and not lint */
 
 #include <assert.h>
@@ -28,7 +28,7 @@ tfind(vkey, vrootp, compar)
 	void * const *vrootp;		/* address of the tree root */
 	int (*compar) __P((const void *, const void *));
 {
-	node_t **rootp = (node_t **)vrootp;
+	node_t * const *rootp = (node_t * const*)vrootp;
 
 	_DIAGASSERT(vkey != NULL);
 	_DIAGASSERT(compar != NULL);
