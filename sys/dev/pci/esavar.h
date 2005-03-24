0e80d02786f60c5a1889adde10c4994dac854d76
jmcneill <jmcneill@NetBSD.org>
2005-03-24 22:31:58 +0000

    Update my email address (the one listed hasn't been active in years).

--- sys/dev/pci/esavar.h
+++ sys/dev/pci/esavar.h
@@ -1,7 +1,7 @@
-/* $NetBSD: esavar.h,v 1.5 2005/01/15 15:19:52 kent Exp $ */
+/* $NetBSD: esavar.h,v 1.6 2005/03/24 22:35:12 jmcneill Exp $ */
 
 /*
- * Copyright (c) 2001, 2002 Jared D. McNeill <jmcneill@invisible.yi.org>
+ * Copyright (c) 2001, 2002 Jared D. McNeill <jmcneill@invisible.ca>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
