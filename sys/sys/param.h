b91008e6a8428d1dfca81d4792fab27e747dc27d
agc <agc@NetBSD.org>
2005-03-27 23:12:55 +0000

    Update the NetBSD version number in the trailing comment to match the __NetBSD_Version__ in rev 1.213 of this file.

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.213 2005/03/26 05:12:36 fvdl Exp $	*/
+/*	$NetBSD: param.h,v 1.214 2005/03/27 23:12:55 agc Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -63,7 +63,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	399000200	/* NetBSD 3.99.1 */
+#define	__NetBSD_Version__	399000200	/* NetBSD 3.99.2 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) >= __NetBSD_Version__)
