078351358081399143f3c396b8f533689850dcc5
thorpej <thorpej@NetBSD.org>
2005-03-29 04:06:20 +0000

    - Add a alpha_shared_intr_reset_strays() function that resets the stray   interrupt counter for a given shared interrupt descriptor. - When an interrupt is successfully handled, reset the strays counter,   thus preventing a "slow leak" from eventually shutting off the interrupt   vector.  Idea taken from pci_kn300.c (which was changed to use the new   alpha_shared_intr_reset_strays() function).

--- sys/arch/alpha/pci/pci_550.c
+++ sys/arch/alpha/pci/pci_550.c
@@ -1,4 +1,4 @@
-/* $NetBSD: pci_550.c,v 1.24 2002/09/27 15:35:37 provos Exp $ */
+/* $NetBSD: pci_550.c,v 1.25 2005/03/29 04:06:20 thorpej Exp $ */
 
 /*-
  * Copyright (c) 1998, 2000 The NetBSD Foundation, Inc.
@@ -66,7 +66,7 @@
 
 #include <sys/cdefs.h>			/* RCS ID & Copyright macro defns */
 
-__KERNEL_RCSID(0, "$NetBSD: pci_550.c,v 1.24 2002/09/27 15:35:37 provos Exp $");
+__KERNEL_RCSID(0, "$NetBSD: pci_550.c,v 1.25 2005/03/29 04:06:20 thorpej Exp $");
 
 #include <sys/types.h>
 #include <sys/param.h>
@@ -419,7 +419,8 @@ dec_550_iointr(arg, vec)
 		    "dec 550 irq");
 		if (ALPHA_SHARED_INTR_DISABLE(dec_550_pci_intr, irq))
 			dec_550_intr_disable(irq);
-	}
+	} else
+		alpha_shared_intr_reset_strays(dec_550_pci_intr, irq);
 }
 
 void
