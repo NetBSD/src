89a9d99104786e4c3b676f6e4342b70ea125ad86
christos <christos>
2005-03-30 17:10:18 +0000

    Centralize error checking for malloc,calloc,strdup.

--- sbin/cgdconfig/utils.h
+++ sbin/cgdconfig/utils.h
@@ -1,4 +1,4 @@
-/* $NetBSD: utils.h,v 1.4 2004/08/13 15:03:57 tv Exp $ */
+/* $NetBSD: utils.h,v 1.5 2005/03/30 17:10:18 christos Exp $ */
 
 /*-
  * Copyright (c) 2002, 2003 The NetBSD Foundation, Inc.
@@ -50,6 +50,9 @@ struct bits;
 typedef struct bits bits_t;
 
 __BEGIN_DECLS
+void		 *emalloc(size_t);
+void		 *ecalloc(size_t, size_t);
+char		 *estrdup(const char *);
 char		**words(const char *, int *);
 void	  	  words_free(char **, int);
 
