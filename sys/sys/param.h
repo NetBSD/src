aaaedb6a9b1beff945b86d98913ae85a97d672eb
yamt <yamt@NetBSD.org>
2005-04-01 12:01:32 +0000

    bump to 3.99.3. uvm_km_* API changes. (yamt-km merge)

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.214 2005/03/27 23:12:55 agc Exp $	*/
+/*	$NetBSD: param.h,v 1.215 2005/04/01 12:01:32 yamt Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -63,7 +63,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	399000200	/* NetBSD 3.99.2 */
+#define	__NetBSD_Version__	399000300	/* NetBSD 3.99.3 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) >= __NetBSD_Version__)
