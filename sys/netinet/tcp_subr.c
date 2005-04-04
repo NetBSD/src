0a2853cc3e96aba1a9922ae51f752ae87c930be8
tron <tron@NetBSD.org>
2005-04-04 17:21:46 +0000

    Pull up revision 1.188 (requested by yamt in ticket #90): protect tcpipqent with splvm.

--- sys/netinet/tcp_subr.c
+++ sys/netinet/tcp_subr.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: tcp_subr.c,v 1.187 2005/03/16 00:39:57 yamt Exp $	*/
+/*	$NetBSD: tcp_subr.c,v 1.187.2.1 2005/04/04 17:21:46 tron Exp $	*/
 
 /*
  * Copyright (C) 1995, 1996, 1997, and 1998 WIDE Project.
@@ -98,7 +98,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: tcp_subr.c,v 1.187 2005/03/16 00:39:57 yamt Exp $");
+__KERNEL_RCSID(0, "$NetBSD: tcp_subr.c,v 1.187.2.1 2005/04/04 17:21:46 tron Exp $");
 
 #include "opt_inet.h"
 #include "opt_ipsec.h"
@@ -1238,7 +1238,7 @@ tcp_freeq(tp)
 		TAILQ_REMOVE(&tp->segq, qe, ipqe_q);
 		TAILQ_REMOVE(&tp->timeq, qe, ipqe_timeq);
 		m_freem(qe->ipqe_m);
-		pool_put(&tcpipqent_pool, qe);
+		tcpipqent_free(qe);
 		rv = 1;
 	}
 	tp->t_segqlen = 0;
