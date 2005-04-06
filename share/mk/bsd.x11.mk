6297bdd46a234a8173b462f0d4ab4119f34e13ef
tron <tron>
2005-04-06 22:38:24 +0000

    Pull up revision 1.49 (requested by jmc in ticket #114): Add -fno-merge-constants (as the xsrc build does too) to make the module loader work with the gcc versions we use now. No idea why i386 apparently was not affected by this.

--- share/mk/bsd.x11.mk
+++ share/mk/bsd.x11.mk
@@ -1,4 +1,4 @@
-#	$NetBSD: bsd.x11.mk,v 1.45.2.1 2005/04/06 21:08:07 tron Exp $
+#	$NetBSD: bsd.x11.mk,v 1.45.2.2 2005/04/06 22:38:24 tron Exp $
 
 .include <bsd.init.mk>
 
@@ -75,7 +75,8 @@ X11FLAGS.EXTENSION+=	-D__GLX_ALIGN64
     ${MACHINE} == "macppc"	|| \
     ${MACHINE} == "sgimips"
 #	LOADABLE
-X11FLAGS.LOADABLE=	-DXFree86LOADER -DIN_MODULE -DXFree86Module
+X11FLAGS.LOADABLE=	-DXFree86LOADER -DIN_MODULE -DXFree86Module \
+			-fno-merge-constants
 .endif
 
 # Extract X11VERSION
