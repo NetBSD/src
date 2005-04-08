274986a8c5cd2ef7b4b337825bc4c3b89cd6d59e
tron <tron>
2005-04-08 21:43:30 +0000

    Pull up revision 1.11 (requested by jmc in ticket #1032): Rework to use curses.h only on the target, not on the host.

--- games/phantasia/misc.c
+++ games/phantasia/misc.c
@@ -1,10 +1,11 @@
-/*	$NetBSD: misc.c,v 1.10 2004/01/01 16:03:46 jsm Exp $	*/
+/*	$NetBSD: misc.c,v 1.10.4.1 2005/04/08 21:43:30 tron Exp $	*/
 
 /*
  * misc.c  Phantasia miscellaneous support routines
  */
 
 #include "include.h"
+#include <curses.h>
 
 
 void
