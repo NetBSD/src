1d12c531886a0deb79dfb4fbe13d573a9ae60d30
yamt <yamt@NetBSD.org>
2005-04-16 08:53:09 +0000

    add files which i forgot to add with arch/x86/x86/bus_dma.c rev.1.21.

---
+++ sys/arch/i386/include/bus_private.h
@@ -0,0 +1,3 @@
+/*	$NetBSD: bus_private.h,v 1.1 2005/04/16 08:53:09 yamt Exp $	*/
+
+#include <x86/bus_private.h>
---
+++ sys/arch/xen/include/bus_private.h
@@ -0,0 +1,48 @@
+/*	$NetBSD: bus_private.h,v 1.1 2005/04/16 08:53:09 yamt Exp $	*/
+
+/*-
+ * Copyright (c)2005 YAMAMOTO Takashi,
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <uvm/uvm_extern.h>
+
+#define	_BUS_PHYS_TO_BUS(pa)	((bus_addr_t)xpmap_ptom(pa))
+#define	_BUS_VIRT_TO_BUS(pm, va) _bus_virt_to_bus((pm), (va))
+
+static __inline bus_addr_t _bus_virt_to_bus(struct pmap *, vaddr_t);
+
+static __inline bus_addr_t
+_bus_virt_to_bus(struct pmap *pm, vaddr_t va)
+{
+	bus_addr_t ba;
+
+	if (!pmap_extract_ma(pm, va, &ba)) {
+		panic("_bus_virt_to_bus");
+	}
+
+	return ba;
+}
+
+#include <x86/bus_private.h>
