ec407af1b76c85e8c84af144350d95410c4ba5cb
tron <tron@NetBSD.org>
2005-04-19 12:31:03 +0000

    Pull up revision 1.4 (requested by kleink in ticket #163): Push back the descriptions of NaN formats, and descriptions of the distinction between signalling NaNs and quiet NaNs back into the machine-dependent headers; treat the implementation of __nanf in the same spirit. IEEE 754 leaves the distinction between signalling NaNs and quiet NANs to the implementation, and unlike our headers used to suggest they're not identical in the interpretation of the fraction's MSb; in due course, make those of hppa, mips, sh3, and sh5 reflect reality.

--- sys/arch/sh5/include/ieee.h
+++ sys/arch/sh5/include/ieee.h
@@ -1,3 +1,13 @@
-/*	$NetBSD: ieee.h,v 1.3 2003/10/26 20:55:30 kleink Exp $	*/
+/*	$NetBSD: ieee.h,v 1.3.14.1 2005/04/19 12:31:03 tron Exp $	*/
 
 #include <sys/ieee754.h>
+
+/*
+ * A NaN is a `signalling NaN' if its QUIETNAN bit is set in its
+ * high fraction; if the bit is clear, it is a `quiet NaN'.
+ */
+
+#if 0
+#define	SNG_QUIETNAN	(1 << 22)
+#define	DBL_QUIETNAN	(1 << 19)
+#endif
