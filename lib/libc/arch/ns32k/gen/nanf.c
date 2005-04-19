167b87ff6a3d96d83c10fabe9b7f7f34602922a4
tron <tron@NetBSD.org>
2005-04-19 12:30:50 +0000

    Pull up revision 1.3 (requested by kleink in ticket #163): Push back the descriptions of NaN formats, and descriptions of the distinction between signalling NaNs and quiet NaNs back into the machine-dependent headers; treat the implementation of __nanf in the same spirit. IEEE 754 leaves the distinction between signalling NaNs and quiet NANs to the implementation, and unlike our headers used to suggest they're not identical in the interpretation of the fraction's MSb; in due course, make those of hppa, mips, sh3, and sh5 reflect reality.

---
+++ lib/libc/arch/ns32k/gen/nanf.c
@@ -0,0 +1,13 @@
+/*	$NetBSD: nanf.c,v 1.3.2.2 2005/04/19 12:30:50 tron Exp $	*/
+
+#include <sys/cdefs.h>
+#if defined(LIBC_SCCS) && !defined(lint)
+__RCSID("$NetBSD: nanf.c,v 1.3.2.2 2005/04/19 12:30:50 tron Exp $");
+#endif /* LIBC_SCCS and not lint */
+
+#include <math.h>
+#include <machine/endian.h>
+
+/* bytes for quiet NaN (IEEE single precision) */
+const union __float_u __nanf =
+		{ {    0,    0, 0xc0, 0x7f } };
---
+++ lib/libc/arch/sh3/gen/nanf.c
@@ -0,0 +1,17 @@
+/*	$NetBSD: nanf.c,v 1.3.2.2 2005/04/19 12:30:51 tron Exp $	*/
+
+#include <sys/cdefs.h>
+#if defined(LIBC_SCCS) && !defined(lint)
+__RCSID("$NetBSD: nanf.c,v 1.3.2.2 2005/04/19 12:30:51 tron Exp $");
+#endif /* LIBC_SCCS and not lint */
+
+#include <math.h>
+#include <machine/endian.h>
+
+/* bytes for quiet NaN (IEEE single precision) */
+const union __float_u __nanf =
+#if BYTE_ORDER == BIG_ENDIAN
+		{ { 0x7f, 0xa0,    0,    0 } };
+#else
+		{ {    0,    0, 0xa0, 0x7f } };
+#endif
