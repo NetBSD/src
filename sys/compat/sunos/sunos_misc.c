e7b36256b4971e1d25fef526134b3860990c3854
christos <christos@NetBSD.org>
2005-04-19 19:00:25 +0000

    PR/29696: Joel Carnat: NetBSD freezes when accessing smbfs mounted FS with firefox/linux due to compat getdents() call assumption that all filesystems support cookies.

--- sys/compat/sunos/sunos_misc.c
+++ sys/compat/sunos/sunos_misc.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: sunos_misc.c,v 1.131 2005/02/26 23:10:21 perry Exp $	*/
+/*	$NetBSD: sunos_misc.c,v 1.132 2005/04/19 19:00:25 christos Exp $	*/
 
 /*
  * Copyright (c) 1992, 1993
@@ -50,7 +50,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: sunos_misc.c,v 1.131 2005/02/26 23:10:21 perry Exp $");
+__KERNEL_RCSID(0, "$NetBSD: sunos_misc.c,v 1.132 2005/04/19 19:00:25 christos Exp $");
 
 #if defined(_KERNEL_OPT)
 #include "opt_nfsserver.h"
@@ -554,7 +554,10 @@ again:
 		}
 		if (bdp->d_fileno == 0) {
 			inp += reclen;	/* it is a hole; squish it out */
-			off = *cookie++;
+			if (cookie)
+				off = *cookie++;
+			else
+				off += reclen;
 			continue;
 		}
 		sunos_reclen = SUNOS_RECLEN(&idb, bdp->d_namlen);
@@ -563,7 +566,10 @@ again:
 			outp++;
 			break;
 		}
-		off = *cookie++;	/* each entry points to next */
+		if (cookie)
+			off = *cookie++;	/* each entry points to next */
+		else
+			off += reclen;
 		/*
 		 * Massage in place to make a Sun-shaped dirent (otherwise
 		 * we have to worry about touching user memory outside of
