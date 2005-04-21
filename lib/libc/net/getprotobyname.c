f541300f037e95198ac2bca21b231ffc35cd5a7b
tron <tron>
2005-04-21 16:59:02 +0000

    Pull up revision 1.3 (requested by kleink in ticket #172): Move the (at this time private) get{proto,serv}ent_r() prototypes and data structures into the libc sources until the interface gets redone. Approved by christos.

--- lib/libc/net/getprotobyname.c
+++ lib/libc/net/getprotobyname.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: getprotobyname.c,v 1.2 2005/01/07 22:22:49 mycroft Exp $	*/
+/*	$NetBSD: getprotobyname.c,v 1.2.2.1 2005/04/21 16:59:02 tron Exp $	*/
 
 /*-
  * Copyright (c) 2004 The NetBSD Foundation, Inc.
@@ -37,13 +37,15 @@
  */
 #include <sys/cdefs.h>
 #if defined(LIBC_SCCS) && !defined(lint)
-__RCSID("$NetBSD: getprotobyname.c,v 1.2 2005/01/07 22:22:49 mycroft Exp $");
+__RCSID("$NetBSD: getprotobyname.c,v 1.2.2.1 2005/04/21 16:59:02 tron Exp $");
 #endif /* LIBC_SCCS and not lint */
 
 #include "namespace.h"
 #include "reentrant.h"
 
 #include <netdb.h>
+
+#include "protoent.h"
 
 #ifdef __weak_alias
 __weak_alias(getprotobyname,_getprotobyname)
