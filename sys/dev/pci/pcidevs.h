4d2dc78aea54a2819bbbc88e154c491ee106a1b2
tron <tron@NetBSD.org>
2005-05-01 10:03:05 +0000

    Regen for ticket 5756.

--- sys/dev/pci/pcidevs.h
+++ sys/dev/pci/pcidevs.h
@@ -1,10 +1,10 @@
-/*	$NetBSD: pcidevs.h,v 1.433.2.33 2005/03/19 10:22:57 tron Exp $	*/
+/*	$NetBSD: pcidevs.h,v 1.433.2.34 2005/05/01 10:03:05 tron Exp $	*/
 
 /*
  * THIS FILE AUTOMATICALLY GENERATED.  DO NOT EDIT.
  *
  * generated from:
- *	NetBSD: pcidevs,v 1.428.2.37 2005/03/19 10:22:00 tron Exp 
+ *	NetBSD: pcidevs,v 1.428.2.38 2005/05/01 10:02:18 tron Exp 
  */
 
 /*
@@ -1073,6 +1073,7 @@
 #define	PCI_PRODUCT_COMPAQ_USB_MEDIAGX	0xa0f8		/* USB Controller */
 #define	PCI_PRODUCT_COMPAQ_SMART2P	0xae10		/* SMART2P RAID */
 #define	PCI_PRODUCT_COMPAQ_N100TX	0xae32		/* Netelligent 10/100 TX */
+#define	PCI_PRODUCT_COMPAQ_N10T2	0xb012		/* Netelligent 10 T/2 UTP/Coax */
 #define	PCI_PRODUCT_COMPAQ_INT100TX	0xb030		/* Integrated Netelligent 10/100 TX */
 #define	PCI_PRODUCT_COMPAQ_N10T	0xae34		/* Netelligent 10 T */
 #define	PCI_PRODUCT_COMPAQ_IntNF3P	0xae35		/* Integrated NetFlex 3/P */
