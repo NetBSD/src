e5ef9478f82045ceb0d7a977b418506972fd4c9e
kochi <kochi>
2005-05-02 14:54:46 +0000

    Merge changes for ACPI-CA 20050408.

--- sys/arch/x86/x86/acpi_machdep.c
+++ sys/arch/x86/x86/acpi_machdep.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: acpi_machdep.c,v 1.6 2004/04/10 14:17:21 kochi Exp $	*/
+/*	$NetBSD: acpi_machdep.c,v 1.7 2005/05/02 14:55:12 kochi Exp $	*/
 
 /*
  * Copyright 2001 Wasabi Systems, Inc.
@@ -40,7 +40,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: acpi_machdep.c,v 1.6 2004/04/10 14:17:21 kochi Exp $");
+__KERNEL_RCSID(0, "$NetBSD: acpi_machdep.c,v 1.7 2005/05/02 14:55:12 kochi Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -76,7 +76,7 @@ static int acpi_intrcold = 1;
 
 struct acpi_intr_defer {
 	UINT32	number;
-	OSD_HANDLER function;
+	ACPI_OSD_HANDLER function;
 	void *context;
 	void *ih;
 	LIST_ENTRY(acpi_intr_defer) list;
@@ -110,7 +110,7 @@ acpi_md_OsGetRootPointer(UINT32 Flags, ACPI_POINTER *PhysicalAddress)
 
 ACPI_STATUS
 acpi_md_OsInstallInterruptHandler(UINT32 InterruptNumber,
-    OSD_HANDLER ServiceRoutine, void *Context, void **cookiep)
+    ACPI_OSD_HANDLER ServiceRoutine, void *Context, void **cookiep)
 {
 	void *ih;
 	struct pic *pic;
