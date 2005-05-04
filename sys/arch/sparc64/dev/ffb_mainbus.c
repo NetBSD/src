bc66ff1e01c303d1212bb0ab70caf164f0fb5455
martin <martin@NetBSD.org>
2005-05-04 14:38:44 +0000

    Make ffb take part in the /dev/fbN circus.

--- sys/arch/sparc64/dev/ffb_mainbus.c
+++ sys/arch/sparc64/dev/ffb_mainbus.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: ffb_mainbus.c,v 1.6 2005/05/04 10:01:32 martin Exp $	*/
+/*	$NetBSD: ffb_mainbus.c,v 1.7 2005/05/04 14:38:44 martin Exp $	*/
 /*	$OpenBSD: creator_mainbus.c,v 1.4 2002/07/26 16:39:04 jason Exp $	*/
 
 /*
@@ -34,7 +34,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: ffb_mainbus.c,v 1.6 2005/05/04 10:01:32 martin Exp $");
+__KERNEL_RCSID(0, "$NetBSD: ffb_mainbus.c,v 1.7 2005/05/04 14:38:44 martin Exp $");
 
 #include <sys/types.h>
 #include <sys/param.h>
@@ -47,9 +47,8 @@ __KERNEL_RCSID(0, "$NetBSD: ffb_mainbus.c,v 1.6 2005/05/04 10:01:32 martin Exp $
 #include <machine/openfirm.h>
 
 #include <dev/wscons/wsconsio.h>
-#include <dev/wscons/wsdisplayvar.h>
-#include <dev/wscons/wscons_raster.h>
-#include <dev/rasops/rasops.h>
+#include <dev/sun/fbio.h>
+#include <dev/sun/fbvar.h>
 
 #include <sparc64/dev/ffbreg.h>
 #include <sparc64/dev/ffbvar.h>
