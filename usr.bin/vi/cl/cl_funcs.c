f1461f4d002e83ad79e9f9ad403efaef41e0111b
riz <riz@NetBSD.org>
2005-05-06 14:42:14 +0000

    Pull up revision 1.8 (requested by aymeric in ticket #1195): Fix the RCSID's to be $NetBSD$ instead of $NetBSD

--- usr.bin/vi/cl/cl_funcs.c
+++ usr.bin/vi/cl/cl_funcs.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: cl_funcs.c,v 1.7 2002/04/09 01:47:30 thorpej Exp $	*/
+/*	$NetBSD: cl_funcs.c,v 1.7.6.1 2005/05/06 14:42:14 riz Exp $	*/
 
 /*-
  * Copyright (c) 1993, 1994
@@ -16,7 +16,7 @@
 #if 0
 static const char sccsid[] = "@(#)cl_funcs.c	10.50 (Berkeley) 9/24/96";
 #else
-__RCSID("$NetBSD");
+__RCSID("$NetBSD: cl_funcs.c,v 1.7.6.1 2005/05/06 14:42:14 riz Exp $");
 #endif
 #endif /* not lint */
 
