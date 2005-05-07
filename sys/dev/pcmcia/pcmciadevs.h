64d52a7c55633bbfa7f8b2be5234a0cef708240d
martin <martin@NetBSD.org>
2005-05-07 17:01:11 +0000

    Regen.

--- sys/dev/pcmcia/pcmciadevs.h
+++ sys/dev/pcmcia/pcmciadevs.h
@@ -1,10 +1,10 @@
-/*	$NetBSD: pcmciadevs.h,v 1.212 2005/03/23 11:59:38 martin Exp $	*/
+/*	$NetBSD: pcmciadevs.h,v 1.213 2005/05/07 17:01:11 martin Exp $	*/
 
 /*
  * THIS FILE AUTOMATICALLY GENERATED.  DO NOT EDIT.
  *
  * generated from:
- *	NetBSD: pcmciadevs,v 1.210 2005/03/23 11:58:56 martin Exp
+ *	NetBSD: pcmciadevs,v 1.211 2005/05/07 16:59:11 martin Exp
  */
 /* $FreeBSD: src/sys/dev/pccard/pccarddevs,v 1.20 2001/11/19 05:02:55 imp Exp $*/
 
@@ -264,6 +264,8 @@
 #define	PCMCIA_PRODUCT_ELSA_XI800_IEEE	0x0004
 #define	PCMCIA_CIS_ELSA_XI325_IEEE	{ NULL, NULL, NULL, NULL }
 #define	PCMCIA_PRODUCT_ELSA_XI325_IEEE	0x0005
+#define	PCMCIA_CIS_ELSA_SMC2531WB	{ NULL, NULL, NULL, NULL }
+#define	PCMCIA_PRODUCT_ELSA_SMC2531WB	0x0010
 
 /* EMTAC */
 #define	PCMCIA_CIS_EMTAC_WLAN	{ NULL, NULL, NULL, NULL }
