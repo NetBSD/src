470d2d5180065996775b251b1e2dce1e5fb6fd9a
snj <snj@NetBSD.org>
2005-05-11 04:06:08 +0000

    Pull up revision 1.76 (requested by bouyer in ticket #1367): Add missing return, fix handling of Ignore Wide Residue messages.

--- sys/dev/ic/siop.c
+++ sys/dev/ic/siop.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: siop.c,v 1.72.4.3 2005/05/11 03:59:19 snj Exp $	*/
+/*	$NetBSD: siop.c,v 1.72.4.4 2005/05/11 04:06:08 snj Exp $	*/
 
 /*
  * Copyright (c) 2000 Manuel Bouyer.
@@ -33,7 +33,7 @@
 /* SYM53c7/8xx PCI-SCSI I/O Processors driver */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: siop.c,v 1.72.4.3 2005/05/11 03:59:19 snj Exp $");
+__KERNEL_RCSID(0, "$NetBSD: siop.c,v 1.72.4.4 2005/05/11 04:06:08 snj Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -786,6 +786,7 @@ scintr:
 				siop_table_sync(siop_cmd,
 				    BUS_DMASYNC_PREREAD | BUS_DMASYNC_PREWRITE);
 				CALL_SCRIPT(Ent_get_extmsgdata);
+				return 1;
 			}
 			if (xs)
 				scsipi_printaddr(xs->xs_periph);
