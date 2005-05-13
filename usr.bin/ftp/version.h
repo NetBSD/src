1fe9aa6f5517cad520de3ae1a40fbd304779eb23
lukem <lukem@NetBSD.org>
2005-05-13 05:03:49 +0000

    * Correct the "optlen" argument passed to getsockopt(3) and setsockopt(3)   in various places.  Fixes a problem noted by Allen Briggs. * Improve warning printed when connect(2) for the data channel fails.

--- usr.bin/ftp/version.h
+++ usr.bin/ftp/version.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: version.h,v 1.48 2005/05/11 02:29:13 lukem Exp $	*/
+/*	$NetBSD: version.h,v 1.49 2005/05/13 05:03:49 lukem Exp $	*/
 /*-
  * Copyright (c) 1999-2005 The NetBSD Foundation, Inc.
  * All rights reserved.
@@ -40,5 +40,5 @@
 #endif
 
 #ifndef FTP_VERSION
-#define	FTP_VERSION	"20050511"
+#define	FTP_VERSION	"20050513"
 #endif
