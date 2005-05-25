2051390a6673a3618f4fcc6c80e4c04e9dc1458e
kleink <kleink@NetBSD.org>
2005-05-25 20:58:00 +0000

    Include <sys/cdefs.h> for __signed; related to lib/30072.

--- sys/arch/pc532/include/int_types.h
+++ sys/arch/pc532/include/int_types.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: int_types.h,v 1.6 2003/08/07 16:28:59 agc Exp $	*/
+/*	$NetBSD: int_types.h,v 1.7 2005/05/25 20:58:00 kleink Exp $	*/
 
 /*-
  * Copyright (c) 1990 The Regents of the University of California.
@@ -33,6 +33,8 @@
 
 #ifndef	_NS32K_INT_TYPES_H_
 #define	_NS32K_INT_TYPES_H_
+
+#include <sys/cdefs.h>
 
 /*
  * 7.18.1 Integer types
