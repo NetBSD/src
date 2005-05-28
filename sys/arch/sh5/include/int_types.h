51a3d5988eff802f6a1be13160f528494ec91d59
tron <tron@NetBSD.org>
2005-05-28 13:47:57 +0000

    Pull up revision 1.4 (requested by klein in ticket #346): Include <sys/cdefs.h> for __signed; related to lib/30072.

--- sys/arch/sh5/include/int_types.h
+++ sys/arch/sh5/include/int_types.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: int_types.h,v 1.3 2003/08/07 16:29:31 agc Exp $	*/
+/*	$NetBSD: int_types.h,v 1.3.14.1 2005/05/28 13:47:57 tron Exp $	*/
 
 /*-
  * Copyright (c) 1990 The Regents of the University of California.
@@ -33,6 +33,8 @@
 
 #ifndef	_SH5_INT_TYPES_H_
 #define	_SH5_INT_TYPES_H_
+
+#include <sys/cdefs.h>
 
 /*
  * 7.18.1 Integer types
