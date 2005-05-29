94622623d82b9db0fa23d486ba538b2b03c0b9c8
chs <chs@NetBSD.org>
2005-05-29 15:56:59 +0000

    in pmap_enter(), preset the mod/ref bits based on the flags argument. fixes 25640.

--- sys/arch/atari/atari/pmap.c
+++ sys/arch/atari/atari/pmap.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: pmap.c,v 1.89 2005/04/25 06:27:11 skrll Exp $	*/
+/*	$NetBSD: pmap.c,v 1.90 2005/05/29 15:56:59 chs Exp $	*/
 
 /*-
  * Copyright (c) 1999 The NetBSD Foundation, Inc.
@@ -107,7 +107,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: pmap.c,v 1.89 2005/04/25 06:27:11 skrll Exp $");
+__KERNEL_RCSID(0, "$NetBSD: pmap.c,v 1.90 2005/05/29 15:56:59 chs Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -1292,6 +1292,12 @@ validate:
 	         (kernel_copyback || pmap != pmap_kernel()))
 		npte |= PG_CCB;		/* cache copyback */
 #endif
+	if (flags & VM_PROT_ALL) {
+		npte |= PG_U;
+		if (flags & VM_PROT_WRITE)
+			npte |= PG_M;
+	}
+
 	/*
 	 * Remember if this was a wiring-only change.
 	 * If so, we need not flush the TLB and caches.
