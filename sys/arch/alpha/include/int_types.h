6cb4429fb622ed4b884e3d8bf66f493dc1b7665c
riz <riz@NetBSD.org>
2005-05-29 23:38:22 +0000

    Pull up revision 1.7 (requested by kleink in ticket #1555): Include <sys/cdefs.h> for __signed; related to lib/30072.

--- sys/arch/alpha/include/int_types.h
+++ sys/arch/alpha/include/int_types.h
@@ -1,4 +1,4 @@
-/* $NetBSD: int_types.h,v 1.6 2003/08/07 16:26:33 agc Exp $ */
+/* $NetBSD: int_types.h,v 1.6.6.1 2005/05/29 23:38:22 riz Exp $ */
 
 /*-
  * Copyright (c) 1990, 1993
@@ -33,6 +33,8 @@
 
 #ifndef	_ALPHA_INT_TYPES_H_
 #define	_ALPHA_INT_TYPES_H_
+
+#include <sys/cdefs.h>
 
 /*
  * 7.18.1 Integer types
