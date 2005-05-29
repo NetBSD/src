6da14b03d4bf68ab7a2f4e06d478a53e4d8d599d
riz <riz>
2005-05-29 23:38:23 +0000

    Pull up revision 1.8 (requested by kleink in ticket #1555): Include <sys/cdefs.h> for __signed; related to lib/30072.

--- sys/arch/powerpc/include/int_types.h
+++ sys/arch/powerpc/include/int_types.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: int_types.h,v 1.7 2003/08/07 16:29:18 agc Exp $	*/
+/*	$NetBSD: int_types.h,v 1.7.6.1 2005/05/29 23:38:23 riz Exp $	*/
 
 /*-
  * Copyright (c) 1992, 1993
@@ -36,6 +36,8 @@
 
 #ifndef	_POWERPC_INT_TYPES_H_
 #define	_POWERPC_INT_TYPES_H_
+
+#include <sys/cdefs.h>
 
 /*
  * 7.18.1 Integer types
