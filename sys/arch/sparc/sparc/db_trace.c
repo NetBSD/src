88497981638959d90beea84ccc2de07a10b8ec95
chs <chs@NetBSD.org>
2005-05-30 16:04:53 +0000

    adapt to const changes.

--- sys/arch/sparc/sparc/db_trace.c
+++ sys/arch/sparc/sparc/db_trace.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: db_trace.c,v 1.21 2003/09/07 00:30:40 uwe Exp $ */
+/*	$NetBSD: db_trace.c,v 1.22 2005/05/30 16:04:53 chs Exp $ */
 
 /*
  * Mach Operating System
@@ -27,7 +27,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: db_trace.c,v 1.21 2003/09/07 00:30:40 uwe Exp $");
+__KERNEL_RCSID(0, "$NetBSD: db_trace.c,v 1.22 2005/05/30 16:04:53 chs Exp $");
 
 #include <sys/param.h>
 #include <sys/proc.h>
@@ -50,14 +50,15 @@ db_stack_trace_print(addr, have_addr, count, modif, pr)
 	db_expr_t       addr;
 	int             have_addr;
 	db_expr_t       count;
-	char            *modif;
+	const char      *modif;
 	void		(*pr)(const char *, ...);
 {
 	struct frame	*frame, *prevframe;
 	db_addr_t	pc;
 	boolean_t	kernel_only = TRUE;
 	boolean_t	trace_thread = FALSE;
-	char		c, *cp = modif;
+	const char	*cp = modif;
+	char		c;
 
 	if (ddb_cpuinfo == NULL)
 		ddb_cpuinfo = curcpu();
@@ -101,7 +102,7 @@ db_stack_trace_print(addr, have_addr, count, modif, pr)
 	while (count--) {
 		int		i;
 		db_expr_t	offset;
-		char		*name;
+		const char	*name;
 		db_addr_t	prevpc;
 
 #define FR(framep,field) (INKERNEL(framep)			\
