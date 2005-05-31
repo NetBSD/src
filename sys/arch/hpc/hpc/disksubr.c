a44b84c52969e134628fbc09bbccd8499673388d
uwe <uwe>
2005-05-31 22:45:04 +0000

    Constify as -Wcast-qual seyeth.

--- sys/arch/hpc/hpc/disksubr.c
+++ sys/arch/hpc/hpc/disksubr.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: disksubr.c,v 1.9 2003/10/08 04:25:44 lukem Exp $	*/
+/*	$NetBSD: disksubr.c,v 1.10 2005/05/31 22:45:04 uwe Exp $	*/
 
 /*
  * Copyright (c) 1982, 1986, 1988 Regents of the University of California.
@@ -32,7 +32,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: disksubr.c,v 1.9 2003/10/08 04:25:44 lukem Exp $");
+__KERNEL_RCSID(0, "$NetBSD: disksubr.c,v 1.10 2005/05/31 22:45:04 uwe Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -129,7 +129,7 @@ readdisklabel(dev_t dev, void (*strat)(struct buf *), struct disklabel *lp,
 	struct dkbad *bdp;
 	struct buf *bp;
 	struct disklabel *dlp;
-	char *msg = NULL;
+	const char *msg = NULL;
 	int dospartoff, cyl, i, *ip;
 
 	/* minimal requirements for archtypal disk label */
