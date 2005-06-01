2ffe22d629e0caded31b30d947f654df26bbe2c1
riz <riz@NetBSD.org>
2005-06-01 17:39:17 +0000

    Pull up revision 1.13 (requested by tron in ticket #5721): Use PCI id symbol for the Adaptec ASR-2410SA instead of hardcoding the number.

--- sys/dev/pci/aac_pci.c
+++ sys/dev/pci/aac_pci.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: aac_pci.c,v 1.2.4.4 2005/03/19 10:14:17 tron Exp $	*/
+/*	$NetBSD: aac_pci.c,v 1.2.4.5 2005/06/01 17:39:17 riz Exp $	*/
 
 /*-
  * Copyright (c) 2002 The NetBSD Foundation, Inc.
@@ -72,7 +72,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: aac_pci.c,v 1.2.4.4 2005/03/19 10:14:17 tron Exp $");
+__KERNEL_RCSID(0, "$NetBSD: aac_pci.c,v 1.2.4.5 2005/06/01 17:39:17 riz Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -293,7 +293,7 @@ struct aac_ident {
 		PCI_VENDOR_ADP2,
 		PCI_PRODUCT_ADP2_ASR2200S,
 		PCI_VENDOR_ADP2,
-		0x0290,
+		PCI_PRODUCT_ADP2_ASR2410SA,
 		AAC_HWIF_I960RX,
 		0,
 		"Adaptec ASR-2410SA"
