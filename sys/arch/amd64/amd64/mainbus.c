8af33a3a8534650eff8f38ed55575ee81ad13825
drochner <drochner@NetBSD.org>
2005-06-01 18:51:25 +0000

    update for acpi busname removal

--- sys/arch/amd64/amd64/mainbus.c
+++ sys/arch/amd64/amd64/mainbus.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: mainbus.c,v 1.8 2004/08/30 15:05:16 drochner Exp $	*/
+/*	$NetBSD: mainbus.c,v 1.9 2005/06/01 18:51:25 drochner Exp $	*/
 
 /*
  * Copyright (c) 1996 Christopher G. Demetriou.  All rights reserved.
@@ -31,7 +31,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: mainbus.c,v 1.8 2004/08/30 15:05:16 drochner Exp $");
+__KERNEL_RCSID(0, "$NetBSD: mainbus.c,v 1.9 2005/06/01 18:51:25 drochner Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -202,7 +202,6 @@ mainbus_attach(parent, self, aux)
 
 #if NACPI > 0
 	if (acpi_present) {
-		mba.mba_acpi.aa_busname = "acpi";
 		mba.mba_acpi.aa_iot = X86_BUS_SPACE_IO;
 		mba.mba_acpi.aa_memt = X86_BUS_SPACE_MEM;
 		mba.mba_acpi.aa_pc = NULL;
@@ -211,7 +210,7 @@ mainbus_attach(parent, self, aux)
 		    PCI_FLAGS_MRL_OKAY | PCI_FLAGS_MRM_OKAY |
 		    PCI_FLAGS_MWI_OKAY;
 		mba.mba_acpi.aa_ic = &x86_isa_chipset;
-		config_found_ia(self, "acpibus", &mba.mba_acpi, mainbus_print);
+		config_found_ia(self, "acpibus", &mba.mba_acpi, 0);
 	}
 #endif
 
