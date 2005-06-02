2280e1fb962b60449fe4cb5ebe7570265b972785
lukem <lukem@NetBSD.org>
2005-06-02 01:53:01 +0000

    appease gcc -Wuninitialized

--- usr.bin/make/cond.c
+++ usr.bin/make/cond.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: cond.c,v 1.26 2005/03/01 04:34:55 christos Exp $	*/
+/*	$NetBSD: cond.c,v 1.27 2005/06/02 02:03:19 lukem Exp $	*/
 
 /*
  * Copyright (c) 1988, 1989, 1990 The Regents of the University of California.
@@ -70,14 +70,14 @@
  */
 
 #ifndef MAKE_NATIVE
-static char rcsid[] = "$NetBSD: cond.c,v 1.26 2005/03/01 04:34:55 christos Exp $";
+static char rcsid[] = "$NetBSD: cond.c,v 1.27 2005/06/02 02:03:19 lukem Exp $";
 #else
 #include <sys/cdefs.h>
 #ifndef lint
 #if 0
 static char sccsid[] = "@(#)cond.c	8.2 (Berkeley) 1/2/94";
 #else
-__RCSID("$NetBSD: cond.c,v 1.26 2005/03/01 04:34:55 christos Exp $");
+__RCSID("$NetBSD: cond.c,v 1.27 2005/06/02 02:03:19 lukem Exp $");
 #endif
 #endif /* not lint */
 #endif
@@ -707,6 +707,7 @@ CondToken(Boolean doEval)
 		Boolean lhsQuoted;
 		Boolean rhsQuoted;
 
+		rhs = NULL;
 		lhsFree = rhsFree = FALSE;
 		lhsQuoted = rhsQuoted = FALSE;
 		
