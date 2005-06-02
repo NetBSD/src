b40c04aaf877567a83e0f613f5640f5c214129c1
matt <matt>
2005-06-02 17:07:30 +0000

    constify and adapt for newly enabled warnings.

--- sys/arch/powerpc/powerpc/db_memrw.c
+++ sys/arch/powerpc/powerpc/db_memrw.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: db_memrw.c,v 1.7 2003/09/27 04:44:42 matt Exp $	*/
+/*	$NetBSD: db_memrw.c,v 1.8 2005/06/02 17:07:30 matt Exp $	*/
 /*	$OpenBSD: db_memrw.c,v 1.2 1996/12/28 06:21:52 rahnds Exp $	*/
 
 /* 
@@ -39,7 +39,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: db_memrw.c,v 1.7 2003/09/27 04:44:42 matt Exp $");
+__KERNEL_RCSID(0, "$NetBSD: db_memrw.c,v 1.8 2005/06/02 17:07:30 matt Exp $");
 
 #include <sys/param.h>
 #include <sys/proc.h>
@@ -78,17 +78,17 @@ db_read_bytes(vaddr_t addr, size_t size, char *data)
  * Write bytes to kernel address space for debugger.
  */
 void
-db_write_bytes(vaddr_t addr, size_t size, char *data)
+db_write_bytes(vaddr_t addr, size_t size, const char *data)
 {
 	char *dst = (char *)addr;
 
 	if (size == 4) {
 
-		*((int*)dst) = *((int*)data);
+		*((int*)dst) = *((const int*)data);
 
 	} else 	if (size == 2) {
 
-		*((short*)dst) = *((short*)data);
+		*((short*)dst) = *((const short*)data);
 
 	} else {
 
