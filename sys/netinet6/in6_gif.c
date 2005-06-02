41dcb3a310a3d5509e162640cf9de883d1dd5b42
tron <tron@NetBSD.org>
2005-06-02 10:54:58 +0000

    Remove type casts and lint directives which are now longer necessary because the first argument of m_copydata() is "const struct mbuf *" now.

--- sys/netinet6/in6_gif.c
+++ sys/netinet6/in6_gif.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: in6_gif.c,v 1.40 2005/05/29 21:43:51 christos Exp $	*/
+/*	$NetBSD: in6_gif.c,v 1.41 2005/06/02 10:54:58 tron Exp $	*/
 /*	$KAME: in6_gif.c,v 1.62 2001/07/29 04:27:25 itojun Exp $	*/
 
 /*
@@ -31,7 +31,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: in6_gif.c,v 1.40 2005/05/29 21:43:51 christos Exp $");
+__KERNEL_RCSID(0, "$NetBSD: in6_gif.c,v 1.41 2005/06/02 10:54:58 tron Exp $");
 
 #include "opt_inet.h"
 #include "opt_iso.h"
@@ -376,8 +376,7 @@ gif_encapcheck6(m, off, proto, arg)
 	/* sanity check done in caller */
 	sc = (struct gif_softc *)arg;
 
-	/* LINTED const cast */
-	m_copydata((struct mbuf *)m, 0, sizeof(ip6), (caddr_t)&ip6);
+	m_copydata(m, 0, sizeof(ip6), (caddr_t)&ip6);
 	ifp = ((m->m_flags & M_PKTHDR) != 0) ? m->m_pkthdr.rcvif : NULL;
 
 	return gif_validate6(&ip6, sc, ifp);
