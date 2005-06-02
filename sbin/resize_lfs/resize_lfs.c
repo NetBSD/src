620037ae2bff2478ed39492cf61367bcb5931bbb
lukem <lukem@NetBSD.org>
2005-06-02 01:16:06 +0000

    Ensure verbose is initialized before using it. Detected by gcc -Wuninitialized.

--- sbin/resize_lfs/resize_lfs.c
+++ sbin/resize_lfs/resize_lfs.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: resize_lfs.c,v 1.1 2005/04/23 19:47:51 perseant Exp $	*/
+/*	$NetBSD: resize_lfs.c,v 1.2 2005/06/02 01:16:06 lukem Exp $	*/
 /*-
  * Copyright (c) 2005 The NetBSD Foundation, Inc.
  * All rights reserved.
@@ -74,6 +74,7 @@ main(int argc, char **argv)
 	struct statvfs vfs;
 
 	/* Initialize and parse arguments */
+	verbose = 0;
 	newsize = 0;
 	while ((ch = getopt(argc, argv, "s:v")) != -1) {
 		switch(ch) {
