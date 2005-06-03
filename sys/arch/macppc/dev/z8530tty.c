fdfba2c639e51636e2b2da24e546a076bfd7851d
matt <matt@NetBSD.org>
2005-06-03 21:38:05 +0000

    cleanup const/volatile/shadowing

--- sys/arch/macppc/dev/z8530tty.c
+++ sys/arch/macppc/dev/z8530tty.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: z8530tty.c,v 1.21 2003/12/04 13:05:16 keihan Exp $	*/
+/*	$NetBSD: z8530tty.c,v 1.22 2005/06/03 21:38:05 matt Exp $	*/
 
 /*-
  * Copyright (c) 1993, 1994, 1995, 1996, 1997, 1998, 1999
@@ -137,7 +137,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: z8530tty.c,v 1.21 2003/12/04 13:05:16 keihan Exp $");
+__KERNEL_RCSID(0, "$NetBSD: z8530tty.c,v 1.22 2005/06/03 21:38:05 matt Exp $");
 
 #include "opt_kgdb.h"
 
@@ -1398,7 +1398,7 @@ zstty_rxsoft(zst, tp)
 	struct tty *tp;
 {
 	struct zs_chanstate *cs = zst->zst_cs;
-	int (*rint) __P((int c, struct tty *tp)) = tp->t_linesw->l_rint;
+	int (*rint)(int, struct tty *) = tp->t_linesw->l_rint;
 	u_char *get, *end;
 	u_int cc, scc;
 	u_char rr1;
