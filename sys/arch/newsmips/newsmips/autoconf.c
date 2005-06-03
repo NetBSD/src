ad54e60bcbe98bf53d563e0162c57f14ff479403
tsutsui <tsutsui@NetBSD.org>
2005-06-03 13:45:58 +0000

    Fix config_rootfound() arg.

--- sys/arch/newsmips/newsmips/autoconf.c
+++ sys/arch/newsmips/newsmips/autoconf.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: autoconf.c,v 1.24 2005/02/06 02:18:02 tsutsui Exp $	*/
+/*	$NetBSD: autoconf.c,v 1.25 2005/06/03 13:45:58 tsutsui Exp $	*/
 
 /*
  * Copyright (c) 1992, 1993
@@ -85,7 +85,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: autoconf.c,v 1.24 2005/02/06 02:18:02 tsutsui Exp $");
+__KERNEL_RCSID(0, "$NetBSD: autoconf.c,v 1.25 2005/06/03 13:45:58 tsutsui Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -142,7 +142,7 @@ cpu_configure(void)
 		*(char *)INTEN1 = 0;
 	}
 
-	if (config_rootfound("mainbus", "mainbus") == NULL)
+	if (config_rootfound("mainbus", NULL) == NULL)
 		panic("no mainbus found");
 
 	/* Enable hardware interrupt registers. */
