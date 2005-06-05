8440da55514d6c64e9b473be1b641194dc35f75f
he <he>
2005-06-05 11:35:09 +0000

    Adapt to -Wcast-qual by adding a few consts.

--- sys/arch/next68k/next68k/disksubr.c
+++ sys/arch/next68k/next68k/disksubr.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: disksubr.c,v 1.17 2005/01/19 01:58:21 chs Exp $	*/
+/*	$NetBSD: disksubr.c,v 1.18 2005/06/05 11:35:09 he Exp $	*/
 
 /*
  * Copyright (c) 1982, 1986, 1988, 1993
@@ -37,7 +37,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: disksubr.c,v 1.17 2005/01/19 01:58:21 chs Exp $");
+__KERNEL_RCSID(0, "$NetBSD: disksubr.c,v 1.18 2005/06/05 11:35:09 he Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -54,7 +54,7 @@ __KERNEL_RCSID(0, "$NetBSD: disksubr.c,v 1.17 2005/01/19 01:58:21 chs Exp $");
 #define	b_cylinder	b_resid
 
 static unsigned short nextstep_checksum(unsigned char *, unsigned char *);
-static char *parse_nextstep_label(struct next68k_disklabel *,
+static const char *parse_nextstep_label(struct next68k_disklabel *,
 	struct disklabel *, struct cpu_disklabel *);
 static int build_nextstep_label(struct next68k_disklabel *, struct disklabel *);
 
@@ -73,7 +73,7 @@ nextstep_checksum(buf, limit)
 	return (sum & 0xffff);
 }
 
-static char *
+static const char *
 parse_nextstep_label(ondisk, lp, osdep)
 	struct next68k_disklabel *ondisk;
 	struct disklabel *lp;
@@ -325,7 +325,7 @@ readdisklabel(dev, strat, lp, osdep)
 {
 	struct buf *bp;
 	struct disklabel *dlp;
-	char *msg = NULL;
+	const char *msg = NULL;
 	int i;
 
 	/* minimal requirements for archtypal disk label */
