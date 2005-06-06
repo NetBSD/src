092980f9901ca6f14f29698b7285774e0361b96a
tron <tron@NetBSD.org>
2005-06-06 12:16:46 +0000

    Pull up revision 1.147 (requested by chs in ticket #424): in pmap_enter(), preset the mod/ref bits based on the flags argument. fixes 25640.

--- sys/arch/sun3/sun3/pmap.c
+++ sys/arch/sun3/sun3/pmap.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: pmap.c,v 1.145 2005/01/22 15:36:10 chs Exp $	*/
+/*	$NetBSD: pmap.c,v 1.145.6.1 2005/06/06 12:16:46 tron Exp $	*/
 
 /*-
  * Copyright (c) 1996 The NetBSD Foundation, Inc.
@@ -87,7 +87,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: pmap.c,v 1.145 2005/01/22 15:36:10 chs Exp $");
+__KERNEL_RCSID(0, "$NetBSD: pmap.c,v 1.145.6.1 2005/06/06 12:16:46 tron Exp $");
 
 #include "opt_ddb.h"
 #include "opt_pmap_debug.h"
@@ -1997,6 +1997,12 @@ pmap_enter(pmap_t pmap, vaddr_t va, paddr_t pa, vm_prot_t prot, int flags)
 	new_pte |= PG_VALID;
 	if (prot & VM_PROT_WRITE)
 		new_pte |= PG_WRITE;
+	if (flags & VM_PROT_ALL) {
+		new_pte |= PG_REF;
+		if (flags & VM_PROT_WRITE) {
+			new_pte |= PG_MOD;
+		}
+	}
 
 	/* ...and finally the page-frame number. */
 	new_pte |= PA_PGNUM(pa);
