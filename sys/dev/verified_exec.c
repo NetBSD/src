d1ff4d2f003b6049b72844723db2923782f21551
christos <christos@NetBSD.org>
2005-06-06 02:48:20 +0000

    fix a printf format argument type.

--- sys/dev/verified_exec.c
+++ sys/dev/verified_exec.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: verified_exec.c,v 1.11 2005/06/03 13:21:35 elad Exp $	*/
+/*	$NetBSD: verified_exec.c,v 1.12 2005/06/06 02:48:20 christos Exp $	*/
 
 /*-
  * Copyright 2005 Elad Efrat <elad@bsd.org.il>
@@ -31,9 +31,9 @@
 
 #include <sys/cdefs.h>
 #if defined(__NetBSD__)
-__KERNEL_RCSID(0, "$NetBSD: verified_exec.c,v 1.11 2005/06/03 13:21:35 elad Exp $");
+__KERNEL_RCSID(0, "$NetBSD: verified_exec.c,v 1.12 2005/06/06 02:48:20 christos Exp $");
 #else
-__RCSID("$Id: verified_exec.c,v 1.11 2005/06/03 13:21:35 elad Exp $\n$NetBSD: verified_exec.c,v 1.11 2005/06/03 13:21:35 elad Exp $");
+__RCSID("$Id: verified_exec.c,v 1.12 2005/06/06 02:48:20 christos Exp $\n$NetBSD: verified_exec.c,v 1.12 2005/06/06 02:48:20 christos Exp $");
 #endif
 
 #include <sys/param.h>
@@ -290,7 +290,7 @@ veriexecioctl(dev_t dev __unused, u_long cmd, caddr_t data,
 			       "\"%s\" (dev=%lu, inode=%lu), size was %u "
 			       "was expecting %lu\n", params->fp_type,
 			       params->file, va.va_fsid, va.va_fileid,
-			       params->size, e->ops->hash_len);
+			       params->size, (unsigned long)e->ops->hash_len);
 			free(e, M_TEMP);
 			return(EINVAL);
 		}
