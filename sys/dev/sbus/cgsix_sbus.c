67bfaa22d2c4e1298ac9e0907921f897e3c0787e
tron <tron@NetBSD.org>
2005-06-08 11:53:59 +0000

    Pull up revision 1.16 (requested by martin in ticket #378): Added support for virtual consoles, colour and some code to determine the usable amount of VRAM for XFree86

--- sys/dev/sbus/cgsix_sbus.c
+++ sys/dev/sbus/cgsix_sbus.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: cgsix_sbus.c,v 1.15 2005/02/25 16:03:09 martin Exp $ */
+/*	$NetBSD: cgsix_sbus.c,v 1.15.2.1 2005/06/08 11:53:59 tron Exp $ */
 
 /*-
  * Copyright (c) 1998 The NetBSD Foundation, Inc.
@@ -41,7 +41,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: cgsix_sbus.c,v 1.15 2005/02/25 16:03:09 martin Exp $");
+__KERNEL_RCSID(0, "$NetBSD: cgsix_sbus.c,v 1.15.2.1 2005/06/08 11:53:59 tron Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -110,7 +110,7 @@ cgsixattach(parent, self, aux)
 	struct sbusdev *sd = &((struct cgsix_sbus_softc *)self)->bss_sd;
 	struct sbus_attach_args *sa = aux;
 	struct fbdevice *fb = &sc->sc_fb;
-	int node, isconsole, ramsize;
+	int node, isconsole;
 	char *name;
 	bus_space_handle_t bh;
 
@@ -119,7 +119,7 @@ cgsixattach(parent, self, aux)
 	sc->sc_paddr = sbus_bus_addr(sa->sa_bustag, sa->sa_slot, sa->sa_offset);
 
 	node = sa->sa_node;
-
+	
 	fb->fb_device = &sc->sc_dev;
 	fb->fb_type.fb_type = FBTYPE_SUNFAST_COLOR;
 	fb->fb_flags = sc->sc_dev.dv_cfdata->cf_flags & FB_USERMASK;
@@ -186,16 +186,17 @@ cgsixattach(parent, self, aux)
 	name = prom_getpropstring(node, "model");
 
 	isconsole = fb_is_console(node);
+
 	/* we need the address of the framebuffer, no matter if we're console or not. */
-	ramsize = fb->fb_type.fb_height * fb->fb_linebytes;
-		if (sbus_bus_map(sa->sa_bustag,
-				 sa->sa_slot,
-				 sa->sa_offset + CGSIX_RAM_OFFSET,
-				 ramsize,
-				 BUS_SPACE_MAP_LINEAR, &bh) != 0) {
-			printf("%s: cannot map pixels\n", self->dv_xname);
-			return;
-		}
+	sc->sc_ramsize = prom_getpropint(node, "fbmapped", 1024 * 1024);
+	if (sbus_bus_map(sa->sa_bustag,
+			sa->sa_slot,
+			sa->sa_offset + CGSIX_RAM_OFFSET,
+			sc->sc_ramsize,
+			BUS_SPACE_MAP_LINEAR, &bh) != 0) {
+		printf("%s: cannot map pixels\n", self->dv_xname);
+		return;
+	}
 	sc->sc_fb.fb_pixels = (caddr_t)bus_space_vaddr(sa->sa_bustag, bh);
 
 	cg6attach(sc, name, isconsole);
