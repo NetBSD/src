deab8761cd04d14ac01f6adab24eeab4d5cdd3fa
he <he>
2005-06-09 08:05:31 +0000

    Adapt to compiling with -Wcast-qual and -Wshadow by adding const in appropriate places and renaming a few local variables.

--- sys/arch/acorn26/acorn26/start.c
+++ sys/arch/acorn26/acorn26/start.c
@@ -1,4 +1,4 @@
-/* $NetBSD: start.c,v 1.5 2003/12/30 12:33:13 pk Exp $ */
+/* $NetBSD: start.c,v 1.6 2005/06/09 08:05:31 he Exp $ */
 /*-
  * Copyright (c) 1998, 2000 Ben Harris
  * All rights reserved.
@@ -31,7 +31,7 @@
 
 #include <sys/param.h>
 
-__KERNEL_RCSID(0, "$NetBSD: start.c,v 1.5 2003/12/30 12:33:13 pk Exp $");
+__KERNEL_RCSID(0, "$NetBSD: start.c,v 1.6 2005/06/09 08:05:31 he Exp $");
 
 #include <sys/msgbuf.h>
 #include <sys/user.h>
@@ -69,8 +69,8 @@ struct user *proc0paddr;
 extern i2c_tag_t acorn26_i2c_tag;
 
 /* We don't pass a command line yet. */
-char *boot_args = "";
-char *boot_file = "";
+const char *boot_args = "";
+const char *boot_file = "";
 
 #ifdef DIAGNOSTIC
 #define BOOT_SANITY 0x89345846
