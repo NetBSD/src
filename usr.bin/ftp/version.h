a8404ea2964e24f6cce570c5d6b2ec7880fdb504
lukem <lukem@NetBSD.org>
2005-06-10 00:18:46 +0000

    Implement:     int getline(FILE *stream, char *buf, size_t buflen, const char **errormsg) 	Read a line from the FILE stream into buf/buflen using fgets(), so up 	to buflen-1 chars will be read and the result will be NUL terminated. 	If the line has a trailing newline it will be removed. 	If the line is too long, excess characters will be read until 	newline/EOF/error. 	Various -ve return values indicate different errors, and errormsg 	will be changed to an error description if it's not NULL.
    Convert to use getline() instead of fgets() whenever reading user input
    to ensure that an overly long input line doesn't leave excess characters
    for the next input operation to accidentally use as input.
    
    Zero out the password & account after we've finished with it.
    
    Consistently use getpass(3) (i.e, character echo suppressed) when
    reading the account data.  For some reason, historically the "login"
    code suppressed echo for Account: yet the "user" command did not!
    
    Display the hostname in the "getaddrinfo failed" warning.
    
    Appease some -Wcast-qual warnings.  Fixing all of these requires
    significant code refactoring.  (mmm, legacy code).

--- usr.bin/ftp/version.h
+++ usr.bin/ftp/version.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: version.h,v 1.54 2005/06/01 12:10:14 lukem Exp $	*/
+/*	$NetBSD: version.h,v 1.55 2005/06/10 00:18:47 lukem Exp $	*/
 /*-
  * Copyright (c) 1999-2005 The NetBSD Foundation, Inc.
  * All rights reserved.
@@ -40,5 +40,5 @@
 #endif
 
 #ifndef FTP_VERSION
-#define	FTP_VERSION	"20050601"
+#define	FTP_VERSION	"20050610"
 #endif
