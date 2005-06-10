2597501205716b3552614adb45eb885ae1242907
tron <tron@NetBSD.org>
2005-06-10 15:12:11 +0000

    Pull up revision 1.9 (requested by elad in ticket #389): Remove common code for returning supported fingerprints. This is done now via sysctl(8) using kern.veriexec.algorithms. Also add an entry for the 'algorithms' variable in sysctl.8 forgotten in the last commit.

--- sys/dev/verified_exec.c
+++ sys/dev/verified_exec.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: verified_exec.c,v 1.5.2.2 2005/06/10 15:10:10 tron Exp $	*/
+/*	$NetBSD: verified_exec.c,v 1.5.2.3 2005/06/10 15:12:11 tron Exp $	*/
 
 /*-
  * Copyright 2005 Elad Efrat <elad@bsd.org.il>
@@ -31,9 +31,9 @@
 
 #include <sys/cdefs.h>
 #if defined(__NetBSD__)
-__KERNEL_RCSID(0, "$NetBSD: verified_exec.c,v 1.5.2.2 2005/06/10 15:10:10 tron Exp $");
+__KERNEL_RCSID(0, "$NetBSD: verified_exec.c,v 1.5.2.3 2005/06/10 15:12:11 tron Exp $");
 #else
-__RCSID("$Id: verified_exec.c,v 1.5.2.2 2005/06/10 15:10:10 tron Exp $\n$NetBSD: verified_exec.c,v 1.5.2.2 2005/06/10 15:10:10 tron Exp $");
+__RCSID("$Id: verified_exec.c,v 1.5.2.3 2005/06/10 15:12:11 tron Exp $\n$NetBSD: verified_exec.c,v 1.5.2.3 2005/06/10 15:12:11 tron Exp $");
 #endif
 
 #include <sys/param.h>
@@ -154,11 +154,9 @@ veriexecioctl(dev_t dev __unused, u_long cmd, caddr_t data,
 	u_long hashmask;
 
 	/*
-	 * Don't allow updates in multi-user mode, but we will allow
-	 * queries of supported fingerprints.
-	 *
+	 * Don't allow updates in multi-user mode.
 	 */
-	if ((securelevel >= 1) && (cmd != VERIEXEC_FINGERPRINTS)) {
+	if (securelevel >= 1) {
 		printf("Veriexec: veriexecioctl: Securelevel raised, loading"
 		       "fingerprints is not permitted\n");
 
@@ -293,20 +291,6 @@ veriexecioctl(dev_t dev __unused, u_long cmd, caddr_t data,
 		break;
 		}
 
-	case VERIEXEC_FINGERPRINTS: {
-		struct veriexec_fp_report *params =
-			(struct veriexec_fp_report *) data;
-		
-		if (strlen(veriexec_fp_names) >= params->size) {
-			params->size = strlen(veriexec_fp_names) + 1;
-		} else {
-			strlcpy(params->fingerprints, veriexec_fp_names,
-				params->size);
-		}
-		
-		break;
-		}
-	
 	default:
 		/* Invalid operation. */
 		error = ENODEV;
