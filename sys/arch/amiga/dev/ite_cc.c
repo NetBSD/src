a6ce43ea5b11a14e63580b1cafd93987b44a5d63
jmc <jmc@NetBSD.org>
2005-06-13 21:33:15 +0000

    Fix some shadowing of variables

--- sys/arch/amiga/dev/ite_cc.c
+++ sys/arch/amiga/dev/ite_cc.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: ite_cc.c,v 1.35 2004/02/24 15:05:54 wiz Exp $ */
+/*	$NetBSD: ite_cc.c,v 1.36 2005/06/13 21:33:15 jmc Exp $ */
 
 /*
  * Copyright (c) 1994 Christian E. Hopps
@@ -33,7 +33,7 @@
 #include "opt_amigaccgrf.h"
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: ite_cc.c,v 1.35 2004/02/24 15:05:54 wiz Exp $");
+__KERNEL_RCSID(0, "$NetBSD: ite_cc.c,v 1.36 2005/06/13 21:33:15 jmc Exp $");
 
 #include "grfcc.h"
 #if NGRFCC > 0
@@ -418,15 +418,15 @@ cursor32(struct ite_softc *ip, int flag)
 		/*
 		 * erase the cursor
 		 */
-		int h;
+		int hh;
 
 		if (dr_plane) {
-			for (h = cend; h >= 0; h--) {
+			for (hh = cend; hh >= 0; hh--) {
 				BFCLR(pl, ofs, ip->ftwidth);
 				pl += cci->row_offset;
 			}
 		} else {
-			for (h = cend; h >= 0; h--) {
+			for (hh = cend; hh >= 0; hh--) {
 				BFCHG(pl, ofs, ip->ftwidth);
 				pl += cci->row_offset;
 			}
