57e9b73849389ab0e875ca92901a18bf11c53296
tron <tron@NetBSD.org>
2005-06-14 21:16:12 +0000

    Pull up revision 1.67 (requested by wiz in ticket #1989): Add ranges support to the pkg_* tools. See mail on tech-pkg or pkg_info(1) for details. Co-production with dillo@. Bump PACKAGE_TOOLS_VERSION to 20050530.

--- usr.sbin/pkg_install/lib/version.h
+++ usr.sbin/pkg_install/lib/version.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: version.h,v 1.44.4.2 2005/06/14 21:14:16 tron Exp $	*/
+/*	$NetBSD: version.h,v 1.44.4.3 2005/06/14 21:16:12 tron Exp $	*/
 
 /*
  * Copyright (c) 2001 Thomas Klausner.  All rights reserved.
@@ -33,6 +33,6 @@
 #ifndef _INST_LIB_VERSION_H_
 #define _INST_LIB_VERSION_H_
 
-#define PKGTOOLS_VERSION "20050529"
+#define PKGTOOLS_VERSION "20050530"
 
 #endif /* _INST_LIB_VERSION_H_ */
