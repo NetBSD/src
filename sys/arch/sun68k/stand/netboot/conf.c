8f25e4081834354bc114d2dc6c330147ef018e4b
junyoung <junyoung@NetBSD.org>
2005-06-23 19:44:00 +0000

    Use FS_OPS() macro.

--- sys/arch/sun68k/stand/netboot/conf.c
+++ sys/arch/sun68k/stand/netboot/conf.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: conf.c,v 1.1 2001/06/14 12:57:16 fredette Exp $	*/
+/*	$NetBSD: conf.c,v 1.2 2005/06/23 19:44:01 junyoung Exp $	*/
 
 #include <sys/types.h>
 #include <netinet/in.h>
@@ -8,14 +8,14 @@
 #include "dev_net.h"
 
 struct fs_ops file_system[] = {
-	{ nfs_open, nfs_close, nfs_read, nfs_write, nfs_seek, nfs_stat },
+	FS_OPS(nfs),
 };
 int nfsys = 1;
 
 struct devsw devsw[] = {
 	{ "net",  net_strategy,  net_open,  net_close,  net_ioctl },
 };
-int	ndevs = 1;
+int ndevs = 1;
 
 int
 main() {
