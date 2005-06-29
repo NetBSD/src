86178c953542f32fa37de4023ff451172cb456ad
tron <tron>
2005-06-29 12:17:23 +0000

    Pull up revision 1.6 (requested by manu in ticket #489): Consume NAT-T packets that have already been seen through MSG_PEEK

--- crypto/dist/ipsec-tools/src/racoon/isakmp.c
+++ crypto/dist/ipsec-tools/src/racoon/isakmp.c
@@ -1,6 +1,6 @@
-/*	$NetBSD: isakmp.c,v 1.1.1.3.2.4 2005/05/27 23:00:33 riz Exp $	*/
+/*	$NetBSD: isakmp.c,v 1.1.1.3.2.5 2005/06/29 12:17:23 tron Exp $	*/
 
-/* $Id: isakmp.c,v 1.1.1.3.2.4 2005/05/27 23:00:33 riz Exp $ */
+/* $Id: isakmp.c,v 1.1.1.3.2.5 2005/06/29 12:17:23 tron Exp $ */
 
 /*
  * Copyright (C) 1995, 1996, 1997, and 1998 WIDE Project.
@@ -214,8 +214,16 @@ isakmp_handler(so_isakmp)
 	}
 
 	/* keep-alive packet - ignore */
-	if (len == 1 && (x.buf[0]&0xff) == 0xff)
+	if (len == 1 && (x.buf[0]&0xff) == 0xff) {
+		/* Pull the keep-alive packet */
+		if ((len = recvfrom(so_isakmp, (char *)x.buf, 1,
+		    0, (struct sockaddr *)&remote, &remote_len)) != 1) {
+			plog(LLV_ERROR, LOCATION, NULL,
+			    "failed to receive keep alive packet: %s\n",
+			    strerror (errno));
+		}
 		goto end;
+	}
 
 #ifdef ENABLE_NATT
 	/* we don't know about portchange yet, 
