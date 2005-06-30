97435e8120a152eabc2ff126f11f1be8a1c66851
simonb <simonb@NetBSD.org>
2005-06-30 07:47:29 +0000

    We don't need a.out loadfile support for evbppc - this port has only ever been ELF.

--- sys/arch/evbppc/include/loadfile_machdep.h
+++ sys/arch/evbppc/include/loadfile_machdep.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: loadfile_machdep.h,v 1.1 2002/12/09 12:16:08 scw Exp $	*/
+/*	$NetBSD: loadfile_machdep.h,v 1.2 2005/06/30 07:47:29 simonb Exp $	*/
 
 /*-
  * Copyright (c) 1999 The NetBSD Foundation, Inc.
@@ -36,7 +36,6 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
-#define BOOT_AOUT
 #define BOOT_ELF32
 
 #define LOAD_KERNEL	(LOAD_ALL & ~LOAD_TEXTA)
