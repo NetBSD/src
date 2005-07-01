03734870812a82c6e16b4aa5173f2c246fa01040
tron <tron@NetBSD.org>
2005-07-01 12:28:20 +0000

    Pull up revision 1.80 (requested by rpaulo in ticket #493): Add support for ASIX AX88140A/AX88141 chipsets using only the 10/100 MII with inspiration from FreeBSD dc(4) driver. Tested on i386 and macppc. Reviewed by Jason Thorpe and ok'ed by Hubert Feyrer.

--- sys/dev/pci/if_tlp_pci.c
+++ sys/dev/pci/if_tlp_pci.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: if_tlp_pci.c,v 1.78.2.1 2005/06/18 05:56:59 riz Exp $	*/
+/*	$NetBSD: if_tlp_pci.c,v 1.78.2.2 2005/07/01 12:28:20 tron Exp $	*/
 
 /*-
  * Copyright (c) 1998, 1999, 2000, 2002 The NetBSD Foundation, Inc.
@@ -43,7 +43,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: if_tlp_pci.c,v 1.78.2.1 2005/06/18 05:56:59 riz Exp $");
+__KERNEL_RCSID(0, "$NetBSD: if_tlp_pci.c,v 1.78.2.2 2005/07/01 12:28:20 tron Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -169,10 +169,8 @@ static const struct tulip_pci_product {
 	{ PCI_VENDOR_3COM,		PCI_PRODUCT_3COM_3C910SOHOB,
 	  TULIP_CHIP_AN985 },
 
-#if 0
 	{ PCI_VENDOR_ASIX,		PCI_PRODUCT_ASIX_AX88140A,
 	  TULIP_CHIP_AX88140 },
-#endif
 
 	{ 0,				0,
 	  TULIP_CHIP_INVALID },
@@ -486,6 +484,8 @@ tlp_pci_attach(struct device *parent, struct device *self, void *aux)
 	case TULIP_CHIP_MX98725:
 	case TULIP_CHIP_DM9102:
 	case TULIP_CHIP_DM9102A:
+	case TULIP_CHIP_AX88140:
+	case TULIP_CHIP_AX88141:
 		/*
 		 * Clear the "sleep mode" bit in the CFDA register.
 		 */
@@ -928,6 +928,21 @@ tlp_pci_attach(struct device *parent, struct device *self, void *aux)
 		 * to it.
 		 */
 		sc->sc_mediasw = &tlp_dm9102_mediasw;
+		break;
+
+	case TULIP_CHIP_AX88140:
+	case TULIP_CHIP_AX88141:
+		/*
+		 * ASIX AX88140/AX88141 Ethernet Address is located at offset
+		 * 20 of the SROM.
+		 */
+		memcpy(enaddr, &sc->sc_srom[20], ETHER_ADDR_LEN);
+
+		/*
+		 * ASIX AX88140A/AX88141 chip can have a built-in PHY or
+		 * an external MII interface.
+		 */
+		sc->sc_mediasw = &tlp_asix_mediasw;
 		break;
 
 	default:
