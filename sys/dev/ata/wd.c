4e13567a1be4ec8a625c433e74f8365b53d1d8a6
tron <tron@NetBSD.org>
2005-07-02 18:12:36 +0000

    Pull up revision 1.304 (requested by bouyer in ticket #497): The ST3160023A* also need the LBA48 quirk. From Timo Schoeler.

--- sys/dev/ata/wd.c
+++ sys/dev/ata/wd.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: wd.c,v 1.298.2.4 2005/07/02 18:09:12 tron Exp $ */
+/*	$NetBSD: wd.c,v 1.298.2.5 2005/07/02 18:12:36 tron Exp $ */
 
 /*
  * Copyright (c) 1998, 2001 Manuel Bouyer.  All rights reserved.
@@ -66,7 +66,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: wd.c,v 1.298.2.4 2005/07/02 18:09:12 tron Exp $");
+__KERNEL_RCSID(0, "$NetBSD: wd.c,v 1.298.2.5 2005/07/02 18:12:36 tron Exp $");
 
 #ifndef ATADEBUG
 #define ATADEBUG
@@ -235,6 +235,8 @@ static const struct wd_quirk {
 	 * (aka LBA48_THRESHOLD) in LBA mode. The workaround is to force
 	 * LBA48
 	 */
+	{ "ST3160023A*",
+	  WD_QUIRK_FORCE_LBA48 },
 	{ "ST3200822A*",
 	  WD_QUIRK_FORCE_LBA48 },
 	{ "ST3250823A*",
