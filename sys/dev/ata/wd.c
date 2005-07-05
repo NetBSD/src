5f9524ec96b48a621168383fa2a77fa5add3edad
riz <riz@NetBSD.org>
2005-07-05 19:11:05 +0000

    Pull up revision 1.303 via patch (requested by bouyer in ticket #2027): Use pmatch() metacharacters to match both SATA and PATA versions of seagate drives in the quirk table.

--- sys/dev/ata/wd.c
+++ sys/dev/ata/wd.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: wd.c,v 1.274.2.8.2.2 2005/05/16 16:15:10 riz Exp $ */
+/*	$NetBSD: wd.c,v 1.274.2.8.2.3 2005/07/05 19:11:05 riz Exp $ */
 
 /*
  * Copyright (c) 1998, 2001 Manuel Bouyer.  All rights reserved.
@@ -66,7 +66,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: wd.c,v 1.274.2.8.2.2 2005/05/16 16:15:10 riz Exp $");
+__KERNEL_RCSID(0, "$NetBSD: wd.c,v 1.274.2.8.2.3 2005/07/05 19:11:05 riz Exp $");
 
 #ifndef WDCDEBUG
 #define WDCDEBUG
@@ -233,9 +233,9 @@ static const struct wd_quirk {
 	 * (aka LBA48_THRESHOLD) in LBA mode. The workaround is to force
 	 * LBA48
 	 */
-	{ "ST3200822A",
+	{ "ST3200822A*",
 	  WD_QUIRK_FORCE_LBA48 },
-	{ "ST3250823AS",
+	{ "ST3250823A*",
 	  WD_QUIRK_FORCE_LBA48 },
 
 	{ NULL,
