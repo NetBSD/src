c5698322fe4726c24c06f46359c4e5a8f2166e4e
christos <christos@NetBSD.org>
2005-07-10 17:02:51 +0000

    Now that sparc has syscall intern, no need to define it here too.

--- sys/arch/sparc64/include/types.h
+++ sys/arch/sparc64/include/types.h
@@ -1,5 +1,3 @@
-/*       $NetBSD: types.h,v 1.23 2005/07/10 00:50:16 christos Exp $        */
+/*       $NetBSD: types.h,v 1.24 2005/07/10 17:02:51 christos Exp $        */
 
 #include <sparc/types.h>
-
-#define __HAVE_SYSCALL_INTERN
