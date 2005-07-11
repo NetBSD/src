d48dca994c3aad8b2eeafdd27ad676c7b419d3ee
christos <christos@NetBSD.org>
2005-07-11 13:05:33 +0000

    new file

---
+++ sys/arch/hp300/include/sunos_machdep.h
@@ -0,0 +1,3 @@
+/*	$NetBSD: sunos_machdep.h,v 1.1 2005/07/11 13:08:09 christos Exp $	*/
+
+#include <m68k/sunos_machdep.h>
---
+++ sys/arch/sparc64/include/sunos_machdep.h
@@ -0,0 +1,3 @@
+/*       $NetBSD: sunos_machdep.h,v 1.1 2005/07/11 13:05:33 christos Exp $        */
+
+#include <sparc/sunos_machdep.h>
