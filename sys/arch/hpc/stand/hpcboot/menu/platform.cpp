6b5e26e63e95166258099f7042c81b5d236a0956
uwe <uwe@NetBSD.org>
2005-07-12 23:21:54 +0000

    XXX: kludge around constification fallout. No way I'm dealing with constification of C++ code!

--- sys/arch/hpc/stand/hpcboot/menu/platform.cpp
+++ sys/arch/hpc/stand/hpcboot/menu/platform.cpp
@@ -1,4 +1,4 @@
-/* -*-C++-*-	$NetBSD: platform.cpp,v 1.3 2004/08/06 18:33:09 uch Exp $	*/
+/* -*-C++-*-	$NetBSD: platform.cpp,v 1.4 2005/07/12 23:21:54 uwe Exp $	*/
 
 /*-
  * Copyright (c) 2001 The NetBSD Foundation, Inc.
@@ -54,7 +54,9 @@ HpcMenuInterface::_platform(int n, enum _platform_op op)
 		switch(op) {
 		case _PLATFORM_OP_GET:
 			if (n == cnt)
-				return reinterpret_cast <void *>(name->name);
+				// XXX: constification fallout
+				return reinterpret_cast <void *>
+					(const_cast <tchar *>(name->name));
 			break;
 		case _PLATFORM_OP_SET:
 			if (n == cnt) {
