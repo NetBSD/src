1001d345e9a97b3f3ea5846e93f6eb1d181098bd
manu <manu@NetBSD.org>
2005-07-12 16:24:29 +0000

    Don't wipe out IKE ports for SA update as it breaks things: the SA is taken from an existing SA and already has matching IKE ports.

--- crypto/dist/ipsec-tools/src/racoon/pfkey.c
+++ crypto/dist/ipsec-tools/src/racoon/pfkey.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: pfkey.c,v 1.5 2005/07/12 14:14:46 manu Exp $	*/
+/*	$NetBSD: pfkey.c,v 1.6 2005/07/12 16:24:29 manu Exp $	*/
 
 /* Id: pfkey.c,v 1.31.2.1 2005/02/18 10:01:40 vanhu Exp */
 
@@ -1061,10 +1061,6 @@ pk_sendupdate(iph2)
 			natt.frag = iph2->ph1->rmconf->esp_frag;
 		} else {
 			memset (&natt, 0, sizeof (natt));
-
-			/* Remove port information, that SA doesn't use it */
-			set_port(src, 0);
-			set_port(dst, 0);
 		}
 
 		if (pfkey_send_update_nat(
@@ -1088,10 +1084,6 @@ pk_sendupdate(iph2)
 			return -1;
 		}
 #else
-		/* Remove port information, it is not used without NAT-T */
-		set_port(src, 0);
-		set_port(dst, 0);
-
 		plog(LLV_DEBUG, LOCATION, NULL, "call pfkey_send_update\n");
 		if (pfkey_send_update(
 				lcconf->sock_pfkey,
