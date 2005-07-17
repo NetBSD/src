b99cd3ae219b618014514fbe2e3b56ff806dfef7
yamt <yamt@NetBSD.org>
2005-07-17 12:27:47 +0000

    ensure that vnodes with dirty pages are always on syncer's queue.
    - genfs_putpages: wait for i/o completion of PG_RELEASED/PG_PAGEOUT pages by
      setting "wasclean" false when encountering them.
      suggested by Stephan Uphoff in PR/24596 (1).
    
    - genfs_putpages: write protect pages when cleaning out, if
      we're going to take the vnode off the syncer's queue.
      uvm_fault: don't write-map pages unless its vnode is already on
      the syncer's queue.
    
      fix PR/24596 (3) but in the different way from the suggested fix.
      (to keep our current behaviour, ie. not to require explicit msync.
      discussed on tech-kern@.)
    
    - genfs_putpages: don't mistakenly take a vnode off the queue
      by introducing a generation number in genfs_node.
      genfs_getpages: increment the generation number.
      suggested by Stephan Uphoff in PR/24596 (2).
    
    - add some assertions.

--- sys/uvm/uvm_fault.c
+++ sys/uvm/uvm_fault.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: uvm_fault.c,v 1.95 2005/06/27 02:19:48 thorpej Exp $	*/
+/*	$NetBSD: uvm_fault.c,v 1.96 2005/07/17 12:27:47 yamt Exp $	*/
 
 /*
  *
@@ -39,7 +39,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: uvm_fault.c,v 1.95 2005/06/27 02:19:48 thorpej Exp $");
+__KERNEL_RCSID(0, "$NetBSD: uvm_fault.c,v 1.96 2005/07/17 12:27:47 yamt Exp $");
 
 #include "opt_uvmhist.h"
 
@@ -50,6 +50,7 @@ __KERNEL_RCSID(0, "$NetBSD: uvm_fault.c,v 1.95 2005/06/27 02:19:48 thorpej Exp $
 #include <sys/malloc.h>
 #include <sys/mman.h>
 #include <sys/user.h>
+#include <sys/vnode.h>
 
 #include <uvm/uvm.h>
 
@@ -961,8 +962,11 @@ ReFault:
 				 */
 				KASSERT((curpg->flags & PG_PAGEOUT) == 0);
 				KASSERT((curpg->flags & PG_RELEASED) == 0);
+				KASSERT(!UVM_OBJ_IS_CLEAN(uobj) ||
+				    (curpg->flags & PG_CLEAN) != 0);
 				readonly = (curpg->flags & PG_RDONLY)
-				    || (curpg->loan_count > 0);
+				    || (curpg->loan_count > 0)
+				    || UVM_OBJ_IS_CLEAN(uobj);
 
 				(void) pmap_enter(ufi.orig_map->pmap, currva,
 				    VM_PAGE_TO_PHYS(curpg),
@@ -1438,6 +1442,8 @@ Case2:
 	 *  - at this point uobjpage could be PG_WANTED (handle later)
 	 */
 
+	KASSERT(uobj == NULL || !UVM_OBJ_IS_CLEAN(uobj) ||
+	    (uobjpage->flags & PG_CLEAN) != 0);
 	if (promote == FALSE) {
 
 		/*
@@ -1453,7 +1459,8 @@ Case2:
 		anon = NULL;
 
 		uvmexp.flt_obj++;
-		if (UVM_ET_ISCOPYONWRITE(ufi.entry))
+		if (UVM_ET_ISCOPYONWRITE(ufi.entry) ||
+		    UVM_OBJ_IS_CLEAN(uobj))
 			enter_prot &= ~VM_PROT_WRITE;
 		pg = uobjpage;		/* map in the actual object */
 
