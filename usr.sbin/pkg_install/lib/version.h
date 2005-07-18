7460b77e0f719a1157f85c5948cd9743bd1e2871
hubertf <hubertf@NetBSD.org>
2005-07-18 09:09:35 +0000

    Bump version to 20050718 for fixed IGNORE_RECOMMENDS handling.

--- usr.sbin/pkg_install/lib/version.h
+++ usr.sbin/pkg_install/lib/version.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: version.h,v 1.67 2005/05/30 13:10:14 wiz Exp $	*/
+/*	$NetBSD: version.h,v 1.68 2005/07/18 09:09:35 hubertf Exp $	*/
 
 /*
  * Copyright (c) 2001 Thomas Klausner.  All rights reserved.
@@ -33,6 +33,6 @@
 #ifndef _INST_LIB_VERSION_H_
 #define _INST_LIB_VERSION_H_
 
-#define PKGTOOLS_VERSION "20050530"
+#define PKGTOOLS_VERSION "20050718"
 
 #endif /* _INST_LIB_VERSION_H_ */
