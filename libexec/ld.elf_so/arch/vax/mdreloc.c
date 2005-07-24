a45c7d037e9e902946c3a750e778c6431802b32f
skrll <skrll>
2005-07-24 08:02:23 +0000

    Support immediate PLT binding on vax. Tested on simh.
    Nearly there...

--- libexec/ld.elf_so/arch/vax/mdreloc.c
+++ libexec/ld.elf_so/arch/vax/mdreloc.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: mdreloc.c,v 1.17 2003/07/24 10:12:30 skrll Exp $	*/
+/*	$NetBSD: mdreloc.c,v 1.18 2005/07/24 08:02:23 skrll Exp $	*/
 
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -9,6 +9,8 @@
 void _rtld_bind_start(void);
 void _rtld_relocate_nonplt_self(Elf_Dyn *, Elf_Addr);
 caddr_t _rtld_bind(const Obj_Entry *, Elf_Word);
+static inline int _rtld_relocate_plt_object(const Obj_Entry *obj,
+    const Elf_Rela *rela, Elf_Addr *tp);
 
 void
 _rtld_setup_pltgot(const Obj_Entry *obj)
@@ -137,10 +139,9 @@ _rtld_relocate_plt_lazy(const Obj_Entry *obj)
 	return 0;
 }
 
-caddr_t
-_rtld_bind(const Obj_Entry *obj, Elf_Word reloff)
+static inline int
+_rtld_relocate_plt_object(const Obj_Entry *obj, const Elf_Rela *rela, Elf_Addr *tp)
 {
-	const Elf_Rela *rela = (const Elf_Rela *)((caddr_t)obj->pltrela + reloff);
 	Elf_Addr *where = (Elf_Addr *)(obj->relocbase + rela->r_offset);
 	Elf_Addr new_value;
 	const Elf_Sym  *def;
@@ -150,7 +151,7 @@ _rtld_bind(const Obj_Entry *obj, Elf_Word reloff)
 
 	def = _rtld_find_symdef(ELF_R_SYM(rela->r_info), obj, &defobj, true);
 	if (def == NULL)
-		_rtld_die();
+		return -1;
 
 	new_value = (Elf_Addr)(defobj->relocbase + def->st_value +
 	    rela->r_addend);
@@ -159,5 +160,34 @@ _rtld_bind(const Obj_Entry *obj, Elf_Word reloff)
 	if (*where != new_value)
 		*where = new_value;
 
-	return (caddr_t)(new_value - rela->r_addend);
+	if (tp)
+		*tp = new_value - rela->r_addend;
+
+	return 0;
+}
+
+caddr_t
+_rtld_bind(const Obj_Entry *obj, Elf_Word reloff)
+{
+	const Elf_Rela *rela = (const Elf_Rela *)((caddr_t)obj->pltrela + reloff);
+	Elf_Addr result;
+	int err;
+
+	err = _rtld_relocate_plt_object(obj, rela, &result);
+	if (err)
+		_rtld_die();
+
+	return (caddr_t)result;
+}
+
+int
+_rtld_relocate_plt_objects(const Obj_Entry *obj)
+{
+	const Elf_Rela *rela;
+
+	for (rela = obj->pltrela; rela < obj->pltrelalim; rela++)
+		if (_rtld_relocate_plt_object(obj, rela, NULL) < 0)
+			return -1;
+
+	return 0;
 }
