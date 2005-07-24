ee18aaf51f7e28b03fa67bb6bfb06699a79f417e
snj <snj>
2005-07-24 00:50:33 +0000

    Apply patch (requested by riz in ticket #5516): Sync libcurses with HEAD of 2005-07-22.

--- lib/libcurses/PSD.doc/twinkle1.c
+++ lib/libcurses/PSD.doc/twinkle1.c
@@ -1,60 +1,64 @@
-.\"	$NetBSD: twinkle1.c,v 1.5 2003/08/07 16:44:29 agc Exp $
-.\"
-.\" Copyright (c) 1980, 1993
-.\"	 The Regents of the University of California.  All rights reserved.
-.\"
-.\" Redistribution and use in source and binary forms, with or without
-.\" modification, are permitted provided that the following conditions
-.\" are met:
-.\" 1. Redistributions of source code must retain the above copyright
-.\"    notice, this list of conditions and the following disclaimer.
-.\" 2. Redistributions in binary form must reproduce the above copyright
-.\"    notice, this list of conditions and the following disclaimer in the
-.\"    documentation and/or other materials provided with the distribution.
-.\" 3. Neither the name of the University nor the names of its contributors
-.\"    may be used to endorse or promote products derived from this software
-.\"    without specific prior written permission.
-.\"
-.\" THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
-.\" ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-.\" IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-.\" ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
-.\" FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-.\" DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
-.\" OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
-.\" HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-.\" LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
-.\" OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
-.\" SUCH DAMAGE.
-.\"
-.\"	@(#)twinkle1.c	8.1 (Berkeley) 6/8/93
-.\"
-# include	<curses.h>
-# include	<signal.h>
+/*	$NetBSD: twinkle1.c,v 1.5.4.1 2005/07/24 00:50:34 snj Exp $	*/
+
+/*
+ * 
+ *  Copyright (c) 1980, 1993
+ * 	 The Regents of the University of California.  All rights reserved.
+ * 
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *  1. Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *  2. Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *  3. Neither the name of the University nor the names of its contributors
+ *     may be used to endorse or promote products derived from this software
+ *     without specific prior written permission.
+ * 
+ *  THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ *  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ *  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ *  ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ *  FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ *  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ *  OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ *  HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ *  OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ *  SUCH DAMAGE.
+ * 
+ * 	@(#)twinkle1.c	8.1 (Berkeley) 6/8/93
+ */ 
+#include	<curses.h>
+#include	<signal.h>
 
 /*
  * the idea for this program was a product of the imagination of
  * Kurt Schoens.  Not responsible for minds lost or stolen.
  */
 
-# define	NCOLS	80
-# define	NLINES	24
-# define	MAXPATTERNS	4
+#define	NCOLS	80
+#define	NLINES	24
+#define	MAXPATTERNS	4
 
 typedef struct {
 	int	y, x;
 } LOCS;
 
-LOCS	Layout[NCOLS * NLINES];	/* current board layout */
+static LOCS	Layout[NCOLS * NLINES];	/* current board layout */
 
-int	Pattern,		/* current pattern number */
-	Numstars;		/* number of stars in pattern */
+static int	Pattern,		/* current pattern number */
+		Numstars;		/* number of stars in pattern */
 
-char	*getenv();
+static void puton(char);
+static void makeboard(void);
+static int ison(int, int);
+static void die(int);
 
-int	die();
-
-main()
+int
+main(void)
 {
 	srand(getpid());		/* initialize random sequence */
 
@@ -78,12 +82,13 @@ main()
  * We lie and say we used to be at the upper right corner to guarantee
  * absolute addressing.
  */
-die()
+static void
+die(int n)
 {
 	signal(SIGINT, SIG_IGN);
 	mvcur(0, COLS - 1, LINES - 1, 0);
 	endwin();
-	exit(0);
+	exit(n);
 }
 
 
@@ -92,10 +97,11 @@ die()
  * calls ison() to determine if the character is on that pattern
  * or not.
  */
-makeboard()
+static void
+makeboard(void)
 {
-	reg int		y, x;
-	reg LOCS	*lp;
+	int		y, x;
+	LOCS	*lp;
 
 	Pattern = rand() % MAXPATTERNS;
 	lp = Layout;
@@ -112,9 +118,9 @@ makeboard()
 /*
  * Return TRUE if (y, x) is on the current pattern.
  */
-ison(y, x)
-reg int	y, x; {
-
+static int
+ison(int y, int x)
+{
 	switch (Pattern) {
 	  case 0:	/* alternating lines */
 		return !(y & 01);
@@ -132,13 +138,13 @@ reg int	y, x; {
 	/* NOTREACHED */
 }
 
-puton(ch)
-reg char	ch;
+static void
+puton(char ch)
 {
-	reg LOCS	*lp;
-	reg int		r;
-	reg LOCS	*end;
-	LOCS		temp;
+	LOCS	*lp;
+	int	r;
+	LOCS	*end;
+	LOCS	temp;
 
 	end = &Layout[Numstars];
 	for (lp = Layout; lp < end; lp++) {
