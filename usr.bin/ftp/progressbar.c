3976c67c720eb53299cd6b03e73f1a52366f7eea
tron <tron@NetBSD.org>
2005-07-24 10:28:20 +0000

    Pull up revision 1.10 (requested by lukem in ticket #606): copyright maintenance

--- usr.bin/ftp/progressbar.c
+++ usr.bin/ftp/progressbar.c
@@ -1,7 +1,7 @@
-/*	$NetBSD: progressbar.c,v 1.6.2.3 2005/07/24 10:22:59 tron Exp $	*/
+/*	$NetBSD: progressbar.c,v 1.6.2.4 2005/07/24 10:28:20 tron Exp $	*/
 
 /*-
- * Copyright (c) 1997-2003 The NetBSD Foundation, Inc.
+ * Copyright (c) 1997-2005 The NetBSD Foundation, Inc.
  * All rights reserved.
  *
  * This code is derived from software contributed to The NetBSD Foundation
@@ -38,7 +38,7 @@
 
 #include <sys/cdefs.h>
 #ifndef lint
-__RCSID("$NetBSD: progressbar.c,v 1.6.2.3 2005/07/24 10:22:59 tron Exp $");
+__RCSID("$NetBSD: progressbar.c,v 1.6.2.4 2005/07/24 10:28:20 tron Exp $");
 #endif /* not lint */
 
 /*
