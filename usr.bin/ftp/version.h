ab8d76bed795a2f077a6b299c7987d62031e54b4
tron <tron>
2005-07-24 10:36:16 +0000

    Pull up revision 1.56 (requested by lukem in ticket #606): Revert back to using an int (instead of size_t) for holding a value that may be negative. Fixes progressbar display on terminals <43 columns wide. Bug noted and solution suggested by Gavan Fantom.

--- usr.bin/ftp/version.h
+++ usr.bin/ftp/version.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: version.h,v 1.44.2.11 2005/07/24 10:30:09 tron Exp $	*/
+/*	$NetBSD: version.h,v 1.44.2.12 2005/07/24 10:36:16 tron Exp $	*/
 /*-
  * Copyright (c) 1999-2005 The NetBSD Foundation, Inc.
  * All rights reserved.
@@ -40,5 +40,5 @@
 #endif
 
 #ifndef FTP_VERSION
-#define	FTP_VERSION	"20050610"
+#define	FTP_VERSION	"20050718"
 #endif
