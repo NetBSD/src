ef679c5cd3ec470b62c36da6a043893a6a7b9bf9
cube <cube@NetBSD.org>
2005-07-29 13:13:34 +0000

    New Dell Day, continued:  add support for the fxp(4) variant in the 802801G* family of SB.

--- sys/dev/pci/if_fxp_pci.c
+++ sys/dev/pci/if_fxp_pci.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: if_fxp_pci.c,v 1.41 2005/05/18 20:33:46 riz Exp $	*/
+/*	$NetBSD: if_fxp_pci.c,v 1.42 2005/07/29 13:13:34 cube Exp $	*/
 
 /*-
  * Copyright (c) 1997, 1998, 1999, 2000, 2001 The NetBSD Foundation, Inc.
@@ -43,7 +43,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: if_fxp_pci.c,v 1.41 2005/05/18 20:33:46 riz Exp $");
+__KERNEL_RCSID(0, "$NetBSD: if_fxp_pci.c,v 1.42 2005/07/29 13:13:34 cube Exp $");
 
 #include "rnd.h"
 
@@ -150,6 +150,8 @@ static const struct fxp_pci_product {
 	  "Intel 82801EB/ER (ICH5) Network Controller" },
 	{ PCI_PRODUCT_INTEL_82801FB_LAN,
 	  "Intel 82562EZ (ICH6)" },
+	{ PCI_PRODUCT_INTEL_82801G_LAN,
+	  "Intel 82801GB/GR (ICH7) Network Controller" },
 	{ 0,
 	  NULL },
 };
@@ -405,6 +407,7 @@ fxp_pci_attach(struct device *parent, struct device *self, void *aux)
 	case PCI_PRODUCT_INTEL_PRO_100_VM_6:
 	case PCI_PRODUCT_INTEL_82801EB_LAN:
 	case PCI_PRODUCT_INTEL_82801FB_LAN:
+	case PCI_PRODUCT_INTEL_82801G_LAN:
 	default:
 		aprint_normal(": %s, rev %d\n", fpp->fpp_name, sc->sc_rev);
 
