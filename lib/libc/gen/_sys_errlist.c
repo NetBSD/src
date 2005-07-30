88b18a556613d9e57b4cbb19be31adb55d04c6a4
christos <christos>
2005-07-30 15:21:20 +0000

    - Fix the remaining indr_references so that they define lint symbols. - Add an internal symbol for strerror_r (thanks klaus for noticing) - Remove internal __strerror

--- lib/libc/gen/_sys_errlist.c
+++ lib/libc/gen/_sys_errlist.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: _sys_errlist.c,v 1.8 2005/06/12 05:34:34 lukem Exp $	*/
+/*	$NetBSD: _sys_errlist.c,v 1.9 2005/07/30 15:21:20 christos Exp $	*/
 
 /*
  * Written by J.T. Conklin, December 12, 1994
@@ -7,7 +7,7 @@
 
 #include <sys/cdefs.h>
 #if defined(LIBC_SCCS) && !defined(lint)
-__RCSID("$NetBSD: _sys_errlist.c,v 1.8 2005/06/12 05:34:34 lukem Exp $");
+__RCSID("$NetBSD: _sys_errlist.c,v 1.9 2005/07/30 15:21:20 christos Exp $");
 #endif /* LIBC_SCCS and not lint */
 
 __warn_references(sys_errlist,
@@ -21,7 +21,7 @@ __warn_references(__sys_nerr,
     "warning: reference to deprecated __sys_nerr; include <errno.h> and use sys_nerr")
  
 
-#ifdef __indr_reference
+#if defined(__indr_reference) && !defined(__lint__)
 __indr_reference(_sys_errlist, sys_errlist)
 __indr_reference(_sys_errlist, __sys_errlist) /* Backwards compat with v.12 */
 #else
