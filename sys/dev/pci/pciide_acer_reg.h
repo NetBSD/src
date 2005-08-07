8f85cb963e83ba0d45b16792fab217ef105339cd
riz <riz@NetBSD.org>
2005-08-07 15:51:32 +0000

    Pull up revision 1.9 via patch (requested by bouyer in ticket #5555): Add an optionnal controller callback for channel reset. If the callback is set to NULL, use the generic reset code. Use this to work around a bug in some Acer IDE controllers (like the one found in some sparc systems) where a controller disable/enable is required after a reset to avoid data corruption when Ultra-DMA is used. Workaround from opensolaris, thanks to Hiroki Sato for testing.

--- sys/dev/pci/pciide_acer_reg.h
+++ sys/dev/pci/pciide_acer_reg.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: pciide_acer_reg.h,v 1.7 2003/10/05 17:48:49 bouyer Exp $	*/
+/*	$NetBSD: pciide_acer_reg.h,v 1.7.6.1 2005/08/07 15:51:32 riz Exp $	*/
 
 /*
  * Copyright (c) 1999 Manuel Bouyer.
@@ -89,6 +89,10 @@
 #define ACER_0x79	0x79
 #define ACER_0x79_REVC2_EN	0x4
 #define ACER_0x79_EN		0x2
+
+/* OpenSolaris: channel enable/disable in the PCI-ISA bridge */
+#define ACER_PCIB_CTRL	0x58
+#define ACER_PCIB_CTRL_ENCHAN(chan) (0x4 << (chan))
 
 /*
  * IDE bus frequency (1 byte)
