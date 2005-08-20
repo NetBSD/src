e6c72964500e6c2077d0982a4718a2712ebc4dd4
kleink <kleink@NetBSD.org>
2005-08-20 14:54:06 +0000

    Add __{BEGIN,END}_DECLS wrapping.

--- sys/sys/sha1.h
+++ sys/sys/sha1.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: sha1.h,v 1.6 2005/04/19 14:04:38 blymn Exp $	*/
+/*	$NetBSD: sha1.h,v 1.7 2005/08/20 14:54:06 kleink Exp $	*/
 
 /*
  * SHA-1 in C
@@ -9,6 +9,7 @@
 #ifndef _SYS_SHA1_H_
 #define	_SYS_SHA1_H_
 
+#include <sys/cdefs.h>
 #include <sys/types.h>
 
 #define SHA1_DIGEST_LENGTH 20
@@ -19,6 +20,7 @@ typedef struct {
 	u_char buffer[64];
 } SHA1_CTX;
 
+__BEGIN_DECLS
 void	SHA1Transform(u_int32_t[5], const u_char[64]);
 void	SHA1Init(SHA1_CTX *);
 void	SHA1Update(SHA1_CTX *, const u_char *, u_int);
@@ -28,5 +30,6 @@ char	*SHA1End(SHA1_CTX *, char *);
 char	*SHA1File(char *, char *);
 char	*SHA1Data(const u_char *, size_t, char *);
 #endif /* _KERNEL */
+__END_DECLS
 
 #endif /* _SYS_SHA1_H_ */
