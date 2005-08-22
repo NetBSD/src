004b0af716050c7945021570ecb5878c914ac693
elad <elad@NetBSD.org>
2005-08-22 15:33:08 +0000

    Make this usable both in kernel and userland.

--- sys/crypto/ripemd160/rmd160.h
+++ sys/crypto/ripemd160/rmd160.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: rmd160.h,v 1.2 2005/04/19 14:05:53 blymn Exp $	*/
+/*	$NetBSD: rmd160.h,v 1.3 2005/08/22 15:33:08 elad Exp $	*/
 /*	$KAME: rmd160.h,v 1.2 2003/07/25 09:37:55 itojun Exp $	*/
 /*	$OpenBSD: rmd160.h,v 1.3 2002/03/14 01:26:51 millert Exp $	*/
 /*
@@ -27,6 +27,9 @@
 #ifndef  _RMD160_H
 #define  _RMD160_H
 
+#include <sys/cdefs.h>
+#include <sys/types.h>
+
 #define RMD160_DIGEST_LENGTH 20
 
 /* RMD160 context. */
@@ -36,9 +39,16 @@ typedef struct RMD160Context {
 	u_char buffer[64];	/* input buffer */
 } RMD160_CTX;
 
+__BEGIN_DECLS
 void	 RMD160Init(RMD160_CTX *);
 void	 RMD160Transform(u_int32_t [5], const u_char [64]);
 void	 RMD160Update(RMD160_CTX *, const u_char *, u_int32_t);
 void	 RMD160Final(u_char [RMD160_DIGEST_LENGTH], RMD160_CTX *);
+#ifndef _KERNEL
+char	*RMD160End(RMD160_CTX *, char *);
+char	*RMD160File(char *, char *);
+char	*RMD160Data(const u_char *, size_t, char *);
+#endif /* _KERNEL */
+__END_DECLS
 
 #endif  /* _RMD160_H */
