c3cacd109efe3a7240247a3d6e9d2b3702937f35
tron <tron@NetBSD.org>
2005-08-23 13:47:41 +0000

    Pull up revision 1.43 (requested by elad in ticket #685): catch up with verified exec defopt changes.

--- sys/kern/exec_script.c
+++ sys/kern/exec_script.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: exec_script.c,v 1.38.10.4 2005/08/23 13:44:06 tron Exp $	*/
+/*	$NetBSD: exec_script.c,v 1.38.10.5 2005/08/23 13:47:41 tron Exp $	*/
 
 /*
  * Copyright (c) 1993, 1994, 1996 Christopher G. Demetriou
@@ -31,7 +31,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: exec_script.c,v 1.38.10.4 2005/08/23 13:44:06 tron Exp $");
+__KERNEL_RCSID(0, "$NetBSD: exec_script.c,v 1.38.10.5 2005/08/23 13:47:41 tron Exp $");
 
 #if defined(SETUIDSCRIPTS) && !defined(FDSCRIPTS)
 #define FDSCRIPTS		/* Need this for safe set-id scripts. */
@@ -255,7 +255,7 @@ check_shell:
 #ifdef VERIFIED_EXEC
 	if ((error = check_exec(p, epp, VERIEXEC_INDIRECT)) == 0) {
 #else
-	if ((error = check_exec(p, epp)) == 0) {
+	if ((error = check_exec(p, epp, 0)) == 0) {
 #endif
 		/* note that we've clobbered the header */
 		epp->ep_flags |= EXEC_DESTR|EXEC_HASES;
