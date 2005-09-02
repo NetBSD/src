f20c174719fddf7957319058d6aa51f942525cd3
rpaulo <rpaulo@NetBSD.org>
2005-09-02 22:52:24 +0000

    Place the sysctl code under an if block and print an error message if the user tries to fetch information via kvm.
    Discussed with Elad Efrat.

--- usr.bin/netstat/bpf.c
+++ usr.bin/netstat/bpf.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: bpf.c,v 1.3 2005/09/02 22:23:13 rpaulo Exp $	*/
+/*	$NetBSD: bpf.c,v 1.4 2005/09/02 22:52:24 rpaulo Exp $	*/
 
 /*
  * Copyright (c) 2005 The NetBSD Foundation, Inc.
@@ -58,108 +58,124 @@ bpf_stats(void)
 	struct bpf_stat bpf_s;
 	size_t len = sizeof(bpf_s);
 
-	if (sysctlbyname("net.bpf.stats", &bpf_s, &len, NULL, 0) == -1)
-		err(1, "net.bpf.stats");
+	if (use_sysctl) {
+		if (sysctlbyname("net.bpf.stats", &bpf_s, &len, NULL, 0) == -1)
+			err(1, "net.bpf.stats");
 	
-	printf("bpf:\n");
-	printf("\t%" PRIu64 " total packets received\n", bpf_s.bs_recv);
-	printf("\t%" PRIu64 " total packets captured\n", bpf_s.bs_capt);
-	printf("\t%" PRIu64 " total packets dropped\n", bpf_s.bs_drop);
+		printf("bpf:\n");
+		printf("\t%" PRIu64 " total packets received\n", 
+		    bpf_s.bs_recv);
+		printf("\t%" PRIu64 " total packets captured\n", 
+		    bpf_s.bs_capt);
+		printf("\t%" PRIu64 " total packets dropped\n", 
+		    bpf_s.bs_drop);
+	} else {
+		/* XXX */
+		errx(1, "bpf_stats not implemented using kvm");
+	}
 }
 
 void
 bpf_dump(char *interface)
 {
-	int    name[CTL_MAXNAME], rc, i;
-	size_t sz, szproc;
-	u_int  namelen;
-	void  *v;
 	struct bpf_d_ext *dpe;
-	struct kinfo_proc2 p;
 
-	/* adapted from sockstat.c by Andrew Brown */
-
-	sz = CTL_MAXNAME;
-	if (sysctlnametomib("net.bpf.peers", &name[0], &sz) == -1)
-		err(1, "sysctlnametomib");
-	namelen = sz;
-
-	name[namelen++] = sizeof(*dpe);
-	name[namelen++] = INT_MAX;
+	if (use_sysctl) {
+		int	name[CTL_MAXNAME], rc, i;
+		size_t	sz, szproc;
+		u_int	namelen;
+		void	*v;
+		struct kinfo_proc2 p;
 	
-	v = NULL;
-	sz = 0;
-	do {
-		rc = sysctl(&name[0], namelen, v, &sz, NULL, 0);
-		if (rc == -1 && errno != ENOMEM)
-			err(1, "sysctl: net.bpf.peers");
-		if (rc == -1 && v != NULL) {
-			free(v);
-			v = NULL;
-		}
-		if (v == NULL) {
-			v = malloc(sz);
-			rc = -1;
-		}
-		if (v == NULL)
-			err(1, "malloc");
-	} while (rc == -1);
+		/* adapted from sockstat.c by Andrew Brown */
 
-	dpe = v;
+		sz = CTL_MAXNAME;
+		if (sysctlnametomib("net.bpf.peers", &name[0], &sz) == -1)
+			err(1, "sysctlnametomib");
+		namelen = sz;
 
-	printf("Active BPF peers\n");
-	printf("PID\tInt\tRecv     Drop     Capt     Flags  Bufsize  Comm\n");
+		name[namelen++] = sizeof(*dpe);
+		name[namelen++] = INT_MAX;
+		
+		v = NULL;
+		sz = 0;
+		do {
+			rc = sysctl(&name[0], namelen, v, &sz, NULL, 0);
+			if (rc == -1 && errno != ENOMEM)
+				err(1, "sysctl: net.bpf.peers");
+			if (rc == -1 && v != NULL) {
+				free(v);
+				v = NULL;
+			}
+			if (v == NULL) {
+				v = malloc(sz);
+				rc = -1;
+			}
+			if (v == NULL)
+				err(1, "malloc");
+		} while (rc == -1);
+
+		dpe = v;
+
+		printf("Active BPF peers\n");
+		printf("PID\tInt\tRecv     Drop     Capt");
+		printf("     Flags  Bufsize  Comm\n");
 
 #define BPFEXT(entry) dpe->entry
 
-	for (i = 0; i < (sz / sizeof(*dpe)); i++, dpe++) {
-		if (interface && 
-		    strncmp(BPFEXT(bde_ifname), interface, IFNAMSIZ))
-			continue;
-		
-		printf("%-7d ", BPFEXT(bde_pid));
-		printf("%-7s ",
-		       (BPFEXT(bde_ifname)[0] == '\0') ? "-" : 
-		       BPFEXT(bde_ifname));
+		for (i = 0; i < (sz / sizeof(*dpe)); i++, dpe++) {
+			if (interface && 
+			    strncmp(BPFEXT(bde_ifname), interface, IFNAMSIZ))
+				continue;
+			
+			printf("%-7d ", BPFEXT(bde_pid));
+			printf("%-7s ",
+			       (BPFEXT(bde_ifname)[0] == '\0') ? "-" : 
+			       BPFEXT(bde_ifname));
 
-		printf("%-8" PRIu64 " %-8" PRIu64 " %-8" PRIu64 " ", 
-			BPFEXT(bde_rcount), BPFEXT(bde_dcount), 
-			BPFEXT(bde_ccount));
-		
-		switch (BPFEXT(bde_state)) {
-		case BPF_IDLE:
-			printf("I");
-			break;
-		case BPF_WAITING:
-			printf("W");
-			break;
-		case BPF_TIMED_OUT:
-			printf("T");
-			break;
-		default:
-			printf("-");
-			break;
-		}
-		
-		printf("%c", BPFEXT(bde_promisc) ? 'P' : '-');
-		printf("%c", BPFEXT(bde_immediate) ? 'R' : '-');
-		printf("%c", BPFEXT(bde_seesent) ? 'S' : '-');
-		printf("%c", BPFEXT(bde_hdrcmplt) ? 'H' : '-');
-		printf("  %-8d ", BPFEXT(bde_bufsize));
+			printf("%-8" PRIu64 " %-8" PRIu64 " %-8" PRIu64 " ", 
+				BPFEXT(bde_rcount), BPFEXT(bde_dcount), 
+				BPFEXT(bde_ccount));
+			
+			switch (BPFEXT(bde_state)) {
+			case BPF_IDLE:
+				printf("I");
+				break;
+			case BPF_WAITING:
+				printf("W");
+				break;
+			case BPF_TIMED_OUT:
+				printf("T");
+				break;
+			default:
+				printf("-");
+				break;
+			}
+			
+			printf("%c", BPFEXT(bde_promisc) ? 'P' : '-');
+			printf("%c", BPFEXT(bde_immediate) ? 'R' : '-');
+			printf("%c", BPFEXT(bde_seesent) ? 'S' : '-');
+			printf("%c", BPFEXT(bde_hdrcmplt) ? 'H' : '-');
+			printf("  %-8d ", BPFEXT(bde_bufsize));
 
-		szproc = sizeof(p);
-		namelen = 0;
-		name[namelen++] = CTL_KERN;
-		name[namelen++] = KERN_PROC2;
-		name[namelen++] = KERN_PROC_PID;
-		name[namelen++] = BPFEXT(bde_pid);
-		name[namelen++] = szproc;
-		name[namelen++] = 1;
+			szproc = sizeof(p);
+			namelen = 0;
+			name[namelen++] = CTL_KERN;
+			name[namelen++] = KERN_PROC2;
+			name[namelen++] = KERN_PROC_PID;
+			name[namelen++] = BPFEXT(bde_pid);
+			name[namelen++] = szproc;
+			name[namelen++] = 1;
 
-		if (sysctl(&name[0], namelen, &p, &szproc, NULL, 0) == -1)
-			printf("-\n");
-		else
-			printf("%s\n", p.p_comm);
+			if (sysctl(&name[0], namelen, &p, &szproc, 
+			    NULL, 0) == -1)
+				printf("-\n");
+			else
+				printf("%s\n", p.p_comm);
 #undef BPFEXT
-	}
+		}
+	} else {
+                /* XXX */
+                errx(1, "bpf_dump not implemented using kvm");
+        }
 }
