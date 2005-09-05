9c1d2b7dbf5e1bb451e8c557a2613c058a92280e
yamt <yamt>
2005-09-05 21:52:38 +0000

    prevent sa_setconcurrency before sa_enable, rather than crashing. (savp_lwp is still null at that point.)
    noted by skrueger at meinberlikomm.de.

--- sys/kern/kern_sa.c
+++ sys/kern/kern_sa.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: kern_sa.c,v 1.63 2005/08/19 02:04:03 christos Exp $	*/
+/*	$NetBSD: kern_sa.c,v 1.64 2005/09/05 21:52:38 yamt Exp $	*/
 
 /*-
  * Copyright (c) 2001, 2004, 2005 The NetBSD Foundation, Inc.
@@ -37,7 +37,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: kern_sa.c,v 1.63 2005/08/19 02:04:03 christos Exp $");
+__KERNEL_RCSID(0, "$NetBSD: kern_sa.c,v 1.64 2005/09/05 21:52:38 yamt Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -551,6 +551,9 @@ sys_sa_setconcurrency(struct lwp *l, void *v, register_t *retval)
 
 	/* We have to be using scheduler activations */
 	if (sa == NULL)
+		return (EINVAL);
+
+	if ((l->l_proc->p_flag & P_SA) == 0)
 		return (EINVAL);
 
 	if (SCARG(uap, concurrency) < 1)
