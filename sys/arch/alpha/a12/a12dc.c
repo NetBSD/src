a23c8461e5a8cb1534ae482bf6680c8193f0c326
kleink <kleink@NetBSD.org>
2005-09-06 20:24:01 +0000

    Change the driver open function's conditional for overriding exclusive tty use from checking the proc's uid to suser(9), and account for the use of privileges.  Noted by David Holland in PR kern/31126.
    Also, dispose of a redundant instance of that check.

--- sys/arch/alpha/a12/a12dc.c
+++ sys/arch/alpha/a12/a12dc.c
@@ -1,4 +1,4 @@
-/* $NetBSD: a12dc.c,v 1.12 2004/08/30 15:05:15 drochner Exp $ */
+/* $NetBSD: a12dc.c,v 1.13 2005/09/06 20:24:01 kleink Exp $ */
 
 /* [Notice revision 2.2]
  * Copyright (c) 1997, 1998 Avalon Computer Systems, Inc.
@@ -64,7 +64,7 @@
 #ifndef BSIDE
 #include <sys/cdefs.h>			/* RCS ID & Copyright macro defns */
 
-__KERNEL_RCSID(0, "$NetBSD: a12dc.c,v 1.12 2004/08/30 15:05:15 drochner Exp $");
+__KERNEL_RCSID(0, "$NetBSD: a12dc.c,v 1.13 2005/09/06 20:24:01 kleink Exp $");
 
 #include <sys/types.h>
 #include <sys/param.h>
@@ -163,7 +163,7 @@ a12dcattach(parent, self, aux)
 	/* note that we've attached the chipset; can't have 2 A12Cs. */
 	a12dcfound = 1;
 
-	printf(": driver %s\n", "$Revision: 1.12 $");
+	printf(": driver %s\n", "$Revision: 1.13 $");
 
 	tp = a12dc_tty[0] = ttymalloc();
 	tp->t_oproc = a12dcstart;
@@ -286,7 +286,7 @@ a12dcopen(dev, flag, mode, p)
 
 	if ((tp->t_state & TS_ISOPEN) &&
 	    (tp->t_state & TS_XCLUDE) &&
-	    p->p_ucred->cr_uid != 0)
+	    suser(p->p_ucred, &p->p_acflag) != 0)
 		return EBUSY;
 
 	s = spltty();
@@ -306,9 +306,6 @@ a12dcopen(dev, flag, mode, p)
 		/* XXX XXX XXX
 		a12_intr_register_icw(a12dcintr);
 		*/
-	} else if (tp->t_state&TS_XCLUDE && p->p_ucred->cr_uid != 0) {
-		splx(s);
-		return EBUSY;
 	}
 
 	splx(s);
