ef160794a42d3b748fde1a565d176d84315c1bc4
riz <riz@NetBSD.org>
2005-09-06 16:12:00 +0000

    Pull up following revision(s) (requested by xtraeme in ticket #5627): 	sys/fs/msdosfs/msdosfs_vnops.c: revision 1.17 	sys/fs/msdosfs/denode.h: revision 1.6 The maximum file size on MS-DOS filesystems is 4 GB - 1 byte, so don't bother trying to write files bigger than this.  Just return EFBIG to caller, rather than panic()ing later. From OpenBSD. This closes my PR kern/30864: "panic when copying files of >4GB on msdosfs"

--- sys/fs/msdosfs/denode.h
+++ sys/fs/msdosfs/denode.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: denode.h,v 1.4 2003/09/07 22:09:11 itojun Exp $	*/
+/*	$NetBSD: denode.h,v 1.4.6.1 2005/09/06 16:12:00 riz Exp $	*/
 
 /*-
  * Copyright (C) 1994, 1995, 1997 Wolfgang Solfrank.
@@ -176,6 +176,9 @@ struct denode {
  * Note: Must be < sizeof(dirent.d_name)
  */
 #define	WIN_MAXLEN	255
+
+/* Maximum size of a file on a FAT filesystem */
+#define MSDOSFS_FILESIZE_MAX	0xFFFFFFFFLL
 
 /*
  * Transfer directory entries between internal and external form.
