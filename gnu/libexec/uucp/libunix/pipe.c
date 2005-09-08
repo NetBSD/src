630a0e99fda38a0cf6ebcf9aa986b4add294b21e
tron <tron>
2005-09-08 21:20:38 +0000

    Pull up following revision(s) (requested by is in ticket #751): 	gnu/libexec/uucp/libunix/pipe.c: revision 1.3 	gnu/libexec/uucp/doc/uucp.texi: revision 1.6 Allow for \H -> "address ..." substitution in PIPE ports. Useful e.g. for generic UUCP over SSH or SSL... Idea and part of the patch by Joerg Durchain, has apparently been integrated in Debian for a while.

--- gnu/libexec/uucp/libunix/pipe.c
+++ gnu/libexec/uucp/libunix/pipe.c
@@ -27,7 +27,7 @@
 #include "uucp.h"
 
 #if USE_RCS_ID
-const char pipe_rcsid[] = "$Id: pipe.c,v 1.2 1995/08/24 05:20:05 jtc Exp $";
+const char pipe_rcsid[] = "$Id: pipe.c,v 1.2.22.1 2005/09/08 21:20:38 tron Exp $";
 #endif
 
 #include "uudefs.h"
@@ -199,6 +199,7 @@ fspipe_dial (qconn, puuconf, qsys, zphone, qdialer, ptdialer)
   struct ssysdep_conn *q;
   int aidescs[3];
   const char **pzprog;
+  const char **p;
 
   q = (struct ssysdep_conn *) qconn->psysdep;
 
@@ -211,6 +212,12 @@ fspipe_dial (qconn, puuconf, qsys, zphone, qdialer, ptdialer)
       ulog (LOG_ERROR, "No command for pipe connection");
       return FALSE;
     }
+
+  /* Look for a string \H and substitute the address given for this system */
+  for (p=pzprog; *p; p++)
+    if (!strcmp(*p, "\\H"))
+      *p = zphone;
+ 
 
   aidescs[0] = SPAWN_WRITE_PIPE;
   aidescs[1] = SPAWN_READ_PIPE;
