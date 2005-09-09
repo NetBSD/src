e9482769f94d1e8ca9ac521c2effd0ed460df479
tron <tron@NetBSD.org>
2005-09-09 15:28:55 +0000

    Pull up following revision(s) (requested by aymeric in ticket #753): 	usr.bin/vi/ex/version.h: revision 1.18 bump to nb15: . we now handle ^C correctly in all cases . blanks and alnum chars are ignored in the shellmeta option, as the code   brokenly said it should . \ can be used to escape any (special) character in file names

--- usr.bin/vi/ex/version.h
+++ usr.bin/vi/ex/version.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: version.h,v 1.17 2004/11/23 14:36:50 aymeric Exp $	*/
+/*	$NetBSD: version.h,v 1.17.2.1 2005/09/09 15:28:55 tron Exp $	*/
 
 #define	VI_VERSION \
-    "Version 1.79nb14 (10/23/96) The CSRG, University of California, Berkeley."
+    "Version 1.79nb15 (10/23/96) The CSRG, University of California, Berkeley."
