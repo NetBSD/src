17c66be4b4ce4232f5bb6d63ecdfae8000dfb811
jmmv <jmmv>
2005-09-10 19:20:48 +0000

    Initial addition of tmpfs, an efficient memory file-system.  This project was developed as part of Google's Summer of Code 2005 program.  This change adds the kernel code, the mount_tmpfs utility, a regression test suite and does all other related changes to integrate these.
    The file-system is still *experimental*.  Therefore, it is disabled by
    default in all kernels.  However, as typically done, a commented-out
    entry is added in them to ease its setup.
    
    Note that I haven't commited the required mountd(8) changes to be able
    to export tmpfs file-systems because NFS support is still very unstable
    and because, before enabling it, I'd like to do some other changes.
    
    OK'ed by my project mentor, William Studenmund (wrstuden@).

---
+++ sys/fs/tmpfs/tmpfs_fifoops.h
@@ -0,0 +1,107 @@
+/*	$NetBSD: tmpfs_fifoops.h,v 1.1 2005/09/10 19:20:51 jmmv Exp $	*/
+
+/*
+ * Copyright (c) 2005 The NetBSD Foundation, Inc.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to The NetBSD Foundation
+ * by Julio M. Merino Vidal.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *        This product includes software developed by the NetBSD
+ *        Foundation, Inc. and its contributors.
+ * 4. Neither the name of The NetBSD Foundation nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#if !defined(_TMPFS_FIFOOPS_H_)
+#define _TMPFS_FIFOOPS_H_
+
+#if !defined(_KERNEL)
+#  error "This file is not meant to be included by userland."
+#endif
+
+#include <miscfs/fifofs/fifo.h>
+#include <fs/tmpfs/tmpfs_vnops.h>
+
+/* --------------------------------------------------------------------- */
+
+/*
+ * Declarations for tmpfs_fifoops.c.
+ */
+
+extern int (**tmpfs_fifoop_p)(void *);
+
+#define	tmpfs_fifo_lookup	fifo_lookup
+#define	tmpfs_fifo_create	fifo_create
+#define	tmpfs_fifo_mknod	fifo_mknod
+#define	tmpfs_fifo_open		fifo_open
+int	tmpfs_fifo_close	(void *);
+#define	tmpfs_fifo_access	tmpfs_access
+#define	tmpfs_fifo_getattr	tmpfs_getattr
+#define	tmpfs_fifo_setattr	tmpfs_setattr
+int	tmpfs_fifo_read		(void *);
+int	tmpfs_fifo_write	(void *);
+#define	tmpfs_fifo_fcntl	tmpfs_fcntl
+#define	tmpfs_fifo_ioctl	fifo_ioctl
+#define	tmpfs_fifo_poll		fifo_poll
+#define	tmpfs_fifo_kqfilter	fifo_kqfilter
+#define	tmpfs_fifo_revoke	fifo_revoke
+#define	tmpfs_fifo_mmap		fifo_mmap
+#define	tmpfs_fifo_fsync	fifo_fsync
+#define	tmpfs_fifo_seek		fifo_seek
+#define	tmpfs_fifo_remove	fifo_remove
+#define	tmpfs_fifo_link		fifo_link
+#define	tmpfs_fifo_rename	fifo_rename
+#define	tmpfs_fifo_mkdir	fifo_mkdir
+#define	tmpfs_fifo_rmdir	fifo_rmdir
+#define	tmpfs_fifo_symlink	fifo_symlink
+#define	tmpfs_fifo_readdir	fifo_readdir
+#define	tmpfs_fifo_readlink	fifo_readlink
+#define	tmpfs_fifo_abortop	fifo_abortop
+#define	tmpfs_fifo_inactive	tmpfs_inactive
+#define	tmpfs_fifo_reclaim	tmpfs_reclaim
+#define	tmpfs_fifo_lock		tmpfs_lock
+#define	tmpfs_fifo_unlock	tmpfs_unlock
+#define	tmpfs_fifo_bmap		fifo_bmap
+#define	tmpfs_fifo_strategy	fifo_strategy
+#define	tmpfs_fifo_print	tmpfs_print
+#define	tmpfs_fifo_pathconf	fifo_pathconf
+#define	tmpfs_fifo_islocked	tmpfs_islocked
+#define	tmpfs_fifo_advlock	fifo_advlock
+#define	tmpfs_fifo_blkatoff	fifo_blkatoff
+#define	tmpfs_fifo_valloc	fifo_valloc
+#define	tmpfs_fifo_reallocblks	fifo_reallocblks
+#define	tmpfs_fifo_vfree	fifo_vfree
+#define	tmpfs_fifo_truncate	fifo_truncate
+#define	tmpfs_fifo_update	tmpfs_update
+#define	tmpfs_fifo_lease	tmpfs_lease
+#define	tmpfs_fifo_bwrite	tmpfs_bwrite
+#define	tmpfs_fifo_getpages	genfs_badop
+#define	tmpfs_fifo_putpages	fifo_putpages
+
+/* --------------------------------------------------------------------- */
+
+#endif /* !defined(_TMPFS_FIFOOPS_H_) */
