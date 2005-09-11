fbdb3b79e9f0575a6ca8e34f431ef40252cc8ba5
he <he@NetBSD.org>
2005-09-11 23:15:18 +0000

    Patterned after next68k, omit inclusion of <sys/device.h> if we are not in _KERNEL.  This should work around our lint problems related to "unspecified-size array at end of struct" constructs.

--- sys/arch/hp300/include/param.h
+++ sys/arch/hp300/include/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.45 2004/08/28 19:46:41 thorpej Exp $	*/
+/*	$NetBSD: param.h,v 1.46 2005/09/11 23:15:18 he Exp $	*/
 
 /*
  * Copyright (c) 1982, 1986, 1990, 1993
@@ -85,10 +85,12 @@
 #define	_MACHINE	hp300
 #define	MACHINE		"hp300"
 
+#ifdef _KERNEL
 /*
  * Interrupt glue.
  */
 #include <machine/intr.h>
+#endif /* _KERNEL */
 
 #define	PGSHIFT		12		/* LOG2(NBPG) */
 #define	KERNBASE	0x00000000	/* start of kernel virtual */
