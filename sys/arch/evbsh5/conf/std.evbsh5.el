570cbb71de0a757256157da718fc18393c29a0a6
yamt <yamt>
2005-09-17 09:44:05 +0000

    include "conf/std".

--- sys/arch/evbsh5/conf/std.evbsh5.el
+++ sys/arch/evbsh5/conf/std.evbsh5.el
@@ -1,9 +1,10 @@
-#	$NetBSD: std.evbsh5.el,v 1.2 2002/07/11 14:42:55 scw Exp $
+#	$NetBSD: std.evbsh5.el,v 1.3 2005/09/17 09:44:06 yamt Exp $
 #
 # Options/devices that all Little Endian evbsh5s should have
 #
 
 machine evbsh5 sh5
+include		"conf/std"	# MI standard options
 
 options 	EXEC_ELF32
 options 	EXEC_SCRIPT
