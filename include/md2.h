35147e847f3828210ba3fa6e75788fcd6b9c0455
elad <elad>
2005-09-24 22:08:30 +0000

    Fix prototype.

--- include/md2.h
+++ include/md2.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: md2.h,v 1.3 2005/09/24 21:30:38 elad Exp $	*/
+/*	$NetBSD: md2.h,v 1.4 2005/09/24 22:08:30 elad Exp $	*/
 
 #ifndef _MD2_H_
 #define _MD2_H_
@@ -22,7 +22,7 @@ void	MD2Update(MD2_CTX *, const unsigned char *, unsigned int);
 void	MD2Final(unsigned char[16], MD2_CTX *);
 char	*MD2End(MD2_CTX *, char *);
 char	*MD2File(const char *, char *);
-char	*MD2Data(const unsigned char *, unsigned int, char *);
+char	*MD2Data(const unsigned char *, size_t, char *);
 __END_DECLS
 
 #endif /* _MD2_H_ */
