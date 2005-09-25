96ba5552faacd92218f0b82b6b17c5e0a0dd811f
oster <oster@NetBSD.org>
2005-09-25 19:47:17 +0000

    Re-work the handling of incoming I/O in RAIDframe: - introduce rf_buf_queue_check() which checks to see if there is work to do in the incoming buffer queue - rf_RaidIOThread() is now responsible for calling raidstart(), and is also now the only place that calls raidstart() - raidstrategy() now just queues requests in buf_queue and signals rf_RaidIOThread() that work has arrived
    Hopefully addresses PR#30233

--- sys/dev/raidframe/rf_states.c
+++ sys/dev/raidframe/rf_states.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: rf_states.c,v 1.38 2005/02/27 00:27:45 perry Exp $	*/
+/*	$NetBSD: rf_states.c,v 1.39 2005/09/25 19:47:17 oster Exp $	*/
 /*
  * Copyright (c) 1995 Carnegie-Mellon University.
  * All rights reserved.
@@ -27,7 +27,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: rf_states.c,v 1.38 2005/02/27 00:27:45 perry Exp $");
+__KERNEL_RCSID(0, "$NetBSD: rf_states.c,v 1.39 2005/09/25 19:47:17 oster Exp $");
 
 #include <sys/errno.h>
 
@@ -235,8 +235,7 @@ rf_State_LastState(RF_RaidAccessDesc_t *desc)
 	((RF_Raid_t *) desc->raidPtr)->openings++;
 	RF_UNLOCK_MUTEX(((RF_Raid_t *) desc->raidPtr)->mutex);
 
-	/* wake up any pending IO */
-	raidstart(((RF_Raid_t *) desc->raidPtr));
+	wakeup(&(desc->raidPtr->iodone));
 
 	/* printf("Calling biodone on 0x%x\n",desc->bp); */
 	biodone(desc->bp);	/* access came through ioctl */
