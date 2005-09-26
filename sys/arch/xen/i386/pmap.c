084bde7a55752f79b37f1839a1400d36bede95ad
tron <tron@NetBSD.org>
2005-09-26 20:24:52 +0000

    Pull up following revision(s) (requested by chs in ticket #813): 	sys/arch/xen/i386/pmap.c: revision 1.15 	sys/arch/i386/i386/pmap.c: revision 1.185 remove the part of rev. 1.168 that skips TLB invalidations in pmap_enter(). it's wrong for wired mappings.  fixes PR 29804.

--- sys/arch/xen/i386/pmap.c
+++ sys/arch/xen/i386/pmap.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: pmap.c,v 1.8.2.4 2005/09/14 22:22:52 tron Exp $	*/
+/*	$NetBSD: pmap.c,v 1.8.2.5 2005/09/26 20:24:52 tron Exp $	*/
 /*	NetBSD: pmap.c,v 1.179 2004/10/10 09:55:24 yamt Exp		*/
 
 /*
@@ -61,7 +61,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: pmap.c,v 1.8.2.4 2005/09/14 22:22:52 tron Exp $");
+__KERNEL_RCSID(0, "$NetBSD: pmap.c,v 1.8.2.5 2005/09/26 20:24:52 tron Exp $");
 
 #include "opt_cputype.h"
 #include "opt_user_ldt.h"
@@ -3725,22 +3725,6 @@ pmap_enter(pmap, va, pa, prot, flags)
 		opte = pte_atomic_update_ma(&ptes[x86_btop(va)], maptp, npte);
 
 		/*
-		 * Any change in the protection level that the CPU
-		 * should know about ? 
-		 */
-		if ((npte & PG_RW)
-		     || ((opte & (PG_M | PG_RW)) != (PG_M | PG_RW))) {
-			XENPRINTK(("pmap update opte == pa, prot change"));
-			/*
-			 * No need to flush the TLB.
-			 * Just add old PG_M, ... flags in new entry.
-			 */
-			PTE_ATOMIC_SETBITS(&ptes[x86_btop(va)], maptp,
-			    opte & (PG_M | PG_U));
-			goto out_ok;
-		}
-
-		/*
 		 * Might be cached in the TLB as being writable
 		 * if this is on the PVLIST, sync R/M bit
 		 */
@@ -3873,7 +3857,6 @@ shootdown_now:
 #endif
 	}
 
-out_ok:
 	error = 0;
 
 out:
@@ -3993,22 +3976,6 @@ pmap_enter_ma(pmap, va, pa, prot, flags)
 		opte = pte_atomic_update_ma(&ptes[x86_btop(va)], maptp, npte);
 
 		/*
-		 * Any change in the protection level that the CPU
-		 * should know about ? 
-		 */
-		if ((npte & PG_RW)
-		     || ((opte & (PG_M | PG_RW)) != (PG_M | PG_RW))) {
-			XENPRINTK(("pmap update opte == pa, prot change"));
-			/*
-			 * No need to flush the TLB.
-			 * Just add old PG_M, ... flags in new entry.
-			 */
-			PTE_ATOMIC_SETBITS(&ptes[x86_btop(va)], maptp,
-			    opte & (PG_M | PG_U));
-			goto out_ok;
-		}
-
-		/*
 		 * Might be cached in the TLB as being writable
 		 * if this is on the PVLIST, sync R/M bit
 		 */
@@ -4102,7 +4069,6 @@ shootdown_now:
 #endif
 	}
 
-out_ok:
 	error = 0;
 
 out:
