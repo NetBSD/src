479e0a3f372422a48f7cc84b68e334ead06d1646
christos <christos@NetBSD.org>
2005-09-28 16:31:45 +0000

    Rename "hash.c" to "hashhl.c" since this is included from *hl.c files. The name "hash.c" is already used in db/hash/hash.c, and having duplicated names, aside from it being bad style, breaks the tools/nbcompat build because it picks the wrong hash.c file. Thanks to greg for helping debug this.

--- lib/libc/hash/rmd160/rmd160hl.c
+++ lib/libc/hash/rmd160/rmd160hl.c
@@ -1,4 +1,4 @@
-/* $NetBSD: rmd160hl.c,v 1.3 2005/09/26 03:01:41 christos Exp $ */
+/* $NetBSD: rmd160hl.c,v 1.4 2005/09/28 16:31:45 christos Exp $ */
 
 /*
  * Derived from code ritten by Jason R. Thorpe <thorpej@NetBSD.org>,
@@ -8,10 +8,10 @@
 
 #include <sys/cdefs.h>
 #if defined(LIBC_SCCS) && !defined(lint)
-__RCSID("$NetBSD: rmd160hl.c,v 1.3 2005/09/26 03:01:41 christos Exp $");
+__RCSID("$NetBSD: rmd160hl.c,v 1.4 2005/09/28 16:31:45 christos Exp $");
 #endif /* LIBC_SCCS and not lint */
 
 #define	HASH_ALGORITHM	RMD160
 #define HASH_INCLUDE	<crypto/rmd160.h>
 
-#include "../hash.c"
+#include "../hashhl.c"
