6a79ea807166df64cad3140a079a6e2c47680708
riz <riz@NetBSD.org>
2005-10-12 01:02:22 +0000

    Welcome to 2.1_RC6

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.188.2.9.2.9 2005/09/18 17:58:06 riz Exp $	*/
+/*	$NetBSD: param.h,v 1.188.2.9.2.10 2005/10/12 01:02:23 riz Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -63,7 +63,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	201000000	/* NetBSD 2.1_RC5 */
+#define	__NetBSD_Version__	201000000	/* NetBSD 2.1_RC6 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) >= __NetBSD_Version__)
