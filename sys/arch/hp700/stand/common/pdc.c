f1b7dcd6f3d595979d8d660b240d6f42a2d6f5ed
itohy <itohy@NetBSD.org>
2005-10-15 10:58:06 +0000

    When read from a disk partition, add the partition offset properly. Do not use NULL address for disklabel buffer (wow).

--- sys/arch/hp700/stand/common/pdc.c
+++ sys/arch/hp700/stand/common/pdc.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: pdc.c,v 1.4 2003/10/21 13:10:42 itohy Exp $	*/
+/*	$NetBSD: pdc.c,v 1.5 2005/10/15 10:58:06 itohy Exp $	*/
 
 /*	$OpenBSD: pdc.c,v 1.10 1999/05/06 02:27:44 mickey Exp $	*/
 
@@ -148,6 +148,7 @@ iodcstrategy(void *devdata, int rw, daddr_t blk, size_t size, void *buf,
 #endif
 
 	blk <<= DEV_BSHIFT;
+	blk += dp->part_off;
 	if ((pzdev->pz_class & PCL_CLASS_MASK) == PCL_SEQU) {
 		/* rewind and re-read to seek */
 		if (blk < dp->last_blk) {
