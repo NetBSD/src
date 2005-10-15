7de024490ea9f4075757aa2f5a4726b6b1f78cfa
riz <riz@NetBSD.org>
2005-10-15 15:43:09 +0000

    Pull up following revision(s) (requested by aymeric in ticket #878): 	usr.bin/vi/ex/version.h: revision 1.19 bump for latest changes: . reintroduce ^V as a valid quoting character . don't swallow quoting characters which are not followed by anything

--- usr.bin/vi/ex/version.h
+++ usr.bin/vi/ex/version.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: version.h,v 1.17.2.1 2005/09/09 15:28:55 tron Exp $	*/
+/*	$NetBSD: version.h,v 1.17.2.2 2005/10/15 15:43:09 riz Exp $	*/
 
 #define	VI_VERSION \
-    "Version 1.79nb15 (10/23/96) The CSRG, University of California, Berkeley."
+    "Version 1.79nb16 (10/23/96) The CSRG, University of California, Berkeley."
