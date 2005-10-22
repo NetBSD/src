d195410600cff687610e3639f15146f5f66a06db
riz <riz>
2005-10-22 03:54:33 +0000

    Pull up following revision(s) (requested by nathanw in ticket #911): 	sys/kern/uipc_socket.c: revision 1.112 Check the argument to SO_LINGER.

--- sys/kern/uipc_socket.c
+++ sys/kern/uipc_socket.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: uipc_socket.c,v 1.108 2005/02/26 21:34:55 perry Exp $	*/
+/*	$NetBSD: uipc_socket.c,v 1.108.2.1 2005/10/22 03:54:33 riz Exp $	*/
 
 /*-
  * Copyright (c) 2002 The NetBSD Foundation, Inc.
@@ -68,7 +68,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: uipc_socket.c,v 1.108 2005/02/26 21:34:55 perry Exp $");
+__KERNEL_RCSID(0, "$NetBSD: uipc_socket.c,v 1.108.2.1 2005/10/22 03:54:33 riz Exp $");
 
 #include "opt_sock_counters.h"
 #include "opt_sosend_loan.h"
@@ -1403,6 +1403,11 @@ sosetopt(struct socket *so, int level, int optname, struct mbuf *m0)
 		case SO_LINGER:
 			if (m == NULL || m->m_len != sizeof(struct linger)) {
 				error = EINVAL;
+				goto bad;
+			}
+			if (mtod(m, struct linger *)->l_linger < 0 ||
+			    mtod(m, struct linger *)->l_linger > (INT_MAX / hz)) {
+				error = EDOM;
 				goto bad;
 			}
 			so->so_linger = mtod(m, struct linger *)->l_linger;
