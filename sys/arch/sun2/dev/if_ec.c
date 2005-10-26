894143b87071346348c2e947219638f116010ebb
riz <riz>
2005-10-26 18:11:02 +0000

    Pull up following revision(s) (requested by tsutsui in ticket #5785): 	sys/arch/sun2/dev/if_ec.c: revision 1.9 This driver was broken by bouyer on 2003/01/20. I've made it at least config up/down again -- haven't tried actually passing traffic.

--- sys/arch/sun2/dev/if_ec.c
+++ sys/arch/sun2/dev/if_ec.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: if_ec.c,v 1.3.10.1 2003/01/28 05:44:55 jmc Exp $	*/
+/*	$NetBSD: if_ec.c,v 1.3.10.2 2005/10/26 18:11:02 riz Exp $	*/
 
 /*
  * Copyright (c) 2001 The NetBSD Foundation, Inc.
@@ -328,6 +328,7 @@ ec_start(ifp)
 	int s;
 	u_int count, realcount;
 	bus_size_t off;
+	static u_int8_t padding[ETHER_MIN_LEN - ETHER_CRC_LEN] = {0};
 
 	s = splnet();
 
@@ -350,8 +351,7 @@ ec_start(ifp)
 #endif
 
 	/* Size the packet. */
-	for (count = EC_BUF_SZ, m = m0; m != NULL; m = m->m_next)
-		count -= m->m_len;
+	count = EC_BUF_SZ - m0->m_pkthdr.len;
 
 	/* Copy the packet into the xmit buffer. */
 	realcount = MIN(count, EC_PKT_MAXTDOFF);
@@ -359,11 +359,8 @@ ec_start(ifp)
 	for (off = realcount, m = m0; m != 0; off += m->m_len, m = m->m_next)
 		ec_copyout(sc, mtod(m, u_int8_t *), ECREG_TBUF + off, m->m_len);
 	m_freem(m0);
-#if 0
-	bus_space_set_region_1(sc->sc_iot, sc->sc_ioh, ECREG_TBUF + off, 0,
-	    count - realcount);
-#endif
-	w16zero((u_int8_t *)(ECREG_TBUF + off), count - realcount);
+	if (count - realcount)
+		ec_copyout(sc, padding, ECREG_TBUF + off, count - realcount);
 
 	/* Enable the transmitter. */
 	ECREG_CSR_WR((ECREG_CSR_RD & EC_CSR_PA) | EC_CSR_TBSW | EC_CSR_TINT | EC_CSR_JINT);
