8754242bc1325641ae3926b81d7c1698307bb93b
riz <riz@NetBSD.org>
2005-10-26 23:49:27 +0000

    Welcome to 2.1.0_STABLE

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.188.2.9.2.11 2005/10/21 06:16:42 riz Exp $	*/
+/*	$NetBSD: param.h,v 1.188.2.9.2.11.2.1 2005/10/26 23:49:27 riz Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -63,7 +63,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	201000000	/* NetBSD 2.1 */
+#define	__NetBSD_Version__	201000000	/* NetBSD 2.1.0_STABLE */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) >= __NetBSD_Version__)
