014e1097006b34ac762f642b7fe6e94a95a63c90
thorpej <thorpej@NetBSD.org>
2005-10-29 17:56:27 +0000

    Don't bother fetching attributes here; they're never used.

--- regress/usr.bin/awk/string1/test.awk
+++
@@ -1,7 +0,0 @@
-# $Id: test.awk,v 1.1 2005/01/13 12:26:12 yamt Exp $
-
-BEGIN {
-	print "A\
-B";
-	print "CD"
-}
--- sbin/disklabel/dkcksum.h
+++
@@ -1,3 +0,0 @@
-/*	$NetBSD: dkcksum.h,v 1.3 2000/12/24 05:59:11 lukem Exp $	*/
-
-u_short	dkcksum(struct disklabel *);
