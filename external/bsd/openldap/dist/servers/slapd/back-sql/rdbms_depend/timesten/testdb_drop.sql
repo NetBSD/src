876a83095045035e43bdd42feed236cb4ec5975a
thorpej <thorpej>
2005-10-29 17:56:27 +0000

    Don't bother fetching attributes here; they're never used.

--- external/bsd/openldap/dist/servers/slapd/back-sql/rdbms_depend/timesten/testdb_drop.sql
+++
@@ -1,5 +0,0 @@
-DROP TABLE persons;
-DROP TABLE institutes;
-DROP TABLE documents;
-DROP TABLE authors_docs;
-DROP TABLE phones;
--- gnu/dist/groff/src/libs/libgroff/strncasecmp.c
+++
@@ -1,4 +0,0 @@
-/*	$NetBSD: strncasecmp.c,v 1.1.1.1 2003/06/30 17:52:06 wiz Exp $	*/
-
-#define LENGTH_LIMIT
-#include "strcasecmp.c"
