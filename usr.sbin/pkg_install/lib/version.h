75b9797969208a7374307fc3ae4944e3a20b1a76
dillo <dillo@NetBSD.org>
2005-11-03 21:16:41 +0000

    Implement framework to store variable=value pairs about an installed package. Use this in pkg_add to mark installed dependencies as automatically installed.
    pkg_add: new flag -A: marks package as automatically installed.
    pkg_admin: new commands set and unset to modify variable pairs for
    	   installed packages.
    pkg_info: report these variable pairs with -Q/-B.
    	  new flag -u: report only manually installed packages (not
    	  installed with pkg_add -A).
    	  Error out if -a/-u and a package name is specified.
    
    Joint work with Thomas Klausner.
    
    As discussed on tech-pkg.

--- usr.sbin/pkg_install/lib/version.h
+++ usr.sbin/pkg_install/lib/version.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: version.h,v 1.69 2005/10/30 21:52:02 joerg Exp $	*/
+/*	$NetBSD: version.h,v 1.70 2005/11/03 21:16:41 dillo Exp $	*/
 
 /*
  * Copyright (c) 2001 Thomas Klausner.  All rights reserved.
@@ -33,6 +33,6 @@
 #ifndef _INST_LIB_VERSION_H_
 #define _INST_LIB_VERSION_H_
 
-#define PKGTOOLS_VERSION "20051030"
+#define PKGTOOLS_VERSION "20051103"
 
 #endif /* _INST_LIB_VERSION_H_ */
