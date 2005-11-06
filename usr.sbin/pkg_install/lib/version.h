602b589e1ad9d70007e1ef244185c194eea01c17
tron <tron@NetBSD.org>
2005-11-06 13:40:51 +0000

    Pull up following revision(s) (requested by wiz in ticket #938): 	usr.sbin/pkg_install/lib/lib.h: revision 1.77 	usr.sbin/pkg_install/info/show.c: revision 1.32 	usr.sbin/pkg_install/info/main.c: revision 1.46 	usr.sbin/pkg_install/lib/var.c: revision 1.1 	usr.sbin/pkg_install/lib/Makefile: revision 1.24 	usr.sbin/pkg_install/info/pkg_info.1: revision 1.50 	usr.sbin/pkg_install/admin/pkg_admin.1: revision 1.36 	usr.sbin/pkg_install/add/add.h: revision 1.8 	usr.sbin/pkg_install/add/pkg_add.1: revision 1.61 	usr.sbin/pkg_install/info/info.h: revision 1.21 	usr.sbin/pkg_install/lib/automatic.c: revision 1.1 	usr.sbin/pkg_install/info/perform.c: revision 1.63 	usr.sbin/pkg_install/add/perform.c: revision 1.112 	usr.sbin/pkg_install/admin/main.c: revision 1.49 	usr.sbin/pkg_install/lib/version.h: revision 1.70 	usr.sbin/pkg_install/add/main.c: revision 1.35 Implement framework to store variable=value pairs about an installed package. Use this in pkg_add to mark installed dependencies as automatically installed. pkg_add: new flag -A: marks package as automatically installed. pkg_admin: new commands set and unset to modify variable pairs for 	   installed packages. pkg_info: report these variable pairs with -Q/-B. 	  new flag -u: report only manually installed packages (not 	  installed with pkg_add -A). 	  Error out if -a/-u and a package name is specified. Joint work with Thomas Klausner. As discussed on tech-pkg.

--- usr.sbin/pkg_install/lib/version.h
+++ usr.sbin/pkg_install/lib/version.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: version.h,v 1.64.2.5 2005/11/06 13:39:10 tron Exp $	*/
+/*	$NetBSD: version.h,v 1.64.2.6 2005/11/06 13:40:51 tron Exp $	*/
 
 /*
  * Copyright (c) 2001 Thomas Klausner.  All rights reserved.
@@ -33,6 +33,6 @@
 #ifndef _INST_LIB_VERSION_H_
 #define _INST_LIB_VERSION_H_
 
-#define PKGTOOLS_VERSION "20051030"
+#define PKGTOOLS_VERSION "20051103"
 
 #endif /* _INST_LIB_VERSION_H_ */
