43bcecdc417df9dcdd6091d51047444882a45874
christos <christos>
2005-11-07 21:58:12 +0000

    remove asprintf and strlcpy. We don't support NetBSD1.3 and NetBSD1.4. GC Todd's copyright, since it is not needed anymore.

--- usr.sbin/user/user.c
+++ usr.sbin/user/user.c
@@ -1,4 +1,4 @@
-/* $NetBSD: user.c,v 1.96 2005/11/07 21:46:50 christos Exp $ */
+/* $NetBSD: user.c,v 1.97 2005/11/07 21:58:12 christos Exp $ */
 
 /*
  * Copyright (c) 1999 Alistair G. Crooks.  All rights reserved.
@@ -31,38 +31,12 @@
  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-/*
- * Copyright (c) 1997 Todd C. Miller <Todd.Miller@courtesan.com>
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. The name of the author may not be used to endorse or promote products
- *    derived from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
- * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
- * AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL
- * THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
- * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
- * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
- * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
- * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
 #include <sys/cdefs.h>
 
 #ifndef lint
 __COPYRIGHT("@(#) Copyright (c) 1999 \
 	        The NetBSD Foundation, Inc.  All rights reserved.");
-__RCSID("$NetBSD: user.c,v 1.96 2005/11/07 21:46:50 christos Exp $");
+__RCSID("$NetBSD: user.c,v 1.97 2005/11/07 21:58:12 christos Exp $");
 #endif
 
 #include <sys/types.h>
@@ -318,66 +292,6 @@ removehomedir(struct passwd *pwp)
 	}
 	return 1;
 }
-
-#define NetBSD_1_4_K	104110000
-
-#if defined(__NetBSD_Version__) && (__NetBSD_Version__ < NetBSD_1_4_K)
-/* bounds checking strncpy */
-static int
-strlcpy(char *to, char *from, size_t tosize)
-{
-	size_t	n;
-	int	fromsize;
-
-	fromsize = strlen(from);
-	n = MIN(tosize - 1, fromsize);
-	(void)memcpy(to, from, n);
-	to[n] = '\0';
-	return fromsize;
-}
-#endif /* NetBSD < 1.4K */
-
-
-/*
- * research has shown that NetBSD 1.3H was the first version of -current
- * with asprintf in libc. agc
- */
-#define NetBSD_1_3_H	103080000
-
-#if defined(__NetBSD_Version__) && (__NetBSD_Version__ < NetBSD_1_3_H)
-
-int
-asprintf(char **str, char const *fmt, ...)
-{
-	int ret;
-	va_list ap;
-	FILE f;
-	unsigned char *_base;
-
-	f._flags = __SWR | __SSTR | __SALC;
-	f._bf._base = f._p = (unsigned char *)malloc(128);
-	if (f._bf._base == NULL)
-		goto err;
-	f._bf._size = f._w = 127;		/* Leave room for the NUL */
-	va_start(ap, fmt);
-	ret = vfprintf(&f, fmt, ap);
-	va_end(ap);
-	if (ret == -1)
-		goto err;
-	*f._p = '\0';
-	_base = realloc(f._bf._base, (size_t)(ret + 1));
-	if (_base == NULL)
-		goto err;
-	*str = (char *)_base;
-	return (ret);
-
-err:
-	if (f._bf._base)
-		free(f._bf._base);
-	*str = NULL;
-	return (-1);
-}
-#endif /* NetBSD < 1.3H */
 
 /* return 1 if all of `s' is numeric */
 static int
