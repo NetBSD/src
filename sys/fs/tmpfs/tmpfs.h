1b965bbbfd642b822f5960595f1f8a41b02a0b66
skrll <skrll@NetBSD.org>
2005-11-12 17:00:57 +0000

    Adapt to branch

--- sys/fs/tmpfs/tmpfs.h
+++ sys/fs/tmpfs/tmpfs.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: tmpfs.h,v 1.9.2.2 2005/11/10 14:09:44 skrll Exp $	*/
+/*	$NetBSD: tmpfs.h,v 1.9.2.3 2005/11/12 17:00:57 skrll Exp $	*/
 
 /*
  * Copyright (c) 2005 The NetBSD Foundation, Inc.
@@ -330,7 +330,7 @@ int	tmpfs_chown(struct vnode *, uid_t, gid_t, struct ucred *,
 	    struct proc *);
 int	tmpfs_chsize(struct vnode *, u_quad_t, struct ucred *, struct proc *);
 int	tmpfs_chtimes(struct vnode *, struct timespec *, struct timespec *,
-	    int, struct ucred *, struct proc *);
+	    int, struct ucred *, struct lwp *);
 void	tmpfs_itimes(struct vnode *, const struct timespec *,
 	    const struct timespec *);
 
