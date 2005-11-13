b16bb7f430d42c26051c2ce3fce7b746439dd51a
tron <tron>
2005-11-13 16:04:15 +0000

    Welcome to NetBSD 3.0_RC1.

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.211.2.1 2005/03/17 00:26:32 jmc Exp $	*/
+/*	$NetBSD: param.h,v 1.211.2.2 2005/11/13 16:04:15 tron Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -63,7 +63,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	300000000	/* NetBSD 3.0_BETA */
+#define	__NetBSD_Version__	300000000	/* NetBSD 3.0_RC1 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) >= __NetBSD_Version__)
