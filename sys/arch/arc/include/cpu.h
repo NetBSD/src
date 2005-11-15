aca3d836f101d816112c2f5832c9e72cdf9243ad
tsutsui <tsutsui@NetBSD.org>
2005-11-15 14:03:20 +0000

    Remove MIPS_INT_MASK_CLOCK and INT_MASK_REAL_DEV macro and use MIPS_INT_MASK_5 and MIPS3_HARD_INT_MASK directly. They were not used since interrupt service function was moved from MI mips/trap.c to MD part long ago.

--- sys/arch/arc/include/cpu.h
+++ sys/arch/arc/include/cpu.h
@@ -1,21 +1,10 @@
-/*	$NetBSD: cpu.h,v 1.20 2005/11/15 13:45:30 tsutsui Exp $	*/
+/*	$NetBSD: cpu.h,v 1.21 2005/11/15 14:03:20 tsutsui Exp $	*/
 /*	$OpenBSD: cpu.h,v 1.9 1998/01/28 13:46:10 pefo Exp $ */
 
 #ifndef _ARC_CPU_H_
 #define _ARC_CPU_H_
 
-/*
- *  Internal timer causes hard interrupt 5.
- */
-#define MIPS_INT_MASK_CLOCK	MIPS_INT_MASK_5
-
 #include <mips/cpu.h>
 #include <mips/cpuregs.h>
-
-/*
- * definitions of cpu-dependent requirements
- * referenced in generic code
- */
-#define	INT_MASK_REAL_DEV	MIPS3_HARD_INT_MASK	/* XXX */
 
 #endif /* _ARC_CPU_H_ */
