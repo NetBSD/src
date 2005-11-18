2d0f87a8ea57b80c31370e4ce4c238843125c44a
bouyer <bouyer@NetBSD.org>
2005-11-18 23:10:32 +0000

    Some drives disconnect after the last data phase without a save data pointer message. In such case we would not update resid with the proper value (eventually resid would not be updated at all if there was only one data phase). To fix this, have the script save the offset in the data tables at disconnect time if there was a transfer, and use this to compute the resid if the current offset is 0. Problem reported and patch tested by edwin, Roy Bixler and YAMAMOTO Takashi. Fix kern/31990 by YAMAMOTO Takashi.

--- sys/dev/ic/siopvar.h
+++ sys/dev/ic/siopvar.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: siopvar.h,v 1.21 2005/02/27 00:27:02 perry Exp $	*/
+/*	$NetBSD: siopvar.h,v 1.22 2005/11/18 23:10:32 bouyer Exp $	*/
 
 /*
  * Copyright (c) 2000 Manuel Bouyer.
@@ -56,6 +56,7 @@ struct siop_cmd {
 	struct siop_common_cmd cmd_c;
 	struct siop_cbd *siop_cbdp; /* pointer to our siop_cbd */
 	int reselslot;
+	u_int32_t saved_offset; /* offset in table after disc without sdp */
 };
 #define cmd_tables cmd_c.siop_tables
 
