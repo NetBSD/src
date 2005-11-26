dda4e9ddab832d7e667ff38cd377a75c05f6e6d9
tsutsui <tsutsui>
2005-11-26 04:11:31 +0000

    FALLTHROUH -> FALLTHROUGH (from OpenBSD)

--- sys/dev/ic/siop_common.c
+++ sys/dev/ic/siop_common.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: siop_common.c,v 1.37 2005/02/27 00:27:02 perry Exp $	*/
+/*	$NetBSD: siop_common.c,v 1.38 2005/11/26 04:11:31 tsutsui Exp $	*/
 
 /*
  * Copyright (c) 2000, 2002 Manuel Bouyer.
@@ -33,7 +33,7 @@
 /* SYM53c7/8xx PCI-SCSI I/O Processors driver */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: siop_common.c,v 1.37 2005/02/27 00:27:02 perry Exp $");
+__KERNEL_RCSID(0, "$NetBSD: siop_common.c,v 1.38 2005/11/26 04:11:31 tsutsui Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -352,7 +352,7 @@ siop_wdtr_neg(siop_cmd)
 				sc->targets[target]->id |= (SCNTL3_EWS << 24);
 				break;
 			}
-		/* FALLTHROUH */
+		/* FALLTHROUGH */
 		default:
 			/*
  			 * hum, we got more than what we can handle, shouldn't
