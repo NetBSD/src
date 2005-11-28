987f534c84d309945ab8a5d28f461c0967f0faf1
riz <riz@NetBSD.org>
2005-11-28 01:16:24 +0000

    Welcome to NetBSD 3.0_RC4.

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.211.2.4 2005/11/22 21:11:24 tron Exp $	*/
+/*	$NetBSD: param.h,v 1.211.2.5 2005/11/28 01:16:24 riz Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -63,7 +63,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	300000000	/* NetBSD 3.0_RC3 */
+#define	__NetBSD_Version__	300000000	/* NetBSD 3.0_RC4 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) >= __NetBSD_Version__)
