91deb3e5d5ff406ca861cf1e2197a862a7c59d65
riz <riz@NetBSD.org>
2005-12-01 16:27:50 +0000

    Pull up following revision(s) (requested by oster in ticket #8833): 	include/ieeefp.h: revision 1.7 via patch PR/32108: Greg Oster: fpgetmask() linking failure with g++ on macppc Missing __{BEGIN,END}_DECLS.

--- include/ieeefp.h
+++ include/ieeefp.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: ieeefp.h,v 1.5 2000/06/13 01:21:53 simonb Exp $	*/
+/*	$NetBSD: ieeefp.h,v 1.5.10.1 2005/12/01 16:27:50 riz Exp $	*/
 
 /* 
  * Written by J.T. Conklin, Apr 6, 1995
@@ -11,11 +11,13 @@
 #include <sys/cdefs.h>
 #include <machine/ieeefp.h>
 
+__BEGIN_DECLS
 fp_rnd    fpgetround __P((void));
 fp_rnd    fpsetround __P((fp_rnd));
 fp_except fpgetmask __P((void));
 fp_except fpsetmask __P((fp_except));
 fp_except fpgetsticky __P((void));
 fp_except fpsetsticky __P((fp_except));
+__END_DECLS
 
 #endif /* _IEEEFP_H_ */
