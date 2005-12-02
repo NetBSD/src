820cc8772fd681e0841fba96743c2752331c01f2
kleink <kleink@NetBSD.org>
2005-12-02 19:26:14 +0000

    Fix AUDIO_DEBUG printf formatting of size_t; from Pavel Cahyna in PR kern/32207.

--- sys/dev/tc/bba.c
+++ sys/dev/tc/bba.c
@@ -1,4 +1,4 @@
-/* $NetBSD: bba.c,v 1.25 2005/06/01 16:17:40 drochner Exp $ */
+/* $NetBSD: bba.c,v 1.26 2005/12/02 19:26:14 kleink Exp $ */
 
 /*
  * Copyright (c) 2000 The NetBSD Foundation, Inc.
@@ -36,7 +36,7 @@
 /* maxine/alpha baseboard audio (bba) */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: bba.c,v 1.25 2005/06/01 16:17:40 drochner Exp $");
+__KERNEL_RCSID(0, "$NetBSD: bba.c,v 1.26 2005/12/02 19:26:14 kleink Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -300,7 +300,7 @@ bba_allocm(void *addr, int direction, size_t size,
 	int w;
 	int state;
 
-	DPRINTF(("bba_allocm: size = %d\n", size));
+	DPRINTF(("bba_allocm: size = %zu\n", size));
 	asc = addr;
 	sc = addr;
 	state = 0;
@@ -373,7 +373,7 @@ size_t
 bba_round_buffersize(void *addr, int direction, size_t size)
 {
 
-	DPRINTF(("bba_round_buffersize: size=%d\n", size));
+	DPRINTF(("bba_round_buffersize: size=%zu\n", size));
 	return size > BBA_DMABUF_SIZE ? BBA_DMABUF_SIZE :
 	    roundup(size, IOASIC_DMA_BLOCKSIZE);
 }
