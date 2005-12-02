086c7acca71fa9941f2a842750ddcc8f8d4beb65
skrll <skrll@NetBSD.org>
2005-12-02 09:33:48 +0000

    WARNS

--- lib/libc/arch/hppa/gen/fabs.c
+++ lib/libc/arch/hppa/gen/fabs.c
@@ -1,10 +1,12 @@
-/*	$NetBSD: fabs.c,v 1.1 2004/07/24 19:09:29 chs Exp $	*/
+/*	$NetBSD: fabs.c,v 1.2 2005/12/02 09:33:48 skrll Exp $	*/
 
 /*	$OpenBSD: fabs.c,v 1.3 2002/10/21 18:41:05 mickey Exp $	*/
 
 /*
  * Written by Miodrag Vallat.  Public domain
  */
+
+#include <math.h>
 
 double
 fabs(double val)
