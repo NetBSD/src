9042c0cd427d8234188be05b22264ba3d0814a97
tron <tron>
2005-12-06 16:55:30 +0000

    Pull up following revision(s) (requested by riz in ticket #10181): 	sys/dev/usb/usb_subr.c: revision 1.130 usbd_setup_pipe(): Don't call usbd_clear_endpoint_stall(). It's not necessary for normal devices, and it prevents some common (but apparantly buggy) devices from working, including the Apple iPod (mini and photo) and certain M-Systems DiskOnKey flash devices. If (also buggy) devices resurface that need this when they are attached, they can be addressed, ideally in each device's driver.

--- sys/dev/usb/usb_subr.c
+++ sys/dev/usb/usb_subr.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: usb_subr.c,v 1.111.2.1 2004/07/02 17:23:33 he Exp $	*/
+/*	$NetBSD: usb_subr.c,v 1.111.2.1.2.1 2005/12/06 16:55:30 tron Exp $	*/
 /*	$FreeBSD: src/sys/dev/usb/usb_subr.c,v 1.18 1999/11/17 22:33:47 n_hibma Exp $	*/
 
 /*
@@ -39,7 +39,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: usb_subr.c,v 1.111.2.1 2004/07/02 17:23:33 he Exp $");
+__KERNEL_RCSID(0, "$NetBSD: usb_subr.c,v 1.111.2.1.2.1 2005/12/06 16:55:30 tron Exp $");
 
 #include "opt_usbverbose.h"
 
@@ -791,15 +791,6 @@ usbd_setup_pipe(usbd_device_handle dev, usbd_interface_handle iface,
 			 ep->edesc->bEndpointAddress, usbd_errstr(err)));
 		free(p, M_USB);
 		return (err);
-	}
-	/* Clear any stall and make sure DATA0 toggle will be used next. */
-	if (UE_GET_ADDR(ep->edesc->bEndpointAddress) != USB_CONTROL_ENDPOINT) {
-		err = usbd_clear_endpoint_stall(p);
-		/* Some devices reject this command, so ignore a STALL. */
-		if (err && err != USBD_STALLED) {
-			printf("usbd_setup_pipe: failed to start endpoint, %s\n", usbd_errstr(err));
-			return (err);
-		}
 	}
 	*pipe = p;
 	return (USBD_NORMAL_COMPLETION);
