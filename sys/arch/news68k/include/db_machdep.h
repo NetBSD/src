12339991e0cb57010afea12c011178808110562e
tsutsui <tsutsui>
2005-12-06 16:40:37 +0000

    Remove defines DB_ELF_SYMBOLS and DB_ELFSIZE, which are defined in <m68k/db_machdep.h>.

--- sys/arch/news68k/include/db_machdep.h
+++ sys/arch/news68k/include/db_machdep.h
@@ -1,7 +1,4 @@
-/*	$NetBSD: db_machdep.h,v 1.2 2001/01/12 17:11:50 tsutsui Exp $	*/
+/*	$NetBSD: db_machdep.h,v 1.3 2005/12/06 16:40:37 tsutsui Exp $	*/
 
 /* Just use the common m68k definition */
 #include <m68k/db_machdep.h>
-
-#define	DB_ELF_SYMBOLS
-#define	DB_ELFSIZE	32
