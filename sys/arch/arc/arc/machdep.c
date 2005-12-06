603c87629e8d77be48b55f9f2980821301aac617
tsutsui <tsutsui@NetBSD.org>
2005-12-06 17:56:25 +0000

    Call arc_bus_space_malloc_set_safe() explicitly after VM initialization is complete.

--- sys/arch/arc/arc/machdep.c
+++ sys/arch/arc/arc/machdep.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: machdep.c,v 1.92 2005/11/05 09:50:50 tsutsui Exp $	*/
+/*	$NetBSD: machdep.c,v 1.93 2005/12/06 17:56:25 tsutsui Exp $	*/
 /*	$OpenBSD: machdep.c,v 1.36 1999/05/22 21:22:19 weingart Exp $	*/
 
 /*
@@ -78,7 +78,7 @@
 /* from: Utah Hdr: machdep.c 1.63 91/04/24 */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: machdep.c,v 1.92 2005/11/05 09:50:50 tsutsui Exp $");
+__KERNEL_RCSID(0, "$NetBSD: machdep.c,v 1.93 2005/12/06 17:56:25 tsutsui Exp $");
 
 #include "fs_mfs.h"
 #include "opt_ddb.h"
@@ -568,6 +568,8 @@ cpu_startup(void)
 #endif
 	format_bytes(pbuf, sizeof(pbuf), ptoa(uvmexp.free));
 	printf("avail memory = %s\n", pbuf);
+
+	arc_bus_space_malloc_set_safe();
 }
 
 int	waittime = -1;
