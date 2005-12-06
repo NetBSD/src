2bb1282bc6c626c80d70d3b24785de89068fc53b
tsutsui <tsutsui>
2005-12-06 17:05:02 +0000

    Remove defines DB_ELF_SYMBOLS and DB_ELFSIZE, which are defined in <m68k/db_machdep.h>.
    XXX: should we remove DB_AOUT_SYMBOLS from <m68k/db_machdep.h>, or
    XXX: should all m68k ports still have DB_AOUT_SYMBOLS?

--- sys/arch/hp300/include/db_machdep.h
+++ sys/arch/hp300/include/db_machdep.h
@@ -1,8 +1,6 @@
-/*	$NetBSD: db_machdep.h,v 1.3 2001/01/02 04:28:38 simonb Exp $	*/
+/*	$NetBSD: db_machdep.h,v 1.4 2005/12/06 17:05:02 tsutsui Exp $	*/
 
 /* Just use the common m68k definition */
 #include <m68k/db_machdep.h>
 
 #undef DB_AOUT_SYMBOLS
-#define	DB_ELF_SYMBOLS
-#define	DB_ELFSIZE 32
