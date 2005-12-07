bfca101847aa7ce987af2922f5d11dcb95efd672
tron <tron@NetBSD.org>
2005-12-07 18:20:02 +0000

    Pull up following revision(s) (requested by bouyer in ticket #1033): 	sys/dev/ic/aic7xxx_osm.c: revision 1.20 	sys/dev/ic/aic79xx_osm.c: revision 1.14 Wrap debug printf in AHC_DEBUG. Pointed out by Izumi Tsutsui.

--- sys/dev/ic/aic7xxx_osm.c
+++ sys/dev/ic/aic7xxx_osm.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: aic7xxx_osm.c,v 1.16.2.1 2005/11/29 10:23:14 tron Exp $	*/
+/*	$NetBSD: aic7xxx_osm.c,v 1.16.2.2 2005/12/07 18:20:02 tron Exp $	*/
 
 /*
  * Bus independent FreeBSD shim for the aic7xxx based adaptec SCSI controllers
@@ -39,7 +39,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: aic7xxx_osm.c,v 1.16.2.1 2005/11/29 10:23:14 tron Exp $");
+__KERNEL_RCSID(0, "$NetBSD: aic7xxx_osm.c,v 1.16.2.2 2005/12/07 18:20:02 tron Exp $");
 
 #include <dev/ic/aic7xxx_osm.h>
 #include <dev/ic/aic7xxx_inline.h>
@@ -331,7 +331,9 @@ ahc_action(struct scsipi_channel *chan, scsipi_adapter_req_t req, void *arg)
 		break;
 	  }
 	case ADAPTER_REQ_GROW_RESOURCES:
+#ifdef AHC_DEBUG
 		printf("%s: ADAPTER_REQ_GROW_RESOURCES\n", ahc_name(ahc));
+#endif
   		chan->chan_adapter->adapt_openings += ahc_alloc_scbs(ahc);
 		if (ahc->scb_data->numscbs >= AHC_SCB_MAX_ALLOC)
 			chan->chan_flags &= ~SCSIPI_CHAN_CANGROW;
