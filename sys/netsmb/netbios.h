de6cbddc1dbbb6c0454f84afeceb55df572317ec
christos <christos@NetBSD.org>
2005-12-11 06:25:32 +0000

    These headers are used by mount_smb.

--- sys/netsmb/netbios.h
+++ sys/netsmb/netbios.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: netbios.h,v 1.6 2005/12/11 00:06:21 elad Exp $	*/
+/*	$NetBSD: netbios.h,v 1.7 2005/12/11 06:25:32 christos Exp $	*/
 
 /*
  * Copyright (c) 2000-2001 Boris Popov
@@ -35,10 +35,6 @@
  */
 #ifndef _NETSMB_NETBIOS_H_
 #define	_NETSMB_NETBIOS_H_
-
-#ifndef _KERNEL
-#error not supposed to be exposed to userland.
-#endif /* !_KERNEL */
 
 /*
  * make this file dirty...
