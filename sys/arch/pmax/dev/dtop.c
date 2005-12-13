1dbe4d3deecbbdbec0331101224ccbd4f2040491
christos <christos>
2005-12-13 22:08:58 +0000

    p -> l

--- sys/arch/pmax/dev/dtop.c
+++ sys/arch/pmax/dev/dtop.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: dtop.c,v 1.70 2005/12/11 12:18:36 christos Exp $	*/
+/*	$NetBSD: dtop.c,v 1.71 2005/12/13 22:08:58 christos Exp $	*/
 
 /*-
  * Copyright (c) 1992, 1993
@@ -90,7 +90,7 @@ SOFTWARE.
 ********************************************************/
 
 #include <sys/cdefs.h>			/* RCS ID & Copyright macro defns */
-__KERNEL_RCSID(0, "$NetBSD: dtop.c,v 1.70 2005/12/11 12:18:36 christos Exp $");
+__KERNEL_RCSID(0, "$NetBSD: dtop.c,v 1.71 2005/12/13 22:08:58 christos Exp $");
 
 #include "opt_ddb.h"
 #include "rasterconsole.h"
@@ -335,7 +335,7 @@ dtopopen(dev, flag, mode, l)
 		(void) dtopparam(tp, &tp->t_termios);
 		ttsetwater(tp);
 	} else if ((tp->t_state & TS_XCLUDE)
-	    && suser(p->p_ucred, &p->p_acflag) != 0)
+	    && suser(l->l_proc->p_ucred, &l->l_proc->p_acflag) != 0)
 		return (EBUSY);
 	s = spltty();
 	while (!(flag & O_NONBLOCK) && !(tp->t_cflag & CLOCAL) &&
