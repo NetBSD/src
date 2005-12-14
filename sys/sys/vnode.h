3f94414e1bda1a0a173f4d0adfa8bfe2a50eff53
reinoud <reinoud@NetBSD.org>
2005-12-14 21:57:11 +0000

    Add entry for in enum vtagtype and VNODE_TAGS for UDF vnodes.

--- sys/sys/vnode.h
+++ sys/sys/vnode.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: vnode.h,v 1.146 2005/12/11 12:25:21 christos Exp $	*/
+/*	$NetBSD: vnode.h,v 1.147 2005/12/14 21:57:11 reinoud Exp $	*/
 
 /*
  * Copyright (c) 1989, 1993
@@ -71,7 +71,7 @@ enum vtagtype	{
 	VT_FDESC, VT_PORTAL, VT_NULL, VT_UMAP, VT_KERNFS, VT_PROCFS,
 	VT_AFS, VT_ISOFS, VT_UNION, VT_ADOSFS, VT_EXT2FS, VT_CODA,
 	VT_FILECORE, VT_NTFS, VT_VFS, VT_OVERLAY, VT_SMBFS, VT_PTYFS,
-	VT_TMPFS
+	VT_TMPFS, VT_UDF
 };
 
 #define VNODE_TAGS \
@@ -79,7 +79,7 @@ enum vtagtype	{
     "VT_FDESC", "VT_PORTAL", "VT_NULL", "VT_UMAP", "VT_KERNFS", "VT_PROCFS", \
     "VT_AFS", "VT_ISOFS", "VT_UNION", "VT_ADOSFS", "VT_EXT2FS", "VT_CODA", \
     "VT_FILECORE", "VT_NTFS", "VT_VFS", "VT_OVERLAY", "VT_SMBFS", "VT_PTYFS", \
-    "VT_TMPFS"
+    "VT_TMPFS", "VT_UDF"
 
 LIST_HEAD(buflists, buf);
 
