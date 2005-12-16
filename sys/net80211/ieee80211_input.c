d69be51a8710c887804d2d225593e2df24226097
dyoung <dyoung@NetBSD.org>
2005-12-16 10:04:58 +0000

    Cosmetic: normalize whitespace.

--- sys/net80211/ieee80211_input.c
+++ sys/net80211/ieee80211_input.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: ieee80211_input.c,v 1.53 2005/12/08 23:08:39 dyoung Exp $	*/
+/*	$NetBSD: ieee80211_input.c,v 1.54 2005/12/16 10:04:58 dyoung Exp $	*/
 /*-
  * Copyright (c) 2001 Atsushi Onoe
  * Copyright (c) 2002-2005 Sam Leffler, Errno Consulting
@@ -36,7 +36,7 @@
 __FBSDID("$FreeBSD: src/sys/net80211/ieee80211_input.c,v 1.81 2005/08/10 16:22:29 sam Exp $");
 #endif
 #ifdef __NetBSD__
-__KERNEL_RCSID(0, "$NetBSD: ieee80211_input.c,v 1.53 2005/12/08 23:08:39 dyoung Exp $");
+__KERNEL_RCSID(0, "$NetBSD: ieee80211_input.c,v 1.54 2005/12/16 10:04:58 dyoung Exp $");
 #endif
 
 #include "opt_inet.h"
@@ -2045,14 +2045,14 @@ ieee80211_recv_mgmt(struct ieee80211com *ic, struct mbuf *m0,
 			} else if (ni == ic->ic_bss &&
 			           !IEEE80211_ADDR_EQ(wh->i_addr3,
 				                      ni->ni_bssid)) {
-                                /* Mark a change of BSSID.  If
-                                 * ic_des_bssid is set, ni does
-                                 * not now represent a network we
-                                 * want to belong to, so start a
-                                 * scan.  Otherwise, make a RUN->RUN
-                                 * transition to give the driver
-                                 * an opportunity to reprogram its
-                                 * BSSID filter.
+				/* Mark a change of BSSID.  If
+				 * ic_des_bssid is set, ni does
+				 * not now represent a network we
+				 * want to belong to, so start a
+				 * scan.  Otherwise, make a RUN->RUN
+				 * transition to give the driver
+				 * an opportunity to reprogram its
+				 * BSSID filter.
 				 */
 				IEEE80211_ADDR_COPY(ni->ni_bssid, wh->i_addr3);
 				if (ic->ic_flags & IEEE80211_F_DESBSSID) {
