e08b4b601cb3116bd199b2bb2ec85e39fd0b79cb
riz <riz@NetBSD.org>
2005-12-29 15:09:37 +0000

    Pull up following revision(s) (requested by dillo in ticket #1039): 	usr.sbin/pkg_install/add/perform.c: revision 1.115 (pkg_do) When marking already installed package as manually installed, 	 only print message if we succeeded. Noted by wiz.

--- usr.sbin/pkg_install/add/perform.c
+++ usr.sbin/pkg_install/add/perform.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: perform.c,v 1.107.2.6 2005/11/27 15:46:04 riz Exp $	*/
+/*	$NetBSD: perform.c,v 1.107.2.7 2005/12/29 15:09:37 riz Exp $	*/
 
 #if HAVE_CONFIG_H
 #include "config.h"
@@ -14,7 +14,7 @@
 #if 0
 static const char *rcsid = "from FreeBSD Id: perform.c,v 1.44 1997/10/13 15:03:46 jkh Exp";
 #else
-__RCSID("$NetBSD: perform.c,v 1.107.2.6 2005/11/27 15:46:04 riz Exp $");
+__RCSID("$NetBSD: perform.c,v 1.107.2.7 2005/12/29 15:09:37 riz Exp $");
 #endif
 #endif
 
@@ -511,10 +511,10 @@ pkg_do(const char *pkg, lpkg_head_t *pkgs)
 	/* See if this package (exact version) is already registered */
 	if ((isdir(LogDir) || islinktodir(LogDir)) && !Force) {
 		if (!Automatic && is_automatic_installed(LogDir)) {
-			mark_as_automatic_installed(LogDir, 0);
-			warnx("package `%s' was already installed as "
-			      "dependency, now marked as installed manually",
-			      PkgName);
+			if (mark_as_automatic_installed(LogDir, 0) == 0)
+				warnx("package `%s' was already installed as "
+				      "dependency, now marked as installed "
+				      "manually", PkgName);
 		} else {
 			warnx("package `%s' already recorded as installed",
 			      PkgName);
