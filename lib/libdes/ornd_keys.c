0a3f7e1d9e709601ca52375b764dfd7b9f15c91d
riz <riz@NetBSD.org>
2005-12-29 16:13:06 +0000

    Pull up following revision(s) (requested by martin in ticket #1056): 	lib/libdes/ornd_keys.c: revision 1.2 Fix wrong buffer size calculation. From Henning Petersen in PR lib/32291.

--- lib/libdes/ornd_keys.c
+++ lib/libdes/ornd_keys.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: ornd_keys.c,v 1.1 2003/07/24 15:14:20 itojun Exp $	*/
+/*	$NetBSD: ornd_keys.c,v 1.1.6.1 2005/12/29 16:13:06 riz Exp $	*/
 
 #include "des_locl.h"
 #include <sys/time.h>
@@ -72,7 +72,7 @@ des_init_random_number_generator(des_cblock *seed)
 
 	SHA1Init(&sha);
 
-	gethostname(hname, sizeof(hname - 1));
+	gethostname(hname, sizeof(hname) - 1);
 	gettimeofday(&when, NULL);
 
 	memcpy(&seed_q, seed, sizeof(seed_q));
