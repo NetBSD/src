ce6283afb27efde785ac5af9a9d307f0a07b699b
uwe <uwe@NetBSD.org>
2006-01-02 23:32:11 +0000

    Prepend "cpu0: " to autoconfiguration messages. While here move separator space to the beginning of optional messages. Tweak write-through/back message so that is parses as expected.
    PR: port-sh3/31315

--- sys/arch/sh3/sh3/cache.c
+++ sys/arch/sh3/sh3/cache.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: cache.c,v 1.9 2005/12/24 23:24:02 perry Exp $	*/
+/*	$NetBSD: cache.c,v 1.10 2006/01/02 23:32:11 uwe Exp $	*/
 
 /*-
  * Copyright (c) 2002 The NetBSD Foundation, Inc.
@@ -37,7 +37,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: cache.c,v 1.9 2005/12/24 23:24:02 perry Exp $");
+__KERNEL_RCSID(0, "$NetBSD: cache.c,v 1.10 2006/01/02 23:32:11 uwe Exp $");
 
 #include "opt_memsize.h"	/* IOM_RAM_BEGIN */
 
@@ -104,44 +104,45 @@ sh_cache_information()
 #endif
 
 	/* I-cache or I/D-unified cache */
-	printf("%dKB/%dB ", sh_cache_size_icache >> 10, sh_cache_line_size);
+	printf("cpu0: %dKB/%dB",
+	       sh_cache_size_icache >> 10, sh_cache_line_size);
 	if (sh_cache_ways > 1)
-		printf("%d-way set-associative ", sh_cache_ways);
+		printf(" %d-way set-associative", sh_cache_ways);
 	else
-		printf("direct-mapped ");
+		printf(" direct-mapped");
 	if (sh_cache_unified)
-		printf("I/D-unified ");
+		printf(" I/D-unified");
 	else
-		printf("Instruction ");
-	printf("cache. ");
+		printf(" Instruction");
+	printf(" cache.");
 	if (!sh_cache_enable_icache)
-		printf("DISABLED ");
+		printf(" DISABLED");
 	if (sh_cache_unified && sh_cache_ram_mode)
-		printf("RAM-mode ");
+		printf(" RAM-mode");
 	if (sh_cache_index_mode_icache)
-		printf("INDEX-mode ");
+		printf(" INDEX-mode");
 	printf("\n");
 
 	/* D-cache */
 	if (!sh_cache_unified) {
-		printf("%dKB/%dB ", sh_cache_size_dcache >> 10,
+		printf("cpu0: %dKB/%dB", sh_cache_size_dcache >> 10,
 		    sh_cache_line_size);
 		if (sh_cache_ways > 1)
-			printf("%d-way set-associative ", sh_cache_ways);
+			printf(" %d-way set-associative", sh_cache_ways);
 		else
-			printf("direct-mapped ");
-		printf("Data cache. ");
+			printf(" direct-mapped");
+		printf(" Data cache.");
 		if (!sh_cache_enable_dcache)
-			printf("DISABLED ");
+			printf(" DISABLED");
 		if (sh_cache_ram_mode)
-			printf("RAM-mode ");
+			printf(" RAM-mode");
 		if (sh_cache_index_mode_dcache)
-			printf("INDEX-mode ");
+			printf(" INDEX-mode");
 		printf("\n");
 	}
 
 	/* Write-through/back */
-	printf("P0, U0, P3 write-%s P1 write-%s\n",
+	printf("cpu0: P0, U0, P3 write-%s; P1 write-%s\n",
 	    sh_cache_write_through_p0_u0_p3 ? "through" : "back",
 	    sh_cache_write_through_p1 ? "through" : "back");
 }
