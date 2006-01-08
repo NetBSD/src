aaa093a14fad1eec68f72c020fbb420aefc9d00d
oster <oster@NetBSD.org>
2006-01-08 22:26:30 +0000

    Really move all of the bp initalization bits into InitBP().

--- sys/dev/raidframe/rf_netbsdkintf.c
+++ sys/dev/raidframe/rf_netbsdkintf.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: rf_netbsdkintf.c,v 1.198 2006/01/08 22:06:26 oster Exp $	*/
+/*	$NetBSD: rf_netbsdkintf.c,v 1.199 2006/01/08 22:26:30 oster Exp $	*/
 /*-
  * Copyright (c) 1996, 1997, 1998 The NetBSD Foundation, Inc.
  * All rights reserved.
@@ -146,7 +146,7 @@
  ***********************************************************/
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: rf_netbsdkintf.c,v 1.198 2006/01/08 22:06:26 oster Exp $");
+__KERNEL_RCSID(0, "$NetBSD: rf_netbsdkintf.c,v 1.199 2006/01/08 22:26:30 oster Exp $");
 
 #include <sys/param.h>
 #include <sys/errno.h>
@@ -1858,9 +1858,6 @@ rf_DispatchKernelIO(RF_DiskQueue_t *queue, RF_DiskQueueData_t *req)
 			(int) (req->numSector <<
 			    queue->raidPtr->logBytesPerSector),
 			(int) queue->raidPtr->logBytesPerSector));
-		if ((bp->b_flags & B_READ) == 0) {
-			bp->b_vp->v_numoutput++;
-		}
 		VOP_STRATEGY(bp->b_vp, bp);
 
 		break;
@@ -1972,6 +1969,9 @@ InitBP(struct buf *bp, struct vnode *b_vp, unsigned rw_flag, dev_t dev,
 	bp->b_iodone = cbFunc;
 	bp->b_fspriv.bf_private = cbArg;
 	bp->b_vp = b_vp;
+	if ((bp->b_flags & B_READ) == 0) {
+		bp->b_vp->v_numoutput++;
+	}
 
 }
 
