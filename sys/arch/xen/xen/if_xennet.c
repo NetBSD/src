ecfa7ed69b1a564b5699caff218d88b8c74ae17b
bouyer <bouyer@NetBSD.org>
2006-01-26 19:17:25 +0000

    Fif off-by-one error: the last byte of a packet is at offset len - 1, not len. Should fix KASSERT panic reported by Mike M. Volokhov on port-xen.

--- sys/arch/xen/xen/if_xennet.c
+++ sys/arch/xen/xen/if_xennet.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: if_xennet.c,v 1.40 2006/01/08 14:45:41 bouyer Exp $	*/
+/*	$NetBSD: if_xennet.c,v 1.41 2006/01/26 19:17:25 bouyer Exp $	*/
 
 /*
  *
@@ -33,7 +33,7 @@
 
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: if_xennet.c,v 1.40 2006/01/08 14:45:41 bouyer Exp $");
+__KERNEL_RCSID(0, "$NetBSD: if_xennet.c,v 1.41 2006/01/26 19:17:25 bouyer Exp $");
 
 #include "opt_inet.h"
 #include "opt_nfs_boot.h"
@@ -1072,7 +1072,7 @@ xennet_softstart(void *arg)
 		}
 
 		if (m->m_pkthdr.len != m->m_len ||
-		    (pa ^ (pa + m->m_pkthdr.len)) & PG_FRAME) {
+		    (pa ^ (pa + m->m_pkthdr.len - 1)) & PG_FRAME) {
 
 			MGETHDR(new_m, M_DONTWAIT, MT_DATA);
 			if (__predict_false(new_m == NULL)) {
@@ -1108,7 +1108,7 @@ xennet_softstart(void *arg)
 		} else
 			IFQ_DEQUEUE(&ifp->if_snd, m);
 
-		KASSERT(((pa ^ (pa + m->m_pkthdr.len)) & PG_FRAME) == 0);
+		KASSERT(((pa ^ (pa + m->m_pkthdr.len -  1)) & PG_FRAME) == 0);
 
 		bufid = get_bufarray_entry(sc->sc_tx_bufa);
 		KASSERT(bufid < NETIF_TX_RING_SIZE);
