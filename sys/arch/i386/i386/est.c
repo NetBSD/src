c228d338e188f8b30c8c027423ee2809dee7959f
tron <tron@NetBSD.org>
2006-01-27 23:09:14 +0000

    Pull up following revision(s) (requested by xtraeme in ticket #1134): 	sys/arch/i386/i386/est.c: revision 1.15 PR port-i386/32606: Add support for the Pentium M 710.

--- sys/arch/i386/i386/est.c
+++ sys/arch/i386/i386/est.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: est.c,v 1.7.2.2 2005/12/31 16:40:59 tron Exp $	*/
+/*	$NetBSD: est.c,v 1.7.2.3 2006/01/27 23:09:14 tron Exp $	*/
 /*
  * Copyright (c) 2003 Michael Eriksson.
  * All rights reserved.
@@ -84,7 +84,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: est.c,v 1.7.2.2 2005/12/31 16:40:59 tron Exp $");
+__KERNEL_RCSID(0, "$NetBSD: est.c,v 1.7.2.3 2006/01/27 23:09:14 tron Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -243,6 +243,15 @@ static const struct fq_info pentium_m_n758[] = {
 };
 #endif
 
+/* Intel Pentium M processor 710 1.4 Ghz */
+static const struct fq_info pentium_m_n710[] = {
+	{ 1400, 1340 },
+	{ 1200, 1228 },
+	{ 1000, 1148 },
+	{  800, 1068 },
+	{  600,  998 }
+};
+
 /* Intel Pentium M processor 715 1.5 GHz */
 static const struct fq_info pentium_m_n715[] = {
 	{ 1500, 1340 },
@@ -346,6 +355,7 @@ static const struct fqlist pentium_m_dothan[] = {
 #if 0
 	ENTRY("1.50", pentium_m_n758),
 #endif
+	ENTRY("1.40", pentium_m_n710),
 	ENTRY("1.50", pentium_m_n715),
 	ENTRY("1.60", pentium_m_n725),
 	ENTRY("1.70", pentium_m_n735),
