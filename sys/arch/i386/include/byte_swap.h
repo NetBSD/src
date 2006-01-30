0b78aa2057605a868c984cc8a640e3b126f3a679
dsl <dsl>
2006-01-30 22:46:35 +0000

    Move all the stuff that detects bswapxx(constant) into the MI sys/bswap.h Put the minimum to define the required inline assembler or C into the MD files. NB: there may be some fallout from this!

--- sys/arch/i386/include/byte_swap.h
+++ sys/arch/i386/include/byte_swap.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: byte_swap.h,v 1.9 2005/12/28 18:40:13 perry Exp $	*/
+/*	$NetBSD: byte_swap.h,v 1.10 2006/01/30 22:46:36 dsl Exp $	*/
 
 /*-
  * Copyright (c) 1998 The NetBSD Foundation, Inc.
@@ -45,12 +45,14 @@
 #include "opt_cputype.h"
 #endif
 
+#ifdef  __GNUC__
+#include <sys/types.h>
+__BEGIN_DECLS
 
-static __inline uint32_t __byte_swap_long_variable(uint32_t);
-static __inline uint16_t __byte_swap_word_variable(uint16_t);
-
+#define	__BYTE_SWAP_U32_VARIABLE __byte_swap_u32_variable
+static __inline uint32_t __byte_swap_u32_variable(uint32_t);
 static __inline uint32_t
-__byte_swap_long_variable(uint32_t x)
+__byte_swap_u32_variable(uint32_t x)
 {
 	__asm volatile (
 #if defined(_KERNEL) && !defined(_LKM) && !defined(I386_CPU)
@@ -62,35 +64,16 @@ __byte_swap_long_variable(uint32_t x)
 	return (x);
 }
 
+#define	__BYTE_SWAP_U16_VARIABLE __byte_swap_u16_variable
+static __inline uint16_t __byte_swap_u16_variable(uint16_t);
 static __inline uint16_t
-__byte_swap_word_variable(uint16_t x)
+__byte_swap_u16_variable(uint16_t x)
 {
 	__asm volatile ("rorw $8, %w1" : "=r" (x) : "0" (x)); 
 	return (x);
 }
 
-#ifdef __OPTIMIZE__
-
-#define	__byte_swap_long_constant(x) \
-	((((x) & 0xff000000) >> 24) | \
-	 (((x) & 0x00ff0000) >>  8) | \
-	 (((x) & 0x0000ff00) <<  8) | \
-	 (((x) & 0x000000ff) << 24))
-#define	__byte_swap_word_constant(x) \
-	((((x) & 0xff00) >> 8) | \
-	 (((x) & 0x00ff) << 8))
-#define	__byte_swap_long(x) \
-	(__builtin_constant_p((x)) ? \
-	 __byte_swap_long_constant(x) : __byte_swap_long_variable(x))
-#define	__byte_swap_word(x) \
-	(__builtin_constant_p((x)) ? \
-	 __byte_swap_word_constant(x) : __byte_swap_word_variable(x))
-
-#else /* __OPTIMIZE__ */
-
-#define	__byte_swap_long(x)	__byte_swap_long_variable(x)
-#define	__byte_swap_word(x)	__byte_swap_word_variable(x)
-
-#endif /* __OPTIMIZE__ */
+__END_DECLS
+#endif
 
 #endif /* !_I386_BYTE_SWAP_H_ */
