a87fbd0889a4cd2e9400311e492c328fbe87f0ed
dsl <dsl>
2006-01-30 21:52:38 +0000

    Move the definitions of ntohl() and friends into sys/endian.h where they are defined in terms of bswap32() and bswap16(). This makes the definition be in the same place for all systems regardless of creed^Wendianness.

--- sys/arch/sh5/include/endian_machdep.h
+++ sys/arch/sh5/include/endian_machdep.h
@@ -1,20 +1,7 @@
-/*	$NetBSD: endian_machdep.h,v 1.5 2005/12/11 12:19:00 christos Exp $	*/
+/*	$NetBSD: endian_machdep.h,v 1.6 2006/01/30 21:52:38 dsl Exp $	*/
 
 #ifdef __LITTLE_ENDIAN__
 #define _BYTE_ORDER     _LITTLE_ENDIAN
 #else
 #define _BYTE_ORDER     _BIG_ENDIAN
 #endif
-
-#ifdef	__GNUC__
-
-#include <sh5/byte_swap.h>
-
-#if _BYTE_ORDER == _LITTLE_ENDIAN
-#define	ntohl(x)	((uint32_t)_sh5_bswap32((uint32_t)(x)))
-#define	ntohs(x)	((uint16_t)_sh5_bswap16((uint16_t)(x)))
-#define	htonl(x)	((uint32_t)_sh5_bswap32((uint32_t)(x)))
-#define	htons(x)	((uint16_t)_sh5_bswap16((uint16_t)(x)))
-#endif
-
-#endif	/* __GNUC__ */
