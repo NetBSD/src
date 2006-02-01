26f23870a7247092f49ec0b245082e4fb54958ce
dsl <dsl@NetBSD.org>
2006-02-01 20:56:18 +0000

    Need to install byte_swap.h and bswap.h needs to include sh3/byte_swap.h.

--- sys/arch/sh3/include/bswap.h
+++ sys/arch/sh3/include/bswap.h
@@ -1,9 +1,9 @@
-/*      $NetBSD: bswap.h,v 1.6 2006/01/31 07:58:56 dsl Exp $      */
+/*      $NetBSD: bswap.h,v 1.7 2006/02/01 20:56:18 dsl Exp $      */
 
 #ifndef _SH3_BSWAP_H_
 #define	_SH3_BSWAP_H_
 
-#include <machine/byte_swap.h>
+#include <sh3/byte_swap.h>
 
 #include <sys/bswap.h>
 
