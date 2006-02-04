3d3b19a4339c7d98b4c9f177e5c06ed38660f479
jmmv <jmmv>
2006-02-04 11:24:42 +0000

    Revert yesterday's change that attempted to fix the detection of the boot device when using a Multiboot boot loader.  It couldn't work because these boot loaders do not pass a checksum of the disk so matchbiosdisk() cannot really find any matches.  I should have gone to sleep before commiting...
    Found by xtraeme@.

--- sys/arch/x86/x86/x86_autoconf.c
+++ sys/arch/x86/x86/x86_autoconf.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: x86_autoconf.c,v 1.7 2006/02/03 23:33:30 jmmv Exp $	*/
+/*	$NetBSD: x86_autoconf.c,v 1.8 2006/02/04 11:24:42 jmmv Exp $	*/
 
 /*-
  * Copyright (c) 1990 The Regents of the University of California.
@@ -52,8 +52,6 @@ __KERNEL_RCSID(0, "$NetBSD");
 #include <sys/proc.h>
 #include <sys/md5.h>
 
-#include <x86/autoconf.h>
-
 #include <machine/bootinfo.h>
 
 #include "pci.h"
@@ -84,8 +82,8 @@ is_valid_disk(struct device *dv)
  * XXX Ugly bit of code.  But, this is the only safe time that the
  * match between BIOS disks and native disks can be done.
  */
-void
-x86_matchbiosdisks(void)
+static void
+matchbiosdisks(void)
 {
 	struct btinfo_biosgeom *big;
 	struct bi_biosgeom_entry *be;
@@ -201,35 +199,6 @@ x86_matchbiosdisks(void)
 			vput(tv);
 		}
 	}
-}
-
-const char *
-x86_findbiosdisk(int devnum)
-{
-	int i;
-
-	KASSERT(x86_alldisks != NULL);
-
-	for (i = 0; i < x86_alldisks->dl_nnativedisks; i++) {
-		int j;
-		struct nativedisk_info *ni;
-
-		ni = &x86_alldisks->dl_nativedisks[i];
-
-		for (j = 0; j < ni->ni_nmatches; j++) {
-			int k;
-			struct biosdisk_info *bi;
-
-			k = ni->ni_biosmatches[j];
-			KASSERT(k < x86_alldisks->dl_nbiosdisks);
-			bi = &x86_alldisks->dl_biosdisks[k];
-
-			if (bi->bi_dev == devnum)
-				return ni->ni_devname;
-		}
-	}
-
-	return NULL;
 }
 
 /*
@@ -566,10 +535,11 @@ findroot(void)
 }
 
 void
-x86_cpu_rootconf(void)
+cpu_rootconf(void)
 {
 
 	findroot();
+	matchbiosdisks();
 
 	if (booted_wedge) {
 		KASSERT(booted_device != NULL);
