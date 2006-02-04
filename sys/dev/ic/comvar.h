28b670b9f655e204e6058677e8bffa136c011f72
simonb <simonb@NetBSD.org>
2006-02-04 14:00:40 +0000

    Adapt for timecounters and newer PPS API.

--- sys/dev/ic/comvar.h
+++ sys/dev/ic/comvar.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: comvar.h,v 1.50 2005/12/27 00:46:38 chs Exp $	*/
+/*	$NetBSD: comvar.h,v 1.50.6.1 2006/02/04 14:00:40 simonb Exp $	*/
 
 /*
  * Copyright (c) 1996 Christopher G. Demetriou.  All rights reserved.
@@ -137,12 +137,16 @@ struct com_softc {
 	void (*disable)(struct com_softc *);
 	int enabled;
 
+#ifdef __HAVE_TIMECOUNTER
+	struct pps_state sc_pps_state;	/* pps state */
+#else /* !__HAVE_TIMECOUNTER */
 	/* PPS signal on DCD, with or without inkernel clock disciplining */
 	u_char	sc_ppsmask;			/* pps signal mask */
 	u_char	sc_ppsassert;			/* pps leading edge */
 	u_char	sc_ppsclear;			/* pps trailing edge */
 	pps_info_t ppsinfo;
 	pps_params_t ppsparam;
+#endif /* !__HAVE_TIMECOUNTER */
 
 #if NRND > 0 && defined(RND_COM)
 	rndsource_element_t  rnd_source;
