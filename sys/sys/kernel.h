2d0358a706e54e87f996895ca0f989215801e9a6
simonb <simonb@NetBSD.org>
2006-02-04 11:45:33 +0000

    Add bintime structures and support functions from FreeBSD. Move kernel time related variables and declarations to <sys/timevar.h>.

--- sys/sys/kernel.h
+++ sys/sys/kernel.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: kernel.h,v 1.23 2005/12/11 12:25:20 christos Exp $	*/
+/*	$NetBSD: kernel.h,v 1.23.6.1 2006/02/04 11:45:33 simonb Exp $	*/
 
 /*-
  * Copyright (c) 1990, 1993
@@ -48,23 +48,7 @@ extern int hostnamelen;
 extern char domainname[MAXHOSTNAMELEN];
 extern int domainnamelen;
 
-extern volatile struct timeval mono_time;
-extern struct timeval boottime;
-extern volatile struct timeval time;
-
-extern int rtc_offset;		/* offset of rtc from UTC in minutes */
-
 extern int cold;		/* still working on startup */
-extern int tick;		/* usec per tick (1000000 / hz) */
-extern int hardclock_ticks;	/* # of hardclock ticks */
-extern int tickfix;		/* periodic tick adj. tick not integral */
-extern int tickfixinterval;	/* interval at which to apply adjustment */
-extern int tickadj;		/* "standard" clock skew, us./tick */
-extern int hz;			/* system clock's frequency */
-extern int stathz;		/* statistics clock's frequency */
-extern int profhz;		/* profiling clock's frequency */
-extern int lbolt;		/* once a second sleep address */
-
 extern int profsrc;		/* profiling source */
 
 #define PROFSRC_CLOCK	0
