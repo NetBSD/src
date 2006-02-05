b01f76e9c1f42c9d274ef4761807176b048e3bf9
christos <christos@NetBSD.org>
2006-02-05 02:37:45 +0000

    resolve conflicts

--- dist/openpam/lib/openpam_ttyconv.c
+++ dist/openpam/lib/openpam_ttyconv.c
@@ -31,7 +31,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- * $P4: //depot/projects/openpam/lib/openpam_ttyconv.c#25 $
+ * $P4: //depot/projects/openpam/lib/openpam_ttyconv.c#26 $
  */
 
 #include <sys/types.h>
