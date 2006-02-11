db8ecf5c49c64fe5e809b262546f2a2ac5e623dc
rpaulo <rpaulo@NetBSD.org>
2006-02-11 13:04:44 +0000

    PR 31304: Contribute to the random pool. From Rhialto.

--- sys/dev/pci/if_skvar.h
+++ sys/dev/pci/if_skvar.h
@@ -1,4 +1,4 @@
-/* $NetBSD: if_skvar.h,v 1.7 2005/12/11 12:22:49 christos Exp $ */
+/* $NetBSD: if_skvar.h,v 1.8 2006/02/11 13:04:44 rpaulo Exp $ */
 
 /*-
  * Copyright (c) 2003 The NetBSD Foundation, Inc.
@@ -86,6 +86,12 @@
 
 #ifndef _DEV_PCI_IF_SKVAR_H_
 #define _DEV_PCI_IF_SKVAR_H_
+
+#include "rnd.h"
+
+#if NRND > 0
+#include <sys/rnd.h>
+#endif
 
 struct sk_jslot {
 	caddr_t			sk_buf;
@@ -208,6 +214,9 @@ struct sk_softc {
 	u_int32_t		sk_intrmask;
 	bus_dma_tag_t		sc_dmatag;
 	struct sk_if_softc	*sk_if[2];
+#if NRND > 0
+	rndsource_element_t     rnd_source;
+#endif
 };
 
 /* Softc for each logical interface */
