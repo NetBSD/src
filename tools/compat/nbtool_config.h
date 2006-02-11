9e4a818d13214330cbf10b2aefbc2e5698ddce8e
wiz <wiz@NetBSD.org>
2006-02-11 10:35:29 +0000

    Redo previous differently: use AH_TOP and AH_BOTTOM macros to insert header and footer into nbtool_config.h.in directly.

--- tools/compat/nbtool_config.h
+++
@@ -1,9 +0,0 @@
-/*	$NetBSD: nbtool_config.h,v 1.1 2006/02/11 01:28:01 wiz Exp $	*/
- 
-#ifndef	__NETBSD_NBTOOL_CONFIG_H__
-#define	__NETBSD_NBTOOL_CONFIG_H__
-
-#include "nbtool_config_internal.h"
-
-#include "compat_defs.h"
-#endif /* !__NETBSD_NBTOOL_CONFIG_H__ */
