899d1b31b27c14151d46e11049b0d4fb2fb6b3b5
chs <chs@NetBSD.org>
2006-02-12 01:32:06 +0000

    convert "magiclinks" from a per-fs mount option to a system-wide sysctl. as discussed on tech-kern quite some time ago.

--- include/mntopts.h
+++ include/mntopts.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: mntopts.h,v 1.6 2005/06/23 00:30:29 thorpej Exp $	*/
+/*	$NetBSD: mntopts.h,v 1.7 2006/02/12 01:32:06 chs Exp $	*/
 
 /*-
  * Copyright (c) 1994
@@ -56,7 +56,6 @@ struct mntopt {
 #define MOPT_SYMPERM		{ "symperm",	0, MNT_SYMPERM, 0 }
 #define MOPT_SOFTDEP		{ "softdep",	0, MNT_SOFTDEP, 0 }
 #define MOPT_IGNORE		{ "hidden",	0, MNT_IGNORE, 0 }
-#define	MOPT_MAGICLINKS		{ "magiclinks",	0, MNT_MAGICLINKS, 0 }
 
 /* Control flags. */
 #define MOPT_FORCE		{ "force",	0, MNT_FORCE, 0 }
@@ -88,8 +87,7 @@ struct mntopt {
 	MOPT_RDONLY,							\
 	MOPT_UNION,							\
 	MOPT_IGNORE,							\
-	MOPT_SYMPERM,							\
-	MOPT_MAGICLINKS
+	MOPT_SYMPERM
 
 __BEGIN_DECLS
 typedef struct mntoptparse *mntoptparse_t;
