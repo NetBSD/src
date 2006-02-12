9048e27ffc158ff19b7e320ae213b815396947d6
chs <chs@NetBSD.org>
2006-02-12 01:12:48 +0000

    increase NKMEMPAGES_MAX_DEFAULT to 1 GB. this allows lots more memory to be used for amaps, etc.

--- sys/arch/amd64/include/param.h
+++ sys/arch/amd64/include/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.1 2003/04/26 18:39:45 fvdl Exp $	*/
+/*	$NetBSD: param.h,v 1.2 2006/02/12 01:12:48 chs Exp $	*/
 
 #ifdef _KERNEL
 #ifdef _LOCORE
@@ -107,7 +107,7 @@
  * logical pages.
  */
 #define	NKMEMPAGES_MIN_DEFAULT	((8 * 1024 * 1024) >> PAGE_SHIFT)
-#define	NKMEMPAGES_MAX_DEFAULT	((128 * 1024 * 1024) >> PAGE_SHIFT)
+#define	NKMEMPAGES_MAX_DEFAULT	((1 *1024 * 1024 * 1024) >> PAGE_SHIFT)
 
 /* pages ("clicks") to disk blocks */
 #define	ctod(x)		((x) << (PGSHIFT - DEV_BSHIFT))
