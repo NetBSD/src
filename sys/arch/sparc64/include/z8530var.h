1a509d615ad55440e6b7ded9f762a297c61b83eb
cdi <cdi@NetBSD.org>
2006-02-13 21:47:11 +0000

    ANSIfy: u_intN_t -> uintN_t, remove __P(), fix function definitions.

--- sys/arch/sparc64/include/z8530var.h
+++ sys/arch/sparc64/include/z8530var.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: z8530var.h,v 1.9 2005/12/11 12:19:10 christos Exp $	*/
+/*	$NetBSD: z8530var.h,v 1.10 2006/02/13 21:47:12 cdi Exp $	*/
 
 /*
  * Copyright (c) 1992, 1993
@@ -85,13 +85,13 @@ struct zsc_softc {
  * about the function call overhead where ZS_DELAY does nothing.
  */
 
-u_char zs_read_reg __P((struct zs_chanstate *cs, u_char reg));
-u_char zs_read_csr __P((struct zs_chanstate *cs));
-u_char zs_read_data __P((struct zs_chanstate *cs));
+u_char zs_read_reg(struct zs_chanstate *cs, u_char reg);
+u_char zs_read_csr(struct zs_chanstate *cs);
+u_char zs_read_data(struct zs_chanstate *cs);
 
-void  zs_write_reg __P((struct zs_chanstate *cs, u_char reg, u_char val));
-void  zs_write_csr __P((struct zs_chanstate *cs, u_char val));
-void  zs_write_data __P((struct zs_chanstate *cs, u_char val));
+void  zs_write_reg(struct zs_chanstate *cs, u_char reg, u_char val);
+void  zs_write_csr(struct zs_chanstate *cs, u_char val);
+void  zs_write_data(struct zs_chanstate *cs, u_char val);
 
 /* The sparc has splzs() in psl.h */
 
