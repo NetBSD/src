7ed857fc6e382b7b3a5186df12a94c53ba58fb95
gdamore <gdamore@NetBSD.org>
2006-02-16 01:55:17 +0000

    Reenable PCI on DBAU1500.  May still be useful for PIO devices.  Comments in the config are left intact, though. Add a PMAP-driven bus_space for access to upper memory, instead of using wired entries. Convert aupci to use said bus_space -- no measured performance impact.

--- sys/arch/mips/alchemy/include/auvar.h
+++ sys/arch/mips/alchemy/include/auvar.h
@@ -1,4 +1,4 @@
-/* $NetBSD: auvar.h,v 1.7 2006/02/10 20:49:14 gdamore Exp $ */
+/* $NetBSD: auvar.h,v 1.8 2006/02/16 01:55:17 gdamore Exp $ */
 
 /*-
  * Copyright (c) 2006 Itronix Inc.
@@ -70,33 +70,6 @@
 #define	_MIPS_ALCHEMY_AUVAR_H_
 
 #include "opt_alchemy.h"
-
-/*
- * 224MB virtual PCI memory.  Note that we probably cannot
- * really use all this because of limits on the number wired
- * entries we can have at once.  In all likelihood, we won't
- * use wired entries for more than a few megs anyway, since
- * big devices like framebuffers are likely to be mapped into
- * USEG using ordinary TLB entries.
- *
- * This tunable is an area for possible investigation and improvement.
- */
-
-#define	AU_PCI_MEM_VA		0xE0000000UL
-#define	AU_PCI_MEM_SZ		0x0B000000UL	/* 176 MB */
-
-#define	AU_PCI_IO_VA		0xEB000000UL
-#define	AU_PCI_IO_SZ		0x01000000UL	/* 16 MB */
-
-#define	AU_PCI_CFG_VA		0xEC000000UL
-#define	AU_PCI_CFG_SZ		0x01000000UL	/* 16 MB, overkill */
-
-#define	AU_PCIMCIA_IO_VA	0xED000000UL
-#define	AU_PCIMCCA_IO_SZ	0x01000000UL	/* 16 MB */
-#define	AU_PCIMCIA_ATTR_VA	0xEE000000UL
-#define	AU_PCIMCCA_ATTR_SZ	0x01000000UL	/* 16 MB */
-#define	AU_PCIMCIA_MEM_VA	0xEF000000UL
-#define	AU_PCIMCCA_MEM_SZ	0x01000000UL	/* 16 MB */
 
 struct au_dev {
 	const char *name;
