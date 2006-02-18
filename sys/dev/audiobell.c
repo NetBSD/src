f64360e144d34de051de80a9cd66be48fcec54d5
yamt <yamt>
2006-02-18 13:17:05 +0000

    revert the previous as it doesn't fix the problem.

--- sys/dev/audiobell.c
+++ sys/dev/audiobell.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: audiobell.c,v 1.2.2.2 2006/02/18 13:14:37 yamt Exp $	*/
+/*	$NetBSD: audiobell.c,v 1.2.2.3 2006/02/18 13:17:05 yamt Exp $	*/
 
 /*
  * Copyright (c) 1999 Richard Earnshaw
@@ -31,7 +31,7 @@
  */
 
 #include <sys/types.h>
-__KERNEL_RCSID(0, "$NetBSD: audiobell.c,v 1.2.2.2 2006/02/18 13:14:37 yamt Exp $");
+__KERNEL_RCSID(0, "$NetBSD: audiobell.c,v 1.2.2.3 2006/02/18 13:17:05 yamt Exp $");
 
 #include <sys/audioio.h>
 #include <sys/conf.h>
@@ -39,7 +39,6 @@ __KERNEL_RCSID(0, "$NetBSD: audiobell.c,v 1.2.2.2 2006/02/18 13:14:37 yamt Exp $
 #include <sys/fcntl.h>
 #include <sys/malloc.h>
 #include <sys/null.h>
-#include <sys/proc.h>
 #include <sys/systm.h>
 #include <sys/uio.h>
 
