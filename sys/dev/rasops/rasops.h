c45765f637952fea100bd775b0cee5daa16f96f4
jmcneill <jmcneill>
2006-02-18 13:57:33 +0000

    Add shadow framebuffer support.

--- sys/dev/rasops/rasops.h
+++ sys/dev/rasops/rasops.h
@@ -1,4 +1,4 @@
-/* 	$NetBSD: rasops.h,v 1.18 2005/12/11 12:23:43 christos Exp $ */
+/* 	$NetBSD: rasops.h,v 1.19 2006/02/18 13:57:33 jmcneill Exp $ */
 
 /*-
  * Copyright (c) 1999 The NetBSD Foundation, Inc.
@@ -58,6 +58,12 @@ struct rasops_info {
 	int	ri_width;	/* width (pels) */
 	int	ri_height;	/* height (pels) */
 	int	ri_stride;	/* stride in bytes */
+
+	/*
+	 * If you want shadow framebuffer support, point ri_hwbits
+	 * to the real framebuffer, and ri_bits to the shadow framebuffer
+	 */
+	u_char	*ri_hwbits;
 
 	/*
 	 * These can optionally be left zeroed out. If you fill ri_font,
