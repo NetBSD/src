a30cd8da69f2610db92bfda9ca0c730c715648dd
bjh21 <bjh21@NetBSD.org>
2006-02-24 11:46:20 +0000

    Medium-sized overhaul of POOL_SUBPAGE support so that: 1: I can understand it, and 2: It works. Notable externally-visible changes are that POOL_SUBPAGE now has to be a compile-time constant, and that trying to initialise a pool whose objects are larger than POOL_SUBPAGE automatically generates a pool that doesn't use subpages.
    NetBSD/acorn26 now boots multi-user again.

--- sys/sys/pool.h
+++ sys/sys/pool.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: pool.h,v 1.47 2005/12/01 13:21:05 yamt Exp $	*/
+/*	$NetBSD: pool.h,v 1.48 2006/02/24 11:46:20 bjh21 Exp $	*/
 
 /*-
  * Copyright (c) 1997, 1998, 1999, 2000 The NetBSD Foundation, Inc.
@@ -217,6 +217,11 @@ struct pool {
  */
 extern struct pool_allocator pool_allocator_kmem;
 extern struct pool_allocator pool_allocator_nointr;
+#ifdef POOL_SUBPAGE
+/* The above are subpage allocators in this case. */
+extern struct pool_allocator pool_allocator_kmem_fullpage;
+extern struct pool_allocator pool_allocator_nointr_fullpage;
+#endif
 
 struct link_pool_init {	/* same as args to pool_init() */
 	struct pool *pp;
