b8d57ba290bb4e5abee19170eca8ece0c563c118
he <he@NetBSD.org>
2006-02-26 21:50:49 +0000

    Fix one more missing double quote.

--- sys/arch/alpha/alpha/api_up1000.c
+++ sys/arch/alpha/alpha/api_up1000.c
@@ -1,4 +1,4 @@
-/* $NetBSD: api_up1000.c,v 1.20 2006/02/25 17:32:43 thorpej Exp $ */
+/* $NetBSD: api_up1000.c,v 1.21 2006/02/26 21:50:49 he Exp $ */
 
 /*
  * Copyright (c) 1995, 1996, 1997 Carnegie-Mellon University.
@@ -34,7 +34,7 @@
 
 #include <sys/cdefs.h>			/* RCS ID & Copyright macro defns */
 
-__KERNEL_RCSID(0, "$NetBSD: api_up1000.c,v 1.20 2006/02/25 17:32:43 thorpej Exp $");
+__KERNEL_RCSID(0, "$NetBSD: api_up1000.c,v 1.21 2006/02/26 21:50:49 he Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -236,7 +236,7 @@ api_up1000_device_register(dev, aux)
 
 	if (device_is_a(dev, "sd") ||
 	    device_is_a(dev, "st") ||
-	    device_is_a(dev, cd")) {
+	    device_is_a(dev, "cd")) {
 		struct scsipibus_attach_args *sa = aux;
 		struct scsipi_periph *periph = sa->sa_periph;
 		int unit;
