1cb7bc666eeada50de12063b001e17985158b2fe
martin <martin@NetBSD.org>
2006-03-06 08:26:20 +0000

    Add support for Moxa CP104 serial card, from Olivier Cherrier

--- sys/dev/pci/pucdata.c
+++ sys/dev/pci/pucdata.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: pucdata.c,v 1.42 2006/01/24 20:10:55 christos Exp $	*/
+/*	$NetBSD: pucdata.c,v 1.43 2006/03/06 08:26:20 martin Exp $	*/
 
 /*
  * Copyright (c) 1998, 1999 Christopher G. Demetriou.  All rights reserved.
@@ -36,7 +36,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: pucdata.c,v 1.42 2006/01/24 20:10:55 christos Exp $");
+__KERNEL_RCSID(0, "$NetBSD: pucdata.c,v 1.43 2006/03/06 08:26:20 martin Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -903,6 +903,18 @@ const struct puc_device_description puc_devices[] = {
 	    {	0xffff,	0xffff,	0,	0	},
 	    {
 		{ PUC_PORT_TYPE_LPT, 0x10, 0x00, 0x00 },
+	    },
+	},
+
+	/* Moxa Technologies Co., Ltd. PCI I/O Card 4S RS232 */
+	{   "Moxa Technologies, SmartIO CP104/PCI",
+	    {   0x1393, 0x1041, 0,      0    },
+	    {   0xffff, 0xffff, 0,      0    },
+	    {
+		{ PUC_PORT_TYPE_COM, 0x18, 0x00, COM_FREQ * 8 },
+		{ PUC_PORT_TYPE_COM, 0x18, 0x08, COM_FREQ * 8 },
+		{ PUC_PORT_TYPE_COM, 0x18, 0x10, COM_FREQ * 8 },
+		{ PUC_PORT_TYPE_COM, 0x18, 0x18, COM_FREQ * 8 },
 	    },
 	},
 
