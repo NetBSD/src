23bb6b3de9a6e6be4204239ac760944f1917f516
elad <elad@NetBSD.org>
2006-03-08 00:31:57 +0000

    Adapt to kernel authorization changes.

--- sys/uvm/uvm_mmap.c
+++ sys/uvm/uvm_mmap.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: uvm_mmap.c,v 1.94 2005/12/11 12:25:29 christos Exp $	*/
+/*	$NetBSD: uvm_mmap.c,v 1.94.10.1 2006/03/08 00:31:57 elad Exp $	*/
 
 /*
  * Copyright (c) 1997 Charles D. Cranor and Washington University.
@@ -51,7 +51,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: uvm_mmap.c,v 1.94 2005/12/11 12:25:29 christos Exp $");
+__KERNEL_RCSID(0, "$NetBSD: uvm_mmap.c,v 1.94.10.1 2006/03/08 00:31:57 elad Exp $");
 
 #include "opt_compat_netbsd.h"
 
@@ -455,7 +455,7 @@ sys_mmap(l, v, retval)
 			 */
 			if (fp->f_flag & FWRITE) {
 				if ((error =
-				    VOP_GETATTR(vp, &va, p->p_ucred, l)))
+				    VOP_GETATTR(vp, &va, p->p_cred, l)))
 					return (error);
 				if ((va.va_flags &
 				    (SF_SNAPSHOT|IMMUTABLE|APPEND)) == 0)
@@ -1115,7 +1115,7 @@ uvm_mmap(map, addr, size, prot, maxprot, flags, handle, foff, locklimit)
 			return (EACCES);
 
 		if (vp->v_type != VCHR) {
-			error = VOP_MMAP(vp, 0, curproc->p_ucred, curlwp);
+			error = VOP_MMAP(vp, 0, curproc->p_cred, curlwp);
 			if (error) {
 				return error;
 			}
