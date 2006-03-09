cd9eec7284a1110cee46a3f1b4d3407f442ed3f2
christos <christos>
2006-03-09 16:20:27 +0000

    Welcome to the new compat layout.

--- lib/libc/arch/m68k/gen/longjmp.c
+++ lib/libc/arch/m68k/gen/longjmp.c
@@ -1,12 +1,11 @@
-/*	$NetBSD: __semctl.S,v 1.1 1999/08/25 05:09:29 thorpej Exp $	*/
+/*	$NetBSD: longjmp.c,v 1.1 2006/03/09 16:20:27 christos Exp $	*/
 
 /*-
- * Copyright (c) 1999 The NetBSD Foundation, Inc.
+ * Copyright (c) 2003 The NetBSD Foundation, Inc.
  * All rights reserved.
  *
  * This code is derived from software contributed to The NetBSD Foundation
- * by Jason R. Thorpe of the Numerical Aerospace Simulation Facility,
- * NASA Ames Research Center.
+ * by Christian Limpach.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -18,8 +17,8 @@
  *    documentation and/or other materials provided with the distribution.
  * 3. All advertising materials mentioning features or use of this software
  *    must display the following acknowledgement:
- *	This product includes software developed by the NetBSD
- *	Foundation, Inc. and its contributors.
+ *        This product includes software developed by the NetBSD
+ *        Foundation, Inc. and its contributors.
  * 4. Neither the name of The NetBSD Foundation nor the names of its
  *    contributors may be used to endorse or promote products derived
  *    from this software without specific prior written permission.
@@ -37,6 +36,68 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include "SYS.h"
+#include "namespace.h"
+#include <sys/types.h>
+#include <ucontext.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <string.h>
 
-PSEUDO(__semctl,compat_14___semctl)
+#define __LIBC12_SOURCE__
+#include <setjmp.h>
+#include <compat/include/setjmp.h>
+
+typedef struct {
+	__greg_t	__data[6];
+	__greg_t	__addr[4];
+} __jmp_buf_regs_t;
+
+void
+__longjmp14(jmp_buf env, int val)
+{
+	struct sigcontext *sc = (void *)env;
+	__jmp_buf_regs_t *r = (void *)&sc[1];
+	ucontext_t uc;
+
+	/* Ensure non-zero SP */
+	if (sc->sc_sp == 0)
+		goto err;
+
+	/* Make return value non-zero */
+	if (val == 0)
+		val = 1;
+
+	/* Save return value in context */
+	uc.uc_mcontext.__gregs[_REG_D0] = val;
+
+	/*
+	 * Set _UC_SIGMASK, _UC_CPU and _UC_M68K_UC_USER
+	 * Set _UC_{SET,CLR}STACK according to SS_ONSTACK
+	 */
+	uc.uc_flags = _UC_SIGMASK | _UC_CPU | _UC_M68K_UC_USER |
+		(sc->sc_onstack ? _UC_SETSTACK : _UC_CLRSTACK);
+
+	/* Clear uc_link */
+	uc.uc_link = 0;
+
+	/* Copy signal mask */
+	uc.uc_sigmask = sc->sc_mask;
+
+	/* Copy SP/PC/PS/FP */
+	uc.uc_mcontext.__gregs[_REG_A7] = sc->sc_sp;
+	uc.uc_mcontext.__gregs[_REG_PC] = sc->sc_pc;
+	uc.uc_mcontext.__gregs[_REG_PS] = sc->sc_ps;
+	uc.uc_mcontext.__gregs[_REG_A6] = sc->sc_fp;
+
+	/* Copy remaining non-scratch regs. */
+	memcpy(&uc.uc_mcontext.__gregs[_REG_D2],
+	    &r->__data, sizeof(r->__data));
+	memcpy(&uc.uc_mcontext.__gregs[_REG_A2],
+	    &r->__addr, sizeof(r->__addr));
+
+	setcontext(&uc);
+ err:
+	longjmperror();
+	abort();
+	/* NOTREACHED */
+}
