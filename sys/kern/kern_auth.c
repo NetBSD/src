a9cccfe4760a15b178584692e5fcbbd599041854
elad <elad>
2006-03-10 11:53:55 +0000

    process_authorize() -> kauth_authorize_process(), to be closer to the original and as requested by yamt@ and thorpej@.

--- sys/kern/kern_auth.c
+++ sys/kern/kern_auth.c
@@ -1,4 +1,4 @@
-/* $NetBSD: kern_auth.c,v 1.1.2.10 2006/03/09 23:47:07 elad Exp $ */
+/* $NetBSD: kern_auth.c,v 1.1.2.11 2006/03/10 11:53:55 elad Exp $ */
 
 /*-
  * Copyright (c) 2005, 2006 Elad Efrat <elad@NetBSD.org>
@@ -885,8 +885,8 @@ kauth_authorize_cb_process(kauth_cred_t cred, kauth_action_t action,
  * Process scope authorization wrapper.
  */
 int
-process_authorize(kauth_cred_t cred, kauth_action_t action, struct proc *p,
-	       void *arg1, void *arg2, void *arg3)
+kauth_authorize_process(kauth_cred_t cred, kauth_action_t action,
+    struct proc *p, void *arg1, void *arg2, void *arg3)
 {
 	return (kauth_authorize_action(kauth_builtin_scope_process, cred,
 	    action, p, arg1, arg2, arg3));
