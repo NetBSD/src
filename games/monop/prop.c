c86272e15449510feb1690b6787c3ed5237f6fa9
christos <christos@NetBSD.org>
2006-03-19 00:18:47 +0000

    fix typo

--- games/monop/prop.c
+++ games/monop/prop.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: prop.c,v 1.10 2006/03/19 00:05:05 christos Exp $	*/
+/*	$NetBSD: prop.c,v 1.11 2006/03/19 00:18:47 christos Exp $	*/
 
 /*
  * Copyright (c) 1980, 1993
@@ -34,7 +34,7 @@
 #if 0
 static char sccsid[] = "@(#)prop.c	8.1 (Berkeley) 5/31/93";
 #else
-__RCSID("$NetBSD: prop.c,v 1.10 2006/03/19 00:05:05 christos Exp $");
+__RCSID("$NetBSD: prop.c,v 1.11 2006/03/19 00:18:47 christos Exp $");
 #endif
 #endif /* not lint */
 
@@ -120,7 +120,7 @@ del_list(plr, head, op_sqr)
 			break;
 		else
 			last_op = op;
-	if (op) == NULL)
+	if (op == NULL)
 		return;
 	if (last_op == NULL)
 		*head = op->next;
