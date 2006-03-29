75b2c1cb74e8131ce96a4db57dcff8b4ee0b1a50
christos <christos@NetBSD.org>
2006-03-29 16:09:53 +0000

    Coverity CID 2486: Fix uninitialized variable

--- dist/bind/lib/dns/adb.c
+++ dist/bind/lib/dns/adb.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: adb.c,v 1.1.1.4 2005/12/21 23:16:04 christos Exp $	*/
+/*	$NetBSD: adb.c,v 1.2 2006/03/29 16:09:53 christos Exp $	*/
 
 /*
  * Copyright (C) 2004, 2005  Internet Systems Consortium, Inc. ("ISC")
@@ -3094,7 +3094,7 @@ fetch_callback(isc_task_t *task, isc_event_t *ev) {
 	dns_fetchevent_t *dev;
 	dns_adbname_t *name;
 	dns_adb_t *adb;
-	dns_adbfetch_t *fetch;
+	dns_adbfetch_t *fetch = NULL;
 	int bucket;
 	isc_eventtype_t ev_status;
 	isc_stdtime_t now;
@@ -3126,7 +3126,7 @@ fetch_callback(isc_task_t *task, isc_event_t *ev) {
 		fetch = name->fetch_aaaa;
 		name->fetch_aaaa = NULL;
 	}
-	INSIST(address_type != 0);
+	INSIST(address_type != 0 && fetch != NULL);
 
 	dns_resolver_destroyfetch(&fetch->fetch);
 	dev->fetch = NULL;
