05de14a754d05a0c5682f4662fb9eef7e3f586a0
tls <tls@NetBSD.org>
2006-04-02 03:10:54 +0000

    Add "~+" as a synonym for "~C" for Taylor cu compatibility.

--- usr.bin/tip/cmdtab.c
+++ usr.bin/tip/cmdtab.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: cmdtab.c,v 1.5 2003/08/07 11:16:17 agc Exp $	*/
+/*	$NetBSD: cmdtab.c,v 1.6 2006/04/02 03:10:54 tls Exp $	*/
 
 /*
  * Copyright (c) 1983, 1993
@@ -34,7 +34,7 @@
 #if 0
 static char sccsid[] = "@(#)cmdtab.c	8.1 (Berkeley) 6/6/93";
 #endif
-__RCSID("$NetBSD: cmdtab.c,v 1.5 2003/08/07 11:16:17 agc Exp $");
+__RCSID("$NetBSD: cmdtab.c,v 1.6 2006/04/02 03:10:54 tls Exp $");
 #endif /* not lint */
 
 #include "tip.h"
@@ -49,6 +49,7 @@ esctable_t etable[] = {
 	{ '$',	NORM,	"pipe local command to remote host", pipeout },
 #ifdef CONNECT
 	{ 'C',  NORM,	"connect program to remote host",consh },
+	{ '+',	NORM,	"connect program to remote host",consh },
 #endif
 	{ 'c',	NORM,	"change directory",		 chdirectory },
 	{ '.',	NORM,	"exit from tip",		 finish },
