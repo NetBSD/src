385c82ec2e781c4f2871ed2a0212e2e3aa657d87
tron <tron@NetBSD.org>
2006-04-06 13:01:48 +0000

    Pull up following revision(s) (requested by oster in ticket #1243): 	sys/arch/m68k/include/signal.h: revision 1.23 We don't need (or want!) to include cpuframe.h or have sigstate visible to userland on m68k.  Fixes numerous pkgsrc build issues where "struct frame" as defined in cpuframe.h pollutes the local namespace. Compile-tested full builds for hp300, mac68k, amiga, sun3, and next68k. OK'ed by matt@.

--- sys/arch/m68k/include/signal.h
+++ sys/arch/m68k/include/signal.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: signal.h,v 1.20 2004/07/29 12:54:46 is Exp $	*/
+/*	$NetBSD: signal.h,v 1.20.10.1 2006/04/06 13:01:48 tron Exp $	*/
 
 /*
  * Copyright (c) 1982, 1986, 1989, 1991 Regents of the University of California.
@@ -83,6 +83,7 @@ struct sigcontext {
 	sigset_t sc_mask;		/* signal mask to restore (new style) */
 };
 
+#ifdef _KERNEL
 #include <m68k/cpuframe.h>
 
 /*
@@ -97,6 +98,7 @@ struct sigstate {
 #define	SS_RTEFRAME	0x01
 #define	SS_FPSTATE	0x02
 #define	SS_USERREGS	0x04
+#endif
 
 #if defined(__M68K_SIGNAL_PRIVATE)
 
