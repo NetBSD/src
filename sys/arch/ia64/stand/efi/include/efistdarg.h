c50d018f245c1878836336a893c476b01cbf5853
cherry <cherry@NetBSD.org>
2006-04-07 13:57:43 +0000

    Initial import of arch/ia64 sources. These sources are ported from FreeBSD/ia64 code. See individual source files for credits. In addition, code from NetBSD/alpha NetBSD/sparc64, NetBSD/i386 and NetBSD/amd64 were used as templates, along with my own additions.

---
+++ sys/arch/ia64/stand/efi/include/efistdarg.h
@@ -0,0 +1,32 @@
+/*	$NetBSD: efistdarg.h,v 1.1 2006/04/07 14:21:29 cherry Exp $	*/
+#ifndef _EFISTDARG_H_
+#define _EFISTDARG_H_
+
+/*++
+
+Copyright (c) 1998  Intel Corporation
+
+Module Name:
+
+    devpath.h
+
+Abstract:
+
+    Defines for parsing the EFI Device Path structures
+
+
+
+Revision History
+
+--*/
+
+#define _INTSIZEOF(n)   ( (sizeof(n) + sizeof(UINTN) - 1) & ~(sizeof(UINTN) - 1) )
+
+typedef CHAR8 * va_list;
+
+#define va_start(ap,v)  ( ap = (va_list)&v + _INTSIZEOF(v) )
+#define va_arg(ap,t)    ( *(t *)((ap += _INTSIZEOF(t)) - _INTSIZEOF(t)) )
+#define va_end(ap)  ( ap = (va_list)0 )
+
+
+#endif  /* _INC_STDARG */
