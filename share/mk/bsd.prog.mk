02d42e8a4d651eb6ca58f6ac5bdc0fe30220fc4a
tron <tron@NetBSD.org>
2006-04-17 23:14:53 +0000

    Apply patch (requested by tsutsui in ticket #1256): Make PAM work on NetBSD ports without shared library support. This fixes PR lib/32421.

--- share/mk/bsd.prog.mk
+++ share/mk/bsd.prog.mk
@@ -1,4 +1,4 @@
-#	$NetBSD: bsd.prog.mk,v 1.206 2005/03/04 20:41:08 he Exp $
+#	$NetBSD: bsd.prog.mk,v 1.206.4.1 2006/04/17 23:14:53 tron Exp $
 #	@(#)bsd.prog.mk	8.2 (Berkeley) 4/2/94
 
 .ifndef HOSTPROG
@@ -75,8 +75,21 @@ LIB${_lib:tu}=	${DESTDIR}/usr/lib/lib${_lib}.a
 
 # PAM applications, if linked statically, need more libraries
 .if (${MKPIC} == "no")
-PAM_STATIC_LDADD= -lcrypt -lrpcsvc -lutil
-PAM_STATIC_DPADD= ${LIBCRYPT} ${LIBRPCSVC} ${LIBUTIL}
+.if (${MKCRYPTO} != "no")
+PAM_STATIC_LDADD+= -lssh
+PAM_STATIC_DPADD+= ${LIBSSH}
+.endif
+.if (${MKKERBEROS} != "no")
+PAM_STATIC_LDADD+= -lkafs -lkrb -lkrb5 -lasn1 -lroken -lcom_err -lcrypto
+PAM_STATIC_DPADD+= ${LIBKAFS} ${LIBKRB} ${LIBKRB5} ${LIBASN1} ${LIBROKEN} \
+	${LIBCOM_ERR} ${LIBCRYPTO}
+.endif
+.if (${MKSKEY} != "no")
+PAM_STATIC_LDADD+= -lskey
+PAM_STATIC_DPADD+= ${LIBSKEY}
+.endif
+PAM_STATIC_LDADD+= -lradius -lcrypt -lrpcsvc -lutil
+PAM_STATIC_DPADD+= ${LIBRADIUS} ${LIBCRYPT} ${LIBRPCSVC} ${LIBUTIL}
 .else
 PAM_STATIC_LDADD=
 PAM_STATIC_DPADD=
