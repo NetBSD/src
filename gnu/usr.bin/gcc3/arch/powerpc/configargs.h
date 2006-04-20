c804162d9e4695f60f41944b617fd4ed61c5a8c8
skrll <skrll@NetBSD.org>
2006-04-20 20:14:05 +0000

    Do a mknative run to pick up (for real) the added support for various features.
    nan is removed from libstdc++ as it's been added to libc. A major bump
    is avoided as there are very few (no) examples of linkage against
    libstdc++ and not against libc.

--- gnu/usr.bin/gcc3/arch/powerpc/configargs.h
+++ gnu/usr.bin/gcc3/arch/powerpc/configargs.h
@@ -2,5 +2,5 @@
 /* Generated from: 	NetBSD: mknative-gcc,v 1.15 2004/12/10 13:58:11 mrg Exp  */
 
 /* Generated automatically. */
-static const char configuration_arguments[] = "/home/nick/work/netbsd/src/tools/gcc/../../gnu/dist/gcc/configure --enable-long-long --disable-multilib --enable-threads --disable-symvers --build=i386-unknown-netbsdelf2.0. --host=powerpc--netbsd --target=powerpc--netbsd";
+static const char configuration_arguments[] = "/home/nick/work/netbsd/src/tools/gcc/../../gnu/dist/gcc/configure --enable-long-long --disable-multilib --enable-threads --disable-symvers --build=i386-unknown-netbsdelf3.99.18 --host=powerpc--netbsd --target=powerpc--netbsd";
 static const char thread_model[] = "posix";
