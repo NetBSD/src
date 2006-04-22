93e4d2f5fca81c292ae21f23779e925e7b2b6d1c
christos <christos@NetBSD.org>
2006-04-22 19:40:40 +0000

    Coverity CID 529: Call VarFreeEnv to prevent leak.

--- usr.bin/make/var.c
+++ usr.bin/make/var.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: var.c,v 1.106 2006/04/22 19:32:35 christos Exp $	*/
+/*	$NetBSD: var.c,v 1.107 2006/04/22 19:40:40 christos Exp $	*/
 
 /*
  * Copyright (c) 1988, 1989, 1990, 1993
@@ -69,14 +69,14 @@
  */
 
 #ifndef MAKE_NATIVE
-static char rcsid[] = "$NetBSD: var.c,v 1.106 2006/04/22 19:32:35 christos Exp $";
+static char rcsid[] = "$NetBSD: var.c,v 1.107 2006/04/22 19:40:40 christos Exp $";
 #else
 #include <sys/cdefs.h>
 #ifndef lint
 #if 0
 static char sccsid[] = "@(#)var.c	8.3 (Berkeley) 3/19/94";
 #else
-__RCSID("$NetBSD: var.c,v 1.106 2006/04/22 19:32:35 christos Exp $");
+__RCSID("$NetBSD: var.c,v 1.107 2006/04/22 19:40:40 christos Exp $");
 #endif
 #endif /* not lint */
 #endif
@@ -586,6 +586,8 @@ Var_Set(const char *name, const char *val, GNode *ctxt, int flags)
     }
     if (name != cp)
 	free(UNCONST(name));
+    if (v != (Var *)NIL)
+	VarFreeEnv(v, TRUE);
 }
 
 /*-
