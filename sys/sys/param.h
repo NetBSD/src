465215283a399f2fb10519f95744468966728b50
thorpej <thorpej>
2006-05-05 18:28:19 +0000

    Welcome to 3.99.19 (proplib replaces propdb).

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.230 2006/04/13 13:50:14 macallan Exp $	*/
+/*	$NetBSD: param.h,v 1.231 2006/05/05 18:28:19 thorpej Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -63,7 +63,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	399001800	/* NetBSD 3.99.18 */
+#define	__NetBSD_Version__	399001900	/* NetBSD 3.99.19 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) >= __NetBSD_Version__)
