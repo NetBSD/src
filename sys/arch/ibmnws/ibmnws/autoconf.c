51ec4f8bb0e35d511a2e9cdfad639f073feef1b8
rjs <rjs>
2006-05-09 14:07:13 +0000

    KNF.

--- sys/arch/ibmnws/ibmnws/autoconf.c
+++ sys/arch/ibmnws/ibmnws/autoconf.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: autoconf.c,v 1.3 2005/12/11 12:17:50 christos Exp $	*/
+/*	$NetBSD: autoconf.c,v 1.4 2006/05/09 14:07:13 rjs Exp $	*/
 
 /*-
  * Copyright (c) 1990 The Regents of the University of California.
@@ -58,13 +58,13 @@
 #include <machine/pte.h>
 #include <machine/intr.h>
 
-static void findroot __P((void));
+static void findroot(void);
 
 /*
  * Determine i/o configuration for a machine.
  */
 void
-cpu_configure()
+cpu_configure(void)
 {
 	/* startrtclock(); */
 
@@ -80,7 +80,7 @@ cpu_configure()
 }
 
 void
-cpu_rootconf()
+cpu_rootconf(void)
 {
 	findroot();
 
@@ -120,7 +120,7 @@ findroot(void)
 	unit = (bootdev >> B_UNITSHIFT) & B_UNITMASK;
 
 	sprintf(buf, "%s%d", name, unit);
-	for (dv = alldevs.tqh_first; dv != NULL; dv = dv->dv_list.tqe_next) {
+	TAILQ_FOREACH(dv, &alldevs, dv_list) {
 		if (strcmp(buf, dv->dv_xname) == 0) {
 			booted_device = dv;
 			booted_partition = part;
