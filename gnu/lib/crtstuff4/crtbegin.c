96990c11fa7254095e59df5939a30f26001efdbb
mrg <mrg>
2006-05-10 22:53:49 +0000

    add the build framework for in-tree GCC4 libraries, and the mknative output for i386, sparc64 and powerpc.

---
+++ gnu/lib/crtstuff4/crtbegin.c
@@ -0,0 +1,9 @@
+/*	$NetBSD: crtbegin.c,v 1.1 2006/05/10 22:53:49 mrg Exp $	*/
+
+/*
+ * Written by Jason R. Thorpe, August 24, 2004.
+ * Public domain.
+ */
+
+#define	CRT_BEGIN
+#include "crtstuff.c"
