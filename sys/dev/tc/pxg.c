6623bf87ee0dbfc2012583ffe2f4f68515781672
yamt <yamt@NetBSD.org>
2006-05-15 09:25:09 +0000

    - include kauth.h for kauth_authorize_generic. - wrap a long line.

--- sys/dev/tc/pxg.c
+++ sys/dev/tc/pxg.c
@@ -1,4 +1,4 @@
-/* 	$NetBSD: pxg.c,v 1.22 2006/05/14 21:47:00 elad Exp $	*/
+/* 	$NetBSD: pxg.c,v 1.23 2006/05/15 09:25:09 yamt Exp $	*/
 
 /*-
  * Copyright (c) 1999, 2000, 2001 The NetBSD Foundation, Inc.
@@ -42,7 +42,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: pxg.c,v 1.22 2006/05/14 21:47:00 elad Exp $");
+__KERNEL_RCSID(0, "$NetBSD: pxg.c,v 1.23 2006/05/15 09:25:09 yamt Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -50,6 +50,7 @@ __KERNEL_RCSID(0, "$NetBSD: pxg.c,v 1.22 2006/05/14 21:47:00 elad Exp $");
 #include <sys/malloc.h>
 #include <sys/callout.h>
 #include <sys/proc.h>
+#include <sys/kauth.h>
 
 #if defined(pmax)
 #include <mips/cpuregs.h>
@@ -367,7 +368,8 @@ pxg_ioctl(struct stic_info *si, u_long cmd, caddr_t data, int flag,
 	switch (cmd) {
 	case STICIO_START860:
 	case STICIO_RESET860:
-		if ((rv = kauth_authorize_generic(p->p_cred, KAUTH_GENERIC_ISSUSER, &p->p_acflag)) != 0)
+		if ((rv = kauth_authorize_generic(p->p_cred,
+		    KAUTH_GENERIC_ISSUSER, &p->p_acflag)) != 0)
 			return (rv);
 		if (si->si_dispmode != WSDISPLAYIO_MODE_MAPPED)
 			return (EBUSY);
