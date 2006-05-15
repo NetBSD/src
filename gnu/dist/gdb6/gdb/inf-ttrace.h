29486e2a1766c53965f15f36bd0c58b580cef23b
nathanw <nathanw@NetBSD.org>
2006-05-15 14:08:20 +0000

    Import GDB 6.4

---
+++ gnu/dist/gdb6/gdb/inf-ttrace.h
@@ -0,0 +1,30 @@
+/* Low-level child interface to ttrace.
+
+   Copyright 2004 Free Software Foundation, Inc.
+
+   This file is part of GDB.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.  */
+
+#ifndef INF_TTRACE_H
+#define INF_TTRACE_H
+
+/* Create a prototype ttrace target.  The client can override it with
+   local methods.  */
+
+extern struct target_ops *inf_ttrace_target (void);
+
+#endif
