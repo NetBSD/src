9084bfc3dda10a9b27ca2012d7b027fe40175997
rpaulo <rpaulo@NetBSD.org>
2006-05-18 02:14:22 +0000

    Replace the way we calculate the standard deviation with the unbiased form for non-related samples. There are several websites that explain why we should use this form instead of the normal formula to compute the std. dev. (Wikipedia is one of them). Also, ping(8) (where this "new" formula came from) already does it this way so, I'm trying to score some extra points by making both utilities behave the same way.
    Thanks to wrtstuden@ for the initial clarification.

--- sbin/ping6/ping6.c
+++ sbin/ping6/ping6.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: ping6.c,v 1.68 2006/05/07 16:00:18 rpaulo Exp $	*/
+/*	$NetBSD: ping6.c,v 1.69 2006/05/18 02:14:22 rpaulo Exp $	*/
 /*	$KAME: ping6.c,v 1.164 2002/11/16 14:05:37 itojun Exp $	*/
 
 /*
@@ -77,7 +77,7 @@ static char sccsid[] = "@(#)ping.c	8.1 (Berkeley) 6/5/93";
 #else
 #include <sys/cdefs.h>
 #ifndef lint
-__RCSID("$NetBSD: ping6.c,v 1.68 2006/05/07 16:00:18 rpaulo Exp $");
+__RCSID("$NetBSD: ping6.c,v 1.69 2006/05/18 02:14:22 rpaulo Exp $");
 #endif
 #endif
 
@@ -2109,7 +2109,7 @@ summary(void)
 		/* Only display average to microseconds */
 		double num = nreceived + nrepeats;
 		double avg = tsum / num;
-		double dev = sqrt(tsumsq / num - avg * avg);
+		double dev = sqrt((tsumsq - num * avg * avg) / (num - 1));
 		(void)printf(
 		    "round-trip min/avg/max/std-dev = %.3f/%.3f/%.3f/%.3f ms\n",
 		    tmin, avg, tmax, dev);
