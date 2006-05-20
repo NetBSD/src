9d301685a10da06e5fcdb771ba8e167fec4294d8
riz <riz@NetBSD.org>
2006-05-20 22:42:50 +0000

    Pull up following revision(s) (requested by perseant in ticket #1327): 	sys/ufs/lfs/lfs_alloc.c: revision 1.92 	sys/ufs/lfs/lfs.h: revision 1.105 	sys/ufs/lfs/lfs_vfsops.c: revision 1.207 	sys/ufs/lfs/lfs_subr.c: revision 1.59 	sys/ufs/lfs/lfs_vnops.c: revision 1.173 	sys/ufs/lfs/lfs_bio.c: revision 1.92 Introduce another per-filesystem parameter, lfs_resvseg, to separate the notion of "how many segments are reserved for the cleaner" from that of "how many segments are not counted in lfs_bfree".  The default value used for existing filesystems is the same as the previous implicit value of (lfs_minfreeseg / 2 + 1), modulo some sanity checking. Count pending dirops on a per-filesystem basis, since once we start writing them we can't stop until we're done.  This seems to help stave off the "no clean segments" panic in the case of filling the filesystem with directories and small files (e.g. simultaneously unpacking more copies of pkgsrc than will fit).

--- sys/ufs/lfs/lfs_bio.c
+++ sys/ufs/lfs/lfs_bio.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: lfs_bio.c,v 1.81.2.3 2006/05/20 22:19:33 riz Exp $	*/
+/*	$NetBSD: lfs_bio.c,v 1.81.2.4 2006/05/20 22:42:50 riz Exp $	*/
 
 /*-
  * Copyright (c) 1999, 2000, 2001, 2002, 2003 The NetBSD Foundation, Inc.
@@ -67,7 +67,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: lfs_bio.c,v 1.81.2.3 2006/05/20 22:19:33 riz Exp $");
+__KERNEL_RCSID(0, "$NetBSD: lfs_bio.c,v 1.81.2.4 2006/05/20 22:42:50 riz Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -656,6 +656,7 @@ lfs_check(struct vnode *vp, daddr_t blkno, int flags)
 	       (locked_queue_count + INOCOUNT(fs) > LFS_MAX_BUFS ||
 		locked_queue_bytes + INOBYTES(fs) > LFS_MAX_BYTES ||
 		lfs_subsys_pages > LFS_MAX_PAGES ||
+		fs->lfs_dirvcount > LFS_MAX_FSDIROP(fs) ||
 		lfs_dirvcount > LFS_MAX_DIROP || fs->lfs_diropwait > 0))
 	{
 		simple_unlock(&lfs_subsys_lock);
@@ -682,6 +683,9 @@ lfs_check(struct vnode *vp, daddr_t blkno, int flags)
 	if (lfs_dirvcount > LFS_MAX_DIROP)
 		DLOG((DLOG_FLUSH, "lfs_check: ldvc = %d, max %d\n",
 		      lfs_dirvcount, LFS_MAX_DIROP));
+	if (fs->lfs_dirvcount > LFS_MAX_FSDIROP(fs))
+		DLOG((DLOG_FLUSH, "lfs_check: lfdvc = %d, max %d\n",
+		      fs->lfs_dirvcount, LFS_MAX_FSDIROP(fs)));
 	if (fs->lfs_diropwait > 0)
 		DLOG((DLOG_FLUSH, "lfs_check: ldvw = %d\n",
 		      fs->lfs_diropwait));
@@ -690,6 +694,7 @@ lfs_check(struct vnode *vp, daddr_t blkno, int flags)
 	if (locked_queue_count + INOCOUNT(fs) > LFS_MAX_BUFS ||
 	    locked_queue_bytes + INOBYTES(fs) > LFS_MAX_BYTES ||
 	    lfs_subsys_pages > LFS_MAX_PAGES ||
+	    fs->lfs_dirvcount > LFS_MAX_FSDIROP(fs) ||
 	    lfs_dirvcount > LFS_MAX_DIROP || fs->lfs_diropwait > 0) {
 		simple_unlock(&fs->lfs_interlock);
 		lfs_flush(fs, flags, 0);
@@ -707,6 +712,7 @@ lfs_check(struct vnode *vp, daddr_t blkno, int flags)
 	while (locked_queue_count + INOCOUNT(fs) > LFS_WAIT_BUFS ||
 		locked_queue_bytes + INOBYTES(fs) > LFS_WAIT_BYTES ||
 		lfs_subsys_pages > LFS_WAIT_PAGES ||
+		fs->lfs_dirvcount > LFS_MAX_FSDIROP(fs) ||
 		lfs_dirvcount > LFS_MAX_DIROP) {
 
 		if (lfs_dostats)
