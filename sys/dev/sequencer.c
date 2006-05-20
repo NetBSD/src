b3b4aab2c2d7cf71314fc9cbcc5b4cd0798277d2
chap <chap@NetBSD.org>
2006-05-20 03:41:28 +0000

    Spotted a bug in a buffer condition. Never saw any symptom, but it's a bug nonetheless.

--- sys/dev/sequencer.c
+++ sys/dev/sequencer.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: sequencer.c,v 1.30.14.13 2006/05/20 03:32:45 chap Exp $	*/
+/*	$NetBSD: sequencer.c,v 1.30.14.14 2006/05/20 03:41:28 chap Exp $	*/
 
 /*
  * Copyright (c) 1998 The NetBSD Foundation, Inc.
@@ -37,7 +37,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: sequencer.c,v 1.30.14.13 2006/05/20 03:32:45 chap Exp $");
+__KERNEL_RCSID(0, "$NetBSD: sequencer.c,v 1.30.14.14 2006/05/20 03:41:28 chap Exp $");
 
 #include "sequencer.h"
 
