3433ba2de88c8b87282b37b439c3ea9a62958bdd
riz <riz@NetBSD.org>
2006-05-20 21:16:25 +0000

    Pull up following revision(s) (requested by perseant in ticket #1327): 	sys/ufs/lfs/lfs_vnops.c: revision 1.151 VOP_LOCK drops the interlock; pick it up again to avoid an "already unlocked" panic in lfs_putpages.

--- sys/ufs/lfs/lfs_vnops.c
+++ sys/ufs/lfs/lfs_vnops.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: lfs_vnops.c,v 1.137.2.4 2005/05/07 11:21:30 tron Exp $	*/
+/*	$NetBSD: lfs_vnops.c,v 1.137.2.5 2006/05/20 21:16:25 riz Exp $	*/
 
 /*-
  * Copyright (c) 1999, 2000, 2001, 2002, 2003 The NetBSD Foundation, Inc.
@@ -67,7 +67,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: lfs_vnops.c,v 1.137.2.4 2005/05/07 11:21:30 tron Exp $");
+__KERNEL_RCSID(0, "$NetBSD: lfs_vnops.c,v 1.137.2.5 2006/05/20 21:16:25 riz Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -1811,8 +1811,10 @@ lfs_putpages(void *v)
 		simple_unlock(&fs->lfs_interlock);
 
 		simple_lock(&vp->v_interlock);
-		if (locked)
+		if (locked) {
 			VOP_LOCK(vp, LK_EXCLUSIVE | LK_INTERLOCK);
+			simple_lock(&vp->v_interlock);
+		}
 		lfs_writer_leave(fs);
 
 		/* XXX the flush should have taken care of this one too! */
