69290971aabbe4ae80faa21139163e229ccc597e
christos <christos@NetBSD.org>
2006-05-23 01:07:16 +0000

    Coverity CID 3187: Fix memory leak

--- usr.bin/audio/play/play.c
+++ usr.bin/audio/play/play.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: play.c,v 1.45 2005/07/13 10:57:11 mrg Exp $	*/
+/*	$NetBSD: play.c,v 1.46 2006/05/23 01:07:16 christos Exp $	*/
 
 /*
  * Copyright (c) 1999 Matthew R. Green
@@ -30,7 +30,7 @@
 #include <sys/cdefs.h>
 
 #ifndef lint
-__RCSID("$NetBSD: play.c,v 1.45 2005/07/13 10:57:11 mrg Exp $");
+__RCSID("$NetBSD: play.c,v 1.46 2006/05/23 01:07:16 christos Exp $");
 #endif
 
 
@@ -319,8 +319,10 @@ play_fd(file, fd)
 	if (nr < 0)
 		goto read_error;
 	if (nr == 0) {
-		if (fflag)
+		if (fflag) {
+			free(buffer);
 			return;
+		}
 		err(1, "unexpected EOF");
 	}
 	hdrlen = audioctl_write_fromhdr(buffer, nr, audiofd, &datasize, file);
