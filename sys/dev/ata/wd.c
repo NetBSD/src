263b81398799369790cf11d92e807627a89761cf
riz <riz@NetBSD.org>
2006-05-24 02:29:05 +0000

    Pull up following revision(s) (requested by bouyer in ticket #10625): 	sys/dev/ata/wd.c: revision 1.324 Add yet another broken 160Gb seagate drive. I though we had found all of them ... reported by Damian Lubosch on current-users.

--- sys/dev/ata/wd.c
+++ sys/dev/ata/wd.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: wd.c,v 1.274.2.8.2.7 2006/03/28 23:11:26 riz Exp $ */
+/*	$NetBSD: wd.c,v 1.274.2.8.2.8 2006/05/24 02:29:05 riz Exp $ */
 
 /*
  * Copyright (c) 1998, 2001 Manuel Bouyer.  All rights reserved.
@@ -66,7 +66,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: wd.c,v 1.274.2.8.2.7 2006/03/28 23:11:26 riz Exp $");
+__KERNEL_RCSID(0, "$NetBSD: wd.c,v 1.274.2.8.2.8 2006/05/24 02:29:05 riz Exp $");
 
 #ifndef WDCDEBUG
 #define WDCDEBUG
@@ -233,6 +233,8 @@ static const struct wd_quirk {
 	 * (aka LBA48_THRESHOLD) in LBA mode. The workaround is to force
 	 * LBA48
 	 */
+	{ "ST3160812A*",
+	  WD_QUIRK_FORCE_LBA48 },
 	{ "ST3160023A*",
 	  WD_QUIRK_FORCE_LBA48 },
 	{ "ST3160827A*",
