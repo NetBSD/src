165f43686ff18586016f4bb62eeb6a46bc8546c1
agc <agc@NetBSD.org>
2006-05-27 21:29:32 +0000

    Bump version number for the portability fix, and the dynamic number of sessions.

--- dist/iscsi/include/config.h
+++ dist/iscsi/include/config.h
@@ -179,13 +179,13 @@
 #define PACKAGE_NAME "netbsd-iscsi"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "netbsd-iscsi 20060526"
+#define PACKAGE_STRING "netbsd-iscsi 20060527"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "netbsd-iscsi"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "20060526"
+#define PACKAGE_VERSION "20060527"
 
 /* Define to 1 if you have the ANSI C header files. */
 #define STDC_HEADERS 1
