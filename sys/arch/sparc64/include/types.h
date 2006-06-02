41d255b1bb4e8260da00bfd43526fc74a380bf4a
kardel <kardel>
2006-06-02 00:20:23 +0000

    - support timecounters

--- sys/arch/sparc64/include/types.h
+++ sys/arch/sparc64/include/types.h
@@ -1,4 +1,3 @@
-/*       $NetBSD: types.h,v 1.25.6.1 2006/05/27 22:50:40 kardel Exp $        */
+/*       $NetBSD: types.h,v 1.25.6.2 2006/06/02 00:20:37 kardel Exp $        */
 
 #include <sparc/types.h>
-#undef __HAVE_TIMECOUNTER
