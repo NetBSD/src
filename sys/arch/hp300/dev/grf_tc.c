24f0d8a815a555c5195470e63c2fb39c573a01c9
tron <tron@NetBSD.org>
2006-06-04 13:58:45 +0000

    Pull up following revision(s) (requested by tsutsui in ticket #10634): 	sys/arch/hp300/dev/grf_rb.c: revision 1.31 via patch 	sys/arch/hp300/dev/grf_tc.c: revision 1.33 via patch 	sys/arch/hp300/dev/grf_hy.c: revision 1.28 via patch 	sys/arch/hp300/dev/grf_dv.c: revision 1.31 via patch 	sys/arch/hp300/dev/grf_gb.c: revision 1.30 via patch Check scode range for DIO-II based framebuffers correctly in cnattach() functions. Fixes boot failure problem on HP400t with fb console reported by Stephan "doomwarrior" on port-hp300.

--- sys/arch/hp300/dev/grf_tc.c
+++ sys/arch/hp300/dev/grf_tc.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: grf_tc.c,v 1.28.2.1 2004/04/11 03:02:45 jmc Exp $	*/
+/*	$NetBSD: grf_tc.c,v 1.28.2.1.2.1 2006/06/04 13:58:45 tron Exp $	*/
 
 /*-
  * Copyright (c) 1996, 1997 The NetBSD Foundation, Inc.
@@ -117,7 +117,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: grf_tc.c,v 1.28.2.1 2004/04/11 03:02:45 jmc Exp $");
+__KERNEL_RCSID(0, "$NetBSD: grf_tc.c,v 1.28.2.1.2.1 2006/06/04 13:58:45 tron Exp $");
 
 #include "opt_compat_hpux.h"
 
@@ -137,6 +137,7 @@ __KERNEL_RCSID(0, "$NetBSD: grf_tc.c,v 1.28.2.1 2004/04/11 03:02:45 jmc Exp $");
 
 #include <dev/cons.h>
 
+#include <hp300/dev/dioreg.h>
 #include <hp300/dev/diovar.h>
 #include <hp300/dev/diodevs.h>
 #include <hp300/dev/intiovar.h>
@@ -782,7 +783,7 @@ topcatcnattach(bus_space_tag_t bst, bus_addr_t addr, int scode)
 		return (1);
 	}
 
-	if (scode > 132) {
+	if (DIO_ISDIOII(scode)) {
 		dioiidev = (u_int8_t *)va;
 		size =  ((dioiidev[0x101] + 1) * 0x100000);
 	} else
