549c70ea2b65e9cd52645cd14ef13382da5f4d59
skrll <skrll@NetBSD.org>
2006-06-05 10:59:57 +0000

    Import gcc 3.3.6.
    144 gcc bugs were fixed between 3.3.3 and 3.3.6.

--- gnu/dist/gcc/gcc/unroll.c
+++ gnu/dist/gcc/gcc/unroll.c
@@ -794,8 +794,12 @@ unroll_loop (loop, insn_count, strength_reduce_p)
       for (r = FIRST_PSEUDO_REGISTER; r < max_reg_before_loop; ++r)
 	if (REGNO_FIRST_UID (r) > 0 && REGNO_FIRST_UID (r) < max_uid_for_loop
 	    && REGNO_FIRST_LUID (r) >= copy_start_luid
+	    /* See the comment in reg_scan_mark_refs on the relationship between
+	       last_uid and last_note_uid.  */
 	    && REGNO_LAST_UID (r) > 0 && REGNO_LAST_UID (r) < max_uid_for_loop
-	    && REGNO_LAST_LUID (r) <= copy_end_luid)
+	    && REGNO_LAST_LUID (r) <= copy_end_luid
+	    && REGNO_LAST_NOTE_UID (r) > 0 && REGNO_LAST_NOTE_UID (r) < max_uid_for_loop
+	    && REGNO_LAST_NOTE_LUID (r) <= copy_end_luid)
 	  {
 	    /* However, we must also check for loop-carried dependencies.
 	       If the value the pseudo has at the end of iteration X is
@@ -1118,6 +1122,11 @@ unroll_loop (loop, insn_count, strength_reduce_p)
 	  /* Set unroll type to MODULO now.  */
 	  unroll_type = UNROLL_MODULO;
 	  loop_preconditioned = 1;
+
+	  /* Preconditioning changes the loop's initial value.  We set
+	     it to an unknown value so that doloop_optimize won't get
+	     confused.  */
+	  loop_info->initial_value = 0;
 
 	  /* Clean up.  */
 	  free (labels);
