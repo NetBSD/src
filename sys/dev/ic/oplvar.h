d4d9cbc7c52e92d88e361edeef7b9b7ef9163e1a
christos <christos>
2006-06-09 21:55:33 +0000

    stack police: Introduce an opl_match function that uses malloc instead of allocating ~2K of softc on the stack.

--- sys/dev/ic/oplvar.h
+++ sys/dev/ic/oplvar.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: oplvar.h,v 1.11 2005/12/11 12:21:28 christos Exp $	*/
+/*	$NetBSD: oplvar.h,v 1.12 2006/06/09 21:55:33 christos Exp $	*/
 
 /*
  * Copyright (c) 1998 The NetBSD Foundation, Inc.
@@ -105,6 +105,7 @@ extern const struct opl_operators opl2_instrs[];
 extern const struct opl_operators opl3_instrs[];
 
 int	opl_find(struct opl_softc *);
+int	opl_match(bus_space_tag_t, bus_space_handle_t, int);
 void	opl_attach(struct opl_softc *);
 int	opl_detach(struct opl_softc *, int);
 #endif
--- sys/dev/pci/opl_yds.c
+++ sys/dev/pci/opl_yds.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: opl_yds.c,v 1.9 2005/12/11 12:22:50 christos Exp $	*/
+/*	$NetBSD: opl_yds.c,v 1.10 2006/06/09 21:55:34 christos Exp $	*/
 
 /*
  * Copyright (c) 1998 The NetBSD Foundation, Inc.
@@ -37,7 +37,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: opl_yds.c,v 1.9 2005/12/11 12:22:50 christos Exp $");
+__KERNEL_RCSID(0, "$NetBSD: opl_yds.c,v 1.10 2006/06/09 21:55:34 christos Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -70,14 +70,10 @@ opl_yds_match(struct device *parent, struct cfdata *match, void *aux)
 {
 	struct audio_attach_args *aa = (struct audio_attach_args *)aux;
 	struct yds_softc *ssc = (struct yds_softc *)parent;
-	struct opl_softc sc;
 
 	if (aa->type != AUDIODEV_TYPE_OPL)
 		return (0);
-	memset(&sc, 0, sizeof sc);
-	sc.iot = ssc->sc_opl_iot;
-	sc.ioh = ssc->sc_opl_ioh;
-	return (opl_find(&sc));
+	return opl_match(ssc->sc_opl_iot, ssc->sc_opl_ioh, 0);
 }
 
 static void
