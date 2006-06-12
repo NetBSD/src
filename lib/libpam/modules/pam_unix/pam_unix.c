9111426d89f95f92f84fcb450a6629c92bfde05e
tron <tron>
2006-06-12 12:30:26 +0000

    Pull up following revision(s) (requested by jnemeth in ticket #1370): 	lib/libpam/modules/pam_unix/pam_unix.c: revision 1.11 PR/30730 force changing password via chsh fails PR/33502 password aging not working copy old_pwd to new_pwd before modifying

--- lib/libpam/modules/pam_unix/pam_unix.c
+++ lib/libpam/modules/pam_unix/pam_unix.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: pam_unix.c,v 1.5.2.5 2006/03/20 17:37:27 riz Exp $	*/
+/*	$NetBSD: pam_unix.c,v 1.5.2.6 2006/06/12 12:30:26 tron Exp $	*/
 
 /*-
  * Copyright 1998 Juniper Networks, Inc.
@@ -40,7 +40,7 @@
 #ifdef __FreeBSD__
 __FBSDID("$FreeBSD: src/lib/libpam/modules/pam_unix/pam_unix.c,v 1.49 2004/02/10 10:13:21 des Exp $");
 #else
-__RCSID("$NetBSD: pam_unix.c,v 1.5.2.5 2006/03/20 17:37:27 riz Exp $");
+__RCSID("$NetBSD: pam_unix.c,v 1.5.2.6 2006/06/12 12:30:26 tron Exp $");
 #endif
 
 
@@ -404,7 +404,7 @@ PAM_EXTERN int
 pam_sm_chauthtok(pam_handle_t *pamh, int flags,
     int argc __unused, const char *argv[] __unused)
 {
-	struct passwd *pwd, old_pwd;
+	struct passwd *pwd, new_pwd, old_pwd;
 	login_cap_t *lc;
 	const char *user, *passwd_db, *new_pass, *old_pass, *p;
 	int retval, tries, min_pw_len = 0, pw_expiry = 0;
@@ -600,6 +600,8 @@ pam_sm_chauthtok(pam_handle_t *pamh, int flags,
 			return (PAM_SERVICE_ERR);
 		}
 
+		new_pwd = old_pwd;
+		pwd = &new_pwd;
 		pwd->pw_passwd = crypt(new_pass, salt);
 		pwd->pw_change = pw_expiry ? pw_expiry + time(NULL) : 0;
 
