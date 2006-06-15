f84f8a488aa147e69f970ee522de278f9393b2d1
tron <tron@NetBSD.org>
2006-06-15 13:41:26 +0000

    Pull up following revision(s) (requested by bouyer in ticket #1373): 	sys/dev/pci/pucdata.c: revision 1.45 Add entry for "Moxa Technologies, SmartIO CP104-V2/PCI". From Gert Doering in kern/33566.

--- sys/dev/pci/pucdata.c
+++ sys/dev/pci/pucdata.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: pucdata.c,v 1.40.2.2 2006/06/15 13:41:02 tron Exp $	*/
+/*	$NetBSD: pucdata.c,v 1.40.2.3 2006/06/15 13:41:26 tron Exp $	*/
 
 /*
  * Copyright (c) 1998, 1999 Christopher G. Demetriou.  All rights reserved.
@@ -36,7 +36,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: pucdata.c,v 1.40.2.2 2006/06/15 13:41:02 tron Exp $");
+__KERNEL_RCSID(0, "$NetBSD: pucdata.c,v 1.40.2.3 2006/06/15 13:41:26 tron Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -909,6 +909,18 @@ const struct puc_device_description puc_devices[] = {
 	/* Moxa Technologies Co., Ltd. PCI I/O Card 4S RS232 */
 	{   "Moxa Technologies, SmartIO CP104/PCI",
 	    {   0x1393, 0x1041, 0,      0    },
+	    {   0xffff, 0xffff, 0,      0    },
+	    {
+		{ PUC_PORT_TYPE_COM, 0x18, 0x00, COM_FREQ * 8 },
+		{ PUC_PORT_TYPE_COM, 0x18, 0x08, COM_FREQ * 8 },
+		{ PUC_PORT_TYPE_COM, 0x18, 0x10, COM_FREQ * 8 },
+		{ PUC_PORT_TYPE_COM, 0x18, 0x18, COM_FREQ * 8 },
+	    },
+	},
+
+	/* Moxa Technologies Co., Ltd. PCI I/O Card 4S RS232 */
+	{   "Moxa Technologies, SmartIO CP104-V2/PCI",
+	    {   0x1393, 0x1042, 0,      0    },
 	    {   0xffff, 0xffff, 0,      0    },
 	    {
 		{ PUC_PORT_TYPE_COM, 0x18, 0x00, COM_FREQ * 8 },
