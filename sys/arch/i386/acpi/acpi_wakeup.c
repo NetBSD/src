3b2286e38593fcb749a2df9aa004581d8750b885
chap <chap@NetBSD.org>
2006-06-22 03:39:47 +0000

    Complete a sync sys/ with head.

--- sys/arch/i386/acpi/acpi_wakeup.c
+++ sys/arch/i386/acpi/acpi_wakeup.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: acpi_wakeup.c,v 1.22.8.1 2006/06/19 03:44:03 chap Exp $	*/
+/*	$NetBSD: acpi_wakeup.c,v 1.22.8.2 2006/06/22 03:39:47 chap Exp $	*/
 
 /*-
  * Copyright (c) 2002 The NetBSD Foundation, Inc.
@@ -37,7 +37,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: acpi_wakeup.c,v 1.22.8.1 2006/06/19 03:44:03 chap Exp $");
+__KERNEL_RCSID(0, "$NetBSD: acpi_wakeup.c,v 1.22.8.2 2006/06/22 03:39:47 chap Exp $");
 
 /*-
  * Copyright (c) 2001 Takanori Watanabe <takawata@jp.freebsd.org>
@@ -420,7 +420,6 @@ acpi_md_sleep(int state)
 	} else {
 		printf("acpi0: good morning!\n");
 		/* Execute Wakeup */
-		enable_intr();
 
 		npxinit(&cpu_info_primary);
 		i8259_reinit();
@@ -441,6 +440,8 @@ acpi_md_sleep(int state)
 	}
 
 out:
+	enable_intr();
+
 	lcr3(cr3);
 	/* Clean up identity mapping. */
 	pmap_remove(pm, phys_wakeup, phys_wakeup + PAGE_SIZE);
--- sys/sys/time.h
+++ sys/sys/time.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: time.h,v 1.54.2.1 2006/06/19 04:11:13 chap Exp $	*/
+/*	$NetBSD: time.h,v 1.54.2.2 2006/06/22 03:39:50 chap Exp $	*/
 
 /*
  * Copyright (c) 1982, 1986, 1993
@@ -99,7 +99,7 @@ struct timezone {
 		}							\
 	} while (/* CONSTCOND */ 0)
 
-#ifdef _NETBSD_SOURCE
+#ifdef _KERNEL
 struct bintime {
 	time_t	sec;
 	uint64_t frac;
@@ -189,7 +189,7 @@ timeval2bintime(const struct timeval *tv, struct bintime *bt)
 	/* 18446744073709 = int(2^64 / 1000000) */
 	bt->frac = tv->tv_usec * (uint64_t)18446744073709LL;
 }
-#endif /* __BSD_VISIBLE */
+#endif /* _KERNEL */
 
 /* Operations on timespecs. */
 #define	timespecclear(tsp)		(tsp)->tv_sec = (tsp)->tv_nsec = 0
