5a3b3e8e146f48ee58ef60945d6cd199c49f3704
yamt <yamt@NetBSD.org>
2006-06-23 14:13:02 +0000

    fix a simonb-timecounters regression. the precision of getnanotime() is not suitable for file timestamps. esp. when it's nfs-exported.
    - introduce vfs_timestamp().
      (the name is from freebsd.  currently merely a wrapper of nanotime())
    - for ufs-like filesystems, use it rather than getnanotime().
    
    XXX check other filesystems.

--- sys/sys/vnode.h
+++ sys/sys/vnode.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: vnode.h,v 1.154 2006/05/14 21:38:18 elad Exp $	*/
+/*	$NetBSD: vnode.h,v 1.155 2006/06/23 14:13:02 yamt Exp $	*/
 
 /*
  * Copyright (c) 1989, 1993
@@ -589,6 +589,7 @@ void	vfs_getnewfsid(struct mount *);
 int	vfs_drainvnodes(long target, struct lwp *);
 void	vfs_write_resume(struct mount *);
 int	vfs_write_suspend(struct mount *, int, int);
+void	vfs_timestamp(struct timespec *);
 #ifdef DDB
 void	vfs_vnode_print(struct vnode *, int, void (*)(const char *, ...));
 void	vfs_mount_print(struct mount *, int, void (*)(const char *, ...));
