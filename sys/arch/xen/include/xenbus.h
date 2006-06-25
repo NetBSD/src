3c30de769e91993396219ec0c48b93ef05babd2e
bouyer <bouyer@NetBSD.org>
2006-06-25 16:46:59 +0000

    Add a 'base' argument to xenbus_read_ul, so that we can read number is base other than 10.

--- sys/arch/xen/include/xenbus.h
+++ sys/arch/xen/include/xenbus.h
@@ -1,4 +1,4 @@
-/* $NetBSD: xenbus.h,v 1.5 2006/05/23 21:07:56 bouyer Exp $ */
+/* $NetBSD: xenbus.h,v 1.6 2006/06/25 16:46:59 bouyer Exp $ */
 /******************************************************************************
  * xenbus.h
  *
@@ -120,7 +120,7 @@ int xenbus_read(struct xenbus_transaction *t,
 		  const char *dir, const char *node, unsigned int *len,
 		  char **);
 int xenbus_read_ul(struct xenbus_transaction *,
-		  const char *, const char *, unsigned long *);
+		  const char *, const char *, unsigned long *, int);
 int xenbus_write(struct xenbus_transaction *t,
 		 const char *dir, const char *node, const char *string);
 int xenbus_mkdir(struct xenbus_transaction *t,
