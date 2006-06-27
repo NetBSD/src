62aae7a2e08878423d0c0923b8ed82ccc86e81b4
peter <peter@NetBSD.org>
2006-06-27 13:58:08 +0000

    KNF. Correct some spelling errors.

--- sys/arch/arm/sa11x0/sa1111.c
+++ sys/arch/arm/sa11x0/sa1111.c
@@ -1,4 +1,4 @@
-/*      $NetBSD: sa1111.c,v 1.17 2006/03/04 17:24:13 peter Exp $	*/
+/*      $NetBSD: sa1111.c,v 1.18 2006/06/27 13:58:08 peter Exp $	*/
 
 /*-
  * Copyright (c) 2001 The NetBSD Foundation, Inc.
@@ -38,11 +38,11 @@
 
 /*
  * TODO:
- *   - introduce bus abstraction to support SA1101
+ *   - introduce bus abstraction to support SA-1101
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: sa1111.c,v 1.17 2006/03/04 17:24:13 peter Exp $");
+__KERNEL_RCSID(0, "$NetBSD: sa1111.c,v 1.18 2006/06/27 13:58:08 peter Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -63,11 +63,10 @@ __KERNEL_RCSID(0, "$NetBSD: sa1111.c,v 1.17 2006/03/04 17:24:13 peter Exp $");
 
 #include "locators.h"
 
-static	int	sa1111_print(void *, const char *);
+static int	sa1111_print(void *, const char *);
 
 static void	sacc_intr_calculatemasks(struct sacc_softc *);
 static void	sacc_intr_setpolarity(sacc_chipset_tag_t *, int , int);
-int		sacc_intr(void *);
 
 #if !defined(__HAVE_GENERIC_SOFT_INTERRUPTS)
 void *softintr_establish(int, int (*)(void *), void *);
@@ -88,15 +87,15 @@ sacc_probe(struct device *parent, struct cfdata *match, void *aux)
 	uint32_t skid;
 
 	if (bus_space_map(sa->sa_iot, sa->sa_addr, sa->sa_size, 0, &ioh))
-		return (0);
+		return 0;
 
 	skid = bus_space_read_4(sa->sa_iot, ioh, SACCSBI_SKID);
 	bus_space_unmap(sa->sa_iot, ioh, sa->sa_size);
 
 	if ((skid & 0xffffff00) != 0x690cc200)
-		return (0);
+		return 0;
 
-	return (1);
+	return 1;
 }
 
 
@@ -123,7 +122,8 @@ sa1111_search(struct device *parent, struct cfdata *cf, const int *ldesc,
 static int
 sa1111_print(void *aux, const char *name)
 {
-	return (UNCONF);
+
+	return UNCONF;
 }
 
 
@@ -141,7 +141,7 @@ sacc_intr_establish(sacc_chipset_tag_t *ic, int irq, int type, int level,
 		panic("sacc_intr_establish: can't malloc handler info");
 
 	if (irq < 0 || irq > SACCIC_LEN ||
-	    ! (type == IST_EDGE_RAISE || type == IST_EDGE_FALL))
+	    !(type == IST_EDGE_RAISE || type == IST_EDGE_FALL))
 		panic("sacc_intr_establish: bogus irq or type");
 
 	if (sc->sc_intrhand[irq] == NULL) {
@@ -162,7 +162,7 @@ sacc_intr_establish(sacc_chipset_tag_t *ic, int irq, int type, int level,
 	/* map interrupt level to appropriate softinterrupt level */
 	if (level >= IPL_SOFTSERIAL)
 		level = IPL_SOFTSERIAL;
-	else if(level >= IPL_SOFTNET)
+	else if (level >= IPL_SOFTNET)
 		level = IPL_SOFTNET;
 	ih->ih_soft = softintr_establish(level, (void (*)(void *)) ih_fun,
 					 ih_arg);
@@ -171,15 +171,15 @@ sacc_intr_establish(sacc_chipset_tag_t *ic, int irq, int type, int level,
 	ih->ih_next = NULL;
 
 	s = splhigh();
-	for(p = &sc->sc_intrhand[irq]; *p; p = &(*p)->ih_next)
-		;
+	for (p = &sc->sc_intrhand[irq]; *p; p = &(*p)->ih_next)
+		continue;
 
 	*p = ih;
 
 	sacc_intr_calculatemasks(sc);
 	splx(s);
 
-	return(ih);
+	return ih;
 }
 
 void
@@ -199,7 +199,7 @@ sacc_intr_disestablish(sacc_chipset_tag_t *ic, void *arg)
 
 	s = splhigh();
 
-	for(p = &sc->sc_intrhand[irq];; p = &(*p)->ih_next) {
+	for (p = &sc->sc_intrhand[irq];; p = &(*p)->ih_next) {
 		if (*p == NULL)
 			panic("sacc_intr_disestablish: handler not registered");
 		if (*p == ih)
@@ -213,7 +213,7 @@ sacc_intr_disestablish(sacc_chipset_tag_t *ic, void *arg)
 	free(ih, M_DEVBUF);
 }
 
-void
+static void
 sacc_intr_setpolarity(sacc_chipset_tag_t *ic, int irq, int type)
 {
 	struct sacc_softc *sc = (struct sacc_softc *)ic;
@@ -239,17 +239,17 @@ sacc_intr_setpolarity(sacc_chipset_tag_t *ic, int irq, int type)
 	splx(s);
 }
 
-void
+static void
 sacc_intr_calculatemasks(struct sacc_softc *sc)
 {
 	int irq;
 
 	sc->sc_imask.lo = 0;
 	sc->sc_imask.hi = 0;
-	for(irq = 0; irq < 32; irq++)
+	for (irq = 0; irq < 32; irq++)
 		if (sc->sc_intrhand[irq])
 			sc->sc_imask.lo |= (1 << irq);
-	for(irq = 0; irq < SACCIC_LEN - 32; irq++)
+	for (irq = 0; irq < SACCIC_LEN - 32; irq++)
 		if (sc->sc_intrhand[irq + 32])
 			sc->sc_imask.hi |= (1 << irq);
 
