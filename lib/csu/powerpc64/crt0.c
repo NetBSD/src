1a72aafc01f167c00c138e8aeb1626dac6836ecc
ross <ross>
2006-07-01 01:39:17 +0000

    fix comment

--- lib/csu/powerpc64/crt0.c
+++ lib/csu/powerpc64/crt0.c
@@ -1,4 +1,4 @@
-/ $NetBSD: crt0.c,v 1.1 2006/06/30 22:49:50 ross Exp $ */
+/* $NetBSD: crt0.c,v 1.2 2006/07/01 01:39:17 ross Exp $ */
 
 /*
  * Copyright (c) 1997 Jason R. Thorpe.
@@ -93,7 +93,7 @@ _start(int argc, char **argv, char **envp,
  * NOTE: Leave the RCS ID _after_ __start(), in case it gets placed in .text.
  */
 #if defined(LIBC_SCCS) && !defined(lint)
-__RCSID("$NetBSD: crt0.c,v 1.1 2006/06/30 22:49:50 ross Exp $");
+__RCSID("$NetBSD: crt0.c,v 1.2 2006/07/01 01:39:17 ross Exp $");
 #endif /* LIBC_SCCS and not lint */
 
 #include "common.c"
