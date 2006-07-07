8141f357764ccb3974610aecbba2d0bdaa77ff23
simonb <simonb@NetBSD.org>
2006-07-07 01:22:31 +0000

    Regen.

--- sys/dev/pci/pcidevs.h
+++ sys/dev/pci/pcidevs.h
@@ -1,10 +1,10 @@
-/*	$NetBSD: pcidevs.h,v 1.803 2006/06/30 17:19:32 nisimura Exp $	*/
+/*	$NetBSD: pcidevs.h,v 1.804 2006/07/07 01:22:31 simonb Exp $	*/
 
 /*
  * THIS FILE AUTOMATICALLY GENERATED.  DO NOT EDIT.
  *
  * generated from:
- *	NetBSD: pcidevs,v 1.804 2006/06/30 17:17:10 nisimura Exp
+ *	NetBSD: pcidevs,v 1.805 2006/07/07 01:21:37 simonb Exp
  */
 
 /*
@@ -2156,6 +2156,7 @@
 #define	PCI_PRODUCT_INTEL_PRO_WL_3945ABG_1	0x4222		/* PRO/Wireless LAN 3945ABG Mini-PCI Adapter */
 #define	PCI_PRODUCT_INTEL_PRO_WL_2915ABG_1	0x4223		/* PRO/Wireless LAN 2915ABG Mini-PCI Adapter */
 #define	PCI_PRODUCT_INTEL_PRO_WL_2915ABG_2	0x4224		/* PRO/Wireless LAN 2915ABG Mini-PCI Adapter */
+#define	PCI_PRODUCT_INTEL_PRO_WL_3945ABG_2	0x4227		/* PRO/Wireless LAN 2915ABG Mini-PCI Adapter */
 #define	PCI_PRODUCT_INTEL_80312_ATU	0x530d		/* 80310 ATU */
 #define	PCI_PRODUCT_INTEL_82371SB_ISA	0x7000		/* 82371SB (PIIX3) PCI-ISA Bridge */
 #define	PCI_PRODUCT_INTEL_82371SB_IDE	0x7010		/* 82371SB (PIIX3) IDE Interface */
