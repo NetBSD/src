b9497033d773ce485c50fffb4ba6005ea2a1245f
tron <tron@NetBSD.org>
2006-07-13 22:13:27 +0000

    Pull up following revision(s) (requested by ghen in ticket #1426): 	dist/bind/bin/named/query.c: revision 1.2 move is_zone initialisation earlier to avoid a GCC warning.

--- dist/bind/bin/named/query.c
+++ dist/bind/bin/named/query.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: query.c,v 1.1.1.2.2.1 2006/07/13 22:02:05 tron Exp $	*/
+/*	$NetBSD: query.c,v 1.1.1.2.2.2 2006/07/13 22:13:27 tron Exp $	*/
 
 /*
  * Copyright (C) 2004, 2005  Internet Systems Consortium, Inc. ("ISC")
@@ -2382,6 +2382,7 @@ query_find(ns_client_t *client, dns_fetchevent_t *event, dns_rdatatype_t qtype)
 	zone = NULL;
 	need_wildcardproof = ISC_FALSE;
 	empty_wild = ISC_FALSE;
+	is_zone = ISC_FALSE;
 	options = 0;
 
 	if (event != NULL) {
@@ -2392,7 +2393,6 @@ query_find(ns_client_t *client, dns_fetchevent_t *event, dns_rdatatype_t qtype)
 
 		want_restart = ISC_FALSE;
 		authoritative = ISC_FALSE;
-		is_zone = ISC_FALSE;
 
 		qtype = event->qtype;
 		if (qtype == dns_rdatatype_rrsig)
