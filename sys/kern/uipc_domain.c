ff156c46e691f9ecb60795dddfe9cebaed5cf704
elad <elad>
2006-07-16 20:21:42 +0000

    CURTAIN() -> KAUTH_GENERIC_CANSEE.

--- sys/kern/uipc_domain.c
+++ sys/kern/uipc_domain.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: uipc_domain.c,v 1.58 2006/06/13 13:52:39 yamt Exp $	*/
+/*	$NetBSD: uipc_domain.c,v 1.59 2006/07/16 20:21:42 elad Exp $	*/
 
 /*
  * Copyright (c) 1982, 1986, 1993
@@ -32,7 +32,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: uipc_domain.c,v 1.58 2006/06/13 13:52:39 yamt Exp $");
+__KERNEL_RCSID(0, "$NetBSD: uipc_domain.c,v 1.59 2006/07/16 20:21:42 elad Exp $");
 
 #include <sys/param.h>
 #include <sys/socket.h>
@@ -285,8 +285,8 @@ sysctl_unpcblist(SYSCTLFN_ARGS)
 	 * to walk the file list looking for them.  :-/
 	 */
 	LIST_FOREACH(fp, &filehead, f_list) {
-		if (CURTAIN(kauth_cred_geteuid(l->l_proc->p_cred),
-		    kauth_cred_geteuid(fp->f_cred)))
+		if (kauth_authorize_generic(l->l_proc->p_cred,
+		    KAUTH_GENERIC_CANSEE, fp->f_cred) != 0)
 			continue;
 		if (fp->f_type != DTYPE_SOCKET)
 			continue;
