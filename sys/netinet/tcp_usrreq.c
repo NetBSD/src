98f271ed8b428b81e45a00782c023ba76bdca0da
elad <elad@NetBSD.org>
2006-07-16 21:13:00 +0000

    get rid of CURTAIN() macro. inline the last use of it, together with a nice XXX comment (assigned to me of course) that we should be doing this differently.

--- sys/netinet/tcp_usrreq.c
+++ sys/netinet/tcp_usrreq.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: tcp_usrreq.c,v 1.117 2006/05/14 21:19:34 elad Exp $	*/
+/*	$NetBSD: tcp_usrreq.c,v 1.118 2006/07/16 21:13:00 elad Exp $	*/
 
 /*
  * Copyright (C) 1995, 1996, 1997, and 1998 WIDE Project.
@@ -100,7 +100,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: tcp_usrreq.c,v 1.117 2006/05/14 21:19:34 elad Exp $");
+__KERNEL_RCSID(0, "$NetBSD: tcp_usrreq.c,v 1.118 2006/07/16 21:13:00 elad Exp $");
 
 #include "opt_inet.h"
 #include "opt_ipsec.h"
@@ -1257,8 +1257,11 @@ sysctl_inpcblist(SYSCTLFN_ARGS)
 		if (inph->inph_af != pf)
 			continue;
 
-		if (CURTAIN(kauth_cred_getuid(l->l_proc->p_cred),
-			    inph->inph_socket->so_uidinfo->ui_uid)) /* XXX elad */
+		/* XXX elad - should be done better */
+		if (security_curtain &&
+		    (kauth_cred_geteuid(l->l_proc->p_cred) != 0) &&
+		    (kauth_cred_geteuid(l->l_proc->p_cred) !=
+		     inph->inph_socket->so_uidinfo->ui_uid))
 			continue;
 
 		memset(&pcb, 0, sizeof(pcb));
