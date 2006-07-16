9be72299068a5cf5fdf0e3a4568d8376295203e3
elad <elad@NetBSD.org>
2006-07-16 19:37:55 +0000

    expose less api; prompted by yamt@.

--- sys/sys/fileassoc.h
+++ sys/sys/fileassoc.h
@@ -1,4 +1,4 @@
-/* $NetBSD: fileassoc.h,v 1.1 2006/07/14 18:41:40 elad Exp $ */
+/* $NetBSD: fileassoc.h,v 1.2 2006/07/16 19:37:55 elad Exp $ */
 
 /*-
  * Copyright (c) 2006 Elad Efrat <elad@NetBSD.org>
@@ -37,7 +37,6 @@
 
 #include <sys/cdefs.h>
 #include <sys/param.h>
-#include <sys/hash.h>
 
 /* Max. number of hooks. */
 #ifndef FILEASSOC_NHOOKS
@@ -49,49 +48,19 @@
 typedef int fileassoc_t;
 typedef void (*fileassoc_cleanup_cb_t)(void *, int);
 
-/*
- * Hook entry.
- * Includes the hook name for identification and private hook clear callback.
- */
-struct fileassoc_hook {
-	const char *hook_name;			/* Hook name. */
-	fileassoc_cleanup_cb_t hook_cleanup_cb;	/* Hook clear callback. */
-};
-
-/* An entry in the per-device hash table. */
-struct fileassoc_hash_entry {
-	ino_t fileid;					/* File id. */
-	void *hooks[FILEASSOC_NHOOKS];			/* Hooks. */
-	LIST_ENTRY(fileassoc_hash_entry) entries;	/* List pointer. */
-};
-
-LIST_HEAD(fileassoc_hashhead, fileassoc_hash_entry);
-
-struct fileassoc_table {
-	struct fileassoc_hashhead *hash_tbl;
-	size_t hash_size;				/* Number of slots. */
-	struct mount *tbl_mntpt;
-	u_long hash_mask;
-	void *tables[FILEASSOC_NHOOKS];
-	LIST_ENTRY(fileassoc_table) hash_list;		/* List pointer. */
-};
-
 #define	FILEASSOC_CLEANUP_TABLE	0
 #define	FILEASSOC_CLEANUP_FILE	1
 
 void fileassoc_init(void);
 fileassoc_t fileassoc_register(const char *, fileassoc_cleanup_cb_t);
 int fileassoc_deregister(fileassoc_t);
-struct fileassoc_table *fileassoc_table_lookup(struct mount *);
 void *fileassoc_tabledata_lookup(struct mount *, fileassoc_t);
-struct fileassoc_hash_entry *fileassoc_file_lookup(struct vnode *);
 void *fileassoc_lookup(struct vnode *, fileassoc_t);
 int fileassoc_table_add(struct mount *, size_t);
 int fileassoc_table_delete(struct mount *);
 int fileassoc_table_clear(struct mount *, fileassoc_t);
 int fileassoc_tabledata_add(struct mount *, fileassoc_t, void *);
 int fileassoc_tabledata_clear(struct mount *, fileassoc_t);
-struct fileassoc_hash_entry *fileassoc_file_add(struct vnode *);
 int fileassoc_file_delete(struct vnode *);
 int fileassoc_add(struct vnode *, fileassoc_t, void *);
 int fileassoc_clear(struct vnode *, fileassoc_t);
