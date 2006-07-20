23198f432ee509fe0c5faf2dd516f228d2a3ea50
gson <gson@NetBSD.org>
2006-07-20 08:34:32 +0000

    Fix uninitialized variable which was causing pkg_add -u to randomly dump core.  This may fix PR pkg/30292.

--- usr.sbin/pkg_install/add/perform.c
+++ usr.sbin/pkg_install/add/perform.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: perform.c,v 1.120 2006/04/24 13:36:22 dillo Exp $	*/
+/*	$NetBSD: perform.c,v 1.121 2006/07/20 08:34:32 gson Exp $	*/
 
 #if HAVE_CONFIG_H
 #include "config.h"
@@ -14,7 +14,7 @@
 #if 0
 static const char *rcsid = "from FreeBSD Id: perform.c,v 1.44 1997/10/13 15:03:46 jkh Exp";
 #else
-__RCSID("$NetBSD: perform.c,v 1.120 2006/04/24 13:36:22 dillo Exp $");
+__RCSID("$NetBSD: perform.c,v 1.121 2006/07/20 08:34:32 gson Exp $");
 #endif
 #endif
 
@@ -574,6 +574,8 @@ pkg_do(const char *pkg, lpkg_head_t *pkgs)
 							plist_t *depp;
 							char depC[MaxPathSize];
 							
+							depPlist.head = depPlist.tail = NULL;
+
 							s = strrchr(pkg2chk, '\n');
 							if (s)
 								*s = '\0'; /* strip trailing '\n' */
