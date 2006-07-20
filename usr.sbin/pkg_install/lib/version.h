fec06b505b7c8d5e3801b394c608147fb15165fb
gson <gson>
2006-07-20 11:19:33 +0000

    Bump PKGTOOLS_VERSION for today's fix to add/perform.c.

--- usr.sbin/pkg_install/lib/version.h
+++ usr.sbin/pkg_install/lib/version.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: version.h,v 1.80 2006/07/01 19:31:23 dillo Exp $	*/
+/*	$NetBSD: version.h,v 1.81 2006/07/20 11:19:33 gson Exp $	*/
 
 /*
  * Copyright (c) 2001 Thomas Klausner.  All rights reserved.
@@ -33,6 +33,6 @@
 #ifndef _INST_LIB_VERSION_H_
 #define _INST_LIB_VERSION_H_
 
-#define PKGTOOLS_VERSION "20060701"
+#define PKGTOOLS_VERSION "20060720"
 
 #endif /* _INST_LIB_VERSION_H_ */
