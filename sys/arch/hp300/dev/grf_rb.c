ba53811be4729fe9f8468d0b3dcc4b386ad90589
tsutsui <tsutsui@NetBSD.org>
2006-07-21 18:11:01 +0000

    Use more proper macro.

--- sys/arch/hp300/dev/grf_rb.c
+++ sys/arch/hp300/dev/grf_rb.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: grf_rb.c,v 1.33 2006/07/21 10:01:39 tsutsui Exp $	*/
+/*	$NetBSD: grf_rb.c,v 1.34 2006/07/21 18:11:01 tsutsui Exp $	*/
 
 /*-
  * Copyright (c) 1996, 1997 The NetBSD Foundation, Inc.
@@ -117,7 +117,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: grf_rb.c,v 1.33 2006/07/21 10:01:39 tsutsui Exp $");
+__KERNEL_RCSID(0, "$NetBSD: grf_rb.c,v 1.34 2006/07/21 18:11:01 tsutsui Exp $");
 
 #include "opt_compat_hpux.h"
 
@@ -628,10 +628,7 @@ rboxcnattach(bus_space_tag_t bst, bus_addr_t addr, int scode)
 		return 1;
 	}
 
-	if (DIO_ISDIOII(scode))
-		size = DIOII_SIZE(va);
-	else
-		size = DIOCSIZE;
+	size = DIO_SIZE(scode, va);
 
 	bus_space_unmap(bst, bsh, PAGE_SIZE);
 	if (bus_space_map(bst, addr, size, 0, &bsh))
