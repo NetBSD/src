64e4516be161fc4e8f7fccb5e51fbbbb93bae22d
tron <tron@NetBSD.org>
2006-07-26 11:00:07 +0000

    Bluetooth fixes by Iain Hibbert: Remove bluetooth.conf stuff from bthcid(8), and use XML with proplib(3) for keyfile. Also, associate keys with local and remote address.

--- usr.sbin/bthcid/config.c
+++ usr.sbin/bthcid/config.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: config.c,v 1.1 2006/06/19 15:44:56 gdamore Exp $	*/
+/*	$NetBSD: config.c,v 1.2 2006/07/26 11:00:07 tron Exp $	*/
 
 /*-
  * Copyright (c) 2006 Itronix Inc.
@@ -30,104 +30,192 @@
  */
 
 #include <sys/cdefs.h>
-__RCSID("$NetBSD: config.c,v 1.1 2006/06/19 15:44:56 gdamore Exp $");
+__RCSID("$NetBSD: config.c,v 1.2 2006/07/26 11:00:07 tron Exp $");
 
 #include <sys/time.h>
+#include <prop/proplib.h>
 #include <bluetooth.h>
 #include <errno.h>
 #include <event.h>
 #include <fcntl.h>
+#include <stdlib.h>
 #include <string.h>
 #include <syslog.h>
 #include <unistd.h>
 
 #include "bthcid.h"
 
-static	bt_cfgentry_t	*cfg = NULL;
+static const char *key_file = "/var/db/bthcid.keys";
+static const char *new_key_file = "/var/db/bthcid.keys.new";
 
+static prop_dictionary_t
+load_keys(void)
+{
+	prop_dictionary_t dict;
+	char *xml;
+	off_t len;
+	int fd;
+
+	fd = open(key_file, O_RDONLY, 0);
+	if (fd < 0)
+		return NULL;
+
+	len = lseek(fd, 0, SEEK_END);
+	if (len == 0) {
+		close(fd);
+		return NULL;
+	}
+
+	xml = malloc(len);
+	if (xml == NULL) {
+		close(fd);
+		return NULL;
+	}
+
+	(void)lseek(fd, 0, SEEK_SET);
+	if (read(fd, xml, len) != len) {
+		free(xml);
+		close(fd);
+		return NULL;
+	}
+
+	dict = prop_dictionary_internalize(xml);
+	free(xml);
+	close(fd);
+	return dict;
+}
+
+/*
+ * Look up key in keys file. We store a dictionary for each
+ * remote address, and inside that we have a data object for
+ * each local address containing the key.
+ */
 uint8_t *
 lookup_key(bdaddr_t *laddr, bdaddr_t *raddr)
 {
-	bt_handle_t handle;
+	static uint8_t key[HCI_KEY_SIZE];
+	prop_dictionary_t cfg;
+	prop_object_t obj;
 
-	if (cfg != NULL) {
-		bt_freeconfig(cfg);
-		cfg = NULL;
-	}
-
-	handle = bt_openconfig(key_file);
-	if (handle == NULL)
+	cfg = load_keys();
+	if (cfg == NULL)
 		return NULL;
 
-	cfg = bt_getconfig(handle, raddr);
-	bt_closeconfig(handle);
-
-	if (cfg == NULL) {
-		handle = bt_openconfig(config_file);
-		if (handle == NULL)
-			return NULL;
-
-		cfg = bt_getconfig(handle, raddr);
-		bt_closeconfig(handle);
+	obj = prop_dictionary_get(cfg, bt_ntoa(laddr, NULL));
+	if (obj == NULL || prop_object_type(obj) != PROP_TYPE_DICTIONARY) {
+		prop_object_release(cfg);
+		return NULL;
 	}
 
-	if (cfg != NULL)
-		return cfg->key;
-
-	return NULL;
-}
-
-uint8_t *
-lookup_pin(bdaddr_t *laddr, bdaddr_t *raddr)
-{
-	bt_handle_t handle;
-
-	if (cfg != NULL) {
-		bt_freeconfig(cfg);
-		cfg = NULL;
+	obj = prop_dictionary_get(obj, bt_ntoa(raddr, NULL));
+	if (obj == NULL || prop_object_type(obj) != PROP_TYPE_DATA
+	    || prop_data_size(obj) != sizeof(key)) {
+		prop_object_release(cfg);
+		return NULL;
 	}
 
-	handle = bt_openconfig(config_file);
-	if (handle != NULL) {
-		cfg = bt_getconfig(handle, raddr);
-		bt_closeconfig(handle);
-
-		if (cfg != NULL && cfg->pin != NULL)
-			return cfg->pin;
-	}
-
-	return lookup_item(laddr, raddr);
+	memcpy(key, prop_data_data_nocopy(obj), sizeof(key));
+	prop_object_release(cfg);
+	return key;
 }
 
 void
 save_key(bdaddr_t *laddr, bdaddr_t *raddr, uint8_t *key)
 {
-	static const char hex[] = "0123456789abcdef";
-	char buffer[100], keybuf[HCI_KEY_SIZE * 2 + 1];
-	int fd, n, i;
+	prop_dictionary_t cfg, dev;
+	prop_data_t dat;
+	char *xml;
+	int fd;
+	size_t len;
 
-	fd = open(key_file, O_WRONLY|O_APPEND|O_CREAT|O_EXLOCK, 0600);
-	if (fd < 0) {
-		syslog(LOG_ERR, "Cannot open keyfile %s. %s (%d)",
-				key_file, strerror(errno), errno);
+	cfg = load_keys();
+	if (cfg == NULL) {
+		cfg = prop_dictionary_create();
+		if (cfg == NULL) {
+			syslog(LOG_ERR, "prop_dictionary_create() failed. %s (%d)",
+				strerror(errno), errno);
 
+			return;
+		}
+	}
+
+	dev = prop_dictionary_get(cfg, bt_ntoa(laddr, NULL));
+	if (dev == NULL) {
+		dev = prop_dictionary_create();
+		if (dev == NULL) {
+			syslog(LOG_ERR, "prop_dictionary_create() failed. %s (%d)",
+				strerror(errno), errno);
+
+			prop_object_release(cfg);
+			return;
+		}
+
+		if (!prop_dictionary_set(cfg, bt_ntoa(laddr, NULL), dev)) {
+			syslog(LOG_ERR, "prop_dictionary_set() failed. %s (%d)",
+				strerror(errno), errno);
+			
+			prop_object_release(dev);
+			prop_object_release(cfg);
+			return;
+		}
+	}
+
+	dat = prop_data_create_data_nocopy(key, HCI_KEY_SIZE);
+	if (dat == NULL) {
+		syslog(LOG_ERR, "Cannot create data object. %s (%d)",
+			strerror(errno), errno);
+
+		prop_object_release(cfg);
 		return;
 	}
 
-	for (n = i = 0 ; i < HCI_KEY_SIZE ; i++) {
-		keybuf[n++] = hex[(key[i] >> 4) & 0xf];
-		keybuf[n++] = hex[key[i] & 0xf];
+	if (!prop_dictionary_set(dev, bt_ntoa(raddr, NULL), dat)) {
+		syslog(LOG_ERR, "prop_dictionary_set() failed. %s (%d)",
+			strerror(errno), errno);
+
+		prop_object_release(dat);
+		prop_object_release(cfg);
+		return;
 	}
-	keybuf[n] = '\0';
 
-	n = snprintf(buffer, sizeof(buffer),
-		"\ndevice {\n"
-		"\tbdaddr  %s;\n"
-		"\tkey     0x%s;\n"
-		"}\n",
-		bt_ntoa(raddr, NULL),
-		keybuf);
+	xml = prop_dictionary_externalize(cfg);
+	if (xml == NULL) {
+		syslog(LOG_ERR, "prop_dictionary_externalize() failed. %s (%d)",
+			strerror(errno), errno);
 
-	write(fd, buffer, (size_t)n);
+		prop_object_release(cfg);
+		return;
+	}
+
+	prop_object_release(cfg);
+
+	fd = open(new_key_file, O_WRONLY|O_TRUNC|O_CREAT|O_EXLOCK, 0600);
+	if (fd < 0) {
+		syslog(LOG_ERR, "Cannot open new keyfile %s. %s (%d)",
+				key_file, strerror(errno), errno);
+
+		free(xml);
+		return;
+	}
+
+	len = strlen(xml);
+	if (write(fd, xml, len) != len) {
+		syslog(LOG_ERR, "Write of keyfile failed. %s (%d)",
+				strerror(errno), errno);
+
+		free(xml);
+		close(fd);
+		unlink(new_key_file);
+		return;
+	}
+
+	free(xml);
 	close(fd);
+
+	if (rename(new_key_file, key_file) < 0) {
+		syslog(LOG_ERR, "rename(%s, %s) failed. %s (%d)",
+			new_key_file, key_file, strerror(errno), errno);
+
+		unlink(new_key_file);
+	}
 }
