ab3e0c094825dadd00954e72c529c88d698a19be
lukem <lukem@NetBSD.org>
2006-07-26 14:28:11 +0000

    If a file upload (via -u) fails, return an non-zero exit value based on the index of the file that caused the problem (a la auto-fetch retrieval). Problem noted by A P Garcia in private email.

--- usr.bin/ftp/version.h
+++ usr.bin/ftp/version.h
@@ -1,6 +1,6 @@
-/*	$NetBSD: version.h,v 1.57 2005/08/21 16:16:33 lukem Exp $	*/
+/*	$NetBSD: version.h,v 1.58 2006/07/26 14:28:11 lukem Exp $	*/
 /*-
- * Copyright (c) 1999-2005 The NetBSD Foundation, Inc.
+ * Copyright (c) 1999-2006 The NetBSD Foundation, Inc.
  * All rights reserved.
  *
  * This code is derived from software contributed to The NetBSD Foundation
@@ -40,5 +40,5 @@
 #endif
 
 #ifndef FTP_VERSION
-#define	FTP_VERSION	"20050822"
+#define	FTP_VERSION	"20060726"
 #endif
