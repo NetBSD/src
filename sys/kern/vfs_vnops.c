2957dc481efcb9b137fefb1594fb55de3493d75d
dogcow <dogcow@NetBSD.org>
2006-07-26 09:33:57 +0000

    at the request of elad, as veriexec.h has returned, revert the changes from 2006-07-25.

--- sys/kern/vfs_vnops.c
+++ sys/kern/vfs_vnops.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: vfs_vnops.c,v 1.121 2006/07/25 00:23:06 dogcow Exp $	*/
+/*	$NetBSD: vfs_vnops.c,v 1.122 2006/07/26 09:33:57 dogcow Exp $	*/
 
 /*
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -37,10 +37,10 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: vfs_vnops.c,v 1.121 2006/07/25 00:23:06 dogcow Exp $");
+__KERNEL_RCSID(0, "$NetBSD: vfs_vnops.c,v 1.122 2006/07/26 09:33:57 dogcow Exp $");
 
 #include "fs_union.h"
-#include <sys/verified_exec.h>
+#include "veriexec.h"
 
 #include <sys/param.h>
 #include <sys/systm.h>
