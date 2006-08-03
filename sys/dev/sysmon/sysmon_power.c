3b198d3f286ad610b081a869cb3a4f661fcca41d
tron <tron>
2006-08-03 12:35:41 +0000

    Pull up following revision(s) (requested by freza in ticket #10642): 	sys/dev/sysmon/sysmon_power.c: revision 1.12 via patch Advance queue indices properly instead of silently rewriting sysmon_power_event_queue[0] all the time. OK by thorpej@

--- sys/dev/sysmon/sysmon_power.c
+++ sys/dev/sysmon/sysmon_power.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: sysmon_power.c,v 1.8 2003/07/14 15:47:28 lukem Exp $	*/
+/*	$NetBSD: sysmon_power.c,v 1.8.4.1 2006/08/03 12:35:41 tron Exp $	*/
 
 /*
  * Copyright (c) 2003 Wasabi Systems, Inc.
@@ -44,7 +44,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: sysmon_power.c,v 1.8 2003/07/14 15:47:28 lukem Exp $");
+__KERNEL_RCSID(0, "$NetBSD: sysmon_power.c,v 1.8.4.1 2006/08/03 12:35:41 tron Exp $");
 
 #include <sys/param.h>
 #include <sys/reboot.h>
@@ -77,7 +77,7 @@ static char sysmon_power_type[32];
 
 #define	PEVQ_F_WAITING		0x01	/* daemon waiting for event */
 
-#define	SYSMON_NEXT_EVENT(x)		(((x) + 1) / SYSMON_MAX_POWER_EVENTS)
+#define	SYSMON_NEXT_EVENT(x)		(((x) + 1) % SYSMON_MAX_POWER_EVENTS)
 
 /*
  * sysmon_queue_power_event:
