2ccbe56dc0a9685746cb06537bf98f49cb3b9a0f
bjh21 <bjh21@NetBSD.org>
2006-08-05 22:54:28 +0000

    Centralize the various checks for validity of a CPU context in one macro, so we don't have to get it right in lots of different places.

--- sys/arch/arm/arm/compat_13_machdep.c
+++ sys/arch/arm/arm/compat_13_machdep.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: compat_13_machdep.c,v 1.8 2005/12/11 12:16:41 christos Exp $	*/
+/*	$NetBSD: compat_13_machdep.c,v 1.9 2006/08/05 22:54:28 bjh21 Exp $	*/
 
 /*
  * Copyright (c) 1994-1998 Mark Brinicombe.
@@ -38,7 +38,7 @@
 
 #include <sys/param.h>
 
-__KERNEL_RCSID(0, "$NetBSD: compat_13_machdep.c,v 1.8 2005/12/11 12:16:41 christos Exp $");
+__KERNEL_RCSID(0, "$NetBSD: compat_13_machdep.c,v 1.9 2006/08/05 22:54:28 bjh21 Exp $");
 
 #include <sys/systm.h>
 #include <sys/signalvar.h>
@@ -76,15 +76,8 @@ compat_13_sys_sigreturn(struct lwp *l, void *v, register_t *retval)
 	 * Make sure the processor mode has not been tampered with and
 	 * interrupts have not been disabled.
 	 */
-#ifdef __PROG32
-	if ((context.sc_spsr & PSR_MODE) != PSR_USR32_MODE ||
-	    (context.sc_spsr & (I32_bit | F32_bit)) != 0)
-		return (EINVAL);
-#else /* __PROG26 */
-	if ((context.sc_pc & R15_MODE) != R15_MODE_USR ||
-	    (context.sc_pc & (R15_IRQ_DISABLE | R15_FIQ_DISABLE)) != 0)
+	if (!VALID_R15_PSR(context.sc_pc, context.sc_spsr))
 		return EINVAL;
-#endif
 
 	/* Restore register context. */
 	tf = l->l_addr->u_pcb.pcb_tf;
