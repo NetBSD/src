6a8ca801be2c283e71618fa75dcb1a81e8da9059
tsutsui <tsutsui>
2006-08-07 23:29:38 +0000

    Workaround fix for a problem caused after IPL_* macro changes. Based on a patch from mrg on port-sun2.

--- sys/arch/sun68k/sun68k/isr.c
+++ sys/arch/sun68k/sun68k/isr.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: isr.c,v 1.8 2006/03/29 08:55:40 yamt Exp $	*/
+/*	$NetBSD: isr.c,v 1.9 2006/08/07 23:29:38 tsutsui Exp $	*/
 
 /*-
  * Copyright (c) 1996 The NetBSD Foundation, Inc.
@@ -42,7 +42,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: isr.c,v 1.8 2006/03/29 08:55:40 yamt Exp $");
+__KERNEL_RCSID(0, "$NetBSD: isr.c,v 1.9 2006/08/07 23:29:38 tsutsui Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -305,13 +305,18 @@ softintr_establish(int ipl, void (*func)(void *), void *arg)
 {
 	struct softintr_handler *sh;
 	struct softintr_head *shd;
+	int level;
 
-	ipl &= ~PSL_S;
-	if (ipl < _IPL_SOFT_LEVEL_MIN || ipl > _IPL_SOFT_LEVEL_MAX)
+	if (ipl == IPL_SOFT_LEVEL1)
+		level = _IPL_SOFT_LEVEL1;
+	else if (ipl == IPL_SOFT_LEVEL2)
+		level = _IPL_SOFT_LEVEL2;
+	else if (ipl == IPL_SOFT_LEVEL3)
+		level = _IPL_SOFT_LEVEL3;
+	else
 		panic("softintr_establish: unsupported soft IPL");
 
-	shd = &soft_level_heads[ipl - _IPL_SOFT_LEVEL_MIN];
-
+	shd = &soft_level_heads[level - _IPL_SOFT_LEVEL_MIN];
 	sh = malloc(sizeof(*sh), M_SOFTINTR, M_NOWAIT);
 	if (sh == NULL)
 		return NULL;
