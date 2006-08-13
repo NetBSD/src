ac96fed0c2db58c6da863bab0e59bfe9d65eb26c
jmcneill <jmcneill@NetBSD.org>
2006-08-13 20:22:27 +0000

    No longer try to attach unichromefb as an initial console device.

--- sys/arch/x86/x86/consinit.c
+++ sys/arch/x86/x86/consinit.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: consinit.c,v 1.11 2006/08/02 01:44:50 jmcneill Exp $	*/
+/*	$NetBSD: consinit.c,v 1.12 2006/08/13 20:22:27 jmcneill Exp $	*/
 
 /*
  * Copyright (c) 1998
@@ -27,7 +27,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: consinit.c,v 1.11 2006/08/02 01:44:50 jmcneill Exp $");
+__KERNEL_RCSID(0, "$NetBSD: consinit.c,v 1.12 2006/08/13 20:22:27 jmcneill Exp $");
 
 #include "opt_kgdb.h"
 
@@ -75,10 +75,6 @@ __KERNEL_RCSID(0, "$NetBSD: consinit.c,v 1.11 2006/08/02 01:44:50 jmcneill Exp $
 #include "vesafb.h"
 #if (NVESAFB > 0)
 #include <arch/i386/bios/vesafbvar.h>
-#endif
-#include "unichromefb.h"
-#if (NUNICHROMEFB > 0)
-extern int unichromefb_cnattach(void);
 #endif
 #endif
 
@@ -169,11 +165,6 @@ consinit()
 #if (NPC > 0) || (NVGA > 0) || (NEGA > 0) || (NPCDISPLAY > 0) || (NVESAFB > 0)
 	if (!strcmp(consinfo->devname, "pc")) {
 		int error;
-
-#if (NUNICHROMEFB > 0)
-		if (!unichromefb_cnattach())
-			goto dokbd;
-#endif
 #if (NVESAFB > 0)
 		if (!vesafb_cnattach())
 			goto dokbd;
