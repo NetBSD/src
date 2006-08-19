76089dcfa0b07ceed8a2b8386b4b557947c90d81
ghen <ghen@NetBSD.org>
2006-08-19 09:33:56 +0000

    Welcome to NetBSD 3.1_RC1.

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.211.2.9 2005/12/28 15:35:55 riz Exp $	*/
+/*	$NetBSD: param.h,v 1.211.2.10 2006/08/19 09:33:56 ghen Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -63,7 +63,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	300000000	/* NetBSD 3.0_STABLE */
+#define	__NetBSD_Version__	301000000	/* NetBSD 3.1_RC1 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) >= __NetBSD_Version__)
