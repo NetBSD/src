ebab9dadc198a5fe6ed0f86f51ab5b98d0cf3493
christos <christos@NetBSD.org>
2006-08-21 12:45:30 +0000

    Change to a 3 clause copyright after permission of the holders.

--- lib/libedit/filecomplete.c
+++ lib/libedit/filecomplete.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: filecomplete.c,v 1.8 2005/08/03 13:41:38 christos Exp $	*/
+/*	$NetBSD: filecomplete.c,v 1.9 2006/08/21 12:45:30 christos Exp $	*/
 
 /*-
  * Copyright (c) 1997 The NetBSD Foundation, Inc.
@@ -15,11 +15,7 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed by the NetBSD
- *	Foundation, Inc. and its contributors.
- * 4. Neither the name of The NetBSD Foundation nor the names of its
+ * 3. Neither the name of The NetBSD Foundation nor the names of its
  *    contributors may be used to endorse or promote products derived
  *    from this software without specific prior written permission.
  *
@@ -38,7 +34,7 @@
 
 #include "config.h"
 #if !defined(lint) && !defined(SCCSID)
-__RCSID("$NetBSD: filecomplete.c,v 1.8 2005/08/03 13:41:38 christos Exp $");
+__RCSID("$NetBSD: filecomplete.c,v 1.9 2006/08/21 12:45:30 christos Exp $");
 #endif /* not lint && not SCCSID */
 
 #include <sys/types.h>
