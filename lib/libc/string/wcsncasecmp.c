12e7d27ef5b18357d4cff33eaab79409da60725b
christos <christos>
2006-08-22 20:50:46 +0000

    PR/34238: Aleksey Cheusov: add wcsdup, wcscasecmp and wcsncasecmp functions

---
+++ lib/libc/string/wcsncasecmp.c
@@ -0,0 +1,41 @@
+/*	$NetBSD: wcsncasecmp.c,v 1.1 2006/08/22 20:50:46 christos Exp $	*/
+
+#include <sys/cdefs.h>
+#if defined(LIBC_SCCS) && !defined(lint) 
+__RCSID("$NetBSD: wcsncasecmp.c,v 1.1 2006/08/22 20:50:46 christos Exp $"); 
+#endif /* LIBC_SCCS and not lint */ 
+
+#include "namespace.h"
+#include <assert.h>
+#include <wchar.h>
+#include <wctype.h>
+
+__weak_alias(wcsncasecmp,_wcsncasecmp)
+
+int
+wcsncasecmp(const wchar_t *s1, const wchar_t *s2, size_t n)
+{
+	int lc1  = 0;
+	int lc2  = 0;
+	int diff = 0;
+
+	_DIAGASSERT(s1);
+	_DIAGASSERT(s2);
+
+	while (n--) {
+		lc1 = towlower (*s1);
+		lc2 = towlower (*s2);
+
+		diff = lc1 - lc2;
+		if (diff)
+			return diff;
+
+		if (!lc1)
+			return 0;
+
+		++s1;
+		++s2;
+	}
+
+	return 0;
+}
