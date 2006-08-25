9fc27404ee1e9e3452e6630d8d168b7ca520846a
skrll <skrll@NetBSD.org>
2006-08-25 12:44:19 +0000

    Implement MD_DO_NOT_NEED_FALLTHRU for i386.
    Reviewed by christos.

--- lib/csu/i386_elf/dot_init.h
+++ lib/csu/i386_elf/dot_init.h
@@ -1,4 +1,4 @@
-/* $NetBSD: dot_init.h,v 1.4 2005/12/24 22:02:10 perry Exp $ */
+/* $NetBSD: dot_init.h,v 1.5 2006/08/25 12:44:19 skrll Exp $ */
 
 /*-
  * Copyright (c) 2001 Ross Harvey
@@ -35,22 +35,14 @@
 
 #include <sys/cdefs.h>			/* RCS ID & Copyright macro defns */
 
-/*
- * These must be extern to avoid warnings ("declared static but never defined")
- * However, only the declaration is extern, the actually __asm() defines them
- * as static.
- */
-#define	INIT_FALLTHRU_DECL void init_fallthru(void)
-#define	FINI_FALLTHRU_DECL void fini_fallthru(void)
-
-#define	INIT_FALLTHRU()	init_fallthru()
-#define	FINI_FALLTHRU()	fini_fallthru()
-
 #define	MD_SECTION_PROLOGUE(sect, entry_pt)		\
 		__asm (					\
 		".section "#sect",\"ax\",@progbits	\n"\
-		#entry_pt":				\n"\
+		".global "#entry_pt" 			\n"\
 		"	.align	16			\n"\
+		#entry_pt":				\n"\
+		"	pushl	%ebp			\n"\
+		"	movl	%esp, %ebp		\n"\
 		"	/* fall thru */			\n"\
 		".previous")
 
@@ -59,14 +51,17 @@
 #define	MD_SECTION_EPILOGUE(sect)			\
 		__asm (					\
 		".section "#sect",\"ax\",@progbits	\n"\
+		"	leave				\n"\
 		"	ret				\n"\
 		".previous")
 
-#define	MD_INIT_SECTION_PROLOGUE MD_SECTION_PROLOGUE(.init, init_fallthru)
-#define	MD_FINI_SECTION_PROLOGUE MD_SECTION_PROLOGUE(.fini, fini_fallthru)
+#define	MD_INIT_SECTION_PROLOGUE MD_SECTION_PROLOGUE(.init, _init)
+#define	MD_FINI_SECTION_PROLOGUE MD_SECTION_PROLOGUE(.fini, _fini)
 
 #define	MD_INIT_SECTION_EPILOGUE MD_SECTION_EPILOGUE(.init)
 #define	MD_FINI_SECTION_EPILOGUE MD_SECTION_EPILOGUE(.fini)
+
+#define	MD_DO_NOT_NEED_FALLTHRU
 
 #define MD_CALL_STATIC_FUNCTION(section, func) \
 __asm(".section " #section "; call " #func "; .previous");
