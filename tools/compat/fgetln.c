4c01df241cbd6271768f0217c51a0c328a996203
christos <christos@NetBSD.org>
2006-08-26 16:24:29 +0000

    Remove advertising clause.

--- tools/compat/fgetln.c
+++ tools/compat/fgetln.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: fgetln.c,v 1.6 2005/05/15 21:31:26 christos Exp $	*/
+/*	$NetBSD: fgetln.c,v 1.7 2006/08/26 16:24:29 christos Exp $	*/
 
 /*-
  * Copyright (c) 1998 The NetBSD Foundation, Inc.
@@ -15,11 +15,7 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *        This product includes software developed by the NetBSD
- *        Foundation, Inc. and its contributors.
- * 4. Neither the name of The NetBSD Foundation nor the names of its
+ * 3. Neither the name of The NetBSD Foundation nor the names of its
  *    contributors may be used to endorse or promote products derived
  *    from this software without specific prior written permission.
  *
