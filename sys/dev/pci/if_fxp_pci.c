f82bf2573c665bb4a229d2166708c4bd96aee28d
ghen <ghen@NetBSD.org>
2006-08-27 09:37:40 +0000

    Pull up following revision(s) (requested by oster in ticket #1479): 	sys/dev/pci/pcidevs: revision 1.810 	sys/dev/pci/if_fxp_pci.c: revision 1.44 	sys/dev/pci/if_fxp_pci.c: revision 1.47 	sys/dev/pci/pcidevs: revision 1.756 PR/32494: Add ID 0x1068 for Intel PRO/100 VE (LOM) found on Sony Vaio           S4HRP laptops. PR/32494: Dennis Chernoivanov: Intel PRO/100 VE (LOM) not detected on Sony Vaio Recognize another Intel PRO/100 VM Network Controller.

--- sys/dev/pci/if_fxp_pci.c
+++ sys/dev/pci/if_fxp_pci.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: if_fxp_pci.c,v 1.40.2.2 2005/11/21 18:44:37 tron Exp $	*/
+/*	$NetBSD: if_fxp_pci.c,v 1.40.2.3 2006/08/27 09:37:40 ghen Exp $	*/
 
 /*-
  * Copyright (c) 1997, 1998, 1999, 2000, 2001 The NetBSD Foundation, Inc.
@@ -43,7 +43,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: if_fxp_pci.c,v 1.40.2.2 2005/11/21 18:44:37 tron Exp $");
+__KERNEL_RCSID(0, "$NetBSD: if_fxp_pci.c,v 1.40.2.3 2006/08/27 09:37:40 ghen Exp $");
 
 #include "rnd.h"
 
@@ -130,6 +130,10 @@ static const struct fxp_pci_product {
 	  "Intel PRO/100 VE Network Controller with 82562ET/EZ (CNR) PHY" },
 	{ PCI_PRODUCT_INTEL_PRO_100_VE_4,
 	  "Intel PRO/100 VE (MOB) Network Controller" },
+	{ PCI_PRODUCT_INTEL_PRO_100_VE_5,
+	  "Intel PRO/100 VE (LOM) Network Controller" },
+	{ PCI_PRODUCT_INTEL_PRO_100_VE_6,
+	  "Intel PRO/100 VE Network Controller" },
 	{ PCI_PRODUCT_INTEL_PRO_100_VM_0,
 	  "Intel PRO/100 VM Network Controller" },
 	{ PCI_PRODUCT_INTEL_PRO_100_VM_1,
@@ -401,6 +405,7 @@ fxp_pci_attach(struct device *parent, struct device *self, void *aux)
 	case PCI_PRODUCT_INTEL_PRO_100_VE_2:
 	case PCI_PRODUCT_INTEL_PRO_100_VE_3:
 	case PCI_PRODUCT_INTEL_PRO_100_VE_4:
+	case PCI_PRODUCT_INTEL_PRO_100_VE_5:
 	case PCI_PRODUCT_INTEL_PRO_100_VM_3:
 	case PCI_PRODUCT_INTEL_PRO_100_VM_4:
 	case PCI_PRODUCT_INTEL_PRO_100_VM_5:
