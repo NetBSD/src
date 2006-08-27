2f2c83ca0cbb29e5feb1cdbd048edcd18b9be69d
rpaulo <rpaulo@NetBSD.org>
2006-08-27 00:37:46 +0000

    postfix 2.3.3

--- gnu/dist/postfix/src/global/mail_date.c
+++ gnu/dist/postfix/src/global/mail_date.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: mail_date.c,v 1.1.1.4 2004/05/31 00:24:31 heas Exp $	*/
+/*	$NetBSD: mail_date.c,v 1.1.1.5 2006/08/27 00:39:46 rpaulo Exp $	*/
 
 /*++
 /* NAME
@@ -123,7 +123,7 @@ const char *mail_date(time_t when)
      * Finally, add the time zone name.
      */
     while (strftime(vstring_end(vp), vstring_avail(vp), " (%Z)", lt) == 0)
-	VSTRING_SPACE(vp, 100);
+	VSTRING_SPACE(vp, vstring_avail(vp) + 100);
     VSTRING_SKIP(vp);
 
     return (vstring_str(vp));
