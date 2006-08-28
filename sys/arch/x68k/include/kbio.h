2195fa1060b5bb457c67d1b5048a8553d95bb503
he <he@NetBSD.org>
2006-08-28 17:15:24 +0000

    Add duplicate include protection to <dev/sun/kbio.h>. Make the x68k port use this include file, so that what remains is only the port-specific additions.

--- sys/arch/x68k/include/kbio.h
+++ sys/arch/x68k/include/kbio.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: kbio.h,v 1.4 2005/12/11 12:19:44 christos Exp $ */
+/*	$NetBSD: kbio.h,v 1.5 2006/08/28 17:15:24 he Exp $ */
 
 /*
  * Copyright (c) 1992, 1993
@@ -43,34 +43,7 @@
 #ifndef _X68K_KBIO_H_
 #define _X68K_KBIO_H_
 
-/*
- * The following is a minimal emulation of Sun's `kio' structures
- * and related operations necessary to make X11 happy (i.e., make it
- * compile, and make old X11 binaries run).
- */
-
-/*
- * The kiockey structure apparently gets and/or sets keyboard mappings.
- * It seems to be kind of useless, but X11 uses it (according to the
- * comments) to figure out when a Sun 386i has a type-4 keyboard but
- * claims to have a type-3 keyboard.  We need just enough to cause the
- * appropriate ioctl to return the appropriate magic value.
- *
- * KIOCGETKEY fills in kio_entry from kio_station.  Not sure what tablemask
- * is for; X sets it before the call, so it is not an output, but we do not
- * care anyway.  KIOCSDIRECT is supposed to tell the kernel whether to send
- * keys to the console or to X; we just send them to X whenever the keyboard
- * is open at all.  (XXX may need to change this later)
- *
- * Keyboard commands and types are defined in kbd.h as they are actually
- * real hardware commands and type numbers.
- */
-struct okiockey { /* Out-dated key translation structure */
-	int	kio_tablemask;	/* whatever */
-	u_char	kio_station;	/* key number */
-	u_char	kio_entry;	/* HOLE if not present */
-	char	kio_text[10];	/* the silly escape sequences (unsupported) */
-};
+#include <dev/sun/kbio.h>
 
 struct kiockey {
 	int	kio_tablemask;	/* whatever */
@@ -102,32 +75,9 @@ struct kbiocbell {
  * in KIOC[SG]KEY ioctls. Currently, we only have "non-shift" and "shift"
  * tables.
  */
-#define KIOC_NOMASK	0x0
-#define KIOC_CAPSMASK	0x1
-#define KIOC_SHIFTMASK	0xe
-#define KIOC_CTRLMASK	0x30
-#define KIOC_ALTGMASK	0x200
-#define KIOC_NUMLMASK	0x800
 
 #define	HOLE	0x302		/* value for kio_entry to say `really type 3' */
 
-#define	KIOCTRANS	_IOW('k', 0, int)	/* set translation mode */
-			/* (we only accept TR_UNTRANS_EVENT) */
-#define	KIOCGETKEY	_IOWR('k', 2, struct okiockey) /* fill in kio_entry */
-#define	KIOCGTRANS	_IOR('k', 5, int)	/* get translation mode */
-#define	KIOCCMD		_IOW('k', 8, int)	/* X uses this to ring bell */
-#define	KIOCTYPE	_IOR('k', 9, int)	/* get keyboard type */
-#define	KIOCSDIRECT	_IOW('k', 10, int)	/* keys to console? */
-#define KIOCSKEY	_IOW('k', 12, struct kiockey) /* set xlat mode */
-#define KIOCGKEY	_IOWR('k', 13, struct kiockey) /* get xlat mode */
-#define	KIOCLAYOUT	_IOR('k', 20, int)	/* get keyboard layout */
-#define	KIOCSLED	_IOW('k', 14, char)	/* set LED state */
-#define	KIOCGLED	_IOR('k', 15, char)	/* get LED state */
 #define	KIOCSBELL	_IOW('k', 16, struct kbiocbell) /* set bell parameter */
 
-#define	TR_NONE			0	/* X compat, unsupported */
-#define	TR_ASCII		1	/* X compat, unsupported */
-#define	TR_EVENT		2	/* X compat, unsupported */
-#define	TR_UNTRANS_EVENT	3
-
-#endif
+#endif /* _X68K_KBIO_H */
