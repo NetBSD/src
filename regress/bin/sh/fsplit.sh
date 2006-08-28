0e61ccad8394a9821e372e4ddbdac4707d3cbb06
dsl <dsl>
2006-08-28 20:09:59 +0000

    Add some more regression tests for stuff related to the recenty bugs. Repeat after me 'run these tests before committing changes to /bin/sh source'.

--- regress/bin/sh/fsplit.sh
+++ regress/bin/sh/fsplit.sh
@@ -39,47 +39,60 @@ check 'for f in $x; do echo x${f}y; done' ''
 # Check that IFS is applied to text from ${x-...} unless it is inside
 # any set of "..."
 check 'set ${x-a b c}; echo $#' 3
-check 'for i in ${x-a b c}; do echo "z${i}z"; done' 'zaz zbz zcz'
-check 'for i in ${x-"a b" c}; do echo "z${i}z"; done' 'za bz zcz'
-check 'for i in ${x-"a ${x-b c}" d}; do echo "z${i}z"; done' 'za b cz zdz'
+check 'for i in ${x-a b c};            do echo "z${i}z"; done' 'zaz zbz zcz'
+check 'for i in ${x-"a b" c};          do echo "z${i}z"; done' 'za bz zcz'
+check 'for i in ${x-"a ${x-b c}" d};   do echo "z${i}z"; done' 'za b cz zdz'
 check 'for i in ${x-"a ${x-"b c"}" d}; do echo "z${i}z"; done' 'za b cz zdz'
-check 'for i in ${x-a ${x-"b c"} d}; do echo "z${i}z"; done' 'zaz zb cz zdz'
-check 'for i in ${x-a ${x-b c} d}; do echo "z${i}z"; done' 'zaz zbz zcz zdz'
+check 'for i in ${x-a ${x-"b c"} d};   do echo "z${i}z"; done' 'zaz zb cz zdz'
+check 'for i in ${x-a ${x-b c} d};     do echo "z${i}z"; done' 'zaz zbz zcz zdz'
 
 # repeat with an alphabetic in IFS
 check 'IFS=q; set ${x-aqbqc}; echo $#' 3
-check 'IFS=q; for i in ${x-aqbqc}; do echo "z${i}z"; done' 'zaz zbz zcz'
-check 'IFS=q; for i in ${x-"aqb"qc}; do echo "z${i}z"; done' 'zaqbz zcz'
-check 'IFS=q; for i in ${x-"aq${x-bqc}"qd}; do echo "z${i}z"; done' 'zaqbqcz zdz'
+check 'IFS=q; for i in ${x-aqbqc};            do echo "z${i}z"; done' 'zaz zbz zcz'
+check 'IFS=q; for i in ${x-"aqb"qc};          do echo "z${i}z"; done' 'zaqbz zcz'
+check 'IFS=q; for i in ${x-"aq${x-bqc}"qd};   do echo "z${i}z"; done' 'zaqbqcz zdz'
 check 'IFS=q; for i in ${x-"aq${x-"bqc"}"qd}; do echo "z${i}z"; done' 'zaqbqcz zdz'
-check 'IFS=q; for i in ${x-aq${x-"bqc"}qd}; do echo "z${i}z"; done' 'zaz zbqcz zdz'
+check 'IFS=q; for i in ${x-aq${x-"bqc"}qd};  do echo "z${i}z"; done' 'zaz zbqcz zdz'
 
 # Some quote propogation checks
-check 'set "${x-a b c}"; echo $#' 1
+check 'set "${x-a b c}";   echo $#' 1
 check 'set "${x-"a b" c}"; echo $1' 'a b c'
 check 'for i in "${x-a b c}"; do echo "z${i}z"; done' 'za b cz'
 
 # Check we get "$@" right
-check 'set "a b" c; for i; do echo "z${i}z"; done' 'za bz zcz'
-check 'set "a b" c; for i in "$@"; do echo "z${i}z"; done' 'za bz zcz'
-check 'set "a b" c; for i in $@; do echo "z${i}z"; done' 'zaz zbz zcz'
-check 'set " a b " c; for i in "$@"; do echo "z${i}z"; done' 'z a b z zcz'
+check 'set "";        for i;           do echo "z${i}z"; done' 'zz'
+check 'set "";        for i in "$@";   do echo "z${i}z"; done' 'zz'
+check 'set "" "";     for i;           do echo "z${i}z"; done' 'zz zz'
+check 'set "" "";     for i in "$@";   do echo "z${i}z"; done' 'zz zz'
+check 'set "" "";     for i in $@;     do echo "z${i}z"; done' ''
+check 'set "a b" c;   for i;           do echo "z${i}z"; done' 'za bz zcz'
+check 'set "a b" c;   for i in "$@";   do echo "z${i}z"; done' 'za bz zcz'
+check 'set "a b" c;   for i in $@;     do echo "z${i}z"; done' 'zaz zbz zcz'
+check 'set " a b " c; for i in "$@";   do echo "z${i}z"; done' 'z a b z zcz'
+check 'set --;        for i in x"$@"x; do echo "z${i}z"; done' 'zxxz'
+check 'set a;         for i in x"$@"x; do echo "z${i}z"; done' 'zxaxz'
+check 'set a b;       for i in x"$@"x; do echo "z${i}z"; done' 'zxaz zbxz'
 
 # Some IFS tests
-check 't=axb; IFS="x"; set $t; IFS=":"; r="$*"; IFS=; echo $# $r' '2 a:b'
-check 't="a x b"; IFS="x"; set $t; IFS=":"; r="$*"; IFS=; echo $# $r' '2 a : b'
-check 't="a xx b"; IFS="x"; set $t; IFS=":"; r="$*"; IFS=; echo $# $r' '3 a :: b'
+check 't="-- ";    IFS=" ";  set $t; IFS=":"; r="$*"; IFS=; echo $# $r' '0'
+check 't=" x";     IFS=" x"; set $t; IFS=":"; r="$*"; IFS=; echo $# $r' '1'
+check 't=" x ";    IFS=" x"; set $t; IFS=":"; r="$*"; IFS=; echo $# $r' '1'
+check 't=axb;      IFS="x";  set $t; IFS=":"; r="$*"; IFS=; echo $# $r' '2 a:b'
+check 't="a x b";  IFS="x";  set $t; IFS=":"; r="$*"; IFS=; echo $# $r' '2 a : b'
+check 't="a xx b"; IFS="x";  set $t; IFS=":"; r="$*"; IFS=; echo $# $r' '3 a :: b'
 check 't="a xx b"; IFS="x "; set $t; IFS=":"; r="$*"; IFS=; echo $# $r' '3 a::b'
 # A recent 'clarification' means that a single trailing IFS non-whitespace
 # doesn't generate an empty parameter
-check 't="xax"; IFS="x"; set $t; IFS=":"; r="$*"; IFS=; echo $# $r' '2 :a'
-check 'IFS="x"; set axb; IFS=":"; r="$*"; IFS=; echo $# $r' '1 axb'
+check 't="xax";  IFS="x";     set $t; IFS=":"; r="$*"; IFS=; echo $# $r' '2 :a'
+check 't="xax "; IFS="x ";   set $t; IFS=":"; r="$*"; IFS=; echo $# $r' '2 :a'
+# Verify that IFS isn't being applied where it shouldn't be.
+check 'IFS="x";             set axb; IFS=":"; r="$*"; IFS=; echo $# $r' '1 axb'
 
 # Check that we apply IFS to ${#var}
 long=12345678123456781234567812345678
 long=$long$long$long$long
 check 'echo ${#long}; IFS=2; echo ${#long}; set 1 ${#long};echo $#' '128 1 8 3'
-check 'IFS=2; set ${x-${#long}}; IFS=" "; echo $* $#' '1 8 2'
+check 'IFS=2; set ${x-${#long}};   IFS=" "; echo $* $#' '1 8 2'
 check 'IFS=2; set ${x-"${#long}"}; IFS=" "; echo $* $#' '128 1'
 
 exit $rval
