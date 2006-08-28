72c3baadfecca4e8a18bb8f4c5d96630416e0aa2
christos <christos@NetBSD.org>
2006-08-28 20:42:12 +0000

    Remove ad clause. Approved by board@

--- lib/libc/gen/vis.c
+++ lib/libc/gen/vis.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: vis.c,v 1.34 2005/11/18 08:32:46 martin Exp $	*/
+/*	$NetBSD: vis.c,v 1.35 2006/08/28 20:42:12 christos Exp $	*/
 
 /*-
  * Copyright (c) 1989, 1993
@@ -41,11 +41,7 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *        This product includes software developed by the NetBSD
- *        Foundation, Inc. and its contributors.
- * 4. Neither the name of The NetBSD Foundation nor the names of its
+ * 3. Neither the name of The NetBSD Foundation nor the names of its
  *    contributors may be used to endorse or promote products derived
  *    from this software without specific prior written permission.
  *
@@ -64,7 +60,7 @@
 
 #include <sys/cdefs.h>
 #if defined(LIBC_SCCS) && !defined(lint)
-__RCSID("$NetBSD: vis.c,v 1.34 2005/11/18 08:32:46 martin Exp $");
+__RCSID("$NetBSD: vis.c,v 1.35 2006/08/28 20:42:12 christos Exp $");
 #endif /* LIBC_SCCS and not lint */
 
 #include "namespace.h"
