851386cb166353b9536290daa8b8272650a86c86
christos <christos@NetBSD.org>
2006-08-30 15:22:45 +0000

    fix and comment initializers.

--- sys/compat/ibcs2/ibcs2_exec.c
+++ sys/compat/ibcs2/ibcs2_exec.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: ibcs2_exec.c,v 1.64 2005/12/11 12:20:02 christos Exp $	*/
+/*	$NetBSD: ibcs2_exec.c,v 1.65 2006/08/30 15:22:45 christos Exp $	*/
 
 /*
  * Copyright (c) 1994, 1995, 1998 Scott Bartram
@@ -35,7 +35,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: ibcs2_exec.c,v 1.64 2005/12/11 12:20:02 christos Exp $");
+__KERNEL_RCSID(0, "$NetBSD: ibcs2_exec.c,v 1.65 2006/08/30 15:22:45 christos Exp $");
 
 #if defined(_KERNEL_OPT)
 #include "opt_syscall_debug.h"
@@ -93,25 +93,27 @@ const struct emul emul_ibcs2 = {
 #endif
 	ibcs2_sendsig,
 	trapsignal,
-	NULL,
+	NULL,	/* e_tracesig */
 	ibcs2_sigcode,
 	ibcs2_esigcode,
 	&emul_ibcs2_object,
 	ibcs2_setregs,
 	ibcs2_e_proc_exec,
-	NULL,
-	NULL,
-	NULL,
-	NULL,
+	NULL,	/* e_proc_fork */
+	NULL,	/* e_proc_exit */
+	NULL,	/* e_lwp_fork */
+	NULL,	/* e_lwp_exec */
 #ifdef __HAVE_SYSCALL_INTERN
 	ibcs2_syscall_intern,
 #else
 	syscall,
 #endif
-	NULL,
-	NULL,
+	NULL,	/* e_sysctlovly */
+	NULL,	/* e_fault */
 
 	uvm_default_mapaddr,
+	NULL,	/* e_usertrap */
+	NULL,	/* e_sa */
 };
 
 /*
