0e8a1a6d8c9e854a9ddd7b3db40bd6c6f51a936e
mrg <mrg@NetBSD.org>
2006-09-01 09:21:18 +0000

    s/E2BIG/EFBIG/ for bus_dma(9) errors.  this is what every other bus_dma does and several drivers depend on it.  in particular, both re(4) and ath(4) would both spew "can't map mbuf" messages as rapidly as possible (spamming the 9600 bps console) and effectively locking up the interface until ifconfig "down up" cycle was run.  with this fix, i get a much, much slower spew of messages, and the interface (re(4)) continues to operate.

--- sys/arch/sparc64/dev/iommu.c
+++ sys/arch/sparc64/dev/iommu.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: iommu.c,v 1.79 2006/02/13 21:47:12 cdi Exp $	*/
+/*	$NetBSD: iommu.c,v 1.80 2006/09/01 09:21:18 mrg Exp $	*/
 
 /*
  * Copyright (c) 2001, 2002 Eduardo Horvath
@@ -34,7 +34,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: iommu.c,v 1.79 2006/02/13 21:47:12 cdi Exp $");
+__KERNEL_RCSID(0, "$NetBSD: iommu.c,v 1.80 2006/09/01 09:21:18 mrg Exp $");
 
 #include "opt_ddb.h"
 
@@ -501,7 +501,7 @@ iommu_dvmamap_load(bus_dma_tag_t t, struct strbuf_ctl *sb, bus_dmamap_t map,
 			map->_dm_dvmastart = 0;
 			map->_dm_dvmasize = 0;
 			splx(s);
-			return (E2BIG);
+			return (EFBIG);
 		}
 		sgstart += len;
 		map->dm_segs[seg].ds_addr = sgstart;
@@ -734,7 +734,7 @@ iommu_dvmamap_load_raw(bus_dma_tag_t t, struct strbuf_ctl *sb, bus_dmamap_t map,
 			} else {
 				if (j >= map->_dm_segcnt) {
 					iommu_dvmamap_unload(t, sb, map);
-					return (E2BIG);
+					return (EFBIG);
 				}
 				map->dm_segs[j].ds_addr = sgstart;
 				map->dm_segs[j].ds_len = left;
@@ -757,7 +757,7 @@ iommu_dvmamap_load_raw(bus_dma_tag_t t, struct strbuf_ctl *sb, bus_dmamap_t map,
 					(long)map->dm_segs[j].ds_len));
 				if (++j >= map->_dm_segcnt) {
 					iommu_dvmamap_unload(t, sb, map);
-					return (E2BIG);
+					return (EFBIG);
 				}
 				sgstart = roundup(sgstart, boundary);
 				map->dm_segs[j].ds_addr = sgstart;
@@ -831,7 +831,7 @@ iommu_dvmamap_load_raw(bus_dma_tag_t t, struct strbuf_ctl *sb, bus_dmamap_t map,
 			map->_dm_dvmastart = 0;
 			map->_dm_dvmasize = 0;
 			splx(s);
-			return (E2BIG);
+			return (EFBIG);
 		}
 		sgstart = roundup(sgstart, boundary);
 		map->dm_segs[i].ds_addr = sgstart;
