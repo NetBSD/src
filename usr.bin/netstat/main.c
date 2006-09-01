d50a4b4c395c389af91909ce14bdef3b3b20f1ec
tron <tron@NetBSD.org>
2006-09-01 14:39:36 +0000

    Pull up following revision(s) (requested by christos in ticket #77): 	usr.bin/netstat/main.c: revision 1.57 Fix another && || confusion, from Kurt Schreiner, thanks!

--- usr.bin/netstat/main.c
+++ usr.bin/netstat/main.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: main.c,v 1.55.2.1 2006/09/01 14:38:34 tron Exp $	*/
+/*	$NetBSD: main.c,v 1.55.2.2 2006/09/01 14:39:36 tron Exp $	*/
 
 /*
  * Copyright (c) 1983, 1988, 1993
@@ -39,7 +39,7 @@ __COPYRIGHT("@(#) Copyright (c) 1983, 1988, 1993\n\
 #if 0
 static char sccsid[] = "from: @(#)main.c	8.4 (Berkeley) 3/1/94";
 #else
-__RCSID("$NetBSD: main.c,v 1.55.2.1 2006/09/01 14:38:34 tron Exp $");
+__RCSID("$NetBSD: main.c,v 1.55.2.2 2006/09/01 14:39:36 tron Exp $");
 #endif
 #endif /* not lint */
 
@@ -547,7 +547,7 @@ main(argc, argv)
 	}
 #endif
 
-	if (!use_sysctl || (kvm_nlist(kvmd, nl) < 0 || nl[0].n_type == 0)) {
+	if (!use_sysctl && (kvm_nlist(kvmd, nl) < 0 || nl[0].n_type == 0)) {
 		if (nlistf)
 			errx(1, "%s: no namelist", nlistf);
 		else
