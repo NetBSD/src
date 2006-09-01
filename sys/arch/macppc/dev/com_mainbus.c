6b815123e608c932bbc48e1a735ebbea53aeed52
matt <matt@NetBSD.org>
2006-09-01 03:24:08 +0000

    Make this compile.  The "fixed" bus_space_tag_t is horrible.

--- sys/arch/macppc/dev/com_mainbus.c
+++ sys/arch/macppc/dev/com_mainbus.c
@@ -88,19 +88,23 @@ com_mainbus_attach(parent, self, aux)
     struct com_softc *sc = &msc->sc_com;
     int serial, interrupt_length;
     int interrupts[8];
+    bus_space_tag_t iot;
+    bus_space_handle_t ioh;
+    bus_addr_t iobase;
 
     serial = OF_finddevice("/ht@0/isa@4/serial@0x3f8");
     if (serial != -1) {
         interrupt_length =
             OF_getprop(serial, "interrupts", interrupts, sizeof(interrupts));
     }
-    comcnattach(0xf4000000, 0x3f8, 9600, 1843200, COM_TYPE_NORMAL,
-                (CREAD | CS8));
 
 
-    sc->sc_iobase = 0x3f8;
-    sc->sc_iot = 0xf4000000;
-    bus_space_map(sc->sc_iot, sc->sc_iobase, COM_NPORTS, 0, &sc->sc_ioh);
+    iot = (bus_space_tag_t)0xf4000000;
+    iobase = 0x3f8;
+    comcnattach(iot, iobase, 9600, 1843200, COM_TYPE_NORMAL, (CREAD | CS8));
+    bus_space_map(iot, iobase, COM_NPORTS, 0, &ioh);
+    COM_INIT_REGS(sc->sc_regs, iot, ioh, iobase);
+
     sc->sc_frequency = 1843200;
 
     com_attach_subr(sc);
