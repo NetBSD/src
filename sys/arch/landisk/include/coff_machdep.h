79d09833b993bc736ea64628a7978b6da5af5116
uwe <uwe>
2006-09-01 21:26:18 +0000

    Initial import of NetBSD/landisk port by NONAKA Kimihiro <nonaka@NetBSD.org>

---
+++ sys/arch/landisk/include/coff_machdep.h
@@ -0,0 +1,8 @@
+/*	$NetBSD: coff_machdep.h,v 1.1 2006/09/01 21:26:18 uwe Exp $	*/
+
+#ifndef _LANDISK_COFF_MACHDEP_H_
+#define _LANDISK_COFF_MACHDEP_H_
+
+#include <sh3/coff_machdep.h>
+
+#endif /* _LANDISK_COFF_MACHDEP_H_ */
---
+++ sys/arch/landisk/include/float.h
@@ -0,0 +1,8 @@
+/*	$NetBSD: float.h,v 1.1 2006/09/01 21:26:18 uwe Exp $	*/
+
+#ifndef	_LANDISK_FLOAT_H_
+#define	_LANDISK_FLOAT_H_
+
+#include <sh3/float.h>
+
+#endif	/* _LANDISK_FLOAT_H_ */
