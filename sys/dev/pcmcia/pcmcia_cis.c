18ad09609daee7269031e60c3208c62f2c9acab7
christos <christos@NetBSD.org>
2006-09-02 18:58:02 +0000

    - comment out impossible comparison - add missing initializer

--- sys/dev/pcmcia/pcmcia_cis.c
+++ sys/dev/pcmcia/pcmcia_cis.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: pcmcia_cis.c,v 1.46 2006/04/08 20:55:22 christos Exp $	*/
+/*	$NetBSD: pcmcia_cis.c,v 1.47 2006/09/02 18:58:02 christos Exp $	*/
 
 /*
  * Copyright (c) 1997 Marc Horowitz.  All rights reserved.
@@ -30,7 +30,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: pcmcia_cis.c,v 1.46 2006/04/08 20:55:22 christos Exp $");
+__KERNEL_RCSID(0, "$NetBSD: pcmcia_cis.c,v 1.47 2006/09/02 18:58:02 christos Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -305,7 +305,9 @@ pcmcia_scan_cis(dev, fct, arg)
 					 * distant regions
 					 */
 					if ((addr >= PCMCIA_CIS_SIZE) ||
+#if 0
 					    ((addr + length) < 0) ||
+#endif
 					    ((addr + length) >=
 					      PCMCIA_CIS_SIZE)) {
 						DPRINTF((" skipped, "
@@ -996,7 +998,7 @@ decode_config(struct pcmcia_tuple *tuple, struct cis_state *state)
 	/* most of these are educated guesses */
 	static const struct pcmcia_config_entry init_cfe = {
 		-1, PCMCIA_CFE_RDYBSY_ACTIVE | PCMCIA_CFE_WP_ACTIVE |
-		PCMCIA_CFE_BVD_ACTIVE, PCMCIA_IFTYPE_MEMORY,
+		PCMCIA_CFE_BVD_ACTIVE, PCMCIA_IFTYPE_MEMORY, 0,
 	};
 
 	if (tuple->length < 3) {
