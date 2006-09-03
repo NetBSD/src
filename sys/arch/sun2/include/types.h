6e10864bdda69746aa2ef924ed46be5aaf88145c
gdamore <gdamore@NetBSD.org>
2006-09-03 19:20:17 +0000

    Oops, forgot to commit __HAVE_GENERIC_TODR. :-)

--- sys/arch/sun2/include/types.h
+++ sys/arch/sun2/include/types.h
@@ -1,5 +1,6 @@
-/*	$NetBSD: types.h,v 1.4 2005/12/11 12:19:16 christos Exp $	*/
+/*	$NetBSD: types.h,v 1.5 2006/09/03 19:20:17 gdamore Exp $	*/
 
 #include <m68k/types.h>
 
 #define	__HAVE_GENERIC_SOFT_INTERRUPTS
+#define	__HAVE_GENERIC_TODR
