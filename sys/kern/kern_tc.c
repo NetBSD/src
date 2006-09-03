ccf1de8c947869bdba6ce1f5e1ba39df43ca00aa
christos <christos>
2006-09-03 06:40:39 +0000

    use c99 initializers, per gimpy's request

--- sys/kern/kern_tc.c
+++ sys/kern/kern_tc.c
@@ -1,4 +1,4 @@
-/* $NetBSD: kern_tc.c,v 1.9 2006/09/02 13:45:04 kardel Exp $ */
+/* $NetBSD: kern_tc.c,v 1.10 2006/09/03 06:40:39 christos Exp $ */
 
 /*-
  * ----------------------------------------------------------------------------
@@ -11,7 +11,7 @@
 
 #include <sys/cdefs.h>
 /* __FBSDID("$FreeBSD: src/sys/kern/kern_tc.c,v 1.166 2005/09/19 22:16:31 andre Exp $"); */
-__KERNEL_RCSID(0, "$NetBSD: kern_tc.c,v 1.9 2006/09/02 13:45:04 kardel Exp $");
+__KERNEL_RCSID(0, "$NetBSD: kern_tc.c,v 1.10 2006/09/03 06:40:39 christos Exp $");
 
 #include "opt_ntp.h"
 
@@ -69,25 +69,21 @@ struct timehands {
 };
 
 static struct timehands th0;
-static struct timehands th9 = { NULL, 0, 0, 0, {0, 0}, {0, 0}, {0, 0}, 0, &th0};
-static struct timehands th8 = { NULL, 0, 0, 0, {0, 0}, {0, 0}, {0, 0}, 0, &th9};
-static struct timehands th7 = { NULL, 0, 0, 0, {0, 0}, {0, 0}, {0, 0}, 0, &th8};
-static struct timehands th6 = { NULL, 0, 0, 0, {0, 0}, {0, 0}, {0, 0}, 0, &th7};
-static struct timehands th5 = { NULL, 0, 0, 0, {0, 0}, {0, 0}, {0, 0}, 0, &th6};
-static struct timehands th4 = { NULL, 0, 0, 0, {0, 0}, {0, 0}, {0, 0}, 0, &th5};
-static struct timehands th3 = { NULL, 0, 0, 0, {0, 0}, {0, 0}, {0, 0}, 0, &th4};
-static struct timehands th2 = { NULL, 0, 0, 0, {0, 0}, {0, 0}, {0, 0}, 0, &th3};
-static struct timehands th1 = { NULL, 0, 0, 0, {0, 0}, {0, 0}, {0, 0}, 0, &th2};
+static struct timehands th9 = { .th_next = &th0, };
+static struct timehands th8 = { .th_next = &th9, };
+static struct timehands th7 = { .th_next = &th8, };
+static struct timehands th6 = { .th_next = &th7, };
+static struct timehands th5 = { .th_next = &th6, };
+static struct timehands th4 = { .th_next = &th5, };
+static struct timehands th3 = { .th_next = &th4, };
+static struct timehands th2 = { .th_next = &th3, };
+static struct timehands th1 = { .th_next = &th2, };
 static struct timehands th0 = {
-	&dummy_timecounter,
-	0,
-	(uint64_t)-1 / 1000000,
-	0,
-	{1, 0},
-	{0, 0},
-	{0, 0},
-	1,
-	&th1
+	.th_counter = &dummy_timecounter,
+	.th_scale = (uint64_t)-1 / 1000000,
+	.th_offset = { .sec = 1, .frac = 0 },
+	.th_generation = 1,
+	.th_next = &th1,
 };
 
 static struct timehands *volatile timehands = &th0;
