eae90e83fbb20e4a113b292dd335a9d01068111c
christos <christos@NetBSD.org>
2006-09-03 06:23:27 +0000

    comment out unreachable code

--- sys/dev/scsipi/ss_scanjet.c
+++ sys/dev/scsipi/ss_scanjet.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: ss_scanjet.c,v 1.44 2005/12/11 12:23:51 christos Exp $	*/
+/*	$NetBSD: ss_scanjet.c,v 1.45 2006/09/03 06:23:27 christos Exp $	*/
 
 /*
  * Copyright (c) 1995 Kenneth Stailey.  All rights reserved.
@@ -34,7 +34,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: ss_scanjet.c,v 1.44 2005/12/11 12:23:51 christos Exp $");
+__KERNEL_RCSID(0, "$NetBSD: ss_scanjet.c,v 1.45 2006/09/03 06:23:27 christos Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -305,8 +305,10 @@ scanjet_read(struct ss_softc *ss, struct buf *bp)
 	/* with a scsipi_xfer preallocated, scsipi_command can't fail */
 	KASSERT(error == 0);
 	ss->sio.scan_window_size -= bp->b_bcount;
+#if 0
 	if (ss->sio.scan_window_size < 0)
 		ss->sio.scan_window_size = 0;
+#endif
 	return (0);
 }
 
