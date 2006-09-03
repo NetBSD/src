4066552e956baf8f5de6be3e71b1553b548df2e8
bjh21 <bjh21@NetBSD.org>
2006-09-03 11:51:29 +0000

    Use MI inittodr() and friends.

--- sys/arch/acorn26/include/types.h
+++ sys/arch/acorn26/include/types.h
@@ -1,4 +1,4 @@
-/* $NetBSD: types.h,v 1.6 2005/12/11 12:16:04 christos Exp $ */
+/* $NetBSD: types.h,v 1.7 2006/09/03 11:51:29 bjh21 Exp $ */
 
 #ifndef	_ACORN26_TYPES_H_
 #define	_ACORN26_TYPES_H_
@@ -7,5 +7,6 @@
 
 #define	__HAVE_GENERIC_SOFT_INTERRUPTS
 #define	__GENERIC_SOFT_INTERRUPTS_ALL_LEVELS
+#define __HAVE_GENERIC_TODR
 
 #endif	/* _ACORN26_TYPES_H_ */
