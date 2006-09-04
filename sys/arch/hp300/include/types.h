4d0fba3e3d2d007b5711d3733df6f7a9072f70a3
tsutsui <tsutsui>
2006-09-04 20:32:11 +0000

    Switch hp300 to MI todr.

--- sys/arch/hp300/include/types.h
+++ sys/arch/hp300/include/types.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: types.h,v 1.15 2005/12/06 17:06:00 tsutsui Exp $	*/
+/*	$NetBSD: types.h,v 1.16 2006/09/04 20:32:12 tsutsui Exp $	*/
 
 #ifndef _HP300_TYPES_H_
 #define	_HP300_TYPES_H_
@@ -7,6 +7,7 @@
 
 #define	__HAVE_DEVICE_REGISTER
 #define	__HAVE_GENERIC_SOFT_INTERRUPTS
+#define	__HAVE_GENERIC_TODR
 
 #if defined(_KERNEL)
 #define	__HAVE_RAS
