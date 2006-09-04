2f9f5c58466275b6184ca1dd57e6a75da7db0ee9
riz <riz@NetBSD.org>
2006-09-04 00:54:40 +0000

    Remove unused members of sk_softc.

--- sys/dev/pci/if_skvar.h
+++ sys/dev/pci/if_skvar.h
@@ -1,4 +1,4 @@
-/* $NetBSD: if_skvar.h,v 1.10 2006/03/28 18:29:42 riz Exp $ */
+/* $NetBSD: if_skvar.h,v 1.11 2006/09/04 00:54:40 riz Exp $ */
 
 /*-
  * Copyright (c) 2003 The NetBSD Foundation, Inc.
@@ -197,8 +197,6 @@ struct sk_softc {
 	bus_space_handle_t	sk_bhandle;	/* bus space handle */
 	bus_space_tag_t		sk_btag;	/* bus space tag */
 	void			*sk_intrhand;	/* irq handler handle */
-	struct resource		*sk_irq;	/* IRQ resource handle */
-	struct resource		*sk_res;	/* I/O or shared mem handle */
 	u_int8_t		sk_type;
 	u_int8_t		sk_rev;
   	const char		*sk_name;
