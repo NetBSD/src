087695a56bfd456ec2d10ad894ca31d20329a1f3
tsutsui <tsutsui@NetBSD.org>
2006-09-04 20:30:40 +0000

    Switch cobalt to MI todr.

--- sys/arch/cobalt/include/types.h
+++ sys/arch/cobalt/include/types.h
@@ -1,6 +1,7 @@
-/*	$NetBSD: types.h,v 1.5 2005/12/11 12:17:06 christos Exp $	*/
+/*	$NetBSD: types.h,v 1.6 2006/09/04 20:30:40 tsutsui Exp $	*/
 
 #include <mips/types.h>
 
 #define	__HAVE_DEVICE_REGISTER
 #define	__HAVE_GENERIC_SOFT_INTERRUPTS
+#define	__HAVE_GENERIC_TODR
