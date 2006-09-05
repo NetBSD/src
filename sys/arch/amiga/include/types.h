14c9ea00e06a10d4ec248a9b70f49618b5c0ca12
mhitch <mhitch@NetBSD.org>
2006-09-05 05:32:30 +0000

    Switch amiga to MI todr.

--- sys/arch/amiga/include/types.h
+++ sys/arch/amiga/include/types.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: types.h,v 1.17 2005/12/11 12:16:36 christos Exp $	*/
+/*	$NetBSD: types.h,v 1.18 2006/09/05 05:32:30 mhitch Exp $	*/
 
 #ifndef _MACHINE_TYPES_H_
 #define	_MACHINE_TYPES_H_
@@ -7,5 +7,6 @@
 
 #define	__HAVE_GENERIC_SOFT_INTERRUPTS
 #define	__GENERIC_SOFT_INTERRUPTS_ALL_LEVELS
+#define	__HAVE_GENERIC_TODR
 
 #endif
