5a18062bd5d2e429911e56479aefd8ec97f71211
mhitch <mhitch>
2006-09-05 05:32:30 +0000

    Switch amiga to MI todr.

--- sys/arch/amiga/dev/drbbc.c
+++ sys/arch/amiga/dev/drbbc.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: drbbc.c,v 1.14 2006/06/20 19:57:32 is Exp $ */
+/*	$NetBSD: drbbc.c,v 1.15 2006/09/05 05:32:30 mhitch Exp $ */
 
 /*-
  * Copyright (c) 1997 The NetBSD Foundation, Inc.
@@ -37,7 +37,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: drbbc.c,v 1.14 2006/06/20 19:57:32 is Exp $");
+__KERNEL_RCSID(0, "$NetBSD: drbbc.c,v 1.15 2006/09/05 05:32:30 mhitch Exp $");
 
 #include <sys/param.h>
 #include <sys/kernel.h>
@@ -56,6 +56,7 @@ __KERNEL_RCSID(0, "$NetBSD: drbbc.c,v 1.14 2006/06/20 19:57:32 is Exp $");
 #include <amiga/amiga/drcustom.h>
 #include <amiga/dev/rtc.h>
 
+#include <dev/clock_subr.h>
 #include <dev/ic/ds.h>
 
 int draco_ds_read_bit(void *);
@@ -65,11 +66,10 @@ void draco_ds_reset(void *);
 void drbbc_attach(struct device *, struct device *, void *);
 int drbbc_match(struct device *, struct cfdata *, void *);
 
-int dracougettod(struct timeval *);
-#ifdef __NOTYET__
-int dracousettod(struct timeval *);
-#endif
+int dracougettod(todr_chip_handle_t, volatile struct timeval *);
+int dracousettod(todr_chip_handle_t, volatile struct timeval *);
 
+static struct todr_chip_handle dracotodr;
 struct drbbc_softc {
 	struct device sc_dev;
 	struct ds_handle sc_dsh;
@@ -121,9 +121,11 @@ drbbc_attach(struct device *pdp, struct device *dp, void *auxp)
 		rombuf[3], rombuf[2], rombuf[1], rombuf[0],
 		hostid);
 
-	ugettod = dracougettod;
-	usettod = (void *)0;
 	drbbc_sc = sc;
+	dracotodr.cookie = sc;
+	dracotodr.todr_gettime = dracougettod;
+	dracotodr.todr_settime = dracousettod;
+	todr_attach(&dracotodr);
 }
 
 int
@@ -168,7 +170,7 @@ draco_ds_reset(void *p)
 }
 
 int
-dracougettod(struct timeval *tvp)
+dracougettod(todr_chip_handle_t h, volatile struct timeval *tvp)
 {
 	u_int32_t clkbuf;
 	u_int32_t usecs;
@@ -195,5 +197,11 @@ dracougettod(struct timeval *tvp)
 	tvp->tv_sec = clkbuf;
 	tvp->tv_usec = usecs;
 
-	return (1);
+	return (0);
+}
+
+int
+dracousettod(todr_chip_handle_t h, volatile struct timeval *tvp)
+{
+	return 1;
 }
