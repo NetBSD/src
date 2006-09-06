3053244401954ddfe6ad282e9b049d646a988c4a
garbled <garbled@NetBSD.org>
2006-09-06 22:32:56 +0000

    Add preliminary support for the 7025-F40.  The MPIC must not be wired up by the initial PCI_NETBSD_CONFIGURE, nor should it be manually wired by the indirect stuff.  This is all taken care of by the prep_init() function. 7025-F40 support supports the primary pci bus only.  The second PCI bus on the machine is still unsupported, but the machine runs and can access everything except for the two slots the other bus covers.

--- sys/arch/prep/pci/pci_machdep.c
+++ sys/arch/prep/pci/pci_machdep.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: pci_machdep.c,v 1.27 2006/06/09 01:19:11 garbled Exp $	*/
+/*	$NetBSD: pci_machdep.c,v 1.28 2006/09/06 22:32:56 garbled Exp $	*/
 
 /*
  * Copyright (c) 1996 Christopher G. Demetriou.  All rights reserved.
@@ -39,7 +39,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: pci_machdep.c,v 1.27 2006/06/09 01:19:11 garbled Exp $");
+__KERNEL_RCSID(0, "$NetBSD: pci_machdep.c,v 1.28 2006/09/06 22:32:56 garbled Exp $");
 
 #include <sys/types.h>
 #include <sys/param.h>
@@ -303,6 +303,10 @@ prep_pci_conf_hook(pci_chipset_tag_t pct, int bus, int dev, int func,
 	   alone */
 	if (PCI_VENDOR(id) == PCI_VENDOR_IBM &&
 	    PCI_PRODUCT(id) == PCI_PRODUCT_IBM_MPIC2)
+		return 0;
+
+	if (PCI_VENDOR(id) == PCI_VENDOR_IBM &&
+	    PCI_PRODUCT(id) == PCI_PRODUCT_IBM_MPIC)
 		return 0;
 
 	if (PCI_VENDOR(id) == PCI_VENDOR_INTEL &&
