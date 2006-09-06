a4a4ab6d8ad993f5e98bc16b03d5f69fede20662
gdamore <gdamore@NetBSD.org>
2006-09-06 04:51:34 +0000

    pdp10 should use generic todr and timecounters.  By using MI is one less thing that needs to be implemented, and the stubs that just panic() are removed.

--- sys/arch/pdp10/include/types.h
+++ sys/arch/pdp10/include/types.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: types.h,v 1.4 2005/12/24 20:07:24 perry Exp $	*/
+/*	$NetBSD: types.h,v 1.5 2006/09/06 04:51:34 gdamore Exp $	*/
 
 /*-
  * Copyright (c) 1990 The Regents of the University of California.
@@ -67,5 +67,7 @@ typedef volatile int		__cpu_simple_lock_t;
 #define __SIMPLELOCK_UNLOCKED	0
 
 /* #define __HAVE_GENERIC_SOFT_INTERRUPTS */
+#define	__HAVE_TIMECOUNTER	1
+#define	__HAVE_GENERIC_TODR	1
 
 #endif	/* _MACHTYPES_H_ */
