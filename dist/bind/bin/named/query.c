fb7641eceb75c2b7ce94c5b6d1ed145bfe7b6148
tron <tron@NetBSD.org>
2006-09-06 05:58:47 +0000

    Pull up following revision(s) (requested by adrianp in ticket #1498): 	dist/bind/lib/dns/resolver.c: revision 1.2 	dist/bind/bin/named/query.c: revision 1.3 Fixes for CVE-2006-4095 and CVE-2006-4096 from bind-9.3.2-P1 * Assertion failure in ISC BIND SIG query processing (CVE-2006-4095) - Recursive servers Queries for SIG records will trigger an assertion failure if more than one RRset is returned. However exposure can be minimized by restricting which sources can ask for recursion. - Authoritative servers If a nameserver is serving a RFC 2535 DNSSEC zone and is queried for the SIG records where there are multiple RRsets, then the named program will trigger an assertion failure when it tries to construct the response. * INSIST failure in ISC BIND recursive query handling code (CVE-2006-4096) It is possible to trigger an INSIST failure by sending enough recursive queries such that the response to the query arrives after all the clients waiting for the response have left the recursion queue. However exposure can be minimized by restricting which sources can ask for recursion. ok'ed christos@

--- dist/bind/bin/named/query.c
+++ dist/bind/bin/named/query.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: query.c,v 1.1.1.2.2.2 2006/07/13 22:13:27 tron Exp $	*/
+/*	$NetBSD: query.c,v 1.1.1.2.2.3 2006/09/06 05:58:47 tron Exp $	*/
 
 /*
  * Copyright (C) 2004, 2005  Internet Systems Consortium, Inc. ("ISC")
@@ -2395,7 +2395,7 @@ query_find(ns_client_t *client, dns_fetchevent_t *event, dns_rdatatype_t qtype)
 		authoritative = ISC_FALSE;
 
 		qtype = event->qtype;
-		if (qtype == dns_rdatatype_rrsig)
+		if (qtype == dns_rdatatype_rrsig || qtype == dns_rdatatype_sig)
 			type = dns_rdatatype_any;
 		else
 			type = qtype;
@@ -2436,7 +2436,7 @@ query_find(ns_client_t *client, dns_fetchevent_t *event, dns_rdatatype_t qtype)
 	/*
 	 * If it's a SIG query, we'll iterate the node.
 	 */
-	if (qtype == dns_rdatatype_rrsig)
+	if (qtype == dns_rdatatype_rrsig || qtype == dns_rdatatype_sig)
 		type = dns_rdatatype_any;
 	else
 		type = qtype;
