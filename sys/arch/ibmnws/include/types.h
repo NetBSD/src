8dbd5a8389a93b35a5b03cd3a4c7d63b0fdced4e
rjs <rjs@NetBSD.org>
2006-09-07 18:27:09 +0000

    Switch to MI todr. Mostly copied from prep.

--- sys/arch/ibmnws/include/types.h
+++ sys/arch/ibmnws/include/types.h
@@ -1,6 +1,7 @@
-/*	$NetBSD: types.h,v 1.5 2006/09/03 13:51:23 bjh21 Exp $	*/
+/*	$NetBSD: types.h,v 1.6 2006/09/07 18:27:09 rjs Exp $	*/
 
 #define __HAVE_GENERIC_SOFT_INTERRUPTS
 #define __HAVE_TIMECOUNTER
+#define __HAVE_GENERIC_TODR
 
 #include <powerpc/types.h>
