6065bdaa15f85c412604493e7c0aae4d9325dfb4
garbled <garbled@NetBSD.org>
2006-09-07 20:13:05 +0000

    Now that the interrupt bug on the 7043-140 is fixed, it also fixes the long standing issue with interrupts onthe PowerStack E1.  Remove the quirk entry for the PowerStack E1 and revert to using the IVR. Also, add a \n to the attachment of the mk clock printf that was missing.

--- sys/arch/prep/pnpbus/nvram_pnpbus.c
+++ sys/arch/prep/pnpbus/nvram_pnpbus.c
@@ -1,4 +1,4 @@
-/* $NetBSD: nvram_pnpbus.c,v 1.3 2006/06/15 18:15:32 garbled Exp $ */
+/* $NetBSD: nvram_pnpbus.c,v 1.4 2006/09/07 20:13:05 garbled Exp $ */
 
 /*-
  * Copyright (c) 2006 The NetBSD Foundation, Inc.
@@ -37,7 +37,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: nvram_pnpbus.c,v 1.3 2006/06/15 18:15:32 garbled Exp $");
+__KERNEL_RCSID(0, "$NetBSD: nvram_pnpbus.c,v 1.4 2006/09/07 20:13:05 garbled Exp $");
 
 #include <sys/types.h>
 #include <sys/param.h>
@@ -204,6 +204,7 @@ nvram_pnpbus_attach(struct device *parent, struct device *self, void *aux)
 
 	aprint_normal("%s: attaching clock", device_xname(self));
 	mk48txx_attach((struct mk48txx_softc *)&sc->sc_mksc);
+	aprint_normal("\n");
 	todr_attach(&sc->sc_mksc.sc_handle);
 }
 
