56340997e7d8bc9f0cc4c1a94f2c470df326750c
uwe <uwe@NetBSD.org>
2006-09-07 01:55:02 +0000

    Use MI rs5c313 driver attached as: rs5c313rtc0 at shb0 Mark landisk port as __HAVE_GENERIC_TODR.

--- sys/arch/landisk/include/types.h
+++ sys/arch/landisk/include/types.h
@@ -1,8 +1,10 @@
-/*	$NetBSD: types.h,v 1.1 2006/09/01 21:26:18 uwe Exp $	*/
+/*	$NetBSD: types.h,v 1.2 2006/09/07 01:55:03 uwe Exp $	*/
 
 #ifndef _LANDISK_TYPES_H_
 #define	_LANDISK_TYPES_H_
 
 #include <sh3/types.h>
+
+#define	__HAVE_GENERIC_TODR
 
 #endif /* _LANDISK_TYPES_H_ */
