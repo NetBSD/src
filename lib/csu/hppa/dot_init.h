296c01bb4f6b164a05c3169ef898255d9c18e1a4
skrll <skrll@NetBSD.org>
2006-09-08 19:18:26 +0000

    MD_DO_NOT_NEED_FALLTHRU for hppa

--- lib/csu/hppa/dot_init.h
+++ lib/csu/hppa/dot_init.h
@@ -1,4 +1,4 @@
-/* $NetBSD: dot_init.h,v 1.2 2005/05/21 06:46:34 skrll Exp $ */
+/* $NetBSD: dot_init.h,v 1.3 2006/09/08 19:18:26 skrll Exp $ */
 
 /*-
  * Copyright (c) 2004 Nick Hudson
@@ -37,20 +37,10 @@
 #include <sys/cdefs.h>			/* RCS ID & Copyright macro defns */
 #include <machine/asm.h>
 
-/*
- * These must be extern to avoid warnings ("declared static but never defined")
- * However, only the declaration is extern, the actually __asm() defines them
- * as static.
- */
-#define	INIT_FALLTHRU_DECL void init_fallthru(void)
-#define	FINI_FALLTHRU_DECL void fini_fallthru(void)
-
-#define	INIT_FALLTHRU()	init_fallthru()
-#define	FINI_FALLTHRU()	fini_fallthru()
-
 #define	MD_SECTION_PROLOGUE(sect, entry_pt)		\
 		__asm (					\
 		".section "#sect",\"ax\",@progbits	\n"\
+		".global "#entry_pt"			\n"\
 		".proc					\n"\
 		".callinfo frame=64,calls,save_rp,save_sp, entry_gr=4\n"\
 		".entry					\n"\
@@ -75,8 +65,10 @@
 		"	bv,n	%r0(%rp)		\n"\
 		".previous")
 
-#define	MD_INIT_SECTION_PROLOGUE MD_SECTION_PROLOGUE(.init, init_fallthru)
-#define	MD_FINI_SECTION_PROLOGUE MD_SECTION_PROLOGUE(.fini, fini_fallthru)
+#define	MD_INIT_SECTION_PROLOGUE MD_SECTION_PROLOGUE(.init, _init)
+#define	MD_FINI_SECTION_PROLOGUE MD_SECTION_PROLOGUE(.fini, _fini)
 
 #define	MD_INIT_SECTION_EPILOGUE MD_SECTION_EPILOGUE(.init)
 #define	MD_FINI_SECTION_EPILOGUE MD_SECTION_EPILOGUE(.fini)
+
+#define	MD_DO_NOT_NEED_FALLTHRU
