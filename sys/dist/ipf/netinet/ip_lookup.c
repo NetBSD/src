9fb273ba0f9a20f296c00073eed0894a836e679d
rpaulo <rpaulo>
2006-09-08 19:14:13 +0000

    Pull up following revision(s) (requested by christos in ticket #131): 	sys/dist/ipf/netinet/ip_lookup.c: revision 1.4 PR/34284: Gene ENonymous: when "ippool -F" is invoked, error is "ioctl(SIOCLOOKUPFLUSH): Bad address"

--- sys/dist/ipf/netinet/ip_lookup.c
+++ sys/dist/ipf/netinet/ip_lookup.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: ip_lookup.c,v 1.3 2006/04/04 16:17:19 martti Exp $	*/
+/*	$NetBSD: ip_lookup.c,v 1.3.6.1 2006/09/08 19:14:13 rpaulo Exp $	*/
 
 /*
  * Copyright (C) 2002-2003 by Darren Reed.
@@ -496,7 +496,7 @@ caddr_t data;
 
 	if (err == 0) {
 		flush.iplf_count = num;
-		err = COPYOUT(&flush, data, sizeof(flush));
+		BCOPYOUT(&flush, data, sizeof(flush));
 	}
 	return err;
 }
