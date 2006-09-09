8dca156c6527e190b80cac1b7705ad2c68c24db3
tsutsui <tsutsui@NetBSD.org>
2006-09-09 03:14:18 +0000

    Switch news68k to timecounters. From gdamore on port-news68k.

--- sys/arch/news68k/include/types.h
+++ sys/arch/news68k/include/types.h
@@ -1,5 +1,6 @@
-/*	$NetBSD: types.h,v 1.6 2006/09/04 20:32:57 tsutsui Exp $	*/
+/*	$NetBSD: types.h,v 1.7 2006/09/09 03:14:18 tsutsui Exp $	*/
 
 #include <m68k/types.h>
 
 #define	__HAVE_GENERIC_TODR
+#define	__HAVE_TIMECOUNTER
