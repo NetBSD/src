da8d6401ba4d6eac35893b9d3c0b73ac61eb3c0d
gdamore <gdamore>
2006-09-09 22:28:27 +0000

    Convet to MI todr.  Ok steve@.

--- sys/arch/mvme68k/include/types.h
+++ sys/arch/mvme68k/include/types.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: types.h,v 1.9 2002/02/28 03:17:34 simonb Exp $	*/
+/*	$NetBSD: types.h,v 1.10 2006/09/09 22:28:27 gdamore Exp $	*/
 
 #ifndef _MACHINE_TYPES_H_
 #define	_MACHINE_TYPES_H_
@@ -7,5 +7,6 @@
 
 #define	__HAVE_DEVICE_REGISTER
 #define	__HAVE_GENERIC_SOFT_INTERRUPTS
+#define	__HAVE_GENERIC_TODR
 
 #endif
