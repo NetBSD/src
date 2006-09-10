447deb85a2b20991cc693829c3622125ba7d173c
gdamore <gdamore>
2006-09-10 22:04:18 +0000

    First pass at enabling a lot of ARM ports to convert swiftly to __HAVE_GENERIC_TODR.  Just put #define __HAVE_GENERIC_TODR in types.h if your port uses one of these files.

--- sys/arch/arm/xscale/ixp425_timer.c
+++ sys/arch/arm/xscale/ixp425_timer.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: ixp425_timer.c,v 1.11 2006/04/10 03:36:03 simonb Exp $ */
+/*	$NetBSD: ixp425_timer.c,v 1.12 2006/09/10 22:04:18 gdamore Exp $ */
 
 /*
  * Copyright (c) 2003
@@ -34,7 +34,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: ixp425_timer.c,v 1.11 2006/04/10 03:36:03 simonb Exp $");
+__KERNEL_RCSID(0, "$NetBSD: ixp425_timer.c,v 1.12 2006/09/10 22:04:18 gdamore Exp $");
 
 #include "opt_ixp425.h"
 #include "opt_perfctrs.h"
@@ -290,6 +290,9 @@ delay(u_int n)
 	}
 }
 
+
+#ifndef __HAVE_GENERIC_TODR
+
 todr_chip_handle_t todr_handle;
 
 /*
@@ -376,6 +379,8 @@ resettodr(void)
 	    todr_settime(todr_handle, &time) != 0)
 		printf("resettodr: failed to set time\n");
 }
+
+#endif
 
 /*
  * ixpclk_intr:
