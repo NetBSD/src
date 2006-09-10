11f62072e758e4007fff2d78325bd0376fcbae14
plunky <plunky@NetBSD.org>
2006-09-10 15:45:55 +0000

    update to bluetooth device attachment:
    remove pseudo-device btdev(4) and inherent limitations
    
    add bthub(4) which autoconfigures at bluetooth controllers as they
    are enabled. bluetooth devices now attach here.
    
    btdevctl(8) and its cache is updated to handle new semantics
    
    etc/rc.d/btdevctl is updated to configure devices from a list
    in /etc/bluetooth/btdevctl.conf

--- sys/dev/bluetooth/btdev.h
+++ sys/dev/bluetooth/btdev.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: btdev.h,v 1.2 2006/07/26 10:31:00 tron Exp $	*/
+/*	$NetBSD: btdev.h,v 1.3 2006/09/10 15:45:56 plunky Exp $	*/
 
 /*-
  * Copyright (c) 2006 Itronix Inc.
@@ -37,5 +37,18 @@
 /* btdev attach/detach ioctl's */
 #define BTDEV_ATTACH		_IOW('b', 14, struct plistref)
 #define BTDEV_DETACH		_IOW('b', 15, struct plistref)
+
+/* btdev properties */
+#define BTDEVtype		"device-type"
+#define BTDEVladdr		"local-bdaddr"
+#define BTDEVraddr		"remote-bdaddr"
+
+#ifdef _KERNEL
+struct btdev {
+	struct device		sc_dev;
+	int			(*sc_identify)(struct btdev *, prop_dictionary_t);
+	LIST_ENTRY(btdev)	sc_next;
+};
+#endif /* _KERNEL */
 
 #endif /* _DEV_BLUETOOTH_BTDEV_H_ */
