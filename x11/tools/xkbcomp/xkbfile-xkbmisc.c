8fbb856a82abe121fdb075073f93a7f1a64a3bd2
dbj <dbj@NetBSD.org>
2006-09-11 22:13:51 +0000

    avoid objdir case conflicts for lib/xkbfile/xkbmisc.c and lib/X11/XKBMisc.c by creating xkbfile-xkbmisc.c which includes lib/xkbfile/xkbmisc.c

---
+++ x11/tools/xkbcomp/xkbfile-xkbmisc.c
@@ -0,0 +1,9 @@
+/*	$NetBSD: xkbfile-xkbmisc.c,v 1.1 2006/09/11 22:13:51 dbj Exp $	*/
+
+/* This file exists to differentiate between
+ * lib/xkbfile/xkbmisc.c and lib/X11/XKBMisc.c
+ * which causes objdir conflicts on case-insensitive filesystems
+ */
+
+#include <../../lib/xkbfile/xkbmisc.c>
+
