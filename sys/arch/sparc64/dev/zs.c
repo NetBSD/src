a9ed7d111b889e3d0c3092660f64db0942ac59c8
ghen <ghen>
2006-09-12 09:51:40 +0000

    Pull up following revision(s) (requested by martin in ticket #1507): 	sys/arch/sparc64/dev/zs.c: revision 1.61 	sys/arch/sparc64/dev/ffb.c: revision 1.28 Fix wrong prom_getoption() return value checks

--- sys/arch/sparc64/dev/zs.c
+++ sys/arch/sparc64/dev/zs.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: zs.c,v 1.54 2004/03/21 15:08:24 pk Exp $	*/
+/*	$NetBSD: zs.c,v 1.54.14.1 2006/09/12 09:51:40 ghen Exp $	*/
 
 /*-
  * Copyright (c) 1996 The NetBSD Foundation, Inc.
@@ -45,7 +45,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: zs.c,v 1.54 2004/03/21 15:08:24 pk Exp $");
+__KERNEL_RCSID(0, "$NetBSD: zs.c,v 1.54.14.1 2006/09/12 09:51:40 ghen Exp $");
 
 #include "opt_ddb.h"
 #include "opt_kgdb.h"
@@ -839,7 +839,7 @@ zs_console_flags(promunit, node, channel)
 	/* Default to channel 0 if there are no explicit prom args */
 	cookie = 0;
 	if (node == prom_instance_to_package(prom_stdin())) {
-		if (prom_getoption("input-device", buf, sizeof buf) != 0 &&
+		if (prom_getoption("input-device", buf, sizeof buf) == 0 &&
 		    strcmp("ttyb", buf) == 0)
 			cookie = 1;
 
@@ -848,7 +848,7 @@ zs_console_flags(promunit, node, channel)
 	}
 
 	if (node == prom_instance_to_package(prom_stdout())) { 
-		if (prom_getoption("output-device", buf, sizeof buf) != 0 &&
+		if (prom_getoption("output-device", buf, sizeof buf) == 0 &&
 		    strcmp("ttyb", buf) == 0)
 			cookie = 1;
 
