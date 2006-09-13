cf407be2923232af912a20c356bfc3a4a3092359
gdamore <gdamore@NetBSD.org>
2006-09-13 07:14:35 +0000

    Convert shark and ofppc to new common ofrtc and MI todr code.  Tested on shark.  Ok christos@, martin@.

--- sys/arch/ofppc/include/types.h
+++ sys/arch/ofppc/include/types.h
@@ -1,5 +1,6 @@
-/*	$NetBSD: types.h,v 1.5 2002/09/22 08:31:00 simonb Exp $	*/
+/*	$NetBSD: types.h,v 1.6 2006/09/13 07:14:35 gdamore Exp $	*/
 
 #include <powerpc/types.h>
 
 #define	__HAVE_DEVICE_REGISTER
+#define	__HAVE_GENERIC_TODR
