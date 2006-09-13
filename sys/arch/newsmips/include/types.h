a334fbc5c72f13f40c9dfc58927fa8cc69634a3c
tsutsui <tsutsui@NetBSD.org>
2006-09-13 14:50:42 +0000

    Switch newsmips to timecounters. From gdamore@ on port-newsmips with one tweak by me, and regressions passed on NWS-5000.

--- sys/arch/newsmips/include/types.h
+++ sys/arch/newsmips/include/types.h
@@ -1,9 +1,10 @@
-/*	$NetBSD: types.h,v 1.8 2006/09/04 20:33:24 tsutsui Exp $	*/
+/*	$NetBSD: types.h,v 1.9 2006/09/13 14:50:42 tsutsui Exp $	*/
 
 #include <mips/types.h>
 
 #define	__HAVE_GENERIC_SOFT_INTERRUPTS
 #define	__HAVE_GENERIC_TODR
+#define	__HAVE_TIMECOUNTER
 
 /* MIPS specific options */
 #define	__HAVE_BOOTINFO_H
