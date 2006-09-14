469ae2212c2f6a0032ed4b3ebb330c76fed18ec7
gdamore <gdamore>
2006-09-14 00:03:45 +0000

    Convert sbmips to generic TODR.  ok simonb@.

--- sys/arch/sbmips/include/types.h
+++ sys/arch/sbmips/include/types.h
@@ -1,6 +1,7 @@
-/* $NetBSD: types.h,v 1.2 2005/11/11 23:45:56 simonb Exp $ */
+/* $NetBSD: types.h,v 1.3 2006/09/14 00:03:45 gdamore Exp $ */
 
 #include <mips/types.h>
 
 #define	__HAVE_DEVICE_REGISTER
 #define	__HAVE_GENERIC_SOFT_INTERRUPTS
+#define	__HAVE_GENERIC_TODR
