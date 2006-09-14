ec37fcc696d6233f077d1cb23b4a62fcef551e6f
gdamore <gdamore@NetBSD.org>
2006-09-14 16:07:00 +0000

    Convert playstation2 to todr and tc.  This is not compile tested, as the current tree isn't buildable even by the cross tools (which I lack), due to C99.  Approved in principle by martin@, simon@

--- sys/arch/playstation2/include/types.h
+++ sys/arch/playstation2/include/types.h
@@ -1,8 +1,10 @@
-/*	$NetBSD: types.h,v 1.2 2002/01/02 12:38:46 uch Exp $	*/
+/*	$NetBSD: types.h,v 1.3 2006/09/14 16:07:00 gdamore Exp $	*/
 
 #include <mips/types.h>
 
 #define	__HAVE_GENERIC_SOFT_INTERRUPTS
+#define	__HAVE_GENERIC_TODR
+#define	__HAVE_TIMECOUNTER
 
 #if defined(_KERNEL) && defined(MIPS3_5900)
 typedef int int128_t __attribute__((__mode__(TI)));
