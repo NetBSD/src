d17ca6042b428abdcc3633cb9fbe3bb9ef60878b
gdamore <gdamore@NetBSD.org>
2006-09-15 16:03:14 +0000

    Convert sandpoint to GENERIC todr, including breaking of the clock handling out into its own driver.  (NB: mcclock should really, really be an MI driver to avoid this kind of pasteware, but it is "nontrivial" to cope with the fact that some machines have different ideas of mcclock.)

--- sys/arch/sandpoint/include/types.h
+++ sys/arch/sandpoint/include/types.h
@@ -1,3 +1,5 @@
-/*	$NetBSD: types.h,v 1.3 2006/09/03 13:51:23 bjh21 Exp $	*/
+/*	$NetBSD: types.h,v 1.4 2006/09/15 16:03:14 gdamore Exp $	*/
 
 #include <powerpc/types.h>
+
+#define	__HAVE_GENERIC_TODR
