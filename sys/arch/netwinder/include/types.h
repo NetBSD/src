b10a62fed0e50fa0cb61d8b51030e9474184f534
gdamore <gdamore@NetBSD.org>
2006-09-15 16:28:41 +0000

    Convert netwinder to generic TODR.  Committed after total lack of feedback (or even acknowledgement of receipt) from anyone on port-netwinder, so it is untested.  (Compiles though.)

--- sys/arch/netwinder/include/types.h
+++ sys/arch/netwinder/include/types.h
@@ -1,4 +1,4 @@
-/* $NetBSD: types.h,v 1.6 2006/09/11 15:18:23 gdamore Exp $ */
+/* $NetBSD: types.h,v 1.7 2006/09/15 16:28:41 gdamore Exp $ */
 
 #ifndef _ARM32_TYPES_H_
 #define	_ARM32_TYPES_H_
@@ -8,5 +8,6 @@
 #define __HAVE_GENERIC_SOFT_INTERRUPTS
 #define __HAVE_DEVICE_REGISTER
 #define	__HAVE_TIMECOUNTER
+#define	__HAVE_GENERIC_TODR 
 
 #endif
