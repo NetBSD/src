b566c32da1b9b9f1bc3c8a4a4ea5faf9bc644a73
gdamore <gdamore@NetBSD.org>
2006-09-15 08:25:02 +0000

    Convert bebox to MI todr.  As part of this, separate out the mc146818 clock handling.  Originally reviewed with that committed as an MI driver in dev/isa, crazy MD versions of mcclock made that impossible.  So for now I'm only handling it as an MD driver.  Ok garbled@

--- sys/arch/bebox/include/types.h
+++ sys/arch/bebox/include/types.h
@@ -1,3 +1,5 @@
-/*	$NetBSD: types.h,v 1.7 2006/09/03 13:51:23 bjh21 Exp $	*/
+/*	$NetBSD: types.h,v 1.8 2006/09/15 08:25:02 gdamore Exp $	*/
 
 #include <powerpc/types.h>
+
+#define	__HAVE_GENERIC_TODR
