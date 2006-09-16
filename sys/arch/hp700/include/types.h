a8dc515e02d0e2ca4a565ca1a1465cde5cf6a37f
skrll <skrll@NetBSD.org>
2006-09-16 15:43:24 +0000

    Timecounters for hp700 from Garrett. Tested by me.

--- sys/arch/hp700/include/types.h
+++ sys/arch/hp700/include/types.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: types.h,v 1.6 2006/09/15 07:51:17 skrll Exp $	*/
+/*	$NetBSD: types.h,v 1.7 2006/09/16 15:43:24 skrll Exp $	*/
 
 #ifndef	_HP700_TYPES_H_
 #define	_HP700_TYPES_H_
@@ -8,5 +8,6 @@
 #define	__HAVE_GENERIC_SOFT_INTERRUPTS
 #define	__HAVE_DEVICE_REGISTER
 #define	__HAVE_GENERIC_TODR
+#define	__HAVE_TIMECOUNTER
 
 #endif	/* _HP700_TYPES_H_ */
