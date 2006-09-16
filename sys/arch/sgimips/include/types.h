2723fe6ceda284ef5969b944db2cf8c383bbd0dc
gdamore <gdamore@NetBSD.org>
2006-09-16 08:50:27 +0000

    sgimips converted to timecounters.  Systems that can do them get mips_cp0 based logic.  We also use the common mips3_clock functions for those systems, removing some port-specific code.
    Tested by martin@.

--- sys/arch/sgimips/include/types.h
+++ sys/arch/sgimips/include/types.h
@@ -1,10 +1,11 @@
-/*	$NetBSD: types.h,v 1.10 2006/09/05 01:38:59 rumble Exp $	*/
+/*	$NetBSD: types.h,v 1.11 2006/09/16 08:50:27 gdamore Exp $	*/
 
 #include <mips/types.h>
 
 #define	__HAVE_DEVICE_REGISTER
 #define	__HAVE_GENERIC_SOFT_INTERRUPTS
 #define	__HAVE_GENERIC_TODR
+#define	__HAVE_TIMECOUNTER
 
 /* MIPS specific options */
 #define	__HAVE_BOOTINFO_H
