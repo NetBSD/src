665d59466be43639e2c924baaefce9c364503452
gdamore <gdamore@NetBSD.org>
2006-09-16 02:14:56 +0000

    Convert hpcmips to TODR and timecounters.  Ok jun@.

--- sys/arch/hpcmips/include/types.h
+++ sys/arch/hpcmips/include/types.h
@@ -1,6 +1,8 @@
-/*	$NetBSD: types.h,v 1.5 2002/03/23 03:14:54 shin Exp $	*/
+/*	$NetBSD: types.h,v 1.6 2006/09/16 02:14:56 gdamore Exp $	*/
 
 #include <mips/types.h>
 
 #define	__HAVE_GENERIC_SOFT_INTERRUPTS
 #define	__HAVE_OLD_DISKLABEL
+#define	__HAVE_GENERIC_TODR
+#define	__HAVE_TIMECOUNTER
