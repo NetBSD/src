e3fd78fcf75e9f03afd329c7a3c77aabfa8f441d
yamt <yamt@NetBSD.org>
2006-09-17 13:43:15 +0000

    defflag IPFILTER_LOOKUP.

--- sys/dist/ipf/netinet/ip_lookup.c
+++ sys/dist/ipf/netinet/ip_lookup.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: ip_lookup.c,v 1.4 2006/08/26 20:25:23 christos Exp $	*/
+/*	$NetBSD: ip_lookup.c,v 1.5 2006/09/17 13:43:15 yamt Exp $	*/
 
 /*
  * Copyright (C) 2002-2003 by Darren Reed.
@@ -15,6 +15,11 @@
 # define _PROTO_NET_H_
 #endif
 #include <sys/param.h>
+#if defined(__NetBSD__)
+# if (NetBSD >= 199905) && !defined(IPFILTER_LKM) && defined(_KERNEL)
+#  include "opt_ipfilter.h"
+# endif
+#endif
 #include <sys/errno.h>
 #include <sys/types.h>
 #include <sys/time.h>
