b1f5fac10fb42571ccac68a347b4600ba151eb58
christos <christos@NetBSD.org>
2006-09-17 14:49:46 +0000

    PR/34284: Gene ENonymous: Fix the userland copy of ip_lookup.c. Why do we have 2 copies?

--- dist/ipf/ip_lookup.c
+++ dist/ipf/ip_lookup.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: ip_lookup.c,v 1.1.1.3 2006/04/04 16:08:37 martti Exp $	*/
+/*	$NetBSD: ip_lookup.c,v 1.2 2006/09/17 14:49:46 christos Exp $	*/
 
 /*
  * Copyright (C) 2002-2003 by Darren Reed.
@@ -496,7 +496,7 @@ caddr_t data;
 
 	if (err == 0) {
 		flush.iplf_count = num;
-		err = COPYOUT(&flush, data, sizeof(flush));
+		BCOPYOUT(&flush, data, sizeof(flush));
 	}
 	return err;
 }
