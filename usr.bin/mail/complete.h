80690a79be162ba4ebcfb16bfd2e8b2fde44d9b0
christos <christos>
2006-09-18 19:48:44 +0000

    Jumbo mail patch from our anonymous user:
    1) Use editline [optional]:
       Most of this code was borrowed from src/usr.bin/ftp.  It does the
       appropriate editing, history, and completion for all mail commands
       (from cmdtab[]) and also does editing on header strings ('~h' inside
       the mail editor).

---
+++ usr.bin/mail/complete.h
@@ -0,0 +1,10 @@
+/* $Id: complete.h,v 1.1 2006/09/18 19:48:44 christos Exp $ */
+
+#ifndef _COMPLETE_H_
+#define _COMPLETE_H_
+
+char *rl_gets(const char *);
+char *rl_getline(const char *, char *);
+void init_readline (void);
+
+#endif /* _COMPLETE_H_ */
