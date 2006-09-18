fdcaebe666e748c6ad45d6e56c89b2b849c66d35
gdamore <gdamore@NetBSD.org>
2006-09-18 22:05:47 +0000

    Conversion of evbppc to generic TODR.  ok freza@

--- sys/arch/evbppc/include/types.h
+++ sys/arch/evbppc/include/types.h
@@ -1,6 +1,7 @@
-/*	$NetBSD: types.h,v 1.5 2006/09/03 13:51:23 bjh21 Exp $	*/
+/*	$NetBSD: types.h,v 1.6 2006/09/18 22:05:47 gdamore Exp $	*/
 
 #include <powerpc/types.h>
 
 #define	__HAVE_DEVICE_REGISTER
 #define	__HAVE_GENERIC_SOFT_INTERRUPTS
+#define	__HAVE_GENERIC_TODR
