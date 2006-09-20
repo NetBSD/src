57675076eb84bcedc95759cfcad4463f5eaef386
uwe <uwe@NetBSD.org>
2006-09-20 00:41:11 +0000

    Switch the rest of sh3 ports to todr(9) Split code to handle on-chip rtc into a separate driver Eliminate all todr code from clock.c Move __HAVE_GENERIC_TODR to sh3/include/types.h

--- sys/arch/landisk/include/types.h
+++ sys/arch/landisk/include/types.h
@@ -1,10 +1,8 @@
-/*	$NetBSD: types.h,v 1.2 2006/09/07 01:55:03 uwe Exp $	*/
+/*	$NetBSD: types.h,v 1.3 2006/09/20 00:41:13 uwe Exp $	*/
 
 #ifndef _LANDISK_TYPES_H_
 #define	_LANDISK_TYPES_H_
 
 #include <sh3/types.h>
-
-#define	__HAVE_GENERIC_TODR
 
 #endif /* _LANDISK_TYPES_H_ */
--- sys/arch/sh3/include/types.h
+++ sys/arch/sh3/include/types.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: types.h,v 1.22 2005/12/11 12:18:58 christos Exp $	*/
+/*	$NetBSD: types.h,v 1.23 2006/09/20 00:41:12 uwe Exp $	*/
 
 /*-
  * Copyright (c) 1990 The Regents of the University of California.
@@ -66,6 +66,7 @@ typedef	volatile unsigned char __cpu_simple_lock_t;
 
 #define	__HAVE_AST_PERPROC
 #define	__HAVE_GENERIC_SOFT_INTERRUPTS
+#define	__HAVE_GENERIC_TODR
 #define	__HAVE_SYSCALL_INTERN
 
 #if defined(_KERNEL)
