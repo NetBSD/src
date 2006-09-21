a91cfec7f4eeb1add3f1109f19bdb851c370a19a
yamt <yamt@NetBSD.org>
2006-09-21 14:30:50 +0000

    - implement splraiseipl for algor, arc, ews4800mips, hpcmips, and newsmips. - tweak evbmips version to match with others. XXX i think these ought to be merged.

--- sys/arch/algor/include/intr.h
+++ sys/arch/algor/include/intr.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: intr.h,v 1.5 2006/04/15 16:13:25 simonb Exp $	*/
+/*	$NetBSD: intr.h,v 1.5.10.1 2006/09/21 14:30:50 yamt Exp $	*/
 
 /*-
  * Copyright (c) 2000, 2001 The NetBSD Foundation, Inc.
@@ -53,9 +53,14 @@
 #define	IPL_BIO		5	/* disable block I/O interrupts */
 #define	IPL_NET		6	/* disable network interrupts */
 #define	IPL_TTY		7	/* disable terminal interrupts */
+#define	IPL_LPT		IPL_TTY
+#define	IPL_VM		IPL_TTY
 #define	IPL_SERIAL	7	/* disable serial interrupts */
 #define	IPL_CLOCK	8	/* disable clock interrupts */
+#define	IPL_STATCLOCK	IPL_CLOCK
+#define	IPL_SCHED	IPL_CLOCK
 #define	IPL_HIGH	8	/* disable all interrupts */
+#define	IPL_LOCK	IPL_HIGH
 
 #define	_IPL_NSOFT	4
 #define	_IPL_N		9
@@ -92,27 +97,33 @@ extern int		_splnone(int);
 extern int		_setsoftintr(int);
 extern int		_clrsoftintr(int);
 
-#define	splhigh()	_splraise(ipl_sr_bits[IPL_HIGH])
 #define	spl0()		(void) _spllower(0)
 #define	splx(s)		(void) _splset(s)
-#define	splbio()	_splraise(ipl_sr_bits[IPL_BIO])
-#define	splnet()	_splraise(ipl_sr_bits[IPL_NET])
-#define	spltty()	_splraise(ipl_sr_bits[IPL_TTY])
-#define	splserial()	_splraise(ipl_sr_bits[IPL_SERIAL])
-#define	splvm()		spltty()
-#define	splclock()	_splraise(ipl_sr_bits[IPL_CLOCK])
-#define	splstatclock()	splclock()
-
-#define	splsched()	splclock()
-#define	spllock()	splhigh()
-#define	spllpt()	spltty()
 
 #define	splsoft()	_splraise(ipl_sr_bits[IPL_SOFT])
-#define	splsoftclock()	_splraise(ipl_sr_bits[IPL_SOFTCLOCK])
-#define	splsoftnet()	_splraise(ipl_sr_bits[IPL_SOFTNET])
-#define	splsoftserial()	_splraise(ipl_sr_bits[IPL_SOFTSERIAL])
 
 #define	spllowersoftclock() _spllower(ipl_sr_bits[IPL_SOFTCLOCK])
+
+typedef int ipl_t;
+typedef struct {
+	ipl_t _sr;
+} ipl_cookie_t;
+
+static inline ipl_cookie_t
+makeiplcookie(ipl_t ipl)
+{
+
+	return (ipl_cookie_t){._sr = ipl_sr_bits[ipl]};
+}
+
+static inline int
+splraiseipl(ipl_cookie_t icookie)
+{
+
+	return _splraise(icookie._sr);
+}
+
+#include <sys/spl.h>
 
 struct algor_intrhand {
 	LIST_ENTRY(algor_intrhand) ih_q;
--- sys/arch/hpcmips/include/intr.h
+++ sys/arch/hpcmips/include/intr.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: intr.h,v 1.16 2002/05/15 15:19:54 uch Exp $	*/
+/*	$NetBSD: intr.h,v 1.16.58.1 2006/09/21 14:30:50 yamt Exp $	*/
 
 /*
  * Copyright (c) 1998 Jonathan Stone.  All rights reserved.
@@ -47,10 +47,14 @@
 #define	IPL_BIO		5	/* disable block I/O interrupts */
 #define	IPL_NET		6	/* disable network interrupts */
 #define	IPL_TTY		7	/* disable terminal interrupts */
+#define	IPL_LPT		IPL_TTY
+#define	IPL_VM		IPL_TTY
 #define	IPL_SERIAL	7	/* disable serial hardware interrupts */
 #define	IPL_CLOCK	8	/* disable clock interrupts */
-#define	IPL_STATCLOCK	8	/* disable profiling interrupts */
+#define	IPL_STATCLOCK	IPL_CLOCK
+#define	IPL_SCHED	IPL_CLOCK
 #define	IPL_HIGH	8	/* disable all interrupts */
+#define	IPL_LOCK	IPL_HIGH
 
 #define	_IPL_NSOFT	4
 #define	_IPL_N		9
@@ -91,27 +95,33 @@ void	_splnone(void);
 void	_setsoftintr(int);
 void	_clrsoftintr(int);
 
-#define	splhigh()	_splraise(ipl_sr_bits[IPL_HIGH])
 #define	spl0()		(void) _spllower(0)
 #define	splx(s)		(void) _splset(s)
-#define	splbio()	_splraise(ipl_sr_bits[IPL_BIO])
-#define	splnet()	_splraise(ipl_sr_bits[IPL_NET])
-#define	spltty()	_splraise(ipl_sr_bits[IPL_TTY])
-#define	splserial()	_splraise(ipl_sr_bits[IPL_SERIAL])
-#define	splvm()		spltty()
-#define	splclock()	_splraise(ipl_sr_bits[IPL_CLOCK])
-#define	splstatclock()	splclock()
-
-#define	splsched()	splclock()
-#define	spllock()	splhigh()
-#define	spllpt()	spltty()
 
 #define	splsoft()	_splraise(ipl_sr_bits[IPL_SOFT])
-#define	splsoftclock()	_splraise(ipl_sr_bits[IPL_SOFTCLOCK])
-#define	splsoftnet()	_splraise(ipl_sr_bits[IPL_SOFTNET])
-#define	splsoftserial()	_splraise(ipl_sr_bits[IPL_SOFTSERIAL])
 
 #define	spllowersoftclock() _spllower(ipl_sr_bits[IPL_SOFTCLOCK])
+
+typedef int ipl_t;
+typedef struct {
+	ipl_t _sr;
+} ipl_cookie_t;
+
+static inline ipl_cookie_t
+makeiplcookie(ipl_t ipl)
+{
+
+	return (ipl_cookie_t){._sr = ipl_sr_bits[ipl]};
+}
+
+static inline int
+splraiseipl(ipl_cookie_t icookie)
+{
+
+	return _splraise(icookie._sr);
+}
+
+#include <sys/spl.h>
 
 /*
  * software simulated interrupt
