f6782702b97161782769b9fc62df77bf6dd46103
yamt <yamt@NetBSD.org>
2006-09-21 12:57:12 +0000

    implement splraiseipl for hpcarm, shark, and acorn32.

--- sys/arch/hpcarm/hpcarm/intr.c
+++ sys/arch/hpcarm/hpcarm/intr.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: intr.c,v 1.10.22.2 2006/09/21 12:53:23 yamt Exp $	*/
+/*	$NetBSD: intr.c,v 1.10.22.3 2006/09/21 12:57:12 yamt Exp $	*/
 
 /*
  * Copyright (c) 1994-1998 Mark Brinicombe.
@@ -36,7 +36,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: intr.c,v 1.10.22.2 2006/09/21 12:53:23 yamt Exp $");
+__KERNEL_RCSID(0, "$NetBSD: intr.c,v 1.10.22.3 2006/09/21 12:57:12 yamt Exp $");
 
 #include "opt_irqstats.h"
 
@@ -148,7 +148,6 @@ dosoftints(void)
 /* This is interrupt / SPL related */
 
 void    set_spl_masks(void);
-int     ipl_to_spl(int);
 
 int current_spl_level = _SPL_HIGH;
 u_int spl_masks[_SPL_LEVELS + 1];
