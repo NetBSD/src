749d70419666e7fa94de653a240cc58107b1d2ff
kochi <kochi@NetBSD.org>
2006-09-22 13:38:32 +0000

    fixed typo in previous commit

--- sys/arch/ia64/stand/ia64/efi/main.c
+++ sys/arch/ia64/stand/ia64/efi/main.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: main.c,v 1.2 2006/09/21 09:59:45 kochi Exp $	*/
+/*	$NetBSD: main.c,v 1.3 2006/09/22 13:38:32 kochi Exp $	*/
 
 /*-
  * Copyright (c) 1998 Michael Smith <msmith@freebsd.org>
@@ -127,7 +127,7 @@ main(int argc, CHAR16 *argv[])
 	/* Get our loaded image protocol interface structure. */
 	BS->HandleProtocol(IH, &imgid, (VOID**)&img);
 
-	printf("Image base: 0lx%x\n", (u_long)img->ImageBase);
+	printf("Image base: 0x%lx\n", (u_long)img->ImageBase);
 
 	printf("\n");
 	printf("%s, Revision %s\n", bootprog_name, bootprog_rev);
