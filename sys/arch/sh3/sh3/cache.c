92663b5f89b700a05587e2f0c8faa71e23ff4f35
tsutsui <tsutsui@NetBSD.org>
2006-09-24 00:43:44 +0000

    Allow shared PV mappings in pmap_enter(9) even on SH4 if VAs of shared pages have the same virtual cache indexes (i.e. they won't cause virtual cache aliases). Also prepare PMAP_PREFER(9) for SH4 to avoid some possible virtual cache aliases. More stuff should be added in pmap_enter(9), but at least this changes should fix some existing problems. See discussion on port-sh3 ML for details.
    OK'ed by uwe and chs, and closes PR port-sh3/34243.

--- sys/arch/sh3/sh3/cache.c
+++ sys/arch/sh3/sh3/cache.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: cache.c,v 1.11 2006/01/02 23:37:34 uwe Exp $	*/
+/*	$NetBSD: cache.c,v 1.12 2006/09/24 00:43:44 tsutsui Exp $	*/
 
 /*-
  * Copyright (c) 2002 The NetBSD Foundation, Inc.
@@ -37,7 +37,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: cache.c,v 1.11 2006/01/02 23:37:34 uwe Exp $");
+__KERNEL_RCSID(0, "$NetBSD: cache.c,v 1.12 2006/09/24 00:43:44 tsutsui Exp $");
 
 #include "opt_memsize.h"	/* IOM_RAM_BEGIN */
 
@@ -77,6 +77,8 @@ int sh_cache_line_size;
 int sh_cache_ram_mode;
 int sh_cache_index_mode_icache;
 int sh_cache_index_mode_dcache;
+int sh_cache_alias_mask;
+int sh_cache_prefer_mask;
 
 void
 sh_cache_init()
