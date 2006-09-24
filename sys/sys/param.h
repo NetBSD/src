7d945dcf102c4f668e72d700d56d5f0961eb98dd
xtraeme <xtraeme@NetBSD.org>
2006-09-24 07:55:20 +0000

    Update __NetBSD_Version__ comment... it's 4.99.3. (hi dogcow!)

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.240 2006/09/24 06:52:38 dogcow Exp $	*/
+/*	$NetBSD: param.h,v 1.241 2006/09/24 07:55:20 xtraeme Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -63,7 +63,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	499000300	/* NetBSD 4.99.2 */
+#define	__NetBSD_Version__	499000300	/* NetBSD 4.99.3 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) >= __NetBSD_Version__)
