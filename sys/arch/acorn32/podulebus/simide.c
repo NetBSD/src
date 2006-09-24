2893c955bf4d06fe1fde11c7ed610854e21a237c
bjh21 <bjh21@NetBSD.org>
2006-09-24 23:14:58 +0000

    PR 23783: the correct number of elements to insert into cmd_iohs is WDC_NREG, not DRIVE_REGISTERS_SPACE.  The latter is much bigger and causes a buffer overrun and panic.  Patch supplied by Mike Pumford.

--- sys/arch/acorn32/podulebus/simide.c
+++ sys/arch/acorn32/podulebus/simide.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: simide.c,v 1.23 2006/01/16 20:30:18 bouyer Exp $	*/
+/*	$NetBSD: simide.c,v 1.24 2006/09/24 23:14:58 bjh21 Exp $	*/
 
 /*
  * Copyright (c) 1997-1998 Mark Brinicombe
@@ -40,7 +40,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: simide.c,v 1.23 2006/01/16 20:30:18 bouyer Exp $");
+__KERNEL_RCSID(0, "$NetBSD: simide.c,v 1.24 2006/09/24 23:14:58 bjh21 Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -268,7 +268,7 @@ simide_attach(parent, self, aux)
 		    simide_info[channel].drive_registers,
 		    DRIVE_REGISTERS_SPACE, 0, &wdr->cmd_baseioh)) 
 			continue;
-		for (i = 0; i < DRIVE_REGISTERS_SPACE; i++) {
+		for (i = 0; i < WDC_NREG; i++) {
 			if (bus_space_subregion(wdr->cmd_iot, wdr->cmd_baseioh,
 				i, i == 0 ? 4 : 1, &wdr->cmd_iohs[i]) != 0) {
 				bus_space_unmap(wdr->cmd_iot, wdr->cmd_baseioh,
