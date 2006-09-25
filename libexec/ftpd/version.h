8045f1a33a715df9c30c13152715207dae1b15aa
lukem <lukem@NetBSD.org>
2006-09-25 06:30:55 +0000

    Update to 20060923 for the following changes:
    20060201
    debug -> ftpd_debug
    xstrdup -> ftpd_strdup
    
    20060317
    * Make sure that "su" is initialized before dereferencing it.
      Fixes Coverity CID 1075.
    * Set file to NULL after calling fclose().
      Fixes Coverity CID 2669.
    * Remove unreachable code (res could never be NULL here).
      Fixes Coverity CID 712.
    
    20060509
    change (mostly) int to socklen_t.  GCC 4 doesn't like that int and
    socklen_t are different signness.
    
    20060923
    Apply patch from PR bin/33261 sent by FUKAOMI Naoki:
    "ftpd does not update wtmpx".

--- libexec/ftpd/version.h
+++ libexec/ftpd/version.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: version.h,v 1.64 2005/11/24 23:47:23 lukem Exp $	*/
+/*	$NetBSD: version.h,v 1.65 2006/09/25 06:30:55 lukem Exp $	*/
 /*-
  * Copyright (c) 1999-2004 The NetBSD Foundation, Inc.
  * All rights reserved.
@@ -36,5 +36,5 @@
  */
 
 #ifndef FTPD_VERSION
-#define	FTPD_VERSION	"NetBSD-ftpd 20051124"
+#define	FTPD_VERSION	"NetBSD-ftpd 20060923"
 #endif
