0df91ec13b6806a1d891c818f4e251be4371ef14
bjh21 <bjh21>
2006-10-01 21:34:30 +0000

    Don't explicitly specify no DMA support, since that doesn't work if the kernel doesn't support IDE DMA at all.

--- sys/dev/podulebus/dtide.c
+++ sys/dev/podulebus/dtide.c
@@ -1,4 +1,4 @@
-/* $NetBSD: dtide.c,v 1.22 2006/03/29 07:12:56 thorpej Exp $ */
+/* $NetBSD: dtide.c,v 1.23 2006/10/01 21:34:30 bjh21 Exp $ */
 
 /*-
  * Copyright (c) 2000, 2001 Ben Harris
@@ -31,7 +31,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: dtide.c,v 1.22 2006/03/29 07:12:56 thorpej Exp $");
+__KERNEL_RCSID(0, "$NetBSD: dtide.c,v 1.23 2006/10/01 21:34:30 bjh21 Exp $");
 
 #include <sys/param.h>
 
@@ -90,8 +90,6 @@ dtide_attach(struct device *parent, struct device *self, void *aux)
 
 	sc->sc_wdc.sc_atac.atac_cap = ATAC_CAP_DATA16 | ATAC_CAP_NOIRQ;
 	sc->sc_wdc.sc_atac.atac_pio_cap = 0; /* XXX correct? */
-	sc->sc_wdc.sc_atac.atac_dma_cap = 0; /* XXX correct? */
-	sc->sc_wdc.sc_atac.atac_udma_cap = 0;
 	sc->sc_wdc.sc_atac.atac_nchannels = DTIDE_NCHANNELS;
 	sc->sc_wdc.sc_atac.atac_channels = sc->sc_chp;
 	sc->sc_magict = pa->pa_fast_t;
