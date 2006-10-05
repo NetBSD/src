a04cc537d524f8e64984f5a2c2796ccd9adfe183
martin <martin@NetBSD.org>
2006-10-05 22:43:35 +0000

    I can not test this right now, but the equivalent change was needed on sparc64 to make root on raid shutdown cleanly: after using opendisk() and querying wedges, we need to VOP_CLOSE() the resulting vnode.

--- sys/arch/x86/x86/x86_autoconf.c
+++ sys/arch/x86/x86/x86_autoconf.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: x86_autoconf.c,v 1.22 2006/08/27 04:31:55 christos Exp $	*/
+/*	$NetBSD: x86_autoconf.c,v 1.23 2006/10/05 22:43:35 martin Exp $	*/
 
 /*-
  * Copyright (c) 1990 The Regents of the University of California.
@@ -123,6 +123,7 @@ handle_wedges(struct device *dv, int par)
 	wl.dkwl_buf = wi = malloc(wl.dkwl_bufsize, M_TEMP, M_WAITOK);
 
 	error = VOP_IOCTL(vn, DIOCLWEDGES, &wl, FREAD, NOCRED, 0);
+	VOP_CLOSE(vn, FREAD, NOCREAD, 0);
 	vput(vn);
 	if (error) {
 #ifdef DEBUG_WEDGE
