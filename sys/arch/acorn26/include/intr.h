5d4357b3872922d2f1e67c172c376a09b6ce137b
bjh21 <bjh21@NetBSD.org>
2006-10-07 20:51:47 +0000

    Make splsched() and splstatclock() into aliases for splhigh(), since practically no hardware interrupts can occur above splstatclock(). This also makes splsched() faster, which is good since it's called an awful lot.

--- sys/arch/acorn26/include/intr.h
+++ sys/arch/acorn26/include/intr.h
@@ -1,4 +1,4 @@
-/* $NetBSD: intr.h,v 1.6 2006/01/01 14:24:33 yamt Exp $ */
+/* $NetBSD: intr.h,v 1.7 2006/10/07 20:51:47 bjh21 Exp $ */
 /*-
  * Copyright (c) 1998, 2000 Ben Harris
  * All rights reserved.
@@ -53,9 +53,9 @@
 #define IPL_AUDIO	7
 #define IPL_SERIAL	8
 #define IPL_CLOCK	9
-#define IPL_STATCLOCK	10
-#define IPL_SCHED	11
-#define IPL_HIGH	12
+#define IPL_HIGH	10
+#define IPL_STATCLOCK	IPL_HIGH
+#define IPL_SCHED	IPL_HIGH
 #define	IPL_LOCK	IPL_HIGH
 #define NIPL		IPL_HIGH + 1
 
@@ -73,10 +73,9 @@
 #define	splaudio()	raisespl(IPL_AUDIO)
 #define splserial()	raisespl(IPL_SERIAL)
 #define splclock()	raisespl(IPL_CLOCK)
-#define splstatclock()	raisespl(IPL_STATCLOCK)
-#define splsched()	raisespl(IPL_SCHED)
 
-/* #define	splsched()	splhigh() */
+#define splstatclock()	splhigh()
+#define	splsched()	splhigh()
 #define spllock()	splhigh()
 
 #define	splraiseipl(x)	(((x) == IPL_HIGH) ? splhigh() : raisespl(x))
