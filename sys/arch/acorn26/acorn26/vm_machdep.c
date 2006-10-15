6f77fb56dd7a9587bc995efc005aed3530388eed
bouyer <bouyer@NetBSD.org>
2006-10-15 13:09:02 +0000

    Pull up following revision(s) (requested by bjh21 in ticket #10715): 	sys/arch/acorn26/acorn26/vm_machdep.c: revision 1.12 When forking a new LWP, if a new user stack pointer is provided, use it. Necessary to make regress/lib/libc/clone pass.

--- sys/arch/acorn26/acorn26/vm_machdep.c
+++ sys/arch/acorn26/acorn26/vm_machdep.c
@@ -1,4 +1,4 @@
-/* $NetBSD: vm_machdep.c,v 1.7 2004/01/04 11:33:29 jdolecek Exp $ */
+/* $NetBSD: vm_machdep.c,v 1.7.6.1 2006/10/15 13:09:02 bouyer Exp $ */
 
 /*-
  * Copyright (c) 2000, 2001 Ben Harris
@@ -64,7 +64,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: vm_machdep.c,v 1.7 2004/01/04 11:33:29 jdolecek Exp $");
+__KERNEL_RCSID(0, "$NetBSD: vm_machdep.c,v 1.7.6.1 2006/10/15 13:09:02 bouyer Exp $");
 
 #include <sys/param.h>
 #include <sys/buf.h>
@@ -128,6 +128,9 @@ cpu_lwp_fork(struct lwp *l1, struct lwp *l2, void *stack, size_t stacksize,
 		bzero(tf, sizeof(*tf));
 	else
 		*tf = *l1->l_addr->u_pcb.pcb_tf;
+	/* If specified, give the child a different stack. */
+	if (stack != NULL)
+		tf->tf_usr_sp = (u_int)stack + stacksize;
 	l2->l_addr->u_pcb.pcb_tf = tf;
 	/* Fabricate a new switchframe */
 	bzero(sf, sizeof(*sf));
