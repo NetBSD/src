792cd79a434ab6569573bf06329c984eb52165e5
apb <apb@NetBSD.org>
2006-10-16 19:44:23 +0000

    Wrap #ifndef __BIT_TYPES_DEFINED__ around #define __BIT_TYPES_DEFINED__. Reported by Narayana Prasad.

--- tools/compat/db.h
+++ tools/compat/db.h
@@ -1,5 +1,7 @@
-/*	$NetBSD: db.h,v 1.3 2003/10/27 00:12:43 lukem Exp $	*/
+/*	$NetBSD: db.h,v 1.4 2006/10/16 19:44:23 apb Exp $	*/
 
 #include "nbtool_config.h"
+#ifndef __BIT_TYPES_DEFINED__
 #define __BIT_TYPES_DEFINED__
+#endif
 #include "../../include/db.h"
