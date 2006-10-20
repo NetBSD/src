a683e1af1119d8b7617e98b22dfff1890e21f793
he <he>
2006-10-20 12:51:12 +0000

    The symbol to define to 1 if a pointer to an int fits in a long is INTPTR_IS_LONG, not INTPTR_IS_ULONG -- the latter is unused in other parts of lint's code.  This stops vax's lint from complaining about conversion of integer constants to 'unsigned long' in function argument lists, via a proper define of INT_RSIZE in common/inittyp.c. sh3 defined this to 0, so was actually not affected, but better to eradicate the typo there as well.

--- usr.bin/xlint/arch/sh3/targparam.h
+++ usr.bin/xlint/arch/sh3/targparam.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: targparam.h,v 1.3 2003/05/29 18:12:15 christos Exp $	*/
+/*	$NetBSD: targparam.h,v 1.4 2006/10/20 12:51:12 he Exp $	*/
 
 /*
  * Copyright (c) 1994, 1995 Jochen Pohl
@@ -45,7 +45,7 @@
 
 #define	PTRDIFF_IS_LONG		0
 #define	SIZEOF_IS_ULONG		0
-#define	INTPTR_IS_ULONG		0
+#define	INTPTR_IS_LONG		0
 
 #define	FLOAT_SIZE		(4 * CHAR_BIT)
 #define	DOUBLE_SIZE		(8 * CHAR_BIT)
