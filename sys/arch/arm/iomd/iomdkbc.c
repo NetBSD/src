e154a3fe8bb0dcdf6bffabdd65ec3dcdf0b5bc14
bjh21 <bjh21@NetBSD.org>
2006-10-21 22:45:03 +0000

    Interpret the argument to iomdkbc_set_poll() correctly.  Quite how we got this far with it the wrong way around is beyond me.

--- sys/arch/arm/iomd/iomdkbc.c
+++ sys/arch/arm/iomd/iomdkbc.c
@@ -1,4 +1,4 @@
-/* $NetBSD: iomdkbc.c,v 1.2 2005/12/11 12:16:47 christos Exp $ */
+/* $NetBSD: iomdkbc.c,v 1.3 2006/10/21 22:45:03 bjh21 Exp $ */
 
 /*-
  * Copyright (c) 2004 Ben Harris
@@ -28,7 +28,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: iomdkbc.c,v 1.2 2005/12/11 12:16:47 christos Exp $");
+__KERNEL_RCSID(0, "$NetBSD: iomdkbc.c,v 1.3 2006/10/21 22:45:03 bjh21 Exp $");
 
 #include <sys/param.h>
 #include <sys/device.h>
@@ -252,9 +252,9 @@ iomdkbc_set_poll(void *cookie, pckbport_slot_t slot, int on)
 	struct iomdkbc_internal *t = cookie;
 
 	if (on)
-		enable_irq(t->t_rxirq[slot]);
-	else
 		disable_irq(t->t_rxirq[slot]);
+	else
+		enable_irq(t->t_rxirq[slot]);
 }
 
 static int
