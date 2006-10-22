7ee28ec2aaeab13b049753d14a232b3e505695f7
mrg <mrg@NetBSD.org>
2006-10-22 20:01:41 +0000

    regenerate powerpc and sparc64 mknative files for GCC 4.1-20061021.

--- gnu/lib/libobjc4/arch/powerpc/defs.mk
+++ gnu/lib/libobjc4/arch/powerpc/defs.mk
@@ -2,7 +2,7 @@
 # Generated from: 	NetBSD: mknative-gcc,v 1.22 2006/06/25 03:06:15 mrg Exp 
 # Generated from: NetBSD: mknative.common,v 1.8 2006/05/26 19:17:21 mrg Exp 
 #
-G_ALL_OPT_FILES=${GNUHOSTDIST}/gcc/c.opt ${GNUHOSTDIST}/gcc/common.opt ${GNUHOSTDIST}/gcc/config/rs6000/rs6000.opt ${GNUHOSTDIST}/gcc/config/rs6000/sysv4.opt
+G_ALL_OPT_FILES=${GNUHOSTDIST}/gcc/c.opt ${GNUHOSTDIST}/gcc/common.opt ${GNUHOSTDIST}/gcc/config/rs6000/rs6000.opt ${GNUHOSTDIST}/gcc/config/rs6000/sysv4.opt ${GNUHOSTDIST}/gcc/config/rs6000/linux64.opt
 G_ALL_CFLAGS=-I. -I${GNUHOSTDIST}/libobjc   -O2   -W -Wall -Wwrite-strings -Wstrict-prototypes -DIN_GCC -DIN_TARGET_LIBS -fno-strict-aliasing -fexceptions
 G_INCLUDES=-I${GNUHOSTDIST}/libobjc/objc  -I${GNUHOSTDIST}/libobjc/../gcc -I${GNUHOSTDIST}/libobjc/../gcc/config -I../.././gcc -I${GNUHOSTDIST}/libobjc/../include
 G_OBJS=archive.lo class.lo encoding.lo gc.lo hash.lo init.lo linking.lo misc.lo nil_method.lo NXConstStr.lo Object.lo objects.lo Protocol.lo sarray.lo selector.lo sendmsg.lo thr.lo thr-objc.lo exception.lo hash_compat.lo
