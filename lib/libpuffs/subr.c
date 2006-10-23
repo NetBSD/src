96927a6ca72ad0be73b4bcb294b26dc9d45e6dbb
christos <christos@NetBSD.org>
2006-10-23 03:13:13 +0000

    put back the comparison in a lint friendly way to appease pooka.

--- lib/libpuffs/subr.c
+++ lib/libpuffs/subr.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: subr.c,v 1.4 2006/10/23 03:11:53 dogcow Exp $	*/
+/*	$NetBSD: subr.c,v 1.5 2006/10/23 03:13:13 christos Exp $	*/
 
 /*
  * Copyright (c) 2006 Antti Kantee.  All Rights Reserved.
@@ -30,7 +30,7 @@
 
 #include <sys/cdefs.h>
 #if !defined(lint)
-__RCSID("$NetBSD: subr.c,v 1.4 2006/10/23 03:11:53 dogcow Exp $");
+__RCSID("$NetBSD: subr.c,v 1.5 2006/10/23 03:13:13 christos Exp $");
 #endif /* !lint */
 
 #include <sys/types.h>
@@ -167,7 +167,7 @@ int
 puffs_vtype2dt(enum vtype vt)
 {
 
-	if (/* vt >= VNON && */ vt < (sizeof(vdmap)/sizeof(vdmap[0])))
+	if ((int)vt >= VNON && vt < (sizeof(vdmap)/sizeof(vdmap[0])))
 		return vdmap[vt];
 
 	return DT_UNKNOWN;
