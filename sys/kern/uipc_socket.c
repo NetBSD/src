744dd15844fc55120c335c2f364f5c6ffab92bc4
ghen <ghen>
2006-10-25 07:53:19 +0000

    Pull up following revision(s) (requested by ginsbach in ticket #1568): 	sys/kern/uipc_socket.c: revision 1.120 	lib/libc/sys/socket.2: revision 1.32 	lib/libc/sys/socket.2: revision 1.33 Sort ERRORS.  Bump date. Add EAFNOSUPPORT as a possible error if the address family is not supported.  This adds further differentiation between which argument to socket(2) caused the error.  No longer are invalid domain (address family) errors classified as ENOPROTOSUPPORT errors.  This should make socket(2) conform to current POSIX and X/Open standards.  Fixes PR/33676.

--- sys/kern/uipc_socket.c
+++ sys/kern/uipc_socket.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: uipc_socket.c,v 1.108.2.1 2005/10/22 03:54:33 riz Exp $	*/
+/*	$NetBSD: uipc_socket.c,v 1.108.2.2 2006/10/25 07:53:19 ghen Exp $	*/
 
 /*-
  * Copyright (c) 2002 The NetBSD Foundation, Inc.
@@ -68,7 +68,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: uipc_socket.c,v 1.108.2.1 2005/10/22 03:54:33 riz Exp $");
+__KERNEL_RCSID(0, "$NetBSD: uipc_socket.c,v 1.108.2.2 2006/10/25 07:53:19 ghen Exp $");
 
 #include "opt_sock_counters.h"
 #include "opt_sosend_loan.h"
@@ -461,7 +461,16 @@ socreate(int dom, struct socket **aso, int type, int proto, struct proc *p)
 		prp = pffindproto(dom, proto, type);
 	else
 		prp = pffindtype(dom, type);
-	if (prp == 0 || prp->pr_usrreq == 0)
+	if (prp == 0) {
+		/* no support for domain */
+		if (pffinddomain(dom) == 0)
+			return (EAFNOSUPPORT);
+		/* no support for socket type */
+		if (proto == 0 && type != 0)
+			return (EPROTOTYPE);
+		return (EPROTONOSUPPORT);
+	}
+	if (prp->pr_usrreq == 0)
 		return (EPROTONOSUPPORT);
 	if (prp->pr_type != type)
 		return (EPROTOTYPE);
