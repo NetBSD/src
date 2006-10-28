d73f6b99764baa5ffe424781224da703cd5080de
christos <christos@NetBSD.org>
2006-10-28 15:02:02 +0000

    Initialize decimal_point.

--- lib/libc/stdio/vfprintf.c
+++ lib/libc/stdio/vfprintf.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: vfprintf.c,v 1.52 2006/10/28 14:38:55 christos Exp $	*/
+/*	$NetBSD: vfprintf.c,v 1.53 2006/10/28 15:02:02 christos Exp $	*/
 
 /*-
  * Copyright (c) 1990 The Regents of the University of California.
@@ -37,7 +37,7 @@
 #if 0
 static char *sccsid = "@(#)vfprintf.c	5.50 (Berkeley) 12/16/92";
 #else
-__RCSID("$NetBSD: vfprintf.c,v 1.52 2006/10/28 14:38:55 christos Exp $");
+__RCSID("$NetBSD: vfprintf.c,v 1.53 2006/10/28 15:02:02 christos Exp $");
 #endif
 #endif /* LIBC_SCCS and not lint */
 
@@ -214,7 +214,7 @@ __vfprintf_unlocked(fp, fmt0, ap)
 	wchar_t wc;
 	mbstate_t ps;
 #ifndef NO_FLOATING_POINT
-	char *decimal_point;
+	char *decimal_point = NULL;
 	char softsign;		/* temporary negative sign for floats */
 	double _double;		/* double precision arguments %[eEfgG] */
 	int expt;		/* integer value of exponent */
