7c901ddefbe794695b8a38f38cb836b7b7e3739e
yamt <yamt@NetBSD.org>
2006-10-29 22:14:14 +0000

    make agr headers include lock.h and queue.h by themselves.

--- sys/net/agr/ieee8023ad_lacp_impl.h
+++ sys/net/agr/ieee8023ad_lacp_impl.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: ieee8023ad_lacp_impl.h,v 1.3 2005/12/10 23:21:39 elad Exp $	*/
+/*	$NetBSD: ieee8023ad_lacp_impl.h,v 1.4 2006/10/29 22:14:14 yamt Exp $	*/
 
 /*-
  * Copyright (c)2005 YAMAMOTO Takashi,
@@ -34,6 +34,8 @@
  *
  * implementation details.
  */
+
+#include <sys/queue.h>
 
 #define	LACP_TIMER_CURRENT_WHILE	0
 #define	LACP_TIMER_PERIODIC		1
