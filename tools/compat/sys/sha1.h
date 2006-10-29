9696c2d27f1bd4af78659f2d25ddbbaa3c7bdcbd
dogcow <dogcow>
2006-10-29 06:17:08 +0000

    cross (and native) building works again. woo.

--- tools/compat/sys/sha1.h
+++ tools/compat/sys/sha1.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: sha1.h,v 1.3 2003/10/27 00:12:43 lukem Exp $	*/
+/*	$NetBSD: sha1.h,v 1.1 2006/10/29 06:17:08 dogcow Exp $	*/
 
 /* We unconditionally use the NetBSD SHA1 in libnbcompat. */
 #include "nbtool_config.h"
