b9d37170184559c7411e476c95db21b807bbd641
christos <christos@NetBSD.org>
2006-10-30 20:22:53 +0000

    kill crypto/rmd160.h and crypto/sha2.h, and instead make symlinks to /usr/include from /usr/include/sys. This makes all the one way hash header handling identical.

--- usr.bin/cksum/sha384.c
+++ usr.bin/cksum/sha384.c
@@ -1,11 +1,11 @@
-/* $NetBSD: sha384.c,v 1.2 2005/08/25 09:51:23 tron Exp $ */
+/* $NetBSD: sha384.c,v 1.3 2006/10/30 20:22:54 christos Exp $ */
 
 #if HAVE_NBTOOL_CONFIG_H
 #include "nbtool_config.h"
 #endif
 
-#include <crypto/sha2.h>	/* this hash type */
-#include <md5.h>		/* the hash we're replacing */
+#include <sha2.h>	/* this hash type */
+#include <md5.h>	/* the hash we're replacing */
 
 #define HASHTYPE	"SHA384"
 #define HASHLEN		96
