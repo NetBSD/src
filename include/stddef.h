81697c5246e28ea719c5f8c38ef1454b835e6fba
tron <tron@NetBSD.org>
2006-11-01 18:08:11 +0000

    Pull up following revision(s) (requested by joerg in ticket #1473): 	include/stddef.h: revision 1.13 gcc-3.3 does not have __offsetof__, but gcc-3.4 eems to require it: From the redhat web page: http://www.redhat.com/docs/manuals/enterprise/RHEL-4-Manual/gcc/offsetof.html __offsetof__ (expression)     is equivalent to the parenthesized expression, except that the     expression is considered an integral constant expression even if     it contains certain operators that are not normally permitted in     an integral constant expression. Users should never use __offsetof__     directly; the only valid use of __offsetof__ is to implement the     offsetof macro in <stddef.h>.

--- include/stddef.h
+++ include/stddef.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: stddef.h,v 1.10.6.2 2006/11/01 18:07:35 tron Exp $	*/
+/*	$NetBSD: stddef.h,v 1.10.6.3 2006/11/01 18:08:11 tron Exp $	*/
 
 /*-
  * Copyright (c) 1990, 1993
@@ -55,8 +55,11 @@ typedef	_BSD_WCHAR_T_	wchar_t;
 #elif !defined(__cplusplus)
 #define	offsetof(type, member)	((size_t)(unsigned long)(&((type *)0)->member))
 #else
-#define	offsetof(type, member) (reinterpret_cast<size_t> \
-    (&reinterpret_cast<const volatile char &>(static_cast<type *>(0)->member)))
+#ifndef __offsetof__
+#define __offsetof__(a) a
+#endif
+#define	offsetof(type, member) __offsetof__((reinterpret_cast<size_t> \
+    (&reinterpret_cast<const volatile char &>(static_cast<type *>(0)->member))))
 #endif  
  
 #endif /* _STDDEF_H_ */
