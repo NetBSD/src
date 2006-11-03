cdffaf8ab0ecc6d1271c04c30c30517723dc8d0a
joerg <joerg>
2006-11-03 09:17:24 +0000

    If a plist contains @owner or @group entries and first rename failed, pushout was called with NULL argument. This has the semantic of using a working directory of "/", even though already read plist entries are relative to prefix. The calls to chown and chgrp therefore end up with the wrong arguments for the current directory. To compensate for this, don't pushout the first time. As plists are not supposed to have multiple @cwd entries anymore, this could most likely be simplified, but that is outside the scope of this commit.
    Reviewed by agc@

--- usr.sbin/pkg_install/add/extract.c
+++ usr.sbin/pkg_install/add/extract.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: extract.c,v 1.38 2006/05/11 23:50:15 mrg Exp $	*/
+/*	$NetBSD: extract.c,v 1.39 2006/11/03 09:17:24 joerg Exp $	*/
 
 #if HAVE_CONFIG_H
 #include "config.h"
@@ -14,7 +14,7 @@
 #if 0
 static const char *rcsid = "FreeBSD - Id: extract.c,v 1.17 1997/10/08 07:45:35 charnier Exp";
 #else
-__RCSID("$NetBSD: extract.c,v 1.38 2006/05/11 23:50:15 mrg Exp $");
+__RCSID("$NetBSD: extract.c,v 1.39 2006/11/03 09:17:24 joerg Exp $");
 #endif
 #endif
 
@@ -243,7 +243,8 @@ extract_plist(char *home, package_t *pkg)
 				} else {
 					/* rename failed, try copying with a big tar command */
 					if (last_chdir != Directory) {
-						pushout(last_chdir);
+						if (last_chdir != NULL)
+							pushout(last_chdir);
 						last_chdir = Directory;
 					} else if (p->name[0] == '/') {
 						pushout(Directory);
