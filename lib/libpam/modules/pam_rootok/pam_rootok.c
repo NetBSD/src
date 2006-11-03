7edfbd7822e70e94951f9b0a6c50d12d4ffb4ba2
christos <christos>
2006-11-03 18:03:23 +0000

    don't include syslog.h if you are not going to use it.

--- lib/libpam/modules/pam_rootok/pam_rootok.c
+++ lib/libpam/modules/pam_rootok/pam_rootok.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: pam_rootok.c,v 1.2 2004/12/12 08:18:47 christos Exp $	*/
+/*	$NetBSD: pam_rootok.c,v 1.3 2006/11/03 18:03:23 christos Exp $	*/
 
 /*-
  * Copyright (c) 2001 Mark R V Murray
@@ -40,13 +40,12 @@
 #ifdef __FreeBSD__
 __FBSDID("$FreeBSD: src/lib/libpam/modules/pam_rootok/pam_rootok.c,v 1.8 2002/04/12 22:27:23 des Exp $");
 #else
-__RCSID("$NetBSD: pam_rootok.c,v 1.2 2004/12/12 08:18:47 christos Exp $");
+__RCSID("$NetBSD: pam_rootok.c,v 1.3 2006/11/03 18:03:23 christos Exp $");
 #endif
 
 #define _BSD_SOURCE
 
 #include <unistd.h>
-#include <syslog.h>
 
 #define PAM_SM_AUTH
 
