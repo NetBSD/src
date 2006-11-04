fd6ca2d9a12306135db927253f4ba74156f31091
yamt <yamt@NetBSD.org>
2006-11-04 07:58:08 +0000

    add IPL_STATCLOCK, IPL_SCHED, IPL_LOCK.

--- sys/arch/acorn32/include/intr.h
+++ sys/arch/acorn32/include/intr.h
@@ -1,4 +1,4 @@
-/* 	$NetBSD: intr.h,v 1.4 2003/06/16 20:00:56 thorpej Exp $	*/
+/* 	$NetBSD: intr.h,v 1.4.50.1 2006/11/04 07:58:08 yamt Exp $	*/
 
 /*
  * Copyright (c) 1997 Mark Brinicombe.
@@ -46,11 +46,14 @@
 #define IPL_VM		3	/* memory allocation */
 #define IPL_AUDIO	4	/* audio */
 #define IPL_CLOCK	5	/* clock */
-#define IPL_HIGH	6	/*  */
-#define IPL_SERIAL	7	/* serial */
-#define IPL_NONE	8
+#define	IPL_STATCLOCK	6
+#define IPL_HIGH	7	/*  */
+#define	IPL_SCHED	IPL_HIGH
+#define	IPL_LOCK	IPL_HIGH
+#define IPL_SERIAL	8	/* serial */
+#define IPL_NONE	9
 
-#define IPL_LEVELS	9
+#define IPL_LEVELS	10
 
 #define	IST_UNUSABLE	-1	/* interrupt cannot be used */
 #define	IST_NONE	0	/* none (dummy) */
