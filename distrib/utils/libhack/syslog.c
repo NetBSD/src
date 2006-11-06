660075c5de9c12b276bb035328601df9689a2b75
he <he@NetBSD.org>
2006-11-06 17:33:56 +0000

    Implement mostly-stub versions of the new syslog functions ({v,}syslog_{r,ss}) and the corresponding openlog_r/setlogmask_r/closelog_r functions.
    This should allow sparc64 to build its ramdiskbin again.
    
    Reviewed by christos@

--- distrib/utils/libhack/syslog.c
+++ distrib/utils/libhack/syslog.c
@@ -39,3 +39,56 @@ vsyslog(int fac, const char *fmt, va_list ap)
 	(void)fprintf(stderr, "\n");
 	fflush(stderr);
 }
+
+__strong_alias(_syslog_ss, syslog_ss)
+void
+syslog_ss(int priority, struct syslog_data *data, const char *fmt, ...)
+{
+	va_list ap;
+	va_start(ap, fmt);
+	vsyslog(priority, fmt, ap);
+	va_end(ap);
+}
+
+__strong_alias(_vsyslog_ss, vsyslog_ss)
+void
+vsyslog_ss(int priority, struct syslog_data *data, const char *fmt, va_list ap)
+{
+	vsyslog(priority, fmt, ap);
+}
+
+__strong_alias(_syslog_r, syslog_r)
+void
+syslog_r(int priority, struct syslog_data *data, const char *fmt, ...)
+{
+	va_list ap;
+	va_start(ap, fmt);
+	vsyslog(priority, fmt, ap);
+	va_end(ap);
+}
+
+__strong_alias(_vsyslog_r, vsyslog_r)
+void
+vsyslog_r(int priority, struct syslog_data *data, const char *fmt, va_list ap)
+{
+	vsyslog(priority, fmt, ap);
+}
+
+__strong_alias(_closelog_r, closelog_r)
+void
+closelog_r(struct syslog_data *data)
+{
+}
+
+__strong_alias(_setlogmask_r, setlogmask_r)
+int
+setlogmask_r(int maskpri, struct syslog_data *data)
+{
+	return 0xff;
+}
+
+__strong_alias(_openlog_r, openlog_r)
+void
+openlog_r(const char *id, int logopt, int facility, struct syslog_data *data)
+{
+}
