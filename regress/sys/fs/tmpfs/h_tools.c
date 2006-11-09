e03f885a19653cadae4f14fb70e9fbbf5e6ebe47
jmmv <jmmv>
2006-11-09 13:51:49 +0000

    Add several checks to see if tmpfs raises the proper kqueue events during operation.  These all match the behavior exposed by MFS (except for a corner case that is described in t_link).
    Fixes to tmpfs itself to make these tests pass will come soon.

--- regress/sys/fs/tmpfs/h_tools.c
+++ regress/sys/fs/tmpfs/h_tools.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: h_tools.c,v 1.5 2006/07/31 16:34:42 martin Exp $	*/
+/*	$NetBSD: h_tools.c,v 1.6 2006/11/09 13:51:49 jmmv Exp $	*/
 
 /*
  * Copyright (c) 2005 The NetBSD Foundation, Inc.
@@ -45,12 +45,17 @@
 
 #include <sys/param.h>
 #include <sys/types.h>
+#include <sys/event.h>
 #include <sys/mount.h>
 #include <sys/statvfs.h>
 #include <sys/socket.h>
+#include <sys/time.h>
 #include <sys/un.h>
 
+#include <assert.h>
+#include <err.h>
 #include <errno.h>
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -59,6 +64,7 @@
 /* --------------------------------------------------------------------- */
 
 static int getfh_main(int, char **);
+static int kqueue_main(int, char **);
 static int rename_main(int, char **);
 static int sockets_main(int, char **);
 static int statvfs_main(int, char **);
@@ -111,6 +117,94 @@ getfh_main(int argc, char **argv)
 	free(fh);
 
 	return 0;
+}
+
+/* --------------------------------------------------------------------- */
+
+int
+kqueue_main(int argc, char **argv)
+{
+	char *line;
+	int i, kq;
+	size_t len;
+	struct kevent *changes, event;
+
+	if (argc < 2)
+		return EXIT_FAILURE;
+
+	argc--;
+	argv++;
+
+	changes = malloc(sizeof(struct kevent) * (argc - 1));
+	if (changes == NULL)
+		errx(EXIT_FAILURE, "not enough memory");
+
+	for (i = 0; i < argc; i++) {
+		int fd;
+
+		fd = open(argv[i], O_RDONLY);
+		if (fd == -1)
+			err(EXIT_FAILURE, "cannot open %s", argv[i]);
+
+		EV_SET(&changes[i], fd, EVFILT_VNODE,
+		    EV_ADD | EV_ENABLE | EV_ONESHOT,
+		    NOTE_ATTRIB | NOTE_DELETE | NOTE_EXTEND | NOTE_LINK |
+		    NOTE_RENAME | NOTE_REVOKE | NOTE_WRITE,
+		    0, 0);
+	}
+
+	kq = kqueue();
+	if (kq == -1)
+		err(EXIT_FAILURE, "kqueue");
+
+	while ((line = fgetln(stdin, &len)) != NULL) {
+		int ec, nev;
+		struct timespec to;
+
+		to.tv_sec = 0;
+		to.tv_nsec = 100000;
+
+		(void)kevent(kq, changes, argc, &event, 1, &to);
+
+		assert(len > 0);
+		assert(line[len - 1] == '\n');
+		line[len - 1] = '\0';
+		ec = system(line);
+		if (ec != EXIT_SUCCESS)
+			errx(ec, "%s returned %d", line, ec);
+
+		do {
+			nev = kevent(kq, changes, argc, &event, 1, &to);
+			if (nev == -1)
+				err(EXIT_FAILURE, "kevent");
+			else if (nev > 0) {
+				for (i = 0; i < argc; i++)
+					if (event.ident == changes[i].ident)
+						break;
+
+				if (event.fflags & NOTE_ATTRIB)
+					printf("%s - NOTE_ATTRIB\n", argv[i]);
+				if (event.fflags & NOTE_DELETE)
+					printf("%s - NOTE_DELETE\n", argv[i]);
+				if (event.fflags & NOTE_EXTEND)
+					printf("%s - NOTE_EXTEND\n", argv[i]);
+				if (event.fflags & NOTE_LINK)
+					printf("%s - NOTE_LINK\n", argv[i]);
+				if (event.fflags & NOTE_RENAME)
+					printf("%s - NOTE_RENAME\n", argv[i]);
+				if (event.fflags & NOTE_REVOKE)
+					printf("%s - NOTE_REVOKE\n", argv[i]);
+				if (event.fflags & NOTE_WRITE)
+					printf("%s - NOTE_WRITE\n", argv[i]);
+			}
+		} while (nev > 0);
+	}
+
+	for (i = 0; i < argc; i++)
+		close(changes[i].ident);
+	free(changes);
+
+	return EXIT_SUCCESS;
 }
 
 /* --------------------------------------------------------------------- */
@@ -196,6 +290,8 @@ main(int argc, char **argv)
 
 	if (strcmp(argv[0], "getfh") == 0)
 		error = getfh_main(argc, argv);
+	else if (strcmp(argv[0], "kqueue") == 0)
+		error = kqueue_main(argc, argv);
 	else if (strcmp(argv[0], "rename") == 0)
 		error = rename_main(argc, argv);
 	else if (strcmp(argv[0], "sockets") == 0)
