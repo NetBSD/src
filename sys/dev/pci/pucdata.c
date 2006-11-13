5b3f17e412a7a9f99e4715e1d961e612b0baf875
bouyer <bouyer@NetBSD.org>
2006-11-13 20:11:10 +0000

    Add NetMos 9855 Quad UART and 1284 Printer port

--- sys/dev/pci/pucdata.c
+++ sys/dev/pci/pucdata.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: pucdata.c,v 1.48 2006/10/31 23:04:50 manu Exp $	*/
+/*	$NetBSD: pucdata.c,v 1.49 2006/11/13 20:11:10 bouyer Exp $	*/
 
 /*
  * Copyright (c) 1998, 1999 Christopher G. Demetriou.  All rights reserved.
@@ -36,7 +36,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: pucdata.c,v 1.48 2006/10/31 23:04:50 manu Exp $");
+__KERNEL_RCSID(0, "$NetBSD: pucdata.c,v 1.49 2006/11/13 20:11:10 bouyer Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -1007,6 +1007,19 @@ const struct puc_device_description puc_devices[] = {
 		{ PUC_PORT_TYPE_COM, 0x18, 0x00, COM_FREQ },
 		{ PUC_PORT_TYPE_COM, 0x1c, 0x00, COM_FREQ },
 		{ PUC_PORT_TYPE_LPT, 0x20, 0x00, 0x00 },
+	    },
+	},
+
+       /* NetMos 4S1P PCI NM9855 : 4S, 1P */
+       {   "NetMos NM9855 Quad UART and 1284 Printer port (unknown type)",
+	    {	0x9710,	0x9855,	0x1000,	0x0014	},
+	    {	0xffff,	0xffff,	0xffff,	0xffff	},
+	    {
+		{ PUC_PORT_TYPE_LPT, 0x10, 0x00, 0x00 },
+		{ PUC_PORT_TYPE_COM, 0x18, 0x00, COM_FREQ },
+		{ PUC_PORT_TYPE_COM, 0x1c, 0x00, COM_FREQ },
+		{ PUC_PORT_TYPE_COM, 0x20, 0x00, COM_FREQ },
+		{ PUC_PORT_TYPE_COM, 0x24, 0x00, COM_FREQ },
 	    },
 	},
 
