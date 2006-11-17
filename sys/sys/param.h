62e8c57ce0c496c00d0471fc173bedf4282dc670
hannken <hannken>
2006-11-17 17:05:18 +0000

    Add specificdata support to mount points.
    Welcome to NetBSD 4.99.4
    
    Approved by: Jason Thorpe <thorpej@netbsd.org>

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.243 2006/10/04 14:23:28 christos Exp $	*/
+/*	$NetBSD: param.h,v 1.244 2006/11/17 17:05:18 hannken Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -63,7 +63,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	499000300	/* NetBSD 4.99.3 */
+#define	__NetBSD_Version__	499000400	/* NetBSD 4.99.4 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
