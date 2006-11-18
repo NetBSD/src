501e2947f0bafb751dd5ab120f17b0777f985983
tsutsui <tsutsui@NetBSD.org>
2006-11-18 16:03:53 +0000

    Unwrap some lines (after __unused removal).

--- sys/dev/cardbus/if_re_cardbus.c
+++ sys/dev/cardbus/if_re_cardbus.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: if_re_cardbus.c,v 1.11 2006/11/16 01:32:48 christos Exp $	*/
+/*	$NetBSD: if_re_cardbus.c,v 1.12 2006/11/18 16:03:53 tsutsui Exp $	*/
 
 /*
  * Copyright (c) 2004 Jonathan Stone
@@ -36,7 +36,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: if_re_cardbus.c,v 1.11 2006/11/16 01:32:48 christos Exp $");
+__KERNEL_RCSID(0, "$NetBSD: if_re_cardbus.c,v 1.12 2006/11/18 16:03:53 tsutsui Exp $");
 
 #include "opt_inet.h"
 #include "bpfilter.h"
@@ -150,8 +150,7 @@ re_cardbus_lookup(const struct cardbus_attach_args *ca)
 }
 
 int
-re_cardbus_match(struct device *parent,
-	struct cfdata *match, void *aux)
+re_cardbus_match(struct device *parent, struct cfdata *match, void *aux)
 {
 	struct cardbus_attach_args *ca = aux;
 
@@ -163,8 +162,7 @@ re_cardbus_match(struct device *parent,
 
 
 void
-re_cardbus_attach(struct device *parent, struct device *self,
-	void *aux)
+re_cardbus_attach(struct device *parent, struct device *self, void *aux)
 {
 	struct re_cardbus_softc *csc = device_private(self);
 	struct rtk_softc *sc = &csc->sc_rtk;
@@ -399,7 +397,7 @@ re_cardbus_disable(struct rtk_softc *sc)
 }
 
 void
-re_cardbus_power(struct rtk_softc *sc,	int why)
+re_cardbus_power(struct rtk_softc *sc, int why)
 {
 	struct re_cardbus_softc *csc = (void *) sc;
 
