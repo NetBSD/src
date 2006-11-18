0c64d31fcf7c0c359921e1c8e49e3fb3a1848dd6
tsutsui <tsutsui@NetBSD.org>
2006-11-18 00:01:14 +0000

    Regen from pcidevs rev 1.851: > Add and note more Realtek Ethernet chips.

--- sys/dev/pci/pcidevs.h
+++ sys/dev/pci/pcidevs.h
@@ -1,10 +1,10 @@
-/*	$NetBSD: pcidevs.h,v 1.849 2006/11/13 22:03:20 xtraeme Exp $	*/
+/*	$NetBSD: pcidevs.h,v 1.850 2006/11/18 00:01:14 tsutsui Exp $	*/
 
 /*
  * THIS FILE AUTOMATICALLY GENERATED.  DO NOT EDIT.
  *
  * generated from:
- *	NetBSD: pcidevs,v 1.850 2006/11/13 22:02:58 xtraeme Exp
+ *	NetBSD: pcidevs,v 1.851 2006/11/17 23:57:27 tsutsui Exp
  */
 
 /*
@@ -2960,11 +2960,13 @@
 /* Realtek (Creative Labs?) products */
 #define	PCI_PRODUCT_REALTEK_RT8029	0x8029		/* 8029 Ethernet */
 #define	PCI_PRODUCT_REALTEK_RT8129	0x8129		/* 8129 10/100 Ethernet */
+#define	PCI_PRODUCT_REALTEK_RT8101E	0x8136		/* 8101E 10/100 Ethernet */
 #define	PCI_PRODUCT_REALTEK_RT8138	0x8138		/* 8138 10/100 Ethernet */
 #define	PCI_PRODUCT_REALTEK_RT8139B	0x8138		/* 8139B 10/100 Ethernet */
 #define	PCI_PRODUCT_REALTEK_RT8139	0x8139		/* 8139 10/100 Ethernet */
-#define	PCI_PRODUCT_REALTEK_RT8168	0x8168		/* 8168 10/100/1000 Ethernet */
-#define	PCI_PRODUCT_REALTEK_RT8169	0x8169		/* 8169 10/100/1000 Ethernet */
+#define	PCI_PRODUCT_REALTEK_RT8167	0x8167		/* 8169SC/8110SC 10/100/1000 Ethernet */
+#define	PCI_PRODUCT_REALTEK_RT8168	0x8168		/* 8168B/8111B 10/100/1000 Ethernet */
+#define	PCI_PRODUCT_REALTEK_RT8169	0x8169		/* 8169/S/SB 10/100/1000 Ethernet */
 #define	PCI_PRODUCT_REALTEK_RT8180	0x8180		/* 8180 802.11b */
 
 /* RICOH products */
