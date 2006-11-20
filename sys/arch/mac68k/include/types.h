4143abdf368deda70a15f247e3be5eba2d18ff41
hauke <hauke@NetBSD.org>
2006-11-20 19:58:38 +0000

    Add Timecounter support for mac68k, using the otherwise idle second timer of VIA1. Passes regression/sys/kern/time regression test.
    While we are here, GC leftover prototypes from MI todr integration,
    and clean up clockreg.h in the light of "Guide to the Macintosh
    Hardware" information.
    
    (Reviewed and okayed by Martin Husemann)

--- sys/arch/mac68k/include/types.h
+++ sys/arch/mac68k/include/types.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: types.h,v 1.15 2006/09/20 03:40:51 gdamore Exp $	*/
+/*	$NetBSD: types.h,v 1.16 2006/11/20 19:58:38 hauke Exp $	*/
 
 #ifndef _TYPES_MACHINE_
 #define	_TYPES_MACHINE_
@@ -6,5 +6,6 @@
 #include <m68k/types.h>
 
 #define	__HAVE_GENERIC_TODR
+#define	__HAVE_TIMECOUNTER
 
 #endif /* _TYPES_MACHINE_ */
