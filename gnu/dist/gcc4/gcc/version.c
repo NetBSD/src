aa94b7e3579595abf638c798b7fc873ae54da197
mrg <mrg@NetBSD.org>
2006-11-23 05:43:40 +0000

    bump version to NetBSD nb2 20061121

--- gnu/dist/gcc4/gcc/version.c
+++ gnu/dist/gcc4/gcc/version.c
@@ -8,7 +8,7 @@
    in parentheses.  You may also wish to include a number indicating
    the revision of your modified compiler.  */
 
-#define VERSUFFIX " (NetBSD nb1 20061021)"
+#define VERSUFFIX " (NetBSD nb2 20061121)"
 
 /* This is the location of the online document giving instructions for
    reporting bugs.  If you distribute a modified version of GCC,
