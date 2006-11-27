eb98f53decdd53193a1b6cd5db4dcdf5a48e690c
drochner <drochner@NetBSD.org>
2006-11-27 19:58:20 +0000

    match ICH-7M / -DH (The watchdog doesn't reset the box for me, but it is present according to the datasheet.)

--- sys/arch/i386/pci/ichlpcib.c
+++ sys/arch/i386/pci/ichlpcib.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: ichlpcib.c,v 1.18 2006/11/16 01:32:39 christos Exp $	*/
+/*	$NetBSD: ichlpcib.c,v 1.19 2006/11/27 19:58:20 drochner Exp $	*/
 
 /*-
  * Copyright (c) 2004 The NetBSD Foundation, Inc.
@@ -46,7 +46,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: ichlpcib.c,v 1.18 2006/11/16 01:32:39 christos Exp $");
+__KERNEL_RCSID(0, "$NetBSD: ichlpcib.c,v 1.19 2006/11/27 19:58:20 drochner Exp $");
 
 #include <sys/types.h>
 #include <sys/param.h>
@@ -132,6 +132,8 @@ lpcibmatch(struct device *parent, struct cfdata *match,
 		case PCI_PRODUCT_INTEL_82801FB_LPC:	/* ICH6 */
 		case PCI_PRODUCT_INTEL_82801FBM_LPC:	/* ICH6-M */
 		case PCI_PRODUCT_INTEL_82801G_LPC:	/* ICH7 */
+		case PCI_PRODUCT_INTEL_82801GBM_LPC:	/* ICH7-M */
+		case PCI_PRODUCT_INTEL_82801GHM_LPC:	/* ICH7-M DH */
 			return 10;	/* prior to pcib */
 		}
 	}
@@ -247,7 +249,7 @@ tcotimer_configure(struct lpcib_softc *sc, struct pci_attach_args *pa)
 		       "TCO timer disabled\n", sc->sc_dev.dv_xname);
 		return;
 	}
-	
+
 	/* Explicitly stop the TCO timer. */
 	ioreg = bus_space_read_2(sc->sc_iot, sc->sc_ioh, LPCIB_TCO1_CNT);
 	if ((ioreg & LPCIB_TCO1_CNT_TCO_TMR_HLT) == 0)
