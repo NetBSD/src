a9054160c320e545c686c73951b0ed68c8e15a21
is <is@NetBSD.org>
2006-12-10 14:45:09 +0000

    was wrong magic constant. no functional change.

--- sys/net/if_tokensubr.c
+++ sys/net/if_tokensubr.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: if_tokensubr.c,v 1.40 2006/12/10 14:39:03 is Exp $	*/
+/*	$NetBSD: if_tokensubr.c,v 1.41 2006/12/10 14:45:09 is Exp $	*/
 
 /*
  * Copyright (c) 1982, 1989, 1993
@@ -99,7 +99,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: if_tokensubr.c,v 1.40 2006/12/10 14:39:03 is Exp $");
+__KERNEL_RCSID(0, "$NetBSD: if_tokensubr.c,v 1.41 2006/12/10 14:45:09 is Exp $");
 
 #include "opt_inet.h"
 #include "opt_atalk.h"
@@ -584,7 +584,7 @@ token_input(struct ifnet *ifp, struct mbuf *m)
 
 		case LLC_XID:
 		case LLC_XID_P:
-			if(m->m_len < ISO88025_ADDR_LEN)
+			if(m->m_len < LLC_XID_BASIC_MINLEN)
 				goto dropanyway;
 			l->llc_window = 0;
 			l->llc_fid = 9;
