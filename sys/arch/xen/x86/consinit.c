8988af60535f4777ab3235e6f4e2c39f772e8754
tron <tron>
2006-12-11 13:24:54 +0000

    Pull up following revision(s) (requested by bouyer in ticket #1600): 	sys/arch/xen/x86/consinit.c: revision 1.7 Remove extra ) causing compile failure when CONS_OVERRIDE is defined. From Hideo Masuda in PR port-xen/35217.

--- sys/arch/xen/x86/consinit.c
+++ sys/arch/xen/x86/consinit.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: consinit.c,v 1.4.2.1 2005/06/28 10:29:17 tron Exp $	*/
+/*	$NetBSD: consinit.c,v 1.4.2.1.4.1 2006/12/11 13:24:54 tron Exp $	*/
 /*	NetBSD: consinit.c,v 1.4 2004/03/13 17:31:34 bjh21 Exp 	*/
 
 /*
@@ -28,7 +28,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: consinit.c,v 1.4.2.1 2005/06/28 10:29:17 tron Exp $");
+__KERNEL_RCSID(0, "$NetBSD: consinit.c,v 1.4.2.1.4.1 2006/12/11 13:24:54 tron Exp $");
 
 #include "opt_kgdb.h"
 
@@ -168,7 +168,7 @@ consinit()
 	if (xen_start_info.flags & SIF_PRIVILEGED) {
 #ifdef CONS_OVERRIDE
 		if (strcmp(default_consinfo.devname, "tty0") == 0 ||
-		    strcmp(default_consinfo.devname, "pc") == 0)) {
+		    strcmp(default_consinfo.devname, "pc") == 0) {
 #else
 		if (strcmp(xcp.xcp_console, "tty0") == 0 || /* linux name */
 		    strcmp(xcp.xcp_console, "pc") == 0) { /* NetBSD name */
