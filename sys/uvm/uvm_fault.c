abd837912907a11dfb358dd32503ff93477bce07
yamt <yamt@NetBSD.org>
2006-12-15 13:51:30 +0000

    put ->K loaned pages on the page queue, so that page loaning doesn't disturb pagedaemon/pdpolicy.

--- sys/uvm/uvm_fault.c
+++ sys/uvm/uvm_fault.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: uvm_fault.c,v 1.116 2006/12/01 19:15:22 yamt Exp $	*/
+/*	$NetBSD: uvm_fault.c,v 1.117 2006/12/15 13:51:30 yamt Exp $	*/
 
 /*
  *
@@ -39,7 +39,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: uvm_fault.c,v 1.116 2006/12/01 19:15:22 yamt Exp $");
+__KERNEL_RCSID(0, "$NetBSD: uvm_fault.c,v 1.117 2006/12/15 13:51:30 yamt Exp $");
 
 #include "opt_uvmhist.h"
 
@@ -200,7 +200,7 @@ uvmfault_anonflush(struct vm_anon **anons, int n)
 			continue;
 		simple_lock(&anons[lcv]->an_lock);
 		pg = anons[lcv]->an_page;
-		if (pg && (pg->flags & PG_BUSY) == 0 && pg->loan_count == 0) {
+		if (pg && (pg->flags & PG_BUSY) == 0) {
 			uvm_lock_pageq();
 			if (pg->wire_count == 0) {
 				pmap_clear_reference(pg);
