0734c93d040c2f201df6c22d864d3851668ccf65
ober <ober@NetBSD.org>
2006-12-16 05:42:42 +0000

    Zaurus Port from OpenBSD by Nonaka Kimihiro OK gimpy

---
+++ sys/arch/zaurus/include/int_limits.h
@@ -0,0 +1,3 @@
+/*	$NetBSD: int_limits.h,v 1.1 2006/12/16 05:45:45 ober Exp $	*/
+
+#include <arm/int_limits.h>
---
+++ sys/arch/zaurus/include/int_mwgwtypes.h
@@ -0,0 +1,3 @@
+/*	$NetBSD: int_mwgwtypes.h,v 1.1 2006/12/16 05:45:52 ober Exp $	*/
+
+#include <arm/int_mwgwtypes.h>
