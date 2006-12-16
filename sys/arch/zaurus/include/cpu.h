c657d4868d9b9e42de635fc6754de9d604ebead6
ober <ober@NetBSD.org>
2006-12-16 05:42:42 +0000

    Zaurus Port from OpenBSD by Nonaka Kimihiro OK gimpy

---
+++ sys/arch/zaurus/include/cpu.h
@@ -0,0 +1,3 @@
+/*	$NetBSD: cpu.h,v 1.1 2006/12/16 05:43:48 ober Exp $	*/
+
+#include <arm/cpu.h>
---
+++ sys/arch/zaurus/include/types.h
@@ -0,0 +1,13 @@
+/*	$NetBSD: types.h,v 1.1 2006/12/16 05:48:39 ober Exp $	*/
+
+#ifndef _ZAURUS_TYPES_H_
+#define	_ZAURUS_TYPES_H_
+
+#include <arm/arm32/types.h>
+
+#define	__HAVE_DEVICE_REGISTER
+#define	__HAVE_NWSCONS
+#define	__HAVE_GENERIC_SOFT_INTERRUPTS
+#define	__GENERIC_SOFT_INTERRUPTS_ALL_LEVELS
+
+#endif	/* _ZAURUS_TYPES_H_ */
