945ef41fa560b8087b8d520e5a6df8c14c08e3b9
macallan <macallan@NetBSD.org>
2006-12-17 21:26:04 +0000

    don't pretend to support 8kHz since the chip really doesn't noticed by Marco Trillo

--- sys/arch/macppc/dev/snapper.c
+++ sys/arch/macppc/dev/snapper.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: snapper.c,v 1.14 2006/12/11 02:17:53 macallan Exp $	*/
+/*	$NetBSD: snapper.c,v 1.15 2006/12/17 21:26:04 macallan Exp $	*/
 /*	Id: snapper.c,v 1.11 2002/10/31 17:42:13 tsubai Exp	*/
 /*     Id: i2s.c,v 1.12 2005/01/15 14:32:35 tsubai Exp         */
 /*-
@@ -395,9 +395,9 @@ const uint8_t snapper_mixer_gain[178][3] = {
 #define SNAPPER_NFORMATS	2
 static const struct audio_format snapper_formats[SNAPPER_NFORMATS] = {
 	{NULL, AUMODE_PLAY | AUMODE_RECORD, AUDIO_ENCODING_SLINEAR_BE, 16, 16,
-	 2, AUFMT_STEREO, 4, {8000, 32000, 44100, 48000}},
+	 2, AUFMT_STEREO, 4, {32000, 44100, 48000}},
 	{NULL, AUMODE_PLAY | AUMODE_RECORD, AUDIO_ENCODING_SLINEAR_BE, 24, 24,
-	 2, AUFMT_STEREO, 4, {8000, 32000, 44100, 48000}},
+	 2, AUFMT_STEREO, 4, {32000, 44100, 48000}},
 };
 
 static u_char *amp_mute;
@@ -1382,12 +1382,6 @@ snapper_set_rate(struct snapper_softc *sc)
 	uint8_t mcr1;
 
 	switch (rate) {
-	case 8000:
-		clksrc = 18432000;		/* 18MHz */
-		reg = CLKSRC_18MHz;
-		mclk_fs = 256;
-		break;
-
 	case 44100:
 		clksrc = 45158400;		/* 45MHz */
 		reg = CLKSRC_45MHz;
