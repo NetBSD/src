a530d2b2eb53be2cac4f796c1e5fe6d4282decff
bouyer <bouyer@NetBSD.org>
2006-12-20 21:44:06 +0000

    Add missing $NetBSD: $

--- sys/dev/ic/mfireg.h
+++ sys/dev/ic/mfireg.h
@@ -1,3 +1,4 @@
+/* $NetBSD: mfireg.h,v 1.2 2006/12/20 21:44:06 bouyer Exp $ */
 /* $OpenBSD: mfireg.h,v 1.24 2006/06/19 19:05:45 marco Exp $ */
 /*
  * Copyright (c) 2006 Marco Peereboom <marco@peereboom.us>
