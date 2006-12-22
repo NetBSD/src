3d11477c942fc1d8b67cb72aead9f54c4a5fcd57
elad <elad@NetBSD.org>
2006-12-22 11:13:21 +0000

    Add requests indicating access to unmanaged memory for arm, pc532, powerpc, sh3, sh5, and vax, and use them instead of KAUTH_GENERIC_ISSUSER.
    Update documentation and example secmodel code.

--- sys/arch/sh5/sh5/mem.c
+++ sys/arch/sh5/sh5/mem.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: mem.c,v 1.12 2006/07/23 22:06:07 ad Exp $	*/
+/*	$NetBSD: mem.c,v 1.13 2006/12/22 11:13:21 elad Exp $	*/
 
 /*
  * Copyright (c) 1982, 1986, 1990, 1993
@@ -74,7 +74,7 @@
 
 #include <sys/cdefs.h>
 
-__KERNEL_RCSID(0, "$NetBSD: mem.c,v 1.12 2006/07/23 22:06:07 ad Exp $");
+__KERNEL_RCSID(0, "$NetBSD: mem.c,v 1.13 2006/12/22 11:13:21 elad Exp $");
 
 #include <sys/param.h>
 #include <sys/conf.h>
@@ -215,8 +215,9 @@ mmmmap(dev, off, prot)
 
 	/* minor device 0 is physical memory */
 
-	if (off >= ctob(physmem) && kauth_authorize_generic(l->l_cred,
-	    KAUTH_GENERIC_ISSUSER, &l->l_acflag) != 0)
+	if (off >= ctob(physmem) && kauth_authorize_machdep(l->l_cred,
+	    KAUTH_MACHDEP_SH5, KAUTH_REQ_MACHDEP_SH5_UNMANAGEDMEM, NULL, NULL,
+	    NULL) != 0)
 		return -1;
 	return sh5_btop(off);
 }
