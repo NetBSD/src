311d3d0551f208e4605207d7e9ccd649e98b6427
ad <ad>
2006-12-26 15:22:44 +0000

    Define ipl_t as uint8_t so that it can be packed into a word with a lock byte. Ok yamt@.

--- sys/arch/sparc/include/psl.h
+++ sys/arch/sparc/include/psl.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: psl.h,v 1.41 2006/12/21 15:55:25 yamt Exp $ */
+/*	$NetBSD: psl.h,v 1.42 2006/12/26 15:22:44 ad Exp $ */
 
 /*
  * Copyright (c) 1992, 1993
@@ -297,7 +297,7 @@ static __inline void name(void) \
 _SPLSET(spllowersoftclock, IPL_SOFTCLOCK)
 _SPLSET(spllowerschedclock, IPL_SCHED)
 
-typedef int ipl_t;
+typedef uint8_t ipl_t;
 typedef struct {
 	ipl_t _ipl;
 } ipl_cookie_t;
