203998b7f3260bf4a8c38ccf3103e514f206fb5b
martin <martin>
2006-12-26 23:26:13 +0000

    When converting FP exception codes, prefer overflow/underflow over inexact result - this makes regress/lib/libc/ieeefp/except a bit happier.

--- sys/arch/mips/mips/mips_fputrap.c
+++ sys/arch/mips/mips/mips_fputrap.c
@@ -1,4 +1,4 @@
-/* $NetBSD: mips_fputrap.c,v 1.4 2006/03/26 00:57:52 tsutsui Exp $ */
+/* $NetBSD: mips_fputrap.c,v 1.5 2006/12/26 23:26:13 martin Exp $ */
 
 /*
  * Copyright (c) 2004
@@ -67,9 +67,9 @@ static struct {
 	unsigned int bit;
 	int code;
 } fpecodes[] = {
-	{ MIPS_FPU_EXCEPTION_INEXACT, FPE_FLTRES },
 	{ MIPS_FPU_EXCEPTION_UNDERFLOW, FPE_FLTUND },
 	{ MIPS_FPU_EXCEPTION_OVERFLOW, FPE_FLTOVF },
+	{ MIPS_FPU_EXCEPTION_INEXACT, FPE_FLTRES },
 	{ MIPS_FPU_EXCEPTION_DIV0, FPE_FLTDIV },
 	{ MIPS_FPU_EXCEPTION_INVALID, FPE_FLTINV },
 	{ MIPS_FPU_EXCEPTION_UNIMPL, FPE_FLTINV }
