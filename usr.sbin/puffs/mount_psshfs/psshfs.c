c3ef8ea5d0204be9db889e14e8448a4c617c3474
pooka <pooka@NetBSD.org>
2006-12-29 15:35:39 +0000

    puffs sshfs, an sshfs implementation built on top of the puffs continuation framework

---
+++ usr.sbin/puffs/mount_psshfs/psshfs.c
@@ -0,0 +1,363 @@
+/*	$NetBSD: psshfs.c,v 1.1 2006/12/29 15:35:39 pooka Exp $	*/
+
+/*
+ * Copyright (c) 2006  Antti Kantee.  All Rights Reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The name of the company nor the name of the author may be used to
+ *    endorse or promote products derived from this software without specific
+ *    prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS
+ * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+/*
+ * psshfs: puffs sshfs
+ *
+ * psshfs implements sshfs functionality on top of puffs making it
+ * possible to mount a filesystme through the sftp service.
+ *
+ * psshfs can execute multiple operations in "parallel" by using the
+ * puffs_cc framework for continuations.
+ *
+ * Concurrency control is handled currently by vnode locking (this
+ * will change in the future).  Context switch locations are easy to
+ * find by grepping for puffs_cc_yield().
+ *
+ * The operation revolves around an event loop.  Incoming requests from
+ * the kernel are dispatched off to the libpuffs event handler, which
+ * eventually calls the callbacks.  The callbacks do whatever they need
+ * to do, queue output and call puffs_cc_yield() to put them to sleep.
+ * Meanwhile execution continues.  When an answer to the callback's
+ * query arrives, the blocker is located by the protocol's request
+ * id and awakened by calling puffs_docc().  All changes made to the
+ * buffer (psbuf->buf) will be seen by the callback.  Here the buffer
+ * contains the response of the sftp server.  The callback will then
+ * proceed to parse the buffer.
+ *
+ * XXX: struct psbuf is a mess.  it will be fixed sooner or later
+ */
+
+#include <sys/cdefs.h>
+#ifndef lint
+__RCSID("$NetBSD: psshfs.c,v 1.1 2006/12/29 15:35:39 pooka Exp $");
+#endif /* !lint */
+
+#include <sys/types.h>
+
+#include <assert.h>
+#include <err.h>
+#include <errno.h>
+#include <poll.h>
+#include <puffs.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <util.h>
+#include <unistd.h>
+
+#include "psshfs.h"
+
+static void	psshfs_eventloop(struct puffs_usermount *, struct psshfs_ctx *);
+static void	pssh_connect(struct psshfs_ctx *, char **);
+
+#define SSH_PATH "/usr/bin/ssh"
+
+int
+main(int argc, char *argv[])
+{
+	struct psshfs_ctx pctx;
+	struct puffs_usermount *pu;
+	struct puffs_ops *pops;
+	char *sshargs[16];
+	char *userhost;
+	char *hostpath;
+
+	setprogname(argv[0]);
+
+	if (argc != 3)
+		errx(1, "usage: %s user@host:path mountpath", getprogname());
+
+	PUFFSOP_INIT(pops);
+
+	PUFFSOP_SET(pops, psshfs, fs, mount);
+	PUFFSOP_SET(pops, psshfs, fs, unmount);
+	PUFFSOP_SETFSNOP(pops, sync); /* XXX */
+	PUFFSOP_SETFSNOP(pops, statvfs);
+
+	PUFFSOP_SET(pops, psshfs, node, lookup);
+	PUFFSOP_SET(pops, psshfs, node, create);
+	PUFFSOP_SET(pops, psshfs, node, readdir);
+	PUFFSOP_SET(pops, psshfs, node, getattr);
+	PUFFSOP_SET(pops, psshfs, node, setattr);
+	PUFFSOP_SET(pops, psshfs, node, mkdir);
+	PUFFSOP_SET(pops, psshfs, node, remove);
+	PUFFSOP_SET(pops, psshfs, node, readlink);
+	PUFFSOP_SET(pops, psshfs, node, rmdir);
+	PUFFSOP_SET(pops, psshfs, node, symlink);
+	PUFFSOP_SET(pops, psshfs, node, rename);
+	PUFFSOP_SET(pops, psshfs, node, read);
+	PUFFSOP_SET(pops, psshfs, node, write);
+#if 0
+	/* must support this some day */
+	PUFFSOP_SET(pops, psshfs, node, reclaim);
+#endif
+
+	memset(&pctx, 0, sizeof(pctx));
+	TAILQ_INIT(&pctx.outbufq);
+	TAILQ_INIT(&pctx.req_queue);
+
+	userhost = argv[1];
+	hostpath = strchr(userhost, ':');
+	if (hostpath) {
+		*hostpath++ = '\0';
+		pctx.mountpath = hostpath;
+	} else
+		pctx.mountpath = ".";
+
+	/* xblah */
+	sshargs[0] = SSH_PATH;
+	sshargs[1] = argv[1];
+	sshargs[2] = "-oClearAllForwardings=yes";
+	sshargs[3] = "-a";
+	sshargs[4] = "-x";
+	sshargs[5] = "-s";
+	sshargs[6] = "sftp";
+	sshargs[7] = 0;
+	pssh_connect(&pctx, sshargs);
+
+	if ((pu = puffs_mount(pops, argv[2], 0, "psshfs", &pctx,
+	    PUFFS_FLAG_BUILDPATH | PUFFS_KFLAG_NOCACHE, 0))==NULL)
+		err(1, "puffs_mount");
+
+	if (puffs_setblockingmode(pu, PUFFSDEV_NONBLOCK) == -1)
+		err(1, "setblockingmode");
+
+	daemon(0, 0);
+
+	psshfs_eventloop(pu, &pctx);
+	return 0;
+}
+
+void
+pssh_outbuf_enqueue(struct psshfs_ctx *pctx, struct psbuf *pb,
+	struct puffs_cc *pcc, uint32_t reqid)
+{
+
+	pb->psr.reqid = reqid;
+	pb->psr.pcc = pcc;
+	TAILQ_INSERT_TAIL(&pctx->outbufq, pb, psr.entries);
+}
+
+void
+psshreq_put(struct psshfs_ctx *pctx, struct psbuf *pb)
+{
+
+	TAILQ_INSERT_TAIL(&pctx->req_queue, pb, psr.entries);
+}
+
+struct psbuf *
+psshreq_get(struct psshfs_ctx *pctx, uint32_t reqid)
+{
+	struct psbuf *pb;
+
+	TAILQ_FOREACH(pb, &pctx->req_queue, psr.entries)
+		if (pb->psr.reqid == reqid)
+			break;
+
+	if (!pb)
+		return NULL;
+
+	TAILQ_REMOVE(&pctx->req_queue, pb, psr.entries);
+
+	return pb;
+}
+
+static void
+handlebuf(struct psshfs_ctx *pctx, struct psbuf *datapb,
+	struct puffs_putreq *ppr)
+{
+	struct psreq psrtmp;
+	struct psbuf *pb;
+
+	/* is this something we are expecting? */
+	pb = psshreq_get(pctx, datapb->reqid);
+
+	if (pb) {
+		/* keep psreq clean, xxx uknow */
+		psrtmp = pb->psr;
+		*pb = *datapb;
+		pb->psr = psrtmp;
+		free(datapb);
+
+		puffs_docc(ppr, pb->psr.pcc);
+		return;
+	}
+
+	printf("invalid server request response %d\n", datapb->reqid);
+	psbuf_destroy(datapb);
+}
+
+static int
+psshinput(struct psshfs_ctx *pctx, struct puffs_putreq *ppr)
+{
+	struct psbuf *cb;
+	int rv;
+
+	for (;;) {
+		if ((cb = pctx->curpb) == NULL) {
+			cb = psbuf_make(PSB_IN);
+			if (cb == NULL)
+				return -1;
+			pctx->curpb = cb;
+		}
+
+		rv = psbuf_read(pctx, cb);
+		if (rv == -1)
+			err(1, "psbuf read");
+		if (rv == 0)
+			break;
+
+		handlebuf(pctx, cb, ppr);
+		pctx->curpb = NULL;
+	}
+
+	return rv;
+}
+
+static int
+psshoutput(struct psshfs_ctx *pctx)
+{
+	struct psbuf *pb;
+	int rv;
+
+	TAILQ_FOREACH(pb, &pctx->outbufq, psr.entries) {
+		rv = psbuf_write(pctx, pb);
+		if (rv == -1)
+			return -1;
+		if (rv == 0)
+			return 0;
+
+		/* sent everything, move to cookiequeue */
+		TAILQ_REMOVE(&pctx->outbufq, pb, psr.entries);
+		free(pb->buf);
+		TAILQ_INSERT_TAIL(&pctx->req_queue, pb, psr.entries);
+	}
+
+	return 1;
+}
+
+volatile int timetodie;
+
+#define PFD_SSH 0
+#define PFD_PUFFS 1
+static void
+psshfs_eventloop(struct puffs_usermount *pu, struct psshfs_ctx *pctx)
+{
+	struct puffs_getreq *pgr;
+	struct puffs_putreq *ppr;
+	struct pollfd pfds[2];
+	int x;
+
+	pgr = puffs_makegetreq(pu, pu->pu_maxreqlen, 0);
+	if (!pgr)
+		err(1, "makegetreq");
+	ppr = puffs_makeputreq(pu);
+	if (!ppr)
+		err(1, "makeputreq");
+
+	x = 1;
+	if (ioctl(pctx->sshfd, FIONBIO, &x) == -1)
+		err(1, "nonblocking descriptor");
+
+	while (puffs_getstate(pu) != PUFFS_STATE_UNMOUNTED) {
+		if (timetodie) {
+			kill(pctx->sshpid, SIGTERM);
+			break;
+		}
+
+		memset(pfds, 0, sizeof(pfds));
+		pfds[PFD_SSH].events = POLLIN;
+		if (!TAILQ_EMPTY(&pctx->outbufq))
+			pfds[PFD_SSH].events |= POLLOUT;
+		pfds[PFD_SSH].fd = pctx->sshfd;
+		pfds[PFD_PUFFS].fd = puffs_getselectable(pu);
+		pfds[PFD_PUFFS].events = POLLIN;
+
+		puffs_resetputreq(ppr);
+
+		if (poll(pfds, 2, INFTIM) == -1)
+			err(1, "poll");
+
+		if (pfds[PFD_SSH].revents & POLLOUT)
+			if (psshoutput(pctx) == -1)
+				err(1, "psshoutput");
+		
+		/* get & possibly dispatch events from kernel */
+		if (pfds[PFD_PUFFS].revents & POLLIN)
+			if (puffs_handlereqs(pu, pgr, ppr, 0) == -1)
+				err(1, "puffs_handlereqs");
+
+		/* get input from sftpd, possibly build more responses */
+		if (pfds[PFD_SSH].revents & POLLIN)
+			if (psshinput(pctx, ppr) == -1)
+				errx(1, "psshinput");
+
+		/* it's likely we got outputtables, poke the ice with a stick */
+		if (psshoutput(pctx) == -1)
+			err(1, "psshoutput");
+
+		/* stuff all replies from both of the above into kernel */
+		if (puffs_putputreq(ppr) == -1)
+			err(1, "putputreq");
+	}
+
+	puffs_destroygetreq(pgr);
+}
+
+static void
+pssh_connect(struct psshfs_ctx *pctx, char **sshargs)
+{
+	int fds[2];
+	pid_t pid;
+
+	if (socketpair(AF_UNIX, SOCK_STREAM, 0, fds) == -1)
+		err(1, "socketpair");
+
+	pid = fork();
+	switch (pid) {
+	case -1:
+		err(1, "fork");
+		/*NOTREACHED*/
+	case 0: /* child */
+		if (dup2(fds[0], STDIN_FILENO) == -1)
+			err(1, "child dup2");
+		if (dup2(fds[0], STDOUT_FILENO) == -1)
+			err(1, "child dup2");
+		close(fds[0]);
+		close(fds[1]);
+		execvp(sshargs[0], sshargs);
+		break;
+	default:
+		pctx->sshpid = pid;
+		pctx->sshfd = fds[1];
+		close(fds[0]);
+		break;
+	}
+}
---
+++ usr.sbin/puffs/mount_psshfs/subr.c
@@ -0,0 +1,283 @@
+/*      $NetBSD: subr.c,v 1.1 2006/12/29 15:35:40 pooka Exp $        */
+        
+/*      
+ * Copyright (c) 2006  Antti Kantee.  All Rights Reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:     
+ * 1. Redistributions of source code must retain the above copyright  
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The name of the company nor the name of the author may be used to
+ *    endorse or promote products derived from this software without specific
+ *    prior written permission.
+ *      
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS
+ * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */     
+        
+#include <sys/cdefs.h>
+#ifndef lint
+__RCSID("$NetBSD: subr.c,v 1.1 2006/12/29 15:35:40 pooka Exp $");
+#endif /* !lint */
+
+#include <assert.h>
+#include <errno.h>
+#include <puffs.h>
+#include <stdlib.h>
+#include <util.h>
+
+#include "psshfs.h"
+#include "sftp_proto.h"
+
+static void
+freedircache(struct psshfs_dir *base, size_t count)
+{
+	int i;
+
+	for (i = 0; i < count; i++) {
+		free(base[i].entryname);
+		base[i].entryname = NULL;
+	}
+
+	free(base);
+}
+
+#define ENTRYCHUNK 16
+static void
+allocdirs(struct psshfs_node *psn)
+{
+	size_t oldtot = psn->denttot;
+
+	psn->denttot += ENTRYCHUNK;
+	psn->dir = erealloc(psn->dir,
+	    psn->denttot * sizeof(struct psshfs_dir));
+	memset(psn->dir + oldtot, 0, ENTRYCHUNK * sizeof(struct psshfs_dir));
+}
+
+struct psshfs_dir *
+lookup(struct psshfs_dir *basedir, size_t ndir, const char *name)
+{
+	struct psshfs_dir *ent;
+	int i;
+
+	for (i = 0; i < ndir; i++) {
+		ent = &basedir[i];
+		if (ent->valid != 1)
+			continue;
+		if (strcmp(ent->entryname, name) == 0)
+			return ent;
+	}
+
+	return NULL;
+}
+
+int
+sftp_readdir(struct puffs_cc *pcc, struct psshfs_ctx *pctx,
+	struct puffs_node *pn)
+{
+	struct psshfs_node *psn = pn->pn_data;
+	struct psshfs_dir *olddir, *testd;
+	struct psbuf *pb;
+	uint32_t reqid = NEXTREQ(pctx);
+	uint32_t count;
+	char *dhand = NULL;
+	size_t dhandlen, nent;
+	char *longname;
+	int idx, rv;
+
+	assert(pn->pn_va.va_type == VDIR);
+
+	if (psn->dir && (time(NULL) - psn->dentread) < PSSHFS_REFRESHIVAL)
+		return 0;
+
+	pb = psbuf_make(PSB_OUT);
+	psbuf_req_str(pb, SSH_FXP_OPENDIR, reqid, pn->pn_path);
+	pssh_outbuf_enqueue(pctx, pb, pcc, reqid);
+
+	puffs_cc_yield(pcc);
+
+	rv = psbuf_expect_handle(pb, &dhand, &dhandlen);
+	if (rv)
+		goto wayout;
+
+	/*
+	 * Well, the following is O(n^2), so feel free to improve if it
+	 * gets too taxing on your system.
+	 */
+	olddir = psn->dir;
+	nent = psn->dentnext;
+
+	psn->dentnext = 0;
+	psn->denttot = 0;
+	psn->dir = NULL;
+	idx = 0;
+
+	for (;;) {
+		reqid = NEXTREQ(pctx);
+		psbuf_recycle(pb, PSB_OUT);
+		psbuf_req_data(pb, SSH_FXP_READDIR, reqid, dhand, dhandlen);
+		pssh_outbuf_enqueue(pctx, pb, pcc, reqid);
+
+		puffs_cc_yield(pcc);
+
+		/* check for EOF */
+		if (pb->type == SSH_FXP_STATUS) {
+			rv = psbuf_expect_status(pb);
+			goto out;
+		}
+		rv = psbuf_expect_name(pb, &count);
+		if (rv)
+			goto out;
+
+		for (; count--; idx++) {
+			if (idx == psn->denttot)
+				allocdirs(psn);
+			if (!psbuf_get_str(pb, &psn->dir[idx].entryname,
+			    NULL)) {
+				rv = EPROTO;
+				goto out;
+			}
+			if (!psbuf_get_str(pb, &longname, NULL)) {
+				rv = EPROTO;
+				goto out;
+			}
+			if (!psbuf_get_vattr(pb, &psn->dir[idx].va)) {
+				rv = EPROTO;
+				goto out;
+			}
+			if (sscanf(longname, "%*s%d",
+			    &psn->dir[idx].va.va_nlink) != 1) {
+				rv = EPROTO;
+				goto out;
+			}
+			free(longname);
+
+			testd = lookup(olddir, nent, psn->dir[idx].entryname);
+			if (testd) {
+				psn->dir[idx].entry = testd->entry;
+			} else {
+				psn->dir[idx].entry = NULL;
+				psn->dir[idx].va.va_fileid = pctx->nextino++;
+			}
+			psn->dir[idx].valid = 1;
+		}
+	}
+
+ out:
+	/* XXX: rv */
+	psn->dentnext = idx;
+	psn->dentread = time(NULL);
+	freedircache(olddir, nent);
+
+	reqid = NEXTREQ(pctx);
+	psbuf_recycle(pb, PSB_OUT);
+	psbuf_req_data(pb, SSH_FXP_CLOSE, reqid, dhand, dhandlen);
+	pssh_outbuf_enqueue(pctx, pb, pcc, reqid);
+
+	puffs_cc_yield(pcc);
+
+	/* EDONTCARE for the response */
+
+ wayout:
+	free(dhand);
+	psbuf_destroy(pb);
+	return rv;
+}
+
+struct puffs_node *
+makenode(struct puffs_usermount *pu, struct puffs_node *parent,
+	struct psshfs_dir *pd, const struct vattr *vap)
+{
+	struct psshfs_node *psn_parent = parent->pn_data;
+	struct psshfs_node *psn;
+	struct puffs_node *pn;
+
+	psn = emalloc(sizeof(struct psshfs_node));
+	memset(psn, 0, sizeof(struct psshfs_node));
+
+	pn = puffs_pn_new(pu, psn);
+	if (!pn) {
+		free(psn);
+		return NULL;
+	}
+	puffs_setvattr(&pn->pn_va, &pd->va);
+	puffs_setvattr(&pn->pn_va, vap);
+
+	pd->entry = pn;
+	psn->parent = parent;
+	psn_parent->childcount++;
+
+	return pn;
+}
+
+struct puffs_node *
+allocnode(struct puffs_usermount *pu, struct puffs_node *parent,
+	const char *entryname, const struct vattr *vap)
+{
+	struct psshfs_ctx *pctx = pu->pu_privdata;
+	struct psshfs_dir *pd;
+
+	pd = direnter(parent, entryname);
+
+	pd->va.va_fileid = pctx->nextino++;
+	if (vap->va_type == VDIR)
+		pd->va.va_nlink = 2;
+	else
+		pd->va.va_nlink = 1;
+
+	return makenode(pu, parent, pd, vap);
+}
+
+struct psshfs_dir *
+direnter(struct puffs_node *parent, const char *entryname)
+{
+	struct psshfs_node *psn_parent = parent->pn_data;
+	struct psshfs_dir *pd;
+	int i;
+
+	/* create directory entry */
+	if (psn_parent->denttot == psn_parent->dentnext)
+		allocdirs(psn_parent);
+
+	i = psn_parent->dentnext;
+	pd = &psn_parent->dir[i];
+	pd->entryname = estrdup(entryname);
+	pd->valid = 1;
+	psn_parent->dentnext++;
+
+	return pd;
+}
+
+void
+nukenode(struct puffs_node *node, const char *entryname)
+{
+	struct psshfs_node *psn, *psn_parent;
+	struct psshfs_dir *pd;
+
+	psn = node->pn_data;
+	psn_parent = psn->parent->pn_data;
+	psn_parent->childcount--;
+	pd = lookup(psn_parent->dir, psn_parent->dentnext, entryname);
+	assert(pd != NULL);
+	pd->valid = 0;
+	free(pd->entryname);
+	pd->entryname = NULL;
+
+	if (node->pn_va.va_type == VDIR) {
+		psn->parent->pn_va.va_nlink--;
+		freedircache(psn->dir, psn->dentnext);
+	}
+}
