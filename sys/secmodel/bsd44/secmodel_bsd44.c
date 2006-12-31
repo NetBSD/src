844eefb27ac54dba41486f9889fa539a3748d96e
elad <elad>
2006-12-31 10:36:44 +0000

    Explicitly 'extern int securelevel;' in the sysctl(9) setup routine.

--- sys/secmodel/bsd44/secmodel_bsd44.c
+++ sys/secmodel/bsd44/secmodel_bsd44.c
@@ -1,4 +1,4 @@
-/* $NetBSD: secmodel_bsd44.c,v 1.4 2006/10/06 23:01:12 elad Exp $ */
+/* $NetBSD: secmodel_bsd44.c,v 1.5 2006/12/31 10:36:44 elad Exp $ */
 /*-
  * Copyright (c) 2006 Elad Efrat <elad@NetBSD.org>
  * All rights reserved.
@@ -30,7 +30,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: secmodel_bsd44.c,v 1.4 2006/10/06 23:01:12 elad Exp $");
+__KERNEL_RCSID(0, "$NetBSD: secmodel_bsd44.c,v 1.5 2006/12/31 10:36:44 elad Exp $");
 
 #include <sys/types.h>
 #include <sys/param.h>
@@ -48,6 +48,7 @@ SYSCTL_SETUP(sysctl_security_bsd44_setup,
     "sysctl security bsd44 setup")
 {
 	const struct sysctlnode *rnode;
+	extern int securelevel; /* for compatibility. */
 
 	sysctl_createv(clog, 0, NULL, &rnode,
 		       CTLFLAG_PERMANENT,
