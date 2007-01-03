1217bfbb66e90238430f732f218c130580926d4a
bouyer <bouyer@NetBSD.org>
2007-01-03 18:58:26 +0000

    Fix cut'n'paste, don't print version 1.0 when we have a version 1.1 controller. Pointed out by Craig Metz.

--- sys/dev/pci/ahcisata.c
+++ sys/dev/pci/ahcisata.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: ahcisata.c,v 1.2 2006/12/02 13:35:52 bouyer Exp $	*/
+/*	$NetBSD: ahcisata.c,v 1.3 2007/01/03 18:58:26 bouyer Exp $	*/
 
 /*
  * Copyright (c) 2006 Manuel Bouyer.
@@ -31,7 +31,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: ahcisata.c,v 1.2 2006/12/02 13:35:52 bouyer Exp $");
+__KERNEL_RCSID(0, "$NetBSD: ahcisata.c,v 1.3 2007/01/03 18:58:26 bouyer Exp $");
 
 #include <sys/types.h>
 #include <sys/malloc.h>
@@ -243,7 +243,7 @@ ahci_attach(struct device *parent, struct device *self, void *aux)
 		aprint_normal("1.0");
 		break;
 	case AHCI_VS_11:
-		aprint_normal("1.0");
+		aprint_normal("1.1");
 		break;
 	default:
 		aprint_normal("0x%x", ahci_rev);
