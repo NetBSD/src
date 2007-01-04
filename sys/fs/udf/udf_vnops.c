d896a49ced988acbe2c173609fbba40f5c7ce347
reinoud <reinoud@NetBSD.org>
2007-01-04 04:15:43 +0000

    Add UDF_ICB_FILETYPE_REALTIME type as found on UDF 2.50 discs. Also revert a multiplication order to enforce 64 bit math.

--- sys/fs/udf/udf_vnops.c
+++ sys/fs/udf/udf_vnops.c
@@ -1,4 +1,4 @@
-/* $NetBSD: udf_vnops.c,v 1.8 2006/12/09 16:11:51 chs Exp $ */
+/* $NetBSD: udf_vnops.c,v 1.9 2007/01/04 04:15:43 reinoud Exp $ */
 
 /*
  * Copyright (c) 2006 Reinoud Zandijk
@@ -36,7 +36,7 @@
 
 #include <sys/cdefs.h>
 #ifndef lint
-__RCSID("$NetBSD: udf_vnops.c,v 1.8 2006/12/09 16:11:51 chs Exp $");
+__RCSID("$NetBSD: udf_vnops.c,v 1.9 2007/01/04 04:15:43 reinoud Exp $");
 #endif /* not lint */
 
 
@@ -698,7 +698,7 @@ udf_getattr(void *v)
 	vap->va_gen       = 1;		/* no multiple generations yes (!?) */
 	vap->va_flags     = 0;		/* no flags */
 	vap->va_rdev      = udf_node->rdev;
-	vap->va_bytes     = udf_node->ump->discinfo.sector_size * blkssize;
+	vap->va_bytes     = blkssize * udf_node->ump->discinfo.sector_size;
 	vap->va_filerev   = 1;		/* TODO file revision numbers? */
 	vap->va_vaflags   = 0;		/* TODO which va_vaflags? */
 
