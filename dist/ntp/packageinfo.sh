17d7cfe9a097abdb5aaeb37490d6faf040006902
kardel <kardel@NetBSD.org>
2007-01-06 16:04:26 +0000

    Import ntp 4.2.4

--- dist/ntp/packageinfo.sh
+++ dist/ntp/packageinfo.sh
@@ -3,24 +3,27 @@ repotype=stable
 # odd minor numbers are for -dev, even minor numbers are for -stable
 proto=4
 major=2
-minor=2
+minor=4
 version=${proto}.${major}.${minor}
 # Point.  3 cases:
 # - Numeric values increment
 # - empty 'increments' to 1
 # - NEW 'increments' to empty
-point=2
+point=NEW
 # Special.  Normally unused.  A suffix.
 #special=ag
 special=
 # ReleaseCandidate. 'yes' or 'no'.
-#releasecandidate=yes
 releasecandidate=no
+#releasecandidate=no
 # ChangeLog tag
 CLTAG=NTP_4_2_0
 ###
-# The following is for ntp-stable.  3 cases:
+# The following is for ntp-stable.  2 cases:
 # - Numeric values increment
-# - empty 'increments' to 1
-# - NEW 'increments' to empty
-rcpoint=NEW
+# - GO triggers a release
+# - - rcpoint gets set to 0
+# - - releasecandidate gets set to no
+# - GRONK is for -dev
+rcpoint=
+#rcpoint=GRONK
