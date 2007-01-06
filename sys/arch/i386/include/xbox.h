f7f0d94d8732e738add6a64be2b5b12e61891582
jmcneill <jmcneill>
2007-01-06 18:42:36 +0000

    Use the pic16lc@iic driver for setting the system LED, rebooting, and powering off the machine instead of the assembly code in pic16l.S

--- sys/arch/i386/include/xbox.h
+++ sys/arch/i386/include/xbox.h
@@ -1,4 +1,4 @@
-/* $NetBSD: xbox.h,v 1.1 2007/01/04 18:16:44 jmcneill Exp $ */
+/* $NetBSD: xbox.h,v 1.2 2007/01/06 18:42:36 jmcneill Exp $ */
 
 /*-
  * Copyright (c) 2005 Rink Springer
@@ -79,10 +79,9 @@
 extern int arch_i386_is_xbox;
 extern uint32_t arch_i386_xbox_memsize; /* Megabytes */
 
-void pic16l_setbyte(int addr, int reg, int data);
-void pic16l_setled(int val);
-void pic16l_reboot(void);
-void pic16l_poweroff(void);
+void xbox_setled(uint8_t);
+void xbox_reboot(void);
+void xbox_poweroff(void);
 
 void xbox_lcd_init(void);
 void xbox_lcd_output(unsigned char data, unsigned char command);
