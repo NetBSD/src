c7d9361b2ec440e530a8f155db3b0afa64550650
peter <peter@NetBSD.org>
2007-01-08 20:56:34 +0000

    Remove __HAVE_NWSCONS and __GENERIC_SOFT_INTERRUPTS_ALL_LEVELS.

--- sys/arch/zaurus/include/types.h
+++ sys/arch/zaurus/include/types.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: types.h,v 1.1 2006/12/16 05:48:39 ober Exp $	*/
+/*	$NetBSD: types.h,v 1.2 2007/01/08 20:56:34 peter Exp $	*/
 
 #ifndef _ZAURUS_TYPES_H_
 #define	_ZAURUS_TYPES_H_
@@ -6,8 +6,6 @@
 #include <arm/arm32/types.h>
 
 #define	__HAVE_DEVICE_REGISTER
-#define	__HAVE_NWSCONS
 #define	__HAVE_GENERIC_SOFT_INTERRUPTS
-#define	__GENERIC_SOFT_INTERRUPTS_ALL_LEVELS
 
 #endif	/* _ZAURUS_TYPES_H_ */
