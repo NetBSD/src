76b76b47faff7ecea3d6bb5a386d96bea5c621fe
tron <tron>
2007-01-08 22:31:28 +0000

    Pull up following revision(s) (requested by bouyer in ticket #1625): 	sys/arch/xen/xen/xencons.c: revision 1.20 - Add or change memory barrier to reflect the related operation - In xencons_handler(), update in_cons inside the loop, otherwise,   we would trigger the xenconscn_getc() workaround wich reset cons and prod   to their original values, and this creates an infinite loop Should fix the console hang reported by several users on port-xen@.

--- sys/arch/xen/xen/xencons.c
+++ sys/arch/xen/xen/xencons.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: xencons.c,v 1.4.2.6 2006/05/09 12:57:59 tron Exp $	*/
+/*	$NetBSD: xencons.c,v 1.4.2.7 2007/01/08 22:31:28 tron Exp $	*/
 
 /*
  * Copyright (c) 2006 Manuel Bouyer.
@@ -63,7 +63,7 @@
 
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: xencons.c,v 1.4.2.6 2006/05/09 12:57:59 tron Exp $");
+__KERNEL_RCSID(0, "$NetBSD: xencons.c,v 1.4.2.7 2007/01/08 22:31:28 tron Exp $");
 
 #include "opt_xen.h"
 
@@ -381,6 +381,7 @@ xencons_start(struct tty *tp)
 #define XNC_OUT (xencons_interface->out)
 		cons = xencons_interface->out_cons;
 		prod = xencons_interface->out_prod;
+		x86_lfence();
 		while (prod != cons + sizeof(xencons_interface->out)) {
 			if (MASK_XENCONS_IDX(prod, XNC_OUT) <
 			    MASK_XENCONS_IDX(cons, XNC_OUT)) {
@@ -396,9 +397,9 @@ xencons_start(struct tty *tp)
 				break;
 			prod = prod + len;
 		}
-		x86_lfence();
+		x86_sfence();
 		xencons_interface->out_prod = prod;
-		x86_lfence();
+		x86_sfence();
 		hypervisor_notify_via_evtchn(xen_start_info.console_evtchn);
 #undef XNC_OUT
 #else /* XEN3 */
@@ -476,12 +477,13 @@ xencons_handler(void *arg)
 			cons = xencons_interface->in_cons;
 			prod = xencons_interface->in_prod;
 			x86_lfence();
-		} else
+		} else {
 			cons += len;
+			x86_sfence();
+			xencons_interface->in_cons = cons;
+			x86_sfence();
+		}
 	}
-	x86_lfence();
-	xencons_interface->in_cons = cons;
-	x86_lfence();
 	hypervisor_notify_via_evtchn(xen_start_info.console_evtchn);
 	splx(s);
 	return 1;
