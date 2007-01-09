bf77574aa2b5fdad807093b7d251eeb1b425e95f
mjf <mjf@NetBSD.org>
2007-01-09 13:53:31 +0000

    Delete advertising clause.

--- usr.sbin/veriexecgen/veriexecgen.c
+++ usr.sbin/veriexecgen/veriexecgen.c
@@ -1,4 +1,4 @@
-/* $NetBSD: veriexecgen.c,v 1.12 2006/12/23 09:16:38 wiz Exp $ */
+/* $NetBSD: veriexecgen.c,v 1.13 2007/01/09 13:53:31 mjf Exp $ */
 
 /*-
  * Copyright (c) 2006 The NetBSD Foundation, Inc.
@@ -15,11 +15,7 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *        This product includes software developed by the NetBSD
- *        Foundation, Inc. and its contributors.
- * 4. Neither the name of The NetBSD Foundation nor the names of its
+ * 3. Neither the name of The NetBSD Foundation nor the names of its
  *    contributors may be used to endorse or promote products derived
  *    from this software without specific prior written permission.
  *
@@ -43,7 +39,7 @@
 
 #ifndef lint
 #ifdef __RCSID
-__RCSID("$NetBSD: veriexecgen.c,v 1.12 2006/12/23 09:16:38 wiz Exp $");
+__RCSID("$NetBSD: veriexecgen.c,v 1.13 2007/01/09 13:53:31 mjf Exp $");
 #endif
 #endif /* not lint */
 
