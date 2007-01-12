11f4667eec1561f57da9fd1b8bb5d374573c6b2e
cube <cube@NetBSD.org>
2007-01-12 19:33:21 +0000

    Add missing initializers in cdevsw structs.

--- sys/dev/i2c/m41t00.c
+++ sys/dev/i2c/m41t00.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: m41t00.c,v 1.7 2006/09/04 23:45:30 gdamore Exp $	*/
+/*	$NetBSD: m41t00.c,v 1.8 2007/01/12 19:33:21 cube Exp $	*/
 
 /*
  * Copyright (c) 2003 Wasabi Systems, Inc.
@@ -74,7 +74,7 @@ dev_type_write(m41t00_write);
 
 const struct cdevsw m41t00_cdevsw = {
 	m41t00_open, m41t00_close, m41t00_read, m41t00_write, noioctl,
-	nostop, notty, nopoll, nommap, nokqfilter,
+	nostop, notty, nopoll, nommap, nokqfilter, D_OTHER
 };
 
 static int m41t00_clock_read(struct m41t00_softc *, struct clock_ymdhms *);
