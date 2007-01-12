79e2ce82c7612be82a0b10a5be4787a10d4da9ce
ober <ober@NetBSD.org>
2007-01-12 13:06:11 +0000

    Update zaurus port to include TODR/TIMECOUNTER. zaurus# ./timetest -A -t 600 Will test active counter and counters with positive quality from saost_count(q=100, f=3686400 Hz) clockinterrupt(q=0, f=100 Hz) dummy(q=-1000000, f=1000000 Hz) Testing time for monotonicity of timecounter "saost_count" for 600 seconds... claimed resolution 271 nsec (3690036.900369 Hz) or better, observed minimum non zero delta 2712 nsec switching to timecounter "saost_count"... Testing time for monotonicity of timecounter "saost_count" for 600 seconds... claimed resolution 271 nsec (3690036.900369 Hz) or better, observed minimum non zero delta 2712 nsec switching to timecounter "clockinterrupt"... Testing time for monotonicity of timecounter "clockinterrupt" for 600 seconds... claimed resolution 10000000 nsec (100.000000 Hz) or better, observed minimum non zero delta 9999999 nsec TEST SUCCESSFUL
    ok peter@

--- sys/arch/zaurus/include/types.h
+++ sys/arch/zaurus/include/types.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: types.h,v 1.2 2007/01/08 20:56:34 peter Exp $	*/
+/*	$NetBSD: types.h,v 1.3 2007/01/12 13:06:11 ober Exp $	*/
 
 #ifndef _ZAURUS_TYPES_H_
 #define	_ZAURUS_TYPES_H_
@@ -7,5 +7,7 @@
 
 #define	__HAVE_DEVICE_REGISTER
 #define	__HAVE_GENERIC_SOFT_INTERRUPTS
+#define	__HAVE_GENERIC_TODR
+#define	__HAVE_TIMECOUNTER
 
 #endif	/* _ZAURUS_TYPES_H_ */
