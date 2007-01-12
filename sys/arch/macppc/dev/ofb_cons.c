a5b5fa6eaeb2b66ebe2b1d892b424e0bc0059a04
bouyer <bouyer@NetBSD.org>
2007-01-12 23:48:40 +0000

    Pull up following revision(s) (requested by macallan in ticket #353): 	sys/arch/macppc/dev/ofb_cons.c: revision 1.3 	sys/arch/macppc/macppc/machdep.c: revision 1.143 make this compile without wsdisplay

--- sys/arch/macppc/dev/ofb_cons.c
+++ sys/arch/macppc/dev/ofb_cons.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: ofb_cons.c,v 1.1.4.1 2007/01/12 23:38:55 bouyer Exp $	*/
+/*	$NetBSD: ofb_cons.c,v 1.1.4.2 2007/01/12 23:48:40 bouyer Exp $	*/
 
 /*
  * Copyright (c) 1995, 1996 Carnegie-Mellon University.
@@ -28,7 +28,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: ofb_cons.c,v 1.1.4.1 2007/01/12 23:38:55 bouyer Exp $");
+__KERNEL_RCSID(0, "$NetBSD: ofb_cons.c,v 1.1.4.2 2007/01/12 23:48:40 bouyer Exp $");
 
 #include <sys/param.h>
 #include <sys/buf.h>
@@ -53,12 +53,16 @@ __KERNEL_RCSID(0, "$NetBSD: ofb_cons.c,v 1.1.4.1 2007/01/12 23:38:55 bouyer Exp 
 #include <dev/wscons/wsdisplay_vconsvar.h>
 
 #include <macppc/dev/ofbvar.h>
+#include "wsdisplay.h"
+
+/* we need a wsdisplay to do anything halfway useful */
+#if NWSDISPLAY > 0
 
 #if defined(PPC_OEA64) || defined (PPC_OEA64_BRIDGE)
 int ofb_enable_cache = 0;
 #else
 #ifdef OFB_ENABLE_CACHE
-int ofb_enable_cache = 0;
+int ofb_enable_cache = 1;
 #else
 int ofb_enable_cache = 0;
 #endif
@@ -135,17 +139,8 @@ ofb_cnattach()
 	ri->ri_ops.allocattr(ri, 0, 0, 0, &defattr);
 	wsdisplay_preattach(&ofb_stdscreen, ri, 0, crow, defattr);
 	
-	//ofb_init_cmap(NULL);
-	
-#ifdef SPLASHSCREEN
-	si.si_depth = ri->ri_depth;
-	si.si_bits = (char *)fbaddr;
-	si.si_hwbits = (char *)fbaddr;
-	si.si_width = ri->ri_width;
-	si.si_height = ri->ri_height;
-	si.si_stride = ri->ri_stride;
-	si.si_fillrect = NULL;
-	splash_render(&si, SPLASH_F_CENTER|SPLASH_F_FILL);
+#if notyet
+	ofb_init_cmap(NULL);
 #endif
 
 	return 0;
@@ -271,3 +266,10 @@ ofb_init_rasops(int node, struct rasops_info *ri)
 
 	return TRUE;
 }
+#else	/* NWSDISPLAY > 0 */
+int
+ofb_cnattach()
+{
+	return -1;
+}
+#endif
