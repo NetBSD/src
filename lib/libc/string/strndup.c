cd2fcb25a4bfe1a05cbf633c7bcad125d7cef2d9
cbiere <cbiere@NetBSD.org>
2007-01-14 18:48:28 +0000

    Look only at the first `n' characters to avoid traversing a potentially huge string for nothing.

--- lib/libc/string/strndup.c
+++ lib/libc/string/strndup.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: strndup.c,v 1.1 2006/08/12 23:49:54 christos Exp $	*/
+/*	$NetBSD: strndup.c,v 1.2 2007/01/14 18:48:28 cbiere Exp $	*/
 
 /*
  * Copyright (c) 1988, 1993
@@ -34,7 +34,7 @@
 #if 0
 static char sccsid[] = "@(#)strdup.c	8.1 (Berkeley) 6/4/93";
 #else
-__RCSID("$NetBSD: strndup.c,v 1.1 2006/08/12 23:49:54 christos Exp $");
+__RCSID("$NetBSD: strndup.c,v 1.2 2007/01/14 18:48:28 cbiere Exp $");
 #endif
 #endif /* LIBC_SCCS and not lint */
 
@@ -57,12 +57,12 @@ strndup(const char *str, size_t n)
 
 	_DIAGASSERT(str != NULL);
 
-	len = strlen(str);
-	if (len > n)
-		len = n;
-	if (!(copy = malloc(len + 1)))
-		return (NULL);
-	memcpy(copy, str, len + 1);
-	copy[len] = '\0';
+	for (len = 0; len < n && str[len]; len++)
+		continue;
+
+	if ((copy = malloc(len + 1))) {
+		memcpy(copy, str, len);
+		copy[len] = '\0';
+	}
 	return (copy);
 }
