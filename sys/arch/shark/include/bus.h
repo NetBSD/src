3dbd0a64f8e3a874e419fe1a3f1ac240daacea8e
macallan <macallan@NetBSD.org>
2007-01-21 23:19:57 +0000

    add bus_space_*_stream() methods

--- sys/arch/shark/include/bus.h
+++ sys/arch/shark/include/bus.h
@@ -1,4 +1,6 @@
-/*	$NetBSD: bus.h,v 1.1 2002/02/10 01:57:10 thorpej Exp $	*/
+/*	$NetBSD: bus.h,v 1.2 2007/01/21 23:19:57 macallan Exp $	*/
+
+#define __BUS_SPACE_HAS_STREAM_METHODS
 
 #include <arm/bus.h>
 
