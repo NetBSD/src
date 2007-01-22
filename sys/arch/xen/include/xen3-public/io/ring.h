0955de2ed4b8570ed1f1155e7696c631e4088b93
tron <tron@NetBSD.org>
2007-01-22 13:42:54 +0000

    Pull up following revision(s) (requested by bouyer in ticket #1652): 	sys/arch/xen/include/cpufunc.h: revision 1.13 	sys/arch/xen/include/xen3-public/io/ring.h: revision 1.6 Define x86_mfence(). Use it where appropriate. Seems to fix a hang in the network interface front-end/back-end for me ...

--- sys/arch/xen/include/xen3-public/io/ring.h
+++ sys/arch/xen/include/xen3-public/io/ring.h
@@ -1,4 +1,4 @@
-/* $NetBSD: ring.h,v 1.4.2.2 2006/04/07 12:51:26 tron Exp $ */
+/* $NetBSD: ring.h,v 1.4.2.3 2007/01/22 13:42:55 tron Exp $ */
 /******************************************************************************
  * ring.h
  * 
@@ -225,9 +225,9 @@ typedef struct __name##_back_ring __name##_back_ring_t
 #define RING_PUSH_REQUESTS_AND_CHECK_NOTIFY(_r, _notify) do {           \
     RING_IDX __old = (_r)->sring->req_prod;                             \
     RING_IDX __new = (_r)->req_prod_pvt;                                \
-    x86_lfence(); /* back sees requests /before/ updated producer index */ \
+    x86_sfence(); /* back sees requests /before/ updated producer index */ \
     (_r)->sring->req_prod = __new;                                      \
-    x86_lfence(); /* back sees new requests /before/ we check req_event */ \
+    x86_mfence(); /* back sees new requests /before/ we check req_event */ \
     (_notify) = ((RING_IDX)(__new - (_r)->sring->req_event) <           \
                  (RING_IDX)(__new - __old));                            \
 } while (0)
@@ -235,9 +235,9 @@ typedef struct __name##_back_ring __name##_back_ring_t
 #define RING_PUSH_RESPONSES_AND_CHECK_NOTIFY(_r, _notify) do {          \
     RING_IDX __old = (_r)->sring->rsp_prod;                             \
     RING_IDX __new = (_r)->rsp_prod_pvt;                                \
-    x86_lfence(); /* front sees responses /before/ updated producer index */ \
+    x86_sfence(); /* front sees responses /before/ updated producer index */ \
     (_r)->sring->rsp_prod = __new;                                      \
-    x86_lfence(); /* front sees new responses /before/ we check rsp_event */ \
+    x86_mfence(); /* front sees new responses /before/ we check rsp_event */ \
     (_notify) = ((RING_IDX)(__new - (_r)->sring->rsp_event) <           \
                  (RING_IDX)(__new - __old));                            \
 } while (0)
@@ -246,7 +246,7 @@ typedef struct __name##_back_ring __name##_back_ring_t
     (_work_to_do) = RING_HAS_UNCONSUMED_REQUESTS(_r);                   \
     if (_work_to_do) break;                                             \
     (_r)->sring->req_event = (_r)->req_cons + 1;                        \
-    x86_sfence();                                                       \
+    x86_mfence();                                                       \
     (_work_to_do) = RING_HAS_UNCONSUMED_REQUESTS(_r);                   \
 } while (0)
 
@@ -254,7 +254,7 @@ typedef struct __name##_back_ring __name##_back_ring_t
     (_work_to_do) = RING_HAS_UNCONSUMED_RESPONSES(_r);                  \
     if (_work_to_do) break;                                             \
     (_r)->sring->rsp_event = (_r)->rsp_cons + 1;                        \
-    x86_sfence();                                                       \
+    x86_mfence();                                                       \
     (_work_to_do) = RING_HAS_UNCONSUMED_RESPONSES(_r);                  \
 } while (0)
 
