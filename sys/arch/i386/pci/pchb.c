51ccc86d8ad9d70d11efacecf32fcaca7a0727b4
he <he>
2007-01-27 23:10:21 +0000

    Fix description of the 82443BX workaround to use "stepping" instead of "revision".

--- sys/arch/i386/pci/pchb.c
+++ sys/arch/i386/pci/pchb.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: pchb.c,v 1.63 2007/01/23 10:45:33 he Exp $	*/
+/*	$NetBSD: pchb.c,v 1.64 2007/01/27 23:10:21 he Exp $	*/
 
 /*-
  * Copyright (c) 1996, 1998, 2000 The NetBSD Foundation, Inc.
@@ -37,7 +37,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: pchb.c,v 1.63 2007/01/23 10:45:33 he Exp $");
+__KERNEL_RCSID(0, "$NetBSD: pchb.c,v 1.64 2007/01/27 23:10:21 he Exp $");
 
 #include <sys/types.h>
 #include <sys/param.h>
@@ -196,7 +196,7 @@ pchbattach(struct device *parent, struct device *self, void *aux)
 		case PCI_PRODUCT_INTEL_82443BX_NOAGP:
 		/*
 		 * http://www.intel.com/design/chipsets/specupdt/290639.htm
-		 * says this bug is fixed in revisions >= C0 (erratum 11),
+		 * says this bug is fixed in steppings >= C0 (erratum 11),
 		 * so don't tweak the bits in that case.
 		 */
 			if (!(PCI_REVISION(pa->pa_class) >= 0x03)) {
