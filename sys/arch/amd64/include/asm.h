e87b0c443ab48056807166036648ba119a607eb0
ad <ad@NetBSD.org>
2007-02-06 16:58:01 +0000

    Remove now unused SPLLOWER() macro.

--- sys/arch/amd64/include/asm.h
+++ sys/arch/amd64/include/asm.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: asm.h,v 1.6.2.2 2007/01/27 07:09:02 ad Exp $	*/
+/*	$NetBSD: asm.h,v 1.6.2.3 2007/02/06 16:58:01 ad Exp $	*/
 
 /*-
  * Copyright (c) 1990 The Regents of the University of California.
@@ -123,26 +123,5 @@
 	.stabs msg,30,0,0,0 ;						\
 	.stabs __STRING(sym),1,0,0,0
 #endif /* __STDC__ */
-
-/*
- * Assembley equivalent of spllower().  Label contains the label to jump to
- * if we need to fire off pending interrupts (e.g. _C_LABEL(Xspllower)).
- *
- * On entry %rdi = new SPL.
- */
-#define	SPLLOWER(label)						\
-	movq		CPUVAR(SELF), %r9 ;			\
-	cmpl		CPU_INFO_ILEVEL(%r9), %edi ;		\
-	jae		99f ;					\
-	movl		CPU_INFO_IUNMASK(%r9,%rdi,4), %esi ;	\
-	pushfq		;					\
-	popq		%rax ;					\
-	cli		;					\
-	testl		CPU_INFO_IPENDING(%r9), %esi ;		\
-	jnz		label ;					\
-	movl		%edi, CPU_INFO_ILEVEL(%r9) ;		\
-	pushq		%rax ;					\
-	popfq		;					\
-99:
 
 #endif /* !_AMD64_ASM_H_ */
