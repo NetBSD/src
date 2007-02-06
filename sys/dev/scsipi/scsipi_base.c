c4ec59a7bff19d17d624e6e065dde99555a13d6b
ad <ad@NetBSD.org>
2007-02-06 19:14:40 +0000

    Now that kthreads always run with kernel priority, don't rely on preempt() to actually yield the CPU.

--- sys/dev/scsipi/scsipi_base.c
+++ sys/dev/scsipi/scsipi_base.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: scsipi_base.c,v 1.135.8.5 2007/01/30 13:51:39 ad Exp $	*/
+/*	$NetBSD: scsipi_base.c,v 1.135.8.6 2007/02/06 19:14:40 ad Exp $	*/
 
 /*-
  * Copyright (c) 1998, 1999, 2000, 2002, 2003, 2004 The NetBSD Foundation, Inc.
@@ -38,7 +38,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: scsipi_base.c,v 1.135.8.5 2007/01/30 13:51:39 ad Exp $");
+__KERNEL_RCSID(0, "$NetBSD: scsipi_base.c,v 1.135.8.6 2007/02/06 19:14:40 ad Exp $");
 
 #include "opt_scsi.h"
 
@@ -2081,7 +2081,7 @@ scsipi_completion_thread(void *arg)
 			scsipi_channel_thaw(chan, 1);
 			splx(s);
 			if (chan->chan_tflags & SCSIPI_CHANT_GROWRES)
-				preempt();
+				kpause("scsizzz", FALSE, hz/10, NULL);
 			continue;
 		}
 		if (chan->chan_tflags & SCSIPI_CHANT_KICK) {
