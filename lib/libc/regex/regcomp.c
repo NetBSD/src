80ec9db7d1e21850f88cc06667ab0f4b6234581f
junyoung <junyoung@NetBSD.org>
2007-02-09 23:44:18 +0000

    Rearrange headers for better portability.

--- lib/libc/regex/regcomp.c
+++ lib/libc/regex/regcomp.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: regcomp.c,v 1.27 2007/02/08 05:44:18 junyoung Exp $	*/
+/*	$NetBSD: regcomp.c,v 1.28 2007/02/09 23:44:18 junyoung Exp $	*/
 
 /*-
  * Copyright (c) 1992, 1993, 1994
@@ -76,7 +76,7 @@
 #if 0
 static char sccsid[] = "@(#)regcomp.c	8.5 (Berkeley) 3/20/94";
 #else
-__RCSID("$NetBSD: regcomp.c,v 1.27 2007/02/08 05:44:18 junyoung Exp $");
+__RCSID("$NetBSD: regcomp.c,v 1.28 2007/02/09 23:44:18 junyoung Exp $");
 #endif
 #endif /* LIBC_SCCS and not lint */
 
@@ -86,10 +86,10 @@ __RCSID("$NetBSD: regcomp.c,v 1.27 2007/02/08 05:44:18 junyoung Exp $");
 #include <assert.h>
 #include <ctype.h>
 #include <limits.h>
-#include <regex.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <regex.h>
 
 #ifdef __weak_alias
 __weak_alias(regcomp,_regcomp)
