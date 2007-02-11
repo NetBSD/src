0fe1b122f7537fde6dac3beeb87bc9e3e80a6411
tron <tron@NetBSD.org>
2007-02-11 13:22:35 +0000

    Pull up following revision(s) (requested by adrianp in ticket #1664): 	dist/bind/lib/dns/message.c: revision 1.2 Initialize rdataset.

--- dist/bind/lib/dns/message.c
+++ dist/bind/lib/dns/message.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: message.c,v 1.1.1.2.4.1 2007/02/10 19:20:53 tron Exp $	*/
+/*	$NetBSD: message.c,v 1.1.1.2.4.2 2007/02/11 13:22:35 tron Exp $	*/
 
 /*
  * Copyright (C) 2004, 2005  Internet Systems Consortium, Inc. ("ISC")
@@ -1109,7 +1109,7 @@ getsection(isc_buffer_t *source, dns_message_t *msg, dns_decompress_t *dctx,
 	dns_name_t *name;
 	dns_name_t *name2;
 	dns_offsets_t *offsets;
-	dns_rdataset_t *rdataset;
+	dns_rdataset_t *rdataset = NULL;
 	dns_rdatalist_t *rdatalist;
 	isc_result_t result;
 	dns_rdatatype_t rdtype, covers;
