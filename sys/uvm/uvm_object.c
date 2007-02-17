84d2fddf652f125db9777ec6460d42af354a2f7e
rmind <rmind@NetBSD.org>
2007-02-17 20:45:36 +0000

    Mention rmind@ as an author in the license. No functional change.

--- sys/uvm/uvm_object.c
+++ sys/uvm/uvm_object.c
@@ -1,8 +1,11 @@
-/*	$NetBSD: uvm_object.c,v 1.2 2006/10/12 10:13:35 yamt Exp $	*/
+/*	$NetBSD: uvm_object.c,v 1.3 2007/02/17 20:45:36 rmind Exp $	*/
 
 /*
  * Copyright (c) 2006 The NetBSD Foundation, Inc.
  * All rights reserved.
+ *
+ * This code is derived from software contributed to The NetBSD Foundation
+ * by Mindaugas Rasiukevicius.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -42,7 +45,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: uvm_object.c,v 1.2 2006/10/12 10:13:35 yamt Exp $");
+__KERNEL_RCSID(0, "$NetBSD: uvm_object.c,v 1.3 2007/02/17 20:45:36 rmind Exp $");
 
 #include "opt_uvmhist.h"
 
