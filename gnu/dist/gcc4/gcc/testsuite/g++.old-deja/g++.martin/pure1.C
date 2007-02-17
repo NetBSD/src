b0f15430d4a826b7b51f5285c8dc3961dbd237e8
daniel <daniel@NetBSD.org>
2007-02-17 00:28:26 +0000

    Add an opencrypto provider for the AES xcrypt instructions found on VIA C5P and later cores (also known as 'ACE', which is part of the VIA PadLock security engine). Ported from OpenBSD.
    Reviewed on tech-crypto and port-i386, no objections to commiting this.

--- gnu/dist/gcc4/gcc/testsuite/g++.old-deja/g++.martin/pure1.C
+++
@@ -1,8 +0,0 @@
-// { dg-do assemble  }
-class A
-{
-  public:
-    virtual void f(void) = 0; // pure virtual function.
-     A() {f();}               // { dg-error "" } called in a constructor
-    ~A() {f();}               // { dg-error "" } called in a destructor
-};
--- gnu/dist/gcc4/gcc/testsuite/gcc.dg/pch/macro-3.c
+++
@@ -1,8 +0,0 @@
-#define DEFINED_FUNC_2(x) (3 + (x))
-
-#include "macro-3.h"
-
-int main(void) 
-{
-  return DEFINED_FUNC (1) - DEFINED_FUNC_2 (-1);
-}
