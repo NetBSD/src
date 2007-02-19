e46149105d298663602ad167091b118325b627b3
tron <tron@NetBSD.org>
2007-02-19 13:54:29 +0000

    Pull up following revision(s) (requested by christos in ticket #456): 	common/lib/libc/hash/sha2/sha2.c: revision 1.3 make SHA512_Last weak.

--- common/lib/libc/hash/sha2/sha2.c
+++ common/lib/libc/hash/sha2/sha2.c
@@ -1,4 +1,4 @@
-/* $NetBSD: sha2.c,v 1.2 2006/11/01 11:29:08 he Exp $ */
+/* $NetBSD: sha2.c,v 1.2.4.1 2007/02/19 13:54:29 tron Exp $ */
 /*	$KAME: sha2.c,v 1.9 2003/07/20 00:28:38 itojun Exp $	*/
 
 /*
@@ -39,14 +39,14 @@
 #include <sys/cdefs.h>
 
 #if defined(_KERNEL) || defined(_STANDALONE)
-__KERNEL_RCSID(0, "$NetBSD: sha2.c,v 1.2 2006/11/01 11:29:08 he Exp $");
+__KERNEL_RCSID(0, "$NetBSD: sha2.c,v 1.2.4.1 2007/02/19 13:54:29 tron Exp $");
 
 #include <lib/libkern/libkern.h>
 
 #else
 
 #if defined(LIBC_SCCS) && !defined(lint)
-__RCSID("$NetBSD: sha2.c,v 1.2 2006/11/01 11:29:08 he Exp $");
+__RCSID("$NetBSD: sha2.c,v 1.2.4.1 2007/02/19 13:54:29 tron Exp $");
 #endif /* LIBC_SCCS and not lint */
 
 #include "namespace.h"
@@ -339,6 +339,7 @@ __weak_alias(SHA384_Transform,_SHA384_Transform)
 __weak_alias(SHA512_Init,_SHA512_Init) 
 __weak_alias(SHA512_Update,_SHA512_Update)
 __weak_alias(SHA512_Final,_SHA512_Final)
+__weak_alias(SHA512_Last,_SHA512_Last)
 __weak_alias(SHA512_Transform,_SHA512_Transform)
 #endif
 
