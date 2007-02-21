dd5083a8a27865bf9339b27fba61d2673e7344ff
mrg <mrg>
2007-02-21 20:41:23 +0000

    add a pair of new bus_dma(9) functions: 	int _bus_dmatag_subregion(bus_dma_tag_t tag, 				  bus_addr_t min_addr, 				  bus_addr_t max_addr, 				  bus_dma_tag_t *newtag, 				  int flags) 	void _bus_dmatag_destroy(bus_dma_tag_t tag)
    that allow a (normally broken/limited) device to restrict the bus address
    range it can talk to.  this is used by bce(4) to limit DMA addresses to
    1GB range, the maximum the chip can address.
    
    all this is from Yorick Hardy <yhardy@uj.ac.za> with input from several
    people on tech-kern.
    
    XXX: bus_dma(9) needs an update still.

--- sys/arch/arm/include/bus.h
+++ sys/arch/arm/include/bus.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: bus.h,v 1.16 2006/03/01 12:38:11 yamt Exp $	*/
+/*	$NetBSD: bus.h,v 1.17 2007/02/21 20:41:23 mrg Exp $	*/
 
 /*-
  * Copyright (c) 1996, 1997, 1998, 2001 The NetBSD Foundation, Inc.
@@ -901,6 +901,9 @@ do {									\
 	(*(t)->_dmamem_unmap)((t), (k), (s))
 #define	bus_dmamem_mmap(t, sg, n, o, p, f)			\
 	(*(t)->_dmamem_mmap)((t), (sg), (n), (o), (p), (f))
+
+#define bus_dmatag_subregion(t, mna, mxa, nt, f) EOPNOTSUPP
+#define bus_dmatag_destroy(t)
 
 /*
  *	bus_dmamap_t
--- sys/arch/sparc64/include/bus.h
+++ sys/arch/sparc64/include/bus.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: bus.h,v 1.55 2006/11/16 01:32:39 christos Exp $	*/
+/*	$NetBSD: bus.h,v 1.56 2007/02/21 20:41:26 mrg Exp $	*/
 
 /*-
  * Copyright (c) 1996, 1997, 1998, 2001 The NetBSD Foundation, Inc.
@@ -1525,6 +1525,9 @@ struct sparc_bus_dma_tag {
 	(*(t)->_dmamem_unmap)((t), (k), (s))
 #define	bus_dmamem_mmap(t, sg, n, o, p, f)			\
 	(*(t)->_dmamem_mmap)((t), (sg), (n), (o), (p), (f))
+
+#define bus_dmatag_subregion(t, mna, mxa, nt, f) EOPNOTSUPP
+#define bus_dmatag_destroy(t)
 
 /*
  *	bus_dmamap_t
