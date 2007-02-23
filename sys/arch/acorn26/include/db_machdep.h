261eaa231aea869ee3f1f14db1c49ddeec9ebc21
he <he@NetBSD.org>
2007-02-23 09:49:48 +0000

    Adapt to bool conversion of the DDB functions.

--- sys/arch/acorn26/include/db_machdep.h
+++ sys/arch/acorn26/include/db_machdep.h
@@ -1,4 +1,4 @@
-/* $NetBSD: db_machdep.h,v 1.5 2007/02/21 22:59:36 thorpej Exp $ */
+/* $NetBSD: db_machdep.h,v 1.6 2007/02/23 09:49:48 he Exp $ */
 
 #include <arm/db_machdep.h>
 
@@ -7,9 +7,9 @@
 #define DB_ELF_SYMBOLS
 #define DB_ELFSIZE 32
 
-void db_show_panic_cmd	__P((db_expr_t, int, db_expr_t, const char *));
-void db_show_frame_cmd	__P((db_expr_t, int, db_expr_t, const char *));
-void db_bus_write_cmd	__P((db_expr_t, int, db_expr_t, const char *));
-void db_irqstat_cmd	__P((db_expr_t, int, db_expr_t, const char *));
+void db_show_panic_cmd	__P((db_expr_t, bool, db_expr_t, const char *));
+void db_show_frame_cmd	__P((db_expr_t, bool, db_expr_t, const char *));
+void db_bus_write_cmd	__P((db_expr_t, bool, db_expr_t, const char *));
+void db_irqstat_cmd	__P((db_expr_t, bool, db_expr_t, const char *));
 
 extern volatile bool db_validating, db_faulted;
