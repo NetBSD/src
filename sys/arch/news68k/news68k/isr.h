b1bb78fd78f2877938d02d1ee17b9ea79e6555c0
tsutsui <tsutsui@NetBSD.org>
2007-03-03 07:36:11 +0000

    - move netintr() from isr.c to softintr.c and make it static - remove redundant initialization in ipl2psl_table.

--- sys/arch/news68k/news68k/isr.h
+++ sys/arch/news68k/news68k/isr.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: isr.h,v 1.5 2007/02/16 21:52:47 tsutsui Exp $	*/
+/*	$NetBSD: isr.h,v 1.6 2007/03/03 07:36:11 tsutsui Exp $	*/
 
 /*-
  * Copyright (c) 1996 The NetBSD Foundation, Inc.
@@ -85,4 +85,3 @@ void isrunlink_vectored(int);
 void isrdispatch_autovec(int);
 void isrdispatch_vectored(int, int, void *);
 void isrlink_custom(int, void *);
-void netintr(void);
