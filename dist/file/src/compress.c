e442e13a6a044dfbac2f409503a0e4a842c13d73
pooka <pooka@NetBSD.org>
2007-03-04 15:25:10 +0000

    squelch gcc warnings

--- dist/file/src/compress.c
+++ dist/file/src/compress.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: compress.c,v 1.5 2007/03/04 15:22:09 pooka Exp $	*/
+/*	$NetBSD: compress.c,v 1.6 2007/03/04 15:25:10 pooka Exp $	*/
 
 /*
  * Copyright (c) Ian F. Darwin 1986-1995.
@@ -60,7 +60,7 @@
 #if 0
 FILE_RCSID("@(#)$File: compress.c,v 1.50 2007/03/01 22:14:54 christos Exp $")
 #else
-__RCSID("$NetBSD: compress.c,v 1.5 2007/03/04 15:22:09 pooka Exp $");
+__RCSID("$NetBSD: compress.c,v 1.6 2007/03/04 15:25:10 pooka Exp $");
 #endif
 #endif
 
@@ -176,12 +176,12 @@ sread(int fd, void *buf, size_t n, int canbepipe)
 		goto nocheck;
 
 #ifdef FIONREAD
-	if (canbepipe && (ioctl(fd, FIONREAD, &t) == -1) || (t == 0)) {
+	if ((canbepipe && (ioctl(fd, FIONREAD, &t) == -1)) || (t == 0)) {
 #ifdef FD_ZERO
 		for (cnt = 0;; cnt++) {
 			fd_set check;
 			struct timeval tout = {0, 100 * 1000};
-			int rv;
+			int selrv;
 
 			FD_ZERO(&check);
 			FD_SET(fd, &check);
@@ -190,11 +190,11 @@ sread(int fd, void *buf, size_t n, int canbepipe)
 			 * Avoid soft deadlock: do not read if there
 			 * is nothing to read from sockets and pipes.
 			 */
-			rv = select(fd + 1, &check, NULL, NULL, &tout);
-			if (rv == -1) {
+			selrv = select(fd + 1, &check, NULL, NULL, &tout);
+			if (selrv == -1) {
 				if (errno == EINTR || errno == EAGAIN)
 					continue;
-			} else if (rv == 0 && cnt >= 5) {
+			} else if (selrv == 0 && cnt >= 5) {
 				return 0;
 			} else
 				break;
