41d4c3ca95980a18284ffd49d290c2bfd672694f
ad <ad@NetBSD.org>
2007-03-04 20:59:00 +0000

    lwp_userret: if the process is exiting, don't bother looking for signals.

--- sys/kern/kern_lwp.c
+++ sys/kern/kern_lwp.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: kern_lwp.c,v 1.60 2007/02/26 09:20:53 yamt Exp $	*/
+/*	$NetBSD: kern_lwp.c,v 1.61 2007/03/04 20:59:00 ad Exp $	*/
 
 /*-
  * Copyright (c) 2001, 2006, 2007 The NetBSD Foundation, Inc.
@@ -204,7 +204,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: kern_lwp.c,v 1.60 2007/02/26 09:20:53 yamt Exp $");
+__KERNEL_RCSID(0, "$NetBSD: kern_lwp.c,v 1.61 2007/03/04 20:59:00 ad Exp $");
 
 #include "opt_multiprocessor.h"
 #include "opt_lockdebug.h"
@@ -1109,10 +1109,11 @@ lwp_userret(struct lwp *l)
 	while ((l->l_flag & LW_USERRET) != 0) {
 		/*
 		 * Process pending signals first, unless the process
-		 * is dumping core, where we will instead enter the 
-		 * L_WSUSPEND case below.
+		 * is dumping core or exiting, where we will instead
+		 * enter the L_WSUSPEND case below.
 		 */
-		if ((l->l_flag & (LW_PENDSIG | LW_WCORE)) == LW_PENDSIG) {
+		if ((l->l_flag & (LW_PENDSIG | LW_WCORE | LW_WEXIT)) ==
+		    LW_PENDSIG) {
 			KERNEL_LOCK(1, l);	/* XXXSMP pool_put() below */
 			mutex_enter(&p->p_smutex);
 			while ((sig = issignal(l)) != 0)
