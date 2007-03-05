f41cb0f1b1cd4faf6672258b33eedd8036cafbc9
he <he@NetBSD.org>
2007-03-05 22:36:16 +0000

    Use mtod(m, char *) when you want to do pointer arithmetic on the result.

--- sys/arch/prep/isa/isadma_machdep.c
+++ sys/arch/prep/isa/isadma_machdep.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: isadma_machdep.c,v 1.11 2007/03/04 06:00:38 christos Exp $	*/
+/*	$NetBSD: isadma_machdep.c,v 1.12 2007/03/05 22:36:16 he Exp $	*/
 
 /*-
  * Copyright (c) 1996, 1997, 1998 The NetBSD Foundation, Inc.
@@ -38,7 +38,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: isadma_machdep.c,v 1.11 2007/03/04 06:00:38 christos Exp $");
+__KERNEL_RCSID(0, "$NetBSD: isadma_machdep.c,v 1.12 2007/03/05 22:36:16 he Exp $");
 
 #define ISA_DMA_STATS
 
@@ -554,7 +554,7 @@ _isa_bus_dmamap_sync(bus_dma_tag_t t, bus_dmamap_t map, bus_addr_t offset,
 				minlen = len < m->m_len - moff ?
 				    len : m->m_len - moff;
 
-				memcpy(mtod(m, void *) + moff,
+				memcpy(mtod(m, char *) + moff,
 				    (char *)cookie->id_bouncebuf + offset,
 				    minlen);
 
