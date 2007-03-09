eb98e5887be6f7e6749f0cccb52d50de1486429b
ad <ad@NetBSD.org>
2007-03-09 14:30:45 +0000

    If I686_LIBC is defined, make syscalls using the 'sysenter' instruction.

--- lib/libc/arch/i386/SYS.h
+++ lib/libc/arch/i386/SYS.h
@@ -1,3 +1,41 @@
+/*	$NetBSD: SYS.h,v 1.21 2007/03/09 14:30:45 ad Exp $	*/
+
+/*-
+ * Copyright (c) 2007 The NetBSD Foundation, Inc.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to The NetBSD Foundation
+ * by Andrew Doran.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed by the NetBSD
+ *	Foundation, Inc. and its contributors.
+ * 4. Neither the name of The NetBSD Foundation nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
 /*-
  * Copyright (c) 1990 The Regents of the University of California.
  * All rights reserved.
@@ -30,17 +68,33 @@
  * SUCH DAMAGE.
  *
  *	from: @(#)SYS.h	5.5 (Berkeley) 5/7/91
- *	$NetBSD: SYS.h,v 1.20 2003/08/07 16:42:06 agc Exp $
  */
 
 #include <machine/asm.h>
 #include <sys/syscall.h>
 
 #ifdef __STDC__
-#define SYSTRAP(x)	movl $(SYS_ ## x),%eax; int $0x80
+#define _SYSNAM(x)	$(SYS_ ## x)
 #else
-#define SYSTRAP(x)	movl $(SYS_/**/x),%eax; int $0x80
+#define	_SYSNAM(x)	$(SYS_/**/x)
 #endif
+
+#define OSYSTRAP(x)			\
+	movl _SYSNAM(x),%eax		;\
+	int $0x80
+
+#ifdef I686_LIBC
+#define SYSTRAP(x)			\
+	pushl	%ebx			;\
+	movl	_SYSNAM(x),%eax		;\
+	movl	$123f,%edx		;\
+	movl	%esp,%ecx		;\
+	sysenter			;\
+123:	movl	%ebx,%edx		;\
+	popl	%ebx
+#else	/* I686_LIBC */
+#define SYSTRAP(x)	OSYSTRAP(x)
+#endif	/* I686_LIBC */
 
 #ifdef __ELF__
 #define CERROR		_C_LABEL(__cerror)
