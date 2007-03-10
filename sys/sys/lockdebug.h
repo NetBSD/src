64e54fbb704480bd090cbeb3d0e1d7796cef14dc
ad <ad@NetBSD.org>
2007-03-10 15:56:21 +0000

    lockdebug:
    - Initialize on the first allocation.
    - Handle overflow better. PR kern/35723.

--- sys/sys/lockdebug.h
+++ sys/sys/lockdebug.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: lockdebug.h,v 1.2 2007/02/09 21:55:37 ad Exp $	*/
+/*	$NetBSD: lockdebug.h,v 1.3 2007/03/10 15:56:21 ad Exp $	*/
 
 /*-
  * Copyright (c) 2006, 2007 The NetBSD Foundation, Inc.
@@ -62,7 +62,6 @@ void	lockdebug_lock_print(void *, void (*)(const char *, ...));
 
 #ifdef LOCKDEBUG
 
-void	lockdebug_init(void);
 u_int	lockdebug_alloc(volatile void *, lockops_t *);
 void	lockdebug_free(volatile void *, u_int);
 void	lockdebug_wantlock(u_int, uintptr_t, int);
