ae8aa54a163538f843b58ae3b775ddf0efa1ec3f
bouyer <bouyer@NetBSD.org>
2007-03-10 11:50:05 +0000

    Pull up following revision(s) (requested by kent in ticket #1678): 	sys/dev/audio.c: revision 1.221 return information about playing buffer, not recording bufer, for AUDIO_WSEEK. PR#35171

--- sys/dev/audio.c
+++ sys/dev/audio.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: audio.c,v 1.192.4.2.2.2 2006/04/19 21:01:00 tron Exp $	*/
+/*	$NetBSD: audio.c,v 1.192.4.2.2.3 2007/03/10 11:50:05 bouyer Exp $	*/
 
 /*
  * Copyright (c) 1991-1993 Regents of the University of California.
@@ -61,7 +61,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: audio.c,v 1.192.4.2.2.2 2006/04/19 21:01:00 tron Exp $");
+__KERNEL_RCSID(0, "$NetBSD: audio.c,v 1.192.4.2.2.3 2007/03/10 11:50:05 bouyer Exp $");
 
 #include "audio.h"
 #if NAUDIO > 0
@@ -2080,7 +2080,7 @@ audio_ioctl(struct audio_softc *sc, u_long cmd, caddr_t addr, int flag,
 	 * sample of what we write next?
 	 */
 	case AUDIO_WSEEK:
-		*(u_long *)addr = audio_stream_get_used(sc->sc_rustream);
+		*(u_long *)addr = audio_stream_get_used(sc->sc_pustream);
 		break;
 
 	case AUDIO_SETINFO:
