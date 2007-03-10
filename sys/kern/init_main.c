a2c56f618810b2a8ef6104ee2aef1b55cdc34378
ad <ad@NetBSD.org>
2007-03-10 15:56:21 +0000

    lockdebug:
    - Initialize on the first allocation.
    - Handle overflow better. PR kern/35723.

--- sys/kern/init_main.c
+++ sys/kern/init_main.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: init_main.c,v 1.298 2007/03/09 14:11:23 ad Exp $	*/
+/*	$NetBSD: init_main.c,v 1.299 2007/03/10 15:56:21 ad Exp $	*/
 
 /*
  * Copyright (c) 1982, 1986, 1989, 1991, 1992, 1993
@@ -71,7 +71,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: init_main.c,v 1.298 2007/03/09 14:11:23 ad Exp $");
+__KERNEL_RCSID(0, "$NetBSD: init_main.c,v 1.299 2007/03/10 15:56:21 ad Exp $");
 
 #include "opt_ipsec.h"
 #include "opt_kcont.h"
@@ -156,7 +156,6 @@ __KERNEL_RCSID(0, "$NetBSD: init_main.c,v 1.298 2007/03/09 14:11:23 ad Exp $");
 #ifdef KTRACE
 #include <sys/ktrace.h>
 #endif
-#include <sys/lockdebug.h>
 #include <sys/debug.h>
 #include <sys/kauth.h>
 #include <net80211/ieee80211_netbsd.h>
@@ -259,10 +258,6 @@ main(void)
 	l->l_cpu = curcpu();
 	l->l_proc = &proc0;
 	l->l_lid = 1;
-
-#ifdef LOCKDEBUG
-	lockdebug_init();
-#endif
 
 	/*
 	 * Attempt to find console and initialize
