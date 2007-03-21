d1bb43f6cd39e4836b46370a9dbda48e3895b0bb
dyoung <dyoung>
2007-03-21 03:18:08 +0000

    Make all debug messages use GRE_DPRINTF().  Get rid of a redundant if_ierrors++.  Change (type *)0 to NULL.  Get rid of unnecessary casts to void *.

--- sys/net/if_gre.c
+++ sys/net/if_gre.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: if_gre.c,v 1.89 2007/03/21 01:56:05 dyoung Exp $ */
+/*	$NetBSD: if_gre.c,v 1.90 2007/03/21 03:18:08 dyoung Exp $ */
 
 /*
  * Copyright (c) 1998 The NetBSD Foundation, Inc.
@@ -48,7 +48,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: if_gre.c,v 1.89 2007/03/21 01:56:05 dyoung Exp $");
+__KERNEL_RCSID(0, "$NetBSD: if_gre.c,v 1.90 2007/03/21 03:18:08 dyoung Exp $");
 
 #include "opt_gre.h"
 #include "opt_inet.h"
@@ -299,7 +299,7 @@ gre_socreate1(struct gre_softc *sc, struct lwp *l, struct gre_soparm *sp,
 
 	so = *sop;
 
-	gre_upcall_add(so, (void *)sc);
+	gre_upcall_add(so, sc);
 	if ((m = gre_getsockmbuf(so)) == NULL) {
 		rc = ENOBUFS;
 		goto out;
@@ -406,7 +406,7 @@ gre_thread1(struct gre_softc *sc, struct lwp *l)
 
 			if (sc->sc_fp != NULL) {
 				so = (struct socket *)sc->sc_fp->f_data;
-				gre_upcall_add(so, (void *)sc);
+				gre_upcall_add(so, sc);
 				sp = sc->sc_soparm;
 				FILE_USE(sp.sp_fp);
 			} else if (gre_socreate1(sc, l, &sp, &so) != 0)
@@ -442,7 +442,6 @@ gre_thread1(struct gre_softc *sc, struct lwp *l)
 			if (gre_input3(sc, m, 0, gh) == 0) {
 				GRE_DPRINTF(sc, "%s: dropping unsupported\n",
 				    __func__);
-				ifp->if_ierrors++;
 				m_freem(m);
 			}
 		}
@@ -558,7 +557,7 @@ gre_input3(struct gre_softc *sc, struct mbuf *m, int hlen,
 		break;
 #endif
 	default:	   /* others not yet supported */
-		printf("%s: unhandled ethertype 0x%04x\n", __func__,
+		GRE_DPRINTF(sc, "%s: unhandled ethertype 0x%04x\n", __func__,
 		    ntohs(gh->ptype));
 		sc->sc_if.if_noproto++;
 		return 0;
@@ -670,11 +669,11 @@ gre_output(struct ifnet *ifp, struct mbuf *m, const struct sockaddr *dst,
 		}
 		/* XXX Assuming that ip does not dangle after
 		 * M_PREPEND.  In practice, that's true, but
-		 * that's in M_PREPEND's contract.
+		 * that's not in M_PREPEND's contract.
 		 */
 		memmove(mtod(m, void *), ip, sizeof(*ip));
 		ip = mtod(m, struct ip *);
-		memcpy((void *)(ip + 1), &mob_h, (unsigned)msiz);
+		memcpy(ip + 1, &mob_h, (size_t)msiz);
 		ip->ip_len = htons(ntohs(ip->ip_len) + msiz);
 		break;
 	case IPPROTO_UDP:
@@ -809,8 +808,7 @@ gre_kick(struct gre_softc *sc)
 	if (sc->sc_proto == IPPROTO_UDP && (ifp->if_flags & IFF_UP) == IFF_UP &&
 	    !sc->sc_thread) {
 		sc->sc_thread = 1;
-		rc = kthread_create1(gre_thread, (void *)sc, NULL,
-		    ifp->if_xname);
+		rc = kthread_create1(gre_thread, sc, NULL, ifp->if_xname);
 		if (rc != 0)
 			gre_stop(&sc->sc_thread);
 		return rc;
@@ -826,8 +824,7 @@ gre_getname(struct socket *so, int req, struct mbuf *nam, struct lwp *l)
 	int s, error;
 
 	s = splsoftnet();
-	error = (*so->so_proto->pr_usrreq)(so, req, (struct mbuf *)0,
-	    nam, (struct mbuf *)0, l);
+	error = (*so->so_proto->pr_usrreq)(so, req, NULL, nam, NULL, l);
 	splx(s);
 	return error;
 }
@@ -1156,20 +1153,14 @@ gre_compute_route(struct gre_softc *sc)
 	ro->ro_dst.sa_family = AF_INET;
 	ro->ro_dst.sa_len = sizeof(ro->ro_dst);
 
-#ifdef DIAGNOSTIC
-	printf("%s: searching for a route to %s", sc->sc_if.if_xname,
-	    inet_ntoa(satocsin(rtcache_getdst(ro))->sin_addr));
-#endif
-
 	rtcache_init(ro);
 
 	if (ro->ro_rt == NULL || ro->ro_rt->rt_ifp->if_softc == sc) {
-#ifdef DIAGNOSTIC
-		if (ro->ro_rt == NULL)
-			printf(" - no route found!\n");
-		else
-			printf(" - route loops back to ourself!\n");
-#endif
+		GRE_DPRINTF(sc, "%s: route to %s %s\n", sc->sc_if.if_xname,
+		    inet_ntoa(satocsin(rtcache_getdst(ro))->sin_addr),
+		    (ro->ro_rt == NULL)
+		        ?  "does not exist"
+			: "loops back to ourself");
 		rtcache_free(ro);
 		return EADDRNOTAVAIL;
 	}
