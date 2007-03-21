587bcee58e295bb2e73b176a94ee736529cddb9f
tsutsui <tsutsui@NetBSD.org>
2007-03-21 10:56:26 +0000

    Apply MI softintr(9) patch before it becomes rotten. Untested. See also: http://mail-index.netbsd.org/port-atari/2007/03/07/0000.html

--- sys/arch/atari/include/types.h
+++ sys/arch/atari/include/types.h
@@ -1,8 +1,9 @@
-/*	$NetBSD: types.h,v 1.8 2002/02/28 03:17:32 simonb Exp $	*/
+/*	$NetBSD: types.h,v 1.9 2007/03/21 10:56:27 tsutsui Exp $	*/
 
 #ifndef _MACHINE_TYPES_H_
 #define	_MACHINE_TYPES_H_
 
 #include <m68k/types.h>
+#define __HAVE_GENERIC_SOFT_INTERRUPTS
 
 #endif
