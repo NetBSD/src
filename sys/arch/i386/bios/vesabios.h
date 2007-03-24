ef34b019edb3a146b382449f3f4e19231d1e45af
reinoud <reinoud@NetBSD.org>
2007-03-24 00:07:17 +0000

    Implement hardware scrolling in vesafb by utilising the whole frame buffer memory and scrolling trough it. If the hardware is not capable it will disable hardware scrolling.
    For 640x480 at 8 bpp the speedup is around 4 times, at 1280x1280 at 32 bpp
    the speedup is around 9.4 times.
    
    Checked and OK'd by Jared McNeill. Thanks go to Jared and Michael Lorenz
    for their tips and vcons knowledge!

--- sys/arch/i386/bios/vesabios.h
+++ sys/arch/i386/bios/vesabios.h
@@ -1,4 +1,4 @@
-/* $NetBSD: vesabios.h,v 1.6 2006/06/20 20:30:22 drochner Exp $ */
+/* $NetBSD: vesabios.h,v 1.7 2007/03/24 00:07:17 reinoud Exp $ */
 
 /*
  * Copyright (c) 2002, 2005
@@ -32,4 +32,5 @@ struct vesabiosdev_attach_args {
 	const char *vbaa_type;
 	int *vbaa_modes;
 	int vbaa_nmodes;
+	int vbaa_vbeversion;
 };
