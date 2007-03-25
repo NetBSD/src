3fb704b4f1fdfd3fcfb484455f64555992578e28
degroote <degroote@NetBSD.org>
2007-03-25 12:46:42 +0000

    Make an exact match when we are looking for a cached sp for an unconnected socket. If we don't make an exact match, we may use a cached rule which has lower priority than a rule that would otherwise have matched the packet.
    Code submitted by Karl Knutsson in PR/36051

--- sys/netipsec/ipsec.c
+++ sys/netipsec/ipsec.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: ipsec.c,v 1.28 2007/03/04 21:17:54 degroote Exp $	*/
+/*	$NetBSD: ipsec.c,v 1.29 2007/03/25 12:46:42 degroote Exp $	*/
 /*	$FreeBSD: /usr/local/www/cvsroot/FreeBSD/src/sys/netipsec/ipsec.c,v 1.2.2.2 2003/07/01 01:38:13 sam Exp $	*/
 /*	$KAME: ipsec.c,v 1.103 2001/05/24 07:14:18 sakane Exp $	*/
 
@@ -32,7 +32,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: ipsec.c,v 1.28 2007/03/04 21:17:54 degroote Exp $");
+__KERNEL_RCSID(0, "$NetBSD: ipsec.c,v 1.29 2007/03/25 12:46:42 degroote Exp $");
 
 /*
  * IPsec controller part.
@@ -291,13 +291,16 @@ ipsec_checkpcbcache(struct mbuf *m, struct inpcbpolicy *pcbsp, int dir)
 			return NULL;
 		if (ipsec_setspidx(m, &spidx, 1) != 0)
 			return NULL;
-		if (bcmp(&pcbsp->sp_cache[dir].cacheidx, &spidx,
-			 sizeof(spidx))) {
-			if (!key_cmpspidx_withmask(&pcbsp->sp_cache[dir].cachesp->spidx,
-				&spidx))
-				return NULL;
-			pcbsp->sp_cache[dir].cacheidx = spidx;
-		}
+
+		/*
+		 * We have to make an exact match here since the cached rule
+		 * might have lower priority than a rule that would otherwise
+		 * have matched the packet. 
+		 */
+
+		if (bcmp(&pcbsp->sp_cache[dir].cacheidx, &spidx, sizeof(spidx))) 
+			return NULL;
+		
 	} else {
 		/*
 		 * The pcb is connected, and the L4 code is sure that:
