626c841bf4923e16c95d3c7e0d7a20ae258a99f2
jmcneill <jmcneill@NetBSD.org>
2007-03-29 11:42:39 +0000

    Fix include paths, mgadrm compiles now.

--- sys/dev/pci/drm/mga_irq.c
+++ sys/dev/pci/drm/mga_irq.c
@@ -36,10 +36,10 @@
 __FBSDID("$FreeBSD: src/sys/dev/drm/mga_irq.c,v 1.6 2005/11/28 23:13:53 anholt Exp $");
 */
 
-#include "drmP.h"
-#include "drm.h"
-#include "mga_drm.h"
-#include "mga_drv.h"
+#include <dev/drm/drmP.h>
+#include <dev/drm/drm.h>
+#include <dev/pci/drm/mga_drm.h>
+#include <dev/pci/drm/mga_drv.h>
 
 irqreturn_t mga_driver_irq_handler(DRM_IRQ_ARGS)
 {
