386f16fc1785d865f0df1db4c82ed7b8b16c52c4
scw <scw@NetBSD.org>
2007-04-08 10:02:35 +0000

    Remove a test for defined(__SH5__).

--- libexec/ld.elf_so/rtld.c
+++ libexec/ld.elf_so/rtld.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: rtld.c,v 1.111 2006/11/22 21:46:36 christos Exp $	 */
+/*	$NetBSD: rtld.c,v 1.112 2007/04/08 10:02:35 scw Exp $	 */
 
 /*
  * Copyright 1996 John D. Polstra.
@@ -40,7 +40,7 @@
 
 #include <sys/cdefs.h>
 #ifndef lint
-__RCSID("$NetBSD: rtld.c,v 1.111 2006/11/22 21:46:36 christos Exp $");
+__RCSID("$NetBSD: rtld.c,v 1.112 2007/04/08 10:02:35 scw Exp $");
 #endif /* not lint */
 
 #include <err.h>
@@ -101,7 +101,7 @@ char           *__progname;
 char          **environ;
 
 #if defined(RTLD_DEBUG)
-#if !(defined(__sh__) && !defined(__SH5__))
+#ifndef __sh__
 extern Elf_Addr _GLOBAL_OFFSET_TABLE_[];
 #else  /* 32-bit SuperH */
 register Elf_Addr *_GLOBAL_OFFSET_TABLE_ asm("r12");
