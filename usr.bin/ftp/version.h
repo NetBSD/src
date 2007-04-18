8f62c9578dd500e8c77c60146dae0d85cba83ad2
lukem <lukem@NetBSD.org>
2007-04-18 01:39:04 +0000

    Clean up use of confirm() and mbort() so that the current operation is always passed in (instead of depending upon the 'mname' global). For confirm(), if the second argument is NULL print the "Continue with <cmd>" prompt.  This fixes up the the display of interrupted prompts.

--- usr.bin/ftp/version.h
+++ usr.bin/ftp/version.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: version.h,v 1.62 2007/04/17 05:52:04 lukem Exp $	*/
+/*	$NetBSD: version.h,v 1.63 2007/04/18 01:39:04 lukem Exp $	*/
 /*-
  * Copyright (c) 1999-2007 The NetBSD Foundation, Inc.
  * All rights reserved.
@@ -40,5 +40,5 @@
 #endif
 
 #ifndef FTP_VERSION
-#define	FTP_VERSION	"20070416"
+#define	FTP_VERSION	"20070418"
 #endif
