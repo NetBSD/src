6573e1ab9716c480a2bd2ea92aa5f7a5f67af19c
skrll <skrll@NetBSD.org>
2007-04-18 13:39:37 +0000

    Bump the initial stack size limit to bring in line with other ports.
    newfs_lfs doesn't core by default now.

--- sys/arch/hppa/include/vmparam.h
+++ sys/arch/hppa/include/vmparam.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: vmparam.h,v 1.9 2007/04/07 09:02:50 skrll Exp $	*/
+/*	$NetBSD: vmparam.h,v 1.10 2007/04/18 13:39:37 skrll Exp $	*/
 
 /*	$OpenBSD: vmparam.h,v 1.17 2001/09/22 18:00:09 miod Exp $	*/
 
@@ -62,7 +62,7 @@
 #define	MAXDSIZ		(USRSTACK-MAXTSIZ)	/* max data size */
 #endif
 #ifndef	DFLSSIZ
-#define	DFLSSIZ		(512*1024)		/* initial stack size limit */
+#define	DFLSSIZ		(2*1024*1024)		/* initial stack size limit */
 #endif
 #ifndef	MAXSSIZ
 #define	MAXSSIZ		(256*1024*1024)	/* max stack size */
