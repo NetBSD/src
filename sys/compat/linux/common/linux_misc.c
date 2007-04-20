229543f15808e7267dd9f7306fbef889ade8ad98
bouyer <bouyer@NetBSD.org>
2007-04-20 20:14:11 +0000

    Pull up following revision(s) (requested by njoly in ticket #581): 	sys/compat/linux/common/linux_misc.c: revision 1.168 	sys/compat/linux/arch/amd64/syscalls.master: revision 1.18 	sys/compat/linux/common/linux_types.h: revision 1.25 	sys/compat/linux/arch/amd64/linux_machdep.h: revision 1.6 	sys/compat/linux/common/linux_misc_notalpha.c: revision 1.87 Fix compat linux statfs/fstatfs syscalls on amd64. - Do not use statfs64/fstatfs64 as they have an extra size argument. - Add full 64bit linux struct statfs support.

--- sys/compat/linux/common/linux_misc.c
+++ sys/compat/linux/common/linux_misc.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: linux_misc.c,v 1.165 2006/11/16 01:32:42 christos Exp $	*/
+/*	$NetBSD: linux_misc.c,v 1.165.2.1 2007/04/20 20:14:11 bouyer Exp $	*/
 
 /*-
  * Copyright (c) 1995, 1998, 1999 The NetBSD Foundation, Inc.
@@ -64,7 +64,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: linux_misc.c,v 1.165 2006/11/16 01:32:42 christos Exp $");
+__KERNEL_RCSID(0, "$NetBSD: linux_misc.c,v 1.165.2.1 2007/04/20 20:14:11 bouyer Exp $");
 
 #if defined(_KERNEL_OPT)
 #include "opt_ptrace.h"
@@ -178,10 +178,8 @@ const int linux_fstypes_cnt = sizeof(linux_fstypes) / sizeof(linux_fstypes[0]);
 # endif
 
 /* Local linux_misc.c functions: */
-# ifndef __amd64__
 static void bsd_to_linux_statfs __P((const struct statvfs *,
     struct linux_statfs *));
-# endif
 static void linux_to_bsd_mmap_args __P((struct sys_mmap_args *,
     const struct linux_sys_mmap_args *));
 static int linux_mmap __P((struct lwp *, struct linux_sys_mmap_args *,
@@ -313,7 +311,6 @@ linux_sys_brk(l, v, retval)
 	return 0;
 }
 
-# ifndef __amd64__
 /*
  * Convert NetBSD statvfs structure to Linux statfs structure.
  * Linux doesn't have f_flag, and we can't set f_frsize due
@@ -447,7 +444,6 @@ out:
 	STATVFSBUF_PUT(btmp);
 	return error;
 }
-# endif /* !__amd64__ */
 
 /*
  * uname(). Just copy the info from the various strings stored in the
