48fb60512d9f6f8d13e393d0641e43832c51c47b
bouyer <bouyer@NetBSD.org>
2007-04-20 20:26:03 +0000

    Pull up following revision(s) (requested by njoly in ticket #582): 	sys/compat/linux32/common/linux32_types.h: revision 1.4 via patch 	sys/compat/linux32/arch/amd64/syscalls.master: revision 1.14 via patch 	sys/compat/linux/common/linux_statfs.h: revision 1.1 via patch 	sys/compat/linux/common/linux_misc.c: revision 1.170 via patch 	sys/compat/linux32/common/linux32_misc.c: revision 1.5 via patch 	sys/compat/linux32/files.linux32: revision 1.4 via patch - Add compat amd64 linux32 statfs support, adapted from PR/35956. - Move bsd_to_linux_statfs() function to its own file to be shared   between both linux compats.

--- sys/compat/linux32/common/linux32_types.h
+++ sys/compat/linux32/common/linux32_types.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: linux32_types.h,v 1.3 2006/11/16 01:32:44 christos Exp $ */
+/*	$NetBSD: linux32_types.h,v 1.3.2.1 2007/04/20 20:26:03 bouyer Exp $ */
 
 /*-
  * Copyright (c) 2006 Emmanuel Dreyfus, all rights reserved.
@@ -42,6 +42,7 @@ typedef netbsd32_pointer_t linux32_oldmmapp;
 typedef netbsd32_pointer_t linux32_utsnamep;
 typedef netbsd32_pointer_t linux32_stat64p;
 typedef netbsd32_pointer_t linux32_statp;
+typedef netbsd32_pointer_t linux32_statfsp;
 typedef netbsd32_pointer_t linux32_sigactionp_t;
 typedef netbsd32_pointer_t linux32_sigsetp_t;
 typedef netbsd32_pointer_t linux32___sysctlp_t;
