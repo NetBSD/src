bc3b4e2d1e2c927a19447cd67f78466830d59cd9
dsl <dsl@NetBSD.org>
2007-04-22 08:34:48 +0000

    4.99.18 for changes to namei() and emulation roots

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.259 2007/04/08 11:20:49 hannken Exp $	*/
+/*	$NetBSD: param.h,v 1.260 2007/04/22 08:34:48 dsl Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -63,7 +63,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	499001700	/* NetBSD 4.99.17 */
+#define	__NetBSD_Version__	499001800	/* NetBSD 4.99.18 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
