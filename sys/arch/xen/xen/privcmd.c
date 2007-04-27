4ce0891809ce17876e845d80f97fe32ba364ac9e
bouyer <bouyer>
2007-04-27 22:10:39 +0000

    IOCTL_PRIVCMD_MMAPBATCH: mimic linux behavior and return no error even if a pmap_enter_ma() (the underlying hypercall, really) returns one. The fact that the mapping failed is notified to the called by oring 0xF0000000 to the mfn.  This makes Xen 3.0.4 HVM work.

--- sys/arch/xen/xen/privcmd.c
+++ sys/arch/xen/xen/privcmd.c
@@ -1,4 +1,4 @@
-/* $NetBSD: privcmd.c,v 1.15 2006/10/17 19:57:24 bouyer Exp $ */
+/* $NetBSD: privcmd.c,v 1.16 2007/04/27 22:10:39 bouyer Exp $ */
 
 /*-
  * Copyright (c) 2004 Christian Limpach.
@@ -32,7 +32,7 @@
 
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: privcmd.c,v 1.15 2006/10/17 19:57:24 bouyer Exp $");
+__KERNEL_RCSID(0, "$NetBSD: privcmd.c,v 1.16 2007/04/27 22:10:39 bouyer Exp $");
 
 #include "opt_compat_netbsd.h"
 
@@ -222,6 +222,7 @@ privcmd_ioctl(void *v)
 				copyout(&mfn, &pmb->arr[i], sizeof(mfn));
 			}
 		}
+		error = 0;
 		break;
 	}
 #ifndef XEN3
