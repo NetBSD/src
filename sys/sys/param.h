91720d304ba810cd25baa4f546dfb2e2dde8ab58
rmind <rmind@NetBSD.org>
2007-04-30 15:14:13 +0000

    Welcome to NetBSD 4.99.19: Added support for POSIX Asynchronous I/O.

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.260 2007/04/22 08:34:48 dsl Exp $	*/
+/*	$NetBSD: param.h,v 1.261 2007/04/30 15:14:13 rmind Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -63,7 +63,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	499001800	/* NetBSD 4.99.18 */
+#define	__NetBSD_Version__	499001900	/* NetBSD 4.99.19 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
