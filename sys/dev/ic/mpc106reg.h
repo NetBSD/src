502a4cbb1ae1d094c083057b83810db62b8632c8
garbled <garbled@NetBSD.org>
2007-05-01 07:52:51 +0000

    Convert to using BITS macros, and add some additional defines for determining ECC/ram types

--- sys/dev/ic/mpc106reg.h
+++ sys/dev/ic/mpc106reg.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: mpc106reg.h,v 1.1 2007/05/01 05:00:55 garbled Exp $	*/
+/*	$NetBSD: mpc106reg.h,v 1.2 2007/05/01 07:52:51 garbled Exp $	*/
 
 /*-
  * Copyright (c) 2001,2007 The NetBSD Foundation, Inc.
@@ -60,36 +60,36 @@
 #define	MPC106_EXTMEMENDADDR2	0x9c	/* Extd. memory ending address 2 */
 #define	MPC106_MEMEN		0xa0	/* Memory enable */
 #define	MPC106_PICR1		0xa8	/* Processor Interface Config 1 */
-#define	 MPC106_PICR1_CBA_MASK	 0xff000000	/* Copy-back addr mask */
-#define	 MPC106_PICR1_BREAD_WS	 0x00c00000	/* Burst read wait states: */
+#define	 MPC106_PICR1_CBA_MASK	 __BITS(31,24)	/* Copy-back addr mask */
+#define	 MPC106_PICR1_BREAD_WS	 __BITS(23,22)	/* Burst read wait states: */
 #define	 MPC106_PICR1_BREAD_WS0	 0x00000000	/*  0 wait states */
 #define	 MPC106_PICR1_BREAD_WS1	 0x00400000	/*  1 wait state */
 #define	 MPC106_PICR1_BREAD_WS2	 0x00800000	/*  2 wait states */
 #define	 MPC106_PICR1_BREAD_WS3	 0x00c00000	/*  3 wait states */
-#define	 MPC106_PICR1_CACHE_1G	 0x00200000	/* Cache 0-1G only */
-#define	 MPC106_PICR1_RCS0	 0x00100000	/* ROM on 0:PCI, 1:60x bus */
-#define	 MPC106_PICR1_XIO_MODE	 0x00080000	/* 0:Contig, 1:Discontig mode */
-#define	 MPC106_PICR1_PROC_TYPE	 0x00060000	/* Processor type */
+#define	 MPC106_PICR1_CACHE_1G	 __BIT(21)	/* Cache 0-1G only */
+#define	 MPC106_PICR1_RCS0	 __BIT(20)	/* ROM on 0:PCI, 1:60x bus */
+#define	 MPC106_PICR1_XIO_MODE	 __BIT(19)	/* 0:Contig, 1:Discontig mode */
+#define	 MPC106_PICR1_PROC_TYPE	 __BITS(18,17)	/* Processor type */
 #define	 MPC106_PICR1_PROC_TYPE_601	0x00000000
 #define	 MPC106_PICR1_PROC_TYPE_RSVD	0x00020000
 #define	 MPC106_PICR1_PROC_TYPE_603	0x00040000 /* also 740/750 */
 #define	 MPC106_PICR1_PROC_TYPE_604	0x00060000
-#define	 MPC106_PICR1_XATS	 0x00010000	/* Address map 0:B, 1:A */
-#define	 MPC106_PICR1_MP_ID	 0x0000c000	/* Multiprocessor identifier */
+#define	 MPC106_PICR1_XATS	 __BIT(16)	/* Address map 0:B, 1:A */
+#define	 MPC106_PICR1_MP_ID	 __BITS(15,14)	/* Multiprocessor identifier */
 	/* 2 bits describe which proc is reading this reg, 0-3 */
-#define	 MPC106_PICR1_LBA_EN	 0x00002000	/* Local bus slave enable */
-#define	 MPC106_PICR1_FLASHWR_EN 0x00001000	/* Flash writes enable */
-#define	 MPC106_PICR1_MCP_EN	 0x00000800	/* Machine check enable */
-#define	 MPC106_PICR1_TEA_EN	 0x00000400	/* Transfer error enable */
-#define	 MPC106_PICR1_DPARK	 0x00000200	/* Data bus park */
-#define	 MPC106_PICR1_EXT_L2_EN	 0x00000100	/* external l2 enable */
-#define	 MPC106_PICR1_NO_PORT_REGS 0x00000080	/* Implement ext. conf regs */
-#define	 MPC106_PICR1_ST_GATH_EN 0x00000040	/* Store gathering enable */
-#define	 MPC106_PICR1_LE_MODE	 0x00000020	/* 0:Big, 1:Little endian */
-#define	 MPC106_PICR1_LOOP_SNOOP 0x00000010	/* Snoop looping enable */
-#define	 MPC106_PICR1_APARK	 0x00000008	/* Address bus park */
-#define	 MPC106_PICR1_SPECREADS	 0x00000004	/* Speculative read enable */
-#define	 MPC106_PICR1_L2_MP	 0x00000003	/* L2/multiproc config: */
+#define	 MPC106_PICR1_LBA_EN	 __BIT(13)	/* Local bus slave enable */
+#define	 MPC106_PICR1_FLASHWR_EN __BIT(12)	/* Flash writes enable */
+#define	 MPC106_PICR1_MCP_EN	 __BIT(11)	/* Machine check enable */
+#define	 MPC106_PICR1_TEA_EN	 __BIT(10)	/* Transfer error enable */
+#define	 MPC106_PICR1_DPARK	 __BIT(9)	/* Data bus park */
+#define	 MPC106_PICR1_EXT_L2_EN	 __BIT(8)	/* external l2 enable */
+#define	 MPC106_PICR1_NO_PORT_REGS __BIT(7)	/* Implement ext. conf regs */
+#define	 MPC106_PICR1_ST_GATH_EN __BIT(6)	/* Store gathering enable */
+#define	 MPC106_PICR1_LE_MODE	 __BIT(5)	/* 0:Big, 1:Little endian */
+#define	 MPC106_PICR1_LOOP_SNOOP __BIT(4)	/* Snoop looping enable */
+#define	 MPC106_PICR1_APARK	 __BIT(3)	/* Address bus park */
+#define	 MPC106_PICR1_SPECREADS	 __BIT(2)	/* Speculative read enable */
+#define	 MPC106_PICR1_L2_MP	 __BITS(1,0)	/* L2/multiproc config: */
 	/* must be read with MPC106_PICR1_EXT_L2_EN :
 	 * L2_EN	L2_MP	Meaning
 	 * 0		00	Uniprocessor/none
@@ -104,59 +104,59 @@
 #define	 MPC106_PICR1_L2_MP_WB	 0x00000002	/*  Write-back */
 #define	 MPC106_PICR1_L2_MP_MP	 0x00000003	/*  Multiprocessor */
 #define	MPC106_PICR2		0xac	/* Processor Interface Config 2 */
-#define	 MPC106_PICR2_L2_UPD_EN	 0x80000000	/* Service L2 cache misses */
-#define	 MPC106_PICR2_L2_EN	 0x40000000	/* L2 cache internal enable */
+#define	 MPC106_PICR2_L2_UPD_EN	 __BIT(31)	/* Service L2 cache misses */
+#define	 MPC106_PICR2_L2_EN	 __BIT(30)	/* L2 cache internal enable */
 	/* also available at 0x81C */
-#define	 MPC106_PICR2_NOSERCFG	 0x20000000	/* serialized config writes */
-#define	 MPC106_PICR2_FLUSH_L2	 0x10000000	/* 0->1: flush L2 cache */
-#define	 MPC106_PICR2_NOSNOOP_EN 0x08000000	/* snoop transactions */
-#define  MPC106_PICR2_CF_FF0_LOC 0x04000000	/* ROM remapping enable */
-#define	 MPC106_PICR2_FLASH_LOCK 0x02000000	/* Flash write lockout */
-#define	 MPC106_PICR2_FAST_L2_MODE 0x01000000	/* Fast L2 mode timing */
-#define	 MPC106_PICR2_DATA_RAM_TYPE 0x00c00000	/* L2 data RAM type */
+#define	 MPC106_PICR2_NOSERCFG	 __BIT(29)	/* serialized config writes */
+#define	 MPC106_PICR2_FLUSH_L2	 __BIT(28)	/* 0->1: flush L2 cache */
+#define	 MPC106_PICR2_NOSNOOP_EN __BIT(27)	/* snoop transactions */
+#define  MPC106_PICR2_CF_FF0_LOC __BIT(26)	/* ROM remapping enable */
+#define	 MPC106_PICR2_FLASH_LOCK __BIT(25)	/* Flash write lockout */
+#define	 MPC106_PICR2_FAST_L2_MODE __BIT(24)	/* Fast L2 mode timing */
+#define	 MPC106_PICR2_DATA_RAM_TYPE __BITS(23,22)	/* L2 data RAM type */
 #define	 MPC106_PICR2_DATA_RAM_TYPE_SYNCBRST	0x00000000
 #define	 MPC106_PICR2_DATA_RAM_TYPE_PIPEBRST	0x00400000
 #define	 MPC106_PICR2_DATA_RAM_TYPE_ASYNC	0x00800000
 #define	 MPC106_PICR2_DATA_RAM_TYPE_RSVD1	0x00c00000
-#define	 MPC106_PICR2_WMODE	 0x00300000	/* SRAM write timing */
+#define	 MPC106_PICR2_WMODE	 __BITS(21,20)	/* SRAM write timing */
 #define	 MPC106_PICR2_WMODE_NORMAL	0x00000000 /* norm w/o partial update */
 #define	 MPC106_PICR2_WMODE_NORMAL_PART	0x00100000 /* norm with part upd. */
 #define	 MPC106_PICR2_WMODE_DELAYED	0x00200000
 #define	 MPC106_PICR2_WMODE_EARLY	0x00300000
-#define	 MPC106_PICR2_SNOOP_WS	 0x000c0000	/* Snoop wait states: */
+#define	 MPC106_PICR2_SNOOP_WS	 __BITS(19,18)	/* Snoop wait states: */
 #define	 MPC106_PICR2_SNOOP_WS0	 0x00000000	/*  0 wait states */
 #define	 MPC106_PICR2_SNOOP_WS1	 0x00040000	/*  1 wait state */
 #define	 MPC106_PICR2_SNOOP_WS2	 0x00080000	/*  2 wait states */
 #define	 MPC106_PICR2_SNOOP_WS3	 0x000c0000	/*  3 wait states */
-#define	 MPC106_PICR2_MOD_HIGH	 0x00020000	/* Cache modified polarity */
-#define	 MPC106_PICR2_HIT_HIGH	 0x00010000	/* Cache hit polarity */
-#define	 MPC106_PICR2_RSVD2	 0x00008000
-#define	 MPC106_PICR2_ADDR_ONLY_DISABLE 0x00004000 /* L2 ignores CLEAN/
+#define	 MPC106_PICR2_MOD_HIGH	 __BIT(17)	/* Cache modified polarity */
+#define	 MPC106_PICR2_HIT_HIGH	 __BIT(16)	/* Cache hit polarity */
+#define	 MPC106_PICR2_RSVD2	 __BIT(15)
+#define	 MPC106_PICR2_ADDR_ONLY_DISABLE __BIT(14) /* L2 ignores CLEAN/
 						      FLUSH/KILL ops */
-#define	 MPC106_PICR2_HOLD	 0x00002000	/* L2 tag address hold */
-#define	 MPC106_PICR2_INV_MODE	 0x00001000	/* L2 invalidate mode enable */
-#define	 MPC106_PICR2_RWITM	 0x00000800	/* read with intent to modify
+#define	 MPC106_PICR2_HOLD	 __BIT(13)	/* L2 tag address hold */
+#define	 MPC106_PICR2_INV_MODE	 __BIT(12)	/* L2 invalidate mode enable */
+#define	 MPC106_PICR2_RWITM	 __BIT(11)	/* read with intent to modify
 						   line-fill disable */
-#define	 MPC106_PICR2_L2_HIT_DELAY	 0x0000600	/* L2 hit delay */
+#define	 MPC106_PICR2_L2_HIT_DELAY	 __BITS(10,9)	/* L2 hit delay */
 #define	 MPC106_PICR2_L2_HIT_DELAYRSVD	 0x0000000	/*  reserved */
 #define	 MPC106_PICR2_L2_HIT_DELAY1	 0x0000200	/*  1 clock cycle */
 #define	 MPC106_PICR2_L2_HIT_DELAY2	 0x0000400	/*  2 clock cycles */
 #define	 MPC106_PICR2_L2_HIT_DELAY3	 0x0000600	/*  3 clock cycles */
-#define	 MPC106_PICR2_BANKS 	0x00000100	 /* L2: nrof banks */
-#define	 MPC106_PICR2_FAST_CASTOUT 0x00000080	 /* L2 Fast castout timing */
-#define	 MPC106_PICR2_TOE_WIDTH	 0x00000040	 /* TOE 0:2, 1:3 clock cycles */
-#define	 MPC106_PICR2_L2_SIZE	 0x00000030	 /* L2 cache size */
+#define	 MPC106_PICR2_BANKS	__BIT(8) 	 /* L2: nrof banks */
+#define	 MPC106_PICR2_FAST_CASTOUT __BIT(7)	 /* L2 Fast castout timing */
+#define	 MPC106_PICR2_TOE_WIDTH	__BIT(6)	 /* TOE 0:2, 1:3 clock cycles */
+#define	 MPC106_PICR2_L2_SIZE	__BITS(5,4)	 /* L2 cache size */
 #define	 MPC106_PICR2_L2_SIZE_256K	 0x00000000
 #define	 MPC106_PICR2_L2_SIZE_512K	 0x00000010
 #define	 MPC106_PICR2_L2_SIZE_1M	 0x00000020
 #define	 MPC106_PICR2_L2_SIZE_RSVD	 0x00000030
-#define	 MPC106_PICR2_APHASE_WS	 0x0000000c	 /* Addr. phase wait states: */
+#define	 MPC106_PICR2_APHASE_WS	 __BITS(3,2)	 /* Addr. phase wait states: */
 #define	 MPC106_PICR2_APHASE_WS0 0x00000000	 /*  0 clock cycles */
 #define	 MPC106_PICR2_APHASE_WS1 0x00000004	 /*  1 clock cycle */
 #define	 MPC106_PICR2_APHASE_WS2 0x00000008	 /*  2 clock cycles */
 #define	 MPC106_PICR2_APHASE_WS3 0x0000000c	 /*  3 clock cycles */
-#define	 MPC106_PICR2_DOE	 0x00000002	 /* L2 first data read timing */
-#define	 MPC106_PICR2_WDATA	 0x00000001	 /* L2 first data write timing*/
+#define	 MPC106_PICR2_DOE	 __BIT(1)	 /* L2 first data read timing */
+#define	 MPC106_PICR2_WDATA	 __BIT(0)	 /* L2 first data write timing*/
 #define MPC106_ECC_SBECR	0xb8	/* ECC single bit err count reg */
 #define MPC106_ECC_SBETR	0xb9	/* ECC single bit error trigger */
 #define	MPC106_AOVPR1		0xba	/* Alt. OS-visible parameters 1 */
@@ -172,10 +172,35 @@
 #define MPC106_ESCR2		0xe8	/* emulation support conf reg 2 */
 #define MPC106_MMSR1		0xe4	/* modified memory status reg 1 */
 #define MPC106_MMSR2		0xec	/* modified memory status reg 2 */
-#define	MPC106_MEMCTRLCR1	0xf0	/* Memory control configuration 1 */
-#define	MPC106_MEMCTRLCR2	0xf4	/* Memory control configuration 2 */
-#define	MPC106_MEMCTRLCR3	0xf8	/* Memory control configuration 3 */
-#define	MPC106_MEMCTRLCR4	0xfc	/* Memory control configuration 4 */
+#define	MPC106_MCCR1		0xf0	/* Memory control configuration 1 */
+#define  MPC106_MCCR1_ROMNAL	__BITS(21,28)	/* burst mode rom reads */
+#define  MPC106_MCCR1_ROMFAL	__BITS(27,23)	/* burst mode rom reads */
+#define  MPC106_MCCR1_501_MODE	__BIT(22)	/* 501 mode conf signal */
+#define  MPC106_MCCR1_8N64	__BIT(21)
+#define  MPC106_MCCR1_BURST	__BIT(20)
+#define  MPC106_MCCR1_MEMGO	__BIT(19)
+#define  MPC106_MCCR1_SREN	__BIT(18)
+#define  MPC106_MCCR1_RAMTYPE	__BIT(17)	/* 0:SDRAM 1:DRAM/EDO */
+#define  MPC106_MCCR1_PCKEN	__BIT(16)	/* dis/enable parity */
+/* bits 15-0 are ram bank row address bit counts */
+#define	MPC106_MCCR2		0xf4	/* Memory control configuration 2 */
+#define  MPC106_MCRR2_TSWAIT	__BITS(31,29)
+#define  MPC106_MCRR2_RESV0	__BITS(28,22)	/* reserved */
+#define  MPC106_MCRR2_BSTOPRE0	__BIT(21)
+#define  MPC106_MCRR2_BSTOPRE1	__BIT(20)
+#define  MPC106_MCRR2_EXT_ECM_PAR_EN	__BIT(19) /* ext. ECM parity check */
+#define  MPC106_MCRR2_EXT_ECM_ECC_EN	__BIT(18) /* ext. ECM ECC enable */
+#define  MPC106_MCRR2_ECC_EN	__BIT(17)	/* ECC enable */
+#define  MPC106_MCRR2_EDO	__BIT(16)	/* EDO enable */
+#define  MPC106_MCRR2_REFINT	__BITS(15,2)	/* refreash interval */
+#define  MPC106_MCRR2_BUFMODE	__BIT(1)	/* buffer mode */
+#define  MPC106_MCRR2_RMW_PAR	__BIT(0)	/* RMW parity enable */
+#define	MPC106_MCCR3		0xf8	/* Memory control configuration 3 */
+#define	MPC106_MCCR4		0xfc	/* Memory control configuration 4 */
+#define  MPC106_MCRR4_PRETOACT	__BITS(31,28)
+#define  MPC106_MCRR4_ACTOPRE	__BITS(27,24)
+#define  MPC106_MCRR4_EXT_ECM_EN __BITS(23,22)	/* ext ecm enable */
+						/* 00= disabled 11=enabled */
 
 #define MPC106_ECR1	0x092	/* External config register 1 (LE mode) */
 #define MPC106_ECR2	0x81C	/* External config register 2 */
