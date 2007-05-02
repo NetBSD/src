f1762ae875d6d1e1ccef3a6f150abd463d1cdd2e
snj <snj@NetBSD.org>
2007-05-02 04:50:36 +0000

    Regen for ticket 597.

--- sys/dev/pci/pcidevs_data.h
+++ sys/dev/pci/pcidevs_data.h
@@ -1,10 +1,10 @@
-/*	$NetBSD: pcidevs_data.h,v 1.853.2.4 2007/02/21 13:18:48 tron Exp $	*/
+/*	$NetBSD: pcidevs_data.h,v 1.853.2.5 2007/05/02 04:50:37 snj Exp $	*/
 
 /*
  * THIS FILE AUTOMATICALLY GENERATED.  DO NOT EDIT.
  *
  * generated from:
- *	NetBSD: pcidevs,v 1.855.2.4 2007/02/21 13:17:59 tron Exp
+ *	NetBSD
  */
 
 /*
@@ -6896,6 +6896,14 @@ static const struct pci_product pci_products[] = {
 	    "i80003 Gigabit Ethernet (SERDES)",
 	},
 	{
+	    PCI_VENDOR_INTEL, PCI_PRODUCT_INTEL_82801H_IFE_GT,
+	    "i82801H IFE (GT) LAN Controller",
+	},
+	{
+	    PCI_VENDOR_INTEL, PCI_PRODUCT_INTEL_82801H_IFE_G,
+	    "i82801H IFE (G) LAN Controller",
+	},
+	{
 	    PCI_VENDOR_INTEL, PCI_PRODUCT_INTEL_82815_DC100_HUB,
 	    "82815 Hub",
 	},
@@ -12708,4 +12716,4 @@ static const struct pci_product pci_products[] = {
 	    "Video Controller",
 	},
 };
-const int pci_nproducts = 2583;
+const int pci_nproducts = 2585;
