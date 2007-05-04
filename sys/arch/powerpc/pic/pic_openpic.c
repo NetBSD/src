cd54d6ac519496a48e382f759ba16e6df8da5115
macallan <macallan>
2007-05-04 02:54:33 +0000

    nuke unused pic_ops methods, honour priority in pic_establish_irq()

--- sys/arch/powerpc/pic/pic_openpic.c
+++ sys/arch/powerpc/pic/pic_openpic.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: pic_openpic.c,v 1.1.2.6 2007/05/03 19:38:37 garbled Exp $ */
+/*	$NetBSD: pic_openpic.c,v 1.1.2.7 2007/05/04 02:54:33 macallan Exp $ */
 
 /*-
  * Copyright (c) 2007 Michael Lorenz
@@ -30,7 +30,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: pic_openpic.c,v 1.1.2.6 2007/05/03 19:38:37 garbled Exp $");
+__KERNEL_RCSID(0, "$NetBSD: pic_openpic.c,v 1.1.2.7 2007/05/04 02:54:33 macallan Exp $");
 
 #include <sys/param.h>
 #include <sys/malloc.h>
@@ -44,12 +44,11 @@ __KERNEL_RCSID(0, "$NetBSD: pic_openpic.c,v 1.1.2.6 2007/05/03 19:38:37 garbled 
 #include <arch/powerpc/pic/picvar.h>
 
 static void openpic_set_priority(int cpu, int pri);
-static int  opic_irq_is_enabled(struct pic_ops *, int);
 static void opic_enable_irq(struct pic_ops *, int, int);
 static void opic_disable_irq(struct pic_ops *, int);
-static void opic_clear_irq(struct pic_ops *, int);
 static int  opic_get_irq(struct pic_ops *);
 static void opic_ack_irq(struct pic_ops *, int);
+static void opic_establish_irq(struct pic_ops*, int, int, int);
 
 struct openpic_ops {
 	struct pic_ops pic;
@@ -59,7 +58,7 @@ struct openpic_ops {
 volatile unsigned char *openpic_base;
 
 struct pic_ops *
-setup_openpic(uint32_t addr, int passthrough)
+setup_openpic(void *addr, int passthrough)
 {
 	struct openpic_ops *openpic;
 	struct pic_ops *pic;
@@ -77,15 +76,13 @@ setup_openpic(uint32_t addr, int passthrough)
 	    x & 0xff, ((x & 0x1f00) >> 8) + 1, ((x & 0x07ff0000) >> 16) + 1);
 
 	pic->pic_numintrs = ((x & 0x07ff0000) >> 16) + 1;
-	pic->pic_cookie = (void *)addr;
-	pic->pic_irq_is_enabled = opic_irq_is_enabled;
+	pic->pic_cookie = addr;
 	pic->pic_enable_irq = opic_enable_irq;
 	pic->pic_reenable_irq = opic_enable_irq;
 	pic->pic_disable_irq = opic_disable_irq;
-	pic->pic_clear_irq = opic_clear_irq;
 	pic->pic_get_irq = opic_get_irq;
 	pic->pic_ack_irq = opic_ack_irq;
-	pic->pic_establish_irq = dummy_pic_establish_intr;
+	pic->pic_establish_irq = opic_establish_irq;
 	strcpy(pic->pic_name, "openpic");
 	pic_add(pic);
 
@@ -107,7 +104,7 @@ setup_openpic(uint32_t addr, int passthrough)
 		x = irq;
 		x |= OPENPIC_IMASK;
 		x |= (irq == 0) ?
-		    OPENPIC_POLARITY_POSITIVE : OPENPIC_POLARITY_NEGATIVE;
+		    OPENPIC_POLARITY_POSITIVE :	OPENPIC_POLARITY_NEGATIVE;
 		x |= OPENPIC_SENSE_LEVEL;
 		x |= 8 << OPENPIC_PRIORITY_SHIFT;
 		openpic_write(OPENPIC_SRC_VECTOR(irq), x);
@@ -137,6 +134,23 @@ setup_openpic(uint32_t addr, int passthrough)
 
 	return pic;
 }
+static void
+opic_establish_irq(struct pic_ops *pic, int irq, int type, int pri)
+{
+	int realpri = max(1, min(15, pri));
+	uint32_t x;
+
+	x = irq;
+	x |= OPENPIC_IMASK;
+	x |= (irq == 0) ?
+	    OPENPIC_POLARITY_POSITIVE :	OPENPIC_POLARITY_NEGATIVE;
+	x |= (type == IST_EDGE) ? OPENPIC_SENSE_EDGE : OPENPIC_SENSE_LEVEL;
+	x |= realpri << OPENPIC_PRIORITY_SHIFT;
+	openpic_write(OPENPIC_SRC_VECTOR(irq), x);
+	/* send all interrupts to CPU 0 */
+	openpic_write(OPENPIC_IDEST(irq), 1 << 0);
+	aprint_debug("%s: setting IRQ %d to priority %d\n", __func__, irq, realpri);
+}
 
 static void
 openpic_set_priority(int cpu, int pri)
@@ -147,14 +161,6 @@ openpic_set_priority(int cpu, int pri)
 	x &= ~OPENPIC_CPU_PRIORITY_MASK;
 	x |= pri;
 	openpic_write(OPENPIC_CPU_PRIORITY(cpu), x);
-}
-
-static int
-opic_irq_is_enabled(struct pic_ops *pic, int irq)
-{
-	/* XXX */
-
-	return 1; 
 }
 
 static void
@@ -179,13 +185,6 @@ opic_disable_irq(struct pic_ops *pic, int irq)
 	x = openpic_read(OPENPIC_SRC_VECTOR(irq));
 	x |= OPENPIC_IMASK;
 	openpic_write(OPENPIC_SRC_VECTOR(irq), x);
-}
-
-static void
-opic_clear_irq(struct pic_ops *pic, int irq)
-{
-
-	/* we only use ack here */
 }
 
 static int
