cf59fca4d5138af2dddf1cc348d60291828938be
dyoung <dyoung@NetBSD.org>
2007-05-06 02:48:38 +0000

    Oops, commit this straggler from the last change to net/if_gre.[ch].

--- sys/netinet/ip_gre.c
+++ sys/netinet/ip_gre.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: ip_gre.c,v 1.45 2007/03/21 01:56:05 dyoung Exp $ */
+/*	$NetBSD: ip_gre.c,v 1.46 2007/05/06 02:48:38 dyoung Exp $ */
 
 /*
  * Copyright (c) 1998 The NetBSD Foundation, Inc.
@@ -45,7 +45,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: ip_gre.c,v 1.45 2007/03/21 01:56:05 dyoung Exp $");
+__KERNEL_RCSID(0, "$NetBSD: ip_gre.c,v 1.46 2007/05/06 02:48:38 dyoung Exp $");
 
 #include "gre.h"
 #if NGRE > 0
@@ -141,6 +141,7 @@ gre_input(struct mbuf *m, ...)
 int
 gre_input2(struct mbuf *m, int hlen, u_char proto)
 {
+	int rc;
 	const struct greip *gip;
 	struct gre_softc *sc;
 
@@ -156,7 +157,8 @@ gre_input2(struct mbuf *m, int hlen, u_char proto)
 	}
 	gip = mtod(m, const struct greip *);
 
-	return gre_input3(sc, m, hlen, &gip->gi_g);
+	rc = gre_input3(sc, m, hlen, &gip->gi_g, 0);
+	return rc;
 }
 
 /*
