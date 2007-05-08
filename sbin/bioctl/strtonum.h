bb488889f1865328c17244f47cd0c1475781b234
pavel <pavel@NetBSD.org>
2007-05-08 10:45:01 +0000

    Pull up following revision(s) (requested by bouyer in ticket #603): 	distrib/sets/lists/base/mi: revision 1.704 	distrib/sets/lists/comp/mi: revision 1.1022 	distrib/sets/lists/man/mi: revision 1.997 	doc/CHANGES: revision 1.839 	sbin/Makefile: patch 	sbin/bioctl/Makefile: revision 1.1 	sbin/bioctl/bioctl.8: revision 1.1 	sbin/bioctl/bioctl.c: revision 1.1 	sbin/bioctl/strtonum.c: revision 1.1 	sbin/bioctl/strtonum.h: revision 1.1 	share/man/man4/Makefile: revision 1.426 	share/man/man4/bio.4: revision 1.1 	sys/arch/amd64/conf/GENERIC: revision 1.139 	sys/arch/i386/conf/GENERIC: revision 1.825 	sys/arch/i386/conf/XEN2_DOM0: revision 1.25 	sys/conf/files: revision 1.839 	sys/conf/majors: patch 	sys/dev/Makefile: revision 1.25 	sys/dev/bio.c: patch 	sys/dev/biovar.h: patch 	sys/dev/ic/mfi.c: revision 1.4-1.5 	sys/dev/ic/mfivar.h: revision 1.4 	sys/sys/envsys.h: revision 1.11 	usr.sbin/envstat/envstat.c: revision 1.24 Add bio(4) and associated bioctl(8) from OpenBSD, a driver control block device controllers, and more specifically raid controllers. Add a new sensor type, ENVSYS_DRIVE, to report drive status. From OpenBSD. Add bio and sysmon support to mfi(4). This allow userland to query status for drives and logical volumes attached to a mfi(4) controller. While there fix some debug printfs in mfi so they compile. Add bio(4) to amd64 and i386 GENERIC.
    note bio(4), envsys(4) DRIVE, and mfi(4) support for both.
    
    tred->sensor is a u_int.

---
+++ sbin/bioctl/strtonum.h
@@ -0,0 +1,4 @@
+/* $NetBSD: strtonum.h,v 1.1.2.2 2007/05/08 10:45:03 pavel Exp $ */
+
+long long strtonum(const char *, long long, long long, const char **);
+
---
+++ sys/dev/bio.c
@@ -0,0 +1,234 @@
+/*	$NetBSD: bio.c,v 1.1.4.2 2007/05/08 10:45:04 pavel Exp $ */
+/*	$OpenBSD: bio.c,v 1.9 2007/03/20 02:35:55 marco Exp $	*/
+
+/*
+ * Copyright (c) 2002 Niklas Hallqvist.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/* A device controller ioctl tunnelling device.  */
+
+#include <sys/cdefs.h>
+__KERNEL_RCSID(0, "$NetBSD: bio.c,v 1.1.4.2 2007/05/08 10:45:04 pavel Exp $");
+
+#include <sys/param.h>
+#include <sys/conf.h>
+#include <sys/device.h>
+#include <sys/event.h>
+#include <sys/ioctl.h>
+#include <sys/malloc.h>
+#include <sys/queue.h>
+#include <sys/systm.h>
+#include <sys/proc.h>
+#include <sys/kauth.h>
+
+#include <dev/biovar.h>
+
+struct bio_mapping {
+	LIST_ENTRY(bio_mapping) bm_link;
+	struct device *bm_dev;
+	int (*bm_ioctl)(struct device *, u_long, caddr_t);
+};
+
+LIST_HEAD(, bio_mapping) bios = LIST_HEAD_INITIALIZER(bios);
+
+void	bioattach(int);
+int	bioclose(dev_t, int, int, struct lwp *);
+int	bioioctl(dev_t, u_long, caddr_t, int, struct lwp *);
+int	bioopen(dev_t, int, int, struct lwp *);
+
+int	bio_delegate_ioctl(struct bio_mapping *, u_long, void *);
+struct	bio_mapping *bio_lookup(char *);
+int	bio_validate(void *);
+
+const struct cdevsw bio_cdevsw = {
+        bioopen, bioclose, noread, nowrite, bioioctl,
+        nostop, notty, nopoll, nommap, nokqfilter, 0
+};
+
+
+void
+bioattach(int nunits)
+{
+}
+
+int
+bioopen(dev_t dev, int flags, int mode, struct lwp *l)
+{
+	return (0);
+}
+
+int
+bioclose(dev_t dev, int flags, int mode, struct lwp *l)
+{
+	return (0);
+}
+
+int
+bioioctl(dev_t dev, u_long cmd, caddr_t addr, int flag, struct  lwp *l)
+{
+	struct bio_locate *locate;
+	struct bio_common *common;
+	char name[16];
+	int error, s;
+
+	switch(cmd) {
+	case BIOCLOCATE:
+	case BIOCINQ:
+	case BIOCDISK:
+	case BIOCVOL:
+		error = kauth_authorize_device_passthru(l->l_cred, dev,
+		    KAUTH_REQ_DEVICE_RAWIO_PASSTHRU_READCONF, addr);
+		if (error)
+			return (error);
+		break;
+	case BIOCBLINK:
+	case BIOCSETSTATE:
+		error = kauth_authorize_device_passthru(l->l_cred, dev,
+		    KAUTH_REQ_DEVICE_RAWIO_PASSTHRU_WRITECONF, addr);
+		if (error)
+			return (error);
+		break;
+	case BIOCALARM: {
+		struct bioc_alarm *alarm = (struct bioc_alarm *)addr;
+		switch (alarm->ba_opcode) {
+		case BIOC_SADISABLE:
+		case BIOC_SAENABLE:
+		case BIOC_SASILENCE:
+		case BIOC_SATEST:
+			error = kauth_authorize_device_passthru(l->l_cred, dev,
+			    KAUTH_REQ_DEVICE_RAWIO_PASSTHRU_WRITECONF, addr);
+			if (error)
+				return (error);
+			break;
+		case BIOC_GASTATUS:
+			error = kauth_authorize_device_passthru(l->l_cred, dev,
+			    KAUTH_REQ_DEVICE_RAWIO_PASSTHRU_READCONF, addr);
+			if (error)
+				return (error);
+			break;
+		default:
+			return EINVAL;
+		}
+		break;
+	}
+	default:
+		return ENOTTY;
+	}
+
+	switch (cmd) {
+	case BIOCLOCATE:
+		locate = (struct bio_locate *)addr;
+		error = copyinstr(locate->bl_name, name, 16, NULL);
+		if (error != 0)
+			return (error);
+		locate->bl_cookie = bio_lookup(name);
+		if (locate->bl_cookie == NULL)
+			return (ENOENT);
+		break;
+
+	default:
+		common = (struct bio_common *)addr;
+		s = splbio();
+		if (!bio_validate(common->bc_cookie)) {
+			splx(s);
+			return (ENOENT);
+		}
+		error =  bio_delegate_ioctl(
+		    (struct bio_mapping *)common->bc_cookie, cmd, addr);
+		splx(s);
+		return (error);
+	}
+	return (0);
+}
+
+int
+bio_register(struct device *dev, int (*ioctl)(struct device *, u_long, caddr_t))
+{
+	struct bio_mapping *bm;
+	int s;
+
+	MALLOC(bm, struct bio_mapping *, sizeof *bm, M_DEVBUF, M_NOWAIT);
+	if (bm == NULL)
+		return (ENOMEM);
+	bm->bm_dev = dev;
+	bm->bm_ioctl = ioctl;
+	s = splbio();
+	LIST_INSERT_HEAD(&bios, bm, bm_link);
+	splx(s);
+	return (0);
+}
+
+void
+bio_unregister(struct device *dev)
+{
+	struct bio_mapping *bm, *next;
+	int s;
+
+	s = splbio();
+	for (bm = LIST_FIRST(&bios); bm != NULL; bm = next) {
+		next = LIST_NEXT(bm, bm_link);
+
+		if (dev == bm->bm_dev) {
+			LIST_REMOVE(bm, bm_link);
+			free(bm, M_DEVBUF);
+		}
+	}
+	splx(s);
+}
+
+struct bio_mapping *
+bio_lookup(char *name)
+{
+	struct bio_mapping *bm;
+	int s;
+
+	s = splbio();
+	LIST_FOREACH(bm, &bios, bm_link) {
+		if (strcmp(name, bm->bm_dev->dv_xname) == 0) {
+			splx(s);
+			return (bm);
+		}
+	}
+	splx(s);
+	return (NULL);
+}
+
+int
+bio_validate(void *cookie)
+{
+	struct bio_mapping *bm;
+
+	LIST_FOREACH(bm, &bios, bm_link) {
+		if (bm == cookie) {
+			return (1);
+		}
+	}
+	return (0);
+}
+
+int
+bio_delegate_ioctl(struct bio_mapping *bm, u_long cmd, void *addr)
+{
+	
+	return (bm->bm_ioctl(bm->bm_dev, cmd, addr));
+}
