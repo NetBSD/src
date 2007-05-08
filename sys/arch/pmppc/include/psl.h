221e0586608f1c0993f6acbe45478de10243c84f
garbled <garbled>
2007-05-08 20:13:13 +0000

    Now that pmppc has been moved to evbppc/pmppc, remove arch/pmppc.

--- sys/arch/pmppc/include/psl.h
+++
@@ -1,3 +0,0 @@
-/*	$NetBSD: psl.h,v 1.1 2002/05/30 08:51:30 augustss Exp $	*/
-
-#include <powerpc/psl.h>
--- sys/arch/pmppc/include/types.h
+++
@@ -1,8 +0,0 @@
-/*	$NetBSD: types.h,v 1.3.18.1 2007/05/08 17:10:47 garbled Exp $	*/
-
-#include <powerpc/types.h>
-
-#define __HAVE_DEVICE_REGISTER
-#define __HAVE_GENERIC_SOFT_INTERRUPTS
-#define __HAVE_TIMECOUNTER
-#define	__HAVE_GENERIC_TODR
