4ecec94f0579999f060338ddf02ffd071684d526
garbled <garbled@NetBSD.org>
2007-05-08 20:13:13 +0000

    Now that pmppc has been moved to evbppc/pmppc, remove arch/pmppc.

--- sys/arch/pmppc/include/float.h
+++
@@ -1,3 +0,0 @@
-/*	$NetBSD: float.h,v 1.1 2002/05/30 08:51:18 augustss Exp $	*/
-
-#include <powerpc/float.h>
--- sys/arch/pmppc/include/signal.h
+++
@@ -1,3 +0,0 @@
-/*	$NetBSD: signal.h,v 1.1 2002/05/30 08:51:32 augustss Exp $	*/
-
-#include <powerpc/signal.h>
