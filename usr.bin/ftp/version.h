82552a76687df570ae0a6dd0d64e40e20b53b27d
lukem <lukem@NetBSD.org>
2007-05-10 05:17:10 +0000

    Implement copy_bytes() to copy bytes from one fd to another via the provided buffer, with optional rate-limiting and hash-mark printing, using one loop and handle short writes. Refactor sendrequest() and recvrequest() to use copy_data(). Addresses PR 15943.

--- usr.bin/ftp/version.h
+++ usr.bin/ftp/version.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: version.h,v 1.63 2007/04/18 01:39:04 lukem Exp $	*/
+/*	$NetBSD: version.h,v 1.64 2007/05/10 05:17:10 lukem Exp $	*/
 /*-
  * Copyright (c) 1999-2007 The NetBSD Foundation, Inc.
  * All rights reserved.
@@ -40,5 +40,5 @@
 #endif
 
 #ifndef FTP_VERSION
-#define	FTP_VERSION	"20070418"
+#define	FTP_VERSION	"20070510"
 #endif
