544617880aa358c81b7743cae424e856a792b8d2
rumble <rumble@NetBSD.org>
2007-05-11 01:40:18 +0000

    Update a prototype corresponding to rev. 1.36 of sgimips/autoconf.c.

--- sys/arch/sgimips/include/autoconf.h
+++ sys/arch/sgimips/include/autoconf.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: autoconf.h,v 1.8 2005/12/11 12:18:53 christos Exp $	*/
+/*	$NetBSD: autoconf.h,v 1.9 2007/05/11 01:40:18 rumble Exp $	*/
 
 /*
  * Copyright (c) 2000 Soren S. Jorvang
@@ -43,5 +43,5 @@ struct mainbus_attach_args {
 
 extern int ncpus;
 
-void	makebootdev (char *);
+void	makebootdev (const char *);
 
