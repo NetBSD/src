f524750b526fff661a706f6b52cb97a225117e18
mhitch <mhitch@NetBSD.org>
2007-05-13 19:24:20 +0000

    Fix warnings with DEBUG enabled.

--- sys/arch/amiga/dev/siop2.c
+++ sys/arch/amiga/dev/siop2.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: siop2.c,v 1.27 2007/03/04 05:59:28 christos Exp $ */
+/*	$NetBSD: siop2.c,v 1.28 2007/05/13 19:24:20 mhitch Exp $ */
 
 /*
  * Copyright (c) 1990 The Regents of the University of California.
@@ -70,7 +70,7 @@
 #include "opt_ddb.h"
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: siop2.c,v 1.27 2007/03/04 05:59:28 christos Exp $");
+__KERNEL_RCSID(0, "$NetBSD: siop2.c,v 1.28 2007/05/13 19:24:20 mhitch Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -303,7 +303,7 @@ siopng_poll(struct siop_softc *sc, struct siop_acb *acb)
 				    xs->xs_periph->periph_target, acb->cmd.opcode,
 				    rp->siop_sbcl, istat, rp->siop_sbdl, rp->siop_dsp,
 				    rp->siop_dsp - sc->sc_scriptspa,
-				    *((long *)&rp->siop_dcmd), &acb->ds, acb->xs->timeout);
+				    *((volatile long *)&rp->siop_dcmd), &acb->ds, acb->xs->timeout);
 #endif
 				i = 50000;
 				--to;
@@ -1163,7 +1163,7 @@ siopng_checkintr(struct siop_softc *sc, u_char istat, u_char dstat,
 			printf ("Phase mismatch: %x dsp +%lx dcmd %lx\n",
 			    rp->siop_sbcl,
 			    rp->siop_dsp - sc->sc_scriptspa,
-			    *((long *)&rp->siop_dcmd));
+			    *((volatile long *)&rp->siop_dcmd));
 #endif
 		if ((rp->siop_sbcl & SIOP_REQ) == 0) {
 			printf ("Phase mismatch: REQ not asserted! %02x dsp %lx\n",
