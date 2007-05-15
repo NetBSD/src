005a5d609e54d8b85f79716ac86948dfb4c221ea
skrll <skrll>
2007-05-15 16:51:37 +0000

    All platforms are MD_DO_NOT_NEED_FALLTHRU now so no need to keep it conditional.

--- lib/csu/m68k_elf/dot_init.h
+++ lib/csu/m68k_elf/dot_init.h
@@ -1,4 +1,4 @@
-/* $NetBSD: dot_init.h,v 1.3 2007/01/02 14:32:34 martin Exp $ */
+/* $NetBSD: dot_init.h,v 1.4 2007/05/15 16:51:40 skrll Exp $ */
 
 /*-
  * Copyright (c) 2001 Ross Harvey
@@ -58,5 +58,3 @@
 
 #define	MD_INIT_SECTION_EPILOGUE MD_SECTION_EPILOGUE(.init)
 #define	MD_FINI_SECTION_EPILOGUE MD_SECTION_EPILOGUE(.fini)
-
-#define	MD_DO_NOT_NEED_FALLTHRU
