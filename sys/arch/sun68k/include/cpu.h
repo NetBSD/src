bd2911249c7af19bd122a9eafa33305a5e1c1c82
tsutsui <tsutsui@NetBSD.org>
2007-05-18 10:18:24 +0000

    Adapt sun68k ports to yamt-idlelwp. Tested on TME (sun2 and sun3) and 3/80 (sun3x).

--- sys/arch/sun68k/include/cpu.h
+++ sys/arch/sun68k/include/cpu.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: cpu.h,v 1.13 2007/02/16 02:53:51 ad Exp $	*/
+/*	$NetBSD: cpu.h,v 1.14 2007/05/18 10:18:26 tsutsui Exp $	*/
 
 /*
  * Copyright (c) 1982, 1990 The Regents of the University of California.
@@ -107,6 +107,7 @@ struct cpu_info {
 	struct cpu_data ci_data;	/* MI per-cpu data */
 	int	ci_mtx_count;
 	int	ci_mtx_oldspl;
+	int	ci_want_resched;
 };
 
 extern struct cpu_info cpu_info_store;
@@ -152,8 +153,8 @@ extern int astpending;	 /* need to trap before returning to user mode */
  * Preempt the current process if in interrupt from user mode,
  * or after the current trap/syscall if in system mode.
  */
-extern int want_resched;	 /* resched() was called */
-#define	cpu_need_resched(ci)	{ want_resched = 1; aston(); }
+#define	cpu_need_resched(ci, flags)	\
+	do { ci->ci_want_resched = 1; aston(); } while (/* CONSTCOND */0)
 
 /*
  * Give a profiling tick to the current process when the user profiling
@@ -188,8 +189,6 @@ void	netintr(void);
 struct pcb;
 void	proc_trampoline(void);
 void	savectx(struct pcb *);
-void	switch_exit(struct lwp *);
-void	switch_lwp_exit(struct lwp *);
 
 #ifndef _SUN3X_
 #define M68K_VAC
