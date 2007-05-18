570ffc1b5bee87d19eff1d4416d5b9e57e03e966
tsutsui <tsutsui@NetBSD.org>
2007-05-18 10:45:58 +0000

    Update comments to sync yamt-km merge (Sysmap has been moved).
    See also:
    http://mail-index.netbsd.org/source-changes/2005/02/23/0004.html

--- sys/arch/hp300/hp300/pmap_bootstrap.c
+++ sys/arch/hp300/hp300/pmap_bootstrap.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: pmap_bootstrap.c,v 1.30 2007/03/05 12:50:15 tsutsui Exp $	*/
+/*	$NetBSD: pmap_bootstrap.c,v 1.31 2007/05/18 10:45:58 tsutsui Exp $	*/
 
 /*
  * Copyright (c) 1991, 1993
@@ -36,7 +36,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: pmap_bootstrap.c,v 1.30 2007/03/05 12:50:15 tsutsui Exp $");
+__KERNEL_RCSID(0, "$NetBSD: pmap_bootstrap.c,v 1.31 2007/05/18 10:45:58 tsutsui Exp $");
 
 #include <sys/param.h>
 #include <sys/proc.h>
@@ -386,7 +386,7 @@ pmap_bootstrap(paddr_t nextpa, paddr_t firstpa)
 		(pt_entry_t *)(kptmpa - firstpa);
 	/*
 	 * Sysmap: kernel page table (as mapped through Sysptmap)
-	 * Immediately follows `nptpages' of static kernel page table.
+	 * Allocated at the end of KVA space.
 	 */
 	RELOC(Sysmap, pt_entry_t *) =
 	    (pt_entry_t *)m68k_ptob((NPTEPG - 2) * NPTEPG);
