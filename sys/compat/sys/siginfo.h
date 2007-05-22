3a01f6835425f48e53a305074ae686885f514c87
cube <cube@NetBSD.org>
2007-05-22 03:51:54 +0000

    Rename si_sigval -> si_value to match POSIX RTS.  [hi christos!]

--- sys/compat/sys/siginfo.h
+++ sys/compat/sys/siginfo.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: siginfo.h,v 1.2 2005/12/11 12:20:29 christos Exp $	 */
+/*	$NetBSD: siginfo.h,v 1.3 2007/05/22 03:51:54 cube Exp $	 */
 
 /*-
  * Copyright (c) 2002 The NetBSD Foundation, Inc.
@@ -59,7 +59,7 @@ struct __ksiginfo32 {
 		struct {
 			pid_t _pid;
 			uid_t _uid;
-			sigval32_t _sigval;
+			sigval32_t _value;
 		} _rt;
 
 		struct {
