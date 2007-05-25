bc775ec466d350e5af6de11f0e8a65e8d830ea1e
nisimura <nisimura>
2007-05-25 12:26:09 +0000

    - __HAVE_DEVICE_REGISTER. - allow bootinfo to pass cookie value to determine rootdev. It's   a PCI tag encoded value (bus:dev:func) when netboot case. - nuke some defunct comments whose origin can be traced back to the   age of VAX bus hose plumbering. - should re-visit when diskboot is being made.

--- sys/arch/sandpoint/include/types.h
+++ sys/arch/sandpoint/include/types.h
@@ -1,7 +1,9 @@
-/*	$NetBSD: types.h,v 1.4.18.1 2007/05/04 10:34:14 nisimura Exp $	*/
+/*	$NetBSD: types.h,v 1.4.18.2 2007/05/25 12:26:09 nisimura Exp $	*/
+
+#define __HAVE_DEVICE_REGISTER
+#define __HAVE_GENERIC_SOFT_INTERRUPTS
+#define __HAVE_TIMECOUNTER
+#define __HAVE_GENERIC_TODR
 
 #include <powerpc/types.h>
 
-#define __HAVE_GENERIC_SOFT_INTERRUPTS
-#define __HAVE_GENERIC_TODR
-#define __HAVE_TIMECOUNTER
