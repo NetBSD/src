17b8ce1eb47ac9c1808ccec69018c339fe12315e
tnozaki <tnozaki>
2007-05-26 13:14:13 +0000

    s/approprate/appropriate/

--- lib/libc/locale/localeconv.c
+++ lib/libc/locale/localeconv.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: localeconv.c,v 1.13 2005/11/29 03:11:59 christos Exp $	*/
+/*	$NetBSD: localeconv.c,v 1.14 2007/05/26 13:14:13 tnozaki Exp $	*/
 
 /*
  * Written by J.T. Conklin <jtc@NetBSD.org>.
@@ -7,7 +7,7 @@
 
 #include <sys/cdefs.h>
 #if defined(LIBC_SCCS) && !defined(lint)
-__RCSID("$NetBSD: localeconv.c,v 1.13 2005/11/29 03:11:59 christos Exp $");
+__RCSID("$NetBSD: localeconv.c,v 1.14 2007/05/26 13:14:13 tnozaki Exp $");
 #endif /* LIBC_SCCS and not lint */
 
 #include <sys/localedef.h>
@@ -18,7 +18,7 @@ __RCSID("$NetBSD: localeconv.c,v 1.13 2005/11/29 03:11:59 christos Exp $");
  * monetary and numeric locales.
  *
  * Because localeconv() may be called many times (especially by library
- * routines like printf() & strtod()), the approprate members of the 
+ * routines like printf() & strtod()), the appropriate members of the 
  * lconv structure are computed only when the monetary or numeric 
  * locale has been changed.
  */
