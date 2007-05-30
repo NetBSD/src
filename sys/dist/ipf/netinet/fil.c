625c9f7515445d871c62ba12d010d2667741fbf9
pavel <pavel>
2007-05-30 21:48:21 +0000

    Pull up following revision(s) (requested by darrenr in ticket #688): 	sys/dist/ipf/netinet/fil.c: revision 1.35 the call to ipf_freetoken() free's the structure pointed to by 't' so doing any sort of dereference of it afterwards is just a bad idea...

--- sys/dist/ipf/netinet/fil.c
+++ sys/dist/ipf/netinet/fil.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: fil.c,v 1.28.2.2 2007/05/22 22:52:03 pavel Exp $	*/
+/*	$NetBSD: fil.c,v 1.28.2.3 2007/05/30 21:48:21 pavel Exp $	*/
 
 /*
  * Copyright (C) 1993-2003 by Darren Reed.
@@ -154,7 +154,7 @@ struct file;
 #if !defined(lint)
 #if defined(__NetBSD__)
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: fil.c,v 1.28.2.2 2007/05/22 22:52:03 pavel Exp $");
+__KERNEL_RCSID(0, "$NetBSD: fil.c,v 1.28.2.3 2007/05/30 21:48:21 pavel Exp $");
 #else
 static const char sccsid[] = "@(#)fil.c	1.36 6/5/96 (C) 1993-2000 Darren Reed";
 static const char rcsid[] = "@(#)Id: fil.c,v 2.243.2.104 2007/05/11 13:41:51 darrenr Exp";
@@ -6782,8 +6782,8 @@ int ipf_getnextrule(ipftoken_t *t, void *ptr)
 			next = &zero;
 			ipf_freetoken(t);
 			fr = NULL;
+			t = NULL;
 			count = 1;
-			t->ipt_data = next;
 		}
 		RWLOCK_EXIT(&ipf_mutex);
 
