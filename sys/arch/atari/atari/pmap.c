d69742200a31b6e307570236bf5645a9cf2ee1b9
riz <riz>
2007-05-30 22:58:05 +0000

    Pull up following revision(s) (requested by mhitch in ticket #682): 	sys/arch/atari/atari/pmap.c: revision 1.99 	sys/arch/amiga/amiga/pmap.c: revision 1.125 From pmap_motorols,c: Allow pmap_enter_ptpage to fail, sometimes. Pass WAIT/NOWAIT flags and fix some obvious locking problems. This allows us to run a LOCKDEBUG ernel now.

--- sys/arch/atari/atari/pmap.c
+++ sys/arch/atari/atari/pmap.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: pmap.c,v 1.94 2006/09/16 18:53:53 mhitch Exp $	*/
+/*	$NetBSD: pmap.c,v 1.94.4.1 2007/05/30 22:58:05 riz Exp $	*/
 
 /*-
  * Copyright (c) 1999 The NetBSD Foundation, Inc.
@@ -107,7 +107,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: pmap.c,v 1.94 2006/09/16 18:53:53 mhitch Exp $");
+__KERNEL_RCSID(0, "$NetBSD: pmap.c,v 1.94.4.1 2007/05/30 22:58:05 riz Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -298,7 +298,7 @@ extern	caddr_t	msgbufaddr;
 extern	vaddr_t	msgbufpa;
 
 static boolean_t	pmap_testbit __P((paddr_t, int));
-static void		pmap_enter_ptpage __P((pmap_t, vaddr_t));
+static int		pmap_enter_ptpage __P((pmap_t, vaddr_t, boolean_t));
 static struct pv_entry* pmap_alloc_pv __P((void));
 static void		pmap_free_pv __P((struct pv_entry *));
 static void		pmap_pinit __P((pmap_t));
@@ -1078,6 +1078,7 @@ pmap_enter(pmap, va, pa, prot, flags)
 	boolean_t cacheable = TRUE;
 	boolean_t checkpv = TRUE;
 	boolean_t wired = (flags & PMAP_WIRED) != 0;
+	boolean_t can_fail = (flags & PMAP_CANFAIL) != 0;
 
 #ifdef DEBUG
 	if (pmapdebug & (PDB_FOLLOW|PDB_ENTER))
@@ -1094,13 +1095,19 @@ pmap_enter(pmap, va, pa, prot, flags)
 	if (pmap->pm_ptab == NULL)
 		pmap->pm_ptab = (u_int *)
 		    uvm_km_alloc(pt_map, ATARI_UPTSIZE, 0,
-		    UVM_KMF_VAONLY | UVM_KMF_WAITVA);
+		    UVM_KMF_VAONLY | 
+		    (can_fail ? UVM_KMF_NOWAIT : UVM_KMF_WAITVA));
+		if (pmap->pm_ptab == NULL)
+			return ENOMEM;
 
 	/*
 	 * Segment table entry not valid, we need a new PT page
 	 */
-	if (!pmap_ste_v(pmap, va))
-		pmap_enter_ptpage(pmap, va);
+	if (!pmap_ste_v(pmap, va)) {
+		int err = pmap_enter_ptpage(pmap, va, can_fail);
+		if (err)
+			return err;
+	}
 
 	pte = pmap_pte(pmap, va);
 	opa = pmap_pte_pa(pte);
@@ -1346,7 +1353,7 @@ pmap_kenter_pa(va, pa, prot)
 
 	if (!pmap_ste_v(pmap, va)) { 
 		s = splvm();
-		pmap_enter_ptpage(pmap, va);
+		pmap_enter_ptpage(pmap, va, false);
 		splx(s);
 	}
 
@@ -2058,7 +2065,9 @@ pmap_remove_mapping(pmap, va, pte, flags)
 #endif
 			pmap_remove_mapping(pmap_kernel(), ptpva,
 			    NULL, PRM_TFLUSH|PRM_CFLUSH);
+			simple_lock(&uvm.kernel_object->vmobjlock);
 			uvm_pagefree(PHYS_TO_VM_PAGE(paddr));
+			simple_unlock(&uvm.kernel_object->vmobjlock);
 #ifdef DEBUG
 			if (pmapdebug & (PDB_REMOVE|PDB_PTPAGE))
 			    printf("remove: PT page 0x%lx (0x%lx) freed\n",
@@ -2380,10 +2389,11 @@ pmap_changebit(pa, bit, setem)
 	splx(s);
 }
 
-static void
-pmap_enter_ptpage(pmap, va)
-	register pmap_t pmap;
-	register vaddr_t va;
+static int
+pmap_enter_ptpage(pmap, va, can_fail)
+	pmap_t pmap;
+	vaddr_t va;
+	boolean_t can_fail;
 {
 	paddr_t ptpa;
 	struct vm_page *pg;
@@ -2409,7 +2419,12 @@ pmap_enter_ptpage(pmap, va)
 	if (pmap->pm_stab == Segtabzero) {
 		pmap->pm_stab = (u_int *)
 		    uvm_km_alloc(kernel_map, ATARI_STSIZE, 0,
-		    UVM_KMF_WIRED | UVM_KMF_ZERO);
+		    UVM_KMF_WIRED | UVM_KMF_ZERO |
+		    (can_fail ? UVM_KMF_NOWAIT : 0));
+		if (pmap->pm_stab == NULL) {
+			pmap->pm_stab = Segtabzero;
+			return ENOMEM;
+		}
 		(void) pmap_extract(pmap_kernel(),
 		    (vaddr_t)pmap->pm_stab, (paddr_t *)&pmap->pm_stpa);
 #if defined(M68040) || defined(M68060)
@@ -2554,11 +2569,15 @@ pmap_enter_ptpage(pmap, va)
 		if (pmapdebug & (PDB_ENTER|PDB_PTPAGE))
 			printf("enter_pt: about to alloc UPT pg at %lx\n", va);
 #endif
+		simple_lock(&uvm.kernel_object->vmobjlock);
 		while ((pg = uvm_pagealloc(uvm.kernel_object,
 					   va - vm_map_min(kernel_map),
 					   NULL, UVM_PGA_ZERO)) == NULL) {
+			simple_unlock(&uvm.kernel_object->vmobjlock);
 			uvm_wait("ptpage");
+			simple_lock(&uvm.kernel_object->vmobjlock);
 		}
+		simple_unlock(&uvm.kernel_object->vmobjlock);
 		pg->flags &= ~(PG_BUSY|PG_FAKE);
 		UVM_PAGE_OWN(pg, NULL);
 		ptpa = VM_PAGE_TO_PHYS(pg);
@@ -2638,6 +2657,8 @@ pmap_enter_ptpage(pmap, va)
 		TBIAU();
 	pmap->pm_ptpages++;
 	splx(s);
+
+	return 0;
 }
 
 /*
