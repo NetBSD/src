68ae1453f29c342da512f7063c6b3bed4521c537
christos <christos@NetBSD.org>
2007-05-31 21:51:48 +0000

    ssp is now part of libc.

---
+++ regress/lib/libc/ssp/strncat/strncat.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int
+main(int argc, char *argv[])
+{
+	char b[10];
+	int len = atoi(argv[1]);
+	(void)strcpy(b, "1");
+	(void)strncat(b, "1020202020202", len);
+
+	(void)printf("%*.*s\n", len, len, b);
+	return 0;
+}
