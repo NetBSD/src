abcf217fc3c84ca46cc07f6bf2cd48364f61febd
xtraeme <xtraeme@NetBSD.org>
2007-06-04 14:34:37 +0000

    Print Intel CPUID extended features if they are available, also simplify ci_feature2_flags and use directly CPUID2_FLAGS on bitmask_snprintf(9).
    Now I can see the same CPUID features than on i386:
    
    $ dmesg|grep -E '(cpu0.*features)'
    cpu0: features: bffbfbff<FPU,VME,DE,PSE,TSC,MSR,PAE,MCE,CX8,APIC,SEP,MTRR>
    cpu0: features: bffbfbff<PGE,MCA,CMOV,PAT,PSE36,CFLUSH,B20,DS,ACPI,MMX>
    cpu0: features: bffbfbff<FXSR,SSE,SSE2,SS,HTT,TM,SBF>
    cpu0: features2: 649d<SSE3,MONITOR,DS-CPL,EST,CID,xTPR>
    cpu0: features3: bffbfbff<SYSCALL/SYSRET,XD,EM64T>
    $
    
    Ok'ed by Andrew Doran.

--- sys/arch/amd64/amd64/identcpu.c
+++ sys/arch/amd64/amd64/identcpu.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: identcpu.c,v 1.20 2007/06/03 19:32:28 xtraeme Exp $	*/
+/*	$NetBSD: identcpu.c,v 1.21 2007/06/04 14:34:37 xtraeme Exp $	*/
 
 /*
  * Copyright (c) 2003 Wasabi Systems, Inc.
@@ -36,7 +36,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: identcpu.c,v 1.20 2007/06/03 19:32:28 xtraeme Exp $");
+__KERNEL_RCSID(0, "$NetBSD: identcpu.c,v 1.21 2007/06/04 14:34:37 xtraeme Exp $");
 
 #include "opt_enhanced_speedstep.h"
 #include "opt_intel_odcm.h"
@@ -62,7 +62,7 @@ identifycpu(struct cpu_info *ci)
 	char buf[512];
 	u_int32_t brand[12];
 	int vendor;
-	const char *feature_str[3], *feature2_str[1];
+	const char *feature_str[3];
 
 	CPUID(1, ci->ci_signature, val,
 	    ci->ci_feature2_flags, ci->ci_feature_flags);
@@ -90,7 +90,8 @@ identifycpu(struct cpu_info *ci)
 	aprint_normal("%s: %s", ci->ci_dev->dv_xname, cpu_model);
 
 	if (ci->ci_tsc_freq != 0)
-		aprint_normal(", %lu.%02lu MHz", (ci->ci_tsc_freq + 4999) / 1000000,
+		aprint_normal(", %lu.%02lu MHz",
+		    (ci->ci_tsc_freq + 4999) / 1000000,
 		    ((ci->ci_tsc_freq + 4999) / 10000) % 100);
 	aprint_normal("\n");
 
@@ -103,8 +104,6 @@ identifycpu(struct cpu_info *ci)
 		feature_str[1] = CPUID_EXT_FLAGS2;
 		feature_str[2] = CPUID_EXT_FLAGS3;
 	}
-
-	feature2_str[0] = CPUID2_FLAGS;
 
 	if ((ci->ci_feature_flags & CPUID_MASK1) != 0) {
 		bitmask_snprintf(ci->ci_feature_flags,
@@ -124,8 +123,14 @@ identifycpu(struct cpu_info *ci)
 
 	if (ci->ci_feature2_flags) {
 		bitmask_snprintf(ci->ci_feature2_flags,
-		    feature2_str[0], buf, sizeof(buf));
+		    CPUID2_FLAGS, buf, sizeof(buf));
 		aprint_normal("%s: features2: %s\n", ci->ci_dev->dv_xname, buf);
+	}
+
+	if ((ci->ci_feature_flags & CPUID_MASK4) != 0) {
+		bitmask_snprintf(ci->ci_feature_flags,
+		    CPUID_FLAGS4, buf, sizeof(buf));
+		aprint_normal("%s: features3: %s\n", ci->ci_dev->dv_xname, buf);
 	}
 
 	x86_print_cacheinfo(ci);
