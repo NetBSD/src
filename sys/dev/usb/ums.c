a3f1b914afeaf44f31396e8230d13a43810686a4
itohy <itohy@NetBSD.org>
2007-06-17 01:19:34 +0000

    - Pullup 1.68 in a different way. - struct lwp * -> usb_proc_ptr

--- sys/dev/usb/ums.c
+++ sys/dev/usb/ums.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: ums.c,v 1.67 2006/11/16 01:33:27 christos Exp $	*/
+/*	$NetBSD: ums.c,v 1.67.10.1 2007/06/17 01:19:34 itohy Exp $	*/
 
 /*
  * Copyright (c) 1998 The NetBSD Foundation, Inc.
@@ -42,7 +42,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: ums.c,v 1.67 2006/11/16 01:33:27 christos Exp $");
+__KERNEL_RCSID(0, "$NetBSD: ums.c,v 1.67.10.1 2007/06/17 01:19:34 itohy Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -118,7 +118,7 @@ Static void ums_intr(struct uhidev *addr, void *ibuf, u_int len);
 
 Static int	ums_enable(void *);
 Static void	ums_disable(void *);
-Static int	ums_ioctl(void *, u_long, caddr_t, int, struct lwp * );
+Static int	ums_ioctl(void *, u_long, usb_ioctlarg_t, int, usb_proc_ptr);
 
 const struct wsmouse_accessops ums_accessops = {
 	ums_enable,
@@ -379,8 +379,7 @@ ums_disable(void *v)
 }
 
 Static int
-ums_ioctl(void *v, u_long cmd, caddr_t data, int flag,
-    struct lwp * p)
+ums_ioctl(void *v, u_long cmd, usb_ioctlarg_t data, int flag, usb_proc_ptr p)
 
 {
 	switch (cmd) {
