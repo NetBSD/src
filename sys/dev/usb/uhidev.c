e66154fef3589028105f044b2e74e0d952f1013f
itohy <itohy@NetBSD.org>
2007-06-21 15:17:29 +0000

    Pullup 1.35: use generic submatch function

--- sys/dev/usb/uhidev.c
+++ sys/dev/usb/uhidev.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: uhidev.c,v 1.33.10.2 2007/06/18 13:49:20 itohy Exp $	*/
+/*	$NetBSD: uhidev.c,v 1.33.10.3 2007/06/21 15:17:29 itohy Exp $	*/
 
 /*
  * Copyright (c) 2001 The NetBSD Foundation, Inc.
@@ -42,7 +42,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: uhidev.c,v 1.33.10.2 2007/06/18 13:49:20 itohy Exp $");
+__KERNEL_RCSID(0, "$NetBSD: uhidev.c,v 1.33.10.3 2007/06/21 15:17:29 itohy Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -82,8 +82,6 @@ Static void uhidev_intr(usbd_xfer_handle, usbd_private_handle, usbd_status);
 
 Static int uhidev_maxrepid(void *, int);
 Static int uhidevprint(void *, const char *);
-Static int uhidevsubmatch(struct device *, struct cfdata *,
-			  const int *, void *);
 
 USB_DECLARE_DRIVER(uhidev);
 
@@ -304,7 +302,7 @@ nomem:
 
 			dev = (struct uhidev *)config_found_sm_loc(self,
 				"uhidbus", locs, &uha,
-				uhidevprint, uhidevsubmatch);
+				uhidevprint, config_stdsubmatch);
 			sc->sc_subdevs[repid] = dev;
 			if (dev != NULL) {
 				dev->sc_in_rep_size = repsizes[repid];
@@ -357,17 +355,6 @@ uhidevprint(void *aux, const char *pnp)
 	if (uha->reportid != 0)
 		aprint_normal(" reportid %d", uha->reportid);
 	return (UNCONF);
-}
-
-int
-uhidevsubmatch(struct device *parent, struct cfdata *cf,
-	       const int *locs, void *aux)
-{
-	if (cf->cf_loc[UHIDBUSCF_REPORTID] != UHIDBUSCF_REPORTID_DEFAULT &&
-	    cf->cf_loc[UHIDBUSCF_REPORTID] != locs[UHIDBUSCF_REPORTID])
-		return (0);
-
-	return (config_match(parent, cf, aux));
 }
 
 int
