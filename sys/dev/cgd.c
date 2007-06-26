a57520427256459f376c03a2cd3952b2c85c2921
cube <cube@NetBSD.org>
2007-06-26 15:22:23 +0000

    Change dk_lookup() to accept an additional argument of the type enum uio_seg that tells whether the given path is in user space or kernel space, so it can tell NDINIT().
    While the raidframe calls were ok, both ccd(4) and cgd(4) were passing
    pointers to user space data, which leads to strange error on i386, as
    reported by Jukka Salmi on current-users.
    
    The issue has been there since last august, I'm actually a bit surprised
    that no one in the meantime has used ccd(4) or cgd(4) on an arch where it
    would have simply faulted.

--- sys/dev/cgd.c
+++ sys/dev/cgd.c
@@ -1,4 +1,4 @@
-/* $NetBSD: cgd.c,v 1.44 2007/03/04 06:01:41 christos Exp $ */
+/* $NetBSD: cgd.c,v 1.45 2007/06/26 15:22:24 cube Exp $ */
 
 /*-
  * Copyright (c) 2002 The NetBSD Foundation, Inc.
@@ -37,7 +37,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: cgd.c,v 1.44 2007/03/04 06:01:41 christos Exp $");
+__KERNEL_RCSID(0, "$NetBSD: cgd.c,v 1.45 2007/06/26 15:22:24 cube Exp $");
 
 #include <sys/types.h>
 #include <sys/param.h>
@@ -504,7 +504,7 @@ cgd_ioctl_set(struct cgd_softc *cs, void *data, struct lwp *l)
 	char	 *inbuf;
 
 	cp = ci->ci_disk;
-	if ((ret = dk_lookup(cp, l, &vp)) != 0)
+	if ((ret = dk_lookup(cp, l, &vp, UIO_USERSPACE)) != 0)
 		return ret;
 
 	inbuf = malloc(MAX_KEYSIZE, M_TEMP, M_WAITOK);
