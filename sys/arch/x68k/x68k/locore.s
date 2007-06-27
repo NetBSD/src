0810535058f9ed014cfc0041f4230e930208e42d
mhitch <mhitch@NetBSD.org>
2007-06-27 17:55:51 +0000

    Ooops - I missed another call to trap.  Fixes PR#36567.

--- sys/arch/x68k/x68k/locore.s
+++ sys/arch/x68k/x68k/locore.s
@@ -1,4 +1,4 @@
-/*	$NetBSD: locore.s,v 1.79 2007/06/12 03:34:33 mhitch Exp $	*/
+/*	$NetBSD: locore.s,v 1.80 2007/06/27 17:55:51 mhitch Exp $	*/
 
 /*
  * Copyright (c) 1980, 1990, 1993
@@ -721,8 +721,9 @@ Lsir1:
 	clrl	%sp@-			| VA == none
 	clrl	%sp@-			| code == none
 	movl	#T_SSIR,%sp@-		| type == software interrupt
+	pea	%sp@(12)		| fp = trap frame address
 	jbsr	_C_LABEL(trap)		| go handle it
-	lea	%sp@(12),%sp		| pop value args
+	lea	%sp@(16),%sp		| pop value args
 	movl	%sp@(FR_SP),%a0		| restore
 	movl	%a0,%usp		|   user SP
 	moveml	%sp@+,#0x7FFF		| and all remaining registers
