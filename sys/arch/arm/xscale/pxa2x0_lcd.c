91bb4835b98465258e29011e44110139f7fee95d
nonaka <nonaka@NetBSD.org>
2007-06-28 15:05:27 +0000

    use LCCR3_BPP3 instead of magic number.

--- sys/arch/arm/xscale/pxa2x0_lcd.c
+++ sys/arch/arm/xscale/pxa2x0_lcd.c
@@ -1,4 +1,4 @@
-/* $NetBSD: pxa2x0_lcd.c,v 1.22 2007/06/28 14:47:47 nonaka Exp $ */
+/* $NetBSD: pxa2x0_lcd.c,v 1.23 2007/06/28 15:05:27 nonaka Exp $ */
 
 /*
  * Copyright (c) 2002  Genetec Corporation.  All rights reserved.
@@ -38,7 +38,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: pxa2x0_lcd.c,v 1.22 2007/06/28 14:47:47 nonaka Exp $");
+__KERNEL_RCSID(0, "$NetBSD: pxa2x0_lcd.c,v 1.23 2007/06/28 15:05:27 nonaka Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -101,7 +101,7 @@ pxa2x0_lcd_geometry(struct pxa2x0_lcd_softc *sc,
 
 	ccr0 = LCCR0_IMASK;
 	if (CPU_IS_PXA270)
-		ccr0 |= LCCR0_CMDIM|LCCR0_RDSTM;
+		ccr0 |= LCCR0_CMDIM|LCCR0_RDSTM|LCCR0_LDDALT;
 	if (info->panel_info & LCDPANEL_ACTIVE)
 		ccr0 |= LCCR0_PAS;	/* active mode */
 	if ((info->panel_info & (LCDPANEL_DUAL|LCDPANEL_ACTIVE))
@@ -338,7 +338,7 @@ pxa2x0_lcd_start_dma(struct pxa2x0_lcd_softc *sc,
 	tmp = bus_space_read_4(iot, ioh, LCDC_LCCR3);
 	if (CPU_IS_PXA270) {
 		bus_space_write_4(iot, ioh, LCDC_LCCR3, 
-		  (tmp & ~(LCCR3_BPP|(1<<29))) | (val << LCCR3_BPP_SHIFT));
+		  (tmp & ~(LCCR3_BPP|LCCR3_BPP3)) | (val << LCCR3_BPP_SHIFT));
 	} else {
 		bus_space_write_4(iot, ioh, LCDC_LCCR3, 
 		    (tmp & ~LCCR3_BPP) | (val << LCCR3_BPP_SHIFT));
