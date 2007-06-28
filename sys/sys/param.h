71d0915144a7c9a8ce2f2a14ba614bfc7c41a20d
ghen <ghen>
2007-06-28 18:40:53 +0000

    Welcome to NetBSD 3.0.3.

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.211.2.8.2.6 2007/06/28 11:43:22 ghen Exp $	*/
+/*	$NetBSD: param.h,v 1.211.2.8.2.7 2007/06/28 18:40:53 ghen Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -63,7 +63,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	300002001	/* NetBSD 3.0.2_PATCH */
+#define	__NetBSD_Version__	301003000	/* NetBSD 3.0.3 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) >= __NetBSD_Version__)
