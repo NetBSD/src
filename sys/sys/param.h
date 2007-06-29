40b84d4b7f1965cb54aa9e30ad9ce73f50c92400
ghen <ghen@NetBSD.org>
2007-06-29 16:59:04 +0000

    Welcome to NetBSD 3.1.1_PATCH.

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.211.2.14.2.3 2007/06/28 18:40:57 ghen Exp $	*/
+/*	$NetBSD: param.h,v 1.211.2.14.2.4 2007/06/29 16:59:04 ghen Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -63,7 +63,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	301001000	/* NetBSD 3.1.1 */
+#define	__NetBSD_Version__	301001000	/* NetBSD 3.1.1_PATCH */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) >= __NetBSD_Version__)
