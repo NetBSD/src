fa8fe84c48b122daa2280b5876584c136449dbee
dsl <dsl>
2007-06-30 15:27:02 +0000

    Updates for changes prototype of kauth_cred_set/getgroups().

--- sys/miscfs/portal/portal_vnops.c
+++ sys/miscfs/portal/portal_vnops.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: portal_vnops.c,v 1.70 2007/02/09 21:55:36 ad Exp $	*/
+/*	$NetBSD: portal_vnops.c,v 1.71 2007/06/30 15:27:03 dsl Exp $	*/
 
 /*
  * Copyright (c) 1992, 1993
@@ -40,7 +40,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: portal_vnops.c,v 1.70 2007/02/09 21:55:36 ad Exp $");
+__KERNEL_RCSID(0, "$NetBSD: portal_vnops.c,v 1.71 2007/06/30 15:27:03 dsl Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -389,7 +389,8 @@ portal_open(v)
 	pcred.pcr_uid = kauth_cred_geteuid(ap->a_cred);
 	pcred.pcr_gid = kauth_cred_getegid(ap->a_cred);
 	pcred.pcr_ngroups = kauth_cred_ngroups(ap->a_cred);
-	kauth_cred_getgroups(ap->a_cred, pcred.pcr_groups, pcred.pcr_ngroups);
+	kauth_cred_getgroups(ap->a_cred, pcred.pcr_groups, pcred.pcr_ngroups,
+	    UIO_SYSSPACE);
 	aiov[0].iov_base = &pcred;
 	aiov[0].iov_len = sizeof(pcred);
 	aiov[1].iov_base = pt->pt_arg;
