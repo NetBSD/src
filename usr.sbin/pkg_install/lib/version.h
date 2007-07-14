2619e0b9954305effb7fa499fb37d59f1c1181a7
joerg <joerg@NetBSD.org>
2007-07-14 22:53:27 +0000

    When fixing vsftpd, other FTP server were broken as they return different error codes for unmatching glob patterns. Basically ignore the error code and just try the other pattern. chdir errors are still considered fatal.
    Bump version to 20070715.

--- usr.sbin/pkg_install/lib/version.h
+++ usr.sbin/pkg_install/lib/version.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: version.h,v 1.85 2007/07/14 20:03:41 adrianp Exp $	*/
+/*	$NetBSD: version.h,v 1.86 2007/07/14 22:53:28 joerg Exp $	*/
 
 /*
  * Copyright (c) 2001 Thomas Klausner.  All rights reserved.
@@ -33,6 +33,6 @@
 #ifndef _INST_LIB_VERSION_H_
 #define _INST_LIB_VERSION_H_
 
-#define PKGTOOLS_VERSION "20070714"
+#define PKGTOOLS_VERSION "20070715"
 
 #endif /* _INST_LIB_VERSION_H_ */
