7e7f52bb34df31cbed51f20354af9a09c235cd0c
dsl <dsl>
2007-07-15 08:40:22 +0000

    Default MEMORY_DISK_SERVER to 1 instead of using 'undefined' to mean 1.

--- sys/dev/md.c
+++ sys/dev/md.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: md.c,v 1.48 2007/03/04 06:01:42 christos Exp $	*/
+/*	$NetBSD: md.c,v 1.49 2007/07/15 08:40:22 dsl Exp $	*/
 
 /*
  * Copyright (c) 1995 Gordon W. Ross, Leo Weppelman.
@@ -46,7 +46,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: md.c,v 1.48 2007/03/04 06:01:42 christos Exp $");
+__KERNEL_RCSID(0, "$NetBSD: md.c,v 1.49 2007/07/15 08:40:22 dsl Exp $");
 
 #include "opt_md.h"
 
@@ -67,11 +67,11 @@ __KERNEL_RCSID(0, "$NetBSD: md.c,v 1.48 2007/03/04 06:01:42 christos Exp $");
 #include <dev/md.h>
 
 /*
- * By default, include the user-space functionality.
+ * The user-space functionality is included by default.
  * Use  `options MEMORY_DISK_SERVER=0' to turn it off.
  */
 #ifndef MEMORY_DISK_SERVER
-#define	MEMORY_DISK_SERVER 1
+#error MEMORY_DISK_SERVER should be defined by opt_md.h
 #endif	/* MEMORY_DISK_SERVER */
 
 /*
