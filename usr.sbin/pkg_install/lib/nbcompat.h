8e66919a30b8c6850261c0f38d4cd7ab88442a80
joerg <joerg@NetBSD.org>
2007-07-16 19:33:02 +0000

    Switch to src/dist as primary src location of pkg_install. Adjust the Makefiles to be reachover Makefiles and remove some unnecessary warning exceptions.
    audit-packages.conf is stored redundantly in src/etc, not changed yet.
    pkgsrc.7 is also kept in share/man/man7.
    
    Add a comment in lib/nbcompat.h to make the file non-empty.

---
+++ usr.sbin/pkg_install/lib/nbcompat.h
@@ -0,0 +1,6 @@
+/* $NetBSD: nbcompat.h,v 1.2 2007/07/16 19:33:15 joerg Exp $ */
+
+/*
+ * In pkgsrc, libnbcompat is used to provide compatibility functions.
+ * This is not needed on native NetBSD, so provide an empty header.
+ */
