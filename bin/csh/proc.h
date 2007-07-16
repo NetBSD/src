3c0a09d8473d62803bc12a6f8c44f14a8760a47b
christos <christos@NetBSD.org>
2007-07-16 14:07:00 +0000

    PR/36650: Michael van Elst: Get rid of bool, because csh uses bool as a small int.

--- bin/csh/proc.h
+++ bin/csh/proc.h
@@ -1,4 +1,4 @@
-/* $NetBSD: proc.h,v 1.10 2007/07/16 02:26:52 dogcow Exp $ */
+/* $NetBSD: proc.h,v 1.11 2007/07/16 14:07:01 christos Exp $ */
 
 /*-
  * Copyright (c) 1980, 1991, 1993
@@ -91,7 +91,7 @@ struct process {
 #define	AREASON		0200
 
 struct process proclist;	/* list head of all processes */
-cshbool pnoprocesses;		/* pchild found nothing to wait for */
+cshint pnoprocesses;		/* pchild found nothing to wait for */
 
 struct process *pholdjob;	/* one level stack of current jobs */
 
