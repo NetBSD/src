5d00956818bc9a8e163f5c42345ace37d06aa7f1
pooka <pooka>
2007-07-17 11:27:11 +0000

    4.99.24 - set_statvfs_info() vfsname parameter

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.266 2007/07/09 21:13:56 ad Exp $	*/
+/*	$NetBSD: param.h,v 1.267 2007/07/17 11:27:11 pooka Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -63,7 +63,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	499002300	/* NetBSD 4.99.23 */
+#define	__NetBSD_Version__	499002400	/* NetBSD 4.99.24 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
