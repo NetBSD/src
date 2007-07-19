d5950e0fa256a4db0b7f014435d3720b98d3c453
dsl <dsl@NetBSD.org>
2007-07-19 22:17:24 +0000

    include sys/cdefs.h

---
+++ sys/compat/linux32/arch/amd64/linux32_missing.c
@@ -0,0 +1,27 @@
+/*	$NetBSD: linux32_missing.c,v 1.3.22.2 2007/07/19 22:17:24 dsl Exp $ */
+
+#include <sys/cdefs.h>
+
+#include "opt_compat_linux32.h"
+
+#include <sys/types.h>
+#include <sys/time.h>
+#include <sys/param.h>
+#include <sys/lwp.h>
+#include <sys/ucred.h>
+#include <sys/mount.h>
+#include <sys/systm.h>
+#include <sys/syscallargs.h>
+
+#include <compat/netbsd32/netbsd32.h>
+
+#include <compat/linux32/common/linux32_types.h>
+#include <compat/linux32/common/linux32_signal.h>
+
+#include <compat/linux32/arch/amd64/linux32_missing.h>
+#include <compat/linux32/arch/amd64/linux32_syscallargs.h>
+
+#include <compat/linux/common/linux_file64.c>
+#include <compat/linux/common/linux_llseek.c>
+#include <compat/linux/common/linux_misc_notalpha.c>
+#include <compat/linux/common/linux_misc.c>
