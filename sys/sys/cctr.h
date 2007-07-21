6bc7bb31ff39dd0dfe727948241fe6b1bd4107b4
tsutsui <tsutsui@NetBSD.org>
2007-07-21 11:47:08 +0000

    Add sys/kern/kern_cctr.c, which contains MI cpu cyclecounter based timecounter(9) support functions split out from sys/arch/x86/x86/tsc.c. Written by and ok'ed to commit by kardel@.
    Also add <sys/cctr.h>, which is copied from #ifdef __HAVE_TIMECOUNTER
    part of <sys/cc_microtime.h> to migrate MI kern_cctr.c per each port.

---
+++ sys/sys/cctr.h
@@ -0,0 +1,57 @@
+/*	$NetBSD: cctr.h,v 1.1.34.2 2007/07/21 11:47:09 tsutsui Exp $	*/
+
+/*-
+ * Copyright (c) 2004 The NetBSD Foundation, Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *        This product includes software developed by the NetBSD
+ *        Foundation, Inc. and its contributors.
+ * 4. Neither the name of The NetBSD Foundation nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef _SYS_CCTR_H_
+#define	_SYS_CCTR_H_
+
+/*
+ * Variables used by cycle counter in kern_cctr.c.
+ */
+struct cctr_state {
+	volatile u_int   cc_gen;   /* generation number for this data set */
+	volatile int64_t cc_val;   /* reference CC value at calibration time */
+	volatile int64_t cc_cc;	   /* local CC value at calibration time */
+	volatile int64_t cc_delta; /* reference CC difference for
+				      last calibration period */
+	volatile int64_t cc_denom; /* local CC difference for
+				      last calibration period */
+};
+
+struct cpu_info;
+
+void cc_calibrate_cpu(struct cpu_info *);
+struct timecounter *cc_init(uint64_t, const char *, int);
+
+#endif /* _SYS_CCTR_H_ */
