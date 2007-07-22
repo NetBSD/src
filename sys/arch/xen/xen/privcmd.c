2e190f1794f50020db05c57fc44c7fe95ab30ad6
ad <ad@NetBSD.org>
2007-07-22 08:50:26 +0000

    +#include <sys/proc.h>

--- sys/arch/xen/xen/privcmd.c
+++ sys/arch/xen/xen/privcmd.c
@@ -1,4 +1,4 @@
-/* $NetBSD: privcmd.c,v 1.17 2007/06/12 18:53:52 bouyer Exp $ */
+/* $NetBSD: privcmd.c,v 1.18 2007/07/22 08:50:26 ad Exp $ */
 
 /*-
  * Copyright (c) 2004 Christian Limpach.
@@ -32,7 +32,7 @@
 
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: privcmd.c,v 1.17 2007/06/12 18:53:52 bouyer Exp $");
+__KERNEL_RCSID(0, "$NetBSD: privcmd.c,v 1.18 2007/07/22 08:50:26 ad Exp $");
 
 #include "opt_compat_netbsd.h"
 
@@ -42,6 +42,7 @@ __KERNEL_RCSID(0, "$NetBSD: privcmd.c,v 1.17 2007/06/12 18:53:52 bouyer Exp $");
 #include <sys/malloc.h>
 #include <sys/dirent.h>
 #include <sys/stat.h>
+#include <sys/proc.h>
 
 #include <miscfs/specfs/specdev.h>
 #include <miscfs/kernfs/kernfs.h>
