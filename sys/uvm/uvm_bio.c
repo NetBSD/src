ba1ebbab1cb23e6178cf51eb177e5e9b286da2b8
yamt <yamt@NetBSD.org>
2007-07-27 09:28:20 +0000

    remove a debug printf.

--- sys/uvm/uvm_bio.c
+++ sys/uvm/uvm_bio.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: uvm_bio.c,v 1.56.4.7 2007/07/18 21:39:23 ad Exp $	*/
+/*	$NetBSD: uvm_bio.c,v 1.56.4.8 2007/07/27 09:28:20 yamt Exp $	*/
 
 /*
  * Copyright (c) 1998 Chuck Silvers.
@@ -34,7 +34,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: uvm_bio.c,v 1.56.4.7 2007/07/18 21:39:23 ad Exp $");
+__KERNEL_RCSID(0, "$NetBSD: uvm_bio.c,v 1.56.4.8 2007/07/27 09:28:20 yamt Exp $");
 
 #include "opt_uvmhist.h"
 #include "opt_ubc.h"
@@ -684,7 +684,6 @@ ubc_uiomove(struct uvm_object *uobj, struct uio *uio, vsize_t todo, int flags)
 			 * do it now.  it's safe to use memset here
 			 * because we just mapped the pages above.
 			 */
-			printf("%s: error=%d\n", __func__, error);
 			memset(win, 0, bytelen);
 		}
 		ubc_release(win, flags);
