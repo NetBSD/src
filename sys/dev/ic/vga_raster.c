fffe391d170eddc5ce9b65a27b574e5be34d5820
mjf <mjf@NetBSD.org>
2007-07-28 20:28:56 +0000

    Implement bounds checking in some places in display driver code to avoid the possibility of a local user panic.

--- sys/dev/ic/vga_raster.c
+++ sys/dev/ic/vga_raster.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: vga_raster.c,v 1.28 2007/07/09 21:00:40 ad Exp $	*/
+/*	$NetBSD: vga_raster.c,v 1.29 2007/07/28 20:28:57 mjf Exp $	*/
 
 /*
  * Copyright (c) 2001, 2002 Bang Jun-Young
@@ -56,7 +56,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: vga_raster.c,v 1.28 2007/07/09 21:00:40 ad Exp $");
+__KERNEL_RCSID(0, "$NetBSD: vga_raster.c,v 1.29 2007/07/28 20:28:57 mjf Exp $");
 
 #include "opt_wsmsgattrs.h" /* for WSDISPLAY_CUSTOM_OUTPUT */
 
@@ -1118,11 +1118,14 @@ void
 vga_raster_putchar(void *id, int row, int col, u_int c, long attr)
 {
 	struct vgascreen *scr = id;
-	int off;
+	size_t off;
 	struct vga_raster_font *fs;
 	u_int tmp_ch;
 
 	off = row * scr->type->ncols + col;
+
+	if (__predict_false(off >= (scr->type->ncols * scr->type->nrows)))
+		return;
 
 	LIST_FOREACH(fs, &scr->fontset, next) {
 		if ((scr->encoding == fs->font->encoding) &&
@@ -1393,6 +1396,10 @@ vga_raster_allocattr(void *id, int fg, int bg, int flags, long *attrp)
 {
 	struct vgascreen *scr = id;
 	struct vga_config *vc = scr->cfg;
+
+	if (__predict_false((unsigned int)fg >= sizeof(fgansitopc) || 
+	    (unsigned int)bg >= sizeof(bgansitopc)))
+	    	return (EINVAL);
 
 	if (vc->hdl.vh_mono) {
 		if (flags & WSATTR_WSCOLORS)
--- sys/dev/rasops/rasops.c
+++ sys/dev/rasops/rasops.c
@@ -1,4 +1,4 @@
-/*	 $NetBSD: rasops.c,v 1.55 2007/02/02 02:10:24 ober Exp $	*/
+/*	 $NetBSD: rasops.c,v 1.56 2007/07/28 20:28:57 mjf Exp $	*/
 
 /*-
  * Copyright (c) 1999 The NetBSD Foundation, Inc.
@@ -37,7 +37,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: rasops.c,v 1.55 2007/02/02 02:10:24 ober Exp $");
+__KERNEL_RCSID(0, "$NetBSD: rasops.c,v 1.56 2007/07/28 20:28:57 mjf Exp $");
 
 #include "opt_rasops.h"
 #include "rasops_glue.h"
@@ -436,6 +436,10 @@ rasops_allocattr_color(void *cookie, int fg, int bg, int flg,
     long *attr)
 {
 	int swap;
+
+	if (__predict_false((unsigned int)fg >= sizeof(rasops_isgray) ||
+	    (unsigned int)bg >= sizeof(rasops_isgray)))
+		return (EINVAL);
 
 #ifdef RASOPS_CLIPPING
 	fg &= 7;
@@ -1256,6 +1260,14 @@ rasops_putchar_rotated(cookie, row, col, uc, attr)
 	int height;
 
 	ri = (struct rasops_info *)cookie;
+
+	if (__predict_false((unsigned int)row > ri->ri_rows ||
+	    (unsigned int)col > ri->ri_cols))
+		return;
+
+	/* Avoid underflow */
+	if ((ri->ri_rows - row - 1) < 0)
+		return;
 
 	/* Do rotated char sans (side)underline */
 	ri->ri_real_ops.putchar(cookie, col, ri->ri_rows - row - 1, uc,
