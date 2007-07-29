6d5f189ff23ceffcb13885a8a215456419bf3cb0
ad <ad@NetBSD.org>
2007-07-29 11:31:54 +0000

    Destroy the map's locks before it's freed.

--- sys/uvm/uvm_map.c
+++ sys/uvm/uvm_map.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: uvm_map.c,v 1.235.2.5 2007/04/13 15:49:50 ad Exp $	*/
+/*	$NetBSD: uvm_map.c,v 1.235.2.6 2007/07/29 11:31:54 ad Exp $	*/
 
 /*
  * Copyright (c) 1997 Charles D. Cranor and Washington University.
@@ -71,7 +71,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: uvm_map.c,v 1.235.2.5 2007/04/13 15:49:50 ad Exp $");
+__KERNEL_RCSID(0, "$NetBSD: uvm_map.c,v 1.235.2.6 2007/07/29 11:31:54 ad Exp $");
 
 #include "opt_ddb.h"
 #include "opt_uvmhist.h"
@@ -4058,6 +4058,10 @@ uvmspace_free(struct vmspace *vm)
 	}
 	KASSERT(map->nentries == 0);
 	KASSERT(map->size == 0);
+	mutex_destroy(&map->misc_lock);
+	mutex_destroy(&map->hint_lock);
+	mutex_destroy(&map->mutex);
+	rw_destroy(&map->lock);
 	pmap_destroy(map->pmap);
 	pool_put(&uvm_vmspace_pool, vm);
 }
@@ -4964,10 +4968,10 @@ uvm_map_setup(struct vm_map *map, vaddr_t vmin, vaddr_t vmax, int flags)
 	if ((flags & VM_MAP_INTRSAFE) != 0) {
 		ipl = IPL_VM;
 	} else {
-		rw_init(&map->lock);
 		ipl = IPL_NONE;
 	}
 
+	rw_init(&map->lock);
 	cv_init(&map->cv, "vm_map");
 	mutex_init(&map->misc_lock, MUTEX_DRIVER, ipl);
 	mutex_init(&map->mutex, MUTEX_DRIVER, ipl);
