70a34ab7fb3f822d5157d36c36a45dcf6273d3ca
ad <ad@NetBSD.org>
2007-07-29 11:30:10 +0000

    GC spllock, splstatclock.

--- sys/sys/spl.h
+++ sys/sys/spl.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: spl.h,v 1.5 2006/12/21 15:55:26 yamt Exp $	*/
+/*	$NetBSD: spl.h,v 1.5.6.1 2007/07/29 11:30:10 ad Exp $	*/
 
 /*-
  * Copyright (c)2005 YAMAMOTO Takashi,
@@ -57,10 +57,8 @@ _SPL_DECL(bio, BIO)
 _SPL_DECL(net, NET)
 _SPL_DECL(tty, TTY)
 _SPL_DECL(vm, VM)
-_SPL_DECL(statclock, STATCLOCK)
 _SPL_DECL(clock, CLOCK)
 _SPL_DECL(sched, SCHED)
-_SPL_DECL(lock, LOCK)
 _SPL_DECL(high, HIGH)
 
 #if defined(IPL_SERIAL)
