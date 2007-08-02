175c7970c8e73b0fc54f45e2dd15a27266e41ecf
heas <heas>
2007-08-02 08:04:22 +0000

    Update Postfix to 2.4.5.
    2.4.2 through 2.4.5 include various build, milter, and other fixes.

---
+++ gnu/dist/postfix/src/global/mail_version.h
@@ -0,0 +1,81 @@
+/*	$NetBSD: mail_version.h,v 1.1.1.30.12.2 2007/08/02 08:05:12 heas Exp $	*/
+
+#ifndef _MAIL_VERSION_H_INCLUDED_
+#define _MAIL_VERSION_H_INCLUDED_
+
+/*++
+/* NAME
+/*	mail_version 3h
+/* SUMMARY
+/*	globally configurable parameters
+/* SYNOPSIS
+/*	#include <mail_version.h>
+/* DESCRIPTION
+/* .nf
+
+ /*
+  * Version of this program. Official versions are called a.b.c, and
+  * snapshots are called a.b-yyyymmdd, where a=major release number, b=minor
+  * release number, c=patchlevel, and yyyymmdd is the release date:
+  * yyyy=year, mm=month, dd=day.
+  *
+  * Patches change both the patchlevel and the release date. Snapshots have no
+  * patchlevel; they change the release date only.
+  */
+#define MAIL_RELEASE_DATE	"20070731"
+#define MAIL_VERSION_NUMBER	"2.4.5"
+
+#ifdef SNAPSHOT
+# define MAIL_VERSION_DATE	"-" MAIL_RELEASE_DATE
+#else
+# define MAIL_VERSION_DATE	""
+#endif
+
+#ifdef NONPROD
+# define MAIL_VERSION_PROD	"-nonprod"
+#else
+# define MAIL_VERSION_PROD	""
+#endif
+
+#define VAR_MAIL_VERSION	"mail_version"
+#define DEF_MAIL_VERSION	MAIL_VERSION_NUMBER MAIL_VERSION_DATE MAIL_VERSION_PROD
+
+extern char *var_mail_version;
+
+ /*
+  * Release date.
+  */
+#define VAR_MAIL_RELEASE	"mail_release_date"
+#define DEF_MAIL_RELEASE	MAIL_RELEASE_DATE
+extern char *var_mail_release;
+
+ /*
+  * The following macros stamp executable files as well as core dumps. This
+  * information helps to answer the following questions:
+  * 
+  * - What Postfix versions(s) are installed on this machine?
+  * 
+  * - Is this installation mixing multiple Postfix versions?
+  * 
+  * - What Postfix version generated this core dump?
+  */
+#include <string.h>
+
+#define MAIL_VERSION_STAMP_DECLARE \
+    char *mail_version_stamp
+
+#define MAIL_VERSION_STAMP_ALLOCATE \
+    mail_version_stamp = strdup(VAR_MAIL_VERSION "=" DEF_MAIL_VERSION)
+
+/* LICENSE
+/* .ad
+/* .fi
+/*	The Secure Mailer license must be distributed with this software.
+/* AUTHOR(S)
+/*	Wietse Venema
+/*	IBM T.J. Watson Research
+/*	P.O. Box 704
+/*	Yorktown Heights, NY 10598, USA
+/*--*/
+
+#endif
