dbffeedbb90de45e6a3763cc9f0e129ca04d876f
macallan <macallan>
2007-08-03 05:40:48 +0000

    unbork fallout from newlock2 merge: - reinitializing video after X exits works again - backlight levels should be right again

--- gnu/dist/gcc4/gcc/testsuite/gcc.c-torture/execute/ieee/fp-cmp-4l.c
+++
@@ -1,2 +0,0 @@
-#define FLOAT long double
-#include "fp-cmp-4.c"
--- gnu/dist/gcc4/gcc/testsuite/gcc.target/powerpc/20040121-1.c
+++
@@ -1,8 +0,0 @@
-/* { dg-do compile { target powerpc*-*-darwin* } } */
-/* { dg-options "-O2 -mcpu=G5 " } */
-
-long long (*y)(int t);
-long long get_alias_set (int t)
-{
-  return y(t);
-}
