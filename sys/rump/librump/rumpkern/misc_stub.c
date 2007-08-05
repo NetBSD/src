daafee8742c0bfd9347f47806a93fee7accc038f
pooka <pooka@NetBSD.org>
2007-08-05 22:28:00 +0000

    Introduce RUMPs - Runnable Userspace Meta-Programs
    /sys/rump contains programs which run unmodified kernel code in an
    emulated userspace environment.  The kernel environment is provided
    by librump.  Currently supported are a number of file systems,
    which by using puffs integrate seamlessly into the system and
    provide a similar user experience to if the code was running as
    part of the kernel.  Potential future rumpification targets include
    for example parts of the networking stack and some device drivers.
    
    This work was supported by Google Summer of Code 2007.

---
+++ sys/rump/librump/rumpkern/misc_stub.c
@@ -0,0 +1,66 @@
+/*	$NetBSD: misc_stub.c,v 1.1 2007/08/05 22:28:08 pooka Exp $	*/
+
+/*
+ * Copyright (c) 2007 Antti Kantee.  All Rights Reserved.
+ *
+ * Development of this software was supported by Google Summer of Code.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS
+ * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <sys/param.h>
+#include <sys/event.h>
+#include <sys/sched.h>
+#include <sys/sysctl.h>
+#include <sys/systm.h>
+
+void
+preempt()
+{
+
+	return;
+}
+
+void
+knote(struct klist *list, long hint)
+{
+
+	return;
+}
+
+/* not tonight, honey */
+int
+sysctl_createv(struct sysctllog **log, int cflags,
+	const struct sysctlnode **rnode, const struct sysctlnode **cnode,
+	int flags, int type, const char *namep, const char *desc,
+	sysctlfn func, u_quad_t qv, void *newp, size_t newlen, ...)
+{
+
+	return 0;
+}
+
+int
+sysctl_notavail(SYSCTLFN_ARGS)
+{
+
+	return EOPNOTSUPP;
+}
