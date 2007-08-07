6eedbdad3e1228b2ac17bb47cedd8579e68d7f10
ad <ad@NetBSD.org>
2007-08-07 11:39:18 +0000

    Export itimespecfix() until itimerfix() dies.

--- sys/kern/kern_time.c
+++ sys/kern/kern_time.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: kern_time.c,v 1.125 2007/07/09 21:10:54 ad Exp $	*/
+/*	$NetBSD: kern_time.c,v 1.126 2007/08/07 11:39:18 ad Exp $	*/
 
 /*-
  * Copyright (c) 2000, 2004, 2005 The NetBSD Foundation, Inc.
@@ -68,7 +68,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: kern_time.c,v 1.125 2007/07/09 21:10:54 ad Exp $");
+__KERNEL_RCSID(0, "$NetBSD: kern_time.c,v 1.126 2007/08/07 11:39:18 ad Exp $");
 
 #include <sys/param.h>
 #include <sys/resourcevar.h>
@@ -95,10 +95,6 @@ POOL_INIT(ptimer_pool, sizeof(struct ptimer), 0, 0, 0, "ptimerpl",
     &pool_allocator_nointr, IPL_NONE);
 POOL_INIT(ptimers_pool, sizeof(struct ptimers), 0, 0, 0, "ptimerspl",
     &pool_allocator_nointr, IPL_NONE);
-
-#ifdef __HAVE_TIMECOUNTER
-static int itimespecfix(struct timespec *);		/* XXX move itimerfix to timespecs */
-#endif /* __HAVE_TIMECOUNTER */
 
 /* Time of day and interval timer support.
  *
