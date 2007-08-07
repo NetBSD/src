0bbc79c02f708a616aeaf194be109d0243355851
ad <ad>
2007-08-07 19:02:43 +0000

    NetBSD 4.99.27: _lwp_park() behaviour changed. libpthread will check kern.osrevision.

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.270 2007/08/05 01:19:17 rmind Exp $	*/
+/*	$NetBSD: param.h,v 1.271 2007/08/07 19:02:43 ad Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -63,7 +63,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	499002600	/* NetBSD 4.99.26 */
+#define	__NetBSD_Version__	499002700	/* NetBSD 4.99.27 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
