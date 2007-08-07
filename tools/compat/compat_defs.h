5f58128fe6a73587dfcc37068ce18dbbd730f227
apb <apb>
2007-08-07 08:22:04 +0000

    Provide definitions for {be,le}{16,32,64}{enc,dec} if necessary. This is adapted from the very detailed fix provided by Giles Lean in PR bin/36678.
    Only one simplified implementation is provided, instead of the
    two implementations conditional on __GNUC_PREREQ__(2,95) that are
    provided in NetBSD's <sys/endian.h>.  The use of memcpy instead of
    __builtin__memcpy, and the absence of __inline or __unused, should make
    it independent of GCC.

--- tools/compat/compat_defs.h
+++ tools/compat/compat_defs.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: compat_defs.h,v 1.59 2007/07/03 12:11:09 nakayama Exp $	*/
+/*	$NetBSD: compat_defs.h,v 1.60 2007/08/07 08:22:04 apb Exp $	*/
 
 #ifndef	__NETBSD_COMPAT_DEFS_H__
 #define	__NETBSD_COMPAT_DEFS_H__
@@ -646,6 +646,61 @@ int	 cgetustr(char *, const char *, char **);
 #if !HAVE_DECL_LE64TOH
 #define le64toh(x)	htole64(x)
 #endif
+
+#define __GEN_ENDIAN_ENC(bits, endian) \
+static void \
+endian ## bits ## enc(void *dst, uint ## bits ## _t u) \
+{ \
+	u = hto ## endian ## bits (u); \
+	memcpy(dst, &u, sizeof(u)); \
+}
+#if !HAVE_DECL_BE16ENC
+__GEN_ENDIAN_ENC(16, be)
+#endif
+#if !HAVE_DECL_BE32ENC
+__GEN_ENDIAN_ENC(32, be)
+#endif
+#if !HAVE_DECL_BE64ENC
+__GEN_ENDIAN_ENC(64, be)
+#endif
+#if !HAVE_DECL_LE16ENC
+__GEN_ENDIAN_ENC(16, le)
+#endif
+#if !HAVE_DECL_LE32ENC
+__GEN_ENDIAN_ENC(32, le)
+#endif
+#if !HAVE_DECL_LE64ENC
+__GEN_ENDIAN_ENC(64, le)
+#endif
+#undef __GEN_ENDIAN_ENC
+
+#define __GEN_ENDIAN_DEC(bits, endian) \
+static uint ## bits ## _t \
+endian ## bits ## dec(const void *buf) \
+{ \
+	uint ## bits ## _t u; \
+	memcpy(&u, buf, sizeof(u)); \
+	return endian ## bits ## toh (u); \
+}
+#if !HAVE_DECL_BE16DEC
+__GEN_ENDIAN_DEC(16, be)
+#endif
+#if !HAVE_DECL_BE32DEC
+__GEN_ENDIAN_DEC(32, be)
+#endif
+#if !HAVE_DECL_BE64DEC
+__GEN_ENDIAN_DEC(64, be)
+#endif
+#if !HAVE_DECL_LE16DEC
+__GEN_ENDIAN_DEC(16, le)
+#endif
+#if !HAVE_DECL_LE32DEC
+__GEN_ENDIAN_DEC(32, le)
+#endif
+#if !HAVE_DECL_LE64DEC
+__GEN_ENDIAN_DEC(64, le)
+#endif
+#undef __GEN_ENDIAN_DEC
 
 /* <sys/mman.h> */
 
