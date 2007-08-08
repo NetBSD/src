56dba892c67e91cd336a22542011ee82d57060af
joerg <joerg@NetBSD.org>
2007-08-08 22:38:48 +0000

    Import pkg_install-20070804: Fix ordering in pkg_order.

--- dist/pkg_install/create/util.c
+++ dist/pkg_install/create/util.c
@@ -33,14 +33,30 @@
 
 #include <nbcompat.h>
 
+#if HAVE_SYS_STAT_H
 #include <sys/stat.h>
+#endif
+#if HAVE_ERR_H
 #include <err.h>
-#include <pwd.h>
+#endif
+#if HAVE_PWD_H
 #include <grp.h>
+#endif
+#if HAVE_PWD_H
+#include <pwd.h>
+#endif
+#if HAVE_STDLIB_H
 #include <stdlib.h>
+#endif
+#if HAVE_STRING_H
 #include <string.h>
+#endif
+#if HAVE_TIME_H
 #include <time.h>
+#endif
+#if HAVE_UNISTD_H
 #include <unistd.h>
+#endif
 
 #include "lib.h"
 #include "create.h"
