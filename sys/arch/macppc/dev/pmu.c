10b2f055c86b28c3118af1c72fbb028b9c7a480b
macallan <macallan@NetBSD.org>
2007-08-08 22:19:55 +0000

    sync with ppcoea-renovation

--- sys/arch/macppc/dev/pmu.c
+++ sys/arch/macppc/dev/pmu.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: pmu.c,v 1.7 2007/07/09 20:52:22 ad Exp $ */
+/*	$NetBSD: pmu.c,v 1.8 2007/08/08 22:19:55 macallan Exp $ */
 
 /*-
  * Copyright (c) 2006 Michael Lorenz
@@ -30,7 +30,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: pmu.c,v 1.7 2007/07/09 20:52:22 ad Exp $");
+__KERNEL_RCSID(0, "$NetBSD: pmu.c,v 1.8 2007/08/08 22:19:55 macallan Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -351,6 +351,13 @@ next:
 		printf("%s: enabling ohare backlight control\n",
 		    device_xname(dev));
 		sc->sc_flags |= PMU_HAS_BACKLIGHT_CONTROL;
+		cmd[0] = 0;
+		cmd[1] = 0;
+		memset(resp, 0, 6);
+		if (pmu_send(sc, PMU_READ_BRIGHTNESS, 1, cmd, 16, resp) > 1) {
+			sc->sc_brightness_wanted = resp[1];
+			pmu_update_brightness(sc);
+		}
 	}
 
 	/* attach batteries */
@@ -737,7 +744,7 @@ pmu_autopoll(void *cookie, int flag)
 {
 	struct pmu_softc *sc = cookie;
 	/* magical incantation to re-enable autopolling */
-	uint8_t cmd[] = {0, 0x86, (flag >> 8) & 0xff, flag & 0xff};
+	uint8_t cmd[] = {0, PMU_SET_POLL_MASK, (flag >> 8) & 0xff, flag & 0xff};
 	uint8_t resp[16];
 
 	if (sc->sc_autopoll == flag)
@@ -960,9 +967,7 @@ pmu_thread(void *cookie)
 					pmu_eject_card(sc, i);
 			}
 		}
-#if NLWPM > 0
-		lwpm_poll();
-#endif
+
 		/* see if we need to update brightness */
 		if (sc->sc_brightness_wanted != sc->sc_brightness) {
 			pmu_update_brightness(sc);
@@ -970,7 +975,9 @@ pmu_thread(void *cookie)
 
 		/* see if we need to update audio volume */
 		if (sc->sc_volume_wanted != sc->sc_volume) {
-			//set_volume(sc->sc_volume_wanted);
+#if 0
+			set_volume(sc->sc_volume_wanted);
+#endif
 			sc->sc_volume = sc->sc_volume_wanted;
 		}
 
