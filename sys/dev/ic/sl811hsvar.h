44c89c76460108083a983da61bb7794a32a28ac0
kiyohara <kiyohara@NetBSD.org>
2007-08-15 03:53:08 +0000

    Replace to Matthew Orgass's slhci(4). http://mail-index.netbsd.org/tech-kern/2007/06/26/0001.html

--- sys/dev/ic/sl811hsvar.h
+++ sys/dev/ic/sl811hsvar.h
@@ -1,102 +1,96 @@
-/*	$NetBSD: sl811hsvar.h,v 1.1 2002/08/11 13:17:54 isaki Exp $	*/
+/*	$NetBSD: sl811hsvar.h,v 1.2 2007/08/15 03:53:10 kiyohara Exp $	*/
 
 /*
- * Copyright (c) 2001 The NetBSD Foundation, Inc.
- * All rights reserved.
- *
- * This code is derived from software contributed to The NetBSD Foundation
- * by Tetsuya Isaki.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *      This product includes software developed by the NetBSD
- *      Foundation, Inc. and its contributors.
- * 4. Neither the name of The NetBSD Foundation nor the names of its
- *    contributors may be used to endorse or promote products derived
- *    from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
- * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
- * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS
- * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
+ * Not (c) 2007 Matthew Orgass
+ * This file is public domain, meaning anyone can make any use of part or all 
+ * of this file including copying into other works without credit.  Any use, 
+ * modified or not, is solely the responsibility of the user.  If this file is 
+ * part of a collection then use in the collection is governed by the terms of 
+ * the collection.
  */
 
 /*
- * ScanLogic SL811HS/T USB Host Controller
+ * Cypress/ScanLogic SL811HS USB Host Controller
  */
 
-#define MS_TO_TICKS(ms) ((ms) * hz / 1000)
-#define delay_ms(X) \
-	ltsleep(&slhci_dummy, PZERO | PCATCH, "slhci", MS_TO_TICKS(X), NULL)
+#include <sys/gcq.h>
+#include <sys/lock.h>
+#include "opt_slhci.h"
 
-#define SL11_PID_OUT	(0x1)
-#define SL11_PID_IN 	(0x9)
-#define SL11_PID_SOF	(0x5)
-#define SL11_PID_SETUP	(0xd)
+#define SC_DEV(sc)	(&((sc)->sc_bus.bdev))
+#define SC_NAME(sc)	(SC_DEV(sc)->dv_xname)
 
-struct slhci_xfer {
-	usbd_xfer_handle sx_xfer;
-	usb_callout_t sx_callout_t;
+typedef unsigned int Frame;
+struct slhci_pipe;
+
+/* Generally transfer related items. */
+struct slhci_transfers {
+	struct usbd_xfer *rootintr;
+	struct slhci_pipe *spipe[2]; 	/* current transfer (unless canceled) */
+	struct gcq_head q[3];		/* transfer queues, Q_* index */
+	struct gcq_head timed;		/* intr transfer multi-frame wait */
+	struct gcq_head to;		/* timeout list */
+	struct gcq_head ap;		/* all pipes */
+	Frame frame;			/* current frame */
+	unsigned int flags;		/* F_* flags */
+	int pend;			/* pending for waitintr */
+	int reserved_bustime;
+	int16_t len[2];		     	/* length of transfer or -1 if none */
+	uint8_t current_tregs[2][4]; 	/* ab, ADR, LEN, PID, DEV */
+	uint8_t copyin[2]; 		/* copyin ADR, LEN */
+	uint8_t rootaddr;		/* device address of root hub */
+	uint8_t rootconf;		/* root configuration */
+	uint8_t max_current;		/* max current / 2 */
+	uint8_t sltype;			/* revision */
 };
 
+enum power_change {
+	POWER_OFF,
+	POWER_ON,
+};
+
+typedef void (*PowerFunc)(void *, enum power_change);
+
+/* Attachment code must call slhci_preinit before registering the ISR */
 struct slhci_softc {
-	struct usbd_bus		 sc_bus;
-	bus_space_tag_t		 sc_iot;
-	bus_space_handle_t	 sc_ioh;
-	bus_dma_tag_t		 sc_dmat;
+	struct usbd_bus		sc_bus;
 
-	void				(*sc_enable_power)(void *, int);
-	void				(*sc_enable_intr)(void *, int);
-	void				*sc_arg;
-	int					 sc_powerstat;
-#define POWER_ON	(1)
-#define POWER_OFF	(0)
-#define INTR_ON 	(1)
-#define INTR_OFF	(0)
+	struct simplelock	sc_lock;
+	struct simplelock	sc_wait_lock;
 
-	device_ptr_t		 sc_child;
+	struct slhci_transfers	sc_transfers;	/* Info useful in transfers. */
 
-	struct device		*sc_parent;	/* parent device */
-	int			 sc_sltype;	/* revision */
-#define SLTYPE_SL11H	(0x00)
-#define SLTYPE_SL811HS	(0x01)
-#define SLTYPE_SL811HS_R12	SLTYPE_SL811HS
-#define SLTYPE_SL811HS_R14	(0x02)
+	struct gcq_head		sc_waitq;
 
-	u_int8_t		 sc_addr;	/* device address of root hub */
-	u_int8_t		 sc_conf;
-	SIMPLEQ_HEAD(, usbd_xfer) sc_free_xfers;
+	bus_space_tag_t		sc_iot;
+	bus_space_handle_t	sc_ioh;
 
-	/* Information for the root hub interrupt pipe */
-	int			 sc_interval;
-	usbd_xfer_handle	 sc_intr_xfer;
-	usb_callout_t		 sc_poll_handle;
+	struct callout		sc_timer; 	/* for reset */
 
-	int				 sc_flags;
-#define SLF_RESET	(0x01)
-#define SLF_INSERT	(0x02)
+	PowerFunc		sc_enable_power;
 
-	/* Root HUB specific members */
-	int				sc_fullspeed;
-	int				sc_connect;	/* XXX */
-	int				sc_change;
+	struct device		*sc_child;
+
+	struct timeval		sc_reserved_warn_rate;
+	struct timeval		sc_overflow_warn_rate;
+
+	void			*sc_cb_softintr;
+
+	unsigned int		sc_ier_check;
+
+	int			sc_mem_use; /* XXX SLHCI_MEM_ACCOUNTING */
+
+	uint8_t			sc_ier; 	/* enabled interrupts */
+	uint8_t			sc_stride;	/* port stride */
 };
 
-int  sl811hs_find(struct slhci_softc *);
-int  slhci_attach(struct slhci_softc *, struct device *);
+/* last preinit arguments are: max current (in mA, not mA/2), port stride */
+/* register access uses byte access, but stride offsets the data port */
+int  slhci_supported_rev(uint8_t);
+void slhci_preinit(struct slhci_softc *, PowerFunc, bus_space_tag_t, 
+    bus_space_handle_t, uint16_t, uint8_t);
+int  slhci_attach(struct slhci_softc *);
+int  slhci_detach(struct slhci_softc *, int);
+int  slhci_activate(struct device *, enum devact);
 int  slhci_intr(void *);
+
