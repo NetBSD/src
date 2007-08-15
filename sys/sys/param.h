6395faae55ae2c1358fb75ec34895ed0707936af
ad <ad@NetBSD.org>
2007-08-15 12:10:33 +0000

    NetBSD 4.99.29 - change ktrace to be LKM friendly.

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.272 2007/08/09 07:34:27 hannken Exp $	*/
+/*	$NetBSD: param.h,v 1.273 2007/08/15 12:10:33 ad Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -63,7 +63,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	499002800	/* NetBSD 4.99.28 */
+#define	__NetBSD_Version__	499002900	/* NetBSD 4.99.29 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
