17585e2f7e552c2dc00549cb3935326c4410913d
pooka <pooka@NetBSD.org>
2007-08-22 17:52:16 +0000

    struct nameidata/componentname size changes
    Welcome to 4.99.30, we wish you a pleasant stay.

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.273 2007/08/15 12:10:33 ad Exp $	*/
+/*	$NetBSD: param.h,v 1.274 2007/08/22 17:52:16 pooka Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -63,7 +63,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	499002900	/* NetBSD 4.99.29 */
+#define	__NetBSD_Version__	499003000	/* NetBSD 4.99.30 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
