f02d9c93b609e2494d3aef6caecc2b15e85eaccc
liamjfoy <liamjfoy@NetBSD.org>
2007-08-25 17:52:20 +0000

    Pull up following (requested by mjf in ticket #828):
    Sync OpenSSL with HEAD

--- crypto/dist/openssl/crypto/opensslconf.h
+++ crypto/dist/openssl/crypto/opensslconf.h
@@ -20,6 +20,9 @@
 #ifndef OPENSSL_NO_RC5
 # define OPENSSL_NO_RC5
 #endif
+#ifndef OPENSSL_NO_RFC3779
+# define OPENSSL_NO_RFC3779
+#endif
 #else
 #ifndef OPENSSL_THREADS
 #define OPENSSL_THREADS
@@ -50,6 +53,9 @@
 # endif
 # if defined(OPENSSL_NO_RC5) && !defined(NO_RC5)
 #  define NO_RC5
+# endif
+# if defined(OPENSSL_NO_RFC3779) && !defined(NO_RFC3779)
+#  define NO_RFC3779
 # endif
 #endif
 
