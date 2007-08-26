37d8b075fab37127ca725d355e01e7432f3b6755
bouyer <bouyer@NetBSD.org>
2007-08-26 19:55:17 +0000

    Pull up following revision(s) (requested by ghen in ticket #1818): 	sbin/ping/ping.c: revisions 1.83 - 1.85 PR/28741: Michael Santos: ping does [not] drop root privileges

--- sbin/ping/ping.c
+++ sbin/ping/ping.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: ping.c,v 1.77 2004/05/13 20:27:38 kleink Exp $	*/
+/*	$NetBSD: ping.c,v 1.77.6.1 2007/08/26 19:55:17 bouyer Exp $	*/
 
 /*
  * Copyright (c) 1989, 1993
@@ -58,7 +58,7 @@
 
 #include <sys/cdefs.h>
 #ifndef lint
-__RCSID("$NetBSD: ping.c,v 1.77 2004/05/13 20:27:38 kleink Exp $");
+__RCSID("$NetBSD: ping.c,v 1.77.6.1 2007/08/26 19:55:17 bouyer Exp $");
 #endif
 
 #include <stdio.h>
@@ -262,6 +262,14 @@ main(int argc, char *argv[])
 #endif
   
 
+	if ((s = cap_socket(AF_INET, SOCK_RAW, IPPROTO_ICMP)) < 0)
+		err(1, "Cannot create socket");
+	if ((sloop = cap_socket(AF_INET, SOCK_RAW, IPPROTO_ICMP)) < 0)
+		err(1, "Cannot create socket");
+
+	if (setuid(getuid()) == -1)
+		err(1, "setuid");
+
 	setprogname(argv[0]);
 
 #ifndef IPSEC
@@ -461,8 +469,6 @@ main(int argc, char *argv[])
 
 	ident = arc4random() & 0xFFFF;
 
-	if ((s = cap_socket(AF_INET, SOCK_RAW, IPPROTO_ICMP)) < 0)
-		err(1, "Cannot create socket");
 	if (options & SO_DEBUG) {
 		if (setsockopt(s, SOL_SOCKET, SO_DEBUG,
 			       (char *)&on, sizeof(on)) == -1)
@@ -474,8 +480,6 @@ main(int argc, char *argv[])
 			warn("SO_DONTROUTE");
 	}
 
-	if ((sloop = cap_socket(AF_INET, SOCK_RAW, IPPROTO_ICMP)) < 0)
-		err(1, "Cannot create socket");
 	if (options & SO_DEBUG) {
 		if (setsockopt(sloop, SOL_SOCKET, SO_DEBUG,
 			       (char *)&on, sizeof(on)) == -1)
