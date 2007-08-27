a73fea0350d5bf3c502925bcb6cda20392ca53de
jmcneill <jmcneill@NetBSD.org>
2007-08-27 03:17:17 +0000

    Add 965 PM AGP support.

--- sys/arch/i386/pci/pchb.c
+++ sys/arch/i386/pci/pchb.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: pchb.c,v 1.64.22.2 2007/08/16 11:02:21 jmcneill Exp $	*/
+/*	$NetBSD: pchb.c,v 1.64.22.3 2007/08/27 03:17:17 jmcneill Exp $	*/
 
 /*-
  * Copyright (c) 1996, 1998, 2000 The NetBSD Foundation, Inc.
@@ -37,7 +37,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: pchb.c,v 1.64.22.2 2007/08/16 11:02:21 jmcneill Exp $");
+__KERNEL_RCSID(0, "$NetBSD: pchb.c,v 1.64.22.3 2007/08/27 03:17:17 jmcneill Exp $");
 
 #include <sys/types.h>
 #include <sys/param.h>
@@ -319,6 +319,7 @@ pchbattach(struct device *parent, struct device *self, void *aux)
 		case PCI_PRODUCT_INTEL_82945P_MCH:
 		case PCI_PRODUCT_INTEL_82945GM_HB:
 		case PCI_PRODUCT_INTEL_82965Q_HB:
+		case PCI_PRODUCT_INTEL_82965PM_HB:
 			/*
 			 * The host bridge is either in GFX mode (internal
 			 * graphics) or in AGP mode. In GFX mode, we pretend
