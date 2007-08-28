504f84468790be631f4224d9ae8e0d623aa95178
liamjfoy <liamjfoy@NetBSD.org>
2007-08-28 19:33:25 +0000

    Pull up following revision(s) (requested by tsutsui in ticket #844): 	sys/arch/shark/isa/isa_irq.S: revision 1.8 	sys/arch/shark/isa/clock.c: revision 1.15 Make clockintr() return 1 since it's always handled so that clock interrupt is properly acounted by evcnt(9) in isa_irq.S.
    In irq_setmasks(), refer spl_masks[current_spl_level] directly
    rather than spl_mask which is a saved value in splraise() and
    splx() functions because the latter one is not always sync'ed with
    current_spl_level and interrupt state could be mangled.
    Okay'ed by chris@, and should close PR port-shark/22355.

--- sys/arch/shark/isa/clock.c
+++ sys/arch/shark/isa/clock.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: clock.c,v 1.13 2006/09/13 07:13:03 gdamore Exp $	*/
+/*	$NetBSD: clock.c,v 1.13.4.1 2007/08/28 19:33:25 liamjfoy Exp $	*/
 
 /*
  * Copyright 1997
@@ -154,7 +154,7 @@ WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: clock.c,v 1.13 2006/09/13 07:13:03 gdamore Exp $");
+__KERNEL_RCSID(0, "$NetBSD: clock.c,v 1.13.4.1 2007/08/28 19:33:25 liamjfoy Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -376,7 +376,7 @@ clockintr(void *arg)
 	}
 #endif
 	hardclock(frame);
-	return(0);
+	return(1);
 }
 
 static int
