782de888890b5bbb5bb3db3b6893d4bddde611e1
ghen <ghen@NetBSD.org>
2007-08-28 13:31:46 +0000

    Pull up patches (requested by joerg in ticket #1812): 	lib/libc/hash/sha2/sha2.c: patch 	sys/crypto/ripemd160/rmd160.c: patch 	sys/crypto/sha2/sha2.c: patch Fix SIGBUS issues on strict alignment issues. Use le32dec in RMD160 as the data pointer to RMD160_Update doesn't have to be aligned. In SHA256_Update and SHA512_Update, only operate directly on the passed in data if no left-over in the context exists and the data is correctly aligned. The problem was exposed by the audit-packages rewrite in C and reported for the libnbcompat version in PR pkg/36662. cast to void* to avoid a fatal warning

--- lib/libc/hash/sha2/sha2.c
+++ lib/libc/hash/sha2/sha2.c
@@ -1,4 +1,4 @@
-/* $NetBSD: sha2.c,v 1.3.2.2 2005/09/08 19:15:44 tron Exp $ */
+/* $NetBSD: sha2.c,v 1.3.2.3 2007/08/28 13:31:46 ghen Exp $ */
 /*	$KAME: sha2.c,v 1.9 2003/07/20 00:28:38 itojun Exp $	*/
 
 /*
@@ -526,12 +526,30 @@ void SHA256_Update(SHA256_CTX* context, const sha2_byte *data, size_t len) {
 			return;
 		}
 	}
-	while (len >= SHA256_BLOCK_LENGTH) {
-		/* Process as many complete blocks as we can */
-		SHA256_Transform(context, (const sha2_word32*)(const void *)data);
-		context->bitcount += SHA256_BLOCK_LENGTH << 3;
-		len -= SHA256_BLOCK_LENGTH;
-		data += SHA256_BLOCK_LENGTH;
+	/*
+	 * Process as many complete blocks as possible.
+	 *
+	 * Check alignment of the data pointer. If it is 32bit aligned,
+	 * SHA256_Transform can be called directly on the data stream,
+	 * otherwise enforce the alignment by copy into the buffer.
+	 */
+	if ((uintptr_t)data % 4 == 0) {
+		while (len >= SHA256_BLOCK_LENGTH) {
+			SHA256_Transform(context,
+			    (const sha2_word32 *)(const void *)data);
+			context->bitcount += SHA256_BLOCK_LENGTH << 3;
+			len -= SHA256_BLOCK_LENGTH;
+			data += SHA256_BLOCK_LENGTH;
+		}
+	} else {
+		while (len >= SHA256_BLOCK_LENGTH) {
+			memcpy(context->buffer, data, SHA256_BLOCK_LENGTH);
+			SHA256_Transform(context,
+			    (const sha2_word32 *)(const void *)context->buffer);
+			context->bitcount += SHA256_BLOCK_LENGTH << 3;
+			len -= SHA256_BLOCK_LENGTH;
+			data += SHA256_BLOCK_LENGTH;
+		}
 	}
 	if (len > 0) {
 		/* There's left-overs, so save 'em */
@@ -816,12 +834,30 @@ void SHA512_Update(SHA512_CTX* context, const sha2_byte *data, size_t len) {
 			return;
 		}
 	}
-	while (len >= SHA512_BLOCK_LENGTH) {
-		/* Process as many complete blocks as we can */
-		SHA512_Transform(context, (const sha2_word64*)(const void *)data);
-		ADDINC128(context->bitcount, SHA512_BLOCK_LENGTH << 3);
-		len -= SHA512_BLOCK_LENGTH;
-		data += SHA512_BLOCK_LENGTH;
+	/*
+	 * Process as many complete blocks as possible.
+	 *
+	 * Check alignment of the data pointer. If it is 64bit aligned,
+	 * SHA512_Transform can be called directly on the data stream,
+	 * otherwise enforce the alignment by copy into the buffer.
+	 */
+	if ((uintptr_t)data % 8 == 0) {
+		while (len >= SHA512_BLOCK_LENGTH) {
+			SHA512_Transform(context,
+			    (const sha2_word64*)(const void *)data);
+			ADDINC128(context->bitcount, SHA512_BLOCK_LENGTH << 3);
+			len -= SHA512_BLOCK_LENGTH;
+			data += SHA512_BLOCK_LENGTH;
+		}
+	} else {
+		while (len >= SHA512_BLOCK_LENGTH) {
+			memcpy(context->buffer, data, SHA512_BLOCK_LENGTH);
+			SHA512_Transform(context,
+			    (const void *)context->buffer);
+			ADDINC128(context->bitcount, SHA512_BLOCK_LENGTH << 3);
+			len -= SHA512_BLOCK_LENGTH;
+			data += SHA512_BLOCK_LENGTH;
+		}
 	}
 	if (len > 0) {
 		/* There's left-overs, so save 'em */
