92203533790b3281f55dda5d8939126f570318ae
jmmv <jmmv@NetBSD.org>
2007-08-29 13:02:41 +0000

    Move the definition of multiple video-related variables into a new header file (machine/video.h) so that we can kill all the "manual" externs spread around the code (which were inconsistent among them).

--- sys/arch/mac68k/dev/maccons.c
+++ sys/arch/mac68k/dev/maccons.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: maccons.c,v 1.6 2005/12/11 12:18:02 christos Exp $	*/
+/*	$NetBSD: maccons.c,v 1.7 2007/08/29 13:02:41 jmmv Exp $	*/
 
 /*
  * Copyright (C) 1999 Scott Reynolds.  All rights reserved.
@@ -27,7 +27,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: maccons.c,v 1.6 2005/12/11 12:18:02 christos Exp $");
+__KERNEL_RCSID(0, "$NetBSD: maccons.c,v 1.7 2007/08/29 13:02:41 jmmv Exp $");
 
 #include "wsdisplay.h"
 #include "wskbd.h"
@@ -40,6 +40,7 @@ __KERNEL_RCSID(0, "$NetBSD: maccons.c,v 1.6 2005/12/11 12:18:02 christos Exp $")
 #include <machine/autoconf.h>
 #include <machine/cpu.h>
 
+#include <machine/video.h>
 #include <dev/cons.h>
 #include <dev/wscons/wskbdvar.h>
 #include <dev/wscons/wsdisplayvar.h>
@@ -53,9 +54,6 @@ void maccnputc(dev_t, int);
 void maccnpollc(dev_t, int);
 
 static int	maccons_initted = (-1);
-
-/* From Booter via locore */
-extern u_int32_t	mac68k_vidphys;
 
 void
 maccnprobe(struct consdev *cp)
