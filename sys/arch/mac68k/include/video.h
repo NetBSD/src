a7b1a7d1360fb8f3cbf9b768aff21923d0708345
jmmv <jmmv>
2007-08-29 16:09:31 +0000

    Pack all global video-related variables into a structure for clarity.
    A comment said that they weren't in a struct for speed reasons but...
    this should not affect performance because these variables are mostly
    used to set other variables (hence they are read few times).

--- sys/arch/mac68k/include/video.h
+++ sys/arch/mac68k/include/video.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: video.h,v 1.1 2007/08/29 13:02:41 jmmv Exp $	*/
+/*	$NetBSD: video.h,v 1.2 2007/08/29 16:09:32 jmmv Exp $	*/
 /*
  * Copyright (c) 2007 The NetBSD Foundation, Inc.
  * All rights reserved.
@@ -36,19 +36,31 @@
 #ifndef _CPU_VIDEO_H_
 #define _CPU_VIDEO_H_
 
-/* From Booter via locore */
-extern long	videoaddr;	/* Address used in kernel for video */
-extern long	videorowbytes;	/* Length of row in video RAM */
-extern long	videobitdepth;	/* Number of bits per pixel */
-extern long	videowidth;	/* Framebuffer width */
-extern long	videoheight;	/* Framebuffer height */
+#include <sys/types.h>
 
-/*
- * Values for IIvx-like internal video
- * -- should be zero if it is not used (usual case).
- */
-extern uint32_t mac68k_vidlog;	/* logical addr */
-extern uint32_t mac68k_vidphys;	/* physical addr */
-extern uint32_t mac68k_vidlen;	/* mem length */
+struct mac68k_video {
+	/* From Booter via locore */
+	vaddr_t mv_kvaddr;	/* Address used in kernel for video */
+	size_t mv_stride;	/* Length of row in video RAM */
+	size_t mv_depth;	/* Number of bits per pixel */
+	size_t mv_width;	/* Framebuffer width */
+	size_t mv_height;	/* Framebuffer height */
+
+	/*
+	 * Values for IIvx-like internal video
+	 * -- should be zero if it is not used (usual case).
+	 *
+	 * XXX This doesn't seem to hold true nowadays.  It seems that
+	 * these fields are accessed from many places and they are
+	 * non-zero of machines with non-internal video.  They should also
+	 * have better names to clarify, e.g, why we need mv_log and
+	 * mv_kvaddr (do we need to have these two?) -- jmmv 20070829
+	 */
+	vaddr_t mv_log;		/* logical addr */
+	paddr_t mv_phys;	/* physical addr */
+	size_t mv_len;		/* mem length */
+};
+
+extern struct mac68k_video mac68k_video;
 
 #endif /* _CPU_VIDEO_H_ */
