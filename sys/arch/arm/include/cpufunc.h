b6c3877f135284d67be9706449f7af98fd06951e
matt <matt@NetBSD.org>
2007-08-29 04:42:10 +0000

    Make arm11 use the armv6 cpufuncs.

--- sys/arch/arm/include/cpufunc.h
+++ sys/arch/arm/include/cpufunc.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: cpufunc.h,v 1.40 2007/03/04 14:47:18 bjh21 Exp $	*/
+/*	$NetBSD: cpufunc.h,v 1.40.22.1 2007/08/29 04:42:10 matt Exp $	*/
 
 /*
  * Copyright (c) 1997 Mark Brinicombe.
@@ -352,7 +352,7 @@ void	arm10_context_switch	(void);
 void	arm10_setup		(char *);
 #endif
 
-#ifdef CPU_ARM11
+#if defined(CPU_ARM11)
 void	arm11_setttb		(u_int);
 
 void	arm11_tlb_flushID_SE	(u_int);
@@ -360,11 +360,15 @@ void	arm11_tlb_flushI_SE	(u_int);
 
 void	arm11_context_switch	(void);
 
+void	arm11_cpu_sleep		(int);
 void	arm11_setup		(char *string);
 void	arm11_tlb_flushID	(void);
 void	arm11_tlb_flushI	(void);
 void	arm11_tlb_flushD	(void);
 void	arm11_tlb_flushD_SE	(u_int va);
+
+void	armv11_dcache_wbinv_all (void);
+void	armv11_idcache_wbinv_all(void);
 
 void	arm11_drain_writebuf	(void);
 #endif
@@ -384,7 +388,7 @@ void	armv5_ec_idcache_wbinv_all	(void);
 void	armv5_ec_idcache_wbinv_range	(vaddr_t, vsize_t);
 #endif
 
-#if defined (CPU_ARM10) || defined (CPU_ARM11)
+#if defined (CPU_ARM10)
 void	armv5_setttb		(u_int);
 
 void	armv5_icache_sync_all	(void);
@@ -402,6 +406,21 @@ extern unsigned armv5_dcache_sets_max;
 extern unsigned armv5_dcache_sets_inc;
 extern unsigned armv5_dcache_index_max;
 extern unsigned armv5_dcache_index_inc;
+#endif
+
+#if defined(CPU_ARM11)
+void	armv6_setttb		(u_int);
+
+void	armv6_icache_sync_all	(void);
+void	armv6_icache_sync_range	(vaddr_t, vsize_t);
+
+void	armv6_dcache_wbinv_all	(void);
+void	armv6_dcache_wbinv_range (vaddr_t, vsize_t);
+void	armv6_dcache_inv_range	(vaddr_t, vsize_t);
+void	armv6_dcache_wb_range	(vaddr_t, vsize_t);
+
+void	armv6_idcache_wbinv_all	(void);
+void	armv6_idcache_wbinv_range (vaddr_t, vsize_t);
 #endif
 
 #if defined(CPU_ARM9) || defined(CPU_ARM9E) || defined(CPU_ARM10) || \
@@ -499,6 +518,40 @@ __set_cpsr_c(u_int bic, u_int eor)
 
 #define restore_interrupts(old_cpsr)					\
 	(__set_cpsr_c((I32_bit | F32_bit), (old_cpsr) & (I32_bit | F32_bit)))
+
+static inline void cpsie(register_t psw) __attribute__((__unused__));
+static inline register_t cpsid(register_t psw) __attribute__((__unused__));
+
+static inline void
+cpsie(register_t psw)
+{
+	if (!__builtin_constant_p(psw)) {
+		enable_interrupts(psw);
+		return;
+	}
+	switch (psw & (I32_bit|F32_bit)) {
+	case I32_bit:		__asm("cpsie\ti"); break;
+	case F32_bit:		__asm("cpsie\tf"); break;
+	case I32_bit|F32_bit:	__asm("cpsie\tif"); break;
+	}
+}
+
+static inline register_t
+cpsid(register_t psw)
+{
+	register_t oldpsw;
+	if (!__builtin_constant_p(psw))
+		return disable_interrupts(psw);
+
+	__asm("mrs	%0, cpsr" : "=r"(oldpsw));
+	switch (psw & (I32_bit|F32_bit)) {
+	case I32_bit:		__asm("cpsid\ti"); break;
+	case F32_bit:		__asm("cpsid\tf"); break;
+	case I32_bit|F32_bit:	__asm("cpsid\tif"); break;
+	}
+	return oldpsw;
+}
+
 #else /* ! __PROG32 */
 #define	disable_interrupts(mask)					\
 	(set_r15((mask) & (R15_IRQ_DISABLE | R15_FIQ_DISABLE),		\
@@ -554,6 +607,7 @@ extern int	arm_picache_ways;
 extern int	arm_pdcache_size;	/* and unified */
 extern int	arm_pdcache_line_size;
 extern int	arm_pdcache_ways;
+extern int	arm_cache_prefer_mask;
 
 extern int	arm_pcache_type;
 extern int	arm_pcache_unified;
