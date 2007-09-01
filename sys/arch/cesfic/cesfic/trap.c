c34c267aa06ee0b8593953757a78efc46df31538
mhitch <mhitch@NetBSD.org>
2007-09-01 21:31:55 +0000

    68030 and 68040 processors consider the read portion of a read-modify-write transfer as a write to ensure the memory is writable before starting any transfer.  The fault status information does not reflect this in the 'read' status bit (i.e. it shows up as a read access), so faults with a RMW access to non-writable memory was not getting the correct protection.  The page would be read-only and the instruction would fault over and over.
    A specific example is when a process forks, and the child process attempts
    to execute a RMW access to a data page, which is read-only because it's CoP
    Copy-On-Write.
    
    When checking if the page needs to be writablek, also check the locked transfer
    and treat any locked transfer as a write.
    
    68060 already handled this correctly, since it has separate read and write
    fault bits, and both are set on a RMW access and the trap code was checking
    the write status bit.
    
    Fixes PR#36848.

--- sys/arch/cesfic/cesfic/trap.c
+++ sys/arch/cesfic/cesfic/trap.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: trap.c,v 1.35 2007/06/12 03:34:30 mhitch Exp $	*/
+/*	$NetBSD: trap.c,v 1.36 2007/09/01 21:31:55 mhitch Exp $	*/
 
 /*
  * Copyright (c) 1982, 1986, 1990, 1993
@@ -77,7 +77,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: trap.c,v 1.35 2007/06/12 03:34:30 mhitch Exp $");
+__KERNEL_RCSID(0, "$NetBSD: trap.c,v 1.36 2007/09/01 21:31:55 mhitch Exp $");
 
 #include "opt_ddb.h"
 #include "opt_execfmt.h"
@@ -189,7 +189,7 @@ short	exframesize[] = {
 #define	KDFAULT_040(c)	(cputype == CPU_68040 && \
 			 ((c) & SSW4_TMMASK) == SSW4_TMKD)
 #define	WRFAULT_040(c)	(cputype == CPU_68040 && \
-			 ((c) & SSW4_RW) == 0)
+			 ((c) & (SSW4_LK|SSW4_RW)) != SSW4_RW)
 #else
 #define	KDFAULT_040(c)	0
 #define	WRFAULT_040(c)	0
@@ -199,7 +199,8 @@ short	exframesize[] = {
 #define	KDFAULT_OTH(c)	(cputype <= CPU_68030 && \
 			 ((c) & (SSW_DF|SSW_FCMASK)) == (SSW_DF|FC_SUPERD))
 #define	WRFAULT_OTH(c)	(cputype <= CPU_68030 && \
-			 ((c) & (SSW_DF|SSW_RW)) == SSW_DF)
+			 (((c) & SSW_DF) != 0 && \
+			 ((((c) & SSW_RW) == 0) || (((c) & SSW_RM) != 0))))
 #else
 #define	KDFAULT_OTH(c)	0
 #define	WRFAULT_OTH(c)	0
