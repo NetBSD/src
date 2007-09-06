e310bc2318141af218a44168e3aaff2b147e4daa
jmcneill <jmcneill@NetBSD.org>
2007-09-06 22:12:53 +0000

    Add Intel G33 AGP support, resolves PR36902, PR36903, PR36904, PR36906, PR36907, PR36908, PR36909, PR36910, PR36911, PR36912, PR36913, and PR36925.

--- sys/arch/amd64/pci/pchb.c
+++ sys/arch/amd64/pci/pchb.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: pchb.c,v 1.7.22.1 2007/08/27 03:16:20 jmcneill Exp $	*/
+/*	$NetBSD: pchb.c,v 1.7.22.2 2007/09/06 22:12:53 jmcneill Exp $	*/
 
 /*-
  * Copyright (c) 1996, 1998, 2000 The NetBSD Foundation, Inc.
@@ -37,7 +37,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: pchb.c,v 1.7.22.1 2007/08/27 03:16:20 jmcneill Exp $");
+__KERNEL_RCSID(0, "$NetBSD: pchb.c,v 1.7.22.2 2007/09/06 22:12:53 jmcneill Exp $");
 
 #include <sys/types.h>
 #include <sys/param.h>
@@ -141,6 +141,7 @@ pchbattach(struct device *parent, struct device *self, void *aux)
 		case PCI_PRODUCT_INTEL_82945GM_HB:
 		case PCI_PRODUCT_INTEL_82965Q_HB:
 		case PCI_PRODUCT_INTEL_82965PM_HB:
+		case PCI_PRODUCT_INTEL_82G33_HB:
 			/*
 			 * The host bridge is either in GFX mode (internal
 			 * graphics) or in AGP mode. In GFX mode, we pretend
