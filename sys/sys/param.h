6e034d3992b46875eab34bb9669552bc358081cb
rmind <rmind>
2007-09-07 19:21:41 +0000

    Added support for POSIX message queues.
    Welcome to NetBSD 4.99.31!

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.274 2007/08/22 17:52:16 pooka Exp $	*/
+/*	$NetBSD: param.h,v 1.275 2007/09/07 19:21:42 rmind Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -63,7 +63,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	499003000	/* NetBSD 4.99.30 */
+#define	__NetBSD_Version__	499003100	/* NetBSD 4.99.31 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
