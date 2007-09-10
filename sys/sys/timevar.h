6142832dcd3ef9af3b975a2ff93b38c247dc10b7
cube <cube@NetBSD.org>
2007-09-10 10:35:51 +0000

    Remove 3rd clause and my name from all the licences which were only in my name.

--- sys/sys/timevar.h
+++ sys/sys/timevar.h
@@ -1,11 +1,8 @@
-/*	$NetBSD: timevar.h,v 1.13 2007/08/07 11:39:18 ad Exp $	*/
+/*	$NetBSD: timevar.h,v 1.14 2007/09/10 10:35:55 cube Exp $	*/
 
 /*
  *  Copyright (c) 2005 The NetBSD Foundation.
  *  All rights reserved.
- *
- *  This code is derived from software contributed to the NetBSD Foundation
- *   by Quentin Garnier.
  *
  *  Redistribution and use in source and binary forms, with or without
  *  modification, are permitted provided that the following conditions
@@ -15,11 +12,7 @@
  *  2. Redistributions in binary form must reproduce the above copyright
  *     notice, this list of conditions and the following disclaimer in the
  *     documentation and/or other materials provided with the distribution.
- *  3. All advertising materials mentioning features or use of this software
- *     must display the following acknowledgement:
- *         This product includes software developed by the NetBSD
- *         Foundation, Inc. and its contributors.
- *  4. Neither the name of The NetBSD Foundation nor the names of its
+ *  3. Neither the name of The NetBSD Foundation nor the names of its
  *     contributors may be used to endorse or promote products derived
  *     from this software without specific prior written permission.
  *
