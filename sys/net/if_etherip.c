0a26b9d55207022f8975e406f2e002c43c818f66
cube <cube>
2007-09-10 10:35:51 +0000

    Remove 3rd clause and my name from all the licences which were only in my name.

--- sys/net/if_etherip.c
+++ sys/net/if_etherip.c
@@ -1,4 +1,4 @@
-/*      $NetBSD: if_etherip.c,v 1.11 2007/08/30 02:17:35 dyoung Exp $        */
+/*      $NetBSD: if_etherip.c,v 1.12 2007/09/10 10:35:54 cube Exp $        */
 
 /*
  *  Copyright (c) 2006, Hans Rosenfeld <rosenfeld@grumpf.hope-2000.org>
@@ -32,9 +32,6 @@
  *  Copyright (c) 2003, 2004 The NetBSD Foundation.
  *  All rights reserved.
  *
- *  This code is derived from software contributed to the NetBSD Foundation
- *   by Quentin Garnier
- *
  *  Redistribution and use in source and binary forms, with or without
  *  modification, are permitted provided that the following conditions
  *  are met:
@@ -43,11 +40,7 @@
  *  2. Redistributions in binary form must reproduce the above copyright
  *     notice, this list of conditions and the following disclaimer in the
  *     documentation and/or other materials provided with the distribution.
- *  3. All advertising materials mentioning features or use of this software
- *     must display the following acknowledgement:
- *         This product includes software developed by the NetBSD
- *         Foundation, Inc. and its contributors.
- *  4. Neither the name of The NetBSD Foundation nor the names of its
+ *  3. Neither the name of The NetBSD Foundation nor the names of its
  *     contributors may be used to endorse or promote products derived
  *     from this software without specific prior written permission.
  *
