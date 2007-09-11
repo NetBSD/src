bee17a80c0bd2216c98831f9933d99dab80cfde2
matt <matt@NetBSD.org>
2007-09-11 02:32:26 +0000

    Initial support for Freescale i.MX31 (ARM1136).

--- sys/arch/arm/imx/imx31reg.h
+++ sys/arch/arm/imx/imx31reg.h
@@ -1,4 +1,4 @@
-/* $NetBSD: imx31reg.h,v 1.1.2.1 2007/08/29 05:24:24 matt Exp $ */
+/* $NetBSD: imx31reg.h,v 1.1.2.2 2007/09/11 02:32:27 matt Exp $ */
 /*-
  * Copyright (c) 2007 The NetBSD Foundation, Inc.
  * All rights reserved.
@@ -64,8 +64,10 @@
 #define	GPIO2_BASE	0x53fd0000
 #define	GPIO3_BASE	0x53fa4000
 
+#define	GPIO_SIZE	0x0100	/* Size of GPIO registers */
+
 #define	GPIO_DR		0x0000	/* GPIO Data (RW) */
-#define	GPIO_GDIR	0x0004	/* GPIO Direction (RW), 1=Output */
+#define	GPIO_DIR	0x0004	/* GPIO Direction (RW), 1=Output */
 #define	GPIO_PSR	0x0008	/* GPIO Pad Status (R) */
 #define	GPIO_ICR1	0x000c	/* GPIO Interrupt Configuration 1 (RW) */
 #define	GPIO_ICR2	0x0010	/* GPIO Interrupt Configuration 2 (RW) */
@@ -76,5 +78,50 @@
 #define	GPIO_ICR_LEVEL_HIGH	1
 #define	GPIO_ICR_EDGE_RISING	2
 #define	GPIO_ICR_EDGE_FALLING	3
+
+#define	INTC_BASE		0x68000000
+#define	INTC_SIZE		0x0400
+#define	IMX31_INTCNTL		0x0000	/* Interrupt Control (RW) */
+#define	IMX31_NIMASK		0x0004	/* Normal Interrupt Mask (RW) */
+#define	IMX31_INTENNUM		0x0008	/* Interrupt Enable Number (RW) */
+#define	IMX31_INTDISNUM		0x000c	/* Interrupt Disable Number (RW) */
+#define	IMX31_INTENABLEH	0x0010	/* Interrupt Enable High (RW) */
+#define	IMX31_INTENABLEL	0x0014	/* Interrupt Enable Low (RW) */
+#define	IMX31_INTTYPEH		0x0018	/* Interrupt Type High (RW) */
+#define	IMX31_INTTYPEL		0x001c	/* Interrupt Type Low (RW) */
+#define	IMX31_NIPRIORITY7	0x0020	/* Normal Intr Priority Level 7 (RW) */
+#define	IMX31_NIPRIORITY6	0x0024	/* Normal Intr Priority Level 6 (RW) */
+#define	IMX31_NIPRIORITY5	0x0028	/* Normal Intr Priority Level 5 (RW) */
+#define	IMX31_NIPRIORITY4	0x002c	/* Normal Intr Priority Level 4 (RW) */
+#define	IMX31_NIPRIORITY3	0x0030	/* Normal Intr Priority Level 3 (RW) */
+#define	IMX31_NIPRIORITY2	0x0034	/* Normal Intr Priority Level 2 (RW) */
+#define	IMX31_NIPRIORITY1	0x0038	/* Normal Intr Priority Level 1 (RW) */
+#define	IMX31_NIPRIORITY0	0x003c	/* Normal Intr Priority Level 0 (RW) */
+#define	IMX31_NIVECSR		0x0040	/* Normal Interrupt Vector Status (R) */
+#define	IMX31_FIVECSR		0x0044	/* Fast Interrupt Vector Status (R) */
+#define	IMX31_INTSRCH		0x0048	/* Interrupt Source High (R) */
+#define	IMX31_INTSRCL		0x004c	/* Interrupt Source Low (R) */
+#define	IMX31_INTFRCH		0x0050	/* Interrupt Force High (RW) */
+#define	IMX31_INTFRCL		0x0054	/* Interrupt Force Low (RW) */
+#define	IMX31_NIPNDH		0x0058	/* Normal Intr Pending High (R) */
+#define	IMX31_NIPNDL		0x005c	/* Normal Intr Pending Low (R) */
+#define	IMX31_FIPNDH		0x0060	/* Fast Intr Pending High (R) */
+#define	IMX31_FIPNDL		0x0064	/* Fast Intr Pending Low (R) */
+
+#define	IMX31_VECTOR(n)		(0x0100 + (n) * 4)	/* Vector [N] */
+
+#define	INTCNTL_ABFLAG	(1 << 25)	/* Core Arb. Priorty Risen (W1C) */
+#define	INTCNTL_ABFEN	(1 << 24)	/* ABFLAG Sticky Enable */
+#define	INTCNTL_NIDIS	(1 << 22)	/* Normal Intr. Disable */
+#define	INTCNTL_FIDIS	(1 << 21)	/* Fast Intr. Disable */
+#define	INTCNTL_NIAD	(1 << 20)	/* Normal Intr. Arbiter Rise ARM Lvl */
+#define	INTCNTL_FIAD	(1 << 19)	/* Fast Intr. Arbiter Rise ARM Level */
+#define	INTCNTL_NM	(1 << 18)	/* Normal Intr. Mode Control (1=AVIC) */
+
+#define	NIMASK_DIS_NONE		-1
+
+/*
+ * INTTYPE (0 = IRQ, 1 = FIQ)
+ */
 
 #endif /* _ARM_IMX_IMX31REG_H_ */
---
+++ sys/arch/arm/imx/imx31var.h
@@ -0,0 +1,24 @@
+/*	$NetBSD: imx31var.h,v 1.1.2.1 2007/09/11 02:32:28 matt Exp $	*/
+
+#ifndef _ARM_IMX_IMX31VAR_H
+#define _ARM_IMX_IMX31VAR_H
+
+extern struct bus_space imx31_bs_tag;
+
+struct aips_attach_args {
+	bus_space_tag_t	aipsa_memt;
+	bus_addr_t	aipsa_addr;
+	bus_size_t	aipsa_size;
+	int		aipsa_intr;
+};
+
+struct ahb_attach_args {
+	bus_space_tag_t	ahba_memt;
+	bus_dma_tag_t	ahba_dmat;
+	bus_addr_t	ahba_addr;
+	bus_size_t	ahba_size;
+	int		ahba_intr;
+	int		ahba_irqbase;
+};
+
+#endif	/* _ARM_IMX_IMX31VAR_H */
