8b937dec652a9151c1eaa8f6bc83d809fec4b07c
dsl <dsl@NetBSD.org>
2007-09-16 22:44:31 +0000

    Fix previous, 'long long' is 8 byte aligned on sparc32

--- sys/arch/sparc64/include/netbsd32_machdep.h
+++ sys/arch/sparc64/include/netbsd32_machdep.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: netbsd32_machdep.h,v 1.23 2007/09/16 22:35:02 dsl Exp $	*/
+/*	$NetBSD: netbsd32_machdep.h,v 1.24 2007/09/16 22:44:31 dsl Exp $	*/
 
 /*
  * Copyright (c) 1998, 2001 Matthew R. Green
@@ -39,8 +39,8 @@ struct proc;
 #define NETBSD32_POINTER_TYPE uint32_t
 typedef struct { NETBSD32_POINTER_TYPE i32; } netbsd32_pointer_t;
 
-/* sparc32 has 32bit aligned 64bit integers */
-#define NETBSD32_INT64_ALIGN __attribute__((__aligned__(4)))
+/* sparc32 has 64bit aligned 64bit integers */
+#define NETBSD32_INT64_ALIGN
 
 /* from <arch/sparc/include/signal.h> */
 typedef uint32_t netbsd32_sigcontextp_t;
