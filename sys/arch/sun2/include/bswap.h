b048efc22493f12f6384744cd7d77e9a10372a15
he <he@NetBSD.org>
2007-09-19 20:31:34 +0000

    Improve consistency between <sys/bswap.h> and what's actually defined by the C library for the various m68k ports by borrowing some #ifs from the i386 port.
    Also, align sun2 with the other m68k ports as to whether they
    define __bswap{16,32} or bswap{16,32} in their C library (all
    now define the __ variants).
    
    This should make the m68k ports build sys/rump again, except
    for sun2 which hits another problem later on.

--- sys/arch/sun2/include/bswap.h
+++ sys/arch/sun2/include/bswap.h
@@ -1,8 +1,9 @@
-/*     $NetBSD: bswap.h,v 1.1 2001/04/06 13:13:02 fredette Exp $   */
+/*     $NetBSD: bswap.h,v 1.2 2007/09/19 20:31:34 he Exp $   */
 
 #ifndef _MACHINE_BSWAP_H_
 #define	_MACHINE_BSWAP_H_
 
+#define __BSWAP_RENAME
 #include <sys/bswap.h>
 
 #endif /* !_MACHINE_BSWAP_H_ */
