40cfdb5e3a7ee3468bf0094b70635c5864af2a58
christos <christos@NetBSD.org>
2007-09-20 14:14:25 +0000

    Use AT_SUN_EXECPATH instead of argv[0] for the executable name. This needs kernel support, which is not there yet, thus $ORIGIN is broken at this time. This is ok, nothing uses it yet.

--- libexec/ld.elf_so/expand.c
+++ libexec/ld.elf_so/expand.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: expand.c,v 1.2 2007/05/18 21:46:39 christos Exp $	*/
+/*	$NetBSD: expand.c,v 1.3 2007/09/20 14:14:25 christos Exp $	*/
 
 /*-
  * Copyright (c) 2007 The NetBSD Foundation, Inc.
@@ -37,7 +37,7 @@
  */
 #include <sys/cdefs.h>
 #ifndef lint
-__RCSID("$NetBSD: expand.c,v 1.2 2007/05/18 21:46:39 christos Exp $");
+__RCSID("$NetBSD: expand.c,v 1.3 2007/09/20 14:14:25 christos Exp $");
 #endif /* not lint */
 
 #include <ctype.h>
@@ -75,7 +75,7 @@ static int mib[3][2] = {
 };
 
 static size_t
-expand(char *buf, const char *av, int what, size_t bl)
+expand(char *buf, const char *execname, int what, size_t bl)
 {
 	const char *p, *ep;
 	char *bp = buf;
@@ -88,11 +88,11 @@ expand(char *buf, const char *av, int what, size_t bl)
 		return 0;
 
 	case 2:	/* ORIGIN */
-		ep = strrchr(p = av, '/');
-		if (ep == NULL) {
-			p = ".";
-			ep = p + 1;
-		}
+		xprintf("execname %p\n", execname);
+		if (execname == NULL)
+			xerr(1, "execname not specified in AUX vector");
+		if ((ep = strrchr(p = execname, '/')) == NULL)
+			xerr(1, "bad execname `%s' in AUX vector", execname);
 		break;
 
 	case 3:	/* OSNAME */
@@ -117,8 +117,8 @@ expand(char *buf, const char *av, int what, size_t bl)
 		
 
 size_t
-_rtld_expand_path(char *buf, size_t bufsize, const char *av, const char *bp,
-    const char *ep)
+_rtld_expand_path(char *buf, size_t bufsize, const char *execname,
+    const char *bp, const char *ep)
 {
 	size_t i, ds = bufsize;
 	char *dp = buf;
@@ -142,7 +142,7 @@ _rtld_expand_path(char *buf, size_t bufsize, const char *av, const char *bp,
 					continue;
 
 				if (strncmp(bltn[i].name, p, s) == 0) {
-					size_t ls = expand(dp, av, i, ds);
+					size_t ls = expand(dp, execname, i, ds);
 					if (ls >= ds)
 						return bufsize;
 					ds -= ls;
