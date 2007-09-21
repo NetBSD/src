04b2c805970159f7cd35ec6e3ff8bc40089e750f
dsl <dsl@NetBSD.org>
2007-09-21 19:14:12 +0000

    Include sys/cdefs.h first.

--- sys/kern/kern_mutex.c
+++ sys/kern/kern_mutex.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: kern_mutex.c,v 1.17 2007/09/17 21:33:34 ad Exp $	*/
+/*	$NetBSD: kern_mutex.c,v 1.18 2007/09/21 19:14:12 dsl Exp $	*/
 
 /*-
  * Copyright (c) 2002, 2006, 2007 The NetBSD Foundation, Inc.
@@ -44,12 +44,12 @@
  *	    Richard McDougall.
  */
 
-#include "opt_multiprocessor.h"
-
 #define	__MUTEX_PRIVATE
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: kern_mutex.c,v 1.17 2007/09/17 21:33:34 ad Exp $");
+__KERNEL_RCSID(0, "$NetBSD: kern_mutex.c,v 1.18 2007/09/21 19:14:12 dsl Exp $");
+
+#include "opt_multiprocessor.h"
 
 #include <sys/param.h>
 #include <sys/proc.h>
