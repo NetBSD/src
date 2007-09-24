86161c2f6c2dcd73e7e9f85653369d345aaed8cc
joerg <joerg@NetBSD.org>
2007-09-24 16:33:00 +0000

    Generate the ACPI wakecode image dynamically at build time.

--- sys/arch/amd64/acpi/genwakecode.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-# $NetBSD: genwakecode.sh,v 1.1.2.2 2007/09/07 21:01:04 jmcneill Exp $
-
-sh ../../x86/acpi/genwakecode.sh $*
--- sys/arch/i386/acpi/genwakecode.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-# $NetBSD: genwakecode.sh,v 1.4.38.2 2007/09/07 21:01:05 jmcneill Exp $
-
-sh ../../x86/acpi/genwakecode.sh $*
