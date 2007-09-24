38ffc5694d519476d6980bbc30d1f0fd652938df
joerg <joerg@NetBSD.org>
2007-09-24 16:37:27 +0000

    Until the various drivers are fixed to handle interrupts correctly, restore original splhigh during state transitions.

--- sys/dev/acpi/acpi.c
+++ sys/dev/acpi/acpi.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: acpi.c,v 1.101.16.11 2007/09/10 00:25:17 christos Exp $	*/
+/*	$NetBSD: acpi.c,v 1.101.16.12 2007/09/24 16:37:27 joerg Exp $	*/
 
 /*-
  * Copyright (c) 2003, 2007 The NetBSD Foundation, Inc.
@@ -77,7 +77,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: acpi.c,v 1.101.16.11 2007/09/10 00:25:17 christos Exp $");
+__KERNEL_RCSID(0, "$NetBSD: acpi.c,v 1.101.16.12 2007/09/24 16:37:27 joerg Exp $");
 
 #include "opt_acpi.h"
 #include "opt_pcifixup.h"
@@ -1114,7 +1114,7 @@ is_available_state(struct acpi_softc *sc, int state)
 ACPI_STATUS
 acpi_enter_sleep_state(struct acpi_softc *sc, int state)
 {
-	int err;
+	int err, s;
 	ACPI_STATUS ret = AE_OK;
 
 	if (state == acpi_sleepstate)
@@ -1135,8 +1135,10 @@ acpi_enter_sleep_state(struct acpi_softc *sc, int state)
 			break;
 		}
 
+		s = splhigh();
 		if (state != ACPI_STATE_S1)
 			pnp_global_transition(PNP_STATE_D3);
+		splx(s);
 
 		ret = AcpiEnterSleepStatePrep(state);
 		if (ACPI_FAILURE(ret)) {
@@ -1158,8 +1160,10 @@ acpi_enter_sleep_state(struct acpi_softc *sc, int state)
 			if (state == ACPI_STATE_S4)
 				AcpiEnable();
 		}
+		s = splhigh();
 		if (state != ACPI_STATE_S1)
 			pnp_global_transition(PNP_STATE_D0);
+		splx(s);
 		AcpiLeaveSleepState((UINT8)state);
 
 		break;
