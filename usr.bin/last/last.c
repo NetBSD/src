3255f0a4c554c4a74983aa38f236ad6ea18f2295
lukem <lukem>
2007-10-05 07:27:41 +0000

    Convert to raise_default_signal(3).

--- usr.bin/last/last.c
+++ usr.bin/last/last.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: last.c,v 1.30 2007/01/06 13:31:06 cbiere Exp $	*/
+/*	$NetBSD: last.c,v 1.31 2007/10/05 07:27:42 lukem Exp $	*/
 
 /*
  * Copyright (c) 1987, 1993, 1994
@@ -40,7 +40,7 @@ __COPYRIGHT(
 #if 0
 static char sccsid[] = "@(#)last.c	8.2 (Berkeley) 4/2/94";
 #endif
-__RCSID("$NetBSD: last.c,v 1.30 2007/01/06 13:31:06 cbiere Exp $");
+__RCSID("$NetBSD: last.c,v 1.31 2007/10/05 07:27:42 lukem Exp $");
 #endif /* not lint */
 
 #include <sys/param.h>
@@ -50,7 +50,6 @@ __RCSID("$NetBSD: last.c,v 1.30 2007/01/06 13:31:06 cbiere Exp $");
 #include <errno.h>
 #include <fcntl.h>
 #include <paths.h>
-#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
