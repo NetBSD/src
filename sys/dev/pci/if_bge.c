7bfcdfb41616413bc3af4ed1e3ee2c61558c4f73
xtraeme <xtraeme@NetBSD.org>
2007-10-07 14:15:20 +0000

    Pull up following revision(s) (requested by msaitoh in ticket #918): 	sys/dev/pci/if_bge.c: revision 1.139 Fix three bugs. 1) Move a magic number writing code. Fix a watchdog timeout and "block failed to stop" problem on BCM5701 B5 card. 2) Fix incorrect register write. Fix a watchdog timeout "block failed to stop" problem on BCM5700 B2 card. 3) Wait more long time for PCISTATE register to return to its original pre-reset state. Fix "pcistate failed to revert" problem on BCM5703 A2 card.

--- sys/dev/pci/if_bge.c
+++ sys/dev/pci/if_bge.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: if_bge.c,v 1.122.2.6 2007/08/31 21:52:41 xtraeme Exp $	*/
+/*	$NetBSD: if_bge.c,v 1.122.2.7 2007/10/07 14:15:20 xtraeme Exp $	*/
 
 /*
  * Copyright (c) 2001 Wind River Systems
@@ -79,7 +79,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: if_bge.c,v 1.122.2.6 2007/08/31 21:52:41 xtraeme Exp $");
+__KERNEL_RCSID(0, "$NetBSD: if_bge.c,v 1.122.2.7 2007/10/07 14:15:20 xtraeme Exp $");
 
 #include "bpfilter.h"
 #include "vlan.h"
@@ -1102,7 +1102,7 @@ bge_init_tx_ring(struct bge_softc *sc)
 	/* NIC-memory send ring  not used; initialize to zero. */
 	CSR_WRITE_4(sc, BGE_MBX_TX_NIC_PROD0_LO, 0);
 	if (sc->bge_quirks & BGE_QUIRK_PRODUCER_BUG)	/* 5700 b2 errata */
-		CSR_WRITE_4(sc, BGE_MBX_TX_HOST_PROD0_LO, 0);
+		CSR_WRITE_4(sc, BGE_MBX_TX_NIC_PROD0_LO, 0);
 
 	SLIST_INIT(&sc->txdma_list);
 	for (i = 0; i < BGE_RSLOTS; i++) {
@@ -2828,11 +2828,6 @@ bge_reset(struct bge_softc *sc)
 			val |= (1<<29);
 		}
 	}
-	/*
-	 * Write the magic number to the firmware mailbox at 0xb50
-	 * so that the driver can synchronize with the firmware.
-	 */
-	bge_writemem_ind(sc, BGE_SOFTWARE_GENCOMM, BGE_MAGIC_NUMBER);
 
 	/* Issue global reset */
 	bge_writereg_ind(sc, BGE_MISC_CFG, val);
@@ -2881,6 +2876,12 @@ bge_reset(struct bge_softc *sc)
 	}
 
 	/*
+	 * Write the magic number to the firmware mailbox at 0xb50
+	 * so that the driver can synchronize with the firmware.
+	 */
+	bge_writemem_ind(sc, BGE_SOFTWARE_GENCOMM, BGE_MAGIC_NUMBER);
+
+	/*
 	 * Poll the value location we just wrote until
 	 * we see the 1's complement of the magic number.
 	 * This indicates that the firmware initialization
@@ -2913,7 +2914,7 @@ bge_reset(struct bge_softc *sc)
 	 * from the device's non-PCI registers may yield garbage
 	 * results.
 	 */
-	for (i = 0; i < BGE_TIMEOUT; i++) {
+	for (i = 0; i < 10000; i++) {
 		new_pcistate = pci_conf_read(pa->pa_pc, pa->pa_tag,
 		    BGE_PCI_PCISTATE);
 		if ((new_pcistate & ~BGE_PCISTATE_RESERVED) ==
