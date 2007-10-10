75d9f02661346e3ea0d72a5004caf0e766dc5355
ad <ad@NetBSD.org>
2007-10-10 21:59:11 +0000

    v_flag -> v_iflag

--- sys/compat/svr4_32/svr4_32_fcntl.c
+++ sys/compat/svr4_32/svr4_32_fcntl.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: svr4_32_fcntl.c,v 1.23 2007/10/08 15:12:06 ad Exp $	 */
+/*	$NetBSD: svr4_32_fcntl.c,v 1.24 2007/10/10 21:59:11 ad Exp $	 */
 
 /*-
  * Copyright (c) 1994, 1997 The NetBSD Foundation, Inc.
@@ -37,7 +37,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: svr4_32_fcntl.c,v 1.23 2007/10/08 15:12:06 ad Exp $");
+__KERNEL_RCSID(0, "$NetBSD: svr4_32_fcntl.c,v 1.24 2007/10/10 21:59:11 ad Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -263,7 +263,7 @@ fd_revoke(struct lwp *l, int fd, register_t *retval)
 	    KAUTH_GENERIC_ISSUSER, NULL)) != 0)
 		goto out;
 
-	if (vp->v_usecount > 1 || (vp->v_flag & VALIASED))
+	if (vp->v_usecount > 1 || (vp->v_iflag & VI_ALIASED))
 		VOP_REVOKE(vp, REVOKEALL);
 out:
 	vrele(vp);
