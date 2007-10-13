2da9f2b2876b824a34ac3261480dd8ed6430b667
isaki <isaki@NetBSD.org>
2007-10-13 13:05:19 +0000

    Correct indent.

--- sys/dev/mii/rlphy.c
+++ sys/dev/mii/rlphy.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: rlphy.c,v 1.15 2007/05/09 23:16:37 tsutsui Exp $	*/
+/*	$NetBSD: rlphy.c,v 1.16 2007/10/13 13:05:19 isaki Exp $	*/
 /*	$OpenBSD: rlphy.c,v 1.20 2005/07/31 05:27:30 pvalchev Exp $	*/
 
 /*
@@ -38,7 +38,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: rlphy.c,v 1.15 2007/05/09 23:16:37 tsutsui Exp $");
+__KERNEL_RCSID(0, "$NetBSD: rlphy.c,v 1.16 2007/10/13 13:05:19 isaki Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -78,7 +78,7 @@ const struct mii_phy_funcs rlphy_funcs = {
 
 static const struct mii_phydesc rlphys[] = {
 	{ MII_OUI_yyREALTEK,		MII_MODEL_yyREALTEK_RTL8201L,
-          MII_STR_yyREALTEK_RTL8201L },
+	  MII_STR_yyREALTEK_RTL8201L },
 	{ MII_OUI_ICPLUS,		MII_MODEL_ICPLUS_IP101,
 	  MII_STR_ICPLUS_IP101 },
 
