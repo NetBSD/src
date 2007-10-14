30c69af3c8dc45cebecc44dfcce7731f307b945c
riz <riz@NetBSD.org>
2007-10-14 05:54:35 +0000

    Pull up following revision(s) (requested by msaitoh in ticket #1801): 	sbin/ifconfig/ifconfig.c: revision 1.173 via patch
            If the kernel does not set the interface up after we set an address,
            do it explicitly, unless we (previously) set it down on user request
            (i.e. "ifconfig <name> x.x.x.x down").
    
            This will allow the network drivers to be "fixed" while keeping visible
            behaviour the same. Part of fixing PR 30694.

--- sbin/ifconfig/ifconfig.c
+++ sbin/ifconfig/ifconfig.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: ifconfig.c,v 1.152.2.1 2006/03/24 22:50:59 riz Exp $	*/
+/*	$NetBSD: ifconfig.c,v 1.152.2.2 2007/10/14 05:54:35 riz Exp $	*/
 
 /*-
  * Copyright (c) 1997, 1998, 2000 The NetBSD Foundation, Inc.
@@ -76,7 +76,7 @@ __COPYRIGHT("@(#) Copyright (c) 1983, 1993\n\
 #if 0
 static char sccsid[] = "@(#)ifconfig.c	8.2 (Berkeley) 2/16/94";
 #else
-__RCSID("$NetBSD: ifconfig.c,v 1.152.2.1 2006/03/24 22:50:59 riz Exp $");
+__RCSID("$NetBSD: ifconfig.c,v 1.152.2.2 2007/10/14 05:54:35 riz Exp $");
 #endif
 #endif /* not lint */
 
@@ -141,6 +141,7 @@ u_long	metric, mtu;
 int	clearaddr, s;
 int	newaddr = -1;
 int	conflicting = 0;
+int	check_up_state = -1;
 int	nsellength = 1;
 int	af;
 int	aflag, bflag, Cflag, dflag, lflag, mflag, sflag, uflag, vflag, zflag;
@@ -159,6 +160,7 @@ void 	notrailers(const char *, int);
 void 	setifaddr(const char *, int);
 void 	setifdstaddr(const char *, int);
 void 	setifflags(const char *, int);
+void	check_ifflags_up(const char *);
 void	setifcaps(const char *, int);
 void 	setifbroadaddr(const char *, int);
 void 	setifipdst(const char *, int);
@@ -671,6 +673,8 @@ main(int argc, char *argv[])
 		(void) strncpy(afp->af_addreq, name, sizeof ifr.ifr_name);
 		if (ioctl(s, afp->af_aifaddr, afp->af_addreq) == -1)
 			warn("SIOCAIFADDR");
+		else if (check_up_state < 0)
+			check_up_state = 1;
 	}
 
 	if (g_ifcr_updated) {
@@ -679,6 +683,9 @@ main(int argc, char *argv[])
 		if (ioctl(s, SIOCSIFCAP, &g_ifcr) == -1)
 			err(EXIT_FAILURE, "SIOCSIFCAP");
 	}
+
+	if (check_up_state == 1)
+		check_ifflags_up(name);
 
 	exit(0);
 }
@@ -1104,6 +1111,21 @@ setifdstaddr(const char *addr, int param)
 }
 
 void
+check_ifflags_up(const char *vname)
+{
+	struct ifreq ifreq;
+
+	(void) strncpy(ifreq.ifr_name, name, sizeof(ifreq.ifr_name));
+	if (ioctl(s, SIOCGIFFLAGS, &ifreq) == -1)
+		err(EXIT_FAILURE, "SIOCGIFFLAGS");
+	if (ifreq.ifr_flags & IFF_UP)
+		return;
+	ifreq.ifr_flags |= IFF_UP;
+	if (ioctl(s, SIOCSIFFLAGS, &ifreq) == -1)
+		err(EXIT_FAILURE, "SIOCSIFFLAGS");
+}
+
+void
 setifflags(const char *vname, int value)
 {
 	struct ifreq ifreq;
@@ -1115,6 +1137,8 @@ setifflags(const char *vname, int value)
 
 	if (value < 0) {
 		value = -value;
+		if (value == IFF_UP)
+			check_up_state = 0;
 		flags &= ~value;
 	} else
 		flags |= value;
