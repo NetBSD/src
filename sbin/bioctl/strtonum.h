807311e5f131f8101ca18410785c504ae7f32087
riz <riz>
2007-10-15 20:37:06 +0000

    Clean up after a mistake I made pulling up ticket #1838.  We really only need one copy of each new file.  :-)

--- sbin/bioctl/strtonum.h
+++ sbin/bioctl/strtonum.h
@@ -1,12 +1,4 @@
-/* $NetBSD: strtonum.h,v 1.1.8.2 2007/10/15 05:09:51 riz Exp $ */
-
-long long strtonum(const char *, long long, long long, const char **);
-
-/* $NetBSD: strtonum.h,v 1.1.8.2 2007/10/15 05:09:51 riz Exp $ */
-
-long long strtonum(const char *, long long, long long, const char **);
-
-/* $NetBSD: strtonum.h,v 1.1.8.2 2007/10/15 05:09:51 riz Exp $ */
+/* $NetBSD: strtonum.h,v 1.1.8.3 2007/10/15 20:37:07 riz Exp $ */
 
 long long strtonum(const char *, long long, long long, const char **);
 
