dff6736f7416bc1bc19ec31819ae202b3cc0982e
plunky <plunky>
2007-10-25 19:32:15 +0000

    remove #include <usbhid.h> as its not needed

--- sys/dev/usb/uslsa.c
+++ sys/dev/usb/uslsa.c
@@ -1,4 +1,4 @@
-/* $NetBSD: uslsa.c,v 1.3 2007/06/13 17:14:25 nathanw Exp $ */
+/* $NetBSD: uslsa.c,v 1.4 2007/10/25 19:32:15 plunky Exp $ */
 
 /*
  * Copyright (c) 2007 The NetBSD Foundation, Inc.
@@ -41,7 +41,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: uslsa.c,v 1.3 2007/06/13 17:14:25 nathanw Exp $");
+__KERNEL_RCSID(0, "$NetBSD: uslsa.c,v 1.4 2007/10/25 19:32:15 plunky Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -51,7 +51,6 @@ __KERNEL_RCSID(0, "$NetBSD: uslsa.c,v 1.3 2007/06/13 17:14:25 nathanw Exp $");
 #include <sys/tty.h>
 
 #include <dev/usb/usb.h>
-#include <dev/usb/usbhid.h>
 
 #include <dev/usb/usbdi.h>
 #include <dev/usb/usbdi_util.h>
