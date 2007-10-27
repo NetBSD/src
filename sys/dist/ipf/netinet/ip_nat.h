004bb2f26ba02c9cb79f650ecc9a80790b38f712
pavel <pavel@NetBSD.org>
2007-10-27 15:58:55 +0000

    revert pullup #880. It reportedly causes RDR sessions to not expire and fill up the NAT table. See PR kern/37037 and possibly kern/37174.

--- sys/dist/ipf/netinet/ip_nat.h
+++ sys/dist/ipf/netinet/ip_nat.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: ip_nat.h,v 1.7.12.4 2007/09/16 15:46:49 xtraeme Exp $	*/
+/*	$NetBSD: ip_nat.h,v 1.7.12.5 2007/10/27 15:58:55 pavel Exp $	*/
 
 /*
  * Copyright (C) 1995-2001, 2003 by Darren Reed.
@@ -124,7 +124,6 @@ typedef	struct	nat	{
 	char		nat_ifnames[2][LIFNAMSIZ];
 	int		nat_rev;		/* 0 = forward, 1 = reverse */
 	int		nat_redir;		/* copy of in_redir */
-	u_32_t		nat_seqnext[2];
 } nat_t;
 
 #define	nat_inip	nat_inip6.in4
