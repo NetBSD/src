b76f664d4909e28b59a89a71b422231639bfb0d0
rjs <rjs>
2007-10-28 13:29:21 +0000

    Move prototype for kd_later() before first use.

--- sys/arch/sun3/dev/kd.c
+++ sys/arch/sun3/dev/kd.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: kd.c,v 1.51 2007/10/18 18:54:59 joerg Exp $	*/
+/*	$NetBSD: kd.c,v 1.52 2007/10/28 13:29:21 rjs Exp $	*/
 
 /*-
  * Copyright (c) 1996 The NetBSD Foundation, Inc.
@@ -46,7 +46,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: kd.c,v 1.51 2007/10/18 18:54:59 joerg Exp $");
+__KERNEL_RCSID(0, "$NetBSD: kd.c,v 1.52 2007/10/28 13:29:21 rjs Exp $");
 
 #include <sys/param.h>
 #include <sys/proc.h>
@@ -92,6 +92,7 @@ static int kdparam(struct tty *, struct termios *);
 static void kdstart(struct tty *);
 static void kd_init(struct kd_softc *);
 static void kd_cons_input(int);
+static void kd_later(void *);
 
 dev_type_open(kdopen);
 dev_type_close(kdclose);
@@ -281,7 +282,6 @@ kdparam(struct tty *tp, struct termios *t)
 }
 
 
-static void kd_later(void*);
 static void kd_putfb(struct tty *);
 
 static void 
