83e6426ad71ae67269f11f963e331fa674291b04
ad <ad@NetBSD.org>
2007-10-28 15:47:13 +0000

    /dev/klog:
    - Put locking around it since it's used by printf().
    - Mark it MPSAFE.

--- sys/kern/subr_prf.c
+++ sys/kern/subr_prf.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: subr_prf.c,v 1.105.4.4 2007/10/23 20:17:13 ad Exp $	*/
+/*	$NetBSD: subr_prf.c,v 1.105.4.5 2007/10/28 15:47:15 ad Exp $	*/
 
 /*-
  * Copyright (c) 1986, 1988, 1991, 1993
@@ -37,7 +37,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: subr_prf.c,v 1.105.4.4 2007/10/23 20:17:13 ad Exp $");
+__KERNEL_RCSID(0, "$NetBSD: subr_prf.c,v 1.105.4.5 2007/10/28 15:47:15 ad Exp $");
 
 #include "opt_ddb.h"
 #include "opt_ipkdb.h"
@@ -368,7 +368,6 @@ addlog(const char *fmt, ...)
 static void
 putchar(int c, int flags, struct tty *tp)
 {
-	struct kern_msgbuf *mbp;
 
 	if (panicstr)
 		constty = NULL;
@@ -381,30 +380,8 @@ putchar(int c, int flags, struct tty *tp)
 	    (flags & TOCONS) && tp == constty)
 		constty = NULL;
 	if ((flags & TOLOG) &&
-	    c != '\0' && c != '\r' && c != 0177 && msgbufenabled) {
-		mbp = msgbufp;
-		if (mbp->msg_magic != MSG_MAGIC) {
-			/*
-			 * Arguably should panic or somehow notify the
-			 * user...  but how?  Panic may be too drastic,
-			 * and would obliterate the message being kicked
-			 * out (maybe a panic itself), and printf
-			 * would invoke us recursively.  Silently punt
-			 * for now.  If syslog is running, it should
-			 * notice.
-			 */
-			msgbufenabled = 0;
-		} else {
-			mbp->msg_bufc[mbp->msg_bufx++] = c;
-			if (mbp->msg_bufx < 0 || mbp->msg_bufx >= mbp->msg_bufs)
-				mbp->msg_bufx = 0;
-			/* If the buffer is full, keep the most recent data. */
-			if (mbp->msg_bufr == mbp->msg_bufx) {
-				 if (++mbp->msg_bufr >= mbp->msg_bufs)
-					mbp->msg_bufr = 0;
-			}
-		}
-	}
+	    c != '\0' && c != '\r' && c != 0177)
+	    	logputchar(c);
 	if ((flags & TOCONS) && constty == NULL && c != '\0')
 		(*v_putc)(c);
 #ifdef DDB
