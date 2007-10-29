9c0fb59d44f6b00ab21789880f8affedba311362
pavel <pavel>
2007-10-29 19:47:12 +0000

    Pull up following revisions (requested by plunky in ticket #956): 	usr.sbin/apm/apm.c: revision 1.18 	sys/dev/acpi/acpi_apm.c: revision 1.11 When no estimate of battery minutes_left can be made, apm should return a value of 0.

--- sys/dev/acpi/acpi_apm.c
+++ sys/dev/acpi/acpi_apm.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: acpi_apm.c,v 1.8 2006/11/16 01:32:47 christos Exp $	*/
+/*	$NetBSD: acpi_apm.c,v 1.8.2.1 2007/10/29 19:47:12 pavel Exp $	*/
 
 /*-
  * Copyright (c) 2006 The NetBSD Foundation, Inc.
@@ -42,7 +42,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: acpi_apm.c,v 1.8 2006/11/16 01:32:47 christos Exp $");
+__KERNEL_RCSID(0, "$NetBSD: acpi_apm.c,v 1.8.2.1 2007/10/29 19:47:12 pavel Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -289,7 +289,7 @@ acpiapm_get_powstat(void *opaque, u_int batteryid,
 	(void)memset(pinfo, 0, sizeof(*pinfo));
 	pinfo->ac_state = APM_AC_UNKNOWN;
 	pinfo->minutes_valid = 0;
-	pinfo->minutes_left = 0xffff; /* unknown */
+	pinfo->minutes_left = 0;
 	pinfo->batteryid = 0;
 	pinfo->nbattery = 0;	/* to be incremented as batteries are found */
 	pinfo->battery_flags = 0;
