cd4d6d59faff7144b08aba96bda552d336c0b799
xtraeme <xtraeme>
2007-10-30 20:58:57 +0000

    regen

--- sys/dev/pci/pcidevs_data.h
+++ sys/dev/pci/pcidevs_data.h
@@ -1,10 +1,10 @@
-/*	$NetBSD: pcidevs_data.h,v 1.911 2007/10/30 20:44:31 xtraeme Exp $	*/
+/*	$NetBSD: pcidevs_data.h,v 1.912 2007/10/30 20:58:57 xtraeme Exp $	*/
 
 /*
  * THIS FILE AUTOMATICALLY GENERATED.  DO NOT EDIT.
  *
  * generated from:
- *	NetBSD: pcidevs,v 1.911 2007/10/30 20:44:12 xtraeme Exp
+ *	NetBSD: pcidevs,v 1.912 2007/10/30 20:58:38 xtraeme Exp
  */
 
 /*
@@ -7060,12 +7060,12 @@ static const struct pci_product pci_products[] = {
 	    "80321 I/O Processor",
 	},
 	{
-	    PCI_VENDOR_INTEL, PCI_PRODUCT_INTEL_IOP332_A,
-	    "IOP332 PCI Express-to-PCI Bridge #0",
+	    PCI_VENDOR_INTEL, PCI_PRODUCT_INTEL_IOP333_A,
+	    "IOP333 PCI Express-to-PCI Bridge #0",
 	},
 	{
-	    PCI_VENDOR_INTEL, PCI_PRODUCT_INTEL_IOP332_B,
-	    "IOP332 PCI Express-to-PCI Bridge #1",
+	    PCI_VENDOR_INTEL, PCI_PRODUCT_INTEL_IOP333_B,
+	    "IOP333 PCI Express-to-PCI Bridge #1",
 	},
 	{
 	    PCI_VENDOR_INTEL, PCI_PRODUCT_INTEL_6700PXH_PCIE0,
