e3bd97acf2c7e13c70707329ad0b10afda1bb7d5
xtraeme <xtraeme@NetBSD.org>
2007-11-04 21:27:13 +0000

    Pull up following revision(s) (requested by tron in ticket #976): 	usr.sbin/rpc.lockd/lock_proc.c: revision 1.8
    Fix off-by-one error accessing "clnt_cache_addr" array which causes heap
    corruption. This will hopefully fix PR bin/37236.

--- usr.sbin/rpc.lockd/lock_proc.c
+++ usr.sbin/rpc.lockd/lock_proc.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: lock_proc.c,v 1.7 2000/10/11 20:23:56 is Exp $	*/
+/*	$NetBSD: lock_proc.c,v 1.7.18.1 2007/11/04 21:27:13 xtraeme Exp $	*/
 
 /*
  * Copyright (c) 1995
@@ -35,7 +35,7 @@
 
 #include <sys/cdefs.h>
 #ifndef lint
-__RCSID("$NetBSD: lock_proc.c,v 1.7 2000/10/11 20:23:56 is Exp $");
+__RCSID("$NetBSD: lock_proc.c,v 1.7.18.1 2007/11/04 21:27:13 xtraeme Exp $");
 #endif
 
 #include <sys/param.h>
@@ -237,7 +237,7 @@ get_client(host_addr, vers)
 	memcpy(&clnt_cache_addr[clnt_cache_next_to_use], host_addr,
 	    host_addr->sa_len);
 	clnt_cache_time[clnt_cache_next_to_use] = time_now.tv_sec;
-	if (++clnt_cache_next_to_use > CLIENT_CACHE_SIZE)
+	if (++clnt_cache_next_to_use >= CLIENT_CACHE_SIZE)
 		clnt_cache_next_to_use = 0;
 
 	/*
