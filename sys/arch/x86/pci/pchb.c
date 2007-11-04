9b06addd2ebeb463e37569333ac71458c5efba91
jmcneill <jmcneill@NetBSD.org>
2007-11-04 22:19:52 +0000

    Re-add 965 support lost in an earlier merge.

--- sys/arch/x86/pci/pchb.c
+++ sys/arch/x86/pci/pchb.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: pchb.c,v 1.1.4.3 2007/10/31 23:14:03 joerg Exp $ */
+/*	$NetBSD: pchb.c,v 1.1.4.4 2007/11/04 22:19:52 jmcneill Exp $ */
 
 /*-
  * Copyright (c) 1996, 1998, 2000 The NetBSD Foundation, Inc.
@@ -37,7 +37,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: pchb.c,v 1.1.4.3 2007/10/31 23:14:03 joerg Exp $");
+__KERNEL_RCSID(0, "$NetBSD: pchb.c,v 1.1.4.4 2007/11/04 22:19:52 jmcneill Exp $");
 
 #include <sys/types.h>
 #include <sys/param.h>
@@ -327,6 +327,7 @@ pchbattach(struct device *parent, struct device *self, void *aux)
 		case PCI_PRODUCT_INTEL_82945GM_HB:
 		case PCI_PRODUCT_INTEL_82965Q_HB:
 		case PCI_PRODUCT_INTEL_82965G_HB:
+		case PCI_PRODUCT_INTEL_82965PM_HB:
 			/*
 			 * The host bridge is either in GFX mode (internal
 			 * graphics) or in AGP mode. In GFX mode, we pretend
