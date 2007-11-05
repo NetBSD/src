33d1424687da15695af4ae77b844204c8f502a18
xtraeme <xtraeme>
2007-11-05 23:57:32 +0000

    Missed a struct device -> device_t, found by joerg.

--- sys/dev/acpi/aiboost.c
+++ sys/dev/acpi/aiboost.c
@@ -1,4 +1,4 @@
-/* $NetBSD: aiboost.c,v 1.16 2007/11/04 13:59:49 xtraeme Exp $ */
+/* $NetBSD: aiboost.c,v 1.17 2007/11/05 23:57:32 xtraeme Exp $ */
 
 /*-
  * Copyright (c) 2007 Juan Romero Pardines
@@ -28,7 +28,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: aiboost.c,v 1.16 2007/11/04 13:59:49 xtraeme Exp $");
+__KERNEL_RCSID(0, "$NetBSD: aiboost.c,v 1.17 2007/11/05 23:57:32 xtraeme Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -79,7 +79,7 @@ static int	aiboost_refresh_sensors(struct sysmon_envsys *,
 
 /* autoconf(9) glue */
 static int	aiboost_acpi_match(device_t, struct cfdata *, void *);
-static void	aiboost_acpi_attach(device_t, struct device *, void *);
+static void	aiboost_acpi_attach(device_t, device_t, void *);
 
 CFATTACH_DECL_NEW(aiboost, sizeof(struct aiboost_softc), aiboost_acpi_match,
     aiboost_acpi_attach, NULL, NULL);
