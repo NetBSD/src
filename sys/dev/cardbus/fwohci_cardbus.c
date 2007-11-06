0ee100fd1a3f42e57318b4f8c19fc820e7aeac1a
kiyohara <kiyohara@NetBSD.org>
2007-11-06 15:24:10 +0000

    The argument of *_intr_establish() is changed to fwohci_flit.   Our ieee1394 coordinates from FreeBSD implementation. Add fwohci_pci_shutdown() and fwohci_pci_power(). Move __KERNEL_RCSID to ahead.

--- sys/dev/cardbus/fwohci_cardbus.c
+++ sys/dev/cardbus/fwohci_cardbus.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: fwohci_cardbus.c,v 1.20 2007/10/19 11:59:38 ad Exp $	*/
+/*	$NetBSD: fwohci_cardbus.c,v 1.21 2007/11/06 15:24:11 kiyohara Exp $	*/
 
 /*-
  * Copyright (c) 2000, 2001 The NetBSD Foundation, Inc.
@@ -37,7 +37,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: fwohci_cardbus.c,v 1.20 2007/10/19 11:59:38 ad Exp $");
+__KERNEL_RCSID(0, "$NetBSD: fwohci_cardbus.c,v 1.21 2007/11/06 15:24:11 kiyohara Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -144,7 +144,7 @@ XXX	(ct->ct_cf->cardbus_mem_open)(cc, 0, iob, iob + 0x40);
 	    csr | CARDBUS_COMMAND_MASTER_ENABLE | CARDBUS_COMMAND_MEM_ENABLE);
 
 	sc->sc_ih = cardbus_intr_establish(cc, cf, ca->ca_intrline,
-					   IPL_BIO, fwohci_intr, sc);
+					   IPL_BIO, fwohci_filt, sc);
 	if (sc->sc_ih == NULL) {
 		printf("%s: couldn't establish interrupt\n", devname);
 		return;
