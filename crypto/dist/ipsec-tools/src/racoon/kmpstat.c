21ccf706b78a04aa4754291158e1a8c60c1f8a7c
vanhu <vanhu@NetBSD.org>
2007-11-06 16:41:33 +0000

    From Scott Lamb: plog changed to _plog to work with new plog macro

--- crypto/dist/ipsec-tools/src/racoon/kmpstat.c
+++ crypto/dist/ipsec-tools/src/racoon/kmpstat.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: kmpstat.c,v 1.4.6.1 2007/06/06 12:26:53 manu Exp $	*/
+/*	$NetBSD: kmpstat.c,v 1.4.6.2 2007/11/06 16:41:33 vanhu Exp $	*/
 
 /*	$KAME: kmpstat.c,v 1.33 2004/08/16 08:20:28 itojun Exp $	*/
 
@@ -186,7 +186,7 @@ bad1:
  * Dumb plog functions (used by sockmisc.c) 
  */
 void
-plog(int pri, const char *func, struct sockaddr *sa, const char *fmt, ...)
+_plog(int pri, const char *func, struct sockaddr *sa, const char *fmt, ...)
 {
 	va_list ap;
 
