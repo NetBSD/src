f2917076e2778529050943ba97fc285227da50d1
ad <ad@NetBSD.org>
2007-11-07 00:39:10 +0000

    4.99.35 - more merges from vmlocking.

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.282 2007/11/07 00:23:42 ad Exp $	*/
+/*	$NetBSD: param.h,v 1.283 2007/11/07 00:39:10 ad Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -63,7 +63,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	499003400	/* NetBSD 4.99.34 */
+#define	__NetBSD_Version__	499003500	/* NetBSD 4.99.35 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
