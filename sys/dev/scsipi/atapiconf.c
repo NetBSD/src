7186758ae3d4a527b4070accc7a22bbca6f46b73
joerg <joerg@NetBSD.org>
2007-11-12 12:09:45 +0000

    Add null handlers for atapibus, it doesn't seem to need any magic.

--- sys/dev/scsipi/atapiconf.c
+++ sys/dev/scsipi/atapiconf.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: atapiconf.c,v 1.72 2006/11/16 01:33:26 christos Exp $	*/
+/*	$NetBSD: atapiconf.c,v 1.72.22.1 2007/11/12 12:09:45 joerg Exp $	*/
 
 /*
  * Copyright (c) 1996, 2001 Manuel Bouyer.  All rights reserved.
@@ -30,7 +30,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: atapiconf.c,v 1.72 2006/11/16 01:33:26 christos Exp $");
+__KERNEL_RCSID(0, "$NetBSD: atapiconf.c,v 1.72.22.1 2007/11/12 12:09:45 joerg Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -160,6 +160,9 @@ atapibusattach(struct device *parent, struct device *self, void *aux)
 	chan->chan_init_cb = NULL;
 	chan->chan_init_cb_arg = NULL;
 	scsipi_channel_init(chan);
+
+	if (!pnp_device_register(self, NULL, NULL))
+		aprint_error_dev(self, "couldn't establish power handler\n");
 
 	/* Probe the bus for devices. */
 	atapi_probe_bus(sc, -1);
