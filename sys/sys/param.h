cea10c711a4a831bcb7ae151b22fb502a6e0c28e
ad <ad@NetBSD.org>
2007-11-12 23:13:30 +0000

    4.99.36: struct proc/lwp changed.

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.284 2007/11/12 23:12:00 ad Exp $	*/
+/*	$NetBSD: param.h,v 1.285 2007/11/12 23:13:30 ad Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -63,7 +63,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	499003500	/* NetBSD 4.99.35 */
+#define	__NetBSD_Version__	499003600	/* NetBSD 4.99.36 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
