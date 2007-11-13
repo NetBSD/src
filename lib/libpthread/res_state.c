d513914376a5299db12ae4386d9789ece5e6fc68
ad <ad>
2007-11-13 15:57:10 +0000

    For PR bin/37347:
    - Override __libc_thr_init() instead of using our own constructor.
    - Add pthread__getenv() and use instead of getenv(). This is used before
      we are up and running and unfortunatley getenv() takes locks.
    
    Other changes:
    
    - Cache the spinlock vectors in pthread__st. Internal spinlock operations
      now take 1 function call instead of 3 (i386).
    - Use pthread__self() internally, not pthread_self().
    - Use __attribute__ ((visibility("hidden"))) in some places.
    - Kill PTHREAD_MAIN_DEBUG.

--- lib/libpthread/res_state.c
+++ lib/libpthread/res_state.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: res_state.c,v 1.4 2004/06/03 19:32:27 christos Exp $	*/
+/*	$NetBSD: res_state.c,v 1.5 2007/11/13 15:57:14 ad Exp $	*/
 
 /*-
  * Copyright (c) 2004 The NetBSD Foundation, Inc.
@@ -38,7 +38,7 @@
 
 #include <sys/cdefs.h>
 #if defined(LIBC_SCCS) && !defined(lint)
-__RCSID("$NetBSD: res_state.c,v 1.4 2004/06/03 19:32:27 christos Exp $");
+__RCSID("$NetBSD: res_state.c,v 1.5 2007/11/13 15:57:14 ad Exp $");
 #endif
 
 #include <sys/types.h>
@@ -73,7 +73,7 @@ static void
 res_state_debug(const char *msg, void *p)
 {
 	char buf[512];
-	pthread_t self = pthread_self();
+	pthread_t self = pthread__self();
 	int len = snprintf(buf, sizeof(buf), "%p: %s %p\n", self, msg, p);
 
 	(void)write(STDOUT_FILENO, buf, (size_t)len);
