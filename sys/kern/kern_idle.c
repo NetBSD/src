75f65f6250df6c078cb3d4e9c5340b2d52565971
ad <ad>
2007-11-15 20:12:25 +0000

    Lock curlwp when updating the start time.

--- sys/kern/kern_idle.c
+++ sys/kern/kern_idle.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: kern_idle.c,v 1.8 2007/11/13 22:14:35 ad Exp $	*/
+/*	$NetBSD: kern_idle.c,v 1.9 2007/11/15 20:12:25 ad Exp $	*/
 
 /*-
  * Copyright (c)2002, 2006, 2007 YAMAMOTO Takashi,
@@ -28,7 +28,7 @@
 
 #include <sys/cdefs.h>
 
-__KERNEL_RCSID(0, "$NetBSD: kern_idle.c,v 1.8 2007/11/13 22:14:35 ad Exp $");
+__KERNEL_RCSID(0, "$NetBSD: kern_idle.c,v 1.9 2007/11/15 20:12:25 ad Exp $");
 
 #include <sys/param.h>
 #include <sys/cpu.h>
@@ -48,7 +48,9 @@ idle_loop(void *dummy)
 	struct lwp *l = curlwp;
 
 	/* Update start time for this thread. */
+	lwp_lock(l);
 	microtime(&l->l_stime);
+	lwp_unlock(l);
 
 	KERNEL_UNLOCK_ALL(l, NULL);
 	l->l_stat = LSONPROC;
