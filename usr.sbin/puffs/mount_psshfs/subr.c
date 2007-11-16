519f6a8ecfc4d131b84e1a83a46e897f3a37aeb4
jmmv <jmmv@NetBSD.org>
2007-11-16 15:10:06 +0000

    Drop trailing whitespace.  On behalf of pooka.

--- usr.sbin/puffs/mount_psshfs/subr.c
+++ usr.sbin/puffs/mount_psshfs/subr.c
@@ -1,17 +1,17 @@
-/*      $NetBSD: subr.c,v 1.34 2007/11/16 14:25:47 pooka Exp $        */
-        
-/*      
+/*      $NetBSD: subr.c,v 1.35 2007/11/16 15:10:06 jmmv Exp $        */
+
+/*
  * Copyright (c) 2006  Antti Kantee.  All Rights Reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
- * are met:     
- * 1. Redistributions of source code must retain the above copyright  
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
  *    notice, this list of conditions and the following disclaimer.
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- *      
+ *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS
  * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -23,11 +23,11 @@
  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
- */     
-        
+ */
+
 #include <sys/cdefs.h>
 #ifndef lint
-__RCSID("$NetBSD: subr.c,v 1.34 2007/11/16 14:25:47 pooka Exp $");
+__RCSID("$NetBSD: subr.c,v 1.35 2007/11/16 15:10:06 jmmv Exp $");
 #endif /* !lint */
 
 #include <assert.h>
@@ -148,7 +148,7 @@ readdir_getattr_resp(struct puffs_usermount *pu,
 	} else if (pdir) {
 		pdir->getattr_pb = NULL;
 	}
-		
+
 	free(rda);
 	puffs_framebuf_destroy(pb);
 }
