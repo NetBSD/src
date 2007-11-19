05a88d703cb0563a440b892742e23147b50f0023
bouyer <bouyer@NetBSD.org>
2007-11-19 21:24:24 +0000

    Pull up following revision(s) via patch (requested by msaitoh in ticket #1863): 	sys/dev/pci/if_wm.c			 1.137-1.138,1.141,1.144-1.146 	sys/dev/pci/if_wmreg.h			 1.23 	sys/dev/pci/pcidevs			 1.893 via patch 	share/man/man4/wm.4			 1.18-1.19 Fix typo. Fix EEPROM reload sequence. Add ICH9 support. Workaround for 82541 Errata 29 and 82547 Errata 28:  These devices have to reset the PHY before reset the MAC.  Reported and tested by salo.

--- sys/dev/pci/if_wm.c
+++ sys/dev/pci/if_wm.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: if_wm.c,v 1.100.2.9 2007/10/14 05:07:51 riz Exp $	*/
+/*	$NetBSD: if_wm.c,v 1.100.2.10 2007/11/19 21:24:24 bouyer Exp $	*/
 
 /*
  * Copyright (c) 2001, 2002, 2003, 2004 Wasabi Systems, Inc.
@@ -79,7 +79,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: if_wm.c,v 1.100.2.9 2007/10/14 05:07:51 riz Exp $");
+__KERNEL_RCSID(0, "$NetBSD: if_wm.c,v 1.100.2.10 2007/11/19 21:24:24 bouyer Exp $");
 
 #include "bpfilter.h"
 #include "rnd.h"
@@ -251,6 +251,7 @@ typedef enum {
 	WM_T_82573,			/* i82573 */
 	WM_T_80003,			/* i80003 */
 	WM_T_ICH8,			/* ICH8 LAN */
+	WM_T_ICH9,			/* ICH9 LAN */
 } wm_chip_type;
 
 /*
@@ -560,6 +561,7 @@ static void	wm_kmrn_i80003_writereg(struct wm_softc *, int, int);
 static int	wm_match(struct device *, struct cfdata *, void *);
 static void	wm_attach(struct device *, struct device *, void *);
 static int	wm_is_onboard_nvm_eeprom(struct wm_softc *);
+static void	wm_get_auto_rd_done(struct wm_softc *);
 static int	wm_get_swsm_semaphore(struct wm_softc *);
 static void	wm_put_swsm_semaphore(struct wm_softc *);
 static int	wm_poll_eerd_eewr_done(struct wm_softc *, int);
@@ -729,7 +731,7 @@ static const struct wm_product {
 	  WM_T_82547,		WMP_F_1000T },
 
 	{ PCI_VENDOR_INTEL,	PCI_PRODUCT_INTEL_82547EI_MOBILE,
-	  "Intel i82547EI Moblie 1000BASE-T Ethernet",
+	  "Intel i82547EI Mobile 1000BASE-T Ethernet",
 	  WM_T_82547,		WMP_F_1000T },
 
 	{ PCI_VENDOR_INTEL,	PCI_PRODUCT_INTEL_82547GI,
@@ -823,7 +825,21 @@ static const struct wm_product {
 	{ PCI_VENDOR_INTEL,	PCI_PRODUCT_INTEL_82801H_IFE_G,
 	  "Intel i82801H IFE (G) LAN Controller",
 	  WM_T_ICH8,		WMP_F_1000T },
-
+	{ PCI_VENDOR_INTEL,	PCI_PRODUCT_INTEL_82801I_IGP_AMT,
+	  "82801I (AMT) LAN Controller",
+	  WM_T_ICH9,		WMP_F_1000T },
+	{ PCI_VENDOR_INTEL,	PCI_PRODUCT_INTEL_82801I_IFE,
+	  "82801I LAN Controller",
+	  WM_T_ICH9,		WMP_F_1000T },
+	{ PCI_VENDOR_INTEL,	PCI_PRODUCT_INTEL_82801I_IFE_G,
+	  "82801I (G) LAN Controller",
+	  WM_T_ICH9,		WMP_F_1000T },
+	{ PCI_VENDOR_INTEL,	PCI_PRODUCT_INTEL_82801I_IFE_GT,
+	  "82801I (GT) LAN Controller",
+	  WM_T_ICH9,		WMP_F_1000T },
+	{ PCI_VENDOR_INTEL,	PCI_PRODUCT_INTEL_82801I_IGP_C,
+	  "82801I (C) LAN Controller",
+	  WM_T_ICH9,		WMP_F_1000T },
 	{ 0,			0,
 	  NULL,
 	  0,			0 },
@@ -1070,7 +1086,7 @@ wm_attach(struct device *parent, struct device *self, void *aux)
 		}
 	} else if (sc->sc_type >= WM_T_82571) {
 		sc->sc_flags |= WM_F_PCIE;
-		if (sc->sc_type != WM_T_ICH8)
+		if ((sc->sc_type != WM_T_ICH8) || (sc->sc_type != WM_T_ICH9))
 			sc->sc_flags |= WM_F_EEPROM_SEMAPHORE;
 		aprint_verbose("%s: PCI-Express bus\n", sc->sc_dev.dv_xname);
 	} else {
@@ -1240,7 +1256,7 @@ wm_attach(struct device *parent, struct device *self, void *aux)
 	/*
 	 * Get some information about the EEPROM.
 	 */
-	if (sc->sc_type == WM_T_ICH8) {
+	if ((sc->sc_type == WM_T_ICH8) || (sc->sc_type == WM_T_ICH9)) {
 		uint32_t flash_size;
 		sc->sc_flags |= WM_F_SWFWHW_SYNC | WM_F_EEPROM_FLASH;
 		memtype = pci_mapreg_type(pa->pa_pc, pa->pa_tag, WM_ICH8_FLASH);
@@ -1408,7 +1424,8 @@ wm_attach(struct device *parent, struct device *self, void *aux)
 	 * Determine if we're TBI or GMII mode, and initialize the
 	 * media structures accordingly.
 	 */
-	if (sc->sc_type == WM_T_ICH8 || sc->sc_type == WM_T_82573) {
+	if (sc->sc_type == WM_T_ICH8 || sc->sc_type == WM_T_ICH9
+	    || sc->sc_type == WM_T_82573) {
 		/* STATUS_TBIMODE reserved/reused, can't rely on it */
 		wm_gmii_mediainit(sc);
 	} else if (sc->sc_type < WM_T_82543 ||
@@ -2697,7 +2714,7 @@ wm_tick(void *arg)
 static void
 wm_reset(struct wm_softc *sc)
 {
-	int i;
+	uint32_t reg;
 
 	/*
 	 * Allocate on-chip memory according to the MTU size.
@@ -2727,12 +2744,42 @@ wm_reset(struct wm_softc *sc)
 		sc->sc_pba = PBA_8K;
 		CSR_WRITE(sc, WMREG_PBS, PBA_16K);
 		break;
+	case WM_T_ICH9:
+		sc->sc_pba = PBA_10K;
+		break;
 	default:
 		sc->sc_pba = sc->sc_ethercom.ec_if.if_mtu > 8192 ?
 		    PBA_40K : PBA_48K;
 		break;
 	}
 	CSR_WRITE(sc, WMREG_PBA, sc->sc_pba);
+
+	if (sc->sc_flags & WM_F_PCIE) {
+		int timeout = 800;
+
+		sc->sc_ctrl |= CTRL_GIO_M_DIS;
+		CSR_WRITE(sc, WMREG_CTRL, sc->sc_ctrl);
+
+		while (timeout) {
+			if ((CSR_READ(sc, WMREG_STATUS) & STATUS_GIO_M_ENA) == 0)
+				break;
+			delay(100);
+		}
+	}
+
+	/* clear interrupt */
+	CSR_WRITE(sc, WMREG_IMC, 0xffffffffU);
+
+	/*
+	 * 82541 Errata 29? & 82547 Errata 28?
+	 * See also the description about PHY_RST bit in CTRL register
+	 * in 8254x_GBe_SDM.pdf.
+	 */
+	if ((sc->sc_type == WM_T_82541) || (sc->sc_type == WM_T_82547)) {
+		CSR_WRITE(sc, WMREG_CTRL,
+		    CSR_READ(sc, WMREG_CTRL) | CTRL_PHY_RESET);
+		delay(5000);
+	}
 
 	switch (sc->sc_type) {
 	case WM_T_82544:
@@ -2765,8 +2812,10 @@ wm_reset(struct wm_softc *sc)
 		break;
 
 	case WM_T_ICH8:
+	case WM_T_ICH9:
 		wm_get_swfwhw_semaphore(sc);
 		CSR_WRITE(sc, WMREG_CTRL, CTRL_RST | CTRL_PHY_RESET);
+		delay(10000);
 
 	default:
 		/* Everything else can safely use the documented method. */
@@ -2775,27 +2824,47 @@ wm_reset(struct wm_softc *sc)
 	}
 	delay(10000);
 
+	/* reload EEPROM */
+	switch(sc->sc_type) {
+	case WM_T_82542_2_0:
+	case WM_T_82542_2_1:
+	case WM_T_82543:
+	case WM_T_82544:
+		delay(10);
+		reg = CSR_READ(sc, WMREG_CTRL_EXT) | CTRL_EXT_EE_RST;
+		CSR_WRITE(sc, WMREG_CTRL_EXT, reg);
+		delay(2000);
+		break;
+	case WM_T_82541:
+	case WM_T_82541_2:
+	case WM_T_82547:
+	case WM_T_82547_2:
+		delay(20000);
+		break;
+	case WM_T_82573:
+		if (sc->sc_flags & WM_F_EEPROM_FLASH) {
+			delay(10);
+			reg = CSR_READ(sc, WMREG_CTRL_EXT) | CTRL_EXT_EE_RST;
+			CSR_WRITE(sc, WMREG_CTRL_EXT, reg);
+		}
+		/* FALLTHROUGH */
+	default:
+		/* check EECD_EE_AUTORD */
+		wm_get_auto_rd_done(sc);
+	}
+
+#if 0
 	for (i = 0; i < 1000; i++) {
-		if ((CSR_READ(sc, WMREG_CTRL) & CTRL_RST) == 0)
+		if ((CSR_READ(sc, WMREG_CTRL) & CTRL_RST) == 0) {
 			return;
+		}
 		delay(20);
 	}
 
 	if (CSR_READ(sc, WMREG_CTRL) & CTRL_RST)
 		log(LOG_ERR, "%s: reset failed to complete\n",
 		    sc->sc_dev.dv_xname);
-
-	if (sc->sc_type >= WM_T_80003) {
-		/* wait for eeprom to reload */
-		for (i = 1000; i > 0; i--) {
-			if (CSR_READ(sc, WMREG_EECD) & EECD_EE_AUTORD)
-				break;
-		}
-		if (i == 0) {
-			log(LOG_ERR, "%s: auto read from eeprom failed to "
-			    "complete\n", sc->sc_dev.dv_xname);
-		}
-	}
+#endif
 }
 
 /*
@@ -3181,6 +3250,39 @@ wm_stop(struct ifnet *ifp, int disable)
 	ifp->if_timer = 0;
 }
 
+void
+wm_get_auto_rd_done(struct wm_softc *sc)
+{
+	int i;
+
+	/* wait for eeprom to reload */
+	switch (sc->sc_type) {
+	case WM_T_82571:
+	case WM_T_82572:
+	case WM_T_82573:
+	case WM_T_80003:
+	case WM_T_ICH8:
+	case WM_T_ICH9:
+		for (i = 10; i > 0; i--) {
+			if (CSR_READ(sc, WMREG_EECD) & EECD_EE_AUTORD)
+				break;
+			delay(1000);
+		}
+		if (i == 0) {
+			log(LOG_ERR, "%s: auto read from eeprom failed to "
+			    "complete\n", sc->sc_dev.dv_xname);
+		}
+		break;
+	default:
+		delay(5000);
+		break;
+	}
+
+	/* Phy configuration starts after EECD_AUTO_RD is set */
+	if (sc->sc_type == WM_T_82573)
+		delay(25000);
+}
+
 /*
  * wm_acquire_eeprom:
  *
@@ -3445,7 +3547,7 @@ wm_read_eeprom(struct wm_softc *sc, int word, int wordcnt, uint16_t *data)
 	if (wm_acquire_eeprom(sc))
 		return (1);
 
-	if (sc->sc_type == WM_T_ICH8)
+	if ((sc->sc_type == WM_T_ICH8) || (sc->sc_type == WM_T_ICH9))
 		rv = wm_read_eeprom_ich8(sc, word, wordcnt, data);
 	else if (sc->sc_flags & WM_F_EEPROM_EERDEEWR)
 		rv = wm_read_eeprom_eerd(sc, word, wordcnt, data);
@@ -3643,7 +3745,7 @@ wm_set_filter(struct wm_softc *sc)
 	for (i = 1; i < size; i++)
 		wm_set_ral(sc, NULL, i);
 
-	if (sc->sc_type == WM_T_ICH8)
+	if ((sc->sc_type == WM_T_ICH8) || (sc->sc_type == WM_T_ICH9))
 		size = WM_ICH8_MC_TABSIZE;
 	else
 		size = WM_MC_TABSIZE;
@@ -3668,7 +3770,7 @@ wm_set_filter(struct wm_softc *sc)
 		hash = wm_mchash(sc, enm->enm_addrlo);
 
 		reg = (hash >> 5);
-		if (sc->sc_type == WM_T_ICH8)
+		if ((sc->sc_type == WM_T_ICH8) || (sc->sc_type == WM_T_ICH9))
 			reg &= 0x1f;
 		else
 			reg &= 0x7f;
@@ -3970,7 +4072,7 @@ wm_gmii_reset(struct wm_softc *sc)
 	uint32_t reg;
 	int func = 0; /* XXX gcc */
 
-	if (sc->sc_type == WM_T_ICH8) {
+	if ((sc->sc_type == WM_T_ICH8) || (sc->sc_type == WM_T_ICH9)) {
 		if (wm_get_swfwhw_semaphore(sc))
 			return;
 	}
@@ -4014,7 +4116,7 @@ wm_gmii_reset(struct wm_softc *sc)
 		sc->sc_ctrl_ext = reg | CTRL_EXT_SWDPIN(4);
 #endif
 	}
-	if (sc->sc_type == WM_T_ICH8)
+	if ((sc->sc_type == WM_T_ICH8) || (sc->sc_type == WM_T_ICH9))
 		wm_put_swfwhw_semaphore(sc);
 	if (sc->sc_type == WM_T_80003)
 		wm_put_swfw_semaphore(sc, func ? SWFW_PHY1_SM : SWFW_PHY0_SM);
@@ -4845,7 +4947,7 @@ wm_read_ich8_data(struct wm_softc *sc, uint32_t index,
 static int32_t
 wm_read_ich8_byte(struct wm_softc *sc, uint32_t index, uint8_t* data)
 {
-    int32_t status = 0;
+    int32_t status;
     uint16_t word = 0;
 
     status = wm_read_ich8_data(sc, index, 1, &word);
@@ -4867,7 +4969,8 @@ wm_read_ich8_byte(struct wm_softc *sc, uint32_t index, uint8_t* data)
 static int32_t
 wm_read_ich8_word(struct wm_softc *sc, uint32_t index, uint16_t *data)
 {
-    int32_t status = 0;
+    int32_t status;
+
     status = wm_read_ich8_data(sc, index, 2, data);
     return status;
 }
