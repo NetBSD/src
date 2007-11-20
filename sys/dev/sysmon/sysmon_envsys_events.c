0adb3a84c98c454e0e5e6ad18ffdf91c4dc33b30
xtraeme <xtraeme@NetBSD.org>
2007-11-20 17:24:32 +0000

    After comments from Andrew Doran:
    - The mutex for the callout handler must run at IPL_SOFTCLOCK.
    - Just stop the callout in sysmon_envsys_unregister() and don't wait
      for the callout to finish.

--- sys/dev/sysmon/sysmon_envsys_events.c
+++ sys/dev/sysmon/sysmon_envsys_events.c
@@ -1,4 +1,4 @@
-/* $NetBSD: sysmon_envsys_events.c,v 1.45 2007/11/16 08:00:16 xtraeme Exp $ */
+/* $NetBSD: sysmon_envsys_events.c,v 1.46 2007/11/20 17:24:32 xtraeme Exp $ */
 
 /*-
  * Copyright (c) 2007 Juan Romero Pardines.
@@ -30,7 +30,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: sysmon_envsys_events.c,v 1.45 2007/11/16 08:00:16 xtraeme Exp $");
+__KERNEL_RCSID(0, "$NetBSD: sysmon_envsys_events.c,v 1.46 2007/11/20 17:24:32 xtraeme Exp $");
 
 #include <sys/param.h>
 #include <sys/types.h>
@@ -63,7 +63,7 @@ static const struct sme_sensor_event sme_sensor_event[] = {
 };
 
 kmutex_t sme_mtx, sme_events_mtx, sme_callout_mtx;
-kcondvar_t sme_cv, sme_callout_cv;
+kcondvar_t sme_cv;
 static bool sysmon_low_power = false;
 
 #define SME_EVTIMO	(SME_EVENTS_DEFTIMEOUT * hz)
@@ -413,7 +413,6 @@ sme_events_check(void *arg)
 	KASSERT(sme != NULL);
 
 	mutex_enter(&sme_callout_mtx);
-	sme->sme_flags |= SME_CALLOUT_BUSY;
 
 	LIST_FOREACH(see, &sme->sme_events_list, see_list)
 		workqueue_enqueue(sme->sme_wq, &see->see_wk, NULL);
@@ -429,11 +428,9 @@ sme_events_check(void *arg)
 	else
 		timo = SME_EVTIMO;
 
-	sme->sme_flags &= ~SME_CALLOUT_BUSY;
 	if (!sysmon_low_power)
 		callout_schedule(&sme->sme_callout, timo);
 
-	cv_broadcast(&sme_callout_cv);
 	mutex_exit(&sme_callout_mtx);
 }
 
