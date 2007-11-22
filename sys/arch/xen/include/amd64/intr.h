9c23628d9193735c8c6fa0bef55aa5fd67c6bb2d
bouyer <bouyer>
2007-11-22 16:16:59 +0000

    Pull up the bouyer-xenamd64 branch to HEAD. This brings in amd64 support to NetBSD/Xen, both Dom0 and DomU.

---
+++ sys/arch/xen/include/amd64/intr.h
@@ -0,0 +1,3 @@
+/*	$NetBSD: intr.h,v 1.2.14.2 2007/11/22 16:17:01 bouyer Exp $	*/
+
+#include <xen/intr.h>
---
+++ sys/arch/xen/include/amd64/pci_machdep.h
@@ -0,0 +1,5 @@
+/*	$NetBSD: pci_machdep.h,v 1.2.14.2 2007/11/22 16:17:02 bouyer Exp $	*/
+
+#define _PCI_HAVE_DMA64
+
+#include <xen/pci_machdep.h>
