707b987ddfb27f629cf5b56b4a4348543ba36524
bouyer <bouyer@NetBSD.org>
2007-11-22 16:16:59 +0000

    Pull up the bouyer-xenamd64 branch to HEAD. This brings in amd64 support to NetBSD/Xen, both Dom0 and DomU.

---
+++ sys/arch/xen/include/amd64/bus_private.h
@@ -0,0 +1,3 @@
+/*	$NetBSD: bus_private.h,v 1.2.14.2 2007/11/22 16:16:59 bouyer Exp $	*/
+
+#include <xen/bus_private.h>
---
+++ sys/arch/xen/include/amd64/i82489var.h
@@ -0,0 +1,3 @@
+/*	$NetBSD: i82489var.h,v 1.2.14.2 2007/11/22 16:17:00 bouyer Exp $	*/
+
+#include <xen/i82489var.h>
