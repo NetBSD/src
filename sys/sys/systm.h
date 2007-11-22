b83f9a6a6d7d50e97e575e675e93e0761065ea20
plunky <plunky@NetBSD.org>
2007-11-22 22:56:16 +0000

    remove insque/remque from libkern, they are no longer used

--- sys/sys/systm.h
+++ sys/sys/systm.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: systm.h,v 1.202 2007/11/13 22:14:34 ad Exp $	*/
+/*	$NetBSD: systm.h,v 1.203 2007/11/22 22:56:16 plunky Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1988, 1991, 1993
@@ -132,13 +132,6 @@ extern int boothowto;		/* reboot flags, from console subsystem */
 #define	bootquiet	(boothowto & AB_QUIET)
 
 extern void (*v_putc)(int); /* Virtual console putc routine */
-
-extern	void	_insque(void *, void *);
-extern	void	_remque(void *);
-
-/* casts to keep lint happy, but it should be happy with void **/
-#define	insque(q,p)	_insque(q, p)
-#define	remque(q)	_remque(q)
 
 /*
  * General function declarations.
