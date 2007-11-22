583cf20eee473098645410251ddd6a59db4bd7b3
bouyer <bouyer@NetBSD.org>
2007-11-22 21:21:14 +0000

    Fix bouyer-xenamd64 merge fallout: we can #include "opt_xen.h" when #if defined(_KERNEL) && !defined(_RUMPKERNEL) && !defined(_LKM), #ifdef _KERNEL isn't enough.

--- sys/arch/amd64/include/types.h
+++ sys/arch/amd64/include/types.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: types.h,v 1.18 2007/11/22 16:16:45 bouyer Exp $	*/
+/*	$NetBSD: types.h,v 1.19 2007/11/22 21:21:14 bouyer Exp $	*/
 
 /*-
  * Copyright (c) 1990 The Regents of the University of California.
@@ -73,7 +73,7 @@ typedef	volatile unsigned char		__cpu_simple_lock_t;
 #define __HAVE_CPU_MAXPROC
 #define __HAVE_TIMECOUNTER
 
-#if defined(_KERNEL)
+#if defined(_KERNEL) && !defined(_RUMPKERNEL) && !defined(_LKM)
 #include "opt_xen.h"
 #define __HAVE_RAS
 #ifndef XEN
