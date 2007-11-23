f65d0f7ad20ab9a0c9d9a797b8c714a4e2219170
dyoung <dyoung@NetBSD.org>
2007-11-23 22:58:54 +0000

    Delete state GRE_S_DOCONF, I no longer use it.

--- sys/net/if_gre.h
+++ sys/net/if_gre.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: if_gre.h,v 1.30 2007/10/06 03:30:26 dyoung Exp $ */
+/*	$NetBSD: if_gre.h,v 1.31 2007/11/23 22:58:54 dyoung Exp $ */
 
 /*
  * Copyright (c) 1998 The NetBSD Foundation, Inc.
@@ -61,7 +61,6 @@ struct gre_soparm {
 enum gre_state {
 	  GRE_S_IDLE = 0
 	, GRE_S_IOCTL
-	, GRE_S_DOCONF
 	, GRE_S_DIE
 };
 
