d8edaad123c7db2b7c235b19e49f013c5888e4f0
markd <markd@NetBSD.org>
2007-11-24 02:19:37 +0000

    Add agp support for Intel Q35/G33/Q33.

--- sys/dev/pci/agp.c
+++ sys/dev/pci/agp.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: agp.c,v 1.51 2007/11/12 20:00:14 joerg Exp $	*/
+/*	$NetBSD: agp.c,v 1.52 2007/11/24 02:19:37 markd Exp $	*/
 
 /*-
  * Copyright (c) 2000 Doug Rabson
@@ -65,7 +65,7 @@
 
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: agp.c,v 1.51 2007/11/12 20:00:14 joerg Exp $");
+__KERNEL_RCSID(0, "$NetBSD: agp.c,v 1.52 2007/11/24 02:19:37 markd Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -165,6 +165,12 @@ const struct agp_product {
 	{ PCI_VENDOR_INTEL,	PCI_PRODUCT_INTEL_82965PM_HB,
 	  NULL,			agp_i810_attach },
 	{ PCI_VENDOR_INTEL,	PCI_PRODUCT_INTEL_82965G_HB,
+	  NULL,			agp_i810_attach },
+	{ PCI_VENDOR_INTEL,	PCI_PRODUCT_INTEL_82Q35_HB,
+	  NULL,			agp_i810_attach },
+	{ PCI_VENDOR_INTEL,	PCI_PRODUCT_INTEL_82G33_HB,
+	  NULL,			agp_i810_attach },
+	{ PCI_VENDOR_INTEL,	PCI_PRODUCT_INTEL_82Q33_HB,
 	  NULL,			agp_i810_attach },
 #endif
 
