adf2b7a6eb929474105155dba58bc3f94e62397d
pooka <pooka@NetBSD.org>
2007-11-26 12:57:26 +0000

    In case the userspace wait is interrupted, don't use ERESTART as the return value, rather use EINTR.
    reported by Reinoud

--- sys/fs/puffs/puffs_msgif.c
+++ sys/fs/puffs/puffs_msgif.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: puffs_msgif.c,v 1.59 2007/11/20 11:51:01 pooka Exp $	*/
+/*	$NetBSD: puffs_msgif.c,v 1.60 2007/11/26 12:57:26 pooka Exp $	*/
 
 /*
  * Copyright (c) 2005, 2006, 2007  Antti Kantee.  All Rights Reserved.
@@ -30,7 +30,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: puffs_msgif.c,v 1.59 2007/11/20 11:51:01 pooka Exp $");
+__KERNEL_RCSID(0, "$NetBSD: puffs_msgif.c,v 1.60 2007/11/26 12:57:26 pooka Exp $");
 
 #include <sys/param.h>
 #include <sys/fstrans.h>
@@ -493,7 +493,7 @@ puffs_msg_wait(struct puffs_mount *pmp, struct puffs_msgpark *park)
 			}
 			mutex_exit(&pmp->pmp_lock);
 
-			rv = error;
+			rv = EINTR;
 		}
 	} else {
 		rv = preq->preq_rv;
