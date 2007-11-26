6ddc1ca4b1ba22c61f2c0c0f7d032dfe92ec1ea2
bjs <bjs@NetBSD.org>
2007-11-26 09:39:50 +0000

    Add PCI_PRODUCT_ATI_RADEON_R430_554F to the R420 chip class.
    This will not work until (at the very least) we map the memory properly
    (MMIO/VRAM BARs are 64-bit)--some of the values in the configuration
    space may be different.  I will discuss this with others, and maybe
    it will work someday.

--- sys/dev/pci/radeonfb.c
+++ sys/dev/pci/radeonfb.c
@@ -1,4 +1,4 @@
-/* $NetBSD: radeonfb.c,v 1.21 2007/10/19 12:00:54 ad Exp $ */
+/* $NetBSD: radeonfb.c,v 1.22 2007/11/26 09:39:50 bjs Exp $ */
 
 /*-
  * Copyright (c) 2006 Itronix Inc.
@@ -70,7 +70,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: radeonfb.c,v 1.21 2007/10/19 12:00:54 ad Exp $");
+__KERNEL_RCSID(0, "$NetBSD: radeonfb.c,v 1.22 2007/11/26 09:39:50 bjs Exp $");
 
 #define RADEONFB_DEFAULT_DEPTH 32
 
@@ -357,6 +357,7 @@ static struct {
 	{ PCI_PRODUCT_ATI_RADEON_R423_UR,	RADEON_R420, 0 },
 	{ PCI_PRODUCT_ATI_RADEON_R423_UT,	RADEON_R420, 0 },
 	{ PCI_PRODUCT_ATI_RADEON_R423_5D57,	RADEON_R420, 0 },
+	{ PCI_PRODUCT_ATI_RADEON_R430_554F,	RADEON_R420, 0 },
 
 	{ 0, 0, 0 }
 };
