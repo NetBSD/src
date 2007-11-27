c37d3b6e9c542d960374bea016d4096e39851c43
christos <christos@NetBSD.org>
2007-11-27 15:49:48 +0000

    Shuffle things around so that pcb_savefpu goes back to be aligned in a 16 bit boundary. Noted by Arto Huusko.

--- sys/arch/amd64/include/pcb.h
+++ sys/arch/amd64/include/pcb.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: pcb.h,v 1.9 2007/11/26 15:38:29 christos Exp $	*/
+/*	$NetBSD: pcb.h,v 1.10 2007/11/27 15:49:48 christos Exp $	*/
 
 /*-
  * Copyright (c) 1998 The NetBSD Foundation, Inc.
@@ -100,6 +100,11 @@ struct pcb {
 	 * Should just make this a pointer and allocate.
 	 */
 	struct	x86_64_tss pcb_tss;
+	int	  pcb_flags;
+#define	PCB_USER_LDT	0x01		/* has user-set LDT */
+#define PCB_GS64	0x02
+#define PCB_FS64	0x04
+	int	  pcb_cr0;		/* saved image of CR0 */
 	u_int64_t pcb_cr2;		/* page fault address (CR2) */
 	u_int64_t pcb_cr3;
 	u_int64_t pcb_rsp;
@@ -107,16 +112,11 @@ struct pcb {
 	u_int64_t pcb_usersp;
 	u_int64_t pcb_ldt_sel;
 	struct	savefpu pcb_savefpu;	/* floating point state */
-	int	pcb_cr0;		/* saved image of CR0 */
-	int	pcb_flags;
-#define	PCB_USER_LDT	0x01		/* has user-set LDT */
-#define PCB_GS64	0x02
-#define PCB_FS64	0x04
-	void *	pcb_onfault;		/* copyin/out fault recovery */
+	void     *pcb_onfault;		/* copyin/out fault recovery */
 	struct cpu_info *pcb_fpcpu;	/* cpu holding our fp state. */
-	unsigned pcb_iomap[NIOPORTS/32];	/* I/O bitmap */
-	uint64_t pcb_gs;
-	uint64_t pcb_fs;
+	unsigned  pcb_iomap[NIOPORTS/32];	/* I/O bitmap */
+	uint64_t  pcb_gs;
+	uint64_t  pcb_fs;
 };
 
 /*    
