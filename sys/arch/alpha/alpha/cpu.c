d3d80270ab69ba06ede866b8abb8c19143db2983
ad <ad@NetBSD.org>
2007-11-28 17:40:02 +0000

    Hook in the atomic ops on alpha.

--- sys/arch/alpha/alpha/cpu.c
+++ sys/arch/alpha/alpha/cpu.c
@@ -1,4 +1,4 @@
-/* $NetBSD: cpu.c,v 1.79 2007/11/19 02:12:11 ad Exp $ */
+/* $NetBSD: cpu.c,v 1.80 2007/11/28 17:40:02 ad Exp $ */
 
 /*-
  * Copyright (c) 1998, 1999, 2000, 2001 The NetBSD Foundation, Inc.
@@ -66,7 +66,7 @@
 
 #include <sys/cdefs.h>			/* RCS ID & Copyright macro defns */
 
-__KERNEL_RCSID(0, "$NetBSD: cpu.c,v 1.79 2007/11/19 02:12:11 ad Exp $");
+__KERNEL_RCSID(0, "$NetBSD: cpu.c,v 1.80 2007/11/28 17:40:02 ad Exp $");
 
 #include "opt_ddb.h"
 #include "opt_multiprocessor.h"
@@ -397,6 +397,7 @@ cpu_boot_secondary_processors(void)
 {
 	struct cpu_info *ci;
 	u_long i;
+	bool did_patch = false;
 
 	for (i = 0; i < ALPHA_MAXPROCS; i++) {
 		ci = cpu_info[i];
@@ -406,6 +407,12 @@ cpu_boot_secondary_processors(void)
 			continue;
 		if ((cpus_booted & (1UL << i)) == 0)
 			continue;
+
+		/* Patch MP-criticial kernel routines. */
+		if (did_patch == false) {
+			alpha_patch(true);
+			did_patch = true;
+		}
 
 		/*
 		 * Link the processor into the list, and launch it.
