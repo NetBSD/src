ca1c89e40ab95182bd39eac9ba7bb92fd8b618d0
ad <ad@NetBSD.org>
2007-11-29 16:14:28 +0000

    - Change lock_cas from "compare and set" to "compare and swap". - Add aliases for atomic_cas_ulong(), etc.
    Ok skrll@

--- sys/arch/hppa/include/rwlock.h
+++ sys/arch/hppa/include/rwlock.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: rwlock.h,v 1.3 2007/11/21 10:19:07 yamt Exp $	*/
+/*	$NetBSD: rwlock.h,v 1.4 2007/11/29 16:14:28 ad Exp $	*/
 
 /*-
  * Copyright (c) 2002, 2007 The NetBSD Foundation, Inc.
@@ -53,9 +53,10 @@ struct krwlock {
 #define	RW_RECEIVE(rw)			/* nothing */
 #define	RW_GIVE(rw)			/* nothing */
 
-#define	RW_CAS(p, o, n)			_lock_cas((p), (o), (n))
+uintptr_t	_lock_cas(volatile uintptr_t *, uintptr_t, uintptr_t);
 
-int	_lock_cas(volatile uintptr_t *, uintptr_t, uintptr_t);
+#define	RW_CAS(p, o, n)			\
+    (_lock_cas((volatile uintptr_t *)(p), (o), (n)) == (o))
 
 #endif	/* __RWLOCK_PRIVATE */
 
