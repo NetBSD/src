0c9310728a0c978f35916876dd388cb8f736e8bc
ad <ad@NetBSD.org>
2007-11-29 01:02:40 +0000

    Only build the 64-bit versions if __HAVE_ATOMIC64_OPS.

--- common/lib/libc/atomic/atomic_swap_64_cas.c
+++ common/lib/libc/atomic/atomic_swap_64_cas.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: atomic_swap_64_cas.c,v 1.3 2007/11/28 16:59:11 ad Exp $	*/
+/*	$NetBSD: atomic_swap_64_cas.c,v 1.4 2007/11/29 01:02:45 ad Exp $	*/
 
 /*-
  * Copyright (c) 2007 The NetBSD Foundation, Inc.
@@ -40,6 +40,8 @@
 
 #include <sys/atomic.h>
 
+#ifdef __HAVE_ATOMIC64_OPS
+
 uint64_t
 atomic_swap_64(volatile uint64_t *addr, uint64_t new)
 {
@@ -62,3 +64,5 @@ __strong_alias(_atomic_swap_ulong,_atomic_swap_64)
 atomic_op_alias(atomic_swap_ptr,_atomic_swap_64)
 __strong_alias(_atomic_swap_ptr,_atomic_swap_64)
 #endif /* _LP64 */
+
+#endif
