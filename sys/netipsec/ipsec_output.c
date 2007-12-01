3ae6fee2004bc1793dad4bfd8065b27760eadbd8
bouyer <bouyer@NetBSD.org>
2007-12-01 17:33:14 +0000

    Pull up following revision(s) (requested by adrianp in ticket #11395): 	sys/netipsec/xform_ah.c: revision 1.19 via patch 	sys/netipsec/ipsec.c: revision 1.34 via patch 	sys/netipsec/xform_ipip.c: revision 1.18 via patch 	sys/netipsec/ipsec_output.c: revision 1.23 via patch 	sys/netipsec/ipsec_osdep.h: revision 1.21 via patch The function ipsec4_get_ulp assumes that ip_off is in host order. This results in IPsec processing that is dependent on protocol and/or port can be bypassed. Bug report, analysis and initial fix from Karl Knutsson. Final patch and ok from degroote@

--- sys/netipsec/ipsec_output.c
+++ sys/netipsec/ipsec_output.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: ipsec_output.c,v 1.12.2.1 2004/05/10 15:02:18 tron Exp $	*/
+/*	$NetBSD: ipsec_output.c,v 1.12.2.2 2007/12/01 17:33:15 bouyer Exp $	*/
 
 /*-
  * Copyright (c) 2002, 2003 Sam Leffler, Errno Consulting
@@ -29,7 +29,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: ipsec_output.c,v 1.12.2.1 2004/05/10 15:02:18 tron Exp $");
+__KERNEL_RCSID(0, "$NetBSD: ipsec_output.c,v 1.12.2.2 2007/12/01 17:33:15 bouyer Exp $");
 
 /*
  * IPsec output processing.
@@ -462,9 +462,7 @@ ipsec4_process_packet(
 					goto bad;
 				}
 				ip = mtod(m, struct ip *);
-				ip->ip_off = ntohs(ip->ip_off);
-				ip->ip_off |= IP_DF;
-				ip->ip_off = htons(ip->ip_off);
+				ip->ip_off |= IP_OFF_CONVERT(IP_DF);
 			}
 		}
 	}
