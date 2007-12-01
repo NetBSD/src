09ef3d7043840f449486f4601c7b0ea172425609
jmcneill <jmcneill@NetBSD.org>
2007-12-01 04:10:03 +0000

    pnp events are now executed in a separate workqueue thread.

--- sys/kern/kern_pnp.c
+++ sys/kern/kern_pnp.c
@@ -1,4 +1,4 @@
-/* $NetBSD: kern_pnp.c,v 1.1.2.15 2007/11/23 16:03:15 joerg Exp $ */
+/* $NetBSD: kern_pnp.c,v 1.1.2.16 2007/12/01 04:10:03 jmcneill Exp $ */
 
 /*-
  * Copyright (c) 2007 Jared D. McNeill <jmcneill@invisible.ca>
@@ -33,7 +33,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: kern_pnp.c,v 1.1.2.15 2007/11/23 16:03:15 joerg Exp $");
+__KERNEL_RCSID(0, "$NetBSD: kern_pnp.c,v 1.1.2.16 2007/12/01 04:10:03 jmcneill Exp $");
 
 #include <sys/types.h>
 #include <sys/param.h>
@@ -44,7 +44,9 @@ __KERNEL_RCSID(0, "$NetBSD: kern_pnp.c,v 1.1.2.15 2007/11/23 16:03:15 joerg Exp 
 #include <sys/device.h>
 #include <sys/pnp.h>
 #include <sys/queue.h>
+#include <sys/kmem.h>
 #include <sys/syscallargs.h> /* for sys_sync */
+#include <sys/workqueue.h>
 #include <prop/proplib.h>
 
 #ifdef PNP_DEBUG
@@ -68,6 +70,46 @@ int pnp_debug_transition;
 MALLOC_DEFINE(M_PNP, "pnp", "device pnp messaging memory");
 
 static prop_dictionary_t pnp_platform = NULL;
+static struct workqueue *pnp_event_workqueue;
+
+typedef struct pnp_event_handler {
+	TAILQ_ENTRY(pnp_event_handler) pnp_link;
+	pnp_generic_event_t pnp_event;
+	void (*pnp_handler)(device_t);
+	device_t pnp_device;
+	bool pnp_global;
+} pnp_event_handler_t;
+
+static TAILQ_HEAD(, pnp_event_handler) pnp_all_events =
+    TAILQ_HEAD_INITIALIZER(pnp_all_events);
+
+typedef struct pnp_event_workitem {
+	struct work		pew_work;
+	pnp_generic_event_t	pew_event;
+	device_t		pew_device;
+} pnp_event_workitem_t;
+
+static void
+pnp_event_worker(struct work *wk, void *dummy)
+{
+	pnp_event_workitem_t *pew;
+	pnp_event_handler_t *event;
+
+	pew = (void *)wk;
+	KASSERT(wk == &pew->pew_work);
+	KASSERT(pew != NULL);
+	
+	TAILQ_FOREACH(event, &pnp_all_events, pnp_link) {
+		if (event->pnp_event != pew->pew_event)
+			continue;
+		if (event->pnp_device != pew->pew_device || event->pnp_global)
+			(*event->pnp_handler)(event->pnp_device);
+	}
+
+	kmem_free(pew, sizeof(pnp_event_workitem_t));
+
+	return;
+}
 
 static bool
 pnp_check_system_drivers(void)
@@ -388,42 +430,33 @@ pnp_class_network_register(device_t dev, struct ifnet *ifp)
 	    pnp_class_network_resume, NULL);
 }
 
-struct pnp_event_handler {
-	TAILQ_ENTRY(pnp_event_handler) pnp_link;
-	pnp_generic_event_t pnp_event;
-	void (*pnp_handler)(device_t);
-	device_t pnp_device;
-	bool pnp_global;
-};
-
-static TAILQ_HEAD(, pnp_event_handler) pnp_all_events =
-    TAILQ_HEAD_INITIALIZER(pnp_all_events);
-
 bool
 pnp_event_inject(device_t dv, pnp_generic_event_t ev)
 {
-	struct pnp_event_handler *event;
-	bool rv = false;
+	pnp_event_workitem_t *pew;
 
-	PNP_EVENT_PRINTF(("%s: PNP event %d injected\n", device_xname(dv),
-	    ev));
-
-	TAILQ_FOREACH(event, &pnp_all_events, pnp_link) {
-		if (event->pnp_event != ev)
-			continue;
-		if (event->pnp_device != dv || event->pnp_global) {
-			(*event->pnp_handler)(dv);
-			rv = true;
-		}
+	pew = kmem_alloc(sizeof(pnp_event_workitem_t), KM_NOSLEEP);
+	if (pew == NULL) {
+		PNP_EVENT_PRINTF(("%s: PNP event %d dropped (no memory)\n",
+		    dv ? device_xname(dv) : "<anonymous>", ev));
+		return false;
 	}
-	return rv;
+
+	pew->pew_event = ev;
+	pew->pew_device = dv;
+
+	workqueue_enqueue(pnp_event_workqueue, (void *)pew, NULL);
+	PNP_EVENT_PRINTF(("%s: PNP event %d injected\n",
+	    dv ? device_xname(dv) : "<anonymous>", ev));
+
+	return true;
 }
 
 bool
 pnp_event_register(device_t dv, pnp_generic_event_t ev,
     void (*handler)(device_t), bool global)
 {
-	struct pnp_event_handler *event; 
+	pnp_event_handler_t *event; 
 	
 	event = malloc(sizeof(*event), M_DEVBUF, M_WAITOK);
 	event->pnp_event = ev;
@@ -439,7 +472,7 @@ void
 pnp_event_deregister(device_t dv, pnp_generic_event_t ev,
     void (*handler)(device_t), bool global)
 {
-	struct pnp_event_handler *event;
+	pnp_event_handler_t *event;
 
 	TAILQ_FOREACH(event, &pnp_all_events, pnp_link) {
 		if (event->pnp_event != ev)
@@ -535,6 +568,16 @@ pnp_class_display_register(device_t dv)
 void
 pnp_init(void)
 {
+	int err;
+
+	KASSERT(pnp_event_workqueue == NULL);
+	err = workqueue_create(&pnp_event_workqueue, "pnpevent",
+	    pnp_event_worker, NULL, PRI_IDLE, IPL_VM, 0);
+	if (err)
+		panic("couldn't create pnpevent workqueue");
+
 	callout_init(&global_idle_counter, 0);
 	callout_setfunc(&global_idle_counter, input_idle, NULL);
+
+	return;
 }
