e6eb4721005007da4b10e78e0735da8993929e0d
hannken <hannken@NetBSD.org>
2007-12-02 18:24:34 +0000

    Make it compile again.

--- sys/rump/librump/rumpkern/fstrans_stub.c
+++ sys/rump/librump/rumpkern/fstrans_stub.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: fstrans_stub.c,v 1.2 2007/10/07 13:39:04 hannken Exp $	*/
+/*	$NetBSD: fstrans_stub.c,v 1.3 2007/12/02 18:24:34 hannken Exp $	*/
 
 /*
  * Copyright (c) 2007 Antti Kantee.  All Rights Reserved.
@@ -66,14 +66,7 @@ fstrans_is_owner(struct mount *mp)
 }
 
 int
-fscow_establish(struct mount *mp, int (*func)(void *, struct buf *), void *arg)
-{
-
-	return 0;
-}
-
-int
-fscow_disestablish(struct mount *mp, int (*func)(void *, struct buf *),
+fscow_establish(struct mount *mp, int (*func)(void *, struct buf *, bool),
     void *arg)
 {
 
@@ -81,7 +74,15 @@ fscow_disestablish(struct mount *mp, int (*func)(void *, struct buf *),
 }
 
 int
-fscow_run(struct buf *bp)
+fscow_disestablish(struct mount *mp, int (*func)(void *, struct buf *, bool),
+    void *arg)
+{
+
+	return 0;
+}
+
+int
+fscow_run(struct buf *bp, bool data_valid)
 {
 
 	return 0;
