7501fedd402d9335884385e155b2e84d6d55b378
lukem <lukem@NetBSD.org>
2007-12-11 12:09:21 +0000

    use __KERNEL_RCSID()

--- sys/dev/i2c/m41st84.c
+++ sys/dev/i2c/m41st84.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: m41st84.c,v 1.9 2007/01/12 19:33:21 cube Exp $	*/
+/*	$NetBSD: m41st84.c,v 1.10 2007/12/11 12:09:22 lukem Exp $	*/
 
 /*
  * Copyright (c) 2003 Wasabi Systems, Inc.
@@ -34,6 +34,9 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
+
+#include <sys/cdefs.h>
+__KERNEL_RCSID(0, "$NetBSD: m41st84.c,v 1.10 2007/12/11 12:09:22 lukem Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
