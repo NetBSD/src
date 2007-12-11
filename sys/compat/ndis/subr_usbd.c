e26f271ab071022fcfc76fa90a23e8b9c2a46221
lukem <lukem>
2007-12-11 23:32:44 +0000

    fix the __KERNEL_RCSID

--- sys/compat/ndis/subr_usbd.c
+++ sys/compat/ndis/subr_usbd.c
@@ -35,7 +35,7 @@
 __FBSDID("$FreeBSD: src/sys/compat/ndis/subr_usbd.c,v 1.1.2.1 2005/03/31 04:24:36 wpaul Exp $");
 #endif
 #ifdef __NetBSD__
-__KERNEL_RCSID(0, "$NetBSD");
+__KERNEL_RCSID(0, "$NetBSD: subr_usbd.c,v 1.5 2007/12/11 23:32:44 lukem Exp $");
 #endif
 
 #include <sys/param.h>
