88b3c3bba07d9cf35dbdcb5f9c153d6ab4ee272d
he <he@NetBSD.org>
2007-12-12 18:36:10 +0000

    Fix a use of lfs_truncate() inside an #ifdef notyet (so no resulting change); lfs_truncate() has lost its lwp argument.

--- sys/ufs/lfs/lfs_alloc.c
+++ sys/ufs/lfs/lfs_alloc.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: lfs_alloc.c,v 1.103 2007/10/10 20:42:34 ad Exp $	*/
+/*	$NetBSD: lfs_alloc.c,v 1.104 2007/12/12 18:36:10 he Exp $	*/
 
 /*-
  * Copyright (c) 1999, 2000, 2001, 2002, 2003, 2007 The NetBSD Foundation, Inc.
@@ -67,7 +67,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: lfs_alloc.c,v 1.103 2007/10/10 20:42:34 ad Exp $");
+__KERNEL_RCSID(0, "$NetBSD: lfs_alloc.c,v 1.104 2007/12/12 18:36:10 he Exp $");
 
 #if defined(_KERNEL_OPT)
 #include "opt_quota.h"
@@ -633,7 +633,7 @@ lfs_order_freelist(struct lfs *fs)
 		/* Address orphaned files */
 		if (ifp->if_nextfree == LFS_ORPHAN_NEXTFREE &&
 		    VFS_VGET(fs->lfs_ivnode->v_mount, ino, &vp) == 0) {
-			lfs_truncate(vp, 0, 0, NOCRED, curlwp);
+			lfs_truncate(vp, 0, 0, NOCRED);
 			vput(vp);
 			LFS_SEGENTRY(sup, fs, dtosn(fs, ifp->if_daddr), bp);
 			KASSERT(sup->su_nbytes >= DINODE1_SIZE);
