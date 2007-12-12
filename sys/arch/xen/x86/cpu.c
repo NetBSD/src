288f1dc922287262f7cab9ac730dcc021d5b724a
bouyer <bouyer@NetBSD.org>
2007-12-12 19:25:38 +0000

    Initialize ci_idepth in cpu_info_primary, makes LOCKDEBUG kernels boot.

--- sys/arch/xen/x86/cpu.c
+++ sys/arch/xen/x86/cpu.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: cpu.c,v 1.3 2007/12/10 13:39:06 bouyer Exp $	*/
+/*	$NetBSD: cpu.c,v 1.4 2007/12/12 19:25:38 bouyer Exp $	*/
 /* NetBSD: cpu.c,v 1.18 2004/02/20 17:35:01 yamt Exp  */
 
 /*-
@@ -72,7 +72,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: cpu.c,v 1.3 2007/12/10 13:39:06 bouyer Exp $");
+__KERNEL_RCSID(0, "$NetBSD: cpu.c,v 1.4 2007/12/12 19:25:38 bouyer Exp $");
 
 #include "opt_ddb.h"
 #include "opt_multiprocessor.h"
@@ -161,6 +161,7 @@ struct cpu_info cpu_info_primary = {
 #ifndef __x86_64__
 	.ci_self150 = (uint8_t *)&cpu_info_primary + 0x150,
 #endif
+	.ci_idepth = -1,
 	.ci_curlwp = &lwp0,
 #ifdef TRAPLOG
 	.ci_tlog = &tlog_primary,
