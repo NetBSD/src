7b13e58aa67dca37c6d0f6616bdc09df18fd39d7
xtraeme <xtraeme@NetBSD.org>
2007-12-12 14:36:21 +0000

    Check for the first 'D' letter in the cpu_model string for POWERNOW_K8, so that it works in Dual Core Opterons (and maybe others).
    Patch from Akihiko Taniguchi in PR port-amd64/37526.

--- sys/arch/amd64/amd64/identcpu.c
+++ sys/arch/amd64/amd64/identcpu.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: identcpu.c,v 1.31 2007/11/22 16:16:42 bouyer Exp $	*/
+/*	$NetBSD: identcpu.c,v 1.32 2007/12/12 14:36:21 xtraeme Exp $	*/
 
 /*
  * Copyright (c) 2003 Wasabi Systems, Inc.
@@ -36,7 +36,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: identcpu.c,v 1.31 2007/11/22 16:16:42 bouyer Exp $");
+__KERNEL_RCSID(0, "$NetBSD: identcpu.c,v 1.32 2007/12/12 14:36:21 xtraeme Exp $");
 
 #include "opt_enhanced_speedstep.h"
 #include "opt_intel_coretemp.h"
@@ -153,8 +153,9 @@ identifycpu(struct cpu_info *ci)
 
 #ifdef POWERNOW_K8
 	if (CPUID2FAMILY(ci->ci_signature) == 15 &&
-	    (cpu_model[0] == 'A' || cpu_model[0] == 'O') &&
-	    powernow_probe(ci))
+	    (cpu_model[0] == 'A' || cpu_model[0] == 'O' ||
+	     cpu_model[0] == 'D') &&
+	     powernow_probe(ci))
 		k8_powernow_init();
 #endif
 
