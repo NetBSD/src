261292569c738ea109cd95bf187fb2d5767c6ca6
joerg <joerg@NetBSD.org>
2007-12-15 09:15:37 +0000

    Steal some rope from innocent users and disallow ACPI suspend if more than one CPU was found.

--- sys/arch/i386/acpi/acpi_wakeup.c
+++ sys/arch/i386/acpi/acpi_wakeup.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: acpi_wakeup.c,v 1.42 2007/12/10 03:12:46 dogcow Exp $	*/
+/*	$NetBSD: acpi_wakeup.c,v 1.43 2007/12/15 09:15:37 joerg Exp $	*/
 
 /*-
  * Copyright (c) 2002 The NetBSD Foundation, Inc.
@@ -37,7 +37,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: acpi_wakeup.c,v 1.42 2007/12/10 03:12:46 dogcow Exp $");
+__KERNEL_RCSID(0, "$NetBSD: acpi_wakeup.c,v 1.43 2007/12/15 09:15:37 joerg Exp $");
 
 /*-
  * Copyright (c) 2001 Takanori Watanabe <takawata@jp.freebsd.org>
@@ -257,6 +257,11 @@ acpi_md_sleep(int state)
 
 	KASSERT(acpi_wakeup_paddr != 0);
 	KASSERT(sizeof(wakecode) <= PAGE_SIZE);
+
+	if (ncpu > 1) {
+		printf("acpi: sleep is not supported on MP.\n");
+		return -1;
+	}
 
 	if (acpi_wakeup_vaddr == 0) {
 		/* Map ACPI wakecode */
