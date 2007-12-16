51507f01694f8320d2b5505ead580db89a7a2a91
mrg <mrg@NetBSD.org>
2007-12-16 07:31:47 +0000

    move the arm32 cpu_intr_p() function into arm32_machdep.c from arm_machdep.c.  this lets acorn26 link again.

--- sys/arch/arm/arm/arm_machdep.c
+++ sys/arch/arm/arm/arm_machdep.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: arm_machdep.c,v 1.16 2007/12/11 17:02:32 ad Exp $	*/
+/*	$NetBSD: arm_machdep.c,v 1.17 2007/12/16 07:31:47 mrg Exp $	*/
 
 /*
  * Copyright (c) 2001 Wasabi Systems, Inc.
@@ -77,7 +77,7 @@
 
 #include <sys/param.h>
 
-__KERNEL_RCSID(0, "$NetBSD: arm_machdep.c,v 1.16 2007/12/11 17:02:32 ad Exp $");
+__KERNEL_RCSID(0, "$NetBSD: arm_machdep.c,v 1.17 2007/12/16 07:31:47 mrg Exp $");
 
 #include <sys/exec.h>
 #include <sys/proc.h>
@@ -179,11 +179,4 @@ startlwp(void *arg)
 	pool_put(&lwp_uc_pool, uc);
 
 	userret(l);
-}
-
-bool
-cpu_intr_p(void)
-{
-
-	return curcpu()->ci_idepth != 0;
 }
