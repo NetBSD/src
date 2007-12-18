8653b2a78fa21b8aef2af2eb8eed7ef917dfb0a6
garbled <garbled>
2007-12-18 18:26:36 +0000

    First crack at a new unified mkbootimage for powerpc.  Still some work left to do here, so the build isn't enabled yet.

--- sys/arch/powerpc/stand/mkbootimage/prep_magic.h
+++ sys/arch/powerpc/stand/mkbootimage/prep_magic.h
@@ -1,7 +1,7 @@
-/*	$NetBSD: prep_magic.h,v 1.1 2007/12/18 18:19:07 garbled Exp $	*/
+/*	$NetBSD: prep_magic.h,v 1.2 2007/12/18 18:26:36 garbled Exp $	*/
 
-char magic[] = "KMA";
+char prep_magic[] = "KMA";
 int kern_len;
-#define	MAGICSIZE	sizeof (magic)
+#define	PREP_MAGICSIZE	sizeof (prep_magic)
 #define	KERNLENSIZE	sizeof (kern_len)
 #define	ENTRY		0x100000
