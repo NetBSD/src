c04e60f3b9161d032007240e5853e027e5b8476d
christos <christos>
2007-12-18 02:35:25 +0000

    merge conflicts

--- crypto/dist/ssh/monitor_wrap.c
+++ crypto/dist/ssh/monitor_wrap.c
@@ -1,5 +1,5 @@
-/*	$NetBSD: monitor_wrap.c,v 1.18 2007/03/10 23:05:25 christos Exp $	*/
-/* $OpenBSD: monitor_wrap.c,v 1.55 2007/02/19 10:45:58 dtucker Exp $ */
+/*	$NetBSD: monitor_wrap.c,v 1.19 2007/12/18 02:35:28 christos Exp $	*/
+/* $OpenBSD: monitor_wrap.c,v 1.57 2007/06/07 19:37:34 pvalchev Exp $ */
 /*
  * Copyright 2002 Niels Provos <provos@citi.umich.edu>
  * Copyright 2002 Markus Friedl <markus@openbsd.org>
@@ -27,7 +27,7 @@
  */
 
 #include "includes.h"
-__RCSID("$NetBSD: monitor_wrap.c,v 1.18 2007/03/10 23:05:25 christos Exp $");
+__RCSID("$NetBSD: monitor_wrap.c,v 1.19 2007/12/18 02:35:28 christos Exp $");
 #include <sys/types.h>
 #include <sys/uio.h>
 
@@ -471,8 +471,8 @@ mm_newkeys_from_blob(u_char *blob, int blen)
 
 	/* Mac structure */
 	mac->name = buffer_get_string(&b, NULL);
-	if (mac->name == NULL || mac_init(mac, mac->name) == -1)
-		fatal("%s: can not init mac %s", __func__, mac->name);
+	if (mac->name == NULL || mac_setup(mac, mac->name) == -1)
+		fatal("%s: can not setup mac %s", __func__, mac->name);
 	mac->enabled = buffer_get_int(&b);
 	mac->key = buffer_get_string(&b, &len);
 	if (len > mac->key_len)
