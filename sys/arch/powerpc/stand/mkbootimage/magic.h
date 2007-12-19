4fcef35aaced618c2e5b6c62f7c967edbec30136
garbled <garbled@NetBSD.org>
2007-12-19 19:46:31 +0000

    Add rs6000 stuff in here now.

--- sys/arch/powerpc/stand/mkbootimage/magic.h
+++ sys/arch/powerpc/stand/mkbootimage/magic.h
@@ -1,7 +1,9 @@
-/*	$NetBSD: magic.h,v 1.1 2007/12/19 19:45:32 garbled Exp $	*/
+/*	$NetBSD: magic.h,v 1.2 2007/12/19 19:46:31 garbled Exp $	*/
 
 char prep_magic[] = "KMA";
+char rs6000_magic[] = "KMZ";
 int kern_len;
-#define	PREP_MAGICSIZE	sizeof (prep_magic)
-#define	KERNLENSIZE	sizeof (kern_len)
-#define	ENTRY		0x100000
+#define	PREP_MAGICSIZE		sizeof (prep_magic)
+#define RS6000_MAGICSIZE	sizeof (rs6000_magic)
+#define	KERNLENSIZE		sizeof (kern_len)
+#define	ENTRY			0x100000
