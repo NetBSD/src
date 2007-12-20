4cefd377d148d6a0445a5a2dd310c82fa0da3607
garbled <garbled@NetBSD.org>
2007-12-20 22:59:59 +0000

    Add bebox mkbootimage stuff to the unified mkbootimage.  It should work in theory, assuming:
    a) I didn't mess up
    b) The original code in bebox/stand actually works

--- sys/arch/powerpc/stand/mkbootimage/magic.h
+++ sys/arch/powerpc/stand/mkbootimage/magic.h
@@ -1,8 +1,11 @@
-/*	$NetBSD: magic.h,v 1.2 2007/12/19 19:46:31 garbled Exp $	*/
+/*	$NetBSD: magic.h,v 1.3 2007/12/20 23:00:00 garbled Exp $	*/
 
+char bebox_magic[] = "BSD";
 char prep_magic[] = "KMA";
 char rs6000_magic[] = "KMZ";
 int kern_len;
+
+#define BEBOX_MAGICSIZE		sizeof (bebox_magic)
 #define	PREP_MAGICSIZE		sizeof (prep_magic)
 #define RS6000_MAGICSIZE	sizeof (rs6000_magic)
 #define	KERNLENSIZE		sizeof (kern_len)
