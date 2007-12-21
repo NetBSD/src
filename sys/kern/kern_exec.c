5568796ceda9ce2be6b49461bba5c115c95776ca
ad <ad@NetBSD.org>
2007-12-21 12:53:39 +0000

    Use a mutex to serialize sigobject init.

--- sys/kern/kern_exec.c
+++ sys/kern/kern_exec.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: kern_exec.c,v 1.256.2.1 2007/12/15 03:16:55 ad Exp $	*/
+/*	$NetBSD: kern_exec.c,v 1.256.2.2 2007/12/21 12:53:39 ad Exp $	*/
 
 /*-
  * Copyright (C) 1993, 1994, 1996 Christopher G. Demetriou
@@ -33,7 +33,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: kern_exec.c,v 1.256.2.1 2007/12/15 03:16:55 ad Exp $");
+__KERNEL_RCSID(0, "$NetBSD: kern_exec.c,v 1.256.2.2 2007/12/21 12:53:39 ad Exp $");
 
 #include "opt_ktrace.h"
 #include "opt_syscall_debug.h"
@@ -206,6 +206,8 @@ krwlock_t exec_lock;
 
 static void link_es(struct execsw_entry **, const struct execsw *);
 #endif /* LKM */
+
+static kmutex_t sigobject_lock;
 
 /*
  * check exec:
@@ -1394,6 +1396,7 @@ exec_init(int init_boot)
 	if (init_boot) {
 		/* do one-time initializations */
 		rw_init(&exec_lock);
+		mutex_init(&sigobject_lock, MUTEX_DEFAULT, IPL_NONE);
 
 		/* register compiled-in emulations */
 		for(i=0; i < nexecs_builtin; i++) {
@@ -1519,23 +1522,28 @@ exec_sigcode_map(struct proc *p, const struct emul *e)
 
 	uobj = *e->e_sigobject;
 	if (uobj == NULL) {
-		uobj = uao_create(sz, 0);
-		(*uobj->pgops->pgo_reference)(uobj);
-		va = vm_map_min(kernel_map);
-		if ((error = uvm_map(kernel_map, &va, round_page(sz),
-		    uobj, 0, 0,
-		    UVM_MAPFLAG(UVM_PROT_RW, UVM_PROT_RW,
-		    UVM_INH_SHARE, UVM_ADV_RANDOM, 0)))) {
-			printf("kernel mapping failed %d\n", error);
-			(*uobj->pgops->pgo_detach)(uobj);
-			return (error);
-		}
-		memcpy((void *)va, e->e_sigcode, sz);
+		mutex_enter(&sigobject_lock);
+		if ((uobj = *e->e_sigobject) == NULL) {
+			uobj = uao_create(sz, 0);
+			(*uobj->pgops->pgo_reference)(uobj);
+			va = vm_map_min(kernel_map);
+			if ((error = uvm_map(kernel_map, &va, round_page(sz),
+			    uobj, 0, 0,
+			    UVM_MAPFLAG(UVM_PROT_RW, UVM_PROT_RW,
+			    UVM_INH_SHARE, UVM_ADV_RANDOM, 0)))) {
+				printf("kernel mapping failed %d\n", error);
+				(*uobj->pgops->pgo_detach)(uobj);
+				mutex_exit(&sigobject_lock);
+				return (error);
+			}
+			memcpy((void *)va, e->e_sigcode, sz);
 #ifdef PMAP_NEED_PROCWR
-		pmap_procwr(&proc0, va, sz);
+			pmap_procwr(&proc0, va, sz);
 #endif
-		uvm_unmap(kernel_map, va, va + round_page(sz));
-		*e->e_sigobject = uobj;
+			uvm_unmap(kernel_map, va, va + round_page(sz));
+			*e->e_sigobject = uobj;
+		}
+		mutex_exit(&sigobject_lock);
 	}
 
 	/* Just a hint to uvm_map where to put it. */
