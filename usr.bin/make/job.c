58c32a263309dc9c39d411d9836198e0bd544743
dsl <dsl@NetBSD.org>
2007-12-21 20:32:24 +0000

    If make can't find a rule to create a file it needs to satisfy a rule that came from .depend then just output a message and contine.

--- usr.bin/make/job.c
+++ usr.bin/make/job.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: job.c,v 1.133 2007/10/14 20:22:53 apb Exp $	*/
+/*	$NetBSD: job.c,v 1.134 2007/12/21 20:32:24 dsl Exp $	*/
 
 /*
  * Copyright (c) 1988, 1989, 1990 The Regents of the University of California.
@@ -70,14 +70,14 @@
  */
 
 #ifndef MAKE_NATIVE
-static char rcsid[] = "$NetBSD: job.c,v 1.133 2007/10/14 20:22:53 apb Exp $";
+static char rcsid[] = "$NetBSD: job.c,v 1.134 2007/12/21 20:32:24 dsl Exp $";
 #else
 #include <sys/cdefs.h>
 #ifndef lint
 #if 0
 static char sccsid[] = "@(#)job.c	8.2 (Berkeley) 3/19/94";
 #else
-__RCSID("$NetBSD: job.c,v 1.133 2007/10/14 20:22:53 apb Exp $");
+__RCSID("$NetBSD: job.c,v 1.134 2007/12/21 20:32:24 dsl Exp $");
 #endif
 #endif /* not lint */
 #endif
@@ -1221,6 +1221,12 @@ Job_CheckCommands(GNode *gn, void (*abortProc)(const char *, ...))
 	     * this node's parents so they never get examined.
 	     */
 	    static const char msg[] = ": don't know how to make";
+
+	    if (gn->flags & FROM_DEPEND) {
+		fprintf(stdout, "%s: ignoring stale .depend for %s\n",
+			progname, gn->name);
+		return TRUE;
+	    }
 
 	    if (gn->type & OP_OPTIONAL) {
 		(void)fprintf(stdout, "%s%s %s(ignored)\n", progname,
--- usr.bin/make/targ.c
+++ usr.bin/make/targ.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: targ.c,v 1.49 2006/12/21 20:05:37 dsl Exp $	*/
+/*	$NetBSD: targ.c,v 1.50 2007/12/21 20:32:24 dsl Exp $	*/
 
 /*
  * Copyright (c) 1988, 1989, 1990, 1993
@@ -69,14 +69,14 @@
  */
 
 #ifndef MAKE_NATIVE
-static char rcsid[] = "$NetBSD: targ.c,v 1.49 2006/12/21 20:05:37 dsl Exp $";
+static char rcsid[] = "$NetBSD: targ.c,v 1.50 2007/12/21 20:32:24 dsl Exp $";
 #else
 #include <sys/cdefs.h>
 #ifndef lint
 #if 0
 static char sccsid[] = "@(#)targ.c	8.2 (Berkeley) 3/19/94";
 #else
-__RCSID("$NetBSD: targ.c,v 1.49 2006/12/21 20:05:37 dsl Exp $");
+__RCSID("$NetBSD: targ.c,v 1.50 2007/12/21 20:32:24 dsl Exp $");
 #endif
 #endif /* not lint */
 #endif
@@ -355,6 +355,8 @@ Targ_FindNode(const char *name, int flags)
 	Hash_SetValue(he, gn);
     Var_Append(".ALLTARGETS", name, VAR_GLOBAL);
     (void)Lst_AtEnd(allTargets, gn);
+    if (doing_depend)
+	gn->flags |= FROM_DEPEND;
     return gn;
 }
 
