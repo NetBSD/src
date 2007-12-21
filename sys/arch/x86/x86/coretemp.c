905f8c0f22daa87afd44c26cd8227fe12c8d7763
xtraeme <xtraeme@NetBSD.org>
2007-12-21 21:36:22 +0000

    After comments from joerg@, backout previous and use 'cpuN'.

--- sys/arch/x86/x86/coretemp.c
+++ sys/arch/x86/x86/coretemp.c
@@ -1,4 +1,4 @@
-/* $NetBSD: coretemp.c,v 1.4 2007/12/21 01:10:54 xtraeme Exp $ */
+/* $NetBSD: coretemp.c,v 1.5 2007/12/21 21:36:22 xtraeme Exp $ */
 
 /*-
  * Copyright (c) 2007 Juan Romero Pardines.
@@ -36,7 +36,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: coretemp.c,v 1.4 2007/12/21 01:10:54 xtraeme Exp $");
+__KERNEL_RCSID(0, "$NetBSD: coretemp.c,v 1.5 2007/12/21 21:36:22 xtraeme Exp $");
 
 #include <sys/param.h>
 #include <sys/kmem.h>
@@ -127,7 +127,7 @@ coretemp_register(struct cpu_info *ci)
 	sc->sc_sensor.flags = ENVSYS_FMONCRITICAL;
 	sc->sc_sensor.monitor = true;
 	(void)snprintf(sc->sc_sensor.desc, sizeof(sc->sc_sensor.desc),
-	    "core%d temperature", (int)ci->ci_cpuid);
+	    "cpu%d temperature", (int)ci->ci_cpuid);
 
 	sc->sc_sme = sysmon_envsys_create();
 	if (sysmon_envsys_sensor_attach(sc->sc_sme, &sc->sc_sensor)) {
