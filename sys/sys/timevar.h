de62a7cf85e7d0af962409ebbcf04ab37dae31f5
yamt <yamt@NetBSD.org>
2007-12-22 00:34:37 +0000

    provide binuptime etc for !__HAVE_TIMECOUNTER.

--- sys/sys/timevar.h
+++ sys/sys/timevar.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: timevar.h,v 1.18 2007/11/25 00:35:28 elad Exp $	*/
+/*	$NetBSD: timevar.h,v 1.19 2007/12/22 00:34:37 yamt Exp $	*/
 
 /*
  *  Copyright (c) 2005 The NetBSD Foundation.
@@ -129,7 +129,6 @@ struct	ptimers {
  * 
  */
 
-#ifdef __HAVE_TIMECOUNTER
 void	binuptime(struct bintime *);
 void	nanouptime(struct timespec *);
 void	microuptime(struct timeval *);
@@ -145,19 +144,6 @@ void	getmicrouptime(struct timeval *);
 void	getbintime(struct bintime *);
 void	getnanotime(struct timespec *);
 void	getmicrotime(struct timeval *);
-#else /* !__HAVE_TIMECOUNTER */
-/* timecounter compat functions */
-void	microtime(struct timeval *);
-void	nanotime(struct timespec *);
-
-void	nanouptime(struct timespec *);
-void	getbinuptime(struct bintime *);
-void	getnanouptime(struct timespec *);
-void	getmicrouptime(struct timeval *);
-
-void	getnanotime(struct timespec *);
-void	getmicrotime(struct timeval *);
-#endif /* !__HAVE_TIMECOUNTER */
 
 /* Other functions */
 int	adjtime1(const struct timeval *, struct timeval *, struct proc *);
