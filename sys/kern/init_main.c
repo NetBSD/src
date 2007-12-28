669683fb67ec9cf79950d2720c3e9f1a058661c3
ad <ad@NetBSD.org>
2007-12-28 15:04:24 +0000

    Fix (harmless) merge error.

--- sys/kern/init_main.c
+++ sys/kern/init_main.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: init_main.c,v 1.330.2.4 2007/12/26 21:39:36 ad Exp $	*/
+/*	$NetBSD: init_main.c,v 1.330.2.5 2007/12/28 15:04:24 ad Exp $	*/
 
 /*
  * Copyright (c) 1982, 1986, 1989, 1991, 1992, 1993
@@ -71,7 +71,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: init_main.c,v 1.330.2.4 2007/12/26 21:39:36 ad Exp $");
+__KERNEL_RCSID(0, "$NetBSD: init_main.c,v 1.330.2.5 2007/12/28 15:04:24 ad Exp $");
 
 #include "opt_ipsec.h"
 #include "opt_ntp.h"
@@ -334,9 +334,6 @@ main(void)
 
 	/* Initialize signal-related data structures. */
 	signal_init();
-
-	/* Initialize resource management. */
-	resource_init();
 
 	/* Initialize resource management. */
 	resource_init();
