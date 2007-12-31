7d61ffaff939bb82a37179ef78cc41285f9864cc
dyoung <dyoung@NetBSD.org>
2007-12-31 22:49:39 +0000

    struct ethercom changed.  Welcome to 4.99.47.

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.296 2007/12/29 08:16:46 agc Exp $	*/
+/*	$NetBSD: param.h,v 1.297 2007/12/31 22:49:39 dyoung Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -63,7 +63,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	499004600	/* NetBSD 4.99.46 */
+#define	__NetBSD_Version__	499004700	/* NetBSD 4.99.47 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
