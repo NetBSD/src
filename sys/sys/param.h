38f488e453ccf6fcf6b52e65e2828d8e6bd00339
ad <ad@NetBSD.org>
2008-01-02 11:51:59 +0000

    4.99.48 - vmlocking2 merged

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.297 2007/12/31 22:49:39 dyoung Exp $	*/
+/*	$NetBSD: param.h,v 1.298 2008/01/02 11:51:59 ad Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -63,7 +63,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	499004700	/* NetBSD 4.99.47 */
+#define	__NetBSD_Version__	499004800	/* NetBSD 4.99.48 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
