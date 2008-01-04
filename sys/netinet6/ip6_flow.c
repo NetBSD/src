e2828f132253e66de33a5bf99c488ad7fcf01bdf
dyoung <dyoung>
2008-01-04 23:26:44 +0000

    Replace rtcache_down() with rtcache_validate() and update rtcache_down() uses.

--- sys/netinet6/ip6_flow.c
+++ sys/netinet6/ip6_flow.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: ip6_flow.c,v 1.11 2007/12/20 19:53:33 dyoung Exp $	*/
+/*	$NetBSD: ip6_flow.c,v 1.12 2008/01/04 23:26:44 dyoung Exp $	*/
 
 /*-
  * Copyright (c) 2007 The NetBSD Foundation, Inc.
@@ -45,7 +45,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: ip6_flow.c,v 1.11 2007/12/20 19:53:33 dyoung Exp $");
+__KERNEL_RCSID(0, "$NetBSD: ip6_flow.c,v 1.12 2008/01/04 23:26:44 dyoung Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -269,8 +269,7 @@ ip6flow_fastforward(struct mbuf *m)
 	/*
 	 * Route and interface still up?
 	 */
-	if (rtcache_down(&ip6f->ip6f_ro) ||
-	    (rt = rtcache_getrt(&ip6f->ip6f_ro)) == NULL ||
+	if ((rt = rtcache_validate(&ip6f->ip6f_ro)) == NULL ||
 	    (rt->rt_ifp->if_flags & IFF_UP) == 0) {
 	    	/* Route or interface is down */
 		return 0;
@@ -317,8 +316,7 @@ ip6flow_addstats(struct ip6flow *ip6f)
 {
 	struct rtentry *rt;
 
-	if (!rtcache_down(&ip6f->ip6f_ro) &&
-	    (rt = rtcache_getrt(&ip6f->ip6f_ro)) != NULL)
+	if ((rt = rtcache_validate(&ip6f->ip6f_ro)) != NULL)
 		rt->rt_use += ip6f->ip6f_uses;
 	ip6stat.ip6s_fastforwardflows = ip6flow_inuse;
 	ip6stat.ip6s_cantforward += ip6f->ip6f_dropped;
@@ -367,8 +365,7 @@ ip6flow_reap(int just_one)
 			 * If this no longer points to a valid route -
 			 * reclaim it.
 			 */
-			if (rtcache_down(&ip6f->ip6f_ro) ||
-			    rtcache_getrt(&ip6f->ip6f_ro) == NULL)
+			if (rtcache_validate(&ip6f->ip6f_ro) == NULL)
 				goto done;
 			/*
 			 * choose the one that's been least recently
@@ -412,8 +409,7 @@ ip6flow_slowtimo(void)
 	for (ip6f = LIST_FIRST(&ip6flowlist); ip6f != NULL; ip6f = next_ip6f) {
 		next_ip6f = LIST_NEXT(ip6f, ip6f_list);
 		if (PRT_SLOW_ISEXPIRED(ip6f->ip6f_timer) ||
-		    rtcache_down(&ip6f->ip6f_ro) ||
-		    rtcache_getrt(&ip6f->ip6f_ro) == NULL) {
+		    rtcache_validate(&ip6f->ip6f_ro) == NULL) {
 			ip6flow_free(ip6f);
 		} else {
 			ip6f->ip6f_last_uses = ip6f->ip6f_uses;
