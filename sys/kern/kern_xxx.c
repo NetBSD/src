c9848793d91edae9c0c8def5519149f054ea6cd7
dsl <dsl@NetBSD.org>
2008-01-05 12:30:47 +0000

    Don't pass l (== curlwp) into the system call trace code. Delete inclusion of opt_syscall_debug.h into systm.h, every kernel object file doesn't need to depend on opt_syscall_debug.h!

--- sys/kern/kern_xxx.c
+++ sys/kern/kern_xxx.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: kern_xxx.c,v 1.67 2007/12/20 23:03:10 dsl Exp $	*/
+/*	$NetBSD: kern_xxx.c,v 1.68 2008/01/05 12:30:47 dsl Exp $	*/
 
 /*
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -32,7 +32,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: kern_xxx.c,v 1.67 2007/12/20 23:03:10 dsl Exp $");
+__KERNEL_RCSID(0, "$NetBSD: kern_xxx.c,v 1.68 2008/01/05 12:30:47 dsl Exp $");
 
 #include "opt_syscall_debug.h"
 
@@ -108,8 +108,9 @@ int	scdebug = SCDEBUG_CALLS|SCDEBUG_RETURNS|SCDEBUG_SHOWARGS|SCDEBUG_ALL;
 #endif
 
 void
-scdebug_call(struct lwp *l, register_t code, register_t args[])
+scdebug_call(register_t code, const register_t args[])
 {
+	struct lwp *l = curlwp;
 	struct proc *p = l->l_proc;
 	const struct sysent *sy;
 	const struct emul *em;
@@ -150,8 +151,9 @@ scdebug_call(struct lwp *l, register_t code, register_t args[])
 }
 
 void
-scdebug_ret(struct lwp *l, register_t code, int error, register_t retval[])
+scdebug_ret(register_t code, int error, const register_t retval[])
 {
+	struct lwp *l = curlwp;
 	struct proc *p = l->l_proc;
 	const struct sysent *sy;
 	const struct emul *em;
