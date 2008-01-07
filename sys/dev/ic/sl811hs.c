8b7aa2e9c155ad30adb94f47bae0f1b22cf998d7
ad <ad@NetBSD.org>
2008-01-07 01:55:15 +0000

    Really, remove last vestiges of simplelock debugging.

--- sys/dev/ic/sl811hs.c
+++ sys/dev/ic/sl811hs.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: sl811hs.c,v 1.18 2008/01/07 00:27:35 ad Exp $	*/
+/*	$NetBSD: sl811hs.c,v 1.19 2008/01/07 01:55:15 ad Exp $	*/
 
 /*
  * Not (c) 2007 Matthew Orgass
@@ -86,7 +86,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: sl811hs.c,v 1.18 2008/01/07 00:27:35 ad Exp $");
+__KERNEL_RCSID(0, "$NetBSD: sl811hs.c,v 1.19 2008/01/07 01:55:15 ad Exp $");
 
 #include <sys/cdefs.h>
 #include <sys/param.h>
@@ -650,16 +650,8 @@ DDOLOGBUF(uint8_t *buf, unsigned int length)
 #define DLOGBUF(x, b, l) ((void)0)
 #endif /* SLHCI_DEBUG */
 
-#ifdef LOCKDEBUG
-#define SLHCI_MAINLOCKASSERT(sc) 		/* nothing */
-#define SLHCI_LOCKASSERT(sc, main, wait) do {				 \
-	simple_lock_assert_ ## main (&(sc)->sc_lock, "slhci");	    	 \
-	simple_lock_assert_ ## wait (&(sc)->sc_wait_lock, "slhci wait"); \
-} while (/*CONSTCOND*/0)
-#else
 #define SLHCI_MAINLOCKASSERT(sc) ((void)0)
 #define SLHCI_LOCKASSERT(sc, main, wait) ((void)0)
-#endif
 
 #ifdef DIAGNOSTIC
 #define LK_SLASSERT(exp, sc, spipe, xfer, ext) do {			\
