e3a0d0e9a595bb7b11799d2cd878d2c08ba58916
joerg <joerg@NetBSD.org>
2008-01-08 13:47:49 +0000

    Finish conversion of ppc and evbppc to timecounter. Tested by simonb@ on WALNUT.

--- sys/arch/powerpc/include/types.h
+++ sys/arch/powerpc/include/types.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: types.h,v 1.30 2007/11/29 00:58:26 ad Exp $	*/
+/*	$NetBSD: types.h,v 1.31 2008/01/08 13:47:49 joerg Exp $	*/
 
 /*-
  * Copyright (C) 1995 Wolfgang Solfrank.
@@ -72,5 +72,7 @@ typedef volatile int __cpu_simple_lock_t;
 #ifdef _LP64
 #define	__HAVE_ATOMIC64_OPS
 #endif
+
+#define	__HAVE_TIMECOUNTER
 
 #endif	/* _MACHTYPES_H_ */
