c8c0ba0464112760d7365e895fbffbbb3ef11816
dogcow <dogcow@NetBSD.org>
2008-01-08 14:28:35 +0000

    I'm not positive that the mips_ipl_si_to_sr assignments are correct - but at least the thing compiles now.

--- sys/arch/evbmips/malta/malta_intr.c
+++ sys/arch/evbmips/malta/malta_intr.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: malta_intr.c,v 1.15 2007/10/17 19:54:15 garbled Exp $	*/
+/*	$NetBSD: malta_intr.c,v 1.16 2008/01/08 14:28:35 dogcow Exp $	*/
 
 /*
  * Copyright 2001, 2002 Wasabi Systems, Inc.
@@ -40,7 +40,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: malta_intr.c,v 1.15 2007/10/17 19:54:15 garbled Exp $");
+__KERNEL_RCSID(0, "$NetBSD: malta_intr.c,v 1.16 2008/01/08 14:28:35 dogcow Exp $");
 
 #include <sys/param.h>
 #include <sys/device.h>
@@ -66,27 +66,10 @@ __KERNEL_RCSID(0, "$NetBSD: malta_intr.c,v 1.15 2007/10/17 19:54:15 garbled Exp 
 const uint32_t ipl_sr_bits[_IPL_N] = {
 	0,					/*  0: IPL_NONE */
 
-	MIPS_SOFT_INT_MASK_0,			/*  1: IPL_SOFT */
-
-	MIPS_SOFT_INT_MASK_0,			/*  2: IPL_SOFTCLOCK */
+	MIPS_SOFT_INT_MASK_0,			/*  1: IPL_VM */
 
 	MIPS_SOFT_INT_MASK_0|
-		MIPS_SOFT_INT_MASK_1,		/*  3: IPL_SOFTNET */
-
-	MIPS_SOFT_INT_MASK_0|
-		MIPS_SOFT_INT_MASK_1,		/*  4: IPL_SOFTSERIAL */
-
-	MIPS_SOFT_INT_MASK_0|
-		MIPS_SOFT_INT_MASK_1|
-		MIPS_INT_MASK_0,		/*  5: IPL_BIO */
-
-	MIPS_SOFT_INT_MASK_0|
-		MIPS_SOFT_INT_MASK_1|
-		MIPS_INT_MASK_0,		/*  6: IPL_NET */
-
-	MIPS_SOFT_INT_MASK_0|
-		MIPS_SOFT_INT_MASK_1|
-		MIPS_INT_MASK_0,		/*  7: IPL_{TTY,SERIAL} */
+		MIPS_SOFT_INT_MASK_1,		/*  3: IPL_SCHED */
 
 	MIPS_SOFT_INT_MASK_0|
 		MIPS_SOFT_INT_MASK_1|
@@ -95,7 +78,7 @@ const uint32_t ipl_sr_bits[_IPL_N] = {
 		MIPS_INT_MASK_2|
 		MIPS_INT_MASK_3|
 		MIPS_INT_MASK_4|
-		MIPS_INT_MASK_5,		/*  8: IPL_{CLOCK,HIGH} */
+		MIPS_INT_MASK_5,		/*  8: IPL_HIGH */
 };
 
 /*
@@ -103,11 +86,9 @@ const uint32_t ipl_sr_bits[_IPL_N] = {
  * given software interrupt priority level.
  * Hardware ipls are port/board specific.
  */
-const uint32_t mips_ipl_si_to_sr[SI_NQUEUES] = {
-	[SI_SOFT] = MIPS_SOFT_INT_MASK_0,
-	[SI_SOFTCLOCK] = MIPS_SOFT_INT_MASK_0,
-	[SI_SOFTNET] = MIPS_SOFT_INT_MASK_1,
-	[SI_SOFTSERIAL] = MIPS_SOFT_INT_MASK_1,
+const uint32_t mips_ipl_si_to_sr[2] = {
+	MIPS_SOFT_INT_MASK_0,
+	MIPS_SOFT_INT_MASK_1, /* XXX is this right with the new softints? */
 };
 
 struct malta_cpuintr {
