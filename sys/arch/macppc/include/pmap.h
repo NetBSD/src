cf3691bced77f4cbc2b585e4d0d699623b396282
garbled <garbled@NetBSD.org>
2008-01-09 21:23:18 +0000

    Mac doesn't need the special hack for the bridge mode prototype anymore.

--- sys/arch/macppc/include/pmap.h
+++ sys/arch/macppc/include/pmap.h
@@ -1,14 +1,7 @@
-/*	$NetBSD: pmap.h,v 1.8 2006/08/05 21:26:48 sanjayl Exp $	*/
+/*	$NetBSD: pmap.h,v 1.9 2008/01/09 21:23:18 garbled Exp $	*/
 #ifndef _MACPPC_PMAP_H_
 #define _MACPPC_PMAP_H_
 
 #include <powerpc/pmap.h>
-
-/* Mac specific routines */
-#if defined (PPC_OEA64_BRIDGE)
-#ifndef _LOCORE
-int pmap_setup_segment0_map(int use_large_pages, ...);
-#endif
-#endif
 
 #endif /* _MACPPC_PMAP_H_ */
