add6c7817658a4c4e81b15397b2cae63305df52e
wiz <wiz@NetBSD.org>
2008-01-09 20:38:34 +0000

    Fix typo in macro name and comments.

--- sys/arch/arc/arc/machdep.c
+++ sys/arch/arc/arc/machdep.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: machdep.c,v 1.109 2007/12/03 15:33:13 ad Exp $	*/
+/*	$NetBSD: machdep.c,v 1.110 2008/01/09 20:38:34 wiz Exp $	*/
 /*	$OpenBSD: machdep.c,v 1.36 1999/05/22 21:22:19 weingart Exp $	*/
 
 /*
@@ -78,7 +78,7 @@
 /* from: Utah Hdr: machdep.c 1.63 91/04/24 */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: machdep.c,v 1.109 2007/12/03 15:33:13 ad Exp $");
+__KERNEL_RCSID(0, "$NetBSD: machdep.c,v 1.110 2008/01/09 20:38:34 wiz Exp $");
 
 #include "fs_mfs.h"
 #include "opt_ddb.h"
@@ -402,7 +402,7 @@ mach_init(int argc, char *argv[], u_int bim, void *bip)
 		curcpu()->ci_cycles_per_hz /= 2;
 		curcpu()->ci_divisor_delay /= 2;
 	}
-	MIPS_SET_CI_RECIPRICAL(curcpu());
+	MIPS_SET_CI_RECIPROCAL(curcpu());
 	sprintf(cpu_model, "%s %s%s",
 	    platform->vendor, platform->model, platform->variant);
 
