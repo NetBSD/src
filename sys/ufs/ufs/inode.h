32353155a4048bbba424ac9057300e6eaaf40665
ad <ad@NetBSD.org>
2008-01-09 16:15:22 +0000

    Go back to freeing on disk inodes in the inactive routine. It would be better not to do this, but it rules out potential side effects with softdep.

--- sys/ufs/ufs/inode.h
+++ sys/ufs/ufs/inode.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: inode.h,v 1.50 2008/01/07 16:56:27 ad Exp $	*/
+/*	$NetBSD: inode.h,v 1.51 2008/01/09 16:15:23 ad Exp $	*/
 
 /*
  * Copyright (c) 1982, 1989, 1993
@@ -128,7 +128,6 @@ struct inode {
 	int32_t   i_gen;	/* Generation number. */
 	u_int32_t i_uid;	/* File owner. */
 	u_int32_t i_gid;	/* File group. */
-	u_int16_t i_omode;	/* Old mode, for ufs_reclaim */
 
 	struct dirhash *i_dirhash;	/* Hashing for large directories */
 
