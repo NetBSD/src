83e60dbbb46800536da152c1623ad807113ae518
elad <elad@NetBSD.org>
2008-01-09 08:18:12 +0000

    Refactor part of the sys_revoke() code so that it can be used in the compat code. Allows for the removal of two redundant kauth(9) calls.
    okay christos@.

--- sys/compat/svr4_32/svr4_32_fcntl.c
+++ sys/compat/svr4_32/svr4_32_fcntl.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: svr4_32_fcntl.c,v 1.30 2008/01/05 19:14:09 dsl Exp $	 */
+/*	$NetBSD: svr4_32_fcntl.c,v 1.31 2008/01/09 08:18:12 elad Exp $	 */
 
 /*-
  * Copyright (c) 1994, 1997 The NetBSD Foundation, Inc.
@@ -37,7 +37,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: svr4_32_fcntl.c,v 1.30 2008/01/05 19:14:09 dsl Exp $");
+__KERNEL_RCSID(0, "$NetBSD: svr4_32_fcntl.c,v 1.31 2008/01/09 08:18:12 elad Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -245,16 +245,7 @@ fd_revoke(struct lwp *l, int fd, register_t *retval)
 		goto out;
 	}
 
-	if ((error = VOP_GETATTR(vp, &vattr, l->l_cred)) != 0)
-		goto out;
-
-	if (kauth_cred_geteuid(l->l_cred) != vattr.va_uid &&
-	    (error = kauth_authorize_generic(l->l_cred,
-	    KAUTH_GENERIC_ISSUSER, NULL)) != 0)
-		goto out;
-
-	if (vp->v_usecount > 1 || (vp->v_iflag & VI_ALIASED))
-		VOP_REVOKE(vp, REVOKEALL);
+	error = dorevoke(vp, l->l_cred);
 out:
 	vrele(vp);
 	FILE_UNUSE(fp, l);
