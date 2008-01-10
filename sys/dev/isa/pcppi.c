0d28d05c6196422c84b1a8920124f2a01c8d679a
dyoung <dyoung@NetBSD.org>
2008-01-10 07:58:39 +0000

    Support pcppi(4) detachment.
    Use device_t, device_private().  Get rid of a struct device * cast.

--- sys/dev/isa/pcppi.c
+++ sys/dev/isa/pcppi.c
@@ -1,4 +1,4 @@
-/* $NetBSD: pcppi.c,v 1.25 2007/12/16 19:01:36 christos Exp $ */
+/* $NetBSD: pcppi.c,v 1.26 2008/01/10 07:58:39 dyoung Exp $ */
 
 /*
  * Copyright (c) 1996 Carnegie-Mellon University.
@@ -28,7 +28,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: pcppi.c,v 1.25 2007/12/16 19:01:36 christos Exp $");
+__KERNEL_RCSID(0, "$NetBSD: pcppi.c,v 1.26 2008/01/10 07:58:39 dyoung Exp $");
 
 #include "attimer.h"
 
@@ -60,7 +60,7 @@ int	pcppi_match(struct device *, struct cfdata *, void *);
 void	pcppi_isa_attach(struct device *, struct device *, void *);
 
 CFATTACH_DECL(pcppi, sizeof(struct pcppi_softc),
-    pcppi_match, pcppi_isa_attach, NULL, NULL);
+    pcppi_match, pcppi_isa_attach, pcppi_detach, NULL);
 
 static int pcppisearch(device_t, cfdata_t, const int *, void *);
 static void pcppi_bell_stop(void*);
@@ -159,7 +159,8 @@ pcppi_isa_attach(struct device *parent, struct device *self, void *aux)
 
         sc->sc_iot = iot = ia->ia_iot;
 
-        if (bus_space_map(iot, IO_PPI, 1, 0, &sc->sc_ppi_ioh))
+        sc->sc_size = 1;
+        if (bus_space_map(iot, IO_PPI, sc->sc_size, 0, &sc->sc_ppi_ioh))
                 panic("pcppi_attach: couldn't map");
 
         printf("\n");
@@ -170,6 +171,29 @@ pcppi_isa_attach(struct device *parent, struct device *self, void *aux)
 			aprint_error_dev(self,
 			    "couldn't establish power handler\n"); 
 
+}
+
+int
+pcppi_detach(device_t self, int flags)
+{
+	int rc;
+	struct pcppi_softc *sc = device_private(self);
+
+	if ((rc = config_detach_children(&sc->sc_dv, flags)) != 0)
+		return rc;
+
+	if (!pmf_device_register(self, NULL, NULL))
+		aprint_error_dev(self, "couldn't establish power handler\n");
+
+#if NPCKBD > 0
+	pckbd_unhook_bell(pcppi_pckbd_bell, sc);
+#endif
+	pcppi_bell_stop(sc);
+
+	callout_stop(&sc->sc_bell_ch);
+	callout_destroy(&sc->sc_bell_ch);
+	bus_space_unmap(sc->sc_iot, sc->sc_ppi_ioh, sc->sc_size);
+	return 0;
 }
 
 void
@@ -186,7 +210,7 @@ pcppi_attach(struct pcppi_softc *sc)
 	pckbd_hookup_bell(pcppi_pckbd_bell, sc);
 #endif
 #if NATTIMER > 0
-	config_defer((struct device *)sc, pcppi_attach_speaker);
+	config_defer(&sc->sc_dv, pcppi_attach_speaker);
 #endif
 
 	pa.pa_cookie = sc;
