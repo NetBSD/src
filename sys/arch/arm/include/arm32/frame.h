edd49d5150a7176051d270bf7a45b145ee30f195
skrll <skrll@NetBSD.org>
2008-01-12 20:50:23 +0000

    Push a switchframe in dumpsys and cpu_switchto, but as dumpsys calls other funcs a switchframe needs to be a multiple of 8 bytes. Stash sp as well in the switchframe to bump it to 24bytes.
    Setup the switchframe appropriately in cpu_lwp_fork.
    
    Remove savectx - nothing uses it.
    
    All of this make gdb's life much easier when dealing with crash dumps and
    live kernels.
    
    Reviewd by chris.

--- sys/arch/arm/include/arm32/frame.h
+++ sys/arch/arm/include/arm32/frame.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: frame.h,v 1.15 2007/03/09 19:21:58 thorpej Exp $	*/
+/*	$NetBSD: frame.h,v 1.16 2008/01/12 20:50:24 skrll Exp $	*/
 
 /*
  * Copyright (c) 1994-1997 Mark Brinicombe.
@@ -81,7 +81,9 @@ struct clockframe {
 };
 
 /*
- * Switch frame
+ * Switch frame.
+ *
+ * Should be a multiple of 8 bytes for dumpsys.
  */
 
 struct switchframe {
@@ -89,6 +91,7 @@ struct switchframe {
 	u_int	sf_r5;
 	u_int	sf_r6;
 	u_int	sf_r7;
+	u_int	sf_sp;
 	u_int	sf_pc;
 };
  
