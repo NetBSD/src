8c93baa8029e6d242a3a186be2e0c447d9ca33db
yamt <yamt@NetBSD.org>
2008-01-14 08:53:42 +0000

    constify

--- libexec/ld.elf_so/symbol.c
+++ libexec/ld.elf_so/symbol.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: symbol.c,v 1.43 2007/12/26 21:10:03 christos Exp $	 */
+/*	$NetBSD: symbol.c,v 1.44 2008/01/14 08:53:42 yamt Exp $	 */
 
 /*
  * Copyright 1996 John D. Polstra.
@@ -40,7 +40,7 @@
 
 #include <sys/cdefs.h>
 #ifndef lint
-__RCSID("$NetBSD: symbol.c,v 1.43 2007/12/26 21:10:03 christos Exp $");
+__RCSID("$NetBSD: symbol.c,v 1.44 2008/01/14 08:53:42 yamt Exp $");
 #endif /* not lint */
 
 #include <err.h>
@@ -63,7 +63,7 @@ typedef void (*fptr_t)(void);
 static bool
 _rtld_is_exported(const Elf_Sym *def)
 {
-	static fptr_t _rtld_exports[] = {
+	static const fptr_t _rtld_exports[] = {
 		(fptr_t)dlopen,
 		(fptr_t)dlclose,
 		(fptr_t)dlsym,
