a189da8d2dc0ee626f560bca5b326d142bf7e62e
jklos <jklos@NetBSD.org>
2008-01-17 15:54:35 +0000

    More Chelsio 10 gigabit driver files to complete the ones checked in yesterday.

--- sys/dev/pci/cxgb_config.h
+++ sys/dev/pci/cxgb_config.h
@@ -26,7 +26,7 @@ ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 POSSIBILITY OF SUCH DAMAGE.
 
 
-$FreeBSD: src/sys/dev/cxgb/cxgb_config.h,v 1.3 2007/05/25 09:48:19 kmacy Exp $
+$FreeBSD: src/sys/dev/cxgb/cxgb_config.h,v 1.4 2007/06/13 05:35:59 kmacy Exp $
 
 ***************************************************************************/
 #ifndef _CXGB_CONFIG_H_
@@ -34,7 +34,6 @@ $FreeBSD: src/sys/dev/cxgb/cxgb_config.h,v 1.3 2007/05/25 09:48:19 kmacy Exp $
 
 #ifndef CONFIG_DEFINED
 #define CONFIG_CHELSIO_T3_CORE
-#define DEFAULT_JUMBO
 #endif
 
 #endif
