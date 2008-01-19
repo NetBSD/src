f049e8f385df7ef45a14265799ea391db625f1d8
dyoung <dyoung@NetBSD.org>
2008-01-19 22:20:42 +0000

    Remove unused sc_media #define.

--- sys/dev/ic/hmevar.h
+++ sys/dev/ic/hmevar.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: hmevar.h,v 1.15 2007/03/04 06:01:55 christos Exp $	*/
+/*	$NetBSD: hmevar.h,v 1.16 2008/01/19 22:21:05 dyoung Exp $	*/
 
 /*-
  * Copyright (c) 1999 The NetBSD Foundation, Inc.
@@ -67,7 +67,6 @@ struct hme_softc {
 	struct device	sc_dev;		/* boilerplate device view */
 	struct ethercom	sc_ethercom;	/* Ethernet common part */
 	struct mii_data	sc_mii;		/* MII media control */
-#define sc_media	sc_mii.mii_media/* shorthand */
 	struct callout	sc_tick_ch;	/* tick callout */
 
 	/* The following bus handles are to be provided by the bus front-end */
