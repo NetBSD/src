10936ec1d5757f36af4c42e103c42c7215922ae7
dyoung <dyoung@NetBSD.org>
2008-01-20 21:49:57 +0000

    Do not reserve ISA bus space for npx@isa unless we hook IRQ 13, which we ought to never do.  If we have not hooked IRQ 13, we do not need any resources in npx_softc to handle numeric coprocessor exceptions, so let npx@isa detach.

--- sys/arch/i386/isa/npxvar.h
+++ sys/arch/i386/isa/npxvar.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: npxvar.h,v 1.6 2006/12/08 15:05:18 yamt Exp $	*/
+/*	$NetBSD: npxvar.h,v 1.7 2008/01/20 21:49:57 dyoung Exp $	*/
 
 /*-
  * Copyright (c) 1991 The Regents of the University of California.
@@ -87,4 +87,5 @@ struct npx_softc {
 
 enum npx_type npxprobe1(bus_space_tag_t, bus_space_handle_t, int);
 void npxattach(struct npx_softc *);
+int npxdetach(device_t, int);
 int npxintr(void *, struct intrframe *);
