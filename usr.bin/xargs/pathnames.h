08629af153f2a5e8218e5654354cdb26e22fee21
bouyer <bouyer>
2008-01-20 21:03:48 +0000

    Pull up following revision(s) (requested by apb in ticket #1893): 	usr.bin/xargs/strnsubst.c: revision 1.1 	usr.bin/xargs/xargs.1: revision 1.18, 1.19 	usr.bin/xargs/Makefile: revision 1.4 	usr.bin/xargs/pathnames.h: revision 1.6 	usr.bin/xargs/xargs.c: revision 1.15, 1.16, 1.18 Query kernel for maximum argument size instead of using ARG_MAX. PR/34182 from der Mouse -- clarify -0 option Bring in xargs from FreeBSD to gain -I, -J etc. but keep our GNU compatible exit values. Add more detail in comments about maximum size of environment and arguments passed by xargs(1) to execvp(2).  Research done while investigating PR 37666, which is not a problem in -current. Fix PR/34182, PR/36154 and PR/37666.

--- usr.bin/xargs/pathnames.h
+++ usr.bin/xargs/pathnames.h
@@ -1,5 +1,3 @@
-/*	$NetBSD: pathnames.h,v 1.5 2003/08/07 11:17:49 agc Exp $	*/
-
 /*-
  * Copyright (c) 1990, 1993
  *	The Regents of the University of California.  All rights reserved.
@@ -12,7 +10,11 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. Neither the name of the University nor the names of its contributors
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed by the University of
+ *	California, Berkeley and its contributors.
+ * 4. Neither the name of the University nor the names of its contributors
  *    may be used to endorse or promote products derived from this software
  *    without specific prior written permission.
  *
