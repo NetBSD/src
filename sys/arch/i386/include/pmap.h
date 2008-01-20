7ece7d8d01ecd54eb4c7aa2c1887ab2abd7c2334
bouyer <bouyer@NetBSD.org>
2008-01-20 18:42:38 +0000

    Sync with HEAD: make first argument of pmap_pte_cas() volatile.

--- sys/arch/i386/include/pmap.h
+++ sys/arch/i386/include/pmap.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: pmap.h,v 1.97.6.8 2008/01/19 14:08:09 bouyer Exp $	*/
+/*	$NetBSD: pmap.h,v 1.97.6.9 2008/01/20 18:42:39 bouyer Exp $	*/
 
 /*
  *
@@ -350,13 +350,13 @@ pmap_pte_set(pt_entry_t *pte, pt_entry_t npte)
 }
 
 static __inline pt_entry_t
-pmap_pte_cas(pt_entry_t *ptep, pt_entry_t o, pt_entry_t n)
+pmap_pte_cas(volatile pt_entry_t *ptep, pt_entry_t o, pt_entry_t n)
 {
 	int s = splvm();
 	pt_entry_t opte = *ptep;
 
 	if (opte == o) {
-		xpq_queue_pte_update(xpmap_ptetomach(ptep), n);
+		xpq_queue_pte_update(xpmap_ptetomach(__UNVOLATILE(ptep)), n);
 		xpq_flush_queue();
 	}
 	splx(s);
