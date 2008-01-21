41cf7262ad4e24c72262ab40385d28d0a791b644
bouyer <bouyer@NetBSD.org>
2008-01-21 20:36:47 +0000

    Pull up following revision(s) (requested by tsutsui in ticket #1040): 	sys/arch/algor/isa/mcclock_isa.c: revision 1.9 	sys/arch/algor/algor/algor_p5064_intr.c: revision 1.18, 1.20 via patch 	sys/arch/algor/algor/algor_p4032_intr.c: revision 1.15, 1.17 via patch 	sys/arch/algor/algor/algor_p6032_intr.c: revision 1.12, 1.14 via patch 	sys/arch/mips/mips/mips3_clock.c: revision 1.8, 1.9 	sys/arch/algor/dev/mcclock_mainbus.c: revision 1.7 	sys/arch/algor/algor/locore_machdep.S: revision 1.4 Fix botch on MI todr(9) migration of algor on September 2006: - year0 should be 1980, not 80 - put a newline after MI mc146818_attach() - call todr_attach(9) - in algor_p????_cal_timer() functions, initialize ci_divisor_delay   and ci_cycles_per_hz values in curcpu() for MI mips3_clock.c:mips3_delay()   rather than obsolete and homegrown delay_divisor - remove old _delay() stuff from locore_machdep.S - make MI mips3_delay() work even before frequency values in curcpu()   are initialized Now NetBSD/algor P5064 kernel works on gxemul. While here, add a hack for emulators: - ignore a measured CPU frequency if the value looks unlikely

--- sys/arch/algor/dev/mcclock_mainbus.c
+++ sys/arch/algor/dev/mcclock_mainbus.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: mcclock_mainbus.c,v 1.6 2006/09/05 01:33:24 gdamore Exp $	*/
+/*	$NetBSD: mcclock_mainbus.c,v 1.6.6.1 2008/01/21 20:36:49 bouyer Exp $	*/
 
 /*
  * Copyright (c) 1995, 1996 Carnegie-Mellon University.
@@ -29,7 +29,7 @@
 
 #include <sys/cdefs.h>			/* RCS ID & Copyright macro defns */
 
-__KERNEL_RCSID(0, "$NetBSD: mcclock_mainbus.c,v 1.6 2006/09/05 01:33:24 gdamore Exp $");
+__KERNEL_RCSID(0, "$NetBSD: mcclock_mainbus.c,v 1.6.6.1 2008/01/21 20:36:49 bouyer Exp $");
 
 #include <sys/param.h>
 #include <sys/kernel.h>
@@ -87,9 +87,13 @@ mcclock_mainbus_attach(struct device *parent, struct device *self, void *aux)
 	sc->sc_flag = 0;
 
 	/* Algor uses year 1980 as offset */
-	sc->sc_year0 = 80;
+	sc->sc_year0 = 1980;
 
 	mc146818_attach(sc);
+
+	aprint_normal("\n");
+
+	todr_attach(&sc->sc_handle);
 }
 
 void
--- sys/arch/mips/mips/mips3_clock.c
+++ sys/arch/mips/mips/mips3_clock.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: mips3_clock.c,v 1.4 2006/09/08 23:39:28 gdamore Exp $	*/
+/*	$NetBSD: mips3_clock.c,v 1.4.6.1 2008/01/21 20:36:49 bouyer Exp $	*/
 
 /*
  * Copyright (c) 1992, 1993
@@ -78,10 +78,11 @@
 
 #include <sys/cdefs.h>			/* RCS ID & Copyright macro defns */
 
-__KERNEL_RCSID(0, "$NetBSD: mips3_clock.c,v 1.4 2006/09/08 23:39:28 gdamore Exp $");
+__KERNEL_RCSID(0, "$NetBSD: mips3_clock.c,v 1.4.6.1 2008/01/21 20:36:49 bouyer Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
+#include <sys/kernel.h>
 #include <sys/timetc.h>
 #include <mips/mips3_clock.h>
 #include <machine/intr.h>
@@ -93,25 +94,37 @@ __KERNEL_RCSID(0, "$NetBSD: mips3_clock.c,v 1.4 2006/09/08 23:39:28 gdamore Exp 
 void
 mips3_delay(int n)
 {
+	u_long divisor_delay;
 	uint32_t cur, last, delta, usecs;
 
 	last = mips3_cp0_count_read();
 	delta = usecs = 0;
 
+	divisor_delay = curcpu()->ci_divisor_delay;
+	if (divisor_delay == 0) {
+		/*
+		 * Frequency values in curcpu() are not initialized.
+		 * Assume faster frequency since longer delays are harmless.
+		 * Note CPU_MIPS_DOUBLE_COUNT is ignored here.
+		 */
+#define FAST_FREQ	(300 * 1000 * 1000)	/* fast enough? */
+		divisor_delay = FAST_FREQ / (1000 * 1000);
+	}
+
 	while (n > usecs) {
 		cur = mips3_cp0_count_read();
 
-		/* Check to see if the timer has wrapped around. */
-		if (cur < last)
-			delta += ((curcpu()->ci_cycles_per_hz - last) + cur);
-		else
-			delta += (cur - last);
+		/*
+		 * The MIPS3 CP0 counter always counts upto UINT32_MAX,
+		 * so no need to check wrapped around case.
+		 */
+		delta += (cur - last);
 
 		last = cur;
 
-		if (delta >= curcpu()->ci_divisor_delay) {
-			usecs += delta / curcpu()->ci_divisor_delay;
-			delta %= curcpu()->ci_divisor_delay;
+		if (delta >= divisor_delay) {
+			usecs += delta / divisor_delay;
+			delta %= divisor_delay;
 		}
 	}
 }
