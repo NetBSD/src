99daa6239245ff0102d3d3e19119cbd1cb09204d
lukem <lukem>
2008-01-21 10:31:28 +0000

    Turn off the alarmtimer before resetting the SIGALRM handler back to SIG_DFL. Fix from dieter roelants <dieter.NetBSD@pandora.be> Should fix PR 35630.

--- usr.bin/ftp/version.h
+++ usr.bin/ftp/version.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: version.h,v 1.71 2007/12/05 00:15:25 lukem Exp $	*/
+/*	$NetBSD: version.h,v 1.72 2008/01/21 10:31:28 lukem Exp $	*/
 /*-
  * Copyright (c) 1999-2007 The NetBSD Foundation, Inc.
  * All rights reserved.
@@ -40,5 +40,5 @@
 #endif
 
 #ifndef FTP_VERSION
-#define	FTP_VERSION	"20071205"
+#define	FTP_VERSION	"20080121"
 #endif
