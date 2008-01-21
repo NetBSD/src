8255793e2ff2f702313d97aa3c31f22160a447f6
ichiro <ichiro@NetBSD.org>
2008-01-21 12:11:21 +0000

    change umass quirks logic in USB_MATCH()
    PR/37687 from Yojiro UO

--- sys/dev/usb/umassvar.h
+++ sys/dev/usb/umassvar.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: umassvar.h,v 1.25 2007/01/09 16:46:02 christos Exp $	*/
+/*	$NetBSD: umassvar.h,v 1.26 2008/01/21 12:11:21 ichiro Exp $	*/
 /*-
  * Copyright (c) 1999 MAEKAWA Masahide <bishop@rr.iij4u.or.jp>,
  *		      Nick Hibma <n_hibma@freebsd.org>
@@ -179,6 +179,8 @@ struct umass_softc {
 #define	UMASS_QUIRK_WRONG_CSWSIG	0x00000001
 #define	UMASS_QUIRK_WRONG_CSWTAG	0x00000002
 #define	UMASS_QUIRK_RBC_PAD_TO_12	0x00000004
+
+#define UMASS_QUIRK_USE_DEFAULTMATCH	-1
 
 	u_int32_t		sc_busquirks;
 
