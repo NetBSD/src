b814eb63c70f636da597808db4d5cef02609582e
jmcneill <jmcneill@NetBSD.org>
2008-01-22 01:05:05 +0000

    Add ossaudio support for COMPAT_LINUX32, with help from mrg. With this change, audio now works on amd64 with native firefox, nspluginwrapper, and the 32-bit linux flash binaries.

--- sys/compat/ossaudio/ossaudiovar.h
+++ sys/compat/ossaudio/ossaudiovar.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: ossaudiovar.h,v 1.13 2007/09/18 19:49:53 mlelstv Exp $	*/
+/*	$NetBSD: ossaudiovar.h,v 1.14 2008/01/22 01:05:05 jmcneill Exp $	*/
 
 /*-
  * Copyright (c) 1997 The NetBSD Foundation, Inc.
@@ -44,7 +44,7 @@ struct oss_sys_ioctl_args {
 #define OSS_IOC_IN          0x40000000      /* copy in parameters */
 #define OSS_IOC_OUT         0x80000000      /* copy out parameters */
 #define OSS_IOC_INOUT       (OSS_IOC_IN | OSS_IOC_OUT)
-#define	_OSS_IOCTL(w,x,y,z) ((int)((w)|(((z)&OSS_IOCPARM_MASK)<<16)|((x)<<8)|(y)))
+#define	_OSS_IOCTL(w,x,y,z) ((uint)((w)|(((z)&OSS_IOCPARM_MASK)<<16)|((x)<<8)|(y)))
 #define _OSS_IO(x,y)        _OSS_IOCTL(OSS_IOC_VOID, x, y, 0)
 #define _OSS_IOR(x,y,t)     _OSS_IOCTL(OSS_IOC_OUT, x, y, sizeof(t))
 #define _OSS_IOW(x,y,t)     _OSS_IOCTL(OSS_IOC_IN, x, y, sizeof(t))
