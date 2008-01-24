0beb82c81b453ff913546c97b7c2dda430d3a2e6
garbled <garbled@NetBSD.org>
2008-01-24 01:12:57 +0000

    Add another optional argument to mkbootinfo.sh, the location of the bitmap.

--- sys/arch/ofppc/stand/bootinfo/mkbootinfo.sh
+++ sys/arch/ofppc/stand/bootinfo/mkbootinfo.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
-# $NetBSD: mkbootinfo.sh,v 1.1 2008/01/23 23:09:43 garbled Exp $
+# $NetBSD: mkbootinfo.sh,v 1.2 2008/01/24 01:12:57 garbled Exp $
 
 OS=$1
 BPART=$2
 OUT=$3
+BITMAP=$4
 
 if [ -z "$OS" ]; then
 	OS="NetBSD/ofppc"
@@ -17,13 +18,15 @@ if [ -z "$OUT" ]; then
 	OUT="bootinfo.txt"
 fi
 
+if [ -z "$BITMAP" ]; then
+	BITMAP="/usr/mdec/netbsd.chrp"
+fi
+
 echo "<chrp-boot>" > $OUT
 echo "<description>${OS}</description>" >>$OUT
 echo "<os-name>${OS}</os-name>" >>$OUT
 echo "<boot-script>boot &device;:${BPART}</boot-script>" >>$OUT
-if [ -f "./netbsd.chrp" ]; then
-	/bin/cat ./netbsd.chrp >>$OUT
-else
-	/bin/cat /usr/mdec/netbsd.chrp >>$OUT
+if [ -f "${BITMAP}" ]; then
+	/bin/cat ${BITMAP} >>$OUT
 fi
 echo "</chrp-boot>" >>$OUT
