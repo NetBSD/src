62c79262334e2dbd1865ae844e79092e7e67dde0
christos <christos>
2008-01-27 00:54:36 +0000

    from www.openpam.org

--- dist/openpam/lib/openpam_free_envlist.c
+++ dist/openpam/lib/openpam_free_envlist.c
@@ -25,7 +25,7 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
- * $P4: //depot/projects/openpam/lib/openpam_free_envlist.c#2 $
+ * $Id: openpam_free_envlist.c,v 1.1.1.2 2008/01/27 00:54:49 christos Exp $
  */
 
 #include <stdlib.h>
