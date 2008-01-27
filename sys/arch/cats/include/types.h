3a3d5afa14dbeaa63e047a550ae26e3a7c151c7f
chris <chris>
2008-01-27 13:08:36 +0000

    Sync to HEAD.

--- sys/arch/cats/include/types.h
+++ sys/arch/cats/include/types.h
@@ -1,12 +1,10 @@
-/*	$NetBSD: types.h,v 1.9.10.2 2008/01/01 15:39:54 chris Exp $	*/
+/*	$NetBSD: types.h,v 1.9.10.3 2008/01/27 13:08:41 chris Exp $	*/
 
 #ifndef _ARM32_TYPES_H_
 #define	_ARM32_TYPES_H_
 
 #include <arm/arm32/types.h>
 #define __HAVE_DEVICE_REGISTER
-#define	__HAVE_TIMECOUNTER
-#define __HAVE_GENERIC_TODR
 #define __HAVE_GENERIC_ARM_INTERRUPTS
 
 #endif
--- sys/arch/shark/include/types.h
+++ sys/arch/shark/include/types.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: types.h,v 1.7.10.1 2008/01/01 15:39:56 chris Exp $	*/
+/*	$NetBSD: types.h,v 1.7.10.2 2008/01/27 13:08:41 chris Exp $	*/
 
 #ifndef _SHARK_TYPES_H_
 #define	_SHARK_TYPES_H_
@@ -9,7 +9,5 @@
 #define	__ARM_FIQ_INDIRECT
 
 #define	__HAVE_DEVICE_REGISTER
-#define	__HAVE_TIMECOUNTER
-#define	__HAVE_GENERIC_TODR
 
 #endif /* _SHARK_TYPES_H_ */
