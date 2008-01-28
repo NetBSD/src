cbf7b0adc1e84d44710276c5b8f6bf5184140583
matt <matt@NetBSD.org>
2008-01-28 18:18:25 +0000

    Add new fast softint switch code (common for all arm).

--- sys/arch/arm/arm/softintr.c
+++ sys/arch/arm/arm/softintr.c
@@ -1,10 +1,11 @@
-/*	$NetBSD: softintr.c,v 1.4 2007/02/19 21:45:23 matt Exp $	*/
+/*	$NetBSD: softintr.c,v 1.4.24.1 2008/01/28 18:18:25 matt Exp $	*/
 
-/*
- * Copyright (c) 2001 Wasabi Systems, Inc.
+/*-
+ * Copyright (c) 2007 The NetBSD Foundation, Inc.
  * All rights reserved.
  *
- * Written by Jason R. Thorpe for Wasabi Systems, Inc.
+ * This code is derived from software contributed to The NetBSD Foundation
+ * by Matt Thomas.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -16,16 +17,16 @@
  *    documentation and/or other materials provided with the distribution.
  * 3. All advertising materials mentioning features or use of this software
  *    must display the following acknowledgement:
- *	This product includes software developed for the NetBSD Project by
- *	Wasabi Systems, Inc.
- * 4. The name of Wasabi Systems, Inc. may not be used to endorse
- *    or promote products derived from this software without specific prior
- *    written permission.
+ *        This product includes software developed by the NetBSD
+ *        Foundation, Inc. and its contributors.
+ * 4. Neither the name of The NetBSD Foundation nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
  *
- * THIS SOFTWARE IS PROVIDED BY WASABI SYSTEMS, INC. ``AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
  * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL WASABI SYSTEMS, INC
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS
  * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
  * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
  * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
@@ -34,168 +35,81 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
-
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: softintr.c,v 1.4 2007/02/19 21:45:23 matt Exp $");
+__KERNEL_RCSID(0, "$NetBSD: softintr.c,v 1.4.24.1 2008/01/28 18:18:25 matt Exp $");
 
 #include <sys/param.h>
+#include <sys/systm.h>
+#include <sys/syslog.h>
 #include <sys/malloc.h>
-
-/* XXX Network interrupts should be converted to new softintrs. */
-#include <net/netisr.h>
+#include <sys/conf.h>
 
 #include <uvm/uvm_extern.h>
 
+#include <machine/atomic.h>
 #include <machine/intr.h>
+#include <machine/cpu.h>
 
-struct soft_intrq soft_intrq[SI_NQUEUES];
+#ifdef __HAVE_FAST_SOFTINTS
+void softint_switch(lwp_t *, int);
 
-struct soft_intrhand *softnet_intrhand;
+/* Handle software interrupts */
 
-void	netintr(void);
+#define	SM(x)	(1 << SOFTINT_ ## x)
 
-/*
- * softintr_init:
- *
- *	Initialize the software interrupt system.
- */
 void
-softintr_init(void)
+softint_init_md(lwp_t *l, u_int level, uintptr_t *machdep)
 {
-	static const char * const softintr_names[] = SI_QUEUENAMES;
-	struct soft_intrq *siq;
-	int i;
-
-	for (i = 0; i < SI_NQUEUES; i++) {
-		siq = &soft_intrq[i];
-		TAILQ_INIT(&siq->siq_list);
-		evcnt_attach_dynamic(&siq->siq_evcnt, EVCNT_TYPE_INTR,
-		    NULL, "soft", softintr_names[i]);
-		siq->siq_si = i;
-	}
-
-	/* XXX Establish legacy software interrupt handlers. */
-	softnet_intrhand = softintr_establish(IPL_SOFTNET,
-	    (void (*)(void *))netintr, NULL);
-
-	assert(softnet_intrhand != NULL);
-}
-
-/*
- * softintr_dispatch:
- *
- *	Process pending software interrupts on the specified queue.
- *
- *	NOTE: We must already be at the correct interrupt priority level.
- */
-void
-softintr_dispatch(int si)
-{
-	struct soft_intrq *siq = &soft_intrq[si];
-	struct soft_intrhand *sih;
-	int oldirqstate;
-
-	siq->siq_evcnt.ev_count++;
-	for (;;) {
-		oldirqstate = disable_interrupts(I32_bit);
-		sih = TAILQ_FIRST(&siq->siq_list);
-		if (sih == NULL) {
-			restore_interrupts(oldirqstate);
-			break;
-		}
-
-		TAILQ_REMOVE(&siq->siq_list, sih, sih_list);
-		sih->sih_pending = 0;
-
-		uvmexp.softs++;
-
-		restore_interrupts(oldirqstate);
-
-		(*sih->sih_func)(sih->sih_arg);
-	}
-}
-
-/*
- * softintr_establish:		[interface]
- *
- *	Register a software interrupt handler.
- */
-void *
-softintr_establish(int ipl, void (*func)(void *), void *arg)
-{
-	struct soft_intrhand *sih;
-	int si;
-
-	switch (ipl) {
-	case IPL_SOFT:
-		si = SI_SOFT;
-		break;
-
-	case IPL_SOFTCLOCK:
-		si = SI_SOFTCLOCK;
-		break;
-
-	case IPL_SOFTNET:
-		si = SI_SOFTNET;
-		break;
-
-	case IPL_SOFTSERIAL:
-		si = SI_SOFTSERIAL;
-		break;
-
-	default:
-		panic("softintr_establish: unknown soft IPL %d", ipl);
-	}
-
-	sih = malloc(sizeof(*sih), M_DEVBUF, M_NOWAIT);
-	if (__predict_true(sih != NULL)) {
-		sih->sih_func = func;
-		sih->sih_arg = arg;
-		sih->sih_siq = &soft_intrq[si];
-		sih->sih_pending = 0;
-	}
-	return (sih);
-}
-
-/*
- * softintr_disestablish:	[interface]
- *
- *	Unregister a software interrupt handler.
- */
-void
-softintr_disestablish(void *arg)
-{
-	struct soft_intrhand *sih = arg;
-	struct soft_intrq *siq = sih->sih_siq;
-	int oldirqstate;
-
-	oldirqstate = disable_interrupts(I32_bit);
-	if (sih->sih_pending) {
-		TAILQ_REMOVE(&siq->siq_list, sih, sih_list);
-		sih->sih_pending = 0;
-	}
-	restore_interrupts(oldirqstate);
-
-	free(sih, M_DEVBUF);
+	/*
+	 * Remember the lwp for this level;
+	 */
+	*machdep = 1 << level;
+	l->l_cpu->ci_softlwps[level] = l;
 }
 
 void
-netintr(void)
+softint_trigger(uintptr_t machdep)
 {
-	int n, s;
+	curcpu()->ci_softints |= machdep;
+}
 
+#if IPL_NONE != 0 || IPL_SOFTCLOCK != 1 || IPL_SOFTBIO != 2 \
+	|| IPL_SOFTNET != 3 || IPL_SOFTSERIAL != 4
+#error IPL levels are wrong.
+#endif
+
+void
+dosoftints(void)
+{
+	struct cpu_info * const ci = curcpu();
+	const int cpl = curcpl();
+	uint8_t softints;
+	int s;
+
+	if (cpl >= IPL_SOFTSERIAL || (softints = ci->ci_softints) == 0)
+		return;
+	/*
+	 * Mask out the softints that are equal or before the current
+	 * spl level.
+	 */
+	softints &= ~((1 << cpl) - 1);
+	if (softints == 0)
+		return;
+
+	/*
+	 * Serial software interrupts
+	 */
+#define	DOSOFTINT(x)	do {						\
+	if (softints & SM(x)) {						\
+		softint_switch(ci->ci_softlwps[SOFTINT_ ## x], IPL_SOFT ## x);\
+	}								\
+} while (0)
 	s = splhigh();
-	n = netisr;
-	netisr = 0;
-	splx(s);
-
-#define	DONETISR(bit, fn)						\
-	do {								\
-		if (n & (1 << (bit)))					\
-			fn();						\
-	} while (/*CONSTCOND*/0)
-
-#include <net/netisr_dispatch.h>
-
-#undef DONETISR
+	ci->ci_softints ^= softints;	/* clear any pending bits */
+	DOSOFTINT(SERIAL);
+	DOSOFTINT(NET);
+	DOSOFTINT(BIO);
+	DOSOFTINT(CLOCK);
+	(void)splx(s);
 }
+#endif	/* __HAVE_FAST_SOFTINTS */
