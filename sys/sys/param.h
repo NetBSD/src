4b72de18c5b026daf8191a41522b7dc008bc86c3
pooka <pooka@NetBSD.org>
2008-01-28 15:38:13 +0000

    update name to 4.99.51 (hi dh ;)

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.302 2008/01/28 14:34:36 dholland Exp $	*/
+/*	$NetBSD: param.h,v 1.303 2008/01/28 15:38:13 pooka Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -63,7 +63,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	499005100	/* NetBSD 4.99.50 */
+#define	__NetBSD_Version__	499005100	/* NetBSD 4.99.51 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
