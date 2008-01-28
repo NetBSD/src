a5e013d5bb99dbc8108afd7ec8226f74add665fd
njoly <njoly@NetBSD.org>
2008-01-28 14:31:35 +0000

    Make shmctl + {IPC,SHM}_STAT work an amd64, by forcing use of IPC_64. Following the Linux kernel behaviour.

--- sys/compat/linux/arch/amd64/linux_machdep.h
+++ sys/compat/linux/arch/amd64/linux_machdep.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: linux_machdep.h,v 1.10 2007/12/04 18:40:13 dsl Exp $ */
+/*	$NetBSD: linux_machdep.h,v 1.11 2008/01/28 14:31:35 njoly Exp $ */
 
 /*-
  * Copyright (c) 2005 Emmanuel Dreyfus, all rights reserved.
@@ -116,6 +116,7 @@ __END_DECLS
 #define LINUX_UNAME_ARCH MACHINE_ARCH
 #define LINUX_NPTL
 #define LINUX_LARGEFILE64
+#define LINUX_SHMCTL_FORCEIPC64
 
 /*
  * Used in ugly patch to fake device numbers.
@@ -125,6 +126,5 @@ __END_DECLS
 #define LINUX_PTS_MAJOR                3
 /* Major device numbers of VT device on both Linux and NetBSD. */
 #define LINUX_CONS_MAJOR       4
-
 
 #endif /* _AMD64_LINUX_MACHDEP_H */
