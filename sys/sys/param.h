c2ea464039d87b2d86bc868fbb59705a5cadb3df
ad <ad@NetBSD.org>
2008-01-30 12:19:22 +0000

    4.99.52 - vnode, mount locking changes

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.303 2008/01/28 15:38:13 pooka Exp $	*/
+/*	$NetBSD: param.h,v 1.304 2008/01/30 12:19:22 ad Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -63,7 +63,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	499005100	/* NetBSD 4.99.51 */
+#define	__NetBSD_Version__	499005200	/* NetBSD 4.99.52 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
