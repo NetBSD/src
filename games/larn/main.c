a15579ca0a9aaa36ad745371f159d6c3210b4e56
riz <riz@NetBSD.org>
2008-02-03 18:29:04 +0000

    Pull up following revision(s) (requested by dholland in ticket #1899): 	games/larn/scores.c: revision 1.16 	games/larn/header.h: revision 1.18 	games/larn/bill.c: revision 1.9 	games/larn/main.c: revision 1.21 Since games are (now) setgid, not setuid, it is no longer necessary to manipulate the effective uid, only the effective gid. Use mkstemp to make the temporary files used when you win.

--- games/larn/main.c
+++ games/larn/main.c
@@ -1,9 +1,9 @@
-/*	$NetBSD: main.c,v 1.16 2001/02/05 00:57:33 christos Exp $	*/
+/*	$NetBSD: main.c,v 1.16.8.1 2008/02/03 18:29:05 riz Exp $	*/
 
 /* main.c		 */
 #include <sys/cdefs.h>
 #ifndef lint
-__RCSID("$NetBSD: main.c,v 1.16 2001/02/05 00:57:33 christos Exp $");
+__RCSID("$NetBSD: main.c,v 1.16.8.1 2008/02/03 18:29:05 riz Exp $");
 #endif				/* not lint */
 
 #include <sys/types.h>
@@ -20,7 +20,7 @@ int             srcount = 0;	/* line counter for showstr()	 */
 int             dropflag = 0;	/* if 1 then don't lookforobject() next round */
 int             rmst = 80;	/* random monster creation counter		 */
 int             userid;		/* the players login user id number */
-uid_t           uid, euid;	/* used for security */
+gid_t           gid, egid;	/* used for security */
 u_char          nowelcome = 0, nomove = 0;	/* if (nomove) then don't
 						 * count next iteration as a
 						 * move */
@@ -63,9 +63,9 @@ main(argc, argv)
 	struct passwd  *pwe;
 
 	i = 0;
-	euid = geteuid();
-	uid = getuid();
-	seteuid(uid);		/* give up "games" if we have it */
+	egid = getegid();
+	gid = getgid();
+	setegid(gid);		/* give up "games" if we have it */
 	/*
 	 *	first task is to identify the player
 	 */
