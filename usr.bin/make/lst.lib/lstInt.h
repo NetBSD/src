de40f4fcfcee299b1e143521b94f7c5d83cbe458
joerg <joerg@NetBSD.org>
2008-02-05 15:11:50 +0000

    Merge from pkgsrc's bmake: don't use signed short for bitfields. This makes IBM xlc happy.

--- usr.bin/make/lst.lib/lstInt.h
+++ usr.bin/make/lst.lib/lstInt.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: lstInt.h,v 1.13 2006/10/27 21:37:25 dsl Exp $	*/
+/*	$NetBSD: lstInt.h,v 1.14 2008/02/05 15:11:50 joerg Exp $	*/
 
 /*
  * Copyright (c) 1988, 1989, 1990, 1993
@@ -47,7 +47,7 @@
 typedef struct ListNode {
 	struct ListNode	*prevPtr;   /* previous element in list */
 	struct ListNode	*nextPtr;   /* next in list */
-	short	    	useCount:8, /* Count of functions using the node.
+	unsigned short	useCount:8, /* Count of functions using the node.
 				     * node may not be deleted until count
 				     * goes to 0 */
  	    	    	flags:8;    /* Node status flags */
