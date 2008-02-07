56b935b236282fb4d307847a1b71d0ce969365cd
joerg <joerg>
2008-02-07 23:46:47 +0000

    Deal with statvfs compat issue by including it in nbcompat.h for now.

--- usr.sbin/pkg_install/lib/nbcompat.h
+++ usr.sbin/pkg_install/lib/nbcompat.h
@@ -1,6 +1,8 @@
-/* $NetBSD: nbcompat.h,v 1.2 2007/07/16 19:33:15 joerg Exp $ */
+/* $NetBSD: nbcompat.h,v 1.3 2008/02/07 23:46:47 joerg Exp $ */
 
 /*
  * In pkgsrc, libnbcompat is used to provide compatibility functions.
  * This is not needed on native NetBSD, so provide an empty header.
  */
+
+#include <sys/statvfs.h>
