5ca1f720023c48aa22ec3f5d7c816d04dcd73ddd
jmmv <jmmv>
2008-02-10 16:02:24 +0000

    Add more detailed checks for each of the primary modctl commands.

--- tests/modules/t_modctl.cpp
+++ tests/modules/t_modctl.cpp
@@ -1,4 +1,4 @@
-// $NetBSD: t_modctl.cpp,v 1.1 2008/02/10 12:40:10 jmmv Exp $
+// $NetBSD: t_modctl.cpp,v 1.2 2008/02/10 16:02:24 jmmv Exp $
 //
 // Copyright (c) 2008 The NetBSD Foundation, Inc.
 // All rights reserved.
@@ -35,12 +35,13 @@
 
 extern "C" {
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: t_modctl.cpp,v 1.1 2008/02/10 12:40:10 jmmv Exp $");
+__KERNEL_RCSID(0, "$NetBSD: t_modctl.cpp,v 1.2 2008/02/10 16:02:24 jmmv Exp $");
 
 #include <sys/module.h>
 #include <sys/sysctl.h>
 }
 
+#include <cassert>
 #include <cerrno>
 #include <cstring>
 #include <iostream>
@@ -48,6 +49,8 @@ __KERNEL_RCSID(0, "$NetBSD: t_modctl.cpp,v 1.1 2008/02/10 12:40:10 jmmv Exp $");
 #include <atf.hpp>
 
 static bool have_modular = false;
+
+enum presence_check { both_checks, stat_check, sysctl_check };
 
 /* --------------------------------------------------------------------- */
 /* Auxiliary functions                                                   */
@@ -91,13 +94,68 @@ require_modular(void)
 		ATF_SKIP("Kernel does not have 'options MODULAR'.");
 }
 
+static
+bool
+get_modstat_info(const char *name, modstat_t *msdest)
+{
+	bool found;
+	size_t len;
+	struct iovec iov;
+	modstat_t *ms;
+
+	for (len = 4096; ;) {
+		iov.iov_base = ::malloc(len);
+		iov.iov_len = len;
+		if (::modctl(MODCTL_STAT, &iov) != 0) {
+			int err = errno;
+			std::cerr << "modctl(MODCTL_STAT) failed: "
+			    << std::strerror(err)
+			    << std::endl;
+			ATF_FAIL("Failed to query module status");
+		}
+		if (len >= iov.iov_len)
+			break;
+		::free(iov.iov_base);
+		len = iov.iov_len;
+	}
+
+	found = false;
+	len = iov.iov_len / sizeof(modstat_t);
+	for (ms = (modstat_t *)iov.iov_base; len != 0 && !found;
+	    ms++, len--) {
+		if (std::strcmp(ms->ms_name, name) == 0) {
+			if (msdest != NULL)
+				*msdest = *ms;
+			found = true;
+		}
+	}
+
+	::free(iov.iov_base);
+
+	return found;
+}
+
 /*
  * Returns a boolean indicating if the k_helper module was loaded
- * successfully.
+ * successfully.  This implementation uses modctl(2)'s MODCTL_STAT
+ * subcommand to do the check.
  */
 static
 bool
-k_helper_is_present(void)
+k_helper_is_present_stat(void)
+{
+
+	return get_modstat_info("k_helper", NULL);
+}
+
+/*
+ * Returns a boolean indicating if the k_helper module was loaded
+ * successfully.  This implementation uses the module's sysctl
+ * installed node to do the check.
+ */
+static
+bool
+k_helper_is_present_sysctl(void)
 {
 	size_t present, presentsize;
 	int ret;
@@ -115,21 +173,59 @@ k_helper_is_present(void)
 }
 
 /*
- * Loads the specified module from a file.
+ * Returns a boolean indicating if the k_helper module was loaded
+ * successfully.  The 'how' parameter specifies the implementation to
+ * use to do the check.
  */
 static
-void
-load(const std::string& filename)
+bool
+k_helper_is_present(presence_check how)
 {
+	bool found;
+
+	switch (how) {
+	case both_checks:
+		found = k_helper_is_present_stat();
+		ATF_CHECK(k_helper_is_present_sysctl() == found);
+		break;
+
+	case stat_check:
+		found = k_helper_is_present_stat();
+		break;
+
+	case sysctl_check:
+		found = k_helper_is_present_sysctl();
+		break;
+
+	default:
+		assert(false);
+	}
+
+	return found;
+}
+
+/*
+ * Loads the specified module from a file.  If fatal is set and an error
+ * occurs when loading the module, an error message is printed and the
+ * test case is aborted.
+ */
+static
+int
+load(const std::string& filename, bool fatal = true)
+{
+	int err;
 
 	std::cout << "Loading module " << filename << std::endl;
+	err = 0;
 	if (::modctl(MODCTL_LOAD, __UNCONST(filename.c_str())) == -1) {
-		int err = errno;
+		err = errno;
 		std::cerr << "modctl(MODCTL_LOAD, " << filename
 		    << ") failed: " << std::strerror(err)
 		    << std::endl;
-		ATF_FAIL("Module load failed");
+		if (fatal)
+			ATF_FAIL("Module load failed");
 	}
+	return err;
 }
 
 /*
@@ -137,51 +233,121 @@ load(const std::string& filename)
  * printed and no errors will be raised if the unload was unsuccessful.
  */
 static
-void
-unload(const char *name, bool silent = false)
+int
+unload(const char *name, bool fatal = true)
+{
+	int err;
+
+	std::cout << "Unloading module " << name << std::endl;
+	err = 0;
+	if (::modctl(MODCTL_UNLOAD, __UNCONST(name)) == -1) {
+		err = errno;
+		std::cerr << "modctl(MODCTL_UNLOAD, " << name
+		    << ") failed: " << std::strerror(err)
+		    << std::endl;
+		if (fatal)
+			ATF_FAIL("Module unload failed");
+	}
+	return err;
+}
+
+/*
+ * A silent version of unload, to be called as part of the cleanup
+ * process only.
+ */
+static
+int
+unload_cleanup(const char *name)
 {
 
-	if (!silent) {
-		std::cout << "Unloading module " << name << std::endl;
-		if (::modctl(MODCTL_UNLOAD, __UNCONST(name)) == -1) {
-			int err = errno;
-			std::cerr << "modctl(MODCTL_UNLOAD, " << name
-			    << ") failed: " << std::strerror(err)
-			    << std::endl;
-			ATF_FAIL("Module unload failed");
-		}
-	} else {
-		(void)::modctl(MODCTL_UNLOAD, __UNCONST(name));
-	}
+	(void)::modctl(MODCTL_UNLOAD, __UNCONST(name));
 }
 
 /* --------------------------------------------------------------------- */
 /* Test cases                                                            */
 /* --------------------------------------------------------------------- */
 
-ATF_TEST_CASE_WITH_CLEANUP(plain);
-ATF_TEST_CASE_HEAD(plain)
+ATF_TEST_CASE_WITH_CLEANUP(cmd_load);
+ATF_TEST_CASE_HEAD(cmd_load)
 {
-	set("descr", "Checks that plain loading and unloading works");
+	set("descr", "Tests for the MODCTL_LOAD command");
 	set("require.user", "root");
 }
-ATF_TEST_CASE_BODY(plain)
+ATF_TEST_CASE_BODY(cmd_load)
+{
+	require_modular();
+
+	ATF_CHECK(load("", false) == ENOENT);
+	ATF_CHECK(load("non-existent.o", false) == ENOENT);
+
+	std::string longname(MAXPATHLEN, 'a');
+	ATF_CHECK(load(longname.c_str(), false) == ENAMETOOLONG);
+
+	ATF_CHECK(!k_helper_is_present(stat_check));
+	load(get_srcdir() + "/k_helper.o");
+	std::cout << "Checking if load was successful" << std::endl;
+	ATF_CHECK(k_helper_is_present(stat_check));
+}
+ATF_TEST_CASE_CLEANUP(cmd_load)
+{
+	unload_cleanup("k_helper");
+}
+
+ATF_TEST_CASE_WITH_CLEANUP(cmd_stat);
+ATF_TEST_CASE_HEAD(cmd_stat)
+{
+	set("descr", "Tests for the MODCTL_STAT command");
+	set("require.user", "root");
+}
+ATF_TEST_CASE_BODY(cmd_stat)
+{
+	require_modular();
+
+	ATF_CHECK(!k_helper_is_present(both_checks));
+
+	load(get_srcdir() + "/k_helper.o");
+	ATF_CHECK(k_helper_is_present(both_checks));
+	{
+		modstat_t ms;
+		ATF_CHECK(get_modstat_info("k_helper", &ms));
+
+		ATF_CHECK(ms.ms_class == MODULE_CLASS_MISC);
+		ATF_CHECK(ms.ms_source == MODULE_SOURCE_FILESYS);
+		ATF_CHECK(ms.ms_refcnt == 0);
+	}
+	unload("k_helper");
+
+	ATF_CHECK(!k_helper_is_present(both_checks));
+}
+ATF_TEST_CASE_CLEANUP(cmd_stat)
+{
+	unload_cleanup("k_helper");
+}
+
+ATF_TEST_CASE_WITH_CLEANUP(cmd_unload);
+ATF_TEST_CASE_HEAD(cmd_unload)
+{
+	set("descr", "Tests for the MODCTL_UNLOAD command");
+	set("require.user", "root");
+}
+ATF_TEST_CASE_BODY(cmd_unload)
 {
 	require_modular();
 
 	load(get_srcdir() + "/k_helper.o");
 
-	std::cout << "Checking if load was successful" << std::endl;
-	ATF_CHECK(k_helper_is_present());
+	ATF_CHECK(unload("", false) == ENOENT);
+	ATF_CHECK(unload("non-existent.o", false) == ENOENT);
+	ATF_CHECK(unload("k_helper.o", false) == ENOENT);
 
+	ATF_CHECK(k_helper_is_present(stat_check));
 	unload("k_helper");
-
 	std::cout << "Checking if unload was successful" << std::endl;
-	ATF_CHECK(!k_helper_is_present());
+	ATF_CHECK(!k_helper_is_present(stat_check));
 }
-ATF_TEST_CASE_CLEANUP(plain)
+ATF_TEST_CASE_CLEANUP(cmd_unload)
 {
-	unload("k_helper", true);
+	unload_cleanup("k_helper");
 }
 
 /* --------------------------------------------------------------------- */
@@ -192,5 +358,7 @@ ATF_INIT_TEST_CASES(tcs)
 {
 	have_modular = check_modular();
 
-	ATF_ADD_TEST_CASE(tcs, plain);
+	ATF_ADD_TEST_CASE(tcs, cmd_load);
+	ATF_ADD_TEST_CASE(tcs, cmd_stat);
+	ATF_ADD_TEST_CASE(tcs, cmd_unload);
 }
