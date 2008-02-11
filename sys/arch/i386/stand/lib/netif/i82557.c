f8115446f2b27334a28b5af1c7ad956145defa22
mlelstv <mlelstv@NetBSD.org>
2008-02-11 13:51:35 +0000

    Can't do math on void *

--- sys/arch/i386/stand/lib/netif/i82557.c
+++ sys/arch/i386/stand/lib/netif/i82557.c
@@ -1,4 +1,4 @@
-/* $NetBSD: i82557.c,v 1.9 2007/03/04 06:00:02 christos Exp $ */
+/* $NetBSD: i82557.c,v 1.10 2008/02/11 13:51:35 mlelstv Exp $ */
 
 /*
  * Copyright (c) 1998, 1999
@@ -413,7 +413,7 @@ EtherReceive(pkt, maxlen)
 	if (rfa->rfa_status & FXP_RFA_STATUS_C) {
 		len = rfa->actual_size & 0x7ff;
 		if (len <= maxlen) {
-			memcpy(pkt, (void *) rfa + RFA_SIZE, maxlen);
+			memcpy(pkt, (char *) rfa + RFA_SIZE, maxlen);
 #if 0
 			printf("rfa status=%x, len=%x\n",
 			       rfa->rfa_status, len);
