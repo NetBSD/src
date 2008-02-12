4177fa1babc581caad05220e8bc472365eff8ac9
dyoung <dyoung@NetBSD.org>
2008-02-12 00:53:06 +0000

    #include <sys/evcnt.h> for event counters.

--- sys/net/if_gre.h
+++ sys/net/if_gre.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: if_gre.h,v 1.33 2008/02/11 22:32:48 dyoung Exp $ */
+/*	$NetBSD: if_gre.h,v 1.34 2008/02/12 00:53:06 dyoung Exp $ */
 
 /*
  * Copyright (c) 1998 The NetBSD Foundation, Inc.
@@ -39,6 +39,7 @@
 #ifndef _NET_IF_GRE_H_
 #define _NET_IF_GRE_H_
 
+#include <sys/evcnt.h>
 #include <sys/queue.h>
 #include <sys/mutex.h>
 #include <sys/condvar.h>
