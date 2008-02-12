a1cfb17b3839b083cd4b3b9ed89f34b58f3fa941
he <he>
2008-02-12 10:59:57 +0000

    Need to include <sys/fstypes.h> before <sys/vfs_syscalls.h> to get fhandle_t defined, which is used in a function prototype in the latter.

--- sys/compat/linux/common/linux_fcntl64.c
+++ sys/compat/linux/common/linux_fcntl64.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: linux_fcntl64.c,v 1.2 2008/02/02 21:54:01 dsl Exp $	*/
+/*	$NetBSD: linux_fcntl64.c,v 1.3 2008/02/12 10:59:57 he Exp $	*/
 
 /*-
  * Copyright (c) 1995, 1998, 2000 The NetBSD Foundation, Inc.
@@ -41,13 +41,14 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: linux_fcntl64.c,v 1.2 2008/02/02 21:54:01 dsl Exp $");
+__KERNEL_RCSID(0, "$NetBSD: linux_fcntl64.c,v 1.3 2008/02/12 10:59:57 he Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
 #include <sys/proc.h>
 #include <sys/file.h>
 #include <sys/filedesc.h>
+#include <sys/fstypes.h>
 #include <sys/vfs_syscalls.h>
 #include <sys/conf.h>
 
