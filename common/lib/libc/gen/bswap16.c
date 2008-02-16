c33ddd47f362aceb6c428801228cbe204d524d45
apb <apb@NetBSD.org>
2008-02-16 17:37:13 +0000

    For all files under src/common, as well as related man pages under src/lib/libc, or related headers under src/sys/sys: change u_intNN_t to uintNN_t.

--- common/lib/libc/gen/bswap16.c
+++ common/lib/libc/gen/bswap16.c
@@ -1,4 +1,4 @@
-/*  $NetBSD: bswap16.c,v 1.1 2005/12/20 19:28:51 christos Exp $    */
+/*  $NetBSD: bswap16.c,v 1.2 2008/02/16 17:37:13 apb Exp $    */
 
 /*
  * Written by Manuel Bouyer <bouyer@NetBSD.org>.
@@ -7,7 +7,7 @@
 
 #include <sys/cdefs.h>
 #if defined(LIBC_SCCS) && !defined(lint)
-__RCSID("$NetBSD: bswap16.c,v 1.1 2005/12/20 19:28:51 christos Exp $");
+__RCSID("$NetBSD: bswap16.c,v 1.2 2008/02/16 17:37:13 apb Exp $");
 #endif /* LIBC_SCCS and not lint */
 
 #include <sys/types.h>
@@ -15,9 +15,9 @@ __RCSID("$NetBSD: bswap16.c,v 1.1 2005/12/20 19:28:51 christos Exp $");
 
 #undef bswap16
 
-u_int16_t
+uint16_t
 bswap16(x)
-	u_int16_t x;
+	uint16_t x;
 {
 	return ((x << 8) & 0xff00) | ((x >> 8) & 0x00ff);
 }
