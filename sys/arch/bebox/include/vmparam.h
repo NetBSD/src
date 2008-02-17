11bbd2be50458e23067b90e0c5b0b340cdacfebf
kiyohara <kiyohara@NetBSD.org>
2008-02-17 03:57:15 +0000

    USER_SR defines 10, like prep.

--- sys/arch/bebox/include/vmparam.h
+++ sys/arch/bebox/include/vmparam.h
@@ -1,3 +1,4 @@
-/*	$NetBSD: vmparam.h,v 1.14 2003/02/03 17:09:55 matt Exp $	*/
+/*	$NetBSD: vmparam.h,v 1.15 2008/02/17 03:57:15 kiyohara Exp $	*/
 
+#define USER_SR		10
 #include <powerpc/oea/vmparam.h>
