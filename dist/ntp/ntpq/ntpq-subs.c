da9b96007e3977d83d6aebc86d8f38a2ec1c43e5
matt <matt@NetBSD.org>
2008-02-20 18:16:09 +0000

    Fix a new bugs that -combine found

--- dist/ntp/ntpq/ntpq-subs.c
+++ dist/ntp/ntpq/ntpq-subs.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: ntpq-subs.c,v 1.1.1.2 2007/06/24 15:50:25 kardel Exp $	*/
+/*	$NetBSD: ntpq-subs.c,v 1.2 2008/02/20 18:16:09 matt Exp $	*/
 
 /*
  * ntpq_ops.c - subroutines which are called to perform operations by ntpq
@@ -12,7 +12,7 @@
 #include "ntpq.h"
 #include "ntp_stdlib.h"
 
-extern char *	chosts[];
+extern const char *	chosts[];
 extern char currenthost[];
 extern int	numhosts;
 int 	maxhostlen;
