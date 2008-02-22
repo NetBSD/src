d9ffad86525b097c80654ffa6eb75cca465c69ea
drochner <drochner@NetBSD.org>
2008-02-22 19:47:06 +0000

    Add a horrible hack to allow the DRM driver to get at the bus_space_handle allocated by the AGP driver in the case of Intel chipset graphics. This is different from the patch by Yorick Hardy circulated a while ago in that it doesn't change the semantics of reference counting within the (3rd-party) DRM code -- it just bypasses it. Needless to say that it is uglier, but it eases future updates to the DRM code because the change there is just 2 LOC.
    Imo, a cleaner solution for all that would be to attach "agp" to "vga"
    in the intel chipset graphics case, which better reflects how the hardware
    is structured. This would still need a hack to the DRM code, but
    it would be confined to childs of the "vga" device, without need for
    global variables.
    Since there is a variety of intel chipsets with AGP and/or builtin
    graphics options, this would need a considerable testing effort.

--- sys/dev/pci/agpvar.h
+++ sys/dev/pci/agpvar.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: agpvar.h,v 1.15 2007/08/04 09:33:05 kiyohara Exp $	*/
+/*	$NetBSD: agpvar.h,v 1.16 2008/02/22 19:47:06 drochner Exp $	*/
 
 /*-
  * Copyright (c) 2000 Doug Rabson
@@ -263,5 +263,11 @@ int agp_unbind_memory(void *, void *);
  * agp_alloc_memory().
  */
 void agp_memory_info(void *, void *, struct agp_memory_info *);
+
+/*
+ * XXX horrible hack to allow drm code to use our mapping
+ * of VGA chip registers
+ */
+int agp_i810_borrow(bus_addr_t, bus_space_handle_t *);
 
 #endif /* !_PCI_AGPPRIV_H_ */
