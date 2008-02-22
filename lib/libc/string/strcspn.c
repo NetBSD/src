6b1eeeaaea1613db482236f6d48b48bd3983461a
joerg <joerg@NetBSD.org>
2008-02-22 13:32:19 +0000

    Replace O(nm) versions of strspn, strcspn and strpbrk with O(n+m) implementations based on the suggestions I made for DragonFly's libc ages ago. For charset with more than one entry and iterating over the first two characters of s, this is consistently faster on amd64.

--- lib/libc/string/strcspn.c
+++ lib/libc/string/strcspn.c
@@ -1,11 +1,8 @@
-/*	$NetBSD: strcspn.c,v 1.10 2003/08/07 16:43:50 agc Exp $	*/
+/*	$NetBSD: strcspn.c,v 1.11 2008/02/22 13:32:19 joerg Exp $	*/
 
 /*-
- * Copyright (c) 1990, 1993
- *	The Regents of the University of California.  All rights reserved.
- *
- * This code is derived from software contributed to Berkeley by
- * Chris Torek.
+ * Copyright (c) 2008 Joerg Sonnenberger
+ * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -15,60 +12,44 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. Neither the name of the University nor the names of its contributors
- *    may be used to endorse or promote products derived from this software
- *    without specific prior written permission.
  *
- * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR(S) ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR(S) BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
 #include <sys/cdefs.h>
-#if defined(LIBC_SCCS) && !defined(lint)
-#if 0
-static char sccsid[] = "@(#)strcspn.c	8.1 (Berkeley) 6/4/93";
-#else
-__RCSID("$NetBSD: strcspn.c,v 1.10 2003/08/07 16:43:50 agc Exp $");
-#endif
-#endif /* LIBC_SCCS and not lint */
+__RCSID("$NetBSD: strcspn.c,v 1.11 2008/02/22 13:32:19 joerg Exp $");
 
 #include <assert.h>
+#include <inttypes.h>
 #include <string.h>
 
-/*
- * Span the complement of string s2.
- */
 size_t
-strcspn(s1, s2)
-	const char *s1;
-	const char *s2;
+strcspn(const char *s, const char *charset)
 {
-	const char *p, *spanp;
-	char c, sc;
+	static const size_t idx[8] = { 1, 2, 4, 8, 16, 32, 64, 128 };
+	const char *t;
+	uint8_t set[32];
 
-	_DIAGASSERT(s1 != NULL);
-	_DIAGASSERT(s2 != NULL);
+	_DIAGASSERT(s != NULL);
+	_DIAGASSERT(charset != NULL);
 
-	/*
-	 * Stop as soon as we find any character from s2.  Note that there
-	 * must be a NUL in s2; it suffices to stop when we find that, too.
-	 */
-	for (p = s1;;) {
-		c = *p++;
-		spanp = s2;
-		do {
-			if ((sc = *spanp++) == c)
-				return (p - 1 - s1);
-		} while (sc != 0);
+	memset(set, 0, sizeof(set));
+
+	for (; *charset != '\0'; ++charset)
+		set[*charset >> 3] |= idx[*charset & 7];
+
+	for (t = s; *t != '\0'; ++t) {
+		if (set[*s >> 3] & idx[*s & 7])
+			break;
 	}
-	/* NOTREACHED */
+	return t - s;
 }
