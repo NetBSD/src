6ee9ace370c5c5ac2b8ffdccec080e337f707d24
manu <manu@NetBSD.org>
2008-02-25 20:06:55 +0000

    From Brian Haley <brian.haley@hp.com> There's a cut/paste error in cmp_aproppair_i(), it's supposed to be checking spi_size but it's not.  I'm not sure this patch is correct, but what's there isn't either.

--- crypto/dist/ipsec-tools/src/racoon/ipsec_doi.c
+++ crypto/dist/ipsec-tools/src/racoon/ipsec_doi.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: ipsec_doi.c,v 1.33 2008/02/10 12:11:08 spz Exp $	*/
+/*	$NetBSD: ipsec_doi.c,v 1.34 2008/02/25 20:06:55 manu Exp $	*/
 
 /* Id: ipsec_doi.c,v 1.55 2006/08/17 09:20:41 vanhu Exp */
 
@@ -1062,10 +1062,10 @@ cmp_aproppair_i(a, b)
 			return -1;
 		}
 
-		if (p->prop->proto_id != r->prop->proto_id) {
+		if (p->prop->spi_size != r->prop->spi_size) {
 			plog(LLV_ERROR, LOCATION, NULL,
 				"invalid spi size: %d.\n",
-				p->prop->proto_id);
+				p->prop->spi_size);
 			return -1;
 		}
 
