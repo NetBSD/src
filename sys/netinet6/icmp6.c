c9395522091aedfc290226b8539f83138cf82118
matt <matt@NetBSD.org>
2008-02-27 19:40:56 +0000

    Convert to ansi definitions from old-style definitons. Remember that func() is not ansi, func(void) is.

--- sys/netinet6/icmp6.c
+++ sys/netinet6/icmp6.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: icmp6.c,v 1.141 2007/12/04 10:27:33 dyoung Exp $	*/
+/*	$NetBSD: icmp6.c,v 1.142 2008/02/27 19:40:56 matt Exp $	*/
 /*	$KAME: icmp6.c,v 1.217 2001/06/20 15:03:29 jinmei Exp $	*/
 
 /*
@@ -62,7 +62,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: icmp6.c,v 1.141 2007/12/04 10:27:33 dyoung Exp $");
+__KERNEL_RCSID(0, "$NetBSD: icmp6.c,v 1.142 2008/02/27 19:40:56 matt Exp $");
 
 #include "opt_inet.h"
 #include "opt_ipsec.h"
@@ -171,7 +171,7 @@ static void icmp6_redirect_timeout(struct rtentry *, struct rttimer *);
 
 
 void
-icmp6_init()
+icmp6_init(void)
 {
 	mld_init();
 	icmp6_mtudisc_timeout_q = rt_timer_queue_create(pmtu_expire);
