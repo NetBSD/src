fa7474025b25ec1d59ea08b3cbcfcc78f644c9a0
ad <ad@NetBSD.org>
2008-02-27 16:37:31 +0000

    +#define _KERNEL +#include <sys/types.h> +#undef _KERNEL

--- usr.bin/sockstat/sockstat.c
+++ usr.bin/sockstat/sockstat.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: sockstat.c,v 1.11 2007/12/26 16:01:38 ad Exp $ */
+/*	$NetBSD: sockstat.c,v 1.12 2008/02/27 16:37:31 ad Exp $ */
 
 /*
  * Copyright (c) 2005 The NetBSD Foundation, Inc.
@@ -34,9 +34,12 @@
 
 #include <sys/cdefs.h>
 #ifndef lint
-__RCSID("$NetBSD: sockstat.c,v 1.11 2007/12/26 16:01:38 ad Exp $");
+__RCSID("$NetBSD: sockstat.c,v 1.12 2008/02/27 16:37:31 ad Exp $");
 #endif
 
+#define _KERNEL
+#include <sys/types.h>
+#undef _KERNEL
 #include <sys/param.h>
 #include <sys/sysctl.h>
 #include <sys/socket.h>
