d55ea8d31593ad5b4a669890bb5db02504fcecb9
matt <matt@NetBSD.org>
2008-02-28 04:55:47 +0000

    constify dkdriver

--- sys/kern/subr_disk.c
+++ sys/kern/subr_disk.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: subr_disk.c,v 1.91 2008/01/31 18:30:55 dyoung Exp $	*/
+/*	$NetBSD: subr_disk.c,v 1.92 2008/02/28 04:55:47 matt Exp $	*/
 
 /*-
  * Copyright (c) 1996, 1997, 1999, 2000 The NetBSD Foundation, Inc.
@@ -74,7 +74,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: subr_disk.c,v 1.91 2008/01/31 18:30:55 dyoung Exp $");
+__KERNEL_RCSID(0, "$NetBSD: subr_disk.c,v 1.92 2008/02/28 04:55:47 matt Exp $");
 
 #include <sys/param.h>
 #include <sys/kernel.h>
@@ -185,7 +185,7 @@ disk_find(const char *name)
 }
 
 void
-disk_init(struct disk *diskp, const char *name, struct dkdriver *driver)
+disk_init(struct disk *diskp, const char *name, const struct dkdriver *driver)
 {
 
 	/*
