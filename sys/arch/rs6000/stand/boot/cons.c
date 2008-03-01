bdd66b06966c8a64e852fa59b4e67c1a75f6e713
tsutsui <tsutsui@NetBSD.org>
2008-03-01 05:21:50 +0000

    cnscan() should return -1 (no input) if there is no console device.

--- sys/arch/rs6000/stand/boot/cons.c
+++ sys/arch/rs6000/stand/boot/cons.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: cons.c,v 1.1 2007/12/17 19:09:47 garbled Exp $	*/
+/*	$NetBSD: cons.c,v 1.2 2008/03/01 05:21:50 tsutsui Exp $	*/
 
 /*
  * Copyright (c) 1990, 1993
@@ -151,7 +151,7 @@ cnscan(void)
 
 	if (cn_tab)
 		return ((*cn_tab->cn_scan)(cn_tab->cn_dev));
-	return (0);
+	return -1;
 }
 
 #ifdef CONS_SERIAL
