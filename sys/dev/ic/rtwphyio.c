bc44052e717c7b650f973dd712a75fa9af2f6781
tsutsui <tsutsui>
2008-03-03 12:30:57 +0000

    Explicitly include <sys/device.h> for device_t.

--- sys/dev/ic/rtwphyio.c
+++ sys/dev/ic/rtwphyio.c
@@ -1,4 +1,4 @@
-/* $NetBSD: rtwphyio.c,v 1.15 2007/10/19 12:00:00 ad Exp $ */
+/* $NetBSD: rtwphyio.c,v 1.16 2008/03/03 12:30:57 tsutsui Exp $ */
 /*-
  * Copyright (c) 2004, 2005 David Young.  All rights reserved.
  *
@@ -35,11 +35,12 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: rtwphyio.c,v 1.15 2007/10/19 12:00:00 ad Exp $");
+__KERNEL_RCSID(0, "$NetBSD: rtwphyio.c,v 1.16 2008/03/03 12:30:57 tsutsui Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
 #include <sys/types.h>
+#include <sys/device.h>
 
 #include <sys/bus.h>
 
