7ed84929f867960acd251bedabc8d0ac39984b16
vanhu <vanhu@NetBSD.org>
2008-03-06 17:00:03 +0000

    Generates a log if cert validation has been disabled by configuration

--- crypto/dist/ipsec-tools/src/racoon/oakley.c
+++ crypto/dist/ipsec-tools/src/racoon/oakley.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: oakley.c,v 1.11 2007/04/04 13:07:31 vanhu Exp $	*/
+/*	$NetBSD: oakley.c,v 1.12 2008/03/06 17:00:03 vanhu Exp $	*/
 
 /* Id: oakley.c,v 1.32 2006/05/26 12:19:46 manubsd Exp */
 
@@ -1452,8 +1452,15 @@ oakley_validate_auth(iph1)
 				return ISAKMP_NTYPE_INVALID_CERT_AUTHORITY;
 			}
 		}
-
-		plog(LLV_DEBUG, LOCATION, NULL, "CERT validated\n");
+	
+		/* Generate a warning if verify_cert == 0
+		 */
+		if (iph1->rmconf->verify_cert){
+			plog(LLV_DEBUG, LOCATION, NULL, "CERT validated\n");
+		}else{
+			plog(LLV_WARNING, LOCATION, NULL,
+				"CERT validation disabled by configuration\n");
+		}
 
 		/* compute hash */
 		switch (iph1->etype) {
