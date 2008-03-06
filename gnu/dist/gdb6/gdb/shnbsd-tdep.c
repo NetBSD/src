79124eec32d3e902be14f40d898a07d90136d132
skrll <skrll@NetBSD.org>
2008-03-06 19:03:17 +0000

    Support siginfo stack frame detection and unwind.

--- gnu/dist/gdb6/gdb/shnbsd-tdep.c
+++ gnu/dist/gdb6/gdb/shnbsd-tdep.c
@@ -27,6 +27,9 @@
 #include "value.h"
 #include "osabi.h"
 
+#include "trad-frame.h"
+#include "tramp-frame.h"
+
 #include "gdb_assert.h"
 #include "gdb_string.h"
 
@@ -60,6 +63,34 @@ static const int regmap[] =
 
 /* Sizeof `struct reg' in <machine/reg.h>.  */
 #define SHNBSD_SIZEOF_GREGS	(21 * 4)
+
+/* From <machine/mcontext.h>.  */
+static const int shnbsd_mc_reg_offset[] =
+{
+  (20 * 4),	/* r0 */
+  (19 * 4),	/* r1 */
+  (18 * 4),	/* r2 */ 
+  (17 * 4),	/* r3 */ 
+  (16 * 4),	/* r4 */
+  (15 * 4),	/* r5 */
+  (14 * 4),	/* r6 */
+  (13 * 4),	/* r7 */
+  (12 * 4),	/* r8 */ 
+  (11 * 4),	/* r9 */
+  (10 * 4),	/* r10 */
+  ( 9 * 4),	/* r11 */
+  ( 8 * 4),	/* r12 */
+  ( 7 * 4),	/* r13 */
+  ( 6 * 4),	/* r14 */
+  (21 * 4),	/* r15/sp */
+  ( 1 * 4),	/* pc */
+  ( 5 * 4),	/* pr */
+  -1,
+  -1,
+  ( 4 * 4),	/* mach */
+  ( 3 * 4),	/* macl */
+  ( 2 * 4),	/* sr */
+};
 
 /* Supply register REGNUM from the buffer specified by GREGS and LEN
    in the general-purpose register set REGSET to register cache
@@ -375,6 +406,69 @@ shnbsd_fill_reg (const struct regcache *regcache, char *regs, int regnum)
 			  regs, SHNBSD_SIZEOF_GREGS);
 }
 
+static void
+shnbsd_sigtramp_cache_init (const struct tramp_frame *,
+			     struct frame_info *,
+			     struct trad_frame_cache *,
+			     CORE_ADDR);
+
+/* The siginfo signal trampoline for NetBSD/sh3 versions 2.0 and later */
+static const struct tramp_frame shnbsd_sigtramp_si2 =
+{
+  SIGTRAMP_FRAME,
+  2,
+  {
+    { 0x64f3, -1 },			/* mov     r15,r4 */
+    { 0xd002, -1 },			/* mov.l   .LSYS_setcontext */
+    { 0xc380, -1 },			/* trapa   #-128 */
+    { 0xa003, -1 },			/* bra     .Lskip1 */
+    { 0x0009, -1 },			/* nop */
+    { 0x0009, -1 },			/* nop */
+ /* .LSYS_setcontext */
+    { 0x0134, -1 }, { 0x0000, -1 },     /* 0x134 */
+ /* .Lskip1 */
+    { 0x6403, -1 },			/* mov     r0,r4 */
+    { 0xd002, -1 },			/* mov.l   .LSYS_exit  */
+    { 0xc380, -1 },			/* trapa   #-128 */
+    { 0xa003, -1 },			/* bra     .Lskip2 */
+    { 0x0009, -1 },			/* nop */
+    { 0x0009, -1 },			/* nop */
+ /* .LSYS_exit */
+    { 0x0001, -1 }, { 0x0000, -1 },     /* 0x1 */
+/* .Lskip2 */
+    { TRAMP_SENTINEL_INSN, -1 }
+  },
+  shnbsd_sigtramp_cache_init
+};
+
+static void
+shnbsd_sigtramp_cache_init (const struct tramp_frame *self,
+			     struct frame_info *next_frame,
+			     struct trad_frame_cache *this_cache,
+			     CORE_ADDR func)
+{
+  struct gdbarch *gdbarch = get_frame_arch (next_frame);
+  struct gdbarch_tdep *tdep = gdbarch_tdep (gdbarch);
+  CORE_ADDR sp = frame_unwind_register_unsigned (next_frame, SP_REGNUM);
+  CORE_ADDR base;
+  const int *reg_offset;
+  int num_regs;
+  int i;
+
+  reg_offset = shnbsd_mc_reg_offset;
+  num_regs = ARRAY_SIZE (shnbsd_mc_reg_offset);
+  /* SP already points at the ucontext. */
+  base = sp;
+  /* offsetof(ucontext_t, uc_mcontext) == 36 */
+  base += 36;
+
+  for (i = 0; i < num_regs; i++)
+    if (reg_offset[i] != -1)
+      trad_frame_set_reg_addr (this_cache, i, base + reg_offset[i]);
+
+  /* Construct the frame ID using the function start.  */
+  trad_frame_set_id (this_cache, frame_id_build (sp, func));
+}
 
 static void
 shnbsd_init_abi (struct gdbarch_info info,
@@ -389,6 +483,8 @@ shnbsd_init_abi (struct gdbarch_info info,
 
   set_solib_svr4_fetch_link_map_offsets (gdbarch,
 		                nbsd_ilp32_solib_svr4_fetch_link_map_offsets);
+  tramp_frame_prepend_unwinder (gdbarch, &shnbsd_sigtramp_si2);
+
 }
 
 static enum gdb_osabi
