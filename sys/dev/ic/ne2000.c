cf80f73d174c04bb807aa0d53564257478145720
cube <cube@NetBSD.org>
2008-03-12 14:31:11 +0000

    Split device_t and softc for the NE2000 Ethernet chip and all its variants and attachments.  Use device_t accessors, correct types, and ANSIfy when appropriate.

--- sys/dev/ic/ne2000.c
+++ sys/dev/ic/ne2000.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: ne2000.c,v 1.56 2008/03/09 20:32:45 dholland Exp $	*/
+/*	$NetBSD: ne2000.c,v 1.57 2008/03/12 14:31:11 cube Exp $	*/
 
 /*-
  * Copyright (c) 1997, 1998 The NetBSD Foundation, Inc.
@@ -55,7 +55,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: ne2000.c,v 1.56 2008/03/09 20:32:45 dholland Exp $");
+__KERNEL_RCSID(0, "$NetBSD: ne2000.c,v 1.57 2008/03/12 14:31:11 cube Exp $");
 
 #include "opt_ipkdb.h"
 
@@ -136,7 +136,7 @@ ne2000_attach(nsc, myea)
 	switch (nsc->sc_type) {
 	case NE2000_TYPE_UNKNOWN:
 	default:
-		printf("%s: where did the card go?\n", dsc->sc_dev.dv_xname);
+		aprint_error_dev(dsc->sc_dev, "where did the card go?\n");
 		return (1);
 	case NE2000_TYPE_NE1000:
 		memsize = 8192;
@@ -295,7 +295,7 @@ ne2000_attach(nsc, myea)
 		dsc->sc_media_init = dp8390_media_init;
 
 	if (dp8390_config(dsc)) {
-		printf("%s: setup failed\n", dsc->sc_dev.dv_xname);
+		aprint_error_dev(dsc->sc_dev, "setup failed\n");
 		return (1);
 	}
 
@@ -642,7 +642,7 @@ ne2000_write_mbuf(sc, m, buf)
 	if (maxwait == 0) {
 		log(LOG_WARNING,
 		    "%s: remote transmit DMA failed to complete\n",
-		    sc->sc_dev.dv_xname);
+		    device_xname(sc->sc_dev));
 		dp8390_reset(sc);
 	}
 
