9232d07175903aa98daec91d03908549ff8960fc
he <he@NetBSD.org>
2008-03-16 19:54:11 +0000

    Patterned after the other ports, define _MACHINE and MACHINE before including <arm/arm32/param.h>.  Fixes build problem for rump.

--- sys/arch/iyonix/include/param.h
+++ sys/arch/iyonix/include/param.h
@@ -1,11 +1,11 @@
-/*	$NetBSD: param.h,v 1.2 2005/12/11 12:17:51 christos Exp $	*/
+/*	$NetBSD: param.h,v 1.3 2008/03/16 19:54:11 he Exp $	*/
 
 #ifndef _IYONIX_PARAM_H_
 #define _IYONIX_PARAM_H_
 
-#include <arm/arm32/param.h>
-
 #define	_MACHINE	iyonix
 #define	MACHINE		"iyonix"
+
+#include <arm/arm32/param.h>
 
 #endif /* _IYONIX_PARAM_H_ */
