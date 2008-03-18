c2958911065c50c93e7d5692234193aba30fdfaa
dogcow <dogcow>
2008-03-18 05:05:35 +0000

    pesky missing spaces.

--- sys/arch/sparc/dev/pckbc_js.c
+++ sys/arch/sparc/dev/pckbc_js.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: pckbc_js.c,v 1.17 2008/03/15 13:23:24 cube Exp $ */
+/*	$NetBSD: pckbc_js.c,v 1.18 2008/03/18 05:05:35 dogcow Exp $ */
 
 /*
  * Copyright (c) 2002 Valeriy E. Ushakov
@@ -28,7 +28,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: pckbc_js.c,v 1.17 2008/03/15 13:23:24 cube Exp $");
+__KERNEL_RCSID(0, "$NetBSD: pckbc_js.c,v 1.18 2008/03/18 05:05:35 dogcow Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -88,7 +88,7 @@ pckbc_obio_match(device_t parent, cfdata_t cf, void *aux)
 }
 
 static int
-pckbc_ebus_match(device_tparent, cfdata_t cf, void *aux)
+pckbc_ebus_match(device_t parent, cfdata_t cf, void *aux)
 {
 	struct ebus_attach_args *ea = aux;
 
