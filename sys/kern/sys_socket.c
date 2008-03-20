bb5d935d92b2f5270cba74c6df2d2e19beb2181c
ad <ad>
2008-03-20 19:23:15 +0000

    - Extract the guts of soo_poll() into sopoll(), which takes a struct socket *.   This is for netsmb which wants to poll sockets directly. - When polling a socket, first check for pending I/O without acquring any   locks. If no I/O seems to be pending, acquire locks/spl and check again   doing selrecord() if necessary.

--- sys/kern/sys_socket.c
+++ sys/kern/sys_socket.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: sys_socket.c,v 1.53 2008/02/27 19:56:33 matt Exp $	*/
+/*	$NetBSD: sys_socket.c,v 1.54 2008/03/20 19:23:15 ad Exp $	*/
 
 /*
  * Copyright (c) 1982, 1986, 1990, 1993
@@ -32,7 +32,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: sys_socket.c,v 1.53 2008/02/27 19:56:33 matt Exp $");
+__KERNEL_RCSID(0, "$NetBSD: sys_socket.c,v 1.54 2008/03/20 19:23:15 ad Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -188,41 +188,8 @@ soo_fcntl(struct file *fp, u_int cmd, void *data, struct lwp *l)
 int
 soo_poll(struct file *fp, int events, struct lwp *l)
 {
-	struct socket *so = (struct socket *)fp->f_data;
-	int revents = 0;
-	int s;
 
-	KERNEL_LOCK(1, curlwp);
-	s = splsoftnet();
-
-	if (events & (POLLIN | POLLRDNORM))
-		if (soreadable(so))
-			revents |= events & (POLLIN | POLLRDNORM);
-
-	if (events & (POLLOUT | POLLWRNORM))
-		if (sowritable(so))
-			revents |= events & (POLLOUT | POLLWRNORM);
-
-	if (events & (POLLPRI | POLLRDBAND))
-		if (so->so_oobmark || (so->so_state & SS_RCVATMARK))
-			revents |= events & (POLLPRI | POLLRDBAND);
-
-	if (revents == 0) {
-		if (events & (POLLIN | POLLPRI | POLLRDNORM | POLLRDBAND)) {
-			selrecord(l, &so->so_rcv.sb_sel);
-			so->so_rcv.sb_flags |= SB_SEL;
-		}
-
-		if (events & (POLLOUT | POLLWRNORM)) {
-			selrecord(l, &so->so_snd.sb_sel);
-			so->so_snd.sb_flags |= SB_SEL;
-		}
-	}
-
-	splx(s);
-	KERNEL_UNLOCK_ONE(curlwp);
-
-	return revents;
+	return sopoll(fp->f_data, events);
 }
 
 int
