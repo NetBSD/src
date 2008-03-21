b0d3ffb19f50843431b8ff4a07674c61af6c1424
plunky <plunky>
2008-03-21 17:59:57 +0000

    convert more insque/remque style queuing to use a queue(3) TAILQ, this necessitates using a flag to indicate vc_open instead of abusing the queue pointer but apart from that there is no functional difference.

--- sys/coda/cnode.h
+++ sys/coda/cnode.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: cnode.h,v 1.16 2007/03/04 06:01:11 christos Exp $	*/
+/*	$NetBSD: cnode.h,v 1.17 2008/03/21 17:59:57 plunky Exp $	*/
 
 /*
  *
@@ -53,11 +53,6 @@
 
 MALLOC_DECLARE(M_CODA);
 #endif
-
-/*
- * tmp below since we need struct queue
- */
-#include <coda/coda_kernel.h>
 
 /*
  * Cnode lookup stuff.
@@ -129,16 +124,19 @@ struct cnode {
 #define VALID_SYMLINK(cp)	((cp->c_flags) & C_SYMLINK)
 #define IS_UNMOUNTING(cp)	((cp)->c_flags & C_UNMOUNTING)
 
+struct vmsg;
+
 struct vcomm {
-	u_long		vc_seq;
-	struct selinfo	vc_selproc;
-	struct queue	vc_requests;
-	struct queue	vc_replys;
+	u_long			vc_seq;
+	int			vc_open;
+	struct selinfo		vc_selproc;
+	TAILQ_HEAD(,vmsg)	vc_requests;
+	TAILQ_HEAD(,vmsg)	vc_replies;
 };
 
-#define	VC_OPEN(vcp)	    ((vcp)->vc_requests.forw != NULL)
-#define MARK_VC_CLOSED(vcp) (vcp)->vc_requests.forw = NULL;
-#define MARK_VC_OPEN(vcp)    /* MT */
+#define VC_OPEN(vcp)		((vcp)->vc_open == 1)
+#define MARK_VC_CLOSED(vcp)	((vcp)->vc_open = 0)
+#define MARK_VC_OPEN(vcp)	((vcp)->vc_open = 1)
 
 struct coda_clstat {
 	int	ncalls;			/* client requests */
