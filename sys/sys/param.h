ea83aa5dbcb59ad22250fff6082c09b424931fa7
ad <ad@NetBSD.org>
2008-03-21 21:56:53 +0000

    4.99.56 - file descriptor and kqueue changes

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.308 2008/03/17 08:20:05 yamt Exp $	*/
+/*	$NetBSD: param.h,v 1.309 2008/03/21 21:56:53 ad Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -63,7 +63,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	499005500	/* NetBSD 4.99.55 */
+#define	__NetBSD_Version__	499005600	/* NetBSD 4.99.56 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
