9adb9a3a7f7f7be2aea8cb5844915d6e884851fa
ad <ad@NetBSD.org>
2008-03-21 21:35:43 +0000

    Move pthread__errno() into pthread_specific.c so it gets the "no stack frame" treatment.

--- lib/libpthread/pthread_specific.c
+++ lib/libpthread/pthread_specific.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: pthread_specific.c,v 1.17 2008/01/08 20:55:58 christos Exp $	*/
+/*	$NetBSD: pthread_specific.c,v 1.18 2008/03/21 21:35:43 ad Exp $	*/
 
 /*-
  * Copyright (c) 2001, 2007 The NetBSD Foundation, Inc.
@@ -37,7 +37,7 @@
  */
 
 #include <sys/cdefs.h>
-__RCSID("$NetBSD: pthread_specific.c,v 1.17 2008/01/08 20:55:58 christos Exp $");
+__RCSID("$NetBSD: pthread_specific.c,v 1.18 2008/03/21 21:35:43 ad Exp $");
 
 /* Functions and structures dealing with thread-specific data */
 
@@ -86,4 +86,11 @@ pthread_curcpu_np(void)
 		return 0;
 
 	return cpu;
+}
+
+int *
+pthread__errno(void)
+{
+
+	return &(pthread__self()->pt_errno);
 }
