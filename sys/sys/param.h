56af15c0435aad2a22515c775948232da4234788
ad <ad@NetBSD.org>
2008-03-22 18:05:13 +0000

    4.99.57 - struct selinfo changed

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.309 2008/03/21 21:56:53 ad Exp $	*/
+/*	$NetBSD: param.h,v 1.310 2008/03/22 18:05:13 ad Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -63,7 +63,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	499005600	/* NetBSD 4.99.56 */
+#define	__NetBSD_Version__	499005700	/* NetBSD 4.99.57 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
