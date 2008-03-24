44650ee919054d7e755c175685e588ea9324d9c8
yamt <yamt@NetBSD.org>
2008-03-24 12:26:30 +0000

    4.99.58 - merge yamt-lazymbuf branch

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.310 2008/03/22 18:05:13 ad Exp $	*/
+/*	$NetBSD: param.h,v 1.311 2008/03/24 12:26:30 yamt Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -63,7 +63,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	499005700	/* NetBSD 4.99.57 */
+#define	__NetBSD_Version__	499005800	/* NetBSD 4.99.58 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
