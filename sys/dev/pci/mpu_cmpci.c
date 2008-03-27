b850b7d60784c0ab82229ac43772ba35b6e041b4
xtraeme <xtraeme>
2008-03-27 10:22:00 +0000

    Split device_t/softc for mpu(4) and its attachments, plus other related cosmetic changes.
    Reviewed by cube@.

--- sys/dev/pci/mpu_cmpci.c
+++ sys/dev/pci/mpu_cmpci.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: mpu_cmpci.c,v 1.13 2007/10/19 12:00:52 ad Exp $	*/
+/*	$NetBSD: mpu_cmpci.c,v 1.14 2008/03/27 10:22:01 xtraeme Exp $	*/
 
 /*
  * Copyright (c) 1998 The NetBSD Foundation, Inc.
@@ -37,7 +37,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: mpu_cmpci.c,v 1.13 2007/10/19 12:00:52 ad Exp $");
+__KERNEL_RCSID(0, "$NetBSD: mpu_cmpci.c,v 1.14 2008/03/27 10:22:01 xtraeme Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -60,34 +60,35 @@ __KERNEL_RCSID(0, "$NetBSD: mpu_cmpci.c,v 1.13 2007/10/19 12:00:52 ad Exp $");
 #include <dev/pci/cmpcivar.h>
 
 static int
-mpu_cmpci_match(struct device *parent, struct cfdata *match, void *aux)
+mpu_cmpci_match(device_t parent, cfdata_t match, void *aux)
 {
-	struct audio_attach_args *aa = (struct audio_attach_args *)aux;
-	struct cmpci_softc *ysc = (struct cmpci_softc *)parent;
+	struct audio_attach_args *aa = aux;
+	struct cmpci_softc *ysc = device_private(parent);
 	struct mpu_softc sc;
 
 	if (aa->type != AUDIODEV_TYPE_MPU)
-		return (0);
+		return 0;
 	memset(&sc, 0, sizeof sc);
 	sc.iot = ysc->sc_iot;
 	sc.ioh = ysc->sc_mpu_ioh;
-	return (mpu_find(&sc));
+	return mpu_find(&sc);
 }
 
 static void
-mpu_cmpci_attach(struct device *parent, struct device *self, void *aux)
+mpu_cmpci_attach(device_t parent, device_t self, void *aux)
 {
-	struct cmpci_softc *ysc = (struct cmpci_softc *)parent;
-	struct mpu_softc *sc = (struct mpu_softc *)self;
+	struct cmpci_softc *ysc = device_private(parent);
+	struct mpu_softc *sc = device_private(self);
 
-	printf("\n");
+	aprint_normal("\n");
 
 	sc->iot = ysc->sc_iot;
 	sc->ioh = ysc->sc_mpu_ioh;
 	sc->model = "CMPCI MPU-401 MIDI UART";
+	sc->sc_dev = self;
 
 	mpu_attach(sc);
 }
 
-CFATTACH_DECL(mpu_cmpci, sizeof (struct mpu_softc),
+CFATTACH_DECL_NEW(mpu_cmpci, sizeof (struct mpu_softc),
     mpu_cmpci_match, mpu_cmpci_attach, NULL, NULL);
