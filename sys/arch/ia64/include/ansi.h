12f9b697ad381a3d658146d8a4b3f6868962192c
christos <christos@NetBSD.org>
2008-03-30 21:01:59 +0000

    time_t is now __int64_t

--- sys/arch/ia64/include/ansi.h
+++ sys/arch/ia64/include/ansi.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: ansi.h,v 1.5.18.1 2008/03/29 20:46:57 christos Exp $	*/
+/*	$NetBSD: ansi.h,v 1.5.18.2 2008/03/30 21:01:59 christos Exp $	*/
 
 /*-
  * Copyright (c) 1990, 1993
@@ -53,7 +53,7 @@
 #define	_BSD_PTRDIFF_T_		long int	/* ptr1 - ptr2 */
 #define	_BSD_SIZE_T_		unsigned long int /* sizeof() */
 #define	_BSD_SSIZE_T_		long int	/* byte count or error */
-#define	_BSD_TIME_T_		long long	/* time() */
+#define	_BSD_TIME_T_		__int64_t	/* time() */
 #else
 #define	_BSD_CLOCK_T_		unsigned long int /* clock() */
 #define	_BSD_PTRDIFF_T_		int		/* ptr1 - ptr2 */
