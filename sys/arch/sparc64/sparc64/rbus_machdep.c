09275ba76df6e5c818c95bf357d4b9e84f7b1261
cegger <cegger@NetBSD.org>
2008-04-05 13:40:05 +0000

    use aprint_*_dev and device_xname OK martin

--- sys/arch/sparc64/sparc64/rbus_machdep.c
+++ sys/arch/sparc64/sparc64/rbus_machdep.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: rbus_machdep.c,v 1.9 2006/02/11 17:57:32 cdi Exp $	*/
+/*	$NetBSD: rbus_machdep.c,v 1.10 2008/04/05 13:40:05 cegger Exp $	*/
 
 /*
  * Copyright (c) 2003 Takeshi Nakayama.
@@ -28,7 +28,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: rbus_machdep.c,v 1.9 2006/02/11 17:57:32 cdi Exp $");
+__KERNEL_RCSID(0, "$NetBSD: rbus_machdep.c,v 1.10 2008/04/05 13:40:05 cegger Exp $");
 
 #include <sys/param.h>
 #include <sys/device.h>
@@ -223,7 +223,7 @@ pccbb_cardbus_isvalid(void *arg)
 	/* check CardBus card is present */
 	sockstat = bus_space_read_4(memt, memh, CB_SOCKET_STAT);
 	DPRINTF("%s: pccbb_cardbus_isvalid: sockstat %08x\n",
-		sc->sc_dev.dv_xname, sockstat);
+		device_xname(&sc->sc_dev), sockstat);
 	if ((sockstat & CB_SOCKET_STAT_CB) == 0 ||
 	    (sockstat & CB_SOCKET_STAT_CD) != 0)
 		return 0;
@@ -231,7 +231,7 @@ pccbb_cardbus_isvalid(void *arg)
 	/* check card is powered on */
 	sockctrl = bus_space_read_4(memt, memh, CB_SOCKET_CTRL);
 	DPRINTF("%s: pccbb_cardbus_isvalid: sockctrl %08x\n",
-		sc->sc_dev.dv_xname, sockctrl);
+		device_xname(&sc->sc_dev), sockctrl);
 	if ((sockctrl & CB_SOCKET_CTRL_VCCMASK) == 0)
 		return 0;
 
