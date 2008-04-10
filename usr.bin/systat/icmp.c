88364dba6207ea5da405c67772717c9e967d0d26
thorpej <thorpej>
2008-04-10 17:16:39 +0000

    Don't even bother initializing the namelist if we're not going to use it.

--- usr.bin/systat/icmp.c
+++ usr.bin/systat/icmp.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: icmp.c,v 1.11 2008/04/10 17:14:25 thorpej Exp $	*/
+/*	$NetBSD: icmp.c,v 1.12 2008/04/10 17:16:39 thorpej Exp $	*/
 
 /*
  * Copyright (c) 1999, 2000 Andrew Doran <ad@NetBSD.org>
@@ -29,7 +29,7 @@
 
 #include <sys/cdefs.h>
 #ifndef lint
-__RCSID("$NetBSD: icmp.c,v 1.11 2008/04/10 17:14:25 thorpej Exp $");
+__RCSID("$NetBSD: icmp.c,v 1.12 2008/04/10 17:16:39 thorpej Exp $");
 #endif /* not lint */
 
 #include <sys/param.h>
@@ -160,14 +160,16 @@ int
 initicmp(void)
 {
 
-	if (namelist[0].n_type == 0) {
-		if (kvm_nlist(kd, namelist)) {
-			nlisterr(namelist);
-			return(0);
-		}
+	if (! use_sysctl) {
 		if (namelist[0].n_type == 0) {
-			error("No namelist");
-			return(0);
+			if (kvm_nlist(kd, namelist)) {
+				nlisterr(namelist);
+				return(0);
+			}
+			if (namelist[0].n_type == 0) {
+				error("No namelist");
+				return(0);
+			}
 		}
 	}
 	
