4bec66d6f55c60ae19805332ff1742cf260709ea
jmcneill <jmcneill@NetBSD.org>
2008-04-11 11:24:41 +0000

    Revert previous.

--- sys/arch/i386/include/acpi_func.h
+++ sys/arch/i386/include/acpi_func.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: acpi_func.h,v 1.8 2008/04/11 02:27:57 jmcneill Exp $	*/
+/*	$NetBSD: acpi_func.h,v 1.9 2008/04/11 11:24:41 jmcneill Exp $	*/
 
 #include <machine/cpufunc.h>
 
@@ -24,6 +24,7 @@ do { \
 	: "=&a" (Acq), "+m" (*GLptr) \
 	: "i" (~1L) \
 	: "edx"); \
+	(Acq) = -1; \
 } while (0)
 
 #define ACPI_RELEASE_GLOBAL_LOCK(GLptr, Acq) \
