336c35ebb20e5ca38e7c60c0a0ea10c19d5e4305
ad <ad@NetBSD.org>
2008-04-11 15:26:24 +0000

    4.99.59 - struct cpu_data changed (namecache changes)

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.313 2008/03/27 18:28:20 ad Exp $	*/
+/*	$NetBSD: param.h,v 1.314 2008/04/11 15:26:24 ad Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -63,7 +63,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	499005800	/* NetBSD 4.99.58 */
+#define	__NetBSD_Version__	499005900	/* NetBSD 4.99.59 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
