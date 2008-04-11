1166064bb99ddc82db091626a6ab58e0076169fb
ad <ad@NetBSD.org>
2008-04-11 15:23:45 +0000

    More stubs.

--- sys/rump/librump/rumpkern/locks.c
+++ sys/rump/librump/rumpkern/locks.c
@@ -1,4 +1,37 @@
-/*	$NetBSD: locks.c,v 1.13 2008/04/01 19:49:31 drochner Exp $	*/
+/*	$NetBSD: locks.c,v 1.14 2008/04/11 15:23:45 ad Exp $	*/
+
+/*-
+ * Copyright (c) 2008 The NetBSD Foundation, Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed by the NetBSD
+ *	Foundation, Inc. and its contributors.
+ * 4. Neither the name of The NetBSD Foundation nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 
 /*
  * Copyright (c) 2007 Antti Kantee.  All Rights Reserved.
@@ -31,6 +64,7 @@
 #include <sys/param.h>
 #include <sys/mutex.h>
 #include <sys/rwlock.h>
+#include <sys/atomic.h>
 
 #include "rump_private.h"
 
@@ -245,4 +279,42 @@ _kernel_unlock(int nlocks, int *countp)
 	mutex_exit(&rump_giantlock);
 	if (countp)
 		*countp = 1;
+}
+
+struct kmutexobj {
+	kmutex_t	mo_lock;
+	u_int		mo_refcnt;
+};
+
+kmutex_t *
+mutex_obj_alloc(kmutex_type_t type, int ipl)
+{
+	struct kmutexobj *mo;
+
+	mo = kmem_alloc(sizeof(*mo), KM_SLEEP);
+	mutex_init(&mo->mo_lock, type, ipl);
+	mo->mo_refcnt = 1;
+
+	return (kmutex_t *)mo;
+}
+
+void
+mutex_obj_hold(kmutex_t *lock)
+{
+	struct kmutexobj *mo = (struct kmutexobj *)lock;
+
+	atomic_inc_uint(&mo->mo_refcnt);
+}
+
+bool
+mutex_obj_free(kmutex_t *lock)
+{
+	struct kmutexobj *mo = (struct kmutexobj *)lock;
+
+	if (atomic_dec_uint_nv(&mo->mo_refcnt) > 0) {
+		return false;
+	}
+	mutex_destroy(&mo->mo_lock);
+	kmem_free(mo, sizeof(*mo));
+	return true;
 }
