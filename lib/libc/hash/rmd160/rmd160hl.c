052fb4c32d3864098911a5372081d0efdec83a89
dholland <dholland@NetBSD.org>
2008-04-13 02:04:31 +0000

    Fix replicated typo

--- lib/libc/hash/rmd160/rmd160hl.c
+++ lib/libc/hash/rmd160/rmd160hl.c
@@ -1,14 +1,14 @@
-/* $NetBSD: rmd160hl.c,v 1.5 2006/10/28 13:05:42 agc Exp $ */
+/* $NetBSD: rmd160hl.c,v 1.6 2008/04/13 02:04:32 dholland Exp $ */
 
 /*
- * Derived from code ritten by Jason R. Thorpe <thorpej@NetBSD.org>,
+ * Derived from code written by Jason R. Thorpe <thorpej@NetBSD.org>,
  * April 29, 1997.
  * Public domain.
  */
 
 #include <sys/cdefs.h>
 #if defined(LIBC_SCCS) && !defined(lint)
-__RCSID("$NetBSD: rmd160hl.c,v 1.5 2006/10/28 13:05:42 agc Exp $");
+__RCSID("$NetBSD: rmd160hl.c,v 1.6 2008/04/13 02:04:32 dholland Exp $");
 #endif /* LIBC_SCCS and not lint */
 
 #define	HASH_ALGORITHM	RMD160
