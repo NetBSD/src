3e6ce8a4311cf94da90995f7c29fda9df295d8e2
tsutsui <tsutsui@NetBSD.org>
2008-04-14 13:41:43 +0000

    Don't make linker warnings fatal on linking static libs since there is no proper way to avoid "FOO is a patented algorithm" warnings. Ok'ed by christos@ on tech-toolchain.

--- share/mk/bsd.sys.mk
+++ share/mk/bsd.sys.mk
@@ -1,4 +1,4 @@
-#	$NetBSD: bsd.sys.mk,v 1.159 2008/03/03 06:33:17 lukem Exp $
+#	$NetBSD: bsd.sys.mk,v 1.160 2008/04/14 13:41:43 tsutsui Exp $
 #
 # Build definitions used for NetBSD source tree builds.
 
@@ -17,7 +17,11 @@ CFLAGS+=	-Wall -Wstrict-prototypes -Wmissing-prototypes -Wpointer-arith
 # we wanted, and now we don't get anymore.
 CFLAGS+=	-Wno-sign-compare -Wno-traditional
 # Set linker warnings to be fatal
+# XXX no proper way to avoid "FOO is a patented algorithm" warnings
+# XXX on linking static libs
+.if (${MKPIC} != "no" && ${LDSTATIC} != "-static")
 LDFLAGS+=	-Wl,--fatal-warnings
+.endif
 .endif
 .if ${WARNS} > 1
 CFLAGS+=	-Wreturn-type -Wswitch -Wshadow
