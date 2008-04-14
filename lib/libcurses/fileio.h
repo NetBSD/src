e501372810164dda7dfb8ceee68a7eda1a66158c
jdc <jdc@NetBSD.org>
2008-04-14 20:40:37 +0000

    File IO functions header file.

---
+++ lib/libcurses/fileio.h
@@ -0,0 +1,8 @@
+/*
+ * Do not edit!  Automatically generated file:
+ *   from: NetBSD: shlib_version,v 1.39 2008/04/14 20:39:58 jdc Exp 
+ *   by  : NetBSD: genfileioh.awk,v 1.1 2008/04/14 20:35:23 jdc Exp 
+ */
+
+#define CURSES_LIB_MAJOR 6
+#define CURSES_LIB_MINOR 4
