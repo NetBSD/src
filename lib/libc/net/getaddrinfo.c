cd3309137b24b7becdd1b59d85367daf430b72ff
lukem <lukem@NetBSD.org>
2008-04-19 07:56:34 +0000

    Add a missing const.

--- lib/libc/net/getaddrinfo.c
+++ lib/libc/net/getaddrinfo.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: getaddrinfo.c,v 1.90 2007/06/29 15:53:21 christos Exp $	*/
+/*	$NetBSD: getaddrinfo.c,v 1.91 2008/04/19 07:56:34 lukem Exp $	*/
 /*	$KAME: getaddrinfo.c,v 1.29 2000/08/31 17:26:57 itojun Exp $	*/
 
 /*
@@ -55,7 +55,7 @@
 
 #include <sys/cdefs.h>
 #if defined(LIBC_SCCS) && !defined(lint)
-__RCSID("$NetBSD: getaddrinfo.c,v 1.90 2007/06/29 15:53:21 christos Exp $");
+__RCSID("$NetBSD: getaddrinfo.c,v 1.91 2008/04/19 07:56:34 lukem Exp $");
 #endif /* LIBC_SCCS and not lint */
 
 #include "namespace.h"
@@ -1477,7 +1477,7 @@ _files_getaddrinfo(void *rv, void *cb_data, va_list ap)
 	FILE *hostf = NULL;
 
 	name = va_arg(ap, char *);
-	pai = va_arg(ap, struct addrinfo *);
+	pai = va_arg(ap, const struct addrinfo *);
 
 	memset(&sentinel, 0, sizeof(sentinel));
 	cur = &sentinel;
