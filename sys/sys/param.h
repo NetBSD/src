a09cefd37b7d9a7370b281807c85ee6f82f825e3
ad <ad@NetBSD.org>
2008-04-28 22:44:18 +0000

    4.99.62 - struct lwp changed for kernel preemption

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.316 2008/04/24 11:52:53 ad Exp $	*/
+/*	$NetBSD: param.h,v 1.317 2008/04/28 22:44:18 ad Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -63,7 +63,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	499006100	/* NetBSD 4.99.61 */
+#define	__NetBSD_Version__	499006200	/* NetBSD 4.99.62 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
