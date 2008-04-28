6ef1e22e90e689941e1c9f07516d23fc937db9c9
ad <ad@NetBSD.org>
2008-04-28 15:18:43 +0000

    key_usrreq: set in a lock on PRU_ATTACH. Spotted by yamt@.

--- sys/netkey/keysock.c
+++ sys/netkey/keysock.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: keysock.c,v 1.47 2008/04/24 11:38:38 ad Exp $	*/
+/*	$NetBSD: keysock.c,v 1.48 2008/04/28 15:18:43 ad Exp $	*/
 /*	$KAME: keysock.c,v 1.32 2003/08/22 05:45:08 itojun Exp $	*/
 
 /*
@@ -31,7 +31,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: keysock.c,v 1.47 2008/04/24 11:38:38 ad Exp $");
+__KERNEL_RCSID(0, "$NetBSD: keysock.c,v 1.48 2008/04/28 15:18:43 ad Exp $");
 
 #include "opt_inet.h"
 
@@ -119,6 +119,7 @@ key_usrreq(so, req, m, nam, control, l)
 
 	s = splsoftnet();
 	if (req == PRU_ATTACH) {
+		sosetlock(so);
 		kp = (struct keycb *)malloc(sizeof(*kp), M_PCB,
 		    M_WAITOK|M_ZERO);
 		so->so_pcb = (void *)kp;
