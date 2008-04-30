c3077f9533f32eb16918492a1b9a9c142bb2b505
garbled <garbled@NetBSD.org>
2008-04-30 21:20:37 +0000

    Remove prep's copy of mkbootimage

--- sys/arch/prep/stand/mkbootimage/magic.h
+++
@@ -1,7 +0,0 @@
-/*	$NetBSD: magic.h,v 1.1 2000/02/29 15:21:52 nonaka Exp $	*/
-
-char magic[] = "KMA";
-int kern_len;
-#define	MAGICSIZE	sizeof (magic)
-#define	KERNLENSIZE	sizeof (kern_len)
-#define	ENTRY		0x100000
