35b9dd0971afa745535e4d421a89165e8bfef6d4
martin <martin@NetBSD.org>
2008-04-30 13:10:46 +0000

    Convert TNF licenses to new 2 clause variant

--- tests/modules/t_modctl.cpp
+++ tests/modules/t_modctl.cpp
@@ -1,4 +1,4 @@
-// $NetBSD: t_modctl.cpp,v 1.3 2008/03/02 11:22:10 jmmv Exp $
+// $NetBSD: t_modctl.cpp,v 1.4 2008/04/30 13:11:00 martin Exp $
 //
 // Copyright (c) 2008 The NetBSD Foundation, Inc.
 // All rights reserved.
@@ -11,13 +11,6 @@
 // 2. Redistributions in binary form must reproduce the above copyright
 //    notice, this list of conditions and the following disclaimer in the
 //    documentation and/or other materials provided with the distribution.
-// 3. All advertising materials mentioning features or use of this
-//    software must display the following acknowledgement:
-//        This product includes software developed by the NetBSD
-//        Foundation, Inc. and its contributors.
-// 4. Neither the name of The NetBSD Foundation nor the names of its
-//    contributors may be used to endorse or promote products derived
-//    from this software without specific prior written permission.
 //
 // THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND
 // CONTRIBUTORS ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
@@ -35,7 +28,7 @@
 
 extern "C" {
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: t_modctl.cpp,v 1.3 2008/03/02 11:22:10 jmmv Exp $");
+__KERNEL_RCSID(0, "$NetBSD: t_modctl.cpp,v 1.4 2008/04/30 13:11:00 martin Exp $");
 
 #include <sys/module.h>
 #include <sys/sysctl.h>
