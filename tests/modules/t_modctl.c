2b817135aa7487953d9871af5768f235ec24eec3
jmmv <jmmv@NetBSD.org>
2008-05-01 15:38:17 +0000

    Convert NetBSD-specific tests that were previously written in C++ to C now that ATF provides a C-only binding (comes with 0.5).

--- tests/modules/t_modctl.c
+++ tests/modules/t_modctl.c
@@ -1,48 +1,48 @@
-// $NetBSD: t_modctl.cpp,v 1.4 2008/04/30 13:11:00 martin Exp $
-//
-// Copyright (c) 2008 The NetBSD Foundation, Inc.
-// All rights reserved.
-//
-// Redistribution and use in source and binary forms, with or without
-// modification, are permitted provided that the following conditions
-// are met:
-// 1. Redistributions of source code must retain the above copyright
-//    notice, this list of conditions and the following disclaimer.
-// 2. Redistributions in binary form must reproduce the above copyright
-//    notice, this list of conditions and the following disclaimer in the
-//    documentation and/or other materials provided with the distribution.
-//
-// THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND
-// CONTRIBUTORS ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
-// INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
-// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
-// IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS BE LIABLE FOR ANY
-// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
-// GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
-// IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
-// OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
-// IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-//
+/*	$NetBSD: t_modctl.c,v 1.1 2008/05/01 15:38:17 jmmv Exp $	*/
+/*
+ * Copyright (c) 2008 The NetBSD Foundation, Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND
+ * CONTRIBUTORS ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+ * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
-extern "C" {
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: t_modctl.cpp,v 1.4 2008/04/30 13:11:00 martin Exp $");
+__KERNEL_RCSID(0, "$NetBSD: t_modctl.c,v 1.1 2008/05/01 15:38:17 jmmv Exp $");
 
 #include <sys/module.h>
 #include <sys/sysctl.h>
 
+#include <assert.h>
+#include <errno.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 #include <prop/proplib.h>
-}
 
-#include <cassert>
-#include <cerrno>
-#include <cstdlib>
-#include <cstring>
-#include <iostream>
-
-#include <atf.hpp>
+#include <atf-c.h>
 
 static bool have_modular = false;
 
@@ -68,7 +68,7 @@ check_modular(void)
 	iov.iov_base = NULL;
 	iov.iov_len = 0;
 
-	if (::modctl(MODCTL_STAT, &iov) == 0)
+	if (modctl(MODCTL_STAT, &iov) == 0)
 		res = true;
 	else
 		res = (errno != ENOSYS);
@@ -87,7 +87,7 @@ require_modular(void)
 {
 
 	if (!have_modular)
-		ATF_SKIP("Kernel does not have 'options MODULAR'.");
+		atf_tc_skip("Kernel does not have 'options MODULAR'.");
 }
 
 static
@@ -100,18 +100,17 @@ get_modstat_info(const char *name, modstat_t *msdest)
 	modstat_t *ms;
 
 	for (len = 4096; ;) {
-		iov.iov_base = ::malloc(len);
+		iov.iov_base = malloc(len);
 		iov.iov_len = len;
-		if (::modctl(MODCTL_STAT, &iov) != 0) {
+		if (modctl(MODCTL_STAT, &iov) != 0) {
 			int err = errno;
-			std::cerr << "modctl(MODCTL_STAT) failed: "
-			    << std::strerror(err)
-			    << std::endl;
-			ATF_FAIL("Failed to query module status");
+			fprintf(stderr, "modctl(MODCTL_STAT) failed: %s\n",
+			    strerror(err));
+			atf_tc_fail("Failed to query module status");
 		}
 		if (len >= iov.iov_len)
 			break;
-		::free(iov.iov_base);
+		free(iov.iov_base);
 		len = iov.iov_len;
 	}
 
@@ -119,14 +118,14 @@ get_modstat_info(const char *name, modstat_t *msdest)
 	len = iov.iov_len / sizeof(modstat_t);
 	for (ms = (modstat_t *)iov.iov_base; len != 0 && !found;
 	    ms++, len--) {
-		if (std::strcmp(ms->ms_name, name) == 0) {
+		if (strcmp(ms->ms_name, name) == 0) {
 			if (msdest != NULL)
 				*msdest = *ms;
 			found = true;
 		}
 	}
 
-	::free(iov.iov_base);
+	free(iov.iov_base);
 
 	return found;
 }
@@ -136,15 +135,15 @@ get_modstat_info(const char *name, modstat_t *msdest)
  */
 static
 bool
-get_sysctl(const std::string& name, void *buf, const size_t len)
+get_sysctl(const char *name, void *buf, const size_t len)
 {
 	size_t len2 = len;
-	std::cout << "Querying sysctl variable: " << name << std::endl;
-	int ret = ::sysctlbyname(name.c_str(), buf, &len2, NULL, 0);
+	printf("Querying sysctl variable: %s\n", name);
+	int ret = sysctlbyname(name, buf, &len2, NULL, 0);
 	if (ret == -1 && errno != ENOENT) {
-		std::cerr << "sysctlbyname(2) failed: "
-		    << std::strerror(errno) << std::endl;
-		ATF_FAIL("Failed to query " + name);
+		fprintf(stderr, "sysctlbyname(2) failed: %s\n",
+		    strerror(errno));
+		atf_tc_fail("Failed to query %s", name);
 	}
 	return ret != -1;
 }
@@ -174,7 +173,7 @@ k_helper_is_present_sysctl(void)
 	size_t present;
 
 	return get_sysctl("vendor.k_helper.present", &present,
-	                  sizeof(present));
+	    sizeof(present));
 }
 
 /*
@@ -184,7 +183,7 @@ k_helper_is_present_sysctl(void)
  */
 static
 bool
-k_helper_is_present(presence_check how)
+k_helper_is_present(enum presence_check how)
 {
 	bool found;
 
@@ -216,11 +215,11 @@ k_helper_is_present(presence_check how)
  */
 static
 int
-load(const std::string& filename, prop_dictionary_t props = NULL,
-     bool fatal = true)
+load(prop_dictionary_t props, bool fatal, const char *fmt, ...)
 {
 	int err;
-	char *propsstr;
+	va_list ap;
+	char filename[MAXPATHLEN], *propsstr;
 	modctl_load_t ml;
 
 	if (props == NULL) {
@@ -233,23 +232,26 @@ load(const std::string& filename, prop_dictionary_t props = NULL,
 		ATF_CHECK(propsstr != NULL);
 	}
 
-	ml.ml_filename = filename.c_str();
+	va_start(ap, fmt);
+	vsnprintf(filename, sizeof(filename), fmt, ap);
+	va_end(ap);
+
+	ml.ml_filename = filename;
 	ml.ml_flags = 0;
 	ml.ml_props = propsstr;
 	ml.ml_propslen = strlen(propsstr);
 
-	std::cout << "Loading module " << filename << std::endl;
+	printf("Loading module %s\n", filename);
 	err = 0;
-	if (::modctl(MODCTL_LOAD, &ml) == -1) {
+	if (modctl(MODCTL_LOAD, &ml) == -1) {
 		err = errno;
-		std::cerr << "modctl(MODCTL_LOAD, " << filename
-		    << ") failed: " << std::strerror(err)
-		    << std::endl;
+		fprintf(stderr, "modctl(MODCTL_LOAD, %s), failed: %s\n",
+		    filename, strerror(err));
 		if (fatal)
-			ATF_FAIL("Module load failed");
+			atf_tc_fail("Module load failed");
 	}
 
-	::free(propsstr);
+	free(propsstr);
 
 	return err;
 }
@@ -260,19 +262,18 @@ load(const std::string& filename, prop_dictionary_t props = NULL,
  */
 static
 int
-unload(const char *name, bool fatal = true)
+unload(const char *name, bool fatal)
 {
 	int err;
 
-	std::cout << "Unloading module " << name << std::endl;
+	printf("Unloading module %s\n", name);
 	err = 0;
-	if (::modctl(MODCTL_UNLOAD, __UNCONST(name)) == -1) {
+	if (modctl(MODCTL_UNLOAD, __UNCONST(name)) == -1) {
 		err = errno;
-		std::cerr << "modctl(MODCTL_UNLOAD, " << name
-		    << ") failed: " << std::strerror(err)
-		    << std::endl;
+		fprintf(stderr, "modctl(MODCTL_UNLOAD, %s) failed: %s\n",
+		    name, strerror(err));
 		if (fatal)
-			ATF_FAIL("Module unload failed");
+			atf_tc_fail("Module unload failed");
 	}
 	return err;
 }
@@ -286,121 +287,125 @@ int
 unload_cleanup(const char *name)
 {
 
-	(void)::modctl(MODCTL_UNLOAD, __UNCONST(name));
+	(void)modctl(MODCTL_UNLOAD, __UNCONST(name));
 }
 
 /* --------------------------------------------------------------------- */
 /* Test cases                                                            */
 /* --------------------------------------------------------------------- */
 
-ATF_TEST_CASE_WITH_CLEANUP(cmd_load);
-ATF_TEST_CASE_HEAD(cmd_load)
+ATF_TC_WITH_CLEANUP(cmd_load);
+ATF_TC_HEAD(cmd_load, tc)
 {
-	set("descr", "Tests for the MODCTL_LOAD command");
-	set("require.user", "root");
+	atf_tc_set_md_var(tc, "descr", "Tests for the MODCTL_LOAD command");
+	atf_tc_set_md_var(tc, "require.user", "root");
 }
-ATF_TEST_CASE_BODY(cmd_load)
+ATF_TC_BODY(cmd_load, tc)
 {
+	char longname[MAXPATHLEN];
+	size_t i;
+
 	require_modular();
 
-	ATF_CHECK(load("", NULL, false) == ENOENT);
-	ATF_CHECK(load("non-existent.o", NULL, false) == ENOENT);
+	ATF_CHECK(load(NULL, false, "") == ENOENT);
+	ATF_CHECK(load(NULL, false, "non-existent.o") == ENOENT);
 
-	std::string longname(MAXPATHLEN, 'a');
-	ATF_CHECK(load(longname.c_str(), NULL, false) == ENAMETOOLONG);
+	for (i = 0; i < MAXPATHLEN - 1; i++)
+		longname[i] = 'a';
+	longname[MAXPATHLEN - 1] = '\0';
+	ATF_CHECK(load(NULL, false, longname) == ENAMETOOLONG);
 
 	ATF_CHECK(!k_helper_is_present(stat_check));
-	load(get_srcdir() + "/k_helper.o");
-	std::cout << "Checking if load was successful" << std::endl;
+	load(NULL, true, "%s/k_helper.o", atf_tc_get_config_var(tc, "srcdir"));
+	printf("Checking if load was successful\n");
 	ATF_CHECK(k_helper_is_present(stat_check));
 }
-ATF_TEST_CASE_CLEANUP(cmd_load)
+ATF_TC_CLEANUP(cmd_load, tc)
 {
 	unload_cleanup("k_helper");
 }
 
-ATF_TEST_CASE_WITH_CLEANUP(cmd_load_props);
-ATF_TEST_CASE_HEAD(cmd_load_props)
+ATF_TC_WITH_CLEANUP(cmd_load_props);
+ATF_TC_HEAD(cmd_load_props, tc)
 {
-	set("descr", "Tests for the MODCTL_LOAD command, providing extra "
-	    "load-time properties");
-	set("require.user", "root");
+	atf_tc_set_md_var(tc, "descr", "Tests for the MODCTL_LOAD command, "
+	    "providing extra load-time properties");
+	atf_tc_set_md_var(tc, "require.user", "root");
 }
-ATF_TEST_CASE_BODY(cmd_load_props)
+ATF_TC_BODY(cmd_load_props, tc)
 {
 	prop_dictionary_t props;
 
 	require_modular();
 
-	std::cout << "Loading module without properties" << std::endl;
+	printf("Loading module without properties\n");
 	props = prop_dictionary_create();
-	load(get_srcdir() + "/k_helper.o", props);
+	load(props, true, "%s/k_helper.o", atf_tc_get_config_var(tc, "srcdir"));
 	prop_object_release(props);
 	{
 		int ok;
 		ATF_CHECK(get_sysctl("vendor.k_helper.prop_str_ok",
-		                     &ok, sizeof(ok)));
+		    &ok, sizeof(ok)));
 		ATF_CHECK(!ok);
 	}
-	unload("k_helper");
+	unload("k_helper", true);
 
-	std::cout << "Loading module with a string property" << std::endl;
+	printf("Loading module with a string property\n");
 	props = prop_dictionary_create();
 	prop_dictionary_set(props, "prop_str",
-	                    prop_string_create_cstring("1st string"));
-	load(get_srcdir() + "/k_helper.o", props);
+	    prop_string_create_cstring("1st string"));
+	load(props, true, "%s/k_helper.o", atf_tc_get_config_var(tc, "srcdir"));
 	prop_object_release(props);
 	{
 		int ok;
 		ATF_CHECK(get_sysctl("vendor.k_helper.prop_str_ok",
-		                     &ok, sizeof(ok)));
+		    &ok, sizeof(ok)));
 		ATF_CHECK(ok);
 
 		char val[128];
 		ATF_CHECK(get_sysctl("vendor.k_helper.prop_str_val",
-		                     &val, sizeof(val)));
-		ATF_CHECK(std::strcmp(val, "1st string") == 0);
+		    &val, sizeof(val)));
+		ATF_CHECK(strcmp(val, "1st string") == 0);
 	}
-	unload("k_helper");
+	unload("k_helper", true);
 
-	std::cout << "Loading module with a different string property"
-	          << std::endl;
+	printf("Loading module with a different string property\n");
 	props = prop_dictionary_create();
 	prop_dictionary_set(props, "prop_str",
-	                    prop_string_create_cstring("2nd string"));
-	load(get_srcdir() + "/k_helper.o", props);
+	    prop_string_create_cstring("2nd string"));
+	load(props, true, "%s/k_helper.o", atf_tc_get_config_var(tc, "srcdir"));
 	prop_object_release(props);
 	{
 		int ok;
 		ATF_CHECK(get_sysctl("vendor.k_helper.prop_str_ok",
-		                     &ok, sizeof(ok)));
+		    &ok, sizeof(ok)));
 		ATF_CHECK(ok);
 
 		char val[128];
 		ATF_CHECK(get_sysctl("vendor.k_helper.prop_str_val",
-		                     &val, sizeof(val)));
-		ATF_CHECK(std::strcmp(val, "2nd string") == 0);
+		    &val, sizeof(val)));
+		ATF_CHECK(strcmp(val, "2nd string") == 0);
 	}
-	unload("k_helper");
+	unload("k_helper", true);
 }
-ATF_TEST_CASE_CLEANUP(cmd_load_props)
+ATF_TC_CLEANUP(cmd_load_props, tc)
 {
 	unload_cleanup("k_helper");
 }
 
-ATF_TEST_CASE_WITH_CLEANUP(cmd_stat);
-ATF_TEST_CASE_HEAD(cmd_stat)
+ATF_TC_WITH_CLEANUP(cmd_stat);
+ATF_TC_HEAD(cmd_stat, tc)
 {
-	set("descr", "Tests for the MODCTL_STAT command");
-	set("require.user", "root");
+	atf_tc_set_md_var(tc, "descr", "Tests for the MODCTL_STAT command");
+	atf_tc_set_md_var(tc, "require.user", "root");
 }
-ATF_TEST_CASE_BODY(cmd_stat)
+ATF_TC_BODY(cmd_stat, tc)
 {
 	require_modular();
 
 	ATF_CHECK(!k_helper_is_present(both_checks));
 
-	load(get_srcdir() + "/k_helper.o");
+	load(NULL, true, "%s/k_helper.o", atf_tc_get_config_var(tc, "srcdir"));
 	ATF_CHECK(k_helper_is_present(both_checks));
 	{
 		modstat_t ms;
@@ -410,37 +415,37 @@ ATF_TEST_CASE_BODY(cmd_stat)
 		ATF_CHECK(ms.ms_source == MODULE_SOURCE_FILESYS);
 		ATF_CHECK(ms.ms_refcnt == 0);
 	}
-	unload("k_helper");
+	unload("k_helper", true);
 
 	ATF_CHECK(!k_helper_is_present(both_checks));
 }
-ATF_TEST_CASE_CLEANUP(cmd_stat)
+ATF_TC_CLEANUP(cmd_stat, tc)
 {
 	unload_cleanup("k_helper");
 }
 
-ATF_TEST_CASE_WITH_CLEANUP(cmd_unload);
-ATF_TEST_CASE_HEAD(cmd_unload)
+ATF_TC_WITH_CLEANUP(cmd_unload);
+ATF_TC_HEAD(cmd_unload, tc)
 {
-	set("descr", "Tests for the MODCTL_UNLOAD command");
-	set("require.user", "root");
+	atf_tc_set_md_var(tc, "descr", "Tests for the MODCTL_UNLOAD command");
+	atf_tc_set_md_var(tc, "require.user", "root");
 }
-ATF_TEST_CASE_BODY(cmd_unload)
+ATF_TC_BODY(cmd_unload, tc)
 {
 	require_modular();
 
-	load(get_srcdir() + "/k_helper.o");
+	load(NULL, true, "%s/k_helper.o", atf_tc_get_config_var(tc, "srcdir"));
 
 	ATF_CHECK(unload("", false) == ENOENT);
 	ATF_CHECK(unload("non-existent.o", false) == ENOENT);
 	ATF_CHECK(unload("k_helper.o", false) == ENOENT);
 
 	ATF_CHECK(k_helper_is_present(stat_check));
-	unload("k_helper");
-	std::cout << "Checking if unload was successful" << std::endl;
+	unload("k_helper", true);
+	printf("Checking if unload was successful\n");
 	ATF_CHECK(!k_helper_is_present(stat_check));
 }
-ATF_TEST_CASE_CLEANUP(cmd_unload)
+ATF_TC_CLEANUP(cmd_unload, tc)
 {
 	unload_cleanup("k_helper");
 }
@@ -449,12 +454,14 @@ ATF_TEST_CASE_CLEANUP(cmd_unload)
 /* Main                                                                  */
 /* --------------------------------------------------------------------- */
 
-ATF_INIT_TEST_CASES(tcs)
+ATF_TP_ADD_TCS(tp)
 {
 	have_modular = check_modular();
 
-	ATF_ADD_TEST_CASE(tcs, cmd_load);
-	ATF_ADD_TEST_CASE(tcs, cmd_load_props);
-	ATF_ADD_TEST_CASE(tcs, cmd_stat);
-	ATF_ADD_TEST_CASE(tcs, cmd_unload);
+	ATF_TP_ADD_TC(tp, cmd_load);
+	ATF_TP_ADD_TC(tp, cmd_load_props);
+	ATF_TP_ADD_TC(tp, cmd_stat);
+	ATF_TP_ADD_TC(tp, cmd_unload);
+
+	return atf_no_error();
 }
