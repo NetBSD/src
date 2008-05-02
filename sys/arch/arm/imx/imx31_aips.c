4249a9e02f7d695e494c708fc94e21ed4387a43d
martin <martin@NetBSD.org>
2008-05-02 22:00:29 +0000

    Replace TNF licenses with standard 2 clause variant.

--- sys/arch/arm/imx/imx31_aips.c
+++ sys/arch/arm/imx/imx31_aips.c
@@ -1,7 +1,7 @@
-/*	$Id: imx31_aips.c,v 1.2 2008/04/27 18:58:44 matt Exp $	*/
+/*	$Id: imx31_aips.c,v 1.3 2008/05/02 22:00:29 martin Exp $	*/
 
 /* derived from:	*/
-/*	$NetBSD: imx31_aips.c,v 1.2 2008/04/27 18:58:44 matt Exp $ */
+/*	$NetBSD: imx31_aips.c,v 1.3 2008/05/02 22:00:29 martin Exp $ */
 
 /*
  * Copyright (c) 2002, 2005  Genetec Corporation.  All rights reserved.
@@ -54,13 +54,18 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *      This product includes software developed by the NetBSD
- *      Foundation, Inc. and its contributors.
- * 4. Neither the name of The NetBSD Foundation nor the names of its
- *    contributors may be used to endorse or promote products derived
- *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
  */
 /*-
  * Copyright (c) 1999
@@ -97,7 +102,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$Id: imx31_aips.c,v 1.2 2008/04/27 18:58:44 matt Exp $");
+__KERNEL_RCSID(0, "$Id: imx31_aips.c,v 1.3 2008/05/02 22:00:29 martin Exp $");
 
 #include "locators.h"
 
