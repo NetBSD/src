1103499abdfd860c99b4765724cf1f9ecb1be5f9
martin <martin@NetBSD.org>
2008-05-04 12:59:38 +0000

    Move to standard TNF 2 clause license

--- sys/net/if_gre.h
+++ sys/net/if_gre.h
@@ -1,8 +1,8 @@
-/*	$NetBSD: if_gre.h,v 1.35 2008/02/20 17:05:53 matt Exp $ */
+/*	$NetBSD: if_gre.h,v 1.36 2008/05/04 13:13:36 martin Exp $ */
 
 /*
  * Copyright (c) 1998 The NetBSD Foundation, Inc.
- * All rights reserved
+ * All rights reserved.
  *
  * This code is derived from software contributed to The NetBSD Foundation
  * by Heiko W.Rupp <hwr@pilhuhn.de>
@@ -15,13 +15,6 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *        This product includes software developed by the NetBSD
- *        Foundation, Inc. and its contributors.
- * 4. Neither the name of The NetBSD Foundation nor the names of its
- *    contributors may be used to endorse or promote products derived
- *    from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
  * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
