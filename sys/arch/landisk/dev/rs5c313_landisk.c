bc8b981514a878573678329719830a5a722179d9
martin <martin@NetBSD.org>
2008-05-04 19:43:05 +0000

    Move TNF licenses to 2 clause form

--- sys/arch/landisk/dev/rs5c313_landisk.c
+++ sys/arch/landisk/dev/rs5c313_landisk.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: rs5c313_landisk.c,v 1.3 2008/03/27 02:15:29 uwe Exp $	*/
+/*	$NetBSD: rs5c313_landisk.c,v 1.4 2008/05/04 19:43:05 martin Exp $	*/
 
 /*-
  * Copyright (c) 2002 The NetBSD Foundation, Inc.
@@ -12,10 +12,6 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *        This product includes software developed by the NetBSD
- *        Foundation, Inc. and its contributors.
  *
  * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
  * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
@@ -31,7 +27,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: rs5c313_landisk.c,v 1.3 2008/03/27 02:15:29 uwe Exp $");
+__KERNEL_RCSID(0, "$NetBSD: rs5c313_landisk.c,v 1.4 2008/05/04 19:43:05 martin Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
