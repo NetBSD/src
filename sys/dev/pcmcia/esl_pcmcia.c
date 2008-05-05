729313d52c0bd93f91226cb9bb84854b7fdb7a72
jmcneill <jmcneill@NetBSD.org>
2008-05-05 00:12:49 +0000

    Use 2-clause license.

--- sys/dev/pcmcia/esl_pcmcia.c
+++ sys/dev/pcmcia/esl_pcmcia.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: esl_pcmcia.c,v 1.19 2008/04/05 21:31:23 cegger Exp $	*/
+/*	$NetBSD: esl_pcmcia.c,v 1.20 2008/05/05 00:15:26 jmcneill Exp $	*/
 
 /*
  * Copyright (c) 2000 Jared D. McNeill <jmcneill@invisible.ca>
@@ -12,12 +12,6 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *      This product includes software developed by Jared D. McNeill.
- * 4. Neither the name of the author nor the names of any contributors may
- *    be used to endorse or promote products derived from this software
- *    without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
@@ -34,7 +28,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: esl_pcmcia.c,v 1.19 2008/04/05 21:31:23 cegger Exp $");
+__KERNEL_RCSID(0, "$NetBSD: esl_pcmcia.c,v 1.20 2008/05/05 00:15:26 jmcneill Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
