961a142a6fff8007cd218b6256d7ab5b33031395
macallan <macallan>
2008-05-08 01:37:04 +0000

    nuke license clause 3

--- sys/arch/macppc/dev/adt7467_ki2c.c
+++ sys/arch/macppc/dev/adt7467_ki2c.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: adt7467_ki2c.c,v 1.4 2008/05/08 01:36:03 macallan Exp $	*/
+/*	$NetBSD: adt7467_ki2c.c,v 1.5 2008/05/08 01:37:04 macallan Exp $	*/
 
 /*-
  * Copyright (C) 2005 Michael Lorenz
@@ -11,8 +11,6 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. The name of the author may not be used to endorse or promote products
- *    derived from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
@@ -32,7 +30,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: adt7467_ki2c.c,v 1.4 2008/05/08 01:36:03 macallan Exp $");
+__KERNEL_RCSID(0, "$NetBSD: adt7467_ki2c.c,v 1.5 2008/05/08 01:37:04 macallan Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
