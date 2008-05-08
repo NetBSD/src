7b134f1438d74831d5b5ca83edcac323cab53453
jmcneill <jmcneill@NetBSD.org>
2008-05-08 02:10:59 +0000

    Use 2-clause license.

--- sys/arch/sgimips/dev/crmfb.c
+++ sys/arch/sgimips/dev/crmfb.c
@@ -1,4 +1,4 @@
-/* $NetBSD: crmfb.c,v 1.24 2008/03/18 02:41:38 jmcneill Exp $ */
+/* $NetBSD: crmfb.c,v 1.25 2008/05/08 02:10:59 jmcneill Exp $ */
 
 /*-
  * Copyright (c) 2007 Jared D. McNeill <jmcneill@invisible.ca>
@@ -13,12 +13,6 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *        This product includes software developed by Jared D. McNeill.
- * 4. Neither the name of The NetBSD Foundation nor the names of its
- *    contributors may be used to endorse or promote products derived
- *    from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
  * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
@@ -38,7 +32,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: crmfb.c,v 1.24 2008/03/18 02:41:38 jmcneill Exp $");
+__KERNEL_RCSID(0, "$NetBSD: crmfb.c,v 1.25 2008/05/08 02:10:59 jmcneill Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
