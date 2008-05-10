1d610a91ff4bdb532740a5b2b72ebe05ae0db6aa
skd <skd@NetBSD.org>
2008-05-10 00:05:31 +0000

    Add epsv6 and epsv to disable extended passive mode for ipv6 or both ipv4 and ipv6 respectively.  This hack is due to our friends a Juniper Networks who break epsv in ipv6.  Should be fixed in ScreenOS 6.2.X.

--- usr.bin/ftp/cmdtab.c
+++ usr.bin/ftp/cmdtab.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: cmdtab.c,v 1.48 2008/04/28 20:24:13 martin Exp $	*/
+/*	$NetBSD: cmdtab.c,v 1.49 2008/05/10 00:05:31 skd Exp $	*/
 
 /*-
  * Copyright (c) 1996-2005 The NetBSD Foundation, Inc.
@@ -63,7 +63,7 @@
 #if 0
 static char sccsid[] = "@(#)cmdtab.c	8.4 (Berkeley) 10/9/94";
 #else
-__RCSID("$NetBSD: cmdtab.c,v 1.48 2008/04/28 20:24:13 martin Exp $");
+__RCSID("$NetBSD: cmdtab.c,v 1.49 2008/05/10 00:05:31 skd Exp $");
 #endif
 #endif /* not lint */
 
@@ -93,7 +93,9 @@ HSTR	deletehelp[] =	"delete remote file";
 HSTR	disconhelp[] =	"terminate ftp session";
 HSTR	domachelp[] =	"execute macro";
 HSTR	edithelp[] =	"toggle command line editing";
+HSTR	epsvhelp[] =	"toggle use of EPSV/EPRT on both IPv4 and IPV6 ftp";
 HSTR	epsv4help[] =	"toggle use of EPSV/EPRT on IPv4 ftp";
+HSTR	epsv6help[] =	"toggle use of EPSV/EPRT on IPv6 ftp";
 HSTR	feathelp[] =	"show FEATures supported by remote system";
 HSTR	formhelp[] =	"set file transfer format";
 HSTR	gatehelp[] =	"toggle gate-ftp; specify host[:port] to change proxy";
@@ -204,7 +206,9 @@ struct cmd cmdtab[] = {
 	{ "dir",	H(lshelp),	1, 1, 1, CMPL(rl)	ls },
 	{ "disconnect",	H(disconhelp),	0, 1, 1, CMPL0		disconnect },
 	{ "edit",	H(edithelp),	0, 0, 0, CMPL0		setedit },
+	{ "epsv",	H(epsvhelp),	0, 0, 0, CMPL0		setepsv },
 	{ "epsv4",	H(epsv4help),	0, 0, 0, CMPL0		setepsv4 },
+	{ "epsv6",	H(epsv6help),	0, 0, 0, CMPL0		setepsv6 },
 	{ "exit",	H(quithelp),	0, 0, 0, CMPL0		quit },
 	{ "features",	H(feathelp),	0, 1, 1, CMPL0		feat },
 	{ "fget",	H(fgethelp),	1, 1, 1, CMPL(l)	fget },
--- usr.bin/ftp/util.c
+++ usr.bin/ftp/util.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: util.c,v 1.146 2008/04/28 20:24:13 martin Exp $	*/
+/*	$NetBSD: util.c,v 1.147 2008/05/10 00:05:31 skd Exp $	*/
 
 /*-
  * Copyright (c) 1997-2008 The NetBSD Foundation, Inc.
@@ -64,7 +64,7 @@
 
 #include <sys/cdefs.h>
 #ifndef lint
-__RCSID("$NetBSD: util.c,v 1.146 2008/04/28 20:24:13 martin Exp $");
+__RCSID("$NetBSD: util.c,v 1.147 2008/05/10 00:05:31 skd Exp $");
 #endif /* not lint */
 
 /*
@@ -303,6 +303,7 @@ cleanuppeer(void)
 		anonftp = 0;
 	data = -1;
 	epsv4bad = 0;
+	epsv6bad = 0;
 	if (username)
 		free(username);
 	username = NULL;
