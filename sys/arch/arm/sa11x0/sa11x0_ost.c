5d1469bd77da1a75a35fb94949a4d0e7c7c6d893
martin <martin@NetBSD.org>
2008-05-10 15:31:03 +0000

    Backout previous: the license sweep touched these files in error, so restore the old license.

--- sys/arch/arm/sa11x0/sa11x0_ost.c
+++ sys/arch/arm/sa11x0/sa11x0_ost.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: sa11x0_ost.c,v 1.24 2008/04/28 20:23:14 martin Exp $	*/
+/*	$NetBSD: sa11x0_ost.c,v 1.25 2008/05/10 15:31:04 martin Exp $	*/
 
 /*
  * Copyright (c) 1997 Mark Brinicombe.
@@ -16,6 +16,13 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed by the NetBSD
+ *	Foundation, Inc. and its contributors.
+ * 4. Neither the name of The NetBSD Foundation nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
  * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
@@ -31,7 +38,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: sa11x0_ost.c,v 1.24 2008/04/28 20:23:14 martin Exp $");
+__KERNEL_RCSID(0, "$NetBSD: sa11x0_ost.c,v 1.25 2008/05/10 15:31:04 martin Exp $");
 
 #include <sys/types.h>
 #include <sys/param.h>
--- sys/dev/splash/splash.c
+++ sys/dev/splash/splash.c
@@ -1,4 +1,4 @@
-/* $NetBSD: splash.c,v 1.5 2008/04/28 20:23:58 martin Exp $ */
+/* $NetBSD: splash.c,v 1.6 2008/05/10 15:31:05 martin Exp $ */
 
 /*-
  * Copyright (c) 2006 Jared D. McNeill <jmcneill@invisible.ca>
@@ -12,6 +12,13 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *        This product includes software developed by the NetBSD
+ *        Foundation, Inc. and its contributors.
+ * 4. Neither the name of The NetBSD Foundation nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
  * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
@@ -27,7 +34,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: splash.c,v 1.5 2008/04/28 20:23:58 martin Exp $");
+__KERNEL_RCSID(0, "$NetBSD: splash.c,v 1.6 2008/05/10 15:31:05 martin Exp $");
 
 #include "opt_splash.h"
 
