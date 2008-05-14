b87210fa510e6eecfd48e218f3aefcb05cbe4636
tsutsui <tsutsui@NetBSD.org>
2008-05-14 13:29:27 +0000

    Normalize my licenses.

--- sys/arch/sgimips/mace/if_mec.c
+++ sys/arch/sgimips/mace/if_mec.c
@@ -1,8 +1,7 @@
-/* $NetBSD: if_mec.c,v 1.19 2008/04/29 15:50:39 tsutsui Exp $ */
+/* $NetBSD: if_mec.c,v 1.20 2008/05/14 13:29:28 tsutsui Exp $ */
 
-/*
- * Copyright (c) 2004 Izumi Tsutsui.
- * All rights reserved.
+/*-
+ * Copyright (c) 2004 Izumi Tsutsui.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -12,8 +11,6 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. The name of the author may not be used to endorse or promote products
- *    derived from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
@@ -64,7 +61,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: if_mec.c,v 1.19 2008/04/29 15:50:39 tsutsui Exp $");
+__KERNEL_RCSID(0, "$NetBSD: if_mec.c,v 1.20 2008/05/14 13:29:28 tsutsui Exp $");
 
 #include "opt_ddb.h"
 #include "bpfilter.h"
