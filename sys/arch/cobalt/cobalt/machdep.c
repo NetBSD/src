be1c4ff125e0f8047e2a507192dba853fabca16a
tsutsui <tsutsui@NetBSD.org>
2008-05-14 13:29:27 +0000

    Normalize my licenses.

--- sys/arch/cobalt/cobalt/machdep.c
+++ sys/arch/cobalt/cobalt/machdep.c
@@ -1,8 +1,7 @@
-/*	$NetBSD: machdep.c,v 1.95 2008/03/22 18:46:49 tsutsui Exp $	*/
+/*	$NetBSD: machdep.c,v 1.96 2008/05/14 13:29:27 tsutsui Exp $	*/
 
-/*
- * Copyright (c) 2006 Izumi Tsutsui.
- * All rights reserved.
+/*-
+ * Copyright (c) 2006 Izumi Tsutsui.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -12,8 +11,6 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. The name of the author may not be used to endorse or promote products
- *    derived from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
@@ -53,7 +50,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: machdep.c,v 1.95 2008/03/22 18:46:49 tsutsui Exp $");
+__KERNEL_RCSID(0, "$NetBSD: machdep.c,v 1.96 2008/05/14 13:29:27 tsutsui Exp $");
 
 #include "opt_ddb.h"
 #include "opt_kgdb.h"
--- sys/arch/ews4800mips/sbd/osiop_sbdio.c
+++ sys/arch/ews4800mips/sbd/osiop_sbdio.c
@@ -1,6 +1,6 @@
-/*	$NetBSD: osiop_sbdio.c,v 1.3 2008/03/29 09:11:35 tsutsui Exp $	*/
+/*	$NetBSD: osiop_sbdio.c,v 1.4 2008/05/14 13:29:28 tsutsui Exp $	*/
 
-/*
+/*-
  * Copyright (c) 2001, 2005 Izumi Tsutsui.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -11,8 +11,6 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. The name of the author may not be used to endorse or promote products
- *    derived from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
@@ -27,7 +25,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: osiop_sbdio.c,v 1.3 2008/03/29 09:11:35 tsutsui Exp $");
+__KERNEL_RCSID(0, "$NetBSD: osiop_sbdio.c,v 1.4 2008/05/14 13:29:28 tsutsui Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
