aeb6b9c7c6b936b63b92bd1b866ea3a197968731
tsutsui <tsutsui>
2008-05-14 13:29:27 +0000

    Normalize my licenses.

--- sys/dev/pci/pciide_acard_reg.h
+++ sys/dev/pci/pciide_acard_reg.h
@@ -1,7 +1,7 @@
-/*	$NetBSD: pciide_acard_reg.h,v 1.7 2007/12/25 18:33:41 perry Exp $	*/
+/*	$NetBSD: pciide_acard_reg.h,v 1.8 2008/05/14 13:29:29 tsutsui Exp $	*/
 
-/*
- * Copyright (c) 2001 Izumi Tsutsui.
+/*-
+ * Copyright (c) 2001 Izumi Tsutsui.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -11,8 +11,6 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. The name of the author may not be used to endorse or promote products
- *    derived from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
@@ -24,7 +22,6 @@
  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
  */
 
 #define ATP850_IDETIME(channel)	(0x40 + (channel) * 4)
