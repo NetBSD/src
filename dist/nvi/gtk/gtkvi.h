bdeb7005536c98c64b6d28c1634c5c90398f29d3
aymeric <aymeric@NetBSD.org>
2008-05-16 18:03:01 +0000

    import nvi 1.81.6 verbatim, untouched from the git tree

---
+++ dist/nvi/gtk/gtkvi.h
@@ -0,0 +1,13 @@
+#ifndef __GTK_VI_H__
+#define __GTK_VI_H__
+
+typedef struct _GtkVi           GtkVi;
+
+struct _GtkVi
+{
+//    GtkWidget	*term;
+//    GtkWidget	*vi;	    /* XXX */
+//    GtkWidget	*vi_window;
+    IPVI    *ipvi;
+};
+#endif /* __GTK_VI_H__ */
---
+++ dist/nvi/vi/v_status.c
@@ -0,0 +1,39 @@
+/*-
+ * Copyright (c) 1992, 1993, 1994
+ *	The Regents of the University of California.  All rights reserved.
+ * Copyright (c) 1992, 1993, 1994, 1995, 1996
+ *	Keith Bostic.  All rights reserved.
+ *
+ * See the LICENSE file for redistribution information.
+ */
+
+#include "config.h"
+
+#ifndef lint
+static const char sccsid[] = "$Id: v_status.c,v 1.1.1.1 2008/05/16 18:04:24 aymeric Exp $ (Berkeley) $Date: 2008/05/16 18:04:24 $";
+#endif /* not lint */
+
+#include <sys/types.h>
+#include <sys/queue.h>
+#include <sys/stat.h>
+#include <sys/time.h>
+
+#include <bitstring.h>
+#include <limits.h>
+#include <stdio.h>
+
+#include "../common/common.h"
+#include "vi.h"
+
+/*
+ * v_status -- ^G
+ *	Show the file status.
+ *
+ * PUBLIC: int v_status __P((SCR *, VICMD *));
+ */
+int
+v_status(SCR *sp, VICMD *vp)
+{
+	(void)msgq_status(sp, vp->m_start.lno, MSTAT_SHOWLAST);
+	return (0);
+}
