2857d0fb8e2d6941f212c558e2c108f20f1ab7b5
lukem <lukem@NetBSD.org>
2008-05-22 14:20:36 +0000

    OpenLDAP 2.4.9

---
+++ external/bsd/openldap/dist/contrib/ldapc++/src/LDAPRebind.cpp
@@ -0,0 +1,9 @@
+// $OpenLDAP: pkg/ldap/contrib/ldapc++/src/LDAPRebind.cpp,v 1.1.10.1 2008/04/14 23:09:26 quanah Exp $
+/*
+ * Copyright 2000, OpenLDAP Foundation, All Rights Reserved.
+ * COPYING RESTRICTIONS APPLY, see COPYRIGHT file
+ */
+
+#include "LDAPRebind.h"
+
+
