6ee6cc32f7af4042bd35e273ee0a5db162a45e6e
hamajima <hamajima@NetBSD.org>
2008-05-25 04:03:31 +0000

    fix gpio port direction for i2c_wait_for_scl(). Armadillo9 works again.

--- sys/arch/evbarm/armadillo/armadillo9_iic.c
+++ sys/arch/evbarm/armadillo/armadillo9_iic.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: armadillo9_iic.c,v 1.4 2007/12/06 17:00:32 ad Exp $	*/
+/*	$NetBSD: armadillo9_iic.c,v 1.5 2008/05/25 04:03:31 hamajima Exp $	*/
 
 /*
  * Copyright (c) 2005 HAMAJIMA Katsuomi. All rights reserved.
@@ -26,7 +26,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: armadillo9_iic.c,v 1.4 2007/12/06 17:00:32 ad Exp $");
+__KERNEL_RCSID(0, "$NetBSD: armadillo9_iic.c,v 1.5 2008/05/25 04:03:31 hamajima Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -118,7 +118,7 @@ armadillo9iic_attach(struct device *parent, struct device *self, void *aux)
 
 	iba.iba_tag = &sc->sc_i2c;
 
-	epgpio_in(sc->sc_gpio, sc->sc_port, sc->sc_sda);
+	epgpio_out(sc->sc_gpio, sc->sc_port, sc->sc_sda);
 	epgpio_out(sc->sc_gpio, sc->sc_port, sc->sc_scl);
 	epgpio_set(sc->sc_gpio, sc->sc_port, sc->sc_scl);
 
@@ -214,18 +214,16 @@ armadillo9iic_bb_set_bits(void *cookie, uint32_t bits)
 void
 armadillo9iic_bb_set_dir(void *cookie, uint32_t bits)
 {
-	struct armadillo9iic_softc *sc = cookie;
-
-	if(bits)
-		epgpio_out(sc->sc_gpio, sc->sc_port, sc->sc_sda);
-	else
-		epgpio_in(sc->sc_gpio, sc->sc_port, sc->sc_sda);
 }
 
 uint32_t
 armadillo9iic_bb_read_bits(void *cookie)
 {
 	struct armadillo9iic_softc *sc = cookie;
+	uint32_t bits = 0;
 
-	return epgpio_read(sc->sc_gpio, sc->sc_port, sc->sc_sda) << sc->sc_sda;
+	bits |= epgpio_read(sc->sc_gpio, sc->sc_port, sc->sc_sda) << sc->sc_sda;
+	bits |= epgpio_read(sc->sc_gpio, sc->sc_port, sc->sc_scl) << sc->sc_scl;
+
+	return bits;
 }
