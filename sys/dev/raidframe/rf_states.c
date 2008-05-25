1c468897f33b4db6ab4cdc8510332a3b2d120ace
bouyer <bouyer@NetBSD.org>
2008-05-25 19:10:20 +0000

    Pull up following revision(s) (requested by oster in ticket #1154): 	sys/dev/raidframe/rf_states.c: revision 1.43 Add in a missing "bp->b_resid = bp->b_bcount" in the EIO case. Spotted by Juergen Hannken-Illjes.  Thanks!

--- sys/dev/raidframe/rf_states.c
+++ sys/dev/raidframe/rf_states.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: rf_states.c,v 1.40 2005/12/11 12:23:37 christos Exp $	*/
+/*	$NetBSD: rf_states.c,v 1.40.24.1 2008/05/25 19:10:20 bouyer Exp $	*/
 /*
  * Copyright (c) 1995 Carnegie-Mellon University.
  * All rights reserved.
@@ -27,7 +27,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: rf_states.c,v 1.40 2005/12/11 12:23:37 christos Exp $");
+__KERNEL_RCSID(0, "$NetBSD: rf_states.c,v 1.40.24.1 2008/05/25 19:10:20 bouyer Exp $");
 
 #include <sys/errno.h>
 
@@ -530,6 +530,7 @@ rf_State_CreateDAG(RF_RaidAccessDesc_t *desc)
 		bp = (struct buf *)desc->bp;
 		bp->b_flags |= B_ERROR;
 		bp->b_error = EIO;
+		bp->b_resid = bp->b_bcount;
 	} else {
 		/* bind dags to desc */
 		dagList = desc->dagList;
