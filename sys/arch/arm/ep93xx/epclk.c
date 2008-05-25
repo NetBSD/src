7acbc44d08c0c072bc81a9cd3b53232804ddad6f
hamajima <hamajima@NetBSD.org>
2008-05-25 03:57:22 +0000

    fix countdown of "remaining" variable in delay(). Armadillo9 works again. speed up an initial value  calculation of "remaining" variable in delay().

--- sys/arch/arm/ep93xx/epclk.c
+++ sys/arch/arm/ep93xx/epclk.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: epclk.c,v 1.13 2008/05/10 15:31:04 martin Exp $	*/
+/*	$NetBSD: epclk.c,v 1.14 2008/05/25 03:57:22 hamajima Exp $	*/
 
 /*
  * Copyright (c) 2004 Jesse Off
@@ -47,7 +47,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: epclk.c,v 1.13 2008/05/10 15:31:04 martin Exp $");
+__KERNEL_RCSID(0, "$NetBSD: epclk.c,v 1.14 2008/05/25 03:57:22 hamajima Exp $");
 
 #include <sys/types.h>
 #include <sys/param.h>
@@ -240,6 +240,7 @@ delay(unsigned int n)
 {
 	unsigned int cur_tick, initial_tick;
 	int remaining;
+	u_int32_t scalar = 4222124650UL;
 
 #ifdef DEBUG
 	if (epclk_sc == NULL) {
@@ -254,25 +255,17 @@ delay(unsigned int n)
 	 */
 	initial_tick = TIMER4VAL();
 
-	if (n <= UINT_MAX / TIMER_FREQ) {
-		/*
-		 * For unsigned arithmetic, division can be replaced with
-		 * multiplication with the inverse and a shift.
-		 */
-		remaining = n * TIMER_FREQ / 1000000;
-	} else {
-		/* This is a very long delay.
-		 * Being slow here doesn't matter.
-		 */
-		remaining = (unsigned long long) n * TIMER_FREQ / 1000000;
-	}
+	/* This is a quick ARM way to multiply by 983040/1000000 */
+	__asm volatile ("umull %0, %1, %2, %3;"
+			: "=r"(n), "=&r"(remaining)
+			: "r"((scalar)), "0"(n));
 
 	while (remaining > 0) {
 		cur_tick = TIMER4VAL();
-		if (cur_tick > initial_tick)
-			remaining -= UINT_MAX - (cur_tick - initial_tick);
+		if (cur_tick >= initial_tick)
+			remaining -= cur_tick - initial_tick;
 		else
-			remaining -= initial_tick - cur_tick;
+			remaining -= UINT_MAX - initial_tick + cur_tick + 1;
 		initial_tick = cur_tick;
 	}
 }
