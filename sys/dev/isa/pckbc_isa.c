c26630cba2940d123c47a03e402564deacc8117f
jmcneill <jmcneill@NetBSD.org>
2008-05-25 16:19:12 +0000

    Add pmf support.

--- sys/dev/isa/pckbc_isa.c
+++ sys/dev/isa/pckbc_isa.c
@@ -1,4 +1,4 @@
-/* $NetBSD: pckbc_isa.c,v 1.24 2008/03/15 13:23:25 cube Exp $ */
+/* $NetBSD: pckbc_isa.c,v 1.25 2008/05/25 16:19:12 jmcneill Exp $ */
 
 /*
  * Copyright (c) 1998
@@ -26,7 +26,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: pckbc_isa.c,v 1.24 2008/03/15 13:23:25 cube Exp $");
+__KERNEL_RCSID(0, "$NetBSD: pckbc_isa.c,v 1.25 2008/05/25 16:19:12 jmcneill Exp $");
 
 #include "opt_pckbc.h"
 
@@ -202,6 +202,9 @@ pckbc_isa_attach(device_t parent, device_t self, void *aux)
 	sc->id = t;
 
 	aprint_normal("\n");
+
+	if (!pmf_device_register(self, NULL, pckbc_resume))
+		aprint_error_dev(self, "couldn't establish power handler\n");
 
 	/* Finish off the attach. */
 	pckbc_attach(sc);
