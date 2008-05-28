a5775f69e58f37b6b6f545003de63fdc8fdb6133
ad <ad>
2008-05-28 13:35:32 +0000

    PR kern/38355 lockf deadlock detection is broken after vmlocking
    - Fix it; tested with Sun's libMicro.
    - Use pool_cache.
    - Use a global lock, so the deadlock detection code is safer.

--- sys/sys/lockf.h
+++ sys/sys/lockf.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: lockf.h,v 1.18 2006/05/20 12:02:47 yamt Exp $	*/
+/*	$NetBSD: lockf.h,v 1.19 2008/05/28 13:35:32 ad Exp $	*/
 
 /*
  * Copyright (c) 1991, 1993
@@ -41,6 +41,7 @@
 struct lockf;
 
 int lf_advlock(struct vop_advlock_args *, struct lockf **, off_t);
+void lf_init(void);
 #endif /* _KERNEL */
 
 #endif /* !_SYS_LOCKF_H_ */
