6704bc72a3c0603540796569ccd77fd046af48cf
ad <ad@NetBSD.org>
2008-05-31 19:32:33 +0000

    4.99.64 - kcondvar_t changed

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.318 2008/05/16 09:22:00 hannken Exp $	*/
+/*	$NetBSD: param.h,v 1.319 2008/05/31 19:32:33 ad Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -63,7 +63,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	499006300	/* NetBSD 4.99.63 */
+#define	__NetBSD_Version__	499006400	/* NetBSD 4.99.64 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
