5444b424c9aa1c3ac68d27f00c1a7a7b6844b44b
nonaka <nonaka>
2008-06-04 12:38:58 +0000

    Added missing splx() at ohci_resume().

--- sys/dev/usb/ohci.c
+++ sys/dev/usb/ohci.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: ohci.c,v 1.193 2008/05/21 17:19:44 drochner Exp $	*/
+/*	$NetBSD: ohci.c,v 1.194 2008/06/04 12:38:58 nonaka Exp $	*/
 /*	$FreeBSD: src/sys/dev/usb/ohci.c,v 1.22 1999/11/17 22:33:40 n_hibma Exp $	*/
 
 /*
@@ -41,7 +41,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: ohci.c,v 1.193 2008/05/21 17:19:44 drochner Exp $");
+__KERNEL_RCSID(0, "$NetBSD: ohci.c,v 1.194 2008/06/04 12:38:58 nonaka Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -1038,6 +1038,7 @@ ohci_resume(device_t dv PMF_FN_ARGS)
 	usb_delay_ms(&sc->sc_bus, USB_RESUME_RECOVERY);
 	sc->sc_control = sc->sc_intre = 0;
 	sc->sc_bus.use_polling--;
+	splx(s);
 
 	return true;
 }
