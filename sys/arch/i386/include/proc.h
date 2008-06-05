f87bccd75d4ae9d8d4cc60b09dbb3c07950b7f4a
ad <ad>
2008-06-05 21:09:12 +0000

    pmap_remove_all() for x86. Also, always defer freeing ptps to pmap_update(). There may be a better way to do this, but for now this is simple and avoids potential bugs.
    Proposed on tech-kern and discussed with chs@.

--- sys/arch/i386/include/proc.h
+++ sys/arch/i386/include/proc.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: proc.h,v 1.35 2008/01/04 15:55:33 yamt Exp $	*/
+/*	$NetBSD: proc.h,v 1.36 2008/06/05 21:09:12 ad Exp $	*/
 
 /*
  * Copyright (c) 1991 Regents of the University of California.
@@ -44,10 +44,15 @@
 /*
  * Machine-dependent part of the lwp structure for i386.
  */
+struct pmap;
+struct vm_page;
+
 struct mdlwp {
 	struct	trapframe *md_regs;	/* registers on current frame */
 	int	md_flags;		/* machine-dependent flags */
 	volatile int md_astpending;	/* AST pending for this process */
+	struct pmap *md_gc_pmap;	/* pmap being garbage collected */
+	struct vm_page *md_gc_ptp;	/* pages from pmap g/c */
 };
 
 /* md_flags */
