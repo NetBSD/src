7fa5e8d6cf59991727c0257d1debdb8fbfa359f8
martin <martin@NetBSD.org>
2008-06-12 08:39:21 +0000

    License police

--- sys/ufs/ffs/ffs_inode.c
+++ sys/ufs/ffs/ffs_inode.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: ffs_inode.c,v 1.97.2.1 2008/06/10 14:51:23 simonb Exp $	*/
+/*	$NetBSD: ffs_inode.c,v 1.97.2.2 2008/06/12 08:39:22 martin Exp $	*/
 
 /*-
  * Copyright (c) 2008 The NetBSD Foundation, Inc.
@@ -15,13 +15,6 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed by the NetBSD
- *	Foundation, Inc. and its contributors.
- * 4. Neither the name of The NetBSD Foundation nor the names of its
- *    contributors may be used to endorse or promote products derived
- *    from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
  * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
@@ -68,7 +61,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: ffs_inode.c,v 1.97.2.1 2008/06/10 14:51:23 simonb Exp $");
+__KERNEL_RCSID(0, "$NetBSD: ffs_inode.c,v 1.97.2.2 2008/06/12 08:39:22 martin Exp $");
 
 #if defined(_KERNEL_OPT)
 #include "opt_ffs.h"
