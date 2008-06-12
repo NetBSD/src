c413d96ccdf988df1cda3907bcd09837fc7507f4
cegger <cegger@NetBSD.org>
2008-06-12 22:30:30 +0000

    use device_lookup_private to get softc

--- sys/dev/ic/mb89352.c
+++ sys/dev/ic/mb89352.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: mb89352.c,v 1.48 2008/05/04 12:59:38 martin Exp $	*/
+/*	$NetBSD: mb89352.c,v 1.49 2008/06/12 22:30:30 cegger Exp $	*/
 /*	NecBSD: mb89352.c,v 1.4 1998/03/14 07:31:20 kmatsuda Exp	*/
 
 /*-
@@ -77,7 +77,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: mb89352.c,v 1.48 2008/05/04 12:59:38 martin Exp $");
+__KERNEL_RCSID(0, "$NetBSD: mb89352.c,v 1.49 2008/06/12 22:30:30 cegger Exp $");
 
 #ifdef DDB
 #define	integrate
@@ -2136,7 +2136,7 @@ void
 spc_print_active_acb(void)
 {
 	struct spc_acb *acb;
-	struct spc_softc *sc = device_private(spc_cd.cd_devs[0]); /* XXX */
+	struct spc_softc *sc = device_lookup_private(&spc_cd, 0); /* XXX */
 
 	printf("ready list:\n");
 	TAILQ_FOREACH(acb, &sc->ready_list, chain)
