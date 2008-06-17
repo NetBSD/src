256520114e63e96663a4cff8d3aa808ef8a8e3f2
mlelstv <mlelstv@NetBSD.org>
2008-06-17 15:05:15 +0000

    fix copy&paste error

--- sys/dev/fss.c
+++ sys/dev/fss.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: fss.c,v 1.47 2008/06/17 15:04:12 mlelstv Exp $	*/
+/*	$NetBSD: fss.c,v 1.48 2008/06/17 15:05:15 mlelstv Exp $	*/
 
 /*-
  * Copyright (c) 2003 The NetBSD Foundation, Inc.
@@ -36,7 +36,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: fss.c,v 1.47 2008/06/17 15:04:12 mlelstv Exp $");
+__KERNEL_RCSID(0, "$NetBSD: fss.c,v 1.48 2008/06/17 15:05:15 mlelstv Exp $");
 
 #include "fss.h"
 
@@ -900,7 +900,7 @@ restart:
 		bp->b_dev = sc->sc_bdev;
 		bp->b_private = scp;
 		bp->b_iodone = fss_cluster_iodone;
-		SET(nbp->b_cflags, BC_BUSY);	/* mark buffer busy */
+		SET(bp->b_cflags, BC_BUSY);	/* mark buffer busy */
 
 		bdev_strategy(bp);
 
