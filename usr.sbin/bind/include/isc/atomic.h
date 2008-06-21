e73e2f498f5c201971fb58197549c47cdb590c29
christos <christos@NetBSD.org>
2008-06-21 22:53:06 +0000

    use our atomic functions.

--- usr.sbin/bind/include/isc/atomic.h
+++ usr.sbin/bind/include/isc/atomic.h
@@ -1,18 +1,41 @@
-/*	$NetBSD: atomic.h,v 1.4 2007/01/30 00:49:15 christos Exp $	*/
+/*	$NetBSD: atomic.h,v 1.5 2008/06/21 22:53:06 christos Exp $	*/
 
-#if defined(__i386__) || defined(__x86_64__)
-/* Bind shares those for now, although it provides both of them */
-#include "lib/isc/x86_32/include/isc/atomic.h"
-#elif defined(__sparc__) && defined(_LP64)
-#include "lib/isc/sparc64/include/isc/atomic.h"
-#elif defined(__alpha__)
-#include "lib/isc/alpha/include/isc/atomic.h"
-#elif defined(__powerpc__) && defined(notyet)
-#include "lib/isc/powerpc/include/isc/atomic.h"
-#elif defined(__mips__) && !defined(_MIPS_ARCH_MIPS1)
-#include "lib/isc/mips/include/isc/atomic.h"
-#elif defined(__ia64__)
-#include "lib/isc/ia64/include/isc/atomic.h"
-#else
-#include "lib/isc/noatomic/include/isc/atomic.h"
-#endif
+#include <sys/atomic.h>
+
+/*
+ * This routine atomically increments the value stored in 'p' by 'val', and
+ * returns the previous value.
+ */
+static __inline isc_int32_t
+isc_atomic_xadd(isc_int32_t *p, isc_int32_t val) {
+        isc_int32_t prev = val;
+	atomic_add_32((volatile uint32_t *)p, (uint32_t)val);
+	return prev;
+}
+
+static __inline isc_int64_t
+isc_atomic_xaddq(isc_int64_t *p, isc_int64_t val) {
+        isc_int64_t prev = val;
+	atomic_add_64((volatile uint64_t *)p, (uint64_t)val);
+        return prev;
+}
+
+/*
+ * This routine atomically stores the value 'val' in 'p'.
+ */
+static __inline void
+isc_atomic_store(isc_int32_t *p, isc_int32_t val) {
+	(void)atomic_swap_32((volatile uint32_t *)p, (uint32_t)val);
+}
+
+/*
+ * This routine atomically replaces the value in 'p' with 'val', if the
+ * original value is equal to 'cmpval'.  The original value is returned in any
+ * case.
+ */
+static __inline__ isc_int32_t
+isc_atomic_cmpxchg(isc_int32_t *p, isc_int32_t cmpval, isc_int32_t val) {
+	return (isc_int32_t) atomic_cas_32((volatile uint32_t *)p,
+	    (uint32_t)cmpval, (uint32_t)val);
+}
+
