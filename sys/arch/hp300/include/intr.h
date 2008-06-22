5ba11e15db9771dabc677c2eb17fe5c8e7475a3a
tsutsui <tsutsui@NetBSD.org>
2008-06-22 16:29:36 +0000

    Don't recompute an ipl level for IPL_VM during or after cpu_configure(9) and use a static and possible highest ipl value (ipl 5) for IPL_VM so that we can put ipl2psl_table[] conversion into makeiplcookie(9), which could be called before cpu_configure(9).
    Fixes "panic: sbappendaddr" on NFS load.

--- sys/arch/hp300/include/intr.h
+++ sys/arch/hp300/include/intr.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: intr.h,v 1.29 2008/06/15 07:15:30 tsutsui Exp $	*/
+/*	$NetBSD: intr.h,v 1.30 2008/06/22 16:29:36 tsutsui Exp $	*/
 
 /*-
  * Copyright (c) 1996, 1997, 1999 The NetBSD Foundation, Inc.
@@ -63,7 +63,7 @@
 #define	IPLTOPSL(x)	((((x) & 0xf) << 8) | PSL_S)
 
 extern int idepth;
-extern uint16_t ipl2psl_table[NIPL];
+extern const uint16_t ipl2psl_table[NIPL];
 
 typedef int ipl_t;
 typedef struct {
@@ -94,7 +94,7 @@ splraiseipl(ipl_cookie_t icookie)
 #define	splsoftclock()	splraise1()
 #define	splsoftnet()	splraise1()
 #define	splsoftserial()	splraise1()
-#define	splvm()		_splraise(ipl2psl_table[IPL_VM])
+#define	splvm()		splraise5()
 #define	splsched()	spl6()
 #define	splhigh()	spl7()
 
@@ -122,6 +122,5 @@ void	intr_init(void);
 void	*intr_establish(int (*)(void *), void *, int, int);
 void	intr_disestablish(void *);
 void	intr_dispatch(int);
-void	intr_printlevels(void);
 
 #endif /* _HP300_INTR_H_ */
