c74916713219c5322d4b1589b80eaaa459f99b1a
drochner <drochner@NetBSD.org>
2008-06-24 19:44:51 +0000

    clean up the cardbus interrupt stuff: There were cardbus_intr_line_t and cardbus_intr_handle_t used intermixed for the same variable, and that variable is pretty much useless because cardbus doesn't follow the PCI interrupt swizzling etc scheme. Useless interrupt numbers were printed on cardbus device attach. So as a first step to sanity, kill cardbus_intr_handle_t and poison cardbus_intr_line_t to discourage printing it as a %d. Use cardbus_intr_line_t consistently throughout the code. Remove the "interrupting at foo" messages because the information is misleading. We could come up with a better interrupt vector information, but because cardbus interrupts are mediated by pccbb it would still be misleading.

--- sys/dev/cardbus/if_rtw_cardbus.c
+++ sys/dev/cardbus/if_rtw_cardbus.c
@@ -1,4 +1,4 @@
-/* $NetBSD: if_rtw_cardbus.c,v 1.26 2008/05/14 19:24:48 dyoung Exp $ */
+/* $NetBSD: if_rtw_cardbus.c,v 1.27 2008/06/24 19:44:52 drochner Exp $ */
 
 /*-
  * Copyright (c) 2004, 2005 David Young.  All rights reserved.
@@ -67,7 +67,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: if_rtw_cardbus.c,v 1.26 2008/05/14 19:24:48 dyoung Exp $");
+__KERNEL_RCSID(0, "$NetBSD: if_rtw_cardbus.c,v 1.27 2008/06/24 19:44:52 drochner Exp $");
 
 #include "opt_inet.h"
 #include "bpfilter.h"
@@ -137,7 +137,7 @@ struct rtw_cardbus_softc {
 	int			sc_bar_reg;	/* which BAR to use */
 	pcireg_t		sc_bar_val;	/* value of the BAR */
 
-	int			sc_intrline;	/* interrupt line */
+	cardbus_intr_line_t	sc_intrline;	/* interrupt line */
 };
 
 int	rtw_cardbus_match(device_t, struct cfdata *, void *);
@@ -294,7 +294,6 @@ rtw_cardbus_attach(device_t parent, device_t self, void *aux)
 	/* Remember which interrupt line. */
 	csc->sc_intrline = ca->ca_intrline;
 
-	aprint_normal_dev(self, "interrupting at %d\n", csc->sc_intrline);
 	/*
 	 * Finish off the attach.
 	 */
@@ -365,7 +364,7 @@ rtw_cardbus_resume(device_t self PMF_FN_ARGS)
 	    rtw_intr, sc);
 	if (csc->sc_ih == NULL) {
 		aprint_error_dev(sc->sc_dev,
-		    "unable to establish interrupt at %d\n", csc->sc_intrline);
+		    "unable to establish interrupt\n");
 		return false;
 	}
 
