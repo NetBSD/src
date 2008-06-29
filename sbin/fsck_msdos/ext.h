f425a5f8eebe39c7d32089757a19f356c994a72c
mjf <mjf@NetBSD.org>
2008-06-29 08:35:32 +0000

    Sync with HEAD.

--- sbin/fsck_msdos/ext.h
+++ sbin/fsck_msdos/ext.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: ext.h,v 1.11 2005/01/19 20:00:45 xtraeme Exp $	*/
+/*	$NetBSD: ext.h,v 1.11.26.1 2008/06/29 08:41:57 mjf Exp $	*/
 
 /*
  * Copyright (C) 1995, 1996, 1997 Wolfgang Solfrank
@@ -12,13 +12,6 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed by Martin Husemann
- *	and Wolfgang Solfrank.
- * 4. Neither the name of the University nor the names of its contributors
- *    may be used to endorse or promote products derived from this software
- *    without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHORS ``AS IS'' AND ANY EXPRESS OR
  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
