fe2fa37e15001bb2749c02beb1c774470e67db8d
vanhu <vanhu@NetBSD.org>
2008-07-02 14:47:53 +0000

    From Timo Teras: fixed some %d to %zu (size_t values).

--- crypto/dist/ipsec-tools/src/racoon/isakmp_inf.c
+++ crypto/dist/ipsec-tools/src/racoon/isakmp_inf.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: isakmp_inf.c,v 1.14.4.12 2008/06/18 07:30:19 mgrooms Exp $	*/
+/*	$NetBSD: isakmp_inf.c,v 1.14.4.13 2008/07/02 14:47:53 vanhu Exp $	*/
 
 /* Id: isakmp_inf.c,v 1.44 2006/05/06 20:45:52 manubsd Exp */
 
@@ -169,7 +169,7 @@ isakmp_info_recv(iph1, msg0)
 	if (msg->l < sizeof(*isakmp) + sizeof(*gen)) {
 		plog(LLV_ERROR, LOCATION, NULL, 
 			"ignore information because the "
-			"message is way too short - %d byte(s).\n", msg->l);
+			"message is way too short - %zu byte(s).\n", msg->l);
 		goto end;
 	}
 
@@ -196,7 +196,7 @@ isakmp_info_recv(iph1, msg0)
 		if (msg->l < sizeof(*isakmp) + ntohs(gen->len) + sizeof(*nd)) {
 			plog(LLV_ERROR, LOCATION, NULL, 
 				"ignore information because the "
-				"message is too short - %d byte(s).\n", msg->l);
+				"message is too short - %zu byte(s).\n", msg->l);
 			goto end;
 		}
 
