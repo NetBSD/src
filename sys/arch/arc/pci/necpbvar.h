1a974eec498e68ce3aa0060658e8f29616a4860e
tsutsui <tsutsui>
2008-07-05 08:46:25 +0000

    Split device_t/softc with misc related changes.

--- sys/arch/arc/pci/necpbvar.h
+++ sys/arch/arc/pci/necpbvar.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: necpbvar.h,v 1.6 2005/12/11 12:16:41 christos Exp $	*/
+/*	$NetBSD: necpbvar.h,v 1.7 2008/07/05 08:46:25 tsutsui Exp $	*/
 
 /*-
  * Copyright (C) 2000 Shuichiro URATA.  All rights reserved.
@@ -35,7 +35,7 @@ struct necpb_context {
 };
 
 struct necpb_softc {
-	struct device sc_dev;
+	device_t sc_dev;
 	struct necpb_context *sc_ncp;
 };
 
