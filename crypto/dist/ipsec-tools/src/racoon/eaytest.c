2f7357de7665a9384e8063f53584670f1bd8c0a9
mgrooms <mgrooms>
2008-07-15 00:55:48 +0000

    Fix a conflict with the FreeBSD 8 system hexdump function.

--- crypto/dist/ipsec-tools/src/racoon/eaytest.c
+++ crypto/dist/ipsec-tools/src/racoon/eaytest.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: eaytest.c,v 1.7.6.1 2007/06/06 15:36:38 vanhu Exp $	*/
+/*	$NetBSD: eaytest.c,v 1.7.6.2 2008/07/15 00:55:48 mgrooms Exp $	*/
 
 /* Id: eaytest.c,v 1.22 2005/06/19 18:02:54 manubsd Exp */
 
@@ -65,7 +65,7 @@
 
 #include "package_version.h"
 
-#define PVDUMP(var) hexdump((var)->v, (var)->l)
+#define PVDUMP(var) racoon_hexdump((var)->v, (var)->l)
 
 /*#define CERTTEST_BROKEN */
 
