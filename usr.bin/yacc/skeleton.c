a7bd41f94517c7957c99e25afc5e97c75ccf9f5b
drochner <drochner@NetBSD.org>
2008-07-17 16:28:14 +0000

    fix a longstanding bug, from Otto Moerbeck: if we're reducing a rule that has an empty right hand side and the yacc stackpointer is pointing at the very end of the allocated stack, we end up accessing the stack out of bounds by the implicit $$ = $1 action
    do it a bit differently than in Openbsd: avoid pulling in <string.h>
    because this is not compatible with wacky namespace games we do in libc

--- usr.bin/yacc/skeleton.c
+++ usr.bin/yacc/skeleton.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: skeleton.c,v 1.26 2006/05/24 18:01:43 christos Exp $	*/
+/*	$NetBSD: skeleton.c,v 1.27 2008/07/17 16:28:14 drochner Exp $	*/
 
 /*
  * Copyright (c) 1989 The Regents of the University of California.
@@ -37,7 +37,7 @@
 #if 0
 static char sccsid[] = "@(#)skeleton.c	5.8 (Berkeley) 4/29/95";
 #else
-__RCSID("$NetBSD: skeleton.c,v 1.26 2006/05/24 18:01:43 christos Exp $");
+__RCSID("$NetBSD: skeleton.c,v 1.27 2008/07/17 16:28:14 drochner Exp $");
 #endif /* 0 */
 #endif /* not lint */
 
@@ -63,7 +63,7 @@ const char * const banner[] =
     "static char yysccsid[] = \"@(#)yaccpar	1.9 (Berkeley) 02/21/93\";",
     "#else",
     "#if defined(__NetBSD__) && defined(__IDSTRING)",
-    "__IDSTRING(yyrcsid, \"$NetBSD: skeleton.c,v 1.26 2006/05/24 18:01:43 christos Exp $\");",
+    "__IDSTRING(yyrcsid, \"$NetBSD: skeleton.c,v 1.27 2008/07/17 16:28:14 drochner Exp $\");",
     "#endif /* __NetBSD__ && __IDSTRING */",
     "#endif /* 0 */",
     "#endif /* lint */",
@@ -119,6 +119,7 @@ const char * const header[] =
     "short *yyssp;",
     "YYSTYPE *yyvsp;",
     "YYSTYPE yyval;",
+    "const YYSTYPE yyvalzero;",
     "YYSTYPE yylval;",
     "short *yyss;",
     "short *yysslim;",
@@ -294,7 +295,10 @@ const char * const body[] =
     "                YYPREFIX, yystate, yyn, yyrule[yyn]);",
     "#endif",
     "    yym = yylen[yyn];",
-    "    yyval = yyvsp[1-yym];",
+    "    if (yym)",
+    "        yyval = yyvsp[1-yym];",
+    "    else",
+    "        yyval = yyvalzero;",
     "    switch (yyn)",
     "    {",
     0
