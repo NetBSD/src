6e6541e58fcb65c8a29dce621675652938ed872b
tsutsui <tsutsui@NetBSD.org>
2008-07-20 16:28:24 +0000

    No need to specify '&' to get array's address. No binary changes.

--- sys/arch/atari/dev/atari5380.c
+++ sys/arch/atari/dev/atari5380.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: atari5380.c,v 1.43 2008/07/20 16:23:38 tsutsui Exp $	*/
+/*	$NetBSD: atari5380.c,v 1.44 2008/07/20 16:28:24 tsutsui Exp $	*/
 
 /*
  * Copyright (c) 1995 Leo Weppelman.
@@ -31,7 +31,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: atari5380.c,v 1.43 2008/07/20 16:23:38 tsutsui Exp $");
+__KERNEL_RCSID(0, "$NetBSD: atari5380.c,v 1.44 2008/07/20 16:28:24 tsutsui Exp $");
 
 #include "opt_atariscsi.h"
 
@@ -342,8 +342,8 @@ scsi_tt_dmasetup(SC_REQ *reqp, u_int phase, u_char	mode)
 		SCSI_DMA->s_dma_ctrl = SD_IN;
 		if (machineid & ATARI_HADES)
 		    SCSI_DMA->s_hdma_ctrl &= ~(SDH_BUSERR|SDH_EOP);
-		set_scsi_dma(&(SCSI_DMA->s_dma_ptr), reqp->dm_cur->dm_addr);
-		set_scsi_dma(&(SCSI_DMA->s_dma_cnt), reqp->dm_cur->dm_count);
+		set_scsi_dma(SCSI_DMA->s_dma_ptr, reqp->dm_cur->dm_addr);
+		set_scsi_dma(SCSI_DMA->s_dma_cnt, reqp->dm_cur->dm_count);
 		SET_TT_REG(NCR5380_ICOM, 0);
 		SET_TT_REG(NCR5380_MODE, mode);
 		SCSI_DMA->s_dma_ctrl = SD_ENABLE;
@@ -353,8 +353,8 @@ scsi_tt_dmasetup(SC_REQ *reqp, u_int phase, u_char	mode)
 		SCSI_DMA->s_dma_ctrl = SD_OUT;
 		if (machineid & ATARI_HADES)
 		    SCSI_DMA->s_hdma_ctrl &= ~(SDH_BUSERR|SDH_EOP);
-		set_scsi_dma(&(SCSI_DMA->s_dma_ptr), reqp->dm_cur->dm_addr);
-		set_scsi_dma(&(SCSI_DMA->s_dma_cnt), reqp->dm_cur->dm_count);
+		set_scsi_dma(SCSI_DMA->s_dma_ptr, reqp->dm_cur->dm_addr);
+		set_scsi_dma(SCSI_DMA->s_dma_cnt, reqp->dm_cur->dm_count);
 		SET_TT_REG(NCR5380_MODE, mode);
 		SET_TT_REG(NCR5380_ICOM, SC_ADTB);
 		SET_TT_REG(NCR5380_DMSTAT, 0);
