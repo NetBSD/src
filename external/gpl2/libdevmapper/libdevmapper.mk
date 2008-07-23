3e954d1e58974b27c3f37c6dabeba1011aec7090
lukem <lukem>
2008-07-23 13:44:02 +0000

    rototill, per discussion with haad@ * remove unnecessary .includes * fix the whitespace ! * fix determining OBJDIRS * refactor Makefiles; moving OBJDIR setting to the only place they're used * remove unnecessary variables * use NOxxx= instead of MKxxx=no * .WAIT for libdevmapper before lvm2tools

--- external/gpl2/libdevmapper/libdevmapper.mk
+++ external/gpl2/libdevmapper/libdevmapper.mk
@@ -1,16 +1,8 @@
+#	$NetBSD: libdevmapper.mk,v 1.1.2.3 2008/07/23 13:44:02 lukem Exp $
+
 .include <bsd.own.mk>
 
-LIBDM_VERSION=   1.02.27 
+LIBDM_SRCDIR=	${NETBSDSRCDIR}/external/gpl2/libdevmapper
+LIBDM_DISTDIR=	${NETBSDSRCDIR}/external/gpl2/libdevmapper/dist
 
-LIBDM_SRCDIR=    ${NETBSDSRCDIR}/external/gpl2/libdevmapper
-LIBDM_DISTDIR=   ${NETBSDSRCDIR}/external/gpl2/libdevmapper/dist
-
-LIBDM_PREFIX=    /usr
-
-.if !defined(LIBDMOBJDIR.devmapper)
-LIBDMOBJDIR.devmapper	!=    cd ${LIBDM_SRCDIR}/lib/libdevmapper && ${PRINTOBJDIR}
-.MAKEOVERRIDES		+=    LIBDMOBJDIR.devmapper
-.endif
-
-#LIBDMLIB.devmapper	 =    ${LIBDMOBJDIR.devmapper}/libdevmapper.a
-
+LIBDM_PREFIX=	/usr
