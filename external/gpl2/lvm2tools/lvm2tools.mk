96dd396d6924a37ea25e8a59dd12f48b55280d12
lukem <lukem@NetBSD.org>
2008-07-23 13:44:02 +0000

    rototill, per discussion with haad@ * remove unnecessary .includes * fix the whitespace ! * fix determining OBJDIRS * refactor Makefiles; moving OBJDIR setting to the only place they're used * remove unnecessary variables * use NOxxx= instead of MKxxx=no * .WAIT for libdevmapper before lvm2tools

--- external/gpl2/lvm2tools/lvm2tools.mk
+++ external/gpl2/lvm2tools/lvm2tools.mk
@@ -1,22 +1,21 @@
+#	$NetBSD: lvm2tools.mk,v 1.1.2.2 2008/07/23 13:44:02 lukem Exp $
+
 .include <bsd.own.mk>
 
-LVM2TOOLS_VERSION=   2.02.39
+LVM2TOOLS_SRCDIR=	${NETBSDSRCDIR}/external/gpl2/lvm2tools
+LVM2TOOLS_DISTDIR=	${NETBSDSRCDIR}/external/gpl2/lvm2tools/dist
 
-LVM2TOOLS_SRCDIR=    ${NETBSDSRCDIR}/external/gpl2/lvm2tools
-LVM2TOOLS_DISTDIR=   ${NETBSDSRCDIR}/external/gpl2/lvm2tools/dist
+LVM2TOOLS_PREFIX=	/
 
-LVM2TOOLS_PREFIX=    /
+LIBDM_SRCDIR=		${NETBSDSRCDIR}/external/gpl2/libdevmapper
+LIBDM_INCLUDE=		${LIBDM_SRCDIR}/dist/include
 
-LVM2TOOLSOBJDIR.liblvm= ${LVM2TOOLS_SRCDIR}/lib/liblvm
-
-
-LIBDM_SRCDIR=    ${NETBSDSRCDIR}/external/gpl2/libdevmapper
-LIBDM_INCLUDE=    ${LIBDM_SRCDIR}/dist/include
-LIBDM_OBJDIR.libdevmapper=    ${LIBDM_SRCDIR}/lib/libdevmapper/
-
+#
+#LIBDM_OBJDIR.libdevmapper=${LIBDM_SRCDIR}/lib/libdevmapper/
+#
 #.if !defined(LVM2TOOLSOBJDIR.liblvm)
-#LVM2TOOLSOBJDIR.liblvm   !=    cd ${LVM2TOOLS_SRCDIR}/lib/liblvm & ${PRINTOBJDIR}
-#.MAKEOVERRIDES           +=    LVM2TOOLSOBJDIR.liblvm 
+#LVM2TOOLSOBJDIR.liblvm!=	cd ${LVM2TOOLS_SRCDIR}/lib/liblvm && ${PRINTOBJDIR}
+#.MAKEOVERRIDES+=	LVM2TOOLSOBJDIR.liblvm 
 #.endif
-
-#LVM2TOOLS.liblvm=    ${LVM2TOOLSOBJDIR.liblvm}/liblvm.a
+#
+#LVM2TOOLS.liblvm=	${LVM2TOOLSOBJDIR.liblvm}/liblvm.a
