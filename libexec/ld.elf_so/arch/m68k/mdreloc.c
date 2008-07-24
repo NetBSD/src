3d2ae35a3b073f82d92c0f505cfd2f054b52aca8
matt <matt>
2008-07-24 04:39:25 +0000

    Refactor common code to _rtld_relocate_plt_object to i386 and arm so they act like the other versions. In _rtld_bind, if the result is 0, call _rtld_die. Initialize _rtld_sym_zero.st_value to -_rtld_objself.maprelocbase.  Now when the symbol is resolved, st_value + maprelocbase will equal 0 and the above check in _rtld_bind will fire and a call to NULL will be avoided.

--- libexec/ld.elf_so/arch/m68k/mdreloc.c
+++ libexec/ld.elf_so/arch/m68k/mdreloc.c
@@ -1,8 +1,8 @@
-/*	$NetBSD: mdreloc.c,v 1.20 2006/05/20 07:09:44 mrg Exp $	*/
+/*	$NetBSD: mdreloc.c,v 1.21 2008/07/24 04:39:25 matt Exp $	*/
 
 #include <sys/cdefs.h>
 #ifndef lint
-__RCSID("$NetBSD: mdreloc.c,v 1.20 2006/05/20 07:09:44 mrg Exp $");
+__RCSID("$NetBSD: mdreloc.c,v 1.21 2008/07/24 04:39:25 matt Exp $");
 #endif /* not lint */
 
 #include <sys/types.h>
@@ -197,7 +197,7 @@ _rtld_bind(const Obj_Entry *obj, Elf_Word reloff)
 	result = 0;	/* XXX gcc */
 
 	err = _rtld_relocate_plt_object(obj, rela, &result);
-	if (err)
+	if (err || result == 0)
 		_rtld_die();
 
 	return (caddr_t)result;
