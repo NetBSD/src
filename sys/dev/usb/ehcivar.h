113366c3957031504f6f30d6edab3873834a625f
jmcneill <jmcneill@NetBSD.org>
2008-08-02 22:23:18 +0000

    High speed isochronous support, from Jeremy Morse as part of Google Summer of Code 2008.

--- sys/dev/usb/ehcivar.h
+++ sys/dev/usb/ehcivar.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: ehcivar.h,v 1.31 2008/06/28 17:42:53 bouyer Exp $ */
+/*	$NetBSD: ehcivar.h,v 1.32 2008/08/02 22:23:18 jmcneill Exp $ */
 
 /*
  * Copyright (c) 2001 The NetBSD Foundation, Inc.
@@ -54,12 +54,36 @@ typedef struct ehci_soft_qh {
 #define EHCI_SQH_SIZE ((sizeof (struct ehci_soft_qh) + EHCI_QH_ALIGN - 1) / EHCI_QH_ALIGN * EHCI_QH_ALIGN)
 #define EHCI_SQH_CHUNK (EHCI_PAGE_SIZE / EHCI_SQH_SIZE)
 
+typedef struct ehci_soft_itd {
+	ehci_itd_t itd;
+	union {
+		struct {
+			/* soft_itds links in a periodic frame*/
+			struct ehci_soft_itd *next;
+			struct ehci_soft_itd *prev;
+		} frame_list;
+		/* circular list of free itds */
+		LIST_ENTRY(ehci_soft_itd) free_list;
+	} u;
+	struct ehci_soft_itd *xfer_next; /* Next soft_itd in xfer */
+	ehci_physaddr_t physaddr;
+	usb_dma_t dma;
+	int offs;
+	int slot;
+	struct timeval t; /* store free time */
+} ehci_soft_itd_t;
+#define EHCI_ITD_SIZE ((sizeof(struct ehci_soft_itd) + EHCI_QH_ALIGN - 1) / EHCI_ITD_ALIGN * EHCI_ITD_ALIGN)
+#define EHCI_ITD_CHUNK (EHCI_PAGE_SIZE / EHCI_ITD_SIZE)
+
 struct ehci_xfer {
 	struct usbd_xfer xfer;
 	struct usb_task	abort_task;
 	LIST_ENTRY(ehci_xfer) inext; /* list of active xfers */
 	ehci_soft_qtd_t *sqtdstart;
 	ehci_soft_qtd_t *sqtdend;
+	ehci_soft_itd_t *itdstart;
+	ehci_soft_itd_t *itdend;
+	u_int isoc_len;
 	int isdone;	/* used only when DIAGNOSTIC is defined */
 };
 #define EXFER(xfer) ((struct ehci_xfer *)(xfer))
@@ -80,6 +104,8 @@ struct ehci_soft_islot {
 
 #define EHCI_HASH_SIZE 128
 #define EHCI_COMPANION_MAX 8
+
+#define EHCI_FREE_LIST_INTERVAL 100
 
 typedef struct ehci_softc {
 	device_t sc_dev;
@@ -107,10 +133,16 @@ typedef struct ehci_softc {
 
 	struct ehci_soft_islot sc_islots[EHCI_INTRQHS];
 
+	/* jcmm - an array matching sc_flist, but with software pointers,
+	 * not hardware address pointers
+	 */
+	struct ehci_soft_itd **sc_softitds;
+
 	LIST_HEAD(, ehci_xfer) sc_intrhead;
 
 	ehci_soft_qh_t *sc_freeqhs;
 	ehci_soft_qtd_t *sc_freeqtds;
+	LIST_HEAD(sc_freeitds, ehci_soft_itd) sc_freeitds;
 
 	int sc_noport;
 	u_int8_t sc_hasppc;		/* has Port Power Control */
