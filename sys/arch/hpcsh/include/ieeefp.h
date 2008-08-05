91d4704c12a10b4a5342b04ef0a12f227cef808c
matt <matt@NetBSD.org>
2008-08-05 16:47:41 +0000

    Update <machine/ieeefp.h> to use the C99 FE_* definitions instead of the NetBSD defined ones.  Redefine the NetBSD ones in terms of the C99 ones. Step 1 to having <fenv.h>

--- sys/arch/hpcsh/include/ieeefp.h
+++ sys/arch/hpcsh/include/ieeefp.h
@@ -1,7 +1,3 @@
-/*	$NetBSD: ieeefp.h,v 1.2 2001/01/28 03:31:31 uch Exp $	*/
-#ifndef _HPCSH_IEEEFP_H_
-#define _HPCSH_IEEEFP_H_
+/*	$NetBSD: ieeefp.h,v 1.3 2008/08/05 16:47:41 matt Exp $	*/
 
 #include <sh3/ieeefp.h>
-
-#endif /* _HPCSH_IEEEFP_H_ */
