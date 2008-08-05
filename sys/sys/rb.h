3441e035cb1cec21d49343bea993d233ffe4c4de
matt <matt@NetBSD.org>
2008-08-05 04:12:09 +0000

    RBSMALL is dead.

--- sys/sys/rb.h
+++ sys/sys/rb.h
@@ -1,4 +1,4 @@
-/* $NetBSD: rb.h,v 1.11 2008/07/03 18:30:39 simonb Exp $ */
+/* $NetBSD: rb.h,v 1.12 2008/08/05 04:12:09 matt Exp $ */
 
 /*-
  * Copyright (c) 2001 The NetBSD Foundation, Inc.
@@ -135,9 +135,7 @@ struct rb_tree_ops {
 struct rb_tree {
 	struct rb_node *rbt_root;
 	const struct rb_tree_ops *rbt_ops;
-#ifndef RBSMALL
 	struct rb_node *rbt_minmax[2];
-#endif
 #ifdef RBDEBUG
 	struct rb_node_qh rbt_nodes;
 #endif
