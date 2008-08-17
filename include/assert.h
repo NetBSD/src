1e9b9138c47dcd890add8557a7fb84207bc334c4
gmcgarry <gmcgarry@NetBSD.org>
2008-08-17 00:24:35 +0000

    Define away __assert_function__ if __lint__.

--- include/assert.h
+++ include/assert.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: assert.h,v 1.18 2005/02/03 04:39:32 perry Exp $	*/
+/*	$NetBSD: assert.h,v 1.19 2008/08/17 00:24:35 gmcgarry Exp $	*/
 
 /*-
  * Copyright (c) 1992, 1993
@@ -84,7 +84,9 @@
 #endif /* _DIAGNOSTIC */
 
 
-#if defined(__STDC_VERSION__) && __STDC_VERSION__ >= 199901L
+#if defined(__lint__)
+#define	__assert_function__	(__static_cast(const void *,0))
+#elif defined(__STDC_VERSION__) && __STDC_VERSION__ >= 199901L
 #define	__assert_function__	__func__
 #elif __GNUC_PREREQ__(2, 6)
 #define	__assert_function__	__PRETTY_FUNCTION__
