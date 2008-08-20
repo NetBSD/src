9011f961a2f17c459d8ec75c9cd068c3b079a31f
bouyer <bouyer@NetBSD.org>
2008-08-20 19:39:29 +0000

    Pull up following revision(s) (requested by plunky in ticket #1176): 	sys/netbt/l2cap_lower.c: revision 1.8 drop packets that exceed the negotiated MTU

--- sys/netbt/l2cap_lower.c
+++ sys/netbt/l2cap_lower.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: l2cap_lower.c,v 1.1.18.1 2007/07/19 16:04:17 liamjfoy Exp $	*/
+/*	$NetBSD: l2cap_lower.c,v 1.1.18.2 2008/08/20 19:39:29 bouyer Exp $	*/
 
 /*-
  * Copyright (c) 2005 Iain Hibbert.
@@ -31,7 +31,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: l2cap_lower.c,v 1.1.18.1 2007/07/19 16:04:17 liamjfoy Exp $");
+__KERNEL_RCSID(0, "$NetBSD: l2cap_lower.c,v 1.1.18.2 2008/08/20 19:39:29 bouyer Exp $");
 
 #include <sys/param.h>
 #include <sys/kernel.h>
@@ -137,6 +137,7 @@ l2cap_recv_frame(struct mbuf *m, struct hci_link *link)
 
 	chan = l2cap_cid_lookup(hdr.dcid);
 	if (chan != NULL && chan->lc_link == link
+	    && chan->lc_imtu >= hdr.length
 	    && chan->lc_state == L2CAP_OPEN) {
 		(*chan->lc_proto->input)(chan->lc_upper, m);
 		return;
