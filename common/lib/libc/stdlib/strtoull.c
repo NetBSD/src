0f5edaf28fec92c0ab22468e55bac7abecf9f703
joerg <joerg@NetBSD.org>
2008-08-20 12:42:26 +0000

    Unify the implementation of strto{l,ul,ll,ull,imax,umax,q,uq} into one version for signed and one version for unsigned data types.
    Add a check for supported bases and set errno (userland) or panic
    (kernel, libsa) otherwise.
    
    Make strto{ll,ull,imax,umax} normal symbols and just keep the underscore
    versions as strong alias.
    
    Obtained from DragonFly, based on the wide char version from Citrus.
    Reviewed by christos@

--- common/lib/libc/stdlib/strtoull.c
+++ common/lib/libc/stdlib/strtoull.c
@@ -1,8 +1,9 @@
-/*	$NetBSD: strtoull.c,v 1.1 2006/10/08 03:14:55 thorpej Exp $	*/
+/* $NetBSD: strtoull.c,v 1.2 2008/08/20 12:42:26 joerg Exp $ */
 
-/*
- * Copyright (c) 1990, 1993
- *	The Regents of the University of California.  All rights reserved.
+/*-
+ * Copyright (c) 2005 The DragonFly Project.  All rights reserved.
+ * Copyright (c) 2003 Citrus Project,
+ * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -12,14 +13,11 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. Neither the name of the University nor the names of its contributors
- *    may be used to endorse or promote products derived from this software
- *    without specific prior written permission.
  *
- * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
@@ -29,121 +27,27 @@
  * SUCH DAMAGE.
  */
 
-#if !defined(_KERNEL) && !defined(_STANDALONE)
 #include <sys/cdefs.h>
-#if defined(LIBC_SCCS) && !defined(lint)
-#if 0
-static char sccsid[] = "from: @(#)strtoul.c	8.1 (Berkeley) 6/4/93";
-#else
-__RCSID("$NetBSD: strtoull.c,v 1.1 2006/10/08 03:14:55 thorpej Exp $");
-#endif
-#endif /* LIBC_SCCS and not lint */
+__RCSID("$NetBSD: strtoull.c,v 1.2 2008/08/20 12:42:26 joerg Exp $");
 
-#include "namespace.h"
+#if !defined(_KERNEL) && !defined(_STANDALONE)
 #include <assert.h>
 #include <ctype.h>
 #include <errno.h>
 #include <limits.h>
+#include <stdint.h>
 #include <stdlib.h>
-
-#ifdef __weak_alias
-__weak_alias(strtoull, _strtoull)
-#endif
-
-#else /* !_KERNEL && !_STANDALONE */
+#else
 #include <sys/param.h>
 #include <lib/libkern/libkern.h>
-#define	isspace(x) ((x) == ' ' || (x) == '\t' || (x) == '\n' || (x) == '\r')
-#define	isdigit(x) ((x) >= '0' && (x) <= '9')
-#define	isalpha(x) (((x) >= 'a' && (x) <= 'z') || ((x) >= 'A' && (x) <= 'Z'))
-#define	toupper(x) ((x) & ~0x20)
 #endif
 
-/*
- * Convert a string to an unsigned long long integer.
- *
- * Ignores `locale' stuff.  Assumes that the upper and lower case
- * alphabets and digits are each contiguous.
- */
-/* LONGLONG */
-unsigned long long int
-strtoull(nptr, endptr, base)
-	const char *nptr;
-	char **endptr;
-	int base;
-{
-	const char *s;
-	/* LONGLONG */
-	unsigned long long int acc, cutoff;
-	int c;
-	int neg, any, cutlim;
+#define	_FUNCNAME	strtoull
+#define	__UINT		unsigned long long int
+#define	__UINT_MAX	ULLONG_MAX
 
-	_DIAGASSERT(nptr != NULL);
-	/* endptr may be NULL */
+#include "_strtoul.h"
 
-	/*
-	 * See strtol for comments as to the logic used.
-	 */
-	s = nptr;
-	do {
-		c = (unsigned char) *s++;
-	} while (isspace(c));
-	if (c == '-') {
-		neg = 1;
-		c = *s++;
-	} else {
-		neg = 0;
-		if (c == '+')
-			c = *s++;
-	}
-	if ((base == 0 || base == 16) &&
-	    c == '0' && (*s == 'x' || *s == 'X')) {
-		c = s[1];
-		s += 2;
-		base = 16;
-	}
-	if (base == 0)
-		base = c == '0' ? 8 : 10;
-
-	/* LONGLONG */
-	cutoff = ULLONG_MAX / (unsigned long long int)base;
-	/* LONGLONG */
-	cutlim = (int)(ULLONG_MAX % (unsigned long long int)base);
-	for (acc = 0, any = 0;; c = (unsigned char) *s++) {
-		if (isdigit(c))
-			c -= '0';
-		else if (isalpha(c)) {
-#if defined(_KERNEL) || defined(_STANDALONE)
-			c = toupper(c) - 'A' + 10;
-#else
-			c -= isupper(c) ? 'A' - 10 : 'a' - 10;
+#if !defined(_KERNEL) && !defined(_STANDALONE)
+__strong_alias(_strtoull, strtoull)
 #endif
-		} else
-			break;
-		if (c >= base)
-			break;
-		if (any < 0)
-			continue;
-		if (acc > cutoff || (acc == cutoff && c > cutlim)) {
-#if defined(_KERNEL) || defined(_STANDALONE)
-			if (endptr)
-				*endptr = __UNCONST(nptr);
-			return (ULLONG_MAX);
-#else
-			any = -1;
-			acc = ULLONG_MAX;
-			errno = ERANGE;
-#endif
-		} else {
-			any = 1;
-			/* LONGLONG */
-			acc *= (unsigned long long int)base;
-			acc += c;
-		}
-	}
-	if (neg && any > 0)
-		acc = -acc;
-	if (endptr != 0)
-		*endptr = __UNCONST(any ? s - 1 : nptr);
-	return (acc);
-}
