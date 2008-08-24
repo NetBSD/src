f22556500ab991da1084e72af1952fa092c66baa
tsutsui <tsutsui@NetBSD.org>
2008-08-24 13:03:39 +0000

    Pacify haltwo audio on shutdown hook to avoid noise on poweroff.

--- sys/arch/sgimips/hpc/haltwovar.h
+++ sys/arch/sgimips/hpc/haltwovar.h
@@ -1,4 +1,4 @@
-/* $NetBSD: haltwovar.h,v 1.4 2007/03/04 06:00:39 christos Exp $ */
+/* $NetBSD: haltwovar.h,v 1.5 2008/08/24 13:03:39 tsutsui Exp $ */
 
 /*
  * Copyright (c) 2003 Ilpo Ruotsalainen
@@ -81,6 +81,8 @@ struct haltwo_softc {
 	bus_space_handle_t sc_aes_sh;
 	bus_space_handle_t sc_vol_sh;
 	bus_space_handle_t sc_syn_sh;
+
+	void *sc_sdhook;
 };
 
 #endif
