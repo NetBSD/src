7c36a4a5dc9fc9be0ca35eebaa8abff1d07929b8
pooka <pooka@NetBSD.org>
2008-08-24 18:26:05 +0000

    fix tyop in error message, no functional change

--- usr.sbin/puffs/mount_9p/fs.c
+++ usr.sbin/puffs/mount_9p/fs.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: fs.c,v 1.7 2007/11/30 19:02:38 pooka Exp $	*/
+/*	$NetBSD: fs.c,v 1.8 2008/08/24 18:26:05 pooka Exp $	*/
 
 /*
  * Copyright (c) 2007  Antti Kantee.  All Rights Reserved.
@@ -27,7 +27,7 @@
 
 #include <sys/cdefs.h>
 #ifndef lint
-__RCSID("$NetBSD: fs.c,v 1.7 2007/11/30 19:02:38 pooka Exp $");
+__RCSID("$NetBSD: fs.c,v 1.8 2008/08/24 18:26:05 pooka Exp $");
 #endif /* !lint */
 
 #include <assert.h>
@@ -46,7 +46,7 @@ __RCSID("$NetBSD: fs.c,v 1.7 2007/11/30 19:02:38 pooka Exp $");
 	puffs_framebuf_seekset(a2, 0);					\
 	*(a4) = 0;							\
 	rv = fname(a1, a2, a3, a4);					\
-	if (rv || a4 == 0) errx(1, "p9p_handshake_ io failed %d, %d", rv, *a4) 
+	if (rv || a4 == 0) errx(1, "p9p_handshake io failed %d, %d", rv, *a4) 
 
 struct puffs_node *
 p9p_handshake(struct puffs_usermount *pu,
