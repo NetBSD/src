01d7d02b4e69fca31a058a4317ca505c9f5c1038
hannken <hannken>
2008-08-24 09:19:03 +0000

    Destroy cb_buf before pool_put().
    Resolves PR #38636

--- sys/dev/ata/ld_ataraid.c
+++ sys/dev/ata/ld_ataraid.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: ld_ataraid.c,v 1.27 2008/05/04 13:59:41 xtraeme Exp $	*/
+/*	$NetBSD: ld_ataraid.c,v 1.28 2008/08/24 09:19:03 hannken Exp $	*/
 
 /*
  * Copyright (c) 2003 Wasabi Systems, Inc.
@@ -45,7 +45,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: ld_ataraid.c,v 1.27 2008/05/04 13:59:41 xtraeme Exp $");
+__KERNEL_RCSID(0, "$NetBSD: ld_ataraid.c,v 1.28 2008/08/24 09:19:03 hannken Exp $");
 
 #include "rnd.h"
 
@@ -515,6 +515,7 @@ ld_ataraid_iodone_raid0(struct buf *vbp)
 			other_cbp->cb_flags |= CBUF_IODONE;
 	}
 	count = cbp->cb_buf.b_bcount;
+	buf_destroy(&cbp->cb_buf);
 	CBUF_PUT(cbp);
 
 	if (other_cbp != NULL)
