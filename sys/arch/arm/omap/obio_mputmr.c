400c13d3b0f77e149752e9136ca964f69d538b2b
matt <matt>
2008-08-27 11:03:09 +0000

    Cleanup OMAP2 files.  Change most omap2430* to omap2_*.  This is in preperation for the OMAP 3530 (OMAP 3) support.  Some 3530 definitions added.

--- sys/arch/arm/omap/obio_mputmr.c
+++ sys/arch/arm/omap/obio_mputmr.c
@@ -1,4 +1,4 @@
-/* $NetBSD: obio_mputmr.c,v 1.2 2008/04/27 18:58:45 matt Exp $ */
+/* $NetBSD: obio_mputmr.c,v 1.3 2008/08/27 11:03:10 matt Exp $ */
 
 /*
  * Based on omap_mputmr.c
@@ -101,7 +101,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: obio_mputmr.c,v 1.2 2008/04/27 18:58:45 matt Exp $");
+__KERNEL_RCSID(0, "$NetBSD: obio_mputmr.c,v 1.3 2008/08/27 11:03:10 matt Exp $");
 
 #include "opt_omap.h"
 #include "opt_cpuoptions.h"
@@ -118,13 +118,13 @@ __KERNEL_RCSID(0, "$NetBSD: obio_mputmr.c,v 1.2 2008/04/27 18:58:45 matt Exp $")
 #include <machine/bus.h>
 #include <machine/intr.h>
 
-#include <arm/omap/omap2430obiovar.h>
+#include <arm/omap/omap2_obiovar.h>
 
 #include <arm/omap/omap2_mputmrvar.h>
 #include <arm/omap/omap2_mputmrreg.h>
 
 
-#include <arm/omap/omap2430reg.h>
+#include <arm/omap/omap2_reg.h>
 
 #ifndef OMAP_MPU_TIMER_CLOCK_FREQ
 #error Specify the timer frequency in Hz with the OMAP_MPU_TIMER_CLOCK_FREQ option.
