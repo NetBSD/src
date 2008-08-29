6a294d3ed4ca5d249f07b734b009bd11c4dfc46d
bouyer <bouyer>
2008-08-29 20:53:57 +0000

    apply patch (requested by adrianp in ticket #1189): 	dist/bind/CHANGES				patch 	dist/bind/COPYRIGHT				patch 	dist/bind/configure				patch 	dist/bind/configure.in				patch 	dist/bind/version				patch 	dist/bind/bin/dig/dighost.c			patch 	dist/bind/bin/named/client.c			patch 	dist/bind/bin/named/config.c			patch 	dist/bind/bin/named/controlconf.c		patch 	dist/bind/bin/named/interfacemgr.c		patch 	dist/bind/bin/named/lwresd.c			patch 	dist/bind/bin/named/named.conf.docbook		patch 	dist/bind/bin/named/server.c			patch 	dist/bind/bin/rndc/rndc.c			patch 	dist/bind/bin/tests/sig0_test.c			patch 	dist/bind/bin/tests/sock_test.c			patch 	dist/bind/bin/tests/system/ifconfig.sh		patch 	dist/bind/doc/arm/Bv9ARM-book.xml		patch 	dist/bind/lib/bind/configure			patch 	dist/bind/lib/bind/configure.in			patch 	dist/bind/lib/dns/api				patch 	dist/bind/lib/dns/dispatch.c			patch 	dist/bind/lib/dns/request.c			patch 	dist/bind/lib/dns/resolver.c			patch 	dist/bind/lib/dns/xfrin.c			patch 	dist/bind/lib/dns/include/dns/dispatch.h	patch 	dist/bind/lib/isc/api				patch 	dist/bind/lib/isc/timer.c			patch 	dist/bind/lib/isc/include/isc/resource.h	patch 	dist/bind/lib/isc/include/isc/socket.h		patch 	dist/bind/lib/isc/include/isc/timer.h		patch 	dist/bind/lib/isc/unix/app.c			patch 	dist/bind/lib/isc/unix/resource.c		patch 	dist/bind/lib/isc/unix/socket.c			patch 	dist/bind/lib/isc/unix/socket_p.h		patch 	dist/bind/lib/isc/win32/libisc.def		patch 	dist/bind/lib/isc/win32/resource.c		patch 	dist/bind/lib/isc/win32/socket.c		patch 	dist/bind/lib/isccfg/api			patch 	dist/bind/lib/isccfg/namedconf.c		patch 	doc/3RDPARTY					patch
    Update BIND to 9.3.5-P2 (performance and bug fixes)

--- dist/bind/lib/isc/include/isc/timer.h
+++ dist/bind/lib/isc/include/isc/timer.h
@@ -1,10 +1,10 @@
-/*	$NetBSD: timer.h,v 1.1.1.3.4.1 2007/05/17 00:42:38 jdc Exp $	*/
+/*	$NetBSD: timer.h,v 1.1.1.3.4.1.2.1 2008/08/29 20:54:00 bouyer Exp $	*/
 
 /*
- * Copyright (C) 2004, 2005  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2004, 2005, 2008  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 1998-2002  Internet Software Consortium.
  *
- * Permission to use, copy, modify, and distribute this software for any
+ * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
  * copyright notice and this permission notice appear in all copies.
  *
@@ -17,7 +17,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* Id: timer.h,v 1.31.18.3 2005/10/26 06:50:50 marka Exp */
+/* Id: timer.h,v 1.31.18.3.52.2 2008/07/24 23:48:09 tbox Exp */
 
 #ifndef ISC_TIMER_H
 #define ISC_TIMER_H 1
@@ -78,6 +78,7 @@
 #include <isc/event.h>
 #include <isc/eventclass.h>
 #include <isc/lang.h>
+#include <isc/time.h>
 
 ISC_LANG_BEGINDECLS
 
@@ -95,6 +96,7 @@ typedef enum {
 
 typedef struct isc_timerevent {
 	struct isc_event	common;
+	isc_time_t		due;
 } isc_timerevent_t;
 
 #define ISC_TIMEREVENT_FIRSTEVENT	(ISC_EVENTCLASS_TIMER + 0)
