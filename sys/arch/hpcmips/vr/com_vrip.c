ef7ede5cb6763ec88659888cb8eca0f29708fac5
tsutsui <tsutsui@NetBSD.org>
2008-08-29 12:08:30 +0000

    Fix a fallout of device_t/softc split for com(4) devices. Per PR port-hpcmips/39426 from Nozomu Ando.

--- sys/arch/hpcmips/vr/com_vrip.c
+++ sys/arch/hpcmips/vr/com_vrip.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: com_vrip.c,v 1.20 2008/03/14 15:09:10 cube Exp $	*/
+/*	$NetBSD: com_vrip.c,v 1.21 2008/08/29 12:08:30 tsutsui Exp $	*/
 
 /*-
  * Copyright (c) 1999 SASAKI Takesi. All rights reserved.
@@ -35,7 +35,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: com_vrip.c,v 1.20 2008/03/14 15:09:10 cube Exp $");
+__KERNEL_RCSID(0, "$NetBSD: com_vrip.c,v 1.21 2008/08/29 12:08:30 tsutsui Exp $");
 
 #include "opt_kgdb.h"
 
@@ -197,7 +197,7 @@ com_vrip_attach(device_t parent, device_t self, void *aux)
 
 	DPRINTF(("Establish intr"));
 	if (!vrip_intr_establish(va->va_vc, va->va_unit, 0, IPL_TTY,
-	    comintr, self)) {
+	    comintr, sc)) {
 		aprint_error_dev(self, "can't map interrupt line.\n");
 	}
 
