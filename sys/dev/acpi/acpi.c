1074527216fe6fc576589d25b856a06f2c75eff2
jmcneill <jmcneill@NetBSD.org>
2008-09-10 03:56:12 +0000

    PR# 38683 - T61 cannot suspend with recent kernels
    Don't restore spl until after AcpiLeaveSleepState.

--- sys/dev/acpi/acpi.c
+++ sys/dev/acpi/acpi.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: acpi.c,v 1.118 2008/07/15 16:15:28 dyoung Exp $	*/
+/*	$NetBSD: acpi.c,v 1.119 2008/09/10 03:56:12 jmcneill Exp $	*/
 
 /*-
  * Copyright (c) 2003, 2007 The NetBSD Foundation, Inc.
@@ -70,7 +70,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: acpi.c,v 1.118 2008/07/15 16:15:28 dyoung Exp $");
+__KERNEL_RCSID(0, "$NetBSD: acpi.c,v 1.119 2008/09/10 03:56:12 jmcneill Exp $");
 
 #include "opt_acpi.h"
 #include "opt_pcifixup.h"
@@ -1271,11 +1271,13 @@ acpi_enter_sleep_state(struct acpi_softc *sc, int state)
 				    AcpiFormatException(ret));
 			AcpiLeaveSleepState((UINT8)state);
 		} else {
+			int s = splhigh();
 			err = acpi_md_sleep(state);
 			if (state == ACPI_STATE_S4)
 				AcpiEnable();
 			pmf_system_bus_resume(PMF_F_NONE);
 			AcpiLeaveSleepState((UINT8)state);
+			splx(s);
 			pmf_system_resume(PMF_F_NONE);
 		}
 
