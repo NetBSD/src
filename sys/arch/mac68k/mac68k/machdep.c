8743d6a02fc40001356d54f81745d0684669db5f
tsutsui <tsutsui@NetBSD.org>
2008-09-14 15:03:17 +0000

    Remove ancient workaround hacks for gcc 2.7.2.

--- sys/arch/mac68k/mac68k/machdep.c
+++ sys/arch/mac68k/mac68k/machdep.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: machdep.c,v 1.321 2008/07/02 17:28:56 ad Exp $	*/
+/*	$NetBSD: machdep.c,v 1.322 2008/09/14 15:03:17 tsutsui Exp $	*/
 
 /*
  * Copyright (c) 1982, 1990 The Regents of the University of California.
@@ -107,7 +107,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: machdep.c,v 1.321 2008/07/02 17:28:56 ad Exp $");
+__KERNEL_RCSID(0, "$NetBSD: machdep.c,v 1.322 2008/09/14 15:03:17 tsutsui Exp $");
 
 #include "opt_adb.h"
 #include "opt_ddb.h"
@@ -516,9 +516,6 @@ cpu_reboot(int howto, char *bootstr)
 {
 	extern u_long maxaddr;
 
-#if __GNUC__	/* XXX work around lame compiler problem (gcc 2.7.2) */
-	(void)&howto;
-#endif
 	/* take a snap shot before clobbering any registers */
 	if (curlwp->l_addr)
 		savectx(&curlwp->l_addr->u_pcb);
