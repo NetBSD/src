9a8800d8b76520a441956564ed6187c468c277b1
jakllsch <jakllsch>
2008-09-14 21:53:49 +0000

    A few cleanups for siisata(4)
    No functional changes intended (except maybe un-reseting the chip a bit later
    in attach).
    
     - pass fewer arguments to local functions where arguments can be derived from
       an existing argument
     - some coding style fixes
     - more abstraction for PRB activation and deactivation
     - bus_dma(9) properification in error cases
     - undefine SIISATA_DEBUG, and cleanup variables used only for DEBUG_PRINT()s

--- sys/dev/ic/siisatavar.h
+++ sys/dev/ic/siisatavar.h
@@ -1,4 +1,4 @@
-/* $NetBSD: siisatavar.h,v 1.1 2008/05/23 21:11:40 jnemeth Exp $ */
+/* $NetBSD: siisatavar.h,v 1.2 2008/09/14 21:53:49 jakllsch Exp $ */
 /* Id: siisatavar.h,v 1.15 2008/05/22 13:48:54 jakllsch Exp  */
 
 /* from ahcisatavar.h */
@@ -64,8 +64,6 @@
 
 #include <dev/ic/siisatareg.h>
 #include <dev/ata/atavar.h>
-
-#define SIISATA_DEBUG
 
 #define DEBUG_INTR   0x01
 #define DEBUG_XFERS  0x02
