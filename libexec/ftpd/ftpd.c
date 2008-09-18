eb4f35b4b451ef0b215059a80b1875be6747b33b
bouyer <bouyer@NetBSD.org>
2008-09-18 18:13:12 +0000

    Pull up following revision(s) (requested by lukem in ticket #1200): 	libexec/ftpd/ftpd.c: revision 1.182 Rename cred_t to ftpd_cred_t; the former causes a nameclash on Solaris.

--- libexec/ftpd/ftpd.c
+++ libexec/ftpd/ftpd.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: ftpd.c,v 1.177.6.1 2008/09/18 18:09:21 bouyer Exp $	*/
+/*	$NetBSD: ftpd.c,v 1.177.6.2 2008/09/18 18:13:12 bouyer Exp $	*/
 
 /*
  * Copyright (c) 1997-2004 The NetBSD Foundation, Inc.
@@ -105,7 +105,7 @@ __COPYRIGHT(
 #if 0
 static char sccsid[] = "@(#)ftpd.c	8.5 (Berkeley) 4/28/95";
 #else
-__RCSID("$NetBSD: ftpd.c,v 1.177.6.1 2008/09/18 18:09:21 bouyer Exp $");
+__RCSID("$NetBSD: ftpd.c,v 1.177.6.2 2008/09/18 18:13:12 bouyer Exp $");
 #endif
 #endif /* not lint */
 
@@ -3655,18 +3655,17 @@ cprintf(FILE *fd, const char *fmt, ...)
  */
 #define COPY_STRING(s) (s ? strdup(s) : NULL)
 
-struct cred_t {
+typedef struct {
 	const char *uname;		/* user name */
 	const char *pass;		/* password */
-};
-typedef struct cred_t cred_t;
+} ftpd_cred_t;
 
 static int
 auth_conv(int num_msg, const struct pam_message **msg,
     struct pam_response **resp, void *appdata)
 {
 	int i;
-	cred_t *cred = (cred_t *) appdata;
+	ftpd_cred_t *cred = (ftpd_cred_t *) appdata;
 	struct pam_response *myreply;
 
 	myreply = calloc(num_msg, sizeof *myreply);
@@ -3714,7 +3713,7 @@ auth_pam(struct passwd **ppw, const char *pwstr)
 	const void *item;
 	int rval;
 	int e;
-	cred_t auth_cred = { (*ppw)->pw_name, pwstr };
+	ftpd_cred_t auth_cred = { (*ppw)->pw_name, pwstr };
 	struct pam_conv conv = { &auth_conv, &auth_cred };
 
 	e = pam_start("ftpd", (*ppw)->pw_name, &conv, &pamh);
