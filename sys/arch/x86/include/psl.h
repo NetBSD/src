522c5ae1a62d9682bfd0eeb7c1a6641a27a98477
christos <christos@NetBSD.org>
2008-09-18 15:57:04 +0000

    Define a PSL_CLEARSIG macro for the psl flags to be cleared on signal delivery and use it everywhere.

--- sys/arch/x86/include/psl.h
+++ sys/arch/x86/include/psl.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: psl.h,v 1.4 2008/09/17 12:28:52 christos Exp $	*/
+/*	$NetBSD: psl.h,v 1.5 2008/09/18 15:57:04 christos Exp $	*/
 
 /*-
  * Copyright (c) 1990 The Regents of the University of California.
@@ -70,7 +70,10 @@
 #else
 #define	PSL_USERSTATIC	(PSL_MBO | PSL_MBZ | PSL_I | PSL_IOPL | PSL_NT | PSL_VM | PSL_VIF | PSL_VIP)
 #endif
-#define PSL_USER (PSL_C | PSL_MBO | PSL_PF | PSL_AF | PSL_Z | PSL_N | PSL_T | PSL_V | PSL_D)
+#define PSL_USER	(PSL_C | PSL_MBO | PSL_PF | PSL_AF | PSL_Z | PSL_N | \
+    PSL_T | PSL_V | PSL_D)
+#define	PSL_CLEARSIG	(PSL_T | PSL_VM | PSL_AC | PSL_D)
+
 
 /*
  * ???
