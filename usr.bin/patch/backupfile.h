b7c37ce898e7772e3f91f6896dce226431b18f27
joerg <joerg@NetBSD.org>
2008-09-19 18:33:34 +0000

    Update patch to the version used by DragonFly and derived from OpenBSD. Major changes are: - better detection of double applied patches - rejects remain unified diffs for unified patches - far less limitations, e.g. patch lines may be arbitrary long
    This addresses PR standards/11220 by changing patch -b behavior to be
    POSIX compliant. Old behavior can be obtained using --suffix, which
    works since NetBSD 1.4. pkgsrc has been adjusted accordingly.

--- usr.bin/patch/backupfile.h
+++ usr.bin/patch/backupfile.h
@@ -1,36 +1,42 @@
-/*	$NetBSD: backupfile.h,v 1.5 2003/07/08 01:55:35 kristerw Exp $	*/
+/*
+ * $OpenBSD: backupfile.h,v 1.6 2003/07/28 18:35:36 otto Exp $
+ * $DragonFly: src/usr.bin/patch/backupfile.h,v 1.3 2007/09/29 23:11:10 swildner Exp $
+ * $NetBSD: backupfile.h,v 1.6 2008/09/19 18:33:34 joerg Exp $
+ */
 
-/* backupfile.h -- declarations for making Emacs style backup file names
-   Copyright (C) 1990 Free Software Foundation, Inc.
-
-   This program is free software; you can redistribute it and/or modify
-   it without restriction.
-
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-*/
+/*
+ * backupfile.h -- declarations for making Emacs style backup file names
+ * Copyright (C) 1990 Free Software Foundation, Inc.
+ * 
+ * This program is free software; you can redistribute it and/or modify it
+ * without restriction.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.
+ * 
+ */
 
 /* When to make backup files. */
-enum backup_type
-{
-  /* Never make backups. */
-  none,
+enum backup_type {
+	/* Never make backups. */
+	none,
 
-  /* Make simple backups of every file. */
-  simple,
+	/* Make simple backups of every file. */
+	simple,
 
-  /* Make numbered backups of files that already have numbered backups,
-     and simple backups of the others. */
-  numbered_existing,
+	/*
+	 * Make numbered backups of files that already have numbered backups,
+	 * and simple backups of the others.
+	 */
+	numbered_existing,
 
-  /* Make numbered backups of every file. */
-  numbered
+	/* Make numbered backups of every file. */
+	numbered
 };
 
 extern enum backup_type backup_type;
-extern const char *simple_backup_suffix;
+extern const char	*simple_backup_suffix;
 
-char *find_backup_file_name(char *);
-enum backup_type get_version(char *);
-char *basename(char *);
+char		*find_backup_file_name(const char *file);
+enum backup_type get_version(const char *version);
