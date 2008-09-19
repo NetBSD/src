239c53ce7b0554a9e8a4319586cf1d766969effe
christos <christos>
2008-09-19 20:07:11 +0000

    import current am-utils.

---
+++ external/bsd/am-utils/dist/conf/fh_dref/fh_dref_osf4.h
@@ -0,0 +1,4 @@
+/*	$NetBSD: fh_dref_osf4.h,v 1.1.1.1 2008/09/19 20:07:17 christos Exp $	*/
+
+/* $srcdir/conf/fh_dref/fh_dref_osf4.h */
+#define	NFS_FH_DREF(dst, src) (dst) = (caddr_t) (src)
---
+++ external/bsd/am-utils/dist/fsinfo/null_gram.y
@@ -0,0 +1,6 @@
+/*	$NetBSD: null_gram.y,v 1.1.1.1 2008/09/19 20:07:21 christos Exp $	*/
+
+
+%%
+
+token:
