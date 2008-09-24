d5cefaddd91001f52d622abffcf89b7688cd32ae
christos <christos@NetBSD.org>
2008-09-24 16:24:30 +0000

    fix newer gcc warning.

--- lib/libc/string/strpbrk.c
+++ lib/libc/string/strpbrk.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: strpbrk.c,v 1.17 2008/09/24 14:36:02 christos Exp $	*/
+/*	$NetBSD: strpbrk.c,v 1.18 2008/09/24 16:24:30 christos Exp $	*/
 
 /*-
  * Copyright (c) 2008 Joerg Sonnenberger
@@ -26,7 +26,7 @@
  */
 
 #include <sys/cdefs.h>
-__RCSID("$NetBSD: strpbrk.c,v 1.17 2008/09/24 14:36:02 christos Exp $");
+__RCSID("$NetBSD: strpbrk.c,v 1.18 2008/09/24 16:24:30 christos Exp $");
 
 #include <assert.h>
 #include <inttypes.h>
@@ -39,10 +39,10 @@ __RCSID("$NetBSD: strpbrk.c,v 1.17 2008/09/24 14:36:02 christos Exp $");
 #ifdef FAST_STRPBRK
 #define ADD_NEW_TO_SET(i) (set[inv[i] = idx++] = (i))
 #define IS_IN_SET(i) (inv[i] < idx && set[inv[i]] == (i))
-#define ADD_TO_SET(i) (IS_IN_SET(i) || ADD_NEW_TO_SET(i))
+#define ADD_TO_SET(i) (void)(IS_IN_SET(i) || ADD_NEW_TO_SET(i))
 #else
 #define IS_IN_SET(i) (set[(i) >> 3] & idx[(i) & 7])
-#define ADD_TO_SET(i) (set[(i) >> 3] |= idx[(i) & 7])
+#define ADD_TO_SET(i) (void)(set[(i) >> 3] |= idx[(i) & 7])
 #endif
 
 char *
