99b3c0ec11750de5ff6beda00de253cf2310c639
rmind <rmind@NetBSD.org>
2008-10-01 16:01:51 +0000

    PR/39664: Dave Huang: ip_mrouter_done: free hash using hashdone(9).

--- sys/netinet/ip_mroute.c
+++ sys/netinet/ip_mroute.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: ip_mroute.c,v 1.115 2008/08/06 15:01:23 plunky Exp $	*/
+/*	$NetBSD: ip_mroute.c,v 1.116 2008/10/01 16:01:51 rmind Exp $	*/
 
 /*
  * Copyright (c) 1992, 1993
@@ -93,7 +93,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: ip_mroute.c,v 1.115 2008/08/06 15:01:23 plunky Exp $");
+__KERNEL_RCSID(0, "$NetBSD: ip_mroute.c,v 1.116 2008/10/01 16:01:51 rmind Exp $");
 
 #include "opt_inet.h"
 #include "opt_ipsec.h"
@@ -694,7 +694,7 @@ ip_mrouter_done(void)
 	}
 
 	bzero((void *)nexpire, sizeof(nexpire));
-	free(mfchashtbl, M_MRTABLE);
+	hashdone(mfchashtbl, HASH_LIST, mfchash);
 	mfchashtbl = NULL;
 
 	bw_upcalls_n = 0;
