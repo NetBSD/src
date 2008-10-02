56e83e48d2a622bcc423dcd7c6d3f51637dafd66
joerg <joerg@NetBSD.org>
2008-10-02 20:49:40 +0000

    pkg_install-20081002: Explicitly cast time_t values to long before printing them. Reported by bjs.

--- external/bsd/pkg_install/dist/lib/version.h
+++ external/bsd/pkg_install/dist/lib/version.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: version.h,v 1.1.1.1 2008/09/30 19:00:27 joerg Exp $	*/
+/*	$NetBSD: version.h,v 1.1.1.2 2008/10/02 20:49:41 joerg Exp $	*/
 
 /*
  * Copyright (c) 2001 Thomas Klausner.  All rights reserved.
@@ -27,6 +27,6 @@
 #ifndef _INST_LIB_VERSION_H_
 #define _INST_LIB_VERSION_H_
 
-#define PKGTOOLS_VERSION "20080916"
+#define PKGTOOLS_VERSION "20081002"
 
 #endif /* _INST_LIB_VERSION_H_ */
