1c0122ec7a8607df261fdf2107fb9205c8870c4a
njoly <njoly@NetBSD.org>
2008-10-03 22:39:36 +0000

    Compat linux/linux32 nice(2) fix. The syscall argument is an increment to be added to the current nice value, not an absolute value to be set.

--- sys/compat/linux/common/linux_misc_notalpha.c
+++ sys/compat/linux/common/linux_misc_notalpha.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: linux_misc_notalpha.c,v 1.103 2008/07/15 16:18:08 christos Exp $	*/
+/*	$NetBSD: linux_misc_notalpha.c,v 1.104 2008/10/03 22:39:36 njoly Exp $	*/
 
 /*-
  * Copyright (c) 1995, 1998, 2008 The NetBSD Foundation, Inc.
@@ -31,7 +31,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: linux_misc_notalpha.c,v 1.103 2008/07/15 16:18:08 christos Exp $");
+__KERNEL_RCSID(0, "$NetBSD: linux_misc_notalpha.c,v 1.104 2008/10/03 22:39:36 njoly Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -199,11 +199,13 @@ linux_sys_nice(struct lwp *l, const struct linux_sys_nice_args *uap, register_t 
 	/* {
 		syscallarg(int) incr;
 	} */
+	struct proc *p = l->l_proc;
         struct sys_setpriority_args bsa;
 
         SCARG(&bsa, which) = PRIO_PROCESS;
         SCARG(&bsa, who) = 0;
-	SCARG(&bsa, prio) = SCARG(uap, incr);
+	SCARG(&bsa, prio) = p->p_nice - NZERO + SCARG(uap, incr);
+
         return sys_setpriority(l, &bsa, retval);
 }
 #endif /* !__amd64__ */
