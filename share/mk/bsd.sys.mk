f2b2ba69e31bedbb98704704ae5ce8323d1321a6
apb <apb@NetBSD.org>
2008-10-05 20:20:55 +0000

    Remove the definition of HOST_SED.  HOST_SED was previously used only in tools/atf-compile, which has now been adjusted to use TOOL_SED.

--- share/mk/bsd.sys.mk
+++ share/mk/bsd.sys.mk
@@ -1,4 +1,4 @@
-#	$NetBSD: bsd.sys.mk,v 1.168 2008/09/26 18:23:09 apb Exp $
+#	$NetBSD: bsd.sys.mk,v 1.169 2008/10/05 20:20:55 apb Exp $
 #
 # Build definitions used for NetBSD source tree builds.
 
@@ -119,8 +119,6 @@ HOST_AR?=	ar
 HOST_RANLIB?=	ranlib
 
 HOST_LN?=	ln
-
-HOST_SED?=	sed
 
 .if !empty(HOST_CYGWIN)
 HOST_SH?=	/usr/bin/bash
