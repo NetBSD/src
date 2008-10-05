9c71f6fc2a1c11f70e3814ea3adc2a71350eb45f
bouyer <bouyer@NetBSD.org>
2008-10-05 08:44:03 +0000

    Welcome to NetBSD 4.0.1

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.244.2.9.2.1 2007/12/17 12:49:29 ghen Exp $	*/
+/*	$NetBSD: param.h,v 1.244.2.9.2.2 2008/10/05 08:44:03 bouyer Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -63,7 +63,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	400000003	/* NetBSD 4.0.0_PATCH */
+#define	__NetBSD_Version__	400000003	/* NetBSD 4.0.1 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
