f298aa92020cc1a432e7a55adcfb2cf8743535f5
joerg <joerg@NetBSD.org>
2008-10-07 15:55:20 +0000

    Update to libfetch-2.17. Fix a bug in the line buffering code to not drop output if the server actually send more than one line.

--- external/bsd/fetch/dist/libfetch/common.c
+++ external/bsd/fetch/dist/libfetch/common.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: common.c,v 1.1.1.1 2008/09/30 18:46:07 joerg Exp $	*/
+/*	$NetBSD: common.c,v 1.1.1.2 2008/10/07 15:55:20 joerg Exp $	*/
 /*-
  * Copyright (c) 1998-2004 Dag-Erling Coïdan Smørgrav
  * Copyright (c) 2008 Joerg Sonnenberger <joerg@NetBSD.org>
@@ -234,6 +234,8 @@ fetch_reopen(int sd)
 	/* allocate and fill connection structure */
 	if ((conn = calloc(1, sizeof(*conn))) == NULL)
 		return (NULL);
+	conn->next_buf = NULL;
+	conn->next_len = 0;
 	conn->sd = sd;
 	++conn->ref;
 	return (conn);
@@ -405,6 +407,15 @@ fetch_read(conn_t *conn, char *buf, size_t len)
 	if (len == 0)
 		return 0;
 
+	if (conn->next_len != 0) {
+		if (conn->next_len < len)
+			len = conn->next_len;
+		memmove(buf, conn->next_buf, len);
+		conn->next_len -= len;
+		conn->next_buf += len;
+		return len;
+	}
+
 	if (fetchTimeout) {
 		FD_ZERO(&readfds);
 		gettimeofday(&timeout, NULL);
@@ -459,13 +470,12 @@ fetch_read(conn_t *conn, char *buf, size_t len)
 int
 fetch_getln(conn_t *conn)
 {
-	char *tmp;
+	char *tmp, *next;
 	size_t tmpsize;
 	ssize_t len;
-	int done;
 
 	if (conn->buf == NULL) {
-		if ((conn->buf = malloc(MIN_BUF_SIZE)) == NULL) {
+		if ((conn->buf = malloc(MIN_BUF_SIZE + 1)) == NULL) {
 			errno = ENOMEM;
 			return (-1);
 		}
@@ -474,6 +484,7 @@ fetch_getln(conn_t *conn)
 
 	conn->buf[0] = '\0';
 	conn->buflen = 0;
+	next = NULL;
 
 	do {
 		len = fetch_read(conn, conn->buf + conn->buflen,
@@ -482,9 +493,10 @@ fetch_getln(conn_t *conn)
 			return (-1);
 		if (len == 0)
 			break;
-		done = memchr(conn->buf + conn->buflen, '\n', len) != NULL;
+		next = memchr(conn->buf + conn->buflen, '\n', len);
 		conn->buflen += len;
-		if (conn->buflen == conn->bufsize) {
+		if (conn->buflen == conn->bufsize &&
+		    (next == NULL || next[1] == '\0')) {
 			tmp = conn->buf;
 			tmpsize = conn->bufsize * 2 + 1;
 			if ((tmp = realloc(tmp, tmpsize)) == NULL) {
@@ -494,8 +506,13 @@ fetch_getln(conn_t *conn)
 			conn->buf = tmp;
 			conn->bufsize = tmpsize;
 		}
-	} while (!done);
+	} while (next == NULL);
 
+	if (next != NULL) {
+		conn->next_buf = next + 1;
+		conn->next_len = conn->buflen - (conn->next_buf - conn->buf);
+		conn->buflen = next - conn->buf;
+	}
 	conn->buf[conn->buflen] = '\0';
 	return (0);
 }
