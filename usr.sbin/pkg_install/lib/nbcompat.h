3d4926cda48d97c6f6ec4e547fd361886321bf96
joerg <joerg@NetBSD.org>
2008-10-07 16:06:12 +0000

    Finish moving pkg_install to src/external/bsd/pkg_install.

--- usr.sbin/pkg_install/lib/nbcompat.h
+++
@@ -1,8 +0,0 @@
-/* $NetBSD: nbcompat.h,v 1.3 2008/02/07 23:46:47 joerg Exp $ */
-
-/*
- * In pkgsrc, libnbcompat is used to provide compatibility functions.
- * This is not needed on native NetBSD, so provide an empty header.
- */
-
-#include <sys/statvfs.h>
