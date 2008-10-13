c356fd4f354b6bd12a607a9d3c34cd89cbdc97de
is <is>
2008-10-13 10:01:24 +0000

    Load firmware from .../firmware/stuirda, not ../uirda, to avoid confusion.

--- sys/dev/usb/stuirda.c
+++ sys/dev/usb/stuirda.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: stuirda.c,v 1.8 2008/05/24 16:40:58 cube Exp $	*/
+/*	$NetBSD: stuirda.c,v 1.9 2008/10/13 10:01:24 is Exp $	*/
 
 /*
  * Copyright (c) 2001,2007 The NetBSD Foundation, Inc.
@@ -30,7 +30,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: stuirda.c,v 1.8 2008/05/24 16:40:58 cube Exp $");
+__KERNEL_RCSID(0, "$NetBSD: stuirda.c,v 1.9 2008/10/13 10:01:24 is Exp $");
 
 #include <sys/param.h>
 
@@ -166,7 +166,7 @@ stuirda_fwload(struct uirda_softc *sc) {
 	printf("%s: Attempting to load firmware %s\n",
 		USBDEVNAME(sc->sc_dev), fwname);
 	
-	rc = firmware_open("uirda", fwname, &fh);
+	rc = firmware_open("stuirda", fwname, &fh);
 
 	if (rc) {
 		printf("%s: Cannot load firmware\n",
