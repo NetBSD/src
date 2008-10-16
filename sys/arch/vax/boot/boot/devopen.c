ce49696b3010900b9e4c04eec727c87fcc4990ad
hans <hans@NetBSD.org>
2008-10-16 13:39:25 +0000

    Set nexaddr on qbus vaxen to enable remapping of buffers when booting from MSCP devices. Fixes MSCP boot problem introduced by moving boot up to allow for bigger kernels. OK by matt.

--- sys/arch/vax/boot/boot/devopen.c
+++ sys/arch/vax/boot/boot/devopen.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: devopen.c,v 1.14 2005/12/11 12:19:30 christos Exp $ */
+/*	$NetBSD: devopen.c,v 1.15 2008/10/16 13:39:25 hans Exp $ */
 /*
  * Copyright (c) 1997 Ludd, University of Lule}, Sweden.
  * All rights reserved.
@@ -184,9 +184,11 @@ devopen(f, fname, file)
 		csrbase = 0x20000000;
 		/* Always map in the lowest 4M on qbus-based machines */
 		mapregs = (void *)0x20088000;
-		if (bootrpb.adpphy == 0x20087800)
+		if (bootrpb.adpphy == 0x20087800) {
+			nexaddr = bootrpb.adpphy;
 			for (i = 0; i < 8192; i++)
 				mapregs[i] = PG_V | i;
+		}
 		break;
 	}
 
