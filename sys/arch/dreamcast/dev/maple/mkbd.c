af1fdcf689a5987327cc21dd424566dfb345913e
marcus <marcus@NetBSD.org>
2008-10-19 21:24:20 +0000

    Removed a local variable that is no longer needed (should have been removed in last commit...)

--- sys/arch/dreamcast/dev/maple/mkbd.c
+++ sys/arch/dreamcast/dev/maple/mkbd.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: mkbd.c,v 1.26 2008/10/19 14:57:22 marcus Exp $	*/
+/*	$NetBSD: mkbd.c,v 1.27 2008/10/19 21:24:20 marcus Exp $	*/
 
 /*-
  * Copyright (c) 2001 Marcus Comstedt
@@ -33,7 +33,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: mkbd.c,v 1.26 2008/10/19 14:57:22 marcus Exp $");
+__KERNEL_RCSID(0, "$NetBSD: mkbd.c,v 1.27 2008/10/19 21:24:20 marcus Exp $");
 
 #include <sys/param.h>
 #include <sys/device.h>
@@ -308,7 +308,6 @@ mkbd_cngetc(void *v, u_int *type, int *data)
 	while (polledkey == -1) {
 		if (mkbd_console_softc != NULL &&
 		    mkbd_console_softc->sc_parent != NULL) {
-			int t;
 			DELAY(20000);
 			maple_run_polling(mkbd_console_softc->sc_parent);
 		}
