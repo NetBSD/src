95166d0ddeac9f1c88c7d18626924acf24e31564
apb <apb@NetBSD.org>
2008-10-19 07:56:42 +0000

    Use uint32_t and include <stdint.h>, instead of using u_int32_t and including <sys/types.h>.

--- games/fortune/strfile/strfile.h
+++ games/fortune/strfile/strfile.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: strfile.h,v 1.6 2003/08/07 09:37:15 agc Exp $	*/
+/*	$NetBSD: strfile.h,v 1.7 2008/10/19 07:56:42 apb Exp $	*/
 
 /*-
  * Copyright (c) 1991, 1993
@@ -34,21 +34,21 @@
  *	@(#)strfile.h	8.1 (Berkeley) 5/31/93
  */
 
-#include <sys/types.h>
+#include <stdint.h>
 
 #define	STR_ENDSTRING(line,tbl) \
 	((line)[0] == (tbl).str_delim && (line)[1] == '\n')
 
 typedef struct {				/* information table */
 #define	VERSION		1
-	u_int32_t	str_version;		/* version number */
-	u_int32_t	str_numstr;		/* # of strings in the file */
-	u_int32_t	str_longlen;		/* length of longest string */
-	u_int32_t	str_shortlen;		/* length of shortest string */
+	uint32_t	str_version;		/* version number */
+	uint32_t	str_numstr;		/* # of strings in the file */
+	uint32_t	str_longlen;		/* length of longest string */
+	uint32_t	str_shortlen;		/* length of shortest string */
 #define	STR_RANDOM	0x1			/* randomized pointers */
 #define	STR_ORDERED	0x2			/* ordered pointers */
 #define	STR_ROTATED	0x4			/* rot-13'd text */
-	u_int32_t	str_flags;		/* bit field for flags */
+	uint32_t	str_flags;		/* bit field for flags */
 	unsigned char	stuff[4];		/* long aligned space */
 #define	str_delim	stuff[0]		/* delimiting character */
 } STRFILE;
