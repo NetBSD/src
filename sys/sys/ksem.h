2d3e942a472ba0747b98228a3e9719b10b70a3a3
ad <ad@NetBSD.org>
2008-10-22 11:57:14 +0000

    Fix it again.

--- sys/sys/ksem.h
+++ sys/sys/ksem.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: ksem.h,v 1.8 2008/10/22 11:49:23 ad Exp $	*/
+/*	$NetBSD: ksem.h,v 1.9 2008/10/22 11:57:14 ad Exp $	*/
 
 /*
  * Copyright (c) 2002 Alfred Perlstein <alfred@FreeBSD.org>
@@ -32,6 +32,8 @@
 #include <sys/cdefs.h>
 
 #ifdef _KERNEL
+void ksem_init(void);
+
 int do_ksem_init(struct lwp *, unsigned int, semid_t *, copyout_t);
 int do_ksem_open(struct lwp *, const char *, int, mode_t, unsigned int,
     semid_t *, copyout_t);
