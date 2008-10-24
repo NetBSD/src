dacc9bf7cb66bda706ea6a33359f826fbed68e62
matt <matt@NetBSD.org>
2008-10-24 16:17:08 +0000

    Make this reflect reality.

--- sys/arch/arm/gemini/gemini_intr.h
+++ sys/arch/arm/gemini/gemini_intr.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: gemini_intr.h,v 1.1 2008/10/24 04:23:18 matt Exp $	*/
+/*	$NetBSD: gemini_intr.h,v 1.2 2008/10/24 16:17:08 matt Exp $	*/
 
 #ifndef _ARM_GEMINI_INTR_H_
 #define _ARM_GEMINI_INTR_H_
@@ -6,27 +6,8 @@
 #define	ARM_IRQ_HANDLER	_C_LABEL(gemini_irq_handler)
 
 #ifndef _LOCORE
-
-extern void gemini_splx(int);
-extern int gemini_splraise(int);
-extern int gemini_spllower(int);
-
-int	_splraise(int);
-int	_spllower(int);
-void	splx(int);
 void	gemini_irq_handler(void *);
-
-#if !defined(EVBARM_SPL_NOINLINE)
-#define splx(new)		gemini_splx(new)
-#define _spllower(ipl)		gemini_spllower(ipl)
-#define _splraise(ipl)		gemini_splraise(ipl)
-#ifdef __HAVE_FAST_SOFTINTS
-# define _setsoftintr(si)	omap_setsoftintr(si)
-#endif
-#endif  /* !EVBARM_SPL_NOINTR */
-
-
-
+#include <arm/pic/picvar.h>
 #endif	/* _LOCORE */
 
 #endif	/* _ARM_GEMINI_INTR_H_ */
