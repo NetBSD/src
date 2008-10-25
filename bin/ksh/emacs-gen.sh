b129640e8879f66a40a7eba3a56a32fdf6e06151
apb <apb@NetBSD.org>
2008-10-25 22:18:15 +0000

    In shell scripts run during the build, add a SED variable, defaulting to "sed".  SED=${TOOL_SED:Q} should be passed in the environment to override this.

--- bin/ksh/emacs-gen.sh
+++ bin/ksh/emacs-gen.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
-#	$NetBSD: emacs-gen.sh,v 1.3 2008/10/19 22:10:04 apb Exp $
+#	$NetBSD: emacs-gen.sh,v 1.4 2008/10/25 22:18:15 apb Exp $
 
 : ${AWK:=awk}
+: ${SED:=sed}
 
 case $# in
 1)	file=$1;;
@@ -25,7 +26,7 @@ E_O_F
 
 # Pass 1: print out lines before @START-FUNC-TAB@
 #	  and generate defines and function declarations,
-sed -e '1,/@START-FUNC-TAB@/d' -e '/@END-FUNC-TAB@/,$d' < $file |
+${SED} -e '1,/@START-FUNC-TAB@/d' -e '/@END-FUNC-TAB@/,$d' < $file |
 	${AWK} 'BEGIN { nfunc = 0; }
 	    /^[	 ]*#/ {
 			    print $0;
