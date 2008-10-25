3ec88344f88ff765c58cf566df5708e05f9348a8
mrg <mrg@NetBSD.org>
2008-10-25 19:11:28 +0000

    allow _GCC_*CRT* defines to be overridden in the in-tree toolchain case.

--- share/mk/bsd.gcc.mk
+++ share/mk/bsd.gcc.mk
@@ -1,4 +1,4 @@
-#	$NetBSD: bsd.gcc.mk,v 1.2 2003/06/27 17:13:27 drochner Exp $
+#	$NetBSD: bsd.gcc.mk,v 1.3 2008/10/25 19:11:28 mrg Exp $
 
 .if !defined(_BSD_GCC_MK_)
 _BSD_GCC_MK_=1
@@ -11,12 +11,12 @@ _GCC_CRTENDS!=		${CC} --print-file-name=crtendS.o
 _GCC_CRTDIR!=		dirname ${_GCC_CRTBEGIN}
 _GCC_LIBGCCDIR!=	dirname `${CC} --print-libgcc-file-name`
 .else
-_GCC_CRTBEGIN=		${DESTDIR}/usr/lib/crtbegin.o
-_GCC_CRTBEGINS=		${DESTDIR}/usr/lib/crtbeginS.o
-_GCC_CRTEND=		${DESTDIR}/usr/lib/crtend.o
-_GCC_CRTENDS=		${DESTDIR}/usr/lib/crtendS.o
-_GCC_CRTDIR=		${DESTDIR}/usr/lib
-_GCC_LIBGCCDIR=		${DESTDIR}/usr/lib
+_GCC_CRTBEGIN?=		${DESTDIR}/usr/lib/crtbegin.o
+_GCC_CRTBEGINS?=	${DESTDIR}/usr/lib/crtbeginS.o
+_GCC_CRTEND?=		${DESTDIR}/usr/lib/crtend.o
+_GCC_CRTENDS?=		${DESTDIR}/usr/lib/crtendS.o
+_GCC_CRTDIR?=		${DESTDIR}/usr/lib
+_GCC_LIBGCCDIR?=	${DESTDIR}/usr/lib
 .endif
 
 .endif	# ! defined(_BSD_GCC_MK_)
