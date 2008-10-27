2071954247785a5cdad2802a57f9ae5775b02f80
apb <apb@NetBSD.org>
2008-10-27 19:52:28 +0000

    In foo="`echo \"hi\"`", the backslashes should be removed by the outer double quotes, not seen by the inner backquotes.  Previously, ksh(1) handled this correctly in non-posix mode but incorrectly in posix mode.
    The previous comments in src/bin/ksh/lex.c quote parts of some version
    of the POSIX specification.  The version of POSIX being quoted is not
    identified, but the wording is very similar to that in SUSv3
    <http://www.opengroup.org/onlinepubs/009695399/utilities/xcu_chap02.html>.
    
    It seems clear that the words "having escape characters removed"
    mean, in that context, that the backslash before the double quote in
    "...`...\"...`..." is removed as part of the handling of the outer
    double quotes, so that the backslash is not present when the inner
    backquotes are handled.  The new code in this commit follows this
    interpretation.  The previous code followed a different interpretation
    which is now believed to be incorrect.
    
    Discussed in tech-userlevel.  Closes PR 26493.

--- bin/ksh/lex.c
+++ bin/ksh/lex.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: lex.c,v 1.12 2005/09/11 22:16:00 christos Exp $	*/
+/*	$NetBSD: lex.c,v 1.13 2008/10/27 19:52:28 apb Exp $	*/
 
 /*
  * lexical analysis and source input
@@ -6,7 +6,7 @@
 #include <sys/cdefs.h>
 
 #ifndef lint
-__RCSID("$NetBSD: lex.c,v 1.12 2005/09/11 22:16:00 christos Exp $");
+__RCSID("$NetBSD: lex.c,v 1.13 2008/10/27 19:52:28 apb Exp $");
 #endif
 
 
@@ -334,41 +334,27 @@ yylex(cf)
 				*wp++ = COMSUB;
 				/* Need to know if we are inside double quotes
 				 * since sh/at&t-ksh translate the \" to " in
-				 * "`..\"..`".
-				 * This is not done in posix mode (section
-				 * 3.2.3, Double Quotes: "The backquote shall
-				 * retain its special meaning introducing the
-				 * other form of command substitution (see
-				 * 3.6.3). The portion of the quoted string
-				 * from the initial backquote and the
-				 * characters up to the next backquote that
-				 * is not preceded by a backslash (having
-				 * escape characters removed) defines that
-				 * command whose output replaces `...` when
-				 * the word is expanded."
-				 * Section 3.6.3, Command Substitution:
-				 * "Within the backquoted style of command
-				 * substitution, backslash shall retain its
-				 * literal meaning, except when followed by
-				 * $ ` \.").
+				 * "`..\"..`".  POSIX also requires this.
+				 * An earlier version of ksh misinterpreted
+				 * the POSIX specification and performed
+				 * removal of backslash escapes only if
+				 * posix mode was not in effect.
 				 */
 				statep->ls_sbquote.indquotes = 0;
-				if (!Flag(FPOSIX)) {
-					Lex_state *s = statep;
-					Lex_state *base = state_info.base;
-					while (1) {
-						for (; s != base; s--) {
-							if (s->ls_state == SDQUOTE) {
-								statep->ls_sbquote.indquotes = 1;
-								break;
-							}
+				Lex_state *s = statep;
+				Lex_state *base = state_info.base;
+				while (1) {
+					for (; s != base; s--) {
+						if (s->ls_state == SDQUOTE) {
+							statep->ls_sbquote.indquotes = 1;
+							break;
 						}
-						if (s != base)
-							break;
-						if (!(s = s->ls_info.base))
-							break;
-						base = s-- - STATE_BSIZE;
 					}
+					if (s != base)
+						break;
+					if (!(s = s->ls_info.base))
+						break;
+					base = s-- - STATE_BSIZE;
 				}
 				break;
 			  default:
