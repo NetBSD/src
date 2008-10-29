e0e4bd694d816a6712cab8536ec5bb268c43adc3
sjg <sjg@NetBSD.org>
2008-10-29 15:37:08 +0000

    If Parse_Error is called after curFile is invalid, set it to a dummy to avoid seg-fault.  Also the NULL filename will cause ParseVErrorInternal to skip trying to report file and line number info.
    If CondToken is called outside the context of a .if etc, variables in
    the expression being parsed will already be expanded, so allow for
    an unqouted number to jump us back into the compare logic.
    This allows ${${SomeNumber:U42} > 0:?True:False} to work anywhere.
    
    Job_Finish should process postCommands if it has commands or children.

--- usr.bin/make/job.c
+++ usr.bin/make/job.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: job.c,v 1.140 2008/10/06 22:09:21 joerg Exp $	*/
+/*	$NetBSD: job.c,v 1.141 2008/10/29 15:37:08 sjg Exp $	*/
 
 /*
  * Copyright (c) 1988, 1989, 1990 The Regents of the University of California.
@@ -70,14 +70,14 @@
  */
 
 #ifndef MAKE_NATIVE
-static char rcsid[] = "$NetBSD: job.c,v 1.140 2008/10/06 22:09:21 joerg Exp $";
+static char rcsid[] = "$NetBSD: job.c,v 1.141 2008/10/29 15:37:08 sjg Exp $";
 #else
 #include <sys/cdefs.h>
 #ifndef lint
 #if 0
 static char sccsid[] = "@(#)job.c	8.2 (Berkeley) 3/19/94";
 #else
-__RCSID("$NetBSD: job.c,v 1.140 2008/10/06 22:09:21 joerg Exp $");
+__RCSID("$NetBSD: job.c,v 1.141 2008/10/29 15:37:08 sjg Exp $");
 #endif
 #endif /* not lint */
 #endif
@@ -2530,7 +2530,9 @@ JobInterrupt(int runINTERRUPT, int signo)
 int
 Job_Finish(void)
 {
-    if (postCommands != NILGNODE && !Lst_IsEmpty(postCommands->commands)) {
+    if (postCommands != NILGNODE &&
+	(!Lst_IsEmpty(postCommands->commands) ||
+	 !Lst_IsEmpty(postCommands->children))) {
 	if (errors) {
 	    Error("Errors reported so .END ignored");
 	} else {
