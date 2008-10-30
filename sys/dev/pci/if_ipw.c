cb36f8d0102c103a18b722d2fd331eba4a2d5e06
joerg <joerg>
2008-10-30 00:27:31 +0000

    Add glue to ship Intel firmware images. For ipw and iwi require user to accept the EULA via sysctl as discussed with core@.

--- sys/dev/pci/if_ipw.c
+++ sys/dev/pci/if_ipw.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: if_ipw.c,v 1.41 2008/05/27 21:41:01 dyoung Exp $	*/
+/*	$NetBSD: if_ipw.c,v 1.42 2008/10/30 00:27:32 joerg Exp $	*/
 /*	FreeBSD: src/sys/dev/ipw/if_ipw.c,v 1.15 2005/11/13 17:17:40 damien Exp 	*/
 
 /*-
@@ -29,7 +29,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: if_ipw.c,v 1.41 2008/05/27 21:41:01 dyoung Exp $");
+__KERNEL_RCSID(0, "$NetBSD: if_ipw.c,v 1.42 2008/10/30 00:27:32 joerg Exp $");
 
 /*-
  * Intel(R) PRO/Wireless 2100 MiniPCI driver
@@ -87,6 +87,9 @@ int ipw_debug = 0;
 #define DPRINTF(x)
 #define DPRINTFN(n, x)
 #endif
+
+/* Permit loading the Intel firmware */
+static int ipw_accept_eula;
 
 static int	ipw_dma_alloc(struct ipw_softc *);
 static void	ipw_release(struct ipw_softc *);
@@ -1875,6 +1878,12 @@ ipw_cache_firmware(struct ipw_softc *sc)
 
 	ipw_free_firmware(sc);
 
+	if (ipw_accept_eula == 0) {
+		aprint_error_dev(&sc->sc_dev,
+		    "EULA not accepted, see hw.ipw.accept_eula\n");
+		return EPERM;
+	}
+
 	if ((error = firmware_open("if_ipw", sc->sc_fwname, &fwh)) != 0)
 		goto fail0;
 
@@ -2264,4 +2273,34 @@ ipw_write_mem_1(struct ipw_softc *sc, bus_size_t offset, uint8_t *datap,
 		CSR_WRITE_4(sc, IPW_CSR_INDIRECT_ADDR, offset & ~3);
 		CSR_WRITE_1(sc, IPW_CSR_INDIRECT_DATA + (offset & 3), *datap);
 	}
+}
+
+SYSCTL_SETUP(sysctl_hw_ipw_accept_eula_setup, "sysctl hw.ipw.accept_eula")
+{
+	const struct sysctlnode *rnode;
+	const struct sysctlnode *cnode;
+
+	sysctl_createv(NULL, 0, NULL, &rnode,
+		CTLFLAG_PERMANENT,
+		CTLTYPE_NODE, "hw",
+		NULL,
+		NULL, 0,
+		NULL, 0,
+		CTL_HW, CTL_EOL);
+
+	sysctl_createv(NULL, 0, &rnode, &rnode,
+		CTLFLAG_PERMANENT,
+		CTLTYPE_NODE, "ipw",
+		NULL,
+		NULL, 0,
+		NULL, 0,
+		CTL_CREATE, CTL_EOL);
+
+	sysctl_createv(NULL, 0, &rnode, &cnode,
+		CTLFLAG_PERMANENT | CTLFLAG_READWRITE,
+		CTLTYPE_INT, "accept_eula",
+		SYSCTL_DESCR("Accept Intel EULA and permit use of ipw(4) firmware"),
+		NULL, 0,
+		&ipw_accept_eula, sizeof(ipw_accept_eula),
+		CTL_CREATE, CTL_EOL);
 }
