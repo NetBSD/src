36f267b5ec1d7b8d7f516aba887d8d511fbee198
agc <agc@NetBSD.org>
2008-10-31 04:27:01 +0000

    Welcome to NetBSD 5.0_BETA

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.330 2008/10/09 11:02:17 pooka Exp $	*/
+/*	$NetBSD: param.h,v 1.330.4.1 2008/10/31 04:27:01 agc Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -63,7 +63,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	499007300	/* NetBSD 4.99.73 */
+#define	__NetBSD_Version__	500000000	/* NetBSD 5.0_BETA */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
