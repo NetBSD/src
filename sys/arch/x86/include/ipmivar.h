cfeb108e606c1f6615af9d91cb0c7542709121e0
cegger <cegger@NetBSD.org>
2008-11-03 12:25:53 +0000

    The functions called from ipmi_match use the DEVNAME macro. But the softc is allocated on the stack and the accessed sc_dev member is not initialized.
    Initialize the sc_dev.dv_xname in ipmi_match, which is enough to make DEVNAME work. Finally this also allows the device_t/softc split.

--- sys/arch/x86/include/ipmivar.h
+++ sys/arch/x86/include/ipmivar.h
@@ -1,4 +1,4 @@
-/* $NetBSD: ipmivar.h,v 1.8 2008/09/23 10:17:06 ad Exp $ */
+/* $NetBSD: ipmivar.h,v 1.9 2008/11/03 12:25:53 cegger Exp $ */
 
 /*
  * Copyright (c) 2005 Jordan Hargrave
@@ -77,7 +77,7 @@ struct ipmi_if {
 };
 
 struct ipmi_softc {
-	struct device		sc_dev;
+	device_t		sc_dev;
 
 	struct ipmi_if		*sc_if;		/* Interface layer */
 	int			sc_if_iospacing; /* Spacing of I/O ports */
