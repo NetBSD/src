76d489193e3b5a458fb0c3c3d4f4e9f1e3be6e7f
cegger <cegger@NetBSD.org>
2008-11-09 15:34:14 +0000

    struct device *  -> device_t

--- sys/arch/x86/include/mpacpi.h
+++ sys/arch/x86/include/mpacpi.h
@@ -1,15 +1,15 @@
-/*	$NetBSD: mpacpi.h,v 1.7 2008/11/09 14:24:14 cegger Exp $	*/
+/*	$NetBSD: mpacpi.h,v 1.8 2008/11/09 15:34:14 cegger Exp $	*/
 
 #ifndef _X86_MPACPI_H_
 #define _X86_MPACPI_H_
 
 struct pcibus_attach_args;
 
-int mpacpi_scan_apics(struct device *, int *);
+int mpacpi_scan_apics(device_t, int *);
 int mpacpi_find_interrupts(void *);
-int mpacpi_pci_attach_hook(struct device *, struct device *,
+int mpacpi_pci_attach_hook(device_t, device_t,
 			   struct pcibus_attach_args *);
-int mpacpi_scan_pci(struct device *, struct pcibus_attach_args *, cfprint_t);
+int mpacpi_scan_pci(device_t, struct pcibus_attach_args *, cfprint_t);
 
 struct mp_intr_map;
 int mpacpi_findintr_linkdev(struct mp_intr_map *);
