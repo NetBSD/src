fb6f7962ffe800ab41c253218838163430393728
joerg <joerg@NetBSD.org>
2008-11-18 14:01:03 +0000

    Make uuidgen available for the kernel.

--- sys/sys/uuid.h
+++ sys/sys/uuid.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: uuid.h,v 1.4 2007/07/09 21:11:34 ad Exp $	*/
+/*	$NetBSD: uuid.h,v 1.5 2008/11/18 14:01:03 joerg Exp $	*/
 
 /*
  * Copyright (c) 2002 Marcel Moolenaar
@@ -67,6 +67,7 @@ void	uuid_dec_le(const void *, struct uuid *);
 void	uuid_enc_be(void *, const struct uuid *);
 void	uuid_enc_le(void *, const struct uuid *);
 void	uuid_init(void);
+int	uuidgen(struct uuid *, int);
 
 #else	/* _KERNEL */
 
