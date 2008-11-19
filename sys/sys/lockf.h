74f93b9804f904f6f985f437d58cc6dbade854cf
pooka <pooka@NetBSD.org>
2008-11-19 13:34:11 +0000

    Bring in vnode.h
    Why is this file still installed to /usr/include?

--- sys/sys/lockf.h
+++ sys/sys/lockf.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: lockf.h,v 1.19 2008/05/28 13:35:32 ad Exp $	*/
+/*	$NetBSD: lockf.h,v 1.20 2008/11/19 13:34:11 pooka Exp $	*/
 
 /*
  * Copyright (c) 1991, 1993
@@ -38,6 +38,9 @@
 #define _SYS_LOCKF_H_
 
 #ifdef _KERNEL
+
+#include <sys/vnode.h>
+
 struct lockf;
 
 int lf_advlock(struct vop_advlock_args *, struct lockf **, off_t);
