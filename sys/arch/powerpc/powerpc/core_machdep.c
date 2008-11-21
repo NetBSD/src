c1aefc5a53b1c1c74371061a4bacf674c9ef935b
he <he@NetBSD.org>
2008-11-21 20:21:47 +0000

    Wrap includes of opt* headers in _KERNEL_OPT.

--- sys/arch/powerpc/powerpc/core_machdep.c
+++ sys/arch/powerpc/powerpc/core_machdep.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: core_machdep.c,v 1.1 2008/11/19 18:36:00 ad Exp $	*/
+/*	$NetBSD: core_machdep.c,v 1.2 2008/11/21 20:21:47 he Exp $	*/
 
 /*
  * Copyright (C) 1995, 1996 Wolfgang Solfrank.
@@ -32,10 +32,12 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: core_machdep.c,v 1.1 2008/11/19 18:36:00 ad Exp $");
+__KERNEL_RCSID(0, "$NetBSD: core_machdep.c,v 1.2 2008/11/21 20:21:47 he Exp $");
 
+#ifdef _KERNEL_OPT
 #include "opt_altivec.h"
 #include "opt_ppcarch.h"
+#endif
 
 #include <sys/param.h>
 #include <sys/core.h>
