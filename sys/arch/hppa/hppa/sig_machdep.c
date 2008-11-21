de277f4545f6cc5b11823a770ef6979372fe3543
he <he@NetBSD.org>
2008-11-21 01:57:33 +0000

    Rename sendsig() to sendsig_siginfo(), so that we don't conflict with the newly added sendsig() in kern_sig.c.

--- sys/arch/hppa/hppa/sig_machdep.c
+++ sys/arch/hppa/hppa/sig_machdep.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: sig_machdep.c,v 1.21 2008/04/28 20:23:23 martin Exp $	*/
+/*	$NetBSD: sig_machdep.c,v 1.22 2008/11/21 01:57:33 he Exp $	*/
 
 /*-
  * Copyright (c) 2002 The NetBSD Foundation, Inc.
@@ -104,7 +104,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: sig_machdep.c,v 1.21 2008/04/28 20:23:23 martin Exp $");
+__KERNEL_RCSID(0, "$NetBSD: sig_machdep.c,v 1.22 2008/11/21 01:57:33 he Exp $");
 
 #include "opt_compat_netbsd.h"
 
@@ -153,7 +153,7 @@ struct sigframe_siginfo {
 };
 
 void
-sendsig(const struct ksiginfo *ksi, const sigset_t *mask)
+sendsig_siginfo(const struct ksiginfo *ksi, const sigset_t *mask)
 {
 	struct lwp *l = curlwp;
 	struct proc *p = l->l_proc;
