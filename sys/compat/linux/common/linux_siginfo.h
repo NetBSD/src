c84acc9a252476c2fa016f8c6d19acc9355af28d
njoly <njoly>
2008-11-23 23:48:48 +0000

    Move si_code definitions from MD to MI linux_siginfo.h, all archs (except mips for 3 values) are identical. While here, remove unused/unneeded LINUX__SI_CODE macro. Only the lower 16-bits are used for userland si_code.

--- sys/compat/linux/common/linux_siginfo.h
+++ sys/compat/linux/common/linux_siginfo.h
@@ -1,4 +1,4 @@
-/* 	$NetBSD: linux_siginfo.h,v 1.13 2008/10/21 20:24:15 njoly Exp $	*/
+/* 	$NetBSD: linux_siginfo.h,v 1.14 2008/11/23 23:48:48 njoly Exp $	*/
 
 /*-
  * Copyright (c) 1998 The NetBSD Foundation, Inc.
@@ -48,9 +48,69 @@
 #include <compat/linux/arch/amd64/linux_siginfo.h>
 #endif
 
-/* From linux/include/asm-generic/siginfo.h */
-#define LINUX_CLD_EXITED	1
-#define LINUX_CLD_KILLED	2
-#define LINUX_CLD_DUMPED	3
+/* si_code values for non signal */
+#define	LINUX_SI_USER		0
+#define	LINUX_SI_KERNEL		0x80
+#define	LINUX_SI_QUEUE		-1
+#ifndef LINUX_SI_CODE_ARCH /* all except mips */
+#define	LINUX_SI_TIMER		-2
+#define	LINUX_SI_MESGQ		-3
+#define	LINUX_SI_ASYNCIO	-4
+#endif /* LINUX_SI_CODE_ARCH */
+#define	LINUX_SI_SIGIO		-5
+#define	LINUX_SI_TKILL		-6
+#define	LINUX_SI_DETHREAD	-7
+
+/* si_code values for SIGILL */
+#define	LINUX_ILL_ILLOPC	1
+#define	LINUX_ILL_ILLOPN	2
+#define	LINUX_ILL_ILLADR	3
+#define	LINUX_ILL_ILLTRP	4
+#define	LINUX_ILL_PRVOPC	5
+#define	LINUX_ILL_PRVREG	6
+#define	LINUX_ILL_COPROC	7
+#define	LINUX_ILL_BADSTK	8
+
+/* si_code values for SIGFPE */
+#define	LINUX_FPE_INTDIV	1
+#define	LINUX_FPE_INTOVF	2
+#define	LINUX_FPE_FLTDIV	3
+#define	LINUX_FPE_FLTOVF	4
+#define	LINUX_FPE_FLTUND	5
+#define	LINUX_FPE_FLTRES	6
+#define	LINUX_FPE_FLTINV	7
+#define	LINUX_FPE_FLTSUB	8
+
+/* si_code values for SIGSEGV */
+#define	LINUX_SEGV_MAPERR	1
+#define	LINUX_SEGV_ACCERR	2
+
+/* si_code values for SIGBUS */
+#define	LINUX_BUS_ADRALN	1
+#define	LINUX_BUS_ADRERR	2
+#define	LINUX_BUS_OBJERR	3
+
+/* si_code values for SIGTRAP */
+#define	LINUX_TRAP_BRKPT	1
+#define	LINUX_TRAP_TRACE	2
+
+/* si_code values for SIGCHLD */
+#define	LINUX_CLD_EXITED	1
+#define	LINUX_CLD_KILLED	2
+#define	LINUX_CLD_DUMPED	3
+#define	LINUX_CLD_TRAPPED	4
+#define	LINUX_CLD_STOPPED	5
+#define	LINUX_CLD_CONTINUED	6
+
+/* si_code values for SIGPOLL */
+#define	LINUX_POLL_IN		1
+#define	LINUX_POLL_OUT		2
+#define	LINUX_POLL_MSG		3
+#define	LINUX_POLL_ERR		4
+#define	LINUX_POLL_PRI		5
+#define	LINUX_POLL_HUP		6
+
+#define	LINUX_SI_FROMUSER(sp)	((sp)->si_code <= 0)
+#define	LINUX_SI_FROMKERNEL(sp)	((sp)->si_code > 0)
 
 #endif /* !_LINUX_SIGINFO_H */
