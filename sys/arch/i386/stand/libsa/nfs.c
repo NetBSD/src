a54ad3526546efbdc3039695f39784bb213afb9c
ad <ad@NetBSD.org>
2008-11-25 13:18:37 +0000

    Cut-and-paste strikes again. Set fsmod here too.

--- sys/arch/i386/stand/libsa/nfs.c
+++ sys/arch/i386/stand/libsa/nfs.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: nfs.c,v 1.13 2006/12/29 13:05:48 yamt Exp $	*/
+/*	$NetBSD: nfs.c,v 1.14 2008/11/25 13:18:37 ad Exp $	*/
 
 /*-
  *  Copyright (c) 1993 John Brezak
@@ -508,6 +508,7 @@ out:
 #endif
 	if (!error) {
 		f->f_fsdata = (void *)currfd;
+		fsmod = "nfs";
 		return (0);
 	}
 
