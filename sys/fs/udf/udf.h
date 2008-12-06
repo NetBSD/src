5ca3633c26e988352bc947c98716a9e3b276e2f0
snj <snj@NetBSD.org>
2008-12-06 21:47:31 +0000

    Pull up following revision(s) (requested by reinoud in ticket #167): 	sys/fs/udf/udf.h: revision 1.28 	sys/fs/udf/udf_strat_rmw.c: revision 1.13 Two fold commit: 1) Enhance write speed significantly on RMW media like CD-RW, DVD-RW but also on the DVD+RW and all other ECC blocked media. Significant speedups of access to the device for say compilation on the DVD. Streaming copy is also still at maximum speed though vast amounts of directory copy work can show side effects that appear it to slow down but are actually logical when you consider that most small files are embedded into the descriptors itself. 2) explicit wait for the created RMW thread to spinup

--- sys/fs/udf/udf.h
+++ sys/fs/udf/udf.h
@@ -1,4 +1,4 @@
-/* $NetBSD: udf.h,v 1.27 2008/09/27 13:05:34 reinoud Exp $ */
+/* $NetBSD: udf.h,v 1.27.4.1 2008/12/06 21:47:31 snj Exp $ */
 
 /*
  * Copyright (c) 2006, 2008 Reinoud Zandijk
@@ -121,8 +121,8 @@ extern int udf_verbose;
 #define UDF_DIRHASH_HASHSIZE	(1<<UDF_DIRHASH_HASHBITS)
 #define UDF_DIRHASH_HASHMASK	(UDF_DIRHASH_HASHSIZE -1)
 
-#define UDF_ECCLINE_MAXFREE	10			/* picked */
-#define UDF_ECCLINE_MAXBUSY	100			/* picked */
+#define UDF_ECCLINE_MAXFREE	5			/* picked, needs calculation */
+#define UDF_ECCLINE_MAXBUSY	100			/* picked, needs calculation */
 
 #define UDF_MAX_MAPPINGS	(MAXPHYS/DEV_BSIZE)	/* 128 */
 #define UDF_VAT_CHUNKSIZE	(64*1024)		/* picked */
