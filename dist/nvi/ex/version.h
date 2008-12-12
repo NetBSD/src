3a367666e4053348bdcc38cf8710ca920df2c774
lukem <lukem@NetBSD.org>
2008-12-12 23:05:36 +0000

    Bump the version to ""nvi-1.81.6nb1" for NetBSD changes: 	expandtab 	gtagsmode 	matchchars 	WARNS=4
    Per discussion with Christos.

--- dist/nvi/ex/version.h
+++ dist/nvi/ex/version.h
@@ -1,3 +1,3 @@
-/*	$NetBSD: version.h,v 1.1.1.1 2008/05/18 14:31:21 aymeric Exp $ */
+/*	$NetBSD: version.h,v 1.2 2008/12/12 23:05:36 lukem Exp $ */
 
-#define VI_VERSION "nvi-1.81.6 (2007-11-18)"
+#define VI_VERSION "nvi-1.81.6nb1 (2007-11-18)"
