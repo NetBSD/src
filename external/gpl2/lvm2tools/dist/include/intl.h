9fd82f8f56210a69e902d01baf85495a1f032612
haad <haad@NetBSD.org>
2008-12-12 11:55:05 +0000

    Resolve conflicts created by import of lvm2tools-2.02.43.

--- external/gpl2/lvm2tools/dist/include/intl.h
+++ external/gpl2/lvm2tools/dist/include/intl.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: intl.h,v 1.1.1.1 2008/12/12 11:42:10 haad Exp $	*/
+/*	$NetBSD: intl.h,v 1.2 2008/12/12 11:55:06 haad Exp $	*/
 
 /*
  * Copyright (C) 2004 Sistina Software, Inc. All rights reserved.  
--- external/gpl2/lvm2tools/dist/include/util.h
+++ external/gpl2/lvm2tools/dist/include/util.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: util.h,v 1.1.1.1 2008/12/12 11:42:10 haad Exp $	*/
+/*	$NetBSD: util.h,v 1.2 2008/12/12 11:55:06 haad Exp $	*/
 
 /*
  * Copyright (C) 2007 Red Hat, Inc. All rights reserved.
