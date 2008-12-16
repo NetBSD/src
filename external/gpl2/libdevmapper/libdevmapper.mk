313166a5616f6acf1061da1d33e5c6a5fc8c76d2
haad <haad@NetBSD.org>
2008-12-16 01:26:58 +0000

    Remove libdevmaper code, lvm2tools are now distributed with libdevmapper source inside.

--- external/gpl2/libdevmapper/libdevmapper.mk
+++
@@ -1,8 +0,0 @@
-#	$NetBSD: libdevmapper.mk,v 1.1.2.3 2008/07/23 13:44:02 lukem Exp $
-
-.include <bsd.own.mk>
-
-LIBDM_SRCDIR=	${NETBSDSRCDIR}/external/gpl2/libdevmapper
-LIBDM_DISTDIR=	${NETBSDSRCDIR}/external/gpl2/libdevmapper/dist
-
-LIBDM_PREFIX=	/usr
