300e83b2a5e6900a1c12dfaed7b9782e18346529
reinoud <reinoud@NetBSD.org>
2008-12-18 15:41:44 +0000

    Implement auto-track repair on tracks marked damaged when searching for writable tracks on session opening. Note that this an optionally implemented feature and thus no error will be generated if it fails; the drive will most likely autorepair it.

--- sys/fs/udf/udf_subr.c
+++ sys/fs/udf/udf_subr.c
@@ -1,4 +1,4 @@
-/* $NetBSD: udf_subr.c,v 1.81 2008/12/18 13:03:49 reinoud Exp $ */
+/* $NetBSD: udf_subr.c,v 1.82 2008/12/18 15:41:44 reinoud Exp $ */
 
 /*
  * Copyright (c) 2006, 2008 Reinoud Zandijk
@@ -29,7 +29,7 @@
 
 #include <sys/cdefs.h>
 #ifndef lint
-__KERNEL_RCSID(0, "$NetBSD: udf_subr.c,v 1.81 2008/12/18 13:03:49 reinoud Exp $");
+__KERNEL_RCSID(0, "$NetBSD: udf_subr.c,v 1.82 2008/12/18 15:41:44 reinoud Exp $");
 #endif /* not lint */
 
 
@@ -487,7 +487,9 @@ udf_check_track_metadata_overlap(struct udf_mount *ump,
 int
 udf_search_writing_tracks(struct udf_mount *ump)
 {
+	struct vnode *devvp = ump->devvp;
 	struct mmc_trackinfo trackinfo;
+	struct mmc_op        op;
 	struct part_desc *part;
 	uint32_t tracknr, start_track, num_tracks;
 	uint32_t track_start, track_end, part_start, part_end;
@@ -526,6 +528,26 @@ udf_search_writing_tracks(struct udf_mount *ump)
 		if (error)
 			return error;
 
+		/*
+		 * If this track is marked damaged, ask for repair. This is an
+		 * optional command, so ignore its error but report warning.
+		 */
+		if (trackinfo.flags & MMC_TRACKINFO_DAMAGED) {
+			memset(&op, 0, sizeof(op));
+			op.operation   = MMC_OP_REPAIRTRACK;
+			op.mmc_profile = ump->discinfo.mmc_profile;
+			op.tracknr     = tracknr;
+			error = VOP_IOCTL(devvp, MMCOP, &op, FKIOCTL, NOCRED);
+			if (error)
+				(void)printf("Drive can't explicitly repair "
+					"damaged track %d, but it might "
+					"autorepair\n", tracknr);
+
+			/* reget track info */
+			error = udf_update_trackinfo(ump, &trackinfo);
+			if (error)
+				return error;
+		}
 		if ((trackinfo.flags & MMC_TRACKINFO_NWA_VALID) == 0)
 			continue;
 	
