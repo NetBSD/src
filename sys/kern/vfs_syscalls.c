221e1e0b40fe42ea60ecbe351d95bedb8850c5ca
snj <snj@NetBSD.org>
2008-12-18 00:52:16 +0000

    Pull up following revision(s) (requested by elad in ticket #188): 	sys/kern/vfs_syscalls.c: revision 1.382 Fix length passed to strlcpy(): we used to get names one character shorter than reality. Should be pulled up to netbsd-5.

--- sys/kern/vfs_syscalls.c
+++ sys/kern/vfs_syscalls.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: vfs_syscalls.c,v 1.376 2008/10/22 11:16:29 ad Exp $	*/
+/*	$NetBSD: vfs_syscalls.c,v 1.376.4.1 2008/12/18 00:52:16 snj Exp $	*/
 
 /*-
  * Copyright (c) 2008 The NetBSD Foundation, Inc.
@@ -63,7 +63,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: vfs_syscalls.c,v 1.376 2008/10/22 11:16:29 ad Exp $");
+__KERNEL_RCSID(0, "$NetBSD: vfs_syscalls.c,v 1.376.4.1 2008/12/18 00:52:16 snj Exp $");
 
 #include "opt_compat_netbsd.h"
 #include "opt_compat_43.h"
@@ -3426,10 +3426,10 @@ do_sys_rename(const char *from, const char *to, enum uio_seg seg, int retain)
 		char *f1, *f2;
 
 		f1 = malloc(fromnd.ni_cnd.cn_namelen + 1, M_TEMP, M_WAITOK);
-		strlcpy(f1, fromnd.ni_cnd.cn_nameptr, fromnd.ni_cnd.cn_namelen);
+		strlcpy(f1, fromnd.ni_cnd.cn_nameptr, fromnd.ni_cnd.cn_namelen + 1);
 
 		f2 = malloc(tond.ni_cnd.cn_namelen + 1, M_TEMP, M_WAITOK);
-		strlcpy(f2, tond.ni_cnd.cn_nameptr, tond.ni_cnd.cn_namelen);
+		strlcpy(f2, tond.ni_cnd.cn_nameptr, tond.ni_cnd.cn_namelen + 1);
 
 		error = veriexec_renamechk(l, fvp, f1, tvp, f2);
 
