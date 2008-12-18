b6832e7f4bb11d56facc6620b87b601bfd008e99
snj <snj@NetBSD.org>
2008-12-18 01:08:58 +0000

    Pull up following revision(s) (requested by reinoud in ticket #192): 	sys/fs/udf/udf_vnops.c: revision 1.34 Fix endless loop occuring on empty directories listing on 512 bytes/sector media like harddiscs. Also provide provide a `safety net' on readdir so it will signal EOF when its requested an entry outside the directory.

--- sys/fs/udf/udf_vnops.c
+++ sys/fs/udf/udf_vnops.c
@@ -1,4 +1,4 @@
-/* $NetBSD: udf_vnops.c,v 1.30.4.2 2008/12/18 01:07:49 snj Exp $ */
+/* $NetBSD: udf_vnops.c,v 1.30.4.3 2008/12/18 01:08:58 snj Exp $ */
 
 /*
  * Copyright (c) 2006, 2008 Reinoud Zandijk
@@ -32,7 +32,7 @@
 
 #include <sys/cdefs.h>
 #ifndef lint
-__KERNEL_RCSID(0, "$NetBSD: udf_vnops.c,v 1.30.4.2 2008/12/18 01:07:49 snj Exp $");
+__KERNEL_RCSID(0, "$NetBSD: udf_vnops.c,v 1.30.4.3 2008/12/18 01:08:58 snj Exp $");
 #endif /* not lint */
 
 
@@ -555,8 +555,7 @@ udf_readdir(void *v)
 
 	/* we are called just as long as we keep on pushing data in */
 	error = 0;
-	if ((uio->uio_offset < file_size) &&
-	    (uio->uio_resid >= sizeof(struct dirent))) {
+	if (uio->uio_offset < file_size) {
 		/* allocate temporary space for fid */
 		lb_size = udf_rw32(udf_node->ump->logical_vol->lb_size);
 		fid = malloc(lb_size, M_UDFTEMP, M_WAITOK);
@@ -606,7 +605,7 @@ udf_readdir(void *v)
 	}
 
 	if (ap->a_eofflag)
-		*ap->a_eofflag = (uio->uio_offset == file_size);
+		*ap->a_eofflag = (uio->uio_offset >= file_size);
 
 #ifdef DEBUG
 	if (udf_verbose & UDF_DEBUG_READDIR) {
