30fd42e845d6c09530829b3bb4d12dec530dc0ec
ad <ad@NetBSD.org>
2008-12-19 11:21:24 +0000

    PR kern/40213 my i386 machine can't boot because of tsc
    - Patch in atomic_cas_64() twice. The first patch is early and makes it
      the MP-atomic version available if we have cmpxchg8b. The second patch
      strips the lock prefix if ncpu==1.
    
    - Fix the i486 atomic_cas_64() to not unconditionally enable interrupts.

--- sys/arch/x86/x86/identcpu.c
+++ sys/arch/x86/x86/identcpu.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: identcpu.c,v 1.11 2008/12/18 12:18:20 cegger Exp $	*/
+/*	$NetBSD: identcpu.c,v 1.12 2008/12/19 11:21:24 ad Exp $	*/
 
 /*-
  * Copyright (c) 1999, 2000, 2001, 2006, 2007, 2008 The NetBSD Foundation, Inc.
@@ -56,7 +56,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: identcpu.c,v 1.11 2008/12/18 12:18:20 cegger Exp $");
+__KERNEL_RCSID(0, "$NetBSD: identcpu.c,v 1.12 2008/12/19 11:21:24 ad Exp $");
 
 #include "opt_enhanced_speedstep.h"
 #include "opt_intel_odcm.h"
@@ -701,6 +701,8 @@ cpu_probe(struct cpu_info *ci)
 		/* If first. */
 		cpu_feature = ci->ci_feature_flags;
 		cpu_feature2 = ci->ci_feature2_flags;
+		/* Early patch of text segment. */
+		x86_patch(true);
 	} else {
 		/* If not first. */
 		cpu_feature &= ci->ci_feature_flags;
