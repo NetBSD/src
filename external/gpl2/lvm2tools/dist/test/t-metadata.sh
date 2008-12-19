800ebf9f617336a6d067e5aa24670e58d61a3a45
haad <haad@NetBSD.org>
2008-12-19 15:24:03 +0000

    Merge the haad-dm branch to -current. This branch adds LVM functionality to the base NetBSD system. It uses Linux LVM2 tools and our BSD licensed device-mapper driver.
    The device-mapper driver can be used to create virtual block devices which
    maps virtual blocks to real with target mapping called target. Currently
    these targets are available a linear, zero, error and a snapshot (this is
    work in progress and doesn't work yet).
    
    The lvm2tools adds lvm and dmsetup binary to based system, where the lvm
    tool is used to manage and administer whole LVM and the dmestup is used to
    communicate iwith device-mapper kernel driver. With these tools also
    a libdevmapper library is instaled to the base system.
    
    Building of tools and driver is currently disable and can be enabled with
    MKLVM=yes in mk.conf. I will add sets lists and rc.d script soon.
    
    Oked by agc@ and cube@.

--- external/gpl2/lvm2tools/dist/test/t-metadata.sh
+++ external/gpl2/lvm2tools/dist/test/t-metadata.sh
@@ -33,3 +33,38 @@ pvcreate -M1 $dev2
 pvcreate -M1 $dev3
 vgcreate -M1 $vg $dev1 $dev2 $dev3
 pvchange --uuid $dev1
+# Copyright (C) 2008 Red Hat, Inc. All rights reserved.
+#
+# This copyrighted material is made available to anyone wishing to use,
+# modify, copy, or redistribute it subject to the terms and conditions
+# of the GNU General Public License v.2.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software Foundation,
+# Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+. ./test-utils.sh
+
+aux prepare_devs 5
+
+pvcreate $dev1
+pvcreate --metadatacopies 0 $dev2
+pvcreate --metadatacopies 0 $dev3
+pvcreate $dev4
+pvcreate --metadatacopies 0 $dev5
+
+vgcreate -c n "$vg" $devs
+lvcreate -n $lv -l 1 -i5 -I256 $vg
+
+pvchange -x n $dev1
+pvchange -x y $dev1
+vgchange -a n $vg
+pvchange --uuid $dev1
+pvchange --uuid $dev2
+vgremove -f $vg
+
+pvcreate -M1 $dev1
+pvcreate -M1 $dev2
+pvcreate -M1 $dev3
+vgcreate -M1 $vg $dev1 $dev2 $dev3
+pvchange --uuid $dev1
--- external/gpl2/lvm2tools/dist/test/t-vgrename-usage.sh
+++ external/gpl2/lvm2tools/dist/test/t-vgrename-usage.sh
@@ -27,3 +27,32 @@ check_vg_field_ $vg1 vg_uuid $UUID
 vgrename $UUID $vg2
 check_vg_field_ $vg2 vg_name $vg2
 vgremove $vg2
+# Copyright (C) 2008 Red Hat, Inc. All rights reserved.
+#
+# This copyrighted material is made available to anyone wishing to use,
+# modify, copy, or redistribute it subject to the terms and conditions
+# of the GNU General Public License v.2.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software Foundation,
+# Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+. ./test-utils.sh
+
+aux prepare_devs 4
+pvcreate $dev1 $dev2
+pvcreate --metadatacopies 0 $dev3 $dev4
+
+# vgrename normal operation - rename vg1 to vg2
+vgcreate $vg1 $dev1 $dev2
+vgrename $vg1 $vg2
+check_vg_field_ $vg2 vg_name $vg2
+vgremove $vg2
+
+# vgrename by uuid (bz231187)
+vgcreate $vg1 $dev1 $dev3
+UUID=$(vgs --noheading -o vg_uuid $vg1)
+check_vg_field_ $vg1 vg_uuid $UUID
+vgrename $UUID $vg2
+check_vg_field_ $vg2 vg_name $vg2
+vgremove $vg2
