aad31b3924b0ae41479dfca32032f94c4f1c3581
cegger <cegger@NetBSD.org>
2008-12-19 15:11:55 +0000

    x86_patch() is not available on Xen. Make Xen kernels link again.

--- sys/arch/x86/include/cpufunc.h
+++ sys/arch/x86/include/cpufunc.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: cpufunc.h,v 1.9 2008/12/19 11:21:24 ad Exp $	*/
+/*	$NetBSD: cpufunc.h,v 1.10 2008/12/19 15:11:55 cegger Exp $	*/
 
 /*-
  * Copyright (c) 1998, 2007 The NetBSD Foundation, Inc.
@@ -49,7 +49,9 @@ void	x86_lfence(void);
 void	x86_sfence(void);
 void	x86_mfence(void);
 void	x86_flush(void);
+#ifndef XEN
 void	x86_patch(bool);
+#endif
 void	invlpg(vaddr_t);
 void	lidt(struct region_descriptor *);
 void	lldt(u_short);
