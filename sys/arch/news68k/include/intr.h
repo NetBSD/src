98dbddf6036f772723b6dfadb0027537e0ad72b8
tsutsui <tsutsui@NetBSD.org>
2008-12-21 17:43:49 +0000

    __inline -> inline

--- sys/arch/news68k/include/intr.h
+++ sys/arch/news68k/include/intr.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: intr.h,v 1.23 2008/06/22 17:33:41 tsutsui Exp $	*/
+/*	$NetBSD: intr.h,v 1.24 2008/12/21 17:43:49 tsutsui Exp $	*/
 
 /*-
  * Copyright (c) 1998 The NetBSD Foundation, Inc.
@@ -77,7 +77,7 @@ splraiseipl(ipl_cookie_t icookie)
 	return _splraise(icookie._psl);
 }
 
-static __inline void
+static inline void
 splx(int sr)
 {
 
