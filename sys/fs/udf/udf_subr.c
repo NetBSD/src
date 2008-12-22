81d74ee0085e11aeab63113e53ff7fa965609396
snj <snj@NetBSD.org>
2008-12-22 03:05:00 +0000

    Pull up following revision(s) (requested by reinoud in ticket #194): 	sys/fs/udf/udf_subr.c: revision 1.81 Fix critital panic when mounting a CD-R/DVD*R disc. The check for VV_ROOT was not done correctly and could panic when requested to load a VAT descriptor since the fileset was not yet fetched.

--- sys/fs/udf/udf_subr.c
+++ sys/fs/udf/udf_subr.c
@@ -1,4 +1,4 @@
-/* $NetBSD: udf_subr.c,v 1.73.4.5 2008/12/18 01:10:51 snj Exp $ */
+/* $NetBSD: udf_subr.c,v 1.73.4.6 2008/12/22 03:05:00 snj Exp $ */
 
 /*
  * Copyright (c) 2006, 2008 Reinoud Zandijk
@@ -29,7 +29,7 @@
 
 #include <sys/cdefs.h>
 #ifndef lint
-__KERNEL_RCSID(0, "$NetBSD: udf_subr.c,v 1.73.4.5 2008/12/18 01:10:51 snj Exp $");
+__KERNEL_RCSID(0, "$NetBSD: udf_subr.c,v 1.73.4.6 2008/12/22 03:05:00 snj Exp $");
 #endif /* not lint */
 
 
@@ -4804,8 +4804,11 @@ udf_get_node(struct udf_mount *ump, struct long_ad *node_icb_loc,
 	udf_node->outstanding_bufs = 0;
 	udf_node->outstanding_nodedscr = 0;
 
-	if (memcmp(&udf_node->loc, &ump->fileset_desc->rootdir_icb, sizeof(struct long_ad)) == 0)
-		nvp->v_vflag |= VV_ROOT;
+	/* check if we're fetching the root */
+	if (ump->fileset_desc)
+		if (memcmp(&udf_node->loc, &ump->fileset_desc->rootdir_icb,
+		    sizeof(struct long_ad)) == 0)
+			nvp->v_vflag |= VV_ROOT;
 
 	/* insert into the hash lookup */
 	udf_register_node(udf_node);
