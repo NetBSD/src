56a34939419542e88b386b2229be7565f4f45461
haad <haad@NetBSD.org>
2008-12-22 00:17:35 +0000

    Import of lvm2tools 2.03.43 and libdevmapper 1.02.29. Needed to fix haad-dm merge, because current lvm2tools sources are broken and will be removed.

---
+++ external/gpl2/lvm2/dist/lib/misc/lvm-wrappers.c
@@ -0,0 +1,24 @@
+/*	$NetBSD: lvm-wrappers.c,v 1.1.1.1 2008/12/22 00:18:13 haad Exp $	*/
+
+/*
+ * Copyright (C) 2006 Red Hat, Inc. All rights reserved.
+ *
+ * This file is part of LVM2.
+ *
+ * This copyrighted material is made available to anyone wishing to use,
+ * modify, copy, or redistribute it subject to the terms and conditions
+ * of the GNU Lesser General Public License v.2.1.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#include "lib.h"
+
+#include <unistd.h>
+
+int lvm_getpagesize(void)
+{
+	return getpagesize();
+}
