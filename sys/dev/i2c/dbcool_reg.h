c6b84858348171f649ad1a994184e22ad92e016b
snj <snj@NetBSD.org>
2008-12-23 03:40:02 +0000

    Pull up following revision(s) (requested by pgoyette in ticket #195): 	sys/dev/i2c/dbcool.c: revision 1.7 	sys/dev/i2c/dbcool_reg.h: revision 1.3 Add i2c bus address checking for these chips.  All supported chips live at predefined addresses 0x2c, 0x2d, or 0x2e.

--- sys/dev/i2c/dbcool_reg.h
+++ sys/dev/i2c/dbcool_reg.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: dbcool_reg.h,v 1.2 2008/10/06 01:35:35 pgoyette Exp $ */
+/*	$NetBSD: dbcool_reg.h,v 1.2.8.1 2008/12/23 03:40:02 snj Exp $ */
 
 /*-
  * Copyright (c) 2008 The NetBSD Foundation, Inc.
@@ -37,10 +37,10 @@
 #define DBCOOLREG_H
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: dbcool_reg.h,v 1.2 2008/10/06 01:35:35 pgoyette Exp $");
+__KERNEL_RCSID(0, "$NetBSD: dbcool_reg.h,v 1.2.8.1 2008/12/23 03:40:02 snj Exp $");
 
-#define DBCOOL_ADDRMASK		0x7f
-#define	DBCOOL_ADDR		0x2e	/* Some chips have multiple addrs */
+#define DBCOOL_ADDRMASK		0x7c
+#define	DBCOOL_ADDR		0x2c	/* Some chips have multiple addrs */
 
 /* The dBCool chip family register set */
 
