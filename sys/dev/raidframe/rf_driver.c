b3402b39c24b45900795594127db629e3e73b51b
bouyer <bouyer>
2008-12-27 19:32:58 +0000

    Pull up following revision(s) (requested by oster in ticket #1249): 	sys/dev/raidframe/rf_driver.c: revision 1.120 	sys/dev/raidframe/rf_reconstruct.c: revision 1.106 When unconfiguring an array where a reconstruct is in progress, abort the reconstruct and wait for IOs to drain before pulling the plug. Should fix the panic reported by der Mouse on tech-kern.

--- sys/dev/raidframe/rf_driver.c
+++ sys/dev/raidframe/rf_driver.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: rf_driver.c,v 1.113.2.1 2008/04/19 15:50:54 bouyer Exp $	*/
+/*	$NetBSD: rf_driver.c,v 1.113.2.2 2008/12/27 19:32:58 bouyer Exp $	*/
 /*-
  * Copyright (c) 1999 The NetBSD Foundation, Inc.
  * All rights reserved.
@@ -73,7 +73,7 @@
 
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: rf_driver.c,v 1.113.2.1 2008/04/19 15:50:54 bouyer Exp $");
+__KERNEL_RCSID(0, "$NetBSD: rf_driver.c,v 1.113.2.2 2008/12/27 19:32:58 bouyer Exp $");
 
 #include "opt_raid_diagnostic.h"
 
@@ -230,9 +230,18 @@ rf_Shutdown(RF_Raid_t *raidPtr)
 
 	/* Wait for any parity re-writes to stop... */
 	while (raidPtr->parity_rewrite_in_progress) {
-		printf("Waiting for parity re-write to exit...\n");
+		printf("raid%d: Waiting for parity re-write to exit...\n",
+		       raidPtr->raidid);
 		tsleep(&raidPtr->parity_rewrite_in_progress, PRIBIO,
 		       "rfprwshutdown", 0);
+	}
+
+	/* Wait for any reconstruction to stop... */
+	while (raidPtr->reconInProgress) {
+		printf("raid%d: Waiting for reconstruction to stop...\n",
+		       raidPtr->raidid);
+		tsleep(&raidPtr->waitForReconCond, PRIBIO,
+		       "rfreshutdown",0);
 	}
 
 	raidPtr->valid = 0;
