06d5a26fdbdd27a680e7f299fc41944229fd44bb
bjh21 <bjh21@NetBSD.org>
2009-01-06 23:58:00 +0000

    Autoconfiguration cleanup for arckbd(4): - Use device_t and cfdata_t rather than explicit structure pointers. - Split device and (non-existent) softc for arcwskbd and arcwsmouse. - Use aprint_*() rather than printf(). - Use device_*() to access fields of device_t. - Use device_private() rather than a cast to convert from device_t to softc. device/softc split not done for arckbd(4) itself because there are lots of void* cookies that get treated as arckbd_softc at one end and device_t at the other, and they'll need a little thought to clean up.

--- sys/arch/acorn26/ioc/arckbdvar.h
+++ sys/arch/acorn26/ioc/arckbdvar.h
@@ -1,4 +1,4 @@
-/* $NetBSD: arckbdvar.h,v 1.2 2002/03/24 23:37:44 bjh21 Exp $ */
+/* $NetBSD: arckbdvar.h,v 1.3 2009/01/06 23:58:00 bjh21 Exp $ */
 /*-
  * Copyright (c) 1998 Ben Harris
  * All rights reserved.
@@ -39,6 +39,6 @@ struct arckbd_kbidtab {
 extern const struct wskbd_mapdata arckbd_mapdata_default;
 extern const struct arckbd_kbidtab arckbd_kbidtab[];
 
-extern void arckbd_cnattach __P((struct device * self));
+extern void arckbd_cnattach(device_t self);
 
 #endif
