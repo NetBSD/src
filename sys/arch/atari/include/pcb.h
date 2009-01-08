7c39de890d0d042bc8d9dada22aa3b8edb558267
snj <snj@NetBSD.org>
2009-01-08 22:33:41 +0000

    Pull up following revision(s) (requested by tsutsui in ticket #230): 	sys/arch/atari/atari/genassym.cf: revision 1.28 	sys/arch/atari/include/pcb.h: revision 1.8 Remove quite obsolete pcb_cmap2.

--- sys/arch/atari/include/pcb.h
+++ sys/arch/atari/include/pcb.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: pcb.h,v 1.7 2007/03/04 05:59:41 christos Exp $	*/
+/*	$NetBSD: pcb.h,v 1.7.54.1 2009/01/08 22:33:41 snj Exp $	*/
 
 /*
  * Copyright (c) 1982, 1986, 1990 The Regents of the University of California.
@@ -91,7 +91,6 @@ struct pcb
 	int	__pcb_spare0;
 	int	pcb_usp;	/* user stack pointer (+8) */
 	int	pcb_regs[12];	/* D2-D7, A2-A7 (+C) */
-	int	pcb_cmap2;	/* temporary copy PTE */
 	void *	pcb_onfault;	/* for copyin/out faults */
 	struct	fpframe pcb_fpregs; /* 68881/2 context save area */
 	int	pcb_exec[16];	/* exec structure for core dumps */
