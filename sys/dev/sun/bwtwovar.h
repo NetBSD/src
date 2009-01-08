38fa5dc84bf11516f770849da5beab9efa648886
snj <snj>
2009-01-08 21:44:07 +0000

    Pull up following revision(s) (requested by jdc in ticket #221): 	sys/dev/sun/bwtwo.c: revision 1.19 	sys/dev/sun/bwtwovar.h: revision 1.7 	sys/dev/sun/files.sun: revision 1.16 Add wscons/vcons support for bw2 (sbus and P4).

--- sys/dev/sun/bwtwovar.h
+++ sys/dev/sun/bwtwovar.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: bwtwovar.h,v 1.6 2008/04/28 20:23:58 martin Exp $ */
+/*	$NetBSD: bwtwovar.h,v 1.6.10.1 2009/01/08 21:44:07 snj Exp $ */
 
 /*-
  * Copyright (c) 1996, 1997 The NetBSD Foundation, Inc.
@@ -89,6 +89,13 @@ struct bwtwo_softc {
 	/* Video status */
 	int	(*sc_get_video)(struct bwtwo_softc *);
 	void	(*sc_set_video)(struct bwtwo_softc *, int);
+#if NWSDISPLAY > 0	
+	uint32_t sc_width;
+	uint32_t sc_height;	/* display width / height */
+	uint32_t sc_stride;
+	int sc_mode;
+	struct vcons_data vd;
+#endif
 };
 
 void	bwtwoattach(struct bwtwo_softc *, const char *, int);
