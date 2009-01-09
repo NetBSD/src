a93104f4604b41df83a4c544b5ca891a83fce6b8
jym <jym>
2009-01-09 22:26:25 +0000

    Do not probe for console when configuring xenbus devices, as it is attached earlier during boot, when initializing hypervisor.
    This avoids the "unknown type console at xenbus0 id 0 not configured"
    autoconf(9) messages, which are misleading during domU's boot.
    
    See also http://mail-index.netbsd.org/port-xen/2009/01/05/msg004621.html
    
    Ok by bouyer@ in private mail.

--- sys/arch/xen/xenbus/xenbus_probe.c
+++ sys/arch/xen/xenbus/xenbus_probe.c
@@ -1,4 +1,4 @@
-/* $NetBSD: xenbus_probe.c,v 1.26 2008/10/29 13:53:15 cegger Exp $ */
+/* $NetBSD: xenbus_probe.c,v 1.27 2009/01/09 22:26:25 jym Exp $ */
 /******************************************************************************
  * Talks to Xen Store to figure out what devices we have.
  *
@@ -29,7 +29,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: xenbus_probe.c,v 1.26 2008/10/29 13:53:15 cegger Exp $");
+__KERNEL_RCSID(0, "$NetBSD: xenbus_probe.c,v 1.27 2009/01/09 22:26:25 jym Exp $");
 
 #if 0
 #define DPRINTK(fmt, args...) \
@@ -395,6 +395,14 @@ xenbus_probe_frontends(void)
 		return err;
 
 	for (i = 0; i < dir_n; i++) {
+		/*
+		 * console is configured through xen_start_info when
+		 * xencons is attaching to hypervisor, so avoid console
+		 * probing when configuring xenbus devices
+		 */
+		if (strcmp(dir[i], "console") == 0)
+			continue;
+
 		snprintf(path, sizeof(path), "device/%s", dir[i]);
 		err = xenbus_probe_device_type(path, dir[i], NULL);
 		if (err)
