9d1f7dc0d04a3cb6244cd24d47426ce2a38ea0b9
pooka <pooka@NetBSD.org>
2009-01-11 20:31:03 +0000

    Default to threading, non-threaded debug mode can be compiled separately. Useful if running this from fstab.

--- usr.sbin/puffs/rump_nfs/rump_nfs.c
+++ usr.sbin/puffs/rump_nfs/rump_nfs.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: rump_nfs.c,v 1.6 2009/01/10 20:21:15 pooka Exp $	*/
+/*	$NetBSD: rump_nfs.c,v 1.7 2009/01/11 20:31:03 pooka Exp $	*/
 
 /*
  * Copyright (c) 2008 Antti Kantee.  All Rights Reserved.
@@ -46,10 +46,10 @@ main(int argc, char *argv[])
 	struct nfs_args args;
 	char canon_dev[MAXPATHLEN], canon_dir[MAXPATHLEN];
 	int rv, mntflags;
-	char *thr;
 
 #if 0
 	extern int rumpns_nfs_niothreads; /* XXX */
+	char *thr;
 	/*
 	 * XXX: but we can't call rump init before we detach (fork()) in
 	 * p2k_run_fs() lest we lose our threads.
@@ -57,6 +57,8 @@ main(int argc, char *argv[])
 	thr = getenv("RUMP_THREADS");
 	if (!thr || !*thr)
 		rumpns_nfs_niothreads = 0;
+#else
+	setenv("RUMP_THREADS", "1", 1);
 #endif
 
 	setprogname(argv[0]);
