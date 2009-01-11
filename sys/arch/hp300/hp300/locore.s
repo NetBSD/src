87683d1476cf772bc873c82fec94df8360849928
tsutsui <tsutsui@NetBSD.org>
2009-01-11 06:02:18 +0000

    Fix inverted logic on FPU type check in fpfault().  From OpenBSD.

--- sys/arch/hp300/hp300/locore.s
+++ sys/arch/hp300/hp300/locore.s
@@ -1,4 +1,4 @@
-/*	$NetBSD: locore.s,v 1.145 2008/12/21 17:42:05 tsutsui Exp $	*/
+/*	$NetBSD: locore.s,v 1.146 2009/01/11 06:02:18 tsutsui Exp $	*/
 
 /*
  * Copyright (c) 1980, 1990, 1993
@@ -860,7 +860,7 @@ ENTRY_NOPROFILE(fpfault)
 #if defined(M68040) || defined(M68060)
 	/* always null state frame on 68040, 68060 */
 	cmpl	#FPU_68040,_C_LABEL(fputype)
-	jle	Lfptnull
+	jge	Lfptnull
 #endif
 	tstb	%a0@			| null state frame?
 	jeq	Lfptnull		| yes, safe
