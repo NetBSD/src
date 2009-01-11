e0ca4f3a68603dde5485e9b9826b03bfa87fccc2
pooka <pooka@NetBSD.org>
2009-01-11 12:33:50 +0000

    Warn that autoselecting nfs based on : or @ in the device path will be removed in a future release.

--- sbin/mount/mount.c
+++ sbin/mount/mount.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: mount.c,v 1.86 2008/08/05 20:57:45 pooka Exp $	*/
+/*	$NetBSD: mount.c,v 1.87 2009/01/11 12:33:50 pooka Exp $	*/
 
 /*
  * Copyright (c) 1980, 1989, 1993, 1994
@@ -39,7 +39,7 @@ __COPYRIGHT("@(#) Copyright (c) 1980, 1989, 1993, 1994\
 #if 0
 static char sccsid[] = "@(#)mount.c	8.25 (Berkeley) 5/8/95";
 #else
-__RCSID("$NetBSD: mount.c,v 1.86 2008/08/05 20:57:45 pooka Exp $");
+__RCSID("$NetBSD: mount.c,v 1.87 2009/01/11 12:33:50 pooka Exp $");
 #endif
 #endif /* not lint */
 
@@ -280,9 +280,14 @@ main(int argc, char *argv[])
 		 * specified ala Sun.
 		 */
 		if (vfslist == NULL) {
-			if (strpbrk(argv[0], ":@") != NULL)
+			if (strpbrk(argv[0], ":@") != NULL) {
+				fprintf(stderr, "WARNING: autoselecting nfs "
+				    "based on : or @ in the device name is "
+				    "deprecated!\n"
+				    "WARNING: This behaviour will be removed "
+				    "in a future release\n");
 				vfstype = "nfs";
-			else {
+			} else {
 				vfstype = getfslab(argv[0]);
 				if (vfstype == NULL)
 					vfstype = ffs_fstype;
