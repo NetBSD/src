dea0819649e7868454b1de1d609b7a5cba6fdc63
bouyer <bouyer@NetBSD.org>
2009-01-15 22:05:38 +0000

    Pull up following revision(s) (requested by lukem in ticket #1256): 	lib/libc/gen/fts.c: revision 1.35 Ensure fts_close() doesn't spuriously close fd 0, by testing FTS_SYMFOLLOW in fts_flags instead of fts_options. Fix provided by Ben Harris in PR 40319

--- lib/libc/gen/fts.c
+++ lib/libc/gen/fts.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: fts.c,v 1.31 2006/03/30 01:23:50 christos Exp $	*/
+/*	$NetBSD: fts.c,v 1.31.4.1 2009/01/15 22:05:38 bouyer Exp $	*/
 
 /*-
  * Copyright (c) 1990, 1993, 1994
@@ -38,7 +38,7 @@
 #if 0
 static char sccsid[] = "@(#)fts.c	8.6 (Berkeley) 8/14/94";
 #else
-__RCSID("$NetBSD: fts.c,v 1.31 2006/03/30 01:23:50 christos Exp $");
+__RCSID("$NetBSD: fts.c,v 1.31.4.1 2009/01/15 22:05:38 bouyer Exp $");
 #endif
 #endif /* LIBC_SCCS and not lint */
 
@@ -249,7 +249,7 @@ fts_close(FTS *sp)
 	 * list which has a valid parent pointer.
 	 */
 	if (sp->fts_cur) {
-		if (ISSET(FTS_SYMFOLLOW))
+		if (sp->fts_cur->fts_flags & FTS_SYMFOLLOW)
 			(void)close(sp->fts_cur->fts_symfd);
 		for (p = sp->fts_cur; p->fts_level >= FTS_ROOTLEVEL;) {
 			freep = p;
