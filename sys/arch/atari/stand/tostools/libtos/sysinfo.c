9d693f1e6958e3dd968da5443bfc61b99a9939c5
bouyer <bouyer>
2009-01-15 23:20:02 +0000

    Pull up following revision(s) (requested by tsutsui in ticket #1263): 	sys/arch/atari/stand/xxboot/Makefile.xxboot: revision 1.5 	sys/arch/atari/stand/tostools/libtos/libtos.h: revision 1.5 	sys/arch/atari/stand/installboot/disklabel.c: revision 1.3 	sys/arch/atari/stand/bootxxx/Makefile.bootxxx: revision 1.4 	sys/arch/atari/stand/bootxxx/Makefile.bootxxx: revision 1.5 	sys/arch/atari/stand/libsa/Makefile: revision 1.28 	sys/arch/atari/stand/bootxx/start.S: revision 1.1 	distrib/utils/sysinst/arch/atari/md.c: revision 1.23 	sys/arch/atari/stand/bootxx/Makefile.bootxx: revision 1.8 	sys/arch/atari/stand/tostools/libtos/aout.c: revision 1.11 	sys/arch/atari/stand/installboot/installboot.h: revision 1.9 	sys/arch/atari/stand/bootxxx/bootxxx.c: revision 1.4 	distrib/sets/lists/base/md.atari: revision 1.50 	sys/arch/atari/stand/Makefile.booters: revision 1.1 	sys/arch/atari/stand/bootxx/bootxx.c: revision 1.13 	sys/arch/atari/stand/installboot/installboot.c: revision 1.22 	sys/arch/atari/stand/tostools/libtos/elf.c: revision 1.9 	sys/arch/atari/stand/tostools/libtos/sysinfo.c: revision 1.8 	sys/arch/atari/stand/bootxxx/start.S: revision 1.1 Fix PR port-atari/40315: Bootloader fails on Atari TT030  Add asm sources which provide a fixed entry point for raw bootxx and bootxxx  because modern aggressive gcc4 may reorder functions in the same source  and the first function in a C source won't always appear at the beginning  in its object. Also overhaul various files in the stand directory: - rename stand/Makefile.inc to stand/Makefile.booters and   explicitly include it from each Makefile because the stand   directory contains non standalone programs like installboot and   Makefile.inc will be included implicitly from all Makefiles in SUBDIRs - put more common options into Makefile.booters so that   all boot programs use proper options (-Os etc.) - make standalone boot programs compile without installed ${DESTDIR}:   - create machine and m68k symlinks in ${.OBJDIR} in all boot programs   - set appropriate make environments to suppress errors and warnings   - <string.h> isn't there in _STANDALONE case   - put #ifdef TOSTOOLS (looks equivarent with !_STANDALONE)     to some files in stand/tostools   - use ${LD} ${LINKFLAGS} rather than ${CC} ${LDFLAGS} - create raw binaries on ${PROG} target rather than on beforeinstall   and stop weird renaming on beforeinstall and afterinstall - print proper error message if bootxx size is larger than limit - create ${DESTDIR}/usr/mdec/{milan,std} directories on beforeinstall   in all boot programs   (XXX: how can we put MD /usr/mdec/foo directories into src/etc/mtree?) - prototype warnsfy - cleanup Makefiles for readablity and remove unnecessary rules - no need to use daddr_t in installboot because the AHDI label doesn't   support 64 bit block numbers Tested by David Ross (PR submitter) on port-atari, and finally NetBSD/atari on TT030 is now fully functional. Should be pulled up to netbsd-4 (where gcc4 was initially imported) and netbsd-5. Don't install built boot.atari loaders into / of DESTDIR because there are two variants of boot.atari per machine types (std or milan) and they should be selected and copied from /usr/mdec/{std,milan} dirs to /targetroot during installation. Problem on default installation with sysinst was reported by David Ross. Remove /boot.atari, which is no longer installed during build and removed from base.tgz. Note we must not mark this obsolete because this tertiary bootloader will be copied into / during installation and it shouldn't be removed by postinstall(8). Before installboot(8), check running machine types (milan or others) via sysctl(3) and copy appropriate tertiary boot.atari loader from /usr/mdec/{std,milan} into targetroot. Full fresh installations of 5.0_BETA and 4.0_STABLE with the same change have been tested by David Ross on port-atari. Fixes the final portion of PR port-atari/40315, and pullup requests to both netbsd-4 and netbsd-5 will be sent shortly.

--- sys/arch/atari/stand/tostools/libtos/sysinfo.c
+++ sys/arch/atari/stand/tostools/libtos/sysinfo.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: sysinfo.c,v 1.6 2005/12/11 12:17:00 christos Exp $	*/
+/*	$NetBSD: sysinfo.c,v 1.6.24.1 2009/01/15 23:20:02 bouyer Exp $	*/
 
 /*-
  * Copyright (c) 1998 The NetBSD Foundation, Inc.
@@ -43,7 +43,6 @@
 
 #include <lib/libsa/stand.h>
 #include <atari_stand.h>
-#include <string.h>
 #include <libkern.h>
 #include <machine/cpu.h>
 #endif /* TOSTOOLS */
