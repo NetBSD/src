96ca5563b0b6ab684d51bb95a6551e3daae4344c
rtr <rtr>
2009-01-15 12:35:13 +0000

    use PRIu64 format to print major and minor dev_t from Wojciech Galazka on current-users@

--- sys/arch/hp700/hp700/machdep.c
+++ sys/arch/hp700/hp700/machdep.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: machdep.c,v 1.55 2008/12/16 22:35:23 christos Exp $	*/
+/*	$NetBSD: machdep.c,v 1.56 2009/01/15 12:35:13 rtr Exp $	*/
 
 /*-
  * Copyright (c) 2001, 2002 The NetBSD Foundation, Inc.
@@ -63,7 +63,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: machdep.c,v 1.55 2008/12/16 22:35:23 christos Exp $");
+__KERNEL_RCSID(0, "$NetBSD: machdep.c,v 1.56 2009/01/15 12:35:13 rtr Exp $");
 
 #include "opt_cputype.h"
 #include "opt_ddb.h"
@@ -1608,10 +1608,12 @@ dumpsys(void)
 	if (dumpsize == 0)
 		cpu_dumpconf();
 	if (dumplo <= 0) {
-		printf("\ndump to dev %x not possible\n", dumpdev);
+		printf("\ndump to dev %" PRIu64 ",%" PRIu64 " not possible\n",
+		    major(dumpdev), minor(dumpdev));
 		return;
 	}
-	printf("\ndumping to dev %x, offset %ld\n", dumpdev, dumplo);
+	printf("\ndumping to dev %" PRIu64 ",%" PRIu64 " offset %ld\n",
+	    major(dumpdev), minor(dumpdev), dumplo);
 
 	psize = (*bdev->d_psize)(dumpdev);
 	printf("dump ");
