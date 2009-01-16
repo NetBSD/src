32081d9a92dc23f7b69e5b59eb87421c357f5f1e
christos <christos>
2009-01-16 21:18:37 +0000

    make this compile [now that I am on a machine that can compile :-)]

--- lib/libc/locale/_wctrans.c
+++ lib/libc/locale/_wctrans.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: _wctrans.c,v 1.9 2009/01/16 21:12:11 christos Exp $	*/
+/*	$NetBSD: _wctrans.c,v 1.10 2009/01/16 21:18:37 christos Exp $	*/
 
 /*-
  * Copyright (c)2003 Citrus Project,
@@ -60,7 +60,7 @@
 
 #include <sys/cdefs.h>
 #if defined(LIBC_SCCS) && !defined(lint)
-__RCSID("$NetBSD: _wctrans.c,v 1.9 2009/01/16 21:12:11 christos Exp $");
+__RCSID("$NetBSD: _wctrans.c,v 1.10 2009/01/16 21:18:37 christos Exp $");
 #endif /* LIBC_SCCS and not lint */
 
 #include "namespace.h"
@@ -82,7 +82,7 @@ _wctrans_init(_RuneLocale *rl)
 	if (rl == &_DefaultRuneLocale) {
 		if ((rl = malloc(sizeof(*rl))) == NULL)
 			return NULL;
-		(void)memcpy(rl, &_DefaultRuneLocale, sizeof(*rl))
+		(void)memcpy(rl, &_DefaultRuneLocale, sizeof(*rl));
 	}
 	rl->rl_wctrans[_WCTRANS_INDEX_LOWER].te_name = "tolower";
 	rl->rl_wctrans[_WCTRANS_INDEX_LOWER].te_cached = rl->rl_maplower;
@@ -90,6 +90,7 @@ _wctrans_init(_RuneLocale *rl)
 	rl->rl_wctrans[_WCTRANS_INDEX_UPPER].te_name = "toupper";
 	rl->rl_wctrans[_WCTRANS_INDEX_UPPER].te_cached = rl->rl_mapupper;
 	rl->rl_wctrans[_WCTRANS_INDEX_UPPER].te_extmap = &rl->rl_mapupper_ext;
+	return rl;
 }
 
 /*
