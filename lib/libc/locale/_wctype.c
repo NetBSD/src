50406167ad8c97e6b3707c313e5fcba00de47e70
christos <christos@NetBSD.org>
2009-01-16 21:12:11 +0000

    PR/40411: Hisashi T Fujinaka: standalone-tcsh dumps core when tab completing
    The new rune code abuses __UNCONST to lazily initialize _RuneLocale. If
    that happens to be the _DefaultRuneLocale which is const, the program will
    core-dump because it will attempt to write to read-only memory. Catch this
    and clone a copy of the locale and return it. The reason we don't see
    everything core-dumping is because our shared library code probably loads
    the text segment COW so it works (Is there an mprotect missing somewhere?).
    But on statically linked binaries this is not the case and we die.
    
    XXX[1]: Vet the code so that we are sure that there is no more of that
            happening trying to get rid of much of the __UNCONST'ing.
    XXX[2]: This needs to be fixed for 5.0 and all related fixes.
    XXX[3]: There is one place in the code where _DefaultRuneLocale is treated
    	specially, does the patch break things?

--- lib/libc/locale/_wctype.c
+++ lib/libc/locale/_wctype.c
@@ -1,4 +1,4 @@
-/* $NetBSD: _wctype.c,v 1.2 2009/01/11 02:46:28 christos Exp $ */
+/* $NetBSD: _wctype.c,v 1.3 2009/01/16 21:12:11 christos Exp $ */
 
 /*-
  * Copyright (c)2008 Citrus Project,
@@ -60,7 +60,7 @@
 
 #include <sys/cdefs.h>
 #if defined(LIBC_SCCS) && !defined(lint)
-__RCSID("$NetBSD: _wctype.c,v 1.2 2009/01/11 02:46:28 christos Exp $");
+__RCSID("$NetBSD: _wctype.c,v 1.3 2009/01/16 21:12:11 christos Exp $");
 #endif /* LIBC_SCCS and not lint */
 
 #include <assert.h>
@@ -127,7 +127,7 @@ _towctrans_priv(_RuneLocale const *rl,
 	if (wc == WEOF)
 		return wc;
 	if (te->te_name == NULL)
-		_wctrans_init(__UNCONST(rl));
+		rl = _wctrans_init(__UNCONST(rl));
 	if (_RUNE_ISCACHED(wc))
 		return te->te_cached[(size_t)wc];
 	wc0 = (__nbrune_t)wc;
