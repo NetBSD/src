441088174a468ed87573273bd1221437d8086c0d
tsutsui <tsutsui>
2009-01-17 07:17:35 +0000

    Move extern decls for Sysseg, Sysptmap, Sysptsize, mem_size, virtual_avail, virtual_end, and protection_codes[] to common <m68k/pmap_motorola.h>, and also make protection_codes[] unsigned.
    XXX: avail_start and avail_end should also be moved, but it causes
    XXX: -Wshadow warnings in uvm/uvm_page.c:uvm_page_physload() and
    XXX: I don't have a good idea of alternative names for now.

--- sys/arch/cesfic/cesfic/pmap_bootstrap.c
+++ sys/arch/cesfic/cesfic/pmap_bootstrap.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: pmap_bootstrap.c,v 1.13 2008/12/28 05:15:59 tsutsui Exp $	*/
+/*	$NetBSD: pmap_bootstrap.c,v 1.14 2009/01/17 07:17:35 tsutsui Exp $	*/
 
 /* 
  * Copyright (c) 1991, 1993
@@ -36,7 +36,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: pmap_bootstrap.c,v 1.13 2008/12/28 05:15:59 tsutsui Exp $");
+__KERNEL_RCSID(0, "$NetBSD: pmap_bootstrap.c,v 1.14 2009/01/17 07:17:35 tsutsui Exp $");
 
 #include <sys/param.h>
 #include <sys/msgbuf.h>
@@ -53,14 +53,10 @@ __KERNEL_RCSID(0, "$NetBSD: pmap_bootstrap.c,v 1.13 2008/12/28 05:15:59 tsutsui 
 #define RELOCPTR(v, t)	((t)((uintptr_t)RELOC((v), t) + firstpa - KERNBASE))
 
 extern char *etext;
-extern int Sysptsize;
 extern char *proc0paddr;
-extern st_entry_t *Sysseg;
-extern pt_entry_t *Sysptmap, *Sysmap;
+extern paddr_t avail_start, avail_end;
 
 extern int physmem;
-extern vm_offset_t avail_start, avail_end, virtual_avail, virtual_end;
-extern int protection_codes[];
 
 void	pmap_bootstrap __P((vm_offset_t, vm_offset_t));
 
@@ -402,9 +398,9 @@ pmap_bootstrap(nextpa, firstpa)
 	 * absolute "jmp" table.
 	 */
 	{
-		int *kp;
+		u_int *kp;
 
-		kp = &RELOC(protection_codes, int);
+		kp = &RELOC(protection_codes, u_int);
 		kp[VM_PROT_NONE|VM_PROT_NONE|VM_PROT_NONE] = 0;
 		kp[VM_PROT_READ|VM_PROT_NONE|VM_PROT_NONE] = PG_RO;
 		kp[VM_PROT_READ|VM_PROT_NONE|VM_PROT_EXECUTE] = PG_RO;
