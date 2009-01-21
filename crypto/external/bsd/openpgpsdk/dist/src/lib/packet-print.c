bbfe341047d7363d2f5e2d76ec51270dcc463105
agc <agc@NetBSD.org>
2009-01-21 20:17:14 +0000

    gmtime(3) returns a pointer to a struct tm with a month value in the range [0,11], so add 1 to this to get a useful value for human interpretation.

--- crypto/external/bsd/openpgpsdk/dist/src/lib/packet-print.c
+++ crypto/external/bsd/openpgpsdk/dist/src/lib/packet-print.c
@@ -398,7 +398,7 @@ static void showtime_short(time_t t)
     printf(buf);
     */
     tm=gmtime(&t);
-    printf ("%04d-%02d-%02d", tm->tm_year+1900, tm->tm_mon, tm->tm_mday);
+    printf ("%04d-%02d-%02d", tm->tm_year+1900, tm->tm_mon + 1, tm->tm_mday);
     }
 
 
