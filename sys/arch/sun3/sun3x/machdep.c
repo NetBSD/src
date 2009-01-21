48de2907d4dcb1c641a536c55969f5fc28264be3
he <he>
2009-01-21 16:24:33 +0000

    Re-adapt to the changed return types for major() and minor().

--- sys/arch/sun3/sun3x/machdep.c
+++ sys/arch/sun3/sun3x/machdep.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: machdep.c,v 1.116 2009/01/12 17:17:38 tsutsui Exp $	*/
+/*	$NetBSD: machdep.c,v 1.117 2009/01/21 16:24:34 he Exp $	*/
 
 /*
  * Copyright (c) 1982, 1986, 1990, 1993
@@ -75,7 +75,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: machdep.c,v 1.116 2009/01/12 17:17:38 tsutsui Exp $");
+__KERNEL_RCSID(0, "$NetBSD: machdep.c,v 1.117 2009/01/21 16:24:34 he Exp $");
 
 #include "opt_ddb.h"
 #include "opt_kgdb.h"
@@ -621,7 +621,7 @@ dumpsys(void)
 	if (dumpsize == 0)
 		cpu_dumpconf();
 	if (dumplo <= 0) {
-		printf("\ndump to dev %" PRIu64 ",%" PRIu64 " not possible\n",
+		printf("\ndump to dev %u,%u not possible\n",
 		    major(dumpdev), minor(dumpdev));
 		return;
 	}
@@ -633,7 +633,7 @@ dumpsys(void)
 		return;
 	}
 
-	printf("\ndumping to dev %" PRIu64 ",%" PRIu64" offset %ld\n",
+	printf("\ndumping to dev %u,%u offset %ld\n",
 	    major(dumpdev), minor(dumpdev), dumplo);
 
 	/*
