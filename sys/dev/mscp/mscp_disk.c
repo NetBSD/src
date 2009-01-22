f907d9477d23cf43f824da411413534115dd3047
cegger <cegger@NetBSD.org>
2009-01-22 17:32:20 +0000

    buildfix: re-adapt to major() return a 32bit value again.

--- sys/dev/mscp/mscp_disk.c
+++ sys/dev/mscp/mscp_disk.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: mscp_disk.c,v 1.62 2009/01/13 13:35:53 yamt Exp $	*/
+/*	$NetBSD: mscp_disk.c,v 1.63 2009/01/22 17:32:20 cegger Exp $	*/
 /*
  * Copyright (c) 1988 Regents of the University of California.
  * All rights reserved.
@@ -81,7 +81,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: mscp_disk.c,v 1.62 2009/01/13 13:35:53 yamt Exp $");
+__KERNEL_RCSID(0, "$NetBSD: mscp_disk.c,v 1.63 2009/01/22 17:32:20 cegger Exp $");
 
 #include <sys/param.h>
 #include <sys/buf.h>
@@ -984,7 +984,7 @@ rriodone(usc, bp)
 		ra = device_lookup_private(&rx_cd, unit);
 	else
 #endif
-		panic("rriodone: unexpected major %"PRIu64" unit %u",
+		panic("rriodone: unexpected major %"PRIu32" unit %u",
 		    major(bp->b_dev), unit);
 	disk_unbusy(&ra->ra_disk, bp->b_bcount, (bp->b_flags & B_READ));
 
