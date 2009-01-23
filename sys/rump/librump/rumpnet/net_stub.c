25a94238994a16ff3df4a81ddbbd67aabf520a8e
pooka <pooka@NetBSD.org>
2009-01-23 21:57:35 +0000

    Add a few stubs to allow to compile - i'll have to sort these out later.

--- sys/rump/librump/rumpnet/net_stub.c
+++ sys/rump/librump/rumpnet/net_stub.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: net_stub.c,v 1.7 2008/12/29 23:18:35 pooka Exp $	*/
+/*	$NetBSD: net_stub.c,v 1.8 2009/01/23 21:57:35 pooka Exp $	*/
 
 /*
  * Copyright (c) 2008 Antti Kantee.  All Rights Reserved.
@@ -26,7 +26,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: net_stub.c,v 1.7 2008/12/29 23:18:35 pooka Exp $");
+__KERNEL_RCSID(0, "$NetBSD: net_stub.c,v 1.8 2009/01/23 21:57:35 pooka Exp $");
 
 #include <sys/param.h>
 #include <sys/protosw.h>
@@ -52,6 +52,9 @@ __weak_alias(rtrequest,__rumpnet_stub);
 __weak_alias(ifioctl,__rumpnet_stub);
 __weak_alias(ifunit,__rumpnet_stub);
 __weak_alias(ifreq_setaddr,__rumpnet_stub);
+__weak_alias(rt_msg1,__rumpnet_stub);
+__weak_alias(route_enqueue,__rumpnet_stub);
+__weak_alias(route_cb,__rumpnet_stub);
 
 struct ifnet_head ifnet;
 
