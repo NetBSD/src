d82b81688ba90abb08f8f52a0e9dc6cf1d88dc12
rmind <rmind@NetBSD.org>
2009-01-24 22:42:32 +0000

    Set l_psid to PS_NONE for lwp0 (consistency change).

--- sys/kern/kern_proc.c
+++ sys/kern/kern_proc.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: kern_proc.c,v 1.146 2008/12/17 20:51:36 cegger Exp $	*/
+/*	$NetBSD: kern_proc.c,v 1.147 2009/01/24 22:42:32 rmind Exp $	*/
 
 /*-
  * Copyright (c) 1999, 2006, 2007, 2008 The NetBSD Foundation, Inc.
@@ -62,7 +62,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: kern_proc.c,v 1.146 2008/12/17 20:51:36 cegger Exp $");
+__KERNEL_RCSID(0, "$NetBSD: kern_proc.c,v 1.147 2009/01/24 22:42:32 rmind Exp $");
 
 #include "opt_kstack.h"
 #include "opt_maxuprc.h"
@@ -80,6 +80,7 @@ __KERNEL_RCSID(0, "$NetBSD: kern_proc.c,v 1.146 2008/12/17 20:51:36 cegger Exp $
 #include <sys/uio.h>
 #include <sys/malloc.h>
 #include <sys/pool.h>
+#include <sys/pset.h>
 #include <sys/mbuf.h>
 #include <sys/ioctl.h>
 #include <sys/tty.h>
@@ -200,6 +201,7 @@ struct lwp lwp0 __aligned(MIN_LWP_ALIGNMENT) = {
 	.l_priority = PRI_USER + NPRI_USER - 1,
 	.l_inheritedprio = -1,
 	.l_class = SCHED_OTHER,
+	.l_psid = PS_NONE,
 	.l_pi_lenders = SLIST_HEAD_INITIALIZER(&lwp0.l_pi_lenders),
 	.l_name = __UNCONST("swapper"),
 };
