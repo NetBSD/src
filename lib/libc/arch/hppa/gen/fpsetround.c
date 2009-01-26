365b4def08de36194f387b029d4f7675cf3b9c92
snj <snj@NetBSD.org>
2009-01-26 00:54:12 +0000

    Pull up following revision(s) (requested by skrll in ticket #318): 	lib/libc/arch/hppa/gen/fpsetmask.c: revision 1.5 	lib/libc/arch/hppa/gen/fpsetround.c: revision 1.5 	lib/libc/arch/hppa/gen/fpsetsticky.c: revision 1.5 Sprinkle the "memory" clobber register so the fpsr manipulation actually happens. Fixes the ieeefp/except regresion test.

--- lib/libc/arch/hppa/gen/fpsetround.c
+++ lib/libc/arch/hppa/gen/fpsetround.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: fpsetround.c,v 1.4 2005/12/24 21:42:32 perry Exp $	*/
+/*	$NetBSD: fpsetround.c,v 1.4.26.1 2009/01/26 00:54:12 snj Exp $	*/
 
 /*	$OpenBSD: fpsetround.c,v 1.3 2002/10/21 18:41:05 mickey Exp $	*/
 
@@ -8,7 +8,7 @@
 
 #include <sys/cdefs.h>
 #if defined(LIBC_SCCS) && !defined(lint)
-__RCSID("$NetBSD: fpsetround.c,v 1.4 2005/12/24 21:42:32 perry Exp $");
+__RCSID("$NetBSD: fpsetround.c,v 1.4.26.1 2009/01/26 00:54:12 snj Exp $");
 #endif /* LIBC_SCCS and not lint */
 
 #include <sys/types.h>
@@ -20,10 +20,10 @@ fpsetround(fp_rnd rnd_dir)
 	uint64_t fpsr;
 	fp_rnd old;
 
-	__asm volatile("fstd %%fr0,0(%1)" : "=m" (fpsr) : "r" (&fpsr));
+	__asm volatile("fstd %%fr0,0(%1)" : "=m" (fpsr) : "r" (&fpsr) : "memory");
 	old = (fpsr >> 41) & 0x03;
 	fpsr = (fpsr & 0xfffff9ff00000000LL) |
 	    ((uint64_t)(rnd_dir & 0x03) << 41);
-	__asm volatile("fldd 0(%0),%%fr0" : : "r" (&fpsr));
+	__asm volatile("fldd 0(%0),%%fr0" : : "r" (&fpsr) : "memory");
 	return (old);
 }
