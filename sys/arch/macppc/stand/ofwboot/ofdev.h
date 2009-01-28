722f4c13b83b959234b39782d8d5e5b0707a3cbe
tsutsui <tsutsui@NetBSD.org>
2009-01-28 15:03:28 +0000

    - modify a pathname parse function to explicitly return devname and filename - check floppyboot properly per devname before loadfile(), not in devopen() - disable LOAD_NOTE on floppyboot to avoid backward seek - bump revision
    Tested floppy boot and IDE boot on Apus2000 with OpenFirmware 2.0.
    
    Fixes PR install/38943 on macppc.

--- sys/arch/macppc/stand/ofwboot/ofdev.h
+++ sys/arch/macppc/stand/ofwboot/ofdev.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: ofdev.h,v 1.5 2005/12/11 12:18:06 christos Exp $	*/
+/*	$NetBSD: ofdev.h,v 1.6 2009/01/28 15:03:28 tsutsui Exp $	*/
 
 /*
  * Copyright (C) 1995, 1996 Wolfgang Solfrank.
@@ -47,7 +47,8 @@ struct of_dev {
 #define OFDEV_NET	1
 #define OFDEV_DISK	2
 
+bool parsefilepath(const char *, char *, char *, char *);
+
 extern char opened_name[];
-extern int floppyboot;
 
 #endif
