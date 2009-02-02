276950dceee68fb53dda60f1090825b8b4ff5f7f
snj <snj@NetBSD.org>
2009-02-02 22:19:09 +0000

    Pull up following revision(s) (requested by tsutsui in ticket #407): 	sys/arch/macppc/stand/ofwboot/boot.c: revision 1.23 	sys/arch/macppc/stand/ofwboot/boot.h: revision 1.3 	sys/arch/macppc/stand/ofwboot/ofdev.c: revision 1.22 	sys/arch/macppc/stand/ofwboot/ofdev.h: revision 1.6 	sys/arch/macppc/stand/ofwboot/version: revision 1.12 - modify a pathname parse function to explicitly return devname and filename - check floppyboot properly per devname before loadfile(), not in devopen() - disable LOAD_NOTE on floppyboot to avoid backward seek - bump revision Tested floppy boot and IDE boot on Apus2000 with OpenFirmware 2.0. Fixes PR install/38943 on macppc.

--- sys/arch/macppc/stand/ofwboot/boot.h
+++ sys/arch/macppc/stand/ofwboot/boot.h
@@ -7,6 +7,10 @@ typedef void (*boot_entry_t)(int, int, int (*)(void *), void *, u_int);
 
 void main(void);
 
+#define MAXBOOTPATHLEN	256
+extern char bootdev[MAXBOOTPATHLEN];
+extern bool floppyboot;
+
 #ifdef HAVE_CHANGEDISK_HOOK
 struct open_file;
 
