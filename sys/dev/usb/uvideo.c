a174097b6f8ac8e26d75b77aed916c1fafd97615
jmorse <jmorse@NetBSD.org>
2009-02-09 23:28:50 +0000

    Fix uvideo_stream_init uninitialized variable / bad argument, leading to panic when opening video stream.

--- sys/dev/usb/uvideo.c
+++ sys/dev/usb/uvideo.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: uvideo.c,v 1.26 2009/01/30 12:55:46 jmcneill Exp $	*/
+/*	$NetBSD: uvideo.c,v 1.27 2009/02/09 23:28:50 jmorse Exp $	*/
 
 /*
  * Copyright (c) 2008 Patrick Mahoney
@@ -42,7 +42,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: uvideo.c,v 1.26 2009/01/30 12:55:46 jmcneill Exp $");
+__KERNEL_RCSID(0, "$NetBSD: uvideo.c,v 1.27 2009/02/09 23:28:50 jmorse Exp $");
 
 #ifdef _MODULE
 #include <sys/module.h>
@@ -233,6 +233,7 @@ struct uvideo_stream {
 
 	/* current video format */
 	uint32_t		vs_max_payload_size;
+	uint32_t		vs_max_frame_size;
 	uint32_t		vs_frame_interval;
 	SLIST_ENTRY(uvideo_stream) entries;
 };
@@ -1069,16 +1070,20 @@ uvideo_stream_init(struct uvideo_stream *vs,
 	/* Initialize probe and commit data size.  This value is
 	 * dependent on the version of the spec the hardware
 	 * implements. */
-	err = uvideo_stream_probe(vs, UR_GET_LEN, len);
+	err = uvideo_stream_probe(vs, UR_GET_LEN, &len);
 	if (err != USBD_NORMAL_COMPLETION) {
 		DPRINTF(("uvideo_stream_init: "
 			 "error getting probe data len: "
 			 "%s (%d)\n",
 			 usbd_errstr(err), err));
 		vs->vs_probelen = 26; /* conservative v1.0 length */
-	} else {
+	} else if (UGETW(len) <= sizeof(uvideo_probe_and_commit_data_t)) {
 		DPRINTFN(15,("uvideo_stream_init: probelen=%d\n", UGETW(len)));
 		vs->vs_probelen = UGETW(len);
+	} else {
+		DPRINTFN(15,("uvideo_stream_init: device returned invalid probe"
+				" len %d, using default\n", UGETW(len)));
+		vs->vs_probelen = 26;
 	}
 	
 	return USBD_NORMAL_COMPLETION;
