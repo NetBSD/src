66e175b5083d3662f4f8f57905ef716eb9a7aab8
abs <abs@NetBSD.org>
2009-02-13 01:38:32 +0000

    avoid long/unsigned long printf warning on some platforms

--- sys/dev/pci/if_dge.c
+++ sys/dev/pci/if_dge.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: if_dge.c,v 1.22 2008/11/07 00:20:07 dyoung Exp $ */
+/*	$NetBSD: if_dge.c,v 1.23 2009/02/13 01:38:32 abs Exp $ */
 
 /*
  * Copyright (c) 2004, SUNET, Swedish University Computer Network.
@@ -80,7 +80,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: if_dge.c,v 1.22 2008/11/07 00:20:07 dyoung Exp $");
+__KERNEL_RCSID(0, "$NetBSD: if_dge.c,v 1.23 2009/02/13 01:38:32 abs Exp $");
 
 #include "bpfilter.h"
 #include "rnd.h"
@@ -1306,8 +1306,8 @@ dge_start(struct ifnet *ifp)
 			DPRINTF(DGE_DEBUG_TX,
 			    ("%s: TX: desc %d: low 0x%08lx, len 0x%04lx\n",
 			    device_xname(&sc->sc_dev), nexttx,
-			    le32toh(dmamap->dm_segs[seg].ds_addr),
-			    le32toh(dmamap->dm_segs[seg].ds_len)));
+			    (unsigned long)le32toh(dmamap->dm_segs[seg].ds_addr),
+			    (unsigned long)le32toh(dmamap->dm_segs[seg].ds_len)));
 		}
 
 		KASSERT(lasttx != -1);
