cfb43e185ce419bdead3f7c7cba1a92fc62666f9
plunky <plunky@NetBSD.org>
2009-02-14 16:57:05 +0000

    consistency checks made inside #ifdef SAFETY should really be #ifdef DIAGNOSTIC

--- sys/miscfs/genfs/layer_vnops.c
+++ sys/miscfs/genfs/layer_vnops.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: layer_vnops.c,v 1.36 2009/01/03 04:38:07 dholland Exp $	*/
+/*	$NetBSD: layer_vnops.c,v 1.37 2009/02/14 16:57:05 plunky Exp $	*/
 
 /*
  * Copyright (c) 1999 National Aeronautics & Space Administration
@@ -232,7 +232,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: layer_vnops.c,v 1.36 2009/01/03 04:38:07 dholland Exp $");
+__KERNEL_RCSID(0, "$NetBSD: layer_vnops.c,v 1.37 2009/02/14 16:57:05 plunky Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -261,7 +261,6 @@ __KERNEL_RCSID(0, "$NetBSD: layer_vnops.c,v 1.36 2009/01/03 04:38:07 dholland Ex
  *    The 10-Apr-92 version was optimized for speed, throwing away some
  * safety checks.  It should still always work, but it's not as
  * robust to programmer errors.
- *    Define SAFETY to include some error checking code.
  *
  * In general, we map all vnodes going down and unmap them on the way back.
  *
@@ -300,7 +299,7 @@ layer_bypass(v)
 	struct vnodeop_desc *descp = ap->a_desc;
 	int reles, i, flags;
 
-#ifdef SAFETY
+#ifdef DIAGNOSTIC
 	/*
 	 * We require at least one vp.
 	 */
