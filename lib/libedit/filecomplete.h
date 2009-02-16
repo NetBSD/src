7939d24e16e882be35f1abe6c0d2d404efa97e39
christos <christos@NetBSD.org>
2009-02-16 00:15:45 +0000

    fix sign compare issues.

--- lib/libedit/filecomplete.h
+++ lib/libedit/filecomplete.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: filecomplete.h,v 1.7 2009/02/15 21:55:23 christos Exp $	*/
+/*	$NetBSD: filecomplete.h,v 1.8 2009/02/16 00:15:45 christos Exp $	*/
 
 /*-
  * Copyright (c) 1997 The NetBSD Foundation, Inc.
@@ -34,7 +34,7 @@
 int fn_complete(EditLine *,
     char *(*)(const char *, int),
     char **(*)(const char *, int, int),
-    const char *, const char *, const char *(*)(const char *), int,
+    const char *, const char *, const char *(*)(const char *), size_t,
     int *, int *, int *, int *);
 
 void fn_display_match_list(EditLine *, char **, size_t, size_t);
