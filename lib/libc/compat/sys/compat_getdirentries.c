c2dbe506517af39181db77ced365cca51f0782cd
dholland <dholland@NetBSD.org>
2009-02-22 06:33:38 +0000

    Add __warn_references to getdirentries(), per PR 37730. Okayed by Christos a while back.

--- lib/libc/compat/sys/compat_getdirentries.c
+++ lib/libc/compat/sys/compat_getdirentries.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: compat_getdirentries.c,v 1.1 2005/09/13 01:44:09 christos Exp $	*/
+/*	$NetBSD: compat_getdirentries.c,v 1.2 2009/02/22 06:33:38 dholland Exp $	*/
 
 /*
  * Copyright (c) 1997 Frank van der Linden
@@ -34,7 +34,7 @@
 
 #include <sys/cdefs.h>
 #if defined(LIBC_SCCS) && !defined(lint)
-__RCSID("$NetBSD: compat_getdirentries.c,v 1.1 2005/09/13 01:44:09 christos Exp $");
+__RCSID("$NetBSD: compat_getdirentries.c,v 1.2 2009/02/22 06:33:38 dholland Exp $");
 #endif /* LIBC_SCCS and not lint */
 
 #define __LIBC12_SOURCE__
@@ -43,6 +43,9 @@ __RCSID("$NetBSD: compat_getdirentries.c,v 1.1 2005/09/13 01:44:09 christos Exp 
 #include <dirent.h>
 #include <compat/include/dirent.h>
 #include <unistd.h>
+
+__warn_references(getdirentries,
+    "reference to compatibility-only getdirentries(); this will break; use getdents() or readdir() instead")
 
 int
 getdirentries(fd, buf, nbytes, basep)
