04608e7f6db74a8605fefb59e5499e4e95539a64
ahoka <ahoka@NetBSD.org>
2009-02-22 18:05:42 +0000

    Add a missing opt_multiboot.h into sys/arch/i386/i386/machdep.c. Also do the missed rename of multiboot_ksyms_init to multiboot_ksyms_addsyms_elf to compile with MULTIBOOT set.
    This caused a minor and a more serious bug in the past:
    - dmesg did not contain the information about the loader
    - /dev/ksyms did not work when the kernel was booted with a
      multiboot bootloader (grub for example)
    
    Ok by jmcneill, joerg.

--- sys/arch/i386/include/multiboot.h
+++ sys/arch/i386/include/multiboot.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: multiboot.h,v 1.7 2008/10/11 11:06:19 joerg Exp $	*/
+/*	$NetBSD: multiboot.h,v 1.8 2009/02/22 18:05:42 ahoka Exp $	*/
 
 /*-
  * Copyright (c) 2005, 2006 The NetBSD Foundation, Inc.
@@ -197,7 +197,7 @@ struct multiboot_module {
 void		multiboot_pre_reloc(struct multiboot_info *);
 void		multiboot_post_reloc(void);
 void		multiboot_print_info(void);
-bool		multiboot_ksyms_init(void);
+bool		multiboot_ksyms_addsyms_elf(void);
 #endif /* !defined(_LOCORE) */
 
 /* --------------------------------------------------------------------- */
