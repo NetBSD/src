e435731cb33d14aea3ef800c140a9e7b2df5ec9c
snj <snj>
2009-02-24 02:47:30 +0000

    Pull up following revision(s) (requested by dogcow in ticket #473): 	usr.bin/newsyslog/newsyslog.c: revision 1.57 Fix an embarrassing mistake regarding moving down log files. Reported by (and fix included in) PR bin/40635 from Jukka Salmi.

--- usr.bin/newsyslog/newsyslog.c
+++ usr.bin/newsyslog/newsyslog.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: newsyslog.c,v 1.53 2007/12/21 06:46:31 dogcow Exp $	*/
+/*	$NetBSD: newsyslog.c,v 1.53.10.1 2009/02/24 02:47:30 snj Exp $	*/
 
 /*
  * Copyright (c) 1999, 2000 Andrew Doran <ad@NetBSD.org>
@@ -55,7 +55,7 @@
 
 #include <sys/cdefs.h>
 #ifndef lint
-__RCSID("$NetBSD: newsyslog.c,v 1.53 2007/12/21 06:46:31 dogcow Exp $");
+__RCSID("$NetBSD: newsyslog.c,v 1.53.10.1 2009/02/24 02:47:30 snj Exp $");
 #endif /* not lint */
 
 #include <sys/types.h>
@@ -542,9 +542,9 @@ log_trim(struct conf_entry *log)
 	for (i = log->numhist - 1; i > 0; i--) {
 		for (j = 0; j < (int)__arraycount(compress); j++) {
 			snprintf(file1, sizeof(file1), "%s.%d%s", log->logfile,
-			    i - 1, compress[ziptype].suffix);
+			    i - 1, compress[j].suffix);
 			snprintf(file2, sizeof(file2), "%s.%d%s", log->logfile,
-			    i, compress[ziptype].suffix);
+			    i, compress[j].suffix);
 			k = lstat(file1, &st);
 			if (!k) break;
 		}
