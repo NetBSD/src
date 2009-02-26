7ad644e9707a3c92251d20709613ec4fa50bc71a
drochner <drochner@NetBSD.org>
2009-02-26 19:49:15 +0000

    sync TSC on resume (because CPUs were switched off in the meantime), otherwise we get diverging timecounters leading to eg the monotonic clock jump backwards (pullup candidate)

--- sys/arch/x86/acpi/acpi_wakeup.c
+++ sys/arch/x86/acpi/acpi_wakeup.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: acpi_wakeup.c,v 1.11 2008/11/17 23:29:49 joerg Exp $	*/
+/*	$NetBSD: acpi_wakeup.c,v 1.12 2009/02/26 19:49:15 drochner Exp $	*/
 
 /*-
  * Copyright (c) 2002 The NetBSD Foundation, Inc.
@@ -30,7 +30,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: acpi_wakeup.c,v 1.11 2008/11/17 23:29:49 joerg Exp $");
+__KERNEL_RCSID(0, "$NetBSD: acpi_wakeup.c,v 1.12 2009/02/26 19:49:15 drochner Exp $");
 
 /*-
  * Copyright (c) 2001 Takanori Watanabe <takawata@jp.freebsd.org>
@@ -101,6 +101,7 @@ __KERNEL_RCSID(0, "$NetBSD: acpi_wakeup.c,v 1.11 2008/11/17 23:29:49 joerg Exp $
 #include <machine/mtrr.h>
 
 #include <x86/cpuvar.h>
+#include <x86/x86/tsc.h>
 
 #include "acpi_wakecode.h"
 
@@ -307,6 +308,8 @@ acpi_cpu_sleep(struct cpu_info *ci)
 
 	atomic_or_32(&ci->ci_flags, CPUF_RUNNING);
 	atomic_or_32(&cpus_running, ci->ci_cpumask);
+	tsc_sync_ap(ci);
+	tsc_sync_ap(ci);
 
 	x86_enable_intr();
 }
@@ -391,6 +394,9 @@ out:
 
 		while ((ci->ci_flags & CPUF_RUNNING) == 0)
 			x86_pause();
+
+		tsc_sync_bp(ci);
+		tsc_sync_bp(ci);
 	}
 #endif
 
