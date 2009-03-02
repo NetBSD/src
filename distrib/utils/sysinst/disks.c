0c4db46f214891289b4902746c07c44b1b624338
snj <snj>
2009-03-02 19:39:30 +0000

    Pull up following revision(s) (requested by ad in ticket #497): 	distrib/utils/sysinst/disks.c: revision 1.103 Put back /kern, apparently xen uses it (and probably scripts too).

--- distrib/utils/sysinst/disks.c
+++ distrib/utils/sysinst/disks.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: disks.c,v 1.100.2.1 2009/03/02 19:36:31 snj Exp $ */
+/*	$NetBSD: disks.c,v 1.100.2.2 2009/03/02 19:39:30 snj Exp $ */
 
 /*
  * Copyright 1997 Piermont Information Systems Inc.
@@ -530,7 +530,7 @@ make_fstab(void)
 	}
 
 	/* Add /kern, /proc and /dev/pts to fstab and make mountpoint. */
-	scripting_fprintf(f, "kernfs\t\t/kern\tkernfs\trw,noauto\n");
+	scripting_fprintf(f, "kernfs\t\t/kern\tkernfs\trw\n");
 	scripting_fprintf(f, "ptyfs\t\t/dev/pts\tptyfs\trw\n");
 	scripting_fprintf(f, "procfs\t\t/proc\tprocfs\trw\n");
 	scripting_fprintf(f, "/dev/cd0a\t\t/cdrom\tcd9660\tro,noauto\n");
