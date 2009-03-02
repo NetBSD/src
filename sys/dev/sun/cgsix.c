b0071cddae8cc2883682ae87ec7db0fd83fde3b4
snj <snj@NetBSD.org>
2009-03-02 19:56:34 +0000

    Pull up following revision(s) (requested by jdc in ticket #525): 	sys/dev/sun/cgsix.c: revision 1.43 Back out previous and clear the screen directly. Pointed out by macallan@.

--- sys/dev/sun/cgsix.c
+++ sys/dev/sun/cgsix.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: cgsix.c,v 1.38.6.4 2009/03/02 19:55:44 snj Exp $ */
+/*	$NetBSD: cgsix.c,v 1.38.6.5 2009/03/02 19:56:34 snj Exp $ */
 
 /*-
  * Copyright (c) 1998 The NetBSD Foundation, Inc.
@@ -78,7 +78,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: cgsix.c,v 1.38.6.4 2009/03/02 19:55:44 snj Exp $");
+__KERNEL_RCSID(0, "$NetBSD: cgsix.c,v 1.38.6.5 2009/03/02 19:56:34 snj Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -1255,12 +1255,13 @@ cgsix_init_screen(void *cookie, struct vcons_screen *scr,
 	ri->ri_height = sc->sc_height;
 	ri->ri_stride = sc->sc_stride;
 	ri->ri_flg = RI_CENTER;
-	/* Use software for initial screen clear on old revisions */
-	if (sc->sc_fhcrev < 2)
-		ri->ri_flg |= RI_CLEAR;
 
 	ri->ri_bits = sc->sc_fb.fb_pixels;
 	
+	/* We need unaccelerated initial screen clear on old revisions */
+	if (sc->sc_fhcrev < 2)
+		memset(sc->sc_fb.fb_pixels, (*defattr >> 16) & 0xff,
+		    sc->sc_stride * sc->sc_height);
 	rasops_init(ri, sc->sc_height/8, sc->sc_width/8);
 	ri->ri_caps = WSSCREEN_WSCOLORS | WSSCREEN_REVERSE;
 	rasops_reconfig(ri, sc->sc_height / ri->ri_font->fontheight,
