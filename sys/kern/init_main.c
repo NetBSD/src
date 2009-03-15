71907da8394e82907c2faad18ce5096f31eef469
snj <snj>
2009-03-15 19:43:48 +0000

    Pull up following revision(s) (requested by christos in ticket #458): 	sys/conf/Makefile.kern.inc: revision 1.121 	sys/conf/files: revision 1.940 	sys/kern/init_main.c: revision 1.381 	sys/kern/kern_ssp.c: revision 1.1 	sys/kern/subr_autoconf.c: revision 1.168 	sys/sys/device.h: revision 1.116 	sys/sys/systm.h: revision 1.233 Unbreak ssp kernels. The issue here that when the ssp_init() call was deferred, it caused the return from the enclosing function to break, as well as the ssp return on i386. To fix both issues, split configure in two pieces the one before calling ssp_init and the one after, and move the ssp_init() call back in main. Put ssp_init() in its own file, and compile this new file with -fno-stack-protector. Tested on amd64. XXX: If we want to have ssp kernels working on 5.0, this change needs to be pulled up.

--- sys/kern/init_main.c
+++ sys/kern/init_main.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: init_main.c,v 1.371 2008/10/28 15:33:10 tsutsui Exp $	*/
+/*	$NetBSD: init_main.c,v 1.371.2.1 2009/03/15 19:43:48 snj Exp $	*/
 
 /*-
  * Copyright (c) 2008 The NetBSD Foundation, Inc.
@@ -97,7 +97,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: init_main.c,v 1.371 2008/10/28 15:33:10 tsutsui Exp $");
+__KERNEL_RCSID(0, "$NetBSD: init_main.c,v 1.371.2.1 2009/03/15 19:43:48 snj Exp $");
 
 #include "opt_ddb.h"
 #include "opt_ipsec.h"
@@ -252,52 +252,6 @@ int	start_init_exec;		/* semaphore for start_init() */
 static void check_console(struct lwp *l);
 static void start_init(void *);
 void main(void);
-void ssp_init(void);
-
-#if defined(__SSP__) || defined(__SSP_ALL__)
-long __stack_chk_guard[8] = {0, 0, 0, 0, 0, 0, 0, 0};
-void __stack_chk_fail(void);
-
-void
-__stack_chk_fail(void)
-{
-	panic("stack overflow detected; terminated");
-}
-
-void
-ssp_init(void)
-{
-	int s;
-
-#ifdef DIAGNOSTIC
-	printf("Initializing SSP:");
-#endif
-	/*
-	 * We initialize ssp here carefully:
-	 *	1. after we got some entropy
-	 *	2. without calling a function
-	 */
-	size_t i;
-	long guard[__arraycount(__stack_chk_guard)];
-
-	arc4randbytes(guard, sizeof(guard));
-	s = splhigh();
-	for (i = 0; i < __arraycount(guard); i++)
-		__stack_chk_guard[i] = guard[i];
-	splx(s);
-#ifdef DIAGNOSTIC
-	for (i = 0; i < __arraycount(guard); i++)
-		printf("%lx ", guard[i]);
-	printf("\n");
-#endif
-}
-#else
-void
-ssp_init(void)
-{
-
-}
-#endif
 
 void __secmodel_none(void);
 __weak_alias(secmodel_start,__secmodel_none);
@@ -506,6 +460,10 @@ main(void)
 
 	/* Configure the system hardware.  This will enable interrupts. */
 	configure();
+
+	ssp_init();
+
+	configure2();
 
 	ubc_init();		/* must be after autoconfig */
 
