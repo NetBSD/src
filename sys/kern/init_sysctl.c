065fff54cdee627dda531ca5d97bb3daec4157bd
snj <snj>
2009-03-15 20:23:26 +0000

    Pull up following revision(s) (requested by mrg in ticket #566): 	sys/kern/init_sysctl.c: revision 1.157 	sys/kern/kern_descrip.c: revision 1.187 	usr.sbin/pstat/pstat.c: revision 1.112 Don't bother with file_t::f_iflags any more, as it's not used. Noted by mrg@.

--- sys/kern/init_sysctl.c
+++ sys/kern/init_sysctl.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: init_sysctl.c,v 1.149.4.2 2009/03/15 20:20:02 snj Exp $ */
+/*	$NetBSD: init_sysctl.c,v 1.149.4.3 2009/03/15 20:23:26 snj Exp $ */
 
 /*-
  * Copyright (c) 2003, 2007, 2008 The NetBSD Foundation, Inc.
@@ -30,7 +30,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: init_sysctl.c,v 1.149.4.2 2009/03/15 20:20:02 snj Exp $");
+__KERNEL_RCSID(0, "$NetBSD: init_sysctl.c,v 1.149.4.3 2009/03/15 20:23:26 snj Exp $");
 
 #include "opt_sysv.h"
 #include "opt_compat_netbsd32.h"
@@ -2118,7 +2118,7 @@ fill_file(struct kinfo_file *kp, const file_t *fp, const fdfile_t *ff,
 
 	kp->ki_fileaddr =	PTRTOUINT64(fp);
 	kp->ki_flag =		fp->f_flag;
-	kp->ki_iflags =		fp->f_iflags;
+	kp->ki_iflags =		0;
 	kp->ki_ftype =		fp->f_type;
 	kp->ki_count =		fp->f_count;
 	kp->ki_msgcount =	fp->f_msgcount;
