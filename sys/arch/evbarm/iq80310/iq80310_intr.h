cab9000e75dcf35f7edb27ba2bc51efae22eaf28
snj <snj@NetBSD.org>
2009-03-15 19:48:17 +0000

    Pull up following revision(s) (requested by chrost in ticket #458): 	sys/arch/evbarm/iq80310/iq80310_intr.h: revision 1.8 Also include <arm/cpu.h> here for the required definitions of curcpl() and set_curcpl(), so that kern_ssp.c builds here as well.

--- sys/arch/evbarm/iq80310/iq80310_intr.h
+++ sys/arch/evbarm/iq80310/iq80310_intr.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: iq80310_intr.h,v 1.7 2008/04/27 18:58:46 matt Exp $	*/
+/*	$NetBSD: iq80310_intr.h,v 1.7.10.1 2009/03/15 19:48:17 snj Exp $	*/
 
 /*
  * Copyright (c) 2001, 2002 Wasabi Systems, Inc.
@@ -40,6 +40,7 @@
 
 #include "opt_iop310.h"
 
+#include <arm/cpu.h>
 #include <arm/armreg.h>
 #include <arm/cpufunc.h>
 
