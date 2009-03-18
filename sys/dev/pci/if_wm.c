b5ad90c0f210a136b1c217b2f94beddfdf84e776
snj <snj@NetBSD.org>
2009-03-18 05:43:40 +0000

    Pull up following revision(s) (requested by msaitoh in ticket #583): 	sys/dev/pci/if_wm.c: revision 1.166 Fix the multicast hash bug on ICH9's wm. Now we can catch ff02::9 on ICH9's wm.

--- sys/dev/pci/if_wm.c
+++ sys/dev/pci/if_wm.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: if_wm.c,v 1.162.4.1 2008/12/07 19:13:15 bouyer Exp $	*/
+/*	$NetBSD: if_wm.c,v 1.162.4.2 2009/03/18 05:43:40 snj Exp $	*/
 
 /*
  * Copyright (c) 2001, 2002, 2003, 2004 Wasabi Systems, Inc.
@@ -79,7 +79,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: if_wm.c,v 1.162.4.1 2008/12/07 19:13:15 bouyer Exp $");
+__KERNEL_RCSID(0, "$NetBSD: if_wm.c,v 1.162.4.2 2009/03/18 05:43:40 snj Exp $");
 
 #include "bpfilter.h"
 #include "rnd.h"
@@ -3867,7 +3867,7 @@ wm_mchash(struct wm_softc *sc, const uint8_t *enaddr)
 	static const int ich8_hi_shift[4] = { 2, 3, 4, 6 };
 	uint32_t hash;
 
-	if (sc->sc_type == WM_T_ICH8) {
+	if ((sc->sc_type == WM_T_ICH8) || (sc->sc_type == WM_T_ICH9)) {
 		hash = (enaddr[4] >> ich8_lo_shift[sc->sc_mchash_type]) |
 		    (((uint16_t) enaddr[5]) << ich8_hi_shift[sc->sc_mchash_type]);
 		return (hash & 0x3ff);
@@ -3912,7 +3912,7 @@ wm_set_filter(struct wm_softc *sc)
 	 * Set the station address in the first RAL slot, and
 	 * clear the remaining slots.
 	 */
-	if (sc->sc_type == WM_T_ICH8)
+	if ((sc->sc_type == WM_T_ICH8) || (sc->sc_type == WM_T_ICH9))
 		size = WM_ICH8_RAL_TABSIZE;
 	else
 		size = WM_RAL_TABSIZE;
