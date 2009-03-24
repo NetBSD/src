e6481860ab4743d217488b8a209371410135cf90
snj <snj@NetBSD.org>
2009-03-24 20:24:38 +0000

    Pull up following revision(s) (requested by uwe in ticket #591): 	sys/arch/sh3/include/intr.h: revision 1.26 Interrupt handling changes (revision 1.23) bumped down _IPL_N to 8 for some reason.  Restore it to correct 16. HTF did hpcsh work all this time with hd6446x_imask[_IPL_N] indexed with IPLs 0..15 is a mistery...  Discovered by accidentally triggering hd64461 timer interrupt that is not enabled and should have been masked. _IPL_N used to be *also incorrect* 15 before Andy's change, which makes one wonder if dumb luck is severly underappreciated.

--- sys/arch/sh3/include/intr.h
+++ sys/arch/sh3/include/intr.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: intr.h,v 1.25 2008/04/28 20:23:35 martin Exp $	*/
+/*	$NetBSD: intr.h,v 1.25.10.1 2009/03/24 20:24:38 snj Exp $	*/
 
 /*-
  * Copyright (c) 2002 The NetBSD Foundation, Inc.
@@ -38,7 +38,7 @@
 #define	IST_LEVEL		3	/* level-triggered */
 
 /* Interrupt priority levels */
-#define	_IPL_N		8
+#define	_IPL_N		16
 #define	_IPL_NSOFT	4
 
 #define	IPL_NONE	0	/* nothing */
