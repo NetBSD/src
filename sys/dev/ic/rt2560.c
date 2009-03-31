59776356af1102279305fea3912ffc007d1900e8
bouyer <bouyer@NetBSD.org>
2009-03-31 18:08:24 +0000

    Pull up following revision(s) (requested by msaitoh in ticket #1298): 	sys/dev/usb/if_ural.c: revision 1.26 via patch 	sys/dev/usb/if_rum.c: revision 1.17 via patch 	sys/dev/ic/rt2661.c: revision 1.20 via patch 	sys/dev/ic/rt2560.c: revision 1.14 via patch Fix a number of driver which doesn't check wep flag in *_tx_mgt. It is incorrect because we need to encrypt some management frame in case of shared authentification.

--- sys/dev/ic/rt2560.c
+++ sys/dev/ic/rt2560.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: rt2560.c,v 1.7 2006/11/16 01:32:52 christos Exp $	*/
+/*	$NetBSD: rt2560.c,v 1.7.2.1 2009/03/31 18:08:24 bouyer Exp $	*/
 /*	$OpenBSD: rt2560.c,v 1.15 2006/04/20 20:31:12 miod Exp $  */
 /*	$FreeBSD: rt2560.c,v 1.3 2006/03/21 21:15:43 damien Exp $*/
 
@@ -24,7 +24,7 @@
  * http://www.ralinktech.com/
  */
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: rt2560.c,v 1.7 2006/11/16 01:32:52 christos Exp $");
+__KERNEL_RCSID(0, "$NetBSD: rt2560.c,v 1.7.2.1 2009/03/31 18:08:24 bouyer Exp $");
 
 #include "bpfilter.h"
 
@@ -1800,6 +1800,7 @@ rt2560_tx_mgt(struct rt2560_softc *sc, struct mbuf *m0,
 	struct rt2560_tx_desc *desc;
 	struct rt2560_tx_data *data;
 	struct ieee80211_frame *wh;
+	struct ieee80211_key *k;
 	uint16_t dur;
 	uint32_t flags = 0;
 	int rate, error;
@@ -1808,6 +1809,16 @@ rt2560_tx_mgt(struct rt2560_softc *sc, struct mbuf *m0,
 	data = &sc->prioq.data[sc->prioq.cur];
 
 	rate = IEEE80211_IS_CHAN_5GHZ(ni->ni_chan) ? 12 : 2;
+
+	wh = mtod(m0, struct ieee80211_frame *);
+
+	if (wh->i_fc[1] & IEEE80211_FC1_WEP) {
+		k = ieee80211_crypto_encap(ic, ni, m0);
+		if (k == NULL) {
+			m_freem(m0);
+			return ENOBUFS;
+		}
+	}
 
 	error = bus_dmamap_load_mbuf(sc->sc_dmat, data->map, m0,
 	    BUS_DMA_NOWAIT);
--- sys/dev/usb/if_ural.c
+++ sys/dev/usb/if_ural.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: if_ural.c,v 1.18.2.2 2009/03/31 18:04:49 bouyer Exp $ */
+/*	$NetBSD: if_ural.c,v 1.18.2.3 2009/03/31 18:08:24 bouyer Exp $ */
 /*	$FreeBSD: /repoman/r/ncvs/src/sys/dev/usb/if_ural.c,v 1.40 2006/06/02 23:14:40 sam Exp $	*/
 
 /*-
@@ -24,7 +24,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: if_ural.c,v 1.18.2.2 2009/03/31 18:04:49 bouyer Exp $");
+__KERNEL_RCSID(0, "$NetBSD: if_ural.c,v 1.18.2.3 2009/03/31 18:08:24 bouyer Exp $");
 
 #include "bpfilter.h"
 
@@ -1211,6 +1211,7 @@ ural_tx_mgt(struct ural_softc *sc, struct mbuf *m0, struct ieee80211_node *ni)
 	struct ural_tx_desc *desc;
 	struct ural_tx_data *data;
 	struct ieee80211_frame *wh;
+	struct ieee80211_key *k;
 	uint32_t flags = 0;
 	uint16_t dur;
 	usbd_status error;
@@ -1220,6 +1221,16 @@ ural_tx_mgt(struct ural_softc *sc, struct mbuf *m0, struct ieee80211_node *ni)
 	desc = (struct ural_tx_desc *)data->buf;
 
 	rate = IEEE80211_IS_CHAN_5GHZ(ic->ic_curchan) ? 12 : 2;
+
+	wh = mtod(m0, struct ieee80211_frame *);
+
+	if (wh->i_fc[1] & IEEE80211_FC1_WEP) {
+		k = ieee80211_crypto_encap(ic, ni, m0);
+		if (k == NULL) {
+			m_freem(m0);
+			return ENOBUFS;
+		}
+	}
 
 	data->m = m0;
 	data->ni = ni;
