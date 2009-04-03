62428014e97e6162c08365a2714caa4c81651793
snj <snj@NetBSD.org>
2009-04-03 17:59:03 +0000

    Pull up following revision(s) (requested by christos in ticket #650): 	sys/net/route.c: revision 1.117 	sys/net/route.h: revision 1.73 	sys/net/rtsock.c: revision 1.125 	usr.sbin/arp/arp.c: revision 1.48 	usr.sbin/pppd/pppd/sys-bsd.c: revision 1.59 Centralize the ROUNDUP and ADVANCE macro in a header file, give them an RT_ prefix and use them appropriately, instead of making copies. Make pppd use the RT_ROUNDUP macro; fixes proxyarp setting on 64 bit hosts.

--- sys/net/route.h
+++ sys/net/route.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: route.h,v 1.70 2008/03/26 14:54:19 ad Exp $	*/
+/*	$NetBSD: route.h,v 1.70.16.1 2009/04/03 17:59:03 snj Exp $	*/
 
 /*
  * Copyright (c) 1980, 1986, 1993
@@ -239,6 +239,10 @@ struct rt_msghdr {
 #define RTAX_AUTHOR	6	/* sockaddr for author of redirect */
 #define RTAX_BRD	7	/* for NEWADDR, broadcast or p-p dest addr */
 #define RTAX_MAX	8	/* size of array to allocate */
+
+#define RT_ROUNDUP(a) \
+	((a) > 0 ? (1 + (((a) - 1) | (sizeof(long) - 1))) : sizeof(long))
+#define RT_ADVANCE(x, n) (x += RT_ROUNDUP((n)->sa_len))
 
 struct rt_addrinfo {
 	int	rti_addrs;
