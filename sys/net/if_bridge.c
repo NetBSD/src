d0fad19c7404ab2dfd3665be812d015a52524d50
snj <snj@NetBSD.org>
2009-04-04 18:02:21 +0000

    Pull up following revision(s) (requested by bouyer in ticket #660): 	sys/net/if_bridge.c: revision 1.67 Fix a comment, and make it build.

--- sys/net/if_bridge.c
+++ sys/net/if_bridge.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: if_bridge.c,v 1.62.6.2 2009/04/04 18:01:25 snj Exp $	*/
+/*	$NetBSD: if_bridge.c,v 1.62.6.3 2009/04/04 18:02:21 snj Exp $	*/
 
 /*
  * Copyright 2001 Wasabi Systems, Inc.
@@ -80,7 +80,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: if_bridge.c,v 1.62.6.2 2009/04/04 18:01:25 snj Exp $");
+__KERNEL_RCSID(0, "$NetBSD: if_bridge.c,v 1.62.6.3 2009/04/04 18:02:21 snj Exp $");
 
 #include "opt_bridge_ipf.h"
 #include "opt_inet.h"
@@ -92,6 +92,7 @@ __KERNEL_RCSID(0, "$NetBSD: if_bridge.c,v 1.62.6.2 2009/04/04 18:01:25 snj Exp $
 #include <sys/mbuf.h>
 #include <sys/queue.h>
 #include <sys/socket.h>
+#include </sys/socketvar.h> /* for softnet_lock */
 #include <sys/sockio.h>
 #include <sys/systm.h>
 #include <sys/proc.h>
@@ -1453,7 +1454,7 @@ bridge_forward(void *v)
  *
  *	Receive input from a member interface.  Queue the packet for
  *	bridging if it is not for us.
- *	should be called at splbio()
+ *	should be called at splnet()
  */
 struct mbuf *
 bridge_input(struct ifnet *ifp, struct mbuf *m)
