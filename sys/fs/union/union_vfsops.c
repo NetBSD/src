48cde93b825d7aea5c8731fe9958e775c6baa978
snj <snj@NetBSD.org>
2009-04-04 18:12:55 +0000

    Pull up following revision(s) (requested by ad in ticket #663): 	sys/fs/union/union_vfsops.c: revision 1.59 Turn up the volume on the warning message a bit and note that unionfs can corrupt the underlying file system. This is an old problem but is now much easier to trigger because VFS has gone fully multithreaded.

--- sys/fs/union/union_vfsops.c
+++ sys/fs/union/union_vfsops.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: union_vfsops.c,v 1.57 2008/06/28 01:34:05 rumble Exp $	*/
+/*	$NetBSD: union_vfsops.c,v 1.57.6.1 2009/04/04 18:12:55 snj Exp $	*/
 
 /*
  * Copyright (c) 1994 The Regents of the University of California.
@@ -77,7 +77,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: union_vfsops.c,v 1.57 2008/06/28 01:34:05 rumble Exp $");
+__KERNEL_RCSID(0, "$NetBSD: union_vfsops.c,v 1.57.6.1 2009/04/04 18:12:55 snj Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -149,8 +149,8 @@ union_mount(struct mount *mp, const char *path, void *data, size_t *data_len)
 		goto bad;
 	}
 
-	printf("WARNING: the union file system is experimental and "
-	    "may be unstable\n");
+	printf("WARNING: the union file system is experimental\n"
+	    "WARNING: it can cause crashes and file system corruption\n");
 
 	lowerrootvp = mp->mnt_vnodecovered;
 	VREF(lowerrootvp);
