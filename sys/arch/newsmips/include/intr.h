9efa36bc271c75c9a606c5f603a65a9ebeffb79b
tsutsui <tsutsui@NetBSD.org>
2009-04-10 13:29:30 +0000

    Include <sys/evcnt.h> instead of <sys/device.h> for struct evcnt to avoid recursive dependency.

--- sys/arch/newsmips/include/intr.h
+++ sys/arch/newsmips/include/intr.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: intr.h,v 1.23 2008/04/28 20:23:30 martin Exp $	*/
+/*	$NetBSD: intr.h,v 1.24 2009/04/10 13:29:30 tsutsui Exp $	*/
 
 /*-
  * Copyright (c) 2000, 2001 The NetBSD Foundation, Inc.
@@ -52,7 +52,7 @@
 #ifdef _KERNEL
 #ifndef _LOCORE
 
-#include <sys/device.h>
+#include <sys/evcnt.h>
 #include <mips/locore.h>
 
 extern const uint32_t ipl_sr_bits[_IPL_N];
