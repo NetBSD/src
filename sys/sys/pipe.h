f0be5a13d734f6620ba6b6ed26d3842cc5c0328b
christos <christos@NetBSD.org>
2009-04-11 15:46:18 +0000

    rename ctime to btime for consistency.

--- sys/sys/pipe.h
+++ sys/sys/pipe.h
@@ -1,4 +1,4 @@
-/* $NetBSD: pipe.h,v 1.26 2009/04/11 14:42:28 christos Exp $ */
+/* $NetBSD: pipe.h,v 1.27 2009/04/11 15:46:18 christos Exp $ */
 
 /*
  * Copyright (c) 1996 John S. Dyson
@@ -111,7 +111,7 @@ struct pipe {
 	struct	selinfo pipe_sel;	/* for compat with select */
 	struct	timespec pipe_atime;	/* time of last access */
 	struct	timespec pipe_mtime;	/* time of last modify */
-	struct	timespec pipe_ctime;	/* time of status change */
+	struct	timespec pipe_btime;	/* time of creation */
 	pid_t	pipe_pgid;		/* process group for sigio */
 	struct	pipe *pipe_peer;	/* link with other direction */
 	u_int	pipe_state;		/* pipe status info */
