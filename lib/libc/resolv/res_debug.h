bd9758e11a7eaf14d27c9b5c8709ae5328cd5ccc
christos <christos@NetBSD.org>
2009-04-12 16:35:44 +0000

    libc portion of libbind from external/bsd/libbind

--- lib/libc/resolv/res_debug.h
+++ lib/libc/resolv/res_debug.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: res_debug.h,v 1.1.1.3 2007/03/30 20:16:22 ghen Exp $	*/
+/*	$NetBSD: res_debug.h,v 1.1.1.4 2009/04/12 16:35:47 christos Exp $	*/
 
 /*
  * Copyright (c) 2004 by Internet Systems Consortium, Inc. ("ISC")
