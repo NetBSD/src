b5677b36047b601b9addaaa494a58ceae82c2a6c
christos <christos@NetBSD.org>
2009-04-12 15:32:58 +0000

    from ftp:///ftp.isc.org/isc/libbind import 6.0rc1.

---
+++ external/bsd/libbind/dist/port/hpux/include/paths.h
@@ -0,0 +1,4 @@
+/*	$NetBSD: paths.h,v 1.1.1.1 2009/04/12 15:33:55 christos Exp $	*/
+
+#define _PATH_DEVNULL "/dev/null"
+
---
+++ external/bsd/libbind/dist/port/mpe/include/sys/param.h
@@ -0,0 +1,6 @@
+/*	$NetBSD: param.h,v 1.1.1.1 2009/04/12 15:33:52 christos Exp $	*/
+
+/* Omitted from MPE. */
+
+#define MAXHOSTNAMELEN	64
+#define MAXPATHLEN	1024
