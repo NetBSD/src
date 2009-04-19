bfb0571b4eeccd421be35b15e41bdd2b333a620e
tsutsui <tsutsui>
2009-04-19 18:08:56 +0000

    Fix harmless merge botch.

--- sys/dist/ipf/netinet/ip_fil_netbsd.c
+++ sys/dist/ipf/netinet/ip_fil_netbsd.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: ip_fil_netbsd.c,v 1.48 2009/02/12 12:12:39 bouyer Exp $	*/
+/*	$NetBSD: ip_fil_netbsd.c,v 1.49 2009/04/19 18:08:56 tsutsui Exp $	*/
 
 /*
  * Copyright (C) 1993-2003 by Darren Reed.
@@ -8,7 +8,7 @@
 #if !defined(lint)
 #if defined(__NetBSD__)
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: ip_fil_netbsd.c,v 1.48 2009/02/12 12:12:39 bouyer Exp $");
+__KERNEL_RCSID(0, "$NetBSD: ip_fil_netbsd.c,v 1.49 2009/04/19 18:08:56 tsutsui Exp $");
 #else
 static const char sccsid[] = "@(#)ip_fil.c	2.41 6/5/96 (C) 1993-2000 Darren Reed";
 static const char rcsid[] = "@(#)Id: ip_fil_netbsd.c,v 2.55.2.59 2008/03/01 23:16:38 darrenr Exp";
@@ -48,10 +48,6 @@ static const char rcsid[] = "@(#)Id: ip_fil_netbsd.c,v 2.55.2.59 2008/03/01 23:1
 #include <sys/poll.h>
 #if (__NetBSD_Version__ >= 399002000)
 # include <sys/kauth.h>
-#endif
-
-#if (__NetBSD_Version__ >= 399002000)
-#include <sys/kauth.h>
 #endif
 
 #include <net/if.h>
