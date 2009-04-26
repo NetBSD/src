82810d9eedacbf762b7349ea07cb3e278e2f410f
snj <snj@NetBSD.org>
2009-04-26 01:39:54 +0000

    Welcome to NetBSD 5.0!

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.330.4.8 2009/04/14 17:24:21 snj Exp $	*/
+/*	$NetBSD: param.h,v 1.330.4.9 2009/04/26 01:39:55 snj Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -63,7 +63,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	500000003	/* NetBSD 5.0_RC4 */
+#define	__NetBSD_Version__	500000003	/* NetBSD 5.0 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
