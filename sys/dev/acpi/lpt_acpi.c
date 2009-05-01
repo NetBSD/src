390d70636d76a5ffe3a92d806285774986cb23bb
snj <snj@NetBSD.org>
2009-05-01 01:38:16 +0000

    Pull up following revision(s) (requested by jmcneill in ticket #498): 	sys/dev/acpi/acpi_ec.c: revision 1.53 	sys/dev/acpi/acpi_resource.c: revision 1.27 	sys/dev/acpi/atppc_acpi.c: revision 1.16 	sys/dev/acpi/attimer_acpi.c: revision 1.12 	sys/dev/acpi/com_acpi.c: revision 1.28 	sys/dev/acpi/fdc_acpi.c: revision 1.35 	sys/dev/acpi/hpet_acpi.c: revision 1.4 	sys/dev/acpi/joy_acpi.c: revision 1.9 	sys/dev/acpi/lpt_acpi.c: revision 1.18 	sys/dev/acpi/mpu_acpi.c: revision 1.9 	sys/dev/acpi/pckbc_acpi.c: revision 1.31 	sys/dev/acpi/pcppi_acpi.c: revision 1.11 	sys/dev/acpi/ug_acpi.c: revision 1.5 	sys/dev/acpi/wss_acpi.c: revision 1.20 	sys/dev/acpi/ym_acpi.c: revision 1.6 Cut the amount of spam from acpi devices in roughly half by printing resources on the same line as autoconf messages. Things like:   pcppi1 at acpi0 (SPKR, PNP0800)   pcppi1: io 0x61 become:   pcppi1 at acpi0 (SPKR, PNP0800): io 0x61

--- sys/dev/acpi/lpt_acpi.c
+++ sys/dev/acpi/lpt_acpi.c
@@ -1,4 +1,4 @@
-/* $NetBSD: lpt_acpi.c,v 1.17 2008/05/31 14:07:03 jmcneill Exp $ */
+/* $NetBSD: lpt_acpi.c,v 1.17.8.1 2009/05/01 01:38:16 snj Exp $ */
 
 /*
  * Copyright (c) 2002 Jared D. McNeill <jmcneill@invisible.ca>
@@ -26,7 +26,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: lpt_acpi.c,v 1.17 2008/05/31 14:07:03 jmcneill Exp $");
+__KERNEL_RCSID(0, "$NetBSD: lpt_acpi.c,v 1.17.8.1 2009/05/01 01:38:16 snj Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -94,9 +94,6 @@ lpt_acpi_attach(device_t parent, device_t self, void *aux)
 	struct acpi_io *io;
 	struct acpi_irq *irq;
 	ACPI_STATUS rv;
-
-	aprint_naive("\n");
-	aprint_normal("\n");
 
 	sc->sc_dev = self;
 
