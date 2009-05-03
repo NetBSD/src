9dfb329ca395f7248faf1d1502be20fcbc09b7ac
roy <roy@NetBSD.org>
2009-05-03 06:48:58 +0000

    Import dhcpcd-5.0.1
    Changes from dhcpcd-5.0.0
     * Silence compile warnings on Sparc64
     * Fix existing address detection

--- external/bsd/dhcpcd/dist/config.h
+++ external/bsd/dhcpcd/dist/config.h
@@ -1,6 +1,6 @@
 /*
  * dhcpcd - DHCP client daemon
- * Copyright 2006-2009 Roy Marples <roy@marples.name>
+ * Copyright (c) 2006-2009 Roy Marples <roy@marples.name>
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -28,7 +28,7 @@
 #define CONFIG_H
 
 #define PACKAGE			"dhcpcd"
-#define VERSION			"5.0.0"
+#define VERSION			"5.0.1"
 
 /* Some systems do not have a working fork. */
 /* #define THERE_IS_NO_FORK */
