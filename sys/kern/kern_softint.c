dd1f21c83998bbfac92cdc06874cb40688687efc
bouyer <bouyer@NetBSD.org>
2009-05-05 20:26:36 +0000

    Declare sh_flags volatile. Without it, on ports where splhigh() is inline, the compiler will optimise the second SOFTINT_PENDING test in softint_schedule(). A dissasembly of softint_schedule() with and without the volatile sh_flags confirm this on sparc. Because of this there is a race that could lead to the softhand_t being enqueued twice on si_q, leading to a corrupted queue and some handler being SOFTINT_PENDING but never called.
    Should fix PR kern/38637

--- sys/kern/kern_softint.c
+++ sys/kern/kern_softint.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: kern_softint.c,v 1.26 2009/04/06 21:22:47 dyoung Exp $	*/
+/*	$NetBSD: kern_softint.c,v 1.27 2009/05/05 20:26:36 bouyer Exp $	*/
 
 /*-
  * Copyright (c) 2007, 2008 The NetBSD Foundation, Inc.
@@ -176,7 +176,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: kern_softint.c,v 1.26 2009/04/06 21:22:47 dyoung Exp $");
+__KERNEL_RCSID(0, "$NetBSD: kern_softint.c,v 1.27 2009/05/05 20:26:36 bouyer Exp $");
 
 #include <sys/param.h>
 #include <sys/malloc.h>
@@ -210,7 +210,7 @@ typedef struct softhand {
 	void			(*sh_func)(void *);
 	void			*sh_arg;
 	softint_t		*sh_isr;
-	u_int			sh_flags;
+	volatile u_int		sh_flags;
 } softhand_t;
 
 typedef struct softcpu {
