be7ce357613dfc97064d97d27a5a51058fc9a5e5
bouyer <bouyer@NetBSD.org>
2009-05-05 18:17:57 +0000

    Pull up following revision(s) (requested by snj in ticket #737): 	sys/arch/x86/pci/pchb.c: revisions 1.15 - 1.17 	sys/dev/pci/agp.c: revisions 1.63 - 1.65 	sys/dev/pci/agp_i810.c: revisions 1.57 - 1.64 	sys/dev/pci/pcidevs: revisions 1.965, 1.967 via patch 	sys/dev/pci/agpreg.h: revision 1.20 Add AGP support for Intel G35, G45, and Q45.

--- sys/dev/pci/agp.c
+++ sys/dev/pci/agp.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: agp.c,v 1.62 2008/10/17 17:15:09 christos Exp $	*/
+/*	$NetBSD: agp.c,v 1.62.4.1 2009/05/05 18:17:57 bouyer Exp $	*/
 
 /*-
  * Copyright (c) 2000 Doug Rabson
@@ -65,7 +65,7 @@
 
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: agp.c,v 1.62 2008/10/17 17:15:09 christos Exp $");
+__KERNEL_RCSID(0, "$NetBSD: agp.c,v 1.62.4.1 2009/05/05 18:17:57 bouyer Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -180,8 +180,18 @@ const struct agp_product {
 	  NULL,			agp_i810_attach },
 	{ PCI_VENDOR_INTEL,	PCI_PRODUCT_INTEL_82Q33_HB,
 	  NULL,			agp_i810_attach },
+	{ PCI_VENDOR_INTEL,	PCI_PRODUCT_INTEL_82G35_HB,
+	  NULL,			agp_i810_attach },
 	{ PCI_VENDOR_INTEL,	PCI_PRODUCT_INTEL_82946GZ_HB,
 	  NULL,			agp_i810_attach },
+	{ PCI_VENDOR_INTEL,	PCI_PRODUCT_INTEL_82GM45_HB,
+	  NULL, 		agp_i810_attach },
+	{ PCI_VENDOR_INTEL,	PCI_PRODUCT_INTEL_82IGD_E_HB,
+	  NULL, 		agp_i810_attach },
+	{ PCI_VENDOR_INTEL,	PCI_PRODUCT_INTEL_82Q45_HB,
+	  NULL, 		agp_i810_attach },
+	{ PCI_VENDOR_INTEL,	PCI_PRODUCT_INTEL_82G45_HB,
+	  NULL, 		agp_i810_attach },
 #endif
 
 #if NAGP_INTEL > 0
