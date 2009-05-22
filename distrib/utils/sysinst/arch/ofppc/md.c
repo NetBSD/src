c36eaad614ca2a747e77c661e2716ab57c4c9836
snj <snj>
2009-05-22 21:47:56 +0000

    Pull up following revision(s) (requested by sborrill in ticket #773): 	distrib/utils/sysinst/arch/arc/md.c: revision 1.32 	distrib/utils/sysinst/arch/bebox/md.c: revision 1.52 	distrib/utils/sysinst/arch/cobalt/md.c: revision 1.3 	distrib/utils/sysinst/arch/evbmips/md.c: revision 1.5 	distrib/utils/sysinst/arch/evbppc/md.c: revision 1.28 	distrib/utils/sysinst/arch/hpcarm/md.c: revision 1.6 	distrib/utils/sysinst/arch/hpcmips/md.c: revision 1.44 	distrib/utils/sysinst/arch/landisk/md.c: revision 1.6 	distrib/utils/sysinst/arch/ofppc/md.c: revision 1.5 	distrib/utils/sysinst/arch/playstation2/md.c: revision 1.23 	distrib/utils/sysinst/arch/prep/md.c: revision 1.30 	distrib/utils/sysinst/arch/sandpoint/md.c: revision 1.29 	distrib/utils/sysinst/arch/zaurus/md.c: revision 1.4 Support drives over 1TB in size (i.e. stop errors like drive is -654343MB in size). Error if drive is over 2TB in size (and thus over the disklabel limit).

--- distrib/utils/sysinst/arch/ofppc/md.c
+++ distrib/utils/sysinst/arch/ofppc/md.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: md.c,v 1.4 2008/10/07 09:58:15 abs Exp $	*/
+/*	$NetBSD: md.c,v 1.4.2.1 2009/05/22 21:47:58 snj Exp $	*/
 
 /*
  * Copyright 1997 Piermont Information Systems Inc.
@@ -493,7 +493,8 @@ md_pre_update(void)
 int
 md_bios_info(char *dev)
 {
-	int cyl, head, sec;
+	int cyl, head;
+	daddr_t sec;
 
 	msg_display(MSG_nobiosgeom, dlcyl, dlhead, dlsec);
 	if (guess_biosgeom_from_mbr(&mbr, &cyl, &head, &sec) >= 0)
