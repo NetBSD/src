4b80de85f24d4b067aa744e1fc954175f342145e
skrll <skrll@NetBSD.org>
2009-05-24 06:53:34 +0000

    u_intNN_t -> uintNN_t
    "same" code before and after.

--- sys/arch/hppa/include/reg.h
+++ sys/arch/hppa/include/reg.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: reg.h,v 1.8 2009/05/16 12:57:05 skrll Exp $	*/
+/*	$NetBSD: reg.h,v 1.9 2009/05/24 06:53:35 skrll Exp $	*/
 
 /*	$OpenBSD: reg.h,v 1.7 2000/06/15 17:00:37 mickey Exp $	*/
 
@@ -187,30 +187,30 @@
 #ifndef __ASSEMBLER__
 
 struct reg {
-	u_int32_t r_regs[HPPA_NREGS];	/* r0 is psw */
+	uint32_t r_regs[HPPA_NREGS];	/* r0 is psw */
 
-	u_int32_t r_sar;
+	uint32_t r_sar;
 
-	u_int32_t r_pcsqh;
-	u_int32_t r_pcsqt;
-	u_int32_t r_pcoqh;
-	u_int32_t r_pcoqt;
+	uint32_t r_pcsqh;
+	uint32_t r_pcsqt;
+	uint32_t r_pcoqh;
+	uint32_t r_pcoqt;
 	
-	u_int32_t r_sr0;
-	u_int32_t r_sr1;
-	u_int32_t r_sr2;
-	u_int32_t r_sr3;
-	u_int32_t r_sr4;
-	u_int32_t r_sr5;	/* !mcontext */
-	u_int32_t r_sr6;	/* !mcontext */
-	u_int32_t r_sr7;	/* !mcontext */
+	uint32_t r_sr0;
+	uint32_t r_sr1;
+	uint32_t r_sr2;
+	uint32_t r_sr3;
+	uint32_t r_sr4;
+	uint32_t r_sr5;	/* !mcontext */
+	uint32_t r_sr6;	/* !mcontext */
+	uint32_t r_sr7;	/* !mcontext */
 
-	u_int32_t r_cr26;
-	u_int32_t r_cr27;
+	uint32_t r_cr26;
+	uint32_t r_cr27;
 };
 
 struct fpreg {
-	u_int64_t fpr_regs[HPPA_NFPREGS];
+	uint64_t fpr_regs[HPPA_NFPREGS];
 };
 #endif /* !__ASSEMBLER__ */
 
