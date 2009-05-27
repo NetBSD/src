d93337a8b7ad96c3607921a526f06bf73c1e3467
dholland <dholland@NetBSD.org>
2009-05-27 17:44:38 +0000

    Rearrange where the alternate definition of __dead goes. It can't be before "include.h" as that includes a bunch of system headers, but it can't be after either as it also includes a bunch of local headers. Therefore, it needs to be *in* include.h.

--- games/phantasia/include.h
+++ games/phantasia/include.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: include.h,v 1.5 2004/04/07 17:46:47 ross Exp $	*/
+/*	$NetBSD: include.h,v 1.6 2009/05/27 17:44:38 dholland Exp $	*/
 
 /*
  * include.h - includes all important files for Phantasia
@@ -14,6 +14,10 @@
 #include <string.h>
 #include <time.h>
 #include <unistd.h>
+
+#ifndef __dead /* Not NetBSD */
+#define __dead
+#endif
 
 #include "macros.h"
 #include "phantdefs.h"
