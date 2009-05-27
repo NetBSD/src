109f69e8852035be9e87aa4b82c730986a340bb6
pooka <pooka@NetBSD.org>
2009-05-27 17:46:49 +0000

    Make it possible to register delayed radix tree head inits which will be processed when the radix "subsystem" is initialized -- all users must be attached before any inits to know the max keylength. Use of link sets is no longer required, and only attached domains need to be considered.

--- sys/netinet/ip_encap.c
+++ sys/netinet/ip_encap.c
@@ -74,7 +74,7 @@
 #define USE_RADIX
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: ip_encap.c,v 1.37 2009/04/18 14:58:05 tsutsui Exp $");
+__KERNEL_RCSID(0, "$NetBSD: ip_encap.c,v 1.38 2009/05/27 17:46:49 pooka Exp $");
 
 #include "opt_mrouting.h"
 #include "opt_inet.h"
@@ -162,13 +162,13 @@ encap_init(void)
 
 #ifdef USE_RADIX
 	/*
-	 * initialize radix lookup table.
-	 * max_keylen initialization happen in the rn_init().
+	 * initialize radix lookup table when the radix subsystem is inited.
 	 */
-	rn_init();
-	rn_inithead((void *)&encap_head[0], sizeof(struct sockaddr_pack) << 3);
+	rn_delayedinit((void *)&encap_head[0],
+	    sizeof(struct sockaddr_pack) << 3);
 #ifdef INET6
-	rn_inithead((void *)&encap_head[1], sizeof(struct sockaddr_pack) << 3);
+	rn_delayedinit((void *)&encap_head[1],
+	    sizeof(struct sockaddr_pack) << 3);
 #endif
 #endif
 }
