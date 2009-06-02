5c40b0bf0cee1bf2c5a1a6f2f7ec3e35b7cf634b
njoly <njoly@NetBSD.org>
2009-06-02 13:00:23 +0000

    Make nice(2) return EPERM on error, not EACCES (from sys_setpriority).

--- sys/compat/linux32/common/linux32_unistd.c
+++ sys/compat/linux32/common/linux32_unistd.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: linux32_unistd.c,v 1.32 2009/03/29 19:21:19 christos Exp $ */
+/*	$NetBSD: linux32_unistd.c,v 1.33 2009/06/02 13:00:24 njoly Exp $ */
 
 /*-
  * Copyright (c) 2006 Emmanuel Dreyfus, all rights reserved.
@@ -33,7 +33,7 @@
 
 #include <sys/cdefs.h>
 
-__KERNEL_RCSID(0, "$NetBSD: linux32_unistd.c,v 1.32 2009/03/29 19:21:19 christos Exp $");
+__KERNEL_RCSID(0, "$NetBSD: linux32_unistd.c,v 1.33 2009/06/02 13:00:24 njoly Exp $");
 
 #include <sys/types.h>
 #include <sys/param.h>
@@ -443,12 +443,14 @@ linux32_sys_nice(struct lwp *l, const struct linux32_sys_nice_args *uap, registe
 	} */
 	struct proc *p = l->l_proc;
 	struct sys_setpriority_args bsa;
+	int error;
 
 	SCARG(&bsa, which) = PRIO_PROCESS;
 	SCARG(&bsa, who) = 0;
 	SCARG(&bsa, prio) = p->p_nice - NZERO + SCARG(uap, incr);
 
-	return sys_setpriority(l, &bsa, retval);
+	error = sys_setpriority(l, &bsa, retval);
+	return (error) ? EPERM : 0;
 }
 
 int
