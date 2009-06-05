b3b9619529f3b1add7b21dac40a437046c957876
snj <snj>
2009-06-05 18:28:34 +0000

    Pull up following revision(s) (requested by martin in ticket #786): 	sys/arch/sparc64/include/cpu.h: revision 1.86 	sys/arch/sparc64/sparc64/locore.s: revision 1.291 	sys/arch/sparc64/sparc64/vm_machdep.c: revision 1.89 cpu_setfunc() can not use lwp_trampoline, as that has additional lwp startup semantics. Use a simpler setfunc_trampoline instead.

--- sys/arch/sparc64/sparc64/locore.s
+++ sys/arch/sparc64/sparc64/locore.s
@@ -1,4 +1,4 @@
-/*	$NetBSD: locore.s,v 1.286 2008/10/30 12:29:45 nakayama Exp $	*/
+/*	$NetBSD: locore.s,v 1.286.2.1 2009/06/05 18:28:34 snj Exp $	*/
 
 /*
  * Copyright (c) 1996-2002 Eduardo Horvath
@@ -6811,6 +6811,16 @@ ENTRY(lwp_trampoline)
 	 * Here we finish up as in syscall, but simplified.
 	 */
 	CHKPT(%o3,%o4,0x35)
+	ba,a,pt	%icc, return_from_trap
+	 nop
+
+	/*
+	 * Like lwp_trampoline, but for cpu_setfunc(), i.e. without newlwp
+	 * arguement and will not call lwp_startup.
+	 */
+ENTRY(setfunc_trampoline)
+	call	%l0			! re-use current frame
+	 mov	%l1, %o0
 	ba,a,pt	%icc, return_from_trap
 	 nop
 
