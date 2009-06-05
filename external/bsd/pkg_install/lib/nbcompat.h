a295617628abd7617e52496a0f763351678da4ea
snj <snj@NetBSD.org>
2009-06-05 17:01:43 +0000

    Apply patch (requested by joerg in ticket #1322): Pull up pkg_install-20090528, libarchive-2.5.5 and libfetch-2.23 with the associated build infrastructure as of 20090529 from HEAD.

---
+++ external/bsd/pkg_install/lib/nbcompat.h
@@ -0,0 +1,8 @@
+/* $NetBSD: nbcompat.h,v 1.1.12.2 2009/06/05 17:02:01 snj Exp $ */
+
+/*
+ * In pkgsrc, libnbcompat is used to provide compatibility functions.
+ * This is not needed on native NetBSD, so provide an empty header.
+ */
+
+#include <sys/statvfs.h>
