12317bb39e143bf746937622b1e7ae490fb5fa44
taca <taca@NetBSD.org>
2009-06-07 06:11:18 +0000

    Make ip_carp.c compile, fixing usage of CARP_LOG().

--- sys/netinet/ip_carp.c
+++ sys/netinet/ip_carp.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: ip_carp.c,v 1.37 2009/05/27 21:32:31 christos Exp $	*/
+/*	$NetBSD: ip_carp.c,v 1.38 2009/06/07 06:11:18 taca Exp $	*/
 /*	$OpenBSD: ip_carp.c,v 1.113 2005/11/04 08:11:54 mcbride Exp $	*/
 
 /*
@@ -28,7 +28,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: ip_carp.c,v 1.37 2009/05/27 21:32:31 christos Exp $");
+__KERNEL_RCSID(0, "$NetBSD: ip_carp.c,v 1.38 2009/06/07 06:11:18 taca Exp $");
 
 /*
  * TODO:
@@ -697,7 +697,7 @@ carp_proto_input_c(struct mbuf *m, struct carp_header *ch, sa_family_t af)
 		if (timercmp(&sc_tv, &ch_tv, >) ||
 		    timercmp(&sc_tv, &ch_tv, ==)) {
 			callout_stop(&sc->sc_ad_tmo);
-			CARP_LOG(sc, "MASTER -> BACKUP (more frequent advertisement received)");
+			CARP_LOG(sc, ("MASTER -> BACKUP (more frequent advertisement received)"));
 			carp_set_state(sc, BACKUP);
 			carp_setrun(sc, 0);
 			carp_setroute(sc, RTM_DELETE);
@@ -709,7 +709,7 @@ carp_proto_input_c(struct mbuf *m, struct carp_header *ch, sa_family_t af)
 		 * and this one claims to be slower, treat him as down.
 		 */
 		if (carp_opts[CARPCTL_PREEMPT] && timercmp(&sc_tv, &ch_tv, <)) {
-			CARP_LOG(sc, "BACKUP -> MASTER (preempting a slower master)");
+			CARP_LOG(sc, ("BACKUP -> MASTER (preempting a slower master)"));
 			carp_master_down(sc);
 			break;
 		}
@@ -721,7 +721,7 @@ carp_proto_input_c(struct mbuf *m, struct carp_header *ch, sa_family_t af)
 		 */
 		sc_tv.tv_sec = sc->sc_advbase * 3;
 		if (timercmp(&sc_tv, &ch_tv, <)) {
-			CARP_LOG(sc, "BACKUP -> MASTER (master timed out)")
+			CARP_LOG(sc, ("BACKUP -> MASTER (master timed out)"));
 			carp_master_down(sc);
 			break;
 		}
@@ -1391,7 +1391,7 @@ carp_master_down(void *v)
 	case MASTER:
 		break;
 	case BACKUP:
-		CARP_LOG(sc, "INIT -> MASTER (preempting)");
+		CARP_LOG(sc, ("INIT -> MASTER (preempting)"));
 		carp_set_state(sc, MASTER);
 		carp_send_ad(sc);
 		carp_send_arp(sc);
