55848dcf364b267aec29f89b2aa465ee9a914bb9
yamt <yamt>
2009-06-14 21:37:06 +0000

    bump the kernel version to 5.99.14 for the vm_anon change.

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.347 2009/06/01 20:08:44 pgoyette Exp $	*/
+/*	$NetBSD: param.h,v 1.348 2009/06/14 21:37:06 yamt Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -63,7 +63,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	599001300	/* NetBSD 5.99.13 */
+#define	__NetBSD_Version__	599001400	/* NetBSD 5.99.14 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
