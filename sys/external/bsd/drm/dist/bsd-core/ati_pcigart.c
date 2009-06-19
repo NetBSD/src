fdfb1affde279e7ea99494ec8fd83d4bef747c97
mrg <mrg>
2009-06-19 03:50:03 +0000

    merge the changes from drm 85b9f737db0d2a845e4d7e2bbf9ad12ff9e2227c with our changes and the work recently done by Arto Huusko <arto.huusko@pp2.inet.fi> and FUKAUMI Naoki <fun@naobsd.org>.
    it includes all the changes arto provided from both mesa-drm and
    the r6xx-r7xx-support branch.  it does not yet include code to
    handle the (deleted) drm_pciids.h file, but i'll probably just
    check in a generated one for now.
    
    i have not yet merged the changes from outside this dir.
    
    
    from arto's messages to tech-x11:
    
    The important change that was needed is that drm_scatter.c was
    fixed to return pointer to all allocated pages, not just the
    beginning of the allocated segments.
    
    Other changes:
    
      - drm_scatter maps COHERENT memory
      - drm_drawable: drawable handle allocation is done
        inside lock
      - drm_memory: when mapping "agp" memory, store offset
        of mapped area, so that new requests to same offset
        return the same area instead of trying to remap
        and fail
      - drm_vm: use bus_space_mmap for frame buffer and registers
    
      - r600_cp.c: ioremapfree allocated gart range
      - radeon_cp.c: use mtsleep
      - some memset calls I had added had their args swapped,
        and no memory was cleared

--- sys/external/bsd/drm/dist/bsd-core/ati_pcigart.c
+++ sys/external/bsd/drm/dist/bsd-core/ati_pcigart.c
@@ -39,26 +39,169 @@
 #define ATI_PCIE_WRITE 0x4
 #define ATI_PCIE_READ 0x8
 
-static int drm_ati_alloc_pcigart_table(struct drm_device *dev,
-				       struct drm_ati_pcigart_info *gart_info)
+#if defined(__FreeBSD__)
+static void
+drm_ati_alloc_pcigart_table_cb(void *arg, bus_dma_segment_t *segs,
+			       int nsegs, int error)
 {
-	dev->sg->dmah = drm_pci_alloc(dev, gart_info->table_size,
-						PAGE_SIZE,
-						gart_info->table_mask);
-	if (dev->sg->dmah == NULL)
+	struct drm_dma_handle *dmah = arg;
+
+	if (error != 0)
+		return;
+
+	DRM_KASSERT(nsegs == 1,
+	    ("drm_ati_alloc_pcigart_table_cb: bad dma segment count"));
+
+	dmah->busaddr = segs[0].ds_addr;
+}
+#endif
+
+static int
+drm_ati_alloc_pcigart_table(struct drm_device *dev,
+			    struct drm_ati_pcigart_info *gart_info)
+{
+	struct drm_dma_handle *dmah;
+	int flags, ret;
+#if defined(__NetBSD__)
+    int nsegs;
+#endif
+
+	dmah = malloc(sizeof(struct drm_dma_handle), DRM_MEM_DMA,
+	    M_ZERO | M_NOWAIT);
+	if (dmah == NULL)
 		return ENOMEM;
+
+#if defined(__FreeBSD__)
+	DRM_UNLOCK();
+	ret = bus_dma_tag_create(NULL, PAGE_SIZE, 0, /* tag, align, boundary */
+	    gart_info->table_mask, BUS_SPACE_MAXADDR, /* lowaddr, highaddr */
+	    NULL, NULL, /* filtfunc, filtfuncargs */
+	    gart_info->table_size, 1, /* maxsize, nsegs */
+	    gart_info->table_size, /* maxsegsize */
+	    BUS_DMA_ALLOCNOW, NULL, NULL, /* flags, lockfunc, lockfuncargs */
+	    &dmah->tag);
+	if (ret != 0) {
+		free(dmah, DRM_MEM_DMA);
+		return ENOMEM;
+	}
+
+	flags = BUS_DMA_NOWAIT | BUS_DMA_ZERO;
+	if (gart_info->gart_reg_if == DRM_ATI_GART_IGP)
+	    flags |= BUS_DMA_NOCACHE;
+	
+	ret = bus_dmamem_alloc(dmah->tag, &dmah->vaddr, flags, &dmah->map);
+	if (ret != 0) {
+		bus_dma_tag_destroy(dmah->tag);
+		free(dmah, DRM_MEM_DMA);
+		return ENOMEM;
+	}
+	DRM_LOCK();
+
+	ret = bus_dmamap_load(dmah->tag, dmah->map, dmah->vaddr,
+	    gart_info->table_size, drm_ati_alloc_pcigart_table_cb, dmah, 0);
+	if (ret != 0) {
+		bus_dmamem_free(dmah->tag, dmah->vaddr, dmah->map);
+		bus_dma_tag_destroy(dmah->tag);
+		free(dmah, DRM_MEM_DMA);
+		return ENOMEM;
+	}
+
+#elif   defined(__NetBSD__)
+    dmah->tag = dev->pa.pa_dmat;
+
+	flags = BUS_DMA_NOWAIT;
+	if (gart_info->gart_reg_if == DRM_ATI_GART_IGP)
+	    flags |= BUS_DMA_NOCACHE;
+
+    ret = bus_dmamem_alloc(dmah->tag, gart_info->table_size, PAGE_SIZE,
+            0, dmah->segs, 1, &nsegs, flags);
+    if (ret != 0) {
+        printf("drm: unable to allocate %zu bytes of DMA, error %d\n",
+                (size_t)gart_info->table_size, ret);
+        dmah->tag = NULL;
+        free(dmah, DRM_MEM_DMA);
+        return ENOMEM;
+    }
+    if (nsegs != 1) {
+        printf("drm: bad segment count\n");
+        bus_dmamem_free(dmah->tag, dmah->segs, 1);
+        dmah->tag = NULL;
+        free(dmah, DRM_MEM_DMA);
+        return ENOMEM;
+    }
+
+    ret = bus_dmamem_map(dmah->tag, dmah->segs, nsegs, gart_info->table_size,
+            &dmah->vaddr, flags | BUS_DMA_COHERENT);
+    if (ret != 0) {
+        printf("drm: Unable to map DMA, error %d\n", ret);
+        bus_dmamem_free(dmah->tag, dmah->segs, 1);
+        dmah->tag = NULL;
+        free(dmah, DRM_MEM_DMA);
+        return ENOMEM;
+    }
+
+    ret = bus_dmamap_create(dmah->tag, gart_info->table_size, 1,
+            gart_info->table_size, 0,
+            BUS_DMA_NOWAIT | BUS_DMA_ALLOCNOW, &dmah->map);
+    if (ret != 0) {
+		printf("drm: Unable to create DMA map, error %d\n", ret);
+    	bus_dmamem_unmap(dmah->tag, dmah->vaddr, gart_info->table_size);
+        bus_dmamem_free(dmah->tag, dmah->segs, 1);
+        dmah->tag = NULL;
+        free(dmah, DRM_MEM_DMA);
+        return ENOMEM;
+    }
+
+    ret = bus_dmamap_load(dmah->tag, dmah->map, dmah->vaddr,
+            gart_info->table_size, NULL, BUS_DMA_NOWAIT);
+    if (ret != 0) {
+		printf("drm: Unable to load DMA map, error %d\n", ret);
+        bus_dmamap_destroy(dmah->tag, dmah->map);
+    	bus_dmamem_unmap(dmah->tag, dmah->vaddr, gart_info->table_size);
+        bus_dmamem_free(dmah->tag, dmah->segs, 1);
+        dmah->tag = NULL;
+        free(dmah, DRM_MEM_DMA);
+        return ENOMEM;
+    }
+    dmah->busaddr = dmah->map->dm_segs[0].ds_addr;
+    dmah->size = gart_info->table_size;
+    dmah->nsegs = 1;
+    /*
+     * Mirror here FreeBSD doing BUS_DMA_ZERO.
+     * But I see this same memset() is done in drm_ati_pcigart_init(),
+     * so maybe this is not needed.
+     */
+    memset(dmah->vaddr, 0, gart_info->table_size);
+#endif
+
+	dev->sg->dmah = dmah;
 
 	return 0;
 }
 
-static void drm_ati_free_pcigart_table(struct drm_device *dev,
-				       struct drm_ati_pcigart_info *gart_info)
+static void
+drm_ati_free_pcigart_table(struct drm_device *dev,
+			   struct drm_ati_pcigart_info *gart_info)
 {
-	drm_pci_free(dev, dev->sg->dmah);
+	struct drm_dma_handle *dmah = dev->sg->dmah;
+
+#if defined(__FreeBSD__)
+	bus_dmamem_free(dmah->tag, dmah->vaddr, dmah->map);
+	bus_dma_tag_destroy(dmah->tag);
+#elif   defined(__NetBSD__)
+    bus_dmamap_unload(dmah->tag, dmah->map);
+    bus_dmamap_destroy(dmah->tag, dmah->map);
+    bus_dmamem_unmap(dmah->tag, dmah->vaddr, dmah->size);
+    bus_dmamem_free(dmah->tag, dmah->segs, 1);
+    dmah->tag = NULL;
+#endif
+	free(dmah, DRM_MEM_DMA);
 	dev->sg->dmah = NULL;
 }
 
-int drm_ati_pcigart_cleanup(struct drm_device *dev, struct drm_ati_pcigart_info *gart_info)
+int
+drm_ati_pcigart_cleanup(struct drm_device *dev,
+			struct drm_ati_pcigart_info *gart_info)
 {
 	/* we need to support large memory configurations */
 	if (dev->sg == NULL) {
@@ -77,17 +220,17 @@ int drm_ati_pcigart_cleanup(struct drm_device *dev, struct drm_ati_pcigart_info 
 	return 1;
 }
 
-int drm_ati_pcigart_init(struct drm_device *dev,
-			 struct drm_ati_pcigart_info *gart_info)
+int
+drm_ati_pcigart_init(struct drm_device *dev,
+		     struct drm_ati_pcigart_info *gart_info)
 {
-
 	void *address = NULL;
 	unsigned long pages;
 	u32 *pci_gart, page_base;
 	dma_addr_t bus_address = 0;
+	dma_addr_t entry_addr;
 	int i, j, ret = 0;
 	int max_pages;
-	dma_addr_t entry_addr;
 
 	/* we need to support large memory configurations */
 	if (dev->sg == NULL) {
@@ -129,12 +272,14 @@ int drm_ati_pcigart_init(struct drm_device *dev,
 			page_base = (u32) entry_addr & ATI_PCIGART_PAGE_MASK;
 			switch(gart_info->gart_reg_if) {
 			case DRM_ATI_GART_IGP:
-				page_base |= (upper_32_bits(entry_addr) & 0xff) << 4;
+				page_base |=
+				    (upper_32_bits(entry_addr) & 0xff) << 4;
 				page_base |= 0xc;
 				break;
 			case DRM_ATI_GART_PCIE:
 				page_base >>= 8;
-				page_base |= (upper_32_bits(entry_addr) & 0xff) << 24;
+				page_base |=
+				    (upper_32_bits(entry_addr) & 0xff) << 24;
 				page_base |= ATI_PCIE_READ | ATI_PCIE_WRITE;
 				break;
 			default:
@@ -146,8 +291,6 @@ int drm_ati_pcigart_init(struct drm_device *dev,
 			entry_addr += ATI_PCIGART_PAGE_SIZE;
 		}
 	}
-
-	DRM_MEMORYBARRIER();
 
 	ret = 1;
 
