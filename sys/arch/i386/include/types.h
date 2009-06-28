6cdc8b7df8a7b0fede6cba08da3d490fa5de2ac7
ad <ad@NetBSD.org>
2009-06-28 09:24:09 +0000

    Define __HAVE_ATOMIC64_OPS only in kernel, since it doesn't work in userspace on the i486.

--- sys/arch/i386/include/types.h
+++ sys/arch/i386/include/types.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: types.h,v 1.65 2009/04/19 14:11:37 ad Exp $	*/
+/*	$NetBSD: types.h,v 1.66 2009/06/28 09:24:09 ad Exp $	*/
 
 /*-
  * Copyright (c) 1990 The Regents of the University of California.
@@ -81,12 +81,12 @@ typedef	volatile unsigned char		__cpu_simple_lock_t;
 #define	__HAVE_SYSCALL_INTERN
 #define	__HAVE_MINIMAL_EMUL
 #define	__HAVE_OLD_DISKLABEL
-#define	__HAVE_ATOMIC64_OPS
 #define	__HAVE_ATOMIC_AS_MEMBAR
 #define	__HAVE_CPU_LWP_SETPRIVATE
 #define	__HAVE_INTR_CONTROL
 
 #if defined(_KERNEL)
+#define	__HAVE_ATOMIC64_OPS		/* not true for i486 in userspace */
 #define	__HAVE_RAS
 #endif
 
