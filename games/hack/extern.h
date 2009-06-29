5181e6c7d46b8e6d0dadfb6da90b1a7477f72b7b
snj <snj@NetBSD.org>
2009-06-29 23:31:28 +0000

    Pull up following revision(s) (requested by dholland in ticket #844): 	games/hack/hack.fight.c: revision 1.10 	games/hack/hack.topl.c: revision 1.10 	games/hack/hack.invent.c: revision 1.12 	games/hack/hack.rip.c: revision 1.10 	games/hack/hack.options.c: revision 1.9 	games/hack/hack.main.c: revision 1.12 	games/hack/hack.eat.c: revision 1.8 	games/hack/hack.do_name.c: revision 1.9 	games/hack/hack.shk.c: revision 1.10 	games/hack/hack.end.c: revision 1.12 	games/hack/extern.h: revision 1.11 	games/hack/hack.pri.c: revision 1.11 	games/hack/hack.objnam.c: revision 1.9 	games/hack/hack.unix.c: revision 1.12 sprintf -> snprintf, plus some use of strlcpy/strlcat where appropriate XXX: there's still one sprintf left which will take some hacking to expunge.

--- games/hack/extern.h
+++ games/hack/extern.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: extern.h,v 1.8 2008/04/28 20:22:54 martin Exp $	*/
+/*	$NetBSD: extern.h,v 1.8.6.1 2009/06/29 23:31:28 snj Exp $	*/
 
 /*-
  * Copyright (c) 1997 The NetBSD Foundation, Inc.
@@ -356,7 +356,7 @@ char *sitoa(int);
 char *typename(int);
 char *xname(struct obj *);
 char *doname(struct obj *);
-void setan(const char *, char *);
+void setan(const char *, char *, size_t);
 char *aobjnam(struct obj *, const char *);
 char *Doname(struct obj *);
 struct obj *readobjnam(char *);
