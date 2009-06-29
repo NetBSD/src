e3618d49aade1926e2ea61b93d84a69e8cf24f62
snj <snj@NetBSD.org>
2009-06-29 23:46:51 +0000

    Pull up following revision(s) (requested by dholland in ticket #1331): 	games/hack/hack.do_name.c: revision 1.10 	games/hack/hack.h: revision 1.13 via patch 	games/hack/hack.invent.c: revision 1.13 	games/hack/hack.main.c: revision 1.13 	games/hack/hack.rip.c: revision 1.11 	games/hack/hack.topl.c: revision 1.11 	games/hack/hack.unix.c: revision 1.13 Fix two serious string-handling bugs (one exploitable, one probably exploitable) and also add proper checking/paranoia in several other places.

--- games/hack/hack.main.c
+++ games/hack/hack.main.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: hack.main.c,v 1.9.26.1 2009/06/29 23:43:48 snj Exp $	*/
+/*	$NetBSD: hack.main.c,v 1.9.26.2 2009/06/29 23:46:51 snj Exp $	*/
 
 /*
  * Copyright (c) 1985, Stichting Centrum voor Wiskunde en Informatica,
@@ -63,7 +63,7 @@
 
 #include <sys/cdefs.h>
 #ifndef lint
-__RCSID("$NetBSD: hack.main.c,v 1.9.26.1 2009/06/29 23:43:48 snj Exp $");
+__RCSID("$NetBSD: hack.main.c,v 1.9.26.2 2009/06/29 23:46:51 snj Exp $");
 #endif				/* not lint */
 
 #include <signal.h>
@@ -302,7 +302,8 @@ main(argc, argv)
 				}
 				*gp = 0;
 			} else
-				(void) strcpy(genocided, sfoo);
+				(void) strlcpy(genocided, sfoo,
+						sizeof(genocided));
 			(void) strcpy(fut_geno, genocided);
 		}
 	}
@@ -481,12 +482,12 @@ glo(foo)
 	int foo;
 {
 	/* construct the string  xlock.n  */
-	char           *tf;
+	size_t pos;
 
-	tf = lock;
-	while (*tf && *tf != '.')
-		tf++;
-	(void) sprintf(tf, ".%d", foo);
+	pos = 0;
+	while (lock[pos] && lock[pos] != '.')
+		pos++;
+	(void) snprintf(lock + pos, sizeof(lock) - pos, ".%d", foo);
 }
 
 /*
