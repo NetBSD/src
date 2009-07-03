308c50249da8da45d3d392111d7e6f1bea2d5e85
elad <elad>
2009-07-03 21:17:40 +0000

    Where possible, extract the file-system's access() routine to two internal functions: the first checking if the operation is possible (regardless of permissions), the second checking file-system permissions, ACLs, etc.
    Mailing list reference:
    
    	http://mail-index.netbsd.org/tech-kern/2009/06/21/msg005311.html

--- sys/miscfs/procfs/procfs_vnops.c
+++ sys/miscfs/procfs/procfs_vnops.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: procfs_vnops.c,v 1.175 2009/06/23 19:36:40 elad Exp $	*/
+/*	$NetBSD: procfs_vnops.c,v 1.176 2009/07/03 21:17:42 elad Exp $	*/
 
 /*-
  * Copyright (c) 2006, 2007, 2008 The NetBSD Foundation, Inc.
@@ -105,7 +105,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: procfs_vnops.c,v 1.175 2009/06/23 19:36:40 elad Exp $");
+__KERNEL_RCSID(0, "$NetBSD: procfs_vnops.c,v 1.176 2009/07/03 21:17:42 elad Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -909,6 +909,21 @@ procfs_setattr(void *v)
 	return (0);
 }
 
+static int
+procfs_check_possible(struct vnode *vp, mode_t mode)
+{
+
+	return 0;
+}
+
+static int
+procfs_check_permitted(struct vattr *va, mode_t mode, kauth_cred_t cred)
+{
+
+	return genfs_can_access(va->va_type, va->va_mode,
+	    va->va_uid, va->va_gid, mode, cred);
+}
+
 /*
  * implement access checking.
  *
@@ -932,8 +947,13 @@ procfs_access(void *v)
 	if ((error = VOP_GETATTR(ap->a_vp, &va, ap->a_cred)) != 0)
 		return (error);
 
-	return (genfs_can_access(va.va_type, va.va_mode,
-	    va.va_uid, va.va_gid, ap->a_mode, ap->a_cred));
+	error = procfs_check_possible(ap->a_vp, ap->a_mode);
+	if (error)
+		return error;
+
+	error = procfs_check_permitted(&va, ap->a_mode, ap->a_cred);
+
+	return error;
 }
 
 /*
