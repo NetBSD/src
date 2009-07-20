e4fee361e479b1c4e0b1abe10cee3cb6480f1377
kiyohara <kiyohara@NetBSD.org>
2009-07-20 05:10:49 +0000

    Fix little bit, and add a few files.   current status is to see following thread.     http://mail-index.netbsd.org/port-ia64/2009/06/18/msg000102.html Also call banner().

--- sys/arch/ia64/ia64/mainbus.c
+++ sys/arch/ia64/ia64/mainbus.c
@@ -1,10 +1,10 @@
-/*	$NetBSD: mainbus.c,v 1.4 2009/03/14 21:04:10 dsl Exp $	*/
+/*	$NetBSD: mainbus.c,v 1.5 2009/07/20 05:10:49 kiyohara Exp $	*/
 
 /*-
  * Copyright (c) 2006 The NetBSD Foundation, Inc.
  * All rights reserved.
  *
- * Author: 
+ * Author:
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -29,25 +29,31 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: mainbus.c,v 1.4 2009/03/14 21:04:10 dsl Exp $");
+__KERNEL_RCSID(0, "$NetBSD: mainbus.c,v 1.5 2009/07/20 05:10:49 kiyohara Exp $");
 
+#include "acpi.h"
 
 #include <sys/param.h>
-#include <sys/systm.h>
 #include <sys/device.h>
+#include <sys/errno.h>
 
-int	mainbus_match(struct device *, struct cfdata *, void *);
-void	mainbus_attach(struct device *, struct device *, void *);
+#include <machine/bus.h>
 
-CFATTACH_DECL(mainbus, sizeof(struct device),
+#include <dev/acpi/acpivar.h>
+
+
+static int mainbus_match(struct device *, struct cfdata *, void *);
+static void mainbus_attach(struct device *, struct device *, void *);
+
+CFATTACH_DECL_NEW(mainbus, sizeof(struct device),
     mainbus_match, mainbus_attach, NULL, NULL);
 
 
 /*
  * Probe for the mainbus; always succeeds.
  */
-int
-mainbus_match(struct device *parent, struct cfdata *match, void *aux)
+static int
+mainbus_match(device_t parent, struct cfdata *match, void *aux)
 {
 
 	return 1;
@@ -56,8 +62,29 @@ mainbus_match(struct device *parent, struct cfdata *match, void *aux)
 /*
  * Attach the mainbus.
  */
-void
-mainbus_attach(struct device *parent, struct device *self, void *aux)
+static void
+mainbus_attach(device_t parent, device_t self, void *aux)
 {
+#if NACPI > 0
+	struct acpibus_attach_args aaa;
+#endif
+
+	aprint_naive("\n");
+	aprint_normal("\n");
+
+#if NACPI > 0
+	acpi_probe();
+
+	aaa.aa_iot = IA64_BUS_SPACE_IO;
+	aaa.aa_memt = IA64_BUS_SPACE_MEM;
+	aaa.aa_pc = 0;
+	aaa.aa_pciflags =
+	    PCI_FLAGS_IO_ENABLED | PCI_FLAGS_MEM_ENABLED |
+	    PCI_FLAGS_MRL_OKAY | PCI_FLAGS_MRM_OKAY |
+	    PCI_FLAGS_MWI_OKAY;
+	aaa.aa_ic = 0;
+	config_found_ia(self, "acpibus", &aaa, 0);
+#endif
+
 	return;
 }
