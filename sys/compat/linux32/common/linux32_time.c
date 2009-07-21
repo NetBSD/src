7d645a3ea428d7d2667a1c453d40266e4ece0a2f
njoly <njoly@NetBSD.org>
2009-07-21 18:50:43 +0000

    Kill unreachable return statement.

--- sys/compat/linux32/common/linux32_time.c
+++ sys/compat/linux32/common/linux32_time.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: linux32_time.c,v 1.28 2009/01/16 13:10:47 njoly Exp $ */
+/*	$NetBSD: linux32_time.c,v 1.29 2009/07/21 18:50:43 njoly Exp $ */
 
 /*-
  * Copyright (c) 2006 Emmanuel Dreyfus, all rights reserved.
@@ -33,7 +33,7 @@
 
 #include <sys/cdefs.h>
 
-__KERNEL_RCSID(0, "$NetBSD: linux32_time.c,v 1.28 2009/01/16 13:10:47 njoly Exp $");
+__KERNEL_RCSID(0, "$NetBSD: linux32_time.c,v 1.29 2009/07/21 18:50:43 njoly Exp $");
 
 #include <sys/types.h>
 #include <sys/param.h>
@@ -349,8 +349,6 @@ linux32_sys_clock_getres(struct lwp *l,
 	ts.tv_nsec = 1000000000 / tc_getfrequency();
 	native_to_linux32_timespec(&lts, &ts);
 	return copyout(&lts, SCARG_P32(uap, tp), sizeof lts);
-
-	return 0;
 }
 
 int
