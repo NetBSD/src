74c9ebf3706a13e93ed091859b64e622c3db6cda
matt <matt@NetBSD.org>
2009-08-06 00:50:25 +0000

    GDIUM support.  It compiles but doesn't yet work.  It dies quickly in mips_vector_init.

---
+++ sys/arch/evbmips/gdium/autoconf.c
@@ -0,0 +1,112 @@
+/*	$NetBSD: autoconf.c,v 1.1 2009/08/06 00:50:25 matt Exp $	*/
+
+/*
+ * Copyright 2002 Wasabi Systems, Inc.
+ * All rights reserved.
+ *
+ * Written by Simon Burge for Wasabi Systems, Inc.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *      This product includes software developed for the NetBSD Project by
+ *      Wasabi Systems, Inc.
+ * 4. The name of Wasabi Systems, Inc. may not be used to endorse
+ *    or promote products derived from this software without specific prior
+ *    written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY WASABI SYSTEMS, INC. ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL WASABI SYSTEMS, INC
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <sys/cdefs.h>
+__KERNEL_RCSID(0, "$NetBSD: autoconf.c,v 1.1 2009/08/06 00:50:25 matt Exp $");
+
+#include <sys/param.h>
+#include <sys/systm.h>
+#include <sys/buf.h>
+#include <sys/conf.h>
+#include <sys/device.h>
+#include <sys/cpu.h>
+
+static void	findroot(void);
+
+void
+cpu_configure(void)
+{
+
+	intr_init();
+
+	/* Kick off autoconfiguration. */
+	(void)splhigh();
+	if (config_rootfound("mainbus", NULL) == NULL)
+		panic("no mainbus found");
+
+	/*
+	 * Hardware interrupts will be enabled in
+	 * sys/arch/mips/mips/mips3_clockintr.c:mips3_initclocks()
+	 * to avoid hardclock(9) by CPU INT5 before softclockintr is
+	 * initialized in initclocks().
+	 */
+}
+
+void
+cpu_rootconf(void)
+{
+	findroot();
+
+	printf("boot device: %s\n",
+		booted_device ? booted_device->dv_xname : "<unknown>");
+
+	setroot(booted_device, booted_partition);
+}
+
+extern char	bootstring[];
+extern int	netboot;
+
+static void
+findroot(void)
+{
+	struct device *dv;
+
+	if (booted_device)
+		return;
+
+	if ((booted_device == NULL) && netboot == 0)
+		for (dv = TAILQ_FIRST(&alldevs); dv != NULL;
+		     dv = TAILQ_NEXT(dv, dv_list))
+			if (device_class(dv) == DV_DISK &&
+			    device_is_a(dv, "wd"))
+				    booted_device = dv;
+
+	/*
+	 * XXX Match up MBR boot specification with BSD disklabel for root?
+	 */
+	booted_partition = 0;
+
+	return;
+}
+
+void
+device_register(struct device *dev, void *aux)
+{
+	if ((booted_device == NULL) && (netboot == 1))
+		if (device_class(dev) == DV_IFNET)
+			booted_device = dev;
+}
---
+++ sys/arch/evbmips/gdium/gdiumvar.h
@@ -0,0 +1,89 @@
+/*	$NetBSD: gdiumvar.h,v 1.1 2009/08/06 00:50:26 matt Exp $	*/
+
+/*-
+ * Copyright (c) 2001 The NetBSD Foundation, Inc.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to The NetBSD Foundation
+ * by Jason R. Thorpe.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <machine/bus.h>
+#include <dev/pci/pcivar.h>
+
+#include <mips/bonito/bonitovar.h>
+
+struct gdium_config {
+	struct bonito_config gc_bonito;
+
+	struct mips_bus_space gc_iot;
+	struct mips_bus_space gc_memt;
+
+	struct mips_bus_dma_tag gc_pci_dmat;
+
+	struct mips_pci_chipset gc_pc;
+
+	struct extent *gc_io_ex;
+	struct extent *gc_mem_ex;
+
+	int	gc_mallocsafe;
+};
+
+struct mainbus_attach_args {
+	const char *maa_name;
+};
+
+#define	GDIUM_IRQ_GPIO0		0
+#define	GDIUM_IRQ_GPIO1		1
+#define	GDIUM_IRQ_GPIO2		2
+#define	GDIUM_IRQ_GPIO3		3
+#define	GDIUM_IRQ_PCI_INTA	4
+#define	GDIUM_IRQ_PCI_INTB	5
+#define	GDIUM_IRQ_PCI_INTC	6
+#define	GDIUM_IRQ_PCI_INTD	7
+#define	GDIUM_IRQ_PCI_PERR	8
+#define	GDIUM_IRQ_PCI_SERR	9 
+#define	GDIUM_IRQ_DENALI	10
+#define	GDIUM_IRQ_INT0		11
+#define	GDIUM_IRQ_INT1		12
+#define	GDIUM_IRQ_INT2		13
+#define	GDIUM_IRQ_INT3		14
+#define	GDIUM_IRQ_MAX		14
+
+#ifdef _KERNEL
+extern struct gdium_config gdium_configuration;
+
+int	gdium_cnattach(struct gdium_config *);
+
+void	gdium_bus_io_init(bus_space_tag_t, void *);
+void	gdium_bus_mem_init(bus_space_tag_t, void *);
+
+void	gdium_dma_init(struct gdium_config *);
+
+void	gdium_intr_init(struct gdium_config *);
+
+//void	gdium_iointr(uint32_t, uint32_t, register_t, uint32_t);
+
+void	gdium_cal_timer(bus_space_tag_t, bus_space_handle_t);
+#endif /* _KERNEL */
