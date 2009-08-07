191e82d579c40b49ca88fd6306af2e4313950761
lukem <lukem@NetBSD.org>
2009-08-07 16:32:43 +0000

    Crank version to nb4

--- dist/nvi/ex/version.h
+++ dist/nvi/ex/version.h
@@ -1,3 +1,3 @@
-/*	$NetBSD: version.h,v 1.2 2008/12/12 23:05:36 lukem Exp $ */
+/*	$NetBSD: version.h,v 1.3 2009/08/07 16:32:43 lukem Exp $ */
 
-#define VI_VERSION "nvi-1.81.6nb1 (2007-11-18)"
+#define VI_VERSION "nvi-1.81.6nb4 (2009-08-07)"
