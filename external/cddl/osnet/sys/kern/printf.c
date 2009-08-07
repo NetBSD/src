fc8ec0b896620494ee06c82427885881da50d528
haad <haad@NetBSD.org>
2009-08-07 20:57:55 +0000

    Add NetBSD Solaris compat code and reachover Makefiles. Most of code written by Andrew Doran some of it by me.
    This is still work in progress code. There are known issues. Zfs can panic
    your system still. I will commit integration bits and needed kernel bits
    later when we fix most obvious bugs. This code need some support from our
    kernel which I will commit in a separate patch and therefore itnot work yet.
    
    Issues:
    
    1) Support for openat/fstatat syscalls.
    2) getnewvnode locking against myself panic when reclaiming vnodes
    3) Replace solaris taskq with our workqueue which doesn't need to alloc
       anything during work.
    4) Investigate problems with NO_WAIT allocations.
    
    Oked by core@ and ad@.

---
+++ external/cddl/osnet/sys/kern/printf.c
@@ -0,0 +1,65 @@
+/*	$NetBSD: printf.c,v 1.1 2009/08/07 20:57:57 haad Exp $	*/
+
+/*
+ * CDDL HEADER START
+ *
+ * The contents of this file are subject to the terms of the
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
+ *
+ * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
+ * or http://www.opensolaris.org/os/licensing.
+ * See the License for the specific language governing permissions
+ * and limitations under the License.
+ *
+ * When distributing Covered Code, include this CDDL HEADER in each
+ * file and include the License file at usr/src/OPENSOLARIS.LICENSE.
+ * If applicable, add the following below this CDDL HEADER, with the
+ * fields enclosed by brackets "[]" replaced with your own identifying
+ * information: Portions Copyright [yyyy] [name of copyright owner]
+ *
+ * CDDL HEADER END
+ */
+/*
+ * Copyright 2006 Sun Microsystems, Inc.  All rights reserved.
+ * Use is subject to license terms.
+ */
+
+#pragma ident	"%Z%%M%	%I%	%E% SMI"
+
+#include <sys/param.h>
+#include <sys/types.h>
+#include <sys/systm.h>
+#include <sys/cmn_err.h>
+
+static char ce_prefix[CE_IGNORE][10] = { "", "NOTICE: ", "WARNING: ", "" };
+static char ce_suffix[CE_IGNORE][2] = { "", "\n", "\n", "" };
+
+void
+vcmn_err(int ce, const char *fmt, va_list adx)
+{
+	char buf[256];
+
+	if (ce == CE_PANIC) {
+		vprintf(fmt, adx);
+		panic("panic");
+	}
+
+	if ((uint_t)ce < CE_IGNORE) {
+		strcpy(buf, ce_prefix[ce]);
+		vsnprintf(buf + strlen(ce_prefix[ce]), sizeof(buf),
+		    fmt, adx);
+		strlcat(buf, ce_suffix[ce], sizeof(buf));
+		printf("%s", buf);
+ 	}
+}
+
+void
+cmn_err(int ce, const char *fmt, ...)
+{
+	va_list adx;
+
+	va_start(adx, fmt);
+	vcmn_err(ce, fmt, adx);
+	va_end(adx);
+}
---
+++ external/cddl/osnet/sys/sys/vm/seg_vn.h
@@ -0,0 +1,5 @@
+#ifndef _SYS_SEG_VN_H
+#define _SYS_SEG_VN_H
+
+
+#endif
