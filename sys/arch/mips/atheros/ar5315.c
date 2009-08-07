6b964160a2a748cb34b57c2039a51d9beec3c6a4
snj <snj>
2009-08-07 06:50:47 +0000

    Pull up following revision(s) (requested by jmcneill in ticket #775): 	sys/arch/mips/atheros/ar5312_board.c: revision 1.2 	sys/arch/mips/atheros/ar5315.c: revision 1.6 	sys/arch/mips/atheros/ar5315_board.c: revision 1.2 	sys/arch/mips/atheros/dev/if_ath_arbus.c: revision 1.16 	sys/arch/mips/atheros/include/ar5312reg.h: revision 1.3 Update reference to the Atheros HAL exported headers. `external/isc/atheros_hal/dist', former `contrib/dev/ath/' is now in cpp(1)'s include path. Fix build of MERAKI kernel.

--- sys/arch/mips/atheros/ar5315.c
+++ sys/arch/mips/atheros/ar5315.c
@@ -1,4 +1,4 @@
-/* $NetBSD: ar5315.c,v 1.5 2008/01/23 05:23:59 dyoung Exp $ */
+/* $NetBSD: ar5315.c,v 1.5.20.1 2009/08/07 06:50:48 snj Exp $ */
 
 /*
  * Copyright (c) 2006 Urbana-Champaign Independent Media Center.
@@ -48,7 +48,7 @@
  * family.
  */
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: ar5315.c,v 1.5 2008/01/23 05:23:59 dyoung Exp $");
+__KERNEL_RCSID(0, "$NetBSD: ar5315.c,v 1.5.20.1 2009/08/07 06:50:48 snj Exp $");
 
 #include "opt_ddb.h"
 #include "opt_kgdb.h"
@@ -69,7 +69,7 @@ __KERNEL_RCSID(0, "$NetBSD: ar5315.c,v 1.5 2008/01/23 05:23:59 dyoung Exp $");
 
 #include <prop/proplib.h>
 
-#include <contrib/dev/ath/ah_soc.h>	/* XXX really doesn't belong in hal */
+#include <ah_soc.h>	/* XXX really doesn't belong in hal */
 
 #include <mips/atheros/include/ar5315reg.h>
 #include <mips/atheros/include/ar531xvar.h>
