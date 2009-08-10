4c1e749f2d7acfe1c28d8f6e8742570173be6b08
haad <haad@NetBSD.org>
2009-08-10 22:38:02 +0000

    Add some NetBSD fixes which I have forgot to commit during first round. With these patches I can build solaris and zfs module again.

--- external/cddl/osnet/dist/uts/common/fs/zfs/dmu.c
+++ external/cddl/osnet/dist/uts/common/fs/zfs/dmu.c
@@ -751,6 +751,7 @@ dmu_write_uio(objset_t *os, uint64_t object, uio_t *uio, uint64_t size,
 	return (err);
 }
 
+#ifndef __NetBSD__
 int
 dmu_write_pages(objset_t *os, uint64_t object, uint64_t offset, uint64_t size,
     page_t *pp, dmu_tx_t *tx)
@@ -808,6 +809,7 @@ dmu_write_pages(objset_t *os, uint64_t object, uint64_t offset, uint64_t size,
 	dmu_buf_rele_array(dbp, numbufs, FTAG);
 	return (err);
 }
+#endif  /* __NetBSD__ */
 #endif
 
 typedef struct {
