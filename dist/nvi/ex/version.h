d7f16ad59af7106bb4c67395ac999cfb7b583d5c
aymeric <aymeric@NetBSD.org>
2009-08-11 21:42:03 +0000

    bump to nb5: . :ESC works again . #+ and #- work again

--- dist/nvi/ex/version.h
+++ dist/nvi/ex/version.h
@@ -1,3 +1,3 @@
-/*	$NetBSD: version.h,v 1.3 2009/08/07 16:32:43 lukem Exp $ */
+/*	$NetBSD: version.h,v 1.4 2009/08/11 21:42:03 aymeric Exp $ */
 
-#define VI_VERSION "nvi-1.81.6nb4 (2009-08-07)"
+#define VI_VERSION "nvi-1.81.6nb5 (2009-08-11)"
