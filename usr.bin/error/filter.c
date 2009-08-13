29c06c07e9094465380d6e8b35281c84e67cc194
dholland <dholland@NetBSD.org>
2009-08-13 03:07:49 +0000

    Whitespace.

--- usr.bin/error/filter.c
+++ usr.bin/error/filter.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: filter.c,v 1.11 2009/08/13 02:10:50 dholland Exp $	*/
+/*	$NetBSD: filter.c,v 1.12 2009/08/13 03:07:49 dholland Exp $	*/
 
 /*
  * Copyright (c) 1980, 1993
@@ -34,7 +34,7 @@
 #if 0
 static char sccsid[] = "@(#)filter.c	8.1 (Berkeley) 6/6/93";
 #endif
-__RCSID("$NetBSD: filter.c,v 1.11 2009/08/13 02:10:50 dholland Exp $");
+__RCSID("$NetBSD: filter.c,v 1.12 2009/08/13 03:07:49 dholland Exp $");
 #endif /* not lint */
 
 #include <sys/param.h>
@@ -59,30 +59,30 @@ static int lexsort(const void *, const void *);
 static int search_ignore(char *);
 
 /*
- *	Read the file ERRORNAME of the names of functions in lint
- *	to ignore complaints about.
+ * Read the file ERRORNAME of the names of functions in lint
+ * to ignore complaints about.
  */
 void
 getignored(char *auxname)
 {
-	int	i;
-	FILE	*fyle;
-	char	inbuffer[256];
-	uid_t	uid;
-	char	filename[MAXPATHLEN];
-	char	*username;
-	struct	passwd *passwdentry;
+	int i;
+	FILE *fyle;
+	char inbuffer[256];
+	uid_t uid;
+	char filename[MAXPATHLEN];
+	char *username;
+	struct passwd *passwdentry;
 
 	nignored = 0;
-	if (auxname == 0){	/* use the default */
-		if ( (username = (char *)getlogin()) == NULL){
+	if (auxname == 0) {	/* use the default */
+		if ((username = (char *)getlogin()) == NULL) {
 			username = "Unknown";
 			uid = getuid();
-			if ( (passwdentry = (struct passwd *)getpwuid(uid)) == NULL){
+			if ((passwdentry = (struct passwd *)getpwuid(uid)) == NULL) {
 				return;
 			}
 		} else {
-			if ( (passwdentry = (struct passwd *)getpwnam(username)) == NULL)
+			if ((passwdentry = (struct passwd *)getpwnam(username)) == NULL)
 				return;
 		}
 		strlcpy(filename, passwdentry->pw_dir, sizeof(filename));
@@ -93,22 +93,23 @@ getignored(char *auxname)
 	printf("Opening file \"%s\" to read names to ignore.\n",
 		filename);
 #endif
-	if ( (fyle = fopen(filename, "r")) == NULL){
+	if ((fyle = fopen(filename, "r")) == NULL) {
 #ifdef FULLDEBUG
 		fprintf(stderr, "%s: Can't open file \"%s\"\n",
 			processname, filename);
 #endif
 		return;
 	}
+
 	/*
-	 *	Make the first pass through the file, counting lines
+	 * Make the first pass through the file, counting lines
 	 */
 	for (nignored = 0;
 	     fgets(inbuffer, sizeof(inbuffer)-1, fyle) != NULL; nignored++)
 		continue;
 	names_ignored = (char **)Calloc(nignored+1, sizeof (char *));
 	fclose(fyle);
-	if (freopen(filename, "r", fyle) == NULL){
+	if (freopen(filename, "r", fyle) == NULL) {
 #ifdef FULLDEBUG
 		fprintf(stderr, "%s: Failure to open \"%s\" for second read.\n",
 			processname, filename);
@@ -117,14 +118,14 @@ getignored(char *auxname)
 		return;
 	}
 	for (i=0; i < nignored &&
-	          (fgets (inbuffer, sizeof(inbuffer)-1, fyle) != NULL); i++){
+	          (fgets (inbuffer, sizeof(inbuffer)-1, fyle) != NULL); i++) {
 		names_ignored[i] = strdup(inbuffer);
 		(void)substitute(names_ignored[i], '\n', '\0');
 	}
 	qsort(names_ignored, nignored, sizeof *names_ignored, lexsort);
 #ifdef FULLDEBUG
 	printf("Names to ignore follow.\n");
-	for (i=0; i < nignored; i++){
+	for (i=0; i < nignored; i++) {
 		printf("\tIgnore: %s\n", names_ignored[i]);
 	}
 #endif
@@ -133,72 +134,72 @@ getignored(char *auxname)
 static int
 lexsort(const void *c1, const void *c2)
 {
-	char	**cpp1, **cpp2;
+	char **cpp1, **cpp2;
 
 	cpp1 = (char **)c1;
 	cpp2 = (char **)c2;
-	return(strcmp(*cpp1, *cpp2));
+	return (strcmp(*cpp1, *cpp2));
 }
 
 static int
 search_ignore(char *key)
 {
-	int	ub, lb;
-	int	halfway;
-	int	order;
+	int ub, lb;
+	int halfway;
+	int order;
 
 	if (nignored == 0)
-		return(-1);
-	for(lb = 0, ub = nignored - 1; ub >= lb; ){
+		return (-1);
+	for (lb = 0, ub = nignored - 1; ub >= lb; ) {
 		halfway = (ub + lb)/2;
 		if ( (order = strcmp(key, names_ignored[halfway])) == 0)
-			return(halfway);
-		if (order < 0)	/*key is less than probe, throw away above*/
+			return (halfway);
+		if (order < 0)	/* key is less than probe, throw away above */
 			ub = halfway - 1;
 		 else
 			lb = halfway + 1;
 	}
-	return(-1);
+	return (-1);
 }
 
 /*
- *	Tell if the error text is to be ignored.
- *	The error must have been canonicalized, with
- *	the file name the zeroth entry in the errorv,
- *	and the linenumber the second.
- *	Return the new categorization of the error class.
+ * Tell if the error text is to be ignored.
+ * The error must have been canonicalized, with
+ * the file name the zeroth entry in the errorv,
+ * and the linenumber the second.
+ * Return the new categorization of the error class.
  */
 Errorclass
 discardit(Eptr errorp)
 {
-	int		language;
-	int		i;
-	Errorclass	errorclass = errorp->error_e_class;
+	int language;
+	int i;
+	Errorclass errorclass = errorp->error_e_class;
 
-	switch(errorclass){
-		case	C_SYNC:
-		case	C_NONSPEC:
-		case	C_UNKNOWN:	return(errorclass);
+	switch (errorclass) {
+		case C_SYNC:
+		case C_NONSPEC:
+		case C_UNKNOWN: return (errorclass);
 		default:	;
 	}
-	if(errorp->error_lgtext < 2){
-		return(C_NONSPEC);
+	if (errorp->error_lgtext < 2) {
+		return (C_NONSPEC);
 	}
 	language = errorp->error_language;
-	if(language == INLINT){
-		if (errorclass != C_NONSPEC){	/* no file */
-			for(i=0; lint_libs[i] != 0; i++){
-				if (strcmp(errorp->error_text[0], lint_libs[i]) == 0){
-					return(C_DISCARD);
+	if (language == INLINT) {
+		if (errorclass != C_NONSPEC) {	/* no file */
+			for (i=0; lint_libs[i] != 0; i++) {
+				if (strcmp(errorp->error_text[0], lint_libs[i]) == 0) {
+					return (C_DISCARD);
 				}
 			}
 		}
-		/* check if the argument to the error message is to be ignored*/
+		/* check if the argument to the error message is to be ignored */
 		if (ispunct((unsigned char)lastchar(errorp->error_text[2])))
 			clob_last(errorp->error_text[2], '\0');
-		if (search_ignore(errorp->error_text[errorclass == C_NONSPEC ? 0 : 2]) >= 0){
-			return(C_NULLED);
+		if (search_ignore(errorp->error_text[errorclass == C_NONSPEC ? 0 : 2]) >= 0) {
+			return (C_NULLED);
 		}
 	}
-	return(errorclass);
+	return (errorclass);
 }
