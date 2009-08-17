1890251c554526f1cb446761a41e851e39bae046
matt <matt@NetBSD.org>
2009-08-17 19:05:58 +0000

    Make site MACHINE is set to an empty string.  This avoid generated linker scripts have their OUTPUT_ARCH set to the wrong value since nbmake always sets MACHINE

--- gnu/dist/binutils/ld/emulparams/elf32bmipn32-defs.sh
+++ gnu/dist/binutils/ld/emulparams/elf32bmipn32-defs.sh
@@ -6,6 +6,7 @@ SCRIPT_NAME=elf
 
 # Handle both big- and little-ended 32-bit MIPS objects.
 ARCH=mips
+MACHINE=
 OUTPUT_FORMAT="elf32-bigmips"
 BIG_OUTPUT_FORMAT="elf32-bigmips"
 LITTLE_OUTPUT_FORMAT="elf32-littlemips"
