cc3417e7067b68711939281f16fde2bdc88eee1f
skrll <skrll@NetBSD.org>
2009-08-20 19:44:53 +0000

    Re-run mknative for the PKGVERSION fix.

--- external/gpl3/binutils/lib/libbfd/arch/sparc/bfdver.h
+++ external/gpl3/binutils/lib/libbfd/arch/sparc/bfdver.h
@@ -4,5 +4,5 @@
 
 #define BFD_VERSION_DATE 20090202
 #define BFD_VERSION 219010000
-#define BFD_VERSION_STRING  "(GNU Binutils)" " " "2.19.1"
+#define BFD_VERSION_STRING  "(GNU Binutils) " "2.19.1"
 #define REPORT_BUGS_TO "<http://www.sourceware.org/bugzilla/>"
