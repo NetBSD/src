2ebfea6c79573ce0f28f9ae82c04e2b7d3ff1a30
skrll <skrll@NetBSD.org>
2009-08-21 14:31:02 +0000

    __strong_alias(foo,foo) is... pointless.

--- common/lib/libc/atomic/membar_ops_nop.c
+++ common/lib/libc/atomic/membar_ops_nop.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: membar_ops_nop.c,v 1.4 2008/04/28 20:22:53 martin Exp $	*/
+/*	$NetBSD: membar_ops_nop.c,v 1.5 2009/08/21 14:31:02 skrll Exp $	*/
 
 /*-
  * Copyright (c) 2007 The NetBSD Foundation, Inc.
@@ -54,4 +54,3 @@ atomic_op_alias(membar_consumer,_membar_sync)
 __strong_alias(_membar_consumer,_membar_sync)
 #undef membar_sync
 atomic_op_alias(membar_sync,_membar_sync)
-__strong_alias(_membar_sync,_membar_sync)
