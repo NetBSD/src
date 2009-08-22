ad194bc1b25ad0bf4d4864489a95e44584dc838f
matt <matt@NetBSD.org>
2009-08-22 06:46:44 +0000

    Move ELF{32,64}_MACHDEP_ENDIANNESS to <mips/elf_machdep.h>

--- sys/arch/ews4800mips/include/elf_machdep.h
+++ sys/arch/ews4800mips/include/elf_machdep.h
@@ -1,5 +1,3 @@
-/*	$NetBSD: elf_machdep.h,v 1.1 2005/12/29 15:20:08 tsutsui Exp $	*/
+/*	$NetBSD: elf_machdep.h,v 1.1.98.1 2009/08/22 06:46:45 matt Exp $	*/
 
 #include <mips/elf_machdep.h>
-
-#define	ELF32_MACHDEP_ENDIANNESS	ELFDATA2MSB
--- sys/arch/newsmips/include/elf_machdep.h
+++ sys/arch/newsmips/include/elf_machdep.h
@@ -1,11 +1,3 @@
-/*	$NetBSD: elf_machdep.h,v 1.2 1999/10/25 13:55:09 kleink Exp $	*/
+/*	$NetBSD: elf_machdep.h,v 1.2.154.1 2009/08/22 06:46:45 matt Exp $	*/
 
 #include <mips/elf_machdep.h>
-
-/*
- * newses are mipseb machines
- */
-
-#define ELF32_MACHDEP_ENDIANNESS	ELFDATA2MSB
-
-#define ELF64_MACHDEP_ENDIANNESS	XXX	/* break compilation */
