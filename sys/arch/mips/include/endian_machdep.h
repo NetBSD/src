ae9fd8d4693e2cd710d67e769cf32fc9da27405c
matt <matt@NetBSD.org>
2009-08-23 03:51:35 +0000

    Add REG_SHI and REG_SLO

--- sys/arch/mips/include/endian_machdep.h
+++ sys/arch/mips/include/endian_machdep.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: endian_machdep.h,v 1.1.154.1 2009/08/20 10:05:34 matt Exp $	*/
+/*	$NetBSD: endian_machdep.h,v 1.1.154.2 2009/08/23 03:51:35 matt Exp $	*/
 
 #ifndef _BYTE_ORDER
 # error  Define MIPS target CPU endian-ness in port-specific header file.
@@ -17,9 +17,13 @@
 # if SZREG == 4
 #  define REG_LHI   lwr
 #  define REG_LLO   lwl
+#  define REG_SHI   swr
+#  define REG_SLO   swl
 # else
 #  define REG_LHI   ldr
 #  define REG_LLO   ldl
+#  define REG_SHI   sdr
+#  define REG_SLO   sdl
 # endif
 #endif
 
@@ -31,9 +35,13 @@
 # if SZREG == 4
 #  define REG_LHI   lwl
 #  define REG_LLO   lwr
+#  define REG_SHI   swl
+#  define REG_SLO   swr
 # else
 #  define REG_LHI   ldl
 #  define REG_LLO   ldr
+#  define REG_SHI   sdl
+#  define REG_SLO   sdr
 # endif
 #endif
 
