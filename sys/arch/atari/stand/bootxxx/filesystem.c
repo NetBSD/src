72b1eca8804f592cd50a31b0325796b635e41bd1
tsutsui <tsutsui>
2009-08-24 13:03:43 +0000

    Use __arraycount().

--- sys/arch/atari/stand/bootxxx/filesystem.c
+++ sys/arch/atari/stand/bootxxx/filesystem.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: filesystem.c,v 1.3 2005/12/11 12:17:00 christos Exp $	*/
+/*	$NetBSD: filesystem.c,v 1.4 2009/08/24 13:03:44 tsutsui Exp $	*/
 
 /*
  * Copyright (c) 1993 Philip A. Nelson.
@@ -38,4 +38,4 @@ struct fs_ops file_system[] = {
 	FS_OPS(ufs),
 };
 
-int nfsys = sizeof(file_system) / sizeof(file_system[0]);
+int nfsys = __arraycount(file_system);
