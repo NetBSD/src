163d5c04bb9e9df8c512c4f0ecb244348b39e563
dholland <dholland>
2009-08-25 06:56:52 +0000

    Whn ths cd ws wrttn, thr ws bt shrtg nd vwls wr xtrml xpnsv. Nowadays, however, we have an ample vowel budget, and bit shortages are a thing of the past (even in a down economy) so spend a bit to improve readability.

--- games/adventure/hdr.h
+++ games/adventure/hdr.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: hdr.h,v 1.12 2009/08/12 04:28:27 dholland Exp $	*/
+/*	$NetBSD: hdr.h,v 1.13 2009/08/25 06:56:52 dholland Exp $	*/
 
 /*-
  * Copyright (c) 1991, 1993
@@ -65,11 +65,11 @@ extern char data_file[];	/* Virtual data file */
 #define FLUSHLINE do { int flushline_ch; while ((flushline_ch = getchar()) != EOF && flushline_ch != '\n'); } while (0)
 #define FLUSHLF   while (next()!=LF)
 
-extern int     loc, newloc, oldloc, oldlc2, wzdark, gaveup, kq, k, k2;
+extern int     loc, newloc, oldloc, oldloc2, wasdark, gaveup, kq, k, k2;
 extern char   *wd1, *wd2;		/* the complete words */
 extern int     verb, obj, spk;
 extern int blklin;
-extern int     saveday, savet, mxscor, latncy;
+extern int     saveday, savet, maxscore, latency;
 
 #define SHORT 50		/* How short is a demo game? */
 
@@ -87,47 +87,47 @@ struct text {
 	int     txtlen;	/* length of msg starting here */
 };
 
-#define RTXSIZ  205
-extern	struct text rtext[RTXSIZ];	/* random text messages */
+#define RTXSIZE 205
+extern	struct text rtext[RTXSIZE];	/* random text messages */
 
-#define MAGSIZ  35
-extern	struct text mtext[MAGSIZ];	/* magic messages */
+#define MAGSIZE 35
+extern	struct text mtext[MAGSIZE];	/* magic messages */
 
-extern	int     clsses;
+extern	int     classes;
 #define CLSMAX  12
 extern	struct text ctext[CLSMAX];	/* classes of adventurer */
 extern	int     cval[CLSMAX];
 
 extern	struct text ptext[101];		/* object descriptions */
 
-#define LOCSIZ  141			/* number of locations */
-extern	struct text ltext[LOCSIZ];	/* long loc description */
-extern	struct text stext[LOCSIZ];	/* short loc descriptions */
+#define LOCSIZE 141			/* number of locations */
+extern	struct text ltext[LOCSIZE];	/* long loc description */
+extern	struct text stext[LOCSIZE];	/* short loc descriptions */
 
 extern	struct travlist {		/* direcs & conditions of travel */
 	struct travlist *next;	/* ptr to next list entry */
 	int     conditions;	/* m in writeup (newloc / 1000) */
 	int     tloc;		/* n in writeup (newloc % 1000) */
 	int     tverb;		/* the verb that takes you there */
-}      *travel[LOCSIZ], *tkk;	/* travel is closer to keys(...) */
+}      *travel[LOCSIZE], *tkk;	/* travel is closer to keys(...) */
 
-extern	int     atloc[LOCSIZ];
+extern	int     atloc[LOCSIZE];
 
 extern	int     plac[101];		/* initial object placement */
 extern	int     fixd[101], fixed[101];	/* location fixed? */
 
-extern	int     actspk[35];		/* rtext msg for verb <n> */
+extern	int     actspeak[35];		/* rtext msg for verb <n> */
 
-extern	int     cond[LOCSIZ];		/* various condition bits */
+extern	int     cond[LOCSIZE];		/* various condition bits */
 
 extern	int	setbit[16];		/* bit defn masks 1,2,4,... */
 
-extern	int     hntmax;
+extern	int     hintmax;
 extern	int     hints[20][5];		/* info on hints */
 extern	int     hinted[20], hintlc[20];
 
 extern	int     place[101], prop[101], links[201];
-extern	int     abb[LOCSIZ];
+extern	int     abb[LOCSIZE];
 
 extern	int     maxtrs, tally, tally2;	/* treasure values */
 
@@ -135,22 +135,22 @@ extern	int     maxtrs, tally, tally2;	/* treasure values */
 #define TRUE    1
 
 extern	int     keys, lamp, grate, cage, rod, rod2, steps,	/* mnemonics */
-	        bird, door, pillow, snake, fissur, tablet, clam, oyster,
-	        magzin, dwarf, knife, food, bottle, water, oil, plant, plant2,
-	        axe, mirror, dragon, chasm, troll, troll2, bear, messag,
-	        vend, batter, nugget, coins, chest, eggs, tridnt, vase,
-	        emrald, pyram, pearl, rug, chain, spices, back, look, cave,
-		null, entrnc, dprssn, /*enter, stream, pour,*/ say, lock, throw,
-	        find, invent;
+	        bird, door, pillow, snake, fissure, tablet, clam, oyster,
+	        magazine, dwarf, knife, food, bottle, water, oil, plant, plant2,
+	        axe, mirror, dragon, chasm, troll, troll2, bear, message,
+	        vend, batter, nugget, coins, chest, eggs, trident, vase,
+	        emerald, pyramid, pearl, rug, chain, spices, back, look, cave,
+	        null, entrance, depression, /*enter, stream, pour,*/ say, lock,
+	        throw, find, invent;
 
 extern	int     chloc, chloc2, dseen[7], dloc[7],	/* dwarf stuff */
-	        odloc[7], dflag, daltlc;
+	        odloc[7], dflag, daltloc;
 
 extern	int     tk[21], stick, dtotal, attack;
 extern	int     turns, lmwarn, iwest, knfloc, detail,	/* various flags and
 							 * counters */
-	        abbnum, maxdie, numdie, holdng, dkill, foobar, bonus, clock1,
-	        clock2, saved, closng, panic, closed, scorng;
+	        abbnum, maxdie, numdie, holding, dkill, foobar, bonus, clock1,
+	        clock2, saved, isclosing, panic, closed, scoring;
 
 extern	int     demo, limit;
 
