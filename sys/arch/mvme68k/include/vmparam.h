50642b1f7455dca716eb0f67067201cb8b2cc5cf
thorpej <thorpej>
2009-08-26 00:30:01 +0000

    Minor tweak to the pv_table management in the Hibler-derived m68k pmaps: The head of the list is now a pv_header, which contains the first pv_entry as well as a 16-bit attributes field (replaces the pmap_attributes array plus the pv_entry::pv_flags field) as a 16-bit count of caller-specified cache-inhibited mappings.
    Tested on hp300 (shared pmap_motorola.c), changes to atari and amiga copies
    are purely mechanical.

--- sys/arch/mvme68k/include/vmparam.h
+++ sys/arch/mvme68k/include/vmparam.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: vmparam.h,v 1.29 2009/03/06 20:31:50 joerg Exp $	*/
+/*	$NetBSD: vmparam.h,v 1.30 2009/08/26 00:30:03 thorpej Exp $	*/
 
 /*
  * Copyright (c) 1982, 1986, 1990, 1993
@@ -174,8 +174,7 @@
  * pmap-specific data stored in the vm_physmem[] array.
  */
 struct pmap_physseg {
-	struct pv_entry *pvent;		/* pv table for this seg */
-	char *attrs;			/* page attributes for this seg */
+	struct pv_header *pvheader;	/* pv table for this seg */
 };
 
 #endif /* _MVME68K_VMPARAM_H_ */
