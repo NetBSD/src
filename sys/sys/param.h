9b53d1f76516a85b2868788d2613921d211728d9
tls <tls@NetBSD.org>
2009-09-02 15:45:10 +0000

    Bump __NetBSD_Version__ for previous, per ad@ -- welcome to 5.99.16.

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.350 2009/06/29 05:47:35 dholland Exp $	*/
+/*	$NetBSD: param.h,v 1.351 2009/09/02 15:45:10 tls Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -63,7 +63,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	599001500	/* NetBSD 5.99.15 */
+#define	__NetBSD_Version__	599001600	/* NetBSD 5.99.16 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
