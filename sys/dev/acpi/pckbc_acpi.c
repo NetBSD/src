08d42a7d402b46b546ea62553ee6565a4d2c6d8f
bouyer <bouyer@NetBSD.org>
2009-09-05 13:10:18 +0000

    Pull up following revision(s) (requested by jmcneill in ticket #939): 	sys/dev/acpi/pckbc_acpi.c: revision 1.32 PR# port-i386/39671: panic while booting with an acpi kernel on a 790GX board If the firmware describes duplicate keyboard controller nodes, don't panic when the driver fails to map registers.

--- sys/dev/acpi/pckbc_acpi.c
+++ sys/dev/acpi/pckbc_acpi.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: pckbc_acpi.c,v 1.30 2008/09/16 11:24:55 pgoyette Exp $	*/
+/*	$NetBSD: pckbc_acpi.c,v 1.30.10.1 2009/09/05 13:10:18 bouyer Exp $	*/
 
 /*-
  * Copyright (c) 2000 The NetBSD Foundation, Inc.
@@ -42,7 +42,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: pckbc_acpi.c,v 1.30 2008/09/16 11:24:55 pgoyette Exp $");
+__KERNEL_RCSID(0, "$NetBSD: pckbc_acpi.c,v 1.30.10.1 2009/09/05 13:10:18 bouyer Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -207,8 +207,11 @@ pckbc_acpi_attach(device_t parent, device_t self, void *aux)
 			if (bus_space_map(aa->aa_iot, io0->ar_base,
 					  io0->ar_length, 0, &ioh_d) ||
 			    bus_space_map(aa->aa_iot, io1->ar_base,
-					  io1->ar_length, 0, &ioh_c))
-				panic("pckbc_acpi_attach: couldn't map");
+					  io1->ar_length, 0, &ioh_c)) {
+				aprint_error_dev(self,
+				    "unable to map registers\n");
+				goto out;
+			}
 
 			t = malloc(sizeof(struct pckbc_internal),
 			    M_DEVBUF, M_WAITOK|M_ZERO);
@@ -229,9 +232,11 @@ pckbc_acpi_attach(device_t parent, device_t self, void *aux)
 
 		first->sc_pckbc.intr_establish = pckbc_acpi_intr_establish;
 		config_defer(first->sc_pckbc.sc_dv, pckbc_acpi_finish_attach);
-	} else if (!pmf_device_register(self, NULL, NULL))
+	}
+
+out:
+	if (!pmf_device_register(self, NULL, NULL))
 		aprint_error_dev(self, "couldn't establish power handler\n");
- out:
 	acpi_resource_cleanup(&res);
 }
 
