83cd35d1a1fcb062b7fb82eb56b8c9413e176ae0
pooka <pooka@NetBSD.org>
2009-09-06 20:02:49 +0000

    add a very simple version of pollsock() XXX: it seems to exist purely for the pleasure of netsmb??

---
+++ sys/rump/librump/rumpnet/net_emul.c
@@ -0,0 +1,66 @@
+/*	$NetBSD: net_emul.c,v 1.1 2009/09/06 20:02:49 pooka Exp $	*/
+
+/*
+ * Copyright (c) 2009 Antti Kantee.  All Rights Reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS
+ * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <sys/cdefs.h>
+__KERNEL_RCSID(0, "$NetBSD: net_emul.c,v 1.1 2009/09/06 20:02:49 pooka Exp $");
+
+#include <sys/param.h>
+#include <sys/kernel.h>
+#include <sys/proc.h>
+#include <sys/socket.h>
+#include <sys/socketvar.h>
+#include <sys/time.h>
+
+/*
+ * A very simplistic (and polling ;) implementation of pollsock().
+ * Hopefully I'll soon be able to run sys_select.c
+ */
+
+int
+pollsock(struct socket *so, const struct timespec *tsp, int events)
+{
+	struct timespec ts, sts;
+
+	if (tsp) {
+		ts = *tsp;
+		if (inittimeleft(&ts, &sts) == -1)
+			return EINVAL;
+	}
+
+	for (;;) {
+		if (tsp) {
+			if (gettimeleft(&ts, &sts) < 0) {
+				break;
+			}
+		}
+		if (sopoll(so, events) != 0)
+			break;
+		kpause("lol", false, hz/10, 0);
+	}
+
+	return 0;
+}
