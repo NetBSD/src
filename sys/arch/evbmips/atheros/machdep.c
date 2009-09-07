87278dd2e1e52965c59860d0ad3884f08c2ef845
matt <matt@NetBSD.org>
2009-09-07 23:20:28 +0000

    Update to deal with pcb_context being a label_t (this should be a common routine).

--- sys/arch/evbmips/atheros/machdep.c
+++ sys/arch/evbmips/atheros/machdep.c
@@ -1,4 +1,4 @@
-/* $NetBSD: machdep.c,v 1.13 2008/07/02 17:28:55 ad Exp $ */
+/* $NetBSD: machdep.c,v 1.13.10.1 2009/09/07 23:20:29 matt Exp $ */
 
 /*
  * Copyright (c) 2006 Urbana-Champaign Independent Media Center.
@@ -147,7 +147,7 @@
  */
 
 #include <sys/cdefs.h>			/* RCS ID & Copyright macro defns */
-__KERNEL_RCSID(0, "$NetBSD: machdep.c,v 1.13 2008/07/02 17:28:55 ad Exp $");
+__KERNEL_RCSID(0, "$NetBSD: machdep.c,v 1.13.10.1 2009/09/07 23:20:29 matt Exp $");
 
 #include "opt_ddb.h"
 #include "opt_kgdb.h"
@@ -312,7 +312,13 @@ mach_init(void)
 	v = (void *) uvm_pageboot_alloc(USPACE);
 	lwp0.l_addr = proc0paddr = (struct user *)v;
 	lwp0.l_md.md_regs = (struct frame *)((char *)v + USPACE) - 1;
-	proc0paddr->u_pcb.pcb_context[11] =
+#ifdef _LP64
+	lwp0.l_md.md_regs->f_regs[_R_SR] = MIPS_SR_KX;
+#endif
+	lwp0.l_addr->u_pcb.pcb_context.val[_L_SR] =
+#ifdef _LP64
+	    MIPS_SR_KX |
+#endif
 	    MIPS_INT_MASK | MIPS_SR_INT_IE; /* SR */
 
 	/*
