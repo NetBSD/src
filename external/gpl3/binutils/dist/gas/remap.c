77fe790a9e76b940ed4f0f1c8392655a76c33201
tron <tron@NetBSD.org>
2009-09-11 10:41:42 +0000

    Avoid using alloca(3) to unbreak SSP builds.

--- external/gpl3/binutils/dist/gas/remap.c
+++ external/gpl3/binutils/dist/gas/remap.c
@@ -82,8 +82,8 @@ remap_debug_filename (const char *filename)
     return filename;
   name = filename + map->old_len;
   name_len = strlen (name) + 1;
-  s = (char *) alloca (name_len + map->new_len);
+  s = (char *) xmalloc (name_len + map->new_len);
   memcpy (s, map->new_prefix, map->new_len);
   memcpy (s + map->new_len, name, name_len);
-  return xstrdup (s);
+  return s;
 }
