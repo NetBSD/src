662dfba47df81cbc7c566f47a730a676e500062b
dyoung <dyoung@NetBSD.org>
2009-09-15 18:37:02 +0000

    To ease a future device_t/softc split, se device_private() instead of casting device_t to softc.

--- sys/dev/pcmcia/if_awi_pcmcia.c
+++ sys/dev/pcmcia/if_awi_pcmcia.c
@@ -1,4 +1,4 @@
-/* $NetBSD: if_awi_pcmcia.c,v 1.43 2009/05/12 14:42:18 cegger Exp $ */
+/* $NetBSD: if_awi_pcmcia.c,v 1.44 2009/09/15 18:37:02 dyoung Exp $ */
 
 /*-
  * Copyright (c) 1999, 2004 The NetBSD Foundation, Inc.
@@ -38,7 +38,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: if_awi_pcmcia.c,v 1.43 2009/05/12 14:42:18 cegger Exp $");
+__KERNEL_RCSID(0, "$NetBSD: if_awi_pcmcia.c,v 1.44 2009/09/15 18:37:02 dyoung Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -181,7 +181,7 @@ static void
 awi_pcmcia_attach(device_t parent, device_t self,
     void *aux)
 {
-	struct awi_pcmcia_softc *psc = (void *)self;
+	struct awi_pcmcia_softc *psc = device_private(self);
 	struct awi_softc *sc = &psc->sc_awi;
 	struct pcmcia_attach_args *pa = aux;
 	struct pcmcia_config_entry *cfe;
@@ -241,7 +241,7 @@ fail:
 static int
 awi_pcmcia_detach(device_t self, int flags)
 {
-	struct awi_pcmcia_softc *psc = (struct awi_pcmcia_softc *)self;
+	struct awi_pcmcia_softc *psc = device_private(self);
 	int error;
 
 	if (psc->sc_state != AWI_PCMCIA_ATTACHED)
