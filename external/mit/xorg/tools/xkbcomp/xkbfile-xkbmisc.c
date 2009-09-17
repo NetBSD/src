f5750ab8e1d5fc1580e3db7612b17638cff8343e
snj <snj@NetBSD.org>
2009-09-17 04:24:26 +0000

    Apply patch (requested by mrg in ticket #958): Sync X.Org reachover build framework and setlists with the new X.Org.

---
+++ external/mit/xorg/tools/xkbcomp/xkbfile-xkbmisc.c
@@ -0,0 +1,8 @@
+/*	$NetBSD: xkbfile-xkbmisc.c,v 1.1.4.2 2009/09/17 04:24:42 snj Exp $	*/
+
+/* This file exists to differentiate between
+ * lib/xkbfile/xkbmisc.c and lib/X11/XKBMisc.c
+ * which causes objdir conflicts on case-insensitive filesystems
+ */
+
+#include <../../../libxkbfile/dist/src/xkbmisc.c>
