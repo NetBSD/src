51e15be515eb6000c7c115806dffd3fb2b7a8cf2
pooka <pooka>
2009-10-02 09:32:01 +0000

    * allow callers to store a private data pointer behind the ukfs handle * release reference on root vnode before unmounting   (and reaquire the root vnode if unmount fails) * return correct error value if unmount fails

--- lib/libukfs/ukfs.h
+++ lib/libukfs/ukfs.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: ukfs.h,v 1.9 2009/07/22 20:46:34 pooka Exp $	*/
+/*	$NetBSD: ukfs.h,v 1.10 2009/10/02 09:32:01 pooka Exp $	*/
 
 /*
  * Copyright (c) 2007, 2008  Antti Kantee.  All Rights Reserved.
@@ -108,6 +108,8 @@ int		ukfs_lutimes(struct ukfs *, const char *,
 
 struct mount	*ukfs_getmp(struct ukfs *);
 struct vnode	*ukfs_getrvp(struct ukfs *);
+void		ukfs_setspecific(struct ukfs *, void *);
+void *		ukfs_getspecific(struct ukfs *);
 
 /* dynamic loading of library modules */
 int		ukfs_modload(const char *);
