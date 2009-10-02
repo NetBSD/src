937f07f69266b68f5356292febab2870afe8498a
elad <elad@NetBSD.org>
2009-10-02 22:46:18 +0000

    Stick nice policy in its own subsystem and call the listener "resource" rather than "rlimit"...

--- sys/secmodel/suser/secmodel_suser.c
+++ sys/secmodel/suser/secmodel_suser.c
@@ -1,4 +1,4 @@
-/* $NetBSD: secmodel_suser.c,v 1.6 2009/10/02 22:38:45 elad Exp $ */
+/* $NetBSD: secmodel_suser.c,v 1.7 2009/10/02 22:46:18 elad Exp $ */
 /*-
  * Copyright (c) 2006 Elad Efrat <elad@NetBSD.org>
  * All rights reserved.
@@ -38,7 +38,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: secmodel_suser.c,v 1.6 2009/10/02 22:38:45 elad Exp $");
+__KERNEL_RCSID(0, "$NetBSD: secmodel_suser.c,v 1.7 2009/10/02 22:46:18 elad Exp $");
 
 #include <sys/types.h>
 #include <sys/param.h>
@@ -645,19 +645,7 @@ secmodel_suser_process_cb(kauth_cred_t cred, kauth_action_t action,
 		break;
 
 	case KAUTH_PROCESS_NICE:
-		if (isroot) {
-			result = KAUTH_RESULT_ALLOW;
-			break;
-		}
-
-		if (kauth_cred_geteuid(cred) !=
-		    kauth_cred_geteuid(p->p_cred) &&
-		    kauth_cred_getuid(cred) !=
-		    kauth_cred_geteuid(p->p_cred)) {
-			break;
-		}
-
-		if ((u_long)arg1 >= p->p_nice)
+		if (isroot)
 			result = KAUTH_RESULT_ALLOW;
 
 		break;
