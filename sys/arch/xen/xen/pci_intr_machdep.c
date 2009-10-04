96f6d0bd870253537496667655998ce5a61ce692
snj <snj@NetBSD.org>
2009-10-04 00:03:19 +0000

    Pull up following revision(s) (requested by bouyer in ticket #1054): 	sys/arch/xen/x86/intr.c: revision 1.22 	sys/arch/xen/xen/isa_machdep.c: revision 1.14 	sys/arch/xen/xen/pci_intr_machdep.c: revision 1.9 	sys/arch/xen/xen/pciide_machdep.c: revision 1.12 When ioapic is used, for ISA interrupts, reuse the legacy ISA interrupt number instead of allocating a new one. Force allocating a new interrupt number for PCI devices, as the number stored in the PCI interrupt register may be wrong. This should help using a pciide controller in compat mode or ISA devices in a non-0 domain.

--- sys/arch/xen/xen/pci_intr_machdep.c
+++ sys/arch/xen/xen/pci_intr_machdep.c
@@ -1,4 +1,4 @@
-/*      $NetBSD: pci_intr_machdep.c,v 1.7.6.1 2009/10/03 23:54:05 snj Exp $      */
+/*      $NetBSD: pci_intr_machdep.c,v 1.7.6.2 2009/10/04 00:03:20 snj Exp $      */
 
 /*
  * Copyright (c) 2005 Manuel Bouyer.
@@ -31,7 +31,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: pci_intr_machdep.c,v 1.7.6.1 2009/10/03 23:54:05 snj Exp $");
+__KERNEL_RCSID(0, "$NetBSD: pci_intr_machdep.c,v 1.7.6.2 2009/10/04 00:03:20 snj Exp $");
 
 #include <sys/types.h>
 #include <sys/param.h>
@@ -109,8 +109,8 @@ pci_intr_map(struct pci_attach_args *pa, pci_intr_handle_t *ihp)
 	pci_decompose_tag(pc, pa->pa_tag, &bus, &dev, &func);
 	if (mp_busses != NULL) {
 		if (intr_find_mpmapping(bus, (dev<<2)|(rawpin-1), ihp) == 0) {
-			if ((ihp->pirq & 0xff) == 0)
-				ihp->pirq |= line;
+			/* make sure a new IRQ will be allocated */
+			ihp->pirq &= ~0xff;
 			goto end;
 		}
 		/*
