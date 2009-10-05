dc45f8201b548b18b6780bc67d2d5d232848c600
sborrill <sborrill@NetBSD.org>
2009-10-05 11:46:21 +0000

    Pull up the following revisions(s) (requested by pooka in ticket #1047): 	include/util.h:	revision 1.52 	lib/libutil/opendisk.c:	revision 1.11 Add opendisk1(), which functions like opendisk(), but takes a function pointer to the routine to be used for open().

--- include/util.h
+++ include/util.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: util.h,v 1.49 2007/12/14 16:36:19 christos Exp $	*/
+/*	$NetBSD: util.h,v 1.49.12.1 2009/10/05 11:46:21 sborrill Exp $	*/
 
 /*-
  * Copyright (c) 1995
@@ -80,6 +80,8 @@ int		logoutx(const char *, int, int);
 void		logwtmp(const char *, const char *, const char *);
 void		logwtmpx(const char *, const char *, const char *, int, int);
 int		opendisk(const char *, int, char *, size_t, int);
+int		opendisk1(const char *, int, char *, size_t, int,
+			  int (*)(const char *, int, mode_t));
 int		openpty(int *, int *, char *, struct termios *,
 		    struct winsize *);
 time_t		parsedate(const char *, const time_t *, const int *);
