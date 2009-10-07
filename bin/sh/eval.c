10aa8c24e96ca8fb78c0f43c9b09cf138c528aa9
christos <christos@NetBSD.org>
2009-10-07 18:12:11 +0000

    only for when trap if we are going to exit.

--- bin/sh/eval.c
+++ bin/sh/eval.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: eval.c,v 1.97 2009/10/06 20:05:10 christos Exp $	*/
+/*	$NetBSD: eval.c,v 1.98 2009/10/07 18:12:11 christos Exp $	*/
 
 /*-
  * Copyright (c) 1993
@@ -37,7 +37,7 @@
 #if 0
 static char sccsid[] = "@(#)eval.c	8.9 (Berkeley) 6/8/95";
 #else
-__RCSID("$NetBSD: eval.c,v 1.97 2009/10/06 20:05:10 christos Exp $");
+__RCSID("$NetBSD: eval.c,v 1.98 2009/10/07 18:12:11 christos Exp $");
 #endif
 #endif /* not lint */
 
@@ -821,7 +821,7 @@ evalcommand(union node *cmd, int flgs, struct backcmd *backcmd)
 	}
 
 	/* Fork off a child process if necessary. */
-	if (cmd->ncmd.backgnd || trap[0]
+	if (cmd->ncmd.backgnd || (trap[0] && (flags & EV_EXIT) != 0)
 	 || (cmdentry.cmdtype == CMDNORMAL && (flags & EV_EXIT) == 0)
 	 || ((flags & EV_BACKCMD) != 0
 	    && ((cmdentry.cmdtype != CMDBUILTIN && cmdentry.cmdtype != CMDSPLBLTIN)
