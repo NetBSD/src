075e083e3215abafd5b55dbacca9d9b332aa3073
sborrill <sborrill@NetBSD.org>
2009-10-11 18:03:21 +0000

    Pull up the following revisions(s) (requested by dsl in ticket #1087): 	sys/kern/tty.c:	revision 1.234
    Check for zero length read here - and return zero. Without this there is a
    simple local-user panic in ureadc().

--- sys/kern/tty.c
+++ sys/kern/tty.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: tty.c,v 1.227.4.1 2009/02/06 02:05:18 snj Exp $	*/
+/*	$NetBSD: tty.c,v 1.227.4.2 2009/10/11 18:03:21 sborrill Exp $	*/
 
 /*-
  * Copyright (c) 2008 The NetBSD Foundation, Inc.
@@ -63,7 +63,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: tty.c,v 1.227.4.1 2009/02/06 02:05:18 snj Exp $");
+__KERNEL_RCSID(0, "$NetBSD: tty.c,v 1.227.4.2 2009/10/11 18:03:21 sborrill Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -1654,6 +1654,9 @@ ttread(struct tty *tp, struct uio *uio, int flag)
 	int		c, first, error, has_stime, last_cc;
 	long		lflag, slp;
 	struct timeval	now, stime;
+
+	if (uio->uio_resid == 0)
+		return 0;
 
 	stime.tv_usec = 0;	/* XXX gcc */
 	stime.tv_sec = 0;	/* XXX gcc */
