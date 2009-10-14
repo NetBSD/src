771cb87ff9744a9264ab7913a09305e6cad96945
pooka <pooka>
2009-10-14 17:28:13 +0000

    generate rump local interfaces

---
+++ sys/rump/include/rump/rumpnet_if_pub.h
@@ -0,0 +1,9 @@
+/*	$NetBSD: rumpnet_if_pub.h,v 1.1 2009/10/14 17:28:13 pooka Exp $	*/
+
+/*
+ * Automatically generated.  DO NOT EDIT.
+ * from: NetBSD: rumpnet.ifspec,v 1.1 2009/10/14 17:17:00 pooka Exp 
+ * by:   NetBSD: makerumpif.sh,v 1.2 2009/10/14 17:26:09 pooka Exp 
+ */
+
+int rump_virtif_create(int);
---
+++ sys/rump/librump/rumpvfs/rumpvfs_if_wrappers.c
@@ -0,0 +1,325 @@
+/*	$NetBSD: rumpvfs_if_wrappers.c,v 1.1 2009/10/14 17:28:14 pooka Exp $	*/
+
+/*
+ * Automatically generated.  DO NOT EDIT.
+ * from: NetBSD: rumpvfs.ifspec,v 1.1 2009/10/14 17:17:00 pooka Exp 
+ * by:   NetBSD: makerumpif.sh,v 1.2 2009/10/14 17:26:09 pooka Exp 
+ */
+
+#include <sys/cdefs.h>
+#include <sys/systm.h>
+
+#include <rump/rump.h>
+#include <rump/rumpvfs_if_pub.h>
+
+#include "rumpvfs_if_priv.h"
+
+void __dead rump_vfs_unavailable(void);
+void __dead
+rump_vfs_unavailable(void)
+{
+
+	panic("vfs interface unavailable");
+}
+
+void
+rump_getvninfo(struct vnode *arg1, enum vtype *arg2, off_t *arg3, dev_t *arg4)
+{
+
+	rumppriv_getvninfo(arg1, arg2, arg3, arg4);
+}
+
+struct vfsops *
+rump_vfslist_iterate(struct vfsops *arg1)
+{
+	struct vfsops * rv;
+
+	rv = rumppriv_vfslist_iterate(arg1);
+
+	return rv;
+}
+
+struct vfsops *
+rump_vfs_getopsbyname(const char *arg1)
+{
+	struct vfsops * rv;
+
+	rv = rumppriv_vfs_getopsbyname(arg1);
+
+	return rv;
+}
+
+struct vattr *
+rump_vattr_init(void)
+{
+	struct vattr * rv;
+
+	rv = rumppriv_vattr_init();
+
+	return rv;
+}
+
+void
+rump_vattr_settype(struct vattr *arg1, enum vtype arg2)
+{
+
+	rumppriv_vattr_settype(arg1, arg2);
+}
+
+void
+rump_vattr_setmode(struct vattr *arg1, mode_t arg2)
+{
+
+	rumppriv_vattr_setmode(arg1, arg2);
+}
+
+void
+rump_vattr_setrdev(struct vattr *arg1, dev_t arg2)
+{
+
+	rumppriv_vattr_setrdev(arg1, arg2);
+}
+
+void
+rump_vattr_free(struct vattr *arg1)
+{
+
+	rumppriv_vattr_free(arg1);
+}
+
+void
+rump_vp_incref(struct vnode *arg1)
+{
+
+	rumppriv_vp_incref(arg1);
+}
+
+int
+rump_vp_getref(struct vnode *arg1)
+{
+	int rv;
+
+	rv = rumppriv_vp_getref(arg1);
+
+	return rv;
+}
+
+void
+rump_vp_rele(struct vnode *arg1)
+{
+
+	rumppriv_vp_rele(arg1);
+}
+
+void
+rump_vp_interlock(struct vnode *arg1)
+{
+
+	rumppriv_vp_interlock(arg1);
+}
+
+int
+rump_etfs_register(const char *arg1, const char *arg2, enum rump_etfs_type arg3)
+{
+	int rv;
+
+	rv = rumppriv_etfs_register(arg1, arg2, arg3);
+
+	return rv;
+}
+
+int
+rump_etfs_register_withsize(const char *arg1, const char *arg2, enum rump_etfs_type arg3, uint64_t arg4, uint64_t arg5)
+{
+	int rv;
+
+	rv = rumppriv_etfs_register_withsize(arg1, arg2, arg3, arg4, arg5);
+
+	return rv;
+}
+
+int
+rump_etfs_remove(const char *arg1)
+{
+	int rv;
+
+	rv = rumppriv_etfs_remove(arg1);
+
+	return rv;
+}
+
+void
+rump_freecn(struct componentname *arg1, int arg2)
+{
+
+	rumppriv_freecn(arg1, arg2);
+}
+
+int
+rump_checksavecn(struct componentname *arg1)
+{
+	int rv;
+
+	rv = rumppriv_checksavecn(arg1);
+
+	return rv;
+}
+
+int
+rump_namei(uint32_t arg1, uint32_t arg2, const char *arg3, struct vnode **arg4, struct vnode **arg5, struct componentname **arg6)
+{
+	int rv;
+
+	rv = rumppriv_namei(arg1, arg2, arg3, arg4, arg5, arg6);
+
+	return rv;
+}
+
+struct componentname *
+rump_makecn(u_long arg1, u_long arg2, const char *arg3, size_t arg4, kauth_cred_t arg5, struct lwp *arg6)
+{
+	struct componentname * rv;
+
+	rv = rumppriv_makecn(arg1, arg2, arg3, arg4, arg5, arg6);
+
+	return rv;
+}
+
+int
+rump_vfs_unmount(struct mount *arg1, int arg2)
+{
+	int rv;
+
+	rv = rumppriv_vfs_unmount(arg1, arg2);
+
+	return rv;
+}
+
+int
+rump_vfs_root(struct mount *arg1, struct vnode **arg2, int arg3)
+{
+	int rv;
+
+	rv = rumppriv_vfs_root(arg1, arg2, arg3);
+
+	return rv;
+}
+
+int
+rump_vfs_statvfs(struct mount *arg1, struct statvfs *arg2)
+{
+	int rv;
+
+	rv = rumppriv_vfs_statvfs(arg1, arg2);
+
+	return rv;
+}
+
+int
+rump_vfs_sync(struct mount *arg1, int arg2, kauth_cred_t arg3)
+{
+	int rv;
+
+	rv = rumppriv_vfs_sync(arg1, arg2, arg3);
+
+	return rv;
+}
+
+int
+rump_vfs_fhtovp(struct mount *arg1, struct fid *arg2, struct vnode **arg3)
+{
+	int rv;
+
+	rv = rumppriv_vfs_fhtovp(arg1, arg2, arg3);
+
+	return rv;
+}
+
+int
+rump_vfs_vptofh(struct vnode *arg1, struct fid *arg2, size_t *arg3)
+{
+	int rv;
+
+	rv = rumppriv_vfs_vptofh(arg1, arg2, arg3);
+
+	return rv;
+}
+
+void
+rump_vfs_syncwait(struct mount *arg1)
+{
+
+	rumppriv_vfs_syncwait(arg1);
+}
+
+int
+rump_vfs_getmp(const char *arg1, struct mount **arg2)
+{
+	int rv;
+
+	rv = rumppriv_vfs_getmp(arg1, arg2);
+
+	return rv;
+}
+
+void
+rump_rcvp_set(struct vnode *arg1, struct vnode *arg2)
+{
+
+	rumppriv_rcvp_set(arg1, arg2);
+}
+
+struct vnode *
+rump_cdir_get(void)
+{
+	struct vnode * rv;
+
+	rv = rumppriv_cdir_get();
+
+	return rv;
+}
+
+int
+rump_syspuffs_glueinit(int arg1, int *arg2)
+{
+	int rv;
+
+	rv = rumppriv_syspuffs_glueinit(arg1, arg2);
+
+	return rv;
+}
+__weak_alias(rumppriv_syspuffs_glueinit,rump_vfs_unavailable);
+
+int
+rump_sys___stat30(const char *arg1, struct stat *arg2)
+{
+	int rv;
+
+	rv = rumppriv_sys___stat30(arg1, arg2);
+
+	return rv;
+}
+
+int
+rump_sys___lstat30(const char *arg1, struct stat *arg2)
+{
+	int rv;
+
+	rv = rumppriv_sys___lstat30(arg1, arg2);
+
+	return rv;
+}
+
+void
+rump_vattr50_to_vattr(const struct vattr *arg1, struct vattr *arg2)
+{
+
+	rumppriv_vattr50_to_vattr(arg1, arg2);
+}
+
+void
+rump_vattr_to_vattr50(const struct vattr *arg1, struct vattr *arg2)
+{
+
+	rumppriv_vattr_to_vattr50(arg1, arg2);
+}
