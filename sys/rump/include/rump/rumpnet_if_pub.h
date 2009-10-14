5b34965989026b483f87cf3d9a9ae0204f062e66
pooka <pooka>
2009-10-14 18:16:41 +0000

    regen: put all public interfaces created by ifspec into a rump_pub namespace

--- sys/rump/include/rump/rumpnet_if_pub.h
+++ sys/rump/include/rump/rumpnet_if_pub.h
@@ -1,9 +1,9 @@
-/*	$NetBSD: rumpnet_if_pub.h,v 1.1 2009/10/14 17:28:13 pooka Exp $	*/
+/*	$NetBSD: rumpnet_if_pub.h,v 1.2 2009/10/14 18:16:41 pooka Exp $	*/
 
 /*
  * Automatically generated.  DO NOT EDIT.
  * from: NetBSD: rumpnet.ifspec,v 1.1 2009/10/14 17:17:00 pooka Exp 
- * by:   NetBSD: makerumpif.sh,v 1.2 2009/10/14 17:26:09 pooka Exp 
+ * by:   NetBSD: makerumpif.sh,v 1.3 2009/10/14 18:14:48 pooka Exp 
  */
 
-int rump_virtif_create(int);
+int rump_pub_virtif_create(int);
--- sys/rump/librump/rumpkern/rumpkern_if_priv.h
+++ sys/rump/librump/rumpkern/rumpkern_if_priv.h
@@ -1,27 +1,27 @@
-/*	$NetBSD: rumpkern_if_priv.h,v 1.1 2009/10/14 17:28:14 pooka Exp $	*/
+/*	$NetBSD: rumpkern_if_priv.h,v 1.2 2009/10/14 18:16:41 pooka Exp $	*/
 
 /*
  * Automatically generated.  DO NOT EDIT.
  * from: NetBSD: rumpkern.ifspec,v 1.1 2009/10/14 17:17:00 pooka Exp 
- * by:   NetBSD: makerumpif.sh,v 1.2 2009/10/14 17:26:09 pooka Exp 
+ * by:   NetBSD: makerumpif.sh,v 1.3 2009/10/14 18:14:48 pooka Exp 
  */
 
-void rumppriv_reboot(int);
-int rumppriv_getversion(void);
-int rumppriv_module_init(struct modinfo *, prop_dictionary_t);
-int rumppriv_module_fini(struct modinfo *);
-struct uio * rumppriv_uio_setup(void *, size_t, off_t, enum rump_uiorw);
-size_t rumppriv_uio_getresid(struct uio *);
-off_t rumppriv_uio_getoff(struct uio *);
-size_t rumppriv_uio_free(struct uio *);
-kauth_cred_t rumppriv_cred_create(uid_t, gid_t, size_t, gid_t *);
-kauth_cred_t rumppriv_cred_suserget(void);
-void rumppriv_cred_put(kauth_cred_t);
-struct lwp * rumppriv_newproc_switch(void);
-struct lwp * rumppriv_setup_curlwp(pid_t, lwpid_t, int);
-struct lwp * rumppriv_get_curlwp(void);
-void rumppriv_set_curlwp(struct lwp *);
-void rumppriv_clear_curlwp(void);
-int rumppriv_sysproxy_set(rump_sysproxy_t, void *);
-int rumppriv_sysproxy_socket_setup_client(int);
-int rumppriv_sysproxy_socket_setup_server(int);
+void rump_reboot(int);
+int rump_getversion(void);
+int rump_module_init(struct modinfo *, prop_dictionary_t);
+int rump_module_fini(struct modinfo *);
+struct uio * rump_uio_setup(void *, size_t, off_t, enum rump_uiorw);
+size_t rump_uio_getresid(struct uio *);
+off_t rump_uio_getoff(struct uio *);
+size_t rump_uio_free(struct uio *);
+kauth_cred_t rump_cred_create(uid_t, gid_t, size_t, gid_t *);
+kauth_cred_t rump_cred_suserget(void);
+void rump_cred_put(kauth_cred_t);
+struct lwp * rump_newproc_switch(void);
+struct lwp * rump_setup_curlwp(pid_t, lwpid_t, int);
+struct lwp * rump_get_curlwp(void);
+void rump_set_curlwp(struct lwp *);
+void rump_clear_curlwp(void);
+int rump_sysproxy_set(rump_sysproxy_t, void *);
+int rump_sysproxy_socket_setup_client(int);
+int rump_sysproxy_socket_setup_server(int);
