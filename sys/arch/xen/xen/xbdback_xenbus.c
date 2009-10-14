0079e064373bc73d9dc3d7fac2a44b8b6065365e
bouyer <bouyer@NetBSD.org>
2009-10-14 18:54:00 +0000

    Don't pass a NULL arg to DIOCCACHESYNC, sd(4) dereferences it. Pass a pointer to a int initialized to 1, so that a DIOCCACHESYNC will be forced on sd(4). Should fix dom0 panic reported by Sarton O'Brien.

--- sys/arch/xen/xen/xbdback_xenbus.c
+++ sys/arch/xen/xen/xbdback_xenbus.c
@@ -1,4 +1,4 @@
-/*      $NetBSD: xbdback_xenbus.c,v 1.25 2009/09/23 17:48:55 bouyer Exp $      */
+/*      $NetBSD: xbdback_xenbus.c,v 1.26 2009/10/14 18:54:00 bouyer Exp $      */
 
 /*
  * Copyright (c) 2006 Manuel Bouyer.
@@ -31,7 +31,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: xbdback_xenbus.c,v 1.25 2009/09/23 17:48:55 bouyer Exp $");
+__KERNEL_RCSID(0, "$NetBSD: xbdback_xenbus.c,v 1.26 2009/10/14 18:54:00 bouyer Exp $");
 
 #include <sys/types.h>
 #include <sys/param.h>
@@ -1316,9 +1316,10 @@ xbdback_do_io(struct work *wk, void *dummy)
 
 	if (xbd_io->xio_operation == BLKIF_OP_FLUSH_DISKCACHE) {
 		int error;
+		int force = 1;
 		struct xbdback_instance *xbdi = xbd_io->xio_xbdi;
 
-		error = VOP_IOCTL(xbdi->xbdi_vp, DIOCCACHESYNC, NULL, FWRITE,
+		error = VOP_IOCTL(xbdi->xbdi_vp, DIOCCACHESYNC, &force, FWRITE,
 		    kauth_cred_get());
 		if (error) {
 			aprint_error("xbdback %s: DIOCCACHESYNC returned %d\n",
