991bc1afbcf2583fd68eb7bbea18faac14b2b9d0
pooka <pooka@NetBSD.org>
2009-10-15 16:40:15 +0000

    regen: lwp interface changes

--- sys/rump/librump/rumpkern/rumpkern_if_priv.h
+++ sys/rump/librump/rumpkern/rumpkern_if_priv.h
@@ -1,8 +1,8 @@
-/*	$NetBSD: rumpkern_if_priv.h,v 1.3 2009/10/15 00:31:25 pooka Exp $	*/
+/*	$NetBSD: rumpkern_if_priv.h,v 1.4 2009/10/15 16:40:15 pooka Exp $	*/
 
 /*
  * Automatically generated.  DO NOT EDIT.
- * from: NetBSD: rumpkern.ifspec,v 1.1 2009/10/14 17:17:00 pooka Exp 
+ * from: NetBSD: rumpkern.ifspec,v 1.2 2009/10/15 16:39:22 pooka Exp 
  * by:   NetBSD: makerumpif.sh,v 1.4 2009/10/15 00:29:19 pooka Exp 
  */
 
@@ -18,10 +18,11 @@ kauth_cred_t rump_cred_create(uid_t, gid_t, size_t, gid_t *);
 kauth_cred_t rump_cred_suserget(void);
 void rump_cred_put(kauth_cred_t);
 struct lwp * rump_newproc_switch(void);
-struct lwp * rump_setup_curlwp(pid_t, lwpid_t, int);
-struct lwp * rump_get_curlwp(void);
-void rump_set_curlwp(struct lwp *);
-void rump_clear_curlwp(void);
+struct lwp * rump_lwp_alloc(pid_t, lwpid_t);
+struct lwp * rump_lwp_alloc_and_switch(pid_t, lwpid_t);
+struct lwp * rump_lwp_curlwp(void);
+void rump_lwp_switch(struct lwp *);
+void rump_lwp_release(struct lwp *);
 int rump_sysproxy_set(rump_sysproxy_t, void *);
 int rump_sysproxy_socket_setup_client(int);
 int rump_sysproxy_socket_setup_server(int);
