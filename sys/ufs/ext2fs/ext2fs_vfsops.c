ff84ed32998556675f6cc954530c424f1e7cc01f
snj <snj>
2009-10-16 05:51:03 +0000

    Pull up following revision(s) (requested by tsutsui in ticket #1060): 	sys/ufs/ext2fs/ext2fs_vfsops.c: revision 1.148 Add a missed brelse(9) call after bread(9) in ext2fs_reload(). This may close PR kern/28712 (ext2fs hang on mount after fsck).

--- sys/ufs/ext2fs/ext2fs_vfsops.c
+++ sys/ufs/ext2fs/ext2fs_vfsops.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: ext2fs_vfsops.c,v 1.137.6.3 2009/10/16 05:50:05 snj Exp $	*/
+/*	$NetBSD: ext2fs_vfsops.c,v 1.137.6.4 2009/10/16 05:51:03 snj Exp $	*/
 
 /*
  * Copyright (c) 1989, 1991, 1993, 1994
@@ -65,7 +65,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: ext2fs_vfsops.c,v 1.137.6.3 2009/10/16 05:50:05 snj Exp $");
+__KERNEL_RCSID(0, "$NetBSD: ext2fs_vfsops.c,v 1.137.6.4 2009/10/16 05:51:03 snj Exp $");
 
 #if defined(_KERNEL_OPT)
 #include "opt_compat_netbsd.h"
@@ -580,6 +580,7 @@ ext2fs_reload(struct mount *mountp, kauth_cred_t cred)
 	    howmany(fs->e2fs_ncg, fs->e2fs_bsize / sizeof(struct ext2_gd));
 	fs->e2fs_ipb = fs->e2fs_bsize / EXT2_DINODE_SIZE;
 	fs->e2fs_itpg = fs->e2fs.e2fs_ipg / fs->e2fs_ipb;
+	brelse(bp, 0);
 
 	/*
 	 * Step 3: re-read summary information from disk.
