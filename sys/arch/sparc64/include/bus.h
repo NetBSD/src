b6c6491592feca6c4b913bc66e2d577dc67846f9
bouyer <bouyer@NetBSD.org>
2009-10-18 14:44:12 +0000

    Back out ticket 967

--- sys/arch/sparc64/include/bus.h
+++ sys/arch/sparc64/include/bus.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: bus.h,v 1.58.10.1 2009/10/18 13:34:49 bouyer Exp $	*/
+/*	$NetBSD: bus.h,v 1.58.10.2 2009/10/18 14:44:12 bouyer Exp $	*/
 
 /*-
  * Copyright (c) 1996, 1997, 1998, 2001 The NetBSD Foundation, Inc.
@@ -307,8 +307,7 @@ bus_intr_establish(bus_space_tag_t t, int p, int l, int	(*h)(void *), void *a)
 #define	BUS_SPACE_MAP_BUS2		0x0200
 #define	BUS_SPACE_MAP_BUS3		0x0400
 #define	BUS_SPACE_MAP_BUS4		0x0800
-/* sparc uses this, it's not supposed to do anything on sparc64 */
-#define BUS_SPACE_MAP_LARGE		0
+
 
 /* flags for bus_space_barrier() */
 #define	BUS_SPACE_BARRIER_READ	0x01		/* force read barrier */
