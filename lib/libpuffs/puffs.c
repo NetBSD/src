dcda1d6289cffd941e08c23d32af59c695544d66
sborrill <sborrill>
2009-10-18 12:46:07 +0000

    Pull up the following revisions(s) (requested by pooka in ticket #1100): 	lib/libpuffs/dispatcher.c:	revision 1.33 	lib/libpuffs/puffs.c:	revision 1.99 	lib/libpuffs/puffs.h:	revision 1.111 	sys/fs/puffs/puffs_msgif.h:	revision 1.67 via patch 	sys/fs/puffs/puffs_vnops.c:	revision 1.136
    Support VOP_ABORTOP() in puffs.

--- lib/libpuffs/puffs.c
+++ lib/libpuffs/puffs.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: puffs.c,v 1.92.4.2 2009/10/16 12:10:54 sborrill Exp $	*/
+/*	$NetBSD: puffs.c,v 1.92.4.3 2009/10/18 12:46:07 sborrill Exp $	*/
 
 /*
  * Copyright (c) 2005, 2006, 2007  Antti Kantee.  All Rights Reserved.
@@ -31,7 +31,7 @@
 
 #include <sys/cdefs.h>
 #if !defined(lint)
-__RCSID("$NetBSD: puffs.c,v 1.92.4.2 2009/10/16 12:10:54 sborrill Exp $");
+__RCSID("$NetBSD: puffs.c,v 1.92.4.3 2009/10/18 12:46:07 sborrill Exp $");
 #endif /* !lint */
 
 #include <sys/param.h>
@@ -99,6 +99,7 @@ fillvnopmask(struct puffs_ops *pops, uint8_t *opmask)
 	FILLOP(print,    PRINT);
 	FILLOP(read,     READ);
 	FILLOP(write,    WRITE);
+	FILLOP(abortop,  ABORTOP);
 }
 #undef FILLOP
 
@@ -584,20 +585,14 @@ do {									\
 	return rv;
 }
 
+/*ARGSUSED*/
 struct puffs_usermount *
-_puffs_init(int develv, struct puffs_ops *pops, const char *mntfromname,
+_puffs_init(int dummy, struct puffs_ops *pops, const char *mntfromname,
 	const char *puffsname, void *priv, uint32_t pflags)
 {
 	struct puffs_usermount *pu;
 	struct puffs_kargs *pargs;
 	int sverrno;
-
-	if (develv != PUFFS_DEVEL_LIBVERSION) {
-		warnx("puffs_init: mounting with lib version %d, need %d",
-		    develv, PUFFS_DEVEL_LIBVERSION);
-		errno = EINVAL;
-		return NULL;
-	}
 
 	pu = malloc(sizeof(struct puffs_usermount));
 	if (pu == NULL)
