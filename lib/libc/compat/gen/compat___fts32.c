64322e573db50c1852360bf918b1ce26d09e26a4
christos <christos@NetBSD.org>
2009-10-19 17:52:01 +0000

    PR/42201: NAKAJIMA Yoshihiro: fts(3): broken compatibility As new field types were added, we did not provide overrides for older compat versions. This restores compatibility.

--- lib/libc/compat/gen/compat___fts32.c
+++ lib/libc/compat/gen/compat___fts32.c
@@ -1,10 +1,10 @@
-/*	$NetBSD: compat___fts32.c,v 1.2 2009/01/11 02:46:25 christos Exp $	*/
+/*	$NetBSD: compat___fts32.c,v 1.3 2009/10/19 17:52:05 christos Exp $	*/
 
 #include "namespace.h"
 #include <sys/cdefs.h>
 #include <dirent.h>
 
-#define __LIBC12_SOURCE__
+#define	__LIBC12_SOURCE__
 __warn_references(__fts_children32,
     "warning: reference to compatibility __fts_children32();"
     " include <fts.h> for correct reference")
@@ -23,16 +23,18 @@ __warn_references(__fts_set32,
 
 #define	__fts_stat_t	struct stat30
 #define	__fts_dev_t	uint32_t
-#undef fts_children
-#define fts_children __fts_children32
-#undef fts_close
-#define fts_close __fts_close32
-#undef fts_open
-#define fts_open  __fts_open32
-#undef fts_read
-#define fts_read __fts_read32
-#undef fts_set
-#define fts_set __fts_set32
+#define	__fts_level_t	short
+
+#undef	fts_children
+#define	fts_children __fts_children32
+#undef	fts_close
+#define	fts_close __fts_close32
+#undef	fts_open
+#define	fts_open  __fts_open32
+#undef	fts_read
+#define	fts_read __fts_read32
+#undef	fts_set
+#define	fts_set __fts_set32
 
 #include <sys/time.h>
 #include <compat/sys/time.h>
@@ -42,10 +44,10 @@ __warn_references(__fts_set32,
 #include <fts.h>
 #include <compat/include/fts.h>
 
-#define	__FTS_COMPAT_LENGTH
+#define	__FTS_COMPAT_LEVEL
 
-#define stat		__stat30
-#define lstat		__lstat30
-#define fstat		__fstat30
+#define	stat		__stat30
+#define	lstat		__lstat30
+#define	fstat		__fstat30
 
 #include "gen/fts.c"
