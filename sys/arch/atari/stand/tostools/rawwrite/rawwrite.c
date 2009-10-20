ee8ebaca7b5885b436f43b95386f7ca6e5314b37
snj <snj@NetBSD.org>
2009-10-20 19:10:09 +0000

    Remove 3rd and 4th clause on Leo Weppelman's license.  OK leo@.

--- sys/arch/atari/stand/tostools/rawwrite/rawwrite.c
+++ sys/arch/atari/stand/tostools/rawwrite/rawwrite.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: rawwrite.c,v 1.10 2009/03/18 10:22:26 cegger Exp $	*/
+/*	$NetBSD: rawwrite.c,v 1.11 2009/10/20 19:10:11 snj Exp $	*/
 
 /*
  * Copyright (c) 1995 Leo Weppelman.
@@ -12,11 +12,6 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *      This product includes software developed by Leo Weppelman.
- * 4. The name of the author may not be used to endorse or promote products
- *    derived from this software without specific prior written permission
  *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
@@ -53,7 +48,7 @@ int	v_flag = 0;	/* Verbose (a dot for each track copied)	*/
 int	V_flag = 0;	/* Show version					*/
 char	*progname;
 
-const char version[] = "$Revision: 1.10 $";
+const char version[] = "$Revision: 1.11 $";
 
 int
 main(int argc, char *argv[])
