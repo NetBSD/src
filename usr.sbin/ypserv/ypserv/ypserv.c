397fffb47770dd18c3d9e9175850f1212de6d152
snj <snj@NetBSD.org>
2009-10-20 00:51:13 +0000

    Follow upstream's lead and remove third and fourth clauses (except on from usr.sbin/mopd/common/pf.c, where only the ad clause is removed, because it has a shared UCB copyright) on Mats O Jansson's files.
    thorpej OK'd usr.sbin/rpc.yppasswdd/yppasswdd_mkpw.c, where he shares
    copyright.

--- usr.sbin/ypserv/ypserv/ypserv.c
+++ usr.sbin/ypserv/ypserv/ypserv.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: ypserv.c,v 1.22 2008/05/16 16:41:42 chuck Exp $	*/
+/*	$NetBSD: ypserv.c,v 1.23 2009/10/20 00:51:15 snj Exp $	*/
 
 /*
  * Copyright (c) 1994 Mats O Jansson <moj@stacken.kth.se>
@@ -12,11 +12,6 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed by Mats O Jansson
- * 4. The name of the author may not be used to endorse or promote products
- *    derived from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS
  * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
@@ -33,7 +28,7 @@
 
 #include <sys/cdefs.h>
 #ifndef lint
-__RCSID("$NetBSD: ypserv.c,v 1.22 2008/05/16 16:41:42 chuck Exp $");
+__RCSID("$NetBSD: ypserv.c,v 1.23 2009/10/20 00:51:15 snj Exp $");
 #endif
 
 #include <sys/types.h>
