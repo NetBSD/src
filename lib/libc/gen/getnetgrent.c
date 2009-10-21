550147bd6a5bef946e7a7f153e49c0d11314e598
snj <snj@NetBSD.org>
2009-10-21 01:07:44 +0000

    Remove 3rd and 4th clauses in christos' license.  OK christos.

--- lib/libc/gen/getnetgrent.c
+++ lib/libc/gen/getnetgrent.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: getnetgrent.c,v 1.40 2008/04/05 08:01:54 rtr Exp $	*/
+/*	$NetBSD: getnetgrent.c,v 1.41 2009/10/21 01:07:45 snj Exp $	*/
 
 /*
  * Copyright (c) 1994 Christos Zoulas
@@ -12,11 +12,6 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed by Christos Zoulas.
- * 4. The name of the author may not be used to endorse or promote products
- *    derived from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS
  * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
@@ -33,7 +28,7 @@
 
 #include <sys/cdefs.h>
 #if defined(LIBC_SCCS) && !defined(lint)
-__RCSID("$NetBSD: getnetgrent.c,v 1.40 2008/04/05 08:01:54 rtr Exp $");
+__RCSID("$NetBSD: getnetgrent.c,v 1.41 2009/10/21 01:07:45 snj Exp $");
 #endif /* LIBC_SCCS and not lint */
 
 #include "namespace.h"
