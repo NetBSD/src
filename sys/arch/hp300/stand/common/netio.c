4657ac786070c3ec0766fd874736905b85c6be71
snj <snj@NetBSD.org>
2009-10-21 23:12:09 +0000

    Drop 3rd and 4th clauses.  Approved by gwr@ (copyright holder).

--- sys/arch/hp300/stand/common/netio.c
+++ sys/arch/hp300/stand/common/netio.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: netio.c,v 1.12 2008/04/28 20:23:19 martin Exp $	*/
+/*	$NetBSD: netio.c,v 1.13 2009/10/21 23:12:09 snj Exp $	*/
 
 /*-
  * Copyright (c) 1996, 1997 The NetBSD Foundation, Inc.
@@ -41,11 +41,6 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. The name of the author may not be used to endorse or promote products
- *    derived from this software without specific prior written permission.
- * 4. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *      This product includes software developed by Gordon W. Ross
  *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
