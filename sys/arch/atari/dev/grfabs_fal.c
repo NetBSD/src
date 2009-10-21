7f108050aaf99331089c74ae4ed369f65962e9a2
snj <snj@NetBSD.org>
2009-10-21 21:50:14 +0000

    Drop 3rd and 4th clause.  Approved by leo@ and thomas@ (copyright holders).

--- sys/arch/atari/dev/grfabs_fal.c
+++ sys/arch/atari/dev/grfabs_fal.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: grfabs_fal.c,v 1.25 2009/07/19 05:43:22 tsutsui Exp $	*/
+/*	$NetBSD: grfabs_fal.c,v 1.26 2009/10/21 21:50:14 snj Exp $	*/
 
 /*
  * Copyright (c) 1995 Thomas Gerner.
@@ -13,11 +13,6 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *      This product includes software developed by Leo Weppelman.
- * 4. The name of the author may not be used to endorse or promote products
- *    derived from this software without specific prior written permission
  *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
@@ -32,7 +27,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: grfabs_fal.c,v 1.25 2009/07/19 05:43:22 tsutsui Exp $");
+__KERNEL_RCSID(0, "$NetBSD: grfabs_fal.c,v 1.26 2009/10/21 21:50:14 snj Exp $");
 
 #ifdef FALCON_VIDEO
 /*
