12c2292047ef8a3d590119481453783e71efc358
wiz <wiz>
2009-10-21 13:56:36 +0000

    Fix problems with SIGWINCH. From Paul Ripke in PR 42161.

--- usr.bin/systat/vmstat.c
+++ usr.bin/systat/vmstat.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: vmstat.c,v 1.73 2009/04/13 23:20:27 lukem Exp $	*/
+/*	$NetBSD: vmstat.c,v 1.74 2009/10/21 13:56:36 wiz Exp $	*/
 
 /*-
  * Copyright (c) 1983, 1989, 1992, 1993
@@ -34,7 +34,7 @@
 #if 0
 static char sccsid[] = "@(#)vmstat.c	8.2 (Berkeley) 1/12/94";
 #endif
-__RCSID("$NetBSD: vmstat.c,v 1.73 2009/04/13 23:20:27 lukem Exp $");
+__RCSID("$NetBSD: vmstat.c,v 1.74 2009/10/21 13:56:36 wiz Exp $");
 #endif /* not lint */
 
 /*
@@ -498,7 +498,7 @@ showvmstat(void)
 		etime = cur.cp_etime;
 		/* < 5 ticks - ignore this trash */
 		if ((etime * hertz) < 1.0) {
-			if (failcnt++ > MAXFAIL)
+			if (failcnt++ <= MAXFAIL)
 				return;
 			clear();
 			mvprintw(2, 10, "The alternate system clock has died!");
