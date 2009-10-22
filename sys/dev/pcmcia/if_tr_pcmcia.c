4cc7a6d88a2428b0b74b452ec53a720c6ff41dbb
snj <snj@NetBSD.org>
2009-10-22 18:47:56 +0000

    Drop 3rd and 4th clauses.  Approved by soren@ and Onno van der Linden (copyright holders).

--- sys/dev/pcmcia/if_tr_pcmcia.c
+++ sys/dev/pcmcia/if_tr_pcmcia.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: if_tr_pcmcia.c,v 1.23 2009/05/12 14:42:18 cegger Exp $	*/
+/*	$NetBSD: if_tr_pcmcia.c,v 1.24 2009/10/22 18:47:56 snj Exp $	*/
 
 /*
  * Copyright (c) 2000 Soren S. Jorvang.  All rights reserved.
@@ -12,12 +12,6 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *  This product includes software developed by Soren S. Jorvang.
- *  This product includes software developed by Onno van der Linden.
- * 4. The name of the author may not be used to endorse or promote products
- *    derived from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
@@ -41,7 +35,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: if_tr_pcmcia.c,v 1.23 2009/05/12 14:42:18 cegger Exp $");
+__KERNEL_RCSID(0, "$NetBSD: if_tr_pcmcia.c,v 1.24 2009/10/22 18:47:56 snj Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
