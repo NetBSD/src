c9d23ced63003fcab3d6ff240ad4f1e8edcf3bac
snj <snj@NetBSD.org>
2009-10-23 05:10:48 +0000

    Drop 3rd and 4th clauses.  Approved by cl@ and Keir Fraser (copyright holders).

--- sys/arch/xen/xen/evtchn.c
+++ sys/arch/xen/xen/evtchn.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: evtchn.c,v 1.45 2009/10/19 18:41:11 bouyer Exp $	*/
+/*	$NetBSD: evtchn.c,v 1.46 2009/10/23 05:10:48 snj Exp $	*/
 
 /*
  * Copyright (c) 2006 Manuel Bouyer.
@@ -39,11 +39,6 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *      This product includes software developed by Christian Limpach.
- * 4. The name of the author may not be used to endorse or promote products
- *    derived from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
@@ -59,7 +54,7 @@
 
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: evtchn.c,v 1.45 2009/10/19 18:41:11 bouyer Exp $");
+__KERNEL_RCSID(0, "$NetBSD: evtchn.c,v 1.46 2009/10/23 05:10:48 snj Exp $");
 
 #include "opt_xen.h"
 #include "isa.h"
