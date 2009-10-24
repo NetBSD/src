0b807be843328576003ad098c43f0f771b21d903
dsl <dsl@NetBSD.org>
2009-10-24 15:20:15 +0000

    Remove a load of pointless casts - one that even lint doesn't bleat about.

--- lib/libc/stdio/vasprintf.c
+++ lib/libc/stdio/vasprintf.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: vasprintf.c,v 1.10 2005/02/09 21:35:47 kleink Exp $	*/
+/*	$NetBSD: vasprintf.c,v 1.11 2009/10/24 15:20:15 dsl Exp $	*/
 
 /*
  * Copyright (c) 1997 Todd C. Miller <Todd.Miller@courtesan.com>
@@ -29,7 +29,7 @@
 
 #include <sys/cdefs.h>
 #if defined(LIBC_SCCS) && !defined(lint)
-__RCSID("$NetBSD: vasprintf.c,v 1.10 2005/02/09 21:35:47 kleink Exp $");
+__RCSID("$NetBSD: vasprintf.c,v 1.11 2009/10/24 15:20:15 dsl Exp $");
 #endif /* LIBC_SCCS and not lint */
 
 #include <assert.h>
@@ -64,7 +64,7 @@ vasprintf(str, fmt, ap)
 	if (ret == -1)
 		goto err;
 	*f._p = '\0';
-	_base = realloc(f._bf._base, (size_t)(ret + 1));
+	_base = realloc(f._bf._base, ret + 1);
 	if (_base == NULL)
 		goto err;
 	*str = (char *)_base;
