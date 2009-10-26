be1270fb968d28d3142900be59045dfb786911d2
christos <christos@NetBSD.org>
2009-10-26 21:08:59 +0000

    Import new m4 from OpenBSD.

---
+++ usr.bin/m4/lib/ohash_int.h
@@ -0,0 +1,19 @@
+/*	$OpenBSD: ohash_int.h,v 1.3 2006/01/16 15:52:25 espie Exp $	*/
+
+#include <stddef.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include "ohash.h"
+
+struct _ohash_record {
+	u_int32_t	hv;
+	const char 	*p;
+};
+
+#define DELETED		((const char *)h)
+#define NONE		(h->size)
+
+/* Don't bother changing the hash table if the change is small enough.  */
+#define MINSIZE		(1UL << 4)
+#define MINDELETED	4
