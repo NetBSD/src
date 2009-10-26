1ac08edc3e7d2996a9420796fbd933e24945f687
joerg <joerg@NetBSD.org>
2009-10-26 14:50:45 +0000

    Obfuscate RCS ID matching so that CVS doesn't expand it.

--- external/bsd/mdocml/prepare-import.sh
+++ external/bsd/mdocml/prepare-import.sh
@@ -6,9 +6,8 @@ cd dist
 rm -rf ChangeLog.xsl Makefile example.style.css index.css *.sgml
 
 uuencode external.png < external.png > external.png.uu
-
 rm external.png
 
 for f in [a-z]*; do
-	sed 's/\$Id: prepare-import.sh,v 1.1 2009/10/21 18:04:52 joerg Exp $Vendor-Id:/' < $f > $f.new && mv $f.new $f
+	sed 's/[$]Id:/\$Vendor-Id:/' < $f > $f.new && mv $f.new $f
 done
