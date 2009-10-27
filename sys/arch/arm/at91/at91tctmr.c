2d5873969e26682cb78f7c77e7b7a7305edb7326
snj <snj@NetBSD.org>
2009-10-27 03:42:31 +0000

    Drop 3rd and 4th clauses.  Approved by all copyright holders (Embedtronics Oy's owners, Jon Sevy, and joff).

--- sys/arch/arm/at91/at91tctmr.c
+++ sys/arch/arm/at91/at91tctmr.c
@@ -1,4 +1,4 @@
-/*$NetBSD: at91tctmr.c,v 1.2 2008/07/03 01:15:38 matt Exp $*/
+/*$NetBSD: at91tctmr.c,v 1.3 2009/10/27 03:42:31 snj Exp $*/
 
 /*
  * AT91 Timer Counter (TC) based clock functions
@@ -20,13 +20,6 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *This product includes software developed by the NetBSD
- *Foundation, Inc. and its contributors.
- * 4. Neither the name of The NetBSD Foundation nor the names of its
- *    contributors may be used to endorse or promote products derived
- *    from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
  * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
@@ -47,7 +40,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: at91tctmr.c,v 1.2 2008/07/03 01:15:38 matt Exp $");
+__KERNEL_RCSID(0, "$NetBSD: at91tctmr.c,v 1.3 2009/10/27 03:42:31 snj Exp $");
 
 #include <sys/types.h>
 #include <sys/param.h>
