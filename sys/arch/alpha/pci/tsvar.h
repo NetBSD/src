411bae3ef7ec297d4a4d29c05935f3c8be075aeb
sborrill <sborrill>
2009-10-31 13:35:03 +0000

    Pull up the following revisions(s) (requested by mhitch in ticket #1120): 	sys/arch/alpha/pci/tsp_bus_io.c:	revision 1.6 	sys/arch/alpha/pci/tsp_bus_mem.c:	revision 1.9 	sys/arch/alpha/pci/tsvar.h:	revision 1.7
    The tsc(4) bus initialization was using a single statically allocated
    extent storage for each tsp(4), which caused a LOCKDEBUG kernel to fail
    because the extent storage contained a mutex which panics when the second
    mutex_init() is attempted.  Put the extent storage into the tsp_config
    structure so each tsp(4) gets it own.  Fixes PR port-alpha/38358.

--- sys/arch/alpha/pci/tsvar.h
+++ sys/arch/alpha/pci/tsvar.h
@@ -1,4 +1,4 @@
-/* $NetBSD: tsvar.h,v 1.5 2005/12/11 12:16:17 christos Exp $ */
+/* $NetBSD: tsvar.h,v 1.5.88.1 2009/10/31 13:35:03 sborrill Exp $ */
 
 /*-
  * Copyright (c) 1999 by Ross Harvey.  All rights reserved.
@@ -35,6 +35,8 @@
 #include <dev/pci/pcivar.h>
 #include <alpha/pci/pci_sgmap_pte64.h>
 
+#define	_FSTORE	(EXTENT_FIXED_STORAGE_SIZE(8) / sizeof(long))
+
 #define	tsvar() { Generate ctags(1) key. }
 
 struct tsc_softc {
@@ -58,6 +60,8 @@ struct tsp_config {
 	u_int32_t pc_hae_mem;
 	u_int32_t pc_hae_io;
 
+	long	pc_io_exstorage[_FSTORE];
+	long	pc_mem_exstorage[_FSTORE];
 	struct	extent *pc_io_ex, *pc_mem_ex;
 	int	pc_mallocsafe;
 };
