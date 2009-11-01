531c945985ae34a0093ff53edfec8653fe52273b
snj <snj@NetBSD.org>
2009-11-01 01:51:35 +0000

    Drop 3rd and 4th clauses.  Approved by Brad Grantham (copyright holder).

--- sys/arch/macppc/dev/adb.c
+++ sys/arch/macppc/dev/adb.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: adb.c,v 1.27 2009/03/14 21:04:11 dsl Exp $	*/
+/*	$NetBSD: adb.c,v 1.28 2009/11/01 01:51:35 snj Exp $	*/
 
 /*-
  * Copyright (C) 1994	Bradley A. Grantham
@@ -12,11 +12,6 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed by Bradley A. Grantham.
- * 4. The name of the author may not be used to endorse or promote products
- *    derived from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
@@ -31,7 +26,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: adb.c,v 1.27 2009/03/14 21:04:11 dsl Exp $");
+__KERNEL_RCSID(0, "$NetBSD: adb.c,v 1.28 2009/11/01 01:51:35 snj Exp $");
 
 #include <sys/param.h>
 #include <sys/device.h>
