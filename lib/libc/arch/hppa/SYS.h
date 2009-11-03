be482f11da00b302f8e2a2158d0e1c142a1e1f74
snj <snj>
2009-11-03 05:07:25 +0000

    Follow upstream license changes for files with Michael Shalayeff's copyright.  In most cases, this means dropping the 3rd and 4th clauses.

--- lib/libc/arch/hppa/SYS.h
+++ lib/libc/arch/hppa/SYS.h
@@ -1,9 +1,9 @@
-/*	$NetBSD: SYS.h,v 1.6 2007/11/18 13:25:39 skrll Exp $	*/
+/*	$NetBSD: SYS.h,v 1.7 2009/11/03 05:07:25 snj Exp $	*/
 
 /*	$OpenBSD: SYS.h,v 1.9 2001/09/20 20:52:09 millert Exp $	*/
 
 /*
- * Copyright (c) 1998-1999 Michael Shalayeff
+ * Copyright (c) 1998-2002 Michael Shalayeff
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -14,11 +14,6 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed by Michael Shalayeff.
- * 4. The name of the author may not be used to endorse or promote products
- *    derived from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
--- sys/arch/hp700/stand/common/exec_som.c
+++ sys/arch/hp700/stand/common/exec_som.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: exec_som.c,v 1.3 2007/03/03 14:37:54 skrll Exp $	*/
+/*	$NetBSD: exec_som.c,v 1.4 2009/11/03 05:07:26 snj Exp $	*/
 
 /*	$OpenBSD: exec_som.c,v 1.1 1999/12/23 04:10:30 mickey Exp $	*/
 
@@ -14,11 +14,6 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *      This product includes software developed by Michael Shalayeff.
- * 4. The name of the author may not be used to endorse or promote products
- *    derived from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
