506012b76993abb96fc326a8c68792e7aee8abe5
joerg <joerg>
2009-11-06 18:34:22 +0000

    Retire __SCCSID. It has only archeological value now. Also retire lint conditional around __RCSID, lint can handle that fine.

--- usr.bin/sort/fsort.c
+++ usr.bin/sort/fsort.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: fsort.c,v 1.45 2009/10/09 20:32:57 dsl Exp $	*/
+/*	$NetBSD: fsort.c,v 1.46 2009/11/06 18:34:22 joerg Exp $	*/
 
 /*-
  * Copyright (c) 2000-2003 The NetBSD Foundation, Inc.
@@ -71,10 +71,7 @@
 #include "sort.h"
 #include "fsort.h"
 
-#ifndef lint
-__RCSID("$NetBSD: fsort.c,v 1.45 2009/10/09 20:32:57 dsl Exp $");
-__SCCSID("@(#)fsort.c	8.1 (Berkeley) 6/6/93");
-#endif /* not lint */
+__RCSID("$NetBSD: fsort.c,v 1.46 2009/11/06 18:34:22 joerg Exp $");
 
 #include <stdlib.h>
 #include <string.h>
