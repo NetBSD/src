886d7f6c39552a12a0a6a5d020325e6e440b44e4
snj <snj@NetBSD.org>
2009-11-07 19:54:17 +0000

    Drop 3rd and 4th clauses on this TNF-copyrighted file.

--- sys/arch/x68k/dev/mfp.h
+++ sys/arch/x68k/dev/mfp.h
@@ -1,7 +1,6 @@
-/*	$NetBSD: mfp.h,v 1.8 2009/01/17 10:02:23 isaki Exp $	*/
+/*	$NetBSD: mfp.h,v 1.9 2009/11/07 19:54:17 snj Exp $	*/
 
 /*
- *
  * Copyright (c) 1998 NetBSD Foundation, Inc.
  * All rights reserved.
  *
@@ -13,12 +12,6 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *      This product includes software developed by Charles D. Cranor and
- *      Washington University.
- * 4. The name of the author may not be used to endorse or promote products
- *    derived from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
