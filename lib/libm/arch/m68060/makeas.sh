e9195c3b1149bc07d29ccbded08c2dfdbebc7bfe
is <is@NetBSD.org>
2009-11-09 15:35:27 +0000

    Move to two-clause license.

--- lib/libm/arch/m68060/makeas.sh
+++ lib/libm/arch/m68060/makeas.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# $NetBSD: makeas.sh,v 1.6 2000/05/16 19:05:37 is Exp $
+# $NetBSD: makeas.sh,v 1.7 2009/11/09 15:35:28 is Exp $
 
 # Copyright (c) 1999, 2000 Ignatios Souvatzis
 # All rights reserved.
@@ -13,12 +13,6 @@
 # 2. Redistributions in binary form must reproduce the above copyright
 #    notice, this list of conditions and the following disclaimer in the
 #    documentation and/or other materials provided with the distribution.
-# 3. All advertising materials mentioning features or use of this software
-#    must display the following acknowledgement:
-#      This product includes software developed for the NetBSD Project
-#      by Ignatios Souvatzis.
-# 4. The name of the author may not be used to endorse or promote products
-#    derived from this software without specific prior written permission
 #
 # THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
 # IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
--- sys/arch/amiga/dev/grf_ultms.g
+++ sys/arch/amiga/dev/grf_ultms.g
@@ -1,4 +1,4 @@
-; $NetBSD: grf_ultms.g,v 1.9 2005/12/11 12:16:28 christos Exp $
+; $NetBSD: grf_ultms.g,v 1.10 2009/11/09 15:35:27 is Exp $
 ;
 ; ite support for A2410.
 
@@ -14,12 +14,6 @@
 ; 2. Redistributions in binary form must reproduce the above copyright
 ;    notice, this list of conditions and the following disclaimer in the
 ;    documentation and/or other materials provided with the distribution.
-; 3. All advertising materials mentioning features or use of this software
-;    must display the following acknowledgement:
-;	This product contains software developed by Ignatios Souvatzis
-;	for the NetBSD project.
-; 4. The name of the author may not be used to endorse or promote products
-;    derived from this software without specific prior written permission
 ;
 ; THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
 ; IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
