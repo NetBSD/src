019cd4cfc627cfc8d84da4f24f84a0b43ff4d3c1
snj <snj@NetBSD.org>
2009-11-15 20:38:35 +0000

    Drop 3rd and 4th clauses (except in one case, where copyright is shared with UCB and only the 3rd clause is dropped).  Approved by mhitch@ and is@ (copyright holders).

--- sys/arch/amiga/stand/bootblock/boot/main.c
+++ sys/arch/amiga/stand/bootblock/boot/main.c
@@ -1,5 +1,5 @@
 /*
- * $NetBSD: main.c,v 1.24 2009/01/12 07:42:30 tsutsui Exp $
+ * $NetBSD: main.c,v 1.25 2009/11/15 20:38:35 snj Exp $
  *
  *
  * Copyright (c) 1996,1999 Ignatios Souvatzis
@@ -14,11 +14,6 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *      This product includes software developed by Michael L. Hitch.
- * 4. The name of the authors may not be used to endorse or promote products
- *    derived from this software without specific prior written permission
  *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
