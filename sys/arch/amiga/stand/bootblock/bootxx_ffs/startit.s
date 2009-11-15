71586ddec94ddcf449c4506a278e564838844efd
snj <snj@NetBSD.org>
2009-11-15 20:38:35 +0000

    Drop 3rd and 4th clauses (except in one case, where copyright is shared with UCB and only the 3rd clause is dropped).  Approved by mhitch@ and is@ (copyright holders).

--- sys/arch/amiga/stand/bootblock/bootxx_ffs/startit.s
+++ sys/arch/amiga/stand/bootblock/bootxx_ffs/startit.s
@@ -1,4 +1,4 @@
-/*	$NetBSD: startit.s,v 1.2 2002/05/20 05:58:45 mhitch Exp $	*/
+/*	$NetBSD: startit.s,v 1.3 2009/11/15 20:38:36 snj Exp $	*/
 
 /*
  * Copyright (c) 1996 Ignatios Souvatzis
@@ -13,11 +13,6 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *      This product includes software developed by Michael L. Hitch.
- * 4. The name of the author may not be used to endorse or promote products
- *    derived from this software without specific prior written permission
  *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
@@ -31,7 +26,7 @@
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
  *
- * From: $NetBSD: startit.s,v 1.2 2002/05/20 05:58:45 mhitch Exp $
+ * From: $NetBSD: startit.s,v 1.3 2009/11/15 20:38:36 snj Exp $
  */
 #include "machine/asm.h"
 
