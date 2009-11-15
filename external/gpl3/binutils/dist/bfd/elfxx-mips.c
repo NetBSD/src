3116cb401fa28f80c2d0f9b8a0b2fe884db66a11
skrll <skrll@NetBSD.org>
2009-11-15 10:25:16 +0000

    Pull across most of the changes from matt-nb5-mips64.
    Only remaining change is bfd/elf.c:_bfd_elf_is_local_label_name which
    should probably be done via mips_elf_is_local_label_name.

--- external/gpl3/binutils/dist/bfd/elfxx-mips.c
+++ external/gpl3/binutils/dist/bfd/elfxx-mips.c
@@ -7706,10 +7706,24 @@ _bfd_mips_elf_check_relocs (bfd *abfd, struct bfd_link_info *info,
 		     reloc types into the output file as R_MIPS_REL32
 		     relocs.  Make room for this reloc in .rel(a).dyn.  */
 		  mips_elf_allocate_dynamic_relocations (dynobj, info, 1);
-		  if (MIPS_ELF_READONLY_SECTION (sec))
-		    /* We tell the dynamic linker that there are
-		       relocations against the text segment.  */
-		    info->flags |= DF_TEXTREL;
+		  /* In the N32 and 64-bit ABIs there may be multiple
+		     consecutive relocations for the same offset.  If we have
+		     a R_MIPS_GPREL32 followed by a R_MIPS_64 then that
+		     relocation is complete and needs no futher adjustment.  */
+		  if ((rel == relocs
+		      || rel[-1].r_offset != rel->r_offset
+		      || r_type != R_MIPS_64
+		      || ELF_R_TYPE(abfd, rel[-1].r_info) != R_MIPS_GPREL32)
+		      && (MIPS_ELF_READONLY_SECTION (sec))
+		    {
+		      /* We tell the dynamic linker that there are
+		         relocations against the text segment.  */
+		      info->flags |= DF_TEXTREL;
+		      info->callbacks->warning
+			(info,
+			 _("relocation emitted against readonly section"),
+			 NULL, abfd, sec, rel->r_offset);
+		    }
 		}
 	      else
 		{
