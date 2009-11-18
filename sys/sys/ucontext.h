2b821af391140baaca97be23310a1890202f484e
yamt <yamt@NetBSD.org>
2009-11-18 12:29:22 +0000

    comment which bits of uc_flags are used by MD code.
    + * if your port needs more MD bits, please try to choose bits from _UC_MD
    + * first, rather than picking random unused bits.

--- sys/sys/ucontext.h
+++ sys/sys/ucontext.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: ucontext.h,v 1.11 2008/10/15 06:51:21 wrstuden Exp $	*/
+/*	$NetBSD: ucontext.h,v 1.12 2009/11/18 12:29:22 yamt Exp $	*/
 
 /*-
  * Copyright (c) 1999, 2003 The NetBSD Foundation, Inc.
@@ -57,6 +57,22 @@ struct __ucontext {
 #define _UC_STACK	0x02		/* valid uc_stack */
 #define _UC_CPU		0x04		/* valid GPR context in uc_mcontext */
 #define _UC_FPU		0x08		/* valid FPU context in uc_mcontext */
+#define	_UC_MD		0x40070020	/* MD bits.  see below */
+
+/*
+ * _UC_MD includes:
+ *	_UC_SETSTACK	0x00010000 (many ports) and 0x00020000 (arm)
+ *	_UC_CLRSTACK	0x00020000 (many ports) and 0x00040000 (arm)
+ *	_UC_POWERPC_VEC	0x00010000 (powerpc)
+ *	_UC_M68K_UC_USER 0x40000000 (m68k)
+ *	_UC_UNIQUE	0x00000020 (alpha)
+ *	_UC_ARM_VFP	0x00010000 (arm)
+ *	_UC_VM		0x00040000 (i386)
+ *	_UC_FXSAVE	0x00000020 (i386)
+ *
+ * if your port needs more MD bits, please try to choose bits from _UC_MD
+ * first, rather than picking random unused bits.
+ */
 
 #ifdef _KERNEL
 struct lwp;
