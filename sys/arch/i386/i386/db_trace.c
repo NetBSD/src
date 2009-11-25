e60c7e0fdcfd87b2d0d8eb2861050c2b7b6560a9
rmind <rmind@NetBSD.org>
2009-11-25 13:53:19 +0000

    Restore exporting of lwp_getpcb() to userland and fix DDB properly. While the header might be abstracted better, it is a separate issue.

--- sys/arch/i386/i386/db_trace.c
+++ sys/arch/i386/i386/db_trace.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: db_trace.c,v 1.65 2009/11/21 19:59:51 dsl Exp $	*/
+/*	$NetBSD: db_trace.c,v 1.66 2009/11/25 13:53:19 rmind Exp $	*/
 
 /* 
  * Mach Operating System
@@ -27,7 +27,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: db_trace.c,v 1.65 2009/11/21 19:59:51 dsl Exp $");
+__KERNEL_RCSID(0, "$NetBSD: db_trace.c,v 1.66 2009/11/25 13:53:19 rmind Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -452,6 +452,7 @@ db_stack_trace_print(db_expr_t addr, bool have_addr, db_expr_t count,
 				    sizeof(l), (char *)&l);
 			}
 			(*pr)("lid %d ", l.l_lid);
+			pcb = lwp_getpcb(&l);
 #ifdef _KERNEL
 			if (l.l_proc == curproc &&
 			    (lwp_t *)lwpaddr == curlwp) {
@@ -461,11 +462,6 @@ db_stack_trace_print(db_expr_t addr, bool have_addr, db_expr_t count,
 			} else
 #endif
 			{
-#ifdef _KERNEL
-				pcb = lwp_getpcb(&l);
-#else
-				pcb = &l.l_addr->u_pcb;
-#endif
 				db_read_bytes((db_addr_t)&pcb->pcb_ebp,
 				    sizeof(frame), (char *)&frame);
 				db_read_bytes((db_addr_t)(frame + 1),
