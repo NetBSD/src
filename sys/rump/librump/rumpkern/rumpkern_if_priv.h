4f28ef325012ed23a2343cc7eb241fac0525ccd2
pooka <pooka@NetBSD.org>
2009-11-26 09:21:16 +0000

    regen: rump_kernelfsym_load()

--- sys/rump/librump/rumpkern/rumpkern_if_priv.h
+++ sys/rump/librump/rumpkern/rumpkern_if_priv.h
@@ -1,8 +1,8 @@
-/*	$NetBSD: rumpkern_if_priv.h,v 1.4 2009/10/15 16:40:15 pooka Exp $	*/
+/*	$NetBSD: rumpkern_if_priv.h,v 1.5 2009/11/26 09:21:16 pooka Exp $	*/
 
 /*
  * Automatically generated.  DO NOT EDIT.
- * from: NetBSD: rumpkern.ifspec,v 1.2 2009/10/15 16:39:22 pooka Exp 
+ * from: NetBSD: rumpkern.ifspec,v 1.3 2009/11/26 09:20:07 pooka Exp 
  * by:   NetBSD: makerumpif.sh,v 1.4 2009/10/15 00:29:19 pooka Exp 
  */
 
@@ -10,6 +10,7 @@ void rump_reboot(int);
 int rump_getversion(void);
 int rump_module_init(struct modinfo *, prop_dictionary_t);
 int rump_module_fini(struct modinfo *);
+int rump_kernelfsym_load(void *, uint64_t, char *, uint64_t);
 struct uio * rump_uio_setup(void *, size_t, off_t, enum rump_uiorw);
 size_t rump_uio_getresid(struct uio *);
 off_t rump_uio_getoff(struct uio *);
