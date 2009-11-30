0cd3492163af4d444d5844f79cd36ff3a30eca3c
uebayasi <uebayasi@NetBSD.org>
2009-11-30 10:26:36 +0000

    Snapshot of generated files.  Makfile glue will come soon.

---
+++ gnu/lib/libgcc4/arch/mips64eb/srcs.cppflags.mk
@@ -0,0 +1,272 @@
+_CPPFLAGS.tmp___gcc_bcmp.c=-DL__gcc_bcmp
+_CPPFLAGS.tmp___main.c=-DL__main
+_CPPFLAGS.tmp___main_s.c=-DL__main -DSHARED
+_CPPFLAGS.tmp__absvdi2.c=-DL_absvdi2
+_CPPFLAGS.tmp__absvdi2_s.c=-DL_absvdi2 -DSHARED
+_CPPFLAGS.tmp__absvsi2.c=-DL_absvsi2
+_CPPFLAGS.tmp__absvsi2_s.c=-DL_absvsi2 -DSHARED
+_CPPFLAGS.tmp__addsub_df.c=-DFINE_GRAINED_LIBRARIES -DL_addsub_df
+_CPPFLAGS.tmp__addsub_df_s.c=-DFINE_GRAINED_LIBRARIES -DL_addsub_df -DSHARED
+_CPPFLAGS.tmp__addsub_sf.c=-DFINE_GRAINED_LIBRARIES -DL_addsub_sf
+_CPPFLAGS.tmp__addsub_sf_s.c=-DFINE_GRAINED_LIBRARIES -DL_addsub_sf -DSHARED
+_CPPFLAGS.tmp__addvdi3.c=-DL_addvdi3
+_CPPFLAGS.tmp__addvdi3_s.c=-DL_addvdi3 -DSHARED
+_CPPFLAGS.tmp__addvsi3.c=-DL_addvsi3
+_CPPFLAGS.tmp__addvsi3_s.c=-DL_addvsi3 -DSHARED
+_CPPFLAGS.tmp__ashldi3.c=-DL_ashldi3
+_CPPFLAGS.tmp__ashldi3_s.c=-DL_ashldi3 -DSHARED
+_CPPFLAGS.tmp__ashrdi3.c=-DL_ashrdi3
+_CPPFLAGS.tmp__ashrdi3_s.c=-DL_ashrdi3 -DSHARED
+_CPPFLAGS.tmp__clear_cache.c=-DL_clear_cache
+_CPPFLAGS.tmp__clear_cache_s.c=-DL_clear_cache -DSHARED
+_CPPFLAGS.tmp__clz.c=-DL_clz
+_CPPFLAGS.tmp__clz_s.c=-DL_clz -DSHARED
+_CPPFLAGS.tmp__clzdi2.c=-DL_clzdi2
+_CPPFLAGS.tmp__clzdi2_s.c=-DL_clzdi2 -DSHARED
+_CPPFLAGS.tmp__clzsi2.c=-DL_clzsi2
+_CPPFLAGS.tmp__clzsi2_s.c=-DL_clzsi2 -DSHARED
+_CPPFLAGS.tmp__cmpdi2.c=-DL_cmpdi2
+_CPPFLAGS.tmp__cmpdi2_s.c=-DL_cmpdi2 -DSHARED
+_CPPFLAGS.tmp__compare_df.c=-DFINE_GRAINED_LIBRARIES -DL_compare_df
+_CPPFLAGS.tmp__compare_df_s.c=-DFINE_GRAINED_LIBRARIES -DL_compare_df -DSHARED
+_CPPFLAGS.tmp__compare_sf.c=-DFINE_GRAINED_LIBRARIES -DL_compare_sf
+_CPPFLAGS.tmp__compare_sf_s.c=-DFINE_GRAINED_LIBRARIES -DL_compare_sf -DSHARED
+_CPPFLAGS.tmp__ctors.c=-DL_ctors
+_CPPFLAGS.tmp__ctors_s.c=-DL_ctors -DSHARED
+_CPPFLAGS.tmp__ctzdi2.c=-DL_ctzdi2
+_CPPFLAGS.tmp__ctzdi2_s.c=-DL_ctzdi2 -DSHARED
+_CPPFLAGS.tmp__ctzsi2.c=-DL_ctzsi2
+_CPPFLAGS.tmp__ctzsi2_s.c=-DL_ctzsi2 -DSHARED
+_CPPFLAGS.tmp__df_to_sf.c=-DFINE_GRAINED_LIBRARIES -DL_df_to_sf
+_CPPFLAGS.tmp__df_to_sf_s.c=-DFINE_GRAINED_LIBRARIES -DL_df_to_sf -DSHARED
+_CPPFLAGS.tmp__df_to_si.c=-DFINE_GRAINED_LIBRARIES -DL_df_to_si
+_CPPFLAGS.tmp__df_to_si_s.c=-DFINE_GRAINED_LIBRARIES -DL_df_to_si -DSHARED
+_CPPFLAGS.tmp__df_to_usi.c=-DFINE_GRAINED_LIBRARIES -DL_df_to_usi
+_CPPFLAGS.tmp__df_to_usi_s.c=-DFINE_GRAINED_LIBRARIES -DL_df_to_usi -DSHARED
+_CPPFLAGS.tmp__div_df.c=-DFINE_GRAINED_LIBRARIES -DL_div_df
+_CPPFLAGS.tmp__div_df_s.c=-DFINE_GRAINED_LIBRARIES -DL_div_df -DSHARED
+_CPPFLAGS.tmp__div_sf.c=-DFINE_GRAINED_LIBRARIES -DL_div_sf
+_CPPFLAGS.tmp__div_sf_s.c=-DFINE_GRAINED_LIBRARIES -DL_div_sf -DSHARED
+_CPPFLAGS.tmp__divdc3.c=-DL_divdc3
+_CPPFLAGS.tmp__divdc3_s.c=-DL_divdc3 -DSHARED
+_CPPFLAGS.tmp__divdi3.c=-DL_divdi3
+_CPPFLAGS.tmp__divdi3_s.c=-DL_divdi3 -DSHARED
+_CPPFLAGS.tmp__divsc3.c=-DL_divsc3
+_CPPFLAGS.tmp__divsc3_s.c=-DL_divsc3 -DSHARED
+_CPPFLAGS.tmp__divtc3.c=-DL_divtc3
+_CPPFLAGS.tmp__divtc3_s.c=-DL_divtc3 -DSHARED
+_CPPFLAGS.tmp__divxc3.c=-DL_divxc3
+_CPPFLAGS.tmp__divxc3_s.c=-DL_divxc3 -DSHARED
+_CPPFLAGS.tmp__enable_execute_stack.c=-DL_enable_execute_stack
+_CPPFLAGS.tmp__enable_execute_stack_s.c=-DL_enable_execute_stack -DSHARED
+_CPPFLAGS.tmp__eprintf.c=-DL_eprintf
+_CPPFLAGS.tmp__eq_df.c=-DFINE_GRAINED_LIBRARIES -DL_eq_df
+_CPPFLAGS.tmp__eq_df_s.c=-DFINE_GRAINED_LIBRARIES -DL_eq_df -DSHARED
+_CPPFLAGS.tmp__eq_sf.c=-DFINE_GRAINED_LIBRARIES -DL_eq_sf
+_CPPFLAGS.tmp__eq_sf_s.c=-DFINE_GRAINED_LIBRARIES -DL_eq_sf -DSHARED
+_CPPFLAGS.tmp__ffsdi2.c=-DL_ffsdi2
+_CPPFLAGS.tmp__ffsdi2_s.c=-DL_ffsdi2 -DSHARED
+_CPPFLAGS.tmp__ffssi2.c=-DL_ffssi2
+_CPPFLAGS.tmp__ffssi2_s.c=-DL_ffssi2 -DSHARED
+_CPPFLAGS.tmp__fixdfdi.c=-DLIBGCC2_UNITS_PER_WORD=4 -DL_fixdfdi
+_CPPFLAGS.tmp__fixdfdi_s.c=-DLIBGCC2_UNITS_PER_WORD=4 -DL_fixdfdi -DSHARED
+_CPPFLAGS.tmp__fixdfti.c=-DLIBGCC2_UNITS_PER_WORD=8 -DL_fixdfdi
+_CPPFLAGS.tmp__fixdfti_s.c=-DLIBGCC2_UNITS_PER_WORD=8 -DL_fixdfdi -DSHARED
+_CPPFLAGS.tmp__fixsfdi.c=-DLIBGCC2_UNITS_PER_WORD=4 -DL_fixsfdi
+_CPPFLAGS.tmp__fixsfdi_s.c=-DLIBGCC2_UNITS_PER_WORD=4 -DL_fixsfdi -DSHARED
+_CPPFLAGS.tmp__fixsfti.c=-DLIBGCC2_UNITS_PER_WORD=8 -DL_fixsfdi
+_CPPFLAGS.tmp__fixsfti_s.c=-DLIBGCC2_UNITS_PER_WORD=8 -DL_fixsfdi -DSHARED
+_CPPFLAGS.tmp__fixtfdi.c=-DLIBGCC2_UNITS_PER_WORD=4 -DL_fixtfdi
+_CPPFLAGS.tmp__fixtfdi_s.c=-DLIBGCC2_UNITS_PER_WORD=4 -DL_fixtfdi -DSHARED
+_CPPFLAGS.tmp__fixtfti.c=-DLIBGCC2_UNITS_PER_WORD=8 -DL_fixtfdi
+_CPPFLAGS.tmp__fixtfti_s.c=-DLIBGCC2_UNITS_PER_WORD=8 -DL_fixtfdi -DSHARED
+_CPPFLAGS.tmp__fixunsdfdi.c=-DLIBGCC2_UNITS_PER_WORD=4 -DL_fixunsdfdi
+_CPPFLAGS.tmp__fixunsdfdi_s.c=-DLIBGCC2_UNITS_PER_WORD=4 -DL_fixunsdfdi -DSHARED
+_CPPFLAGS.tmp__fixunsdfsi.c=-DLIBGCC2_UNITS_PER_WORD=4 -DL_fixunsdfsi
+_CPPFLAGS.tmp__fixunsdfsi_s.c=-DLIBGCC2_UNITS_PER_WORD=4 -DL_fixunsdfsi -DSHARED
+_CPPFLAGS.tmp__fixunsdfti.c=-DLIBGCC2_UNITS_PER_WORD=8 -DL_fixunsdfdi
+_CPPFLAGS.tmp__fixunsdfti_s.c=-DLIBGCC2_UNITS_PER_WORD=8 -DL_fixunsdfdi -DSHARED
+_CPPFLAGS.tmp__fixunssfdi.c=-DLIBGCC2_UNITS_PER_WORD=4 -DL_fixunssfdi
+_CPPFLAGS.tmp__fixunssfdi_s.c=-DLIBGCC2_UNITS_PER_WORD=4 -DL_fixunssfdi -DSHARED
+_CPPFLAGS.tmp__fixunssfsi.c=-DLIBGCC2_UNITS_PER_WORD=4 -DL_fixunssfsi
+_CPPFLAGS.tmp__fixunssfsi_s.c=-DLIBGCC2_UNITS_PER_WORD=4 -DL_fixunssfsi -DSHARED
+_CPPFLAGS.tmp__fixunssfti.c=-DLIBGCC2_UNITS_PER_WORD=8 -DL_fixunssfdi
+_CPPFLAGS.tmp__fixunssfti_s.c=-DLIBGCC2_UNITS_PER_WORD=8 -DL_fixunssfdi -DSHARED
+_CPPFLAGS.tmp__fixunstfdi.c=-DLIBGCC2_UNITS_PER_WORD=4 -DL_fixunstfdi
+_CPPFLAGS.tmp__fixunstfdi_s.c=-DLIBGCC2_UNITS_PER_WORD=4 -DL_fixunstfdi -DSHARED
+_CPPFLAGS.tmp__fixunstfti.c=-DLIBGCC2_UNITS_PER_WORD=8 -DL_fixunstfdi
+_CPPFLAGS.tmp__fixunstfti_s.c=-DLIBGCC2_UNITS_PER_WORD=8 -DL_fixunstfdi -DSHARED
+_CPPFLAGS.tmp__fixunsxfdi.c=-DLIBGCC2_UNITS_PER_WORD=4 -DL_fixunsxfdi
+_CPPFLAGS.tmp__fixunsxfdi_s.c=-DLIBGCC2_UNITS_PER_WORD=4 -DL_fixunsxfdi -DSHARED
+_CPPFLAGS.tmp__fixunsxfsi.c=-DLIBGCC2_UNITS_PER_WORD=4 -DL_fixunsxfsi
+_CPPFLAGS.tmp__fixunsxfsi_s.c=-DLIBGCC2_UNITS_PER_WORD=4 -DL_fixunsxfsi -DSHARED
+_CPPFLAGS.tmp__fixunsxfti.c=-DLIBGCC2_UNITS_PER_WORD=8 -DL_fixunsxfdi
+_CPPFLAGS.tmp__fixunsxfti_s.c=-DLIBGCC2_UNITS_PER_WORD=8 -DL_fixunsxfdi -DSHARED
+_CPPFLAGS.tmp__fixxfdi.c=-DLIBGCC2_UNITS_PER_WORD=4 -DL_fixxfdi
+_CPPFLAGS.tmp__fixxfdi_s.c=-DLIBGCC2_UNITS_PER_WORD=4 -DL_fixxfdi -DSHARED
+_CPPFLAGS.tmp__fixxfti.c=-DLIBGCC2_UNITS_PER_WORD=8 -DL_fixxfdi
+_CPPFLAGS.tmp__fixxfti_s.c=-DLIBGCC2_UNITS_PER_WORD=8 -DL_fixxfdi -DSHARED
+_CPPFLAGS.tmp__floatdidf.c=-DLIBGCC2_UNITS_PER_WORD=4 -DL_floatdidf
+_CPPFLAGS.tmp__floatdidf_s.c=-DLIBGCC2_UNITS_PER_WORD=4 -DL_floatdidf -DSHARED
+_CPPFLAGS.tmp__floatdisf.c=-DLIBGCC2_UNITS_PER_WORD=4 -DL_floatdisf
+_CPPFLAGS.tmp__floatdisf_s.c=-DLIBGCC2_UNITS_PER_WORD=4 -DL_floatdisf -DSHARED
+_CPPFLAGS.tmp__floatditf.c=-DLIBGCC2_UNITS_PER_WORD=4 -DL_floatditf
+_CPPFLAGS.tmp__floatditf_s.c=-DLIBGCC2_UNITS_PER_WORD=4 -DL_floatditf -DSHARED
+_CPPFLAGS.tmp__floatdixf.c=-DLIBGCC2_UNITS_PER_WORD=4 -DL_floatdixf
+_CPPFLAGS.tmp__floatdixf_s.c=-DLIBGCC2_UNITS_PER_WORD=4 -DL_floatdixf -DSHARED
+_CPPFLAGS.tmp__floattidf.c=-DLIBGCC2_UNITS_PER_WORD=8 -DL_floatdidf
+_CPPFLAGS.tmp__floattidf_s.c=-DLIBGCC2_UNITS_PER_WORD=8 -DL_floatdidf -DSHARED
+_CPPFLAGS.tmp__floattisf.c=-DLIBGCC2_UNITS_PER_WORD=8 -DL_floatdisf
+_CPPFLAGS.tmp__floattisf_s.c=-DLIBGCC2_UNITS_PER_WORD=8 -DL_floatdisf -DSHARED
+_CPPFLAGS.tmp__floattitf.c=-DLIBGCC2_UNITS_PER_WORD=8 -DL_floatditf
+_CPPFLAGS.tmp__floattitf_s.c=-DLIBGCC2_UNITS_PER_WORD=8 -DL_floatditf -DSHARED
+_CPPFLAGS.tmp__floattixf.c=-DLIBGCC2_UNITS_PER_WORD=8 -DL_floatdixf
+_CPPFLAGS.tmp__floattixf_s.c=-DLIBGCC2_UNITS_PER_WORD=8 -DL_floatdixf -DSHARED
+_CPPFLAGS.tmp__fpcmp_parts_df.c=-DFINE_GRAINED_LIBRARIES -DL_fpcmp_parts_df
+_CPPFLAGS.tmp__fpcmp_parts_df_s.c=-DFINE_GRAINED_LIBRARIES -DL_fpcmp_parts_df -DSHARED
+_CPPFLAGS.tmp__fpcmp_parts_sf.c=-DFINE_GRAINED_LIBRARIES -DL_fpcmp_parts_sf
+_CPPFLAGS.tmp__fpcmp_parts_sf_s.c=-DFINE_GRAINED_LIBRARIES -DL_fpcmp_parts_sf -DSHARED
+_CPPFLAGS.tmp__gcov.c=-DL_gcov
+_CPPFLAGS.tmp__gcov_execl.c=-DL_gcov_execl
+_CPPFLAGS.tmp__gcov_execle.c=-DL_gcov_execle
+_CPPFLAGS.tmp__gcov_execlp.c=-DL_gcov_execlp
+_CPPFLAGS.tmp__gcov_execv.c=-DL_gcov_execv
+_CPPFLAGS.tmp__gcov_execve.c=-DL_gcov_execve
+_CPPFLAGS.tmp__gcov_execvp.c=-DL_gcov_execvp
+_CPPFLAGS.tmp__gcov_fork.c=-DL_gcov_fork
+_CPPFLAGS.tmp__gcov_interval_profiler.c=-DL_gcov_interval_profiler
+_CPPFLAGS.tmp__gcov_merge_add.c=-DL_gcov_merge_add
+_CPPFLAGS.tmp__gcov_merge_delta.c=-DL_gcov_merge_delta
+_CPPFLAGS.tmp__gcov_merge_single.c=-DL_gcov_merge_single
+_CPPFLAGS.tmp__gcov_one_value_profiler.c=-DL_gcov_one_value_profiler
+_CPPFLAGS.tmp__gcov_pow2_profiler.c=-DL_gcov_pow2_profiler
+_CPPFLAGS.tmp__ge_df.c=-DFINE_GRAINED_LIBRARIES -DL_ge_df
+_CPPFLAGS.tmp__ge_df_s.c=-DFINE_GRAINED_LIBRARIES -DL_ge_df -DSHARED
+_CPPFLAGS.tmp__ge_sf.c=-DFINE_GRAINED_LIBRARIES -DL_ge_sf
+_CPPFLAGS.tmp__ge_sf_s.c=-DFINE_GRAINED_LIBRARIES -DL_ge_sf -DSHARED
+_CPPFLAGS.tmp__gt_df.c=-DFINE_GRAINED_LIBRARIES -DL_gt_df
+_CPPFLAGS.tmp__gt_df_s.c=-DFINE_GRAINED_LIBRARIES -DL_gt_df -DSHARED
+_CPPFLAGS.tmp__gt_sf.c=-DFINE_GRAINED_LIBRARIES -DL_gt_sf
+_CPPFLAGS.tmp__gt_sf_s.c=-DFINE_GRAINED_LIBRARIES -DL_gt_sf -DSHARED
+_CPPFLAGS.tmp__le_df.c=-DFINE_GRAINED_LIBRARIES -DL_le_df
+_CPPFLAGS.tmp__le_df_s.c=-DFINE_GRAINED_LIBRARIES -DL_le_df -DSHARED
+_CPPFLAGS.tmp__le_sf.c=-DFINE_GRAINED_LIBRARIES -DL_le_sf
+_CPPFLAGS.tmp__le_sf_s.c=-DFINE_GRAINED_LIBRARIES -DL_le_sf -DSHARED
+_CPPFLAGS.tmp__lshrdi3.c=-DL_lshrdi3
+_CPPFLAGS.tmp__lshrdi3_s.c=-DL_lshrdi3 -DSHARED
+_CPPFLAGS.tmp__lt_df.c=-DFINE_GRAINED_LIBRARIES -DL_lt_df
+_CPPFLAGS.tmp__lt_df_s.c=-DFINE_GRAINED_LIBRARIES -DL_lt_df -DSHARED
+_CPPFLAGS.tmp__lt_sf.c=-DFINE_GRAINED_LIBRARIES -DL_lt_sf
+_CPPFLAGS.tmp__lt_sf_s.c=-DFINE_GRAINED_LIBRARIES -DL_lt_sf -DSHARED
+_CPPFLAGS.tmp__make_df.c=-DFINE_GRAINED_LIBRARIES -DL_make_df
+_CPPFLAGS.tmp__make_df_s.c=-DFINE_GRAINED_LIBRARIES -DL_make_df -DSHARED
+_CPPFLAGS.tmp__make_sf.c=-DFINE_GRAINED_LIBRARIES -DL_make_sf
+_CPPFLAGS.tmp__make_sf_s.c=-DFINE_GRAINED_LIBRARIES -DL_make_sf -DSHARED
+_CPPFLAGS.tmp__moddi3.c=-DL_moddi3
+_CPPFLAGS.tmp__moddi3_s.c=-DL_moddi3 -DSHARED
+_CPPFLAGS.tmp__mul_df.c=-DFINE_GRAINED_LIBRARIES -DL_mul_df
+_CPPFLAGS.tmp__mul_df_s.c=-DFINE_GRAINED_LIBRARIES -DL_mul_df -DSHARED
+_CPPFLAGS.tmp__mul_sf.c=-DFINE_GRAINED_LIBRARIES -DL_mul_sf
+_CPPFLAGS.tmp__mul_sf_s.c=-DFINE_GRAINED_LIBRARIES -DL_mul_sf -DSHARED
+_CPPFLAGS.tmp__muldc3.c=-DL_muldc3
+_CPPFLAGS.tmp__muldc3_s.c=-DL_muldc3 -DSHARED
+_CPPFLAGS.tmp__muldi3.c=-DL_muldi3
+_CPPFLAGS.tmp__muldi3_s.c=-DL_muldi3 -DSHARED
+_CPPFLAGS.tmp__mulsc3.c=-DL_mulsc3
+_CPPFLAGS.tmp__mulsc3_s.c=-DL_mulsc3 -DSHARED
+_CPPFLAGS.tmp__multc3.c=-DL_multc3
+_CPPFLAGS.tmp__multc3_s.c=-DL_multc3 -DSHARED
+_CPPFLAGS.tmp__mulvdi3.c=-DL_mulvdi3
+_CPPFLAGS.tmp__mulvdi3_s.c=-DL_mulvdi3 -DSHARED
+_CPPFLAGS.tmp__mulvsi3.c=-DL_mulvsi3
+_CPPFLAGS.tmp__mulvsi3_s.c=-DL_mulvsi3 -DSHARED
+_CPPFLAGS.tmp__mulxc3.c=-DL_mulxc3
+_CPPFLAGS.tmp__mulxc3_s.c=-DL_mulxc3 -DSHARED
+_CPPFLAGS.tmp__ne_df.c=-DFINE_GRAINED_LIBRARIES -DL_ne_df
+_CPPFLAGS.tmp__ne_df_s.c=-DFINE_GRAINED_LIBRARIES -DL_ne_df -DSHARED
+_CPPFLAGS.tmp__ne_sf.c=-DFINE_GRAINED_LIBRARIES -DL_ne_sf
+_CPPFLAGS.tmp__ne_sf_s.c=-DFINE_GRAINED_LIBRARIES -DL_ne_sf -DSHARED
+_CPPFLAGS.tmp__negate_df.c=-DFINE_GRAINED_LIBRARIES -DL_negate_df
+_CPPFLAGS.tmp__negate_df_s.c=-DFINE_GRAINED_LIBRARIES -DL_negate_df -DSHARED
+_CPPFLAGS.tmp__negate_sf.c=-DFINE_GRAINED_LIBRARIES -DL_negate_sf
+_CPPFLAGS.tmp__negate_sf_s.c=-DFINE_GRAINED_LIBRARIES -DL_negate_sf -DSHARED
+_CPPFLAGS.tmp__negdi2.c=-DL_negdi2
+_CPPFLAGS.tmp__negdi2_s.c=-DL_negdi2 -DSHARED
+_CPPFLAGS.tmp__negvdi2.c=-DL_negvdi2
+_CPPFLAGS.tmp__negvdi2_s.c=-DL_negvdi2 -DSHARED
+_CPPFLAGS.tmp__negvsi2.c=-DL_negvsi2
+_CPPFLAGS.tmp__negvsi2_s.c=-DL_negvsi2 -DSHARED
+_CPPFLAGS.tmp__pack_df.c=-DFINE_GRAINED_LIBRARIES -DL_pack_df
+_CPPFLAGS.tmp__pack_df_s.c=-DFINE_GRAINED_LIBRARIES -DL_pack_df -DSHARED
+_CPPFLAGS.tmp__pack_sf.c=-DFINE_GRAINED_LIBRARIES -DL_pack_sf
+_CPPFLAGS.tmp__pack_sf_s.c=-DFINE_GRAINED_LIBRARIES -DL_pack_sf -DSHARED
+_CPPFLAGS.tmp__paritydi2.c=-DL_paritydi2
+_CPPFLAGS.tmp__paritydi2_s.c=-DL_paritydi2 -DSHARED
+_CPPFLAGS.tmp__paritysi2.c=-DL_paritysi2
+_CPPFLAGS.tmp__paritysi2_s.c=-DL_paritysi2 -DSHARED
+_CPPFLAGS.tmp__popcount_tab.c=-DL_popcount_tab
+_CPPFLAGS.tmp__popcount_tab_s.c=-DL_popcount_tab -DSHARED
+_CPPFLAGS.tmp__popcountdi2.c=-DL_popcountdi2
+_CPPFLAGS.tmp__popcountdi2_s.c=-DL_popcountdi2 -DSHARED
+_CPPFLAGS.tmp__popcountsi2.c=-DL_popcountsi2
+_CPPFLAGS.tmp__popcountsi2_s.c=-DL_popcountsi2 -DSHARED
+_CPPFLAGS.tmp__powidf2.c=-DL_powidf2
+_CPPFLAGS.tmp__powidf2_s.c=-DL_powidf2 -DSHARED
+_CPPFLAGS.tmp__powisf2.c=-DL_powisf2
+_CPPFLAGS.tmp__powisf2_s.c=-DL_powisf2 -DSHARED
+_CPPFLAGS.tmp__powitf2.c=-DL_powitf2
+_CPPFLAGS.tmp__powitf2_s.c=-DL_powitf2 -DSHARED
+_CPPFLAGS.tmp__powixf2.c=-DL_powixf2
+_CPPFLAGS.tmp__powixf2_s.c=-DL_powixf2 -DSHARED
+_CPPFLAGS.tmp__sf_to_df.c=-DFINE_GRAINED_LIBRARIES -DL_sf_to_df
+_CPPFLAGS.tmp__sf_to_df_s.c=-DFINE_GRAINED_LIBRARIES -DL_sf_to_df -DSHARED
+_CPPFLAGS.tmp__sf_to_si.c=-DFINE_GRAINED_LIBRARIES -DL_sf_to_si
+_CPPFLAGS.tmp__sf_to_si_s.c=-DFINE_GRAINED_LIBRARIES -DL_sf_to_si -DSHARED
+_CPPFLAGS.tmp__sf_to_usi.c=-DFINE_GRAINED_LIBRARIES -DL_sf_to_usi
+_CPPFLAGS.tmp__sf_to_usi_s.c=-DFINE_GRAINED_LIBRARIES -DL_sf_to_usi -DSHARED
+_CPPFLAGS.tmp__si_to_df.c=-DFINE_GRAINED_LIBRARIES -DL_si_to_df
+_CPPFLAGS.tmp__si_to_df_s.c=-DFINE_GRAINED_LIBRARIES -DL_si_to_df -DSHARED
+_CPPFLAGS.tmp__si_to_sf.c=-DFINE_GRAINED_LIBRARIES -DL_si_to_sf
+_CPPFLAGS.tmp__si_to_sf_s.c=-DFINE_GRAINED_LIBRARIES -DL_si_to_sf -DSHARED
+_CPPFLAGS.tmp__subvdi3.c=-DL_subvdi3
+_CPPFLAGS.tmp__subvdi3_s.c=-DL_subvdi3 -DSHARED
+_CPPFLAGS.tmp__subvsi3.c=-DL_subvsi3
+_CPPFLAGS.tmp__subvsi3_s.c=-DL_subvsi3 -DSHARED
+_CPPFLAGS.tmp__thenan_df.c=-DFINE_GRAINED_LIBRARIES -DL_thenan_df
+_CPPFLAGS.tmp__thenan_df_s.c=-DFINE_GRAINED_LIBRARIES -DL_thenan_df -DSHARED
+_CPPFLAGS.tmp__thenan_sf.c=-DFINE_GRAINED_LIBRARIES -DL_thenan_sf
+_CPPFLAGS.tmp__thenan_sf_s.c=-DFINE_GRAINED_LIBRARIES -DL_thenan_sf -DSHARED
+_CPPFLAGS.tmp__trampoline.c=-DL_trampoline
+_CPPFLAGS.tmp__trampoline_s.c=-DL_trampoline -DSHARED
+_CPPFLAGS.tmp__ucmpdi2.c=-DL_ucmpdi2
+_CPPFLAGS.tmp__ucmpdi2_s.c=-DL_ucmpdi2 -DSHARED
+_CPPFLAGS.tmp__udiv_w_sdiv.c=-DL_udiv_w_sdiv
+_CPPFLAGS.tmp__udiv_w_sdiv_s.c=-DL_udiv_w_sdiv -DSHARED
+_CPPFLAGS.tmp__udivdi3.c=-DL_udivdi3
+_CPPFLAGS.tmp__udivdi3_s.c=-DL_udivdi3 -DSHARED
+_CPPFLAGS.tmp__udivmoddi4.c=-DL_udivmoddi4
+_CPPFLAGS.tmp__udivmoddi4_s.c=-DL_udivmoddi4 -DSHARED
+_CPPFLAGS.tmp__umoddi3.c=-DL_umoddi3
+_CPPFLAGS.tmp__umoddi3_s.c=-DL_umoddi3 -DSHARED
+_CPPFLAGS.tmp__unord_df.c=-DFINE_GRAINED_LIBRARIES -DL_unord_df
+_CPPFLAGS.tmp__unord_df_s.c=-DFINE_GRAINED_LIBRARIES -DL_unord_df -DSHARED
+_CPPFLAGS.tmp__unord_sf.c=-DFINE_GRAINED_LIBRARIES -DL_unord_sf
+_CPPFLAGS.tmp__unord_sf_s.c=-DFINE_GRAINED_LIBRARIES -DL_unord_sf -DSHARED
+_CPPFLAGS.tmp__unpack_df.c=-DFINE_GRAINED_LIBRARIES -DL_unpack_df
+_CPPFLAGS.tmp__unpack_df_s.c=-DFINE_GRAINED_LIBRARIES -DL_unpack_df -DSHARED
+_CPPFLAGS.tmp__unpack_sf.c=-DFINE_GRAINED_LIBRARIES -DL_unpack_sf
+_CPPFLAGS.tmp__unpack_sf_s.c=-DFINE_GRAINED_LIBRARIES -DL_unpack_sf -DSHARED
+_CPPFLAGS.tmp__usi_to_df.c=-DFINE_GRAINED_LIBRARIES -DL_usi_to_df
+_CPPFLAGS.tmp__usi_to_df_s.c=-DFINE_GRAINED_LIBRARIES -DL_usi_to_df -DSHARED
+_CPPFLAGS.tmp__usi_to_sf.c=-DFINE_GRAINED_LIBRARIES -DL_usi_to_sf
+_CPPFLAGS.tmp__usi_to_sf_s.c=-DFINE_GRAINED_LIBRARIES -DL_usi_to_sf -DSHARED
+_CPPFLAGS.tmp_gthr-gnat.c=
+_CPPFLAGS.tmp_gthr-gnat_s.c=-DSHARED
+_CPPFLAGS.tmp_unwind-c.c=
+_CPPFLAGS.tmp_unwind-c_s.c=-DSHARED
+_CPPFLAGS.tmp_unwind-dw2-fde.c=
+_CPPFLAGS.tmp_unwind-dw2-fde_s.c=-DSHARED
+_CPPFLAGS.tmp_unwind-dw2.c=
+_CPPFLAGS.tmp_unwind-dw2_s.c=-DSHARED
+_CPPFLAGS.tmp_unwind-sjlj.c=
+_CPPFLAGS.tmp_unwind-sjlj_s.c=-DSHARED
