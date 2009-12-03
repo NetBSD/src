499a19cff39d578a5ac24ce1076619a6459c1bf5
sborrill <sborrill@NetBSD.org>
2009-12-03 09:40:00 +0000

    Pull up the following revisions(s) (requested by plunky in ticket #1173): 	sys/net/if_tap.c:	revision 1.60
    Fix a potential leak on tap device close; purging the send queue
    did not actually release the dequeued mbufs.

--- sys/net/if_tap.c
+++ sys/net/if_tap.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: if_tap.c,v 1.47.4.6 2009/04/04 23:36:28 snj Exp $	*/
+/*	$NetBSD: if_tap.c,v 1.47.4.7 2009/12/03 09:40:00 sborrill Exp $	*/
 
 /*
  *  Copyright (c) 2003, 2004, 2008, 2009 The NetBSD Foundation.
@@ -33,7 +33,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: if_tap.c,v 1.47.4.6 2009/04/04 23:36:28 snj Exp $");
+__KERNEL_RCSID(0, "$NetBSD: if_tap.c,v 1.47.4.7 2009/12/03 09:40:00 sborrill Exp $");
 
 #if defined(_KERNEL_OPT)
 #include "bpfilter.h"
@@ -852,6 +852,7 @@ tap_dev_close(struct tap_softc *sc)
 			if (ifp->if_bpf)
 				bpf_mtap(ifp->if_bpf, m);
 #endif
+			m_freem(m);
 		}
 	}
 	splx(s);
