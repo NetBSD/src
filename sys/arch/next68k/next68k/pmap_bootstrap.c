353cefd7f11ff9ed3cb80d77a746ae4084419783
tsutsui <tsutsui@NetBSD.org>
2009-12-04 18:32:31 +0000

    No need to initialize lwp0 u-area in pmap_bootstrap() since it will be done in pmap_bootstrap_finalize().

--- sys/arch/next68k/next68k/pmap_bootstrap.c
+++ sys/arch/next68k/next68k/pmap_bootstrap.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: pmap_bootstrap.c,v 1.29 2009/12/04 18:06:28 tsutsui Exp $	*/
+/*	$NetBSD: pmap_bootstrap.c,v 1.30 2009/12/04 18:32:31 tsutsui Exp $	*/
 
 /*
  * This file was taken from mvme68k/mvme68k/pmap_bootstrap.c
@@ -45,7 +45,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: pmap_bootstrap.c,v 1.29 2009/12/04 18:06:28 tsutsui Exp $");
+__KERNEL_RCSID(0, "$NetBSD: pmap_bootstrap.c,v 1.30 2009/12/04 18:32:31 tsutsui Exp $");
 
 #include <sys/param.h>
 #include <sys/kcore.h>
@@ -438,17 +438,6 @@ pmap_bootstrap(paddr_t nextpa, paddr_t firstpa)
 	RELOC(Sysmap, pt_entry_t *) =
 	    (pt_entry_t *)m68k_ptob((NPTEPG - 2) * NPTEPG);
 
-	/*
-	 * Setup u-area for lwp 0.
-	 */
-	/*
-	 * Zero the u-area.
-	 * NOTE: `pte' and `epte' aren't PTEs here.
-	 */
-	pte = (u_int *)lwp0upa;
-	epte = (u_int *)(lwp0upa + USPACE);
-	while (pte < epte)
-		*pte++ = 0;
 	/*
 	 * Remember the u-area address so it can be loaded in the lwp0
 	 * via uvm_lwp_setuarea() later in pmap_bootstrap_finalize().
