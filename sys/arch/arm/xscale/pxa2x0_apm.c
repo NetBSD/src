faa8e1b3e3a35abcfeac7660f0c8c14a7bd206ac
pooka <pooka@NetBSD.org>
2009-12-05 22:34:43 +0000

    Convert tsleep(&lbolt) to kpause().  Make ltsleep/mtsleep on lbolt illegal.  I examined all places where lbolt is referenced to make sure there were pointer aliases of it passed to tsleep, but put a KASSERT in m/ltsleep() just to be sure.

--- sys/arch/arm/xscale/pxa2x0_apm.c
+++ sys/arch/arm/xscale/pxa2x0_apm.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: pxa2x0_apm.c,v 1.1 2008/12/06 22:10:41 ober Exp $	*/
+/*	$NetBSD: pxa2x0_apm.c,v 1.2 2009/12/05 22:34:43 pooka Exp $	*/
 /*	$OpenBSD: pxa2x0_apm.c,v 1.28 2007/03/29 18:42:38 uwe Exp $	*/
 
 /*-
@@ -443,7 +443,7 @@ apm_thread(void *v)
 		apm_battlow = apm_suspends = apm_userstandbys = 0;
 
 		APM_UNLOCK(sc);
-		tsleep(&lbolt, PWAIT, "apmev", 0);
+		kpause("apmev", false, hz, NULL);
 	}
 }
 
