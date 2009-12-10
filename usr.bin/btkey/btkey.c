75847ce3f83e97edd5f65024237f2ac73156397b
plunky <plunky@NetBSD.org>
2009-12-10 18:57:31 +0000

    accept uppercase hex digits when parsing a provided key

--- usr.bin/btkey/btkey.c
+++ usr.bin/btkey/btkey.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: btkey.c,v 1.2 2008/07/21 14:19:21 lukem Exp $	*/
+/*	$NetBSD: btkey.c,v 1.3 2009/12/10 18:57:31 plunky Exp $	*/
 
 /*-
  * Copyright (c) 2007 Iain Hibbert
@@ -29,9 +29,10 @@
 
 #include <sys/cdefs.h>
 __COPYRIGHT("@(#) Copyright (c) 2007 Iain Hibbert.  All rights reserved.");
-__RCSID("$NetBSD: btkey.c,v 1.2 2008/07/21 14:19:21 lukem Exp $");
+__RCSID("$NetBSD: btkey.c,v 1.3 2009/12/10 18:57:31 plunky Exp $");
 
 #include <bluetooth.h>
+#include <ctype.h>
 #include <err.h>
 #include <errno.h>
 #include <stdbool.h>
@@ -253,7 +254,7 @@ scan_key(const char *arg)
 			if (*arg == '\0')
 				return true;
 
-			for (p = digits ; *p != *arg ; p++)
+			for (p = digits ; *p != tolower((int)*arg) ; p++)
 				if (*p == '\0')
 					return false;
 
