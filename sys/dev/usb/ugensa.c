8c5632d3016810b1fa2906904ee0725905454667
cherry <cherry@NetBSD.org>
2009-12-12 14:18:27 +0000

    Add ID for Qualcomminc AC8700 as found in "ZTE, Incorporated ZTE CDMA Tech, rev 1.10/0.00,  Works with BSNL (http://www.bsnl.co.in) EVDO service

--- sys/dev/usb/ugensa.c
+++ sys/dev/usb/ugensa.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: ugensa.c,v 1.24 2009/11/12 19:51:44 dyoung Exp $	*/
+/*	$NetBSD: ugensa.c,v 1.25 2009/12/12 14:18:27 cherry Exp $	*/
 
 /*
  * Copyright (c) 2004, 2005 The NetBSD Foundation, Inc.
@@ -30,7 +30,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: ugensa.c,v 1.24 2009/11/12 19:51:44 dyoung Exp $");
+__KERNEL_RCSID(0, "$NetBSD: ugensa.c,v 1.25 2009/12/12 14:18:27 cherry Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -96,6 +96,7 @@ static const struct ugensa_type ugensa_devs[] = {
 	{{ USB_VENDOR_NOVATEL, USB_PRODUCT_NOVATEL_FLEXPACKGPS }, 0 },
 	{{ USB_VENDOR_QUALCOMM_K, USB_PRODUCT_QUALCOMM_K_CDMA_MSM_K }, 0 },
 	{{ USB_VENDOR_DELL, USB_PRODUCT_DELL_HSDPA }, 0 },
+	{{ USB_VENDOR_QUALCOMMINC, USB_PRODUCT_QUALCOMMINC_AC8700 }, 0 },
 
 	/*
 	 * The following devices are untested, but they are purported to
