0b2f67869833a14459c0831772a19c058262b0e5
mrg <mrg@NetBSD.org>
2009-12-13 08:50:56 +0000

    - adjust the logic for compat ldd: add an elf32_compat module and arrange   for mips64 to build this with MLIBDIR set to o32 - make mips64 ldd call elf32_compat_ldd as well - make mips64 elf64 set MLIBDIR to 64 - don't need ld.elf_so's Makefile.ld32 files anymore (they are going away   soon anyway)
    tested on amd64 and sparc64, and an earlier version tested on mips64.

--- usr.bin/ldd/ldd.h
+++ usr.bin/ldd/ldd.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: ldd.h,v 1.4 2009/09/07 04:49:03 dholland Exp $	*/
+/*	$NetBSD: ldd.h,v 1.5 2009/12/13 08:50:56 mrg Exp $	*/
 
 /*
  * Copyright (c) 2008 Matthew R. Green
@@ -33,6 +33,8 @@ int elf32_ldd(int, char *, const char *, const char *);
 #ifdef _LP64
 int elf64_ldd(int, char *, const char *, const char *);
 #define elf_ldd elf64_ldd
+#elif defined(ELF32_COMPAT)
+#define elf_ldd elf32_compat_ldd
 #else
 #define elf_ldd elf32_ldd
 #endif
