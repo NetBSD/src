31aa2afd74d1fd7bee383027e9b29557380a9164
sborrill <sborrill@NetBSD.org>
2009-12-14 19:36:57 +0000

    Revert previous version bump which should not have been in the supplied patch. This maintains compatibility between 5.0 and 5.1 (at the cost of needing userland libraries recompiled if one's been tracking netbsd-5).

--- sys/fs/puffs/puffs_msgif.h
+++ sys/fs/puffs/puffs_msgif.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: puffs_msgif.h,v 1.65.20.1 2009/10/18 12:46:07 sborrill Exp $	*/
+/*	$NetBSD: puffs_msgif.h,v 1.65.20.2 2009/12/14 19:36:57 sborrill Exp $	*/
 
 /*
  * Copyright (c) 2005, 2006, 2007  Antti Kantee.  All Rights Reserved.
@@ -102,7 +102,7 @@ enum {
 #define PUFFS_ERR_MAX PUFFS_ERR_VPTOFH
 
 #define PUFFSDEVELVERS	0x80000000
-#define PUFFSVERSION	28
+#define PUFFSVERSION	26
 #define PUFFSNAMESIZE	32
 
 #define PUFFS_TYPEPREFIX "puffs|"
