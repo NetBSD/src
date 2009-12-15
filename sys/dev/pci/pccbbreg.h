be360188fc94e20ee581840bd857390b859b0b88
snj <snj@NetBSD.org>
2009-12-15 22:17:12 +0000

    Move to 2-clause license.  Approved by HAYAKAWA Koichi (copyright holder).

--- sys/dev/pci/pccbbreg.h
+++ sys/dev/pci/pccbbreg.h
@@ -1,4 +1,5 @@
-/*	$NetBSD: pccbbreg.h,v 1.14 2008/07/03 13:37:35 drochner Exp $	*/
+/*	$NetBSD: pccbbreg.h,v 1.15 2009/12/15 22:17:12 snj Exp $	*/
+
 /*
  * Copyright (c) 1999 HAYAKAWA Koichi.  All rights reserved.
  *
@@ -10,11 +11,6 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed by HAYAKAWA Koichi.
- * 4. The name of the author may not be used to endorse or promote products
- *    derived from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
@@ -27,7 +23,6 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-
 
 #ifndef _DEV_PCI_PCCBBREG_H_
 #define	_DEV_PCI_PCCBBREG_H_
