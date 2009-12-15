7573a55e18e40b89c1564830ed8c55b42d4c3fe8
snj <snj>
2009-12-15 22:17:12 +0000

    Move to 2-clause license.  Approved by HAYAKAWA Koichi (copyright holder).

--- sys/dev/cardbus/rbus.c
+++ sys/dev/cardbus/rbus.c
@@ -1,4 +1,5 @@
-/*	$NetBSD: rbus.c,v 1.27 2009/03/14 21:04:19 dsl Exp $	*/
+/*	$NetBSD: rbus.c,v 1.28 2009/12/15 22:17:12 snj Exp $	*/
+
 /*
  * Copyright (c) 1999 and 2000
  *     HAYAKAWA Koichi.  All rights reserved.
@@ -11,11 +12,6 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed by HAYAKAWA Koichi.
- * 4. The name of the author may not be used to endorse or promote products
- *    derived from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
@@ -30,7 +26,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: rbus.c,v 1.27 2009/03/14 21:04:19 dsl Exp $");
+__KERNEL_RCSID(0, "$NetBSD: rbus.c,v 1.28 2009/12/15 22:17:12 snj Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
