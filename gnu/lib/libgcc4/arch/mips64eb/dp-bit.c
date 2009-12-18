f69e7f5bf95f52ea95d76e17a6be7587a762e602
uebayasi <uebayasi@NetBSD.org>
2009-12-18 12:51:43 +0000

    Libgcc softfloat wrapper source code.  This file is not auto-generated.  Due to the design of libgcc build procedure I had to reverse engineer this from generated files.

---
+++ gnu/lib/libgcc4/arch/mips64eb/dp-bit.c
@@ -0,0 +1,3 @@
+/*	$NetBSD: dp-bit.c,v 1.1 2009/12/18 12:51:43 uebayasi Exp $	*/
+#define QUIET_NAN_NEGATED
+#include "gcc/config/fp-bit.c"
---
+++ gnu/lib/libgcc4/arch/mips64eb/fp-bit.c
@@ -0,0 +1,4 @@
+/*	$NetBSD: fp-bit.c,v 1.1 2009/12/18 12:51:43 uebayasi Exp $	*/
+#define QUIET_NAN_NEGATED
+#define FLOAT
+#include "gcc/config/fp-bit.c"
