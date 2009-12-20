be072712c3c6075de3116635b751f8e6c985695d
pooka <pooka@NetBSD.org>
2009-12-20 15:43:13 +0000

    Add ucom@usb support.  This uses the uplcom hardware driver currently (because that's the hardware I happen to have), but others can just be plugged in if a need arises.

---
+++ sys/rump/dev/wip/libucom/locators.h
@@ -0,0 +1,7 @@
+/*	$NetBSD: locators.h,v 1.1 2009/12/20 15:43:13 pooka Exp $	*/
+
+/* locators borrowed from config.  redo properly some day */
+
+#define UCOMBUSCF_PORTNO 0
+#define UCOMBUSCF_PORTNO_DEFAULT -1
+#define UCOMBUSCF_NLOCS 1
