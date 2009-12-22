c8630b6eba9ec25ab7ef56faa70d09dac5413824
jmmv <jmmv>
2009-12-22 13:38:10 +0000

    Update build files to match atf 0.7.

--- external/bsd/atf/lib/libatf-c/bconfig.h
+++ external/bsd/atf/lib/libatf-c/bconfig.h
@@ -83,22 +83,25 @@
 #define PACKAGE_BUGREPORT "atf-devel@NetBSD.org"
 
 /* Define to the copyright string applicable to this package. */
-#define PACKAGE_COPYRIGHT "Copyright (c) 2007, 2008 The NetBSD Foundation, Inc."
+#define PACKAGE_COPYRIGHT "Copyright (c) 2007, 2008, 2009 The NetBSD Foundation, Inc."
 
 /* Define to the full name of this package. */
 #define PACKAGE_NAME "Automated Testing Framework"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "Automated Testing Framework 0.6"
+#define PACKAGE_STRING "Automated Testing Framework 0.7"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "atf"
 
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "0.6"
+#define PACKAGE_VERSION "0.7"
 
 /* Define to 1 if you have the ANSI C header files. */
 #define STDC_HEADERS 1
 
 /* Version number of package */
-#define VERSION "0.6"
+#define VERSION "0.7"
