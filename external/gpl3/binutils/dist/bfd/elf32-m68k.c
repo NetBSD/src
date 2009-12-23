97f5320e6610b99cb7e90dfa6525d6c241bef83a
mbalmer <mbalmer@NetBSD.org>
2009-12-23 09:22:16 +0000

    Fix typo in a comment, no code change.

--- external/gpl3/binutils/dist/bfd/elf32-m68k.c
+++ external/gpl3/binutils/dist/bfd/elf32-m68k.c
@@ -2118,7 +2118,7 @@ elf_m68k_copy_indirect_symbol (struct bfd_link_info *info,
   if (ind->got_entry_key != 0)
     {
       BFD_ASSERT (dir->got_entry_key == 0);
-      /* Assert that GOTs aren't partioned yet.  */
+      /* Assert that GOTs aren't partitioned yet.  */
       BFD_ASSERT (ind->glist == NULL);
 
       dir->got_entry_key = ind->got_entry_key;
