ec358e5c7bc1ca591388cd2bfc3e466635d68700
snj <snj@NetBSD.org>
2010-01-09 01:31:57 +0000

    Pull up following revision(s) (requested by cegger in ticket #1217): 	distrib/utils/sysinst/disks.c: revision 1.108 When creating /etc/fstab: for the first swap partition use type sw,dp instead of sw, so dump device gets configured correctly if swap is not on the second partition. Fixes PR install/42148.

--- distrib/utils/sysinst/disks.c
+++ distrib/utils/sysinst/disks.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: disks.c,v 1.100.2.5 2009/10/03 23:22:23 snj Exp $ */
+/*	$NetBSD: disks.c,v 1.100.2.6 2010/01/09 01:31:57 snj Exp $ */
 
 /*
  * Copyright 1997 Piermont Information Systems Inc.
@@ -652,6 +652,7 @@ make_fstab(void)
 {
 	FILE *f;
 	int i, swap_dev = -1;
+	const char *dump_dev;
 
 	/* Create the fstab. */
 	make_target_dir("/etc");
@@ -707,10 +708,14 @@ make_fstab(void)
 			fstype = "msdos";
 			break;
 		case FS_SWAP:
-			if (swap_dev == -1)
+			if (swap_dev == -1) {
 				swap_dev = i;
-			scripting_fprintf(f, "/dev/%s%c\t\tnone\tswap\tsw\t\t 0 0\n",
-				diskdev, 'a' + i);
+				dump_dev = ",dp";
+			} else {
+				dump_dev ="";
+			}
+			scripting_fprintf(f, "/dev/%s%c\t\tnone\tswap\tsw%s\t\t 0 0\n",
+				diskdev, 'a' + i, dump_dev);
 			continue;
 #ifdef USE_SYSVBFS
 		case FS_SYSVBFS:
