ff73f880f1f36811872ff77663e848b680fce859
cliff <cliff@NetBSD.org>
2010-01-13 09:40:09 +0000

    - add autoconf.h, need mainbus_atach_args

---
+++ sys/arch/evbmips/rmixl/autoconf.h
@@ -0,0 +1,11 @@
+/*	$NetBSD: autoconf.h,v 1.1.2.1 2010/01/13 09:40:09 cliff Exp $	*/
+
+#ifndef _EVBMIPS_RMIXL_AUTOCONF_H_
+#define _EVBMIPS_RMIXL_AUTOCONF_H_
+
+struct mainbus_attach_args {
+	const char     *ma_name;
+	int		ma_core;
+};
+
+#endif	/* _EVBMIPS_RMIXL_AUTOCONF_H_ */
