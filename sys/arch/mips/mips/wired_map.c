6c5777ed0a347bff75f1c8ffae748ca25b3851af
matt <matt@NetBSD.org>
2010-01-15 06:46:58 +0000

    Get rid of most of the studly caps. First to MULTIPROCESSOR support for pmap: move some pmap globals to cpu_info. Define per-cpu ASID number spaces. Remove some mips1/!mips1 difference in db_interface.c Add mips32/64 knowledge to stacktrace.

--- sys/arch/mips/mips/wired_map.c
+++ sys/arch/mips/mips/wired_map.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: wired_map.c,v 1.4 2007/02/28 04:21:54 thorpej Exp $	*/
+/*	$NetBSD: wired_map.c,v 1.4.62.1 2010/01/15 06:47:00 matt Exp $	*/
 
 /*-
  * Copyright (c) 2005 Tadpole Computer Inc.
@@ -63,7 +63,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: wired_map.c,v 1.4 2007/02/28 04:21:54 thorpej Exp $");
+__KERNEL_RCSID(0, "$NetBSD: wired_map.c,v 1.4.62.1 2010/01/15 06:47:00 matt Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -86,7 +86,7 @@ int mips3_nwired_page;
 bool
 mips3_wired_enter_page(vaddr_t va, paddr_t pa, vsize_t pgsize)
 {
-	struct tlb tlb;
+	struct tlbmask tlb;
 	vaddr_t va0;
 	int found, index;
 
@@ -157,7 +157,7 @@ mips3_wired_enter_page(vaddr_t va, paddr_t pa, vsize_t pgsize)
 			mips3_wired_map[index].pa1) |
 		    MIPS3_PG_IOPAGE(
 			    PMAP_CCA_FOR_PA(mips3_wired_map[index].pa1));
-	MachTLBWriteIndexedVPS(MIPS3_TLB_WIRED_UPAGES + index, &tlb);
+	tlb_write_indexed_VPS(MIPS3_TLB_WIRED_UPAGES + index, &tlb);
 	return true;
 }
 
