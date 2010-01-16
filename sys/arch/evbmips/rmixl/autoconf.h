63f0fae4b4bb89ed0acdf298e366b9036fba8c99
cliff <cliff@NetBSD.org>
2010-01-16 23:50:32 +0000

    - add struct mainbus_softc - struct mainbus_attach_args loses ma_core, gets ma_node

--- sys/arch/evbmips/rmixl/autoconf.h
+++ sys/arch/evbmips/rmixl/autoconf.h
@@ -1,11 +1,17 @@
-/*	$NetBSD: autoconf.h,v 1.1.2.1 2010/01/13 09:40:09 cliff Exp $	*/
+/*	$NetBSD: autoconf.h,v 1.1.2.2 2010/01/16 23:50:32 cliff Exp $	*/
 
 #ifndef _EVBMIPS_RMIXL_AUTOCONF_H_
 #define _EVBMIPS_RMIXL_AUTOCONF_H_
 
+struct mainbus_softc {
+	device_t	sc_dev;
+	int		sc_node_next;
+	uint64_t	sc_node_mask;
+};
+
 struct mainbus_attach_args {
 	const char     *ma_name;
-	int		ma_core;
+	int		ma_node;
 };
 
 #endif	/* _EVBMIPS_RMIXL_AUTOCONF_H_ */
