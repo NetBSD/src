af8e5f10d5720e3970bba74ac91f5c9460364b2e
matt <matt@NetBSD.org>
2010-01-21 04:09:05 +0000

    Needed for pci support.

---
+++ sys/arch/sbmips/include/pci_machdep.h
@@ -0,0 +1,6 @@
+/* $NetBSD: pci_machdep.h,v 1.1.2.1 2010/01/21 04:09:05 matt Exp $ */
+
+/* Before including <mips/pci_machdep.h> */
+#define	__HAVE_PCIIDE_MACHDEP_COMPAT_INTR_ESTABLISH
+
+#include <mips/pci_machdep.h>
