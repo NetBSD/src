b41fe51bacb77fde4e9e4873cdf27829ed63e670
matt <matt@NetBSD.org>
2010-01-21 04:07:56 +0000

    sbmips needs 64bit paddr_t

--- sys/arch/sbmips/include/types.h
+++ sys/arch/sbmips/include/types.h
@@ -1,4 +1,6 @@
-/* $NetBSD: types.h,v 1.7 2008/01/20 18:09:09 joerg Exp $ */
+/* $NetBSD: types.h,v 1.7.28.1 2010/01/21 04:07:56 matt Exp $ */
+
+#define	_MIPS_PADDR_T_64BIT
 
 #include <mips/types.h>
 
