d818ef69741fd5b0265b72a89b6fdc6c2554dede
matt <matt@NetBSD.org>
2010-01-21 06:59:11 +0000

    Add rest of pci framework functions. Disable pciide compat intr establish for sbmips

--- sys/arch/sbmips/include/pci_machdep.h
+++ sys/arch/sbmips/include/pci_machdep.h
@@ -1,6 +1,3 @@
-/* $NetBSD: pci_machdep.h,v 1.1.2.1 2010/01/21 04:09:05 matt Exp $ */
-
-/* Before including <mips/pci_machdep.h> */
-#define	__HAVE_PCIIDE_MACHDEP_COMPAT_INTR_ESTABLISH
+/* $NetBSD: pci_machdep.h,v 1.1.2.2 2010/01/21 06:59:12 matt Exp $ */
 
 #include <mips/pci_machdep.h>
