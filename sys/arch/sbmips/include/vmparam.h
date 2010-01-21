b60050c67304a326703114042ffa24d0f1b12f21
matt <matt@NetBSD.org>
2010-01-21 09:03:17 +0000

    Define the FIRST512 and FIRST4G vm freelists

--- sys/arch/sbmips/include/vmparam.h
+++ sys/arch/sbmips/include/vmparam.h
@@ -1,6 +1,19 @@
-/* $NetBSD: vmparam.h,v 1.1.142.1 2009/12/11 20:22:15 matt Exp $ */
+/* $NetBSD: vmparam.h,v 1.1.142.2 2010/01/21 09:03:17 matt Exp $ */
 
+#ifndef _SBMIPS_VMPARAM_H_
+#define _SBMIPS_VMPARAM_H_
 #include <mips/vmparam.h>
 
 /* XXXcgd */
 #define	VM_PHYSSEG_MAX		8
+
+#undef VM_FREELIST_MAX
+#define VM_FREELIST_MAX		3
+#if defined(_MIPS_PADDR_T_64BIT) || defined(_LP64)
+#define VM_FREELIST_FIRST4G	2
+#endif
+#if !defined(_LP64)
+#define VM_FREELIST_FIRST512M	1
+#endif
+
+#endif /* _SBMIPS_VMPARAM_H_ */
