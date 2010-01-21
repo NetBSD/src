b49e6f8cf399277afded087d7f3e139ab76964a4
matt <matt>
2010-01-21 04:08:18 +0000

    add some whitespace

--- sys/arch/sbmips/include/bus.h
+++ sys/arch/sbmips/include/bus.h
@@ -1,7 +1,8 @@
-/* $NetBSD: bus.h,v 1.1.2.1 2010/01/14 00:37:26 matt Exp $ */
+/* $NetBSD: bus.h,v 1.1.2.2 2010/01/21 04:08:18 matt Exp $ */
 
 #ifndef _SBMIPS_BUS_H_
 #define	_SBMIPS_BUS_H_
+
 #define _MIPS_NEED_BUS_DMA_BOUNCE
 
 #include <mips/bus_space.h>
