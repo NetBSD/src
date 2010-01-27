01c9dbd527c9ddf8d7f291794780647129ff64d0
sborrill <sborrill>
2010-01-27 21:33:36 +0000

    Pull up the following revisions(s) (requested by bouyer in ticket #1256): 	sys/dev/ic/mfi.c:	revision 1.21
    Don't release CCB twice. Found by code inspection.

--- sys/dev/ic/mfi.c
+++ sys/dev/ic/mfi.c
@@ -1,4 +1,4 @@
-/* $NetBSD: mfi.c,v 1.19.4.1 2009/09/10 07:06:34 snj Exp $ */
+/* $NetBSD: mfi.c,v 1.19.4.2 2010/01/27 21:33:36 sborrill Exp $ */
 /* $OpenBSD: mfi.c,v 1.66 2006/11/28 23:59:45 dlg Exp $ */
 /*
  * Copyright (c) 2006 Marco Peereboom <marco@peereboom.us>
@@ -17,7 +17,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: mfi.c,v 1.19.4.1 2009/09/10 07:06:34 snj Exp $");
+__KERNEL_RCSID(0, "$NetBSD: mfi.c,v 1.19.4.2 2010/01/27 21:33:36 sborrill Exp $");
 
 #include "bio.h"
 
@@ -1106,7 +1106,6 @@ mfi_scsipi_request(struct scsipi_channel *chan, scsipi_adapter_req_t req,
 			/* XXX check for sense in ccb->ccb_sense? */
 			printf("%s: mfi_scsipi_request poll failed\n",
 			    DEVNAME(sc));
-			mfi_put_ccb(ccb);
 			bzero(&xs->sense, sizeof(xs->sense));
 			xs->sense.scsi_sense.response_code =
 			    SSD_RCODE_VALID | SSD_RCODE_CURRENT;
