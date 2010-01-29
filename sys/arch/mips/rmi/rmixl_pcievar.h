581b24c436aec926662a9bc687f72737433b810b
cliff <cliff>
2010-01-29 00:23:34 +0000

    - add pcie interrupt dispatch stuff

--- sys/arch/mips/rmi/rmixl_pcievar.h
+++ sys/arch/mips/rmi/rmixl_pcievar.h
@@ -1,4 +1,4 @@
-/*      $NetBSD: rmixl_pcievar.h,v 1.1.2.2 2009/11/15 23:11:06 cliff Exp $	*/
+/*      $NetBSD: rmixl_pcievar.h,v 1.1.2.3 2010/01/29 00:23:34 cliff Exp $	*/
 
 #ifndef _MIPS_RMI_PCIE_VAR_H_
 #define _MIPS_RMI_PCIE_VAR_H_
@@ -22,7 +22,30 @@ typedef struct rmixl_pcie_lnktab {
 	const rmixl_pcie_lnkcfg_t *cfg;
 } rmixl_pcie_lnktab_t;
 
-struct rmixl_pcie_softc {
+typedef struct rmixl_pcie_link_dispatch {
+	LIST_ENTRY(rmixl_pcie_link_dispatch) next;
+	int (*func)(void *);
+	void *arg;
+	u_int link;
+	u_int bitno;
+	u_int irq;
+	struct evcnt count;
+} rmixl_pcie_link_dispatch_t;
+
+struct rmixl_pcie_softc;
+
+typedef struct rmixl_pcie_link_intr {
+	struct rmixl_pcie_softc *sc;
+	LIST_HEAD(, rmixl_pcie_link_dispatch) dispatch;
+	u_int link;
+	u_int ipl;
+	bool enabled;
+	void *ih;			/* mips interrupt handle */
+} rmixl_pcie_link_intr_t;
+
+#define RMIXL_PCIE_NLINKS_MAX	4
+
+typedef struct rmixl_pcie_softc {
 	device_t                	sc_dev;
 	struct mips_pci_chipset 	sc_pci_chipset;
 	bus_space_tag_t              	sc_pcie_cfg_memt;
@@ -31,7 +54,9 @@ struct rmixl_pcie_softc {
 	bus_dma_tag_t			sc_32bit_dmat;
 	bus_dma_tag_t			sc_64bit_dmat;
 	rmixl_pcie_lnktab_t		sc_pcie_lnktab;
-};
+	void 			       *sc_fatal_ih;
+	rmixl_pcie_link_intr_t		sc_link_intr[RMIXL_PCIE_NLINKS_MAX];
+} rmixl_pcie_softc_t;
 
 #endif  /* _MIPS_RMI_PCIE_VAR_H_ */
 
