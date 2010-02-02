f6d24259f392ffd08f493926853cf91014b5d57d
phx <phx@NetBSD.org>
2010-02-02 19:03:31 +0000

    Fix compilation warnings and errors with amigappc.

--- sys/arch/amiga/dev/hyper.c
+++ sys/arch/amiga/dev/hyper.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: hyper.c,v 1.19 2008/04/28 20:23:12 martin Exp $ */
+/*	$NetBSD: hyper.c,v 1.20 2010/02/02 19:03:31 phx Exp $ */
 
 /*-
  * Copyright (c) 1997,1998 The NetBSD Foundation, Inc.
@@ -30,7 +30,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: hyper.c,v 1.19 2008/04/28 20:23:12 martin Exp $");
+__KERNEL_RCSID(0, "$NetBSD: hyper.c,v 1.20 2010/02/02 19:03:31 phx Exp $");
 
 /*
  * zbus HyperCom driver
@@ -38,8 +38,8 @@ __KERNEL_RCSID(0, "$NetBSD: hyper.c,v 1.19 2008/04/28 20:23:12 martin Exp $");
 
 #include <sys/types.h>
 
-#include <sys/conf.h>
 #include <sys/device.h>
+#include <sys/conf.h>
 #include <sys/systm.h>
 #include <sys/param.h>
 
