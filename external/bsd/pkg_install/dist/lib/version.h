077047e21d2479042fe34894eb8a59aa900ddb82
joerg <joerg@NetBSD.org>
2010-02-03 19:27:01 +0000

    Fix enum to prevent crash (PR 42734)

--- external/bsd/pkg_install/dist/lib/version.h
+++ external/bsd/pkg_install/dist/lib/version.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: version.h,v 1.1.1.25 2010/02/03 14:23:59 joerg Exp $	*/
+/*	$NetBSD: version.h,v 1.2 2010/02/03 19:27:01 joerg Exp $	*/
 
 /*
  * Copyright (c) 2001 Thomas Klausner.  All rights reserved.
@@ -27,6 +27,6 @@
 #ifndef _INST_LIB_VERSION_H_
 #define _INST_LIB_VERSION_H_
 
-#define PKGTOOLS_VERSION 20100203
+#define PKGTOOLS_VERSION 20100204
 
 #endif /* _INST_LIB_VERSION_H_ */
