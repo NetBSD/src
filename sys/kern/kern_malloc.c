16568116ab37f944e5d90f6ecc4b6874fa2d26b8
bouyer <bouyer@NetBSD.org>
2010-02-14 13:37:42 +0000

    Pull up following revision(s) (requested by hubertf in ticket #1292): 	sys/kern/kern_malloc.c: revision 1.128 Let kernel build when MALLOCLOG is defined but DIAGNOSTIC is not. Else, hitmlog() is defined but not used, which triggers a warning.

--- sys/kern/kern_malloc.c
+++ sys/kern/kern_malloc.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: kern_malloc.c,v 1.121 2008/10/26 12:23:28 blymn Exp $	*/
+/*	$NetBSD: kern_malloc.c,v 1.121.4.1 2010/02/14 13:37:42 bouyer Exp $	*/
 
 /*
  * Copyright (c) 1987, 1991, 1993
@@ -66,7 +66,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: kern_malloc.c,v 1.121 2008/10/26 12:23:28 blymn Exp $");
+__KERNEL_RCSID(0, "$NetBSD: kern_malloc.c,v 1.121.4.1 2010/02/14 13:37:42 bouyer Exp $");
 
 #include <sys/param.h>
 #include <sys/proc.h>
@@ -215,6 +215,7 @@ domlog(void *a, long size, struct malloc_type *type, int action,
 		malloclogptr = 0;
 }
 
+#ifdef DIAGNOSTIC
 static void
 hitmlog(void *a)
 {
@@ -271,6 +272,7 @@ hitmlog(void *a)
 
 #undef PRT
 }
+#endif /* DIAGNOSTIC */
 #endif /* MALLOCLOG */
 
 #ifdef DIAGNOSTIC
