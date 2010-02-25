ec4306c95e6338f4004a23037e5a1dedfeef4a10
matt <matt@NetBSD.org>
2010-02-25 23:33:43 +0000

    Adapt to <spr.h> breakup.

--- sys/arch/evbppc/virtex/machdep.c
+++ sys/arch/evbppc/virtex/machdep.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: machdep.c,v 1.13 2010/02/08 19:02:28 joerg Exp $ */
+/*	$NetBSD: machdep.c,v 1.14 2010/02/25 23:33:44 matt Exp $ */
 
 /*
  * Copyright (c) 2006 Jachym Holecek
@@ -34,7 +34,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: machdep.c,v 1.13 2010/02/08 19:02:28 joerg Exp $");
+__KERNEL_RCSID(0, "$NetBSD: machdep.c,v 1.14 2010/02/25 23:33:44 matt Exp $");
 
 #include "opt_compat_netbsd.h"
 #include "opt_ddb.h"
@@ -71,6 +71,7 @@ __KERNEL_RCSID(0, "$NetBSD: machdep.c,v 1.13 2010/02/08 19:02:28 joerg Exp $");
 #include <machine/trap.h>
 
 #include <powerpc/spr.h>
+#include <powerpc/ibm4xx/spr.h>
 
 #include <evbppc/virtex/dcr.h>
 #include <evbppc/virtex/virtex.h>
