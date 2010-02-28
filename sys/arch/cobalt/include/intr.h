5141ff131a105ffadbf80e76b6b76a9464390bd7
matt <matt>
2010-02-28 04:04:46 +0000

    Adapt to new common interrupts. Enable cobalt64

--- sys/arch/cobalt/include/intr.h
+++ sys/arch/cobalt/include/intr.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: intr.h,v 1.32.12.1 2010/02/06 02:57:43 matt Exp $	*/
+/*	$NetBSD: intr.h,v 1.32.12.2 2010/02/28 04:04:46 matt Exp $	*/
 
 /*
  * Copyright (c) 2000 Soren S. Jorvang.  All rights reserved.
@@ -28,58 +28,13 @@
 #ifndef	_COBALT_INTR_H_
 #define	_COBALT_INTR_H_
 
-#define	IPL_NONE	0	/* Disable only this interrupt. */
-#define	IPL_SOFTCLOCK	1	/* generic software interrupts */
-#define	IPL_SOFTBIO	1	/* clock software interrupts */
-#define	IPL_SOFTNET	2	/* network software interrupts */
-#define	IPL_SOFTSERIAL	2	/* serial software interrupts */
-#define	IPL_VM		3	/* Memory allocation */
-#define	IPL_SCHED	4	/* Disable clock interrupts. */
-#define	IPL_HIGH	4	/* Disable all interrupts. */
-#define NIPL		5
-
-/* Interrupt sharing types. */
-#define IST_NONE	0	/* none */
-#define IST_PULSE	1	/* pulsed */
-#define IST_EDGE	2	/* edge-triggered */
-#define IST_LEVEL	3	/* level-triggered */
+#include <mips/intr.h>
 
 #ifdef _KERNEL
 #ifndef _LOCORE
 
 #include <sys/evcnt.h>
 #include <mips/cpuregs.h>
-#include <mips/locore.h>
-
-#define SPLVM		(MIPS_SOFT_INT_MASK_0 | MIPS_SOFT_INT_MASK_1 | \
-			MIPS_INT_MASK_1 | MIPS_INT_MASK_2 | \
-			MIPS_INT_MASK_3 | MIPS_INT_MASK_4)
-#define SPLSCHED	(SPLVM | MIPS_INT_MASK_5)
-
-#define spl0()          (void)_spllower(0)
-#define splx(s)         (void)_splset(s)
-#define splvm()		_splraise(SPLVM)
-#define splsched()	_splraise(SPLSCHED)
-#define splhigh()       _splraise(MIPS_INT_MASK)
-
-#define splsoftclock()	_splraise(MIPS_SOFT_INT_MASK_0)
-#define splsoftbio()	_splraise(MIPS_SOFT_INT_MASK_0)
-#define splsoftnet()	_splraise(MIPS_SOFT_INT_MASK_0|MIPS_SOFT_INT_MASK_1)
-#define splsoftserial()	_splraise(MIPS_SOFT_INT_MASK_0|MIPS_SOFT_INT_MASK_1)
-
-typedef int ipl_t;
-typedef struct {
-	int _spl;
-} ipl_cookie_t;
-
-ipl_cookie_t makeiplcookie(ipl_t);
-
-static inline int
-splraiseipl(ipl_cookie_t icookie)
-{
-
-	return _splraise(icookie._spl);
-}
 
 #define NCPU_INT	6
 #define NICU_INT	16
---
+++ sys/arch/cobalt/include/netbsd32_machdep.h
@@ -0,0 +1,3 @@
+/* $NetBSD: netbsd32_machdep.h,v 1.1.4.2 2010/02/28 04:04:46 matt Exp $ */
+
+#include <mips/netbsd32_machdep.h>
