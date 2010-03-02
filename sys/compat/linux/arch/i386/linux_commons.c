efebafd598f9158ce962f7106c4756603f7d2bc2
pooka <pooka@NetBSD.org>
2010-03-02 15:46:19 +0000

    Nuke fs_lfs.h included for unknown reasons (if there was a reason other than "accident", i hereby declare that reason invalid)

--- sys/compat/linux/arch/i386/linux_commons.c
+++ sys/compat/linux/arch/i386/linux_commons.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: linux_commons.c,v 1.13 2008/11/19 18:36:03 ad Exp $	*/
+/*	$NetBSD: linux_commons.c,v 1.14 2010/03/02 15:46:19 pooka Exp $	*/
 
 /*
  * This file includes C files from the common
@@ -13,11 +13,10 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(1, "$NetBSD: linux_commons.c,v 1.13 2008/11/19 18:36:03 ad Exp $");
+__KERNEL_RCSID(1, "$NetBSD: linux_commons.c,v 1.14 2010/03/02 15:46:19 pooka Exp $");
 
 #if defined(_KERNEL_OPT)
 #include "opt_sysv.h"
-#include "fs_lfs.h"
 #endif
 
 #include <sys/param.h>
