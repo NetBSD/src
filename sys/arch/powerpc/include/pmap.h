52f40f45acf037afbdfc603a67a3d7511330b91c
matt <matt@NetBSD.org>
2010-03-09 22:40:06 +0000

    Include BookE version of pmap.h

--- sys/arch/powerpc/include/pmap.h
+++ sys/arch/powerpc/include/pmap.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: pmap.h,v 1.34 2008/12/09 20:45:45 pooka Exp $	*/
+/*	$NetBSD: pmap.h,v 1.35 2010/03/09 22:40:06 matt Exp $	*/
 
 #ifdef _KERNEL_OPT
 #include "opt_ppcarch.h"
@@ -6,6 +6,8 @@
 
 #ifdef PPC_IBM4XX
 #include <powerpc/ibm4xx/pmap.h>
+#elif defined(PPC_BOOKE)
+#include <powerpc/booke/pmap.h>
 #elif defined(PPC_OEA) || defined (PPC_OEA64) || defined (PPC_OEA64_BRIDGE)
 #include <powerpc/oea/pmap.h>
 #else
