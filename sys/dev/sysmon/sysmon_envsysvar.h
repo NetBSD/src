c652cc4f07a2142c40a24cc36f59037927022a1c
pgoyette <pgoyette@NetBSD.org>
2010-03-19 01:16:44 +0000

    Separate event delivery from polling and limit evaluation in sme_events_worker().
    Provide a wrapper that can be called from sensor drivers' interrupt
    routines to find and deliver a specific event.
    
    XXX Actually updating sensor drivers (and their parent devices) to
    XXX have interrupts is a future enhancement.  This is simply an
    XXX enabler.

--- sys/dev/sysmon/sysmon_envsysvar.h
+++ sys/dev/sysmon/sysmon_envsysvar.h
@@ -1,4 +1,4 @@
-/* $NetBSD: sysmon_envsysvar.h,v 1.32 2010/03/14 18:03:15 pgoyette Exp $ */
+/* $NetBSD: sysmon_envsysvar.h,v 1.33 2010/03/19 01:16:44 pgoyette Exp $ */
 
 /*-
  * Copyright (c) 2007, 2008 Juan Romero Pardines.
@@ -129,7 +129,9 @@ void	sme_event_drvadd(void *);
 int	sme_events_init(struct sysmon_envsys *);
 void	sme_events_destroy(struct sysmon_envsys *);
 void	sme_events_check(void *);
-void 	sme_events_worker(struct work *, void *);
+void	sme_events_worker(struct work *, void *);
+void	sme_deliver_event(sme_event_t *);
+void	sme_deliver_typed_event(struct sysmon_envsys *, envsys_data_t *, int);
 
 /* 
  * common functions to create/update objects in a dictionary.
