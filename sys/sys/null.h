f04bdfbc9d987cdd1444e7c4e8bf70654a775380
snj <snj@NetBSD.org>
2010-03-21 03:45:33 +0000

    Pull up following revision(s) (requested by joerg in ticket #1344): 	sys/sys/null.h: revision 1.8 Enclose (void *)0 in an extra set of parenthese to make the result usable in arbitrary expressions. Fixes PR/41890, I can't think of any downsides!

--- sys/sys/null.h
+++ sys/sys/null.h
@@ -1,11 +1,11 @@
-/*	$NetBSD: null.h,v 1.7 2005/12/03 17:10:46 christos Exp $	*/
+/*	$NetBSD: null.h,v 1.7.86.1 2010/03/21 03:45:33 snj Exp $	*/
 
 #ifndef _SYS_NULL_H_
 #define _SYS_NULL_H_
 #ifndef	NULL
 #if !defined(__GNUG__) || __GNUG__ < 2 || (__GNUG__ == 2 && __GNUC_MINOR__ < 90)
 #if !defined(__cplusplus)
-#define	NULL	(void *)0
+#define	NULL	((void *)0)
 #else
 #define	NULL	0
 #endif /* !__cplusplus */
