6d15884e49aec5893236aeda01dae00b03ba8800
pooka <pooka@NetBSD.org>
2010-03-25 19:54:08 +0000

    use config_init_component()

--- sys/rump/dev/lib/libubt/ubt_at_usb.c
+++ sys/rump/dev/lib/libubt/ubt_at_usb.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: ubt_at_usb.c,v 1.2 2010/03/22 14:47:02 pooka Exp $	*/
+/*	$NetBSD: ubt_at_usb.c,v 1.3 2010/03/25 19:54:08 pooka Exp $	*/
 
 #include <sys/param.h>
 #include <sys/types.h>
@@ -11,21 +11,8 @@
 #include "rump_private.h"
 #include "rump_dev_private.h"
 
-#define FLAWLESSCALL(call)						\
-do {									\
-	int att_error;							\
-	if ((att_error = call) != 0)					\
-		panic("\"%s\" failed", #call);				\
-} while (/*CONSTCOND*/0)
-
 RUMP_COMPONENT(RUMP_COMPONENT_DEV)
 {
 
-	FLAWLESSCALL(config_cfdata_attach(cfdata_ubt, 0));
-
-	FLAWLESSCALL(config_cfdriver_attach(&ubt_cd));
-	FLAWLESSCALL(config_cfattach_attach("ubt", &ubt_ca));
-
-	FLAWLESSCALL(config_cfdriver_attach(&bthub_cd));
-	FLAWLESSCALL(config_cfattach_attach("bthub", &bthub_ca));
+	config_init_component(cfdriver_comp_ubt, cfattach_comp_ubt, cfdata_ubt);
 }
