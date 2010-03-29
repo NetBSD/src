ddc15e7725586136283875bd7c433fd959e9d340
pooka <pooka@NetBSD.org>
2010-03-29 13:41:06 +0000

    5.99.25: fifofs stopped exporting its private parts

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.359 2010/01/19 22:12:00 pooka Exp $	*/
+/*	$NetBSD: param.h,v 1.360 2010/03/29 13:41:06 pooka Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -63,7 +63,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	599002400	/* NetBSD 5.99.24 */
+#define	__NetBSD_Version__	599002500	/* NetBSD 5.99.25 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
