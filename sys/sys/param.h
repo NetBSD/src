bb89b7208d3b4ed315ac8fb4278d1748da102e9f
he <he@NetBSD.org>
2010-04-05 07:16:12 +0000

    Extend struct malloc_type to count the number of active allocations per size, and make vmstat report this information under the "Memory statistics by type" display, which is only printed when the kernel has been compiled with KMEMSTATS defined, like this:
    Memory statistics by type                                Type  Kern
               Type InUse  MemUse HighUse   Limit   Requests Limit Limit Size(s)
              wapbl    15   4192K   4192K  78644K     376426     0     0 32:0,256:3,512:6,131072:1,262144:2,524288:3
    
    Since struct malloc_type is user-visible and is changed, bump kernel
    revision to 5.99.26.
    
    While it is true that malloc(9) is in general on the path of slowly
    being replaced by kmem(9) (kmem_alloc/kmem_free), there remains a
    lot of points of usage of malloc/free, and this could aid in finding
    any leaks.  (It helped finding the leak fixed in PR#42661.)
    
    This was discussed with and somewhat hestitantly OKed by rmind@

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.360 2010/03/29 13:41:06 pooka Exp $	*/
+/*	$NetBSD: param.h,v 1.361 2010/04/05 07:16:12 he Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -63,7 +63,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	599002500	/* NetBSD 5.99.25 */
+#define	__NetBSD_Version__	599002600	/* NetBSD 5.99.26 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
