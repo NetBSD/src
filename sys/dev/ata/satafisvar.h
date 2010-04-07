9d1be6b12ecd991ca7510e8cdb1feae1e0b5e7b5
jakllsch <jakllsch@NetBSD.org>
2010-04-07 17:51:16 +0000

    satafis: - Add function to parse RDH FIS for use in implementing AT_READREG. - Correct and clean up some structure definitions. - Sprinkle a bit of const. - Remove dependency on <dev/ic/wdcreg.h>, WDCTL_4BIT doesn't seem   to be specified by any recent ATA standard, and it seems to make   no difference in practice. - Stop using WDSD_IBM, these bits have been obsolete since before SATA.
    siisata:
    - Implement AT_READREG.
    - Reap now-unneeded <dev/ic/wdcreg.h>.
    - Whitespace and slight debug code cleanup.
    - Some possibly-uncessary code reordering.
    
    ahcisata:
    - Implement AT_READREG.
    - Reap now-unneeded <dev/ic/wdcreg.h>.

--- sys/dev/ata/satafisvar.h
+++ sys/dev/ata/satafisvar.h
@@ -1,7 +1,7 @@
-/* $NetBSD: satafisvar.h,v 1.2 2009/07/04 20:57:15 jakllsch Exp $ */
+/* $NetBSD: satafisvar.h,v 1.3 2010/04/07 17:51:16 jakllsch Exp $ */
 
-/*-
- * Copyright (c) 2009 Jonathan A. Kollasch.
+/*
+ * Copyright (c) 2009, 2010 Jonathan A. Kollasch.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -33,6 +33,8 @@
 void satafis_rhd_construct_cmd(struct ata_command *, uint8_t *);
 void satafis_rhd_construct_bio(struct ata_xfer *, uint8_t *);
 void satafis_rhd_construct_atapi(struct ata_xfer *, uint8_t *);
-void satafis_rdh_parse(struct ata_channel *, uint8_t *);
+
+void satafis_rdh_parse(struct ata_channel *, const uint8_t *);
+void satafis_rdh_cmd_readreg(struct ata_command *, const uint8_t *);
 
 #endif /* _DEV_ATA_FISVAR_H_ */
