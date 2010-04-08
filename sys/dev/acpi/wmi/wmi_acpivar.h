be68114264c78bd7731aaf48cd4b7fca03debbdd
jruoho <jruoho@NetBSD.org>
2010-04-08 12:14:19 +0000

    Move acpiwmi(4) to the "sys/dev/acpi/wmi" -subdirectory.

--- sys/dev/acpi/wmi/wmi_acpivar.h
+++ sys/dev/acpi/wmi/wmi_acpivar.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: wmi_acpivar.h,v 1.8 2010/04/08 10:33:13 jruoho Exp $	*/
+/*	$NetBSD: wmi_acpivar.h,v 1.1 2010/04/08 12:14:19 jruoho Exp $	*/
 
 /*-
  * Copyright (c) 2009, 2010 Jukka Ruohonen <jruohonen@iki.fi>
@@ -31,7 +31,7 @@
 #define _SYS_DEV_ACPI_WMI_ACPIVAR_H
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: wmi_acpivar.h,v 1.8 2010/04/08 10:33:13 jruoho Exp $");
+__KERNEL_RCSID(0, "$NetBSD: wmi_acpivar.h,v 1.1 2010/04/08 12:14:19 jruoho Exp $");
 
 ACPI_STATUS	acpi_wmi_event_register(device_t, ACPI_NOTIFY_HANDLER);
 ACPI_STATUS	acpi_wmi_event_deregister(device_t);
