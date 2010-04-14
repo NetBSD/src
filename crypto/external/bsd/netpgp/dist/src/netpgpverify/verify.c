937298e7265571c83944060c8731d95d9c198822
agc <agc@NetBSD.org>
2010-04-14 06:31:23 +0000

    Update netpgp to version 3.99.1
    Changes to 3.99.1/20100413
    
    + bump major command versions to be compatible with shlib major
    + fixed a number of bugs in (RSA) key generation
    + modified netpgpkeys(1) to take an optional argument to --generate-key
      if the argument is provided, it is used as the equivalent of the gecos
      field for the newly-generated key.

--- crypto/external/bsd/netpgp/dist/src/netpgpverify/verify.c
+++ crypto/external/bsd/netpgp/dist/src/netpgpverify/verify.c
@@ -55,7 +55,7 @@
 
 #if defined(__NetBSD__)
 __COPYRIGHT("@(#) Copyright (c) 2009 The NetBSD Foundation, Inc. All rights reserved.");
-__RCSID("$NetBSD: verify.c,v 1.8 2010/03/16 04:14:30 agc Exp $");
+__RCSID("$NetBSD: verify.c,v 1.9 2010/04/14 06:31:23 agc Exp $");
 #endif
 
 #include <sys/types.h>
@@ -161,7 +161,7 @@ __RCSID("$NetBSD: verify.c,v 1.8 2010/03/16 04:14:30 agc Exp $");
 #undef USE_SHA384
 
 /* development versions have .99 suffix */
-#define NETPGP_BASE_VERSION	"2.99.2"
+#define NETPGP_BASE_VERSION	"3.99.1"
 
 #define NETPGP_VERSION_CAT(a, b)	"NetPGP portable " a "/[" b "]"
 #define NETPGP_VERSION_STRING \
