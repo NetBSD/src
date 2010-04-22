8efab1fe75f8c1cdd28905ff59b0c7351eb2eb69
jym <jym@NetBSD.org>
2010-04-22 21:02:25 +0000

    CPUID_TSC => cpu_hascounter(). This one was missing in my cpu_feature rework patch.
    Should fix the i386/ALL build issue many have reported. Thanks!

--- sys/arch/i386/i386/pmc.c
+++ sys/arch/i386/i386/pmc.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: pmc.c,v 1.17 2008/05/11 14:44:54 ad Exp $	*/
+/*	$NetBSD: pmc.c,v 1.18 2010/04/22 21:02:25 jym Exp $	*/
 
 /*-
  * Copyright (c) 2000 Zembu Labs, Inc.
@@ -38,7 +38,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: pmc.c,v 1.17 2008/05/11 14:44:54 ad Exp $");
+__KERNEL_RCSID(0, "$NetBSD: pmc.c,v 1.18 2010/04/22 21:02:25 jym Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -122,7 +122,7 @@ pmc_init(void)
 		break;
 	}
 
-	if (pmc_type != PMC_TYPE_NONE && (cpu_feature & CPUID_TSC) != 0)
+	if (pmc_type != PMC_TYPE_NONE && cpu_hascounter())
 		pmc_flags |= PMC_INFO_HASTSC;
 
 #ifdef MULTIPROCESSOR
