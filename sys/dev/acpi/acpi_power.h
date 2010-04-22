6c7f25e4c7fd1a57f3fdf130bdea77fa15269eed
jruoho <jruoho@NetBSD.org>
2010-04-22 18:40:09 +0000

    Merge new code for ACPI power resources.
    The old code served us well, but a major overhaul would have been needed for
    it to cope with the increased demands of the code -- and the specifications.
    
    ok jmcneill@, pgoyette@

---
+++ sys/dev/acpi/acpi_power.h
@@ -0,0 +1,44 @@
+/* $NetBSD: acpi_power.h,v 1.1 2010/04/22 18:40:09 jruoho Exp $ */
+
+/*-
+ * Copyright (c) 2009, 2010 The NetBSD Foundation, Inc.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to The NetBSD Foundation
+ * by Jukka Ruohonen.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef _SYS_DEV_ACPI_ACPI_POWER_H
+#define _SYS_DEV_ACPI_ACPI_POWER_H
+
+#define ACPI_STATE_ERROR	-1
+
+void	acpi_power_res_add(struct acpi_devnode *);
+bool	acpi_power_register(struct acpi_devnode *);
+void	acpi_power_deregister(struct acpi_devnode *);
+bool	acpi_power_get(struct acpi_devnode *, int *);
+bool	acpi_power_set(struct acpi_devnode *, int);
+bool	acpi_power_set_from_handle(ACPI_HANDLE, int);
+
+#endif	/* !_SYS_DEV_ACPI_ACPI_POWER_H */
