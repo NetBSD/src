f8a8ba56b133c9b06b709ead5b3166af8ec6468b
dbj <dbj@NetBSD.org>
2010-04-24 19:58:13 +0000

    switch from 4 clause to 2 clause BSD license.

--- sys/arch/next68k/next68k/clock.c
+++ sys/arch/next68k/next68k/clock.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: clock.c,v 1.11 2006/09/11 15:07:50 gdamore Exp $	*/
+/*	$NetBSD: clock.c,v 1.12 2010/04/24 19:58:13 dbj Exp $	*/
 /*
  * Copyright (c) 1998 Darrin B. Jewell
  * All rights reserved.
@@ -11,11 +11,6 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *      This product includes software developed by Darrin B. Jewell
- * 4. The name of the author may not be used to endorse or promote products
- *    derived from this software without specific prior written permission
  *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
@@ -30,7 +25,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: clock.c,v 1.11 2006/09/11 15:07:50 gdamore Exp $");
+__KERNEL_RCSID(0, "$NetBSD: clock.c,v 1.12 2010/04/24 19:58:13 dbj Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
