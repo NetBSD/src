a9f6aaa132beb6c12b2205a112b9695e1a49933e
jakllsch <jakllsch@NetBSD.org>
2010-05-02 18:49:13 +0000

    This only needs one bus_space_tag_t. While here, rename one of the bus_space_handle_ts to be be descriptive of its function.
    Fixes crash caused by uninitialized bus_space_tag_t
    discovered by Paul Goyette.

--- sys/dev/isa/itesio_isavar.h
+++ sys/dev/isa/itesio_isavar.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: itesio_isavar.h,v 1.6 2008/04/26 19:01:53 xtraeme Exp $	*/
+/*	$NetBSD: itesio_isavar.h,v 1.7 2010/05/02 18:49:13 jakllsch Exp $	*/
 /*	$OpenBSD: itvar.h,v 1.2 2003/11/05 20:57:10 grange Exp $	*/
 
 /*
@@ -139,9 +139,8 @@
 
 struct itesio_softc {
 	bus_space_tag_t 	sc_iot;
-	bus_space_handle_t 	sc_ioh;
 
-	bus_space_tag_t		sc_ec_iot;
+	bus_space_handle_t 	sc_pnp_ioh;
 	bus_space_handle_t	sc_ec_ioh;
 
 	struct sysmon_wdog	sc_smw;
