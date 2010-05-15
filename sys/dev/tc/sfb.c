83f23f325bab2699a4aac18acaf182ece63c7e6c
tsutsui <tsutsui@NetBSD.org>
2010-05-15 07:01:37 +0000

    Set RI_NO_AUTO in ri_flg if *fb_common_init() is invoked from cnattach. Untested.

--- sys/dev/tc/sfb.c
+++ sys/dev/tc/sfb.c
@@ -1,4 +1,4 @@
-/* $NetBSD: sfb.c,v 1.80 2009/08/22 17:38:06 tsutsui Exp $ */
+/* $NetBSD: sfb.c,v 1.81 2010/05/15 07:01:37 tsutsui Exp $ */
 
 /*-
  * Copyright (c) 1998, 1999 The NetBSD Foundation, Inc.
@@ -30,7 +30,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: sfb.c,v 1.80 2009/08/22 17:38:06 tsutsui Exp $");
+__KERNEL_RCSID(0, "$NetBSD: sfb.c,v 1.81 2010/05/15 07:01:37 tsutsui Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -345,6 +345,8 @@ sfb_common_init(struct rasops_info *ri)
 	sfbhwinit(base);
 
 	ri->ri_flg = RI_CENTER;
+	if (ri == &sfb_console_ri)
+		ri->ri_flg |= RI_NO_AUTO;
 	ri->ri_depth = 8;
 	ri->ri_width = (hsetup & 0x1ff) << 2;
 	ri->ri_height = (vsetup & 0x7ff);
