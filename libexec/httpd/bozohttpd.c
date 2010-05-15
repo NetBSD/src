14a8282e454989975c8e2cda5e31c578f05532c0
mrg <mrg@NetBSD.org>
2010-05-15 06:47:15 +0000

    import bozohttpd 20100512.  it includes these changes:         o  fix SSL mode.  from rtr.

--- libexec/httpd/bozohttpd.c
+++ libexec/httpd/bozohttpd.c
@@ -1,4 +1,4 @@
-/*	$eterna: bozohttpd.c,v 1.168 2010/05/10 14:49:19 mrg Exp $	*/
+/*	$eterna: bozohttpd.c,v 1.169 2010/05/13 04:17:58 mrg Exp $	*/
 
 /*
  * Copyright (c) 1997-2010 Matthew R. Green
@@ -107,7 +107,7 @@
 #define INDEX_HTML		"index.html"
 #endif
 #ifndef SERVER_SOFTWARE
-#define SERVER_SOFTWARE		"bozohttpd/20100510"
+#define SERVER_SOFTWARE		"bozohttpd/20100512"
 #endif
 #ifndef DIRECT_ACCESS_FILE
 #define DIRECT_ACCESS_FILE	".bzdirect"
