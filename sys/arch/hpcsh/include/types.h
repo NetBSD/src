978902bf61e1eb260ff4cf5c0b1dcaaa83f37e0a
tsutsui <tsutsui@NetBSD.org>
2010-05-22 15:51:31 +0000

    HPW-50PA PERSONA seems to use a different RTC base year value (1996, not 1900 or 2000) so handle the quirk by device properties and set it in MD device_register(). Also make RTC baseyear value patchable in sh3/dev/rtc.c.
    No visible changes to other sh3 RTC users.

--- sys/arch/hpcsh/include/types.h
+++ sys/arch/hpcsh/include/types.h
@@ -1,7 +1,9 @@
-/*	$NetBSD: types.h,v 1.3 2002/02/28 03:17:28 simonb Exp $	*/
+/*	$NetBSD: types.h,v 1.4 2010/05/22 15:51:32 tsutsui Exp $	*/
 #ifndef _HPCSH_TYPES_H_
 #define	_HPCSH_TYPES_H_
 
 #include <sh3/types.h>
+
+#define __HAVE_DEVICE_REGISTER
 
 #endif /* _HPCSH_TYPES_H_ */
