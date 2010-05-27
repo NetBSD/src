a058742c019d9aa71eca49f95cc2b985166e784a
pooka <pooka@NetBSD.org>
2010-05-27 23:40:12 +0000

    Mark files removed in the in-memory structure.  This allows us to do two things:
     1) properly set "recycle?" in inactive
     2) easily check if we are renaming a removed vnode.  without the
        check, it was possible to enter a dirent in the file system for
        a removed (and hence scheduled to be vcleaned) vnode.  this would
        lead to the succesful vget() of a clean vnode.  the use of the
        cleaned vnode was, however, less succesful, except for purposes
        of crashing.

--- sys/fs/sysvbfs/sysvbfs.h
+++ sys/fs/sysvbfs/sysvbfs.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: sysvbfs.h,v 1.8 2008/09/04 12:07:30 pooka Exp $	*/
+/*	$NetBSD: sysvbfs.h,v 1.9 2010/05/27 23:40:12 pooka Exp $	*/
 
 /*-
  * Copyright (c) 2004 The NetBSD Foundation, Inc.
@@ -52,6 +52,7 @@ struct sysvbfs_node {
 	int update_ctime;
 	int update_atime;
 	int update_mtime;
+	int removed;
 
 	LIST_ENTRY(sysvbfs_node) link;
 };
