237e130e9d0c61e0830911b697235d6889177c12
uebayasi <uebayasi@NetBSD.org>
2010-05-28 15:42:17 +0000

    Redo previous.

--- sys/arch/acorn26/acorn26/pmap.c
+++ sys/arch/acorn26/acorn26/pmap.c
@@ -1,4 +1,4 @@
-/* $NetBSD: pmap.c,v 1.30.2.3 2010/05/28 15:40:05 uebayasi Exp $ */
+/* $NetBSD: pmap.c,v 1.30.2.4 2010/05/28 15:42:17 uebayasi Exp $ */
 /*-
  * Copyright (c) 1997, 1998, 2000 Ben Harris
  * All rights reserved.
@@ -102,7 +102,7 @@
 
 #include <sys/param.h>
 
-__KERNEL_RCSID(0, "$NetBSD: pmap.c,v 1.30.2.3 2010/05/28 15:40:05 uebayasi Exp $");
+__KERNEL_RCSID(0, "$NetBSD: pmap.c,v 1.30.2.4 2010/05/28 15:42:17 uebayasi Exp $");
 
 #include <sys/kernel.h> /* for cold */
 #include <sys/malloc.h>
@@ -634,7 +634,7 @@ pmap_enter(pmap_t pmap, vaddr_t va, paddr_t pa, vm_prot_t prot, u_int flags)
 	UVMHIST_FUNC("pmap_enter");
 
 	UVMHIST_CALLED(pmaphist);
-	return pmap_enter1(pmap, va, pa, prot, flags, 1);
+	return pmap_enter1(pmap, va, pa, prot, flags, 0);
 }
 
 static int
