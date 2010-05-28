89dde936efc894f596918013e617c615b6d97a40
mrg <mrg@NetBSD.org>
2010-05-28 06:24:59 +0000

    add / retire some xf86-video drivers.

--- share/mk/bsd.own.mk
+++ share/mk/bsd.own.mk
@@ -1,4 +1,4 @@
-#	$NetBSD: bsd.own.mk,v 1.625 2010/04/23 19:41:03 joerg Exp $
+#	$NetBSD: bsd.own.mk,v 1.626 2010/05/28 06:24:59 mrg Exp $
 
 # This needs to be before bsd.init.mk
 .if defined(BSD_MK_COMPAT_FILE)
@@ -943,10 +943,13 @@ X11SRCDIR.xf86-input-${_i}?=	${X11SRCDIRMIT}/xf86-input-${_i}/dist
 .endfor
 
 .for _v in \
-	ag10e apm ark ast ati chips cirrus crime cyrix glint i128 i740 igs \
-	imstt intel mach64 mga neomagic newport nsc nv nvxbox pnozz r128 \
-	radeonhd rendition s3 s3virge savage siliconmotion sis suncg14 suncg6 \
-	sunffb sunleo suntcx tdfx tga trident tseng vesa vga via vmware wsfb
+	ag10e apm ark ast ati chips cirrus crime \
+	geode glint i128 i740 igs imstt intel mach64 mga \
+	neomagic newport nsc nv nvxbox openchrome pnozz \
+	r128 radeonhd rendition \
+	s3 s3virge savage siliconmotion sis suncg14 \
+	suncg6 sunffb sunleo suntcx \
+	tdfx tga trident tseng vesa vga via vmware wsfb
 	
 X11SRCDIR.xf86-video-${_v}?=	${X11SRCDIRMIT}/xf86-video-${_v}/dist
 .endfor
