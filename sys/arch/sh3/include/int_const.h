20185c9c23178bd6726d0a774b06320ade33f364
tnozaki <tnozaki>
2010-05-29 17:33:57 +0000

    fix wrong integer promotion rule(removed U suffix from UINT{8,16}_C). see ISO/IEC 9899:1999 7.18.4.3.

--- sys/arch/sh3/include/int_const.h
+++ sys/arch/sh3/include/int_const.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: int_const.h,v 1.3 2008/04/28 20:23:35 martin Exp $	*/
+/*	$NetBSD: int_const.h,v 1.4 2010/05/29 17:33:58 tnozaki Exp $	*/
 
 /*-
  * Copyright (c) 2001 The NetBSD Foundation, Inc.
@@ -43,8 +43,8 @@
 #define	INT32_C(c)	c
 #define	INT64_C(c)	c ## LL
 
-#define	UINT8_C(c)	c ## U
-#define	UINT16_C(c)	c ## U
+#define	UINT8_C(c)	c
+#define	UINT16_C(c)	c
 #define	UINT32_C(c)	c ## U
 #define	UINT64_C(c)	c ## ULL
 
