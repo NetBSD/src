d76ef2bf513e5f10dc0a1c6ff751ff7740d6f046
jruoho <jruoho>
2010-06-07 14:07:25 +0000

    Refactor slightly: we need to expose the function that turns individual power resource on or off.

--- sys/dev/acpi/acpi_power.h
+++ sys/dev/acpi/acpi_power.h
@@ -1,4 +1,4 @@
-/* $NetBSD: acpi_power.h,v 1.4 2010/06/07 13:04:31 jruoho Exp $ */
+/* $NetBSD: acpi_power.h,v 1.5 2010/06/07 14:07:25 jruoho Exp $ */
 
 /*-
  * Copyright (c) 2009, 2010 The NetBSD Foundation, Inc.
@@ -34,12 +34,13 @@
 
 #define ACPI_STATE_ERROR	-1
 
-bool	acpi_power_register(struct acpi_devnode *);
-void	acpi_power_deregister(struct acpi_devnode *);
-void	acpi_power_deregister_from_handle(ACPI_HANDLE);
-bool	acpi_power_get(struct acpi_devnode *, int *);
-bool	acpi_power_set(struct acpi_devnode *, int);
-bool	acpi_power_set_from_handle(ACPI_HANDLE, int);
-void	acpi_power_add(struct acpi_devnode *);
+bool		acpi_power_register(struct acpi_devnode *);
+void		acpi_power_deregister(struct acpi_devnode *);
+void		acpi_power_deregister_from_handle(ACPI_HANDLE);
+bool		acpi_power_get(struct acpi_devnode *, int *);
+bool		acpi_power_set(struct acpi_devnode *, int);
+bool		acpi_power_set_from_handle(ACPI_HANDLE, int);
+ACPI_STATUS	acpi_power_res(ACPI_HANDLE, ACPI_HANDLE, bool);
+void		acpi_power_add(struct acpi_devnode *);
 
 #endif	/* !_SYS_DEV_ACPI_ACPI_POWER_H */
