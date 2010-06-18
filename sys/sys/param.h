dce448ab02e54735cae7cfab7c4201383b165e2f
hannken <hannken>
2010-06-18 16:29:01 +0000

    Remove the concept of recursive vnode locks by eliminating vn_setrecurse(), vn_restorerecurse() and LK_CANRECURSE. Welcome to 5.99.31
    Discussed on tech-kern.

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.365 2010/06/06 08:01:32 hannken Exp $	*/
+/*	$NetBSD: param.h,v 1.366 2010/06/18 16:29:02 hannken Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -63,7 +63,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	599003000	/* NetBSD 5.99.30 */
+#define	__NetBSD_Version__	599003100	/* NetBSD 5.99.31 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
