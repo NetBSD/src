a0bdfc6191232f84c2d9902fc90baaa896a3bae6
matt <matt@NetBSD.org>
2010-06-19 19:47:34 +0000

    Misc fixes to get this to compile again.

--- sys/arch/arm/at91/at91pio.c
+++ sys/arch/arm/at91/at91pio.c
@@ -1,5 +1,5 @@
-/*	$Id: at91pio.c,v 1.2 2008/07/03 01:15:38 matt Exp $	*/
-/*	$NetBSD: at91pio.c,v 1.2 2008/07/03 01:15:38 matt Exp $	*/
+/*	$Id: at91pio.c,v 1.3 2010/06/19 19:47:34 matt Exp $	*/
+/*	$NetBSD: at91pio.c,v 1.3 2010/06/19 19:47:34 matt Exp $	*/
 
 /*
  * Copyright (c) 2007 Embedtronics Oy. All rights reserved.
@@ -30,12 +30,13 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: at91pio.c,v 1.2 2008/07/03 01:15:38 matt Exp $");
+__KERNEL_RCSID(0, "$NetBSD: at91pio.c,v 1.3 2010/06/19 19:47:34 matt Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
 #include <sys/kernel.h>
 #include <sys/device.h>
+#include <sys/gpio.h>
 #include <machine/bus.h>
 #include <machine/intr.h>
 #include <dev/gpio/gpiovar.h>
