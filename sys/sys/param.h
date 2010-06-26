dc0da7bbb2500d3eba12598942492e0a858d3009
kefren <kefren>
2010-06-26 14:24:27 +0000

    Add MPLS support, proposed on tech-net@ a couple of days ago
    Welcome to 5.99.33

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.367 2010/06/24 13:03:18 hannken Exp $	*/
+/*	$NetBSD: param.h,v 1.368 2010/06/26 14:24:27 kefren Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -63,7 +63,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	599003200	/* NetBSD 5.99.32 */
+#define	__NetBSD_Version__	599003300	/* NetBSD 5.99.33 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
