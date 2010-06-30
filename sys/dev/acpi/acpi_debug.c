8baa4e629e13c14fb0a382c9ff00385ef53a79c9
jruoho <jruoho@NetBSD.org>
2010-06-30 07:42:36 +0000

    Provide a sysctl-knob for the Debug() opcode, hw.acpi.debug.object. This prints debug-messages possibly used in the AML. Only enabled for ACPI_DEBUG kernels.

--- sys/dev/acpi/acpi_debug.c
+++ sys/dev/acpi/acpi_debug.c
@@ -1,4 +1,4 @@
-/* $NetBSD: acpi_debug.c,v 1.2 2010/04/12 12:14:26 jruoho Exp $ */
+/* $NetBSD: acpi_debug.c,v 1.3 2010/06/30 07:42:36 jruoho Exp $ */
 
 /*-
  * Copyright (c) 2010 Jukka Ruohonen <jruohonen@iki.fi>
@@ -27,7 +27,7 @@
  * SUCH DAMAGE.
  */
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: acpi_debug.c,v 1.2 2010/04/12 12:14:26 jruoho Exp $");
+__KERNEL_RCSID(0, "$NetBSD: acpi_debug.c,v 1.3 2010/06/30 07:42:36 jruoho Exp $");
 
 #include <sys/param.h>
 #include <sys/sysctl.h>
@@ -115,6 +115,15 @@ acpi_debug_init(void)
 	    CTLFLAG_READWRITE, CTLTYPE_STRING, "level",
 	    SYSCTL_DESCR("ACPI debug level"),
 	    acpi_debug_sysctl_level, 0, acpi_debug_level_s, ACPI_DEBUG_MAX,
+	    CTL_CREATE, CTL_EOL);
+
+	if (rv != 0)
+		goto fail;
+
+	rv = sysctl_createv(NULL, 0, &rnode, NULL,
+	    CTLFLAG_READWRITE, CTLTYPE_BOOL, "object",
+	    SYSCTL_DESCR("ACPI debug object"),
+	    NULL, 0, &AcpiGbl_EnableAmlDebugObject, 0,
 	    CTL_CREATE, CTL_EOL);
 
 	if (rv != 0)
