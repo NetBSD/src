9c8ebeed66a399d94ecd616106a6f96bc94d447e
jmmv <jmmv@NetBSD.org>
2010-07-05 16:34:41 +0000

    Use expect_fail instead of defining xfail.  Looks like I missed this one during the import of atf 0.10.  Thanks pooka@.

--- tests/fs/ffs/t_mount.c
+++ tests/fs/ffs/t_mount.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: t_mount.c,v 1.1 2010/06/30 21:54:56 njoly Exp $	*/
+/*	$NetBSD: t_mount.c,v 1.2 2010/07/05 16:34:41 jmmv Exp $	*/
 
 /*
  * Adapted for rump and atf from a testcase supplied
@@ -19,7 +19,6 @@ ATF_TC_HEAD(48Kimage, tc)
 {
 	atf_tc_set_md_var(tc, "descr", "mount small 48K ffs image");
 	atf_tc_set_md_var(tc, "use.fs", "true");
-	atf_tc_set_md_var(tc, "xfail", "No PR yet");
 }
 
 ATF_TC_BODY(48Kimage, tc)
@@ -29,8 +28,11 @@ ATF_TC_BODY(48Kimage, tc)
 	if (ffs_newfs(&tmp, IMGNAME, IMGSIZE) != 0)
 		atf_tc_fail("newfs failed");
 
-	if (ffs_mount(tmp, MNTDIR, 0) != 0)
+	if (ffs_mount(tmp, MNTDIR, 0) != 0) {
+		atf_tc_expect_fail("No PR yet");
 		atf_tc_fail("mount failed");
+		atf_tc_expect_pass();
+	}
 	if (ffs_unmount(MNTDIR, 0) != 0)
 		atf_tc_fail("unmount failed");
 
