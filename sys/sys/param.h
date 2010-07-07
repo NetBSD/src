acbfa43e8baf432e9170c1fb259973c46561ff9d
chs <chs@NetBSD.org>
2010-07-07 01:37:35 +0000

    welcome to 5.99.35 (changes for updated COMPAT_LINUX).

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.369 2010/07/01 13:00:57 hannken Exp $	*/
+/*	$NetBSD: param.h,v 1.370 2010/07/07 01:37:35 chs Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -63,7 +63,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	599003400	/* NetBSD 5.99.34 */
+#define	__NetBSD_Version__	599003500	/* NetBSD 5.99.35 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
