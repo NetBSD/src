e135914084251f93c2ea777786da493694da0096
tsutsui <tsutsui@NetBSD.org>
2010-07-17 15:25:57 +0000

    Remove obsolete __HAVE_GENERIC_SOFT_INTERRUPTS, which is mandatory since 2007/07: http://mail-index.NetBSD.org/source-changes/2007/07/14/0039.html

--- sys/arch/bebox/include/types.h
+++ sys/arch/bebox/include/types.h
@@ -1,6 +1,5 @@
-/*	$NetBSD: types.h,v 1.10 2008/01/20 18:09:05 joerg Exp $	*/
+/*	$NetBSD: types.h,v 1.11 2010/07/17 15:25:57 tsutsui Exp $	*/
 
 #define __HAVE_DEVICE_REGISTER
-#define __HAVE_GENERIC_SOFT_INTERRUPTS
 
 #include <powerpc/types.h>
