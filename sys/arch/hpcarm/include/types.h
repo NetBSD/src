c202c819874959f010f022921de99c6616c23f8f
tsutsui <tsutsui@NetBSD.org>
2010-07-17 15:13:18 +0000

    Bump MAXPARTITIONS from 8 to 16 using __HAVE_OLD_DISKLABEL compat method as i386 and hpcmips. PR port-hpcarm/18256

--- sys/arch/hpcarm/include/types.h
+++ sys/arch/hpcarm/include/types.h
@@ -1,4 +1,4 @@
-/* $NetBSD: types.h,v 1.13 2008/01/20 18:09:06 joerg Exp $ */
+/* $NetBSD: types.h,v 1.14 2010/07/17 15:13:18 tsutsui Exp $ */
 
 #ifndef _HPCARM_TYPES_H_
 #define	_HPCARM_TYPES_H_
@@ -6,6 +6,7 @@
 #include <arm/arm32/types.h>
 
 #define	__HAVE_DEVICE_REGISTER
+#define	__HAVE_OLD_DISKLABEL
 #define	__GENERIC_SOFT_INTERRUPTS_ALL_LEVELS
 
 #endif
