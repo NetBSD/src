55b6ce35a3dd66e7214a964a9eec8f5b7f2a5606
jruoho <jruoho>
2010-07-18 09:29:11 +0000

    Merge a driver for ACPI CPUs with basic support for processor power states, also known as C-states. The code is modular and provides an easy way to add the remaining functionality later (namely throttling and P-states).
    Remarks:
    
      1.	Commented out in the GENERICs; more testing exposure is needed.
    
      2.	The C3-state is disabled for the time being because it turns off
    	timers, among them the local APIC timer. This may not be universally
    	true on all x86 processors; define ACPICPU_ENABLE_C3 to test.
    
      3.	The algorithm used to choose a power state may need tuning. When
    	evaluating the appropriate state, the implementation uses the
    	previous sleep time as an indicator. Additional hints would include
    	for example the system load.
    
    	Also bus master activity is evaluated when choosing a state. The
    	usb(4) stack is notorious for such activity even when unused.
    	Typically it must be disabled in order to reach the C3-state,
    	but it may also prevent the use of C2.
    
      4.	While no extensive empirical measurements have been carried out, the
    	power savings are somewhere between 1-2 W with C1 and C2, depending
    	on the processor, firmware, and load. With C3 even up to 4 W can be
    	saved.  The less something ticks, the more power is saved.
    
    ok jmcneill@, joerg@, and discussed with various people.

---
+++ sys/arch/x86/acpi/acpi_cpu_md.c
@@ -0,0 +1,187 @@
+/* $NetBSD */
+
+/*-
+ * Copyright (c) 2010 Jukka Ruohonen <jruohonen@iki.fi>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+#include <sys/cdefs.h>
+__KERNEL_RCSID(0, "$NetBSD: acpi_cpu_md.c,v 1.1 2010/07/18 09:29:13 jruoho Exp $");
+
+#include <sys/param.h>
+#include <sys/bus.h>
+#include <sys/kcore.h>
+
+#include <x86/cpu.h>
+#include <x86/cpuvar.h>
+#include <x86/machdep.h>
+#include <x86/cputypes.h>
+
+#include <dev/acpi/acpica.h>
+#include <dev/acpi/acpi_cpu.h>
+
+static char	native_idle_text[16];
+void	      (*native_idle)(void) = NULL;
+
+extern uint32_t	cpus_running;
+
+uint32_t
+acpicpu_md_cap(void)
+{
+	struct cpu_info *ci = curcpu();
+	uint32_t val = 0;
+
+	if (cpu_vendor != CPUVENDOR_INTEL)
+		return val;
+
+	/*
+	 * Basic SMP C-states (required for _CST).
+	 */
+	val |= ACPICPU_PDC_C_C1PT | ACPICPU_PDC_C_C2C3;
+
+        /*
+	 * If MONITOR/MWAIT is available, announce
+	 * support for native instructions in all C-states.
+	 */
+        if ((ci->ci_feat_val[1] & CPUID2_MONITOR) != 0)
+		val |= ACPICPU_PDC_C_C1_FFH | ACPICPU_PDC_C_C2C3_FFH;
+
+	return val;
+}
+
+uint32_t
+acpicpu_md_quirks(void)
+{
+	struct cpu_info *ci = curcpu();
+	uint32_t val = 0;
+
+	if (acpicpu_md_cpus_running() == 1)
+		val |= ACPICPU_FLAG_C_BM;
+
+	if ((ci->ci_feat_val[1] & CPUID2_MONITOR) != 0)
+		val |= ACPICPU_FLAG_C_MWAIT;
+
+	switch (cpu_vendor) {
+
+	case CPUVENDOR_INTEL:
+		val |= ACPICPU_FLAG_C_BM | ACPICPU_FLAG_C_ARB;
+
+		/*
+		 * Bus master arbitration is not
+		 * needed on some recent Intel CPUs.
+		 */
+		if (CPUID2FAMILY(ci->ci_signature) > 15)
+			val &= ~ACPICPU_FLAG_C_ARB;
+
+		if (CPUID2FAMILY(ci->ci_signature) == 6 &&
+		    CPUID2MODEL(ci->ci_signature) >= 15)
+			val &= ~ACPICPU_FLAG_C_ARB;
+
+		break;
+
+	case CPUVENDOR_AMD:
+
+		/*
+		 * XXX: Deal with the AMD C1E extension here.
+		 */
+		break;
+	}
+
+	return val;
+}
+
+uint32_t
+acpicpu_md_cpus_running(void)
+{
+
+	return popcount32(cpus_running);
+}
+
+int
+acpicpu_md_idle_init(void)
+{
+	const size_t size = sizeof(native_idle_text);
+
+	KASSERT(native_idle == NULL);
+
+	x86_disable_intr();
+	x86_cpu_idle_get(&native_idle, native_idle_text, size);
+	x86_enable_intr();
+
+	return 0;
+}
+
+int
+acpicpu_md_idle_start(void)
+{
+
+	KASSERT(native_idle != NULL);
+	KASSERT(native_idle_text[0] != '\0');
+
+	x86_disable_intr();
+	x86_cpu_idle_set(acpicpu_cstate_idle, "acpi");
+	x86_enable_intr();
+
+	DELAY(10000);
+
+	return 0;
+}
+
+int
+acpicpu_md_idle_stop(void)
+{
+
+	KASSERT(native_idle != NULL);
+	KASSERT(native_idle_text[0] != '\0');
+
+	x86_disable_intr();
+	x86_cpu_idle_set(native_idle, native_idle_text);
+	x86_enable_intr();
+
+	DELAY(10000);
+
+	return 0;
+}
+
+void
+acpicpu_md_idle_enter(int method, int state)
+{
+
+	KASSERT(native_idle != NULL);
+
+	switch (method) {
+
+	case ACPICPU_C_STATE_FFH:
+		x86_mwait((state - 1) << 4, 0);
+		break;
+
+	case ACPICPU_C_STATE_HALT:
+		x86_stihlt();
+		break;
+
+	default:
+		(*native_idle)();
+		break;
+	}
+}
