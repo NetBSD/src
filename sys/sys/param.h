6537f41b2e2767c3ebd7da4f4defeb8132c2691d
hannken <hannken@NetBSD.org>
2010-07-21 09:06:37 +0000

    Using vfinddev() leads to vnode races as it returns an unreferenced vnode that may disappear before the caller has a chance to reference it.
    Reference the vnode while the specfs cache is locked.
    
    Welcome to 5.99.37.
    
    No objections on tech-kern.

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.371 2010/07/08 12:23:31 rmind Exp $	*/
+/*	$NetBSD: param.h,v 1.372 2010/07/21 09:06:37 hannken Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -63,7 +63,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	599003600	/* NetBSD 5.99.36 */
+#define	__NetBSD_Version__	599003700	/* NetBSD 5.99.37 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
