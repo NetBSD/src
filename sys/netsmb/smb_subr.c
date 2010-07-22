ca5fd0dee476f628ce2a7c85ba775e1eaa45f7e3
riz <riz@NetBSD.org>
2010-07-22 20:36:22 +0000

    Pull up following revision(s) (requested by christos in ticket #1398): 	sys/netsmb/subr_mchain.c: revision 1.19 via patch 	sys/netsmb/smb_subr.c: revision 1.35 	sys/netsmb/smb_subr.h: revision 1.19 	sys/netsmb/mchain.h: revision 1.9 via patch 	sys/netsmb/smb_dev.h: revision 1.7 Convert sizes/lengths to unsigned (size_t) or uint32_t (for binary compatibility).

--- sys/netsmb/smb_subr.c
+++ sys/netsmb/smb_subr.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: smb_subr.c,v 1.29 2006/11/16 01:33:51 christos Exp $	*/
+/*	$NetBSD: smb_subr.c,v 1.29.2.1 2010/07/22 20:36:22 riz Exp $	*/
 
 /*
  * Copyright (c) 2000-2001 Boris Popov
@@ -35,7 +35,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: smb_subr.c,v 1.29 2006/11/16 01:33:51 christos Exp $");
+__KERNEL_RCSID(0, "$NetBSD: smb_subr.c,v 1.29.2.1 2010/07/22 20:36:22 riz Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -93,7 +93,7 @@ char *
 smb_strdup(const char *s)
 {
 	char *p;
-	int len;
+	size_t len;
 
 	len = s ? strlen(s) + 1 : 1;
 	p = malloc(len, M_SMBSTR, M_WAITOK);
@@ -108,10 +108,10 @@ smb_strdup(const char *s)
  * duplicate string from a user space.
  */
 char *
-smb_strdupin(char *s, int maxlen)
+smb_strdupin(char *s, size_t maxlen)
 {
 	char *p, bt;
-	int len = 0;
+	size_t len = 0;
 
 	for (p = s; ;p++) {
 		if (copyin(p, &bt, 1))
@@ -131,7 +131,7 @@ smb_strdupin(char *s, int maxlen)
  * duplicate memory block from a user space.
  */
 void *
-smb_memdupin(void *umem, int len)
+smb_memdupin(void *umem, size_t len)
 {
 	char *p;
 
@@ -157,7 +157,7 @@ smb_memfree(void *s)
 }
 
 void *
-smb_zmalloc(unsigned long size, struct malloc_type *type, int flags)
+smb_zmalloc(size_t size, struct malloc_type *type, int flags)
 {
 
 	return malloc(size, type, flags | M_ZERO);
@@ -176,12 +176,12 @@ smb_strtouni(u_int16_t *dst, const char *src)
 void
 m_dumpm(struct mbuf *m) {
 	char *p;
-	int len;
+	size_t len;
 	printf("d=");
 	while(m) {
-		p=mtod(m,char *);
-		len=m->m_len;
-		printf("(%d)",len);
+		p = mtod(m,char *);
+		len = m->m_len;
+		printf("(%zu)", len);
 		while(len--){
 			printf("%02x ",((int)*(p++)) & 0xff);
 		}
@@ -312,7 +312,7 @@ smb_copy_iconv(struct mbchain *mbp, const char *src, char *dst, size_t len)
 
 int
 smb_put_dmem(struct mbchain *mbp, struct smb_vc *vcp, const char *src,
-	int size, int caseopt)
+	size_t size, int caseopt)
 {
 	struct iconv_drv *dp = vcp->vc_toserver;
 
