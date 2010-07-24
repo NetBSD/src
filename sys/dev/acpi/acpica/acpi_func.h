6c0ce2941c2b825e054fa1f649218d633392b8a5
jruoho <jruoho@NetBSD.org>
2010-07-24 21:53:53 +0000

    Revert the previous partially for the time being.

--- sys/dev/acpi/acpica/acpi_func.h
+++ sys/dev/acpi/acpica/acpi_func.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: acpi_func.h,v 1.3 2010/07/24 09:35:36 jruoho Exp $	*/
+/*	$NetBSD: acpi_func.h,v 1.4 2010/07/24 21:53:54 jruoho Exp $	*/
 
 /*-
  * Copyright (c) 2000 Michael Smith
@@ -27,6 +27,9 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  */
+
+#ifndef _SYS_DEV_ACPI_ACPICA_ACPI_FUNC_H
+#define _SYS_DEV_ACPI_ACPICA_ACPI_FUNC_H
 
 #include <machine/cpufunc.h>
 
@@ -77,3 +80,13 @@ acpi_release_global_lock(uint32_t *lock)
 	return old & GL_BIT_PENDING;
 }
 
+/*
+ * XXX: Should be in a MD header.
+ */
+#ifndef __ia64__
+#define	ACPI_FLUSH_CPU_CACHE()	wbinvd()
+#else
+#define ACPI_FLUSH_CPU_CACHE()	/* XXX: ia64_fc()? */
+#endif
+
+#endif /* !_SYS_DEV_ACPI_ACPICA_ACPI_FUNC_H */
