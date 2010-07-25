887ab5aec4d7a9f45d20b71ba63a49633cc60823
pgoyette <pgoyette@NetBSD.org>
2010-07-25 14:14:25 +0000

    Move setting of pci_verbose_loaded flag into the module's init routine. This ensures that the flag is set even if the module was manually loaded by the user rather than just auto-loaded.

--- sys/dev/pci/pci_verbose.c
+++ sys/dev/pci/pci_verbose.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: pci_verbose.c,v 1.6 2010/06/06 18:58:24 pgoyette Exp $	*/
+/*	$NetBSD: pci_verbose.c,v 1.7 2010/07/25 14:14:25 pgoyette Exp $	*/
 
 /*
  * Copyright (c) 1997 Zubin D. Dittia.  All rights reserved.
@@ -40,7 +40,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: pci_verbose.c,v 1.6 2010/06/06 18:58:24 pgoyette Exp $");
+__KERNEL_RCSID(0, "$NetBSD: pci_verbose.c,v 1.7 2010/07/25 14:14:25 pgoyette Exp $");
 
 #include <sys/param.h>
 
@@ -86,6 +86,7 @@ pciverbose_modcmd(modcmd_t cmd, void *arg)
 		pci_findvendor = pci_findvendor_real;
 		pci_findproduct = pci_findproduct_real;
 		pci_unmatched = "unmatched ";
+		pciverbose_loaded = 1;
 		return 0;
 	case MODULE_CMD_FINI:
 		pci_findvendor = saved_findvendor;
