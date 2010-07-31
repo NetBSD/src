0c52a34e234177666a327cd7a14d04721b60261f
joerg <joerg@NetBSD.org>
2010-07-31 21:47:52 +0000

    Add support for fenv.h interface for i386 and amd64.
    Submitted by Stathis Kamperis as part of GSoC 2010 and ported from
    FreeBSD.

--- sys/arch/i386/include/npx.h
+++ sys/arch/i386/include/npx.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: npx.h,v 1.24 2010/07/25 19:19:57 christos Exp $	*/
+/*	$NetBSD: npx.h,v 1.25 2010/07/31 21:47:54 joerg Exp $	*/
 
 /*-
  * Copyright (c) 1990 The Regents of the University of California.
@@ -44,25 +44,25 @@
 
 /* Environment information of floating point unit */
 struct env87 {
-	long	en_cw;		/* control word (16bits) */
-	long	en_sw;		/* status word (16bits) */
-	long	en_tw;		/* tag word (16bits) */
-	long	en_fip;		/* floating point instruction pointer */
-	u_short	en_fcs;		/* floating code segment selector */
-	u_short	en_opcode;	/* opcode last executed (11 bits ) */
-	long	en_foo;		/* floating operand offset */
-	long	en_fos;		/* floating operand segment selector */
+	long	 en_cw;		/* control word (16bits) */
+	long	 en_sw;		/* status word (16bits) */
+	long	 en_tw;		/* tag word (16bits) */
+	long	 en_fip;	/* floating point instruction pointer */
+	uint16_t en_fcs;	/* floating code segment selector */
+	uint16_t en_opcode;	/* opcode last executed (11 bits ) */
+	long	 en_foo;	/* floating operand offset */
+	long	 en_fos;	/* floating operand segment selector */
 };
 
 /* Contents of each floating point accumulator */
 struct fpacc87 {
 #ifdef dontdef	/* too unportable */
-	u_long	fp_mantlo;	/* mantissa low (31:0) */
-	u_long	fp_manthi;	/* mantissa high (63:32) */
-	int	fp_exp:15;	/* exponent */
-	int	fp_sgn:1;	/* mantissa sign */
+	uint32_t fp_mantlo;	/* mantissa low (31:0) */
+	uint32_t fp_manthi;	/* mantissa high (63:32) */
+	int	 fp_exp:15;	/* exponent */
+	int	 fp_sgn:1;	/* mantissa sign */
 #else
-	u_char	fp_bytes[10];
+	uint8_t	 fp_bytes[10];
 #endif
 };
 
@@ -71,9 +71,9 @@ struct save87 {
 	struct	env87 sv_env;		/* floating point control/status */
 	struct	fpacc87	sv_ac[8];	/* accumulator contents, 0-7 */
 #ifndef dontdef
-	u_long	sv_ex_sw;	/* status word for last exception (was pad) */
-	u_long	sv_ex_tw;	/* tag word for last exception (was pad) */
-	u_char	sv_pad[8 * 2 - 2 * 4];	/* bogus historical padding */
+	uint32_t sv_ex_sw;	/* status word for last exception (was pad) */
+	uint32_t sv_ex_tw;	/* tag word for last exception (was pad) */
+	uint8_t	 sv_pad[8 * 2 - 2 * 4];	/* bogus historical padding */
 #endif
 };
 
