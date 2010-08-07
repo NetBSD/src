f7f083a06d5e254efcdcdb61cc08e70315a9b7b2
sjg <sjg@NetBSD.org>
2010-08-07 21:28:40 +0000

    In jobs mode, if we cannot make a target we die without any further clue.  Call PrintOnError() before DieHorribly().

--- usr.bin/make/job.c
+++ usr.bin/make/job.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: job.c,v 1.153 2010/08/07 06:44:08 sjg Exp $	*/
+/*	$NetBSD: job.c,v 1.154 2010/08/07 21:28:40 sjg Exp $	*/
 
 /*
  * Copyright (c) 1988, 1989, 1990 The Regents of the University of California.
@@ -70,14 +70,14 @@
  */
 
 #ifndef MAKE_NATIVE
-static char rcsid[] = "$NetBSD: job.c,v 1.153 2010/08/07 06:44:08 sjg Exp $";
+static char rcsid[] = "$NetBSD: job.c,v 1.154 2010/08/07 21:28:40 sjg Exp $";
 #else
 #include <sys/cdefs.h>
 #ifndef lint
 #if 0
 static char sccsid[] = "@(#)job.c	8.2 (Berkeley) 3/19/94";
 #else
-__RCSID("$NetBSD: job.c,v 1.153 2010/08/07 06:44:08 sjg Exp $");
+__RCSID("$NetBSD: job.c,v 1.154 2010/08/07 21:28:40 sjg Exp $");
 #endif
 #endif /* not lint */
 #endif
@@ -1553,6 +1553,7 @@ JobStart(GNode *gn, int flags)
 	 * also dead...
 	 */
 	if (!cmdsOK) {
+	    PrintOnError(gn, NULL);	/* provide some clue */
 	    DieHorribly();
 	}
 
