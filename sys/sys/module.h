da32abd91e358860db7657b2d9005cfb2e69c5d7
pgoyette <pgoyette@NetBSD.org>
2010-08-21 13:17:31 +0000

    Define a set of new kernel locking primitives to implement the recursive kernconfig_mutex.  Update module subsystem to use this mutex rather than its own internal (non-recursive) mutex.  Make module_autoload() do its own locking to be consistent with the rest of the module_xxx() calls. Update module(9) man page appropriately.
    As discussed on tech-kern over the last few weeks.
    
    Welcome to NetBSD 5.99.39 !

--- sys/sys/module.h
+++ sys/sys/module.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: module.h,v 1.24 2010/06/26 07:23:57 pgoyette Exp $	*/
+/*	$NetBSD: module.h,v 1.25 2010/08/21 13:17:32 pgoyette Exp $	*/
 
 /*-
  * Copyright (c) 2008 The NetBSD Foundation, Inc.
@@ -115,7 +115,6 @@ __link_set_add_rodata(modules, name##_modinfo);
 TAILQ_HEAD(modlist, module);
 
 extern struct vm_map	*module_map;
-extern kmutex_t		module_lock;
 extern u_int		module_count;
 extern u_int		module_builtinlist;
 extern struct modlist	module_list;
