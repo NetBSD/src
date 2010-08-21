1175047d3642fa76984bb1e1d86098e39122a388
pgoyette <pgoyette>
2010-08-21 13:17:31 +0000

    Define a set of new kernel locking primitives to implement the recursive kernconfig_mutex.  Update module subsystem to use this mutex rather than its own internal (non-recursive) mutex.  Make module_autoload() do its own locking to be consistent with the rest of the module_xxx() calls. Update module(9) man page appropriately.
    As discussed on tech-kern over the last few weeks.
    
    Welcome to NetBSD 5.99.39 !

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.373 2010/07/28 11:03:47 hannken Exp $	*/
+/*	$NetBSD: param.h,v 1.374 2010/08/21 13:17:32 pgoyette Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -63,7 +63,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	599003800	/* NetBSD 5.99.38 */
+#define	__NetBSD_Version__	599003900	/* NetBSD 5.99.39 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
