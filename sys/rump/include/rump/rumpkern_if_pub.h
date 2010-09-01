4257608f33288b4383858edf8ecdd3e7539c13e1
pooka <pooka@NetBSD.org>
2010-09-01 19:33:04 +0000

    regen: new lwp/proc interfaces

--- sys/rump/include/rump/rumpkern_if_pub.h
+++ sys/rump/include/rump/rumpkern_if_pub.h
@@ -1,9 +1,9 @@
-/*	$NetBSD: rumpkern_if_pub.h,v 1.8 2010/07/19 15:38:28 pooka Exp $	*/
+/*	$NetBSD: rumpkern_if_pub.h,v 1.9 2010/09/01 19:33:04 pooka Exp $	*/
 
 /*
  * Automatically generated.  DO NOT EDIT.
- * from: NetBSD: rumpkern.ifspec,v 1.5 2010/04/14 14:12:48 pooka Exp 
- * by:   NetBSD: makerumpif.sh,v 1.4 2009/10/15 00:29:19 pooka Exp 
+ * from: NetBSD: rumpkern.ifspec,v 1.6 2010/09/01 19:13:38 pooka Exp 
+ * by:   NetBSD: makerumpif.sh,v 1.5 2010/09/01 19:32:11 pooka Exp 
  */
 
 void rump_pub_reboot(int);
@@ -16,14 +16,13 @@ size_t rump_pub_uio_getresid(struct uio *);
 off_t rump_pub_uio_getoff(struct uio *);
 size_t rump_pub_uio_free(struct uio *);
 struct kauth_cred* rump_pub_cred_create(uid_t, gid_t, size_t, gid_t *);
-struct kauth_cred* rump_pub_cred_suserget(void);
 void rump_pub_cred_put(struct kauth_cred *);
-struct lwp * rump_pub_newproc_switch(void);
-struct lwp * rump_pub_lwp_alloc(pid_t, lwpid_t);
-struct lwp * rump_pub_lwp_alloc_and_switch(pid_t, lwpid_t);
-struct lwp * rump_pub_lwp_curlwp(void);
-void rump_pub_lwp_switch(struct lwp *);
-void rump_pub_lwp_release(struct lwp *);
+int rump_pub_lwproc_newproc(void);
+int rump_pub_lwproc_newlwp(pid_t);
+void rump_pub_lwproc_switch(struct lwp *);
+void rump_pub_lwproc_releaselwp(void);
+struct lwp * rump_pub_lwproc_curlwp(void);
+void rump_pub_allbetsareoff_setid(pid_t, int);
 int rump_pub_sysproxy_set(rump_sysproxy_t, void *);
 int rump_pub_sysproxy_socket_setup_client(int);
 int rump_pub_sysproxy_socket_setup_server(int);
