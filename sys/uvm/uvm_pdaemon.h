96fd79a0c43aafa2815373344ca5ea68512fb210
pooka <pooka@NetBSD.org>
2010-09-07 04:45:22 +0000

    Make "no options VMSWAP" kernels compile again.

--- sys/uvm/uvm_pdaemon.h
+++ sys/uvm/uvm_pdaemon.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: uvm_pdaemon.h,v 1.15 2008/01/02 11:49:20 ad Exp $	*/
+/*	$NetBSD: uvm_pdaemon.h,v 1.16 2010/09/07 04:45:22 pooka Exp $	*/
 
 /*
  * Copyright (c) 1997 Charles D. Cranor and Washington University.
@@ -83,7 +83,11 @@ void uvm_wait(const char *);
 bool uvm_reclaimable(void);
 
 kmutex_t *uvmpd_trylockowner(struct vm_page *);
+#ifdef VMSWAP
 bool uvmpd_trydropswap(struct vm_page *);
+#else
+#define uvmpd_trydropswap(_a_) (/*CONSTCOND*/false)
+#endif
 
 #endif /* _KERNEL */
 
