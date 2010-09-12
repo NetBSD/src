54d0dafe67361e401d2f4a070347a868afea241e
snj <snj>
2010-09-12 04:52:45 +0000

    Pull up following revision(s) (requested by skrll in ticket #1451): 	lib/libc/stdlib/exit.c: revision 1.13 Only do the __libc_init hack in libc, i.e. remove it from ld.elf_so. This fixes hppa ld.elf_so by reducing the number of PLABELs required to the number before the hack was applied.

--- lib/libc/stdlib/exit.c
+++ lib/libc/stdlib/exit.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: exit.c,v 1.11.12.1 2010/07/16 18:34:08 riz Exp $	*/
+/*	$NetBSD: exit.c,v 1.11.12.2 2010/09/12 04:52:45 snj Exp $	*/
 
 /*-
  * Copyright (c) 1990, 1993
@@ -34,7 +34,7 @@
 #if 0
 static char sccsid[] = "@(#)exit.c	8.1 (Berkeley) 6/4/93";
 #else
-__RCSID("$NetBSD: exit.c,v 1.11.12.1 2010/07/16 18:34:08 riz Exp $");
+__RCSID("$NetBSD: exit.c,v 1.11.12.2 2010/09/12 04:52:45 snj Exp $");
 #endif
 #endif /* LIBC_SCCS and not lint */
 
@@ -45,9 +45,11 @@ __RCSID("$NetBSD: exit.c,v 1.11.12.1 2010/07/16 18:34:08 riz Exp $");
 #include "atexit.h"
 #endif
 
+#ifdef _LIBC
 extern void __libc_init(void);
 #ifndef __lint
 static void (*force_ref)(void) __used = __libc_init;
+#endif
 #endif
 
 void (*__cleanup) __P((void));
