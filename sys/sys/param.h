f38bb06c9757fd8cf69962cd962e6671eaa99a51
snj <snj>
2010-09-13 07:32:05 +0000

    Welcome to 5.1_RC4!

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.330.4.13 2010/06/12 02:51:38 riz Exp $	*/
+/*	$NetBSD: param.h,v 1.330.4.14 2010/09/13 07:32:06 snj Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -63,7 +63,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	501000000	/* NetBSD 5.1_RC3 */
+#define	__NetBSD_Version__	501000000	/* NetBSD 5.1_RC4 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
