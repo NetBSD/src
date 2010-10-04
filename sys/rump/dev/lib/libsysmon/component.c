84828a95c0797a8ba0b009798af5f70f5f1701e1
pgoyette <pgoyette@NetBSD.org>
2010-10-04 15:57:27 +0000

    Update error message - no funcitonal change.

--- sys/rump/dev/lib/libsysmon/component.c
+++ sys/rump/dev/lib/libsysmon/component.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: component.c,v 1.3 2010/08/03 19:55:17 pooka Exp $	*/
+/*	$NetBSD: component.c,v 1.4 2010/10/04 15:57:27 pgoyette Exp $	*/
 
 /*
  * Copyright (c) 2010 Antti Kantee.  All Rights Reserved.
@@ -26,7 +26,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: component.c,v 1.3 2010/08/03 19:55:17 pooka Exp $");
+__KERNEL_RCSID(0, "$NetBSD: component.c,v 1.4 2010/10/04 15:57:27 pgoyette Exp $");
 
 #include <sys/param.h>
 #include <sys/conf.h>
@@ -52,7 +52,7 @@ RUMP_COMPONENT(RUMP_COMPONENT_DEV)
 
 	if ((error = config_init_component(cfdriver_ioconf_swwdog,
 	    cfattach_ioconf_swwdog, cfdata_ioconf_swwdog)) != 0) {
-		printf("cannot attach sysmon: %d\n", error);
+		printf("cannot attach swwdog: %d\n", error);
 		return;
 	}
 
