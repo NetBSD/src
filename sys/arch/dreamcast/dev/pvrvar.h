dd839ea27641dae427e4f190cbff82cbdc71b282
tsutsui <tsutsui@NetBSD.org>
2010-10-24 13:34:27 +0000

    - make local functions/variables static - export more cons(9) functions in pvrvar.h

--- sys/arch/dreamcast/dev/pvrvar.h
+++ sys/arch/dreamcast/dev/pvrvar.h
@@ -1,4 +1,4 @@
-/* $NetBSD: pvrvar.h,v 1.3 2001/02/01 01:25:56 thorpej Exp $ */
+/* $NetBSD: pvrvar.h,v 1.4 2010/10/24 13:34:27 tsutsui Exp $ */
 
 /*-
  * Copyright (c) 2001 Marcus Comstedt
@@ -32,4 +32,6 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
+void	pvrcnprobe(struct consdev *);
+void	pvrcninit(struct consdev *);
 int	pvr_cnattach(void);
