5a3e5dd445125a15f886f8d0753fe24fcaa798d1
mbalmer <mbalmer@NetBSD.org>
2010-10-31 11:16:48 +0000

    Import the (unaltered) Lua 5.1.4 source distribution.

---
+++ external/mit/lua/dist/src/lapi.h
@@ -0,0 +1,18 @@
+/*	$NetBSD: lapi.h,v 1.1.1.1 2010/10/31 11:16:54 mbalmer Exp $	*/
+
+/*
+** Id: lapi.h,v 2.2.1.1 2007/12/27 13:02:25 roberto Exp
+** Auxiliary functions from Lua API
+** See Copyright Notice in lua.h
+*/
+
+#ifndef lapi_h
+#define lapi_h
+
+
+#include "lobject.h"
+
+
+LUAI_FUNC void luaA_pushobject (lua_State *L, const TValue *o);
+
+#endif
