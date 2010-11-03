4666f14cd455121fec82928e164faa2488a25fb0
dyoung <dyoung@NetBSD.org>
2010-11-03 23:46:35 +0000

    Stop using the compatibility macros USB_ATTACH(), USB_DETACH(), USB_MATCH(), et cetera.  These files produce the same assembly (according to objdump -d) before and after the change, except for minor differences in moscom.c, uark.c, and xboxcontroller.c which I traced to line-number shifts and KASSERT() invocation in the child-detachment routines---e.g., moscom_childdet().

--- sys/dev/usb/moscom.c
+++ sys/dev/usb/moscom.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: moscom.c,v 1.3 2009/11/12 19:53:14 dyoung Exp $	*/
+/*	$NetBSD: moscom.c,v 1.4 2010/11/03 23:46:35 dyoung Exp $	*/
 /*	$OpenBSD: moscom.c,v 1.11 2007/10/11 18:33:14 deraadt Exp $	*/
 
 /*
@@ -18,7 +18,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: moscom.c,v 1.3 2009/11/12 19:53:14 dyoung Exp $");
+__KERNEL_RCSID(0, "$NetBSD: moscom.c,v 1.4 2010/11/03 23:46:35 dyoung Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -34,6 +34,8 @@ __KERNEL_RCSID(0, "$NetBSD: moscom.c,v 1.3 2009/11/12 19:53:14 dyoung Exp $");
 
 #include <dev/usb/usbdevs.h>
 #include <dev/usb/ucomvar.h>
+
+#include <dev/usb/usb_port.h>
 
 #define MOSCOMBUFSZ		256
 #define MOSCOM_CONFIG_NO	0
@@ -137,10 +139,10 @@ __KERNEL_RCSID(0, "$NetBSD: moscom.c,v 1.3 2009/11/12 19:53:14 dyoung Exp $");
 #define MOSCOM_BAUD_REF		115200
 
 struct moscom_softc {
-	USBBASEDEVICE		 sc_dev;
+	device_t		 sc_dev;
 	usbd_device_handle	 sc_udev;
 	usbd_interface_handle	 sc_iface;
-	device_ptr_t		 sc_subdev;
+	device_t		 sc_subdev;
 
 	u_char			 sc_msr;
 	u_char			 sc_lsr;
@@ -181,17 +183,20 @@ int moscom_activate(device_t, enum devact);
 CFATTACH_DECL2_NEW(moscom, sizeof(struct moscom_softc), moscom_match,
     moscom_attach, moscom_detach, moscom_activate, NULL, moscom_childdet);
 
-USB_MATCH(moscom)
+int 
+moscom_match(device_t parent, cfdata_t match, void *aux)
 {
-	USB_MATCH_START(moscom, uaa);
+	struct usb_attach_arg *uaa = aux;
 
 	return (moscom_lookup(uaa->vendor, uaa->product) != NULL ?
 		UMATCH_VENDOR_PRODUCT : UMATCH_NONE);
 }
 
-USB_ATTACH(moscom)
+void 
+moscom_attach(device_t parent, device_t self, void *aux)
 {
-	USB_ATTACH_START(moscom, sc, uaa);
+	struct moscom_softc *sc = device_private(self);
+	struct usb_attach_arg *uaa = aux;
 	usbd_device_handle dev = uaa->device;
 	struct ucom_attach_args uca;
 	usb_interface_descriptor_t *id;
@@ -264,12 +269,12 @@ USB_ATTACH(moscom)
 	uca.info = NULL;
 
 	usbd_add_drv_event(USB_EVENT_DRIVER_ATTACH, sc->sc_udev,
-	    USBDEV(sc->sc_dev));
+	    sc->sc_dev);
 	
 	sc->sc_subdev = config_found_sm_loc(self, "ucombus", NULL, &uca,
 					    ucomprint, ucomsubmatch);
 
-	USB_ATTACH_SUCCESS_RETURN;
+	return;
 }
 
 void
@@ -281,9 +286,10 @@ moscom_childdet(device_t self, device_t child)
 	sc->sc_subdev = NULL;
 }
 
-USB_DETACH(moscom)
+int 
+moscom_detach(device_t self, int flags)
 {
-	USB_DETACH_START(moscom, sc);
+	struct moscom_softc *sc = device_private(self);
 	int rv = 0;
 
 	sc->sc_dying = 1;
@@ -291,7 +297,7 @@ USB_DETACH(moscom)
 		rv = config_detach(sc->sc_subdev, flags);
 
 	usbd_add_drv_event(USB_EVENT_DRIVER_DETACH, sc->sc_udev,
-			   USBDEV(sc->sc_dev));
+			   sc->sc_dev);
 
 	return (rv);
 }
