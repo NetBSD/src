f2dcf759983b7ae203390f1ed80b4c5417ee00e4
skrll <skrll@NetBSD.org>
2010-11-09 12:24:47 +0000

    Update sti(4) from OpenBSD bringing across support for sti @ pci.
    Thanks to Adam Hoka for testing.

---
+++ sys/arch/hp700/dev/sti_pci_machdep.c
@@ -0,0 +1,88 @@
+/*	$NetBSD: sti_pci_machdep.c,v 1.1 2010/11/09 12:24:47 skrll Exp $	*/
+
+/*	$OpenBSD: sti_pci_machdep.c,v 1.2 2009/04/10 17:11:27 miod Exp $	*/
+
+/*
+ * Copyright (c) 2007, 2009 Miodrag Vallat.
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice, this permission notice, and the disclaimer below
+ * appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#include <sys/param.h>
+#include <sys/systm.h>
+#include <sys/device.h>
+
+#include <machine/iomod.h>
+#include <machine/autoconf.h>
+
+#include <dev/pci/pcivar.h>
+
+#include <hp700/hp700/machdep.h>
+
+int	sti_pci_is_console(struct pci_attach_args *, bus_addr_t *);
+
+int
+sti_pci_is_console(struct pci_attach_args *paa, bus_addr_t *bases)
+{
+	hppa_hpa_t consaddr;
+	uint32_t cf;
+	int pagezero_cookie;
+	int bar;
+	int rc;
+
+	KASSERT(paa != NULL);
+
+	pagezero_cookie = hp700_pagezero_map();
+	consaddr = (hppa_hpa_t)PAGE0->mem_cons.pz_hpa;
+	hp700_pagezero_unmap(pagezero_cookie);
+	/*
+	 * PAGE0 console information will point to one of our BARs,
+	 * but depending on the particular sti model, this might not
+	 * be the BAR mapping the rom (region #0).
+	 *
+	 * For example, on Visualize FXe, regions #0, #2 and #3 are
+	 * mapped by BAR 0x18, while region #1 is mapped by BAR 0x10,
+	 * which matches PAGE0 console address.
+	 *
+	 * Rather than trying to be smart, reread the region->BAR array
+	 * again, and compare the BAR mapping region #1 against PAGE0
+	 * values, we simply try all the valid BARs; if any of them
+	 * matches what PAGE0 says, then we are the console, and it
+	 * doesn't matter which BAR matched.
+	 */
+	for (bar = PCI_MAPREG_START; bar <= PCI_MAPREG_PPB_END; ) {
+		bus_addr_t addr;
+		bus_size_t size;
+
+		cf = pci_conf_read(paa->pa_pc, paa->pa_tag, bar);
+
+		rc = pci_mapreg_info(paa->pa_pc, paa->pa_tag, bar,
+		    PCI_MAPREG_TYPE(cf), &addr, &size, NULL);
+
+		if (PCI_MAPREG_TYPE(cf) == PCI_MAPREG_TYPE_IO) {
+			bar += 4;
+		} else {
+			if (PCI_MAPREG_MEM_TYPE(cf) ==
+			    PCI_MAPREG_MEM_TYPE_64BIT)
+				bar += 8;
+			else
+				bar += 4;
+		}
+
+		if (rc == 0 && (hppa_hpa_t)addr == consaddr)
+			return 1;
+	}
+
+	return 0;
+}
--- sys/dev/ic/stivar.h
+++ sys/dev/ic/stivar.h
@@ -1,6 +1,6 @@
-/*	$NetBSD: stivar.h,v 1.5 2010/11/01 06:41:50 skrll Exp $	*/
+/*	$NetBSD: stivar.h,v 1.6 2010/11/09 12:24:48 skrll Exp $	*/
 
-/*	$OpenBSD: stivar.h,v 1.15 2003/12/16 06:07:13 mickey Exp $	*/
+/*	$OpenBSD: stivar.h,v 1.24 2009/02/06 22:51:04 miod Exp $	*/
 
 /*
  * Copyright (c) 2000-2003 Michael Shalayeff
@@ -31,52 +31,104 @@
 #ifndef _IC_STIVAR_H_
 #define _IC_STIVAR_H_
 
-struct sti_softc {
-	device_t sc_dev;
-	void *sc_ih;
+struct sti_softc;
 
-	u_int	sc_wsmode;
-	u_int	sc_flags;
-#define	STI_TEXTMODE	0x0001
-#define	STI_CLEARSCR	0x0002
-#define	STI_CONSOLE	0x0004
-	int	sc_devtype;
-	int	sc_nscreens;
-	int	sc_bpp;
+/*
+ * STI ROM information - one per device
+ */
+struct sti_rom {
+	struct sti_softc	*rom_softc;	/* backpointer to device */
+	int			 rom_devtype;
 
-	bus_space_tag_t iot, memt;
-	bus_space_handle_t romh;
-	bus_addr_t base, fbaddr;
-	bus_size_t fblen;
+	bus_space_tag_t		 iot, memt;	/* XXX iot unused */
+	bus_space_handle_t	 romh;
+	bus_addr_t		*bases;
 
-	struct sti_dd sc_dd;		/* in word format */
-	struct sti_font sc_curfont;
-	struct sti_cfg sc_cfg;
-	struct sti_ecfg sc_ecfg;
+	struct sti_dd		 rom_dd;	/* in word format */
 
-	void	*sc_romfont;		/* ROM font copy, either in memory... */
-	u_int	sc_fontmaxcol;		/* ...or in off-screen frame buffer */
-	u_int	sc_fontbase;
-
-	uint8_t sc_rcmap[STI_NCMAP], sc_gcmap[STI_NCMAP], sc_bcmap[STI_NCMAP];
-	vaddr_t	sc_code;
-
-	sti_init_t	init;
-	sti_mgmt_t	mgmt;
-	sti_unpmv_t	unpmv;
-	sti_blkmv_t	blkmv;
-	sti_test_t	test;
-	sti_exhdl_t	exhdl;
-	sti_inqconf_t	inqconf;
-	sti_scment_t	scment;
-	sti_dmac_t	dmac;
-	sti_flowc_t	flowc;
-	sti_utiming_t	utiming;
-	sti_pmgr_t	pmgr;
-	sti_util_t	util;
+	vaddr_t			 rom_code;
+	sti_init_t		 init;
+	sti_mgmt_t		 mgmt;
+	sti_unpmv_t		 unpmv;
+	sti_blkmv_t		 blkmv;
+	sti_test_t		 test;
+	sti_exhdl_t		 exhdl;
+	sti_inqconf_t		 inqconf;
+	sti_scment_t		 scment;
+	sti_dmac_t		 dmac;
+	sti_flowc_t		 flowc;
+	sti_utiming_t		 utiming;
+	sti_pmgr_t		 pmgr;
+	sti_util_t		 util;
 };
 
-void sti_attach_common(struct sti_softc *sc);
-int sti_intr(void *v);
+/*
+ * STI screen information - one per head
+ */
+struct sti_screen {
+	struct sti_rom		*scr_rom;
+
+#ifdef notyet
+	u_int			 scr_flags;
+#endif
+
+	int			 scr_bpp;
+
+	struct sti_font		 scr_curfont;
+	struct sti_cfg		 scr_cfg;
+	struct sti_ecfg		 scr_ecfg;
+	char			 name[STI_DEVNAME_LEN];
+
+	void			*scr_romfont;	/* ROM font copy in memory... */
+	u_int			 scr_fontmaxcol;/* ...or in off-screen area */
+	u_int			 scr_fontbase;
+
+	uint8_t			 scr_rcmap[STI_NCMAP],
+				 scr_gcmap[STI_NCMAP],
+				 scr_bcmap[STI_NCMAP];
+
+	uint16_t		 fbheight, fbwidth;
+	uint16_t		 oheight, owidth;	/* offscreen size */
+	bus_addr_t		 fbaddr;
+	bus_size_t		 fblen;
+
+	/* wsdisplay information */
+	int			 scr_nscreens;
+	u_int			 scr_wsmode;
+	struct	wsscreen_descr	 scr_wsd;
+	struct	wsscreen_descr	*scr_scrlist[1];
+	struct	wsscreen_list	 scr_screenlist;
+};
+
+/*
+ * STI Device state
+ */
+struct sti_softc {
+	device_t sc_dev;
+#if notyet
+	void *sc_ih;
+#endif
+
+	u_int			sc_flags;
+#define	STI_CONSOLE	0x0001	/* first head is console... */
+#define	STI_ATTACHED	0x0002	/* ... and wsdisplay_cnattach() has been done */
+#define	STI_ROM_ENABLED	0x0004	/* PCI ROM is enabled */
+
+	bus_addr_t		bases[STI_REGION_MAX];
+	struct sti_rom		*sc_rom;
+	struct sti_screen	*sc_scr;
+
+	/* optional, required for PCI */
+	void			(*sc_enable_rom)(struct sti_softc *);
+	void			(*sc_disable_rom)(struct sti_softc *);
+};
+
+int	sti_attach_common(struct sti_softc *, bus_space_tag_t, bus_space_tag_t,
+	    bus_space_handle_t, u_int);
+int	sti_cnattach(struct sti_rom *, struct sti_screen *, bus_space_tag_t,
+	    bus_addr_t *, u_int);
+void	sti_describe(struct sti_softc *);
+void	sti_end_attach(struct sti_softc *);
+u_int	sti_rom_size(bus_space_tag_t, bus_space_handle_t);
 
 #endif /* _IC_STIVAR_H_ */
