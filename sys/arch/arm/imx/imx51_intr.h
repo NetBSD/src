1372648b698bd8956f2fc060688d8d2aa1646370
bsh <bsh>
2010-11-13 07:11:02 +0000

    i.MX51 support by SHIMIZU Ryo, Hashimoto Kenichi, and me.

---
+++ sys/arch/arm/imx/imx51_intr.h
@@ -0,0 +1,304 @@
+/*	$NetBSD: imx51_intr.h,v 1.1 2010/11/13 07:11:02 bsh Exp $	*/
+/*-
+ * Copyright (c) 2009 SHIMIZU Ryo <ryo@nerv.org>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+ * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+#ifndef _ARM_IMX_IMX51_INTR_H_
+#define	_ARM_IMX_IMX51_INTR_H_
+
+#define	IRQ_RSVD0	0	/* Reserved */
+#define	IRQ_ESDHC1	1	/* Enhanced SDHC Interrupt Request */
+#define	IRQ_ESDHC2	2	/* Enhanced SDHC Interrupt Request */
+#define	IRQ_ESDHC3	3	/* CE-ATA Interrupt Request based on eSDHC-3 */
+#define	IRQ_ESDHC4	4	/* Enhanced SDHC Interrupt Request */
+#define	IRQ_DAP		5	/* Power-up Request */
+#define	IRQ_SDMA	6	/* "AND" of all 48 interrupts from all the channels */
+#define	IRQ_IOMUX	7	/* POWER FAIL interrupt */
+#define	IRQ_EMI_NFC	8	/* nfc interrupt out */
+#define	IRQ_VPU		9	/* VPU Interrupt Request */
+#define	IRQ_IPUEXERR	10	/* IPUEX Error Interrupt */
+#define	IRQ_IPUEXSYNC	11	/* IPUEX Sync Interrupt */
+#define	IRQ_GPU3D	12	/* GPU3D Interrupt Request */
+#define	IRQ_RSVD13	13	/* Reserved */
+#define	IRQ_USBOH1	14	/* USB Host 1 */
+#define	IRQ_EMI		15	/* Consolidated EMI Interrupt  */
+#define	IRQ_USBOH2	16	/* USB Host 2 */
+#define	IRQ_USBOH3	17	/* USB Host 3 */
+#define	IRQ_USBOH_OTG	18	/* USB OTG */
+#define	IRQ_SAHARA0	19	/* SAHARA host 0 (TrustZone) Intr */
+#define	IRQ_SAHARA1	20	/* SAHARA host 1 (non-TrustZone) Intr */
+#define	IRQ_SCC_HIGH	21	/* Security Monitor High Priority Interrupt Request. */
+#define	IRQ_SCC_SEC	22	/* Secure (TrustZone) Interrupt Request. */
+#define	IRQ_SCC		23	/* Regular (Non-Secure) Interrupt Request. */
+#define	IRQ_SRTC_CONS	24	/* SRTC Consolidated Interrupt. Non TZ. */
+#define	IRQ_SRTC_SEC	25	/* SRTC Security Interrupt. TZ. */
+#define	IRQ_RTIC	26	/* RTIC (Trust Zone) Interrupt Request. Indicates that the RTI selected memory block(s) during single-hash/boot mode. */
+#define	IRQ_CSU		27	/* CSU Interrupt Request 1. Indicates to the processor that on asserted */
+#define	IRQ_SLIMBUS	28	/* Slimbus Interrupt Request */
+#define	IRQ_SSI1	29	/* SSI-1 Interrupt Request */
+#define	IRQ_SSI2	30	/* SSI-2 Interrupt Request */
+#define	IRQ_UART1	31	/* UART-1 ORed interrupt */
+#define	IRQ_UART2	32	/* UART-2 ORed interrupt */
+#define	IRQ_UART3	33	/* UART-3 ORed interrupt */
+#define	IRQ_RSVD34	34	/* Reserved */
+#define	IRQ_RSVD35	35	/* Reserved */
+#define	IRQ_ECSPI1	36	/* eCSPI1 interrupt request line to the core. */
+#define	IRQ_ECSPI2	37	/* eCSPI2 interrupt request line to the core. */
+#define	IRQ_CSPI	38	/* CSPI interrupt request line to the core. */
+#define	IRQ_GPT		39	/* "OR" of GPT Rollover interrupt line, Input Capture 1 and 2 lin 3 Interrupt lines" */
+#define	IRQ_EPIT1	40	/* EPIT1 output compare interrupt */
+#define	IRQ_EPIT2	41	/* EPIT2 output compare interrupt */
+#define	IRQ_GPIO7	42	/* Active HIGH Interrupt from INT7 from GPIO */
+#define	IRQ_GPIO6	43	/* Active HIGH Interrupt from INT6 from GPIO */
+#define	IRQ_GPIO5	44	/* Active HIGH Interrupt from INT5 from GPIO */
+#define	IRQ_GPIO4	45	/* Active HIGH Interrupt from INT4 from GPIO */
+#define	IRQ_GPIO3	46	/* Active HIGH Interrupt from INT3 from GPIO */
+#define	IRQ_GPIO2	47	/* Active HIGH Interrupt from INT2 from GPIO */
+#define	IRQ_GPIO1	48	/* Active HIGH Interrupt from INT1 from GPIO */
+#define	IRQ_GPIO0	49	/* Active HIGH Interrupt from INT0 from GPIO */
+#define	IRQ_GPIO1_LOW	50	/* Combined interrupt indication for GPIO1 signal 0 throughout 15 */
+#define	IRQ_GPIO1_HIGH	51	/* Combined interrupt indication for GPIO1 signal 16 throughout 31 */
+#define	IRQ_GPIO2_LOW	52	/* Combined interrupt indication for GPIO2 signal 0 throughout 15 */
+#define	IRQ_GPIO2_HIGH	53	/* Combined interrupt indication for GPIO2 signal 16 throughout 31 */
+#define	IRQ_GPIO3_LOW	54	/* Combined interrupt indication for GPIO3 signal 0 throughout 15 */
+#define	IRQ_GPIO3_HIGH	55	/* Combined interrupt indication for GPIO3 signal 16 throughout 31 */
+#define	IRQ_GPIO4_LOW	56	/* Combined interrupt indication for GPIO4 signal 0 throughout 15 */
+#define	IRQ_GPIO4_HIGH	57	/* Combined interrupt indication for GPIO4 signal 16 throughout 31 */
+#define	IRQ_WDOG1	58	/* Watchdog Timer reset */
+#define	IRQ_WDOG2	59	/* Watchdog Timer reset */
+#define	IRQ_KPP		60	/* Keypad Interrupt */
+#define	IRQ_PWM1	61	/* "Cumulative interrupt line. "OR" of Rollover Interrupt line, Compare Interrupt line and FIFO Waterlevel crossing interrupt line." */
+#define	IRQ_I2C1	62	/* I2C-1 Interrupt */
+#define	IRQ_I2C2	63	/* I2C-2 Interrupt */
+#define	IRQ_HS_I2C	64	/* High Speed I2C Interrupt */
+#define	IRQ_RSVD65	65	/* Reserved */
+#define	IRQ_RSVD66	66	/* Reserved */
+#define	IRQ_SIM1	67	/* "SIM interrupt composed of oef, xte, sdi1, and sdi0" */
+#define	IRQ_SIM2	68	/* "SIM interrupt composed of tc, etc, tfe, and rdrf" */
+#define	IRQ_IIM		69	/* Interrupt request to the processor. Indicates to the processor that program or explicit sense cycle is completed successfully or in case of error. This signal is low-asserted. */
+#define	IRQ_PATA	70	/* Parallel ATA host controller interrupt request */
+#define	IRQ_CCM1	71	/* "CCM, Interrupt Request 1" */
+#define	IRQ_CCM2	72	/* "CCM, Interrupt Request 2" */
+#define	IRQ_GPC1	73	/* "GPC, Interrupt Request 1" */
+#define	IRQ_GPC2	74	/* "GPC, Interrupt Request 2" */
+#define	IRQ_SRC		75	/* SRC interrupt request */
+#define	IRQ_NEON	76	/* Neon Monitor Interrupt */
+#define	IRQ_PERFUNIT	77	/* Performance Unit Interrupt */
+#define	IRQ_CTI		78	/* CTI IRQ */
+#define	IRQ_DEBUG1	79	/* "Debug Interrupt, from Cross-Trigger Interface 1" */
+#define	IRQ_DEBUG1B	80	/* "Debug Interrupt, from Cross-Trigger Interface 1" */
+#define	IRQ_GPU2D	84	/* GPU2D (OpenVG) general interrupt */
+#define	IRQ_GPU2D_BUSY	85	/* GPU2D (OpenVG) busy signal (for S/W power gating feasibility) */
+#define	IRQ_RSVD86	86	/* Reserved */
+#define	IRQ_FEC		87	/* Fast Ethernet Controller Interrupt request (OR of 13 interrupt sources) */
+#define	IRQ_OWIRE	88	/* 1-Wire Interrupt Request */
+#define	IRQ_DEBUG2	89	/* "Debug Interrupt, from Cross-Trigger Interface 2" */
+#define	IRQ_SJC		90	/* SJC */
+#define	IRQ_SPDIF	91	/* SPDIF */
+#define	IRQ_TVE		92	/* TVE */
+#define	IRQ_FIRI	93	/* FIRI Intr (OR of all 4 interrupt sources) */
+#define	IRQ_PWM2	94	/* "Cumulative interrupt line. "OR" of Rollover Interrupt line, Compare Interrupt line and FIFO" */
+#define	IRQ_SLIMBUS_E	95	/* Slimbus Interrupt Request (Exceptional cases) */
+#define	IRQ_SSI3	96	/* SSI-3 Interrupt Request */
+#define	IRQ_EMI_BOOT	97	/* Boot sequence completed interrupt */
+#define	IRQ_DEBUG3	98	/* "Debug Interrupt, from Cross-Trigger Interface 3" */
+#define	IRQ_RXSMC	99	/* Rx SMC receive interrupt (Generated whenever SLM receives a shared channel message) */
+#define	IRQ_VPU_IDLE	100	/* Idle interrupt from VPU (for S/W power gating) */
+#define	IRQ_EMI_TRNSFER	101	/* Indicates all pages have been transferred to NFC during an auto program operation. */
+#define	IRQ_GPU3D_IDLE	102	/* Idle interrupt from GPU3D (for S/W power gating) */
+#define	IRQ_RSVD103	103	/* Reserved */
+#define	IRQ_RSVD104	104	/* Reserved */
+#define	IRQ_RSVD105	105	/* Reserved */
+#define	IRQ_RSVD106	106	/* Reserved */
+#define	IRQ_RSVD107	107	/* Reserved */
+#define	IRQ_RSVD108	108	/* Reserved */
+#define	IRQ_RSVD109	109	/* Reserved */
+#define	IRQ_RSVD110	110	/* Reserved */
+#define	IRQ_RSVD111	111	/* Reserved */
+#define	IRQ_RSVD112	112	/* Reserved */
+#define	IRQ_RSVD113	113	/* Reserved */
+#define	IRQ_RSVD114	114	/* Reserved */
+#define	IRQ_RSVD115	115	/* Reserved */
+#define	IRQ_RSVD116	116	/* Reserved */
+#define	IRQ_RSVD117	117	/* Reserved */
+#define	IRQ_RSVD118	118	/* Reserved */
+#define	IRQ_RSVD119	119	/* Reserved */
+#define	IRQ_RSVD120	120	/* Reserved */
+#define	IRQ_RSVD121	121	/* Reserved */
+#define	IRQ_RSVD122	122	/* Reserved */
+#define	IRQ_RSVD123	123	/* Reserved */
+#define	IRQ_RSVD124	124	/* Reserved */
+#define	IRQ_RSVD125	125	/* Reserved */
+#define	IRQ_RSVD126	126	/* Reserved */
+#define	IRQ_RSVD127	127	/* Reserved */
+
+#ifdef _LOCORE
+
+#define	ARM_IRQ_HANDLER	_C_LABEL(imx51_irq_handler)
+
+#else
+
+#define	TZIC_INTR_SOURCE_NAMES		\
+{	"rsvd0",	/* IRQ0 */	\
+	"esdhc1",	/* IRQ1 */	\
+	"esdhc2",	/* IRQ2 */	\
+	"esdhc3",	/* IRQ3 */	\
+	"esdhc4",	/* IRQ4 */	\
+	"dap",		/* IRQ5 */	\
+	"sdma",		/* IRQ6 */	\
+	"iomux",	/* IRQ7 */	\
+	"emi",		/* IRQ8 */	\
+	"vpu",		/* IRQ9 */	\
+	"ipuexerr",	/* IRQ10 */	\
+	"ipuexsync",	/* IRQ11 */	\
+	"gpu3d",	/* IRQ12 */	\
+	"rsvd13",	/* IRQ13 */	\
+	"usboh1",	/* IRQ14 */	\
+	"emi",		/* IRQ15 */	\
+	"usboh2",	/* IRQ16 */	\
+	"usboh3",	/* IRQ17 */	\
+	"usboh3",	/* IRQ18 */	\
+	"sahara0",	/* IRQ19 */	\
+	"sahara1",	/* IRQ20 */	\
+	"scc_high",	/* IRQ21 */	\
+	"scc_sec",	/* IRQ22 */	\
+	"scc",		/* IRQ23 */	\
+	"srtc_cons",	/* IRQ24 */	\
+	"srtc_sec",	/* IRQ25 */	\
+	"rtic",		/* IRQ26 */	\
+	"csu",		/* IRQ27 */	\
+	"slimbus",	/* IRQ28 */	\
+	"ssi1",		/* IRQ29 */	\
+	"ssi2",		/* IRQ30 */	\
+	"uart1",	/* IRQ31 */	\
+	"uart2",	/* IRQ32 */	\
+	"uart3",	/* IRQ33 */	\
+	"rsvd34",	/* IRQ34 */	\
+	"rsvd35",	/* IRQ35 */	\
+	"ecspi1",	/* IRQ36 */	\
+	"ecspi2",	/* IRQ37 */	\
+	"cspi",		/* IRQ38 */	\
+	"gpt",		/* IRQ39 */	\
+	"epit1",	/* IRQ40 */	\
+	"epit2",	/* IRQ41 */	\
+	"gpio7",	/* IRQ42 */	\
+	"gpio6",	/* IRQ43 */	\
+	"gpio5",	/* IRQ44 */	\
+	"gpio4",	/* IRQ45 */	\
+	"gpio3",	/* IRQ46 */	\
+	"gpio2",	/* IRQ47 */	\
+	"gpio1",	/* IRQ48 */	\
+	"gpio0",	/* IRQ49 */	\
+	"gpio1_low",	/* IRQ50 */	\
+	"gpio1_high",	/* IRQ51 */	\
+	"gpio2_low",	/* IRQ52 */	\
+	"gpio2_high",	/* IRQ53 */	\
+	"gpio3_low",	/* IRQ54 */	\
+	"gpio3_high",	/* IRQ55 */	\
+	"gpio4_low",	/* IRQ56 */	\
+	"gpio4_high",	/* IRQ57 */	\
+	"wdog1",	/* IRQ58 */	\
+	"wdog2",	/* IRQ59 */	\
+	"kpp",		/* IRQ60 */	\
+	"pwm1",		/* IRQ61 */	\
+	"i2c1",		/* IRQ62 */	\
+	"i2c2",		/* IRQ63 */	\
+	"hs_i2c",	/* IRQ64 */	\
+	"rsvd65",	/* IRQ65 */	\
+	"rsvd66",	/* IRQ66 */	\
+	"sim1",		/* IRQ67 */	\
+	"sim2",		/* IRQ68 */	\
+	"iim",		/* IRQ69 */	\
+	"pata",		/* IRQ70 */	\
+	"ccm1",		/* IRQ71 */	\
+	"ccm2",		/* IRQ72 */	\
+	"gpc1",		/* IRQ73 */	\
+	"gpc2",		/* IRQ74 */	\
+	"src",		/* IRQ75 */	\
+	"neon",		/* IRQ76 */	\
+	"perfunit",	/* IRQ77 */	\
+	"cti",		/* IRQ78 */	\
+	"debug1",	/* IRQ79 */	\
+	"debug1",	/* IRQ80 */	\
+	"gpu2d",	/* IRQ84 */	\
+	"gpu2d_busy",	/* IRQ85 */	\
+	"rsvd86",	/* IRQ86 */	\
+	"fec",		/* IRQ87 */	\
+	"owire",	/* IRQ88 */	\
+	"debug2",	/* IRQ89 */	\
+	"sjc",		/* IRQ90 */	\
+	"spdif",	/* IRQ91 */	\
+	"tve",		/* IRQ92 */	\
+	"firi",		/* IRQ93 */	\
+	"pwm2",		/* IRQ94 */	\
+	"slimbus_e",	/* IRQ95 */	\
+	"ssi3",		/* IRQ96 */	\
+	"emi_boot",	/* IRQ97 */	\
+	"debug3",	/* IRQ98 */	\
+	"rxsmc",	/* IRQ99 */	\
+	"vpu_idle",	/* IRQ100 */	\
+	"emi_nfc",	/* IRQ101 */	\
+	"gpu3d_idle",	/* IRQ102 */	\
+	"rsvd103",	/* IRQ103 */	\
+	"rsvd104",	/* IRQ104 */	\
+	"rsvd105",	/* IRQ105 */	\
+	"rsvd106",	/* IRQ106 */	\
+	"rsvd107",	/* IRQ107 */	\
+	"rsvd108",	/* IRQ108 */	\
+	"rsvd109",	/* IRQ109 */	\
+	"rsvd110",	/* IRQ110 */	\
+	"rsvd111",	/* IRQ111 */	\
+	"rsvd112",	/* IRQ112 */	\
+	"rsvd113",	/* IRQ113 */	\
+	"rsvd114",	/* IRQ114 */	\
+	"rsvd115",	/* IRQ115 */	\
+	"rsvd116",	/* IRQ116 */	\
+	"rsvd117",	/* IRQ117 */	\
+	"rsvd118",	/* IRQ118 */	\
+	"rsvd119",	/* IRQ119 */	\
+	"rsvd120",	/* IRQ120 */	\
+	"rsvd121",	/* IRQ121 */	\
+	"rsvd122",	/* IRQ122 */	\
+	"rsvd123",	/* IRQ123 */	\
+	"rsvd124",	/* IRQ124 */	\
+	"rsvd125",	/* IRQ125 */	\
+	"rsvd126",	/* IRQ126 */	\
+	"rsvd127"	/* IRQ127 */	\
+}
+
+#define	PIC_MAXSOURCES		128
+#define	PIC_MAXMAXSOURCES	(PIC_MAXSOURCES+128)
+
+#include <arm/pic/picvar.h>
+
+int _splraise(int);
+int _spllower(int);
+void splx(int);
+const char *intr_typename(int);
+
+void imx51_irq_handler(void *);
+
+#endif /* !_LOCORE */
+
+#endif /* _ARM_IMX_IMX51_INTR_H_ */
---
+++ sys/arch/arm/imx/imx51reg.h
@@ -0,0 +1,295 @@
+/* $NetBSD: imx51reg.h,v 1.1 2010/11/13 07:11:03 bsh Exp $ */
+/*-
+ * Copyright (c) 2007 The NetBSD Foundation, Inc.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to The NetBSD Foundation
+ * by Matt Thomas.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef _ARM_IMX_IMX51REG_H_
+#define	_ARM_IMX_IMX51REG_H_
+
+#define	BOOTROM_BASE	0x00000000
+#define	BOOTROM_SIZE	0x9000
+
+#define	SCCRAM_BASE	0x1ffe0000
+#define	SCCRAM_SIZE	0x20000
+
+#define	GPUMEM_BASE	0x20000000
+#define	GPUMEM_SIZE	0x20000
+
+#define	GPU_BASE	0x30000000
+#define	GPU_SIZE	0x10000000
+
+/* LCD controller */
+#define	IPUEX_BASE	0x40000000
+#define	IPUEX_SIZE	0x20000000
+
+#define	DEBUGROM_BASE	0x60000000
+#define	DEBUGROM_SIZE	0x1000
+
+#define	ESDHC1_BASE	0x70004000
+#define	ESDHC2_BASE	0x70008000
+#define	ESDHC3_BASE	0x70020000
+#define	ESDHC4_BASE	0x70024000
+#define	ESDHC_SIZE	0x4000
+
+#define	UART1_BASE	0x73fbc000
+#define	UART2_BASE	0x73fc0000
+#define	UART3_BASE	0x7000c000
+/* register definitions in imxuartreg.h */
+
+#define	ECSPI1_BASE	0x70010000
+#define	ECSPI2_BASE	0x83fac000
+#define	ECSPI_SIZE	0x4000
+
+#define	SSI1_BASE	0x83fcc000
+#define	SSI2_BASE	0x70014000
+#define	SSI3_BASE	0x83fe8000
+/* register definitions in imxssireg.h */
+
+#define	SPDIF_BASE	0x70028000
+#define	SPDIF_SIZE	0x4000
+
+#define	PATA_UDMA_BASE	0x70030000
+#define	PATA_UDMA_SIZE	0x4000
+#define	PATA_PIO_BASE	0x83fe0000
+#define	PATA_PIO_SIZE	0x4000
+
+#define	SLM_BASE	0x70034000
+#define	SLM_SIZE	0x4000
+
+#define	HSI2C_BASE	0x70038000
+#define	HSI2C_SIZE	0x4000
+
+#define	SPBA_BASE	0x7003c000
+#define	SPBA_SIZE	0x4000
+
+#define	USBOH3_BASE	0x73f80000
+#define	USBOH3_PL301_BASE	0x73fc4000
+#define	USB_OTG_BASE	0x43F88000
+#define	USB_EHCI1_BASE	0x43F88200
+#define	USB_EHCI2_BASE	0x43F88400
+#define	USB_EHCI_SIZE	0x0200
+#define	USB_CONTROL	0x43F88600
+
+
+/* GPIO module */
+
+#define	GPIO_BASE(n)	(0x73f84000 + 0x4000 * ((n)-1))
+
+#define	GPIO1_BASE	GPIO_BASE(1)
+#define	GPIO2_BASE	GPIO_BASE(2)
+#define	GPIO3_BASE	GPIO_BASE(3)
+#define	GPIO4_BASE	GPIO_BASE(4)
+
+#define	GPIO_NPINS		32
+#define	GPIO_NGROUPS		3
+
+#define	KPP_BASE	0x73f94000
+/* register definitions in imxkppreg.h */
+
+#define	WDOG1_BASE	0x73f98000
+#define	WDOG2_BASE	0x73f9c000
+#define	WDOG_SIZE	0x4000
+
+#define	GPT_BASE	0x73fa0000
+#define	GPT_SIZE	0x4000
+
+#define	SRTC_BASE	0x73fa4000
+#define	SRTC_SIZE	0x4000
+
+/* IO multiplexor */
+#define	IOMUXC_BASE	0x73fa8000
+#define	IOMUXC_SIZE	0x4000
+
+#define	IOMUXC_MUX_CTL		0x001c		/* multiprex control */
+#define	 IOMUX_CONFIG_ALT0	(0)
+#define	 IOMUX_CONFIG_ALT1	(1)
+#define	 IOMUX_CONFIG_ALT2	(2)
+#define	 IOMUX_CONFIG_ALT3	(3)
+#define	 IOMUX_CONFIG_ALT4	(4)
+#define	 IOMUX_CONFIG_ALT5	(5)
+#define	 IOMUX_CONFIG_ALT6	(6)
+#define	 IOMUX_CONFIG_ALT7	(7)
+#define	IOMUXC_PAD_CTL		0x03f0		/* pad control */
+#define	 PAD_CTL_HYS_NONE	(0x0 << 8)
+#define	 PAD_CTL_HYS_ENABLE	(0x1 << 8)
+#define	 PAD_CTL_PKE_NONE	(0x0 << 7)
+#define	 PAD_CTL_PKE_ENABLE	(0x1 << 7)
+#define	 PAD_CTL_PUE_KEEPER	(0x0 << 6)
+#define	 PAD_CTL_PUE_PULL	(0x1 << 6)
+#define	 PAD_CTL_PUS_100K_PD	(0x0 << 4)
+#define	 PAD_CTL_PUS_47K_PU	(0x1 << 4)
+#define	 PAD_CTL_PUS_100K_PU	(0x2 << 4)
+#define	 PAD_CTL_PUS_22K_PU	(0x3 << 4)
+#define	 PAD_CTL_ODE_CMOS	(0x0 << 3)
+#define	 PAD_CTL_ODE_OpenDrain	(0x1 << 3)
+#define	 PAD_CTL_DSE_LOW	(0x0 << 1)
+#define	 PAD_CTL_DSE_MID	(0x1 << 1)
+#define	 PAD_CTL_DSE_HIGH	(0x2 << 1)
+#define	 PAD_CTL_DSE_MAX	(0x3 << 1)
+#define	 PAD_CTL_SRE_SLOW	(0x0 << 0)
+#define	 PAD_CTL_SRE_FAST	(0x1 << 0)
+#define	IOMUXC_INPUT_CTL	0x08c4		/* input control */
+#define	 INPUT_DAISY_0		0
+#define	 INPUT_DAISY_1		1
+#define	 INPUT_DAISY_2		2
+#define	 INPUT_DAISY_3		3
+#define	 INPUT_DAISY_4		4
+#define	 INPUT_DAISY_5		5
+#define	 INPUT_DAISY_6		6
+#define	 INPUT_DAISY_7		7
+
+/*
+ * IOMUX index
+ */
+#define	IOMUX_PIN_TO_MUX_ADDRESS(pin)	(((pin) >> 16) & 0xffff)
+#define	IOMUX_PIN_TO_PAD_ADDRESS(pin)	(((pin) >>  0) & 0xffff)
+
+#define	IOMUX_PIN(mux_adr, pad_adr)			\
+	(((mux_adr) << 16) | (((pad_adr) << 0)))
+#define	IOMUX_MUX_NONE	0xffff
+#define	IOMUX_PAD_NONE	0xffff
+
+/* EPIT */
+#define	EPIT1_BASE	0x73FAC000
+#define	EPIT2_BASE	0x73FB0000
+/* register definitions in imxepitreg.h */
+
+#define	PWM1_BASE	0x73fb4000
+#define	PWM2_BASE	0x73fb8000
+#define	PWM_SIZE	0x4000
+
+#define	SRC_BASE	0x73fd0000
+#define	SRC_SIZE	0x4000
+
+#define	CCM_BASE	0x73fd4000
+#define	CCM_SIZE	0x4000
+
+#define	GPC_BASE	0x73fd8000
+#define	GPC_SIZE	0x4000
+
+#define	DPLLIP1_BASE	0x83f80000
+#define	DPLLIP2_BASE	0x83f84000
+#define	DPLLIP3_BASE	0x83f88000
+#define	DPLLIP_SIZE	0x4000
+
+#define	AHBMAX_BASE	0x83f94000
+#define	AHBMAX_SIZE	0x4000
+
+#define	IIM_BASE	0x83f98000
+#define	IIM_SIZE	0x4000
+
+#define	CSU_BASE	0x83f9c000
+#define	CSU_SIZE	0x4000
+
+#define	OWIRE_BASE	0x83fa4000	/* 1-wire */
+#define	OWIRE_SIZE	0x4000
+
+#define	FIRI_BASE	0x83fa8000
+#define	FIRI_SIZE	0x4000
+
+
+#define	SDMA_BASE	0x83fb0000
+#define	SDMA_SIZE	0x4000
+/* see imxsdmareg.h for register definitions */
+
+#define	SCC_BASE	0x83fb4000
+#define	SCC_SIZE	0x4000
+
+#define	ROMCP_BASE	0x83fb8000
+#define	ROMCP_SIZE	0x4000
+
+#define	RTIC_BASE	0x83fbc000
+#define	RTIC_SIZE	0x4000
+
+#define	CSPI_BASE	0x83fc0000
+#define	CSPI_SIZE	0x4000
+
+#define	I2C1_BASE	0x83fc8000
+#define	I2C2_BASE	0x83fc4000
+/* register definitions in imxi2creg.h */
+
+#define	AUDMUX_BASE	0x83fd0000
+#define	AUDMUX_SIZE	0x4000
+#define	AUDMUX_PTCR(n)	((n - 1) * 0x8)
+#define	 PTCR_TFSDIR	(1 << 31)
+#define	 PTCR_TFSEL(x)	(((x) & 0x7) << 27)
+#define	 PTCR_TCLKDIR	(1 << 26)
+#define	 PTCR_TCSEL(x)	(((x) & 0x7) << 22)
+#define	 PTCR_RFSDIR	(1 << 21)
+#define	 PTCR_RFSEL(x)	(((x) & 0x7) << 17)
+#define	 PTCR_RCLKDIR	(1 << 16)
+#define	 PTCR_RCSEL(x)	(((x) & 0x7) << 12)
+#define	 PTCR_SYN	(1 << 11)
+
+#define	AUDMUX_PDCR(n)	((n - 1) * 0x8 + 0x4)
+#define	 PDCR_RXDSEL(x)	(((x) & 0x7) << 13)
+#define	 PDCR_TXRXEN	(1 << 12)
+#define	 PDCR_MODE(x)	(((x) & 0x3) << 8)
+#define	 PDCR_INMMASK(x)	(((x) & 0xff) << 0)
+#define	AUDMUX_CNMCR	0x38
+
+#define	EMI_BASE	0x83fd8000
+#define	EMI_SIZE	0x4000
+
+#define	SIM_BASE	0x83fe4000
+#define	SIM_SIZE	0x4000
+
+#define	FEC_BASE	0x83fec000
+#define	FEC_SIZE	0x4000
+#define	TVE_BASE	0x83ff0000
+#define	TVE_SIZE	0x4000
+#define	VPU_BASE	0x83ff4000
+#define	VPU_SIZE	0x4000
+#define	SAHARA_BASE	0x83ff8000
+#define	SAHARA_SIZE	0x4000
+
+#define	CSD0DDR_BASE	0x90000000
+#define	CSD1DDR_BASE	0xa0000000
+#define	CSDDDR_SIZE	0x10000000	/* 256MiB */
+#define	CS0_BASE	0xb0000000
+#define	CS0_SIZE	0x08000000	/* 128MiB */
+#define	CS1_BASE	0xb8000000
+#define	CS1_SIZE	0x08000000	/* 128MiB */
+#define	CS2_BASE	0xc0000000
+#define	CS2_SIZE	0x08000000	/* 128MiB */
+#define	CS3_BASE	0xc8000000
+#define	CS3_SIZE	0x04000000	/* 64MiB */
+#define	CS4_BASE	0xcc000000
+#define	CS4_SIZE	0x02000000	/* 32MiB */
+#define	CS5_BASE	0xcefe0000
+#define	CS5_SIZE	0x00010000	/* 32MiB */
+#define	NAND_FLASH_BASE	0xcfff0000	/* internal buffer */
+#define	NAND_FLASH_SIZE	0x00010000
+
+#define	GPU2D_BASE	0xd0000000
+#define	GPU2D_SIZE	0x10000000
+
+#define	TZIC_BASE		0xe0000000
+/* register definitions in imx51_tzicreg.h */
+
+#endif /* _ARM_IMX_IMX51REG_H_ */
