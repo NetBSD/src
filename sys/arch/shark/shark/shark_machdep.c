bcd123c8a9028c9bc031a5023456d4185f5ec770
uebayasi <uebayasi@NetBSD.org>
2010-11-14 03:36:52 +0000

    This uses autoconf(9) API, include sys/device.h.

--- sys/arch/shark/shark/shark_machdep.c
+++ sys/arch/shark/shark/shark_machdep.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: shark_machdep.c,v 1.37 2009/03/14 15:36:13 dsl Exp $	*/
+/*	$NetBSD: shark_machdep.c,v 1.38 2010/11/14 03:36:52 uebayasi Exp $	*/
 
 /*
  * Copyright 1997
@@ -38,7 +38,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: shark_machdep.c,v 1.37 2009/03/14 15:36:13 dsl Exp $");
+__KERNEL_RCSID(0, "$NetBSD: shark_machdep.c,v 1.38 2010/11/14 03:36:52 uebayasi Exp $");
 
 #include "opt_ddb.h"
 #include "opt_modular.h"
@@ -52,6 +52,7 @@ __KERNEL_RCSID(0, "$NetBSD: shark_machdep.c,v 1.37 2009/03/14 15:36:13 dsl Exp $
 #include <sys/buf.h>
 #include <sys/exec.h>
 #include <sys/ksyms.h>
+#include <sys/device.h>
 
 #include <uvm/uvm_extern.h>
 
