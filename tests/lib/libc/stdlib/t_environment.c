cdb4736e39b21616d9c40af66280a04ed01ee740
tron <tron@NetBSD.org>
2010-11-14 19:19:24 +0000

    Add a new regression test "t_threaded" which tests using getenv_r(3), putenv(3), setenv(3) and unsetenv(3) concurrently.

--- tests/lib/libc/stdlib/t_environment.c
+++ tests/lib/libc/stdlib/t_environment.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: t_environment.c,v 1.10 2010/11/14 18:15:08 tron Exp $	*/
+/*	$NetBSD: t_environment.c,v 1.11 2010/11/14 19:19:24 tron Exp $	*/
 /*-
  * Copyright (c) 2010 The NetBSD Foundation, Inc.
  * All rights reserved.
@@ -32,19 +32,22 @@
  */
 
 #include <sys/cdefs.h>
-__RCSID("$NetBSD: t_environment.c,v 1.10 2010/11/14 18:15:08 tron Exp $");
+__RCSID("$NetBSD: t_environment.c,v 1.11 2010/11/14 19:19:24 tron Exp $");
 
 #include <atf-c.h>
 #include <errno.h>
+#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 ATF_TC(t_setenv);
 ATF_TC(t_putenv);
 ATF_TC(t_clearenv);
 ATF_TC(t_mixed);
 ATF_TC(t_getenv);
+ATF_TC(t_threaded);
 
 ATF_TC_HEAD(t_setenv, tc)
 {
@@ -75,6 +78,12 @@ ATF_TC_HEAD(t_getenv, tc)
 {
 	atf_tc_set_md_var(tc, "descr",
 	    "Test setenv(3), getenv(3)");
+}
+
+ATF_TC_HEAD(t_threaded, tc)
+{
+	atf_tc_set_md_var(tc, "descr",
+	    "Test concurrent environment access by multiple threads");
 }
 
 ATF_TC_BODY(t_setenv, tc)
@@ -185,6 +194,120 @@ ATF_TC_BODY(t_getenv, tc)
 	ATF_CHECK(unsetenv("EVIL") != -1);
 }
 
+#define	THREADED_NUM_THREADS	4
+#define	THREADED_NUM_VARS	16
+#define	THREADED_VAR_NAME	"THREADED%zu"
+#define	THREADED_RUN_TIME	5
+
+static void *
+thread_getenv_r(void *arg)
+{
+	time_t endtime;
+
+	endtime = *(time_t *)arg;
+	do {
+		size_t i;
+		char name[32], value[128];
+
+		i = lrand48() % THREADED_NUM_VARS;
+		(void)snprintf(name, sizeof(name), THREADED_VAR_NAME, i);
+
+		if (getenv_r(name, value, sizeof(value)) == -1) {
+			ATF_CHECK(errno == ENOENT);
+		}
+	} while (time(NULL) < endtime);
+
+	return NULL;
+}
+
+static void *
+thread_putenv(void *arg)
+{
+	time_t endtime;
+	size_t i;
+	static char vars[THREADED_NUM_VARS][128];
+
+	for (i = 0; i < THREADED_NUM_VARS; i++) {
+		(void)snprintf(vars[i], sizeof(vars[i]),
+		    THREADED_VAR_NAME "=putenv %ld", i, lrand48());
+	}
+
+	endtime = *(time_t *)arg;
+	do {
+		char name[128];
+
+		i = lrand48() % THREADED_NUM_VARS;
+		(void)strlcpy(name, vars[i], sizeof(name));
+		*strchr(name, '=') = '\0';
+
+		ATF_CHECK(unsetenv(name) != -1);
+		ATF_CHECK(putenv(vars[i]) != -1);
+	} while (time(NULL) < endtime);
+
+	return NULL;
+}
+
+static void *
+thread_setenv(void *arg)
+{
+	time_t endtime;
+
+	endtime = *(time_t *)arg;
+	do {
+		size_t i;
+		char name[32], value[64];
+
+		i = lrand48() % THREADED_NUM_VARS;
+		(void)snprintf(name, sizeof(name), THREADED_VAR_NAME, i);
+		(void)snprintf(value, sizeof(value), "setenv %ld", lrand48());
+
+		ATF_CHECK(setenv(name, value, 1) != -1);
+	} while (time(NULL) < endtime);
+
+	return NULL;
+}
+
+static void *
+thread_unsetenv(void *arg)
+{
+	time_t endtime;
+
+	endtime = *(time_t *)arg;
+	do {
+		size_t i;
+		char name[32];
+
+		i = lrand48() % THREADED_NUM_VARS;
+		(void)snprintf(name, sizeof(name), THREADED_VAR_NAME, i);
+
+		ATF_CHECK(unsetenv(name) != -1);
+	} while (time(NULL) < endtime);
+
+	return NULL;
+}
+
+ATF_TC_BODY(t_threaded, tc)
+{
+	time_t endtime;
+	size_t i;
+	pthread_t threads[THREADED_NUM_THREADS];
+
+	endtime = time(NULL) + THREADED_RUN_TIME;
+
+	i = 0;
+	ATF_CHECK(pthread_create(&threads[i++], NULL, thread_getenv_r,
+	    &endtime) == 0);
+	ATF_CHECK(pthread_create(&threads[i++], NULL, thread_putenv,
+	    &endtime) == 0);
+	ATF_CHECK(pthread_create(&threads[i++], NULL, thread_setenv,
+	    &endtime) == 0);
+	ATF_CHECK(pthread_create(&threads[i++], NULL, thread_unsetenv,
+	    &endtime) == 0);
+
+	while(i-- > 0)
+		ATF_CHECK(pthread_join(threads[i], NULL) == 0);
+}
+
 ATF_TP_ADD_TCS(tp)
 {
 	ATF_TP_ADD_TC(tp, t_setenv);
@@ -192,6 +315,7 @@ ATF_TP_ADD_TCS(tp)
 	ATF_TP_ADD_TC(tp, t_clearenv);
 	ATF_TP_ADD_TC(tp, t_mixed);
 	ATF_TP_ADD_TC(tp, t_getenv);
+	ATF_TP_ADD_TC(tp, t_threaded);
 
 	return atf_no_error();
 }
