a4088a423bfb38afc308d8efb23d1b5a99517832
bouyer <bouyer@NetBSD.org>
2010-11-14 13:40:31 +0000

    Explain why we hardwire lapic_cpu_number() to 0 on Xen.

--- sys/arch/xen/include/i82489var.h
+++ sys/arch/xen/include/i82489var.h
@@ -1,5 +1,11 @@
-/*	$NetBSD: i82489var.h,v 1.1 2006/09/28 18:53:15 bouyer Exp $	*/
+/*	$NetBSD: i82489var.h,v 1.2 2010/11/14 13:40:31 bouyer Exp $	*/
 
 #include <x86/i82489var.h>
+
+/*
+ * Xen doesn't give acces to the lapic. In addition, the lapic number provided
+ * by the dom0 to Xen when setting up iopics is ignored, the hypervisor will
+ * decide itself to which physical CPU the interrupt should be routed to.
+ */
 #undef lapic_cpu_number
 #define lapic_cpu_number() (0)
