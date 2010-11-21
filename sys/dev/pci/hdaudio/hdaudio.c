b8605c591b7fc52a558534f66e4302aba6911dd7
riz <riz>
2010-11-21 20:18:41 +0000

    Pull up following revision(s) (requested by jmcneill in ticket #1441): 	sys/dev/pci/hdaudio/hdaudio_afg.c: revision 1.23 	sys/dev/pci/hdaudio/hdaudio.c: revision 1.7 	sys/dev/pci/hdaudio/hdaudio_pci.h: revision 1.1 	sys/dev/pci/hdaudio/hdaudio_pci.c: revision 1.6 Reject invalid vendor IDs (say if there was a RIRB timeout while reading the VID registers) add support for nvidia hdaudio controllers properly round block and buffer sizes -- nvidia HDA controllers work now

--- sys/dev/pci/hdaudio/hdaudio.c
+++ sys/dev/pci/hdaudio/hdaudio.c
@@ -1,4 +1,4 @@
-/* $NetBSD: hdaudio.c,v 1.4.4.3 2009/10/18 16:50:13 bouyer Exp $ */
+/* $NetBSD: hdaudio.c,v 1.4.4.4 2010/11/21 20:18:41 riz Exp $ */
 
 /*
  * Copyright (c) 2009 Precedence Technologies Ltd <support@precedence.co.uk>
@@ -30,7 +30,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: hdaudio.c,v 1.4.4.3 2009/10/18 16:50:13 bouyer Exp $");
+__KERNEL_RCSID(0, "$NetBSD: hdaudio.c,v 1.4.4.4 2010/11/21 20:18:41 riz Exp $");
 
 #include <sys/types.h>
 #include <sys/param.h>
@@ -667,6 +667,10 @@ hdaudio_codec_attach(struct hdaudio_codec *co)
 	rid = hdaudio_command(co, 0, CORB_GET_PARAMETER, COP_REVISION_ID);
 	snc = hdaudio_command(co, 0, CORB_GET_PARAMETER,
 	    COP_SUBORDINATE_NODE_COUNT);
+
+	/* make sure the vendor and product IDs are valid */
+	if (vid == 0xffffffff || vid == 0x00000000)
+		return;
 
 #ifdef HDAUDIO_DEBUG
 	hda_print(sc, "Codec%02X: %04X:%04X HDA %d.%d rev %d stepping %d\n",
