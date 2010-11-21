3ecee4487a91728f5342e4e98e6332baaaf6adc4
uebayasi <uebayasi@NetBSD.org>
2010-11-21 12:42:59 +0000

    Resurrect PGO_ZERO support.
    When vnode pager encounters hole pages in XIP'ed vnodes, it fills
    page slots with PGO_ZERO and returns them back to the caller (fault
    handler).  Fault handlers are responsible to check page slots and
    redirect PGO_ZERO to the single "zero page" allocated by calling
    uvm_page_zeropage_alloc(9).
    
    The zero page is wired, read-only (PG_RDONLY) page.  It's shared
    by multiple vnodes, it has no single owner.
    
    XIP'ed vnodes are supposed to be "stable" during I/O (unlocked).
    Because XIP'ed mounts are always read-only.  There's no chance to
    change mappings of XIP'ed vnodes and their XIP'ed pages.  Thus the
    cached uobj is reused after pgo_get() for PGO_ZERO.
    
    (Do we need a new concept of "read-only UVM object"?)

--- sys/uvm/uvm_pager.h
+++ sys/uvm/uvm_pager.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: uvm_pager.h,v 1.38.14.1 2010/10/22 07:22:57 uebayasi Exp $	*/
+/*	$NetBSD: uvm_pager.h,v 1.38.14.2 2010/11/21 12:42:59 uebayasi Exp $	*/
 
 /*
  *
@@ -168,6 +168,8 @@ struct uvm_pagerops {
 
 /* page we are not interested in getting */
 #define PGO_DONTCARE ((struct vm_page *) -1L)	/* [get only] */
+/* page redirected to zero page (XIP hole page) */
+#define PGO_ZERO ((struct vm_page *) -2L)	/* [get only] */
 
 #ifdef _KERNEL
 
