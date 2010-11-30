01c4fb4e936cf104a5b71cb85eb1ae483367fc1e
dholland <dholland>
2010-11-30 10:50:29 +0000

    Time for 5.99.41 - struct componentbuf and struct nameidata both changed.

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.376 2010/11/19 06:46:33 dholland Exp $	*/
+/*	$NetBSD: param.h,v 1.377 2010/11/30 10:50:29 dholland Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -63,7 +63,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	599004000	/* NetBSD 5.99.40 */
+#define	__NetBSD_Version__	599004100	/* NetBSD 5.99.41 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
