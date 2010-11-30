7111131c113679f396b3404d176594df37511b19
pooka <pooka@NetBSD.org>
2010-11-30 14:29:05 +0000

    Get rid of the "sleep 1" by using rump_daemonize_begin/end(). Notably, md is a little tricky for this, since the ioctl that configures the service also blocks in the kernel.  Therefore, use an additional pthread to probe when the service is fully configured and the server can detach.
    Also, rawpart love.

--- tests/dev/md/h_mdserv.c
+++ tests/dev/md/h_mdserv.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: h_mdserv.c,v 1.1 2010/11/23 15:38:54 pooka Exp $	*/
+/*	$NetBSD: h_mdserv.c,v 1.2 2010/11/30 14:29:05 pooka Exp $	*/
 
 #include <sys/types.h>
 #include <sys/mman.h>
@@ -6,28 +6,104 @@
 
 #include <dev/md.h>
 
+#include <err.h>
+#include <errno.h>
 #include <fcntl.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 
 #include <rump/rump.h>
 #include <rump/rump_syscalls.h>
 
-#include "../../h_macros.h"
-
 #define MDSIZE (1024*1024)
 
-int
-main(void)
+#define REQUIRE(a, msg) if ((a) != 0) err(1, msg);
+
+static void *
+prober(void *arg)
 {
+	int fd, error;
+	char buf[4];
+	ssize_t n;
+
+	fd = rump_sys_open(arg, O_RDONLY);
+	for (;;) {
+		n = rump_sys_read(fd, buf, sizeof(buf));
+
+		switch (n) {
+		case 4:
+			error = 0;
+			goto out;
+
+		case -1:
+			if (errno == ENXIO) {
+				usleep(1000);
+				continue;
+			}
+
+			/* FALLTHROUGH */
+		default:
+			error = EPIPE;
+			goto out;
+		}
+	}
+ out:
+
+	error = rump_daemonize_done(error);
+	REQUIRE(error, "rump_daemonize_done");
+
+	if (error)
+		exit(1);
+
+	return NULL;
+}
+
+int
+main(int argc, char *argv[])
+{
+	pthread_t pt;
 	struct md_conf md;
-	int fd;
+	int fd, error;
+
+	if (argc != 2)
+		exit(1);
 
 	md.md_addr = malloc(MDSIZE);
 	md.md_size = MDSIZE;
 	md.md_type = MD_UMEM_SERVER;
 
-	RL(rump_init());
-	RL(fd = rump_sys_open("/dev/rmd0d", O_RDWR));
-	RL(rump_sys_ioctl(fd, MD_SETCONF, &md));
-	pause();
+	error = rump_daemonize_begin();
+	REQUIRE(error, "rump_daemonize_begin");
+
+	error = rump_init();
+	REQUIRE(error, "rump_init");
+
+	error = rump_init_server("unix://commsock");
+	REQUIRE(error, "init server");
+
+	if ((fd = rump_sys_open(argv[1], O_RDWR)) == -1)
+		err(1, "open");
+
+	/*
+	 * Now, configuring the md driver also causes our process
+	 * to start acting as the worker for the md.  Splitting is
+	 * into two steps in the driver is not easy, since md is
+	 * supposed to be unconfigured when the process dies
+	 * (process may exit between calling ioctl1 and ioctl2).
+	 * So, start a probe thread which attempt to read the md
+	 * and declares the md as configured when the read is
+	 * succesful.
+	 */
+	error = pthread_create(&pt, NULL, prober, argv[1]);
+	REQUIRE(error, "pthread_create");
+	pthread_detach(pt);
+
+	if (rump_sys_ioctl(fd, MD_SETCONF, &md) == -1) {
+		rump_daemonize_done(errno);
+		exit(1);
+	}
+
+	return 0;
 }
