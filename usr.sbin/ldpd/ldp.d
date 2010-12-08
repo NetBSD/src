57ad72fc9c6a91758e30329e1696045c95f4f227
kefren <kefren>
2010-12-08 07:20:14 +0000

    Add ldpd, a RFC 3036 compatible LDP speaker.

---
+++ usr.sbin/ldpd/ldp.d
@@ -0,0 +1,50 @@
+ldp.o: ldp.c /usr/include/netinet/in.h /usr/include/machine/int_types.h \
+  /usr/include/sys/cdefs.h /usr/include/machine/cdefs.h \
+  /usr/include/sys/cdefs_elf.h /usr/include/sys/ansi.h \
+  /usr/include/machine/ansi.h /usr/include/netinet6/in6.h \
+  /usr/include/sys/socket.h /usr/include/sys/featuretest.h \
+  /usr/include/sys/uio.h /usr/include/sys/stat.h /usr/include/sys/types.h \
+  /usr/include/machine/types.h /usr/include/machine/endian.h \
+  /usr/include/sys/endian.h /usr/include/machine/endian_machdep.h \
+  /usr/include/machine/bswap.h /usr/include/machine/byte_swap.h \
+  /usr/include/sys/bswap.h /usr/include/sys/fd_set.h \
+  /usr/include/pthread_types.h /usr/include/sys/time.h \
+  /usr/include/sys/select.h /usr/include/sys/sigtypes.h \
+  /usr/include/sys/satypes.h /usr/include/time.h /usr/include/sys/null.h \
+  /usr/include/arpa/inet.h /usr/include/stdio.h /usr/include/stdlib.h \
+  /usr/include/strings.h /usr/include/stdint.h \
+  /usr/include/machine/int_mwgwtypes.h /usr/include/machine/int_limits.h \
+  /usr/include/machine/int_const.h /usr/include/machine/wchar_limits.h \
+  /usr/include/string.h /usr/include/unistd.h /usr/include/sys/unistd.h \
+  ldp.h ldp_command.h socketops.h mpls_routes.h /usr/include/net/if.h \
+  /usr/include/sys/mutex.h /usr/include/sys/inttypes.h \
+  /usr/include/sys/stdint.h /usr/include/machine/int_fmtio.h \
+  /usr/include/machine/mutex.h /usr/include/x86/mutex.h \
+  /usr/include/sys/condvar.h /usr/include/sys/queue.h \
+  /usr/include/net/dlt.h /usr/include/net/pfil.h \
+  /usr/include/altq/if_altq.h /usr/include/net/if_arp.h \
+  /usr/include/net/route.h /usr/include/stdbool.h \
+  /usr/include/net/radix.h /usr/include/netmpls/mpls.h \
+  /usr/include/sys/param.h /usr/include/sys/syslimits.h \
+  /usr/include/sys/signal.h /usr/include/sys/siginfo.h \
+  /usr/include/machine/signal.h /usr/include/machine/trap.h \
+  /usr/include/x86/trap.h /usr/include/machine/fpu.h \
+  /usr/include/machine/mcontext.h /usr/include/machine/frame_regs.h \
+  /usr/include/sys/ucontext.h /usr/include/machine/param.h \
+  /usr/include/machine/limits.h /usr/include/sys/proc.h \
+  /usr/include/machine/proc.h /usr/include/sys/user.h \
+  /usr/include/machine/pcb.h /usr/include/machine/segments.h \
+  /usr/include/machine/tss.h /usr/include/machine/sysarch.h \
+  /usr/include/x86/sysarch.h /usr/include/machine/frame.h \
+  /usr/include/sys/aio.h /usr/include/sys/rwlock.h \
+  /usr/include/machine/rwlock.h /usr/include/x86/rwlock.h \
+  /usr/include/sys/mqueue.h /usr/include/sys/lwp.h \
+  /usr/include/sys/callout.h /usr/include/sys/signalvar.h \
+  /usr/include/sys/sched.h /usr/include/sys/specificdata.h \
+  /usr/include/sys/syncobj.h /usr/include/sys/resource.h \
+  /usr/include/sys/event.h /usr/include/sys/ioctl.h \
+  /usr/include/sys/ttycom.h /usr/include/sys/ioccom.h \
+  /usr/include/sys/dkio.h /usr/include/prop/plistref.h \
+  /usr/include/sys/filio.h /usr/include/sys/sockio.h \
+  /usr/include/net/if_dl.h ldp_peer.h tlv.h pdu.h fsm.h ldp_errors.h \
+  mpls_interface.h
