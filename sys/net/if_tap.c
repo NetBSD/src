c7320633f514f6c8c1180cfed80acf904a4f9c65
riz <riz>
2010-12-09 04:11:39 +0000

    Pull up following revision(s) (requested by christos in ticket #1492): 	sys/net/if_tap.c: revision 1.66 PR/44131: Matthew Mondor: if_tap.c tap_dev_ioctl() not propagating error, always returns 0.

--- sys/net/if_tap.c
+++ sys/net/if_tap.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: if_tap.c,v 1.47.4.7 2009/12/03 09:40:00 sborrill Exp $	*/
+/*	$NetBSD: if_tap.c,v 1.47.4.8 2010/12/09 04:11:39 riz Exp $	*/
 
 /*
  *  Copyright (c) 2003, 2004, 2008, 2009 The NetBSD Foundation.
@@ -33,7 +33,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: if_tap.c,v 1.47.4.7 2009/12/03 09:40:00 sborrill Exp $");
+__KERNEL_RCSID(0, "$NetBSD: if_tap.c,v 1.47.4.8 2010/12/09 04:11:39 riz Exp $");
 
 #if defined(_KERNEL_OPT)
 #include "bpfilter.h"
@@ -1056,12 +1056,10 @@ tap_fops_ioctl(file_t *fp, u_long cmd, void *data)
 static int
 tap_dev_ioctl(int unit, u_long cmd, void *data, struct lwp *l)
 {
-	struct tap_softc *sc =
-	    device_lookup_private(&tap_cd, unit);
-	int error = 0;
+	struct tap_softc *sc = device_lookup_private(&tap_cd, unit);
 
 	if (sc == NULL)
-		return (ENXIO);
+		return ENXIO;
 
 	switch (cmd) {
 	case FIONREAD:
@@ -1078,27 +1076,26 @@ tap_dev_ioctl(int unit, u_long cmd, void *data, struct lwp *l)
 			else
 				*(int *)data = m->m_pkthdr.len;
 			splx(s);
-		} break;
+			return 0;
+		} 
 	case TIOCSPGRP:
 	case FIOSETOWN:
-		error = fsetown(&sc->sc_pgid, cmd, data);
-		break;
+		return fsetown(&sc->sc_pgid, cmd, data);
 	case TIOCGPGRP:
 	case FIOGETOWN:
-		error = fgetown(sc->sc_pgid, cmd, data);
-		break;
+		return fgetown(sc->sc_pgid, cmd, data);
 	case FIOASYNC:
 		if (*(int *)data)
 			sc->sc_flags |= TAP_ASYNCIO;
 		else
 			sc->sc_flags &= ~TAP_ASYNCIO;
-		break;
+		return 0;
 	case FIONBIO:
 		if (*(int *)data)
 			sc->sc_flags |= TAP_NBIO;
 		else
 			sc->sc_flags &= ~TAP_NBIO;
-		break;
+		return 0;
 #ifdef OTAPGIFNAME
 	case OTAPGIFNAME:
 #endif
@@ -1108,13 +1105,11 @@ tap_dev_ioctl(int unit, u_long cmd, void *data, struct lwp *l)
 			struct ifnet *ifp = &sc->sc_ec.ec_if;
 
 			strlcpy(ifr->ifr_name, ifp->if_xname, IFNAMSIZ);
-		} break;
+			return 0;
+		}
 	default:
-		error = ENOTTY;
-		break;
+		return ENOTTY;
 	}
-
-	return (0);
 }
 
 static int
