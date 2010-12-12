e0dabe87afb0f89b0f97b838dafe08a04faa1750
adam <adam@NetBSD.org>
2010-12-12 16:10:42 +0000

    Update to OpenLDAP 2.4.23

--- external/bsd/openldap/include/ldap_config.h
+++ external/bsd/openldap/include/ldap_config.h
@@ -1,8 +1,8 @@
-/* Generated from /usr/src/external/bsd/openldap/dist/include/ldap_config.hin */
-/* OpenLDAP: pkg/ldap/include/ldap_config.hin,v 1.3.2.4 2009/01/22 00:00:52 kurt Exp */
+/* Generated from /dist/src/external/bsd/openldap/dist/include/ldap_config.hin */
+/* OpenLDAP: pkg/ldap/include/ldap_config.hin,v 1.3.2.5 2010/04/13 20:22:48 kurt Exp */
 /* This work is part of OpenLDAP Software <http://www.openldap.org/>.
  *
- * Copyright 1998-2009 The OpenLDAP Foundation.
+ * Copyright 1998-2010 The OpenLDAP Foundation.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
