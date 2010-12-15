03f49af59c8ff0292254561bd930107c6a15c563
matt <matt@NetBSD.org>
2010-12-15 00:06:47 +0000

    make this compile again.

--- sys/arch/mips/atheros/ar5312.c
+++ sys/arch/mips/atheros/ar5312.c
@@ -1,4 +1,4 @@
-/* $NetBSD: ar5312.c,v 1.6 2010/01/22 08:56:05 martin Exp $ */
+/* $NetBSD: ar5312.c,v 1.7 2010/12/15 00:06:47 matt Exp $ */
 
 /*
  * Copyright (c) 2006 Urbana-Champaign Independent Media Center.
@@ -52,6 +52,7 @@
 #include "opt_memsize.h"
 #include <sys/param.h>
 #include <sys/systm.h>
+#include <sys/device.h>
 #include <sys/kernel.h>
 #include <sys/buf.h>
 
