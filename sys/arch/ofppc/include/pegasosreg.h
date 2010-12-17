e05e6028084ef665ade5674a013216fd1f73272b
phx <phx@NetBSD.org>
2010-12-17 19:18:49 +0000

    Set the GPIO to enable AGP configuration space access when attaching gtpci0 and do not reset it again. This keeps AGP devices visible after configure(). Improved the match-function to look for a Discovery controller on the PCI bus.

--- sys/arch/ofppc/include/pegasosreg.h
+++ sys/arch/ofppc/include/pegasosreg.h
@@ -1,9 +1,14 @@
-/* $NetBSD: pegasosreg.h,v 1.2 2010/06/09 04:41:42 kiyohara Exp $ */
+/* $NetBSD: pegasosreg.h,v 1.3 2010/12/17 19:18:49 phx Exp $ */
 
 #ifndef _MACHINE_PEGASOSREG_H_
 #define _MACHINE_PEGASOSREG_H_
 
+/* Marvell Discovery System Controller */
 #define PEGASOS2_GT_REGBASE		0xf1000000
+
+/* GPIO to control AGP configuration space access */
+#define PEGASOS2_AGP_CONF_ENABLE	(1 << 23)
+
 #define PEGASOS2_SRAM_BASE		0xf2000000
 #define PEGASOS2_SRAM_SIZE		0x40000
 
