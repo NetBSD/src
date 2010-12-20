3d6e99caf2f1fdde99c0410d792b616486a40313
joerg <joerg@NetBSD.org>
2010-12-20 21:11:24 +0000

    Consistently use .gnu.warning with .pushsectio and .popsection on all architectures instead of obsolete STABS frames for linker warnings.

--- sys/arch/sh3/include/asm.h
+++ sys/arch/sh3/include/asm.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: asm.h,v 1.26 2008/09/19 03:02:35 uwe Exp $	*/
+/*	$NetBSD: asm.h,v 1.27 2010/12/20 21:11:25 joerg Exp $	*/
 
 /*-
  * Copyright (c) 1990 The Regents of the University of California.
@@ -227,7 +227,16 @@
 	.globl _C_LABEL(alias);						\
 	_C_LABEL(alias) = _C_LABEL(sym)
 
-#define	WARN_REFERENCES(_sym,_msg)				\
-	.section .gnu.warning._sym; .ascii _msg; .previous
+#ifdef __STDC__
+#define	WARN_REFERENCES(sym,msg)					\
+	.pushsection .gnu.warning. ## sym;				\
+	.ascii msg;							\
+	.popsection
+#else
+#define	WARN_REFERENCES(sym,msg)					\
+	.pushsection .gnu.warning./**/sym;				\
+	.ascii msg;							\
+	.popsection
+#endif /* __STDC__ */
 
 #endif /* !_SH3_ASM_H_ */
