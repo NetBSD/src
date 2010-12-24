7a17d19804d386de8364e93bbefc265578ce7d6e
jakllsch <jakllsch@NetBSD.org>
2010-12-24 20:40:42 +0000

    Sprinkle daddr_t.

--- sys/arch/i386/stand/boot/devopen.h
+++ sys/arch/i386/stand/boot/devopen.h
@@ -1,5 +1,5 @@
-/*	$NetBSD: devopen.h,v 1.3 2005/12/11 12:17:47 christos Exp $	*/
+/*	$NetBSD: devopen.h,v 1.4 2010/12/24 20:40:42 jakllsch Exp $	*/
 
 extern int boot_biosdev;
 
-void bios2dev(int, u_int, char **, int *, int *);
+void bios2dev(int, daddr_t, char **, int *, int *);
