dfcbe3e122ca80cd5c7a951fe2860ecd14d0b691
christos <christos@NetBSD.org>
2010-12-25 23:43:30 +0000

    defs.h already includes nbtool_config.h, so don't do it twice.

--- external/bsd/byacc/dist/verbose.c
+++ external/bsd/byacc/dist/verbose.c
@@ -1,14 +1,10 @@
-/*	$NetBSD: verbose.c,v 1.4 2010/12/25 19:24:28 joerg Exp $	*/
+/*	$NetBSD: verbose.c,v 1.5 2010/12/25 23:43:30 christos Exp $	*/
 /* Id: verbose.c,v 1.9 2010/06/09 08:58:29 tom Exp */
 
-#if HAVE_NBTOOL_CONFIG_H
-#include "nbtool_config.h"
-#endif
+#include "defs.h"
 
 #include <sys/cdefs.h>
-__RCSID("$NetBSD: verbose.c,v 1.4 2010/12/25 19:24:28 joerg Exp $");
-
-#include "defs.h"
+__RCSID("$NetBSD: verbose.c,v 1.5 2010/12/25 23:43:30 christos Exp $");
 
 static void log_conflicts(void);
 static void log_unused(void);
