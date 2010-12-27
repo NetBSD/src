84f6bceff2b60037e26beb370b6568ad286529f5
tsutsui <tsutsui@NetBSD.org>
2010-12-27 15:39:07 +0000

    Fix typo (and pasted lines). From OpenBSD.

--- sys/arch/x68k/x68k/locore.s
+++ sys/arch/x68k/x68k/locore.s
@@ -1,4 +1,4 @@
-/*	$NetBSD: locore.s,v 1.99 2010/12/20 00:25:46 matt Exp $	*/
+/*	$NetBSD: locore.s,v 1.100 2010/12/27 15:39:07 tsutsui Exp $	*/
 
 /*
  * Copyright (c) 1980, 1990, 1993
@@ -1122,7 +1122,7 @@ Lspldone:
 /*
  * _delay(u_int N)
  *
- * Delay for at least (N/256) microsecends.
+ * Delay for at least (N/256) microseconds.
  * This routine depends on the variable:  delay_divisor
  * which should be set based on the CPU clock rate.
  */
