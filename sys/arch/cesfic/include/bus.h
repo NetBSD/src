6eb063d6b162ae197e37d3015140e908bf8e8104
tsutsui <tsutsui@NetBSD.org>
2011-01-02 08:22:35 +0000

    Add a dummy <machine/bus.h> which nows seems to be mandatory in MI code including <sys/bus.h> even if a port doesn't use it. Taken from sbmips.

---
+++ sys/arch/cesfic/include/bus.h
@@ -0,0 +1,16 @@
+/* $NetBSD: bus.h,v 1.1 2011/01/02 08:22:35 tsutsui Exp $ */
+
+/*
+ * XXX: A dummy <machine/bus.h> for MI <sys/bus.h>.
+ */
+
+#ifndef _MACHINE_BUS_H_
+#define	_MACHINE_BUS_H_
+
+typedef paddr_t	bus_addr_t;
+typedef psize_t	bus_size_t;
+
+typedef int     bus_space_tag_t;
+typedef int     bus_space_handle_t;
+
+#endif /* _MACHINE_BUS_H_ */
