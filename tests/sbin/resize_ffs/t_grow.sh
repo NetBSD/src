e40eddba9b382d4fff1c2060af94fc46c98c5210
riz <riz@NetBSD.org>
2011-01-05 02:25:27 +0000

    Update resize_ffs tests for byteswapped file system support, and for UFS2 growth support.  Also, reduce the number of tests run by default while still maintaining decent coverage of features and block sizes. Anyone working on resize_Ffs should run the tests with RESIZE_FFS_ALL_TESTS set in the environment, which adds a lot more testing.

--- tests/sbin/resize_ffs/t_grow.sh
+++ tests/sbin/resize_ffs/t_grow.sh
@@ -1,4 +1,4 @@
-# $NetBSD: t_grow.sh,v 1.6 2010/12/16 17:50:13 riz Exp $
+# $NetBSD: t_grow.sh,v 1.7 2011/01/05 02:25:27 riz Exp $
 #
 # Copyright (c) 2010 The NetBSD Foundation, Inc.
 # All rights reserved.
@@ -35,10 +35,37 @@
 # determined manually based on the maximum number that will fit in the
 # created fs.  'level' is the fs-level (-O 0,1,2) passed to newfs.
 # If 'swap' is included, byteswap the fs
+
+# v0 == newfs -O0  (4.3BSD layout, ffsv1 superblock)
+test_case grow_16M_v0_4096 resize_ffs 4096 512 32768 131072 0 28
+test_case grow_16M_v0_8192 resize_ffs 8192 1024 32768 131072 0 28
+test_case grow_16M_v0_16384 resize_ffs 16384 2048 32768 131072 0 29
+test_case grow_16M_v0_32768 resize_ffs 32768 4096 32768 131072 0 28
+test_case grow_16M_v0_65536 resize_ffs 65536 8192 32768 131072 0 26
+# these grow_24M grow a smaller amount; sometimes there's different issues
+test_case grow_24M_v0_4096 resize_ffs 4096 512 49152 65536 0 41
+test_case grow_24M_v0_8192 resize_ffs 8192 1024 49152 65536 0 42
+test_case grow_24M_v0_16384 resize_ffs 16384 2048 49152 65536 0 43
+test_case grow_24M_v0_32768 resize_ffs 32768 4096 49152 65536 0 42
+test_case grow_24M_v0_65536 resize_ffs 65536 8192 49152 65536 0 38
+test_case grow_32M_v0_4096 resize_ffs 4096 512 65536 131072 0 55
+test_case grow_32M_v0_8192 resize_ffs 8192 1024 65536 131072 0 56
+test_case grow_32M_v0_16384 resize_ffs 16384 2048 65536 131072 0 58
+test_case grow_32M_v0_32768 resize_ffs 32768 4096 65536 131072 0 56
+test_case grow_32M_v0_65536 resize_ffs 65536 8192 65536 131072 0 51
+test_case grow_48M_v0_4096 resize_ffs 4096 512 98304 131072 0 82
+test_case grow_48M_v0_8192 resize_ffs 8192 1024 98304 131072 0 84
+test_case grow_48M_v0_16384 resize_ffs 16384 2048 98304 131072 0 87
+test_case grow_48M_v0_32768 resize_ffs 32768 4096 98304 131072 0 83
+test_case grow_48M_v0_65536 resize_ffs 65536 8192 98304 131072 0 76
+test_case grow_64M_v0_4096 resize_ffs 4096 512 131072 262144 0 109
+test_case grow_64M_v0_8192 resize_ffs 8192 1024 131072 262144 0 111
+test_case grow_64M_v0_16384 resize_ffs 16384 2048 131072 262144 0 115
+test_case grow_64M_v0_32768 resize_ffs 32768 4096 131072 262144 0 111
+test_case grow_64M_v0_65536 resize_ffs 65536 8192 131072 262144 0 101
+
+# v1 == newfs -O1 (FFSv1, v2 superblock layout)
 test_case grow_16M_v1_4096 resize_ffs 4096 512 32768 131072 1 28
-test_case_xfail grow_16M_v1_4096_swapped "PR bin/44203" resize_ffs 4096 512 32768 131072 1 28 swap
-test_case_xfail grow_16M_v2_4096 "PR bin/44205" resize_ffs 4096 512 32768 131072 2 28
-test_case_xfail grow_16M_v2_4096_swapped "PR bin/44203, PR bin/44205" resize_ffs 4096 512 32768 131072 2 28 swap
 test_case grow_16M_v1_8192 resize_ffs 8192 1024 32768 131072 1 28
 test_case grow_16M_v1_16384 resize_ffs 16384 2048 32768 131072 1 29
 test_case grow_16M_v1_32768 resize_ffs 32768 4096 32768 131072 1 28
@@ -64,9 +91,31 @@ test_case grow_64M_v1_8192 resize_ffs 8192 1024 131072 262144 1 111
 test_case grow_64M_v1_16384 resize_ffs 16384 2048 131072 262144 1 115
 test_case grow_64M_v1_32768 resize_ffs 32768 4096 131072 262144 1 111
 test_case grow_64M_v1_65536 resize_ffs 65536 8192 131072 262144 1 101
-test_case_xfail grow_64M_v1_65536_swapped "PR bin/44203" resize_ffs 65536 8192 131072 262144 1 101 swap
-test_case_xfail grow_64M_v2_65536 "PR bin/44205" resize_ffs 65536 8192 131072 262144 2 101
-test_case_xfail grow_64M_v2_65536_swapped "PR bin/44203, PR bin/44205" resize_ffs 65536 8192 131072 262144 2 101 swap
+test_case grow_16M_v2_4096 resize_ffs 4096 512 32768 131072 2 26
+test_case grow_16M_v2_8192 resize_ffs 8192 1024 32768 131072 2 28
+test_case grow_16M_v2_16384 resize_ffs 16384 2048 32768 131072 2 29
+test_case grow_16M_v2_32768 resize_ffs 32768 4096 32768 131072 2 28
+test_case grow_16M_v2_65536 resize_ffs 65536 8192 32768 131072 2 25
+test_case grow_24M_v2_4096 resize_ffs 4096 512 49152 65536 2 41
+test_case grow_24M_v2_8192 resize_ffs 8192 1024 49152 65536 2 42
+test_case grow_24M_v2_16384 resize_ffs 16384 2048 49152 65536 2 43
+test_case grow_24M_v2_32768 resize_ffs 32768 4096 49152 65536 2 42
+test_case grow_24M_v2_65536 resize_ffs 65536 8192 49152 65536 2 38
+test_case grow_32M_v2_4096 resize_ffs 4096 512 65536 131072 2 55
+test_case grow_32M_v2_8192 resize_ffs 8192 1024 65536 131072 2 56
+test_case grow_32M_v2_16384 resize_ffs 16384 2048 65536 131072 2 58
+test_case grow_32M_v2_32768 resize_ffs 32768 4096 65536 131072 2 56
+test_case grow_32M_v2_65536 resize_ffs 65536 8192 65536 131072 2 51
+test_case grow_48M_v2_4096 resize_ffs 4096 512 98304 131072 2 82
+test_case grow_48M_v2_8192 resize_ffs 8192 1024 98304 131072 2 84
+test_case grow_48M_v2_16384 resize_ffs 16384 2048 98304 131072 2 87
+test_case grow_48M_v2_32768 resize_ffs 32768 4096 98304 131072 2 83
+test_case grow_48M_v2_65536 resize_ffs 65536 8192 98304 131072 2 76
+test_case grow_64M_v2_4096 resize_ffs 4096 512 131072 262144 2 109
+test_case grow_64M_v2_8192 resize_ffs 8192 1024 131072 262144 2 111
+test_case grow_64M_v2_16384 resize_ffs 16384 2048 131072 262144 2 115
+test_case grow_64M_v2_32768 resize_ffs 32768 4096 131072 262144 2 111
+test_case grow_64M_v2_65536 resize_ffs 65536 8192 131072 262144 2 101
 
 atf_test_case grow_ffsv1_partial_cg
 grow_ffsv1_partial_cg_head()
@@ -82,22 +131,50 @@ grow_ffsv1_partial_cg_body()
 	atf_check -o ignore -e ignore newfs -V1 -s 4000 -F ${IMG}
 
 	# size to grow to is chosen to cause partial cg
-	atf_check -s exit:0 resize_ffs -y -s 5760 ${IMG}
+	atf_check -s exit:0 -o ignore resize_ffs -y -s 5760 ${IMG}
 	atf_check -s exit:0 -o ignore fsck_ffs -f -n -F ${IMG}
 }
 
 atf_init_test_cases()
 {
 	setupvars
-	atf_add_test_case grow_16M_v1_4096
-	atf_add_test_case grow_16M_v1_4096_swapped
-	atf_add_test_case grow_16M_v2_4096
-	atf_add_test_case grow_16M_v2_4096_swapped
-	atf_add_test_case grow_16M_v1_8192
+	atf_add_test_case grow_16M_v0_8192
 	atf_add_test_case grow_16M_v1_16384
+	atf_add_test_case grow_16M_v2_32768
+if [ "${RESIZE_FFS_ALL_TESTS-X}" != "X" ]; then
+	atf_add_test_case grow_16M_v0_4096
+	atf_add_test_case grow_16M_v0_16384
+	atf_add_test_case grow_16M_v0_32768
+	atf_add_test_case grow_16M_v0_65536
+	atf_add_test_case grow_16M_v1_4096
+	atf_add_test_case grow_16M_v1_8192
 	atf_add_test_case grow_16M_v1_32768
 	atf_add_test_case grow_16M_v1_65536
-if [ "${RESIZE_FFS_ALL_TESTS-X}" != "X" ]; then
+	atf_add_test_case grow_16M_v2_4096
+	atf_add_test_case grow_16M_v2_8192
+	atf_add_test_case grow_16M_v2_16384
+	atf_add_test_case grow_16M_v2_65536
+	atf_add_test_case grow_24M_v0_4096
+	atf_add_test_case grow_24M_v0_8192
+	atf_add_test_case grow_24M_v0_16384
+	atf_add_test_case grow_24M_v0_32768
+	atf_add_test_case grow_24M_v0_65536
+	atf_add_test_case grow_32M_v0_4096
+	atf_add_test_case grow_32M_v0_8192
+	atf_add_test_case grow_32M_v0_16384
+	atf_add_test_case grow_32M_v0_32768
+	atf_add_test_case grow_32M_v0_65536
+	atf_add_test_case grow_48M_v0_4096
+	atf_add_test_case grow_48M_v0_8192
+	atf_add_test_case grow_48M_v0_16384
+	atf_add_test_case grow_48M_v0_32768
+	atf_add_test_case grow_48M_v0_65536
+	atf_add_test_case grow_64M_v0_4096
+	atf_add_test_case grow_64M_v0_8192
+	atf_add_test_case grow_64M_v0_16384
+	atf_add_test_case grow_64M_v0_32768
+	atf_add_test_case grow_64M_v0_65536
+
 	atf_add_test_case grow_24M_v1_4096
 	atf_add_test_case grow_24M_v1_8192
 	atf_add_test_case grow_24M_v1_16384
@@ -118,9 +195,27 @@ if [ "${RESIZE_FFS_ALL_TESTS-X}" != "X" ]; then
 	atf_add_test_case grow_64M_v1_16384
 	atf_add_test_case grow_64M_v1_32768
 	atf_add_test_case grow_64M_v1_65536
-	atf_add_test_case grow_64M_v1_65536_swapped
+
+	atf_add_test_case grow_24M_v2_4096
+	atf_add_test_case grow_24M_v2_8192
+	atf_add_test_case grow_24M_v2_16384
+	atf_add_test_case grow_24M_v2_32768
+	atf_add_test_case grow_24M_v2_65536
+	atf_add_test_case grow_32M_v2_4096
+	atf_add_test_case grow_32M_v2_8192
+	atf_add_test_case grow_32M_v2_16384
+	atf_add_test_case grow_32M_v2_32768
+	atf_add_test_case grow_32M_v2_65536
+	atf_add_test_case grow_48M_v2_4096
+	atf_add_test_case grow_48M_v2_8192
+	atf_add_test_case grow_48M_v2_16384
+	atf_add_test_case grow_48M_v2_32768
+	atf_add_test_case grow_48M_v2_65536
+	atf_add_test_case grow_64M_v2_4096
+	atf_add_test_case grow_64M_v2_8192
+	atf_add_test_case grow_64M_v2_16384
+	atf_add_test_case grow_64M_v2_32768
 	atf_add_test_case grow_64M_v2_65536
-	atf_add_test_case grow_64M_v2_65536_swapped
 fi
 	atf_add_test_case grow_ffsv1_partial_cg
 }
