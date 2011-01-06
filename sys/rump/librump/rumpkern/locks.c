8ebe9b53a043236cdb90360043fcbf9953496fcf
pooka <pooka@NetBSD.org>
2011-01-06 11:22:54 +0000

    Support LOCKDEBUG.  To use it, compile sys/rump with RUMP_LOCKDEBUG=yes.
    requested by martin (sparc64 gdb cannot reliably produce a stack trace)

--- sys/rump/librump/rumpkern/locks.c
+++ sys/rump/librump/rumpkern/locks.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: locks.c,v 1.44 2010/12/01 17:22:51 pooka Exp $	*/
+/*	$NetBSD: locks.c,v 1.45 2011/01/06 11:22:55 pooka Exp $	*/
 
 /*
  * Copyright (c) 2007, 2008 Antti Kantee.  All Rights Reserved.
@@ -29,7 +29,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: locks.c,v 1.44 2010/12/01 17:22:51 pooka Exp $");
+__KERNEL_RCSID(0, "$NetBSD: locks.c,v 1.45 2011/01/06 11:22:55 pooka Exp $");
 
 #include <sys/param.h>
 #include <sys/kmem.h>
@@ -39,6 +39,42 @@ __KERNEL_RCSID(0, "$NetBSD: locks.c,v 1.44 2010/12/01 17:22:51 pooka Exp $");
 #include <rump/rumpuser.h>
 
 #include "rump_private.h"
+
+/*
+ * Simple lockdebug.  If it's compiled in, it's always active.
+ * Currently available only for mtx/rwlock.
+ */
+#ifdef LOCKDEBUG
+#include <sys/lockdebug.h>
+
+static lockops_t mutex_lockops = {
+	"mutex",
+	LOCKOPS_SLEEP,
+	NULL
+};
+static lockops_t rw_lockops = {
+	"mutex",
+	LOCKOPS_SLEEP,
+	NULL
+};
+
+#define ALLOCK(lock, ops)		\
+    lockdebug_alloc(lock, ops, (uintptr_t)__builtin_return_address(0))
+#define FREELOCK(lock)			\
+    lockdebug_free(lock)
+#define WANTLOCK(lock, shar, try)	\
+    lockdebug_wantlock(lock, (uintptr_t)__builtin_return_address(0), shar, try)
+#define LOCKED(lock, shar)		\
+    lockdebug_locked(lock, NULL, (uintptr_t)__builtin_return_address(0), shar)
+#define UNLOCKED(lock, shar)		\
+    lockdebug_unlocked(lock, (uintptr_t)__builtin_return_address(0), shar)
+#else
+#define ALLOCK(a, b)
+#define FREELOCK(a)
+#define WANTLOCK(a, b, c)
+#define LOCKED(a, b)
+#define UNLOCKED(a, b)
+#endif
 
 /*
  * We map locks to pthread routines.  The difference between kernel
@@ -61,12 +97,14 @@ mutex_init(kmutex_t *mtx, kmutex_type_t type, int ipl)
 	CTASSERT(sizeof(kmutex_t) >= sizeof(void *));
 
 	rumpuser_mutex_init_kmutex((struct rumpuser_mtx **)mtx);
+	ALLOCK(mtx, &mutex_lockops);
 }
 
 void
 mutex_destroy(kmutex_t *mtx)
 {
 
+	FREELOCK(mtx);
 	rumpuser_mutex_destroy(RUMPMTX(mtx));
 }
 
@@ -74,36 +112,33 @@ void
 mutex_enter(kmutex_t *mtx)
 {
 
+	WANTLOCK(mtx, false, false);
 	rumpuser_mutex_enter(RUMPMTX(mtx));
+	LOCKED(mtx, false);
 }
-
-void
-mutex_spin_enter(kmutex_t *mtx)
-{
-
-	mutex_enter(mtx);
-}
+__strong_alias(mutex_spin_enter,mutex_enter);
 
 int
 mutex_tryenter(kmutex_t *mtx)
 {
+	int rv;
 
-	return rumpuser_mutex_tryenter(RUMPMTX(mtx));
+	rv = rumpuser_mutex_tryenter(RUMPMTX(mtx));
+	if (rv) {
+		WANTLOCK(mtx, false, true);
+		LOCKED(mtx, false);
+	}
+	return rv;
 }
 
 void
 mutex_exit(kmutex_t *mtx)
 {
 
+	UNLOCKED(mtx, false);
 	rumpuser_mutex_exit(RUMPMTX(mtx));
 }
-
-void
-mutex_spin_exit(kmutex_t *mtx)
-{
-
-	mutex_exit(mtx);
-}
+__strong_alias(mutex_spin_exit,mutex_exit);
 
 int
 mutex_owned(kmutex_t *mtx)
@@ -130,12 +165,14 @@ rw_init(krwlock_t *rw)
 	CTASSERT(sizeof(krwlock_t) >= sizeof(void *));
 
 	rumpuser_rw_init((struct rumpuser_rw **)rw);
+	ALLOCK(rw, &rw_lockops);
 }
 
 void
 rw_destroy(krwlock_t *rw)
 {
 
+	FREELOCK(rw);
 	rumpuser_rw_destroy(RUMPRW(rw));
 }
 
@@ -143,20 +180,36 @@ void
 rw_enter(krwlock_t *rw, const krw_t op)
 {
 
+
+	WANTLOCK(rw, op == RW_READER, false);
 	rumpuser_rw_enter(RUMPRW(rw), op == RW_WRITER);
+	LOCKED(rw, op == RW_READER);
 }
 
 int
 rw_tryenter(krwlock_t *rw, const krw_t op)
 {
+	int rv;
 
-	return rumpuser_rw_tryenter(RUMPRW(rw), op == RW_WRITER);
+	rv = rumpuser_rw_tryenter(RUMPRW(rw), op == RW_WRITER);
+	if (rv) {
+		WANTLOCK(rw, op == RW_READER, true);
+		LOCKED(rw, op == RW_READER);
+	}
+	return rv;
 }
 
 void
 rw_exit(krwlock_t *rw)
 {
 
+#ifdef LOCKDEBUG
+	bool shared = !rw_write_held(rw);
+
+	if (shared)
+		KASSERT(rw_read_held(rw));
+	UNLOCKED(rw, shared);
+#endif
 	rumpuser_rw_exit(RUMPRW(rw));
 }
 
@@ -215,7 +268,9 @@ cv_wait(kcondvar_t *cv, kmutex_t *mtx)
 
 	if (__predict_false(rump_threads == 0))
 		panic("cv_wait without threads");
+	UNLOCKED(mtx, false);
 	rumpuser_cv_wait(RUMPCV(cv), RUMPMTX(mtx));
+	LOCKED(mtx, false);
 }
 
 int
@@ -224,7 +279,9 @@ cv_wait_sig(kcondvar_t *cv, kmutex_t *mtx)
 
 	if (__predict_false(rump_threads == 0))
 		panic("cv_wait without threads");
+	UNLOCKED(mtx, false);
 	rumpuser_cv_wait(RUMPCV(cv), RUMPMTX(mtx));
+	LOCKED(mtx, false);
 	return 0;
 }
 
@@ -233,10 +290,11 @@ cv_timedwait(kcondvar_t *cv, kmutex_t *mtx, int ticks)
 {
 	struct timespec ts, tick;
 	extern int hz;
+	int rv;
 
 	if (ticks == 0) {
 		cv_wait(cv, mtx);
-		return 0;
+		rv = 0;
 	} else {
 		/*
 		 * XXX: this fetches rump kernel time, but
@@ -247,20 +305,18 @@ cv_timedwait(kcondvar_t *cv, kmutex_t *mtx, int ticks)
 		tick.tv_nsec = (ticks % hz) * (1000000000/hz);
 		timespecadd(&ts, &tick, &ts);
 
+		UNLOCKED(mtx, false);
 		if (rumpuser_cv_timedwait(RUMPCV(cv), RUMPMTX(mtx),
 		    ts.tv_sec, ts.tv_nsec))
-			return EWOULDBLOCK;
+			rv = EWOULDBLOCK;
 		else
-			return 0;
+			rv = 0;
+		LOCKED(mtx, false);
 	}
-}
 
-int
-cv_timedwait_sig(kcondvar_t *cv, kmutex_t *mtx, int ticks)
-{
-
-	return cv_timedwait(cv, mtx, ticks);
+	return rv;
 }
+__strong_alias(cv_timedwait_sig,cv_timedwait);
 
 void
 cv_signal(kcondvar_t *cv)
