ec9aaf31bd5eb8935305def0edbb28ed7a863d2e
apb <apb@NetBSD.org>
2011-01-06 21:39:01 +0000

    Change printf formats to match the data type of the values being printed.  There's now a lot of PRIu16 and PRIu32, some PRIu8, some SCNu32, and a few cases where %u and %d were reversed.  Multiplication of 32-bit and 8-bit values is cast to uint64_t and printed with PRIu64.
    Inspired by a report from Patrick Welche on current-users.

--- sbin/disklabel/printlabel.c
+++ sbin/disklabel/printlabel.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: printlabel.c,v 1.15 2009/01/18 21:15:14 apb Exp $	*/
+/*	$NetBSD: printlabel.c,v 1.16 2011/01/06 21:39:01 apb Exp $	*/
 
 /*
  * Copyright (c) 1987, 1993
@@ -38,7 +38,7 @@
 
 #include <sys/cdefs.h>
 #ifndef lint
-__RCSID("$NetBSD: printlabel.c,v 1.15 2009/01/18 21:15:14 apb Exp $");
+__RCSID("$NetBSD: printlabel.c,v 1.16 2011/01/06 21:39:01 apb Exp $");
 #endif	/* not lint */
 
 #include <sys/param.h>
@@ -65,7 +65,7 @@ showinfo(FILE *f, struct disklabel *lp, const char *specialname)
 	if ((unsigned) lp->d_type < DKMAXTYPES)
 		(void)fprintf(f, "type: %s\n", dktypenames[lp->d_type]);
 	else
-		(void)fprintf(f, "type: %d\n", lp->d_type);
+		(void)fprintf(f, "type: %" PRIu16 "\n", lp->d_type);
 	(void)fprintf(f, "disk: %.*s\n", (int) sizeof(lp->d_typename),
 	    lp->d_typename);
 	(void)fprintf(f, "label: %.*s\n", (int) sizeof(lp->d_packname),
@@ -99,7 +99,7 @@ showinfo(FILE *f, struct disklabel *lp, const char *specialname)
 	if (i < 0)
 		i = 0;
 	for (j = 0; j <= i; j++)
-		(void)fprintf(f, "%d ", lp->d_drivedata[j]);
+		(void)fprintf(f, "%" PRIu32 " ", lp->d_drivedata[j]);
 	(void)fprintf(f, "\n\n");
 	(void)fflush(f);
 }
@@ -114,12 +114,14 @@ showpartition(FILE *f, struct disklabel *lp, int i, int ctsformat)
 	if (ctsformat && lp->d_secpercyl && lp->d_nsectors) {
 		char sbuf[64], obuf[64];
 
-		(void)snprintf(sbuf, sizeof(sbuf), "%u/%u/%u",
+		(void)snprintf(sbuf, sizeof(sbuf),
+                    "%" PRIu32 "/%" PRIu32 "/%" PRIu32,
 		    pp->p_size/lp->d_secpercyl,
 		    (pp->p_size%lp->d_secpercyl) / lp->d_nsectors,
 		    pp->p_size%lp->d_nsectors);
 
-		(void)snprintf(obuf, sizeof(obuf), "%u/%u/%u",
+		(void)snprintf(obuf, sizeof(obuf),
+                    "%" PRIu32 "/%" PRIu32 "/%" PRIu32,
 		    pp->p_offset/lp->d_secpercyl,
 		    (pp->p_offset%lp->d_secpercyl) / lp->d_nsectors,
 		    pp->p_offset%lp->d_nsectors);
@@ -127,40 +129,41 @@ showpartition(FILE *f, struct disklabel *lp, int i, int ctsformat)
 		(void)fprintf(f, " %c: %9s %9s ",
 		    'a' + i, sbuf, obuf);
 	} else {
-		(void)fprintf(f, " %c: %9u %9u ", 'a' + i,
-		    pp->p_size, pp->p_offset);
+		(void)fprintf(f, " %c: %9" PRIu32 " %9" PRIu32 " ",
+		    'a' + i, pp->p_size, pp->p_offset);
 	}
 
 	if ((unsigned) pp->p_fstype < FSMAXTYPES)
 		(void)fprintf(f, "%10.10s", fstypenames[pp->p_fstype]);
 	else
-		(void)fprintf(f, "%10d", pp->p_fstype);
+		(void)fprintf(f, "%10" PRIu8, pp->p_fstype);
 
 	switch (pp->p_fstype) {
 	case FS_UNUSED:				/* XXX */
-		(void)fprintf(f, "  %5u %5u %5.5s ",
-		    pp->p_fsize, pp->p_fsize * pp->p_frag, "");
+		(void)fprintf(f, "  %5" PRIu32 " %5" PRIu64 " %5.5s ",
+		    pp->p_fsize, (uint64_t)pp->p_fsize * pp->p_frag, "");
 		break;
 
 	case FS_BSDFFS:
 	case FS_ADOS:
 	case FS_APPLEUFS:
-		(void)fprintf(f, "  %5u %5u %5u ",
-		    pp->p_fsize, pp->p_fsize * pp->p_frag, pp->p_cpg);
+		(void)fprintf(f, "  %5" PRIu32 " %5" PRIu64 " %5" PRIu16 " ",
+		    pp->p_fsize, (uint64_t)pp->p_fsize * pp->p_frag, pp->p_cpg);
 		break;
 
 	case FS_BSDLFS:
-		(void)fprintf(f, "  %5u %5u %5u ",
-		    pp->p_fsize, pp->p_fsize * pp->p_frag, pp->p_sgs);
+		(void)fprintf(f, "  %5" PRIu32 " %5" PRIu64 " %5" PRIu16 " ",
+		    pp->p_fsize, (uint64_t)pp->p_fsize * pp->p_frag, pp->p_sgs);
 		break;
 
 	case FS_EX2FS:
-		(void)fprintf(f, "  %5u %5u       ",
-		    pp->p_fsize, pp->p_fsize * pp->p_frag);
+		(void)fprintf(f, "  %5" PRIu32 " %5" PRIu64 "       ",
+		    pp->p_fsize, (uint64_t)pp->p_fsize * pp->p_frag);
 		break;
 
 	case FS_ISO9660:
-		(void)fprintf(f, "  %6u            ", pp->p_cdsession);
+		(void)fprintf(f, "  %6" PRIu32 "            ",
+		    pp->p_cdsession);
 		break;
 
 	default:
@@ -168,7 +171,7 @@ showpartition(FILE *f, struct disklabel *lp, int i, int ctsformat)
 		break;
 	}
 	if (lp->d_secpercyl != 0) {
-		(void)fprintf(f, " # (Cyl. %6u",
+		(void)fprintf(f, " # (Cyl. %6" PRIu32,
 		    pp->p_offset / lp->d_secpercyl);
 
 		if (pp->p_offset > lp->d_secperunit)
@@ -178,7 +181,7 @@ showpartition(FILE *f, struct disklabel *lp, int i, int ctsformat)
 		else
 		    putc(' ', f);
 
-		(void)fprintf(f, "- %6u",
+		(void)fprintf(f, "- %6" PRIu32,
 		    (pp->p_offset + 
 		    pp->p_size + lp->d_secpercyl - 1) /
 		    lp->d_secpercyl - 1);
@@ -198,7 +201,7 @@ showpartitions(FILE *f, struct disklabel *lp, int ctsformat)
 {
 	int	i;
 
-	(void)fprintf(f, "%d partitions:\n", lp->d_npartitions);
+	(void)fprintf(f, "%" PRIu16 " partitions:\n", lp->d_npartitions);
 	(void)fprintf(f,
 	    "#        size    offset     fstype [fsize bsize cpg/sgs]\n");
 
