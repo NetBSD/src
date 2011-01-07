afa3d6e81a9bfa117a2cb6bc7959e7f8a8ccd2cc
riz <riz@NetBSD.org>
2011-01-07 18:25:21 +0000

    Clean up for ticket #1499 - doubled content, not sure how it happened.

--- compat/compatsubdir.mk
+++ compat/compatsubdir.mk
@@ -1,42 +1,4 @@
-#	$NetBSD: compatsubdir.mk,v 1.5.2.2 2011/01/06 05:19:55 riz Exp $
-
-# Build netbsd libraries.
-
-.include <bsd.own.mk>
-
-.if ${MKCOMPAT} != "no"
-.if !make(includes)
-
-# make sure we get an objdir built early enough
-.include <bsd.prog.mk>
-
-# XXX make this use MAKEOBJDIR
-MAKEDIRTARGETENV=	MAKEOBJDIRPREFIX=${.OBJDIR} MKOBJDIRS=yes MKSHARE=no BSD_MK_COMPAT_FILE=${BSD_MK_COMPAT_FILE}
-
-# XXX fix the "library" list to include all 'external' libs?
-.if defined(BOOTSTRAP_SUBDIRS)
-SUBDIR=	${BOOTSTRAP_SUBDIRS}
-.else
-SUBDIR= ../../../gnu/lib/crtstuff4 .WAIT \
-	../../../lib/csu .WAIT \
-	../../../gnu/lib/libgcc4 .WAIT \
-	../../../lib/libc .WAIT \
-	../../../lib/libutil .WAIT \
-	../../../lib .WAIT \
-	../../../gnu/lib \
-	../../../libexec/ld.elf_so
-
-.if (${MKLDAP} != "no")
-SUBDIR+= ../../../external/bsd/openldap/lib
-.endif
-
-.endif
-
-.include <bsd.subdir.mk>
-
-.endif
-.endif
-#	$NetBSD: compatsubdir.mk,v 1.5.2.2 2011/01/06 05:19:55 riz Exp $
+#	$NetBSD: compatsubdir.mk,v 1.5.2.3 2011/01/07 18:25:21 riz Exp $
 
 # Build netbsd libraries.
 
