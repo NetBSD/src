24caeb6eab95260ccd245db428b58a62c0a1b7ed
matt <matt>
2011-01-07 03:00:15 +0000

    Hack Alert.  In order to get a compiler that supports soft-float, use a hacked version of configargs.h which initializes configure_default_options woth { { "float", "soft" } } if _SOFT_FLOAT is defined.  In turn, tools/gcc will be configured with "--with-float=soft" if MKSOFTFLOAT=yes which makes the cross build compiler use soft-float by default and define _SOFT_FLOAT which allows the above to happen.

--- gnu/usr.bin/gcc4/arch/powerpc/configargs.h
+++ gnu/usr.bin/gcc4/arch/powerpc/configargs.h
@@ -3,9 +3,18 @@
 /* Generated from: NetBSD: mknative.common,v 1.9 2007/02/05 18:26:01 apb Exp  */
 
 /* Generated automatically. */
+#ifdef _SOFT_FLOAT
+static const char configuration_arguments[] = "/usr/src/tools/gcc/../../gnu/dist/gcc4/configure --enable-long-long --disable-multilib --enable-threads --disable-symvers --build=x86_64-unknown-netbsd4.99.72 --host=powerpc--netbsd --target=powerpc--netbsd --enable-__cxa_atexit --with-float=soft";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "float", "soft" } };
+#else
 static const char configuration_arguments[] = "/usr/src/tools/gcc/../../gnu/dist/gcc4/configure --enable-long-long --disable-multilib --enable-threads --disable-symvers --build=x86_64-unknown-netbsd4.99.72 --host=powerpc--netbsd --target=powerpc--netbsd --enable-__cxa_atexit";
 static const char thread_model[] = "posix";
 
 static const struct {
   const char *name, *value;
 } configure_default_options[] = { { NULL, NULL} };
+#endif
