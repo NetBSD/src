175a424cfe60fefcbd71274ad73d1ff75c2b4c97
matt <matt>
2011-01-07 01:58:43 +0000

    Add booke support.

--- sys/arch/powerpc/include/pmap.h
+++ sys/arch/powerpc/include/pmap.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: pmap.h,v 1.33 2006/08/05 21:26:49 sanjayl Exp $	*/
+/*	$NetBSD: pmap.h,v 1.33.82.1 2011/01/07 01:58:43 matt Exp $	*/
 
 #ifdef _KERNEL_OPT
 #include "opt_ppcarch.h"
@@ -6,6 +6,8 @@
 
 #ifdef PPC_IBM4XX
 #include <powerpc/ibm4xx/pmap.h>
+#elif defined(PPC_BOOKE)
+#include <powerpc/booke/pmap.h>
 #elif defined(PPC_OEA) || defined (PPC_OEA64) || defined (PPC_OEA64_BRIDGE)
 #include <powerpc/oea/pmap.h>
 #else
