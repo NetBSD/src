3646dd3c8629549daabcb0dd4cfc6c88eeec2ea3
joerg <joerg@NetBSD.org>
2011-01-12 23:00:51 +0000

    Use example.style.css in place of style.css. Replace $Mdocdate$ with the content in correct order, so that groff gives proper output.

--- external/bsd/mdocml/prepare-import.sh
+++ external/bsd/mdocml/prepare-import.sh
@@ -3,11 +3,12 @@
 set -e
 
 cd dist
-rm -rf ChangeLog.xsl example.style.css index.css *.sgml
+rm -rf ChangeLog.xsl style.css index.css *.sgml
 
 uuencode external.png < external.png > external.png.uu
 rm external.png
 
 for f in [a-z]*; do
-	sed 's/[$]Id:/\$Vendor-Id:/' < $f > $f.new && mv $f.new $f
+	sed -e 's/[$]Id:/\$Vendor-Id:/' \
+	    -e 's/[$]Mdocdate: \([^$]*\) \([0-9][0-9][0-9][0-9]\) [$]/\1, \2/' < $f > $f.new && mv $f.new $f
 done
