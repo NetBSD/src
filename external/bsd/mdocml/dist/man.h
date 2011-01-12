e4fbeb7e34ba824ccba55977a40aa7103baff983
joerg <joerg@NetBSD.org>
2011-01-12 22:57:08 +0000

    Import mdocml-1.10.9 and additional post-release bugfixes (tbl_data.c 1.15, tbl_layout.c 1.13, tbl_opts.c 1.8).
    This features many small improvements and the initial integration of
    tbl(1) support on all output devices.

--- external/bsd/mdocml/dist/man.h
+++ external/bsd/mdocml/dist/man.h
@@ -1,6 +1,6 @@
-/*	$Vendor-Id: man.h,v 1.41 2010/07/22 23:03:15 kristaps Exp $ */
+/*	$Vendor-Id: man.h,v 1.50 2011/01/01 12:59:17 kristaps Exp $ */
 /*
- * Copyright (c) 2009 Kristaps Dzonsons <kristaps@bsd.lv>
+ * Copyright (c) 2009, 2010 Kristaps Dzonsons <kristaps@bsd.lv>
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -17,8 +17,9 @@
 #ifndef MAN_H
 #define MAN_H
 
-#include <time.h>
-
+/* 
+ * What follows is a list of ALL possible macros. 
+ */
 enum	mant {
 	MAN_br = 0,
 	MAN_TH,
@@ -42,64 +43,71 @@ enum	mant {
 	MAN_IR,
 	MAN_RI,
 	MAN_na,
-	MAN_i,
 	MAN_sp,
 	MAN_nf,
 	MAN_fi,
-	MAN_r,
 	MAN_RE,
 	MAN_RS,
 	MAN_DT,
 	MAN_UC,
 	MAN_PD,
-	MAN_Sp,
-	MAN_Vb,
-	MAN_Ve,
 	MAN_AT,
 	MAN_in,
+	MAN_ft,
 	MAN_MAX
 };
 
+/* 
+ * Type of a syntax node. 
+ */
 enum	man_type {
 	MAN_TEXT,
 	MAN_ELEM,
 	MAN_ROOT,
 	MAN_BLOCK,
 	MAN_HEAD,
-	MAN_BODY
+	MAN_BODY,
+	MAN_TBL
 };
 
+/* 
+ * Information from prologue. 
+ */
 struct	man_meta {
-	char		*msec;
-	time_t		 date;
-	char		*rawdate;
-	char		*vol;
-	char		*title;
-	char		*source;
+	char		*msec; /* `TH' section (1, 3p, etc.) */
+	time_t		 date; /* `TH' normalised date */
+	char		*rawdate; /* raw `TH' date */
+	char		*vol; /* `TH' volume */
+	char		*title; /* `TH' title (e.g., FOO) */
+	char		*source; /* `TH' source (e.g., GNU) */
 };
 
+/* 
+ * Single node in tree-linked AST. 
+ */
 struct	man_node {
-	struct man_node	*parent;
-	struct man_node	*child;
-	struct man_node	*next;
-	struct man_node	*prev;
-	int		 nchild;
+	struct man_node	*parent; /* parent AST node */
+	struct man_node	*child; /* first child AST node */
+	struct man_node	*next; /* sibling AST node */
+	struct man_node	*prev; /* prior sibling AST node */
+	int		 nchild; /* number children */
 	int		 line;
 	int		 pos;
-	enum mant	 tok;
+	enum mant	 tok; /* tok or MAN__MAX if none */
 	int		 flags;
-#define	MAN_VALID	(1 << 0)
-#define	MAN_ACTED	(1 << 1)
-#define	MAN_EOS		(1 << 2)
-	enum man_type	 type;
-	char		*string;
-	struct man_node	*head;
-	struct man_node	*body;
+#define	MAN_VALID	(1 << 0) /* has been validated */
+#define	MAN_EOS		(1 << 2) /* at sentence boundary */
+	enum man_type	 type; /* AST node type */
+	char		*string; /* TEXT node argument */
+	struct man_node	*head; /* BLOCK node HEAD ptr */
+	struct man_node	*body; /* BLOCK node BODY ptr */
+	const struct tbl_span *span; /* TBL */
 };
 
-#define	MAN_IGN_MACRO	 (1 << 0)
-#define	MAN_IGN_ESCAPE	 (1 << 2)
-
+/*
+ * Names of macros.  Index is enum mant.  Indexing into this returns
+ * the normalised name, e.g., man_macronames[MAN_SH] -> "SH".
+ */
 extern	const char *const *man_macronames;
 
 __BEGIN_DECLS
@@ -107,10 +115,12 @@ __BEGIN_DECLS
 struct	man;
 
 void	 	  man_free(struct man *);
-struct	man	 *man_alloc(struct regset *, void *, int, mandocmsg);
+struct	man	 *man_alloc(struct regset *, void *, mandocmsg);
 void		  man_reset(struct man *);
 int	 	  man_parseln(struct man *, int, char *, int);
 int		  man_endparse(struct man *);
+int		  man_addspan(struct man *,
+			const struct tbl_span *);
 
 const struct man_node *man_node(const struct man *);
 const struct man_meta *man_meta(const struct man *);
