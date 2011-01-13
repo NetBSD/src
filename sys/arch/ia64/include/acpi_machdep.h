a34d3467c9d6b88d4d910a9aacd1700aeff57f1f
jruoho <jruoho@NetBSD.org>
2011-01-13 03:40:50 +0000

    Move the function that counts the CPUs from acpicpu(4) to the MD layer.

--- sys/arch/ia64/include/acpi_machdep.h
+++ sys/arch/ia64/include/acpi_machdep.h
@@ -1,8 +1,8 @@
-/*	$NetBSD: acpi_machdep.h,v 1.2 2009/07/20 04:41:37 kiyohara Exp $	*/
+/*	$NetBSD: acpi_machdep.h,v 1.3 2011/01/13 03:40:50 jruoho Exp $	*/
 
 ACPI_STATUS acpi_md_OsInitialize(void);
 ACPI_STATUS acpi_md_OsTerminate(void);
-ACPI_PHYSICAL_ADDRESS acpi_md_OsGetRootPointer(void); 
+ACPI_PHYSICAL_ADDRESS acpi_md_OsGetRootPointer(void);
 
 #define acpi_md_OsIn8(x)	inb((x))
 #define acpi_md_OsIn16(x)	inw((x))
@@ -13,7 +13,7 @@ ACPI_PHYSICAL_ADDRESS acpi_md_OsGetRootPointer(void);
 #define acpi_md_OsOut32(x, v)	outl((x), (v))
 
 ACPI_STATUS acpi_md_OsInstallInterruptHandler(UINT32, ACPI_OSD_HANDLER,
-					      void *, void **); 
+					      void *, void **);
 void acpi_md_OsRemoveInterruptHandler(void *);
 
 ACPI_STATUS acpi_md_OsMapMemory(ACPI_PHYSICAL_ADDRESS, UINT32, void **);
@@ -21,10 +21,11 @@ void acpi_md_OsUnmapMemory(void *, UINT32);
 ACPI_STATUS acpi_md_OsGetPhysicalAddress(void *LogicalAddress,
 					 ACPI_PHYSICAL_ADDRESS *PhysicalAddress);
 
-BOOLEAN acpi_md_OsReadable(void *, UINT32);
-BOOLEAN acpi_md_OsWritable(void *, UINT32);
-void acpi_md_OsEnableInterrupt(void); 
-void acpi_md_OsDisableInterrupt(void);
+BOOLEAN		acpi_md_OsReadable(void *, UINT32);
+BOOLEAN		acpi_md_OsWritable(void *, UINT32);
+void		acpi_md_OsEnableInterrupt(void);
+void		acpi_md_OsDisableInterrupt(void);
 
-int acpi_md_sleep(int);
-void acpi_md_callback(void);
+int		acpi_md_sleep(int);
+uint32_t	acpi_md_ncpus(void);
+void		acpi_md_callback(void);
