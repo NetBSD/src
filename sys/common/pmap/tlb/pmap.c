4678bccf1fb282b374eaa208c65e45b48b54eadc
matt <matt@NetBSD.org>
2011-01-17 07:54:53 +0000

    Don't define DEBUG by default.

--- sys/common/pmap/tlb/pmap.c
+++ sys/common/pmap/tlb/pmap.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: pmap.c,v 1.1.2.1 2011/01/07 01:21:10 matt Exp $	*/
+/*	$NetBSD: pmap.c,v 1.1.2.2 2011/01/17 07:54:53 matt Exp $	*/
 
 /*-
  * Copyright (c) 1998, 2001 The NetBSD Foundation, Inc.
@@ -67,7 +67,7 @@
 
 #include <sys/cdefs.h>
 
-__KERNEL_RCSID(0, "$NetBSD: pmap.c,v 1.1.2.1 2011/01/07 01:21:10 matt Exp $");
+__KERNEL_RCSID(0, "$NetBSD: pmap.c,v 1.1.2.2 2011/01/17 07:54:53 matt Exp $");
 
 /*
  *	Manages physical address maps.
@@ -113,8 +113,6 @@ __KERNEL_RCSID(0, "$NetBSD: pmap.c,v 1.1.2.1 2011/01/07 01:21:10 matt Exp $");
 #include <sys/socketvar.h>	/* XXX: for sock_loan_thresh */
 
 #include <uvm/uvm.h>
-
-#define DEBUG 1
 
 #define	PMAP_COUNT(name)	(pmap_evcnt_##name.ev_count++ + 0)
 #define PMAP_COUNTER(name, desc) \
