1f73e4e9df46af881a81d1557dc0a856bfd7c601
joerg <joerg>
2011-01-17 18:11:09 +0000

    Move SPARC and SPARC64 to modern CPP. Update UPDATING note for this and Alpha.

--- share/mk/sys.mk
+++ share/mk/sys.mk
@@ -1,4 +1,4 @@
-#	$NetBSD: sys.mk,v 1.102 2011/01/16 19:29:59 joerg Exp $
+#	$NetBSD: sys.mk,v 1.103 2011/01/17 18:11:10 joerg Exp $
 #	@(#)sys.mk	8.2 (Berkeley) 3/21/94
 
 unix?=		We run NetBSD.
@@ -16,7 +16,8 @@ AFLAGS?=
 COMPILE.s?=	${CC} ${AFLAGS} -c
 LINK.s?=	${CC} ${AFLAGS} ${LDFLAGS}
 .if ${MACHINE_ARCH} == "i386" || ${MACHINE_ARCH} == "x86_64" || \
-    ${MACHINE_ARCH} == "alpha"
+    ${MACHINE_ARCH} == "alpha" || ${MACHINE_ARCH} == "sparc" || \
+    ${MACHINE_ARCH} == "sparc64"
 _ASM_TRADITIONAL_CPP=	-x assembler-with-cpp
 .else
 _ASM_TRADITIONAL_CPP=	-traditional-cpp
