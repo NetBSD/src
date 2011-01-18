4ad5cd531831f452d6c8ff2ed555fa1e85fd5475
matt <matt@NetBSD.org>
2011-01-18 01:10:24 +0000

    Add support for various MPC85xx platforms.  The MPC8536DS and MPC8548CDS development systems and the RB800 (routerboard 800) platform.  The CF on the RB800 doesn't work yet.

---
+++ sys/arch/evbppc/mpc85xx/autoconf.c
@@ -0,0 +1,129 @@
+/*	$NetBSD: autoconf.c,v 1.2 2011/01/18 01:10:25 matt Exp $	*/
+/*-
+ * Copyright (c) 2010, 2011 The NetBSD Foundation, Inc.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to The NetBSD Foundation
+ * by Raytheon BBN Technologies Corp and Defense Advanced Research Projects
+ * Agency and which was developed by Matt Thomas of 3am Software Foundry.
+ *
+ * This material is based upon work supported by the Defense Advanced Research
+ * Projects Agency and Space and Naval Warfare Systems Center, Pacific, under
+ * Contract No. N66001-09-C-2073.
+ * Approved for Public Release, Distribution Unlimited
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <sys/cdefs.h>
+__KERNEL_RCSID(0, "$NetBSD: autoconf.c,v 1.2 2011/01/18 01:10:25 matt Exp $");
+
+#define __INTR_PRIVATE
+
+#include "locators.h"
+
+#include <sys/param.h>
+#include <sys/conf.h>
+#include <sys/device.h>
+#include <sys/intr.h>
+#include <sys/systm.h>
+#include <sys/bus.h>
+
+#include <sys/cpu.h>
+#include <powerpc/booke/cpuvar.h>
+
+/*
+ * Determine device configuration for a machine.
+ */
+void
+cpu_configure(void)
+{
+
+	(*powerpc_intrsw->intrsw_init)();
+	calc_delayconst();
+
+	if (config_rootfound("mainbus", NULL) == NULL)
+		panic("%s: mainbus not configured", __func__);
+
+	spl0();
+}
+
+/*
+ * Setup root device.
+ * Configure swap area.
+ */
+void
+cpu_rootconf(void)
+{
+
+	setroot(booted_device, booted_partition);
+}
+
+void
+device_register(device_t dev, void *aux)
+{
+	if (cpu_md_ops.md_device_register != NULL)
+		(*cpu_md_ops.md_device_register)(dev, aux);
+}
+
+static bool mainbus_found;
+
+static int
+mainbus_print(void *aux, const char *pnp)
+{
+	struct mainbus_attach_args *ma = aux;
+
+	if (pnp != NULL)
+		return QUIET;
+
+	if (pnp)
+		aprint_normal("%s at %s", ma->ma_name, pnp);
+	if (ma->ma_node != MAINBUSCF_NODE_DEFAULT)
+		aprint_normal(" node %d", ma->ma_node);
+
+	return (UNCONF);
+}
+
+static int
+mainbus_match(device_t parent, cfdata_t cf, void *aux)
+{
+	return mainbus_found == false;
+}
+
+static void
+mainbus_attach(device_t parent, device_t self, void *aux)
+{
+	struct mainbus_attach_args ma;
+
+	mainbus_found = true;
+
+	aprint_normal("\n");
+
+	ma.ma_name = "cpunode";
+	ma.ma_node = 0;
+	ma.ma_memt = curcpu()->ci_softc->cpu_bst;
+	ma.ma_dmat = &booke_bus_dma_tag;
+
+	config_found_sm_loc(self, "mainbus", NULL, &ma, mainbus_print, NULL);
+}
+
+CFATTACH_DECL_NEW(mainbus, 0, mainbus_match, mainbus_attach, NULL, NULL);
