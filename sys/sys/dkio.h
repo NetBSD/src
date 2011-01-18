831f64ba8b45e443303dc42fc6e787f77a3ab54f
matt <matt@NetBSD.org>
2011-01-18 19:52:23 +0000

    Make struct disklabel 8 byte aligned.  This increases its size by 4 bytes on IPL32 platforms so add code in sys_ioctl (and netbsd32_ioctl) to deal with the older/smaller diskabel size.  This change makes disklabel the same for both IPL32 and LP64 platforms.

--- sys/sys/dkio.h
+++ sys/sys/dkio.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: dkio.h,v 1.16 2010/03/22 16:49:41 martin Exp $	*/
+/*	$NetBSD: dkio.h,v 1.17 2011/01/18 19:52:24 matt Exp $	*/
 
 /*
  * Copyright (c) 1987, 1988, 1993
@@ -44,6 +44,7 @@
 #define DIOCWDINFO	_IOW('d', 103, struct disklabel)/* set, update disk */
 
 #ifdef _KERNEL
+#define DIOCGDINFO32	(DIOCGDINFO - (sizeof(uint32_t) << IOCPARM_SHIFT))
 #define DIOCGPART	_IOW('d', 104, struct partinfo)	/* get partition */
 #endif
 
