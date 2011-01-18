8ae99a5ca4399a08cc7fceb21d42865e0c3bf3bf
matt <matt>
2011-01-18 21:45:28 +0000

    Welcome to 5.99.44

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.379 2011/01/02 05:13:53 dholland Exp $	*/
+/*	$NetBSD: param.h,v 1.380 2011/01/18 21:45:28 matt Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -63,7 +63,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	599004300	/* NetBSD 5.99.43 */
+#define	__NetBSD_Version__	599004400	/* NetBSD 5.99.44 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
