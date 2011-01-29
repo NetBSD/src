9ba70f47396b9bfd8f525c0533b5d88f600552e2
bouyer <bouyer@NetBSD.org>
2011-01-29 17:42:37 +0000

    Allow to pass HN_PRIV_UNLIMITED flag (private to printquota consumers), which cause it to return "unlimited" instead of "-" of UQUAD_MAX. Introduce intrd(), which parses a string and return a value appropriate for quota limits. The string can be a decimal number, a value in understandable by dehumanize_number(), "-" or "unlimited".

--- usr.bin/quota/printquota.h
+++ usr.bin/quota/printquota.h
@@ -1,4 +1,7 @@
-/*	$NetBSD: printquota.h,v 1.1.2.2 2011/01/28 22:15:36 bouyer Exp $ */
+/*	$NetBSD: printquota.h,v 1.1.2.3 2011/01/29 17:42:37 bouyer Exp $ */
 
-const char *intprt(uint64_t, int, int);
+const char *intprt(uint64_t, u_int, int);
+#define HN_PRIV_UNLIMITED 0x80000000	/* print "unlimited" instead of "-" */
 const char *timeprt(time_t);
+int intrd(char *str, uint64_t *val, u_int);
+
