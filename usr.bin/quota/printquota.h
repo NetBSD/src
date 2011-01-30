560ca22f8200ecd8a38b2bb6ada2aaabb72eb0b6
bouyer <bouyer@NetBSD.org>
2011-01-30 21:37:39 +0000

    Revert to previous format for timeprt, which is less precise but more user-friendly. Introduce timepprt for cases where we want human-readable but precise time display.

--- usr.bin/quota/printquota.h
+++ usr.bin/quota/printquota.h
@@ -1,6 +1,7 @@
-/*	$NetBSD: printquota.h,v 1.1.2.5 2011/01/30 20:54:22 bouyer Exp $ */
+/*	$NetBSD: printquota.h,v 1.1.2.6 2011/01/30 21:37:39 bouyer Exp $ */
 
 const char *intprt(uint64_t, u_int, int, int);
 const char *timeprt(time_t, time_t, int space);
+const char *timepprt(time_t, time_t, int space);
 int intrd(char *str, uint64_t *val, u_int);
 
