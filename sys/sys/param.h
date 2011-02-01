beefdbc05913fb932eb52bd7e9bba74e2739a404
matt <matt@NetBSD.org>
2011-02-01 01:40:00 +0000

    Welcome to NetBSD 5.99.45 (Routing socket changes)

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.380 2011/01/18 21:45:28 matt Exp $	*/
+/*	$NetBSD: param.h,v 1.381 2011/02/01 01:40:00 matt Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -63,7 +63,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	599004400	/* NetBSD 5.99.44 */
+#define	__NetBSD_Version__	599004500	/* NetBSD 5.99.45 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
