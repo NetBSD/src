39094c9da3fcde5a07dc9e7aa34980fc1d75529a
chuck <chuck@NetBSD.org>
2011-02-01 20:19:31 +0000

    udpate license clauses on my code to match the new-style BSD licenses. remove no-longer-valid wustl email address for me. based on diff that rmind@ sent me.
    no functional change with this commit.

--- sys/arch/x86/x86/pmap.c
+++ sys/arch/x86/x86/pmap.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: pmap.c,v 1.113 2010/07/24 00:45:56 jym Exp $	*/
+/*	$NetBSD: pmap.c,v 1.114 2011/02/01 20:19:32 chuck Exp $	*/
 
 /*
  * Copyright (c) 2007 Manuel Bouyer.
@@ -42,7 +42,6 @@
  */
 
 /*
- *
  * Copyright (c) 1997 Charles D. Cranor and Washington University.
  * All rights reserved.
  *
@@ -54,12 +53,6 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *      This product includes software developed by Charles D. Cranor and
- *      Washington University.
- * 4. The name of the author may not be used to endorse or promote products
- *    derived from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
@@ -149,7 +142,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: pmap.c,v 1.113 2010/07/24 00:45:56 jym Exp $");
+__KERNEL_RCSID(0, "$NetBSD: pmap.c,v 1.114 2011/02/01 20:19:32 chuck Exp $");
 
 #include "opt_user_ldt.h"
 #include "opt_lockdebug.h"
