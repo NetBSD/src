d989401c208e11e2d8832d09b729c2816928035f
chuck <chuck@NetBSD.org>
2011-02-02 15:13:33 +0000

    udpate license clauses on my code to match the new-style BSD licenses. based on diff that rmind@ sent me.
    no functional change with this commit.

--- sys/uvm/uvm_ddb.h
+++ sys/uvm/uvm_ddb.h
@@ -1,7 +1,6 @@
-/*	$NetBSD: uvm_ddb.h,v 1.13 2009/05/03 16:52:54 pooka Exp $	*/
+/*	$NetBSD: uvm_ddb.h,v 1.14 2011/02/02 15:13:34 chuck Exp $	*/
 
 /*
- *
  * Copyright (c) 1997 Charles D. Cranor and Washington University.
  * All rights reserved.
  *
@@ -13,12 +12,6 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *      This product includes software developed by Charles D. Cranor and
- *      Washington University.
- * 4. The name of the author may not be used to endorse or promote products
- *    derived from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
--- sys/uvm/uvm_pager.h
+++ sys/uvm/uvm_pager.h
@@ -1,7 +1,6 @@
-/*	$NetBSD: uvm_pager.h,v 1.39 2010/09/01 16:56:19 chs Exp $	*/
+/*	$NetBSD: uvm_pager.h,v 1.40 2011/02/02 15:13:34 chuck Exp $	*/
 
 /*
- *
  * Copyright (c) 1997 Charles D. Cranor and Washington University.
  * All rights reserved.
  *
@@ -13,12 +12,6 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *      This product includes software developed by Charles D. Cranor and
- *      Washington University.
- * 4. The name of the author may not be used to endorse or promote products
- *    derived from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
