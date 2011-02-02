3c56edc419f4815aecfb37294f79c876cd28ab6f
chuck <chuck@NetBSD.org>
2011-02-02 15:25:27 +0000

    udpate license clauses on my code to match the new-style BSD licenses. based on second diff that rmind@ sent me.
    no functional change with this commit.

--- sys/uvm/uvm_page.c
+++ sys/uvm/uvm_page.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: uvm_page.c,v 1.169 2011/01/04 08:26:33 matt Exp $	*/
+/*	$NetBSD: uvm_page.c,v 1.170 2011/02/02 15:25:27 chuck Exp $	*/
 
 /*
  * Copyright (c) 2010 The NetBSD Foundation, Inc.
@@ -43,12 +43,7 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed by Charles D. Cranor,
- *      Washington University, the University of California, Berkeley and
- *      its contributors.
- * 4. Neither the name of the University nor the names of its contributors
+ * 3. Neither the name of the University nor the names of its contributors
  *    may be used to endorse or promote products derived from this software
  *    without specific prior written permission.
  *
@@ -97,7 +92,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: uvm_page.c,v 1.169 2011/01/04 08:26:33 matt Exp $");
+__KERNEL_RCSID(0, "$NetBSD: uvm_page.c,v 1.170 2011/02/02 15:25:27 chuck Exp $");
 
 #include "opt_ddb.h"
 #include "opt_uvmhist.h"
