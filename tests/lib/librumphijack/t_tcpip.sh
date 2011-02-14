61b0a492b137040b54f951a4ad2c7916af62a1ab
pooka <pooka@NetBSD.org>
2011-02-14 15:14:00 +0000

    Test that hijacked ssh/sshd work.
    Copypastes jmmv's sshd magic from fs/psshfs.
    (dunno if it's worth sharing the code, or even what the
    best practice for doing so would be)

--- tests/lib/librumphijack/t_tcpip.sh
+++ tests/lib/librumphijack/t_tcpip.sh
@@ -1,4 +1,4 @@
-#       $NetBSD: t_tcpip.sh,v 1.1 2011/02/06 18:44:30 pooka Exp $
+#       $NetBSD: t_tcpip.sh,v 1.2 2011/02/14 15:14:00 pooka Exp $
 #
 # Copyright (c) 2011 The NetBSD Foundation, Inc.
 # All rights reserved.
@@ -42,7 +42,7 @@ http_body()
 	export RUMPHIJACK_RETRY='die'
 
 	# start bozo in daemon mode
-	atf_check -s exit:0 -e ignore env LD_PRELOAD=/usr/lib/librumphijack.so \
+	atf_check -s exit:0 env LD_PRELOAD=/usr/lib/librumphijack.so \
 	    /usr/libexec/httpd -b -s $(atf_get_srcdir)
 
 	atf_check -s exit:0 -o file:"$(atf_get_srcdir)/netstat.expout" \
@@ -64,7 +64,90 @@ http_cleanup()
 	rump.halt
 }
 
+#
+# Starts a SSH server and sets up the client to access it.
+# Authentication is allowed and done using an RSA key exclusively, which
+# is generated on the fly as part of the test case.
+# XXX: Ideally, all the tests in this test program should be able to share
+# the generated key, because creating it can be a very slow process on some
+# machines.
+#
+# XXX2: copypasted from jmmv's sshd thingamob in the psshfs test.
+# ideally code (and keys, like jmmv notes above) could be shared
+#
+start_sshd() {
+	echo "Setting up SSH server configuration"
+	sed -e "s,@SRCDIR@,$(atf_get_srcdir),g" -e "s,@WORKDIR@,$(pwd),g" \
+	    $(atf_get_srcdir)/sshd_config.in >sshd_config || \
+	    atf_fail "Failed to create sshd_config"
+	atf_check -s ignore -o empty -e ignore \
+	    cp $(atf_get_srcdir)/ssh_host_key .
+	atf_check -s ignore -o empty -e ignore \
+	    cp $(atf_get_srcdir)/ssh_host_key.pub .
+	atf_check -s eq:0 -o empty -e empty chmod 400 ssh_host_key
+	atf_check -s eq:0 -o empty -e empty chmod 444 ssh_host_key.pub
+
+        env LD_PRELOAD=/usr/lib/librumphijack.so \
+	    /usr/sbin/sshd -e -f ./sshd_config
+	while [ ! -f sshd.pid ]; do
+		sleep 0.01
+	done
+	echo "SSH server started (pid $(cat sshd.pid))"
+
+	echo "Setting up SSH client configuration"
+	atf_check -s eq:0 -o empty -e empty \
+	    ssh-keygen -f ssh_user_key -t rsa -b 1024 -N "" -q
+	atf_check -s eq:0 -o empty -e empty \
+	    cp ssh_user_key.pub authorized_keys
+	echo "127.0.0.1,localhost,::1 " \
+	    "$(cat $(atf_get_srcdir)/ssh_host_key.pub)" >known_hosts || \
+	    atf_fail "Failed to create known_hosts"
+	atf_check -s eq:0 -o empty -e empty chmod 600 authorized_keys
+	sed -e "s,@SRCDIR@,$(atf_get_srcdir),g" -e "s,@WORKDIR@,$(pwd),g" \
+	    $(atf_get_srcdir)/ssh_config.in >ssh_config || \
+	    atf_fail "Failed to create ssh_config"
+	
+	echo "sshd running"
+}
+
+atf_test_case ssh cleanup
+ssh_head()
+{
+        atf_set "descr" "Test that hijacked ssh/sshd works"
+}
+
+ssh_body()
+{
+
+	atf_check -s exit:0 ${rumpnetsrv} ${RUMP_SERVER}
+	# make sure clients die after we nuke the server
+	export RUMPHIJACK_RETRY='die'
+
+	export LD_LIBRARY_PATH=/home/pooka/src/nb5/src/lib/libssh
+
+	start_sshd
+
+	# create some sort of directory for us to "ls"
+	mkdir testdir
+	cd testdir
+	jot 11 | xargs touch
+	jot 11 12 | xargs mkdir
+	cd ..
+
+	atf_check -s exit:0 -o save:ssh.out				\
+	    env LD_PRELOAD=/usr/lib/librumphijack.so			\
+	    ssh -T -F ssh_config 127.0.0.1 ls -li $(pwd)/testdir
+	atf_check -s exit:0 -o file:ssh.out ls -li $(pwd)/testdir
+}
+
+ssh_cleanup()
+{
+	rump.halt
+	# sshd dies due to RUMPHIJACK_RETRY=1d6
+}
+
 atf_init_test_cases()
 {
 	atf_add_test_case http
+	atf_add_test_case ssh
 }
