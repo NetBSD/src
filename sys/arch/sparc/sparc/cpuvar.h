5b2566c5a30fbf4a0182e7e2ceda62b6b216949f
bouyer <bouyer@NetBSD.org>
2011-02-16 21:33:25 +0000

    Apply patch, requested my mrg in ticket 1553: 	sys/arch/sparc/sparc/cpu.c:	patch 	sys/arch/sparc/sparc/cpuvar.h:	patch 	sys/arch/sparc/sparc/pmap.c:	patch 	sys/arch/sparc/sparc/timer_sun4m.c:	patch - print the curpcb in ddb "mach cpu" output as well. - bump the size of cpus[] by one, so we have a NULL pointer at the end,   from tsutsui - for MP kernels, copy the loop to find the bootcpu in mainbus_attach()   into getcacheinfo_obp() so we can get cache properties on the bootcpu   before calling main() - in getcpuinfo(), move the call of getmid() before the call to   getcacheinfo() so that the above change to getcacheinfo_obp() can work - move the struct cpu_info setup to the end of the initial kernel page   setup and don't access this space until after we have switched to the   kernel pagetables - revive most of the old CPUINFO_VA alignment/congruency code from the   old alloc_cpuinfo_global_va() function, and ensure that all cpuinfo   structures are sanely aligned.  this makes hypersparc work again - introduce a new way to free the wasted pages back to UVM, as we can't   simply uvm_unmap() them this early in bootstrap - make sure to initialise the cpuinfo sz in all cases.  noted by martin. - add per-cpu event counters for lev10 and lev14 interrupts. - make CPU_INFO_FOREACH() set the iterator count to '0' in the !MP case. - add some disabled MP code to poke other cpus on level 14 interrupts. - add a diagnostic to ensure that cpus[0] == cpu0's cpu_info->ci_self - if a cpu doesn't have any mappings allocated, don't copy them.  this   occurs if a cpu isn't attached (such as a MP kernel with only "cpu0"   listed in the config file..) - fix the previous to compile !MULTIPROCESSOR.

--- sys/arch/sparc/sparc/cpuvar.h
+++ sys/arch/sparc/sparc/cpuvar.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: cpuvar.h,v 1.75.10.3 2011/01/28 07:16:13 snj Exp $ */
+/*	$NetBSD: cpuvar.h,v 1.75.10.4 2011/02/16 21:33:25 bouyer Exp $ */
 
 /*
  *  Copyright (c) 1996 The NetBSD Foundation, Inc.
@@ -325,6 +325,16 @@ struct cpu_info {
 	/*bus_space_handle_t*/ long ci_mxccregs;
 
 	u_int	ci_tt;			/* Last trap (if tracing) */
+
+	/*
+	 * Start/End VA's of this cpu_info region; we upload the other pages
+	 * in this region that aren't part of the cpu_info to uvm.
+	 */
+	vaddr_t	ci_free_sva1, ci_free_eva1, ci_free_sva2, ci_free_eva2;
+
+	char ci_cpuname[8];	/* "cpu/0", etc. */
+	struct evcnt ci_lev10;
+	struct evcnt ci_lev14;
 };
 
 /*
@@ -415,7 +425,7 @@ struct cpu_info {
 #if defined(MULTIPROCESSOR) || defined(MODULAR) || defined(_MODULE)
 #define	CPU_INFO_FOREACH(cii, cp)	cii = 0; (cp = cpus[cii]) && cp->eintstack && cii < sparc_ncpus; cii++
 #else
-#define CPU_INFO_FOREACH(cii, cp)	(void)cii, cp = curcpu(); cp != NULL; cp = NULL
+#define CPU_INFO_FOREACH(cii, cp)	cii = 0, cp = curcpu(); cp != NULL; cp = NULL
 #endif
 
 /*
