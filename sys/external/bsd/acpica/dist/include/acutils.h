8b73a1e7eba09567d08c60e3ec4e5dc106cdaf95
jruoho <jruoho>
2011-02-17 11:21:40 +0000

    Sigh. Why some people are unable to write proper C?

--- sys/external/bsd/acpica/dist/include/acutils.h
+++ sys/external/bsd/acpica/dist/include/acutils.h
@@ -133,11 +133,11 @@ AcpiUtGetNotifyName (
 
 #endif
 
-char *
+const char *
 AcpiUtGetTypeName (
     ACPI_OBJECT_TYPE        Type);
 
-char *
+const char *
 AcpiUtGetNodeName (
     void                    *Object);
 
@@ -149,15 +149,15 @@ const char *
 AcpiUtGetReferenceName (
     ACPI_OPERAND_OBJECT     *Object);
 
-char *
+const char *
 AcpiUtGetObjectTypeName (
     ACPI_OPERAND_OBJECT     *ObjDesc);
 
-char *
+const char *
 AcpiUtGetRegionName (
     UINT8                   SpaceId);
 
-char *
+const char *
 AcpiUtGetEventName (
     UINT32                  EventId);
 
