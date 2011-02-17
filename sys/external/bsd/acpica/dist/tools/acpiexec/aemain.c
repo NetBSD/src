68471b325c244ca8dbec52474665c41263820e18
jruoho <jruoho@NetBSD.org>
2011-02-17 10:00:50 +0000

    Import 20110211.

--- sys/external/bsd/acpica/dist/tools/acpiexec/aemain.c
+++ sys/external/bsd/acpica/dist/tools/acpiexec/aemain.c
@@ -4,114 +4,42 @@
  *
  *****************************************************************************/
 
-/******************************************************************************
- *
- * 1. Copyright Notice
- *
- * Some or all of this work - Copyright (c) 1999 - 2010, Intel Corp.
+/*
+ * Copyright (C) 2000 - 2011, Intel Corp.
  * All rights reserved.
  *
- * 2. License
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions, and the following disclaimer,
+ *    without modification.
+ * 2. Redistributions in binary form must reproduce at minimum a disclaimer
+ *    substantially similar to the "NO WARRANTY" disclaimer below
+ *    ("Disclaimer") and any redistribution must be conditioned upon
+ *    including a substantially similar Disclaimer requirement for further
+ *    binary redistribution.
+ * 3. Neither the names of the above-listed copyright holders nor the names
+ *    of any contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
  *
- * 2.1. This is your license from Intel Corp. under its intellectual property
- * rights.  You may have additional license terms from the party that provided
- * you this software, covering your right to use that party's intellectual
- * property rights.
+ * Alternatively, this software may be distributed under the terms of the
+ * GNU General Public License ("GPL") version 2 as published by the Free
+ * Software Foundation.
  *
- * 2.2. Intel grants, free of charge, to any person ("Licensee") obtaining a
- * copy of the source code appearing in this file ("Covered Code") an
- * irrevocable, perpetual, worldwide license under Intel's copyrights in the
- * base code distributed originally by Intel ("Original Intel Code") to copy,
- * make derivatives, distribute, use and display any portion of the Covered
- * Code in any form, with the right to sublicense such rights; and
- *
- * 2.3. Intel grants Licensee a non-exclusive and non-transferable patent
- * license (with the right to sublicense), under only those claims of Intel
- * patents that are infringed by the Original Intel Code, to make, use, sell,
- * offer to sell, and import the Covered Code and derivative works thereof
- * solely to the minimum extent necessary to exercise the above copyright
- * license, and in no event shall the patent license extend to any additions
- * to or modifications of the Original Intel Code.  No other license or right
- * is granted directly or by implication, estoppel or otherwise;
- *
- * The above copyright and patent license is granted only if the following
- * conditions are met:
- *
- * 3. Conditions
- *
- * 3.1. Redistribution of Source with Rights to Further Distribute Source.
- * Redistribution of source code of any substantial portion of the Covered
- * Code or modification with rights to further distribute source must include
- * the above Copyright Notice, the above License, this list of Conditions,
- * and the following Disclaimer and Export Compliance provision.  In addition,
- * Licensee must cause all Covered Code to which Licensee contributes to
- * contain a file documenting the changes Licensee made to create that Covered
- * Code and the date of any change.  Licensee must include in that file the
- * documentation of any changes made by any predecessor Licensee.  Licensee
- * must include a prominent statement that the modification is derived,
- * directly or indirectly, from Original Intel Code.
- *
- * 3.2. Redistribution of Source with no Rights to Further Distribute Source.
- * Redistribution of source code of any substantial portion of the Covered
- * Code or modification without rights to further distribute source must
- * include the following Disclaimer and Export Compliance provision in the
- * documentation and/or other materials provided with distribution.  In
- * addition, Licensee may not authorize further sublicense of source of any
- * portion of the Covered Code, and must include terms to the effect that the
- * license from Licensee to its licensee is limited to the intellectual
- * property embodied in the software Licensee provides to its licensee, and
- * not to intellectual property embodied in modifications its licensee may
- * make.
- *
- * 3.3. Redistribution of Executable. Redistribution in executable form of any
- * substantial portion of the Covered Code or modification must reproduce the
- * above Copyright Notice, and the following Disclaimer and Export Compliance
- * provision in the documentation and/or other materials provided with the
- * distribution.
- *
- * 3.4. Intel retains all right, title, and interest in and to the Original
- * Intel Code.
- *
- * 3.5. Neither the name Intel nor any other trademark owned or controlled by
- * Intel shall be used in advertising or otherwise to promote the sale, use or
- * other dealings in products derived from or relating to the Covered Code
- * without prior written authorization from Intel.
- *
- * 4. Disclaimer and Export Compliance
- *
- * 4.1. INTEL MAKES NO WARRANTY OF ANY KIND REGARDING ANY SOFTWARE PROVIDED
- * HERE.  ANY SOFTWARE ORIGINATING FROM INTEL OR DERIVED FROM INTEL SOFTWARE
- * IS PROVIDED "AS IS," AND INTEL WILL NOT PROVIDE ANY SUPPORT,  ASSISTANCE,
- * INSTALLATION, TRAINING OR OTHER SERVICES.  INTEL WILL NOT PROVIDE ANY
- * UPDATES, ENHANCEMENTS OR EXTENSIONS.  INTEL SPECIFICALLY DISCLAIMS ANY
- * IMPLIED WARRANTIES OF MERCHANTABILITY, NONINFRINGEMENT AND FITNESS FOR A
- * PARTICULAR PURPOSE.
- *
- * 4.2. IN NO EVENT SHALL INTEL HAVE ANY LIABILITY TO LICENSEE, ITS LICENSEES
- * OR ANY OTHER THIRD PARTY, FOR ANY LOST PROFITS, LOST DATA, LOSS OF USE OR
- * COSTS OF PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES, OR FOR ANY INDIRECT,
- * SPECIAL OR CONSEQUENTIAL DAMAGES ARISING OUT OF THIS AGREEMENT, UNDER ANY
- * CAUSE OF ACTION OR THEORY OF LIABILITY, AND IRRESPECTIVE OF WHETHER INTEL
- * HAS ADVANCE NOTICE OF THE POSSIBILITY OF SUCH DAMAGES.  THESE LIMITATIONS
- * SHALL APPLY NOTWITHSTANDING THE FAILURE OF THE ESSENTIAL PURPOSE OF ANY
- * LIMITED REMEDY.
- *
- * 4.3. Licensee shall not export, either directly or indirectly, any of this
- * software or system incorporating such software without first obtaining any
- * required license or other approval from the U. S. Department of Commerce or
- * any other agency or department of the United States Government.  In the
- * event Licensee exports any such software from the United States or
- * re-exports any such software from a foreign destination, Licensee shall
- * ensure that the distribution and export/re-export of the software is in
- * compliance with all laws, regulations, orders, or other restrictions of the
- * U.S. Export Administration Regulations. Licensee agrees that neither it nor
- * any of its subsidiaries will export/re-export any technical data, process,
- * software, or service, directly or indirectly, to any country for which the
- * United States government or any agency thereof requires an export license,
- * other governmental approval, or letter of assurance, without first obtaining
- * such license, approval or letter.
- *
- *****************************************************************************/
+ * NO WARRANTY
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * HOLDERS OR CONTRIBUTORS BE LIABLE FOR SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+ * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGES.
+ */
 
 #include "aecommon.h"
 
@@ -122,17 +50,18 @@
 #define _COMPONENT          PARSER
         ACPI_MODULE_NAME    ("aemain")
 
-UINT8           AcpiGbl_BatchMode = 0;
-UINT8           AcpiGbl_RegionFillValue = 0;
-BOOLEAN         AcpiGbl_IgnoreErrors = FALSE;
-BOOLEAN         AcpiGbl_DbOpt_NoRegionSupport = FALSE;
-BOOLEAN         AcpiGbl_DebugTimeout = FALSE;
-char            BatchBuffer[128];
-AE_TABLE_DESC   *AeTableListHead = NULL;
+
+UINT8                   AcpiGbl_RegionFillValue = 0;
+BOOLEAN                 AcpiGbl_IgnoreErrors = FALSE;
+BOOLEAN                 AcpiGbl_DbOpt_NoRegionSupport = FALSE;
+BOOLEAN                 AcpiGbl_DebugTimeout = FALSE;
+
+static UINT8            AcpiGbl_BatchMode = 0;
+static char             BatchBuffer[128];
+static AE_TABLE_DESC    *AeTableListHead = NULL;
 
 #define ASL_MAX_FILES   256
-char                    *FileList[ASL_MAX_FILES];
-int                     FileCount;
+static char             *FileList[ASL_MAX_FILES];
 
 
 #define AE_SUPPORTED_OPTIONS    "?b:d:e:f:gm^ovx:"
@@ -202,6 +131,7 @@ AcpiDbRunBatchMode (
     char                    *Ptr = BatchBuffer;
     char                    *Cmd = Ptr;
     UINT8                   Run = 0;
+
 
     AcpiGbl_MethodExecuting = FALSE;
     AcpiGbl_StepToNextCall = FALSE;
@@ -360,6 +290,7 @@ AsDoWildcard (
 #ifdef WIN32
     void                    *DirInfo;
     char                    *Filename;
+    int                     FileCount;
 
 
     FileCount = 0;
@@ -441,7 +372,7 @@ main (
     ACPI_TABLE_HEADER       *Table = NULL;
     UINT32                  TableCount;
     AE_TABLE_DESC           *TableDesc;
-    char                    **FileList;
+    char                    **WildcardList;
     char                    *Filename;
     char                    *Directory;
     char                    *FullPathname;
@@ -452,14 +383,12 @@ main (
                     _CrtSetDbgFlag(_CRTDBG_REPORT_FLAG));
 #endif
 
-    printf ("\nIntel ACPI Component Architecture\nAML Execution/Debug Utility");
-    printf (" version %8.8X", ((UINT32) ACPI_CA_VERSION));
-    printf (" [%s]\n\n",  __DATE__);
+    printf (ACPI_COMMON_SIGNON ("AML Execution/Debug Utility"));
 
     if (argc < 2)
     {
         usage ();
-        return 0;
+        return (0);
     }
 
     signal (SIGINT, AeCtrlCHandler);
@@ -471,7 +400,8 @@ main (
 
     /* Init ACPI and start debugger thread */
 
-    AcpiInitializeSubsystem ();
+    Status = AcpiInitializeSubsystem ();
+    AE_CHECK_OK (AcpiInitializeSubsystem, Status);
 
     /* Get the command line options */
 
@@ -482,7 +412,7 @@ main (
         {
             printf ("**** The length of command line (%u) exceeded maximum (127)\n",
                 (UINT32) strlen (AcpiGbl_Optarg));
-            return -1;
+            return (-1);
         }
         AcpiGbl_BatchMode = 1;
         strcpy (BatchBuffer, AcpiGbl_Optarg);
@@ -586,7 +516,7 @@ main (
     case 'h':
     default:
         usage();
-        return -1;
+        return (-1);
     }
 
 
@@ -617,21 +547,21 @@ main (
 
             /* Expand wildcards (Windows only) */
 
-            FileList = AsDoWildcard (Directory, Filename);
-            if (!FileList)
+            WildcardList = AsDoWildcard (Directory, Filename);
+            if (!WildcardList)
             {
-                return -1;
+                return (-1);
             }
 
-            while (*FileList)
+            while (*WildcardList)
             {
                 FullPathname = AcpiOsAllocate (
-                    strlen (Directory) + strlen (*FileList) + 1);
+                    strlen (Directory) + strlen (*WildcardList) + 1);
 
                 /* Construct a full path to the file */
 
                 strcpy (FullPathname, Directory);
-                strcat (FullPathname, *FileList);
+                strcat (FullPathname, *WildcardList);
 
                 /* Get one table */
 
@@ -644,9 +574,9 @@ main (
                 }
 
                 AcpiOsFree (FullPathname);
-                AcpiOsFree (*FileList);
-                *FileList = NULL;
-                FileList++;
+                AcpiOsFree (*WildcardList);
+                *WildcardList = NULL;
+                WildcardList++;
 
                 /*
                  * Ignore an FACS or RSDT, we can't use them.
@@ -676,7 +606,7 @@ main (
         Status = AeBuildLocalTables (TableCount, AeTableListHead);
         if (ACPI_FAILURE (Status))
         {
-            return -1;
+            return (-1);
         }
 
         Status = AeInstallTables ();
@@ -686,15 +616,18 @@ main (
             goto enterloop;
         }
 
-        Status = AeInstallHandlers ();
+         /*
+          * Install most of the handlers.
+          * Override some default region handlers, especially SystemMemory
+          */
+        Status = AeInstallEarlyHandlers ();
         if (ACPI_FAILURE (Status))
         {
             goto enterloop;
         }
 
         /*
-         * TBD:
-         * Need a way to call this after the "LOAD" command
+         * TBD: Need a way to call this after the "LOAD" command
          */
         Status = AcpiEnableSubsystem (InitFlags);
         if (ACPI_FAILURE (Status))
@@ -710,6 +643,11 @@ main (
             goto enterloop;
         }
 
+        /*
+         * Install handlers for "device driver" space IDs (EC,SMBus, etc.)
+         * and fixed event handlers
+         */
+        AeInstallLateHandlers ();
         AeMiscellaneousTests ();
     }
 
@@ -730,6 +668,6 @@ enterloop:
         AcpiDbUserCommands (ACPI_DEBUGGER_COMMAND_PROMPT, NULL);
     }
 
-    return 0;
+    return (0);
 }
 
