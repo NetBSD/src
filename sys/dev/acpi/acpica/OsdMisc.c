b38b87362a062e227089e5e48a236c9dc7dffb7e
jruoho <jruoho@NetBSD.org>
2011-02-17 12:08:46 +0000

    Include acdebug.h for the ACPICA debugger.

--- sys/dev/acpi/acpica/OsdMisc.c
+++ sys/dev/acpi/acpica/OsdMisc.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: OsdMisc.c,v 1.10 2011/02/17 07:34:42 jruoho Exp $	*/
+/*	$NetBSD: OsdMisc.c,v 1.11 2011/02/17 12:08:46 jruoho Exp $	*/
 
 /*
  * Copyright 2001 Wasabi Systems, Inc.
@@ -42,7 +42,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: OsdMisc.c,v 1.10 2011/02/17 07:34:42 jruoho Exp $");
+__KERNEL_RCSID(0, "$NetBSD: OsdMisc.c,v 1.11 2011/02/17 12:08:46 jruoho Exp $");
 
 #include "opt_acpi.h"
 #include "opt_ddb.h"
@@ -57,6 +57,10 @@ __KERNEL_RCSID(0, "$NetBSD: OsdMisc.c,v 1.10 2011/02/17 07:34:42 jruoho Exp $");
 
 #include <dev/acpi/acpica.h>
 #include <dev/acpi/acpi_osd.h>
+
+#ifdef ACPI_DEBUG
+#include <external/bsd/acpica/dist/include/acdebug.h>
+#endif
 
 #ifdef ACPI_DSDT_OVERRIDE
 #ifndef ACPI_DSDT_FILE
