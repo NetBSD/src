b1c533477289715999d6d259c6ea22ab65f4f2d5
jmcneill <jmcneill@NetBSD.org>
2011-02-18 13:56:58 +0000

    call vcons_hard_switch to switch to the console screen when entering polling mode, makes ddb-from-X work with genfb on x86

--- sys/dev/wsfb/genfb.c
+++ sys/dev/wsfb/genfb.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: genfb.c,v 1.36 2011/02/12 17:15:27 phx Exp $ */
+/*	$NetBSD: genfb.c,v 1.37 2011/02/18 13:56:58 jmcneill Exp $ */
 
 /*-
  * Copyright (c) 2007 Michael Lorenz
@@ -27,7 +27,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: genfb.c,v 1.36 2011/02/12 17:15:27 phx Exp $");
+__KERNEL_RCSID(0, "$NetBSD: genfb.c,v 1.37 2011/02/18 13:56:58 jmcneill Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -692,6 +692,7 @@ genfb_enable_polling(device_t dev)
 	struct genfb_softc *sc = device_private(dev);
 
 	SCREEN_ENABLE_DRAWING(&sc->sc_console_screen);
+	vcons_hard_switch(&sc->sc_console_screen);
 	vcons_enable_polling(&sc->vd);
 }
 
