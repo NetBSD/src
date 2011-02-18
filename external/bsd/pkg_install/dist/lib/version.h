3bd4937d46779f709cb02da58d2f703e474ad29a
aymeric <aymeric@NetBSD.org>
2011-02-18 22:36:00 +0000

    merge pkg_install 20110215

--- external/bsd/pkg_install/dist/lib/version.h
+++ external/bsd/pkg_install/dist/lib/version.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: version.h,v 1.6 2010/06/26 00:17:13 joerg Exp $	*/
+/*	$NetBSD: version.h,v 1.7 2011/02/18 22:36:00 aymeric Exp $	*/
 
 /*
  * Copyright (c) 2001 Thomas Klausner.  All rights reserved.
@@ -27,6 +27,6 @@
 #ifndef _INST_LIB_VERSION_H_
 #define _INST_LIB_VERSION_H_
 
-#define PKGTOOLS_VERSION 20100616
+#define PKGTOOLS_VERSION 20110215
 
 #endif /* _INST_LIB_VERSION_H_ */
