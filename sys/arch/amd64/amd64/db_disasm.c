ad944aa0af64ff9300e5fa0d0bf6399fe3aa859c
drochner <drochner@NetBSD.org>
2011-02-18 18:00:52 +0000

    fix misinterpretation of REX prefixes where use of the accumulator as operand is hardwired into the instruction code, mostly from Wolfgang Stukenbrock per PR port-amd64/44405

--- sys/arch/amd64/amd64/db_disasm.c
+++ sys/arch/amd64/amd64/db_disasm.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: db_disasm.c,v 1.13 2009/03/14 21:04:03 dsl Exp $	*/
+/*	$NetBSD: db_disasm.c,v 1.14 2011/02/18 18:00:52 drochner Exp $	*/
 
 /* 
  * Mach Operating System
@@ -33,7 +33,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: db_disasm.c,v 1.13 2009/03/14 21:04:03 dsl Exp $");
+__KERNEL_RCSID(0, "$NetBSD: db_disasm.c,v 1.14 2011/02/18 18:00:52 drochner Exp $");
 
 #ifndef _KERNEL
 #include "stubs.h"
@@ -1393,11 +1393,9 @@ db_disasm(db_addr_t loc, bool altfmt)
 		    case Si:
 			db_printf("%s", db_seg_reg[f_reg(inst)]);
 			break;
-		    case A: {
-			int ext = ((rex & REX_w) != 0);
-			db_printf("%s", db_reg[ext][size][0]);	/* acc */
+		    case A:
+			db_printf("%s", db_reg[0][size][0]);	/* acc */
 			break;
-		    }
 		    case BX:
 			if (seg)
 				db_printf("%s:", seg);
