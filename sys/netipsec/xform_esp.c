85efb08bf8f741a06a4996825c8e1dab25e922b9
degroote <degroote@NetBSD.org>
2011-02-19 18:26:50 +0000

    Fix a missing const in FAST_IPSEC && IPSEC_DEBUG

--- sys/netipsec/xform_esp.c
+++ sys/netipsec/xform_esp.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: xform_esp.c,v 1.28 2011/02/18 20:40:58 drochner Exp $	*/
+/*	$NetBSD: xform_esp.c,v 1.29 2011/02/19 18:26:50 degroote Exp $	*/
 /*	$FreeBSD: src/sys/netipsec/xform_esp.c,v 1.2.2.1 2003/01/24 05:11:36 sam Exp $	*/
 /*	$OpenBSD: ip_esp.c,v 1.69 2001/06/26 06:18:59 angelos Exp $ */
 
@@ -39,7 +39,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: xform_esp.c,v 1.28 2011/02/18 20:40:58 drochner Exp $");
+__KERNEL_RCSID(0, "$NetBSD: xform_esp.c,v 1.29 2011/02/19 18:26:50 degroote Exp $");
 
 #include "opt_inet.h"
 #ifdef __FreeBSD__
@@ -993,7 +993,7 @@ esp_output_cb(struct cryptop *crp)
 	/* Emulate man-in-the-middle attack when ipsec_integrity is TRUE. */
 	if (ipsec_integrity) {
 		static unsigned char ipseczeroes[AH_HMAC_HASHLEN];
-		struct auth_hash *esph;
+		const struct auth_hash *esph;
 
 		/*
 		 * Corrupt HMAC if we want to test integrity verification of
