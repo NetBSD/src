72f5be80a13a5678c85ec682de7876f01cbd10af
mrg <mrg>
2011-02-20 10:02:01 +0000

    remove the evcnt for NULL struct fpstate * ipi, and switch over to an explicit panic() if we get one.
    i'm pretty sure that converting xmpsg_lock from IPL_VM to IPL_SCHED
    fixed this problem properly.  i have not seen the evcnt trigger since
    that change, nor have several others queried.

--- sys/arch/sparc/sparc/locore.s
+++ sys/arch/sparc/sparc/locore.s
@@ -1,4 +1,4 @@
-/*	$NetBSD: locore.s,v 1.262 2011/02/15 11:30:21 mrg Exp $	*/
+/*	$NetBSD: locore.s,v 1.263 2011/02/20 10:02:01 mrg Exp $	*/
 
 /*
  * Copyright (c) 1996 Paul Kranenburg
@@ -5912,24 +5912,13 @@ Lfp_finish:
 	 std	%f30, [%o0 + FS_REGS + (4*30)]
 
 /*
- * We really should panic here but while we figure out what the bug is
- * that a remote CPU gets a NULL struct fpstate *, this lets the system
- * work at least seemingly stably.
+ * We got a NULL struct fpstate * on the IPI.  We panic.
  */
 Lfp_null_fpstate:
-#if 1
-	sethi	%hi(CPUINFO_VA), %o5
-	ldd	[%o5 + CPUINFO_SAVEFPSTATE_NULL], %o2
-	inccc   %o3
-	addx    %o2, 0, %o2
-	retl
-	 std	%o2, [%o5 + CPUINFO_SAVEFPSTATE_NULL]
-#else
 	ld	[%o5 + CPUINFO_CPUNO], %o1
 	sethi	%hi(Lpanic_savefpstate), %o0
 	call	_C_LABEL(panic)
 	 or	%o0, %lo(Lpanic_savefpstate), %o0
-#endif
 1:
 
 /*
