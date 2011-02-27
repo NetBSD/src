0d963eaa1dcfa9c7afeee4b4e6de95546476b0e2
jruoho <jruoho@NetBSD.org>
2011-02-27 17:10:33 +0000

    Move acpicpu(4) from "acpinodebus" to "cpufeaturebus".

--- sys/dev/acpi/acpi_cpu.c
+++ sys/dev/acpi/acpi_cpu.c
@@ -1,4 +1,4 @@
-/* $NetBSD: acpi_cpu.c,v 1.29 2011/02/25 19:55:06 jruoho Exp $ */
+/* $NetBSD: acpi_cpu.c,v 1.30 2011/02/27 17:10:33 jruoho Exp $ */
 
 /*-
  * Copyright (c) 2010, 2011 Jukka Ruohonen <jruohonen@iki.fi>
@@ -27,7 +27,7 @@
  * SUCH DAMAGE.
  */
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: acpi_cpu.c,v 1.29 2011/02/25 19:55:06 jruoho Exp $");
+__KERNEL_RCSID(0, "$NetBSD: acpi_cpu.c,v 1.30 2011/02/27 17:10:33 jruoho Exp $");
 
 #include <sys/param.h>
 #include <sys/cpu.h>
@@ -35,7 +35,6 @@ __KERNEL_RCSID(0, "$NetBSD: acpi_cpu.c,v 1.29 2011/02/25 19:55:06 jruoho Exp $")
 #include <sys/kmem.h>
 #include <sys/module.h>
 #include <sys/mutex.h>
-#include <sys/once.h>
 #include <sys/sysctl.h>
 
 #include <dev/acpi/acpireg.h>
@@ -43,6 +42,7 @@ __KERNEL_RCSID(0, "$NetBSD: acpi_cpu.c,v 1.29 2011/02/25 19:55:06 jruoho Exp $")
 #include <dev/acpi/acpi_cpu.h>
 
 #include <machine/acpi_machdep.h>
+#include <machine/cpuvar.h>
 
 #define _COMPONENT	  ACPI_BUS_COMPONENT
 ACPI_MODULE_NAME	  ("acpi_cpu")
@@ -52,14 +52,14 @@ static void		  acpicpu_attach(device_t, device_t, void *);
 static int		  acpicpu_detach(device_t, int);
 static int		  acpicpu_once_attach(void);
 static int		  acpicpu_once_detach(void);
-static void		  acpicpu_prestart(device_t);
 static void		  acpicpu_start(device_t);
-static void		  acpicpu_debug_print(struct acpicpu_softc *);
+static void		  acpicpu_debug_print(device_t);
 static const char	 *acpicpu_debug_print_dep(uint32_t);
 static void		  acpicpu_sysctl(device_t);
 
-static int		  acpicpu_object(ACPI_HANDLE, struct acpicpu_object *);
-static struct cpu_info	 *acpicpu_ci(uint32_t);
+static ACPI_STATUS	  acpicpu_object(ACPI_HANDLE, struct acpicpu_object *);
+static int		  acpicpu_find(struct cpu_info *,
+				       struct acpi_devnode **);
 static uint32_t		  acpicpu_cap(struct acpicpu_softc *);
 static ACPI_STATUS	  acpicpu_cap_pdc(struct acpicpu_softc *, uint32_t);
 static ACPI_STATUS	  acpicpu_cap_osc(struct acpicpu_softc *,
@@ -68,7 +68,7 @@ static void		  acpicpu_notify(ACPI_HANDLE, uint32_t, void *);
 static bool		  acpicpu_suspend(device_t, const pmf_qual_t *);
 static bool		  acpicpu_resume(device_t, const pmf_qual_t *);
 
-extern uint32_t		  acpi_cpus;
+static uint32_t		  acpicpu_count = 0;
 struct acpicpu_softc	**acpicpu_sc = NULL;
 static struct sysctllog	 *acpicpu_log = NULL;
 static bool		  acpicpu_dynamic = true;
@@ -85,89 +85,90 @@ CFATTACH_DECL_NEW(acpicpu, sizeof(struct acpicpu_softc),
 static int
 acpicpu_match(device_t parent, cfdata_t match, void *aux)
 {
-	struct acpi_attach_args *aa = aux;
-	struct acpicpu_object ao;
-	int rv;
+	struct cpufeature_attach_args *cfaa = aux;
 
-	if (aa->aa_node->ad_type != ACPI_TYPE_PROCESSOR)
+	if (acpi_softc == NULL)
 		return 0;
 
-	if (acpi_match_hid(aa->aa_node->ad_devinfo, acpicpu_hid) != 0)
-		return 1;
-
-	rv = acpicpu_object(aa->aa_node->ad_handle, &ao);
-
-	if (rv != 0 || acpicpu_ci(ao.ao_procid) == NULL)
+	if (strcmp(cfaa->name, "frequency") != 0)
 		return 0;
 
-	return 1;
+	return acpicpu_find(cfaa->ci, NULL);
 }
 
 static void
 acpicpu_attach(device_t parent, device_t self, void *aux)
 {
 	struct acpicpu_softc *sc = device_private(self);
-	struct acpi_attach_args *aa = aux;
-	static ONCE_DECL(once_attach);
+	struct cpufeature_attach_args *cfaa = aux;
+	struct cpu_info *ci = cfaa->ci;
 	cpuid_t id;
 	int rv;
 
-	rv = acpicpu_object(aa->aa_node->ad_handle, &sc->sc_object);
-
-	if (rv != 0)
-		return;
-
-	rv = RUN_ONCE(&once_attach, acpicpu_once_attach);
-
-	if (rv != 0)
-		return;
-
+	sc->sc_ci = ci;
 	sc->sc_dev = self;
 	sc->sc_cold = true;
-	sc->sc_node = aa->aa_node;
+	sc->sc_node = NULL;
 
-	sc->sc_ci = acpicpu_ci(sc->sc_object.ao_procid);
+	rv = acpicpu_find(ci, &sc->sc_node);
 
-	if (sc->sc_ci == NULL) {
-		aprint_error(": invalid CPU\n");
+	if (rv == 0) {
+		aprint_normal(": failed to match processor\n");
 		return;
 	}
+
+	if (acpicpu_once_attach() != 0) {
+		aprint_normal(": failed to initialize\n");
+		return;
+	}
+
+	KASSERT(acpi_softc != NULL);
+	KASSERT(acpicpu_sc != NULL);
+	KASSERT(sc->sc_node != NULL);
 
 	id = sc->sc_ci->ci_acpiid;
 
 	if (acpicpu_sc[id] != NULL) {
-		aprint_error(": already attached\n");
+		aprint_normal(": already attached\n");
 		return;
 	}
 
 	aprint_naive("\n");
 	aprint_normal(": ACPI CPU\n");
 
-	acpi_cpus++;
+	rv = acpicpu_object(sc->sc_node->ad_handle, &sc->sc_object);
+
+	if (ACPI_FAILURE(rv))
+		aprint_verbose_dev(self, "failed to obtain CPU object\n");
+
+	acpicpu_count++;
 	acpicpu_sc[id] = sc;
 
 	sc->sc_cap = acpicpu_cap(sc);
 	sc->sc_ncpus = acpi_md_ncpus();
-	sc->sc_flags |= acpicpu_md_flags();
+	sc->sc_flags = acpicpu_md_flags();
 
+	KASSERT(acpicpu_count <= sc->sc_ncpus);
+	KASSERT(sc->sc_node->ad_device == NULL);
+
+	sc->sc_node->ad_device = self;
 	mutex_init(&sc->sc_mtx, MUTEX_DEFAULT, IPL_NONE);
 
 	acpicpu_cstate_attach(self);
 	acpicpu_pstate_attach(self);
 	acpicpu_tstate_attach(self);
 
-	(void)config_defer(self, acpicpu_prestart);
+	acpicpu_debug_print(self);
+
+	(void)config_interrupts(self, acpicpu_start);
 	(void)acpi_register_notify(sc->sc_node, acpicpu_notify);
 	(void)pmf_device_register(self, acpicpu_suspend, acpicpu_resume);
-
-	acpicpu_debug_print(sc);
 }
 
 static int
 acpicpu_detach(device_t self, int flags)
 {
 	struct acpicpu_softc *sc = device_private(self);
-	static ONCE_DECL(once_detach);
 	int rv = 0;
 
 	sc->sc_cold = true;
@@ -191,13 +192,11 @@ acpicpu_detach(device_t self, int flags)
 	if (rv != 0)
 		return rv;
 
-	rv = RUN_ONCE(&once_detach, acpicpu_once_detach);
-
-	if (rv != 0)
-		return rv;
-
 	mutex_destroy(&sc->sc_mtx);
-	acpi_cpus--;
+	sc->sc_node->ad_device = NULL;
+
+	acpicpu_count--;
+	acpicpu_once_detach();
 
 	return 0;
 }
@@ -207,6 +206,12 @@ acpicpu_once_attach(void)
 {
 	struct acpicpu_softc *sc;
 	unsigned int i;
+
+	if (acpicpu_count != 0)
+		return 0;
+
+	KASSERT(acpicpu_sc == NULL);
+	KASSERT(acpicpu_log == NULL);
 
 	acpicpu_sc = kmem_zalloc(maxcpus * sizeof(*sc), KM_SLEEP);
 
@@ -224,44 +229,42 @@ acpicpu_once_detach(void)
 {
 	struct acpicpu_softc *sc;
 
-	if (acpicpu_sc != NULL)
-		kmem_free(acpicpu_sc, maxcpus * sizeof(*sc));
+	if (acpicpu_count != 0)
+		return EDEADLK;
 
 	if (acpicpu_log != NULL)
 		sysctl_teardown(&acpicpu_log);
 
+	if (acpicpu_sc != NULL)
+		kmem_free(acpicpu_sc, maxcpus * sizeof(*sc));
+
 	return 0;
-}
-
-static void
-acpicpu_prestart(device_t self)
-{
-	struct acpicpu_softc *sc = device_private(self);
-	static bool once = false;
-
-	if (once != false) {
-		sc->sc_cold = false;
-		return;
-	}
-
-	once = true;
-
-	(void)config_interrupts(self, acpicpu_start);
 }
 
 static void
 acpicpu_start(device_t self)
 {
 	struct acpicpu_softc *sc = device_private(self);
+	static uint32_t count = 0;
 
 	/*
-	 * Run the state-specific initialization
-	 * routines. These should be called only
-	 * once, after interrupts are enabled and
-	 * all ACPI CPUs have attached.
+	 * Run the state-specific initialization routines. These
+	 * must run only once, after interrupts have been enabled,
+	 * all CPUs are running, and all ACPI CPUs have attached.
+	 */
+	if (++count != acpicpu_count || acpicpu_count != sc->sc_ncpus) {
+		sc->sc_cold = false;
+		return;
+	}
+
+	/*
+	 * Set the last ACPI CPU as non-cold
+	 * only after C-states are enabled.
 	 */
 	if ((sc->sc_flags & ACPICPU_FLAG_C) != 0)
 		acpicpu_cstate_start(self);
+
+	sc->sc_cold = false;
 
 	if ((sc->sc_flags & ACPICPU_FLAG_P) != 0)
 		acpicpu_pstate_start(self);
@@ -270,11 +273,7 @@ acpicpu_start(device_t self)
 		acpicpu_tstate_start(self);
 
 	acpicpu_sysctl(self);
-
-	aprint_debug_dev(sc->sc_dev, "ACPI CPUs started (cap "
-	    "0x%02x, flags 0x%06x)\n", sc->sc_cap, sc->sc_flags);
-
-	sc->sc_cold = false;
+	aprint_debug_dev(self, "ACPI CPUs started\n");
 }
 
 static void
@@ -282,6 +281,8 @@ acpicpu_sysctl(device_t self)
 {
 	const struct sysctlnode *node;
 	int err;
+
+	KASSERT(acpicpu_log == NULL);
 
 	err = sysctl_createv(&acpicpu_log, 0, NULL, &node,
 	    CTLFLAG_PERMANENT, CTLTYPE_NODE, "hw", NULL,
@@ -326,7 +327,7 @@ fail:
 	aprint_error_dev(self, "failed to initialize sysctl (err %d)\n", err);
 }
 
-static int
+static ACPI_STATUS
 acpicpu_object(ACPI_HANDLE hdl, struct acpicpu_object *ao)
 {
 	ACPI_OBJECT *obj;
@@ -336,7 +337,7 @@ acpicpu_object(ACPI_HANDLE hdl, struct acpicpu_object *ao)
 	rv = acpi_eval_struct(hdl, NULL, &buf);
 
 	if (ACPI_FAILURE(rv))
-		return 1;
+		goto out;
 
 	obj = buf.Pointer;
 
@@ -362,22 +363,53 @@ out:
 	if (buf.Pointer != NULL)
 		ACPI_FREE(buf.Pointer);
 
-	return ACPI_FAILURE(rv) ? 1 : 0;
+	return rv;
 }
 
-static struct cpu_info *
-acpicpu_ci(uint32_t id)
+static int
+acpicpu_find(struct cpu_info *ci, struct acpi_devnode **ptr)
 {
-	CPU_INFO_ITERATOR cii;
-	struct cpu_info *ci;
+	struct acpi_softc *sc = acpi_softc;
+	struct acpicpu_object ao;
+	struct acpi_devnode *ad;
+	ACPI_INTEGER val;
+	ACPI_STATUS rv;
 
-	for (CPU_INFO_FOREACH(cii, ci)) {
+	if (sc == NULL || acpi_active == 0)
+		return 0;
 
-		if (id == ci->ci_acpiid)
-			return ci;
+	/*
+	 * CPUs are declared in the ACPI namespace
+	 * either as a Processor() or as a Device().
+	 * In both cases the MADT entries are used
+	 * for the match (see ACPI 4.0, section 8.4).
+	 */
+	SIMPLEQ_FOREACH(ad, &sc->ad_head, ad_list) {
+
+		if (ad->ad_type == ACPI_TYPE_PROCESSOR) {
+
+			rv = acpicpu_object(ad->ad_handle, &ao);
+
+			if (ACPI_SUCCESS(rv) && ci->ci_acpiid == ao.ao_procid)
+				goto out;
+		}
+
+		if (acpi_match_hid(ad->ad_devinfo, acpicpu_hid) != 0) {
+
+			rv = acpi_eval_integer(ad->ad_handle, "_UID", &val);
+
+			if (ACPI_SUCCESS(rv) && ci->ci_acpiid == val)
+				goto out;
+		}
 	}
 
-	return NULL;
+	return 0;
+
+out:
+	if (ptr != NULL)
+		*ptr = ad;
+
+	return 10;		/* Beat est(4) and powernow(4). */
 }
 
 static uint32_t
@@ -638,9 +670,15 @@ acpicpu_resume(device_t self, const pmf_qual_t *qual)
 }
 
 static void
-acpicpu_debug_print(struct acpicpu_softc *sc)
+acpicpu_debug_print(device_t self)
 {
+	struct acpicpu_softc *sc = device_private(self);
+	struct cpu_info *ci = sc->sc_ci;
 	struct acpicpu_dep *dep;
+
+	aprint_debug_dev(sc->sc_dev, "id %u, lapic id %u, "
+	    "cap 0x%04x, flags 0x%08x\n", ci->ci_acpiid,
+	    (uint32_t)ci->ci_cpuid, sc->sc_cap, sc->sc_flags);
 
 	if ((sc->sc_flags & ACPICPU_FLAG_C_DEP) != 0) {
 
