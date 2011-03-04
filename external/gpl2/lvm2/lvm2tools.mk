40e072211555a365d11043a65ae9b4fbfb56ba05
matt <matt@NetBSD.org>
2011-03-04 22:48:29 +0000

    Use static libdm so dmsetup and lvm can be linked dynamically. Change LIBDM_* to LIBDEVMAPPER_* so LIBDM can refer to libdm.

--- external/gpl2/lvm2/lvm2tools.mk
+++ external/gpl2/lvm2/lvm2tools.mk
@@ -1,20 +1,22 @@
-#	$NetBSD: lvm2tools.mk,v 1.3 2011/01/05 14:57:27 haad Exp $
+#	$NetBSD: lvm2tools.mk,v 1.4 2011/03/04 22:48:29 matt Exp $
 
 .include <bsd.own.mk>
 
 LVM2_SRCDIR=	${NETBSDSRCDIR}/external/gpl2/lvm2
 LVM2_DISTDIR=	${NETBSDSRCDIR}/external/gpl2/lvm2/dist
 
-LIBDM_SRCDIR=		${NETBSDSRCDIR}/external/gpl2/lvm2/lib/libdevmapper
-LIBDM_DISTDIR=		${NETBSDSRCDIR}/external/gpl2/lvm2/dist/libdm
-LIBDM_INCLUDE=		${NETBSDSRCDIR}/external/gpl2/lvm2/dist/include
+LIBDEVMAPPER_SRCDIR=		${LVM2_SRCDIR}/lib/libdevmapper
+LIBDEVMAPPER_DISTDIR=		${LVM2_DISTDIR}/libdm
+LIBDEVMAPPER_INCLUDE=		${LVM2_DISTDIR}/include
+
+LIBDM_SRCDIR=			${NETBSDSRCDIR}/lib/libdm
 
 # root:operator [cb]rw-r-----
 CPPFLAGS+=-DDM_DEVICE_UID=0 -DDM_DEVICE_GID=5 -DDM_DEVICE_MODE=0640 \
 	  -DDM_CONTROL_DEVICE_MODE=0660 -DLVM_LOCKDIR_MODE=0770
 
 #
-#LIBDM_OBJDIR.libdevmapper=${LIBDM_SRCDIR}/lib/libdevmapper/
+#LIBDEVMAPPER_OBJDIR.libdevmapper=${LIBDEVMAPPER_SRCDIR}/lib/libdevmapper/
 #
 #.if !defined(LVM2OBJDIR.liblvm)
 #LVM2OBJDIR.liblvm!=	cd ${LVM2_SRCDIR}/lib/liblvm && ${PRINTOBJDIR}
