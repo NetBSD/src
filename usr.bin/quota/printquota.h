e03ec6e9c831686704d2d4f92d84b2b4d8ecae9c
christos <christos@NetBSD.org>
2011-03-06 22:36:07 +0000

    - merge more code. - simplify struct access.

--- usr.bin/quota/printquota.h
+++ usr.bin/quota/printquota.h
@@ -1,10 +1,7 @@
-/*	$NetBSD: printquota.h,v 1.3 2011/03/06 20:47:59 christos Exp $ */
+/*	$NetBSD: printquota.h,v 1.4 2011/03/06 22:36:07 christos Exp $ */
 
 const char *intprt(char *, size_t, uint64_t, int, int);
 const char *timeprt(char *, size_t, time_t, time_t);
-#if 0
-const char *timepprt(time_t, int, int);
+const char *timepprt(char *, size_t, time_t, int);
 int timeprd(const char *, time_t *);
 int intrd(char *str, uint64_t *val, u_int);
-#endif
-
