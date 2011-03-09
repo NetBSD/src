2c591d6c2ae9cf2a8956b9467cf167ad88fd6d3d
pooka <pooka@NetBSD.org>
2011-03-09 19:02:04 +0000

    Bump version for new quota code -- even if ABIs didn't change, it's still a major feature.

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.384 2011/02/25 13:29:46 joerg Exp $	*/
+/*	$NetBSD: param.h,v 1.385 2011/03/09 19:02:04 pooka Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -63,7 +63,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	599004700	/* NetBSD 5.99.47 */
+#define	__NetBSD_Version__	599004800	/* NetBSD 5.99.48 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
