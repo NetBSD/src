45269e9ed8f86da009cab774379844004e30c895
joerg <joerg@NetBSD.org>
2011-03-12 22:54:36 +0000

    Add TLS support for AMD64, i386 and SH3.
    This material is based upon work partially supported by
    The NetBSD Foundation under a contract with Joerg Sonnenberger.

--- sys/arch/i386/include/types.h
+++ sys/arch/i386/include/types.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: types.h,v 1.71 2011/02/24 04:28:46 joerg Exp $	*/
+/*	$NetBSD: types.h,v 1.72 2011/03/12 22:54:37 joerg Exp $	*/
 
 /*-
  * Copyright (c) 1990 The Regents of the University of California.
@@ -113,6 +113,8 @@ typedef	volatile unsigned char		__cpu_simple_lock_t;
 #define	__HAVE_CPU_LWP_SETPRIVATE
 #define	__HAVE_INTR_CONTROL
 #define	__HAVE___LWP_GETPRIVATE_FAST
+#define	__HAVE_TLS_VARIANT_II
+#define	__HAVE_COMMON___TLS_GET_ADDR
 
 #if defined(_KERNEL)
 #define	__HAVE_RAS
