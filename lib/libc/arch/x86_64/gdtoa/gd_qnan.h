9f47492a6f9ea35624b2276393bc647dee3165b9
christos <christos@NetBSD.org>
2011-03-20 23:16:07 +0000

    Add ld formats for the unit-tests.

--- lib/libc/arch/x86_64/gdtoa/gd_qnan.h
+++ lib/libc/arch/x86_64/gdtoa/gd_qnan.h
@@ -1,8 +1,12 @@
-/* $NetBSD: gd_qnan.h,v 1.1 2006/01/25 15:33:28 kleink Exp $ */
+/* $NetBSD: gd_qnan.h,v 1.2 2011/03/20 23:16:07 christos Exp $ */
 
 #define f_QNAN 0x7fc00000
 #define d_QNAN0 0x0
 #define d_QNAN1 0x7ff80000
+#define ld_QNAN0 0x0
+#define ld_QNAN1 0xc0000000
+#define ld_QNAN2 0x7fff
+#define ld_QNAN3 0x0
 #define ldus_QNAN0 0x0
 #define ldus_QNAN1 0x0
 #define ldus_QNAN2 0x0
