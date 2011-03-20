f0943ebc81ea2ba1fd5c9a01ef7ff69783702fe8
tsutsui <tsutsui@NetBSD.org>
2011-03-20 17:38:11 +0000

    Move MALLOC_DEFINE()s from usb_mem.c to usb_subr.c. usb_mem.c provides functions for DMA memory allocation that is required by DMA capable host controllers only, but MALLOC_DEFINE() isn't DMA specific and could be required by any USB drivers.
    Discussed on current-users.

--- sys/dev/usb/usb_subr.c
+++ sys/dev/usb/usb_subr.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: usb_subr.c,v 1.177 2010/11/03 22:34:24 dyoung Exp $	*/
+/*	$NetBSD: usb_subr.c,v 1.178 2011/03/20 17:38:11 tsutsui Exp $	*/
 /*	$FreeBSD: src/sys/dev/usb/usb_subr.c,v 1.18 1999/11/17 22:33:47 n_hibma Exp $	*/
 
 /*
@@ -32,7 +32,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: usb_subr.c,v 1.177 2010/11/03 22:34:24 dyoung Exp $");
+__KERNEL_RCSID(0, "$NetBSD: usb_subr.c,v 1.178 2011/03/20 17:38:11 tsutsui Exp $");
 
 #include "opt_compat_netbsd.h"
 #include "opt_usbverbose.h"
@@ -67,6 +67,10 @@ extern int usbdebug;
 #define DPRINTF(x)
 #define DPRINTFN(n,x)
 #endif
+
+MALLOC_DEFINE(M_USB, "USB", "USB misc. memory");
+MALLOC_DEFINE(M_USBDEV, "USB device", "USB device driver");
+MALLOC_DEFINE(M_USBHC, "USB HC", "USB host controller");
 
 Static usbd_status usbd_set_config(usbd_device_handle, int);
 Static void usbd_devinfo(usbd_device_handle, int, char *, size_t);
