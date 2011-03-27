22699d746720fba2d6489d6a6222e7f4f30903d9
martin <martin@NetBSD.org>
2011-03-27 18:47:08 +0000

    On second thought do not use bit 13 as a flag for syscall numbers, userland typically sets those from a 13bit signed integer immediate field in the instruction, so would need to jump through hoops (ok, small hoops) to avoid sign extension. Use a combination of the existing syscall flags instead.

--- sys/arch/sparc64/include/trap.h
+++ sys/arch/sparc64/include/trap.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: trap.h,v 1.8 2011/03/23 20:41:31 martin Exp $ */
+/*	$NetBSD: trap.h,v 1.9 2011/03/27 18:47:09 martin Exp $ */
 
 /*
  * Copyright (c) 1996-1999 Eduardo Horvath
@@ -142,7 +142,7 @@
 /* flags to system call (flags in %g1 along with syscall number) */
 #define	SYSCALL_G2RFLAG	0x400	/* on success, return to %g2 rather than npc */
 #define	SYSCALL_G7RFLAG	0x800	/* use %g7 as above (deprecated) */
-#define	SYSCALL_G5RFLAG	0x1000	/* use %g5 as above (only ABI compatible way) */
+#define	SYSCALL_G5RFLAG	0xc00	/* use %g5 as above (only ABI compatible way) */
 
 /*
  * `software trap' macros to keep people happy (sparc v8 manual says not
