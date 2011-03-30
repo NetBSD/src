be9d27a73ec0c19e8dab7f58ccdfeb443c20fb72
jym <jym@NetBSD.org>
2011-03-30 22:57:24 +0000

    Add the HYPERVISOR_sysctl() hypercall.
    Although the hypercall arguments (like struct sysctl_readconsole) are not
    compatible between different XEN_SYSCTL_INTERFACE_VERSIONs (one of the
    reasons why the sysctl calls should only be used by xentools directly),
    it's still practical to have when one wants to query Xen's dmesg from
    ddb(4) in case of a panic.
    
    Note: additional code is needed for readconsole() functionality, but adding
    the hypercall should not cause any harm.

--- sys/arch/xen/include/amd64/hypercalls.h
+++ sys/arch/xen/include/amd64/hypercalls.h
@@ -1,4 +1,4 @@
-/* $NetBSD: hypercalls.h,v 1.5 2008/11/13 01:45:48 cegger Exp $ */
+/* $NetBSD: hypercalls.h,v 1.6 2011/03/30 22:57:24 jym Exp $ */
 /******************************************************************************
  * hypercall.h
  * 
@@ -404,6 +404,12 @@ HYPERVISOR_machine_check(struct xen_mc *mc)
 {
 	mc->interface_version = XEN_MCA_INTERFACE_VERSION;
 	return _hypercall1(int, mca, mc);
+}
+
+static inline int
+HYPERVISOR_sysctl(void *sysctl)
+{
+	return _hypercall1(int, sysctl, sysctl);
 }
 
 #endif /* __HYPERCALL_H__ */
