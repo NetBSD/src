08f3fa17e9f58e84d668be6b2f1e9fb0c36d35d4
jruoho <jruoho>
2011-03-30 17:03:02 +0000

    Add missing __RCSID(3), again.

--- tests/lib/libutil/t_efun.c
+++ tests/lib/libutil/t_efun.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: t_efun.c,v 1.1 2011/03/30 17:02:17 jruoho Exp $ */
+/*	$NetBSD: t_efun.c,v 1.2 2011/03/30 17:03:02 jruoho Exp $ */
 
 /*-
  * Copyright (c) 2011 The NetBSD Foundation, Inc.
@@ -28,6 +28,8 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
+#include <sys/cdefs.h>
+__RCSID("$NetBSD: t_efun.c,v 1.2 2011/03/30 17:03:02 jruoho Exp $");
 
 #include <atf-c.h>
 
