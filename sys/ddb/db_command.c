a3752b7aa6fccbee39643f1a0b71d4ccb6b4ee8c
christos <christos@NetBSD.org>
2011-04-10 20:59:22 +0000

    Add: usage: show proc [/a] [/p] address|pid         /a == argument is an address of any lwp 	/p == argument is a pid [default] From: Vladimir Kirillov proger at wilab dot org dot ua

--- sys/ddb/db_command.c
+++ sys/ddb/db_command.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: db_command.c,v 1.134 2010/09/13 08:42:04 drochner Exp $	*/
+/*	$NetBSD: db_command.c,v 1.135 2011/04/10 20:59:22 christos Exp $	*/
 
 /*
  * Copyright (c) 1996, 1997, 1998, 1999, 2002, 2009 The NetBSD Foundation, Inc.
@@ -60,7 +60,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: db_command.c,v 1.134 2010/09/13 08:42:04 drochner Exp $");
+__KERNEL_RCSID(0, "$NetBSD: db_command.c,v 1.135 2011/04/10 20:59:22 christos Exp $");
 
 #ifdef _KERNEL_OPT
 #include "opt_aio.h"
@@ -219,10 +219,12 @@ static const struct db_command db_show_cmds[] = {
 #endif
 	{ DDB_ADD_CMD("pages",	db_show_all_pages,
 	    0 ,"List all used memory pages.",NULL,NULL) },
+	{ DDB_ADD_CMD("proc",	db_show_proc,
+	    0 ,"Print process information.",NULL,NULL) },
 	{ DDB_ADD_CMD("procs",	db_show_all_procs,
 	    0 ,"List all processes.",NULL,NULL) },
 	{ DDB_ADD_CMD("pools",	db_show_all_pools,
-	    0 ,"Show all poolS",NULL,NULL) },
+	    0 ,"Show all pools",NULL,NULL) },
 #ifdef AIO
 	/*added from all sub cmds*/
 	{ DDB_ADD_CMD("aio_jobs",	db_show_aio_jobs,	0,
