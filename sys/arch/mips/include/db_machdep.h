793f8215b215b360296da3d8a98d79e71d0b9c38
cliff <cliff@NetBSD.org>
2011-04-14 05:07:56 +0000

    - remove include <mips/proc.h>, unused - db_mach_watch_set_all() is deprecated, removed, superceded by cpuwatch_set_all()

--- sys/arch/mips/include/db_machdep.h
+++ sys/arch/mips/include/db_machdep.h
@@ -1,4 +1,4 @@
-/* $NetBSD: db_machdep.h,v 1.25 2011/04/06 05:53:27 matt Exp $ */
+/* $NetBSD: db_machdep.h,v 1.26 2011/04/14 05:07:56 cliff Exp $ */
 
 /*
  * Copyright (c) 1997 Jonathan Stone (hereinafter referred to as the author)
@@ -38,7 +38,6 @@
 #include <mips/trap.h>			/* T_BREAK */
 #include <mips/reg.h>			/* register state */
 #include <mips/regnum.h>		/* symbolic register indices */
-#include <mips/proc.h>
 #include <mips/pcb.h>
 
 
@@ -117,9 +116,6 @@ db_addr_t next_instr_address(db_addr_t pc, bool bd);
 bool ddb_running_on_this_cpu_p(void);
 bool ddb_running_on_any_cpu_p(void);
 void db_resume_others(void);
-#ifdef MIPS_DDB_WATCH
-void db_mach_watch_set_all(void);
-#endif
 
 /*
  * We have machine-dependent commands.
