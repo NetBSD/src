55f874b42553c079132acd1e9804e0709a6a608f
drochner <drochner@NetBSD.org>
2011-04-18 15:59:09 +0000

    according to C99/POSIX, nextafter(x,y) should return y if x==y, from Henning Petersen per PR lib/44875

--- lib/libm/src/s_nextafter.c
+++ lib/libm/src/s_nextafter.c
@@ -12,7 +12,7 @@
 
 #include <sys/cdefs.h>
 #if defined(LIBM_SCCS) && !defined(lint)
-__RCSID("$NetBSD: s_nextafter.c,v 1.11 2002/05/26 22:01:57 wiz Exp $");
+__RCSID("$NetBSD: s_nextafter.c,v 1.12 2011/04/18 15:59:09 drochner Exp $");
 #endif
 
 /* IEEE functions
@@ -39,7 +39,7 @@ nextafter(double x, double y)
 	if(((ix>=0x7ff00000)&&((ix-0x7ff00000)|lx)!=0) ||   /* x is nan */
 	   ((iy>=0x7ff00000)&&((iy-0x7ff00000)|ly)!=0))     /* y is nan */
 	   return x+y;
-	if(x==y) return x;		/* x=y, return x */
+	if(x==y) return y;		/* x=y, return y */
 	if((ix|lx)==0) {			/* x == 0 */
 	    INSERT_WORDS(x,hy&0x80000000,1);	/* return +-minsubnormal */
 	    y = x*x;
