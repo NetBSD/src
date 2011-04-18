7cf28efb087961c48a380ff49a20054d2c1a0d94
joerg <joerg@NetBSD.org>
2011-04-18 03:27:40 +0000

    Include nbtool_config.h for tool builds.

--- usr.bin/grep/file.c
+++ usr.bin/grep/file.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: file.c,v 1.5 2011/02/16 18:35:39 joerg Exp $	*/
+/*	$NetBSD: file.c,v 1.6 2011/04/18 03:27:40 joerg Exp $	*/
 /*	$FreeBSD: head/usr.bin/grep/file.c 211496 2010-08-19 09:28:59Z des $	*/
 /*	$OpenBSD: file.c,v 1.11 2010/07/02 20:48:48 nicm Exp $	*/
 
@@ -30,8 +30,12 @@
  * SUCH DAMAGE.
  */
 
+#if HAVE_NBTOOL_CONFIG_H
+#include "nbtool_config.h"
+#endif
+
 #include <sys/cdefs.h>
-__RCSID("$NetBSD: file.c,v 1.5 2011/02/16 18:35:39 joerg Exp $");
+__RCSID("$NetBSD: file.c,v 1.6 2011/04/18 03:27:40 joerg Exp $");
 
 #include <sys/param.h>
 #include <sys/types.h>
