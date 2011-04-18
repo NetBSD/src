f90dcde195605fe8d0754dab3129ea4a13ddc91b
dholland <dholland@NetBSD.org>
2011-04-18 00:43:56 +0000

    Regen for ISSYMLINK removal.

--- sys/rump/include/rump/rump_namei.h
+++ sys/rump/include/rump/rump_namei.h
@@ -1,11 +1,11 @@
-/*	$NetBSD: rump_namei.h,v 1.14 2011/01/07 11:25:43 pooka Exp $	*/
+/*	$NetBSD: rump_namei.h,v 1.15 2011/04/18 00:43:56 dholland Exp $	*/
 
 
 /*
  * WARNING: GENERATED FILE.  DO NOT EDIT
  * (edit namei.src and run make namei in src/sys/sys)
  *   by:   NetBSD: gennameih.awk,v 1.5 2009/12/23 14:17:19 pooka Exp 
- *   from: NetBSD: namei.src,v 1.22 2011/01/07 11:25:10 pooka Exp 
+ *   from: NetBSD: namei.src,v 1.23 2011/04/18 00:40:54 dholland Exp 
  */
 
 #ifndef _RUMP_RUMP_NAMEI_H_
@@ -30,13 +30,12 @@
 #define RUMP_NAMEI_ISDOTDOT	0x0002000
 #define RUMP_NAMEI_MAKEENTRY	0x0004000
 #define RUMP_NAMEI_ISLASTCN	0x0008000
-#define RUMP_NAMEI_ISSYMLINK	0x0010000
 #define RUMP_NAMEI_ISWHITEOUT	0x0020000
 #define RUMP_NAMEI_DOWHITEOUT	0x0040000
 #define RUMP_NAMEI_REQUIREDIR	0x0080000
 #define RUMP_NAMEI_CREATEDIR	0x0200000
 #define RUMP_NAMEI_INRENAME	0x0400000
 #define RUMP_NAMEI_INRELOOKUP	0x0800000
-#define RUMP_NAMEI_PARAMASK	0x0efe300
+#define RUMP_NAMEI_PARAMASK	0x0eee300
 
 #endif /* _RUMP_RUMP_NAMEI_H_ */
