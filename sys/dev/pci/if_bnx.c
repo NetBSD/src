a30b258c9ce390170589709cdf057fbad4c6453e
jym <jym@NetBSD.org>
2011-05-02 09:03:10 +0000

    Fix BCM5709 PHY detection for ethernet PHYs (the SerDes case being already handled): - export bge(4) and bnx(4) CHIP ID and PHY flags to brgphy(4). Move to "unsigned int" rather than "int", and reuse the same softc members for chipid and phyflags (behavior controlled by the sc_isbge/isbnx boolean). - apply bug fix for revisions A and B, so that autonegotiation can complete (from OpenBSD).
    Bug reported by Rivo Nurges via private mail, patch tested and
    confirmed working by him (with thanks!)

--- sys/dev/pci/if_bnx.c
+++ sys/dev/pci/if_bnx.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: if_bnx.c,v 1.42 2011/01/26 00:09:27 dyoung Exp $	*/
+/*	$NetBSD: if_bnx.c,v 1.43 2011/05/02 09:03:10 jym Exp $	*/
 /*	$OpenBSD: if_bnx.c,v 1.85 2009/11/09 14:32:41 dlg Exp $ */
 
 /*-
@@ -35,7 +35,7 @@
 #if 0
 __FBSDID("$FreeBSD: src/sys/dev/bce/if_bce.c,v 1.3 2006/04/13 14:12:26 ru Exp $");
 #endif
-__KERNEL_RCSID(0, "$NetBSD: if_bnx.c,v 1.42 2011/01/26 00:09:27 dyoung Exp $");
+__KERNEL_RCSID(0, "$NetBSD: if_bnx.c,v 1.43 2011/05/02 09:03:10 jym Exp $");
 
 /*
  * The following controllers are supported by this driver:
@@ -717,9 +717,10 @@ bnx_attach(device_t parent, device_t self, void *aux)
 	ifmedia_init(&sc->bnx_mii.mii_media, 0, ether_mediachange,
 	    ether_mediastatus);
 
-	/* set phyflags before mii_attach() */
+	/* set phyflags and chipid before mii_attach() */
 	dict = device_properties(self);
 	prop_dictionary_set_uint32(dict, "phyflags", sc->bnx_phy_flags);
+	prop_dictionary_set_uint32(dict, "chipid", sc->bnx_chipid);
 
 	if (sc->bnx_phy_flags & BNX_PHY_SERDES_FLAG)
 		mii_flags |= MIIF_HAVEFIBER;
