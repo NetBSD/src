98f862fb44bfcc212039ffd6aa625893be666ea1
drochner <drochner@NetBSD.org>
2011-05-05 17:44:39 +0000

    support camellia-cbc by swcrypt

--- sys/opencrypto/cryptosoft_xform.c
+++ sys/opencrypto/cryptosoft_xform.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: cryptosoft_xform.c,v 1.14 2011/02/24 20:03:41 drochner Exp $ */
+/*	$NetBSD: cryptosoft_xform.c,v 1.15 2011/05/05 17:44:39 drochner Exp $ */
 /*	$FreeBSD: src/sys/opencrypto/xform.c,v 1.1.2.1 2002/11/21 23:34:23 sam Exp $	*/
 /*	$OpenBSD: xform.c,v 1.19 2002/08/16 22:47:25 dhartmei Exp $	*/
 
@@ -40,13 +40,14 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(1, "$NetBSD: cryptosoft_xform.c,v 1.14 2011/02/24 20:03:41 drochner Exp $");
+__KERNEL_RCSID(1, "$NetBSD: cryptosoft_xform.c,v 1.15 2011/05/05 17:44:39 drochner Exp $");
 
 #include <crypto/blowfish/blowfish.h>
 #include <crypto/cast128/cast128.h>
 #include <crypto/des/des.h>
 #include <crypto/rijndael/rijndael.h>
 #include <crypto/skipjack/skipjack.h>
+#include <crypto/camellia/camellia.h>
 
 #include <opencrypto/deflate.h>
 
@@ -86,24 +87,28 @@ static	int blf_setkey(u_int8_t **, const u_int8_t *, int);
 static	int cast5_setkey(u_int8_t **, const u_int8_t *, int);
 static  int skipjack_setkey(u_int8_t **, const u_int8_t *, int);
 static  int rijndael128_setkey(u_int8_t **, const u_int8_t *, int);
+static  int cml_setkey(u_int8_t **, const u_int8_t *, int);
 static	void des1_encrypt(void *, u_int8_t *);
 static	void des3_encrypt(void *, u_int8_t *);
 static	void blf_encrypt(void *, u_int8_t *);
 static	void cast5_encrypt(void *, u_int8_t *);
 static	void skipjack_encrypt(void *, u_int8_t *);
 static	void rijndael128_encrypt(void *, u_int8_t *);
+static  void cml_encrypt(void *, u_int8_t *);
 static	void des1_decrypt(void *, u_int8_t *);
 static	void des3_decrypt(void *, u_int8_t *);
 static	void blf_decrypt(void *, u_int8_t *);
 static	void cast5_decrypt(void *, u_int8_t *);
 static	void skipjack_decrypt(void *, u_int8_t *);
 static	void rijndael128_decrypt(void *, u_int8_t *);
+static  void cml_decrypt(void *, u_int8_t *);
 static	void des1_zerokey(u_int8_t **);
 static	void des3_zerokey(u_int8_t **);
 static	void blf_zerokey(u_int8_t **);
 static	void cast5_zerokey(u_int8_t **);
 static	void skipjack_zerokey(u_int8_t **);
 static	void rijndael128_zerokey(u_int8_t **);
+static  void cml_zerokey(u_int8_t **);
 
 static	void null_init(void *);
 static	int null_update(void *, const u_int8_t *, u_int16_t);
@@ -191,6 +196,14 @@ static const struct swcr_enc_xform swcr_enc_xform_arc4 = {
 	NULL,
 	NULL,
 	NULL,
+};
+
+static const struct swcr_enc_xform swcr_enc_xform_camellia = {
+	&enc_xform_camellia,
+	cml_encrypt,
+	cml_decrypt,
+	cml_setkey,
+	cml_zerokey
 };
 
 /* Authentication instances */
@@ -556,6 +569,46 @@ static void
 rijndael128_zerokey(u_int8_t **sched)
 {
 	memset(*sched, 0, sizeof(rijndael_ctx));
+	free(*sched, M_CRYPTO_DATA);
+	*sched = NULL;
+}
+
+static void
+cml_encrypt(void *key, u_int8_t *blk)
+{
+
+	camellia_encrypt(key, blk, blk);
+}
+
+static void
+cml_decrypt(void *key, u_int8_t *blk)
+{
+
+	camellia_decrypt(key, blk, blk);
+}
+
+static int
+cml_setkey(u_int8_t **sched, const u_int8_t *key, int len)
+{
+	int err;
+
+	if (len != 16 && len != 24 && len != 32)
+		return (EINVAL);
+	*sched = malloc(sizeof(camellia_ctx), M_CRYPTO_DATA,
+			M_NOWAIT|M_ZERO);
+	if (*sched != NULL) {
+		camellia_set_key((camellia_ctx *) *sched, key, len * 8);
+		err = 0;
+	} else
+		err = ENOMEM;
+	return err;
+}
+
+static void
+cml_zerokey(u_int8_t **sched)
+{
+
+	memset(*sched, 0, sizeof(camellia_ctx));
 	free(*sched, M_CRYPTO_DATA);
 	*sched = NULL;
 }
