e72f92d552a47a5568b4637dbd986a651dbbfc9d
msaitoh <msaitoh@NetBSD.org>
2011-05-15 01:00:52 +0000

    Add support for Intel 6 series.

--- sys/dev/pci/ichsmb.c
+++ sys/dev/pci/ichsmb.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: ichsmb.c,v 1.24 2011/02/10 13:52:36 hannken Exp $	*/
+/*	$NetBSD: ichsmb.c,v 1.25 2011/05/15 01:00:52 msaitoh Exp $	*/
 /*	$OpenBSD: ichiic.c,v 1.18 2007/05/03 09:36:26 dlg Exp $	*/
 
 /*
@@ -22,7 +22,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: ichsmb.c,v 1.24 2011/02/10 13:52:36 hannken Exp $");
+__KERNEL_RCSID(0, "$NetBSD: ichsmb.c,v 1.25 2011/05/15 01:00:52 msaitoh Exp $");
 
 #include <sys/param.h>
 #include <sys/device.h>
@@ -107,6 +107,7 @@ ichsmb_match(device_t parent, cfdata_t match, void *aux)
 		case PCI_PRODUCT_INTEL_82801JD_SMB:
 		case PCI_PRODUCT_INTEL_82801JI_SMB:
 		case PCI_PRODUCT_INTEL_3400_SMB:
+		case PCI_PRODUCT_INTEL_6SERIES_SMB:
 			return 1;
 		}
 	}
