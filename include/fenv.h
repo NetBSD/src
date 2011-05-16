2b6b29f8cb73677de5f500a950fdbc97a78d9e2b
he <he@NetBSD.org>
2011-05-16 07:59:18 +0000

    The support of <fenv.h> is actually for __sparc64__, not for __sparc__ (which is also defined on sparc64).  This fixes a build problem for plain sparc.

--- include/fenv.h
+++ include/fenv.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: fenv.h,v 1.3 2011/01/31 00:19:33 christos Exp $	*/
+/*	$NetBSD: fenv.h,v 1.4 2011/05/16 07:59:19 he Exp $	*/
 /*
  * Copyright (c) 2010 The NetBSD Foundation, Inc.
  * All rights reserved.
@@ -25,7 +25,7 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
-#if !defined(__i386__) && !defined(__amd64__) && !defined(__sparc__)
+#if !defined(__i386__) && !defined(__amd64__) && !defined(__sparc64__)
 #error	"fenv.h is currently not supported for this architecture"
 #endif
 
