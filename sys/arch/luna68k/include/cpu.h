effab97ee041c694074f59399f70fd4d1edfa2a2
tsutsui <tsutsui@NetBSD.org>
2011-05-16 13:22:51 +0000

    - merge and move pasted m68k MD setreg() functions into m68k/m68k_machdep.c - move m68881_save() and m68881_restore() declarations into <m68k/m68k.h>
    Briefly tested and no obvious breakage on atari, sun3, and x68k.

--- sys/arch/luna68k/include/cpu.h
+++ sys/arch/luna68k/include/cpu.h
@@ -1,4 +1,4 @@
-/* $NetBSD: cpu.h,v 1.29 2011/02/08 20:20:16 rmind Exp $ */
+/* $NetBSD: cpu.h,v 1.30 2011/05/16 13:22:53 tsutsui Exp $ */
 
 /*
  * Copyright (c) 1988 University of Utah.
@@ -122,11 +122,8 @@ void	dumpconf(void);
 void	dumpsys(void);
 
 /* locore.s functions */
-struct fpframe;
 int	suline(void *, void *);
 void	loadustp(int);
-void	m68881_save(struct fpframe *);
-void	m68881_restore(struct fpframe *);
 
 /* machdep.c functions */
 int	badaddr(void *, int);
--- sys/arch/next68k/next68k/machdep.c
+++ sys/arch/next68k/next68k/machdep.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: machdep.c,v 1.103 2011/05/10 14:45:28 tsutsui Exp $	*/
+/*	$NetBSD: machdep.c,v 1.104 2011/05/16 13:22:55 tsutsui Exp $	*/
 
 /*
  * Copyright (c) 1998 Darrin B. Jewell
@@ -40,7 +40,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: machdep.c,v 1.103 2011/05/10 14:45:28 tsutsui Exp $");
+__KERNEL_RCSID(0, "$NetBSD: machdep.c,v 1.104 2011/05/16 13:22:55 tsutsui Exp $");
 
 #include "opt_ddb.h"
 #include "opt_kgdb.h"
@@ -313,40 +313,6 @@ cpu_startup(void)
 	 * Set up CPU-specific registers, cache, etc.
 	 */
 	initcpu();
-}
-
-/*
- * Set registers on exec.
- */
-void
-setregs(struct lwp *l, struct exec_package *pack, vaddr_t stack)
-{
-	struct frame *frame = (struct frame *)l->l_md.md_regs;
-	struct pcb *pcb = lwp_getpcb(l);
-
-	frame->f_sr = PSL_USERSET;
-	frame->f_pc = pack->ep_entry & ~1;
-	frame->f_regs[D0] = 0;
-	frame->f_regs[D1] = 0;
-	frame->f_regs[D2] = 0;
-	frame->f_regs[D3] = 0;
-	frame->f_regs[D4] = 0;
-	frame->f_regs[D5] = 0;
-	frame->f_regs[D6] = 0;
-	frame->f_regs[D7] = 0;
-	frame->f_regs[A0] = 0;
-	frame->f_regs[A1] = 0;
-	frame->f_regs[A2] = l->l_proc->p_psstrp;
-	frame->f_regs[A3] = 0;
-	frame->f_regs[A4] = 0;
-	frame->f_regs[A5] = 0;
-	frame->f_regs[A6] = 0;
-	frame->f_regs[SP] = stack;
-
-	/* restore a null state frame */
-	pcb->pcb_fpregs.fpf_null = 0;
-	if (fputype)
-		m68881_restore(&pcb->pcb_fpregs);
 }
 
 /*
