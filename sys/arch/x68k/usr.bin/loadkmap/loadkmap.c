2fa63217bb505e52bdde3f4279399222ca5247d9
tsutsui <tsutsui@NetBSD.org>
2011-05-19 21:26:38 +0000

    WARNSfy.
    XXX: rtcalarm/rtcalarm.c refers obsolete /dev/pow0

--- sys/arch/x68k/usr.bin/loadkmap/loadkmap.c
+++ sys/arch/x68k/usr.bin/loadkmap/loadkmap.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: loadkmap.c,v 1.9 2006/08/04 02:30:00 mhitch Exp $	*/
+/*	$NetBSD: loadkmap.c,v 1.10 2011/05/19 21:26:39 tsutsui Exp $	*/
 /*
  * loadkmap - load keyboard map (for NetBSD/X680x0)
  * from: amiga/stand/loadkmap/loadkmap.c
@@ -6,10 +6,12 @@
  */
 
 #include <sys/cdefs.h>
-__RCSID("$NetBSD: loadkmap.c,v 1.9 2006/08/04 02:30:00 mhitch Exp $");
+__RCSID("$NetBSD: loadkmap.c,v 1.10 2011/05/19 21:26:39 tsutsui Exp $");
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
 #include <sys/types.h>
 #include <sys/ioctl.h>
 #include <machine/kbdmap.h>
