e23d2fb4cf33d45f69639378e4dd674b73813d18
joerg <joerg@NetBSD.org>
2011-05-24 12:03:04 +0000

    Use bitwise operators, logical and with a constant doesn't make sense

--- sbin/routed/parms.c
+++ sbin/routed/parms.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: parms.c,v 1.24 2009/10/26 02:53:15 christos Exp $	*/
+/*	$NetBSD: parms.c,v 1.25 2011/05/24 12:03:04 joerg Exp $	*/
 
 /*
  * Copyright (c) 1983, 1993
@@ -38,7 +38,7 @@
 #include <sys/stat.h>
 
 #ifdef __NetBSD__
-__RCSID("$NetBSD: parms.c,v 1.24 2009/10/26 02:53:15 christos Exp $");
+__RCSID("$NetBSD: parms.c,v 1.25 2011/05/24 12:03:04 joerg Exp $");
 #elif defined(__FreeBSD__)
 __RCSID("$FreeBSD$");
 #else
@@ -900,11 +900,11 @@ check_parms(struct parm *new)
 		if ((0 != (new->parm_int_state & GROUP_IS_SOL_OUT)
 		     && 0 != (parmp->parm_int_state & GROUP_IS_SOL_OUT)
 		     && 0 != ((new->parm_int_state ^ parmp->parm_int_state)
-			      && GROUP_IS_SOL_OUT))
+			      & GROUP_IS_SOL_OUT))
 		    || (0 != (new->parm_int_state & GROUP_IS_ADV_OUT)
 			&& 0 != (parmp->parm_int_state & GROUP_IS_ADV_OUT)
 			&& 0 != ((new->parm_int_state ^ parmp->parm_int_state)
-				 && GROUP_IS_ADV_OUT))
+				 & GROUP_IS_ADV_OUT))
 		    || (new->parm_rdisc_pref != 0
 			&& parmp->parm_rdisc_pref != 0
 			&& new->parm_rdisc_pref != parmp->parm_rdisc_pref)
