ebc232a582a6fe08a6e1d8012078e86bbf667016
drochner <drochner@NetBSD.org>
2011-05-24 19:10:08 +0000

    copy AES-XCBC-MAC support from KAME IPSEC to FAST_IPSEC For this to fit, an API change in cryptosoft was adopted from OpenBSD (addition of a "Setkey" method to hashes) which was done for GCM/GMAC support there, so it might be useful in the future anyway. tested against KAME IPSEC AFAICT, FAST_IPSEC now supports as much as KAME.

--- sys/opencrypto/cryptodev.h
+++ sys/opencrypto/cryptodev.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: cryptodev.h,v 1.22 2011/05/23 13:51:10 drochner Exp $ */
+/*	$NetBSD: cryptodev.h,v 1.23 2011/05/24 19:10:09 drochner Exp $ */
 /*	$FreeBSD: src/sys/opencrypto/cryptodev.h,v 1.2.2.6 2003/07/02 17:04:50 sam Exp $	*/
 /*	$OpenBSD: cryptodev.h,v 1.33 2002/07/17 23:52:39 art Exp $	*/
 
@@ -139,7 +139,8 @@
 #define CRYPTO_SHA2_512_HMAC	25
 #define CRYPTO_CAMELLIA_CBC	26
 #define CRYPTO_AES_CTR		27
-#define CRYPTO_ALGORITHM_MAX	27 /* Keep updated - see below */
+#define CRYPTO_AES_XCBC_MAC_96	28
+#define CRYPTO_ALGORITHM_MAX	28 /* Keep updated - see below */
 
 /* Algorithm flags */
 #define	CRYPTO_ALG_FLAG_SUPPORTED	0x01 /* Algorithm is supported */
