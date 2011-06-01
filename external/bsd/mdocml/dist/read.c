b6730ba4b667bac4a68b0a207d7d9786eb0b915c
hans <hans@NetBSD.org>
2011-06-01 15:52:48 +0000

    include config.h

--- external/bsd/mdocml/dist/read.c
+++ external/bsd/mdocml/dist/read.c
@@ -15,6 +15,10 @@
  * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
 #include <sys/stat.h>
 #include <sys/mman.h>
 
