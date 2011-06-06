34379fbc052a38b6bdf2f6d4379456da7ae35c0d
msaitoh <msaitoh>
2011-06-06 18:27:12 +0000

    Add two new capabilities(PCI_CAP_SATA and PCI_CAP_PCIAF).

--- sys/dev/pci/pci_subr.c
+++ sys/dev/pci/pci_subr.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: pci_subr.c,v 1.86 2010/12/11 18:22:24 matt Exp $	*/
+/*	$NetBSD: pci_subr.c,v 1.87 2011/06/06 18:27:12 msaitoh Exp $	*/
 
 /*
  * Copyright (c) 1997 Zubin D. Dittia.  All rights reserved.
@@ -40,7 +40,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: pci_subr.c,v 1.86 2010/12/11 18:22:24 matt Exp $");
+__KERNEL_RCSID(0, "$NetBSD: pci_subr.c,v 1.87 2011/06/06 18:27:12 msaitoh Exp $");
 
 #ifdef _KERNEL_OPT
 #include "opt_pci.h"
@@ -1068,6 +1068,12 @@ pci_conf_print_caplist(
 			break;
 		case PCI_CAP_MSIX:
 			printf("MSI-X");
+			break;
+		case PCI_CAP_SATA:
+			printf("SATA");
+			break;
+		case PCI_CAP_PCIAF:
+			printf("Advanced Features");
 			break;
 		default:
 			printf("unknown");
