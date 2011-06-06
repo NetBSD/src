418a54d166484dae93c16253325a2f6093edd072
drochner <drochner@NetBSD.org>
2011-06-06 17:02:29 +0000

    make sure the infinity returned by HUGE_VALL has the "implicit" bit set, otherwise it is invalid This code is unlikely to be hit because gcc (and clang) use their builtins for these special values.

--- lib/libc/arch/i386/gen/infinityl.c
+++ lib/libc/arch/i386/gen/infinityl.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: infinityl.c,v 1.3 2011/04/06 21:55:36 christos Exp $	*/
+/*	$NetBSD: infinityl.c,v 1.4 2011/06/06 17:02:30 drochner Exp $	*/
 
 /*
  * IEEE-compatible infinityl.c for little-endian 80-bit format -- public domain.
@@ -7,10 +7,10 @@
 
 #include <sys/cdefs.h>
 #if defined(LIBC_SCCS) && !defined(lint)
-__RCSID("$NetBSD: infinityl.c,v 1.3 2011/04/06 21:55:36 christos Exp $");
+__RCSID("$NetBSD: infinityl.c,v 1.4 2011/06/06 17:02:30 drochner Exp $");
 #endif /* LIBC_SCCS and not lint */
 
 #include <math.h>
 
 const union __long_double_u __infinityl =
-	{ { 0, 0, 0, 0, 0, 0, 0, 0, 0xff, 0x7f, 0, 0 } };
+	{ { 0, 0, 0, 0, 0, 0, 0, 0x80, 0xff, 0x7f, 0, 0 } };
