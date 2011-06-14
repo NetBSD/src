3f6cfd7d8c3d355b22f87527a157fe4996ce7cd7
tsutsui <tsutsui@NetBSD.org>
2011-06-14 15:51:35 +0000

    Remove duplicated functions that are implemented in machdep.c.
    XXX: why do only sun2 and sun3 (and sun3x) have separate mm_md.c?

--- sys/arch/sun2/sun2/mm_md.c
+++ sys/arch/sun2/sun2/mm_md.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: mm_md.c,v 1.2 2011/06/12 03:35:47 rmind Exp $	*/
+/*	$NetBSD: mm_md.c,v 1.3 2011/06/14 15:51:35 tsutsui Exp $	*/
 
 /*
  * Copyright (c) 1982, 1986, 1990, 1993
@@ -76,7 +76,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: mm_md.c,v 1.2 2011/06/12 03:35:47 rmind Exp $");
+__KERNEL_RCSID(0, "$NetBSD: mm_md.c,v 1.3 2011/06/14 15:51:35 tsutsui Exp $");
 
 #include <sys/param.h>
 #include <sys/errno.h>
@@ -89,45 +89,6 @@ __KERNEL_RCSID(0, "$NetBSD: mm_md.c,v 1.2 2011/06/12 03:35:47 rmind Exp $");
 #define DEV_VME24D16	6	/* minor device 6 is /dev/vme24d16 */
 #define DEV_EEPROM	11 	/* minor device 11 is eeprom */
 #define DEV_LEDS	13 	/* minor device 13 is leds */
-
-int
-mm_md_physacc(paddr_t pa, vm_prot_t prot)
-{
-
-	/* Allow access only in "managed" RAM. */
-	if (pa < avail_start || pa >= avail_end)
-		return EFAULT;
-	return 0;
-}
-
-bool
-mm_md_direct_mapped_phys(paddr_t paddr, vaddr_t *vaddr)
-{
-
-	if (paddr >= avail_start)
-		return false;
-	*vaddr = paddr;
-	return true;
-}
-
-/*
- * Allow access to the PROM mapping similiar to uvm_kernacc().
- */
-int
-mm_md_kernacc(void *ptr, vm_prot_t prot, bool *handled)
-{
-
-	if ((vaddr_t)ptr < SUN2_PROM_BASE || (vaddr_t)ptr > SUN2_MONEND) {
-		*handled = false;
-		return 0;
-	}
-
-	*handled = true;
-	/* Read in the PROM itself is OK, write not. */
-	if ((prot & VM_PROT_WRITE) == 0)
-		return 0;
-	return EFAULT;
-}
 
 int
 mm_md_readwrite(dev_t dev, struct uio *uio)
