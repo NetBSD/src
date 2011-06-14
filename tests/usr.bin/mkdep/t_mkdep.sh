4a07d68015b7cd07d3d32b25ba584c2a28dec7f1
njoly <njoly@NetBSD.org>
2011-06-14 11:44:25 +0000

    mkdep(1) needs cc(1). Make testcase check for them.

--- tests/usr.bin/mkdep/t_mkdep.sh
+++ tests/usr.bin/mkdep/t_mkdep.sh
@@ -1,4 +1,4 @@
-# $NetBSD: t_mkdep.sh,v 1.2 2011/05/31 13:22:56 njoly Exp $
+# $NetBSD: t_mkdep.sh,v 1.3 2011/06/14 11:44:25 njoly Exp $
 #
 # Copyright (c) 2011 The NetBSD Foundation, Inc.
 # All rights reserved.
@@ -31,6 +31,7 @@
 atf_test_case suffixes
 suffixes_head() {
 	atf_set "descr" "Test suffixes list"
+	atf_set "require.progs" "mkdep cc"
 }
 
 suffixes_body() {
