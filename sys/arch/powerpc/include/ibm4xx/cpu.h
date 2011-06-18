89bb05272e284aaa252e10c5ba31d7d938fb27cc
matt <matt@NetBSD.org>
2011-06-18 17:06:52 +0000

    Move PVR definitions to <powerpc/ibm4xx/spr.h> No need to include <powerpc/cpu.h> in <powerpc/ib4xx/cpu.h>

--- sys/arch/powerpc/include/ibm4xx/cpu.h
+++ sys/arch/powerpc/include/ibm4xx/cpu.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: cpu.h,v 1.17 2011/06/17 19:03:03 matt Exp $	*/
+/*	$NetBSD: cpu.h,v 1.18 2011/06/18 17:06:52 matt Exp $	*/
 
 /*
  * Copyright 2002 Wasabi Systems, Inc.
@@ -41,30 +41,6 @@
 #include <powerpc/spr.h>
 #include <powerpc/ibm4xx/spr.h>
 #include <powerpc/ibm4xx/dcr4xx.h>
-
-/* PVRs for different IBM CPUs */
-#define	PVR_401A1		0x00210000
-#define	PVR_401B2		0x00220000
-#define	PVR_401C2		0x00230000
-#define	PVR_401D2		0x00240000
-#define	PVR_401E2		0x00250000
-#define	PVR_401F2		0x00260000
-#define	PVR_401G2		0x00270000
-
-#define	PVR_403			0x00200000
-
-#define PVR_405GP		0x40110000
-#define PVR_405GP_PASS1 	0x40110000	/* RevA */
-#define PVR_405GP_PASS2 	0x40110040	/* RevB */
-#define PVR_405GP_PASS2_1 	0x40110082	/* RevC */
-#define PVR_405GP_PASS3 	0x401100c4	/* RevD */
-#define PVR_405GPR		0x50910000
-#define PVR_405GPR_REVB		0x50910951
-
-#define	PVR_405D5X1 		0x20010000 	/* Virtex II Pro */
-#define	PVR_405D5X2 		0x20011000 	/* Virtex 4 FX */
-
-#define PVR_405EX		0x12910000
 
 #if defined(_KERNEL)
 extern char bootpath[];
@@ -146,8 +122,6 @@ mfsdr(int reg)
 	return mfdcr(DCR_SDR0_CFGDATA);
 }
 #endif /* _KERNEL */
-
-#include <powerpc/cpu.h>
 
 /* Board info dictionary */
 extern prop_dictionary_t board_properties;
