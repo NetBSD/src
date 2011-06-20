312f4952fde545a95f4a48ced9f3b344628843d3
matt <matt@NetBSD.org>
2011-06-20 06:29:53 +0000

    Cleanup powerpc param.h.  If compiling a MODULE, ignore port-specific stuff. Only include <machine/cpu.h> in <powerpc/param.h> at the end.

--- sys/arch/ofppc/include/param.h
+++ sys/arch/ofppc/include/param.h
@@ -1,7 +1,12 @@
-/*	$NetBSD: param.h,v 1.11 2008/03/07 17:13:49 phx Exp $	*/
+/*	$NetBSD: param.h,v 1.12 2011/06/20 06:29:53 matt Exp $	*/
 
-#define MACHINE		"ofppc"
-#include <powerpc/param.h>
+#if defined(_KERNEL) && !defined(_MODULE)
+
+#define MACHINE			"ofppc"
 
 /* at this offset we mmap() the PCI IO range in display drivers */
 #define PCI_MAGIC_IO_RANGE      0xfeff0000
+
+#endif /* _KERNEL && !_MODULE */
+
+#include <powerpc/param.h>
