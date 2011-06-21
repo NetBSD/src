feed8e18a5c659603dd942f3a9326621b4b97c6c
mrg <mrg@NetBSD.org>
2011-06-21 02:48:27 +0000

    import the non-arch specific makefiles we use to build GCC 4.1 as a base reference.

---
+++ external/gpl3/gcc/lib/crtstuff4/crtbegin.c
@@ -0,0 +1,9 @@
+/*	$NetBSD: crtbegin.c,v 1.1.1.1 2011/06/21 02:48:27 mrg Exp $	*/
+
+/*
+ * Written by Jason R. Thorpe, August 24, 2004.
+ * Public domain.
+ */
+
+#define	CRT_BEGIN
+#include "crtstuff.c"
