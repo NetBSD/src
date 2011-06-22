686ed5acf081ae4ca270acfbe9e5133e549bb37d
matt <matt@NetBSD.org>
2011-06-22 18:17:17 +0000

    ibm4xx now uses <powerpc/pci_machdep.h>

--- sys/modules/arch/powerpc/powerpc-4xx/bsd.powerpc-4xx.mk
+++ sys/modules/arch/powerpc/powerpc-4xx/bsd.powerpc-4xx.mk
@@ -1,4 +1,4 @@
-#	$NetBSD: bsd.powerpc-4xx.mk,v 1.2 2011/06/20 20:18:43 matt Exp $
+#	$NetBSD: bsd.powerpc-4xx.mk,v 1.3 2011/06/22 18:17:17 matt Exp $
 
 .ifndef _BSD_POWERPC_4XX_MK_
 _BSD_POWERPC_4XX_MK_=1
@@ -10,7 +10,7 @@ PPC_IBM4XX=	1
 
 # hack into bsd.kmodule.mk
 PPC_INTR_IMPL=\"powerpc/intr.h\"
-PPC_PCI_MACHDEP_IMPL=\"powerpc/ibm4xx/pci_machdep.h\"
+PPC_PCI_MACHDEP_IMPL=\"powerpc/pci_machdep.h\"
 
 AFLAGS+=	-mcpu=403
 
