448aaa234b822a4447ca24e760a51faebbbbec82
matt <matt@NetBSD.org>
2011-06-23 01:27:20 +0000

    Switch to using the common <common/pmap/tlb/tlb.h>

--- sys/arch/powerpc/booke/booke_stubs.c
+++ sys/arch/powerpc/booke/booke_stubs.c
@@ -35,7 +35,7 @@
 
 #include <sys/cdefs.h>
 
-__KERNEL_RCSID(0, "$NetBSD: booke_stubs.c,v 1.6 2011/06/15 15:22:56 matt Exp $");
+__KERNEL_RCSID(0, "$NetBSD: booke_stubs.c,v 1.7 2011/06/23 01:27:20 matt Exp $");
 
 #include <sys/param.h>
 #include <sys/cpu.h>
@@ -45,17 +45,17 @@ __KERNEL_RCSID(0, "$NetBSD: booke_stubs.c,v 1.6 2011/06/15 15:22:56 matt Exp $")
 
 #define	__stub	__section(".stub") __noprofile
 
-void tlb_set_asid(uint32_t) __stub;
+void tlb_set_asid(tlb_asid_t) __stub;
 
 void
-tlb_set_asid(uint32_t asid)
+tlb_set_asid(tlb_asid_t asid)
 {
 	(*cpu_md_ops.md_tlb_ops->md_tlb_set_asid)(asid);
 }
 
-uint32_t tlb_get_asid(void) __stub;
+tlb_asid_t tlb_get_asid(void) __stub;
 
-uint32_t
+tlb_asid_t
 tlb_get_asid(void)
 {
 	return (*cpu_md_ops.md_tlb_ops->md_tlb_get_asid)();
@@ -77,26 +77,26 @@ tlb_invalidate_globals(void)
 	(*cpu_md_ops.md_tlb_ops->md_tlb_invalidate_globals)();
 }
 
-void tlb_invalidate_asids(uint32_t, uint32_t) __stub;
+void tlb_invalidate_asids(tlb_asid_t, tlb_asid_t) __stub;
 
 void
-tlb_invalidate_asids(uint32_t asid_lo, uint32_t asid_hi)
+tlb_invalidate_asids(tlb_asid_t asid_lo, tlb_asid_t asid_hi)
 {
 	(*cpu_md_ops.md_tlb_ops->md_tlb_invalidate_asids)(asid_lo, asid_hi);
 }
 
-void tlb_invalidate_addr(vaddr_t, uint32_t) __stub;
+void tlb_invalidate_addr(vaddr_t, tlb_asid_t) __stub;
 
 void
-tlb_invalidate_addr(vaddr_t va, uint32_t asid)
+tlb_invalidate_addr(vaddr_t va, tlb_asid_t asid)
 {
 	(*cpu_md_ops.md_tlb_ops->md_tlb_invalidate_addr)(va, asid);
 }
 
-bool tlb_update_addr(vaddr_t, uint32_t, uint32_t, bool) __stub;
+bool tlb_update_addr(vaddr_t, tlb_asid_t, pt_entry_t, bool) __stub;
 
 bool
-tlb_update_addr(vaddr_t va, uint32_t asid, uint32_t pte, bool insert_p)
+tlb_update_addr(vaddr_t va, tlb_asid_t asid, pt_entry_t pte, bool insert_p)
 {
 	return (*cpu_md_ops.md_tlb_ops->md_tlb_update_addr)(va, asid, pte, insert_p);
 }
@@ -109,44 +109,20 @@ tlb_read_entry(size_t pos, struct tlbmask *tlb)
 	(*cpu_md_ops.md_tlb_ops->md_tlb_read_entry)(pos, tlb);
 }
 
-u_int tlb_record_asids(u_long *, uint32_t) __stub;
-
-u_int
-tlb_record_asids(u_long *bitmap, uint32_t start)
-{
-	return (*cpu_md_ops.md_tlb_ops->md_tlb_record_asids)(bitmap, start);
-}
-
-void *tlb_mapiodev(paddr_t, psize_t) __stub;
-
-void *
-tlb_mapiodev(paddr_t pa, psize_t len)
-{
-	return (*cpu_md_ops.md_tlb_ops->md_tlb_mapiodev)(pa, len);
-}
-
-void tlb_unmapiodev(vaddr_t, vsize_t) __stub;
+void tlb_write_entry(size_t, const struct tlbmask *) __stub;
 
 void
-tlb_unmapiodev(vaddr_t va, vsize_t len)
+tlb_write_entry(size_t pos, const struct tlbmask *tlb)
 {
-	(*cpu_md_ops.md_tlb_ops->md_tlb_unmapiodev)(va, len);
+	(*cpu_md_ops.md_tlb_ops->md_tlb_write_entry)(pos, tlb);
 }
 
-int tlb_ioreserve(vaddr_t, vsize_t, uint32_t) __stub;
+u_int tlb_record_asids(u_long *) __stub;
 
-int
-tlb_ioreserve(vaddr_t va, vsize_t len, uint32_t pte)
+u_int
+tlb_record_asids(u_long *bitmap)
 {
-	return (*cpu_md_ops.md_tlb_ops->md_tlb_ioreserve)(va, len, pte);
-}
-
-int tlb_iorelease(vaddr_t) __stub;
-
-int
-tlb_iorelease(vaddr_t va)
-{
-	return (*cpu_md_ops.md_tlb_ops->md_tlb_iorelease)(va);
+	return (*cpu_md_ops.md_tlb_ops->md_tlb_record_asids)(bitmap);
 }
 
 void tlb_dump(void (*)(const char *, ...)) __stub;
@@ -164,4 +140,36 @@ void
 tlb_walk(void *ctx, bool (*func)(void *, vaddr_t, uint32_t, uint32_t))
 {
 	(*cpu_md_ops.md_tlb_ops->md_tlb_walk)(ctx, func);
+}
+
+void *tlb_mapiodev(paddr_t, psize_t) __stub;
+
+void *
+tlb_mapiodev(paddr_t pa, psize_t len)
+{
+	return (*cpu_md_ops.md_tlb_io_ops->md_tlb_mapiodev)(pa, len);
+}
+
+void tlb_unmapiodev(vaddr_t, vsize_t) __stub;
+
+void
+tlb_unmapiodev(vaddr_t va, vsize_t len)
+{
+	(*cpu_md_ops.md_tlb_io_ops->md_tlb_unmapiodev)(va, len);
+}
+
+int tlb_ioreserve(vaddr_t, vsize_t, uint32_t) __stub;
+
+int
+tlb_ioreserve(vaddr_t va, vsize_t len, uint32_t pte)
+{
+	return (*cpu_md_ops.md_tlb_io_ops->md_tlb_ioreserve)(va, len, pte);
+}
+
+int tlb_iorelease(vaddr_t) __stub;
+
+int
+tlb_iorelease(vaddr_t va)
+{
+	return (*cpu_md_ops.md_tlb_io_ops->md_tlb_iorelease)(va);
 }
