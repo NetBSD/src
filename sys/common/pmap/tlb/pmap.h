055140992d14fc90448bf088181d1c1e0f8ecc78
matt <matt@NetBSD.org>
2011-06-23 02:33:44 +0000

    Move some MD parts back to the booke pmap.c.  Cleanup initialization a bit.

--- sys/common/pmap/tlb/pmap.h
+++ sys/common/pmap/tlb/pmap.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: pmap.h,v 1.5 2011/06/20 20:24:29 matt Exp $	*/
+/*	$NetBSD: pmap.h,v 1.6 2011/06/23 02:33:44 matt Exp $	*/
 
 /*
  * Copyright (c) 1992, 1993
@@ -245,7 +245,7 @@ int	pmap_tlb_update_addr(pmap_t, vaddr_t, uint32_t, u_int);
 #define	PMAP_TLB_NEED_IPI	0x01
 #define	PMAP_TLB_INSERT		0x02
 void	pmap_tlb_invalidate_addr(pmap_t, vaddr_t);
-void	pmap_tlb_check(pmap_t);
+void	pmap_tlb_check(pmap_t, bool (*)(void *, vaddr_t, tlb_asid_t, pt_entry_t));
 
 uint16_t pmap_pvlist_lock(struct vm_page_md *, bool);
 
