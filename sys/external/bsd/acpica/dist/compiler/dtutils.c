32903b1237e27509673c08d29f92fed2b9296210
jruoho <jruoho@NetBSD.org>
2011-06-28 09:01:37 +0000

    Import ACPICA 20110623.
    Selected changes since 20110211 include, among other things, fixes to _PRW
    parsing, fix to a regression with the Load() operator, improvements in NULL
    package element removal, support for "spurious" global lock interrupts,
    execution of orphaned _REG methods under acpiec(4), and smaller iasl(8)
    improvements.

--- sys/external/bsd/acpica/dist/compiler/dtutils.c
+++ sys/external/bsd/acpica/dist/compiler/dtutils.c
@@ -189,8 +189,16 @@ DtFatal (
 
     DtError (ASL_ERROR, MessageId, FieldObject, ExtraMessage);
 
+/*
+ * TBD: remove this entire function, DtFatal
+ *
+ * We cannot abort the compiler on error, because we may be compiling a
+ * list of files. We must move on to the next file.
+ */
+#ifdef __OBSOLETE
     CmCleanupAndExit ();
     exit (1);
+#endif
 }
 
 
@@ -316,7 +324,6 @@ DtGetFileSize (
  * FUNCTION:    DtGetFieldValue
  *
  * PARAMETERS:  Field               - Current field list pointer
- *              Name                - Field name
  *
  * RETURN:      Field value
  *
@@ -326,23 +333,14 @@ DtGetFileSize (
 
 char *
 DtGetFieldValue (
-    DT_FIELD                *Field,
-    char                    *Name)
+    DT_FIELD                *Field)
 {
-
-    /* Search the field list for the name */
-
-    while (Field)
+    if (!Field)
     {
-        if (!ACPI_STRCMP (Name, Field->Name))
-        {
-            return (Field->Value);
-        }
-
-        Field = Field->Next;
+        return (NULL);
     }
 
-    return (NULL);
+    return (Field->Value);
 }
 
 
@@ -401,6 +399,7 @@ DtGetFieldType (
     case ACPI_DMT_BUFFER:
     case ACPI_DMT_BUF7:
     case ACPI_DMT_BUF16:
+    case ACPI_DMT_BUF128:
     case ACPI_DMT_PCI_PATH:
         Type = DT_FIELD_TYPE_BUFFER;
         break;
@@ -478,7 +477,7 @@ DtGetBufferLength (
  *
  * FUNCTION:    DtGetFieldLength
  *
- * PARAMETERS:  Field               - Current field list pointer
+ * PARAMETERS:  Field               - Current field
  *              Info                - Data table info
  *
  * RETURN:      Field length
@@ -546,6 +545,7 @@ DtGetFieldLength (
 
     case ACPI_DMT_UINT32:
     case ACPI_DMT_NAME4:
+    case ACPI_DMT_SLIC:
     case ACPI_DMT_SIG:
         ByteLength = 4;
         break;
@@ -565,7 +565,7 @@ DtGetFieldLength (
         break;
 
     case ACPI_DMT_STRING:
-        Value = DtGetFieldValue (Field, Info->Name);
+        Value = DtGetFieldValue (Field);
         if (Value)
         {
             ByteLength = ACPI_STRLEN (Value) + 1;
@@ -575,6 +575,7 @@ DtGetFieldLength (
 
             sprintf (MsgBuffer, "Expected \"%s\"", Info->Name);
             DtFatal (ASL_MSG_COMPILER_INTERNAL, NULL, MsgBuffer);
+            return (0);
         }
         break;
 
@@ -587,7 +588,7 @@ DtGetFieldLength (
         break;
 
     case ACPI_DMT_BUFFER:
-        Value = DtGetFieldValue (Field, Info->Name);
+        Value = DtGetFieldValue (Field);
         if (Value)
         {
             ByteLength = DtGetBufferLength (Value);
@@ -597,6 +598,7 @@ DtGetFieldLength (
 
             sprintf (MsgBuffer, "Expected \"%s\"", Info->Name);
             DtFatal (ASL_MSG_COMPILER_INTERNAL, NULL, MsgBuffer);
+            return (0);
         }
         break;
 
@@ -605,8 +607,12 @@ DtGetFieldLength (
         ByteLength = 16;
         break;
 
+    case ACPI_DMT_BUF128:
+        ByteLength = 128;
+        break;
+
     case ACPI_DMT_UNICODE:
-        Value = DtGetFieldValue (Field, Info->Name);
+        Value = DtGetFieldValue (Field);
 
         /* TBD: error if Value is NULL? (as below?) */
 
@@ -615,7 +621,7 @@ DtGetFieldLength (
 
     default:
         DtFatal (ASL_MSG_COMPILER_INTERNAL, Field, "Invalid table opcode");
-        break;
+        return (0);
     }
 
     return (ByteLength);
