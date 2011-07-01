1566d50022022c8ee50920df4a87d8d0fc8da4f0
mrg <mrg@NetBSD.org>
2011-07-01 01:26:38 +0000

    add GCC 4.5 support

--- sys/arch/arm/include/stdarg.h
+++ sys/arch/arm/include/stdarg.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: stdarg.h,v 1.10 2008/06/21 00:56:39 gmcgarry Exp $	*/
+/*	$NetBSD: stdarg.h,v 1.11 2011/07/01 01:26:38 mrg Exp $	*/
 
 /*
  * Copyright (c) 1991, 1993
@@ -41,13 +41,18 @@ typedef _BSD_VA_LIST_	va_list;
 #ifdef __lint__
 #define __builtin_next_arg(t)		((t) ? 0 : 0)
 #define	__builtin_stdarg_start(a, l)	((a) = ((l) ? 0 : 0))
+#define	__builtin_va_start(a, l)	((a) = ((l) ? 0 : 0))
 #define	__builtin_va_arg(a, t)		((t)((a) ? 0 : 0))
 #define	__builtin_va_end		/* nothing */
 #define	__builtin_va_copy(d, s)		((d) = (s))
 #endif
 
 #if __GNUC_PREREQ__(2, 96)
+#if __GNUC_PREREQ__(4, 5)
+#define	va_start(ap, last)	__builtin_va_start((ap), (last))
+#else
 #define	va_start(ap, last)	__builtin_stdarg_start((ap), (last))
+#endif
 #define	va_arg			__builtin_va_arg
 #define	va_end			__builtin_va_end
 #define	__va_copy(dest, src)	__builtin_va_copy((dest), (src))
