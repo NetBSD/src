67b88feb0f67722ead340905c4c06b77a197c676
mrg <mrg@NetBSD.org>
2011-07-01 01:30:16 +0000

    - add support for src/compat builds - use .include not .sinclude for the arch Makefile.inc file - clean up

--- compat/mips64/o32/bsd.o32.mk
+++ compat/mips64/o32/bsd.o32.mk
@@ -1,4 +1,4 @@
-#	$NetBSD: bsd.o32.mk,v 1.4 2009/12/19 04:11:33 christos Exp $
+#	$NetBSD: bsd.o32.mk,v 1.5 2011/07/01 01:30:16 mrg Exp $
 
 .if ${MACHINE_ARCH} == "mips64eb"
 LD+=		-m elf32btsmip
@@ -12,5 +12,8 @@ CPUFLAGS+=	-mabi=32 -march=mips3
 LDADD+=		-mabi=32 -march=mips3
 LDFLAGS+=	-mabi=32 -march=mips3
 MKDEPFLAGS+=	-mabi=32 -march=mips3
+
+LIBMPFR_MACHINE_ARCH=	${MLIBDIR}
+LIBGMP_MACHINE_ARCH=	${MLIBDIR}
 
 .include "${.PARSEDIR}/../../Makefile.compat"
