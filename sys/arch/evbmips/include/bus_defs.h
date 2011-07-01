59adf08e17836c1a68a0316cc7343f658e18991d
dyoung <dyoung@NetBSD.org>
2011-07-01 17:09:58 +0000

    Per discussion at <http://mail-index.netbsd.org/tech-kern/2010/04/02/msg007941.html>, divide each machine's bus.h into bus_defs.h (constants & data types) and bus_funcs.h (macro implementations of bus_space(9) routines and MD prototypes).
    Note that some bus_space(9) routines' implementation will move to .c
    files from inline subroutines or macros in .h files.
    
    I've only made the split for machine architectures where there is PCI.
    All of the non-PCI-having architectures will require a similar split.
    
    These #include files are not referenced by any (committed) Makefiles or
    header files, yet.  Changes to Makefiles, to <sys/bus.h>, and to some
    more machine-dependent files will dribble in before I throw the switch.

---
+++ sys/arch/evbmips/include/bus_defs.h
@@ -0,0 +1,10 @@
+/* $NetBSD: bus_defs.h,v 1.1 2011/07/01 17:09:59 dyoung Exp $ */
+
+#ifndef _EVBMIPS_BUS_DEFS_H_
+#define	_EVBMIPS_BUS_DEFS_H_
+#define _MIPS_NEED_BUS_DMA_BOUNCE
+
+#include <mips/bus_space_defs.h>
+#include <mips/bus_dma_defs.h>
+
+#endif /* _EVBMIPS_BUS_DEFS_H_ */
