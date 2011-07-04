ea43260b016a455efb03b365633db42920049d68
mrg <mrg@NetBSD.org>
2011-07-04 12:00:49 +0000

    the gmp/mpfr subdirs are mipsel/mipseb.

--- compat/mips64/o32/bsd.o32.mk
+++ compat/mips64/o32/bsd.o32.mk
@@ -1,4 +1,4 @@
-#	$NetBSD: bsd.o32.mk,v 1.5 2011/07/01 01:30:16 mrg Exp $
+#	$NetBSD: bsd.o32.mk,v 1.6 2011/07/04 12:00:49 mrg Exp $
 
 .if ${MACHINE_ARCH} == "mips64eb"
 LD+=		-m elf32btsmip
@@ -13,7 +13,7 @@ LDADD+=		-mabi=32 -march=mips3
 LDFLAGS+=	-mabi=32 -march=mips3
 MKDEPFLAGS+=	-mabi=32 -march=mips3
 
-LIBMPFR_MACHINE_ARCH=	${MLIBDIR}
-LIBGMP_MACHINE_ARCH=	${MLIBDIR}
+LIBMPFR_MACHINE_ARCH=	mipsel
+LIBGMP_MACHINE_ARCH=	mipsel
 
 .include "${.PARSEDIR}/../../Makefile.compat"
