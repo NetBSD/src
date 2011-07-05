8e6c56877f76be3925026ec264e53207da803aaa
spz <spz@NetBSD.org>
2011-07-05 21:22:43 +0000

    Import of bind 9.8.0-P4
    Upstream changelog:
    
            --- 9.8.0-P4 released ---
    
    3124.   [bug]           Use an rdataset attribute flag to indicate
                            negative-cache records rather than using rrtype 0;
                            this will prevent problems when that rrtype is
                            used in actual DNS packets.  [RT #24777]
    
            --- 9.8.0-P3 released (withdrawn) ---
    
    3126.   [security]      Using DNAME record to generate replacements caused
                            RPZ to exit with a assertion failure. [RT #23766]
    
    3125.   [security]      Using wildcard CNAME records as a replacement with
                            RPZ caused named to exit with a assertion failure.
                            [RT #24715]
    
    3123.   [security]      Change #2912 exposed a latent flaw in
                            dns_rdataset_totext() that could cause named to
                            crash with an assertion failure. [RT #24777]
    
    3115.   [bug]           Named could fail to return requested data when
                            following a CNAME that points into the same zone.
                            [RT #2445]

--- external/bsd/bind/dist/bin/tests/system/nsupdate/tests.sh
+++ external/bsd/bind/dist/bin/tests/system/nsupdate/tests.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# Copyright (C) 2004, 2007, 2009, 2010  Internet Systems Consortium, Inc. ("ISC")
+# Copyright (C) 2004, 2007, 2009-2011  Internet Systems Consortium, Inc. ("ISC")
 # Copyright (C) 2000, 2001  Internet Software Consortium.
 #
 # Permission to use, copy, modify, and/or distribute this software for any
@@ -15,7 +15,7 @@
 # OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
 # PERFORMANCE OF THIS SOFTWARE.
 
-# Id: tests.sh,v 1.32.24.1 2011-02-03 06:18:51 marka Exp
+# Id: tests.sh,v 1.32.24.1.2.1 2011-06-02 23:47:34 tbox Exp
 
 SYSTEMTESTTOP=..
 . $SYSTEMTESTTOP/conf.sh
