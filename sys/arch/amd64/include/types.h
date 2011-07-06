dcad838c67bffa4c7eddd69d386689a40273f4e0
dyoung <dyoung@NetBSD.org>
2011-07-06 18:46:04 +0000

    Implement bus_space_tag_create() and _destroy().
    Factor bus_space_reserve(), bus_space_release(), et cetera out of
    bus_space_alloc(), bus_space_map(), bus_space_free(), bus_space_unmap(),
    et cetera.
    
    For i386 and amd64, activate the use of <machine/bus_defs.h> and
    <machine/bus_funcs.h> by #defining __HAVE_NEW_STYLE_BUS_H in
    their respective types.h.  While I'm here, remove unnecessary
    __HAVE_DEVICE_REGISTER #defines.

--- sys/arch/amd64/include/types.h
+++ sys/arch/amd64/include/types.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: types.h,v 1.38 2011/06/12 03:35:38 rmind Exp $	*/
+/*	$NetBSD: types.h,v 1.39 2011/07/06 18:46:04 dyoung Exp $	*/
 
 /*-
  * Copyright (c) 1990 The Regents of the University of California.
@@ -76,7 +76,7 @@ typedef	volatile unsigned char		__cpu_simple_lock_t;
 /* The amd64 does not have strict alignment requirements. */
 #define	__NO_STRICT_ALIGNMENT
 
-#define	__HAVE_DEVICE_REGISTER
+#define	__HAVE_NEW_STYLE_BUS_H
 #define	__HAVE_CPU_COUNTER
 #define	__HAVE_CPU_DATA_FIRST
 #define	__HAVE_MD_CPU_OFFLINE
