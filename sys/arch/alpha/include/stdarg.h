3e3f7791c38e04184e742688467d40fe2e9ed55c
mrg <mrg>
2011-07-07 01:27:13 +0000

    port to GCC 4.5.

--- sys/arch/alpha/include/stdarg.h
+++ sys/arch/alpha/include/stdarg.h
@@ -1,4 +1,4 @@
-/* $NetBSD: stdarg.h,v 1.18 2005/12/11 12:16:16 christos Exp $ */
+/* $NetBSD: stdarg.h,v 1.19 2011/07/07 01:27:13 mrg Exp $ */
 
 /*-
  * Copyright (c) 1991, 1993
@@ -45,7 +45,11 @@ typedef _BSD_VA_LIST_	va_list;
 #define	va_end(a)		/* nothing */
 #else /* !__lint__ */
 #if __GNUC_PREREQ__(2, 96)
+#if __GNUC_PREREQ__(4, 5)
+#define	va_start(ap, last)	__builtin_va_start((ap), (last))
+#else
 #define	va_start(ap, last)	__builtin_stdarg_start((ap), (last))
+#endif
 #define	va_arg			__builtin_va_arg
 #define	va_end			__builtin_va_end
 #define	__va_copy(dest, src)	__builtin_va_copy((dest), (src))
