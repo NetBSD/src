20a7ff2c0758847fbf80456a41e914e1a690a827
uwe <uwe@NetBSD.org>
2011-07-08 20:59:53 +0000

    Augment previous.  Use -Wno-uninitialized for sh3 in general, not just dreamcast.  The problem is not sh3 per-se, but the fact that for sh3 we use -Os by default.  That causes false positives since gcc doesn't detect that e.g. a variable is set and used under the same condition in different "if" statements.
    XXX: This should probably check for optimization level instead,
    though, for all I know, phase of moon might be a contributing cause
    too.
    
    XXX2: MACHINE_CPU is set in bsd.own.mk and is not available here, so
    we have to spell the test in terms of MACHINE_ARCH.

--- share/mk/bsd.sys.mk
+++ share/mk/bsd.sys.mk
@@ -1,4 +1,4 @@
-#	$NetBSD: bsd.sys.mk,v 1.206 2011/07/08 03:29:52 mrg Exp $
+#	$NetBSD: bsd.sys.mk,v 1.207 2011/07/08 20:59:53 uwe Exp $
 #
 # Build definitions used for NetBSD source tree builds.
 
@@ -63,8 +63,10 @@ CXXFLAGS+=	${${ACTIVE_CXX} == "gcc":? -Wno-non-template-friend -Wno-pmf-conversi
 CFLAGS+=	-Wsign-compare
 CFLAGS+=	${${ACTIVE_CC} == "clang":? -Wpointer-sign :}
 .endif
-.if defined(HAVE_GCC) && ${HAVE_GCC} == 45 && ${MACHINE} == "dreamcast"
-# XXX GCC 4.5 for dreamcast is extra noisy for cases it should be better with
+.if (defined(HAVE_GCC) && ${HAVE_GCC} == 45 \
+     && (${MACHINE_ARCH} == "sh3eb" || ${MACHINE_ARCH} == "sh3el"))
+# XXX GCC 4.5 for sh3 (which we compile with -Os) is extra noisy for
+# cases it should be better with
 CFLAGS+=	-Wno-uninitialized
 .endif
 .endif
