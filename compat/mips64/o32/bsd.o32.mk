3f66c8f78fc841719acd7ee8f76791f0aa2de774
mrg <mrg@NetBSD.org>
2011-07-10 03:05:33 +0000

    make mips64 builds of gmp and mpfr work including compat libs.

--- compat/mips64/o32/bsd.o32.mk
+++ compat/mips64/o32/bsd.o32.mk
@@ -1,10 +1,11 @@
-#	$NetBSD: bsd.o32.mk,v 1.6 2011/07/04 12:00:49 mrg Exp $
+#	$NetBSD: bsd.o32.mk,v 1.7 2011/07/10 03:05:33 mrg Exp $
 
 .if ${MACHINE_ARCH} == "mips64eb"
 LD+=		-m elf32btsmip
 .else
 LD+=		-m elf32ltsmip
 .endif
+LIBGMP_MACHINE_ARCH=	${MACHINE_ARCH}
 MLIBDIR=	o32
 
 COPTS+=		-mabi=32 -march=mips3
@@ -12,8 +13,5 @@ CPUFLAGS+=	-mabi=32 -march=mips3
 LDADD+=		-mabi=32 -march=mips3
 LDFLAGS+=	-mabi=32 -march=mips3
 MKDEPFLAGS+=	-mabi=32 -march=mips3
-
-LIBMPFR_MACHINE_ARCH=	mipsel
-LIBGMP_MACHINE_ARCH=	mipsel
 
 .include "${.PARSEDIR}/../../Makefile.compat"
