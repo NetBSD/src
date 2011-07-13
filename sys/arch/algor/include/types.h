5276f2af8ac6abbab4a13c19360037c96dbdc666
dyoung <dyoung@NetBSD.org>
2011-07-13 04:53:39 +0000

    Switch algor to new-style <sys/bus.h>.  While I'm here, get rid of __HAVE_DEVICE_REGISTER.

--- sys/arch/algor/include/types.h
+++ sys/arch/algor/include/types.h
@@ -1,5 +1,5 @@
-/*	$NetBSD: types.h,v 1.5 2008/01/20 18:09:04 joerg Exp $	*/
+/*	$NetBSD: types.h,v 1.6 2011/07/13 04:53:39 dyoung Exp $	*/
 
 #include <mips/types.h>
 
-#define	__HAVE_DEVICE_REGISTER
+#define	__HAVE_NEW_STYLE_BUS_H
