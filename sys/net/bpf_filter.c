d70483c5b99520d2f3caca109dc6544ca6e01f6c
drochner <drochner@NetBSD.org>
2011-07-14 10:43:55 +0000

    clear the packet filter's scratch memory before running the filter program, otherwise kernel memory can be leaked, from Guy Harris per PR kern/45142

--- sys/net/bpf_filter.c
+++ sys/net/bpf_filter.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: bpf_filter.c,v 1.46 2011/02/19 17:21:48 christos Exp $	*/
+/*	$NetBSD: bpf_filter.c,v 1.47 2011/07/14 10:43:55 drochner Exp $	*/
 
 /*-
  * Copyright (c) 1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997
@@ -37,7 +37,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: bpf_filter.c,v 1.46 2011/02/19 17:21:48 christos Exp $");
+__KERNEL_RCSID(0, "$NetBSD: bpf_filter.c,v 1.47 2011/07/14 10:43:55 drochner Exp $");
 
 #if 0
 #if !(defined(lint) || defined(KERNEL))
@@ -148,6 +148,7 @@ bpf_filter(const struct bpf_insn *pc, const u_char *p, u_int wirelen,
 		return (u_int)-1;
 	A = 0;
 	X = 0;
+	memset(mem, 0, sizeof(mem));
 	--pc;
 	for (;;) {
 		++pc;
