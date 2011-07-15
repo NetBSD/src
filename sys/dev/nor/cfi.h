fb19d2b7895e9a1fe6a9c5835268f5fb61fa3488
cliff <cliff@NetBSD.org>
2011-07-15 19:19:55 +0000

    Add support for CFI NOR, using MPC8536DS as initial example. Only AMD/Fujitsu command set is suported so far. This is still work in progress, be advised.

--- sys/dev/nor/cfi.h
+++ sys/dev/nor/cfi.h
@@ -1,16 +1,240 @@
+/*	$NetBSD: cfi.h,v 1.2 2011/07/15 19:19:57 cliff Exp $	*/
+
 #ifndef _CFI_H_
 #define _CFI_H_
 
-enum {
-	CFI_QUERY_MODE_ADDRESS = 0x55, /* some devices accept anything */
-	CFI_QUERY_DATA = 0x98,
+#include <dev/nor/cfi_0002.h>
+#include <machine/bus.h>
 
-	CFI_READ_ARRAY_MODE_ADDRESS = 0x00, /* could be anything */
-	CFI_READ_ARRAY_MODE_DATA = 0xf0,    /* also can be 0xff */
+/*
+ * minimum size to bus_space_map for probe/identify QRY:
+ * larget offset needed is CFI_QUERY_MODE_ALT_ADDRESS
+ * scaled by maximum attempted port width, so
+ *	min >= (0x555 * sizeof(uint32_t))
+ */
+#define CFI_QRY_MIN_MAP_SIZE	0x2000
 
-	CFI_ADDR_IDENTIFICATION_STRING = 0x10,
-	CFI_ADDR_SYS_INTERFACE_INFO = 0x1b,
-	CFI_ADDR_DEV_GEOMETRY = 0x27
+
+typedef enum {
+	CFI_STATE_DATA_ARRAY = 0,
+	CFI_STATE_QUERY,
+	/* TBD */
+} cfi_state_t;
+
+
+/*
+ * CFI Query structure
+ */
+struct cfi_query_data {
+    /* Query info */
+    uint8_t	qry[3];			/* { 'Q', 'R', 'Y' } */
+    uint16_t	id_pri;			/* primary comand set ID */
+    uint16_t	addr_pri;		/* primary table addr */
+    uint16_t	id_alt;			/* alternate command set ID */
+    uint16_t	addr_alt;		/* alternate table addr */
+    /* System Interface info */
+    uint8_t	vcc_min;		/* min Vcc */
+    uint8_t	vcc_max;		/* max Vcc */
+    uint8_t	vpp_min;		/* min Vpp */
+    uint8_t	vpp_max;		/* max Vpp */
+    uint8_t	write_word_time_typ;	/* typ 1-word timeout, 1<<N usec */
+    uint8_t	write_nbyte_time_typ;	/* typ multi-byte timeout, 1<<N usec */
+    uint8_t	erase_blk_time_typ;	/* typ 1-blk erase timeout, 1<<N msec */
+    uint8_t	erase_chiptime_typ;	/* typ chip erase timeout, 1<<N msec */
+    uint8_t	write_word_time_max;	/* max 1-word timeout, typ<<N */
+    uint8_t	write_nbyte_time_max;	/* max multi-byte timeout, typ<<N */
+    uint8_t	erase_blk_time_max;	/* max 1-blk erase timeout, typ<<N */
+    uint8_t	erase_chiptime_max;	/* max chip erase timeout, typ<<N */
+    /* Device Geometry Definition */
+    uint8_t	device_size;		/* 1<<N bytes */
+    uint16_t	interface_code_desc;	/* JEP137 interface code description */
+    uint16_t	write_nbyte_size_max;	/* max size of multi-byte write, 1<<N */
+    uint8_t	erase_blk_regions;	/* number of erase block regions */
+    struct {
+	uint16_t z;			/* Erase Blocks are z * 256 bytes */
+	uint16_t y;			/* y+1 = #Erase Blocks in region */
+    } erase_blk_info[4];
+    /* Vendor-specific Primary command set info */
+    union {
+	struct cmdset_0002_query_data cmd_0002;
+    } pri;
+#ifdef NOTYET
+    /* Vendor-specific Alternate command set info */
+    union {
+	/* some command set structure here */
+    } pri;
+#endif
 };
 
+/*
+ * decode interface_code_desc
+ */
+static inline const char *
+cfi_interface_desc_str(uint16_t icd)
+{
+	switch(icd) {
+	case 0:
+		return "x8";
+	case 1:
+		return "x16";
+	case 2:
+		return "x8/x16";
+	default:
+		return "";
+	}
+}
+
+/*
+ * id_pri: CFI Command set and control assignments
+ */
+#define CFI_ID_PRI_NONE		0x0000
+#define CFI_ID_PRI_INTEL_EXT	0x0001
+#define CFI_ID_PRI_AMD_STD	0x0002
+#define CFI_ID_PRI_INTEL_STD	0x0003
+#define CFI_ID_PRI_AMD_EXT	0x0004
+#define CFI_ID_PRI_WINBOND	0x0005
+#define CFI_ID_PRI_ST_ADV	0x0020
+#define CFI_ID_PRI_MITSU_ADV	0x0100
+#define CFI_ID_PRI_MITSU_EXT	0x0101
+#define CFI_ID_PRI_SST_PAGE	0x0102
+#define CFI_ID_PRI_SST_OLD	0x0701
+#define CFI_ID_PRI_INTEL_PERF	0x0200
+#define CFI_ID_PRI_INTEL_DATA	0x0210
+#define CFI_ID_PRI_RESV		0xffff	/* not allowed, reserved */
+
+/*
+ * JEDEC ID (autoselect) data
+ */
+struct cfi_jedec_id_data {
+	uint16_t	id_mid;		/* manufacturer ID */
+	uint16_t	id_did[3];	/* device ID */
+	uint16_t	id_prot_state;
+	uint16_t	id_indicators;
+	uint8_t		id_swb_lo;	/* lower software bits */
+	uint8_t		id_swb_hi;	/* upper software bits */
+};
+
+/*
+ * table entry used to determine operating mode by QRY signature
+ */
+struct cfi_opmodes {
+	uint8_t		portwidth;	/* (1<<N) bytes */
+	uint8_t		chipwidth;	/* (1<<N) bytes */
+	uint8_t		interleave;	/* (1<<N) bytes */
+	uint8_t		qsa;		/* Query Start Address (in bytes) */
+	uint8_t		len;		/* signature length */
+	const uint8_t  *sig;		/* signature */
+	const char     *str;		/* descriptive string */
+};
+
+struct cfi;	/* fwd ref */
+
+struct cfi_ops {
+	void	(*cfi_reset)(struct cfi *);
+	int 	(*cfi_busy)(struct cfi *, flash_off_t);
+	int	(*cfi_program_word)(struct cfi *, flash_off_t);
+	int	(*cfi_erase_sector)(struct cfi *, flash_off_t);
+};
+
+/* NOTE:
+ * CFI_0002_STATS are just meant temporarily for debugging
+ * not for long-term use. Some event counters at the flash and nor
+ * layers might be helpful eventually
+ */
+#define CFI_0002_STATS	/* XXX TMP */
+#ifdef CFI_0002_STATS
+struct cfi_0002_stats {
+	u_long read_page;
+	u_long program_page;
+	u_long erase_all;
+	u_long erase_block;
+	u_long busy;
+	u_long busy_usec_min;
+	u_long busy_usec_max;
+	struct timeval busy_poll_tv;
+	struct timeval busy_yield_tv;
+	u_long busy_poll;
+	u_long busy_yield;
+	u_long busy_yield_hit;
+	u_long busy_yield_miss;
+	u_long busy_yield_timo;
+};
+
+extern void cfi_0002_stats_reset(struct cfi *);
+extern void cfi_0002_stats_print(struct cfi *);
+#define CFI_0002_STATS_INIT(dev, cfi)			\
+    do {						\
+	aprint_normal_dev(dev, "cfi=%p\n", cfi);	\
+	cfi_0002_stats_reset(cfi);			\
+    } while (0)
+#define CFI_0002_STATS_INC(cfi, field)	(cfi)->cfi_0002_stats.field++
+
+#else
+
+#define CFI_0002_STATS_INIT(cfi)
+#define CFI_0002_STATS_INC(cfi, field)
+
+#endif	/* CFI_0002_STATS */
+
+struct cfi {
+	bus_space_tag_t		cfi_bst;
+	bus_space_handle_t	cfi_bsh;
+	cfi_state_t		cfi_state;
+	uint8_t			cfi_portwidth;	/* port width, 1<<N bytes */
+	uint8_t			cfi_chipwidth;	/* chip width, 1<<N bytes */
+	struct cfi_query_data	cfi_qry_data;	/* CFI Query data */
+	struct cfi_jedec_id_data
+				cfi_id_data;	/* JEDEC ID data */
+	const struct cfi_opmodes 
+			       *cfi_opmode;
+	struct cfi_ops		cfi_ops;	/* chip dependent functions */
+	u_long			cfi_yield_time;	/* thresh. for yield in wait */
+#ifdef CFI_0002_STATS
+	struct cfi_0002_stats	cfi_0002_stats;
 #endif
+};
+
+
+enum {
+	CFI_ADDRESS_ANY = 0x00,		    /* XXX "don't care" */
+
+	CFI_RESET_DATA = 0xf0,
+	CFI_ALT_RESET_DATA = 0xff,
+
+	CFI_QUERY_MODE_ADDRESS = 0x55,	    /* some devices accept anything */
+	CFI_QUERY_MODE_ALT_ADDRESS = 0x555,
+	CFI_QUERY_DATA = 0x98,
+};
+
+static inline void
+cfi_reset(struct cfi * const cfi)
+{
+	KASSERT(cfi->cfi_ops.cfi_reset != NULL);
+	cfi->cfi_ops.cfi_reset(cfi);
+}
+
+static inline int
+cfi_erase_sector(struct cfi * const cfi, flash_off_t offset)
+{
+	KASSERT(cfi->cfi_ops.cfi_erase_sector != NULL);
+	return cfi->cfi_ops.cfi_erase_sector(cfi, offset);
+}
+
+static inline int
+cfi_program_word(struct cfi * const cfi, flash_off_t offset)
+{
+	KASSERT(cfi->cfi_ops.cfi_program_word != NULL);
+	return cfi->cfi_ops.cfi_program_word(cfi, offset);
+}
+
+extern const struct nor_interface nor_interface_cfi;
+
+extern bool cfi_probe(struct cfi * const);
+extern bool cfi_identify(struct cfi * const);
+extern void cfi_print(device_t, struct cfi * const);
+extern void cfi_reset_default(struct cfi * const);
+extern void cfi_reset_std(struct cfi * const);
+extern void cfi_reset_alt(struct cfi * const);
+extern void cfi_cmd(struct cfi * const, bus_size_t, uint32_t);
+
+#endif	/* _CFI_H_ */
