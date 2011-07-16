9f4406a64a5deab32848a5d1943a54d2431241cd
riz <riz@NetBSD.org>
2011-07-16 00:45:36 +0000

    dist/bind/lib/dns/masterdump.c                          patch dist/bind/lib/dns/message.c                             patch dist/bind/lib/dns/ncache.c                              patch dist/bind/lib/dns/rbtdb.c                               patch dist/bind/lib/dns/rdataset.c                            patch dist/bind/lib/dns/resolver.c                            patch dist/bind/lib/dns/validator.c                           patch dist/bind/lib/dns/include/dns/rdataset.h                patch
            Patch bind to address CVE-2011-2464.
            [spz, ticket #1431]

--- dist/bind/lib/dns/validator.c
+++ dist/bind/lib/dns/validator.c
@@ -1,7 +1,7 @@
-/*        $NetBSD: validator.c,v 1.4.4.6 2011/06/18 10:36:31 bouyer Exp $      */
+/*        $NetBSD: validator.c,v 1.4.4.7 2011/07/16 00:45:38 riz Exp $      */
 
 /*
- * Copyright (C) 2004-2010  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2004-2011  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 2000-2003  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and/or distribute this software for any
@@ -122,6 +122,8 @@
 
 #define SHUTDOWN(v)		(((v)->attributes & VALATTR_SHUTDOWN) != 0)
 #define CANCELED(v)		(((v)->attributes & VALATTR_CANCELED) != 0)
+
+#define NEGATIVE(r)    (((r)->attributes & DNS_RDATASETATTR_NEGATIVE) != 0)
 
 static void
 destroy(dns_validator_t *val);
@@ -646,7 +648,7 @@ dsvalidated(isc_task_t *task, isc_event_t *event) {
 		name = dns_fixedname_name(&val->fname);
 		if ((val->attributes & VALATTR_INSECURITY) != 0 &&
 		    val->frdataset.covers == dns_rdatatype_ds &&
-		    val->frdataset.type == 0 &&
+		    NEGATIVE(&val->frdataset) &&
 		    isdelegation(name, &val->frdataset, DNS_R_NCACHENXRRSET)) {
 			if (val->mustbesecure) {
 				validator_log(val, ISC_LOG_WARNING,
@@ -3315,7 +3317,7 @@ validator_start(isc_task_t *task, isc_event_t *event) {
 			val->attributes |= VALATTR_NEEDNODATA;
 		result = nsecvalidate(val, ISC_FALSE);
 	} else if (val->event->rdataset != NULL &&
-		    val->event->rdataset->type == 0)
+		    NEGATIVE(val->event->rdataset))
 	{
 		/*
 		 * This is a nonexistence validation.
