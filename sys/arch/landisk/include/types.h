c059118feb94b8048e871f91cdff32c606e292f4
dyoung <dyoung@NetBSD.org>
2011-07-17 23:48:35 +0000

    Switch to new-style <sys/bus.h>.

--- sys/arch/landisk/include/types.h
+++ sys/arch/landisk/include/types.h
@@ -1,8 +1,10 @@
-/*	$NetBSD: types.h,v 1.3 2006/09/20 00:41:13 uwe Exp $	*/
+/*	$NetBSD: types.h,v 1.4 2011/07/17 23:48:35 dyoung Exp $	*/
 
 #ifndef _LANDISK_TYPES_H_
 #define	_LANDISK_TYPES_H_
 
 #include <sh3/types.h>
+
+#define __HAVE_NEW_STYLE_BUS_H
 
 #endif /* _LANDISK_TYPES_H_ */
