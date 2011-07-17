47a841ecdc432f67b2e75385f05fabbcdee0eacc
dyoung <dyoung@NetBSD.org>
2011-07-17 23:40:16 +0000

    Switch to new-style <sys/bus.h>.

--- sys/arch/atari/include/types.h
+++ sys/arch/atari/include/types.h
@@ -1,10 +1,10 @@
-/*	$NetBSD: types.h,v 1.14 2011/06/12 03:35:40 rmind Exp $	*/
+/*	$NetBSD: types.h,v 1.15 2011/07/17 23:40:16 dyoung Exp $	*/
 
 #ifndef _MACHINE_TYPES_H_
 #define	_MACHINE_TYPES_H_
 
 #include <m68k/types.h>
 
-#define	__HAVE_MM_MD_READWRITE
+#define __HAVE_NEW_STYLE_BUS_H
 
 #endif
