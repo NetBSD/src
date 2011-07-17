7138e570fb1aa97a7a33f407f7fb20e5ce3e5563
dyoung <dyoung@NetBSD.org>
2011-07-17 23:32:21 +0000

    Switch to new-style <sys/bus.h>. hp700 releases are unbuildable for reasons not related to my changes here, so I'm not 100% that a release build will run to completion with these changes in place.

--- sys/arch/hp700/include/types.h
+++ sys/arch/hp700/include/types.h
@@ -1,11 +1,11 @@
-/*	$NetBSD: types.h,v 1.11 2011/06/12 03:35:41 rmind Exp $	*/
+/*	$NetBSD: types.h,v 1.12 2011/07/17 23:32:21 dyoung Exp $	*/
 
 #ifndef	_HP700_TYPES_H_
 #define	_HP700_TYPES_H_
 
 #include <hppa/types.h>
 
-#define	__HAVE_DEVICE_REGISTER
+#define	__HAVE_NEW_STYLE_BUS_H
 
 #define	__HAVE_MM_MD_DIRECT_MAPPED_PHYS
 #define	__HAVE_MM_MD_KERNACC
