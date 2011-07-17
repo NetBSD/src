d181d57cd18c714d3a7fdb490055f71d8298aeeb
dyoung <dyoung@NetBSD.org>
2011-07-17 23:23:53 +0000

    Enable new-style <sys/bus.h> on several PowerPC ports supporting PCI buses.  Make non-inline implementations of bus_space(9) and bus_dma(9) routines and move them to appropriate .c files.
    This may leave amigappc in a bad state, sorry.  Fortunately, it will be
    easy to repair by imitating the bus.h -> bus_{defs,funcs}.h split in
    some other PowerPC port.

--- sys/arch/ofppc/include/types.h
+++ sys/arch/ofppc/include/types.h
@@ -1,5 +1,5 @@
-/*	$NetBSD: types.h,v 1.10 2008/01/20 18:09:08 joerg Exp $	*/
+/*	$NetBSD: types.h,v 1.11 2011/07/17 23:23:53 dyoung Exp $	*/
 
-#define	__HAVE_DEVICE_REGISTER
+#define	__HAVE_NEW_STYLE_BUS_H
 
 #include <powerpc/types.h>
