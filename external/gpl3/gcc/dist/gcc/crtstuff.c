74f8aaba7b520844705c2017411296e0b9fe0873
drochner <drochner>
2011-07-18 21:48:03 +0000

    let gcc-4.5 use dl_iterate_phdr (which has been present for a while) for exception handling rather than register_frame_info -- this is what other OSes are doing, and what is supposedly more efficient. committed generated files for i386 only

--- external/gpl3/gcc/dist/gcc/crtstuff.c
+++ external/gpl3/gcc/dist/gcc/crtstuff.c
@@ -92,6 +92,15 @@ call_ ## FUNC (void)					\
     && !defined(OBJECT_FORMAT_FLAT) \
     && defined(HAVE_LD_EH_FRAME_HDR) \
     && !defined(inhibit_libc) && !defined(CRTSTUFFT_O) \
+    && defined(__NetBSD__)
+#include <link.h>
+# define USE_PT_GNU_EH_FRAME
+#endif
+
+#if defined(OBJECT_FORMAT_ELF) \
+    && !defined(OBJECT_FORMAT_FLAT) \
+    && defined(HAVE_LD_EH_FRAME_HDR) \
+    && !defined(inhibit_libc) && !defined(CRTSTUFFT_O) \
     && defined(__GLIBC__) && __GLIBC__ >= 2
 #include <link.h>
 /* uClibc pretends to be glibc 2.2 and DT_CONFIG is defined in its link.h.
