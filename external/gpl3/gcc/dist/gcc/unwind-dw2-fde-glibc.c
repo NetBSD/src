cd7d34b311b67db2094abd234ffa154cacc2cca0
drochner <drochner@NetBSD.org>
2011-07-18 21:48:03 +0000

    let gcc-4.5 use dl_iterate_phdr (which has been present for a while) for exception handling rather than register_frame_info -- this is what other OSes are doing, and what is supposedly more efficient. committed generated files for i386 only

--- external/gpl3/gcc/dist/gcc/unwind-dw2-fde-glibc.c
+++ external/gpl3/gcc/dist/gcc/unwind-dw2-fde-glibc.c
@@ -57,6 +57,12 @@
 # define USE_PT_GNU_EH_FRAME
 #endif
 
+#if !defined(inhibit_libc) && defined(HAVE_LD_EH_FRAME_HDR) \
+    && defined(__NetBSD__)
+# define ElfW(n) Elf_##n
+# define USE_PT_GNU_EH_FRAME
+#endif
+
 #if defined(USE_PT_GNU_EH_FRAME)
 
 #ifndef __RELOC_POINTER
