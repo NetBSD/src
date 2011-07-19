5456dd757faf0b84034967df837b4abf6eb92d6c
dyoung <dyoung>
2011-07-19 15:17:20 +0000

    Change <machine/bus.h> to <sys/bus.h> throughout.
    Split bus.h -> bus_{defs,funcs}.h.
    
    Mark mmeye/bus.h obsolete.

--- sys/arch/mmeye/include/bus_defs.h
+++ sys/arch/mmeye/include/bus_defs.h
@@ -1,13 +1,13 @@
-/*	$NetBSD: bus.h,v 1.3 2011/03/23 13:01:04 tsutsui Exp $	*/
+/*	$NetBSD: bus_defs.h,v 1.1 2011/07/19 15:17:20 dyoung Exp $	*/
 
-#ifndef _MMEYE_BUS_H_
-#define _MMEYE_BUS_H_
+#ifndef _MMEYE_BUS_DEFS_H_
+#define _MMEYE_BUS_DEFS_H_
 
-#include <sh3/bus.h>
+#include <sh3/bus_defs.h>
 
 /*
  * Dummy bus_dma(9)
- * XXXX: mmeye don't use bus_dma.
+ * XXX: mmeye doesn't use bus_dma.
  */
 typedef void *bus_dma_tag_t;
 
@@ -23,4 +23,4 @@ typedef struct bus_dmamap {
 	bus_dma_segment_t *dm_segs;
 } *bus_dmamap_t;
 
-#endif	/* _MMEYE_BUS_H_ */
+#endif	/* _MMEYE_BUS_DEFS_H_ */
---
+++ sys/arch/mmeye/include/bus_funcs.h
@@ -0,0 +1,3 @@
+/*	$NetBSD: bus_funcs.h,v 1.1 2011/07/19 15:17:20 dyoung Exp $	*/
+
+#include <sh3/bus_funcs.h>
