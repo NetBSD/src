b602207310a8d5c03f6ca206f44f3dc0e7250a4a
macallan <macallan@NetBSD.org>
2011-07-28 01:30:22 +0000

    userland needs to see PCI_MAGIC_IO_RANGE

--- sys/arch/ofppc/include/param.h
+++ sys/arch/ofppc/include/param.h
@@ -1,12 +1,12 @@
-/*	$NetBSD: param.h,v 1.12 2011/06/20 06:29:53 matt Exp $	*/
+/*	$NetBSD: param.h,v 1.13 2011/07/28 01:30:22 macallan Exp $	*/
 
 #if defined(_KERNEL) && !defined(_MODULE)
 
 #define MACHINE			"ofppc"
 
+#endif /* _KERNEL && !_MODULE */
+
 /* at this offset we mmap() the PCI IO range in display drivers */
 #define PCI_MAGIC_IO_RANGE      0xfeff0000
-
-#endif /* _KERNEL && !_MODULE */
 
 #include <powerpc/param.h>
