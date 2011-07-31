dbd9a744fdd08a0a756cf1964f99c68d65440fac
tron <tron@NetBSD.org>
2011-07-31 10:01:44 +0000

    Import Postfix version 2.8.4. Changes since version 2.8.3: - Performance: a high load of DSN success notification requests could   slow down the queue manager. Solution: make the trace client   asynchronous, just like the bounce and defer clients. - The local(8) delivery agent ignored table lookup errors in   mailbox_command_maps, mailbox_transport_maps, fallback_transport_maps   and (while bouncing mail to alias) alias owner lookup. - Workaround: dbl.spamhaus.org rejects lookups with "No IP queries" even   if the name has an alphanumerical prefix. We play safe, and skip both   RHSBL and RHSWL queries for names ending in a numerical suffix. - The "sendmail -t" command reported "protocol error" instead of "file   too large", "no space left on device" etc. - The Postfix Milter client reported a temporary error instead of   "file too large" in three cases.

--- external/ibm-public/postfix/dist/src/global/abounce.h
+++ external/ibm-public/postfix/dist/src/global/abounce.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: abounce.h,v 1.1.1.1 2009/06/23 10:08:44 tron Exp $	*/
+/*	$NetBSD: abounce.h,v 1.1.1.2 2011/07/31 10:02:34 tron Exp $	*/
 
 #ifndef _ABOUNCE_H_INCLUDED_
 #define _ABOUNCE_H_INCLUDED_
@@ -26,6 +26,7 @@ typedef void (*ABOUNCE_FN) (int, char *);
 extern void abounce_flush(int, const char *, const char *, const char *, const char *, const char *, int, ABOUNCE_FN, char *);
 extern void adefer_flush(int, const char *, const char *, const char *, const char *, const char *, int, ABOUNCE_FN, char *);
 extern void adefer_warn(int, const char *, const char *, const char *, const char *, const char *, int, ABOUNCE_FN, char *);
+extern void atrace_flush(int, const char *, const char *, const char *, const char *, const char *, int, ABOUNCE_FN, char *);
 
 extern void abounce_flush_verp(int, const char *, const char *, const char *, const char *, const char *, int, const char *, ABOUNCE_FN, char *);
 extern void adefer_flush_verp(int, const char *, const char *, const char *, const char *, const char *, int, const char *, ABOUNCE_FN, char *);
