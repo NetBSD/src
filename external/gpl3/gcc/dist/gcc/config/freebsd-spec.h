42b6963a2a5ab73facdcb261de10402a7e499a64
matt <matt@NetBSD.org>
2011-07-31 03:43:33 +0000

    Don't define LINK_EH_SPEC if it's already defined (from netbsd*.h)

--- external/gpl3/gcc/dist/gcc/config/freebsd-spec.h
+++ external/gpl3/gcc/dist/gcc/config/freebsd-spec.h
@@ -153,7 +153,7 @@ is built with the --enable-threads configure-time option.}		\
 #define FBSD_DYNAMIC_LINKER "/libexec/ld-elf.so.1"
 #endif
 
-#if defined(HAVE_LD_EH_FRAME_HDR)
+#if defined(HAVE_LD_EH_FRAME_HDR) && !defined(LINK_EH_SPEC)
 #define LINK_EH_SPEC "%{!static:--eh-frame-hdr} "
 #endif
 
