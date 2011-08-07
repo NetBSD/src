4d52a7ba18a7f21e690fd41ca1bec8dd5940a0ca
mrg <mrg@NetBSD.org>
2011-08-07 12:20:10 +0000

    re-run mknative for hppa.

--- external/gpl3/gcc/lib/libstdc++-v3/arch/hppa/gstdint.h
+++ external/gpl3/gcc/lib/libstdc++-v3/arch/hppa/gstdint.h
@@ -2,7 +2,7 @@
 /* Generated from: 	NetBSD: mknative-gcc,v 1.61 2011/07/03 12:26:02 mrg Exp  */
 /* Generated from: NetBSD: mknative.common,v 1.9 2007/02/05 18:26:01 apb Exp  */
 
-/* generated for  hppa--netbsd-gcc (GCC) 4.1.3 20080704 (prerelease) (NetBSD nb2 20081120) */
+/* generated for  hppa--netbsd-gcc (NetBSD nb1 20110620) 4.5.3 */
 
 #ifndef GCC_GENERATED_STDINT_H
 #define GCC_GENERATED_STDINT_H 1
