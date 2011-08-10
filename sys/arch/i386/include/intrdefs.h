9bb602ca29c4098d6e799062405cf34e88b921bf
cherry <cherry@NetBSD.org>
2011-08-10 06:30:59 +0000

    Include Xen specific definitions.

--- sys/arch/i386/include/intrdefs.h
+++ sys/arch/i386/include/intrdefs.h
@@ -1,3 +1,6 @@
-/*	$NetBSD: intrdefs.h,v 1.3 2003/02/26 21:29:01 fvdl Exp $	*/
+/*	$NetBSD: intrdefs.h,v 1.4 2011/08/10 06:30:59 cherry Exp $	*/
 
 #include <x86/intrdefs.h>
+#ifdef XEN
+#include <xen/intrdefs.h>
+#endif /* XEN */
