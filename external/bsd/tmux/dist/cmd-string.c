073c5bac5cb37efda5e46a6430336ee2715b3f73
jmmv <jmmv>
2011-08-17 18:48:34 +0000

    Fix merge conflicts after tmux 1.5 import.

--- external/bsd/tmux/dist/cmd-string.c
+++ external/bsd/tmux/dist/cmd-string.c
@@ -1,4 +1,4 @@
-/* $Id: cmd-string.c,v 1.2 2011/03/12 03:02:58 christos Exp $ */
+/* $Id: cmd-string.c,v 1.3 2011/08/17 18:48:36 jmmv Exp $ */
 
 /*
  * Copyright (c) 2008 Nicholas Marriott <nicm@users.sourceforge.net>
