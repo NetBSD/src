5c901f17861e8dc3f2745ea779c0809d5c739257
reinoud <reinoud@NetBSD.org>
2011-08-22 15:30:16 +0000

    Add thunk_munmap() to NetBSD/usermode's thunk

--- sys/arch/usermode/usermode/thunk.c
+++ sys/arch/usermode/usermode/thunk.c
@@ -1,4 +1,4 @@
-/* $NetBSD: thunk.c,v 1.8 2011/08/21 17:11:59 reinoud Exp $ */
+/* $NetBSD: thunk.c,v 1.9 2011/08/22 15:30:16 reinoud Exp $ */
 
 /*-
  * Copyright (c) 2011 Jared D. McNeill <jmcneill@invisible.ca>
@@ -27,7 +27,7 @@
  */
 
 #include <sys/cdefs.h>
-__RCSID("$NetBSD: thunk.c,v 1.8 2011/08/21 17:11:59 reinoud Exp $");
+__RCSID("$NetBSD: thunk.c,v 1.9 2011/08/22 15:30:16 reinoud Exp $");
 
 #include <machine/thunk.h>
 
@@ -206,6 +206,12 @@ void *
 thunk_mmap(void *addr, size_t len, int prot, int flags, int fd, off_t offset)
 {
 	return mmap(addr, len, prot, flags, fd, offset);
+}
+
+int
+thunk_munmap(void *addr, size_t len)
+{
+	return munmap(addr, len);
 }
 
 int
