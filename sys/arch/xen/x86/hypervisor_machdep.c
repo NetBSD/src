c16adfd110582951e6740b2d91e8b17e1c4c0658
jym <jym@NetBSD.org>
2011-08-27 15:56:48 +0000

    Rename the functions for suspend to reflect that Xen does not hijack the ACPI "sleepstate" sysctl(7) node anymore.
    Add a boolean value to mark that the save/suspend operation has been
    notified by dom0, so as to avoid possible errors where admin would like
    to schedule the domain for sleep without dom0 being prepared for that. Fail
    with EAGAIN in this case.
    
    Sprinkle some KNF.

--- sys/arch/xen/x86/hypervisor_machdep.c
+++ sys/arch/xen/x86/hypervisor_machdep.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: hypervisor_machdep.c,v 1.11.8.9 2011/08/27 15:44:09 jym Exp $	*/
+/*	$NetBSD: hypervisor_machdep.c,v 1.11.8.10 2011/08/27 15:56:48 jym Exp $	*/
 
 /*
  *
@@ -54,7 +54,7 @@
 
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: hypervisor_machdep.c,v 1.11.8.9 2011/08/27 15:44:09 jym Exp $");
+__KERNEL_RCSID(0, "$NetBSD: hypervisor_machdep.c,v 1.11.8.10 2011/08/27 15:56:48 jym Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -383,7 +383,7 @@ hypervisor_machdep_attach(void)
  	/* dom0 does not require the arch-dependent P2M translation table */
 	if (!xendomain_is_dom0()) {
 		build_p2m_frame_list_list();
-		sysctl_xen_sleepstate_setup();
+		sysctl_xen_suspend_setup();
 	}
 }
 
