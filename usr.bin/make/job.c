3b5e4335df6caaf55dcb8ea0de1214be64a42da1
sjg <sjg@NetBSD.org>
2011-08-28 03:54:07 +0000

    In meta mode, we create .meta files for most targets. These capture all the interesting data - useful for debugging. In such cases there is no need to replicate commands in the build log. Rather than run the entire build .SILENT, allow meta mode to set that flag per target iff a .meta file is created. Normal behavior is retained for targets where no .meta file is created, ensuring that no build data is lost.

--- usr.bin/make/job.c
+++ usr.bin/make/job.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: job.c,v 1.158 2011/08/14 13:06:09 christos Exp $	*/
+/*	$NetBSD: job.c,v 1.159 2011/08/28 03:54:07 sjg Exp $	*/
 
 /*
  * Copyright (c) 1988, 1989, 1990 The Regents of the University of California.
@@ -70,14 +70,14 @@
  */
 
 #ifndef MAKE_NATIVE
-static char rcsid[] = "$NetBSD: job.c,v 1.158 2011/08/14 13:06:09 christos Exp $";
+static char rcsid[] = "$NetBSD: job.c,v 1.159 2011/08/28 03:54:07 sjg Exp $";
 #else
 #include <sys/cdefs.h>
 #ifndef lint
 #if 0
 static char sccsid[] = "@(#)job.c	8.2 (Berkeley) 3/19/94";
 #else
-__RCSID("$NetBSD: job.c,v 1.158 2011/08/14 13:06:09 christos Exp $");
+__RCSID("$NetBSD: job.c,v 1.159 2011/08/28 03:54:07 sjg Exp $");
 #endif
 #endif /* not lint */
 #endif
@@ -1592,6 +1592,9 @@ JobStart(GNode *gn, int flags)
 #ifdef USE_META
 	if (useMeta) {
 	    meta_job_start(job, gn);
+	    if (Targ_Silent(gn)) {	/* might have changed */
+		job->flags |= JOB_SILENT;
+	    }
 	}
 #endif
 	/*
