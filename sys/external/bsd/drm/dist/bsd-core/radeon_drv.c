b4d729eb337f2b955649454a51cb85b6f27ed06d
jmcneill <jmcneill>
2011-08-28 15:40:49 +0000

    set default WARNS for modules to 3 -- the only one that needs < 3 now is pf

--- sys/external/bsd/drm/dist/bsd-core/radeon_drv.c
+++ sys/external/bsd/drm/dist/bsd-core/radeon_drv.c
@@ -201,64 +201,36 @@ radeondrm_detach(device_t self, int flags)
 CFATTACH_DECL_NEW(radeondrm, sizeof(struct drm_device),
     radeondrm_probe, radeondrm_attach, radeondrm_detach, NULL);
 
+MODULE(MODULE_CLASS_DRIVER, radeondrm, "drm");
+
 #ifdef _MODULE
-
-MODULE(MODULE_CLASS_DRIVER, radeondrm, NULL);
-
-CFDRIVER_DECL(radeondrm, DV_DULL, NULL);
-extern struct cfattach radeondrm_ca;
-static int drmloc[] = { -1 };
-static struct cfparent drmparent = {
-	"drm", "vga", DVUNIT_ANY
-};
-static struct cfdata radeondrm_cfdata[] = {
-	{
-		.cf_name = "radeondrm",
-		.cf_atname = "radeondrm",
-		.cf_unit = 0,
-		.cf_fstate = FSTATE_STAR,
-		.cf_loc = drmloc,
-		.cf_flags = 0,
-		.cf_pspec = &drmparent,
-	},
-	{ NULL }
-};
+#include "ioconf.c"
+#endif
 
 static int
 radeondrm_modcmd(modcmd_t cmd, void *arg)
 {
-	int err;
+	int error = 0;
 
 	switch (cmd) {
 	case MODULE_CMD_INIT:
-		err = config_cfdriver_attach(&radeondrm_cd);
-		if (err)
-			return err;
-		err = config_cfattach_attach("radeondrm", &radeondrm_ca);
-		if (err) {
-			config_cfdriver_detach(&radeondrm_cd);
-			return err;
-		}
-		err = config_cfdata_attach(radeondrm_cfdata, 1);
-		if (err) {
-			config_cfattach_detach("radeondrm", &radeondrm_ca);
-			config_cfdriver_detach(&radeondrm_cd);
-			return err;
-		}
-		return 0;
+#ifdef _MODULE
+		error = config_init_component(cfdriver_ioconf_radeondrm,
+		    cfattach_ioconf_radeondrm, cfdata_ioconf_radeondrm);
+#endif
+		break;
 	case MODULE_CMD_FINI:
-		err = config_cfdata_detach(radeondrm_cfdata);
-		if (err)
-			return err;
-		config_cfattach_detach("radeondrm", &radeondrm_ca);
-		config_cfdriver_detach(&radeondrm_cd);
-		return 0;
+#ifdef _MODULE
+		error = config_fini_component(cfdriver_ioconf_radeondrm,
+		    cfattach_ioconf_radeondrm, cfdata_ioconf_radeondrm);
+#endif
+		break;
 	default:
 		return ENOTTY;
 	}
-}
-#endif /* _MODULE */
 
+	return error;
+}
 
 #include <dev/firmload.h>
 
