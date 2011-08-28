d0fb0909ca5ef70a19bae9716a822c9f7a4e2601
jmcneill <jmcneill@NetBSD.org>
2011-08-28 16:19:07 +0000

    add mach64drm, mgadrm, r128drm, savagedrm, sisdrm, and tdfxdrm modules

--- sys/external/bsd/drm/dist/bsd-core/mach64_drv.c
+++ sys/external/bsd/drm/dist/bsd-core/mach64_drv.c
@@ -163,62 +163,35 @@ mach64drm_attach(device_t parent, device_t self, void *aux)
 CFATTACH_DECL_NEW(mach64drm, sizeof(struct drm_device),
     mach64drm_probe, mach64drm_attach, drm_detach, NULL);
 
+MODULE(MODULE_CLASS_DRIVER, mach64drm, "drm");
+
 #ifdef _MODULE
-
-MODULE(MODULE_CLASS_DRIVER, mach64drm, NULL);
-
-CFDRIVER_DECL(mach64drm, DV_DULL, NULL);
-extern struct cfattach mach64drm_ca;
-static int drmloc[] = { -1 };
-static struct cfparent drmparent = {
-	"drm", "vga", DVUNIT_ANY
-};
-static struct cfdata mach64drm_cfdata[] = {
-	{
-		.cf_name = "mach64drm",
-		.cf_atname = "mach64drm",
-		.cf_unit = 0,
-		.cf_fstate = FSTATE_STAR,
-		.cf_loc = drmloc,
-		.cf_flags = 0,
-		.cf_pspec = &drmparent,
-	},
-	{ NULL }
-};
+#include "ioconf.c"
+#endif
 
 static int
 mach64drm_modcmd(modcmd_t cmd, void *arg)
 {
-	int err;
+	int error = 0;
 
 	switch (cmd) {
 	case MODULE_CMD_INIT:
-		err = config_cfdriver_attach(&mach64drm_cd);
-		if (err)
-			return err;
-		err = config_cfattach_attach("mach64drm", &mach64drm_ca);
-		if (err) {
-			config_cfdriver_detach(&mach64drm_cd);
-			return err;
-		}
-		err = config_cfdata_attach(mach64drm_cfdata, 1);
-		if (err) {
-			config_cfattach_detach("mach64drm", &mach64drm_ca);
-			config_cfdriver_detach(&mach64drm_cd);
-			return err;
-		}
-		return 0;
+#ifdef _MODULE
+		error = config_init_component(cfdriver_ioconf_mach64drm,
+		    cfattach_ioconf_mach64drm, cfdata_ioconf_mach64drm);
+#endif
+		break;
 	case MODULE_CMD_FINI:
-		err = config_cfdata_detach(mach64drm_cfdata);
-		if (err)
-			return err;
-		config_cfattach_detach("mach64drm", &mach64drm_ca);
-		config_cfdriver_detach(&mach64drm_cd);
-		return 0;
+#ifdef _MODULE
+		error = config_fini_component(cfdriver_ioconf_mach64drm,
+		    cfattach_ioconf_mach64drm, cfdata_ioconf_mach64drm);
+#endif
+		break;
 	default:
 		return ENOTTY;
 	}
+
+	return 0;
 }
-#endif /* _MODULE */
 
 #endif
