a8c86ec231782ea3feb0665a3c0c17369d8f1f35
reinoud <reinoud@NetBSD.org>
2011-09-05 11:56:52 +0000

    Initialise the alternative signal stack before referring to it

--- sys/arch/usermode/usermode/trap.c
+++ sys/arch/usermode/usermode/trap.c
@@ -1,4 +1,4 @@
-/* $NetBSD: trap.c,v 1.24 2011/09/05 11:10:36 reinoud Exp $ */
+/* $NetBSD: trap.c,v 1.25 2011/09/05 11:56:52 reinoud Exp $ */
 
 /*-
  * Copyright (c) 2011 Reinoud Zandijk <reinoud@netbsd.org>
@@ -27,7 +27,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: trap.c,v 1.24 2011/09/05 11:10:36 reinoud Exp $");
+__KERNEL_RCSID(0, "$NetBSD: trap.c,v 1.25 2011/09/05 11:56:52 reinoud Exp $");
 
 #include <sys/types.h>
 #include <sys/param.h>
@@ -70,6 +70,14 @@ setup_signal_handlers(void)
 {
 	static struct sigaction sa;
 
+	if ((sigstk.ss_sp = thunk_malloc(SIGSTKSZ)) == NULL)
+		panic("can't allocate signal stack space\n");
+	sigstk.ss_size  = SIGSTKSZ;
+	sigstk.ss_flags = 0;
+	if (thunk_sigaltstack(&sigstk, 0) < 0)
+		panic("can't set alternate stacksize : %d",
+		    thunk_geterrno());
+
 	sigemptyset(&sa.sa_mask);
 	sa.sa_flags = SA_RESTART | SA_SIGINFO | SA_ONSTACK;
 	sa.sa_sigaction = mem_access_handler;
@@ -86,14 +94,6 @@ setup_signal_handlers(void)
 	thunk_sigaddset(&sa.sa_mask, SIGALRM);
 	if (thunk_sigaction(SIGILL, &sa, NULL) == -1)
 		panic("couldn't register SIGILL handler : %d", thunk_geterrno());
-
-	if ((sigstk.ss_sp = thunk_malloc(SIGSTKSZ)) == NULL)
-		panic("can't allocate signal stack space\n");
-	sigstk.ss_size  = SIGSTKSZ;
-	sigstk.ss_flags = 0;
-	if (thunk_sigaltstack(&sigstk, 0) < 0)
-		panic("can't set alternate stacksize : %d",
-		    thunk_geterrno());
 
 //	debug_fh = thunk_open("/usr/sources/debug", O_RDWR | O_TRUNC | O_CREAT, 0666);
 }
