4a2bcd835ab0c6892137bfeccef5be381316cefd
jruoho <jruoho@NetBSD.org>
2011-09-13 08:58:41 +0000

    Assign missing copyright to TNF.
    XXX: If these originated from you, please modify accordingly.

--- tests/lib/libm/t_round.c
+++ tests/lib/libm/t_round.c
@@ -1,4 +1,30 @@
-/* $NetBSD: t_round.c,v 1.2 2011/07/04 22:33:29 mrg Exp $ */
+/* $NetBSD: t_round.c,v 1.3 2011/09/13 08:58:41 jruoho Exp $ */
+
+/*-
+ * Copyright (c) 2011 The NetBSD Foundation, Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 
 #include <atf-c.h>
 #include <math.h>
@@ -33,14 +59,14 @@ ATF_TC_BODY(round_dir, tc)
 	b = round(a);
 	bf = roundf(af);
 
-	ATF_REQUIRE(fabs(b) < SMALL_NUM);
-	ATF_REQUIRE(fabsf(bf) < SMALL_NUM);
+	ATF_CHECK(fabs(b) < SMALL_NUM);
+	ATF_CHECK(fabsf(bf) < SMALL_NUM);
 
 	c = round(-a);
 	cf = roundf(-af);
 
-	ATF_REQUIRE(fabs(c) < SMALL_NUM);
-	ATF_REQUIRE(fabsf(cf) < SMALL_NUM);
+	ATF_CHECK(fabs(c) < SMALL_NUM);
+	ATF_CHECK(fabsf(cf) < SMALL_NUM);
 }
 
 ATF_TP_ADD_TCS(tp)
