992092eb22283b664c73fd53a246d3f28b8d82c5
jruoho <jruoho@NetBSD.org>
2011-09-14 06:50:43 +0000

    Use fabsf(3) when appropriate.

--- tests/lib/libm/t_sin.c
+++ tests/lib/libm/t_sin.c
@@ -1,4 +1,4 @@
-/* $NetBSD: t_sin.c,v 1.1 2011/09/14 05:18:19 jruoho Exp $ */
+/* $NetBSD: t_sin.c,v 1.2 2011/09/14 06:50:43 jruoho Exp $ */
 
 /*-
  * Copyright (c) 2011 The NetBSD Foundation, Inc.
@@ -173,7 +173,7 @@ ATF_TC_BODY(sinf_angles, tc)
 		x = angles[i].x;
 		y = angles[i].y;
 
-		if (fabs(sinf(x) - y) > eps)
+		if (fabsf(sinf(x) - y) > eps)
 			atf_tc_fail_nonfatal("sinf(%d deg) != %0.01f",
 			    angles[i].angle, angles[i].y);
 	}
