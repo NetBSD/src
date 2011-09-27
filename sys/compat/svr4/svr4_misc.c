943e2ae38541aaa05107c1f210842602a4387838
christos <christos@NetBSD.org>
2011-09-27 00:52:55 +0000

    return the namemax from the bsd statvfs which is filesystem dependent, not a random value.

--- sys/compat/svr4/svr4_misc.c
+++ sys/compat/svr4/svr4_misc.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: svr4_misc.c,v 1.154 2010/11/30 10:43:02 dholland Exp $	 */
+/*	$NetBSD: svr4_misc.c,v 1.155 2011/09/27 00:52:55 christos Exp $	 */
 
 /*-
  * Copyright (c) 1994, 2008 The NetBSD Foundation, Inc.
@@ -37,7 +37,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: svr4_misc.c,v 1.154 2010/11/30 10:43:02 dholland Exp $");
+__KERNEL_RCSID(0, "$NetBSD: svr4_misc.c,v 1.155 2011/09/27 00:52:55 christos Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -1076,7 +1076,7 @@ svr4_copyout_statvfs(const struct statvfs *bfs, struct svr4_statvfs *sufs)
 		sfs->f_flag |= SVR4_ST_RDONLY;
 	if (bfs->f_flag & MNT_NOSUID)
 		sfs->f_flag |= SVR4_ST_NOSUID;
-	sfs->f_namemax = MAXNAMLEN;
+	sfs->f_namemax = bfs->f_namemax;
 	memcpy(sfs->f_fstr, bfs->f_fstypename, sizeof(sfs->f_fstr)); /* XXX */
 	memset(sfs->f_filler, 0, sizeof(sfs->f_filler));
 
@@ -1108,7 +1108,7 @@ svr4_copyout_statvfs64(const struct statvfs *bfs, struct svr4_statvfs64 *sufs)
 		sfs->f_flag |= SVR4_ST_RDONLY;
 	if (bfs->f_flag & MNT_NOSUID)
 		sfs->f_flag |= SVR4_ST_NOSUID;
-	sfs->f_namemax = MAXNAMLEN;
+	sfs->f_namemax = bfs->f_namemax;
 	memcpy(sfs->f_fstr, bfs->f_fstypename, sizeof(sfs->f_fstr)); /* XXX */
 	memset(sfs->f_filler, 0, sizeof(sfs->f_filler));
 
