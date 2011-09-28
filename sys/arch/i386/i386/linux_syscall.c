b7a127283d79294308add6fcdfeccaaa04898fc8
christos <christos@NetBSD.org>
2011-09-28 17:27:21 +0000

    syscall (setcontext) can alter eax, so don't attempt to get code again from it in order to avoid register spills.

--- sys/arch/i386/i386/linux_syscall.c
+++ sys/arch/i386/i386/linux_syscall.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: linux_syscall.c,v 1.50 2009/11/21 03:11:00 rmind Exp $	*/
+/*	$NetBSD: linux_syscall.c,v 1.51 2011/09/28 17:27:21 christos Exp $	*/
 
 /*-
  * Copyright (c) 1998, 2000 The NetBSD Foundation, Inc.
@@ -30,7 +30,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: linux_syscall.c,v 1.50 2009/11/21 03:11:00 rmind Exp $");
+__KERNEL_RCSID(0, "$NetBSD: linux_syscall.c,v 1.51 2011/09/28 17:27:21 christos Exp $");
 
 #if defined(_KERNEL_OPT)
 #include "opt_vm86.h"
@@ -103,7 +103,6 @@ linux_syscall(struct trapframe *frame)
 		error = trace_enter(code, args, callp->sy_narg);
 		if (__predict_true(error == 0)) {
 			error = sy_call(callp, l, args, rval);
-			code = frame->tf_eax & (LINUX_SYS_NSYSENT - 1);
 			trace_exit(code, rval, error);
 		}
 	} else
