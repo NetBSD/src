bc3b670f7dcf5f929d91794967a0631622e70548
christos <christos@NetBSD.org>
2011-10-01 19:25:24 +0000

    The arm .arm.atpcs section does not have segment information, so skip it.

--- external/gpl3/binutils/dist/gas/config/tc-arm.c
+++ external/gpl3/binutils/dist/gas/config/tc-arm.c
@@ -16133,12 +16133,16 @@ check_it_blocks_finished (void)
   asection *sect;
 
   for (sect = stdoutput->sections; sect != NULL; sect = sect->next)
-    if (seg_info (sect)->tc_segment_info_data.current_it.state
-	== MANUAL_IT_BLOCK)
-      {
-	as_warn (_("section '%s' finished with an open IT block."),
-		 sect->name);
-      }
+    {
+      segment_info_type *seginfo = seg_info (sect);
+
+      if (seginfo && seginfo->tc_segment_info_data.current_it.state
+	  == MANUAL_IT_BLOCK)
+        {
+	  as_warn (_("section '%s' finished with an open IT block."),
+		   sect->name);
+        }
+    }
 #else
   if (now_it.state == MANUAL_IT_BLOCK)
     as_warn (_("file finished with an open IT block."));
