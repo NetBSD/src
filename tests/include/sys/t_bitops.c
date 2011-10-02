72fa100c93b894cb1ddd36b47cdc5faa6d228425
christos <christos@NetBSD.org>
2011-10-02 18:34:17 +0000

    remove stray underscore.

--- tests/include/sys/t_bitops.c
+++ tests/include/sys/t_bitops.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: t_bitops.c,v 1.12 2011/10/01 18:38:39 christos Exp $ */
+/*	$NetBSD: t_bitops.c,v 1.13 2011/10/02 18:34:17 christos Exp $ */
 
 /*-
  * Copyright (c) 2011 The NetBSD Foundation, Inc.
@@ -29,7 +29,7 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 #include <sys/cdefs.h>
-__RCSID("$NetBSD: t_bitops.c,v 1.12 2011/10/01 18:38:39 christos Exp $");
+__RCSID("$NetBSD: t_bitops.c,v 1.13 2011/10/02 18:34:17 christos Exp $");
 
 #include <atf-c.h>
 
@@ -165,7 +165,7 @@ ATF_TC_HEAD(ilog2_log2, tc)
 
 ATF_TC_BODY(ilog2_log2, tc)
 {
-#ifdef __vax___
+#ifdef __vax__
 	atf_tc_skip("Test is unavailable on vax because of lack of log2()");
 #else
 	double  x, y;
