4cf1067833ec8f49e23498443a7f3ae20f6f1685
rkujawa <rkujawa@NetBSD.org>
2011-10-07 08:44:21 +0000

    Added experimental support for CV64/3D PCI bridge to p5pb(4). Approved by phx.

---
+++ sys/arch/amiga/pci/p5pbvar.h
@@ -0,0 +1,58 @@
+/*	$NetBSD: p5pbvar.h,v 1.1 2011/10/07 08:44:21 rkujawa Exp $ */
+
+/*-
+ * Copyright (c) 2011 The NetBSD Foundation, Inc.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to The NetBSD Foundation
+ * by Radoslaw Kujawa.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef _AMIGA_P5PBVAR_H_
+
+#include <sys/types.h>
+#include <dev/pci/pcivar.h>
+#include <dev/pci/pciconf.h>
+#include <machine/pci_machdep.h>
+
+struct p5pb_bridge_type {
+	const char	*name;		/* descriptive name */
+	bool		configure_bus;	/* should we configure the bus? */
+	bool		set_genfb_props;/* should we set props for genfb(4)? */
+	int		maxdevs;	/* max number of devices on the bus */
+};
+
+struct p5pb_softc {
+	device_t sc_dev;
+	volatile char *ba;
+	struct bus_space_tag pci_conf_area;
+	struct bus_space_tag pci_mem_area;
+	struct bus_space_tag pci_io_area;
+	struct amiga_pci_chipset apc;
+	struct p5pb_bridge_type bridge_type;
+	bool	(*p5pb_bus_map)(struct p5pb_softc *);
+};
+
+
+#endif /* _AMIGA_P5PBVAR_H_ */
