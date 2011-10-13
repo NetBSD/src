e552cb3462c0f88545c4eef356159e0b6cb96148
matt <matt@NetBSD.org>
2011-10-13 19:51:58 +0000

    Regen.

--- external/gpl3/gcc/usr.bin/gcc/arch/powerpc/configargs.h
+++ external/gpl3/gcc/usr.bin/gcc/arch/powerpc/configargs.h
@@ -3,7 +3,7 @@
 /* Generated from: NetBSD: mknative.common,v 1.9 2007/02/05 18:26:01 apb Exp  */
 
 /* Generated automatically. */
-#ifdef _SOFT_FLOAT_
+#ifdef _SOFT_FLOAT
 static const char configuration_arguments[] = "/usr/src2/tools/gcc/../../external/gpl3/gcc/dist/configure --target=powerpc--netbsd --enable-long-long --enable-threads --with-bugurl=http://www.NetBSD.org/Misc/send-pr.html --with-pkgversion='NetBSD nb1 20110620' --enable-__cxa_atexit --with-mpc=/var/obj/ofppc/usr/src2/destdir.ofppc/usr --with-mpfr=/var/obj/ofppc/usr/src2/destdir.ofppc/usr --with-gmp=/var/obj/ofppc/usr/src2/destdir.ofppc/usr --disable-multilib --disable-symvers --disable-libstdcxx-pch --build=x86_64-unknown-netbsd5.99.54 --host=powerpc--netbsd -with-float=soft";
 #else
 static const char configuration_arguments[] = "/usr/src2/tools/gcc/../../external/gpl3/gcc/dist/configure --target=powerpc--netbsd --enable-long-long --enable-threads --with-bugurl=http://www.NetBSD.org/Misc/send-pr.html --with-pkgversion='NetBSD nb1 20110620' --enable-__cxa_atexit --with-mpc=/var/obj/ofppc/usr/src2/destdir.ofppc/usr --with-mpfr=/var/obj/ofppc/usr/src2/destdir.ofppc/usr --with-gmp=/var/obj/ofppc/usr/src2/destdir.ofppc/usr --disable-multilib --disable-symvers --disable-libstdcxx-pch --build=x86_64-unknown-netbsd5.99.54 --host=powerpc--netbsd";
@@ -13,8 +13,9 @@ static const char thread_model[] = "posix";
 static const struct {
   const char *name, *value;
 } configure_default_options[] = { 
-#ifdef _SOFT_FLOAT_
+#ifdef _SOFT_FLOAT
   { "float", "soft" },
-#endif
+#else
   { NULL, NULL }
+#endif
 };
