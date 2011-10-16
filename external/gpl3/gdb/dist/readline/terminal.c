aadbc9f925252bfa53bf8c229c9dbf10bb347ac4
christos <christos@NetBSD.org>
2011-10-16 19:41:30 +0000

    add necessary headers for this to compile without warnings

--- external/gpl3/gdb/dist/readline/terminal.c
+++ external/gpl3/gdb/dist/readline/terminal.c
@@ -32,6 +32,10 @@
 #  include <sys/file.h>
 #endif /* HAVE_SYS_FILE_H */
 
+#if defined (HAVE_SYS_IOCTL_H)
+#include <sys/ioctl.h>
+#endif /* HAVE_SYS_IOCTL_H */
+
 #if defined (HAVE_UNISTD_H)
 #  include <unistd.h>
 #endif /* HAVE_UNISTD_H */
