a997745423d1eade7e973c653d4edc7b4eb02980
isaki <isaki@NetBSD.org>
2011-10-16 03:10:17 +0000

    Switch to 2-clause license.

--- sys/arch/x68k/dev/bmd.c
+++ sys/arch/x68k/dev/bmd.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: bmd.c,v 1.17 2010/08/08 09:30:29 isaki Exp $	*/
+/*	$NetBSD: bmd.c,v 1.18 2011/10/16 03:10:17 isaki Exp $	*/
 
 /*
  * Copyright (c) 2002 Tetsuya Isaki. All rights reserved.
@@ -11,8 +11,6 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. The name of the author may not be used to endorse or promote products
- *    derived from this software without specific prior written permission
  *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
@@ -32,7 +30,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: bmd.c,v 1.17 2010/08/08 09:30:29 isaki Exp $");
+__KERNEL_RCSID(0, "$NetBSD: bmd.c,v 1.18 2011/10/16 03:10:17 isaki Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
