ba0f3693163ea5d57cefdfe08674c417f09d647e
christos <christos@NetBSD.org>
2011-10-16 19:41:30 +0000

    add necessary headers for this to compile without warnings

--- external/gpl3/gdb/dist/readline/rltty.c
+++ external/gpl3/gdb/dist/readline/rltty.c
@@ -37,9 +37,9 @@
 
 #include "rldefs.h"
 
-#if defined (GWINSZ_IN_SYS_IOCTL)
+#if defined (HAVE_SYS_IOCTL_H)
 #  include <sys/ioctl.h>
-#endif /* GWINSZ_IN_SYS_IOCTL */
+#endif /* HAVE_SYS_IOCTL_H */
 
 #include "rltty.h"
 #include "readline.h"
