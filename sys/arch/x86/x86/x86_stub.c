281d024f3467feb9c45226cca6ea1a59b942d737
dyoung <dyoung>
2011-10-18 23:25:20 +0000

    Define some optional routines that will help device_register() to register ISA & PCI devices.  Add stub implementations of the routines.

--- sys/arch/x86/x86/x86_stub.c
+++ sys/arch/x86/x86/x86_stub.c
@@ -1,13 +1,16 @@
-/* $NetBSD: x86_stub.c,v 1.1 2011/04/03 22:29:27 dyoung Exp $ */
+/* $NetBSD: x86_stub.c,v 1.2 2011/10/18 23:25:20 dyoung Exp $ */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: x86_stub.c,v 1.1 2011/04/03 22:29:27 dyoung Exp $");
+__KERNEL_RCSID(0, "$NetBSD: x86_stub.c,v 1.2 2011/10/18 23:25:20 dyoung Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
 #include <sys/kgdb.h>
 
+#include <machine/autoconf.h>
+
 int x86_nullop(void);
+void *x86_zeroop(void);
 void x86_voidop(void);
 
 void
@@ -21,8 +24,17 @@ x86_nullop(void)
 	return 0;
 }
 
-__weak_alias(kdb_trap, x86_nullop);
-__weak_alias(kgdb_disconnected, x86_nullop);
-__weak_alias(kgdb_trap, x86_nullop);
+void *
+x86_zeroop(void)
+{
+	return NULL;
+}
+
+__weak_alias(device_pci_props_register, x86_voidop);
+__weak_alias(device_pci_register, x86_nullop);
+__weak_alias(device_isa_register, x86_nullop);
+__weak_alias(kdb_trap, x86_zeroop);
+__weak_alias(kgdb_disconnected, x86_zeroop);
+__weak_alias(kgdb_trap, x86_zeroop);
 __weak_alias(mca_nmi, x86_voidop);
 __weak_alias(x86_nmi, x86_voidop);
