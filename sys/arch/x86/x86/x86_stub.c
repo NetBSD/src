06c689d43f7153aaeb53696e8d1ee19192ce91f2
dyoung <dyoung>
2011-10-19 05:01:43 +0000

    Create a stub implementation of pci_ranges_infer().

--- sys/arch/x86/x86/x86_stub.c
+++ sys/arch/x86/x86/x86_stub.c
@@ -1,13 +1,14 @@
-/* $NetBSD: x86_stub.c,v 1.3 2011/10/18 23:41:45 dyoung Exp $ */
+/* $NetBSD: x86_stub.c,v 1.4 2011/10/19 05:01:43 dyoung Exp $ */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: x86_stub.c,v 1.3 2011/10/18 23:41:45 dyoung Exp $");
+__KERNEL_RCSID(0, "$NetBSD: x86_stub.c,v 1.4 2011/10/19 05:01:43 dyoung Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
 #include <sys/kgdb.h>
 
 #include <machine/autoconf.h>
+#include <dev/pci/pcivar.h>
 
 int x86_zeroop(void);
 void *x86_nullop(void);
@@ -30,11 +31,12 @@ x86_zeroop(void)
 	return 0;
 }
 
+__weak_alias(device_isa_register, x86_nullop);
 __weak_alias(device_pci_props_register, x86_voidop);
 __weak_alias(device_pci_register, x86_nullop);
-__weak_alias(device_isa_register, x86_nullop);
 __weak_alias(kdb_trap, x86_zeroop);
 __weak_alias(kgdb_disconnected, x86_zeroop);
 __weak_alias(kgdb_trap, x86_zeroop);
 __weak_alias(mca_nmi, x86_voidop);
+__weak_alias(pci_ranges_infer, x86_voidop);
 __weak_alias(x86_nmi, x86_voidop);
