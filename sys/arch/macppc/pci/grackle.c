54dfa3ecb2f7e5a9cd5d0659b2f2b0770380351f
macallan <macallan@NetBSD.org>
2011-10-26 04:56:23 +0000

    actually split device and softc

--- sys/arch/macppc/pci/grackle.c
+++ sys/arch/macppc/pci/grackle.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: grackle.c,v 1.14 2011/06/30 00:52:58 matt Exp $	*/
+/*	$NetBSD: grackle.c,v 1.15 2011/10/26 04:56:23 macallan Exp $	*/
 
 /*-
  * Copyright (c) 2000 Tsubai Masanari.  All rights reserved.
@@ -27,7 +27,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: grackle.c,v 1.14 2011/06/30 00:52:58 matt Exp $");
+__KERNEL_RCSID(0, "$NetBSD: grackle.c,v 1.15 2011/10/26 04:56:23 macallan Exp $");
 
 #include <sys/param.h>
 #include <sys/device.h>
@@ -41,7 +41,7 @@ __KERNEL_RCSID(0, "$NetBSD: grackle.c,v 1.14 2011/06/30 00:52:58 matt Exp $");
 #include <machine/pio.h>
 
 struct grackle_softc {
-	struct device sc_dev;
+	device_t sc_dev;
 	struct genppc_pci_chipset sc_pc;
 	struct powerpc_bus_space sc_iot;
 	struct powerpc_bus_space sc_memt;
@@ -53,7 +53,7 @@ static int grackle_match(device_t, cfdata_t, void *);
 static pcireg_t grackle_conf_read(void *, pcitag_t, int);
 static void grackle_conf_write(void *, pcitag_t, int, pcireg_t);
 
-CFATTACH_DECL(grackle, sizeof(struct grackle_softc),
+CFATTACH_DECL_NEW(grackle, sizeof(struct grackle_softc),
     grackle_match, grackle_attach, NULL, NULL);
 
 static int
@@ -92,6 +92,7 @@ grackle_attach(device_t parent, device_t self, void *aux)
 	} ranges[6], *rp = ranges;
 
 	aprint_normal("\n");
+	sc->sc_dev = self;
 
 	/* PCI bus number */
 	if (OF_getprop(node, "bus-range", busrange, sizeof(busrange)) != 8)
