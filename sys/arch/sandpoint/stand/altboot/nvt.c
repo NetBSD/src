d796261b02cca808f005b7e7837b5626220c736a
phx <phx>
2011-10-30 21:08:33 +0000

    Added in8() and out8() to access a byte with reorder-protection. Use it in all drivers instead of (volatile uint8_t *).

--- sys/arch/sandpoint/stand/altboot/nvt.c
+++ sys/arch/sandpoint/stand/altboot/nvt.c
@@ -1,4 +1,4 @@
-/* $NetBSD: nvt.c,v 1.2 2011/01/27 17:38:04 phx Exp $ */
+/* $NetBSD: nvt.c,v 1.3 2011/10/30 21:08:33 phx Exp $ */
 
 /*-
  * Copyright (c) 2007 The NetBSD Foundation, Inc.
@@ -44,8 +44,8 @@
  * - no vtophys() translation, vaddr_t == paddr_t.
  * - PIPT writeback cache aware.
  */
-#define CSR_WRITE_1(l, r, v)	*(volatile uint8_t *)((l)->csr+(r)) = (v)
-#define CSR_READ_1(l, r)	*(volatile uint8_t *)((l)->csr+(r))
+#define CSR_WRITE_1(l, r, v)	out8((l)->csr+(r), (v))
+#define CSR_READ_1(l, r)	in8((l)->csr+(r))
 #define CSR_WRITE_2(l, r, v)	out16rb((l)->csr+(r), (v))
 #define CSR_READ_2(l, r)	in16rb((l)->csr+(r))
 #define CSR_WRITE_4(l, r, v)	out32rb((l)->csr+(r), (v))
