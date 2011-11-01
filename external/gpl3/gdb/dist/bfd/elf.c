5aff6b7422c9e420b51e94036d6377660be53084
uwe <uwe>
2011-11-01 00:31:38 +0000

    elfcore_grok_netbsd_note: sync SuperH PT_GETREGS value with reality. Recognize PT_GETFPREGS that we don't yet actually have, but for which the number is now reserved.

--- external/gpl3/gdb/dist/bfd/elf.c
+++ external/gpl3/gdb/dist/bfd/elf.c
@@ -8495,6 +8495,23 @@ elfcore_grok_netbsd_note (bfd *abfd, Elf_Internal_Note *note)
 	  return TRUE;
 	}
 
+      /* On SuperH, PT_GETREGS == mach+3 and PT_GETFPREGS == mach+5.
+	 There's also old PT___GETREGS40 == mach + 1 for old reg
+	 structure which lacks GBR.  */
+
+    case bfd_arch_sh:
+      switch (note->type)
+	{
+	case NT_NETBSDCORE_FIRSTMACH+3:
+	  return elfcore_make_note_pseudosection (abfd, ".reg", note);
+
+	case NT_NETBSDCORE_FIRSTMACH+5:
+	  return elfcore_make_note_pseudosection (abfd, ".reg2", note);
+
+	default:
+	  return TRUE;
+	}
+
       /* On all other arch's, PT_GETREGS == mach+1 and
 	 PT_GETFPREGS == mach+3.  */
 
