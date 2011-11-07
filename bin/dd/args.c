bd59e9754dbd1b6cd35e7344cab8ff0ba9389b83
jym <jym@NetBSD.org>
2011-11-07 22:24:23 +0000

    As suggested by Enami, rework the msgfmt logic a bit: - error out when an unknown specifier is used. Do this in f_msgfmt(), before dd(1) starts operation. - allow buffer_write() to flush the internal buffer even when NULL is passed as parameter.
    Some whitespace fixes too.

--- bin/dd/args.c
+++ bin/dd/args.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: args.c,v 1.36 2011/11/06 21:22:23 jym Exp $	*/
+/*	$NetBSD: args.c,v 1.37 2011/11/07 22:24:23 jym Exp $	*/
 
 /*-
  * Copyright (c) 1991, 1993, 1994
@@ -38,7 +38,7 @@
 #if 0
 static char sccsid[] = "@(#)args.c	8.3 (Berkeley) 4/2/94";
 #else
-__RCSID("$NetBSD: args.c,v 1.36 2011/11/06 21:22:23 jym Exp $");
+__RCSID("$NetBSD: args.c,v 1.37 2011/11/07 22:24:23 jym Exp $");
 #endif
 #endif /* not lint */
 
@@ -273,6 +273,12 @@ f_msgfmt(char *arg)
 static void
 f_msgfmt(char *arg)
 {
+
+	/*
+	 * If the format string is not valid, dd_write_msg() will print
+	 * an error and exit.
+	 */
+	dd_write_msg(arg, 0);
 
 	msgfmt = arg;
 }
