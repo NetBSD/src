3078de12ec32b4af72c016df0ca1898994b4a286
joerg <joerg@NetBSD.org>
2011-11-17 16:39:11 +0000

    Reorder to make GCC happy.

--- tests/lib/libc/tls/dso/h_tls_dlopen.c
+++ tests/lib/libc/tls/dso/h_tls_dlopen.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: h_tls_dlopen.c,v 1.2 2011/11/17 16:20:11 joerg Exp $	*/
+/*	$NetBSD: h_tls_dlopen.c,v 1.3 2011/11/17 16:39:11 joerg Exp $	*/
 /*-
  * Copyright (c) 2011 The NetBSD Foundation, Inc.
  * All rights reserved.
@@ -32,7 +32,7 @@
  */
 
 #include <sys/cdefs.h>
-__RCSID("$NetBSD: h_tls_dlopen.c,v 1.2 2011/11/17 16:20:11 joerg Exp $");
+__RCSID("$NetBSD: h_tls_dlopen.c,v 1.3 2011/11/17 16:39:11 joerg Exp $");
 
 #include <atf-c.h>
 #include <unistd.h>
@@ -45,7 +45,7 @@ __RCSID("$NetBSD: h_tls_dlopen.c,v 1.2 2011/11/17 16:20:11 joerg Exp $");
 extern __thread int var1;
 extern __thread int var2;
 extern __thread int *var3;
-__thread static pid_t (*local_var)(void) = getpid;
+static __thread pid_t (*local_var)(void) = getpid;
 
 void testf_dso_helper(int x, int y);
 
