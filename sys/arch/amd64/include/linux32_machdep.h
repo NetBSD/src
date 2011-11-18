0aa23f5782a9d9308aabbb45bb8287405e70e906
christos <christos@NetBSD.org>
2011-11-18 04:09:17 +0000

    include the new siginfo.h file

--- sys/arch/amd64/include/linux32_machdep.h
+++ sys/arch/amd64/include/linux32_machdep.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: linux32_machdep.h,v 1.1 2006/02/09 19:18:56 manu Exp $ */
+/*	$NetBSD: linux32_machdep.h,v 1.2 2011/11/18 04:09:17 christos Exp $ */
 
 #ifndef _MACHINE_LINUX32_H_
 #define _MACHINE_LINUX32_H_
@@ -7,6 +7,7 @@
 
 #include <compat/linux32/common/linux32_types.h>
 
+#include <compat/linux32/arch/amd64/linux32_siginfo.h>
 #include <compat/linux32/arch/amd64/linux32_signal.h>
 #include <compat/linux32/arch/amd64/linux32_syscallargs.h>
 #include <compat/linux32/arch/amd64/linux32_syscall.h>
