3dca62a487145997591fe18671df2f33bac70cdd
sborrill <sborrill>
2011-11-18 23:03:22 +0000

    Pull up the following revisions(s) (requested by christos in ticket #1692): 	dist/bind/bin/named/query.c		patch 	dist/bind/lib/dns/rbtdb.c		patch
    Cache lookup could return RRSIG data associated with nonexistent records,
    leading to an assertion failure.  Fixes CVE-2011-4313.

--- dist/bind/lib/dns/rbtdb.c
+++ dist/bind/lib/dns/rbtdb.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: rbtdb.c,v 1.1.1.6.4.5 2011/07/07 12:44:25 sborrill Exp $	*/
+/*	$NetBSD: rbtdb.c,v 1.1.1.6.4.6 2011/11/18 23:03:22 sborrill Exp $	*/
 
 /*
  * Copyright (C) 2004-2010  Internet Systems Consortium, Inc. ("ISC")
@@ -4824,7 +4824,7 @@ cache_find(dns_db_t *db, dns_name_t *name, dns_dbversion_t *version,
 			      rdataset);
 		if (need_headerupdate(found, search.now))
 			update = found;
-		if (foundsig != NULL) {
+		if (!NEGATIVE(found) && foundsig != NULL) {
 			bind_rdataset(search.rbtdb, node, foundsig, search.now,
 				      sigrdataset);
 			if (need_headerupdate(foundsig, search.now))
@@ -5453,7 +5453,7 @@ cache_findrdataset(dns_db_t *db, dns_dbnode_t *node, dns_dbversion_t *version,
 	}
 	if (found != NULL) {
 		bind_rdataset(rbtdb, rbtnode, found, now, rdataset);
-		if (foundsig != NULL)
+		if (!NEGATIVE(found) && foundsig != NULL)
 			bind_rdataset(rbtdb, rbtnode, foundsig, now,
 				      sigrdataset);
 	}
