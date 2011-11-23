0d3c3bb99ba4caeca8f78352e12a5d2aed479fe3
jmcneill <jmcneill@NetBSD.org>
2011-11-23 23:07:28 +0000

    Merge jmcneill-audiomp3 branch, which is derived from ad-audiomp2. From the original ad-audiomp branch notes:
      Add MP locking to the audio drivers.
    
      Making the audio drivers MP safe is necessary before efforts
      can be made to make the VM system MP safe.
    
      The are two locks per device instance, an ISR lock and
      a character device lock. The ISR lock replaces calls to
      splaudio()/splx(), and will be held across calls to device
      methods which were called at splaudio() before (e.g.
      trigger_output). The character device lock is held across
      calls to nearly all of the methods, excluding some only
      used for initialization, e.g. get_locks.
    
    Welcome to 5.99.57.

--- sys/dev/usb/umidivar.h
+++ sys/dev/usb/umidivar.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: umidivar.h,v 1.14 2010/11/03 22:34:24 dyoung Exp $	*/
+/*	$NetBSD: umidivar.h,v 1.15 2011/11/23 23:07:36 jmcneill Exp $	*/
 /*
  * Copyright (c) 2001 The NetBSD Foundation, Inc.
  * All rights reserved.
@@ -119,4 +119,7 @@ struct umidi_softc {
 	struct umidi_endpoint	*sc_in_ep;
 	struct umidi_endpoint	*sc_endpoints;
 	int			cblnums_global;
+
+	kmutex_t		sc_lock;
+	kmutex_t		sc_intr_lock;
 };
