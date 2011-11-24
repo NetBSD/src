4eb5f2e70a566f54d0c25cdc771657fde4040058
joerg <joerg@NetBSD.org>
2011-11-24 18:44:25 +0000

    In wcscspn and wcspbrk, handle set size of 0 and 1 explicitly. For larger sets, use a bloom filter to avoid the inner loop for most of the input. The current implementation uses a simple modular hash as first function (well suited for input e.g. in ISO Latin character sets) and a more complex multiplicative hash as second function with a filter size of 512 Bit. This reduces the typical run time to O(n+m).

--- lib/libc/string/wcscspn.c
+++ lib/libc/string/wcscspn.c
@@ -1,7 +1,8 @@
-/*	$NetBSD: wcscspn.c,v 1.3 2011/11/21 15:02:48 joerg Exp $	*/
+/*	$NetBSD: wcscspn.c,v 1.4 2011/11/24 18:44:25 joerg Exp $	*/
 
 /*-
- * Copyright (c)1999 Citrus Project,
+ * Copyright (c) 1999 Citrus Project,
+ * Copyright (c) 2011 Joerg Sonnenberger,
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -29,25 +30,45 @@
  */
 
 #include <sys/cdefs.h>
-__RCSID("$NetBSD: wcscspn.c,v 1.3 2011/11/21 15:02:48 joerg Exp $");
+__RCSID("$NetBSD: wcscspn.c,v 1.4 2011/11/24 18:44:25 joerg Exp $");
 
 #include <assert.h>
+#include <inttypes.h>
+#include <string.h>
 #include <wchar.h>
+
+#include "wcscspn_bloom.h"
 
 size_t
 wcscspn(const wchar_t *s, const wchar_t *set)
 {
+	size_t bloom[BLOOM_ARRAY_SIZE];
 	const wchar_t *p;
 	const wchar_t *q;
 
 	_DIAGASSERT(s != NULL);
 	_DIAGASSERT(set != NULL);
 
+	if (set[0] == '\0')
+		return wcslen(s);
+	if (set[1] == '\0') {
+		for (p = s; *p; ++p)
+			if (*p == set[0])
+				break;
+		return p - s;
+	}
+
+	wcsspn_bloom_init(bloom, set);
+
 	for (p = s; *p; ++p) {
-		for (q = set; *q; ++q) {
+		if (!wcsspn_in_bloom(bloom, *p))
+			continue;
+
+		q = set;
+		do {
 			if (*p == *q)
 				goto done;
-		}
+		} while (*++q);
 	}
 
 done:
