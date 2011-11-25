e569cc64af2e4bcfa3b5b50c8d5188c96d0f7863
yamt <yamt@NetBSD.org>
2011-11-25 13:58:11 +0000

    radix_tree_gang_lookup_node and its variants: add a option to stop on a hole.

--- sys/sys/radixtree.h
+++ sys/sys/radixtree.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: radixtree.h,v 1.5 2011/10/25 14:11:27 yamt Exp $	*/
+/*	$NetBSD: radixtree.h,v 1.5.2.1 2011/11/25 13:58:11 yamt Exp $	*/
 
 /*-
  * Copyright (c)2011 YAMAMOTO Takashi,
@@ -66,9 +66,9 @@ void *radix_tree_replace_node(struct radix_tree *, uint64_t, void *);
 void *radix_tree_remove_node(struct radix_tree *, uint64_t);
 void *radix_tree_lookup_node(struct radix_tree *, uint64_t);
 unsigned int radix_tree_gang_lookup_node(struct radix_tree *, uint64_t,
-    void **, unsigned int);
+    void **, unsigned int, bool);
 unsigned int radix_tree_gang_lookup_node_reverse(struct radix_tree *, uint64_t,
-    void **, unsigned int);
+    void **, unsigned int, bool);
 
 /*
  * tag
@@ -80,9 +80,9 @@ bool radix_tree_get_tag(struct radix_tree *, uint64_t, radix_tree_tagid_t);
 void radix_tree_set_tag(struct radix_tree *, uint64_t, radix_tree_tagid_t);
 void radix_tree_clear_tag(struct radix_tree *, uint64_t, radix_tree_tagid_t);
 unsigned int radix_tree_gang_lookup_tagged_node(struct radix_tree *, uint64_t,
-    void **, unsigned int, radix_tree_tagid_t);
+    void **, unsigned int, bool, radix_tree_tagid_t);
 unsigned int radix_tree_gang_lookup_tagged_node_reverse(struct radix_tree *,
-    uint64_t, void **, unsigned int, radix_tree_tagid_t);
+    uint64_t, void **, unsigned int, bool, radix_tree_tagid_t);
 bool radix_tree_empty_tagged_tree_p(struct radix_tree *, radix_tree_tagid_t);
 
 #endif /* !defined(_SYS_RADIXTREE_H_) */
