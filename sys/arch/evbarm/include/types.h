cb929b5aede55572b98c7db8702ea20dc1106ba8
skrll <skrll@NetBSD.org>
2011-11-26 16:31:40 +0000

    G/c empty #ifndef __OLD_INTERRUPT_CODE

--- sys/arch/evbarm/include/types.h
+++ sys/arch/evbarm/include/types.h
@@ -1,12 +1,10 @@
-/*	$NetBSD: types.h,v 1.11 2011/07/17 23:46:49 dyoung Exp $	*/
+/*	$NetBSD: types.h,v 1.12 2011/11/26 16:31:40 skrll Exp $	*/
 
 #ifndef _EVBARM_TYPES_H_
 #define	_EVBARM_TYPES_H_
 
 #include <arm/arm32/types.h>
 
-#ifndef __OLD_INTERRUPT_CODE		/* XXX */
-#endif
 #define	__HAVE_NEW_STYLE_BUS_H
 
 #endif
