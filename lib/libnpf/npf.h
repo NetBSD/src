85111ad7b14124d88e5f5f82cfbb19a5959b0afd
christos <christos@NetBSD.org>
2011-11-26 23:42:27 +0000

    - consistency with types - prefer sizeof(*var)

--- lib/libnpf/npf.h
+++ lib/libnpf/npf.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: npf.h,v 1.3 2011/11/26 23:11:19 rmind Exp $	*/
+/*	$NetBSD: npf.h,v 1.4 2011/11/26 23:42:27 christos Exp $	*/
 
 /*-
  * Copyright (c) 2011 The NetBSD Foundation, Inc.
@@ -79,20 +79,20 @@ bool		npf_rproc_exists_p(nl_config_t *, const char *);
 int		npf_rproc_insert(nl_config_t *, nl_rproc_t *);
 
 #ifdef _NPF_PRIVATE
-int		_npf_rproc_setnorm(nl_rproc_t *, bool, bool, int, int);
+int		_npf_rproc_setnorm(nl_rproc_t *, bool, bool, u_int, u_int);
 int		_npf_rproc_setlog(nl_rproc_t *, u_int);
 #endif
 
-nl_nat_t *	npf_nat_create(int, int, u_int, npf_addr_t *, int, in_port_t);
+nl_nat_t *	npf_nat_create(int, u_int, u_int, npf_addr_t *, int, in_port_t);
 int		npf_nat_insert(nl_config_t *, nl_nat_t *, pri_t);
 
-nl_table_t *	npf_table_create(int, int);
+nl_table_t *	npf_table_create(u_int, int);
 int		npf_table_add_entry(nl_table_t *, npf_addr_t *, npf_netmask_t);
 bool		npf_table_exists_p(nl_config_t *, u_int);
 int		npf_table_insert(nl_config_t *, nl_table_t *);
 void		npf_table_destroy(nl_table_t *);
 
-int		npf_update_rule(int, char *, nl_rule_t *);
+int		npf_update_rule(int, const char *, nl_rule_t *);
 int		npf_sessions_send(int, const char *);
 int		npf_sessions_recv(int, const char *);
 
