d5b3335853584ba60002e238981e15fcf3e3ad55
jym <jym@NetBSD.org>
2011-12-04 23:55:36 +0000

    Update secmodel_examples to better describe the secmodel(9) API.

--- share/examples/secmodel/example.h
+++ share/examples/secmodel/example.h
@@ -1,4 +1,4 @@
-/* $NetBSD: example.h,v 1.1 2006/09/15 15:49:29 elad Exp $ */
+/* $NetBSD: example.h,v 1.2 2011/12/04 23:55:36 jym Exp $ */
 
 /* 
  * This file is placed in the public domain.
@@ -7,7 +7,7 @@
 #ifndef _SECMODEL_EXAMPLE_EXAMPLE_H_
 #define	_SECMODEL_EXAMPLE_EXAMPLE_H_
 
-void secmodel_example_init(void);
-void secmodel_example_start(void);
+#define SECMODEL_EXAMPLE_ID   "id.unique.secmodel.example"
+#define SECMODEL_EXAMPLE_NAME "Example security model"
 
 #endif /* !_SECMODEL_EXAMPLE_EXAMPLE_H_ */
