926571dfa7ef284889680baba2775046b1ad67ea
jym <jym@NetBSD.org>
2011-12-04 19:24:58 +0000

    Implement the register/deregister/evaluation API for secmodel(9). It allows registration of callbacks that can be used later for cross-secmodel "safe" communication.
    When a secmodel wishes to know a property maintained by another
    secmodel, it has to submit a request to it so the other secmodel can
    proceed to evaluating the request. This is done through the
    secmodel_eval(9) call; example:
    
        bool isroot;
        error = secmodel_eval("org.netbsd.secmodel.suser", "is-root",
            cred, &isroot);
        if (error == 0 && !isroot)
                result = KAUTH_RESULT_DENY;
    
    This one asks the suser module if the credentials are assumed to be root
    when evaluated by suser module. If the module is present, it will
    respond. If absent, the call will return an error.
    
    Args and command are arbitrarily defined; it's up to the secmodel(9) to
    document what it expects.
    
    Typical example is securelevel testing: when someone wants to know
    whether securelevel is raised above a certain level or not, the caller
    has to request this property to the secmodel_securelevel(9) module.
    Given that securelevel module may be absent from system's context (thus
    making access to the global "securelevel" variable impossible or
    unsafe), this API can cope with this absence and return an error.
    
    We are using secmodel_eval(9) to implement a secmodel_extensions(9)
    module, which plugs with the bsd44, suser and securelevel secmodels
    to provide the logic behind curtain, usermount and user_set_cpu_affinity
    modes, without adding hooks to traditional secmodels. This solves a
    real issue with the current secmodel(9) code, as usermount or
    user_set_cpu_affinity are not really tied to secmodel_suser(9).
    
    The secmodel_eval(9) is also used to restrict security.models settings
    when securelevel is above 0, through the "is-securelevel-above"
    evaluation:
    - curtain can be enabled any time, but cannot be disabled if
    securelevel is above 0.
    - usermount/user_set_cpu_affinity can be disabled any time, but cannot
    be enabled if securelevel is above 0.
    
    Regarding sysctl(7) entries:
    curtain and usermount are now found under security.models.extensions
    tree. The security.curtain and vfs.generic.usermount are still
    accessible for backwards compat.
    
    Documentation is incoming, I am proof-reading my writings.
    
    Written by elad@, reviewed and tested (anita test + interact for rights
    tests) by me. ok elad@.
    
    See also
    http://mail-index.netbsd.org/tech-security/2011/11/29/msg000422.html
    
    XXX might consider va0 mapping too.
    
    XXX Having a secmodel(9) specific printf (like aprint_*) for reporting
    secmodel(9) errors might be a good idea, but I am not sure on how
    to design such a function right now.

--- sys/secmodel/secmodel.h
+++ sys/secmodel/secmodel.h
@@ -1,6 +1,6 @@
-/* $NetBSD: overlay.h,v 1.5 2011/12/04 19:25:00 jym Exp $ */
+/* $NetBSD: secmodel.h,v 1.4 2011/12/04 19:24:59 jym Exp $ */
 /*-
- * Copyright (c) 2006 Elad Efrat <elad@NetBSD.org>
+ * Copyright (c) 2006, 2011 Elad Efrat <elad@NetBSD.org>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -26,31 +26,38 @@
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifndef _SECMODEL_OVERLAY_OVERLAY_H_
-#define	_SECMODEL_OVERLAY_OVERLAY_H_
+#ifndef _SECMODEL_SECMODEL_H_
+#define	_SECMODEL_SECMODEL_H_
 
-#define SECMODEL_OVERLAY_NAME "Overlay security model"
-#define SECMODEL_OVERLAY_ID   "org.netbsd.secmodel.overlay"
+#include <prop/proplib.h>
 
-void secmodel_overlay_init(void);
-void secmodel_overlay_start(void);
-void secmodel_overlay_stop(void);
+void secmodel_init(void);
 
-void sysctl_security_overlay_setup(struct sysctllog **);
+/*
+ * Functions used for inter-secmodel communication, allowing evaluation
+ * or setting information.
+ */
+typedef int (*secmodel_eval_t)(const char *, void *, void *);
+typedef int (*secmodel_setinfo_t)(void *); /* XXX TODO */
 
-int secmodel_overlay_generic_cb(kauth_cred_t, kauth_action_t, void *,
-    void *, void *, void *, void *);
-int secmodel_overlay_system_cb(kauth_cred_t, kauth_action_t, void *,
-    void *, void *, void *, void *);
-int secmodel_overlay_process_cb(kauth_cred_t, kauth_action_t, void *,
-    void *, void *, void *, void *);
-int secmodel_overlay_network_cb(kauth_cred_t, kauth_action_t, void *,
-    void *, void *, void *, void *);
-int secmodel_overlay_machdep_cb(kauth_cred_t, kauth_action_t, void *,
-    void *, void *, void *, void *);
-int secmodel_overlay_device_cb(kauth_cred_t, kauth_action_t, void *,
-    void *, void *, void *, void *);
-int secmodel_overlay_vnode_cb(kauth_cred_t, kauth_action_t, void *,
-    void *, void *, void *, void *);
+/*
+ * Secmodel entry.
+ */
+struct secmodel_descr {
+	LIST_ENTRY(secmodel_descr) sm_list;
+	const char *sm_id;
+	const char *sm_name;
+	prop_dictionary_t sm_behavior;
+	secmodel_eval_t sm_eval;
+	secmodel_setinfo_t sm_setinfo;
+};
+typedef struct secmodel_descr *secmodel_t;
 
-#endif /* !_SECMODEL_OVERLAY_OVERLAY_H_ */
+int secmodel_register(secmodel_t *, const char *, const char *,
+    prop_dictionary_t, secmodel_eval_t, secmodel_setinfo_t);
+int secmodel_deregister(secmodel_t);
+int secmodel_nsecmodels(void);
+
+int secmodel_eval(const char *, const char *, void *, void *);
+int secmodel_setinfo(const char *, void *, int *); /* XXX TODO */
+#endif /* !_SECMODEL_SECMODEL_H_ */
