3a6ecb104de2e67520d5e94351d7b48e8a4c4786
tsutsui <tsutsui@NetBSD.org>
2011-12-18 09:12:18 +0000

    Restore __HAVE_MM_MD_READWRITE which was added for 5.99.53 in rev 1.14 but removed by merge botch on __HAVE_NEW_STYLE_BUS_H changes in rev 1.15.
    Fixes "/dev/nvram: Device not configured" problem on installboot(8)
    reported by David Ross.

--- sys/arch/atari/include/types.h
+++ sys/arch/atari/include/types.h
@@ -1,10 +1,11 @@
-/*	$NetBSD: types.h,v 1.15 2011/07/17 23:40:16 dyoung Exp $	*/
+/*	$NetBSD: types.h,v 1.16 2011/12/18 09:12:18 tsutsui Exp $	*/
 
 #ifndef _MACHINE_TYPES_H_
 #define	_MACHINE_TYPES_H_
 
 #include <m68k/types.h>
 
+#define __HAVE_MM_MD_READWRITE
 #define __HAVE_NEW_STYLE_BUS_H
 
 #endif
