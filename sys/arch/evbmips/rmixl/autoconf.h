bed82f5bc3822d1854812f76755b87f7b9425097
matt <matt@NetBSD.org>
2011-12-24 01:44:43 +0000

    Rework to add early support. Add XLP support.  Allow one kernel to support XLR/XLS and XLP. Do bus_dma init in cpu_configure.

--- sys/arch/evbmips/rmixl/autoconf.h
+++ sys/arch/evbmips/rmixl/autoconf.h
@@ -1,16 +1,13 @@
-/*	$NetBSD: autoconf.h,v 1.1.2.2 2010/01/16 23:50:32 cliff Exp $	*/
+/*	$NetBSD: autoconf.h,v 1.1.2.3 2011/12/24 01:44:43 matt Exp $	*/
 
 #ifndef _EVBMIPS_RMIXL_AUTOCONF_H_
 #define _EVBMIPS_RMIXL_AUTOCONF_H_
 
-struct mainbus_softc {
-	device_t	sc_dev;
-	int		sc_node_next;
-	uint64_t	sc_node_mask;
-};
-
 struct mainbus_attach_args {
-	const char     *ma_name;
+	const char *	ma_name;
+	bus_dma_tag_t	ma_dmat29;
+	bus_dma_tag_t	ma_dmat32;
+	bus_dma_tag_t	ma_dmat64;
 	int		ma_node;
 };
 
