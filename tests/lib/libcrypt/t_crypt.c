38b0c08c2f1a526959a4012ae439000d5090fb3f
christos <christos@NetBSD.org>
2011-12-27 01:24:27 +0000

    Proper copyright, RCSID.

--- tests/lib/libcrypt/t_crypt.c
+++ tests/lib/libcrypt/t_crypt.c
@@ -1,59 +1,67 @@
-/* $NetBSD: t_crypt.c,v 1.1 2011/12/27 00:47:23 christos Exp $ */
+/* $NetBSD: t_crypt.c,v 1.2 2011/12/27 01:24:27 christos Exp $ */
 
 /*
- * Copyright (C) 1995-1998 Eric Young (eay@cryptsoft.com) All rights
- * reserved.
- * 
- * This package is an SSL implementation written by Eric Young
- * (eay@cryptsoft.com). The implementation was written so as to conform with
- * Netscapes SSL.
- * 
- * This library is free for commercial and non-commercial use as long as the
- * following conditions are aheared to.  The following conditions apply to
- * all code found in this distribution, be it the RC4, RSA, lhash, DES, etc.,
- * code; not just the SSL code.  The SSL documentation included with this
- * distribution is covered by the same copyright terms except that the holder
- * is Tim Hudson (tjh@cryptsoft.com).
- * 
- * Copyright remains Eric Young's, and as such any Copyright notices in the code
- * are not to be removed. If this package is used in a product, Eric Young
- * should be given attribution as the author of the parts of the library
- * used. This can be in the form of a textual message at program startup or
- * in documentation (online or textual) provided with the package.
- * 
+ * This version is derived from the original implementation of FreeSec
+ * (release 1.1) by David Burren.  I've reviewed the changes made in
+ * OpenBSD (as of 2.7) and modified the original code in a similar way
+ * where applicable.  I've also made it reentrant and made a number of
+ * other changes.
+ * - Solar Designer <solar at openwall.com>
+ */
+
+/*
+ * FreeSec: libcrypt for NetBSD
+ *
+ * Copyright (c) 1994 David Burren
+ * All rights reserved.
+ *
  * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are
- * met: 1. Redistributions of source code must retain the copyright notice,
- * this list of conditions and the following disclaimer. 2. Redistributions
- * in binary form must reproduce the above copyright notice, this list of
- * conditions and the following disclaimer in the documentation and/or other
- * materials provided with the distribution. 3. All advertising materials
- * mentioning features or use of this software must display the following
- * acknowledgement: "This product includes cryptographic software written by
- * Eric Young (eay@cryptsoft.com)" The word 'cryptographic' can be left out
- * if the rouines from the library being used are not cryptographic related
- * :-). 4. If you include any Windows specific code (or a derivative thereof)
- * from the apps directory (application code) you must include an
- * acknowledgement: "This product includes software written by Tim Hudson
- * (tjh@cryptsoft.com)"
- * 
- * THIS SOFTWARE IS PROVIDED BY ERIC YOUNG ``AS IS'' AND ANY EXPRESS OR IMPLIED
- * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
- * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN
- * NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
- * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the author nor the names of other contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
- * 
- * The licence and distribution terms for any publically available version or
- * derivative of this code cannot be changed.  i.e. this code cannot simply
- * be copied and put under another distribution licence [including the GNU
- * Public Licence.]
+ *
+ *	$Owl: Owl/packages/glibc/crypt_freesec.c,v 1.6 2010/02/20 14:45:06 solar Exp $
+ *	Id: crypt.c,v 1.15 1994/09/13 04:58:49 davidb Exp
+ *
+ * This is an original implementation of the DES and the crypt(3) interfaces
+ * by David Burren <davidb at werj.com.au>.
+ *
+ * An excellent reference on the underlying algorithm (and related
+ * algorithms) is:
+ *
+ *	B. Schneier, Applied Cryptography: protocols, algorithms,
+ *	and source code in C, John Wiley & Sons, 1994.
+ *
+ * Note that in that book's description of DES the lookups for the initial,
+ * pbox, and final permutations are inverted (this has been brought to the
+ * attention of the author).  A list of errata for this book has been
+ * posted to the sci.crypt newsgroup by the author and is available for FTP.
+ *
+ * ARCHITECTURE ASSUMPTIONS:
+ *	This code used to have some nasty ones, but these have been removed
+ *	by now.	 The code requires a 32-bit integer type, though.
  */
+#include <sys/cdefs.h>
+__RCSID("$NetBSD: t_crypt.c,v 1.2 2011/12/27 01:24:27 christos Exp $");
 
 #include <atf-c.h>
 #include <stdio.h>
