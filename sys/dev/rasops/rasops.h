4aa3210629302ebd4525df39bb7678563b1035a4
macallan <macallan@NetBSD.org>
2012-01-03 23:13:59 +0000

    add a macro to identify alpha fonts

--- sys/dev/rasops/rasops.h
+++ sys/dev/rasops/rasops.h
@@ -1,4 +1,4 @@
-/* 	$NetBSD: rasops.h,v 1.28 2011/12/28 08:36:46 macallan Exp $ */
+/* 	$NetBSD: rasops.h,v 1.29 2012/01/03 23:13:59 macallan Exp $ */
 
 /*-
  * Copyright (c) 1999 The NetBSD Foundation, Inc.
@@ -143,6 +143,7 @@ struct rasops_info {
 			  (ri->ri_optfont.data != NULL)) ? \
 			 &ri->ri_optfont : ri->ri_font
 
+#define FONT_IS_ALPHA(f) ((f)->fontwidth <= (f)->stride)
 /*
  * rasops_init().
  *
