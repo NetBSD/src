2dbd2905da6cf8dffc7ba175ad3127784594d883
drochner <drochner@NetBSD.org>
2012-01-05 17:32:02 +0000

    pull in some patches from upstream CVS, following secadv_20120104.txt: -rev.21964 for DTLS Plaintext Recovery Attack (CVE-2011-4108) -rev.21961 for Uninitialized SSL 3.0 Padding (CVE-2011-4576) -rev.21456+21954 for Malformed RFC 3779 Data Can Cause Assertion Failures  (CVE-2011-4577)  (rev.21456 is not mentioned in the advisory, but there is code overlap) -rev.21958 for SGC Restart DoS Attack (CVE-2011-4619) -rev.21956 for Invalid GOST parameters DoS Attack (CVE-2012-0027)

--- crypto/external/bsd/openssl/dist/ssl/s3_srvr.c
+++ crypto/external/bsd/openssl/dist/ssl/s3_srvr.c
@@ -287,6 +287,7 @@ int ssl3_accept(SSL *s)
 				}
 
 			s->init_num=0;
+			s->s3->flags &= ~SSL3_FLAGS_SGC_RESTART_DONE;
 
 			if (s->state != SSL_ST_RENEGOTIATE)
 				{
@@ -836,6 +837,14 @@ int ssl3_check_client_hello(SSL *s)
 	int ok;
 	long n;
 
+	/* We only allow the client to restart the handshake once per
+	 * negotiation. */
+	if (s->s3->flags & SSL3_FLAGS_SGC_RESTART_DONE)
+		{
+		SSLerr(SSL_F_SSL3_CHECK_CLIENT_HELLO, SSL_R_MULTIPLE_SGC_RESTARTS);
+		return -1;
+		}
+
 	/* this function is called when we really expect a Certificate message,
 	 * so permit appropriate message length */
 	n=s->method->ssl_get_message(s,
@@ -859,6 +868,7 @@ int ssl3_check_client_hello(SSL *s)
 			s->s3->tmp.dh = NULL;
 			}
 #endif
+		s->s3->flags |= SSL3_FLAGS_SGC_RESTART_DONE;
 		return 2;
 		}
 	return 1;
