72f276a36686e4364525ac734ee5e82962fc2ea9
reinoud <reinoud@NetBSD.org>
2012-01-05 15:19:52 +0000

    Revert MAP_NOSYSCALLS patch.

--- sys/uvm/uvm_extern.h
+++ sys/uvm/uvm_extern.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: uvm_extern.h,v 1.178 2011/12/22 13:12:50 reinoud Exp $	*/
+/*	$NetBSD: uvm_extern.h,v 1.179 2012/01/05 15:19:53 reinoud Exp $	*/
 
 /*
  * Copyright (c) 1997 Charles D. Cranor and Washington University.
@@ -679,11 +679,6 @@ void			uvmspace_share(struct proc *, struct proc *);
 void			uvmspace_unshare(struct lwp *);
 
 void			uvm_whatis(uintptr_t, void (*)(const char *, ...));
-
-void			uvm_map_setattr(struct vm_map *, vaddr_t,
-			    vaddr_t, uint32_t);
-bool			uvm_map_checkattr(struct vm_map *, vaddr_t,
-			    vaddr_t, uint32_t);
 
 /* uvm_meter.c */
 int			uvm_sysctl(int *, u_int, void *, size_t *,
