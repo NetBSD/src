abcb3bee8acce9f6ad8c03aee1a6e8435373d84a
drochner <drochner@NetBSD.org>
2012-01-11 14:37:45 +0000

    protect "union sockaddr_union" from being defined twice by a CPP symbol (copied from FreeBSD), allows coexistence of (FAST_)IPSEC and pf

--- sys/netipsec/keydb.h
+++ sys/netipsec/keydb.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: keydb.h,v 1.10 2011/05/23 15:17:25 drochner Exp $	*/
+/*	$NetBSD: keydb.h,v 1.11 2012/01/11 14:37:45 drochner Exp $	*/
 /*	$FreeBSD: src/sys/netipsec/keydb.h,v 1.1.4.1 2003/01/24 05:11:36 sam Exp $	*/
 /*	$KAME: keydb.h,v 1.14 2000/08/02 17:58:26 sakane Exp $	*/
 
@@ -42,6 +42,8 @@
 #include <net/route.h>
 #include <netinet/in.h>
 
+#ifndef _SOCKADDR_UNION_DEFINED
+#define _SOCKADDR_UNION_DEFINED
 /*
  * The union of all possible address formats we handle.
  */
@@ -50,6 +52,7 @@ union sockaddr_union {
 	struct sockaddr_in	sin;
 	struct sockaddr_in6	sin6;
 };
+#endif /* _SOCKADDR_UNION_DEFINED */
 
 /* Security Assocciation Index */
 /* NOTE: Ensure to be same address family */
