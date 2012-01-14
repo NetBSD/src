ac36ad83a63ac67861382c775bf4b6ed53b14de4
phx <phx@NetBSD.org>
2012-01-14 19:39:25 +0000

    New device nhpow(4) to support the onboard GPIO pins on a NH230/231 board for buttons, powerdown, reset, LEDs and fan control. It attaches gpio(4) for its 8 pins on demand.

--- sys/arch/sandpoint/include/bus_funcs.h
+++ sys/arch/sandpoint/include/bus_funcs.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: bus_funcs.h,v 1.2 2011/12/17 20:20:37 phx Exp $	*/
+/*	$NetBSD: bus_funcs.h,v 1.3 2012/01/14 19:39:25 phx Exp $	*/
 /*	$OpenBSD: bus.h,v 1.1 1997/10/13 10:53:42 pefo Exp $	*/
 
 #ifndef _SANDPOINT_BUS_FUNCS_H_
@@ -11,6 +11,7 @@ extern struct powerpc_bus_space sandpoint_mem_space_tag;
 extern struct powerpc_bus_space genppc_isa_mem_space_tag;
 extern struct powerpc_bus_space sandpoint_eumb_space_tag;
 extern struct powerpc_bus_space sandpoint_flash_space_tag;
+extern struct powerpc_bus_space sandpoint_nhgpio_space_tag;
 #endif
 
 #include <powerpc/bus_funcs.h>
