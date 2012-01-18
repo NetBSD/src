f533493c9a606fdc5183f796c9a0068aac8aa441
drochner <drochner@NetBSD.org>
2012-01-18 21:34:38 +0000

    revert previous, the assumption "all buses 1 and up must be subordinate to pci0" doesn't even hold on i386 -- there are server-class chipsets with multiple primary PCI buses, see arch/x86/pci/pchb.c for examples

--- sys/arch/i386/i386/mainbus.c
+++ sys/arch/i386/i386/mainbus.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: mainbus.c,v 1.92 2011/10/21 21:35:28 dyoung Exp $	*/
+/*	$NetBSD: mainbus.c,v 1.93 2012/01/18 21:34:38 drochner Exp $	*/
 
 /*
  * Copyright (c) 1996 Christopher G. Demetriou.  All rights reserved.
@@ -31,7 +31,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: mainbus.c,v 1.92 2011/10/21 21:35:28 dyoung Exp $");
+__KERNEL_RCSID(0, "$NetBSD: mainbus.c,v 1.93 2012/01/18 21:34:38 drochner Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -406,7 +406,6 @@ mainbus_rescan(device_t self, const char *ifattr, const int *locators)
 		mba.mba_pba.pba_pc = NULL;
 		mba.mba_pba.pba_flags = pci_bus_flags();
 		mba.mba_pba.pba_bus = 0;
-		mba.mba_pba.pba_sub = 255;
 		mba.mba_pba.pba_bridgetag = NULL;
 #if NACPICA > 0 && defined(ACPI_SCANPCI)
 		if (npcibus == 0 && sc->sc_mpacpi_active)
