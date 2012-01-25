d869187c0b25791fcabe66a547346f79fc60a70d
riz <riz@NetBSD.org>
2012-01-25 20:20:14 +0000

    Apply patch (requested by drochner in ticket #1711): crypto/dist/openssl/crypto/x509v3/pcy_map.c     patch crypto/dist/openssl/crypto/x509v3/pcy_tree.c    patch
            Address CVE-2011-4109.
            [drochner, ticket #1711]

--- crypto/dist/openssl/crypto/x509v3/pcy_tree.c
+++ crypto/dist/openssl/crypto/x509v3/pcy_tree.c
@@ -611,6 +611,10 @@ int X509_policy_check(X509_POLICY_TREE **ptree, int *pexplicit_policy,
 		return 1;
 
 		/* Some internal error */
+		case -1:
+		return -1;
+
+		/* Some internal error */
 		case 0:
 		return 0;
 
