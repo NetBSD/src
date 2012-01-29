d1247914f2d7fe6176e7cf7f801d777ec89f3bf9
dholland <dholland>
2012-01-29 06:23:20 +0000

    Change dqblk_to_quotaval() from quota1_subr.c to dqblk_to_quotavals(), and pass in two single quotaval structs (for blocks and inodes) instead of an array of (implicitly) QUOTA_NLIMITS quotaval structs indexed by constants from quotaprop.h.
    Note: because this code is used by COMPAT_50 as well as ufs, this
    change requires a kernel version bump. (The code is also used by
    edquota, but via .PATH so it's not ABI-sensitive there.)

--- sys/ufs/ufs/quota1.h
+++ sys/ufs/ufs/quota1.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: quota1.h,v 1.5 2011/11/25 16:55:05 dholland Exp $	*/
+/*	$NetBSD: quota1.h,v 1.6 2012/01/29 06:23:20 dholland Exp $	*/
 
 /*
  * Copyright (c) 1982, 1986, 1993
@@ -104,8 +104,9 @@ struct dqblk {
 };
 
 /* quota1_subr.c */
-struct quota2_entry;
-void dqblk_to_quotaval(const struct dqblk *, struct quotaval *);
-void quotaval_to_dqblk(const struct quotaval *, struct dqblk *);
+void dqblk_to_quotavals(const struct dqblk *,
+			struct quotaval *, struct quotaval *);
+void quotavals_to_dqblk(const struct quotaval *, const struct quotaval *,
+			struct dqblk *);
 
 #endif /* !_UFS_UFS_QUOTA1_H_ */
