286e7eaf72be5268f95e09629ed402bdd5b88ec4
dholland <dholland@NetBSD.org>
2012-01-30 06:14:43 +0000

    Remove stray p in identifier name. This has (as far as I can tell) prevented quotacheck and other old-style quota bits from working since last March. Use a correct declaration in the header file, so that if something similar happens again affected programs will fail to link instead of failing to work.
    This kind of nonsense is why I like -Wl,-warn-common.

--- usr.bin/quota/quotautil.h
+++ usr.bin/quota/quotautil.h
@@ -1,7 +1,7 @@
-/*	$NetBSD: quotautil.h,v 1.3 2011/03/24 17:05:46 bouyer Exp $ */
+/*	$NetBSD: quotautil.h,v 1.4 2012/01/30 06:14:43 dholland Exp $ */
 
-const char *qfextension[MAXQUOTAS];
-const char *qfname;
+extern const char *qfextension[MAXQUOTAS];
+extern const char *qfname;
 struct fstab;
 int hasquota(char *, size_t, struct fstab *, int);
 int oneof(const char *, char *[], int);
