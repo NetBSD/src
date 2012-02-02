6abaf60626ff2bddd0f521734d7b770adde04d9b
tsutsui <tsutsui@NetBSD.org>
2012-02-02 16:00:35 +0000

    Select a proper kernel set (GENERIC or C700) per running INSTALL kernel name.

--- distrib/utils/sysinst/arch/zaurus/msg.md.pl
+++ distrib/utils/sysinst/arch/zaurus/msg.md.pl
@@ -1,4 +1,4 @@
-/*	$NetBSD: msg.md.pl,v 1.4 2011/04/04 08:30:46 mbalmer Exp $	*/
+/*	$NetBSD: msg.md.pl,v 1.5 2012/02/02 16:00:35 tsutsui Exp $	*/
 /*	Based on english version: */
 /*	NetBSD: msg.md.en,v 1.1 2002/02/11 13:50:18 skrll Exp */
 
@@ -54,3 +54,6 @@ message zaurusfspart
 
 message set_kernel_1
 {Kernel (GENERIC)}
+
+message set_kernel_2
+{Kernel (C700)}
