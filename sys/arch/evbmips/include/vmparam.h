8d513d9e4a96b0535a78ae99680cd40b8e34c44b
matt <matt@NetBSD.org>
2012-02-09 03:11:39 +0000

    VM_FREELIST_MAX is not used by anything.  Should be VM_NFREELIST Add macros to test a PA to see if it belonds to a VM_FREELIST_*

--- sys/arch/evbmips/include/vmparam.h
+++ sys/arch/evbmips/include/vmparam.h
@@ -1,19 +1,21 @@
-/*	$NetBSD: vmparam.h,v 1.1.142.5 2011/11/29 07:48:32 matt Exp $	*/
+/*	vmparam.h,v 1.1.142.5 2011/11/29 07:48:32 matt Exp	*/
 
 #ifndef _EVBMIPS_VMPARAM_H_
 #define _EVBMIPS_VMPARAM_H_
 
 #include <mips/vmparam.h>
 
-#define	VM_PHYSSEG_MAX		32
+#define	VM_PHYSSEG_MAX			32
 
-#undef VM_FREELIST_MAX
-#define	VM_FREELIST_MAX		3
+#undef VM_NFREELIST
+#define	VM_NFREELIST			3
 #if defined(_MIPS_PADDR_T_64BIT) || defined(_LP64)
-#define	VM_FREELIST_FIRST4G	2
+#define	VM_FREELIST_FIRST4G		2
+#define	VM_FREELIST_FIRST4G_P(pa)	(((pa) >> 32) == 0)
 #endif
 #if !defined(_LP64)
-#define	VM_FREELIST_FIRST512M	1
+#define	VM_FREELIST_FIRST512M		1
+#define	VM_FREELIST_FIRST512M_P(pa)	(((pa) >> 29) == 0)
 #endif
 
 #define	VM_FREELIST_NORMALOK_P(lcv) \
