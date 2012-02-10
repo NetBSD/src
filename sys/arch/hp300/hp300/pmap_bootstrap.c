366e882e50831ecdc34cec5651a2fde03d4823d1
mhitch <mhitch>
2012-02-10 06:21:15 +0000

    Oops - have to RELOC(physmem) here.

--- sys/arch/hp300/hp300/pmap_bootstrap.c
+++ sys/arch/hp300/hp300/pmap_bootstrap.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: pmap_bootstrap.c,v 1.55 2012/02/10 04:49:45 mhitch Exp $	*/
+/*	$NetBSD: pmap_bootstrap.c,v 1.56 2012/02/10 06:21:15 mhitch Exp $	*/
 
 /*
  * Copyright (c) 1991, 1993
@@ -36,7 +36,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: pmap_bootstrap.c,v 1.55 2012/02/10 04:49:45 mhitch Exp $");
+__KERNEL_RCSID(0, "$NetBSD: pmap_bootstrap.c,v 1.56 2012/02/10 06:21:15 mhitch Exp $");
 
 #include <sys/param.h>
 #include <uvm/uvm_extern.h>
@@ -126,7 +126,7 @@ pmap_bootstrap(paddr_t nextpa, paddr_t firstpa)
 	lkptpa = nextpa;
 	nextpa += PAGE_SIZE;
 	kptpa = nextpa;
-	nptpages = RELOC(Sysptsize, int) + howmany(physmem, NPTEPG) +
+	nptpages = RELOC(Sysptsize, int) + howmany(RELOC(physmem), NPTEPG) +
 		(IIOMAPSIZE + EIOMAPSIZE + NPTEPG - 1) / NPTEPG;
 	nextpa += nptpages * PAGE_SIZE;
 
