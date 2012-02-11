9b0df829212e233f21a06626ba584ee72ceb64b6
martin <martin@NetBSD.org>
2012-02-11 23:19:11 +0000

    Welcome to 5.99.65 - posix_spawn syscall added

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.407 2012/02/03 20:12:48 matt Exp $	*/
+/*	$NetBSD: param.h,v 1.408 2012/02/11 23:19:11 martin Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -63,7 +63,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	599006400	/* NetBSD 5.99.64 */
+#define	__NetBSD_Version__	599006500	/* NetBSD 5.99.65 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
