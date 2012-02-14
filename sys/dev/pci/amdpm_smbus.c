f76013d4aad8748e14c0b435ad34d8d3a4b54fe2
pgoyette <pgoyette@NetBSD.org>
2012-02-14 15:08:07 +0000

    Replace the xxx_acquire()/xxx_release() rwlocks with mutexes.  There are only RW_WRITERs for these, and no RW_READERs, so no need to incur the extra overhead of allowing for both.  As discussed on tech-kern.
    For piixpm and ichsmb, the acquire/release protocol needs to be used,
    even if the request is I2C_F_POLL'd (or if the device supports only
    polled mode).  Otherwise multiple requests can be running at the same
    time, and they stomp on each other and create anomolous results.
    
    Part 2 addresses my PR kern/45889
    
    3 ACKs from releng

--- sys/dev/pci/amdpm_smbus.c
+++ sys/dev/pci/amdpm_smbus.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: amdpm_smbus.c,v 1.17 2011/11/19 02:39:14 christos Exp $ */
+/*	$NetBSD: amdpm_smbus.c,v 1.18 2012/02/14 15:08:07 pgoyette Exp $ */
 
 /*
  * Copyright (c) 2005 Anil Gopinath (anil_public@yahoo.com)
@@ -32,14 +32,14 @@
  * AMD-8111 HyperTransport I/O Hub
  */
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: amdpm_smbus.c,v 1.17 2011/11/19 02:39:14 christos Exp $");
+__KERNEL_RCSID(0, "$NetBSD: amdpm_smbus.c,v 1.18 2012/02/14 15:08:07 pgoyette Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
 #include <sys/kernel.h>
 #include <sys/device.h>
 #include <sys/rnd.h>
-#include <sys/rwlock.h>
+#include <sys/mutex.h>
 
 #include <dev/pci/pcireg.h>
 #include <dev/pci/pcivar.h>
@@ -83,7 +83,7 @@ amdpm_smbus_attach(struct amdpm_softc *sc)
 	sc->sc_i2c.ic_write_byte = NULL;
 	sc->sc_i2c.ic_exec = amdpm_smbus_exec;
 
-	rw_init(&sc->sc_rwlock);
+	mutex_init(&sc->sc_mutex, MUTEX_DEFAULT, IPL_NONE);
 
 	iba.iba_tag = &sc->sc_i2c;
 	(void)config_found_ia(&sc->sc_dev, "i2cbus", &iba, iicbus_print);
@@ -94,7 +94,7 @@ amdpm_smbus_acquire_bus(void *cookie, int flags)
 {
 	struct amdpm_softc *sc = cookie;
 
-	rw_enter(&sc->sc_rwlock, RW_WRITER);
+	mutex_enter(&sc->sc_mutex);
 	return 0;
 }
 
@@ -103,7 +103,7 @@ amdpm_smbus_release_bus(void *cookie, int flags)
 {
 	struct amdpm_softc *sc = cookie;
 
-	rw_exit(&sc->sc_rwlock);
+	mutex_exit(&sc->sc_mutex);
 }
 
 static int
