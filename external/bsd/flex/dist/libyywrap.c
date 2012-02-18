6e1b5dd40f2e23dd36487e1fbb65918a764ad41d
christos <christos@NetBSD.org>
2012-02-18 19:17:54 +0000

    fix prototypes

--- external/bsd/flex/dist/libyywrap.c
+++ external/bsd/flex/dist/libyywrap.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: libyywrap.c,v 1.1.1.1 2009/10/26 00:26:24 christos Exp $	*/
+/*	$NetBSD: libyywrap.c,v 1.2 2012/02/18 19:17:54 christos Exp $	*/
 
 /* libyywrap - flex run-time support library "yywrap" function */
 
@@ -23,7 +23,9 @@
 /*  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR */
 /*  PURPOSE. */
 
-int     yywrap (void)
+int yywrap(void);
+int
+yywrap(void)
 {
 	return 1;
 }
