e22b64ecd6d2096cba30e9135e2b1f3d3d345aa4
tsutsui <tsutsui@NetBSD.org>
2012-02-19 12:02:55 +0000

    Fix boot failure on OF 2.0 machines.
    Discussed with phx@ on port-macppc@:
    http://mail-index.NetBSD.org/port-macppc/2012/02/18/msg001556.html
    
    Should be pulled up to netbsd-6.

--- sys/arch/macppc/stand/ofwboot/boot.c
+++ sys/arch/macppc/stand/ofwboot/boot.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: boot.c,v 1.26 2011/01/22 19:19:19 joerg Exp $	*/
+/*	$NetBSD: boot.c,v 1.27 2012/02/19 12:02:55 tsutsui Exp $	*/
 
 /*-
  * Copyright (c) 1997 The NetBSD Foundation, Inc.
@@ -96,8 +96,8 @@ char bootdev[MAXBOOTPATHLEN];
 char bootfile[MAXBOOTPATHLEN];
 int boothowto;
 bool floppyboot;
+int ofw_version = 0;
 
-static int ofw_version = 0;
 static const char *kernels[] = { "/netbsd", "/netbsd.gz", "/netbsd.macppc", NULL };
 
 static void
