61a7a02d886b60e9354525f466af353bbdeea0a1
mbalmer <mbalmer@NetBSD.org>
2012-02-19 14:56:16 +0000

    Add support for the QUAN DM9601. From Loganaden Velvindron <loganaden@wolfman.devio.us>.

--- sys/dev/usb/if_udav.c
+++ sys/dev/usb/if_udav.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: if_udav.c,v 1.34 2012/02/02 19:43:07 tls Exp $	*/
+/*	$NetBSD: if_udav.c,v 1.35 2012/02/19 14:56:16 mbalmer Exp $	*/
 /*	$nabe: if_udav.c,v 1.3 2003/08/21 16:57:19 nabe Exp $	*/
 /*
  * Copyright (c) 2003
@@ -44,7 +44,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: if_udav.c,v 1.34 2012/02/02 19:43:07 tls Exp $");
+__KERNEL_RCSID(0, "$NetBSD: if_udav.c,v 1.35 2012/02/19 14:56:16 mbalmer Exp $");
 
 #include "opt_inet.h"
 
@@ -154,6 +154,8 @@ static const struct udav_type {
 	{{ USB_VENDOR_SHANTOU, USB_PRODUCT_SHANTOU_ADM8515 }, 0},
 	/* SUNRISING SR9600 */
 	{{ USB_VENDOR_SUNRISING, USB_PRODUCT_SUNRISING_SR9600 }, 0 },
+	/* QUAN DM9601 */
+	{{USB_VENDOR_QUAN, USB_PRODUCT_QUAN_DM9601 }, 0},
 #if 0
 	/* DAVICOM DM9601 Generic? */
 	/*  XXX: The following ids was obtained from the data sheet. */
