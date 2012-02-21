f326ce849e92a3c3b174e22ae7b2e19ed8af1cc6
matt <matt@NetBSD.org>
2012-02-21 02:19:01 +0000

    Restore back to double alignment.

--- sys/arch/powerpc/include/cdefs.h
+++ sys/arch/powerpc/include/cdefs.h
@@ -1,8 +1,8 @@
-/*	$NetBSD: cdefs.h,v 1.8 2012/02/21 02:09:35 matt Exp $	*/
+/*	$NetBSD: cdefs.h,v 1.9 2012/02/21 02:19:01 matt Exp $	*/
 
 #ifndef	_POWERPC_CDEFS_H_
 #define	_POWERPC_CDEFS_H_
 
-#define	__ALIGNBYTES	(16 - 1)		/* AltiVec */
+#define	__ALIGNBYTES	(sizeof(double) - 1)
 
 #endif /* !_POWERPC_CDEFS_H_ */
