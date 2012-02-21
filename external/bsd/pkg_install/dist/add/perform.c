cd3fdc625d6eb8a3752578b2861ee98bfacaa135
wiz <wiz@NetBSD.org>
2012-02-21 13:32:45 +0000

    Fix PR 46068 using the patch by Nicolas Thauvin: pkg_add fails to install a package when subdirectories are missing in the path to package directory in /var/db/pkg.

--- external/bsd/pkg_install/dist/add/perform.c
+++ external/bsd/pkg_install/dist/add/perform.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: perform.c,v 1.1.1.19 2012/02/19 17:46:46 tron Exp $	*/
+/*	$NetBSD: perform.c,v 1.2 2012/02/21 13:32:45 wiz Exp $	*/
 #if HAVE_CONFIG_H
 #include "config.h"
 #endif
@@ -6,7 +6,7 @@
 #if HAVE_SYS_CDEFS_H
 #include <sys/cdefs.h>
 #endif
-__RCSID("$NetBSD: perform.c,v 1.1.1.19 2012/02/19 17:46:46 tron Exp $");
+__RCSID("$NetBSD: perform.c,v 1.2 2012/02/21 13:32:45 wiz Exp $");
 
 /*-
  * Copyright (c) 2003 Grant Beattie <grant@NetBSD.org>
@@ -206,7 +206,7 @@ mkdir_p(const char *path)
 
 		if (mkdir(p, 0777) == -1) {
 			saved_errno = errno;
-			if (stat(path, &sb) == 0) {
+			if (stat(p, &sb) == 0) {
 				if (S_ISDIR(sb.st_mode))
 					goto pass;
 				errno = ENOTDIR;
