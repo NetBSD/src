268a453af76930ec646c534bab072605e75c8307
christos <christos@NetBSD.org>
2012-02-24 22:41:12 +0000

    PR/46068: Fix symbol loading on i386 kernels. On i386 lma != vma and gdb7 depends on them being != to detect overlays and avoid loading them. I've disabled the test for now.

--- external/gpl3/gdb/dist/gdb/objfiles.c
+++ external/gpl3/gdb/dist/gdb/objfiles.c
@@ -1125,6 +1125,13 @@ static int
 insert_section_p (const struct bfd *abfd,
 		  const struct bfd_section *section)
 {
+#ifndef __NetBSD__
+  /*
+   * On NetBSD we don't typically have overlay sections and in some of
+   * our kernels (i386 vma = lma | 0xc0000000), so the following test
+   * makes kernels not load any symbols. There must be a better way to
+   * detect overlays.
+   */
   const bfd_vma lma = bfd_section_lma (abfd, section);
 
   if (lma != 0 && lma != bfd_section_vma (abfd, section)
@@ -1133,6 +1140,7 @@ insert_section_p (const struct bfd *abfd,
        discarding sections from the "system supplied DSO" (aka vdso)
        on some Linux systems (e.g. Fedora 11).  */
     return 0;
+#endif
   if ((bfd_get_section_flags (abfd, section) & SEC_THREAD_LOCAL) != 0)
     /* This is a TLS section.  */
     return 0;
