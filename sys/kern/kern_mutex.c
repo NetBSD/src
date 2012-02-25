cb5b5a7b86b1c8fa361d5d0d23d185398d489509
rmind <rmind@NetBSD.org>
2012-02-25 22:32:44 +0000

    {mutex,rw}_vector_enter: use macro versions to disable/enable preemption.

--- sys/kern/kern_mutex.c
+++ sys/kern/kern_mutex.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: kern_mutex.c,v 1.52 2012/02/19 21:06:52 rmind Exp $	*/
+/*	$NetBSD: kern_mutex.c,v 1.53 2012/02/25 22:32:44 rmind Exp $	*/
 
 /*-
  * Copyright (c) 2002, 2006, 2007, 2008 The NetBSD Foundation, Inc.
@@ -40,7 +40,7 @@
 #define	__MUTEX_PRIVATE
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: kern_mutex.c,v 1.52 2012/02/19 21:06:52 rmind Exp $");
+__KERNEL_RCSID(0, "$NetBSD: kern_mutex.c,v 1.53 2012/02/25 22:32:44 rmind Exp $");
 
 #include <sys/param.h>
 #include <sys/atomic.h>
@@ -534,9 +534,9 @@ mutex_vector_enter(kmutex_t *mtx)
 			LOCKSTAT_START_TIMER(lsflag, spintime);
 			count = SPINLOCK_BACKOFF_MIN;
 			do {
-				kpreempt_enable();
+				KPREEMPT_ENABLE(curlwp);
 				SPINLOCK_BACKOFF(count);
-				kpreempt_disable();
+				KPREEMPT_DISABLE(curlwp);
 				owner = mtx->mtx_owner;
 			} while (mutex_oncpu(owner));
 			LOCKSTAT_STOP_TIMER(lsflag, spintime);
