de07e2701bece8b4170cbade570c149e9519af55
macallan <macallan@NetBSD.org>
2012-02-28 20:21:16 +0000

    support anti-aliased fonts in 8 bit as well

--- sys/dev/pci/radeonfbvar.h
+++ sys/dev/pci/radeonfbvar.h
@@ -1,4 +1,4 @@
-/* $NetBSD: radeonfbvar.h,v 1.14 2012/02/16 17:33:28 macallan Exp $ */
+/* $NetBSD: radeonfbvar.h,v 1.15 2012/02/28 20:21:16 macallan Exp $ */
 
 /*-
  * Copyright (c) 2006 Itronix Inc.
@@ -317,6 +317,7 @@ struct radeonfb_softc {
 
 #define	GET32(sc, r)	radeonfb_get32(sc, r)
 #define	PUT32(sc, r, v)	radeonfb_put32(sc, r, v)
+#define	PUT32S(sc, r, v)	radeonfb_put32s(sc, r, v)
 #define	SET32(sc, r, v)	PUT32(sc, r, GET32(sc, r) | (v))
 #define	CLR32(sc, r, v)	PUT32(sc, r, GET32(sc, r) & ~(v))
 #define	PATCH32(sc, r, v, m)	PUT32(sc, r, (GET32(sc, r) & (m)) | (v))
@@ -349,6 +350,7 @@ struct radeonfb_softc {
 
 uint32_t radeonfb_get32(struct radeonfb_softc *, uint32_t);
 void radeonfb_put32(struct radeonfb_softc *, uint32_t, uint32_t);
+void radeonfb_put32s(struct radeonfb_softc *, uint32_t, uint32_t);
 void radeonfb_mask32(struct radeonfb_softc *, uint32_t, uint32_t, uint32_t);
 
 uint32_t radeonfb_getindex(struct radeonfb_softc *, uint32_t);
