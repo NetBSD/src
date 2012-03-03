970b854d1754cfba4c660be8f4242a5b81cd83f3
riz <riz@NetBSD.org>
2012-03-03 21:35:32 +0000

    Add quirk for Kingston DataTraveler 100 G2 (which apparently has a Toshiba vendor ID)

--- sys/dev/usb/umass_quirks.c
+++ sys/dev/usb/umass_quirks.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: umass_quirks.c,v 1.92 2011/12/23 00:51:47 jakllsch Exp $	*/
+/*	$NetBSD: umass_quirks.c,v 1.93 2012/03/03 21:35:32 riz Exp $	*/
 
 /*
  * Copyright (c) 2001, 2004 The NetBSD Foundation, Inc.
@@ -32,7 +32,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: umass_quirks.c,v 1.92 2011/12/23 00:51:47 jakllsch Exp $");
+__KERNEL_RCSID(0, "$NetBSD: umass_quirks.c,v 1.93 2012/03/03 21:35:32 riz Exp $");
 
 #ifdef _KERNEL_OPT
 #include "opt_umass.h"
@@ -312,6 +312,15 @@ Static const struct umass_quirk umass_quirks[] = {
 	},
 
 	{ { USB_VENDOR_KINGSTON, USB_PRODUCT_KINGSTON_DTMINI10 },
+	  UMASS_WPROTO_UNSPEC, UMASS_CPROTO_UNSPEC, 
+	  0,
+	  PQUIRK_NODOORLOCK,
+	  UMATCH_VENDOR_PRODUCT,
+	  NULL, NULL
+	},
+
+	/* Also, some Kingston pendrives have Toshiba vendor ID */
+	{ { USB_VENDOR_TOSHIBA, USB_PRODUCT_KINGSTON_DT100_G2 },
 	  UMASS_WPROTO_UNSPEC, UMASS_CPROTO_UNSPEC, 
 	  0,
 	  PQUIRK_NODOORLOCK,
