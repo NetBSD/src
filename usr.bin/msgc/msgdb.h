6ff85dbbd63283809f8538695afe40057f72f2c7
mbalmer <mbalmer@NetBSD.org>
2012-03-06 16:26:01 +0000

    Remove the advertising clause with the explicit ok from Perry E. Metzger.

--- usr.bin/msgc/msgdb.h
+++ usr.bin/msgc/msgdb.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: msgdb.h,v 1.2 2012/03/02 13:11:57 mbalmer Exp $	*/
+/*	$NetBSD: msgdb.h,v 1.3 2012/03/06 16:26:01 mbalmer Exp $	*/
 
 /*
  * Copyright 1997 Piermont Information Systems Inc.
@@ -14,11 +14,7 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *      This product includes software develooped for the NetBSD Project by
- *      Piermont Information Systems Inc.
- * 4. The name of Piermont Information Systems Inc. may not be used to endorse
+ * 3. The name of Piermont Information Systems Inc. may not be used to endorse
  *    or promote products derived from this software without specific prior
  *    written permission.
  *
