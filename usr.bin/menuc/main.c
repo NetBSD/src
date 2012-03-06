9aee8c6e5f396dcc1ee794d43d194d8eca885abe
mbalmer <mbalmer@NetBSD.org>
2012-03-06 16:55:18 +0000

    Remove the advertising clause and appease the eye a bit (still not good, but better).  Almost no functional change.

--- usr.bin/menuc/main.c
+++ usr.bin/menuc/main.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: main.c,v 1.10 2004/06/20 22:20:16 jmc Exp $	*/
+/*	$NetBSD: main.c,v 1.11 2012/03/06 16:55:18 mbalmer Exp $	*/
 
 /*
  * Copyright 1997 Piermont Information Systems Inc.
@@ -14,11 +14,7 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *      This product includes software develooped for the NetBSD Project by
- *      Piermont Information Systems Inc.
- * 4. The name of Piermont Information Systems Inc. may not be used to endorse
+ * 3. The name of Piermont Information Systems Inc. may not be used to endorse
  *    or promote products derived from this software without specific prior
  *    written permission.
  *
@@ -45,7 +41,7 @@
 #include <sys/cdefs.h>
 
 #if defined(__RCSID) && !defined(lint)
-__RCSID("$NetBSD: main.c,v 1.10 2004/06/20 22:20:16 jmc Exp $");
+__RCSID("$NetBSD: main.c,v 1.11 2012/03/06 16:55:18 mbalmer Exp $");
 #endif
 
 
@@ -57,36 +53,34 @@ __RCSID("$NetBSD: main.c,v 1.10 2004/06/20 22:20:16 jmc Exp $");
 #include "defs.h"
 
 /* Local prototypes */
-void usage (char *);
+__dead void usage(void);
 
 int
-main (int argc, char **argv)
+main(int argc, char **argv)
 {
 	int ch;
 
-	prog_name = argv[0];
-	
 	/* Process the arguments. */
-	while ( (ch = getopt (argc, argv, "o:")) != -1 ) {
+	while ((ch = getopt(argc, argv, "o:")) != -1 ) {
 		switch (ch) {
 		case 'o': /* output file name */
 			out_name = optarg;
 			break;
 		default:
-			usage (prog_name);
+			usage();
 		}
 	}
 
-	if (optind != argc-1)
-		usage (prog_name);
+	if (optind != argc - 1)
+		usage();
 
 	src_name = argv[optind];
 
-	yyin = fopen (src_name, "r");
+	yyin = fopen(src_name, "r");
 	if (yyin == NULL) {
-		(void) fprintf (stderr, "%s: could not open %s.\n",
-				prog_name, src_name);
-		exit (1);
+		(void)fprintf(stderr, "%s: could not open %s.\n", prog_name,
+		    src_name);
+		exit(1);
 	}
 
 	/* The default menu */
@@ -107,18 +101,15 @@ main (int argc, char **argv)
 	default_info.exitact.endwin = FALSE;
 
 	/* Do the parse */
-	(void) yyparse ();
+	(void)yyparse();
 
-	if (had_errors)
-		return 1;
-
-	return 0;
+	return had_errors ? 1 : 0;
 }
 
 
-void
-usage (char *prog)
+__dead void
+usage(void)
 {
-	(void) fprintf (stderr, "%s [-o name] file\n", prog);
-	exit (1);
+	(void)fprintf (stderr, "%s [-o name] file\n", getprogname());
+	exit(1);
 }
