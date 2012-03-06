7fc4c364a612619bd17598271772bb2cd00d208a
mbalmer <mbalmer@NetBSD.org>
2012-03-06 16:55:18 +0000

    Remove the advertising clause and appease the eye a bit (still not good, but better).  Almost no functional change.

--- usr.bin/menuc/mdb.c
+++ usr.bin/menuc/mdb.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: mdb.c,v 1.45 2011/09/06 18:16:38 joerg Exp $	*/
+/*	$NetBSD: mdb.c,v 1.46 2012/03/06 16:55:18 mbalmer Exp $	*/
 
 /*
  * Copyright 1997 Piermont Information Systems Inc.
@@ -14,11 +14,7 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *      This product includes software develooped for the NetBSD Project by
- *      Piermont Information Systems Inc.
- * 4. The name of Piermont Information Systems Inc. may not be used to endorse
+ * 3. The name of Piermont Information Systems Inc. may not be used to endorse
  *    or promote products derived from this software without specific prior
  *    written permission.
  *
@@ -45,7 +41,7 @@
 #include <sys/cdefs.h>
 
 #if defined(__RCSID) && !defined(lint)
-__RCSID("$NetBSD: mdb.c,v 1.45 2011/09/06 18:16:38 joerg Exp $");
+__RCSID("$NetBSD: mdb.c,v 1.46 2012/03/06 16:55:18 mbalmer Exp $");
 #endif
 
 
@@ -71,7 +67,7 @@ static id_rec *menus[MAX];
 /* get_menu returns a pointer to a newly created id_rec or an old one. */
 
 id_rec *
-get_menu (char *name)
+get_menu(char *name)
 {
 	id_rec *temp;
 
@@ -79,15 +75,15 @@ get_menu (char *name)
 
 	if (temp == NULL) {
 		if (menu_no < MAX) {
-			temp = (id_rec *) malloc (sizeof(id_rec));
+			temp = (id_rec *)malloc(sizeof(id_rec));
 			temp->id = strdup(name);
 			temp->info = NULL;
 			temp->menu_no = menu_no;
 			menus[menu_no++] = temp;
-			insert_id (&root, temp);
+			insert_id(&root, temp);
 		} else {
-			(void) fprintf (stderr, "Too many menus.  "
-					"Increase MAX.\n");
+			(void)fprintf(stderr, "Too many menus.  "
+			    "Increase MAX.\n");
 			exit(1);
 		}
 	}
@@ -97,13 +93,12 @@ get_menu (char *name)
 
 
 /* Verify that all menus are defined. */
-
 void
-check_defined (void)
+check_defined(void)
 {
 	int i;
 
-	for (i=0; i<menu_no; i++)
+	for (i = 0; i < menu_no; i++)
 		if (!menus[i]->info)
 			yyerror ("Menu '%s' undefined.", menus[i]->id);
 }
@@ -111,56 +106,47 @@ check_defined (void)
 
 /* Write out the menu file. */
 void
-write_menu_file (char *initcode)
+write_menu_file(char *initcode)
 {
-	FILE *out_file;
-	FILE *sys_file;
+	FILE *out_file, *sys_file;
 	int i, j;
-	char hname[1024];
-	char cname[1024];
-	char sname[1024];
-	char *sys_prefix;
-	char *tmpstr;
-	int name_is_code;
-
-	int nlen;
-
-	int ch;
-
+	char hname[1024], cname[1024], sname[1024];
+	char *sys_prefix, *tmpstr;
+	int name_is_code, nlen, ch;
 	optn_info *toptn;
 
 	/* Generate file names */
-	snprintf (hname, 1024, "%s.h", out_name);
+	snprintf(hname, 1024, "%s.h", out_name);
 	nlen = strlen(hname);
 	if (hname[nlen-2] != '.' || hname[nlen-1] != 'h') {
-		(void) fprintf (stderr, "%s: name `%s` too long.\n",
-				prog_name, out_name);
+		(void)fprintf(stderr, "%s: name `%s` too long.\n",
+		    prog_name, out_name);
 		exit(1);
 	}
-	snprintf (cname, 1024, "%s.c", out_name);
+	snprintf(cname, 1024, "%s.c", out_name);
 
 	/* Open the menu_sys file first. */
-	sys_prefix = getenv ("MENUDEF");
+	sys_prefix = getenv("MENUDEF");
 	if (sys_prefix == NULL)
 		sys_prefix = _PATH_DEFSYSPREFIX;
-	snprintf (sname, 1024, "%s/%s", sys_prefix, sys_name);
+	snprintf(sname, 1024, "%s/%s", sys_prefix, sys_name);
 	sys_file = fopen (sname, "r");
 	if (sys_file == NULL) {
-		(void) fprintf (stderr, "%s: could not open %s.\n",
-				prog_name, sname);
-		exit (1);
+		(void)fprintf(stderr, "%s: could not open %s.\n",
+		    prog_name, sname);
+		exit(1);
 	}
 
 	/* Output the .h file first. */
-	out_file = fopen (hname, "w");
+	out_file = fopen(hname, "w");
 	if (out_file == NULL) {
-		(void) fprintf (stderr, "%s: could not open %s.\n",
-				prog_name, hname);
-		exit (1);
+		(void)fprintf(stderr, "%s: could not open %s.\n",
+		    prog_name, hname);
+		exit(1);
 	}
 
 	/* Write it */
-	(void) fprintf (out_file, "%s",
+	(void)fprintf(out_file, "%s",
 		"/* menu system definitions. */\n"
 		"\n"
 		"#ifndef MENU_DEFS_H\n"
@@ -228,7 +214,7 @@ write_menu_file (char *initcode)
 #undef STR
 	);
 
-	(void) fprintf (out_file, "%s",
+	(void)fprintf(out_file, "%s",
 		"\n"
 		"/* Prototypes */\n"
 		"int menu_init(void);\n"
@@ -237,7 +223,7 @@ write_menu_file (char *initcode)
 		);
 
 	if (do_dynamic)
-		(void) fprintf (out_file, "%s",
+		(void)fprintf(out_file, "%s",
 			"int new_menu(const char *, menu_ent *, int, \n"
 			    "\tint, int, int, int, int,\n"
 			    "\tvoid (*)(menudesc *, void *), "
@@ -248,100 +234,93 @@ write_menu_file (char *initcode)
 			"void set_menu_numopts(int, int);\n"
 			);
 
-	(void) fprintf (out_file, "\n/* Menu names */\n");
-	for (i=0; i<menu_no; i++) {
-		(void) fprintf (out_file, "#define MENU_%s\t%d\n",
-				menus[i]->id, i);
-	}
+	(void)fprintf(out_file, "\n/* Menu names */\n");
+	for (i = 0; i < menu_no; i++)
+		(void)fprintf(out_file, "#define MENU_%s\t%d\n",
+		    menus[i]->id, i);
 
 	if (do_dynamic)
-		(void) fprintf (out_file, "\n#define DYN_MENU_START\t%d",
-				menu_no);
+		(void)fprintf(out_file, "\n#define DYN_MENU_START\t%d",
+		    menu_no);
 
-	(void) fprintf (out_file, "\n#define MAX_STRLEN %d\n"
-			"#endif\n", max_strlen);
-	fclose (out_file);
+	(void)fprintf (out_file, "\n#define MAX_STRLEN %d\n"
+	    "#endif\n", max_strlen);
+	fclose(out_file);
 
 	/* Now the C file */
-	out_file = fopen (cname, "w");
+	out_file = fopen(cname, "w");
 	if (out_file == NULL) {
-		(void) fprintf (stderr, "%s: could not open %s.\n",
-				prog_name, cname);
-		exit (1);
+		(void)fprintf(stderr, "%s: could not open %s.\n",
+		    prog_name, cname);
+		exit(1);
 	}
 
 	/* initial code */
-	fprintf (out_file, "#include \"%s\"\n\n", hname);
-	fprintf (out_file, "%s\n\n", initcode);
+	fprintf(out_file, "#include \"%s\"\n\n", hname);
+	fprintf(out_file, "%s\n\n", initcode);
 
 	/* data definitions */
 
 	/* func defs */
-	for (i=0; i<menu_no; i++) {
+	for (i = 0; i < menu_no; i++) {
 		if (strlen(menus[i]->info->postact.code)) {
-			(void) fprintf (out_file,
-				"/*ARGSUSED*/\n"
-				"static void menu_%d_postact(menudesc *menu, void *arg)\n{\n", i);
+			(void)fprintf(out_file, "/*ARGSUSED*/\n"
+			    "static void menu_%d_postact(menudesc *menu, void *arg)\n{\n", i);
 			if (menus[i]->info->postact.endwin)
-				(void) fprintf (out_file, "\tendwin();\n");
-			(void) fprintf (out_file,
-					"\t%s\n}\n\n",
-					menus[i]->info->postact.code);
+				(void)fprintf(out_file, "\tendwin();\n");
+			(void)fprintf(out_file, "\t%s\n}\n\n",
+			    menus[i]->info->postact.code);
 		}
 		if (strlen(menus[i]->info->exitact.code)) {
-			(void) fprintf (out_file,
-				"/*ARGSUSED*/\n"
-				"static void menu_%d_exitact(menudesc *menu, void *arg)\n{\n", i);
+			(void)fprintf(out_file, "/*ARGSUSED*/\n"
+			    "static void menu_%d_exitact(menudesc *menu, void *arg)\n{\n", i);
 			if (menus[i]->info->exitact.endwin)
-				(void) fprintf (out_file, "\tendwin();\n");
-			(void) fprintf (out_file, "\t%s\n}\n\n",
-					menus[i]->info->exitact.code);
+				(void)fprintf(out_file, "\tendwin();\n");
+			(void)fprintf(out_file, "\t%s\n}\n\n",
+			    menus[i]->info->exitact.code);
 		}
 		j = 0;
 		toptn = menus[i]->info->optns;
-		for (;toptn != NULL; j++, toptn = toptn->next) {
+		for (; toptn != NULL; j++, toptn = toptn->next) {
 			if (strlen(toptn->optact.code) == 0)
 				continue;
 
-			(void) fprintf (out_file,
-				"/*ARGSUSED*/\n"
-				"static int opt_act_%d_%d(menudesc *m, void *arg)\n"
-				"{\n\t%s\n\treturn %s;\n}\n\n",
-				i, j, toptn->optact.code,
-				(toptn->doexit ? "1" : "0"));
+			(void)fprintf(out_file, "/*ARGSUSED*/\n"
+			    "static int opt_act_%d_%d(menudesc *m, void *arg)\n"
+			    "{\n\t%s\n\treturn %s;\n}\n\n",
+			    i, j, toptn->optact.code,
+			    (toptn->doexit ? "1" : "0"));
 		}
 
 	}
 
 	/* optentX */
-	for (i=0; i<menu_no; i++) {
+	for (i = 0; i < menu_no; i++) {
 		if (menus[i]->info->numopt > 53) {
-			(void) fprintf (stderr, "Menu %s has "
+			(void)fprintf(stderr, "Menu %s has "
 				"too many options.\n",
 				menus[i]->info->title);
-			exit (1);
+			exit(1);
 		}
-		(void) fprintf (out_file,
-				"static menu_ent optent%d[] = {\n", i);
+		(void)fprintf(out_file, "static menu_ent optent%d[] = {\n", i);
 		name_is_code = 0;
 		for (j = 0, toptn = menus[i]->info->optns; toptn;
 		    toptn = toptn->next, j++) {
 			name_is_code += toptn->name_is_code;
-			(void) fprintf (out_file, "\t{%s,%d,%d,",
+			(void)fprintf(out_file, "\t{%s,%d,%d,",
 				toptn->name_is_code ? "0" : toptn->name,
 				toptn->menu,
 				(toptn->issub ? OPT_SUB : 0)
 				+(toptn->doexit ? OPT_EXIT : 0)
 				+(toptn->optact.endwin ? OPT_ENDWIN : 0));
 			if (strlen(toptn->optact.code)) 
-				(void) fprintf (out_file, "opt_act_%d_%d}",
-					i, j);
+				(void)fprintf(out_file, "opt_act_%d_%d}", i, j);
 			else
-				(void) fprintf (out_file, "NULL}");
-			(void) fprintf (out_file, "%s\n",
+				(void)fprintf(out_file, "NULL}");
+			(void)fprintf(out_file, "%s\n",
 				(toptn->next ? "," : ""));
 		}
-		(void) fprintf (out_file, "\t};\n\n");
+		(void)fprintf(out_file, "\t};\n\n");
 
 		if (name_is_code) {
 			menus[i]->info->name_is_code = 1;
@@ -361,20 +340,20 @@ write_menu_file (char *initcode)
 
 
 	/* menus */
-	if (!do_dynamic) {
-	    (void) fprintf (out_file, "static int num_menus = %d;\n", menu_no);
-	}
+	if (!do_dynamic)
+		(void)fprintf(out_file, "static int num_menus = %d;\n",
+		    menu_no);
 
-	(void) fprintf (out_file, "static struct menudesc menu_def[] = {\n");
-	for (i=0; i<menu_no; i++) {
-		(void) fprintf (out_file,
+	(void)fprintf(out_file, "static struct menudesc menu_def[] = {\n");
+	for (i = 0; i < menu_no; i++) {
+		(void)fprintf(out_file,
 			"\t{%s,%d,%d,%d,%d,%d,%d,0,0,optent%d,NULL,NULL,",
 			menus[i]->info->title, 	menus[i]->info->y,
 			menus[i]->info->x, menus[i]->info->h,
 			menus[i]->info->w, menus[i]->info->mopt,
 			menus[i]->info->numopt, i);
 		if (menus[i]->info->helpstr == NULL)
-			(void) fprintf (out_file, "NULL");
+			(void)fprintf(out_file, "NULL");
 		else {
 			tmpstr = menus[i]->info->helpstr;
 			if (*tmpstr != '"')
@@ -383,50 +362,49 @@ write_menu_file (char *initcode)
 				/* Skip an initial newline. */
 				if (tmpstr[1] == '\n')
 					*++tmpstr = '"';
-				(void) fprintf (out_file, "\n");
+				(void)fprintf(out_file, "\n");
 				while (*tmpstr) {
 					if (*tmpstr != '\n') {
-						fputc (*tmpstr++, out_file);
+						fputc(*tmpstr++, out_file);
 						continue;
 					}
-					(void) fprintf (out_file, "\\n\"\n\"");
+					(void)fprintf(out_file, "\\n\"\n\"");
 					tmpstr++;
 				}
 			}
 		}
-		(void) fprintf (out_file, ",");
+		(void)fprintf(out_file, ",");
 		if (menus[i]->info->mopt & MC_NOEXITOPT)
 			(void) fprintf (out_file, "NULL");
 		else if (menus[i]->info->exitstr != NULL)
-			(void) fprintf (out_file, "%s",
-				menus[i]->info->exitstr);
+			(void)fprintf(out_file, "%s", menus[i]->info->exitstr);
 		else
-			(void) fprintf (out_file, "\"Exit\"");
+			(void)fprintf(out_file, "\"Exit\"");
 		if (strlen(menus[i]->info->postact.code))
-			(void) fprintf (out_file, ",menu_%d_postact", i);
+			(void)fprintf(out_file, ",menu_%d_postact", i);
 		else
-			(void) fprintf (out_file, ",NULL");
+			(void)fprintf(out_file, ",NULL");
 		if (strlen(menus[i]->info->exitact.code))
-			(void) fprintf (out_file, ",menu_%d_exitact", i);
+			(void)fprintf(out_file, ",menu_%d_exitact", i);
 		else
-			(void) fprintf (out_file, ",NULL");
+			(void)fprintf(out_file, ",NULL");
 		if (menus[i]->info->name_is_code)
-			(void) fprintf (out_file, ",menu_%d_legend", i);
+			(void)fprintf(out_file, ",menu_%d_legend", i);
 		else
-			(void) fprintf (out_file, ",NULL");
+			(void)fprintf(out_file, ",NULL");
 
-		(void) fprintf (out_file, "},\n");
+		(void)fprintf(out_file, "},\n");
 
 	}
-	(void) fprintf (out_file, "{NULL, 0, 0, 0, 0, 0, 0, 0, 0, "
+	(void)fprintf(out_file, "{NULL, 0, 0, 0, 0, 0, 0, 0, 0, "
 		"NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL}};\n\n");
 
 	/* __menu_initerror: initscr failed. */
-	(void) fprintf (out_file, 
+	(void)fprintf(out_file, 
 		"/* __menu_initerror: initscr failed. */\n"
 		"void __menu_initerror (void) {\n");
 	if (error_act.code == NULL) {
-		(void) fprintf (out_file,
+		(void)fprintf(out_file,
 			"\t(void) fprintf (stderr, "
 			"\"%%s: Could not initialize curses\\n\", "
 			"getprogname());\n"
@@ -434,8 +412,8 @@ write_menu_file (char *initcode)
 			"}\n");
 	} else {
 		if (error_act.endwin)
-			(void) fprintf (out_file, "\tendwin();\n");
-		(void) fprintf (out_file, "%s\n}\n", error_act.code);
+			(void)fprintf(out_file, "\tendwin();\n");
+		(void)fprintf(out_file, "%s\n}\n", error_act.code);
 	}
 
 	/* Copy menu_sys.def file. */
@@ -448,7 +426,6 @@ write_menu_file (char *initcode)
 		while ((ch = fgetc(sys_file)) != EOF)
 			fputc(ch, out_file);
 	}
-		
-	fclose (out_file);
-	fclose (sys_file);
+	fclose(out_file);
+	fclose(sys_file);
 }
