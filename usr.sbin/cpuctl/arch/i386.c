9ccecb168a03fee7b439481fd73e3b9fd31a9fbb
riz <riz@NetBSD.org>
2012-03-07 23:26:01 +0000

    Pull up following revision(s) (requested by sborrill in ticket #79): 	usr.sbin/cpuctl/arch/i386.c: revision 1.29 Print CPU stepping level

--- usr.sbin/cpuctl/arch/i386.c
+++ usr.sbin/cpuctl/arch/i386.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: i386.c,v 1.27.2.1 2012/03/05 19:12:06 sborrill Exp $	*/
+/*	$NetBSD: i386.c,v 1.27.2.2 2012/03/07 23:26:01 riz Exp $	*/
 
 /*-
  * Copyright (c) 1999, 2000, 2001, 2006, 2007, 2008 The NetBSD Foundation, Inc.
@@ -57,7 +57,7 @@
 
 #include <sys/cdefs.h>
 #ifndef lint
-__RCSID("$NetBSD: i386.c,v 1.27.2.1 2012/03/05 19:12:06 sborrill Exp $");
+__RCSID("$NetBSD: i386.c,v 1.27.2.2 2012/03/07 23:26:01 riz Exp $");
 #endif /* not lint */
 
 #include <sys/types.h>
@@ -1517,9 +1517,10 @@ identifycpu(const char *cpuname)
 #endif
 
 	aprint_normal_dev(ci->ci_dev, "family %02x model %02x "
-	    "extfamily %02x extmodel %02x\n", CPUID2FAMILY(ci->ci_signature),
-	    CPUID2MODEL(ci->ci_signature), CPUID2EXTFAMILY(ci->ci_signature),
-	    CPUID2EXTMODEL(ci->ci_signature));
+	    "extfamily %02x extmodel %02x stepping %02x\n",
+	    CPUID2FAMILY(ci->ci_signature), CPUID2MODEL(ci->ci_signature),
+	    CPUID2EXTFAMILY(ci->ci_signature), CPUID2EXTMODEL(ci->ci_signature),
+	    CPUID2STEPPING(ci->ci_signature));
 }
 
 static const char *
