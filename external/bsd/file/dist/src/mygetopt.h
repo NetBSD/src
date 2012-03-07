2434ae5ad81b5bac290d24528e9ead972e41bdc1
riz <riz>
2012-03-07 23:18:21 +0000

    Pull up following revision(s) (requested by christos in ticket #30): 	Update file to 5.11 (CDF security fixes)

--- external/bsd/file/dist/src/mygetopt.h
+++ external/bsd/file/dist/src/mygetopt.h
@@ -1,4 +1,6 @@
-/*	$NetBSD: mygetopt.h,v 1.1.1.1 2009/05/08 16:35:06 christos Exp $	*/
+/*	$NetBSD: mygetopt.h,v 1.1.1.1.10.1 2012/03/07 23:18:29 riz Exp $	*/
+
+/*	NetBSD: getopt.h,v 1.8 2007/11/06 19:21:18 christos Exp 	*/
 
 /*-
  * Copyright (c) 2000 The NetBSD Foundation, Inc.
