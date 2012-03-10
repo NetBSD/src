255494091471c3e08bf018d0594cfed06ac5be2d
martin <martin@NetBSD.org>
2012-03-10 08:46:45 +0000

    Make sure the child of a posix_spawn operation is not preempted during the times when it does not have any vm_space. Should fix PR kern/46153.

--- sys/kern/kern_exec.c
+++ sys/kern/kern_exec.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: kern_exec.c,v 1.344 2012/02/21 04:13:22 christos Exp $	*/
+/*	$NetBSD: kern_exec.c,v 1.345 2012/03/10 08:46:45 martin Exp $	*/
 
 /*-
  * Copyright (c) 2008 The NetBSD Foundation, Inc.
@@ -59,7 +59,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: kern_exec.c,v 1.344 2012/02/21 04:13:22 christos Exp $");
+__KERNEL_RCSID(0, "$NetBSD: kern_exec.c,v 1.345 2012/03/10 08:46:45 martin Exp $");
 
 #include "opt_exec.h"
 #include "opt_ktrace.h"
@@ -1739,11 +1739,13 @@ spawn_return(void *arg)
 	register_t retval;
 	bool have_reflock;
 
+	/* we have been created non-preemptable */
+	KASSERT(l->l_nopreempt == 1);
+
 	/*
 	 * The following actions may block, so we need a temporary
 	 * vmspace - borrow the kernel one
 	 */
-	KPREEMPT_DISABLE(l);
 	l->l_proc->p_vmspace = proc0.p_vmspace;
 	pmap_activate(l);
 	KPREEMPT_ENABLE(l);
@@ -1869,8 +1871,6 @@ spawn_return(void *arg)
 	KPREEMPT_DISABLE(l);
 	pmap_deactivate(l);
 	l->l_proc->p_vmspace = NULL;
-	KPREEMPT_ENABLE(l);
-
 
 	/* now do the real exec */
 	rw_enter(&exec_lock, RW_READER);
@@ -1880,6 +1880,9 @@ spawn_return(void *arg)
 		error = 0;
 	else if (error)
 		goto report_error;
+
+	/* we now have our own vmspace */
+	KPREEMPT_ENABLE(l);
 
 	/* done, signal parent */
 	mutex_enter(&spawn_data->sed_mtx_child);
@@ -2229,6 +2232,8 @@ sys_posix_spawn(struct lwp *l1, const struct sys_posix_spawn_args *uap,
 		mutex_exit(p2->p_lock);
 		kauth_cred_free(ocred);
 	}
+
+	l2->l_nopreempt = 1; /* start it non-preemptable */
 
 	/*
 	 * It's now safe for the scheduler and other processes to see the
