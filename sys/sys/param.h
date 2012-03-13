08fbdbce48bc1606e003d35244126df564728720
elad <elad@NetBSD.org>
2012-03-13 18:50:41 +0000

    6.99.4

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.411 2012/02/19 21:06:58 rmind Exp $	*/
+/*	$NetBSD: param.h,v 1.412 2012/03/13 18:50:41 elad Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -63,7 +63,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	699000300	/* NetBSD 6.99.3 */
+#define	__NetBSD_Version__	699000400	/* NetBSD 6.99.4 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
