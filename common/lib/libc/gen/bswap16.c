71319606ee8c8a9c237b40a55329dade72d2066e
martin <martin@NetBSD.org>
2012-03-17 20:57:35 +0000

    Mark linted

--- common/lib/libc/gen/bswap16.c
+++ common/lib/libc/gen/bswap16.c
@@ -1,4 +1,4 @@
-/*  $NetBSD: bswap16.c,v 1.3 2011/07/04 21:20:27 joerg Exp $    */
+/*  $NetBSD: bswap16.c,v 1.4 2012/03/17 20:57:35 martin Exp $    */
 
 /*
  * Written by Manuel Bouyer <bouyer@NetBSD.org>.
@@ -7,7 +7,7 @@
 
 #include <sys/cdefs.h>
 #if defined(LIBC_SCCS) && !defined(lint)
-__RCSID("$NetBSD: bswap16.c,v 1.3 2011/07/04 21:20:27 joerg Exp $");
+__RCSID("$NetBSD: bswap16.c,v 1.4 2012/03/17 20:57:35 martin Exp $");
 #endif /* LIBC_SCCS and not lint */
 
 #include <sys/types.h>
@@ -18,5 +18,6 @@ __RCSID("$NetBSD: bswap16.c,v 1.3 2011/07/04 21:20:27 joerg Exp $");
 uint16_t
 bswap16(uint16_t x)
 {
+	/*LINTED*/
 	return ((x << 8) & 0xff00) | ((x >> 8) & 0x00ff);
 }
