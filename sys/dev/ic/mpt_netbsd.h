aa4eaabb97b98f8129c1390016ce18697e60f3aa
martin <martin@NetBSD.org>
2012-03-18 21:05:21 +0000

    Split device_t

--- sys/dev/ic/mpt_netbsd.h
+++ sys/dev/ic/mpt_netbsd.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: mpt_netbsd.h,v 1.9 2010/11/13 13:52:01 uebayasi Exp $	*/
+/*	$NetBSD: mpt_netbsd.h,v 1.10 2012/03/18 21:05:21 martin Exp $	*/
 
 /*
  * Copyright (c) 2003 Wasabi Systems, Inc.
@@ -160,7 +160,7 @@ typedef struct req_entry {
 } request_t;
 
 typedef struct mpt_softc {
-	struct device	sc_dev;		/* base device glue */
+	device_t	sc_dev;		/* base device glue */
 
 	/* Locking context */
 	int		mpt_splsaved;
