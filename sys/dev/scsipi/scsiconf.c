d68eb50079d65aa15d9db88b2abf897f92fcf64f
riz <riz@NetBSD.org>
2012-03-19 23:13:59 +0000

    Pull up following revision(s) (requested by mrg in ticket #125): 	sys/dev/scsipi/scsiconf.c: revision 1.263 	sys/dev/scsipi/scsiconf.c: revision 1.264 	sys/dev/ic/ncr53c9x.c: revision 1.144 	sys/dev/ic/ninjascsi32.c: revision 1.22 	sys/dev/usb/uhub.c: revision 1.117 take the kernel lock in functions called from attach*(). scsidevdetached ioctl path enters scsipi code without kernel lock and this upsets the newer kasserts.  take kernel lock here. take the kernel lock a few more places when doing detach, to avoid triggering KERNEL_LOCK_P() asserts in both scsi and usb code. with this and other recent fixes i can now "drvctl -d ehci0".

--- sys/dev/scsipi/scsiconf.c
+++ sys/dev/scsipi/scsiconf.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: scsiconf.c,v 1.262 2011/04/26 07:41:18 hannken Exp $	*/
+/*	$NetBSD: scsiconf.c,v 1.262.10.1 2012/03/19 23:13:59 riz Exp $	*/
 
 /*-
  * Copyright (c) 1998, 1999, 2004 The NetBSD Foundation, Inc.
@@ -48,7 +48,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: scsiconf.c,v 1.262 2011/04/26 07:41:18 hannken Exp $");
+__KERNEL_RCSID(0, "$NetBSD: scsiconf.c,v 1.262.10.1 2012/03/19 23:13:59 riz Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -256,11 +256,18 @@ scsibusdetach(device_t self, int flags)
 	struct scsipi_xfer *xs;
 	int error;
 
+	/* XXXSMP scsipi */
+	KERNEL_LOCK(1, curlwp);
+
 	/*
 	 * Detach all of the periphs.
 	 */
-	if ((error = scsipi_target_detach(chan, -1, -1, flags)) != 0)
+	if ((error = scsipi_target_detach(chan, -1, -1, flags)) != 0) {
+		/* XXXSMP scsipi */
+		KERNEL_UNLOCK_ONE(curlwp);
+
 		return error;
+	}
 
 	pmf_device_deregister(self);
 
@@ -290,6 +297,10 @@ scsibusdetach(device_t self, int flags)
 	 * Now shut down the channel.
 	 */
 	scsipi_channel_shutdown(chan);
+
+	/* XXXSMP scsipi */
+	KERNEL_UNLOCK_ONE(curlwp);
+
 	return 0;
 }
 
@@ -378,11 +389,17 @@ scsidevdetached(device_t self, device_t child)
 	target = device_locator(child, SCSIBUSCF_TARGET);
 	lun = device_locator(child, SCSIBUSCF_LUN);
 
+	/* XXXSMP scsipi */
+	KERNEL_LOCK(1, curlwp);
+
 	periph = scsipi_lookup_periph(chan, target, lun);
 	KASSERT(periph->periph_dev == child);
 
 	scsipi_remove_periph(chan, periph);
 	free(periph, M_DEVBUF);
+
+	/* XXXSMP scsipi */
+	KERNEL_UNLOCK_ONE(curlwp);
 }
 
 /*
