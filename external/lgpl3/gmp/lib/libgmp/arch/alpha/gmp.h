2466239b541b7e32dc52354e68c8ddce6d6558cb
mrg <mrg@NetBSD.org>
2012-03-20 08:21:38 +0000

    remove references to my src/obj trees, and make a note about avoiding this in the future / should automate it too.

--- external/lgpl3/gmp/lib/libgmp/arch/alpha/gmp.h
+++ external/lgpl3/gmp/lib/libgmp/arch/alpha/gmp.h
@@ -2271,7 +2271,7 @@ enum
 };
 
 /* Define CC and CFLAGS which were used to build this version of GMP */
-#define __GMP_CC "/var/obj/alpha/usr/src3/tooldir.NetBSD-5.99.54-amd64/bin/alpha--netbsd-gcc --sysroot=/var/obj/alpha/usr/src3/destdir.alpha -L/var/obj/alpha/usr/src3/destdir.alpha/lib -L/var/obj/alpha/usr/src3/destdir.alpha/usr/lib -B/var/obj/alpha/usr/src3/destdir.alpha/usr/lib/ -I/var/tmp/gmp/alpha/.native/gcc/include -std=gnu99"
+#define __GMP_CC "gcc -std=gnu99"
 #define __GMP_CFLAGS "-O2 -pedantic -Wa,-mev4 -mcpu=ev4"
 
 /* Major version number is the value of __GNU_MP__ too, above and in mp.h. */
