4144639ef7da97842a00b5456b525722177e61f3
riz <riz>
2012-03-21 16:10:21 +0000

    Pull up following revision(s) (requested by mrg in ticket #131): 	sys/arch/sparc64/sparc64/trap.c: revision 1.170 	sys/arch/sparc64/sparc64/trap.c: revision 1.171 	sys/arch/sparc64/sparc64/locore.s: revision 1.341 port the corrected ECC error handling code from freebsd.  i noticed my U10 took one of these and then hang.  it shouldn't hang, there's a 'sir' here that doesn't seem to reset properly.  oh well. tested by simulated a trap via 'ta 0x10' and considering that the same, but it hasn't been tested against a real ECC error yet. count ECC corrected traps with evcnt(9).

--- sys/arch/sparc64/sparc64/locore.s
+++ sys/arch/sparc64/sparc64/locore.s
@@ -1,4 +1,4 @@
-/*	$NetBSD: locore.s,v 1.338.8.1 2012/03/05 20:59:25 sborrill Exp $	*/
+/*	$NetBSD: locore.s,v 1.338.8.2 2012/03/21 16:10:21 riz Exp $	*/
 
 /*
  * Copyright (c) 2006-2010 Matthew R. Green
@@ -491,7 +491,7 @@ _C_LABEL(trapbase):
 	VTRAP(0x060, interrupt_vector); ! 060 = interrupt vector
 	TRAP(T_PA_WATCHPT)		! 061 = physical address data watchpoint
 	TRAP(T_VA_WATCHPT)		! 062 = virtual address data watchpoint
-	UTRAP(T_ECCERR)			! We'll implement this one later
+	TRAP(T_ECCERR)			! 063 = corrected ECC error
 ufast_IMMU_miss:			! 064 = fast instr access MMU miss
 	ldxa	[%g0] ASI_IMMU_8KPTR, %g2 ! Load IMMU 8K TSB pointer
 #ifdef NO_TSB
@@ -727,7 +727,7 @@ kdatafault:
 	VTRAP(0x060, interrupt_vector); ! 060 = interrupt vector
 	TRAP(T_PA_WATCHPT)		! 061 = physical address data watchpoint
 	TRAP(T_VA_WATCHPT)		! 062 = virtual address data watchpoint
-	UTRAP(T_ECCERR)			! We'll implement this one later
+	TRAP(T_ECCERR)			! 063 = corrected ECC error
 kfast_IMMU_miss:			! 064 = fast instr access MMU miss
 	ldxa	[%g0] ASI_IMMU_8KPTR, %g2 ! Load IMMU 8K TSB pointer
 #ifdef NO_TSB
