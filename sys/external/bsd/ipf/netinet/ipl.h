ecd7789b3520ddf1f3d0999568dae7eaec51c78e
christos <christos@NetBSD.org>
2012-03-23 20:36:52 +0000

    import kernel portion of ipfilter 5.1.1

---
+++ sys/external/bsd/ipf/netinet/ipl.h
@@ -0,0 +1,19 @@
+/*	$NetBSD: ipl.h,v 1.1.1.1 2012/03/23 20:37:04 christos Exp $	*/
+
+/*
+ * Copyright (C) 2010 by Darren Reed.
+ *
+ * See the IPFILTER.LICENCE file for details on licencing.
+ *
+ * @(#)ipl.h	1.21 6/5/96
+ * Id
+ */
+
+#ifndef	__IPL_H__
+#define	__IPL_H__
+
+#define	IPL_VERSION	"IP Filter: v5.1.1"
+
+#define	IPFILTER_VERSION	5010100
+
+#endif
