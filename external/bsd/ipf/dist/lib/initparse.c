a30924a661ed5b92fcca2798cda58065c2c6715e
christos <christos>
2012-03-23 21:19:53 +0000

    import 5.1.1

---
+++ external/bsd/ipf/dist/lib/initparse.c
@@ -0,0 +1,20 @@
+/*	$NetBSD: initparse.c,v 1.1.1.1 2012/03/23 21:20:08 christos Exp $	*/
+
+/*
+ * Copyright (C) 2009 by Darren Reed.
+ *
+ * See the IPFILTER.LICENCE file for details on licencing.
+ *
+ * Id
+ */
+#include "ipf.h"
+
+
+char	thishost[MAXHOSTNAMELEN];
+
+
+void initparse __P((void))
+{
+	gethostname(thishost, sizeof(thishost));
+	thishost[sizeof(thishost) - 1] = '\0';
+}
