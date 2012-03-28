8d49af7f6198be9ff192c912f4a39a532a289d9f
christos <christos@NetBSD.org>
2012-03-28 17:03:27 +0000

    - Normalize inclusion protection (remove) - Move CHAR_{MIN,MAX} to a common file. - Fix broken comments

--- sys/arch/news68k/include/limits.h
+++ sys/arch/news68k/include/limits.h
@@ -1,4 +1,3 @@
-/*	$NetBSD: limits.h,v 1.2 2005/12/06 16:51:17 tsutsui Exp $	*/
+/*	$NetBSD: limits.h,v 1.3 2012/03/28 17:03:30 christos Exp $	*/
 
-/* Just use the common m68k definition */
 #include <m68k/limits.h>
