5c675bb1a94da014cc57126e3bf390d191396a10
riz <riz@NetBSD.org>
2012-04-03 16:14:02 +0000

    Pull up following revision(s) (requested by para in ticket #155): 	sys/kern/subr_vmem.c: revision 1.73 	sys/kern/subr_kmem.c: revision 1.43 	sys/rump/librump/rumpkern/vm.c: revision 1.124 make accounting for vm_inuse sane while here don't statically allocated for more caches then required adjust rump for static pool_cache count should have went in with subr_vmem 1.73 don't overallocated once we leave the caches

--- sys/rump/librump/rumpkern/vm.c
+++ sys/rump/librump/rumpkern/vm.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: vm.c,v 1.122.2.1 2012/03/25 11:29:59 bouyer Exp $	*/
+/*	$NetBSD: vm.c,v 1.122.2.2 2012/04/03 16:14:02 riz Exp $	*/
 
 /*
  * Copyright (c) 2007-2011 Antti Kantee.  All Rights Reserved.
@@ -41,7 +41,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: vm.c,v 1.122.2.1 2012/03/25 11:29:59 bouyer Exp $");
+__KERNEL_RCSID(0, "$NetBSD: vm.c,v 1.122.2.2 2012/04/03 16:14:02 riz Exp $");
 
 #include <sys/param.h>
 #include <sys/atomic.h>
@@ -340,7 +340,7 @@ uvm_init(void)
 
 	kmem_va_arena = vmem_create("kva", 0, 0, PAGE_SIZE,
 	    vmem_alloc, vmem_free, kmem_arena,
-	    32 * PAGE_SIZE, VM_NOSLEEP | VM_BOOTSTRAP, IPL_VM);
+	    8 * PAGE_SIZE, VM_NOSLEEP | VM_BOOTSTRAP, IPL_VM);
 
 	pool_cache_bootstrap(&pagecache, sizeof(struct vm_page), 0, 0, 0,
 	    "page$", NULL, IPL_NONE, pgctor, pgdtor, NULL);
