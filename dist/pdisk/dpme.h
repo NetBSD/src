eccb1e5894eb3cb738f4151061e4c7c6229ab6b2
christos <christos>
2012-04-06 22:41:47 +0000

    make this work on 64 bit machines; use stdint/inttypes.

--- dist/pdisk/dpme.h
+++ dist/pdisk/dpme.h
@@ -53,76 +53,72 @@
 // A/UX only stuff (tradition!)
 #define	dpme_bzb	dpme_boot_args
 #define	BZBMAGIC 0xABADBABE	/* BZB magic number */
-#define	FST	((u8) 0x1)	/* standard UNIX FS */
-#define	FSTEFS	((u8) 0x2)	/* Autorecovery FS */
-#define	FSTSFS	((u8) 0x3)	/* Swap FS */
+#define	FST	((uint8_t) 0x1)	/* standard UNIX FS */
+#define	FSTEFS	((uint8_t) 0x2)	/* Autorecovery FS */
+#define	FSTSFS	((uint8_t) 0x3)	/* Swap FS */
 
 
 //
 // Types
 //
-typedef	unsigned char	u8;
-typedef	unsigned short	u16;
-typedef	unsigned long	u32;
-
 
 // Physical block zero of the disk has this format
 struct Block0 {
-    u16 	sbSig;		/* unique value for SCSI block 0 */
-    u16 	sbBlkSize;	/* block size of device */
-    u32 	sbBlkCount;	/* number of blocks on device */
-    u16 	sbDevType;	/* device type */
-    u16 	sbDevId;	/* device id */
-    u32 	sbData;		/* not used */
-    u16 	sbDrvrCount;	/* driver descriptor count */
-    u16 	sbMap[247];	/* descriptor map */
+    uint16_t 	sbSig;		/* unique value for SCSI block 0 */
+    uint16_t 	sbBlkSize;	/* block size of device */
+    uint32_t 	sbBlkCount;	/* number of blocks on device */
+    uint16_t 	sbDevType;	/* device type */
+    uint16_t 	sbDevId;	/* device id */
+    uint32_t 	sbData;		/* not used */
+    uint16_t 	sbDrvrCount;	/* driver descriptor count */
+    uint16_t 	sbMap[247];	/* descriptor map */
 };
 typedef struct Block0 Block0;
 
 // Where &sbMap[0] is actually an array DDMap[sbDrvrCount]
 // kludge to get around alignment junk
 struct DDMap {
-    u32 	ddBlock;	/* 1st driver's starting block (in sbBlkSize blocks!) */
-    u16 	ddSize;		/* size of 1st driver (512-byte blks) */
-    u16 	ddType;		/* system type (1 for Mac+) */
+    uint32_t 	ddBlock;	/* 1st driver's starting block (in sbBlkSize blocks!) */
+    uint16_t 	ddSize;		/* size of 1st driver (512-byte blks) */
+    uint16_t 	ddType;		/* system type (1 for Mac+) */
 };
 typedef struct DDMap DDMap;
 
 
 // Each partition map entry (blocks 1 through n) has this format
 struct dpme {
-    u16     dpme_signature          ;
-    u16     dpme_reserved_1         ;
-    u32     dpme_map_entries        ;
-    u32     dpme_pblock_start       ;
-    u32     dpme_pblocks            ;
+    uint16_t     dpme_signature          ;
+    uint16_t     dpme_reserved_1         ;
+    uint32_t     dpme_map_entries        ;
+    uint32_t     dpme_pblock_start       ;
+    uint32_t     dpme_pblocks            ;
     char    dpme_name[DPISTRLEN]    ;  /* name of partition */
     char    dpme_type[DPISTRLEN]    ;  /* type of partition */
-    u32     dpme_lblock_start       ;
-    u32     dpme_lblocks            ;
-    u32     dpme_flags;
+    uint32_t     dpme_lblock_start       ;
+    uint32_t     dpme_lblocks            ;
+    uint32_t     dpme_flags;
 #if 0
-    u32     dpme_reserved_2    : 23 ;  /* Bit 9 through 31.        */
-    u32     dpme_os_specific_1 :  1 ;  /* Bit 8.                   */
-    u32     dpme_os_specific_2 :  1 ;  /* Bit 7.                   */
-    u32     dpme_os_pic_code   :  1 ;  /* Bit 6.                   */
-    u32     dpme_writable      :  1 ;  /* Bit 5.                   */
-    u32     dpme_readable      :  1 ;  /* Bit 4.                   */
-    u32     dpme_bootable      :  1 ;  /* Bit 3.                   */
-    u32     dpme_in_use        :  1 ;  /* Bit 2.                   */
-    u32     dpme_allocated     :  1 ;  /* Bit 1.                   */
-    u32     dpme_valid         :  1 ;  /* Bit 0.                   */
+    uint32_t     dpme_reserved_2    : 23 ;  /* Bit 9 through 31.        */
+    uint32_t     dpme_os_specific_1 :  1 ;  /* Bit 8.                   */
+    uint32_t     dpme_os_specific_2 :  1 ;  /* Bit 7.                   */
+    uint32_t     dpme_os_pic_code   :  1 ;  /* Bit 6.                   */
+    uint32_t     dpme_writable      :  1 ;  /* Bit 5.                   */
+    uint32_t     dpme_readable      :  1 ;  /* Bit 4.                   */
+    uint32_t     dpme_bootable      :  1 ;  /* Bit 3.                   */
+    uint32_t     dpme_in_use        :  1 ;  /* Bit 2.                   */
+    uint32_t     dpme_allocated     :  1 ;  /* Bit 1.                   */
+    uint32_t     dpme_valid         :  1 ;  /* Bit 0.                   */
 #endif
-    u32     dpme_boot_block         ;
-    u32     dpme_boot_bytes         ;
-    u8     *dpme_load_addr          ;
-    u8     *dpme_load_addr_2        ;
-    u8     *dpme_goto_addr          ;
-    u8     *dpme_goto_addr_2        ;
-    u32     dpme_checksum           ;
+    uint32_t     dpme_boot_block         ;
+    uint32_t     dpme_boot_bytes         ;
+    uint32_t     dpme_load_addr          ;
+    uint32_t     dpme_load_addr_2        ;
+    uint32_t     dpme_goto_addr          ;
+    uint32_t     dpme_goto_addr_2        ;
+    uint32_t     dpme_checksum           ;
     char    dpme_process_id[16]     ;
-    u32     dpme_boot_args[32]      ;
-    u32     dpme_reserved_3[62]     ;
+    uint32_t     dpme_boot_args[32]      ;
+    uint32_t     dpme_reserved_3[62]     ;
 };
 typedef struct dpme DPME;
 
@@ -158,9 +154,9 @@ typedef struct dpme DPME;
 // Alternate block map (aka bad block remaping) [Never really used]
 struct abm		/* altblk map info stored in bzb */
 {
-    u32  abm_size;	/* size of map in bytes */
-    u32  abm_ents;	/* number of used entries */
-    u32  abm_start;	/* start of altblk map */
+    uint32_t  abm_size;	/* size of map in bytes */
+    uint32_t  abm_ents;	/* number of used entries */
+    uint32_t  abm_start;	/* start of altblk map */
 };
 typedef	struct abm ABM;
 
@@ -169,25 +165,25 @@ typedef	struct abm ABM;
 // kludge to get around alignment junk
 struct	bzb			/* block zero block format */
 {
-    u32  bzb_magic;		/* magic number */
-    u8   bzb_cluster;		/* Autorecovery cluster grouping */
-    u8   bzb_type;		/* FS type */
-    u16  bzb_inode;		/* bad block inode number */
-    u32  bzb_flags;
+    uint32_t  bzb_magic;		/* magic number */
+    uint8_t   bzb_cluster;		/* Autorecovery cluster grouping */
+    uint8_t   bzb_type;		/* FS type */
+    uint16_t  bzb_inode;		/* bad block inode number */
+    uint32_t  bzb_flags;
 #if 0
-    u16  bzb_root:1,		/* FS is a root FS */
+    uint16_t  bzb_root:1,		/* FS is a root FS */
 	 bzb_usr:1,		/* FS is a usr FS */
 	 bzb_crit:1,		/* FS is a critical FS */
 	 bzb_rsrvd:8,		/* reserved for later use */
 	 bzb_slice:5;		/* slice number to associate with plus one */
-    u16  bzb_filler;		/* pad bitfield to 32 bits */
+    uint16_t  bzb_filler;		/* pad bitfield to 32 bits */
 #endif
-    u32  bzb_tmade;		/* time of FS creation */
-    u32  bzb_tmount;		/* time of last mount */
-    u32  bzb_tumount;		/* time of last umount */
+    uint32_t  bzb_tmade;		/* time of FS creation */
+    uint32_t  bzb_tmount;		/* time of last mount */
+    uint32_t  bzb_tumount;		/* time of last umount */
     ABM  bzb_abm;		/* altblk map info */
-    u32  bzb_fill2[7];		/* for expansion of ABM (ha!ha!) */
-    u8   bzb_mount_point[64];	/* default mount point name */
+    uint32_t  bzb_fill2[7];		/* for expansion of ABM (ha!ha!) */
+    uint8_t   bzb_mount_point[64];	/* default mount point name */
 };
 typedef	struct bzb	BZB;
 
