815853c3dfe02f92e9293dd5eecb1f17ae5b877f
christos <christos@NetBSD.org>
2012-04-18 02:55:05 +0000

    NetBSD fixes: 1. thread_self() returns a pointer, not an unsigned integer 2. Add NetBSD to Apple and FreeBSD defines 3. Add _NETBSD_SOURCE where needed 4. Add an extra define BEGIN_PROTECTV for void functions to avoid return    free(); where free is void. 5. Avoid weak symbol hacks to determine if we are threaded or not. We    have a threaded copy of the library, why bother? 6. change __attribute -> __attribute__ since the former is not covered by    our cdefs.h

--- external/gpl3/gcc/dist/libmudflap/mf-hooks1.c
+++ external/gpl3/gcc/dist/libmudflap/mf-hooks1.c
@@ -33,7 +33,7 @@ see the files COPYING3 and COPYING.RUNTIME respectively.  If not, see
 
 /* These attempt to coax various unix flavours to declare all our
    needed tidbits in the system headers.  */
-#if !defined(__FreeBSD__)  && !defined(__APPLE__)
+#if !defined(__FreeBSD__)  && !defined(__APPLE__) && !defined(__NetBSD__)
 #define _POSIX_SOURCE
 #endif /* Some BSDs break <sys/socket.h> if this is defined. */
 #define _GNU_SOURCE
@@ -238,7 +238,7 @@ WRAPPER(void, free, void *buf)
   static int freeq_initialized = 0;
   DECLARE(void, free, void *);
 
-  BEGIN_PROTECT (free, buf);
+  BEGIN_PROTECTV (free, buf);
 
   if (UNLIKELY(buf == NULL))
     return;
