f89a1d6f2bb94ea042b1bc0bebfa0378eb0a5e57
christos <christos@NetBSD.org>
2012-04-18 02:55:05 +0000

    NetBSD fixes: 1. thread_self() returns a pointer, not an unsigned integer 2. Add NetBSD to Apple and FreeBSD defines 3. Add _NETBSD_SOURCE where needed 4. Add an extra define BEGIN_PROTECTV for void functions to avoid return    free(); where free is void. 5. Avoid weak symbol hacks to determine if we are threaded or not. We    have a threaded copy of the library, why bother? 6. change __attribute -> __attribute__ since the former is not covered by    our cdefs.h

--- external/gpl3/gcc/dist/libmudflap/mf-impl.h
+++ external/gpl3/gcc/dist/libmudflap/mf-impl.h
@@ -273,12 +273,12 @@ extern struct __mf_options __mf_opts;
 #ifdef LIBMUDFLAPTH
 #define VERBOSE_TRACE(...) \
   do { if (UNLIKELY (__mf_opts.verbose_trace)) {  \
-      fprintf (stderr, "mf(%u): ", (unsigned) pthread_self ()); \
+      fprintf (stderr, "mf(%ju): ", (intmax_t)(intptr_t) pthread_self ()); \
       fprintf (stderr, __VA_ARGS__); \
     } } while (0)
 #define TRACE(...) \
   do { if (UNLIKELY (__mf_opts.trace_mf_calls)) { \
-      fprintf (stderr, "mf(%u): ", (unsigned) pthread_self ()); \
+      fprintf (stderr, "mf(%ju): ", (intmax_t)(intptr_t) pthread_self ()); \
       fprintf (stderr, __VA_ARGS__); \
     } } while (0)
 #else
@@ -390,6 +390,29 @@ ret __mfwrap_ ## fname (__VA_ARGS__)
   else if (UNLIKELY (__mf_get_state () == in_malloc))   \
   {                                         \
     return CALL_REAL(fname, __VA_ARGS__);   \
+  }                                         \
+  else                                      \
+  {                                         \
+    TRACE ("%s\n", __PRETTY_FUNCTION__); \
+  }
+
+#define BEGIN_PROTECTV(fname, ...)       \
+  if (UNLIKELY (__mf_starting_p)) \
+  {                                         \
+    CALL_BACKUP(fname, __VA_ARGS__); \
+    return;				\
+  }                                         \
+  else if (UNLIKELY (__mf_get_state () == reentrant))   \
+  {                                         \
+    extern unsigned long __mf_reentrancy;   \
+    __mf_reentrancy ++; \
+    CALL_REAL(fname, __VA_ARGS__);   \
+    return;				\
+  }                                         \
+  else if (UNLIKELY (__mf_get_state () == in_malloc))   \
+  {                                         \
+    CALL_REAL(fname, __VA_ARGS__);   \
+    return;				\
   }                                         \
   else                                      \
   {                                         \
