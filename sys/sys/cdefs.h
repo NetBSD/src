f44a1ba95a67e71a84e997fb84a206cfe04e844f
pgoyette <pgoyette@NetBSD.org>
2012-04-30 20:41:33 +0000

    Fix a (obvious) typo

--- sys/sys/cdefs.h
+++ sys/sys/cdefs.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: cdefs.h,v 1.97 2012/03/18 16:30:54 christos Exp $	*/
+/*	$NetBSD: cdefs.h,v 1.98 2012/04/30 20:41:33 pgoyette Exp $	*/
 
 /*
  * Copyright (c) 1991, 1993
@@ -491,7 +491,7 @@
 #define	__link_set_foreach(pvar, set)					\
 	for (pvar = __link_set_start(set); pvar < __link_set_end(set); pvar++)
 
-#define	__link_set_entry(set, idx)	(__link_set_begin(set)[idx])
+#define	__link_set_entry(set, idx)	(__link_set_start(set)[idx])
 
 /*
  * Return the number of elements in a statically-allocated array,
