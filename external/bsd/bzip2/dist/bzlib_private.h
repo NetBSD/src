644aba31854c4f391d7bbd7c1aff2cb68fe7c8a0
wiz <wiz@NetBSD.org>
2012-05-07 00:45:47 +0000

    merge 1.0.6

--- external/bsd/bzip2/dist/bzlib_private.h
+++ external/bsd/bzip2/dist/bzlib_private.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: bzlib_private.h,v 1.2 2012/05/07 00:30:05 wiz Exp $	*/
+/*	$NetBSD: bzlib_private.h,v 1.3 2012/05/07 00:45:47 wiz Exp $	*/
 
 
 /*-------------------------------------------------------------*/
@@ -10,8 +10,8 @@
    This file is part of bzip2/libbzip2, a program and library for
    lossless, block-sorting data compression.
 
-   bzip2/libbzip2 version 1.0.5 of 10 December 2007
-   Copyright (C) 1996-2007 Julian Seward <jseward@bzip.org>
+   bzip2/libbzip2 version 1.0.6 of 6 September 2010
+   Copyright (C) 1996-2010 Julian Seward <jseward@bzip.org>
 
    Please read the WARNING, DISCLAIMER and PATENTS sections in the 
    README file.
@@ -38,7 +38,7 @@
 
 /*-- General stuff. --*/
 
-#define BZ_VERSION  "1.0.5, 10-Dec-2007"
+#define BZ_VERSION  "1.0.6, 6-Sept-2010"
 
 typedef char            Char;
 typedef unsigned char   Bool;
