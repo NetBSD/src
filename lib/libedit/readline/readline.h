07026d24e797d2bc0bf7579640150a8c23fc8f97
christos <christos@NetBSD.org>
2012-05-15 17:30:04 +0000

    Add rl_completion_word_break_hook from: http://gitweb.dragonflybsd.org/dragonfly.git/commitdiff/\ 5ec6a45fa762b8cbd0305ca06acb8780335a486a

--- lib/libedit/readline/readline.h
+++ lib/libedit/readline/readline.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: readline.h,v 1.32 2010/09/16 20:08:52 christos Exp $	*/
+/*	$NetBSD: readline.h,v 1.33 2012/05/15 17:30:04 christos Exp $	*/
 
 /*-
  * Copyright (c) 1997 The NetBSD Foundation, Inc.
@@ -109,6 +109,7 @@ extern char		*rl_basic_word_break_characters;
 extern char		*rl_completer_word_break_characters;
 extern char		*rl_completer_quote_characters;
 extern Function		*rl_completion_entry_function;
+extern char		*(*rl_completion_word_break_hook)(void);
 extern CPPFunction	*rl_attempted_completion_function;
 extern int		 rl_attempted_completion_over;
 extern int		rl_completion_type;
