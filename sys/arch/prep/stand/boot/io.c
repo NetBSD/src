666f1682aee860cd57774265616401af4af32a45
kiyohara <kiyohara@NetBSD.org>
2012-05-19 14:40:12 +0000

    Support load kernel from SCSI HDD with siop.  It copy from bebox. (EXPERIMENTAL)

--- sys/arch/prep/stand/boot/io.c
+++ sys/arch/prep/stand/boot/io.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: io.c,v 1.4 2006/06/27 23:26:13 garbled Exp $	*/
+/*	$NetBSD: io.c,v 1.5 2012/05/19 14:40:13 kiyohara Exp $	*/
 
 /*-
  * Copyright (C) 1995-1997 Gary Thomas (gdt@linuxppc.org)
@@ -36,31 +36,14 @@
 #include <sys/bswap.h>
 #include "boot.h"
 
-#define PCI_NSLOTS	8
-#define PCI_NREGS	5
-#define PCI_DEVID	0
-#define PCI_CMD		1
-#define PCI_CLASS	2
-#define PCI_MEMBASE	4
 
-
+volatile u_char *PCI_mem = (u_char *)0xc0000000;
 volatile u_char *ISA_io  = (u_char *)0x80000000;
 volatile u_char *ISA_mem = (u_char *)0xc0000000;
 volatile char *videomem = (char *)0xc00b8000; /* + vram offset */
 
-struct PCI_cinfo {
-	u_long *config_addr;
-	u_long regs[PCI_NREGS];
-} PCI_slots[PCI_NSLOTS] = {
-	{ (u_long *)0x80808000, {0xDEADBEEF,} },
-	{ (u_long *)0x80800800, {0xDEADBEEF,} },
-	{ (u_long *)0x80801000, {0xDEADBEEF,} },
-	{ (u_long *)0x80802000, {0xDEADBEEF,} },
-	{ (u_long *)0x80804000, {0xDEADBEEF,} },
-	{ (u_long *)0x80810000, {0xDEADBEEF,} },
-	{ (u_long *)0x80820000, {0xDEADBEEF,} },
-	{ (u_long *)0x80840000, {0xDEADBEEF,} },
-};
+static int dcache_line_size = 32;
+
 
 void
 outb(int port, char val)
@@ -83,67 +66,89 @@ inb(int port)
 	return (ISA_io[port]);
 }
 
+u_char
+readb(u_long addr)
+{
+
+	return PCI_mem[addr];
+}
+
+u_short
+readw(u_long addr)
+{
+
+	return le16toh(*((u_short *)&PCI_mem[addr]));
+}
+
+u_long
+readl(u_long addr)
+{
+
+	return le32toh(*((u_long *)&PCI_mem[addr]));
+}
+
+void
+writeb(u_long addr, u_char val)
+{
+
+	PCI_mem[addr] = val;
+}
+
+void
+writel(u_long addr, u_long val)
+{
+
+	*((u_long *)&PCI_mem[addr]) = htole32(val);
+}
+
+void
+_wbinv(uint32_t adr, uint32_t siz)
+{
+	uint32_t bnd;
+
+	asm volatile("eieio");
+	for (bnd = adr + siz; adr < bnd; adr += dcache_line_size)
+		asm volatile ("dcbf 0,%0" :: "r"(adr));
+	asm volatile ("sync");
+}
+
+void
+_inv(uint32_t adr, uint32_t siz)
+{
+	uint32_t bnd, off;
+
+	off = adr & (dcache_line_size - 1);
+	adr -= off;
+	siz += off;
+	asm volatile ("eieio");
+	if (off != 0) {
+		/* wbinv() leading unaligned dcache line */
+		asm volatile ("dcbf 0,%0" :: "r"(adr));
+		if (siz < dcache_line_size)
+			goto done;
+		adr += dcache_line_size;
+		siz -= dcache_line_size;
+	}
+	bnd = adr + siz;
+	off = bnd & (dcache_line_size - 1);
+	if (off != 0) {
+		/* wbinv() trailing unaligned dcache line */
+		asm volatile ("dcbf 0,%0" :: "r"(bnd)); /* it's OK */
+		if (siz < dcache_line_size)
+			goto done;
+		siz -= off;
+	}
+	for (bnd = adr + siz; adr < bnd; adr += dcache_line_size) {
+		/* inv() intermediate dcache lines if ever */
+		asm volatile ("dcbi 0,%0" :: "r"(adr));
+	}
+  done:
+	asm volatile ("sync");
+}
+
 u_long
 local_to_PCI(u_long addr)
 {
 
 	return ((addr & 0x7FFFFFFF) | 0x80000000);
-}
-
-void
-unlockVideo(int slot)
-{
-	volatile u_int8_t *ppci;
-
-	ppci = (u_int8_t *)PCI_slots[slot].config_addr;
-	ppci[4] = 0x0003;	/* enable memory and IO Access */
-#if 0
-	ppci[0x10] = 0x00000;	/* Turn off memory mapping */
-	ppci[0x11] = 0x00000;	/* mem base = 0 */
-	ppci[0x12] = 0x00000;
-	ppci[0x13] = 0x00000;
-#endif
-	__asm__ volatile("eieio");
-
-	outb(0x3d4, 0x11);
-	outb(0x3d5, 0x0e);   /* unlock CR0-CR7 */
-}
-
-int
-scan_PCI(int start)
-{
-	int slot, r;
-	struct PCI_cinfo *pslot;
-	int VGAslot = -1;
-	int highVGAslot = 0;
-
-	for (slot = start + 1; slot < PCI_NSLOTS; slot++) {
-		pslot = &PCI_slots[slot];
-		for (r = 0; r < PCI_NREGS; r++)
-			pslot->regs[r] = bswap32(pslot->config_addr[r]);
-		if (pslot->regs[PCI_DEVID] != 0xffffffff) {
-			/* we have a card */
-			if (((pslot->regs[PCI_CLASS] & 0xffffff00) == 
-				0x03000000) ||
-			    ((pslot->regs[PCI_CLASS] & 0xffffff00) ==
-				0x00010000)) {
-				/* it's a VGA card */
-				highVGAslot = slot;
-				if ((pslot->regs[PCI_CMD] & 0x03)) {
-					/* fW enabled it */
-					VGAslot = slot;
-					break;
-				}
-			}
-		}
-	}
-	return VGAslot;
-}
-
-int
-PCI_vendor(int slotnum)
-{
-	struct PCI_cinfo *pslot = &PCI_slots[slotnum];
-
-	return (pslot->regs[PCI_DEVID] & 0xffff);
 }
