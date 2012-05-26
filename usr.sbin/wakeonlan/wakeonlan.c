3478bc703e2bc828b77463c5062680cddc713489
uebayasi <uebayasi@NetBSD.org>
2012-05-26 01:58:17 +0000

    Rename wake(8) to wakeonlan(8).  Approved by core@.

--- usr.sbin/wakeonlan/wakeonlan.c
+++ usr.sbin/wakeonlan/wakeonlan.c
@@ -1,4 +1,4 @@
-/* $NetBSD: wake.c,v 1.13 2011/08/31 13:32:41 joerg Exp $ */
+/* $NetBSD: wakeonlan.c,v 1.1 2012/05/26 01:58:21 uebayasi Exp $ */
 
 /*
  * Copyright (C) 2006, 2007, 2008, 2009, 2010 Marc Balmer <marc@msys.ch>
