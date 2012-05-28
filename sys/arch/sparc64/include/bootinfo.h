a47afdb76c8b6e4f81ea28fd7652ac937d61a6c7
martin <martin@NetBSD.org>
2012-05-28 19:24:29 +0000

    Pass the "boothowto" from the bootloader to the kernel. If (an old) bootloader did not pass the flags, check explicitly if we missed RB_USERCONF and invoke userconf_prompt() in MD code, as MI code already missed it. This fixes PR 46466.

--- sys/arch/sparc64/include/bootinfo.h
+++ sys/arch/sparc64/include/bootinfo.h
@@ -1,4 +1,4 @@
-/*       $NetBSD: bootinfo.h,v 1.5 2010/04/02 18:34:16 martin Exp $        */
+/*       $NetBSD: bootinfo.h,v 1.6 2012/05/28 19:24:30 martin Exp $        */
 
 /*-
  * Copyright (c) 2005 The NetBSD Foundation, Inc.
@@ -98,6 +98,7 @@
 #define BTINFO_ITLB			103
 #define BTINFO_KERNEND			104
 #define BTINFO_BOOTDEV			105
+#define BTINFO_BOOTHOWTO		106
 
 #define LOOKUP_BOOTINFO(btp, info) \
 do { \
@@ -129,6 +130,11 @@ struct btinfo_kernend {
 struct btinfo_bootdev {
 	struct btinfo_common common;
 	char name[1];
+};
+
+struct btinfo_boothowto {
+	struct btinfo_common common;
+	int boothowto;
 };
 
 #endif /* _BOOTINFO_H_ */
