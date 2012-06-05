c7892d77c539138096c320c582ae43aa7fc76a33
martin <martin@NetBSD.org>
2012-06-05 08:23:05 +0000

    Measure kinfo_proc2::p_vm_vsize in pages, as it was always documented. This value seems to never have been used anywhere. This makes it consistent with it's cousin p_vm_msize (which is in pages as well and has several uses).

--- sys/kern/kern_proc.c
+++ sys/kern/kern_proc.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: kern_proc.c,v 1.183 2012/04/13 15:32:15 yamt Exp $	*/
+/*	$NetBSD: kern_proc.c,v 1.184 2012/06/05 08:23:05 martin Exp $	*/
 
 /*-
  * Copyright (c) 1999, 2006, 2007, 2008 The NetBSD Foundation, Inc.
@@ -62,7 +62,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: kern_proc.c,v 1.183 2012/04/13 15:32:15 yamt Exp $");
+__KERNEL_RCSID(0, "$NetBSD: kern_proc.c,v 1.184 2012/06/05 08:23:05 martin Exp $");
 
 #ifdef _KERNEL_OPT
 #include "opt_kstack.h"
@@ -2279,7 +2279,7 @@ fill_kproc2(struct proc *p, struct kinfo_proc2 *ki, bool zombie)
 		ki->p_vm_tsize = vm->vm_tsize;
 		ki->p_vm_dsize = vm->vm_dsize;
 		ki->p_vm_ssize = vm->vm_ssize;
-		ki->p_vm_vsize = vm->vm_map.size;
+		ki->p_vm_vsize = atop(vm->vm_map.size);
 		/*
 		 * Since the stack is initially mapped mostly with
 		 * PROT_NONE and grown as needed, adjust the "mapped size"
