6370b166d5c813bebd1870f5f02e99355fe0893b
bouyer <bouyer@NetBSD.org>
2012-06-05 20:01:02 +0000

    Apply patch, requested by christos in ticket #1447 	dist/bind/CHANGES				patch 	dist/bind/version				patch 	dist/bind/lib/dns/rdata.c			patch 	dist/bind/lib/dns/rdataslab.c			patch Update bind to 9.4-ESV-R4-P2 3331.  [security]      dns_rdataslab_fromrdataset could produce bad                        rdataslabs. [RT #29644]

--- dist/bind/lib/dns/rdata.c
+++ dist/bind/lib/dns/rdata.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: rdata.c,v 1.1.1.3.4.2 2011/01/23 21:47:39 bouyer Exp $	*/
+/*	$NetBSD: rdata.c,v 1.1.1.3.4.3 2012/06/05 20:01:02 bouyer Exp $	*/
 
 /*
  * Copyright (C) 2004-2006, 2008  Internet Systems Consortium, Inc. ("ISC")
@@ -338,6 +338,8 @@ dns_rdata_compare(const dns_rdata_t *rdata1, const dns_rdata_t *rdata2) {
 	REQUIRE(rdata2 != NULL);
 	REQUIRE(rdata1->data != NULL);
 	REQUIRE(rdata2->data != NULL);
+	REQUIRE(rdata1->length == 0 || rdata1->data != NULL);
+	REQUIRE(rdata2->length == 0 || rdata2->data != NULL);
 	REQUIRE(DNS_RDATA_VALIDFLAGS(rdata1));
 	REQUIRE(DNS_RDATA_VALIDFLAGS(rdata2));
 
