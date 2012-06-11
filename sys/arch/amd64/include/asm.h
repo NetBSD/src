6ff27c10cf2edab134da07cc424db376b8f4c335
chs <chs@NetBSD.org>
2012-06-11 15:18:05 +0000

    make dtrace work on amd64.

--- sys/arch/amd64/include/asm.h
+++ sys/arch/amd64/include/asm.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: asm.h,v 1.14 2010/12/20 21:11:24 joerg Exp $	*/
+/*	$NetBSD: asm.h,v 1.15 2012/06/11 15:18:05 chs Exp $	*/
 
 /*-
  * Copyright (c) 1990 The Regents of the University of California.
@@ -101,6 +101,7 @@
 
 #define	ENTRY(y)	_ENTRY(_C_LABEL(y)); _PROF_PROLOGUE
 #define	NENTRY(y)	_ENTRY(_C_LABEL(y))
+#define	ALTENTRY(x)	NENTRY(x)
 #define	ASENTRY(y)	_ENTRY(_ASM_LABEL(y)); _PROF_PROLOGUE
 #define	LABEL(y)	_LABEL(_C_LABEL(y))
 
