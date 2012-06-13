8e87ee4f89844c31737d9a0f327c93000a017892
riz <riz@NetBSD.org>
2012-06-13 19:44:11 +0000

    Pull up following revision(s) (requested by matt in ticket #340): 	sys/arch/powerpc/include/types.h: revision 1.47 	sys/arch/powerpc/include/userret.h: revision 1.24 Add __HAVE_RAS support.  Do it in userret.

--- sys/arch/powerpc/include/userret.h
+++ sys/arch/powerpc/include/userret.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: userret.h,v 1.22 2011/09/27 01:02:36 jym Exp $	*/
+/*	$NetBSD: userret.h,v 1.22.8.1 2012/06/13 19:44:11 riz Exp $	*/
 
 /*
  * Copyright (C) 1995, 1996 Wolfgang Solfrank.
@@ -35,6 +35,7 @@
 #include "opt_altivec.h"
 
 #include <sys/userret.h>
+#include <sys/ras.h>
 
 #include <powerpc/fpu.h>
 #include <powerpc/psl.h>
@@ -77,6 +78,18 @@ userret(struct lwp *l, struct trapframe *tf)
 	if (__predict_false(tf->tf_srr1 & PSL_SE)) {
 		extern void booke_sstep(struct trapframe *); /* ugly */
 		booke_sstep(tf);
+	}
+#endif
+
+#ifdef __HAVE_RAS
+	/*
+	 * Check to see if a RAS was interrupted and restart it if it was.      
+	 */
+	struct proc * const p = l->l_proc;
+	if (__predict_false(p->p_raslist != NULL)) {
+		void * const ras_pc = ras_lookup(p, (void *) tf->tf_srr0);
+		if (ras_pc != (void *) -1)
+			tf->tf_srr0 = (vaddr_t) ras_pc;
 	}
 #endif
 }
