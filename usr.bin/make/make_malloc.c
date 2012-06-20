e47157f5d2dbf87e340031a767f12a4502ec9b7f
sjg <sjg@NetBSD.org>
2012-06-20 17:46:28 +0000

    Do not include make_malloc.h or declare progname, both are already done in make.h

--- usr.bin/make/make_malloc.c
+++ usr.bin/make/make_malloc.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: make_malloc.c,v 1.9 2012/06/12 19:21:51 joerg Exp $	*/
+/*	$NetBSD: make_malloc.c,v 1.10 2012/06/20 17:46:28 sjg Exp $	*/
 
 /*-
  * Copyright (c) 2009 The NetBSD Foundation, Inc.
@@ -28,7 +28,7 @@
 
 #ifdef MAKE_NATIVE
 #include <sys/cdefs.h>
-__RCSID("$NetBSD: make_malloc.c,v 1.9 2012/06/12 19:21:51 joerg Exp $");
+__RCSID("$NetBSD: make_malloc.c,v 1.10 2012/06/20 17:46:28 sjg Exp $");
 #endif
 
 #include <stdio.h>
@@ -37,7 +37,6 @@ __RCSID("$NetBSD: make_malloc.c,v 1.9 2012/06/12 19:21:51 joerg Exp $");
 #include <errno.h>
 
 #include "make.h"
-#include "make_malloc.h"
 
 #ifndef USE_EMALLOC
 static void enomem(void) MAKE_ATTR_DEAD;
@@ -46,8 +45,6 @@ static void enomem(void) MAKE_ATTR_DEAD;
  * enomem --
  *	die when out of memory.
  */
-extern char *progname;
-
 static void
 enomem(void)
 {
