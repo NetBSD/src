33cd246ab38efedfcd02262f2097a8070a8cf23c
abs <abs@NetBSD.org>
2012-07-10 22:30:22 +0000

    Adjust MSCP attach routines. The current code worked fine on simh-vax, but unfortunately failed on at least one 'real' SCSI MSCP adaptor. In the updated code mscp_attach() still iterates over each unit on a bus, but mscp_dorsp() now detects if we have reached the last unit and does not return a bogus additional unit. It also loses a few gotos and handles noncontiguous unit numbers better.

--- sys/dev/mscp/mscp.h
+++ sys/dev/mscp/mscp.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: mscp.h,v 1.8 2005/12/11 12:22:47 christos Exp $	*/
+/*	$NetBSD: mscp.h,v 1.9 2012/07/10 22:30:23 abs Exp $	*/
 
 /*
  * Copyright (c) 1988 Regents of the University of California.
@@ -39,6 +39,8 @@
  * I WISH I KNEW WHAT MORE OF THESE WERE.  IT SURE WOULD BE NICE
  * IF DEC SOLD DOCUMENTATION FOR THEIR OWN CONTROLLERS.
  */
+
+#define MSCP_MAX_UNIT	4095
 
 /*
  * Control message opcodes
