883f7cba1d446eb4c5d5249edeae2351dad37d59
matt <matt@NetBSD.org>
2012-07-15 20:53:23 +0000

    Add stream methods

--- sys/arch/arm/omap/omap_space.c
+++ sys/arch/arm/omap/omap_space.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: omap_space.c,v 1.4 2011/07/01 20:30:21 dyoung Exp $ */
+/*	$NetBSD: omap_space.c,v 1.5 2012/07/15 20:54:15 matt Exp $ */
 
 /*
  * bus_space functions for Texas Instruments OMAP processor.
@@ -73,7 +73,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: omap_space.c,v 1.4 2011/07/01 20:30:21 dyoung Exp $");
+__KERNEL_RCSID(0, "$NetBSD: omap_space.c,v 1.5 2012/07/15 20:54:15 matt Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -163,6 +163,44 @@ struct bus_space omap_bs_tag = {
 	generic_armv4_bs_c_2,
 	bs_notimpl_bs_c_4,
 	bs_notimpl_bs_c_8,
+
+#ifdef __BUS_SPACE_HAS_STREAM_METHODS
+	/* read (single) */
+	generic_bs_r_1,
+	generic_armv4_bs_r_2,
+	generic_bs_r_4,
+	bs_notimpl_bs_r_8,
+
+	/* read multiple */
+	generic_bs_rm_1,
+	generic_armv4_bs_rm_2,
+	generic_bs_rm_4,
+	bs_notimpl_bs_rm_8,
+
+	/* read region */
+	generic_bs_rr_1,
+	generic_armv4_bs_rr_2,
+	generic_bs_rr_4,
+	bs_notimpl_bs_rr_8,
+
+	/* write (single) */
+	generic_bs_w_1,
+	generic_armv4_bs_w_2,
+	generic_bs_w_4,
+	bs_notimpl_bs_w_8,
+
+	/* write multiple */
+	generic_bs_wm_1,
+	generic_armv4_bs_wm_2,
+	generic_bs_wm_4,
+	bs_notimpl_bs_wm_8,
+
+	/* write region */
+	generic_bs_wr_1,
+	generic_armv4_bs_wr_2,
+	generic_bs_wr_4,
+	bs_notimpl_bs_wr_8,
+#endif
 };
 
 int
@@ -268,4 +306,3 @@ omap_bs_free(void *t, bus_space_handle_t bsh, bus_size_t size)
 
 	panic("%s(): not implemented\n", __func__);
 }
-
