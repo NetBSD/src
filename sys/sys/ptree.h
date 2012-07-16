a832d6165eea638775768ed5b96a5668ed180f83
riz <riz@NetBSD.org>
2012-07-16 22:10:46 +0000

    Pull up following revision(s) (requested by rmind in ticket #420): 	common/lib/libc/gen/ptree.c: revision 1.7 	common/lib/libc/gen/ptree.c: revision 1.8 	common/lib/libc/gen/ptree.c: revision 1.9 	sys/sys/ptree.h: revision 1.7 Don't bother testing 0 length keys since they can only have one possible value. Add code to protect the ptree from multiple insertions of the same node. ptree_find_filtered_node: make key argument const.

--- sys/sys/ptree.h
+++ sys/sys/ptree.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: ptree.h,v 1.4.8.1 2012/07/12 18:35:10 riz Exp $	*/
+/*	$NetBSD: ptree.h,v 1.4.8.2 2012/07/16 22:10:47 riz Exp $	*/
 
 /*-
  * Copyright (c) 2008 The NetBSD Foundation, Inc.
@@ -184,7 +184,7 @@ typedef bool (*pt_filter_t)(void *, const void *, int);
 void	ptree_init(pt_tree_t *, const pt_tree_ops_t *, void *, size_t, size_t);
 bool	ptree_insert_node(pt_tree_t *, void *);
 bool	ptree_insert_mask_node(pt_tree_t *, void *, pt_bitlen_t);
-void *	ptree_find_filtered_node(pt_tree_t *, void *, pt_filter_t, void *);
+void *	ptree_find_filtered_node(pt_tree_t *, const void *, pt_filter_t, void *);
 #define	ptree_find_node(pt,key)	\
 	ptree_find_filtered_node((pt), (key), NULL, NULL)
 void	ptree_remove_node(pt_tree_t *, void *);
