8655ce29097264648710bd5cb553309883baafcd
tsutsui <tsutsui@NetBSD.org>
2012-07-20 19:31:53 +0000

    Switch luna68k wscons framebuffer driver to using rasops(9) APIs instead of deprecated rcons(4).  This allows "options FONT_foo" in kernel config files. Mostly taken from OpenBSD/luna88k, but unnecessary MI rasops(9) stuff is omitted since omrasops.c has own raster wsdisplay_emulops functions. Tested on LUNA with 4bpp fb and LUNA-II with 1bpp fb.

--- sys/arch/luna68k/dev/omrasops.c
+++ sys/arch/luna68k/dev/omrasops.c
@@ -1,4 +1,4 @@
-/* $NetBSD: omrasops.c,v 1.12 2012/07/16 11:41:05 tsutsui Exp $ */
+/* $NetBSD: omrasops.c,v 1.13 2012/07/20 19:31:53 tsutsui Exp $ */
 
 /*-
  * Copyright (c) 2000 The NetBSD Foundation, Inc.
@@ -31,7 +31,7 @@
 
 #include <sys/cdefs.h>			/* RCS ID & Copyright macro defns */
 
-__KERNEL_RCSID(0, "$NetBSD: omrasops.c,v 1.12 2012/07/16 11:41:05 tsutsui Exp $");
+__KERNEL_RCSID(0, "$NetBSD: omrasops.c,v 1.13 2012/07/20 19:31:53 tsutsui Exp $");
 
 /*
  * Designed speficically for 'm68k bitorder';
@@ -42,22 +42,15 @@ __KERNEL_RCSID(0, "$NetBSD: omrasops.c,v 1.12 2012/07/16 11:41:05 tsutsui Exp $"
  *	- font glyphs are stored in 32bit padded.
  */
 
-#define USE_OMRONFONT
-
 #include <sys/param.h>
 #include <sys/systm.h>
 #include <sys/device.h>
 
-#include <dev/rcons/raster.h>
-#include <dev/wscons/wscons_raster.h>
-#ifndef USE_OMRONFONT
-#include <dev/wscons/wscons_rfont.h>
-#else
-#include <arch/luna68k/dev/omron_rfont.h>
-#endif
+#include <dev/wscons/wsconsio.h>
 #include <dev/wscons/wsdisplayvar.h>
+#include <dev/rasops/rasops.h>
 
-void rcons_init(struct rcons *, int, int);
+#include <arch/luna68k/dev/omrasopsvar.h>
 
 /* wscons emulator operations */
 static void	om_cursor(void *, int, int, int);
@@ -68,17 +61,6 @@ static void	om_copyrows(void *, int, int, int num);
 static void	om_erasecols(void *, int, int, int, long);
 static void	om_eraserows(void *, int, int, long);
 static int	om_allocattr(void *, int, int, int, long *);
-
-struct wsdisplay_emulops omfb_emulops = {
-	om_cursor,
-	om_mapchar,
-	om_putchar,
-	om_copycols,
-	om_erasecols,
-	om_copyrows,
-	om_eraserows,
-	om_allocattr
-};
 
 #define	ALL1BITS	(~0U)
 #define	ALL0BITS	(0U)
@@ -95,33 +77,36 @@ struct wsdisplay_emulops omfb_emulops = {
 static void
 om_putchar(void *cookie, int row, int startcol, u_int uc, long attr)
 {
-	struct rcons *rc = cookie;
-	struct raster *rap = rc->rc_sp;
+	struct rasops_info *ri = cookie;
 	uint8_t *p;
 	int scanspan, startx, height, width, align, y;
 	uint32_t lmask, rmask, glyph, inverse;
-	uint32_t *g;
+	int i;
+	uint8_t *fb;
 
-	scanspan = rap->linelongs * 4;
-	y = rc->rc_yorigin + rc->rc_font->height * row;
-	startx = rc->rc_xorigin + rc->rc_font->width * startcol;
-	height = rc->rc_font->height;
-	g = rc->rc_font->chars[uc].r->pixels;
+	scanspan = ri->ri_stride;
+	y = ri->ri_font->fontheight * row;
+	startx = ri->ri_font->fontwidth * startcol;
+	height = ri->ri_font->fontheight;
+	fb = (uint8_t *)ri->ri_font->data +
+	    (uc - ri->ri_font->firstchar) * ri->ri_fontscale;
 	inverse = (attr != 0) ? ALL1BITS : ALL0BITS;
 
-	p = (uint8_t *)rap->pixels + y * scanspan + ((startx / 32) * 4);
+	p = (uint8_t *)ri->ri_bits + y * scanspan + ((startx / 32) * 4);
 	align = startx & ALIGNMASK;
-	width = rc->rc_font->width + align;
+	width = ri->ri_font->fontwidth + align;
 	lmask = ALL1BITS >> align;
 	rmask = ALL1BITS << (-width & ALIGNMASK);
 	if (width <= BLITWIDTH) {
 		lmask &= rmask;
 		while (height > 0) {
-			glyph = *g;
+			glyph = 0;
+			for (i = ri->ri_font->stride; i != 0; i--)
+				glyph = (glyph << 8) | *fb++;
+			glyph <<= (4 - ri->ri_font->stride) * NBBY;
 			glyph = (glyph >> align) ^ inverse;
 			W(p) = (R(p) & ~lmask) | (glyph & lmask);
 			p += scanspan;
-			g += 1;
 			height--;
 		}
 	} else {
@@ -129,7 +114,10 @@ om_putchar(void *cookie, int row, int startcol, u_int uc, long attr)
 		uint32_t lhalf, rhalf;
 
 		while (height > 0) {
-			glyph = *g;
+			glyph = 0;
+			for (i = ri->ri_font->stride; i != 0; i--)
+				glyph = (glyph << 8) | *fb++;
+			glyph <<= (4 - ri->ri_font->stride) * NBBY;
 			lhalf = (glyph >> align) ^ inverse;
 			W(p) = (R(p) & ~lmask) | (lhalf & lmask);
 			p += BYTESDONE;
@@ -137,7 +125,6 @@ om_putchar(void *cookie, int row, int startcol, u_int uc, long attr)
 			W(p) = (rhalf & rmask) | (R(p) & ~rmask);
 
 			p = (q += scanspan);
-			g += 1;
 			height--;
 		}
 	}
@@ -146,20 +133,20 @@ om_putchar(void *cookie, int row, int startcol, u_int uc, long attr)
 static void
 om_erasecols(void *cookie, int row, int startcol, int ncols, long attr)
 {
-        struct rcons *rc = cookie;
-        struct raster *rap = rc->rc_sp;
-        uint8_t *p;
-        int scanspan, startx, height, width, align, w, y;
-        uint32_t lmask, rmask, fill;
+	struct rasops_info *ri = cookie;
+	uint8_t *p;
+	int scanspan, startx, height, width, align, w, y;
+	uint32_t lmask, rmask, fill;
 
-        scanspan = rap->linelongs * 4;
-        y = rc->rc_yorigin + rc->rc_font->height * row;
-        startx = rc->rc_xorigin + rc->rc_font->width * startcol;
-        height = rc->rc_font->height;
-        w = rc->rc_font->width * ncols;
+	scanspan = ri->ri_stride;;
+	fill = (attr != 0) ? ALL1BITS : ALL0BITS;
+	y = ri->ri_font->fontheight * row;
+	startx = ri->ri_font->fontwidth * startcol;
+	height = ri->ri_font->fontheight;
+	w = ri->ri_font->fontwidth * ncols;
 	fill = (attr != 0) ? ALL1BITS : ALL0BITS;
 
-	p = (uint8_t *)rap->pixels + y * scanspan + ((startx / 32) * 4);
+	p = (uint8_t *)ri->ri_bits + y * scanspan + ((startx / 32) * 4);
 	align = startx & ALIGNMASK;
 	width = w + align;
 	lmask = ALL1BITS >> align;
@@ -195,22 +182,20 @@ om_erasecols(void *cookie, int row, int startcol, int ncols, long attr)
 static void
 om_eraserows(void *cookie, int startrow, int nrows, long attr)
 {
-	struct rcons *rc = cookie;
-	struct raster *rap = rc->rc_sp;
+	struct rasops_info *ri = cookie;
 	uint8_t *p, *q;
 	int scanspan, starty, height, width, w;
 	uint32_t rmask, fill;
 
-	scanspan = rap->linelongs * 4;
-	starty = rc->rc_yorigin + rc->rc_font->height * startrow;
-	height = rc->rc_font->height * nrows;
-	w = rc->rc_font->width * rc->rc_maxcol;
-	fill = (attr == 1) ? ALL1BITS : ALL0BITS;
+	scanspan = ri->ri_stride;
+	starty = ri->ri_font->fontheight * startrow;
+	height = ri->ri_font->fontheight * nrows;
+	w = ri->ri_emuwidth;
+	fill = (attr != 0) ? ALL1BITS : ALL0BITS;
 
-	p = (uint8_t *)rap->pixels + starty * scanspan;
-	p += (rc->rc_xorigin / 32) * 4;
+	p = (uint8_t *)ri->ri_bits + starty * scanspan;
 	width = w;
-        rmask = ALL1BITS << (-width & ALIGNMASK);
+	rmask = ALL1BITS << (-width & ALIGNMASK);
 	q = p;
 	while (height > 0) {
 		W(p) = fill;				/* always aligned */
@@ -231,24 +216,22 @@ om_eraserows(void *cookie, int startrow, int nrows, long attr)
 static void
 om_copyrows(void *cookie, int srcrow, int dstrow, int nrows)
 {
-        struct rcons *rc = cookie;
-        struct raster *rap = rc->rc_sp;
-        uint8_t *p, *q;
+	struct rasops_info *ri = cookie;
+	uint8_t *p, *q;
 	int scanspan, offset, srcy, height, width, w;
-        uint32_t rmask;
-        
-	scanspan = rap->linelongs * 4;
-	height = rc->rc_font->height * nrows;
-	offset = (dstrow - srcrow) * scanspan * rc->rc_font->height;
-	srcy = rc->rc_yorigin + rc->rc_font->height * srcrow;
+	uint32_t rmask;
+
+	scanspan = ri->ri_stride;
+	height = ri->ri_font->fontheight * nrows;
+	offset = (dstrow - srcrow) * scanspan * ri->ri_font->fontheight;
+	srcy = ri->ri_font->fontheight * srcrow;
 	if (srcrow < dstrow && srcrow + nrows > dstrow) {
 		scanspan = -scanspan;
 		srcy += height;
 	}
 
-	p = (uint8_t *)rap->pixels + srcy * (rap->linelongs * 4);
-	p += (rc->rc_xorigin / 32) * 4;
-	w = rc->rc_font->width * rc->rc_maxcol;
+	p = (uint8_t *)ri->ri_bits + srcy * ri->ri_stride;
+	w = ri->ri_emuwidth;
 	width = w;
 	rmask = ALL1BITS << (-width & ALIGNMASK);
 	q = p;
@@ -272,26 +255,25 @@ om_copyrows(void *cookie, int srcrow, int dstrow, int nrows)
 static void
 om_copycols(void *cookie, int startrow, int srccol, int dstcol, int ncols)
 {
-	struct rcons *rc = cookie;
-	struct raster *rap = rc->rc_sp;
+	struct rasops_info *ri = cookie;
 	uint8_t *sp, *dp, *basep;
 	int scanspan, height, width, align, shift, w, y, srcx, dstx;
 	uint32_t lmask, rmask;
 
-	scanspan = rap->linelongs * 4;
-	y = rc->rc_yorigin + rc->rc_font->height * startrow;
-	srcx = rc->rc_xorigin + rc->rc_font->width * srccol;
-	dstx = rc->rc_xorigin + rc->rc_font->width * dstcol;
-	height = rc->rc_font->height;
-	w = rc->rc_font->width * ncols;
-	basep = (uint8_t *)rap->pixels + y * scanspan;
+	scanspan = ri->ri_stride;
+	y = ri->ri_font->fontheight * startrow;
+	srcx = ri->ri_font->fontwidth * srccol;
+	dstx = ri->ri_font->fontwidth * dstcol;
+	height = ri->ri_font->fontheight;
+	w = ri->ri_font->fontwidth * ncols;
+	basep = (uint8_t *)ri->ri_bits + y * scanspan;
 
 	align = shift = srcx & ALIGNMASK;
 	width = w + align;
 	align = dstx & ALIGNMASK;
 	lmask = ALL1BITS >> align;
 	rmask = ALL1BITS << (-(w + align) & ALIGNMASK);
-	shift = align - shift; 
+	shift = align - shift;
 	sp = basep + (srcx / 32) * 4;
 	dp = basep + (dstx / 32) * 4;
 
@@ -370,11 +352,22 @@ om_copycols(void *cookie, int startrow, int srccol, int dstcol, int ncols)
 static int
 om_mapchar(void *cookie, int c, u_int *cp)
 {
+	struct rasops_info *ri = cookie;
+	struct wsdisplay_font *wf = ri->ri_font;
 
-	if (c < 128) {
-		*cp = c;
-		return 5;
+	if (wf->encoding != WSDISPLAY_FONTENC_ISO) {
+		c = wsfont_map_unichar(wf, c);
+
+		if (c < 0)
+			goto fail;
 	}
+	if (c < wf->firstchar || c >= (wf->firstchar + wf->numchars))
+		goto fail;
+
+	*cp = c;
+	return 5;
+
+ fail:
 	*cp = ' ';
 	return 0;
 }
@@ -385,33 +378,32 @@ om_mapchar(void *cookie, int c, u_int *cp)
 static void
 om_cursor(void *cookie, int on, int row, int col)
 {
-	struct rcons *rc = cookie;
-	struct raster *rap = rc->rc_sp;
+	struct rasops_info *ri = cookie;
 	uint8_t *p;
 	int scanspan, startx, height, width, align, y;
 	uint32_t lmask, rmask, image;
 
 	if (!on) {
 		/* make sure it's on */
-		if ((rc->rc_bits & RC_CURSOR) == 0)
+		if ((ri->ri_flg & RI_CURSOR) == 0)
 			return;
 
-		row = *rc->rc_crowp;
-		col = *rc->rc_ccolp;
+		row = ri->ri_crow;
+		col = ri->ri_ccol;
 	} else {
 		/* unpaint the old copy. */
-		*rc->rc_crowp = row;
-		*rc->rc_ccolp = col;
+		ri->ri_crow = row;
+		ri->ri_ccol = col;
 	}
 
-	scanspan = rap->linelongs * 4;
-	y = rc->rc_yorigin + rc->rc_font->height * row;
-	startx = rc->rc_xorigin + rc->rc_font->width * col;
-	height = rc->rc_font->height;
+	scanspan = ri->ri_stride;
+	y = ri->ri_font->fontheight * row;
+	startx = ri->ri_font->fontwidth * col;
+	height = ri->ri_font->fontheight;
 
-	p = (uint8_t *)rap->pixels + y * scanspan + ((startx / 32) * 4);
+	p = (uint8_t *)ri->ri_bits + y * scanspan + ((startx / 32) * 4);
 	align = startx & ALIGNMASK;
-	width = rc->rc_font->width + align;
+	width = ri->ri_font->fontwidth + align;
 	lmask = ALL1BITS >> align;
 	rmask = ALL1BITS << (-width & ALIGNMASK);
 	if (width <= BLITWIDTH) {
@@ -436,7 +428,7 @@ om_cursor(void *cookie, int on, int row, int col)
 			height--;
 		}
 	}
-	rc->rc_bits ^= RC_CURSOR;
+	ri->ri_flg ^= RI_CURSOR;
 }
 
 /*
@@ -447,7 +439,7 @@ om_allocattr(void *id, int fg, int bg, int flags, long *attrp)
 {
 
 	if (flags & (WSATTR_HILIT | WSATTR_BLINK |
-		     WSATTR_UNDERLINE | WSATTR_WSCOLORS))
+	    WSATTR_UNDERLINE | WSATTR_WSCOLORS))
 		return EINVAL;
 	if (flags & WSATTR_REVERSE)
 		*attrp = 1;
@@ -456,42 +448,92 @@ om_allocattr(void *id, int fg, int bg, int flags, long *attrp)
 	return 0;
 }
 
-void
-rcons_init(struct rcons *rc, int mrow, int mcol)
+/*
+ * Init subset of rasops(9) for omrasops.
+ */
+int
+omrasops_init(struct rasops_info *ri, int wantrows, int wantcols)
 {
-	struct raster *rp = rc->rc_sp;
-	int i;
+	int wsfcookie, bpp;
 
-#ifdef USE_OMRONFONT
-	rc->rc_font = &omron20; /* 12x22 monospacing font */
-#else
-	rc->rc_font = &gallant19; /* 12x22 monospacing font */
-#endif
+	if (wantrows == 0)
+		wantrows = 34;
+	if (wantrows < 10)
+		wantrows = 10;
+	if (wantcols == 0)
+		wantcols = 80;
+	if (wantcols < 20)
+		wantcols = 20;
 
-	/* Get distance to top and bottom of font from font origin */
-	rc->rc_font_ascent = -(rc->rc_font->chars)['a'].homey;
+	/* Use default font */
+	wsfont_init();
+	wsfcookie = wsfont_find(NULL, 0, 0, 0, WSDISPLAY_FONTORDER_L2R,
+	    WSDISPLAY_FONTORDER_L2R, WSFONT_FIND_BITMAP);
+	if (wsfcookie < 0)
+		panic("%s: no font available", __func__);
+	if (wsfont_lock(wsfcookie, &ri->ri_font))
+		panic("%s: unable to lock font", __func__);
+	ri->ri_wsfcookie = wsfcookie;
 
-	i = rp->height / rc->rc_font->height;
-	rc->rc_maxrow = min(i, mrow);
+	KASSERT(ri->ri_font->fontwidth > 4 && ri->ri_font->fontwidth <= 32);
 
-	i = rp->width / rc->rc_font->width;
-	rc->rc_maxcol = min(i, mcol);
+	bpp = ri->ri_depth;
 
-	/* Center emulator screen (but align x origin to 32 bits) */
-	rc->rc_xorigin =
-	    ((rp->width - rc->rc_maxcol * rc->rc_font->width) / 2) & ~ALIGNMASK;
-	rc->rc_yorigin =
-	    (rp->height - rc->rc_maxrow * rc->rc_font->height) / 2;
-#if 0
-	/* Raster width used for row copies */
-	rc->rc_raswidth = rc->rc_maxcol * rc->rc_font->width;
-	if (rc->rc_raswidth & ALIGNMASK) {
-		/* Pad to 32 bits */
-		i = (rc->rc_raswidth + ALIGNMASK) & ~ALIGNMASK;
-		/* Make sure width isn't too wide */
-		if (rc->rc_xorigin + i <= rp->width)
-			rc->rc_raswidth = i;
-	}
-#endif
-	rc->rc_bits = 0;
+	/* Now constrain what they get */
+	ri->ri_emuwidth = ri->ri_font->fontwidth * wantcols;
+	ri->ri_emuheight = ri->ri_font->fontheight * wantrows;
+	if (ri->ri_emuwidth > ri->ri_width)
+		ri->ri_emuwidth = ri->ri_width;
+	if (ri->ri_emuheight > ri->ri_height)
+		ri->ri_emuheight = ri->ri_height;
+
+	/* Reduce width until aligned on a 32-bit boundary */
+	while ((ri->ri_emuwidth * bpp & 31) != 0)
+		ri->ri_emuwidth--;
+
+	ri->ri_cols = ri->ri_emuwidth / ri->ri_font->fontwidth;
+	ri->ri_rows = ri->ri_emuheight / ri->ri_font->fontheight;
+	ri->ri_emustride = ri->ri_emuwidth * bpp >> 3;
+	ri->ri_delta = ri->ri_stride - ri->ri_emustride;
+	ri->ri_ccol = 0;
+	ri->ri_crow = 0;
+	ri->ri_pelbytes = bpp >> 3;
+
+	ri->ri_xscale = (ri->ri_font->fontwidth * bpp) >> 3;
+	ri->ri_yscale = ri->ri_font->fontheight * ri->ri_stride;
+	ri->ri_fontscale = ri->ri_font->fontheight * ri->ri_font->stride;
+
+	/* Clear the entire display */
+	if ((ri->ri_flg & RI_CLEAR) != 0)
+		memset(ri->ri_bits, 0, ri->ri_stride * ri->ri_height);
+
+	/* Now centre our window if needs be */
+	ri->ri_origbits = ri->ri_bits;
+
+	if ((ri->ri_flg & RI_CENTER) != 0) {
+		ri->ri_bits += (((ri->ri_width * bpp >> 3) -
+		    ri->ri_emustride) >> 1) & ~3;
+		ri->ri_bits += ((ri->ri_height - ri->ri_emuheight) >> 1) *
+		    ri->ri_stride;
+		ri->ri_yorigin = (int)(ri->ri_bits - ri->ri_origbits)
+		   / ri->ri_stride;
+		ri->ri_xorigin = (((int)(ri->ri_bits - ri->ri_origbits)
+		   % ri->ri_stride) * 8 / bpp);
+	} else
+		ri->ri_xorigin = ri->ri_yorigin = 0;
+
+	/* fill our own emulops */
+	ri->ri_ops.cursor    = om_cursor;
+	ri->ri_ops.mapchar   = om_mapchar;
+	ri->ri_ops.putchar   = om_putchar;
+	ri->ri_ops.copycols  = om_copycols;
+	ri->ri_ops.erasecols = om_erasecols;
+	ri->ri_ops.copyrows  = om_copyrows;
+	ri->ri_ops.eraserows = om_eraserows;
+	ri->ri_ops.allocattr = om_allocattr;
+	ri->ri_caps = WSSCREEN_REVERSE;
+
+	ri->ri_flg |= RI_CFGDONE;
+
+	return 0;
 }
