10ab324e4c032f358f5f99ddf053d5e3e9f68ead
skrll <skrll@NetBSD.org>
2012-07-26 06:21:57 +0000

    Initial commit of support for the RaspberryPI (www.raspberrypi.org)
    This is enough for serial console via the gpio header pins and to get to
    multiuser.
    
    A huge thank you to Matt Thomas for all his help.

---
+++ sys/arch/arm/broadcom/bcm2835_intr.h
@@ -0,0 +1,55 @@
+/*	$NetBSD: bcm2835_intr.h,v 1.1 2012/07/26 06:21:57 skrll Exp $	*/
+
+/*-
+ * Copyright (c) 2012 The NetBSD Foundation, Inc.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to The NetBSD Foundation
+ * by Nick Hudson
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef _BCM2835_INTR_H_
+#define	_BCM2835_INTR_H_
+
+#define	ARM_IRQ_HANDLER	_C_LABEL(bcm2835_irq_handler)
+
+#ifndef _LOCORE
+
+void bcm2835_irq_handler(void *);
+
+#define PIC_MAXSOURCES 96
+#define PIC_MAXMAXSOURCES 128
+
+#include <arm/pic/picvar.h>
+
+static __inline void *
+bcm2835_intr_establish(int irq, int ipl, int (*func)(void *), void *arg)
+{
+
+	return intr_establish(irq, ipl, IST_LEVEL, func, arg);
+}
+
+#endif	/* _LOCORE */
+
+#endif /* _BCM2835_INTR_H_ */
