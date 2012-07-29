1313f46b774fa0dd9edc3a0b6d72904c2138cde5
matt <matt>
2012-07-29 00:08:19 +0000

    Another failed typedef fixed.

--- sys/arch/x68k/dev/mhavar.h
+++ sys/arch/x68k/dev/mhavar.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: mhavar.h,v 1.9 2008/11/29 05:59:40 isaki Exp $	*/
+/*	$NetBSD: mhavar.h,v 1.10 2012/07/29 00:08:19 matt Exp $	*/
 
 /*
  * Copyright (c) 1994 Peter Galbavy.  All rights reserved.
@@ -82,7 +82,7 @@ struct spc_tinfo {
 	u_char  period;		/* Period suggestion */
 	u_char  offset;		/* Offset suggestion */
 	u_char	width;		/* Width suggestion */
-} tinfo_t;
+};
 
 struct mha_softc {
 	device_t sc_dev;
