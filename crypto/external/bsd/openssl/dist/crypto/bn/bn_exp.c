139e3d8518314f02011f01c37ee40d1d3beb8a3f
christos <christos@NetBSD.org>
2012-07-30 17:15:45 +0000

    only use alloca if not __SSP__

--- crypto/external/bsd/openssl/dist/crypto/bn/bn_exp.c
+++ crypto/external/bsd/openssl/dist/crypto/bn/bn_exp.c
@@ -120,8 +120,10 @@
 #  define alloca _alloca
 # endif
 #elif defined(__GNUC__)
-# ifndef alloca
-#  define alloca(s) __builtin_alloca((s))
+# ifndef __SSP__
+#  ifndef alloca
+#   define alloca(s) __builtin_alloca((s))
+#  endif
 # endif
 #endif
 
