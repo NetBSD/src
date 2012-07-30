db22d317876c75ebc46a98a5ad163c9f70c654d4
martin <martin@NetBSD.org>
2012-07-30 08:24:57 +0000

    Pull up following revision(s) (requested by dholland in ticket #450): 	sys/ufs/ufs/ufs_quota.c: revision 1.110 	sys/ufs/ufs/ufs_quota.h: revision 1.21 	sys/ufs/ufs/ufs_quota.c: revision 1.109 Eliminate a common in a header file (add a missing extern) and declare it in the approriate C file. Restore accidentally lost initialization of quotatypes[]. Fixes (null) in the kernel message triggered when you go over quota, and maybe other things. Reported by Matthew Mondor.

--- sys/ufs/ufs/ufs_quota.h
+++ sys/ufs/ufs/ufs_quota.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: ufs_quota.h,v 1.20 2012/01/29 07:09:52 dholland Exp $	*/
+/*	$NetBSD: ufs_quota.h,v 1.20.2.1 2012/07/30 08:24:57 martin Exp $	*/
 
 /*
  * Copyright (c) 1982, 1986, 1990, 1993, 1995
@@ -103,7 +103,7 @@ extern kcondvar_t dqcv;
 /*
  * Quota name to error message mapping.
  */
-const char *quotatypes[MAXQUOTAS];
+extern const char *quotatypes[MAXQUOTAS];
 
 int  getinoquota(struct inode *);
 int  dqget(struct vnode *, u_long, struct ufsmount *, int, struct dquot **);
