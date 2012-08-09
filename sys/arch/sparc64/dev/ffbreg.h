b1dfce18168b49bbcc73419ad832de3059cd52c1
macallan <macallan@NetBSD.org>
2012-08-09 00:48:06 +0000

    split ffb_putchar() into a version for mono fonts and one for anti-aliased ones. While there use the blitter to draw the cursor and remove some waits that are unnecessary now that characters are drawn by hardware.

--- sys/arch/sparc64/dev/ffbreg.h
+++ sys/arch/sparc64/dev/ffbreg.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: ffbreg.h,v 1.10 2011/12/22 05:08:05 macallan Exp $	*/
+/*	$NetBSD: ffbreg.h,v 1.11 2012/08/09 00:48:06 macallan Exp $	*/
 /*	$OpenBSD: creatorreg.h,v 1.5 2002/07/29 06:21:45 jason Exp $	*/
 
 /*
@@ -284,6 +284,7 @@
 
 #define	FBC_ROP_NEW		0x83
 #define	FBC_ROP_OLD		0x85
+#define	FBC_ROP_INVERT		0x8a
 
 #define	FBC_UCSR_FIFO_MASK	0x00000fff
 #define	FBC_UCSR_FB_BUSY	0x01000000
