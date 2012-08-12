3309bf3906c65064b9b92e8042c7eda7b54ba93b
martin <martin>
2012-08-12 18:53:11 +0000

    Pull up following revision(s) (requested by riastradh in ticket #477): 	lib/libm/arch/i387/fenv.c: revision 1.4 	lib/libm/arch/x86_64/fenv.c: revision 1.2 Mask off the result of feenableexcept and fedisableexcept on x86. These shouldn't return bits outside FE_ALL_EXCEPT.

--- lib/libm/arch/i387/fenv.c
+++ lib/libm/arch/i387/fenv.c
@@ -1,4 +1,4 @@
-/* $NetBSD: fenv.c,v 1.3 2010/08/01 06:34:38 taca Exp $ */
+/* $NetBSD: fenv.c,v 1.3.8.1 2012/08/12 18:53:11 martin Exp $ */
 
 /*-
  * Copyright (c) 2004-2005 David Schultz <das@FreeBSD.ORG>
@@ -27,7 +27,7 @@
  */
 
 #include <sys/cdefs.h>
-__RCSID("$NetBSD: fenv.c,v 1.3 2010/08/01 06:34:38 taca Exp $");
+__RCSID("$NetBSD: fenv.c,v 1.3.8.1 2012/08/12 18:53:11 martin Exp $");
 
 #include <sys/param.h>
 #include <sys/sysctl.h>
@@ -472,7 +472,7 @@ feenableexcept(int mask)
 		__ldmxcsr(mxcsr);
 	}
 
-	return (~omask);
+	return (FE_ALL_EXCEPT & ~omask);
 }
 
 int
@@ -496,7 +496,7 @@ fedisableexcept(int mask)
 		__ldmxcsr(mxcsr);
 	}
 
-	return (~omask);
+	return (FE_ALL_EXCEPT & ~omask);
 }
 
 int
