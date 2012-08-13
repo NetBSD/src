5ab624a9098edccc07f634ba1b693ab26df5801f
riz <riz>
2012-08-13 17:49:51 +0000

    Pull up following revision(s) (requested by rmind in ticket #485): 	lib/libnpf/npf.c: revision 1.11 	sys/net/npf/npf_session.c: revision 1.17 	sys/modules/npf/Makefile: revision 1.10 	usr.sbin/npf/npftest/npftest.c: revision 1.4 	usr.sbin/npf/npftest/README: revision 1.1 	sys/net/npf/npf_tableset.c: revision 1.14 	usr.sbin/npf/npftest/npftest.h: revision 1.4 	lib/libnpf/npf.h: revision 1.10 	sys/net/npf/npf_ruleset.c: revision 1.14 	usr.sbin/npf/npfctl/npf_data.c: revision 1.18 	usr.sbin/npf/npftest/npftest.conf: revision 1.1 	sys/net/npf/npf_handler.c: revision 1.21 	sys/net/npf/npf_impl.h: revision 1.21 	usr.sbin/npf/npfctl/npfctl.c: revision 1.18 	usr.sbin/npf/npftest/libnpftest/npf_nat_test.c: revision 1.1 	usr.sbin/npf/npfctl/npf_build.c: revision 1.13 	usr.sbin/npf/npftest/libnpftest/npf_rule_test.c: revision 1.1 	usr.sbin/npf/npftest/npfstream.c: revision 1.3 	usr.sbin/npf/npftest/libnpftest/Makefile: revision 1.4 	usr.sbin/npf/npfctl/npfctl.h: revision 1.19 	sys/net/npf/npf_nat.c: revision 1.16 	sys/net/npf/npf_state.c: revision 1.11 	usr.sbin/npf/npftest/libnpftest/npf_test_subr.c: revision 1.3 	usr.sbin/npf/npftest/libnpftest/npf_test.h: revision 1.5 	usr.sbin/npf/npfctl/npf_parse.y: revision 1.12 - Extend npftest: add ruleset inspection testing from the config generated   by npfctl debug functionality.  Auto-create npftest interfaces for this. - NPF sessions: combine protocol and interface into a separate substructure,   share between the entries and thus fix the handling of them.  Constify. - npftest: add regression tests for NAT policies. - npf_build_nat: simplify and fix bi-NAT regression. - Bump yacc stack size for npfctl.

--- sys/net/npf/npf_state.c
+++ sys/net/npf/npf_state.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: npf_state.c,v 1.6.4.4 2012/07/25 20:45:24 jdc Exp $	*/
+/*	$NetBSD: npf_state.c,v 1.6.4.5 2012/08/13 17:49:52 riz Exp $	*/
 
 /*-
  * Copyright (c) 2010-2012 The NetBSD Foundation, Inc.
@@ -34,7 +34,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: npf_state.c,v 1.6.4.4 2012/07/25 20:45:24 jdc Exp $");
+__KERNEL_RCSID(0, "$NetBSD: npf_state.c,v 1.6.4.5 2012/08/13 17:49:52 riz Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -107,7 +107,7 @@ npf_state_init(const npf_cache_t *npc, nbuf_t *nbuf, npf_state_t *nst)
 	default:
 		ret = false;
 	}
-	NPF_TCP_STATE_SAMPLE(nst, ret);
+	NPF_STATE_SAMPLE(nst, ret);
 	return ret;
 }
 
@@ -148,7 +148,7 @@ npf_state_inspect(const npf_cache_t *npc, nbuf_t *nbuf,
 	default:
 		ret = false;
 	}
-	NPF_TCP_STATE_SAMPLE(nst, ret);
+	NPF_STATE_SAMPLE(nst, ret);
 	mutex_exit(&nst->nst_lock);
 
 	return ret;
--- usr.sbin/npf/npftest/npftest.h
+++ usr.sbin/npf/npftest/npftest.h
@@ -11,6 +11,8 @@
 #include <stdbool.h>
 
 int		rumpns_npf_test_load(const void *);
+unsigned	rumpns_npf_test_addif(const char *, unsigned, bool);
+unsigned	rumpns_npf_test_getif(const char *);
 int		rumpns_npf_test_handlepkt(const void *, size_t,
 		    unsigned, bool, int64_t *);
 
@@ -18,6 +20,9 @@ bool		rumpns_npf_nbuf_test(bool);
 bool		rumpns_npf_processor_test(bool);
 bool		rumpns_npf_table_test(bool);
 bool		rumpns_npf_state_test(bool);
+
+bool		rumpns_npf_rule_test(bool);
+bool		rumpns_npf_nat_test(bool);
 
 int		process_stream(const char *, const char *, unsigned);
 
