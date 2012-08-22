1733f9ce9a72e25ebd3f9be1bdd74409fb2e710b
jakllsch <jakllsch@NetBSD.org>
2012-08-22 13:19:47 +0000

    As we'll soon get boot-time information via the mailbox method, seperate the actual access code into a seperate file that can be used by itself.

--- sys/arch/arm/broadcom/bcm2835_mbox_subr.c
+++ sys/arch/arm/broadcom/bcm2835_mbox_subr.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: bcm2835_mbox.c,v 1.2 2012/08/22 13:19:47 jakllsch Exp $	*/
+/*	$NetBSD: bcm2835_mbox_subr.c,v 1.1 2012/08/22 13:19:47 jakllsch Exp $	*/
 
 /*-
  * Copyright (c) 2012 The NetBSD Foundation, Inc.
@@ -30,7 +30,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: bcm2835_mbox.c,v 1.2 2012/08/22 13:19:47 jakllsch Exp $");
+__KERNEL_RCSID(0, "$NetBSD: bcm2835_mbox_subr.c,v 1.1 2012/08/22 13:19:47 jakllsch Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -44,71 +44,55 @@ __KERNEL_RCSID(0, "$NetBSD: bcm2835_mbox.c,v 1.2 2012/08/22 13:19:47 jakllsch Ex
 #include <arm/broadcom/bcm2835_mboxreg.h>
 #include <arm/broadcom/bcm2835reg.h>
 
-struct bcm2835mbox_softc {
-	device_t sc_dev;
-
-	bus_space_tag_t sc_iot;
-	bus_space_handle_t sc_ioh;
-};
-
-static struct bcm2835mbox_softc *bcm2835mbox_sc;
-
-static int bcmmbox_match(device_t, cfdata_t, void *);
-static void bcmmbox_attach(device_t, device_t, void *);
-
-CFATTACH_DECL_NEW(bcmmbox, sizeof(struct bcm2835mbox_softc),
-    bcmmbox_match, bcmmbox_attach, NULL, NULL);
-
-/* ARGSUSED */
-static int
-bcmmbox_match(device_t parent, cfdata_t match, void *aux)
+void
+bcm2835_mbox_read(bus_space_tag_t iot, bus_space_handle_t ioh, uint8_t chan,
+    uint32_t *data)
 {
-	struct amba_attach_args *aaa = aux;
+	uint32_t mbox;
 
-	if (strcmp(aaa->aaa_name, "bcmmbox") != 0)
-		return 0;
+	for (;;) {
+		uint8_t rchan;
+		uint32_t rdata;
 
-	return 1;
-}
+		bus_space_barrier(iot, ioh, 0,
+		    BCM2835_MBOX_SIZE, BUS_SPACE_BARRIER_READ);
 
-static void
-bcmmbox_attach(device_t parent, device_t self, void *aux)
-{
-        struct bcm2835mbox_softc *sc = device_private(self);
- 	struct amba_attach_args *aaa = aux;
+		if ((bus_space_read_4(iot, ioh,
+		    BCM2835_MBOX0_STATUS) & BCM2835_MBOX_STATUS_EMPTY) != 0)
+			continue;
 
-	aprint_naive("\n");
-	aprint_normal(": VC mailbox\n");
+		mbox = bus_space_read_4(iot, ioh,
+		    BCM2835_MBOX0_READ);
 
-	if (bcm2835mbox_sc == NULL)
-		bcm2835mbox_sc = sc;
-	
-	sc->sc_dev = self;
-	sc->sc_iot = aaa->aaa_iot;
+		rchan = BCM2835_MBOX_CHAN(mbox);
+		rdata = BCM2835_MBOX_DATA(mbox);
 
-	if (bus_space_map(aaa->aaa_iot, aaa->aaa_addr, BCM2835_MBOX_SIZE, 0,
-	    &sc->sc_ioh)) {
-		aprint_error_dev(sc->sc_dev, "unable to map device\n");
-		return;
+		if (rchan == chan) {
+			*data = rdata;
+			return;
+		}
 	}
 }
 
 void
-bcmmbox_read(uint8_t chan, uint32_t *data)
+bcm2835_mbox_write(bus_space_tag_t iot, bus_space_handle_t ioh, uint8_t chan,
+    uint32_t data)
 {
-	struct bcm2835mbox_softc *sc = bcm2835mbox_sc;
+	uint32_t rdata;
 
-	KASSERT(sc != NULL);
+	for (;;) {
 
-	return bcm2835_mbox_read(sc->sc_iot, sc->sc_ioh, chan, data);
-}
+		bus_space_barrier(iot, ioh, 0,
+		    BCM2835_MBOX_SIZE, BUS_SPACE_BARRIER_READ);
 
-void
-bcmmbox_write(uint8_t chan, uint32_t data)
-{
-	struct bcm2835mbox_softc *sc = bcm2835mbox_sc;
+		if ((rdata = bus_space_read_4(iot, ioh,
+		    BCM2835_MBOX0_STATUS) & BCM2835_MBOX_STATUS_FULL) == 0)
+			break;
+	}
 
-	KASSERT(sc != NULL);
+	bus_space_write_4(iot, ioh,
+	    BCM2835_MBOX1_WRITE, BCM2835_MBOX_MSG(chan, data));
 
-	return bcm2835_mbox_write(sc->sc_iot, sc->sc_ioh, chan, data);
+	bus_space_barrier(iot, ioh, 0,
+	    BCM2835_MBOX_SIZE, BUS_SPACE_BARRIER_WRITE);
 }
