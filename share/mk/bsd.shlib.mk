89cae5bb89a6fe1d7c57ef5d2e8f099150ee97b4
joerg <joerg@NetBSD.org>
2012-08-23 15:45:02 +0000

    Remove SHLIBINSTALLDIR and SHLINKINSTALLLIBDIR. Add rpath entries, if SHLIBDIR differs from /usr/lib.

--- share/mk/bsd.shlib.mk
+++ share/mk/bsd.shlib.mk
@@ -1,13 +1,7 @@
-#	$NetBSD: bsd.shlib.mk,v 1.6 2007/08/29 21:34:17 hira Exp $
+#	$NetBSD: bsd.shlib.mk,v 1.7 2012/08/23 15:45:03 joerg Exp $
 
 .if !defined(_BSD_SHLIB_MK_)
 _BSD_SHLIB_MK_=1
-
-.if ${MKDYNAMICROOT} == "no"
-SHLIBINSTALLDIR?= /usr/lib
-.else
-SHLIBINSTALLDIR?= /lib
-.endif
 
 .if ${MKDYNAMICROOT} == "no" || \
     (${BINDIR:Ux} != "/bin" && ${BINDIR:Ux} != "/sbin" && \
@@ -17,17 +11,7 @@ SHLIBDIR?=	/usr/lib
 SHLIBDIR?=	/lib
 .endif
 
-.if ${USE_SHLIBDIR:Uno} != "no"
-_LIBSODIR?=	${SHLIBINSTALLDIR}
-.else
-_LIBSODIR?=	${LIBDIR}
-.endif
-
-.if ${MKDYNAMICROOT} == "no"
-SHLINKINSTALLDIR?= /usr/libexec
-.else
-SHLINKINSTALLDIR?= /libexec
-.endif
+_LIBSODIR?=	${SHLIBDIR}
 
 .if ${MKDYNAMICROOT} == "no" || \
     (${BINDIR:Ux} != "/bin" && ${BINDIR:Ux} != "/sbin" && \
