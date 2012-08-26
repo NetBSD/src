d2ec691bf1c7fb30d84accc49ecc1667968aec48
jmmv <jmmv@NetBSD.org>
2012-08-26 22:37:19 +0000

    Add a new "-P prefix" option to mkdep(1).
    This option prepends the string given in prefix to every target
    filename.  This is useful for programs that have source files in
    multiple subdirectories and a single Makefile that references all of
    them explicitly (without using the VPATH functionality because there
    can be files with the same name in each subdirectory).

--- usr.bin/mkdep/mkdep.c
+++ usr.bin/mkdep/mkdep.c
@@ -1,4 +1,4 @@
-/* $NetBSD: mkdep.c,v 1.40 2011/09/04 20:30:06 joerg Exp $ */
+/* $NetBSD: mkdep.c,v 1.41 2012/08/26 22:37:19 jmmv Exp $ */
 
 /*-
  * Copyright (c) 1999 The NetBSD Foundation, Inc.
@@ -37,7 +37,7 @@
 #if !defined(lint)
 __COPYRIGHT("@(#) Copyright (c) 1999 The NetBSD Foundation, Inc.\
  All rights reserved.");
-__RCSID("$NetBSD: mkdep.c,v 1.40 2011/09/04 20:30:06 joerg Exp $");
+__RCSID("$NetBSD: mkdep.c,v 1.41 2012/08/26 22:37:19 jmmv Exp $");
 #endif /* not lint */
 
 #include <sys/mman.h>
@@ -91,7 +91,8 @@ __dead static void
 usage(void)
 {
 	(void)fprintf(stderr,
-	    "usage: %s [-aDdopq] [-f file] [-s suffixes] -- [flags] file ...\n",
+	    "usage: %s [-aDdopq] [-f file] [-P prefix] [-s suffixes] "
+	    "-- [flags] file ...\n",
 	    getprogname());
 	exit(EXIT_FAILURE);
 }
@@ -216,6 +217,7 @@ main(int argc, char **argv)
 	int	fd;
 	size_t  slen;
 	const char *fname;
+	const char *prefix = NULL;
 	const char *suffixes = NULL, *s;
 	suff_list_t *suff_list = NULL, *sl;
 
@@ -235,7 +237,7 @@ main(int argc, char **argv)
 	opterr = 0;	/* stop getopt() bleating about errors. */
 	for (;;) {
 		ok_ind = optind;
-		ch = getopt_long(argc, argv, "aDdf:opqRs:", longopt, NULL);
+		ch = getopt_long(argc, argv, "aDdf:oP:pqRs:", longopt, NULL);
 		switch (ch) {
 		case -1:
 			ok_ind = optind;
@@ -256,6 +258,9 @@ main(int argc, char **argv)
 			continue;
 		case 'o':	/* Mark dependent files .OPTIONAL */
 			oflag = 1;
+			continue;
+		case 'P':	/* Prefix for each target filename */
+			prefix = optarg;
 			continue;
 		case 'p':	/* Program mode (x.o: -> x:) */
 			suffixes = "";
@@ -401,12 +406,18 @@ main(int argc, char **argv)
 				{
 					if (sl != suff_list)
 						write(dependfile, " ", 1);
+					if (prefix != NULL)
+						write(dependfile, prefix,
+						    strlen(prefix));
 					write(dependfile, line, suf - line);
 					write(dependfile, sl->suff, sl->len);
 				}
 				write(dependfile, colon, eol - colon);
-			} else
+			} else {
+				if (prefix != NULL)
+					write(dependfile, prefix, strlen(prefix));
 				write(dependfile, line, eol - line);
+			}
 
 			if (oflag)
 				save_for_optional(colon + 1, eol);
