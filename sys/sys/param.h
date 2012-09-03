832150f5d7d0f9a7195e70949c1ae777a25e041f
riz <riz@NetBSD.org>
2012-09-03 18:30:59 +0000

    report this as "6.0_POST_RC1", in order to make some fixes before RC2 is released, to make any PRs which show up in the meantime more clear. Not intended to set a precedent. :)

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.408.2.3 2012/08/17 23:57:22 riz Exp $	*/
+/*	$NetBSD: param.h,v 1.408.2.4 2012/09/03 18:30:59 riz Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -63,7 +63,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	600000000	/* NetBSD 6.0_RC1 */
+#define	__NetBSD_Version__	600000000	/* NetBSD 6.0_POST_RC1 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
