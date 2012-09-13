62e65f0a1fff235879b639fe89b2a6d93223d9e8
riz <riz@NetBSD.org>
2012-09-13 23:58:51 +0000

    Pull up following revision(s) (requested by 1796 in ticket #spz): 	external/bsd/bind/dist/lib/dns/tests/rdata_test.c: revision 1.1 	external/bsd/bind/dist/lib/dns/tests/testdata/master/master16.data: revision 1.1 	external/bsd/bind/dist/version: revision 1.3 	external/bsd/bind/dist/lib/dns/rdataslab.c: revision 1.6 	external/bsd/bind/dist/lib/dns/master.c: revision 1.7 	external/bsd/bind/dist/lib/dns/include/dns/rdata.h: revision 1.5 	external/bsd/bind/dist/CHANGES: revision 1.6 	external/bsd/bind/dist/lib/dns/tests/master_test.c: revision 1.2 	external/bsd/bind/dist/lib/dns/tests/testdata/master/master15.data: revision 1.1 	external/bsd/bind/dist/lib/dns/tests/Makefile.in: revision 1.2 	external/bsd/bind/dist/lib/dns/rdata.c: revision 1.5 apply fix for CVE-2012-4244 from upstream upstream changelog:           --- 9.9.1-P3 released ---    3364.  [security]      Named could die on specially crafted record.                           [RT #30416]

--- dist/bind/lib/dns/master.c
+++ dist/bind/lib/dns/master.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: master.c,v 1.2.4.4 2012/07/25 11:58:45 jdc Exp $	*/
+/*	$NetBSD: master.c,v 1.2.4.5 2012/09/13 23:58:52 riz Exp $	*/
 
 /*
  * Copyright (C) 2004-2009, 2011, 2012  Internet Systems Consortium, Inc. ("ISC")
@@ -77,7 +77,7 @@
 /*%
  * max message size - header - root - type - class - ttl - rdlen
  */
-#define MINTSIZ (65535 - 12 - 1 - 2 - 2 - 4 - 2)
+#define MINTSIZ DNS_RDATA_MAXLENGTH
 /*%
  * Size for tokens in the presentation format,
  * The largest tokens are the base64 blocks in KEY and CERT records,
