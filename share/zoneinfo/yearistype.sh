6ee5ae06ece1614cae916665d17ae08a9e490956
bouyer <bouyer>
2012-09-15 09:26:27 +0000

    Apply patch, requested by apb in ticket #1783 	doc/3RDPARTY						patch 	share/zoneinfo/africa					patch 	share/zoneinfo/antarctica				patch 	share/zoneinfo/asia					patch 	share/zoneinfo/australasia				patch 	share/zoneinfo/backward					patch 	share/zoneinfo/etcetera					patch 	share/zoneinfo/europe					patch 	share/zoneinfo/factory					patch 	share/zoneinfo/iso3166.tab				patch 	share/zoneinfo/leapseconds				patch 	share/zoneinfo/northamerica				patch 	share/zoneinfo/pacificnew				patch 	share/zoneinfo/solar87					patch 	share/zoneinfo/solar88					patch 	share/zoneinfo/solar89					patch 	share/zoneinfo/southamerica				patch 	share/zoneinfo/systemv					patch 	share/zoneinfo/yearistype.sh				patch 	share/zoneinfo/zone.tab					patch
    Update to tzdata2012e
    Major changes from tzdata2011b to tzdata2011c:
    
          africa
    	     Summer time changes for Morocco (to start late April 2012)
    
          asia
    	     Changes for 2012 for Gaza & the West Bank (Hebron) and Syria
    
          northamerica
    	     Haiti following US/Canada rules for 2012 (and we're assuming,
    	     for now anyway, for the future).
    
    Major changes from tzdata2011c to tzdata2011d:
    
          Morocco does not observe DST from Jul 20 03:00 to
          Aug 20 02:00 [2012].
    
          Infrastructure changes to accommodate how the tz
          code and data are released on IANA.
    
    Major changes from tzdata2011d to tzdata2011e:
    
        * australasia (Pacific/Fakaofo): Tokelau is UTC+13, not UTC+14.
          (Thanks to Steffen Thorsen.)
    
        * Use a single version number for both code and data.

--- share/zoneinfo/yearistype.sh
+++ share/zoneinfo/yearistype.sh
@@ -1,12 +1,10 @@
 #! /bin/sh
 #
-#	$NetBSD: yearistype.sh,v 1.6 2006/09/23 17:34:36 kleink Exp $
+#	$NetBSD: yearistype.sh,v 1.6.28.1 2012/09/15 09:26:30 bouyer Exp $
 #
 
 : 'This file is in the public domain, so clarified as of'
 : '2006-07-17 by Arthur David Olson.'
-
-: '@(#)yearistype.sh	8.2'
 
 case $#-$1 in
 	2-|2-0*|2-*[!0-9]*)
