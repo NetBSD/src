aa623ac322fb067f9bdbd354142a04373e22688e
riz <riz@NetBSD.org>
2012-09-17 19:00:25 +0000

    Pull up following revision(s) (requested by spz in ticket #557): distrib/sets/lists/base/ad.mips64eb		1.94-1.95 via patch distrib/sets/lists/base/ad.mips64el		1.93-1.94 via patch distrib/sets/lists/base/md.amd64		1.169-1.170 via patch distrib/sets/lists/base/md.sparc64		1.159-1.160 via patch distrib/sets/lists/base/shl.mi			1.629,1.630,1.632 via patch distrib/sets/lists/comp/ad.mips64eb		1.81-1.82 via patch distrib/sets/lists/comp/ad.mips64el		1.821.83 via patch distrib/sets/lists/comp/md.amd64		1.168-1.169 via patch distrib/sets/lists/comp/md.sparc64		1.150-1.151 via patch distrib/sets/lists/comp/mi			1.1769 via patch distrib/sets/lists/comp/shl.mi			1.226-1.227 via patch distrib/sets/lists/tests/mi			1.477 via patch distrib/sets/sets.subr				1.137 via patch etc/Makefile					1.397 via patch share/man/man5/mk.conf.5			1.60 via patch share/mk/bsd.README				1.297 via patch share/mk/bsd.own.mk				1.700 via patch tests/crypto/libcrypto/Makefile			patch tests/crypto/libcrypto/t_ciphers.sh		1.4 via patch tests/crypto/libcrypto/t_hashes.sh		1.2 via patch tests/crypto/libcrypto/idea/Makefile		patch tests/crypto/libcrypto/mdc2/Makefile		patch usr.sbin/racoon/Makefile			1.25 via patch
    	Clean up MKCRYPTO_IDEA and MKCRYPTO_MDC2 removal, so that
    	a tree with MKCRYPTO_RC5=yes actually builds.
    	[spz, ticket #557]
    
    The "iso" directory never made it into this file, but now
    that "iso" is "images", document it here for the edification of
    folks browsing the download directories.
    add crypto shared libraries
    mark idea and mdc2 libraries obsolete.
    remove mdc2 and idea handling
    MKCRYPTO_{IDEA,MDC2} is no more
    MKCRYPTO_IDEA is no more
    no more CRYPTO_{IDEA,MDC2}
    more MKCRYPTO_IDEA and MKCRYPTO_MDC2 removal (resp, adjustment to IDEA and
    MBC2 now always being included)
    MKCRYPTO_RC5 now builds shared libs, add the missing files to sets

--- tests/crypto/libcrypto/t_ciphers.sh
+++ tests/crypto/libcrypto/t_ciphers.sh
@@ -1,4 +1,4 @@
-# $NetBSD: t_ciphers.sh,v 1.3 2011/06/09 05:25:21 spz Exp $
+# $NetBSD: t_ciphers.sh,v 1.3.4.1 2012/09/17 19:00:26 riz Exp $
 #
 # Copyright (c) 2008, 2009, 2010 The NetBSD Foundation, Inc.
 # All rights reserved.
@@ -93,9 +93,6 @@ idea_head()
 }
 idea_body()
 {
-	[ -x "$(atf_get_srcdir)/h_ideatest" ] \
-	    || atf_skip "IDEA support not available; system built" \
-	                "with MKCRYPTO_IDEA=no"
 	atf_check -o ignore -e ignore "$(atf_get_srcdir)/h_ideatest"
 }
 
