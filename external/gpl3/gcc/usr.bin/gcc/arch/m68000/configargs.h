2f5f88a3d267ef54d3f70d58a921395cd90c2d26
mrg <mrg>
2012-09-18 19:45:46 +0000

    regen mknative files for GCC 4.5.4.
    XXX: earm didn't build.

--- external/gpl3/gcc/usr.bin/gcc/arch/m68000/configargs.h
+++ external/gpl3/gcc/usr.bin/gcc/arch/m68000/configargs.h
@@ -3,7 +3,7 @@
 /* Generated from: NetBSD: mknative.common,v 1.9 2007/02/05 18:26:01 apb Exp  */
 
 /* Generated automatically. */
-static const char configuration_arguments[] = "/usr/src/tools/gcc/../../external/gpl3/gcc/dist/configure --target=m68010--netbsdelf --enable-long-long --enable-threads --with-bugurl=http://www.NetBSD.org/Misc/send-pr.html --with-pkgversion='NetBSD nb2 20111202' --with-system-zlib --enable-__cxa_atexit --with-mpc=/var/obj/mknative/sun2/usr/src/destdir.sun2/usr --with-mpfr=/var/obj/mknative/sun2/usr/src/destdir.sun2/usr --with-gmp=/var/obj/mknative/sun2/usr/src/destdir.sun2/usr --enable-tls --disable-multilib --disable-symvers --disable-libstdcxx-pch --build=x86_64-unknown-netbsd5.99.56 --host=m68010--netbsdelf";
+static const char configuration_arguments[] = "/usr/src/tools/gcc/../../external/gpl3/gcc/dist/configure --target=m68010--netbsdelf --enable-long-long --enable-threads --with-bugurl=http://www.NetBSD.org/Misc/send-pr.html --with-pkgversion='NetBSD nb1 20120916' --with-system-zlib --enable-__cxa_atexit --with-mpc=/var/obj/mknative/sun2/usr/src/destdir.sun2/usr --with-mpfr=/var/obj/mknative/sun2/usr/src/destdir.sun2/usr --with-gmp=/var/obj/mknative/sun2/usr/src/destdir.sun2/usr --enable-tls --disable-multilib --disable-symvers --disable-libstdcxx-pch --build=x86_64-unknown-netbsd5.99.56 --host=m68010--netbsdelf";
 static const char thread_model[] = "posix";
 
 static const struct {
