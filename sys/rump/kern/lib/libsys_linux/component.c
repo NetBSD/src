4a0516d44f74cb0f8fad003efa7fec07647e2b65
pooka <pooka@NetBSD.org>
2012-09-20 17:50:42 +0000

    sort stubs, remove autonamespacing and add some signal-related ones I missed yesterday

--- sys/rump/kern/lib/libsys_linux/component.c
+++ sys/rump/kern/lib/libsys_linux/component.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: component.c,v 1.1 2012/09/19 21:45:40 pooka Exp $	*/
+/*	$NetBSD: component.c,v 1.2 2012/09/20 17:50:42 pooka Exp $	*/
 
 #include <sys/param.h>
 #include <sys/proc.h>
@@ -21,24 +21,37 @@ struct emul emul_rump_sys_linux = {
  */
 void rumplinux__stub(void);
 void rumplinux__stub(void) {panic("unavailable");}
-__weak_alias(rumpns_linux_machdepioctl,rumplinux__stub);
-__weak_alias(rumpns_nanosleep1,rumplinux__stub);
-__weak_alias(rumpns_vm_map_unlock,rumplinux__stub);
-__weak_alias(rumpns_compat_50_sys_settimeofday,rumplinux__stub);
-__weak_alias(rumpns_oss_ioctl_mixer,rumplinux__stub);
-__weak_alias(rumpns_linux_ioctl_sg,rumplinux__stub);
-__weak_alias(rumpns_oss_ioctl_sequencer,rumplinux__stub);
-__weak_alias(rumpns_uvm_mremap,rumplinux__stub);
-__weak_alias(rumpns_sysent,rumplinux__stub);
-__weak_alias(rumpns_sys_swapctl,rumplinux__stub);
-__weak_alias(rumpns_vm_map_lock,rumplinux__stub);
-__weak_alias(rumpns_compat_50_sys_gettimeofday,rumplinux__stub);
-__weak_alias(rumpns_rusage_to_rusage50,rumplinux__stub);
-__weak_alias(rumpns_sys_obreak,rumplinux__stub);
-__weak_alias(rumpns_do_sys_wait,rumplinux__stub);
-__weak_alias(rumpns_sys_mmap,rumplinux__stub);
-__weak_alias(rumpns_oss_ioctl_audio,rumplinux__stub);
-__weak_alias(rumpns_clock_gettime1,rumplinux__stub);
-__weak_alias(rumpns_uvm_map_lookup_entry,rumplinux__stub);
-__weak_alias(rumpns_clock_settime1,rumplinux__stub);
-__weak_alias(rumpns_clock_getres1,rumplinux__stub);
+
+/* timing */
+__weak_alias(clock_gettime1,rumplinux__stub);
+__weak_alias(clock_settime1,rumplinux__stub);
+__weak_alias(clock_getres1,rumplinux__stub);
+__weak_alias(compat_50_sys_gettimeofday,rumplinux__stub);
+__weak_alias(compat_50_sys_settimeofday,rumplinux__stub);
+__weak_alias(nanosleep1,rumplinux__stub);
+
+/* vm-related */
+__weak_alias(sys_mmap,rumplinux__stub);
+__weak_alias(vm_map_unlock,rumplinux__stub);
+__weak_alias(uvm_map_lookup_entry,rumplinux__stub);
+__weak_alias(sys_obreak,rumplinux__stub);
+__weak_alias(sys_swapctl,rumplinux__stub);
+__weak_alias(vm_map_lock,rumplinux__stub);
+__weak_alias(uvm_mremap,rumplinux__stub);
+
+/* signal.c */
+__weak_alias(sigaction1,rumplinux__stub);
+__weak_alias(kpsignal2,rumplinux__stub);
+__weak_alias(sys_kill,rumplinux__stub);
+__weak_alias(sigsuspend1,rumplinux__stub);
+__weak_alias(sigtimedwait1,rumplinux__stub);
+__weak_alias(lwp_find,rumplinux__stub);
+
+/* misc */
+__weak_alias(linux_machdepioctl,rumplinux__stub);
+__weak_alias(linux_ioctl_sg,rumplinux__stub);
+__weak_alias(oss_ioctl_mixer,rumplinux__stub);
+__weak_alias(oss_ioctl_sequencer,rumplinux__stub);
+__weak_alias(oss_ioctl_audio,rumplinux__stub);
+__weak_alias(rusage_to_rusage50,rumplinux__stub);
+__weak_alias(do_sys_wait,rumplinux__stub);
