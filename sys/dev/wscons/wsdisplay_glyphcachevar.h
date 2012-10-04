9c1b7d2132b2e9925541a7a4afb783e57b582e8f
macallan <macallan@NetBSD.org>
2012-10-04 10:26:32 +0000

    allow caching of glyphs with attributes other than the default while there, also add support for underlined characters

--- sys/dev/wscons/wsdisplay_glyphcachevar.h
+++ sys/dev/wscons/wsdisplay_glyphcachevar.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: wsdisplay_glyphcachevar.h,v 1.3 2012/04/22 03:47:53 uebayasi Exp $	*/
+/*	$NetBSD: wsdisplay_glyphcachevar.h,v 1.4 2012/10/04 10:26:32 macallan Exp $	*/
 
 /*
  * Copyright (c) 2012 Michael Lorenz
@@ -30,22 +30,36 @@
 #ifndef WSDISPLAY_GLYPHCACHEVAR_H
 #define WSDISPLAY_GLYPHCACHEVAR_H
 
+#include <sys/time.h>
+
+typedef struct _gc_bucket {
+	int gb_firstcell;
+	int gb_numcells;
+	volatile unsigned int gb_usedcells;
+	int gb_index;		/* -1 for unused buckets */
+	uint32_t gb_map[223];	/* we only care about char codes 0x21 and up */
+	time_t gb_lastread;
+} gc_bucket;
+
 typedef struct _glyphcache {
-	/* mapping char codes to cache cells */
-	volatile unsigned int gc_usedcells;
-	int gc_numcells;
-	uint32_t gc_map[256];
 	/* geometry */
+	int gc_numcells;
 	int gc_cellwidth;
 	int gc_cellheight;
 	int gc_cellsperline;
 	int gc_firstline;	/* first line in vram to use for glyphs */
-	long gc_attr;
+	/* buckets */
+	int gc_numbuckets;
+	gc_bucket *gc_buckets;	/* we allocate as many as we can get into vram */
+	gc_bucket *gc_next;	/* bucket the next glyph goes into */
+	long gc_underline;	/* draw an underline in glyphcache_add() */
+	int gc_attrmap[256];	/* mapping a colour attribute to a bucket */
 	/*
 	 * method to copy glyphs within vram,
 	 * to be initialized before calling glyphcache_init()
 	 */
 	void (*gc_bitblt)(void *, int, int, int, int, int, int, int);
+	void (*gc_rectfill)(void *, int, int, int, int, long);
 	void *gc_blitcookie;
 	int gc_rop;
 } glyphcache;
@@ -61,5 +75,11 @@ int glyphcache_try(glyphcache *, int, int, int, long); /* char code, x, y, attr 
 #define	GC_OK	0 /* glyph was in cache and has been drawn */
 #define GC_ADD	1 /* glyph is not in cache but can be added */
 #define GC_NOPE 2 /* glyph is not in cache and can't be added */
+/*
+ * draw an underline in the given attribute
+ * must be called by the driver if glyphcache_try() returns GC_NOPE and for
+ * blanks
+ */
+void glyphcache_underline(glyphcache *, int, int, long);
 
 #endif /* WSDISPLAY_GLYPHCACHEVAR_H */
