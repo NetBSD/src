d3cfc2e2106000f1a7c687fbfd91659befd449b3
christos <christos@NetBSD.org>
2012-10-05 00:59:35 +0000

    account for differences in digest names

--- usr.sbin/mtree/misc.c
+++ usr.sbin/mtree/misc.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: misc.c,v 1.30 2009/01/18 12:09:38 lukem Exp $	*/
+/*	$NetBSD: misc.c,v 1.31 2012/10/05 00:59:35 christos Exp $	*/
 
 /*-
  * Copyright (c) 1991, 1993
@@ -37,7 +37,7 @@
 
 #include <sys/cdefs.h>
 #if defined(__RCSID) && !defined(lint)
-__RCSID("$NetBSD: misc.c,v 1.30 2009/01/18 12:09:38 lukem Exp $");
+__RCSID("$NetBSD: misc.c,v 1.31 2012/10/05 00:59:35 christos Exp $");
 #endif /* not lint */
 
 #include <sys/types.h>
@@ -72,6 +72,8 @@ static KEY keylist[] = {
 	{"mode",	F_MODE,		NEEDVALUE},
 	{"nlink",	F_NLINK,	NEEDVALUE},
 	{"optional",	F_OPT,		0},
+	{"ripemd160digest", F_RMD160,	NEEDVALUE},
+	{"rmd160digest",F_RMD160,	NEEDVALUE},
 	{"rmd160",	F_RMD160,	NEEDVALUE},
 	{"rmd160digest",F_RMD160,	NEEDVALUE},
 	{"sha1",	F_SHA1,		NEEDVALUE},
