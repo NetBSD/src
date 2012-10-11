4dec72e27e173edace0dc61a7c8a1a7941bfa9cc
christos <christos>
2012-10-11 17:11:16 +0000

    make libhack's syslog.c produce exactly the same symbols as libc's syslog.c, so that in the future we can keep them synced. Avoid strong_alias since it does not play well with symbol renaming.

--- distrib/utils/libhack/syslog.c
+++ distrib/utils/libhack/syslog.c
@@ -1,9 +1,21 @@
+#include "namespace.h"
 #include <sys/types.h>
 #include <sys/syslog.h>
 #include <stdio.h>
 #include <string.h>
 #include <errno.h>
 #include <stdarg.h>
+#include "extern.h"
+
+#ifdef __weak_alias
+__weak_alias(closelog,_closelog)
+__weak_alias(openlog,_openlog)
+__weak_alias(setlogmask,_setlogmask)
+__weak_alias(syslog,_syslog)
+__weak_alias(vsyslog,_vsyslog)
+__weak_alias(syslogp,_syslogp)
+__weak_alias(vsyslogp,_vsyslogp)
+#endif
 
 void
 openlog(const char *path, int opt, int fac)
@@ -21,7 +33,6 @@ setlogmask(int mask)
 	return 0xff;
 }
 
-__strong_alias(_syslog, syslog)
 void
 syslog(int fac, const char *fmt, ...)
 {
@@ -31,7 +42,6 @@ syslog(int fac, const char *fmt, ...)
 	va_end(ap);
 }
 
-__strong_alias(_vsyslog, vsyslog)
 void
 vsyslog(int fac, const char *fmt, va_list ap)
 {
@@ -43,8 +53,6 @@ vsyslog(int fac, const char *fmt, va_list ap)
 	fflush(stderr);
 }
 
-void syslog_ss(int, struct syslog_data *, const char *, ...);
-__strong_alias(_syslog_ss, syslog_ss)
 void
 syslog_ss(int priority, struct syslog_data *data, const char *fmt, ...)
 {
@@ -54,15 +62,12 @@ syslog_ss(int priority, struct syslog_data *data, const char *fmt, ...)
 	va_end(ap);
 }
 
-void vsyslog_ss(int, struct syslog_data *, const char *, va_list);
-__strong_alias(_vsyslog_ss, vsyslog_ss)
 void
 vsyslog_ss(int priority, struct syslog_data *data, const char *fmt, va_list ap)
 {
 	vsyslog(priority, fmt, ap);
 }
 
-__strong_alias(_syslog_r, syslog_r)
 void
 syslog_r(int priority, struct syslog_data *data, const char *fmt, ...)
 {
@@ -72,28 +77,75 @@ syslog_r(int priority, struct syslog_data *data, const char *fmt, ...)
 	va_end(ap);
 }
 
-__strong_alias(_vsyslog_r, vsyslog_r)
 void
 vsyslog_r(int priority, struct syslog_data *data, const char *fmt, va_list ap)
 {
 	vsyslog(priority, fmt, ap);
 }
 
-__strong_alias(_closelog_r, closelog_r)
 void
 closelog_r(struct syslog_data *data)
 {
 }
 
-__strong_alias(_setlogmask_r, setlogmask_r)
 int
 setlogmask_r(int maskpri, struct syslog_data *data)
 {
 	return 0xff;
 }
 
-__strong_alias(_openlog_r, openlog_r)
 void
 openlog_r(const char *id, int logopt, int facility, struct syslog_data *data)
 {
+}
+
+void
+syslogp_r(int priority, struct syslog_data *data, const char *msgid,
+    const char *sdfmt, const char *fmt, ...)
+{
+	va_list ap;
+	va_start(ap, fmt);
+	vsyslog(priority, fmt, ap);
+	va_end(ap);
+}
+
+void
+vsyslogp_r(int priority, struct syslog_data *data, const char *msgid,
+    const char *sdfmt, const char *fmt, va_list ap)
+{
+	vsyslog(priority, fmt, ap);
+}
+
+void
+syslogp_ss(int priority, struct syslog_data *data, const char *msgid,
+    const char *sdfmt, const char *fmt, ...)
+{
+	va_list ap;
+	va_start(ap, fmt);
+	vsyslog(priority, fmt, ap);
+	va_end(ap);
+}
+
+void
+vsyslogp_ss(int priority, struct syslog_data *data, const char *msgid,
+    const char *sdfmt, const char *fmt, va_list ap)
+{
+	vsyslog(priority, fmt, ap);
+}
+
+void
+syslogp(int priority, const char *msgid, const char *sdfmt, const char *fmt,
+    ...)
+{
+	va_list ap;
+	va_start(ap, fmt);
+	vsyslog(priority, fmt, ap);
+	va_end(ap);
+}
+
+void
+vsyslogp(int priority, const char *msgid, const char *sdfmt, const char *fmt,
+    va_list ap)
+{
+	vsyslog(priority, fmt, ap);
 }
