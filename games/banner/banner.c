d225978d487fba796d603dc996f9b2ec3d4a63e8
dholland <dholland@NetBSD.org>
2012-10-13 19:44:36 +0000

    Move scratch variables of main to main, instead of having them be file-static.

--- games/banner/banner.c
+++ games/banner/banner.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: banner.c,v 1.20 2012/02/26 21:06:59 wiz Exp $	*/
+/*	$NetBSD: banner.c,v 1.21 2012/10/13 19:44:36 dholland Exp $	*/
 
 /*
  * Copyright (c) 1980, 1993, 1994
@@ -39,7 +39,7 @@ __COPYRIGHT("@(#) Copyright (c) 1980, 1993, 1994\
 #if 0
 static char sccsid[] = "@(#)banner.c	8.4 (Berkeley) 4/29/95";
 #else
-__RCSID("$NetBSD: banner.c,v 1.20 2012/02/26 21:06:59 wiz Exp $");
+__RCSID("$NetBSD: banner.c,v 1.21 2012/10/13 19:44:36 dholland Exp $");
 #endif
 #endif /* not lint */
 
@@ -1022,7 +1022,7 @@ static const unsigned char data_table[NBYTES] = {
 static char line[DWIDTH];
 static char message[MAXMSG];
 static char print[DWIDTH];
-static int debug, i, j, linen, max, nchars, pc, term, trace, x, y;
+static int debug, linen, max, nchars, pc, term, trace;
 static int width = DWIDTH;	/* -w option: scrunch letters to 80 columns */
 
 __dead static void
@@ -1035,6 +1035,9 @@ int
 main(int argc, char *argv[])
 { 
 	int ch;
+	int i, j, x, y;
+
+	x = y = 0;
 
 	while ((ch = getopt(argc, argv, "w:td")) != -1)
 		switch (ch) {
