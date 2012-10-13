b6f3a43315de0a43f3bbb6b7f8da24732f456f39
tsutsui <tsutsui@NetBSD.org>
2012-10-13 06:25:20 +0000

    struct device * -> device_t, use device_xname()  (from chs@)

--- sys/arch/newsmips/dev/ms_hb.c
+++ sys/arch/newsmips/dev/ms_hb.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: ms_hb.c,v 1.13 2008/04/09 15:40:30 tsutsui Exp $	*/
+/*	$NetBSD: ms_hb.c,v 1.14 2012/10/13 06:25:20 tsutsui Exp $	*/
 
 /*-
  * Copyright (c) 2000 Tsubai Masanari.  All rights reserved.
@@ -27,7 +27,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: ms_hb.c,v 1.13 2008/04/09 15:40:30 tsutsui Exp $");
+__KERNEL_RCSID(0, "$NetBSD: ms_hb.c,v 1.14 2012/10/13 06:25:20 tsutsui Exp $");
 
 #include <sys/param.h>
 #include <sys/device.h>
@@ -50,7 +50,7 @@ struct msreg {
 struct ms_hb_softc {
 	device_t sc_dev;
 	struct msreg *sc_reg;
-	struct device *sc_wsmousedev;
+	device_t sc_wsmousedev;
 	int sc_ndata;
 	uint8_t sc_buf[3];
 };
