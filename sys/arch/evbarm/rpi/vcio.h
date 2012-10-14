697a2ce91b5370c396d5f21feaa1cb14ffa8dfca
skrll <skrll@NetBSD.org>
2012-10-14 16:16:52 +0000

    Add the Raspberry PI firmware VC mailbox channel numbers.
    Add some VC property tag definitions and use them to get ARM/VC memory
    split.
    
    Grab a few others things in the process, but don't do anything other than
    display them when VERBOSE_INIT_ARM is defined.

---
+++ sys/arch/evbarm/rpi/vcio.h
@@ -0,0 +1,45 @@
+/*	$NetBSD: vcio.h,v 1.1 2012/10/14 16:16:52 skrll Exp $	*/
+
+/*-
+ * Copyright (c) 2012 The NetBSD Foundation, Inc.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to The NetBSD Foundation
+ * by Nick Hudson
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef	_EVBARM_RPI_VCIO_H_
+#define	_EVBARM_RPI_VCIO_H_
+
+#define	BCMMBOX_CHANPM		0
+#define	BCMMBOX_CHANFB		1	/* will be depreciated */
+#define	BCMMBOX_CHANVUART	2
+#define	BCMMBOX_CHANVCHIQ	3
+#define	BCMMBOX_CHANLEDS	4
+#define	BCMMBOX_CHANBUTTONS	5
+#define	BCMMBOX_CHANTOUCHSCR	6
+#define	BCMMBOX_CHANARM2VC	8
+#define	BCMMBOX_CHANVC2ARM	9
+
+#endif /* _EVBARM_RPI_VCIO_H_ */
