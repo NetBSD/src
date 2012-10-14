a38595c0b749abbcb38cb47966666a186d8ed18a
tsutsui <tsutsui@NetBSD.org>
2012-10-14 18:38:32 +0000

    Add floppy format support.  Mostly taken from sys/dev/isa/fd.c.
    Tested both fdNa (1232KB, 1024bytes/sector, 8sectors/track) and
    fdNc (1200KB, 512bytes/sector, 15sectors/track) format on X68030
    using fdformat(1).
    
    Finally we can prepare NetBSD/x68k install floppies without Human68k
    (except actual initial bootstrap).

--- sys/arch/x68k/dev/fdreg.h
+++ sys/arch/x68k/dev/fdreg.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: fdreg.h,v 1.5 2012/05/15 12:17:33 tsutsui Exp $	*/
+/*	$NetBSD: fdreg.h,v 1.6 2012/10/14 18:38:32 tsutsui Exp $	*/
 
 /*-
  * Copyright (c) 1991 The Regents of the University of California.
@@ -60,3 +60,52 @@
 #define FDC_INTR 96		/* interrupt vector */
 #define FDC_DMA 0		/* DMA ch# */
 #define FDC_DMAINTR 100		/* DMA interrupt vector */
+
+/*
+ * fdformat parameters
+ */
+
+#define FD_MAX_NSEC	18	/* highest known number of spt */
+
+struct ne7_fd_formb {
+	int cyl, head;
+	int transfer_rate;	/* fdreg.h: FDC_???KBPS */
+
+	union {
+		struct fd_form_data {
+			/*
+			 * DO NOT CHANGE THE LAYOUT OF THIS STRUCTS
+			 * it is hardware-dependent since it exactly
+			 * matches the byte sequence to write to FDC
+			 * during its `format track' operation
+			 */
+			uint8_t secshift; /* 0 -> 128, ...; usually 2 -> 512 */
+			uint8_t nsecs;    /* must be <= FD_MAX_NSEC */
+			uint8_t gaplen;   /* GAP 3 length; usually 84 */
+			uint8_t fillbyte; /* usually 0xf6 */
+			struct fd_idfield_data {
+				/*
+				 * data to write into id fields;
+				 * for obscure formats, they mustn't match
+				 * the real values (but mostly do)
+				 */
+				uint8_t cylno;		/* 0 thru 79 (or 39) */
+				uint8_t headno;		/* 0, or 1 */
+				uint8_t secno;		/* starting at 1! */
+				uint8_t secsize;	/* usually 2 */
+			} idfields[FD_MAX_NSEC]; /* 0 <= idx < nsecs used */
+		} structured;
+		uint8_t raw[1];	/* to have continuous indexed access */
+	} format_info;
+};
+
+/* make life easier */
+# define fd_formb_secshift   format_info.structured.secshift
+# define fd_formb_nsecs      format_info.structured.nsecs
+# define fd_formb_gaplen     format_info.structured.gaplen
+# define fd_formb_fillbyte   format_info.structured.fillbyte
+/* these data must be filled in for(i = 0; i < fd_formb_nsecs; i++) */
+# define fd_formb_cylno(i)   format_info.structured.idfields[i].cylno
+# define fd_formb_headno(i)  format_info.structured.idfields[i].headno
+# define fd_formb_secno(i)   format_info.structured.idfields[i].secno
+# define fd_formb_secsize(i) format_info.structured.idfields[i].secsize
