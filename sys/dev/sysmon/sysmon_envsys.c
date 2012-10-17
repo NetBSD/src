157d13ef7edc4cec595d080c7374bdd75fd41674
riz <riz>
2012-10-17 21:06:55 +0000

    Pull up following revision(s) (requested by pgoyette in ticket #544): 	sys/dev/sysmon/sysmon_envsysvar.h: revision 1.42 	sys/dev/sysmon/sysmon_envsys_events.c: revision 1.99 	sys/dev/sysmon/sysmon_envsys.c: revision 1.119 When unregistering a sensor device, make sure we unregister and delete all the associated events.

--- sys/dev/sysmon/sysmon_envsys.c
+++ sys/dev/sysmon/sysmon_envsys.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: sysmon_envsys.c,v 1.117 2011/08/01 12:28:53 mbalmer Exp $	*/
+/*	$NetBSD: sysmon_envsys.c,v 1.117.8.1 2012/10/17 21:06:56 riz Exp $	*/
 
 /*-
  * Copyright (c) 2007, 2008 Juan Romero Pardines.
@@ -64,7 +64,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: sysmon_envsys.c,v 1.117 2011/08/01 12:28:53 mbalmer Exp $");
+__KERNEL_RCSID(0, "$NetBSD: sysmon_envsys.c,v 1.117.8.1 2012/10/17 21:06:56 riz Exp $");
 
 #include <sys/param.h>
 #include <sys/types.h>
@@ -602,6 +602,7 @@ sysmon_envsys_sensor_detach(struct sysmon_envsys *sme, envsys_data_t *edata)
 	/*
 	 * remove it and decrement the sensors count.
 	 */
+	sme_event_unregister_sensor(sme, edata);
 	TAILQ_REMOVE(&sme->sme_sensors_list, edata, sensors_head);
 	sme->sme_nsensors--;
 	sysmon_envsys_release(sme, true);
