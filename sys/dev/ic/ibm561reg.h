157b91c507e6f8c3531f5736d3f197645d01eab6
macallan <macallan@NetBSD.org>
2012-10-20 13:29:53 +0000

    fix typo

--- sys/dev/ic/ibm561reg.h
+++ sys/dev/ic/ibm561reg.h
@@ -1,4 +1,4 @@
-/* $NetBSD: ibm561reg.h,v 1.5 2008/04/28 20:23:50 martin Exp $ */
+/* $NetBSD: ibm561reg.h,v 1.6 2012/10/20 13:29:53 macallan Exp $ */
 
 /*-
  * Copyright (c) 2001 The NetBSD Foundation, Inc.
@@ -155,7 +155,7 @@
 #define IBM561_CURSOR_LUT		0x0a10
 	/*
 	 * four blocks of 4, transparent, 1, 2, 3 each 
-	 * cursor orimary, cursor blink, xhair primary, xhair blink 
+	 * cursor primary, cursor blink, xhair primary, xhair blink 
 	 */
 
 #define IBM561_CURSOR_BITMAP		0x2000
