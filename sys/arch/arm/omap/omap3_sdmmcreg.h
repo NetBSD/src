bd48ec44c5a70421607d6231ca84c2c96e99bf96
kiyohara <kiyohara@NetBSD.org>
2012-10-29 13:30:25 +0000

    Support omap3 SDHC driver.  tested on OVERO only.

--- sys/arch/arm/omap/omap3_sdmmcreg.h
+++ sys/arch/arm/omap/omap3_sdmmcreg.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: omap3_sdmmcreg.h,v 1.1 2012/07/12 03:08:26 matt Exp $	*/
+/*	$NetBSD: omap3_sdmmcreg.h,v 1.2 2012/10/29 13:30:25 kiyohara Exp $	*/
 /*-
  * Copyright (c) 2012 The NetBSD Foundation, Inc.
  * All rights reserved.
@@ -37,4 +37,38 @@
 
 #define	OMAP3_SDMMC_SDHC_OFFSET	0x100
 #define	OMAP3_SDMMC_SDHC_SIZE	0x100
+
+#define MMCHS_SYSCONFIG		0x010	/* System Configuration */
+#  define SYSCONFIG_CLOCKACTIVITY_MASK	(3 << 8)
+#  define SYSCONFIG_CLOCKACTIVITY_FCLK	(2 << 8)
+#  define SYSCONFIG_CLOCKACTIVITY_ICLK	(1 << 8)
+#  define SYSCONFIG_ENAWAKEUP		(1 << 2)
+#  define SYSCONFIG_SOFTRESET		(1 << 1)
+#  define SYSCONFIG_AUTOIDLE		(1 << 0)
+#define MMCHS_SYSSTATUS		0x014	/* System Status */
+#  define SYSSTATUS_RESETDONE		(1 << 0)
+#define MMCHS_CSRE		0x024	/* Card status response error */
+#define MMCHS_SYSTEST		0x028	/* System Test */
+#define MMCHS_CON		0x02c	/* Configuration */
+#  define CON_CLKEXTFREE		(1 << 16)
+#  define CON_PADEN			(1 << 15)	/* Ctrl Pow for MMC */
+#  define CON_OBIE			(1 << 14)	/* Out-of-Band Intr */
+#  define CON_OBIP			(1 << 13)	/*O-of-B Intr Polarity*/
+#  define CON_CEATA			(1 << 12)	/* CE-ATA */
+#  define CON_CTPL			(1 << 11)	/* Ctrl Power dat[1] */
+#  define CON_DVAL_33US			(0 << 9)	/* debounce filter val*/
+#  define CON_DVAL_231US		(1 << 9)	/* debounce filter val*/
+#  define CON_DVAL_1MS			(2 << 9)	/* debounce filter val*/
+#  define CON_DVAL_8_4MS		(3 << 9)	/*   8.4ms */
+#  define CON_WPP			(1 << 8)	/* Write protect pol */
+#  define CON_CDP			(1 << 7)	/*Card detect polarity*/
+#  define CON_MIT			(1 << 6)	/* MMC interrupt cmd */
+#  define CON_DW8			(1 << 5)	/* 8-bit mode */
+#  define CON_MODE			(1 << 4)	/* SYSTEST mode */
+#  define CON_STR			(1 << 3)	/* Stream command */
+#  define CON_HR			(1 << 2)	/* Broadcast host rsp */
+#  define CON_INIT			(1 << 1)	/* Send init stream */
+#  define CON_OD			(1 << 0)	/* Card open drain */
+#define MMCHS_PWCNT		0x030	/* Power counter */
+
 #endif
