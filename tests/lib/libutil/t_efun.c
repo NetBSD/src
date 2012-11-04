6d5f44a0d7bbc92c65eea2d38ed6a8eb8e82de82
christos <christos@NetBSD.org>
2012-11-04 23:37:02 +0000

    add missing includes

--- tests/lib/libutil/t_efun.c
+++ tests/lib/libutil/t_efun.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: t_efun.c,v 1.2 2011/03/30 17:03:02 jruoho Exp $ */
+/*	$NetBSD: t_efun.c,v 1.3 2012/11/04 23:37:02 christos Exp $ */
 
 /*-
  * Copyright (c) 2011 The NetBSD Foundation, Inc.
@@ -29,10 +29,11 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 #include <sys/cdefs.h>
-__RCSID("$NetBSD: t_efun.c,v 1.2 2011/03/30 17:03:02 jruoho Exp $");
+__RCSID("$NetBSD: t_efun.c,v 1.3 2012/11/04 23:37:02 christos Exp $");
 
 #include <atf-c.h>
 
+#include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
 #include <util.h>
