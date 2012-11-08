667776241826aff565d03b96ea8b252967fd0f8e
joerg <joerg@NetBSD.org>
2012-11-08 11:24:00 +0000

    Provide copy constructor and copy assignment operators for C++11. Clang implements the C++11 semantics properly that require the default to be implicitly deleted.

--- external/gpl3/gcc/dist/libstdc++-v3/include/bits/stl_pair.h
+++ external/gpl3/gcc/dist/libstdc++-v3/include/bits/stl_pair.h
@@ -107,6 +107,9 @@ _GLIBCXX_BEGIN_NAMESPACE(std)
         pair(_U1&& __x, _U2&& __y)
 	: first(std::forward<_U1>(__x)),
 	  second(std::forward<_U2>(__y)) { }
+
+      pair(const pair &) = default;
+      pair(pair &&) = default;
 #endif
 
       /** There is also a templated copy ctor for the @c pair class itself.  */
@@ -120,6 +123,14 @@ _GLIBCXX_BEGIN_NAMESPACE(std)
         pair(pair<_U1, _U2>&& __p)
 	: first(std::forward<_U1>(__p.first)),
 	  second(std::forward<_U2>(__p.second)) { }
+
+      pair&
+      operator=(const pair& __p)
+      { 
+	first = __p.first;
+	second = __p.second;
+	return *this;
+      }
 
       pair&
       operator=(pair&& __p)
