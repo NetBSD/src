cb5a6fca5c0665fe406d943736284eb14b9fbf56
jakllsch <jakllsch@NetBSD.org>
2012-11-14 01:04:45 +0000

    don't attempt to re-unmap the interrupt on detach

--- sys/dev/pci/pciide_common.c
+++ sys/dev/pci/pciide_common.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: pciide_common.c,v 1.57 2012/07/31 15:50:36 bouyer Exp $	*/
+/*	$NetBSD: pciide_common.c,v 1.58 2012/11/14 01:04:45 jakllsch Exp $	*/
 
 
 /*
@@ -70,7 +70,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: pciide_common.c,v 1.57 2012/07/31 15:50:36 bouyer Exp $");
+__KERNEL_RCSID(0, "$NetBSD: pciide_common.c,v 1.58 2012/11/14 01:04:45 jakllsch Exp $");
 
 #include <sys/param.h>
 #include <sys/malloc.h>
@@ -249,12 +249,16 @@ pciide_detach(device_t self, int flags)
 	     channel++) {
 		cp = &sc->pciide_channels[channel];
 		if (cp->compat != 0)
-			if (cp->ih != NULL)
+			if (cp->ih != NULL) {
 			       pciide_unmap_compat_intr(sc->sc_pc, cp, channel);
+			       cp->ih = NULL;
+			}
 	}
 
-	if (sc->sc_pci_ih != NULL)
+	if (sc->sc_pci_ih != NULL) {
 		pci_intr_disestablish(sc->sc_pc, sc->sc_pci_ih);
+		sc->sc_pci_ih = NULL;
+	}
 
 	return pciide_common_detach(sc, flags);
 }
