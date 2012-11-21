c3b22fc99e72e9caaf571fb1f5356ef195b32d86
skrll <skrll@NetBSD.org>
2012-11-21 08:47:13 +0000

    Fix delay.

--- sys/arch/arm/broadcom/bcm2835_tmr.c
+++ sys/arch/arm/broadcom/bcm2835_tmr.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: bcm2835_tmr.c,v 1.1 2012/07/26 06:21:57 skrll Exp $	*/
+/*	$NetBSD: bcm2835_tmr.c,v 1.2 2012/11/21 08:47:13 skrll Exp $	*/
 
 /*-
  * Copyright (c) 2012 The NetBSD Foundation, Inc.
@@ -30,7 +30,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: bcm2835_tmr.c,v 1.1 2012/07/26 06:21:57 skrll Exp $");
+__KERNEL_RCSID(0, "$NetBSD: bcm2835_tmr.c,v 1.2 2012/11/21 08:47:13 skrll Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -159,22 +159,19 @@ delay(unsigned int n)
 	uint32_t delta, usecs;
 
 	KASSERT(sc != NULL);
-	int nhloops = 0;
 
 	last = bus_space_read_4(sc->sc_iot, sc->sc_ioh, BCM2835_STIMER_CLO);
 
 	delta = usecs = 0;
 	while (n > usecs) {
-		if (nhloops++ > 0x100000) Debugger();
 		curr = bus_space_read_4(sc->sc_iot, sc->sc_ioh,
 		    BCM2835_STIMER_CLO);
 
-		/* XXXNH !?!?!?!?!?!?! - use CHI? */
 		/* Check to see if the timer has wrapped around. */
 		if (curr < last)
-			delta += (last + (counts_per_hz - curr));
+			delta += curr + (UINT32_MAX - last);
 		else
-			delta += (last - curr);
+			delta += curr - last;
 
 		last = curr;
 
