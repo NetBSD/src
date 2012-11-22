42f72d3c8582b31ae4182b07f3b6022ffde5b497
riz <riz>
2012-11-22 17:37:38 +0000

    Pull up following revision(s) (requested by chs in ticket #689): 	sys/arch/amd64/amd64/autoconf.c: revision 1.24 add include of opt_intrdebug.h.

--- sys/arch/amd64/amd64/autoconf.c
+++ sys/arch/amd64/amd64/autoconf.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: autoconf.c,v 1.23 2011/02/22 06:37:24 dholland Exp $	*/
+/*	$NetBSD: autoconf.c,v 1.23.10.1 2012/11/22 17:37:38 riz Exp $	*/
 
 /*-
  * Copyright (c) 1990 The Regents of the University of California.
@@ -46,9 +46,10 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: autoconf.c,v 1.23 2011/02/22 06:37:24 dholland Exp $");
+__KERNEL_RCSID(0, "$NetBSD: autoconf.c,v 1.23.10.1 2012/11/22 17:37:38 riz Exp $");
 
 #include "opt_multiprocessor.h"
+#include "opt_intrdebug.h"
 
 #include <sys/param.h>
 #include <sys/systm.h>
