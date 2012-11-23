a6b9d89da45bd3ac23ffae9c372a1b740a5e1665
riz <riz@NetBSD.org>
2012-11-23 16:35:21 +0000

    Pull up following revision(s) (requested by msaitoh in ticket #697): 	sys/dev/pci/if_wm.c: revision 1.237 Fix a bug that PHY isn't set to low-power mode on PCH and PCH2.

--- sys/dev/pci/if_wm.c
+++ sys/dev/pci/if_wm.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: if_wm.c,v 1.227.2.3 2012/09/03 19:09:41 riz Exp $	*/
+/*	$NetBSD: if_wm.c,v 1.227.2.4 2012/11/23 16:35:21 riz Exp $	*/
 
 /*
  * Copyright (c) 2001, 2002, 2003, 2004 Wasabi Systems, Inc.
@@ -76,7 +76,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: if_wm.c,v 1.227.2.3 2012/09/03 19:09:41 riz Exp $");
+__KERNEL_RCSID(0, "$NetBSD: if_wm.c,v 1.227.2.4 2012/11/23 16:35:21 riz Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -4372,7 +4372,7 @@ wm_init(struct ifnet *ifp)
 
 	reg = CSR_READ(sc, WMREG_CTRL_EXT);
 	/* Enable PHY low-power state when MAC is at D3 w/o WoL */
-	if ((sc->sc_type == WM_T_PCH) && (sc->sc_type == WM_T_PCH2))
+	if ((sc->sc_type == WM_T_PCH) || (sc->sc_type == WM_T_PCH2))
 		CSR_WRITE(sc, WMREG_CTRL_EXT, reg | CTRL_EXT_PHYPDEN);
 
 	/* Initialize the transmit descriptor ring. */
