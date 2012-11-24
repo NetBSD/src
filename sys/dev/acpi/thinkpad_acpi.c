3cc18709083df9ba6e61ab1e44a447c364b63453
riastradh <riastradh@NetBSD.org>
2012-11-24 20:56:39 +0000

    Match LEN0068 in thinkpad(4) too, as newer Thinkpads report.
    Tested on a W530.  Lots of stuff is missing, though.

--- sys/dev/acpi/thinkpad_acpi.c
+++ sys/dev/acpi/thinkpad_acpi.c
@@ -1,4 +1,4 @@
-/* $NetBSD: thinkpad_acpi.c,v 1.41 2012/08/14 14:36:43 jruoho Exp $ */
+/* $NetBSD: thinkpad_acpi.c,v 1.42 2012/11/24 20:56:39 riastradh Exp $ */
 
 /*-
  * Copyright (c) 2007 Jared D. McNeill <jmcneill@invisible.ca>
@@ -27,7 +27,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: thinkpad_acpi.c,v 1.41 2012/08/14 14:36:43 jruoho Exp $");
+__KERNEL_RCSID(0, "$NetBSD: thinkpad_acpi.c,v 1.42 2012/11/24 20:56:39 riastradh Exp $");
 
 #include <sys/param.h>
 #include <sys/device.h>
@@ -133,6 +133,7 @@ CFATTACH_DECL_NEW(thinkpad, sizeof(thinkpad_softc_t),
 
 static const char * const thinkpad_ids[] = {
 	"IBM0068",
+	"LEN0068",
 	NULL
 };
 
