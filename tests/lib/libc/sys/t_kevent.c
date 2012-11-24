eef30dafdf0e894ac8642d53f64de894569665dd
jdc <jdc@NetBSD.org>
2012-11-24 21:40:02 +0000

    Pull up revisions:   src/sys/kern/kern_event.c revision 1.79   src/sys/kern/kern_descrip.c revision 1.219   src/lib/libc/sys/kqueue.2 revision 1.33   src/tests/lib/libc/sys/t_kevent.c revision 1.2-1.5 (requested by christos in ticket #716).
    - initialize kn_id
    - in close, invalidate f_data and f_type early to prevent accidental re-use
    - add a DIAGNOSTIC for when we use unsupported fd's and a KASSERT for f_event
      being NULL.
    
    Return EOPNOTSUPP for fnullop_kqfilter to prevent registration of unsupported
    fds. XXX: We should really fix the fd's to be supported in the future.
    Unsupported fd's have a NULL f_event, so registering crashes the kernel with
    a NULL function dereference of f_event.
    
    mention that kevent returns now EOPNOTSUPP.
    
    Move the references to PRs from code comments to the test description. Once
    ATF has the ability to output the metadata in the HTML reports, it should be
    easy to traverse between releng and gnats -reports via links.
    
    Add a (skipped for now) test case for PR 46463
    
    adapt to new reality
    
    Add a test for adding an event to an unsupported fd.

--- tests/lib/libc/sys/t_kevent.c
+++ tests/lib/libc/sys/t_kevent.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: t_kevent.c,v 1.1 2011/11/17 01:14:12 christos Exp $ */
+/*	$NetBSD: t_kevent.c,v 1.1.2.1 2012/11/24 21:40:03 jdc Exp $ */
 
 /*-
  * Copyright (c) 2011 The NetBSD Foundation, Inc.
@@ -29,40 +29,149 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 #include <sys/cdefs.h>
-__RCSID("$NetBSD: t_kevent.c,v 1.1 2011/11/17 01:14:12 christos Exp $");
-
-#include <time.h>
-#include <stdio.h>
+__RCSID("$NetBSD: t_kevent.c,v 1.1.2.1 2012/11/24 21:40:03 jdc Exp $");
 
 #include <sys/types.h>
 #include <sys/event.h>
 
 #include <atf-c.h>
+#include <errno.h>
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <err.h>
+#include <sys/event.h>
+#include <sys/time.h>
+#include <sys/socket.h>
+#include <sys/wait.h>
 
 ATF_TC(kevent_zerotimer);
 ATF_TC_HEAD(kevent_zerotimer, tc)
 {
-
-	/* Cf. PR lib/45618. */
 	atf_tc_set_md_var(tc, "descr", "Checks that kevent with a 0 timer "
-	    "does not crash the system");
+	    "does not crash the system (PR lib/45618)");
 }
 
 ATF_TC_BODY(kevent_zerotimer, tc)
 {
 	struct kevent ev;
 	int kq;
-	
+
 	ATF_REQUIRE((kq = kqueue()) != -1);
 	EV_SET(&ev, 1, EVFILT_TIMER, EV_ADD|EV_ENABLE, 0, 1, 0);
 	ATF_REQUIRE(kevent(kq, &ev, 1, NULL, 0, NULL) != -1);
 	ATF_REQUIRE(kevent(kq, NULL, 0, &ev, 1, NULL) == 1);
 }
 
+ATF_TC(kqueue_desc_passing);
+ATF_TC_HEAD(kqueue_desc_passing, tc)
+{
+	atf_tc_set_md_var(tc, "descr", "Checks that passing a kqueue to "
+		"another process does not crash the kernel (PR 46463)");
+}
+
+ATF_TC_BODY(kqueue_desc_passing, tc)
+{
+	pid_t child;
+	int s[2], storage, status, kq;
+	struct cmsghdr *msg;
+	struct iovec iov;
+	struct msghdr m;
+	struct kevent ev;
+
+	ATF_REQUIRE((kq = kqueue()) != -1);
+
+	// atf_tc_skip("crashes kernel (PR 46463)");
+
+	ATF_REQUIRE(socketpair(AF_LOCAL, SOCK_STREAM, 0, s) != -1);
+	msg = malloc(CMSG_SPACE(sizeof(int)));
+	m.msg_iov = &iov;
+	m.msg_iovlen = 1;
+	m.msg_name = NULL;
+	m.msg_namelen = 0;
+	m.msg_control = msg;
+	m.msg_controllen = CMSG_SPACE(sizeof(int));
+
+	child = fork();
+	if (child == 0) {
+		close(s[0]);
+
+		iov.iov_base = &storage;
+		iov.iov_len = sizeof(int);
+		m.msg_iov = &iov;
+		m.msg_iovlen = 1;
+
+		if (recvmsg(s[1], &m, 0) == -1)
+			err(1, "child: could not recvmsg");
+
+		kq = *(int *)CMSG_DATA(msg);
+		printf("child (pid %d): received kq fd %d\n", getpid(), kq);
+		exit(0);
+	}
+
+	close(s[1]);
+
+	iov.iov_base = &storage;
+	iov.iov_len = sizeof(int);
+
+	msg->cmsg_level = SOL_SOCKET;
+	msg->cmsg_type = SCM_RIGHTS;
+	msg->cmsg_len = CMSG_LEN(sizeof(int));
+
+	*(int *)CMSG_DATA(msg) = kq;
+
+	EV_SET(&ev, 1, EVFILT_TIMER, EV_ADD|EV_ENABLE, 0, 1, 0);
+	ATF_CHECK(kevent(kq, &ev, 1, NULL, 0, NULL) != -1);
+
+	printf("parent (pid %d): sending kq fd %d\n", getpid(), kq);
+	if (sendmsg(s[0], &m, 0) == -1) {
+		ATF_REQUIRE_EQ_MSG(errno, EBADF, "errno is %d", errno);
+		atf_tc_skip("PR kern/46523");
+	}
+
+	close(kq);
+
+	waitpid(child, &status, 0);
+	ATF_CHECK(WIFEXITED(status) && WEXITSTATUS(status)==0);
+}
+
+ATF_TC(kqueue_unsupported_fd);
+ATF_TC_HEAD(kqueue_unsupported_fd, tc)
+{
+	atf_tc_set_md_var(tc, "descr", "Checks that watching an fd whose"
+	    " type is not supported does not crash the kernel");
+}
+
+ATF_TC_BODY(kqueue_unsupported_fd, tc)
+{
+	/* mqueue and semaphore use fnullop_kqueue also */
+	static const char drvctl[] = "/dev/drvctl";
+	int fd, kq;
+	struct kevent ev;
+
+	ATF_REQUIRE((fd = open(drvctl, O_RDONLY)) != -1);
+	ATF_REQUIRE((kq = kqueue()) != -1);
+
+	EV_SET(&ev, fd, EVFILT_VNODE, EV_ADD | EV_ENABLE | EV_CLEAR,
+	   NOTE_DELETE|NOTE_WRITE|NOTE_EXTEND|NOTE_ATTRIB|NOTE_LINK|
+	   NOTE_RENAME|NOTE_REVOKE, 0, 0);
+
+	ATF_REQUIRE(kevent(kq, &ev, 1, NULL, 0, NULL) == -1);
+	ATF_REQUIRE_ERRNO(EOPNOTSUPP, true);
+
+	(void)close(fd);
+}
+
+
 ATF_TP_ADD_TCS(tp)
 {
 
 	ATF_TP_ADD_TC(tp, kevent_zerotimer);
+	ATF_TP_ADD_TC(tp, kqueue_desc_passing);
+	ATF_TP_ADD_TC(tp, kqueue_unsupported_fd);
 
 	return atf_no_error();
 }
