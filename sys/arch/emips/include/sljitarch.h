50e86e8a67dddd6655f37a669e3656217839e4c8
alnsn <alnsn@NetBSD.org>
2012-11-25 21:55:50 +0000

    Add sljitarch.h to all mips machines.

---
+++ sys/arch/emips/include/sljitarch.h
@@ -0,0 +1,3 @@
+/*	$NetBSD: sljitarch.h,v 1.1 2012/11/25 21:55:50 alnsn Exp $	*/
+
+#include <mips/sljitarch.h>
---
+++ sys/arch/newsmips/include/sljitarch.h
@@ -0,0 +1,3 @@
+/*	$NetBSD: sljitarch.h,v 1.1 2012/11/25 21:55:51 alnsn Exp $	*/
+
+#include <mips/sljitarch.h>
