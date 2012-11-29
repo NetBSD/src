fa8e59e7483866c676390f6c1dfe02357f1467dc
christos <christos@NetBSD.org>
2012-11-29 02:07:20 +0000

    Add a new sysctl to mark ports as reserved, so that they are not used in the anonymous or reserved port allocation.

--- sys/netinet/portalgo.h
+++ sys/netinet/portalgo.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: portalgo.h,v 1.1 2012/06/25 15:28:39 christos Exp $	*/
+/*	$NetBSD: portalgo.h,v 1.2 2012/11/29 02:07:20 christos Exp $	*/
 
 /*
  * Copyright 2011 Vlad Balan
@@ -35,8 +35,10 @@
 
 struct inpcb_hdr;
 int portalgo_randport(uint16_t *, struct inpcb_hdr *, kauth_cred_t);
-int sysctl_portalgo_selected(SYSCTLFN_ARGS);
+int sysctl_portalgo_selected4(SYSCTLFN_ARGS);
 int sysctl_portalgo_selected6(SYSCTLFN_ARGS);
+int sysctl_portalgo_reserve4(SYSCTLFN_ARGS);
+int sysctl_portalgo_reserve6(SYSCTLFN_ARGS);
 int sysctl_portalgo_available(SYSCTLFN_ARGS);
 int portalgo_algo_index_select(struct inpcb_hdr *, int);
 
