402cc8781671e8b1ae6cb587636afe967f2f8983
skrll <skrll@NetBSD.org>
2012-12-01 08:20:55 +0000

    Check for _NETBSD_SOURCE being defined wherever we check for _INCOMPLETE_XOPEN_C063 so that we expose the new POSIX extended API set recently added.

--- sys/sys/stat.h
+++ sys/sys/stat.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: stat.h,v 1.64 2012/11/03 19:39:21 christos Exp $	*/
+/*	$NetBSD: stat.h,v 1.65 2012/12/01 08:20:55 skrll Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -244,7 +244,8 @@ int	lchmod(const char *, mode_t);
 /*
  * X/Open Extended API set 2 (a.k.a. C063)
  */
-#if defined(_INCOMPLETE_XOPEN_C063) 
+#if (_POSIX_C_SOURCE - 0) >= 200809L || (_XOPEN_SOURCE - 0 >= 700) || \
+    defined(_INCOMPLETE_XOPEN_C063) || defined(_NETBSD_SOURCE)
 int     fstatat(int, const char *, struct stat *, int);
 int     utimensat(int, const char *, const struct timespec *, int);
 #endif
