b7b08f7c42a443100b15e6f758a14b576eebebee
rmind <rmind@NetBSD.org>
2012-12-04 19:28:16 +0000

    npf_table_list: avoid triggering assert on diagnostic.

--- sys/net/npf/npf_tableset.c
+++ sys/net/npf/npf_tableset.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: npf_tableset.c,v 1.15 2012/10/29 02:27:12 rmind Exp $	*/
+/*	$NetBSD: npf_tableset.c,v 1.16 2012/12/04 19:28:16 rmind Exp $	*/
 
 /*-
  * Copyright (c) 2009-2012 The NetBSD Foundation, Inc.
@@ -41,7 +41,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: npf_tableset.c,v 1.15 2012/10/29 02:27:12 rmind Exp $");
+__KERNEL_RCSID(0, "$NetBSD: npf_tableset.c,v 1.16 2012/12/04 19:28:16 rmind Exp $");
 
 #include <sys/param.h>
 #include <sys/types.h>
@@ -575,8 +575,7 @@ npf_table_list(npf_tableset_t *tset, u_int tid, void *ubuf, size_t len)
 		if (error)
 			break;
 		error = table_tree_list(&t->t_tree[1], 128, ubuf, len, &off);
-		if (error)
-			break;
+		break;
 	default:
 		KASSERT(false);
 	}
