62159a978b49dc8cc2443429b45e37bcd9335394
christos <christos@NetBSD.org>
2012-12-12 18:19:25 +0000

    make sure that our hpn patches are up-to-date

--- crypto/external/bsd/openssh/dist/version.h
+++ crypto/external/bsd/openssh/dist/version.h
@@ -1,9 +1,9 @@
-/*	$NetBSD: version.h,v 1.9 2012/12/12 17:42:40 christos Exp $	*/
+/*	$NetBSD: version.h,v 1.10 2012/12/12 18:19:25 christos Exp $	*/
 /* $OpenBSD: version.h,v 1.65 2012/07/22 18:19:21 markus Exp $ */
 
 #define __OPENSSH_VERSION	"OpenSSH_6.1"
 #define __NETBSDSSH_VERSION	"NetBSD_Secure_Shell-20121212"
-#define SSH_HPN         "-hpn13v11"
+#define SSH_HPN         "-hpn13v14"
 #define SSH_LPK		"-lpk"
 /*
  * it is important to retain OpenSSH version identification part, it is
