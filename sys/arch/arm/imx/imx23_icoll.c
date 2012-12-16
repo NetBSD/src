0718635be130c0cbc65983135ee861e1293c4b49
jkunz <jkunz@NetBSD.org>
2012-12-16 19:40:00 +0000

    Contribution from Petri Laakso: Fix some spelling.

--- sys/arch/arm/imx/imx23_icoll.c
+++ sys/arch/arm/imx/imx23_icoll.c
@@ -1,4 +1,4 @@
-/* $Id: imx23_icoll.c,v 1.1 2012/11/20 19:06:13 jkunz Exp $ */
+/* $Id: imx23_icoll.c,v 1.2 2012/12/16 19:40:00 jkunz Exp $ */
 
 /*
  * Copyright (c) 2012 The NetBSD Foundation, Inc.
@@ -317,7 +317,7 @@ icoll_activate(device_t self, enum devact act)
 /*
  * Reset the ICOLL block.
  *
- * Inspired by i.MX233 RM "39.3.10 Correct Way to Soft Reset a Block"
+ * Inspired by i.MX23 RM "39.3.10 Correct Way to Soft Reset a Block"
  */
 static void
 icoll_reset(struct icoll_softc *sc)
