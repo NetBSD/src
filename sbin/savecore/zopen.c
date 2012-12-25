29eda73fd2e8a0950bfd238a707ea642ffbd2a78
mbalmer <mbalmer@NetBSD.org>
2012-12-25 09:24:45 +0000

    Consistent use of return.

--- sbin/savecore/zopen.c
+++ sbin/savecore/zopen.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: zopen.c,v 1.2 2005/06/27 01:00:06 christos Exp $	*/
+/*	$NetBSD: zopen.c,v 1.3 2012/12/25 09:24:45 mbalmer Exp $	*/
 
 /*
  * Public domain stdio wrapper for libz, written by Johan Danielsson.
@@ -6,7 +6,7 @@
 
 #include <sys/cdefs.h>
 #ifndef lint
-__RCSID("$NetBSD: zopen.c,v 1.2 2005/06/27 01:00:06 christos Exp $");
+__RCSID("$NetBSD: zopen.c,v 1.3 2012/12/25 09:24:45 mbalmer Exp $");
 #endif
 
 #include <stdio.h>
@@ -35,7 +35,7 @@ zopen(const char *fname, const char *mode)
 	return NULL;
 
     if(*mode == 'r')
-	return (funopen(gz, xgzread, NULL, NULL, gzclose));
+	return funopen(gz, xgzread, NULL, NULL, gzclose);
     else
-	return (funopen(gz, NULL, xgzwrite, NULL, gzclose));
+	return funopen(gz, NULL, xgzwrite, NULL, gzclose);
 }
