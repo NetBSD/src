02e5e84c0b2e044cc8511099f85cfb8363149ebc
martin <martin@NetBSD.org>
2012-12-27 20:21:50 +0000

    Make it all compile

--- sys/arch/ia64/stand/ia64/efi/main.c
+++ sys/arch/ia64/stand/ia64/efi/main.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: main.c,v 1.7 2011/01/22 19:19:19 joerg Exp $	*/
+/*	$NetBSD: main.c,v 1.8 2012/12/27 20:21:51 martin Exp $	*/
 
 /*-
  * Copyright (c) 1998 Michael Smith <msmith@freebsd.org>
@@ -32,7 +32,6 @@
 #include <lib/libsa/stand.h>
 #include <lib/libsa/loadfile.h>
 
-
 #include <machine/sal.h>
 #include <machine/pal.h>
 #include <machine/pte.h>
@@ -40,6 +39,7 @@
 
 #include <efi.h>
 #include <efilib.h>
+#include <efifsdev.h>
 
 #include "bootstrap.h"
 #include "efiboot.h"
