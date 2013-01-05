c55cb9a0a871eaeea19b17e4c5b61b02691e995b
christos <christos@NetBSD.org>
2013-01-05 15:04:00 +0000

    remove panic cmd

--- sys/arch/acorn26/include/db_machdep.h
+++ sys/arch/acorn26/include/db_machdep.h
@@ -1,4 +1,4 @@
-/* $NetBSD: db_machdep.h,v 1.8 2011/04/11 04:22:30 mrg Exp $ */
+/* $NetBSD: db_machdep.h,v 1.9 2013/01/05 15:04:00 christos Exp $ */
 
 #include <arm/db_machdep.h>
 
@@ -6,7 +6,6 @@
 #define DB_ELF_SYMBOLS
 #define DB_ELFSIZE 32
 
-void db_show_panic_cmd(db_expr_t, bool, db_expr_t, const char *);
 void db_show_frame_cmd(db_expr_t, bool, db_expr_t, const char *);
 void db_bus_write_cmd(db_expr_t, bool, db_expr_t, const char *);
 void db_irqstat_cmd(db_expr_t, bool, db_expr_t, const char *);
