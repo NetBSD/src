9d00f8570b4da4a60dcccfab920da673fc3c07a7
dsl <dsl>
2013-01-07 23:20:42 +0000

    Add support for the xsave related data from cpuid 8.n. Reorder the output so that the 'brand' string - which actually identifies   the cpu is output first.

--- usr.sbin/cpuctl/arch/cpuctl_i386.h
+++ usr.sbin/cpuctl/arch/cpuctl_i386.h
@@ -1,4 +1,4 @@
-/*      $NetBSD: cpuctl_i386.h,v 1.1 2013/01/05 15:27:45 dsl Exp $      */
+/*      $NetBSD: cpuctl_i386.h,v 1.2 2013/01/07 23:20:42 dsl Exp $      */
 
 /* Interfaces to code in i386-asm.S */
 
@@ -6,3 +6,4 @@
 
 void x86_cpuid2(uint32_t, uint32_t, uint32_t *);
 uint32_t x86_identify(void);
+uint32_t x86_xgetbv(void);
