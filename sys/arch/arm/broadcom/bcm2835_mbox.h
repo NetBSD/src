2cd0c5529f77bca71c101d26ef46d152e60795b2
jmcneill <jmcneill>
2013-01-07 20:15:32 +0000

    allow for a single board specific device to attach to the mbox driver

--- sys/arch/arm/broadcom/bcm2835_mbox.h
+++ sys/arch/arm/broadcom/bcm2835_mbox.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: bcm2835_mbox.h,v 1.3 2012/10/13 08:42:50 skrll Exp $	*/
+/*	$NetBSD: bcm2835_mbox.h,v 1.4 2013/01/07 20:15:32 jmcneill Exp $	*/
 
 /*-
  * Copyright (c) 2012 The NetBSD Foundation, Inc.
@@ -41,5 +41,11 @@ void bcm2835_mbox_write(bus_space_tag_t, bus_space_handle_t, uint8_t,
 
 void bcmmbox_read(uint8_t, uint32_t *);
 void bcmmbox_write(uint8_t, uint32_t);
+
+int bcmmbox_request(uint8_t, void *, size_t, uint32_t *);
+
+struct bcmmbox_attach_args {
+	bus_dma_tag_t	baa_dmat;
+};
 
 #endif /* _BCM2835_MBOX_H_ */
