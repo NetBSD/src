52904b119b9a96cf7744ccb34f23aa0ef7e9d5f4
riz <riz@NetBSD.org>
2013-01-07 15:41:55 +0000

    doc/3RDPARTY					1.989, 1.991 via patch external/ibm-public/postfix/dist/HISTORY	patch external/ibm-public/postfix/dist/makedefs	patch external/ibm-public/postfix/dist/src/global/dict_ldap.c	patch external/ibm-public/postfix/dist/src/global/mail_version.h	patch external/ibm-public/postfix/dist/src/oqmgr/qmgr_message.c	patch external/ibm-public/postfix/dist/src/postscreen/postscreen_access.c patch external/ibm-public/postfix/dist/src/util/myaddrinfo.c	patch external/ibm-public/postfix/dist/src/util/sys_defs.h	patch
    	Update Postfix to version 2.8.13. Changes since version 2.8.12:
    	- The postscreen_access_list feature failed to ignore case in the first
    	  character of a command (e.g., permit, reject, etc.). Reported by
    	  Francis Picabia. (This fix is incorrectly listed in the HISTORY
    	  files of earlier releases, and will be removed with a future patch.)
    	- Strip the datalink suffix (e.g., %eth0) from IPv6 addresses returned
    	  by the system getaddrinfo() routine. Such suffixes break the default
    	  mynetworks value, the Postfix SMTP server's reverse/forward DNS
    	  name/address mapping check, and possibly more.
    	- To eliminate the possibility of collisions with connection cache
    	  lookup keys, the Postfix LDAP client now computes those lookup keys
    	  by joining the number-valued connection properties with ASCII null,
    	  just like it already did with the string-valued connection
    	  properties.
    	- There was a memory leak during one-time TLS library initialization
    	  (introduced with Postfix 2.5). Reported by Coverity.
    	- There was a memory leak in the unused oqmgr(8) program (introduced
    	  with Postfix 2.3). Reported by Coverity.
    	[tron, ticket #771]

--- external/ibm-public/postfix/dist/src/global/mail_version.h
+++ external/ibm-public/postfix/dist/src/global/mail_version.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: mail_version.h,v 1.1.1.12.2.3 2012/08/12 19:25:23 martin Exp $	*/
+/*	$NetBSD: mail_version.h,v 1.1.1.12.2.4 2013/01/07 15:41:56 riz Exp $	*/
 
 #ifndef _MAIL_VERSION_H_INCLUDED_
 #define _MAIL_VERSION_H_INCLUDED_
@@ -22,8 +22,8 @@
   * Patches change both the patchlevel and the release date. Snapshots have no
   * patchlevel; they change the release date only.
   */
-#define MAIL_RELEASE_DATE	"20120801"
-#define MAIL_VERSION_NUMBER	"2.8.12"
+#define MAIL_RELEASE_DATE	"20121213"
+#define MAIL_VERSION_NUMBER	"2.8.13"
 
 #ifdef SNAPSHOT
 # define MAIL_VERSION_DATE	"-" MAIL_RELEASE_DATE
