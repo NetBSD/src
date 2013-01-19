165c43d75bb8b0dee99cbb3a760329535b28b981
christos <christos@NetBSD.org>
2013-01-19 21:05:46 +0000

    add a dependency to realall from Takeshi Nakayama

--- crypto/external/bsd/openssl/pkgconfig.mk
+++ crypto/external/bsd/openssl/pkgconfig.mk
@@ -1,10 +1,11 @@
-# $NetBSD: pkgconfig.mk,v 1.1 2013/01/18 18:09:55 christos Exp $
+# $NetBSD: pkgconfig.mk,v 1.2 2013/01/19 21:05:46 christos Exp $
 
 FILESDIR=/usr/lib/pkgconfig
 .for pkg in ${PKGCONFIG}
 FILES+=${pkg}.pc
 CLEANFILES+=${pkg}.pc
 
+realall: ${pkg}.pc
 ${pkg}.pc: ${.CURDIR}/../../mkpc
 	${.ALLSRC} ${.TARGET} > ${.TARGET}
 .endfor
