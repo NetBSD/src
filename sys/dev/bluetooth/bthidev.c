b50fc1281e445625e438f258748f5ad145d897f4
bouyer <bouyer@NetBSD.org>
2013-01-20 12:10:51 +0000

    Pull up following revision(s) (requested by plunky in ticket #1835): 	sys/dev/bluetooth/bthidev.c: revision 1.23 via patch 	sys/dev/bluetooth/bthidev.c: revision 1.24 via patch 	usr.sbin/btdevctl/btdevctl.8: revision 1.8 via patch 	sys/dev/bluetooth/btmagic.c: revision 1.4 via patch 	sys/dev/bluetooth/btmagic.c: revision 1.5 via patch when no link-mode is specified, explicitly set a mode of (int)0 otherwise l2cap_setmode() will fail during connection setup for bthidev.c, this fixes a problem with Microsoft Wedge Touch mouse (which may not be able to authenticate) for btmagic.c, include this fix in case somebody tries that related to PR/47286 increase some diagnostics add a comment pointing out that the link-mode may need to be changed manually, for devices which cannot authenticate for PR/47286

--- sys/dev/bluetooth/bthidev.c
+++ sys/dev/bluetooth/bthidev.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: bthidev.c,v 1.16 2008/08/06 15:01:23 plunky Exp $	*/
+/*	$NetBSD: bthidev.c,v 1.16.4.1 2013/01/20 12:10:51 bouyer Exp $	*/
 
 /*-
  * Copyright (c) 2006 Itronix Inc.
@@ -32,7 +32,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: bthidev.c,v 1.16 2008/08/06 15:01:23 plunky Exp $");
+__KERNEL_RCSID(0, "$NetBSD: bthidev.c,v 1.16.4.1 2013/01/20 12:10:51 bouyer Exp $");
 
 #include <sys/param.h>
 #include <sys/conf.h>
@@ -181,6 +181,7 @@ bthidev_attach(device_t parent, device_t self, void *aux)
 	const void *desc;
 	int locs[BTHIDBUSCF_NLOCS];
 	int maxid, rep, dlen;
+	int err;
 
 	/*
 	 * Init softc
@@ -220,7 +221,8 @@ bthidev_attach(device_t parent, device_t self, void *aux)
 
 		aprint_verbose(" %s %s", BTDEVmode,
 					 prop_string_cstring_nocopy(obj));
-	}
+	} else
+		sockopt_setint(&sc->sc_mode, 0);
 
 	obj = prop_dictionary_get(dict, BTHIDEVcontrolpsm);
 	if (prop_object_type(obj) == PROP_TYPE_NUMBER) {
@@ -305,8 +307,9 @@ bthidev_attach(device_t parent, device_t self, void *aux)
 	 * start bluetooth connections
 	 */
 	mutex_enter(bt_lock);
-	if ((sc->sc_flags & BTHID_RECONNECT) == 0)
-		bthidev_listen(sc);
+	if ((sc->sc_flags & BTHID_RECONNECT) == 0
+	    && (err = bthidev_listen(sc)) != 0)
+		aprint_error_dev(self, "failed to listen (%d)\n", err);
 
 	if (sc->sc_flags & BTHID_CONNECTING)
 		bthidev_connect(sc);
@@ -514,8 +517,10 @@ bthidev_connect(struct bthidev_softc *sc)
 	}
 
 	err = l2cap_setopt(sc->sc_ctl, &sc->sc_mode);
-	if (err)
+	if (err) {
+		aprint_error_dev(sc->sc_dev, "l2cap_setopt failed (%d)\n", err);
 		return err;
+	}
 
 	bdaddr_copy(&sa.bt_bdaddr, &sc->sc_laddr);
 	err = l2cap_bind(sc->sc_ctl, &sa);
@@ -638,7 +643,7 @@ bthidev_ctl_disconnected(void *arg, int err)
 	sc->sc_state = BTHID_CLOSED;
 
 	if (sc->sc_int == NULL) {
-		aprint_normal_dev(sc->sc_dev, "disconnected\n");
+		aprint_normal_dev(sc->sc_dev, "disconnected (%d)\n", err);
 		sc->sc_flags &= ~BTHID_CONNECTING;
 
 		if (sc->sc_flags & BTHID_RECONNECT)
@@ -670,7 +675,7 @@ bthidev_int_disconnected(void *arg, int err)
 	sc->sc_state = BTHID_CLOSED;
 
 	if (sc->sc_ctl == NULL) {
-		aprint_normal_dev(sc->sc_dev, "disconnected\n");
+		aprint_normal_dev(sc->sc_dev, "disconnected (%d)\n", err);
 		sc->sc_flags &= ~BTHID_CONNECTING;
 
 		if (sc->sc_flags & BTHID_RECONNECT)
