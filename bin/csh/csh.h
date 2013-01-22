360a0eaf2d30cbb9624a4c2e3711dc78d431b7cd
christos <christos@NetBSD.org>
2013-01-22 22:40:31 +0000

    fix compilation without -DEDIT

--- bin/csh/csh.h
+++ bin/csh/csh.h
@@ -1,4 +1,4 @@
-/* $NetBSD: csh.h,v 1.23 2013/01/22 20:35:29 christos Exp $ */
+/* $NetBSD: csh.h,v 1.24 2013/01/22 22:40:31 christos Exp $ */
 
 /*-
  * Copyright (c) 1980, 1991, 1993
@@ -552,7 +552,7 @@ Char **STR_environ;
 #ifdef EDIT
 #include <histedit.h>
 EditLine *el;
-int editing;
 #endif
+int editing;
 
 #endif /* !_CSH_H_ */
