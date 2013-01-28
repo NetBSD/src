fcfaefa80b3ee52d199958db6d94b5c6901d12ec
matt <matt@NetBSD.org>
2013-01-28 06:26:20 +0000

    Add long double aliases

--- lib/libm/src/namespace.h
+++ lib/libm/src/namespace.h
@@ -1,14 +1,18 @@
-/* $NetBSD: namespace.h,v 1.4 2011/07/26 16:10:16 joerg Exp $ */
+/* $NetBSD: namespace.h,v 1.5 2013/01/28 06:26:20 matt Exp $ */
 
 #define atan2 _atan2
 #define atan2f _atan2f
+#define atan2l _atan2l
 #define hypot _hypot
 #define hypotf _hypotf
+#define hypotl _hypotl
 
 #define exp _exp
 #define expf _expf
+#define expl _expl
 #define log _log
 #define logf _logf
+#define logl _logl
 
 #if 0 /* not yet - need to review use in machdep code first */
 #define sin _sin
@@ -20,16 +24,24 @@
 #endif /* notyet */
 #define sinh _sinh
 #define sinhf _sinhf
+#define sinhl _sinhl
 #define cosh _cosh
 #define coshf _coshf
+#define coshl _coshl
 #define asin _asin
 #define asinf _asinf
+#define asinl _asinl
 
 #define casin _casin
 #define casinf _casinf
+#define casinl _casinl
 #define catan _catan
 #define catanf _catanf
+#define catanl _catanl
 
 #define scalbn _scalbn
 #define scalbnf _scalbnf
 #define scalbnl _scalbnl
+#define scalbln _scalbln
+#define scalblnf _scalblnf
+#define scalblnl _scalblnl
