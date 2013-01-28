735c02bbd717f37ab847858522a6bfb3adaa5262
matt <matt@NetBSD.org>
2013-01-28 06:34:09 +0000

    Deal with _LP64 properly.

--- lib/libm/src/s_scalbnf.c
+++ lib/libm/src/s_scalbnf.c
@@ -15,25 +15,35 @@
 
 #include <sys/cdefs.h>
 #if defined(LIBM_SCCS) && !defined(lint)
-__RCSID("$NetBSD: s_scalbnf.c,v 1.9 2010/04/23 19:17:07 drochner Exp $");
+__RCSID("$NetBSD: s_scalbnf.c,v 1.10 2013/01/28 06:34:09 matt Exp $");
 #endif
 
 #include "namespace.h"
 #include "math.h"
 #include "math_private.h"
 
-#ifdef __weak_alias
-__weak_alias(scalbnf, _scalbnf)
+#ifndef _LP64
+__strong_alias(_scalbnf, _scalblnf)
 #endif
+__weak_alias(scalbnf, _scalbnf)
+__weak_alias(scalblnf, _scalblnf)
 
 static const float
-two25   =  3.355443200e+07,	/* 0x4c000000 */
-twom25  =  2.9802322388e-08,	/* 0x33000000 */
+two25   =  0x1.0p25,	/* 0x4c000000 */
+twom25  =  0x1.0p-25,	/* 0x33000000 */
 huge   = 1.0e+30,
 tiny   = 1.0e-30;
 
+#ifdef _LP64
 float
 scalbnf(float x, int n)
+{
+	return scalblnf(x, n);
+}
+#endif
+
+float
+scalblnf(float x, long n)
 {
 	int32_t k,ix;
 	GET_FLOAT_WORD(ix,x);
