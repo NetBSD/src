6a5fbf90ec759985ce028563d3aa51807b14812f
matt <matt@NetBSD.org>
2013-01-31 14:58:51 +0000

    add a fpu_used sysarch

--- sys/arch/arm/include/sysarch.h
+++ sys/arch/arm/include/sysarch.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: sysarch.h,v 1.8 2012/08/12 05:05:47 matt Exp $	*/
+/*	$NetBSD: sysarch.h,v 1.9 2013/01/31 14:58:51 matt Exp $	*/
 
 /*
  * Copyright (c) 1996-1997 Mark Brinicombe.
@@ -49,6 +49,7 @@
 #define ARM_SYNC_ICACHE		0
 #define ARM_DRAIN_WRITEBUF	1
 #define ARM_VFP_FPSCR		2
+#define ARM_FPU_USED		3
 
 struct arm_sync_icache_args {
 	uintptr_t	addr;		/* Virtual start address */
@@ -58,6 +59,10 @@ struct arm_sync_icache_args {
 struct arm_vfp_fpscr_args {
 	uint32_t	fpscr_clear;	/* bits to clear */
 	uint32_t	fpscr_set;	/* bits to set */
+};
+
+struct arm_unaligned_faults_args {
+	bool		enabled;	/* unaligned faults are enabled */ 
 };
 
 #ifndef _KERNEL
