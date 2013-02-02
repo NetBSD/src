32e756387c29fdcb6fd49156d6443800924e2c01
sborrill <sborrill@NetBSD.org>
2013-02-02 15:43:27 +0000

    Pull up the following revisions(s) (requested by dsl in ticket #773): 	bin/sh/expand.c:	revision 1.88 	bin/sh/expand.h:	revision 1.19
    Fix the expansion of "$(foo-$bar}" so that IFS isn't applied when
    expanding $bar. Should fix PR bin/47361

--- bin/sh/expand.c
+++ bin/sh/expand.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: expand.c,v 1.86 2011/08/31 16:24:54 plunky Exp $	*/
+/*	$NetBSD: expand.c,v 1.86.4.1 2013/02/02 15:43:27 sborrill Exp $	*/
 
 /*-
  * Copyright (c) 1991, 1993
@@ -37,7 +37,7 @@
 #if 0
 static char sccsid[] = "@(#)expand.c	8.5 (Berkeley) 5/15/95";
 #else
-__RCSID("$NetBSD: expand.c,v 1.86 2011/08/31 16:24:54 plunky Exp $");
+__RCSID("$NetBSD: expand.c,v 1.86.4.1 2013/02/02 15:43:27 sborrill Exp $");
 #endif
 #endif /* not lint */
 
@@ -247,7 +247,7 @@ argstr(char *p, int flag)
 			break;
 		default:
 			STPUTC(c, expdest);
-			if (flag & EXP_IFS_SPLIT & ifs_split && strchr(ifs, c) != NULL) {
+			if (flag & ifs_split && strchr(ifs, c) != NULL) {
 				/* We need to get the output split here... */
 				recordregion(expdest - stackblock() - 1,
 						expdest - stackblock(), 0);
@@ -702,8 +702,21 @@ again: /* jump here after setting a variable with ${var=text} */
 	}
 
 
-	apply_ifs = ((varflags & VSQUOTE) == 0 ||
-		(*var == '@' && shellparam.nparam != 1));
+	if (flag & EXP_IN_QUOTES)
+		apply_ifs = 0;
+	else if (varflags & VSQUOTE) {
+		if  (*var == '@' && shellparam.nparam != 1)
+		    apply_ifs = 1;
+		else {
+		    /*
+		     * Mark so that we don't apply IFS if we recurse through
+		     * here expanding $bar from "${foo-$bar}".
+		     */
+		    flag |= EXP_IN_QUOTES;
+		    apply_ifs = 0;
+		}
+	} else
+		apply_ifs = 1;
 
 	switch (subtype) {
 	case VSLENGTH:
