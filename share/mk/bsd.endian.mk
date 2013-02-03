0ae431077231874787121be38e68f3f0f7e0ee92
matt <matt@NetBSD.org>
2013-02-03 05:35:25 +0000

    add earmhf

--- share/mk/bsd.endian.mk
+++ share/mk/bsd.endian.mk
@@ -1,4 +1,4 @@
-#	$NetBSD: bsd.endian.mk,v 1.16 2012/08/05 04:11:35 matt Exp $
+#	$NetBSD: bsd.endian.mk,v 1.17 2013/02/03 05:35:25 matt Exp $
 
 .if !defined(_BSD_ENDIAN_MK_)
 _BSD_ENDIAN_MK_=1
@@ -8,6 +8,7 @@ _BSD_ENDIAN_MK_=1
 .if ${MACHINE_ARCH} == "alpha" || \
     ${MACHINE_ARCH} == "arm" || \
     ${MACHINE_ARCH} == "earm" || \
+    ${MACHINE_ARCH} == "earmhf" || \
     ${MACHINE_ARCH} == "i386" || \
     ${MACHINE_ARCH} == "ia64" || \
     ${MACHINE_ARCH} == "vax" || \
