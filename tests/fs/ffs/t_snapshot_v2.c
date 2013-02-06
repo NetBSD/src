63ae5269d1abe77f32e663e2067a95cb005da239
hannken <hannken@NetBSD.org>
2013-02-06 09:05:01 +0000

    Test taking a snapshot from a stressed file system. Checks snapshot meta data only with fsck.
    OK: Antti Kantee <pooka@netbsd.org>

--- tests/fs/ffs/t_snapshot_v2.c
+++ tests/fs/ffs/t_snapshot_v2.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: t_snapshot_v2.c,v 1.1 2010/11/19 12:36:49 pooka Exp $	*/
+/*	$NetBSD: t_snapshot_v2.c,v 1.2 2013/02/06 09:05:01 hannken Exp $	*/
 
 #include <sys/types.h>
 #include <sys/mount.h>
@@ -19,6 +19,7 @@
 
 #define IMGNAME "ffs.img"
 #define NEWFS "newfs -F -s 10000 -O 2 " IMGNAME
+#define FSCK "fsck_ffs -fn -F"
 #define BAKNAME "/mnt/le_snapp"
 
 static void
