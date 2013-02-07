8135bbf5bb8ce676ffc959542d3d125d7224587c
christos <christos@NetBSD.org>
2013-02-07 20:30:29 +0000

    add shaX and rmd160 headers, same as mdX

---
+++ tools/compat/sha2.h
@@ -0,0 +1,5 @@
+/*	$NetBSD: sha2.h,v 1.1 2013/02/07 20:30:29 christos Exp $	*/
+
+/* We unconditionally use the NetBSD sha2 in libnbcompat. */
+#include "nbtool_config.h"
+#include "../../sys/sys/md5.h"
