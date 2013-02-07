2f86154a1e214c4ce3cbca244e8db814bc081aff
christos <christos@NetBSD.org>
2013-02-07 20:31:52 +0000

    yes, but include the right file

--- tools/compat/sha2.h
+++ tools/compat/sha2.h
@@ -1,5 +1,5 @@
-/*	$NetBSD: sha2.h,v 1.1 2013/02/07 20:30:29 christos Exp $	*/
+/*	$NetBSD: sha2.h,v 1.2 2013/02/07 20:31:52 christos Exp $	*/
 
 /* We unconditionally use the NetBSD sha2 in libnbcompat. */
 #include "nbtool_config.h"
-#include "../../sys/sys/md5.h"
+#include "../../sys/sys/sha2.h"
