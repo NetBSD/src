bdd1768e9350a6aaf53c2113936b582f47d8662e
riz <riz@NetBSD.org>
2013-02-08 19:31:18 +0000

    Pull up following revision(s) (requested by rmind in ticket #782): 	sys/rump/include/machine/intr.h: revision 1.19 	sys/kern/subr_pserialize.c: revision 1.6 	sys/kern/kern_softint.c: revision 1.39 - softint_dispatch: perform pserialize(9) switchpoint when softintr processing   finishes (without blocking).  Problem reported by hannken@, thanks! - pserialize_read_enter: use splsoftserial(), not splsoftclock(). - pserialize_perform: add xcall(9) barrier as interrupts may be coalesced. Provide splsoftserial. GRRR RUMP

--- sys/rump/include/machine/intr.h
+++ sys/rump/include/machine/intr.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: intr.h,v 1.18 2011/03/21 16:41:08 pooka Exp $	*/
+/*	$NetBSD: intr.h,v 1.18.16.1 2013/02/08 19:31:18 riz Exp $	*/
 
 /*
  * Copyright (c) 2009, 2010 Antti Kantee.  All Rights Reserved.
@@ -47,6 +47,7 @@ makeiplcookie(ipl_t ipl)
 
 #define spllower(x) ((void)x)
 #define splraise(x) 0
+#define splsoftserial() 0
 #define splsoftnet() 0
 #define splsoftclock() 0
 #define splhigh() 0
