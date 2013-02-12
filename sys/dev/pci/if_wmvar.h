6a593fa67cb83bf1b3b31a61af2db9f6e9a1a257
msaitoh <msaitoh@NetBSD.org>
2013-02-12 03:11:43 +0000

    Use 82580(and I350) specific PHY read/write functions. Fixes PR#47542.

--- sys/dev/pci/if_wmvar.h
+++ sys/dev/pci/if_wmvar.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: if_wmvar.h,v 1.13 2012/05/25 23:37:38 msaitoh Exp $	*/
+/*	$NetBSD: if_wmvar.h,v 1.14 2013/02/12 03:11:43 msaitoh Exp $	*/
 
 /*
  * Copyright (c) 2001, 2002, 2003, 2004 Wasabi Systems, Inc.
@@ -140,7 +140,8 @@ typedef enum {
 	WMPHY_BM,
 	WMPHY_82577,
 	WMPHY_82578,
-	WMPHY_82579
+	WMPHY_82579,
+	WMPHY_82580
 } wm_phy_type;
 
 
