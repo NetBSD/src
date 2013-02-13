9f9ac3cb83012dc07355ce5d1f32fcd939bbfb65
hannken <hannken@NetBSD.org>
2013-02-13 14:03:48 +0000

    Make the spec_node table implementation private to spec_vnops.c.
    To retrieve a spec_node, two new lookup functions (by device or by mount)
    are implemented.  Both return a referenced vnode, for an opened block device
    the opened vnode is returned so further diagnostic checks "vp == ... sd_bdevvp"
    will not fire.  Otherwise any vnode matching the criteria gets returned.
    
    No objections on tech-kern.
    
    Welcome to 6.99.17

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.424 2012/12/20 08:03:44 hannken Exp $	*/
+/*	$NetBSD: param.h,v 1.425 2013/02/13 14:03:49 hannken Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -63,7 +63,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	699001600	/* NetBSD 6.99.16 */
+#define	__NetBSD_Version__	699001700	/* NetBSD 6.99.17 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
