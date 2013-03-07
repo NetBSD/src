014b4da578da091eb652dfb5daa8ed66cdf69dc9
pooka <pooka@NetBSD.org>
2013-03-07 18:50:17 +0000

    *** empty log message ***

--- sys/rump/librump/rumpkern/rumpkern_if_priv.h
+++ sys/rump/librump/rumpkern/rumpkern_if_priv.h
@@ -1,9 +1,9 @@
-/*	$NetBSD: rumpkern_if_priv.h,v 1.12 2011/01/02 12:53:13 pooka Exp $	*/
+/*	$NetBSD: rumpkern_if_priv.h,v 1.13 2013/03/07 18:50:17 pooka Exp $	*/
 
 /*
  * Automatically generated.  DO NOT EDIT.
- * from: NetBSD: rumpkern.ifspec,v 1.10 2011/01/02 12:52:25 pooka Exp 
- * by:   NetBSD: makerumpif.sh,v 1.5 2010/09/01 19:32:11 pooka Exp 
+ * from: NetBSD: rumpkern.ifspec,v 1.11 2013/03/07 18:49:13 pooka Exp 
+ * by:   NetBSD: makerumpif.sh,v 1.6 2013/02/14 10:54:54 pooka Exp 
  */
 
 int rump_getversion(void);
@@ -21,4 +21,5 @@ int rump_lwproc_newlwp(pid_t);
 void rump_lwproc_switch(struct lwp *);
 void rump_lwproc_releaselwp(void);
 struct lwp * rump_lwproc_curlwp(void);
+void rump_lwproc_sysent_usenative(void);
 void rump_allbetsareoff_setid(pid_t, int);
