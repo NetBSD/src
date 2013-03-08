faea3b9e871db7997d225ab62e4c024259854f68
martin <martin@NetBSD.org>
2013-03-08 17:01:54 +0000

    Rename testprogram and make it more general by adding other testcases. One commented out, I didn't manage to get all signal handling correct for now.

--- tests/lib/libc/sys/t_sigtimedwait.c
+++ tests/lib/libc/sys/t_sigtimedwait.c
@@ -1,4 +1,4 @@
-/* $NetBSD: t_sigtimedwait_pr_47625.c,v 1.1 2013/03/08 10:33:51 martin Exp $ */
+/* $NetBSD: t_sigtimedwait.c,v 1.1 2013/03/08 17:01:54 martin Exp $ */
 
 /*-
  * Copyright (c) 2011 The NetBSD Foundation, Inc.
@@ -27,7 +27,7 @@
  */
 
 #include <sys/cdefs.h>
-__RCSID("$NetBSD: t_sigtimedwait_pr_47625.c,v 1.1 2013/03/08 10:33:51 martin Exp $");
+__RCSID("$NetBSD: t_sigtimedwait.c,v 1.1 2013/03/08 17:01:54 martin Exp $");
 
 #include <sys/time.h>
 #include <errno.h>
@@ -37,16 +37,16 @@ __RCSID("$NetBSD: t_sigtimedwait_pr_47625.c,v 1.1 2013/03/08 10:33:51 martin Exp
 #include <atf-c.h>
 
 
-ATF_TC(sigtimedwait);
+ATF_TC(sigtimedwait_all0timeout);
 
-ATF_TC_HEAD(sigtimedwait, tc)
+ATF_TC_HEAD(sigtimedwait_all0timeout, tc)
 {
 	atf_tc_set_md_var(tc, "timeout", "10");
 	atf_tc_set_md_var(tc, "descr", "Test for PR kern/47625: sigtimedwait"
 	    " with a timeout value of all zero should return imediately");
 }
 
-ATF_TC_BODY(sigtimedwait, tc)
+ATF_TC_BODY(sigtimedwait_all0timeout, tc)
 {
 	sigset_t block;
 	struct timespec ts;
@@ -61,10 +61,66 @@ ATF_TC_BODY(sigtimedwait, tc)
 	ATF_REQUIRE_ERRNO(EAGAIN, errno);
 }
 
+#if 0
+
+ATF_TC(sigtimedwait_NULL_timeout);
+
+ATF_TC_HEAD(sigtimedwait_NULL_timeout, tc)
+{
+	atf_tc_set_md_var(tc, "timeout", "10");
+	atf_tc_set_md_var(tc, "descr", "Test sigtimedwait() without timeout");
+}
+
+ATF_TC_BODY(sigtimedwait_NULL_timeout, tc)
+{
+	sigset_t sig;
+	siginfo_t info;
+	struct itimerval it;
+	int r;
+
+	/* arrange for a SIGALRM signal in a few seconds */
+	memset(&it, 0, sizeof it);
+	it.it_interval.tv_sec = 5;
+	ATF_REQUIRE(setitimer(ITIMER_REAL, &it, NULL) == 0);
+
+	/* wait without timeout */
+	sigemptyset(&sig);
+	sigaddset(&sig, SIGALRM);
+	(void)sigprocmask(SIG_UNBLOCK, &sig, NULL);
+	r = sigtimedwait(&sig, &info, NULL);
+	ATF_REQUIRE(r == SIGALRM);
+}
+#endif
+
+ATF_TC(sigtimedwait_small_timeout);
+
+ATF_TC_HEAD(sigtimedwait_small_timeout, tc)
+{
+	atf_tc_set_md_var(tc, "timeout", "15");
+	atf_tc_set_md_var(tc, "descr", "Test sigtimedwait with a small "
+	    "timeout");
+}
+
+ATF_TC_BODY(sigtimedwait_small_timeout, tc)
+{
+	sigset_t block;
+	struct timespec ts;
+	siginfo_t info;
+	int r;
+
+	sigemptyset(&block);
+	ts.tv_sec = 5;
+	ts.tv_nsec = 0;
+	r = sigtimedwait(&block, &info, &ts);
+	ATF_REQUIRE(r == -1);
+	ATF_REQUIRE_ERRNO(EAGAIN, errno);
+}
 
 ATF_TP_ADD_TCS(tp)
 {
-	ATF_TP_ADD_TC(tp, sigtimedwait);
+	ATF_TP_ADD_TC(tp, sigtimedwait_all0timeout);
+//	ATF_TP_ADD_TC(tp, sigtimedwait_NULL_timeout);
+	ATF_TP_ADD_TC(tp, sigtimedwait_small_timeout);
 
 	return atf_no_error();
 }
