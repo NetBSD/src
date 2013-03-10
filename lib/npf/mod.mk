9477fd24246c873314daf80953d9c3243cdfaf24
rmind <rmind@NetBSD.org>
2013-03-10 21:45:46 +0000

    tabify

--- lib/npf/mod.mk
+++ lib/npf/mod.mk
@@ -1,25 +1,26 @@
-#	$NetBSD: mod.mk,v 1.1 2013/03/10 21:41:05 christos Exp $
+#	$NetBSD: mod.mk,v 1.2 2013/03/10 21:45:46 rmind Exp $
 
-WARNS=	5
-MKLINT=	no
-LIBISMODULE= yes
-LIBROOTDIR=/lib
+WARNS=		5
+MKLINT=		no
+
+LIBISMODULE=	yes
+LIBROOTDIR=	/lib
 
 .if exists(${.CURDIR}/../../Makefile.inc)
 .include "${.CURDIR}/../../Makefile.inc"
 .endif
 
 .if defined(MLIBDIR)
-LIBDIR=         ${LIBROOTDIR}/${MLIBDIR}/npf
-SHLIBDIR=       ${LIBROOTDIR}/${MLIBDIR}/npf
+LIBDIR=		${LIBROOTDIR}/${MLIBDIR}/npf
+SHLIBDIR=	${LIBROOTDIR}/${MLIBDIR}/npf
 SHLIBINSTALLDIR=${LIBROOTDIR}/${MLIBDIR}/npf
 .else
-LIBDIR=         ${LIBROOTDIR}/npf
-SHLIBDIR=       ${LIBROOTDIR}/npf
+LIBDIR=		${LIBROOTDIR}/npf
+SHLIBDIR=	${LIBROOTDIR}/npf
 SHLIBINSTALLDIR=${LIBROOTDIR}/npf
 .endif
 
-LIB=${MOD}
-SRCS=npf${MOD}.c
+LIB=		${MOD}
+SRCS=		npf${MOD}.c
 
 .include <bsd.lib.mk>
