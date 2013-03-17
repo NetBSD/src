271b62868e709655ba12bee8f81a805ff874fe3b
jmmv <jmmv@NetBSD.org>
2013-03-17 03:00:05 +0000

    Mark some long-standing failures as known failures.
    The offending tests are these:
    - t_filter_exec: f26, f27.
    - t_filter_parse: i17.
    - t_nat_exec: n12.
    
    These tests are confirmed to fail in NetBSD/current under amd64, i386 and
    sparc as reported by the continuous testing systems.

--- tests/ipf/t_nat_exec.sh
+++ tests/ipf/t_nat_exec.sh
@@ -1,4 +1,4 @@
-# $NetBSD: t_nat_exec.sh,v 1.13 2012/12/02 08:52:06 pgoyette Exp $
+# $NetBSD: t_nat_exec.sh,v 1.14 2013/03/17 03:00:05 jmmv Exp $
 #
 # Copyright (c) 2008, 2010 The NetBSD Foundation, Inc.
 # All rights reserved.
@@ -71,7 +71,7 @@ test_case n8 nattest hex hex -T update_ipid=0
 test_case n9 nattest hex hex -T update_ipid=0
 test_case n10 nattest hex hex -T update_ipid=0
 test_case n11 nattest text text
-test_case n12 nattest hex hex -T update_ipid=0 -v
+failing_test_case n12 nattest "Known to be broken" hex hex -T update_ipid=0 -v
 test_case n13 nattest text text
 test_case n14 nattest text text
 test_case n15 nattest text text -T update_ipid=0
