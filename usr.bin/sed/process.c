7f59abe0bb751aa44eb69fe0241e55649fdb7dc0
uwe <uwe@NetBSD.org>
2013-03-17 21:02:54 +0000

    The change ("c") command should start a new cycle.
    Apply one line patch I posted in PR #45981 and document this in the
    manual page.

--- usr.bin/sed/process.c
+++ usr.bin/sed/process.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: process.c,v 1.38 2009/04/13 07:29:55 lukem Exp $	*/
+/*	$NetBSD: process.c,v 1.39 2013/03/17 21:02:54 uwe Exp $	*/
 
 /*-
  * Copyright (c) 1992, 1993, 1994
@@ -76,7 +76,7 @@
 #if 0
 static char sccsid[] = "@(#)process.c	8.6 (Berkeley) 4/20/94";
 #else
-__RCSID("$NetBSD: process.c,v 1.38 2009/04/13 07:29:55 lukem Exp $");
+__RCSID("$NetBSD: process.c,v 1.39 2013/03/17 21:02:54 uwe Exp $");
 #endif
 #endif /* not lint */
 
@@ -168,7 +168,7 @@ redirect:
 				psl = 0;
 				if (cp->a2 == NULL || lastaddr)
 					(void)printf("%s", cp->t);
-				break;
+				goto new;
 			case 'd':
 				pd = 1;
 				goto new;
