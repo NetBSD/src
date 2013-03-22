5588ac6a999148e0c78db0b19f85269b57ef030a
christos <christos@NetBSD.org>
2013-03-22 01:24:46 +0000

    move the code to expand variables in one place and use it to expand variables inside conditionals. still conditionals with variables don't work.

--- external/bsd/mdocml/dist/read.c
+++ external/bsd/mdocml/dist/read.c
@@ -354,19 +354,8 @@ mparse_buf_r(struct mparse *curp, struct buf blk, int start)
 			}
 
 			if ('\\' == blk.buf[i] && 'n' == blk.buf[i + 1]) {
-				int j, k;
-				i += 2;
-				if ('(' == blk.buf[i]) /* ) */
-					i++;
-				resize_buf(&ln, 256);
-				for (j = i, k = pos; i < j + 256 
-				    && i < (int)blk.sz
-				    && !isspace((unsigned char)blk.buf[i]);)
-				    ln.buf[k++] = blk.buf[i++];
-
-				ln.buf[k] = '\0';
-				pos += roff_expand_nr(curp->roff,
-				    ln.buf + pos, ln.buf + pos, 256);
+				roff_expand_nr(curp->roff,
+				    blk.buf, &i, blk.sz, &ln.buf, &pos, &ln.sz);
 			}
 
 			/*
