85fc5176f6a91d2817060ca94ce0248de2811038
joerg <joerg@NetBSD.org>
2013-03-24 12:15:50 +0000

    Rename __is_signed to __is_signed_val to avoid conflict with a type trait in Clang.

--- external/gpl3/gcc/dist/libstdc++-v3/include/bits/stl_algobase.h
+++ external/gpl3/gcc/dist/libstdc++-v3/include/bits/stl_algobase.h
@@ -929,8 +929,8 @@ _GLIBCXX_BEGIN_NAMESPACE(std)
       typedef typename iterator_traits<_II2>::value_type _ValueType2;
       const bool __simple =
 	(__is_byte<_ValueType1>::__value && __is_byte<_ValueType2>::__value
-	 && !__gnu_cxx::__numeric_traits<_ValueType1>::__is_signed
-	 && !__gnu_cxx::__numeric_traits<_ValueType2>::__is_signed
+	 && !__gnu_cxx::__numeric_traits<_ValueType1>::__is_signed_val
+	 && !__gnu_cxx::__numeric_traits<_ValueType2>::__is_signed_val
 	 && __is_pointer<_II1>::__value
 	 && __is_pointer<_II2>::__value);
 
