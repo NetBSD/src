025b6c272fbc0a70dfdb4fe90652267c00198241
christos <christos@NetBSD.org>
2013-03-24 18:41:59 +0000

    Export all the necessary functions via the registry API instead of playing namespace games with cpp. Remove the BIND9 ifdef. Now the bind programs don't need a special build of libisc.

--- external/bsd/bind/dist/lib/isc/task_p.h
+++ external/bsd/bind/dist/lib/isc/task_p.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: task_p.h,v 1.3 2012/06/05 00:42:32 christos Exp $	*/
+/*	$NetBSD: task_p.h,v 1.4 2013/03/24 18:42:00 christos Exp $	*/
 
 /*
  * Copyright (C) 2004, 2005, 2007, 2009, 2011, 2012  Internet Systems Consortium, Inc. ("ISC")
@@ -24,18 +24,15 @@
 
 /*! \file */
 
-#if defined(BIND9) && defined(ISC_PLATFORM_USETHREADS)
 void
 isc__taskmgr_pause(isc_taskmgr_t *taskmgr);
 
 void
 isc__taskmgr_resume(isc_taskmgr_t *taskmgr);
-#else
 isc_boolean_t
 isc__taskmgr_ready(isc_taskmgr_t *taskmgr);
 
 isc_result_t
 isc__taskmgr_dispatch(isc_taskmgr_t *taskmgr);
-#endif /* !BIND9 || !ISC_PLATFORM_USETHREADS */
 
 #endif /* ISC_TASK_P_H */
