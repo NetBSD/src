431112947ab402b1f71ad6ab856e7095acc60733
riz <riz@NetBSD.org>
2013-03-31 22:24:10 +0000

    Welcome to 6.1_RC3!

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.408.2.9 2013/03/15 23:26:27 riz Exp $	*/
+/*	$NetBSD: param.h,v 1.408.2.10 2013/03/31 22:24:10 riz Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -63,7 +63,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	601000000	/* NetBSD 6.1_RC2 */
+#define	__NetBSD_Version__	601000000	/* NetBSD 6.1_RC3 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
