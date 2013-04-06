c74573e1711ed4706175f8fe490ae15c2de8dacd
christos <christos@NetBSD.org>
2013-04-06 17:57:45 +0000

    import tcpdump 4.3.0

--- external/bsd/tcpdump/dist/oui.h
+++ external/bsd/tcpdump/dist/oui.h
@@ -1,4 +1,4 @@
-/* @(#) Header: /tcpdump/master/tcpdump/oui.h,v 1.8 2008-01-09 09:40:47 hannes Exp (LBL) */
+/* @(#) Header: /tcpdump/master/tcpdump/oui.h,v 1.8 2008-01-09 09:40:47 hannes Exp  (LBL) */
 /* 
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that: (1) source code
@@ -30,6 +30,7 @@ extern const struct tok smi_values[];
 #define OUI_IEEE_8021_PRIVATE 0x0080c2      /* IEEE 802.1 Organisation Specific - Annex F */
 #define OUI_IEEE_8023_PRIVATE 0x00120f      /* IEEE 802.3 Organisation Specific - Annex G */
 #define OUI_TIA         0x0012bb        /* TIA - Telecommunications Industry Association - ANSI/TIA-1057- 2006 */
+#define OUI_DCBX        0x001B21        /* DCBX */
 
 /*
  * These are SMI Network Management Private Enterprise Codes for
