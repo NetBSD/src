8183e31e23297991ff6a5873120848c4616eae41
skrll <skrll@NetBSD.org>
2013-04-14 15:11:52 +0000

    Trailing whitespace

--- sys/arch/arm/broadcom/bcm2835_vcaudioreg.h
+++ sys/arch/arm/broadcom/bcm2835_vcaudioreg.h
@@ -44,7 +44,7 @@ typedef enum
    VC_AUDIO_MSG_TYPE_RESULT,              // Generic result
    VC_AUDIO_MSG_TYPE_COMPLETE,              // playback of samples complete
    VC_AUDIO_MSG_TYPE_CONFIG,                 // Configure
-   VC_AUDIO_MSG_TYPE_CONTROL,                 // control 
+   VC_AUDIO_MSG_TYPE_CONTROL,                 // control
    VC_AUDIO_MSG_TYPE_OPEN,                 //  open
    VC_AUDIO_MSG_TYPE_CLOSE,                 // close/shutdown
    VC_AUDIO_MSG_TYPE_START,                 // start output (i.e. resume)
