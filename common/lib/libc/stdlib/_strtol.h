a67fde035671e323b0b50374f03950beac2f97f2
joerg <joerg@NetBSD.org>
2013-04-16 21:44:06 +0000

    Add strtol_l and friends. Switch _citrus_bcs_strtol to use plain strtol_l unless in tools mode. Add note to retire the BCS code on the next libc major bump.

--- common/lib/libc/stdlib/_strtol.h
+++ common/lib/libc/stdlib/_strtol.h
@@ -1,4 +1,4 @@
-/* $NetBSD: _strtol.h,v 1.4 2013/04/16 19:34:57 joerg Exp $ */
+/* $NetBSD: _strtol.h,v 1.5 2013/04/16 21:44:06 joerg Exp $ */
 
 /*-
  * Copyright (c) 1990, 1993
@@ -41,9 +41,19 @@
  *      __INT_MIN : lower limit of the return type
  *      __INT_MAX : upper limit of the return type
  */
-
+#if defined(_KERNEL) || defined(_STANDALONE) || defined(HAVE_NBTOOL_CONFIG_H)
 __INT
 _FUNCNAME(const char *nptr, char **endptr, int base)
+#else
+#include <locale.h>
+#include "setlocale_local.h"
+#define INT_FUNCNAME_(pre, name, post)	pre ## name ## post
+#define INT_FUNCNAME(pre, name, post)	INT_FUNCNAME_(pre, name, post)
+
+static __INT
+INT_FUNCNAME(_int_, _FUNCNAME, _l)(const char *nptr, char **endptr,
+				   int base, locale_t loc)
+#endif
 {
 	const char *s;
 	__INT acc, cutoff;
@@ -72,9 +82,15 @@ _FUNCNAME(const char *nptr, char **endptr, int base)
 	 * assume decimal; if base is already 16, allow 0x.
 	 */
 	s = nptr;
+#if defined(_KERNEL) || defined(_STANDALONE) || defined(HAVE_NBTOOL_CONFIG_H)
 	do {
 		c = *s++;
 	} while (isspace(c));
+#else
+	do {
+		c = *s++;
+	} while (isspace_l(c, loc));
+#endif
 	if (c == '-') {
 		neg = 1;
 		c = *s++;
@@ -169,3 +185,19 @@ _FUNCNAME(const char *nptr, char **endptr, int base)
 		*endptr = __UNCONST(any ? s - 1 : nptr);
 	return(acc);
 }
+
+#if !defined(_KERNEL) && !defined(_STANDALONE) && !defined(HAVE_NBTOOL_CONFIG_H)
+__INT
+_FUNCNAME(const char *nptr, char **endptr, int base)
+{
+	return INT_FUNCNAME(_int_, _FUNCNAME, _l)(nptr, endptr, base, *_current_locale());
+}
+
+__INT
+INT_FUNCNAME(, _FUNCNAME, _l)(const char *nptr, char **endptr, int base, locale_t loc)
+{
+	if (loc == NULL)
+		loc = _C_locale;
+	return INT_FUNCNAME(_int_, _FUNCNAME, _l)(nptr, endptr, base, loc);
+}
+#endif
