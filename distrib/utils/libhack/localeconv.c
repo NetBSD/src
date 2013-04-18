5921261ae82eb5d2406f28a0f582ebdffb73c308
joerg <joerg@NetBSD.org>
2013-04-18 16:25:55 +0000

    Override localeconv_l as well, when providing an alternative implementation of localeconv.

--- distrib/utils/libhack/localeconv.c
+++ distrib/utils/libhack/localeconv.c
@@ -1,10 +1,11 @@
-/*	$NetBSD: localeconv.c,v 1.3 2003/07/26 17:07:36 salo Exp $	*/
+/*	$NetBSD: localeconv.c,v 1.4 2013/04/18 16:25:55 joerg Exp $	*/
 
 /*
  * Written by J.T. Conklin <jtc@NetBSD.org>.
  * Public domain.
  */
 
+#define _NETBSD_SOURCE
 #include <sys/cdefs.h>
 #include <sys/localedef.h>
 #include <locale.h>
@@ -20,7 +21,7 @@
  * Fixed in the "C" locale.
  */
 struct lconv *
-localeconv()
+localeconv(void)
 {
     static struct lconv ret = {
 	/* char	*decimal_point */ ".",
@@ -44,4 +45,11 @@ localeconv()
     };
 
     return (&ret);
+}
+
+/* ARGSUSED */
+struct lconv *
+localeconv_l(locale_t loc)
+{
+	return localeconv();
 }
