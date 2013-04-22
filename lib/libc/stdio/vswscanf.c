cc8a6f2a21c6fd2a660419a10f30978a7943f1a9
joerg <joerg@NetBSD.org>
2013-04-22 19:33:53 +0000

    Add new line at the end.

--- lib/libc/stdio/vswscanf.c
+++ lib/libc/stdio/vswscanf.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: vswscanf.c,v 1.10 2013/04/19 23:32:17 joerg Exp $	*/
+/*	$NetBSD: vswscanf.c,v 1.11 2013/04/22 19:33:53 joerg Exp $	*/
 
 /*-
  * Copyright (c) 1990, 1993
@@ -42,7 +42,7 @@
 static char sccsid[] = "@(#)vsscanf.c	8.1 (Berkeley) 6/4/93";
 __FBSDID("$FreeBSD: src/lib/libc/stdio/vswscanf.c,v 1.3 2004/04/07 09:55:05 tjr Exp $");
 #else
-__RCSID("$NetBSD: vswscanf.c,v 1.10 2013/04/19 23:32:17 joerg Exp $");
+__RCSID("$NetBSD: vswscanf.c,v 1.11 2013/04/22 19:33:53 joerg Exp $");
 #endif
 #endif /* LIBC_SCCS and not lint */
 
