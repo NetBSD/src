5e6dc7b406d58d18f33ccbffa2a5db26b6198c40
skrll <skrll@NetBSD.org>
2013-04-29 09:32:38 +0000

    libc was missing symbols for a while there. Re-run bintuils mknative against a good libc.

--- external/gpl3/binutils/usr.bin/common/arch/earmeb/config.h
+++ external/gpl3/binutils/usr.bin/common/arch/earmeb/config.h
@@ -145,7 +145,7 @@
 #define HAVE_UNISTD_H 1
 
 /* Define to 1 if you have the `utimes' function. */
-/* #undef HAVE_UTIMES */
+#define HAVE_UTIMES 1
 
 /* Define to 1 if you have the <zlib.h> header file. */
 #define HAVE_ZLIB_H 1
