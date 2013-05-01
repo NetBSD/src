52027de0d7dda5084ec797c9e94b62ef2670f5fc
kiyohara <kiyohara@NetBSD.org>
2013-05-01 13:11:59 +0000

    Support to build for epoc32 userland.

---
+++ sys/arch/epoc32/include/ieeefp.h
@@ -0,0 +1,3 @@
+/*	$NetBSD: ieeefp.h,v 1.1 2013/05/01 13:11:59 kiyohara Exp $	*/
+
+#include <arm/ieeefp.h>
---
+++ sys/arch/epoc32/include/setjmp.h
@@ -0,0 +1,3 @@
+/*	$NetBSD: setjmp.h,v 1.1 2013/05/01 13:12:00 kiyohara Exp $	*/
+
+#include <arm/setjmp.h>
