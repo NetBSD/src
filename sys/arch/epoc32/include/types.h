26c63d2ccecbe8cc67d22aedd64dcdcc65aeab14
matt <matt>
2013-05-02 03:56:38 +0000

    This change arm, armeb, earm, earmeb, earmhf, earmhfeb so all builds that share a MACHINE_ARCH for userland so that except for etc, will produce identical sets.
    usr/include/machine now points to usr/include/arm

--- sys/arch/epoc32/include/types.h
+++ sys/arch/epoc32/include/types.h
@@ -1,8 +1,10 @@
-/*	$NetBSD: types.h,v 1.1 2013/04/28 12:11:25 kiyohara Exp $	*/
+/*	$NetBSD: types.h,v 1.2 2013/05/02 03:56:40 matt Exp $	*/
 
 #ifndef _EPOC32_TYPES_H_
 #define	_EPOC32_TYPES_H_
 
 #include <arm/arm32/types.h>
+
+#define __HAVE_OLD_DISKLABEL
 
 #endif	/* _EPOC32_TYPES_H_ */
