dc8956376903bd3cfa3f93f1d6ffefe12366bc81
matt <matt>
2013-05-07 20:42:45 +0000

    Make all ports use a common disklabel.h with MAXPARTITIONS set to 16. Only RAW_PART varies between ports.

--- sys/arch/hpcarm/include/types.h
+++ sys/arch/hpcarm/include/types.h
@@ -1,4 +1,4 @@
-/* $NetBSD: types.h,v 1.14 2010/07/17 15:13:18 tsutsui Exp $ */
+/* $NetBSD: types.h,v 1.15 2013/05/07 20:42:46 matt Exp $ */
 
 #ifndef _HPCARM_TYPES_H_
 #define	_HPCARM_TYPES_H_
@@ -6,7 +6,6 @@
 #include <arm/arm32/types.h>
 
 #define	__HAVE_DEVICE_REGISTER
-#define	__HAVE_OLD_DISKLABEL
 #define	__GENERIC_SOFT_INTERRUPTS_ALL_LEVELS
 
 #endif
