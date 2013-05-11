b8fd7729d3dd881c66259bfb58dd46129ea79dd7
riz <riz>
2013-05-11 21:43:06 +0000

    Pull up following revision(s) (requested by msaitoh in ticket #883): 	sys/kern/kern_drvctl.c: revision 1.34 Fix memory leak on the following cases when device attached or detached:   - No one open drvctl.   - kmem_alloc() failed in devmon_insert(). XXX pullup to both netbsd-5 and netbsd-6.

--- sys/kern/kern_drvctl.c
+++ sys/kern/kern_drvctl.c
@@ -1,4 +1,4 @@
-/* $NetBSD: kern_drvctl.c,v 1.32 2011/08/31 18:31:02 plunky Exp $ */
+/* $NetBSD: kern_drvctl.c,v 1.32.8.1 2013/05/11 21:43:06 riz Exp $ */
 
 /*
  * Copyright (c) 2004
@@ -27,7 +27,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: kern_drvctl.c,v 1.32 2011/08/31 18:31:02 plunky Exp $");
+__KERNEL_RCSID(0, "$NetBSD: kern_drvctl.c,v 1.32.8.1 2013/05/11 21:43:06 riz Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -115,6 +115,7 @@ devmon_insert(const char *event, prop_dictionary_t ev)
 	mutex_enter(&drvctl_lock);
 
 	if (drvctl_nopen == 0) {
+		prop_object_release(ev);
 		mutex_exit(&drvctl_lock);
 		return;
 	}
@@ -128,6 +129,7 @@ devmon_insert(const char *event, prop_dictionary_t ev)
 
 	dce = kmem_alloc(sizeof(*dce), KM_SLEEP);
 	if (dce == NULL) {
+		prop_object_release(ev);
 		mutex_exit(&drvctl_lock);
 		return;
 	}
