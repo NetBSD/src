57945d36e5b4f8689b0d7edb5ed7f1297e75a405
kiyohara <kiyohara@NetBSD.org>
2013-05-14 14:03:53 +0000

    Add quirk for IODATA2 USB2SC. This works target 0 only.  However supports 4 targets in spec.

--- sys/dev/usb/umass_quirks.c
+++ sys/dev/usb/umass_quirks.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: umass_quirks.c,v 1.94 2012/08/24 12:20:02 drochner Exp $	*/
+/*	$NetBSD: umass_quirks.c,v 1.95 2013/05/14 14:03:53 kiyohara Exp $	*/
 
 /*
  * Copyright (c) 2001, 2004 The NetBSD Foundation, Inc.
@@ -32,7 +32,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: umass_quirks.c,v 1.94 2012/08/24 12:20:02 drochner Exp $");
+__KERNEL_RCSID(0, "$NetBSD: umass_quirks.c,v 1.95 2013/05/14 14:03:53 kiyohara Exp $");
 
 #ifdef _KERNEL_OPT
 #include "opt_umass.h"
@@ -333,6 +333,14 @@ Static const struct umass_quirk umass_quirks[] = {
 	  UMASS_WPROTO_UNSPEC, UMASS_CPROTO_UNSPEC,
 	  0,
 	  PQUIRK_NODOORLOCK,
+	  UMATCH_VENDOR_PRODUCT,
+	  NULL, NULL
+	},
+
+	{ { USB_VENDOR_IODATA2, USB_PRODUCT_IODATA2_USB2SC },
+	  UMASS_WPROTO_UNSPEC, UMASS_CPROTO_UNSPEC,
+	  0,
+	  0,
 	  UMATCH_VENDOR_PRODUCT,
 	  NULL, NULL
 	},
