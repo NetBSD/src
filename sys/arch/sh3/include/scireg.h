0e5f7b65bb5b47017761243b2ee32d61320efdc9
tsutsui <tsutsui@NetBSD.org>
2013-05-14 13:53:47 +0000

    Add a definition of SCSCMR1, smart card mode register available on 7750 etc.

--- sys/arch/sh3/include/scireg.h
+++ sys/arch/sh3/include/scireg.h
@@ -1,4 +1,4 @@
-/* $NetBSD: scireg.h,v 1.9 2009/04/30 05:19:38 nonaka Exp $ */
+/* $NetBSD: scireg.h,v 1.10 2013/05/14 13:53:47 tsutsui Exp $ */
 
 /*-
  * Copyright (C) 1999 SAITOH Masanobu.  All rights reserved.
@@ -56,6 +56,7 @@
 #define	SHREG_SCTDR	(*(volatile unsigned char *)	0xffe0000c)
 #define	SHREG_SCSSR	(*(volatile unsigned char *)	0xffe00010)
 #define	SHREG_SCRDR	(*(volatile unsigned char *)	0xffe00014)
+#define	SHREG_SCSCMR	(*(volatile unsigned char *)	0xffe00018)
 #define	SHREG_SCSPTR	(*(volatile unsigned char *)	0xffe0001c)
 
 #endif
