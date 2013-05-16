69371a9f53a2cc3212ff2f516499706d3ec8d84b
christos <christos@NetBSD.org>
2013-05-16 19:06:44 +0000

    Complete the dosparts -> mbrparts conversion. Only x86k new uses dosparts because it also uses struct dos_partition.

--- sys/arch/hpcmips/include/disklabel.h
+++ sys/arch/hpcmips/include/disklabel.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: disklabel.h,v 1.10 2011/08/30 12:39:55 bouyer Exp $	*/
+/*	$NetBSD: disklabel.h,v 1.11 2013/05/16 19:06:44 christos Exp $	*/
 
 /*
  * Copyright (c) 1994 Christopher G. Demetriou
@@ -67,7 +67,7 @@
 #include <sys/dkbad.h>
 #endif /* HAVE_NBTOOL_CONFIG_H */
 struct cpu_disklabel {
-	struct mbr_partition dosparts[MBR_PART_COUNT];
+	struct mbr_partition mbrparts[MBR_PART_COUNT];
 #define __HAVE_DISKLABEL_DKBAD
 	struct dkbad bad;
 };
