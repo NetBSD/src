b2c25544cae1bda041a8a85742f4d3f6f64c029c
reinoud <reinoud@NetBSD.org>
2013-05-22 13:34:31 +0000

    Including missing include file <sys/ioctl.h> that broke compilation.

--- sys/arch/usermode/usermode/thunk.c
+++ sys/arch/usermode/usermode/thunk.c
@@ -1,4 +1,4 @@
-/* $NetBSD: thunk.c,v 1.82 2012/07/02 12:17:12 reinoud Exp $ */
+/* $NetBSD: thunk.c,v 1.83 2013/05/22 13:34:31 reinoud Exp $ */
 
 /*-
  * Copyright (c) 2011 Jared D. McNeill <jmcneill@invisible.ca>
@@ -28,7 +28,7 @@
 
 #include <sys/cdefs.h>
 #ifdef __NetBSD__
-__RCSID("$NetBSD: thunk.c,v 1.82 2012/07/02 12:17:12 reinoud Exp $");
+__RCSID("$NetBSD: thunk.c,v 1.83 2013/05/22 13:34:31 reinoud Exp $");
 #endif
 
 #include <sys/types.h>
@@ -40,6 +40,7 @@ __RCSID("$NetBSD: thunk.c,v 1.82 2012/07/02 12:17:12 reinoud Exp $");
 #include <sys/socket.h>
 #include <sys/audioio.h>
 #include <sys/shm.h>
+#include <sys/ioctl.h>
 #include <machine/vmparam.h>
 
 #include <net/if.h>
