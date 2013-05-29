d5e3faf06b44b4ad587cd06b77108b9bed1e0e5d
rkujawa <rkujawa@NetBSD.org>
2013-05-29 20:47:14 +0000

    Add support for mvsoc-based Armada XP peripherals.
    Obtained from Marvell, Semihalf.

--- sys/arch/arm/marvell/mvsocreg.h
+++ sys/arch/arm/marvell/mvsocreg.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: mvsocreg.h,v 1.3 2012/10/19 06:14:44 msaitoh Exp $	*/
+/*	$NetBSD: mvsocreg.h,v 1.4 2013/05/29 20:47:14 rkujawa Exp $	*/
 /*
  * Copyright (c) 2007, 2008 KIYOHARA Takashi
  * All rights reserved.
@@ -55,6 +55,11 @@
 #define MVSOC_DSC_CSSR_WINEN		0x00000001
 #define MVSOC_DSC_CSSR_SIZE_MASK	0xff000000
 
+/*
+ * SoC MISC Register
+ */
+
+#define MVSOC_MISC_BASE		(MVSOC_DEVBUS_BASE + 0x8200) /* For Armada XP */
 
 /*
  * Device Bus
@@ -84,13 +89,23 @@
 #define MVSOC_MLMB_BASE		(UNITID2PHYS(MLMB))	/* 0x20000 */
 
 /* CPU Address Map Registers */
+#if defined(ARMADAXP)
+#define MVSOC_MLMB_WCR(w)		  ((w) < 8 ? ((w) << 4) + 0x0 :\
+						     (((w) - 8) << 3) + 0x90)
+#else
 #define MVSOC_MLMB_WCR(w)		  (((w) << 4) + 0x0)
+#endif
 #define MVSOC_MLMB_WCR_WINEN			(1 << 0)
 #define MVSOC_MLMB_WCR_TARGET(t)		(((t) & 0xf) << 4)
 #define MVSOC_MLMB_WCR_ATTR(a)			(((a) & 0xff) << 8)
 #define MVSOC_MLMB_WCR_SIZE_MASK		0xffff0000
 #define MVSOC_MLMB_WCR_SIZE(s)		  (((s) - 1) & MVSOC_MLMB_WCR_SIZE_MASK)
+#if defined(ARMADAXP)
+#define MVSOC_MLMB_WBR(w)		  ((w) < 8 ? ((w) << 4) + 0x4 :\
+						     (((w) - 8) << 3) + 0x94)
+#else
 #define MVSOC_MLMB_WBR(w)		  (((w) << 4) + 0x4)
+#endif
 #define MVSOC_MLMB_WBR_BASE_MASK		0xffff0000
 #define MVSOC_MLMB_WRLR(w)		  (((w) << 4) + 0x8)
 #define MVSOC_MLMB_WRLR_REMAP_MASK		0xffff0000
@@ -101,11 +116,17 @@
 /* CPU Control and Status Registers */
 #define MVSOC_MLMB_CPUCR		  0x100	/* CPU Configuration Register */
 #define MVSOC_MLMB_CPUCSR		  0x104	/* CPU Control/Status Register*/
+#if defined(ARMADAXP)
+#define MVSOC_MLMB_RSTOUTNMASKR		  0x60 /* RSTOUTn Mask Register */
+#define MVSOC_MLMB_SSRR			  0x64	/* System Soft Reset Register */
+#define MVSOC_MLMB_RSTOUTNMASKR_SOFTRSTOUTEN    (1 << 0)
+#else
 #define MVSOC_MLMB_RSTOUTNMASKR		  0x108 /* RSTOUTn Mask Register */
+#define MVSOC_MLMB_SSRR			  0x10c	/* System Soft Reset Register */
+#define MVSOC_MLMB_RSTOUTNMASKR_SOFTRSTOUTEN    (1 << 2)
+#endif
 #define MVSOC_MLMB_RSTOUTNMASKR_PEXRSTOUTEN     (1 << 0)
 #define MVSOC_MLMB_RSTOUTNMASKR_WDRSTOUTEN      (1 << 1)
-#define MVSOC_MLMB_RSTOUTNMASKR_SOFTRSTOUTEN    (1 << 2)
-#define MVSOC_MLMB_SSRR			  0x10c	/* System Soft Reset Register */
 #define MVSOC_MLMB_SSRR_SYSTEMSOFTRST           (1 << 0)
 #define MVSOC_MLMB_MLMBICR		  0x110	/*Mb-L to Mb Bridge Intr Cause*/
 #define MVSOC_MLMB_MLMBIMR		  0x114	/*Mb-L to Mb Bridge Intr Mask */
@@ -114,6 +135,13 @@
 #define MVSOC_MLMB_CLKGATING_BIT(n)	  (1 << (n))
 
 #define MVSOC_MLMB_L2CFG		  0x128	/* L2 Cache Config */
+
+/* Coherent Fabric Control and Status */
+#define MVSOC_MLMB_COHERENCY_FABRIC_CTRL  0x200
+#define MVSOC_MLMB_COHERENCY_FABRIC_CFG	  0x204
+
+/* CIB registers offsets */
+#define MVSOC_MLMB_CIB_CTRL_CFG		  0x280
 
 #define MVSOC_TMR_BASE			(MVSOC_MLMB_BASE + 0x0300)
 
