d1ae8c719c8c908a775c29dc33e5e705ec91279c
stacktic <stacktic@NetBSD.org>
2013-06-01 10:10:57 +0000

    Add IPv6 support

--- sys/rump/net/lib/libsockin/component.c
+++ sys/rump/net/lib/libsockin/component.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: component.c,v 1.2 2010/03/01 13:12:21 pooka Exp $	*/
+/*	$NetBSD: component.c,v 1.3 2013/06/01 10:10:57 stacktic Exp $	*/
 
 /*
  * Copyright (c) 2009 Antti Kantee.  All Rights Reserved.
@@ -28,7 +28,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: component.c,v 1.2 2010/03/01 13:12:21 pooka Exp $");
+__KERNEL_RCSID(0, "$NetBSD: component.c,v 1.3 2013/06/01 10:10:57 stacktic Exp $");
 
 #include <sys/param.h>
 #include <sys/domain.h>
@@ -40,6 +40,8 @@ __KERNEL_RCSID(0, "$NetBSD: component.c,v 1.2 2010/03/01 13:12:21 pooka Exp $");
 RUMP_COMPONENT(RUMP_COMPONENT_NET)
 {
 	extern struct domain sockindomain;
+	extern struct domain sockin6domain;
 
 	DOMAINADD(sockindomain);
+	DOMAINADD(sockin6domain);
 }
