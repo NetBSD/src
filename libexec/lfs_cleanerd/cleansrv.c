176cc2095060e82c7e2fca84f33c8a28a314fd01
dholland <dholland@NetBSD.org>
2013-06-06 00:53:35 +0000

    ufs -> ulfs for lfs_cleanerd

--- libexec/lfs_cleanerd/cleansrv.c
+++ libexec/lfs_cleanerd/cleansrv.c
@@ -1,4 +1,4 @@
-/* $NetBSD: cleansrv.c,v 1.3 2008/04/28 20:23:04 martin Exp $	 */
+/* $NetBSD: cleansrv.c,v 1.4 2013/06/06 00:53:35 dholland Exp $	 */
 
 /*-
  * Copyright (c) 2005 The NetBSD Foundation, Inc.
@@ -39,7 +39,7 @@
 #include <sys/mount.h>
 #include <sys/socket.h>
 #include <sys/un.h>
-#include <ufs/ufs/inode.h>
+#include <ufs/lfs/ulfs_inode.h>
 #include <ufs/lfs/lfs.h>
 
 #include "bufcache.h"
