bad8dcf77ab0782227a9247b0a8aa29dc478abee
riz <riz@NetBSD.org>
2013-06-09 01:13:47 +0000

    Add another field to the SDT_PROBE_DEFINE macro, so our DTrace probes can named the same as those on other platforms.
    For example, proc:::exec-success, not proc:::exec_success.
    
    Implementation follows the same basic principle as FreeBSD's; add
    another field to the SDT_PROBE_DEFINE macro which is the name
    as exposed to userland.

--- sys/kern/kern_fork.c
+++ sys/kern/kern_fork.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: kern_fork.c,v 1.191 2012/07/27 20:52:49 christos Exp $	*/
+/*	$NetBSD: kern_fork.c,v 1.192 2013/06/09 01:13:47 riz Exp $	*/
 
 /*-
  * Copyright (c) 1999, 2001, 2004, 2006, 2007, 2008 The NetBSD Foundation, Inc.
@@ -67,7 +67,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: kern_fork.c,v 1.191 2012/07/27 20:52:49 christos Exp $");
+__KERNEL_RCSID(0, "$NetBSD: kern_fork.c,v 1.192 2013/06/09 01:13:47 riz Exp $");
 
 #include "opt_ktrace.h"
 
@@ -98,7 +98,7 @@ __KERNEL_RCSID(0, "$NetBSD: kern_fork.c,v 1.191 2012/07/27 20:52:49 christos Exp
 /*
  * DTrace SDT provider definitions
  */
-SDT_PROBE_DEFINE(proc,,,create,
+SDT_PROBE_DEFINE(proc,,,create,create,
 	    "struct proc *", NULL,	/* new process */
 	    "struct proc *", NULL,	/* parent process */
 	    "int", NULL,		/* flags */
