6c3ccd46dfe26136ccd50713e1c316cbb3f60b94
msaitoh <msaitoh@NetBSD.org>
2013-06-13 07:11:11 +0000

    Pull up following revision(s) (requested by pettai in ticket #897): 	usr.sbin/syslogd/tls.c: revision 1.11 	usr.sbin/syslogd/syslogd.h: revision 1.4 	usr.sbin/syslogd/syslogd.c: revision 1.115 PR/47861: Fredrik Pettai: keep track of the address family for each socket opened and don't send the a message to the wrong family type.

--- usr.sbin/syslogd/tls.c
+++ usr.sbin/syslogd/tls.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: tls.c,v 1.9 2012/02/13 07:40:24 spz Exp $	*/
+/*	$NetBSD: tls.c,v 1.9.2.1 2013/06/13 07:11:11 msaitoh Exp $	*/
 
 /*-
  * Copyright (c) 2008 The NetBSD Foundation, Inc.
@@ -45,7 +45,7 @@
  */
 
 #include <sys/cdefs.h>
-__RCSID("$NetBSD: tls.c,v 1.9 2012/02/13 07:40:24 spz Exp $");
+__RCSID("$NetBSD: tls.c,v 1.9.2.1 2013/06/13 07:11:11 msaitoh Exp $");
 
 #ifndef DISABLE_TLS
 #include "syslogd.h"
@@ -851,6 +851,7 @@ socksetup_tls(const int af, const char *bindhostname, const char *port)
 			logerror("socket() failed: %s", strerror(errno));
 			continue;
 		}
+		s->af = r->ai_family;
 		if (r->ai_family == AF_INET6
 		 && setsockopt(s->fd, IPPROTO_IPV6, IPV6_V6ONLY,
 			&on, sizeof(on)) == -1) {
