e387569ed54aff89a26a1c72f232e30710e3562e
matt <matt>
2013-06-16 17:47:54 +0000

    #include <arm/omap/omap_var.h> where appropriate Remove unneeded frequency check

--- sys/arch/arm/omap/obio_ohci.c
+++ sys/arch/arm/omap/obio_ohci.c
@@ -1,7 +1,7 @@
-/*	$Id: obio_ohci.c,v 1.8 2012/10/27 17:17:40 chs Exp $	*/
+/*	$Id: obio_ohci.c,v 1.9 2013/06/16 17:47:54 matt Exp $	*/
 
 /* adapted from: */
-/*	$NetBSD: obio_ohci.c,v 1.8 2012/10/27 17:17:40 chs Exp $	*/
+/*	$NetBSD: obio_ohci.c,v 1.9 2013/06/16 17:47:54 matt Exp $	*/
 /*	$OpenBSD: pxa2x0_ohci.c,v 1.19 2005/04/08 02:32:54 dlg Exp $ */
 
 /*
@@ -24,7 +24,7 @@
 #include "locators.h"
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: obio_ohci.c,v 1.8 2012/10/27 17:17:40 chs Exp $");
+__KERNEL_RCSID(0, "$NetBSD: obio_ohci.c,v 1.9 2013/06/16 17:47:54 matt Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -85,12 +85,12 @@ obioohci_match(device_t parent, cfdata_t cf, void *aux)
 	if (obio->obio_addr != OHCI1_BASE_2430)
 		return 0;
 #endif
-#if defined(OMAP_3530)
-	if (obio->obio_addr != OHCI1_BASE_3530)
+#if defined(OMAP3) && !defined(OMAP4)
+	if (obio->obio_addr != OHCI1_BASE_OMAP3)
 		return 0;
 #endif
-#if defined(OMAP_4430)
-	if (obio->obio_addr != OHCI1_BASE_4430)
+#if defined(OMAP4)
+	if (obio->obio_addr != OHCI1_BASE_OMAP4)
 		return 0;
 #endif
 
