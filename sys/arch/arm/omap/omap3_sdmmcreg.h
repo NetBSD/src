caccee66a51a43c77687c61485e6fc208500a1e8
matt <matt@NetBSD.org>
2013-06-18 15:04:53 +0000

    Initial OMAP4 changes. Attaches but ... sdhc0 at obio2 addr 0x4809c000-0x4809ceff intr 115: SDHC controller sdhc0: SD Host Specification 1.0, rev.0 sdmmc0 at sdhc0 slot 0 sdhc0: INIT Procedure timeout sdmmc0: couldn't supply clock sdmmc0: couldn't enable card: 60

--- sys/arch/arm/omap/omap3_sdmmcreg.h
+++ sys/arch/arm/omap/omap3_sdmmcreg.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: omap3_sdmmcreg.h,v 1.4 2012/12/12 15:19:53 matt Exp $	*/
+/*	$NetBSD: omap3_sdmmcreg.h,v 1.5 2013/06/18 15:04:53 matt Exp $	*/
 /*-
  * Copyright (c) 2012 The NetBSD Foundation, Inc.
  * All rights reserved.
@@ -38,6 +38,12 @@
 #define SDMMC1_BASE_3530	0x4809C000
 #define SDMMC2_BASE_3530	0x480B4000
 #define SDMMC3_BASE_3530	0x480AD000
+
+#define SDMMC1_BASE_4430	0x4809C000	// same for omap5
+#define SDMMC2_BASE_4430	0x480B4000	// same for omap5
+#define SDMMC3_BASE_4430	0x480AD000	// same for omap5
+#define SDMMC4_BASE_4430	0x480D1000	// same for omap5
+#define SDMMC5_BASE_4430	0x480D5000	// same for omap5
 
 #define	OMAP3_SDMMC_SDHC_OFFSET	0x100
 #define	OMAP3_SDMMC_SDHC_SIZE	0x100
