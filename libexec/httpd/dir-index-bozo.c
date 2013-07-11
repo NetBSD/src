7ec01116c2dcd659c58ba3eeaa3fa7fe1387a10d
mrg <mrg@NetBSD.org>
2013-07-11 07:46:37 +0000

    copyright maint.

--- libexec/httpd/dir-index-bozo.c
+++ libexec/httpd/dir-index-bozo.c
@@ -1,9 +1,9 @@
-/*	$NetBSD: dir-index-bozo.c,v 1.16 2013/07/11 07:44:19 mrg Exp $	*/
+/*	$NetBSD: dir-index-bozo.c,v 1.17 2013/07/11 07:46:37 mrg Exp $	*/
 
 /*	$eterna: dir-index-bozo.c,v 1.20 2011/11/18 09:21:15 mrg Exp $	*/
 
 /*
- * Copyright (c) 1997-2011 Matthew R. Green
+ * Copyright (c) 1997-2013 Matthew R. Green
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
