2b4c6a1bd6875a3a0ab372c5c1a0a22e1e10040a
mrg <mrg>
2013-07-11 07:46:37 +0000

    copyright maint.

--- libexec/httpd/bozohttpd.c
+++ libexec/httpd/bozohttpd.c
@@ -1,9 +1,9 @@
-/*	$NetBSD: bozohttpd.c,v 1.40 2013/07/11 07:44:19 mrg Exp $	*/
+/*	$NetBSD: bozohttpd.c,v 1.41 2013/07/11 07:46:37 mrg Exp $	*/
 
 /*	$eterna: bozohttpd.c,v 1.178 2011/11/18 09:21:15 mrg Exp $	*/
 
 /*
- * Copyright (c) 1997-2011 Matthew R. Green
+ * Copyright (c) 1997-2013 Matthew R. Green
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
