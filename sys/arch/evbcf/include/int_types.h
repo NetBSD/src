037a2bb4f41c1ed6bc1da038e3d68fb59f44a18d
matt <matt@NetBSD.org>
2013-07-18 22:08:59 +0000

    Add stub port for evbcf which has enough to do a distribution build.

---
+++ sys/arch/evbcf/include/int_types.h
@@ -0,0 +1,3 @@
+/*	$NetBSD: int_types.h,v 1.1 2013/07/18 22:08:59 matt Exp $	*/
+
+#include <m68k/int_types.h>
---
+++ sys/arch/evbcf/include/setjmp.h
@@ -0,0 +1,3 @@
+/*	$NetBSD: setjmp.h,v 1.1 2013/07/18 22:08:59 matt Exp $	*/
+
+#include <m68k/setjmp.h>
