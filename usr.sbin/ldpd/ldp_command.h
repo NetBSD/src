97d66c3b99affdb16652fab6ae79324e30b30c84
kefren <kefren>
2013-07-18 06:07:45 +0000

    Make sure labels are always updated when a route is added and when a peer is added Rework mpls_add_label according to that so no route refresh is done anymore Use poll when reading the PF_ROUTE socket setsockopt SO_USELOOPBACK on the PF_ROUTE socket Output some information on SIGINFO Allow map changing for a ldp peer Finally fix the connected routes admission into labels Correct the route trigger when a label map is received

--- usr.sbin/ldpd/ldp_command.h
+++ usr.sbin/ldpd/ldp_command.h
@@ -1,4 +1,4 @@
-/* $NetBSD: ldp_command.h,v 1.2 2011/06/16 08:27:28 kefren Exp $ */
+/* $NetBSD: ldp_command.h,v 1.3 2013/07/18 06:07:45 kefren Exp $ */
 
 /*-
  * Copyright (c) 2010 The NetBSD Foundation, Inc.
@@ -52,5 +52,10 @@ void	command_accept(int);
 int	add_command_socket(int);
 void	command_dispatch(struct com_sock *);
 void	command_close(int);
+
+/* Used by SIGINFO handler */
+int show_bindings(int, char *);
+int show_labels(int, char *);
+int show_neighbours(int, char *);
 
 #endif	/* !_LDP_COMMAND_H_ */
