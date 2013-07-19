a6b968105effad92e376e3a9641527c4435f7e6e
matt <matt@NetBSD.org>
2013-07-19 15:57:18 +0000

    Allow stripping of the m before m5... so that it can match coldfire cpus.

--- external/gpl3/binutils/dist/gas/config/tc-m68k.c
+++ external/gpl3/binutils/dist/gas/config/tc-m68k.c
@@ -7420,12 +7420,12 @@ m68k_lookup_cpu (const char *arg, const struct m68k_cpu *table,
 	}
     }
   
-  /* Remove 'm' or 'mc' prefix from 68k variants.  */
+  /* Remove 'm' or 'mc' prefix from 68k or coldfire variants.  */
   if (allow_m)
     {
       if (arg[0] == 'm')
 	{
-	  if (arg[1] == '6')
+	  if (arg[1] == '6' || arg[1] == '5')
 	    arg += 1;
 	  else if (arg[1] == 'c'  && arg[2] == '6')
 	    arg += 2;
