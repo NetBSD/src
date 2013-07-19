5ad987052feb593d793ee8778bb1ddf88922fea8
pooka <pooka@NetBSD.org>
2013-07-19 11:43:18 +0000

    Enable opt-out from shlib mapfile creation.  Not all linkers (namely the solaris one) support such fancy features.
    no default functional change

--- share/mk/bsd.lib.mk
+++ share/mk/bsd.lib.mk
@@ -1,4 +1,4 @@
-#	$NetBSD: bsd.lib.mk,v 1.336 2013/07/16 02:58:19 matt Exp $
+#	$NetBSD: bsd.lib.mk,v 1.337 2013/07/19 11:43:18 pooka Exp $
 #	@(#)bsd.lib.mk	8.3 (Berkeley) 4/22/94
 
 .include <bsd.init.mk>
@@ -207,7 +207,9 @@ CFLAGS+=	-g
 SHLIB_SOVERSION=	${SHLIB_MAJOR}
 SHLIB_SHFLAGS=		-Wl,-soname,${_LIB}.so.${SHLIB_SOVERSION}
 SHLIB_SHFLAGS+=		-Wl,--warn-shared-textrel
+.if !defined(SHLIB_MKMAP) || ${SHLIB_MKMAP} != "no"
 SHLIB_SHFLAGS+=		-Wl,-Map=${_LIB}.so.${SHLIB_SOVERSION}.map
+.endif
 CLEANFILES+=		${_LIB}.so.${SHLIB_SOVERSION}.map
 SHLIB_LDSTARTFILE?=	${_GCC_CRTI} ${_GCC_CRTBEGINS}
 SHLIB_LDENDFILE?=	${_GCC_CRTENDS} ${_GCC_CRTN}
