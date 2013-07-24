fe427865889fca203b6825abac67cf2d621df562
riastradh <riastradh@NetBSD.org>
2013-07-24 02:58:09 +0000

    Avoid shadowing global edid_vendor in drm_edid.c.

--- sys/external/bsd/drm2/dist/drm/drm_edid.c
+++ sys/external/bsd/drm2/dist/drm/drm_edid.c
@@ -453,6 +453,16 @@ EXPORT_SYMBOL(drm_get_edid);
  */
 static bool edid_vendor(struct edid *edid, char *vendor)
 {
+#ifdef __NetBSD__		/* XXX Avoid shadowing global definition.  */
+	char edidv[3];
+
+	edidv[0] = ((edid->mfg_id[0] & 0x7c) >> 2) + '@';
+	edidv[1] = (((edid->mfg_id[0] & 0x3) << 3) |
+			  ((edid->mfg_id[1] & 0xe0) >> 5)) + '@';
+	edidv[2] = (edid->mfg_id[1] & 0x1f) + '@';
+
+	return !strncmp(edidv, vendor, 3);
+#else
 	char edid_vendor[3];
 
 	edid_vendor[0] = ((edid->mfg_id[0] & 0x7c) >> 2) + '@';
@@ -461,6 +471,7 @@ static bool edid_vendor(struct edid *edid, char *vendor)
 	edid_vendor[2] = (edid->mfg_id[1] & 0x1f) + '@';
 
 	return !strncmp(edid_vendor, vendor, 3);
+#endif
 }
 
 /**
