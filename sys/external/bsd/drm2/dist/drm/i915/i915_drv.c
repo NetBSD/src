2a687ad4ef4cca88155a63f8730da34b68ab577b
riastradh <riastradh@NetBSD.org>
2013-07-24 03:28:42 +0000

    Ifdef out drm prime stuff from i915_drv.c.

--- sys/external/bsd/drm2/dist/drm/i915/i915_drv.c
+++ sys/external/bsd/drm2/dist/drm/i915/i915_drv.c
@@ -1083,10 +1083,12 @@ static struct drm_driver driver = {
 	.gem_vm_ops = &i915_gem_vm_ops,
 #endif
 
+#ifndef __NetBSD__		/* XXX drm prime */
 	.prime_handle_to_fd = drm_gem_prime_handle_to_fd,
 	.prime_fd_to_handle = drm_gem_prime_fd_to_handle,
 	.gem_prime_export = i915_gem_prime_export,
 	.gem_prime_import = i915_gem_prime_import,
+#endif
 
 	.dumb_create = i915_gem_dumb_create,
 	.dumb_map_offset = i915_gem_mmap_gtt,
