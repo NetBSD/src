2ff1b7fdf91c9cd7c85a5c241bc4a8849dbb0b77
riastradh <riastradh>
2013-07-24 02:39:42 +0000

    Ifdef out declaration of drm_agp_bind_pages in drmP.h.
    Nothing seems to use it, and it uses Linux's struct page anyway, so
    if anything did use it it would need to be adapted.

--- sys/external/bsd/drm2/dist/include/drm/drmP.h
+++ sys/external/bsd/drm2/dist/include/drm/drmP.h
@@ -1468,11 +1468,13 @@ extern unsigned int drm_poll(struct file *filp, struct poll_table_struct *wait);
 #include <drm/drm_memory.h>
 extern void drm_free_agp(DRM_AGP_MEM * handle, int pages);
 extern int drm_bind_agp(DRM_AGP_MEM * handle, unsigned int start);
+#ifndef __NetBSD__
 extern DRM_AGP_MEM *drm_agp_bind_pages(struct drm_device *dev,
 				       struct page **pages,
 				       unsigned long num_pages,
 				       uint32_t gtt_offset,
 				       uint32_t type);
+#endif
 extern int drm_unbind_agp(DRM_AGP_MEM * handle);
 #ifdef __NetBSD__
 extern void *drm_ioremap(struct drm_device *dev, struct drm_local_map *map);
