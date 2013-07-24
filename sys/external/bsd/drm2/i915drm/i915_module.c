2c1260509a5f25d183542f5e74b26d1aa78d1817
riastradh <riastradh@NetBSD.org>
2013-07-24 03:57:33 +0000

    Set num_ioctls and enable DRIVER_MODESET in i915 module init.

--- sys/external/bsd/drm2/i915drm/i915_module.c
+++ sys/external/bsd/drm2/i915drm/i915_module.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: i915_module.c,v 1.1.2.6 2013/07/24 03:57:19 riastradh Exp $	*/
+/*	$NetBSD: i915_module.c,v 1.1.2.7 2013/07/24 03:57:33 riastradh Exp $	*/
 
 /*-
  * Copyright (c) 2013 The NetBSD Foundation, Inc.
@@ -30,13 +30,15 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: i915_module.c,v 1.1.2.6 2013/07/24 03:57:19 riastradh Exp $");
+__KERNEL_RCSID(0, "$NetBSD: i915_module.c,v 1.1.2.7 2013/07/24 03:57:33 riastradh Exp $");
 
 #include <sys/types.h>
 #include <sys/module.h>
 #include <sys/systm.h>
 
 #include <drm/drmP.h>
+
+#include "i915_drv.h"
 
 MODULE(MODULE_CLASS_DRIVER, i915drm2, "drm2"); /* XXX drm2pci, drm2edid */
 
@@ -57,6 +59,10 @@ i915drm2_modcmd(modcmd_t cmd, void *arg __unused)
 	switch (cmd) {
 	case MODULE_CMD_INIT:
 #ifdef _MODULE
+		/* XXX Kludge it up...  Must happen before attachment.  */
+		i915_drm_driver->num_ioctls = i915_max_ioctl;
+		i915_drm_driver->driver_features |= DRIVER_MODESET;
+
 		error = drm_pci_init(i915_drm_driver, NULL);
 		if (error) {
 			aprint_error("i915drm: failed to init pci: %d\n",
