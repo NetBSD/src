18d566f46f4c1976232b7b8332fed2ce3c93dc1c
riastradh <riastradh@NetBSD.org>
2013-07-24 02:23:21 +0000

    drm_global.c needs <linux/kernel.h>, <linux/export.h>, and <asm/bug.h>.

--- sys/external/bsd/drm2/dist/drm/drm_global.c
+++ sys/external/bsd/drm2/dist/drm/drm_global.c
@@ -31,6 +31,9 @@
 #include <linux/mutex.h>
 #include <linux/slab.h>
 #include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/export.h>
+#include <asm/bug.h>
 #include <drm/drm_global.h>
 
 struct drm_global_item {
