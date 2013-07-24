57101d0848540424873cd602217e5702654971ba
riastradh <riastradh@NetBSD.org>
2013-07-24 02:27:52 +0000

    Add atomic_add and atomic_add_return to <linux/atomic.h>.

--- sys/external/bsd/drm2/include/linux/atomic.h
+++ sys/external/bsd/drm2/include/linux/atomic.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: atomic.h,v 1.1.2.6 2013/07/24 02:27:38 riastradh Exp $	*/
+/*	$NetBSD: atomic.h,v 1.1.2.7 2013/07/24 02:27:52 riastradh Exp $	*/
 
 /*-
  * Copyright (c) 2013 The NetBSD Foundation, Inc.
@@ -53,6 +53,18 @@ static inline void
 atomic_set(atomic_t *atomic, int value)
 {
 	atomic->a_u.au_int = value;
+}
+
+static inline void
+atomic_add(int addend, atomic_t *atomic)
+{
+	atomic_add_int(&atomic->a_u.au_uint, addend);
+}
+
+static inline int
+atomic_add_return(int addend, atomic_t *atomic)
+{
+	return (int)atomic_add_int_nv(&atomic->a_u.au_uint, addend);
 }
 
 static inline void
