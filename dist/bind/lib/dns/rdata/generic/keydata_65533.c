9501cf2233bb9e7a26699d0a228799acc9c974f1
msaitoh <msaitoh@NetBSD.org>
2013-07-29 02:12:58 +0000

    Patch for bind CVE-2013-4854.

--- dist/bind/lib/dns/rdata/generic/keydata_65533.c
+++ dist/bind/lib/dns/rdata/generic/keydata_65533.c
@@ -1,4 +1,4 @@
-/*        $NetBSD: keydata_65533.c,v 1.1.4.4 2012/07/25 12:13:31 jdc Exp $      */
+/*        $NetBSD: keydata_65533.c,v 1.1.4.5 2013/07/29 02:12:58 msaitoh Exp $      */
 
 /*
  * Copyright (C) 2009, 2012  Internet Systems Consortium, Inc. ("ISC")
@@ -178,7 +178,7 @@ fromwire_keydata(ARGS_FROMWIRE) {
 	UNUSED(options);
 
 	isc_buffer_activeregion(source, &sr);
-	if (sr.length < 4)
+	if (sr.length < 16)
 		return (ISC_R_UNEXPECTEDEND);
 
 	isc_buffer_forward(source, sr.length);
