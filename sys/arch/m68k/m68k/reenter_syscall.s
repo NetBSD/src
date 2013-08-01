38497409e16de51ac3d1b6477958c42842cf9394
matt <matt@NetBSD.org>
2013-08-01 13:42:52 +0000

    Convert to motorola style.

--- sys/arch/m68k/m68k/reenter_syscall.s
+++ sys/arch/m68k/m68k/reenter_syscall.s
@@ -1,4 +1,4 @@
-/*	$NetBSD: reenter_syscall.s,v 1.2 1999/11/10 23:17:21 thorpej Exp $	*/
+/*	$NetBSD: reenter_syscall.s,v 1.3 2013/08/01 13:42:52 matt Exp $	*/
 
 /*
  * Written by ITOH Yasufumi.
@@ -18,8 +18,8 @@
 
 ENTRY_NOPROFILE(reenter_syscall)
 	addql	#4,%sp			| pop PC
-	movel	%sp@+,%a0		| current frame addr
-	movel	%sp@,%d1		| stkadj
+	movel	(%sp)+,%a0		| current frame addr
+	movel	(%sp),%d1		| stkadj
 
 | The m68k frame (struct trapframe) format:
 |	16:l	d0-d7/a0-a6/usp
@@ -35,11 +35,11 @@ ENTRY_NOPROFILE(reenter_syscall)
 
 	| copy down frame (16*4 + 2 + 2 + 2 + 4 + 2 = 76 bytes = 19 longs)
 	moveq	#19-1,%d0
-Lcpfr:	movel	%a0@+,%a1@+
+Lcpfr:	movel	(%a0)+,(%a1)+
 	dbra	%d0,Lcpfr
 
 	movew	%d1,%sp@(16*4+2)	| set stack adjust count
-	movel	%sp@,%sp@-		| push syscall no (original d0 value)
+	movel	(%sp),-(%sp)		| push syscall no (original d0 value)
 	jbsr	_C_LABEL(syscall)	| re-enter syscall()
 	addql	#4,%sp			| pop syscall no
 #ifdef DEBUG
@@ -50,7 +50,7 @@ Ladjzero:
 #endif
 	moveal	%sp@(15*4),%a0		| grab and restore
 	movel	%a0,%usp		|   user SP
-	moveml	%sp@+,#0x7FFF		| restore user registers
+	moveml	(%sp)+,#0x7FFF		| restore user registers
 	addql	#8,%sp			| pop SP and stack adjust
 	jra	_ASM_LABEL(rei)		| rte
 
