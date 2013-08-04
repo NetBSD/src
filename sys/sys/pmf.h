d3c2abe5396d379048ef9afa435e15f1bf31a54e
rkujawa <rkujawa@NetBSD.org>
2013-08-04 00:21:56 +0000

    Add an event for situations where some kind of power source change occurs.

--- sys/sys/pmf.h
+++ sys/sys/pmf.h
@@ -1,4 +1,4 @@
-/* $NetBSD: pmf.h,v 1.19 2013/04/06 13:19:25 martin Exp $ */
+/* $NetBSD: pmf.h,v 1.20 2013/08/04 00:21:56 rkujawa Exp $ */
 
 /*-
  * Copyright (c) 2007 Jared D. McNeill <jmcneill@invisible.ca>
@@ -49,7 +49,8 @@ typedef enum {
 	PMFE_CHASSIS_LID_OPEN,
 	PMFE_RADIO_ON,
 	PMFE_RADIO_OFF,
-	PMFE_RADIO_TOGGLE
+	PMFE_RADIO_TOGGLE,
+	PMFE_POWER_CHANGED
 } pmf_generic_event_t;
 
 struct pmf_qual {
