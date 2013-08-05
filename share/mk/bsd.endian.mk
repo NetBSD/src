9fc620ce2cf958271dc68f908997699c94563156
matt <matt@NetBSD.org>
2013-08-05 15:33:07 +0000

    Simplify the earm tests

--- share/mk/bsd.endian.mk
+++ share/mk/bsd.endian.mk
@@ -1,4 +1,4 @@
-#	$NetBSD: bsd.endian.mk,v 1.18 2013/07/18 22:06:09 matt Exp $
+#	$NetBSD: bsd.endian.mk,v 1.19 2013/08/05 15:33:07 matt Exp $
 
 .if !defined(_BSD_ENDIAN_MK_)
 _BSD_ENDIAN_MK_=1
@@ -7,8 +7,7 @@ _BSD_ENDIAN_MK_=1
 
 .if ${MACHINE_ARCH} == "alpha" || \
     ${MACHINE_ARCH} == "arm" || \
-    ${MACHINE_ARCH} == "earm" || \
-    ${MACHINE_ARCH} == "earmhf" || \
+    (!empty(MACHINE_ARCH:Mearm*) && empty(MACHINE_ARCH:Mearm*eb)) || \
     ${MACHINE_ARCH} == "i386" || \
     ${MACHINE_ARCH} == "ia64" || \
     ${MACHINE_ARCH} == "vax" || \
