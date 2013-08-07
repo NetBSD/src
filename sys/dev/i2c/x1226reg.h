c5606116b46490c005dd7c089d8e7d65a338cdc6
soren <soren@NetBSD.org>
2013-08-07 19:38:45 +0000

    Allow i2c addr wildcard matching. Use with care!

--- sys/dev/i2c/x1226reg.h
+++ sys/dev/i2c/x1226reg.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: x1226reg.h,v 1.3 2005/12/11 12:21:23 christos Exp $	*/
+/*	$NetBSD: x1226reg.h,v 1.4 2013/08/07 19:38:45 soren Exp $	*/
 
 /*
  * Copyright (c) 2003 Shigeyuki Fukushima.
@@ -47,7 +47,7 @@
  *
  *	110 1111
  */
-#define	X1226_ADDRMASK		0x7f
+#define	X1226_ADDRMASK		0x3ff
 #define	X1226_ADDR		0x6f
 
 /* XICOR X1226 Device Identifier */
