50ec9b60eced4f1da7c7f0b82bba57cb887474d9
pooka <pooka@NetBSD.org>
2013-08-15 21:21:47 +0000

    Give includers a way to say "yea I'll just deal with the type compat on my own, thank you very many"

--- sys/rump/include/rump/rump_syscalls_compat.h
+++ sys/rump/include/rump/rump_syscalls_compat.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: rump_syscalls_compat.h,v 1.11 2013/08/15 21:16:12 pooka Exp $	*/
+/*	$NetBSD: rump_syscalls_compat.h,v 1.12 2013/08/15 21:21:47 pooka Exp $	*/
 
 /*-
  * Copyright (c) 2010, 2011 Antti Kantee.  All Rights Reserved.
@@ -28,11 +28,14 @@
 #ifndef _RUMP_RUMP_SYSCALLS_COMPAT_H_
 #define _RUMP_RUMP_SYSCALLS_COMPAT_H_
 
+/* should have a smaller hammer here */
+#ifndef RUMP_HOST_NOT_POSIX
 #include <sys/types.h> /* typedefs */
 #include <sys/select.h> /* typedefs */
 #include <sys/socket.h> /* typedefs */
 
 #include <signal.h> /* typedefs */
+#endif
 
 #ifdef __NetBSD__
 #include <sys/cdefs.h>
