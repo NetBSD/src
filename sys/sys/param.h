68d09ff6461445674ba4699c6c7ea0d01a39c6b2
snj <snj@NetBSD.org>
2013-08-15 03:27:37 +0000

    Welcome to 6.1.1!

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.408.2.12.2.1 2013/05/19 21:44:51 riz Exp $	*/
+/*	$NetBSD: param.h,v 1.408.2.12.2.2 2013/08/15 03:27:37 snj Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -63,7 +63,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	601000001	/* NetBSD 6.1.0_PATCH */
+#define	__NetBSD_Version__	601000100	/* NetBSD 6.1.1 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
