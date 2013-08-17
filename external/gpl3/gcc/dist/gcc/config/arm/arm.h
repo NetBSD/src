01eac159268d3bb15215a4cbe882246568b8cf4d
matt <matt@NetBSD.org>
2013-08-17 05:42:06 +0000

    If hardware divide is supported on the CPU, predefine __ARM_ARCH_EXT_IDIV__

--- external/gpl3/gcc/dist/gcc/config/arm/arm.h
+++ external/gpl3/gcc/dist/gcc/config/arm/arm.h
@@ -100,6 +100,8 @@ extern char arm_arch_name[];
 	    if (TARGET_HARD_FLOAT && TARGET_VFP)	\
 	      builtin_define ("__ARM_PCS_VFP");		\
 	  }						\
+	if (TARGET_IDIV)				\
+	  builtin_define ("__ARM_ARCH_EXT_IDIV__");	\
     } while (0)
 
 /* The various ARM cores.  */
@@ -269,6 +271,8 @@ extern void (*arm_lang_output_object_attributes_hook)(void);
    for Thumb-2.  */
 #define TARGET_UNIFIED_ASM TARGET_THUMB2
 
+/* Nonzero if integer division instructions supported.  */
+#define TARGET_IDIV (arm_arch_hwdiv)
 
 /* True iff the full BPABI is being used.  If TARGET_BPABI is true,
    then TARGET_AAPCS_BASED must be true -- but the converse does not
