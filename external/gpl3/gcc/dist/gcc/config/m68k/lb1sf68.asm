6f4201aee78217d9f1b25d5d9588531643fe4783
matt <matt>
2013-08-21 21:11:30 +0000

    When compiling use @PLTPC to generate PLT relocs for NetBSD. When building libgcc_s.so for m68k, this gets rid of m68k--netbsdelf/4.5.4/../../../../m68k--netbsdelf/bin/ld: warning: creating a DT_TEXTREL in a shared object.

--- external/gpl3/gcc/dist/gcc/config/m68k/lb1sf68.asm
+++ external/gpl3/gcc/dist/gcc/config/m68k/lb1sf68.asm
@@ -202,6 +202,8 @@ see the files COPYING3 and COPYING.RUNTIME respectively.  If not, see
 #if defined (__mcoldfire__) && !defined (__mcfisab__) && !defined (__mcfisac__)
 	lea	\addr-.-8,a0
 	jsr	pc@(a0)
+#elif defined(__NetBSD__)
+	jbsr	\addr@PLTPC
 #else
 	jbsr	\addr
 #endif
@@ -211,9 +213,11 @@ see the files COPYING3 and COPYING.RUNTIME respectively.  If not, see
 	/* ISA C has no bra.l instruction, and since this assembly file
 	   gets assembled into multiple object files, we avoid the
 	   bra instruction entirely.  */
-#if (defined (__mcoldfire__) && !defined (__mcfisab__)) || defined(__NetBSD__)
+#if (defined (__mcoldfire__) && !defined (__mcfisab__))
 	lea	\addr-.-8,a0
 	jmp	pc@(a0)
+#elif defined(__NetBSD__)
+	bra	\addr@PLTPC
 #else
 	bra	\addr
 #endif
