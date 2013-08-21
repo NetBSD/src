5547b6bf58a81e9303118cf94018d9dfd2f692cf
tron <tron@NetBSD.org>
2013-08-21 20:09:37 +0000

    Import Postfix 2.9.7. Changes since version 2.9.5: - Thanks to OpenSSL documentation, the Postfix 2.9.0..2.9.5 SMTP   client and server used an incorrect procedure to compute TLS   certificate PUBLIC-KEY fingerprints (these may be used in the   check_ccert_access and in smtp_tls_policy_maps features). Support   for certificate PUBLIC-KEY finger prints was introduced with Postfix   2.9; there is no known problem with the certificate fingerprint   algorithms available since Postfix 2.2.   Specify "tls_legacy_public_key_fingerprints = yes" temporarily,   pending a migration from configuration files with incorrect Postfix   2.9.0..2.9.5 certificate PUBLIC-KEY finger prints, to the correct   fingerprints used by Postfix 2.9.6 and later. - Bugfix (introduced: Postfix 2.0): when myhostname is not listed in   mydestination, the trivial-rewrite resolver may log "do not list in both   mydestination and ". The fix is to re-resolve a domain-less address after   adding $myhostname as the surrogate domain, so that it pops out with the   right address-class label. Reported by Quanah Gibson-Mount. - Bugfix (introduced: Postfix 2.3): don't reuse TCP connections when   smtp_tls_policy_maps is specified. TLS policies may depend on the remote   destination, but the Postfix <2.11 SMTP connection cache client does not   distinguish between different destinations that resolve to the same   IP address. Victor Duchovni. Found during Postfix 2.11 code maintenance. - Bugfix (introduced: Postfix 2.2): don't reuse TCP connections when   SASL authentication is enabled. SASL passwords may depend on the   remote SMTP server hostname, but the Postfix <2.11 SMTP connection   cache client does not distinguish between different hostnames that   resolve to the same IP address. Found during Postfix 2.11 code   maintenance.

--- external/ibm-public/postfix/dist/src/global/mail_version.h
+++ external/ibm-public/postfix/dist/src/global/mail_version.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: mail_version.h,v 1.1.1.17 2013/01/02 18:58:59 tron Exp $	*/
+/*	$NetBSD: mail_version.h,v 1.1.1.18 2013/08/21 20:09:52 tron Exp $	*/
 
 #ifndef _MAIL_VERSION_H_INCLUDED_
 #define _MAIL_VERSION_H_INCLUDED_
@@ -22,8 +22,8 @@
   * Patches change both the patchlevel and the release date. Snapshots have no
   * patchlevel; they change the release date only.
   */
-#define MAIL_RELEASE_DATE	"20121213"
-#define MAIL_VERSION_NUMBER	"2.9.5"
+#define MAIL_RELEASE_DATE	"20130622"
+#define MAIL_VERSION_NUMBER	"2.9.7"
 
 #ifdef SNAPSHOT
 # define MAIL_VERSION_DATE	"-" MAIL_RELEASE_DATE
