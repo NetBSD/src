7578f63532cb0ffef217c3a31796873237760269
prlw1 <prlw1@NetBSD.org>
2013-09-05 11:34:40 +0000

    Fix /var/log/httpd/access.log  644  52   *  $W0D23 ZBP /var/run/httpd.pid 30 newsyslog: config line 16: bad signal type (strtol: **endptr is '\0' on success rather than *endptr)

--- usr.bin/newsyslog/newsyslog.c
+++ usr.bin/newsyslog/newsyslog.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: newsyslog.c,v 1.60 2012/06/17 03:52:32 christos Exp $	*/
+/*	$NetBSD: newsyslog.c,v 1.61 2013/09/05 11:34:40 prlw1 Exp $	*/
 
 /*
  * Copyright (c) 1999, 2000 Andrew Doran <ad@NetBSD.org>
@@ -55,7 +55,7 @@
 
 #include <sys/cdefs.h>
 #ifndef lint
-__RCSID("$NetBSD: newsyslog.c,v 1.60 2012/06/17 03:52:32 christos Exp $");
+__RCSID("$NetBSD: newsyslog.c,v 1.61 2013/09/05 11:34:40 prlw1 Exp $");
 #endif /* not lint */
 
 #include <sys/types.h>
@@ -824,7 +824,7 @@ getsig(const char *sig)
 	
 	if (isnumber(sig)) {
 		n = (int)strtol(sig, &p, 0);
-		if (p != '\0' || n < 0 || n >= NSIG)
+		if (*p != '\0' || n < 0 || n >= NSIG)
 			return -1;
 		return n;
 	}
