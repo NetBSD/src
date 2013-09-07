75d1940767ce607a0564a1ff30749da356cea411
bouyer <bouyer@NetBSD.org>
2013-09-07 16:55:04 +0000

    Welcome to 6.1.1_PATCH

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.408.2.12.2.2 2013/08/15 03:27:37 snj Exp $	*/
+/*	$NetBSD: param.h,v 1.408.2.12.2.3 2013/09/07 16:55:04 bouyer Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -63,7 +63,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	601000100	/* NetBSD 6.1.1 */
+#define	__NetBSD_Version__	601000101	/* NetBSD 6.1.1_PATCH */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
