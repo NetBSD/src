886b5836c83f41a70e86d63963f44fd8770beb48
martin <martin@NetBSD.org>
2013-09-13 21:03:04 +0000

    Remove unused variable

--- sys/dev/pci/if_ex_pci.c
+++ sys/dev/pci/if_ex_pci.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: if_ex_pci.c,v 1.55 2013/03/30 03:21:05 christos Exp $	*/
+/*	$NetBSD: if_ex_pci.c,v 1.56 2013/09/13 21:03:04 martin Exp $	*/
 
 /*-
  * Copyright (c) 1998 The NetBSD Foundation, Inc.
@@ -31,7 +31,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: if_ex_pci.c,v 1.55 2013/03/30 03:21:05 christos Exp $");
+__KERNEL_RCSID(0, "$NetBSD: if_ex_pci.c,v 1.56 2013/09/13 21:03:04 martin Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -327,7 +327,6 @@ ex_d3tod0(pci_chipset_tag_t pc, pcitag_t tag, device_t self, pcireg_t state)
 	uint32_t base0;
 	uint32_t base1;
 	uint32_t romaddr;
-	uint32_t pci_command;
 	uint32_t pci_int_lat;
 	uint32_t pci_cache_lat;
 
@@ -336,7 +335,8 @@ ex_d3tod0(pci_chipset_tag_t pc, pcitag_t tag, device_t self, pcireg_t state)
 
 	aprint_normal_dev(self, "found in power state D%d, "
 	    "attempting to recover.\n", state);
-	pci_command = pci_conf_read(pc, tag, PCI_COMMAND_STATUS_REG);
+	/* XXX is this needed? */
+	(void)pci_conf_read(pc, tag, PCI_COMMAND_STATUS_REG);
 	base0 = pci_conf_read(pc, tag, PCI_BAR0);
 	base1 = pci_conf_read(pc, tag, PCI_BAR1);
 	romaddr	= pci_conf_read(pc, tag, PCI_EXP_ROM_BAR);
