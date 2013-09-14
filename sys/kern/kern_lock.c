2a9283d87756fe00cfdb0e086e4133921f34fe90
martin <martin@NetBSD.org>
2013-09-14 20:24:22 +0000

    Move a CTASSERT to global scope (easiest way to avoid gcc 4.8.1 local unused typedef warnings)

--- sys/kern/kern_lock.c
+++ sys/kern/kern_lock.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: kern_lock.c,v 1.154 2013/04/27 08:12:34 mlelstv Exp $	*/
+/*	$NetBSD: kern_lock.c,v 1.155 2013/09/14 20:24:22 martin Exp $	*/
 
 /*-
  * Copyright (c) 2002, 2006, 2007, 2008, 2009 The NetBSD Foundation, Inc.
@@ -31,7 +31,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: kern_lock.c,v 1.154 2013/04/27 08:12:34 mlelstv Exp $");
+__KERNEL_RCSID(0, "$NetBSD: kern_lock.c,v 1.155 2013/09/14 20:24:22 martin Exp $");
 
 #include <sys/param.h>
 #include <sys/proc.h>
@@ -127,11 +127,11 @@ void
 kernel_lock_init(void)
 {
 
-	CTASSERT(CACHE_LINE_SIZE >= sizeof(__cpu_simple_lock_t));
 	__cpu_simple_lock_init(kernel_lock);
 	kernel_lock_dodebug = LOCKDEBUG_ALLOC(kernel_lock, &_kernel_lock_ops,
 	    RETURN_ADDRESS);
 }
+CTASSERT(CACHE_LINE_SIZE >= sizeof(__cpu_simple_lock_t));
 
 /*
  * Print debugging information about the kernel lock.
