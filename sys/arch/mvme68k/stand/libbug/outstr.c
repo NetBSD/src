cd9a6fbc2fc7e94835277e9cc3e7f3f9e1275dd2
tsutsui <tsutsui@NetBSD.org>
2013-09-23 01:39:27 +0000

    Make MVMEPROM_ARG2() macro take two args in a single asm statement.
    Fixes a problem that a wrong address is passed to the MVMEPROM outstr
    function (then caused garbages on screen) after we switched to gcc-4.5.
    Reported and confirmed by Andrew Gillham on port-mvme68k@:
    http://mail-index.NetBSD.org/port-mvme68k/2013/09/17/msg000084.html
    http://mail-index.NetBSD.org/port-mvme68k/2013/09/19/msg000085.html
    http://mail-index.NetBSD.org/port-mvme68k/2013/09/22/msg000095.html
    http://mail-index.NetBSD.org/port-mvme68k/2013/09/22/msg000097.html
    No error on "build.sh -m mvme68k build" builds.
    
    Should be pulled up to netbsd-6 branches.

--- sys/arch/mvme68k/stand/libbug/outstr.c
+++ sys/arch/mvme68k/stand/libbug/outstr.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: outstr.c,v 1.3 2008/01/12 09:54:31 tsutsui Exp $	*/
+/*	$NetBSD: outstr.c,v 1.4 2013/09/23 01:39:27 tsutsui Exp $	*/
 
 /*
  * bug routines -- assumes that the necessary sections of memory
@@ -13,7 +13,6 @@ void
 mvmeprom_outstr(char *start, char *end)
 {
 
-	MVMEPROM_ARG1(end);
-	MVMEPROM_ARG2(start);
+	MVMEPROM_ARG2(end, start);
 	MVMEPROM_CALL(MVMEPROM_OUTSTR);
 }
