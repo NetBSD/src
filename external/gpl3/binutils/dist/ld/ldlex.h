b829d540ec6e306d2c5c4345daa5c91b31254c5d
christos <christos@NetBSD.org>
2013-09-29 14:03:29 +0000

    resolve conflicts

--- external/gpl3/binutils/dist/ld/ldlex.h
+++ external/gpl3/binutils/dist/ld/ldlex.h
@@ -136,6 +136,7 @@ enum option_values
 #endif /* ENABLE_PLUGINS */
   OPTION_DEFAULT_SCRIPT,
   OPTION_PRINT_OUTPUT_FORMAT,
+  OPTION_IGNORE_UNRESOLVED_SYMBOL
 };
 
 /* The initial parser states.  */
