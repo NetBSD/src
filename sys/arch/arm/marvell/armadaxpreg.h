d9ab8d87e8bf1c7c444dfc02de8bac442ceb48e8
kiyohara <kiyohara>
2013-09-30 13:33:05 +0000

    Move armadaxpreg.h into arm/marvell. And add some defines and reorder.

--- sys/arch/arm/marvell/armadaxpreg.h
+++ sys/arch/arm/marvell/armadaxpreg.h
@@ -47,13 +47,20 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #define ARMADAXP_UNITID_MLMB		MVSOC_UNITID_MLMB
 #define ARMADAXP_UNITID_PEX		MVSOC_UNITID_PEX
 #define ARMADAXP_UNITID_USB		0x5	/* USB registers */
+#define ARMADAXP_UNITID_XORE0		0x6	/* Reserved? */
 #define ARMADAXP_UNITID_GBE0		0x7
 #define ARMADAXP_UNITID_GBE2		0x3
 #define ARMADAXP_UNITID_PEX0		MVSOC_UNITID_PEX
 #define ARMADAXP_UNITID_PEX1		0x8
 #define ARMADAXP_UNITID_PEX2		MVSOC_UNITID_PEX
 #define ARMADAXP_UNITID_PEX3		0x8
+#define ARMADAXP_UNITID_CRYPT		0x9
 #define ARMADAXP_UNITID_SATA		0xa
+#define ARMADAXP_UNITID_BM		0xc
+#define ARMADAXP_UNITID_PNC		0xc
+#define ARMADAXP_UNITID_SDIO		0xd	/* SDIO registers */
+#define ARMADAXP_UNITID_LCD		0xe	/* Reserved? */
+#define ARMADAXP_UNITID_XORE1		0xf	/* Reserved? */
 
 #define ARMADAXP_ATTR_PEXx0_MEM		0xe8
 #define ARMADAXP_ATTR_PEXx0_IO		0xe0
@@ -68,20 +75,53 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #define ARMADAXP_ATTR_PEX3_MEM		0xf8
 #define ARMADAXP_ATTR_PEX3_IO		0xf0
 
-#define ARMADAXP_MLMB_NWINDOW		19
-#define ARMADAXP_MLMB_NREMAP		8
 
-#define ARMADAXP_PEX00_BASE		0x40000
-#define ARMADAXP_PEX01_BASE		0x44000
-#define ARMADAXP_PEX02_BASE		0x48000
-#define ARMADAXP_PEX03_BASE		0x4c000
-#define ARMADAXP_PEX10_BASE		0x80000
-#define ARMADAXP_PEX11_BASE		0x84000
-#define ARMADAXP_PEX12_BASE		0x88000
-#define ARMADAXP_PEX13_BASE		0x8c000
-#define ARMADAXP_PEX2_BASE		0x42000
-#define ARMADAXP_PEX3_BASE		0x82000
+#define ARMADAXP_IRQ_GBE0_TH_RXTX	8	/* GBE0_TH_RXTX_Int */
+#define ARMADAXP_IRQ_GBE1_TH_RXTX	10	/* GBE1_TH_RXTX_Int */
+#define ARMADAXP_IRQ_GBE2_TH_RXTX	12	/* GBE2_TH_RXTX_Int */
+#define ARMADAXP_IRQ_GBE3_TH_RXTX	14	/* GBE3_TH_RXTX_Int */
 
+#define ARMADAXP_IRQ_LCD		29
+#define ARMADAXP_IRQ_SPI		30
+#define ARMADAXP_IRQ_TWSI0		31
+#define ARMADAXP_IRQ_TWSI1		32
+#define ARMADAXP_IRQ_IDMA0		33	/* IDMA Channel 0 */
+#define ARMADAXP_IRQ_IDMA1		34	/* IDMA Channel 1 */
+#define ARMADAXP_IRQ_IDMA2		35	/* IDMA Channel 2 */
+#define ARMADAXP_IRQ_IDMA3		36	/* IDMA Channel 3 */
+#define ARMADAXP_IRQ_TIMER0		37
+#define ARMADAXP_IRQ_TIMER1		38
+#define ARMADAXP_IRQ_TIMER2		39
+#define ARMADAXP_IRQ_TIMER3		40
+#define ARMADAXP_IRQ_UART0		41
+#define ARMADAXP_IRQ_UART1		42
+#define ARMADAXP_IRQ_UART2		43
+#define ARMADAXP_IRQ_UART3		44
+#if 0
+#define ARMADAXP_IRQ_USB0		45
+#define ARMADAXP_IRQ_USB1		46
+#define ARMADAXP_IRQ_USB2		47
+#else
+/*
+ * According to functional specification (MV-S107021-00B.pdf), interrupt number
+ * for USB0_int is 47, in fact this number is 45. Because of that interrupt
+ * number definitions are not equivalent to ArmadaXP functional specification
+ * (MV-S107021-00B.pdf).
+ */
+#define ARMADAXP_IRQ_USB0		45	/* USB2 */
+#define ARMADAXP_IRQ_USB1		46	/* USB1 */
+#define ARMADAXP_IRQ_USB2		47	/* USB0 */
+#endif
+#define ARMADAXP_IRQ_CESA0		48
+#define ARMADAXP_IRQ_CESA1		49
+#define ARMADAXP_IRQ_RTC		50
+#define ARMADAXP_IRQ_XOR0CH0		51	/* XOR0 Ch0 */
+#define ARMADAXP_IRQ_XOR0CH1		52	/* XOR0 Ch1 */
+#define ARMADAXP_IRQ_BM			53	/* Buffer Management */
+#define ARMADAXP_IRQ_SDIO		54
+#define ARMADAXP_IRQ_SATA0		55
+#define ARMADAXP_IRQ_TDM		56
+#define ARMADAXP_IRQ_SATA1		57
 #define ARMADAXP_IRQ_PEX00		58	/* PCIe Port0.0 INTA/B/C/D */
 #define ARMADAXP_IRQ_PEX01		59	/* PCIe Port0.1 INTA/B/C/D */
 #define ARMADAXP_IRQ_PEX02		60	/* PCIe Port0.2 INTA/B/C/D */
@@ -90,37 +130,14 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #define ARMADAXP_IRQ_PEX11		63	/* PCIe Port1.1 INTA/B/C/D */
 #define ARMADAXP_IRQ_PEX12		64	/* PCIe Port1.2 INTA/B/C/D */
 #define ARMADAXP_IRQ_PEX13		65	/* PCIe Port1.3 INTA/B/C/D */
+#define ARMADAXP_IRQ_XOR1CH2		94	/* XOR1 Ch2 */
+#define ARMADAXP_IRQ_XOR1CH3		95	/* XOR1 Ch3 */
 #define ARMADAXP_IRQ_PEX2		99	/* PCIe Port2 INTA/B/C/D */
 #define ARMADAXP_IRQ_PEX3		103	/* PCIe Port3 INTA/B/C/D */
 
-#define ARMADAXP_IRQ_SATA0		55
-#define ARMADAXP_IRQ_SATA1		57
 
-#define ARMADAXP_IRQ_SPI0		30	/* SPI0 */
-#define ARMADAXP_IRQ_TWSI0		31	/* TWSI0 */
-#define ARMADAXP_IRQ_TWSI1		32	/* TWSI1 */
-#define ARMADAXP_IRQ_TIMER0		37	/* Timer0 */
-#define ARMADAXP_IRQ_UART0INT		41	/* UART0 */
-#define ARMADAXP_IRQ_UART1INT		42	/* UART1 */
-#define ARMADAXP_IRQ_UART2INT		43	/* UART2 */
-#define ARMADAXP_IRQ_UART3INT		44	/* UART3 */
-/*
- * According to functional specification (MV-S107021-00B.pdf), interrupt number
- * for USB0_int is 47, in fact this number is 45. Because of that interrupt
- * number definitions are not equivalent to ArmadaXP functional specification
- * (MV-S107021-00B.pdf).
- */
-#define ARMADAXP_IRQ_USB0INT		45	/* USB2 */
-#define ARMADAXP_IRQ_USB1INT		46	/* USB1 */
-#define ARMADAXP_IRQ_USB2INT		47	/* USB0 */
-#define ARMADAXP_IRQ_RTCINT		50	/* RTC */
-
-#define ARMADAXP_IRQ_GBE0_TH_RXTX_Int	8	/* GBE0_TH_RXTX_Int */
-#define ARMADAXP_IRQ_GBE1_TH_RXTX_Int	10	/* GBE1_TH_RXTX_Int */
-#define ARMADAXP_IRQ_GBE2_TH_RXTX_Int	12	/* GBE2_TH_RXTX_Int */
-#define ARMADAXP_IRQ_GBE3_TH_RXTX_Int	14	/* GBE3_TH_RXTX_Int */
-
-#define ARMADAXP_IRQ_SDIO		54	/* SDIO */
+#define ARMADAXP_MLMB_NWINDOW		19
+#define ARMADAXP_MLMB_NREMAP		8
 
 /*
  * Physical address of integrated peripherals
@@ -132,54 +149,35 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 /*
  * Real-Time Clock Unit Registers
  */
-#define ARMADAXP_RTC_BASE		(MVSOC_DEVBUS_BASE + 0x0300)
+#define ARMADAXP_RTC_BASE	(MVSOC_DEVBUS_BASE + 0x0300)
+
+/*
+ * SPI Interface Registers
+ */
+#define ARMADAXP_SPI_BASE	(MVSOC_DEVBUS_BASE + 0x0600)
 
 /*
  * TWSI Interface Registers
  */
-#define ARMADAXP_TWSI0_BASE		(MVSOC_DEVBUS_BASE + 0x1000)
-#define ARMADAXP_TWSI1_BASE		(MVSOC_DEVBUS_BASE + 0x1100)
-
-/* SPI Interface Registers
-+ */
-#define ARMADAXP_SPI0_BASE		(MVSOC_DEVBUS_BASE + 0x0600)
-#define ARMADAXP_SPI1_BASE		(MVSOC_DEVBUS_BASE + 0x0680)
+#define ARMADAXP_TWSI1_BASE	(MVSOC_DEVBUS_BASE + 0x1100)
 
 /*
  * UART Interface Registers
- */					/* NS16550 compatible */
-#define ARMADAXP_COM2_BASE		(MVSOC_DEVBUS_BASE + 0x2200)
-#define ARMADAXP_COM3_BASE		(MVSOC_DEVBUS_BASE + 0x2300)
+ */				/* NS16550 compatible */
+#define ARMADAXP_COM2_BASE	(MVSOC_DEVBUS_BASE + 0x2200)
+#define ARMADAXP_COM3_BASE	(MVSOC_DEVBUS_BASE + 0x2300)
 
 /*
- * PCI Express Interface Registers
+ * General Purpose Input/Output Port Registers
  */
-#define ARMADAXP_PEX_BASE	(UNITID2PHYS(PEX))	/* 0x40000 */
+#define ARMADAXP_GPIO0_BASE	(MVSOC_DEVBUS_BASE + 0x8100)
+#define ARMADAXP_GPIO1_BASE	(MVSOC_DEVBUS_BASE + 0x8120)
+#define ARMADAXP_GPIO2_BASE	(MVSOC_DEVBUS_BASE + 0x8140)
 
 /*
- * USB 2.0 Interface Registers
- */
-#define ARMADAXP_USB_BASE	(UNITID2PHYS(USB))	/* 0x50000 */
-#define ARMADAXP_USB0_BASE	(ARMADAXP_USB_BASE)
-#define ARMADAXP_USB1_BASE	(ARMADAXP_USB_BASE + 0x1000)
-
-/*
- * Gigabit Ethernet Registers
- */
-#define ARMADAXP_GBE0_BASE	(UNITID2PHYS(GBE0))	/* 0x70000 */
-#define ARMADAXP_GBE1_BASE	(ARMADAXP_GBE0_BASE + 0x4000)
-#define ARMADAXP_GBE2_BASE	(UNITID2PHYS(GBE2))	/* 0x30000 */
-#define ARMADAXP_GBE3_BASE	(ARMADAXP_GBE2_BASE + 0x4000)
-
-/*
- * Serial-ATA Host Controller (SATAHC) Registers
- */
-#define ARMADAXP_SATAHC_BASE	(UNITID2PHYS(SATA))	/* 0xa0000 */
-
-/*
- * SDIO Registers
- */
-#define ARMADAXP_SDIO_BASE	0xD4000			/* XXX: UNIT ID ? */
+ * Power Management Unit Registers
+ */				/* NS16550 compatible */
+#define ARMADAXP_PMU_BASE	(MVSOC_DEVBUS_BASE + 0xc000)
 
 /*
  * SoC MISC Registers
@@ -198,9 +196,9 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #define	ARMADAXP_MLMB_MPIC_ISCR_BASE		0x100
 
 #define	ARMADAXP_MLMB_MPIC_DOORBELL		0x78
-#define	ARMADAXP_MLMB_MPIC_DOORBELL_MASK		0x7c
+#define	ARMADAXP_MLMB_MPIC_DOORBELL_MASK	0x7c
 #define	ARMADAXP_MLMB_MPIC_CTP			0xb0
-#define	ARMADAXP_MLMB_MPIC_IIACK			0xb4
+#define	ARMADAXP_MLMB_MPIC_IIACK		0xb4
 #define	ARMADAXP_MLMB_MPIC_ISM			0xb8
 #define	ARMADAXP_MLMB_MPIC_ICM			0xbc
 #define	ARMADAXP_MLMB_MPIC_ERR_MASK		0xec0
@@ -223,5 +221,81 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #define L2_REP_STRAT_MASK		(3 << 27)
 #define L2_REP_STRAT_SEMIPLRU		(3 << 27)
 #define L2_ALL_WAYS			0xffffffff
+
+/*
+ * PCI-Express Interface Registers
+ */
+#define ARMADAXP_PEX01_BASE	(MVSOC_PEX_BASE + 0x4000)
+#define ARMADAXP_PEX02_BASE	(MVSOC_PEX_BASE + 0x8000)
+#define ARMADAXP_PEX03_BASE	(MVSOC_PEX_BASE + 0xc000)
+#define ARMADAXP_PEX10_BASE	(UNITID2PHYS(PEX1))
+#define ARMADAXP_PEX11_BASE	(ARMADAXP_PEX10_BASE + 0x4000)
+#define ARMADAXP_PEX12_BASE	(ARMADAXP_PEX10_BASE + 0x8000)
+#define ARMADAXP_PEX13_BASE	(ARMADAXP_PEX10_BASE + 0xc000)
+#define ARMADAXP_PEX2_BASE	(UNITID2PHYS(PEX2) + 0x2000)
+#define ARMADAXP_PEX3_BASE	(UNITID2PHYS(PEX3) + 0x2000)
+
+/*
+ * USB 2.0 Interface Registers
+ */
+#define ARMADAXP_USB_BASE	(UNITID2PHYS(USB))	/* 0x50000 */
+#define ARMADAXP_USB0_BASE	(ARMADAXP_USB_BASE + 0x0000)
+#define ARMADAXP_USB1_BASE	(ARMADAXP_USB_BASE + 0x1000)
+#define ARMADAXP_USB2_BASE	(ARMADAXP_USB_BASE + 0x2000)
+
+/* 
+ * XOR Engine Registers
+ */
+#define ARMADAXP_XORE0_BASE	(UNITID2PHYS(XORE0))	/* 0x60000 */
+#define ARMADAXP_XORE1_BASE	(UNITID2PHYS(XORE1))	/* 0xf0000 */
+
+/*
+ * Gigabit Ethernet Registers
+ */
+#define ARMADAXP_GBE0_BASE	(UNITID2PHYS(GBE0))	/* 0x70000 */
+#define ARMADAXP_GBE1_BASE	(ARMADAXP_GBE0_BASE + 0x4000)
+#define ARMADAXP_GBE2_BASE	(UNITID2PHYS(GBE2))	/* 0x30000 */
+#define ARMADAXP_GBE3_BASE	(ARMADAXP_GBE2_BASE + 0x4000)
+
+/*
+ * Precise Time Protocol (PTP) Registers
+ */
+#define ARMADAXP_PTP_BASE	(UNITID2PHYS(GBE0))	/* 0x7c000 */
+
+/* 
+ * Cryptographic Engine and Security Accelerator Registers
+ */
+#define ARMADAXP_CESA0_BASE	(UNITID2PHYS(CRYPT) + 0xd000)	/* 0x9d000 */
+#define ARMADAXP_CESA1_BASE	(UNITID2PHYS(CRYPT) + 0xf000)	/* 0x9f000 */
+
+/*
+ * Serial-ATA Host Controller (SATAHC) Registers
+ */
+#define ARMADAXP_SATAHC_BASE	(UNITID2PHYS(SATA))	/* 0xa0000 */
+
+/*
+ * Buffer Management Controller Registers
+ */
+#define ARMADAXP_BMC_BASE	(UNITID2PHYS(BM))	/* 0xc0000 */
+
+/*
+ * NAND Flash Controller Version 2.0 Registers
+ */
+#define ARMADAXP_NAND_BASE	0xc0000
+
+/*
+ * PnC Unit Registers
+ */
+#define ARMADAXP_PNC_BASE	(UNITID2PHYS(PNC))	/* 0xc8000 */
+
+/*
+ * SDIO Registers
+ */
+#define ARMADAXP_SDIO_BASE	(UNITID2PHYS(SDIO) + 0x4000)	/* 0xd4000 */
+
+/*
+ * Liquid Crystal Display Registers
+ */
+#define ARMADAXP_LCD_BASE	(UNITID2PHYS(LCD))	/* 0xe0000 */
 
 #endif /* _ARMADAXPREG_H_ */
