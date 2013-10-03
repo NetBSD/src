387b2ceb98331ce432bce7a1e866a93ecc58a1d4
christos <christos@NetBSD.org>
2013-10-03 18:30:43 +0000

    merge conflicts

--- external/gpl3/gdb/dist/gdb/hppanbsd-nat.c
+++ external/gpl3/gdb/dist/gdb/hppanbsd-nat.c
@@ -1,6 +1,6 @@
 /* Native-dependent code for NetBSD/hppa.
 
-   Copyright (C) 2008, 2009, 2010, 2011 Free Software Foundation, Inc.
+   Copyright (C) 2008-2013 Free Software Foundation, Inc.
 
    This file is part of GDB.
 
