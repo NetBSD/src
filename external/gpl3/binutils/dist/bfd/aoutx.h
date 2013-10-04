5a20656335c16c1120d59baafe4230be80a6f70e
christos <christos@NetBSD.org>
2013-10-04 18:08:02 +0000

    Allow bfd_mach_arm4 machines to produce a.out binaries. Perhaps we should just let every arm machine produce them? Or we should use M_ARM6_NETBSD midmag for those?

--- external/gpl3/binutils/dist/bfd/aoutx.h
+++ external/gpl3/binutils/dist/bfd/aoutx.h
@@ -762,7 +762,7 @@ NAME (aout, machine_type) (enum bfd_architecture arch,
       break;
 
     case bfd_arch_arm:
-      if (machine == 0)
+      if (machine == 0 || machine == 5)
 	arch_flags = M_ARM;
       break;
 
