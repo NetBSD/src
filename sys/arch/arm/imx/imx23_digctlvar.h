486a5160bcc5929d9e91cbf4da4e1dda79c5dd11
matt <matt>
2013-10-07 17:36:40 +0000

    iMX233/OLinuXino changes from Petri Laskso. See http://mail-index.netbsd.org/port-arm/2013/10/07/msg002042.html

--- sys/arch/arm/imx/imx23_digctlvar.h
+++ sys/arch/arm/imx/imx23_digctlvar.h
@@ -1,7 +1,7 @@
-/* $Id: common.h,v 1.4 2013/10/07 17:36:40 matt Exp $ */
+/* $Id: imx23_digctlvar.h,v 1.1 2013/10/07 17:36:40 matt Exp $ */
 
 /*
- * Copyright (c) 2012 The NetBSD Foundation, Inc.
+ * Copyright (c) 2013 The NetBSD Foundation, Inc.
  * All rights reserved.
  *
  * This code is derived from software contributed to The NetBSD Foundation
@@ -29,54 +29,9 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifndef _BOOTIMX23_COMMON_
-#define _BOOTIMX23_COMMON_
+#ifndef _ARM_IMX_IMX23_DIGCTLVAR_H_
+#define _ARM_IMX_IMX23_DIGCTLVAR_H_
 
-#define REG_RD(reg) *(volatile uint32_t *)(reg)
-#define REG_WR(reg, val)						\
-do {									\
-	*(volatile uint32_t *)((reg)) = val;				\
-} while (0)
-#define REG_RD_HW(reg) *(volatile uint16_t *)(reg)
-#define REG_WR_HW(reg, val)						\
-do {									\
-	*(volatile uint16_t *)((reg)) = val;				\
-} while (0)
-#define REG_RD_BYTE(reg) *(volatile uint8_t *)(reg)
-#define REG_WR_BYTE(reg, val)						\
-do {									\
-	*(volatile uint8_t *)((reg)) = val;				\
-} while (0)
+void digctl_usb_clkgate(int);
 
-int emi_prep(void);
-int pinctrl_prep(void);
-int args_prep(void);
-void delay(unsigned int);
-void putchar(int);
-int getchar(void);
-
-/* Power prep. */
-void en_vbusvalid(void);
-int vbusvalid(void);
-void power_tune(void);
-void en_4p2_reg(void);
-void en_4p2_to_dcdc(void);
-void power_vddd_from_dcdc(int, int);
-void power_vdda_from_dcdc(int, int);
-void power_vddio_from_dcdc(int, int);
-void power_vddmem(int);
-
-/* Clock prep. */
-void en_pll(void);
-void set_hbus_div(unsigned int);
-void set_cpu_frac(unsigned int);
-void bypass_cpu(void);
-void set_emi_div(unsigned int);
-void set_emi_frac(unsigned int);
-void bypass_emi(void);
-void set_ssp_div(unsigned int);
-void set_io_frac(unsigned int);
-void bypass_ssp(void);
-void bypass_saif(void);
-
-#endif /* !_BOOTIMX23_COMMON_ */
+#endif /* !_ARM_IMX_IMX23_DIGCTLVAR_H_ */
