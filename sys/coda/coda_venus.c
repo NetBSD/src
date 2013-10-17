371f5d6fb9181b6f9be17939bd3a64502aad307b
christos <christos@NetBSD.org>
2013-10-17 20:56:02 +0000

    remove unused code from macro

--- sys/coda/coda_venus.c
+++ sys/coda/coda_venus.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: coda_venus.c,v 1.29 2012/04/26 03:04:54 christos Exp $	*/
+/*	$NetBSD: coda_venus.c,v 1.30 2013/10/17 20:56:02 christos Exp $	*/
 
 /*
  *
@@ -32,7 +32,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: coda_venus.c,v 1.29 2012/04/26 03:04:54 christos Exp $");
+__KERNEL_RCSID(0, "$NetBSD: coda_venus.c,v 1.30 2013/10/17 20:56:02 christos Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -79,7 +79,6 @@ __KERNEL_RCSID(0, "$NetBSD: coda_venus.c,v 1.29 2012/04/26 03:04:54 christos Exp
 
 #define DECL_NO_OUT(name)				\
     struct name ## _in *inp;				\
-    struct coda_out_hdr *outp;				\
     int name ## _size = sizeof (struct name ## _in);	\
     int Isize = sizeof (struct name ## _in);		\
     int Osize = sizeof (struct coda_out_hdr);		\
@@ -101,7 +100,6 @@ __KERNEL_RCSID(0, "$NetBSD: coda_venus.c,v 1.29 2012/04/26 03:04:54 christos Exp
     if (Osize > name ## _size)				\
     	name ## _size = Osize;				\
     CODA_ALLOC(inp, struct name ## _in *, name ## _size);\
-    outp = (struct coda_out_hdr *) inp
 
 #define STRCPY(struc, name, len) \
     memcpy((char *)inp + (int)inp->struc, name, len); \
