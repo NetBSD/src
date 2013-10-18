c75b6b12cdd95faa7730df6d2d1ceb1d7bdee511
apb <apb@NetBSD.org>
2013-10-18 13:24:40 +0000

    When building gcc using clang, avoid trying to use __attribute__((__format__(__asm_fprintf__, ...))) because clang doesn't support that.

--- external/gpl3/gcc/dist/gcc/output.h
+++ external/gpl3/gcc/dist/gcc/output.h
@@ -115,7 +115,7 @@ extern void output_addr_const (FILE *, rtx);
 
 /* Output a string of assembler code, substituting numbers, strings
    and fixed syntactic prefixes.  */
-#if GCC_VERSION >= 3004
+#if GCC_VERSION >= 3004 && !defined(__clang__)
 #define ATTRIBUTE_ASM_FPRINTF(m, n) __attribute__ ((__format__ (__asm_fprintf__, m, n))) ATTRIBUTE_NONNULL(m)
 /* This is a magic identifier which allows GCC to figure out the type
    of HOST_WIDE_INT for %wd specifier checks.  You must issue this
