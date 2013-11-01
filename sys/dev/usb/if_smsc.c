41ba1948fe8afe1ee5cca8b7db769e0b631587e4
skrll <skrll@NetBSD.org>
2013-11-01 14:24:03 +0000

    Clean up (mostly remove unneeded) #includes

--- sys/dev/usb/if_smsc.c
+++ sys/dev/usb/if_smsc.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: if_smsc.c,v 1.11 2013/09/29 07:25:53 skrll Exp $	*/
+/*	$NetBSD: if_smsc.c,v 1.12 2013/11/01 14:24:03 skrll Exp $	*/
 
 /*	$OpenBSD: if_smsc.c,v 1.4 2012/09/27 12:38:11 jsg Exp $	*/
 /* $FreeBSD: src/sys/dev/usb/net/if_smsc.c,v 1.1 2012/08/15 04:03:55 gonzo Exp $ */
@@ -60,8 +60,9 @@
  * implemented.
  */
 
-#include "vlan.h"
-#include "opt_usb.h"
+#ifdef _KERNEL_OPT
+#include "opt_inet.h"
+#endif
 
 #include <sys/param.h>
 #include <sys/bus.h>
@@ -86,10 +87,7 @@
 
 #ifdef INET
 #include <netinet/in.h>
-#include <netinet/in_systm.h>
-#include <netinet/in_var.h>
-#include <netinet/ip.h>
-#include <netinet/if_ether.h>
+#include <netinet/if_inarp.h>
 #endif
 
 #include <dev/mii/mii.h>
