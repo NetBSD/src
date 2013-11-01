62d425b3f60edb331d3af66dcc2119216330a956
christos <christos@NetBSD.org>
2013-11-01 18:12:49 +0000

    make the code compilable with gcc-4.8.1; this loop has an array overflow, but it is only used for sparc register windows; ifdef it sparc for now, it's the easiest fix.

--- external/gpl3/gcc/dist/gcc/unwind-dw2.c
+++ external/gpl3/gcc/dist/gcc/unwind-dw2.c
@@ -1076,12 +1076,14 @@ execute_cfa_program (const unsigned char *insn_ptr,
 	  break;
 
 	case DW_CFA_GNU_window_save:
+#ifdef __sparc__
 	  /* ??? Hardcoded for SPARC register window configuration.  */
 	  for (reg = 16; reg < 32; ++reg)
 	    {
 	      fs->regs.reg[reg].how = REG_SAVED_OFFSET;
 	      fs->regs.reg[reg].loc.offset = (reg - 16) * sizeof (void *);
 	    }
+#endif
 	  break;
 
 	case DW_CFA_GNU_args_size:
