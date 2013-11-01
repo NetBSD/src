63f128b19a4fa7012881f20b6c972466b25f7cb6
christos <christos>
2013-11-01 18:13:29 +0000

    fix broken volatile assignment that gcc-4.8.1 does not like

--- external/gpl3/gcc/dist/libstdc++-v3/src/atomic.cc
+++ external/gpl3/gcc/dist/libstdc++-v3/src/atomic.cc
@@ -80,7 +80,7 @@ namespace std
     atomic_flag_test_and_set_explicit(__atomic_flag_base* __a,
 				      memory_order __m) throw()
     {
-      atomic_flag* d = static_cast<volatile atomic_flag*>(__a);
+      atomic_flag* d = static_cast<atomic_flag*>(__a);
       return d->test_and_set(__m);
     }
 
@@ -88,7 +88,7 @@ namespace std
     atomic_flag_clear_explicit(__atomic_flag_base* __a, 
 			       memory_order __m) throw()
     {
-      atomic_flag* d = static_cast<volatile atomic_flag*>(__a);
+      atomic_flag* d = static_cast<atomic_flag*>(__a);
       return d->clear(__m);
     }
 
