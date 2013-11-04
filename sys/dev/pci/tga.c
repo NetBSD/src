fc05803d4368be856e53ad0557b6259986b56649
christos <christos@NetBSD.org>
2013-11-04 16:54:12 +0000

    remove unused code

--- sys/dev/pci/tga.c
+++ sys/dev/pci/tga.c
@@ -1,4 +1,4 @@
-/* $NetBSD: tga.c,v 1.83 2012/01/11 20:50:00 macallan Exp $ */
+/* $NetBSD: tga.c,v 1.84 2013/11/04 16:54:12 christos Exp $ */
 
 /*
  * Copyright (c) 1995, 1996 Carnegie-Mellon University.
@@ -28,7 +28,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: tga.c,v 1.83 2012/01/11 20:50:00 macallan Exp $");
+__KERNEL_RCSID(0, "$NetBSD: tga.c,v 1.84 2013/11/04 16:54:12 christos Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -1025,7 +1025,7 @@ tga_rop_vtov(struct rasops_info *dst, int dx, int dy, int w, int h, int rop,
 	struct tga_devconfig *dc = dst->ri_hw;
 	int srcb, dstb, tga_srcb, tga_dstb;
 	int x, y, wb;
-	int xstart, xend, xdir;
+	int xstart, xdir;
 	int ystart, yend, ydir, yinc;
 	int xleft, lastx, lastleft;
 	int offset = 1 * dc->dc_tgaconf->tgac_vvbr_units;
@@ -1063,11 +1063,9 @@ tga_rop_vtov(struct rasops_info *dst, int dx, int dy, int w, int h, int rop,
         wb = w * (dst->ri_depth / 8);
 	if (sx >= dx || (sx + w) <= dx) {	/* copy forwards */
 		xstart = 0;
-		xend = wb;
 		xdir = 1;
 	} else {				/* copy backwards */
 		xstart = wb;
-		xend = 0;
 		xdir = -1;
 	}
 
