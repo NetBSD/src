97368448197abb6b1b987a8a7f7a24663cd8ce28
mrg <mrg@NetBSD.org>
2013-11-06 20:19:03 +0000

    gcc 4.8 issues: - avoid running over the end of an array (this is a real bug, but   i didn't really look closely at what memory is clobbered.  it   may not actually matter.) - move variables inside their #if usage.

--- sys/arch/x86/x86/mpbios.c
+++ sys/arch/x86/x86/mpbios.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: mpbios.c,v 1.61 2013/08/21 16:37:31 christos Exp $	*/
+/*	$NetBSD: mpbios.c,v 1.62 2013/11/06 20:19:03 mrg Exp $	*/
 
 /*-
  * Copyright (c) 2000 The NetBSD Foundation, Inc.
@@ -96,7 +96,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: mpbios.c,v 1.61 2013/08/21 16:37:31 christos Exp $");
+__KERNEL_RCSID(0, "$NetBSD: mpbios.c,v 1.62 2013/11/06 20:19:03 mrg Exp $");
 
 #include "acpica.h"
 #include "lapic.h"
@@ -552,7 +552,9 @@ mpbios_scan(device_t self, int *ncpup)
 	int		count;
 	int		type;
 	int		intr_cnt, cur_intr;
+#if NLAPIC > 0
 	paddr_t		lapic_base;
+#endif
 	const struct dflt_conf_entry *dflt_conf;
 	const int *dflt_bus_irq;
 	const struct mpbios_int *iep;
@@ -580,11 +582,11 @@ mpbios_scan(device_t self, int *ncpup)
 #if NACPICA > 0
 	if (mpacpi_ncpu == 0) {
 #endif
+#if NLAPIC > 0
 		lapic_base = LAPIC_BASE;
 		if (mp_cth != NULL)
 			lapic_base = (paddr_t)mp_cth->apic_address;
 
-#if NLAPIC > 0
 		lapic_boot_init(lapic_base);
 #endif
 #if NACPICA > 0
--- sys/arch/x86/x86/pmap.c
+++ sys/arch/x86/x86/pmap.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: pmap.c,v 1.180 2013/10/05 16:50:31 rmind Exp $	*/
+/*	$NetBSD: pmap.c,v 1.181 2013/11/06 20:19:03 mrg Exp $	*/
 
 /*-
  * Copyright (c) 2008, 2010 The NetBSD Foundation, Inc.
@@ -171,7 +171,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: pmap.c,v 1.180 2013/10/05 16:50:31 rmind Exp $");
+__KERNEL_RCSID(0, "$NetBSD: pmap.c,v 1.181 2013/11/06 20:19:03 mrg Exp $");
 
 #include "opt_user_ldt.h"
 #include "opt_lockdebug.h"
@@ -505,8 +505,8 @@ extern int mem_cluster_cnt;
 #define PTESLEW(pte, id) ((pte)+(id)*NPTECL)
 #define VASLEW(va,id) ((va)+(id)*NPTECL*PAGE_SIZE)
 #else
-#define PTESLEW(pte, id) (pte)
-#define VASLEW(va,id) (va)
+#define PTESLEW(pte, id) ((void)id, pte)
+#define VASLEW(va,id) ((void)id, va)
 #endif
 
 /*
@@ -4220,9 +4220,10 @@ pmap_growkernel(vaddr_t maxkvaddr)
 	struct pmap *kpm = pmap_kernel();
 #if !defined(XEN) || !defined(__x86_64__)
 	struct pmap *pm;
+	long old;
 #endif
 	int s, i;
-	long needed_kptp[PTP_LEVELS], target_nptp, old;
+	long needed_kptp[PTP_LEVELS], target_nptp;
 	bool invalidate = false;
 
 	s = splvm();	/* to be safe */
@@ -4235,7 +4236,10 @@ pmap_growkernel(vaddr_t maxkvaddr)
 	}
 
 	maxkvaddr = x86_round_pdr(maxkvaddr);
+#if !defined(XEN) || !defined(__x86_64__)
 	old = nkptp[PTP_LEVELS - 1];
+#endif
+
 	/*
 	 * This loop could be optimized more, but pmap_growkernel()
 	 * is called infrequently.
