95ea4fb70588d15f2f1791280701c987da8a45bc
christos <christos@NetBSD.org>
2013-11-14 01:14:39 +0000

    CID 1102811: Unitialized variable. Don't pass random sun_len for the BSD's, zero the whole structure as recommended for portability.

--- external/gpl3/gdb/dist/gdb/common/agent.c
+++ external/gpl3/gdb/dist/gdb/common/agent.c
@@ -175,6 +175,7 @@ gdb_connect_sync_socket (int pid)
       return -1;
     }
 
+  memset(&addr, 0, sizeof(addr));
   addr.sun_family = AF_UNIX;
 
   res = xsnprintf (addr.sun_path, UNIX_PATH_MAX, "%s", path);
