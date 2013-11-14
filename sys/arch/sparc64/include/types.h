62dabd97f35a872f2098b6dde82157752d4d3277
martin <martin>
2013-11-14 12:11:13 +0000

    Allow TOPDOWN-VM only for binaries compiled with appropriate code model. No change yet, as __HAVE_TOPDOWN is not yet defined.

--- sys/arch/sparc64/include/types.h
+++ sys/arch/sparc64/include/types.h
@@ -1,3 +1,7 @@
-/*       $NetBSD: types.h,v 1.25 2005/12/11 12:19:10 christos Exp $        */
+/*       $NetBSD: types.h,v 1.26 2013/11/14 12:11:13 martin Exp $        */
 
 #include <sparc/types.h>
+
+#ifdef __arch64__
+#define	MD_TOPDOWN_INIT(epp)	/* no topdown VM flag for exec by default */
+#endif
