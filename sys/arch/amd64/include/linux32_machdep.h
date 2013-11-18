29a841b37168b522ee61ef588c44352bc15584a1
chs <chs@NetBSD.org>
2013-11-18 01:32:52 +0000

    implement the *at() syscalls. bring the unimplemented syscall list up to date.

--- sys/arch/amd64/include/linux32_machdep.h
+++ sys/arch/amd64/include/linux32_machdep.h
@@ -1,10 +1,11 @@
-/*	$NetBSD: linux32_machdep.h,v 1.2 2011/11/18 04:09:17 christos Exp $ */
+/*	$NetBSD: linux32_machdep.h,v 1.3 2013/11/18 01:32:52 chs Exp $ */
 
 #ifndef _MACHINE_LINUX32_H_
 #define _MACHINE_LINUX32_H_
 
 #include <compat/netbsd32/netbsd32.h>
 
+#include <compat/linux/common/linux_types.h>
 #include <compat/linux32/common/linux32_types.h>
 
 #include <compat/linux32/arch/amd64/linux32_siginfo.h>
