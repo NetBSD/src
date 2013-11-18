41f9717ed293c0908d648607919faad9f4225c25
njoly <njoly@NetBSD.org>
2013-11-18 18:45:29 +0000

    Do lwp0 initialisation as early as possible, right after rumpuser_init(). This makes rump applications work again when RUMP_LOCKDEBUG is enabled.
    ok pooka.

--- sys/rump/librump/rumpkern/rump.c
+++ sys/rump/librump/rumpkern/rump.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: rump.c,v 1.275 2013/09/09 19:18:08 pooka Exp $	*/
+/*	$NetBSD: rump.c,v 1.276 2013/11/18 18:45:29 njoly Exp $	*/
 
 /*
  * Copyright (c) 2007-2011 Antti Kantee.  All Rights Reserved.
@@ -26,7 +26,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: rump.c,v 1.275 2013/09/09 19:18:08 pooka Exp $");
+__KERNEL_RCSID(0, "$NetBSD: rump.c,v 1.276 2013/11/18 18:45:29 njoly Exp $");
 
 #include <sys/systm.h>
 #define ELFSIZE ARCH_ELFSIZE
@@ -268,6 +268,16 @@ rump_init(void)
 		return EINVAL;
 	}
 
+	/* init minimal lwp/cpu context */
+	l = &lwp0;
+	l->l_lid = 1;
+	l->l_cpu = l->l_target_cpu = rump_cpu;
+	l->l_fd = &filedesc0;
+
+	/* lwp0 isn't created like other threads, so notify hypervisor here */
+	rumpuser_curlwpop(RUMPUSER_LWP_CREATE, l);
+	rumpuser_curlwpop(RUMPUSER_LWP_SET, l);
+
 	/* retrieve env vars which affect the early stage of bootstrap */
 	if (rumpuser_getparam("RUMP_THREADS", buf, sizeof(buf)) == 0) {
 		rump_threads = *buf != '0';
@@ -296,16 +306,6 @@ rump_init(void)
 	aprint_verbose("%s%s", copyright, version);
 
 	rump_intr_init(numcpu);
-
-	/* init minimal lwp/cpu context */
-	l = &lwp0;
-	l->l_lid = 1;
-	l->l_cpu = l->l_target_cpu = rump_cpu;
-	l->l_fd = &filedesc0;
-
-	/* lwp0 isn't created like other threads, so notify hypervisor here */
-	rumpuser_curlwpop(RUMPUSER_LWP_CREATE, l);
-	rumpuser_curlwpop(RUMPUSER_LWP_SET, l);
 
 	rump_tsleep_init();
 
