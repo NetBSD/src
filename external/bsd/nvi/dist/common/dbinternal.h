c080ee1640ea41c48955644d32005b4621df351a
christos <christos>
2013-11-29 22:56:19 +0000

    Bring back the compatibility db3->db1 code from the old vi. This is selected with -DUSE_DB1 in the Makefile. By default use use the db1 specific code from the current nvi which is enabled using -DUSE_BUNDLED_DB. Unfortunately recovery seems to be broken for both.

--- external/bsd/nvi/dist/common/dbinternal.h
+++ external/bsd/nvi/dist/common/dbinternal.h
@@ -1,5 +1,5 @@
-/*	$NetBSD: dbinternal.h,v 1.1 2013/11/22 15:52:05 christos Exp $	*/
-#ifndef db_env_create
+/*	$NetBSD: dbinternal.h,v 1.2 2013/11/29 22:56:19 christos Exp $	*/
+#if !defined(db_env_create) || defined(USE_DB1)
 int db_env_create(DB_ENV **, u_int32_t);
 #endif
 int db_create(DB **, DB_ENV *, u_int32_t);
