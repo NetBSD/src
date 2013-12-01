e6c90101264cad60d0f5c9eb0c525a47035fc906
christos <christos@NetBSD.org>
2013-12-01 19:28:08 +0000

    from ftp.astron.com, this is a bug fix release:        * always leave magic file loaded, don't unload for magic_check, etc.        * fix default encoding to binary instead of unknown which broke recently        * handle empty and one byte files, less specially so that          --mime-encoding does not break completely.        * fix erroneous non-zero exit code from non-existant file and message        * add CDF MSI file detection (Guy Helmer)

--- external/bsd/file/dist/src/pread.c
+++ external/bsd/file/dist/src/pread.c
@@ -1,18 +1,18 @@
-/*	$NetBSD: pread.c,v 1.1.1.1 2013/03/23 15:49:15 christos Exp $	*/
+/*	$NetBSD: pread.c,v 1.1.1.2 2013/12/01 19:28:16 christos Exp $	*/
 
 #include "file.h"
 #ifndef lint
 #if 0
-FILE_RCSID("@(#)$File: pread.c,v 1.1 2013/02/18 15:40:59 christos Exp $")
+FILE_RCSID("@(#)$File: pread.c,v 1.2 2013/04/02 16:23:07 christos Exp $")
 #else
-__RCSID("$NetBSD: pread.c,v 1.1.1.1 2013/03/23 15:49:15 christos Exp $");
+__RCSID("$NetBSD: pread.c,v 1.1.1.2 2013/12/01 19:28:16 christos Exp $");
 #endif
 #endif  /* lint */
 #include <fcntl.h>
 #include <unistd.h>
 
 ssize_t
-pread(int fd, void *buf, ssize_t len, off_t off) {
+pread(int fd, void *buf, size_t len, off_t off) {
 	if (lseek(fd, off, SEEK_SET) == (off_t)-1)
 		return -1;
 
