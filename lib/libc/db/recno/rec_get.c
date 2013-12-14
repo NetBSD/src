54638a48c760d33b9d20dd758254e3c2596530a5
christos <christos@NetBSD.org>
2013-12-14 18:04:56 +0000

    It's been many years since realloc(NULL, size) == malloc(size). Also don't destroy pointers on allocation errors so someone can free them later.

--- lib/libc/db/recno/rec_get.c
+++ lib/libc/db/recno/rec_get.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: rec_get.c,v 1.16 2008/09/11 12:58:00 joerg Exp $	*/
+/*	$NetBSD: rec_get.c,v 1.17 2013/12/14 18:04:56 christos Exp $	*/
 
 /*-
  * Copyright (c) 1990, 1993, 1994
@@ -34,7 +34,7 @@
 #endif
 
 #include <sys/cdefs.h>
-__RCSID("$NetBSD: rec_get.c,v 1.16 2008/09/11 12:58:00 joerg Exp $");
+__RCSID("$NetBSD: rec_get.c,v 1.17 2013/12/14 18:04:56 christos Exp $");
 
 #include "namespace.h"
 #include <sys/types.h>
@@ -127,11 +127,10 @@ __rec_fpipe(BTREE *t, recno_t top)
 	uint8_t *p;
 
 	if (t->bt_rdata.size < t->bt_reclen) {
-		t->bt_rdata.data = t->bt_rdata.data == NULL ?
-		    malloc(t->bt_reclen) :
-		    realloc(t->bt_rdata.data, t->bt_reclen);
-		if (t->bt_rdata.data == NULL)
+		void *np = realloc(t->bt_rdata.data, t->bt_reclen);
+		if (np == NULL)
 			return (RET_ERROR);
+		t->bt_rdata.data = np;
 		t->bt_rdata.size = t->bt_reclen;
 	}
 	data.data = t->bt_rdata.data;
@@ -196,13 +195,14 @@ __rec_vpipe(BTREE *t, recno_t top)
 				break;
 			}
 			if (sz == 0) {
+				void *np;
 				len = p - (uint8_t *)t->bt_rdata.data;
-				t->bt_rdata.size += (sz = 256);
-				t->bt_rdata.data = t->bt_rdata.data == NULL ?
-				    malloc(t->bt_rdata.size) :
-				    realloc(t->bt_rdata.data, t->bt_rdata.size);
-				if (t->bt_rdata.data == NULL)
+				sz = t->bt_rdata.size + 256;
+				np = realloc(t->bt_rdata.data, sz);
+				if (np == NULL)
 					return (RET_ERROR);
+				t->bt_rdata.size = sz;
+				t->bt_rdata.data = np;
 				p = (uint8_t *)t->bt_rdata.data + len;
 			}
 		}
@@ -235,11 +235,10 @@ __rec_fmap(BTREE *t, recno_t top)
 	size_t len;
 
 	if (t->bt_rdata.size < t->bt_reclen) {
-		t->bt_rdata.data = t->bt_rdata.data == NULL ?
-		    malloc(t->bt_reclen) :
-		    realloc(t->bt_rdata.data, t->bt_reclen);
-		if (t->bt_rdata.data == NULL)
+		void *np = realloc(t->bt_rdata.data, t->bt_reclen);
+		if (np == NULL)
 			return (RET_ERROR);
+		t->bt_rdata.data = np;
 		t->bt_rdata.size = t->bt_reclen;
 	}
 	data.data = t->bt_rdata.data;
