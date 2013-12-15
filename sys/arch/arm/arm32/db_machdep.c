3478d4668436ea6eec417ab748e929641af781fc
skrll <skrll@NetBSD.org>
2013-12-15 09:14:09 +0000

    Trailing whitespace

--- sys/arch/arm/arm32/db_machdep.c
+++ sys/arch/arm/arm32/db_machdep.c
@@ -1,35 +1,35 @@
-/*	$NetBSD: db_machdep.c,v 1.16 2013/01/05 15:06:51 christos Exp $	*/
+/*	$NetBSD: db_machdep.c,v 1.17 2013/12/15 09:14:09 skrll Exp $	*/
 
-/* 
+/*
  * Copyright (c) 1996 Mark Brinicombe
  *
  * Mach Operating System
  * Copyright (c) 1991,1990 Carnegie Mellon University
  * All Rights Reserved.
- * 
+ *
  * Permission to use, copy, modify and distribute this software and its
  * documentation is hereby granted, provided that both the copyright
  * notice and this permission notice appear in all copies of the
  * software, derivative works or modified versions, and any portions
  * thereof, and that both notices appear in supporting documentation.
- * 
+ *
  * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS "AS IS"
  * CONDITION.  CARNEGIE MELLON DISCLAIMS ANY LIABILITY OF ANY KIND FOR
  * ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.
- * 
+ *
  * Carnegie Mellon requests users of this software to return to
- * 
+ *
  *  Software Distribution Coordinator  or  Software.Distribution@CS.CMU.EDU
  *  School of Computer Science
  *  Carnegie Mellon University
  *  Pittsburgh PA 15213-3890
- * 
+ *
  * any improvements or extensions that they make and grant Carnegie the
  * rights to redistribute these changes.
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: db_machdep.c,v 1.16 2013/01/05 15:06:51 christos Exp $");
+__KERNEL_RCSID(0, "$NetBSD: db_machdep.c,v 1.17 2013/12/15 09:14:09 skrll Exp $");
 
 #include <sys/param.h>
 #include <sys/proc.h>
