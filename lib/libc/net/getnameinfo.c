017b9d3ce440f866afb553f4a2099a3c105f9fe3
bouyer <bouyer@NetBSD.org>
2013-12-18 22:39:32 +0000

    Pullup the following revisions, requested by bad in ticket 975: 	lib/libc/net/gethnamaddr.c			1.79-1.81, 1.84 	lib/libc/net/getnameinfo.3			1.37-1.40 	lib/libc/net/getnameinfo.c			1.51, 1.52, 1.54 	lib/libc/net/hostent.h				1.1, 1.2 	lib/libc/net/sethostent.c			1.17-1.19
    Make getnameinfo(3) re-entrant (ahem __ypdomain).
    Add not advertised reentrant functions: {get,set,end}hostent_r,
    gethostbyname{,2}_r, gethostbyaddr_r. Make getnameinfo(3) use
    gethostbyaddr_r(3).
    Implement no-check-names.

--- lib/libc/net/getnameinfo.c
+++ lib/libc/net/getnameinfo.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: getnameinfo.c,v 1.50 2010/06/29 14:44:19 seanb Exp $	*/
+/*	$NetBSD: getnameinfo.c,v 1.50.8.1 2013/12/18 22:39:32 bouyer Exp $	*/
 /*	$KAME: getnameinfo.c,v 1.45 2000/09/25 22:43:56 itojun Exp $	*/
 
 /*
@@ -47,7 +47,7 @@
 
 #include <sys/cdefs.h>
 #if defined(LIBC_SCCS) && !defined(lint)
-__RCSID("$NetBSD: getnameinfo.c,v 1.50 2010/06/29 14:44:19 seanb Exp $");
+__RCSID("$NetBSD: getnameinfo.c,v 1.50.8.1 2013/12/18 22:39:32 bouyer Exp $");
 #endif /* LIBC_SCCS and not lint */
 
 #include "namespace.h"
@@ -69,6 +69,7 @@ __RCSID("$NetBSD: getnameinfo.c,v 1.50 2010/06/29 14:44:19 seanb Exp $");
 #include <string.h>
 
 #include "servent.h"
+#include "hostent.h"
 
 #ifdef __weak_alias
 __weak_alias(getnameinfo,_getnameinfo)
@@ -95,32 +96,29 @@ struct sockinet {
 	u_short	si_port;
 };
 
-static int getnameinfo_inet __P((const struct sockaddr *, socklen_t, char *,
-    socklen_t, char *, socklen_t, int));
+static int getnameinfo_inet(const struct sockaddr *, socklen_t, char *,
+    socklen_t, char *, socklen_t, int);
 #ifdef INET6
-static int ip6_parsenumeric __P((const struct sockaddr *, const char *, char *,
-				 socklen_t, int));
-static int ip6_sa2str __P((const struct sockaddr_in6 *, char *, size_t,
-				 int));
+static int ip6_parsenumeric(const struct sockaddr *, const char *, char *,
+				 socklen_t, int);
+static int ip6_sa2str(const struct sockaddr_in6 *, char *, size_t, int);
 #endif
-static int getnameinfo_atalk __P((const struct sockaddr *, socklen_t, char *,
-    socklen_t, char *, socklen_t, int));
+static int getnameinfo_atalk(const struct sockaddr *, socklen_t, char *,
+    socklen_t, char *, socklen_t, int);
 
-static int getnameinfo_link __P((const struct sockaddr *, socklen_t, char *,
-    socklen_t, char *, socklen_t, int));
-static int hexname __P((const u_int8_t *, size_t, char *, socklen_t));
+static int getnameinfo_link(const struct sockaddr *, socklen_t, char *,
+    socklen_t, char *, socklen_t, int);
+static int hexname(const uint8_t *, size_t, char *, socklen_t);
 
 /*
  * Top-level getnameinfo() code.  Look at the address family, and pick an
  * appropriate function to call.
  */
 int
-getnameinfo(sa, salen, host, hostlen, serv, servlen, flags)
-	const struct sockaddr *sa;
-	socklen_t salen;
-	char *host, *serv;
-	socklen_t hostlen, servlen;
-	int flags;
+getnameinfo(const struct sockaddr *sa, socklen_t salen,
+	char *host, socklen_t hostlen,
+	char *serv, socklen_t servlen,
+	int flags)
 {
 
 	switch (sa->sa_family) {
@@ -204,14 +202,10 @@ errout:
  * Format an IPv4 or IPv6 sockaddr into a printable string.
  */
 static int
-getnameinfo_inet(sa, salen, host, hostlen, serv, servlen, flags)
-	const struct sockaddr *sa;
-	socklen_t salen;
-	char *host;
-	socklen_t hostlen;
-	char *serv;
-	socklen_t servlen;
-	int flags;
+getnameinfo_inet(const struct sockaddr *sa, socklen_t salen,
+	char *host, socklen_t hostlen,
+	char *serv, socklen_t servlen,
+	int flags)
 {
 	const struct afd *afd;
 	struct servent *sp;
@@ -219,7 +213,7 @@ getnameinfo_inet(sa, salen, host, hostlen, serv, servlen, flags)
 	u_short port;
 	int family, i;
 	const char *addr;
-	u_int32_t v4a;
+	uint32_t v4a;
 	char numserv[512];
 	char numaddr[512];
 
@@ -281,7 +275,7 @@ getnameinfo_inet(sa, salen, host, hostlen, serv, servlen, flags)
 
 	switch (sa->sa_family) {
 	case AF_INET:
-		v4a = (u_int32_t)
+		v4a = (uint32_t)
 		    ntohl(((const struct sockaddr_in *)
 		    (const void *)sa)->sin_addr.s_addr);
 		if (IN_MULTICAST(v4a) || IN_EXPERIMENTAL(v4a))
@@ -343,8 +337,8 @@ getnameinfo_inet(sa, salen, host, hostlen, serv, servlen, flags)
 		}
 #endif
 		default:
-			if (inet_ntop(afd->a_af, addr, numaddr, sizeof(numaddr))
-			    == NULL)
+			if (inet_ntop(afd->a_af, addr, numaddr,
+			    (socklen_t)sizeof(numaddr)) == NULL)
 				return EAI_SYSTEM;
 			numaddrlen = strlen(numaddr);
 			if (numaddrlen + 1 > hostlen) /* don't forget terminator */
@@ -353,7 +347,11 @@ getnameinfo_inet(sa, salen, host, hostlen, serv, servlen, flags)
 			break;
 		}
 	} else {
-		hp = gethostbyaddr(addr, afd->a_addrlen, afd->a_af);
+		struct hostent hent;
+		char hbuf[4096];
+		int he;
+		hp = gethostbyaddr_r(addr, afd->a_addrlen, afd->a_af, &hent,
+		    hbuf, sizeof(hbuf), &he);
 
 		if (hp) {
 #if 0
@@ -401,12 +399,8 @@ getnameinfo_inet(sa, salen, host, hostlen, serv, servlen, flags)
 
 #ifdef INET6
 static int
-ip6_parsenumeric(sa, addr, host, hostlen, flags)
-	const struct sockaddr *sa;
-	const char *addr;
-	char *host;
-	socklen_t hostlen;
-	int flags;
+ip6_parsenumeric(const struct sockaddr *sa, const char *addr, char *host,
+	socklen_t hostlen, int flags)
 {
 	size_t numaddrlen;
 	char numaddr[512];
@@ -415,7 +409,8 @@ ip6_parsenumeric(sa, addr, host, hostlen, flags)
 	_DIAGASSERT(addr != NULL);
 	_DIAGASSERT(host != NULL);
 
-	if (inet_ntop(AF_INET6, addr, numaddr, sizeof(numaddr)) == NULL)
+	if (inet_ntop(AF_INET6, addr, numaddr, (socklen_t)sizeof(numaddr))
+	    == NULL)
 		return EAI_SYSTEM;
 
 	numaddrlen = strlen(numaddr);
@@ -446,11 +441,7 @@ ip6_parsenumeric(sa, addr, host, hostlen, flags)
 
 /* ARGSUSED */
 static int
-ip6_sa2str(sa6, buf, bufsiz, flags)
-	const struct sockaddr_in6 *sa6;
-	char *buf;
-	size_t bufsiz;
-	int flags;
+ip6_sa2str(const struct sockaddr_in6 *sa6, char *buf, size_t bufsiz, int flags)
 {
 	unsigned int ifindex;
 	const struct in6_addr *a6;
@@ -477,7 +468,7 @@ ip6_sa2str(sa6, buf, bufsiz, flags)
 	    bufsiz >= IF_NAMESIZE) {
 		char *p = if_indextoname(ifindex, buf);
 		if (p) {
-			return(strlen(p));
+			return (int)strlen(p);
 		}
 	}
 
@@ -564,17 +555,13 @@ getnameinfo_link(const struct sockaddr *sa, socklen_t salen,
 	case IFT_HIPPI:
 	case IFT_ISO88025:
 	default:
-		return hexname((const u_int8_t *)CLLADDR(sdl),
+		return hexname((const uint8_t *)CLLADDR(sdl),
 		    (size_t)sdl->sdl_alen, host, hostlen);
 	}
 }
 
 static int
-hexname(cp, len, host, hostlen)
-	const u_int8_t *cp;
-	char *host;
-	size_t len;
-	socklen_t hostlen;
+hexname(const uint8_t *cp, size_t len, char *host, socklen_t hostlen)
 {
 	int n;
 	size_t i;
