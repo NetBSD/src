15a64b1bc9f5536312b703030fefc1b1b27cfa68
kiyohara <kiyohara@NetBSD.org>
2013-12-21 15:20:39 +0000

    Add Marvell 88E1543.  Tested on OPENBLOCKS_AX3.

--- sys/dev/mii/makphy.c
+++ sys/dev/mii/makphy.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: makphy.c,v 1.37 2012/01/21 16:48:08 chs Exp $	*/
+/*	$NetBSD: makphy.c,v 1.38 2013/12/21 15:20:39 kiyohara Exp $	*/
 
 /*-
  * Copyright (c) 1998, 1999, 2000, 2001 The NetBSD Foundation, Inc.
@@ -59,7 +59,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: makphy.c,v 1.37 2012/01/21 16:48:08 chs Exp $");
+__KERNEL_RCSID(0, "$NetBSD: makphy.c,v 1.38 2013/12/21 15:20:39 kiyohara Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -121,6 +121,9 @@ static const struct mii_phydesc makphys[] = {
 
 	{ MII_OUI_xxMARVELL,		MII_MODEL_xxMARVELL_E1116R_29,
 	  MII_STR_xxMARVELL_E1116R_29 },
+
+	{ MII_OUI_xxMARVELL,		MII_MODEL_xxMARVELL_E1543,
+	  MII_STR_xxMARVELL_E1543 },
 
 	{ 0,				0,
 	  NULL },
