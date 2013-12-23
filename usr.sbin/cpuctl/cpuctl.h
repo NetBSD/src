d96997aaa3869c93619f2c5afa25dcdc6daeb0e6
msaitoh <msaitoh@NetBSD.org>
2013-12-23 12:35:33 +0000

    Add verbose flag. On x86 cpu, cpuctl -v identify dumps the return values of the cpuid functions. The max levels are taken from CPUID 0 and CPUID 8000_0000. It's useful for the future CPU.

--- usr.sbin/cpuctl/cpuctl.h
+++ usr.sbin/cpuctl/cpuctl.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: cpuctl.h,v 1.4 2012/08/29 17:13:23 drochner Exp $	*/
+/*	$NetBSD: cpuctl.h,v 1.5 2013/12/23 12:35:33 msaitoh Exp $	*/
 
 /*-
  * Copyright (c) 2008 The NetBSD Foundation, Inc.
@@ -35,3 +35,5 @@ int	aprint_error_dev(const char *, const char *, ...) __printflike(2, 3);
 
 void	identifycpu(int, const char *);
 int	ucodeupdate_check(int, struct cpu_ucode *);
+
+extern int verbose;
