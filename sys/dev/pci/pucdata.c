ce410e416a71ccad6034fb59faaf3561d6db36cc
msaitoh <msaitoh>
2013-12-25 15:17:56 +0000

    Add support S1200's UART. Part of PR#48016.

--- sys/dev/pci/pucdata.c
+++ sys/dev/pci/pucdata.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: pucdata.c,v 1.87 2013/11/07 15:55:08 msaitoh Exp $	*/
+/*	$NetBSD: pucdata.c,v 1.88 2013/12/25 15:17:56 msaitoh Exp $	*/
 
 /*
  * Copyright (c) 1998, 1999 Christopher G. Demetriou.  All rights reserved.
@@ -36,7 +36,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: pucdata.c,v 1.87 2013/11/07 15:55:08 msaitoh Exp $");
+__KERNEL_RCSID(0, "$NetBSD: pucdata.c,v 1.88 2013/12/25 15:17:56 msaitoh Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -1845,6 +1845,15 @@ const struct puc_device_description puc_devices[] = {
 	/* Intel EG20T UART */
 	{   "Intel EG20T UART #3",
 	    {	PCI_VENDOR_INTEL, PCI_PRODUCT_INTEL_EG20T_UART_3, 0, 3 },
+	    {	0xffff,	0xffff,	0,	0	},
+	    {
+		{ PUC_PORT_TYPE_COM, 0x10, 0x00, COM_FREQ },
+	    },
+	},
+
+	/* Intel S1200 UART */
+	{   "Intel S1200 UART",
+	    {	PCI_VENDOR_INTEL, PCI_PRODUCT_INTEL_S1200_UART, 0, 0 },
 	    {	0xffff,	0xffff,	0,	0	},
 	    {
 		{ PUC_PORT_TYPE_COM, 0x10, 0x00, COM_FREQ },
