4da823eff6482b5855f7ec387c6a0f523aee8780
christos <christos@NetBSD.org>
2013-12-27 23:30:11 +0000

    import devel ntpd to avoid amplification attacks.

--- external/bsd/ntp/dist/lib/isc/include/isc/msgcat.h
+++ external/bsd/ntp/dist/lib/isc/include/isc/msgcat.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: msgcat.h,v 1.1.1.1 2009/12/13 16:54:28 kardel Exp $	*/
+/*	$NetBSD: msgcat.h,v 1.1.1.2 2013/12/27 23:30:43 christos Exp $	*/
 
 /*
  * Copyright (C) 2004, 2005, 2007  Internet Systems Consortium, Inc. ("ISC")
@@ -17,7 +17,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* Id: msgcat.h,v 1.13 2007/06/19 23:47:18 tbox Exp */
+/* Id: msgcat.h,v 1.13 2007/06/19 23:47:18 tbox Exp  */
 
 #ifndef ISC_MSGCAT_H
 #define ISC_MSGCAT_H 1
--- external/bsd/ntp/dist/lib/isc/win32/os.c
+++ external/bsd/ntp/dist/lib/isc/win32/os.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: os.c,v 1.1.1.1 2009/12/13 16:54:44 kardel Exp $	*/
+/*	$NetBSD: os.c,v 1.1.1.2 2013/12/27 23:30:41 christos Exp $	*/
 
 /*
  * Copyright (C) 2004, 2007  Internet Systems Consortium, Inc. ("ISC")
@@ -17,7 +17,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* Id: os.c,v 1.8 2007/06/19 23:47:19 tbox Exp */
+/* Id: os.c,v 1.8 2007/06/19 23:47:19 tbox Exp  */
 
 #include <windows.h>
 
