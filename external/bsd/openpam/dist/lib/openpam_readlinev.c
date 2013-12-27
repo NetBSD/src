8a70f57e4d05862d97fff994f8c29f045578ffc4
christos <christos@NetBSD.org>
2013-12-27 19:27:40 +0000

    Import openpam-20130907

--- external/bsd/openpam/dist/lib/openpam_readlinev.c
+++ external/bsd/openpam/dist/lib/openpam_readlinev.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: openpam_readlinev.c,v 1.1.1.1 2013/04/06 01:23:31 christos Exp $	*/
+/*	$NetBSD: openpam_readlinev.c,v 1.1.1.2 2013/12/27 19:27:41 christos Exp $	*/
 
 /*-
  * Copyright (c) 2012 Dag-Erling Smørgrav
@@ -8,8 +8,7 @@
  * modification, are permitted provided that the following conditions
  * are met:
  * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer
- *    in this position and unchanged.
+ *    notice, this list of conditions and the following disclaimer.
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
@@ -29,7 +28,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- * Id: openpam_readlinev.c 588 2012-04-08 11:52:25Z des 
+ * Id: openpam_readlinev.c 648 2013-03-05 17:54:27Z des 
  */
 
 #ifdef HAVE_CONFIG_H
