2b3787f6372d783bbdc1dc241d42a308edf49a7a
christos <christos@NetBSD.org>
2013-12-27 23:30:11 +0000

    import devel ntpd to avoid amplification attacks.

--- external/bsd/ntp/dist/lib/isc/win32/resource.c
+++ external/bsd/ntp/dist/lib/isc/win32/resource.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: resource.c,v 1.1.1.1 2009/12/13 16:54:44 kardel Exp $	*/
+/*	$NetBSD: resource.c,v 1.1.1.2 2013/12/27 23:30:40 christos Exp $	*/
 
 /*
  * Copyright (C) 2004, 2007, 2008  Internet Systems Consortium, Inc. ("ISC")
@@ -17,7 +17,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* Id: resource.c,v 1.10 2008/07/11 23:47:09 tbox Exp */
+/* Id: resource.c,v 1.10 2008/07/11 23:47:09 tbox Exp  */
 
 #include <config.h>
 
