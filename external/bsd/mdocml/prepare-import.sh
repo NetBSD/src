b33af43f88adb4e906da3eb38b8f22ff6477a5e5
christos <christos@NetBSD.org>
2013-12-29 00:23:34 +0000

    Use cleantags to remove RCS tags, missed the NEWS file before.

--- external/bsd/mdocml/prepare-import.sh
+++ external/bsd/mdocml/prepare-import.sh
@@ -1,4 +1,4 @@
-#/bin/sh
+#!/bin/sh
 
 set -e
 
@@ -7,7 +7,7 @@ rm -rf ChangeLog.xsl style.css index.css *.sgml
 uuencode external.png < external.png > external.png.uu
 rm external.png
 
+cleantags .
 for f in [a-z]*; do
-	sed -e 's/[$]Id:/\$Vendor-Id:/' \
-	    -e 's/[$]Mdocdate: \([^$]*\) \([0-9][0-9][0-9][0-9]\) [$]/\1, \2/' < $f > $f.new && mv $f.new $f
+	sed -e 's/[$]Mdocdate: \([^$]*\) \([0-9][0-9][0-9][0-9]\) [$]/\1, \2/' < $f > $f.new && mv $f.new $f
 done
