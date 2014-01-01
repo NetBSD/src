df4ef199b08e02c61b53623ab35cfc6e20dcba4b
christos <christos@NetBSD.org>
2014-01-01 16:55:28 +0000

    explain the previous fix.

--- bin/sh/parser.c
+++ bin/sh/parser.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: parser.c,v 1.86 2013/12/31 22:53:57 christos Exp $	*/
+/*	$NetBSD: parser.c,v 1.87 2014/01/01 16:55:28 christos Exp $	*/
 
 /*-
  * Copyright (c) 1991, 1993
@@ -37,7 +37,7 @@
 #if 0
 static char sccsid[] = "@(#)parser.c	8.7 (Berkeley) 5/16/95";
 #else
-__RCSID("$NetBSD: parser.c,v 1.86 2013/12/31 22:53:57 christos Exp $");
+__RCSID("$NetBSD: parser.c,v 1.87 2014/01/01 16:55:28 christos Exp $");
 #endif
 #endif /* not lint */
 
@@ -432,7 +432,14 @@ TRACE(("expecting DO got %s %s\n", tokname[got], got == TWORD ? wordtext : ""));
 		cpp = &n1->ncase.cases;
 		noalias = 1;
 		checkkwd = 2, readtoken();
-		while(lasttoken != TESAC) {
+		/*
+		 * This should be a do {} while loop as it was originally
+		 * since POSIX mandates at least one case statement, but
+		 * unfortunately both ksh and bash accept 'case x in esac'
+		 * so configure scripts started taking advantage of this.
+		 * so do we.
+		 */
+		while (lasttoken != TESAC) {
 			*cpp = cp = (union node *)stalloc(sizeof (struct nclist));
 			if (lasttoken == TLP)
 				readtoken();
