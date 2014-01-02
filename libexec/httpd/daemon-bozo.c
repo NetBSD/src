a0e6b15ed136ebeee0eeb33fc6335f073815c0e4
mrg <mrg@NetBSD.org>
2014-01-02 08:21:38 +0000

    - update CHANGES with recent changes - update version to 20140102 - update copyrights - use getcwd() over getwd() - fix lean build (don't include lua)

--- libexec/httpd/daemon-bozo.c
+++ libexec/httpd/daemon-bozo.c
@@ -1,9 +1,9 @@
-/*	$NetBSD: daemon-bozo.c,v 1.15 2011/11/18 09:51:31 mrg Exp $	*/
+/*	$NetBSD: daemon-bozo.c,v 1.16 2014/01/02 08:21:38 mrg Exp $	*/
 
 /*	$eterna: daemon-bozo.c,v 1.24 2011/11/18 09:21:15 mrg Exp $	*/
 
 /*
- * Copyright (c) 1997-2011 Matthew R. Green
+ * Copyright (c) 1997-2014 Matthew R. Green
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
