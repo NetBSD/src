6d322f2f4598f0d8a138f10ea648ec4fabe41f8b
joerg <joerg@NetBSD.org>
2014-01-05 16:38:05 +0000

    Update LLVM family to 3.5svn r198450. Interesting changes include: - Improvements in the SPARC64 support - ARM backend can almost build a full NetBSD/EABI system with the   integrated assembler - PowerPC grows disassembler support - LLD supports copy relocation, beginning of MIPS backend - New diagnostic for use of bool expression as size_t argument for   strncmp and friends

--- external/bsd/llvm/config/clang/Config/config.h
+++ external/bsd/llvm/config/clang/Config/config.h
@@ -11,10 +11,10 @@
 #define CLANG_RESOURCE_DIR ""
 
 /* Directories clang will search for headers */
-#define C_INCLUDE_DIRS "/usr/include/clang-3.4:/usr/include"
+#define C_INCLUDE_DIRS "/usr/include/clang-3.5:/usr/include"
 
 /* Linker version detected at compile time. */
-#define HOST_LINK_VERSION "2.23.2"
+#define HOST_LINK_VERSION "2.23.52.20130913"
 
 /* Default <path> to all compiler invocations for --sysroot=<path>. */
 #define DEFAULT_SYSROOT ""
