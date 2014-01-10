6d46271b6a0f8f87fb68f4526d3a85b7e9bccc4b
njoly <njoly>
2014-01-10 19:44:47 +0000

    Do not redefine linux syscalls arguments, just use compat ones instead. Include linux_socketcall.h to bring missing socket syscall arguments/prototypes on arches that have socketcall(2). Finally, cleanup rump linux syscall wrapper, to remove linux syscall argument/prototype local copy.

--- sys/rump/kern/lib/libsys_linux/linux_rump.c
+++ sys/rump/kern/lib/libsys_linux/linux_rump.c
@@ -1,27 +1,12 @@
-/*	$NetBSD: linux_rump.c,v 1.1 2013/12/14 10:29:45 njoly Exp $	*/
+/*	$NetBSD: linux_rump.c,v 1.2 2014/01/10 19:44:47 njoly Exp $	*/
 
 #include <sys/param.h>
 
 #include <compat/linux/common/linux_types.h>
 #include <compat/linux/common/linux_signal.h>
+#include <compat/linux/linux_syscallargs.h>
 
 #include "rump_linux_syscallargs.h"
-
-/*
-    compat/linux/arch/.../syscallargs.h and rump_linux_syscallargs.h
-    define the same syscall arguments and prototypes, thus cannot be
-    both used. Just copy needed linux stuff for now to avoid conflicts.
-*/
-
-struct linux_sys_mknodat_args {
-        syscallarg(int) fd;
-        syscallarg(const char *) path;
-        syscallarg(mode_t) mode;
-        syscallarg(unsigned) dev;
-};
-check_syscall_args(linux_sys_mknodat)
-
-int     linux_sys_mknodat(struct lwp *, const struct linux_sys_mknodat_args *, register_t *);
 
 int
 rump_linux_sys_mknodat(struct lwp *l,
