25e766da305f8d777544989857c1051ff8dbf94e
tsutsui <tsutsui@NetBSD.org>
2014-01-10 11:12:03 +0000

    Reorgenize boot device configuration.  The idea is taken from OpenBSD/luna88k.
    - remove kernel-like autoconfiguration to probe bootable devices
    - initialize SCSI and Ethernet controllers statically instead
    - reorganize device softc structures per autoconf removal
    - probe and print all SCSI disks (but don't assign unit numbers)
    - make sdopen() to recheck the device and allocate softc dynamically
    - use controller number and SCSI target ID (ctlr * 10 + id) to specify
      the boot disk on the "boot" command arg
    - bump version to denote changes
    
    Now bootloader works as the following:
    ---
    >> NetBSD/luna68k boot, Revision 1.8 (Wed Jan  8 22:13:12 JST 2014)
    >> (based on Stinger ver 0.0 [Phase-31])
    
    Machine model   = LUNA-II
    Physical Memory = 0x4000000  (64 MB)
    
    sc0 at 0xe1000000: async, parity, ID 7
     ID 3: TEAC FC-1     HGF  10 rev , 512 bytes/sect x 2879 sectors
     ID 6: IBM DPES-31080 rev S31Q, 512 bytes/sect x 2118143 sectors
    sc1 at 0xe1000040: async, parity, ID 7
     ID 6: MELCO DSC-G rev 1.00, 512 bytes/sect x 62533295 sectors
    le0: Am7990 LANCE Ethernet, mem at 0x71010000
    le0: Ethernet address = 00:00:0a:03:42:77
    
    Press return to boot now, any other key for boot menu
    booting sd(16,0)netbsd - starting in 0 seconds.
    auto-boot sd(16,0)netbsd
    1911696+96040 [280480+159179]=0x255a30
     :

--- sys/arch/luna68k/stand/boot/if_le.c
+++ sys/arch/luna68k/stand/boot/if_le.c
@@ -1,4 +1,4 @@
-/* $NetBSD: if_le.c,v 1.4 2014/01/03 03:25:25 tsutsui Exp $ */
+/* $NetBSD: if_le.c,v 1.5 2014/01/10 11:12:03 tsutsui Exp $ */
 
 /*
  * Copyright (c) 2013 Izumi Tsutsui.  All rights reserved.
@@ -72,7 +72,6 @@
 #include <lib/libsa/netif.h>
 
 #include <luna68k/stand/boot/samachdep.h>
-#include <luna68k/stand/boot/device.h>
 
 /* libsa netif_driver glue functions */
 static int  le_match(struct netif *, void *);
@@ -83,13 +82,6 @@ static int  le_put(struct iodesc *, void *, size_t);
 static void le_end(struct netif *);
 
 static void myetheraddr(uint8_t *);
-
-/* luna68k driver glue stuff */
-struct driver ledriver = {
-	leinit,
-	"le",
-	NULL
-};
 
 /* libsa netif glue stuff */
 struct netif_stats le_stats;
@@ -114,27 +106,25 @@ int debug;
 #endif
 
 int
-leinit(void *arg)
+leinit(int unit, void *addr)
 {
-	struct hp_device *hd = arg;
 	void *cookie;
 	void *reg, *mem;
 	uint8_t eaddr[6];
 
-	reg = hd->hpd_addr;
+	reg = addr;
 	mem = (void *)0x71010000;	/* XXX */
 
 	myetheraddr(eaddr);
 
-	cookie = lance_attach(hd->hpd_unit, reg, mem, eaddr);
+	cookie = lance_attach(unit, reg, mem, eaddr);
 	if (cookie == NULL)
 		return 0;
 
-	printf("%s%d: Am7990 LANCE Ethernet, mem at 0x%x\n",
-	    hd->hpd_driver->d_name, hd->hpd_unit, (uint32_t)mem);
-	printf("%s%d: Ethernet address = %s\n",
-	    hd->hpd_driver->d_name, hd->hpd_unit,
-	    ether_sprintf(eaddr));
+	printf("le%d: Am7990 LANCE Ethernet, mem at 0x%x\n",
+	    unit, (uint32_t)mem);
+	printf("le%d: Ethernet address = %s\n",
+	    unit, ether_sprintf(eaddr));
 
 	return 1;
 }
