d9d0affa150ab78ab76951de48dde25c40867edd
christos <christos@NetBSD.org>
2014-01-11 16:31:20 +0000

    Reorder code to avoid use-after-free on error. From Maxime Villard

--- sys/kern/kern_verifiedexec.c
+++ sys/kern/kern_verifiedexec.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: kern_verifiedexec.c,v 1.131 2013/11/27 17:24:44 christos Exp $	*/
+/*	$NetBSD: kern_verifiedexec.c,v 1.132 2014/01/11 16:31:20 christos Exp $	*/
 
 /*-
  * Copyright (c) 2005, 2006 Elad Efrat <elad@NetBSD.org>
@@ -29,7 +29,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: kern_verifiedexec.c,v 1.131 2013/11/27 17:24:44 christos Exp $");
+__KERNEL_RCSID(0, "$NetBSD: kern_verifiedexec.c,v 1.132 2014/01/11 16:31:20 christos Exp $");
 
 #include "opt_veriexec.h"
 
@@ -1309,18 +1309,6 @@ veriexec_file_add(struct lwp *l, prop_dictionary_t dict)
 	vfe->npages = 0;
 	vfe->last_page_size = 0;
 
-	vte = veriexec_table_lookup(vp->v_mount);
-	if (vte == NULL)
-		vte = veriexec_table_add(l, vp->v_mount);
-
-	/* XXX if we bail below this, we might want to gc newly created vtes. */
-
-	error = fileassoc_add(vp, veriexec_hook, vfe);
-	if (error)
-		goto unlock_out;
-
-	vte->vte_count++;
-
 	if (prop_bool_true(prop_dictionary_get(dict, "eval-on-load")) ||
 	    (vfe->type & VERIEXEC_UNTRUSTED)) {
 		u_char *digest;
@@ -1341,6 +1329,18 @@ veriexec_file_add(struct lwp *l, prop_dictionary_t dict)
 
 		kmem_free(digest, vfe->ops->hash_len);
 	}
+
+	vte = veriexec_table_lookup(vp->v_mount);
+	if (vte == NULL)
+		vte = veriexec_table_add(l, vp->v_mount);
+
+	/* XXX if we bail below this, we might want to gc newly created vtes. */
+
+	error = fileassoc_add(vp, veriexec_hook, vfe);
+	if (error)
+		goto unlock_out;
+
+	vte->vte_count++;
 
 	veriexec_file_report(NULL, "New entry.", file, NULL, REPORT_DEBUG);
 	veriexec_bypass = 0;
