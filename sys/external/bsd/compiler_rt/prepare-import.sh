898114d8b5cbb8aa5fbad2348f4574958337fa75
joerg <joerg>
2014-01-15 21:07:19 +0000

    More sanitizers we are still not interested in.

--- sys/external/bsd/compiler_rt/prepare-import.sh
+++ sys/external/bsd/compiler_rt/prepare-import.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# $NetBSD: prepare-import.sh,v 1.1 2013/07/04 22:12:40 joerg Exp $
+# $NetBSD: prepare-import.sh,v 1.2 2014/01/15 21:07:19 joerg Exp $
 #
 # Checkout compiler_rt into dist.
 # Run this script and check for additional files and directories to prune,
@@ -11,8 +11,7 @@ cd dist
 rm -rf .svn
 rm -rf BlocksRuntime SDKs cmake include make www
 rm -f .arcconfig .gitignore CMakeLists.txt Makefile
-rm -rf lib/asan lib/interception lib/msan lib/msandr lib/sanitizer_common
-rm -rf lib/tsan lib/ubsan
+rm -rf lib/asan lib/dfsan lib/interception lib/lsan lib/msan lib/msandr
+rm -rf lib/sanitizer_common lib/tsan lib/ubsan
 rm -f lib/*/Makefile.mk lib/Makefile.mk lib/CMakeLists.txt
 rm -f lib/apple_versioning.c lib/lit.common.* lib/profile/CMakeLists.txt
-rmdir lib/sparc64
