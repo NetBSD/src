f0f8e972e19eb96d285acbbda1732e44b699296a
riastradh <riastradh@NetBSD.org>
2014-01-15 13:53:53 +0000

    Rewrite drm locking support for NetBSD.

--- sys/external/bsd/drm2/dist/drm/drm_context.c
+++ sys/external/bsd/drm2/dist/drm/drm_context.c
@@ -267,9 +267,12 @@ static int drm_context_switch_complete(struct drm_device *dev,
 	dev->last_context = new;	/* PRE/POST: This is the _only_ writer. */
 	dev->last_switch = jiffies;
 
-	if (!_DRM_LOCK_IS_HELD(file_priv->master->lock.hw_lock->lock)) {
+	spin_lock(&file_priv->master->lock.spinlock);
+	if (file_priv->master->lock.hw_lock == NULL ||
+	    !_DRM_LOCK_IS_HELD(file_priv->master->lock.hw_lock->lock)) {
 		DRM_ERROR("Lock isn't held after context switch\n");
 	}
+	spin_unlock(&file_priv->master->lock.spinlock);
 
 	/* If a context switch is ever initiated
 	   when the kernel holds the lock, release
