bd8aadb737203425737e6cfb15adac7092f0d97c
mrg <mrg@NetBSD.org>
2014-01-19 00:22:33 +0000

    remove schedintr_4m(), and go back to only having the level 14 timer call statclock/schedclock.  the randomness for this clock makes the hardclock() call very jittery.
    instead of this, trigger a softintr IPI at level 10 on non-primary
    CPUs from the primary CPU's clock interrupt (at level 10.)  this
    cleans up the generic sparc timer code a little as well.
    
    this makes the time goes backwards problem *much* less frequent,
    but i still see it sometimes.

--- sys/arch/sparc/sparc/timer_sun4m.c
+++ sys/arch/sparc/sparc/timer_sun4m.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: timer_sun4m.c,v 1.29 2013/11/16 23:54:01 mrg Exp $	*/
+/*	$NetBSD: timer_sun4m.c,v 1.30 2014/01/19 00:22:33 mrg Exp $	*/
 
 /*
  * Copyright (c) 1992, 1993
@@ -58,7 +58,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: timer_sun4m.c,v 1.29 2013/11/16 23:54:01 mrg Exp $");
+__KERNEL_RCSID(0, "$NetBSD: timer_sun4m.c,v 1.30 2014/01/19 00:22:33 mrg Exp $");
 
 #include <sys/param.h>
 #include <sys/kernel.h>
@@ -76,6 +76,11 @@ __KERNEL_RCSID(0, "$NetBSD: timer_sun4m.c,v 1.29 2013/11/16 23:54:01 mrg Exp $")
 
 struct timer_4m		*timerreg4m;
 #define	counterreg4m	cpuinfo.counterreg_4m
+
+/*
+ * SMP hardclock handler.
+ */
+#define IPL_HARDCLOCK	10
 
 /*
  * Set up the real-time and statistics clocks.
@@ -96,30 +101,44 @@ timer_init_4m(void)
 	icr_si_bic(SINTR_T);
 }
 
-void
-schedintr_4m(void *v)
-{
-
-	kpreempt_disable();
 #ifdef MULTIPROCESSOR
-	/*
-	 * We call hardclock() here so that we make sure it is called on
-	 * all CPUs.  This function ends up being called on sun4m systems
-	 * every tick.
-	 */
-	if (!CPU_IS_PRIMARY(curcpu()))
-		hardclock(v);
+/*
+ * Handle SMP hardclock() calling for this CPU.
+ */
+static void
+hardclock_ipi(void *cap)
+{
+	int s = splsched();
 
-	/*
-	 * The factor 8 is only valid for stathz==100.
-	 * See also clock.c
-	 */
-	if ((++cpuinfo.ci_schedstate.spc_schedticks & 7) == 0 && schedhz != 0)
-#endif
-		schedclock(curlwp);
-	kpreempt_enable();
+	hardclock((struct clockframe *)cap);
+	splx(s);
 }
+#endif
 
+/*
+ * Call hardclock on all CPUs.
+ */
+static void
+handle_hardclock(struct clockframe *cap)
+{
+	int s;
+#ifdef MULTIPROCESSOR
+	struct cpu_info *cpi;
+	CPU_INFO_ITERATOR n;
+
+	for (CPU_INFO_FOREACH(n, cpi)) {
+		if (cpi == cpuinfo.ci_self) {
+			KASSERT(CPU_IS_PRIMARY(cpi));
+			continue;
+		}
+		
+		raise_ipi(cpi, IPL_HARDCLOCK);
+	}
+#endif
+	s = splsched();
+	hardclock(cap);
+	splx(s);
+}
 
 /*
  * Level 10 (clock) interrupts from system counter.
@@ -128,7 +147,6 @@ int
 clockintr_4m(void *cap)
 {
 
-	KASSERT(CPU_IS_PRIMARY(curcpu()));
 	/*
 	 * XXX this needs to be fixed in a more general way
 	 * problem is that the kernel enables interrupts and THEN
@@ -146,7 +164,14 @@ clockintr_4m(void *cap)
 	/* read the limit register to clear the interrupt */
 	*((volatile int *)&timerreg4m->t_limit);
 	tickle_tc();
-	hardclock((struct clockframe *)cap);
+
+	/*
+	 * We don't have a system-clock per-cpu, and we'd like to keep
+	 * the per-cpu timer for the statclock, so, send an IPI to
+	 * everyone to call hardclock.
+	 */
+	handle_hardclock(cap);
+
 	kpreempt_enable();
 	return (1);
 }
@@ -183,23 +208,19 @@ statintr_4m(void *cap)
 	 * The factor 8 is only valid for stathz==100.
 	 * See also clock.c
 	 */
-#if !defined(MULTIPROCESSOR)
 	if ((++cpuinfo.ci_schedstate.spc_schedticks & 7) == 0 && schedhz != 0) {
-#endif
 		if (CLKF_LOPRI(frame, IPL_SCHED)) {
 			/* No need to schedule a soft interrupt */
 			spllowerschedclock();
-			schedintr_4m(cap);
+			schedintr(cap);
 		} else {
 			/*
 			 * We're interrupting a thread that may have the
-			 * scheduler lock; run schedintr_4m() on this CPU later.
+			 * scheduler lock; run schedintr() on this CPU later.
 			 */
 			raise_ipi(&cpuinfo, IPL_SCHED); /* sched_cookie->pil */
 		}
-#if !defined(MULTIPROCESSOR)
 	}
-#endif
 	kpreempt_enable();
 
 	return (1);
@@ -260,6 +281,22 @@ timerattach_obio_4m(device_t parent, device_t self, void *aux)
 		}
 		cpi->counterreg_4m = (struct counter_4m *)bh;
 	}
+
+#if defined(MULTIPROCESSOR)
+	if (sparc_ncpus > 1) {
+		/*
+		 * Note that we don't actually use this cookie after checking
+		 * it was establised, we call directly via raise_ipi() on
+		 * IPL_HARDCLOCK.
+		 */
+		void *hardclock_cookie;
+
+		hardclock_cookie = sparc_softintr_establish(IPL_HARDCLOCK,
+		    hardclock_ipi, NULL);
+		if (hardclock_cookie == NULL)
+			panic("timerattach: cannot establish hardclock_intr");
+	}
+#endif
 
 	/* Put processor counter in "timer" mode */
 	timerreg4m->t_cfg = 0;
