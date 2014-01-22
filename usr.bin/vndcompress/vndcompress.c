7c5bfcbe348e637b3b9320127d7f9b97fe0b6bb4
riastradh <riastradh@NetBSD.org>
2014-01-22 06:15:57 +0000

    Add option -w to vnd(un)compress to specify the window size.

--- usr.bin/vndcompress/vndcompress.c
+++ usr.bin/vndcompress/vndcompress.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: vndcompress.c,v 1.18 2014/01/22 06:15:22 riastradh Exp $	*/
+/*	$NetBSD: vndcompress.c,v 1.19 2014/01/22 06:15:57 riastradh Exp $	*/
 
 /*-
  * Copyright (c) 2013 The NetBSD Foundation, Inc.
@@ -30,7 +30,7 @@
  */
 
 #include <sys/cdefs.h>
-__RCSID("$NetBSD: vndcompress.c,v 1.18 2014/01/22 06:15:22 riastradh Exp $");
+__RCSID("$NetBSD: vndcompress.c,v 1.19 2014/01/22 06:15:57 riastradh Exp $");
 
 #include <sys/endian.h>
 
@@ -479,7 +479,7 @@ compress_init(int argc, char **argv, const struct options *O,
 	__CTASSERT(MAX_N_OFFSETS == (MAX_N_BLOCKS + 1));
 	__CTASSERT(MAX_N_OFFSETS <= (SIZE_MAX / sizeof(uint64_t)));
 	/* XXX Make the window size an option.  */
-	offtab_init(&S->offtab, S->n_offsets, S->n_offsets, S->cloop2_fd,
+	offtab_init(&S->offtab, S->n_offsets, O->window_size, S->cloop2_fd,
 	    CLOOP2_OFFSET_TABLE_OFFSET);
 
 	/* Attempt to restart a partial transfer if requested.  */
