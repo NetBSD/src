c6e0548d832c19eb38c9cee94edc9e8e06c6486a
hannken <hannken@NetBSD.org>
2014-01-23 10:13:55 +0000

    Change vnode operations create, mknod, mkdir and symlink to return the resulting vnode *vpp unlocked.
    Discussed on tech-kern@
    
    Welcome to 6.99.30

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.438 2014/01/17 10:55:03 hannken Exp $	*/
+/*	$NetBSD: param.h,v 1.439 2014/01/23 10:13:57 hannken Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -63,7 +63,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	699002900	/* NetBSD 6.99.29 */
+#define	__NetBSD_Version__	699003000	/* NetBSD 6.99.30 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
