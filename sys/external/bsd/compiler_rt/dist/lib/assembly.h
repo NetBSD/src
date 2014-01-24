fd8a2cd7c2b07651027d8de343b37b03b5674f08
joerg <joerg@NetBSD.org>
2014-01-24 14:52:00 +0000

    Import compiler-rt r200007. Use .size on all assembler functions. Reimplement ARM division based on Matt Thomas's common version.

--- sys/external/bsd/compiler_rt/dist/lib/assembly.h
+++ sys/external/bsd/compiler_rt/dist/lib/assembly.h
@@ -31,7 +31,49 @@
 #define HIDDEN_DIRECTIVE .hidden
 #define LOCAL_LABEL(name) .L_##name
 #define FILE_LEVEL_DIRECTIVE
-#define SYMBOL_IS_FUNC(name) .type name, @function
+#  if defined(__arm__)
+#  define SYMBOL_IS_FUNC(name) .type name, %function
+#  else
+#  define SYMBOL_IS_FUNC(name) .type name, @function
+#  endif
+#endif
+
+#if defined(__arm__)
+# ifndef __ARM_ARCH
+#  if defined (__ARM_ARCH_7__) || defined (__ARM_ARCH_7A__) || \
+     defined (__ARM_ARCH_7R__) || defined (__ARM_ARCH_7M__) || \
+     defined (__ARM_ARCH_7EM__)
+#  define __ARM_ARCH 7
+#  endif
+# endif
+
+# ifndef __ARM_ARCH
+#  if defined (__ARM_ARCH_6__) || \
+      defined (__ARM_ARCH_6J__) || defined (__ARM_ARCH_6K__) || \
+      defined (__ARM_ARCH_6Z__) || defined (__ARM_ARCH_6ZK__) || \
+      defined (__ARM_ARCH_6ZM__)
+#  define __ARM_ARCH 6
+#  endif
+# endif
+
+# ifndef __ARM_ARCH
+#  if defined (__ARM_ARCH_5__) || defined (__ARM_ARCH_5T__) || \
+      defined (__ARM_ARCH_5TE__) || defined (__ARM_ARCH_5TEJ__)
+#  define __ARM_ARCH 5
+#  endif
+# endif
+
+# ifndef __ARM_ARCH
+# define __ARM_ARCH 4
+# endif
+
+# if defined(__ARM_ARCH_4T__) || __ARM_ARCH >= 5
+# define ARM_HAS_BX
+# endif
+# if !defined(__ARM_FEATURE_CLZ) && \
+     (__ARM_ARCH >= 6 || (__ARM_ARCH == 5 && !defined(__ARM_ARCH_5__)))
+# define __ARM_FEATURE_CLZ
+# endif
 #endif
 
 #define GLUE2(a, b) a ## b
@@ -74,6 +116,13 @@
   DEFINE_COMPILERRT_FUNCTION_ALIAS(aeabi_name, name)
 #else
 # define DEFINE_AEABI_FUNCTION_ALIAS(aeabi_name, name)
+#endif
+
+#ifdef __ELF__
+#define END_COMPILERRT_FUNCTION(name) \
+	.size	SYMBOL_NAME(name), . - SYMBOL_NAME(name)
+#else
+#define END_COMPILERRT_FUNCTION(name)
 #endif
 
 #endif /* COMPILERRT_ASSEMBLY_H */
