f763970b34b2488bc63773daec90343bb239746c
msaitoh <msaitoh@NetBSD.org>
2014-01-26 10:54:24 +0000

    PUCCN improvements:  - Fix a bug that the puc cn mechanism doesn't use the UART's frequency    in pucdata.c's table.
     - Add a new option PUC_CNAUTO. If this option is set, consinit() in
       x86/x86/consinit.c checks puc com device to use it as console.
       Without this option, the behavior is the same as before.
    
     - Add a new config parameter PUC_CNBUS. The old code scans bus #0 only.
       If PUC_CNBUS is set, the specified number's bus will be scanned.
    
     - Rename comcnprobe() to puc_cnprobe() to make it clear.
    
     - Rename comcninit() to puc_cninit() to make it clear.
    
     - Add code for a device that a device's com register is MMIO (#if0 ed).

--- sys/dev/pci/puccn.h
+++ sys/dev/pci/puccn.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: puccn.h,v 1.5 2013/07/22 13:40:36 soren Exp $ */
+/*	$NetBSD: puccn.h,v 1.6 2014/01/26 10:54:24 msaitoh Exp $ */
 
 /*
  * Derived from  pci.c
@@ -35,6 +35,7 @@
  */
 
 #include <dev/cons.h>
+#include <dev/pci/pcivar.h>
 
 /*
  * Machine independent support for PCI serial console support.
@@ -44,4 +45,6 @@
  * used before the normal PCI bus initialization.
  */
 
-int cpu_comcnprobe(struct consdev *, struct pci_attach_args *);
+void puc_cnprobe(struct consdev *);
+int puc_cninit(struct consdev *);
+int cpu_puc_cnprobe(struct consdev *, struct pci_attach_args *);
