ab574d9857dd628cd34862be651f4ad3eeef9eb1
bouyer <bouyer>
2014-01-27 21:32:03 +0000

    This is 6.1.3_PATCH, not (yet) 6.1.4

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.408.2.12.2.7 2014/01/27 21:27:50 bouyer Exp $	*/
+/*	$NetBSD: param.h,v 1.408.2.12.2.8 2014/01/27 21:32:03 bouyer Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -63,7 +63,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	601000301	/* NetBSD 6.1.4 */
+#define	__NetBSD_Version__	601000301	/* NetBSD 6.1.3_PATCH */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
