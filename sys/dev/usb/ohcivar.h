3a33b7b706aa20ae825a2f0fe7e3ab6b009631e7
skrll <skrll@NetBSD.org>
2014-01-28 17:24:42 +0000

    Support the OHCI in the NS "SUPERIO" chip as found in hppa machines by twiddling the same bits as Linux.

--- sys/dev/usb/ohcivar.h
+++ sys/dev/usb/ohcivar.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: ohcivar.h,v 1.54 2013/01/29 00:00:15 christos Exp $	*/
+/*	$NetBSD: ohcivar.h,v 1.55 2014/01/28 17:24:42 skrll Exp $	*/
 
 /*
  * Copyright (c) 1998 The NetBSD Foundation, Inc.
@@ -116,6 +116,9 @@ typedef struct ohci_softc {
 #define	OHCI_LITTLE_ENDIAN	0	/* typical (uninitialized default) */
 #define	OHCI_BIG_ENDIAN		1	/* big endian OHCI? never seen it */
 #define	OHCI_HOST_ENDIAN	2	/* if OHCI always matches CPU */
+
+	int sc_flags;
+#define OHCIF_SUPERIO		0x0001
 
 	char sc_softwake;
 	kcondvar_t sc_softwake_cv;
