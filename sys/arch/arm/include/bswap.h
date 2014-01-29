25b6650f1bb1cdf935167c8ba9516513dba22d9e
matt <matt@NetBSD.org>
2014-01-29 01:36:43 +0000

    Add latent AArch64 support.

--- sys/arch/arm/include/bswap.h
+++ sys/arch/arm/include/bswap.h
@@ -1,9 +1,13 @@
-/*      $NetBSD: bswap.h,v 1.5 2014/01/29 00:42:15 matt Exp $      */
+/*      $NetBSD: bswap.h,v 1.6 2014/01/29 01:36:43 matt Exp $      */
 
 #ifndef _ARM_BSWAP_H_
 #define	_ARM_BSWAP_H_
 
+#ifdef __aarch64__
+#include <aarch64/byte_swap.h>
+#else
 #include <arm/byte_swap.h>
+#endif
 
 #define __BSWAP_RENAME
 #include <sys/bswap.h>
