c60231f75fe21ae75581d181c129c995ea4fc91b
matt <matt@NetBSD.org>
2014-01-29 01:01:14 +0000

    AAPCS64 supports 16-byte long doubles

--- sys/arch/arm/include/math.h
+++ sys/arch/arm/include/math.h
@@ -1,3 +1,6 @@
-/*	$NetBSD: math.h,v 1.2 2002/02/19 13:08:14 simonb Exp $	*/
+/*	$NetBSD: math.h,v 1.3 2014/01/29 01:01:14 matt Exp $	*/
 
 #define	__HAVE_NANF
+#ifdef __ARM_PCS_AAPCS64
+#define __HAVE_LONG_DOUBLE
+#endif
