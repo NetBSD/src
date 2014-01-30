262dd157f6059c68151d9dbf28af1f74b1038f33
matt <matt@NetBSD.org>
2014-01-30 06:38:00 +0000

    ldrht is v6t2 or later so use ldrt and extract the right halfword.

--- sys/arch/arm/include/locore.h
+++ sys/arch/arm/include/locore.h
@@ -197,7 +197,16 @@ read_thumb_insn(vaddr_t va, bool user_p)
 	va &= ~1;
 	uint32_t insn;
 	if (user_p) {
+#ifdef _ARM_ARCH_T2
 		__asm __volatile("ldrht %0, [%1]" : "=&r"(insn) : "r"(va));
+#else
+		__asm __volatile("ldrt %0, [%1]" : "=&r"(insn) : "r"(va & ~3));
+#ifdef __ARMEB__
+		insn = (uint16_t) (insn >> (((va ^ 2) & 2) << 3));
+#else
+		insn = (uint16_t) (insn >> ((va & 2) << 3));
+#endif
+#endif
 	} else {
 		insn = *(const uint16_t *)va;
 	}
