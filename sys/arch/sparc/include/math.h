4d035bbcf3c1edc5babf1ef0d1be5c6b0f87198f
matt <matt@NetBSD.org>
2014-01-31 19:38:06 +0000

    Consolidate the 128-bit long double defintions to <sys/ieee754.h> Each arch that uses it now defines __HAVE_LONG_DOUBLE to 128. <machine/ieee.h> is now just include the machine's math.h followed by <sys/ieee754.h>

--- sys/arch/sparc/include/math.h
+++ sys/arch/sparc/include/math.h
@@ -1,7 +1,7 @@
-/*	$NetBSD: math.h,v 1.5 2008/12/15 00:25:05 mrg Exp $	*/
+/*	$NetBSD: math.h,v 1.6 2014/01/31 19:38:06 matt Exp $	*/
 
 #define	__HAVE_NANF
 
 #ifdef _LP64
-#define	__HAVE_LONG_DOUBLE
+#define	__HAVE_LONG_DOUBLE	128
 #endif
