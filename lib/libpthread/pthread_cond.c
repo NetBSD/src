0ea23e51a6ebd316cf8c3f975065f597ede187ae
christos <christos@NetBSD.org>
2014-01-31 19:22:00 +0000

    PR/44756: Sad Clouds: Prevent leakage of errno = ESRCH from _lwp_park. This has two parts: 	- in pthread_cond_timedwait() if the thread we are trying to unpark 	  exited, retry the the _lwp_park call without it. 	- pthread_mutex() was affecting errno since it is calling _lwp_park() 	  from pthread_mutex_lock_slow(). preserve the original errno. Note that the example problem still causes an occassional deadlock on machines with many CPUs and it is the same deadlock we observe with named.

--- lib/libpthread/pthread_cond.c
+++ lib/libpthread/pthread_cond.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: pthread_cond.c,v 1.61 2013/04/01 13:28:21 christos Exp $	*/
+/*	$NetBSD: pthread_cond.c,v 1.62 2014/01/31 19:22:00 christos Exp $	*/
 
 /*-
  * Copyright (c) 2001, 2006, 2007, 2008 The NetBSD Foundation, Inc.
@@ -46,7 +46,7 @@
  */
 
 #include <sys/cdefs.h>
-__RCSID("$NetBSD: pthread_cond.c,v 1.61 2013/04/01 13:28:21 christos Exp $");
+__RCSID("$NetBSD: pthread_cond.c,v 1.62 2014/01/31 19:22:00 christos Exp $");
 
 #include <stdlib.h>
 #include <errno.h>
@@ -184,10 +184,12 @@ pthread_cond_timedwait(pthread_cond_t *cond, pthread_mutex_t *mutex,
 		pthread_mutex_unlock(mutex);
 		self->pt_willpark = 0;
 		self->pt_blocking++;
-		retval = _lwp_park(abstime, self->pt_unpark,
-		    __UNVOLATILE(&mutex->ptm_waiters),
-		    __UNVOLATILE(&mutex->ptm_waiters));
-		self->pt_unpark = 0;
+		do {
+			retval = _lwp_park(abstime, self->pt_unpark,
+			    __UNVOLATILE(&mutex->ptm_waiters),
+			    __UNVOLATILE(&mutex->ptm_waiters));
+			self->pt_unpark = 0;
+		} while (retval == -1 && errno == ESRCH);
 		self->pt_blocking--;
 		membar_sync();
 		pthread_mutex_lock(mutex);
