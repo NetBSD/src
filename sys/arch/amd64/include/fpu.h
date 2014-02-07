d41378b37a41428ba28414aa510e73640ea49b11
dsl <dsl@NetBSD.org>
2014-02-07 22:40:22 +0000

    Convert the amd64 build to use x86/cpu_extended_state.h so that the fpu   definitions match those of i386. Mostly just structure and field renames, in addition: 1) process_xmm_to_s87() and process_s87_to_xmm() moved into    x86/convert_xmm_s87.c so they can be used by amd64's netbsd32 code. 2) The linux signal code simplified to use a structure copy for ths fxsave    data - it matches the hardware definition and won't change.

--- sys/arch/amd64/include/fpu.h
+++ sys/arch/amd64/include/fpu.h
@@ -1,70 +1,9 @@
-/*	$NetBSD: fpu.h,v 1.11 2013/12/11 22:06:51 dsl Exp $	*/
+/*	$NetBSD: fpu.h,v 1.12 2014/02/07 22:40:22 dsl Exp $	*/
 
 #ifndef	_AMD64_FPU_H_
 #define	_AMD64_FPU_H_
 
-/*
- * NetBSD/amd64 only uses the extended save/restore format used
- * by fxsave/fsrestore, to always deal with the SSE registers,
- * which are part of the ABI to pass floating point values.
- *
- * The memory used for the 'fsave' instruction must be 16 byte aligned,
- * but the definition here isn't aligned to avoid padding elsewhere.
- */
-
-struct fxsave64 {
-	uint16_t  fx_fcw;           /* 0: FPU control word */
-	uint16_t  fx_fsw;           /* 2: FPU status word */
-	uint8_t   fx_ftw;           /* 4: Abridged FPU tag word */
-	uint8_t   fx_reserved1;     /* 5: */
-	uint16_t  fx_fop;           /* 6: Low 11 bits are FPU opcode */
-	uint64_t  fx_rip;           /* 8: Address of faulting instruction */
-	uint64_t  fx_rdp;           /* 16: Data address associated with fault */
-	uint32_t  fx_mxcsr;         /* 24: SIMD control & status */
-	uint32_t  fx_mxcsr_mask;    /* 28: */
-	uint64_t  fx_st[8][2];      /* 32: 8 normal FP regs (80 bit) */
-	uint64_t  fx_xmm[16][2];    /* 160: 16 SSE2 registers */
-	uint8_t   fx_reserved2[48]; /* 416: */
-	uint8_t   fx_available[48]; /* 464: could be used by kernel */
-};
-
-__CTASSERT(sizeof (struct fxsave64) == 512);
-
-struct savefpu {
-	struct fxsave64 fp_fxsave;	/* see above */
-};
-
-/*
- * The i387 defaults to Intel extended precision mode and round to nearest,
- * with all exceptions masked.
- */
-#define	__INITIAL_NPXCW__	0x037f
-#define __INITIAL_MXCSR__ 	0x1f80
-#define __INITIAL_MXCSR_MASK__	0xffbf
-
-/* Modern NetBSD uses the default control word.. */
-#define	__NetBSD_NPXCW__	0x037f
-/* NetBSD before 6.99.26 forced IEEE double precision. */
-#define	__NetBSD_COMPAT_NPXCW__	0x127f
-/* Linux just uses the default control word. */
-#define	__Linux_NPXCW__		0x037f
-
-/*
- * The standard control word from finit is 0x37F, giving:
- *	round to nearest
- *	64-bit precision
- *	all exceptions masked.
- *
- * Now we want:
- *	affine mode (if we decide to support 287's)
- *	round to nearest
- *	53-bit precision
- *	all exceptions masked.
- *
- * 64-bit precision often gives bad results with high level languages
- * because it makes the results of calculations depend on whether
- * intermediate values are stored in memory or in FPU registers.
- */
+#include <x86/cpu_extended_state.h>
 
 #ifdef _KERNEL
 /*
