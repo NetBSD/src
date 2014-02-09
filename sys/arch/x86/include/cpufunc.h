2736f96cdbe71b36c6d6505ab8b8d05ce8cfae2c
dsl <dsl@NetBSD.org>
2014-02-09 17:07:41 +0000

    Add x86_stmxcsr for amd64.

--- sys/arch/x86/include/cpufunc.h
+++ sys/arch/x86/include/cpufunc.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: cpufunc.h,v 1.14 2013/12/08 18:00:51 dsl Exp $	*/
+/*	$NetBSD: cpufunc.h,v 1.15 2014/02/09 17:07:41 dsl Exp $	*/
 
 /*-
  * Copyright (c) 1998, 2007 The NetBSD Foundation, Inc.
@@ -80,7 +80,7 @@ void	fldcw(void *);
 void	fnclex(void);
 void	fninit(void);
 void	fnsave(void *);
-void	fnstcw(void *);
+void	fnstcw(uint16_t *);
 void	fnstsw(void *);
 void	fp_divide_by_0(void);
 void	frstor(void *);
@@ -92,7 +92,8 @@ void	fxsave(void *);
 void	fxrstor(void *);
 void	x86_monitor(const void *, uint32_t, uint32_t);
 void	x86_mwait(uint32_t, uint32_t);
-void	x86_ldmxcsr(void *);
+void	x86_ldmxcsr(const uint32_t *);
+void	x86_stmxcsr(uint32_t *);
 /* x86_cpuid2() writes four 32bit values, %eax, %ebx, %ecx and %edx */
 #define	x86_cpuid(a,b)	x86_cpuid2((a),0,(b))
 void	x86_cpuid2(uint32_t, uint32_t, uint32_t *);
