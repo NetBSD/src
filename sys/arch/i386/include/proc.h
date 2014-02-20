773aea64e152ec29f7a79a2dc65baa356727de89
dsl <dsl@NetBSD.org>
2014-02-20 18:19:09 +0000

    Move the amd64 and i386 pcb to the bottom of the uarea, and move the   kernel stack to the top. Change the pcb layouts so that fpu save area is at the end and is   64byte aligned ready for xsave (saving the ymm registers). Welcome to 6.99.32

--- sys/arch/i386/include/proc.h
+++ sys/arch/i386/include/proc.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: proc.h,v 1.41 2014/02/15 10:11:15 dsl Exp $	*/
+/*	$NetBSD: proc.h,v 1.42 2014/02/20 18:19:10 dsl Exp $	*/
 
 /*
  * Copyright (c) 1991 Regents of the University of California.
@@ -62,11 +62,5 @@ struct mdproc {
 
 /* md_flags */
 #define MDP_USEDMTRR	0x0002	/* has set volatile MTRRs */
-
-/* Kernel stack parameters. */
-#define	UAREA_PCB_OFFSET	(USPACE - ALIGN(sizeof(struct pcb)))
-#define	KSTACK_LOWEST_ADDR(l)	\
-    ((void *)((vaddr_t)(l)->l_addr - UAREA_PCB_OFFSET))
-#define	KSTACK_SIZE		UAREA_PCB_OFFSET
 
 #endif /* _I386_PROC_H_ */
