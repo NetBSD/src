07ece4eabb6d327c320416d49d51617a7c0fb3be
mrg <mrg@NetBSD.org>
2014-02-25 18:38:34 +0000

    import the -current GCC 4.5 sources as gcc.old in preparation for upgrading the normal GCC to 4.8.
    this tree has had ChangeLog entries removed, as well as all the
    other components we delete, and "only" weighs in around 140MB now.

---
+++ external/gpl3/gcc.old/lib/crtstuff/crtbeginT.c
@@ -0,0 +1,10 @@
+/*	$NetBSD: crtbeginT.c,v 1.1.1.1 2014/02/25 18:42:04 mrg Exp $	*/
+
+/*
+ * Written by Jason R. Thorpe, August 24, 2004.
+ * Public domain.
+ */
+
+#define	CRT_BEGIN
+#define	CRTSTUFFT_O
+#include "crtstuff.c"
