198fcdd94a1611e66a4b43ad13e3f5a5f614311c
hannken <hannken@NetBSD.org>
2014-02-27 16:51:37 +0000

    The current implementation of vn_lock() is racy.  Modification of the vnode operations vector for active vnodes is unsafe because it is not known whether deadfs or the original file system will be called.
    - Pass down LK_RETRY to the lock operation (hint for deadfs only).
    
    - Change deadfs lock operation to return ENOENT if LK_RETRY is unset.
    
    - Change all other lock operations to check for dead vnode once
      the vnode is locked and unlock and return ENOENT in this case.
    
    With these changes in place vnode lock operations will never succeed
    after vclean() has marked the vnode as VI_XLOCK and before vclean()
    has changed the operations vector.
    
    Adresses PR kern/37706 (Forced unmount of file systems is unsafe)
    
    Discussed on tech-kern.
    
    Welcome to 6.99.33

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.441 2014/02/20 18:19:10 dsl Exp $	*/
+/*	$NetBSD: param.h,v 1.442 2014/02/27 16:51:39 hannken Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -63,7 +63,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	699003200	/* NetBSD 6.99.32 */
+#define	__NetBSD_Version__	699003300	/* NetBSD 6.99.33 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
