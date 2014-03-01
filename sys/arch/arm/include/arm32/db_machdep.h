352c9e4fa44348fd312ef79d6a293cb8a21446c0
matt <matt@NetBSD.org>
2014-03-01 05:28:23 +0000

    Add db_show_tlb_cmd prototype

--- sys/arch/arm/include/arm32/db_machdep.h
+++ sys/arch/arm/include/arm32/db_machdep.h
@@ -1,4 +1,4 @@
-/* $NetBSD: db_machdep.h,v 1.7 2013/01/05 15:06:51 christos Exp $ */
+/* $NetBSD: db_machdep.h,v 1.8 2014/03/01 05:28:23 matt Exp $ */
 
 #ifndef _ARM32_DB_MACHDEP_H_
 #define _ARM32_DB_MACHDEP_H_
@@ -7,5 +7,8 @@
 
 void db_show_frame_cmd(db_expr_t, bool, db_expr_t, const char *);
 void db_show_fault_cmd(db_expr_t, bool, db_expr_t, const char *);
+#ifdef _KERNEL
+void db_show_tlb_cmd(db_expr_t, bool, db_expr_t, const char *);
+#endif
 
 #endif
