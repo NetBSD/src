63644cefdf11e8e1223652359fd559aed02a8e2e
pooka <pooka>
2014-03-03 13:56:40 +0000

    Pull in the changes from the dpdk, netmap and snabb switch repos. There are two major changes:
    1) All thread context policy is pushed down to the hypercalls.
       This is meant to help performance-mongering implementations be
       able to control packet scheduling better (e.g. pin down packet
       reception to certain physical cores).
    2) Generalize linkstr, meaning that the interface can now take an
       arbitrary string which is passed to the create() hypercall.  This can
       be used to map backend device characteristics to the rump kernel
       interface instance.

--- sys/rump/net/lib/libvirtif/if_virt.h
+++ sys/rump/net/lib/libvirtif/if_virt.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: if_virt.h,v 1.2 2013/07/04 11:58:11 pooka Exp $	*/
+/*	$NetBSD: if_virt.h,v 1.3 2014/03/03 13:56:40 pooka Exp $	*/
 
 /*
  * NOTE!  This file is supposed to work on !NetBSD platforms.
@@ -22,4 +22,9 @@
 #define VIFHYPER_DYING VIF_BASENAME3(rumpcomp_,VIRTIF_BASE,_dying)
 #define VIFHYPER_DESTROY VIF_BASENAME3(rumpcomp_,VIRTIF_BASE,_destroy)
 #define VIFHYPER_SEND VIF_BASENAME3(rumpcomp_,VIRTIF_BASE,_send)
-#define VIFHYPER_RECV VIF_BASENAME3(rumpcomp_,VIRTIF_BASE,_recv)
+
+#define VIFHYPER_FLAGS VIF_BASENAME3(rumpcomp_,VIRTIF_BASE,_flags)
+
+#define VIF_DELIVERPKT VIF_BASENAME3(rump_virtif_,VIRTIF_BASE,_deliverpkt)
+
+struct virtif_sc;
