cdb08943ae7f4ef02da5faf80e3895fba28e59ad
matt <matt@NetBSD.org>
2014-03-04 15:27:58 +0000

    Remove duplicate END(y) macro

--- sys/arch/arm/include/asm.h
+++ sys/arch/arm/include/asm.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: asm.h,v 1.26 2014/01/29 00:42:15 matt Exp $	*/
+/*	$NetBSD: asm.h,v 1.27 2014/03/04 15:27:58 matt Exp $	*/
 
 /*
  * Copyright (c) 1990 The Regents of the University of California.
@@ -103,7 +103,6 @@
 #define	ARM_ENTRY_NP(y)		_ARM_ENTRY(_C_LABEL(y))
 #define	THUMB_ENTRY(y)		_THUMB_ENTRY(_C_LABEL(y)); _PROF_PROLOGUE
 #define	THUMB_ENTRY_NP(y)	_THUMB_ENTRY(_C_LABEL(y))
-#define	END(y)			_END(_C_LABEL(y))
 #define	ASENTRY(y)		_ENTRY(_ASM_LABEL(y)); _PROF_PROLOGUE
 #define	ASENTRY_NP(y)		_ENTRY(_ASM_LABEL(y))
 #define	ASEND(y)		_END(_ASM_LABEL(y))
