0b476e4fb162595bf78919dd9f0a200d2d3618e7
joerg <joerg@NetBSD.org>
2014-03-04 20:49:27 +0000

    Import LLVM 3.5svn r202566.

--- external/bsd/llvm/dist/llvm/lib/Support/CommandLine.cpp
+++ external/bsd/llvm/dist/llvm/lib/Support/CommandLine.cpp
@@ -36,7 +36,6 @@
 #include <cerrno>
 #include <cstdlib>
 #include <map>
-#include <set>
 using namespace llvm;
 using namespace cl;
 
@@ -126,8 +125,21 @@ static ManagedStatic<OptionCatSet> RegisteredOptionCategories;
 // Initialise the general option category.
 OptionCategory llvm::cl::GeneralCategory("General options");
 
+struct HasName {
+  HasName(StringRef Name) : Name(Name) {}
+  bool operator()(const OptionCategory *Category) const {
+    return Name == Category->getName();
+  }
+  StringRef Name;
+};
+
 void OptionCategory::registerCategory()
 {
+  assert(std::count_if(RegisteredOptionCategories->begin(),
+                       RegisteredOptionCategories->end(),
+                       HasName(getName())) == 0 &&
+         "Duplicate option categories");
+
   RegisteredOptionCategories->insert(this);
 }
 
@@ -1484,7 +1496,7 @@ public:
     MoreHelp->clear();
 
     // Halt the program since help information was printed
-    exit(1);
+    exit(0);
   }
 };
 
@@ -1506,7 +1518,6 @@ protected:
   virtual void printOptions(StrOptionPairVector &Opts, size_t MaxArgLen) {
     std::vector<OptionCategory *> SortedCategories;
     std::map<OptionCategory *, std::vector<Option *> > CategorizedOptions;
-    std::set<std::string> CategoryNames;
 
     // Collect registered option categories into vector in preparation for
     // sorting.
@@ -1514,11 +1525,6 @@ protected:
                                       E = RegisteredOptionCategories->end();
          I != E; ++I) {
       SortedCategories.push_back(*I);
-      // FIXME: Move this check to OptionCategory::registerCategory after the
-      // problem with analyzer plugins linking to llvm/Support and causing
-      // assertion on the duplicate llvm::cl::GeneralCategory is solved.
-      assert(CategoryNames.insert((*I)->getName()).second &&
-             "Duplicate option categories");
     }
 
     // Sort the different option categories alphabetically.
@@ -1726,7 +1732,7 @@ public:
 
     if (OverrideVersionPrinter != 0) {
       (*OverrideVersionPrinter)();
-      exit(1);
+      exit(0);
     }
     print();
 
@@ -1740,7 +1746,7 @@ public:
         (*I)();
     }
 
-    exit(1);
+    exit(0);
   }
 };
 } // End anonymous namespace
