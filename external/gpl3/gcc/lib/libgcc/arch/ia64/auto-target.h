f3603cfe80135c8c5f662ec3f492dc3d527dea18
mrg <mrg@NetBSD.org>
2014-03-09 21:02:44 +0000

    mknative-gcc for GCC 4.8 and ia64.

--- external/gpl3/gcc/lib/libgcc/arch/ia64/auto-target.h
+++ external/gpl3/gcc/lib/libgcc/arch/ia64/auto-target.h
@@ -2,75 +2,74 @@
 /* Generated from: NetBSD: mknative-gcc,v 1.78 2014/03/02 04:58:20 mrg Exp  */
 /* Generated from: NetBSD: mknative.common,v 1.11 2014/02/17 21:39:43 christos Exp  */
 
-/* config.h.  Generated from config.h.in by configure.  */
-/* config.h.in.  Generated from configure.ac by autoheader.  */
+/* auto-target.h.  Generated from config.in by configure.  */
+/* config.in.  Generated from configure.ac by autoheader.  */
 
 /* Define to 1 if the target assembler supports thread-local storage. */
 /* #undef HAVE_CC_TLS */
 
-/* Define to 1 if you have the <dlfcn.h> header file. */
-#define HAVE_DLFCN_H 1
+/* Define if _Unwind_GetIPInfo is available. */
+#define HAVE_GETIPINFO 1
+
+/* Define if the compiler supports init priority. */
+#define HAVE_INIT_PRIORITY 1
 
 /* Define to 1 if you have the <inttypes.h> header file. */
-#define HAVE_INTTYPES_H 1
+/* #undef HAVE_INTTYPES_H */
 
 /* Define to 1 if you have the <memory.h> header file. */
-#define HAVE_MEMORY_H 1
-
-/* Define to 1 if you have the <sched.h> header file. */
-#define HAVE_SCHED_H 1
+/* #undef HAVE_MEMORY_H */
 
 /* Define to 1 if you have the <stdint.h> header file. */
-#define HAVE_STDINT_H 1
+/* #undef HAVE_STDINT_H */
 
 /* Define to 1 if you have the <stdlib.h> header file. */
-#define HAVE_STDLIB_H 1
+/* #undef HAVE_STDLIB_H */
 
 /* Define to 1 if you have the <strings.h> header file. */
-#define HAVE_STRINGS_H 1
+/* #undef HAVE_STRINGS_H */
 
 /* Define to 1 if you have the <string.h> header file. */
-#define HAVE_STRING_H 1
+/* #undef HAVE_STRING_H */
 
 /* Define to 1 if you have the <sys/stat.h> header file. */
-#define HAVE_SYS_STAT_H 1
+/* #undef HAVE_SYS_STAT_H */
 
 /* Define to 1 if you have the <sys/types.h> header file. */
-#define HAVE_SYS_TYPES_H 1
-
-/* Define to 1 if the target supports thread-local storage. */
-#define HAVE_TLS 1
+/* #undef HAVE_SYS_TYPES_H */
 
 /* Define to 1 if you have the <unistd.h> header file. */
-#define HAVE_UNISTD_H 1
+/* #undef HAVE_UNISTD_H */
 
-/* Define to the sub-directory in which libtool stores uninstalled libraries.
-   */
-#define LT_OBJDIR ".libs/"
-
-/* Define to 1 if your C compiler doesn't accept -c and -o together. */
-/* #undef NO_MINUS_C_MINUS_O */
+/* Define if the C compiler is configured for setjmp/longjmp exceptions. */
+/* #undef LIBGCC_SJLJ_EXCEPTIONS */
 
 /* Define to the address where bug reports for this package should be sent. */
 #define PACKAGE_BUGREPORT ""
 
 /* Define to the full name of this package. */
-#define PACKAGE_NAME "package-unused"
+#define PACKAGE_NAME "GNU C Runtime Library"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "package-unused version-unused"
+#define PACKAGE_STRING "GNU C Runtime Library 1.0"
 
 /* Define to the one symbol short name of this package. */
-#define PACKAGE_TARNAME "libobjc"
+#define PACKAGE_TARNAME "libgcc"
 
 /* Define to the home page for this package. */
-#define PACKAGE_URL ""
+#define PACKAGE_URL "http://www.gnu.org/software/libgcc/"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "version-unused"
+#define PACKAGE_VERSION "1.0"
 
-/* Define if the compiler is configured for setjmp/longjmp exceptions. */
-/* #undef SJLJ_EXCEPTIONS */
+/* The size of `double', as computed by sizeof. */
+#define SIZEOF_DOUBLE 8
+
+/* The size of `long double', as computed by sizeof. */
+#define SIZEOF_LONG_DOUBLE 16
 
 /* Define to 1 if you have the ANSI C header files. */
-#define STDC_HEADERS 1
+/* #undef STDC_HEADERS */
+
+/* Define to 1 if the target use emutls for thread-local storage. */
+/* #undef USE_EMUTLS */
--- external/gpl3/gcc/usr.bin/include/arch/ia64.mk
+++ external/gpl3/gcc/usr.bin/include/arch/ia64.mk
@@ -2,6 +2,4 @@
 # Generated from: NetBSD: mknative-gcc,v 1.78 2014/03/02 04:58:20 mrg Exp 
 # Generated from: NetBSD: mknative.common,v 1.11 2014/02/17 21:39:43 christos Exp 
 #
-# This spec file is read by gcc when linking.  It is used to specify the
-# standard libraries we need in order to link with -fopenmp.
-*link_gomp: -lgomp %{static: }
+G_EXTRA_HEADERS=${GNUHOSTDIST}/gcc/config/ia64/ia64intrin.h ${GNUHOSTDIST}/gcc/ginclude/tgmath.h
