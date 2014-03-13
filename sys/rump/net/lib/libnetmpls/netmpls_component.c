4db5ed1145f1fbad521d9e1ede10df2131d8423b
pooka <pooka@NetBSD.org>
2014-03-13 02:05:29 +0000

    rename component.c -> netmpls_component.c

--- sys/rump/net/lib/libnetmpls/netmpls_component.c
+++ sys/rump/net/lib/libnetmpls/netmpls_component.c
@@ -1,4 +1,4 @@
-/* $NetBSD: component.c,v 1.2 2014/02/14 01:43:13 pooka Exp $ */
+/* $NetBSD: netmpls_component.c,v 1.1 2014/03/13 02:05:29 pooka Exp $ */
 
 /*
  * Copyright (c) 2009 Antti Kantee.  All Rights Reserved.
@@ -31,7 +31,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: component.c,v 1.2 2014/02/14 01:43:13 pooka Exp $");
+__KERNEL_RCSID(0, "$NetBSD: netmpls_component.c,v 1.1 2014/03/13 02:05:29 pooka Exp $");
 
 #include <sys/param.h>
 #include <sys/domain.h>
