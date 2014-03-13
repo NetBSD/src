f4699ea1e52cbd37679dc223a77f5dd032d760eb
pooka <pooka@NetBSD.org>
2014-03-13 02:08:44 +0000

    rename component.c -> netinet6_component.c

--- sys/rump/net/lib/libnetinet6/netinet6_component.c
+++ sys/rump/net/lib/libnetinet6/netinet6_component.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: component.c,v 1.4 2014/02/14 01:43:13 pooka Exp $	*/
+/*	$NetBSD: netinet6_component.c,v 1.1 2014/03/13 02:08:44 pooka Exp $	*/
 
 /*
  * Copyright (c) 2013 Antti Kantee.  All Rights Reserved.
@@ -26,7 +26,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: component.c,v 1.4 2014/02/14 01:43:13 pooka Exp $");
+__KERNEL_RCSID(0, "$NetBSD: netinet6_component.c,v 1.1 2014/03/13 02:08:44 pooka Exp $");
 
 #include <sys/param.h>
 #include <sys/domain.h>
