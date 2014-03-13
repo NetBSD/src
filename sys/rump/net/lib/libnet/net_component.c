9fa21880a427aa6d323ed13071fe6511c3d62c1b
pooka <pooka@NetBSD.org>
2014-03-13 02:06:32 +0000

    rename component.c -> net_component.c

--- sys/rump/net/lib/libnet/net_component.c
+++ sys/rump/net/lib/libnet/net_component.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: component.c,v 1.10 2011/02/04 09:57:53 pooka Exp $	*/
+/*	$NetBSD: net_component.c,v 1.1 2014/03/13 02:06:32 pooka Exp $	*/
 
 /*
  * Copyright (c) 2009 Antti Kantee.  All Rights Reserved.
@@ -28,7 +28,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: component.c,v 1.10 2011/02/04 09:57:53 pooka Exp $");
+__KERNEL_RCSID(0, "$NetBSD: net_component.c,v 1.1 2014/03/13 02:06:32 pooka Exp $");
 
 #include <sys/param.h>
 #include <sys/domain.h>
