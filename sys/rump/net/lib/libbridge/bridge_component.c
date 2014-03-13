05994a834b7f347d8458d13f837cc3ddb7109e19
pooka <pooka@NetBSD.org>
2014-03-13 02:08:24 +0000

    rename component.c -> bridge_component.c

--- sys/rump/net/lib/libbridge/bridge_component.c
+++ sys/rump/net/lib/libbridge/bridge_component.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: component.c,v 1.1 2010/12/08 18:06:43 pooka Exp $	*/
+/*	$NetBSD: bridge_component.c,v 1.1 2014/03/13 02:08:24 pooka Exp $	*/
 
 /*
  * Copyright (c) 2009 Antti Kantee.  All Rights Reserved.
@@ -26,7 +26,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: component.c,v 1.1 2010/12/08 18:06:43 pooka Exp $");
+__KERNEL_RCSID(0, "$NetBSD: bridge_component.c,v 1.1 2014/03/13 02:08:24 pooka Exp $");
 
 #include <sys/param.h>
 
