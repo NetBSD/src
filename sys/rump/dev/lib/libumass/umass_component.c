b6e5593e4d91fb6fe1919945255bf5042ca9b0cd
pooka <pooka@NetBSD.org>
2014-03-13 01:54:59 +0000

    rename component.c -> umass_component.c

--- sys/rump/dev/lib/libumass/umass_component.c
+++ sys/rump/dev/lib/libumass/umass_component.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: component.c,v 1.1 2010/08/23 20:49:53 pooka Exp $	*/
+/*	$NetBSD: umass_component.c,v 1.1 2014/03/13 01:54:59 pooka Exp $	*/
 
 #include <sys/param.h>
 #include <sys/conf.h>
