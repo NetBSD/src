31f4bfc86a87b5a2229ea2fd18c326b95161d59b
pooka <pooka@NetBSD.org>
2014-03-13 01:52:04 +0000

    rename component.c -> pad_component.c

--- sys/rump/dev/lib/libpad/pad_component.c
+++ sys/rump/dev/lib/libpad/pad_component.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: component.c,v 1.2 2013/04/04 11:28:02 pooka Exp $	*/
+/*	$NetBSD: pad_component.c,v 1.1 2014/03/13 01:52:04 pooka Exp $	*/
 
 /*-
  * Copyright (c) 2010 Antti Kantee.  All Rights Reserved.
@@ -26,7 +26,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: component.c,v 1.2 2013/04/04 11:28:02 pooka Exp $");
+__KERNEL_RCSID(0, "$NetBSD: pad_component.c,v 1.1 2014/03/13 01:52:04 pooka Exp $");
 
 #include <sys/param.h>
 #include <sys/conf.h>
