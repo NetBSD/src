3b7da4ef28f8bab4cf4fccfe3c645219e7b07370
pooka <pooka@NetBSD.org>
2014-03-13 02:02:30 +0000

    rename component.c -> tty_component.c

--- sys/rump/kern/lib/libtty/tty_component.c
+++ sys/rump/kern/lib/libtty/tty_component.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: component.c,v 1.2 2011/01/07 15:10:22 pooka Exp $	*/
+/*	$NetBSD: tty_component.c,v 1.1 2014/03/13 02:02:30 pooka Exp $	*/
 
 /*
  * Copyright (c) 2010 Antti Kantee.  All Rights Reserved.
@@ -26,7 +26,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: component.c,v 1.2 2011/01/07 15:10:22 pooka Exp $");
+__KERNEL_RCSID(0, "$NetBSD: tty_component.c,v 1.1 2014/03/13 02:02:30 pooka Exp $");
 
 #include <sys/param.h>
 #include <sys/conf.h>
