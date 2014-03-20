2a66c0d8b296eb11caca99f96dcdc34ffb0ed099
joerg <joerg@NetBSD.org>
2014-03-20 01:46:19 +0000

    Always use --eh-frame-hdr, even when linking statically.

--- external/gpl3/gcc/dist/gcc/config/netbsd.h
+++ external/gpl3/gcc/dist/gcc/config/netbsd.h
@@ -188,9 +188,7 @@ along with GCC; see the file COPYING3.  If not see
 #undef WINT_TYPE
 #define WINT_TYPE "int"
 
-#if defined(HAVE_LD_EH_FRAME_HDR)
-#define LINK_EH_SPEC "%{!static:--eh-frame-hdr} "
-#endif
+#define LINK_EH_SPEC "--eh-frame-hdr "
 
 /* Use --as-needed -lgcc_s for eh support.  */
 #ifdef HAVE_LD_AS_NEEDED
