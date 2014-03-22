3e8db1b06ce77370ff395b935e1d03f5870c0f72
dholland <dholland@NetBSD.org>
2014-03-22 23:33:33 +0000

    Use random(), not rand().

--- games/battlestar/init.c
+++ games/battlestar/init.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: init.c,v 1.15 2005/07/01 06:04:54 jmc Exp $	*/
+/*	$NetBSD: init.c,v 1.16 2014/03/22 23:33:33 dholland Exp $	*/
 
 /*
  * Copyright (c) 1983, 1993
@@ -34,7 +34,7 @@
 #if 0
 static char sccsid[] = "@(#)init.c	8.4 (Berkeley) 4/30/95";
 #else
-__RCSID("$NetBSD: init.c,v 1.15 2005/07/01 06:04:54 jmc Exp $");
+__RCSID("$NetBSD: init.c,v 1.16 2014/03/22 23:33:33 dholland Exp $");
 #endif
 #endif				/* not lint */
 
@@ -54,7 +54,7 @@ initialize(const char *filename)
 	puts("First Adventure game written by His Lordship, the honorable");
 	puts("Admiral D.W. Riggle\n");
 	location = dayfile;
-	srand(getpid());
+	srandom(time(NULL));
 	username = getutmp();
 	wordinit();
 	if (filename == NULL) {
