460da1c70ed15e95572e3fe17f01870831ed8552
christos <christos>
2014-03-24 19:29:59 +0000

    use cpu_{g,s}etmodel

--- sys/arch/pmax/pmax/dec_5100.c
+++ sys/arch/pmax/pmax/dec_5100.c
@@ -1,4 +1,4 @@
-/* $NetBSD: dec_5100.c,v 1.47 2012/10/13 06:51:23 tsutsui Exp $ */
+/* $NetBSD: dec_5100.c,v 1.48 2014/03/24 19:31:40 christos Exp $ */
 
 /*
  * Copyright (c) 1998 Jonathan Stone.  All rights reserved.
@@ -31,7 +31,7 @@
  */
 #include <sys/cdefs.h>			/* RCS ID & Copyright macro defns */
 
-__KERNEL_RCSID(0, "$NetBSD: dec_5100.c,v 1.47 2012/10/13 06:51:23 tsutsui Exp $");
+__KERNEL_RCSID(0, "$NetBSD: dec_5100.c,v 1.48 2014/03/24 19:31:40 christos Exp $");
 
 #define __INTR_PRIVATE
 #include <sys/param.h>
@@ -99,7 +99,7 @@ dec_5100_init(void)
 	/* calibrate cpu_mhz value */
 	mc_cpuspeed(MIPS_PHYS_TO_KSEG1(KN01_SYS_CLOCK), MIPS_INT_MASK_2);
 
-	sprintf(cpu_model, "DECsystem 5100 (MIPSMATE)");
+	cpu_setmodel("DECsystem 5100 (MIPSMATE)");
 }
 
 /*
