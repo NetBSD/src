e6fb148c2a862d57c2c132b38ec6f41a47e160e4
joerg <joerg@NetBSD.org>
2014-03-24 21:26:01 +0000

    _Unwind_GetLanguageSpecificData is supposed to return a _Unwind_Ptr. This makes a difference on M68K as it uses a different register for integer vs pointer values.

--- external/gpl3/gcc/dist/libgcc/unwind-dw2.c
+++ external/gpl3/gcc/dist/libgcc/unwind-dw2.c
@@ -365,7 +365,7 @@ _Unwind_SetIP (struct _Unwind_Context *context, _Unwind_Ptr val)
   context->ra = (void *) val;
 }
 
-void *
+_Unwind_Ptr
 _Unwind_GetLanguageSpecificData (struct _Unwind_Context *context)
 {
   return context->lsda;
