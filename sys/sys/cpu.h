2bfeeb5aa39aef0339b5977be23b0ba89a336972
christos <christos@NetBSD.org>
2014-03-24 20:07:40 +0000

    - create  cpu_{g,s}etmodel() and hide cpu_model from direct access.

--- sys/sys/cpu.h
+++ sys/sys/cpu.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: cpu.h,v 1.39 2013/11/25 03:06:08 christos Exp $	*/
+/*	$NetBSD: cpu.h,v 1.40 2014/03/24 20:07:40 christos Exp $	*/
 
 /*-
  * Copyright (c) 2007 YAMAMOTO Takashi,
@@ -79,6 +79,8 @@ void	cpu_offline_md(void);
 
 struct lwp *cpu_switchto(struct lwp *, struct lwp *, bool);
 struct	cpu_info *cpu_lookup(u_int);
+int	cpu_setmodel(const char *fmt, ...)	__printflike(1, 2);
+const char *cpu_getmodel(void);
 int	cpu_setstate(struct cpu_info *, bool);
 int	cpu_setintr(struct cpu_info *, bool);
 bool	cpu_intr_p(void);
