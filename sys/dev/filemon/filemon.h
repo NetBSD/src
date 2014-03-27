92abf38ef45eb94d48e59e631c8ce85753e8ce32
christos <christos@NetBSD.org>
2014-03-27 18:27:34 +0000

    remove a bunch of repetitive code by introducing filemon_printf.

--- sys/dev/filemon/filemon.h
+++ sys/dev/filemon/filemon.h
@@ -1,4 +1,4 @@
-/* $NetBSD: filemon.h,v 1.4 2012/11/19 22:20:10 sjg Exp $ */
+/* $NetBSD: filemon.h,v 1.5 2014/03/27 18:27:34 christos Exp $ */
 /*
  * Copyright (c) 2010, Juniper Networks, Inc.
  *
@@ -49,6 +49,7 @@ struct filemon * filemon_lookup(struct proc *);
 void filemon_output(struct filemon *, char *, size_t);
 void filemon_wrapper_install(void);
 int  filemon_wrapper_deinstall(void);
+void filemon_printf(struct filemon *, const char *, ...) __printflike(2, 3);
 #endif
 
 #endif
