5e6e193b3461c06b5fbce79c883ce168e43c5d64
martin <martin@NetBSD.org>
2014-03-31 11:41:01 +0000

    Adapt to -current

--- sys/arch/playstation2/include/intr.h
+++ sys/arch/playstation2/include/intr.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: intr.h,v 1.11 2014/03/31 11:25:49 martin Exp $	*/
+/*	$NetBSD: intr.h,v 1.12 2014/03/31 11:41:01 martin Exp $	*/
 
 /*-
  * Copyright (c) 2001 The NetBSD Foundation, Inc.
@@ -33,45 +33,7 @@
 #define _PLAYSTATION2_INTR_H_
 #ifdef _KERNEL
 
-#include <mips/locore.h>
-
-/* Interrupt sharing types. */
-#define	IST_NONE		0	/* none */
-#define	IST_PULSE		1	/* pulsed */
-#define	IST_EDGE		2	/* edge-triggered */
-#define	IST_LEVEL		3	/* level-triggered */
-
-/* Interrupt priority levels */
-#define	IPL_NONE		0	/* nothing */
-#define IPL_SOFTCLOCK		1	/* timeouts */
-#define	IPL_SOFTBIO		1	/* bio */
-#define	IPL_SOFTNET		2	/* protocol stacks */
-#define IPL_SOFTSERIAL		2	/* serial */
-#define	IPL_VM			3	/* i/o */
-#define	IPL_SCHED		4	/* clock */
-#define	IPL_HIGH		4	/* everything */
-
-#define	_IPL_NSOFT	4
-#define	_IPL_N		5
-
-/*
- * Hardware interrupt masks
- */
-extern u_int32_t __icu_mask[_IPL_N];
-
-#define splsoftclock()		splraise(__icu_mask[IPL_SOFTCLOCK])
-#define	splsoftbio()		splraise(__icu_mask[IPL_SOFTBIO])
-#define	splsoftnet()		splraise(__icu_mask[IPL_SOFTNET])
-#define	splsoftserial()		splraise(__icu_mask[IPL_SOFTSERIAL])
-#define	splvm()			splraise(__icu_mask[IPL_VM])
-#define	splsched()		splraise(__icu_mask[IPL_SCHED])
-#define	splx(s)			splset(s)
-
-void	spllowersofthigh(void);
-
-int splraise(int);
-void splset(int);
-void spl0(void);
+#include <mips/intr.h>
 
 /* R5900 EI/DI instruction */
 int _intr_suspend(void);
