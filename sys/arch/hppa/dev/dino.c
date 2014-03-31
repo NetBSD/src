f120484eac890aaa4682d4db5423e1960a96e53d
christos <christos@NetBSD.org>
2014-03-31 20:51:20 +0000

    fix eisa_intr_string

--- sys/arch/hppa/dev/dino.c
+++ sys/arch/hppa/dev/dino.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: dino.c,v 1.1 2014/02/24 07:23:42 skrll Exp $ */
+/*	$NetBSD: dino.c,v 1.2 2014/03/31 20:51:20 christos Exp $ */
 
 /*	$OpenBSD: dino.c,v 1.5 2004/02/13 20:39:31 mickey Exp $	*/
 
@@ -29,7 +29,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: dino.c,v 1.1 2014/02/24 07:23:42 skrll Exp $");
+__KERNEL_RCSID(0, "$NetBSD: dino.c,v 1.2 2014/03/31 20:51:20 christos Exp $");
 
 /* #include "cardbus.h" */
 
@@ -149,7 +149,7 @@ pcireg_t dino_conf_read(void *, pcitag_t, int);
 void dino_conf_write(void *, pcitag_t, int, pcireg_t);
 
 int dino_intr_map(const struct pci_attach_args *, pci_intr_handle_t *);
-const char *dino_intr_string(void *, pci_intr_handle_t);
+const char *dino_intr_string(void *, pci_intr_handle_t, char *, size_t);
 void *dino_intr_establish(void *, pci_intr_handle_t, int,
     int (*)(void *), void *);
 void dino_intr_disestablish(void *, void *);
@@ -387,11 +387,9 @@ dino_intr_map(const struct pci_attach_args *pa, pci_intr_handle_t *ihp)
 }
 
 const char *
-dino_intr_string(void *v, pci_intr_handle_t ih)
+dino_intr_string(void *v, pci_intr_handle_t ih, char *buf, size_t len)
 {
-	static char buf[32];
-
-	snprintf(buf, 32, "irq %ld", ih);
+	snprintf(buf, len, "irq %ld", ih);
 	return buf;
 }
 
