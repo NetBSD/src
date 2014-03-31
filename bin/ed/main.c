e70b4a8ed599db7bb461ca5961e83c2a8b46aae4
christos <christos@NetBSD.org>
2014-03-31 12:55:46 +0000

    PR/48692: Henning Petersen: Missplaced paranthesis in bin/ed gbl.c and main.c

--- bin/ed/main.c
+++ bin/ed/main.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: main.c,v 1.26 2014/03/23 05:06:42 dholland Exp $	*/
+/*	$NetBSD: main.c,v 1.27 2014/03/31 12:55:46 christos Exp $	*/
 
 /* main.c: This file contains the main control and user-interface routines
    for the ed line editor. */
@@ -39,7 +39,7 @@ __COPYRIGHT(
 #if 0
 static char *rcsid = "@(#)main.c,v 1.1 1994/02/01 00:34:42 alm Exp";
 #else
-__RCSID("$NetBSD: main.c,v 1.26 2014/03/23 05:06:42 dholland Exp $");
+__RCSID("$NetBSD: main.c,v 1.27 2014/03/31 12:55:46 christos Exp $");
 #endif
 #endif /* not lint */
 
@@ -226,7 +226,7 @@ top:
 		    (status = exec_command()) >= 0)
 			if (!status || (status &&
 			    (status = display_lines(current_addr, current_addr,
-			        status))) >= 0)
+			        status)) >= 0))
 				continue;
 		switch (status) {
 		case EOF:
