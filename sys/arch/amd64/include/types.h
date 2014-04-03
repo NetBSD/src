b7471f452e4df9c077a8ffab5a0a6a363e800ef7
christos <christos@NetBSD.org>
2014-04-03 15:22:19 +0000

    we have cpu_bootconf()

--- sys/arch/amd64/include/types.h
+++ sys/arch/amd64/include/types.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: types.h,v 1.44 2014/03/20 20:45:32 christos Exp $	*/
+/*	$NetBSD: types.h,v 1.45 2014/04/03 15:22:36 christos Exp $	*/
 
 /*-
  * Copyright (c) 1990 The Regents of the University of California.
@@ -81,6 +81,7 @@ typedef	volatile unsigned char		__cpu_simple_lock_t;
 #define	__HAVE_NEW_STYLE_BUS_H
 #define	__HAVE_CPU_COUNTER
 #define	__HAVE_CPU_DATA_FIRST
+#define __HAVE_CPU_BOOTCONF
 #define	__HAVE_MD_CPU_OFFLINE
 #define	__HAVE_SYSCALL_INTERN
 #define	__HAVE_MINIMAL_EMUL
