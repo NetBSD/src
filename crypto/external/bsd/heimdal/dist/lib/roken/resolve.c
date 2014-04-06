c1828d73c7f1980efbfbd415e7e5f897cf722284
christos <christos@NetBSD.org>
2014-04-06 14:36:35 +0000

    PR/48718: Juergen Hannken-Illjes: Heimdal leaks file descriptors Add missing call to free the resolve handle on success. XXX: pullup 6

--- crypto/external/bsd/heimdal/dist/lib/roken/resolve.c
+++ crypto/external/bsd/heimdal/dist/lib/roken/resolve.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: resolve.c,v 1.1.1.1 2011/04/13 18:15:42 elric Exp $	*/
+/*	$NetBSD: resolve.c,v 1.2 2014/04/06 14:36:35 christos Exp $	*/
 
 /*
  * Copyright (c) 1995 - 2006 Kungliga Tekniska Högskolan
@@ -590,6 +590,7 @@ dns_lookup_int(const char *domain, int rr_class, int rr_type)
 
     len = min(len, size);
     r = parse_reply(reply, len);
+    resolve_free_handle(handle);
     free(reply);
     return r;
 }
