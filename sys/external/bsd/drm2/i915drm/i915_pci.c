dda94b5d51711ee37aee83916f8b92fcb56185d2
riastradh <riastradh@NetBSD.org>
2014-04-06 16:38:34 +0000

    Add missing declaration from last commit.

--- sys/external/bsd/drm2/i915drm/i915_pci.c
+++ sys/external/bsd/drm2/i915drm/i915_pci.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: i915_pci.c,v 1.5 2014/04/04 16:02:34 riastradh Exp $	*/
+/*	$NetBSD: i915_pci.c,v 1.6 2014/04/06 16:38:34 riastradh Exp $	*/
 
 /*-
  * Copyright (c) 2013 The NetBSD Foundation, Inc.
@@ -30,7 +30,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: i915_pci.c,v 1.5 2014/04/04 16:02:34 riastradh Exp $");
+__KERNEL_RCSID(0, "$NetBSD: i915_pci.c,v 1.6 2014/04/06 16:38:34 riastradh Exp $");
 
 #include <sys/types.h>
 #ifndef _MODULE
@@ -75,6 +75,7 @@ static int	i915drm_detach_framebuffer(device_t, int);
 
 static int	i915drm_fb_probe(struct drm_fb_helper *,
 		    struct drm_fb_helper_surface_size *);
+static void	i915drm_fb_detach(struct drm_fb_helper *);
 static int	i915drm_fb_create_handle(struct drm_framebuffer *,
 		    struct drm_file *, unsigned int *);
 static void	i915drm_fb_destroy(struct drm_framebuffer *);
@@ -411,7 +412,7 @@ i915drm_fb_probe(struct drm_fb_helper *fb_helper,
 		goto fail3;
 	}
 
-	prop_dictionary_set_bool(dict, "is_console", 1); /* XXX */
+	prop_dictionary_set_bool(dict, "is_console", 0); /* XXX */
 	prop_dictionary_set_uint32(dict, "width", mode_cmd.width);
 	prop_dictionary_set_uint32(dict, "height", mode_cmd.height);
 	prop_dictionary_set_uint8(dict, "depth", sizes->surface_bpp);
