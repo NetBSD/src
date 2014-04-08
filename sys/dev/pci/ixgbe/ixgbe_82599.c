a02d934e74479fcbb79b05cdddebaa473378a319
christos <christos@NetBSD.org>
2014-04-08 19:39:06 +0000

    Add dell's flavor (from uwe, in current-users@)

--- sys/dev/pci/ixgbe/ixgbe_82599.c
+++ sys/dev/pci/ixgbe/ixgbe_82599.c
@@ -31,7 +31,7 @@
 
 ******************************************************************************/
 /*$FreeBSD: src/sys/dev/ixgbe/ixgbe_82599.c,v 1.6 2011/01/19 19:36:27 jfv Exp $*/
-/*$NetBSD: ixgbe_82599.c,v 1.1 2011/08/12 21:55:29 dyoung Exp $*/
+/*$NetBSD: ixgbe_82599.c,v 1.2 2014/04/08 19:39:06 christos Exp $*/
 
 #include "ixgbe_type.h"
 #include "ixgbe_api.h"
@@ -434,6 +434,7 @@ enum ixgbe_media_type ixgbe_get_media_type_82599(struct ixgbe_hw *hw)
 		break;
 	case IXGBE_DEV_ID_82599_SFP:
 	case IXGBE_DEV_ID_82599_SFP_FCOE:
+	case IXGBE_DEV_ID_82599_SFP_DELL:
 		media_type = ixgbe_media_type_fiber;
 		break;
 	case IXGBE_DEV_ID_82599_CX4:
