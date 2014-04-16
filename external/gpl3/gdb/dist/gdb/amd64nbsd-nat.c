58574d7becd28486ea11df88a6625114a567b96b
christos <christos@NetBSD.org>
2014-04-16 00:32:33 +0000

    supply a fake cs that has has not have SEL_UPL set, so that our trapframe sniffer works.

--- external/gpl3/gdb/dist/gdb/amd64nbsd-nat.c
+++ external/gpl3/gdb/dist/gdb/amd64nbsd-nat.c
@@ -78,6 +78,7 @@ amd64nbsd_supply_pcb (struct regcache *regcache, struct pcb *pcb)
 {
   struct switchframe sf;
   int regnum;
+  long zero = 0;
 
   /* The following is true for NetBSD/amd64:
 
@@ -119,6 +120,8 @@ amd64nbsd_supply_pcb (struct regcache *regcache, struct pcb *pcb)
   regcache_raw_supply (regcache, AMD64_RBP_REGNUM, &pcb->pcb_rbp);
   regcache_raw_supply (regcache, AMD64_FS_REGNUM, &pcb->pcb_fs);
   regcache_raw_supply (regcache, AMD64_GS_REGNUM, &pcb->pcb_gs);
+  /* fake it to 0 so SEL_UPL is not set (our sniffer looks at CS) */
+  regcache_raw_supply (regcache, AMD64_CS_REGNUM, &zero);
 
   return 1;
 }
