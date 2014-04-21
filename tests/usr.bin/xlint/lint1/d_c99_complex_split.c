084b9a8547334d56c5178dba99bb8166c37e9a7c
christos <christos@NetBSD.org>
2014-04-21 19:09:52 +0000

    use a different function instead of __builtin_isnan, because it is polymorphic and we don't handle this yet.

--- tests/usr.bin/xlint/lint1/d_c99_complex_split.c
+++ tests/usr.bin/xlint/lint1/d_c99_complex_split.c
@@ -1,5 +1,8 @@
+int b(double a) {
+	return a == 0;
+}
 void a(void) {
     double _Complex z = 0;
-    if (__builtin_isnan((__real__ z)) && __builtin_isnan((__imag__ z)))
+    if (b(__real__ z) && b(__imag__ z))
 	return;
 }
