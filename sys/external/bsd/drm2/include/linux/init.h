ac93d6dfaa0f5eab7e14f1b5739f03d1353742cf
riastradh <riastradh@NetBSD.org>
2014-04-25 23:54:59 +0000

    Implement Linux dmi_check_system API.
    Use it to get a Thinkpad quirk for i915 graphics.
    
    Patch from nonoka@, PR 48708.

--- sys/external/bsd/drm2/include/linux/init.h
+++ sys/external/bsd/drm2/include/linux/init.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: init.h,v 1.2 2014/03/18 18:20:43 riastradh Exp $	*/
+/*	$NetBSD: init.h,v 1.3 2014/04/25 23:54:59 riastradh Exp $	*/
 
 /*-
  * Copyright (c) 2013 The NetBSD Foundation, Inc.
@@ -31,5 +31,7 @@
 
 #ifndef _LINUX_INIT_H_
 #define _LINUX_INIT_H_
+
+#define __init
 
 #endif  /* _LINUX_INIT_H_ */
