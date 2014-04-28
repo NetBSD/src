12014f31d74c095abbd713ae1ccc1110a60d6692
christos <christos@NetBSD.org>
2014-04-28 15:41:15 +0000

    use the same for the route metrics part, both in the sysctl and kmem paths. From Takahiro HAYASHI

--- usr.bin/netstat/netstat.h
+++ usr.bin/netstat/netstat.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: netstat.h,v 1.49 2014/04/24 14:54:51 christos Exp $	*/
+/*	$NetBSD: netstat.h,v 1.50 2014/04/28 15:41:15 christos Exp $	*/
 
 /*
  * Copyright (c) 1992, 1993
@@ -116,6 +116,8 @@ void	impstats(u_long, u_long);
 
 void	pr_rthdr(int, int);
 void	pr_family(int);
+struct rt_metrics;
+void	pr_rtrmx(struct rt_metrics *);
 void	rt_stats(u_long);
 char	*ns_phost(struct sockaddr *);
 
