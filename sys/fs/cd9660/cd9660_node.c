230fdb5d025e0392ddf4aa9c9621d461d9d2acdd
martin <martin@NetBSD.org>
2014-05-10 14:11:58 +0000

    PR kern/48787: inode calculation from ISO9660 block offset might get truncated to 32bit - force the whole expression to be evaluated as ino_t. Patch from Thomas Schmitt, with minor modifications (and reworded comment).

--- sys/fs/cd9660/cd9660_node.c
+++ sys/fs/cd9660/cd9660_node.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: cd9660_node.c,v 1.30 2014/02/27 16:51:38 hannken Exp $	*/
+/*	$NetBSD: cd9660_node.c,v 1.31 2014/05/10 14:11:58 martin Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1994
@@ -37,7 +37,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: cd9660_node.c,v 1.30 2014/02/27 16:51:38 hannken Exp $");
+__KERNEL_RCSID(0, "$NetBSD: cd9660_node.c,v 1.31 2014/05/10 14:11:58 martin Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -440,7 +440,14 @@ isodirino(struct iso_directory_record *isodir, struct iso_mnt *imp)
 {
 	ino_t ino;
 
-	ino = (isonum_733(isodir->extent) + isonum_711(isodir->ext_attr_length))
-	      << imp->im_bshift;
-	return (ino);
+	/*
+	 * Note there is an inverse calculation in
+	 * cd9660_vfsops.c:cd9660_vget_internal():
+	 *   ip->iso_start = ino >> imp->im_bshift;
+	 * and also a calculation of the isodir pointer
+	 * from an inode in cd9660_vnops.c:cd9660_readlink()
+	 */
+	ino = ((ino_t)isonum_733(isodir->extent) +
+		isonum_711(isodir->ext_attr_length)) << imp->im_bshift;
+	return ino;
 }
