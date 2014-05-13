3910868948c649c2d03625e21791b8ed644b3e65
bouyer <bouyer@NetBSD.org>
2014-05-13 19:36:16 +0000

    Make sure *(if_output)() is called with KERNEL_LOCK held. Add some KASSERT for this. See http://mail-index.netbsd.org/tech-net/2014/04/09/msg004511.html for details.

--- sys/net/if_ethersubr.c
+++ sys/net/if_ethersubr.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: if_ethersubr.c,v 1.196 2014/02/25 22:42:06 pooka Exp $	*/
+/*	$NetBSD: if_ethersubr.c,v 1.197 2014/05/13 19:36:16 bouyer Exp $	*/
 
 /*
  * Copyright (C) 1995, 1996, 1997, and 1998 WIDE Project.
@@ -61,7 +61,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: if_ethersubr.c,v 1.196 2014/02/25 22:42:06 pooka Exp $");
+__KERNEL_RCSID(0, "$NetBSD: if_ethersubr.c,v 1.197 2014/05/13 19:36:16 bouyer Exp $");
 
 #include "opt_inet.h"
 #include "opt_atalk.h"
@@ -210,6 +210,8 @@ ether_output(struct ifnet * const ifp0, struct mbuf * const m0,
 #ifdef NETATALK
 	struct at_ifaddr *aa;
 #endif /* NETATALK */
+
+	KASSERT(KERNEL_LOCKED_P());
 
 #ifdef MBUFTRACE
 	m_claimm(m, ifp->if_mowner);
