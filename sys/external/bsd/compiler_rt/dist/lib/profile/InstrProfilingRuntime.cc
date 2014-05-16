1d6622399820a0d07148aa73b697b928945dbf80
joerg <joerg@NetBSD.org>
2014-05-16 00:04:17 +0000

    Import compiler-rt r208593. Fix a build bug in __clear_cache by not explicitly forcing the ABI. Add first part of IEEE 754 quad support.

---
+++ sys/external/bsd/compiler_rt/dist/lib/profile/InstrProfilingRuntime.cc
@@ -0,0 +1,27 @@
+//===- InstrProfilingRuntime.cpp - PGO runtime initialization -------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+extern "C" {
+
+#include "InstrProfiling.h"
+
+int __llvm_profile_runtime;
+
+}
+
+namespace {
+
+class RegisterAtExit {
+public:
+  RegisterAtExit() { __llvm_profile_register_write_file_atexit(); }
+};
+
+RegisterAtExit Registration;
+
+}
