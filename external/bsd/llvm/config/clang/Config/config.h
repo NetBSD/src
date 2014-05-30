06b9ef4b70b2d946002484d9c839e27cf9f7e969
joerg <joerg@NetBSD.org>
2014-05-30 18:28:13 +0000

    Update build system for LLVM/Clang snapshot r209886. This brings in the merged AArch64 backend and the move of eARM to DWARF exception handling for NetBSD. C++11 is required for building.

--- external/bsd/llvm/config/clang/Config/config.h
+++ external/bsd/llvm/config/clang/Config/config.h
@@ -14,7 +14,7 @@
 #define C_INCLUDE_DIRS "/usr/include/clang-3.5:/usr/include"
 
 /* Linker version detected at compile time. */
-#define HOST_LINK_VERSION "2.23.52.20130913"
+#define HOST_LINK_VERSION "1"
 
 /* Default <path> to all compiler invocations for --sysroot=<path>. */
 #define DEFAULT_SYSROOT ""
