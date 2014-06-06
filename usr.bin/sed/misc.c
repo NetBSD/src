61e412cca503ad386a0deced0da3309761351cc2
wiz <wiz@NetBSD.org>
2014-06-06 21:56:39 +0000

    Renumber so that 3 follows 2.

--- usr.bin/sed/misc.c
+++ usr.bin/sed/misc.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: misc.c,v 1.13 2014/06/06 12:46:54 joerg Exp $	*/
+/*	$NetBSD: misc.c,v 1.14 2014/06/06 21:56:39 wiz Exp $	*/
 
 /*-
  * Copyright (c) 1992 Diomidis Spinellis.
@@ -16,7 +16,7 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 4. Neither the name of the University nor the names of its contributors
+ * 3. Neither the name of the University nor the names of its contributors
  *    may be used to endorse or promote products derived from this software
  *    without specific prior written permission.
  *
@@ -38,7 +38,7 @@
 #endif
 
 #include <sys/cdefs.h>
-__RCSID("$NetBSD: misc.c,v 1.13 2014/06/06 12:46:54 joerg Exp $");
+__RCSID("$NetBSD: misc.c,v 1.14 2014/06/06 21:56:39 wiz Exp $");
 #ifdef __FBSDID
 __FBSDID("$FreeBSD: head/usr.bin/sed/misc.c 200462 2009-12-13 03:14:06Z delphij $");
 #endif
