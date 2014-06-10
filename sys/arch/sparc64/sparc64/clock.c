b8ad3a0f5a5558541477179c90ae48b83d1c96e6
palle <palle@NetBSD.org>
2014-06-10 18:27:41 +0000

    sun4v: make cpu_initclocks() work on sun4v - %tick and %stick on sun4v systems are read-only. ok martin@

--- sys/arch/sparc64/sparc64/clock.c
+++ sys/arch/sparc64/sparc64/clock.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: clock.c,v 1.115 2014/04/30 00:09:29 mrg Exp $ */
+/*	$NetBSD: clock.c,v 1.116 2014/06/10 18:27:41 palle Exp $ */
 
 /*
  * Copyright (c) 1992, 1993
@@ -55,7 +55,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: clock.c,v 1.115 2014/04/30 00:09:29 mrg Exp $");
+__KERNEL_RCSID(0, "$NetBSD: clock.c,v 1.116 2014/06/10 18:27:41 palle Exp $");
 
 #include "opt_multiprocessor.h"
 
@@ -110,6 +110,7 @@ __KERNEL_RCSID(0, "$NetBSD: clock.c,v 1.115 2014/04/30 00:09:29 mrg Exp $");
  *  no counter-timer	 %tick		 -		 %tick
  *  US-IIe		 STICK		 -		 STICK
  *  US-IIIi		 %stick		 -		 %stick
+ *  sun4v		 %stick		 -		 %stick
  *
  * US-IIe and US-IIIi could use %tick as statclock
  */
@@ -472,7 +473,8 @@ cpu_initclocks(void)
 	}
 
 	/* Initialize the %tick register */
-	settick(0);
+	if (CPU_ISSUN4U || CPU_ISSUN4US)
+		settick(0);
 
 	/* Register timecounter "tick-counter" */
 	tick_timecounter.tc_frequency = ci->ci_cpu_clockrate[0];
@@ -480,7 +482,7 @@ cpu_initclocks(void)
 
 	/* Register timecounter "stick-counter" */
 	if (ci->ci_system_clockrate[0] != 0) {
-		if (CPU_IS_HUMMINGBIRD()) {
+		if (CPU_ISSUN4U && CPU_IS_HUMMINGBIRD()) {
 #if NPSYCHO > 0
 			psycho_setstick(0);
 			stick2e_timecounter.tc_frequency =
@@ -488,7 +490,8 @@ cpu_initclocks(void)
 			tc_init(&stick2e_timecounter);
 #endif
 		} else {
-			setstick(0);
+			if (CPU_ISSUN4U || CPU_ISSUN4US)
+				setstick(0);
 			stick_timecounter.tc_frequency =
 			    ci->ci_system_clockrate[0];
 			tc_init(&stick_timecounter);
@@ -513,7 +516,7 @@ cpu_initclocks(void)
 
 			/* We don't have a counter-timer -- use %tick */
 			tickintr_establish(PIL_CLOCK, tickintr);
-		} else if (CPU_IS_HUMMINGBIRD()) {
+		} else if (CPU_ISSUN4U && CPU_IS_HUMMINGBIRD()) {
 #if NPSYCHO > 0
 			aprint_normal("No counter-timer -- using STICK "
 			    "at %luMHz as system clock.\n",
