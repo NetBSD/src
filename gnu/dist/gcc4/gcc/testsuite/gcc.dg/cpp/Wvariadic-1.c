901f7f731cb957e613f28231f31b49be4d785e3e
mrg <mrg@NetBSD.org>
2014-06-13 00:35:37 +0000

    delete GCC 4.1 sources.

--- gnu/dist/gcc4/gcc/testsuite/gcc.dg/cpp/Wvariadic-1.c
+++
@@ -1,6 +0,0 @@
-/* { dg-do compile } */
-/* { dg-options "-std=iso9899:1990 -pedantic -Werror" } */
-
-#define f(x,...)	/* { dg-error "variadic" } */
-#define g(x,y...)	/* { dg-error "variadic" } */
-int not_empty;
