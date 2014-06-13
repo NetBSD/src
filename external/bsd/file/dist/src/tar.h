0e74392004c097d46fba47aaafbd26782a4d6f6a
christos <christos@NetBSD.org>
2014-06-13 01:48:18 +0000

    import file-5.19
    2014-06-09   9:04  Christos Zoulas <christos@zoulas.com>
    
    	* Misc buffer overruns and missing buffer size tests in cdf parsing
    	  (Francisco Alonso, Jan Kaluza)
    
    2014-06-02  14:50  Christos Zoulas <christos@zoulas.com>
    
    	* Enforce limit of 8K on regex searches that have no limits
    	* Allow the l modifier for regex to mean line count. Default
    	  to byte count. If line count is specified, assume a max
    	  of 80 characters per line to limit the byte count.
    	* Don't allow conversions to be used for dates, allowing
    	  the mask field to be used as an offset.
    
    2014-05-30  12:51  Christos Zoulas <christos@zoulas.com>
    
    	* Make the range operator limit the length of the
    	  regex search.
    
    2014-05-14  19:23  Christos Zoulas <christos@zoulas.com>
    
    	* PR/347: Windows fixes
    	* PR/352: Hangul word processor recognition
    	* PR/354: Encoding irregularities in text files
    
    2014-05-06  6:12  Christos Zoulas <christos@zoulas.com>
    
    	* Fix uninitialized title in CDF files (Jan Kaluza)
    
    2014-05-04  14:55  Christos Zoulas <christos@zoulas.com>
    
    	* PR/351: Fix compilation of empty files
    
    2014-04-30  17:39  Christos Zoulas <christos@zoulas.com>
    
    	* Fix integer formats: We don't specify 'l' or
    	  'h' and 'hh' specifiers anymore, only 'll' for
    	  quads and nothing for the rest. This is so that
    	  magic writing is simpler.
    
    2014-04-01  15:25  Christos Zoulas <christos@zoulas.com>
    
    	* PR/341: Jan Kaluza, fix memory leak
    	* PR/342: Jan Kaluza, fix out of bounds read
    
    2014-03-28  15:25  Christos Zoulas <christos@zoulas.com>
    
    	* Fix issue with long formats not matching fmtcheck

--- external/bsd/file/dist/src/tar.h
+++ external/bsd/file/dist/src/tar.h
@@ -1,5 +1,4 @@
-/*	$NetBSD: tar.h,v 1.1.1.2 2011/05/12 20:46:54 christos Exp $	*/
-
+/*	$NetBSD: tar.h,v 1.1.1.3 2014/06/13 01:48:23 christos Exp $	*/
 /*
  * Copyright (c) Ian F. Darwin 1986-1995.
  * Software written by Ian F. Darwin and others;
