b7049df3424fdffb163ce1b482041ff5f4fc583a
mrg <mrg@NetBSD.org>
2014-06-13 00:35:37 +0000

    delete GCC 4.1 sources.

--- gnu/dist/gcc4/gcc/testsuite/g++.dg/init/complex1.C
+++
@@ -1,7 +0,0 @@
-/* PR c++/21210 */
-/* { dg-do compile } */
-/* { dg-options "-O2" } */
-
-typedef float __complex__ fcomplex;
-fcomplex cplx = fcomplex();
-
--- gnu/dist/gcc4/gcc/testsuite/g++.old-deja/g++.pt/crash14.C
+++
@@ -1,5 +0,0 @@
-// { dg-do assemble  }
-
-template <class T> struct A {};
-template <class T> struct A<T*>;
-A<int*> ai; // { dg-error "" } incomplete type
