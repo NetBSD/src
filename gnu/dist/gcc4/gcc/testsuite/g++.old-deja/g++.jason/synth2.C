96dcd295da4c681fe94f63c68768370b4f4d8019
mrg <mrg@NetBSD.org>
2014-06-13 00:35:37 +0000

    delete GCC 4.1 sources.

--- gnu/dist/gcc4/gcc/testsuite/g++.old-deja/g++.jason/synth2.C
+++
@@ -1,8 +0,0 @@
-// { dg-do assemble  }
-// PRMS Id: 4623
-// Bug: g++ tries and fails to synthesize a copy constructor for D.
-
-class A { };
-class B: public virtual A { };
-class C: public A { };
-class D: public B, public C { }; // { dg-bogus "" } bad synthesis
