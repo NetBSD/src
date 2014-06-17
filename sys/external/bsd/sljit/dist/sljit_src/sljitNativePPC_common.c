61fe5216748704ad429d416a58d2bbd75ccad892
alnsn <alnsn@NetBSD.org>
2014-06-17 19:33:20 +0000

    Add $NetBSD$.

--- sys/external/bsd/sljit/dist/sljit_src/sljitNativePPC_common.c
+++ sys/external/bsd/sljit/dist/sljit_src/sljitNativePPC_common.c
@@ -1,3 +1,5 @@
+/*	$NetBSD: sljitNativePPC_common.c,v 1.4 2014/06/17 19:33:20 alnsn Exp $	*/
+
 /*
  *    Stack-less Just-In-Time compiler
  *
--- sys/external/bsd/sljit/dist/sljit_src/sljitNativeX86_32.c
+++ sys/external/bsd/sljit/dist/sljit_src/sljitNativeX86_32.c
@@ -1,3 +1,5 @@
+/*	$NetBSD: sljitNativeX86_32.c,v 1.4 2014/06/17 19:33:20 alnsn Exp $	*/
+
 /*
  *    Stack-less Just-In-Time compiler
  *
