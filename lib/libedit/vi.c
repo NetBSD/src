e06822a79d0da795fcbf829bc1641dca0f54cf20
christos <christos@NetBSD.org>
2014-06-18 18:12:28 +0000

    Don't depend on weak aliases to define the vi "alias" expansion function, provide an API instead to set it.

--- lib/libedit/vi.c
+++ lib/libedit/vi.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: vi.c,v 1.44 2014/06/18 13:03:08 christos Exp $	*/
+/*	$NetBSD: vi.c,v 1.45 2014/06/18 18:12:28 christos Exp $	*/
 
 /*-
  * Copyright (c) 1992, 1993
@@ -42,7 +42,7 @@
 #if 0
 static char sccsid[] = "@(#)vi.c	8.1 (Berkeley) 6/4/93";
 #else
-__RCSID("$NetBSD: vi.c,v 1.44 2014/06/18 13:03:08 christos Exp $");
+__RCSID("$NetBSD: vi.c,v 1.45 2014/06/18 18:12:28 christos Exp $");
 #endif
 #endif /* not lint && not SCCSID */
 
@@ -918,43 +918,26 @@ vi_comment_out(EditLine *el, Int c __attribute__((__unused__)))
  * NB: posix implies that we should enter insert mode, however
  * this is against historical precedent...
  */
-#if defined(__weak_reference)
-# define libedit_weak(a) __weak_reference(a)
-# define libedit_weak_visible __weakref_visible
-#elif defined(__weak_extern)
-# define libedit_weak(a) __weak_extern(a)
-# define libedit_weak_visible
-#endif
-
-#ifdef libedit_weak
-libedit_weak_visible
-char *my_get_alias_text(const char *) libedit_weak(get_alias_text);
-#endif
-
 protected el_action_t
 /*ARGSUSED*/
 vi_alias(EditLine *el, Int c __attribute__((__unused__)))
 {
-#ifdef libedit_weak
 	char alias_name[3];
-	char *alias_text;
+	const char *alias_text;
 
-	if (my_get_alias_text == 0) {
+	if (el->el_chared.c_aliasfun == NULL)
 		return CC_ERROR;
-	}
 
 	alias_name[0] = '_';
 	alias_name[2] = 0;
 	if (el_getc(el, &alias_name[1]) != 1)
 		return CC_ERROR;
 
-	alias_text = my_get_alias_text(alias_name);
+	alias_text = (*el->el_chared.c_aliasfun)(el->el_chared.c_aliasarg,
+	    alias_name);
 	if (alias_text != NULL)
 		FUN(el,push)(el, ct_decode_string(alias_text, &el->el_scratch));
 	return CC_NORM;
-#else
-	return CC_ERROR;
-#endif
 }
 
 /* vi_to_history_line():
