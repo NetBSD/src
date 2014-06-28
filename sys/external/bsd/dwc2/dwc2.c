1d68997e4ce2af55ee9ed6681dfb0ad5c55b0e48
skrll <skrll@NetBSD.org>
2014-06-28 07:01:51 +0000

    Use DWC2_XFER2DPIPE

--- sys/external/bsd/dwc2/dwc2.c
+++ sys/external/bsd/dwc2/dwc2.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: dwc2.c,v 1.27 2014/06/27 07:28:26 skrll Exp $	*/
+/*	$NetBSD: dwc2.c,v 1.28 2014/06/28 07:01:51 skrll Exp $	*/
 
 /*-
  * Copyright (c) 2013 The NetBSD Foundation, Inc.
@@ -30,7 +30,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: dwc2.c,v 1.27 2014/06/27 07:28:26 skrll Exp $");
+__KERNEL_RCSID(0, "$NetBSD: dwc2.c,v 1.28 2014/06/28 07:01:51 skrll Exp $");
 
 #include "opt_usb.h"
 
@@ -1096,7 +1096,7 @@ dwc2_device_intr_transfer(usbd_xfer_handle xfer)
 Static usbd_status
 dwc2_device_intr_start(usbd_xfer_handle xfer)
 {
-	struct dwc2_pipe *dpipe = (struct dwc2_pipe *)xfer->pipe;
+	struct dwc2_pipe *dpipe = DWC2_XFER2DPIPE(xfer)
 	usbd_device_handle dev = dpipe->pipe.device;
 	struct dwc2_softc *sc = dev->bus->hci_private;
 	usbd_status err;
@@ -1178,7 +1178,7 @@ dwc2_device_isoc_transfer(usbd_xfer_handle xfer)
 usbd_status
 dwc2_device_isoc_start(usbd_xfer_handle xfer)
 {
-	struct dwc2_pipe *dpipe = (struct dwc2_pipe *)xfer->pipe;
+	struct dwc2_pipe *dpipe = DWC2_XFER2DPIPE(xfer);
 	usbd_device_handle dev = dpipe->pipe.device;
 	struct dwc2_softc *sc = dev->bus->hci_private;
 	usbd_status err;
