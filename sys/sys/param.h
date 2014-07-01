9f747ce3bd4ae9e875d71a5efb6bc4986491223f
rtr <rtr@NetBSD.org>
2014-07-01 13:25:21 +0000

    Bump to version 6.99.45 for removal of struct lwp * parameter from pr_ioctl

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.454 2014/06/14 07:39:00 hannken Exp $	*/
+/*	$NetBSD: param.h,v 1.455 2014/07/01 13:25:21 rtr Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -63,7 +63,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	699004400	/* NetBSD 6.99.44 */
+#define	__NetBSD_Version__	699004500	/* NetBSD 6.99.45 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
