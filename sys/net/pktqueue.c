d42d6967e361d0cfe1a280888792c142b25ac62c
ozaki-r <ozaki-r@NetBSD.org>
2014-07-04 01:50:22 +0000

    Fix pktq_enqueue for rump
    Add _RUMP_NATIVE_ABI to the macro condition for i386 and x86_64 because
    _RUMPKERNEL is not defined for them. See sys/rump/Makefile.rump.
    
    Found by ATF

--- sys/net/pktqueue.c
+++ sys/net/pktqueue.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: pktqueue.c,v 1.7 2014/07/02 07:30:37 ozaki-r Exp $	*/
+/*	$NetBSD: pktqueue.c,v 1.8 2014/07/04 01:50:22 ozaki-r Exp $	*/
 
 /*-
  * Copyright (c) 2014 The NetBSD Foundation, Inc.
@@ -36,7 +36,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: pktqueue.c,v 1.7 2014/07/02 07:30:37 ozaki-r Exp $");
+__KERNEL_RCSID(0, "$NetBSD: pktqueue.c,v 1.8 2014/07/04 01:50:22 ozaki-r Exp $");
 
 #include <sys/param.h>
 #include <sys/types.h>
@@ -208,7 +208,7 @@ pktq_rps_hash(const struct mbuf *m __unused)
 bool
 pktq_enqueue(pktqueue_t *pq, struct mbuf *m, const u_int hash __unused)
 {
-#ifdef _RUMPKERNEL
+#if defined(_RUMPKERNEL) || defined(_RUMP_NATIVE_ABI)
 	const unsigned cpuid = curcpu()->ci_index;
 #else
 	const unsigned cpuid = hash % ncpu;
