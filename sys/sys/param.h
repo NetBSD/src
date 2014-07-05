6d2196c21625678df1b90e8ff5da8c4fa983b3d4
hannken <hannken@NetBSD.org>
2014-07-05 09:33:15 +0000

    Add vcache operations to support key changes:
    vcache_rekey_enter locks the old cache node and creates and locks the
      new cache node.  It is an error if the new cache node exists.
    
    vcache_rekey_exit removes the old cache node and finalizes and
      unlocks the new cache node.
    
    No objections on tech-kern@
    
    Welcome to 6.99.46

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.455 2014/07/01 13:25:21 rtr Exp $	*/
+/*	$NetBSD: param.h,v 1.456 2014/07/05 09:33:15 hannken Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -63,7 +63,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	699004500	/* NetBSD 6.99.45 */
+#define	__NetBSD_Version__	699004600	/* NetBSD 6.99.46 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
