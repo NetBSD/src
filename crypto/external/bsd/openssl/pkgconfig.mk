a448574c7464989d40aba251e1ccb68c06c7639d
joerg <joerg@NetBSD.org>
2014-07-07 09:14:43 +0000

    Use cross-cpp when extracting OpenSSL version.

--- crypto/external/bsd/openssl/pkgconfig.mk
+++ crypto/external/bsd/openssl/pkgconfig.mk
@@ -1,4 +1,4 @@
-# $NetBSD: pkgconfig.mk,v 1.5 2014/04/07 17:40:49 apb Exp $
+# $NetBSD: pkgconfig.mk,v 1.6 2014/07/07 09:14:43 joerg Exp $
 
 .include <bsd.own.mk>
 
@@ -8,5 +8,5 @@ FILES+=${pkg}.pc
 FILESBUILD_${pkg}.pc=yes
 
 ${pkg}.pc: ${.CURDIR}/../../mkpc
-	${HOST_SH} ${.ALLSRC} ${OPENSSLSRC}/crypto ${.TARGET} > ${.TARGET}
+	CPPFLAGS=${CPPFLAGS:N-DOPENSSLDIR=*:N-DENGINESDIR=*:Q} CPP=${CPP:Q} ${HOST_SH} ${.ALLSRC} ${OPENSSLSRC}/crypto ${.TARGET} > ${.TARGET}
 .endfor
