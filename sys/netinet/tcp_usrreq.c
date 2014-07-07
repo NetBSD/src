3dc067143859cb159684552a1b9e63d0e2ef78ee
rtr <rtr@NetBSD.org>
2014-07-07 15:13:21 +0000

    backout change that made pr_stat return EOPNOTSUPP for protocols that were not filling in struct stat.
    decision made after further discussion with rmind and investigation of
    how other operating systems behave.  soo_stat() is doing just enough to
    be able to call what gets returned valid and thus justifys a return of
    success.
    
    additional review will be done to determine of the pr_stat functions
    that were already returning EOPNOTSUPP can be considered successful with
    what soo_stat() is doing.

--- sys/netinet/tcp_usrreq.c
+++ sys/netinet/tcp_usrreq.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: tcp_usrreq.c,v 1.182 2014/07/07 07:09:58 rtr Exp $	*/
+/*	$NetBSD: tcp_usrreq.c,v 1.183 2014/07/07 15:13:21 rtr Exp $	*/
 
 /*
  * Copyright (C) 1995, 1996, 1997, and 1998 WIDE Project.
@@ -99,7 +99,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: tcp_usrreq.c,v 1.182 2014/07/07 07:09:58 rtr Exp $");
+__KERNEL_RCSID(0, "$NetBSD: tcp_usrreq.c,v 1.183 2014/07/07 15:13:21 rtr Exp $");
 
 #include "opt_inet.h"
 #include "opt_ipsec.h"
@@ -956,7 +956,8 @@ tcp_ioctl(struct socket *so, u_long cmd, void *nam, struct ifnet *ifp)
 static int
 tcp_stat(struct socket *so, struct stat *ub)
 {
-	return EOPNOTSUPP;
+	/* stat: don't bother with a blocksize.  */
+	return 0;
 }
 
 /*
