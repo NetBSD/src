764dfc763e12e0f7af1795a19c80a7c13c6657fd
alnsn <alnsn@NetBSD.org>
2014-07-08 21:44:26 +0000

    Clone libbpfjit tests to check kernel implementation of bpfjit. Old content of t_bpfjit.c will be moved to t_mbuf.c shortly. Change packet buffers to unsigned char[] type.

--- tests/net/bpf/h_bpf.h
+++ tests/net/bpf/h_bpf.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: h_bpf.h,v 1.1 2014/07/07 19:34:32 alnsn Exp $	*/
+/*	$NetBSD: h_bpf.h,v 1.2 2014/07/08 21:44:26 alnsn Exp $	*/
 
 /*-
  * Copyright (c) 2014 Alexander Nasonov.
@@ -57,18 +57,18 @@ void rumpns_bpfjit_free_code(bpfjit_func_t);
  */
 static inline const uint8_t *
 init_mchain2(struct mbuf *mb1, struct mbuf *mb2,
-    char pkt[], size_t pktsize, size_t split)
+    unsigned char pkt[], size_t pktsize, size_t split)
 {
 
 	(void)memset(mb1, 0, sizeof(*mb1));
-	mb1->m_data = pkt;
+	mb1->m_data = (char *)pkt;
 	mb1->m_next = (split < pktsize) ? mb2 : NULL;
 	mb1->m_len = (split < pktsize) ? split : pktsize;
 
 	if (split < pktsize) {
 		(void)memset(mb2, 0, sizeof(*mb2));
 		mb2->m_next = NULL;
-		mb2->m_data = &pkt[split];
+		mb2->m_data = (char *)&pkt[split];
 		mb2->m_len = pktsize - split;
 	}
 
@@ -76,11 +76,39 @@ init_mchain2(struct mbuf *mb1, struct mbuf *mb2,
 }
 
 /*
+ * Compile and run a filter program.
+ */
+static inline unsigned int
+exec_prog(struct bpf_insn *insns, size_t insn_count,
+    unsigned char pkt[], size_t pktsize)
+{
+	bpfjit_func_t fn;
+	bpf_args_t args;
+	unsigned int res;
+
+	args.pkt = (const uint8_t *)pkt;
+	args.buflen = pktsize;
+	args.wirelen = pktsize;
+
+	rump_schedule();
+	fn = rumpns_bpfjit_generate_code(NULL, insns, insn_count);
+	rump_unschedule();
+
+	res = fn(NULL, &args);
+
+	rump_schedule();
+	rumpns_bpfjit_free_code(fn);
+	rump_unschedule();
+
+	return res;
+}
+
+/*
  * Interpret a filter program with mbuf chain passed to bpf_filter_ext().
  */
 static inline unsigned int
 interp_prog_mchain2(struct bpf_insn *insns,
-    char *pkt, size_t pktsize, size_t split)
+    unsigned char pkt[], size_t pktsize, size_t split)
 {
 	uint32_t mem[BPF_MEMWORDS];
 	struct mbuf mb1, mb2;
@@ -104,7 +132,7 @@ interp_prog_mchain2(struct bpf_insn *insns,
  */
 static inline unsigned int
 exec_prog_mchain2(struct bpf_insn *insns, size_t insn_count,
-    char *pkt, size_t pktsize, size_t split)
+    unsigned char pkt[], size_t pktsize, size_t split)
 {
 	bpfjit_func_t fn;
 	struct mbuf mb1, mb2;
