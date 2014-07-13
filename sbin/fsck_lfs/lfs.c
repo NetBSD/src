c84ddfc681d73a71610436020d4bed66ea1cd3b1
dholland <dholland@NetBSD.org>
2014-07-13 02:44:21 +0000

    Revert previous; it doesn't work because all the lfs tools promiscuously .PATH in each other's source files, and I haven't the time or patience to deal with it tonight.

--- sbin/fsck_lfs/lfs.c
+++ sbin/fsck_lfs/lfs.c
@@ -1,4 +1,4 @@
-/* $NetBSD: lfs.c,v 1.42 2014/07/12 19:44:00 dholland Exp $ */
+/* $NetBSD: lfs.c,v 1.43 2014/07/13 02:44:21 dholland Exp $ */
 /*-
  * Copyright (c) 2003 The NetBSD Foundation, Inc.
  * All rights reserved.
@@ -85,7 +85,6 @@
 #include <unistd.h>
 #include <util.h>
 
-#include "fsutil.h"
 #include "bufcache.h"
 #include "vnode.h"
 #include "lfs_user.h"
@@ -108,6 +107,7 @@ static int
 lfs_fragextend(struct uvnode *, int, int, daddr_t, struct ubuf **);
 
 int fsdirty = 0;
+void (*panic_func)(int, const char *, va_list) = my_vpanic;
 
 /*
  * LFS buffer and uvnode operations
@@ -835,12 +835,19 @@ check_summary(struct lfs *fs, SEGSUM *sp, ulfs_daddr_t pseg_addr, int debug,
 
 /* print message and exit */
 void
+my_vpanic(int fatal, const char *fmt, va_list ap)
+{
+        (void) vprintf(fmt, ap);
+	exit(8);
+}
+
+void
 call_panic(const char *fmt, ...)
 {
 	va_list ap;
 
 	va_start(ap, fmt);
-        vmsg(1, fmt, ap);
+        panic_func(1, fmt, ap);
 	va_end(ap);
 }
 
