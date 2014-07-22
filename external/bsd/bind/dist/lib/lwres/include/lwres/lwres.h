87d0a8b43642044a3a3b16f54d42621eea880f93
christos <christos@NetBSD.org>
2014-07-22 00:11:22 +0000

    fix lint build

--- external/bsd/bind/dist/lib/lwres/include/lwres/lwres.h
+++ external/bsd/bind/dist/lib/lwres/include/lwres/lwres.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: lwres.h,v 1.5 2014/07/08 05:43:40 spz Exp $	*/
+/*	$NetBSD: lwres.h,v 1.6 2014/07/22 00:11:22 christos Exp $	*/
 
 /*
  * Copyright (C) 2004-2007  Internet Systems Consortium, Inc. ("ISC")
@@ -215,8 +215,8 @@ typedef struct {
  * resolv.conf data
  */
 
-#define LWRES_CONFMAXNAMESERVERS 3	/*%< max 3 "nameserver" entries */
-#define LWRES_CONFMAXLWSERVERS 1	/*%< max 1 "lwserver" entry */
+#define LWRES_CONFMAXNAMESERVERS 3	/*%< max 3 nameserver entries */
+#define LWRES_CONFMAXLWSERVERS 1	/*%< max 1 lwserver entry */
 #define LWRES_CONFMAXSEARCH 8		/*%< max 8 domains in "search" entry */
 #define LWRES_CONFMAXLINELEN 256	/*%< max size of a line */
 #define LWRES_CONFMAXSORTLIST 10	/*%< max 10 */
