e6cd9e832a6cac257c7f17601f90c56c146df80b
riastradh <riastradh@NetBSD.org>
2014-07-24 21:18:40 +0000

    Rework Intel framebuffer attachment.
    Turns out genfb_softc must be at the start of its own device_private,
    so we can't have it in the middle of a structure, and certainly can't
    have potentially multiple genfbs per graphics device.  Failing to do
    this is why entering ddb didn't work with an i915drmkms console.
    
    Instead of putting a genfb_softc into each drm_fb_helper, put a
    device_t into each drm_fb_helper and create a new intelfb(4) device
    for each Intel framebuffer.  This will be more flexible, too, in case
    we want accelerated framebuffers later on.  (XXX Need to adapt the
    radeon code, which I'm disabling until that happens to avoid breaking
    the build.)
    
    While here, defer disabling VGA, vga_cndetach, and genfb_attach until
    we're actually ready to do them all together along with the mode
    switch.  This should reduce the amount of time during which the
    screen is blanked, in case it hangs.

---
+++ sys/external/bsd/drm2/i915drm/i915_pci.h
@@ -0,0 +1,56 @@
+/*	$NetBSD: i915_pci.h,v 1.1 2014/07/24 21:18:40 riastradh Exp $	*/
+
+/*-
+ * Copyright (c) 2014 The NetBSD Foundation, Inc.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to The NetBSD Foundation
+ * by Taylor R. Campbell.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef	_I915DRM_I915_PCI_H_
+#define	_I915DRM_I915_PCI_H_
+
+#include <sys/queue.h>
+#include <sys/workqueue.h>
+
+struct i915drmkms_task {
+	union {
+		SIMPLEQ_ENTRY(i915drmkms_task)	queue;
+		struct work			work;
+	}			ift_u;
+	void			(*ift_fn)(struct i915drmkms_task *);
+};
+
+static inline void
+i915drmkms_task_init(struct i915drmkms_task *task,
+    void (*fn)(struct i915drmkms_task *))
+{
+
+	task->ift_fn = fn;
+}
+
+int	i915drmkms_task_schedule(device_t, struct i915drmkms_task *);
+
+#endif	/* _I915DRM_I915_PCI_H_ */
