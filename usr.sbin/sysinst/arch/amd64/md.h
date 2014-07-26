b6a98f4ac92805d8fcef9a77aacc4b584bcf6a33
dholland <dholland@NetBSD.org>
2014-07-26 19:30:39 +0000

    Move sysinst sources to usr.sbin.
    This commit only physically moves the sources - there are no other
    changes, to maximize the probability that this will be treated as a
    rename if we ever do manage to migrate away from CVS.
    
    Moving sysinst has been discussed on and off for years and has two
    goals: making it easier to work on sysinst, and also making sysinst
    available on running systems for use installing chroots and VM images
    and other such things. None of the latter is possible yet, but as they
    say, one thing at a time.
    
    Doing this now was approved in an impromptu fashion by mrg, riz,
    riastradh, me, and groo.

---
+++ usr.sbin/sysinst/arch/amd64/md.h
@@ -0,0 +1,117 @@
+/*	$NetBSD: md.h,v 1.1 2014/07/26 19:30:44 dholland Exp $	*/
+
+/*
+ * Copyright 1997 Piermont Information Systems Inc.
+ * All rights reserved.
+ *
+ * Written by Philip A. Nelson for Piermont Information Systems Inc.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The name of Piermont Information Systems Inc. may not be used to endorse
+ *    or promote products derived from this software without specific prior
+ *    written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY PIERMONT INFORMATION SYSTEMS INC. ``AS IS''
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL PIERMONT INFORMATION SYSTEMS INC. BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+/* md.h -- Machine specific definitions for the i386 */
+
+
+#include <machine/cpu.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/bootblock.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+/* i386 uses the mbr code. */
+#include "mbr.h"
+
+/* constants and defines */
+#define	DEFUSRSIZE	0
+#define	DEFSWAPSIZE	(-1)
+
+/* Megs required for a full X installation. */
+#define XNEEDMB 50
+
+/* use UFS2 by default for ffs */
+#define	DEFAULT_UFS2
+
+/* have support for booting from UFS2 */
+#define	HAVE_UFS2_BOOT
+
+/* allow using tmpfs for /tmp instead of mfs */
+#define HAVE_TMPFS
+
+/* have file system specific primary boot loader */
+#define	HAVE_BOOTXX_xFS
+#define	BOOTXXDIR	"/usr/mdec"
+#define	BOOTXX_FFSV1	"bootxx_ffsv1"
+#define	BOOTXX_FFSV2	"bootxx_ffsv2"
+
+
+/*
+ *  Default filesets to fetch and install during installation
+ *  or upgrade. The standard sets are:
+ *      base etc comp games man misc tests text xbase xcomp xetc xfont xserver
+ *
+ * x86_64 has the  MD set kern first, because generic kernels are  too
+ * big to fit on install floppies.
+ *
+ * Third entry is the last extension name in the split sets for loading
+ * from floppy.
+ */
+#define SET_KERNEL_1_NAME	"kern-GENERIC"
+
+#define SET_KERNEL_GENERIC	SET_KERNEL_1
+
+/*
+ * Disk names accepted as valid targets for a from-scratch installation.
+ *
+ * On amd64, we allow "wd"  ST-506/IDE disks,  "sd" scsi disks, "ld" logical
+ * disks and "raid" raidframe disks.
+ */
+#define DISK_NAMES "wd", "sd", "ld", "raid:no_mbr", "xbd:no_mbr"
+
+/*
+ * Machine-specific command to write a new label to a disk.
+ * For example, i386  uses "/sbin/disklabel -w -r", just like i386
+ * miniroot scripts, though this may leave a bogus incore label.
+ * Sun ports should probably use  DISKLABEL_CMD "/sbin/disklabel -w"
+ * to get incore  to ondisk inode translation for the Sun proms.
+ * If not defined, we assume the port does not support disklabels and
+ * hand-edited disklabel will NOT be written by MI code.
+ *
+ * On amd64, do what the 1.2 install scripts did.
+ */
+#define DISKLABEL_CMD "disklabel -w -r"
+
+#define _PATH_MBR	DEFAULT_BOOTDIR "/" DEFAULT_BOOTCODE
+#define _PATH_BOOTSEL	DEFAULT_BOOTDIR "/" DEFAULT_BOOTSELCODE
+
+extern struct mbr_bootsel *mbs;
+
+
+/*
+ *  prototypes for MD code.
+ */
+
+
---
+++ usr.sbin/sysinst/arch/luna68k/md.c
@@ -0,0 +1,258 @@
+/*	$NetBSD: md.c,v 1.1 2014/07/26 19:30:46 dholland Exp $	*/
+
+/*
+ * Copyright 1997 Piermont Information Systems Inc.
+ * All rights reserved.
+ *
+ * Based on code written by Philip A. Nelson for Piermont Information
+ * Systems Inc.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The name of Piermont Information Systems Inc. may not be used to endorse
+ *    or promote products derived from this software without specific prior
+ *    written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY PIERMONT INFORMATION SYSTEMS INC. ``AS IS''
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL PIERMONT INFORMATION SYSTEMS INC. BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/* md.c -- luna68k machine specific routines */
+
+#include <sys/types.h>
+#include <sys/disklabel.h>
+#include <sys/ioctl.h>
+#include <sys/param.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <curses.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <util.h>
+
+#include "defs.h"
+#include "md.h"
+#include "msg_defs.h"
+#include "menu_defs.h"
+
+#define PART_BOOT_BSIZE	4096
+#define PART_BOOT_FSIZE	512
+
+void
+md_init(void)
+{
+}
+
+void
+md_init_set_status(int flags)
+{
+
+	(void)flags;
+}
+
+int
+md_get_info(void)
+{
+	struct disklabel disklabel;
+	int fd;
+	char dev_name[100];
+
+	snprintf(dev_name, sizeof(dev_name), "/dev/r%sc", diskdev);
+
+	fd = open(dev_name, O_RDONLY, 0);
+	if (fd < 0) {
+		endwin();
+		fprintf (stderr, "Can't open %s\n", dev_name);
+		exit(1);
+	}
+	if (ioctl(fd, DIOCGDINFO, &disklabel) == -1) {
+		endwin();
+		fprintf (stderr, "Can't read disklabel on %s.\n", dev_name);
+		close(fd);
+		exit(1);
+	}
+	close(fd);
+
+	dlcyl = disklabel.d_ncylinders;
+	dlhead = disklabel.d_ntracks;
+	dlsec = disklabel.d_nsectors;
+	sectorsize = disklabel.d_secsize;
+	dlcylsize = disklabel.d_secpercyl;
+
+	/*
+	 * Compute whole disk size. Take max of (dlcyl*dlhead*dlsec)
+	 * and secperunit,  just in case the disk is already labelled.
+	 * (If our new label's RAW_PART size ends up smaller than the
+	 * in-core RAW_PART size  value, updating the label will fail.)
+	 */
+	dlsize = dlcyl * dlhead * dlsec;
+	if (disklabel.d_secperunit > dlsize)
+		dlsize = disklabel.d_secperunit;
+
+	return 1;
+}
+
+/*
+ * md back-end code for menu-driven BSD disklabel editor.
+ */
+int
+md_make_bsd_partitions(void)
+{
+
+	return make_bsd_partitions();
+}
+
+/*
+ * any additional partition validation
+ */
+int
+md_check_partitions(void)
+{
+
+	/*
+	 * Make sure that a boot partition (old 4.3BSD UFS) is prepared
+	 * properly for our native bootloader.
+	 */
+	if (bsdlabel[PART_BOOT].pi_fstype != FS_BSDFFS ||
+	    (bsdlabel[PART_BOOT].pi_flags & PIF_NEWFS) == 0) {
+		msg_display(MSG_nobootpartdisklabel);
+		process_menu(MENU_ok, NULL);
+		return 0;
+	}
+	return 1;
+}
+
+/*
+ * hook called before writing new disklabel.
+ */
+int
+md_pre_disklabel(void)
+{
+
+	return 0;
+}
+
+/*
+ * hook called after writing disklabel to new target disk.
+ */
+int
+md_post_disklabel(void)
+{
+
+	if (get_ramsize() <= 32)
+		set_swap(diskdev, bsdlabel);
+
+	return 0;
+}
+
+static int
+copy_bootloader(void)
+{
+	const char *mntdir = "/mnt2";
+
+	msg_display(MSG_copybootloader, diskdev);
+	if (!run_program(RUN_SILENT | RUN_ERROR_OK,
+	    "mount /dev/%s%c %s", diskdev, 'a' + PART_BOOT, mntdir)) {
+		mnt2_mounted = 1;
+		run_program(0, "/bin/cp /usr/mdec/boot %s", mntdir);
+		run_program(RUN_SILENT | RUN_ERROR_OK, "umount %s", mntdir);
+		mnt2_mounted = 0;
+	} else {
+		/* XXX print proper error message */
+		return 1;
+	}
+	return 0;
+}
+
+/*
+ * hook called after install() has finished setting up the target disk
+ * but immediately before the user is given the ``disks are now set up''
+ * message.
+ */
+int
+md_post_newfs(void)
+{
+
+	if (run_program(RUN_DISPLAY | RUN_PROGRESS,
+	    "/sbin/newfs -V2 -O 0 -b %d -f %d /dev/r%s%c",
+	    PART_BOOT_BSIZE, PART_BOOT_FSIZE, diskdev, 'a' + PART_BOOT))
+		return 1;
+	return copy_bootloader();
+}
+
+int
+md_post_extract(void)
+{
+
+	return 0;
+}
+
+void
+md_cleanup_install(void)
+{
+
+#ifndef DEBUG
+	enable_rc_conf();
+#endif
+	msg_display(MSG_howtoboot);
+	process_menu(MENU_ok, NULL);
+}
+
+int
+md_pre_update(void)
+{
+
+	if (get_ramsize() <= 32)
+		set_swap(diskdev, bsdlabel);
+
+	return 1;
+}
+
+/* Upgrade support */
+int
+md_update(void)
+{
+	const char *mntdir = "/mnt2";
+	char bootpath[MAXPATHLEN];
+	struct stat sb;
+	bool hasboot = false;
+
+	/*
+	 * Check if there is a boot UFS parttion and it has the old bootloader.
+	 * We'll update bootloader only if the old one was installed.
+	 */
+	if (!run_program(RUN_SILENT | RUN_ERROR_OK,
+	    "mount -r /dev/%s%c %s", diskdev, 'a' + PART_BOOT, mntdir)) {
+		mnt2_mounted = 1;
+		snprintf(bootpath, sizeof(bootpath), "%s/%s", mntdir, "boot");
+		if (stat(bootpath, &sb) == 0 && S_ISREG(sb.st_mode))
+			hasboot = true;
+		run_program(RUN_SILENT | RUN_ERROR_OK, "umount %s", mntdir);
+		mnt2_mounted = 0;
+		if (hasboot)
+			(void)copy_bootloader();
+	}
+	return 1;
+}
+
+int
+md_pre_mount()
+{
+	return 0;
+}
