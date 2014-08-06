10887a23c4cbb87bde5c01a859ed87264f058bb9
joerg <joerg>
2014-08-06 21:57:48 +0000

    Consistently define WARN in a way that passes format string checks, i.e. always uses the same number of arguments as given in the format string.

--- sys/arch/acorn32/include/loadfile_machdep.h
+++ sys/arch/acorn32/include/loadfile_machdep.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: loadfile_machdep.h,v 1.5 2009/03/14 14:45:51 dsl Exp $	*/
+/*	$NetBSD: loadfile_machdep.h,v 1.6 2014/08/06 21:57:48 joerg Exp $	*/
 
 /*-
  * Copyright (c) 1998, 1999 The NetBSD Foundation, Inc.
@@ -44,9 +44,14 @@
 #define READ(f, b, c)		boot32_read((f), (void *)LOADADDR(b), (c))
 #define	BCOPY(s, d, c)		boot32_memcpy((void *)LOADADDR(d), (s), (c))
 #define	BZERO(d, c)		boot32_memset((void *)LOADADDR(d), 0, (c))
-#define	WARN(a)			(void)(printf a, \
-				    printf((errno ? ": %s\n" : "\n"), \
-				    strerror(errno)))
+#define	WARN(a)			do { \
+					(void)printf a; \
+					if (errno) \
+						(void)printf(": %s\n", \
+						             strerror(errno)); \
+					else \
+						(void)printf("\n"); \
+				} while(/* CONSTCOND */0)
 #define	PROGRESS(a)		(void) printf a
 #define	ALLOC(a)		alloc(a)
 #define	DEALLOC(a, b)		dealloc(a, b)
