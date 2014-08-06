fcd768afa5f789259063d75ae9481dabf11e9cc1
riastradh <riastradh@NetBSD.org>
2014-08-06 12:36:23 +0000

    Reimport Nouveau from Linux 3.15.
    This renames .c files to avoid basename clashes, and adds RCS IDs.
    
    Generated using nouveau2netbsd.

--- sys/external/bsd/drm2/dist/drm/nouveau/nouveau_connector.c
+++ sys/external/bsd/drm2/dist/drm/nouveau/nouveau_connector.c
@@ -1,3 +1,5 @@
+/*	$NetBSD: nouveau_connector.c,v 1.1.1.2 2014/08/06 12:36:23 riastradh Exp $	*/
+
 /*
  * Copyright (C) 2008 Maarten Maathuis.
  * All Rights Reserved.
@@ -23,6 +25,9 @@
  * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  *
  */
+
+#include <sys/cdefs.h>
+__KERNEL_RCSID(0, "$NetBSD: nouveau_connector.c,v 1.1.1.2 2014/08/06 12:36:23 riastradh Exp $");
 
 #include <acpi/button.h>
 
