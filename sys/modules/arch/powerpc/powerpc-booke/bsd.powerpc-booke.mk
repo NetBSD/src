98cc645a9c78860e20036498ea03f6e54bbc3f78
joerg <joerg@NetBSD.org>
2014-08-08 16:56:30 +0000

    Don't set -mmultiple or -mno-string for PowerPC, the difference is too small to really matter and it sometimes even increases the size.

--- sys/modules/arch/powerpc/powerpc-booke/bsd.powerpc-booke.mk
+++ sys/modules/arch/powerpc/powerpc-booke/bsd.powerpc-booke.mk
@@ -1,4 +1,4 @@
-#	$NetBSD: bsd.powerpc-booke.mk,v 1.1 2011/06/15 09:45:59 mrg Exp $
+#	$NetBSD: bsd.powerpc-booke.mk,v 1.2 2014/08/08 16:56:32 joerg Exp $
 
 .ifndef _BSD_POWERPC_BOOKE_MK_
 _BSD_POWERPC_BOOKE_MK_=1
@@ -7,7 +7,7 @@ KMODULEARCHDIR:=	powerpc-booke
 
 # gcc emits bad code with these options
 #CPPFLAGS+=	-mcpu=8548
-CPPFLAGS+=	-mmultiple -mno-string -misel -Wa,-me500
+CPPFLAGS+=	-misel -Wa,-me500
 PPC_BOOKE=	1
 
 # hack into bsd.kmodule.mk
