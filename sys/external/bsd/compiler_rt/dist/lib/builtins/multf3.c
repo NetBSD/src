4da36ccb6a9a21a54fdd2d1ab2fd783deafd9d5b
joerg <joerg@NetBSD.org>
2014-08-10 10:14:03 +0000

    Import compiler-rt r215309. Extends 128bit IEEE support in soft-float, uses Thumb mode by default on ARM when available and fixes a bug in the division code for ARMs with hardware integer division.

--- sys/external/bsd/compiler_rt/dist/lib/builtins/multf3.c
+++ sys/external/bsd/compiler_rt/dist/lib/builtins/multf3.c
@@ -1,4 +1,4 @@
-//===-- lib/muldf3.c - Double-precision multiplication ------------*- C -*-===//
+//===-- lib/multf3.c - Quad-precision multiplication --------------*- C -*-===//
 //
 //                     The LLVM Compiler Infrastructure
 //
@@ -7,16 +7,19 @@
 //
 //===----------------------------------------------------------------------===//
 //
-// This file implements double-precision soft-float multiplication
+// This file implements quad-precision soft-float multiplication
 // with the IEEE-754 default rounding (to nearest, ties to even).
 //
 //===----------------------------------------------------------------------===//
 
-#define DOUBLE_PRECISION
+#define QUAD_PRECISION
+#include "fp_lib.h"
+
+#if defined(CRT_HAS_128BIT) && defined(CRT_LDBL_128BIT)
 #include "fp_mul_impl.inc"
 
-ARM_EABI_FNALIAS(dmul, muldf3)
-
-COMPILER_RT_ABI fp_t __muldf3(fp_t a, fp_t b) {
+COMPILER_RT_ABI fp_t __multf3(fp_t a, fp_t b) {
     return __mulXf3__(a, b);
 }
+
+#endif
