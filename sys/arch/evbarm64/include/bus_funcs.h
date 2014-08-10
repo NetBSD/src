101235c66a9ea974f356dbc5a706b89e774c41e9
matt <matt>
2014-08-10 05:47:35 +0000

    Preliminary files for AARCH64 (64-bit ARM) support. Enough for a distribution build.

---
+++ sys/arch/evbarm64/include/bus_funcs.h
@@ -0,0 +1,3 @@
+/* $NetBSD: bus_funcs.h,v 1.1 2014/08/10 05:47:38 matt Exp $ */
+
+#include <aarch64/bus_funcs.h>
---
+++ sys/arch/evbarm64/include/signal.h
@@ -0,0 +1,3 @@
+/* $NetBSD: signal.h,v 1.1 2014/08/10 05:47:38 matt Exp $ */
+
+#include <aarch64/signal.h>
