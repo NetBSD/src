ad1b33612dba2ad0269a15b1ad0b93707076f88c
joerg <joerg@NetBSD.org>
2014-08-10 17:46:54 +0000

    Put GCC/GAS options under ACTIVE_CC == "gcc".

--- sys/modules/arch/powerpc/powerpc-booke/bsd.powerpc-booke.mk
+++ sys/modules/arch/powerpc/powerpc-booke/bsd.powerpc-booke.mk
@@ -1,4 +1,4 @@
-#	$NetBSD: bsd.powerpc-booke.mk,v 1.2 2014/08/08 16:56:32 joerg Exp $
+#	$NetBSD: bsd.powerpc-booke.mk,v 1.3 2014/08/10 17:46:54 joerg Exp $
 
 .ifndef _BSD_POWERPC_BOOKE_MK_
 _BSD_POWERPC_BOOKE_MK_=1
@@ -7,13 +7,13 @@ KMODULEARCHDIR:=	powerpc-booke
 
 # gcc emits bad code with these options
 #CPPFLAGS+=	-mcpu=8548
-CPPFLAGS+=	-misel -Wa,-me500
+CPPFLAGS+=	${${ACTIVE_CC} == "gcc":? -misel -Wa,-me500 :}
 PPC_BOOKE=	1
 
 # hack into bsd.kmodule.mk
 PPC_INTR_IMPL=\"powerpc/booke/intr.h\"
 PPC_PCI_MACHDEP_IMPL=\"powerpc/pci_machdep.h\"
 
-AFLAGS+=	-Wa,-me500
+AFLAGS+=	${${ACTIVE_CC} == "gcc":? -Wa,-me500 :}
 
 .endif # _BSD_POWERPC_BOOKE_MK_
