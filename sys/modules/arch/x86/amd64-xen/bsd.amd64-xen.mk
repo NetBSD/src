1f6643996028c012154edbe28a3e622e994f8514
jnemeth <jnemeth@NetBSD.org>
2014-08-11 03:43:25 +0000

    Add the infrastructure for MODULAR support for Xen kernels.  At the moment, this can only load very simple modules due to missing symbols.  It is being add at this time to make pullups to the netbsd-7 branch easier.  It is not enabled by default in any kernels.

---
+++ sys/modules/arch/x86/amd64-xen/bsd.amd64-xen.mk
@@ -0,0 +1,10 @@
+#	$NetBSD: bsd.amd64-xen.mk,v 1.1 2014/08/11 03:43:25 jnemeth Exp $
+
+.ifndef _BSD_AMD64_XEN_MK_
+_BSD_AMD64_XEN_MK_=1
+
+KMODULEARCHDIR:=	amd64-xen
+
+XEN=	1
+
+.endif # _BSD_AMD64_XEN_MK_
