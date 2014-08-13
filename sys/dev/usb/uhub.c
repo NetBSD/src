96f1adec09fd2e5005babcd4f7877765dc915f71
skrll <skrll@NetBSD.org>
2014-08-13 06:26:32 +0000

    SS device detection needs more work. #if 0 previous attempt for now.
    PR/49106: panic: ehci_open: bad device speed 4

--- sys/dev/usb/uhub.c
+++ sys/dev/usb/uhub.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: uhub.c,v 1.125 2014/08/12 13:36:40 skrll Exp $	*/
+/*	$NetBSD: uhub.c,v 1.126 2014/08/13 06:26:32 skrll Exp $	*/
 /*	$FreeBSD: src/sys/dev/usb/uhub.c,v 1.18 1999/11/17 22:33:43 n_hibma Exp $	*/
 
 /*
@@ -36,7 +36,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: uhub.c,v 1.125 2014/08/12 13:36:40 skrll Exp $");
+__KERNEL_RCSID(0, "$NetBSD: uhub.c,v 1.126 2014/08/13 06:26:32 skrll Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -540,9 +540,12 @@ uhub_explore(usbd_device_handle dev)
 		}
 
 		/* Figure out device speed */
+#if 0
 		if (status & UPS_SUPER_SPEED)
 			speed = USB_SPEED_SUPER;
-		else if (status & UPS_HIGH_SPEED)
+		else
+#endif
+		if (status & UPS_HIGH_SPEED)
 			speed = USB_SPEED_HIGH;
 		else if (status & UPS_LOW_SPEED)
 			speed = USB_SPEED_LOW;
