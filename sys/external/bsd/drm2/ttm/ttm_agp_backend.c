85e67137b1e6746833c11e576cca103b85cd1c71
riastradh <riastradh@NetBSD.org>
2014-08-14 16:47:52 +0000

    Use ttm_dma_tt_init in ttm_agp_tt_create so we can use ttm_bus_dma.

--- sys/external/bsd/drm2/ttm/ttm_agp_backend.c
+++ sys/external/bsd/drm2/ttm/ttm_agp_backend.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: ttm_agp_backend.c,v 1.1 2014/07/16 20:59:58 riastradh Exp $	*/
+/*	$NetBSD: ttm_agp_backend.c,v 1.2 2014/08/14 16:47:52 riastradh Exp $	*/
 
 /*-
  * Copyright (c) 2014 The NetBSD Foundation, Inc.
@@ -30,7 +30,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: ttm_agp_backend.c,v 1.1 2014/07/16 20:59:58 riastradh Exp $");
+__KERNEL_RCSID(0, "$NetBSD: ttm_agp_backend.c,v 1.2 2014/08/14 16:47:52 riastradh Exp $");
 
 #include <sys/types.h>
 #include <sys/kmem.h>
@@ -61,7 +61,7 @@ ttm_agp_tt_create(struct ttm_bo_device *bdev, struct agp_bridge_data *bridge,
 	ttm_agp->agp = &bridge->abd_sc;
 	ttm_agp->ttm_dma.ttm.func = &ttm_agp_backend_func;
 
-	if (ttm_tt_init(&ttm_agp->ttm_dma.ttm, bdev, size, page_flags,
+	if (ttm_dma_tt_init(&ttm_agp->ttm_dma, bdev, size, page_flags,
 		dummy_read_page) != 0)
 		goto fail;
 
