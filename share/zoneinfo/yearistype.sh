09568cf80d5cbdb21cc2b11f1a8bc047dfd2349c
riz <riz>
2014-08-15 22:54:35 +0000

    Apply patch (requested by apb in ticket #1917): distrib/sets/lists/base/mi				patch doc/3RDPARTY						patch share/zoneinfo/Makefile					patch share/zoneinfo/africa					patch share/zoneinfo/antarctica				patch share/zoneinfo/asia					patch share/zoneinfo/australasia				patch share/zoneinfo/backward					patch share/zoneinfo/etcetera					patch share/zoneinfo/europe					patch share/zoneinfo/factory					patch share/zoneinfo/iso3166.tab				patch share/zoneinfo/leap-seconds.list			patch share/zoneinfo/northamerica				patch share/zoneinfo/pacificnew				patch share/zoneinfo/southamerica				patch share/zoneinfo/systemv					patch share/zoneinfo/yearistype.sh				patch share/zoneinfo/zone.tab					patch share/zoneinfo/zone1970.tab				patch
    	Update timezone database from tzdata2014c to tzdata2014f.
    	This adds two new timezones (Asia/Chita and Asia/Srednekolymsk),
    	updates many other timezones, and adds two new
    	data files in the /usr/share/zoneinfo directory (leapseconds
    	and zone1970.dat).
    	[apb, ticket #1917]

--- share/zoneinfo/yearistype.sh
+++ share/zoneinfo/yearistype.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 #
-#	$NetBSD: yearistype.sh,v 1.6.24.1 2012/09/15 09:26:44 bouyer Exp $
+#	$NetBSD: yearistype.sh,v 1.6.24.2 2014/08/15 22:54:36 riz Exp $
 #
 
 : 'This file is in the public domain, so clarified as of'
@@ -8,7 +8,7 @@
 
 case $#-$1 in
 	2-|2-0*|2-*[!0-9]*)
-		echo "$0: wild year - $1" >&2
+		echo "$0: wild year: $1" >&2
 		exit 1 ;;
 esac
 
@@ -34,7 +34,7 @@ case $#-$2 in
 			*)				exit 1 ;;
 		esac ;;
 	2-*)
-		echo "$0: wild type - $2" >&2 ;;
+		echo "$0: wild type: $2" >&2 ;;
 esac
 
 echo "$0: usage is $0 year even|odd|uspres|nonpres|nonuspres" >&2
