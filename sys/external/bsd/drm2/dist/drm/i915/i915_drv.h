a4f89ee965930d022c3381918c9a1b2f6f08113b
riastradh <riastradh@NetBSD.org>
2014-08-18 16:55:34 +0000

    Restore Intel opregion stuff.

--- sys/external/bsd/drm2/dist/drm/i915/i915_drv.h
+++ sys/external/bsd/drm2/dist/drm/i915/i915_drv.h
@@ -2720,8 +2720,8 @@ extern void intel_i2c_reset(struct drm_device *dev);
 
 /* intel_opregion.c */
 struct intel_encoder;
-#ifdef CONFIG_ACPI
 extern int intel_opregion_setup(struct drm_device *dev);
+#ifdef CONFIG_ACPI
 extern void intel_opregion_init(struct drm_device *dev);
 extern void intel_opregion_fini(struct drm_device *dev);
 extern void intel_opregion_asle_intr(struct drm_device *dev);
@@ -2730,7 +2730,6 @@ extern int intel_opregion_notify_encoder(struct intel_encoder *intel_encoder,
 extern int intel_opregion_notify_adapter(struct drm_device *dev,
 					 pci_power_t state);
 #else
-static inline int intel_opregion_setup(struct drm_device *dev) { return 0; }
 static inline void intel_opregion_init(struct drm_device *dev) { return; }
 static inline void intel_opregion_fini(struct drm_device *dev) { return; }
 static inline void intel_opregion_asle_intr(struct drm_device *dev) { return; }
