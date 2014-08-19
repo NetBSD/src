e8e41d019a3c95e90a746a8a47fdb7d94354819a
tsutsui <tsutsui@NetBSD.org>
2014-08-19 14:43:41 +0000

    Fix panic() on opening fd(4), caused by a wrong pointer passed to memset().
    I'm not sure why this 18 year old bug didn't cause problem before
    (at least my old 5.99.23 kernel worked), but probably it's triggered
    by new gcc 4.8 which might do more aggressive memory allocation.
    The problem is found by Nobuyoshi Sato on trying eject(1) against fd(4).
    
    Should be pulled up to netbsd-7.

--- sys/arch/sparc/dev/fd.c
+++ sys/arch/sparc/dev/fd.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: fd.c,v 1.154 2014/07/25 08:10:34 dholland Exp $	*/
+/*	$NetBSD: fd.c,v 1.155 2014/08/19 14:43:41 tsutsui Exp $	*/
 
 /*-
  * Copyright (c) 2000 The NetBSD Foundation, Inc.
@@ -101,7 +101,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: fd.c,v 1.154 2014/07/25 08:10:34 dholland Exp $");
+__KERNEL_RCSID(0, "$NetBSD: fd.c,v 1.155 2014/08/19 14:43:41 tsutsui Exp $");
 
 #include "opt_ddb.h"
 #include "opt_md.h"
@@ -2244,7 +2244,7 @@ fdgetdisklabel(dev_t dev)
 	struct cpu_disklabel *clp = fd->sc_dk.dk_cpulabel;
 
 	memset(lp, 0, sizeof(struct disklabel));
-	memset(lp, 0, sizeof(struct cpu_disklabel));
+	memset(clp, 0, sizeof(struct cpu_disklabel));
 
 	lp->d_type = DTYPE_FLOPPY;
 	lp->d_secsize = FD_BSIZE(fd);
