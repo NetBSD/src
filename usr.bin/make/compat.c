7b4e35bc06233e7c645af795eedef097c2d02e66
christos <christos@NetBSD.org>
2014-08-23 15:05:40 +0000

    PR/46096: Jarmo Jaakkola: fix many problems with dependencies (PR 49086)
    Quite extensive rewrite of the Suff module.  Some ripple effects into
    Parse and Targ modules too.
    
    Dependency searches in general were made to honor explicit rules so
    implicit and explicit sources are no longer applied on targets that
    do not invoke a transformation rule.
    
    Archive member dependency search was rewritten.  Explicit rules now
    work properly and $(.TARGET) is set correctly.  POSIX semantics for
    lib(member.o) and .s1.a rules are supported.
    
    .SUFFIXES list maintenance was rewritten so that scanning of existing
    rules works when suffixes are added and that clearing the suffix list
    removes single suffix rules too.  Transformation rule nodes are now
    mixed with regular nodes so they are available as regular targets too
    if needed (especially after the known suffixes are cleared).
    
    The .NULL target was documented in the manual page, especially to
    warn against using it when a single suffix rule would work.
    A deprecation warning was also added to the manual and make also
    warns the user if it encounters .NULL.
    
    Search for suffix rules no longer allows the explicit dependencies
    to override the selected transformation rule.  A check is made in
    the search that the transformation that would be tried does not
    already exist in the chain.  This prevents getting stuck in an infinite
    loop under specific circumstances.  Local variables are now set
    before node's children are expanded so dynamic sources work in
    multi-stage transformations.  Make_HandleUse() no longer expands
    the added children for transformation nodes, preventing triple
    expansion and allowing the Suff module to properly postpone their
    expansion until proper values are set for the local variables.
    
    Directory prefix is no longer removed from $(.PREFIX) if the target
    is found via directory search.
    
    The last rule defined is now used instead of the first one (POSIX
    requirement) in case a rule is defined multiple times.  Everything
    defined in the first instance is undone, but things added "globally"
    are honored.  To implement this, each node tracks attribute bits
    which have been set by special targets (global) instead of special
    sources (local).  They also track dependencies that were added by
    a rule with commands (local) instead of rule with no commands (global).
    
    New attribute, OP_FROM_SYS_MK is introduced.  It is set on all targets
    found in system makefiles so that they are not eligible to become
    the main target.  We cannot just set OP_NOTMAIN because it is one of
    the attributes inherited from transformation and .USE rules and would
    make any eligible target that uses a built-in inference rule ineligible.
    
    The $(.IMPSRC) local variable now works like in gmake: it is set to
    the first prerequisite for explicit rules.  For implicit rules it
    is still the implied source.
    
    The manual page is improved regarding the fixed features.  Test cases
    for the fixed problems are added.
    
    Other improvements in the Suff module include:
      - better debug messages for transformation rule search (length of
        the chain is now visualized by indentation)
      - Suff structures are created, destroyed and moved around by a set
        of maintenance functions so their reference counts are easier
        to track (this also gets rid of a lot of code duplication)
      - some unreasonably long functions were split into smaller ones
      - many local variables had their names changed to describe their
        purpose instead of their type

--- usr.bin/make/compat.c
+++ usr.bin/make/compat.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: compat.c,v 1.94 2014/01/03 00:02:01 sjg Exp $	*/
+/*	$NetBSD: compat.c,v 1.95 2014/08/23 15:05:40 christos Exp $	*/
 
 /*
  * Copyright (c) 1988, 1989, 1990 The Regents of the University of California.
@@ -70,14 +70,14 @@
  */
 
 #ifndef MAKE_NATIVE
-static char rcsid[] = "$NetBSD: compat.c,v 1.94 2014/01/03 00:02:01 sjg Exp $";
+static char rcsid[] = "$NetBSD: compat.c,v 1.95 2014/08/23 15:05:40 christos Exp $";
 #else
 #include <sys/cdefs.h>
 #ifndef lint
 #if 0
 static char sccsid[] = "@(#)compat.c	8.2 (Berkeley) 3/19/94";
 #else
-__RCSID("$NetBSD: compat.c,v 1.94 2014/01/03 00:02:01 sjg Exp $");
+__RCSID("$NetBSD: compat.c,v 1.95 2014/08/23 15:05:40 christos Exp $");
 #endif
 #endif /* not lint */
 #endif
@@ -531,12 +531,7 @@ Compat_Make(void *gnp, void *pgnp)
 	    goto cohorts;
 	}
 
-	if (Lst_Member(gn->iParents, pgn) != NULL) {
-	    char *p1;
-	    Var_Set(IMPSRC, Var_Value(TARGET, gn, &p1), pgn, 0);
-	    if (p1)
-		free(p1);
-	}
+	Make_SetImpsrcLocalVar(gn, pgn);
 
 	/*
 	 * All the children were made ok. Now cmgn->mtime contains the
@@ -635,12 +630,7 @@ Compat_Make(void *gnp, void *pgnp)
 	 */
 	pgn->flags &= ~REMAKE;
     } else {
-	if (Lst_Member(gn->iParents, pgn) != NULL) {
-	    char *p1;
-	    Var_Set(IMPSRC, Var_Value(TARGET, gn, &p1), pgn, 0);
-	    if (p1)
-		free(p1);
-	}
+	Make_SetImpsrcLocalVar(gn, pgn);
 	switch(gn->made) {
 	    case BEINGMADE:
 		Error("Graph cycles through %s", gn->name);
