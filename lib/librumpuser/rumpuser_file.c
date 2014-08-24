cafcaf7ff983d0d7a3a3fce350022b79b94cd5c0
pooka <pooka@NetBSD.org>
2014-08-24 14:37:31 +0000

    RCS Id police (because we can. or have you heard of git id police?)

--- lib/librumpuser/rumpuser_file.c
+++ lib/librumpuser/rumpuser_file.c
@@ -1,3 +1,5 @@
+/*	$NetBSD: rumpuser_file.c,v 1.2 2014/08/24 14:37:31 pooka Exp $	*/
+
 /*
  * Copyright (c) 2007-2010 Antti Kantee.  All Rights Reserved.
  *
@@ -28,7 +30,7 @@
 #include "rumpuser_port.h"
 
 #if !defined(lint)
-__RCSID("$NetBSD: rumpuser_file.c,v 1.1 2014/07/09 23:41:40 justin Exp $");
+__RCSID("$NetBSD: rumpuser_file.c,v 1.2 2014/08/24 14:37:31 pooka Exp $");
 #endif /* !lint */
 
 #include <sys/ioctl.h>
