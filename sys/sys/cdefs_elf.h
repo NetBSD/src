526509498f48a3d041fa6919f5b0346d87b6cabc
christos <christos@NetBSD.org>
2014-08-26 09:03:17 +0000

    alpha (and others) don't like .set name, expr. Use name = expr like elsewhere.

--- sys/sys/cdefs_elf.h
+++ sys/sys/cdefs_elf.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: cdefs_elf.h,v 1.45 2014/08/25 20:40:52 joerg Exp $	*/
+/*	$NetBSD: cdefs_elf.h,v 1.46 2014/08/26 09:03:17 christos Exp $	*/
 
 /*
  * Copyright (c) 1995, 1996 Carnegie-Mellon University.
@@ -110,12 +110,12 @@
 #define __ifunc(name, resolver) \
 	__asm(".globl	" _C_LABEL_STRING(#name) "\n" \
 	      ".type	" _C_LABEL_STRING(#name) ", %gnu_indirect_function\n" \
-	      ".set	" _C_LABEL_STRING(#name) ", " _C_LABEL_STRING(#resolver))
+	       _C_LABEL_STRING(#name) " = " _C_LABEL_STRING(#resolver))
 #else
 #define __ifunc(name, resolver) \
 	__asm(".globl	" _C_LABEL_STRING(#name) "\n" \
 	      ".type	" _C_LABEL_STRING(#name) ", @gnu_indirect_function\n" \
-	      ".set	" _C_LABEL_STRING(#name) ", " _C_LABEL_STRING(#resolver))
+	      _C_LABEL_STRING(#name) " = " _C_LABEL_STRING(#resolver))
 #endif
 
 #if __STDC__
