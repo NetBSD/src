ea53215bd4bbce49043d5326b68c6b5cb5fd8626
matt <matt@NetBSD.org>
2014-08-26 17:03:51 +0000

    Allow STT_GNU_IFUNC to be dynamic symbols

--- external/gpl3/binutils/dist/bfd/elf32-m68k.c
+++ external/gpl3/binutils/dist/bfd/elf32-m68k.c
@@ -3104,6 +3104,7 @@ elf_m68k_adjust_dynamic_symbol (struct bfd_link_info *info,
   /* Make sure we know what is going on here.  */
   BFD_ASSERT (dynobj != NULL
 	      && (h->needs_plt
+		  || h->type == STT_GNU_IFUNC
 		  || h->u.weakdef != NULL
 		  || (h->def_dynamic
 		      && h->ref_regular
@@ -3112,7 +3113,7 @@ elf_m68k_adjust_dynamic_symbol (struct bfd_link_info *info,
   /* If this is a function, put it in the procedure linkage table.  We
      will fill in the contents of the procedure linkage table later,
      when we know the address of the .got section.  */
-  if (h->type == STT_FUNC
+  if ((h->type == STT_FUNC || h->type == STT_GNU_IFUNC)
       || h->needs_plt)
     {
       if ((h->plt.refcount <= 0
