50420adc417bc44361b96500222f24824530c493
christos <christos@NetBSD.org>
2014-08-29 09:27:43 +0000

    undo eating the trailing backslash now that the shell has been fixed.

--- usr.bin/make/parse.c
+++ usr.bin/make/parse.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: parse.c,v 1.201 2014/08/28 14:39:13 christos Exp $	*/
+/*	$NetBSD: parse.c,v 1.202 2014/08/29 09:27:43 christos Exp $	*/
 
 /*
  * Copyright (c) 1988, 1989, 1990, 1993
@@ -69,14 +69,14 @@
  */
 
 #ifndef MAKE_NATIVE
-static char rcsid[] = "$NetBSD: parse.c,v 1.201 2014/08/28 14:39:13 christos Exp $";
+static char rcsid[] = "$NetBSD: parse.c,v 1.202 2014/08/29 09:27:43 christos Exp $";
 #else
 #include <sys/cdefs.h>
 #ifndef lint
 #if 0
 static char sccsid[] = "@(#)parse.c	8.3 (Berkeley) 3/19/94";
 #else
-__RCSID("$NetBSD: parse.c,v 1.201 2014/08/28 14:39:13 christos Exp $");
+__RCSID("$NetBSD: parse.c,v 1.202 2014/08/29 09:27:43 christos Exp $");
 #endif
 #endif /* not lint */
 #endif
@@ -2876,10 +2876,7 @@ ParseGetLine(int flags, int *length)
 		ptr++;
 	    ch = ' ';
 	} else {
-	    // Don't add a trailign backslash at theend of a command
-	    // This is what gmake does, but what does POSIX have to say?
-	    if (ptr[0] != '\n' && ptr[0] != '\0')
-		*tp++ = '\\';
+	    *tp++ = '\\';
 	    if (ptr[0] == '\t')
 		ptr++;
 	}
