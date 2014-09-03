d7c89a773fc3b949eba0ee61e155cb8ded7ae323
matt <matt@NetBSD.org>
2014-09-03 19:28:20 +0000

    or1k support <fenv.h>

--- include/fenv.h
+++ include/fenv.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: fenv.h,v 1.10 2014/08/10 05:57:30 matt Exp $	*/
+/*	$NetBSD: fenv.h,v 1.11 2014/09/03 19:28:20 matt Exp $	*/
 /*
  * Copyright (c) 2010 The NetBSD Foundation, Inc.
  * All rights reserved.
@@ -26,7 +26,7 @@
  */
 
 #if !defined(__aarch64__) && !defined(__arm__) && !defined(__i386__) \
-    && !defined(__sparc__) && !defined(__x86_64__)
+    && !defined(__or1k__) && !defined(__sparc__) && !defined(__x86_64__)
 #error	"fenv.h is currently not supported for this architecture"
 #endif
 
