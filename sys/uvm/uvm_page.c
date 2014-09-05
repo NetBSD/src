2b41da1d9e965f4bb36ae931cc52b923ec7ab9c0
matt <matt>
2014-09-05 05:36:21 +0000

    Don't use C++ try keyword as a variable name.

--- sys/uvm/uvm_page.c
+++ sys/uvm/uvm_page.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: uvm_page.c,v 1.185 2014/08/10 16:44:37 tls Exp $	*/
+/*	$NetBSD: uvm_page.c,v 1.186 2014/09/05 05:36:21 matt Exp $	*/
 
 /*
  * Copyright (c) 1997 Charles D. Cranor and Washington University.
@@ -66,7 +66,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: uvm_page.c,v 1.185 2014/08/10 16:44:37 tls Exp $");
+__KERNEL_RCSID(0, "$NetBSD: uvm_page.c,v 1.186 2014/09/05 05:36:21 matt Exp $");
 
 #include "opt_ddb.h"
 #include "opt_uvmhist.h"
@@ -886,7 +886,7 @@ static inline int
 vm_physseg_find_bsearch(struct vm_physseg *segs, int nsegs, paddr_t pframe, int *offp)
 {
 	/* binary search for it */
-	u_int	start, len, try;
+	u_int	start, len, guess;
 
 	/*
 	 * if try is too large (thus target is less than try) we reduce
@@ -902,17 +902,17 @@ vm_physseg_find_bsearch(struct vm_physseg *segs, int nsegs, paddr_t pframe, int 
 	 */
 
 	for (start = 0, len = nsegs ; len != 0 ; len = len / 2) {
-		try = start + (len / 2);	/* try in the middle */
+		guess = start + (len / 2);	/* try in the middle */
 
 		/* start past our try? */
-		if (pframe >= segs[try].start) {
+		if (pframe >= segs[guess].start) {
 			/* was try correct? */
-			if (pframe < segs[try].end) {
+			if (pframe < segs[guess].end) {
 				if (offp)
-					*offp = pframe - segs[try].start;
-				return(try);            /* got it */
+					*offp = pframe - segs[guess].start;
+				return guess;            /* got it */
 			}
-			start = try + 1;	/* next time, start here */
+			start = guess + 1;	/* next time, start here */
 			len--;			/* "adjust" */
 		} else {
 			/*
