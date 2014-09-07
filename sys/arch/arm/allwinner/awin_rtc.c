6fdaeffd6073445313685c16d65fffce105569d4
jmcneill <jmcneill@NetBSD.org>
2014-09-07 17:49:39 +0000

    a10/a20 rtc driver

---
+++ sys/arch/arm/allwinner/awin_rtc.c
@@ -0,0 +1,143 @@
+/* $NetBSD: awin_rtc.c,v 1.1 2014/09/07 17:49:39 jmcneill Exp $ */
+
+/*-
+ * Copyright (c) 2014 Jared D. McNeill <jmcneill@invisible.ca>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <sys/cdefs.h>
+__KERNEL_RCSID(0, "$NetBSD: awin_rtc.c,v 1.1 2014/09/07 17:49:39 jmcneill Exp $");
+
+#include <sys/param.h>
+#include <sys/bus.h>
+#include <sys/device.h>
+#include <sys/intr.h>
+#include <sys/systm.h>
+#include <sys/mutex.h>
+
+#include <arm/allwinner/awin_reg.h>
+#include <arm/allwinner/awin_var.h>
+
+#include <dev/clock_subr.h>
+
+struct awin_rtc_softc {
+	device_t sc_dev;
+	bus_space_tag_t sc_bst;
+	bus_space_handle_t sc_bsh;
+	struct todr_chip_handle sc_todr;
+};
+
+static int	awin_rtc_match(device_t, cfdata_t, void *);
+static void	awin_rtc_attach(device_t, device_t, void *);
+
+static int	awin_rtc_gettime(todr_chip_handle_t, struct clock_ymdhms *);
+static int	awin_rtc_settime(todr_chip_handle_t, struct clock_ymdhms *);
+
+CFATTACH_DECL_NEW(awin_rtc, sizeof(struct awin_rtc_softc),
+	awin_rtc_match, awin_rtc_attach, NULL, NULL);
+
+static int
+awin_rtc_match(device_t parent, cfdata_t cf, void *aux)
+{
+	struct awinio_attach_args * const aio = aux;
+	const struct awin_locators * const loc = &aio->aio_loc;
+
+	if (strcmp(cf->cf_name, loc->loc_name))
+		return 0;
+
+	return 1;
+}
+
+static void
+awin_rtc_attach(device_t parent, device_t self, void *aux)
+{
+	struct awin_rtc_softc *sc = device_private(self);
+	struct awinio_attach_args * const aio = aux;
+	const struct awin_locators * const loc = &aio->aio_loc;
+
+	sc->sc_dev = self;
+	sc->sc_bst = aio->aio_core_bst;
+	bus_space_subregion(sc->sc_bst, aio->aio_core_bsh,
+	    loc->loc_offset, loc->loc_size, &sc->sc_bsh);
+
+	aprint_naive("\n");
+	aprint_normal(": RTC\n");
+
+	sc->sc_todr.todr_gettime_ymdhms = awin_rtc_gettime;
+	sc->sc_todr.todr_settime_ymdhms = awin_rtc_settime;
+	sc->sc_todr.cookie = sc;
+	todr_attach(&sc->sc_todr);
+}
+
+static int
+awin_rtc_gettime(todr_chip_handle_t tch, struct clock_ymdhms *dt)
+{
+	struct awin_rtc_softc *sc = tch->cookie;
+	uint32_t yymmdd, hhmmss;
+
+	yymmdd = bus_space_read_4(sc->sc_bst, sc->sc_bsh,
+	    AWIN_RTC_YY_MM_DD_REG);
+	hhmmss = bus_space_read_4(sc->sc_bst, sc->sc_bsh,
+	    AWIN_RTC_HH_MM_SS_REG);
+
+	dt->dt_year = __SHIFTOUT(yymmdd, AWIN_RTC_YY_MM_DD_YEAR) +
+	    POSIX_BASE_YEAR;
+	dt->dt_mon = __SHIFTOUT(yymmdd, AWIN_RTC_YY_MM_DD_MONTH);
+	dt->dt_day = __SHIFTOUT(yymmdd, AWIN_RTC_YY_MM_DD_DAY);
+	dt->dt_wday = __SHIFTOUT(hhmmss, AWIN_RTC_HH_MM_SS_WK_NO);
+	dt->dt_hour = __SHIFTOUT(hhmmss, AWIN_RTC_HH_MM_SS_HOUR);
+	dt->dt_min = __SHIFTOUT(hhmmss, AWIN_RTC_HH_MM_SS_MINUTE);
+	dt->dt_sec = __SHIFTOUT(hhmmss, AWIN_RTC_HH_MM_SS_SECOND);
+
+	return 0;
+}
+
+static int
+awin_rtc_settime(todr_chip_handle_t tch, struct clock_ymdhms *dt)
+{
+	struct awin_rtc_softc *sc = tch->cookie;
+	uint32_t yymmdd, hhmmss, losc;
+
+	losc = bus_space_read_4(sc->sc_bst, sc->sc_bsh, AWIN_LOSC_CTRL_REG);
+	if (losc & AWIN_LOSC_CTRL_BUSY)
+		return EBUSY;
+
+	yymmdd = 0;
+	yymmdd |= __SHIFTIN(dt->dt_year - POSIX_BASE_YEAR,
+	    AWIN_RTC_YY_MM_DD_YEAR);
+	yymmdd |= __SHIFTIN(dt->dt_mon, AWIN_RTC_YY_MM_DD_MONTH);
+	yymmdd |= __SHIFTIN(dt->dt_day, AWIN_RTC_YY_MM_DD_DAY);
+	hhmmss = 0;
+	hhmmss |= __SHIFTIN(dt->dt_wday, AWIN_RTC_HH_MM_SS_WK_NO);
+	hhmmss |= __SHIFTIN(dt->dt_hour, AWIN_RTC_HH_MM_SS_HOUR);
+	hhmmss |= __SHIFTIN(dt->dt_min, AWIN_RTC_HH_MM_SS_MINUTE);
+	hhmmss |= __SHIFTIN(dt->dt_sec, AWIN_RTC_HH_MM_SS_SECOND);
+
+	bus_space_write_4(sc->sc_bst, sc->sc_bsh, AWIN_RTC_YY_MM_DD_REG,
+	    yymmdd);
+	bus_space_write_4(sc->sc_bst, sc->sc_bsh, AWIN_RTC_HH_MM_SS_REG,
+	    hhmmss);
+
+	return 0;
+}
