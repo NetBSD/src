7169ff625c83ba4494d377e44f6132c7bbd1c4d0
matt <matt>
2014-09-10 07:04:48 +0000

    g/u_int[0-9]*_t/ s/u_int/uint/g

--- sys/dev/ata/ata.c
+++ sys/dev/ata/ata.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: ata.c,v 1.131 2014/07/25 08:10:36 dholland Exp $	*/
+/*	$NetBSD: ata.c,v 1.132 2014/09/10 07:04:48 matt Exp $	*/
 
 /*
  * Copyright (c) 1998, 2001 Manuel Bouyer.  All rights reserved.
@@ -25,7 +25,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: ata.c,v 1.131 2014/07/25 08:10:36 dholland Exp $");
+__KERNEL_RCSID(0, "$NetBSD: ata.c,v 1.132 2014/09/10 07:04:48 matt Exp $");
 
 #include "opt_ata.h"
 
@@ -724,7 +724,7 @@ atabus_free_drives(struct ata_channel *chp)
 
 /* Get the disk's parameters */
 int
-ata_get_params(struct ata_drive_datas *drvp, u_int8_t flags,
+ata_get_params(struct ata_drive_datas *drvp, uint8_t flags,
     struct ataparams *prms)
 {
 	struct ata_command ata_c;
@@ -732,7 +732,7 @@ ata_get_params(struct ata_drive_datas *drvp, u_int8_t flags,
 	struct atac_softc *atac = chp->ch_atac;
 	char *tb;
 	int i, rv;
-	u_int16_t *p;
+	uint16_t *p;
 
 	ATADEBUG_PRINT(("%s\n", __func__), DEBUG_FUNCS);
 
@@ -806,15 +806,15 @@ ata_get_params(struct ata_drive_datas *drvp, u_int8_t flags,
 	     }
 #undef M
 	for (i = 0; i < sizeof(prms->atap_model); i += 2) {
-		p = (u_int16_t *)(prms->atap_model + i);
+		p = (uint16_t *)(prms->atap_model + i);
 		*p = bswap16(*p);
 	}
 	for (i = 0; i < sizeof(prms->atap_serial); i += 2) {
-		p = (u_int16_t *)(prms->atap_serial + i);
+		p = (uint16_t *)(prms->atap_serial + i);
 		*p = bswap16(*p);
 	}
 	for (i = 0; i < sizeof(prms->atap_revision); i += 2) {
-		p = (u_int16_t *)(prms->atap_revision + i);
+		p = (uint16_t *)(prms->atap_revision + i);
 		*p = bswap16(*p);
 	}
 
@@ -825,7 +825,7 @@ ata_get_params(struct ata_drive_datas *drvp, u_int8_t flags,
 }
 
 int
-ata_set_mode(struct ata_drive_datas *drvp, u_int8_t mode, u_int8_t flags)
+ata_set_mode(struct ata_drive_datas *drvp, uint8_t mode, uint8_t flags)
 {
 	struct ata_command ata_c;
 	struct ata_channel *chp = drvp->chnl_softc;
