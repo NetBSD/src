943a53de0e01538a5468a8cef611d0424c8cd7e0
riastradh <riastradh@NetBSD.org>
2014-09-17 15:46:57 +0000

    Fix header guard name.

--- sys/external/bsd/drm2/include/asm/processor.h
+++ sys/external/bsd/drm2/include/asm/processor.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: processor.h,v 1.2 2014/03/18 18:20:42 riastradh Exp $	*/
+/*	$NetBSD: processor.h,v 1.3 2014/09/17 15:46:57 riastradh Exp $	*/
 
 /*-
  * Copyright (c) 2013 The NetBSD Foundation, Inc.
@@ -30,7 +30,7 @@
  */
 
 #ifndef _ASM_PROCESSOR_H_
-#define _ASM_CURRENT_H_
+#define _ASM_PROCESSOR_H_
 
 #include <machine/param.h>
 
