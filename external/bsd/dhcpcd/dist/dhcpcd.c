fe63fba1440f3506cf7e9022c04a962a8aa69b3b
roy <roy@NetBSD.org>
2014-09-18 20:46:30 +0000

    Sync

--- external/bsd/dhcpcd/dist/dhcpcd.c
+++ external/bsd/dhcpcd/dist/dhcpcd.c
@@ -1,5 +1,5 @@
 #include <sys/cdefs.h>
- __RCSID("$NetBSD: dhcpcd.c,v 1.8 2014/09/16 22:27:04 roy Exp $");
+ __RCSID("$NetBSD: dhcpcd.c,v 1.9 2014/09/18 20:46:30 roy Exp $");
 
 /*
  * dhcpcd - DHCP client daemon
@@ -102,7 +102,7 @@ read_pid(const char *pidfile)
 	return pid;
 }
 
-static inline int
+static int
 write_pid(int fd, pid_t pid)
 {
 
@@ -381,7 +381,7 @@ configure_interface1(struct interface *ifp)
 	if (ifo->options & DHCPCD_IPV6RS) {
 		ra_global = if_checkipv6(ifp->ctx, NULL,
 		    ifp->ctx->options & DHCPCD_IPV6RA_OWN ? 1 : 0);
-		ra_iface = if_checkipv6(ifp->ctx, ifp->name,
+		ra_iface = if_checkipv6(ifp->ctx, ifp,
 		    ifp->options->options & DHCPCD_IPV6RA_OWN ? 1 : 0);
 		if (ra_global == -1 || ra_iface == -1)
 			ifo->options &= ~DHCPCD_IPV6RS;
