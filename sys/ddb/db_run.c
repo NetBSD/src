f1926b3d6fe75d06c59eb7c67a0dc491c408de67
matt <matt@NetBSD.org>
2014-09-19 17:29:01 +0000

    Add description of inst_return

--- sys/ddb/db_run.c
+++ sys/ddb/db_run.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: db_run.c,v 1.32 2013/10/19 14:33:07 martin Exp $	*/
+/*	$NetBSD: db_run.c,v 1.33 2014/09/19 17:29:01 matt Exp $	*/
 
 /*
  * Mach Operating System
@@ -34,7 +34,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: db_run.c,v 1.32 2013/10/19 14:33:07 martin Exp $");
+__KERNEL_RCSID(0, "$NetBSD: db_run.c,v 1.33 2014/09/19 17:29:01 matt Exp $");
 
 #include "opt_ddb.h"
 
@@ -344,6 +344,9 @@ db_continue_cmd(db_expr_t addr, bool have_addr,
  * bool inst_branch(int inst)
  * bool inst_call(int inst)
  *	returns true if the instruction might branch
+ *
+ * bool inst_return(int inst)
+ *	returns true is the instruction will return to its caller
  *
  * bool inst_unconditional_flow_transfer(int inst)
  *	returns true if the instruction is an unconditional
