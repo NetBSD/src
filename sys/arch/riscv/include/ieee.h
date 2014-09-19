859df22f5d4b8576f677bc79f1c14590e4848c8a
matt <matt>
2014-09-19 17:36:24 +0000

    New files for Userland support of UCB RISC-V (both 32-bit and 64-bit)

---
+++ sys/arch/riscv/include/ieee.h
@@ -0,0 +1,3 @@
+/* $NetBSD: ieee.h,v 1.1 2014/09/19 17:36:26 matt Exp $ */
+
+#include <sys/ieee754.h>
---
+++ sys/arch/riscv/include/math.h
@@ -0,0 +1,3 @@
+/* $NetBSD: math.h,v 1.1 2014/09/19 17:36:26 matt Exp $ */
+
+#define __HAVE_NANF
