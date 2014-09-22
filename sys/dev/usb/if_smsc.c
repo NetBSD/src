dbe322ddcc23fec6ad838f12d3a07e1849773085
martin <martin@NetBSD.org>
2014-09-22 11:11:10 +0000

    Pull up following revision(s) (requested by jmcneill in ticket #113): 	sys/dev/usb/if_smsc.c: revision 1.22 if the pkt length in rx header is < ETHER_HDR_LEN, drop it

--- sys/dev/usb/if_smsc.c
+++ sys/dev/usb/if_smsc.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: if_smsc.c,v 1.19 2014/08/10 16:44:36 tls Exp $	*/
+/*	$NetBSD: if_smsc.c,v 1.19.2.1 2014/09/22 11:11:10 martin Exp $	*/
 
 /*	$OpenBSD: if_smsc.c,v 1.4 2012/09/27 12:38:11 jsg Exp $	*/
 /* $FreeBSD: src/sys/dev/usb/net/if_smsc.c,v 1.1 2012/08/15 04:03:55 gonzo Exp $ */
@@ -1302,6 +1302,13 @@ smsc_rxeof(usbd_xfer_handle xfer, usbd_private_handle priv, usbd_status status)
 		pktlen = (uint16_t)SMSC_RX_STAT_FRM_LENGTH(rxhdr);
 		smsc_dbg_printf(sc, "rxeof total_len %d pktlen %d rxhdr "
 		    "0x%08x\n", total_len, pktlen, rxhdr);
+
+		if (pktlen < ETHER_HDR_LEN) {
+			smsc_dbg_printf(sc, "pktlen %d < ETHER_HDR_LEN %d\n",
+			    pktlen, ETHER_HDR_LEN);
+			ifp->if_ierrors++;
+			goto done;
+		}
 
 		pktlen += ETHER_ALIGN;
 
