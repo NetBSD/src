3ead1ba954f3b706637787b76211129f2469f005
snj <snj@NetBSD.org>
2014-09-29 18:44:42 +0000

    6.1.5_PATCH

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.408.2.12.2.11 2014/09/22 00:15:09 snj Exp $	*/
+/*	$NetBSD: param.h,v 1.408.2.12.2.12 2014/09/29 18:44:42 snj Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -63,7 +63,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	601000500	/* NetBSD 6.1.5 */
+#define	__NetBSD_Version__	601000500	/* NetBSD 6.1.5_PATCH */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
