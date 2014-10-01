c18e39ff0f05967f8f9d4696324e4a3a30842b32
jmcneill <jmcneill@NetBSD.org>
2014-10-01 00:25:43 +0000

    Don't check slot type for pre-3.0 controllers, the bits were reserved at the time. If an older controller advertises 1.8V support, just use it. This fixes high-speed mode support for me on BeagleBone Black.

--- sys/dev/sdmmc/sdhc.c
+++ sys/dev/sdmmc/sdhc.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: sdhc.c,v 1.47 2014/09/14 08:47:08 skrll Exp $	*/
+/*	$NetBSD: sdhc.c,v 1.48 2014/10/01 00:25:43 jmcneill Exp $	*/
 /*	$OpenBSD: sdhc.c,v 1.25 2009/01/13 19:44:20 grange Exp $	*/
 
 /*
@@ -23,7 +23,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: sdhc.c,v 1.47 2014/09/14 08:47:08 skrll Exp $");
+__KERNEL_RCSID(0, "$NetBSD: sdhc.c,v 1.48 2014/10/01 00:25:43 jmcneill Exp $");
 
 #ifdef _KERNEL_OPT
 #include "opt_sdmmc.h"
@@ -353,8 +353,9 @@ sdhc_host_found(struct sdhc_softc *sc, bus_space_tag_t iot,
 	/*
 	 * Determine SD bus voltage levels supported by the controller.
 	 */
-	if (ISSET(caps, SDHC_EMBEDDED_SLOT) &&
-	    ISSET(caps, SDHC_VOLTAGE_SUPP_1_8V)) {
+	if (ISSET(caps, SDHC_VOLTAGE_SUPP_1_8V) &&
+	    (hp->specver < SDHC_SPEC_VERS_300 ||
+	     ISSET(caps, SDHC_EMBEDDED_SLOT))) {
 		SET(hp->ocr, MMC_OCR_1_7V_1_8V | MMC_OCR_1_8V_1_9V);
 	}
 	if (ISSET(caps, SDHC_VOLTAGE_SUPP_3_0V)) {
