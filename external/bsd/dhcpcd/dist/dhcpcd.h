4c368d60e82568cfd1dc9a76c34804ca66894725
roy <roy@NetBSD.org>
2014-10-17 23:40:29 +0000

    Import dhcpcd-6.5.1 with the following changes:
      *  Don't work on bridge, or ptp interfaces unless explicitly told
      *  Poll for IFF_RUNNING again but avoid constantly sending IFF_UP
         (should now fix all carrier problems on BSD virtual interfaces)
      *  Don't crash when processing IPv6 route calls from the kernel
         when IPv6 resources have been disabled in dhcpcd
      *  Allow the same IP address to be shared across different interfaces
         Interface with the lowest metric gets the IP address, will move
         to the next highest if dropped (interface departs, carrier drops, etc)
      *  Delegated prefix addresses are now reported via DELEGATE6
      *  Work better with unknown delegated prefix lengths
      *  Move IPv4LL and ARP to the DHCP eloop queue to fix timing issues
      *  Add IA PD documentation update from christos

--- external/bsd/dhcpcd/dist/dhcpcd.h
+++ external/bsd/dhcpcd/dist/dhcpcd.h
@@ -1,4 +1,4 @@
-/* $NetBSD: dhcpcd.h,v 1.1.1.20 2014/09/16 22:23:21 roy Exp $ */
+/* $NetBSD: dhcpcd.h,v 1.1.1.21 2014/10/17 23:40:32 roy Exp $ */
 
 /*
  * dhcpcd - DHCP client daemon
@@ -54,6 +54,10 @@
 #define IF_DATA_DHCP6	4
 #define IF_DATA_MAX	5
 
+/* If the interface does not support carrier status (ie PPP),
+ * dhcpcd can poll it for the relevant flags periodically */
+#define IF_POLL_UP	100	/* milliseconds */
+
 struct interface {
 	struct dhcpcd_ctx *ctx;
 	TAILQ_ENTRY(interface) next;
@@ -61,6 +65,9 @@ struct interface {
 	unsigned int index;
 	unsigned int flags;
 	sa_family_t family;
+#ifdef __FreeBSD__
+	struct sockaddr_storage linkaddr;
+#endif
 	unsigned char hwaddr[HWADDR_LEN];
 	uint8_t hwlen;
 	unsigned int metric;
