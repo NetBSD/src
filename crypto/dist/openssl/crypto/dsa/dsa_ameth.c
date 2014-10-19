4c084431cb0a34a9509ca3bb5ad85a32108c16d7
snj <snj@NetBSD.org>
2014-10-19 20:04:07 +0000

    Apply patch (requested by spz in ticket #1927): Apply OpenSSL security fixes derived from the diff between OpenSSL 1.0.0n and 1.0.0o, fixing CVE-2014-3567, CVE-2014-3568, and adding POODLE mitigation via support for TLS_FALLBACK_SCSV.

--- crypto/dist/openssl/crypto/dsa/dsa_ameth.c
+++ crypto/dist/openssl/crypto/dsa/dsa_ameth.c
@@ -299,6 +299,12 @@ static int dsa_priv_encode(PKCS8_PRIV_KEY_INFO *p8, const EVP_PKEY *pkey)
 	unsigned char *dp = NULL;
 	int dplen;
 
+	if (!pkey->pkey.dsa || !pkey->pkey.dsa->priv_key)
+		{
+		DSAerr(DSA_F_DSA_PRIV_ENCODE,DSA_R_MISSING_PARAMETERS);
+		goto err;
+		}
+
 	params = ASN1_STRING_new();
 
 	if (!params)
@@ -646,4 +652,3 @@ const EVP_PKEY_ASN1_METHOD dsa_asn1_meths[] =
 		old_dsa_priv_encode
 		}
 	};
-
