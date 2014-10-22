e9759b0bee78ef7a7beed070c3a7caaadfe6ba2d
christos <christos>
2014-10-22 16:52:23 +0000

    Add libraries the statically linked address sanitizer library needs.

--- external/gpl3/gcc/dist/gcc/config/netbsd.h
+++ external/gpl3/gcc/dist/gcc/config/netbsd.h
@@ -144,6 +144,8 @@ along with GCC; see the file COPYING3.  If not see
 #undef LIB_SPEC
 #define LIB_SPEC NETBSD_LIB_SPEC
 
+#define ADD_STATIC_LIBASAN_LIBS -lstdc++ -lpthread
+
 /* Pass -cxx-isystem to cc1/cc1plus.  */
 #define NETBSD_CC1_AND_CC1PLUS_SPEC		\
   "%{cxx-isystem}"
