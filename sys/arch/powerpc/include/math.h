0959c4a297bbef90fb6240420cff6c1f696b61c1
joerg <joerg@NetBSD.org>
2014-10-22 10:32:50 +0000

    Support copysignl on PowerPC.

--- sys/arch/powerpc/include/math.h
+++ sys/arch/powerpc/include/math.h
@@ -1,3 +1,4 @@
-/*	$NetBSD: math.h,v 1.4 2002/02/19 13:08:15 simonb Exp $	*/
+/*	$NetBSD: math.h,v 1.5 2014/10/22 10:32:50 joerg Exp $	*/
 
 #define	__HAVE_NANF
+#define __HAVE_IBM_LONGDOUBLE	/* Uses double-double as long double */
