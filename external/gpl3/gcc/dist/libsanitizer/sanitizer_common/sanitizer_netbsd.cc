52b9fb4b8942e748ef597ecea8ed554cf1428c3d
christos <christos@NetBSD.org>
2014-10-23 17:50:24 +0000

    use the appropriate unwind.h header

--- external/gpl3/gcc/dist/libsanitizer/sanitizer_common/sanitizer_netbsd.cc
+++ external/gpl3/gcc/dist/libsanitizer/sanitizer_common/sanitizer_netbsd.cc
@@ -29,7 +29,6 @@
 #include <sys/time.h>
 #include <sys/types.h>
 #include <unistd.h>
-#include <unwind.h>
 #include <errno.h>
 
 namespace __sanitizer {
@@ -392,9 +391,11 @@ bool SanitizerGetThreadName(char *name, int max_len) {
 #ifndef SANITIZER_GO
 //------------------------- SlowUnwindStack -----------------------------------
 #ifdef __arm__
+#include "unwind-arm-common.h"
 #define UNWIND_STOP _URC_END_OF_STACK
 #define UNWIND_CONTINUE _URC_NO_REASON
 #else
+#include <unwind.h>
 #define UNWIND_STOP _URC_NORMAL_STOP
 #define UNWIND_CONTINUE _URC_NO_REASON
 #endif
