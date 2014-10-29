e9f75ee194516d43371a0ce481cfe001e7a6fa88
roy <roy@NetBSD.org>
2014-10-29 01:08:31 +0000

    Sync

--- external/bsd/dhcpcd/dist/if.c
+++ external/bsd/dhcpcd/dist/if.c
@@ -1,5 +1,5 @@
 #include <sys/cdefs.h>
- __RCSID("$NetBSD: if.c,v 1.3 2014/10/17 23:42:24 roy Exp $");
+ __RCSID("$NetBSD: if.c,v 1.4 2014/10/29 01:08:31 roy Exp $");
 
 /*
  * dhcpcd - DHCP client daemon
