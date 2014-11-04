7776a84d727ba4ddce4dccce38e1619c2aa890c8
martin <martin@NetBSD.org>
2014-11-04 09:23:20 +0000

    Pull up following revision(s) (requested by msaitoh in ticket #1146): 	sys/dev/mii/mii_physubr.c: revision 1.79 	sys/dev/mii/rgephy.c: revision 1.33 	sys/dev/mii/brgphy.c: revision 1.66 	sys/dev/mii/atphy.c: revision 1.15 	sys/dev/mii/mii.h: revision 1.17   The bit location of link ability is different between 1000Base-X and others (See annex 28B.2 and 28D). The old mii.h defined ANAR_X_PAUSE_* macros. Those macros were named for 1000Base-X, but the bit definitions were not for 1000Base-X but for others (e.g. 1000BaseT). So there was bug in auto negotiation for 1000Base-X, but there was no bug for other media. Define new macro named ANAR_PAUSE_* and ANLPAR_PAUSE_* for other than 1000Base-X and fix the bit definitions of ANAR_X_PAUSE_* and ANLPPAR_X_*. Change some PHY drivers to use true macro. Same as other *BSDs.

--- sys/dev/mii/mii_physubr.c
+++ sys/dev/mii/mii_physubr.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: mii_physubr.c,v 1.73 2011/12/10 02:46:07 buhrow Exp $	*/
+/*	$NetBSD: mii_physubr.c,v 1.73.2.1 2014/11/04 09:23:20 martin Exp $	*/
 
 /*-
  * Copyright (c) 1998, 1999, 2000, 2001 The NetBSD Foundation, Inc.
@@ -35,7 +35,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: mii_physubr.c,v 1.73 2011/12/10 02:46:07 buhrow Exp $");
+__KERNEL_RCSID(0, "$NetBSD: mii_physubr.c,v 1.73.2.1 2014/11/04 09:23:20 martin Exp $");
 
 #include <sys/param.h>
 #include <sys/device.h>
@@ -177,8 +177,8 @@ mii_phy_setmedia(struct mii_softc *sc)
 			/* XXX Only 1000BASE-T has PAUSE_ASYM? */
 			if ((sc->mii_flags & MIIF_HAVE_GTCR) &&
 			    (sc->mii_extcapabilities &
-			     (EXTSR_1000THDX|EXTSR_1000TFDX)))
-				anar |= ANAR_X_PAUSE_ASYM;
+			     (EXTSR_1000THDX | EXTSR_1000TFDX)))
+				anar |= ANAR_PAUSE_ASYM;
 		}
 	}
 
@@ -231,8 +231,8 @@ mii_phy_auto(struct mii_softc *sc, int waitfor)
 				/* XXX Only 1000BASE-T has PAUSE_ASYM? */
 				if ((sc->mii_flags & MIIF_HAVE_GTCR) &&
 				    (sc->mii_extcapabilities &
-				     (EXTSR_1000THDX|EXTSR_1000TFDX)))
-					anar |= ANAR_X_PAUSE_ASYM;
+				     (EXTSR_1000THDX | EXTSR_1000TFDX)))
+					anar |= ANAR_PAUSE_ASYM;
 			}
 
 			/*
@@ -630,30 +630,35 @@ mii_phy_flowstatus(struct mii_softc *sc)
 	anar = PHY_READ(sc, MII_ANAR);
 	anlpar = PHY_READ(sc, MII_ANLPAR);
 
-	if ((anar & ANAR_X_PAUSE_SYM) & (anlpar & ANLPAR_X_PAUSE_SYM))
+	/* For 1000baseX, the bits are in a different location. */
+	if (sc->mii_flags & MIIF_IS_1000X) {
+		anar <<= 3;
+		anlpar <<= 3;
+	}
+
+	if ((anar & ANAR_PAUSE_SYM) & (anlpar & ANLPAR_PAUSE_SYM))
 		return (IFM_FLOW|IFM_ETH_TXPAUSE|IFM_ETH_RXPAUSE);
 
-	if ((anar & ANAR_X_PAUSE_SYM) == 0) {
-		if ((anar & ANAR_X_PAUSE_ASYM) &&
-		    ((anlpar &
-		      ANLPAR_X_PAUSE_TOWARDS) == ANLPAR_X_PAUSE_TOWARDS))
+	if ((anar & ANAR_PAUSE_SYM) == 0) {
+		if ((anar & ANAR_PAUSE_ASYM) &&
+		    ((anlpar & ANLPAR_PAUSE_TOWARDS) == ANLPAR_PAUSE_TOWARDS))
 			return (IFM_FLOW|IFM_ETH_TXPAUSE);
 		else
 			return (0);
 	}
 
-	if ((anar & ANAR_X_PAUSE_ASYM) == 0) {
-		if (anlpar & ANLPAR_X_PAUSE_SYM)
+	if ((anar & ANAR_PAUSE_ASYM) == 0) {
+		if (anlpar & ANLPAR_PAUSE_SYM)
 			return (IFM_FLOW|IFM_ETH_TXPAUSE|IFM_ETH_RXPAUSE);
 		else
 			return (0);
 	}
 
-	switch ((anlpar & ANLPAR_X_PAUSE_TOWARDS)) {
-	case ANLPAR_X_PAUSE_NONE:
+	switch ((anlpar & ANLPAR_PAUSE_TOWARDS)) {
+	case ANLPAR_PAUSE_NONE:
 		return (0);
 
-	case ANLPAR_X_PAUSE_ASYM:
+	case ANLPAR_PAUSE_ASYM:
 		return (IFM_FLOW|IFM_ETH_RXPAUSE);
 
 	default:
