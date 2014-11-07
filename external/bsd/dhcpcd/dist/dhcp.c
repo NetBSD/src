79221b1c4dbca226d99733aed57ba6ceca00b93d
roy <roy@NetBSD.org>
2014-11-07 20:32:04 +0000

    Sync

--- external/bsd/dhcpcd/dist/dhcp.c
+++ external/bsd/dhcpcd/dist/dhcp.c
@@ -1,6 +1,3 @@
-#include <sys/cdefs.h>
- __RCSID("$NetBSD: dhcp.c,v 1.20 2014/10/29 01:08:31 roy Exp $");
-
 /*
  * dhcpcd - DHCP client daemon
  * Copyright (c) 2006-2014 Roy Marples <roy@marples.name>
