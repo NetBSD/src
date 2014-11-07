c38e639c097e6c9d2937ba418f055b25071e9129
roy <roy@NetBSD.org>
2014-11-07 20:51:02 +0000

    Add RCSIDs

--- external/bsd/dhcpcd/dist/ipv6.c
+++ external/bsd/dhcpcd/dist/ipv6.c
@@ -1,3 +1,6 @@
+#include <sys/cdefs.h>
+ __RCSID("$NetBSD: ipv6.c,v 1.6 2014/11/07 20:51:03 roy Exp $");
+
 /*
  * dhcpcd - DHCP client daemon
  * Copyright (c) 2006-2014 Roy Marples <roy@marples.name>
