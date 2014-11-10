b51ab42cd56cbbdd4ccedd7f6ce75893968d9d94
martin <martin@NetBSD.org>
2014-11-10 16:32:56 +0000

    Revision 1.7 got somehow lost in the merge from -current

--- sys/arch/arm/arm32/arm32_tlb.c
+++ sys/arch/arm/arm32/arm32_tlb.c
@@ -30,7 +30,7 @@
 #include "opt_multiprocessor.h"
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(1, "$NetBSD: arm32_tlb.c,v 1.2.6.1 2014/11/09 16:05:25 martin Exp $");
+__KERNEL_RCSID(1, "$NetBSD: arm32_tlb.c,v 1.2.6.2 2014/11/10 16:32:56 martin Exp $");
 
 #include <sys/param.h>
 #include <sys/types.h>
@@ -122,6 +122,7 @@ tlb_invalidate_addr(vaddr_t va, tlb_asid_t asid)
 #endif
 		//armreg_tlbiall_write(asid);
 	}
+	arm_dsb();
 	arm_isb();
 }
 
