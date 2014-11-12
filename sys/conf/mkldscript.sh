02219c9812b15af00e70f6a2f80fc9fefaeb38eb
christos <christos>
2014-11-12 02:15:58 +0000

    I don't know what was using this before (but it did not work since it did not produce valid LDSCRIPT code) and now it does.

--- sys/conf/mkldscript.sh
+++ sys/conf/mkldscript.sh
@@ -1,15 +1,21 @@
 #!/bin/sh
-#	$NetBSD: mkldscript.sh,v 1.1 2007/03/04 02:35:49 tsutsui Exp $
+#	$NetBSD: mkldscript.sh,v 1.2 2014/11/12 02:15:58 christos Exp $
 
-TEMPLATE=$1
+TEMPLATE="$1"
 shift
 
-SETS=`$OBJDUMP -x $* | fgrep "RELOCATION RECORDS FOR [link_set" | \
-        sort -u | sed 's/^.*\[\(.*\)\]:$/\1/'`
+mksets() {
+    "${OBJDUMP:-objdump}" -x "$@" | fgrep "RELOCATION RECORDS FOR [link_set" | \
+        sort -u | sed 's/^.*\[\(.*\)\]:$/\1/'
+}
+SETS=$(mksets "$@")
 
+
+grep -v '^}$' "$TEMPLATE"
 for s in $SETS; do
-        printf "    . = ALIGN(4);\n"
-        printf "    PROVIDE (__start_%s = .);\n" $s
-        printf "    *(%s)\n" $s
-        printf "    PROVIDE (__stop_%s = .);\n" $s
-done 
+        printf '   . = ALIGN(4);\n'
+        printf '   PROVIDE (__start_%s = .);\n' $s
+        printf '   %s : { *(%s) }\n' $s $s
+        printf '   PROVIDE (__stop_%s = .);\n' $s
+done
+printf '}\n'
