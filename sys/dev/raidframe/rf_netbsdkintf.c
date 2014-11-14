404e5f061e51545513149997472c80364cedbe22
oster <oster@NetBSD.org>
2014-11-14 14:29:16 +0000

    Fix a long-standing bug related to rebooting while a reconstruct-to-spare is underway but not yet complete.
    The issue was that a component was being marked as a used_spare when
    the rebuild started, not when the rebuild was actually finished.
    Marking it as a used_spare meant that the component label on the spare
    was being updated such that after a reboot the component would be
    considered up-to-date, regardless of whether the rebuild actually
    completed!
    
    This fix includes:
     1) Add an additional state "rf_ds_rebuilding_spare" which is used
        to denote that a spare is currently being rebuilt from the live
        components.
     2) Update the comments on the disk states, which were out-of-sync
        with reality.
     3) When rebuilding to a spare component, that spare now enters the
        state rf_ds_rebuilding_spare instead of the state rf_ds_used_spare.
     4) When the rebuild is actually complete then the spare component
        enters the rf_ds_used_spare state.  rf_ds_used_spare is now used
        exclusively for the case where the rebuilding to the spare has
        completed successfully.
    
    XXX: Someday we need to teach raidctl(8) about this new state, and
    take out the backwards compatibility code in rf_netbsdkintf.c (see
    RAIDFRAME_GET_INFO in raidioctl()).  For today, this fix needs to be
    generic enough that it can get backported without major grief.
    
    XXX: Needs pullup to netbsd-5*, netbsd-6*, and netbsd-7
    
    Fixes PR#49244.

--- sys/dev/raidframe/rf_netbsdkintf.c
+++ sys/dev/raidframe/rf_netbsdkintf.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: rf_netbsdkintf.c,v 1.315 2014/11/04 07:51:55 mlelstv Exp $	*/
+/*	$NetBSD: rf_netbsdkintf.c,v 1.316 2014/11/14 14:29:16 oster Exp $	*/
 
 /*-
  * Copyright (c) 1996, 1997, 1998, 2008-2011 The NetBSD Foundation, Inc.
@@ -101,7 +101,7 @@
  ***********************************************************/
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: rf_netbsdkintf.c,v 1.315 2014/11/04 07:51:55 mlelstv Exp $");
+__KERNEL_RCSID(0, "$NetBSD: rf_netbsdkintf.c,v 1.316 2014/11/14 14:29:16 oster Exp $");
 
 #ifdef _KERNEL_OPT
 #include "opt_compat_netbsd.h"
@@ -1532,6 +1532,10 @@ raidioctl(dev_t dev, u_long cmd, void *data, int flag, struct lwp *l)
 		}
 		for (j = d_cfg->cols, i = 0; i < d_cfg->nspares; i++, j++) {
 			d_cfg->spares[i] = raidPtr->Disks[j];
+			if (d_cfg->spares[i].status == rf_ds_rebuilding_spare) {
+				/* XXX: raidctl(8) expects to see this as a used spare */
+				d_cfg->spares[i].status = rf_ds_used_spare;
+			}
 		}
 		retcode = copyout(d_cfg, *ucfgp, sizeof(RF_DeviceConfig_t));
 		RF_Free(d_cfg, sizeof(RF_DeviceConfig_t));
