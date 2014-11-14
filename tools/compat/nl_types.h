085cf9e340c48bbe67c381a0cb72489718bd094b
martin <martin@NetBSD.org>
2014-11-14 16:24:40 +0000

    Pull up following revision(s) (requested by jmcneill in ticket #236): 	tools/compat/nl_types.h: revision 1.3 If the internals are not explicitly requested, use #include_next to pick up the system version. <locale> from libc++ requires this and is used by src/tools/elftosb2.

--- tools/compat/nl_types.h
+++ tools/compat/nl_types.h
@@ -1,5 +1,7 @@
-/*	$NetBSD: nl_types.h,v 1.2 2014/02/27 01:07:52 matt Exp $	*/
+/*	$NetBSD: nl_types.h,v 1.2.4.1 2014/11/14 16:24:40 martin Exp $	*/
 
-#if defined(_NLS_PRIVATE) || defined(__NetBSD__)
+#if defined(_NLS_PRIVATE)
 #include "../../include/nl_types.h"
+#elif defined(__GNUC__)
+#include_next <nl_types.h>
 #endif
