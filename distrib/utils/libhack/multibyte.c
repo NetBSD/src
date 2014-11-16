131d2fe128c6a96b28ab8b748a4fc971605cb523
martin <martin@NetBSD.org>
2014-11-16 10:39:54 +0000

    Pull up following revision(s) (requested by christos in ticket #242): 	distrib/utils/libhack/multibyte.c: revision 1.6 handle the "special" wc NULL case.

--- distrib/utils/libhack/multibyte.c
+++ distrib/utils/libhack/multibyte.c
@@ -1,4 +1,4 @@
-/*      $NetBSD: multibyte.c,v 1.5 2013/04/19 18:45:03 joerg Exp $      */
+/*      $NetBSD: multibyte.c,v 1.5.8.1 2014/11/16 10:39:54 martin Exp $      */
 
 /*
  * Ignore all multibyte sequences, removes all the citrus code.
@@ -12,7 +12,15 @@
 size_t
 mbrtowc(wchar_t *wc, const char *str, size_t max_sz, mbstate_t *ps)
 {
-	return str == NULL || (*wc = (unsigned char)*str) == 0 ? 0 : 1;
+	wchar_t c;
+
+	if (str == NULL)
+		return 0;
+
+	if (wc != NULL)
+		*wc = (unsigned char)*str;
+
+	return *str == '\0' ? 0 : 1;
 }
 
 size_t
