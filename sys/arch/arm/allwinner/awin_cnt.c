563f1ba8e3315b56e383841eee1f74d60f6ca2bf
skrll <skrll@NetBSD.org>
2014-11-17 08:48:59 +0000

    s/IPL_SCHED/IPL_HIGH/
    This allows binuptime to be called from any interrupt handler.

--- sys/arch/arm/allwinner/awin_cnt.c
+++ sys/arch/arm/allwinner/awin_cnt.c
@@ -1,4 +1,4 @@
-/* $NetBSD: awin_cnt.c,v 1.1 2014/11/05 15:05:20 jmcneill Exp $ */
+/* $NetBSD: awin_cnt.c,v 1.2 2014/11/17 08:48:59 skrll Exp $ */
 
 /*-
  * Copyright (c) 2014 Jared D. McNeill <jmcneill@invisible.ca>
@@ -29,7 +29,7 @@
 #include "opt_multiprocessor.h"
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: awin_cnt.c,v 1.1 2014/11/05 15:05:20 jmcneill Exp $");
+__KERNEL_RCSID(0, "$NetBSD: awin_cnt.c,v 1.2 2014/11/17 08:48:59 skrll Exp $");
 
 #include <sys/param.h>
 #include <sys/bus.h>
@@ -84,7 +84,7 @@ awin_cnt_attach(device_t parent, device_t self, void *aux)
 
 	sc->sc_dev = self;
 	sc->sc_bst = aio->aio_core_bst;
-	mutex_init(&sc->sc_lock, MUTEX_DEFAULT, IPL_SCHED);
+	mutex_init(&sc->sc_lock, MUTEX_DEFAULT, IPL_HIGH);
 	bus_space_subregion(sc->sc_bst, aio->aio_core_bsh,
 	    loc->loc_offset, loc->loc_size, &sc->sc_bsh);
 
