8650bc5eb3e53be79ee7e1a11ec04070a38ce7fd
martin <martin@NetBSD.org>
2014-11-28 09:09:19 +0000

    Pull up following revision(s) (requested by christos in ticket #270): 	usr.bin/patch/common.h: revision 1.20 	usr.bin/patch/pch.c: revision 1.26 	usr.bin/patch/pch.c: revision 1.27 Avoid coredumps when the linenumbers in the hunks turn negative. From OpenBSD. PR/49422: Ryo ONODERA: patch(1) cannot handle context diffs

--- usr.bin/patch/common.h
+++ usr.bin/patch/common.h
@@ -1,7 +1,7 @@
 /*
  * $OpenBSD: common.h,v 1.26 2006/03/11 19:41:30 otto Exp $
  * $DragonFly: src/usr.bin/patch/common.h,v 1.5 2008/08/10 23:50:12 joerg Exp $
- * $NetBSD: common.h,v 1.19 2008/09/19 18:33:34 joerg Exp $
+ * $NetBSD: common.h,v 1.19.38.1 2014/11/28 09:09:19 martin Exp $
  */
 
 /*
@@ -43,6 +43,7 @@
 #define INITHUNKMAX 125		/* initial dynamic allocation size */
 #define MAXLINELEN 8192
 #define BUFFERSIZE 1024
+#define LINENUM_MAX LONG_MAX
 
 #define SCCSPREFIX "s."
 #define GET "get -e %s"
