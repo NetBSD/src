fc56a2df1bf90cdeaec06f488dd5d3ae753c338e
snj <snj@NetBSD.org>
2014-12-01 20:03:35 +0000

    Pull up following revision(s) (requested by martin in ticket #287): 	tests/ipf/t_nat_exec.sh: revision 1.21 Remove the "broken on BE" mark from the N14 testcase - ozaki-r fixed it recently.

--- tests/ipf/t_nat_exec.sh
+++ tests/ipf/t_nat_exec.sh
@@ -1,4 +1,4 @@
-# $NetBSD: t_nat_exec.sh,v 1.20 2014/07/15 20:00:23 martin Exp $
+# $NetBSD: t_nat_exec.sh,v 1.20.2.1 2014/12/01 20:03:35 snj Exp $
 #
 # Copyright (c) 2008, 2010 The NetBSD Foundation, Inc.
 # All rights reserved.
@@ -73,7 +73,7 @@ test_case n10 nattest hex hex -T update_ipid=0
 test_case n11 nattest text text
 failing_test_case n12 nattest "Known to be broken" hex hex -T update_ipid=0 -v
 test_case n13 nattest text text
-failing_test_case_be n14 nattest "See PR kern/47665" text text
+test_case n14 nattest text text
 test_case n15 nattest text text -T update_ipid=0
 test_case n16 nattest hex hex -D
 test_case n17 nattest hex hex -D
