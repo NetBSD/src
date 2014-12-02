509e6d90a19025b05eef69e1da3ffb2c216602e8
snj <snj@NetBSD.org>
2014-12-02 22:06:58 +0000

    Pull up following revision(s) (requested by oster in ticket #1194): 	sys/dev/raidframe/raidframevar.h: revision 1.17 	sys/dev/raidframe/rf_netbsdkintf.c: revision 1.316 	sys/dev/raidframe/rf_reconstruct.c: revision 1.121 Fix a long-standing bug related to rebooting while a reconstruct-to-spare is underway but not yet complete. The issue was that a component was being marked as a used_spare when the rebuild started, not when the rebuild was actually finished. Marking it as a used_spare meant that the component label on the spare was being updated such that after a reboot the component would be considered up-to-date, regardless of whether the rebuild actually completed! This fix includes:  1) Add an additional state "rf_ds_rebuilding_spare" which is used     to denote that a spare is currently being rebuilt from the live     components.  2) Update the comments on the disk states, which were out-of-sync     with reality.  3) When rebuilding to a spare component, that spare now enters the     state rf_ds_rebuilding_spare instead of the state rf_ds_used_spare.  4) When the rebuild is actually complete then the spare component     enters the rf_ds_used_spare state.  rf_ds_used_spare is now used     exclusively for the case where the rebuilding to the spare has     completed successfully. XXX: Someday we need to teach raidctl(8) about this new state, and take out the backwards compatibility code in rf_netbsdkintf.c (see RAIDFRAME_GET_INFO in raidioctl()).  For today, this fix needs to be generic enough that it can get backported without major grief. XXX: Needs pullup to netbsd-5*, netbsd-6*, and netbsd-7 Fixes PR#49244.

--- sys/dev/raidframe/raidframevar.h
+++ sys/dev/raidframe/raidframevar.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: raidframevar.h,v 1.15 2011/02/19 07:11:09 enami Exp $ */
+/*	$NetBSD: raidframevar.h,v 1.15.16.1 2014/12/02 22:06:58 snj Exp $ */
 /*-
  * Copyright (c) 1996, 1997, 1998 The NetBSD Foundation, Inc.
  * All rights reserved.
@@ -384,17 +384,17 @@ struct RF_SparetWait_s {
  * IF YOU ADD A STATE, CHECK TO SEE IF YOU NEED TO MODIFY RF_DEAD_DISK().
  */
 enum RF_DiskStatus_e {
-        rf_ds_optimal,          /* no problems */
-        rf_ds_failed,           /* reconstruction ongoing */
-        rf_ds_reconstructing,   /* reconstruction complete to spare, dead disk
-                                 * not yet replaced */
-        rf_ds_dist_spared,      /* reconstruction complete to distributed
+	rf_ds_optimal,          /* no problems */
+	rf_ds_failed,           /* disk has failed */
+	rf_ds_reconstructing,   /* reconstruction ongoing */
+	rf_ds_dist_spared,      /* reconstruction complete to distributed
                                  * spare space, dead disk not yet replaced */
-        rf_ds_spared,           /* reconstruction complete to distributed
-                                 * spare space, dead disk not yet replaced */
-        rf_ds_spare,            /* an available spare disk */
-        rf_ds_used_spare        /* a spare which has been used, and hence is
+	rf_ds_spared,           /* reconstruction complete, dead disk not 
+				   yet replaced */
+	rf_ds_spare,            /* an available spare disk */
+	rf_ds_used_spare,       /* a spare which has been used, and hence is
                                  * not available */
+	rf_ds_rebuilding_spare	/* a spare which is being rebuilt to */
 };
 typedef enum RF_DiskStatus_e RF_DiskStatus_t;
 
