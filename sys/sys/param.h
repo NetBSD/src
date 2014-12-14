b1d9d8d5ace686e0bf6b06f7ef53ac92159b5e77
chs <chs>
2014-12-14 23:49:45 +0000

    welcome to 7.99.3.

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.462 2014/11/27 01:44:59 ozaki-r Exp $	*/
+/*	$NetBSD: param.h,v 1.463 2014/12/14 23:49:45 chs Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -63,7 +63,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	799000200	/* NetBSD 7.99.2 */
+#define	__NetBSD_Version__	799000300	/* NetBSD 7.99.3 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
