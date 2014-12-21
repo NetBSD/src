d4255b63c48fa5d24b37c36a1e52f8fa1f27fe36
hannken <hannken@NetBSD.org>
2014-12-21 10:48:53 +0000

    Change smbfs from hashlist to vcache. - Use (parent_vnode, name, name_len) as key. - Change smbfs_nget() to return a referenced but unlocked vnode and   adapt smbfs_setroot(), smbfs_create(), smbfs_mkdir() and smbfs_lookup().

--- sys/fs/smbfs/smbfs_smb.c
+++ sys/fs/smbfs/smbfs_smb.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: smbfs_smb.c,v 1.46 2014/11/15 18:52:44 nakayama Exp $	*/
+/*	$NetBSD: smbfs_smb.c,v 1.47 2014/12/21 10:48:53 hannken Exp $	*/
 
 /*-
  * Copyright (c) 2003 The NetBSD Foundation, Inc.
@@ -64,7 +64,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: smbfs_smb.c,v 1.46 2014/11/15 18:52:44 nakayama Exp $");
+__KERNEL_RCSID(0, "$NetBSD: smbfs_smb.c,v 1.47 2014/12/21 10:48:53 hannken Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -116,7 +116,7 @@ smbfs_getino(struct smbnode *dnp, const char *name, int nmlen)
 #endif
 	u_int32_t ino;
 
-	ino = dnp->n_ino + smbfs_hash(name, nmlen);
+	ino = dnp->n_ino + hash32_strn(name, nmlen, HASH32_STR_INIT);
 	if (ino <= 2)
 		ino += 3;
 	return ino;
