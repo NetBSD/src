94d121de750f600eceffbb0ab485dba87dbaf463
nonaka <nonaka@NetBSD.org>
2014-12-22 11:11:34 +0000

    pmap->pm_active and pmap->pm_onproc must be destroyed.

--- sys/uvm/pmap/pmap.c
+++ sys/uvm/pmap/pmap.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: pmap.c,v 1.5 2014/12/19 04:25:52 nonaka Exp $	*/
+/*	$NetBSD: pmap.c,v 1.6 2014/12/22 11:11:34 nonaka Exp $	*/
 
 /*-
  * Copyright (c) 1998, 2001 The NetBSD Foundation, Inc.
@@ -67,7 +67,7 @@
 
 #include <sys/cdefs.h>
 
-__KERNEL_RCSID(0, "$NetBSD: pmap.c,v 1.5 2014/12/19 04:25:52 nonaka Exp $");
+__KERNEL_RCSID(0, "$NetBSD: pmap.c,v 1.6 2014/12/22 11:11:34 nonaka Exp $");
 
 /*
  *	Manages physical address maps.
@@ -541,6 +541,11 @@ pmap_destroy(pmap_t pmap)
 	kpreempt_disable();
 	pmap_tlb_asid_release_all(pmap);
 	pmap_segtab_destroy(pmap, NULL, 0);
+
+#ifdef MULTIPROCESSOR
+	kcpuset_destroy(&pmap->pm_active);
+	kcpuset_destroy(&pmap->pm_onproc);
+#endif
 
 	pool_put(&pmap_pmap_pool, pmap);
 	kpreempt_enable();
