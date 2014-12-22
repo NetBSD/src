f11dfbdfa61e99c473241b0b312a5fe2d10d4458
msaitoh <msaitoh@NetBSD.org>
2014-12-22 04:11:38 +0000

    Pull up following revision(s) (requested by taca in ticket #1216): 	sys/dev/raidframe/rf_netbsdkintf.c: revision 1.317 Stop useless disklabel warning if there are wedges, using GPT partition. Fix PR kern/47989. XXX: Pullup 6 and 7 (maybe 5)

--- sys/dev/raidframe/rf_netbsdkintf.c
+++ sys/dev/raidframe/rf_netbsdkintf.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: rf_netbsdkintf.c,v 1.295.6.3 2014/12/02 22:05:14 snj Exp $	*/
+/*	$NetBSD: rf_netbsdkintf.c,v 1.295.6.4 2014/12/22 04:11:38 msaitoh Exp $	*/
 
 /*-
  * Copyright (c) 1996, 1997, 1998, 2008-2011 The NetBSD Foundation, Inc.
@@ -101,7 +101,7 @@
  ***********************************************************/
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: rf_netbsdkintf.c,v 1.295.6.3 2014/12/02 22:05:14 snj Exp $");
+__KERNEL_RCSID(0, "$NetBSD: rf_netbsdkintf.c,v 1.295.6.4 2014/12/22 04:11:38 msaitoh Exp $");
 
 #ifdef _KERNEL_OPT
 #include "opt_compat_netbsd.h"
@@ -735,6 +735,7 @@ raidopen(dev_t dev, int flags, int fmt,
 	pmask = (1 << part);
 
 	if ((rs->sc_flags & RAIDF_INITED) &&
+	    (rs->sc_dkdev.dk_nwedges == 0) &&
 	    (rs->sc_dkdev.dk_openmask == 0))
 		raidgetdisklabel(dev);
 
