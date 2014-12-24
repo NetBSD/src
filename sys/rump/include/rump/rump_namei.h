7071eba98e9a442d2333326f8fedabe2a6ebe763
dennis <dennis@NetBSD.org>
2014-12-24 19:56:49 +0000

    Update stats-keeping in sys/kern/vfs_cache.c to remove (most) races while allowing consistent lockless sampling of the per-cpu statistics without atomic operations.  Update comment describing the locking protocol to include this.

--- sys/rump/include/rump/rump_namei.h
+++ sys/rump/include/rump/rump_namei.h
@@ -1,11 +1,11 @@
-/*	$NetBSD: rump_namei.h,v 1.24 2014/06/03 21:16:37 joerg Exp $	*/
+/*	$NetBSD: rump_namei.h,v 1.25 2014/12/24 19:56:49 dennis Exp $	*/
 
 
 /*
  * WARNING: GENERATED FILE.  DO NOT EDIT
  * (edit namei.src and run make namei in src/sys/sys)
  *   by:   NetBSD: gennameih.awk,v 1.5 2009/12/23 14:17:19 pooka Exp 
- *   from: NetBSD: namei.src,v 1.33 2014/06/03 21:16:15 joerg Exp 
+ *   from: NetBSD: namei.src,v 1.34 2014/12/24 19:50:04 dennis Exp 
  */
 
 #ifndef _RUMP_RUMP_NAMEI_H_
