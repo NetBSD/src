6a4c315367cd26daf0136bb8bd14a85b8a09890f
jmcneill <jmcneill@NetBSD.org>
2014-12-27 01:18:48 +0000

    add DesignWare SD/MMC controller driver

---
+++ sys/dev/ic/dwc_mmc_var.h
@@ -0,0 +1,53 @@
+/* $NetBSD: dwc_mmc_var.h,v 1.1 2014/12/27 01:18:48 jmcneill Exp $ */
+
+/*-
+ * Copyright (c) 2014 Jared D. McNeill <jmcneill@invisible.ca>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#ifndef _DWC_MMC_VAR_H
+#define _DWC_MMC_VAR_H
+
+struct dwc_mmc_softc {
+	device_t		sc_dev;
+	bus_space_tag_t		sc_bst;
+	bus_space_handle_t	sc_bsh;
+	bus_dma_tag_t		sc_dmat;
+	void			*sc_ih;
+	unsigned int		sc_clock_freq;
+	unsigned int		sc_fifo_depth;
+	uint32_t		sc_flags;
+#define DWC_MMC_F_USE_HOLD_REG	0x0001
+
+	device_t		sc_sdmmc_dev;
+	kmutex_t		sc_intr_lock;
+	kcondvar_t		sc_intr_cv;
+
+	uint32_t		sc_intr_rint;
+};
+
+void	dwc_mmc_init(struct dwc_mmc_softc *);
+int	dwc_mmc_intr(void *);
+
+#endif /* !_DWC_MMC_VAR_H */
