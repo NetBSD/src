4b9bb38ee95f1542780be4a29416d5119ffbf64e
martin <martin@NetBSD.org>
2014-12-29 14:43:06 +0000

    If we launch a program with RUN_SILENT, give it more than two seconds to run before we create an output window. On a swapping VAX it can take seriously longer, and we get interactive status display even if the program did not produce any error output otherwise.

--- usr.sbin/sysinst/run.c
+++ usr.sbin/sysinst/run.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: run.c,v 1.3 2014/10/14 16:35:20 christos Exp $	*/
+/*	$NetBSD: run.c,v 1.4 2014/12/29 14:43:06 martin Exp $	*/
 
 /*
  * Copyright 1997 Piermont Information Systems Inc.
@@ -497,7 +497,7 @@ launch_subwin(WINDOW **actionwin, char **args, struct winsize *win, int flags,
 			errx(1, mmsg);
 		}
 		read_fd_set = active_fd_set;
-		tmo.tv_sec = 2;
+		tmo.tv_sec = flags & RUN_SILENT ? 20 : 2;
 		tmo.tv_usec = 0;
 		i = select(FD_SETSIZE, &read_fd_set, NULL, NULL, &tmo);
 		if (i == 0 && *actionwin == NULL)
