03cf64aad4ecc7ac140fce383ee5446ccaeb0e9c
martin <martin@NetBSD.org>
2015-01-02 11:46:53 +0000

    Use pinset-func 3 for gmac on BananaPI (value taken from the only working u-boot driver). This makes root on NFS and ftp transfers work for me.

--- sys/arch/evbarm/awin/awin_machdep.c
+++ sys/arch/evbarm/awin/awin_machdep.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: awin_machdep.c,v 1.36 2014/12/11 23:35:11 jmcneill Exp $ */
+/*	$NetBSD: awin_machdep.c,v 1.37 2015/01/02 11:46:53 martin Exp $ */
 
 /*
  * Machine dependent functions for kernel setup for TI OSK5912 board.
@@ -125,7 +125,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: awin_machdep.c,v 1.36 2014/12/11 23:35:11 jmcneill Exp $");
+__KERNEL_RCSID(0, "$NetBSD: awin_machdep.c,v 1.37 2015/01/02 11:46:53 martin Exp $");
 
 #include "opt_machdep.h"
 #include "opt_ddb.h"
@@ -817,7 +817,7 @@ awin_device_register(device_t self, void *aux)
 #if AWIN_BOARD == AWIN_bpi
 		prop_dictionary_set_cstring(dict, "phy-power", "gmacpwren");
 		prop_dictionary_set_cstring(dict, "phy-type", "rgmii-bpi");
-		prop_dictionary_set_uint8(dict, "pinset-func", 5);
+		prop_dictionary_set_uint8(dict, "pinset-func", 3);
 #endif
 		return;
 	}
