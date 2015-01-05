0406f5aa6f338926dd4e70584a82eb5b46f75f61
palle <palle@NetBSD.org>
2015-01-05 11:40:56 +0000

    cache_flush_phys(): Avoid run-time check for cpu type/implementation by installing correct function pointer in cache_setup_funcs()

--- sys/arch/sparc64/sparc64/cache.c
+++ sys/arch/sparc64/sparc64/cache.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: cache.c,v 1.9 2014/12/30 18:29:20 palle Exp $	*/
+/*	$NetBSD: cache.c,v 1.10 2015/01/05 11:40:56 palle Exp $	*/
 
 /*
  * Copyright (c) 2011 Matthew R. Green
@@ -35,7 +35,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: cache.c,v 1.9 2014/12/30 18:29:20 palle Exp $");
+__KERNEL_RCSID(0, "$NetBSD: cache.c,v 1.10 2015/01/05 11:40:56 palle Exp $");
 
 #include "opt_multiprocessor.h"
 
@@ -78,6 +78,8 @@ void    (*sp_dcache_flush_page)(paddr_t) = dcache_flush_page_us;
 void	(*sp_tlb_flush_pte)(vaddr_t, int)	= sp_tlb_flush_pte_us;
 void	(*sp_tlb_flush_all)(void)		= sp_tlb_flush_all_us;
 
+void	(*cache_flush_phys)(paddr_t, psize_t, int) = cache_flush_phys_us;
+
 static void
 sp_tlb_flush_pte_sun4v(vaddr_t va, int ctx)
 {
@@ -91,6 +93,13 @@ static void
 sp_tlb_flush_all_sun4v(void)
 {
 	panic("sp_tlb_flush_all_sun4v() not implemented yet");
+}
+
+
+static void
+cache_flush_phys_sun4v(paddr_t pa, psize_t size, int ecache)
+{
+	panic("cache_flush_phys_sun4v() not implemented yet");
 }
 
 void
@@ -122,14 +131,16 @@ cache_setup_funcs(void)
 #endif
 	}
 
-	/* Prepare sp_tlb_flush_* functions */
+	/* Prepare sp_tlb_flush_* and cache_flush_phys() functions */
 	if (CPU_ISSUN4V) {
 		sp_tlb_flush_pte = sp_tlb_flush_pte_sun4v;
 		sp_tlb_flush_all = sp_tlb_flush_all_sun4v;
+		cache_flush_phys = cache_flush_phys_sun4v; 
 	} else {
 		if (CPU_IS_USIII_UP() || CPU_IS_SPARC64_V_UP()) {
 			sp_tlb_flush_pte = sp_tlb_flush_pte_usiii;
 			sp_tlb_flush_all = sp_tlb_flush_all_usiii;
+			cache_flush_phys = cache_flush_phys_usiii;
 		}
 	}
 
