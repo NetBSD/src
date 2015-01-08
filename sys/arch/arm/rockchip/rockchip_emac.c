3e609c23b816d1bb7fd47d637bf42b22394b4bab
jmcneill <jmcneill@NetBSD.org>
2015-01-08 14:17:42 +0000

    fix a couple txq fencepost issues, from FUKAUMI Naoki <fun@naobsd.org>

--- sys/arch/arm/rockchip/rockchip_emac.c
+++ sys/arch/arm/rockchip/rockchip_emac.c
@@ -1,4 +1,4 @@
-/* $NetBSD: rockchip_emac.c,v 1.8 2015/01/08 13:37:54 jmcneill Exp $ */
+/* $NetBSD: rockchip_emac.c,v 1.9 2015/01/08 14:17:42 jmcneill Exp $ */
 
 /*-
  * Copyright (c) 2015 Jared D. McNeill <jmcneill@invisible.ca>
@@ -29,7 +29,7 @@
 #include "opt_rkemac.h"
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: rockchip_emac.c,v 1.8 2015/01/08 13:37:54 jmcneill Exp $");
+__KERNEL_RCSID(0, "$NetBSD: rockchip_emac.c,v 1.9 2015/01/08 14:17:42 jmcneill Exp $");
 
 #include <sys/param.h>
 #include <sys/bus.h>
@@ -701,7 +701,7 @@ rkemac_queue(struct rkemac_softc *sc, struct mbuf *m0)
 	const u_int nbufs = map->dm_nsegs +
 	    ((m0->m_pkthdr.len < ETHER_MIN_LEN) ? 1 : 0);
 
-	if (sc->sc_txq.t_queued + nbufs >= RKEMAC_TX_RING_COUNT - 1) {
+	if (sc->sc_txq.t_queued + nbufs >= RKEMAC_TX_RING_COUNT) {
 		bus_dmamap_unload(sc->sc_dmat, map);
 		return ENOBUFS;
 	}
@@ -758,7 +758,7 @@ rkemac_txdesc_sync(struct rkemac_softc *sc, int start, int end, int ops)
 	}
 	bus_dmamap_sync(sc->sc_dmat, sc->sc_ring_dmamap,
 	    TX_DESC_OFFSET(start),
-	    TX_DESC_OFFSET(RKEMAC_TX_RING_COUNT + 1) - TX_DESC_OFFSET(start),
+	    TX_DESC_OFFSET(RKEMAC_TX_RING_COUNT) - TX_DESC_OFFSET(start),
 	    ops);
 	bus_dmamap_sync(sc->sc_dmat, sc->sc_ring_dmamap,
 	    TX_DESC_OFFSET(0), TX_DESC_OFFSET(end) - TX_DESC_OFFSET(0), ops);
