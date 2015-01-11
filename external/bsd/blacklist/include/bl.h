e057888425a1b8534afc1663c6c4d7724ef6de8a
christos <christos@NetBSD.org>
2015-01-11 17:56:21 +0000

    add receiver.

--- external/bsd/blacklist/include/bl.h
+++ external/bsd/blacklist/include/bl.h
@@ -1,13 +1,16 @@
 
+#include <stdbool.h>
+
 typedef enum {
+	BL_INVALID,
 	BL_ADD,
-	BL_PING,
 } bl_type_t;
 
 typedef struct blacklist *bl_t;
 
-bl_t bl_create(void);
+bl_t bl_create(bool);
 void bl_destroy(bl_t);
-int bl_add(bl_t, bl_type_t, int, int, const char *ctx);
+int bl_send(bl_t, bl_type_t, int, int, const char *);
+int bl_recv(bl_t, bl_type_t *, int *, int *, char *, size_t);
 
 #define _PATH_BLACKLIST "/tmp/blacklist"
