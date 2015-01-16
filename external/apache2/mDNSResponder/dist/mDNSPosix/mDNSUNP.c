e113d729947dc5765c56259d9e3a08648025b059
christos <christos@NetBSD.org>
2015-01-16 20:16:36 +0000

    use calloc, remove cast.

--- external/apache2/mDNSResponder/dist/mDNSPosix/mDNSUNP.c
+++ external/apache2/mDNSResponder/dist/mDNSPosix/mDNSUNP.c
@@ -221,7 +221,7 @@ struct ifi_info *get_ifi_info(int family, int doaliases)
     lastlen = 0;
     len = 100 * sizeof(struct ifreq);   /* initial buffer size guess */
     for ( ; ; ) {
-        buf = (char*)malloc(len);
+        buf = calloc(len, 1);
         if (buf == NULL) {
             goto gotError;
         }
