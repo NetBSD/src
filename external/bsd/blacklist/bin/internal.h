b2e607ad71a1fc5dcc62a09c9baa688f9d2a5d91
christos <christos>
2015-01-19 18:52:55 +0000

    *** empty log message ***

--- external/bsd/blacklist/bin/internal.h
+++ external/bsd/blacklist/bin/internal.h
@@ -1,16 +1,6 @@
 
+struct conf *conf;
+size_t nconf;
+int debug;
 
-typedef struct {
-	uint32_t bl_len;
-	uint32_t bl_version;
-	uint32_t bl_type;
-	char bl_data[];
-} bl_message_t;
-
-struct blacklist {
-	int b_fd;
-	int b_connected;
-};
-
-
-#define BL_VERSION	1
+void (*lfun)(int, const char *, ...);
