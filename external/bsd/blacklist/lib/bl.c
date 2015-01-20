0fb510ecf1ff1197fe33436de0e4e343d4372fb8
christos <christos@NetBSD.org>
2015-01-20 00:52:15 +0000

    *** empty log message ***

--- external/bsd/blacklist/lib/bl.c
+++ external/bsd/blacklist/lib/bl.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: bl.c,v 1.6 2015/01/20 00:19:21 christos Exp $	*/
+/*	$NetBSD: bl.c,v 1.7 2015/01/20 00:52:15 christos Exp $	*/
 
 /*-
  * Copyright (c) 2014 The NetBSD Foundation, Inc.
@@ -29,7 +29,7 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 #include <sys/cdefs.h>
-__RCSID("$NetBSD: bl.c,v 1.6 2015/01/20 00:19:21 christos Exp $");
+__RCSID("$NetBSD: bl.c,v 1.7 2015/01/20 00:52:15 christos Exp $");
 
 #include <sys/param.h>
 #include <sys/types.h>
@@ -92,7 +92,11 @@ bl_init(bl_t b, bool srv)
 		.sun_family = AF_LOCAL,
 		.sun_len = sizeof(sun),
 	};
+	mode_t om;
+	int rv;
+
 	strlcpy(sun.sun_path, b->b_path, sizeof(sun.sun_path));
+
 	if (srv)
 		(void)unlink(b->b_path);
 
@@ -109,8 +113,11 @@ bl_init(bl_t b, bool srv)
 	if (b->b_connected)
 		return 0;
 
-	if ((srv ? bind : connect)(b->b_fd, (const void *)&sun,
-	    (socklen_t)sizeof(sun)) == -1) {
+	om = umask(0);
+	rv = (srv ? bind : connect)(b->b_fd, (const void *)&sun,
+	    (socklen_t)sizeof(sun));
+	(void)umask(om);
+	if (rv == -1) {
 		(*b->b_fun)(LOG_ERR, "%s: %s failed (%m)", __func__,
 		    srv ? "bind" : "connect");
 		goto out;
