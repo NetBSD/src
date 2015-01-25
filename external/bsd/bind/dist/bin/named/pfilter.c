49983766968c07b2ca4df0beae8111b8ff822f39
christos <christos@NetBSD.org>
2015-01-25 16:17:20 +0000

    remove debugging.

--- external/bsd/bind/dist/bin/named/pfilter.c
+++ external/bsd/bind/dist/bin/named/pfilter.c
@@ -23,7 +23,6 @@ pfilter_open(void)
 void
 pfilter_notify(isc_result_t res, ns_client_t *client, const char *msg)
 {
-	int rv;
 	isc_socket_t *socket;
 
 	pfilter_open();
@@ -32,22 +31,17 @@ pfilter_notify(isc_result_t res, ns_client_t *client, const char *msg)
 		socket = client->tcpsocket;
 	else {
 		socket = client->udpsocket;
-		if (!client->peeraddr_valid) {
-			syslog(LOG_ERR, "no peer res=%d\n", res);
+		if (!client->peeraddr_valid)
 			return;
-		}
 	}
-	if (socket == NULL) {
-		syslog(LOG_ERR, "null socket res=%d\n", res);
+
+	if (socket == NULL)
 		return;
-	}
-	if (blstate == NULL) {
-		syslog(LOG_ERR, "null blstate res=%d\n", res);
+
+	if (blstate == NULL)
 		return;
-	}
-	rv = blacklist_sa_r(blstate, 
+
+	blacklist_sa_r(blstate, 
 	    res != ISC_R_SUCCESS, isc_socket_getfd(socket),
 	    &client->peeraddr.type.sa, client->peeraddr.length, msg);
-	if (rv || res != ISC_R_SUCCESS)
-		syslog(LOG_ERR, "blacklist rv=%d res=%d\n", rv, res);
 }
