2023a0f91dd3734a74f4f20b5a7eb88a603e1c83
roy <roy@NetBSD.org>
2015-01-30 09:47:05 +0000

    Sync

--- external/bsd/dhcpcd/dist/auth.h
+++ external/bsd/dhcpcd/dist/auth.h
@@ -1,8 +1,8 @@
-/* $NetBSD: auth.h,v 1.7 2014/11/14 12:00:54 roy Exp $ */
+/* $NetBSD: auth.h,v 1.8 2015/01/30 09:47:05 roy Exp $ */
 
 /*
  * dhcpcd - DHCP client daemon
- * Copyright (c) 2006-2014 Roy Marples <roy@marples.name>
+ * Copyright (c) 2006-2015 Roy Marples <roy@marples.name>
  * All rights reserved
 
  * Redistribution and use in source and binary forms, with or without
