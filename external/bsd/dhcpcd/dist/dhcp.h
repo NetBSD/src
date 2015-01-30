5999eb2019d4a4c88d3ecebd2380ee06d2d0a57d
roy <roy@NetBSD.org>
2015-01-30 09:40:21 +0000

    Import dhcpcd-6.7.0 with the following changes:   *  Only release the DHCPv6 lease when dropping it.   *  Fix handling of ND6_IFF_OVERRIDE_RTADV on BSD.   *  Include paths.h to get _PATH_BPF. Thanks to Joerg Sonnenberger.   *  Report a better error of the kernel lacks a BPF equivalent filter.   *  Implement RFC4941, Privacy Extensions for Stateless Address      Autoconfiguration in IPv6 when dhcpcd is overriding the in-kernel      RA support. For BSD, this is a full userland implementation.   *  reject <option> will now reject any DHCP message that contains      that option.   *  Ignore RA's from ourself for very badly configured stations.

--- external/bsd/dhcpcd/dist/dhcp.h
+++ external/bsd/dhcpcd/dist/dhcp.h
@@ -1,6 +1,6 @@
 /*
  * dhcpcd - DHCP client daemon
- * Copyright (c) 2006-2014 Roy Marples <roy@marples.name>
+ * Copyright (c) 2006-2015 Roy Marples <roy@marples.name>
  * All rights reserved
 
  * Redistribution and use in source and binary forms, with or without
@@ -290,13 +290,13 @@ void dhcp_close(struct interface *);
 void dhcp_free(struct interface *);
 int dhcp_dump(struct interface *);
 #else
-#define dhcp_drop(a, b)
+#define dhcp_drop(a, b) {}
 #define dhcp_start(a) {}
-#define dhcp_reboot(a, b) b = b
-#define dhcp_reboot_newopts(a, b)
-#define dhcp_close(a)
-#define dhcp_free(a)
-#define dhcp_dump(a) -1
+#define dhcp_reboot(a, b) (b = b)
+#define dhcp_reboot_newopts(a, b) (b = b)
+#define dhcp_close(a) {}
+#define dhcp_free(a) {}
+#define dhcp_dump(a) (-1)
 #endif
 
 #endif
