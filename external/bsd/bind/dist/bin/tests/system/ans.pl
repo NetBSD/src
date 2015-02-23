ef1687fec0d59198e2cf7dc00ae401013f297adb
msaitoh <msaitoh@NetBSD.org>
2015-02-23 07:24:42 +0000

    Pull up following revision(s) (requested by spz in ticket #1259): external/bsd/bind/dist/CHANGES					patch external/bsd/bind/dist/README					patch external/bsd/bind/dist/srcid					patch external/bsd/bind/dist/version					patch external/bsd/bind/dist/bin/tests/system/ans.pl			patch external/bsd/bind/dist/doc/arm/Bv9ARM.ch04.html			patch external/bsd/bind/dist/doc/arm/Bv9ARM.ch06.html			patch external/bsd/bind/dist/doc/arm/Bv9ARM.ch07.html			patch external/bsd/bind/dist/doc/arm/Bv9ARM.ch08.html			patch external/bsd/bind/dist/doc/arm/Bv9ARM.ch09.html			patch external/bsd/bind/dist/doc/arm/Bv9ARM.html			patch external/bsd/bind/dist/doc/arm/man.arpaname.html		patch external/bsd/bind/dist/doc/arm/man.ddns-confgen.html		patch external/bsd/bind/dist/doc/arm/man.dig.html			patch external/bsd/bind/dist/doc/arm/man.dnssec-checkds.html		patch external/bsd/bind/dist/doc/arm/man.dnssec-coverage.html		patch external/bsd/bind/dist/doc/arm/man.dnssec-dsfromkey.html	patch external/bsd/bind/dist/doc/arm/man.dnssec-keyfromlabel.html	patch external/bsd/bind/dist/doc/arm/man.dnssec-keygen.html		patch external/bsd/bind/dist/doc/arm/man.dnssec-revoke.html		patch external/bsd/bind/dist/doc/arm/man.dnssec-settime.html		patch external/bsd/bind/dist/doc/arm/man.dnssec-signzone.html		patch external/bsd/bind/dist/doc/arm/man.dnssec-verify.html		patch external/bsd/bind/dist/doc/arm/man.genrandom.html		patch external/bsd/bind/dist/doc/arm/man.host.html			patch external/bsd/bind/dist/doc/arm/man.isc-hmac-fixup.html		patch external/bsd/bind/dist/doc/arm/man.named-checkconf.html		patch external/bsd/bind/dist/doc/arm/man.named-checkzone.html		patch external/bsd/bind/dist/doc/arm/man.named-journalprint.html	patch external/bsd/bind/dist/doc/arm/man.named.html			patch external/bsd/bind/dist/doc/arm/man.nsec3hash.html		patch external/bsd/bind/dist/doc/arm/man.nsupdate.html		patch external/bsd/bind/dist/doc/arm/man.rndc-confgen.html		patch external/bsd/bind/dist/doc/arm/man.rndc.conf.html		patch external/bsd/bind/dist/doc/arm/man.rndc.html			patch external/bsd/bind/dist/lib/dns/api				patch external/bsd/bind/dist/lib/dns/zone.c				patch
    	Security patch for bind from ISC (to 9.9.6-P2).
    	Only the change to lib/dns/zone.c is security relevant (CVE-2015-1349).

--- external/bsd/bind/dist/bin/tests/system/ans.pl
+++ external/bsd/bind/dist/bin/tests/system/ans.pl
@@ -327,6 +327,8 @@ sub handleTCP {
 	my $qclass = $questions[0]->qclass;
 	my $id = $request->header->id;
 
+	my $opaque;
+
 	my $packet = new Net::DNS::Packet($qname, $qtype, $qclass);
 	$packet->header->qr(1);
 	$packet->header->aa(1);
@@ -336,9 +338,11 @@ sub handleTCP {
 	my $prev_tsig;
 	my $signer;
 	my $continuation = 0;
-	while (my $rr = $request->pop("additional")) {
-		if ($rr->type eq "TSIG") {
-			$prev_tsig = $rr;
+	if ($Net::DNS::VERSION < 0.81) {
+		while (my $rr = $request->pop("additional")) {
+			if ($rr->type eq "TSIG") {
+				$prev_tsig = $rr;
+			}
 		}
 	}
 
@@ -356,7 +360,7 @@ sub handleTCP {
 			foreach $a (@{$r->{answer}}) {
 				$packet->push("answer", $a);
 			}
-			if(defined($key_name) && defined($key_data)) {
+			if (defined($key_name) && defined($key_data)) {
 				my $tsig;
 				# sign the packet
 				print "  Signing the data with " . 
@@ -365,6 +369,8 @@ sub handleTCP {
 				if ($Net::DNS::VERSION < 0.69) {
 					$tsig = Net::DNS::RR->new(
 						   "$key_name TSIG $key_data");
+				} elsif ($Net::DNS::VERSION >= 0.81 &&
+					 $continuation) {
 				} elsif ($Net::DNS::VERSION >= 0.75 &&
 					 $continuation) {
 					$tsig = $prev_tsig;
@@ -394,7 +400,7 @@ sub handleTCP {
 						$prev_tsig->mac);
 					$tsig->{"request_mac"} =
 						unpack("H*", $rmac);
-					} else {
+					} elsif ($Net::DNS::VERSION < 0.81) {
 						$tsig->request_mac(
 							 $prev_tsig->mac);
 					}
@@ -404,7 +410,13 @@ sub handleTCP {
 				$tsig->continuation($continuation) if
 					 ($Net::DNS::VERSION >= 0.71 &&
 					  $Net::DNS::VERSION <= 0.74 );
-				$packet->sign_tsig($tsig);
+				if ($Net::DNS::VERSION < 0.81) {
+					$packet->sign_tsig($tsig);
+				} elsif ($continuation) {
+					$opaque = $packet->sign_tsig($opaque);
+				} else {
+					$opaque = $packet->sign_tsig($request);
+				}
 				$signer = \&sign_tcp_continuation
 					if ($Net::DNS::VERSION < 0.70);
 				$continuation = 1;
