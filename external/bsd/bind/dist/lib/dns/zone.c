15b47ad6cff5c9f1b1bac75c7c8f3fb6670ec830
msaitoh <msaitoh@NetBSD.org>
2015-02-23 05:43:17 +0000

    Pull up following revision(s) (requested by spz in ticket #544): 	external/bsd/bind/dist/doc/arm/man.named-checkzone.html: revision 1.2 	external/bsd/bind/dist/doc/arm/man.dnssec-settime.html: revision 1.2 	external/bsd/bind/dist/doc/arm/man.dnssec-keygen.html: revision 1.2 	external/bsd/bind/dist/doc/arm/man.named-rrchecker.html: revision 1.2 	external/bsd/bind/dist/doc/arm/Bv9ARM.html: revision 1.2 	external/bsd/bind/dist/doc/arm/man.named.html: revision 1.2 	external/bsd/bind/dist/doc/arm/man.delv.html: revision 1.2 	external/bsd/bind/dist/doc/arm/man.nsupdate.html: revision 1.2 	external/bsd/bind/dist/doc/arm/man.rndc.conf.html: revision 1.2 	external/bsd/bind/dist/doc/arm/man.dnssec-dsfromkey.html: revision 1.2 	external/bsd/bind/dist/doc/arm/Bv9ARM.ch08.html: revision 1.2 	external/bsd/bind/dist/doc/arm/Bv9ARM.ch06.html: revision 1.2 	external/bsd/bind/dist/doc/arm/man.dig.html: revision 1.2 	external/bsd/bind/dist/doc/arm/man.dnssec-signzone.html: revision 1.2 	external/bsd/bind/dist/doc/arm/Bv9ARM.ch04.html: revision 1.2 	external/bsd/bind/dist/doc/arm/man.dnssec-keyfromlabel.html: revision 1.2 	external/bsd/bind/dist/lib/dns/tests/geoip_test.c: revision 1.2 	external/bsd/bind/dist/doc/arm/man.arpaname.html: revision 1.2 	external/bsd/bind/dist/srcid: revision 1.8 	external/bsd/bind/dist/doc/arm/man.rndc.html: revision 1.2 	external/bsd/bind/dist/doc/arm/man.dnssec-revoke.html: revision 1.2 	external/bsd/bind/dist/doc/arm/man.host.html: revision 1.2 	external/bsd/bind/dist/doc/arm/man.nsec3hash.html: revision 1.2 	external/bsd/bind/dist/lib/dns/api: revision 1.2 	external/bsd/bind/dist/doc/arm/man.genrandom.html: revision 1.2 	external/bsd/bind/dist/doc/arm/man.named-journalprint.html: revision 1.2 	external/bsd/bind/dist/doc/arm/man.named-checkconf.html: revision 1.2 	external/bsd/bind/dist/doc/arm/man.isc-hmac-fixup.html: revision 1.2 	external/bsd/bind/dist/doc/arm/man.dnssec-verify.html: revision 1.2 	external/bsd/bind/dist/README: revision 1.2 	external/bsd/bind/dist/doc/arm/Bv9ARM.ch09.html: revision 1.2 	external/bsd/bind/dist/doc/arm/Bv9ARM.ch07.html: revision 1.2 	external/bsd/bind/dist/version: revision 1.12 	external/bsd/bind/dist/doc/arm/man.rndc-confgen.html: revision 1.2 	external/bsd/bind/dist/CHANGES: revision 1.14 	external/bsd/bind/dist/lib/dns/zone.c: revision 1.13 	external/bsd/bind/dist/bin/tests/system/ans.pl: revision 1.2 	external/bsd/bind/dist/doc/arm/man.dnssec-importkey.html: revision 1.2 	external/bsd/bind/dist/doc/arm/man.dnssec-checkds.html: revision 1.2 	external/bsd/bind/dist/doc/arm/man.ddns-confgen.html: revision 1.2 	external/bsd/bind/dist/doc/arm/man.dnssec-coverage.html: revision 1.2 security patch for bind from ISC (to 9.10.1-P2). Only the change to lib/dns/zone.c is security relevant Upstream changelog:         --- 9.10.1-P2 released --- 4053.   [security]      Revoking a managed trust anchor and supplying                         an untrusted replacement could cause named                         to crash with an assertion failure.                         (CVE-2015-1349) [RT #38344] 4027.   [port]          Net::DNS 0.81 compatibility. [RT #38165]

--- external/bsd/bind/dist/lib/dns/zone.c
+++ external/bsd/bind/dist/lib/dns/zone.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: zone.c,v 1.11.2.1 2014/12/22 03:28:45 msaitoh Exp $	*/
+/*	$NetBSD: zone.c,v 1.11.2.2 2015/02/23 05:43:18 msaitoh Exp $	*/
 
 /*
  * Copyright (C) 2004-2014  Internet Systems Consortium, Inc. ("ISC")
@@ -8948,6 +8948,12 @@ keyfetch_done(isc_task_t *task, isc_event_t *event) {
 					     namebuf, tag);
 				trustkey = ISC_TRUE;
 			}
+		} else {
+			/*
+			 * No previously known key, and the key is not
+			 * secure, so skip it.
+			 */
+			continue;
 		}
 
 		/* Delete old version */
@@ -8996,7 +9002,7 @@ keyfetch_done(isc_task_t *task, isc_event_t *event) {
 			trust_key(zone, keyname, &dnskey, mctx);
 		}
 
-		if (!deletekey) {
+		if (secure && !deletekey) {
 			INSIST(newkey || updatekey);
 			set_refreshkeytimer(zone, &keydata, now);
 		}
