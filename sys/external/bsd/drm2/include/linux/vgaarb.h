78c37165a382eeec0dc373a4600c5879aea7c003
riastradh <riastradh@NetBSD.org>
2015-02-25 21:55:30 +0000

    <linux/vgaarb.h> needs <sys/types.h> and <sys/errno.h>.

--- sys/external/bsd/drm2/include/linux/vgaarb.h
+++ sys/external/bsd/drm2/include/linux/vgaarb.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: vgaarb.h,v 1.2 2014/03/18 18:20:43 riastradh Exp $	*/
+/*	$NetBSD: vgaarb.h,v 1.3 2015/02/25 21:55:30 riastradh Exp $	*/
 
 /*-
  * Copyright (c) 2013 The NetBSD Foundation, Inc.
@@ -31,6 +31,9 @@
 
 #ifndef _LINUX_VGAARB_H_
 #define _LINUX_VGAARB_H_
+
+#include <sys/types.h>
+#include <sys/errno.h>
 
 struct pci_dev;
 
