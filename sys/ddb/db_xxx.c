bcf8ab1eab79307abd79c07c017bd7bcfec2a9ae
ozaki-r <ozaki-r@NetBSD.org>
2015-02-27 00:47:30 +0000

    Don't use an unset value as an address
    Instead, show usage if no address is passed.

--- sys/ddb/db_xxx.c
+++ sys/ddb/db_xxx.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: db_xxx.c,v 1.70 2014/09/05 09:27:24 matt Exp $	*/
+/*	$NetBSD: db_xxx.c,v 1.71 2015/02/27 00:47:30 ozaki-r Exp $	*/
 
 /*
  * Copyright (c) 1982, 1986, 1989, 1991, 1993
@@ -37,7 +37,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: db_xxx.c,v 1.70 2014/09/05 09:27:24 matt Exp $");
+__KERNEL_RCSID(0, "$NetBSD: db_xxx.c,v 1.71 2015/02/27 00:47:30 ozaki-r Exp $");
 
 #ifdef _KERNEL_OPT
 #include "opt_kgdb.h"
@@ -160,6 +160,12 @@ db_show_files_cmd(db_expr_t addr, bool haddr,
 	struct vnode *vp;
 	bool full = false;
 	fdtab_t *dt;
+
+	if (!haddr) {
+		db_printf("usage: show files address\n");
+		db_printf("\taddress == an address of a proc structure\n");
+		return;
+	}
 
 	if (modif[0] == 'f')
 		full = true;
