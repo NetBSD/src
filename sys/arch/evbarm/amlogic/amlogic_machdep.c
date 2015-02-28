dcc0e5327b9930e4cd600d2a92beca3a313bf9e0
jmcneill <jmcneill@NetBSD.org>
2015-02-28 15:21:07 +0000

    set correct a9tmr/a9wdt frequency

--- sys/arch/evbarm/amlogic/amlogic_machdep.c
+++ sys/arch/evbarm/amlogic/amlogic_machdep.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: amlogic_machdep.c,v 1.6 2015/02/27 21:13:52 jmcneill Exp $ */
+/*	$NetBSD: amlogic_machdep.c,v 1.7 2015/02/28 15:21:07 jmcneill Exp $ */
 
 /*
  * Machine dependent functions for kernel setup for TI OSK5912 board.
@@ -125,7 +125,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: amlogic_machdep.c,v 1.6 2015/02/27 21:13:52 jmcneill Exp $");
+__KERNEL_RCSID(0, "$NetBSD: amlogic_machdep.c,v 1.7 2015/02/28 15:21:07 jmcneill Exp $");
 
 #include "opt_machdep.h"
 #include "opt_ddb.h"
@@ -558,7 +558,7 @@ amlogic_device_register(device_t self, void *aux)
 	 */
 	if (device_is_a(self, "a9tmr") || device_is_a(self, "a9wdt")) {
                 prop_dictionary_set_uint32(dict, "frequency",
-		    1600000000 / 2);
+		    amlogic_get_rate_a9periph());
 
 		return;
 	}
