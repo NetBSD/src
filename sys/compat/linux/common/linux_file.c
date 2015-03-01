32499cafbb6660cfa49572eb994905c28a839550
njoly <njoly@NetBSD.org>
2015-03-01 13:19:39 +0000

    Add Linux specific fcntl(2) commands. Support F_DUPFD_CLOEXEC.

--- sys/compat/linux/common/linux_file.c
+++ sys/compat/linux/common/linux_file.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: linux_file.c,v 1.114 2014/11/09 17:48:08 maxv Exp $	*/
+/*	$NetBSD: linux_file.c,v 1.115 2015/03/01 13:19:39 njoly Exp $	*/
 
 /*-
  * Copyright (c) 1995, 1998, 2008 The NetBSD Foundation, Inc.
@@ -35,7 +35,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: linux_file.c,v 1.114 2014/11/09 17:48:08 maxv Exp $");
+__KERNEL_RCSID(0, "$NetBSD: linux_file.c,v 1.115 2015/03/01 13:19:39 njoly Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -416,6 +416,10 @@ linux_sys_fcntl(struct lwp *l, const struct linux_sys_fcntl_args *uap, register_
 		tp->t_pgrp = pgrp;
 		mutex_exit(proc_lock);
 		return 0;
+
+	case LINUX_F_DUPFD_CLOEXEC:
+		cmd = F_DUPFD_CLOEXEC;
+		break;
 
 	default:
 		return EOPNOTSUPP;
