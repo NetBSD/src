561e5b0384a0fbd4d3f62ba511072941eae5f3ae
njoly <njoly@NetBSD.org>
2015-03-01 13:19:39 +0000

    Add Linux specific fcntl(2) commands. Support F_DUPFD_CLOEXEC.

--- sys/compat/linux/common/linux_fcntl.h
+++ sys/compat/linux/common/linux_fcntl.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: linux_fcntl.h,v 1.16 2014/06/01 13:42:12 njoly Exp $	*/
+/*	$NetBSD: linux_fcntl.h,v 1.17 2015/03/01 13:19:39 njoly Exp $	*/
 
 /*-
  * Copyright (c) 1995, 1998 The NetBSD Foundation, Inc.
@@ -86,6 +86,16 @@ struct linux_flock64 {
 #else
 #error Undefined linux_fcntl.h machine type.
 #endif
+
+/* Linux specific fcntl(2) commands */
+#define	LINUX_F_SPECIFIC_BASE 	1024
+#define	LINUX_F_SETLEASE 	(LINUX_F_SPECIFIC_BASE + 0)
+#define	LINUX_F_GETLEASE 	(LINUX_F_SPECIFIC_BASE + 1)
+#define	LINUX_F_NOTIFY 		(LINUX_F_SPECIFIC_BASE + 2)
+#define	LINUX_F_CANCELLK 	(LINUX_F_SPECIFIC_BASE + 5)
+#define	LINUX_F_DUPFD_CLOEXEC 	(LINUX_F_SPECIFIC_BASE + 6)
+#define	LINUX_F_SETPIPE_SZ 	(LINUX_F_SPECIFIC_BASE + 7)
+#define	LINUX_F_GETPIPE_SZ 	(LINUX_F_SPECIFIC_BASE + 8)
 
 /*
  * We have to have 4 copies of the code that converts linux fcntl() file
