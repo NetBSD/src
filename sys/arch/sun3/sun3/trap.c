81a7da5587128bdba46570c4c01c932dc1108d46
martin <martin@NetBSD.org>
2015-03-04 20:29:59 +0000

    Handle EINVAL in the fault path and send SIGBUS on mmap'd access past EOF

--- sys/arch/sun3/sun3/trap.c
+++ sys/arch/sun3/sun3/trap.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: trap.c,v 1.143 2013/09/07 15:56:11 tsutsui Exp $	*/
+/*	$NetBSD: trap.c,v 1.144 2015/03/04 20:30:00 martin Exp $	*/
 
 /*
  * Copyright (c) 1982, 1986, 1990, 1993
@@ -78,7 +78,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: trap.c,v 1.143 2013/09/07 15:56:11 tsutsui Exp $");
+__KERNEL_RCSID(0, "$NetBSD: trap.c,v 1.144 2015/03/04 20:30:00 martin Exp $");
 
 #include "opt_ddb.h"
 #include "opt_execfmt.h"
@@ -591,14 +591,26 @@ trap(struct trapframe *tf, int type, u_int code, u_int v)
 			goto dopanic;
 		}
 		ksi.ksi_addr = (void *)v;
-		if (rv == ENOMEM) {
+		switch (rv) {
+		case ENOMEM:
 			printf("UVM: pid %d (%s), uid %d killed: out of swap\n",
 			       p->p_pid, p->p_comm,
 			       l->l_cred ?
 			       kauth_cred_geteuid(l->l_cred) : -1);
 			ksi.ksi_signo = SIGKILL;
-		} else {
+			break;
+		case EINVAL:
+			ksi.ksi_signo = SIGBUS;
+			ksi.ksi_code = BUS_ADRERR;
+			break;
+		case EACCES:
 			ksi.ksi_signo = SIGSEGV;
+			ksi.ksi_code = SEGV_ACCERR;
+			break;
+		default:
+			ksi.ksi_signo = SIGSEGV;
+			ksi.ksi_code = SEGV_MAPERR;
+			break;
 		}
 		break;
 		} /* T_MMUFLT */
