b988e660a20e834c8ff24a1391c51bcfa92b23d5
jmcneill <jmcneill>
2015-03-07 14:16:51 +0000

    i2c devices don't actually have a dependency on "iic" (the bus driver) but on "i2cexec". Create an i2cexec module (i2c_exec.c) to mirror the dependencies in files.i2c and switch device drivers to depend on "i2cexec" module instead.
    A kernel with a USB stack and no I2C controller / bus attachment would spit
    out a bunch of "WARNING: module error: can't find builtin dependency `iic'"
    messages at boot. This change solves that problem.

--- sys/dev/i2c/hytp14.c
+++ sys/dev/i2c/hytp14.c
@@ -37,7 +37,7 @@
  */ 
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: hytp14.c,v 1.2 2014/06/29 09:52:43 kardel Exp $");
+__KERNEL_RCSID(0, "$NetBSD: hytp14.c,v 1.3 2015/03/07 14:16:51 jmcneill Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -283,7 +283,7 @@ hytp14_refresh(struct sysmon_envsys *sme, envsys_data_t *edata)
 }
 
 
-MODULE(MODULE_CLASS_DRIVER, hythygtemp, "iic");
+MODULE(MODULE_CLASS_DRIVER, hythygtemp, "i2cexec");
 
 #ifdef _MODULE
 #include "ioconf.c"
