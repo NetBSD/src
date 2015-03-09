4c7d04fd1f6825d408d5a4020037385c37ce6489
snj <snj@NetBSD.org>
2015-03-09 17:45:57 +0000

    Pull up following revision(s) (requested by nonaka in ticket #577): 	sys/dev/sdmmc/sdmmc_mem.c: revision 1.34 	sys/dev/sdmmc/sdmmcvar.h: revision 1.16 fix to simulate multi-segment dma transfer for pq3sdhc(4).

--- sys/dev/sdmmc/sdmmcvar.h
+++ sys/dev/sdmmc/sdmmcvar.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: sdmmcvar.h,v 1.15 2014/03/19 15:26:42 nonaka Exp $	*/
+/*	$NetBSD: sdmmcvar.h,v 1.15.4.1 2015/03/09 17:45:57 snj Exp $	*/
 /*	$OpenBSD: sdmmcvar.h,v 1.13 2009/01/09 10:55:22 jsg Exp $	*/
 
 /*
@@ -189,6 +189,7 @@ struct sdmmc_function {
 
 	void *bbuf;			/* bounce buffer */
 	bus_dmamap_t bbuf_dmap;		/* DMA map for bounce buffer */
+	bus_dmamap_t sseg_dmap;		/* DMA map for single segment */
 };
 
 /*
