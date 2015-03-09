415de6e83abb18179e1e0671f72d2c7e6f6b0a58
joerg <joerg@NetBSD.org>
2015-03-09 11:03:19 +0000

    Provide size_t.

--- sys/arch/arm/include/sysarch.h
+++ sys/arch/arm/include/sysarch.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: sysarch.h,v 1.11 2014/01/16 21:48:41 matt Exp $	*/
+/*	$NetBSD: sysarch.h,v 1.12 2015/03/09 11:03:19 joerg Exp $	*/
 
 /*
  * Copyright (c) 1996-1997 Mark Brinicombe.
@@ -38,11 +38,17 @@
 #include <sys/cdefs.h>
 
 /*
- * Pickup definition of uintptr_t
+ * Pickup definition of size_t and uintptr_t
  */
+#include <machine/ansi.h>
 #include <sys/stdint.h>
 #ifndef _KERNEL
 #include <stdbool.h>
+#endif
+
+#ifdef	_BSD_SIZE_T_
+typedef	_BSD_SIZE_T_ size_t;
+#undef	_BSD_SIZE_T_
 #endif
 
 /*
