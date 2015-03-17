b0e07faf0bd0c40174f52072bca68f08afeb68e1
joerg <joerg@NetBSD.org>
2015-03-17 12:20:02 +0000

    __BEGIN_DECLS and __END_DECLS only exist if sys/cdefs.h was included.

--- sys/arch/arm/include/fenv.h
+++ sys/arch/arm/include/fenv.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: fenv.h,v 1.2 2014/01/29 00:22:09 matt Exp $	*/
+/*	$NetBSD: fenv.h,v 1.3 2015/03/17 12:20:02 joerg Exp $	*/
 
 /* 
  * Based on ieeefp.h written by J.T. Conklin, Apr 28, 1995
@@ -7,6 +7,8 @@
 
 #ifndef _ARM_FENV_H_
 #define _ARM_FENV_H_
+
+#include <sys/cdefs.h>
 
 #ifdef __ARM_PCS_AAPCS64
 /* AArch64 split FPSCR into two registers FPCR and FPSR */
