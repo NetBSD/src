51a739de792575a232b303a27dae8a31fb5390e1
riastradh <riastradh@NetBSD.org>
2015-03-19 17:16:22 +0000

    Move initialization of XUIO_XUZC_PRIV out of #ifdef PORT_SOLARIS.
    This code is not currently used, so no functional change, but it may
    be used when we hook zfs into the ubc.

--- external/cddl/osnet/dist/uts/common/fs/zfs/dmu.c
+++ external/cddl/osnet/dist/uts/common/fs/zfs/dmu.c
@@ -679,14 +679,15 @@ dmu_xuio_init(xuio_t *xuio, int nblk)
 	priv->cnt = nblk;
 	priv->bufs = kmem_zalloc(nblk * sizeof (arc_buf_t *), KM_SLEEP);
 	priv->iovp = uio->uio_iov;
-#ifdef PORT_SOLARIS
 	XUIO_XUZC_PRIV(xuio) = priv;
 
+#ifdef PORT_SOLARIS
 	if (XUIO_XUZC_RW(xuio) == UIO_READ)
 		XUIOSTAT_INCR(xuiostat_onloan_rbuf, nblk);
 	else
 		XUIOSTAT_INCR(xuiostat_onloan_wbuf, nblk);
 #endif
+
 	return (0);
 }
 
@@ -699,12 +700,13 @@ dmu_xuio_fini(xuio_t *xuio)
 	kmem_free(priv->iovp, nblk * sizeof (iovec_t));
 	kmem_free(priv->bufs, nblk * sizeof (arc_buf_t *));
 	kmem_free(priv, sizeof (dmu_xuio_t));
+
 #ifdef PORT_SOLARIS
 	if (XUIO_XUZC_RW(xuio) == UIO_READ)
 		XUIOSTAT_INCR(xuiostat_onloan_rbuf, -nblk);
 	else
 		XUIOSTAT_INCR(xuiostat_onloan_wbuf, -nblk);
-#endif	
+#endif
 }
 
 /*
