576db837ac4232f56b0e0f6d6c3a214432cce972
joerg <joerg@NetBSD.org>
2015-03-19 21:22:59 +0000

    Call libc's fpgetround.

--- lib/libc/arch/mips/gen/flt_rounds.c
+++ lib/libc/arch/mips/gen/flt_rounds.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: flt_rounds.c,v 1.8 2014/09/17 11:02:55 joerg Exp $	*/
+/*	$NetBSD: flt_rounds.c,v 1.9 2015/03/19 21:22:59 joerg Exp $	*/
 
 /*
  * Written by J.T. Conklin, Apr 11, 1995
@@ -7,9 +7,10 @@
 
 #include <sys/cdefs.h>
 #if defined(LIBC_SCCS) && !defined(lint)
-__RCSID("$NetBSD: flt_rounds.c,v 1.8 2014/09/17 11:02:55 joerg Exp $");
+__RCSID("$NetBSD: flt_rounds.c,v 1.9 2015/03/19 21:22:59 joerg Exp $");
 #endif /* LIBC_SCCS and not lint */
 
+#include "namespace.h"
 #include <machine/float.h>
 #include <ieeefp.h>
 
