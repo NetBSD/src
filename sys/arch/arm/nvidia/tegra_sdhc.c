91717cd878466593c82748a6e4ba472ce861ab8d
jmcneill <jmcneill>
2015-03-29 10:41:59 +0000

    NVIDIA Tegra K1 support, work in progress.

---
+++ sys/arch/arm/nvidia/tegra_sdhc.c
@@ -0,0 +1,135 @@
+/* $NetBSD: tegra_sdhc.c,v 1.1 2015/03/29 10:41:59 jmcneill Exp $ */
+
+/*-
+ * Copyright (c) 2015 Jared D. McNeill <jmcneill@invisible.ca>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include "locators.h"
+
+#include <sys/cdefs.h>
+__KERNEL_RCSID(0, "$NetBSD: tegra_sdhc.c,v 1.1 2015/03/29 10:41:59 jmcneill Exp $");
+
+#include <sys/param.h>
+#include <sys/bus.h>
+#include <sys/device.h>
+#include <sys/intr.h>
+#include <sys/systm.h>
+#include <sys/kernel.h>
+
+#include <dev/sdmmc/sdhcreg.h>
+#include <dev/sdmmc/sdhcvar.h>
+#include <dev/sdmmc/sdmmcvar.h>
+
+#include <arm/nvidia/tegra_var.h>
+
+/* 8-bit eMMC is supported on SDMMC2 and SDMMC4 */
+#define SDMMC_8BIT_P(port)	((port) == 1 || (port) == 3)
+
+static int	tegra_sdhc_match(device_t, cfdata_t, void *);
+static void	tegra_sdhc_attach(device_t, device_t, void *);
+
+static void	tegra_sdhc_attach_i(device_t);
+
+struct tegra_sdhc_softc {
+	struct sdhc_softc	sc;
+
+	bus_space_tag_t		sc_bst;
+	bus_space_handle_t	sc_bsh;
+	bus_size_t		sc_bsz;
+	struct sdhc_host	*sc_host;
+	void			*sc_ih;
+};
+
+CFATTACH_DECL_NEW(tegra_sdhc, sizeof(struct tegra_sdhc_softc),
+	tegra_sdhc_match, tegra_sdhc_attach, NULL, NULL);
+
+static int
+tegra_sdhc_match(device_t parent, cfdata_t cf, void *aux)
+{
+	return 1;
+}
+
+static void
+tegra_sdhc_attach(device_t parent, device_t self, void *aux)
+{
+	struct tegra_sdhc_softc * const sc = device_private(self);
+	struct tegraio_attach_args * const tio = aux;
+	const struct tegra_locators * const loc = &tio->tio_loc;
+
+	sc->sc.sc_dev = self;
+	sc->sc.sc_dmat = tio->tio_dmat;
+	sc->sc.sc_flags = SDHC_FLAG_32BIT_ACCESS |
+			  SDHC_FLAG_USE_DMA;
+	if (SDMMC_8BIT_P(loc->loc_port)) {
+		sc->sc.sc_flags |= SDHC_FLAG_8BIT_MODE;
+	}
+	sc->sc.sc_host = &sc->sc_host;
+
+	sc->sc_bst = tio->tio_bst;
+	bus_space_subregion(tio->tio_bst, tio->tio_bsh,
+	    loc->loc_offset, loc->loc_size, &sc->sc_bsh);
+	sc->sc_bsz = loc->loc_size;
+
+#if notyet
+	sc->sc.sc_clkbase = tegra_sdhc_get_freq(loc->loc_port) / 1000;
+#else
+	sc->sc.sc_clkbase = 0;
+#endif
+
+	aprint_naive("\n");
+	aprint_normal(": SDMMC%d\n", loc->loc_port + 1);
+
+	if (sc->sc.sc_clkbase == 0) {
+		aprint_error_dev(self, "couldn't determine frequency\n");
+		return;
+	}
+
+	sc->sc_ih = intr_establish(loc->loc_intr, IPL_SDMMC, IST_LEVEL,
+	    sdhc_intr, &sc->sc);
+	if (sc->sc_ih == NULL) {
+		aprint_error_dev(self, "couldn't establish interrupt %d\n",
+		    loc->loc_intr);
+		return;
+	}
+	aprint_normal_dev(self, "interrupting on irq %d\n", loc->loc_intr);
+
+	config_interrupts(self, tegra_sdhc_attach_i);
+}
+
+static void
+tegra_sdhc_attach_i(device_t self)
+{
+	struct tegra_sdhc_softc * const sc = device_private(self);
+	int error;
+
+	error = sdhc_host_found(&sc->sc, sc->sc_bst, sc->sc_bsh, sc->sc_bsz);
+	if (error) {
+		aprint_error_dev(self, "couldn't initialize host, error = %d\n",
+		    error);
+		intr_disestablish(sc->sc_ih);
+		sc->sc_ih = NULL;
+		return;
+	}
+}
---
+++ sys/arch/arm/nvidia/tegra_var.h
@@ -0,0 +1,90 @@
+/* $NetBSD: tegra_var.h,v 1.1 2015/03/29 10:41:59 jmcneill Exp $ */
+
+/*-
+ * Copyright (c) 2015 Jared D. McNeill <jmcneill@invisible.ca>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#ifndef _ARM_TEGRA_VAR_H
+#define _ARM_TEGRA_VAR_H
+
+#include <sys/types.h>
+#include <sys/bus.h>
+
+#include "opt_tegra.h"
+
+struct tegra_locators {
+	const char *loc_name;
+	bus_addr_t loc_offset;
+	bus_size_t loc_size;
+	int loc_port;
+	int loc_intr;
+#define TEGRAIO_INTR_DEFAULT	0
+};
+
+struct tegraio_attach_args {
+	struct tegra_locators tio_loc;
+	bus_space_tag_t tio_bst;
+	bus_space_tag_t tio_a4x_bst;
+	bus_space_handle_t tio_bsh;
+	bus_dma_tag_t tio_dmat;
+};
+
+extern struct bus_space tegra_bs_tag;
+extern struct bus_space tegra_a4x_bs_tag;
+extern bus_space_handle_t tegra_host1x_bsh;
+extern bus_space_handle_t tegra_apb_bsh;
+extern bus_space_handle_t tegra_ahb_a2_bsh;
+extern struct arm32_bus_dma_tag tegra_dma_tag;
+
+#define CHIP_ID_TEGRA20		0x20
+#define CHIP_ID_TEGRA30		0x30
+#define CHIP_ID_TEGRA114	0x35
+#define CHIP_ID_TEGRA124	0x40
+#define CHIP_ID_TEGRA132	0x13
+
+u_int	tegra_chip_id(void);
+const char *tegra_chip_name(void);
+void	tegra_bootstrap(void);
+
+void	tegra_pmc_reset(void);
+
+psize_t	tegra_mc_memsize(void);
+
+#if defined(SOC_TEGRA124)
+void	tegra124_mpinit(void);
+#endif
+
+static void inline
+tegra_reg_set_clear(bus_space_tag_t bst, bus_space_handle_t bsh,
+    bus_size_t o, uint32_t set_mask, uint32_t clr_mask)
+{
+	const uint32_t old = bus_space_read_4(bst, bsh, o);
+	const uint32_t new = set_mask | (old & ~clr_mask);
+	if (old != new) {
+		bus_space_write_4(bst, bsh, o, new);
+	}
+}
+
+#endif /* _ARM_TEGRA_VAR_H */
