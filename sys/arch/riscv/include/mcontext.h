0af81e8e49fa27d3ed8a56094a085bcc1e822aaf
matt <matt@NetBSD.org>
2015-04-01 21:55:33 +0000

    Add _REG_S0

--- sys/arch/riscv/include/mcontext.h
+++ sys/arch/riscv/include/mcontext.h
@@ -1,4 +1,4 @@
-/* $NetBSD: mcontext.h,v 1.3 2015/03/27 06:57:21 matt Exp $ */
+/* $NetBSD: mcontext.h,v 1.4 2015/04/01 21:55:33 matt Exp $ */
 
 /*-
  * Copyright (c) 2014 The NetBSD Foundation, Inc.
@@ -91,6 +91,7 @@ typedef _BSD_FPREG_T_	__fregset_t[_NFREG];
 #define	_REG_SP		_REG_X2
 #define	_REG_GP		_REG_X3
 #define	_REG_TP		_REG_X4
+#define	_REG_S0		_REG_X8
 #define	_REG_RV		_REG_X10
 #define	_REG_A0		_REG_X10
 
