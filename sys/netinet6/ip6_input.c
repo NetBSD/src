8e73c84974dc9d08169e5c21142eaca483535b88
ozaki-r <ozaki-r@NetBSD.org>
2015-04-01 02:49:44 +0000

    Pull out ipsec routines from ip6_input
    This change reduces symbol references from netinet6 to netipsec
    and improves modularity of netipsec.
    
    No functional change is intended.

--- sys/netinet6/ip6_input.c
+++ sys/netinet6/ip6_input.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: ip6_input.c,v 1.150 2015/01/20 21:27:36 roy Exp $	*/
+/*	$NetBSD: ip6_input.c,v 1.151 2015/04/01 02:49:44 ozaki-r Exp $	*/
 /*	$KAME: ip6_input.c,v 1.188 2001/03/29 05:34:31 itojun Exp $	*/
 
 /*
@@ -62,7 +62,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: ip6_input.c,v 1.150 2015/01/20 21:27:36 roy Exp $");
+__KERNEL_RCSID(0, "$NetBSD: ip6_input.c,v 1.151 2015/04/01 02:49:44 ozaki-r Exp $");
 
 #include "opt_gateway.h"
 #include "opt_inet.h"
@@ -748,11 +748,6 @@ ip6_input(struct mbuf *m)
 
 #ifdef IPSEC
 		if (ipsec_used) {
-			struct m_tag *mtag;
-			struct tdb_ident *tdbi;
-			struct secpolicy *sp;
-			int s, error;
-
 			/*
 			 * enforce IPsec policy checking if we are seeing last
 			 * header. note that we do not visit this with
@@ -760,39 +755,7 @@ ip6_input(struct mbuf *m)
 			 */
 			if ((inet6sw[ip_protox[nxt]].pr_flags
 			    & PR_LASTHDR) != 0) {
-				/*
-				 * Check if the packet has already had IPsec
-				 * processing done. If so, then just pass it
-				 * along. This tag gets set during AH, ESP,
-				 * etc. input handling, before the packet is
-				 * returned to the ip input queue for delivery.
-				 */
-				mtag = m_tag_find(m, PACKET_TAG_IPSEC_IN_DONE,
-				    NULL);
-				s = splsoftnet();
-				if (mtag != NULL) {
-					tdbi = (struct tdb_ident *)(mtag + 1);
-					sp = ipsec_getpolicy(tdbi,
-					    IPSEC_DIR_INBOUND);
-				} else {
-					sp = ipsec_getpolicybyaddr(m,
-					    IPSEC_DIR_INBOUND, IP_FORWARDING,
-					    &error);
-				}
-				if (sp != NULL) {
-					/*
-					 * Check security policy against packet
-					 * attributes.
-					 */
-					error = ipsec_in_reject(sp, m);
-					KEY_FREESP(&sp);
-				} else {
-					/* XXX error stat??? */
-					error = EINVAL;
-					DPRINTF(("ip6_input: no SP, packet"
-					    " discarded\n"));/*XXX*/
-				}
-				splx(s);
+				int error = ipsec6_input(m);
 				if (error)
 					goto bad;
 			}
