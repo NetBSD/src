6d0550f51ccdaffb372defbd5293c849b9d0b678
skrll <skrll@NetBSD.org>
2015-04-05 09:24:21 +0000

    PR/49814: USB Quirk for Texas Instrument MSP430 FET programmer

--- sys/dev/usb/usb_quirks.c
+++ sys/dev/usb/usb_quirks.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: usb_quirks.c,v 1.82 2015/01/26 11:05:20 gson Exp $	*/
+/*	$NetBSD: usb_quirks.c,v 1.83 2015/04/05 09:24:21 skrll Exp $	*/
 /*	$FreeBSD: src/sys/dev/usb/usb_quirks.c,v 1.30 2003/01/02 04:15:55 imp Exp $	*/
 
 /*
@@ -32,7 +32,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: usb_quirks.c,v 1.82 2015/01/26 11:05:20 gson Exp $");
+__KERNEL_RCSID(0, "$NetBSD: usb_quirks.c,v 1.83 2015/04/05 09:24:21 skrll Exp $");
 
 #ifdef _KERNEL_OPT
 #include "opt_usb.h"
@@ -68,6 +68,7 @@ Static const struct usbd_quirk_entry {
  { USB_VENDOR_MISC, USB_PRODUCT_MISC_WISPY_24X, ANY, { UQ_HID_IGNORE }},
  { USB_VENDOR_WELTREND, USB_PRODUCT_WELTREND_HID,   ANY,   { UQ_HID_IGNORE }},
  { USB_VENDOR_SILABS, USB_PRODUCT_SILABS_EC3,       ANY,   { UQ_HID_IGNORE }},
+ { USB_VENDOR_TI, USB_PRODUCT_TI_MSP430,            ANY,   { UQ_HID_IGNORE }},
 
  { USB_VENDOR_KYE, USB_PRODUCT_KYE_NICHE,	    0x100, { UQ_NO_SET_PROTO}},
  { USB_VENDOR_INSIDEOUT, USB_PRODUCT_INSIDEOUT_EDGEPORT4,
