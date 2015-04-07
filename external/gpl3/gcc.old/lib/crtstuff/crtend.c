8931a81cefcfa6a1fd793fd6453c6dd6c370ae6b
mrg <mrg@NetBSD.org>
2015-04-07 21:19:28 +0000

    delete GCC 4.5.

--- external/gpl3/gcc.old/lib/crtstuff/crtend.c
+++
@@ -1,9 +0,0 @@
-/*	$NetBSD: crtend.c,v 1.1.1.1 2014/02/25 18:42:04 mrg Exp $	*/
-
-/*
- * Written by Jason R. Thorpe, August 24, 2004.
- * Public domain.
- */
-
-#define	CRT_END
-#include "crtstuff.c"
