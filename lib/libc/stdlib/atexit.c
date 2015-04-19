878d5342b3be82d0cf2c22df071dd8456d2bbff5
msaitoh <msaitoh@NetBSD.org>
2015-04-19 05:03:02 +0000

    Pull up following revision(s) (requested by joerg in ticket #708): 	lib/libc/stdlib/atexit.c: revision 1.28 Add __aeabi_atexit alias for another case of NIH on ARM.

--- lib/libc/stdlib/atexit.c
+++ lib/libc/stdlib/atexit.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: atexit.c,v 1.26 2013/08/19 22:14:37 matt Exp $	*/
+/*	$NetBSD: atexit.c,v 1.26.4.1 2015/04/19 05:03:02 msaitoh Exp $	*/
 
 /*-
  * Copyright (c) 2003 The NetBSD Foundation, Inc.
@@ -31,7 +31,7 @@
 
 #include <sys/cdefs.h>
 #if defined(LIBC_SCCS) && !defined(lint)
-__RCSID("$NetBSD: atexit.c,v 1.26 2013/08/19 22:14:37 matt Exp $");
+__RCSID("$NetBSD: atexit.c,v 1.26.4.1 2015/04/19 05:03:02 msaitoh Exp $");
 #endif /* LIBC_SCCS and not lint */
 
 #include "reentrant.h"
@@ -133,6 +133,10 @@ __libc_atexit_init(void)
  *
  *	http://www.codesourcery.com/cxx-abi/abi.html#dso-dtor
  */
+#if defined(__ARM_EABI__) && !defined(lint)
+__strong_alias(__aeabi_atexit,__cxa_atexit);
+#endif
+
 int
 __cxa_atexit(void (*func)(void *), void *arg, void *dso)
 {
