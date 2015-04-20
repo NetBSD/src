dcb91e6cce5266f80f5fc753ee8d9f05b4f9cc47
matt <matt@NetBSD.org>
2015-04-20 01:33:22 +0000

    Switch to armv7_generic bus_space.

--- sys/arch/arm/allwinner/awin_wdt.c
+++ sys/arch/arm/allwinner/awin_wdt.c
@@ -33,7 +33,7 @@
 
 #include <sys/cdefs.h>
 
-__KERNEL_RCSID(1, "$NetBSD: awin_wdt.c,v 1.6 2014/12/05 11:53:43 jmcneill Exp $");
+__KERNEL_RCSID(1, "$NetBSD: awin_wdt.c,v 1.7 2015/04/20 01:33:22 matt Exp $");
 
 #include <sys/param.h>
 #include <sys/bus.h>
@@ -109,7 +109,7 @@ static struct awin_wdt_softc {
 	bus_size_t sc_ctrl_reg;
 	bus_size_t sc_mode_reg;
 } awin_wdt_sc = {
-	.sc_bst = &awin_bs_tag,
+	.sc_bst = &armv7_generic_bs_tag,
 	.sc_wdog_period = AWIN_WDT_PERIOD_DEFAULT,
 };
 
@@ -266,6 +266,7 @@ void
 awin_wdog_reset(void)
 {
 	bus_size_t off;
+	bus_space_tag_t bst = &armv7_generic_bs_tag;
 
 	cpsid(I32_bit|F32_bit);
 
@@ -274,16 +275,16 @@ awin_wdog_reset(void)
 	case AWIN_CHIP_ID_A80:
 		off = awin_chip_id() == AWIN_CHIP_ID_A80 ?
 		    AWIN_A80_TIMER_OFFSET : AWIN_TMR_OFFSET;
-		bus_space_write_4(&awin_bs_tag, awin_core_bsh,
+		bus_space_write_4(bst, awin_core_bsh,
 		    off + AWIN_A31_WDOG1_CFG_REG,
 		    __SHIFTIN(AWIN_A31_WDOG_CFG_CONFIG_SYS,
 			      AWIN_A31_WDOG_CFG_CONFIG));
-		bus_space_write_4(&awin_bs_tag, awin_core_bsh,
+		bus_space_write_4(bst, awin_core_bsh,
 		    off + AWIN_A31_WDOG1_MODE_REG,
 		    AWIN_A31_WDOG_MODE_EN);
 		break;
 	default:
-		bus_space_write_4(&awin_bs_tag, awin_core_bsh,
+		bus_space_write_4(bst, awin_core_bsh,
 		    AWIN_TMR_OFFSET + AWIN_WDOG_MODE_REG,
 		    AWIN_WDOG_MODE_EN | AWIN_WDOG_MODE_RST_EN);
 		break;
