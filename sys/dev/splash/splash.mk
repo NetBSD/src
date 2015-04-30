3f26ea07eedf67f004549b8c81b657e51d6d2711
nat <nat@NetBSD.org>
2015-04-30 13:46:47 +0000

    Added a new option for embedding a splash screen into kernel. Add: options SPLASHSCREEN      makeoptions SPLASHSCREEN_IMAGE="path/to/image" to your config file.  So far it will work on amd64 and RPI/RPI2.
    This commit was with ideas, help, and OK from jmcneill@.

---
+++ sys/dev/splash/splash.mk
@@ -0,0 +1,26 @@
+# Makefile for embedding splash image into kernel.
+.include <bsd.endian.mk>
+
+MD_OBJS+=	splash_image.o
+CFLAGS+=	-DSPLASHSCREEN_IMAGE
+
+.if (${OBJECT_FMTS:Melf64})
+BFD_ELFTARGET=elf64
+.else
+BFD_ELFTARGET=elf32
+.endif
+
+BFD_ENDIANNESS=${TARGET_ENDIANNESS:S/1234/little/C/4321/big/}
+BFD_CPU=${MACHINE_CPU:S/_/-/}
+
+.if (${BFD_CPU:Maarch64} || ${BFD_CPU:Marm} || ${BFD_CPU:Mmips} || ${BFD_CPU:Mscore})
+BFD_TARGET=${BFD_ELFTARGET}-${BFD_ENDIANNESS}${BFD_CPU}
+.else
+BFD_TARGET=${BFD_ELFTARGET}-${BFD_CPU}
+.endif
+
+splash_image.o:	${SPLASHSCREEN_IMAGE}
+	cp ${SPLASHSCREEN_IMAGE} splash.image
+	${OBJCOPY} -I binary -B ${MACHINE_CPU:C/x86_64/i386/} \
+		-O ${BFD_TARGET} splash.image splash_image.o
+	rm splash.image
