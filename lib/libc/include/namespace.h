0325e57d92ed5ca0cf05190ea58ee17ddb486c9c
christos <christos@NetBSD.org>
2015-05-01 14:17:56 +0000

    - new test for strtoi - namespace protection for strto{i,u} - separate manpages for strto{i,u} from the ones for strto{u,}l From: Kamil Rytarowski

--- lib/libc/include/namespace.h
+++ lib/libc/include/namespace.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: namespace.h,v 1.177 2015/02/17 20:29:21 joerg Exp $	*/
+/*	$NetBSD: namespace.h,v 1.178 2015/05/01 14:17:56 christos Exp $	*/
 
 /*-
  * Copyright (c) 1997-2004 The NetBSD Foundation, Inc.
@@ -65,12 +65,16 @@
 #define strtod_l	_strtod_l
 #define strtof		_strtof
 #define strtof_l	_strtof_l
+#define strtoi		_strtoi
+#define strtoi_l	_strtoi_l
 #define strtoimax	_strtoimax
 #define strtoimax_l	_strtoimax_l
 #define strtold		_strtold
 #define strtold_l	_strtold_l
 #define strtoll		_strtoll
 #define strtoll_l	_strtoll_l
+#define strtou		_strtou
+#define strtou_l	_strtou_l
 #define strtoull	_strtoull
 #define strtoull_l	_strtoull_l
 #define strtoumax	_strtoumax
