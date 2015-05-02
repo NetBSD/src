a840efccb9bb3070613ee3381b4c0c72fd16c6ba
rtr <rtr@NetBSD.org>
2015-05-02 21:15:33 +0000

    remove unnecessary check that nam != NULL before deref in soconnect() (added in previous commit).
    sockargs copyin() makes sure we don't get NULL here

--- sys/kern/uipc_socket.c
+++ sys/kern/uipc_socket.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: uipc_socket.c,v 1.241 2015/05/02 20:10:26 rtr Exp $	*/
+/*	$NetBSD: uipc_socket.c,v 1.242 2015/05/02 21:15:33 rtr Exp $	*/
 
 /*-
  * Copyright (c) 2002, 2007, 2008, 2009 The NetBSD Foundation, Inc.
@@ -71,7 +71,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: uipc_socket.c,v 1.241 2015/05/02 20:10:26 rtr Exp $");
+__KERNEL_RCSID(0, "$NetBSD: uipc_socket.c,v 1.242 2015/05/02 21:15:33 rtr Exp $");
 
 #include "opt_compat_netbsd.h"
 #include "opt_sock_counters.h"
@@ -830,8 +830,7 @@ soconnect(struct socket *so, struct sockaddr *nam, struct lwp *l)
 	    (error = sodisconnect(so)))) {
 		error = EISCONN;
 	} else {
-		if (NULL != nam &&
-		    nam->sa_family != so->so_proto->pr_domain->dom_family) {
+		if (nam->sa_family != so->so_proto->pr_domain->dom_family) {
 			return EAFNOSUPPORT;
 		}
 		error = (*so->so_proto->pr_usrreqs->pr_connect)(so, nam, l);
