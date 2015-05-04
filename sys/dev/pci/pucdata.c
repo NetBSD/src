8a25d04f3aa392f4133c5d0dea24f212368af430
ryo <ryo@NetBSD.org>
2015-05-04 21:21:38 +0000

    PR/49819: Roberto E. Vargas Caballero: Add support for SystemBase SB16C1050 PCI serial card

--- sys/dev/pci/pucdata.c
+++ sys/dev/pci/pucdata.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: pucdata.c,v 1.94 2015/03/18 05:54:14 msaitoh Exp $	*/
+/*	$NetBSD: pucdata.c,v 1.95 2015/05/04 21:21:39 ryo Exp $	*/
 
 /*
  * Copyright (c) 1998, 1999 Christopher G. Demetriou.  All rights reserved.
@@ -36,7 +36,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: pucdata.c,v 1.94 2015/03/18 05:54:14 msaitoh Exp $");
+__KERNEL_RCSID(0, "$NetBSD: pucdata.c,v 1.95 2015/05/04 21:21:39 ryo Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -2780,6 +2780,16 @@ const struct puc_device_description puc_devices[] = {
 		{ PUC_PORT_TYPE_COM, PCI_BAR0, 0x3e8, COM_FREQ },
 		{ PUC_PORT_TYPE_COM, PCI_BAR0, 0x3f8, COM_FREQ },
 		{ PUC_PORT_TYPE_LPT, PCI_BAR0, 0x000, 0x00 },
+	    },
+	},
+
+	/* SystemBase SB16C1050 UARTs */
+	{   "SystemBase SB16C1050",
+	    {	PCI_VENDOR_SYSTEMBASE, PCI_PRODUCT_SYSTEMBASE_SB16C1050, 0, 0 },
+	    {	0xffff, 0xffff,						 0, 0 },
+	    {
+		{ PUC_PORT_TYPE_COM, PCI_BAR0, 0x00, COM_FREQ * 8},
+		{ PUC_PORT_TYPE_COM, PCI_BAR0, 0x08, COM_FREQ * 8},
 	    },
 	},
 
