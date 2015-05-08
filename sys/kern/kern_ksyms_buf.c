b86768c1aa5d5bc6a7c211933ffd1fe7cf74c42e
martin <martin>
2015-05-08 07:41:07 +0000

    On mips64, the size calculation for the symbol table is off by a few bytes (probably a bfd bug or misconfiguration), so add a bit of slope here untill we fix it.

--- sys/kern/kern_ksyms_buf.c
+++ sys/kern/kern_ksyms_buf.c
@@ -4,7 +4,12 @@
 #ifndef SYMTAB_SPACE
 char		db_symtab[] = SYMTAB_FILLER;
 #else
-char		db_symtab[SYMTAB_SPACE] = SYMTAB_FILLER;
+#ifdef __mips64
+#define MDADD	32	/* work around a bfd/dbsym/ld bug on mips64 */
+#else
+#define	MDADD	0
+#endif
+char		db_symtab[SYMTAB_SPACE+MDADD] = SYMTAB_FILLER;
 #endif
 int		db_symtabsize = sizeof(db_symtab);
 #endif
