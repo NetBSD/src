d95056bb0c83262c1b2476e53daae20d5d35b3f0
martin <martin@NetBSD.org>
2015-05-08 09:43:41 +0000

    Backout previous, Nick has a fix for binutils.

--- sys/kern/kern_ksyms_buf.c
+++ sys/kern/kern_ksyms_buf.c
@@ -4,12 +4,7 @@
 #ifndef SYMTAB_SPACE
 char		db_symtab[] = SYMTAB_FILLER;
 #else
-#ifdef __mips64
-#define MDADD	32	/* work around a bfd/dbsym/ld bug on mips64 */
-#else
-#define	MDADD	0
-#endif
-char		db_symtab[SYMTAB_SPACE+MDADD] = SYMTAB_FILLER;
+char		db_symtab[SYMTAB_SPACE] = SYMTAB_FILLER;
 #endif
 int		db_symtabsize = sizeof(db_symtab);
 #endif
