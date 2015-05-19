062ac7c279abd4a970878ed3d54c889b118ad0cf
snj <snj@NetBSD.org>
2015-05-19 04:58:31 +0000

    Pull up following revision(s) (requested by joerg in ticket #777): 	sys/arch/arm/arm32/pmap.c: revision 1.308 pmap_tlb_flushD is for !ARM_MMU_EXTENDED only now

--- sys/arch/arm/arm32/pmap.c
+++ sys/arch/arm/arm32/pmap.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: pmap.c,v 1.295.2.4 2015/04/10 20:17:52 snj Exp $	*/
+/*	$NetBSD: pmap.c,v 1.295.2.5 2015/05/19 04:58:31 snj Exp $	*/
 
 /*
  * Copyright 2003 Wasabi Systems, Inc.
@@ -216,7 +216,7 @@
 #include <arm/locore.h>
 //#include <arm/arm32/katelib.h>
 
-__KERNEL_RCSID(0, "$NetBSD: pmap.c,v 1.295.2.4 2015/04/10 20:17:52 snj Exp $");
+__KERNEL_RCSID(0, "$NetBSD: pmap.c,v 1.295.2.5 2015/05/19 04:58:31 snj Exp $");
 
 //#define PMAP_DEBUG
 #ifdef PMAP_DEBUG
@@ -850,12 +850,10 @@ pmap_tlb_flushID(pmap_t pm)
 #endif /* ARM_MMU_EXTENDED */
 }
 
+#ifndef ARM_MMU_EXTENDED
 static inline void
 pmap_tlb_flushD(pmap_t pm)
 {
-#ifdef ARM_MMU_EXTENDED
-	pmap_tlb_asid_release_all(pm);
-#else
 	if (pm->pm_cstate.cs_tlb_d) {
 		cpu_tlb_flushD();
 #if ARM_MMU_V7 == 0
@@ -869,8 +867,8 @@ pmap_tlb_flushD(pmap_t pm)
 		pm->pm_cstate.cs_tlb_d = 0;
 #endif /* ARM_MMU_V7 */
 	}
-#endif /* ARM_MMU_EXTENDED */
 }
+#endif /* ARM_MMU_EXTENDED */
 
 #ifdef PMAP_CACHE_VIVT
 static inline void
