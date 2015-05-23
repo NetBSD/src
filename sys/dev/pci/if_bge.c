d769d30f62aeaaa3d9cca3427436d4e3dda6b6ce
msaitoh <msaitoh@NetBSD.org>
2015-05-23 13:44:40 +0000

    Remove debug printf().

--- sys/dev/pci/if_bge.c
+++ sys/dev/pci/if_bge.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: if_bge.c,v 1.290 2015/05/18 01:06:35 msaitoh Exp $	*/
+/*	$NetBSD: if_bge.c,v 1.291 2015/05/23 13:44:40 msaitoh Exp $	*/
 
 /*
  * Copyright (c) 2001 Wind River Systems
@@ -79,7 +79,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: if_bge.c,v 1.290 2015/05/18 01:06:35 msaitoh Exp $");
+__KERNEL_RCSID(0, "$NetBSD: if_bge.c,v 1.291 2015/05/23 13:44:40 msaitoh Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -4745,7 +4745,6 @@ bge_intr(void *xsc)
 	if (sc->bge_flags & BGEF_TAGGED_STATUS) {
 		if (sc->bge_lasttag == statustag &&
 		    (~pcistate & intrmask)) {
-			printf("[SP]");
 			return (0);
 		}
 		sc->bge_lasttag = statustag;
