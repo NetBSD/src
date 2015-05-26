0ae3d4cd2742f91e8d37317db2afcf0e02dc2ce4
christos <christos@NetBSD.org>
2015-05-26 21:35:15 +0000

    Drop privileges when executed set{u,g}id unless -p is specified like other shells do to avoid system() and popen() abuse.

--- bin/sh/main.c
+++ bin/sh/main.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: main.c,v 1.58 2014/05/31 14:42:18 christos Exp $	*/
+/*	$NetBSD: main.c,v 1.59 2015/05/26 21:35:15 christos Exp $	*/
 
 /*-
  * Copyright (c) 1991, 1993
@@ -42,7 +42,7 @@ __COPYRIGHT("@(#) Copyright (c) 1991, 1993\
 #if 0
 static char sccsid[] = "@(#)main.c	8.7 (Berkeley) 7/19/95";
 #else
-__RCSID("$NetBSD: main.c,v 1.58 2014/05/31 14:42:18 christos Exp $");
+__RCSID("$NetBSD: main.c,v 1.59 2015/05/26 21:35:15 christos Exp $");
 #endif
 #endif /* not lint */
 
@@ -106,6 +106,11 @@ main(int argc, char **argv)
 	struct stackmark smark;
 	volatile int state;
 	char *shinit;
+	uid_t uid;
+	gid_t gid;
+
+	uid = getuid();
+	gid = getgid();
 
 	setlocale(LC_ALL, "");
 
@@ -178,6 +183,18 @@ main(int argc, char **argv)
 	initpwd();
 	setstackmark(&smark);
 	procargs(argc, argv);
+
+	/*
+	 * Limit bogus system(3) or popen(3) calls in setuid binaries,
+	 * by requiring the -p flag
+	 */
+	if (!pflag && (uid != geteuid() || gid != getegid())) {
+		setuid(uid);
+		setgid(gid);
+		/* PS1 might need to be changed accordingly. */
+		choose_ps1();
+	}
+
 	if (argv[0] && argv[0][0] == '-') {
 		state = 1;
 		read_profile("/etc/profile");
