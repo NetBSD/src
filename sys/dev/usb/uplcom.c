07771ac2f461527bcd12b733117954dad47dc996
riastradh <riastradh>
2015-05-30 16:44:28 +0000

    Null suspend/resume handler for uplcom(4).

--- sys/dev/usb/uplcom.c
+++ sys/dev/usb/uplcom.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: uplcom.c,v 1.74 2014/07/14 12:04:48 ryoon Exp $	*/
+/*	$NetBSD: uplcom.c,v 1.75 2015/05/30 16:44:28 riastradh Exp $	*/
 /*
  * Copyright (c) 2001 The NetBSD Foundation, Inc.
  * All rights reserved.
@@ -34,7 +34,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: uplcom.c,v 1.74 2014/07/14 12:04:48 ryoon Exp $");
+__KERNEL_RCSID(0, "$NetBSD: uplcom.c,v 1.75 2015/05/30 16:44:28 riastradh Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -418,6 +418,9 @@ uplcom_attach(device_t parent, device_t self, void *aux)
 	sc->sc_subdev = config_found_sm_loc(self, "ucombus", NULL, &uca,
 					    ucomprint, ucomsubmatch);
 
+	if (!pmf_device_register(self, NULL, NULL))
+		aprint_error_dev(self, "couldn't establish power handler\n");
+
 	return;
 }
 
@@ -451,6 +454,9 @@ uplcom_detach(device_t self, int flags)
 
 	usbd_add_drv_event(USB_EVENT_DRIVER_DETACH, sc->sc_udev,
 			   sc->sc_dev);
+
+	if (rv == 0)
+		pmf_device_deregister(self);
 
 	return (rv);
 }
