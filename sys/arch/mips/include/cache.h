a3058da4e42e0b2c2869d4ea9e6f3a72ff5e34e5
matt <matt@NetBSD.org>
2015-06-07 07:14:47 +0000

    Multiple inclusion protection

--- sys/arch/mips/include/cache.h
+++ sys/arch/mips/include/cache.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: cache.h,v 1.11 2011/03/15 07:39:22 matt Exp $	*/
+/*	$NetBSD: cache.h,v 1.12 2015/06/07 07:14:47 matt Exp $	*/
 
 /*
  * Copyright 2001 Wasabi Systems, Inc.
@@ -34,6 +34,9 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
+
+#ifndef _MIPS_CACHE_H_
+#define _MIPS_CACHE_H_
 
 /*
  * Cache operations.
@@ -287,3 +290,5 @@ void	mips_config_cache(void);
 void	mips_dcache_compute_align(void);
 
 #include <mips/cache_mipsNN.h>
+
+#endif /* _MIPS_CACHE_H_ */
