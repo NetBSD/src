5d8cb0d30d155e421fa8339d7539a19731a7d93e
matt <matt>
2015-06-11 08:22:08 +0000

    Add struct pmap_limits and pm_{min,max}addr from uvm/pmap/map.h and use it to store avail_start, avail_end, virtual_start, and virtual_end. Remove iospace and let emips just bump pmap_limits.virtual_start to get the VA space it needs. pmap_segtab.c is almost identical to uvm/pmap/pmap_segtab.c now.  It won't be long until we switch to the uvm/pmap one.

--- sys/arch/newsmips/newsmips/bus.c
+++ sys/arch/newsmips/newsmips/bus.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: bus.c,v 1.32 2012/10/02 23:54:53 christos Exp $	*/
+/*	$NetBSD: bus.c,v 1.33 2015/06/11 08:22:09 matt Exp $	*/
 
 /*
  * Copyright (c) 1998 The NetBSD Foundation, Inc.
@@ -31,7 +31,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: bus.c,v 1.32 2012/10/02 23:54:53 christos Exp $");
+__KERNEL_RCSID(0, "$NetBSD: bus.c,v 1.33 2015/06/11 08:22:09 matt Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -705,7 +705,8 @@ _bus_dmamem_alloc(bus_dma_tag_t t, bus_size_t size, bus_size_t alignment,
 {
 	return (_bus_dmamem_alloc_range_common(t, size, alignment, boundary,
 	    segs, nsegs, rsegs, flags,
-	    mips_avail_start /*low*/, mips_avail_end - PAGE_SIZE /*high*/));
+	    pmap_limits.avail_start /*low*/,
+	    pmap_limits.avail_end - PAGE_SIZE /*high*/));
 }
 
 /*
