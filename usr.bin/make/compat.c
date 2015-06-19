156ff47067a9e5115cb68e7583d6e35ae9b3987a
christos <christos@NetBSD.org>
2015-06-19 14:32:04 +0000

    make needshell customizable, so that xinstall can use it.

--- usr.bin/make/compat.c
+++ usr.bin/make/compat.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: compat.c,v 1.99 2015/06/19 14:25:34 christos Exp $	*/
+/*	$NetBSD: compat.c,v 1.100 2015/06/19 14:32:04 christos Exp $	*/
 
 /*
  * Copyright (c) 1988, 1989, 1990 The Regents of the University of California.
@@ -70,14 +70,14 @@
  */
 
 #ifndef MAKE_NATIVE
-static char rcsid[] = "$NetBSD: compat.c,v 1.99 2015/06/19 14:25:34 christos Exp $";
+static char rcsid[] = "$NetBSD: compat.c,v 1.100 2015/06/19 14:32:04 christos Exp $";
 #else
 #include <sys/cdefs.h>
 #ifndef lint
 #if 0
 static char sccsid[] = "@(#)compat.c	8.2 (Berkeley) 3/19/94";
 #else
-__RCSID("$NetBSD: compat.c,v 1.99 2015/06/19 14:25:34 christos Exp $");
+__RCSID("$NetBSD: compat.c,v 1.100 2015/06/19 14:32:04 christos Exp $");
 #endif
 #endif /* not lint */
 #endif
@@ -280,7 +280,7 @@ CompatRunCommand(void *cmdp, void *gnp)
      * meta characters as documented in make(1).
      */
     
-    useShell = needshell(cmd);
+    useShell = needshell(cmd, FALSE);
 #endif
 
     /*
