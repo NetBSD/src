bfc163275e6d805e9d18d0eec063bc2fec09363a
skrll <skrll@NetBSD.org>
2015-06-20 07:13:25 +0000

    __USE_TOPDOWN_VM
    Fixes PR/49974: mmap(2) of large anonymous regions is broken
    
    "go for it" from matt@

--- sys/arch/arm/include/arm32/vmparam.h
+++ sys/arch/arm/include/arm32/vmparam.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: vmparam.h,v 1.38 2015/06/02 05:54:23 matt Exp $	*/
+/*	$NetBSD: vmparam.h,v 1.39 2015/06/20 07:13:25 skrll Exp $	*/
 
 /*
  * Copyright (c) 2001, 2002 Wasabi Systems, Inc.
@@ -47,6 +47,7 @@
 #include <arm/cpuconf.h>
 #include <arm/arm32/pte.h>	/* pt_entry_t */
 
+#define	__USE_TOPDOWN_VM 
 #define	USRSTACK	VM_MAXUSER_ADDRESS
 
 /*
