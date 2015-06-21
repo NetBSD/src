5736286013de95970766f3a5cfb013a746614c89
christos <christos@NetBSD.org>
2015-06-21 01:13:21 +0000

    Restart the loop each time we delete an entry because the hash code does not handle it well. Is that a db bug?

--- external/bsd/blacklist/bin/blacklistd.c
+++ external/bsd/blacklist/bin/blacklistd.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: blacklistd.c,v 1.32 2015/01/28 22:30:42 christos Exp $	*/
+/*	$NetBSD: blacklistd.c,v 1.33 2015/06/21 01:13:21 christos Exp $	*/
 
 /*-
  * Copyright (c) 2015 The NetBSD Foundation, Inc.
@@ -32,7 +32,7 @@
 #include "config.h"
 #endif
 #include <sys/cdefs.h>
-__RCSID("$NetBSD: blacklistd.c,v 1.32 2015/01/28 22:30:42 christos Exp $");
+__RCSID("$NetBSD: blacklistd.c,v 1.33 2015/06/21 01:13:21 christos Exp $");
 
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -284,6 +284,7 @@ update(void)
 		return;
 	}
 
+again:
 	for (n = 0, f = 1; state_iterate(state, &c, &dbi, f) == 1;
 	    f = 0, n++)
 	{
@@ -305,6 +306,7 @@ update(void)
 			    buf, c.c_lmask, c.c_port, c.c_duration);
 		}
 		state_del(state, &c);
+		goto again;
 	}
 }
 
