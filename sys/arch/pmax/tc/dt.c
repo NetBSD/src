421a9e73767e6e1f1d41e6df42757066c1c50eac
snj <snj@NetBSD.org>
2015-07-05 20:32:56 +0000

    Pull up following revision(s) (requested by maxv in ticket #856): 	sys/arch/pmax/tc/dt.c: revision 1.12 Use-after-free. ok christos@ Found by The Brainy Code Scanner.

--- sys/arch/pmax/tc/dt.c
+++ sys/arch/pmax/tc/dt.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: dt.c,v 1.11 2011/06/04 01:37:36 tsutsui Exp $	*/
+/*	$NetBSD: dt.c,v 1.11.28.1 2015/07/05 20:32:56 snj Exp $	*/
 
 /*-
  * Copyright (c) 2002, 2003 The NetBSD Foundation, Inc.
@@ -133,7 +133,7 @@ SOFTWARE.
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: dt.c,v 1.11 2011/06/04 01:37:36 tsutsui Exp $");
+__KERNEL_RCSID(0, "$NetBSD: dt.c,v 1.11.28.1 2015/07/05 20:32:56 snj Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -225,6 +225,7 @@ dt_attach(device_t parent, device_t self, void *aux)
 	if (sc->sc_sih == NULL) {
 		printf("%s: memory exhausted\n", device_xname(self));
 		free(msg, M_DEVBUF);
+		return;
 	}
 
 	SIMPLEQ_INIT(&sc->sc_queue);
