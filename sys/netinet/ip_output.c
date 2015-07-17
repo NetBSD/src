6ea5b05305161f19117c8c31ff7da0f782523176
ozaki-r <ozaki-r>
2015-07-17 02:21:08 +0000

    Reform use of rt_refcnt
    rt_refcnt of rtentry was used in bad manners, for example, direct rt_refcnt++
    and rt_refcnt-- outside route.c, "rt->rt_refcnt++; rtfree(rt);" idiom, and
    touching rt after rt->rt_refcnt--.
    
    These abuses seem to be needed because rt_refcnt manages only references
    between rtentry and doesn't take care of references during packet processing
    (IOW references from local variables). In order to reduce the above abuses,
    the latter cases should be counted by rt_refcnt as well as the former cases.
    
    This change improves consistency of use of rt_refcnt:
    - rtentry is always accessed with rt_refcnt incremented
    - rtentry's rt_refcnt is decremented after use (rtfree is always used instead
      of rt_refcnt--)
    - functions returning rtentry increment its rt_refcnt (and caller rtfree it)
    
    Note that rt_refcnt prevents rtentry from being freed but doesn't prevent
    rtentry from being updated. Toward MP-safe, we need to provide another
    protection for rtentry, e.g., locks. (Or introduce a better data structure
    allowing concurrent readers during updates.)

--- sys/netinet/ip_output.c
+++ sys/netinet/ip_output.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: ip_output.c,v 1.243 2015/07/14 08:44:59 ozaki-r Exp $	*/
+/*	$NetBSD: ip_output.c,v 1.244 2015/07/17 02:21:08 ozaki-r Exp $	*/
 
 /*
  * Copyright (C) 1995, 1996, 1997, and 1998 WIDE Project.
@@ -91,7 +91,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: ip_output.c,v 1.243 2015/07/14 08:44:59 ozaki-r Exp $");
+__KERNEL_RCSID(0, "$NetBSD: ip_output.c,v 1.244 2015/07/17 02:21:08 ozaki-r Exp $");
 
 #include "opt_inet.h"
 #include "opt_ipsec.h"
@@ -201,12 +201,17 @@ klock_if_output(struct ifnet * const ifp, struct mbuf * const m,
  */
 int
 ip_hresolv_output(struct ifnet * const ifp0, struct mbuf * const m,
-    const struct sockaddr * const dst, struct rtentry *rt0)
+    const struct sockaddr * const dst, struct rtentry *rt00)
 {
 	int error = 0;
 	struct ifnet *ifp = ifp0;
-	struct rtentry *rt;
+	struct rtentry *rt, *rt0, *gwrt;
 
+#define RTFREE_IF_NEEDED(_rt) \
+	if ((_rt) != NULL && (_rt) != rt00) \
+		rtfree((_rt));
+
+	rt0 = rt00;
 retry:
 	if (!ip_hresolv_needed(ifp)) {
 		rt = rt0;
@@ -232,10 +237,8 @@ retry:
 			goto bad;
 		}
 		rt0 = rt;
-		rt->rt_refcnt--;
 		if (rt->rt_ifp != ifp) {
 			ifp = rt->rt_ifp;
-			rt0 = rt;
 			goto retry;
 		}
 	}
@@ -243,24 +246,27 @@ retry:
 	if ((rt->rt_flags & RTF_GATEWAY) == 0)
 		goto out;
 
-	rt = rt->rt_gwroute;
+	gwrt = rt_get_gwroute(rt);
+	RTFREE_IF_NEEDED(rt);
+	rt = gwrt;
 	if (rt == NULL || (rt->rt_flags & RTF_UP) == 0) {
 		if (rt != NULL) {
-			rtfree(rt);
+			RTFREE_IF_NEEDED(rt);
 			rt = rt0;
 		}
 		if (rt == NULL) {
 			error = EHOSTUNREACH;
 			goto bad;
 		}
-		rt = rt->rt_gwroute = rtalloc1(rt->rt_gateway, 1);
+		gwrt = rt->rt_gwroute = rtalloc1(rt->rt_gateway, 1);
+		RTFREE_IF_NEEDED(rt);
+		rt = gwrt;
 		if (rt == NULL) {
 			error = EHOSTUNREACH;
 			goto bad;
 		}
 		/* the "G" test below also prevents rt == rt0 */
 		if ((rt->rt_flags & RTF_GATEWAY) != 0 || rt->rt_ifp != ifp) {
-			rt->rt_refcnt--;
 			rt0->rt_gwroute = NULL;
 			error = EHOSTUNREACH;
 			goto bad;
@@ -300,12 +306,18 @@ out:
 	}
 #endif
 
-	return klock_if_output(ifp, m, dst, rt);
+	error = klock_if_output(ifp, m, dst, rt);
+	goto exit;
+
 bad:
 	if (m != NULL)
 		m_freem(m);
+exit:
+	RTFREE_IF_NEEDED(rt);
 
 	return error;
+
+#undef RTFREE_IF_NEEDED
 }
 
 /*
