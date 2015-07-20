afa763f59c3ae3e49bd398bb5f81cdd0b66b630c
mrg <mrg@NetBSD.org>
2015-07-20 02:07:36 +0000

    use xorg-pkg-ver.mk for silicon-motion.

--- external/mit/xorg/xorg-pkg-ver.mk
+++ external/mit/xorg/xorg-pkg-ver.mk
@@ -1,16 +1,18 @@
-#	$NetBSD: xorg-pkg-ver.mk,v 1.1 2015/07/19 19:49:17 mrg Exp $
+#	$NetBSD: xorg-pkg-ver.mk,v 1.2 2015/07/20 02:07:36 mrg Exp $
 
 # when including this make sure PROG is set so that $X11SRCDIR.$PROG
-# is a valid setting.
+# is a valid setting.  set XORG_PKG_VER_PROG if PROG is wrong.
 
-.if exists(${X11SRCDIR.${PROG}}/configure)
+XORG_PKG_VER_PROG?=	${PROG}
+
+.if exists(${X11SRCDIR.${XORG_PKG_VER_PROG}}/configure)
 XORG_PKG_PACKAGE_VERSION!= \
 	awk -F= '/^PACKAGE_VERSION=/ {				\
 	     match($$2, "([0-9]+\\.)+[0-9]+");			\
 	     version = substr($$2, RSTART, RLENGTH);		\
 	     print version;					\
 	     exit 0;						\
-	}' ${X11SRCDIR.${PROG}}/configure
+	}' ${X11SRCDIR.${XORG_PKG_VER_PROG}}/configure
 .if !empty(XORG_PKG_PACKAGE_VERSION)
 CPPFLAGS+=	-DPACKAGE_VERSION=\"${XORG_PKG_PACKAGE_VERSION:Q}\"
 .endif
@@ -21,7 +23,7 @@ XORG_PKG_PACKAGE_STRING!= \
 	     string = substr($$2, RSTART, RLENGTH);		\
 	     print string;					\
 	     exit 0;						\
-	}' ${X11SRCDIR.${PROG}}/configure
+	}' ${X11SRCDIR.${XORG_PKG_VER_PROG}}/configure
 .if !empty(XORG_PKG_PACKAGE_STRING)
 CPPFLAGS+=	-DPACKAGE_STRING=\"${XORG_PKG_PACKAGE_STRING:Q}\"
 .endif
