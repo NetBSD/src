06cb46469bd508958abcaa4777945ff162cf4516
snj <snj@NetBSD.org>
2015-07-31 16:59:43 +0000

    Pull up following revision(s) (requested by christos in ticket #917): 	doc/3RDPARTY: patch 	external/bsd/bind/dist/CHANGES: up to 1.16 	external/bsd/bind/dist/README: up to 1.4 	external/bsd/bind/dist/doc/arm/Bv9ARM.ch01.html: up to 1.1.1.14 	external/bsd/bind/dist/doc/arm/Bv9ARM.ch02.html: up to 1.1.1.11 	external/bsd/bind/dist/doc/arm/Bv9ARM.ch03.html: up to 1.1.1.16 	external/bsd/bind/dist/doc/arm/Bv9ARM.ch04.html: up to 1.4 	external/bsd/bind/dist/doc/arm/Bv9ARM.ch05.html: up to 1.1.1.17 	external/bsd/bind/dist/doc/arm/Bv9ARM.ch06.html: up to 1.4 	external/bsd/bind/dist/doc/arm/Bv9ARM.ch07.html: up to 1.4 	external/bsd/bind/dist/doc/arm/Bv9ARM.ch08.html: up to 1.4 	external/bsd/bind/dist/doc/arm/Bv9ARM.ch09.html: up to 1.4 	external/bsd/bind/dist/doc/arm/Bv9ARM.ch10.html: up to 1.1.1.13 	external/bsd/bind/dist/doc/arm/Bv9ARM.ch11.html: up to 1.1.1.2 	external/bsd/bind/dist/doc/arm/Bv9ARM.ch12.html: up to 1.1.1.2 	external/bsd/bind/dist/doc/arm/Bv9ARM.ch13.html: up to 1.1.1.2 	external/bsd/bind/dist/doc/arm/Bv9ARM.html: up to 1.4 	external/bsd/bind/dist/doc/arm/Bv9ARM.pdf: up to 1.10 	external/bsd/bind/dist/doc/arm/man.arpaname.html: up to 1.4 	external/bsd/bind/dist/doc/arm/man.ddns-confgen.html: up to 1.4 	external/bsd/bind/dist/doc/arm/man.delv.html: up to 1.4 	external/bsd/bind/dist/doc/arm/man.dig.html: up to 1.4 	external/bsd/bind/dist/doc/arm/man.dnssec-checkds.html: up to 1.4 	external/bsd/bind/dist/doc/arm/man.dnssec-coverage.html: up to 1.4 	external/bsd/bind/dist/doc/arm/man.dnssec-dsfromkey.html: up to 1.4 	external/bsd/bind/dist/doc/arm/man.dnssec-importkey.html: up to 1.4 	external/bsd/bind/dist/doc/arm/man.dnssec-keyfromlabel.html: up to 1.4 	external/bsd/bind/dist/doc/arm/man.dnssec-keygen.html: up to 1.4 	external/bsd/bind/dist/doc/arm/man.dnssec-revoke.html: up to 1.4 	external/bsd/bind/dist/doc/arm/man.dnssec-settime.html: up to 1.4 	external/bsd/bind/dist/doc/arm/man.dnssec-signzone.html: up to 1.4 	external/bsd/bind/dist/doc/arm/man.dnssec-verify.html: up to 1.4 	external/bsd/bind/dist/doc/arm/man.genrandom.html: up to 1.4 	external/bsd/bind/dist/doc/arm/man.host.html: up to 1.4 	external/bsd/bind/dist/doc/arm/man.isc-hmac-fixup.html: up to 1.4 	external/bsd/bind/dist/doc/arm/man.named-checkconf.html: up to 1.4 	external/bsd/bind/dist/doc/arm/man.named-checkzone.html: up to 1.4 	external/bsd/bind/dist/doc/arm/man.named-journalprint.html: up to 1.4 	external/bsd/bind/dist/doc/arm/man.named-rrchecker.html: up to 1.4 	external/bsd/bind/dist/doc/arm/man.named.html: up to 1.4 	external/bsd/bind/dist/doc/arm/man.nsec3hash.html: up to 1.4 	external/bsd/bind/dist/doc/arm/man.nsupdate.html: up to 1.4 	external/bsd/bind/dist/doc/arm/man.rndc-confgen.html: up to 1.4 	external/bsd/bind/dist/doc/arm/man.rndc.conf.html: up to 1.4 	external/bsd/bind/dist/doc/arm/man.rndc.html: up to 1.4 	external/bsd/bind/dist/doc/arm/notes.html: up to 1.1.1.2 	external/bsd/bind/dist/doc/arm/notes.pdf: up to 1.1.1.2 	external/bsd/bind/dist/doc/arm/notes.xml: up to 1.1.1.2 	external/bsd/bind/dist/lib/dns/api: up to 1.4 	external/bsd/bind/dist/lib/dns/tkey.c: up to 1.9 	external/bsd/bind/dist/srcid: up to 1.10 	external/bsd/bind/dist/version: up to 1.14 Update BIND to 9.10.2-P3, addressing CVE-2015-5477.

--- external/bsd/bind/dist/lib/dns/tkey.c
+++ external/bsd/bind/dist/lib/dns/tkey.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: tkey.c,v 1.6.4.1 2015/07/17 04:31:33 snj Exp $	*/
+/*	$NetBSD: tkey.c,v 1.6.4.2 2015/07/31 16:59:45 snj Exp $	*/
 
 /*
  * Copyright (C) 2004-2015  Internet Systems Consortium, Inc. ("ISC")
@@ -657,6 +657,7 @@ dns_tkey_processquery(dns_message_t *msg, dns_tkeyctx_t *tctx,
 		 * Try the answer section, since that's where Win2000
 		 * puts it.
 		 */
+		name = NULL;
 		if (dns_message_findname(msg, DNS_SECTION_ANSWER, qname,
 					 dns_rdatatype_tkey, 0, &name,
 					 &tkeyset) != ISC_R_SUCCESS) {
