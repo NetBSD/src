96d298821e9702af2a81266fadf8a3499739aee5
maxv <maxv@NetBSD.org>
2015-08-05 15:58:01 +0000

    stupid comment, and make sure we are not executing a lib

--- sys/kern/exec_elf.c
+++ sys/kern/exec_elf.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: exec_elf.c,v 1.74 2015/08/04 18:28:09 maxv Exp $	*/
+/*	$NetBSD: exec_elf.c,v 1.75 2015/08/05 15:58:01 maxv Exp $	*/
 
 /*-
  * Copyright (c) 1994, 2000, 2005 The NetBSD Foundation, Inc.
@@ -57,7 +57,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(1, "$NetBSD: exec_elf.c,v 1.74 2015/08/04 18:28:09 maxv Exp $");
+__KERNEL_RCSID(1, "$NetBSD: exec_elf.c,v 1.75 2015/08/05 15:58:01 maxv Exp $");
 
 #ifdef _KERNEL_OPT
 #include "opt_pax.h"
@@ -646,10 +646,7 @@ exec_elf_makecmds(struct lwp *l, struct exec_package *epp)
 		return error;
 
 	if (eh->e_type == ET_DYN)
-		/*
-		 * XXX allow for executing shared objects. It seems silly
-		 * but other ELF-based systems allow it as well.
-		 */
+		/* PIE */
 		is_dyn = true;
 	else if (eh->e_type != ET_EXEC)
 		return ENOEXEC;
@@ -693,6 +690,12 @@ exec_elf_makecmds(struct lwp *l, struct exec_package *epp)
 			}
 			break;
 		}
+	}
+
+	if (is_dyn && (interp == NULL)) {
+		/* PIEs must have an interpreter */
+		error = ENOEXEC;
+		goto bad;
 	}
 
 	/*
