14dcf9ff75e61fc83aaa7c336ae702eae1eff4f3
ozaki-r <ozaki-r@NetBSD.org>
2015-08-13 10:22:21 +0000

    Reflect a fix on rt_refcnt
    The test was adjusted based on wrong behavior.

--- tests/net/arp/t_arp.sh
+++ tests/net/arp/t_arp.sh
@@ -1,4 +1,4 @@
-#	$NetBSD: t_arp.sh,v 1.7 2015/08/07 05:32:12 ozaki-r Exp $
+#	$NetBSD: t_arp.sh,v 1.8 2015/08/13 10:22:21 ozaki-r Exp $
 #
 # Copyright (c) 2015 The NetBSD Foundation, Inc.
 # All rights reserved.
@@ -140,8 +140,7 @@ test_cache_expiration()
 	$DEBUG && rump.arp -n -a
 	atf_check -s exit:0 -o ignore rump.arp -n $IP4SRC
 	# Should be expired
-	#atf_check -s not-exit:0 -e ignore rump.arp -n $IP4DST
-	atf_check -s exit:0 -o match:'incomplete' rump.arp -n $IP4DST
+	atf_check -s not-exit:0 -e ignore rump.arp -n $IP4DST
 }
 
 cache_expiration_5s_body()
@@ -227,7 +226,7 @@ command_body()
 	# Hm? the cache doesn't expire...
 	atf_check -s exit:0 sleep $(($arp_keep + $arp_prune + $bonus))
 	$DEBUG && rump.arp -n -a
-	#atf_check -s exit:0 -o match:'incomplete' rump.arp -n 10.0.1.10
+	#atf_check -s not-exit:0 -e ignore rump.arp -n 10.0.1.10
 
 	return 0
 }
