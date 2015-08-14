71a7dda67fca3d2226f0489813d111bf4ca757d2
msaitoh <msaitoh@NetBSD.org>
2015-08-14 05:59:38 +0000

    Pull up following revision(s) (requested by spz in ticket #1976):
    crypto/dist/openssl/e_os2.h			patch
    crypto/dist/openssl/crypto/asn1/a_int.c		patch
    crypto/dist/openssl/crypto/asn1/tasn_new.c	patch
    crypto/dist/openssl/crypto/bn/bn.h		patch
    crypto/dist/openssl/crypto/bn/bn_err.c		patch
    crypto/dist/openssl/crypto/bn/bn_print.c	patch
    crypto/dist/openssl/crypto/bn/bn_rand.c		patch
    crypto/dist/openssl/crypto/bn/bn_shift.c	patch
    crypto/dist/openssl/crypto/cms/cms_smime.c	patch
    crypto/dist/openssl/crypto/ec/ec2_smpl.c	patch
    crypto/dist/openssl/crypto/ec/ec_check.c	patch
    crypto/dist/openssl/crypto/ec/ec_key.c		patch
    crypto/dist/openssl/crypto/ec/ec_lib.c		patch
    crypto/dist/openssl/crypto/ec/ecp_smpl.c	patch
    crypto/dist/openssl/crypto/ec/ectest.c		patch
    crypto/dist/openssl/crypto/ecdsa/ecdsatest.c	patch
    crypto/dist/openssl/crypto/ecdsa/ecs_ossl.c	patch
    crypto/dist/openssl/crypto/objects/obj_dat.c	patch
    crypto/dist/openssl/crypto/ocsp/ocsp_vfy.c	patch
    crypto/dist/openssl/crypto/pem/pem_pk8.c	patch
    crypto/dist/openssl/crypto/pkcs7/pk7_doit.c	patch
    crypto/dist/openssl/crypto/x509/x509_lu.c	patch
    crypto/dist/openssl/crypto/x509/x509_vfy.c	patch
    crypto/dist/openssl/doc/crypto/BN_rand.pod	patch
    crypto/dist/openssl/doc/crypto/BN_set_bit.pod	patch
    crypto/dist/openssl/doc/crypto/pem.pod		patch
    crypto/dist/openssl/ssl/d1_lib.c		patch
    crypto/dist/openssl/ssl/s3_clnt.c		patch
    crypto/dist/openssl/ssl/s3_srvr.c		patch
    crypto/dist/openssl/ssl/ssl.h			patch
    crypto/dist/openssl/ssl/ssl_err.c		patch
    crypto/dist/openssl/ssl/ssl_lib.c		patch
    crypto/dist/openssl/ssl/ssl_locl.h		patch
    crypto/dist/openssl/ssl/ssl_sess.c		patch
    crypto/dist/openssl/util/mkerr.pl		patch
    
    	This change covers the vulnerabilities relevant to netbsd-5 from the
    	June OpenSSL advisory, and also fixes a regression introduced with the
    	POODLE fix in October last year that caused the SSL server side	to
    	fail to handshake.

--- crypto/dist/openssl/crypto/x509/x509_lu.c
+++ crypto/dist/openssl/crypto/x509/x509_lu.c
@@ -207,6 +207,8 @@ X509_STORE *X509_STORE_new(void)
 
 static void cleanup(X509_OBJECT *a)
 	{
+	if (!a)
+		return;
 	if (a->type == X509_LU_X509)
 		{
 		X509_free(a->data.x509);
--- crypto/dist/openssl/ssl/d1_lib.c
+++ crypto/dist/openssl/ssl/d1_lib.c
@@ -125,13 +125,18 @@ int dtls1_new(SSL *s)
 
 void dtls1_free(SSL *s)
 	{
-    pitem *item = NULL;
-    hm_fragment *frag = NULL;
+	pitem *item = NULL;
+	hm_fragment *frag = NULL;
+	DTLS1_RECORD_DATA *rdata;
 
 	ssl3_free(s);
 
     while( (item = pqueue_pop(s->d1->unprocessed_rcds.q)) != NULL)
         {
+	rdata = (DTLS1_RECORD_DATA *)item->data;
+        if (rdata->rbuf.buf) {
+            OPENSSL_free(rdata->rbuf.buf);
+        }
         OPENSSL_free(item->data);
         pitem_free(item);
         }
@@ -139,6 +144,10 @@ void dtls1_free(SSL *s)
 
     while( (item = pqueue_pop(s->d1->processed_rcds.q)) != NULL)
         {
+	rdata = (DTLS1_RECORD_DATA *)item->data;
+        if (rdata->rbuf.buf) {
+            OPENSSL_free(rdata->rbuf.buf);
+        }
         OPENSSL_free(item->data);
         pitem_free(item);
         }
