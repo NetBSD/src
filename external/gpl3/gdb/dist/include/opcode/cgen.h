3aab8d4422f971d82042fd824613980dc5a99b0a
christos <christos@NetBSD.org>
2015-08-15 10:24:36 +0000

    import new gdb

--- external/gpl3/gdb/dist/include/opcode/cgen.h
+++ external/gpl3/gdb/dist/include/opcode/cgen.h
@@ -1,7 +1,6 @@
 /* Header file for targets using CGEN: Cpu tools GENerator.
 
-   Copyright 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2005, 2009, 2010
-   Free Software Foundation, Inc.
+   Copyright (C) 1996-2015 Free Software Foundation, Inc.
 
    This file is part of GDB, the GNU debugger, and the GNU Binutils.
 
