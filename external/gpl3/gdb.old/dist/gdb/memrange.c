111afa6c5be2cef157533aa5bdc314dc7908e906
christos <christos@NetBSD.org>
2015-08-15 09:27:49 +0000

    import the head of gdb in gdb.old in preparation for the new gdb import.

---
+++ external/gpl3/gdb.old/dist/gdb/memrange.c
@@ -0,0 +1,88 @@
+/* Memory ranges
+
+   Copyright (C) 2010-2014 Free Software Foundation, Inc.
+
+   This file is part of GDB.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+#include "defs.h"
+#include "memrange.h"
+
+int
+mem_ranges_overlap (CORE_ADDR start1, int len1,
+		    CORE_ADDR start2, int len2)
+{
+  ULONGEST h, l;
+
+  l = max (start1, start2);
+  h = min (start1 + len1, start2 + len2);
+  return (l < h);
+}
+
+/* qsort comparison function, that compares mem_ranges.  Ranges are
+   sorted in ascending START order.  */
+
+static int
+compare_mem_ranges (const void *ap, const void *bp)
+{
+  const struct mem_range *r1 = ap;
+  const struct mem_range *r2 = bp;
+
+  if (r1->start > r2->start)
+    return 1;
+  else if (r1->start < r2->start)
+    return -1;
+  else
+    return 0;
+}
+
+void
+normalize_mem_ranges (VEC(mem_range_s) *ranges)
+{
+  /* This function must not use any VEC operation on RANGES that
+     reallocates the memory block as that invalidates the RANGES
+     pointer, which callers expect to remain valid.  */
+
+  if (!VEC_empty (mem_range_s, ranges))
+    {
+      struct mem_range *ra, *rb;
+      int a, b;
+
+      qsort (VEC_address (mem_range_s, ranges),
+	     VEC_length (mem_range_s, ranges),
+	     sizeof (mem_range_s),
+	     compare_mem_ranges);
+
+      a = 0;
+      ra = VEC_index (mem_range_s, ranges, a);
+      for (b = 1; VEC_iterate (mem_range_s, ranges, b, rb); b++)
+	{
+	  /* If mem_range B overlaps or is adjacent to mem_range A,
+	     merge them.  */
+	  if (rb->start <= ra->start + ra->length)
+	    {
+	      ra->length = max (ra->length,
+				(rb->start - ra->start) + rb->length);
+	      continue;		/* next b, same a */
+	    }
+	  a++;			/* next a */
+	  ra = VEC_index (mem_range_s, ranges, a);
+
+	  if (a != b)
+	    *ra = *rb;
+	}
+      VEC_truncate (mem_range_s, ranges, a + 1);
+    }
+}
---
+++ external/gpl3/gdb.old/dist/gdb/testsuite/gdb.base/gnu_vector.c
@@ -0,0 +1,63 @@
+/* This testcase is part of GDB, the GNU debugger.
+
+   Copyright 2010-2014 Free Software Foundation, Inc.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+   Contributed by Ken Werner <ken.werner@de.ibm.com>  */
+
+typedef int __attribute__ ((vector_size (4 * sizeof(int)))) int4;
+typedef unsigned int __attribute__ ((vector_size (4 * sizeof(unsigned int)))) uint4;
+typedef char __attribute__ ((vector_size (4 * sizeof(char)))) char4;
+typedef float __attribute__ ((vector_size (4 * sizeof(float)))) float4;
+
+typedef int __attribute__ ((vector_size (2 * sizeof(int)))) int2;
+typedef long long __attribute__ ((vector_size (2 * sizeof(long long)))) longlong2;
+typedef float __attribute__ ((vector_size (2 * sizeof(float)))) float2;
+typedef double __attribute__ ((vector_size (2 * sizeof(double)))) double2;
+
+int ia = 2;
+int ib = 1;
+float fa = 2;
+float fb = 1;
+long long lla __attribute__ ((mode(DI))) = 0x0000000100000001ll;
+char4 c4 = {1, 2, 3, 4};
+int4 i4a = {2, 4, 8, 16};
+int4 i4b = {1, 2, 8, 4};
+float4 f4a = {2, 4, 8, 16};
+float4 f4b = {1, 2, 8, 4};
+uint4 ui4 = {2, 4, 8, 16};
+int2 i2 = {1, 2};
+longlong2 ll2 = {1, 2};
+float2 f2 = {1, 2};
+double2 d2 = {1, 2};
+
+union
+{
+  int i;
+  char cv __attribute__ ((vector_size (sizeof (int))));
+} union_with_vector_1;
+
+struct
+{
+  int i;
+  char cv __attribute__ ((vector_size (sizeof (int))));
+  float4 f4;
+} struct_with_vector_1;
+
+int
+main ()
+{
+  return 0;
+}
