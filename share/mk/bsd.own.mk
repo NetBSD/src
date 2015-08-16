d0cb7a9501d12f653f6963ac6f149e7073c507c8
christos <christos@NetBSD.org>
2015-08-16 09:13:30 +0000

    switch amd64 to gdb 7.9.1

--- share/mk/bsd.own.mk
+++ share/mk/bsd.own.mk
@@ -1,4 +1,4 @@
-#	$NetBSD: bsd.own.mk,v 1.862 2015/07/23 08:03:26 mrg Exp $
+#	$NetBSD: bsd.own.mk,v 1.863 2015/08/16 09:13:30 christos Exp $
 
 # This needs to be before bsd.init.mk
 .if defined(BSD_MK_COMPAT_FILE)
@@ -111,7 +111,11 @@ HAVE_LIBGCC_EH?=	no
 HAVE_LIBGCC_EH?=	yes
 .endif
 
+.if (${MACHINE_ARCH} == "x86_64")
+HAVE_GDB?=	79
+.else
 HAVE_GDB?=	7
+.endif
 
 .if (${MACHINE_ARCH} == "alpha") || \
     (${MACHINE_ARCH} == "hppa") || \
