86397ff33529413b4054959b88f933d1b730acce
christos <christos@NetBSD.org>
2015-08-17 08:54:55 +0000

    add sparc

--- share/mk/bsd.own.mk
+++ share/mk/bsd.own.mk
@@ -1,4 +1,4 @@
-#	$NetBSD: bsd.own.mk,v 1.864 2015/08/17 08:31:45 christos Exp $
+#	$NetBSD: bsd.own.mk,v 1.865 2015/08/17 08:54:55 christos Exp $
 
 # This needs to be before bsd.init.mk
 .if defined(BSD_MK_COMPAT_FILE)
@@ -112,6 +112,7 @@ HAVE_LIBGCC_EH?=	yes
 .endif
 
 .if (${MACHINE_ARCH} == "x86_64") || \
+    (${MACHINE_ARCH} == "sparc") || \
     (${MACHINE_ARCH} == "i386")
 HAVE_GDB?=	79
 .else
