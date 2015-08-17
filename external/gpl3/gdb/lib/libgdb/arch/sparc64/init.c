c6bb8cabfc5ecce706b992c4a86b5c527b55d6f5
christos <christos@NetBSD.org>
2015-08-17 10:07:00 +0000

    regen sparc64

--- external/gpl3/gdb/lib/libgdb/arch/sparc64/init.c
+++ external/gpl3/gdb/lib/libgdb/arch/sparc64/init.c
@@ -25,6 +25,8 @@ extern initialize_file_ftype _initialize_tracepoint;
 extern initialize_file_ftype _initialize_ax_gdb;
 extern initialize_file_ftype _initialize_notif;
 extern initialize_file_ftype _initialize_ctf;
+extern initialize_file_ftype _initialize_tracefile;
+extern initialize_file_ftype _initialize_tracefile_tfile;
 extern initialize_file_ftype _initialize_elfread;
 extern initialize_file_ftype _initialize_stap_probe;
 extern initialize_file_ftype _initialize_annotate;
@@ -55,6 +57,7 @@ extern initialize_file_ftype _initialize_infcmd;
 extern initialize_file_ftype _initialize_infrun;
 extern initialize_file_ftype _initialize_stack;
 extern initialize_file_ftype _initialize_thread;
+extern initialize_file_ftype _initialize_extension;
 extern initialize_file_ftype _initialize_filesystem;
 extern initialize_file_ftype _initialize_interpreter;
 extern initialize_file_ftype _initialize_macrocmd;
@@ -140,6 +143,7 @@ extern initialize_file_ftype _initialize_cli_interp;
 extern initialize_file_ftype _initialize_mi_cmds;
 extern initialize_file_ftype _initialize_mi_cmd_env;
 extern initialize_file_ftype _initialize_mi_interp;
+extern initialize_file_ftype _initialize_mi_main;
 extern initialize_file_ftype _initialize_tui_hooks;
 extern initialize_file_ftype _initialize_tui_interp;
 extern initialize_file_ftype _initialize_tui_layout;
@@ -148,6 +152,8 @@ extern initialize_file_ftype _initialize_tui_regs;
 extern initialize_file_ftype _initialize_tui_stack;
 extern initialize_file_ftype _initialize_tui_win;
 extern initialize_file_ftype _initialize_python;
+extern initialize_file_ftype _initialize_guile;
+extern initialize_file_ftype _initialize_compile;
 void
 initialize_all_files (void)
 {
@@ -170,6 +176,8 @@ initialize_all_files (void)
   _initialize_ax_gdb ();
   _initialize_notif ();
   _initialize_ctf ();
+  _initialize_tracefile ();
+  _initialize_tracefile_tfile ();
   _initialize_elfread ();
   _initialize_stap_probe ();
   _initialize_annotate ();
@@ -200,6 +208,7 @@ initialize_all_files (void)
   _initialize_infrun ();
   _initialize_stack ();
   _initialize_thread ();
+  _initialize_extension ();
   _initialize_filesystem ();
   _initialize_interpreter ();
   _initialize_macrocmd ();
@@ -285,6 +294,7 @@ initialize_all_files (void)
   _initialize_mi_cmds ();
   _initialize_mi_cmd_env ();
   _initialize_mi_interp ();
+  _initialize_mi_main ();
   _initialize_tui_hooks ();
   _initialize_tui_interp ();
   _initialize_tui_layout ();
@@ -293,4 +303,6 @@ initialize_all_files (void)
   _initialize_tui_stack ();
   _initialize_tui_win ();
   _initialize_python ();
+  _initialize_guile ();
+  _initialize_compile ();
 }
