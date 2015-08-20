66dd7fb98bcc220195ce01de2a2da243846e26cd
christos <christos@NetBSD.org>
2015-08-20 12:20:09 +0000

    add ioconf files for pseudo device attach prototypes

--- sys/rump/net/lib/libtap/tap_component.c
+++ sys/rump/net/lib/libtap/tap_component.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: tap_component.c,v 1.1 2015/05/29 12:32:23 pooka Exp $	*/
+/*	$NetBSD: tap_component.c,v 1.2 2015/08/20 12:20:09 christos Exp $	*/
 
 /*
  * Copyright (c) 2015 Wei Liu.  All Rights Reserved.
@@ -26,7 +26,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: tap_component.c,v 1.1 2015/05/29 12:32:23 pooka Exp $");
+__KERNEL_RCSID(0, "$NetBSD: tap_component.c,v 1.2 2015/08/20 12:20:09 christos Exp $");
 
 #include <sys/param.h>
 #include <sys/device.h>
@@ -36,9 +36,9 @@ __KERNEL_RCSID(0, "$NetBSD: tap_component.c,v 1.1 2015/05/29 12:32:23 pooka Exp 
 #include "rump_net_private.h"
 #include "rump_vfs_private.h"
 
-CFDRIVER_DECL(tap, DV_IFNET, NULL);
+#include "ioconf.h"
 
-void tapattach(int);
+CFDRIVER_DECL(tap, DV_IFNET, NULL);
 
 RUMP_COMPONENT(RUMP_COMPONENT_NET_IF)
 {
