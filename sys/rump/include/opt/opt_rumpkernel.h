52e9895b48e97ffb57d59068b28ddd8730c39014
pooka <pooka@NetBSD.org>
2015-08-21 12:39:07 +0000

    Move constant cpp macros from Makefile.rump into opt_rumpkernel.h (makes cc invocation lines a bit shorter)

---
+++ sys/rump/include/opt/opt_rumpkernel.h
@@ -0,0 +1,14 @@
+/*	$NetBSD: opt_rumpkernel.h,v 1.1 2015/08/21 12:39:07 pooka Exp $	*/
+
+#ifndef __NetBSD__
+#define __NetBSD__
+#endif
+
+#define _KERNEL 1
+#define _MODULE 1
+
+#define MODULAR 1
+#define MULTIPROCESSOR 1
+#define MAXUSERS 32
+
+#define DEBUGPRINT
