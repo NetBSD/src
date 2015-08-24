d0b433d7a728b4bc04220cec4d48e0654d38d4f2
pooka <pooka@NetBSD.org>
2015-08-24 22:25:50 +0000

    Remove a bunch of opt files.
    It's a cute idea to have component-specific opt files, but also a
    completely stupid one since there's no way of knowing how options
    transcend component boundaries, and therefore if a set of options is
    conflicting or not.  So, just continue concentrating all of opt_foo.h in
    opt_rumpkernel.h in accordance with the monolithic opt model.

--- sys/rump/librump/rumpkern/opt/rnd.h
+++
@@ -1,2 +0,0 @@
-/*	$NetBSD: rnd.h,v 1.1 2009/01/04 20:30:21 pooka Exp $	*/
-#define NRND 0 /* XXXbad */
--- sys/rump/net/lib/libnetinet/opt/opt_wlan.h
+++
@@ -1,3 +0,0 @@
-/*	$NetBSD: opt_wlan.h,v 1.1 2014/07/30 13:32:09 ozaki-r Exp $	*/
-
-/* dummy */
