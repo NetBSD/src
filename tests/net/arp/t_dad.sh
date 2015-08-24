e4b62fc31542573370bc16027eba08cbb9d02fbf
ozaki-r <ozaki-r>
2015-08-24 02:02:25 +0000

    Disable another tentative state check
    It's too ephemeral to check robustly.

--- tests/net/arp/t_dad.sh
+++ tests/net/arp/t_dad.sh
@@ -1,4 +1,4 @@
-#	$NetBSD: t_dad.sh,v 1.4 2015/08/17 07:06:58 ozaki-r Exp $
+#	$NetBSD: t_dad.sh,v 1.5 2015/08/24 02:02:25 ozaki-r Exp $
 #
 # Copyright (c) 2015 The NetBSD Foundation, Inc.
 # All rights reserved.
@@ -139,7 +139,8 @@ dad_basic_body()
 	atf_check -s exit:0 rump.ifconfig shmif0 inet 10.0.0.3/24 alias
 
 	# The new address starts with tentative state
-	atf_check -s exit:0 -x "rump.ifconfig shmif0 |grep 10.0.0.3 |grep -q tentative"
+	# XXX we have no stable way to check this, so skip for now
+	#atf_check -s exit:0 -x "rump.ifconfig shmif0 |grep 10.0.0.3 |grep -q tentative"
 
 	# Check DAD probe packets
 	atf_check -s exit:0 sleep 2
