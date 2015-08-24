4ff49cdfe202e7a15c388af8e8ccab6ff39a1d3d
pooka <pooka@NetBSD.org>
2015-08-24 22:25:50 +0000

    Remove a bunch of opt files.
    It's a cute idea to have component-specific opt files, but also a
    completely stupid one since there's no way of knowing how options
    transcend component boundaries, and therefore if a set of options is
    conflicting or not.  So, just continue concentrating all of opt_foo.h in
    opt_rumpkernel.h in accordance with the monolithic opt model.

--- sys/rump/librump/rumpnet/opt/opt_inet.h
+++
@@ -1,4 +0,0 @@
-/*	$NetBSD: opt_inet.h,v 1.4 2015/01/18 17:33:08 justin Exp $	*/
-
-#define INET  1
-#define INET6 1
--- sys/rump/net/lib/libnet/opt/opt_net_mpsafe.h
+++
@@ -1,3 +0,0 @@
-/*	$NetBSD: opt_net_mpsafe.h,v 1.1 2015/04/03 07:56:54 ozaki-r Exp $	*/
-
-/* dummy */
