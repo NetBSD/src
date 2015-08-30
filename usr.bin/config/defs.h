08a08c9a02e5d7a1ae0eb3b0612b85263d71b6f3
uebayasi <uebayasi@NetBSD.org>
2015-08-30 21:58:19 +0000

    Revert "Accept only relative paths ...".  This will be redone in much better, stricter way.

--- usr.bin/config/defs.h
+++ usr.bin/config/defs.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: defs.h,v 1.72 2015/08/30 05:12:00 uebayasi Exp $	*/
+/*	$NetBSD: defs.h,v 1.73 2015/08/30 21:58:19 uebayasi Exp $	*/
 
 /*
  * Copyright (c) 1992, 1993
@@ -107,7 +107,7 @@ extern const char *progname;
  * The next two lines define the current version of the config(1) binary,
  * and the minimum version of the configuration files it supports.
  */
-#define CONFIG_VERSION		20150832
+#define CONFIG_VERSION		20150833
 #define CONFIG_MINVERSION	0
 
 /*
