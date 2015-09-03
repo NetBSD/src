2a2e390f89f3bca8c04cf662e93efe14de04f38e
christos <christos@NetBSD.org>
2015-09-03 07:21:32 +0000

    Import bind 9.10.2-P4

--- external/bsd/bind/dist/lib/dns/pkcs11rsa_link.c
+++ external/bsd/bind/dist/lib/dns/pkcs11rsa_link.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: pkcs11rsa_link.c,v 1.1.1.4 2014/12/10 03:34:40 christos Exp $	*/
+/*	$NetBSD: pkcs11rsa_link.c,v 1.1.1.5 2015/09/03 07:21:37 christos Exp $	*/
 
 /*
  * Copyright (C) 2014  Internet Systems Consortium, Inc. ("ISC")
@@ -793,23 +793,21 @@ pkcs11rsa_fromdns(dst_key_t *key, isc_buffer_t *data) {
 	unsigned int e_bytes, mod_bytes;
 	CK_BYTE *exponent = NULL, *modulus = NULL;
 	CK_ATTRIBUTE *attr;
+	unsigned int length;
 
 	isc_buffer_remainingregion(data, &r);
 	if (r.length == 0)
 		return (ISC_R_SUCCESS);
+	length = r.length;
 
 	rsa = (pk11_object_t *) isc_mem_get(key->mctx, sizeof(*rsa));
 	if (rsa == NULL)
 		return (ISC_R_NOMEMORY);
+
 	memset(rsa, 0, sizeof(*rsa));
 
-	if (r.length < 1) {
-		memset(rsa, 0, sizeof(*rsa));
-		isc_mem_put(key->mctx, rsa, sizeof(*rsa));
-		return (DST_R_INVALIDPUBLICKEY);
-	}
-	e_bytes = *r.base++;
-	r.length--;
+	e_bytes = *r.base;
+	isc_region_consume(&r, 1);
 
 	if (e_bytes == 0) {
 		if (r.length < 2) {
@@ -817,9 +815,10 @@ pkcs11rsa_fromdns(dst_key_t *key, isc_buffer_t *data) {
 			isc_mem_put(key->mctx, rsa, sizeof(*rsa));
 			return (DST_R_INVALIDPUBLICKEY);
 		}
-		e_bytes = ((*r.base++) << 8);
-		e_bytes += *r.base++;
-		r.length -= 2;
+		e_bytes = (*r.base) << 8;
+		isc_region_consume(&r, 1);
+		e_bytes += *r.base;
+		isc_region_consume(&r, 1);
 	}
 
 	if (r.length < e_bytes) {
@@ -828,14 +827,13 @@ pkcs11rsa_fromdns(dst_key_t *key, isc_buffer_t *data) {
 		return (DST_R_INVALIDPUBLICKEY);
 	}
 	exponent = r.base;
-	r.base += e_bytes;
-	r.length -= e_bytes;
+	isc_region_consume(&r, e_bytes);
 	modulus = r.base;
 	mod_bytes = r.length;
 
 	key->key_size = pk11_numbits(modulus, mod_bytes);
 
-	isc_buffer_forward(data, r.length);
+	isc_buffer_forward(data, length);
 
 	rsa->repr = (CK_ATTRIBUTE *) isc_mem_get(key->mctx, sizeof(*attr) * 2);
 	if (rsa->repr == NULL)
