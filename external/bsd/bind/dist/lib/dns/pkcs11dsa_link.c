64f4a829a4c7caf388f5d2338bd2a918886ecf2f
christos <christos>
2015-09-03 07:21:32 +0000

    Import bind 9.10.2-P4

--- external/bsd/bind/dist/lib/dns/pkcs11dsa_link.c
+++ external/bsd/bind/dist/lib/dns/pkcs11dsa_link.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: pkcs11dsa_link.c,v 1.1.1.4 2014/12/10 03:34:40 christos Exp $	*/
+/*	$NetBSD: pkcs11dsa_link.c,v 1.1.1.5 2015/09/03 07:21:36 christos Exp $	*/
 
 /*
  * Copyright (C) 2014  Internet Systems Consortium, Inc. ("ISC")
@@ -390,6 +390,7 @@ pkcs11dsa_sign(dst_context_t *dctx, isc_buffer_t *sig) {
 	isc_region_t r;
 	pk11_context_t *pk11_ctx = dctx->ctxdata.pk11_ctx;
 	isc_result_t ret = ISC_R_SUCCESS;
+	unsigned int klen;
 
 	isc_buffer_availableregion(sig, &r);
 	if (r.length < ISC_SHA1_DIGESTLENGTH * 2 + 1)
@@ -401,7 +402,10 @@ pkcs11dsa_sign(dst_context_t *dctx, isc_buffer_t *sig) {
 	if (siglen != ISC_SHA1_DIGESTLENGTH * 2)
 		return (DST_R_SIGNFAILURE);
 
-	*r.base = (dctx->key->key_size - 512)/64;
+	klen = (dctx->key->key_size - 512)/64;
+	if (klen > 255)
+		return (ISC_R_FAILURE);
+	*r.base = klen;
 	isc_buffer_add(sig, ISC_SHA1_DIGESTLENGTH * 2 + 1);
 
     err:
@@ -746,23 +750,25 @@ pkcs11dsa_todns(const dst_key_t *key, isc_buffer_t *data) {
 		return (ISC_R_NOSPACE);
 
 	memset(r.base, 0, dnslen);
-	*r.base++ = t;
+	*r.base = t;
+	isc_region_consume(&r, 1);
+
 	cp = (CK_BYTE *) subprime->pValue;
 	memmove(r.base + ISC_SHA1_DIGESTLENGTH - subprime->ulValueLen,
 		cp, subprime->ulValueLen);
-	r.base += ISC_SHA1_DIGESTLENGTH;
+	isc_region_consume(&r, ISC_SHA1_DIGESTLENGTH);
 	cp = (CK_BYTE *) prime->pValue;
 	memmove(r.base + key->key_size/8 - prime->ulValueLen,
 		cp, prime->ulValueLen);
-	r.base += p_bytes;
+	isc_region_consume(&r, p_bytes);
 	cp = (CK_BYTE *) base->pValue;
 	memmove(r.base + key->key_size/8 - base->ulValueLen,
 		cp, base->ulValueLen);
-	r.base += p_bytes;
+	isc_region_consume(&r, p_bytes);
 	cp = (CK_BYTE *) pub_key->pValue;
 	memmove(r.base + key->key_size/8 - pub_key->ulValueLen,
 		cp, pub_key->ulValueLen);
-	r.base += p_bytes;
+	isc_region_consume(&r, p_bytes);
 
 	isc_buffer_add(data, dnslen);
 
@@ -786,7 +792,8 @@ pkcs11dsa_fromdns(dst_key_t *key, isc_buffer_t *data) {
 		return (ISC_R_NOMEMORY);
 	memset(dsa, 0, sizeof(*dsa));
 
-	t = (unsigned int) *r.base++;
+	t = (unsigned int) *r.base;
+	isc_region_consume(&r, 1);
 	if (t > 8) {
 		memset(dsa, 0, sizeof(*dsa));
 		isc_mem_put(key->mctx, dsa, sizeof(*dsa));
@@ -794,23 +801,23 @@ pkcs11dsa_fromdns(dst_key_t *key, isc_buffer_t *data) {
 	}
 	p_bytes = 64 + 8 * t;
 
-	if (r.length < 1 + ISC_SHA1_DIGESTLENGTH + 3 * p_bytes) {
+	if (r.length < ISC_SHA1_DIGESTLENGTH + 3 * p_bytes) {
 		memset(dsa, 0, sizeof(*dsa));
 		isc_mem_put(key->mctx, dsa, sizeof(*dsa));
 		return (DST_R_INVALIDPUBLICKEY);
 	}
 
 	subprime = r.base;
-	r.base += ISC_SHA1_DIGESTLENGTH;
+	isc_region_consume(&r, ISC_SHA1_DIGESTLENGTH);
 
 	prime = r.base;
-	r.base += p_bytes;
+	isc_region_consume(&r, p_bytes);
 
 	base = r.base;
-	r.base += p_bytes;
+	isc_region_consume(&r, p_bytes);
 
 	pub_key = r.base;
-	r.base += p_bytes;
+	isc_region_consume(&r, p_bytes);
 
 	key->key_size = p_bytes * 8;
 
