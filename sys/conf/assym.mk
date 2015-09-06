fbf34fb15ab5aa366a745ba3cf3d0c50fbc482f5
uebayasi <uebayasi@NetBSD.org>
2015-09-06 06:13:16 +0000

    Clean up.  Move assym.h/vers.c related code out of Makefile.kern.inc.

---
+++ sys/conf/assym.mk
@@ -0,0 +1,28 @@
+# $NetBSD: assym.mk,v 1.1 2015/09/06 06:13:16 uebayasi Exp $
+
+assym.h: ${GENASSYM_CONF} ${GENASSYM_EXTRAS} $S/conf/genassym.cf
+	${_MKTARGET_CREATE}
+	cat ${GENASSYM_CONF} ${GENASSYM_EXTRAS} $S/conf/genassym.cf | \
+	    ${GENASSYM} -- ${CC} ${CFLAGS:N-Wa,*} ${CPPFLAGS} ${PROF} \
+	    ${GENASSYM_CPPFLAGS} > assym.h.tmp && \
+	mv -f assym.h.tmp assym.h
+${_MD_SFILES:C/\.[Ss]/.o/} ${_SFILES:C/\.[Ss]/.o/}: assym.h
+
+assym.d: assym.h
+	${_MKTARGET_CREATE}
+	cat ${GENASSYM_CONF} ${GENASSYM_EXTRAS} | \
+	    ${GENASSYM} -- ${MKDEP} -f assym.dep -- \
+	    ${CFLAGS:N-Wa,*} ${CPPFLAGS} ${GENASSYM_CPPFLAGS}
+	${TOOL_SED} -e 's/.*\.o:.*\.c/assym.h:/' < assym.dep >${.TARGET}
+	rm -f assym.dep
+
+DEPS+=	assym.d
+
+.if defined(___USE_SUFFIX_RULES___)
+.SUFFIXES: .genassym .assym.h
+.genassym.assym.h:
+	${_MKTARGET_CREATE}
+	${GENASSYM} -- ${CC} ${CFLAGS:N-Wa,*} ${CPPFLAGS} ${PROF} \
+	    ${GENASSYM_CPPFLAGS} < $< > $@
+	mv -f $@.tmp $@
+.endif # ___USE_SUFFIX_RULES___
