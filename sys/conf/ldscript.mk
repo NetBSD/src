ca11ee33ffd00ec4191df68dcb499a0a535a3552
uebayasi <uebayasi@NetBSD.org>
2015-09-06 15:20:59 +0000

    More build ordering.

--- sys/conf/ldscript.mk
+++ sys/conf/ldscript.mk
@@ -1,9 +1,9 @@
-# $NetBSD: ldscript.mk,v 1.1 2015/09/06 06:41:14 uebayasi Exp $
+# $NetBSD: ldscript.mk,v 1.2 2015/09/06 15:20:59 uebayasi Exp $
 
 # Give MD generated ldscript dependency on ${SYSTEM_OBJ}
 .if defined(KERNLDSCRIPT)
 .if target(${KERNLDSCRIPT})
-${KERNLDSCRIPT}: ${SYSTEM_OBJ}
+${KERNLDSCRIPT}: ${SYSTEM_OBJ:O}
 .endif
 .endif
 
