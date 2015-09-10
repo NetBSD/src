e250f63ce6b0574210ca56a5fd6dad723c8c869d
skrll <skrll@NetBSD.org>
2015-09-10 06:32:47 +0000

    Trailing whitespace.

--- sys/arch/arm/imx/imxusb.c
+++ sys/arch/arm/imx/imxusb.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: imxusb.c,v 1.9 2015/09/10 04:00:32 hkenken Exp $	*/
+/*	$NetBSD: imxusb.c,v 1.10 2015/09/10 06:32:47 skrll Exp $	*/
 /*
  * Copyright (c) 2009, 2010  Genetec Corporation.  All rights reserved.
  * Written by Hashimoto Kenichi and Hiroyuki Bessho for Genetec Corporation.
@@ -25,7 +25,7 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: imxusb.c,v 1.9 2015/09/10 04:00:32 hkenken Exp $");
+__KERNEL_RCSID(0, "$NetBSD: imxusb.c,v 1.10 2015/09/10 06:32:47 skrll Exp $");
 
 #include "opt_imx.h"
 
@@ -56,7 +56,7 @@ __KERNEL_RCSID(0, "$NetBSD: imxusb.c,v 1.9 2015/09/10 04:00:32 hkenken Exp $");
 
 static int	imxehci_match(device_t, cfdata_t, void *);
 static void	imxehci_attach(device_t, device_t, void *);
-						     
+
 uint8_t imxusb_ulpi_read(struct imxehci_softc *sc, int addr);
 void imxusb_ulpi_write(struct imxehci_softc *sc, int addr, uint8_t data);
 static void ulpi_reset(struct imxehci_softc *sc);
@@ -72,11 +72,11 @@ static int
 imxehci_match(device_t parent, cfdata_t cf, void *aux)
 {
 	struct imxusbc_attach_args *aa = aux;
-	
+
 	if (aa->aa_unit < 0 || 3 < aa->aa_unit) {
 		return 0;
 	}
-		
+
 	return 1;
 }
 
@@ -105,7 +105,7 @@ imxehci_attach(device_t parent, device_t self, void *aux)
 	aprint_normal(": i.MX USB Controller\n");
 
 	/* per unit registers */
-	if (bus_space_subregion(iot, aa->aa_ioh, 
+	if (bus_space_subregion(iot, aa->aa_ioh,
 		aa->aa_unit * IMXUSB_EHCI_SIZE, IMXUSB_EHCI_SIZE,
 		&sc->sc_ioh) ||
 	    bus_space_subregion(iot, aa->aa_ioh,
@@ -121,11 +121,11 @@ imxehci_attach(device_t parent, device_t self, void *aux)
 	hcirev = bus_space_read_2(iot, sc->sc_hsc.ioh, EHCI_HCIVERSION);
 
 	aprint_normal_dev(self,
-	    "id=%d revision=%d HCI revision=0x%x\n", 
+	    "id=%d revision=%d HCI revision=0x%x\n",
 	    (int)__SHIFTOUT(id, IMXUSB_ID_ID),
 	    (int)__SHIFTOUT(id, IMXUSB_ID_REVISION),
 	    hcirev);
-			  
+
 	hwhost = bus_space_read_4(iot, sc->sc_ioh, IMXUSB_HWHOST);
 	hwdevice = bus_space_read_4(iot, sc->sc_ioh, IMXUSB_HWDEVICE);
 
@@ -149,7 +149,7 @@ imxehci_attach(device_t parent, device_t self, void *aux)
 
 	sc->sc_hsc.sc_bus.dmatag = aa->aa_dmat;
 
-	sc->sc_hsc.sc_offs = bus_space_read_1(iot, sc->sc_hsc.ioh, 
+	sc->sc_hsc.sc_offs = bus_space_read_1(iot, sc->sc_hsc.ioh,
 	    EHCI_CAPLENGTH);
 
 	/* Platform dependent setup */
@@ -289,7 +289,7 @@ ulpi_wait(struct imxehci_softc *sc, int tout)
 }
 
 #define	TIMEOUT	100000
-						     
+
 uint8_t
 imxusb_ulpi_read(struct imxehci_softc *sc, int addr)
 {
