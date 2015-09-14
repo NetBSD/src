4fe431db41755209d1bdda786d42f894941f3fec
uebayasi <uebayasi@NetBSD.org>
2015-09-14 01:40:03 +0000

    Handle splash image generation better.

--- sys/conf/splash.mk
+++ sys/conf/splash.mk
@@ -1,7 +1,6 @@
-# $NetBSD: splash.mk,v 1.2 2015/09/11 15:56:56 nat Exp $
+# $NetBSD: splash.mk,v 1.3 2015/09/14 01:40:03 uebayasi Exp $
 
 # Option for embedding a splashscreen image.
 .if defined(SPLASHSCREEN_IMAGE) 
 .include "${S}/dev/splash/splash.mk"
-OBJS+= splash_image.o
 .endif
