bca57c01c7f9fdce0e1af1918b45d575cf9a5db5
christos <christos@NetBSD.org>
2015-09-25 16:05:17 +0000

    For processors that have memory breakpoints, add macros for them to help libproc

--- sys/arch/alpha/include/ptrace.h
+++ sys/arch/alpha/include/ptrace.h
@@ -1,4 +1,4 @@
-/* $NetBSD: ptrace.h,v 1.7 2015/09/15 15:49:02 christos Exp $ */
+/* $NetBSD: ptrace.h,v 1.8 2015/09/25 16:05:17 christos Exp $ */
 
 /*
  * Copyright (c) 1994 Christopher G. Demetriou
@@ -46,7 +46,11 @@
 	"PT_SETFPREGS",
 
 #include <machine/reg.h>
+
 #define PTRACE_REG_PC(r)	(r)->r_regs[R_ZERO]
 #define PTRACE_REG_SET_PC(r, v)	(r)->r_regs[R_ZERO] = (v)
 #define PTRACE_REG_SP(r)	(r)->r_regs[R_SP]
 #define PTRACE_REG_INTRV(r)	(r)->r_regs[R_V0]
+
+#define PTRACE_BREAKPOINT	((const uint8_t[]) { 0x80, 0x00, 0x00, 0x00 })
+#define PTRACE_BREAKPOINT_SIZE	4
