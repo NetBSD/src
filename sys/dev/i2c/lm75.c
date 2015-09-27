e0b8b7d8fa3bbe1856df6392a5c9ee33823be0d0
phx <phx@NetBSD.org>
2015-09-27 13:02:21 +0000

    Direct config: Only match via the list of compatible hardware when we have such a list available (ia_ncompat > 0). Otherwise fall back to matching the device name with ia_name.

--- sys/dev/i2c/lm75.c
+++ sys/dev/i2c/lm75.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: lm75.c,v 1.25 2012/10/27 17:18:17 chs Exp $	*/
+/*	$NetBSD: lm75.c,v 1.26 2015/09/27 13:02:21 phx Exp $	*/
 
 /*
  * Copyright (c) 2003 Wasabi Systems, Inc.
@@ -36,7 +36,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: lm75.c,v 1.25 2012/10/27 17:18:17 chs Exp $");
+__KERNEL_RCSID(0, "$NetBSD: lm75.c,v 1.26 2015/09/27 13:02:21 phx Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -132,10 +132,15 @@ lmtemp_match(device_t parent, cfdata_t cf, void *aux)
 	} else {
 		/*
 		 * Direct config - match via the list of compatible
-		 * hardware.
+		 * hardware or simply match the device name.
 		 */
-		if (iic_compat_match(ia, lmtemp_compats))
-			return 1;
+		if (ia->ia_ncompat > 0) {
+			if (iic_compat_match(ia, lmtemp_compats))
+				return 1;
+		} else {
+			if (strcmp(ia->ia_name, "lmtemp") == 0)
+				return 1;
+		}
 	}
 
 
