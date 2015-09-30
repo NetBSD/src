c29d51755812ace2e87aeefdb06cb2b4dac7087a
christos <christos@NetBSD.org>
2015-09-30 22:01:06 +0000

    Import the dtrace toolkit from FreeBSD; simple scripts such as dtruss work unmodified. For others we'll need to add the missing probes and adjust. This is not attached to the build.

---
+++ external/cddl/dtracetoolkit/dist/Bin/syscallbysysc.d
@@ -0,0 +1,10 @@
+#!/usr/sbin/dtrace -s
+/*
+ * syscallbysysc.d - report on syscalls by syscall. DTrace OneLiner.
+ *
+ * This is a DTrace OneLiner from the DTraceToolkit.
+ *
+ * $Id: syscallbysysc.d,v 1.1.1.1 2015/09/30 22:01:07 christos Exp $
+ */
+
+syscall:::entry { @num[probefunc] = count(); }
