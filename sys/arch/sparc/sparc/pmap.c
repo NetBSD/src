106e3b42b08b7bf52c5103b1d52de0100610bf9b
joerg <joerg@NetBSD.org>
2015-10-04 08:18:49 +0000

    Use pointer computation for references outside an object.

--- sys/arch/sparc/sparc/pmap.c
+++ sys/arch/sparc/sparc/pmap.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: pmap.c,v 1.358 2014/05/03 11:17:06 nakayama Exp $ */
+/*	$NetBSD: pmap.c,v 1.359 2015/10/04 08:18:49 joerg Exp $ */
 
 /*
  * Copyright (c) 1996
@@ -56,7 +56,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: pmap.c,v 1.358 2014/05/03 11:17:06 nakayama Exp $");
+__KERNEL_RCSID(0, "$NetBSD: pmap.c,v 1.359 2015/10/04 08:18:49 joerg Exp $");
 
 #include "opt_ddb.h"
 #include "opt_kgdb.h"
@@ -3214,7 +3214,7 @@ pmap_bootstrap4_4c(void *top, int nctx, int nregion, int nsegment)
 	 * above NUREG, we save storage space and can index kernel and
 	 * user regions in the same way.
 	 */
-	kernel_pmap_store.pm_regmap = &kernel_regmap_store[-NUREG];
+	kernel_pmap_store.pm_regmap = kernel_regmap_store - NUREG;
 	for (i = NKREG; --i >= 0;) {
 #if defined(SUN4_MMU3L)
 		kernel_regmap_store[i].rg_smeg = reginval;
@@ -3592,7 +3592,7 @@ pmap_bootstrap4m(void *top)
 	 * above NUREG, we save storage space and can index kernel and
 	 * user regions in the same way.
 	 */
-	kernel_pmap_store.pm_regmap = &kernel_regmap_store[-NUREG];
+	kernel_pmap_store.pm_regmap = kernel_regmap_store - NUREG;
 	memset(kernel_regmap_store, 0, NKREG * sizeof(struct regmap));
 	memset(kernel_segmap_store, 0, NKREG * NSEGRG * sizeof(struct segmap));
 	for (i = NKREG; --i >= 0;) {
