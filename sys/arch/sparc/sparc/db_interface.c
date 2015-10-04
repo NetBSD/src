042ade9fafc84910166f36d71bb4db860272ca72
joerg <joerg@NetBSD.org>
2015-10-04 08:17:03 +0000

    Arrays are never NULL.

--- sys/arch/sparc/sparc/db_interface.c
+++ sys/arch/sparc/sparc/db_interface.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: db_interface.c,v 1.90 2014/02/28 10:16:51 skrll Exp $ */
+/*	$NetBSD: db_interface.c,v 1.91 2015/10/04 08:17:03 joerg Exp $ */
 
 /*
  * Mach Operating System
@@ -33,7 +33,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: db_interface.c,v 1.90 2014/02/28 10:16:51 skrll Exp $");
+__KERNEL_RCSID(0, "$NetBSD: db_interface.c,v 1.91 2015/10/04 08:17:03 joerg Exp $");
 
 #include "opt_ddb.h"
 #include "opt_kgdb.h"
@@ -239,9 +239,6 @@ db_suspend_others(void)
 {
 	int cpu_me = cpu_number();
 	int win;
-
-	if (cpus == NULL)
-		return 1;
 
 	__cpu_simple_lock(&db_lock);
 	if (ddb_cpu == NOCPU)
