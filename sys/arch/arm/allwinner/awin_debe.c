cc38bfaebca65d8dfcaa95f02e4e6050fdaf4377
bouyer <bouyer@NetBSD.org>
2015-10-05 14:42:19 +0000

    When disabling video ouput also turn off the tcon. This will cause the HDMI output to stop too, and the monitor will go to sleep.

--- sys/arch/arm/allwinner/awin_debe.c
+++ sys/arch/arm/allwinner/awin_debe.c
@@ -1,4 +1,4 @@
-/* $NetBSD: awin_debe.c,v 1.14 2014/12/21 18:36:05 jmcneill Exp $ */
+/* $NetBSD: awin_debe.c,v 1.15 2015/10/05 14:42:19 bouyer Exp $ */
 
 /*-
  * Copyright (c) 2014 Jared D. McNeill <jmcneill@invisible.ca>
@@ -37,7 +37,7 @@
 #define AWIN_DEBE_CURMAX	64
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: awin_debe.c,v 1.14 2014/12/21 18:36:05 jmcneill Exp $");
+__KERNEL_RCSID(0, "$NetBSD: awin_debe.c,v 1.15 2015/10/05 14:42:19 bouyer Exp $");
 
 #include <sys/param.h>
 #include <sys/bus.h>
@@ -582,6 +582,7 @@ awin_debe_ioctl(device_t self, u_long cmd, void *data)
 			val &= ~AWIN_DEBE_MODCTL_HWC_EN;
 		}
 		DEBE_WRITE(sc, AWIN_DEBE_MODCTL_REG, val);
+		awin_tcon_enable(enable);
 		return 0;
 	case WSDISPLAYIO_GVIDEO:
 		val = DEBE_READ(sc, AWIN_DEBE_MODCTL_REG);
