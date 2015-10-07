392365fcf2c98eac7cda63a36b43f803a8dae353
martin <martin@NetBSD.org>
2015-10-07 11:56:41 +0000

    Fix the -m32 compat build for sparc64 after recent sparc/include/types.h changes. XXX I suspect this variant has not been tested in real life.

--- lib/libkvm/kvm_sparc.c
+++ lib/libkvm/kvm_sparc.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: kvm_sparc.c,v 1.33 2014/02/19 20:21:22 dsl Exp $	*/
+/*	$NetBSD: kvm_sparc.c,v 1.34 2015/10/07 11:56:41 martin Exp $	*/
 
 /*-
  * Copyright (c) 1992, 1993
@@ -38,7 +38,7 @@
 #if 0
 static char sccsid[] = "@(#)kvm_sparc.c	8.1 (Berkeley) 6/4/93";
 #else
-__RCSID("$NetBSD: kvm_sparc.c,v 1.33 2014/02/19 20:21:22 dsl Exp $");
+__RCSID("$NetBSD: kvm_sparc.c,v 1.34 2015/10/07 11:56:41 martin Exp $");
 #endif
 #endif /* LIBC_SCCS and not lint */
 
@@ -325,8 +325,8 @@ _kvm_kvatop4u(kvm_t *kd, vaddr_t va, paddr_t *pa)
 	 *	segmap[cpup->nsegmap];
 	 */
 	segmaps = (int64_t **)((long)kd->cpu_data + cpup->segmapoffset);
-	ptes = (int64_t *)(int)_kvm_pa2off(kd,
-	    (paddr_t)segmaps[sparc64_va_to_seg(va)]);
+	ptes = (int64_t *)(intptr_t)_kvm_pa2off(kd,
+	    (paddr_t)(intptr_t)segmaps[sparc64_va_to_seg(va)]);
 	pte = ptes[sparc64_va_to_pte(va)];
 	if ((pte & SPARC64_TLB_V) != 0)
 		return ((pte & SPARC64_TLB_PA_MASK) | (va & (kd->nbpg - 1)));
@@ -363,7 +363,7 @@ _kvm_pa2off(kvm_t *kd, paddr_t pa)
 		off += mp->size;
 	}
 	if (nmem < 0) {
-		_kvm_err(kd, 0, "invalid address (%lx)", pa);
+		_kvm_err(kd, 0, "invalid address (%lx)", (unsigned long)pa);
 		return (-1);
 	}
 
