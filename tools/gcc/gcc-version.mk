ab31df357b2a1bef45c0cc242d7c66d1bf57b0ff
christos <christos@NetBSD.org>
2015-10-16 00:41:07 +0000

    bump for printf/syslog format changres

--- tools/gcc/gcc-version.mk
+++ tools/gcc/gcc-version.mk
@@ -1,5 +1,5 @@
-#	$NetBSD: gcc-version.mk,v 1.3 2015/01/15 08:42:14 skrll Exp $
+#	$NetBSD: gcc-version.mk,v 1.4 2015/10/16 00:41:07 christos Exp $
 
 # common location for tools and native build
 
-NETBSD_GCC_VERSION=nb2 20150115
+NETBSD_GCC_VERSION=nb3 20151015
