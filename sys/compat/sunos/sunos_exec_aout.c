33cd1402fc8a51c38f30823959f88ece34a4bf51
maxv <maxv@NetBSD.org>
2015-10-18 16:59:19 +0000

    Make sure we have space for the aout header.

--- sys/compat/sunos/sunos_exec_aout.c
+++ sys/compat/sunos/sunos_exec_aout.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: sunos_exec_aout.c,v 1.18 2009/08/15 23:39:35 matt Exp $	*/
+/*	$NetBSD: sunos_exec_aout.c,v 1.19 2015/10/18 16:59:19 maxv Exp $	*/
 
 /*
  * Copyright (c) 1993 Theo de Raadt
@@ -28,7 +28,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: sunos_exec_aout.c,v 1.18 2009/08/15 23:39:35 matt Exp $");
+__KERNEL_RCSID(0, "$NetBSD: sunos_exec_aout.c,v 1.19 2015/10/18 16:59:19 maxv Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -71,6 +71,8 @@ exec_sunos_aout_makecmds(struct lwp *l, struct exec_package *epp)
 	struct sunos_exec *sunmag = epp->ep_hdr;
 	int error = ENOEXEC;
 
+	if (epp->ep_hdrvalid < sizeof(struct sunos_exec))
+		return ENOEXEC;
 	if (!SUNOS_M_NATIVE(sunmag->a_machtype))
 		return (ENOEXEC);
 
