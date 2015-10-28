f47b3cb1b65e48d25032941e590e5bd2ee690765
christos <christos@NetBSD.org>
2015-10-28 01:54:10 +0000

    remove bogus KASSERT, there are error paths that don't satisfy this. XXX: should improve error reporting to userland.

--- sys/net/npf/npf_ctl.c
+++ sys/net/npf/npf_ctl.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: npf_ctl.c,v 1.42 2015/06/08 01:00:43 rmind Exp $	*/
+/*	$NetBSD: npf_ctl.c,v 1.43 2015/10/28 01:54:10 christos Exp $	*/
 
 /*-
  * Copyright (c) 2009-2014 The NetBSD Foundation, Inc.
@@ -37,7 +37,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: npf_ctl.c,v 1.42 2015/06/08 01:00:43 rmind Exp $");
+__KERNEL_RCSID(0, "$NetBSD: npf_ctl.c,v 1.43 2015/10/28 01:54:10 christos Exp $");
 
 #include <sys/param.h>
 #include <sys/conf.h>
@@ -609,7 +609,6 @@ fail:
 	/*
 	 * Note: destroy rulesets first, to drop references to the tableset.
 	 */
-	KASSERT(error == 0 || (nset || rpset || rlset || tblset));
 	if (nset) {
 		npf_ruleset_destroy(nset);
 	}
