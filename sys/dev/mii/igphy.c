1c99fb979292a2f3215ab88eee553774abe2ba06
msaitoh <msaitoh@NetBSD.org>
2015-10-30 07:35:30 +0000

    igphy(4),ukphy(4): Set mii_mpd_* entries. wm(4): check PHY type correctly.

--- sys/dev/mii/igphy.c
+++ sys/dev/mii/igphy.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: igphy.c,v 1.24 2015/08/24 23:55:04 pooka Exp $	*/
+/*	$NetBSD: igphy.c,v 1.25 2015/10/30 07:35:30 msaitoh Exp $	*/
 
 /*
  * The Intel copyright applies to the analog register setup, and the
@@ -70,7 +70,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: igphy.c,v 1.24 2015/08/24 23:55:04 pooka Exp $");
+__KERNEL_RCSID(0, "$NetBSD: igphy.c,v 1.25 2015/10/30 07:35:30 msaitoh Exp $");
 
 #ifdef _KERNEL_OPT
 #include "opt_mii.h"
@@ -156,6 +156,9 @@ igphyattach(device_t parent, device_t self, void *aux)
 	sc->mii_dev = self;
 	sc->mii_inst = mii->mii_instance;
 	sc->mii_phy = ma->mii_phyno;
+	sc->mii_mpd_oui = MII_OUI(ma->mii_id1, ma->mii_id2);
+	sc->mii_mpd_model = MII_MODEL(ma->mii_id2);
+	sc->mii_mpd_rev = MII_REV(ma->mii_id2);
 	sc->mii_funcs = &igphy_funcs;
 	sc->mii_pdata = mii;
 	sc->mii_flags = ma->mii_flags;
