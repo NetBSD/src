8898181ae709f7aa522d95e47490fe717baf42e4
sborrill <sborrill@NetBSD.org>
2015-11-09 09:59:18 +0000

    Fix breakage from ticket #1974

--- sys/ufs/lfs/lfs_segment.c
+++ sys/ufs/lfs/lfs_segment.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: lfs_segment.c,v 1.213.8.1 2015/11/07 20:30:41 snj Exp $	*/
+/*	$NetBSD: lfs_segment.c,v 1.213.8.2 2015/11/09 09:59:18 sborrill Exp $	*/
 
 /*-
  * Copyright (c) 1999, 2000, 2001, 2002, 2003 The NetBSD Foundation, Inc.
@@ -60,7 +60,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: lfs_segment.c,v 1.213.8.1 2015/11/07 20:30:41 snj Exp $");
+__KERNEL_RCSID(0, "$NetBSD: lfs_segment.c,v 1.213.8.2 2015/11/09 09:59:18 sborrill Exp $");
 
 #ifdef DEBUG
 # define vndebug(vp, str) do {						\
@@ -1700,7 +1700,7 @@ lfs_rewind(struct lfs *fs, int newsn)
 		panic("lfs_rewind: no clean segments");
 	if (newsn >= 0 && sn >= newsn)
 		return ENOENT;
-	fs->lfs_nextseg = lfs_sntod(fs, sn);
+	fs->lfs_nextseg = sntod(fs, sn);
 	lfs_newseg(fs);
 	fs->lfs_offset = fs->lfs_curseg;
 
