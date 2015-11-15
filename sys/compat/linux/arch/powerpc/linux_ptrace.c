edfeb8c1a68725fd004cac67ecf6e0c2196b108d
bouyer <bouyer@NetBSD.org>
2015-11-15 20:42:36 +0000

    Pull up following revision(s) (requested by pgoyette in ticket #1335): 	sys/compat/linux/arch/i386/linux_ptrace.c: revision 1.31 	sys/compat/linux/arch/arm/linux_ptrace.c: revision 1.19 	sys/compat/linux/arch/powerpc/linux_ptrace.c: revision 1.29 Don't release proc_lock until we're done looking at things that are protected by the lock, particularly p_stat and p_waited.  Found by Robert Elz. XXX Pullup to NetBSD-7, -6, -6-0, and -6-1

--- sys/compat/linux/arch/powerpc/linux_ptrace.c
+++ sys/compat/linux/arch/powerpc/linux_ptrace.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: linux_ptrace.c,v 1.23 2010/07/01 02:38:28 rmind Exp $ */
+/*	$NetBSD: linux_ptrace.c,v 1.23.20.1 2015/11/15 20:42:36 bouyer Exp $ */
 
 /*-
  * Copyright (c) 1999, 2001 The NetBSD Foundation, Inc.
@@ -30,7 +30,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: linux_ptrace.c,v 1.23 2010/07/01 02:38:28 rmind Exp $");
+__KERNEL_RCSID(0, "$NetBSD: linux_ptrace.c,v 1.23.20.1 2015/11/15 20:42:36 bouyer Exp $");
 
 #include <sys/param.h>
 #include <sys/malloc.h>
@@ -162,7 +162,6 @@ linux_sys_ptrace_arch(struct lwp *l, const struct linux_sys_ptrace_args *uap,
 		return ESRCH;
 	}
 	mutex_enter(t->p_lock);
-	mutex_exit(proc_lock);
 
 	/*
 	 * You cannot do what you want to the process if:
@@ -170,6 +169,7 @@ linux_sys_ptrace_arch(struct lwp *l, const struct linux_sys_ptrace_args *uap,
 	 */
 	if (!ISSET(t->p_slflag, PSL_TRACED)) {
 		mutex_exit(t->p_lock);
+		mutex_exit(proc_lock);
 		error = EPERM;
 		goto out;
 	}
@@ -182,9 +182,11 @@ linux_sys_ptrace_arch(struct lwp *l, const struct linux_sys_ptrace_args *uap,
 	if (ISSET(t->p_slflag, PSL_FSTRACE) || t->p_pptr != p ||
 	    t->p_stat != SSTOP || !t->p_waited) {
 		mutex_exit(t->p_lock);
+		mutex_exit(proc_lock);
 		error = EBUSY;
 		goto out;
 	}
+	mutex_exit(proc_lock);
 	/* XXX: ptrace needs revamp for multi-threading support. */
 	if (t->p_nlwps > 1) {
 		mutex_exit(t->p_lock);
