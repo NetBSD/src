3af2d1b32b881313dfea7139cb5dd1ec1d7bca5a
sjg <sjg@NetBSD.org>
2015-11-30 23:15:43 +0000

    Avoid accessing beyond end of empty string.
    Detected by Address Sanitizer - dim at FreeBSD.org

--- usr.bin/make/cond.c
+++ usr.bin/make/cond.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: cond.c,v 1.69 2015/10/11 04:51:24 sjg Exp $	*/
+/*	$NetBSD: cond.c,v 1.70 2015/11/30 23:15:43 sjg Exp $	*/
 
 /*
  * Copyright (c) 1988, 1989, 1990 The Regents of the University of California.
@@ -70,14 +70,14 @@
  */
 
 #ifndef MAKE_NATIVE
-static char rcsid[] = "$NetBSD: cond.c,v 1.69 2015/10/11 04:51:24 sjg Exp $";
+static char rcsid[] = "$NetBSD: cond.c,v 1.70 2015/11/30 23:15:43 sjg Exp $";
 #else
 #include <sys/cdefs.h>
 #ifndef lint
 #if 0
 static char sccsid[] = "@(#)cond.c	8.2 (Berkeley) 1/2/94";
 #else
-__RCSID("$NetBSD: cond.c,v 1.69 2015/10/11 04:51:24 sjg Exp $");
+__RCSID("$NetBSD: cond.c,v 1.70 2015/11/30 23:15:43 sjg Exp $");
 #endif
 #endif /* not lint */
 #endif
@@ -489,6 +489,10 @@ CondCvtArg(char *str, double *value)
     unsigned long l_val;
     double d_val;
 
+    if (!*str) {
+	errno = EINVAL;
+	return FALSE;
+    }
     errno = 0;
     l_val = strtoul(str, &eptr, str[1] == 'x' ? 16 : 10);
     ech = *eptr;
