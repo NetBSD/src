c72fcc81b34d38cd2ac804f676d6a37f6a82fd7c
he <he@NetBSD.org>
2015-11-30 10:31:04 +0000

    As far as I (and my compiler) can tell, there's no need to cast the arg to strncmp().

--- external/bsd/ntp/dist/sntp/libopts/enum.c
+++ external/bsd/ntp/dist/sntp/libopts/enum.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: enum.c,v 1.2.6.2 2015/11/08 01:51:12 riz Exp $	*/
+/*	$NetBSD: enum.c,v 1.2.6.3 2015/11/30 10:31:04 he Exp $	*/
 
 
 /**
@@ -217,7 +217,7 @@ find_name(char const * name, tOptions * pOpts, tOptDesc * pOD,
      *  Multiple partial matches means we have an ambiguous match.
      */
     for (idx = 0; idx < name_ct; idx++) {
-        if (strncmp(paz_names[idx], (const char *)name, len) == 0) {
+        if (strncmp(paz_names[idx], name, len) == 0) {
             if (paz_names[idx][len] == NUL)
                 return idx;  /* full match */
 
