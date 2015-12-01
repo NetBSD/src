b8197dcff04345ab640749be07e12b587f65d11d
joerg <joerg@NetBSD.org>
2015-12-01 22:18:26 +0000

    Don't set TOOL_GREP to something which doesn't exist, just use the host tool for now.

--- share/mk/bsd.own.mk
+++ share/mk/bsd.own.mk
@@ -1,4 +1,4 @@
-#	$NetBSD: bsd.own.mk,v 1.880 2015/11/15 13:50:10 pooka Exp $
+#	$NetBSD: bsd.own.mk,v 1.881 2015/12/01 22:18:26 joerg Exp $
 
 # This needs to be before bsd.init.mk
 .if defined(BSD_MK_COMPAT_FILE)
@@ -332,7 +332,9 @@ TOOL_FGEN=		${TOOLDIR}/bin/${_TOOL_PREFIX}fgen
 TOOL_GENASSYM=		${TOOLDIR}/bin/${_TOOL_PREFIX}genassym
 TOOL_GENCAT=		${TOOLDIR}/bin/${_TOOL_PREFIX}gencat
 TOOL_GMAKE=		${TOOLDIR}/bin/${_TOOL_PREFIX}gmake
-TOOL_GREP=		${TOOLDIR}/bin/${_TOOL_PREFIX}grep
+# grep exists in src/tools, but is not hooked up into the build.
+#TOOL_GREP=		${TOOLDIR}/bin/${_TOOL_PREFIX}grep
+TOOL_GREP=		grep
 TOOL_GROFF=		PATH=${TOOLDIR}/lib/groff:$${PATH} ${TOOLDIR}/bin/${_TOOL_PREFIX}groff
 TOOL_HEXDUMP=		${TOOLDIR}/bin/${_TOOL_PREFIX}hexdump
 TOOL_HP300MKBOOT=	${TOOLDIR}/bin/${_TOOL_PREFIX}hp300-mkboot
