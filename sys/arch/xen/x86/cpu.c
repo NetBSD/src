a620b7d8a7386037dee06656e60d7ba5956b6ac7
christos <christos@NetBSD.org>
2015-12-13 15:22:31 +0000

    fix the build.

--- sys/arch/xen/x86/cpu.c
+++ sys/arch/xen/x86/cpu.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: cpu.c,v 1.101 2014/12/08 15:22:47 msaitoh Exp $	*/
+/*	$NetBSD: cpu.c,v 1.102 2015/12/13 15:22:31 christos Exp $	*/
 /* NetBSD: cpu.c,v 1.18 2004/02/20 17:35:01 yamt Exp  */
 
 /*-
@@ -66,7 +66,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: cpu.c,v 1.101 2014/12/08 15:22:47 msaitoh Exp $");
+__KERNEL_RCSID(0, "$NetBSD: cpu.c,v 1.102 2015/12/13 15:22:31 christos Exp $");
 
 #include "opt_ddb.h"
 #include "opt_multiprocessor.h"
@@ -172,12 +172,14 @@ struct cpu_info phycpu_info_primary __aligned(CACHE_LINE_SIZE) = {
 struct cpu_info *cpu_info_list = &cpu_info_primary;
 struct cpu_info *phycpu_info_list = &phycpu_info_primary;
 
-uint32_t cpu_feature[5]; /* X86 CPUID feature bits
+extern uint32_t cpu_feature[]; /* X86 CPUID feature bits
 			  *	[0] basic features %edx
 			  *	[1] basic features %ecx
 			  *	[2] extended features %edx
 			  *	[3] extended features %ecx
 			  *	[4] VIA padlock features
+			  *	[5] structured extended features cpuid.7:%ebx
+			  *	[6] structured extended features cpuid.7:%ecx
 			  */
 
 bool x86_mp_online;
