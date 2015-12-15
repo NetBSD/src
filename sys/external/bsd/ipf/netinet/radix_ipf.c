bc8dd3c7ab8469826375a85606cf9114cd1cd130
christos <christos@NetBSD.org>
2015-12-15 12:30:34 +0000

    PR/50559: David Binderman: Add missing free()'s after calls to randomize().

--- sys/external/bsd/ipf/netinet/radix_ipf.c
+++ sys/external/bsd/ipf/netinet/radix_ipf.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: radix_ipf.c,v 1.5 2014/03/20 20:43:12 christos Exp $	*/
+/*	$NetBSD: radix_ipf.c,v 1.6 2015/12/15 12:30:34 christos Exp $	*/
 
 /*
  * Copyright (C) 2012 by Darren Reed.
@@ -1480,6 +1480,8 @@ random_add(rnh)
 		add_addr(rnh, i, order[i]);
 		checktree(rnh);
 	}
+
+	free(order);
 }
 
 
@@ -1497,5 +1499,7 @@ random_delete(rnh)
 		delete_addr(rnh, i);
 		checktree(rnh);
 	}
+
+	free(order);
 }
 #endif /* RDX_DEBUG */
