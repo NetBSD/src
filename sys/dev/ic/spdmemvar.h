363f842c179df137cc6961fce277207e62a259fb
msaitoh <msaitoh@NetBSD.org>
2015-12-24 14:16:18 +0000

    - Add DDR4E, LPDDR3 and LPDDR4. - Check NVDIMM hybrid. - add DDR4's tWR and tWTR. - Print cycle time with aprint_verbose on DDR4 as DDR[23].

--- sys/dev/ic/spdmemvar.h
+++ sys/dev/ic/spdmemvar.h
@@ -1,4 +1,4 @@
-/* $NetBSD: spdmemvar.h,v 1.9 2015/12/07 09:41:37 mlelstv Exp $ */
+/* $NetBSD: spdmemvar.h,v 1.10 2015/12/24 14:16:18 msaitoh Exp $ */
 
 /*
  * Copyright (c) 2007 Paul Goyette
@@ -719,7 +719,8 @@ struct spdmem_ddr4 {				/* Dual Data Rate 4 SDRAM */
 	uint8_t	ddr4_type;
 	SPD_BITFIELD(				\
 		uint8_t	ddr4_mod_type:4,	\
-		uint8_t	ddr4_unused1:4, ,	\
+		uint8_t	ddr4_hybrid_media:3,	\
+		uint8_t	ddr4_hybrid:1,		\
 	);
 	SPD_BITFIELD(				\
 		/* capacity is offset by 28: 0 = 256M, 1 = 512M, ... */ \
@@ -813,7 +814,12 @@ struct spdmem_ddr4 {				/* Dual Data Rate 4 SDRAM */
 	uint8_t	ddr4_tRRD_Smin_mtb;
 	uint8_t	ddr4_tRRD_Lmin_mtb;
 	uint8_t	ddr4_tCCD_Lmin_mtb;
-	uint8_t	ddr4_unused15[19];
+	uint8_t	ddr4_tWR_min_msb;
+	uint8_t	ddr4_tWR_min_mtb;
+	uint8_t	ddr4_tWTR_min;
+	uint8_t	ddr4_tWTR_Smin_mtb;
+	uint8_t	ddr4_tWTR_Lmin_mtb;
+	uint8_t	ddr4_unused15[14];
 	uint8_t	ddr4_connector_map[18];
 	uint8_t	ddr4_unused16[39];
 	uint8_t	ddr4_tCCD_Lmin_ftb;
@@ -882,7 +888,7 @@ struct spdmem {
 #define	sm_refresh	sm_fpm.fpm_refresh
 #define	sm_selfrefresh	sm_fpm.fpm_selfrefresh
 
-#define SPDMEM_TYPE_MAXLEN 24
+#define SPDMEM_TYPE_MAXLEN 40
 
 struct spdmem_softc {
 	uint8_t		(*sc_read)(struct spdmem_softc *, uint16_t);
