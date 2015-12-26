652e2a0d73bd0d151c25a6cf0539d7d9d1cc528d
pgoyette <pgoyette@NetBSD.org>
2015-12-26 08:01:58 +0000

    Remove the "expected failure" from test n12, and change it not to use the "-v" command line option to ipf_test.  The -v option causes it to display additional information, some of which is not fixed (looks like an internal data structure address).
    One fewer on the expected-failures list.  :)

--- tests/ipf/t_nat_exec.sh
+++ tests/ipf/t_nat_exec.sh
@@ -1,4 +1,4 @@
-# $NetBSD: t_nat_exec.sh,v 1.21 2014/12/01 19:46:29 martin Exp $
+# $NetBSD: t_nat_exec.sh,v 1.22 2015/12/26 08:01:58 pgoyette Exp $
 #
 # Copyright (c) 2008, 2010 The NetBSD Foundation, Inc.
 # All rights reserved.
@@ -71,7 +71,7 @@ test_case n8 nattest hex hex -T update_ipid=0
 test_case n9 nattest hex hex -T update_ipid=0
 test_case n10 nattest hex hex -T update_ipid=0
 test_case n11 nattest text text
-failing_test_case n12 nattest "Known to be broken" hex hex -T update_ipid=0 -v
+test_case n12 nattest hex hex -T update_ipid=0
 test_case n13 nattest text text
 test_case n14 nattest text text
 test_case n15 nattest text text -T update_ipid=0
