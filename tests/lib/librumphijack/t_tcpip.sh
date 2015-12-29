cb5c700db20794358f36787295be2a12781ab54e
pgoyette <pgoyette@NetBSD.org>
2015-12-29 07:17:19 +0000

    Since we're expecting to autoload modules here, make sure that the kernel (or rump-server) has autoload enabled.
    This should finish fixing PR bin/49153
    
    XXX The test still does not run successfully, due to another bug that
    XXX was recently introduced.  This second bug is being worked on.

--- tests/lib/librumphijack/t_tcpip.sh
+++ tests/lib/librumphijack/t_tcpip.sh
@@ -1,4 +1,4 @@
-#       $NetBSD: t_tcpip.sh,v 1.15 2015/08/26 09:19:20 martin Exp $
+#       $NetBSD: t_tcpip.sh,v 1.16 2015/12/29 07:17:19 pgoyette Exp $
 #
 # Copyright (c) 2011 The NetBSD Foundation, Inc.
 # All rights reserved.
@@ -178,6 +178,8 @@ test_nfs()
 	atf_check -s exit:0 -x \
 	    'echo "/export -noresvport -noresvmnt 10.1.1.100" | \
 		dd of=/rump/etc/exports 2> /dev/null'
+
+	atf_check -s exit:0 rump.sysctl -q -w kern.module.autoload=1
 
 	atf_check -s exit:0 -e ignore mount_ffs /dk /rump/export
 	atf_check -s exit:0 -x "echo ${magicstr} > /rump/export/im_alive"
