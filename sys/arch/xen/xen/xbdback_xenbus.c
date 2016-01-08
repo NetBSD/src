48f16d66ebb8e948c9896ae2c6ed0b8ee748f749
snj <snj@NetBSD.org>
2016-01-08 21:25:28 +0000

    Pull up following revision(s) (requested by bouyer in ticket #1358): 	sys/arch/xen/include/xen-public/io/ring.h: revision 1.3 via patch 	sys/arch/xen/xen/pciback.c: revision 1.10 via patch 	sys/arch/xen/xen/xbdback_xenbus.c: revision 1.62 via patch 	sys/arch/xen/xen/xennetback_xenbus.c: revision 1.54 via patch Apply patch from xsa155: make sure that the backend won't read parts of the request again (possibly because of compiler optimisations), by using copies and barrier. From XSA155: The compiler can emit optimizations in the PV backend drivers which can lead to double fetch vulnerabilities. Specifically the shared memory between the frontend and backend can be fetched twice (during which time the frontend can alter the contents) possibly leading to arbitrary code execution in backend.

--- sys/arch/xen/xen/xbdback_xenbus.c
+++ sys/arch/xen/xen/xbdback_xenbus.c
@@ -1,4 +1,4 @@
-/*      $NetBSD: xbdback_xenbus.c,v 1.55.2.1.6.2 2015/11/16 07:52:12 msaitoh Exp $      */
+/*      $NetBSD: xbdback_xenbus.c,v 1.55.2.1.6.3 2016/01/08 21:25:28 snj Exp $      */
 
 /*
  * Copyright (c) 2006 Manuel Bouyer.
@@ -26,7 +26,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: xbdback_xenbus.c,v 1.55.2.1.6.2 2015/11/16 07:52:12 msaitoh Exp $");
+__KERNEL_RCSID(0, "$NetBSD: xbdback_xenbus.c,v 1.55.2.1.6.3 2016/01/08 21:25:28 snj Exp $");
 
 #include <sys/atomic.h>
 #include <sys/buf.h>
@@ -1023,6 +1023,7 @@ xbdback_co_main_loop(struct xbdback_instance *xbdi, void *obj)
 			req->sector_number = req64->sector_number;
 			break;
 		}
+		__insn_barrier();
 		XENPRINTF(("xbdback op %d req_cons 0x%x req_prod 0x%x "
 		    "resp_prod 0x%x id %" PRIu64 "\n", req->operation,
 			xbdi->xbdi_ring.ring_n.req_cons,
