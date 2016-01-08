b4f75b404c4f6a5f2f77b323e2b08d8df823163d
christos <christos@NetBSD.org>
2016-01-08 20:55:28 +0000

    Import ntp 4.2.8p5

--- external/bsd/ntp/dist/sntp/version.c
+++ external/bsd/ntp/dist/sntp/version.c
@@ -1,7 +1,5 @@
-/*	$NetBSD: version.c,v 1.1.1.3 2015/10/23 17:47:42 christos Exp $	*/
-
 /*
  * version file for sntp
  */
 #include <config.h>
-const char * Version = "sntp 4.2.8p4@1.3265-o Wed Oct 21 16:41:07 UTC 2015 (25)";
+const char * Version = "sntp 4.2.8p5@1.3618-o Tue Jan  5 11:48:52 UTC 2016 (3)";
