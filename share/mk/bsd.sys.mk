edf693e04bc2e345bf730694a569b7fc536c5ee0
christos <christos@NetBSD.org>
2016-01-09 02:06:22 +0000

    - use -fdebug-regex-map to remap one style of object dirs. There are others. - change DESTDIR to nothing so that we don't get // in the front - apply the mappings to both c and c++

--- share/mk/bsd.sys.mk
+++ share/mk/bsd.sys.mk
@@ -1,4 +1,4 @@
-#	$NetBSD: bsd.sys.mk,v 1.246 2015/12/22 16:46:56 christos Exp $
+#	$NetBSD: bsd.sys.mk,v 1.247 2016/01/09 02:06:22 christos Exp $
 #
 # Build definitions used for NetBSD source tree builds.
 
@@ -19,9 +19,14 @@ CPPFLAGS+=	-Wp,-iremap,${NETBSDSRCDIR}:/usr/src
 CPPFLAGS+=	-Wp,-iremap,${DESTDIR}/:/
 CPPFLAGS+=	-Wp,-iremap,${X11SRCDIR}:/usr/xsrc
 .export NETBSDSRCDIR DESTDIR X11SRCDIR
-CFLAGS+=	-fdebug-prefix-map=\$$NETBSDSRCDIR=/usr/src
-CFLAGS+=	-fdebug-prefix-map=\$$DESTDIR=/
-CFLAGS+=	-fdebug-prefix-map=\$$X11SRCDIR=/usr/xsrc
+REPROFLAGS+=	-fdebug-prefix-map=\$$NETBSDSRCDIR=/usr/src
+.if !empty(DESTDIR)
+REPROFLAGS+=	-fdebug-prefix-map=\$$DESTDIR=
+.endif
+REPROFLAGS+=	-fdebug-prefix-map=\$$X11SRCDIR=/usr/xsrc
+REPROFLAGS+=	-fdebug-regex-map='/usr/src/(.*)/obj.${MACHINE}=/usr/obj/\1'
+CFLAGS+=	${REPROFLAGS}
+CXXFLAGS+=	${REPROFLAGS}
 .endif
 
 # NetBSD sources use C99 style, with some GCC extensions.
