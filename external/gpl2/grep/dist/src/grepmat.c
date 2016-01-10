4f6f6467d20901b194d376eafaba59e42e34f0b8
christos <christos@NetBSD.org>
2016-01-10 21:36:14 +0000

    Import latest GPLv2 grep

---
+++ external/gpl2/grep/dist/src/grepmat.c
@@ -0,0 +1,8 @@
+/*	$NetBSD: grepmat.c,v 1.1.1.1 2016/01/10 21:36:21 christos Exp $	*/
+
+#ifdef HAVE_CONFIG_H
+# include <config.h>
+#endif
+#include "system.h"
+#include "grep.h"
+char const *matcher;
