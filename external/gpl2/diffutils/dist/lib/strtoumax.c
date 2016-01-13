7c6a55faa310d730520d308da39cfbaff26c1eff
christos <christos@NetBSD.org>
2016-01-13 03:15:29 +0000

    Import latest gpl2 diffutils

---
+++ external/gpl2/diffutils/dist/lib/strtoumax.c
@@ -0,0 +1,4 @@
+/*	$NetBSD: strtoumax.c,v 1.1.1.1 2016/01/13 03:15:30 christos Exp $	*/
+
+#define UNSIGNED 1
+#include "strtoimax.c"
---
+++ external/gpl2/diffutils/dist/lib/umaxtostr.c
@@ -0,0 +1,5 @@
+/*	$NetBSD: umaxtostr.c,v 1.1.1.1 2016/01/13 03:15:30 christos Exp $	*/
+
+#define inttostr umaxtostr
+#define inttype uintmax_t
+#include "inttostr.c"
