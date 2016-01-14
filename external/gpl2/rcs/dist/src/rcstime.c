0d69c6cf372d59b20fb6d9d5102f89d83d033790
christos <christos@NetBSD.org>
2016-01-14 04:22:38 +0000

    - merge local changes - provide reach-over infrastructure

--- external/gpl2/rcs/dist/src/rcstime.c
+++ external/gpl2/rcs/dist/src/rcstime.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: rcstime.c,v 1.1.1.1 2016/01/14 03:05:06 christos Exp $	*/
+/*	$NetBSD: rcstime.c,v 1.2 2016/01/14 04:22:39 christos Exp $	*/
 
 /* Convert between RCS time format and Posix and/or C formats.  */
 
