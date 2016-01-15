e3af3223e38a8e01d58a178584dcbc7cca2ecce2
mlelstv <mlelstv@NetBSD.org>
2016-01-15 07:48:22 +0000

    Allow dump to raidframe component which is a wedge.
    N.B. ordinary devices check the partition type only in the xxxsize routine.

--- sys/dev/dkwedge/dk.c
+++ sys/dev/dkwedge/dk.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: dk.c,v 1.87 2015/12/27 00:47:47 mlelstv Exp $	*/
+/*	$NetBSD: dk.c,v 1.88 2016/01/15 07:48:22 mlelstv Exp $	*/
 
 /*-
  * Copyright (c) 2004, 2005, 2006, 2007 The NetBSD Foundation, Inc.
@@ -30,7 +30,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: dk.c,v 1.87 2015/12/27 00:47:47 mlelstv Exp $");
+__KERNEL_RCSID(0, "$NetBSD: dk.c,v 1.88 2016/01/15 07:48:22 mlelstv Exp $");
 
 #ifdef _KERNEL_OPT
 #include "opt_dkwedge.h"
@@ -1554,7 +1554,8 @@ dkdump(dev_t dev, daddr_t blkno, void *va, size_t size)
 
 	/* Our content type is static, no need to open the device. */
 
-	if (strcmp(sc->sc_ptype, DKW_PTYPE_SWAP) != 0) {
+	if (strcmp(sc->sc_ptype, DKW_PTYPE_SWAP) != 0 &&
+	    strcmp(sc->sc_ptype, DKW_PTYPE_RAID) != 0) {
 		rv = ENXIO;
 		goto out;
 	}
