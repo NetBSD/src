f616b3317009eafd9afa5db533bca9d979229a5e
christos <christos@NetBSD.org>
2016-01-17 17:45:21 +0000

    remove free NULL checks (Tilman Sauerbeck)

--- usr.bin/make/make.c
+++ usr.bin/make/make.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: make.c,v 1.93 2016/01/09 00:55:17 christos Exp $	*/
+/*	$NetBSD: make.c,v 1.94 2016/01/17 17:45:21 christos Exp $	*/
 
 /*
  * Copyright (c) 1988, 1989, 1990, 1993
@@ -69,14 +69,14 @@
  */
 
 #ifndef MAKE_NATIVE
-static char rcsid[] = "$NetBSD: make.c,v 1.93 2016/01/09 00:55:17 christos Exp $";
+static char rcsid[] = "$NetBSD: make.c,v 1.94 2016/01/17 17:45:21 christos Exp $";
 #else
 #include <sys/cdefs.h>
 #ifndef lint
 #if 0
 static char sccsid[] = "@(#)make.c	8.1 (Berkeley) 6/6/93";
 #else
-__RCSID("$NetBSD: make.c,v 1.93 2016/01/09 00:55:17 christos Exp $");
+__RCSID("$NetBSD: make.c,v 1.94 2016/01/17 17:45:21 christos Exp $");
 #endif
 #endif /* not lint */
 #endif
@@ -482,8 +482,7 @@ Make_HandleUse(GNode *cgn, GNode *pgn)
 	    if (gn->uname == NULL) {
 		gn->uname = gn->name;
 	    } else {
-		if (gn->name)
-		    free(gn->name);
+		free(gn->name);
 	    }
 	    gn->name = Var_Subst(NULL, gn->uname, pgn, FALSE, TRUE, FALSE);
 	    if (gn->name && gn->uname && strcmp(gn->name, gn->uname) != 0) {
@@ -692,8 +691,7 @@ Make_Update(GNode *cgn)
     checked++;
 
     cname = Var_Value(TARGET, cgn, &p1);
-    if (p1)
-	free(p1);
+    free(p1);
 
     if (DEBUG(MAKE))
 	fprintf(debug_file, "Make_Update: %s%s\n", cgn->name, cgn->cohort_num);
@@ -838,8 +836,7 @@ Make_Update(GNode *cgn)
 		    Var_Set(PREFIX, cpref, pgn, 0);
 	    }
 	}
-	if (p1)
-	    free(p1);
+	free(p1);
 	Lst_Close(cgn->iParents);
     }
 }
@@ -907,8 +904,7 @@ MakeAddAllSrc(void *cgnp, void *pgnp)
 	}
 	if (allsrc != NULL)
 		Var_Append(ALLSRC, allsrc, pgn);
-	if (p2)
-	    free(p2);
+	free(p2);
 	if (pgn->type & OP_JOIN) {
 	    if (cgn->made == MADE) {
 		Var_Append(OODATE, child, pgn);
@@ -934,8 +930,7 @@ MakeAddAllSrc(void *cgnp, void *pgnp)
 	     */
 	    Var_Append(OODATE, child, pgn);
 	}
-	if (p1)
-	    free(p1);
+	free(p1);
     }
     return (0);
 }
@@ -981,8 +976,7 @@ Make_DoAllVar(GNode *gn)
     if (gn->type & OP_JOIN) {
 	char *p1;
 	Var_Set(TARGET, Var_Value(ALLSRC, gn, &p1), gn, 0);
-	if (p1)
-	    free(p1);
+	free(p1);
     }
     gn->flags |= DONE_ALLSRC;
 }
