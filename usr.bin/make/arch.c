f393345c09137cbd0151c7c54b3d15751860ff2e
christos <christos@NetBSD.org>
2016-01-17 15:32:38 +0000

    provide missing FD_CLOEXEC for the havenots.

--- usr.bin/make/arch.c
+++ usr.bin/make/arch.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: arch.c,v 1.65 2016/01/09 00:55:17 christos Exp $	*/
+/*	$NetBSD: arch.c,v 1.66 2016/01/17 15:32:38 christos Exp $	*/
 
 /*
  * Copyright (c) 1988, 1989, 1990, 1993
@@ -69,14 +69,14 @@
  */
 
 #ifndef MAKE_NATIVE
-static char rcsid[] = "$NetBSD: arch.c,v 1.65 2016/01/09 00:55:17 christos Exp $";
+static char rcsid[] = "$NetBSD: arch.c,v 1.66 2016/01/17 15:32:38 christos Exp $";
 #else
 #include <sys/cdefs.h>
 #ifndef lint
 #if 0
 static char sccsid[] = "@(#)arch.c	8.2 (Berkeley) 1/2/94";
 #else
-__RCSID("$NetBSD: arch.c,v 1.65 2016/01/09 00:55:17 christos Exp $");
+__RCSID("$NetBSD: arch.c,v 1.66 2016/01/17 15:32:38 christos Exp $");
 #endif
 #endif /* not lint */
 #endif
@@ -136,7 +136,6 @@ __RCSID("$NetBSD: arch.c,v 1.65 2016/01/09 00:55:17 christos Exp $");
 
 #include    <ar.h>
 #include    <ctype.h>
-#include    <fcntl.h>
 #include    <stdio.h>
 #include    <stdlib.h>
 #include    <utime.h>
