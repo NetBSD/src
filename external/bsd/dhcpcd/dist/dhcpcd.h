427f55cf28f834b995a5e5940e7ea3197e874c53
roy <roy@NetBSD.org>
2016-01-20 19:30:30 +0000

    Import dhcpcd-6.10.1 with the following changes:
      *  Add -P, --printpidfile to print the pidfile dhcpcd will use to
         stdout
      *  Fix a crash when a non active interface departs
      *  Add the -1, --oneshot option which causes dhcpcd to exit once an
         interface has been configured
      *  Fix delegation activating interfaces

--- external/bsd/dhcpcd/dist/dhcpcd.h
+++ external/bsd/dhcpcd/dist/dhcpcd.h
@@ -202,6 +202,6 @@ void dhcpcd_dropinterface(struct interface *, const char *);
 int dhcpcd_selectprofile(struct interface *, const char *);
 
 void dhcpcd_startinterface(void *);
-void dhcpcd_initstate(struct interface *, unsigned long long);
+void dhcpcd_activateinterface(struct interface *);
 
 #endif
