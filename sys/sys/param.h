1571e1a5b63bd0ae65079e5c2df4f489df98f339
pooka <pooka@NetBSD.org>
2016-01-24 22:50:23 +0000

    bump version for pr_output change

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.490 2015/12/21 12:35:15 mlelstv Exp $	*/
+/*	$NetBSD: param.h,v 1.491 2016/01/24 22:50:23 pooka Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -67,7 +67,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	799002500	/* NetBSD 7.99.25 */
+#define	__NetBSD_Version__	799002600	/* NetBSD 7.99.26 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
