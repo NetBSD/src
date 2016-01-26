537d5ef0026b907373435ffc4f827e29026edd33
pooka <pooka@NetBSD.org>
2016-01-26 23:25:40 +0000

    regen rump kernel interfaces for header change. (they were already manually edited for a prior commit, so not much change)

--- sys/rump/include/rump-sys/net_if.h
+++ sys/rump/include/rump-sys/net_if.h
@@ -1,9 +1,15 @@
-/*	$NetBSD: net_if.h,v 1.1 2016/01/26 23:12:17 pooka Exp $	*/
+/*	$NetBSD: net_if.h,v 1.2 2016/01/26 23:25:40 pooka Exp $	*/
 
 /*
  * Automatically generated.  DO NOT EDIT.
- * from: NetBSD: rumpnet.ifspec,v 1.3 2013/07/03 19:21:11 pooka Exp 
- * by:   NetBSD: makerumpif.sh,v 1.6 2013/02/14 10:54:54 pooka Exp 
+ * from: NetBSD: rumpnet.ifspec,v 1.4 2016/01/26 23:22:22 pooka Exp 
+ * by:   NetBSD: makerumpif.sh,v 1.10 2016/01/26 23:21:18 pooka Exp 
  */
 
+#ifndef _RUMP_PRIF_NET_H_
+#define _RUMP_PRIF_NET_H_
+
 int rump_shmif_create(const char *, int *);
+typedef int (*rump_shmif_create_fn)(const char *, int *);
+
+#endif /* _RUMP_PRIF_NET_H_ */
