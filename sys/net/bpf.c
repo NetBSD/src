6af89be73b07dccf2fa48a08eff0cf9f78387bbf
christos <christos@NetBSD.org>
2016-02-01 16:32:28 +0000

    Do less work under the kernel lock, otherwise dhcpcd aborting causes us to deadlock.

--- sys/net/bpf.c
+++ sys/net/bpf.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: bpf.c,v 1.193 2015/12/16 23:14:42 christos Exp $	*/
+/*	$NetBSD: bpf.c,v 1.194 2016/02/01 16:32:28 christos Exp $	*/
 
 /*
  * Copyright (c) 1990, 1991, 1993
@@ -39,7 +39,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: bpf.c,v 1.193 2015/12/16 23:14:42 christos Exp $");
+__KERNEL_RCSID(0, "$NetBSD: bpf.c,v 1.194 2016/02/01 16:32:28 christos Exp $");
 
 #if defined(_KERNEL_OPT)
 #include "opt_bpf.h"
@@ -475,11 +475,17 @@ bpfopen(dev_t dev, int flag, int mode, struct lwp *l)
 static int
 bpf_close(struct file *fp)
 {
-	struct bpf_d *d = fp->f_bpf;
+	struct bpf_d *d;
 	int s;
 
 	KERNEL_LOCK(1, NULL);
 	mutex_enter(&bpf_mtx);
+
+	if ((d = fp->f_bpf) == NULL) {
+		mutex_exit(&bpf_mtx);
+		KERNEL_UNLOCK_ONE(NULL);
+		return 0;
+	}
 
 	/*
 	 * Refresh the PID associated with this bpf file.
@@ -495,14 +501,15 @@ bpf_close(struct file *fp)
 	splx(s);
 	bpf_freed(d);
 	LIST_REMOVE(d, bd_list);
-	callout_destroy(&d->bd_callout);
-	seldestroy(&d->bd_sel);
-	softint_disestablish(d->bd_sih);
-	free(d, M_DEVBUF);
 	fp->f_bpf = NULL;
 
 	mutex_exit(&bpf_mtx);
 	KERNEL_UNLOCK_ONE(NULL);
+
+	callout_destroy(&d->bd_callout);
+	seldestroy(&d->bd_sel);
+	softint_disestablish(d->bd_sih);
+	free(d, M_DEVBUF);
 
 	return (0);
 }
