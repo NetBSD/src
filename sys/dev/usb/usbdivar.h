59d193e77c86126c7411ac067f7e2b51330364ec
snj <snj@NetBSD.org>
2016-02-06 20:58:13 +0000

    Pull up following revision(s) (requested by skrll in ticket #1097): 	sys/dev/usb/usb.c: revision 1.161 	sys/dev/usb/usb_subr.c: revisions 1.207, 1.208 	sys/dev/usb/usbdivar.h: revision 1.111 	sys/dev/usb/xhci.c: revision 1.33 Get the iManufacturer, iProduct, and iSerialNumber strings before probing for drivers and cache them for later use.  This reduces bus transactions and fixes attachment for at least two of my umass(4)s. -- Need sys/kmem.h

--- sys/dev/usb/usbdivar.h
+++ sys/dev/usb/usbdivar.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: usbdivar.h,v 1.107 2013/10/03 19:04:00 skrll Exp $	*/
+/*	$NetBSD: usbdivar.h,v 1.107.4.1 2016/02/06 20:58:13 snj Exp $	*/
 
 /*
  * Copyright (c) 1998, 2012 The NetBSD Foundation, Inc.
@@ -196,6 +196,10 @@ struct usbd_device {
 	device_t	       *subdevs;       /* sub-devices */
 	int			nifaces_claimed; /* number of ifaces in use */
 	void		       *hci_private;
+
+	char		       *ud_serial;	/* serial number, can be NULL */
+	char		       *ud_vendor;	/* vendor string, can be NULL */
+	char		       *ud_product;	/* product string can be NULL */
 };
 
 struct usbd_interface {
@@ -285,6 +289,7 @@ void usbd_dump_pipe(usbd_pipe_handle pipe);
 
 /* Routines from usb_subr.c */
 int		usbctlprint(void *, const char *);
+void		usbd_get_device_strings(struct usbd_device *);
 void		usb_delay_ms_locked(usbd_bus_handle, u_int, kmutex_t *);
 void		usb_delay_ms(usbd_bus_handle, u_int);
 void		usbd_delay_ms_locked(usbd_device_handle, u_int, kmutex_t *);
