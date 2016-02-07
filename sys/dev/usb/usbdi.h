3b8ff23b2e03944ef96b7243a58ae362917dbe97
skrll <skrll@NetBSD.org>
2016-02-07 15:48:44 +0000

    G/C usbd_do_request_async declaration - it doesn't exist.

--- sys/dev/usb/usbdi.h
+++ sys/dev/usb/usbdi.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: usbdi.h,v 1.90.4.9 2015/12/23 09:52:41 skrll Exp $	*/
+/*	$NetBSD: usbdi.h,v 1.90.4.10 2016/02/07 15:48:44 skrll Exp $	*/
 /*	$FreeBSD: src/sys/dev/usb/usbdi.h,v 1.18 1999/11/17 22:33:49 n_hibma Exp $	*/
 
 /*
@@ -133,8 +133,6 @@ usbd_status usbd_open_pipe_intr(struct usbd_interface *, uint8_t,
 				void *, void *,
 				uint32_t, usbd_callback, int);
 usbd_status usbd_do_request(struct usbd_device *, usb_device_request_t *, void *);
-usbd_status usbd_do_request_async(struct usbd_device *,
-				  usb_device_request_t *, void *);
 usbd_status usbd_do_request_flags(struct usbd_device *, usb_device_request_t *,
 				  void *, uint16_t, int*, uint32_t);
 usbd_status usbd_do_request_flags_pipe(struct usbd_device *, struct usbd_pipe *,
