a9657545c3eb607eca0486fcbb1bffe8fa9481c4
snj <snj@NetBSD.org>
2016-02-11 22:48:22 +0000

    Pull up following revision(s) (requested by riastradh in ticket #1092): 	sys/external/bsd/drm2/nouveau/files.nouveau: revision 1.9 	sys/external/bsd/drm2/nouveau/nouveau_module.c: revision 1.4 	sys/external/bsd/drm2/nouveau/nouveau_pci.c: revision 1.4 	sys/external/bsd/drm2/nouveau/nouveau_pci.h: revision 1.2 	sys/external/bsd/drm2/nouveau/nouveaufb.c: revision 1.3 Split nouveau@pci from nouveau so we can attach it to other busses. nouveaufb still depends on nouveau_pci, but it is not applicable to non-PCI devices anyway.

--- sys/external/bsd/drm2/nouveau/nouveau_module.c
+++ sys/external/bsd/drm2/nouveau/nouveau_module.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: nouveau_module.c,v 1.1.4.2 2015/01/11 05:59:17 snj Exp $	*/
+/*	$NetBSD: nouveau_module.c,v 1.1.4.3 2016/02/11 22:48:22 snj Exp $	*/
 
 /*-
  * Copyright (c) 2014 The NetBSD Foundation, Inc.
@@ -30,13 +30,10 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: nouveau_module.c,v 1.1.4.2 2015/01/11 05:59:17 snj Exp $");
+__KERNEL_RCSID(0, "$NetBSD: nouveau_module.c,v 1.1.4.3 2016/02/11 22:48:22 snj Exp $");
 
 #include <sys/types.h>
 #include <sys/module.h>
-#ifndef _MODULE
-#include <sys/once.h>
-#endif
 #include <sys/systm.h>
 
 #include <drm/drmP.h>
@@ -45,7 +42,7 @@ __KERNEL_RCSID(0, "$NetBSD: nouveau_module.c,v 1.1.4.2 2015/01/11 05:59:17 snj E
 #include <core/object.h>
 #include <engine/device.h>
 
-MODULE(MODULE_CLASS_DRIVER, nouveau, "drmkms,drmkms_pci"); /* XXX drmkms_i2c, drmkms_ttm */
+MODULE(MODULE_CLASS_DRIVER, nouveau, "drmkms"); /* XXX drmkms_i2c, drmkms_ttm */
 
 #ifdef _MODULE
 #include "ioconf.c"
@@ -58,40 +55,11 @@ extern struct drm_driver *const nouveau_drm_driver; /* XXX */
 static int
 nouveau_init(void)
 {
-	extern int drm_guarantee_initialized(void);
-	int error;
-
-	error = drm_guarantee_initialized();
-	if (error)
-		return error;
-
-	error = drm_pci_init(nouveau_drm_driver, NULL);
-	if (error) {
-		aprint_error("nouveau: failed to init pci: %d\n", error);
-		return error;
-	}
-
 	nouveau_objects_init();
 	nouveau_devices_init();
-#if 0				/* XXX nouveau acpi */
-	nouveau_register_dsm_handler();
-#endif
 	drm_sysctl_init(&nouveau_def);
 
 	return 0;
-}
-
-int	nouveau_guarantee_initialized(void); /* XXX */
-int
-nouveau_guarantee_initialized(void)
-{
-#ifdef _MODULE
-	return 0;
-#else
-	static ONCE_DECL(nouveau_init_once);
-
-	return RUN_ONCE(&nouveau_init_once, &nouveau_init);
-#endif
 }
 
 static void
@@ -99,12 +67,8 @@ nouveau_fini(void)
 {
 
 	drm_sysctl_fini(&nouveau_def);
-#if 0				/* XXX nouveau acpi */
-	nouveau_unregister_dsm_handler();
-#endif
 	nouveau_devices_fini();
 	nouveau_objects_fini();
-	drm_pci_exit(nouveau_drm_driver, NULL);
 }
 
 static int
@@ -114,11 +78,7 @@ nouveau_modcmd(modcmd_t cmd, void *arg __unused)
 
 	switch (cmd) {
 	case MODULE_CMD_INIT:
-#ifdef _MODULE
 		error = nouveau_init();
-#else
-		error = nouveau_guarantee_initialized();
-#endif
 		if (error) {
 			aprint_error("nouveau: failed to initialize: %d\n",
 			    error);
