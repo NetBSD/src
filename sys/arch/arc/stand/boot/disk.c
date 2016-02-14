8f0927452be85baecf4c6f35ce4f63ec82e5ba4f
dholland <dholland@NetBSD.org>
2016-02-14 18:01:45 +0000

    Add missing va_end. PR 50793 from David Binderman.

--- sys/arch/arc/stand/boot/disk.c
+++ sys/arch/arc/stand/boot/disk.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: disk.c,v 1.6 2011/07/17 20:54:37 joerg Exp $	*/
+/*	$NetBSD: disk.c,v 1.7 2016/02/14 18:01:45 dholland Exp $	*/
 
 /*
  * Copyright (c) 1992, 1993
@@ -112,8 +112,8 @@ diskopen(struct open_file *f, ...)
 	va_list ap;
 
 	va_start(ap, f);
-
 	device = va_arg(ap, char *);
+	va_end(ap);
 
 	/*
 	 * For NetBSD/sgimips, since we use the SGI partition map directly,
