ebe45ed9f5b6b8397eeaf7a2df2cbd8edf74da30
christos <christos@NetBSD.org>
2016-02-16 14:04:24 +0000

    No need to include "sys.h" from here; it is included from config.h

--- lib/libedit/tty.h
+++ lib/libedit/tty.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: tty.h,v 1.15 2014/05/19 19:54:12 christos Exp $	*/
+/*	$NetBSD: tty.h,v 1.16 2016/02/16 14:04:24 christos Exp $	*/
 
 /*-
  * Copyright (c) 1992, 1993
@@ -40,7 +40,6 @@
 #ifndef _h_el_tty
 #define	_h_el_tty
 
-#include "sys.h"
 #include "histedit.h"
 #include <termios.h>
 #include <unistd.h>
