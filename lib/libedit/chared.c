f09cb8c626641fb1b676df3139a39de4324876d9
christos <christos@NetBSD.org>
2016-02-16 14:08:25 +0000

    cleanup chartype.h includes (Ingo Schwarze)

--- lib/libedit/chared.c
+++ lib/libedit/chared.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: chared.c,v 1.42 2016/02/16 14:06:05 christos Exp $	*/
+/*	$NetBSD: chared.c,v 1.43 2016/02/16 14:08:25 christos Exp $	*/
 
 /*-
  * Copyright (c) 1992, 1993
@@ -37,7 +37,7 @@
 #if 0
 static char sccsid[] = "@(#)chared.c	8.1 (Berkeley) 6/4/93";
 #else
-__RCSID("$NetBSD: chared.c,v 1.42 2016/02/16 14:06:05 christos Exp $");
+__RCSID("$NetBSD: chared.c,v 1.43 2016/02/16 14:08:25 christos Exp $");
 #endif
 #endif /* not lint && not SCCSID */
 
@@ -47,6 +47,7 @@ __RCSID("$NetBSD: chared.c,v 1.42 2016/02/16 14:06:05 christos Exp $");
 #include <stdlib.h>
 
 #include "histedit.h"
+#include "chartype.h"
 #include "el.h"
 
 private void ch__clearmacro (EditLine *);
--- lib/libedit/tty.c
+++ lib/libedit/tty.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: tty.c,v 1.52 2016/02/16 14:06:05 christos Exp $	*/
+/*	$NetBSD: tty.c,v 1.53 2016/02/16 14:08:25 christos Exp $	*/
 
 /*-
  * Copyright (c) 1992, 1993
@@ -37,7 +37,7 @@
 #if 0
 static char sccsid[] = "@(#)tty.c	8.1 (Berkeley) 6/4/93";
 #else
-__RCSID("$NetBSD: tty.c,v 1.52 2016/02/16 14:06:05 christos Exp $");
+__RCSID("$NetBSD: tty.c,v 1.53 2016/02/16 14:08:25 christos Exp $");
 #endif
 #endif /* not lint && not SCCSID */
 
@@ -51,6 +51,7 @@ __RCSID("$NetBSD: tty.c,v 1.52 2016/02/16 14:06:05 christos Exp $");
 #include <stdlib.h>	/* for abort */
 
 #include "histedit.h"
+#include "chartype.h"
 #include "el.h"
 #include "tty.h"
 
