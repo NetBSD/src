aefc1e4460579373ce0056f2ecc82e9f9fbf2e5c
christos <christos@NetBSD.org>
2016-02-16 15:53:48 +0000

    From Ingo Scharze: Let "el.h" include everything needed for struct editline, and don't include that stuff multiple times.  That also improves consistency, also avoids circular inclusions, and also makes it easier to follow what is going on, even though not quite as nice. But it seems like the best we can do...

--- lib/libedit/hist.c
+++ lib/libedit/hist.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: hist.c,v 1.22 2016/02/16 14:08:25 christos Exp $	*/
+/*	$NetBSD: hist.c,v 1.23 2016/02/16 15:53:48 christos Exp $	*/
 
 /*-
  * Copyright (c) 1992, 1993
@@ -37,7 +37,7 @@
 #if 0
 static char sccsid[] = "@(#)hist.c	8.1 (Berkeley) 6/4/93";
 #else
-__RCSID("$NetBSD: hist.c,v 1.22 2016/02/16 14:08:25 christos Exp $");
+__RCSID("$NetBSD: hist.c,v 1.23 2016/02/16 15:53:48 christos Exp $");
 #endif
 #endif /* not lint && not SCCSID */
 
@@ -45,9 +45,6 @@ __RCSID("$NetBSD: hist.c,v 1.22 2016/02/16 14:08:25 christos Exp $");
  * hist.c: History access functions
  */
 #include <stdlib.h>
-
-#include "histedit.h"
-#include "chartype.h"
 #include "el.h"
 
 /* hist_init():
