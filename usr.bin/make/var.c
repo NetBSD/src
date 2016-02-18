2a4bdf952b16c5329208925348c4c573c8495ceb
sjg <sjg@NetBSD.org>
2016-02-18 23:33:25 +0000

    Delete :@ loop iterator when done with it

--- usr.bin/make/var.c
+++ usr.bin/make/var.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: var.c,v 1.203 2016/02/18 20:25:08 sjg Exp $	*/
+/*	$NetBSD: var.c,v 1.204 2016/02/18 23:33:25 sjg Exp $	*/
 
 /*
  * Copyright (c) 1988, 1989, 1990, 1993
@@ -69,14 +69,14 @@
  */
 
 #ifndef MAKE_NATIVE
-static char rcsid[] = "$NetBSD: var.c,v 1.203 2016/02/18 20:25:08 sjg Exp $";
+static char rcsid[] = "$NetBSD: var.c,v 1.204 2016/02/18 23:33:25 sjg Exp $";
 #else
 #include <sys/cdefs.h>
 #ifndef lint
 #if 0
 static char sccsid[] = "@(#)var.c	8.3 (Berkeley) 3/19/94";
 #else
-__RCSID("$NetBSD: var.c,v 1.203 2016/02/18 20:25:08 sjg Exp $");
+__RCSID("$NetBSD: var.c,v 1.204 2016/02/18 23:33:25 sjg Exp $");
 #endif
 #endif /* not lint */
 #endif
@@ -2672,6 +2672,7 @@ ApplyModifiers(char *nstr, const char *tstr,
 		loop.ctxt = ctxt;
 		newStr = VarModify(ctxt, &parsestate, nstr, VarLoopExpand,
 				   &loop);
+		Var_Delete(loop.tvar, ctxt);
 		free(loop.tvar);
 		free(loop.str);
 		break;
