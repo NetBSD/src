5a2b226ebc9cc779476177720819c25b756256e7
snj <snj@NetBSD.org>
2016-02-22 11:24:18 +0000

    Pull up following revision(s) (requested by christos in ticket #1075): 	crypto/external/bsd/openssh/dist/readconf.c: patch 	crypto/external/bsd/openssh/dist/ssh.c: patch Fix CVE CVE-2016-0777 by disabling roaming completely.

--- crypto/external/bsd/openssh/dist/readconf.c
+++ crypto/external/bsd/openssh/dist/readconf.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: readconf.c,v 1.11.4.1 2015/04/30 06:07:30 riz Exp $	*/
+/*	$NetBSD: readconf.c,v 1.11.4.1.2.1 2016/02/22 11:24:18 snj Exp $	*/
 /* $OpenBSD: readconf.c,v 1.232 2015/02/16 22:13:32 djm Exp $ */
 /*
  * Author: Tatu Ylonen <ylo@cs.hut.fi>
@@ -14,7 +14,7 @@
  */
 
 #include "includes.h"
-__RCSID("$NetBSD: readconf.c,v 1.11.4.1 2015/04/30 06:07:30 riz Exp $");
+__RCSID("$NetBSD: readconf.c,v 1.11.4.1.2.1 2016/02/22 11:24:18 snj Exp $");
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/socket.h>
@@ -1745,7 +1745,7 @@ initialize_options(Options * options)
 	options->tun_remote = -1;
 	options->local_command = NULL;
 	options->permit_local_command = -1;
-	options->use_roaming = -1;
+	options->use_roaming = 0;
 	options->visual_host_key = -1;
 	options->ip_qos_interactive = -1;
 	options->ip_qos_bulk = -1;
@@ -1960,8 +1960,7 @@ fill_default_options(Options * options)
 		options->tun_remote = SSH_TUNID_ANY;
 	if (options->permit_local_command == -1)
 		options->permit_local_command = 0;
-	if (options->use_roaming == -1)
-		options->use_roaming = 1;
+	options->use_roaming = 0;
 	if (options->visual_host_key == -1)
 		options->visual_host_key = 0;
 	if (options->ip_qos_interactive == -1)
