65d7373b0570a2b7adfb64e5272efd76eb4888fe
christos <christos@NetBSD.org>
2016-02-23 14:21:37 +0000

    Add new redirection test, from kre

--- tests/bin/sh/t_redir.sh
+++ tests/bin/sh/t_redir.sh
@@ -1,4 +1,4 @@
-# $NetBSD: t_redir.sh,v 1.1 2016/02/22 19:52:03 christos Exp $
+# $NetBSD: t_redir.sh,v 1.2 2016/02/23 14:21:37 christos Exp $
 #
 # Copyright (c) 2016 The NetBSD Foundation, Inc.
 # All rights reserved.
@@ -24,6 +24,8 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 #
+# the implementation of "sh" to test
+: ${TEST_SH:="/bin/sh"}
 
 atf_test_case redir_in_case
 redir_in_case_head() {
@@ -31,10 +33,25 @@ redir_in_case_head() {
 	                "in case statements. (PR bin/48631)"
 }
 redir_in_case_body() {
-	atf_check -s exit:0 -o ignore -e ignore \
-	    -x '/bin/sh -c "case x in (whatever) >foo;; esac"'
+	atf_check -s exit:0 -o empty -e empty \
+	    ${TEST_SH} -c 'case x in (whatever) >foo;; esac'
+}
+
+atf_test_case redir_here_doc
+redir_here_doc_head() {
+	atf_set "descr" "Tests that sh(1) correctly processes 'here' doc " \
+	                "input redirections"
+}
+redir_here_doc_body() {
+	atf_check -s exit:0 -o match:'hello\\n' -e empty \
+		"${TEST_SH}" -ec '{
+					echo "cat <<EOF"
+					echo '"'"'"hello\n"'"'"'
+					echo "EOF"
+				} | '"'${TEST_SH}'"' -e'
 }
 
 atf_init_test_cases() {
 	atf_add_test_case redir_in_case
+	atf_add_test_case redir_here_doc
 }
