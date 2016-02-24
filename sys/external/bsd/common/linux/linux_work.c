9a27a5e02bee4e816c92ddc7bd22c9711993735c
skrll <skrll>
2016-02-24 22:04:15 +0000

    Move linux_work.c and workqueue.h from sys/external/bsd/drm2 to sys/external/common so that they can be used by others.
    LGTM from riastradh@
    
    These should really live outside sys/external, but that can be decided
    later

--- sys/external/bsd/common/linux/linux_work.c
+++ sys/external/bsd/common/linux/linux_work.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: linux_work.c,v 1.12 2015/11/15 22:56:24 jakllsch Exp $	*/
+/*	$NetBSD: linux_work.c,v 1.1 2016/02/24 22:04:15 skrll Exp $	*/
 
 /*-
  * Copyright (c) 2013 The NetBSD Foundation, Inc.
@@ -30,7 +30,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: linux_work.c,v 1.12 2015/11/15 22:56:24 jakllsch Exp $");
+__KERNEL_RCSID(0, "$NetBSD: linux_work.c,v 1.1 2016/02/24 22:04:15 skrll Exp $");
 
 #include <sys/types.h>
 #include <sys/param.h>
