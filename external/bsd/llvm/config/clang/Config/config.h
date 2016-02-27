c1f4a441636fd2600c913df15cb49749ddebc4df
joerg <joerg@NetBSD.org>
2016-02-27 21:47:30 +0000

    Update build system for LLVM 3.8.0rc3.

--- external/bsd/llvm/config/clang/Config/config.h
+++ external/bsd/llvm/config/clang/Config/config.h
@@ -1,13 +1,16 @@
 /* tools/clang/include/clang/Config/config.h.  Generated from config.h.in by configure.  */
 /* This generated file is for internal use. Do not include it from headers. */
 
-#ifdef CONFIG_H
+#ifdef CLANG_CONFIG_H
 #error config.h can only be included once
 #else
-#define CONFIG_H
+#define CLANG_CONFIG_H
 
 /* Bug report URL. */
 #define BUG_REPORT_URL "http://llvm.org/bugs/"
+
+/* Default OpenMP runtime used by -fopenmp. */
+#define CLANG_DEFAULT_OPENMP_RUNTIME "libomp"
 
 /* Multilib suffix for libdir. */
 #define CLANG_LIBDIR_SUFFIX ""
@@ -16,7 +19,7 @@
 #define CLANG_RESOURCE_DIR ""
 
 /* Directories clang will search for headers */
-#define C_INCLUDE_DIRS "/usr/include/clang-3.6:/usr/include"
+#define C_INCLUDE_DIRS "/usr/include/clang-3.8:/usr/include"
 
 /* Default <path> to all compiler invocations for --sysroot=<path>. */
 #define DEFAULT_SYSROOT ""
@@ -27,7 +30,7 @@
 /* Define if we have libxml2 */
 /* #undef CLANG_HAVE_LIBXML */
 
-#define PACKAGE_STRING "LLVM 3.6.1"
+#define PACKAGE_STRING "LLVM 3.8.0"
 
 /* The LLVM product name and version */
 #define BACKEND_PACKAGE_STRING PACKAGE_STRING
