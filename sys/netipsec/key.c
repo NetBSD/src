1410dff88a9804343b3ff50521d17e8590aed1f1
christos <christos@NetBSD.org>
2016-03-05 20:26:07 +0000

    kill stray &

--- sys/netipsec/key.c
+++ sys/netipsec/key.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: key.c,v 1.94 2016/03/05 20:13:40 christos Exp $	*/
+/*	$NetBSD: key.c,v 1.95 2016/03/05 20:26:07 christos Exp $	*/
 /*	$FreeBSD: src/sys/netipsec/key.c,v 1.3.2.3 2004/02/14 22:23:23 bms Exp $	*/
 /*	$KAME: key.c,v 1.191 2001/06/27 10:46:49 sakane Exp $	*/
 
@@ -32,7 +32,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: key.c,v 1.94 2016/03/05 20:13:40 christos Exp $");
+__KERNEL_RCSID(0, "$NetBSD: key.c,v 1.95 2016/03/05 20:26:07 christos Exp $");
 
 /*
  * This code is referd to RFC 2367
@@ -4283,7 +4283,7 @@ key_cmpsaidx(
 		if (key_sockaddrcmp(sa0src, sa1src, chkport) != 0) {
 			return 0;
 		}
-		if (key_sockaddrcmp(sa0dst, &sa1dst, chkport) != 0) {
+		if (key_sockaddrcmp(sa0dst, sa1dst, chkport) != 0) {
 			return 0;
 		}
 	}
