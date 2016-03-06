906c555554ac80dc253d7d98ee838503a78bea07
mlelstv <mlelstv@NetBSD.org>
2016-03-06 10:59:56 +0000

    Non-x86 doesn't have AGP. This should really be handled by config logic.

--- sys/external/bsd/drm2/drm/drm_memory.c
+++ sys/external/bsd/drm2/drm/drm_memory.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: drm_memory.c,v 1.9 2016/03/05 06:48:31 mlelstv Exp $	*/
+/*	$NetBSD: drm_memory.c,v 1.10 2016/03/06 10:59:56 mlelstv Exp $	*/
 
 /*-
  * Copyright (c) 2013 The NetBSD Foundation, Inc.
@@ -30,27 +30,33 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: drm_memory.c,v 1.9 2016/03/05 06:48:31 mlelstv Exp $");
+__KERNEL_RCSID(0, "$NetBSD: drm_memory.c,v 1.10 2016/03/06 10:59:56 mlelstv Exp $");
 
 #if defined(__i386__) || defined(__x86_64__)
-#define HAS_AGP_I810	1
-#else
-#define HAS_AGP_I810	0
-#endif
 
-#ifdef _KERNEL_OPT
-# include "agp.h"
-# if NAGP == 0
+# ifdef _KERNEL_OPT
+#  include "agp.h"
+#  if NAGP > 0
+#   include "agp_i810.h"
+#  else
+#   define NAGP_I810	0
+#  endif
+#  include "genfb.h"
+# else
+#  define NAGP_I810	1
+#  define NGENFB	0
+# endif
+
+#else
+
+# ifdef _KERNEL_OPT
 #  define NAGP_I810	0
-# elif HAS_AGP_I810 > 0
-#  include "agp_i810.h"
+#  include "genfb.h"
 # else
 #  define NAGP_I810	0
+#  define NGENFB	0
 # endif
-# include "genfb.h"
-#else
-# define NAGP_I810	HAS_AGP_I810	/* XXX WTF?  */
-# define NGENFB		0	/* XXX WTF?  */
+
 #endif
 
 #include <sys/bus.h>
