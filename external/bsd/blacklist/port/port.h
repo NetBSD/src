3a3561822d54c5cc32ecc7677d809e9a0756e2d5
christos <christos>
2016-03-11 17:17:35 +0000

    missed one blsock

--- external/bsd/blacklist/port/port.h
+++ external/bsd/blacklist/port/port.h
@@ -80,5 +80,5 @@ int clock_gettime(int, struct timespec *);
 
 #define _PATH_BLCONF "conf"
 #define _PATH_BLCONTROL "control"
-#define _PATH_BLSOCK "blsock"
+#define _PATH_BLSOCK "blacklistd.sock"
 #define _PATH_BLSTATE "blacklistd.db"
