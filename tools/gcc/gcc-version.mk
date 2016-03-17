2ec160d446a5012f257bac3a971a26ba4a87a01f
mrg <mrg@NetBSD.org>
2016-03-17 23:41:21 +0000

    set NETBSD_GCC_VERSION for GCC 5.3 separately. don't collect crtstuff anymore.

--- tools/gcc/gcc-version.mk
+++ tools/gcc/gcc-version.mk
@@ -1,5 +1,9 @@
-#	$NetBSD: gcc-version.mk,v 1.4 2015/10/16 00:41:07 christos Exp $
+#	$NetBSD: gcc-version.mk,v 1.5 2016/03/17 23:41:21 mrg Exp $
 
 # common location for tools and native build
 
+.if ${HAVE_GCC} == 48
 NETBSD_GCC_VERSION=nb3 20151015
+.else
+NETBSD_GCC_VERSION=nb1 20160317
+.endif
