1c1845c87f4a8f28ea7b89f137d64c037b79b4a0
christos <christos@NetBSD.org>
2016-03-17 17:38:14 +0000

    undo the dso protected for clang

--- lib/libc/locale/global_locale.c
+++ lib/libc/locale/global_locale.c
@@ -1,4 +1,4 @@
-/* $NetBSD: global_locale.c,v 1.23 2016/03/08 04:00:11 christos Exp $ */
+/* $NetBSD: global_locale.c,v 1.24 2016/03/17 17:38:14 christos Exp $ */
 
 /*-
  * Copyright (c)2008 Citrus Project,
@@ -28,7 +28,7 @@
 
 #include <sys/cdefs.h>
 #if defined(LIBC_SCCS) && !defined(lint)
-__RCSID("$NetBSD: global_locale.c,v 1.23 2016/03/08 04:00:11 christos Exp $");
+__RCSID("$NetBSD: global_locale.c,v 1.24 2016/03/17 17:38:14 christos Exp $");
 #endif /* LIBC_SCCS and not lint */
 
 #include <sys/types.h>
@@ -143,7 +143,10 @@ __dso_hidden const struct _locale_cache_t _C_cache = {
     .numeric_name = _lc_C_locale_name,
 };
 
-__dso_protected struct _locale _lc_global_locale = {
+#ifndef __clang__
+__dso_protected
+#endif
+struct _locale _lc_global_locale = {
     .cache = &_C_cache,
     .query = { _C_LOCALE },
     .part_name = {
@@ -171,7 +174,10 @@ __dso_protected struct _locale _lc_global_locale = {
     },
 };
 
-__dso_protected const struct _locale _lc_C_locale = {
+#ifndef __clang__
+__dso_protected
+#endif
+const struct _locale _lc_C_locale = {
     .cache = &_C_cache,
     .query = { _C_LOCALE },
     .part_name = {
