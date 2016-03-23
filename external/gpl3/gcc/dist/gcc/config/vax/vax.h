1eee2fe3859b01138b66d4a939e079b1b80762ac
christos <christos@NetBSD.org>
2016-03-23 12:45:50 +0000

    Fix vax build, now we fail in: /usr/src/lib/csu/common/crt0-common.c: In function '___start': /usr/src/lib/csu/common/crt0-common.c:184:1: internal compiler error: in dwf_regno, at dwarf2cfi.c:988  }   ^

--- external/gpl3/gcc/dist/gcc/config/vax/vax.h
+++ external/gpl3/gcc/dist/gcc/config/vax/vax.h
@@ -17,6 +17,7 @@ You should have received a copy of the GNU General Public License
 along with GCC; see the file COPYING3.  If not see
 <http://www.gnu.org/licenses/>.  */
 
+#include "vax-protos.h"
 
 /* Target CPU builtins.  */
 #define TARGET_CPU_CPP_BUILTINS()		\
