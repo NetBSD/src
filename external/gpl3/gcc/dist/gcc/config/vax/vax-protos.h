d99c91b4ed946990ef1dc8d1d0649fd080a4f69d
christos <christos@NetBSD.org>
2016-03-23 21:09:04 +0000

    need stdbool.h

--- external/gpl3/gcc/dist/gcc/config/vax/vax-protos.h
+++ external/gpl3/gcc/dist/gcc/config/vax/vax-protos.h
@@ -16,6 +16,7 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with GCC; see the file COPYING3.  If not see
 <http://www.gnu.org/licenses/>.  */
+#include <stdbool.h>
 
 extern bool legitimate_constant_address_p (rtx);
 extern bool legitimate_pic_operand_p (rtx);
