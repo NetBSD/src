2f7e481b895e551bc6ca2df6a1c3fe8a3dc2185f
christos <christos@NetBSD.org>
2016-04-12 00:16:06 +0000

    From Ingo Schwarze:
     * Delete the stubs of the XK_EXE mechanism that was never implemented.
       From a security, stability, and simplicity perspective, i would
       consider implementing it a truly terrible idea, so let's better
       get rid of it.
    
     * Do not use the local variable "num" in el_wgets() alternately for
       two completely different purposes.  Only use it for the number
       of characters read, as stated in the comment (or -1 as long as
       that number is still unknown), not for the (more or less boolean)
       return value of read_getcmd().  Actually, there is no need at
       all to save the latter return value after testing it once.
    
     * The function read_getcmd() has very unusual return values:
       It returns -1 for success and 0 for EOF/error.  Switch that around
       to 0 for success and -1 for EOF/error to be less confusing, and
       get rid of the OKCMD preprocessor macro.
    
     * Get rid of one #ifdef section in el_wgets() by using
       el->el_chared.c_macro directly at the only place
       where it is used.
    
     * Delete the unused MIN() macro.

--- lib/libedit/keymacro.h
+++ lib/libedit/keymacro.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: keymacro.h,v 1.4 2016/04/11 00:50:13 christos Exp $	*/
+/*	$NetBSD: keymacro.h,v 1.5 2016/04/12 00:16:06 christos Exp $	*/
 
 /*-
  * Copyright (c) 1992, 1993
@@ -56,7 +56,6 @@ typedef struct el_keymacro_t {
 #define	XK_CMD	0
 #define	XK_STR	1
 #define	XK_NOD	2
-#define	XK_EXE	3
 
 protected int keymacro_init(EditLine *);
 protected void keymacro_end(EditLine *);
