7d85fac39e7a3eeba8cfad837de3e299c0fa95c7
ozaki-r <ozaki-r@NetBSD.org>
2016-04-19 04:13:56 +0000

    Constify rtentry of arpresolve
    We don't need to (rather shouldn't) modify rtentry in there.

--- sys/netinet/if_inarp.h
+++ sys/netinet/if_inarp.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: if_inarp.h,v 1.48 2016/04/07 03:22:15 christos Exp $	*/
+/*	$NetBSD: if_inarp.h,v 1.49 2016/04/19 04:13:56 ozaki-r Exp $	*/
 
 /*
  * Copyright (c) 1982, 1986, 1993
@@ -75,7 +75,7 @@ MALLOC_DECLARE(M_IPARP);
 extern struct ifqueue arpintrq;
 void arp_ifinit(struct ifnet *, struct ifaddr *);
 void arp_rtrequest(int, struct rtentry *, const struct rt_addrinfo *);
-int arpresolve(struct ifnet *, struct rtentry *, struct mbuf *,
+int arpresolve(struct ifnet *, const struct rtentry *, struct mbuf *,
     const struct sockaddr *, void *, size_t);
 void arpintr(void);
 void arprequest(struct ifnet *, const struct in_addr *, const struct in_addr *,
