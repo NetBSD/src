7b9e42e870835257d8926a0123c3b6c1056c04f9
knakahara <knakahara>
2016-04-20 08:50:43 +0000

    Add init function for mbuf.
    some functions use mbuf as stack variable instead of allocating by m_get().
    They should use this function(s) to prevent access to uninitialized fields.
    
    Currently, the mbuf stack allocating functions are the following.
        + sys/dev/ic/bwi.c
          - bwi_rxeof()
          - bwi_encap()
        + sys/dev/ic/dp8390.c
          - dp8390_ipkdb_send()
        + sys/dev/pci/if_txp.c
          - txp_download_fw_section()
        + sys/dev/ppbus/if_plip.c
          - lptap()
        + sys/net/bpf.c
          - _pf_mtap2()
          - _pf_mtap_af()
          - _pf_mtap_sl_out()
        + sys/netisdn/i4b_ipr.c
          - ipr_rx_data_rdy()
          - ipr_tx_queue_empty()
    
    Reviewed by kre@n.o and christos@n.o, thanks.

--- sys/kern/uipc_mbuf.c
+++ sys/kern/uipc_mbuf.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: uipc_mbuf.c,v 1.163 2015/08/24 22:21:26 pooka Exp $	*/
+/*	$NetBSD: uipc_mbuf.c,v 1.164 2016/04/20 08:50:43 knakahara Exp $	*/
 
 /*-
  * Copyright (c) 1999, 2001 The NetBSD Foundation, Inc.
@@ -62,7 +62,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: uipc_mbuf.c,v 1.163 2015/08/24 22:21:26 pooka Exp $");
+__KERNEL_RCSID(0, "$NetBSD: uipc_mbuf.c,v 1.164 2016/04/20 08:50:43 knakahara Exp $");
 
 #ifdef _KERNEL_OPT
 #include "opt_mbuftrace.h"
@@ -583,14 +583,8 @@ m_get(int nowait, int type)
 		return NULL;
 
 	mbstat_type_add(type, 1);
-	mowner_init(m, type);
-	m->m_ext_ref = m;
-	m->m_type = type;
-	m->m_len = 0;
-	m->m_next = NULL;
-	m->m_nextpkt = NULL;
-	m->m_data = m->m_dat;
-	m->m_flags = 0;
+
+	m_hdr_init(m, type, NULL, m->m_dat, 0);
 
 	return m;
 }
@@ -604,13 +598,7 @@ m_gethdr(int nowait, int type)
 	if (m == NULL)
 		return NULL;
 
-	m->m_data = m->m_pktdat;
-	m->m_flags = M_PKTHDR;
-	m->m_pkthdr.rcvif = NULL;
-	m->m_pkthdr.len = 0;
-	m->m_pkthdr.csum_flags = 0;
-	m->m_pkthdr.csum_data = 0;
-	SLIST_INIT(&m->m_pkthdr.tags);
+	m_pkthdr_init(m);
 
 	return m;
 }
