4b837129f9201fc0c381f281fe24505bcdd46af8
ozaki-r <ozaki-r@NetBSD.org>
2016-04-23 08:54:20 +0000

    Add more tests of RTF_REJECT

--- tests/net/route/t_flags6.sh
+++ tests/net/route/t_flags6.sh
@@ -1,4 +1,4 @@
-#	$NetBSD: t_flags6.sh,v 1.3 2016/04/22 06:24:10 ozaki-r Exp $
+#	$NetBSD: t_flags6.sh,v 1.4 2016/04/23 08:54:20 ozaki-r Exp $
 #
 # Copyright (c) 2016 Internet Initiative Japan Inc.
 # All rights reserved.
@@ -203,7 +203,7 @@ test_reject()
 	# Shouldn't be created
 	check_entry_fail $IP6_PEER UH
 
-	# Gateway is lo0
+	# Gateway is lo0 (RTF_GATEWAY)
 
 	# Delete an existing route first
 	atf_check -s exit:0 -o ignore \
@@ -222,6 +222,23 @@ test_reject()
 
 	# Shouldn't be created
 	check_entry_fail $IP6_PEER UH
+
+	# Gateway is lo0 (RTF_HOST)
+
+	# Delete an existing route first
+	atf_check -s exit:0 -o ignore \
+	    rump.route delete -inet6 -net fc00::/64
+
+	atf_check -s exit:0 -o ignore \
+	    rump.route add -inet6 -host fc00::/64 ::1 -iface -reject
+	$DEBUG && rump.netstat -rn -f inet6
+
+	# Up, Host, Reject, Static
+	check_entry_flags fc00:: UHRS
+
+	atf_check -s not-exit:0 -o ignore -e match:'No route to host' \
+	    rump.ping6 -n -X 1 -c 1 $IP6_PEER
+	$DEBUG && rump.netstat -rn -f inet6
 }
 
 test_announce()
