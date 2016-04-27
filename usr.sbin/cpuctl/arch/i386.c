2aa14186aa6e4ddb96a5ecf9ef1661e10509322d
msaitoh <msaitoh@NetBSD.org>
2016-04-27 08:53:28 +0000

    Add some name from the latest Intel SDM.  - Quark X1000, Xeon E5 v4 and the future processors.

--- usr.sbin/cpuctl/arch/i386.c
+++ usr.sbin/cpuctl/arch/i386.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: i386.c,v 1.71 2016/04/27 06:58:06 msaitoh Exp $	*/
+/*	$NetBSD: i386.c,v 1.72 2016/04/27 08:53:28 msaitoh Exp $	*/
 
 /*-
  * Copyright (c) 1999, 2000, 2001, 2006, 2007, 2008 The NetBSD Foundation, Inc.
@@ -57,7 +57,7 @@
 
 #include <sys/cdefs.h>
 #ifndef lint
-__RCSID("$NetBSD: i386.c,v 1.71 2016/04/27 06:58:06 msaitoh Exp $");
+__RCSID("$NetBSD: i386.c,v 1.72 2016/04/27 08:53:28 msaitoh Exp $");
 #endif /* not lint */
 
 #include <sys/types.h>
@@ -300,7 +300,7 @@ const struct cpu_cpuid_nameclass i386_cpuid_cpus[] = {
 				"Pentium (P54C)", "Pentium (P24T)",
 				"Pentium/MMX", "Pentium", 0,
 				"Pentium (P54C)", "Pentium/MMX (Tillamook)",
-				0, 0, 0, 0, 0, 0, 0,
+				"Quark X1000", 0, 0, 0, 0, 0, 0,
 			},
 			"Pentium",	/* Default */
 			NULL,
@@ -370,12 +370,17 @@ const struct cpu_cpuid_nameclass i386_cpuid_cpus[] = {
 				[0x4c] = "Atom X[57]-Z8000 (Airmont)",
 				[0x4d] = "Atom C2000",
 				[0x4e] = "6th gen Core, Xeon E3-1[25]00 v5 (Skylake)",
-				[0x4f] = "Future gen Xeon (Broadwell)",
+				[0x4f] = "Xeon E5 v4 (Broadwell)",
+				[0x55] = "Future Xeon",
 				[0x56] = "Xeon D-1500 (Broadwell)",
 				[0x57] = "Next gen Xeon Phi",
 				[0x5a] = "Atom E3500",
+				[0x5c] = "Next Atom (Goldmont)",
 				[0x5d] = "Atom X3-C3000 (Silvermont)",
 				[0x5e] = "6th gen Core, Xeon E3-1[25]00 v5 (Skylake)",
+				[0x5f] = "Future Atom (Goldmont)",
+				[0x8e] = "Future Core",
+				[0x9e] = "Future Core",
 			},
 			"Pentium Pro, II or III",	/* Default */
 			NULL,
