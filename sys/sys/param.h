0a548c08f272f440c860c22ada8cdb0e59c4e72b
christos <christos>
2016-04-29 01:14:23 +0000

    For struct ifnet if_transmit.

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.493 2016/04/23 12:53:52 skrll Exp $	*/
+/*	$NetBSD: param.h,v 1.494 2016/04/29 01:14:23 christos Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -67,7 +67,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	799002800	/* NetBSD 7.99.28 */
+#define	__NetBSD_Version__	799002900	/* NetBSD 7.99.29 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
