bf6c51a39e43a04227bf286fcb763aa6568ac294
kre <kre>
2016-05-09 20:36:07 +0000

    PR bin/48489 -- Shell "simple commands" are now not allowed to be completely empty, they must have something (var assignment, redirect, or command, or multiple of those) to avoid a syntax error.  This matches the requirements of the grammar in the standard.   Correct the parser (using FreeBSD's sh as a guide) and update the man page to remove text that noted a couple of places this was previously OK.
    OK christos@

--- bin/sh/parser.c
+++ bin/sh/parser.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: parser.c,v 1.118 2016/05/03 03:16:55 kre Exp $	*/
+/*	$NetBSD: parser.c,v 1.119 2016/05/09 20:36:07 kre Exp $	*/
 
 /*-
  * Copyright (c) 1991, 1993
@@ -37,7 +37,7 @@
 #if 0
 static char sccsid[] = "@(#)parser.c	8.7 (Berkeley) 5/16/95";
 #else
-__RCSID("$NetBSD: parser.c,v 1.118 2016/05/03 03:16:55 kre Exp $");
+__RCSID("$NetBSD: parser.c,v 1.119 2016/05/09 20:36:07 kre Exp $");
 #endif
 #endif /* not lint */
 
@@ -498,20 +498,32 @@ TRACE(("expecting DO got %s %s\n", tokname[got], got == TWORD ? wordtext : ""));
 			synexpect(TEND, 0);
 		checkkwd = 1;
 		break;
-	/* Handle an empty command like other simple commands.  */
+
 	case TSEMI:
+	case TAND:
+	case TOR:
+	case TPIPE:
+	case TNL:
+	case TEOF:
+	case TRP:
 		/*
-		 * An empty command before a ; doesn't make much sense, and
-		 * should certainly be disallowed in the case of `if ;'.
+		 * simple commands must have something in them,
+		 * either a word (which at this point includes a=b)
+		 * or a redirection.  If we reached the end of the
+		 * command (which one of these tokens indicates)
+		 * when we are just starting, and have not had a
+		 * redirect, then ...
+		 *
+		 * nb: it is still possible to end up with empty
+		 * simple commands, if the "command" is a var
+		 * expansion that produces nothing
+		 *	X= ; $X && $X
+		 * -->          &&
+		 * I am not sure if this is intended to be legal or not.
 		 */
 		if (!redir)
 			synexpect(-1, 0);
-	case TAND:
-	case TOR:
-	case TNL:
-	case TEOF:
 	case TWORD:
-	case TRP:
 		tokpushback++;
 		n1 = simplecmd(rpp, redir);
 		goto checkneg;
