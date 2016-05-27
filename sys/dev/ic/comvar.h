ea9878fec4fc8d01c6d812e0f9fa589c0fffcba4
bouyer <bouyer@NetBSD.org>
2016-05-27 20:01:49 +0000

    The UART in the allwiner SoCs is not full-compatible with the 16550, and it's not a 16750 either. Like the 16750 it has the IIR_BUSY interrupt, which is triggered when writing to LCR while the chip can't accept it. But unlike the 16750, it has a specific register, HALT, to allow writing to the LCR and divisor registers, and then commit the changes. Tested on an A20 SoC, changing the baud rate while keeping the tty device open and incoming data.

--- sys/dev/ic/comvar.h
+++ sys/dev/ic/comvar.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: comvar.h,v 1.81 2015/05/03 17:22:54 jmcneill Exp $	*/
+/*	$NetBSD: comvar.h,v 1.82 2016/05/27 20:01:49 bouyer Exp $	*/
 
 /*
  * Copyright (c) 1996 Christopher G. Demetriou.  All rights reserved.
@@ -94,6 +94,12 @@ int com_is_console(bus_space_tag_t, bus_addr_t, bus_space_handle_t *);
 #ifdef	COM_16750
 #define	COM_REG_USR		31
 #endif
+#ifdef	COM_AWIN
+#define	COM_REG_USR		31
+#define	COM_REG_TFL		32
+#define	COM_REG_RFL		33
+#define	COM_REG_HALT		41
+#endif
 
 struct com_regs {
 	bus_space_tag_t		cr_iot;
@@ -140,6 +146,12 @@ extern const bus_size_t com_std_map[16];
 #define	COM_REG_MDR1		8
 #ifdef	COM_16750
 #define COM_REG_USR		com_usr
+#endif
+#ifdef	COM_AWIN
+#define COM_REG_USR		com_usr
+#define	COM_REG_TFL		com_tfl
+#define	COM_REG_RFL		com_rfl
+#define	COM_REG_HALT		com_halt
 #endif
 
 struct com_regs {
