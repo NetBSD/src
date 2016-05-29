34eb4a3dc34fa8f48bff370869f9852a0fef06da
alnsn <alnsn@NetBSD.org>
2016-05-29 18:15:05 +0000

    Version bump after sljit import.
    New version of sljit is not binary compatible with the previous
    version.

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.494 2016/04/29 01:14:23 christos Exp $	*/
+/*	$NetBSD: param.h,v 1.495 2016/05/29 18:15:05 alnsn Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -67,7 +67,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	799002900	/* NetBSD 7.99.29 */
+#define	__NetBSD_Version__	799003000	/* NetBSD 7.99.29 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
