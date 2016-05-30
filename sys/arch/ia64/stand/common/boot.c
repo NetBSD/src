626c0685a486fb496435bdd94ea06ec2a2f82558
dholland <dholland@NetBSD.org>
2016-05-30 02:32:19 +0000

    PR 51183 David Binderman: simplify redundant conditional

--- sys/arch/ia64/stand/common/boot.c
+++ sys/arch/ia64/stand/common/boot.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: boot.c,v 1.7 2014/04/08 21:51:06 martin Exp $	*/
+/*	$NetBSD: boot.c,v 1.8 2016/05/30 02:32:19 dholland Exp $	*/
 
 /*-
  * Copyright (c) 1998 Michael Smith <msmith@freebsd.org>
@@ -318,7 +318,7 @@ getrootmount(char *rootdev)
 	while ((*cp != 0) && isspace(*cp))
 	    cp++;
 	/* must have /<space> to be root */
-	if ((*cp == 0) || (*cp != '/') || !isspace(*(cp + 1)))
+	if ((*cp != '/') || !isspace(*(cp + 1)))
 	    continue;
 	/* skip whitespace up to fstype */
 	cp += 2;
