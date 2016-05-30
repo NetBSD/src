c6d3db4b146ac906b3077ece633a78e0f52bf7b8
martin <martin@NetBSD.org>
2016-05-30 10:37:14 +0000

    David Binderman in PR port-sparc/51188: simplify while condition

--- sys/arch/sparc/stand/ofwboot/boot.c
+++ sys/arch/sparc/stand/ofwboot/boot.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: boot.c,v 1.31 2014/06/28 09:16:18 rtr Exp $	*/
+/*	$NetBSD: boot.c,v 1.32 2016/05/30 10:37:14 martin Exp $	*/
 
 /*
  * Copyright (c) 1997, 1999 Eduardo E. Horvath.  All rights reserved.
@@ -141,7 +141,7 @@ bootoptions(const char *ap, char *loaddev, char *kernel, char *options)
 		}
 		end1 = ap;
 
-		while (*ap != '\0' && *ap == ' ') {
+		while (*ap == ' ') {
 			ap++;
 		}
 
