3bc179bd28d408a050e8c412e8699f94359ff24b
christos <christos>
2016-06-01 22:27:49 +0000

    Put the sanitizer libraries before everything else, since it needs to be the first shared object.

--- external/gpl3/gcc/dist/gcc/config/netbsd-elf.h
+++ external/gpl3/gcc/dist/gcc/config/netbsd-elf.h
@@ -85,7 +85,10 @@ along with GCC; see the file COPYING3.  If not see
      %{!static: \
        %{rdynamic:-export-dynamic} \
        -dynamic-linker /usr/libexec/ld.elf_so} \
-     %{static:-static}}"
+     %{static:-static}} \
+   %{!nostdlib:%{!nodefaultlibs:\
+     %{%:sanitize(address): -lasan } \
+     %{%:sanitize(undefined): -lubsan}}}"
 
 /* Use --as-needed -lgcc_s for eh support.  */
 #ifdef HAVE_LD_AS_NEEDED
