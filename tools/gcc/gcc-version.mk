7ba77df77c60351ce315672e7fc0ddd460400d83
mrg <mrg@NetBSD.org>
2016-06-07 08:12:13 +0000

    set gcc 5.4 netbsd version to 2016-06-06

--- tools/gcc/gcc-version.mk
+++ tools/gcc/gcc-version.mk
@@ -1,9 +1,9 @@
-#	$NetBSD: gcc-version.mk,v 1.5 2016/03/17 23:41:21 mrg Exp $
+#	$NetBSD: gcc-version.mk,v 1.6 2016/06/07 08:12:13 mrg Exp $
 
 # common location for tools and native build
 
 .if ${HAVE_GCC} == 48
 NETBSD_GCC_VERSION=nb3 20151015
 .else
-NETBSD_GCC_VERSION=nb1 20160317
+NETBSD_GCC_VERSION=nb1 20160606
 .endif
