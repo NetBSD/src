fa9642631efe0e7eb0d23d5f7a308e82defe3f11
christos <christos>
2016-06-08 01:19:05 +0000

    Port to linux

--- usr.sbin/mopd/moptrace/moptrace.c
+++ usr.sbin/mopd/moptrace/moptrace.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: moptrace.c,v 1.11 2011/08/30 19:49:11 joerg Exp $	*/
+/*	$NetBSD: moptrace.c,v 1.12 2016/06/08 01:19:05 christos Exp $	*/
 
 /*
  * Copyright (c) 1993-95 Mats O Jansson.  All rights reserved.
@@ -24,9 +24,9 @@
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include <sys/cdefs.h>
+#include "port.h"
 #ifndef lint
-__RCSID("$NetBSD: moptrace.c,v 1.11 2011/08/30 19:49:11 joerg Exp $");
+__RCSID("$NetBSD: moptrace.c,v 1.12 2016/06/08 01:19:05 christos Exp $");
 #endif
 
 /*
