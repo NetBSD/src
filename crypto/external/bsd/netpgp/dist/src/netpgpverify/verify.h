c1589a907caa7cbecb0589c184a2dd7e53bfe1c9
agc <agc@NetBSD.org>
2016-06-15 20:34:28 +0000

    Update netpgpverify to 20160617
    Sync with changes in pkgsrc
    
    + don't assume a string is NUL-terminated - use fwrite(3) with a specific size

--- crypto/external/bsd/netpgp/dist/src/netpgpverify/verify.h
+++ crypto/external/bsd/netpgp/dist/src/netpgpverify/verify.h
@@ -23,9 +23,9 @@
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 #ifndef NETPGP_VERIFY_H_
-#define NETPGP_VERIFY_H_	20160616
+#define NETPGP_VERIFY_H_	20160617
 
-#define NETPGPVERIFY_VERSION	"netpgpverify portable 20160616"
+#define NETPGPVERIFY_VERSION	"netpgpverify portable 20160617"
 
 #include <sys/types.h>
 
