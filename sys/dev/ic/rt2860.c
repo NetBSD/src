eb487b03cfac02809ba318469cfd4db9d8a9e68e
riastradh <riastradh@NetBSD.org>
2016-06-16 15:51:13 +0000

    Use m_set_rcvif, not M_SETCTX.
    M_SETCTX is only for driver-private use of rcvif, whereas here we
    really mean to set the receiving interface with m_set_rcvif.

--- sys/dev/ic/rt2860.c
+++ sys/dev/ic/rt2860.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: rt2860.c,v 1.13 2016/06/16 15:21:49 nonaka Exp $	*/
+/*	$NetBSD: rt2860.c,v 1.14 2016/06/16 15:51:13 riastradh Exp $	*/
 /*	$OpenBSD: rt2860.c,v 1.90 2016/04/13 10:49:26 mpi Exp $	*/
 
 /*-
@@ -23,7 +23,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: rt2860.c,v 1.13 2016/06/16 15:21:49 nonaka Exp $");
+__KERNEL_RCSID(0, "$NetBSD: rt2860.c,v 1.14 2016/06/16 15:51:13 riastradh Exp $");
 
 #include <sys/param.h>
 #include <sys/sockio.h>
@@ -1337,7 +1337,7 @@ rt2860_rx_intr(struct rt2860_softc *sc)
 
 		/* finalize mbuf */
 		m->m_data = (void *)(rxwi + 1);
-		M_SETCTX(m, ifp);
+		m_set_rcvif(m, ifp);
 		m->m_pkthdr.len = m->m_len = le16toh(rxwi->len) & 0xfff;
 
 		wh = mtod(m, struct ieee80211_frame *);
