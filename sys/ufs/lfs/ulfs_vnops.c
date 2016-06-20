d3a125a547604c7bdb1cf07d5ad5cca5fb70f1b2
dholland <dholland@NetBSD.org>
2016-06-20 02:42:08 +0000

    With the previous we seem to have the changes from -r1.225 of ufs_vnops.c. (as that was stuff from moving ffs to the new vcache and lfs has also been moved, this is not surprising)

--- sys/ufs/lfs/ulfs_vnops.c
+++ sys/ufs/lfs/ulfs_vnops.c
@@ -1,5 +1,5 @@
-/*	$NetBSD: ulfs_vnops.c,v 1.42 2016/06/20 02:31:47 dholland Exp $	*/
-/*  from NetBSD: ufs_vnops.c,v 1.224 2014/10/29 01:13:28 christos Exp  */
+/*	$NetBSD: ulfs_vnops.c,v 1.43 2016/06/20 02:42:08 dholland Exp $	*/
+/*  from NetBSD: ufs_vnops.c,v 1.225 2015/03/17 09:39:29 hannken Exp  */
 
 /*-
  * Copyright (c) 2008 The NetBSD Foundation, Inc.
@@ -67,7 +67,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: ulfs_vnops.c,v 1.42 2016/06/20 02:31:47 dholland Exp $");
+__KERNEL_RCSID(0, "$NetBSD: ulfs_vnops.c,v 1.43 2016/06/20 02:42:08 dholland Exp $");
 
 #if defined(_KERNEL_OPT)
 #include "opt_lfs.h"
