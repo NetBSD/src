4363fe269e3e46de99105fb4d5c79ba3db3f15ed
ozaki-r <ozaki-r@NetBSD.org>
2016-06-21 03:34:22 +0000

    Bump kernel version for ip_moptions and ip6_moptions changes
    And maybe if_output_lock changes also needed a bump?

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.497 2016/06/16 02:54:41 ozaki-r Exp $	*/
+/*	$NetBSD: param.h,v 1.498 2016/06/21 03:34:22 ozaki-r Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -67,7 +67,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	799003100	/* NetBSD 7.99.31 */
+#define	__NetBSD_Version__	799003200	/* NetBSD 7.99.32 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
