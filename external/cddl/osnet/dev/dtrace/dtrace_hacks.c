8e039bf902e32493775888e904af971b9db66b63
pgoyette <pgoyette@NetBSD.org>
2016-06-23 06:44:52 +0000

    One more removal of a redefined symbol while loading dtrace module.
    Should finish the issues with PR kern/51265

--- external/cddl/osnet/dev/dtrace/dtrace_hacks.c
+++ external/cddl/osnet/dev/dtrace/dtrace_hacks.c
@@ -1,11 +1,9 @@
-/*	$NetBSD: dtrace_hacks.c,v 1.4 2016/06/23 04:35:35 pgoyette Exp $	*/
+/*	$NetBSD: dtrace_hacks.c,v 1.5 2016/06/23 06:44:52 pgoyette Exp $	*/
 
 /* $FreeBSD: src/sys/cddl/dev/dtrace/dtrace_hacks.c,v 1.1.4.1 2009/08/03 08:13:06 kensmith Exp $ */
 /* XXX Hacks.... */
 
 dtrace_cacheid_t dtrace_predcache_id;
-
-cpu_core_t	cpu_core[MAXCPUS];
 
 boolean_t
 priv_policy_only(const cred_t *a, int b, boolean_t c)
