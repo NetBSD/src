30407759e187dcb9808f43dcacdbecdbb495792f
pgoyette <pgoyette@NetBSD.org>
2016-06-23 04:35:35 +0000

    Import fix from FreeBSD - see [1], [2] references below...
    Fix DTrace's panic() action.
    
    It would previously call into some unfinished Solaris compatibility code
    and return without actually calling panic(9). The compatibility code is
    unneeded, however, so just remove it and have dtrace_panic() call vpanic(9)
    directly.
    
    Fixes immediate problem in PR-51265.  However, this now reveals another
    redefined symbol 'strpbrk' in the required module "solaris".
    
    
    [1] https://svnweb.freebsd.org/changeset/base/281916
    [2] http://lists.freebsd.org/pipermail/svn-src-head/2015-April/071019.html

--- external/cddl/osnet/dev/dtrace/dtrace_hacks.c
+++ external/cddl/osnet/dev/dtrace/dtrace_hacks.c
@@ -1,12 +1,9 @@
-/*	$NetBSD: dtrace_hacks.c,v 1.3 2010/02/28 07:01:43 darran Exp $	*/
+/*	$NetBSD: dtrace_hacks.c,v 1.4 2016/06/23 04:35:35 pgoyette Exp $	*/
 
 /* $FreeBSD: src/sys/cddl/dev/dtrace/dtrace_hacks.c,v 1.1.4.1 2009/08/03 08:13:06 kensmith Exp $ */
 /* XXX Hacks.... */
 
 dtrace_cacheid_t dtrace_predcache_id;
-
-int panic_quiesce;
-char panic_stack[PANICSTKSIZE];
 
 cpu_core_t	cpu_core[MAXCPUS];
 
