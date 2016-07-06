9323cda8d2b703820b95993032b120b98d102377
ozaki-r <ozaki-r@NetBSD.org>
2016-07-06 08:42:34 +0000

    Switch the IPv4 address list to pslist(9)
    Note that we leave the old list just in case; it seems there are some
    kvm(3) users accessing the list. We can remove it later if we confirmed
    nobody does actually.

--- sys/netipsec/key.c
+++ sys/netipsec/key.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: key.c,v 1.98 2016/07/04 06:48:14 ozaki-r Exp $	*/
+/*	$NetBSD: key.c,v 1.99 2016/07/06 08:42:34 ozaki-r Exp $	*/
 /*	$FreeBSD: src/sys/netipsec/key.c,v 1.3.2.3 2004/02/14 22:23:23 bms Exp $	*/
 /*	$KAME: key.c,v 1.191 2001/06/27 10:46:49 sakane Exp $	*/
 
@@ -32,7 +32,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: key.c,v 1.98 2016/07/04 06:48:14 ozaki-r Exp $");
+__KERNEL_RCSID(0, "$NetBSD: key.c,v 1.99 2016/07/06 08:42:34 ozaki-r Exp $");
 
 /*
  * This code is referd to RFC 2367
@@ -4116,7 +4116,7 @@ key_ismyaddr(const struct sockaddr *sa)
 {
 #ifdef INET
 	const struct sockaddr_in *sin;
-	const struct in_ifaddr *ia;
+	struct in_ifaddr *ia;
 #endif
 
 	/* sanity check */
@@ -4127,9 +4127,7 @@ key_ismyaddr(const struct sockaddr *sa)
 #ifdef INET
 	case AF_INET:
 		sin = (const struct sockaddr_in *)sa;
-		for (ia = in_ifaddrhead.tqh_first; ia;
-		     ia = ia->ia_link.tqe_next)
-		{
+		IN_ADDRLIST_READER_FOREACH(ia) {
 			if (sin->sin_family == ia->ia_addr.sin_family &&
 			    sin->sin_len == ia->ia_addr.sin_len &&
 			    sin->sin_addr.s_addr == ia->ia_addr.sin_addr.s_addr)
