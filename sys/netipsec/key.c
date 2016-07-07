07e3555eef9dcfc72b07fd675a67ad3e7f8a9664
ozaki-r <ozaki-r@NetBSD.org>
2016-07-07 06:59:30 +0000

    Restore const qualifier dropped due to switching to IN_ADDRLIST_READER_FOREACH
    IN_ADDRLIST_READER_FOREACH (pslist(9)) now allows const variables.

--- sys/netipsec/key.c
+++ sys/netipsec/key.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: key.c,v 1.99 2016/07/06 08:42:34 ozaki-r Exp $	*/
+/*	$NetBSD: key.c,v 1.100 2016/07/07 06:59:30 ozaki-r Exp $	*/
 /*	$FreeBSD: src/sys/netipsec/key.c,v 1.3.2.3 2004/02/14 22:23:23 bms Exp $	*/
 /*	$KAME: key.c,v 1.191 2001/06/27 10:46:49 sakane Exp $	*/
 
@@ -32,7 +32,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: key.c,v 1.99 2016/07/06 08:42:34 ozaki-r Exp $");
+__KERNEL_RCSID(0, "$NetBSD: key.c,v 1.100 2016/07/07 06:59:30 ozaki-r Exp $");
 
 /*
  * This code is referd to RFC 2367
@@ -4116,7 +4116,7 @@ key_ismyaddr(const struct sockaddr *sa)
 {
 #ifdef INET
 	const struct sockaddr_in *sin;
-	struct in_ifaddr *ia;
+	const struct in_ifaddr *ia;
 #endif
 
 	/* sanity check */
