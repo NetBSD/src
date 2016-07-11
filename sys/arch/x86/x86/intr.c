be9b6723185202b8fd814d9e9fcde1ed814ca9b8
knakahara <knakahara@NetBSD.org>
2016-07-11 23:09:34 +0000

    should use strlcpy instead of strncpy.
    pointed out by dholland@n.o.

--- sys/arch/x86/x86/intr.c
+++ sys/arch/x86/x86/intr.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: intr.c,v 1.93 2016/07/11 09:42:20 knakahara Exp $	*/
+/*	$NetBSD: intr.c,v 1.94 2016/07/11 23:09:34 knakahara Exp $	*/
 
 /*-
  * Copyright (c) 2007, 2008, 2009 The NetBSD Foundation, Inc.
@@ -133,7 +133,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: intr.c,v 1.93 2016/07/11 09:42:20 knakahara Exp $");
+__KERNEL_RCSID(0, "$NetBSD: intr.c,v 1.94 2016/07/11 23:09:34 knakahara Exp $");
 
 #include "opt_intrdebug.h"
 #include "opt_multiprocessor.h"
@@ -2103,7 +2103,7 @@ interrupt_get_devname(const char *intrid, char *buf, size_t len)
 	}
 	slot = ih->ih_slot;
 	isp = ih->ih_cpu->ci_isources[slot];
-	strncpy(buf, isp->is_xname, len);
+	strlcpy(buf, isp->is_xname, len);
 
  out:
 	mutex_exit(&cpu_lock);
