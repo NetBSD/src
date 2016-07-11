dca032f9f47d2f7f899966e7eab367241319ecbb
ozaki-r <ozaki-r@NetBSD.org>
2016-07-11 07:37:00 +0000

    Run timers in workqueue
    Timers (such as nd6_timer) typically free/destroy some data in callout
    (softint). If we apply psz/psref for such data, we cannot do free/destroy
    process in there because synchronization of psz/psref cannot be used in
    softint. So run timer callbacks in workqueue works (normal LWP context).
    
    Doing workqueue_enqueue a work twice (i.e., call workqueue_enqueue before
    a previous task is scheduled) isn't allowed. For nd6_timer and
    rt_timer_timer, this doesn't happen because callout_reset is called only
    from workqueue's work. OTOH, ip{,6}flow_slowtimo's callout can be called
    before its work starts and completes because the callout is periodically
    called regardless of completion of the work. To avoid such a situation,
    add a flag for each protocol; the flag is set true when a work is
    enqueued and set false after the work finished. workqueue_enqueue is
    called only if the flag is false.
    
    Proposed on tech-net and tech-kern.

--- sys/net/route.c
+++ sys/net/route.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: route.c,v 1.169 2016/07/07 06:55:43 msaitoh Exp $	*/
+/*	$NetBSD: route.c,v 1.170 2016/07/11 07:37:00 ozaki-r Exp $	*/
 
 /*-
  * Copyright (c) 1998, 2008 The NetBSD Foundation, Inc.
@@ -96,7 +96,7 @@
 #endif
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: route.c,v 1.169 2016/07/07 06:55:43 msaitoh Exp $");
+__KERNEL_RCSID(0, "$NetBSD: route.c,v 1.170 2016/07/11 07:37:00 ozaki-r Exp $");
 
 #include <sys/param.h>
 #ifdef RTFLUSH_DEBUG
@@ -114,6 +114,7 @@ __KERNEL_RCSID(0, "$NetBSD: route.c,v 1.169 2016/07/07 06:55:43 msaitoh Exp $");
 #include <sys/ioctl.h>
 #include <sys/pool.h>
 #include <sys/kauth.h>
+#include <sys/workqueue.h>
 
 #include <net/if.h>
 #include <net/if_dl.h>
@@ -136,6 +137,8 @@ static struct pool	rtentry_pool;
 static struct pool	rttimer_pool;
 
 static struct callout	rt_timer_ch; /* callout for rt_timer_timer() */
+struct workqueue	*rt_timer_wq;
+struct work		rt_timer_wk;
 
 #ifdef RTFLUSH_DEBUG
 static int _rtcache_debug = 0;
@@ -1147,14 +1150,22 @@ static int rt_init_done = 0;
  * that this is run when the first queue is added...
  */
 
+static void rt_timer_work(struct work *, void *);
+
 void
 rt_timer_init(void)
 {
+	int error;
+
 	assert(rt_init_done == 0);
 
 	LIST_INIT(&rttimer_queue_head);
 	callout_init(&rt_timer_ch, 0);
 	callout_reset(&rt_timer_ch, hz, rt_timer_timer, NULL);
+	error = workqueue_create(&rt_timer_wq, "rt_timer",
+	    rt_timer_work, NULL, PRI_SOFTNET, IPL_SOFTNET, WQ_MPSAFE);
+	if (error)
+		panic("%s: workqueue_create failed (%d)\n", __func__, error);
 	rt_init_done = 1;
 }
 
@@ -1287,9 +1298,8 @@ rt_timer_add(struct rtentry *rt,
 	return 0;
 }
 
-/* ARGSUSED */
-void
-rt_timer_timer(void *arg)
+static void
+rt_timer_work(struct work *wk, void *arg)
 {
 	struct rttimer_queue *rtq;
 	struct rttimer *r;
@@ -1313,6 +1323,13 @@ rt_timer_timer(void *arg)
 	splx(s);
 
 	callout_reset(&rt_timer_ch, hz, rt_timer_timer, NULL);
+}
+
+void
+rt_timer_timer(void *arg)
+{
+
+	workqueue_enqueue(rt_timer_wq, &rt_timer_wk, NULL);
 }
 
 static struct rtentry *
