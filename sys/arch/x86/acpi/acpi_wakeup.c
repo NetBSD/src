b36512e54b660c2d9be9624044c73d02943ea77e
maxv <maxv@NetBSD.org>
2016-07-27 13:04:28 +0000

    Call cpu_init_msrs on i386 when waking up. Currently it does not change anything, since MSR_EFER is already enabled earlier. But if we add new MSRs in the future, we will want them when waking up as well.

--- sys/arch/x86/acpi/acpi_wakeup.c
+++ sys/arch/x86/acpi/acpi_wakeup.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: acpi_wakeup.c,v 1.40 2016/07/24 14:09:22 maxv Exp $	*/
+/*	$NetBSD: acpi_wakeup.c,v 1.41 2016/07/27 13:04:28 maxv Exp $	*/
 
 /*-
  * Copyright (c) 2002, 2011 The NetBSD Foundation, Inc.
@@ -30,7 +30,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: acpi_wakeup.c,v 1.40 2016/07/24 14:09:22 maxv Exp $");
+__KERNEL_RCSID(0, "$NetBSD: acpi_wakeup.c,v 1.41 2016/07/27 13:04:28 maxv Exp $");
 
 /*-
  * Copyright (c) 2001 Takanori Watanabe <takawata@jp.freebsd.org>
@@ -62,7 +62,7 @@ __KERNEL_RCSID(0, "$NetBSD: acpi_wakeup.c,v 1.40 2016/07/24 14:09:22 maxv Exp $"
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: acpi_wakeup.c,v 1.40 2016/07/24 14:09:22 maxv Exp $");
+__KERNEL_RCSID(0, "$NetBSD: acpi_wakeup.c,v 1.41 2016/07/27 13:04:28 maxv Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -262,10 +262,9 @@ acpi_cpu_sleep(struct cpu_info *ci)
 		return;
 
 	/* Execute Wakeup */
-#ifndef __i386__
 	cpu_init_msrs(ci, false);
-#endif
 	fpuinit(ci);
+
 #if NLAPIC > 0
 	lapic_enable();
 	lapic_set_lvt();
