a25bc2ccdc2449014ebb0183d7449d49351f9d87
kre <kre@NetBSD.org>
2016-08-09 20:03:05 +0000

    Revert previous.   This work isn't complete enough to include yet, and the build of current really does need to go back to a working state.

--- sys/ufs/ext2fs/ext2fs_extern.h
+++ sys/ufs/ext2fs/ext2fs_extern.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: ext2fs_extern.h,v 1.50 2016/08/09 07:15:35 christos Exp $	*/
+/*	$NetBSD: ext2fs_extern.h,v 1.51 2016/08/09 20:03:05 kre Exp $	*/
 
 /*-
  * Copyright (c) 1991, 1993, 1994
@@ -79,7 +79,6 @@ struct mbuf;
 struct componentname;
 struct ufs_lookup_results;
 struct ext2fs_searchslot;
-struct ext2fs_direct;
 
 extern struct pool ext2fs_inode_pool;		/* memory pool for inodes */
 extern struct pool ext2fs_dinode_pool;		/* memory pool for dinodes */
@@ -133,8 +132,6 @@ int ext2fs_dirremove(struct vnode *, const struct ufs_lookup_results *,
 int ext2fs_dirrewrite(struct inode *, const struct ufs_lookup_results *,
 			   struct inode *, struct componentname *);
 int ext2fs_dirempty(struct inode *, ino_t, kauth_cred_t);
-int ext2fs_add_entry(struct vnode *, struct ext2fs_direct *,
-    const struct ufs_lookup_results *); 
 
 /* ext2fs_subr.c */
 int ext2fs_blkatoff(struct vnode *, off_t, char **, struct buf **);
@@ -187,10 +184,6 @@ int ext2fs_htree_hash(const char *, int, uint32_t *, int, uint32_t *,
 int ext2fs_htree_has_idx(struct inode *);
 int ext2fs_htree_lookup(struct inode *, const char *, int, struct buf **,
     int *, doff_t *, doff_t *, doff_t *, struct ext2fs_searchslot *);
-int ext2fs_htree_create_index(struct vnode *, struct componentname *,
-    struct ext2fs_direct *);
-int ext2fs_htree_add_entry(struct vnode *, struct ext2fs_direct *,
-    struct componentname *);
 
 __END_DECLS
 
