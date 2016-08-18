fcd78ae6697a93049d8e25d23e982108b3a74499
skrll <skrll@NetBSD.org>
2016-08-18 19:24:31 +0000

    Make the fixup code play nicely with the current exception handlers.
    From matt@

--- sys/arch/mips/mips/mips_fixup.c
+++ sys/arch/mips/mips/mips_fixup.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: mips_fixup.c,v 1.16 2016/07/11 19:17:55 skrll Exp $	*/
+/*	$NetBSD: mips_fixup.c,v 1.17 2016/08/18 19:24:31 skrll Exp $	*/
 
 /*-
  * Copyright (c) 2010 The NetBSD Foundation, Inc.
@@ -30,7 +30,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: mips_fixup.c,v 1.16 2016/07/11 19:17:55 skrll Exp $");
+__KERNEL_RCSID(0, "$NetBSD: mips_fixup.c,v 1.17 2016/08/18 19:24:31 skrll Exp $");
 
 #include "opt_mips3_wired.h"
 #include "opt_multiprocessor.h"
@@ -145,6 +145,14 @@ mips_fixup_exceptions(mips_fixup_callback_t callback, void *arg)
 				}
 				lui_insnp = NULL;
 			}
+		} else if (INSN_LOAD_P(insn)) {
+			/*
+			 * If we are loading the register used in the LUI,
+			 * then that LUI is meaningless now.
+			 */
+			size_t rt = (insn >> 16) & 31;
+			if (lui_reg == rt)
+				lui_insn = 0;
 		}
 	}
 
