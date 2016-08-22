62682f0cbc447bd9bd224e0d20e88eaad0a984e8
maya <maya@NetBSD.org>
2016-08-22 08:49:33 +0000

    add failing test for PR lib/51427 ilogb(INFINITY)=-INT_MAX, it should be INT_MAX
    while here, test raised exceptions, other values.
    
    XXX some platforms don't have exceptions

---
+++ tests/lib/libm/t_ilogb.c
@@ -0,0 +1,109 @@
+/* $NetBSD: t_ilogb.c,v 1.1 2016/08/22 08:49:33 maya Exp $ */
+
+/*-
+ * Copyright (c) 2016 The NetBSD Foundation, Inc.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to The NetBSD Foundation
+ * by Maya Rashish.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <atf-c.h>
+#include <fenv.h>
+#include <math.h>
+
+#define ATF_CHECK_RAISED_INVALID do { \
+	int r; \
+	r = feclearexcept(FE_ALL_EXCEPT); \
+	ATF_CHECK(r == FE_INVALID); \
+	} while (0)
+
+#define ATF_CHECK_RAISED_NOTHING do { \
+	int r; \
+	r = feclearexcept(FE_ALL_EXCEPT); \
+	ATF_CHECK(r == 0); \
+	} while (0)
+
+ATF_TC(ilogb);
+ATF_TC_HEAD(ilogb, tc)
+{
+	atf_tc_set_md_var(tc, "descr","Check ilogb family");
+}
+
+ATF_TC_BODY(ilogb, tc)
+{
+  atf_tc_expect_fail("PR lib/51427");
+
+	ATF_CHECK(ilogbf(0) == FP_ILOGB0);
+	ATF_CHECK_RAISED_INVALID;
+	ATF_CHECK(ilogb(0) == FP_ILOGB0);
+	ATF_CHECK_RAISED_INVALID;
+	ATF_CHECK(ilogbl(0) == FP_ILOGB0);
+	ATF_CHECK_RAISED_INVALID;
+
+	ATF_CHECK(ilogbf(-0) == FP_ILOGB0);
+	ATF_CHECK_RAISED_INVALID;
+	ATF_CHECK(ilogb(-0) == FP_ILOGB0);
+	ATF_CHECK_RAISED_INVALID;
+	ATF_CHECK(ilogbl(-0) == FP_ILOGB0);
+	ATF_CHECK_RAISED_INVALID;
+
+	ATF_CHECK(ilogbf(INFINITY) == INT_MAX);
+	ATF_CHECK_RAISED_INVALID;
+	ATF_CHECK(ilogb(INFINITY) == INT_MAX);
+	ATF_CHECK_RAISED_INVALID;
+	ATF_CHECK(ilogbl(INFINITY) == INT_MAX);
+	ATF_CHECK_RAISED_INVALID;
+
+	ATF_CHECK(ilogbf(-INFINITY) == INT_MAX);
+	ATF_CHECK_RAISED_INVALID;
+	ATF_CHECK(ilogb(-INFINITY) == INT_MAX);
+	ATF_CHECK_RAISED_INVALID;
+	ATF_CHECK(ilogbl(-INFINITY) == INT_MAX);
+	ATF_CHECK_RAISED_INVALID;
+
+	ATF_CHECK(ilogbf(2^20) == 20);
+	ATF_CHECK_RAISED_NOTHING;
+	ATF_CHECK(ilogb(2^20) == 20);
+	ATF_CHECK_RAISED_NOTHING;
+	ATF_CHECK(ilogbl(2^20) == 20);
+	ATF_CHECK_RAISED_NOTHING;
+
+#ifndef __vax__
+	ATF_CHECK(ilogbf(NAN) == FP_ILOGBNAN);
+	ATF_CHECK_RAISED_INVALID;
+	ATF_CHECK(ilogb(NAN) == FP_ILOGBNAN);
+	ATF_CHECK_RAISED_INVALID;
+	ATF_CHECK(ilogbl(NAN) == FP_ILOGBNAN);
+	ATF_CHECK_RAISED_INVALID;
+#endif
+}
+
+ATF_TP_ADD_TCS(tp)
+{
+
+	ATF_TP_ADD_TC(tp, ilogb);
+
+	return atf_no_error();
+}
