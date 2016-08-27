adf957f14cbb787d028f102b52fc2aa7280eaff6
bouyer <bouyer>
2016-08-27 14:40:35 +0000

    Pull up following revision(s) (requested by martin in ticket #1395): 	sys/ufs/ffs/ffs_vfsops.c: revision 1.340 	usr.sbin/quot/quot.c: revision 1.34 	sbin/fsdb/fsdb.c: revision 1.49 From Michael Plass: The superblock field that distinguishes between 4.2BSD and 4.4BSD inodes is really only relevant on a UFS1 file system. Make sure that it is a UFS1 fs before using fs_old_inodefmt. Note that the NetBSD newfs and mkfs utilities initialize fs_old_inodefmt even for UFS2, so problems were apparent only on file systems created by other operating systems, for example, FreeBSD.

--- usr.sbin/quot/quot.c
+++ usr.sbin/quot/quot.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: quot.c,v 1.29 2011/03/06 23:41:47 christos Exp $	*/
+/*	$NetBSD: quot.c,v 1.29.6.1 2016/08/27 14:40:35 bouyer Exp $	*/
 
 /*
  * Copyright (C) 1991, 1994 Wolfgang Solfrank.
@@ -33,7 +33,7 @@
 
 #include <sys/cdefs.h>
 #ifndef lint
-__RCSID("$NetBSD: quot.c,v 1.29 2011/03/06 23:41:47 christos Exp $");
+__RCSID("$NetBSD: quot.c,v 1.29.6.1 2016/08/27 14:40:35 bouyer Exp $");
 #endif /* not lint */
 
 #include <sys/param.h>
@@ -517,7 +517,8 @@ ffs_oldfscompat(struct fs *fs)
 {
 	int i;
 
-	if (fs->fs_old_inodefmt < FS_44INODEFMT) {
+	if (fs->fs_magic == FS_UFS1_MAGIC &&
+	    fs->fs_old_inodefmt < FS_44INODEFMT) {
 		quad_t sizepb = fs->fs_bsize;
 
 		fs->fs_maxfilesize = fs->fs_bsize * NDADDR - 1;
