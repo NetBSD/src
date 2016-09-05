ff1e102f4ce85a56172d94d0974383a83fdf3f7b
dholland <dholland@NetBSD.org>
2016-09-05 21:11:11 +0000

    Check bounds on input. From Michael Plass.

--- sys/lib/libsa/checkpasswd.c
+++ sys/lib/libsa/checkpasswd.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: checkpasswd.c,v 1.9 2011/01/06 02:45:13 jakllsch Exp $	*/
+/*	$NetBSD: checkpasswd.c,v 1.10 2016/09/05 21:11:11 dholland Exp $	*/
 
 /*-
  * Copyright (c) 1993
@@ -84,8 +84,10 @@ getpass(const char *prompt)
 			putchar('\n');
 			break;
 		default:
-			*lp++ = c;
-			putchar('*');
+			if ((size_t)(lp - buf) < sizeof(buf) - 1) {
+				*lp++ = c;
+				putchar('*');
+			}
 			break;
 		}
 	}
