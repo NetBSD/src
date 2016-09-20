97a0b1668a53c054472f0eb3d279a006949a6870
christos <christos@NetBSD.org>
2016-09-20 15:03:07 +0000

    use a define, and cast.

--- lib/libc/time/zic.c
+++ lib/libc/time/zic.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: zic.c,v 1.61 2016/09/20 13:09:08 christos Exp $	*/
+/*	$NetBSD: zic.c,v 1.62 2016/09/20 15:03:07 christos Exp $	*/
 /*
 ** This file is in the public domain, so clarified as of
 ** 2006-07-17 by Arthur David Olson.
@@ -10,7 +10,7 @@
 
 #include <sys/cdefs.h>
 #ifndef lint
-__RCSID("$NetBSD: zic.c,v 1.61 2016/09/20 13:09:08 christos Exp $");
+__RCSID("$NetBSD: zic.c,v 1.62 2016/09/20 15:03:07 christos Exp $");
 #endif /* !defined lint */
 
 #include "private.h"
@@ -437,9 +437,9 @@ growalloc(void *ptr, size_t itemsize, int nitems, int *nitems_alloc)
 	if (nitems < *nitems_alloc)
 		return ptr;
 	else {
-		static const int imax = INT_MAX < SIZE_MAX ? INT_MAX : SIZE_MAX;
-		int nitems_max = imax - WORK_AROUND_QTBUG_53071;
-		int amax = nitems_max < imax ? nitems_max : imax;
+#define IMAX (INT_MAX < SIZE_MAX ? INT_MAX : (int)SIZE_MAX)
+		int nitems_max = IMAX - WORK_AROUND_QTBUG_53071;
+		int amax = nitems_max < IMAX ? nitems_max : IMAX;
 		if ((amax - 1) / 3 * 2 < *nitems_alloc)
 			memory_exhausted(_("int overflow"));
 		*nitems_alloc = *nitems_alloc + (*nitems_alloc >> 1) + 1;
