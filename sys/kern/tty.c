e385d71ee6586a6be0dd78c4a4e58af517dccd7f
christos <christos@NetBSD.org>
2016-10-01 04:42:54 +0000

    Require exact credential match; this way even if we su to the original user that created the session, we won't match his credentials.

--- sys/kern/tty.c
+++ sys/kern/tty.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: tty.c,v 1.273 2016/10/01 03:46:00 christos Exp $	*/
+/*	$NetBSD: tty.c,v 1.274 2016/10/01 04:42:54 christos Exp $	*/
 
 /*-
  * Copyright (c) 2008 The NetBSD Foundation, Inc.
@@ -63,7 +63,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: tty.c,v 1.273 2016/10/01 03:46:00 christos Exp $");
+__KERNEL_RCSID(0, "$NetBSD: tty.c,v 1.274 2016/10/01 04:42:54 christos Exp $");
 
 #ifdef _KERNEL_OPT
 #include "opt_compat_netbsd.h"
@@ -1246,10 +1246,8 @@ ttioctl(struct tty *tp, u_long cmd, void *data, int flag, struct lwp *l)
 				return EPERM;
 			if (!isctty(p, tp))
 				return EACCES;
-			if (!proc_uidmatch(tp->t_session->s_leader->p_cred,
-			    p->p_cred))
+			if (tp->t_session->s_leader->p_cred != p->p_cred)
 				return error;
-			return error;
 		}
 		(*tp->t_linesw->l_rint)(*(u_char *)data, tp);
 		break;
