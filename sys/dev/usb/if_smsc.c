13bb1af7a44afed6d42daf2bd37b2c96aa8bbcf9
skrll <skrll@NetBSD.org>
2016-10-02 10:39:29 +0000

    Use our if_percpupq... driver works again on rpi2

--- sys/dev/usb/if_smsc.c
+++ sys/dev/usb/if_smsc.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: if_smsc.c,v 1.22.2.13 2016/07/15 08:50:59 skrll Exp $	*/
+/*	$NetBSD: if_smsc.c,v 1.22.2.14 2016/10/02 10:39:29 skrll Exp $	*/
 
 /*	$OpenBSD: if_smsc.c,v 1.4 2012/09/27 12:38:11 jsg Exp $	*/
 /* $FreeBSD: src/sys/dev/usb/net/if_smsc.c,v 1.1 2012/08/15 04:03:55 gonzo Exp $ */
@@ -1439,7 +1439,7 @@ smsc_rxeof(struct usbd_xfer *xfer, void *priv, usbd_status status)
 		/* push the packet up */
 		s = splnet();
 		bpf_mtap(ifp, m);
-		if_percpuq_enqueue(ifp->if_percpuq, m);
+		if_percpuq_enqueue(sc->sc_ipq, m);
 		splx(s);
 	}
 
