da548da125ccf09ecb57caf2cce68c646a9533d1
mrg <mrg@NetBSD.org>
2016-10-04 18:33:00 +0000

    update the -V documentation to be more clear about what it does. inspired by Swift Griggs on netbsd-users.

--- libexec/httpd/bozohttpd.c
+++ libexec/httpd/bozohttpd.c
@@ -1,9 +1,9 @@
-/*	$NetBSD: bozohttpd.c,v 1.82 2016/06/30 13:17:48 mrg Exp $	*/
+/*	$NetBSD: bozohttpd.c,v 1.83 2016/10/04 18:33:00 mrg Exp $	*/
 
 /*	$eterna: bozohttpd.c,v 1.178 2011/11/18 09:21:15 mrg Exp $	*/
 
 /*
- * Copyright (c) 1997-2015 Matthew R. Green
+ * Copyright (c) 1997-2016 Matthew R. Green
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
--- libexec/httpd/main.c
+++ libexec/httpd/main.c
@@ -1,10 +1,10 @@
-/*	$NetBSD: main.c,v 1.15 2016/10/04 18:26:47 mrg Exp $	*/
+/*	$NetBSD: main.c,v 1.16 2016/10/04 18:33:00 mrg Exp $	*/
 
 /*	$eterna: main.c,v 1.6 2011/11/18 09:21:15 mrg Exp $	*/
 /* from: eterna: bozohttpd.c,v 1.159 2009/05/23 02:14:30 mrg Exp 	*/
 
 /*
- * Copyright (c) 1997-2014 Matthew R. Green
+ * Copyright (c) 1997-2016 Matthew R. Green
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
