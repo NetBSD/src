ae7ba107f44aa630f38c36036946461704fcae22
skrll <skrll@NetBSD.org>
2016-10-07 10:35:25 +0000

    Actually add sshub_p for previous

--- sys/dev/usb/usb_subr.c
+++ sys/dev/usb/usb_subr.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: usb_subr.c,v 1.198.2.32 2016/10/07 10:32:49 skrll Exp $	*/
+/*	$NetBSD: usb_subr.c,v 1.198.2.33 2016/10/07 10:35:25 skrll Exp $	*/
 /*	$FreeBSD: src/sys/dev/usb/usb_subr.c,v 1.18 1999/11/17 22:33:47 n_hibma Exp $	*/
 
 /*
@@ -32,7 +32,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: usb_subr.c,v 1.198.2.32 2016/10/07 10:32:49 skrll Exp $");
+__KERNEL_RCSID(0, "$NetBSD: usb_subr.c,v 1.198.2.33 2016/10/07 10:35:25 skrll Exp $");
 
 #ifdef _KERNEL_OPT
 #include "opt_compat_netbsd.h"
@@ -1554,6 +1554,7 @@ usbd_fill_deviceinfo(struct usbd_device *dev, struct usb_device_info *di,
 			err = p->up_dev->ud_addr;
 		else {
 			int s = UGETW(p->up_status.wPortStatus);
+			const bool sshub_p = USB_IS_SS(dev->ud_speed);
 			if (s & UPS_PORT_ENABLED)
 				err = USB_PORT_ENABLED;
 			else if (s & UPS_SUSPEND)
