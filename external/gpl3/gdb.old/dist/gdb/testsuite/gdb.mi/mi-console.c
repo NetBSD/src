f35e3f3d22adc1f3bec32f61c7421b793d9b99fd
christos <christos>
2016-10-12 15:36:05 +0000

    re-add all the removed files

---
+++ external/gpl3/gdb.old/dist/gdb/testsuite/gdb.mi/mi-console.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+
+void
+hello ()
+{
+  char *hello = "Hello \\\"!\r\n";
+  int i;
+  for (i = 0; hello[i]; i++)
+    write (1, hello + i, 1);
+}
+
+int
+main ()
+{
+  hello ();
+} /* after-hello */
