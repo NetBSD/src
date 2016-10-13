d25b89e3522817191c3a823f4939ce1372051335
flxd <flxd@NetBSD.org>
2016-10-13 16:18:20 +0000

    Match the two prom_getenv() and fix buffer overflow causing wrong host controller SCSI ID for DEC 3000. OK skrll@

--- sys/arch/alpha/stand/common/prom.c
+++ sys/arch/alpha/stand/common/prom.c
@@ -1,4 +1,4 @@
-/* $NetBSD: prom.c,v 1.14 2009/03/18 10:22:22 cegger Exp $ */
+/* $NetBSD: prom.c,v 1.15 2016/10/13 16:18:20 flxd Exp $ */
 
 /*  
  * Mach Operating System
@@ -57,7 +57,7 @@ init_prom_calls(void)
 	prom_dispatch_v.routine = c->crb_v_dispatch->entry_va;
 
 	/* Look for console tty. */
-	prom_getenv(PROM_E_TTY_DEV, buf, 4);
+	prom_getenv(PROM_E_TTY_DEV, buf, sizeof(buf));
 	console = buf[0] - '0';
 }
 
