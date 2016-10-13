81da5d5c29555ce776257981017abe1b0837f635
christos <christos>
2016-10-13 01:10:15 +0000

    One more level down

---
+++ external/gpl3/gdb.old/dist/sim/testsuite/sim/cris/c/badldso2.c
@@ -0,0 +1,8 @@
+/*
+#notarget: cris*-*-elf
+#dynamic:
+#xerror:
+#cc: additional_flags=-Wl,-dynamic-linker,/dev/null
+#output: *: could not load ELF interpreter `*' for program `*'\n
+ */
+#include "hello.c"
