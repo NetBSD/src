00053952e759c8e022a06da22e7fde53655f2a54
maxv <maxv@NetBSD.org>
2016-10-15 08:37:55 +0000

    There are several leaks in here, just fix one that should have been fixed in rev1.21

--- sys/dev/if_ndis/if_ndis_pci.c
+++ sys/dev/if_ndis/if_ndis_pci.c
@@ -31,7 +31,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: if_ndis_pci.c,v 1.21 2015/04/04 15:22:02 christos Exp $");
+__KERNEL_RCSID(0, "$NetBSD: if_ndis_pci.c,v 1.22 2016/10/15 08:37:55 maxv Exp $");
 #ifdef __FreeBSD__
 __FBSDID("$FreeBSD: src/sys/dev/if_ndis/if_ndis_pci.c,v 1.8.2.3 2005/03/31 04:24:36 wpaul Exp $");
 #endif
@@ -300,7 +300,7 @@ void ndis_attach_pci(device_t parent, device_t self, void *aux)
 					if((sc->ndis_res_altmem = 
 						malloc(sizeof(struct ndis_resource), M_DEVBUF, M_NOWAIT | M_ZERO)) == NULL) {
 						sc->error = ENOMEM;
-						return;
+						goto out;
 					}
 					sc->ndis_res_altmem->res_base = base;
 					sc->ndis_res_altmem->res_size = size;
