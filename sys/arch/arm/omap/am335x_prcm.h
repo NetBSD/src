65bbd141318cfcfa644f76908739863a97cdd397
kiyohara <kiyohara@NetBSD.org>
2016-10-18 15:10:35 +0000

    Move set_mpu_volt() from */*_machdep.c to am335x_prcm.c.

--- sys/arch/arm/omap/am335x_prcm.h
+++ sys/arch/arm/omap/am335x_prcm.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: am335x_prcm.h,v 1.9 2015/06/06 14:00:32 jmcneill Exp $	*/
+/*	$NetBSD: am335x_prcm.h,v 1.10 2016/10/18 15:10:35 kiyohara Exp $	*/
 
 /*
  * TI OMAP Power, Reset, and Clock Management on the AM335x
@@ -150,7 +150,6 @@ struct omap_module {
 #define RST_GLOBAL_COLD_SW	__BIT(1)
 
 #ifdef _KERNEL
-int  set_mpu_volt(int);
 void am335x_sys_clk(bus_space_handle_t);
 void am335x_cpu_clk(void);
 #endif
