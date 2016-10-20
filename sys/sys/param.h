acebeafe0dc7a1437e61d91f6d6179de3b3bb2b6
mrg <mrg@NetBSD.org>
2016-10-20 08:18:16 +0000

    bump kernel version for ptrace32 related changes.

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.505 2016/09/21 10:52:13 roy Exp $	*/
+/*	$NetBSD: param.h,v 1.506 2016/10/20 08:18:16 mrg Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -67,7 +67,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	799003900	/* NetBSD 7.99.39 */
+#define	__NetBSD_Version__	799004000	/* NetBSD 7.99.40 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
