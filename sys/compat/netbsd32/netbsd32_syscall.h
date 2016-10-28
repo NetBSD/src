580fd3e9638138358322fde8ac88ce6f253e6931
christos <christos@NetBSD.org>
2016-10-28 23:44:54 +0000

    regen

--- sys/compat/netbsd32/netbsd32_syscall.h
+++ sys/compat/netbsd32/netbsd32_syscall.h
@@ -1,4 +1,4 @@
-/* $NetBSD: netbsd32_syscall.h,v 1.125 2016/10/19 09:45:25 skrll Exp $ */
+/* $NetBSD: netbsd32_syscall.h,v 1.126 2016/10/28 23:44:54 christos Exp $ */
 
 /*
  * System call numbers.
