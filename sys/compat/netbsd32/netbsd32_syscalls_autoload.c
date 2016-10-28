7255ba6d9ce1702d8e30baa6abe51d1c050d113f
christos <christos@NetBSD.org>
2016-10-28 23:44:54 +0000

    regen

--- sys/compat/netbsd32/netbsd32_syscalls_autoload.c
+++ sys/compat/netbsd32/netbsd32_syscalls_autoload.c
@@ -1,4 +1,4 @@
-/* $NetBSD: netbsd32_syscalls_autoload.c,v 1.7 2016/10/19 09:45:25 skrll Exp $ */
+/* $NetBSD: netbsd32_syscalls_autoload.c,v 1.8 2016/10/28 23:44:54 christos Exp $ */
 
 /*
  * System call autoload table.
@@ -8,7 +8,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: netbsd32_syscalls_autoload.c,v 1.7 2016/10/19 09:45:25 skrll Exp $");
+__KERNEL_RCSID(0, "$NetBSD: netbsd32_syscalls_autoload.c,v 1.8 2016/10/28 23:44:54 christos Exp $");
 
 #include <sys/proc.h>
 static struct sc_autoload netbsd32_syscalls_autoload[] = {
