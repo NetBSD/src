31b12659e5076e5ff9d61ae5b55788e7896e34c8
pgoyette <pgoyette@NetBSD.org>
2016-11-02 03:06:19 +0000

    Belatedly bump the kernel version for recent modularization changes for ptrace(2).

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.508 2016/10/26 01:16:06 pgoyette Exp $	*/
+/*	$NetBSD: param.h,v 1.509 2016/11/02 03:06:19 pgoyette Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -67,7 +67,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	799004100	/* NetBSD 7.99.41 */
+#define	__NetBSD_Version__	799004200	/* NetBSD 7.99.42 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
