175d720a94868ee67e6ea9d8b572bc065f783335
hannken <hannken@NetBSD.org>
2016-11-03 11:03:31 +0000

    Split sys/vnode.h into sys/vnode.h and sys/vnode_impl.h - Move _VFS_VNODE_PRIVATE protected operations into vnode_impl.h. - Move struct vnode_impl definition and operations into vnode_impl.h. - Include vnode_impl.h where we include vnode.h with _VFS_VNODE_PRIVATE defined. - Get rid of _VFS_VNODE_PRIVATE.

---
+++ sys/sys/vnode_impl.h
@@ -0,0 +1,71 @@
+/*	$NetBSD: vnode_impl.h,v 1.1 2016/11/03 11:03:31 hannken Exp $	*/
+
+/*-
+ * Copyright (c) 2016 The NetBSD Foundation, Inc.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to The NetBSD Foundation
+ * by 
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef _SYS_VNODE_IMPL_H_
+#define	_SYS_VNODE_IMPL_H_
+
+#include <sys/vnode.h>
+
+enum vnode_state {
+	VS_MARKER,	/* Stable, used as marker. Will not change. */
+	VS_LOADING,	/* Intermediate, initialising the fs node. */
+	VS_ACTIVE,	/* Stable, valid fs node attached. */
+	VS_BLOCKED,	/* Intermediate, active, no new references allowed. */
+	VS_RECLAIMING,	/* Intermediate, detaching the fs node. */
+	VS_RECLAIMED	/* Stable, no fs node attached. */
+};
+struct vcache_key {
+	struct mount *vk_mount;
+	const void *vk_key;
+	size_t vk_key_len;
+};
+struct vnode_impl {
+	struct vnode vi_vnode;
+	enum vnode_state vi_state;
+	SLIST_ENTRY(vnode_impl) vi_hash;
+	struct vcache_key vi_key;
+};
+typedef struct vnode_impl vnode_impl_t;
+
+#define VIMPL_TO_VNODE(node)	((vnode_t *)(node))
+#define VNODE_TO_VIMPL(vp)	((vnode_impl_t *)(vp))
+
+/*
+ * Vnode manipulation functions.
+ */
+vnode_t *
+	vnalloc_marker(struct mount *);
+void	vnfree_marker(vnode_t *);
+bool	vnis_marker(vnode_t *);
+void	vcache_print(vnode_t *, const char *,
+    void (*)(const char *, ...) __printflike(1, 2));
+
+#endif /* !_SYS_VNODE_IMPL_H_ */
