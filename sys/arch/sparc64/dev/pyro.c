103fa87018028c53a4ac8b9d57030746d7fe9b4e
macallan <macallan@NetBSD.org>
2016-11-10 06:44:35 +0000

    weed out BUS_SPACE_MAP_PREFETCHABLE in resp. _map() and _mmap() methods it works with UPA graphics cards and at least some psycho but causes problems on these

--- sys/arch/sparc64/dev/pyro.c
+++ sys/arch/sparc64/dev/pyro.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: pyro.c,v 1.17 2016/05/10 19:23:59 palle Exp $	*/
+/*	$NetBSD: pyro.c,v 1.18 2016/11/10 06:44:35 macallan Exp $	*/
 /*	from: $OpenBSD: pyro.c,v 1.20 2010/12/05 15:15:14 kettenis Exp $	*/
 
 /*
@@ -31,7 +31,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: pyro.c,v 1.17 2016/05/10 19:23:59 palle Exp $");
+__KERNEL_RCSID(0, "$NetBSD: pyro.c,v 1.18 2016/11/10 06:44:35 macallan Exp $");
 
 #include <sys/param.h>
 #include <sys/device.h>
@@ -495,6 +495,12 @@ pyro_bus_map(bus_space_tag_t t, bus_addr_t offset,
 	    (unsigned long long)size,
 	    flags));
 
+	/*
+	 * BUS_SPACE_MAP_PREFETCHABLE causes hard hangs on schizo, so weed it
+	 * out for now until someone can verify wether it works on pyro
+	 */
+	flags &= ~BUS_SPACE_MAP_PREFETCHABLE;
+
 	ss = sparc_pci_childspace(t->type);
 	DPRINTF(PDB_BUSMAP, (" cspace %d", ss));
 
@@ -526,6 +532,12 @@ pyro_bus_mmap(bus_space_tag_t t, bus_addr_t paddr,
 	struct pyro_pbm *pbm = t->cookie;
 	struct pyro_softc *sc = pbm->pp_sc;
 	int i, ss;
+
+	/*
+	 * BUS_SPACE_MAP_PREFETCHABLE causes hard hangs on schizo, so weed it
+	 * out for now until someone can verify wether it works on pyro
+	 */
+	flags &= ~BUS_SPACE_MAP_PREFETCHABLE;
 
 	ss = sparc_pci_childspace(t->type);
 
