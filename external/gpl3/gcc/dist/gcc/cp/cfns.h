735e01e94caefb1e6ce45df390aad0d3e4142d27
snj <snj>
2016-11-13 06:49:49 +0000

    Pull up following revision(s) (requested by pgoyette in ticket #1417): 	external/gpl3/gcc/dist/gcc/cp/cfns.gper: patch 	external/gpl3/gcc/dist/gcc/cp/cfns.h: patch Fix build with gcc5.

--- external/gpl3/gcc/dist/gcc/cp/cfns.h
+++ external/gpl3/gcc/dist/gcc/cp/cfns.h
@@ -53,6 +53,9 @@ __inline
 static unsigned int hash (const char *, unsigned int);
 #ifdef __GNUC__
 __inline
+#ifdef __GNUC_STDC_INLINE__
+__attribute__ ((__gnu_inline__))
+#endif
 #endif
 const char * libc_name_p (const char *, unsigned int);
 /* maximum key range = 391, duplicates = 0 */
