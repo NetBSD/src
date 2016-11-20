50fda29758f85968362c04f3062913ad23218ab0
riastradh <riastradh@NetBSD.org>
2016-11-20 21:21:26 +0000

    KASSERT(mutex_owner(...)) ---> KASSERT(mutex_owned(...))
    Fixes part of PR kern/47114.  Tested by code inspection.

--- sys/ufs/ufs/ufs_quota2.c
+++ sys/ufs/ufs/ufs_quota2.c
@@ -1,4 +1,4 @@
-/* $NetBSD: ufs_quota2.c,v 1.40 2015/03/28 19:24:05 maxv Exp $ */
+/* $NetBSD: ufs_quota2.c,v 1.41 2016/11/20 21:21:26 riastradh Exp $ */
 /*-
   * Copyright (c) 2010 Manuel Bouyer
   * All rights reserved.
@@ -26,7 +26,7 @@
   */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: ufs_quota2.c,v 1.40 2015/03/28 19:24:05 maxv Exp $");
+__KERNEL_RCSID(0, "$NetBSD: ufs_quota2.c,v 1.41 2016/11/20 21:21:26 riastradh Exp $");
 
 #include <sys/buf.h>
 #include <sys/param.h>
@@ -200,7 +200,7 @@ quota2_walk_list(struct ufsmount *ump, struct buf *hbp, int type,
 	struct quota2_entry *q2e;
 	daddr_t lblkno, blkoff, olblkno = 0;
 
-	KASSERT(mutex_owner(&dqlock));
+	KASSERT(mutex_owned(&dqlock));
 
 	while (off != 0) {
 		lblkno = (off >> ump->um_mountp->mnt_fs_bshift);
