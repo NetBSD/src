0e72af928fa05f658f7246bb96d1e5b8554f7998
skrll <skrll@NetBSD.org>
2016-11-25 12:56:29 +0000

    +#include "opt_usb.h"

--- sys/dev/usb/uftdi.c
+++ sys/dev/usb/uftdi.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: uftdi.c,v 1.62 2016/07/07 06:55:42 msaitoh Exp $	*/
+/*	$NetBSD: uftdi.c,v 1.63 2016/11/25 12:56:29 skrll Exp $	*/
 
 /*
  * Copyright (c) 2000 The NetBSD Foundation, Inc.
@@ -30,7 +30,11 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: uftdi.c,v 1.62 2016/07/07 06:55:42 msaitoh Exp $");
+__KERNEL_RCSID(0, "$NetBSD: uftdi.c,v 1.63 2016/11/25 12:56:29 skrll Exp $");
+
+#ifdef _KERNEL_OPT
+#include "opt_usb.h"
+#endif
 
 #include <sys/param.h>
 #include <sys/systm.h>
