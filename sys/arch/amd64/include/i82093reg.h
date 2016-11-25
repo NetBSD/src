3e6f2a9524435a6460b27c58511af8a4e5667f9c
maxv <maxv@NetBSD.org>
2016-11-25 14:12:55 +0000

    Move the virtual address of the LAPIC page out of the data segment on amd64 and i386. The old design was error-prone, and it didn't allow us to map the data segment with large pages.
    Now, the VA is allocated dynamically in the pmap bootstrap code, and entered
    manually later. We go from using &local_apic to using *local_apic_va, and we
    therefore need one more level of indirection in the asm code.
    
    Discussed on tech-kern.

--- sys/arch/amd64/include/i82093reg.h
+++ sys/arch/amd64/include/i82093reg.h
@@ -1,4 +1,4 @@
-/*	 $NetBSD: i82093reg.h,v 1.6 2016/08/11 15:45:39 maxv Exp $ */
+/*	 $NetBSD: i82093reg.h,v 1.7 2016/11/25 14:12:55 maxv Exp $ */
 
 #include <x86/i82093reg.h>
 
@@ -9,7 +9,8 @@
 #endif
 
 #define ioapic_asm_ack(num) \
-	movl	$0,_C_LABEL(local_apic)+LAPIC_EOI
+	movq	_C_LABEL(local_apic_va),%rax	; \
+	movl	$0,LAPIC_EOI(%rax)
 
 #ifdef MULTIPROCESSOR
 
