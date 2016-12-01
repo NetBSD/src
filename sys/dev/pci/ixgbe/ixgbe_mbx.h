1f093dd9c9d47fc2737c9ee99c7c5a56c0727a80
msaitoh <msaitoh@NetBSD.org>
2016-12-01 06:56:28 +0000

    Update ixg(4) and ixv(4) up to FreeBSD r282299: - Add support for X55x. - ADD EEE support (not tested). - Add WOL support (not tested). - Add suspend/resume support (not testd). - Add LPLU support (not tested). - Add DMA Coalescing (note verified). - Not tested well for sysctls. - Fix ixgbe_set_advertise() a bit. At least, FreeBSD r294578 is required   to work hw.ixg0.advertise_speed sysctl correctly.

--- sys/dev/pci/ixgbe/ixgbe_mbx.h
+++ sys/dev/pci/ixgbe/ixgbe_mbx.h
@@ -1,6 +1,6 @@
 /******************************************************************************
 
-  Copyright (c) 2001-2014, Intel Corporation 
+  Copyright (c) 2001-2015, Intel Corporation 
   All rights reserved.
   
   Redistribution and use in source and binary forms, with or without 
@@ -30,8 +30,8 @@
   POSSIBILITY OF SUCH DAMAGE.
 
 ******************************************************************************/
-/*$FreeBSD: head/sys/dev/ixgbe/ixgbe_mbx.h 280182 2015-03-17 18:32:28Z jfv $*/
-/*$NetBSD: ixgbe_mbx.h,v 1.5 2016/12/01 06:27:18 msaitoh Exp $*/
+/*$FreeBSD: head/sys/dev/ixgbe/ixgbe_mbx.h 282289 2015-04-30 22:53:27Z erj $*/
+/*$NetBSD: ixgbe_mbx.h,v 1.6 2016/12/01 06:56:28 msaitoh Exp $*/
 
 #ifndef _IXGBE_MBX_H_
 #define _IXGBE_MBX_H_
