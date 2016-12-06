508f9fb7716416a3e0b2aa9b02fe0c983c0399b0
ozaki-r <ozaki-r@NetBSD.org>
2016-12-06 07:09:38 +0000

    Use if_percpuq_enqueue, not if_input
    Because gmac_hwqueue_rxconsume is called in hardware interrupt.

--- sys/arch/arm/gemini/gemini_gmac.c
+++ sys/arch/arm/gemini/gemini_gmac.c
@@ -1,4 +1,4 @@
-/* $NetBSD: gemini_gmac.c,v 1.9 2016/06/10 13:27:11 ozaki-r Exp $ */
+/* $NetBSD: gemini_gmac.c,v 1.10 2016/12/06 07:09:38 ozaki-r Exp $ */
 /*-
  * Copyright (c) 2008 The NetBSD Foundation, Inc.
  * All rights reserved.
@@ -49,7 +49,7 @@
 
 #include <sys/gpio.h>
 
-__KERNEL_RCSID(0, "$NetBSD: gemini_gmac.c,v 1.9 2016/06/10 13:27:11 ozaki-r Exp $");
+__KERNEL_RCSID(0, "$NetBSD: gemini_gmac.c,v 1.10 2016/12/06 07:09:38 ozaki-r Exp $");
 
 #define	SWFREEQ_DESCS	256	/* one page worth */
 #define	HWFREEQ_DESCS	256	/* one page worth */
@@ -856,7 +856,7 @@ gmac_hwqueue_rxconsume(gmac_hwqueue_t *hwq, const gmac_desc_t *d)
 		m->m_data += 2;
 		KASSERT(m_length(m) == m->m_pkthdr.len);
 		bpf_mtap(ifp, m);
-		if_input(ifp, m);
+		if_percpuq_enqueue(ifp->if_percpuq, m);
 		break;
 	default:
 		ifp->if_ierrors++;
