47fe690432d7f583496a8032937ec6ed79de25fd
christos <christos@NetBSD.org>
2016-12-09 05:45:20 +0000

    include <sys/module.h for now so that things compile.

--- sys/dev/spkrvar.h
+++ sys/dev/spkrvar.h
@@ -1,7 +1,9 @@
-/* $NetBSD: spkrvar.h,v 1.2 2016/12/09 04:46:39 christos Exp $ */
+/* $NetBSD: spkrvar.h,v 1.3 2016/12/09 05:45:20 christos Exp $ */
 
 #ifndef _SYS_DEV_SPKRVAR_H
 #define _SYS_DEV_SPKRVAR_H
+
+#include <sys/module.h>
 
 device_t speakerattach_mi(device_t);
 void speaker_play(u_int, u_int, u_int);
