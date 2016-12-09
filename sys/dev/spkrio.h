5c277ab4e558cf6bb6d0ab414e88b769fd2b982e
christos <christos@NetBSD.org>
2016-12-09 04:46:39 +0000

    move spkrio.h out of isa

--- sys/dev/spkrio.h
+++ sys/dev/spkrio.h
@@ -1,11 +1,11 @@
-/*	$NetBSD: spkrio.h,v 1.1 2016/12/09 04:32:39 christos Exp $	*/
+/*	$NetBSD: spkrio.h,v 1.2 2016/12/09 04:46:39 christos Exp $	*/
 
 /*
- * spkr.h -- interface definitions for speaker ioctl()
+ * spkrio.h -- interface definitions for speaker ioctl()
  */
 
-#ifndef _DEV_ISA_SPKR_H_
-#define _DEV_ISA_SPKR_H_
+#ifndef _DEV_SPKRIO_H_
+#define _DEV_SPKRIO_H_
 
 #include <sys/ioccom.h>
 
@@ -16,11 +16,5 @@ typedef struct {
 	int	frequency;	/* in hertz */
 	int	duration;	/* in 1/100ths of a second */
 } tone_t;
-
-void spkr_tone(u_int, u_int);
-void spkr_rest(int);
-int spkr__modcmd(modcmd_t, void *);
-int spkr_probe(device_t, cfdata_t, void *);
-extern int spkr_attached;
 
 #endif
--- sys/dev/spkrvar.h
+++ sys/dev/spkrvar.h
@@ -1,9 +1,16 @@
-/* $NetBSD: spkrvar.h,v 1.1 2016/12/08 11:31:08 nat Exp $ */
+/* $NetBSD: spkrvar.h,v 1.2 2016/12/09 04:46:39 christos Exp $ */
 
 #ifndef _SYS_DEV_SPKRVAR_H
 #define _SYS_DEV_SPKRVAR_H
 
 device_t speakerattach_mi(device_t);
 void speaker_play(u_int, u_int, u_int);
+
+// XXX:
+void spkr_tone(u_int, u_int);
+void spkr_rest(int);
+int spkr__modcmd(modcmd_t, void *);
+int spkr_probe(device_t, cfdata_t, void *);
+extern int spkr_attached;
 
 #endif /* _SYS_DEV_SPKRVAR_H */
