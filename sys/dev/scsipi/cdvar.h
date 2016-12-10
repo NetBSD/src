7403842f20c51b0a07e2d127f98595b32cada95c
mlelstv <mlelstv@NetBSD.org>
2016-12-10 10:26:38 +0000

    Refactored sd and cd to use common disk subroutines.

--- sys/dev/scsipi/cdvar.h
+++ sys/dev/scsipi/cdvar.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: cdvar.h,v 1.32 2015/04/14 20:32:36 riastradh Exp $	*/
+/*	$NetBSD: cdvar.h,v 1.33 2016/12/10 10:26:38 mlelstv Exp $	*/
 
 /*
  * Copyright (c) 1997 Manuel Bouyer.  All rights reserved.
@@ -24,19 +24,16 @@
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include <sys/rndsource.h>
+#include <dev/dkvar.h>
 
 #define	CDRETRIES	4
 
 struct cd_softc {
-	device_t sc_dev;
-	struct disk sc_dk;
-	kmutex_t sc_lock;
+	struct dk_softc sc_dksc;
 
 	int flags;
-#define	CDF_WLABEL	0x04		/* label is writable */
-#define	CDF_LABELLING	0x08		/* writing label */
 #define	CDF_ANCIENT	0x10		/* disk is ancient; for minphys */
+#define	CDF_EJECTED	0x20		/* be silent when flushing cache */
 
 	struct scsipi_periph *sc_periph;
 
@@ -46,8 +43,9 @@ struct cd_softc {
 		u_long disksize512;	/* total number sectors */
 	} params;
 
-	struct bufq_state *buf_queue;
 	struct callout sc_callout;
-
-	krndsource_t	rnd_source;
 };
+
+#define CDGP_RESULT_OK          0       /* parameters obtained */
+#define CDGP_RESULT_OFFLINE     1       /* no media, or otherwise losing */
+#define CDGP_RESULT_UNFORMATTED 2       /* unformatted media (max params) */
