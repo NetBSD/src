e0c9eca3808214c47377f90b103070cd183ad507
christos <christos@NetBSD.org>
2016-12-10 21:04:12 +0000

    use array notation

--- lib/libnpf/npf.h
+++ lib/libnpf/npf.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: npf.h,v 1.29 2016/12/10 05:37:55 christos Exp $	*/
+/*	$NetBSD: npf.h,v 1.30 2016/12/10 21:04:12 christos Exp $	*/
 
 /*-
  * Copyright (c) 2011-2014 The NetBSD Foundation, Inc.
@@ -111,7 +111,7 @@ int		npf_rproc_insert(nl_config_t *, nl_rproc_t *);
 nl_nat_t *	npf_nat_create(int, u_int, const char *,
 		    int, npf_addr_t *, npf_netmask_t, in_port_t);
 int		npf_nat_insert(nl_config_t *, nl_nat_t *, pri_t);
-int		npf_nat_lookup(int, int, npf_addr_t **, in_port_t *, int, int);
+int		npf_nat_lookup(int, int, npf_addr_t *[2], in_port_t [2], int, int);
 
 nl_table_t *	npf_table_create(const char *, u_int, int);
 int		npf_table_add_entry(nl_table_t *, int,
