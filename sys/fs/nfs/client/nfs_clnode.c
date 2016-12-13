17bc9b9efcee24feb9bbfc6186c385bf013b3a5a
pgoyette <pgoyette@NetBSD.org>
2016-12-13 22:17:33 +0000

    Adapt code for our location of the nfs headers, and some updates for differences in kernel options(4) handling

--- sys/fs/nfs/client/nfs_clnode.c
+++ sys/fs/nfs/client/nfs_clnode.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: nfs_clnode.c,v 1.2 2016/11/18 22:58:08 pgoyette Exp $	*/
+/*	$NetBSD: nfs_clnode.c,v 1.3 2016/12/13 22:17:33 pgoyette Exp $	*/
 /*-
  * Copyright (c) 1989, 1993
  *	The Regents of the University of California.  All rights reserved.
@@ -35,7 +35,7 @@
 
 #include <sys/cdefs.h>
 /* __FBSDID("FreeBSD: head/sys/fs/nfsclient/nfs_clnode.c 302210 2016-06-26 14:18:28Z kib "); */
-__RCSID("$NetBSD: nfs_clnode.c,v 1.2 2016/11/18 22:58:08 pgoyette Exp $");
+__RCSID("$NetBSD: nfs_clnode.c,v 1.3 2016/12/13 22:17:33 pgoyette Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -52,13 +52,13 @@ __RCSID("$NetBSD: nfs_clnode.c,v 1.2 2016/11/18 22:58:08 pgoyette Exp $");
 
 #include <vm/uma.h>
 
-#include <fs/nfs/nfsport.h>
-#include <fs/nfsclient/nfsnode.h>
-#include <fs/nfsclient/nfsmount.h>
-#include <fs/nfsclient/nfs.h>
-#include <fs/nfsclient/nfs_kdtrace.h>
+#include <fs/nfs/common/nfsport.h>
+#include <fs/nfs/client/nfsnode.h>
+#include <fs/nfs/client/nfsmount.h>
+#include <fs/nfs/client/nfs.h>
+#include <fs/nfs/client/nfs_kdtrace.h>
 
-#include <nfs/nfs_lock.h>
+#include <fs/nfs/common/nfs_lock.h>
 
 extern struct vop_vector newnfs_vnodeops;
 extern struct buf_ops buf_ops_newnfs;
