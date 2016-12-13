b8854a5a711a02e1531912cbdce395266e1d48a4
kamil <kamil@NetBSD.org>
2016-12-13 12:25:05 +0000

    Remove dbregs[12] from t_ptrace_wait{,3,4,6,id,pid}
    CPU Debug Registers won't be exposed as is to userland.
    
    Hardware Watchpoints will be exported to userland dedicated interface
    through the ptrace(2) interface.
    
    Sponsored by <The NetBSD Foundation>

--- tests/kernel/t_ptrace_wait.c
+++ tests/kernel/t_ptrace_wait.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: t_ptrace_wait.c,v 1.46 2016/12/08 13:32:17 kamil Exp $	*/
+/*	$NetBSD: t_ptrace_wait.c,v 1.47 2016/12/13 12:25:05 kamil Exp $	*/
 
 /*-
  * Copyright (c) 2016 The NetBSD Foundation, Inc.
@@ -27,7 +27,7 @@
  */
 
 #include <sys/cdefs.h>
-__RCSID("$NetBSD: t_ptrace_wait.c,v 1.46 2016/12/08 13:32:17 kamil Exp $");
+__RCSID("$NetBSD: t_ptrace_wait.c,v 1.47 2016/12/13 12:25:05 kamil Exp $");
 
 #include <sys/param.h>
 #include <sys/types.h>
@@ -3907,118 +3907,6 @@ ATF_TC_BODY(fpregs2, tc)
 }
 #endif
 
-#if defined(HAVE_DBREGS)
-ATF_TC(dbregs1);
-ATF_TC_HEAD(dbregs1, tc)
-{
-	atf_tc_set_md_var(tc, "descr",
-	    "Verify plain PT_GETDBREGS call without further steps");
-}
-
-ATF_TC_BODY(dbregs1, tc)
-{
-	const int exitval = 5;
-	const int sigval = SIGSTOP;
-	pid_t child, wpid;
-#if defined(TWAIT_HAVE_STATUS)
-	int status;
-#endif
-	struct dbreg r;
-
-	printf("Before forking process PID=%d\n", getpid());
-	ATF_REQUIRE((child = fork()) != -1);
-	if (child == 0) {
-		printf("Before calling PT_TRACE_ME from child %d\n", getpid());
-		FORKEE_ASSERT(ptrace(PT_TRACE_ME, 0, NULL, 0) != -1);
-
-		printf("Before raising %s from child\n", strsignal(sigval));
-		FORKEE_ASSERT(raise(sigval) == 0);
-
-		printf("Before exiting of the child process\n");
-		_exit(exitval);
-	}
-	printf("Parent process PID=%d, child's PID=%d\n", getpid(), child);
-
-	printf("Before calling %s() for the child\n", TWAIT_FNAME);
-	TWAIT_REQUIRE_SUCCESS(wpid = TWAIT_GENERIC(child, &status, 0), child);
-
-	validate_status_stopped(status, sigval);
-
-	printf("Call GETDBREGS for the child process\n");
-	ATF_REQUIRE(ptrace(PT_GETDBREGS, child, &r, 0) != -1);
-
-	printf("Before resuming the child process where it left off and "
-	    "without signal to be sent\n");
-	ATF_REQUIRE(ptrace(PT_CONTINUE, child, (void *)1, 0) != -1);
-
-	printf("Before calling %s() for the child\n", TWAIT_FNAME);
-	TWAIT_REQUIRE_SUCCESS(wpid = TWAIT_GENERIC(child, &status, 0), child);
-
-	validate_status_exited(status, exitval);
-
-	printf("Before calling %s() for the child\n", TWAIT_FNAME);
-	TWAIT_REQUIRE_FAILURE(ECHILD, wpid = TWAIT_GENERIC(child, &status, 0));
-}
-#endif
-
-#if defined(HAVE_DBREGS)
-ATF_TC(dbregs2);
-ATF_TC_HEAD(dbregs2, tc)
-{
-	atf_tc_set_md_var(tc, "descr",
-	    "Verify PT_GETDBREGS and PT_SETDBREGS calls without changing "
-	    "regs");
-}
-
-ATF_TC_BODY(dbregs2, tc)
-{
-	const int exitval = 5;
-	const int sigval = SIGSTOP;
-	pid_t child, wpid;
-#if defined(TWAIT_HAVE_STATUS)
-	int status;
-#endif
-	struct dbreg r;
-
-	printf("Before forking process PID=%d\n", getpid());
-	ATF_REQUIRE((child = fork()) != -1);
-	if (child == 0) {
-		printf("Before calling PT_TRACE_ME from child %d\n", getpid());
-		FORKEE_ASSERT(ptrace(PT_TRACE_ME, 0, NULL, 0) != -1);
-
-		printf("Before raising %s from child\n", strsignal(sigval));
-		FORKEE_ASSERT(raise(sigval) == 0);
-
-		printf("Before exiting of the child process\n");
-		_exit(exitval);
-	}
-	printf("Parent process PID=%d, child's PID=%d\n", getpid(), child);
-
-	printf("Before calling %s() for the child\n", TWAIT_FNAME);
-	TWAIT_REQUIRE_SUCCESS(wpid = TWAIT_GENERIC(child, &status, 0), child);
-
-	validate_status_stopped(status, sigval);
-
-	printf("Call GETDBREGS for the child process\n");
-	ATF_REQUIRE(ptrace(PT_GETDBREGS, child, &r, 0) != -1);
-
-	printf("Call SETDBREGS for the child (without changed regs)\n");
-	ATF_REQUIRE(ptrace(PT_SETDBREGS, child, &r, 0) != -1);
-
-	printf("Before resuming the child process where it left off and "
-	    "without signal to be sent\n");
-	ATF_REQUIRE(ptrace(PT_CONTINUE, child, (void *)1, 0) != -1);
-
-	printf("Before calling %s() for the child\n", TWAIT_FNAME);
-	TWAIT_REQUIRE_SUCCESS(wpid = TWAIT_GENERIC(child, &status, 0), child);
-
-	validate_status_exited(status, exitval);
-
-	printf("Before calling %s() for the child\n", TWAIT_FNAME);
-	TWAIT_REQUIRE_FAILURE(ECHILD, wpid = TWAIT_GENERIC(child, &status, 0));
-}
-#endif
-
 #if defined(PT_STEP)
 ATF_TC(step1);
 ATF_TC_HEAD(step1, tc)
@@ -4640,9 +4528,6 @@ ATF_TP_ADD_TCS(tp)
 
 	ATF_TP_ADD_TC_HAVE_FPREGS(tp, fpregs1);
 	ATF_TP_ADD_TC_HAVE_FPREGS(tp, fpregs2);
-
-	ATF_TP_ADD_TC_HAVE_DBREGS(tp, dbregs1);
-	ATF_TP_ADD_TC_HAVE_DBREGS(tp, dbregs2);
 
 	ATF_TP_ADD_TC_PT_STEP(tp, step1);
 	ATF_TP_ADD_TC_PT_STEP(tp, step2);
