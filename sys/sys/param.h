ecadf6ddd06d7abc44ba47c2c2a9c57560a9ad3d
kamil <kamil@NetBSD.org>
2016-12-15 12:20:39 +0000

    Welcome to NetBSD 7.99.50!
    New ptrace(2) calls for hardware assisted watchpoints/breakpoints:
     - PT_COUNT_WATCHPOINTS,
     - PT_READ_WATCHPOINT,
     - PT_WRITE_WATCHPOINT.
    
    Sponsored by <The NetBSD Foundation>

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.516 2016/12/15 09:39:24 ozaki-r Exp $	*/
+/*	$NetBSD: param.h,v 1.517 2016/12/15 12:20:39 kamil Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -67,7 +67,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	799004900	/* NetBSD 7.99.49 */
+#define	__NetBSD_Version__	799005000	/* NetBSD 7.99.50 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
