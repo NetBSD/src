92042881a1d92bc85081cd7dd519098ebd00094a
kamil <kamil@NetBSD.org>
2016-12-15 12:04:17 +0000

    Add support for hardware assisted watchpoints/breakpoints API in ptrace(2)
    Add new ptrace(2) calls:
     - PT_COUNT_WATCHPOINTS - count the number of available hardware watchpoints
     - PT_READ_WATCHPOINT   - read struct ptrace_watchpoint from the kernel state
     - PT_WRITE_WATCHPOINT  - write new struct ptrace_watchpoint state, this
                              includes enabling and disabling watchpoints
    
    The ptrace_watchpoint structure contains MI and MD parts:
    
    typedef struct ptrace_watchpoint {
    	int		pw_index;	/* HW Watchpoint ID (count from 0) */
    	lwpid_t		pw_lwpid;	/* LWP described */
    	struct mdpw	pw_md;		/* MD fields */
    } ptrace_watchpoint_t;
    
    For example amd64 defines MD as follows:
    struct mdpw {
    	void	*md_address;
    	int	 md_condition;
    	int	 md_length;
    };
    
    These calls are protected with the __HAVE_PTRACE_WATCHPOINTS guard.
    
    Tested on amd64, initial support added for i386 and XEN.
    
    Sponsored by <The NetBSD Foundation>

--- sys/arch/amd64/amd64/netbsd32_machdep.c
+++ sys/arch/amd64/amd64/netbsd32_machdep.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: netbsd32_machdep.c,v 1.97 2016/10/19 09:44:00 skrll Exp $	*/
+/*	$NetBSD: netbsd32_machdep.c,v 1.98 2016/12/15 12:04:17 kamil Exp $	*/
 
 /*
  * Copyright (c) 2001 Wasabi Systems, Inc.
@@ -36,7 +36,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: netbsd32_machdep.c,v 1.97 2016/10/19 09:44:00 skrll Exp $");
+__KERNEL_RCSID(0, "$NetBSD: netbsd32_machdep.c,v 1.98 2016/12/15 12:04:17 kamil Exp $");
 
 #ifdef _KERNEL_OPT
 #include "opt_compat_netbsd.h"
@@ -128,6 +128,8 @@ netbsd32_setregs(struct lwp *l, struct exec_package *pack, vaddr_t stack)
 	    ?  __NetBSD_NPXCW__ : __NetBSD_COMPAT_NPXCW__);
 
 	p->p_flag |= PK_32;
+
+	memset(l->l_md.md_watchpoint, 0, sizeof(*l->l_md.md_watchpoint));
 
 	tf = l->l_md.md_regs;
 	tf->tf_ds = LSEL(LUDATA32_SEL, SEL_UPL);
