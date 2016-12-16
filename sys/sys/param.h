aeec4acded527fc65f8c553742103ae57204dd58
riastradh <riastradh@NetBSD.org>
2016-12-16 23:37:21 +0000

    Welcome to 7.99.51, courtesy replacement ABI for nommap.

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.517 2016/12/15 12:20:39 kamil Exp $	*/
+/*	$NetBSD: param.h,v 1.518 2016/12/16 23:37:21 riastradh Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -67,7 +67,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	799005000	/* NetBSD 7.99.50 */
+#define	__NetBSD_Version__	799005100	/* NetBSD 7.99.51 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
