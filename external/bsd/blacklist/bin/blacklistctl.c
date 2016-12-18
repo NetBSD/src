14ac132d3df1b6edb787dbf30d474e855312abdb
snj <snj@NetBSD.org>
2016-12-18 07:58:52 +0000

    Pull up following revision(s) (requested by jnemeth in ticket #1330): 	external/bsd/blacklist/bin/blacklistctl.c: revision 1.21 Correct misplaced break; from FreeBSD. Approved By: christos

--- external/bsd/blacklist/bin/blacklistctl.c
+++ external/bsd/blacklist/bin/blacklistctl.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: blacklistctl.c,v 1.17.2.3 2015/08/07 04:10:23 snj Exp $	*/
+/*	$NetBSD: blacklistctl.c,v 1.17.2.4 2016/12/18 07:58:52 snj Exp $	*/
 
 /*-
  * Copyright (c) 2015 The NetBSD Foundation, Inc.
@@ -33,7 +33,7 @@
 #endif
 
 #include <sys/cdefs.h>
-__RCSID("$NetBSD: blacklistctl.c,v 1.17.2.3 2015/08/07 04:10:23 snj Exp $");
+__RCSID("$NetBSD: blacklistctl.c,v 1.17.2.4 2016/12/18 07:58:52 snj Exp $");
 
 #include <stdio.h>
 #include <time.h>
@@ -93,9 +93,9 @@ main(int argc, char *argv[])
 			break;
 		case 'b':
 			blocked = 1;
+			break;
 		case 'D':
 			dbname = optarg;
-			break;
 			break;
 		case 'd':
 			debug++;
