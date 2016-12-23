c13687d14a1f0d2b7c623cce26d21f9923b3fc41
snj <snj@NetBSD.org>
2016-12-23 07:47:40 +0000

    Pull up following revision(s) (requested by mrg in ticket #1309): 	libexec/httpd/CHANGES: revisions 1.23, 1.24 	libexec/httpd/bozohttpd.8: revisions 1.60-1.62 	libexec/httpd/bozohttpd.c: revisions 1.81-1.84 	libexec/httpd/bozohttpd.h: revision 1.46 	libexec/httpd/cgi-bozo.c: revision 1.35 	libexec/httpd/content-bozo.c: revision 1.14 	libexec/httpd/main.c: revisions 1.14-1.16 	libexec/httpd/testsuite/Makefile: revision 1.6 	libexec/httpd/testsuite/test-bigfile: revision 1.3 	libexec/httpd/testsuite/test-simple: revisions 1.1, 1.2 update bozohttpd to 2016072: - fix memory leak - addd -G option to display version - fix some content type issues - fix issues in testsuite

--- libexec/httpd/bozohttpd.h
+++ libexec/httpd/bozohttpd.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: bozohttpd.h,v 1.33.2.2.2.1 2016/04/15 18:55:49 snj Exp $	*/
+/*	$NetBSD: bozohttpd.h,v 1.33.2.2.2.2 2016/12/23 07:47:40 snj Exp $	*/
 
 /*	$eterna: bozohttpd.h,v 1.39 2011/11/18 09:21:15 mrg Exp $	*/
 
@@ -366,5 +366,7 @@ bozoheaders_t *addmerge_replheader(bozo_httpreq_t *, const char *,
 /* variables */
 int bozo_set_pref(bozohttpd_t *, bozoprefs_t *, const char *, const char *);
 char *bozo_get_pref(bozoprefs_t *, const char *);
+
+int bozo_get_version(char */*buf*/, size_t /*size*/);
 
 #endif	/* BOZOHTTOPD_H_ */
