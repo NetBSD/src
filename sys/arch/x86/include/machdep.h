d452cdf41e267f07eabef5a7dfb0e1c856397d15
cherry <cherry>
2016-12-26 17:54:06 +0000

    the i386 and amd64 boot time msgbuf init code is nearly identical.
    Unify them into x86/x86_machdep.c:init_x86_msgbuf()
    
    Boot tested on GENERIC (i386, amd64), XEN3_DOM0 (amd64)

--- sys/arch/x86/include/machdep.h
+++ sys/arch/x86/include/machdep.h
@@ -1,4 +1,4 @@
-/* $NetBSD: machdep.h,v 1.8 2016/07/16 17:02:34 maxv Exp $ */
+/* $NetBSD: machdep.h,v 1.9 2016/12/26 17:54:07 cherry Exp $ */
 /*
  * Copyright (c) 2000, 2007 The NetBSD Foundation, Inc.
  * All rights reserved.
@@ -32,10 +32,20 @@
 
 extern phys_ram_seg_t mem_clusters[];
 extern int mem_cluster_cnt;
+extern vaddr_t msgbuf_vaddr;
+extern unsigned int msgbuf_p_cnt;
+
 
 struct btinfo_memmap;
 struct extent;
 struct sysctllog;
+
+struct msgbuf_p_seg {
+	paddr_t paddr;
+	psize_t sz;
+};
+
+extern struct msgbuf_p_seg msgbuf_p_seg[];
 
 void	x86_cpu_idle_init(void);
 void	x86_cpu_idle_get(void (**)(void), char *, size_t);
@@ -45,6 +55,7 @@ int	x86_select_freelist(uint64_t);
 
 void	init_x86_clusters(void);
 int	init_x86_vm(paddr_t);
+void	init_x86_msgbuf(void);
 
 void	x86_startup(void);
 void	x86_sysctl_machdep_setup(struct sysctllog **);
