c6028493eb9b928d850e34d151c1f8e394c7f2eb
skrll <skrll>
2017-01-02 16:55:50 +0000

    Parse the extended capabilies to and log each controller port to SS/HS bus root hub ports.
    Create/attach the two buses and adapt the xhci_roothub_ctrl to deal with
    both buses and sets of roothub ports.
    
    XXX the roothub ub_devices entry needs work to interact with usbdevs(1)
    XXX correctly

--- sys/arch/arm/nvidia/tegra_xusb.c
+++ sys/arch/arm/nvidia/tegra_xusb.c
@@ -1,4 +1,4 @@
-/* $NetBSD: tegra_xusb.c,v 1.1.2.2 2016/10/05 20:55:25 skrll Exp $ */
+/* $NetBSD: tegra_xusb.c,v 1.1.2.3 2017/01/02 16:55:50 skrll Exp $ */
 
 /*
  * Copyright (c) 2016 Jonathan A. Kollasch
@@ -30,7 +30,7 @@
 #include "opt_tegra.h"
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: tegra_xusb.c,v 1.1.2.2 2016/10/05 20:55:25 skrll Exp $");
+__KERNEL_RCSID(0, "$NetBSD: tegra_xusb.c,v 1.1.2.3 2017/01/02 16:55:50 skrll Exp $");
 
 #include <sys/param.h>
 #include <sys/bus.h>
@@ -348,6 +348,8 @@ tegra_xusb_mountroot(device_t self)
 	}
 
 	sc->sc_child = config_found(self, &sc->sc_bus, usbctlprint);
+
+	sc->sc_child2 = config_found(self, &sc->sc_bus2, usbctlprint);
 
 	error = xusb_mailbox_send(psc, 0x01000000);
 	if (error) {
--- sys/dev/pci/xhci_pci.c
+++ sys/dev/pci/xhci_pci.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: xhci_pci.c,v 1.4.2.7 2016/12/05 10:55:16 skrll Exp $	*/
+/*	$NetBSD: xhci_pci.c,v 1.4.2.8 2017/01/02 16:55:50 skrll Exp $	*/
 /*	OpenBSD: xhci_pci.c,v 1.4 2014/07/12 17:38:51 yuo Exp	*/
 
 /*
@@ -32,7 +32,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: xhci_pci.c,v 1.4.2.7 2016/12/05 10:55:16 skrll Exp $");
+__KERNEL_RCSID(0, "$NetBSD: xhci_pci.c,v 1.4.2.8 2017/01/02 16:55:50 skrll Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -126,7 +126,6 @@ xhci_pci_attach(device_t parent, device_t self, void *aux)
 	char intrbuf[PCI_INTRSTR_LEN];
 
 	sc->sc_dev = self;
-	sc->sc_bus.ub_hcpriv = sc;
 
 	pci_aprint_devinfo(pa, "USB Controller");
 
@@ -219,8 +218,11 @@ xhci_pci_attach(device_t parent, device_t self, void *aux)
 	                          xhci_shutdown))
 		aprint_error_dev(self, "couldn't establish power handler\n");
 
-	/* Attach usb device. */
+	/* Attach usb buses. */
 	sc->sc_child = config_found(self, &sc->sc_bus, usbctlprint);
+
+ 	sc->sc_child2 = config_found(self, &sc->sc_bus2, usbctlprint);
+
 	return;
 
 fail:
