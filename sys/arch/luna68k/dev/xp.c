a8e3a8332859e7a90b435adeec884474fe6b2d7d
tsutsui <tsutsui@NetBSD.org>
2017-01-08 16:41:35 +0000

    Consistently use sc_shm_size initialized at device attach.

--- sys/arch/luna68k/dev/xp.c
+++ sys/arch/luna68k/dev/xp.c
@@ -1,4 +1,4 @@
-/* $NetBSD: xp.c,v 1.2 2017/01/08 16:34:44 tsutsui Exp $ */
+/* $NetBSD: xp.c,v 1.3 2017/01/08 16:41:35 tsutsui Exp $ */
 
 /*-
  * Copyright (c) 2016 Izumi Tsutsui.  All rights reserved.
@@ -29,7 +29,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: xp.c,v 1.2 2017/01/08 16:34:44 tsutsui Exp $");
+__KERNEL_RCSID(0, "$NetBSD: xp.c,v 1.3 2017/01/08 16:41:35 tsutsui Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -220,7 +220,7 @@ xp_ioctl(dev_t dev, u_long cmd, void *addr, int flags, struct lwp *l)
 	case XPIOCDOWNLD:
 		downld = addr;
 		loadsize = downld->size;
-		if (loadsize == 0 || loadsize > XP_SHM_SIZE) {
+		if (loadsize == 0 || loadsize > sc->sc_shm_size) {
 			return EINVAL;
 		}
 
