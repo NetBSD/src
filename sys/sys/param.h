a67a46484029d338ceae7225e488ae099f618e1b
kamil <kamil@NetBSD.org>
2017-01-13 23:05:24 +0000

    Welcome to NetBSD 7.99.59 - PTRACE_VFORK and PTRACE_VFORK_DONE in ptrace(2)
    Sponsored by <The NetBSD Foundation>

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.525 2017/01/11 09:09:42 hannken Exp $	*/
+/*	$NetBSD: param.h,v 1.526 2017/01/13 23:05:24 kamil Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -67,7 +67,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	799005800	/* NetBSD 7.99.58 */
+#define	__NetBSD_Version__	799005900	/* NetBSD 7.99.59 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
