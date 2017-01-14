09d09fb1088a47dd4c473156f752fe4f42692ba9
sjg <sjg@NetBSD.org>
2017-01-14 22:58:04 +0000

    Allow providing a utc value to :{gm,local}time
    Reviewed by: christos

--- usr.bin/make/unit-tests/varmisc.mk
+++ usr.bin/make/unit-tests/varmisc.mk
@@ -1,8 +1,9 @@
-# $Id: varmisc.mk,v 1.5 2015/10/12 17:09:29 sjg Exp $
+# $Id: varmisc.mk,v 1.6 2017/01/14 22:58:04 sjg Exp $
 #
 # Miscellaneous variable tests.
 
-all: unmatched_var_paren D_true U_true D_false U_false Q_lhs Q_rhs NQ_none
+all: unmatched_var_paren D_true U_true D_false U_false Q_lhs Q_rhs NQ_none \
+	strftime
 
 unmatched_var_paren:
 	@echo ${foo::=foo-text}
@@ -40,3 +41,11 @@ Q_rhs:
 NQ_none:
 	@echo do not evaluate or expand :? if discarding
 	@echo ${VSET:U${1:L:?${True}:${False}}}
+
+April1= 1459494000
+
+# slightly contorted syntax to use utc via variable
+strftime:
+	@echo ${year=%Y month=%m day=%d:L:gmtime=1459494000}
+	@echo date=${%Y%m%d:L:${gmtime=${April1}:L}}
+
