cfb4319973d4a63e10d2004a8b9fe074a19f7999
christos <christos@NetBSD.org>
2017-01-16 17:39:22 +0000

    descend and regen

--- sys/compat/linux32/arch/amd64/linux32_sysent.c
+++ sys/compat/linux32/arch/amd64/linux32_sysent.c
@@ -1,4 +1,4 @@
-/* $NetBSD: linux32_sysent.c,v 1.76 2017/01/13 06:19:36 christos Exp $ */
+/* $NetBSD: linux32_sysent.c,v 1.77 2017/01/16 17:39:22 christos Exp $ */
 
 /*
  * System call switch table.
@@ -8,7 +8,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: linux32_sysent.c,v 1.76 2017/01/13 06:19:36 christos Exp $");
+__KERNEL_RCSID(0, "$NetBSD: linux32_sysent.c,v 1.77 2017/01/16 17:39:22 christos Exp $");
 
 #include <sys/param.h>
 #include <sys/poll.h>
