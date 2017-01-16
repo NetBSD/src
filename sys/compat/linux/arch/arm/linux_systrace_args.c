19765dfb8bfa28c5d9a79895404931749ee922bf
christos <christos@NetBSD.org>
2017-01-16 00:11:09 +0000

    regen all

--- sys/compat/linux/arch/arm/linux_systrace_args.c
+++ sys/compat/linux/arch/arm/linux_systrace_args.c
@@ -1,4 +1,4 @@
-/* $NetBSD: linux_systrace_args.c,v 1.8 2017/01/15 17:00:37 christos Exp $ */
+/* $NetBSD: linux_systrace_args.c,v 1.9 2017/01/16 00:11:09 christos Exp $ */
 
 /*
  * System call argument to DTrace register array converstion.
