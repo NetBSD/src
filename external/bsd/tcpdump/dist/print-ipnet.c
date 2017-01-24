9515a2b16579388c78ec75b13ebdd6f00be8bb73
christos <christos@NetBSD.org>
2017-01-24 21:33:37 +0000

    Tuesday October 25, 2016 mcr@sandelman.ca   Summary for 4.8.1 tcpdump release 	Fix "-x" for Apple PKTAP and PPI packets         Use PRIx64 to print a 64-bit number in hex.         Printer for HNCP (RFCs 7787 and 7788).         dagid is always an IPv6 address, not an opaque 128-bit string, and other fixes to RPL printer.         RSVP: Add bounds and length checks         OSPF: Do more bounds checking         Handle OpenSSL 1.1.x.         Initial support for the REdis Serialization Protocol known as RESP.         Add printing function for Generic Protocol Extension for VXLAN             draft-ietf-nvo3-vxlan-gpe-01         Network Service Header: draft-ietf-sfc-nsh-01         Don't recompile the filter if the new file has the same DLT.         Pass an adjusted struct pcap_pkthdr to the sub-printer.         Add three test cases for already fixed CVEs            CVE-2014-8767: OLSR            CVE-2014-8768: Geonet            CVE-2014-8769: AODV         Don't do the DDP-over-UDP heuristic first: GitHub issue #499.         Use the new debugging routines in libpcap.         Harmonize TCP source or destination ports tests with UDP ones         Introduce data types to use for integral values in packet structures.         RSVP: Fix an infinite loop         Support of Type 3 and Type 4 LISP packets.         Don't require IPv6 library support in order to support IPv6 addresses.         Many many changes to support libnetdissect usage.         Add a test that makes unaligned accesses: GitHub issue #478.         add a DNSSEC test case: GH #445 and GH #467.         BGP: add decoding of ADD-PATH capability         fixes to LLC header printing, and RFC948-style IP packets
    Friday April 10, 2015 guy@alum.mit.edu
      Summary for 4.7.4 tcpdump release
    	RPKI to Router Protocol: Fix Segmentation Faults and other problems
    	RPKI to Router Protocol: print strings with fn_printn()
    	wb: fix some bounds checks

--- external/bsd/tcpdump/dist/print-ipnet.c
+++ external/bsd/tcpdump/dist/print-ipnet.c
@@ -1,11 +1,10 @@
-#define NETDISSECT_REWORKED
 #ifdef HAVE_CONFIG_H
 #include "config.h"
 #endif
 
-#include <tcpdump-stdinc.h>
+#include <netdissect-stdinc.h>
 
-#include "interface.h"
+#include "netdissect.h"
 
 typedef struct ipnet_hdr {
 	uint8_t		iph_version;
@@ -55,7 +54,7 @@ ipnet_hdr_print(netdissect_options *ndo, const u_char *bp, u_int length)
 static void
 ipnet_print(netdissect_options *ndo, const u_char *p, u_int length, u_int caplen)
 {
-	ipnet_hdr_t *hdr;
+	const ipnet_hdr_t *hdr;
 
 	if (caplen < sizeof(ipnet_hdr_t)) {
 		ND_PRINT((ndo, "[|ipnet]"));
@@ -67,7 +66,7 @@ ipnet_print(netdissect_options *ndo, const u_char *p, u_int length, u_int caplen
 
 	length -= sizeof(ipnet_hdr_t);
 	caplen -= sizeof(ipnet_hdr_t);
-	hdr = (ipnet_hdr_t *)p;
+	hdr = (const ipnet_hdr_t *)p;
 	p += sizeof(ipnet_hdr_t);
 
 	switch (hdr->iph_family) {
@@ -82,7 +81,7 @@ ipnet_print(netdissect_options *ndo, const u_char *p, u_int length, u_int caplen
 
 	default:
 		if (!ndo->ndo_eflag)
-			ipnet_hdr_print(ndo, (u_char *)hdr,
+			ipnet_hdr_print(ndo, (const u_char *)hdr,
 					length + sizeof(ipnet_hdr_t));
 
 		if (!ndo->ndo_suppress_default_print)
