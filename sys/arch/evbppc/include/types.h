e49432d92b11c4ce8f833368984775c963bfcd85
christos <christos@NetBSD.org>
2017-01-26 15:55:08 +0000

    provide __HAVE_COMPAT_NETBSD32 and fix multiple include protection consistently.

--- sys/arch/evbppc/include/types.h
+++ sys/arch/evbppc/include/types.h
@@ -1,5 +1,11 @@
-/*	$NetBSD: types.h,v 1.10 2011/07/17 23:23:53 dyoung Exp $	*/
+/*	$NetBSD: types.h,v 1.11 2017/01/26 15:55:09 christos Exp $	*/
+
+#ifndef _EVBPPC_TYPES_H_
+#define	_EVBPPC_TYPES_H_
 
 #include <powerpc/types.h>
 
 #define	__HAVE_NEW_STYLE_BUS_H
+#define	__HAVE_COMPAT_NETBSD32
+
+#endif
