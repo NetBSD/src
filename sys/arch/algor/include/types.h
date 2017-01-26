36c4515e8e531a002a2b8a8d4180745e8c665682
christos <christos@NetBSD.org>
2017-01-26 15:55:08 +0000

    provide __HAVE_COMPAT_NETBSD32 and fix multiple include protection consistently.

--- sys/arch/algor/include/types.h
+++ sys/arch/algor/include/types.h
@@ -1,5 +1,11 @@
-/*	$NetBSD: types.h,v 1.6 2011/07/13 04:53:39 dyoung Exp $	*/
+/*	$NetBSD: types.h,v 1.7 2017/01/26 15:55:09 christos Exp $	*/
+
+#ifndef _ALGOR_TYPES_H_
+#define	_ALGOR_TYPES_H_
 
 #include <mips/types.h>
 
 #define	__HAVE_NEW_STYLE_BUS_H
+#define	__HAVE_COMPAT_NETBSD32
+
+#endif
