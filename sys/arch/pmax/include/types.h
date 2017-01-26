bac7b3cffac157fb71a15c3a3ad776a88ce6e22b
christos <christos@NetBSD.org>
2017-01-26 15:55:08 +0000

    provide __HAVE_COMPAT_NETBSD32 and fix multiple include protection consistently.

--- sys/arch/pmax/include/types.h
+++ sys/arch/pmax/include/types.h
@@ -1,9 +1,15 @@
-/*	$NetBSD: types.h,v 1.25 2008/01/20 18:09:08 joerg Exp $	*/
+/*	$NetBSD: types.h,v 1.26 2017/01/26 15:55:10 christos Exp $	*/
+
+#ifndef _PMAX_TYPES_H_
+#define	_PMAX_TYPES_H_
 
 #include <mips/types.h>
 
 #define	__HAVE_DEVICE_REGISTER
+#define	__HAVE_COMPAT_NETBSD32
 
 /* MIPS specific options */
 #define	__HAVE_BOOTINFO_H
 #define	__HAVE_MIPS_MACHDEP_CACHE_CONFIG
+
+#endif
