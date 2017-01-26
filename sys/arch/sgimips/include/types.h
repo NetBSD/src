6245b7d2b00c14a297cbf7f88ddbfb0204219c72
christos <christos>
2017-01-26 15:55:08 +0000

    provide __HAVE_COMPAT_NETBSD32 and fix multiple include protection consistently.

--- sys/arch/sgimips/include/types.h
+++ sys/arch/sgimips/include/types.h
@@ -1,4 +1,7 @@
-/*	$NetBSD: types.h,v 1.16 2011/07/17 01:36:50 dyoung Exp $	*/
+/*	$NetBSD: types.h,v 1.17 2017/01/26 15:55:10 christos Exp $	*/
+
+#ifndef _SGIMIPS_TYPES_H_
+#define	_SGIMIPS_TYPES_H_
 
 #define _MIPS_PADDR_T_64BIT
 
@@ -9,3 +12,6 @@
 /* MIPS specific options */
 #define	__HAVE_BOOTINFO_H
 #define	__HAVE_MIPS_MACHDEP_CACHE_CONFIG
+#define	__HAVE_COMPAT_NETBSD32
+
+#endif
