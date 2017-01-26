f7e2dec3add72b46d3d8315a92522f823c2c564a
christos <christos@NetBSD.org>
2017-01-26 15:54:31 +0000

    use __HAVE_COMPAT_NETBSD32

--- sys/kern/kern_proc.c
+++ sys/kern/kern_proc.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: kern_proc.c,v 1.202 2017/01/26 08:09:27 martin Exp $	*/
+/*	$NetBSD: kern_proc.c,v 1.203 2017/01/26 15:54:31 christos Exp $	*/
 
 /*-
  * Copyright (c) 1999, 2006, 2007, 2008 The NetBSD Foundation, Inc.
@@ -62,7 +62,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: kern_proc.c,v 1.202 2017/01/26 08:09:27 martin Exp $");
+__KERNEL_RCSID(0, "$NetBSD: kern_proc.c,v 1.203 2017/01/26 15:54:31 christos Exp $");
 
 #ifdef _KERNEL_OPT
 #include "opt_kstack.h"
@@ -104,9 +104,10 @@ __KERNEL_RCSID(0, "$NetBSD: kern_proc.c,v 1.202 2017/01/26 08:09:27 martin Exp $
 #include <uvm/uvm_extern.h>
 #include <uvm/uvm.h>
 
-#if defined(_LP64) && !defined(COMPAT_NETBSD32) && !defined(_RUMPKERNEL)
+#if defined(__HAVE_COMPAT_NETBSD32) && !defined(COMPAT_NETBSD32)
 #define COMPAT_NETBSD32
 #endif
+
 #ifdef COMPAT_NETBSD32
 #include <compat/netbsd32/netbsd32.h>
 #endif
