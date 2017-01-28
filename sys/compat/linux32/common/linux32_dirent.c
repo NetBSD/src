b6a9076bf9815ed5c3d12a44abe3e82e767ee2d7
christos <christos@NetBSD.org>
2017-01-28 15:09:55 +0000

    we have space for one more.

--- sys/compat/linux32/common/linux32_dirent.c
+++ sys/compat/linux32/common/linux32_dirent.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: linux32_dirent.c,v 1.15 2017/01/28 15:08:06 christos Exp $ */
+/*	$NetBSD: linux32_dirent.c,v 1.16 2017/01/28 15:09:55 christos Exp $ */
 
 /*-
  * Copyright (c) 2006 Emmanuel Dreyfus, all rights reserved.
@@ -33,7 +33,7 @@
 
 #include <sys/cdefs.h>
 
-__KERNEL_RCSID(0, "$NetBSD: linux32_dirent.c,v 1.15 2017/01/28 15:08:06 christos Exp $");
+__KERNEL_RCSID(0, "$NetBSD: linux32_dirent.c,v 1.16 2017/01/28 15:09:55 christos Exp $");
 
 #include <sys/types.h>
 #include <sys/param.h>
@@ -215,7 +215,7 @@ again:
 			idb.d_off = (linux32_off_t)off;
 			idb.d_reclen = (u_short)linux32_reclen;
 		}
-		size_t l = MIN(sizeof(idb.d_name) - 2, bdp->d_namlen + 1);
+		size_t l = MIN(sizeof(idb.d_name) - 1, bdp->d_namlen + 1);
 		memcpy(idb.d_name, bdp->d_name, l);
 		idb.d_name[l + 1] = bdp->d_type;
 		if ((error = copyout((void *)&idb, outp, linux32_reclen)))
