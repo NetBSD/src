d3b3f557c0f394624732813887be4402e230b9b4
roy <roy@NetBSD.org>
2017-01-31 09:17:53 +0000

    Move ESCDELAY to curses.c so all globals are close to each other. Remove _reentrant and use ESCDELAY and TABSIZE as we're not really reentrant.

--- lib/libcurses/get_wch.c
+++ lib/libcurses/get_wch.c
@@ -1,4 +1,4 @@
-/*   $NetBSD: get_wch.c,v 1.13 2017/01/30 14:55:58 roy Exp $ */
+/*   $NetBSD: get_wch.c,v 1.14 2017/01/31 09:17:53 roy Exp $ */
 
 /*
  * Copyright (c) 2005 The NetBSD Foundation Inc.
@@ -36,7 +36,7 @@
 
 #include <sys/cdefs.h>
 #ifndef lint
-__RCSID("$NetBSD: get_wch.c,v 1.13 2017/01/30 14:55:58 roy Exp $");
+__RCSID("$NetBSD: get_wch.c,v 1.14 2017/01/31 09:17:53 roy Exp $");
 #endif						  /* not lint */
 
 #include <string.h>
@@ -91,8 +91,6 @@ inkey(wchar_t *wc, int to, int delay)
 				*working = &_cursesi_screen->cbuf_cur,
 				*end = &_cursesi_screen->cbuf_tail;
 	char		*inbuf = &_cursesi_screen->cbuf[ 0 ];
-	int		escdelay = _reentrant ?
-			    _cursesi_screen->ESCDELAY : ESCDELAY;
 
 #ifdef DEBUG
 	__CTRACE(__CTRACE_INPUT, "inkey (%p, %d, %d)\n", wc, to, delay);
@@ -141,11 +139,11 @@ inkey(wchar_t *wc, int to, int delay)
 		} else if (wstate == INKEY_ASSEMBLING) {
 			/* assembling a key sequence */
 			if (delay) {
-				if (__timeout(to ? (escdelay / 100) : delay)
+				if (__timeout(to ? (ESCDELAY / 100) : delay)
 						== ERR)
 					return ERR;
 			} else {
-				if (to && (__timeout(escdelay / 100) == ERR))
+				if (to && (__timeout(ESCDELAY / 100) == ERR))
 					return ERR;
 			}
 
@@ -191,11 +189,11 @@ inkey(wchar_t *wc, int to, int delay)
 		} else if (wstate == INKEY_WCASSEMBLING) {
 			/* assembling a wide-char sequence */
 			if (delay) {
-				if (__timeout(to ? (escdelay / 100) : delay)
+				if (__timeout(to ? (ESCDELAY / 100) : delay)
 						== ERR)
 					return ERR;
 			} else {
-				if (to && (__timeout(escdelay / 100) == ERR))
+				if (to && (__timeout(ESCDELAY / 100) == ERR))
 					return ERR;
 			}
 
--- lib/libcurses/setterm.c
+++ lib/libcurses/setterm.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: setterm.c,v 1.63 2017/01/30 16:50:44 roy Exp $	*/
+/*	$NetBSD: setterm.c,v 1.64 2017/01/31 09:17:53 roy Exp $	*/
 
 /*
  * Copyright (c) 1981, 1993, 1994
@@ -34,7 +34,7 @@
 #if 0
 static char sccsid[] = "@(#)setterm.c	8.8 (Berkeley) 10/25/94";
 #else
-__RCSID("$NetBSD: setterm.c,v 1.63 2017/01/30 16:50:44 roy Exp $");
+__RCSID("$NetBSD: setterm.c,v 1.64 2017/01/31 09:17:53 roy Exp $");
 #endif
 #endif /* not lint */
 
@@ -406,7 +406,6 @@ set_tabsize(int tabsize)
 	if (_cursesi_screen == NULL)
 		return ERR;
 	_cursesi_screen->TABSIZE = tabsize;
-	_reentrant = true;
 	TABSIZE = tabsize;
 	return OK;
 }
