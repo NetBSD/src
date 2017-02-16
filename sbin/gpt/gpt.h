b06118a7905625602e8c98a2033cb96209be3d51
christos <christos>
2017-02-16 03:32:17 +0000

    Add -T timestamp for reproducible builds.

--- sbin/gpt/gpt.h
+++ sbin/gpt/gpt.h
@@ -74,7 +74,7 @@ struct gpt_cmd {
 uint32_t crc32(const void *, size_t);
 void	gpt_close(gpt_t);
 int	gpt_gpt(gpt_t, off_t, int);
-gpt_t	gpt_open(const char *, int, int, off_t, u_int);
+gpt_t	gpt_open(const char *, int, int, off_t, u_int, time_t);
 #define GPT_READONLY	0x01
 #define GPT_MODIFIED	0x02
 #define GPT_QUIET	0x04
