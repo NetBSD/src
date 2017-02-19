45eb8da2f1c99000385f8dc0f9cea9e8f877c6f3
snj <snj@NetBSD.org>
2017-02-19 07:26:18 +0000

    Pull up following revision(s) (requested by spz in ticket #1435): 	external/mit/expat/dist/CMake.README: up to 1.1.1.3 	external/mit/expat/dist/CMakeLists.txt: up to 1.1.1.3 	external/mit/expat/dist/COPYING: up to 1.1.1.2 	external/mit/expat/dist/Changes: up to 1.1.1.4 	external/mit/expat/dist/MANIFEST: up to 1.1.1.4 	external/mit/expat/dist/Makefile.in: up to 1.1.1.4 	external/mit/expat/dist/README: up to 1.1.1.4 	external/mit/expat/dist/aclocal.m4: up to 1.1.1.3 	external/mit/expat/dist/amiga/expat_lib.c: up to 1.1.1.2 	external/mit/expat/dist/amiga/expat_vectors.c: up to 1.1.1.2 	external/mit/expat/dist/amiga/launch.c: up to 1.1.1.2 	external/mit/expat/dist/bcb5/expat.bpr: up to 1.1.1.3 	external/mit/expat/dist/bcb5/expat.mak: up to 1.1.1.2 	external/mit/expat/dist/bcb5/expat_static.bpr: up to 1.1.1.3 	external/mit/expat/dist/bcb5/expat_static.mak: up to 1.1.1.2 	external/mit/expat/dist/bcb5/expatw.bpr: up to 1.1.1.3 	external/mit/expat/dist/bcb5/expatw.mak: up to 1.1.1.2 	external/mit/expat/dist/bcb5/expatw_static.bpr: up to 1.1.1.3 	external/mit/expat/dist/bcb5/expatw_static.mak: up to 1.1.1.2 	external/mit/expat/dist/bcb5/xmlwf.bpr: up to 1.1.1.3 	external/mit/expat/dist/bcb5/xmlwf.mak: up to 1.1.1.2 	external/mit/expat/dist/configure: up to 1.1.1.4 	external/mit/expat/dist/configure.ac: up to 1.1.1.2 	external/mit/expat/dist/conftools/config.guess: up to 1.3 	external/mit/expat/dist/conftools/config.sub: up to 1.3 	external/mit/expat/dist/conftools/install-sh: up to 1.1.1.4 	external/mit/expat/dist/conftools/ltmain.sh: up to 1.1.1.4 	external/mit/expat/dist/doc/expat.png: up to 1.1.1.2 	external/mit/expat/dist/doc/valid-xhtml10.png: up to 1.1.1.2 	external/mit/expat/dist/doc/xmlwf.1: up to 1.1.1.3 	external/mit/expat/dist/doc/xmlwf.sgml          delete 	external/mit/expat/dist/doc/xmlwf.xml: up to 1.1.1.1 	external/mit/expat/dist/examples/elements.c: up to 1.1.1.3 	external/mit/expat/dist/examples/outline.c: up to 1.1.1.2 	external/mit/expat/dist/expat_config.h.in: up to 1.1.1.4 	external/mit/expat/dist/lib/expat.dsp: up to 1.1.1.3 	external/mit/expat/dist/lib/expat.h: up to 1.1.1.4 	external/mit/expat/dist/lib/expat_external.h: up to 1.1.1.2 	external/mit/expat/dist/lib/expat_static.dsp: up to 1.1.1.3 	external/mit/expat/dist/lib/expatw.dsp: up to 1.1.1.3 	external/mit/expat/dist/lib/expatw_static.dsp: up to 1.1.1.2 	external/mit/expat/dist/lib/internal.h: up to 1.1.1.2 	external/mit/expat/dist/lib/xmlparse.c: up to 1.6 	external/mit/expat/dist/lib/xmlrole.c: up to 1.1.1.3 	external/mit/expat/dist/lib/xmltok.c: up to 1.3 	external/mit/expat/dist/lib/xmltok_impl.c: up to 1.5 	external/mit/expat/dist/m4/libtool.m4: up to 1.1.1.3 	external/mit/expat/dist/m4/ltoptions.m4: up to 1.1.1.3 	external/mit/expat/dist/m4/ltsugar.m4: up to 1.1.1.2 	external/mit/expat/dist/m4/ltversion.m4: up to 1.1.1.3 	external/mit/expat/dist/m4/lt~obsolete.m4: up to 1.1.1.2 	external/mit/expat/dist/tests/chardata.c: up to 1.1.1.3 	external/mit/expat/dist/tests/minicheck.c: up to 1.1.1.2 	external/mit/expat/dist/tests/minicheck.h: up to 1.1.1.3 	external/mit/expat/dist/tests/runtests.c: up to 1.1.1.3 	external/mit/expat/dist/tests/xmltest.sh: up to 1.2 	external/mit/expat/dist/win32/README.txt: up to 1.1.1.3 	external/mit/expat/dist/win32/expat.iss: up to 1.1.1.4 	external/mit/expat/dist/xmlwf/codepage.c: up to 1.1.1.2 	external/mit/expat/dist/xmlwf/readfilemap.c: up to 1.1.1.3 	external/mit/expat/dist/xmlwf/xmlfile.c: up to 1.1.1.2 	external/mit/expat/dist/xmlwf/xmlwf.c: up to 1.1.1.4 	external/mit/expat/dist/xmlwf/xmlwf.dsp: up to 1.1.1.3 	external/mit/expat/lib/libexpat/expat_config.h: up to 1.4 Update expat to 2.2.0, fixing CVE-2016-0718, CVE-2016-4472, CVE-2016-5300, and CVE-2012-6702.

--- external/mit/expat/lib/libexpat/expat_config.h
+++ external/mit/expat/lib/libexpat/expat_config.h
@@ -1,5 +1,5 @@
 /* expat_config.h.  Generated from expat_config.h.in by configure.  */
-/* expat_config.h.in.  Generated from configure.in by autoheader.  */
+/* expat_config.h.in.  Generated from configure.ac by autoheader.  */
 
 /* 1234 = LIL_ENDIAN, 4321 = BIGENDIAN */
 #define BYTEORDER BYTE_ORDER
@@ -52,8 +52,7 @@
 /* Define to 1 if you have the <unistd.h> header file. */
 #define HAVE_UNISTD_H 1
 
-/* Define to the sub-directory in which libtool stores uninstalled libraries.
-   */
+/* Define to the sub-directory where libtool stores uninstalled libraries. */
 #define LT_OBJDIR ".libs/"
 
 /* Define to the address where bug reports for this package should be sent. */
@@ -63,7 +62,7 @@
 #define PACKAGE_NAME "expat"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "expat 2.1.1"
+#define PACKAGE_STRING "expat 2.2.0"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "expat"
@@ -72,7 +71,7 @@
 #define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "2.1.1"
+#define PACKAGE_VERSION "2.2.0"
 
 /* Define to 1 if you have the ANSI C header files. */
 #define STDC_HEADERS 1
