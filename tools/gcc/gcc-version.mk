8748f80523b71defb86750a84016cd806efc733d
maya <maya@NetBSD.org>
2017-02-22 12:34:49 +0000

    GC more gcc 4.8 specific (dead) logic

--- tools/gcc/gcc-version.mk
+++ tools/gcc/gcc-version.mk
@@ -1,9 +1,5 @@
-#	$NetBSD: gcc-version.mk,v 1.6 2016/06/07 08:12:13 mrg Exp $
+#	$NetBSD: gcc-version.mk,v 1.7 2017/02/22 12:34:49 maya Exp $
 
 # common location for tools and native build
 
-.if ${HAVE_GCC} == 48
-NETBSD_GCC_VERSION=nb3 20151015
-.else
 NETBSD_GCC_VERSION=nb1 20160606
-.endif
