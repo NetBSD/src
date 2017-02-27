be33f6b6ae0c1816c0a86fa116cebc8bc1fd7f82
jdolecek <jdolecek@NetBSD.org>
2017-02-27 21:33:47 +0000

    Welcome to 7.99.63 - struct ld_softc changes

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.529 2017/02/23 03:48:20 kamil Exp $	*/
+/*	$NetBSD: param.h,v 1.530 2017/02/27 21:33:47 jdolecek Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -67,7 +67,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	799006200	/* NetBSD 7.99.62 */
+#define	__NetBSD_Version__	799006300	/* NetBSD 7.99.63 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
