8155a2ae5c2fc6c28d3041034793bff9399e5cba
ozaki-r <ozaki-r@NetBSD.org>
2017-03-03 07:34:04 +0000

    Provide a more robust regexp for time formats of 1day-ish

--- tests/net/ndp/t_ndp.sh
+++ tests/net/ndp/t_ndp.sh
@@ -1,4 +1,4 @@
-#	$NetBSD: t_ndp.sh,v 1.17 2016/11/25 08:51:17 ozaki-r Exp $
+#	$NetBSD: t_ndp.sh,v 1.18 2017/03/03 07:34:04 ozaki-r Exp $
 #
 # Copyright (c) 2015 The NetBSD Foundation, Inc.
 # All rights reserved.
@@ -137,7 +137,7 @@ ndp_cache_expiration_body()
 	$DEBUG && rump.ndp -n -a
 	atf_check -s exit:0 -o match:'permanent' rump.ndp -n $IP6SRC
 	# Expired but remains until GC sweaps it (1 day)
-	atf_check -s exit:0 -o match:'(1d0h0m|23h59m)' rump.ndp -n $IP6DST
+	atf_check -s exit:0 -o match:"$ONEDAYISH" rump.ndp -n $IP6DST
 
 	rump_server_destroy_ifaces
 }
