09f0041c18376f6d5856e152171d9c6a82a87c97
snj <snj@NetBSD.org>
2017-03-08 00:38:18 +0000

    Add back a netbsd-6* workaround accidentally removed in ticket 1437: Explicitly initialize uid to avoid a gcc warning.

--- libexec/httpd/bozohttpd.c
+++ libexec/httpd/bozohttpd.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: bozohttpd.c,v 1.30.2.4 2017/03/07 07:25:18 snj Exp $	*/
+/*	$NetBSD: bozohttpd.c,v 1.30.2.5 2017/03/08 00:38:18 snj Exp $	*/
 
 /*	$eterna: bozohttpd.c,v 1.178 2011/11/18 09:21:15 mrg Exp $	*/
 
@@ -2243,7 +2243,7 @@ bozo_setup(bozohttpd_t *httpd, bozoprefs_t *prefs, const char *vhost,
 	struct passwd	 *pw;
 	extern char	**environ;
 	static char	 *cleanenv[1] = { NULL };
-	uid_t		  uid;
+	uid_t		  uid = 0;
 	int		  uidset = 0;
 	char		 *chrootdir;
 	char		 *username;
