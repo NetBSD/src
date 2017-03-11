b63948223014ccc318682cfc4ea527f34f5f52c7
maya <maya@NetBSD.org>
2017-03-11 21:23:36 +0000

    Add missing comma, else the strings are concatenated (and possibly the wrong message is used). noted by coverity.
    Linux iwlwifi strings seem similar.

--- sys/dev/pci/if_iwnreg.h
+++ sys/dev/pci/if_iwnreg.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: if_iwnreg.h,v 1.15 2014/11/09 14:40:54 nonaka Exp $	*/
+/*	$NetBSD: if_iwnreg.h,v 1.16 2017/03/11 21:23:36 maya Exp $	*/
 /*	$OpenBSD: if_iwnreg.h,v 1.49 2014/09/09 18:56:24 sthen Exp $	*/
 
 /*-
@@ -1940,7 +1940,7 @@ static const char * const iwn_fw_errmsg[] = {
 	"NMI_INTERRUPT_DATA_ACTION_PT",
 	"NMI_TRM_HW_ER",
 	"NMI_INTERRUPT_TRM",
-	"NMI_INTERRUPT_BREAKPOINT"
+	"NMI_INTERRUPT_BREAKPOINT",
 	"DEBUG_0",
 	"DEBUG_1",
 	"DEBUG_2",
