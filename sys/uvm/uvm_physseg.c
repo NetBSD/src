867595e6a0ba53fd76e7baad224d21997e45ef72
riastradh <riastradh@NetBSD.org>
2017-03-19 23:30:39 +0000

    __diagused police

--- sys/uvm/uvm_physseg.c
+++ sys/uvm/uvm_physseg.c
@@ -1,4 +1,4 @@
-/* $NetBSD: uvm_physseg.c,v 1.7 2017/02/02 21:22:08 uwe Exp $ */
+/* $NetBSD: uvm_physseg.c,v 1.8 2017/03/19 23:30:39 riastradh Exp $ */
 
 /*
  * Copyright (c) 1997 Charles D. Cranor and Washington University.
@@ -1226,7 +1226,7 @@ bool
 uvm_physseg_unplug(paddr_t pfn, size_t pages)
 {
 	uvm_physseg_t upm;
-	paddr_t off = 0, start, end;
+	paddr_t off = 0, start __diagused, end;
 	struct uvm_physseg *seg;
 
 	upm = uvm_physseg_find(pfn, &off);
@@ -1246,9 +1246,6 @@ uvm_physseg_unplug(paddr_t pfn, size_t pages)
 		return false;
 	}
 
-#ifndef DIAGNOSTIC
-	(void) start;
-#endif
 	KASSERT(pfn == start + off); /* sanity */
 
 	if (__predict_true(uvm.page_init_done == true)) {
