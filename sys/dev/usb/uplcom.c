4f8ef46655eeeb98eee7ce2dedd2822485465739
snj <snj@NetBSD.org>
2017-03-25 17:30:18 +0000

    Pull up following revision(s) (requested by bad in ticket #1445): 	sys/dev/usb/uplcom.c: revision 1.75 Null suspend/resume handler for uplcom(4).

--- sys/dev/usb/uplcom.c
+++ sys/dev/usb/uplcom.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: uplcom.c,v 1.73.2.1 2017/03/25 17:26:53 snj Exp $	*/
+/*	$NetBSD: uplcom.c,v 1.73.2.2 2017/03/25 17:30:18 snj Exp $	*/
 /*
  * Copyright (c) 2001 The NetBSD Foundation, Inc.
  * All rights reserved.
@@ -34,7 +34,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: uplcom.c,v 1.73.2.1 2017/03/25 17:26:53 snj Exp $");
+__KERNEL_RCSID(0, "$NetBSD: uplcom.c,v 1.73.2.2 2017/03/25 17:30:18 snj Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -416,6 +416,9 @@ uplcom_attach(device_t parent, device_t self, void *aux)
 	sc->sc_subdev = config_found_sm_loc(self, "ucombus", NULL, &uca,
 					    ucomprint, ucomsubmatch);
 
+	if (!pmf_device_register(self, NULL, NULL))
+		aprint_error_dev(self, "couldn't establish power handler\n");
+
 	return;
 }
 
@@ -449,6 +452,9 @@ uplcom_detach(device_t self, int flags)
 
 	usbd_add_drv_event(USB_EVENT_DRIVER_DETACH, sc->sc_udev,
 			   sc->sc_dev);
+
+	if (rv == 0)
+		pmf_device_deregister(self);
 
 	return (rv);
 }
