258f6267934aaeb392cf3e462699b289e66dda51
hannken <hannken@NetBSD.org>
2017-03-30 09:13:01 +0000

    Change _fstrans_start() to allocate per lwp info for layered file systems to get a reference on the mount.
    Set mnt_lower on successfull mount only.

--- sys/miscfs/overlay/overlay_vfsops.c
+++ sys/miscfs/overlay/overlay_vfsops.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: overlay_vfsops.c,v 1.65 2017/03/06 10:10:07 hannken Exp $	*/
+/*	$NetBSD: overlay_vfsops.c,v 1.66 2017/03/30 09:13:01 hannken Exp $	*/
 
 /*
  * Copyright (c) 1999, 2000 National Aeronautics & Space Administration
@@ -74,7 +74,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: overlay_vfsops.c,v 1.65 2017/03/06 10:10:07 hannken Exp $");
+__KERNEL_RCSID(0, "$NetBSD: overlay_vfsops.c,v 1.66 2017/03/30 09:13:01 hannken Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -185,16 +185,18 @@ ov_mount(struct mount *mp, const char *path, void *data, size_t *data_len)
 	vn_lock(vp, LK_EXCLUSIVE | LK_RETRY);
 	vp->v_vflag |= VV_ROOT;
 	nmp->ovm_rootvp = vp;
-	mp->mnt_lower = nmp->ovm_vfs;
 	VOP_UNLOCK(vp);
 
 	error = set_statvfs_info(path, UIO_USERSPACE, args->la.target,
 	    UIO_USERSPACE, mp->mnt_op->vfs_name, mp, l);
+	if (error)
+		return error;
+	mp->mnt_lower = nmp->ovm_vfs;
 #ifdef OVERLAYFS_DIAGNOSTIC
 	printf("ov_mount: lower %s, alias at %s\n",
 	    mp->mnt_stat.f_mntfromname, mp->mnt_stat.f_mntonname);
 #endif
-	return error;
+	return 0;
 }
 
 /*
