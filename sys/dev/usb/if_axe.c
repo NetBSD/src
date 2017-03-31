a8dc14e31270487677a57dd3abd3b6a8549b5903
skrll <skrll@NetBSD.org>
2017-03-31 10:28:21 +0000

    Fix build

--- sys/dev/usb/if_axe.c
+++ sys/dev/usb/if_axe.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: if_axe.c,v 1.67.8.2 2017/01/26 21:54:24 skrll Exp $	*/
+/*	$NetBSD: if_axe.c,v 1.67.8.3 2017/03/31 10:28:21 skrll Exp $	*/
 /*	$OpenBSD: if_axe.c,v 1.137 2016/04/13 11:03:37 mpi Exp $ */
 
 /*
@@ -87,7 +87,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: if_axe.c,v 1.67.8.2 2017/01/26 21:54:24 skrll Exp $");
+__KERNEL_RCSID(0, "$NetBSD: if_axe.c,v 1.67.8.3 2017/03/31 10:28:21 skrll Exp $");
 
 #ifdef _KERNEL_OPT
 #include "opt_inet.h"
@@ -278,6 +278,8 @@ static void	axe_unlock_mii(struct axe_softc *);
 
 static void	axe_ax88178_init(struct axe_softc *);
 static void	axe_ax88772_init(struct axe_softc *);
+static void	axe_ax88772a_init(struct axe_softc *);
+static void	axe_ax88772b_init(struct axe_softc *);
 
 /* Get exclusive access to the MII registers */
 static void
