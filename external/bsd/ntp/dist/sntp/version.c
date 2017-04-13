44222cd2278269102af49468a7d500228c81e56d
christos <christos@NetBSD.org>
2017-04-13 19:17:15 +0000

    Import ntp 4.2.8p10

--- external/bsd/ntp/dist/sntp/version.c
+++ external/bsd/ntp/dist/sntp/version.c
@@ -1,7 +1,7 @@
-/*	$NetBSD: version.c,v 1.1.1.8 2016/11/22 01:35:09 christos Exp $	*/
+/*	$NetBSD: version.c,v 1.1.1.9 2017/04/13 19:17:34 christos Exp $	*/
 
 /*
  * version file for sntp
  */
 #include <config.h>
-const char * Version = "sntp 4.2.8p9@1.3265-o Mon Nov 21 12:58:21 UTC 2016 (31)";
+const char * Version = "sntp 4.2.8p10-beta@1.3728-o Tue Mar 21 14:36:42 UTC 2017 (43)";
