0674fb49158b481371866f7fdf5a19c38cff85de
joerg <joerg@NetBSD.org>
2017-04-20 13:18:23 +0000

    Merge pkg_install-20170419.

--- external/bsd/pkg_install/dist/create/pl.c
+++ external/bsd/pkg_install/dist/create/pl.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: pl.c,v 1.1.1.5 2017/04/20 13:12:47 joerg Exp $	*/
+/*	$NetBSD: pl.c,v 1.2 2017/04/20 13:18:23 joerg Exp $	*/
 
 #if HAVE_CONFIG_H
 #include "config.h"
@@ -7,7 +7,7 @@
 #if HAVE_SYS_CDEFS_H
 #include <sys/cdefs.h>
 #endif
-__RCSID("$NetBSD: pl.c,v 1.1.1.5 2017/04/20 13:12:47 joerg Exp $");
+__RCSID("$NetBSD: pl.c,v 1.2 2017/04/20 13:18:23 joerg Exp $");
 
 /*
  * FreeBSD install - a package for the installation and maintainance
--- external/bsd/pkg_install/dist/lib/gpgsig.c
+++ external/bsd/pkg_install/dist/lib/gpgsig.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: gpgsig.c,v 1.1.1.3 2017/04/20 13:12:48 joerg Exp $	*/
+/*	$NetBSD: gpgsig.c,v 1.2 2017/04/20 13:18:23 joerg Exp $	*/
 #if HAVE_CONFIG_H
 #include "config.h"
 #endif
@@ -7,7 +7,7 @@
 #include <sys/cdefs.h>
 #endif
 
-__RCSID("$NetBSD: gpgsig.c,v 1.1.1.3 2017/04/20 13:12:48 joerg Exp $");
+__RCSID("$NetBSD: gpgsig.c,v 1.2 2017/04/20 13:18:23 joerg Exp $");
 
 /*-
  * Copyright (c) 2008 Joerg Sonnenberger <joerg@NetBSD.org>.
