3e442e12346dbd930ac4a884fd765be6cd3a63da
kamil <kamil@NetBSD.org>
2017-05-03 15:53:31 +0000

    Fix kernel panic triggered with LLDB
    PT_SETSTEP and PT_CLEARSTEP in the current design must unlock proc_lock and
    t->p_lock. These functions use lwp_delref() for a tracee with more than one
    LWP. This function internally lock (t->)p_lock and this is lock against
    self.
    
    There are coming new ATF test with PT_*STEP with multiple LWPs to catch
    these bugs in future changes.
    
    Sponsored by <The NetBSD Foundation>

--- sys/kern/sys_ptrace_common.c
+++ sys/kern/sys_ptrace_common.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: sys_ptrace_common.c,v 1.21 2017/04/08 00:25:50 kamil Exp $	*/
+/*	$NetBSD: sys_ptrace_common.c,v 1.22 2017/05/03 15:53:31 kamil Exp $	*/
 
 /*-
  * Copyright (c) 2008, 2009 The NetBSD Foundation, Inc.
@@ -118,7 +118,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: sys_ptrace_common.c,v 1.21 2017/04/08 00:25:50 kamil Exp $");
+__KERNEL_RCSID(0, "$NetBSD: sys_ptrace_common.c,v 1.22 2017/05/03 15:53:31 kamil Exp $");
 
 #ifdef _KERNEL_OPT
 #include "opt_ptrace.h"
@@ -536,8 +536,6 @@ do_ptrace(struct ptrace_methods *ptm, struct lwp *l, int req, pid_t pid,
 	switch (req) {
 #ifdef PT_STEP
 	case PT_STEP:
-	case PT_SETSTEP:
-	case PT_CLEARSTEP:
 #endif
 	case PT_CONTINUE:
 	case PT_DETACH:
