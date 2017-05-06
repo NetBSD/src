4cb4af11b1521f1626a9baaf40ae4f58e0929844
christos <christos@NetBSD.org>
2017-05-06 19:50:09 +0000

    merge conflicts

--- external/bsd/openpam/dist/include/security/openpam_attr.h
+++ external/bsd/openpam/dist/include/security/openpam_attr.h
@@ -1,7 +1,7 @@
-/*	$NetBSD: openpam_attr.h,v 1.5 2014/10/24 18:17:56 christos Exp $	*/
+/*	$NetBSD: openpam_attr.h,v 1.6 2017/05/06 19:50:09 christos Exp $	*/
 
 /*
- * Id: openpam_attr.h 656 2013-03-06 22:58:45Z des 
+ * $OpenPAM: openpam_attr.h 938 2017-04-30 21:34:42Z des $
  */
 
 #ifndef SECURITY_OPENPAM_ATTR_H_INCLUDED
--- external/bsd/openpam/dist/lib/libpam/openpam_features.h
+++ external/bsd/openpam/dist/lib/libpam/openpam_features.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: openpam_features.h,v 1.2 2014/10/24 18:17:56 christos Exp $	*/
+/*	$NetBSD: openpam_features.h,v 1.3 2017/05/06 19:50:09 christos Exp $	*/
 
 /*-
  * Copyright (c) 2012 Dag-Erling Smørgrav
@@ -28,7 +28,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- * Id: openpam_features.h 648 2013-03-05 17:54:27Z des 
+ * $OpenPAM: openpam_features.h 938 2017-04-30 21:34:42Z des $
  */
 
 #ifndef OPENPAM_FEATURES_H_INCLUDED
