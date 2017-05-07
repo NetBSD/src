48a644d8851ead99d6ed722756d52b48c94df31a
martin <martin@NetBSD.org>
2017-05-07 11:29:21 +0000

    Limit some rintl usage to #ifdef __HAVE_LONG_DOUBLE. XXX should probably declare some aliases in the #else case

--- lib/libm/src/s_llrintl.c
+++ lib/libm/src/s_llrintl.c
@@ -2,12 +2,15 @@
 #ifdef __FreeBSD__
 __FBSDID("$FreeBSD: head/lib/msun/src/s_llrintl.c 175309 2008-01-14 02:12:07Z das $");
 #else
-__RCSID("$NetBSD: s_llrintl.c,v 1.1 2017/05/06 18:03:24 christos Exp $");
+__RCSID("$NetBSD: s_llrintl.c,v 1.2 2017/05/07 11:29:21 martin Exp $");
 #endif
 
+#ifdef __HAVE_LONG_DOUBLE
 #define stype		long double
 #define	roundit		rintl
 #define dtype		long long
 #define	fn		llrintl
 
 #include "s_lrint.c"
+#endif
+
--- lib/libm/src/s_lrintl.c
+++ lib/libm/src/s_lrintl.c
@@ -2,12 +2,14 @@
 #ifdef __FreeBSD__
 __FBSDID("$FreeBSD: head/lib/msun/src/s_lrintl.c 175309 2008-01-14 02:12:07Z das $");
 #else
-__RCSID("$NetBSD: s_lrintl.c,v 1.1 2017/05/06 18:03:24 christos Exp $");
+__RCSID("$NetBSD: s_lrintl.c,v 1.2 2017/05/07 11:29:21 martin Exp $");
 #endif
 
+#ifdef __HAVE_LONG_DOUBLE
 #define stype		long double
 #define	roundit		rintl
 #define dtype		long
 #define	fn		lrintl
 
 #include "s_lrint.c"
+#endif
