5ff34ec0495bf17fed74303caed71d5385751b7a
msaitoh <msaitoh@NetBSD.org>
2017-05-10 02:46:33 +0000

    Use pci_intr_establish_xname().

--- sys/dev/pci/pccbb.c
+++ sys/dev/pci/pccbb.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: pccbb.c,v 1.210 2016/07/07 06:55:41 msaitoh Exp $	*/
+/*	$NetBSD: pccbb.c,v 1.211 2017/05/10 02:46:33 msaitoh Exp $	*/
 
 /*
  * Copyright (c) 1998, 1999 and 2000
@@ -26,7 +26,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: pccbb.c,v 1.210 2016/07/07 06:55:41 msaitoh Exp $");
+__KERNEL_RCSID(0, "$NetBSD: pccbb.c,v 1.211 2017/05/10 02:46:33 msaitoh Exp $");
 
 /*
 #define CBB_DEBUG
@@ -931,7 +931,8 @@ pccbb_intrinit(struct pccbb_softc *sc)
 	 * than any other hard interrupts.
 	 */
 	KASSERT(sc->sc_ih == NULL);
-	sc->sc_ih = pci_intr_establish(pc, ih, IPL_BIO, pccbbintr, sc);
+	sc->sc_ih = pci_intr_establish_xname(pc, ih, IPL_BIO, pccbbintr, sc,
+	    device_xname(sc->sc_dev));
 
 	if (sc->sc_ih == NULL) {
 		aprint_error_dev(sc->sc_dev, "couldn't establish interrupt");
