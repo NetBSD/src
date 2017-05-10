6635c21d6c189cdd97ba1af9ced78d5fd1c26883
ozaki-r <ozaki-r@NetBSD.org>
2017-05-10 09:34:51 +0000

    Stop ipsec4_output returning SP to the caller
    SP isn't used by the caller (ip_output) and also holding its
    reference looks unnecessary.

--- sys/netipsec/ipsec.h
+++ sys/netipsec/ipsec.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: ipsec.h,v 1.45 2017/05/08 06:39:23 ozaki-r Exp $	*/
+/*	$NetBSD: ipsec.h,v 1.46 2017/05/10 09:34:52 ozaki-r Exp $	*/
 /*	$FreeBSD: /usr/local/www/cvsroot/FreeBSD/src/sys/netipsec/ipsec.h,v 1.2.4.2 2004/02/14 22:23:23 bms Exp $	*/
 /*	$KAME: ipsec.h,v 1.53 2001/11/20 08:32:38 itojun Exp $	*/
 
@@ -255,7 +255,7 @@ struct secpolicy *ipsec4_checkpolicy (struct mbuf *, u_int, u_int,
 struct secpolicy * ipsec_getpolicybyaddr(struct mbuf *, u_int,
 	int, int *);
 int ipsec4_output(struct mbuf *, struct inpcb *, int,
-	struct secpolicy **, u_long *, bool *, bool *);
+	u_long *, bool *, bool *);
 int ipsec4_input(struct mbuf *, int);
 int ipsec4_forward(struct mbuf *, int *);
 #ifdef INET6
