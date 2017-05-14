4088a0f51b164ef39ca49b1ce23ad50b2e4de8c5
kre <kre@NetBSD.org>
2017-05-14 11:23:33 +0000

    Make ${NETBSD_SHELL}  value include (a human recognisable form of) MKREPRO_TIMESTAMP (as an additional word in the value, with a "BUILD:" prefix) if it is set during the build.   (Trailing 00 pairs in the time are removed).
    While here, throw in some extra words that list the compilation
    options used which alter sh behaviour (mostly by removing stuff.)
    Usually that will only be noticed in a SMALL shell compiled for
    install media, or similar - none of the others (not that there
    are many) are ever changed from the default in a normal build
    (default settings are just omitted.)   This also allows scripts
    to tell if they are running in a DEBUG shell, which can sometimes
    make debugging easier.

--- bin/sh/var.c
+++ bin/sh/var.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: var.c,v 1.52 2017/05/10 06:18:43 kre Exp $	*/
+/*	$NetBSD: var.c,v 1.53 2017/05/14 11:23:33 kre Exp $	*/
 
 /*-
  * Copyright (c) 1991, 1993
@@ -37,7 +37,7 @@
 #if 0
 static char sccsid[] = "@(#)var.c	8.3 (Berkeley) 5/4/95";
 #else
-__RCSID("$NetBSD: var.c,v 1.52 2017/05/10 06:18:43 kre Exp $");
+__RCSID("$NetBSD: var.c,v 1.53 2017/05/14 11:23:33 kre Exp $");
 #endif
 #endif /* not lint */
 
@@ -162,14 +162,45 @@ INIT {
 	}
 
 	/*
+	 * Set variables which override anything read from environment.
+	 *
 	 * PPID is readonly
-	 *	set after processing environ to override anything there
-	 * Always default IFS, ignore any value from environment.
+	 * Always default IFS
+	 * NETBSD_SHELL is a constant (readonly), and is never exported
 	 */
 	snprintf(buf, sizeof(buf), "%d", (int)getppid());
 	setvar("PPID", buf, VREADONLY);
 	setvar("IFS", ifs_default, VTEXTFIXED);
-	setvar("NETBSD_SHELL", NETBSD_SHELL, VTEXTFIXED|VREADONLY|VNOEXPORT);
+
+	setvar("NETBSD_SHELL", NETBSD_SHELL
+#ifdef BUILD_DATE
+		" BUILD:" BUILD_DATE
+#endif
+#ifdef DEBUG
+		" DEBUG"
+#endif
+#if !defined(JOBS) || JOBS == 0
+		" -JOBS"
+#endif
+#ifndef DO_SHAREDVFORK
+		" -VFORK"
+#endif
+#ifdef SMALL
+		" SMALL"
+#endif
+#ifdef TINY
+		" TINY"
+#endif
+#ifdef OLD_TTY_DRIVER
+		" OLD_TTY"
+#endif
+#ifdef SYSV
+		" SYSV"
+#endif
+#ifndef BSD
+		" -BSD"
+#endif
+		    , VTEXTFIXED|VREADONLY|VNOEXPORT);
 }
 #endif
 
