3f8c47180ddc6f2ddfa8c8809fffe275078ca8c2
christos <christos@NetBSD.org>
2017-05-19 14:42:00 +0000

    mention the man page instead of the command.

--- sys/uvm/uvm_map.c
+++ sys/uvm/uvm_map.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: uvm_map.c,v 1.345 2017/05/19 14:38:46 christos Exp $	*/
+/*	$NetBSD: uvm_map.c,v 1.346 2017/05/19 14:42:00 christos Exp $	*/
 
 /*
  * Copyright (c) 1997 Charles D. Cranor and Washington University.
@@ -66,7 +66,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: uvm_map.c,v 1.345 2017/05/19 14:38:46 christos Exp $");
+__KERNEL_RCSID(0, "$NetBSD: uvm_map.c,v 1.346 2017/05/19 14:42:00 christos Exp $");
 
 #include "opt_ddb.h"
 #include "opt_pax.h"
@@ -1067,9 +1067,8 @@ uvm_map(struct vm_map *map, vaddr_t *startp /* IN/OUT */, vsize_t size,
 #ifndef __USER_VA0_IS_SAFE
 	if ((flags & UVM_FLAG_FIXED) && *startp == 0 &&
 	    !VM_MAP_IS_KERNEL(map) && user_va0_disable) {
-		uprintf("%s: process wants to map page 0; "
-		    "run 'sysctl -w vm.user_va0_disable=0' to enable access.\n",
-		    __func__);
+		uprintf("%s: process wants to map virtual address 0; see "
+		    "vm.user_va0_disable in sysctl(7).\n" __func__);
 		return EACCES;
 	}
 #endif
