6a6027692662ba623e7bf5274322989a7b5d1440
ragge <ragge@NetBSD.org>
2017-05-22 16:53:05 +0000

    Update copyright notice for Ludd (remove clause 3 & 4).

--- sys/arch/vax/vax/locore.c
+++ sys/arch/vax/vax/locore.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: locore.c,v 1.83 2015/08/27 17:18:58 jklos Exp $	*/
+/*	$NetBSD: locore.c,v 1.84 2017/05/22 16:53:05 ragge Exp $	*/
 /*
  * Copyright (c) 1994, 1998 Ludd, University of Lule}, Sweden.
  * All rights reserved.
@@ -11,11 +11,6 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *     This product includes software developed at Ludd, University of Lule}.
- * 4. The name of the author may not be used to endorse or promote products
- *    derived from this software without specific prior written permission
  *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
@@ -32,7 +27,7 @@
  /* All bugs are subject to removal without further notice */
 		
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: locore.c,v 1.83 2015/08/27 17:18:58 jklos Exp $");
+__KERNEL_RCSID(0, "$NetBSD: locore.c,v 1.84 2017/05/22 16:53:05 ragge Exp $");
 
 #include "opt_compat_netbsd.h"
 
