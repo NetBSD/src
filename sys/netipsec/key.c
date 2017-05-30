dd7f5ab85a8781bb87add31bd62055eeb54e13bf
ozaki-r <ozaki-r@NetBSD.org>
2017-05-30 09:39:08 +0000

    Send up an error message on error as well as others

--- sys/netipsec/key.c
+++ sys/netipsec/key.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: key.c,v 1.148 2017/05/30 01:31:07 ozaki-r Exp $	*/
+/*	$NetBSD: key.c,v 1.149 2017/05/30 09:39:08 ozaki-r Exp $	*/
 /*	$FreeBSD: src/sys/netipsec/key.c,v 1.3.2.3 2004/02/14 22:23:23 bms Exp $	*/
 /*	$KAME: key.c,v 1.191 2001/06/27 10:46:49 sakane Exp $	*/
 
@@ -32,7 +32,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: key.c,v 1.148 2017/05/30 01:31:07 ozaki-r Exp $");
+__KERNEL_RCSID(0, "$NetBSD: key.c,v 1.149 2017/05/30 09:39:08 ozaki-r Exp $");
 
 /*
  * This code is referd to RFC 2367
@@ -1910,8 +1910,7 @@ key_spdadd(struct socket *so, struct mbuf *m,
 		break;
 	default:
 		IPSECLOG(LOG_DEBUG, "Invalid SP direction.\n");
-		mhp->msg->sadb_msg_errno = EINVAL;
-		return 0;
+		return key_senderror(so, m, EINVAL);
 	}
 
 	/* check policy */
