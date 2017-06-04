76f53842671088b5d0ebb4d1d53543f2ab7d07bb
snj <snj@NetBSD.org>
2017-06-04 07:14:56 +0000

    8.99.1

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.542 2017/05/28 16:39:42 hannken Exp $	*/
+/*	$NetBSD: param.h,v 1.543 2017/06/04 07:14:56 snj Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -67,7 +67,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	799007500	/* NetBSD 7.99.75 */
+#define	__NetBSD_Version__	899000100	/* NetBSD 8.99.1 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
