779897062ecbd8baaada9c2ad5103735cc737454
nat <nat@NetBSD.org>
2017-06-06 07:32:41 +0000

    Style change.

--- sys/dev/pad/pad.c
+++ sys/dev/pad/pad.c
@@ -1,4 +1,4 @@
-/* $NetBSD: pad.c,v 1.35 2017/06/06 07:29:35 nat Exp $ */
+/* $NetBSD: pad.c,v 1.36 2017/06/06 07:32:41 nat Exp $ */
 
 /*-
  * Copyright (c) 2007 Jared D. McNeill <jmcneill@invisible.ca>
@@ -27,7 +27,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: pad.c,v 1.35 2017/06/06 07:29:35 nat Exp $");
+__KERNEL_RCSID(0, "$NetBSD: pad.c,v 1.36 2017/06/06 07:32:41 nat Exp $");
 
 #include <sys/types.h>
 #include <sys/param.h>
@@ -318,9 +318,8 @@ pad_open(dev_t dev, int flags, int fmt, struct lwp *l)
 	if (sc == NULL)
 		return ENXIO;
 
-	if (atomic_swap_uint(&sc->sc_open, 1) != 0) {
+	if (atomic_swap_uint(&sc->sc_open, 1) != 0)
 		return EBUSY;
-	}
 	
 	return 0;
 }
