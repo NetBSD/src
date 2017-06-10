4f4a2011d5c1e0cb2c2f89d3db7ede6616dc8332
snj <snj@NetBSD.org>
2017-06-10 06:18:52 +0000

    Pull up following revision(s) (requested by skrll in ticket #22): 	sys/arch/mips/mips/mips_machdep.c: revision 1.278 	sys/arch/mips/mips/pmap_machdep.c: revision 1.21 Always use XKPHYS for pool pages on _LP64; otherwise use KSEG0 -- Maintain the split of physical memory into the defined freelists, but only force pool pages to VM_FREELIST_FIRST512M for non _LP64

--- sys/arch/mips/mips/pmap_machdep.c
+++ sys/arch/mips/mips/pmap_machdep.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: pmap_machdep.c,v 1.19.2.1 2017/06/06 09:25:49 martin Exp $	*/
+/*	$NetBSD: pmap_machdep.c,v 1.19.2.2 2017/06/10 06:18:52 snj Exp $	*/
 
 /*-
  * Copyright (c) 1998, 2001 The NetBSD Foundation, Inc.
@@ -67,7 +67,7 @@
 
 #include <sys/cdefs.h>
 
-__KERNEL_RCSID(0, "$NetBSD: pmap_machdep.c,v 1.19.2.1 2017/06/06 09:25:49 martin Exp $");
+__KERNEL_RCSID(0, "$NetBSD: pmap_machdep.c,v 1.19.2.2 2017/06/10 06:18:52 snj Exp $");
 
 /*
  *	Manages physical address maps.
@@ -1065,12 +1065,10 @@ vaddr_t
 pmap_md_pool_phystov(paddr_t pa)
 {
 #ifdef _LP64
-	if ((pa & ~MIPS_PHYS_MASK) != 0) {
-		KASSERT(mips_options.mips3_xkphys_cached);
-		return MIPS_PHYS_TO_XKPHYS_CACHED(pa);
-	}
+	KASSERT(mips_options.mips3_xkphys_cached);
+	return MIPS_PHYS_TO_XKPHYS_CACHED(pa);
 #else
 	KASSERT((pa & ~MIPS_PHYS_MASK) == 0);
-#endif
 	return MIPS_PHYS_TO_KSEG0(pa);
+#endif
 }
