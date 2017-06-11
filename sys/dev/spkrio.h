0cab0c48bae9e11822b6a1661d2401ea956b2587
nat <nat@NetBSD.org>
2017-06-11 03:33:48 +0000

    Add ioctls for setting and getting the beep volume.  Currently only supported on spkr devices attached to audio.
    Ok pgoyette@.

--- sys/dev/spkrio.h
+++ sys/dev/spkrio.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: spkrio.h,v 1.2 2016/12/09 04:46:39 christos Exp $	*/
+/*	$NetBSD: spkrio.h,v 1.3 2017/06/11 03:33:48 nat Exp $	*/
 
 /*
  * spkrio.h -- interface definitions for speaker ioctl()
@@ -11,6 +11,8 @@
 
 #define SPKRTONE        _IOW('S', 1, tone_t)    /* emit tone */
 #define SPKRTUNE        _IO('S', 2)             /* emit tone sequence */
+#define SPKRGETVOL      _IOR('S', 3, u_int)     /* get volume */
+#define SPKRSETVOL      _IOW('S', 4, u_int)     /* set volume */
 
 typedef struct {
 	int	frequency;	/* in hertz */
