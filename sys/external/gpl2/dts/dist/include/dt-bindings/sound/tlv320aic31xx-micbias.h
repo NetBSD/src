f46c7ed43017d916ec9ce7c15af8870d65f60e15
jmcneill <jmcneill@NetBSD.org>
2017-06-15 20:14:23 +0000

    Import dts from Linux 4.11.5

---
+++ sys/external/gpl2/dts/dist/include/dt-bindings/sound/tlv320aic31xx-micbias.h
@@ -0,0 +1,10 @@
+/*	$NetBSD: tlv320aic31xx-micbias.h,v 1.1.1.1 2017/06/15 20:14:23 jmcneill Exp $	*/
+
+#ifndef __DT_TLV320AIC31XX_MICBIAS_H
+#define __DT_TLV320AIC31XX_MICBIAS_H
+
+#define MICBIAS_2_0V		1
+#define MICBIAS_2_5V		2
+#define MICBIAS_AVDDV		3
+
+#endif /* __DT_TLV320AIC31XX_MICBIAS_H */
