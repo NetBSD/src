2fad1798b489ea470792bb91f63ff18e08bc55ec
jmcneill <jmcneill@NetBSD.org>
2017-06-15 20:14:23 +0000

    Import dts from Linux 4.11.5

---
+++ sys/external/gpl2/dts/dist/include/dt-bindings/gpio/gpio.h
@@ -0,0 +1,29 @@
+/*	$NetBSD: gpio.h,v 1.1.1.1 2017/06/15 20:14:23 jmcneill Exp $	*/
+
+/*
+ * This header provides constants for most GPIO bindings.
+ *
+ * Most GPIO bindings include a flags cell as part of the GPIO specifier.
+ * In most cases, the format of the flags cell uses the standard values
+ * defined in this header.
+ */
+
+#ifndef _DT_BINDINGS_GPIO_GPIO_H
+#define _DT_BINDINGS_GPIO_GPIO_H
+
+/* Bit 0 express polarity */
+#define GPIO_ACTIVE_HIGH 0
+#define GPIO_ACTIVE_LOW 1
+
+/* Bit 1 express single-endedness */
+#define GPIO_PUSH_PULL 0
+#define GPIO_SINGLE_ENDED 2
+
+/*
+ * Open Drain/Collector is the combination of single-ended active low,
+ * Open Source/Emitter is the combination of single-ended active high.
+ */
+#define GPIO_OPEN_DRAIN (GPIO_SINGLE_ENDED | GPIO_ACTIVE_LOW)
+#define GPIO_OPEN_SOURCE (GPIO_SINGLE_ENDED | GPIO_ACTIVE_HIGH)
+
+#endif
---
+++ sys/external/gpl2/dts/dist/include/dt-bindings/leds/leds-ns2.h
@@ -0,0 +1,10 @@
+/*	$NetBSD: leds-ns2.h,v 1.1.1.1 2017/06/15 20:14:23 jmcneill Exp $	*/
+
+#ifndef _DT_BINDINGS_LEDS_NS2_H
+#define _DT_BINDINGS_LEDS_NS2_H
+
+#define NS_V2_LED_OFF	0
+#define NS_V2_LED_ON	1
+#define NS_V2_LED_SATA	2
+
+#endif
