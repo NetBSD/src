6029d4abf2297d9a62ec7eb4150bf1ba5138736d
jmcneill <jmcneill@NetBSD.org>
2017-06-15 20:14:23 +0000

    Import dts from Linux 4.11.5

---
+++ sys/external/gpl2/dts/dist/include/dt-bindings/interrupt-controller/mips-gic.h
@@ -0,0 +1,11 @@
+/*	$NetBSD: mips-gic.h,v 1.1.1.1 2017/06/15 20:14:23 jmcneill Exp $	*/
+
+#ifndef _DT_BINDINGS_INTERRUPT_CONTROLLER_MIPS_GIC_H
+#define _DT_BINDINGS_INTERRUPT_CONTROLLER_MIPS_GIC_H
+
+#include <dt-bindings/interrupt-controller/irq.h>
+
+#define GIC_SHARED 0
+#define GIC_LOCAL 1
+
+#endif
---
+++ sys/external/gpl2/dts/dist/include/dt-bindings/sound/samsung-i2s.h
@@ -0,0 +1,10 @@
+/*	$NetBSD: samsung-i2s.h,v 1.1.1.1 2017/06/15 20:14:23 jmcneill Exp $	*/
+
+#ifndef _DT_BINDINGS_SAMSUNG_I2S_H
+#define _DT_BINDINGS_SAMSUNG_I2S_H
+
+#define CLK_I2S_CDCLK		0
+#define CLK_I2S_RCLK_SRC	1
+#define CLK_I2S_RCLK_PSR	2
+
+#endif /* _DT_BINDINGS_SAMSUNG_I2S_H */
