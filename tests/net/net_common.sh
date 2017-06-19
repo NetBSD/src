b261536bb5cf2bacae9bcf2d9a9101d317be4782
ozaki-r <ozaki-r@NetBSD.org>
2017-06-19 09:20:29 +0000

    Do netstat -a for an appropriate protocol

--- tests/net/net_common.sh
+++ tests/net/net_common.sh
@@ -1,4 +1,4 @@
-#	$NetBSD: net_common.sh,v 1.18 2017/06/02 01:18:51 ozaki-r Exp $
+#	$NetBSD: net_common.sh,v 1.19 2017/06/19 09:20:29 ozaki-r Exp $
 #
 # Copyright (c) 2016 Internet Initiative Japan Inc.
 # All rights reserved.
@@ -157,7 +157,11 @@ start_nc_server()
 	pid=$!
 	echo $pid > $NC_PID
 
-	$DEBUG && rump.netstat -a -f inet
+	if [ $proto = ipv4 ]; then
+		$DEBUG && rump.netstat -a -f inet
+	else
+		$DEBUG && rump.netstat -a -f inet6
+	fi
 
 	export RUMP_SERVER=$backup
 
