114ae9c1461b521f64cba9f4646046d3e851c984
joerg <joerg@NetBSD.org>
2017-06-24 13:43:36 +0000

    Update VM_DEFAULT_ADDRESS32_TOPDOWN to include guard area.

--- sys/arch/mips/include/vmparam.h
+++ sys/arch/mips/include/vmparam.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: vmparam.h,v 1.58 2017/06/23 21:28:38 joerg Exp $	*/
+/*	$NetBSD: vmparam.h,v 1.59 2017/06/24 13:43:36 joerg Exp $	*/
 
 /*
  * Copyright (c) 1988 University of Utah.
@@ -190,7 +190,7 @@
     round_page((vaddr_t)(da) + (vsize_t)maxdmap)
 
 #define VM_DEFAULT_ADDRESS32_TOPDOWN(da, sz) \
-    trunc_page(USRSTACK32 - MAXSSIZ32 - (sz))
+    trunc_page(USRSTACK32 - MAXSSIZ32 - (sz) - user_stack_guard_size)
 #define VM_DEFAULT_ADDRESS32_BOTTOMUP(da, sz) \
     round_page((vaddr_t)(da) + (vsize_t)MAXDSIZ32)
 
