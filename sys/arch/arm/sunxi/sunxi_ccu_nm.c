f70a991053a766eb1563b9503dfff879626fd05a
jmcneill <jmcneill@NetBSD.org>
2017-06-29 09:26:06 +0000

    Add H3 MMC support

--- sys/arch/arm/sunxi/sunxi_ccu_nm.c
+++ sys/arch/arm/sunxi/sunxi_ccu_nm.c
@@ -1,4 +1,4 @@
-/* $NetBSD: sunxi_ccu_nm.c,v 1.1 2017/06/28 23:51:29 jmcneill Exp $ */
+/* $NetBSD: sunxi_ccu_nm.c,v 1.2 2017/06/29 09:26:06 jmcneill Exp $ */
 
 /*-
  * Copyright (c) 2017 Jared McNeill <jmcneill@invisible.ca>
@@ -27,7 +27,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: sunxi_ccu_nm.c,v 1.1 2017/06/28 23:51:29 jmcneill Exp $");
+__KERNEL_RCSID(0, "$NetBSD: sunxi_ccu_nm.c,v 1.2 2017/06/29 09:26:06 jmcneill Exp $");
 
 #include <sys/param.h>
 #include <sys/bus.h>
@@ -35,6 +35,28 @@ __KERNEL_RCSID(0, "$NetBSD: sunxi_ccu_nm.c,v 1.1 2017/06/28 23:51:29 jmcneill Ex
 #include <dev/clk/clk_backend.h>
 
 #include <arm/sunxi/sunxi_ccu.h>
+
+int
+sunxi_ccu_nm_enable(struct sunxi_ccu_softc *sc, struct sunxi_ccu_clk *clk,
+    int enable)
+{
+	struct sunxi_ccu_nm *nm = &clk->u.nm;
+	uint32_t val;
+
+	KASSERT(clk->type == SUNXI_CCU_NM);
+
+	if (!nm->enable)
+		return enable ? 0 : EINVAL;
+
+	val = CCU_READ(sc, nm->reg);
+	if (enable)
+		val |= nm->enable;
+	else
+		val &= ~nm->enable;
+	CCU_WRITE(sc, nm->reg, val);
+
+	return 0;
+}
 
 u_int
 sunxi_ccu_nm_get_rate(struct sunxi_ccu_softc *sc,
@@ -60,8 +82,11 @@ sunxi_ccu_nm_get_rate(struct sunxi_ccu_softc *sc,
 	n = __SHIFTOUT(val, nm->n);
 	m = __SHIFTOUT(val, nm->m);
 
+	if (nm->enable && !(val & nm->enable))
+		return 0;
+
 	if (nm->flags & SUNXI_CCU_NM_POWER_OF_TWO)
-		n <<= 1;
+		n = 1 << n;
 	else
 		n++;
 
@@ -72,9 +97,86 @@ sunxi_ccu_nm_get_rate(struct sunxi_ccu_softc *sc,
 
 int
 sunxi_ccu_nm_set_rate(struct sunxi_ccu_softc *sc,
-    struct sunxi_ccu_clk *clk, u_int rate)
+    struct sunxi_ccu_clk *clk, u_int new_rate)
 {
-	return EIO;
+	struct sunxi_ccu_nm *nm = &clk->u.nm;
+	struct clk *clkp, *clkp_parent;
+	u_int parent_rate, best_rate, best_n, best_m, best_parent;
+	u_int n, m, pindex, rate;
+	int best_diff;
+	uint32_t val;
+
+	const u_int n_max = __SHIFTOUT(nm->n, nm->n);
+	const u_int m_max = __SHIFTOUT(nm->m, nm->m);
+
+	clkp = &clk->base;
+	clkp_parent = clk_get_parent(clkp);
+	if (clkp_parent == NULL)
+		return 0;
+
+	rate = clk_get_rate(clkp_parent);
+	if (rate == 0)
+		return 0;
+
+	best_rate = 0;
+	best_diff = INT_MAX;
+	for (pindex = 0; pindex < nm->nparents; pindex++) {
+		/* XXX
+		 * Shouldn't have to set parent to get potential parent clock rate
+		 */
+		val = CCU_READ(sc, nm->reg);
+		val &= ~nm->sel;
+		val |= __SHIFTIN(pindex, nm->sel);
+		CCU_WRITE(sc, nm->reg, val);
+
+		clkp_parent = clk_get_parent(clkp);
+		if (clkp_parent == NULL)
+			continue;
+		parent_rate = clk_get_rate(clkp_parent);
+		if (parent_rate == 0)
+			continue;
+
+		for (n = 0; n <= n_max; n++) {
+			for (m = 0; m <= m_max; m++) {
+				if (nm->flags & SUNXI_CCU_NM_POWER_OF_TWO)
+					rate = parent_rate / (1 << n) / (m + 1);
+				else
+					rate = parent_rate / (n + 1) / (m + 1);
+
+				if (nm->flags & SUNXI_CCU_NM_ROUND_DOWN) {
+					const int diff = new_rate - rate;
+					if (diff >= 0 && rate > best_rate) {
+						best_rate = rate;
+						best_n = n;
+						best_m = m;
+						best_parent = pindex;
+					}
+				} else {
+					const int diff = abs(new_rate - rate);
+					if (diff < best_diff) {
+						best_rate = rate;
+						best_n = n;
+						best_m = m;
+						best_parent = pindex;
+					}
+				}
+			}
+		}
+	}
+
+	if (best_rate == 0)
+		return ERANGE;
+
+	val = CCU_READ(sc, nm->reg);
+	val &= ~nm->sel;
+	val |= __SHIFTIN(best_parent, nm->sel);
+	val &= ~nm->n;
+	val |= __SHIFTIN(best_n, nm->n);
+	val &= ~nm->m;
+	val |= __SHIFTIN(best_m, nm->m);
+	CCU_WRITE(sc, nm->reg, val);
+
+	return 0;
 }
 
 int
