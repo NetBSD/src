2d46091798e0a2fbd488598981086ad13d4edf12
ozaki-r <ozaki-r@NetBSD.org>
2017-07-04 08:11:32 +0000

    Introduce and use SADB_SASTATE_USABLE_P

--- sys/netipsec/key.c
+++ sys/netipsec/key.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: key.c,v 1.164 2017/06/10 14:21:01 ozaki-r Exp $	*/
+/*	$NetBSD: key.c,v 1.165 2017/07/04 08:11:32 ozaki-r Exp $	*/
 /*	$FreeBSD: src/sys/netipsec/key.c,v 1.3.2.3 2004/02/14 22:23:23 bms Exp $	*/
 /*	$KAME: key.c,v 1.191 2001/06/27 10:46:49 sakane Exp $	*/
 
@@ -32,7 +32,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: key.c,v 1.164 2017/06/10 14:21:01 ozaki-r Exp $");
+__KERNEL_RCSID(0, "$NetBSD: key.c,v 1.165 2017/07/04 08:11:32 ozaki-r Exp $");
 
 /*
  * This code is referd to RFC 2367
@@ -1191,8 +1191,7 @@ key_allocsa(
 				/* sanity check */
 				KEY_CHKSASTATE(sav->state, state);
 				/* do not return entries w/ unusable state */
-				if (sav->state != SADB_SASTATE_MATURE &&
-				    sav->state != SADB_SASTATE_DYING) {
+				if (!SADB_SASTATE_USABLE_P(sav)) {
 					KEYDEBUG_PRINTF(KEYDEBUG_MATCH,
 					    "bad state %d\n", sav->state);
 					continue;
