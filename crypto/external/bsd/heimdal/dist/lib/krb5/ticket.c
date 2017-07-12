2b49f51a398d4e3445a9907a62258445c642f988
martin <martin@NetBSD.org>
2017-07-12 13:51:05 +0000

    Pull up following revision(s) (requested by christos in ticket #123): 	crypto/external/bsd/heimdal/dist/lib/krb5/ticket.c: revision 1.3 	crypto/external/bsd/heimdal/dist/lib/krb5/ticket.c: revision 1.4 <a  rel="nofollow" href="https://orpheus-lyre.info/design/index.html">https://orpheus-lyre.info/design/index.html</a> <a  rel="nofollow" href="https://github.com/heimdal/heimdal/commit/6dd3eb836bbb80a00ffced4ad57077a1cdf227ea">https://github.com/heimdal/heimdal/commit/6dd3eb836bbb80a00ffced4ad57077a1cdf227ea</a> In _krb5_extract_ticket() the KDC-REP service name must be obtained from encrypted version stored in 'enc_part' instead of the unencrypted version stored in 'ticket'.  Use of the unecrypted version provides an opportunity for successful server impersonation and other attacks. Identified by Jeffrey Altman, Viktor Duchovni and Nico Williams. XXX: pullup 6, 7, 8. fix typo.

--- crypto/external/bsd/heimdal/dist/lib/krb5/ticket.c
+++ crypto/external/bsd/heimdal/dist/lib/krb5/ticket.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: ticket.c,v 1.2 2017/01/28 21:31:49 christos Exp $	*/
+/*	$NetBSD: ticket.c,v 1.2.4.1 2017/07/12 13:51:05 martin Exp $	*/
 
 /*
  * Copyright (c) 1997 - 2001 Kungliga Tekniska Högskolan
@@ -707,8 +707,8 @@ _krb5_extract_ticket(krb5_context context,
     /* check server referral and save principal */
     ret = _krb5_principalname2krb5_principal (context,
 					      &tmp_principal,
-					      rep->kdc_rep.ticket.sname,
-					      rep->kdc_rep.ticket.realm);
+					      rep->enc_part.sname,
+					      rep->enc_part.srealm);
     if (ret)
 	goto out;
     if((flags & EXTRACT_TICKET_ALLOW_SERVER_MISMATCH) == 0){
