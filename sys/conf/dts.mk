b9a526367c91d622eab98756d71840c5d302b354
christos <christos@NetBSD.org>
2017-07-15 15:53:48 +0000

    do dts depend processing in two passes, one for /include/ and one for #include

--- sys/conf/dts.mk
+++ sys/conf/dts.mk
@@ -1,4 +1,4 @@
-# $NetBSD: dts.mk,v 1.1 2017/07/15 15:20:52 christos Exp $
+# $NetBSD: dts.mk,v 1.2 2017/07/15 15:53:48 christos Exp $
 
 DTSINC?=$S/external/gpl2/dts/dist/include
 DTSGNUPATH?=$S/external/gpl2/dts/dist/arch/${MACHINE_CPU}/boot/dts
@@ -8,9 +8,17 @@ DTSPADDING?=1024
 .SUFFIXES: .dtd .dtb .dts
 
 .dts.dtd:
+	(${CPP} -P -xassembler-with-cpp -I ${DTSINC} -I ${DTSPATH} \
+	    -I ${DTSGNUPATH} -include ${.IMPSRC} /dev/null | \
 	${TOOL_DTC} -i ${DTSINC} -i ${DTSPATH} -i ${DTSGNUPATH} -I dts -O dtb \
-	    -p ${DTSPADDING} -b 0 -o /dev/null -d /dev/stdout ${.IMPSRC} | \
-	${TOOL_SED} -e 's@/dev/null@${.TARGET:.dtd=.dtb}@' > ${.TARGET}
+	    -p ${DTSPADDING} -b 0 -o /dev/null -d /dev/stdout | \
+	${TOOL_SED} -e 's@/dev/null@${.TARGET:.dtd=.dtb}@' \
+	    -e 's@<stdin>@${.IMPSRC}@' && \
+	${CPP} -P -xassembler-with-cpp -I ${DTSINC} -I ${DTSPATH} \
+	    -I ${DTSGNUPATH} -include ${.IMPSRC} -M /dev/null | \
+	${TOOL_SED} -e 's@null.o@${.TARGET:.dtd=.dtb}@' \
+	    -e 's@/dev/null@@') > ${.TARGET}
+
 
 .dts.dtb:
 	${CPP} -P -xassembler-with-cpp -I ${DTSINC} -I ${DTSPATH} \
