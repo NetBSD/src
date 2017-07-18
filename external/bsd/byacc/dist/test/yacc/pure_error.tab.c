4acf61d90fdd05e1f8212a05058708c772e2b57b
snj <snj@NetBSD.org>
2017-07-18 18:09:42 +0000

    Pull up following revision(s) (requested by jmcneill in ticket #114): 	doc/3RDPARTY: 1.1445 	external/bsd/byacc/bin/Makefile: up to 1.7 	external/bsd/byacc/byacc2netbsd: up to 1.5 	external/bsd/byacc/dist/CHANGES: up to 1.1.1.8 	external/bsd/byacc/dist/MANIFEST: up to 1.1.1.4 	external/bsd/byacc/dist/VERSION: up to 1.1.1.8 	external/bsd/byacc/dist/btyaccpar.c: up to 1.4 	external/bsd/byacc/dist/btyaccpar.skel: up to 1.4 	external/bsd/byacc/dist/config.guess: up to 1.3 	external/bsd/byacc/dist/config.sub: up to 1.3 	external/bsd/byacc/dist/defs.h: up to 1.12 	external/bsd/byacc/dist/main.c: up to 1.14 	external/bsd/byacc/dist/output.c: up to 1.18 	external/bsd/byacc/dist/package/byacc.spec: up to 1.1.1.7 	external/bsd/byacc/dist/package/debian/changelog: up to 1.1.1.7 	external/bsd/byacc/dist/package/mingw-byacc.spec: up to 1.1.1.4 	external/bsd/byacc/dist/package/pkgsrc/Makefile: up to 1.1.1.7 	external/bsd/byacc/dist/test/btyacc/btyacc_calc1.tab.c: up to 1.3 	external/bsd/byacc/dist/test/btyacc/btyacc_demo.tab.c: up to 1.3 	external/bsd/byacc/dist/test/btyacc/btyacc_destroy1.tab.c: up to 1.3 	external/bsd/byacc/dist/test/btyacc/btyacc_destroy2.tab.c: up to 1.3 	external/bsd/byacc/dist/test/btyacc/btyacc_destroy3.tab.c: up to 1.3 	external/bsd/byacc/dist/test/btyacc/calc.tab.c: up to 1.3 	external/bsd/byacc/dist/test/btyacc/calc1.tab.c: up to 1.3 	external/bsd/byacc/dist/test/btyacc/calc2.tab.c: up to 1.3 	external/bsd/byacc/dist/test/btyacc/calc3.tab.c: up to 1.3 	external/bsd/byacc/dist/test/btyacc/code_calc.code.c: up to 1.3 	external/bsd/byacc/dist/test/btyacc/code_error.code.c: up to 1.3 	external/bsd/byacc/dist/test/btyacc/empty.tab.c: up to 1.3 	external/bsd/byacc/dist/test/btyacc/err_inherit3.tab.c: up to 1.3 	external/bsd/byacc/dist/test/btyacc/err_inherit4.tab.c: up to 1.3 	external/bsd/byacc/dist/test/btyacc/err_syntax10.tab.c: up to 1.3 	external/bsd/byacc/dist/test/btyacc/err_syntax11.tab.c: up to 1.3 	external/bsd/byacc/dist/test/btyacc/err_syntax12.tab.c: up to 1.3 	external/bsd/byacc/dist/test/btyacc/err_syntax18.tab.c: up to 1.3 	external/bsd/byacc/dist/test/btyacc/err_syntax20.tab.c: up to 1.3 	external/bsd/byacc/dist/test/btyacc/error.tab.c: up to 1.3 	external/bsd/byacc/dist/test/btyacc/expr.oxout.tab.c: up to 1.3 	external/bsd/byacc/dist/test/btyacc/grammar.tab.c: up to 1.3 	external/bsd/byacc/dist/test/btyacc/inherit0.tab.c: up to 1.3 	external/bsd/byacc/dist/test/btyacc/inherit1.tab.c: up to 1.3 	external/bsd/byacc/dist/test/btyacc/inherit2.tab.c: up to 1.3 	external/bsd/byacc/dist/test/btyacc/ok_syntax1.tab.c: up to 1.3 	external/bsd/byacc/dist/test/btyacc/pure_calc.tab.c: up to 1.3 	external/bsd/byacc/dist/test/btyacc/pure_error.tab.c: up to 1.3 	external/bsd/byacc/dist/test/btyacc/quote_calc-s.tab.c: up to 1.3 	external/bsd/byacc/dist/test/btyacc/quote_calc.tab.c: up to 1.3 	external/bsd/byacc/dist/test/btyacc/quote_calc2-s.tab.c: up to 1.3 	external/bsd/byacc/dist/test/btyacc/quote_calc2.tab.c: up to 1.3 	external/bsd/byacc/dist/test/btyacc/quote_calc3-s.tab.c: up to 1.3 	external/bsd/byacc/dist/test/btyacc/quote_calc3.tab.c: up to 1.3 	external/bsd/byacc/dist/test/btyacc/quote_calc4-s.tab.c: up to 1.3 	external/bsd/byacc/dist/test/btyacc/quote_calc4.tab.c: up to 1.3 	external/bsd/byacc/dist/test/btyacc/rename_debug.c: up to 1.3 	external/bsd/byacc/dist/test/btyacc/varsyntax_calc1.tab.c: up to 1.3 	external/bsd/byacc/dist/test/yacc/big_l.output: up to 1.1.1.2 	external/bsd/byacc/dist/test/yacc/calc3.tab.c: up to 1.3 	external/bsd/byacc/dist/test/yacc/ok_syntax1.tab.c: up to 1.3 	external/bsd/byacc/dist/test/yacc/pure_calc.tab.c: up to 1.3 	external/bsd/byacc/dist/test/yacc/pure_error.tab.c: up to 1.3 	external/bsd/byacc/dist/yaccpar.c: up to 1.5 	external/bsd/byacc/dist/yaccpar.skel: up to 1.1.1.5 Import byacc 20170430.

--- external/bsd/byacc/dist/test/yacc/pure_error.tab.c
+++ external/bsd/byacc/dist/test/yacc/pure_error.tab.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: pure_error.tab.c,v 1.2 2017/02/11 19:33:13 christos Exp $	*/
+/*	$NetBSD: pure_error.tab.c,v 1.2.4.1 2017/07/18 18:09:43 snj Exp $	*/
 
 /* original parser id follows */
 /* yysccsid[] = "@(#)yaccpar	1.9 (Berkeley) 02/21/93" */
@@ -337,6 +337,9 @@ YYPARSE_DECL()
             yydebug = yyn - '0';
     }
 #endif
+
+    memset(&yyval,  0, sizeof(yyval));
+    memset(&yylval, 0, sizeof(yylval));
 
     yym = 0;
     yyn = 0;
