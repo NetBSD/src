34f695dfb3ae7842baedaa2585cf47e7562b197c
ozaki-r <ozaki-r@NetBSD.org>
2017-07-19 09:03:08 +0000

    Don't release sav if calling crypto_dispatch again

--- sys/netipsec/xform_esp.c
+++ sys/netipsec/xform_esp.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: xform_esp.c,v 1.62 2017/07/14 12:26:26 ozaki-r Exp $	*/
+/*	$NetBSD: xform_esp.c,v 1.63 2017/07/19 09:03:08 ozaki-r Exp $	*/
 /*	$FreeBSD: src/sys/netipsec/xform_esp.c,v 1.2.2.1 2003/01/24 05:11:36 sam Exp $	*/
 /*	$OpenBSD: ip_esp.c,v 1.69 2001/06/26 06:18:59 angelos Exp $ */
 
@@ -39,7 +39,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: xform_esp.c,v 1.62 2017/07/14 12:26:26 ozaki-r Exp $");
+__KERNEL_RCSID(0, "$NetBSD: xform_esp.c,v 1.63 2017/07/19 09:03:08 ozaki-r Exp $");
 
 #if defined(_KERNEL_OPT)
 #include "opt_inet.h"
@@ -987,7 +987,6 @@ esp_output_cb(struct cryptop *crp)
 			sav->tdb_cryptoid = crp->crp_sid;
 
 		if (crp->crp_etype == EAGAIN) {
-			KEY_FREESAV(&sav);
 			mutex_exit(softnet_lock);
 			splx(s);
 			return crypto_dispatch(crp);
