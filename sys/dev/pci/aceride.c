6147d253a95db5873989e5f901a86b5ee9edbbce
snj <snj@NetBSD.org>
2017-07-23 14:27:24 +0000

    Pull up following revision(s) (requested by nakayama in ticket #1463): 	sys/dev/pci/aceride.c: revision 1.37 	sys/dev/pci/pciide_acer_reg.h: revision 1.13 Apply workaround from FreeBSD to fix read data corruption observed on Fire V100 and mSATA-SSD with mSATA to IDE adapter. The patch is from port-sparc64@.

--- sys/dev/pci/aceride.c
+++ sys/dev/pci/aceride.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: aceride.c,v 1.30 2011/04/04 20:37:56 dyoung Exp $	*/
+/*	$NetBSD: aceride.c,v 1.30.10.1 2017/07/23 14:27:24 snj Exp $	*/
 
 /*
  * Copyright (c) 1999, 2000, 2001 Manuel Bouyer.
@@ -25,7 +25,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: aceride.c,v 1.30 2011/04/04 20:37:56 dyoung Exp $");
+__KERNEL_RCSID(0, "$NetBSD: aceride.c,v 1.30.10.1 2017/07/23 14:27:24 snj Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -193,8 +193,13 @@ acer_chip_map(struct pciide_softc *sc, const struct pci_attach_args *pa)
 	interface = PCI_INTERFACE(pci_conf_read(sc->sc_pc, sc->sc_tag,
 	    PCI_CLASS_REG));
 
-	/* From linux: enable "Cable Detection" */
 	if (rev >= 0xC2) {
+		/* From FreeBSD: use device interrupt as byte count end */
+		pciide_pci_write(sc->sc_pc, sc->sc_tag, ACER_0x4A,
+		    pciide_pci_read(sc->sc_pc, sc->sc_tag, ACER_0x4A)
+		    | ACER_0x4A_BCEINT);
+
+		/* From linux: enable "Cable Detection" */
 		pciide_pci_write(sc->sc_pc, sc->sc_tag, ACER_0x4B,
 		    pciide_pci_read(sc->sc_pc, sc->sc_tag, ACER_0x4B)
 		    | ACER_0x4B_CDETECT);
