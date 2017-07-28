39f1e86852529fc74a36b893f2a15c7e6a88ec2d
riastradh <riastradh@NetBSD.org>
2017-07-28 15:34:06 +0000

    Fail, don't panic, on bad dirents from file system.
    Controllable via puffs from userland.
    
    From Ilja Van Sprundel.

--- sys/compat/linux/common/linux_file64.c
+++ sys/compat/linux/common/linux_file64.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: linux_file64.c,v 1.58 2017/01/28 15:01:01 christos Exp $	*/
+/*	$NetBSD: linux_file64.c,v 1.59 2017/07/28 15:34:06 riastradh Exp $	*/
 
 /*-
  * Copyright (c) 1995, 1998, 2000, 2008 The NetBSD Foundation, Inc.
@@ -34,7 +34,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: linux_file64.c,v 1.58 2017/01/28 15:01:01 christos Exp $");
+__KERNEL_RCSID(0, "$NetBSD: linux_file64.c,v 1.59 2017/07/28 15:34:06 riastradh Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -325,8 +325,10 @@ again:
 	for (cookie = cookiebuf; len > 0; len -= reclen) {
 		bdp = (struct dirent *)inp;
 		reclen = bdp->d_reclen;
-		if (reclen & 3)
-			panic("linux_readdir");
+		if (reclen & 3) {
+			error = EIO;
+			goto out;
+		}
 		if (bdp->d_fileno == 0) {
 			inp += reclen;	/* it is a hole; squish it out */
 			if (cookie)
