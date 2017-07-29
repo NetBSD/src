b4f35d13240cbf0894f90947f1ca56f055c04b18
maxv <maxv@NetBSD.org>
2017-07-29 06:12:50 +0000

    Only compat_43 needs compat_osock. Note that the use of vec_compat_ifioctl is racy.

--- sys/compat/sys/socket.h
+++ sys/compat/sys/socket.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: socket.h,v 1.14 2016/04/07 17:48:40 mrg Exp $	*/
+/*	$NetBSD: socket.h,v 1.15 2017/07/29 06:12:50 maxv Exp $	*/
 
 /*
  * Copyright (c) 1982, 1985, 1986, 1988, 1993, 1994
@@ -43,8 +43,7 @@
 #include "opt_compat_43.h"
 #include "opt_modular.h"
 
-#if defined(COMPAT_43) || defined(COMPAT_LINUX) || defined(COMPAT_SVR4) || \
-    defined(COMPAT_ULTRIX) || defined(MODULAR)
+#if defined(COMPAT_43) || defined(MODULAR)
 #define COMPAT_OSOCK
 #endif
 
