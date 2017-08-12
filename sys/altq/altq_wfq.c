682c486072b1775c7f570edac1a2a587c656d308
snj <snj@NetBSD.org>
2017-08-12 04:44:32 +0000

    Pull up following revision(s) (requested by mrg in ticket #1485): 	sys/altq/altq_cbq.c: revision 1.31 	sys/altq/altq_hfsc.c: revision 1.27 	sys/altq/altq_priq.c: revision 1.24 	sys/altq/altq_jobs.c: revision 1.11 	sys/altq/altq_wfq.c: revision 1.22 Zero buffers copied to userland to avoid stack disclosure. From Ilja Van Sprundel. -- Reject negative indices. (Would be nice to change the types too, and it's *probably* safe to replace int by u_int, but I'm reluctant to touch the ioctl definitions without at least a modicum more thought.  Also one of them is a u_long, because why not?) From Ilja Van Sprundel.

--- sys/altq/altq_wfq.c
+++ sys/altq/altq_wfq.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: altq_wfq.c,v 1.20 2014/03/20 20:51:54 christos Exp $	*/
+/*	$NetBSD: altq_wfq.c,v 1.20.8.1 2017/08/12 04:44:32 snj Exp $	*/
 /*	$KAME: altq_wfq.c,v 1.14 2005/04/13 03:44:25 suz Exp $	*/
 
 /*
@@ -32,7 +32,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: altq_wfq.c,v 1.20 2014/03/20 20:51:54 christos Exp $");
+__KERNEL_RCSID(0, "$NetBSD: altq_wfq.c,v 1.20.8.1 2017/08/12 04:44:32 snj Exp $");
 
 #ifdef _KERNEL_OPT
 #include "opt_altq.h"
@@ -518,13 +518,14 @@ wfq_setweight(struct wfq_setweight *swp)
 	wfq *queue;
 	int old;
 
-	if (swp->weight < 0) {
-		printf("set weight in natural number\n");
+	if (swp->weight < 0)
 		return (EINVAL);
-	}
 
 	if ((wfqp = altq_lookup(swp->iface.wfq_ifacename, ALTQT_WFQ)) == NULL)
 		return (EBADF);
+
+	if (swp->qid < 0 || swp->qid >= wfqp->nums)
+		return (EINVAL);
 
 	queue = &wfqp->queue[swp->qid];
 	old = queue->weight;
@@ -544,7 +545,7 @@ wfq_getstats(struct wfq_getstats *gsp)
 	if ((wfqp = altq_lookup(gsp->iface.wfq_ifacename, ALTQT_WFQ)) == NULL)
 		return (EBADF);
 
-	if (gsp->qid >= wfqp->nums)
+	if (gsp->qid < 0 || gsp->qid >= wfqp->nums)
 		return (EINVAL);
 
 	queue = &wfqp->queue[gsp->qid];
