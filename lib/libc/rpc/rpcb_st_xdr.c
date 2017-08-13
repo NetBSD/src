293ac31897959500d430b4ab3e64d60cca929553
ginsbach <ginsbach@NetBSD.org>
2017-08-13 01:08:41 +0000

    PR lib/15802: Shuuichirou Murata: Add missing xdr_rpcbs_rmtcalllist_ptr
    There was a missing call to xdr_rpcbs_rmtcalllist_ptr in xdr_rpcb_stat.
    This fixes issues with RPCBPROC_GETSTAT not working correctly with
    systems that correctly implement the XDR encode/decode routine.
    
    XXX: pullup-8
    XXX: pullup-7
    XXX: pullup-6

--- lib/libc/rpc/rpcb_st_xdr.c
+++ lib/libc/rpc/rpcb_st_xdr.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: rpcb_st_xdr.c,v 1.11 2017/05/03 21:39:27 christos Exp $	*/
+/*	$NetBSD: rpcb_st_xdr.c,v 1.12 2017/08/13 01:08:41 ginsbach Exp $	*/
 
 /*
  * Copyright (c) 2010, Oracle America, Inc.
@@ -42,7 +42,7 @@
 
 #include <sys/cdefs.h>
 #if defined(LIBC_SCCS) && !defined(lint)
-__RCSID("$NetBSD: rpcb_st_xdr.c,v 1.11 2017/05/03 21:39:27 christos Exp $");
+__RCSID("$NetBSD: rpcb_st_xdr.c,v 1.12 2017/08/13 01:08:41 ginsbach Exp $");
 #endif
 
 #include "namespace.h"
@@ -256,6 +256,9 @@ xdr_rpcb_stat(XDR *xdrs, rpcb_stat *objp)
 		return (FALSE);
 	}
 	if (!xdr_rpcbs_addrlist_ptr(xdrs, &objp->addrinfo)) {
+		return (FALSE);
+	}
+	if (!xdr_rpcbs_rmtcalllist_ptr(xdrs, &objp->rmtinfo)) {
 		return (FALSE);
 	}
 	return (TRUE);
