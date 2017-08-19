0c1ea46dc2c93cdd51a701394a671084b7ff5aa4
snj <snj@NetBSD.org>
2017-08-19 04:20:01 +0000

    Pull up following revision(s) (requested by mrg in ticket #1483): 	sys/compat/common/vfs_syscalls_12.c: revision 1.34 	sys/compat/svr4_32/svr4_32_misc.c: revision 1.78 	sys/compat/sunos32/sunos32_misc.c: revision 1.78 	sys/compat/linux/common/linux_misc.c: revision 1.239 	sys/compat/osf1/osf1_file.c: revision 1.44 	sys/compat/common/vfs_syscalls_43.c: revision 1.60 	sys/compat/svr4/svr4_misc.c: revision 1.158 	sys/compat/ibcs2/ibcs2_misc.c: revision 1.114 	sys/compat/linux/common/linux_file64.c: revision 1.59 	sys/compat/linux32/common/linux32_dirent.c: revision 1.18 	sys/compat/sunos/sunos_misc.c: revision 1.171 Fail, don't panic, on bad dirents from file system. Controllable via puffs from userland. From Ilja Van Sprundel.

--- sys/compat/common/vfs_syscalls_12.c
+++ sys/compat/common/vfs_syscalls_12.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: vfs_syscalls_12.c,v 1.29.12.1 2017/08/12 16:23:28 snj Exp $	*/
+/*	$NetBSD: vfs_syscalls_12.c,v 1.29.12.2 2017/08/19 04:20:01 snj Exp $	*/
 
 /*
  * Copyright (c) 1989, 1993
@@ -37,7 +37,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: vfs_syscalls_12.c,v 1.29.12.1 2017/08/12 16:23:28 snj Exp $");
+__KERNEL_RCSID(0, "$NetBSD: vfs_syscalls_12.c,v 1.29.12.2 2017/08/19 04:20:01 snj Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -171,8 +171,10 @@ again:
 	for (cookie = cookiebuf; len > 0; len -= reclen) {
 		bdp = (struct dirent *)inp;
 		reclen = bdp->d_reclen;
-		if (reclen & 3)
-			panic(__func__);
+		if (reclen & 3) {
+			error = EIO;
+			goto out;
+		}
 		if (bdp->d_fileno == 0) {
 			inp += reclen;	/* it is a hole; squish it out */
 			if (cookie)
