c74a1d125374b43d6eae1b82c146dd879c8fb592
kamil <kamil@NetBSD.org>
2017-08-28 01:27:34 +0000

    Welcome to 8.99.2!
    mount_procfs(8): removal of /proc/#/ctl and filesystem tracing feature

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.544 2017/07/11 15:39:31 dholland Exp $	*/
+/*	$NetBSD: param.h,v 1.545 2017/08/28 01:27:34 kamil Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -67,7 +67,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	899000100	/* NetBSD 8.99.1 */
+#define	__NetBSD_Version__	899000200	/* NetBSD 8.99.2 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
