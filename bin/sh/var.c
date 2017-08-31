37614711f2aed2718f36ddd6cfcb1572b2194e70
martin <martin@NetBSD.org>
2017-08-31 11:43:44 +0000

    Pull up following revision(s) (requested by kre in ticket #250): 	bin/sh/var.c: revision 1.67 Fix a bug noticed by Soren Jacobsen running the netbsd-6-0 build.sh which causes a core dump in some exotic circumstances (when restoring local variables when a function returns).  ("build.sh makewrapper" exposed it.) This was introduced in 1.63 - not as part of the substance of that change (addition) but as an unrelated "must be the right thing to do" cleanup, which wasn't...

--- bin/sh/var.c
+++ bin/sh/var.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: var.c,v 1.55.2.1 2017/07/23 14:58:14 snj Exp $	*/
+/*	$NetBSD: var.c,v 1.55.2.2 2017/08/31 11:43:44 martin Exp $	*/
 
 /*-
  * Copyright (c) 1991, 1993
@@ -37,7 +37,7 @@
 #if 0
 static char sccsid[] = "@(#)var.c	8.3 (Berkeley) 5/4/95";
 #else
-__RCSID("$NetBSD: var.c,v 1.55.2.1 2017/07/23 14:58:14 snj Exp $");
+__RCSID("$NetBSD: var.c,v 1.55.2.2 2017/08/31 11:43:44 martin Exp $");
 #endif
 #endif /* not lint */
 
@@ -472,7 +472,7 @@ setvareq(char *s, int flags)
 		return;
 	}
 	vp = ckmalloc(sizeof (*vp));
-	vp->flags = flags & ~(VNOFUNC|VFUNCREF|VSTRFIXED);
+	vp->flags = flags & ~(VNOFUNC|VFUNCREF);
 	vp->text = s;
 	vp->name_len = nlen;
 	vp->next = *vpp;
