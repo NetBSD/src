8c5650dbbadf5229a2dd93c82a21aa4b049ab6c4
isaki <isaki@NetBSD.org>
2017-09-02 12:57:35 +0000

    Revert previous local conversion for vs(4).

--- sys/dev/ic/msm6258var.h
+++ sys/dev/ic/msm6258var.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: msm6258var.h,v 1.9 2017/08/05 05:53:26 isaki Exp $	*/
+/*	$NetBSD: msm6258var.h,v 1.10 2017/09/02 12:57:35 isaki Exp $	*/
 
 /*
  * Copyright (c) 2001 Tetsuya Isaki. All rights reserved.
@@ -32,11 +32,3 @@ extern stream_filter_factory_t msm6258_slinear16_to_adpcm;
 extern stream_filter_factory_t msm6258_linear8_to_adpcm;
 extern stream_filter_factory_t msm6258_adpcm_to_slinear16;
 extern stream_filter_factory_t msm6258_adpcm_to_linear8;
-
-/* XXX */
-extern void *vs_alloc_msm6258codec(void);
-extern void vs_free_msm6258codec(void *);
-extern void vs_slinear16be_to_adpcm(void *, void *, const void *, int);
-extern void vs_slinear8_to_adpcm(void *, void *, const void *, int);
-extern void vs_adpcm_to_slinear16be(void *, void *, int, const void *);
-extern void vs_adpcm_to_slinear8(void *, void *, int, const void *);
