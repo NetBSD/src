fe19e7a408c53fded3d8db6484c369440c9d7655
snj <snj@NetBSD.org>
2017-09-04 06:43:14 +0000

    Pull up following revision(s) (requested by jakllsch in ticket #263): 	sys/dev/usb/uhidev.c: revision 1.71 	sys/dev/usb/ukbd.c: revision 1.137-1.138 Fix memory leak in report parsing error paths. -- Support more varieties of USB keyboard reports. The previous code asssumed reports would closely match the Bootstrap Keyboard Protocol.  This is no longer always the case, particularly with higher-end keyboards. -- Always try to set USB HID devices into Report Protocol.  (Unless the device is known to be quirky.) Some of the most-widely-compatible methods of implementing USB Keyboard NKRO depend on this Request to function as designed. Issuing this Request is recommended by the HID 1.11 spec (7.2.6): ... "the host should not make any assumptions about the device's state and should set the desired protocol whenever initializing a device."

--- sys/dev/usb/uhidev.c
+++ sys/dev/usb/uhidev.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: uhidev.c,v 1.70 2017/06/01 02:45:12 chs Exp $	*/
+/*	$NetBSD: uhidev.c,v 1.70.2.1 2017/09/04 06:43:14 snj Exp $	*/
 
 /*
  * Copyright (c) 2001, 2012 The NetBSD Foundation, Inc.
@@ -35,7 +35,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: uhidev.c,v 1.70 2017/06/01 02:45:12 chs Exp $");
+__KERNEL_RCSID(0, "$NetBSD: uhidev.c,v 1.70.2.1 2017/09/04 06:43:14 snj Exp $");
 
 #ifdef _KERNEL_OPT
 #include "opt_usb.h"
@@ -153,12 +153,9 @@ uhidev_attach(device_t parent, device_t self, void *aux)
 		aprint_error_dev(self, "couldn't establish power handler\n");
 
 	(void)usbd_set_idle(iface, 0, 0);
-#if 0
 
-	if ((usbd_get_quirks(sc->sc_udev)->uq_flags & UQ_NO_SET_PROTO) == 0 &&
-	    id->bInterfaceSubClass != UISUBCLASS_BOOT)
+	if ((usbd_get_quirks(sc->sc_udev)->uq_flags & UQ_NO_SET_PROTO) == 0)
 		(void)usbd_set_protocol(iface, 1);
-#endif
 
 	maxinpktsize = 0;
 	sc->sc_iep_addr = sc->sc_oep_addr = -1;
