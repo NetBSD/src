dc22bf009b77d28f5fab323e099cf2c021ea986b
jmcneill <jmcneill@NetBSD.org>
2017-09-30 10:29:10 +0000

    0xB -> 0xb on previous

--- sys/arch/arm/sunxi/sunxi_emac.h
+++ sys/arch/arm/sunxi/sunxi_emac.h
@@ -115,9 +115,9 @@
 #define	EMAC_MII_DATA		0x4c
 #define	EMAC_ADDR_HIGH(n)	(0x50 + (n) * 8)
 #define	EMAC_ADDR_LOW(n)	(0x54 + (n) * 8)
-#define	EMAC_TX_DMA_STA		0xB0
-#define	EMAC_TX_DMA_CUR_DESC	0xB4
-#define	EMAC_TX_DMA_CUR_BUF	0xB8
+#define	EMAC_TX_DMA_STA		0xb0
+#define	EMAC_TX_DMA_CUR_DESC	0xb4
+#define	EMAC_TX_DMA_CUR_BUF	0xb8
 #define	EMAC_RX_DMA_STA		0xc0
 #define	EMAC_RX_DMA_CUR_DESC	0xc4
 #define	EMAC_RX_DMA_CUR_BUF	0xc8
