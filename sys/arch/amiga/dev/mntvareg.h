0845226b278623b4b7e1a45cc9bdf612b9b0ff16
rkujawa <rkujawa@NetBSD.org>
2017-10-04 09:44:09 +0000

    Update driver to support firmware v1.5+.
    From Lukas F. Hartmann. With hopeful style fixes from me.

--- sys/arch/amiga/dev/mntvareg.h
+++ sys/arch/amiga/dev/mntvareg.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: mntvareg.h,v 1.1 2016/10/03 14:26:02 rkujawa Exp $  */
+/*	$NetBSD: mntvareg.h,v 1.2 2017/10/04 09:44:09 rkujawa Exp $  */
 
 /*
  * Copyright (c) 2012, 2016 The NetBSD Foundation, Inc. 
@@ -34,10 +34,10 @@
 #define MNTVA2000REG_H
 
 /* address space */
-#define MNTVA_OFF_REG		0x5f0000	/* memory mapped registers */
-#define MNTVA_REG_SIZE		0xFF
+#define MNTVA_OFF_REG		0x000000	/* memory mapped registers */
+#define MNTVA_REG_SIZE	0x001000
 
-#define MNTVA_OFF_FB		0x000000	/* frame buffer */
+#define MNTVA_OFF_FB		0x010000	/* frame buffer */
 #define MNTVA_FB_SIZE		0x5f0000
 
 /* registers */
@@ -47,8 +47,11 @@
 #define MNTVA_SCALEMODE3X		2	
 #define MNTVA_SCALEMODE4X		3	
 
+#define MNTVA_LINEW     0x02
 #define MNTVA_SCREENW		0x06
 #define MNTVA_SCREENH		0x08
+#define MNTVA_MARGIN_X  0x0c
+#define MNTVA_SAFE_X    0x14
 
 #define MNTVA_BLITTERBASEHI	0x1C	/* [23:16] */
 #define MNTVA_BLITTERBASELO	0x1E	/* [15:0] */
@@ -100,7 +103,27 @@
 #define MNTVA_COLORMODE16		__BIT(0)	
 #define MNTVA_COLORMODE32		__BIT(1)
 
+#define MNTVA_BLITTER_ROW_PITCH 0x42
+#define MNTVA_BLITTER_ROW_PITCH_SHIFT 0x44
+#define MNTVA_BLITTER_COLORMODE 0x46
+
 #define MNTVA_PANPTRHI 0x38 /* [23:16] */
 #define MNTVA_PANPTRLO 0x3A /* [15:0] */
+
+#define MNTVA_CAPTURE_MODE 0x4E
+
+#define MNTVA_ROW_PITCH       0x58
+#define MNTVA_ROW_PITCH_SHIFT 0x5c
+
+#define MNTVA_H_SYNC_START  0x70
+#define MNTVA_H_SYNC_END    0x72
+#define MNTVA_H_MAX         0x74
+#define MNTVA_V_SYNC_START  0x76
+#define MNTVA_V_SYNC_END    0x78
+#define MNTVA_V_MAX         0x7a
+
+#define MNTVA_PIXEL_CLK_SEL 0x7c
+#define MNTVA_CLK_75MHZ 0
+#define MNTVA_CLK_40MHZ 1
 
 #endif /* MNTVA2000REG_H */
