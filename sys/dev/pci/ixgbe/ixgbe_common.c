597e8da1b15459d02af1257f92d25c4a0018d80a
msaitoh <msaitoh@NetBSD.org>
2017-10-13 08:17:44 +0000

    Show 2.5GBASE-T and 5GBASE-T correctly on ixv(4). Tested on a virtual function on X550 T1.

--- sys/dev/pci/ixgbe/ixgbe_common.c
+++ sys/dev/pci/ixgbe/ixgbe_common.c
@@ -1,4 +1,4 @@
-/* $NetBSD: ixgbe_common.c,v 1.14 2017/08/30 08:49:18 msaitoh Exp $ */
+/* $NetBSD: ixgbe_common.c,v 1.15 2017/10/13 08:17:44 msaitoh Exp $ */
 
 /******************************************************************************
 
@@ -4248,7 +4248,7 @@ s32 ixgbe_check_mac_link_generic(struct ixgbe_hw *hw, ixgbe_link_speed *speed,
 		break;
 	case IXGBE_LINKS_SPEED_100_82599:
 		*speed = IXGBE_LINK_SPEED_100_FULL;
-		if (hw->mac.type == ixgbe_mac_X550) {
+		if (hw->mac.type >= ixgbe_mac_X550) {
 			if (links_reg & IXGBE_LINKS_SPEED_NON_STD)
 				*speed = IXGBE_LINK_SPEED_5GB_FULL;
 		}
