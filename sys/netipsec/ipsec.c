14289635415cee1866c4ffa1278e4ff06326dc1c
ozaki-r <ozaki-r@NetBSD.org>
2017-10-17 07:23:08 +0000

    Fix buffer length for ipsec_logsastr

--- sys/netipsec/ipsec.c
+++ sys/netipsec/ipsec.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: ipsec.c,v 1.121 2017/10/03 08:25:21 ozaki-r Exp $	*/
+/*	$NetBSD: ipsec.c,v 1.122 2017/10/17 07:23:08 ozaki-r Exp $	*/
 /*	$FreeBSD: /usr/local/www/cvsroot/FreeBSD/src/sys/netipsec/ipsec.c,v 1.2.2.2 2003/07/01 01:38:13 sam Exp $	*/
 /*	$KAME: ipsec.c,v 1.103 2001/05/24 07:14:18 sakane Exp $	*/
 
@@ -32,7 +32,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: ipsec.c,v 1.121 2017/10/03 08:25:21 ozaki-r Exp $");
+__KERNEL_RCSID(0, "$NetBSD: ipsec.c,v 1.122 2017/10/17 07:23:08 ozaki-r Exp $");
 
 /*
  * IPsec controller part.
@@ -2075,7 +2075,6 @@ ipsec_updatereplay(u_int32_t seq, const struct secasvar *sav)
 	int fr;
 	u_int32_t wsizeb;	/* constant: bits of window size */
 	int frlast;		/* constant: last frame */
-	char buf[INET6_ADDRSTRLEN];
 
 	IPSEC_SPLASSERT_SOFTNET(__func__);
 
@@ -2143,6 +2142,7 @@ ipsec_updatereplay(u_int32_t seq, const struct secasvar *sav)
 
 ok:
 	if (replay->count == ~0) {
+		char buf[IPSEC_LOGSASTRLEN];
 
 		/* set overflow flag */
 		replay->overflow++;
