6f06f61966e5ee47331fc060438127146ba5be91
msaitoh <msaitoh@NetBSD.org>
2017-10-17 14:48:42 +0000

    Update from the latest Intel SDM:  0x5c: Atom (Goldmont)  0x5f: Atom (Goldmont, Denverton)  0x7a: Atom (Goldmont Plus)

--- usr.sbin/cpuctl/arch/i386.c
+++ usr.sbin/cpuctl/arch/i386.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: i386.c,v 1.76 2017/10/16 10:10:48 msaitoh Exp $	*/
+/*	$NetBSD: i386.c,v 1.77 2017/10/17 14:48:42 msaitoh Exp $	*/
 
 /*-
  * Copyright (c) 1999, 2000, 2001, 2006, 2007, 2008 The NetBSD Foundation, Inc.
@@ -57,7 +57,7 @@
 
 #include <sys/cdefs.h>
 #ifndef lint
-__RCSID("$NetBSD: i386.c,v 1.76 2017/10/16 10:10:48 msaitoh Exp $");
+__RCSID("$NetBSD: i386.c,v 1.77 2017/10/17 14:48:42 msaitoh Exp $");
 #endif /* not lint */
 
 #include <sys/types.h>
@@ -375,10 +375,11 @@ const struct cpu_cpuid_nameclass i386_cpuid_cpus[] = {
 				[0x56] = "Xeon D-1500 (Broadwell)",
 				[0x57] = "Xeon Phi [357]200",
 				[0x5a] = "Atom E3500",
-				[0x5c] = "Next Atom (Goldmont)",
+				[0x5c] = "Atom (Goldmont)",
 				[0x5d] = "Atom X3-C3000 (Silvermont)",
 				[0x5e] = "6th gen Core, Xeon E3-1[25]00 v5 (Skylake)",
-				[0x5f] = "Future Atom (Denverton)",
+				[0x5f] = "Atom (Goldmont, Denverton)",
+				[0x7a] = "Atom (Goldmont Plus)",
 				[0x85] = "Future Xeon Phi",
 				[0x8e] = "7th gen Core (Kaby Lake)",
 				[0x9e] = "7th gen Core (Kaby Lake)",
