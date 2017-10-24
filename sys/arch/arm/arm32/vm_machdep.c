8ea92352f6e842176f0785bc8e3c8bb0a0ec9a86
snj <snj@NetBSD.org>
2017-10-24 09:12:07 +0000

    Pull up following revision(s) (requested by skrll in ticket #315): 	sys/arch/arm/arm32/vm_machdep.c: 1.71-1.72 In vmapbuf use pmap_enter(pmap_kernel(), ...) and not pmap_kenter_pa as the former handles multiple mappings for VIPT AND VIVT correctly whereas the latter doesn't work for VIVT. PR port-shark/52102 From skrll@. Tested by martin@ and me. -- Fix eva argument to pmap_remove and passed prot bits in flags for pmap_enter, i.e. fix previous.

--- sys/arch/arm/arm32/vm_machdep.c
+++ sys/arch/arm/arm32/vm_machdep.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: vm_machdep.c,v 1.70 2015/03/29 09:47:48 matt Exp $	*/
+/*	$NetBSD: vm_machdep.c,v 1.70.10.1 2017/10/24 09:12:07 snj Exp $	*/
 
 /*
  * Copyright (c) 1994-1998 Mark Brinicombe.
@@ -44,7 +44,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: vm_machdep.c,v 1.70 2015/03/29 09:47:48 matt Exp $");
+__KERNEL_RCSID(0, "$NetBSD: vm_machdep.c,v 1.70.10.1 2017/10/24 09:12:07 snj Exp $");
 
 #include "opt_armfpe.h"
 #include "opt_pmap_debug.h"
@@ -245,8 +245,8 @@ vmapbuf(struct buf *bp, vsize_t len)
 	 */
 	while (len) {
 		(void) pmap_extract(pm, faddr, &fpa);
-		pmap_kenter_pa(taddr, fpa, VM_PROT_READ|VM_PROT_WRITE,
-		    PMAP_WIRED);
+		pmap_enter(pmap_kernel(), taddr, fpa, VM_PROT_READ|VM_PROT_WRITE,
+		    VM_PROT_READ|VM_PROT_WRITE|PMAP_WIRED);
 		faddr += PAGE_SIZE;
 		taddr += PAGE_SIZE;
 		len -= PAGE_SIZE;
@@ -281,7 +281,7 @@ vunmapbuf(struct buf *bp, vsize_t len)
 	off = (vaddr_t)bp->b_data - addr;
 	len = round_page(off + len);
 
-	pmap_kremove(addr, len);
+	pmap_remove(pmap_kernel(), addr, addr + len);
 	pmap_update(pmap_kernel());
 	uvm_km_free(phys_map, addr, len, UVM_KMF_VAONLY);
 	bp->b_data = bp->b_saveaddr;
