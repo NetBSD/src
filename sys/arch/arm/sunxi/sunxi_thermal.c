2f9156b2c076da9e20677699b55f6be7d7432a90
pgoyette <pgoyette@NetBSD.org>
2017-11-01 01:47:00 +0000

    Simplify, and avoid compiler warning for variable format string.
    As discussed on IRC.

--- sys/arch/arm/sunxi/sunxi_thermal.c
+++ sys/arch/arm/sunxi/sunxi_thermal.c
@@ -1,4 +1,4 @@
-/* $NetBSD: sunxi_thermal.c,v 1.1 2017/10/05 01:30:26 jmcneill Exp $ */
+/* $NetBSD: sunxi_thermal.c,v 1.2 2017/11/01 01:47:00 pgoyette Exp $ */
 
 /*-
  * Copyright (c) 2016-2017 Jared McNeill <jmcneill@invisible.ca>
@@ -31,7 +31,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: sunxi_thermal.c,v 1.1 2017/10/05 01:30:26 jmcneill Exp $");
+__KERNEL_RCSID(0, "$NetBSD: sunxi_thermal.c,v 1.2 2017/11/01 01:47:00 pgoyette Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -552,8 +552,8 @@ sunxi_thermal_attach(device_t parent, device_t self, void *aux)
 		sc->data[i].private = i;
 		sc->data[i].units = ENVSYS_STEMP;
 		sc->data[i].state = ENVSYS_SINVALID;
-		snprintf(sc->data[i].desc, sizeof(sc->data[i].desc),
-		    sc->conf->sensors[i].desc);
+		strlcpy(sc->data[1].desc, sc->conf->sensors[i].desc,
+		    sizeof(sc->data[1].desc);
 		sysmon_envsys_sensor_attach(sc->sme, &sc->data[i]);
 	}
 	sysmon_envsys_register(sc->sme);
