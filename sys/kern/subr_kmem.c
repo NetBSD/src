0b071c18fc577489bcec4c8d551d91a59cc87c00
riastradh <riastradh@NetBSD.org>
2017-11-09 23:20:12 +0000

    Assert KM_SLEEP xor KM_NOSLEEP in all kmem allocation.

--- sys/kern/subr_kmem.c
+++ sys/kern/subr_kmem.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: subr_kmem.c,v 1.64 2017/11/07 18:35:57 christos Exp $	*/
+/*	$NetBSD: subr_kmem.c,v 1.65 2017/11/09 23:20:12 riastradh Exp $	*/
 
 /*-
  * Copyright (c) 2009-2015 The NetBSD Foundation, Inc.
@@ -100,7 +100,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: subr_kmem.c,v 1.64 2017/11/07 18:35:57 christos Exp $");
+__KERNEL_RCSID(0, "$NetBSD: subr_kmem.c,v 1.65 2017/11/09 23:20:12 riastradh Exp $");
 
 #ifdef _KERNEL_OPT
 #include "opt_kmem.h"
@@ -259,6 +259,9 @@ kmem_intr_alloc(size_t requested_size, km_flag_t kmflags)
 	uint8_t *p;
 
 	KASSERT(requested_size > 0);
+
+	KASSERT((kmflags & KM_SLEEP) || (kmflags & KM_NOSLEEP));
+	KASSERT(!(kmflags & KM_SLEEP) || !(kmflags & KM_NOSLEEP));
 
 #ifdef KMEM_GUARD
 	if (kmem_guard_enabled) {
