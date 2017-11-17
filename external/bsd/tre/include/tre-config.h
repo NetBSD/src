24d24ffbc715b02db568bd2a2b9fb22708d3ebdb
rin <rin@NetBSD.org>
2017-11-17 16:17:06 +0000

    Regen. Actually stop using alloca(3).

--- external/bsd/tre/include/tre-config.h
+++ external/bsd/tre/include/tre-config.h
@@ -11,7 +11,7 @@
 /* #undef HAVE_REG_ERRCODE_T */
 
 /* Define to 1 if you have the <sys/types.h> header file. */
-#define HAVE_SYS_TYPES_H 1
+/* #undef HAVE_SYS_TYPES_H */
 
 /* Define to 1 if you have the <wchar.h> header file. */
 #define HAVE_WCHAR_H 1
