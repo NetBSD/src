159afd06ddc4fa4762e9802eba292f2eac64d266
christos <christos@NetBSD.org>
2017-11-29 21:35:26 +0000

    avoid re-definition

--- external/gpl3/gdb/dist/gdb/vax-bsd-nat.c
+++ external/gpl3/gdb/dist/gdb/vax-bsd-nat.c
@@ -17,7 +17,9 @@
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
 
+#ifndef _KERNTYPES
 #define _KERNTYPES
+#endif
 #include "defs.h"
 #include "inferior.h"
 #include "regcache.h"
