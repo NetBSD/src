d35ce2c22be31da05706ee907266d07cc3a7c2de
skrll <skrll@NetBSD.org>
2017-11-29 15:01:44 +0000

    Delete the unused gdb.old.
    OK releng.

--- external/gpl3/gdb.old/dist/gdb/testsuite/gdb.linespec/body.h
+++
@@ -1,5 +0,0 @@
-/* This is included directly into the body of a function.  */
-
-/* body breakpoint no code */
-
-return x;
--- external/gpl3/gdb.old/dist/sim/testsuite/sim/cris/c/badldso3.c
+++
@@ -1,9 +0,0 @@
-/*
-#notarget: cris*-*-elf
-#dynamic:
-#xerror:
-#cc: additional_flags=-Wl,-dynamic-linker,/compilercheck.x
-#sim: --sysroot=@exedir@
-#output: *: could not load ELF interpreter `*' for program `*'\n
- */
-#include "hello.c"
