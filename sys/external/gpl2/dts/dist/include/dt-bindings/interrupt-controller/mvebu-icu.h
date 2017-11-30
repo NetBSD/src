9ef525f11681f9ab2ef40b0dd0d1b3a5fdc00147
jmcneill <jmcneill@NetBSD.org>
2017-11-30 19:40:49 +0000

    Import dts from Linux 4.15-rc1

--- sys/external/gpl2/dts/dist/include/dt-bindings/interrupt-controller/mvebu-icu.h
+++ sys/external/gpl2/dts/dist/include/dt-bindings/interrupt-controller/mvebu-icu.h
@@ -1,5 +1,6 @@
-/*	$NetBSD: mvebu-icu.h,v 1.1.1.1 2017/10/28 10:30:32 jmcneill Exp $	*/
+/*	$NetBSD: mvebu-icu.h,v 1.1.1.2 2017/11/30 19:40:51 jmcneill Exp $	*/
 
+/* SPDX-License-Identifier: GPL-2.0 */
 /*
  * This header provides constants for the MVEBU ICU driver.
  */
