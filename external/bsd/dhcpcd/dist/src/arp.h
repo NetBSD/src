ffdc9e005636df9cf5b3e2086a89022352a353aa
roy <roy@NetBSD.org>
2017-12-06 10:33:29 +0000

    Import dhcpcd-7.0.0-rc4 with the following changes:   *  Don't flush prefix routes/routers if kernel does not support RA   *  dhcp: improve errors around UDP checksum failure   *  dhcp: announce existing addresses before rebooting   *  bpf: rework loop so that we can close/reopen fd inside and abort   *  ipv6nd: don't handle NA/RA for non active interfaces   *  dhcp6: listen on all addresses in non master mode   *  dhcpcd-run-hooks: set protocol in dhcpcd, don't guess   *  Ensure that xid is unique across all interfaces   *  dhcp6: redirect message to interface which uses the xid   *  bsd: strip scope from LL addresses when detecting their addition   *  ipv6nd: fix address lifetime overflow on carrier up   *  dhcp6: fix confirmation of lease on carrier up

--- external/bsd/dhcpcd/dist/src/arp.h
+++ external/bsd/dhcpcd/dist/src/arp.h
@@ -76,7 +76,8 @@ struct arp_state {
 TAILQ_HEAD(arp_statehead, arp_state);
 
 struct iarp_state {
-	int fd;
+	int bpf_fd;
+	unsigned int bpf_flags;
 	struct arp_statehead arp_states;
 };
 
@@ -95,6 +96,7 @@ struct arp_state *arp_new(struct interface *, const struct in_addr *);
 struct arp_state *arp_find(struct interface *, const struct in_addr *);
 void arp_announce(struct arp_state *);
 void arp_announceaddr(struct dhcpcd_ctx *, struct in_addr *);
+void arp_ifannounceaddr(struct interface *, struct in_addr *);
 void arp_cancel(struct arp_state *);
 void arp_free(struct arp_state *);
 void arp_free_but(struct arp_state *);
