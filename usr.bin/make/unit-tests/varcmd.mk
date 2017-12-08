410a5867e2ca87fcab91fe3a23a0efd0e1563522
sjg <sjg@NetBSD.org>
2017-12-08 03:36:42 +0000

    Ensure VAR+=val on command line is handled correctly
    If VAR is not previously set, call Var_Set to deal with
    the special case of VAR_CMD.
    
    If VAR is previously set, and ctxt is VAR_CMD we should do the update
    even if VAR_FROM_CMD is set.

--- usr.bin/make/unit-tests/varcmd.mk
+++ usr.bin/make/unit-tests/varcmd.mk
@@ -1,4 +1,4 @@
-# $Id: varcmd.mk,v 1.2 2017/11/18 22:34:04 sjg Exp $
+# $Id: varcmd.mk,v 1.3 2017/12/08 03:36:42 sjg Exp $
 #
 # Test behaviour of recursive make and vars set on command line.
 
@@ -15,7 +15,7 @@ show:
 	@echo "${TAG} FU=<v>${FU}</v> FOO=<v>${FOO}</v> VAR=<v>${VAR}</v>"
 
 one:	show
-	@${.MAKE} -f ${MAKEFILE} FU=bar FOO=goo two
+	@${.MAKE} -f ${MAKEFILE} FU=bar FOO+=goo two
 
 two:	show
 	@${.MAKE} -f ${MAKEFILE} three
@@ -27,10 +27,13 @@ three:	show
 .ifmake two
 # this should not work
 FU+= oops
+FOO+= oops
 _FU:= ${FU}
+_FOO:= ${FOO}
 two: immutable
 immutable:
 	@echo "$@ FU='${_FU}'"
+	@echo "$@ FOO='${_FOO}'"
 .endif
 .ifmake four
 VAR=Internal
