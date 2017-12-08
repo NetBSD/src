9f22166744ca15f516bba65795df391d07e8f92f
kre <kre@NetBSD.org>
2017-12-08 15:31:13 +0000

    Finish the previous change - as well as setting the default for these compat modules back to enabled, stop forcing it to switch back to disabled any time the module is init'd (which is truly barbaric behaviour.)

--- sys/compat/linux32/common/linux32_mod.c
+++ sys/compat/linux32/common/linux32_mod.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: linux32_mod.c,v 1.9 2017/12/01 22:14:52 joerg Exp $	*/
+/*	$NetBSD: linux32_mod.c,v 1.10 2017/12/08 15:31:13 kre Exp $	*/
 
 /*-
  * Copyright (c) 2008 The NetBSD Foundation, Inc.
@@ -30,7 +30,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: linux32_mod.c,v 1.9 2017/12/01 22:14:52 joerg Exp $");
+__KERNEL_RCSID(0, "$NetBSD: linux32_mod.c,v 1.10 2017/12/08 15:31:13 kre Exp $");
 
 #ifdef _KERNEL_OPT
 #include "opt_execfmt.h"
@@ -117,7 +117,6 @@ compat_linux32_modcmd(modcmd_t cmd, void *arg)
 
 	switch (cmd) {
 	case MODULE_CMD_INIT:
-		linux32_enabled = 0;
 		linux32_sysctl_init();
 		return 0;
 
