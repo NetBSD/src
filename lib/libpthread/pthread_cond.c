9bfe847fd3525ca473b91c0ec7a7b6f4eacb3c92
christos <christos@NetBSD.org>
2017-12-08 03:08:19 +0000

    unconst the timestamp

--- lib/libpthread/pthread_cond.c
+++ lib/libpthread/pthread_cond.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: pthread_cond.c,v 1.64 2016/07/03 14:24:58 christos Exp $	*/
+/*	$NetBSD: pthread_cond.c,v 1.65 2017/12/08 03:08:19 christos Exp $	*/
 
 /*-
  * Copyright (c) 2001, 2006, 2007, 2008 The NetBSD Foundation, Inc.
@@ -46,7 +46,7 @@
  */
 
 #include <sys/cdefs.h>
-__RCSID("$NetBSD: pthread_cond.c,v 1.64 2016/07/03 14:24:58 christos Exp $");
+__RCSID("$NetBSD: pthread_cond.c,v 1.65 2017/12/08 03:08:19 christos Exp $");
 
 #include <stdlib.h>
 #include <errno.h>
@@ -166,8 +166,9 @@ pthread_cond_timedwait(pthread_cond_t *cond, pthread_mutex_t *mutex,
 		self->pt_willpark = 0;
 		self->pt_blocking++;
 		do {
-			retval = _lwp_park(clkid, TIMER_ABSTIME, abstime,
-			    self->pt_unpark, __UNVOLATILE(&mutex->ptm_waiters),
+			retval = _lwp_park(clkid, TIMER_ABSTIME,
+			    __UNCONST(abstime), self->pt_unpark,
+			    __UNVOLATILE(&mutex->ptm_waiters),
 			    __UNVOLATILE(&mutex->ptm_waiters));
 			self->pt_unpark = 0;
 		} while (retval == -1 && errno == ESRCH);
