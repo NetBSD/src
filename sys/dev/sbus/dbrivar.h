1df8465eaefdeded9abd87bd746462a4b493c881
mrg <mrg@NetBSD.org>
2017-12-08 07:47:00 +0000

    fix audiomp bugs: - switch from tsleep/wakeup to condvar - fix locking in a bunch of places.  there were several locking   against myself issues.
    also:
    - don't let dbri_process_interrupt_buffer() loop more than once
      over the array of intrs.
    
    this fixes hangs when using audio on ss20 in -current, but does
    not make audio work.  it eventually times out with eg:
    
    dbri0: switching to control mode timed out (0 f6)
    
    and may leave a sample in the audio buffer repeating.

--- sys/dev/sbus/dbrivar.h
+++ sys/dev/sbus/dbrivar.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: dbrivar.h,v 1.13 2011/11/23 23:07:36 jmcneill Exp $	*/
+/*	$NetBSD: dbrivar.h,v 1.14 2017/12/08 07:47:00 mrg Exp $	*/
 
 /*
  * Copyright (C) 1997 Rudolf Koenig (rfkoenig@immd4.informatik.uni-erlangen.de)
@@ -169,6 +169,9 @@ struct dbri_softc {
 
 	kmutex_t	sc_lock;
 	kmutex_t	sc_intr_lock;
+#ifndef DBRI_SPIN
+	kcondvar_t	sc_cv;
+#endif
 };
 
 #define dbri_dma_off(member, elem)	\
