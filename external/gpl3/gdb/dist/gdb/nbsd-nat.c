c25a9b362e205c5290341156251f2310400a1693
christos <christos@NetBSD.org>
2017-12-10 05:36:25 +0000

    PR/52799: Martin Husemann: Correct sysctl args to find executable.

--- external/gpl3/gdb/dist/gdb/nbsd-nat.c
+++ external/gpl3/gdb/dist/gdb/nbsd-nat.c
@@ -50,9 +50,9 @@ nbsd_pid_to_exec_file (struct target_ops *self, int pid)
   int mib[4];
 
   mib[0] = CTL_KERN;
-  mib[1] = KERN_PROC;
-  mib[2] = KERN_PROC_PATHNAME;
-  mib[3] = pid;
+  mib[1] = KERN_PROC_ARGS;
+  mib[2] = pid;
+  mib[3] = KERN_PROC_PATHNAME;
   buflen = sizeof buf;
   if (sysctl (mib, 4, buf, &buflen, NULL, 0) == 0)
     return buf;
