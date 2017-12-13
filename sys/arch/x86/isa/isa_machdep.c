8f08f2f2071e0796cd73cad35a08c5486dcb6300
bouyer <bouyer@NetBSD.org>
2017-12-13 16:30:18 +0000

    Fixes for physical interrupts on Xen: - do not cast int * to intr_handle_t *, they're not the same size - legacy_irq is not always -1 for ioapic interrupts, test pic_type instead - change irq2port[] to hold (port + 1) so that 0 is an invalid value - add KASSERTs to make sure vect, port or irq values extracted from arrays are   valid (or that they are invalid before write) - for the !ioapic case, we still need to do PHYSDEVOP_ASSIGN_VECTOR and   bind_pirq_to_evtch().
    now XEN3_DOM0 boots again

--- sys/arch/x86/isa/isa_machdep.c
+++ sys/arch/x86/isa/isa_machdep.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: isa_machdep.c,v 1.37 2017/11/04 14:56:48 cherry Exp $	*/
+/*	$NetBSD: isa_machdep.c,v 1.38 2017/12/13 16:30:18 bouyer Exp $	*/
 
 /*-
  * Copyright (c) 1996, 1997, 1998 The NetBSD Foundation, Inc.
@@ -65,7 +65,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: isa_machdep.c,v 1.37 2017/11/04 14:56:48 cherry Exp $");
+__KERNEL_RCSID(0, "$NetBSD: isa_machdep.c,v 1.38 2017/12/13 16:30:18 bouyer Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -210,8 +210,8 @@ isa_intr_establish_xname(isa_chipset_tag_t ic, int irq, int type, int level,
 {
 	struct pic *pic;
 	int pin;
-#if NIOAPIC > 0
 	intr_handle_t mpih = 0;
+#if NIOAPIC > 0
 	struct ioapic_softc *ioapic = NULL;
 #endif
 
@@ -245,7 +245,7 @@ isa_intr_establish_xname(isa_chipset_tag_t ic, int irq, int type, int level,
 
 	mpih |= APIC_IRQ_LEGACY_IRQ(irq);
 
-	evtch = xen_pirq_alloc((intr_handle_t *)&mpih, type); /* XXX: legacy - xen just tosses irq back at us */
+	evtch = xen_pirq_alloc(&mpih, type); /* XXX: legacy - xen just tosses irq back at us */
 	if (evtch == -1)
 		return NULL;
 #if NIOAPIC > 0
