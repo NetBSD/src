936ca4eafb8802bf06b080e49ea0078c1ae557a5
jmcneill <jmcneill@NetBSD.org>
2017-12-13 21:37:10 +0000

    Fix regression introduced in r1.2

--- sys/arch/arm/sunxi/sunxi_thermal.c
+++ sys/arch/arm/sunxi/sunxi_thermal.c
@@ -1,4 +1,4 @@
-/* $NetBSD: sunxi_thermal.c,v 1.3 2017/11/01 05:42:32 kre Exp $ */
+/* $NetBSD: sunxi_thermal.c,v 1.4 2017/12/13 21:37:10 jmcneill Exp $ */
 
 /*-
  * Copyright (c) 2016-2017 Jared McNeill <jmcneill@invisible.ca>
@@ -31,7 +31,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: sunxi_thermal.c,v 1.3 2017/11/01 05:42:32 kre Exp $");
+__KERNEL_RCSID(0, "$NetBSD: sunxi_thermal.c,v 1.4 2017/12/13 21:37:10 jmcneill Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -552,8 +552,8 @@ sunxi_thermal_attach(device_t parent, device_t self, void *aux)
 		sc->data[i].private = i;
 		sc->data[i].units = ENVSYS_STEMP;
 		sc->data[i].state = ENVSYS_SINVALID;
-		strlcpy(sc->data[1].desc, sc->conf->sensors[i].desc,
-		    sizeof(sc->data[1].desc));
+		strlcpy(sc->data[i].desc, sc->conf->sensors[i].desc,
+		    sizeof(sc->data[i].desc));
 		sysmon_envsys_sensor_attach(sc->sme, &sc->data[i]);
 	}
 	sysmon_envsys_register(sc->sme);
