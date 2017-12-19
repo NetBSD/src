c66c323f30ae0d99bbb029f7a8dcf666a492e9ae
skrll <skrll@NetBSD.org>
2017-12-19 09:15:23 +0000

    Remove duplicate DEVMAP_* macros

--- sys/arch/arm/ti/ti_platform.c
+++ sys/arch/arm/ti/ti_platform.c
@@ -1,9 +1,9 @@
-/* $NetBSD: ti_platform.c,v 1.1 2017/10/26 01:16:32 jakllsch Exp $ */
+/* $NetBSD: ti_platform.c,v 1.2 2017/12/19 09:15:23 skrll Exp $ */
 
 #include "opt_fdt_arm.h"
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: ti_platform.c,v 1.1 2017/10/26 01:16:32 jakllsch Exp $");
+__KERNEL_RCSID(0, "$NetBSD: ti_platform.c,v 1.2 2017/12/19 09:15:23 skrll Exp $");
 
 #include <sys/param.h>
 
@@ -13,18 +13,6 @@ __KERNEL_RCSID(0, "$NetBSD: ti_platform.c,v 1.1 2017/10/26 01:16:32 jakllsch Exp
 #include <uvm/uvm_extern.h>
 
 #include <dev/ic/comreg.h>
-
-#define DEVMAP_ALIGN(a) ((a) & ~L1_S_OFFSET)
-#define DEVMAP_SIZE(s)	roundup2((s), L1_S_SIZE)
-#define DEVMAP_ENTRY(va, pa, sz)			\
-	{						\
-		.pd_va = DEVMAP_ALIGN(va),		\
-		.pd_pa = DEVMAP_ALIGN(pa),		\
-		.pd_size = DEVMAP_SIZE(sz),		\
-		.pd_prot = VM_PROT_READ|VM_PROT_WRITE,	\
-		.pd_cache = PTE_NOCACHE			\
-	}
-#define	DEVMAP_ENTRY_END	{ 0 }
 
 extern struct bus_space armv7_generic_bs_tag;
 extern struct bus_space armv7_generic_a4x_bs_tag;
