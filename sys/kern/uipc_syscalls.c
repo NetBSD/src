715d4536c39cd0da1ff928c0410f17f5f297f6f6
kamil <kamil@NetBSD.org>
2017-12-26 08:30:57 +0000

    Refactor pipe1() and correct a bug in sys_pipe2() (SYS_pipe2)
    sys_pipe2() returns two integers (values), the 2nd one is a copy of the 2nd
    file descriptor that lands in fildes[2]. This is a side effect of reusing
    the code for sys_pipe() (SYS_pipe) and not cleaning it up.
    
    The first returned value is (on success) 0.
    
    Introduced a small refactoring in pipe1() that it does not operate over
    retval[], but on an array int[2]. A user sets retval[] for pipe() when
    desired and needed.
    
    This refactoring touches compat code: netbsd32, linux, linux32.
    
    Before the changes on NetBSD/amd64:
    
    $ ktruss -i ./a.out
    [...]
     15131      1 a.out    pipe2(0x7f7fff2e62b8, 0)    = 0, 4
    [...]
    
    After the changes:
    
    $ ktruss -i ./a.out
    [...]
       782      1 a.out    pipe2(0x7f7fff97e850, 0)    = 0
    [...]
    
    There should not be a visible change for current users.
    
    Sponsored by <The NetBSD Foundation>

--- sys/kern/uipc_syscalls.c
+++ sys/kern/uipc_syscalls.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: uipc_syscalls.c,v 1.187 2017/06/20 20:34:49 christos Exp $	*/
+/*	$NetBSD: uipc_syscalls.c,v 1.188 2017/12/26 08:30:58 kamil Exp $	*/
 
 /*-
  * Copyright (c) 2008, 2009 The NetBSD Foundation, Inc.
@@ -61,7 +61,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: uipc_syscalls.c,v 1.187 2017/06/20 20:34:49 christos Exp $");
+__KERNEL_RCSID(0, "$NetBSD: uipc_syscalls.c,v 1.188 2017/12/26 08:30:58 kamil Exp $");
 
 #ifdef _KERNEL_OPT
 #include "opt_pipe.h"
@@ -1265,7 +1265,7 @@ sys_getsockopt(struct lwp *l, const struct sys_getsockopt_args *uap,
 #ifdef PIPE_SOCKETPAIR
 
 int
-pipe1(struct lwp *l, register_t *retval, int flags)
+pipe1(struct lwp *l, int *fildes, int flags)
 {
 	file_t		*rf, *wf;
 	struct socket	*rso, *wso;
@@ -1284,7 +1284,7 @@ pipe1(struct lwp *l, register_t *retval, int flags)
 	rso->so_state |= SS_ISAPIPE;
 	if ((error = fd_allocfile(&rf, &fd)) != 0)
 		goto free2;
-	retval[0] = fd;
+	fildes[0] = fd;
 	rf->f_flag = FREAD | flags;
 	rf->f_type = DTYPE_SOCKET;
 	rf->f_ops = &socketops;
@@ -1295,19 +1295,19 @@ pipe1(struct lwp *l, register_t *retval, int flags)
 	wf->f_type = DTYPE_SOCKET;
 	wf->f_ops = &socketops;
 	wf->f_socket = wso;
-	retval[1] = fd;
+	fildes[1] = fd;
 	solock(wso);
 	error = unp_connect2(wso, rso);
 	sounlock(wso);
 	if (error != 0)
 		goto free4;
-	fd_affix(p, wf, (int)retval[1]);
-	fd_affix(p, rf, (int)retval[0]);
+	fd_affix(p, wf, fildes[1]);
+	fd_affix(p, rf, fildes[0]);
 	return (0);
  free4:
-	fd_abort(p, wf, (int)retval[1]);
+	fd_abort(p, wf, fildes[1]);
  free3:
-	fd_abort(p, rf, (int)retval[0]);
+	fd_abort(p, rf, fildes[0]);
  free2:
 	(void)soclose(wso);
  free1:
