a4d0307ee17e3d4462709e91601053e239b216ad
maxv <maxv@NetBSD.org>
2018-01-05 08:04:20 +0000

    Add a __HAVE_PCPU_AREA option, enabled by default on native amd64 but not Xen.
    With this option, the CPU structures that must always be present in the
    CPU's page tables are moved on L4 slot 384, which means address
    0xffffc00000000000.
    
    A new pcpu_area structure is defined. It contains shared structures (IDT,
    LDT), and then an array of pcpu_entry structures, indexed by cpu_index(ci).
    Theoretically the LDT should be in the array, but this will be done later.
    
    During the boot procedure, cpu0 calls pmap_init_pcpu, which creates a
    page tree that is able to map the pcpu_area structure entirely. cpu0 then
    immediately maps the shared structures. Later, every CPU goes through
    cpu_pcpuarea_init, which allocates physical pages and kenters the relevant
    pcpu_entry to them. Finally, each pointer is replaced to point to pcpuarea.
    
    The point of this change is to make sure that the structures that must
    always be present in the page tables have their own L4 slot. Until now
    their L4 slot was that of pmap_kernel, and making a distinction between
    what must be mapped and what does not need to be was complicated.
    
    Even in the non-speculative-bug case this change makes some sense: there
    are several x86 instructions that leak the addresses of the CPU structures,
    and putting these structures inside pmap_kernel actually offered a way to
    compute the address of the kernel heap - which would have made ASLR on it
    plainly useless, had we implemented that.
    
    Note that, for now, pcpuarea does not contain rsp0.
    
    Unfortunately this change adds many #ifdefs, and makes the code harder to
    understand. There is also some duplication, but that will be solved later.

--- sys/arch/amd64/amd64/gdt.c
+++ sys/arch/amd64/amd64/gdt.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: gdt.c,v 1.44 2018/01/04 20:38:30 maxv Exp $	*/
+/*	$NetBSD: gdt.c,v 1.45 2018/01/05 08:04:20 maxv Exp $	*/
 
 /*
  * Copyright (c) 1996, 1997, 2009 The NetBSD Foundation, Inc.
@@ -37,7 +37,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: gdt.c,v 1.44 2018/01/04 20:38:30 maxv Exp $");
+__KERNEL_RCSID(0, "$NetBSD: gdt.c,v 1.45 2018/01/05 08:04:20 maxv Exp $");
 
 #include "opt_multiprocessor.h"
 #include "opt_xen.h"
@@ -132,8 +132,6 @@ void
 gdt_init(void)
 {
 	char *old_gdt;
-	struct vm_page *pg;
-	vaddr_t va;
 	struct cpu_info *ci = &cpu_info_primary;
 
 	/* Initialize the global values */
@@ -141,6 +139,13 @@ gdt_init(void)
 	gdt_bitmap.nslots = NSLOTS(gdt_size);
 
 	old_gdt = gdtstore;
+
+#ifdef __HAVE_PCPU_AREA
+	/* The GDT is part of the pcpuarea */
+	gdtstore = (char *)&pcpuarea->ent[cpu_index(ci)].gdt;
+#else
+	struct vm_page *pg;
+	vaddr_t va;
 
 	/* Allocate gdt_size bytes of memory. */
 	gdtstore = (char *)uvm_km_alloc(kernel_map, gdt_size, 0,
@@ -155,6 +160,7 @@ gdt_init(void)
 		    VM_PROT_READ | VM_PROT_WRITE, 0);
 	}
 	pmap_update(pmap_kernel());
+#endif
 
 	/* Copy the initial bootstrap GDT into the new area. */
 	memcpy(gdtstore, old_gdt, DYNSEL_START);
@@ -174,6 +180,9 @@ gdt_init(void)
 void
 gdt_alloc_cpu(struct cpu_info *ci)
 {
+#ifdef __HAVE_PCPU_AREA
+	ci->ci_gdt = (union descriptor *)&pcpuarea->ent[cpu_index(ci)].gdt;
+#else
 	struct vm_page *pg;
 	vaddr_t va;
 
@@ -189,6 +198,7 @@ gdt_alloc_cpu(struct cpu_info *ci)
 		    VM_PROT_READ | VM_PROT_WRITE, 0);
 	}
 	pmap_update(pmap_kernel());
+#endif
 
 	memcpy(ci->ci_gdt, gdtstore, gdt_size);
 }
