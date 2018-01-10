a6fc935c33707779d46d51453dd1741783184ce0
msaitoh <msaitoh@NetBSD.org>
2018-01-10 07:08:35 +0000

    Print Intel cpuid 7 %edx.
    Example output of cpuctl -v identify 0:
    
    +cpu0: 00000007: 00000000 000027ab 00000000 0c000000
    (snip)
    +cpu0: SEF edx 0xc000000<IBRS,STIBP>

--- usr.sbin/cpuctl/arch/i386.c
+++ usr.sbin/cpuctl/arch/i386.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: i386.c,v 1.78 2017/10/19 03:09:55 msaitoh Exp $	*/
+/*	$NetBSD: i386.c,v 1.79 2018/01/10 07:08:35 msaitoh Exp $	*/
 
 /*-
  * Copyright (c) 1999, 2000, 2001, 2006, 2007, 2008 The NetBSD Foundation, Inc.
@@ -57,7 +57,7 @@
 
 #include <sys/cdefs.h>
 #ifndef lint
-__RCSID("$NetBSD: i386.c,v 1.78 2017/10/19 03:09:55 msaitoh Exp $");
+__RCSID("$NetBSD: i386.c,v 1.79 2018/01/10 07:08:35 msaitoh Exp $");
 #endif /* not lint */
 
 #include <sys/types.h>
@@ -1900,6 +1900,12 @@ identifycpu(int fd, const char *cpuname)
 	if (cpu_vendor == CPUVENDOR_INTEL)
 		print_bits(cpuname, "features6", CPUID_SEF_FLAGS1,
 		    ci->ci_feat_val[6]);
+
+	if ((cpu_vendor == CPUVENDOR_INTEL) && (ci->ci_cpuid_level >= 7)) {
+		x86_cpuid(7, descs);
+		print_bits(cpuname, "SEF edx", CPUID_SEF_FLAGS2, descs[3]);
+	}
+
 	print_bits(cpuname, "xsave features", XCR0_FLAGS1, ci->ci_feat_val[7]);
 	print_bits(cpuname, "xsave instructions", CPUID_PES1_FLAGS,
 	    ci->ci_feat_val[8]);
