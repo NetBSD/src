759fb17c3d4f41c209e29c6d7f82de2996835c8c
maxv <maxv@NetBSD.org>
2018-01-11 09:00:04 +0000

    Declare new SVS_* variants: SVS_ENTER_NOSTACK and SVS_LEAVE_NOSTACK. Use SVS_ENTER_NOSTACK in the syscall entry point, and put it before the code that touches curlwp. (curlwp is located in the direct map.)
    Then, disable __HAVE_CPU_UAREA_ROUTINES (to be removed later). This moves
    the kernel stack into pmap_kernel(), and not the direct map. That's a
    change I've always wanted to make: because of the direct map we can't add
    a redzone on the stack, and basically, a stack overflow can go very far
    in memory without being detected (as far as erasing all of the system's
    memory).
    
    Finally, unmap the direct map from userland.

--- sys/arch/amd64/include/types.h
+++ sys/arch/amd64/include/types.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: types.h,v 1.53 2018/01/05 08:04:21 maxv Exp $	*/
+/*	$NetBSD: types.h,v 1.54 2018/01/11 09:00:04 maxv Exp $	*/
 
 /*-
  * Copyright (c) 1990 The Regents of the University of California.
@@ -106,7 +106,7 @@ typedef	unsigned char		__cpu_simple_lock_nv_t;
 #define	__HAVE_DIRECT_MAP 1
 #define	__HAVE_MM_MD_DIRECT_MAPPED_IO
 #define	__HAVE_MM_MD_DIRECT_MAPPED_PHYS
-#define	__HAVE_CPU_UAREA_ROUTINES
+/* #define	__HAVE_CPU_UAREA_ROUTINES */
 #if !defined(NO_PCI_MSI_MSIX)
 #define	__HAVE_PCI_MSI_MSIX
 #endif
