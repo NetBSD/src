78d5fdc3918dfe9fa88f1c7543092213d2514aa8
christos <christos@NetBSD.org>
2018-01-14 18:43:01 +0000

    import latest mDNSResponder

--- external/apache2/mDNSResponder/dist/mDNSCore/uDNS.h
+++ external/apache2/mDNSResponder/dist/mDNSCore/uDNS.h
@@ -1,13 +1,13 @@
 /* -*- Mode: C; tab-width: 4 -*-
  *
- * Copyright (c) 2002-2003 Apple Computer, Inc. All rights reserved.
+ * Copyright (c) 2002-2013 Apple Computer, Inc. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -21,19 +21,24 @@
 #include "mDNSEmbeddedAPI.h"
 #include "DNSCommon.h"
 
-#ifdef	__cplusplus
-	extern "C" {
+#ifdef  __cplusplus
+extern "C" {
 #endif
 
 #define RESTART_GOODBYE_DELAY    (6 * mDNSPlatformOneSecond) // delay after restarting LLQ before nuking previous known answers (avoids flutter if we restart before we have networking up)
 #define INIT_UCAST_POLL_INTERVAL (3 * mDNSPlatformOneSecond) // this interval is used after send failures on network transitions
-	                                                         // which typically heal quickly, so we start agressively and exponentially back off
+                                                             // which typically heal quickly, so we start agressively and exponentially back off
 #define MAX_UCAST_POLL_INTERVAL (60 * 60 * mDNSPlatformOneSecond)
 //#define MAX_UCAST_POLL_INTERVAL (1 * 60 * mDNSPlatformOneSecond)
 #define LLQ_POLL_INTERVAL       (15 * 60 * mDNSPlatformOneSecond) // Polling interval for zones w/ an advertised LLQ port (ie not static zones) if LLQ fails due to NAT, etc.
 #define RESPONSE_WINDOW (60 * mDNSPlatformOneSecond)         // require server responses within one minute of request
-#define MAX_UCAST_UNANSWERED_QUERIES 2                       // the number of unanswered queries from any one uDNS server before trying another server
-#define DNSSERVER_PENALTY_TIME (60 * mDNSPlatformOneSecond) // number of seconds for which new questions don't pick this server
+#define MAX_DNSSEC_UNANSWERED_QUERIES 1                      // number of unanswered queries from any one uDNS server before turning off DNSSEC Validation
+#define MAX_UCAST_UNANSWERED_QUERIES 2                       // number of unanswered queries from any one uDNS server before trying another server
+#define DNSSERVER_PENALTY_TIME (60 * mDNSPlatformOneSecond)  // number of seconds for which new questions don't pick this server
+
+// On some interfaces, we want to delay the first retransmission to a minimum of 2 seconds
+// rather than the default (1 second).
+#define MIN_UCAST_RETRANS_TIMEOUT (2 * mDNSPlatformOneSecond)
 
 #define DEFAULT_UPDATE_LEASE 7200
 
@@ -41,22 +46,35 @@
 #define QuestionIntervalStep2 (QuestionIntervalStep*QuestionIntervalStep)
 #define QuestionIntervalStep3 (QuestionIntervalStep*QuestionIntervalStep*QuestionIntervalStep)
 #define InitialQuestionInterval ((mDNSPlatformOneSecond + QuestionIntervalStep-1) / QuestionIntervalStep)
+#define MaxQuestionInterval         (3600 * mDNSPlatformOneSecond)
+
+// just move to MaxQuestionInterval once over this threshold
+#define QuestionIntervalThreshold   (QuestionIntervalStep3 * mDNSPlatformOneSecond)
 
 // For Unicast record registrations, we initialize the interval to 1 second. When we send any query for
 // the record registration e.g., GetZoneData, we always back off by QuestionIntervalStep
 // so that the first retry does not happen until 3 seconds which should be enough for TCP/TLS to be done.
 #define INIT_RECORD_REG_INTERVAL (1 * mDNSPlatformOneSecond)
-#define MAX_RECORD_REG_INTERVAL	(15 * 60 * mDNSPlatformOneSecond)
-#define MERGE_DELAY_TIME	(1 * mDNSPlatformOneSecond)
+#define MAX_RECORD_REG_INTERVAL (15 * 60 * mDNSPlatformOneSecond)
+#define MERGE_DELAY_TIME    (1 * mDNSPlatformOneSecond)
 
-// If we are refreshing, we do it at least 5 times with a min update frequency of 
+// If we are refreshing, we do it at least 5 times with a min update frequency of
 // 5 minutes
-#define MAX_UPDATE_REFRESH_COUNT	5
-#define MIN_UPDATE_REFRESH_TIME		(5 * 60 * mDNSPlatformOneSecond)
+#define MAX_UPDATE_REFRESH_COUNT    5
+#define MIN_UPDATE_REFRESH_TIME     (5 * 60 * mDNSPlatformOneSecond)
 
 // For questions that use kDNSServiceFlagsTimeout and we don't have a matching resolver e.g., no dns servers,
 // then use the default value of 30 seconds
-#define DEFAULT_UDNS_TIMEOUT	30 // in seconds
+#define DEFAULT_UDNS_TIMEOUT    30 // in seconds
+
+// For questions that are validating responses (q->ValidatingResponse == 1), use 10 seconds
+// which accomodates two DNS servers and two queries per DNS server.
+#define DEFAULT_UDNSSEC_TIMEOUT    10 // in seconds
+
+// If we are sending queries with EDNS0/DO option and we have no indications that the server
+// is DNSSEC aware and we have already reached MAX_DNSSEC_RETRANSMISSIONS, we disable
+// validation (for optional case only) for any questions that uses this server
+#define MAX_DNSSEC_RETRANSMISSIONS 3
 
 // Entry points into unicast-specific routines
 
@@ -75,7 +93,6 @@ extern void SleepRecordRegistrations(mDNS *m);
 extern mStatus uDNS_UpdateRecord(mDNS *m, AuthRecord *rr);
 
 extern void SetNextQueryTime(mDNS *const m, const DNSQuestion *const q);
-extern CacheGroup *CacheGroupForName(const mDNS *const m, const mDNSu32 slot, const mDNSu32 namehash, const domainname *const name);
 extern mStatus mDNS_Register_internal(mDNS *const m, AuthRecord *const rr);
 extern mStatus mDNS_Deregister_internal(mDNS *const m, AuthRecord *const rr, mDNS_Dereg_type drt);
 extern mStatus mDNS_StartQuery_internal(mDNS *const m, DNSQuestion *const question);
@@ -89,7 +106,7 @@ extern void uDNS_CheckCurrentQuestion(mDNS *const m);
 
 // integer fields of msg header must be in HOST byte order before calling this routine
 extern void uDNS_ReceiveMsg(mDNS *const m, DNSMessage *const msg, const mDNSu8 *const end,
-	const mDNSAddr *const srcaddr, const mDNSIPPort srcport);
+                            const mDNSAddr *const srcaddr, const mDNSIPPort srcport);
 
 extern void uDNS_Tasks(mDNS *const m);
 extern void UpdateAllSRVRecords(mDNS *m);
@@ -97,23 +114,25 @@ extern void CheckNATMappings(mDNS *m);
 
 extern mStatus         uDNS_SetupDNSConfig(mDNS *const m);
 
-// uDNS_SetupSearchDomains by default adds search domains. It also can be called with one or
-// more values for "action" which does the following:
-//
-// -UDNS_START_WAB_QUERY - start Wide Area Bonjour (domain enumeration) queries
+// uDNS_SetupWABQueries reads search domains from the platform layer and starts the Wide Area Bonjour
+// (WAB) domain enumeration queries if necessary.
 
-#define UDNS_START_WAB_QUERY    0x00000001
+#define UDNS_WAB_BROWSE_QUERY    0x00000001 // Browse queries (b, db)
+#define UDNS_WAB_LBROWSE_QUERY   0x00000002 // Browse queries (lb)
+#define UDNS_WAB_REG_QUERY       0x00000004 // Registration queries (r and dr)
 
-extern mStatus         uDNS_SetupSearchDomains(mDNS *const m, int action);
+extern void uDNS_SetupWABQueries(mDNS *const m);
+extern void uDNS_StartWABQueries(mDNS *const m, int queryType);
+extern void uDNS_StopWABQueries(mDNS *const m, int queryType);
 extern domainname      *uDNS_GetNextSearchDomain(mDNS *const m, mDNSInterfaceID InterfaceID, mDNSs8 *searchIndex, mDNSBool ignoreDotLocal);
 
 typedef enum
-	{
-	uDNS_LLQ_Not = 0,	// Normal uDNS answer: Flush any stale records from cache, and respect record TTL
-	uDNS_LLQ_Ignore,	// LLQ initial challenge packet: ignore -- has no useful records for us
-	uDNS_LLQ_Entire,	// LLQ initial set of answers: Flush any stale records from cache, but assume TTL is 2 x LLQ refresh interval
-	uDNS_LLQ_Events		// LLQ event packet: don't flush cache; assume TTL is 2 x LLQ refresh interval
-	} uDNS_LLQType;
+{
+    uDNS_LLQ_Not = 0,   // Normal uDNS answer: Flush any stale records from cache, and respect record TTL
+    uDNS_LLQ_Ignore,    // LLQ initial challenge packet: ignore -- has no useful records for us
+    uDNS_LLQ_Entire,    // LLQ initial set of answers: Flush any stale records from cache, but assume TTL is 2 x LLQ refresh interval
+    uDNS_LLQ_Events     // LLQ event packet: don't flush cache; assume TTL is 2 x LLQ refresh interval
+} uDNS_LLQType;
 
 extern uDNS_LLQType    uDNS_recvLLQResponse(mDNS *const m, const DNSMessage *const msg, const mDNSu8 *const end, const mDNSAddr *const srcaddr, const mDNSIPPort srcport, DNSQuestion **matchQuestion);
 extern DomainAuthInfo *GetAuthInfoForName_internal(mDNS *m, const domainname *const name);
@@ -121,12 +140,12 @@ extern DomainAuthInfo *GetAuthInfoForQuestion(mDNS *m, const DNSQuestion *const 
 extern void DisposeTCPConn(struct tcpInfo_t *tcp);
 
 // NAT traversal
-extern void	uDNS_ReceiveNATPMPPacket(mDNS *m, const mDNSInterfaceID InterfaceID, mDNSu8 *pkt, mDNSu16 len);	// Called for each received NAT-PMP packet
-extern void	natTraversalHandleAddressReply(mDNS *const m, mDNSu16 err, mDNSv4Addr ExtAddr);
-extern void	natTraversalHandlePortMapReply(mDNS *const m, NATTraversalInfo *n, const mDNSInterfaceID InterfaceID, mDNSu16 err, mDNSIPPort extport, mDNSu32 lease);
+extern void uDNS_ReceiveNATPacket(mDNS *m, const mDNSInterfaceID InterfaceID, mDNSu8 *pkt, mDNSu16 len); // Called for each received PCP or NAT-PMP packet
+extern void natTraversalHandleAddressReply(mDNS *const m, mDNSu16 err, mDNSv4Addr ExtAddr);
+extern void natTraversalHandlePortMapReply(mDNS *const m, NATTraversalInfo *n, const mDNSInterfaceID InterfaceID, mDNSu16 err, mDNSIPPort extport, mDNSu32 lease, NATTProtocol protocol);
 
-#ifdef	__cplusplus
-	}
+#ifdef  __cplusplus
+}
 #endif
 
 #endif // __UDNS_H_
