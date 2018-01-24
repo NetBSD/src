0e2fef9d467604ee5568f25bd75712f7fb1fc3d1
maxv <maxv@NetBSD.org>
2018-01-24 13:13:11 +0000

    Revert a part of rev1.49 (six months ago). The pointer given to memcpy was correct.
    Discussed with Christos and Ryota.

--- sys/netipsec/xform_ah.c
+++ sys/netipsec/xform_ah.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: xform_ah.c,v 1.74 2017/10/03 08:56:52 ozaki-r Exp $	*/
+/*	$NetBSD: xform_ah.c,v 1.75 2018/01/24 13:13:11 maxv Exp $	*/
 /*	$FreeBSD: src/sys/netipsec/xform_ah.c,v 1.1.4.1 2003/01/24 05:11:36 sam Exp $	*/
 /*	$OpenBSD: ip_ah.c,v 1.63 2001/06/26 06:18:58 angelos Exp $ */
 /*
@@ -39,7 +39,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: xform_ah.c,v 1.74 2017/10/03 08:56:52 ozaki-r Exp $");
+__KERNEL_RCSID(0, "$NetBSD: xform_ah.c,v 1.75 2018/01/24 13:13:11 maxv Exp $");
 
 #if defined(_KERNEL_OPT)
 #include "opt_inet.h"
@@ -419,7 +419,7 @@ ah_massage_headers(struct mbuf **m0, int proto, int skip, int alg, int out)
 
 				/* Zeroize all other options. */
 				count = ptr[off + 1];
-				memcpy(ptr, ipseczeroes, count);
+				memcpy(ptr + off, ipseczeroes, count);
 				off += count;
 				break;
 			}
