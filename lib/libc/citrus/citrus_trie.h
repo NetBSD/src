17822752834291f88ccccfa26afa42dffd70bf0c
perseant <perseant@NetBSD.org>
2018-01-29 22:08:59 +0000

    Fix issues with trie implementation.  Now passes the locale tests when __STDC_ISO_10646__ is defined, with the exception of tests requiring LC_COLLATE support.

--- lib/libc/citrus/citrus_trie.h
+++ lib/libc/citrus/citrus_trie.h
@@ -11,7 +11,8 @@
  */
 #ifndef VALUE_TYPE
 # define VALUE_TYPE int32_t
-# define VALUE_TYPE_STRING "int32_t"
+# define STRINGIZE(s) #s
+# define VALUE_TYPE_STRING STRINGIZE(VALUE_TYPE) /* "int32_t" */
 #endif
 #define INVALID_VALUE 0xffffffff
 
@@ -32,9 +33,9 @@ typedef struct citrus_trie_header {
 typedef struct citrus_trie_node {
 	size_t tr_len;
 	union citrus_trie_node_union {
-		struct citrus_trie_node *u_child;
-		VALUE_TYPE      u_value;
-	} *tr_u;
+		struct citrus_trie_node **u_child;
+		VALUE_TYPE      *u_value;
+	} tr_u;
 } *citrus_trie_node_t;
 
 #ifdef DEBUG_TRIE
@@ -49,11 +50,11 @@ int citrus_trie_node_insert(citrus_trie_header_t, citrus_trie_node_t, size_t, ci
 int citrus_trie_insert(citrus_trie_header_t, citrus_trie_key_t, VALUE_TYPE);
 VALUE_TYPE citrus_trie_node_lookup(citrus_trie_header_t, citrus_trie_node_t, size_t, citrus_trie_key_t);
 VALUE_TYPE citrus_trie_lookup(citrus_trie_header_t, citrus_trie_key_t);
-citrus_trie_header_t citrus_trie_create_from_flat(VALUE_TYPE *, size_t, int);
+citrus_trie_header_t citrus_trie_create_from_flat(VALUE_TYPE *, size_t, size_t);
 void citrus_trie_node_destroy(citrus_trie_node_t, size_t);
 void citrus_trie_destroy(citrus_trie_header_t);
 
-void citrus_trie_init(citrus_trie_header_t, citrus_trie_node_t *, VALUE_TYPE *);
+void citrus_trie_init(citrus_trie_header_t, size_t *, VALUE_TYPE *);
 void citrus_trie_dump(citrus_trie_header_t, char *, char *, int);
 citrus_trie_header_t citrus_trie_load(char *);
 #endif /* CITRUS_TRIE_H_ */
