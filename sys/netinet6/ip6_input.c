6b2b62130c4595003ab1517154876f8fdeeee092
maxv <maxv@NetBSD.org>
2018-01-30 14:49:25 +0000

    Fix a buffer overflow in ip6_get_prevhdr. Doing
    	mtod(m, char *) + len
    
    is wrong, an option is allowed to be located in another mbuf of the chain.
    If the offset of an option within the chain is bigger than the length of
    the first mbuf in that chain, we are reading/writing one byte of packet-
    controlled data beyond the end of the first mbuf.
    
    The length of this first mbuf depends on the layout the network driver
    chose. In the most difficult case, it will allocate a 2KB cluster, which
    is bigger than the Ethernet MTU.
    
    But there is at least one way of exploiting this case: by sending a
    special combination of nested IPv6 fragments, the packet can control a
    good bunch of 'len'. By luck, the memory pool containing clusters does not
    embed the pool header in front of the items, so it is not straightforward
    to predict what is located at 'mtod(m, char *) + len'.
    
    However, by sending offending fragments in a loop, it is possible to
    crash the kernel - at some point we will hit important data structures.
    
    As far as I can tell, PF protects against this difficult case, because
    it kicks nested fragments. NPF does not protect against this. IPF I don't
    know.
    
    Then there are the more easy cases, if the MTU is bigger than a cluster,
    or if the network driver did not allocate a cluster, or perhaps if the
    fragments are received via a tunnel; I haven't investigated these cases.
    
    Change ip6_get_prevhdr so that it returns an offset in the chain, and
    always use IP6_EXTHDR_GET to get a writable pointer. IP6_EXTHDR_GET
    leaves M_PKTHDR untouched.
    
    This place is still fragile.

--- sys/netinet6/ip6_input.c
+++ sys/netinet6/ip6_input.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: ip6_input.c,v 1.186 2018/01/29 10:57:13 maxv Exp $	*/
+/*	$NetBSD: ip6_input.c,v 1.187 2018/01/30 14:49:25 maxv Exp $	*/
 /*	$KAME: ip6_input.c,v 1.188 2001/03/29 05:34:31 itojun Exp $	*/
 
 /*
@@ -62,7 +62,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: ip6_input.c,v 1.186 2018/01/29 10:57:13 maxv Exp $");
+__KERNEL_RCSID(0, "$NetBSD: ip6_input.c,v 1.187 2018/01/30 14:49:25 maxv Exp $");
 
 #ifdef _KERNEL_OPT
 #include "opt_gateway.h"
@@ -1351,50 +1351,44 @@ ip6_pullexthdr(struct mbuf *m, size_t off, int nxt)
 }
 
 /*
- * Get pointer to the previous header followed by the header
+ * Get offset to the previous header followed by the header
  * currently processed.
- * XXX: This function supposes that
- *	M includes all headers,
- *	the next header field and the header length field of each header
- *	are valid, and
- *	the sum of each header length equals to OFF.
- * Because of these assumptions, this function must be called very
- * carefully. Moreover, it will not be used in the near future when
- * we develop `neater' mechanism to process extension headers.
  */
-u_int8_t *
+int
 ip6_get_prevhdr(struct mbuf *m, int off)
 {
 	struct ip6_hdr *ip6 = mtod(m, struct ip6_hdr *);
 
-	if (off == sizeof(struct ip6_hdr))
-		return (&ip6->ip6_nxt);
-	else {
-		int len, nxt;
-		struct ip6_ext *ip6e = NULL;
+	if (off == sizeof(struct ip6_hdr)) {
+		return offsetof(struct ip6_hdr, ip6_nxt);
+	} else if (off < sizeof(struct ip6_hdr)) {
+		panic("%s: off < sizeof(struct ip6_hdr)", __func__);
+	} else {
+		int len, nlen, nxt;
+		struct ip6_ext ip6e;
 
 		nxt = ip6->ip6_nxt;
 		len = sizeof(struct ip6_hdr);
+		nlen = 0;
 		while (len < off) {
-			ip6e = (struct ip6_ext *)(mtod(m, char *) + len);
+			m_copydata(m, len, sizeof(ip6e), &ip6e);
 
 			switch (nxt) {
 			case IPPROTO_FRAGMENT:
-				len += sizeof(struct ip6_frag);
+				nlen = sizeof(struct ip6_frag);
 				break;
 			case IPPROTO_AH:
-				len += (ip6e->ip6e_len + 2) << 2;
+				nlen = (ip6e.ip6e_len + 2) << 2;
 				break;
 			default:
-				len += (ip6e->ip6e_len + 1) << 3;
+				nlen = (ip6e.ip6e_len + 1) << 3;
 				break;
 			}
-			nxt = ip6e->ip6e_nxt;
+			len += nlen;
+			nxt = ip6e.ip6e_nxt;
 		}
-		if (ip6e)
-			return (&ip6e->ip6e_nxt);
-		else
-			return NULL;
+
+		return (len - nlen);
 	}
 }
 
