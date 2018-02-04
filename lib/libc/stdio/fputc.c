f11833ef09da3ca7ede697eeb2262c0349a30ad5
mrg <mrg@NetBSD.org>
2018-02-04 01:13:45 +0000

    fixes for GCC 6:
    - -Wstrict-prototypes is not available for C++, so don't try to
      ignore it for C++.
    - remove many _DIAGASSERT() checks against not NULL for functions
      with arguments with nonnull attributes.  in two cases, leave
      code behind that should set defaults to "(null)".
    - use -Wno-error=frame-address for i386 mcount, as it seems valid
      to assume the caller will have a frame.fair

--- lib/libc/stdio/fputc.c
+++ lib/libc/stdio/fputc.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: fputc.c,v 1.13 2012/03/15 18:22:30 christos Exp $	*/
+/*	$NetBSD: fputc.c,v 1.14 2018/02/04 01:13:45 mrg Exp $	*/
 
 /*-
  * Copyright (c) 1990, 1993
@@ -37,7 +37,7 @@
 #if 0
 static char sccsid[] = "@(#)fputc.c	8.1 (Berkeley) 6/4/93";
 #else
-__RCSID("$NetBSD: fputc.c,v 1.13 2012/03/15 18:22:30 christos Exp $");
+__RCSID("$NetBSD: fputc.c,v 1.14 2018/02/04 01:13:45 mrg Exp $");
 #endif
 #endif /* LIBC_SCCS and not lint */
 
@@ -51,8 +51,6 @@ int
 fputc(int c, FILE *fp)
 {
 	int r;
-
-	_DIAGASSERT(fp != NULL);
 
 	FLOCKFILE(fp); 
 	r = __sputc(c, fp);
