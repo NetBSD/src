ac3edbdc20b9e995e31234bc31fbec5013f72fa6
mrg <mrg@NetBSD.org>
2018-02-05 06:22:27 +0000

    mark GCC 6.4 has nb1 20180203

--- tools/gcc/gcc-version.mk
+++ tools/gcc/gcc-version.mk
@@ -1,5 +1,9 @@
-#	$NetBSD: gcc-version.mk,v 1.8 2017/11/13 08:47:24 mrg Exp $
+#	$NetBSD: gcc-version.mk,v 1.9 2018/02/05 06:22:27 mrg Exp $
 
 # common location for tools and native build
 
+.if ${HAVE_GCC} == 5
 NETBSD_GCC_VERSION=nb1 20171112
+.elif ${HAVE_GCC} == 6
+NETBSD_GCC_VERSION=nb1 20180203
+.endif
