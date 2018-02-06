bef23c839d16ef9f6e28a5296600c3dccf675c79
martin <martin@NetBSD.org>
2018-02-06 09:05:27 +0000

    Additionally pull up rev 1.3 of sys/arch/x86/pci/amdsmn.c, requested by pgoyette in ticket #524:
    rescan amdsmnbus instead of amdsmn (fixes panic)

--- sys/arch/x86/pci/amdsmn.c
+++ sys/arch/x86/pci/amdsmn.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: amdsmn.c,v 1.3.2.2 2018/02/05 13:06:55 martin Exp $	*/
+/*	$NetBSD: amdsmn.c,v 1.3.2.3 2018/02/06 09:05:27 martin Exp $	*/
 
 /*-
  * Copyright (c) 2017 Conrad Meyer <cem@FreeBSD.org>
@@ -29,7 +29,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: amdsmn.c,v 1.3.2.2 2018/02/05 13:06:55 martin Exp $ ");
+__KERNEL_RCSID(0, "$NetBSD: amdsmn.c,v 1.3.2.3 2018/02/06 09:05:27 martin Exp $ ");
 
 /*
  * Driver for the AMD Family 17h CPU System Management Network.
@@ -101,7 +101,7 @@ amdsmn_attach(device_t parent, device_t self, void *aux)
 	sc->pc = pa->pa_pc;
 	sc->pcitag = pa->pa_tag;
 	aprint_normal(": AMD Family 17h System Management Network\n");
-	amdsmn_rescan(self, "amdsmn", &flags);
+	amdsmn_rescan(self, "amdsmnbus", &flags);
 }
 
 static int
