72f58b5615470aa8420523624dd38423f7bbc9df
christos <christos@NetBSD.org>
2018-02-06 01:57:23 +0000

    merge conflicts

--- external/bsd/openldap/dist/servers/slapd/overlays/deref.c
+++ external/bsd/openldap/dist/servers/slapd/overlays/deref.c
@@ -1,10 +1,10 @@
-/*	$NetBSD: deref.c,v 1.3 2017/02/09 01:53:51 christos Exp $	*/
+/*	$NetBSD: deref.c,v 1.4 2018/02/06 01:57:23 christos Exp $	*/
 
 /* deref.c - dereference overlay */
 /* $OpenLDAP$ */
 /* This work is part of OpenLDAP Software <http://www.openldap.org/>.
  *
- * Copyright 1998-2016 The OpenLDAP Foundation.
+ * Copyright 1998-2017 The OpenLDAP Foundation.
  * Portions Copyright 2008 Pierangelo Masarati.
  * All rights reserved.
  *
@@ -22,7 +22,7 @@
  */
 
 #include <sys/cdefs.h>
-__RCSID("$NetBSD: deref.c,v 1.3 2017/02/09 01:53:51 christos Exp $");
+__RCSID("$NetBSD: deref.c,v 1.4 2018/02/06 01:57:23 christos Exp $");
 
 #include "portable.h"
 
