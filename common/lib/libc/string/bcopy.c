6ab93c1c3f0aeb1800b13a58715c8d698a7c1ea3
martin <martin@NetBSD.org>
2018-02-12 11:14:15 +0000

    Complete previous by complteley removing the _DIAGASSERT from memmove - the accidental left over from previous fired on all legitimate calls and caused PR bin/52986 and PR lib/52987.

--- common/lib/libc/string/bcopy.c
+++ common/lib/libc/string/bcopy.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: bcopy.c,v 1.12 2018/02/04 20:22:17 mrg Exp $	*/
+/*	$NetBSD: bcopy.c,v 1.13 2018/02/12 11:14:15 martin Exp $	*/
 
 /*-
  * Copyright (c) 1990, 1993
@@ -37,7 +37,7 @@
 #if 0
 static char sccsid[] = "@(#)bcopy.c	8.1 (Berkeley) 6/4/93";
 #else
-__RCSID("$NetBSD: bcopy.c,v 1.12 2018/02/04 20:22:17 mrg Exp $");
+__RCSID("$NetBSD: bcopy.c,v 1.13 2018/02/12 11:14:15 martin Exp $");
 #endif
 #endif /* LIBC_SCCS and not lint */
 
@@ -87,10 +87,6 @@ bcopy(const void *src0, void *dst0, size_t length)
 	const char *src = src0;
 	size_t t;
 	unsigned long u;
-
-#if !defined(_KERNEL)
-	_DIAGASSERT(length == 0);
-#endif
 
 	if (length == 0 || dst == src)		/* nothing to do */
 		goto done;
