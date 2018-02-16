3b70cd0d9a584acd1f8908628e0d87d20c5587d2
christos <christos@NetBSD.org>
2018-02-16 16:23:15 +0000

    add getpeereid tests for non-unix sockets, returns garbage...

--- tests/net/net/t_unix.c
+++ tests/net/net/t_unix.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: t_unix.c,v 1.12 2018/02/16 16:08:22 christos Exp $	*/
+/*	$NetBSD: t_unix.c,v 1.13 2018/02/16 16:23:15 christos Exp $	*/
 
 /*-
  * Copyright (c) 2011 The NetBSD Foundation, Inc.
@@ -38,7 +38,7 @@
 
 #include <sys/cdefs.h>
 #ifdef __RCSID
-__RCSID("$Id: t_unix.c,v 1.12 2018/02/16 16:08:22 christos Exp $");
+__RCSID("$Id: t_unix.c,v 1.13 2018/02/16 16:23:15 christos Exp $");
 #else
 #define getprogname() argv[0]
 #endif
@@ -60,20 +60,7 @@ __RCSID("$Id: t_unix.c,v 1.12 2018/02/16 16:08:22 christos Exp $");
 #include <unistd.h>
 #include <stdbool.h>
 
-#ifdef TEST
-#define FAIL(msg, ...)	err(EXIT_FAILURE, msg, ## __VA_ARGS__)
-#define CHECK_EQUAL(a, b) if ((a) != (b)) \
-    errx(EXIT_FAILURE, # a "(%ju) != " # b "(%ju)", (uintmax_t)(a), (uintmax_t)((b));
-#else
-
-#include <atf-c.h>
-#define FAIL(msg, ...)	\
-	do { \
-		ATF_CHECK_MSG(0, msg, ## __VA_ARGS__); \
-		goto fail; \
-	} while (/*CONSTCOND*/0)
-#define CHECK_EQUAL(a, b) ATF_CHECK_EQ(a, b) 
-#endif
+#include "test.h"
 
 #define OF offsetof(struct sockaddr_un, sun_path)
 
@@ -192,15 +179,15 @@ test(bool closeit, size_t len)
 
 	if (getpeereid(clnt, &euid, &egid) == -1)
 		FAIL("getpeereid(clnt)");
-	CHECK_EQUAL(euid, geteuid());
-	CHECK_EQUAL(egid, getegid());
+	CHECK_EQUAL(euid, geteuid(), "client");
+	CHECK_EQUAL(egid, getegid(), "client");
 
 #if 0
 	/* This is not symmetric? */
 	if (getpeereid(srvr, &euid, &egid) == -1)
 		FAIL("getpeereid(srvr)");
-	CHECK_EQUAL(euid, geteuid());
-	CHECK_EQUAL(egid, getegid());
+	CHECK_EQUAL(euid, geteuid(), "server");
+	CHECK_EQUAL(egid, getegid(), "server");
 #endif
 
 	if (closeit) {
