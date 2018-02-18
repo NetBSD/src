45b68d6442e25dc3e785df3db800409a1048c869
christos <christos@NetBSD.org>
2018-02-18 21:51:28 +0000

    PR/53036: Alexander Nasonov: 'block user' in pf's ruleset panics 8.0_BETA Check for NULL.

--- sys/dist/pf/net/pf.c
+++ sys/dist/pf/net/pf.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: pf.c,v 1.78 2018/02/09 21:25:04 maxv Exp $	*/
+/*	$NetBSD: pf.c,v 1.79 2018/02/18 21:51:28 christos Exp $	*/
 /*	$OpenBSD: pf.c,v 1.552.2.1 2007/11/27 16:37:57 henning Exp $ */
 
 /*
@@ -37,7 +37,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: pf.c,v 1.78 2018/02/09 21:25:04 maxv Exp $");
+__KERNEL_RCSID(0, "$NetBSD: pf.c,v 1.79 2018/02/18 21:51:28 christos Exp $");
 
 #include "pflog.h"
 
@@ -2846,6 +2846,13 @@ pf_socket_lookup(int direction, struct pf_pdesc *pd)
 		so = in6p->in6p_socket;
 		break;
 #endif /* INET6 */
+	}
+	if (so == NULL)
+		return -1;
+	if (so->so_cred == NULL) {
+		DPFPRINTF(PF_DEBUG_URGENT,
+		    ("%s: so->so_cred == NULL so=%p\n", __func__, so));
+		return -1;
 	}
 	pd->lookup.uid = kauth_cred_geteuid(so->so_cred);
 	pd->lookup.gid = kauth_cred_getegid(so->so_cred);
