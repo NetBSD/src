20787f4eb88ee6e79583f90fb4da93c2a410e894
msaitoh <msaitoh@NetBSD.org>
2018-02-22 05:09:56 +0000

    Add C620 devices.

--- sys/dev/pci/ichsmb.c
+++ sys/dev/pci/ichsmb.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: ichsmb.c,v 1.51 2017/08/17 01:24:09 msaitoh Exp $	*/
+/*	$NetBSD: ichsmb.c,v 1.52 2018/02/22 05:09:56 msaitoh Exp $	*/
 /*	$OpenBSD: ichiic.c,v 1.18 2007/05/03 09:36:26 dlg Exp $	*/
 
 /*
@@ -22,7 +22,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: ichsmb.c,v 1.51 2017/08/17 01:24:09 msaitoh Exp $");
+__KERNEL_RCSID(0, "$NetBSD: ichsmb.c,v 1.52 2018/02/22 05:09:56 msaitoh Exp $");
 
 #include <sys/param.h>
 #include <sys/device.h>
@@ -126,6 +126,8 @@ ichsmb_match(device_t parent, cfdata_t match, void *aux)
 		case PCI_PRODUCT_INTEL_C600_SMB_1:
 		case PCI_PRODUCT_INTEL_C600_SMB_2:
 		case PCI_PRODUCT_INTEL_C610_SMB:
+		case PCI_PRODUCT_INTEL_C620_SMB:
+		case PCI_PRODUCT_INTEL_C620_SMB_S:
 		case PCI_PRODUCT_INTEL_EP80579_SMB:
 		case PCI_PRODUCT_INTEL_DH89XXCC_SMB:
 		case PCI_PRODUCT_INTEL_DH89XXCL_SMB:
