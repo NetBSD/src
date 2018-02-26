e03a704ec3acf5f283b2d4c688c90deb1672e560
martin <martin@NetBSD.org>
2018-02-26 13:45:11 +0000

    Pull up following revision(s) (requested by msaitoh in ticket #590): 	sys/dev/sbus/be.c: revision 1.86 spl leak, found by Mootja a long time ago

--- sys/dev/sbus/be.c
+++ sys/dev/sbus/be.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: be.c,v 1.85 2016/12/15 09:28:06 ozaki-r Exp $	*/
+/*	$NetBSD: be.c,v 1.85.8.1 2018/02/26 13:45:11 martin Exp $	*/
 
 /*-
  * Copyright (c) 1999 The NetBSD Foundation, Inc.
@@ -57,7 +57,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: be.c,v 1.85 2016/12/15 09:28:06 ozaki-r Exp $");
+__KERNEL_RCSID(0, "$NetBSD: be.c,v 1.85.8.1 2018/02/26 13:45:11 martin Exp $");
 
 #include "opt_ddb.h"
 #include "opt_inet.h"
@@ -1119,6 +1119,7 @@ beinit(struct ifnet *ifp)
 
 	callout_reset(&sc->sc_tick_ch, hz, be_tick, sc);
 
+	splx(s);
 	return 0;
 out:
 	splx(s);
