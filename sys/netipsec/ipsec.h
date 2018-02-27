faad9849ecdbe4a22ddb73ded3f460389fd6d438
maxv <maxv@NetBSD.org>
2018-02-27 15:01:30 +0000

    Dedup: merge ipsec4_set_policy and ipsec6_set_policy. The content of the original ipsec_set_policy function is inlined into the new one.

--- sys/netipsec/ipsec.h
+++ sys/netipsec/ipsec.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: ipsec.h,v 1.70 2018/02/27 14:44:10 maxv Exp $	*/
+/*	$NetBSD: ipsec.h,v 1.71 2018/02/27 15:01:30 maxv Exp $	*/
 /*	$FreeBSD: /usr/local/www/cvsroot/FreeBSD/src/sys/netipsec/ipsec.h,v 1.2.4.2 2004/02/14 22:23:23 bms Exp $	*/
 /*	$KAME: ipsec.h,v 1.53 2001/11/20 08:32:38 itojun Exp $	*/
 
@@ -291,7 +291,7 @@ int ipsec_init_policy(struct socket *so, struct inpcbpolicy **);
 int ipsec_copy_policy(const struct inpcbpolicy *, struct inpcbpolicy *);
 u_int ipsec_get_reqlevel(const struct ipsecrequest *);
 
-int ipsec4_set_policy(struct inpcb *, int, const void *, size_t, kauth_cred_t);
+int ipsec_set_policy(void *, int, const void *, size_t, kauth_cred_t);
 int ipsec_get_policy(void *, const void *, size_t, struct mbuf **);
 int ipsec_delete_pcbpolicy(void *);
 int ipsec_in_reject(struct mbuf *, void *);
