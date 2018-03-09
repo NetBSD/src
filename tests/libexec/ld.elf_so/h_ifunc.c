e13dec0e4eeff35b391d3f4c0984cc048c9050c4
joerg <joerg@NetBSD.org>
2018-03-09 20:15:03 +0000

    Avoid casting fun by switching ifunc helper functions to return long long. Dead beef is too useful to switch to a different constant.

--- tests/libexec/ld.elf_so/h_ifunc.c
+++ tests/libexec/ld.elf_so/h_ifunc.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: h_ifunc.c,v 1.1 2014/08/25 20:40:53 joerg Exp $	*/
+/*	$NetBSD: h_ifunc.c,v 1.2 2018/03/09 20:15:03 joerg Exp $	*/
 
 /*-
  * Copyright (c) 2014 The NetBSD Foundation, Inc.
@@ -31,7 +31,7 @@
 
 #include <stdlib.h>
 
-extern int ifunc(void);
+extern long long ifunc(void);
 
 int
 main(int argc, char **argv)
@@ -39,5 +39,5 @@ main(int argc, char **argv)
 
 	if (argc != 2)
 		return 1;
-	return ifunc() != atoi(argv[1]);
+	return ifunc() != atoll(argv[1]);
 }
