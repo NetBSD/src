8bc3247661b64b5474823613282450733ea70a91
pgoyette <pgoyette@NetBSD.org>
2018-03-09 02:02:59 +0000

    More on renaming the new module

--- sys/compat/common/sysv_mod.c
+++ sys/compat/common/sysv_mod.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: sysv_ipc_mod.c,v 1.1.2.1 2018/03/09 01:27:50 pgoyette Exp $	*/
+/*	$NetBSD: sysv_mod.c,v 1.1.2.1 2018/03/09 02:02:59 pgoyette Exp $	*/
 
 /*-
  * Copyright (c) 2018 The NetBSD Foundation, Inc.
@@ -30,7 +30,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: sysv_ipc_mod.c,v 1.1.2.1 2018/03/09 01:27:50 pgoyette Exp $");
+__KERNEL_RCSID(0, "$NetBSD: sysv_mod.c,v 1.1.2.1 2018/03/09 02:02:59 pgoyette Exp $");
 
 #ifdef _KERNEL_OPT
 #include "opt_compat_netbsd.h"
