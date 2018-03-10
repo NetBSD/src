521b9b35ef500923591908cec3c47785de273ac6
pgoyette <pgoyette@NetBSD.org>
2018-03-10 02:20:13 +0000

    Put declaration of the 5.0 sysctl routine in a header file

--- sys/compat/common/sysv_ipc_50.c
+++ sys/compat/common/sysv_ipc_50.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: sysv_ipc_50.c,v 1.4 2015/12/03 00:28:55 pgoyette Exp $	*/
+/*	$NetBSD: sysv_ipc_50.c,v 1.4.16.1 2018/03/10 02:20:13 pgoyette Exp $	*/
 
 /*-
  * Copyright (c) 1998, 2007 The NetBSD Foundation, Inc.
@@ -30,7 +30,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: sysv_ipc_50.c,v 1.4 2015/12/03 00:28:55 pgoyette Exp $");
+__KERNEL_RCSID(0, "$NetBSD: sysv_ipc_50.c,v 1.4.16.1 2018/03/10 02:20:13 pgoyette Exp $");
 
 #ifdef _KERNEL_OPT
 #include "opt_sysv.h"
@@ -58,7 +58,6 @@ __KERNEL_RCSID(0, "$NetBSD: sysv_ipc_50.c,v 1.4 2015/12/03 00:28:55 pgoyette Exp
 #include <sys/sysctl.h>
 #include <sys/kauth.h>
 
-#ifdef COMPAT_50
 #include <compat/sys/ipc.h>
 #ifdef SYSVMSG
 #include <compat/sys/msg.h>
@@ -73,8 +72,6 @@ __KERNEL_RCSID(0, "$NetBSD: sysv_ipc_50.c,v 1.4 2015/12/03 00:28:55 pgoyette Exp
 /*
  * Check for ipc permission
  */
-
-int sysctl_kern_sysvipc50(SYSCTLFN_PROTO);
 
 int
 sysctl_kern_sysvipc50(SYSCTLFN_ARGS)
@@ -215,4 +212,3 @@ sysctl_kern_sysvipc50(SYSCTLFN_ARGS)
 		free(bf, M_TEMP);
 	return error;
 }
-#endif /* COMPAT_50 */
