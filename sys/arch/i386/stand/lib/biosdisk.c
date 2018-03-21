d3fb6ad16ab70f645e113fb48ec6f41389428697
martin <martin@NetBSD.org>
2018-03-21 10:50:49 +0000

    Pull up following revision(s) (requested by nonaka in ticket #648): 	sys/arch/i386/stand/efiboot/efidisk.c: revision 1.3 	sys/arch/i386/stand/efiboot/devopen.c: revision 1.3 	sys/arch/i386/stand/efiboot/boot.c: revision 1.7 	sys/arch/i386/stand/lib/biosdisk.c: revision 1.48
    efiboot: fix it can't boot from HDD.
    
    efiboot: more pager.
    
    efiboot: boot device is always efi_distlist first element.
    
    efiboot: fix to find boot partition process.
    NetBSD related partitions with no bootme flag set are also candidates
    for boot partition.

--- sys/arch/i386/stand/lib/biosdisk.c
+++ sys/arch/i386/stand/lib/biosdisk.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: biosdisk.c,v 1.46.6.1 2018/03/13 14:54:52 martin Exp $	*/
+/*	$NetBSD: biosdisk.c,v 1.46.6.2 2018/03/21 10:50:49 martin Exp $	*/
 
 /*
  * Copyright (c) 1996, 1998
@@ -802,20 +802,21 @@ biosdisk_findpartition(int biosdev, daddr_t sector)
 			if (d->part[partition].fstype == FS_UNUSED)
 				continue;
 #ifdef EFIBOOT
-			if (d->part[partition].attr & GPT_ENT_ATTR_BOOTME) {
-				switch (d->part[partition].fstype) {
-				case FS_BSDFFS:
-				case FS_BSDLFS:
-				case FS_RAID:
-				case FS_CCD:
-				case FS_CGD:
-				case FS_ISO9660:
-					break;
+			switch (d->part[partition].fstype) {
+			case FS_BSDFFS:
+			case FS_BSDLFS:
+			case FS_RAID:
+			case FS_CCD:
+			case FS_CGD:
+			case FS_ISO9660:
+				if (d->part[partition].attr & GPT_ENT_ATTR_BOOTME)
+					goto found;
+				candidate = partition;
+				break;
 
-				default:
+			default:
+				if (d->part[partition].attr & GPT_ENT_ATTR_BOOTME)
 					candidate = partition;
-					continue;
-				}
 				break;
 			}
 #else
@@ -824,6 +825,7 @@ biosdisk_findpartition(int biosdev, daddr_t sector)
 #endif
 		}
 #ifdef EFIBOOT
+found:
 		if (partition == 0 && candidate != 0)
 			partition = candidate;
 #endif
