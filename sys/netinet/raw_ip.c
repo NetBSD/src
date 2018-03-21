7ee7c80976986e8c014ea5c99b6ef0d5fdc2d6d2
roy <roy@NetBSD.org>
2018-03-21 14:23:54 +0000

    Sprinkle more soroverflow().

--- sys/netinet/raw_ip.c
+++ sys/netinet/raw_ip.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: raw_ip.c,v 1.171 2018/02/28 11:23:24 maxv Exp $	*/
+/*	$NetBSD: raw_ip.c,v 1.172 2018/03/21 14:23:54 roy Exp $	*/
 
 /*
  * Copyright (C) 1995, 1996, 1997, and 1998 WIDE Project.
@@ -65,7 +65,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: raw_ip.c,v 1.171 2018/02/28 11:23:24 maxv Exp $");
+__KERNEL_RCSID(0, "$NetBSD: raw_ip.c,v 1.172 2018/03/21 14:23:54 roy Exp $");
 
 #ifdef _KERNEL_OPT
 #include "opt_inet.h"
@@ -146,7 +146,7 @@ rip_sbappendaddr(struct inpcb *last, struct ip *ip, const struct sockaddr *sa,
 	    || SOOPT_TIMESTAMP(last->inp_socket->so_options))
 		ip_savecontrol(last, &opts, ip, n);
 	if (sbappendaddr(&last->inp_socket->so_rcv, sa, n, opts) == 0) {
-		/* should notify about lost packet */
+		soroverflow(last->inp_socket);
 		m_freem(n);
 		if (opts)
 			m_freem(opts);
