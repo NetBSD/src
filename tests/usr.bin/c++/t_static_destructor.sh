77dc83fa09414efedb0a63963a8d8a18726336ae
kamil <kamil@NetBSD.org>
2018-03-24 00:26:51 +0000

    Add new C++ ATF tests
    Add new variations for existing C++ tests:
     - Static
     - Profile+32-bit
     - PIC+32-bit
     - PIC+Profile
     - PIC+Profile+32-bit
    
    All tests pass for NetBSD/amd64:
     - t_cxxruntime
     - t_hello
     - t_static_destructor
    
    +--------------------------------------------------------------+
    | Options            | cxxruntime | hello  | static_destructor |
    +--------------------------------------------------------------+
    | None               | Passed     | Passed | Passed            |
    | 32-bit             | Passed     | Passed | Passed            |
    | PIC                | Passed     | Passed | Passed            |
    | PIE                | Passed     | Passed | Passed            |
    | Profile            | Passed     | Passed | Passed            |
    | Static             | Passed     | Passed | Passed            |
    | Profile+32-bit     | Passed     | Passed | Passed            |
    | PIC+32-bit         | Passed     | Passed | Passed            |
    | PIC+Profile        | Passed     | Passed | Passed            |
    | PIC+Profile+32-bit | Passed     | Passed | Passed            |
    +--------------------------------------------------------------+
    
    Add new C++11 std::call_once tests:
     - t_call_once
     - t_call_once2
    
    Add new C++11 test with pthread_once(3) and C++ lambda:
     - t_pthread_once
    
    All tests with the profile option for std::call_once and
    pthread_once(3) are marked as expected failure (NetBSD/amd64).
    
    Results for *_once*:
    +------------------------------------------------------------+
    | Options            | call_once | call_once2 | pthread_once |
    +------------------------------------------------------------+
    | None               | Passed    | Passed     | Passed       |
    | 32-bit             | Passed    |  Passed    | Passed       |
    | PIC                | Passed    | Passed     | Passed       |
    | PIE                | Passed    | Passed     | Passed       |
    | Profile            |  Failed   |  Failed    |  Failed      |
    | Static             | Passed    | Passed     | Passed       |
    | Profile+32-bit     |  Failed   |  Failed    |  Failed      |
    | PIC+32-bit         | Passed    | Passed     | Passed       |
    | PIC+Profile        |  Failed   |  Failed    |  Failed      |
    | PIC+Profile+32-bit |  Failed   |  Failed    |  Failed      |
    +------------------------------------------------------------+
    
    Long term there is an option to refacotr the framework for C and C++ tests,
    in order to reduce code duplication.
    
    Patches sent by Yang Zheng <tomsun.0.7@gmail.com>

--- tests/usr.bin/c++/t_static_destructor.sh
+++ tests/usr.bin/c++/t_static_destructor.sh
@@ -1,4 +1,4 @@
-#	$NetBSD: t_static_destructor.sh,v 1.1 2017/05/14 02:02:25 kamil Exp $
+#	$NetBSD: t_static_destructor.sh,v 1.2 2018/03/24 00:26:51 kamil Exp $
 #
 # Copyright (c) 2017 The NetBSD Foundation, Inc.
 # All rights reserved.
@@ -34,9 +34,45 @@ static_destructor_head() {
 	atf_set "require.progs" "c++"
 }
 
+atf_test_case static_destructor_profile
+static_destructor_profile_head() {
+	atf_set "descr" "compile and run \"hello world\" with profiling option"
+	atf_set "require.progs" "c++"
+}
+
+atf_test_case static_destructor_static
+static_destructor_static_head() {
+	atf_set "descr" "compile and run \"hello world\" with static option"
+	atf_set "require.progs" "c++"
+}
+
 atf_test_case static_destructor_pic
 static_destructor_pic_head() {
 	atf_set "descr" "compile and run PIC \"hello world\""
+	atf_set "require.progs" "c++"
+}
+
+atf_test_case static_destructor_pic_32
+static_destructor_pic_32_head() {
+	atf_set "descr" "compile and run 32-bit PIC \"hello world\""
+	atf_set "require.progs" "c++"
+}
+
+atf_test_case static_destructor_pic_profile
+static_destructor_pic_profile_head() {
+	atf_set "descr" "compile and run PIC \"hello world\" with profiling option"
+	atf_set "require.progs" "c++"
+}
+
+atf_test_case static_destructor_pic_profile_32
+static_destructor_pic_profile_32_head() {
+	atf_set "descr" "compile and run 32-bit PIC \"hello world\" with profiling option"
+	atf_set "require.progs" "c++"
+}
+
+atf_test_case static_destructor_profile_32
+static_destructor_profile_32_head() {
+	atf_set "descr" "compile and run 32-bit \"hello world\" with profiling option"
 	atf_set "require.progs" "c++"
 }
 
@@ -71,6 +107,76 @@ EOF
 	atf_check -s exit:0 -o inline:"CTOR A\nCTOR B\nDTOR B:10\nDTOR A:20\n" ./hello
 }
 
+static_destructor_profile_body() {
+	cat > test.cpp << EOF
+#include <iostream>
+struct A {
+	int i;
+	A(int i):i(i){std::cout << "CTOR A" << std::endl;}
+	~A() {std::cout << "DTOR A:" << i << std::endl;}
+};
+struct B {
+	A *m_a;
+	B(){static A s_a(10);m_a=&s_a;std::cout << "CTOR B" << std::endl;}
+	~B(){std::cout << "DTOR B:" << (*m_a).i << std::endl;(*m_a).i = 20;}
+};
+int main(void) {struct B b;return 0;}
+EOF
+	atf_check -s exit:0 -o ignore -e ignore c++ -pg -o hello test.cpp
+	atf_check -s exit:0 -o inline:"CTOR A\nCTOR B\nDTOR B:10\nDTOR A:20\n" ./hello
+}
+
+static_destructor_profile_32_body() {
+	# check whether this arch is 64bit
+	if ! c++ -dM -E - < /dev/null | fgrep -q _LP64; then
+		atf_skip "this is not a 64 bit architecture"
+	fi
+	if ! c++ -m32 -dM -E - < /dev/null 2>/dev/null > ./def32; then
+		atf_skip "c++ -m32 not supported on this architecture"
+	else
+		if fgrep -q _LP64 ./def32; then
+			atf_fail "c++ -m32 does not generate netbsd32 binaries"
+		fi
+	fi
+
+	cat > test.cpp << EOF
+#include <iostream>
+struct A {
+	int i;
+	A(int i):i(i){std::cout << "CTOR A" << std::endl;}
+	~A() {std::cout << "DTOR A:" << i << std::endl;}
+};
+struct B {
+	A *m_a;
+	B(){static A s_a(10);m_a=&s_a;std::cout << "CTOR B" << std::endl;}
+	~B(){std::cout << "DTOR B:" << (*m_a).i << std::endl;(*m_a).i = 20;}
+};
+int main(void) {struct B b;return 0;}
+EOF
+	atf_check -s exit:0 -o ignore -e ignore c++ -m32 -pg -o hello test.cpp
+	atf_check -s exit:0 -o inline:"CTOR A\nCTOR B\nDTOR B:10\nDTOR A:20\n" ./hello
+}
+
+
+static_destructor_static_body() {
+	cat > test.cpp << EOF
+#include <iostream>
+struct A {
+	int i;
+	A(int i):i(i){std::cout << "CTOR A" << std::endl;}
+	~A() {std::cout << "DTOR A:" << i << std::endl;}
+};
+struct B {
+	A *m_a;
+	B(){static A s_a(10);m_a=&s_a;std::cout << "CTOR B" << std::endl;}
+	~B(){std::cout << "DTOR B:" << (*m_a).i << std::endl;(*m_a).i = 20;}
+};
+int main(void) {struct B b;return 0;}
+EOF
+	atf_check -s exit:0 -o ignore -e ignore c++ -static -o hello test.cpp
+	atf_check -s exit:0 -o inline:"CTOR A\nCTOR B\nDTOR B:10\nDTOR A:20\n" ./hello
+}
+
 static_destructor_pic_body() {
 	cat > test.cpp << EOF
 #include <cstdlib>
@@ -96,6 +202,120 @@ EOF
 	    c++ -fPIC -shared -o libtest.so pic.cpp
 	atf_check -s exit:0 -o ignore -e ignore \
 	    c++ -o hello test.cpp -L. -ltest
+
+	export LD_LIBRARY_PATH=.
+	atf_check -s exit:0 -o inline:"CTOR A\nCTOR B\nDTOR B:10\nDTOR A:20\n" ./hello
+}
+
+static_destructor_pic_32_body() {
+	# check whether this arch is 64bit
+	if ! c++ -dM -E - < /dev/null | fgrep -q _LP64; then
+		atf_skip "this is not a 64 bit architecture"
+	fi
+	if ! c++ -m32 -dM -E - < /dev/null 2>/dev/null > ./def32; then
+		atf_skip "c++ -m32 not supported on this architecture"
+	else
+		if fgrep -q _LP64 ./def32; then
+			atf_fail "c++ -m32 does not generate netbsd32 binaries"
+		fi
+	fi
+
+	cat > test.cpp << EOF
+#include <cstdlib>
+int callpic(void);
+int main(void) {callpic();exit(0);}
+EOF
+	cat > pic.cpp << EOF
+#include <iostream>
+struct A {
+	int i;
+	A(int i):i(i){std::cout << "CTOR A" << std::endl;}
+	~A() {std::cout << "DTOR A:" << i << std::endl;}
+};
+struct B {
+	A *m_a;
+	B(){static A s_a(10);m_a=&s_a;std::cout << "CTOR B" << std::endl;}
+	~B(){std::cout << "DTOR B:" << (*m_a).i << std::endl;(*m_a).i = 20;}
+};
+int callpic(void) {struct B b;}
+EOF
+
+	atf_check -s exit:0 -o ignore -e ignore \
+	    c++ -m32 -fPIC -shared -o libtest.so pic.cpp
+	atf_check -s exit:0 -o ignore -e ignore \
+	    c++ -m32 -o hello test.cpp -L. -ltest
+
+	export LD_LIBRARY_PATH=.
+	atf_check -s exit:0 -o inline:"CTOR A\nCTOR B\nDTOR B:10\nDTOR A:20\n" ./hello
+}
+
+static_destructor_pic_profile_body() {
+	cat > test.cpp << EOF
+#include <cstdlib>
+int callpic(void);
+int main(void) {callpic();exit(0);}
+EOF
+	cat > pic.cpp << EOF
+#include <iostream>
+struct A {
+	int i;
+	A(int i):i(i){std::cout << "CTOR A" << std::endl;}
+	~A() {std::cout << "DTOR A:" << i << std::endl;}
+};
+struct B {
+	A *m_a;
+	B(){static A s_a(10);m_a=&s_a;std::cout << "CTOR B" << std::endl;}
+	~B(){std::cout << "DTOR B:" << (*m_a).i << std::endl;(*m_a).i = 20;}
+};
+int callpic(void) {struct B b;}
+EOF
+
+	atf_check -s exit:0 -o ignore -e ignore \
+	    c++ -pg -fPIC -shared -o libtest.so pic.cpp
+	atf_check -s exit:0 -o ignore -e ignore \
+	    c++ -pg -o hello test.cpp -L. -ltest
+
+	export LD_LIBRARY_PATH=.
+	atf_check -s exit:0 -o inline:"CTOR A\nCTOR B\nDTOR B:10\nDTOR A:20\n" ./hello
+}
+
+static_destructor_pic_profile_32_body() {
+	# check whether this arch is 64bit
+	if ! c++ -dM -E - < /dev/null | fgrep -q _LP64; then
+		atf_skip "this is not a 64 bit architecture"
+	fi
+	if ! c++ -m32 -dM -E - < /dev/null 2>/dev/null > ./def32; then
+		atf_skip "c++ -m32 not supported on this architecture"
+	else
+		if fgrep -q _LP64 ./def32; then
+			atf_fail "c++ -m32 does not generate netbsd32 binaries"
+		fi
+	fi
+
+	cat > test.cpp << EOF
+#include <cstdlib>
+int callpic(void);
+int main(void) {callpic();exit(0);}
+EOF
+	cat > pic.cpp << EOF
+#include <iostream>
+struct A {
+	int i;
+	A(int i):i(i){std::cout << "CTOR A" << std::endl;}
+	~A() {std::cout << "DTOR A:" << i << std::endl;}
+};
+struct B {
+	A *m_a;
+	B(){static A s_a(10);m_a=&s_a;std::cout << "CTOR B" << std::endl;}
+	~B(){std::cout << "DTOR B:" << (*m_a).i << std::endl;(*m_a).i = 20;}
+};
+int callpic(void) {struct B b;}
+EOF
+
+	atf_check -s exit:0 -o ignore -e ignore \
+	    c++ -m32 -pg -fPIC -shared -o libtest.so pic.cpp
+	atf_check -s exit:0 -o ignore -e ignore \
+	    c++ -m32 -pg -o hello test.cpp -L. -ltest
 
 	export LD_LIBRARY_PATH=.
 	atf_check -s exit:0 -o inline:"CTOR A\nCTOR B\nDTOR B:10\nDTOR A:20\n" ./hello
@@ -188,7 +408,13 @@ atf_init_test_cases()
 {
 
 	atf_add_test_case static_destructor
+	atf_add_test_case static_destructor_profile
 	atf_add_test_case static_destructor_pic
 	atf_add_test_case static_destructor_pie
 	atf_add_test_case static_destructor32
+	atf_add_test_case static_destructor_static
+	atf_add_test_case static_destructor_pic_32
+	atf_add_test_case static_destructor_pic_profile
+	atf_add_test_case static_destructor_pic_profile_32
+	atf_add_test_case static_destructor_profile_32
 }
