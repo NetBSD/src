36bbee0c3ede256d58c912d5f0fb6c731e6d0e2d
pgoyette <pgoyette@NetBSD.org>
2018-03-25 08:03:26 +0000

    Now that we have a compat_80 module, make sure it is marked as "included" in the monolithic compat module

--- sys/compat/common/compat_mod.c
+++ sys/compat/common/compat_mod.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: compat_mod.c,v 1.24.14.19 2018/03/22 01:44:47 pgoyette Exp $	*/
+/*	$NetBSD: compat_mod.c,v 1.24.14.20 2018/03/25 08:03:26 pgoyette Exp $	*/
 
 /*-
  * Copyright (c) 2008 The NetBSD Foundation, Inc.
@@ -34,7 +34,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: compat_mod.c,v 1.24.14.19 2018/03/22 01:44:47 pgoyette Exp $");
+__KERNEL_RCSID(0, "$NetBSD: compat_mod.c,v 1.24.14.20 2018/03/25 08:03:26 pgoyette Exp $");
 
 #ifdef _KERNEL_OPT
 #include "opt_compat_netbsd.h"
@@ -81,7 +81,7 @@ static struct sysctllog *compat_clog = NULL;
 #endif
 
 static const char * const compat_includes[] = {
-	"compat_70", "compat_60", "compat_50", 
+	"compat_80", "compat_70", "compat_60", "compat_50", 
 	NULL
 };
 
