16fe7290805d7eac813c58b4a54cc5d1c7f56ab7
pgoyette <pgoyette@NetBSD.org>
2018-03-26 10:49:45 +0000

    For now, disable CPU_UCODE stuff since we can't build it properly with XEN.  Re-enable it after we get fix for PR kern/53130

--- sys/modules/arch/x86/i386pae-xen/bsd.i386pae-xen.mk
+++ sys/modules/arch/x86/i386pae-xen/bsd.i386pae-xen.mk
@@ -1,4 +1,4 @@
-#	$NetBSD: bsd.i386pae-xen.mk,v 1.1 2014/08/11 03:43:26 jnemeth Exp $
+#	$NetBSD: bsd.i386pae-xen.mk,v 1.1.28.1 2018/03/26 10:49:45 pgoyette Exp $
 
 .ifndef _BSD_I386PAE_XEN_MK_
 _BSD_I386PAE_XEN_MK_=1
@@ -7,5 +7,7 @@ KMODULEARCHDIR:=	i386pae-xen
 
 XEN=	1
 PAE=	1
+
+CPPFLAGS+=	-DPAE -DXEN
 
 .endif # _BSD_I386PAE_XEN_MK_
