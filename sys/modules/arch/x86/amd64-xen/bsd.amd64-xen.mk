c133794b25ec11ce40b62dc92fa6b815bc43976f
pgoyette <pgoyette@NetBSD.org>
2018-03-27 03:57:37 +0000

    Disable previous change - it breaks some stuff

--- sys/modules/arch/x86/amd64-xen/bsd.amd64-xen.mk
+++ sys/modules/arch/x86/amd64-xen/bsd.amd64-xen.mk
@@ -1,4 +1,4 @@
-#	$NetBSD: bsd.amd64-xen.mk,v 1.1.28.1 2018/03/26 10:49:45 pgoyette Exp $
+#	$NetBSD: bsd.amd64-xen.mk,v 1.1.28.2 2018/03/27 03:57:37 pgoyette Exp $
 
 .ifndef _BSD_AMD64_XEN_MK_
 _BSD_AMD64_XEN_MK_=1
@@ -7,6 +7,6 @@ KMODULEARCHDIR:=	amd64-xen
 
 XEN=	1
 
-CPPFLAGS+=	-DXEN
+#CPPFLAGS+=	-DXEN
 
 .endif # _BSD_AMD64_XEN_MK_
--- sys/modules/arch/x86/i386pae-xen/bsd.i386pae-xen.mk
+++ sys/modules/arch/x86/i386pae-xen/bsd.i386pae-xen.mk
@@ -1,4 +1,4 @@
-#	$NetBSD: bsd.i386pae-xen.mk,v 1.1.28.1 2018/03/26 10:49:45 pgoyette Exp $
+#	$NetBSD: bsd.i386pae-xen.mk,v 1.1.28.2 2018/03/27 03:57:37 pgoyette Exp $
 
 .ifndef _BSD_I386PAE_XEN_MK_
 _BSD_I386PAE_XEN_MK_=1
@@ -8,6 +8,6 @@ KMODULEARCHDIR:=	i386pae-xen
 XEN=	1
 PAE=	1
 
-CPPFLAGS+=	-DPAE -DXEN
+#CPPFLAGS+=	-DPAE -DXEN
 
 .endif # _BSD_I386PAE_XEN_MK_
