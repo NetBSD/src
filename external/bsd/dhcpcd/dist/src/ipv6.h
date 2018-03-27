b8483d1d2f78ca2300a6d0801daffb2562c621be
roy <roy@NetBSD.org>
2018-03-27 06:14:39 +0000

    Import dhcpcd-7.0.2 with the following changes:
      *  Added support for setproctitle(3)
      *  Kernel RA is no longer disabled when IPv6 is disabled in dhcpcd
      *  DHCPv6 PD is no longer stopped if no Routers are found
      *  If the DHCP leased address is deleted, enter the reboot state
      *  DHCPv6 unicast is no longer performed when not in master mode
      *  dhcpcd will now detect netlink/route socket overflows ad re-sync

--- external/bsd/dhcpcd/dist/src/ipv6.h
+++ external/bsd/dhcpcd/dist/src/ipv6.h
@@ -231,14 +231,17 @@ uint8_t ipv6_prefixlen(const struct in6_addr *);
 int ipv6_userprefix( const struct in6_addr *, short prefix_len,
     uint64_t user_number, struct in6_addr *result, short result_len);
 void ipv6_checkaddrflags(void *);
+void ipv6_markaddrsstale(struct interface *, unsigned int);
+void ipv6_deletestaleaddrs(struct interface *);
 int ipv6_addaddr(struct ipv6_addr *, const struct timespec *);
 ssize_t ipv6_addaddrs(struct ipv6_addrhead *addrs);
 void ipv6_deleteaddr(struct ipv6_addr *);
 void ipv6_freedrop_addrs(struct ipv6_addrhead *, int,
     const struct interface *);
 void ipv6_handleifa(struct dhcpcd_ctx *ctx, int, struct if_head *,
-    const char *, const struct in6_addr *, uint8_t, int);
-int ipv6_handleifa_addrs(int, struct ipv6_addrhead *, const struct ipv6_addr *);
+    const char *, const struct in6_addr *, uint8_t, int, pid_t);
+int ipv6_handleifa_addrs(int, struct ipv6_addrhead *, const struct ipv6_addr *,
+    pid_t);
 struct ipv6_addr *ipv6_iffindaddr(struct interface *,
     const struct in6_addr *, int);
 int ipv6_hasaddr(const struct interface *);
@@ -259,7 +262,6 @@ void ipv6_freedrop(struct interface *, int);
 
 #ifdef IPV6_MANAGETEMPADDR
 void ipv6_gentempifid(struct interface *);
-void ipv6_settempstale(struct interface *);
 struct ipv6_addr *ipv6_createtempaddr(struct ipv6_addr *,
     const struct timespec *);
 struct ipv6_addr *ipv6_settemptime(struct ipv6_addr *, int);
