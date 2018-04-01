12be5c4b388dfd3d47f8c43ce2cf7e89ba44904a
martin <martin@NetBSD.org>
2018-04-01 09:00:42 +0000

    Pull up following revision(s) (requested by mrg in ticket #682):
    	tools/gcc/gcc-version.mk: revision 1.10 (via patch)
    	external/gpl3/gcc.old/dist/gcc/config/i386/i386.c: revision 1.10
    	external/gpl3/gcc.old/dist/gcc/config/i386/i386-protos.h: revision 1.6
    	external/gpl3/gcc.old/dist/gcc/config/i386/constraints.md: revision 1.6
    	external/gpl3/gcc.old/dist/gcc/doc/invoke.texi: revision 1.7
    	external/gpl3/gcc.old/dist/gcc/config/i386/i386.md: revision 1.7
    	external/gpl3/gcc.old/dist/gcc/doc/extend.texi: revision 1.7
    	external/gpl3/gcc.old/dist/gcc/config/i386/i386-opts.h: revision 1.4
    	external/gpl3/gcc.old/dist/gcc/config/i386/predicates.md: revision 1.6
    	external/gpl3/gcc.old/dist/gcc/config/i386/i386.h: revision 1.7
    	external/gpl3/gcc.old/dist/gcc/config/i386/i386.opt: revision 1.7
    	(with external/gpl3/gcc.old/ -> external/gpl3/gcc/)
    
    add the spectre mitigation options for x86:
    
      -mindirect-branch=<choice>
      -mfunction-return=<choice>
      -mindirect-branch-register
    
    the values for 'choice' are "keep" (default, existing behaviour),
    "thunk", "thunk-inline", and "thunk-extern".
    
    as taken from the Ubuntu port of these changes in their
    ubuntu:gcc-5_5.5.0-8ubuntu1.diff.  i've also included the doc
    updates that are missing from ubuntu from gcc itself.
    i've tested both i386 and amd64 fairly heavily with these options
    enabled in both kernels and userland, atf runs and hundreds of
    package builds.
    
    bump the NetBSD GCC version.  both GCC 5 and 6 got x86 spectre
    mitigation code, and both are now "nb2 20180327".

--- tools/gcc/gcc-version.mk
+++ tools/gcc/gcc-version.mk
@@ -1,5 +1,5 @@
-#	$NetBSD: gcc-version.mk,v 1.7 2017/02/22 12:34:49 maya Exp $
+#	$NetBSD: gcc-version.mk,v 1.7.4.1 2018/04/01 09:00:42 martin Exp $
 
 # common location for tools and native build
 
-NETBSD_GCC_VERSION=nb1 20160606
+NETBSD_GCC_VERSION=nb2 20180327
