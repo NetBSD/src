65224bf4fa7b8650a402be7ed704e1f6ac118fe3
bouyer <bouyer@NetBSD.org>
2018-04-03 12:52:16 +0000

    work in progress on porting graphics drivers from arm/allwinner to fdt. sunxidebe: display backend drivers sunxitcon: lcd controller driver sunxihdmi: hdmi controller driver sunxidep: display engine pipeline driver. Its role is only to parse the    fdt display-engine node, and activate de backend drivers based on the    content of allwinner,pipelines
    So far HDMI and lvds output works, in dual-framebuffer mode.
    It has only been tested on A20. It should be OK on the A10 too, but
    will likely need more work for other SoCs.
    Console is not handled yet, and it conflicts with the simplefb driver if
    it has been activated by u-boot.

---
+++ sys/arch/arm/sunxi/sunxi_display.h
@@ -0,0 +1,39 @@
+/*	$NetBSD: sunxi_display.h,v 1.1 2018/04/03 12:52:16 bouyer Exp $	*/
+
+/*-
+ * Copyright (c) 2018 The NetBSD Foundation, Inc.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to The NetBSD Foundation
+ * by Manuel Bouyer.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+
+#define SUNXI_PORT_INPUT	0
+#define SUNXI_PORT_OUTPUT	1
+
+struct videomode;
+int sunxi_debe_pipeline(int, bool);
+void sunxi_tcon1_set_videomode(device_t, const struct videomode *);
+void sunxi_debe_set_videomode(device_t, const struct videomode *);
