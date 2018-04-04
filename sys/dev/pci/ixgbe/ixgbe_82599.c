58b5fdb5babaee7ff9cea3a8ee78dcb0df5c12d2
msaitoh <msaitoh@NetBSD.org>
2018-04-04 08:13:07 +0000

    Sync with the remaining part of FreeBSD r328265 except sfp_reinit stuff:  - Always schedule module intterrupt in ixgbe_config_link() when a device is    SFP+ based.  - Use not loop index but txr->me in ixv_initialize_{transmit,receive}_units().    It's required for VMDQ but NetBSD doesn't use it, so it's not a bug in    NetBSD.  - Simplify ixgbe_bp_wd_set(). No functional change.  - Whitespace.

--- sys/dev/pci/ixgbe/ixgbe_82599.c
+++ sys/dev/pci/ixgbe/ixgbe_82599.c
@@ -1,4 +1,4 @@
-/* $NetBSD: ixgbe_82599.c,v 1.17 2018/03/30 06:44:30 msaitoh Exp $ */
+/* $NetBSD: ixgbe_82599.c,v 1.18 2018/04/04 08:13:07 msaitoh Exp $ */
 
 /******************************************************************************
   SPDX-License-Identifier: BSD-3-Clause
@@ -33,7 +33,7 @@
   POSSIBILITY OF SUCH DAMAGE.
 
 ******************************************************************************/
-/*$FreeBSD: head/sys/dev/ixgbe/ixgbe_82599.c 320688 2017-07-05 17:27:03Z erj $*/
+/*$FreeBSD: head/sys/dev/ixgbe/ixgbe_82599.c 326022 2017-11-20 19:36:21Z pfg $*/
 
 #include "ixgbe_type.h"
 #include "ixgbe_82599.h"
