f00e1424b61878171e84d9daf356b00aeebdbef2
martin <martin@NetBSD.org>
2018-04-12 14:06:14 +0000

    Pull up following revision(s) (requested by mrg in ticket #745):
    	sys/arch/sun3/conf/Makefile.sun3: revision 1.106
    	sys/arch/sun3/conf/Makefile.sun3: revision 1.107
    	sys/arch/sun3/conf/GENERIC3X: revision 1.131
    	sys/arch/sun3/include/param3.h: revision 1.57
    	sys/arch/sun3/conf/GENERIC: revision 1.177
    	sys/arch/sun3/conf/GENERIC: revision 1.178
    	sys/arch/sun3/include/vmparam3.h: revision 1.39
    
    sun3:
    - build libkern as an archive for non modular builds.  from maya@
    - cut down GENERIC a bunch to bring below 2MB.
    - reduce UBC_NWINS, MAXEXEC, and PAGER_MAP_DEFAULT_SIZE to recover
      enough lost VA to actually run basic tests.
    
    also disable SYSCTL_INCLUDE_DESCR.
    
    copy sun3 disables to sun3x mostly.  it has the same ufsboot 2MB limit.
    
    Build compat code as .a if not MODULAR.
    
    Removes some more unused functions.

--- sys/arch/sun3/include/param3.h
+++ sys/arch/sun3/include/param3.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param3.h,v 1.56 2012/08/12 20:00:06 tsutsui Exp $	*/
+/*	$NetBSD: param3.h,v 1.56.32.1 2018/04/12 14:06:14 martin Exp $	*/
 
 /*
  * Copyright (c) 1988 University of Utah.
@@ -53,4 +53,5 @@
 #define	NKMEMPAGES_MAX_DEFAULT	((16 * 1024 * 1024) >> PAGE_SHIFT)
 
 /* Use smaller UBC_NWINS to avoid KVA space shortage. */
-#define	UBC_NWINS	512
+#define	UBC_NWINS	128
+#define	MAXEXEC		1
