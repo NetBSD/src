53ed154370f0fcaca639866977895172caa1c043
msaitoh <msaitoh@NetBSD.org>
2018-04-13 07:57:04 +0000

    Add 300 series chipset support.

--- sys/dev/pci/pucdata.c
+++ sys/dev/pci/pucdata.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: pucdata.c,v 1.100 2017/12/27 20:27:02 christos Exp $	*/
+/*	$NetBSD: pucdata.c,v 1.101 2018/04/13 07:57:04 msaitoh Exp $	*/
 
 /*
  * Copyright (c) 1998, 1999 Christopher G. Demetriou.  All rights reserved.
@@ -36,7 +36,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: pucdata.c,v 1.100 2017/12/27 20:27:02 christos Exp $");
+__KERNEL_RCSID(0, "$NetBSD: pucdata.c,v 1.101 2018/04/13 07:57:04 msaitoh Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -1936,7 +1936,16 @@ const struct puc_device_description puc_devices[] = {
 		{ PUC_PORT_TYPE_COM, PCI_BAR0, 0x00, COM_FREQ },
 	    },
 	},
-	
+
+	/* Intel 300 Series KT */
+	{   "Intel 300 Series KT",
+	    {	PCI_VENDOR_INTEL, PCI_PRODUCT_INTEL_3HS_KT, 0, 0 },
+	    {	0xffff,	0xffff,	0,	0	},
+	    {
+		{ PUC_PORT_TYPE_COM, PCI_BAR0, 0x00, COM_FREQ },
+	    },
+	},
+
 	/* Intel C600/X79 Series KT */
 	{   "Intel C600/X79 Series KT",
 	    {	PCI_VENDOR_INTEL, PCI_PRODUCT_INTEL_C600_KT, 0, 0 },
