15e650c72b3b5cc8379ad0d4e88da21cd7ddc83d
nonaka <nonaka@NetBSD.org>
2018-04-13 11:24:34 +0000

    x86: Increase BOOTINFO_MAXSIZE to 8Kib.
    Proposed on port-i386 and port-amd64 with no objections:
    http://mail-index.netbsd.org/port-i386/2018/04/11/msg003692.html
    http://mail-index.netbsd.org/port-amd64/2018/04/11/msg002697.html

--- sys/arch/x86/include/bootinfo.h
+++ sys/arch/x86/include/bootinfo.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: bootinfo.h,v 1.28 2017/11/09 01:01:33 christos Exp $	*/
+/*	$NetBSD: bootinfo.h,v 1.29 2018/04/13 11:24:34 nonaka Exp $	*/
 
 /*
  * Copyright (c) 1997
@@ -251,7 +251,7 @@ struct btinfo_efimemmap {
 
 #ifdef _KERNEL
 
-#define BOOTINFO_MAXSIZE 4096
+#define BOOTINFO_MAXSIZE 8192
 
 #ifndef _LOCORE
 /*
