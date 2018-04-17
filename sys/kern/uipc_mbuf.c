a7818dfe4fd95115cfe7b2788848bf2dc973e493
martin <martin@NetBSD.org>
2018-04-17 08:24:01 +0000

    Pull up following revision(s) (requested by maxv in ticket #770):
    	sys/kern/uipc_mbuf.c: revision 1.190
    
    If the mbuf is shared leave M_PKTHDR in place. Given where this function
    is called from that's not supposed to happen, but I'm growing unconfident
    about our mbuf code.

--- sys/kern/uipc_mbuf.c
+++ sys/kern/uipc_mbuf.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: uipc_mbuf.c,v 1.172.6.2 2018/04/05 14:33:41 martin Exp $	*/
+/*	$NetBSD: uipc_mbuf.c,v 1.172.6.3 2018/04/17 08:24:01 martin Exp $	*/
 
 /*-
  * Copyright (c) 1999, 2001 The NetBSD Foundation, Inc.
@@ -62,7 +62,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: uipc_mbuf.c,v 1.172.6.2 2018/04/05 14:33:41 martin Exp $");
+__KERNEL_RCSID(0, "$NetBSD: uipc_mbuf.c,v 1.172.6.3 2018/04/17 08:24:01 martin Exp $");
 
 #ifdef _KERNEL_OPT
 #include "opt_mbuftrace.h"
@@ -459,6 +459,11 @@ void
 m_pkthdr_remove(struct mbuf *m)
 {
 	KASSERT(m->m_flags & M_PKTHDR);
+
+	if (M_READONLY(m)) {
+		/* Nothing we can do. */
+		return;
+	}
 
 	m_tag_delete_chain(m, NULL);
 	m->m_flags &= ~M_PKTHDR;
