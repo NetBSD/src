7cde077d87236de71ec2bf8dfe053b38993a8b79
knakahara <knakahara@NetBSD.org>
2018-04-24 01:32:30 +0000

    Fix sys/netinet/in.c:r1.229 problem. I have missed FALLTHROUGH, sorry.

--- sys/netinet/in.c
+++ sys/netinet/in.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: in.c,v 1.229 2018/04/20 09:56:22 knakahara Exp $	*/
+/*	$NetBSD: in.c,v 1.230 2018/04/24 01:32:30 knakahara Exp $	*/
 
 /*
  * Copyright (C) 1995, 1996, 1997, and 1998 WIDE Project.
@@ -91,7 +91,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: in.c,v 1.229 2018/04/20 09:56:22 knakahara Exp $");
+__KERNEL_RCSID(0, "$NetBSD: in.c,v 1.230 2018/04/24 01:32:30 knakahara Exp $");
 
 #include "arp.h"
 
@@ -479,9 +479,14 @@ in_control0(struct socket *so, u_long cmd, void *data, struct ifnet *ifp)
 		} else if (in_hosteq(ia->ia_addr.sin_addr,
 		           ifra->ifra_addr.sin_addr))
 			hostIsNew = 0;
+		if (ifra->ifra_addr.sin_family != AF_INET) {
+			error = EAFNOSUPPORT;
+			goto out;
+		}
 		/* FALLTHROUGH */
 	case SIOCSIFDSTADDR:
-		if (ifreq_getaddr(cmd, ifr)->sa_family != AF_INET) {
+		if (cmd == SIOCSIFDSTADDR &&
+		    ifreq_getaddr(cmd, ifr)->sa_family != AF_INET) {
 			error = EAFNOSUPPORT;
 			goto out;
 		}
