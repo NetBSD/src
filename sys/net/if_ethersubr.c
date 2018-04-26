549f114ce2d5dc689907570f19ab528036d78aab
maxv <maxv@NetBSD.org>
2018-04-26 19:56:55 +0000

    m_copy -> m_copym

--- sys/net/if_ethersubr.c
+++ sys/net/if_ethersubr.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: if_ethersubr.c,v 1.263 2018/04/09 16:14:11 maxv Exp $	*/
+/*	$NetBSD: if_ethersubr.c,v 1.264 2018/04/26 19:56:55 maxv Exp $	*/
 
 /*
  * Copyright (C) 1995, 1996, 1997, and 1998 WIDE Project.
@@ -61,7 +61,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: if_ethersubr.c,v 1.263 2018/04/09 16:14:11 maxv Exp $");
+__KERNEL_RCSID(0, "$NetBSD: if_ethersubr.c,v 1.264 2018/04/26 19:56:55 maxv Exp $");
 
 #ifdef _KERNEL_OPT
 #include "opt_inet.h"
@@ -247,7 +247,7 @@ ether_output(struct ifnet * const ifp0, struct mbuf * const m0,
 		}
 		/* If broadcasting on a simplex interface, loopback a copy */
 		if ((m->m_flags & M_BCAST) && (ifp->if_flags & IFF_SIMPLEX))
-			mcopy = m_copy(m, 0, M_COPYALL);
+			mcopy = m_copym(m, 0, M_COPYALL, M_DONTWAIT);
 		etype = htons(ETHERTYPE_IP);
 		break;
 
