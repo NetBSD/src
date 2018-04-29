f3525e2457554ffbcf1f27af3f09ba632e3d4324
maxv <maxv@NetBSD.org>
2018-04-29 07:13:10 +0000

    Remove references to m_copy in comments.

--- sys/net/if_ethersubr.c
+++ sys/net/if_ethersubr.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: if_ethersubr.c,v 1.264 2018/04/26 19:56:55 maxv Exp $	*/
+/*	$NetBSD: if_ethersubr.c,v 1.265 2018/04/29 07:13:10 maxv Exp $	*/
 
 /*
  * Copyright (C) 1995, 1996, 1997, and 1998 WIDE Project.
@@ -61,7 +61,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: if_ethersubr.c,v 1.264 2018/04/26 19:56:55 maxv Exp $");
+__KERNEL_RCSID(0, "$NetBSD: if_ethersubr.c,v 1.265 2018/04/29 07:13:10 maxv Exp $");
 
 #ifdef _KERNEL_OPT
 #include "opt_inet.h"
@@ -328,9 +328,9 @@ ether_output(struct ifnet * const ifp0, struct mbuf * const m0,
 
 		/*
 		 * In the phase 2 case, we need to prepend an mbuf for the
-		 * llc header.  Since we must preserve the value of m,
-		 * which is passed to us by value, we m_copy() the first
-		 * mbuf, and use it for our llc header.
+		 * llc header.
+		 *
+		 * XXX XXX: Do we need to preserve the value of m?
 		 */
 		if (aa->aa_flags & AFA_PHASE2) {
 			struct llc llc;
