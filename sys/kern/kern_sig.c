59c114242bd8356e45f4653f2e4f83c5b1c0e673
kamil <kamil@NetBSD.org>
2018-05-01 16:37:23 +0000

    Implement PTRACE_VFORK
    Add support for tracing vfork(2) events in the context of ptrace(2).
    
    This API covers other frontends to fork1(9) like posix_spawn(2) or clone(2),
    if they cause parent to wait for exec(2) or exit(2) of the child.
    
    Changes:
     - Add new argument to sigswitch() determining whether we need to acquire
       the proc_lock or whether it's already held.
     - Refactor fork1(9) for fork(2) and vfork(2)-like events.
       Call sigswitch() from fork(1) for forking or vforking parent, instead of
       emitting kpsignal(9). We need to emit the signal and suspend the parent,
       returning to user and relock proc_lock.
     - Add missing prototype for proc_stop_done() in kern_sig.c.
     - Make sigswitch a public function accessible from other kernel code
       including <sys/signalvar.h>.
     - Remove an entry about unimplemented PTRACE_VFORK in the ptrace(2) man page.
     - Permin PTRACE_VFORK in the ptrace(2) frontend for userland.
     - Remove expected failure for unimplemented PTRACE_VFORK tests in the ATF
       ptrace(2) test-suite.
     - Relax signal routing constraints under a debugger for a vfork(2)ed child.
       This intended to protect from signaling a parent of a vfork(2)ed child that
       called PT_TRACE_ME, but wrongly misrouted other signals in vfork(2)
       use-cases.
    
    Add XXX comments about still existing problems and future enhancements:
     - correct vfork(2) + PT_TRACE_ME handling.
     - fork1(2) handling of scenarios when a process is collected in valid but
       rare cases.
    
    All ATF ptrace(2) fork[1-8] and vfork[1-8] tests pass.
    
    Fix PR kern/51630 by Kamil Rytarowski (myself).
    
    Sponsored by <The NetBSD Foundation>

--- sys/kern/kern_sig.c
+++ sys/kern/kern_sig.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: kern_sig.c,v 1.341 2018/05/01 13:48:38 kamil Exp $	*/
+/*	$NetBSD: kern_sig.c,v 1.342 2018/05/01 16:37:23 kamil Exp $	*/
 
 /*-
  * Copyright (c) 2006, 2007, 2008 The NetBSD Foundation, Inc.
@@ -70,7 +70,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: kern_sig.c,v 1.341 2018/05/01 13:48:38 kamil Exp $");
+__KERNEL_RCSID(0, "$NetBSD: kern_sig.c,v 1.342 2018/05/01 16:37:23 kamil Exp $");
 
 #include "opt_ptrace.h"
 #include "opt_dtrace.h"
@@ -118,12 +118,12 @@ sigset_t		sigcantmask	__cacheline_aligned;
 
 static void	ksiginfo_exechook(struct proc *, void *);
 static void	proc_stop(struct proc *, int);
+static void	proc_stop_done(struct proc *, int);
 static void	proc_stop_callout(void *);
 static int	sigchecktrace(void);
 static int	sigpost(struct lwp *, sig_t, int, int);
 static int	sigput(sigpend_t *, struct proc *, ksiginfo_t *);
 static int	sigunwait(struct proc *, const ksiginfo_t *);
-static void	sigswitch(int, int);
 
 static void	sigacts_poolpage_free(struct pool *, void *);
 static void	*sigacts_poolpage_alloc(struct pool *, int);
@@ -1528,8 +1528,8 @@ proc_stop_done(struct proc *p, int ppmask)
 /*
  * Stop the current process and switch away when being stopped or traced.
  */
-static void
-sigswitch(int ppmask, int signo)
+void
+sigswitch(int ppmask, int signo, bool relock)
 {
 	struct lwp *l = curlwp;
 	struct proc *p = l->l_proc;
@@ -1555,7 +1555,7 @@ sigswitch(int ppmask, int signo)
 	 * a new signal, then signal the parent.
 	 */
 	if ((p->p_sflag & PS_STOPPING) != 0) {
-		if (!mutex_tryenter(proc_lock)) {
+		if (relock && !mutex_tryenter(proc_lock)) {
 			mutex_exit(p->p_lock);
 			mutex_enter(proc_lock);
 			mutex_enter(p->p_lock);
@@ -1665,7 +1665,7 @@ issignal(struct lwp *l)
 		 * we awaken, check for a signal from the debugger.
 		 */
 		if (p->p_stat == SSTOP || (p->p_sflag & PS_STOPPING) != 0) {
-			sigswitch(PS_NOCLDSTOP, 0);
+			sigswitch(PS_NOCLDSTOP, 0, true);
 			signo = sigchecktrace();
 		} else
 			signo = 0;
@@ -1718,11 +1718,12 @@ issignal(struct lwp *l)
 
 		/*
 		 * If traced, always stop, and stay stopped until released
-		 * by the debugger.  If the our parent process is waiting
-		 * for us, don't hang as we could deadlock.
+		 * by the debugger.  If the our parent is our debugger waiting
+		 * for us and we vforked, don't hang as we could deadlock.
+		 *
+		 * XXX: support PT_TRACE_ME called after vfork(2)
 		 */
-		if ((p->p_slflag & PSL_TRACED) != 0 &&
-		    (p->p_lflag & PL_PPWAIT) == 0 && signo != SIGKILL) {
+		if ((p->p_slflag & PSL_TRACED) != 0 && signo != SIGKILL) {
 			/*
 			 * Take the signal, but don't remove it from the
 			 * siginfo queue, because the debugger can send
@@ -1735,7 +1736,7 @@ issignal(struct lwp *l)
 			/* Emulation-specific handling of signal trace */
 			if (p->p_emul->e_tracesig == NULL ||
 			    (*p->p_emul->e_tracesig)(p, signo) == 0)
-				sigswitch(0, signo);
+				sigswitch(0, signo, true);
 
 			/* Check for a signal from the debugger. */
 			if ((signo = sigchecktrace()) == 0)
@@ -1789,7 +1790,7 @@ issignal(struct lwp *l)
 				p->p_xsig = signo;
 				p->p_sflag &= ~PS_CONTINUED;
 				signo = 0;
-				sigswitch(PS_NOCLDSTOP, p->p_xsig);
+				sigswitch(PS_NOCLDSTOP, p->p_xsig, true);
 			} else if (prop & SA_IGNORE) {
 				/*
 				 * Except for SIGCONT, shouldn't get here.
@@ -2298,7 +2299,7 @@ proc_stoptrace(int trapno)
 	p->p_xsig = signo;
 	p->p_sigctx.ps_lwp = ksi.ksi_lid;
 	p->p_sigctx.ps_info = ksi.ksi_info;
-	sigswitch(0, signo);
+	sigswitch(0, signo, true);
 	mutex_exit(p->p_lock);
 
 	if (ktrpoint(KTR_PSIG)) {
