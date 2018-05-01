11dfd2051ec449eb514c5e0a0897e800974da3b0
maxv <maxv@NetBSD.org>
2018-05-01 08:16:34 +0000

    When the replay check fails, return EACCES instead of ENOBUFS.

--- sys/netipsec/xform_ah.c
+++ sys/netipsec/xform_ah.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: xform_ah.c,v 1.95 2018/04/28 15:45:16 maxv Exp $	*/
+/*	$NetBSD: xform_ah.c,v 1.96 2018/05/01 08:16:34 maxv Exp $	*/
 /*	$FreeBSD: xform_ah.c,v 1.1.4.1 2003/01/24 05:11:36 sam Exp $	*/
 /*	$OpenBSD: ip_ah.c,v 1.63 2001/06/26 06:18:58 angelos Exp $ */
 /*
@@ -39,7 +39,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: xform_ah.c,v 1.95 2018/04/28 15:45:16 maxv Exp $");
+__KERNEL_RCSID(0, "$NetBSD: xform_ah.c,v 1.96 2018/05/01 08:16:34 maxv Exp $");
 
 #if defined(_KERNEL_OPT)
 #include "opt_inet.h"
@@ -546,7 +546,7 @@ ah_input(struct mbuf *m, struct secasvar *sav, int skip, int protoff)
 		DPRINTF(("%s: packet replay failure: %s\n", __func__,
 		    ipsec_logsastr(sav, buf, sizeof(buf))));
 		stat = AH_STAT_REPLAY;
-		error = ENOBUFS;
+		error = EACCES;
 		goto bad;
 	}
 
--- sys/netipsec/xform_esp.c
+++ sys/netipsec/xform_esp.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: xform_esp.c,v 1.83 2018/05/01 08:13:37 maxv Exp $	*/
+/*	$NetBSD: xform_esp.c,v 1.84 2018/05/01 08:16:34 maxv Exp $	*/
 /*	$FreeBSD: xform_esp.c,v 1.2.2.1 2003/01/24 05:11:36 sam Exp $	*/
 /*	$OpenBSD: ip_esp.c,v 1.69 2001/06/26 06:18:59 angelos Exp $ */
 
@@ -39,7 +39,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: xform_esp.c,v 1.83 2018/05/01 08:13:37 maxv Exp $");
+__KERNEL_RCSID(0, "$NetBSD: xform_esp.c,v 1.84 2018/05/01 08:16:34 maxv Exp $");
 
 #if defined(_KERNEL_OPT)
 #include "opt_inet.h"
@@ -354,7 +354,7 @@ esp_input(struct mbuf *m, struct secasvar *sav, int skip, int protoff)
 		DPRINTF(("%s: packet replay check for %s\n", __func__,
 		    ipsec_logsastr(sav, logbuf, sizeof(logbuf))));
 		stat = ESP_STAT_REPLAY;
-		error = ENOBUFS; /* XXX */
+		error = EACCES;
 		goto out;
 	}
 
