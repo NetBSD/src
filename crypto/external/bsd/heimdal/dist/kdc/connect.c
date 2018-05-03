e64832f77951d5a3a1ac9ac3c4f53aca2bbf1776
martin <martin@NetBSD.org>
2018-05-03 15:09:43 +0000

    Pull up following revision(s) (requested by spz in ticket #1601):
    	crypto/external/bsd/heimdal/dist/kdc/connect.c: revision 1.3
    
    avoid busy-waiting on a dead child

--- crypto/external/bsd/heimdal/dist/kdc/connect.c
+++ crypto/external/bsd/heimdal/dist/kdc/connect.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: connect.c,v 1.1.1.2.4.1 2017/08/20 05:42:01 snj Exp $	*/
+/*	$NetBSD: connect.c,v 1.1.1.2.4.2 2018/05/03 15:09:43 martin Exp $	*/
 
 /*
  * Copyright (c) 1997-2005 Kungliga Tekniska Högskolan
@@ -1193,7 +1193,7 @@ start_kdc(krb5_context context,
                 break;
             default:
                 for (i=0; i < max_kdcs; i++) {
-                    if (pids[i] == 0) {
+                    if (pids[i] < 1) {
                         pids[i] = pid;
                         break;
                     }
