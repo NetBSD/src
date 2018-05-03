952a82f28fcaca4ca51c62588b56c61e301f00d3
christos <christos@NetBSD.org>
2018-05-03 21:43:33 +0000

    Fix COMPAT_NETBSD32 cmsg handling:
    1. alignment was wrong for > 1 message
    2. macros were doing incorrect pointer comparisons, fortunately ending
       the iteration early after the fists cmsg instead of crashing.
    3. don't output 32 bit ktrace records for cmsg. 32 bit programs running
       under emulation on 64 bit systems should produce 64 bit ktrace records
       so that the native ktrace can handle the records; remove extra arguments
       that are now not needed (the 32 bit msghdr).
    4. output the correct type for cmsg trace records.
    5. output all the cmsg records in traces instead of just the first one.
    
    Welcome to 8.99.15 because of the argument removal.
    
    XXX: Really all the code should be changed to use the CMSG_{FIRST,NXT}HDR
    macros...

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.559 2018/03/16 17:25:04 christos Exp $	*/
+/*	$NetBSD: param.h,v 1.560 2018/05/03 21:43:33 christos Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -67,7 +67,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	899001400	/* NetBSD 8.99.14 */
+#define	__NetBSD_Version__	899001500	/* NetBSD 8.99.15 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
