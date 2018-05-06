03744b1d1afef3b3ef683d06da823b90737b3809
kamil <kamil@NetBSD.org>
2018-05-06 13:40:50 +0000

    Remove an element from struct emul: e_tracesig
    e_tracesig used to be implemented for Darwin compat. Nowadays the Darwin
    compatiblity layer is gone and there are no other users.
    
    This functionality isn't used where it shall be used in the existing
    codebase.
    
    If we want to emulate debugging interfaces in compat layers we would need
    to implement that from scratch anyway. We would need to be bug compatible
    with other OSes too.
    
    Proposed on tech-kern@.
    
    Welcome to NetBSD 8.99.16!
    
    Sponsored by <The NetBSD Foundation>

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.560 2018/05/03 21:43:33 christos Exp $	*/
+/*	$NetBSD: param.h,v 1.561 2018/05/06 13:40:52 kamil Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -67,7 +67,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	899001500	/* NetBSD 8.99.15 */
+#define	__NetBSD_Version__	899001600	/* NetBSD 8.99.16 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
