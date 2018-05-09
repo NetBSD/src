0be2ad1a12876613b5ba05e498bf56596681bd5a
maxv <maxv@NetBSD.org>
2018-05-09 06:35:10 +0000

    Replace 	m_copym(m, 0, M_COPYALL, M_DONTWAIT) by 	m_copypacket(m, M_DONTWAIT) when it is clear that we are copying a packet (that has M_PKTHDR) and not a raw mbuf chain.

--- sys/net/raw_usrreq.c
+++ sys/net/raw_usrreq.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: raw_usrreq.c,v 1.60 2018/04/26 19:56:55 maxv Exp $	*/
+/*	$NetBSD: raw_usrreq.c,v 1.61 2018/05/09 06:35:10 maxv Exp $	*/
 
 /*
  * Copyright (c) 1980, 1986, 1993
@@ -36,7 +36,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: raw_usrreq.c,v 1.60 2018/04/26 19:56:55 maxv Exp $");
+__KERNEL_RCSID(0, "$NetBSD: raw_usrreq.c,v 1.61 2018/05/09 06:35:10 maxv Exp $");
 
 #include <sys/param.h>
 #include <sys/mbuf.h>
@@ -107,7 +107,7 @@ raw_input(struct mbuf *m0, ...)
 		if (last != NULL) {
 			struct mbuf *n;
 
-			if ((n = m_copym(m, 0, M_COPYALL, M_DONTWAIT)) == NULL ||
+			if ((n = m_copypacket(m, M_DONTWAIT)) == NULL ||
 			    sbappendaddr(&last->so_rcv, src, n, NULL) == 0)
 			{
 				if (n != NULL)
