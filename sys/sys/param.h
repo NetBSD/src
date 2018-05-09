ef8e6ced5ab316865b532d6dbef5258d9ebb57c8
kre <kre@NetBSD.org>
2018-05-09 19:55:35 +0000

    Cause a process's user and system times to become non-decreasing.
    This alters the invented values (ie: statistically calculated)
    that are returned - for small values, the values are likely going to
    be different than they were, but that's largely nonsense anyway
    (except that the sum of utime & stime does equal cpu time consumed
    by the process).   Once the values get large enough to be meaningful
    the difference made by this change will be in the noise, and irrelevant.
    
    This needs a couple of additions to struct proc, so we are now into 8.99.17

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.561 2018/05/06 13:40:52 kamil Exp $	*/
+/*	$NetBSD: param.h,v 1.562 2018/05/09 19:55:35 kre Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -67,7 +67,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	899001600	/* NetBSD 8.99.16 */
+#define	__NetBSD_Version__	899001700	/* NetBSD 8.99.17 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
