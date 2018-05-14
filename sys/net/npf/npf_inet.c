196e4c12beda5cb4b53b2388a7be0713d809f38d
martin <martin@NetBSD.org>
2018-05-14 16:17:19 +0000

    Pull up following revision(s) (requested by maxv in ticket #1605):
    	sys/net/npf/npf_inet.c: revision 1.45
    	sys/net/npf/npf_alg_icmp.c: revision 1.27-1.29
    
    Fix use-after-free.
    
    The nbuf can be reallocated as a result of caching 'enpc', so it is
    necessary to recache 'npc', otherwise it contains pointers to the freed
    mbuf - pointers which are then used in the ruleset machinery.
    We recache 'npc' when we are sure we won't use 'enpc' anymore, because
    'enpc' can be clobbered as a result of caching 'npc' (in other words,
    only one of the two can be cached at the same time).
    Also, we recache 'npc' unconditionally, because there is no way to know
    whether the nbuf got clobbered relatively to it. We can't use the
    NBUF_DATAREF_RESET flag, because it is stored in the nbuf and not in the
    cache.
    Discussed with rmind@.
    
    Change npf_cache_all so that it ensures the potential ICMP Query Id is in
    the nbuf. In such a way that we don't need to ensure that later.
    Change npfa_icmp4_inspect and npfa_icmp6_inspect so that they touch neither
    the nbuf nor npc. Adapt their callers accordingly.
    In the end, if a packet has a Query Id, we set NPC_ICMP_ID in npc and leave
    right away, without recaching npc (not needed since we didn't touch the
    nbuf).
    This fixes the handling of Query Id packets (that I broke in my previous
    commit), and also fixes another possible use-after-free.
    
    Ah, fix compilation. I tested my previous change by loading the kernel
    module from the filesystem, but the Makefile didn't have DIAGNOSTIC
    enabled, and the two KASSERTs I added did not compile properly.

--- sys/net/npf/npf_inet.c
+++ sys/net/npf/npf_inet.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: npf_inet.c,v 1.32 2014/07/20 00:37:41 rmind Exp $	*/
+/*	$NetBSD: npf_inet.c,v 1.32.10.1 2018/05/14 16:17:19 martin Exp $	*/
 
 /*-
  * Copyright (c) 2009-2014 The NetBSD Foundation, Inc.
@@ -39,7 +39,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: npf_inet.c,v 1.32 2014/07/20 00:37:41 rmind Exp $");
+__KERNEL_RCSID(0, "$NetBSD: npf_inet.c,v 1.32.10.1 2018/05/14 16:17:19 martin Exp $");
 
 #include <sys/param.h>
 #include <sys/types.h>
@@ -459,6 +459,11 @@ again:
 	}
 	hlen = npc->npc_hlen;
 
+	/*
+	 * Note: we guarantee that the potential "Query Id" field of the
+	 * ICMPv4/ICMPv6 packets is in the nbuf. This field is used in the
+	 * ICMP ALG.
+	 */
 	switch (npc->npc_proto) {
 	case IPPROTO_TCP:
 		/* Cache: layer 4 - TCP. */
@@ -475,13 +480,13 @@ again:
 	case IPPROTO_ICMP:
 		/* Cache: layer 4 - ICMPv4. */
 		npc->npc_l4.icmp = nbuf_advance(nbuf, hlen,
-		    offsetof(struct icmp, icmp_void));
+		    ICMP_MINLEN);
 		l4flags = NPC_LAYER4 | NPC_ICMP;
 		break;
 	case IPPROTO_ICMPV6:
 		/* Cache: layer 4 - ICMPv6. */
 		npc->npc_l4.icmp6 = nbuf_advance(nbuf, hlen,
-		    offsetof(struct icmp6_hdr, icmp6_data32));
+		    sizeof(struct icmp6_hdr));
 		l4flags = NPC_LAYER4 | NPC_ICMP;
 		break;
 	default:
