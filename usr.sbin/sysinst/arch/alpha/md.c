b77ea2f7104870bee433ff9166f836abd749fd62
joerg <joerg@NetBSD.org>
2018-05-18 12:23:22 +0000

    deconst -> __UNCONST, the former involves UB with NULL arithmetic.

--- usr.sbin/sysinst/arch/alpha/md.c
+++ usr.sbin/sysinst/arch/alpha/md.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: md.c,v 1.2 2014/08/03 16:09:38 martin Exp $ */
+/*	$NetBSD: md.c,v 1.3 2018/05/18 12:23:22 joerg Exp $ */
 
 /*
  * Copyright 1997 Piermont Information Systems Inc.
@@ -163,7 +163,7 @@ md_post_newfs(void)
 
 	if (error != 0)
 		process_menu(MENU_ok,
-		    deconst("Warning: disk is probably not bootable"));
+		    __UNCONST("Warning: disk is probably not bootable"));
 
 	return 0;
 }
