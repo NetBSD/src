cf6baef81b97fdf2170656581ed4f0d03d3b52b2
kamil <kamil@NetBSD.org>
2018-05-22 04:32:56 +0000

    Extend ATF tests in t_trapsignal.sh to verify software breakpoint traps
    There are at least four types of SIGTRAP events:
     - software/hardware single step (trace trap)
     - software breakpoint
     - hardware breakpoint/watchpoint
     - kernel event (exec, fork, vfork, vfork-done, lwp-create, lwp-exit)
    
    A program can execute software breakpoint without the context of being
    traced and this is a regular crash signal emitting SIGTRAP (TRAP_BRKPT).
    
    Rename original trap_* tests (trap_simple, trap_handle, trap_mask,
    trap_handle_recurse and trap_ignore) to segv_* tests and restrict them for
    SIGSEGV.
    
    Add new tests: trap_* testing the same scenarios as segv_ ones, however
    verifying the software breakpoint trap (SIGTRAP).
    
    Keep the original name of h_segv.c, and extend it for software breakpoint
    events.
    
    The purpose of these tests is to verify SIGTRAP kernel paths without the
    ptrace(2) context.
    
    All tests pass.
    
    Sponsored by <The NetBSD Foundation>

--- tests/kernel/t_trapsignal.sh
+++ tests/kernel/t_trapsignal.sh
@@ -1,4 +1,4 @@
-# $NetBSD: t_trapsignal.sh,v 1.2 2018/05/21 08:49:03 kamil Exp $
+# $NetBSD: t_trapsignal.sh,v 1.3 2018/05/22 04:32:56 kamil Exp $
 #
 # Copyright (c) 2017 The NetBSD Foundation, Inc.
 # All rights reserved.
@@ -29,60 +29,128 @@
 #
 
 HELPER=$(atf_get_srcdir)/h_segv
+atf_test_case segv_simple
+segv_simple()
+{
+	atf_set "descr" "Test unhandled SIGSEGV with the right exit code"
+}
+segv_simple_body()
+{
+	atf_check -s signal:11 -o "inline:" -e "inline:" \
+		${HELPER} segv recurse
+}
+
+atf_test_case segv_handle
+segv_handle()
+{
+	atf_set "descr" "Test handled SIGSEGV traps call the signal handler"
+}
+segv_handle_body()
+{
+	atf_check -s exit:0 -o "inline:" -e "inline:got 11\n" \
+		${HELPER} segv handle
+}
+
+atf_test_case segv_mask
+segv_mask()
+{
+	atf_set "descr" "Test that masking SIGSEGV get reset"
+}
+segv_mask_body()
+{
+	atf_check -s signal:11 -o "inline:" -e "inline:" \
+		${HELPER} segv mask
+}
+
+atf_test_case segv_handle_mask
+segv_handle_mask()
+{
+	atf_set "descr" "Test handled and masked SIGSEGV traps get reset"
+}
+segv_handle_mask_body()
+{
+	atf_check -s signal:11 -o "inline:" -e "inline:" \
+		${HELPER} segv mask handle
+}
+
+atf_test_case segv_handle_recurse
+segv_handle_recurse()
+{
+	atf_set "descr" "Test that receiving SIGSEGV in the handler resets"
+}
+
+segv_handle_recurse_body()
+{
+	atf_check -s signal:11 -o "inline:" -e "inline:got 11\n" \
+		${HELPER} segv handle recurse
+}
+
+atf_test_case segv_ignore
+segv_ignore()
+{
+	atf_set "descr" "Test ignored SIGSEGV trap with right exit code"
+}
+
+segv_ignore_body()
+{
+	atf_check -s signal:11 -o "inline:" -e "inline:" \
+		${HELPER} segv ignore
+}
+
 atf_test_case trap_simple
 trap_simple()
 {
-	atf_set "descr" "Test unhandled traps exit with the right exit code"
+	atf_set "descr" "Test unhandled SIGTRAP with the right exit code"
 }
 trap_simple_body()
 {
-	atf_check -s signal:11 -o "inline:" -e "inline:" \
-		${HELPER} recurse
+	atf_check -s signal:5 -o "inline:" -e "inline:" \
+		${HELPER} trap recurse
 }
 
 atf_test_case trap_handle
 trap_handle()
 {
-	atf_set "descr" "Test handled traps call the signal handler"
+	atf_set "descr" "Test handled SIGTRAP traps call the signal handler"
 }
 trap_handle_body()
 {
-	atf_check -s exit:0 -o "inline:" -e "inline:got 11\n" \
-		${HELPER} handle
+	atf_check -s exit:0 -o "inline:" -e "inline:got 5\n" \
+		${HELPER} trap handle
 }
 
 atf_test_case trap_mask
 trap_mask()
 {
-	atf_set "descr" "Test that masking the trapped signal get reset"
+	atf_set "descr" "Test that masking the trapped SIGTRAP signal get reset"
 }
 trap_mask_body()
 {
-	atf_check -s signal:11 -o "inline:" -e "inline:" \
-		${HELPER} mask
+	atf_check -s signal:5 -o "inline:" -e "inline:" \
+		${HELPER} trap mask
 }
 
 atf_test_case trap_handle_mask
 trap_handle_mask()
 {
-	atf_set "descr" "Test handled and masked traps get reset"
+	atf_set "descr" "Test handled and masked SIGTRAP traps get reset"
 }
 trap_handle_mask_body()
 {
-	atf_check -s signal:11 -o "inline:" -e "inline:" \
-		${HELPER} mask handle
+	atf_check -s signal:5 -o "inline:" -e "inline:" \
+		${HELPER} trap mask handle
 }
 
 atf_test_case trap_handle_recurse
 trap_handle_recurse()
 {
-	atf_set "descr" "Test that receiving the trap in the handler resets"
+	atf_set "descr" "Test that receiving SIGTRAP in the handler resets"
 }
 
 trap_handle_recurse_body()
 {
-	atf_check -s signal:11 -o "inline:" -e "inline:got 11\n" \
-		${HELPER} handle recurse
+	atf_check -s signal:5 -o "inline:" -e "inline:got 5\n" \
+		${HELPER} trap handle recurse
 }
 
 atf_test_case trap_ignore
@@ -93,12 +161,18 @@ trap_ignore()
 
 trap_ignore_body()
 {
-	atf_check -s signal:11 -o "inline:" -e "inline:" \
-		${HELPER} ignore
+	atf_check -s signal:5 -o "inline:" -e "inline:" \
+		${HELPER} trap ignore
 }
 
 atf_init_test_cases()
 {
+	atf_add_test_case segv_simple
+	atf_add_test_case segv_handle
+	atf_add_test_case segv_mask
+	atf_add_test_case segv_handle_recurse
+	atf_add_test_case segv_ignore
+
 	atf_add_test_case trap_simple
 	atf_add_test_case trap_handle
 	atf_add_test_case trap_mask
