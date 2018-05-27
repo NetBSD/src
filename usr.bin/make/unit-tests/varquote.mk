833bb5bb70cea687988eee7dc1b1466a1b746f37
christos <christos@NetBSD.org>
2018-05-27 01:14:50 +0000

    - Introduce :q modifier for make variables and make it double escape $'s so   that passing variables to recursive makes with :q works as expected. - Revert :Q to work as before. - Adjust makefiles that use recursive make to use :q
    Discussed on tech-toolchain@
    XXX: pullup 8

--- usr.bin/make/unit-tests/varquote.mk
+++ usr.bin/make/unit-tests/varquote.mk
@@ -1,12 +1,13 @@
-# $NetBSD: varquote.mk,v 1.1 2018/05/24 00:25:44 christos Exp $
+# $NetBSD: varquote.mk,v 1.2 2018/05/27 01:14:51 christos Exp $
 #
-# Test VAR:Q modifier
+# Test VAR:q modifier
 
 .if !defined(REPROFLAGS)
 REPROFLAGS+=    -fdebug-prefix-map=\$$NETBSDSRCDIR=/usr/src
 REPROFLAGS+=    -fdebug-regex-map='/usr/src/(.*)/obj$$=/usr/obj/\1'
 all:
-	@${MAKE} -f ${MAKEFILE} REPROFLAGS=${REPROFLAGS:Q}
+	@${MAKE} -f ${MAKEFILE} REPROFLAGS=${REPROFLAGS:S/\$/&&/g:Q}
+	@${MAKE} -f ${MAKEFILE} REPROFLAGS=${REPROFLAGS:q}
 .else
 all:
 	@echo ${REPROFLAGS}
