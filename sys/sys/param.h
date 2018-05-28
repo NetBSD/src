5b926a30ed90e752bb41f1e231a1e1052e8b37e3
chs <chs@NetBSD.org>
2018-05-28 21:05:16 +0000

    welcome to 8.99.13:  - uao_create()'s size is now 64-bit  - new genfs op to let a file system constrain the range of pages    passed to GOP_WRITE()  - various module interface changes  - changes to various MD hooks for the new dtrace

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.563 2018/05/19 11:40:22 jdolecek Exp $	*/
+/*	$NetBSD: param.h,v 1.564 2018/05/28 21:05:16 chs Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -67,7 +67,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	899001800	/* NetBSD 8.99.18 */
+#define	__NetBSD_Version__	899001900	/* NetBSD 8.99.19 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
