defb6b6ddc595a2a77bd37b2d01ff89341730763
chs <chs@NetBSD.org>
2018-05-28 21:04:41 +0000

    add copyin_pid(), to copyin from a different user address space.

--- sys/sys/systm.h
+++ sys/sys/systm.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: systm.h,v 1.275 2018/02/04 17:31:51 maxv Exp $	*/
+/*	$NetBSD: systm.h,v 1.276 2018/05/28 21:04:41 chs Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1988, 1991, 1993
@@ -267,6 +267,7 @@ typedef int	(*copyout_t)(const void *, void *, size_t);
 
 int	copyin_proc(struct proc *, const void *, void *, size_t);
 int	copyout_proc(struct proc *, const void *, void *, size_t);
+int	copyin_pid(pid_t, const void *, void *, size_t);
 int	copyin_vmspace(struct vmspace *, const void *, void *, size_t);
 int	copyout_vmspace(struct vmspace *, const void *, void *, size_t);
 
