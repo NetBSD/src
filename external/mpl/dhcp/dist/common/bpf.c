a4d091ddc63203e839406a1e9db9375b36988b66
christos <christos@NetBSD.org>
2018-06-01 00:42:49 +0000

    PR/50893: Bruce Lilly: Handle carp interfaces.
    XXX: pullup-8 (in src/external/bsd/dhcp/dist/common/bpf.c)

--- external/mpl/dhcp/dist/common/bpf.c
+++ external/mpl/dhcp/dist/common/bpf.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: bpf.c,v 1.2 2018/04/07 22:37:29 christos Exp $	*/
+/*	$NetBSD: bpf.c,v 1.3 2018/06/01 00:42:49 christos Exp $	*/
 
 /* bpf.c
 
@@ -35,7 +35,7 @@
  */
 
 #include <sys/cdefs.h>
-__RCSID("$NetBSD: bpf.c,v 1.2 2018/04/07 22:37:29 christos Exp $");
+__RCSID("$NetBSD: bpf.c,v 1.3 2018/06/01 00:42:49 christos Exp $");
 
 #include "dhcpd.h"
 #if defined (USE_BPF_SEND) || defined (USE_BPF_RECEIVE)	\
@@ -674,6 +674,9 @@ get_hw_addr(const char *name, struct hardware *hw) {
 	 */
         switch (sa->sdl_type) {
                 case IFT_ETHER:
+#ifdef IFT_CARP
+		case IFT_CARP:
+#endif
 #if defined (IFT_L2VLAN)
 		case IFT_L2VLAN:
 #endif
