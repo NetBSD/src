b83a0e7a122a5814fdcc92a76b44cee9a1a5f980
rin <rin@NetBSD.org>
2018-06-03 08:08:36 +0000

    Make sure that every wide char occupies at least one display width:   - Replace non-printable multibyte char with ?-symbol.   - Put space before non-spacing char.
    Fix problems reported in PR bin/53164 and
    PR bin/53323, that are because we did not take into account non-printable
    multibyte char of wctob(wc) == EOF && wcwidth(wc) == -1.

--- external/bsd/nvi/dist/common/conv.h
+++ external/bsd/nvi/dist/common/conv.h
@@ -1,9 +1,14 @@
-/*	$NetBSD: conv.h,v 1.2 2013/11/22 15:52:05 christos Exp $	*/
+/*	$NetBSD: conv.h,v 1.3 2018/06/03 08:08:36 rin Exp $	*/
+
+/*
+ * We ensure that every wide char occupies at least one display width.
+ * See vs_line.c for more details.
+ */
+#define WIDE_COL(sp, ch)						\
+	(CHAR_WIDTH(sp, ch) >= 0 ? CHAR_WIDTH(sp, ch) : 1)
+
 #define KEY_COL(sp, ch)							\
-	(INTISWIDE(ch) ? 						\
-	    (CHAR_WIDTH(sp, ch) >= 0) ?					\
-	       (size_t)CHAR_WIDTH(sp, ch) : 1 /* extra space */		\
-	    : KEY_LEN(sp,ch))
+	(INTISWIDE(ch) ? (size_t)WIDE_COL(sp, ch) : KEY_LEN(sp, ch))
 
 struct _conv_win {
     void    *bp1;
