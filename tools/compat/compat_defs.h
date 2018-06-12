4fabbeb31ba179c73887564d2154858aae124494
christos <christos@NetBSD.org>
2018-06-12 23:52:56 +0000

    Move the typedefs before the header inclusions (except <features.h>) and explain why.

--- tools/compat/compat_defs.h
+++ tools/compat/compat_defs.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: compat_defs.h,v 1.110 2018/06/11 18:48:24 maya Exp $	*/
+/*	$NetBSD: compat_defs.h,v 1.111 2018/06/12 23:52:56 christos Exp $	*/
 
 #ifndef	__NETBSD_COMPAT_DEFS_H__
 #define	__NETBSD_COMPAT_DEFS_H__
@@ -29,6 +29,48 @@
 #undef _POSIX_C_SOURCE
 #define __USE_ISOC99 1
 #endif	/* __linux__ && HAVE_FEATURES_H */
+
+/*
+ * Type substitutes.
+ * These are controlled via HAVE_TYPE protections and some of them are needed
+ * in other header files (in the build tree not in the host). This is because
+ * we are mixing the header files (which don't need them) with extensions
+ * such as the Solaris headers which depend on types defined by the native
+ * system headers, and might be missing in the build host.
+ */
+
+#if !HAVE_ID_T
+typedef unsigned int id_t;
+#endif
+
+#if !HAVE_SOCKLEN_T
+/*
+ * This is defined as int for compatibility with legacy systems (and not
+ * unsigned int), since universally it was int in most systems that did not
+ * define it.
+ */
+typedef int socklen_t;
+#endif
+
+#if !HAVE_U_LONG
+typedef unsigned long u_long;
+#endif
+
+#if !HAVE_U_CHAR
+typedef unsigned char u_char;
+#endif
+
+#if !HAVE_U_INT
+typedef unsigned int u_int;
+#endif
+
+#if !HAVE_U_SHORT
+typedef unsigned short u_short;
+#endif
+
+#if !HAVE_U_LONGLONG_T
+typedef uint64_t u_longlong_t;
+#endif
 
 /* System headers needed for (re)definitions below. */
 
@@ -191,37 +233,6 @@ struct group;
 # if HAVE_NDIR_H
 #  include <ndir.h>
 # endif
-#endif
-
-/* Type substitutes. */
-
-#if !HAVE_ID_T
-typedef unsigned int id_t;
-#endif
-
-#if !HAVE_SOCKLEN_T
-/*
- * This is defined as int for compatibility with legacy systems (and not
- * unsigned int), since universally it was int in most systems that did not
- * define it.
- */
-typedef int socklen_t;
-#endif
-
-#if !HAVE_U_LONG
-typedef unsigned long u_long;
-#endif
-
-#if !HAVE_U_CHAR
-typedef unsigned char u_char;
-#endif
-
-#if !HAVE_U_INT
-typedef unsigned int u_int;
-#endif
-
-#if !HAVE_U_SHORT
-typedef unsigned short u_short;
 #endif
 
 /* Prototypes for replacement functions. */
@@ -1296,10 +1307,6 @@ __GEN_ENDIAN_DEC(64, le)
 			(((y) <<  0) & 0x000000ff)))
 #ifndef NBBY
 #define NBBY 8
-#endif
-
-#if !HAVE_U_LONGLONG_T
-typedef uint64_t u_longlong_t;
 #endif
 
 #if !HAVE_U_QUAD_T
