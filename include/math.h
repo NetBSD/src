d7f5ae85fbec75d67a4a0ff898389da9e75521e4
christos <christos@NetBSD.org>
2018-06-24 23:55:29 +0000

    Fix the rest of the functions that have been made visible by xopen5, posix 2017, and c99.

--- include/math.h
+++ include/math.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: math.h,v 1.64 2018/06/24 22:07:46 christos Exp $	*/
+/*	$NetBSD: math.h,v 1.65 2018/06/24 23:55:29 christos Exp $	*/
 
 /*
  * ====================================================
@@ -253,6 +253,19 @@ double	y1(double);
 double	yn(int, double);
 
 #if (_XOPEN_SOURCE - 0) >= 500 || defined(_NETBSD_SOURCE)
+double	scalb(double, double);
+#endif /* (_XOPEN_SOURCE - 0) >= 500 || defined(_NETBSD_SOURCE)*/
+#endif /* _XOPEN_SOURCE || _NETBSD_SOURCE */
+
+/*
+ * ISO C99
+ */
+#if !defined(_ANSI_SOURCE) && !defined(_POSIX_C_SOURCE) && \
+    !defined(_XOPEN_SOURCE) || \
+    ((__STDC_VERSION__ - 0) >= 199901L) || \
+    ((_POSIX_C_SOURCE - 0) >= 200809L) || \
+    ((_XOPEN_SOURCE  - 0) >= 500) || \
+    defined(_ISOC99_SOURCE) || defined(_NETBSD_SOURCE)
 double	acosh(double);
 double	asinh(double);
 double	atanh(double);
@@ -263,13 +276,9 @@ double	log1p(double);
 double	logb(double);
 double	nextafter(double, double);
 double	remainder(double, double);
-double	scalb(double, double);
-#endif /* (_XOPEN_SOURCE - 0) >= 500 || defined(_NETBSD_SOURCE)*/
-#endif /* _XOPEN_SOURCE || _NETBSD_SOURCE */
+double	rint(double);
+#endif
 
-/*
- * ISO C99
- */
 #if !defined(_ANSI_SOURCE) && !defined(_POSIX_C_SOURCE) && \
     !defined(_XOPEN_SOURCE) || \
     ((__STDC_VERSION__ - 0) >= 199901L) || \
@@ -380,13 +389,13 @@ long double	lgammal(long double);
 long double	tgammal(long double);
 
 /* 7.12.9 nearest integer */
+
 /* LONGLONG */
 long long int	llrint(double);
 long int	lround(double);
 /* LONGLONG */
 long long int	llround(double);
 long int	lrint(double);
-double	rint(double);
 double	round(double);
 double	trunc(double);
 
