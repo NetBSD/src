5e906befd671c46580a117b41844927ac806ebdf
mrg <mrg@NetBSD.org>
2018-06-24 12:55:36 +0000

    fix the previous.  the code was right, just badly formatted before.
    should fix infinite loops reported in some cases.

--- games/tetris/scores.c
+++ games/tetris/scores.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: scores.c,v 1.23 2018/02/04 08:48:05 mrg Exp $	*/
+/*	$NetBSD: scores.c,v 1.24 2018/06/24 12:55:36 mrg Exp $	*/
 
 /*-
  * Copyright (c) 1992, 1993
@@ -830,9 +830,8 @@ checkscores(struct highscore *hs, int num)
 				continue;
 			}
 		}
-	}
-        if (sp->hs_level < NLEVELS && sp->hs_level >= 0) {
-    		levelfound[sp->hs_level] = 1;
+		if (sp->hs_level < NLEVELS && sp->hs_level >= 0)
+			levelfound[sp->hs_level] = 1;
 		i++, sp++;
 	}
 	return (num > MAXHISCORES ? MAXHISCORES : num);
