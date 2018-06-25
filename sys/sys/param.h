ca361bd25bb3d9a3ee0a958a30e3657138ab5b3b
msaitoh <msaitoh@NetBSD.org>
2018-06-25 03:23:55 +0000

    Bump version to 8.99.20 (removal of bpf_tap()).

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.564 2018/05/28 21:05:16 chs Exp $	*/
+/*	$NetBSD: param.h,v 1.565 2018/06/25 03:23:55 msaitoh Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -67,7 +67,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	899001900	/* NetBSD 8.99.19 */
+#define	__NetBSD_Version__	899002000	/* NetBSD 8.99.20 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
