c86d9d6aa7ac93d23f90e391b3edca93d768ed2b
jmcneill <jmcneill@NetBSD.org>
2018-06-27 16:27:06 +0000

    Import dts from Linux 4.18-rc2

--- sys/external/gpl2/dts/dist/include/dt-bindings/clock/imx6qdl-clock.h
+++ sys/external/gpl2/dts/dist/include/dt-bindings/clock/imx6qdl-clock.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: imx6qdl-clock.h,v 1.1.1.1 2017/06/15 20:14:23 jmcneill Exp $	*/
+/*	$NetBSD: imx6qdl-clock.h,v 1.1.1.2 2018/06/27 16:27:08 jmcneill Exp $	*/
 
 /*
  * Copyright 2014 Freescale Semiconductor, Inc.
@@ -273,6 +273,8 @@
 #define IMX6QDL_CLK_PRE_AXI			258
 #define IMX6QDL_CLK_MLB_SEL			259
 #define IMX6QDL_CLK_MLB_PODF			260
-#define IMX6QDL_CLK_END				261
+#define IMX6QDL_CLK_EPIT1			261
+#define IMX6QDL_CLK_EPIT2			262
+#define IMX6QDL_CLK_END				263
 
 #endif /* __DT_BINDINGS_CLOCK_IMX6QDL_H */
