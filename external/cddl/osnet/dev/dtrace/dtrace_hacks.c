9f4e5ac9826f801438e1b7a0ba22d3ab802b88ee
kamil <kamil@NetBSD.org>
2018-06-29 11:33:46 +0000

    Rework the changes to DTRACE/ZFS files that were fixing Clang/LLVM build
    Rever changes to upstream files and replace them with additional -Wno-
    compiler argument flags.
    
    Tested with base LLVM.
    
    Requested by <chuq>

--- external/cddl/osnet/dev/dtrace/dtrace_hacks.c
+++ external/cddl/osnet/dev/dtrace/dtrace_hacks.c
@@ -1,9 +1,7 @@
-/*	$NetBSD: dtrace_hacks.c,v 1.7 2018/06/06 15:54:03 kamil Exp $	*/
+/*	$NetBSD: dtrace_hacks.c,v 1.8 2018/06/29 11:33:46 kamil Exp $	*/
 
 /* $FreeBSD: head/sys/cddl/dev/dtrace/dtrace_hacks.c 281916 2015-04-24 03:19:30Z markj $ */
 /* XXX Hacks.... */
-
-#include <sys/cdefs.h>
 
 dtrace_cacheid_t dtrace_predcache_id;
 
@@ -11,5 +9,5 @@ boolean_t
 priv_policy_only(const cred_t *cr, int b, boolean_t c)
 {
 
-	return kauth_authorize_generic(__UNCONST(cr), KAUTH_GENERIC_ISSUSER, NULL) == 0;
+	return kauth_authorize_generic(cr, KAUTH_GENERIC_ISSUSER, NULL) == 0;
 }
