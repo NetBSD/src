472a4c081abd791dd3772394caee9c6aba07cf42
kre <kre@NetBSD.org>
2018-07-10 11:00:40 +0000

    Avoid attempting to call arp related functions if there is no arp in the kernel.

--- sys/net/if_llatbl.c
+++ sys/net/if_llatbl.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: if_llatbl.c,v 1.28 2018/07/10 01:23:13 ozaki-r Exp $	*/
+/*	$NetBSD: if_llatbl.c,v 1.29 2018/07/10 11:00:40 kre Exp $	*/
 /*
  * Copyright (c) 2004 Luigi Rizzo, Alessandro Cerri. All rights reserved.
  * Copyright (c) 2004-2008 Qing Li. All rights reserved.
@@ -691,10 +691,12 @@ lla_rt_output(const u_char rtm_type, const int rtm_flags, const time_t rtm_expir
 		 */
 		if (lle != NULL) {
 			size_t pkts_dropped = llentry_free(lle);
+#if defined(INET) && NARP > 0
 			if (dst->sa_family == AF_INET) {
 				arp_stat_add(ARP_STAT_DFRDROPPED,
 				    (uint64_t)pkts_dropped);
 			}
+#endif
 		}
 
 		lle = lla_create(llt, 0, dst, rt);
