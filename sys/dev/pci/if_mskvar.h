9c7364ec08a748c7300fc7adbbfbf73a939f6e49
jdolecek <jdolecek@NetBSD.org>
2018-07-10 18:42:37 +0000

    add the RX refill callout (forgot to commit with if_msk.c change)

--- sys/dev/pci/if_mskvar.h
+++ sys/dev/pci/if_mskvar.h
@@ -1,5 +1,5 @@
 /*	$OpenBSD: if_mskvar.h,v 1.3 2006/12/28 16:34:42 kettenis Exp $	*/
-/*	$NetBSD: if_mskvar.h,v 1.17 2018/07/03 19:56:01 jdolecek Exp $	*/
+/*	$NetBSD: if_mskvar.h,v 1.18 2018/07/10 18:42:37 jdolecek Exp $	*/
 
 /*-
  * Copyright (c) 2003 The NetBSD Foundation, Inc.
@@ -229,6 +229,7 @@ struct sk_if_softc {
 	int			sk_pktlen;	/* XXX set but unused for now */
 	int			sk_link;
 	struct callout		sk_tick_ch;
+	struct callout		sk_tick_rx;
 	struct msk_chain_data	sk_cdata;
 	struct msk_ring_data	*sk_rdata;
 	bus_dmamap_t		sk_ring_map;
