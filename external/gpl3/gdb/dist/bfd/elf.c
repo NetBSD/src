53383571cc57349fd8d5791c059c6414df8c64bb
christos <christos@NetBSD.org>
2018-07-12 23:18:28 +0000

    aarch64 does not have PT_STEP so we start from 0.

--- external/gpl3/gdb/dist/bfd/elf.c
+++ external/gpl3/gdb/dist/bfd/elf.c
@@ -10041,6 +10041,7 @@ elfcore_grok_netbsd_note (bfd *abfd, Elf_Internal_Note *note)
       /* On the Alpha, SPARC (32-bit and 64-bit), PT_GETREGS == mach+0 and
 	 PT_GETFPREGS == mach+2.  */
 
+    case bfd_arch_aarch64:
     case bfd_arch_alpha:
     case bfd_arch_sparc:
       switch (note->type)
