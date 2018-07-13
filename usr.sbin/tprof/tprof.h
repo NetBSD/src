d1d4c687f9d747e8492824a6b58838aa0643fff5
maxv <maxv@NetBSD.org>
2018-07-13 11:03:36 +0000

    Merge tpfmt(1) into tprof(8). We want to have access to everything with only one tool. The code is copied mostly as-is, and the functionality is available via the "analyze" command.
    Eg:
    	tprof monitor -e llc-misses:k -o myfile.out sleep 20
    	tprof analyze < myfile.out
    
    Will move soon, I don't like the reading via stdin.

--- usr.sbin/tprof/tprof.h
+++ usr.sbin/tprof/tprof.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: tprof.h,v 1.1 2018/07/13 07:56:29 maxv Exp $	*/
+/*	$NetBSD: tprof.h,v 1.2 2018/07/13 11:03:36 maxv Exp $	*/
 
 /*
  * Copyright (c) 2018 The NetBSD Foundation, Inc.
@@ -33,3 +33,4 @@ int tprof_event_init(uint32_t);
 void tprof_event_list(void);
 void tprof_event_lookup(const char *, struct tprof_param *);
 
+void tprof_analyze(int, char **);
