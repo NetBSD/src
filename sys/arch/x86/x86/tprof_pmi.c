6d8cce7a501d87e87fbdd1e29f457e1d5f9eb83a
maxv <maxv@NetBSD.org>
2018-07-13 07:56:29 +0000

    Revamp tprof.
    Rewrite the Intel backend to use the generic PMC interface, which is
    available on all Intel CPUs. Synchronize the AMD backend with the new
    interface.
    
    The kernel identifies the PMC interface, and gives its id to userland.
    Userland then queries the events itself (via cpuid etc). These events
    depend on the PMC interface.
    
    The tprof utility is rewritten to allow the user to choose which event
    to count (which was not possible until now, the event was hardcoded in
    the backend). The command line format is based on usr.bin/pmc, eg:
    
    	tprof -e llc-misses:k -o output sleep 20
    
    The man page is updated too, but the arguments will likely change soon
    anyway so it doesn't matter a lot.
    
    The tprof utility has three tables:
    
    	Intel Architectural Version 1
    	Intel Skylake/Kabylake
    	AMD Family 10h
    
    A CPU can support a combination of tables. For example Kabylake has
    Intel-Architectural-Version-1 and its own Intel-Kabylake table.
    
    For now the Intel Skylake/Kabylake table contains only one event, just
    to demonstrate that the combination of tables works. Tested on an
    Intel Core i5 Kabylake.
    
    The code for AMD Family 10h is taken from the code I had written for
    usr.bin/pmc. I haven't tested it yet, but it's the same as pmc(1), so
    I guess it works as-is.
    
    The whole thing is written in such a way that (I think) it is not
    complicated to add more CPU models, and more architectures (other than
    x86).

--- sys/arch/x86/x86/tprof_pmi.c
+++ sys/arch/x86/x86/tprof_pmi.c
@@ -1,6 +1,35 @@
-/*	$NetBSD: tprof_pmi.c,v 1.14 2017/05/23 08:54:39 nonaka Exp $	*/
+/*	$NetBSD: tprof_pmi.c,v 1.15 2018/07/13 07:56:29 maxv Exp $	*/
 
-/*-
+/*
+ * Copyright (c) 2018 The NetBSD Foundation, Inc.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to The NetBSD Foundation
+ * by Maxime Villard.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/*
  * Copyright (c)2008,2009 YAMAMOTO Takashi,
  * All rights reserved.
  *
@@ -27,7 +56,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: tprof_pmi.c,v 1.14 2017/05/23 08:54:39 nonaka Exp $");
+__KERNEL_RCSID(0, "$NetBSD: tprof_pmi.c,v 1.15 2018/07/13 07:56:29 maxv Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -46,157 +75,92 @@ __KERNEL_RCSID(0, "$NetBSD: tprof_pmi.c,v 1.14 2017/05/23 08:54:39 nonaka Exp $"
 #include <x86/nmi.h>
 
 #include <machine/cpufunc.h>
-#include <machine/cputypes.h>	/* CPUVENDER_* */
+#include <machine/cputypes.h>	/* CPUVENDOR_* */
 #include <machine/cpuvar.h>	/* cpu_vendor */
 #include <machine/i82489reg.h>
 #include <machine/i82489var.h>
 
-#define	ESCR_T1_USR		__BIT(0)
-#define	ESCR_T1_OS		__BIT(1)
-#define	ESCR_T0_USR		__BIT(2)
-#define	ESCR_T0_OS		__BIT(3)
-#define	ESCR_TAG_ENABLE		__BIT(4)
-#define	ESCR_TAG_VALUE		__BITS(5, 8)
-#define	ESCR_EVENT_MASK		__BITS(9, 24)
-#define	ESCR_EVENT_SELECT	__BITS(25, 30)
+#define	PERFEVTSEL_EVENT_SELECT	__BITS(0, 7)
+#define	PERFEVTSEL_UNIT_MASK	__BITS(8, 15)
+#define	PERFEVTSEL_USR		__BIT(16)
+#define	PERFEVTSEL_OS		__BIT(17)
+#define	PERFEVTSEL_E		__BIT(18)
+#define	PERFEVTSEL_PC		__BIT(19)
+#define	PERFEVTSEL_INT		__BIT(20)
+#define	PERFEVTSEL_EN		__BIT(22)
+#define	PERFEVTSEL_INV		__BIT(23)
+#define	PERFEVTSEL_COUNTER_MASK	__BITS(24, 31)
 
-#define	CCCR_ENABLE		__BIT(12)
-#define	CCCR_ESCR_SELECT	__BITS(13, 15)
-#define	CCCR_MUST_BE_SET	__BITS(16, 17)
-#define	CCCR_COMPARE		__BIT(18)
-#define	CCCR_COMPLEMENT		__BIT(19)
-#define	CCCR_THRESHOLD		__BITS(20, 23)
-#define	CCCR_EDGE		__BIT(24)
-#define	CCCR_FORCE_OVF		__BIT(25)
-#define	CCCR_OVF_PMI_T0		__BIT(26)
-#define	CCCR_OVF_PMI_T1		__BIT(27)
-#define	CCCR_CASCADE		__BIT(30)
-#define	CCCR_OVF		__BIT(31)
+#define CPUID_0A_VERSION	__BITS(0, 7)
+#define CPUID_0A_NCOUNTERS	__BITS(8, 15)
+#define CPUID_0A_BITWIDTH	__BITS(16, 23)
 
-struct msrs {
-	u_int msr_cccr;
-	u_int msr_escr;
-	u_int msr_counter;
-};
-
-/*
- * parameters (see 253669.pdf Table A-6)
- *
- * XXX should not hardcode
- */
-
-static const struct msrs msrs[] = {
-	{
-		.msr_cccr = 0x360,	/* MSR_BPU_CCCR0 */
-		.msr_escr = 0x3a2,	/* MSR_FSB_ESCR0 */
-		.msr_counter = 0x300,	/* MSR_BPU_COUNTER0 */
-	},
-	{
-		.msr_cccr = 0x362,	/* MSR_BPU_CCCR2 */
-		.msr_escr = 0x3a3,	/* MSR_FSB_ESCR1 */
-		.msr_counter = 0x302,	/* MSR_BPU_COUNTER2 */
-	},
-};
-static const u_int cccr_escr_select = 0x6;	/* MSR_FSB_ESCR? */
-static const u_int escr_event_select = 0x13;	/* global_power_events */
-static const u_int escr_event_mask = 0x1;	/* running */
-
+static uint64_t counter_bitwidth;
 static uint64_t counter_val = 5000000;
 static uint64_t counter_reset_val;
-static uint32_t tprof_pmi_lapic_saved[MAXCPUS];
 
-static nmi_handler_t *tprof_pmi_nmi_handle;
-static tprof_backend_cookie_t *tprof_cookie;
+static uint32_t intel_lapic_saved[MAXCPUS];
+static nmi_handler_t *intel_nmi_handle;
+static tprof_param_t intel_param;
 
 static void
-tprof_pmi_start_cpu(void *arg1, void *arg2)
+tprof_intel_start_cpu(void *arg1, void *arg2)
 {
 	struct cpu_info * const ci = curcpu();
-	const struct msrs *msr;
-	uint64_t cccr;
-	uint64_t escr;
+	uint64_t evtval;
 
-	if (ci->ci_smt_id >= 2) {
-		printf("%s: ignoring %s smt id=%u",
-		    __func__, device_xname(ci->ci_dev),
-		    (u_int)ci->ci_smt_id);
-		return;
-	}
-	msr = &msrs[ci->ci_smt_id];
-	escr = __SHIFTIN(escr_event_mask, ESCR_EVENT_MASK) |
-	    __SHIFTIN(escr_event_select, ESCR_EVENT_SELECT);
-	cccr = CCCR_ENABLE | __SHIFTIN(cccr_escr_select, CCCR_ESCR_SELECT) |
-	    CCCR_MUST_BE_SET;
-	if (ci->ci_smt_id == 0) {
-		escr |= ESCR_T0_OS | ESCR_T0_USR;
-		cccr |= CCCR_OVF_PMI_T0;
-	} else {
-		escr |= ESCR_T1_OS | ESCR_T0_USR;
-		cccr |= CCCR_OVF_PMI_T1;
-	}
+	evtval =
+	    __SHIFTIN(intel_param.p_event, PERFEVTSEL_EVENT_SELECT) |
+	    __SHIFTIN(intel_param.p_unit, PERFEVTSEL_UNIT_MASK) |
+	    ((intel_param.p_flags & TPROF_PARAM_USER) ? PERFEVTSEL_USR : 0) |
+	    ((intel_param.p_flags & TPROF_PARAM_KERN) ? PERFEVTSEL_OS : 0) |
+	    PERFEVTSEL_INT |
+	    PERFEVTSEL_EN;
 
-	wrmsr(msr->msr_counter, counter_reset_val);
-	wrmsr(msr->msr_escr, escr);
-	wrmsr(msr->msr_cccr, cccr);
-	tprof_pmi_lapic_saved[cpu_index(ci)] = lapic_readreg(LAPIC_PCINT);
+	wrmsr(MSR_PERFCTR0, counter_reset_val);
+	wrmsr(MSR_EVNTSEL0, evtval);
+
+	intel_lapic_saved[cpu_index(ci)] = lapic_readreg(LAPIC_PCINT);
 	lapic_writereg(LAPIC_PCINT, LAPIC_DLMODE_NMI);
 }
 
 static void
-tprof_pmi_stop_cpu(void *arg1, void *arg2)
+tprof_intel_stop_cpu(void *arg1, void *arg2)
 {
 	struct cpu_info * const ci = curcpu();
-	const struct msrs *msr;
 
-	if (ci->ci_smt_id >= 2) {
-		printf("%s: ignoring %s smt id=%u",
-		    __func__, device_xname(ci->ci_dev),
-		    (u_int)ci->ci_smt_id);
-		return;
-	}
-	msr = &msrs[ci->ci_smt_id];
+	wrmsr(MSR_EVNTSEL0, 0);
+	wrmsr(MSR_PERFCTR0, 0);
 
-	wrmsr(msr->msr_escr, 0);
-	wrmsr(msr->msr_cccr, 0);
-	lapic_writereg(LAPIC_PCINT, tprof_pmi_lapic_saved[cpu_index(ci)]);
+	lapic_writereg(LAPIC_PCINT, intel_lapic_saved[cpu_index(ci)]);
 }
 
 static int
-tprof_pmi_nmi(const struct trapframe *tf, void *dummy)
+tprof_intel_nmi(const struct trapframe *tf, void *dummy)
 {
-	struct cpu_info * const ci = curcpu();
-	const struct msrs *msr;
 	uint32_t pcint;
-	uint64_t cccr;
+	uint64_t ctr;
 	tprof_frame_info_t tfi;
 
 	KASSERT(dummy == NULL);
 
-	if (ci->ci_smt_id >= 2) {
-		/* not ours */
-		return 0;
-	}
-	msr = &msrs[ci->ci_smt_id];
-
-	/* check if it's for us */
-	cccr = rdmsr(msr->msr_cccr);
-	if ((cccr & CCCR_OVF) == 0) {
-		/* not ours */
+	ctr = rdmsr(MSR_PERFCTR0);
+	/* If the highest bit is non zero, then it's not for us. */
+	if ((ctr & __BIT(counter_bitwidth-1)) != 0) {
 		return 0;
 	}
 
 	/* record a sample */
 #if defined(__x86_64__)
 	tfi.tfi_pc = tf->tf_rip;
-#else /* defined(__x86_64__) */
+#else
 	tfi.tfi_pc = tf->tf_eip;
-#endif /* defined(__x86_64__) */
+#endif
 	tfi.tfi_inkernel = tfi.tfi_pc >= VM_MIN_KERNEL_ADDRESS;
-	tprof_sample(tprof_cookie, &tfi);
+	tprof_sample(NULL, &tfi);
 
 	/* reset counter */
-	wrmsr(msr->msr_counter, counter_reset_val);
-	wrmsr(msr->msr_cccr, cccr & ~CCCR_OVF);
+	wrmsr(MSR_PERFCTR0, counter_reset_val);
 
 	/* unmask PMI */
 	pcint = lapic_readreg(LAPIC_PCINT);
@@ -207,7 +171,7 @@ tprof_pmi_nmi(const struct trapframe *tf, void *dummy)
 }
 
 static uint64_t
-tprof_pmi_estimate_freq(void)
+tprof_intel_estimate_freq(void)
 {
 	uint64_t cpufreq = curcpu()->ci_data.cpu_cc_freq;
 	uint64_t freq = 10000;
@@ -215,54 +179,74 @@ tprof_pmi_estimate_freq(void)
 	counter_val = cpufreq / freq;
 	if (counter_val == 0) {
 		counter_val = UINT64_C(4000000000) / freq;
-		return freq;
 	}
 	return freq;
 }
 
-static int
-tprof_pmi_start(tprof_backend_cookie_t *cookie)
+static uint32_t
+tprof_intel_ident(void)
 {
-	struct cpu_info * const ci = curcpu();
+	uint32_t descs[4];
+
+	if (cpu_vendor != CPUVENDOR_INTEL) {
+		return TPROF_IDENT_NONE;
+	}
+
+	if (cpuid_level < 0x0A) {
+		return TPROF_IDENT_NONE;
+	}
+	x86_cpuid(0x0A, descs);
+	if ((descs[0] & CPUID_0A_VERSION) == 0) {
+		return TPROF_IDENT_NONE;
+	}
+	if ((descs[0] & CPUID_0A_NCOUNTERS) == 0) {
+		return TPROF_IDENT_NONE;
+	}
+
+	counter_bitwidth = __SHIFTOUT(descs[0], CPUID_0A_BITWIDTH);
+
+	return TPROF_IDENT_INTEL_GENERIC;
+}
+
+static int
+tprof_intel_start(const tprof_param_t *param)
+{
 	uint64_t xc;
 
-	if (!(cpu_vendor == CPUVENDOR_INTEL &&
-	    CPUID_TO_BASEFAMILY(ci->ci_signature) == 15)) {
+	if (tprof_intel_ident() == TPROF_IDENT_NONE) {
 		return ENOTSUP;
 	}
 
-	KASSERT(tprof_pmi_nmi_handle == NULL);
-	tprof_pmi_nmi_handle = nmi_establish(tprof_pmi_nmi, NULL);
+	KASSERT(intel_nmi_handle == NULL);
+	intel_nmi_handle = nmi_establish(tprof_intel_nmi, NULL);
 
 	counter_reset_val = - counter_val + 1;
-	xc = xc_broadcast(0, tprof_pmi_start_cpu, NULL, NULL);
-	xc_wait(xc);
+	memcpy(&intel_param, param, sizeof(*param));
 
-	KASSERT(tprof_cookie == NULL);
-	tprof_cookie = cookie;
+	xc = xc_broadcast(0, tprof_intel_start_cpu, NULL, NULL);
+	xc_wait(xc);
 
 	return 0;
 }
 
 static void
-tprof_pmi_stop(tprof_backend_cookie_t *cookie)
+tprof_intel_stop(const tprof_param_t *param)
 {
 	uint64_t xc;
 
-	xc = xc_broadcast(0, tprof_pmi_stop_cpu, NULL, NULL);
+	xc = xc_broadcast(0, tprof_intel_stop_cpu, NULL, NULL);
 	xc_wait(xc);
 
-	KASSERT(tprof_pmi_nmi_handle != NULL);
-	KASSERT(tprof_cookie == cookie);
-	nmi_disestablish(tprof_pmi_nmi_handle);
-	tprof_pmi_nmi_handle = NULL;
-	tprof_cookie = NULL;
+	KASSERT(intel_nmi_handle != NULL);
+	nmi_disestablish(intel_nmi_handle);
+	intel_nmi_handle = NULL;
 }
 
-static const tprof_backend_ops_t tprof_pmi_ops = {
-	.tbo_estimate_freq = tprof_pmi_estimate_freq,
-	.tbo_start = tprof_pmi_start,
-	.tbo_stop = tprof_pmi_stop,
+static const tprof_backend_ops_t tprof_intel_ops = {
+	.tbo_estimate_freq = tprof_intel_estimate_freq,
+	.tbo_ident = tprof_intel_ident,
+	.tbo_start = tprof_intel_start,
+	.tbo_stop = tprof_intel_stop,
 };
 
 MODULE(MODULE_CLASS_DRIVER, tprof_pmi, "tprof");
@@ -273,7 +257,7 @@ tprof_pmi_modcmd(modcmd_t cmd, void *arg)
 
 	switch (cmd) {
 	case MODULE_CMD_INIT:
-		return tprof_backend_register("tprof_pmi", &tprof_pmi_ops,
+		return tprof_backend_register("tprof_pmi", &tprof_intel_ops,
 		    TPROF_BACKEND_VERSION);
 
 	case MODULE_CMD_FINI:
