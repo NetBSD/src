99c1c0e99a6b665bdfddc12975a19166ccab5979
martin <martin@NetBSD.org>
2018-07-13 14:32:01 +0000

    Pull up following revision(s) (requested by kre in ticket #907):
    	bin/sh/expand.c: revision 1.122
    
    When matching a char class ([[:name:]]) in a pattern (for filename
    expansion, case patterrns, etc) do not force '[' to be a member of
    every class.
    
    Before this fix, try:
    
    	case [ in [[:alpha:]]) echo Huh\?;; esac
    
    XXX pullup-8    (Perhaps -7 as well, though that shell version has
    much more relevant bugs than this one.)  This bug is not in -6 as
    that has no charclass support.

--- bin/sh/expand.c
+++ bin/sh/expand.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: expand.c,v 1.110.2.3 2017/10/25 06:51:36 snj Exp $	*/
+/*	$NetBSD: expand.c,v 1.110.2.4 2018/07/13 14:32:01 martin Exp $	*/
 
 /*-
  * Copyright (c) 1991, 1993
@@ -37,7 +37,7 @@
 #if 0
 static char sccsid[] = "@(#)expand.c	8.5 (Berkeley) 5/15/95";
 #else
-__RCSID("$NetBSD: expand.c,v 1.110.2.3 2017/10/25 06:51:36 snj Exp $");
+__RCSID("$NetBSD: expand.c,v 1.110.2.4 2018/07/13 14:32:01 martin Exp $");
 #endif
 #endif /* not lint */
 
@@ -1774,8 +1774,10 @@ patmatch(const char *pattern, const char *string, int squoted)
 				}
 				if (c == '[' && *p == ':') {
 					found |= match_charclass(p, chr, &end);
-					if (end != NULL)
+					if (end != NULL) {
 						p = end;
+						continue;
+					}
 				}
 				if (c == CTLESC)
 					c = *p++;
