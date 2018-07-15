e16cbbf5fb95efc2f0ef4e5e46ce9d1351ff8ee3
martin <martin@NetBSD.org>
2018-07-15 10:59:49 +0000

    Pull up following revision(s) (requested by sevan in ticket #922):
    	crypto/external/bsd/openssh/dist/ssh-agent.1: revision 1.13
    	crypto/external/bsd/openssh/dist/ssh-agent.c: revision 1.23
    
    Amend whitelisted filesystem paths ssh-agent will look for PKCS11 related
    libraries so that things work out of the box with pkgsrc without having to
    explicitly whitelist things.
    
    ok christos

--- crypto/external/bsd/openssh/dist/ssh-agent.c
+++ crypto/external/bsd/openssh/dist/ssh-agent.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: ssh-agent.c,v 1.20.4.1 2017/12/04 10:55:18 snj Exp $	*/
+/*	$NetBSD: ssh-agent.c,v 1.20.4.2 2018/07/15 10:59:49 martin Exp $	*/
 /* $OpenBSD: ssh-agent.c,v 1.224 2017/07/24 04:34:28 djm Exp $ */
 /*
  * Author: Tatu Ylonen <ylo@cs.hut.fi>
@@ -36,7 +36,7 @@
  */
 
 #include "includes.h"
-__RCSID("$NetBSD: ssh-agent.c,v 1.20.4.1 2017/12/04 10:55:18 snj Exp $");
+__RCSID("$NetBSD: ssh-agent.c,v 1.20.4.2 2018/07/15 10:59:49 martin Exp $");
 
 #include <sys/param.h>	/* MIN MAX */
 #include <sys/types.h>
@@ -82,7 +82,7 @@ __RCSID("$NetBSD: ssh-agent.c,v 1.20.4.1 2017/12/04 10:55:18 snj Exp $");
 #endif
 
 #ifndef DEFAULT_PKCS11_WHITELIST
-# define DEFAULT_PKCS11_WHITELIST "/usr/lib*/*,/usr/local/lib*/*"
+# define DEFAULT_PKCS11_WHITELIST "/usr/lib*/*,/usr/pkg/lib*/*"
 #endif
 
 /* Maximum accepted message length */
