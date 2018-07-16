377533e70806012b1053f80b9b52b6a534d271a7
kamil <kamil@NetBSD.org>
2018-07-16 07:25:58 +0000

    Disable profile and compat 32-bit tests cc sanitizer tests
    These tests are known to be faulty. These fatures aren't ready.
    They tend to break to unrelated changes to the codebase and require.
    
    Base Clang and GCC are in general dated to use with sanitizers as these
    features are being fixed on HEAD versions of both compilers (mostly Clang).

--- tests/usr.bin/cc/t_asan_off_by_one.sh
+++ tests/usr.bin/cc/t_asan_off_by_one.sh
@@ -1,4 +1,4 @@
-#	$NetBSD: t_asan_off_by_one.sh,v 1.1 2018/04/04 23:51:35 kamil Exp $
+#	$NetBSD: t_asan_off_by_one.sh,v 1.2 2018/07/16 07:25:58 kamil Exp $
 #
 # Copyright (c) 2018 The NetBSD Foundation, Inc.
 # All rights reserved.
@@ -223,10 +223,10 @@ atf_init_test_cases()
 	}
 
 	atf_add_test_case off_by_one
-	atf_add_test_case off_by_one_profile
+#	atf_add_test_case off_by_one_profile
 	atf_add_test_case off_by_one_pic
 	atf_add_test_case off_by_one_pie
-	atf_add_test_case off_by_one32
+#	atf_add_test_case off_by_one32
 	# static option not supported
 	# -static and -fsanitize=address can't be used together for compilation
 	# (gcc version  5.4.0 and clang 7.1) tested on April 2nd 2018.
