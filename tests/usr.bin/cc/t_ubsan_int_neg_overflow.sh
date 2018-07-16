0f5ae14248710c485edf260230ab7660229e0870
kamil <kamil@NetBSD.org>
2018-07-16 07:25:58 +0000

    Disable profile and compat 32-bit tests cc sanitizer tests
    These tests are known to be faulty. These fatures aren't ready.
    They tend to break to unrelated changes to the codebase and require.
    
    Base Clang and GCC are in general dated to use with sanitizers as these
    features are being fixed on HEAD versions of both compilers (mostly Clang).

--- tests/usr.bin/cc/t_ubsan_int_neg_overflow.sh
+++ tests/usr.bin/cc/t_ubsan_int_neg_overflow.sh
@@ -190,8 +190,8 @@ atf_init_test_cases()
 		return 0
 	}
 	atf_add_test_case int_neg_overflow
-	atf_add_test_case int_neg_overflow_profile
+#	atf_add_test_case int_neg_overflow_profile
 	atf_add_test_case int_neg_overflow_pie
 	atf_add_test_case int_neg_overflow_pic
-	atf_add_test_case int_neg_overflow32
+#	atf_add_test_case int_neg_overflow32
 }
