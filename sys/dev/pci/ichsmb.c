73aad210dfeba36887b1b7e0544e697471c6973d
snj <snj@NetBSD.org>
2018-07-26 20:32:38 +0000

    Pull up following revision(s) (requested by msaitoh in ticket #926): 	sys/dev/pci/ichsmb.c: 1.52, 1.56 Add C620 devices. -- Add Apollo Lake and Gemini Lake devices.

--- sys/dev/pci/ichsmb.c
+++ sys/dev/pci/ichsmb.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: ichsmb.c,v 1.50.6.2 2018/04/16 14:21:48 martin Exp $	*/
+/*	$NetBSD: ichsmb.c,v 1.50.6.3 2018/07/26 20:32:38 snj Exp $	*/
 /*	$OpenBSD: ichiic.c,v 1.18 2007/05/03 09:36:26 dlg Exp $	*/
 
 /*
@@ -22,7 +22,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: ichsmb.c,v 1.50.6.2 2018/04/16 14:21:48 martin Exp $");
+__KERNEL_RCSID(0, "$NetBSD: ichsmb.c,v 1.50.6.3 2018/07/26 20:32:38 snj Exp $");
 
 #include <sys/param.h>
 #include <sys/device.h>
@@ -122,11 +122,15 @@ ichsmb_match(device_t parent, cfdata_t match, void *aux)
 		case PCI_PRODUCT_INTEL_CORE5G_M_SMB:
 		case PCI_PRODUCT_INTEL_BAYTRAIL_PCU_SMB:
 		case PCI_PRODUCT_INTEL_BSW_PCU_SMB:
+		case PCI_PRODUCT_INTEL_APL_SMB:
+		case PCI_PRODUCT_INTEL_GLK_SMB:
 		case PCI_PRODUCT_INTEL_C600_SMBUS:
 		case PCI_PRODUCT_INTEL_C600_SMB_0:
 		case PCI_PRODUCT_INTEL_C600_SMB_1:
 		case PCI_PRODUCT_INTEL_C600_SMB_2:
 		case PCI_PRODUCT_INTEL_C610_SMB:
+		case PCI_PRODUCT_INTEL_C620_SMB:
+		case PCI_PRODUCT_INTEL_C620_SMB_S:
 		case PCI_PRODUCT_INTEL_EP80579_SMB:
 		case PCI_PRODUCT_INTEL_DH89XXCC_SMB:
 		case PCI_PRODUCT_INTEL_DH89XXCL_SMB:
