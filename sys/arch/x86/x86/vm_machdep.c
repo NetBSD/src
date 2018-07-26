da6bf54aeed8200ee8893e4cfc2a64320b510a39
maxv <maxv@NetBSD.org>
2018-07-26 09:29:08 +0000

    Rework dbregs, to switch the registers during context switches, and not on each user->kernel transition via userret. Reloads of DR6/DR7 are expensive on both native and xen.

--- sys/arch/x86/x86/vm_machdep.c
+++ sys/arch/x86/x86/vm_machdep.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: vm_machdep.c,v 1.35 2018/07/01 08:32:41 maxv Exp $	*/
+/*	$NetBSD: vm_machdep.c,v 1.36 2018/07/26 09:29:08 maxv Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986 The Regents of the University of California.
@@ -80,7 +80,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: vm_machdep.c,v 1.35 2018/07/01 08:32:41 maxv Exp $");
+__KERNEL_RCSID(0, "$NetBSD: vm_machdep.c,v 1.36 2018/07/26 09:29:08 maxv Exp $");
 
 #include "opt_mtrr.h"
 
@@ -166,6 +166,7 @@ cpu_lwp_fork(struct lwp *l1, struct lwp *l2, void *stack, size_t stacksize,
 
 	/* Never inherit CPU Debug Registers */
 	pcb2->pcb_dbregs = NULL;
+	pcb2->pcb_flags &= ~PCB_DBREGS;
 
 #if defined(XEN)
 	pcb2->pcb_iopl = IOPL_KPL;
@@ -262,6 +263,9 @@ cpu_lwp_free(struct lwp *l, int proc)
 	/* If we were using the FPU, forget about it. */
 	fpusave_lwp(l, false);
 
+	/* Abandon the dbregs state. */
+	x86_dbregs_abandon(l);
+
 #ifdef MTRR
 	if (proc && l->l_proc->p_md.md_flags & MDP_USEDMTRR)
 		mtrr_clean(l->l_proc);
@@ -287,7 +291,7 @@ cpu_lwp_free2(struct lwp *l)
 	KASSERT(l->l_md.md_gc_pmap == NULL);
 
 	pcb = lwp_getpcb(l);
-
+	KASSERT((pcb->pcb_flags & PCB_DBREGS) == 0);
 	if (pcb->pcb_dbregs) {
 		pool_put(&x86_dbregspl, pcb->pcb_dbregs);
 		pcb->pcb_dbregs = NULL;
