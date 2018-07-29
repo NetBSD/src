39ca8147a16cd8a3bdbc8b17bba3f19fd1c77177
riastradh <riastradh@NetBSD.org>
2018-07-29 02:01:43 +0000

    Use callout_halt and usb_rem_task_wait in udav(4).

--- sys/dev/usb/if_udav.c
+++ sys/dev/usb/if_udav.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: if_udav.c,v 1.53 2018/06/26 06:48:02 msaitoh Exp $	*/
+/*	$NetBSD: if_udav.c,v 1.54 2018/07/29 02:01:43 riastradh Exp $	*/
 /*	$nabe: if_udav.c,v 1.3 2003/08/21 16:57:19 nabe Exp $	*/
 
 /*
@@ -45,7 +45,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: if_udav.c,v 1.53 2018/06/26 06:48:02 msaitoh Exp $");
+__KERNEL_RCSID(0, "$NetBSD: if_udav.c,v 1.54 2018/07/29 02:01:43 riastradh Exp $");
 
 #ifdef _KERNEL_OPT
 #include "opt_inet.h"
@@ -352,11 +352,11 @@ udav_detach(device_t self, int flags)
 	if (!sc->sc_attached)
 		return 0;
 
-	callout_stop(&sc->sc_stat_ch);
+	callout_halt(&sc->sc_stat_ch, NULL);
 
 	/* Remove any pending tasks */
-	usb_rem_task(sc->sc_udev, &sc->sc_tick_task);
-	usb_rem_task(sc->sc_udev, &sc->sc_stop_task);
+	usb_rem_task_wait(sc->sc_udev, &sc->sc_tick_task, USB_TASKQ_DRIVER);
+	usb_rem_task_wait(sc->sc_udev, &sc->sc_stop_task, USB_TASKQ_DRIVER);
 
 	s = splusb();
 
