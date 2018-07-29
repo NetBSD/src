7d2886b531fd20923c4e2bc29db62a9b2488525b
riastradh <riastradh@NetBSD.org>
2018-07-29 01:59:46 +0000

    New function usb_rem_task_wait(dev, task, queue).
    If task is scheduled to run, removes it from the queue.  If it may
    have already begun to run, waits for it to complete.  Caller must
    guarantee it will not switch to another queue.  If caller guarantees
    it will not be scheduled again, then usb_rem_task_wait guarantees it
    is not running on return.
    
    This will enable us to fix a litany of bugs in detach where we
    currently fail to wait for a pending task.

--- sys/dev/usb/usbdi.h
+++ sys/dev/usb/usbdi.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: usbdi.h,v 1.92 2016/08/14 14:42:22 skrll Exp $	*/
+/*	$NetBSD: usbdi.h,v 1.93 2018/07/29 01:59:46 riastradh Exp $	*/
 /*	$FreeBSD: src/sys/dev/usb/usbdi.h,v 1.18 1999/11/17 22:33:49 n_hibma Exp $	*/
 
 /*
@@ -219,6 +219,7 @@ struct usb_task {
 
 void usb_add_task(struct usbd_device *, struct usb_task *, int);
 void usb_rem_task(struct usbd_device *, struct usb_task *);
+void usb_rem_task_wait(struct usbd_device *, struct usb_task *, int);
 #define usb_init_task(t, f, a, fl) ((t)->fun = (f), (t)->arg = (a), (t)->queue = USB_NUM_TASKQS, (t)->flags = (fl))
 
 struct usb_devno {
