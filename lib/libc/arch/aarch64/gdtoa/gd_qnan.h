935233ccbf5a49c72c0aad369486610dfd68b4e3
ryo <ryo@NetBSD.org>
2018-08-01 19:59:49 +0000

    fix long double NaN definition.

--- lib/libc/arch/aarch64/gdtoa/gd_qnan.h
+++ lib/libc/arch/aarch64/gdtoa/gd_qnan.h
@@ -1,10 +1,10 @@
-/* $NetBSD: gd_qnan.h,v 1.1 2014/08/10 05:47:36 matt Exp $ */
+/* $NetBSD: gd_qnan.h,v 1.2 2018/08/01 19:59:49 ryo Exp $ */
 
 #define f_QNAN 0x7fc00000
 #ifdef __AARCH64EB__
 #define d_QNAN0 0x7ff80000
 #define d_QNAN1 0x0
-#define ld_QNAN0 0x7ff80000
+#define ld_QNAN0 0x7fff8000
 #define ld_QNAN1 0x0
 #define ld_QNAN2 0x0
 #define ld_QNAN3 0x0
@@ -14,5 +14,5 @@
 #define ld_QNAN0 0x0
 #define ld_QNAN1 0x0
 #define ld_QNAN2 0x0
-#define ld_QNAN3 0x7ff80000
+#define ld_QNAN3 0x7fff8000
 #endif
