68be2330abe0db96679dd40f4813141e3644a412
ryo <ryo@NetBSD.org>
2018-08-04 10:22:09 +0000

    need to set _UC_TLSBASE of uc_flags for _lwp_makecontext(). pointed out from skrll@. thanks.

--- lib/libc/arch/aarch64/gen/_lwp.c
+++ lib/libc/arch/aarch64/gen/_lwp.c
@@ -1,4 +1,4 @@
-/* $NetBSD: _lwp.c,v 1.1 2014/08/10 05:47:36 matt Exp $ */
+/* $NetBSD: _lwp.c,v 1.2 2018/08/04 10:22:09 ryo Exp $ */
 
 /*-
  * Copyright (c) 2014 The NetBSD Foundation, Inc.
@@ -31,7 +31,7 @@
 
 #include <sys/cdefs.h>
 #if defined(LIBC_SCCS) && !defined(lint)
-__RCSID("$NetBSD: _lwp.c,v 1.1 2014/08/10 05:47:36 matt Exp $");
+__RCSID("$NetBSD: _lwp.c,v 1.2 2018/08/04 10:22:09 ryo Exp $");
 #endif /* LIBC_SCCS and not lint */
 
 #include "namespace.h"
@@ -61,4 +61,5 @@ _lwp_makecontext(ucontext_t *u, void (*start)(void *),
 	u->uc_mcontext.__gregs[_REG_X29] = (__greg_t)(uintptr_t)_lwp_exit;
 	u->uc_mcontext.__gregs[_REG_PC] = (__greg_t)(uintptr_t)start;
 	u->uc_mcontext.__gregs[_REG_TPIDR] = (__greg_t)(uintptr_t)private;
+	u->uc_flags |= _UC_TLSBASE;
 }
