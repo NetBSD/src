34e71cb16b402bd25958ff6bedb51d30d715e64b
martin <martin@NetBSD.org>
2018-08-07 16:04:28 +0000

    Pull up following revision(s) (requested by nonaka in ticket #1625):
    	external/gpl2/gmake/dist/glob/glob.c: revision 1.5
    	(applied to gnu/dist/gmake)
    
    gmake: __alloca and __stat are provided only when building with GLIBC.
    Fix a build failure on Ubuntu 18.04.

--- gnu/dist/gmake/glob/glob.c
+++ gnu/dist/gmake/glob/glob.c
@@ -206,7 +206,7 @@ my_realloc (p, n)
 #endif /* __GNU_LIBRARY__ */
 
 
-#if !defined __alloca && !defined __GNU_LIBRARY__
+#if !defined __alloca
 
 # ifdef	__GNUC__
 #  undef alloca
@@ -229,7 +229,7 @@ extern char *alloca ();
 
 #endif
 
-#ifndef __GNU_LIBRARY__
+#if !defined __stat
 # define __stat stat
 # ifdef STAT_MACROS_BROKEN
 #  undef S_ISDIR
