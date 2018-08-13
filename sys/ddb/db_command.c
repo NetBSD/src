67e7c2ac7cbe54f89960cfbc70b8e9c451460f60
mrg <mrg@NetBSD.org>
2018-08-13 03:20:19 +0000

    extend the ddb "show kernhist" command.  the synopsis is now:
         show kernhist[/i] [addr[,count]]
    
    the i modifier means display info instead of entries, and the count
    restricts to the last count entries.
    
    the count option is only supported for specified histories.  it
    could be extended but requires more logic than the single history,
    as multiple histories are merged during display.

--- sys/ddb/db_command.c
+++ sys/ddb/db_command.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: db_command.c,v 1.155 2018/08/12 22:05:29 mrg Exp $	*/
+/*	$NetBSD: db_command.c,v 1.156 2018/08/13 03:20:19 mrg Exp $	*/
 
 /*
  * Copyright (c) 1996, 1997, 1998, 1999, 2002, 2009 The NetBSD Foundation, Inc.
@@ -60,7 +60,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: db_command.c,v 1.155 2018/08/12 22:05:29 mrg Exp $");
+__KERNEL_RCSID(0, "$NetBSD: db_command.c,v 1.156 2018/08/13 03:20:19 mrg Exp $");
 
 #ifdef _KERNEL_OPT
 #include "opt_aio.h"
@@ -1232,7 +1232,7 @@ db_kernhist_print_cmd(db_expr_t addr, bool have_addr,
     db_expr_t count, const char *modif)
 {
 
-	kernhist_print((void *)(uintptr_t)addr, db_printf);
+	kernhist_print((void *)(uintptr_t)addr, count, modif, db_printf);
 }
 #endif
 
