abd1084a42fe07e78eec0d7e63a396b0dffffb33
skrll <skrll@NetBSD.org>
2018-08-15 06:04:01 +0000

    Fix includes (sys/cdefs.h then sys/param.h)

--- sys/arch/arm/arm32/cpu.c
+++ sys/arch/arm/arm32/cpu.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: cpu.c,v 1.118 2018/08/15 06:00:02 skrll Exp $	*/
+/*	$NetBSD: cpu.c,v 1.119 2018/08/15 06:04:01 skrll Exp $	*/
 
 /*
  * Copyright (c) 1995 Mark Brinicombe.
@@ -45,10 +45,10 @@
 #include "opt_cputypes.h"
 #include "opt_multiprocessor.h"
 
+#include <sys/cdefs.h>
+__KERNEL_RCSID(0, "$NetBSD: cpu.c,v 1.119 2018/08/15 06:04:01 skrll Exp $");
+
 #include <sys/param.h>
-
-__KERNEL_RCSID(0, "$NetBSD: cpu.c,v 1.118 2018/08/15 06:00:02 skrll Exp $");
-
 #include <sys/systm.h>
 #include <sys/conf.h>
 #include <sys/cpu.h>
