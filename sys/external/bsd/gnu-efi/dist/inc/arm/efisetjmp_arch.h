1db94776f6d82b38c625047a391fd3aebaf97805
jmcneill <jmcneill@NetBSD.org>
2018-08-16 18:24:35 +0000

    Rename ALIGN macro to EFI_ALIGN

--- sys/external/bsd/gnu-efi/dist/inc/arm/efisetjmp_arch.h
+++ sys/external/bsd/gnu-efi/dist/inc/arm/efisetjmp_arch.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: efisetjmp_arch.h,v 1.1.1.1 2018/08/16 18:17:47 jmcneill Exp $	*/
+/*	$NetBSD: efisetjmp_arch.h,v 1.2 2018/08/16 18:24:36 jmcneill Exp $	*/
 
 #ifndef GNU_EFI_ARM_SETJMP_H
 #define GNU_EFI_ARM_SETJMP_H
@@ -18,6 +18,6 @@ typedef struct {
 	UINT32 R12;
 	UINT32 R13;
 	UINT32 R14;
-} ALIGN(JMPBUF_ALIGN) jmp_buf;
+} EFI_ALIGN(JMPBUF_ALIGN) jmp_buf;
 
 #endif /* GNU_EFI_ARM_SETJMP_H */
