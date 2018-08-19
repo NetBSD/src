f6b1ae0697d4005372834ef5547132433c41d65a
skrll <skrll@NetBSD.org>
2018-08-19 07:27:33 +0000

    Some changes to bring back exynos4 support and make exynos5 support more modern/generic
    - Provide a exynos_platform_early_putchar based on CONSADDR and remove
      EXYNOS_CONSOLE_EARLY
    - Provide devmaps for exynos4 and exynos5
    - Simplify exynos_bootstrap
    - Update exynos_start.S to use exynos_platform_early_putchar - this
      needs a fix in a9_subr.S for arm_cpuinit to save lr in a callee save
      register and not use ip.  Maybe this change is safe now, but too much
      testing required to commit it now.

--- sys/arch/arm/samsung/exynos_var.h
+++ sys/arch/arm/samsung/exynos_var.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: exynos_var.h,v 1.24 2018/07/02 23:54:52 jmcneill Exp $	*/
+/*	$NetBSD: exynos_var.h,v 1.25 2018/08/19 07:27:33 skrll Exp $	*/
 
 /*-
  * Copyright (c) 2013, 2014 The NetBSD Foundation, Inc.
@@ -143,7 +143,7 @@ extern bus_space_handle_t exynos_pmu_bsh;
 extern bus_space_handle_t exynos_cmu_bsh;
 extern bus_space_handle_t exynos_sysreg_bsh;
 
-extern void exynos_bootstrap(vaddr_t, vaddr_t);
+extern void exynos_bootstrap(vaddr_t);
 extern void exynos_dma_bootstrap(psize_t memsize);
 
 struct exynos_pinctrl_softc;
