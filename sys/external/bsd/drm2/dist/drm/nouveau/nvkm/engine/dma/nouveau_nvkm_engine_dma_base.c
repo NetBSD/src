c81806656589415cc455aeb0b55a15424572402b
riastradh <riastradh@NetBSD.org>
2018-08-27 14:18:30 +0000

    Mark it on the tree so we remove it during destruction.

--- sys/external/bsd/drm2/dist/drm/nouveau/nvkm/engine/dma/nouveau_nvkm_engine_dma_base.c
+++ sys/external/bsd/drm2/dist/drm/nouveau/nvkm/engine/dma/nouveau_nvkm_engine_dma_base.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: nouveau_nvkm_engine_dma_base.c,v 1.3 2018/08/27 07:36:07 riastradh Exp $	*/
+/*	$NetBSD: nouveau_nvkm_engine_dma_base.c,v 1.4 2018/08/27 14:18:30 riastradh Exp $	*/
 
 /*
  * Copyright 2012 Red Hat Inc.
@@ -24,7 +24,7 @@
  * Authors: Ben Skeggs
  */
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: nouveau_nvkm_engine_dma_base.c,v 1.3 2018/08/27 07:36:07 riastradh Exp $");
+__KERNEL_RCSID(0, "$NetBSD: nouveau_nvkm_engine_dma_base.c,v 1.4 2018/08/27 14:18:30 riastradh Exp $");
 
 #include "priv.h"
 
@@ -118,6 +118,7 @@ nvkm_dma_oclass_new(struct nvkm_device *device,
 	if (collision != dmaobj)
 		/* XXX Don't we have to free this?  */
 		return -EEXIST;
+	dmaobj->on_tree = true;
 #else
 	while (*ptr) {
 		struct nvkm_dmaobj *obj = container_of(*ptr, typeof(*obj), rb);
