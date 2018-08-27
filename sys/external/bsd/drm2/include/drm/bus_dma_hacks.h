9ddb97dcfccfa6e3b7b564896380b39822b8b853
riastradh <riastradh@NetBSD.org>
2018-08-27 15:26:50 +0000

    Rework drm prime guts.
    - Our faux struct sg_table is now just an array of _pages_, not of
      bus dma segments.
    
    - We handle bus dma segments only in the presence of an adult bus dma
      tag.
    
    - All bus dma hacks are once again confined to bus_dma_hacks.h, and
      more neatly organized:
      . bus_dmamem_export_pages translates bus dma segs to vm_page array
      . bus_dmamem_import_pages translates vm_page array to bus dma segs

--- sys/external/bsd/drm2/include/drm/bus_dma_hacks.h
+++ sys/external/bsd/drm2/include/drm/bus_dma_hacks.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: bus_dma_hacks.h,v 1.11 2018/08/27 15:11:58 riastradh Exp $	*/
+/*	$NetBSD: bus_dma_hacks.h,v 1.12 2018/08/27 15:26:50 riastradh Exp $	*/
 
 /*-
  * Copyright (c) 2013 The NetBSD Foundation, Inc.
@@ -43,9 +43,11 @@
 #if defined(__i386__) || defined(__x86_64__)
 #  include <x86/bus_private.h>
 #  include <x86/machdep.h>
-#  define	PHYS_TO_BUS_MEM(dmat, paddr)	(paddr)
+#  define	PHYS_TO_BUS_MEM(dmat, paddr)	((bus_addr_t)(paddr))
+#  define	BUS_MEM_TO_PHYS(dmat, baddr)	((paddr_t)(baddr))
 #elif defined(__arm__) || defined(__aarch64__)
-#  define	PHYS_TO_BUS_MEM(dmat, paddr)	(paddr)
+#  define	PHYS_TO_BUS_MEM(dmat, paddr)	((bus_addr_t)(paddr))
+#  define	BUS_MEM_TO_PHYS(dmat, baddr)	((paddr_t)(baddr))
 #elif defined(__powerpc__)
 #else
 #  error DRM GEM/TTM need new MI bus_dma APIs!  Halp!
@@ -111,6 +113,104 @@ fail1: __unused
 fail0:	KASSERT(error);
 out:	kmem_free(segs, (nsegs * sizeof(segs[0])));
 	return error;
+}
+
+static inline int
+bus_dmamap_load_pgarray(bus_dma_tag_t tag, bus_dmamap_t map,
+    struct vm_page **pgs, unsigned npgs, bus_size_t size, int flags)
+{
+	km_flag_t kmflags;
+	bus_dma_segment_t *segs;
+	unsigned i;
+	int error;
+
+	KASSERT((int)npgs <= (SIZE_MAX / sizeof(segs[0])));
+	switch (flags & (BUS_DMA_WAITOK|BUS_DMA_NOWAIT)) {
+	case BUS_DMA_WAITOK:	kmflags = KM_SLEEP;	break;
+	case BUS_DMA_NOWAIT:	kmflags = KM_NOSLEEP;	break;
+	default:		panic("invalid flags: %d", flags);
+	}
+	segs = kmem_alloc((npgs * sizeof(segs[0])), kmflags);
+	if (segs == NULL)
+		return ENOMEM;
+
+	for (i = 0; i < npgs; i++) {
+		paddr_t paddr = VM_PAGE_TO_PHYS(pgs[i]);
+		bus_addr_t baddr = PHYS_TO_BUS_MEM(tag, paddr);
+
+		segs[i].ds_addr = baddr;
+		segs[i].ds_len = PAGE_SIZE;
+	}
+
+	error = bus_dmamap_load_raw(tag, map, segs, npgs, size, flags);
+	if (error)
+		goto fail0;
+
+	/* Success!  */
+	error = 0;
+	goto out;
+
+fail1: __unused
+	bus_dmamap_unload(tag, map);
+fail0:	KASSERT(error);
+out:	kmem_free(segs, (npgs * sizeof(segs[0])));
+	return error;
+}
+
+static inline int
+bus_dmamem_export_pages(bus_dma_tag_t dmat, const bus_dma_segment_t *segs,
+    int nsegs, struct vm_page **pgs, unsigned npgs)
+{
+	int seg;
+	unsigned i;
+
+	i = 0;
+	for (seg = 0; seg < nsegs; seg++) {
+		bus_addr_t baddr = segs[i].ds_addr;
+		bus_size_t len = segs[i].ds_len;
+
+		while (len >= PAGE_SIZE) {
+			paddr_t paddr = BUS_MEM_TO_PHYS(dmat, baddr);
+
+			KASSERT(i < npgs);
+			pgs[i++] = PHYS_TO_VM_PAGE(paddr);
+
+			baddr += PAGE_SIZE;
+			len -= PAGE_SIZE;
+		}
+		KASSERT(len == 0);
+	}
+	KASSERT(i == npgs);
+
+	return 0;
+}
+
+static inline int
+bus_dmamem_import_pages(bus_dma_tag_t dmat, bus_dma_segment_t *segs,
+    int nsegs, int *rsegs, struct vm_page *const *pgs, unsigned npgs)
+{
+	int seg;
+	unsigned i;
+
+	seg = 0;
+	for (i = 0; i < npgs; i++) {
+		paddr_t paddr = VM_PAGE_TO_PHYS(pgs[i]);
+		bus_addr_t baddr = PHYS_TO_BUS_MEM(dmat, paddr);
+
+		if (seg > 0 && segs[seg - 1].ds_addr + PAGE_SIZE == baddr) {
+			segs[seg - 1].ds_len += PAGE_SIZE;
+		} else {
+			KASSERT(seg < nsegs);
+			segs[seg].ds_addr = baddr;
+			segs[seg].ds_len = PAGE_SIZE;
+			seg++;
+			KASSERT(seg <= nsegs);
+		}
+	}
+	KASSERT(seg <= nsegs);
+	*rsegs = seg;
+
+	return 0;
 }
 
 #endif	/* _DRM_BUS_DMA_HACKS_H_ */
