9234dac925f80e7090d5c1aba129655fe003a5e7
riastradh <riastradh@NetBSD.org>
2018-08-27 06:38:22 +0000

    Spell the field names right.

--- sys/external/bsd/drm2/include/linux/interval_tree.h
+++ sys/external/bsd/drm2/include/linux/interval_tree.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: interval_tree.h,v 1.2 2018/08/27 06:37:29 riastradh Exp $	*/
+/*	$NetBSD: interval_tree.h,v 1.3 2018/08/27 06:38:22 riastradh Exp $	*/
 
 /*-
  * Copyright (c) 2018 The NetBSD Foundation, Inc.
@@ -75,8 +75,8 @@ interval_tree_compare_key(void *cookie, const void *vn, const void *vk)
 }
 
 static const rb_tree_ops_t interval_tree_ops = {
-	.rbto_compare_nodes_fn = interval_tree_compare_nodes,
-	.rbto_compare_key_fn = interval_tree_compare_key,
+	.rbto_compare_nodes = interval_tree_compare_nodes,
+	.rbto_compare_key = interval_tree_compare_key,
 	.rbto_node_offset = offsetof(struct interval_tree_node, itn_node),
 };
 
