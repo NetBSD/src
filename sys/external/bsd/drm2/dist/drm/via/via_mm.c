14dc58e1e85b7654289c02e5fbe898049cc7bb98
riastradh <riastradh@NetBSD.org>
2018-08-27 14:14:28 +0000

    Sprinkle idr_preload/idr_preload_end.
    Try to do these outside all mutex locks.
    
    Tricky case is i915_gem_context, where the struct mutex is held for a
    long time.

--- sys/external/bsd/drm2/dist/drm/via/via_mm.c
+++ sys/external/bsd/drm2/dist/drm/via/via_mm.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: via_mm.c,v 1.3 2018/08/27 04:58:37 riastradh Exp $	*/
+/*	$NetBSD: via_mm.c,v 1.4 2018/08/27 14:14:29 riastradh Exp $	*/
 
 /*
  * Copyright 2006 Tungsten Graphics Inc., Bismarck, ND., USA.
@@ -28,7 +28,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: via_mm.c,v 1.3 2018/08/27 04:58:37 riastradh Exp $");
+__KERNEL_RCSID(0, "$NetBSD: via_mm.c,v 1.4 2018/08/27 14:14:29 riastradh Exp $");
 
 #include <drm/drmP.h>
 #include <drm/via_drm.h>
@@ -126,12 +126,14 @@ int via_mem_alloc(struct drm_device *dev, void *data,
 		DRM_ERROR("Unknown memory type allocation\n");
 		return -EINVAL;
 	}
+	idr_preload(GFP_KERNEL);
 	mutex_lock(&dev->struct_mutex);
 	if (0 == ((mem->type == VIA_MEM_VIDEO) ? dev_priv->vram_initialized :
 		      dev_priv->agp_initialized)) {
 		DRM_ERROR
 		    ("Attempt to allocate from uninitialized memory manager.\n");
 		mutex_unlock(&dev->struct_mutex);
+		idr_preload_end();
 		return -EINVAL;
 	}
 
@@ -160,6 +162,7 @@ int via_mem_alloc(struct drm_device *dev, void *data,
 
 	list_add(&item->owner_list, &file_priv->obj_list);
 	mutex_unlock(&dev->struct_mutex);
+	idr_preload_end();
 
 	mem->offset = ((mem->type == VIA_MEM_VIDEO) ?
 		      dev_priv->vram_offset : dev_priv->agp_offset) +
@@ -173,6 +176,7 @@ fail_idr:
 fail_alloc:
 	kfree(item);
 	mutex_unlock(&dev->struct_mutex);
+	idr_preload_end();
 
 	mem->offset = 0;
 	mem->size = 0;
