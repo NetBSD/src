a5cabc9c2db6e35774f22f383764ae211023999a
riastradh <riastradh@NetBSD.org>
2018-08-27 07:14:42 +0000

    Define synchronize_irq to broadcast xcall noop in <linux/hardirq.h>.

---
+++ sys/external/bsd/drm2/include/linux/hardirq.h
@@ -0,0 +1,59 @@
+/*	$NetBSD: hardirq.h,v 1.1 2018/08/27 07:14:42 riastradh Exp $	*/
+
+/*-
+ * Copyright (c) 2018 The NetBSD Foundation, Inc.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to The NetBSD Foundation
+ * by Taylor R. Campbell.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef	_LINUX_HARDIRQ_H_
+#define	_LINUX_HARDIRQ_H_
+
+#include <sys/types.h>
+#include <sys/null.h>
+#include <sys/xcall.h>
+
+static inline void
+synchronize_irq_xc(void *a, void *b)
+{
+}
+
+/*
+ * synchronize(irq)
+ *
+ *	Wait for all interrupt handlers servicing irq to complete on
+ *	all CPUs.  We just do a low-priority cross-call broadcast to
+ *	all CPUs, which can't run until interrupt handlers are done
+ *	running.
+ */
+static inline void
+synchronize_irq(int irq)
+{
+
+	xc_wait(xc_broadcast(0, synchronize_irq_xc, NULL, NULL));
+}
+
+#endif	/* _LINUX_HARDIRQ_H_ */
