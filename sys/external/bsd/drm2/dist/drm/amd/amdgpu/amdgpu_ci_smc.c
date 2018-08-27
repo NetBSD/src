8d1ecfd1edcc2593a86a3684cbf672b2c2a0b75c
riastradh <riastradh@NetBSD.org>
2018-08-27 14:24:03 +0000

    Make amdgpu cik build.
    - Sprinkle __UNCONST and __UNVOLATILE judiciously.
    - Ifdef out debugfs stuff and other unused things.
    - Add includes.
    - Convert void * to char * before arithmetic.
    - Add prototype for amdgpu_cik_gpu_check_soft_reset.
      . XXX Should really be in a header file, but whatever...
    - Mark unused variables.
    - sprintf -> snprintf

--- sys/external/bsd/drm2/dist/drm/amd/amdgpu/amdgpu_ci_smc.c
+++ sys/external/bsd/drm2/dist/drm/amd/amdgpu/amdgpu_ci_smc.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: amdgpu_ci_smc.c,v 1.1 2018/08/27 14:22:31 riastradh Exp $	*/
+/*	$NetBSD: amdgpu_ci_smc.c,v 1.2 2018/08/27 14:24:03 riastradh Exp $	*/
 
 /*
  * Copyright 2011 Advanced Micro Devices, Inc.
@@ -25,9 +25,10 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: amdgpu_ci_smc.c,v 1.1 2018/08/27 14:22:31 riastradh Exp $");
+__KERNEL_RCSID(0, "$NetBSD: amdgpu_ci_smc.c,v 1.2 2018/08/27 14:24:03 riastradh Exp $");
 
 #include <linux/firmware.h>
+#include <asm/byteorder.h>
 #include "drmP.h"
 #include "amdgpu.h"
 #include "cikd.h"
