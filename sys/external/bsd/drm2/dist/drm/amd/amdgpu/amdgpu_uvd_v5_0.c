6c8ecbe45bf55b1752d5a6d6387a3c16e596e0f3
riastradh <riastradh@NetBSD.org>
2018-08-27 14:10:14 +0000

    Rename amdgpu files to avoid clashes with radeon.

--- sys/external/bsd/drm2/dist/drm/amd/amdgpu/amdgpu_uvd_v5_0.c
+++ sys/external/bsd/drm2/dist/drm/amd/amdgpu/amdgpu_uvd_v5_0.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: uvd_v5_0.c,v 1.3 2018/08/27 14:04:50 riastradh Exp $	*/
+/*	$NetBSD: amdgpu_uvd_v5_0.c,v 1.1 2018/08/27 14:10:14 riastradh Exp $	*/
 
 /*
  * Copyright 2014 Advanced Micro Devices, Inc.
@@ -25,7 +25,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: uvd_v5_0.c,v 1.3 2018/08/27 14:04:50 riastradh Exp $");
+__KERNEL_RCSID(0, "$NetBSD: amdgpu_uvd_v5_0.c,v 1.1 2018/08/27 14:10:14 riastradh Exp $");
 
 #include <linux/firmware.h>
 #include <linux/log2.h>
