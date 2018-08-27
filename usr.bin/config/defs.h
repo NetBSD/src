4f88f12a3a2668061da3b1bab5329a29310d8a06
riastradh <riastradh@NetBSD.org>
2018-08-27 16:04:45 +0000

    Bump config(5) version for OPT.foo.c variables.

--- usr.bin/config/defs.h
+++ usr.bin/config/defs.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: defs.h,v 1.103 2018/04/09 17:46:56 christos Exp $	*/
+/*	$NetBSD: defs.h,v 1.104 2018/08/27 16:04:45 riastradh Exp $	*/
 
 /*
  * Copyright (c) 1992, 1993
@@ -107,7 +107,7 @@ extern const char *progname;
  * The next two lines define the current version of the config(1) binary,
  * and the minimum version of the configuration files it supports.
  */
-#define CONFIG_VERSION		20171118
+#define CONFIG_VERSION		20180827
 #define CONFIG_MINVERSION	0
 
 /*
