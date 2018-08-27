b9c2e6b60fb8ccb39d074fabdfb7242cd0b1a487
riastradh <riastradh@NetBSD.org>
2018-08-27 15:08:54 +0000

    Hash-locked atomic64 to give ports like powerpc32 a chance at radeon.

--- sys/external/bsd/drm2/include/linux/atomic.h
+++ sys/external/bsd/drm2/include/linux/atomic.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: atomic.h,v 1.14 2018/08/27 13:58:16 riastradh Exp $	*/
+/*	$NetBSD: atomic.h,v 1.15 2018/08/27 15:08:54 riastradh Exp $	*/
 
 /*-
  * Copyright (c) 2013 The NetBSD Foundation, Inc.
@@ -229,6 +229,11 @@ struct atomic64 {
 
 typedef struct atomic64 atomic64_t;
 
+int		linux_atomic64_init(void);
+void		linux_atomic64_fini(void);
+
+#ifdef __HAVE_ATOMIC64_OPS
+
 static inline uint64_t
 atomic64_read(const struct atomic64 *a)
 {
@@ -285,6 +290,24 @@ atomic64_cmpxchg(struct atomic64 *atomic, uint64_t expect, uint64_t new)
 
 	return old;
 }
+
+#else  /* !defined(__HAVE_ATOMIC64_OPS) */
+
+#define	atomic64_read		linux_atomic64_read
+#define	atomic64_set		linux_atomic64_set
+#define	atomic64_add		linux_atomic64_add
+#define	atomic64_sub		linux_atomic64_sub
+#define	atomic64_xchg		linux_atomic64_xchg
+#define	atomic64_cmpxchg	linux_atomic64_cmpxchg
+
+uint64_t	atomic64_read(const struct atomic64 *);
+void		atomic64_set(struct atomic64 *, uint64_t);
+void		atomic64_add(long long, struct atomic64 *);
+void		atomic64_sub(long long, struct atomic64 *);
+uint64_t	atomic64_xchg(struct atomic64 *, uint64_t);
+uint64_t	atomic64_cmpxchg(struct atomic64 *, uint64_t, uint64_t);
+
+#endif
 
 struct atomic_long {
 	volatile unsigned long	al_v;
