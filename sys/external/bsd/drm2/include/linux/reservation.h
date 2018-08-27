e6b27d8e1598273e9b3279239ec4aeb6ba0a933a
riastradh <riastradh@NetBSD.org>
2018-08-27 13:33:59 +0000

    Draft implementation of the Linux reservation and fence APIs.

--- sys/external/bsd/drm2/include/linux/reservation.h
+++ sys/external/bsd/drm2/include/linux/reservation.h
@@ -1,7 +1,7 @@
-/*	$NetBSD: reservation.h,v 1.4 2018/08/27 07:34:41 riastradh Exp $	*/
+/*	$NetBSD: reservation.h,v 1.5 2018/08/27 13:33:59 riastradh Exp $	*/
 
 /*-
- * Copyright (c) 2014 The NetBSD Foundation, Inc.
+ * Copyright (c) 2018 The NetBSD Foundation, Inc.
  * All rights reserved.
  *
  * This code is derived from software contributed to The NetBSD Foundation
@@ -29,89 +29,61 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifndef _LINUX_RESERVATION_H_
-#define _LINUX_RESERVATION_H_
-
-#include <sys/atomic.h>
+#ifndef	_LINUX_RESERVATION_H_
+#define	_LINUX_RESERVATION_H_
 
 #include <linux/rcupdate.h>
 #include <linux/ww_mutex.h>
 
 struct fence;
 
-extern struct ww_class	reservation_ww_class;
-
 struct reservation_object {
 	struct ww_mutex		lock;
 
-	struct reservation_object_list __rcu	*robj_objlist;
-	struct fence __rcu			*robj_fence_excl;
+	unsigned				robj_version;
+	struct fence __rcu			*robj_fence;
+	struct reservation_object_list __rcu	*robj_list;
+	struct reservation_object_list __rcu	*robj_prealloc;
+	uint32_t				robj_nreserved;
 };
 
 struct reservation_object_list {
+	struct rcu_head		rol_rcu;
+
 	uint32_t		shared_count;
+	uint32_t		shared_max;
 	struct fence __rcu	*shared[];
 };
 
 #define	reservation_object_add_excl_fence	linux_reservation_object_add_excl_fence
 #define	reservation_object_add_shared_fence	linux_reservation_object_add_shared_fence
+#define	reservation_object_fini			linux_reservation_object_fini
+#define	reservation_object_get_excl		linux_reservation_object_get_excl
+#define	reservation_object_get_list		linux_reservation_object_get_list
+#define	reservation_object_held			linux_reservation_object_held
+#define	reservation_object_init			linux_reservation_object_init
 #define	reservation_object_reserve_shared	linux_reservation_object_reserve_shared
 #define	reservation_object_test_signaled_rcu	linux_reservation_object_test_signaled_rcu
 #define	reservation_object_wait_timeout_rcu	linux_reservation_object_wait_timeout_rcu
+#define	reservation_ww_class			linux_reservation_ww_class
 
+extern struct ww_class	reservation_ww_class;
+
+void	reservation_object_init(struct reservation_object *);
+void	reservation_object_fini(struct reservation_object *);
+bool	reservation_object_held(struct reservation_object *);
+struct fence *
+	reservation_object_get_excl(struct reservation_object *);
+struct reservation_object_list *
+	reservation_object_get_list(struct reservation_object *);
+int	reservation_object_reserve_shared(struct reservation_object *);
 void	reservation_object_add_excl_fence(struct reservation_object *,
 	    struct fence *);
 void	reservation_object_add_shared_fence(struct reservation_object *,
 	    struct fence *);
-int	reservation_object_reserve_shared(struct reservation_object *);
 bool	reservation_object_test_signaled_rcu(struct reservation_object *,
 	    bool);
 long	reservation_object_wait_timeout_rcu(struct reservation_object *,
 	    bool, bool, unsigned long);
 
-static inline void
-reservation_object_init(struct reservation_object *reservation)
-{
-
-	ww_mutex_init(&reservation->lock, &reservation_ww_class);
-}
-
-static inline void
-reservation_object_fini(struct reservation_object *reservation)
-{
-
-	ww_mutex_destroy(&reservation->lock);
-}
-
-static inline bool
-reservation_object_held(struct reservation_object *reservation)
-{
-
-	return ww_mutex_is_locked(&reservation->lock);
-}
-
-static inline struct fence *
-reservation_object_get_excl(struct reservation_object *reservation)
-{
-	struct fence *fence;
-
-	KASSERT(reservation_object_held(reservation));
-	fence = reservation->robj_fence_excl;
-	membar_datadep_consumer();
-
-	return fence;
-}
-
-static inline struct reservation_object_list *
-reservation_object_get_list(struct reservation_object *reservation)
-{
-	struct reservation_object_list *objlist;
-
-	KASSERT(reservation_object_held(reservation));
-	objlist = reservation->robj_objlist;
-	membar_datadep_consumer();
-
-	return objlist;
-}
-
-#endif  /* _LINUX_RESERVATION_H_ */
+#endif	/* _LINUX_RESERVATION_H_ */
