75ea30dbac5f0ccc7abd332ce3966d3d28368432
riastradh <riastradh@NetBSD.org>
2018-08-27 14:22:31 +0000

    New option AMDGPU_CIK for devices covered by radeon and amdgpu.

--- sys/external/bsd/drm2/dist/drm/amd/amdgpu/amdgpu_kv_smc.c
+++ sys/external/bsd/drm2/dist/drm/amd/amdgpu/amdgpu_kv_smc.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: kv_smc.c,v 1.2 2018/08/27 04:58:20 riastradh Exp $	*/
+/*	$NetBSD: amdgpu_kv_smc.c,v 1.1 2018/08/27 14:22:31 riastradh Exp $	*/
 
 /*
  * Copyright 2013 Advanced Micro Devices, Inc.
@@ -25,7 +25,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: kv_smc.c,v 1.2 2018/08/27 04:58:20 riastradh Exp $");
+__KERNEL_RCSID(0, "$NetBSD: amdgpu_kv_smc.c,v 1.1 2018/08/27 14:22:31 riastradh Exp $");
 
 #include "drmP.h"
 #include "amdgpu.h"
