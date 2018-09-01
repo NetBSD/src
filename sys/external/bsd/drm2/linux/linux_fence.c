977dae7e2482ce59b18257051ce7c434dee2f121
riastradh <riastradh@NetBSD.org>
2018-09-01 22:28:04 +0000

    Allow fence_get(NULL).
    This is kind of silly: if the caller has exclusive access to the
    fence, they should be able to guarantee it's there, so this could
    serve to suppress legitimate bugs.  But better to preserve Linux
    semantics here than to be picky about API design.
    
    Bug by me, fix from Tobias Ulmer in PR kern/53565.

--- sys/external/bsd/drm2/linux/linux_fence.c
+++ sys/external/bsd/drm2/linux/linux_fence.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: linux_fence.c,v 1.12 2018/08/28 15:04:02 riastradh Exp $	*/
+/*	$NetBSD: linux_fence.c,v 1.13 2018/09/01 22:28:04 riastradh Exp $	*/
 
 /*-
  * Copyright (c) 2018 The NetBSD Foundation, Inc.
@@ -30,7 +30,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: linux_fence.c,v 1.12 2018/08/28 15:04:02 riastradh Exp $");
+__KERNEL_RCSID(0, "$NetBSD: linux_fence.c,v 1.13 2018/09/01 22:28:04 riastradh Exp $");
 
 #include <sys/atomic.h>
 #include <sys/condvar.h>
@@ -179,7 +179,8 @@ struct fence *
 fence_get(struct fence *fence)
 {
 
-	kref_get(&fence->refcount);
+	if (fence)
+		kref_get(&fence->refcount);
 	return fence;
 }
 
