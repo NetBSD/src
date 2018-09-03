7fc47c75fec0cb04ebf67e6834b5747e36d0df39
riastradh <riastradh@NetBSD.org>
2018-09-03 16:55:17 +0000

    Welcome to 8.99.25, courtesy of min/max ---> uimin/uimax.

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.569 2018/08/10 21:48:19 pgoyette Exp $	*/
+/*	$NetBSD: param.h,v 1.570 2018/09/03 16:55:17 riastradh Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -67,7 +67,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	899002400	/* NetBSD 8.99.24 */
+#define	__NetBSD_Version__	899002500	/* NetBSD 8.99.25 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
