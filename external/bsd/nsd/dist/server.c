8ae5c8cd4753af8ccbfbcd14a74a76f9b811765b
christos <christos@NetBSD.org>
2018-09-03 11:29:14 +0000

    Import nsd-4.1.24
    6 August 2018: Wouter
    	- tag for 4.1.24 release.
    
    30 July 2018: Wouter
    	- Tag for NSD 4.1.23 release, trunk is 4.1.24, includes
    	  fix NSD time sensitive TSIG compare vulnerability.
    	- Fix checkconf test for use-systemd option.
    
    25 July 2018: Wouter
    	- #4133: Fix that when IXFR contains a zone with broken NSEC3PARAM
    	  chain, NSD leniently attempts to find a working NSEC3PARAM.
    
    23 July 2018: Wouter
    	- Remove socket activation from systemd code, it was reported as
    	  not useful to enable.  The readiness signalling is still there,
    	  and can be enabled with use-systemd: yes.
    	- Only call sd_notify from systemd when use-systemd is yes.
    
    6 July 2018: Wouter
    	- RFC8162 support, for record type SMIMEA.
    	- Fix that type CAA (and URI) in the zone file can contain
    	  dots when not in quotes.
    
    26 June 2018: Wouter
    	- configure --enable-systemd (needs pkg-config and libsystemd) can
    	  be used to then use-systemd: yes in nsd.conf and use socket
    	  activation and readiness signalling with systemd.
    
    19 June 2018: Wouter
    	- #4106: Fix that stats printed from nsd-control are recast from
    	  unsigned long to unsigned (remote.c).
    
    14 June 2018: Wouter
    	- Fix that first control-interface determines if TLS is used.  Warn
    	  when IP address interfaces are used without TLS.
    
    12 June 2018: Wouter
    	- #4102: control interface via local socket.
    	  configure it with control-interface: "/path/nsd.ctl"  The path
    	  has to start with a / to separate it from an IP address.
    	  The local socket does not use SSL, but unencrypted traffic, use
    	  file and containing directory permissions to restrict access.
    
    6 June 2018: Wouter
    	- Patch to fix openwrt for mac os build darwin detection in configure.
    
    4 June 2018: Wouter
    	- tag for 4.1.22rc1.  Became 4.1.22 on 11 June, trunk is 4.1.23 in
    	  development from this point.
    
    31 May 2018: Wouter
    	- Fix to use same condition for nsec3 hash allocation and free.
    
    23 May 2018: Wouter
    	- Use accept4 to speed up answer of TCP queries, on Linux and FreeBSD
    	  and OpenBSD.
    
    22 May 2018: Wouter
    	- Fix nsec3 hash of parent and child co-hosted nsec3 enabled zones.
    
    15 May 2018: Wouter
    	- Fix memory free in unit test.
    
    14 May 2018: Wouter
    	- Tag for 4.1.21 release.
    	- trunk has 4.1.22 in development.
    	- refuse-any sends truncation (+TC) in reply to ANY queries over UDP,
    	  and allows TCP queries like normal.
    
    7 May 2018: Wouter
    	- Tag for 4.1.21rc1 release.
    
    4 May 2018: Wouter
    	- Fix #4093: Release notes not using 2018.
    
    3 May 2018: Wouter
    	- Fix buffer size warnings from compiler on filename lengths.
    
    26 April 2018: Wouter
    	- lower memory usage for tcp connections, so tcp-count can be higher.
    	- Fix checkconf test for refuse-any option.
    
    3 April 2018: Wouter
    	- refuse-any nsd.conf option that refuses queries of type ANY.
    
    5 March 2018: Wouter
    	- Fix #3562: explain build error when flex missing.
    
    20 February 2018: Wouter
    	- For more clang warnings
    	- Fix spelling error in xfr-inspect.
    
    19 February 2018: Wouter
    	- Fix for clang analysis complaints.
    
    15 February 2018: Wouter
    	- --enable-memclean cleans up memory for use with memory checkers,
    	  eg. valgrind.
    	- Fix unused variable warnings from clang analyzer.
    
    14 February 2018: Wouter
    	- updated RELNOTES for upcoming release.
    	- tag 4.1.20rc1, became release on 20 feb, trunk has 4.1.21 in
    	  development.
    
    9 February 2018: Wouter
    	- make depend: updated the make dependencies in the Makefile.
    
    8 February 2018: Wouter
    	- Fix memory leak when rehashing nsec3 after axfr or zonefile read,
    	  in the selectively allocated precompiled nsec3 hashes.
    
    6 February 2018: Wouter
    	- Fix memory leak in zone file read of unknown rr formatted RRs.

--- external/bsd/nsd/dist/server.c
+++ external/bsd/nsd/dist/server.c
@@ -219,6 +219,7 @@ static void configure_handler_event_types(short event_types);
 static uint16_t *compressed_dname_offsets = 0;
 static uint32_t compression_table_capacity = 0;
 static uint32_t compression_table_size = 0;
+static domain_type* compressed_dnames[MAXRRSPP];
 
 /*
  * Remove the specified pid from the list of child pids.  Returns -1 if
@@ -308,6 +309,9 @@ restart_child_servers(struct nsd *nsd, region_type* region, netio_type* netio,
 				/* the child need not be able to access the
 				 * nsd.db file */
 				namedb_close_udb(nsd->db);
+#ifdef MEMCLEAN /* OS collects memory pages */
+				region_destroy(region);
+#endif
 				nsd->pid = 0;
 				nsd->child_count = 0;
 				nsd->server_kind = nsd->children[i].kind;
@@ -759,7 +763,8 @@ server_init_ifs(struct nsd *nsd, size_t from, size_t to, int* reuseport_works)
 #endif /* SO_BINDANY */
 		}
 
-		if (bind(nsd->udp[i].s, (struct sockaddr *) addr->ai_addr, addr->ai_addrlen) != 0) {
+		if (
+			bind(nsd->udp[i].s, (struct sockaddr *) addr->ai_addr, addr->ai_addrlen) != 0) {
 			log_msg(LOG_ERR, "can't bind udp socket: %s", strerror(errno));
 			return -1;
 		}
@@ -895,7 +900,8 @@ server_init_ifs(struct nsd *nsd, size_t from, size_t to, int* reuseport_works)
 #endif /* SO_BINDANY */
 		}
 
-		if (bind(nsd->tcp[i].s, (struct sockaddr *) addr->ai_addr, addr->ai_addrlen) != 0) {
+		if(
+			bind(nsd->tcp[i].s, (struct sockaddr *) addr->ai_addr, addr->ai_addrlen) != 0) {
 			log_msg(LOG_ERR, "can't bind tcp socket: %s", strerror(errno));
 			return -1;
 		}
@@ -1073,7 +1079,14 @@ server_shutdown(struct nsd *nsd)
 	daemon_remote_delete(nsd->rc); /* ssl-delete secret keys */
 #endif
 
-#if 0 /* OS collects memory pages */
+#ifdef MEMCLEAN /* OS collects memory pages */
+#ifdef RATELIMIT
+	rrl_mmap_deinit_keep_mmap();
+#endif
+	udb_base_free_keep_mmap(nsd->task[0]);
+	udb_base_free_keep_mmap(nsd->task[1]);
+	namedb_close_udb(nsd->db); /* keeps mmap */
+	namedb_close(nsd->db);
 	nsd_options_destroy(nsd->options);
 	region_destroy(nsd->region);
 #endif
@@ -1841,7 +1854,7 @@ server_main(struct nsd *nsd)
 			/* only quit children after xfrd has acked */
 			send_children_quit(nsd);
 
-#if 0 /* OS collects memory pages */
+#ifdef MEMCLEAN /* OS collects memory pages */
 			region_destroy(server_region);
 #endif
 			server_shutdown(nsd);
@@ -1920,7 +1933,7 @@ server_main(struct nsd *nsd)
 		(void)kill(nsd->pid, SIGTERM);
 	}
 
-#if 0 /* OS collects memory pages */
+#ifdef MEMCLEAN /* OS collects memory pages */
 	region_destroy(server_region);
 #endif
 	/* write the nsd.db to disk, wait for it to complete */
@@ -1990,6 +2003,8 @@ server_child(struct nsd *nsd)
 		log_msg(LOG_ERR, "nsd server could not create event base");
 		exit(1);
 	}
+	nsd->event_base = event_base;
+	nsd->server_region = server_region;
 
 #ifdef RATELIMIT
 	rrl_init(nsd->this_child->child_num);
@@ -2038,13 +2053,15 @@ server_child(struct nsd *nsd)
 	if (nsd->server_kind & NSD_SERVER_UDP) {
 #if (defined(NONBLOCKING_IS_BROKEN) || !defined(HAVE_RECVMMSG))
 		udp_query = query_create(server_region,
-			compressed_dname_offsets, compression_table_size);
+			compressed_dname_offsets, compression_table_size,
+			compressed_dnames);
 #else
 		udp_query = NULL;
 		memset(msgs, 0, sizeof(msgs));
 		for (i = 0; i < NUM_RECV_PER_SELECT; i++) {
 			queries[i] = query_create(server_region,
-				compressed_dname_offsets, compression_table_size);
+				compressed_dname_offsets,
+				compression_table_size, compressed_dnames);
 			query_reset(queries[i], UDP_MAX_MESSAGE_LEN, 0);
 			iovecs[i].iov_base          = buffer_begin(queries[i]->packet);
 			iovecs[i].iov_len           = buffer_remaining(queries[i]->packet);;
@@ -2156,7 +2173,10 @@ server_child(struct nsd *nsd)
 	bind8_stats(nsd);
 #endif /* BIND8_STATS */
 
-#if 0 /* OS collects memory pages */
+#ifdef MEMCLEAN /* OS collects memory pages */
+#ifdef RATELIMIT
+	rrl_deinit(nsd->this_child->child_num);
+#endif
 	event_base_free(event_base);
 	region_destroy(server_region);
 #endif
@@ -2862,7 +2882,11 @@ handle_tcp_accept(int fd, short event, void* arg)
 
 	/* Accept it... */
 	addrlen = sizeof(addr);
+#ifndef HAVE_ACCEPT4
 	s = accept(fd, (struct sockaddr *) &addr, &addrlen);
+#else
+	s = accept4(fd, (struct sockaddr *) &addr, &addrlen, SOCK_NONBLOCK);
+#endif
 	if (s == -1) {
 		/**
 		 * EMFILE and ENFILE is a signal that the limit of open
@@ -2899,11 +2923,13 @@ handle_tcp_accept(int fd, short event, void* arg)
 		return;
 	}
 
+#ifndef HAVE_ACCEPT4
 	if (fcntl(s, F_SETFL, O_NONBLOCK) == -1) {
 		log_msg(LOG_ERR, "fcntl failed: %s", strerror(errno));
 		close(s);
 		return;
 	}
+#endif
 
 	/*
 	 * This region is deallocated when the TCP connection is
@@ -2914,7 +2940,7 @@ handle_tcp_accept(int fd, short event, void* arg)
 		tcp_region, sizeof(struct tcp_handler_data));
 	tcp_data->region = tcp_region;
 	tcp_data->query = query_create(tcp_region, compressed_dname_offsets,
-		compression_table_size);
+		compression_table_size, compressed_dnames);
 	tcp_data->nsd = data->nsd;
 	tcp_data->query_count = 0;
 
