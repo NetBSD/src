151694e9e1f86dad0dd2c100b61e918c5bd9b224
mrg <mrg@NetBSD.org>
2018-09-05 06:26:58 +0000

    merge in missing changes from gcc to gcc.old (both GCC 6.4.):
    --
    redo mknative-gcc for all ports.  main changes include:
    - "#define HAVE_CC_TLS 1" for most/all ports, thanks maya@
    - "#define _GLIBCXX_HAVE_LDEXPL 1" and "#define _GLIBCXX_HAVE_TGMATH_H 1"
      for many ports
    - arm64 and amd64 had a broken c++config.h that disabled many things
    - configargs.h has more normalisation
    - ppc64 has a few things fixed, must have missed several mknative rounds
    --
    regen to pull out INTERNAL_CFLAGS.
    --
    pull -DHAVE_CC_TLS out of $(INTERNAL_CFLAGS) if it is there, and add
    it to CPPFLAGS.
    
    this fixes PR#53567 for me.

--- external/gpl3/gcc.old/lib/libstdc++-v3/arch/earmv4/c++config.h
+++ external/gpl3/gcc.old/lib/libstdc++-v3/arch/earmv4/c++config.h
@@ -847,7 +847,7 @@ namespace std
 #define _GLIBCXX_HAVE_LDEXPF 1
 
 /* Define to 1 if you have the `ldexpl' function. */
-/* #undef _GLIBCXX_HAVE_LDEXPL */
+#define _GLIBCXX_HAVE_LDEXPL 1
 
 /* Define to 1 if you have the <libintl.h> header file. */
 /* #undef _GLIBCXX_HAVE_LIBINTL_H */
@@ -1067,7 +1067,7 @@ namespace std
 #define _GLIBCXX_HAVE_TANL 1
 
 /* Define to 1 if you have the <tgmath.h> header file. */
-/* #undef _GLIBCXX_HAVE_TGMATH_H */
+#define _GLIBCXX_HAVE_TGMATH_H 1
 
 /* Define to 1 if the target supports thread-local storage. */
 #define _GLIBCXX_HAVE_TLS 1
