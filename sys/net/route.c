6bef1280e7d6b7df620600491992a7f7e5324bdd
ozaki-r <ozaki-r@NetBSD.org>
2018-09-05 02:49:40 +0000

    route: avoid overwriting rt_free_global.enqueued unexpectedly
    rt_free_global.enqueued can be set to true by rt_free during rt_free_work
    because rt_free_work releases rt_free_global.lock.  So rt_free_work must update
    it once and not update after releasing the lock.

--- sys/net/route.c
+++ sys/net/route.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: route.c,v 1.212 2018/09/05 02:49:09 ozaki-r Exp $	*/
+/*	$NetBSD: route.c,v 1.213 2018/09/05 02:49:40 ozaki-r Exp $	*/
 
 /*-
  * Copyright (c) 1998, 2008 The NetBSD Foundation, Inc.
@@ -97,7 +97,7 @@
 #endif
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: route.c,v 1.212 2018/09/05 02:49:09 ozaki-r Exp $");
+__KERNEL_RCSID(0, "$NetBSD: route.c,v 1.213 2018/09/05 02:49:40 ozaki-r Exp $");
 
 #include <sys/param.h>
 #ifdef RTFLUSH_DEBUG
@@ -702,8 +702,8 @@ rt_free_work(struct work *wk, void *arg)
 		struct rtentry *rt;
 
 		mutex_enter(&rt_free_global.lock);
-		rt_free_global.enqueued = false;
 		if ((rt = SLIST_FIRST(&rt_free_global.queue)) == NULL) {
+			rt_free_global.enqueued = false;
 			mutex_exit(&rt_free_global.lock);
 			return;
 		}
