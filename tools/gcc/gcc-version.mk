773bbb1071e2dbc1db22a84ce3116efbc25987e9
mrg <mrg@NetBSD.org>
2018-09-05 05:03:28 +0000

    remove GCC 5 marker.  this is now nb3 20180905.

--- tools/gcc/gcc-version.mk
+++ tools/gcc/gcc-version.mk
@@ -1,9 +1,9 @@
-#	$NetBSD: gcc-version.mk,v 1.10 2018/03/28 19:30:41 mrg Exp $
+#	$NetBSD: gcc-version.mk,v 1.11 2018/09/05 05:03:28 mrg Exp $
 
 # common location for tools and native build
 
-.if ${HAVE_GCC} == 5
-NETBSD_GCC_VERSION=nb2 20180327
-.elif ${HAVE_GCC} == 6
-NETBSD_GCC_VERSION=nb2 20180327
+.if ${HAVE_GCC} == 6
+NETBSD_GCC_VERSION=nb3 20180905
+.elif ${HAVE_GCC} == 7
+NETBSD_GCC_VERSION=nb1 20180905
 .endif
