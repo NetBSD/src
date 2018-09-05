d4071ebc554a8a5826823d6fd8527f307489d868
martin <martin@NetBSD.org>
2018-09-05 08:59:04 +0000

    Pull up following revision(s) (requested by mrg in ticket #1008):
    	tools/gcc/gcc-version.mk: revision 1.11
    	tools/gcc/mknative-gcc: revision 1.93
    	external/gpl3/gcc/dist/config/tls.m4: revision 1.2
    	external/gpl3/gcc/dist/libgcc/configure: revision 1.5
    
    Correct extra parens, regen configure (selectively)
    Fixes root cause of PR toolchain/53567
    
    in configargs.h, as well as normalising to /usr/src, normalise the
    --build to the same as the --host, so that it never changes based
    upon the host you ran mknative on.  (some recent changes are only
    because i updated a system from netbsd-7 to netbsd-8.  this will
    avoid that in the future.)
    
    remove GCC 5 marker.  this is now nb3 20180905.

--- tools/gcc/gcc-version.mk
+++ tools/gcc/gcc-version.mk
@@ -1,5 +1,5 @@
-#	$NetBSD: gcc-version.mk,v 1.7.4.1 2018/04/01 09:00:42 martin Exp $
+#	$NetBSD: gcc-version.mk,v 1.7.4.2 2018/09/05 08:59:04 martin Exp $
 
 # common location for tools and native build
 
-NETBSD_GCC_VERSION=nb2 20180327
+NETBSD_GCC_VERSION=nb3 20180905
