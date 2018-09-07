25f293be969d13f2855e27c340fa27ac6893864d
martin <martin@NetBSD.org>
2018-09-07 12:34:18 +0000

    Pull up following revision(s) (requested by mrg in ticket #1013):
    	sys/sys/lockdebug.h: revision 1.20
    	sys/ddb/db_command.c: revision 1.155
    	sys/kern/subr_lockdebug.c: revision 1.65
    
    add a lockdebug_dismiss() function for DDB to use that tells
    lockdebug to avoid asserts.  use it for the ddb "reboot"
    command, so that it doesn't matter how ddb was entered, the
    reboot not trigger mutex checks and not work.

--- sys/sys/lockdebug.h
+++ sys/sys/lockdebug.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: lockdebug.h,v 1.15.6.1 2018/04/02 09:07:53 martin Exp $	*/
+/*	$NetBSD: lockdebug.h,v 1.15.6.2 2018/09/07 12:34:18 martin Exp $	*/
 
 /*-
  * Copyright (c) 2006, 2007, 2008 The NetBSD Foundation, Inc.
@@ -53,6 +53,7 @@ typedef	struct lockops {
 #define	LOCKDEBUG_ABORT(f, ln, l, o, m) \
     lockdebug_abort(f, ln, l, o, m)
 
+void	lockdebug_dismiss(void);
 void	lockdebug_abort(const char *, size_t, volatile void *, lockops_t *,
     const char *);
 
