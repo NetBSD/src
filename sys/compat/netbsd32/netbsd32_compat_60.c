5116ba1041ac1e1ea2aec4a8d61c7bec93daa6dc
pgoyette <pgoyette@NetBSD.org>
2018-09-13 01:15:59 +0000

    Another dependency typo

--- sys/compat/netbsd32/netbsd32_compat_60.c
+++ sys/compat/netbsd32/netbsd32_compat_60.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: netbsd32_compat_60.c,v 1.3.2.6 2018/09/11 21:18:32 pgoyette Exp $	*/
+/*	$NetBSD: netbsd32_compat_60.c,v 1.3.2.7 2018/09/13 01:15:59 pgoyette Exp $	*/
 
 /*-
  * Copyright (c) 2008 The NetBSD Foundation, Inc.
@@ -36,7 +36,7 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: netbsd32_compat_60.c,v 1.3.2.6 2018/09/11 21:18:32 pgoyette Exp $");
+__KERNEL_RCSID(0, "$NetBSD: netbsd32_compat_60.c,v 1.3.2.7 2018/09/13 01:15:59 pgoyette Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -94,7 +94,7 @@ static struct syscall_package compat_netbsd32_60_syscalls[] = {
 }; 
 
 
-MODULE(MODULE_CLASS_EXEC, compat_netbsd32_60, "compat_netbsd,compat_60");
+MODULE(MODULE_CLASS_EXEC, compat_netbsd32_60, "compat_netbsd32,compat_60");
 
 static int
 compat_netbsd32_60_modcmd(modcmd_t cmd, void *arg)
