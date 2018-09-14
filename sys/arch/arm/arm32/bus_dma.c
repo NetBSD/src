601e49f1d3673268f6d6f20bde543e15ac48875a
skrll <skrll@NetBSD.org>
2018-09-14 10:13:02 +0000

    Fix  BUS_DMASYNC_PREREAD|BUS_DMASYNC_PREWRITE for aarch64.
    The readonly mbuf optimisation is for the old arm pmap.

--- sys/arch/arm/arm32/bus_dma.c
+++ sys/arch/arm/arm32/bus_dma.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: bus_dma.c,v 1.112 2018/09/03 16:29:23 riastradh Exp $	*/
+/*	$NetBSD: bus_dma.c,v 1.113 2018/09/14 10:13:02 skrll Exp $	*/
 
 /*-
  * Copyright (c) 1996, 1997, 1998 The NetBSD Foundation, Inc.
@@ -36,7 +36,7 @@
 #include "opt_cputypes.h"
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: bus_dma.c,v 1.112 2018/09/03 16:29:23 riastradh Exp $");
+__KERNEL_RCSID(0, "$NetBSD: bus_dma.c,v 1.113 2018/09/14 10:13:02 skrll Exp $");
 
 #include <sys/param.h>
 #include <sys/bus.h>
@@ -796,7 +796,7 @@ _bus_dmamap_sync_segment(vaddr_t va, paddr_t pa, vsize_t len, int ops,
     bool readonly_p)
 {
 
-#ifdef ARM_MMU_EXTENDED
+#if defined(ARM_MMU_EXTENDED) || defined(CPU_CORTEX)
 	/*
 	 * No optimisations are available for readonly mbufs on armv6+, so
 	 * assume it's not readonly from here on.
