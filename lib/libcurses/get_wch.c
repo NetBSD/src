de26853a328e312b5771c6ce9c0cafa7c69f5022
rin <rin@NetBSD.org>
2018-09-18 22:46:18 +0000

    PR lib/53615
    getch() and get_wch() should return KEY_RESIZE when interrupted by SIGWINCH.
    
    OK roy

--- lib/libcurses/get_wch.c
+++ lib/libcurses/get_wch.c
@@ -1,4 +1,4 @@
-/*   $NetBSD: get_wch.c,v 1.14 2017/01/31 09:17:53 roy Exp $ */
+/*   $NetBSD: get_wch.c,v 1.15 2018/09/18 22:46:18 rin Exp $ */
 
 /*
  * Copyright (c) 2005 The NetBSD Foundation Inc.
@@ -36,9 +36,10 @@
 
 #include <sys/cdefs.h>
 #ifndef lint
-__RCSID("$NetBSD: get_wch.c,v 1.14 2017/01/31 09:17:53 roy Exp $");
+__RCSID("$NetBSD: get_wch.c,v 1.15 2018/09/18 22:46:18 rin Exp $");
 #endif						  /* not lint */
 
+#include <errno.h>
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -596,7 +597,12 @@ wget_wch(WINDOW *win, wint_t *ch)
 
 		if (ferror(infd)) {
 			clearerr(infd);
-			return ERR;
+			if (errno == EINTR && _cursesi_screen->resized) {
+				_cursesi_screen->resized = 0;
+				*ch = KEY_RESIZE;
+				return KEY_CODE_YES;
+			} else
+				return ERR;
 		} else {
 			ret = c;
 			inp = c;
