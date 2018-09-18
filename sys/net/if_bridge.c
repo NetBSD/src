8332fba051b8c791d6dc06f4ddd5622258610295
msaitoh <msaitoh@NetBSD.org>
2018-09-18 09:27:35 +0000

    - Fix bridge_enqueue() which was broken by last commit. Use correct mbuf   pointer. - Modify comment.

--- sys/net/if_bridge.c
+++ sys/net/if_bridge.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: if_bridge.c,v 1.157 2018/09/14 11:05:09 msaitoh Exp $	*/
+/*	$NetBSD: if_bridge.c,v 1.158 2018/09/18 09:27:35 msaitoh Exp $	*/
 
 /*
  * Copyright 2001 Wasabi Systems, Inc.
@@ -80,7 +80,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: if_bridge.c,v 1.157 2018/09/14 11:05:09 msaitoh Exp $");
+__KERNEL_RCSID(0, "$NetBSD: if_bridge.c,v 1.158 2018/09/18 09:27:35 msaitoh Exp $");
 
 #ifdef _KERNEL_OPT
 #include "opt_bridge_ipf.h"
@@ -1764,7 +1764,9 @@ bridge_forward(struct bridge_softc *sc, struct mbuf *m)
 	bridge_release_member(sc, bif, &psref);
 
 	/*
-	 * Clear any in-bound checksum flags for this packet.
+	 * Before enqueueing this packet to the destination interface,
+	 * clear any in-bound checksum flags to prevent them from being
+	 * misused as out-bound flags.
 	 */
 	m->m_pkthdr.csum_flags = 0;
 
@@ -1978,11 +1980,12 @@ bridge_broadcast(struct bridge_softc *sc, struct ifnet *src_if,
 				sc->sc_if.if_oerrors++;
 				goto next;
 			}
-
 			/*
-			 * Clear any in-bound checksum flags for this packet.
+			 * Before enqueueing this packet to the destination
+			 * interface, clear any in-bound checksum flags to
+			 * prevent them from being misused as out-bound flags.
 			 */
-			m->m_pkthdr.csum_flags = 0;
+			mc->m_pkthdr.csum_flags = 0;
 
 			ACQUIRE_GLOBAL_LOCKS();
 			bridge_enqueue(sc, dst_if, mc, 1);
