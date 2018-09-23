0c3b500425035157ed1e132ea09fb2a9408cd2c7
christos <christos@NetBSD.org>
2018-09-23 13:32:54 +0000

    merge conflicts

--- crypto/external/bsd/openssl/dist/include/internal/refcount.h
+++ crypto/external/bsd/openssl/dist/include/internal/refcount.h
@@ -17,7 +17,7 @@
 # endif
 
 # if defined(__STDC_VERSION__) && __STDC_VERSION__ >= 201112L \
-     && !defined(__STDC_NO_ATOMICS__)
+     && !defined(__STDC_NO_ATOMICS__) && !defined(__lint__)
 #  include <stdatomic.h>
 #  define HAVE_C11_ATOMICS
 # endif
