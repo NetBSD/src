282e9cf910b66f71d767670cba5466d39cc76830
skrll <skrll@NetBSD.org>
2018-10-04 15:21:23 +0000

    Remove duplicate #include

--- sys/arch/mips/cavium/dev/octeon_fpa.c
+++ sys/arch/mips/cavium/dev/octeon_fpa.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: octeon_fpa.c,v 1.1 2015/04/29 08:32:01 hikaru Exp $	*/
+/*	$NetBSD: octeon_fpa.c,v 1.2 2018/10/04 15:21:23 skrll Exp $	*/
 
 /*
  * Copyright (c) 2007 Internet Initiative Japan, Inc.
@@ -29,9 +29,7 @@
 #undef	FPADEBUG
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: octeon_fpa.c,v 1.1 2015/04/29 08:32:01 hikaru Exp $");
-
-#include "opt_octeon.h"
+__KERNEL_RCSID(0, "$NetBSD: octeon_fpa.c,v 1.2 2018/10/04 15:21:23 skrll Exp $");
 
 #include "opt_octeon.h"
 
