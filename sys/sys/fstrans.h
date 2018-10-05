77f21f2f3b59c8102e2d89c5b21a7d04356c38f5
hannken <hannken@NetBSD.org>
2018-10-05 09:51:55 +0000

    Bring back three state file system suspension:
      NORMAL -> SUSPENDING -> SUSPENDED
    
    and add operation fstrans_start_lazy() that only blocks while SUSPENDED.
    
    Change vndthread() support operation handle_with_rdwr() to bracket
    its file system operations by fstrans_start_lazy() and fstrans_done().
    
    PR kern/53624 (dom0 freeze on domU exit)

--- sys/sys/fstrans.h
+++ sys/sys/fstrans.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: fstrans.h,v 1.11 2017/06/04 08:05:42 hannken Exp $	*/
+/*	$NetBSD: fstrans.h,v 1.12 2018/10/05 09:51:56 hannken Exp $	*/
 
 /*-
  * Copyright (c) 2007 The NetBSD Foundation, Inc.
@@ -43,12 +43,14 @@
 
 enum fstrans_state {
 	FSTRANS_NORMAL,
-	FSTRANS_SUSPENDED
+	FSTRANS_SUSPENDED,
+	FSTRANS_SUSPENDING
 };
 
 void	fstrans_init(void);
 void	fstrans_start(struct mount *);
 int	fstrans_start_nowait(struct mount *);
+void	fstrans_start_lazy(struct mount *);
 void	fstrans_done(struct mount *);
 int	fstrans_is_owner(struct mount *);
 int	fstrans_mount(struct mount *);
