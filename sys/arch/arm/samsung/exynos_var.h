e98b931ccb65c8d77ed00d4eff9edba9f0384086
skrll <skrll@NetBSD.org>
2018-10-08 08:16:59 +0000

    Remove a build dependency on SOC_EXYNOS4 and SOC_EXYNOS5.  Now runtime.

--- sys/arch/arm/samsung/exynos_var.h
+++ sys/arch/arm/samsung/exynos_var.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: exynos_var.h,v 1.26 2018/09/14 11:58:38 skrll Exp $	*/
+/*	$NetBSD: exynos_var.h,v 1.27 2018/10/08 08:17:00 skrll Exp $	*/
 
 /*-
  * Copyright (c) 2013, 2014 The NetBSD Foundation, Inc.
@@ -143,7 +143,7 @@ extern bus_space_handle_t exynos_pmu_bsh;
 extern bus_space_handle_t exynos_cmu_bsh;
 extern bus_space_handle_t exynos_sysreg_bsh;
 
-extern void exynos_bootstrap(void);
+extern void exynos_bootstrap(int);
 extern void exynos_dma_bootstrap(psize_t memsize);
 
 struct exynos_pinctrl_softc;
