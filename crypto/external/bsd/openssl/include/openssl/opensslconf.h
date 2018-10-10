c822cfe197bc759dabc0aca800a0bcb5898d0ccd
martin <martin@NetBSD.org>
2018-10-10 06:16:55 +0000

    On 32bit platforms set OPENSSL_NO_EC_NISTP_64_GCC_128 again, the non standard __uint128_t is required for the code, but only provided by gcc on 64bit archs.

--- crypto/external/bsd/openssl/include/openssl/opensslconf.h
+++ crypto/external/bsd/openssl/include/openssl/opensslconf.h
@@ -48,6 +48,11 @@ extern "C" {
 #ifndef OPENSSL_NO_DEVCRYPTOENG
 # define OPENSSL_NO_DEVCRYPTOENG
 #endif
+#ifndef _LP64
+# ifndef OPENSSL_NO_EC_NISTP_64_GCC_128
+#  define OPENSSL_NO_EC_NISTP_64_GCC_128
+# endif
+#endif
 #ifndef OPENSSL_NO_EGD
 # define OPENSSL_NO_EGD
 #endif
