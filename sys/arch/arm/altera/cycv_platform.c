f91f4a4e30f0550cb61fc32a7a0fa755485fa8de
aymeric <aymeric@NetBSD.org>
2018-10-14 18:58:44 +0000

    Make the DE0 NanoSoC work also in big-endian mode

--- sys/arch/arm/altera/cycv_platform.c
+++ sys/arch/arm/altera/cycv_platform.c
@@ -1,4 +1,4 @@
-/* $NetBSD: cycv_platform.c,v 1.1 2018/09/19 17:31:38 aymeric Exp $ */
+/* $NetBSD: cycv_platform.c,v 1.2 2018/10/14 18:58:44 aymeric Exp $ */
 
 /* This file is in the public domain. */
 
@@ -6,7 +6,7 @@
 #include "opt_multiprocessor.h"
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: cycv_platform.c,v 1.1 2018/09/19 17:31:38 aymeric Exp $");
+__KERNEL_RCSID(0, "$NetBSD: cycv_platform.c,v 1.2 2018/10/14 18:58:44 aymeric Exp $");
 
 #define	_ARM32_BUS_DMA_PRIVATE
 #include <sys/param.h>
@@ -71,7 +71,7 @@ cycv_platform_bootstrap(void) {
 	 * stored very low in RAM so we can't re-map the Boot ROM easily.
 	 */
 
-	*(volatile uint32_t *) 0x0 = 0xea000000 | ((startfunc - 8 - 0x0) >> 2);
+	*(volatile uint32_t *) 0x0 = htole32(0xea000000 | ((startfunc - 8 - 0x0) >> 2));
 
 	arm_cpu_max = 2;
 
@@ -97,10 +97,10 @@ cycv_platform_early_putchar(char c) {
 #define CONSADDR_VA (CONSADDR - CYCV_PERIPHERAL_BASE + CYCV_PERIPHERAL_VBASE)
 	volatile uint32_t *uartaddr = (volatile uint32_t *) CONSADDR_VA;
 
-	while ((uartaddr[com_lsr] & LSR_TXRDY) == 0)
+	while ((le32toh(uartaddr[com_lsr]) & LSR_TXRDY) == 0)
 		;
 
-	uartaddr[com_data] = c;
+	uartaddr[com_data] = htole32(c);
 #endif
 }
 
