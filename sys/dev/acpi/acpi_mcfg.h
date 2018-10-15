7970a7c95d8caa841334016f93685caba1eacd49
jmcneill <jmcneill@NetBSD.org>
2018-10-15 10:01:32 +0000

    Add acpimcfg_configure_bus, which can be used by MD code to implement PCI_NETBSD_CONFIGURE using ACPI MCFG tables.

--- sys/dev/acpi/acpi_mcfg.h
+++ sys/dev/acpi/acpi_mcfg.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: acpi_mcfg.h,v 1.1 2015/10/02 05:22:52 msaitoh Exp $	*/
+/*	$NetBSD: acpi_mcfg.h,v 1.2 2018/10/15 10:01:32 jmcneill Exp $	*/
 
 /*-
  * Copyright (C) 2015 NONAKA Kimihiro <nonaka@NetBSD.org>
@@ -32,6 +32,7 @@ struct acpimcfg_ops;
 void	acpimcfg_probe(struct acpi_softc *);
 int	acpimcfg_init(bus_space_tag_t, const struct acpimcfg_ops *);
 int	acpimcfg_map_bus(device_t, pci_chipset_tag_t, int);
+int	acpimcfg_configure_bus(device_t, pci_chipset_tag_t, ACPI_HANDLE, int, int);
 
 int	acpimcfg_conf_read(pci_chipset_tag_t, pcitag_t, int, pcireg_t *);
 int	acpimcfg_conf_write(pci_chipset_tag_t, pcitag_t, int, pcireg_t);
