518779a50bbb74b2ef7f09dddd38bd76a512efd7
jmcneill <jmcneill@NetBSD.org>
2018-10-16 10:25:33 +0000

    Rename options ACPI_REDUCED_HARDWARE to ACPI_REDUCED_HW. The former is a boolean in the acpica build, so use the latter to select the correct value.

--- sys/external/bsd/acpica/dist/include/platform/acnetbsd.h
+++ sys/external/bsd/acpica/dist/include/platform/acnetbsd.h
@@ -1,7 +1,7 @@
 /******************************************************************************
  *
  * Name: acnetbsd.h - OS specific defines, etc.
- *       $Revision: 1.17 $
+ *       $Revision: 1.18 $
  *
  *****************************************************************************/
 
@@ -113,6 +113,10 @@
 #define ACPI_DEBUGGER
 #endif /* DDB */
 #endif /* ACPI_DEBUG */
+
+#ifdef ACPI_REDUCED_HW
+#define ACPI_REDUCED_HARDWARE TRUE
+#endif /* ACPI_REDUCED_HW */
 
 #else /* defined(_KERNEL) || defined(_STANDALONE) */
 
