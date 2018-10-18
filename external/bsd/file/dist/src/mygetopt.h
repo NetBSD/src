4d19fed532a60c35f3739b62b4531c2366ac3103
christos <christos@NetBSD.org>
2018-10-18 23:54:09 +0000

    2018-10-18  19:32  Christos Zoulas <christos@zoulas.com>
    	* release 5.35
    
    2018-09-10  20:38  Christos Zoulas <christos@zoulas.com>
    
    	* Add FreeBSD ELF core file support (John Baldwin)
    
    2018-08-20  18:40  Christos Zoulas <christos@zoulas.com>
    
    	* PR/30: Allow all parameter values to be set (don't treat 0 specially)
    	* handle default annotations on the softmagic match instead at the
    	  end.
    
    2018-07-25  10:17  Christos Zoulas <christos@zoulas.com>
    
    	* PR/23: Recognize JSON files
    
    2018-07-25  10:17  Christos Zoulas <christos@zoulas.com>
    
    	* PR/18: file --mime-encoding should not print mime-type
    
    2018-07-25   8:50  Christos Zoulas <christos@zoulas.com>
    
    	* release 5.34
    
    2018-06-22  16:38  Christos Zoulas <christos@zoulas.com>
    
    	* Add Quad indirect offsets
    
    2018-05-24  14:10  Christos Zoulas <christos@zoulas.com>
    
    	* Enable parsing of ELF dynamic sections to handle PIE better

--- external/bsd/file/dist/src/mygetopt.h
+++ external/bsd/file/dist/src/mygetopt.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: mygetopt.h,v 1.1.1.4 2015/01/02 20:34:27 christos Exp $	*/
+/*	$NetBSD: mygetopt.h,v 1.1.1.5 2018/10/18 23:54:09 christos Exp $	*/
 
 /*	NetBSD: getopt.h,v 1.8 2007/11/06 19:21:18 christos Exp 	*/
 
@@ -66,5 +66,5 @@ struct option {
 
 int getopt_long(int, char * const *, const char *,
     const struct option *, int *);
- 
+
 #endif /* !_GETOPT_H_ */
