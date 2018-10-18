bf7d766c8db3f9233fbd40a62a4d4c8703849968
kre <kre@NetBSD.org>
2018-10-18 05:28:45 +0000

    Delete the old style (no longer used) DEBUG mode TRACE compat macro definitions (just to avoid any temptation to ever use them again).
    Update a comment which would make no sense without following the
    preceding comment which is being deleted with the macros it describes.
    
    While here, remove another comment that referred to events that have
    long past as if they were still to come.   Also a grammatical comment
    correction - paragraphs start with capital letters...
    
    NFC (even with DEBUG defined).

--- bin/sh/shell.h
+++ bin/sh/shell.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: shell.h,v 1.26 2018/07/22 20:38:06 kre Exp $	*/
+/*	$NetBSD: shell.h,v 1.27 2018/10/18 05:28:45 kre Exp $	*/
 
 /*-
  * Copyright (c) 1991, 1993
@@ -101,21 +101,10 @@ extern	int		ShNest;
 #define	DBG_EXTRAS(n)	((DBG_VBOSE_SHIFT * 2) + (n))
 
 /*
- * Unconditional tracing for compatibility with old tracing setup.
- */
-#define TRACE(param)		do {					\
-					trace param;			\
-				} while (/*CONSTCOND*/ 0)
-#define TRACEV(param)		do {					\
-					tracev param;			\
-				} while (/*CONSTCOND*/ 0)
-
-/*
- * and the newer conditional tracing, so the mainainer can control
+ * Macros to enable tracing, so the mainainer can control
  * just how much debug output is dumped to the trace file
- * (once the rest of the shell is converted to use it).
  *
- * in the X forms, "xtra" can be any legal C statement(s) without (bare) commas
+ * In the X forms, "xtra" can be any legal C statement(s) without (bare) commas
  * executed if the relevant debug flag is enabled, after any tracing output.
  */
 #define CTRACE(when, param)	do {					\
