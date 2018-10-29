8ef65d02758a0c2fff4719f65a218ab0dfbe45b4
christos <christos@NetBSD.org>
2018-10-29 22:02:25 +0000

    Zero out the ntptimeval structure to prevent a 4 byte kernel stack disclosure. Reported by Thomas Barabosch.

--- sys/kern/kern_ntptime.c
+++ sys/kern/kern_ntptime.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: kern_ntptime.c,v 1.59 2018/07/01 15:12:06 riastradh Exp $	*/
+/*	$NetBSD: kern_ntptime.c,v 1.60 2018/10/29 22:02:25 christos Exp $	*/
 
 /*-
  * Copyright (c) 2008 The NetBSD Foundation, Inc.
@@ -60,7 +60,7 @@
 
 #include <sys/cdefs.h>
 /* __FBSDID("$FreeBSD: src/sys/kern/kern_ntptime.c,v 1.59 2005/05/28 14:34:41 rwatson Exp $"); */
-__KERNEL_RCSID(0, "$NetBSD: kern_ntptime.c,v 1.59 2018/07/01 15:12:06 riastradh Exp $");
+__KERNEL_RCSID(0, "$NetBSD: kern_ntptime.c,v 1.60 2018/10/29 22:02:25 christos Exp $");
 
 #ifdef _KERNEL_OPT
 #include "opt_ntp.h"
@@ -239,6 +239,7 @@ static void hardupdate(long offset);
 void
 ntp_gettime(struct ntptimeval *ntv)
 {
+	memset(ntv, 0, sizeof(*ntv));
 
 	mutex_spin_enter(&timecounter_lock);
 	nanotime(&ntv->time);
