95db68a71d433d66a5dd0e32fa2df7babd430062
christos <christos@NetBSD.org>
2018-10-30 20:15:57 +0000

    provide a main prototype (this is standalone)

--- sys/arch/acorn32/stand/boot32/boot32.c
+++ sys/arch/acorn32/stand/boot32/boot32.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: boot32.c,v 1.43 2018/01/24 09:04:44 skrll Exp $	*/
+/*	$NetBSD: boot32.c,v 1.44 2018/10/30 20:15:57 christos Exp $	*/
 
 /*-
  * Copyright (c) 2002 Reinoud Zandijk
@@ -760,6 +760,7 @@ create_configuration(int argc, char **argv, int start_args)
 	}
 }
 
+int main(int, char **argv);
 
 int
 main(int argc, char **argv)
