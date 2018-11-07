5fdd8e5f4d6cda6206cf572e2e30c345043ac26a
maxv <maxv@NetBSD.org>
2018-11-07 07:43:07 +0000

    Add NVMM - for NetBSD Virtual Machine Monitor -, a kernel driver that provides support for hardware-accelerated virtualization on NetBSD.
    It is made of an MI frontend, to which MD backends can be plugged. One
    MD backend is implemented, x86-SVM, for x86 AMD CPUs.
    
    We install
    
    	/usr/include/dev/nvmm/nvmm.h
    	/usr/include/dev/nvmm/nvmm_ioctl.h
    	/usr/include/dev/nvmm/{arch}/nvmm_{arch}.h
    
    And the kernel module. For now, the only architecture where we do that
    is amd64 (arch=x86).
    
    NVMM is not enabled by default in amd64-GENERIC, but is instead easily
    modloadable.
    
    Sent to tech-kern@ a month ago. Validated with kASan, and optimized
    with tprof.

---
+++ sys/dev/nvmm/x86/nvmm_x86.h
@@ -0,0 +1,172 @@
+/*	$NetBSD: nvmm_x86.h,v 1.1 2018/11/07 07:43:08 maxv Exp $	*/
+
+/*
+ * Copyright (c) 2018 The NetBSD Foundation, Inc.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to The NetBSD Foundation
+ * by Maxime Villard.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef _NVMM_X86_H_
+#define _NVMM_X86_H_
+
+/* Segments. */
+#define NVMM_X64_SEG_CS			0
+#define NVMM_X64_SEG_DS			1
+#define NVMM_X64_SEG_ES			2
+#define NVMM_X64_SEG_FS			3
+#define NVMM_X64_SEG_GS			4
+#define NVMM_X64_SEG_SS			5
+#define NVMM_X64_SEG_GDT		6
+#define NVMM_X64_SEG_IDT		7
+#define NVMM_X64_SEG_LDT		8
+#define NVMM_X64_SEG_TR			9
+#define NVMM_X64_NSEG			10
+
+/* General Purpose Registers. */
+#define NVMM_X64_GPR_RAX		0
+#define NVMM_X64_GPR_RBX		1
+#define NVMM_X64_GPR_RCX		2
+#define NVMM_X64_GPR_RDX		3
+#define NVMM_X64_GPR_R8			4
+#define NVMM_X64_GPR_R9			5
+#define NVMM_X64_GPR_R10		6
+#define NVMM_X64_GPR_R11		7
+#define NVMM_X64_GPR_R12		8
+#define NVMM_X64_GPR_R13		9
+#define NVMM_X64_GPR_R14		10
+#define NVMM_X64_GPR_R15		11
+#define NVMM_X64_GPR_RDI		12
+#define NVMM_X64_GPR_RSI		13
+#define NVMM_X64_GPR_RBP		14
+#define NVMM_X64_GPR_RSP		15
+#define NVMM_X64_GPR_RIP		16
+#define NVMM_X64_GPR_RFLAGS		17
+#define NVMM_X64_NGPR			18
+
+/* Control Registers. */
+#define NVMM_X64_CR_CR0			0
+#define NVMM_X64_CR_CR2			1
+#define NVMM_X64_CR_CR3			2
+#define NVMM_X64_CR_CR4			3
+#define NVMM_X64_CR_CR8			4
+#define NVMM_X64_CR_XCR0		5
+#define NVMM_X64_NCR			6
+
+/* Debug Registers. */
+#define NVMM_X64_DR_DR0			0
+#define NVMM_X64_DR_DR1			1
+#define NVMM_X64_DR_DR2			2
+#define NVMM_X64_DR_DR3			3
+#define NVMM_X64_DR_DR6			4
+#define NVMM_X64_DR_DR7			5
+#define NVMM_X64_NDR			6
+
+/* MSRs. */
+#define NVMM_X64_MSR_EFER		0
+#define NVMM_X64_MSR_STAR		1
+#define NVMM_X64_MSR_LSTAR		2
+#define NVMM_X64_MSR_CSTAR		3
+#define NVMM_X64_MSR_SFMASK		4
+#define NVMM_X64_MSR_KERNELGSBASE	5
+#define NVMM_X64_MSR_SYSENTER_CS	6
+#define NVMM_X64_MSR_SYSENTER_ESP	7
+#define NVMM_X64_MSR_SYSENTER_EIP	8
+#define NVMM_X64_MSR_PAT		9
+#define NVMM_X64_NMSR			10
+
+/* Misc. */
+#define NVMM_X64_MISC_CPL		0
+#define NVMM_X64_NMISC			1
+
+#ifndef ASM_NVMM
+
+#include <sys/types.h>
+#include <x86/cpu_extended_state.h>
+
+struct nvmm_x64_state_seg {
+	uint64_t selector;
+	struct {		/* hidden */
+		uint64_t type:5;
+		uint64_t dpl:2;
+		uint64_t p:1;
+		uint64_t avl:1;
+		uint64_t lng:1;
+		uint64_t def32:1;
+		uint64_t gran:1;
+		uint64_t rsvd:52;
+	} attrib;
+	uint64_t limit;		/* hidden */
+	uint64_t base;		/* hidden */
+};
+
+/* VM exit state indexes. */
+#define NVMM_X64_EXITSTATE_CR8	0
+
+/* Flags. */
+#define NVMM_X64_STATE_SEGS	0x01
+#define NVMM_X64_STATE_GPRS	0x02
+#define NVMM_X64_STATE_CRS	0x04
+#define NVMM_X64_STATE_DRS	0x08
+#define NVMM_X64_STATE_MSRS	0x10
+#define NVMM_X64_STATE_MISC	0x20
+#define NVMM_X64_STATE_FPU	0x40
+#define NVMM_X64_STATE_ALL	\
+	(NVMM_X64_STATE_SEGS | NVMM_X64_STATE_GPRS | NVMM_X64_STATE_CRS | \
+	 NVMM_X64_STATE_DRS | NVMM_X64_STATE_MSRS | NVMM_X64_STATE_MISC | \
+	 NVMM_X64_STATE_FPU)
+
+struct nvmm_x64_state {
+	struct nvmm_x64_state_seg segs[NVMM_X64_NSEG];
+	uint64_t gprs[NVMM_X64_NGPR];
+	uint64_t crs[NVMM_X64_NCR];
+	uint64_t drs[NVMM_X64_NDR];
+	uint64_t msrs[NVMM_X64_NMSR];
+	uint64_t misc[NVMM_X64_NMISC];
+	struct fxsave fpu;
+};
+
+#define NVMM_X86_CONF_CPUID	0
+#define NVMM_X86_NCONF		1
+
+struct nvmm_x86_conf_cpuid {
+	uint32_t leaf;
+	struct {
+		uint32_t eax;
+		uint32_t ebx;
+		uint32_t ecx;
+		uint32_t edx;
+	} set;
+	struct {
+		uint32_t eax;
+		uint32_t ebx;
+		uint32_t ecx;
+		uint32_t edx;
+	} del;
+};
+
+#endif /* ASM_NVMM */
+
+#endif /* _NVMM_X86_H_ */
