771909024e4ab363d4ef7f30cfd399ce7cb68050
riastradh <riastradh@NetBSD.org>
2018-11-08 16:29:50 +0000

    Try speling it rite, riasthradhdadhaa or whatever your name is.

--- lib/libc/arch/hppa/gen/fabs.c
+++ lib/libc/arch/hppa/gen/fabs.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: fabs.c,v 1.5 2018/11/07 22:21:42 riastradh Exp $	*/
+/*	$NetBSD: fabs.c,v 1.6 2018/11/08 16:29:50 riastradh Exp $	*/
 
 /*	$OpenBSD: fabs.c,v 1.3 2002/10/21 18:41:05 mickey Exp $	*/
 
@@ -11,7 +11,7 @@
 #include <math.h>
 
 #ifndef __HAVE_LONG_DOUBLE
-__strong_alias(fasbl, fabs)
+__strong_alias(fabsl, fabs)
 #endif
 
 double
