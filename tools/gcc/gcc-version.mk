5dda4a0b4549739bd3a6b820d602f50912b70c8d
mrg <mrg@NetBSD.org>
2018-11-11 23:05:25 +0000

    our GCC 6.5 is "nb4 20181109".

--- tools/gcc/gcc-version.mk
+++ tools/gcc/gcc-version.mk
@@ -1,9 +1,9 @@
-#	$NetBSD: gcc-version.mk,v 1.11 2018/09/05 05:03:28 mrg Exp $
+#	$NetBSD: gcc-version.mk,v 1.12 2018/11/11 23:05:25 mrg Exp $
 
 # common location for tools and native build
 
 .if ${HAVE_GCC} == 6
-NETBSD_GCC_VERSION=nb3 20180905
+NETBSD_GCC_VERSION=nb4 20181109
 .elif ${HAVE_GCC} == 7
 NETBSD_GCC_VERSION=nb1 20180905
 .endif
