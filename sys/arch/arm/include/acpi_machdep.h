3e0d40bd5d23f9c1ba6dea971fab7b50e5ade535
jmcneill <jmcneill@NetBSD.org>
2018-11-16 23:03:55 +0000

    Add MD functions for establishing and disestablishing interrupt handlers.

--- sys/arch/arm/include/acpi_machdep.h
+++ sys/arch/arm/include/acpi_machdep.h
@@ -1,4 +1,4 @@
-/* $NetBSD: acpi_machdep.h,v 1.1 2018/10/12 22:12:12 jmcneill Exp $ */
+/* $NetBSD: acpi_machdep.h,v 1.2 2018/11/16 23:03:55 jmcneill Exp $ */
 
 /*-
  * Copyright (c) 2018 The NetBSD Foundation, Inc.
@@ -46,6 +46,9 @@ BOOLEAN			acpi_md_OsReadable(void *, UINT32);
 BOOLEAN			acpi_md_OsWritable(void *, UINT32);
 void			acpi_md_OsEnableInterrupt(void);
 void			acpi_md_OsDisableInterrupt(void);
+void *			acpi_md_intr_establish(uint32_t, int, int, int (*)(void *),
+					       void *, bool, const char *);
+void			acpi_md_intr_disestablish(void *);
 int			acpi_md_sleep(int);
 uint32_t		acpi_md_pdc(void);
 uint32_t		acpi_md_ncpus(void);
