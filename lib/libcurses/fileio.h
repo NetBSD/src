a887f4aff96f7ec2080cf719fbde24b384b269e2
blymn <blymn@NetBSD.org>
2018-11-16 10:12:00 +0000

    Fix for PR lib/52063 Many thanks to  Onno van der Linden (o.vd.linden@quicknet.nl) for providing the bulk of the patch that fixes the issue.

--- lib/libcurses/fileio.h
+++ lib/libcurses/fileio.h
@@ -1,8 +1,8 @@
 /*
  * Do not edit!  Automatically generated file:
- *   from: NetBSD: shlib_version,v 1.41 2015/11/22 04:56:00 kamil Exp 
+ *   from: NetBSD: shlib_version,v 1.42 2017/01/02 12:38:16 roy Exp 
  *   by  : NetBSD: genfileioh.awk,v 1.2 2008/05/02 11:13:02 martin Exp 
  */
 
 #define CURSES_LIB_MAJOR 7
-#define CURSES_LIB_MINOR 1
+#define CURSES_LIB_MINOR 2
