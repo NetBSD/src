c7bf5d7d6d5d6a8858a5ffc3549e6eb104743fa6
kre <kre@NetBSD.org>
2018-11-16 17:18:29 +0000

    Belated Condolonces on the loss of your m_devget() copy argument. In sympathy we offer you a welcome to 8.99.26

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.570 2018/09/03 16:55:17 riastradh Exp $	*/
+/*	$NetBSD: param.h,v 1.571 2018/11/16 17:18:29 kre Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -67,7 +67,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	899002500	/* NetBSD 8.99.25 */
+#define	__NetBSD_Version__	899002600	/* NetBSD 8.99.26 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
