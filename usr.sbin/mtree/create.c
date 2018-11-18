5da90434629730e057d816349de276ec669225ee
sevan <sevan@NetBSD.org>
2018-11-18 23:03:36 +0000

    Adjust test, we only want to return the structure member if the result is not NULL.
    Heads up by Sascha Wildner @ DragonFly BSD.

--- usr.sbin/mtree/create.c
+++ usr.sbin/mtree/create.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: create.c,v 1.75 2017/12/31 03:04:44 christos Exp $	*/
+/*	$NetBSD: create.c,v 1.76 2018/11/18 23:03:36 sevan Exp $	*/
 
 /*-
  * Copyright (c) 1989, 1993
@@ -38,7 +38,7 @@
 #if 0
 static char sccsid[] = "@(#)create.c	8.1 (Berkeley) 6/6/93";
 #else
-__RCSID("$NetBSD: create.c,v 1.75 2017/12/31 03:04:44 christos Exp $");
+__RCSID("$NetBSD: create.c,v 1.76 2018/11/18 23:03:36 sevan Exp $");
 #endif
 #endif /* not lint */
 
@@ -117,7 +117,7 @@ cwalk(FILE *fp)
 	host[sizeof(host) - 1] = '\0';
 	if ((user = getlogin()) == NULL) {
 		struct passwd *pw;
-		user = (pw = getpwuid(getuid())) == NULL ? pw->pw_name :
+		user = (pw = getpwuid(getuid())) != NULL ? pw->pw_name :
 		    "<unknown>";
 	}
 
