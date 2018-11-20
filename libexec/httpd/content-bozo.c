7e9f11f5486f8d8f0d2a138844f199bc1da4f5d9
mrg <mrg@NetBSD.org>
2018-11-20 01:06:46 +0000

    from CHANGES:
    o  reduce default timeouts, and add expand timeouts to handle the
       initial line, each header, and the total time spent
    o  add -T option to expose new timeout settings
    o  minor RFC fixes related to timeout handling responses
    
    old timeouts:
    60 seconds for initial request like, 60 seconds per header line,
    and no whole timeout (though the recent total header size changes
    do introduce one that would be about 11 hours.)
    new timeouts:
    30 seconds for initial request like, 10 seconds per header line,
    and a total request time of 600 seconds.
    
    
    the new global timeout is implemented using CLOCK_MONOTONIC, with
    a fallback to CLOCK_REALTIME if monotonic time is unavailable.
    
    
    reject multiple Host: headers.  besides being protocol standard,
    this closes one additional memory leak found by JP.  add a simple
    test to check this.
    
    
    clean up option and usage handling some.

--- libexec/httpd/content-bozo.c
+++ libexec/httpd/content-bozo.c
@@ -1,9 +1,9 @@
-/*	$NetBSD: content-bozo.c,v 1.14 2016/07/19 09:27:40 shm Exp $	*/
+/*	$NetBSD: content-bozo.c,v 1.15 2018/11/20 01:06:46 mrg Exp $	*/
 
 /*	$eterna: content-bozo.c,v 1.17 2011/11/18 09:21:15 mrg Exp $	*/
 
 /*
- * Copyright (c) 1997-2015 Matthew R. Green
+ * Copyright (c) 1997-2018 Matthew R. Green
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
