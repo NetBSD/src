fb4d7c7378be14962a24dd99d6236bf184ee9c15
kre <kre@NetBSD.org>
2018-12-05 22:25:38 +0000

    evert previous, linux build problem confirmed fixed by update to mkinit.sh (to 1.10).
    Or more correctly, revert & fix - turns out that there was an off by one
    (failure to adjust for other changes -- in a value printed by debug mode
    trace output).
    
    NFC.

--- bin/sh/trap.c
+++ bin/sh/trap.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: trap.c,v 1.48 2018/12/05 03:14:28 kre Exp $	*/
+/*	$NetBSD: trap.c,v 1.49 2018/12/05 22:25:38 kre Exp $	*/
 
 /*-
  * Copyright (c) 1991, 1993
@@ -37,7 +37,7 @@
 #if 0
 static char sccsid[] = "@(#)trap.c	8.5 (Berkeley) 6/5/95";
 #else
-__RCSID("$NetBSD: trap.c,v 1.48 2018/12/05 03:14:28 kre Exp $");
+__RCSID("$NetBSD: trap.c,v 1.49 2018/12/05 22:25:38 kre Exp $");
 #endif
 #endif /* not lint */
 
@@ -571,6 +571,8 @@ SHELLPROC {
 	for (sm = sigmode ; sm < sigmode + NSIG ; sm++) {
 		if (*sm == S_IGN) {
 			*sm = S_HARD_IGN;
+			VTRACE(DBG_TRAP, ("SHELLPROC: %d -> hard_ign\n",
+			    (sm - sigmode)));
 		}
 	}
 	INTON;
