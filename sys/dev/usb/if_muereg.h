4d35b57ee6bbd0f811f2d75e4371de00d24339ff
rin <rin@NetBSD.org>
2018-12-11 13:35:02 +0000

    Determine the frame size as a function of MTU.
    XXX Jumbo frame is not yet supported.

--- sys/dev/usb/if_muereg.h
+++ sys/dev/usb/if_muereg.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: if_muereg.h,v 1.3 2018/09/16 01:23:09 rin Exp $	*/
+/*	$NetBSD: if_muereg.h,v 1.4 2018/12/11 13:35:02 rin Exp $	*/
 /*	$OpenBSD: if_muereg.h,v 1.1 2018/08/03 01:50:15 kevlo Exp $	*/
 
 /*
@@ -122,11 +122,11 @@
 #define MUE_7800_RX_BUFSIZE		(12 * 1024)
 #define MUE_7800_MAX_RX_FIFO_SIZE	MUE_7800_RX_BUFSIZE
 #define MUE_7800_MAX_TX_FIFO_SIZE	MUE_7800_RX_BUFSIZE
-#define MUE_MAX_TX_LEN			(ETHER_MAX_LEN + ETHER_VLAN_ENCAP_LEN)
-#define MUE_MAX_TSO_LEN			\
-	(ETHER_HDR_LEN + ETHER_VLAN_ENCAP_LEN + IP_MAXPACKET)
+#define MUE_FRAME_LEN(mtu)		\
+	(mtu + ETHER_HDR_LEN + ETHER_CRC_LEN + ETHER_VLAN_ENCAP_LEN)
+#define MUE_TSO_FRAME_LEN		MUE_FRAME_LEN(IP_MAXPACKET)
 #define MUE_TX_BUFSIZE			\
-	(sizeof(struct mue_txbuf_hdr) + MUE_MAX_TSO_LEN)
+	(sizeof(struct mue_txbuf_hdr) + MUE_TSO_FRAME_LEN)
 
 /* interrupt endpoint control register */
 #define MUE_INT_EP_CTL_PHY_INT		0x20000
