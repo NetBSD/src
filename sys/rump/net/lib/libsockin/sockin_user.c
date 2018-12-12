a57097d054360e98c1663784f42b0697fa32bfd6
alnsn <alnsn@NetBSD.org>
2018-12-12 00:48:43 +0000

    Add missing RCSIDs.

--- sys/rump/net/lib/libsockin/sockin_user.c
+++ sys/rump/net/lib/libsockin/sockin_user.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: sockin_user.c,v 1.1 2014/03/13 01:40:30 pooka Exp $	*/
+/*	$NetBSD: sockin_user.c,v 1.2 2018/12/12 00:48:44 alnsn Exp $	*/
 
 /*
  * Copyright (c) 2008 Antti Kantee.  All Rights Reserved.
@@ -24,6 +24,9 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  */
+
+#include <sys/cdefs.h>
+__KERNEL_RCSID(0, "$NetBSD: sockin_user.c,v 1.2 2018/12/12 00:48:44 alnsn Exp $");
 
 /* for struct msghdr content visibility */
 #define _XOPEN_SOURCE 4
--- sys/rump/net/lib/libvirtif/virtif_user.c
+++ sys/rump/net/lib/libvirtif/virtif_user.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: virtif_user.c,v 1.3 2014/03/14 10:06:22 pooka Exp $	*/
+/*	$NetBSD: virtif_user.c,v 1.4 2018/12/12 00:48:44 alnsn Exp $	*/
 
 /*
  * Copyright (c) 2013 Antti Kantee.  All Rights Reserved.
@@ -24,6 +24,9 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  */
+
+#include <sys/cdefs.h>
+__KERNEL_RCSID(0, "$NetBSD: virtif_user.c,v 1.4 2018/12/12 00:48:44 alnsn Exp $");
 
 #ifndef _KERNEL
 #include <sys/types.h>
