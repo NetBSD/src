c81df7efb41c4a32233d45bf8eb3d1572ab93940
maya <maya@NetBSD.org>
2018-12-13 04:29:18 +0000

    Remove unused macros.

--- libexec/telnetd/sys_term.c
+++ libexec/telnetd/sys_term.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: sys_term.c,v 1.47 2013/06/28 15:48:02 christos Exp $	*/
+/*	$NetBSD: sys_term.c,v 1.48 2018/12/13 04:29:18 maya Exp $	*/
 
 /*
  * Copyright (c) 1989, 1993
@@ -34,7 +34,7 @@
 #if 0
 static char sccsid[] = "@(#)sys_term.c	8.4+1 (Berkeley) 5/30/95";
 #else
-__RCSID("$NetBSD: sys_term.c,v 1.47 2013/06/28 15:48:02 christos Exp $");
+__RCSID("$NetBSD: sys_term.c,v 1.48 2018/12/13 04:29:18 maya Exp $");
 #endif
 #endif /* not lint */
 
@@ -50,9 +50,6 @@ __RCSID("$NetBSD: sys_term.c,v 1.47 2013/06/28 15:48:02 christos Exp $");
 #ifdef SUPPORT_UTMPX
 #include <utmpx.h>
 #endif
-
-#define SCPYN(a, b)	(void) strncpy(a, b, sizeof(a))
-#define SCMPN(a, b)	strncmp(a, b, sizeof(a))
 
 struct termios termbuf, termbuf2;	/* pty control structure */
 
