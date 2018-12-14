2bd8bb0f04869900352954440d671233f2b2b4b5
msaitoh <msaitoh@NetBSD.org>
2018-12-14 09:47:40 +0000

    - Fix availability detection of WoL on some chips. This change has no effect other than dmesg because WM_F_WOL is currently not used to change the behavior:   - For ICH/PCH, check the capability not from NVM but from the WUC register.     Check the value before clearing the register.   - 82580 and newer have per-port NVM block, so read the area correctly.     Note that 82580, I350 and I354 may have PCI function 2 and 3.   - Some devices can't detect WoL capability neither from NVM nor from WUC.     Use PCI device ID and the function number. - Print the WUS (WakeUp Status) register bits when resume.

--- sys/dev/pci/if_wmreg.h
+++ sys/dev/pci/if_wmreg.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: if_wmreg.h,v 1.109 2018/11/19 06:38:58 msaitoh Exp $	*/
+/*	$NetBSD: if_wmreg.h,v 1.110 2018/12/14 09:47:40 msaitoh Exp $	*/
 
 /*
  * Copyright (c) 2001 Wasabi Systems, Inc.
@@ -1195,15 +1195,38 @@ struct livengood_tcpip_ctxdesc {
 #define	WMREG_WUC	0x5800	/* Wakeup Control */
 #define	WUC_APME		0x00000001 /* APM Enable */
 #define	WUC_PME_EN		0x00000002 /* PME Enable */
+#define WUC_PME_STATUS		0x00000004 /* PME Status */
+#define WUC_APMPME		0x00000008 /* Assert PME on APM Wakeup */
+#define WUC_PHY_WAKE		0x00000100 /* if PHY supports wakeup */
 
 #define	WMREG_WUFC	0x5808	/* Wakeup Filter Control */
-#define WUFC_MAG		0x00000002 /* Magic Packet Wakeup Enable */
-#define WUFC_EX			0x00000004 /* Directed Exact Wakeup Enable */
-#define WUFC_MC			0x00000008 /* Directed Multicast Wakeup En */
-#define WUFC_BC			0x00000010 /* Broadcast Wakeup Enable */
-#define WUFC_ARP		0x00000020 /* ARP Request Packet Wakeup En */
-#define WUFC_IPV4		0x00000040 /* Directed IPv4 Packet Wakeup En */
-#define WUFC_IPV6		0x00000080 /* Directed IPv6 Packet Wakeup En */
+#define WUFC_LNKC	__BIT(0)	/* Link Status Change Wakeup Enable */
+#define WUFC_MAG	__BIT(1)	/* Magic Packet Wakeup Enable */
+#define WUFC_EX		__BIT(2)	/* Directed Exact Wakeup Enable */
+#define WUFC_MC		__BIT(3)	/* Directed Multicast Wakeup En */
+#define WUFC_BC		__BIT(4)	/* Broadcast Wakeup Enable */
+#define WUFC_ARPDIR	__BIT(5)	/* ARP Request Packet Wakeup En */
+#define WUFC_IPV4	__BIT(6)	/* Directed IPv4 Packet Wakeup En */
+#define WUFC_IPV6	__BIT(7)	/* Directed IPv6 Packet Wakeup En */
+#define WUFC_NS		__BIT(9)	/* NS Wakeup En */
+#define WUFC_NSDIR	__BIT(10)	/* NS Directed En */
+#define WUFC_ARP	__BIT(11)	/* ARP request En */
+#define WUFC_FLEX_HQ	__BIT(14)	/* Flex Filters Host Queueing En */
+#define WUFC_NOTCO	__BIT(15)	/* ? */
+#define WUFC_FLX	__BITS(23, 16)	/* Flexible Filter [0-7] En */
+#define WUFC_FLXACT	__BITS(27, 24)	/* Flexible Filter [0-3] Action */
+#define WUFC_FW_RST_WK	__BIT(31)	/* Wake on Firmware Reset Assert En */
+
+#define	WMREG_WUS	0x5810	/* Wakeup Status (R/W1C) */
+	/* Bit 30-24 and 15-12 are reserved */
+#define WUS_MNG		__BIT(8)	/* Manageability event */
+#define WUS_FLAGS	"\20"						\
+	"\1LINKC"	"\2MAG"		"\3EX"		"\4MC"		\
+	"\5BC"		"\6ARPDIR"	"\7IPV4"	"\10IPV6"	\
+	"\11MNG"	"\12NS"		"\13NSDIR"	"\14ARP"	\
+	"\21FLX0"	"\22FLX1"	"\23FLX2"	"\24FLX3"	\
+	"\25FLX4"	"\26FLX5"	"\27FLX6"	"\30FLX7"	\
+							"\40FW_RST_WK"
 
 #define WMREG_MRQC	0x5818	/* Multiple Receive Queues Command */
 #define MRQC_DISABLE_RSS	0x00000000
