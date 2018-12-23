678c831fb622370c50cdce08cee63f589e80ba30
jakllsch <jakllsch@NetBSD.org>
2018-12-23 20:27:23 +0000

    Restore "defs.h", <sys/cdefs.h> #include ordering from before today's update.  Fixes tools build on, for instance, Linux hosts.

--- external/bsd/byacc/dist/verbose.c
+++ external/bsd/byacc/dist/verbose.c
@@ -1,11 +1,11 @@
-/*	$NetBSD: verbose.c,v 1.11 2018/12/23 15:38:53 christos Exp $	*/
-
-#include <sys/cdefs.h>
-__RCSID("$NetBSD: verbose.c,v 1.11 2018/12/23 15:38:53 christos Exp $");
+/*	$NetBSD: verbose.c,v 1.12 2018/12/23 20:27:23 jakllsch Exp $	*/
 
 /* Id: verbose.c,v 1.12 2016/06/07 00:22:05 tom Exp  */
 
 #include "defs.h"
+
+#include <sys/cdefs.h>
+__RCSID("$NetBSD: verbose.c,v 1.12 2018/12/23 20:27:23 jakllsch Exp $");
 
 static void log_conflicts(void);
 static void log_unused(void);
