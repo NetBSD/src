f54e1208b0b6b5f89c379a191f200e2c8f94aa78
thorpej <thorpej@NetBSD.org>
2018-12-24 16:58:53 +0000

    Add threadpool(9), an abstraction that provides shared pools of kernel threads running at specific priorities, with support for unbound pools and per-cpu pools.
    Written by riastradh@, and based on the May 2014 draft, with a few changes
    by me:
    - Working on the assumption that a relative few priorities will actually
      be used, reduce the memory footprint by using linked lists, rather than
      2 large (and mostly empty) tables.  The performance impact is essentially
      nil, since these lists are consulted only when pools are created (and
      destroyed, for DIAGNOSTIC checks), and the lists will have at most 225
      entries.
    - Make threadpool job object, which the caller must allocate storage for,
      really opaque.
    - Use typedefs for the threadpool types, to reduce the verbosity of the
      API somewhat.
    - Fix a bunch of pool / worker thread / job object lifecycle bugs.
    
    Also include an ATF unit test, written by me, that exercises the basics
    of the API by loading a kernel module that exposes several sysctls that
    allow the ATF test script to create and destroy threadpools, schedule a
    basic job, and verify that it ran.
    
    And thus NetBSD 8.99.29 has arrived.

---
+++ sys/sys/threadpool.h
@@ -0,0 +1,77 @@
+/*	$NetBSD: threadpool.h,v 1.1 2018/12/24 16:58:54 thorpej Exp $	*/
+
+/*-
+ * Copyright (c) 2014 The NetBSD Foundation, Inc.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to The NetBSD Foundation
+ * by Taylor R. Campbell.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef	_SYS_THREADPOOL_H_
+#define	_SYS_THREADPOOL_H_
+
+#if !defined(_KERNEL)
+#error "not supposed to be exposed to userland"
+#endif
+
+#include <sys/types.h>
+#include <sys/param.h>
+#include <sys/mutex.h>
+#include <sys/queue.h>
+
+typedef struct threadpool threadpool_t;
+typedef struct threadpool_percpu threadpool_percpu_t;
+
+typedef struct threadpool_job {
+#ifdef _LP64
+	void	*opaque[11];
+#else
+	void	*opaque[13];
+#endif /* _LP64 */
+} threadpool_job_t;
+
+typedef void (*threadpool_job_fn_t)(threadpool_job_t *);
+
+int	threadpool_get(threadpool_t **, pri_t);
+void	threadpool_put(threadpool_t *, pri_t);
+
+int	threadpool_percpu_get(threadpool_percpu_t **, pri_t);
+void	threadpool_percpu_put(threadpool_percpu_t *, pri_t);
+threadpool_t *
+	threadpool_percpu_ref(threadpool_percpu_t *);
+threadpool_t *
+	threadpool_percpu_ref_remote(threadpool_percpu_t *,
+	    struct cpu_info *);
+
+void	threadpool_job_init(threadpool_job_t *, threadpool_job_fn_t,
+	    kmutex_t *, const char *, ...) __printflike(4,5);
+void	threadpool_job_destroy(threadpool_job_t *);
+void	threadpool_job_done(threadpool_job_t *);
+
+void	threadpool_schedule_job(threadpool_t *, threadpool_job_t *);
+void	threadpool_cancel_job(threadpool_t *, threadpool_job_t *);
+bool	threadpool_cancel_job_async(threadpool_t *, threadpool_job_t *);
+
+#endif	/* _SYS_THREADPOOL_H_ */
