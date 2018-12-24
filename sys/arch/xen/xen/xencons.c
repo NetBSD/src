7f5ff086f6daae55fd598dd0329630c15e6c3a0b
cherry <cherry@NetBSD.org>
2018-12-24 14:55:41 +0000

    Bifurcate the interrupt establish functions between XEN and non-XEN
    Thus intr_establish_xname() becomes xen_intr_establish_xname() etc.
    
    One consequence of this is that dom0 devices expect the native
    function calls to be available and we thus provide weak aliasing for
    dom0 builds to succeed. XEN and non-XEN devices are distinguished by
    the PIC they are established on. XEN interrupts are exclusively
    established on xen_pic, while dom0 interrupts are established on
    natively available PICs.
    
    This allows us an orthogonal path to xen device management (eg:
    xenstore events) in XENPVHVM, without having to worry about unifying
    the vector entry paths, etc., which is quite challenging.

--- sys/arch/xen/xen/xencons.c
+++ sys/arch/xen/xen/xencons.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: xencons.c,v 1.46 2018/10/26 05:33:21 cherry Exp $	*/
+/*	$NetBSD: xencons.c,v 1.47 2018/12/24 14:55:42 cherry Exp $	*/
 
 /*
  * Copyright (c) 2006 Manuel Bouyer.
@@ -53,7 +53,7 @@
 
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: xencons.c,v 1.46 2018/10/26 05:33:21 cherry Exp $");
+__KERNEL_RCSID(0, "$NetBSD: xencons.c,v 1.47 2018/12/24 14:55:42 cherry Exp $");
 
 #include "opt_xen.h"
 
@@ -217,7 +217,7 @@ xencons_suspend(device_t dev, const pmf_qual_t *qual) {
 	if (!xendomain_is_dom0()) {
 		evtch = xen_start_info.console_evtchn;
 		hypervisor_mask_event(evtch);
-		intr_disestablish(ih);
+		xen_intr_disestablish(ih);
 		aprint_verbose_dev(dev, "removed event channel %d\n", ih->ih_pin);
 	}
 
@@ -233,7 +233,7 @@ xencons_resume(device_t dev, const pmf_qual_t *qual) {
 		/* dom0 console resume is required only during first start-up */
 		if (cold) {
 			evtch = bind_virq_to_evtch(VIRQ_CONSOLE);
-			ih = intr_establish_xname(-1, &xen_pic, evtch,
+			ih = xen_intr_establish_xname(-1, &xen_pic, evtch,
 			    IST_LEVEL, IPL_TTY, xencons_intr,
 			    xencons_console_device, false,
 			    device_xname(dev));
@@ -241,7 +241,7 @@ xencons_resume(device_t dev, const pmf_qual_t *qual) {
 		}
 	} else {
 		evtch = xen_start_info.console_evtchn;
-		ih = intr_establish_xname(-1, &xen_pic, evtch,
+		ih = xen_intr_establish_xname(-1, &xen_pic, evtch,
 		    IST_LEVEL, IPL_TTY, xencons_handler,
 		    xencons_console_device, false, device_xname(dev));
 		KASSERT(ih != NULL);
