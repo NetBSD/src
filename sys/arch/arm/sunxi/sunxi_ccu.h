3884b1e35c0c7cd9684b248ba8735a4b5a422dcd
jmcneill <jmcneill@NetBSD.org>
2019-01-02 17:29:58 +0000

    Add support for cluster 0 and 1 CPUX PLLs.

--- sys/arch/arm/sunxi/sunxi_ccu.h
+++ sys/arch/arm/sunxi/sunxi_ccu.h
@@ -1,4 +1,4 @@
-/* $NetBSD: sunxi_ccu.h,v 1.18 2018/05/08 22:05:25 jmcneill Exp $ */
+/* $NetBSD: sunxi_ccu.h,v 1.19 2019/01/02 17:29:58 jmcneill Exp $ */
 
 /*-
  * Copyright (c) 2017 Jared McNeill <jmcneill@invisible.ca>
@@ -113,6 +113,7 @@ struct sunxi_ccu_nkmp {
 #define	SUNXI_CCU_NKMP_SCALE_CLOCK		__BIT(2)
 #define	SUNXI_CCU_NKMP_FACTOR_P_POW2		__BIT(3)
 #define	SUNXI_CCU_NKMP_FACTOR_N_ZERO_IS_ONE	__BIT(4)
+#define	SUNXI_CCU_NKMP_FACTOR_P_X4		__BIT(5)
 };
 
 int	sunxi_ccu_nkmp_enable(struct sunxi_ccu_softc *,
