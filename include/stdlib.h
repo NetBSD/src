8eb0d913be44016c69307c75f5134be519e9579f
maya <maya@NetBSD.org>
2019-01-05 09:16:46 +0000

    We can make a stronger guarantee than __pure. abs/labs don't read global memory.
    Make GCC 9 happier.

--- include/stdlib.h
+++ include/stdlib.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: stdlib.h,v 1.120 2018/08/15 10:21:42 martin Exp $	*/
+/*	$NetBSD: stdlib.h,v 1.121 2019/01/05 09:16:46 maya Exp $	*/
 
 /*-
  * Copyright (c) 1990, 1993
@@ -95,7 +95,7 @@ extern size_t __mb_cur_max;
 __BEGIN_DECLS
 __dead	 void _Exit(int);
 __dead	 void abort(void);
-__pure	 int abs(int);
+__constfunc	int abs(int);
 int	 atexit(void (*)(void));
 double	 atof(const char *);
 int	 atoi(const char *);
@@ -111,7 +111,7 @@ div_t	 div(int, int);
 __dead	 void exit(int);
 void	 free(void *);
 __aconst char *getenv(const char *);
-__pure long
+__constfunc long
 	 labs(long);
 ldiv_t	 ldiv(long, long);
 void	*malloc(size_t);
