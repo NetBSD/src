7e29cdb7151e87a11f0e6a3bcbe8db691b76664a
jdolecek <jdolecek@NetBSD.org>
2019-01-08 19:36:57 +0000

    no need for <machine/param.h>

--- sys/arch/mmeye/stand/boot/wd.c
+++ sys/arch/mmeye/stand/boot/wd.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: wd.c,v 1.4 2015/01/02 19:42:06 christos Exp $	*/
+/*	$NetBSD: wd.c,v 1.5 2019/01/08 19:36:57 jdolecek Exp $	*/
 
 /*-
  * Copyright (c) 2003 The NetBSD Foundation, Inc.
@@ -35,8 +35,6 @@
 
 #include <lib/libsa/stand.h>
 #include <lib/libkern/libkern.h>
-
-#include <machine/param.h>
 
 #include "boot.h"
 #include "wdvar.h"
