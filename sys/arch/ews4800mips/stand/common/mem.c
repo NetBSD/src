c5ac3d4ff0cefce7c1958079ee60ddb0efa08894
christos <christos@NetBSD.org>
2019-01-09 03:28:31 +0000

    use <sys/param.h> instead of <machine/param.h> for everything.

--- sys/arch/ews4800mips/stand/common/mem.c
+++ sys/arch/ews4800mips/stand/common/mem.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: mem.c,v 1.5 2008/04/28 20:23:19 martin Exp $	*/
+/*	$NetBSD: mem.c,v 1.6 2019/01/09 03:28:31 christos Exp $	*/
 
 /*-
  * Copyright (c) 2004 The NetBSD Foundation, Inc.
@@ -33,7 +33,7 @@
 #include <lib/libsa/stand.h>
 #include <lib/libkern/libkern.h>
 
-#include <machine/param.h>
+#include <sys/param.h>
 #include <machine/bfs.h>
 
 #include "local.h"
