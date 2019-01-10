c5e6d7a10171d691ba7f3453551c199b64e3dcb8
christos <christos@NetBSD.org>
2019-01-10 13:53:58 +0000

    delete unused variable

--- lib/libwrap/diag.c
+++ lib/libwrap/diag.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: diag.c,v 1.12 2019/01/10 13:53:26 christos Exp $	*/
+/*	$NetBSD: diag.c,v 1.13 2019/01/10 13:53:58 christos Exp $	*/
 
  /*
   * Routines to report various classes of problems. Each report is decorated
@@ -16,7 +16,7 @@
 #if 0
 static char sccsid[] = "@(#) diag.c 1.1 94/12/28 17:42:20";
 #else
-__RCSID("$NetBSD: diag.c,v 1.12 2019/01/10 13:53:26 christos Exp $");
+__RCSID("$NetBSD: diag.c,v 1.13 2019/01/10 13:53:58 christos Exp $");
 #endif
 #endif
 
@@ -58,8 +58,6 @@ tcpd_diag(int severity, const char *tag, const char *fmt, va_list ap)
 
     if (vasprintf(&buf2, buf, ap) == -1)
 	buf2 = buf;
-
-    errno = oerrno;
 
     /* contruct the tag for the log entry */
     if (tcpd_context.file)
