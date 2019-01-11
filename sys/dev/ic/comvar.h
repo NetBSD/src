42e5e5737c7562a85f3f363b6fa00ecda60d985b
thorpej <thorpej@NetBSD.org>
2019-01-11 23:10:40 +0000

    Simplify regmap initialization, and fix an regmap issue that affected TI OMAP (LCR register would get clobbered due to using the wrong offset for the MDR1 register) reported by Lwazi Dube (who also found the root cause).

--- sys/dev/ic/comvar.h
+++ sys/dev/ic/comvar.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: comvar.h,v 1.91 2018/12/11 06:34:00 thorpej Exp $	*/
+/*	$NetBSD: comvar.h,v 1.92 2019/01/11 23:10:41 thorpej Exp $	*/
 
 /*
  * Copyright (c) 1996 Christopher G. Demetriou.  All rights reserved.
@@ -83,25 +83,27 @@ int com_is_console(bus_space_tag_t, bus_addr_t, bus_space_handle_t *);
 #define	COM_REG_IER		4
 #define	COM_REG_IIR		5
 #define	COM_REG_FIFO		6
-#define	COM_REG_TCR		6
-#define	COM_REG_EFR		7
-#define	COM_REG_TLR		7
-#define	COM_REG_LCR		8
-#define	COM_REG_MDR1		8
-#define	COM_REG_MCR		9
-#define	COM_REG_LSR		10
-#define	COM_REG_MSR		11
-#define	COM_REG_USR		31		/* 16750/DW APB */
-#define	COM_REG_TFL		com_tfl		/* DW APB */
-#define	COM_REG_RFL		com_rfl		/* DW APB */
-#define	COM_REG_HALT		com_halt	/* DW APB */
+#define	COM_REG_TCR		7
+#define	COM_REG_EFR		8
+#define	COM_REG_TLR		9
+#define	COM_REG_LCR		10
+#define	COM_REG_MCR		11
+#define	COM_REG_LSR		12
+#define	COM_REG_MSR		13
+#define	COM_REG_MDR1		14		/* TI OMAP */
+#define	COM_REG_USR		15		/* 16750/DW APB */
+#define	COM_REG_TFL		16		/* DW APB */
+#define	COM_REG_RFL		17		/* DW APB */
+#define	COM_REG_HALT		18		/* DW APB */
+
+#define	COM_REGMAP_NENTRIES	19
 
 struct com_regs {
 	bus_space_tag_t		cr_iot;
 	bus_space_handle_t	cr_ioh;
 	bus_addr_t		cr_iobase;
 	bus_size_t		cr_nports;
-	bus_size_t		cr_map[42];
+	bus_size_t		cr_map[COM_REGMAP_NENTRIES];
 };
 
 void	com_init_regs(struct com_regs *, bus_space_tag_t, bus_space_handle_t,
