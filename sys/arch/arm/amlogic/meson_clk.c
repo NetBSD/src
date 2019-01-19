88499cef2cf91e79802cf52cba2f90b3160bcf92
jmcneill <jmcneill@NetBSD.org>
2019-01-19 20:56:03 +0000

    Add support for (FDT-ized) Amlogic Meson8b.

---
+++ sys/arch/arm/amlogic/meson_clk.c
@@ -0,0 +1,369 @@
+/* $NetBSD: meson_clk.c,v 1.1 2019/01/19 20:56:03 jmcneill Exp $ */
+
+/*-
+ * Copyright (c) 2017-2019 Jared McNeill <jmcneill@invisible.ca>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <sys/cdefs.h>
+__KERNEL_RCSID(0, "$NetBSD: meson_clk.c,v 1.1 2019/01/19 20:56:03 jmcneill Exp $");
+
+#include <sys/param.h>
+#include <sys/bus.h>
+#include <sys/cpu.h>
+#include <sys/device.h>
+
+#include <dev/fdt/fdtvar.h>
+
+#include <dev/clk/clk_backend.h>
+
+#include <arm/amlogic/meson_clk.h>
+
+static void *
+meson_clk_reset_acquire(device_t dev, const void *data, size_t len)
+{
+	struct meson_clk_softc * const sc = device_private(dev);
+	struct meson_clk_reset *reset;
+
+	if (len != 4)
+		return NULL;
+
+	const u_int reset_id = be32dec(data);
+
+	if (reset_id >= sc->sc_nresets)
+		return NULL;
+
+	reset = &sc->sc_resets[reset_id];
+	if (reset->mask == 0)
+		return NULL;
+
+	return reset;
+}
+
+static void
+meson_clk_reset_release(device_t dev, void *priv)
+{
+}
+
+static int
+meson_clk_reset_assert(device_t dev, void *priv)
+{
+	struct meson_clk_softc * const sc = device_private(dev);
+	struct meson_clk_reset * const reset = priv;
+
+	const uint32_t val = CLK_READ(sc, reset->reg);
+	CLK_WRITE(sc, reset->reg, val | reset->mask);
+
+	return 0;
+}
+
+static int
+meson_clk_reset_deassert(device_t dev, void *priv)
+{
+	struct meson_clk_softc * const sc = device_private(dev);
+	struct meson_clk_reset * const reset = priv;
+
+	const uint32_t val = CLK_READ(sc, reset->reg);
+	CLK_WRITE(sc, reset->reg, val & ~reset->mask);
+
+	return 0;
+}
+
+static const struct fdtbus_reset_controller_func meson_clk_fdtreset_funcs = {
+	.acquire = meson_clk_reset_acquire,
+	.release = meson_clk_reset_release,
+	.reset_assert = meson_clk_reset_assert,
+	.reset_deassert = meson_clk_reset_deassert,
+};
+
+static struct clk *
+meson_clk_clock_decode(device_t dev, int cc_phandle, const void *data,
+		       size_t len)
+{
+	struct meson_clk_softc * const sc = device_private(dev);
+	struct meson_clk_clk *clk;
+
+	if (len != 4)
+		return NULL;
+
+	const u_int clock_id = be32dec(data);
+	if (clock_id >= sc->sc_nclks)
+		return NULL;
+
+	clk = &sc->sc_clks[clock_id];
+	if (clk->type == MESON_CLK_UNKNOWN)
+		return NULL;
+
+	return &clk->base;
+}
+
+static const struct fdtbus_clock_controller_func meson_clk_fdtclock_funcs = {
+	.decode = meson_clk_clock_decode,
+};
+
+static struct clk *
+meson_clk_clock_get(void *priv, const char *name)
+{
+	struct meson_clk_softc * const sc = priv;
+	struct meson_clk_clk *clk;
+
+	clk = meson_clk_clock_find(sc, name);
+	if (clk == NULL)
+		return NULL;
+
+	return &clk->base;
+}
+
+static void
+meson_clk_clock_put(void *priv, struct clk *clk)
+{
+}
+
+static u_int
+meson_clk_clock_get_rate(void *priv, struct clk *clkp)
+{
+	struct meson_clk_softc * const sc = priv;
+	struct meson_clk_clk *clk = (struct meson_clk_clk *)clkp;
+	struct clk *clkp_parent;
+
+	if (clk->get_rate)
+		return clk->get_rate(sc, clk);
+
+	clkp_parent = clk_get_parent(clkp);
+	if (clkp_parent == NULL) {
+		aprint_error("%s: no parent for %s\n", __func__, clk->base.name);
+		return 0;
+	}
+
+	return clk_get_rate(clkp_parent);
+}
+
+static int
+meson_clk_clock_set_rate(void *priv, struct clk *clkp, u_int rate)
+{
+	struct meson_clk_softc * const sc = priv;
+	struct meson_clk_clk *clk = (struct meson_clk_clk *)clkp;
+	struct clk *clkp_parent;
+
+	if (clkp->flags & CLK_SET_RATE_PARENT) {
+		clkp_parent = clk_get_parent(clkp);
+		if (clkp_parent == NULL) {
+			aprint_error("%s: no parent for %s\n", __func__, clk->base.name);
+			return ENXIO;
+		}
+		return clk_set_rate(clkp_parent, rate);
+	}
+
+	if (clk->set_rate)
+		return clk->set_rate(sc, clk, rate);
+
+	return ENXIO;
+}
+
+static u_int
+meson_clk_clock_round_rate(void *priv, struct clk *clkp, u_int rate)
+{
+	struct meson_clk_softc * const sc = priv;
+	struct meson_clk_clk *clk = (struct meson_clk_clk *)clkp;
+	struct clk *clkp_parent;
+
+	if (clkp->flags & CLK_SET_RATE_PARENT) {
+		clkp_parent = clk_get_parent(clkp);
+		if (clkp_parent == NULL) {
+			aprint_error("%s: no parent for %s\n", __func__, clk->base.name);
+			return 0;
+		}
+		return clk_round_rate(clkp_parent, rate);
+	}
+
+	if (clk->round_rate)
+		return clk->round_rate(sc, clk, rate);
+
+	return 0;
+}
+
+static int
+meson_clk_clock_enable(void *priv, struct clk *clkp)
+{
+	struct meson_clk_softc * const sc = priv;
+	struct meson_clk_clk *clk = (struct meson_clk_clk *)clkp;
+	struct clk *clkp_parent;
+	int error = 0;
+
+	clkp_parent = clk_get_parent(clkp);
+	if (clkp_parent != NULL) {
+		error = clk_enable(clkp_parent);
+		if (error != 0)
+			return error;
+	}
+
+	if (clk->enable)
+		error = clk->enable(sc, clk, 1);
+
+	return error;
+}
+
+static int
+meson_clk_clock_disable(void *priv, struct clk *clkp)
+{
+	struct meson_clk_softc * const sc = priv;
+	struct meson_clk_clk *clk = (struct meson_clk_clk *)clkp;
+	int error = EINVAL;
+
+	if (clk->enable)
+		error = clk->enable(sc, clk, 0);
+
+	return error;
+}
+
+static int
+meson_clk_clock_set_parent(void *priv, struct clk *clkp,
+    struct clk *clkp_parent)
+{
+	struct meson_clk_softc * const sc = priv;
+	struct meson_clk_clk *clk = (struct meson_clk_clk *)clkp;
+
+	if (clk->set_parent == NULL)
+		return EINVAL;
+
+	return clk->set_parent(sc, clk, clkp_parent->name);
+}
+
+static struct clk *
+meson_clk_clock_get_parent(void *priv, struct clk *clkp)
+{
+	struct meson_clk_softc * const sc = priv;
+	struct meson_clk_clk *clk = (struct meson_clk_clk *)clkp;
+	struct meson_clk_clk *clk_parent;
+	const char *parent;
+
+	if (clk->get_parent == NULL)
+		return NULL;
+
+	parent = clk->get_parent(sc, clk);
+	if (parent == NULL)
+		return NULL;
+
+	clk_parent = meson_clk_clock_find(sc, parent);
+	if (clk_parent != NULL)
+		return &clk_parent->base;
+
+	/* No parent in this domain, try FDT */
+	return fdtbus_clock_get(sc->sc_phandle, parent);
+}
+
+static const struct clk_funcs meson_clk_clock_funcs = {
+	.get = meson_clk_clock_get,
+	.put = meson_clk_clock_put,
+	.get_rate = meson_clk_clock_get_rate,
+	.set_rate = meson_clk_clock_set_rate,
+	.round_rate = meson_clk_clock_round_rate,
+	.enable = meson_clk_clock_enable,
+	.disable = meson_clk_clock_disable,
+	.set_parent = meson_clk_clock_set_parent,
+	.get_parent = meson_clk_clock_get_parent,
+};
+
+struct meson_clk_clk *
+meson_clk_clock_find(struct meson_clk_softc *sc, const char *name)
+{
+	for (int i = 0; i < sc->sc_nclks; i++) {
+		if (sc->sc_clks[i].base.name == NULL)
+			continue;
+		if (strcmp(sc->sc_clks[i].base.name, name) == 0)
+			return &sc->sc_clks[i];
+	}
+
+	return NULL;
+}
+
+int
+meson_clk_attach(struct meson_clk_softc *sc, u_int reg_index)
+{
+	bus_addr_t addr;
+	bus_size_t size;
+	int i;
+
+	if (fdtbus_get_reg(sc->sc_phandle, reg_index, &addr, &size) != 0) {
+		aprint_error(": couldn't get registers\n");
+		return ENXIO;
+	}
+	if (bus_space_map(sc->sc_bst, addr, size, 0, &sc->sc_bsh) != 0) {
+		aprint_error(": couldn't map registers\n");
+		return ENXIO;
+	}
+
+	sc->sc_clkdom.name = device_xname(sc->sc_dev);
+	sc->sc_clkdom.funcs = &meson_clk_clock_funcs;
+	sc->sc_clkdom.priv = sc;
+	for (i = 0; i < sc->sc_nclks; i++) {
+		sc->sc_clks[i].base.domain = &sc->sc_clkdom;
+		clk_attach(&sc->sc_clks[i].base);
+	}
+
+	fdtbus_register_clock_controller(sc->sc_dev, sc->sc_phandle,
+	    &meson_clk_fdtclock_funcs);
+
+	fdtbus_register_reset_controller(sc->sc_dev, sc->sc_phandle,
+	    &meson_clk_fdtreset_funcs);
+
+	return 0;
+}
+
+void
+meson_clk_print(struct meson_clk_softc *sc)
+{
+	struct meson_clk_clk *clk;
+	struct clk *clkp_parent;
+	const char *type;
+	int i;
+
+	for (i = 0; i < sc->sc_nclks; i++) {
+		clk = &sc->sc_clks[i];
+		if (clk->type == MESON_CLK_UNKNOWN)
+			continue;
+
+		clkp_parent = clk_get_parent(&clk->base);
+
+		switch (clk->type) {
+		case MESON_CLK_FIXED:		type = "fixed"; break;
+		case MESON_CLK_GATE:		type = "gate"; break;
+		case MESON_CLK_MPLL:		type = "mpll"; break;
+		case MESON_CLK_PLL:		type = "pll"; break;
+		case MESON_CLK_DIV:		type = "div"; break;
+		case MESON_CLK_FIXED_FACTOR:	type = "fixed-factor"; break;
+		case MESON_CLK_MUX:		type = "mux"; break;
+		default:			type = "???"; break;
+		}
+
+        	aprint_debug_dev(sc->sc_dev,
+		    "%3d %-12s %2s %-12s %-7s ",
+		    i,
+        	    clk->base.name,
+        	    clkp_parent ? "<-" : "",
+        	    clkp_parent ? clkp_parent->name : "",
+        	    type);
+		aprint_debug("%10u Hz\n", clk_get_rate(&clk->base));
+	}
+}
---
+++ sys/arch/arm/amlogic/meson_clk.h
@@ -0,0 +1,344 @@
+/* $NetBSD: meson_clk.h,v 1.1 2019/01/19 20:56:03 jmcneill Exp $ */
+
+/*-
+ * Copyright (c) 2017-2019 Jared McNeill <jmcneill@invisible.ca>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#ifndef _ARM_MESON_CLK_H
+#define _ARM_MESON_CLK_H
+
+#include <dev/clk/clk_backend.h>
+
+struct meson_clk_softc;
+struct meson_clk_clk;
+struct meson_clk_reset;
+
+/*
+ * Resets
+ */
+
+struct meson_clk_reset {
+	bus_size_t	reg;
+	uint32_t	mask;
+};
+
+#define	MESON_CLK_RESET(_id, _reg, _bit)	\
+	[_id] = {				\
+		.reg = (_reg),			\
+		.mask = __BIT(_bit),		\
+	}
+
+/*
+ * Clocks
+ */
+
+enum meson_clk_clktype {
+	MESON_CLK_UNKNOWN,
+	MESON_CLK_FIXED,
+	MESON_CLK_GATE,
+	MESON_CLK_PLL,
+	MESON_CLK_MPLL,
+	MESON_CLK_DIV,
+	MESON_CLK_FIXED_FACTOR,
+	MESON_CLK_MUX,
+};
+
+/*
+ * Fixed clocks
+ */
+
+struct meson_clk_fixed {
+	u_int		rate;
+};
+
+u_int	meson_clk_fixed_get_rate(struct meson_clk_softc *, struct meson_clk_clk *);
+
+#define	MESON_CLK_FIXED(_id, _name, _rate)			\
+	[_id] = {						\
+		.type = MESON_CLK_FIXED,			\
+		.base.name = (_name),				\
+		.base.flags = 0,				\
+		.u.fixed.rate = (_rate),			\
+		.get_rate = meson_clk_fixed_get_rate,		\
+	}
+
+/*
+ * Gate clocks
+ */
+
+struct meson_clk_gate {
+	bus_size_t	reg;
+	uint32_t	mask;
+	const char	*parent;
+	uint32_t	flags;
+#define	MESON_CLK_GATE_SET_TO_DISABLE		__BIT(0)
+};
+
+int	meson_clk_gate_enable(struct meson_clk_softc *,
+			      struct meson_clk_clk *, int);
+const char *meson_clk_gate_get_parent(struct meson_clk_softc *,
+				      struct meson_clk_clk *);
+
+#define	MESON_CLK_GATE_FLAGS(_id, _name, _pname, _reg, _bit, _flags)	\
+	[_id] = {						\
+		.type = MESON_CLK_GATE,				\
+		.base.name = (_name),				\
+		.base.flags = CLK_SET_RATE_PARENT,		\
+		.u.gate.parent = (_pname),			\
+		.u.gate.reg = (_reg),				\
+		.u.gate.mask = __BIT(_bit),			\
+		.u.gate.flags = (_flags),			\
+		.enable = meson_clk_gate_enable,		\
+		.get_parent = meson_clk_gate_get_parent,	\
+	}
+
+#define	MESON_CLK_GATE(_id, _name, _pname, _reg, _bit)		\
+	MESON_CLK_GATE_FLAGS(_id, _name, _pname, _reg, _bit, 0)
+
+/*
+ * Divider clocks
+ */
+
+struct meson_clk_div {
+	bus_size_t	reg;
+	const char	*parent;
+	uint32_t	div;
+	uint32_t	flags;
+#define	MESON_CLK_DIV_POWER_OF_TWO	__BIT(0)
+#define	MESON_CLK_DIV_SET_RATE_PARENT	__BIT(1)
+#define	MESON_CLK_DIV_CPU_SCALE_TABLE	__BIT(2)
+};
+
+u_int	meson_clk_div_get_rate(struct meson_clk_softc *,
+			       struct meson_clk_clk *);
+int	meson_clk_div_set_rate(struct meson_clk_softc *,
+			       struct meson_clk_clk *, u_int);
+const char *meson_clk_div_get_parent(struct meson_clk_softc *,
+				     struct meson_clk_clk *);
+
+#define	MESON_CLK_DIV(_id, _name, _parent, _reg, _div, _flags)	\
+	[_id] = {						\
+		.type = MESON_CLK_DIV,				\
+		.base.name = (_name),				\
+		.u.div.reg = (_reg),				\
+		.u.div.parent = (_parent),			\
+		.u.div.div = (_div),				\
+		.u.div.flags = (_flags),			\
+		.get_rate = meson_clk_div_get_rate,		\
+		.set_rate = meson_clk_div_set_rate,		\
+		.get_parent = meson_clk_div_get_parent,		\
+	}
+
+/*
+ * Fixed-factor clocks
+ */
+
+struct meson_clk_fixed_factor {
+	const char	*parent;
+	u_int		div;
+	u_int		mult;
+};
+
+u_int	meson_clk_fixed_factor_get_rate(struct meson_clk_softc *,
+					struct meson_clk_clk *);
+int	meson_clk_fixed_factor_set_rate(struct meson_clk_softc *,
+					struct meson_clk_clk *, u_int);
+const char *meson_clk_fixed_factor_get_parent(struct meson_clk_softc *,
+					      struct meson_clk_clk *);
+
+#define	MESON_CLK_FIXED_FACTOR(_id, _name, _parent, _div, _mult)	\
+	[_id] = {							\
+		.type = MESON_CLK_FIXED_FACTOR,				\
+		.base.name = (_name),					\
+		.u.fixed_factor.parent = (_parent),			\
+		.u.fixed_factor.div = (_div),				\
+		.u.fixed_factor.mult = (_mult),				\
+		.get_rate = meson_clk_fixed_factor_get_rate,		\
+		.get_parent = meson_clk_fixed_factor_get_parent,	\
+		.set_rate = meson_clk_fixed_factor_set_rate,		\
+	}
+
+/*
+ * Mux clocks
+ */
+
+struct meson_clk_mux {
+	bus_size_t	reg;
+	const char	**parents;
+	u_int		nparents;
+	uint32_t	sel;
+	uint32_t	flags;
+};
+
+const char *meson_clk_mux_get_parent(struct meson_clk_softc *,
+			       struct meson_clk_clk *);
+
+#define	MESON_CLK_MUX(_id, _name, _parents, _reg, _sel, _flags)		\
+	[_id] = {							\
+		.type = MESON_CLK_MUX,					\
+		.base.name = (_name),					\
+		.u.mux.parents = (_parents),				\
+		.u.mux.nparents = __arraycount(_parents),		\
+		.u.mux.reg = (_reg),					\
+		.u.mux.sel = (_sel),					\
+		.u.mux.flags = (_flags),				\
+		.get_parent = meson_clk_mux_get_parent,			\
+	}
+
+/*
+ * PLL clocks
+ */
+
+struct meson_clk_pll_reg {
+	bus_size_t	reg;
+	uint32_t	mask;
+};
+
+#define	MESON_CLK_PLL_REG(_reg, _mask)					\
+	{ .reg = (_reg), .mask = (_mask) }
+#define	MESON_CLK_PLL_REG_INVALID	MESON_CLK_PLL_REG(0,0)
+
+struct meson_clk_pll {
+	struct meson_clk_pll_reg	enable;
+	struct meson_clk_pll_reg	m;
+	struct meson_clk_pll_reg	n;
+	struct meson_clk_pll_reg	frac;
+	struct meson_clk_pll_reg	l;
+	struct meson_clk_pll_reg	reset;
+	const char			*parent;
+	uint32_t			flags;
+};
+
+u_int	meson_clk_pll_get_rate(struct meson_clk_softc *,
+			       struct meson_clk_clk *);
+const char *meson_clk_pll_get_parent(struct meson_clk_softc *,
+				     struct meson_clk_clk *);
+
+#define	MESON_CLK_PLL(_id, _name, _parent, _enable, _m, _n, _frac, _l,	\
+		      _reset, _flags)					\
+	[_id] = {							\
+		.type = MESON_CLK_PLL,					\
+		.base.name = (_name),					\
+		.u.pll.parent = (_parent),				\
+		.u.pll.enable = _enable,				\
+		.u.pll.m = _m,						\
+		.u.pll.n = _n,						\
+		.u.pll.frac = _frac,					\
+		.u.pll.l = _l,						\
+		.u.pll.reset = _reset,					\
+		.u.pll.flags = (_flags),				\
+		.get_rate = meson_clk_pll_get_rate,			\
+		.get_parent = meson_clk_pll_get_parent,			\
+	}
+
+/*
+ * MPLL clocks
+ */
+
+struct meson_clk_mpll {
+	struct meson_clk_pll_reg	sdm;
+	struct meson_clk_pll_reg	sdm_enable;
+	struct meson_clk_pll_reg	n2;
+	struct meson_clk_pll_reg	ssen;
+	const char			*parent;
+	uint32_t			flags;
+};
+
+u_int	meson_clk_mpll_get_rate(struct meson_clk_softc *,
+				struct meson_clk_clk *);
+const char *meson_clk_mpll_get_parent(struct meson_clk_softc *,
+				      struct meson_clk_clk *);
+
+#define	MESON_CLK_MPLL(_id, _name, _parent, _sdm, _sdm_enable, _n2,	\
+		       _ssen, _flags)					\
+	[_id] = {							\
+		.type = MESON_CLK_MPLL,					\
+		.base.name = (_name),					\
+		.u.mpll.parent = (_parent),				\
+		.u.mpll.sdm = _sdm,					\
+		.u.mpll.sdm_enable = _sdm_enable,			\
+		.u.mpll.n2 = _n2,					\
+		.u.mpll.ssen = _ssen,					\
+		.u.mpll.flags = (_flags),				\
+		.get_rate = meson_clk_mpll_get_rate,			\
+		.get_parent = meson_clk_mpll_get_parent,		\
+	}
+
+
+
+struct meson_clk_clk {
+	struct clk	base;
+	enum meson_clk_clktype type;
+	union {
+		struct meson_clk_fixed fixed;
+		struct meson_clk_gate gate;
+		struct meson_clk_div div;
+		struct meson_clk_fixed_factor fixed_factor;
+		struct meson_clk_mux mux;
+		struct meson_clk_pll pll;
+		struct meson_clk_mpll mpll;
+	} u;
+
+	int		(*enable)(struct meson_clk_softc *,
+				  struct meson_clk_clk *, int);
+	u_int		(*get_rate)(struct meson_clk_softc *,
+				    struct meson_clk_clk *);
+	int		(*set_rate)(struct meson_clk_softc *,
+				    struct meson_clk_clk *, u_int);
+	u_int		(*round_rate)(struct meson_clk_softc *,
+				    struct meson_clk_clk *, u_int);
+	const char *	(*get_parent)(struct meson_clk_softc *,
+				      struct meson_clk_clk *);
+	int		(*set_parent)(struct meson_clk_softc *,
+				      struct meson_clk_clk *,
+				      const char *);
+};
+
+struct meson_clk_softc {
+	device_t		sc_dev;
+	int			sc_phandle;
+	bus_space_tag_t		sc_bst;
+	bus_space_handle_t	sc_bsh;
+
+	struct clk_domain	sc_clkdom;
+
+	struct meson_clk_reset *sc_resets;
+	u_int			sc_nresets;
+
+	struct meson_clk_clk	*sc_clks;
+	u_int			sc_nclks;
+};
+
+int	meson_clk_attach(struct meson_clk_softc *, u_int);
+struct meson_clk_clk *meson_clk_clock_find(struct meson_clk_softc *,
+					   const char *);
+void	meson_clk_print(struct meson_clk_softc *);
+
+#define CLK_READ(sc, reg)	\
+	bus_space_read_4((sc)->sc_bst, (sc)->sc_bsh, (reg))
+#define CLK_WRITE(sc, reg, val)	\
+	bus_space_write_4((sc)->sc_bst, (sc)->sc_bsh, (reg), (val))
+
+#endif /* _ARM_MESON_CLK_H */
