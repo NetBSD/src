0b4f44e23707935f38f88e56d09f14dbea7c04fd
mrg <mrg@NetBSD.org>
2019-01-19 12:58:37 +0000

    obsolete gcc 5 support. add gcc 7 support.  call it "nb1 20190119"

--- tools/gcc/gcc-version.mk
+++ tools/gcc/gcc-version.mk
@@ -1,9 +1,9 @@
-#	$NetBSD: gcc-version.mk,v 1.12 2018/11/11 23:05:25 mrg Exp $
+#	$NetBSD: gcc-version.mk,v 1.13 2019/01/19 12:58:37 mrg Exp $
 
 # common location for tools and native build
 
 .if ${HAVE_GCC} == 6
 NETBSD_GCC_VERSION=nb4 20181109
 .elif ${HAVE_GCC} == 7
-NETBSD_GCC_VERSION=nb1 20180905
+NETBSD_GCC_VERSION=nb1 20190119
 .endif
