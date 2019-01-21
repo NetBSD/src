ea592c9cf255278a8dfe66f953296e4d6bdda086
skrll <skrll@NetBSD.org>
2019-01-21 07:49:45 +0000

    Make more KVA available

--- sys/arch/evbarm/fdt/platform.h
+++ sys/arch/evbarm/fdt/platform.h
@@ -1,4 +1,4 @@
-/* $NetBSD: platform.h,v 1.4 2018/04/01 04:35:04 ryo Exp $ */
+/* $NetBSD: platform.h,v 1.5 2019/01/21 07:49:45 skrll Exp $ */
 
 /*-
  * Copyright (c) 2015-2017 Jared McNeill <jmcneill@invisible.ca>
@@ -47,11 +47,11 @@ void fdt_add_reserved_memory_range(uint64_t, uint64_t);
 
 #ifdef __HAVE_MM_MD_DIRECT_MAPPED_PHYS
 #define KERNEL_VM_BASE		0xc0000000
-#define KERNEL_VM_SIZE		0x20000000 /* 0x20000000 = 512MB */
 #else
 #define KERNEL_VM_BASE		0x90000000
-#define KERNEL_VM_SIZE		0x50000000 /* 0x50000000 = 1.25GB */
 #endif
+
+#define KERNEL_VM_SIZE		(KERNEL_IO_VBASE - KERNEL_VM_BASE)
 
 #endif /* !__aarch64 */
 
