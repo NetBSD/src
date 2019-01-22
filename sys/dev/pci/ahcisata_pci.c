ae8181a230b130d19da5218611a63d514f5c6ca3
jdolecek <jdolecek@NetBSD.org>
2019-01-22 22:22:06 +0000

    fix pasto; still PR kern/53307

--- sys/dev/pci/ahcisata_pci.c
+++ sys/dev/pci/ahcisata_pci.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: ahcisata_pci.c,v 1.53 2019/01/22 22:14:20 jdolecek Exp $	*/
+/*	$NetBSD: ahcisata_pci.c,v 1.54 2019/01/22 22:22:06 jdolecek Exp $	*/
 
 /*
  * Copyright (c) 2006 Manuel Bouyer.
@@ -26,7 +26,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: ahcisata_pci.c,v 1.53 2019/01/22 22:14:20 jdolecek Exp $");
+__KERNEL_RCSID(0, "$NetBSD: ahcisata_pci.c,v 1.54 2019/01/22 22:22:06 jdolecek Exp $");
 
 #ifdef _KERNEL_OPT
 #include "opt_ahcisata_pci.h"
@@ -200,9 +200,9 @@ static const struct ahci_pci_quirk ahci_pci_quirks[] = {
 	    AHCI_PCI_QUIRK_FORCE },
 	{ PCI_VENDOR_AMD, PCI_PRODUCT_AMD_HUDSON_SATA,
 	    AHCI_PCI_QUIRK_FORCE },
-	{ PCI_VENDOR_INTEL, 82801JI_SATA_AHCI,
+	{ PCI_VENDOR_INTEL, PCI_PRODUCT_INTEL_82801JI_SATA_AHCI,
 	    AHCI_QUIRK_BADPMP },
-	{ PCI_VENDOR_AMD, HUDSON_SATA_AHCI,
+	{ PCI_VENDOR_AMD, PCI_PRODUCT_AMD_HUDSON_SATA_AHCI,
 	    AHCI_QUIRK_BADPMP },
 };
 
