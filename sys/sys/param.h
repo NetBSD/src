b80f8a85e4964a705df6abada1eebac45d51b1f6
msaitoh <msaitoh@NetBSD.org>
2019-01-23 07:05:52 +0000

    Welcome to 8.99.31 (MII {read,write}reg API change).

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.577 2019/01/07 22:05:50 jdolecek Exp $	*/
+/*	$NetBSD: param.h,v 1.578 2019/01/23 07:05:52 msaitoh Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -67,7 +67,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	899003000	/* NetBSD 8.99.30 */
+#define	__NetBSD_Version__	899003100	/* NetBSD 8.99.31 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
