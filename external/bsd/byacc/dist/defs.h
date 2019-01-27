398d17a0864d28eb12e289c2ce720f51a2b9ea2a
dholland <dholland@NetBSD.org>
2019-01-27 04:21:26 +0000

    fix duplicated chunk from merge

--- external/bsd/byacc/dist/defs.h
+++ external/bsd/byacc/dist/defs.h
@@ -1,6 +1,4 @@
-/*	$NetBSD: defs.h,v 1.14 2019/01/27 02:08:33 pgoyette Exp $	*/
-
-/* Id: defs.h,v 1.60 2017/12/04 17:50:02 erik.b.andersen Exp  */
+/*	$NetBSD: defs.h,v 1.15 2019/01/27 04:25:37 dholland Exp $	*/
 
 /* Id: defs.h,v 1.60 2017/12/04 17:50:02 erik.b.andersen Exp  */
 
