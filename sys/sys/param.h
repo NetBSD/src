89ba8aee4f132e3e5123f9cd167ed38056d6eeb5
pgoyette <pgoyette@NetBSD.org>
2019-01-27 02:37:07 +0000

    Update version on the branch, too

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.557.2.11 2019/01/26 22:00:37 pgoyette Exp $	*/
+/*	$NetBSD: param.h,v 1.557.2.12 2019/01/27 02:37:07 pgoyette Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -67,7 +67,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	899003100	/* NetBSD 8.99.31 */
+#define	__NetBSD_Version__	899003200	/* NetBSD 8.99.32 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
