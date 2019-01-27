340176b1b1b52347636731c899e003d2b3e360ce
rin <rin@NetBSD.org>
2019-01-27 09:19:36 +0000

    Fix merge botches. I hope there's no more left...

--- sys/rump/dev/lib/libulpt/ulpt_at_usb.c
+++ sys/rump/dev/lib/libulpt/ulpt_at_usb.c
@@ -1,10 +1,7 @@
-/*	$NetBSD: ulpt_at_usb.c,v 1.8 2019/01/27 02:08:49 pgoyette Exp $	*/
+/*	$NetBSD: ulpt_at_usb.c,v 1.9 2019/01/27 09:19:37 rin Exp $	*/
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: ulpt_at_usb.c,v 1.8 2019/01/27 02:08:49 pgoyette Exp $");
-
-#include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: ulpt_at_usb.c,v 1.8 2019/01/27 02:08:49 pgoyette Exp $");
+__KERNEL_RCSID(0, "$NetBSD: ulpt_at_usb.c,v 1.9 2019/01/27 09:19:37 rin Exp $");
 
 #include <sys/param.h>
 #include <sys/conf.h>
