7fd1d8026a7c9c7c2caa820ca42644411586ba3f
pgoyette <pgoyette@NetBSD.org>
2019-01-28 13:13:02 +0000

    Add the appropriate module glue.  Seems to have gotten lost during the merge.

--- sys/compat/netbsd32/netbsd32_compat_40.c
+++ sys/compat/netbsd32/netbsd32_compat_40.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: netbsd32_compat_40.c,v 1.2 2018/12/24 21:27:05 mrg Exp $	*/
+/*	$NetBSD: netbsd32_compat_40.c,v 1.3 2019/01/28 13:13:02 pgoyette Exp $	*/
 
 /*
  * Copyright (c) 1998, 2001, 2008, 2018 Matthew R. Green
@@ -29,7 +29,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: netbsd32_compat_40.c,v 1.2 2018/12/24 21:27:05 mrg Exp $");
+__KERNEL_RCSID(0, "$NetBSD: netbsd32_compat_40.c,v 1.3 2019/01/28 13:13:02 pgoyette Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -61,3 +61,30 @@ compat_40_netbsd32_mount(struct lwp *l,
 
 	return compat_40_sys_mount(l, &ua, retval);
 }
+
+static struct syscall_package compat_netbsd32_40_syscalls[] = {
+	{ NETBSD32_SYS_compat_40_netbsd32_mount, 0,
+	    (sy_call_t *)compat_40_netbsd32_mount },
+	{ 0, 0, NULL }
+};    
+
+MODULE(MODULE_CLASS_EXEC, compat_netbsd32_40, "compat_netbsd32_50,compat_40");
+ 
+static int
+compat_netbsd32_40_modcmd(modcmd_t cmd, void *arg)
+{
+ 
+	switch (cmd) {
+	case MODULE_CMD_INIT:
+		return syscall_establish(&emul_netbsd32,
+		    compat_netbsd32_40_syscalls);
+ 
+	case MODULE_CMD_FINI:
+		return syscall_disestablish(&emul_netbsd32,
+		    compat_netbsd32_40_syscalls);
+
+	default:
+		return ENOTTY;
+	}
+}
+
