e9bc6944369ffa92746f274a2214765256e2707c
lukem <lukem@NetBSD.org>
2019-01-29 11:51:05 +0000

    NetBSD-ftpd 20180428
    Update version to "NetBSD-ftpd 20180428" for changes:
    - Fix violations of the sequence point rule.
    - Check that stat and fstat succeed.
    - Support blacklistd(8) hooks.
    - Clear utmpx struct before writing it to wtmpx files.
    - Fix directory stream leaks.
    - Use explicit_memset(3) instead of memset(3) to clear password.
    - Fix scope of variable. PR misc/50665.
    - Ensure that closing socket exists. CID 603440.
    - Add -f option to ftpd to stay in foreground with -D. PR bin/53221.

--- libexec/ftpd/version.h
+++ libexec/ftpd/version.h
@@ -1,6 +1,6 @@
-/*	$NetBSD: version.h,v 1.75 2013/03/21 00:17:26 lukem Exp $	*/
+/*	$NetBSD: version.h,v 1.76 2019/01/29 11:51:05 lukem Exp $	*/
 /*-
- * Copyright (c) 1999-2013 The NetBSD Foundation, Inc.
+ * Copyright (c) 1999-2019 The NetBSD Foundation, Inc.
  * All rights reserved.
  *
  * This code is derived from software contributed to The NetBSD Foundation
@@ -29,5 +29,5 @@
  */
 
 #ifndef FTPD_VERSION
-#define	FTPD_VERSION	"NetBSD-ftpd 20110904"
+#define	FTPD_VERSION	"NetBSD-ftpd 20180428"
 #endif
