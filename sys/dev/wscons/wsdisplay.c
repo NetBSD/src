6a94f23b1cd69da53c73f307f1a5cbdbbc62d1d8
jmcneill <jmcneill@NetBSD.org>
2019-01-30 02:43:47 +0000

    Remove KASSERT in wsdisplay_cndetach. The caller likely doesn't know the state of the console, so simply do cleanup if a previous console has been attached.

--- sys/dev/wscons/wsdisplay.c
+++ sys/dev/wscons/wsdisplay.c
@@ -1,4 +1,4 @@
-/* $NetBSD: wsdisplay.c,v 1.149 2018/11/29 23:18:40 jmcneill Exp $ */
+/* $NetBSD: wsdisplay.c,v 1.150 2019/01/30 02:43:47 jmcneill Exp $ */
 
 /*
  * Copyright (c) 1996, 1997 Christopher G. Demetriou.  All rights reserved.
@@ -31,7 +31,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: wsdisplay.c,v 1.149 2018/11/29 23:18:40 jmcneill Exp $");
+__KERNEL_RCSID(0, "$NetBSD: wsdisplay.c,v 1.150 2019/01/30 02:43:47 jmcneill Exp $");
 
 #ifdef _KERNEL_OPT
 #include "opt_wsdisplay_compat.h"
@@ -966,10 +966,12 @@ wsdisplay_preattach(const struct wsscreen_descr *type, void *cookie,
 void
 wsdisplay_cndetach(void)
 {
-	KASSERT(wsdisplay_console_initted == 2);
-
-	cn_tab = wsdisplay_ocn;
-	wsdisplay_console_initted = 0;
+	if (wsdisplay_console_initted > 0) {
+		cn_tab = wsdisplay_ocn;
+		wsdisplay_console_initted = 0;
+		wsdisplay_console_attached = 0;
+		wsdisplay_console_device = NULL;
+	}
 }
 
 /*
