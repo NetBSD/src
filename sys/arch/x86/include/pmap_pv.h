ba56b89157fa60c29727bb782b31e482b80f2cd6
maxv <maxv@NetBSD.org>
2019-02-01 05:44:29 +0000

    Change the format of the pp_attrs field: instead of using PTE bits directly, use abstracted bits that are converted from/to PTE bits when needed (in pmap_sync_pv).
    This allows us to use the same pp_attrs for pmaps that have PTE bits at
    different locations.

--- sys/arch/x86/include/pmap_pv.h
+++ sys/arch/x86/include/pmap_pv.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: pmap_pv.h,v 1.3 2011/06/12 03:35:50 rmind Exp $	*/
+/*	$NetBSD: pmap_pv.h,v 1.4 2019/02/01 05:44:29 maxv Exp $	*/
 
 /*-
  * Copyright (c)2008 YAMAMOTO Takashi,
@@ -81,7 +81,10 @@ struct pmap_page {
 #define	pp_head	pp_u.u_head
 #define	pp_link	pp_u.u_link
 	uint8_t pp_flags;
-	uint8_t pp_attrs;	/* saved PG_M and PG_U */
+	uint8_t pp_attrs;
+#define PP_ATTRS_M	0x01	/* saved PG_M */
+#define PP_ATTRS_U	0x02	/* saved PG_U */
+#define PP_ATTRS_W	0x04	/* saved PG_RW */
 };
 
 /* pp_flags */
