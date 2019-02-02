aee38134a4071eb20e635290fbcf59eeda64db08
cherry <cherry@NetBSD.org>
2019-02-02 14:50:15 +0000

    Remove mb(), rmb() and wmb() from the kernel namespace.
    These are introduced by external/bsd/common/include/asm/barrier.h
    
    The purpose of barrier.h is to bridge the use of linux API calls
    within code which uses them, such as drm code. The XEN api implicitly
    uses these calls which are linuxisms within io/ring.h
    
    This diff undos the damage.
    
    The correct fix is to modify io/ring.h to not assume that all OSs that
    XEN runs on has these functions, and to appropriately conditionally via
    #ifdef __NetBSD__/#endif use the appropriate NetBSD functions. These
    changes then need to be pushed upstream.

--- sys/arch/xen/include/xenring.h
+++ sys/arch/xen/include/xenring.h
@@ -1,4 +1,4 @@
-/* $NetBSD: xenring.h,v 1.1 2019/02/02 12:32:55 cherry Exp $ */
+/* $NetBSD: xenring.h,v 1.2 2019/02/02 14:50:15 cherry Exp $ */
 
 /*
  * Glue goop for xbd ring request/response protocol structures.
@@ -10,10 +10,28 @@
 #ifndef _XEN_RING_H
 #define _XEN_RING_H
 
-#if __XEN_INTERFACE_VERSION__ >= 0x00030201
+#if (__XEN_INTERFACE_VERSION__ >= 0x00030201) &&
+	(__XEN_INTERFACE_VERSION < 0x00030208)
 
 #include <xen/include/public/io/ring.h>
 
+/*
+ * Undo namespace damage from xen/include/public/io/ring.h
+ * The proper fix is to get upstream to stop assuming that all OSs use
+ * mb(), rmb(), wmb().
+ */
+#undef xen_mb
+#undef xen_rmb
+#undef xen_wmb
+
+#define xen_mb()  membar_sync()
+#define xen_rmb() membar_producer()
+#define xen_wmb() membar_consumer()
+
+/*
+ * Define ring types. These were previously part of the public API.
+ * Not anymore.
+ */
 DEFINE_RING_TYPES(blkif_x86_32, struct blkif_request, struct blkif_response);
 DEFINE_RING_TYPES(blkif_x86_64, struct blkif_request, struct blkif_response);
 
@@ -30,5 +48,6 @@ union blkif_back_ring_proto {
 };
 typedef union blkif_back_ring_proto blkif_back_ring_proto_t;
 
-#endif /* __XEN_INTERFACE_VERSION__ >= 0x00030201 */
+#endif /* __XEN_INTERFACE_VERSION__ */
+
 #endif /* _XEN_RING_H_ */
