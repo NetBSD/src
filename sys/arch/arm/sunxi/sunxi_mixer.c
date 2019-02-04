f3cb3f99dc81a884f91442761cf807ebfb8610d6
jmcneill <jmcneill@NetBSD.org>
2019-02-04 12:10:13 +0000

    Add support for vblank irq and RGB overlay planes.

--- sys/arch/arm/sunxi/sunxi_mixer.c
+++ sys/arch/arm/sunxi/sunxi_mixer.c
@@ -1,4 +1,4 @@
-/* $NetBSD: sunxi_mixer.c,v 1.2 2019/01/31 01:49:28 jmcneill Exp $ */
+/* $NetBSD: sunxi_mixer.c,v 1.3 2019/02/04 12:10:13 jmcneill Exp $ */
 
 /*-
  * Copyright (c) 2019 Jared D. McNeill <jmcneill@invisible.ca>
@@ -27,7 +27,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: sunxi_mixer.c,v 1.2 2019/01/31 01:49:28 jmcneill Exp $");
+__KERNEL_RCSID(0, "$NetBSD: sunxi_mixer.c,v 1.3 2019/02/04 12:10:13 jmcneill Exp $");
 
 #include <sys/param.h>
 #include <sys/bus.h>
@@ -52,6 +52,7 @@ __KERNEL_RCSID(0, "$NetBSD: sunxi_mixer.c,v 1.2 2019/01/31 01:49:28 jmcneill Exp
 #define	GLB_BASE		0x00000
 #define	BLD_BASE		0x01000
 #define	OVL_BASE(n)		(0x02000 + (n) * 0x1000)
+#define	OVL_V_BASE		OVL_BASE(0)
 #define	OVL_UI_BASE		OVL_BASE(1)
 
 /* GLB registers */
@@ -64,13 +65,47 @@ __KERNEL_RCSID(0, "$NetBSD: sunxi_mixer.c,v 1.2 2019/01/31 01:49:28 jmcneill Exp
 
 /* BLD registers */
 #define	BLD_FILL_COLOR_CTL	0x000
+#define	 BLD_FILL_COLOR_CTL_P1_EN		__BIT(9)
 #define	 BLD_FILL_COLOR_CTL_P0_EN		__BIT(8)
 #define	BLD_CH_ISIZE(n)		(0x008 + (n) * 0x10)
 #define	BLD_CH_OFFSET(n)	(0x00c + (n) * 0x10)
 #define	BLD_CH_RTCTL		0x080
+#define	 BLD_CH_RTCTL_P1			__BITS(7,4)
 #define	 BLD_CH_RTCTL_P0			__BITS(3,0)
 #define	BLD_SIZE		0x08c
 #define	BLD_CTL(n)		(0x090 + (n) * 0x04)
+
+/* OVL_V registers */
+#define	OVL_V_ATTCTL(n)		(0x000 + (n) * 0x30)
+#define	 OVL_V_ATTCTL_VIDEO_UI_SEL		__BIT(15)
+#define	 OVL_V_ATTCTL_LAY_FBFMT			__BITS(12,8)
+#define	  OVL_V_ATTCTL_LAY_FBFMT_VYUY		0x00
+#define	  OVL_V_ATTCTL_LAY_FBFMT_YVYU		0x01
+#define	  OVL_V_ATTCTL_LAY_FBFMT_UYVY		0x02
+#define	  OVL_V_ATTCTL_LAY_FBFMT_YUYV		0x03
+#define	  OVL_V_ATTCTL_LAY_FBFMT_YUV422		0x06
+#define	  OVL_V_ATTCTL_LAY_FBFMT_YUV420		0x0a
+#define	  OVL_V_ATTCTL_LAY_FBFMT_YUV411		0x0e
+#define	  OVL_V_ATTCTL_LAY_FBFMT_XRGB_8888	0x04
+#define	 OVL_V_ATTCTL_LAY0_EN			__BIT(0)
+#define	OVL_V_MBSIZE(n)		(0x004 + (n) * 0x30)
+#define	OVL_V_COOR(n)		(0x008 + (n) * 0x30)
+#define	OVL_V_PITCH0(n)		(0x00c + (n) * 0x30)
+#define	OVL_V_PITCH1(n)		(0x010 + (n) * 0x30)
+#define	OVL_V_PITCH2(n)		(0x014 + (n) * 0x30)
+#define	OVL_V_TOP_LADD0(n)	(0x018 + (n) * 0x30)
+#define	OVL_V_TOP_LADD1(n)	(0x01c + (n) * 0x30)
+#define	OVL_V_TOP_LADD2(n)	(0x020 + (n) * 0x30)
+#define	OVL_V_FILL_COLOR(n)	(0x0c0 + (n) * 0x4)
+#define	OVL_V_TOP_HADD0		0x0d0
+#define	OVL_V_TOP_HADD1		0x0d4
+#define	OVL_V_TOP_HADD2		0x0d8
+#define	 OVL_V_TOP_HADD_LAYER0	__BITS(7,0)
+#define	OVL_V_SIZE		0x0e8
+#define	OVL_V_HDS_CTL0		0x0f0
+#define	OVL_V_HDS_CTL1		0x0f4
+#define	OVL_V_VDS_CTL0		0x0f8
+#define	OVL_V_VDS_CTL1		0x0fc
 
 /* OVL_UI registers */
 #define	OVL_UI_ATTR_CTL(n)	(0x000 + (n) * 0x20)
@@ -103,6 +138,11 @@ struct sunxi_mixer_crtc {
 	struct sunxi_mixer_softc *sc;
 };
 
+struct sunxi_mixer_overlay {
+	struct drm_plane	base;
+	struct sunxi_mixer_softc *sc;
+};
+
 struct sunxi_mixer_softc {
 	device_t		sc_dev;
 	bus_space_tag_t		sc_bst;
@@ -110,6 +150,7 @@ struct sunxi_mixer_softc {
 	int			sc_phandle;
 
 	struct sunxi_mixer_crtc	sc_crtc;
+	struct sunxi_mixer_overlay sc_overlay;
 
 	struct fdt_device_ports	sc_ports;
 };
@@ -124,12 +165,18 @@ struct sunxi_mixer_softc {
 #define	BLD_WRITE(sc, reg, val)				\
 	bus_space_write_4((sc)->sc_bst, (sc)->sc_bsh, BLD_BASE + (reg), (val))
 
+#define	OVL_V_READ(sc, reg)				\
+	bus_space_read_4((sc)->sc_bst, (sc)->sc_bsh, OVL_V_BASE + (reg))
+#define	OVL_V_WRITE(sc, reg, val)			\
+	bus_space_write_4((sc)->sc_bst, (sc)->sc_bsh, OVL_V_BASE + (reg), (val))
+
 #define	OVL_UI_READ(sc, reg)				\
 	bus_space_read_4((sc)->sc_bst, (sc)->sc_bsh, OVL_UI_BASE + (reg))
 #define	OVL_UI_WRITE(sc, reg, val)			\
 	bus_space_write_4((sc)->sc_bst, (sc)->sc_bsh, OVL_UI_BASE + (reg), (val))
 
-#define	to_sunxi_mixer_crtc(x)	container_of(x, struct sunxi_mixer_crtc, base)
+#define	to_sunxi_mixer_crtc(x)		container_of(x, struct sunxi_mixer_crtc, base)
+#define	to_sunxi_mixer_overlay(x)	container_of(x, struct sunxi_mixer_overlay, base)
 
 static void
 sunxi_mixer_destroy(struct drm_crtc *crtc)
@@ -193,14 +240,19 @@ sunxi_mixer_mode_set(struct drm_crtc *crtc, struct drm_display_mode *mode,
 	GLB_WRITE(sc, GLB_SIZE, size);
 
 	/* Enable pipe 0 */
-	BLD_WRITE(sc, BLD_FILL_COLOR_CTL, BLD_FILL_COLOR_CTL_P0_EN);
+	val = BLD_READ(sc, BLD_FILL_COLOR_CTL);
+	val |= BLD_FILL_COLOR_CTL_P0_EN;
+	BLD_WRITE(sc, BLD_FILL_COLOR_CTL, val);
 
 	/* Set blender 0 input size */
 	BLD_WRITE(sc, BLD_CH_ISIZE(0), size);
 	/* Set blender 0 offset */
 	BLD_WRITE(sc, BLD_CH_OFFSET(0), offset);
 	/* Route channel 1 to pipe 0 */
-	BLD_WRITE(sc, BLD_CH_RTCTL, __SHIFTIN(1, BLD_CH_RTCTL_P0));
+	val = BLD_READ(sc, BLD_CH_RTCTL);
+	val &= ~BLD_CH_RTCTL_P0;
+	val |= __SHIFTIN(1, BLD_CH_RTCTL_P0);
+	BLD_WRITE(sc, BLD_CH_RTCTL, val);
 	/* Set blender output size */
 	BLD_WRITE(sc, BLD_SIZE, size);
 
@@ -288,6 +340,144 @@ static const struct drm_crtc_helper_funcs sunxi_mixer_crtc_helper_funcs = {
 	.commit = sunxi_mixer_commit,
 };
 
+static void
+sunxi_mixer_overlay_destroy(struct drm_plane *plane)
+{
+}
+
+static bool
+sunxi_mixer_overlay_ui(uint32_t drm_format)
+{
+	switch (drm_format) {
+	case DRM_FORMAT_XRGB8888:
+		return true;
+	default:
+		return false;
+	}
+}
+
+static u_int
+sunxi_mixer_overlay_format(uint32_t drm_format)
+{
+	switch (drm_format) {
+	case DRM_FORMAT_XRGB8888:	return OVL_V_ATTCTL_LAY_FBFMT_XRGB_8888;
+	case DRM_FORMAT_VYUY:		return OVL_V_ATTCTL_LAY_FBFMT_VYUY;
+	case DRM_FORMAT_YVYU:		return OVL_V_ATTCTL_LAY_FBFMT_YVYU;
+	case DRM_FORMAT_UYVY:		return OVL_V_ATTCTL_LAY_FBFMT_UYVY;
+	case DRM_FORMAT_YUYV:		return OVL_V_ATTCTL_LAY_FBFMT_YUYV;
+	case DRM_FORMAT_YUV422:		return OVL_V_ATTCTL_LAY_FBFMT_YUV422;
+	case DRM_FORMAT_YUV420:		return OVL_V_ATTCTL_LAY_FBFMT_YUV420;
+	case DRM_FORMAT_YUV411:		return OVL_V_ATTCTL_LAY_FBFMT_YUV411;
+	default:			return 0;	/* shouldn't happen */
+	}
+}
+
+static int
+sunxi_mixer_overlay_update_plane(struct drm_plane *plane, struct drm_crtc *crtc,
+    struct drm_framebuffer *fb, int crtc_x, int crtc_y, u_int crtc_w, u_int crtc_h,
+    uint32_t src_x, uint32_t src_y, uint32_t src_w, uint32_t src_h)
+{
+	struct sunxi_mixer_overlay *overlay = to_sunxi_mixer_overlay(plane);
+	struct sunxi_mixer_softc * const sc = overlay->sc;
+	struct sunxi_drm_framebuffer *sfb = to_sunxi_drm_framebuffer(fb);
+	uint32_t val;
+
+	const u_int fbfmt = sunxi_mixer_overlay_format(fb->pixel_format);
+	const uint64_t paddr = (uint64_t)sfb->obj->dmamap->dm_segs[0].ds_addr;
+
+	const uint32_t input_size = (((src_h >> 16) - 1) << 16) | ((src_w >> 16) - 1);
+	const uint32_t input_pos = ((src_y >> 16) << 16) | (src_x >> 16);
+
+	OVL_V_WRITE(sc, OVL_V_MBSIZE(0), input_size);
+	OVL_V_WRITE(sc, OVL_V_COOR(0), input_pos);
+
+	OVL_V_WRITE(sc, OVL_V_PITCH0(0), fb->pitches[0]);
+	OVL_V_WRITE(sc, OVL_V_PITCH1(0), fb->pitches[1]);
+	OVL_V_WRITE(sc, OVL_V_PITCH2(0), fb->pitches[2]);
+
+	const uint64_t paddr0 = paddr + fb->offsets[0] +
+	    (src_x >> 16) * drm_format_plane_cpp(fb->pixel_format, 0) +
+	    (src_y >> 16) * fb->pitches[0];
+	const uint64_t paddr1 = paddr + fb->offsets[1] +
+	    (src_x >> 16) * drm_format_plane_cpp(fb->pixel_format, 1) +
+	    (src_y >> 16) * fb->pitches[1];
+	const uint64_t paddr2 = paddr + fb->offsets[2] +
+	    (src_x >> 16) * drm_format_plane_cpp(fb->pixel_format, 2) +
+	    (src_y >> 16) * fb->pitches[2];
+
+	OVL_V_WRITE(sc, OVL_V_TOP_HADD0, (paddr0 >> 32) & OVL_V_TOP_HADD_LAYER0);
+	OVL_V_WRITE(sc, OVL_V_TOP_HADD1, (paddr1 >> 32) & OVL_V_TOP_HADD_LAYER0);
+	OVL_V_WRITE(sc, OVL_V_TOP_HADD2, (paddr2 >> 32) & OVL_V_TOP_HADD_LAYER0);
+
+	OVL_V_WRITE(sc, OVL_V_TOP_LADD0(0), paddr0 & 0xffffffff);
+	OVL_V_WRITE(sc, OVL_V_TOP_LADD1(0), paddr1 & 0xffffffff);
+	OVL_V_WRITE(sc, OVL_V_TOP_LADD2(0), paddr2 & 0xffffffff);
+
+	OVL_V_WRITE(sc, OVL_V_SIZE, input_size);
+
+	val = OVL_V_ATTCTL_LAY0_EN;
+	val |= __SHIFTIN(fbfmt, OVL_V_ATTCTL_LAY_FBFMT);
+	if (sunxi_mixer_overlay_ui(fb->pixel_format) == true)
+		val |= OVL_V_ATTCTL_VIDEO_UI_SEL;
+	OVL_V_WRITE(sc, OVL_V_ATTCTL(0), val);
+
+	/* Set blender 1 input size */
+	BLD_WRITE(sc, BLD_CH_ISIZE(1), input_size);
+	/* Set blender 1 offset */
+	BLD_WRITE(sc, BLD_CH_OFFSET(1), (crtc_y << 16) | crtc_x);
+	/* Route channel 0 to pipe 1 */
+	val = BLD_READ(sc, BLD_CH_RTCTL);
+	val &= ~BLD_CH_RTCTL_P1;
+	val |= __SHIFTIN(0, BLD_CH_RTCTL_P1);
+	BLD_WRITE(sc, BLD_CH_RTCTL, val);
+
+        /* Enable pipe 1 */
+	val = BLD_READ(sc, BLD_FILL_COLOR_CTL);
+	val |= BLD_FILL_COLOR_CTL_P1_EN;
+	BLD_WRITE(sc, BLD_FILL_COLOR_CTL, val);
+
+	/* Commit settings */
+	GLB_WRITE(sc, GLB_DBUFFER, GLB_DBUFFER_DOUBLE_BUFFER_RDY);
+
+	return 0;
+}
+
+static int
+sunxi_mixer_overlay_disable_plane(struct drm_plane *plane)
+{
+	struct sunxi_mixer_overlay *overlay = to_sunxi_mixer_overlay(plane);
+	struct sunxi_mixer_softc * const sc = overlay->sc;
+	uint32_t val;
+
+	val = BLD_READ(sc, BLD_FILL_COLOR_CTL);
+	val &= ~BLD_FILL_COLOR_CTL_P1_EN;
+	BLD_WRITE(sc, BLD_FILL_COLOR_CTL, val);
+
+	/* Commit settings */
+	GLB_WRITE(sc, GLB_DBUFFER, GLB_DBUFFER_DOUBLE_BUFFER_RDY);
+
+	return 0;
+}
+
+static const struct drm_plane_funcs sunxi_mixer_overlay_funcs = {
+	.update_plane = sunxi_mixer_overlay_update_plane,
+	.disable_plane = sunxi_mixer_overlay_disable_plane,
+	.destroy = sunxi_mixer_overlay_destroy,
+};
+
+static uint32_t sunxi_mixer_overlay_formats[] = {
+	DRM_FORMAT_XRGB8888,
+#if notyet
+	DRM_FORMAT_VYUY,
+	DRM_FORMAT_YVYU,
+	DRM_FORMAT_UYVY,
+	DRM_FORMAT_YUYV,
+	DRM_FORMAT_YUV422,
+	DRM_FORMAT_YUV420,
+	DRM_FORMAT_YUV411,
+#endif
+};
+
 static int
 sunxi_mixer_ep_activate(device_t dev, struct fdt_endpoint *ep, bool activate)
 {
@@ -304,9 +494,15 @@ sunxi_mixer_ep_activate(device_t dev, struct fdt_endpoint *ep, bool activate)
 	}
 
 	sc->sc_crtc.sc = sc;
+	sc->sc_overlay.sc = sc;
 
 	drm_crtc_init(ddev, &sc->sc_crtc.base, &sunxi_mixer_crtc_funcs);
 	drm_crtc_helper_add(&sc->sc_crtc.base, &sunxi_mixer_crtc_helper_funcs);
+
+	drm_universal_plane_init(ddev, &sc->sc_overlay.base,
+	    1 << drm_crtc_index(&sc->sc_crtc.base), &sunxi_mixer_overlay_funcs,
+	    sunxi_mixer_overlay_formats, __arraycount(sunxi_mixer_overlay_formats),
+	    DRM_PLANE_TYPE_OVERLAY);
 
 	return fdt_endpoint_activate(ep, activate);
 }
