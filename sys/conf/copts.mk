2d275792d409045d2ba081f4609d0b929e32e185
mrg <mrg@NetBSD.org>
2019-02-05 09:29:19 +0000

    be sure to only apply zlib.c's -Wno-error=implicit-fallthrough to GCC 7. push the setting into the rump and module version too.

--- sys/conf/copts.mk
+++ sys/conf/copts.mk
@@ -1,10 +1,12 @@
-#	$NetBSD: copts.mk,v 1.1 2019/02/05 08:33:25 mrg Exp $
+#	$NetBSD: copts.mk,v 1.2 2019/02/05 09:29:19 mrg Exp $
 
 # MI per-file compiler options required.
 
 .ifndef _SYS_CONF_COPTS_MK_
 _SYS_CONF_COPTS_MK_=1
 
+.if defined(HAVE_GCC) && ${HAVE_GCC} == 7 && ${ACTIVE_CC} == "gcc"
 COPTS.zlib.c+=	-Wno-error=implicit-fallthrough
+.endif
 
 .endif
