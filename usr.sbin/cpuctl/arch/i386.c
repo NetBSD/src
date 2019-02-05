5d0a098dc0fe738e04385b119c3a8c0cd43c479d
msaitoh <msaitoh@NetBSD.org>
2019-02-05 08:07:59 +0000

    Add Ice Lake and Tremont from the latest Intel SDM.

--- usr.sbin/cpuctl/arch/i386.c
+++ usr.sbin/cpuctl/arch/i386.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: i386.c,v 1.91 2019/01/06 16:13:51 maxv Exp $	*/
+/*	$NetBSD: i386.c,v 1.92 2019/02/05 08:07:59 msaitoh Exp $	*/
 
 /*-
  * Copyright (c) 1999, 2000, 2001, 2006, 2007, 2008 The NetBSD Foundation, Inc.
@@ -57,7 +57,7 @@
 
 #include <sys/cdefs.h>
 #ifndef lint
-__RCSID("$NetBSD: i386.c,v 1.91 2019/01/06 16:13:51 maxv Exp $");
+__RCSID("$NetBSD: i386.c,v 1.92 2019/02/05 08:07:59 msaitoh Exp $");
 #endif /* not lint */
 
 #include <sys/types.h>
@@ -382,7 +382,9 @@ const struct cpu_cpuid_nameclass i386_cpuid_cpus[] = {
 				[0x5f] = "Atom (Goldmont, Denverton)",
 				[0x66] = "Future Core (Cannon Lake)",
 				[0x7a] = "Atom (Goldmont Plus)",
+				[0x7e] = "Future Core (Ice Lake)",
 				[0x85] = "Xeon Phi 7215, 7285, 7295 (Knights Mill)",
+				[0x86] = "Atom (Tremont)",
 				[0x8e] = "7th or 8th gen Core (Kaby Lake, Coffee Lake)",
 				[0x9e] = "7th or 8th gen Core (Kaby Lake, Coffee Lake)",
 			},
