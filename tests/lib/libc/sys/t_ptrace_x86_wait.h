5a04cbf3665031028317daeb20a0900bd6bd7506
kamil <kamil@NetBSD.org>
2019-02-10 02:13:45 +0000

    Link t_ptrace_wait* tests with -pthread
    While there, bump (c) for ATF ptrace(2) tests.
    
    Add __used in infinite_thread() for consistency with other functions in
    the file.

--- tests/lib/libc/sys/t_ptrace_x86_wait.h
+++ tests/lib/libc/sys/t_ptrace_x86_wait.h
@@ -1,7 +1,7 @@
-/*	$NetBSD: t_ptrace_x86_wait.h,v 1.8 2019/02/05 02:57:10 kamil Exp $	*/
+/*	$NetBSD: t_ptrace_x86_wait.h,v 1.9 2019/02/10 02:13:45 kamil Exp $	*/
 
 /*-
- * Copyright (c) 2016 The NetBSD Foundation, Inc.
+ * Copyright (c) 2016, 2017, 2018, 2019 The NetBSD Foundation, Inc.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
