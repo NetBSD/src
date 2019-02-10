e390526bd7915a629d0f121c6246cab7ff9647dc
mrg <mrg@NetBSD.org>
2019-02-10 05:01:59 +0000

    mark pf.c, radeon_cs.c and via_dmablit.c for no fall thru error.
    this was already done in the various modules, but not in the main
    kernel.

--- sys/conf/copts.mk
+++ sys/conf/copts.mk
@@ -1,4 +1,4 @@
-#	$NetBSD: copts.mk,v 1.2 2019/02/05 09:29:19 mrg Exp $
+#	$NetBSD: copts.mk,v 1.3 2019/02/10 05:01:59 mrg Exp $
 
 # MI per-file compiler options required.
 
@@ -6,7 +6,10 @@
 _SYS_CONF_COPTS_MK_=1
 
 .if defined(HAVE_GCC) && ${HAVE_GCC} == 7 && ${ACTIVE_CC} == "gcc"
-COPTS.zlib.c+=	-Wno-error=implicit-fallthrough
+COPTS.zlib.c+=		-Wno-error=implicit-fallthrough
+COPTS.pf.c+=		-Wno-error=implicit-fallthrough
+COPTS.radeon_cs.c+=	-Wno-error=implicit-fallthrough
+COPTS.via_dmablit.c+=	-Wno-error=implicit-fallthrough
 .endif
 
 .endif
