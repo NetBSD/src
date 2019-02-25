5eba7b0ad8399db629137826c2168c542e041c2f
kamil <kamil@NetBSD.org>
2019-02-25 13:19:14 +0000

    Fix typo in kcov.h
    Should fix i386.

--- sys/sys/kcov.h
+++ sys/sys/kcov.h
@@ -1,4 +1,4 @@
-/*      $NetBSD: kcov.h,v 1.2 2019/02/24 21:14:43 kamil Exp $        */
+/*      $NetBSD: kcov.h,v 1.3 2019/02/25 13:19:14 kamil Exp $        */
 
 /*
  * Copyright (c) 2019 The NetBSD Foundation, Inc.
@@ -54,7 +54,7 @@ typedef volatile uint64_t kcov_int_t;
 #define KCOV_STORE(x,v)	__atomic_store_n(&(x), (v), __ATOMIC_RELAXED)
 #define KCOV_LOAD(x)	__atomic_load_n(&(x), __ATOMIC_RELAXED)
 #else
-#define KCOV_STORE(x,v)	(x) = (y)
+#define KCOV_STORE(x,v)	(x) = (v)
 #define KCOV_LOAD(x)	(x)
 #endif
 
