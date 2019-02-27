194a5ab70be0652540d76a25d55a09a25131517a
mrg <mrg@NetBSD.org>
2019-02-27 09:10:38 +0000

    bump netbsd gcc version date.

--- tools/gcc/gcc-version.mk
+++ tools/gcc/gcc-version.mk
@@ -1,9 +1,9 @@
-#	$NetBSD: gcc-version.mk,v 1.13 2019/01/19 12:58:37 mrg Exp $
+#	$NetBSD: gcc-version.mk,v 1.14 2019/02/27 09:10:38 mrg Exp $
 
 # common location for tools and native build
 
 .if ${HAVE_GCC} == 6
 NETBSD_GCC_VERSION=nb4 20181109
 .elif ${HAVE_GCC} == 7
-NETBSD_GCC_VERSION=nb1 20190119
+NETBSD_GCC_VERSION=nb1 20190226
 .endif
