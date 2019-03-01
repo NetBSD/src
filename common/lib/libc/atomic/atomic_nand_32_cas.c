3bbf8c07ee158914ceea1a2c61bc4b343b69b25e
isaki <isaki@NetBSD.org>
2019-03-01 06:14:52 +0000

    Add missing alias for __atomic_fetch_nand_N. XXX I'm not sure why does llvm/sparc call __atomic instead of     __sync though. XXX atomic_*_cas.c should be more symmetric...

--- common/lib/libc/atomic/atomic_nand_32_cas.c
+++ common/lib/libc/atomic/atomic_nand_32_cas.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: atomic_nand_32_cas.c,v 1.1 2014/02/21 10:52:50 martin Exp $	*/
+/*	$NetBSD: atomic_nand_32_cas.c,v 1.2 2019/03/01 06:14:52 isaki Exp $	*/
 
 /*-
  * Copyright (c) 2014 The NetBSD Foundation, Inc.
@@ -59,3 +59,5 @@ nand_and_fetch_4(volatile uint32_t *addr, uint32_t val, ...)
 	} while (atomic_cas_32(addr, old, new) != old);
 	return new;
 }
+
+__strong_alias(__atomic_fetch_nand_4,__sync_fetch_and_nand_4)
