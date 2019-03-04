e97c5da083916937ba2a96ce689dc1ddebf7151a
christos <christos@NetBSD.org>
2019-03-04 19:15:39 +0000

    put back __UNCONST for now.

--- external/bsd/jemalloc/dist/src/base.c
+++ external/bsd/jemalloc/dist/src/base.c
@@ -509,6 +509,7 @@ base_postfork_child(tsdn_t *tsdn, base_t *base) {
 
 bool
 base_boot(tsdn_t *tsdn) {
-	b0 = base_new(tsdn, 0, (extent_hooks_t *)__UNCONST(&extent_hooks_default));
+	b0 = base_new(tsdn, 0, (extent_hooks_t *)
+	    __UNCONST(&extent_hooks_default));
 	return (b0 == NULL);
 }
