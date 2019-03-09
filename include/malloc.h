5efbdd40f78aabe23b284ec9abd5aa654b4bc201
christos <christos@NetBSD.org>
2019-03-09 03:52:10 +0000

    PR/54050: Ryo ONODERA: Add the jemalloc-specific api prototypes and extern.

--- include/malloc.h
+++ include/malloc.h
@@ -1,3 +1,32 @@
-/*	$NetBSD: malloc.h,v 1.3 1994/10/26 00:56:03 cgd Exp $	*/
+/*	$NetBSD: malloc.h,v 1.4 2019/03/09 03:52:10 christos Exp $	*/
+
+#ifndef _MALLOC_H_
+#define _MALLOC_H_
 
 #include <stdlib.h>
+
+__BEGIN_DECLS
+
+void *mallocx(size_t, int);
+void *rallocx(void *, size_t, int);
+size_t xallocx(void *, size_t, size_t, int);
+size_t sallocx(void *, int);
+void dallocx(void *, int);
+void sdallocx(void *, size_t, int);
+size_t nallocx(size_t, int);
+
+int mallctl(const char *, void *, size_t *, void *, size_t);
+int mallctltomib(const char *, size_t *, size_t *);
+int mallctlbymib(const size_t *, size_t, void *, size_t *, void *, size_t);
+
+void malloc_stats_print(void (*)(void *, const char *), void *, const char *);
+
+size_t malloc_usable_size(const void *);
+
+void (*malloc_message)(void *, const char *);
+
+const char *malloc_conf;
+
+__END_DECLS
+
+#endif /* _MALLOC_H_ */
