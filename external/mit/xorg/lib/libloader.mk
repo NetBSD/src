1fd2e2d28498c75321ecdd8861b09460af2b8bf4
mrg <mrg@NetBSD.org>
2019-03-10 10:51:58 +0000

    pull across most of the new build infrastruture for mesa18 from maya's git tree.  this includes may of the changes from the changes below:
    commit 52d85e74a1197aace38cc7acb705509e969120e6
    Author: coypu <coypu@sdf.org>
    Date:   Mon Mar 4 12:25:33 2019 +0200
    
        Make 32bit archs happier.
    
        except i386, which now suffers from locked atomics because we
        don't have clever ifunc tricks in libc.
    
    commit ee9b4c19c58127934ed3548ad0d68934cc95ccc7
    Author: coypu <coypu@sdf.org>
    Date:   Mon Mar 4 09:08:35 2019 +0200
    
        Adjust includes, append rather than replace, appease clang ppc builds
    
    commit a5341a3ad42572c78b6a2e6e5545bd323d7f2e4c
    Author: coypu <coypu@sdf.org>
    Date:   Sun Mar 3 11:52:19 2019 +0200
    
        Resolve i386 atomic issue and set lists.
    
        XXX libGL in i386 is still causing issues
        XXX pkgconfig file for vdpau
        XXX set lists for arm32
    
    commit 27bffc20bc15186c92cc5b8d5cc08d7299966b34
    Author: coypu <coypu@sdf.org>
    Date:   Sat Mar 2 21:52:45 2019 +0200
    
        make llvm sources x86 specific
    
        hopefully not breaking non-x86 clang builds, which
        have MKLLVM set.
    
    commit f9d34922619cc8f2a224c0138a73985e50daf87f
    Author: coypu <coypu@sdf.org>
    Date:   Sat Mar 2 21:33:31 2019 +0200
    
        move hack to build llvm libraries soon enough.
    
        add u_process.c to libGL specifically (dri, gallium and libGL all need
        it).
    
        Add some missing includes for pipe-loader when it moved to gallium
    
    commit 2b1083d0d538bb5d5dd88ab2ca0ca6c331e5dd5b
    Author: coypu <coypu@sdf.org>
    Date:   Sat Mar 2 16:47:58 2019 +0200
    
        shuffle around:
    
        some files move from shared driver.mk/loader.mk into gallium.
        others move to shared.
    
        add i915 files.
    
        now i965 glxgears works (amd64).
    
    commit 1897a90569b30b294bcbedadb3745092eca0bb33
    Author: Maya Rashish <maya@NetBSD.org>
    Date:   Sun Feb 24 23:36:26 2019 +0200
    
        Progress towards glxgears on i915
    
    commit 28c9c99a236404de41ae74e88ea6d9578c088b92
    Author: Maya Rashish <maya@NetBSD.org>
    Date:   Sun Feb 24 21:55:56 2019 +0200
    
        rototill i965/i915 causing it to no longer startx.
    
        Now missing sw_screen_create in the dri driver.
    
    commit 48eb746983a5a7967fba221e7b167808af36f44a
    Author: Maya Rashish <maya@NetBSD.org>
    Date:   Sun Feb 24 09:31:22 2019 +0200
    
        More of vdpau. Cogs spin.
    
    commit d9fbba8f61a43648d32f160c5fa62626788566ff
    Author: Maya Rashish <maya@NetBSD.org>
    Date:   Sat Feb 23 22:36:37 2019 +0200
    
        Adjust for MesaLib 18.
    
        Build llvmpipe driver on x86 (the driver itself is x86-only).
        build llvm on all x86, even on GCC builds.
        galahad driver removed (upstream).
        Don't build mesa 7 at all.

--- external/mit/xorg/lib/libloader.mk
+++ external/mit/xorg/lib/libloader.mk
@@ -1,18 +1,20 @@
-#	$NetBSD: libloader.mk,v 1.2 2019/03/10 02:29:52 mrg Exp $
+#	$NetBSD: libloader.mk,v 1.3 2019/03/10 10:51:58 mrg Exp $
 
 # makefile fragment for mesa src/loader
 
 # loader stuff.
 .PATH:		${X11SRCDIR.Mesa}/src/loader
-.PATH:		${X11SRCDIR.Mesa}/src/mesa/drivers/dri/common
+.PATH:		${X11SRCDIR.Mesa}/src/util
 SRCS.loader += \
 	loader.c \
 	pci_id_driver_map.c \
 	xmlconfig.c
 
 .for _f in ${SRCS.loader}
-CPPFLAGS.${_f}= 	-I${X11SRCDIR.Mesa}/src/mesa \
-			-I${X11SRCDIR.Mesa}/src
+CPPFLAGS.${_f}= 	-I${X11SRCDIR.Mesa}/src/util \
+			-I${X11SRCDIR.Mesa}/src/mesa \
+			-I${X11SRCDIR.Mesa}/src \
+			-DUSE_DRICONF
 .endfor
 
 SRCS+=	${SRCS.loader}
