384332cc4092a0ba2c42285d9a1f5514efc87400
christos <christos@NetBSD.org>
2019-03-10 02:49:52 +0000

    Make the non-standard jemalloc api weak.

--- external/bsd/jemalloc/include/jemalloc/jemalloc.h
+++ external/bsd/jemalloc/include/jemalloc/jemalloc.h
@@ -83,6 +83,29 @@ extern "C" {
 #  define je_valloc valloc
 #endif
 
+#ifdef JEMALLOC_WEAK_NOSTD
+
+#undef je_mallocx
+#undef je_rallocx
+#undef je_xallocx
+#undef je_sallocx
+#undef je_dallocx
+#undef je_sdallocx
+#undef je_nallocx
+
+#undef je_mallctl
+#undef je_mallctltomib
+#undef je_mallctlbymib
+
+#undef je_malloc_stats_print
+#undef je_malloc_usable_size
+
+#undef je_malloc_message
+
+#undef je_malloc_conf
+
+#endif
+
 #include <stdlib.h>
 #include <stdbool.h>
 #include <stdint.h>
