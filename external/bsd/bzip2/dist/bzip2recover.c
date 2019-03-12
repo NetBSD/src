edc311f03e919b89e51636803c8b00fdbdfe5d26
snj <snj@NetBSD.org>
2019-03-12 17:48:03 +0000

    Pull up following revision(s) (requested by martin in ticket #1212): 	external/bsd/bzip2/dist/bzip2recover.c: revision 1.4 Apply fix for CVE-2016-3189 bzip2: heap use after free in bzip2recover

--- external/bsd/bzip2/dist/bzip2recover.c
+++ external/bsd/bzip2/dist/bzip2recover.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: bzip2recover.c,v 1.3 2012/05/07 00:45:47 wiz Exp $	*/
+/*	$NetBSD: bzip2recover.c,v 1.3.28.1 2019/03/12 17:48:03 snj Exp $	*/
 
 /*-----------------------------------------------------------*/
 /*--- Block recoverer program for bzip2                   ---*/
@@ -472,6 +472,7 @@ Int32 main ( Int32 argc, Char** argv )
             bsPutUChar ( bsWr, 0x50 ); bsPutUChar ( bsWr, 0x90 );
             bsPutUInt32 ( bsWr, blockCRC );
             bsClose ( bsWr );
+            outFile = NULL;
          }
          if (wrBlock >= rbCtr) break;
          wrBlock++;
