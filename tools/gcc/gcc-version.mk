e5309b49fe8fc6b8152e1d60799dfa473d1e4d5c
mrg <mrg@NetBSD.org>
2019-03-20 05:09:26 +0000

    bump libstdc++.so.
    unfortunately, we attempted to do this for netbsd-8 but we did
    not actually end up having any ABI incompatibility with that,
    as turning off dual ABI in libstdc++ means *only* enabling the
    old ABI.  however, enabling dual ABI with C11 defaults (ie,
    a forward looking ABI) *is* an ABI-change for some special
    cases and thus we should have bumped this a couple of weeks
    ago when we re-enabled dual ABI.
    
    call this "nb3 20190319".
    
    mknative-gcc regen all switched ports and almost all of the
    unswitched ports.

--- tools/gcc/gcc-version.mk
+++ tools/gcc/gcc-version.mk
@@ -1,9 +1,9 @@
-#	$NetBSD: gcc-version.mk,v 1.15 2019/02/27 09:11:01 mrg Exp $
+#	$NetBSD: gcc-version.mk,v 1.16 2019/03/20 05:09:26 mrg Exp $
 
 # common location for tools and native build
 
 .if ${HAVE_GCC} == 6
 NETBSD_GCC_VERSION=nb4 20181109
 .elif ${HAVE_GCC} == 7
-NETBSD_GCC_VERSION=nb2 20190226
+NETBSD_GCC_VERSION=nb3 20190319
 .endif
