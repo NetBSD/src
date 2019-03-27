6506e0db1f3cc9083aeaaa14f6bf2575a0b82ec3
christos <christos@NetBSD.org>
2019-03-27 17:15:29 +0000

    - expose vmparams to userland on arm, like other archs do. - remove unneeded includes

--- sys/arch/atari/include/vmparam.h
+++ sys/arch/atari/include/vmparam.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: vmparam.h,v 1.32 2018/02/26 15:11:12 tsutsui Exp $	*/
+/*	$NetBSD: vmparam.h,v 1.33 2019/03/27 17:15:29 christos Exp $	*/
 
 /*
  * Copyright (c) 1988 University of Utah.
@@ -40,8 +40,6 @@
 
 #ifndef _MACHINE_VMPARAM_H_
 #define _MACHINE_VMPARAM_H_
-
-#include <machine/pte.h>
 
 /*
  * Machine dependent constants for atari
