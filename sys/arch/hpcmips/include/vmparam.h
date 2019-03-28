251caf1d6201ec0b6493ea97c7ffcc2d07bf4c60
christos <christos@NetBSD.org>
2019-03-28 08:33:07 +0000

    protect kernel variables

--- sys/arch/hpcmips/include/vmparam.h
+++ sys/arch/hpcmips/include/vmparam.h
@@ -1,9 +1,11 @@
-/*	$NetBSD: vmparam.h,v 1.5 2009/12/14 00:46:04 matt Exp $	*/
+/*	$NetBSD: vmparam.h,v 1.6 2019/03/28 08:33:07 christos Exp $	*/
 
 #include <mips/vmparam.h>
+#ifdef _KERNEL
 #include <sys/kcore.h>
 
 #define	VM_PHYSSEG_MAX		5
 
 extern phys_ram_seg_t mem_clusters[];
 extern int mem_cluster_cnt;
+#endif
