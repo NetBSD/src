f36998337f524061a546d0a7c47407ecc27edc53
jmcneill <jmcneill@NetBSD.org>
2019-03-30 13:37:45 +0000

    Switch to efiboot

--- distrib/utils/embedded/files/armv7_boot.cmd
+++ distrib/utils/embedded/files/armv7_boot.cmd
@@ -2,59 +2,19 @@ if test "${board}" = "am335x" ; then
 	setenv kernel netbsd-BEAGLEBONE.ub
 	setenv mmcpart 0:1
 	setenv bootargs root=ld0a
+else
+	setenv use_efi 1
 fi
-if test "${board}" = "de0-nano-soc" ; then
-	setenv kernel netbsd-GENERIC.ub
-	setenv bootargs 'root=ld0a'
-	setenv mmcpart 0:1
-	setenv use_fdt 1
-fi
-if test "${soc}" = "exynos" ; then
-	setenv kernel netbsd-GENERIC.ub
-	setenv bootargs 'root=ld1a'
-	setenv mmcpart 2:1
-	setenv use_fdt 1
-fi
-if test "${soc}" = "sunxi" ; then
-	setenv kernel netbsd-GENERIC.ub
-	setenv bootargs 'root=ld0a'
-	setenv mmcpart 0:1
-	setenv use_fdt 1
-fi
-if test "${soc}" = "tegra" ; then
-	setenv kernel netbsd-GENERIC.ub
-	setenv bootargs root=ld1a
-	setenv mmcpart 1:1
-	setenv use_fdt 1
-fi
-if test "${soc}" = "tegra124" ; then
-	setenv kernel netbsd-GENERIC.ub
-	setenv bootargs root=ld0a
-	setenv mmcpart 1:1
-	setenv use_fdt 1
-fi
+
 if test "${soc}" = "tegra210" ; then
-	setenv kernel netbsd-GENERIC.ub
-	setenv bootargs root=ld0a
-	setenv mmcpart 1:1
-	setenv use_fdt 1
-	setenv fdtfile ${soc}-${board}.dtb
 	# enable PCIe
 	pci enum
 fi
 
-if test "${kernel}" = "" ; then
-	echo '>>>'
-	echo '>>> Target device is not supported by this script.'
-	echo '>>>'
-	exit
-fi
-
-if test "${use_fdt}" = "1" ; then
-	fatload mmc ${mmcpart} ${kernel_addr_r} ${kernel}
-	fatload mmc ${mmcpart} ${fdt_addr_r} ${fdtfile}
-	fdt addr ${fdt_addr_r}
-	bootm ${kernel_addr_r} - ${fdt_addr_r}
+if test "${use_efi}" = "1" ; then
+	setenv boot_scripts
+	setenv boot_script_dhcp
+	run distro_bootcmd
 else
 	fatload mmc ${mmcpart} ${kernel_addr_r} ${kernel}
 	bootm ${kernel_addr_r} ${bootargs}
