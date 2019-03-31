9b14fb01573ade234ef82662ad76be7460c02af5
abhinav <abhinav@NetBSD.org>
2019-03-31 03:04:57 +0000

    Perform quoting of filename completions when there are multiple matches as well
    Quoting of special characters in filename completion was implemented for single match
    case, this enables it for multiple matches as well. For example:
    
    $ touch 'foo bar'
    $ touch 'foo baz'
    $ ls fo<TAB>
    autocompletes to =>
    $ ls foo\ ba
    hitting <TAB> again shows:
    foo bar foo baz
    
    This required unescaping escape sequences generated during last completion
    in order to find the word to complete.
    
    While there, also update the test to include cases for multiple matches.
    
    Reviewed by christos

--- lib/libedit/TEST/test_filecompletion.c
+++ lib/libedit/TEST/test_filecompletion.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: test_filecompletion.c,v 1.3 2018/05/04 16:39:15 abhinav Exp $	*/
+/*	$NetBSD: test_filecompletion.c,v 1.4 2019/03/31 03:04:57 abhinav Exp $	*/
 
 /*-
  * Copyright (c) 2017 Abhinav Upadhyay <abhinav@NetBSD.org>
@@ -45,7 +45,7 @@
 typedef struct {
 	const wchar_t *user_typed_text; /* The actual text typed by the user on the terminal */
 	const char *completion_function_input ; /*the text received by fn_filename_completion_function */
-	const char *expanded_text; /* the value to which completion_function_input should be expanded */
+	const char *expanded_text[2]; /* the value to which completion_function_input should be expanded */
 	const wchar_t *escaped_output; /* expected escaped value of expanded_text */
 } test_input;
 
@@ -54,446 +54,463 @@ static test_input inputs[] = {
 		/* simple test for escaping angular brackets */
 		L"ls ang",
 		"ang",
-		"ang<ular>test",
+		{"ang<ular>test", NULL},
 		L"ls ang\\<ular\\>test "
 	},
 	{
 		/* test angular bracket inside double quotes: ls "dq_ang */
 		L"ls \"dq_ang",
 		"dq_ang",
-		"dq_ang<ular>test",
+		{"dq_ang<ular>test", NULL},
 		L"ls \"dq_ang<ular>test\" "
 	},
 	{
 		/* test angular bracket inside singlq quotes: ls "sq_ang */
 		L"ls 'sq_ang",
 		"sq_ang",
-		"sq_ang<ular>test",
+		{"sq_ang<ular>test", NULL},
 		L"ls 'sq_ang<ular>test' "
 	},
 	{
 		/* simple test for backslash */
 		L"ls back",
 		"back",
-		"backslash\\test",
+		{"backslash\\test", NULL},
 		L"ls backslash\\\\test "
 	},
 	{
 		/* backslash inside single quotes */
 		L"ls 'sback",
 		"sback",
-		"sbackslash\\test",
+		{"sbackslash\\test", NULL},
 		L"ls 'sbackslash\\test' "
 	},
 	{
 		/* backslash inside double quotes */
 		L"ls \"dback",
 		"dback",
-		"dbackslash\\test",
+		{"dbackslash\\test", NULL},
 		L"ls \"dbackslash\\\\test\" "
 	},
 	{
 		/* test braces */
 		L"ls br",
 		"br",
-		"braces{test}",
+		{"braces{test}", NULL},
 		L"ls braces\\{test\\} "
 	},
 	{
 		/* test braces inside single quotes */
 		L"ls 'sbr",
 		"sbr",
-		"sbraces{test}",
+		{"sbraces{test}", NULL},
 		L"ls 'sbraces{test}' "
 	},
 	{
 		/* test braces inside double quotes */
 		L"ls \"dbr",
 		"dbr",
-		"dbraces{test}",
+		{"dbraces{test}", NULL},
 		L"ls \"dbraces{test}\" "
 	},
 	{
 		/* test dollar */
 		L"ls doll",
 		"doll",
-		"doll$artest",
+		{"doll$artest", NULL},
 		L"ls doll\\$artest "
 	},
 	{
 		/* test dollar inside single quotes */
 		L"ls 'sdoll",
 		"sdoll",
-		"sdoll$artest",
+		{"sdoll$artest", NULL},
 		L"ls 'sdoll$artest' "
 	},
 	{
 		/* test dollar inside double quotes */
 		L"ls \"ddoll",
 		"ddoll",
-		"ddoll$artest",
+		{"ddoll$artest", NULL},
 		L"ls \"ddoll\\$artest\" "
 	},
 	{
 		/* test equals */
 		L"ls eq",
 		"eq",
-		"equals==test",
+		{"equals==test", NULL},
 		L"ls equals\\=\\=test "
 	},
 	{
 		/* test equals inside sinqle quotes */
 		L"ls 'seq",
 		"seq",
-		"sequals==test",
+		{"sequals==test", NULL},
 		L"ls 'sequals==test' "
 	},
 	{
 		/* test equals inside double quotes */
 		L"ls \"deq",
 		"deq",
-		"dequals==test",
+		{"dequals==test", NULL},
 		L"ls \"dequals==test\" "
 	},
 	{
 		/* test \n */
 		L"ls new",
 		"new",
-		"new\\nline",
+		{"new\\nline", NULL},
 		L"ls new\\\\nline "
 	},
 	{
 		/* test \n inside single quotes */
 		L"ls 'snew",
 		"snew",
-		"snew\nline",
+		{"snew\nline", NULL},
 		L"ls 'snew\nline' "
 	},
 	{
 		/* test \n inside double quotes */
 		L"ls \"dnew",
 		"dnew",
-		"dnew\nline",
+		{"dnew\nline", NULL},
 		L"ls \"dnew\nline\" "
 	},
 	{
 		/* test single space */
 		L"ls spac",
 		"spac",
-		"space test",
+		{"space test", NULL},
 		L"ls space\\ test "
 	},
 	{
 		/* test single space inside singlq quotes */
 		L"ls 's_spac",
 		"s_spac",
-		"s_space test",
+		{"s_space test", NULL},
 		L"ls 's_space test' "
 	},
 	{
 		/* test single space inside double quotes */
 		L"ls \"d_spac",
 		"d_spac",
-		"d_space test",
+		{"d_space test", NULL},
 		L"ls \"d_space test\" "
 	},
 	{
 		/* test multiple spaces */
 		L"ls multi",
 		"multi",
-		"multi space  test",
+		{"multi space  test", NULL},
 		L"ls multi\\ space\\ \\ test "
 	},
 	{
 		/* test multiple spaces inside single quotes */
 		L"ls 's_multi",
 		"s_multi",
-		"s_multi space  test",
+		{"s_multi space  test", NULL},
 		L"ls 's_multi space  test' "
 	},
 	{
 		/* test multiple spaces inside double quotes */
 		L"ls \"d_multi",
 		"d_multi",
-		"d_multi space  test",
+		{"d_multi space  test", NULL},
 		L"ls \"d_multi space  test\" "
 	},
 	{
 		/* test double quotes */
 		L"ls doub",
 		"doub",
-		"doub\"quotes",
+		{"doub\"quotes", NULL},
 		L"ls doub\\\"quotes "
 	},
 	{
 		/* test double quotes inside single quotes */
 		L"ls 's_doub",
 		"s_doub",
-		"s_doub\"quotes",
+		{"s_doub\"quotes", NULL},
 		L"ls 's_doub\"quotes' "
 	},
 	{
 		/* test double quotes inside double quotes */
 		L"ls \"d_doub",
 		"d_doub",
-		"d_doub\"quotes",
+		{"d_doub\"quotes", NULL},
 		L"ls \"d_doub\\\"quotes\" "
 	},
 	{
 		/* test multiple double quotes */
 		L"ls mud",
 		"mud",
-		"mud\"qu\"otes\"",
+		{"mud\"qu\"otes\"", NULL},
 		L"ls mud\\\"qu\\\"otes\\\" "
 	},
 	{
 		/* test multiple double quotes inside single quotes */
 		L"ls 'smud",
 		"smud",
-		"smud\"qu\"otes\"",
+		{"smud\"qu\"otes\"", NULL},
 		L"ls 'smud\"qu\"otes\"' "
 	},
 	{
 		/* test multiple double quotes inside double quotes */
 		L"ls \"dmud",
 		"dmud",
-		"dmud\"qu\"otes\"",
+		{"dmud\"qu\"otes\"", NULL},
 		L"ls \"dmud\\\"qu\\\"otes\\\"\" "
 	},
 	{
 		/* test one single quote */
 		L"ls sing",
 		"sing",
-		"single'quote",
+		{"single'quote", NULL},
 		L"ls single\\'quote "
 	},
 	{
 		/* test one single quote inside single quote */
 		L"ls 'ssing",
 		"ssing",
-		"ssingle'quote",
+		{"ssingle'quote", NULL},
 		L"ls 'ssingle'\\''quote' "
 	},
 	{
 		/* test one single quote inside double quote */
 		L"ls \"dsing",
 		"dsing",
-		"dsingle'quote",
+		{"dsingle'quote", NULL},
 		L"ls \"dsingle'quote\" "
 	},
 	{
 		/* test multiple single quotes */
 		L"ls mu_sing",
 		"mu_sing",
-		"mu_single''quotes''",
+		{"mu_single''quotes''", NULL},
 		L"ls mu_single\\'\\'quotes\\'\\' "
 	},
 	{
 		/* test multiple single quotes inside single quote */
 		L"ls 'smu_sing",
 		"smu_sing",
-		"smu_single''quotes''",
+		{"smu_single''quotes''", NULL},
 		L"ls 'smu_single'\\'''\\''quotes'\\\'''\\''' "
 	},
 	{
 		/* test multiple single quotes inside double quote */
 		L"ls \"dmu_sing",
 		"dmu_sing",
-		"dmu_single''quotes''",
+		{"dmu_single''quotes''", NULL},
 		L"ls \"dmu_single''quotes''\" "
 	},
 	{
 		/* test parenthesis */
 		L"ls paren",
 		"paren",
-		"paren(test)",
+		{"paren(test)", NULL},
 		L"ls paren\\(test\\) "
 	},
 	{
 		/* test parenthesis inside single quote */
 		L"ls 'sparen",
 		"sparen",
-		"sparen(test)",
+		{"sparen(test)", NULL},
 		L"ls 'sparen(test)' "
 	},
 	{
 		/* test parenthesis inside double quote */
 		L"ls \"dparen",
 		"dparen",
-		"dparen(test)",
+		{"dparen(test)", NULL},
 		L"ls \"dparen(test)\" "
 	},
 	{
 		/* test pipe */
 		L"ls pip",
 		"pip",
-		"pipe|test",
+		{"pipe|test", NULL},
 		L"ls pipe\\|test "
 	},
 	{
 		/* test pipe inside single quote */
 		L"ls 'spip",
 		"spip",
-		"spipe|test",
+		{"spipe|test", NULL},
 		L"ls 'spipe|test' ",
 	},
 	{
 		/* test pipe inside double quote */
 		L"ls \"dpip",
 		"dpip",
-		"dpipe|test",
+		{"dpipe|test", NULL},
 		L"ls \"dpipe|test\" "
 	},
 	{
 		/* test tab */
 		L"ls ta",
 		"ta",
-		"tab\ttest",
+		{"tab\ttest", NULL},
 		L"ls tab\\\ttest "
 	},
 	{
 		/* test tab inside single quote */
 		L"ls 'sta",
 		"sta",
-		"stab\ttest",
+		{"stab\ttest", NULL},
 		L"ls 'stab\ttest' "
 	},
 	{
 		/* test tab inside double quote */
 		L"ls \"dta",
 		"dta",
-		"dtab\ttest",
+		{"dtab\ttest", NULL},
 		L"ls \"dtab\ttest\" "
 	},
 	{
 		/* test back tick */
 		L"ls tic",
 		"tic",
-		"tick`test`",
+		{"tick`test`", NULL},
 		L"ls tick\\`test\\` "
 	},
 	{
 		/* test back tick inside single quote */
 		L"ls 'stic",
 		"stic",
-		"stick`test`",
+		{"stick`test`", NULL},
 		L"ls 'stick`test`' "
 	},
 	{
 		/* test back tick inside double quote */
 		L"ls \"dtic",
 		"dtic",
-		"dtick`test`",
+		{"dtick`test`", NULL},
 		L"ls \"dtick\\`test\\`\" "
 	},
 	{
 		/* test for @ */
 		L"ls at",
 		"at",
-		"atthe@rate",
+		{"atthe@rate", NULL},
 		L"ls atthe\\@rate "
 	},
 	{
 		/* test for @ inside single quote */
 		L"ls 'sat",
 		"sat",
-		"satthe@rate",
+		{"satthe@rate", NULL},
 		L"ls 'satthe@rate' "
 	},
 	{
 		/* test for @ inside double quote */
 		L"ls \"dat",
 		"dat",
-		"datthe@rate",
+		{"datthe@rate", NULL},
 		L"ls \"datthe@rate\" "
 	},
 	{
 		/* test ; */
 		L"ls semi",
 		"semi",
-		"semi;colon;test",
+		{"semi;colon;test", NULL},
 		L"ls semi\\;colon\\;test "
 	},
 	{
 		/* test ; inside single quote */
 		L"ls 'ssemi",
 		"ssemi",
-		"ssemi;colon;test",
+		{"ssemi;colon;test", NULL},
 		L"ls 'ssemi;colon;test' "
 	},
 	{
 		/* test ; inside double quote */
 		L"ls \"dsemi",
 		"dsemi",
-		"dsemi;colon;test",
+		{"dsemi;colon;test", NULL},
 		L"ls \"dsemi;colon;test\" "
 	},
 	{
 		/* test & */
 		L"ls amp",
 		"amp",
-		"ampers&and",
+		{"ampers&and", NULL},
 		L"ls ampers\\&and "
 	},
 	{
 		/* test & inside single quote */
 		L"ls 'samp",
 		"samp",
-		"sampers&and",
+		{"sampers&and", NULL},
 		L"ls 'sampers&and' "
 	},
 	{
 		/* test & inside double quote */
 		L"ls \"damp",
 		"damp",
-		"dampers&and",
+		{"dampers&and", NULL},
 		L"ls \"dampers&and\" "
 	},
 	{
 		/* test completion when cursor at \ */
 		L"ls foo\\",
 		"foo",
-		"foo bar",
+		{"foo bar", NULL},
 		L"ls foo\\ bar "
 	},
 	{
 		/* test completion when cursor at single quote */
 		L"ls foo'",
-		"foo",
-		"foo bar",
+		"foo'",
+		{"foo bar", NULL},
 		L"ls foo\\ bar "
 	},
 	{
 		/* test completion when cursor at double quote */
 		L"ls foo\"",
-		"foo",
-		"foo bar",
+		"foo\"",
+		{"foo bar", NULL},
 		L"ls foo\\ bar "
+	},
+	{
+		/* test multiple completion matches */
+		L"ls fo",
+		"fo",
+		{"foo bar", "foo baz"},
+		L"ls foo\\ ba"
+	},
+	{
+		L"ls ba",
+		"ba",
+		{"bar <bar>", "bar <baz>"},
+		L"ls bar\\ \\<ba"
 	}
 };
 
 static const wchar_t break_chars[] = L" \t\n\"\\'`@$><=;|&{(";
 
 /*
- * Custom completion function passed to fn_complete.
+ * Custom completion function passed to fn_complet, NULLe.
  * The function returns hardcoded completion matches
  * based on the test cases present in inputs[] (above)
  */
 static char *
 mycomplet_func(const char *text, int index)
 {
-	static char *last_input = NULL;
+	static int last_index = 0;
 	size_t i = 0;
-	if (last_input && strcmp(last_input, text) == 0) {
-		free(last_input);
-		last_input = NULL;
+	if (last_index == 2) {
+		last_index = 0;
 		return NULL;
 	}
-	last_input = strdup(text);
 
 	for (i = 0; i < sizeof(inputs)/sizeof(inputs[0]); i++) {
-		if (strcmp(text, inputs[i].completion_function_input) == 0)
-			return strdup(inputs[i].expanded_text);
+		if (strcmp(text, inputs[i].completion_function_input) == 0) {
+			if (inputs[i].expanded_text[last_index] != NULL)
+				return strdup(inputs[i].expanded_text[last_index++]);
+			else {
+				last_index = 0;
+				return NULL;
+			}
+		}
 	}
 
 	return NULL;
