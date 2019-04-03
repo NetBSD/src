7b1807eb8f21cba6b2a25e5fef4cc16cb51c19ad
joerg <joerg@NetBSD.org>
2019-04-03 15:12:44 +0000

    Revert MKLLVMCMDS changes.

--- share/mk/bsd.own.mk
+++ share/mk/bsd.own.mk
@@ -1,4 +1,4 @@
-#	$NetBSD: bsd.own.mk,v 1.1119 2019/03/28 15:45:23 christos Exp $
+#	$NetBSD: bsd.own.mk,v 1.1120 2019/04/03 15:12:45 joerg Exp $
 
 # This needs to be before bsd.init.mk
 .if defined(BSD_MK_COMPAT_FILE)
@@ -1066,7 +1066,7 @@ _MKVARS.yes= \
 	MKGCC MKGDB MKGROFF \
 	MKHESIOD MKHTML \
 	MKIEEEFP MKINET6 MKINFO MKIPFILTER MKISCSI \
-	MKKERBEROS MKLLVMCMDS \
+	MKKERBEROS \
 	MKKMOD \
 	MKLDAP MKLIBSTDCXX MKLINKLIB MKLVM \
 	MKMAN MKMANDOC \
@@ -1227,16 +1227,6 @@ MKXORG_SERVER=yes
 .endif
 
 #
-# MesaLib.old and MesaLib7 go together, and MesaLib is alone.
-#
-HAVE_MESA_VER?=	10
-.if ${HAVE_MESA_VER} == "10"
-EXTERNAL_MESALIB_DIR?=	MesaLib.old
-.else
-EXTERNAL_MESALIB_DIR?=	MesaLib
-.endif
-
-#
 # Force some options off if their dependencies are off.
 #
 
@@ -1296,23 +1286,6 @@ _NEEDS_LIBCXX.x86_64=		yes
 
 .if ${MKLLVM} == "yes" && ${_NEEDS_LIBCXX.${MACHINE_ARCH}:Uno} == "yes"
 MKLIBCXX:=	yes
-.endif
-
-#
-# If we're building X11 (not the default) on x86, and we're using
-# Mesa >= 18, turn on LLVM libs, and maybe turn off building clang.
-#
-.if ${HAVE_MESA_VER} == "18"
-_NEEDS_LLVMLIB.x86_64=		yes
-_NEEDS_LLVMLIB.i386=		yes
-.endif
-
-.if ${MKX11} != "no" && ${_NEEDS_LLVMLIB.${MACHINE_ARCH}:Uno} == "yes"
-MKLLVM:=			yes
-.endif
-
-.if ${HAVE_LLVM:Uno} != "yes"
-MKLLVMCMDS:=			no
 .endif
 
 #
@@ -1485,6 +1458,14 @@ X11SRCDIR.${_proto}proto?=		${X11SRCDIRMIT}/${_proto}proto/dist
 HAVE_XORG_SERVER_VER?=110
 .else
 HAVE_XORG_SERVER_VER?=120
+.endif
+
+# MesaLib.old and MesaLib7 go together, and MesaLib is alone.
+HAVE_MESA_VER?=	10
+.if ${HAVE_MESA_VER} == "10"
+EXTERNAL_MESALIB_DIR?=	MesaLib.old
+.else
+EXTERNAL_MESALIB_DIR?=	MesaLib
 .endif
 
 .if ${HAVE_XORG_SERVER_VER} == "120"
