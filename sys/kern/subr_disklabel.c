b38f312fd80f110a47379004043768929439f49f
christos <christos@NetBSD.org>
2019-04-05 01:10:32 +0000

    we need it for non-kernel builds (rump)

--- sys/kern/subr_disklabel.c
+++ sys/kern/subr_disklabel.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: subr_disklabel.c,v 1.1 2019/04/04 20:19:07 christos Exp $	*/
+/*	$NetBSD: subr_disklabel.c,v 1.2 2019/04/05 01:10:32 christos Exp $	*/
 
 /*
  * Copyright (c) 1982, 1986, 1988 Regents of the University of California.
@@ -32,13 +32,13 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: subr_disklabel.c,v 1.1 2019/04/04 20:19:07 christos Exp $");
+__KERNEL_RCSID(0, "$NetBSD: subr_disklabel.c,v 1.2 2019/04/05 01:10:32 christos Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
 #include <sys/disklabel.h>
 
-#ifndef __HAVE_SETDISKLABEL
+#if !defined(__HAVE_SETDISKLABEL) || !defined(_KERNEL)
 
 #ifdef DEBUG
 #define DPRINTF(a, ...) printf(a, ##__VA_ARGS__)
