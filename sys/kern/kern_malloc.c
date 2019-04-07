a6d5868716e6305c9a6887896b8e7cfa8a78c234
maxv <maxv@NetBSD.org>
2019-04-07 09:20:04 +0000

    Provide a code argument in kasan_mark(), and give a code to each caller. Five codes used: GenericRedZone, MallocRedZone, KmemRedZone, PoolRedZone, and PoolUseAfterFree.
    This can greatly help debugging complex memory corruptions.

--- sys/kern/kern_malloc.c
+++ sys/kern/kern_malloc.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: kern_malloc.c,v 1.156 2019/03/07 18:32:10 maxv Exp $	*/
+/*	$NetBSD: kern_malloc.c,v 1.157 2019/04/07 09:20:04 maxv Exp $	*/
 
 /*
  * Copyright (c) 1987, 1991, 1993
@@ -70,7 +70,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: kern_malloc.c,v 1.156 2019/03/07 18:32:10 maxv Exp $");
+__KERNEL_RCSID(0, "$NetBSD: kern_malloc.c,v 1.157 2019/04/07 09:20:04 maxv Exp $");
 
 #include <sys/param.h>
 #include <sys/malloc.h>
@@ -139,7 +139,7 @@ kern_malloc(unsigned long reqsize, int flags)
 #endif
 	mh++;
 
-	kasan_mark(mh, origsize, size);
+	kasan_mark(mh, origsize, size, KASAN_MALLOC_REDZONE);
 
 	return mh;
 }
@@ -153,7 +153,7 @@ kern_free(void *addr)
 	mh--;
 
 	kasan_mark(addr, mh->mh_size - sizeof(struct malloc_header),
-	    mh->mh_size - sizeof(struct malloc_header));
+	    mh->mh_size - sizeof(struct malloc_header), KASAN_MALLOC_REDZONE);
 
 	if (mh->mh_size >= PAGE_SIZE + sizeof(struct malloc_header))
 		kmem_intr_free((char *)addr - PAGE_SIZE,
