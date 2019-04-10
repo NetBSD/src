fbb489fb5233342e17b37a0bb2b3d89524a98411
maxv <maxv@NetBSD.org>
2019-04-10 18:49:04 +0000

    Add the NVMM_CTL ioctl, always privileged regardless of the permissions of /dev/nvmm. We'll use it to provide a way for an admin to control the registered VMs in the kernel.
    Add an associated wrapper in libnvmm.

--- sys/secmodel/suser/secmodel_suser.c
+++ sys/secmodel/suser/secmodel_suser.c
@@ -1,4 +1,4 @@
-/* $NetBSD: secmodel_suser.c,v 1.49 2018/10/05 22:12:38 christos Exp $ */
+/* $NetBSD: secmodel_suser.c,v 1.50 2019/04/10 18:49:04 maxv Exp $ */
 /*-
  * Copyright (c) 2006 Elad Efrat <elad@NetBSD.org>
  * All rights reserved.
@@ -38,7 +38,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: secmodel_suser.c,v 1.49 2018/10/05 22:12:38 christos Exp $");
+__KERNEL_RCSID(0, "$NetBSD: secmodel_suser.c,v 1.50 2019/04/10 18:49:04 maxv Exp $");
 
 #include <sys/types.h>
 #include <sys/param.h>
@@ -893,6 +893,7 @@ secmodel_suser_device_cb(kauth_cred_t cred, kauth_action_t action,
 	case KAUTH_DEVICE_RND_SETPRIV:
 	case KAUTH_DEVICE_WSCONS_KEYBOARD_BELL:
 	case KAUTH_DEVICE_WSCONS_KEYBOARD_KEYREPEAT:
+	case KAUTH_DEVICE_NVMM_CTL:
 		if (isroot)
 			result = KAUTH_RESULT_ALLOW;
 		break;
