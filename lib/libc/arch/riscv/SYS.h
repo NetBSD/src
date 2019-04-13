5098e87f25af3c2483b73435424d8f0d6c011cb3
maya <maya@NetBSD.org>
2019-04-13 16:08:54 +0000

    Looks like libc is too big to be able to use j. use a longer distance jump

--- lib/libc/arch/riscv/SYS.h
+++ lib/libc/arch/riscv/SYS.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: SYS.h,v 1.2 2015/03/27 06:44:28 matt Exp $ */
+/*	$NetBSD: SYS.h,v 1.3 2019/04/13 16:08:54 maya Exp $ */
 
 /*-
  * Copyright (c) 2014 The NetBSD Foundation, Inc.
@@ -33,7 +33,7 @@
 #include <machine/asm.h>
 
 #define SYSTRAP(x)		li	t6,SYS_ ## x; scall
-#define	JUMP_TO_CERROR()	j	_C_LABEL(__cerror)
+#define	JUMP_TO_CERROR()	tail	_C_LABEL(__cerror)
 
 /*
  * Do a syscall that cannot fail (sync, get{p,u,g,eu,eg)id)
