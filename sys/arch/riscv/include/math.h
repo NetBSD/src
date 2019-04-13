bb6fd57a8e7bcd6cbc1ce525e0f5cfd5bc699790
maya <maya@NetBSD.org>
2019-04-13 15:57:31 +0000

    Our current configuration is that long double is 128bit, so reflect that in the relevant headers.
    Taken from sparc64.

--- sys/arch/riscv/include/math.h
+++ sys/arch/riscv/include/math.h
@@ -1,3 +1,7 @@
-/* $NetBSD: math.h,v 1.1 2014/09/19 17:36:26 matt Exp $ */
+/* $NetBSD: math.h,v 1.2 2019/04/13 15:57:31 maya Exp $ */
 
 #define __HAVE_NANF
+
+#if defined(_LP64) || defined(_KERNEL)
+#define	__HAVE_LONG_DOUBLE	128
+#endif
