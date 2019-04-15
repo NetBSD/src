38035b58de353067f927a17c6ed9899cbf3cdfa2
maya <maya@NetBSD.org>
2019-04-15 21:13:36 +0000

    Remove unused definition.

--- external/gpl3/gcc/dist/gcc/config/riscv/netbsd.h
+++ external/gpl3/gcc/dist/gcc/config/riscv/netbsd.h
@@ -20,9 +20,6 @@ along with this program; see the file COPYING.  If not, write to
 the Free Software Foundation, 59 Temple Place - Suite 330,
 Boston, MA 02111-1307, USA.  */
 
-#undef TARGET_USE_GP
-#define TARGET_USE_GP 0
-
 #undef DRIVER_SELF_SPECS
 #define DRIVER_SELF_SPECS ""
 
