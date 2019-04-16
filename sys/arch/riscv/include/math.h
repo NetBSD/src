a64e86819acd0d4ff000d9e8e00f655247418e5b
maya <maya@NetBSD.org>
2019-04-16 07:40:02 +0000

    RISC-V ELF psABI says ILP32 also defaults to 128bit long double.

--- sys/arch/riscv/include/math.h
+++ sys/arch/riscv/include/math.h
@@ -1,7 +1,4 @@
-/* $NetBSD: math.h,v 1.2 2019/04/13 15:57:31 maya Exp $ */
+/* $NetBSD: math.h,v 1.3 2019/04/16 07:40:02 maya Exp $ */
 
 #define __HAVE_NANF
-
-#if defined(_LP64) || defined(_KERNEL)
 #define	__HAVE_LONG_DOUBLE	128
-#endif
