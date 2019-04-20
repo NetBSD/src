48281ab9993f1055b53660bb6403e11718a9bffe
christos <christos@NetBSD.org>
2019-04-20 17:28:19 +0000

    put back hpn/lpk strings

--- crypto/external/bsd/openssh/dist/version.h
+++ crypto/external/bsd/openssh/dist/version.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: version.h,v 1.27 2019/04/20 17:16:40 christos Exp $	*/
+/*	$NetBSD: version.h,v 1.28 2019/04/20 17:28:19 christos Exp $	*/
 /* $OpenBSD: version.h,v 1.84 2019/04/03 15:48:45 djm Exp $ */
 
 #define __OPENSSH_VERSION	"OpenSSH_8.0"
@@ -9,5 +9,5 @@
  * it is important to retain OpenSSH version identification part, it is
  * used for bug compatibility operation.  present NetBSD SSH version as comment
  */
-#define SSH_VERSION	__OPENSSH_VERSION " " __NETBSDSSH_VERSION
+#define SSH_VERSION	__OPENSSH_VERSION " " __NETBSDSSH_VERSION SSH_HPN SSH_LPK
 #define SSH_RELEASE SSH_VERSION SSH_HPN SSH_LPK
