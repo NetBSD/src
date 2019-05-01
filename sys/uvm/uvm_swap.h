1ecc86347cfc0ddfcb5e91381523473eba55a467
mlelstv <mlelstv@NetBSD.org>
2019-05-01 07:17:18 +0000

    allow NONE build

--- sys/uvm/uvm_swap.h
+++ sys/uvm/uvm_swap.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: uvm_swap.h,v 1.24 2018/03/15 03:21:58 christos Exp $	*/
+/*	$NetBSD: uvm_swap.h,v 1.25 2019/05/01 07:17:18 mlelstv Exp $	*/
 
 /*
  * Copyright (c) 1997 Matthew R. Green
@@ -57,7 +57,7 @@ int	uvm_swap_stats(char *, int,
 
 #else /* defined(VMSWAP) */
 #define	uvm_swapisfull()	true
-#define uvm_swap_stats(c, l, f, count, retval) (*retval = 0, ENOSYS)
+#define uvm_swap_stats(c, l, f, count, retval) (__used f, *retval = 0, ENOSYS)
 #endif /* defined(VMSWAP) */
 
 void	uvm_swap_shutdown(struct lwp *);
