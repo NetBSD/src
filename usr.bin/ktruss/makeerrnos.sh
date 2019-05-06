453514a2806e2e10be61aa2983cad9ebf6b4175c
kamil <kamil@NetBSD.org>
2019-05-06 11:58:37 +0000

    Add support for SIGRTMIN+1..SIGRTMAX-1 signals in ktruss(1)
    Generate misc.c and misc.h with entries for signals between SIGRTMIN+1
    and SIGRTMAX-1.

--- usr.bin/ktruss/makeerrnos.sh
+++ usr.bin/ktruss/makeerrnos.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 #
-#	$NetBSD: makeerrnos.sh,v 1.6 2012/03/12 22:02:07 dyoung Exp $
+#	$NetBSD: makeerrnos.sh,v 1.7 2019/05/06 11:58:37 kamil Exp $
 
 if [ $# -ne 3 ]; then
 	echo "usage: makeerrnos.sh errno.h signal.h output"
@@ -53,10 +53,22 @@ ${AWK} '
 			break;
 	i++;
 	j = i + 1;
+
+	if ($i == "SIGRTMIN") {
+		sigrtmin=$j;
+	}
+	if ($i == "SIGRTMAX") {
+		sigrtmax=$j;
+	}
+
 	#
 	printf("\t{ \"%s\", %s },\n", $i, $j);
 }
 END {
+	j = 1;
+	for (i = sigrtmin + 1; i < sigrtmax; i++) {
+		printf("\t{ \"SIGRTMIN+%s\", %s },\n", j++, i);
+	}
 	print "	{ \"0\", 0 },\n";
 }
 ' | sort -n -k 3 >> $CFILE
