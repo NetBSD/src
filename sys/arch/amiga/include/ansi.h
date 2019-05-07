0c3e67621c2b4336a1a5f99dd90ac827a2681f55
kamil <kamil@NetBSD.org>
2019-05-07 03:49:25 +0000

    Switch all users (except ia64) of custom machine/ansi.h to common_ansi.h
    Deduplicate the code among ports and poll definitions of types
    directly from a compiler.
    
    This fixes miscompilation of certain programs that instruct compilers
    to generate code for different types. This bug has been detected with
    -fshort-wchar in EFI firmware.
    
    Proposed and discussed on a mailing list (twice).
    
    Itanium uses custom !ELF fallback switch, temporarily leave it as it is.

--- sys/arch/amiga/include/ansi.h
+++ sys/arch/amiga/include/ansi.h
@@ -1,8 +1,3 @@
-/*	$NetBSD: ansi.h,v 1.8 1994/10/26 02:05:55 cgd Exp $	*/
-
-#ifndef _MACHINE_ANSI_H_
-#define _MACHINE_ANSI_H_
+/*	$NetBSD: ansi.h,v 1.9 2019/05/07 03:49:26 kamil Exp $	*/
 
 #include <m68k/ansi.h>
-
-#endif
