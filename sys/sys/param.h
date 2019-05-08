bf05d12f5a41499085ec3bac2e2c6bcf213146de
isaki <isaki@NetBSD.org>
2019-05-08 13:47:33 +0000

    Merge isaki-audio2 branch.
    Welcome to 8.99.39.

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.586 2019/05/06 08:07:41 kamil Exp $	*/
+/*	$NetBSD: param.h,v 1.587 2019/05/08 13:47:33 isaki Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -67,7 +67,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	899003800	/* NetBSD 8.99.38 */
+#define	__NetBSD_Version__	899003900	/* NetBSD 8.99.39 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
