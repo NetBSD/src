71e94c2d3cf44c7680573e8cf497185b2667b436
ozaki-r <ozaki-r@NetBSD.org>
2019-05-13 07:54:52 +0000

    Bump the version for entry additions to IP_STAT and IP6_STAT
    Welcome to 8.99.40

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.587 2019/05/08 13:47:33 isaki Exp $	*/
+/*	$NetBSD: param.h,v 1.588 2019/05/13 07:54:52 ozaki-r Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -67,7 +67,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	899003900	/* NetBSD 8.99.39 */
+#define	__NetBSD_Version__	899004000	/* NetBSD 8.99.40 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
