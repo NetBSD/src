2123d0f2c0f859c12f2333568be2658cf269a4cf
martin <martin@NetBSD.org>
2019-05-14 17:21:58 +0000

    Welcome to 8.1_RC1!

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.542.2.5 2018/07/26 18:47:47 snj Exp $	*/
+/*	$NetBSD: param.h,v 1.542.2.6 2019/05/14 17:21:58 martin Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -67,7 +67,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	800000000	/* NetBSD 8.0_STABLE */
+#define	__NetBSD_Version__	801000000	/* NetBSD 8.1_RC1 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
