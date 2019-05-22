d4ce352db255845be81f161a521bb692f310e870
martin <martin@NetBSD.org>
2019-05-22 04:17:39 +0000

    Relax test for valid stack frame address (now that we have multiple kernel stacks since ... a long time). Just test for a valid kernel VA, using the same test as for "pc". Very close to a patch suggested by Tobias Ulmer in PR port-sparc64/54221.

--- sys/arch/sparc64/sparc64/db_trace.c
+++ sys/arch/sparc64/sparc64/db_trace.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: db_trace.c,v 1.50 2013/03/04 20:17:46 christos Exp $ */
+/*	$NetBSD: db_trace.c,v 1.51 2019/05/22 04:17:39 martin Exp $ */
 
 /*
  * Copyright (c) 1996-2002 Eduardo Horvath.  All rights reserved.
@@ -28,7 +28,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: db_trace.c,v 1.50 2013/03/04 20:17:46 christos Exp $");
+__KERNEL_RCSID(0, "$NetBSD: db_trace.c,v 1.51 2019/05/22 04:17:39 martin Exp $");
 
 #include <sys/param.h>
 #include <sys/proc.h>
@@ -163,7 +163,7 @@ db_stack_trace_print(db_expr_t addr, bool have_addr, db_expr_t count,
 		if (kernel_only) {
 			if (pc < KERNBASE || pc >= KERNEND)
 				break;
-			if (frame < KERNBASE || frame >= EINTSTACK)
+			if (frame < KERNBASE || frame >= KERNEND)
 				break;
 		} else {
 			if (frame == 0 || frame == (vaddr_t)-1)
