13deebddb073eba9189530967af18d38eeb26a31
nonaka <nonaka@NetBSD.org>
2019-05-24 14:28:48 +0000

    Added drivers for Hyper-V Synthetic Keyboard and Video device.

---
+++ sys/dev/hyperv/hvkbdvar.h
@@ -0,0 +1,8 @@
+/*	$NetBSD: hvkbdvar.h,v 1.1 2019/05/24 14:28:48 nonaka Exp $	*/
+
+#ifndef _DEV_HYPERV_HVKBDVAR_H_
+#define _DEV_HYPERV_HVKBDVAR_H_
+
+int	hvkbd_cnattach(void);
+
+#endif /* _DEV_HYPERV_HVKBDVAR_H_ */
