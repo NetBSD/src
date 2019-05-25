5d6d802b499463ae0cabab2560e63cb0a888ffc2
kamil <kamil@NetBSD.org>
2019-05-25 03:25:08 +0000

    Add new user_va0_disable* tests in t_ptrace_wait*
    Add tests:
     - user_va0_disable_pt_continue
     - user_va0_disable_pt_syscall
     - user_va0_disable_pt_detach
    
    Assert that setting PC to 0x0 in PT_CONTINUE/PT_SYSCALL/PT_DETACH for
    vm.user_va0_disable==0 is disallowed.

--- tests/lib/libc/sys/t_ptrace_wait.c
+++ tests/lib/libc/sys/t_ptrace_wait.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: t_ptrace_wait.c,v 1.121 2019/05/09 13:07:35 mgorny Exp $	*/
+/*	$NetBSD: t_ptrace_wait.c,v 1.122 2019/05/25 03:25:08 kamil Exp $	*/
 
 /*-
  * Copyright (c) 2016, 2017, 2018, 2019 The NetBSD Foundation, Inc.
@@ -27,7 +27,7 @@
  */
 
 #include <sys/cdefs.h>
-__RCSID("$NetBSD: t_ptrace_wait.c,v 1.121 2019/05/09 13:07:35 mgorny Exp $");
+__RCSID("$NetBSD: t_ptrace_wait.c,v 1.122 2019/05/25 03:25:08 kamil Exp $");
 
 #include <sys/param.h>
 #include <sys/types.h>
@@ -7438,6 +7438,94 @@ TRACEME_VFORK_CLONE_TEST(traceme_vfork_clone_vfork, CLONE_VFORK)
 
 /// ----------------------------------------------------------------------------
 
+static void
+user_va0_disable(int operation)
+{
+	pid_t child, wpid;
+#if defined(TWAIT_HAVE_STATUS)
+	int status;
+#endif
+	const int sigval = SIGSTOP;
+	int rv;
+
+	struct ptrace_siginfo info;
+
+	if (get_user_va0_disable() == 0)
+		atf_tc_skip("vm.user_va0_disable is set to 0");
+
+	memset(&info, 0, sizeof(info));
+
+	DPRINTF("Before forking process PID=%d\n", getpid());
+	SYSCALL_REQUIRE((child = fork()) != -1);
+	if (child == 0) {
+		DPRINTF("Before calling PT_TRACE_ME from child %d\n", getpid());
+		FORKEE_ASSERT(ptrace(PT_TRACE_ME, 0, NULL, 0) != -1);
+
+		DPRINTF("Before raising %s from child\n", strsignal(sigval));
+		FORKEE_ASSERT(raise(sigval) == 0);
+
+		/* NOTREACHED */
+		FORKEE_ASSERTX(0 && "This shall not be reached");
+		__unreachable();
+	}
+	DPRINTF("Parent process PID=%d, child's PID=%d\n", getpid(), child);
+
+	DPRINTF("Before calling %s() for the child\n", TWAIT_FNAME);
+	TWAIT_REQUIRE_SUCCESS(wpid = TWAIT_GENERIC(child, &status, 0), child);
+
+	validate_status_stopped(status, sigval);
+
+	DPRINTF("Before calling ptrace(2) with PT_GET_SIGINFO for "
+		"child\n");
+	SYSCALL_REQUIRE(ptrace(PT_GET_SIGINFO, child, &info,
+		sizeof(info)) != -1);
+
+	DPRINTF("Signal traced to lwpid=%d\n", info.psi_lwpid);
+	DPRINTF("Signal properties: si_signo=%#x si_code=%#x "
+		"si_errno=%#x\n",
+		info.psi_siginfo.si_signo, info.psi_siginfo.si_code,
+		info.psi_siginfo.si_errno);
+
+	ATF_REQUIRE_EQ(info.psi_siginfo.si_signo, sigval);
+	ATF_REQUIRE_EQ(info.psi_siginfo.si_code, SI_LWP);
+
+	DPRINTF("Before resuming the child process in PC=0x0 "
+	    "and without signal to be sent\n");
+	errno = 0;
+	rv = ptrace(operation, child, (void *)0, 0);
+	ATF_REQUIRE_EQ(errno, EINVAL);
+	ATF_REQUIRE_EQ(rv, -1);
+
+	SYSCALL_REQUIRE(ptrace(PT_KILL, child, NULL, 0) != -1);
+
+	DPRINTF("Before calling %s() for the child\n", TWAIT_FNAME);
+	TWAIT_REQUIRE_SUCCESS(wpid = TWAIT_GENERIC(child, &status, 0), child);
+	validate_status_signaled(status, SIGKILL, 0);
+
+	DPRINTF("Before calling %s() for the child\n", TWAIT_FNAME);
+	TWAIT_REQUIRE_FAILURE(ECHILD, wpid = TWAIT_GENERIC(child, &status, 0));
+}
+
+#define USER_VA0_DISABLE(test, operation)				\
+ATF_TC(test);								\
+ATF_TC_HEAD(test, tc)							\
+{									\
+	atf_tc_set_md_var(tc, "descr",					\
+	    "Verify behavior of " #operation " with PC set to 0x0");	\
+}									\
+									\
+ATF_TC_BODY(test, tc)							\
+{									\
+									\
+	user_va0_disable(operation);					\
+}
+
+USER_VA0_DISABLE(user_va0_disable_pt_continue, PT_CONTINUE)
+USER_VA0_DISABLE(user_va0_disable_pt_syscall, PT_SYSCALL)
+USER_VA0_DISABLE(user_va0_disable_pt_detach, PT_DETACH)
+
+/// ----------------------------------------------------------------------------
+
 #include "t_ptrace_amd64_wait.h"
 #include "t_ptrace_i386_wait.h"
 #include "t_ptrace_x86_wait.h"
@@ -7885,6 +7973,10 @@ ATF_TP_ADD_TCS(tp)
 //	ATF_TP_ADD_TC_HAVE_PID(tp, traceme_vfork_clone_sighand); // XXX
 	ATF_TP_ADD_TC_HAVE_PID(tp, traceme_vfork_clone_vfork);
 #endif
+
+	ATF_TP_ADD_TC(tp, user_va0_disable_pt_continue);
+	ATF_TP_ADD_TC(tp, user_va0_disable_pt_syscall);
+	ATF_TP_ADD_TC(tp, user_va0_disable_pt_detach);
 
 	ATF_TP_ADD_TCS_PTRACE_WAIT_AMD64();
 	ATF_TP_ADD_TCS_PTRACE_WAIT_I386();
