447285b9eaa10452f58c9774e802b31a9f301070
tnn <tnn@NetBSD.org>
2019-05-30 07:37:17 +0000

    - include uvm.h before uvm_device.h - don't need IPL_SCHED here

--- sys/dev/ic/ssdfb.c
+++ sys/dev/ic/ssdfb.c
@@ -1,4 +1,4 @@
-/* $NetBSD: ssdfb.c,v 1.3 2019/03/17 04:03:17 tnn Exp $ */
+/* $NetBSD: ssdfb.c,v 1.4 2019/05/30 07:37:17 tnn Exp $ */
 
 /*
  * Copyright (c) 2019 The NetBSD Foundation, Inc.
@@ -30,13 +30,14 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: ssdfb.c,v 1.3 2019/03/17 04:03:17 tnn Exp $");
+__KERNEL_RCSID(0, "$NetBSD: ssdfb.c,v 1.4 2019/05/30 07:37:17 tnn Exp $");
 
 #include "opt_ddb.h"
 
 #include <sys/param.h>
 #include <sys/kernel.h>
 #include <sys/conf.h>
+#include <uvm/uvm.h>
 #include <uvm/uvm_page.h>
 #include <uvm/uvm_device.h>
 #include <sys/condvar.h>
@@ -300,7 +301,7 @@ ssdfb_attach(struct ssdfb_softc *sc, int flags)
 	if (sc->sc_is_console)
 		ssdfb_set_usepoll(sc, true);
 
-	mutex_init(&sc->sc_cond_mtx, MUTEX_DEFAULT, IPL_SCHED);
+	mutex_init(&sc->sc_cond_mtx, MUTEX_DEFAULT, IPL_VM);
 	cv_init(&sc->sc_cond, "ssdfb");
 	error = kthread_create(PRI_SOFTCLOCK, KTHREAD_MPSAFE | KTHREAD_MUSTJOIN,
 	    NULL, ssdfb_thread, sc, &sc->sc_thread, "%s",
@@ -989,7 +990,7 @@ ssdfb_pick_font(int *cookiep, struct wsdisplay_font **fontp)
 	uint8_t d[4][2] = {{5, 8}, {8, 8}, {8, 10} ,{8, 16}};
 
 	/*
-	 * Try to find fonts in order of inreasing size.
+	 * Try to find fonts in order of increasing size.
 	 */
 	wsfont_init();
 	for(i = 0; i < __arraycount(d); i++) {
