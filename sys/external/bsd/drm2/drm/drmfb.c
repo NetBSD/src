6f50e35b3982e3c4985fb2bc4603c6342cf12bbf
jmcneill <jmcneill@NetBSD.org>
2019-05-31 20:25:58 +0000

    Check property length, not array (fixes clang build)

--- sys/external/bsd/drm2/drm/drmfb.c
+++ sys/external/bsd/drm2/drm/drmfb.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: drmfb.c,v 1.6 2019/05/31 10:47:48 jmcneill Exp $	*/
+/*	$NetBSD: drmfb.c,v 1.7 2019/05/31 20:25:58 jmcneill Exp $	*/
 
 /*-
  * Copyright (c) 2014 The NetBSD Foundation, Inc.
@@ -39,7 +39,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: drmfb.c,v 1.6 2019/05/31 10:47:48 jmcneill Exp $");
+__KERNEL_RCSID(0, "$NetBSD: drmfb.c,v 1.7 2019/05/31 20:25:58 jmcneill Exp $");
 
 #ifdef _KERNEL_OPT
 #include "vga.h"
@@ -140,7 +140,7 @@ drmfb_attach(struct drmfb_softc *sc, const struct drmfb_attach_args *da)
 		struct drm_connector *connector =
 		    da->da_fb_helper->connector_info[n]->connector;
 		struct drm_property_blob *edid = connector->edid_blob_ptr;
-		if (edid && edid->data) {
+		if (edid && edid->length) {
 			prop_data_t edid_data =
 			    prop_data_create_data(edid->data, edid->length);
 			prop_dictionary_set(dict, "EDID", edid_data);
