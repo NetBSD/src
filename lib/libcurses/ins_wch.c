2a780e62ed82cbe6862249cad03fe76de02d3db8
blymn <blymn@NetBSD.org>
2019-06-09 07:40:14 +0000

    Rework previous fix for getch cursor position when cursor is moved without refresh.  If the window is not dirty but the window cursor position does not match curscr then move the cursor.  This fixes the issues seen in PR lib/54263.

--- lib/libcurses/ins_wch.c
+++ lib/libcurses/ins_wch.c
@@ -1,4 +1,4 @@
-/*   $NetBSD: ins_wch.c,v 1.13 2019/05/20 22:17:41 blymn Exp $ */
+/*   $NetBSD: ins_wch.c,v 1.14 2019/06/09 07:40:14 blymn Exp $ */
 
 /*
  * Copyright (c) 2005 The NetBSD Foundation Inc.
@@ -36,7 +36,7 @@
 
 #include <sys/cdefs.h>
 #ifndef lint
-__RCSID("$NetBSD: ins_wch.c,v 1.13 2019/05/20 22:17:41 blymn Exp $");
+__RCSID("$NetBSD: ins_wch.c,v 1.14 2019/06/09 07:40:14 blymn Exp $");
 #endif						  /* not lint */
 
 #include <string.h>
@@ -72,7 +72,7 @@ mvins_wch(int y, int x, const cchar_t *wch)
 int
 mvwins_wch(WINDOW *win, int y, int x, const cchar_t *wch)
 {
-	if (_cursesi_wmove(win, y, x, 0) == ERR)
+	if (wmove(win, y, x) == ERR)
 		return ERR;
 
 	return wins_wch(stdscr, wch);
