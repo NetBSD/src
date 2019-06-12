02d94452d92062c9a57fd0aff6e7c062cab77561
martin <martin@NetBSD.org>
2019-06-12 11:35:40 +0000

    Missing files from commit for ticket #1280:
            sys/arch/x86/x86/hypervvar.h: revision 1.1
            sys/dev/hyperv/genfb_vmbus.c: revision 1.1
            sys/dev/hyperv/genfb_vmbusvar.h: revision 1.1
            sys/dev/hyperv/hvkbd.c: revision 1.1
            sys/dev/hyperv/hvkbdvar.h: revision 1.1
    
    Added drivers for Hyper-V Synthetic Keyboard and Video device.
    
    Avoid undefined reference to `hyperv_guid_video' without vmbus(4).
    
    Avoid undefined reference to `hyperv_is_gen1' without hyperv(4).
    
    Use efi_probe().

--- sys/dev/hyperv/hvkbdvar.h
+++ sys/dev/hyperv/hvkbdvar.h
@@ -1,4 +1,12 @@
-/*	$NetBSD: hvkbdvar.h,v 1.1.4.2 2019/06/12 10:17:32 martin Exp $	*/
+/*	$NetBSD: hvkbdvar.h,v 1.1.4.3 2019/06/12 11:35:40 martin Exp $	*/
+
+#ifndef _DEV_HYPERV_HVKBDVAR_H_
+#define _DEV_HYPERV_HVKBDVAR_H_
+
+int	hvkbd_cnattach(void);
+
+#endif /* _DEV_HYPERV_HVKBDVAR_H_ */
+/*	$NetBSD: hvkbdvar.h,v 1.1.4.3 2019/06/12 11:35:40 martin Exp $	*/
 
 #ifndef _DEV_HYPERV_HVKBDVAR_H_
 #define _DEV_HYPERV_HVKBDVAR_H_
