2eb952e453c36716a42d3c20aa5881f68502d78b
kamil <kamil@NetBSD.org>
2019-06-18 22:41:32 +0000

    Welcome 8.99.46!
    do_sys_mknod{,at}() changes
    quota modules refactoring

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.593 2019/06/13 20:23:56 kamil Exp $	*/
+/*	$NetBSD: param.h,v 1.594 2019/06/18 22:41:32 kamil Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -67,7 +67,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	899004500	/* NetBSD 8.99.45 */
+#define	__NetBSD_Version__	899004600	/* NetBSD 8.99.46 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
