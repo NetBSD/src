4d79f33340decaed1b0a9b5e607ee733f3ac6d7a
mrg <mrg@NetBSD.org>
2019-06-20 10:29:53 +0000

    delete GCC 6.5.  every platform has been on GCC 7 for a while now.

--- external/gpl3/gcc.old/dist/libgcc/config/stormy16/umodsi3.c
+++
@@ -1,2 +0,0 @@
-#define XSTORMY16_UMODSI3
-#include "lib2funcs.c"
--- external/gpl3/gcc.old/lib/crtstuff/crtbegin.c
+++
@@ -1,9 +0,0 @@
-/*	$NetBSD: crtbegin.c,v 1.6 2018/11/11 22:49:47 mrg Exp $	*/
-
-/*
- * Written by Jason R. Thorpe, August 24, 2004.
- * Public domain.
- */
-
-#define	CRT_BEGIN
-#include "crtstuff.c"
