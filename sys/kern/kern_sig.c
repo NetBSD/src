abaf80cc7583085037f3e301b2e71e346c89bbd5
kamil <kamil@NetBSD.org>
2019-06-21 04:28:12 +0000

    Revert previous
    There is fallout in gdb that will be investigated before relanding this.

--- sys/kern/kern_sig.c
+++ sys/kern/kern_sig.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: kern_sig.c,v 1.363 2019/06/21 04:02:57 kamil Exp $	*/
+/*	$NetBSD: kern_sig.c,v 1.364 2019/06/21 04:28:12 kamil Exp $	*/
 
 /*-
  * Copyright (c) 2006, 2007, 2008 The NetBSD Foundation, Inc.
@@ -70,7 +70,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: kern_sig.c,v 1.363 2019/06/21 04:02:57 kamil Exp $");
+__KERNEL_RCSID(0, "$NetBSD: kern_sig.c,v 1.364 2019/06/21 04:28:12 kamil Exp $");
 
 #include "opt_ptrace.h"
 #include "opt_dtrace.h"
@@ -913,7 +913,6 @@ trapsignal(struct lwp *l, ksiginfo_t *ksi)
 	mutex_enter(proc_lock);
 	mutex_enter(p->p_lock);
 
-repeat:
 	/*
 	 * If we are exiting, demise now.
 	 *
@@ -925,16 +924,6 @@ repeat:
 		lwp_exit(l);
 		panic("trapsignal");
 		/* NOTREACHED */
-	}
-
-	/*
-	 * The process is already stopping.
-	 */
-	if ((p->p_sflag & PS_STOPPING) != 0) {
-		sigswitch(0, p->p_xsig, false);
-		mutex_enter(proc_lock);
-		mutex_enter(p->p_lock);
-		goto repeat; /* XXX */
 	}
 
 	mask = &l->l_sigmask;
@@ -1591,7 +1580,6 @@ eventswitch(int code)
 	KASSERT((code == TRAP_CHLD) || (code == TRAP_LWP) ||
 	        (code == TRAP_EXEC));
 
-repeat:
 	/*
 	 * If we are exiting, demise now.
 	 *
@@ -1613,16 +1601,6 @@ repeat:
 		mutex_exit(p->p_lock);
 		mutex_exit(proc_lock);
 		return;
-	}
-
-	/*
-	 * The process is already stopping.
-	 */
-	if ((p->p_sflag & PS_STOPPING) != 0) {
-		sigswitch(0, p->p_xsig, false);
-		mutex_enter(proc_lock);
-		mutex_enter(p->p_lock);
-		goto repeat; /* XXX */
 	}
 
 	KSI_INIT_TRAP(&ksi);
@@ -2456,7 +2434,6 @@ proc_stoptrace(int trapno, int sysnum, const register_t args[],
 
 	mutex_enter(p->p_lock);
 
-repeat:
 	/*
 	 * If we are exiting, demise now.
 	 *
@@ -2476,15 +2453,6 @@ repeat:
 	    sigismember(&p->p_sigpend.sp_set, SIGKILL)) {
 		mutex_exit(p->p_lock);
 		return;
-	}
-
-	/*
-	 * The process is already stopping.
-	 */
-	if ((p->p_sflag & PS_STOPPING) != 0) {
-		sigswitch(0, p->p_xsig, true);
-		mutex_enter(p->p_lock);
-		goto repeat; /* XXX */
 	}
 
 	/* Needed for ktrace */
