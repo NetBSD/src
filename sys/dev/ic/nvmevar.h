6eeae01bd218f1454a63c7430971ccad7998cd12
jmcneill <jmcneill@NetBSD.org>
2019-06-28 15:08:47 +0000

    Fix a performance issue where one busy queue can starve all other queues.
    In normal operations with multiple queues, the nvme driver will attempt
    to schedule I/O requests on the submitting CPU. This breaks down when any
    one of the queues becomes full; the driver returns EAGAIN to the disk
    layer, which causes the disk layer to stop submitting more requests until
    the blocked request is consumed. When space becomes available in the full
    queue, it pulls the next buffer from the bufq and fills the queue again,
    until finally hitting EAGAIN and preventing other queues from processing
    requests.
    
    Two changes here to fix the problem:
    
     - When processing requests from the bufq, attempt to assign them to the
       queue associated with the CPU that originated the request.
     - If that queue is busy, try to find another queue with available space
       before returning EAGAIN. This way, only when all queues are full will
       the disk layer stop submitting more requests.
    
    Now for some real numbers. On a Rockchip RK3399 board (6 CPUs), with 6
    concurrent readers:
    
    Old code:
    	4294967296 bytes transferred in 52.420 secs (81933752 bytes/sec)
    	4294967296 bytes transferred in 53.969 secs (79582117 bytes/sec)
    	4294967296 bytes transferred in 55.391 secs (77539082 bytes/sec)
    	4294967296 bytes transferred in 55.649 secs (77179595 bytes/sec)
    	4294967296 bytes transferred in 56.102 secs (76556402 bytes/sec)
    	4294967296 bytes transferred in 72.901 secs (58915066 bytes/sec)
    
    New code:
    	4294967296 bytes transferred in 37.171 secs (115546186 bytes/sec)
    	4294967296 bytes transferred in 37.611 secs (114194445 bytes/sec)
    	4294967296 bytes transferred in 37.655 secs (114061009 bytes/sec)
    	4294967296 bytes transferred in 38.247 secs (112295534 bytes/sec)
    	4294967296 bytes transferred in 38.496 secs (111569183 bytes/sec)
    	4294967296 bytes transferred in 38.595 secs (111282997 bytes/sec)

--- sys/dev/ic/nvmevar.h
+++ sys/dev/ic/nvmevar.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: nvmevar.h,v 1.19 2019/04/24 23:39:23 mlelstv Exp $	*/
+/*	$NetBSD: nvmevar.h,v 1.20 2019/06/28 15:08:47 jmcneill Exp $	*/
 /*	$OpenBSD: nvmevar.h,v 1.8 2016/04/14 11:18:32 dlg Exp $ */
 
 /*
@@ -23,6 +23,7 @@
 #include <sys/mutex.h>
 #include <sys/pool.h>
 #include <sys/queue.h>
+#include <sys/buf.h>
 
 struct nvme_dmamem {
 	bus_dmamap_t		ndm_map;
@@ -167,9 +168,20 @@ int	nvme_intr_msi(void *);
 void	nvme_softintr_msi(void *);
 
 static __inline struct nvme_queue *
-nvme_get_q(struct nvme_softc *sc)
+nvme_get_q(struct nvme_softc *sc, struct buf *bp, bool waitok)
 {
-	return sc->sc_q[cpu_index(curcpu()) % sc->sc_nq];
+	struct cpu_info *ci = (bp && bp->b_ci) ? bp->b_ci : curcpu();
+
+	/*
+	 * Find a queue with available ccbs, preferring the originating CPU's queue.
+	 */
+
+	for (u_int qoff = 0; qoff < sc->sc_nq; qoff++) {
+		struct nvme_queue *q = sc->sc_q[(cpu_index(ci) + qoff) % sc->sc_nq];
+		if (!SIMPLEQ_EMPTY(&q->q_ccb_list) || waitok)
+			return q;
+	}
+	return NULL;
 }
 
 /*
