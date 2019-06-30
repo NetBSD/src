1d5b3d7d428aeb46d8013d07cc2c1c0e5ce0389a
he <he@NetBSD.org>
2019-06-30 19:57:23 +0000

    Make the scaling of the "bytes written" sticky, so that if we go to K, we don't return to unscaled, and similar for higher scales (though it takes some effort, due to the wide field...) Fixes PR#54334.

--- usr.bin/vmstat/drvstats.h
+++ usr.bin/vmstat/drvstats.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: drvstats.h,v 1.5 2017/07/04 21:19:33 mlelstv Exp $	*/
+/*	$NetBSD: drvstats.h,v 1.6 2019/06/30 19:57:24 he Exp $	*/
 
 /*
  * Copyright (c) 1996 John M. Vinopal
@@ -45,6 +45,7 @@ struct _drive {
 	u_int64_t	 *seek;	/* # of seeks (currently unused). */
 	u_int64_t	 *rbytes;	/* # of bytes read. */
 	u_int64_t	 *wbytes;	/* # of bytes written. */
+	int		 *scale;	/* Sticky scale for bytes */
 	struct timeval	 *time;		/* Time spent in disk i/o. */
 	struct timeval	 *wait;		/* Time spent in queue waiting. */
 	struct timeval	 *busysum;	/* Time busy * queue length */
