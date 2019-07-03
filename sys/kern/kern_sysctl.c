03eb980b693fc76c4fe8065d1ef5bdfe4786cf23
maxv <maxv@NetBSD.org>
2019-07-03 17:31:32 +0000

    Invert two conditions, to fix uninitialized memory access. If the node is an immediate, then the 64 bits of nnode.sysctl_data may not all be initialized, since this is an union.
    Obviously, this is harmless; but still a bug, so fix it.

--- sys/kern/kern_sysctl.c
+++ sys/kern/kern_sysctl.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: kern_sysctl.c,v 1.263 2019/01/28 15:56:12 christos Exp $	*/
+/*	$NetBSD: kern_sysctl.c,v 1.264 2019/07/03 17:31:32 maxv Exp $	*/
 
 /*-
  * Copyright (c) 2003, 2007, 2008 The NetBSD Foundation, Inc.
@@ -68,7 +68,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: kern_sysctl.c,v 1.263 2019/01/28 15:56:12 christos Exp $");
+__KERNEL_RCSID(0, "$NetBSD: kern_sysctl.c,v 1.264 2019/07/03 17:31:32 maxv Exp $");
 
 #ifdef _KERNEL_OPT
 #include "opt_defcorename.h"
@@ -1077,8 +1077,8 @@ sysctl_create(SYSCTLFN_ARGS)
 	 */
 	if (l != NULL || nnode.sysctl_func == NULL) {
 		if (type != CTLTYPE_NODE &&
-		    nnode.sysctl_data == NULL &&
 		    !(flags & CTLFLAG_IMMEDIATE) &&
+		    nnode.sysctl_data == NULL &&
 		    own == NULL)
 			return (EINVAL);
 	}
