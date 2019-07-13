10efe8c0101de1cb8868b172abc5c6a069a80ee9
martin <martin@NetBSD.org>
2019-07-13 17:13:36 +0000

    We may call md_pre_mount() multiple times for the same install set, so pass an additional index argument so MD code knows which part of the install will be mounted next.

--- usr.sbin/sysinst/arch/evbsh3/md.c
+++ usr.sbin/sysinst/arch/evbsh3/md.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: md.c,v 1.4 2019/06/12 06:20:20 martin Exp $	*/
+/*	$NetBSD: md.c,v 1.5 2019/07/13 17:13:37 martin Exp $	*/
 
 /*
  * Copyright 1997 Piermont Information Systems Inc.
@@ -188,7 +188,7 @@ md_post_extract(struct install_partition_desc *install)
 }
 
 int
-md_pre_mount(struct install_partition_desc *install)
+md_pre_mount(struct install_partition_desc *install, size_t ndx)
 {
 	return 0;
 }
