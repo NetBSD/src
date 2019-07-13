05ef508007f6669bd3a6742d098c93dc7e621339
martin <martin@NetBSD.org>
2019-07-13 17:13:36 +0000

    We may call md_pre_mount() multiple times for the same install set, so pass an additional index argument so MD code knows which part of the install will be mounted next.

--- usr.sbin/sysinst/arch/acorn32/md.c
+++ usr.sbin/sysinst/arch/acorn32/md.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: md.c,v 1.4 2019/06/12 06:20:18 martin Exp $ */
+/*	$NetBSD: md.c,v 1.5 2019/07/13 17:13:36 martin Exp $ */
 
 /*
  * Copyright 1997 Piermont Information Systems Inc.
@@ -346,7 +346,7 @@ filecore_checksum(u_char *bootblock)
 }
 
 int
-md_pre_mount(struct install_partition_desc *install)
+md_pre_mount(struct install_partition_desc *install, size_t ndx)
 {
 	return 0;
 }
