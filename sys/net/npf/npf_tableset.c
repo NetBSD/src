54a17980b75b4763f9064f942ec56d84511228c4
rmind <rmind@NetBSD.org>
2019-07-23 00:52:01 +0000

    NPF improvements: - Add support for dynamic NETMAP algorithm (stateful net-to-net). - Add most of the support for the dynamic NAT rules; a little bit more   userland work is needed to finish this up and enable. - Replace 'stateful-ends' with more permissive 'stateful-all'. - Add various tunable parameters and document them, see npf-params(7). - Reduce the memory usage of the connection state table (conndb). - Portmap rewrite: use memory more efficiently, handle addresses dynamically. - Bug fix: add splsoftnet()/splx() around the thmap writers and comment. - npftest: clean up and simplify; fix some memleaks to make ASAN happy.

--- sys/net/npf/npf_tableset.c
+++ sys/net/npf/npf_tableset.c
@@ -1,5 +1,5 @@
 /*-
- * Copyright (c) 2009-2018 The NetBSD Foundation, Inc.
+ * Copyright (c) 2009-2019 The NetBSD Foundation, Inc.
  * All rights reserved.
  *
  * This material is based upon work partially supported by The
@@ -35,11 +35,18 @@
  *	The tableset is an array of tables.  After the creation, the array
  *	is immutable.  The caller is responsible to synchronise the access
  *	to the tableset.
+ *
+ * Warning (not applicable for the userspace npfkern):
+ *
+ *	The thmap_put()/thmap_del() are not called from the interrupt
+ *	context and are protected by a mutex(9), therefore they do not
+ *	SPL wrappers -- see the comment at the top of the npf_conndb.c
+ *	source file.
  */
 
 #ifdef _KERNEL
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: npf_tableset.c,v 1.32 2019/06/20 17:12:37 christos Exp $");
+__KERNEL_RCSID(0, "$NetBSD: npf_tableset.c,v 1.33 2019/07/23 00:52:01 rmind Exp $");
 
 #include <sys/param.h>
 #include <sys/types.h>
@@ -476,26 +483,6 @@ npf_table_check(npf_tableset_t *ts, const char *name, uint64_t tid, uint64_t typ
 }
 
 static int
-table_cidr_check(int alen, const npf_addr_t *addr, npf_netmask_t mask)
-{
-	switch (alen) {
-	case sizeof(struct in_addr):
-		if (__predict_false(mask > 32 && mask != NPF_NO_NETMASK)) {
-			return EINVAL;
-		}
-		break;
-	case sizeof(struct in6_addr):
-		if (__predict_false(mask > 128 && mask != NPF_NO_NETMASK)) {
-			return EINVAL;
-		}
-		break;
-	default:
-		return EINVAL;
-	}
-	return 0;
-}
-
-static int
 table_ifaddr_insert(npf_table_t *t, const int alen, npf_tblent_t *ent)
 {
 	const unsigned aidx = NPF_ADDRLEN2IDX(alen);
@@ -543,7 +530,7 @@ npf_table_insert(npf_table_t *t, const int alen,
 	npf_tblent_t *ent;
 	int error;
 
-	error = table_cidr_check(alen, addr, mask);
+	error = npf_netmask_check(alen, mask);
 	if (error) {
 		return error;
 	}
@@ -621,7 +608,7 @@ npf_table_remove(npf_table_t *t, const int alen,
 	npf_tblent_t *ent = NULL;
 	int error;
 
-	error = table_cidr_check(alen, addr, mask);
+	error = npf_netmask_check(alen, mask);
 	if (error) {
 		return error;
 	}
@@ -678,7 +665,7 @@ npf_table_lookup(npf_table_t *t, const int alen, const npf_addr_t *addr)
 	bool found;
 	int error;
 
-	error = table_cidr_check(alen, addr, NPF_NO_NETMASK);
+	error = npf_netmask_check(alen, NPF_NO_NETMASK);
 	if (error) {
 		return error;
 	}
