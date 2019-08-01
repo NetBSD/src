d8821a832ca55ccd1bacc132d1626d35266e979e
martin <martin@NetBSD.org>
2019-08-01 13:50:13 +0000

    Pull up following revision(s) (requested by msaitoh in ticket #1312):
    	sys/dev/pci/if_wm.c: revision 1.644
    	sys/dev/pci/if_wm.c: revision 1.645
    	sys/dev/pci/if_wmreg.h: revision 1.115
    
     Define RETA_ENTRY_MASK_Q(used in RSS redirection table) correctly.
    Found by UBSan.
    
     Use aprint_debug_dev() instead of printf() in wm_ulp_disable().
    
    - rnd_add_*() call must be serialized. Unserialized accesses broke the
      sc->rnd_source structure and caused off by one in kern_rndq.c's
      rnd_add_data_ts(). This problem was found by kUBSan. Make krndsource_t's
      data per queue-pair and call rnd_add_uint32() in wm_txrxintr_msix(). One of
      the reason why rnd_add_uint32() calls were moved from wm_{tx,rx}eof() is that
      rnd(9) manual page says "To allow the system to gather the timing information
      accurately, this call should be placed within the actual hardware interrupt
      service routine."
    
    - Pass a better value to rnd_add_uint32() in wm_intr_legacy().
    XXX pullup-8.

--- sys/dev/pci/if_wmreg.h
+++ sys/dev/pci/if_wmreg.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: if_wmreg.h,v 1.98.6.9 2019/07/17 16:12:17 martin Exp $	*/
+/*	$NetBSD: if_wmreg.h,v 1.98.6.10 2019/08/01 13:50:14 martin Exp $	*/
 
 /*
  * Copyright (c) 2001 Wasabi Systems, Inc.
@@ -1269,7 +1269,7 @@ struct livengood_tcpip_ctxdesc {
 
 #define WMREG_RETA_Q(x)		(0x5c00 + ((x) >> 2) * 4) /* Redirection Table */
 #define RETA_NUM_ENTRIES	128
-#define RETA_ENTRY_MASK_Q(x)	(0x000000ff << (((x) % 4) * 8)) /* Redirection Table */
+#define RETA_ENTRY_MASK_Q(x)	(0x000000ffUL << (((x) % 4) * 8)) /* Redirection Table */
 #define RETA_ENT_QINDEX_MASK		__BITS(3,0) /*queue index for 82580 and newer */
 #define RETA_ENT_QINDEX0_MASK_82575	__BITS(3,2) /*queue index for pool0 */
 #define RETA_ENT_QINDEX1_MASK_82575	__BITS(7,6) /*queue index for pool1 and regular RSS */
