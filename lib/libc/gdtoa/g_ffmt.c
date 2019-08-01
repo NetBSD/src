2a28df64bf8e4e5acecd77c09df8d2592a5249a5
riastradh <riastradh@NetBSD.org>
2019-08-01 02:27:43 +0000

    Sprinkle some more const in.

--- lib/libc/gdtoa/g_ffmt.c
+++ lib/libc/gdtoa/g_ffmt.c
@@ -1,4 +1,4 @@
-/* $NetBSD: g_ffmt.c,v 1.3 2011/03/20 23:15:35 christos Exp $ */
+/* $NetBSD: g_ffmt.c,v 1.4 2019/08/01 02:27:43 riastradh Exp $ */
 
 /****************************************************************
 
@@ -40,7 +40,7 @@ g_ffmt(buf, f, ndig, bufsize) char *buf; float *f; int ndig; size_t bufsize;
 g_ffmt(char *buf, float *f, int ndig, size_t bufsize)
 #endif
 {
-	static FPI fpi0 = { 24, 1-127-24+1,  254-127-24+1, 1, 0 };
+	static CONST FPI fpi0 = { 24, 1-127-24+1,  254-127-24+1, 1, 0 };
 	char *b, *s, *se;
 	ULong bits[1], *L, sign;
 	int decpt, ex, i, mode;
