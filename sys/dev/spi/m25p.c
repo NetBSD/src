be30b6f0812d3df029b9d8e7908387a5fea42777
tnn <tnn@NetBSD.org>
2019-08-03 02:22:06 +0000

    m25p: add some ~2010 vintage parts found in junk drawer

--- sys/dev/spi/m25p.c
+++ sys/dev/spi/m25p.c
@@ -1,4 +1,4 @@
-/* $NetBSD: m25p.c,v 1.7 2019/08/03 00:46:02 tnn Exp $ */
+/* $NetBSD: m25p.c,v 1.8 2019/08/03 02:22:06 tnn Exp $ */
 
 /*-
  * Copyright (c) 2006 Urbana-Champaign Independent Media Center.
@@ -42,7 +42,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: m25p.c,v 1.7 2019/08/03 00:46:02 tnn Exp $");
+__KERNEL_RCSID(0, "$NetBSD: m25p.c,v 1.8 2019/08/03 02:22:06 tnn Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -94,10 +94,18 @@ static const struct m25p_info {
 } m25p_infos[] = {
 	{ 0x16, 0x20, 0x2017, "STMicro M25P64", 8192, 64 },	/* 64Mbit */
 	{ 0x14, 0x20, 0x2015, "STMicro M25P16", 2048, 64 },	/* 16Mbit */
-	{ 0x12,	0x20, 0x2013, "STMicro M25P40", 512, 64 },	/* 4Mbit */
+	{ 0x12, 0x20, 0x2013, "STMicro M25P40", 512, 64 },	/* 4Mbit */
 	{ 0xc0, 0x20, 0x7117, "STMicro M25PX64", 8192, 64 },	/* 64Mbit */
 	{ 0x00, 0x20, 0xBB18, "Numonyx N25Q128", 16384, 64 },	/* 128Mbit */
 	{ 0x00, 0xBF, 0x2541, "Microchip SST25VF016B", 2048, 64 }, /* 16Mbit */
+	{ 0x00, 0xC2, 0x2011, "Macronix MX25L10", 128, 4 },	/* 1Mbit */
+	{ 0x00, 0xC2, 0x2012, "Macronix MX25L20", 256, 4 },	/* 2Mbit */
+	{ 0x00, 0xC2, 0x2013, "Macronix MX25L40", 512, 4 },	/* 4Mbit */
+	{ 0x00, 0xC2, 0x2014, "Macronix MX25L80", 1024, 4 },	/* 8Mbit */
+	{ 0x00, 0xEF, 0x3011, "Winbond W25X10", 128, 4 },	/* 1Mbit */
+	{ 0x00, 0xEF, 0x3012, "Winbond W25X20", 256, 4 },	/* 2Mbit */
+	{ 0x00, 0xEF, 0x3013, "Winbond W25X40", 512, 4 },	/* 4Mbit */
+	{ 0x00, 0xEF, 0x3014, "Winbond W25X80", 1024, 4 },	/* 8Mbit */
 	{ 0x13, 0xEF, 0x4014, "Winbond W25Q80.V", 1024, 64 },	/* 8Mbit */
 	{ 0x14, 0xEF, 0x4015, "Winbond W25Q16.V", 2048, 64 },	/* 16Mbit */
 	{ 0x15, 0xEF, 0x4016, "Winbond W25Q32.V", 4096, 64 },	/* 32Mbit */
