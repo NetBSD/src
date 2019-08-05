e1522292a99096560b90102d7de6f288f62dcd2a
joerg <joerg@NetBSD.org>
2019-08-05 16:24:48 +0000

    Don't define register replacements when targetting 32bit ARM.

--- sys/arch/aarch64/include/asm.h
+++ sys/arch/aarch64/include/asm.h
@@ -1,11 +1,13 @@
-/* $NetBSD: asm.h,v 1.3 2018/07/17 18:08:37 christos Exp $ */
+/* $NetBSD: asm.h,v 1.4 2019/08/05 16:24:48 joerg Exp $ */
 
 #ifndef _AARCH64_ASM_H_
 #define _AARCH64_ASM_H_
 
 #include <arm/asm.h>
 
+#ifdef __aarch64__
 #define	fp	x29
 #define	lr	x30
+#endif
 
 #endif /* !_AARCH64_ASM_H_ */
