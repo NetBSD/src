7bee6341467a635ac13e9864afa1f712e7ea4103
pgoyette <pgoyette@NetBSD.org>
2019-08-07 00:39:23 +0000

    Welcome to 9.99.4 (the module sysctl stuff changes struct module, so be safe and bump the kernel version).

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.602 2019/08/06 00:19:57 mrg Exp $	*/
+/*	$NetBSD: param.h,v 1.603 2019/08/07 00:39:23 pgoyette Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -67,7 +67,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	999000300	/* NetBSD 9.99.3 */
+#define	__NetBSD_Version__	999000400	/* NetBSD 9.99.4 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
