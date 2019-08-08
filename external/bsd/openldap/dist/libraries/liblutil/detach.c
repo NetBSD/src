68cd8c0198118cbad1f3e66615cdb21d657e01d8
christos <christos@NetBSD.org>
2019-08-08 13:50:56 +0000

    merge conflicts.

--- external/bsd/openldap/dist/libraries/liblutil/detach.c
+++ external/bsd/openldap/dist/libraries/liblutil/detach.c
@@ -1,10 +1,10 @@
-/*	$NetBSD: detach.c,v 1.5 2018/02/06 01:57:23 christos Exp $	*/
+/*	$NetBSD: detach.c,v 1.6 2019/08/08 13:50:57 christos Exp $	*/
 
 /* detach.c -- routines to daemonize a process */
 /* $OpenLDAP$ */
 /* This work is part of OpenLDAP Software <http://www.openldap.org/>.
  *
- * Copyright 1998-2017 The OpenLDAP Foundation.
+ * Copyright 1998-2019 The OpenLDAP Foundation.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -31,7 +31,7 @@
  */
 
 #include <sys/cdefs.h>
-__RCSID("$NetBSD: detach.c,v 1.5 2018/02/06 01:57:23 christos Exp $");
+__RCSID("$NetBSD: detach.c,v 1.6 2019/08/08 13:50:57 christos Exp $");
 
 #include "portable.h"
 
