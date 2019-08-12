0e495c1863bd32410178c89c76f990c7e28af069
martin <martin@NetBSD.org>
2019-08-12 17:29:46 +0000

    Pull up following revision(s) (requested by christos in ticket #47):
    	external/gpl3/gdb/dist/gdb/auxv.c: revision 1.2
    
    On NetBSD the auxv tag is always 32 bits long. Gdb assumes it is 64 on _LP64.
    This works most of the time, except for 64 bit big endian (sparc64 eg).

--- external/gpl3/gdb/dist/gdb/auxv.c
+++ external/gpl3/gdb/dist/gdb/auxv.c
@@ -266,8 +266,12 @@ default_auxv_parse (struct target_ops *ops, gdb_byte **readptr,
 
   if (endptr - ptr < sizeof_auxv_field * 2)
     return -1;
-
-  *typep = extract_unsigned_integer (ptr, sizeof_auxv_field, byte_order);
+#ifdef __NetBSD__
+  const int sizeof_auxv_type = 4;
+#else
+  const int sizeof_auxv_type = sizeof_auxv_field;
+#endif
+  *typep = extract_unsigned_integer (ptr, sizeof_auxv_type, byte_order);
   ptr += sizeof_auxv_field;
   *valp = extract_unsigned_integer (ptr, sizeof_auxv_field, byte_order);
   ptr += sizeof_auxv_field;
