6fec679845a01443dc1b3c13ac82c6a727451040
martin <martin@NetBSD.org>
2019-08-14 12:55:35 +0000

    When we ask the user to select a partitioning scheme and they refuse (that is: select "Exit" in the menu), abort installation.

--- usr.sbin/sysinst/arch/arc/md.c
+++ usr.sbin/sysinst/arch/arc/md.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: md.c,v 1.8 2019/07/13 17:13:36 martin Exp $ */
+/*	$NetBSD: md.c,v 1.9 2019/08/14 12:55:35 martin Exp $ */
 
 /*
  * Copyright 1997 Piermont Information Systems Inc.
@@ -75,7 +75,7 @@ md_get_info(struct install_partition_desc *install)
 		    select_part_scheme(pm, NULL, true, NULL);
 
 		if (!ps)
-			return true;
+			return false;
 
 		struct disk_partitions *parts =
 		   (*ps->create_new_for_disk)(pm->diskdev,
