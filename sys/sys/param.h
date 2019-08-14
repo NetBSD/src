6abc90d37e34c9c38ce620dac3130bcc8da702d1
mrg <mrg@NetBSD.org>
2019-08-14 03:44:58 +0000

    introduce usbnet_set_dying().  will be used by url(4) conversion. bump version.
    introduce USBNET_MODULE() that encompasses almost all the module
    specific code for usbnet modules.  they still need to include
    the relevant ioconf.c, but everything else is now just, eg,
    
    	USBNET_MODULE(axen)

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.606 2019/08/10 21:13:54 rmind Exp $	*/
+/*	$NetBSD: param.h,v 1.607 2019/08/14 03:44:58 mrg Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -67,7 +67,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	999000700	/* NetBSD 9.99.7 */
+#define	__NetBSD_Version__	999000800	/* NetBSD 9.99.8 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
