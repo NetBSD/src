0d207c092193ee3f53d4dc63650bb5c03e21e026
msaitoh <msaitoh@NetBSD.org>
2019-08-17 17:03:32 +0000

    Use unsigned to avoid undefined behavior. Found by kUBSan.

--- sys/external/bsd/drm2/dist/drm/radeon/rv730d.h
+++ sys/external/bsd/drm2/dist/drm/radeon/rv730d.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: rv730d.h,v 1.2 2018/08/27 04:58:36 riastradh Exp $	*/
+/*	$NetBSD: rv730d.h,v 1.3 2019/08/17 17:03:32 msaitoh Exp $	*/
 
 /*
  * Copyright 2011 Advanced Micro Devices, Inc.
@@ -150,7 +150,7 @@
 #define		POWERMODE2(x)				((x) << 16)
 #define		POWERMODE2_MASK				(0xff << 16)
 #define		POWERMODE3(x)				((x) << 24)
-#define		POWERMODE3_MASK				(0xff << 24)
+#define		POWERMODE3_MASK				(0xffU << 24)
 
 #define	MC_ARB_DRAM_TIMING_1				0x27f0
 #define	MC_ARB_DRAM_TIMING_2				0x27f4
