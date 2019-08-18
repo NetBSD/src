8976e5793c4239563350deb216030e099d44d4ef
kamil <kamil@NetBSD.org>
2019-08-18 04:14:40 +0000

    netstat: Add indirection of symbols to remove clash with sanitizers
    Add indirection and symbol renaming under MKSANITIZER for the linked in
    version of sysctlbyname, sysctlgetmibinfo and sysctlnametomib.

--- usr.bin/netstat/netstat_hostops.c
+++ usr.bin/netstat/netstat_hostops.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: netstat_hostops.c,v 1.1 2010/12/13 21:15:30 pooka Exp $	*/
+/*	$NetBSD: netstat_hostops.c,v 1.2 2019/08/18 04:14:40 kamil Exp $	*/
 
 /*
  * Copyright (c) 2010 The NetBSD Foundation, Inc.
@@ -28,7 +28,7 @@
 
 #include <sys/cdefs.h>
 #ifndef lint
-__RCSID("$NetBSD: netstat_hostops.c,v 1.1 2010/12/13 21:15:30 pooka Exp $");
+__RCSID("$NetBSD: netstat_hostops.c,v 1.2 2019/08/18 04:14:40 kamil Exp $");
 #endif /* !lint */
 
 #include <sys/types.h>
@@ -38,4 +38,10 @@ __RCSID("$NetBSD: netstat_hostops.c,v 1.1 2010/12/13 21:15:30 pooka Exp $");
 
 const struct prog_ops prog_ops = {
 	.op_sysctl = sysctl,
+
+	.op_sysctlbyname = sysctlbyname,
+
+	.op_sysctlgetmibinfo = sysctlgetmibinfo,
+
+	.op_sysctlnametomib = sysctlnametomib,
 };
