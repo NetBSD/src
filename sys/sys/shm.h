287a3c957a59e968bfb0697a71e3fe5de824981b
maxv <maxv@NetBSD.org>
2019-08-23 10:22:14 +0000

    Fix stupid bugs in linux_sys_shmctl(): the index could be out of bound (page fault) and there was no proper locking.
    Maybe we should just remove LINUX_SHM_STAT, like compat_linux32.

--- sys/sys/shm.h
+++ sys/sys/shm.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: shm.h,v 1.53 2019/08/07 00:38:02 pgoyette Exp $	*/
+/*	$NetBSD: shm.h,v 1.54 2019/08/23 10:22:14 maxv Exp $	*/
 
 /*-
  * Copyright (c) 1999 The NetBSD Foundation, Inc.
@@ -177,6 +177,8 @@ int	shmfini(void);
 void	shmfork(struct vmspace *, struct vmspace *);
 void	shmexit(struct vmspace *);
 int	shmctl1(struct lwp *, int, int, struct shmid_ds *);
+
+int	shm_find_segment_perm_by_index(int, struct ipc_perm *);
 
 extern void (*uvm_shmexit)(struct vmspace *);
 extern void (*uvm_shmfork)(struct vmspace *, struct vmspace *);
