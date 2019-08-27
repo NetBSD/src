c48a982d421b50c204a61059e956810b4dacdd64
kamil <kamil@NetBSD.org>
2019-08-27 22:48:53 +0000

    Enhance the support of LLVM sanitizers
    Define _REENTRANT for MKSANITIZER build. This is needed for at least stdio
    code. This caused new build issued with duplicated symbols in few places
    and rump kernel code picking different code paths borrowed from libc.
    Handle all this in one go.
    
    Add bsd.sanitizer.mk to share common code used by programs and libraries.
    
    Switch from realall to beforeinstall target in .syms files. This is more
    reliable in MKSANITIZER.

--- share/mk/bsd.prog.mk
+++ share/mk/bsd.prog.mk
@@ -1,4 +1,4 @@
-#	$NetBSD: bsd.prog.mk,v 1.322 2019/08/17 00:14:48 kamil Exp $
+#	$NetBSD: bsd.prog.mk,v 1.323 2019/08/27 22:48:54 kamil Exp $
 #	@(#)bsd.prog.mk	8.2 (Berkeley) 4/2/94
 
 .ifndef HOSTPROG
@@ -6,29 +6,13 @@
 .include <bsd.init.mk>
 .include <bsd.shlib.mk>
 .include <bsd.gcc.mk>
+.include <bsd.sanitizer.mk>
 
 ##### Sanitizer specific flags.
 
 CFLAGS+=	${SANITIZERFLAGS} ${LIBCSANITIZERFLAGS}
 CXXFLAGS+=	${SANITIZERFLAGS} ${LIBCSANITIZERFLAGS}
 LDFLAGS+=	${SANITIZERFLAGS}
-
-# Rename the local function definitions to not conflict with libc/rt/pthread/m.
-.if ${MKSANITIZER:Uno} == "yes" && defined(SANITIZER_RENAME_SYMBOL)
-.	for _symbol in ${SANITIZER_RENAME_SYMBOL}
-CPPFLAGS+=	-D${_symbol}=__mksanitizer_${_symbol}
-.	endfor
-.endif
-
-.if ${MKSANITIZER:Uno} == "yes" && defined(SANITIZER_RENAME_CLASSES)
-.	for _class in ${SANITIZER_RENAME_CLASSES}
-.		for _file in ${SANITIZER_RENAME_FILES.${_class}}
-.			for _symbol in ${SANITIZER_RENAME_SYMBOL.${_class}}
-COPTS.${_file}+=	-D${_symbol}=__mksanitizer_${_symbol}
-.			endfor
-.		endfor
-.	endfor
-.endif
 
 #
 # Definitions and targets shared among all programs built by a single
