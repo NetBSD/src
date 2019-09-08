44e5e0d6490d3ee4d6247433b1c9e2da46eb5e63
macallan <macallan@NetBSD.org>
2019-09-08 05:55:15 +0000

    add driver for Kensington Turbo Mouse ( actually a trackball ) the adbms driver contains basic support, this driver lets you program the buttons, I'll add other features once I figure out how they work

--- sys/dev/adb/files.adb
+++ sys/dev/adb/files.adb
@@ -1,5 +1,5 @@
 # 
-#	$NetBSD: files.adb,v 1.8 2019/06/06 20:59:58 macallan Exp $
+#	$NetBSD: files.adb,v 1.9 2019/09/08 05:55:15 macallan Exp $
 #
 # Apple Desktop Bus protocol and drivers
 
@@ -8,6 +8,7 @@ defflag	adbdebug.h	ADBKBD_DEBUG
 defflag	adbdebug.h	ADBMS_DEBUG
 defflag	adbdebug.h	ADBBT_DEBUG
 defflag adbdebug.h	ADBKBD_POWER_DDB
+defflag	adbdebug.h	KTM_DEBUG
 
 define adb_bus {}
 
@@ -28,3 +29,7 @@ file dev/adb/adb_bt.c		adbbt
 device adbms : wsmousedev
 attach adbms at nadb
 file dev/adb/adb_ms.c		adbms needs-flag
+
+device ktm : wsmousedev
+attach ktm at nadb
+file dev/adb/adb_ktm.c		ktm needs-flag
