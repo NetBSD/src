35abf8655323235f5a6e033b3974cc25d7234698
ryo <ryo@NetBSD.org>
2019-09-13 18:07:30 +0000

    In pmap_devmap_bootstrap(), cpu_earlydevice_va_p() must not return true until *all* devmap tables have been enabled. console mapping may be present in the last table.

--- sys/arch/aarch64/include/cpufunc.h
+++ sys/arch/aarch64/include/cpufunc.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: cpufunc.h,v 1.6 2019/09/07 11:10:24 ryo Exp $	*/
+/*	$NetBSD: cpufunc.h,v 1.7 2019/09/13 18:07:30 ryo Exp $	*/
 
 /*
  * Copyright (c) 2017 Ryo Shimizu <ryo@nerv.org>
@@ -148,14 +148,14 @@ cpu_clusterid(void)
 static inline bool
 cpu_earlydevice_va_p(void)
 {
-	extern vaddr_t virtual_devmap_addr;	/* in pmap.c */
+	extern bool pmap_devmap_bootstrap_done;	/* in pmap.c */
 
 	/* This function may be called before enabling MMU, or mapping KVA */
 	if ((reg_sctlr_el1_read() & SCTLR_M) == 0)
 		return false;
 
 	/* device mapping will be availabled after pmap_devmap_bootstrap() */
-	if (virtual_devmap_addr == 0)
+	if (!pmap_devmap_bootstrap_done)
 		return false;
 
 	return true;
