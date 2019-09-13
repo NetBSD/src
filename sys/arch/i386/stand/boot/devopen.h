fab0e72b9229e65a392ba5e1c2d616884ef9eb9a
martin <martin@NetBSD.org>
2019-09-13 07:00:13 +0000

    Pull up following revision(s) (requested by manu in ticket #200):
    	sys/arch/i386/stand/boot/boot2.c: revision 1.72
    	sys/arch/i386/stand/lib/biosdisk.c: revision 1.50
    	sys/arch/i386/stand/lib/biosdisk.c: revision 1.51
    	sys/arch/i386/stand/efiboot/devopen.c: revision 1.6
    	sys/arch/i386/stand/efiboot/devopen.h: revision 1.4
    	sys/arch/i386/stand/efiboot/devopen.c: revision 1.7
    	sys/arch/i386/stand/efiboot/efidisk.c: revision 1.8
    	share/man/man8/man8.x86/boot.8: revision 1.16
    	share/man/man8/man8.x86/boot.8: revision 1.17
    	sys/arch/i386/stand/lib/Makefile: revision 1.46
    	sys/arch/i386/stand/boot/devopen.h: revision 1.5
    	sys/arch/i386/stand/boot/devopen.c: revision 1.9
    	sys/arch/i386/stand/efiboot/boot.c: revision 1.14
    	sys/arch/i386/stand/efiboot/boot.c: revision 1.15
    	sys/arch/i386/stand/lib/biosdisk.h: revision 1.11
    	sys/arch/i386/stand/boot/boot2.c: revision 1.71
    
    Add GPT and RAIDframe support to bootloaders
    
    Classic BIOS (/boot) and EFI bootloaders can now name devices
    using the NAME=gpt_label syntax, or using raid partitions. Here
    are examples:
    
    boot NAME=root:/netbsd
    boot raid0e:/netbsd
    
    Correct the memset(3)'s third argument in i386 biosdisk.c
    
    The size of allocation is the size of the structure biosdisk, not the size
    of a pointer.
    
    Document new GPT and RAIDframe capacity of bootstrap code
    While there, also document EFI setup and some bugs
    
    Typo fixes, 'file system'; new sentence, new line; expand IA-32.
    Bump date for previous.
    
    Make sure devices names are copied including last byte
    Fix from M. Levinson.

--- sys/arch/i386/stand/boot/devopen.h
+++ sys/arch/i386/stand/boot/devopen.h
@@ -1,5 +1,5 @@
-/*	$NetBSD: devopen.h,v 1.4 2010/12/24 20:40:42 jakllsch Exp $	*/
+/*	$NetBSD: devopen.h,v 1.4.64.1 2019/09/13 07:00:13 martin Exp $	*/
 
 extern int boot_biosdev;
 
-void bios2dev(int, daddr_t, char **, int *, int *);
+void bios2dev(int, daddr_t, char **, int *, int *, const char **);
