b49767afa65d746ab015befbe58db44016bc314f
christos <christos@NetBSD.org>
2019-09-22 22:59:37 +0000

    Add a new member to struct vfsstat and grow the unused members The new member is caled f_mntfromlabel and it is the dkw_wname of the corresponding wedge. This is now used by df -W to display the mountpoint name as NAME=

--- sys/compat/netbsd32/netbsd32.h
+++ sys/compat/netbsd32/netbsd32.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: netbsd32.h,v 1.123 2019/02/20 06:04:28 mrg Exp $	*/
+/*	$NetBSD: netbsd32.h,v 1.124 2019/09/22 22:59:38 christos Exp $	*/
 
 /*
  * Copyright (c) 1998, 2001, 2008, 2015 Matthew R. Green
@@ -852,8 +852,8 @@ struct netbsd32_stat {
 };
 
 /* from <sys/statvfs.h> */
-typedef netbsd32_pointer_t netbsd32_statvfsp_t;
-struct netbsd32_statvfs {
+typedef netbsd32_pointer_t netbsd32_statvfs90p_t;
+struct netbsd32_statvfs90 {
 	netbsd32_u_long	f_flag;		/* copy of mount exported flags */
 	netbsd32_u_long	f_bsize;	/* system block size */
 	netbsd32_u_long	f_frsize;	/* system fragment size */
@@ -878,6 +878,35 @@ struct netbsd32_statvfs {
 	char	f_fstypename[_VFS_NAMELEN]; /* fs type name */
 	char	f_mntonname[_VFS_MNAMELEN];  /* directory on which mounted */
 	char	f_mntfromname[_VFS_MNAMELEN];  /* mounted file system */
+};
+
+typedef netbsd32_pointer_t netbsd32_statvfsp_t;
+struct netbsd32_statvfs {
+	netbsd32_u_long	f_flag;		/* copy of mount exported flags */
+	netbsd32_u_long	f_bsize;	/* system block size */
+	netbsd32_u_long	f_frsize;	/* system fragment size */
+	netbsd32_u_long	f_iosize;	/* optimal file system block size */
+	netbsd32_uint64	f_blocks;	/* number of blocks in file system */
+	netbsd32_uint64	f_bfree;	/* free blocks avail in file system */
+	netbsd32_uint64	f_bavail;	/* free blocks avail to non-root */
+	netbsd32_uint64	f_bresvd;	/* blocks reserved for root */
+	netbsd32_uint64	f_files;	/* total file nodes in file system */
+	netbsd32_uint64	f_ffree;	/* free file nodes in file system */
+	netbsd32_uint64	f_favail;	/* free file nodes avail to non-root */
+	netbsd32_uint64	f_fresvd;	/* file nodes reserved for root */
+	netbsd32_uint64	f_syncreads;	/* count of sync reads since mount */
+	netbsd32_uint64	f_syncwrites;	/* count of sync writes since mount */
+	netbsd32_uint64	f_asyncreads;	/* count of async reads since mount */
+	netbsd32_uint64	f_asyncwrites;	/* count of async writes since mount */
+	fsid_t		f_fsidx;	/* NetBSD compatible fsid */
+	netbsd32_u_long	f_fsid;		/* Posix compatible fsid */
+	netbsd32_u_long	f_namemax;	/* maximum filename length */
+	uid_t		f_owner;	/* user that mounted the file system */
+	uint64_t	f_spare[4];	/* spare space */
+	char	f_fstypename[_VFS_NAMELEN]; /* fs type name */
+	char	f_mntonname[_VFS_MNAMELEN];  /* directory on which mounted */
+	char	f_mntfromname[_VFS_MNAMELEN];  /* mounted file system */
+	char	f_mntfromlabel[_VFS_MNAMELEN];  /* disk label name if available */
 };
 
 /* from <sys/timex.h> */
--- sys/compat/netbsd32/netbsd32_conv.h
+++ sys/compat/netbsd32/netbsd32_conv.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: netbsd32_conv.h,v 1.38 2019/02/21 03:37:19 mrg Exp $	*/
+/*	$NetBSD: netbsd32_conv.h,v 1.39 2019/09/22 22:59:38 christos Exp $	*/
 
 /*
  * Copyright (c) 1998, 2001 Matthew R. Green
@@ -307,7 +307,7 @@ netbsd32_from_mmsghdr(struct netbsd32_mmsghdr *mmsg32,
 }
 
 static __inline void
-netbsd32_from_statvfs(const struct statvfs *sbp, struct netbsd32_statvfs *sb32p)
+netbsd32_from_statvfs90(const struct statvfs *sbp, struct netbsd32_statvfs90 *sb32p)
 {
 	sb32p->f_flag = sbp->f_flag;
 	sb32p->f_bsize = (netbsd32_u_long)sbp->f_bsize;
@@ -343,6 +343,47 @@ netbsd32_from_statvfs(const struct statvfs *sbp, struct netbsd32_statvfs *sb32p)
 	memcpy(sb32p->f_fstypename, sbp->f_fstypename, sizeof(sb32p->f_fstypename));
 	memcpy(sb32p->f_mntonname, sbp->f_mntonname, sizeof(sb32p->f_mntonname));
 	memcpy(sb32p->f_mntfromname, sbp->f_mntfromname, sizeof(sb32p->f_mntfromname));
+#endif
+}
+
+static __inline void
+netbsd32_from_statvfs(const struct statvfs *sbp, struct netbsd32_statvfs *sb32p)
+{
+	sb32p->f_flag = sbp->f_flag;
+	sb32p->f_bsize = (netbsd32_u_long)sbp->f_bsize;
+	sb32p->f_frsize = (netbsd32_u_long)sbp->f_frsize;
+	sb32p->f_iosize = (netbsd32_u_long)sbp->f_iosize;
+	sb32p->f_blocks = sbp->f_blocks;
+	sb32p->f_bfree = sbp->f_bfree;
+	sb32p->f_bavail = sbp->f_bavail;
+	sb32p->f_bresvd = sbp->f_bresvd;
+	sb32p->f_files = sbp->f_files;
+	sb32p->f_ffree = sbp->f_ffree;
+	sb32p->f_favail = sbp->f_favail;
+	sb32p->f_fresvd = sbp->f_fresvd;
+	sb32p->f_syncreads = sbp->f_syncreads;
+	sb32p->f_syncwrites = sbp->f_syncwrites;
+	sb32p->f_asyncreads = sbp->f_asyncreads;
+	sb32p->f_asyncwrites = sbp->f_asyncwrites;
+	sb32p->f_fsidx = sbp->f_fsidx;
+	sb32p->f_fsid = (netbsd32_u_long)sbp->f_fsid;
+	sb32p->f_namemax = (netbsd32_u_long)sbp->f_namemax;
+	sb32p->f_owner = sbp->f_owner;
+	sb32p->f_spare[0] = 0;
+	sb32p->f_spare[1] = 0;
+	sb32p->f_spare[2] = 0;
+	sb32p->f_spare[3] = 0;
+#if 1
+	/* May as well do the whole batch in one go */
+	memcpy(sb32p->f_fstypename, sbp->f_fstypename,
+	    sizeof(sb32p->f_fstypename) + sizeof(sb32p->f_mntonname) +
+	    sizeof(sb32p->f_mntfromname) + sizeof(sb32p->f_mntfromlabel));
+#else
+	/* If we want to be careful */
+	memcpy(sb32p->f_fstypename, sbp->f_fstypename, sizeof(sb32p->f_fstypename));
+	memcpy(sb32p->f_mntonname, sbp->f_mntonname, sizeof(sb32p->f_mntonname));
+	memcpy(sb32p->f_mntfromname, sbp->f_mntfromname, sizeof(sb32p->f_mntfromname));
+	memcpy(sb32p->f_mntfromlabel, sbp->f_mntfromlabel, sizeof(sb32p->f_mntfromlabel));
 #endif
 }
 
