bacbc066344704261fc7f4b5a44d2cba0951b6df
skrll <skrll@NetBSD.org>
2019-09-23 18:20:07 +0000

    Use "segmap" for uvm_wait message in pmap_segtab_alloc

--- sys/uvm/pmap/pmap_segtab.c
+++ sys/uvm/pmap/pmap_segtab.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: pmap_segtab.c,v 1.9 2019/09/18 18:29:58 skrll Exp $	*/
+/*	$NetBSD: pmap_segtab.c,v 1.10 2019/09/23 18:20:07 skrll Exp $	*/
 
 /*-
  * Copyright (c) 1998, 2001 The NetBSD Foundation, Inc.
@@ -67,7 +67,7 @@
 
 #include <sys/cdefs.h>
 
-__KERNEL_RCSID(0, "$NetBSD: pmap_segtab.c,v 1.9 2019/09/18 18:29:58 skrll Exp $");
+__KERNEL_RCSID(0, "$NetBSD: pmap_segtab.c,v 1.10 2019/09/23 18:20:07 skrll Exp $");
 
 /*
  *	Manages physical address maps.
@@ -299,7 +299,7 @@ pmap_segtab_alloc(void)
 			/*
 			 * XXX What else can we do?  Could we deadlock here?
 			 */
-			uvm_wait("pmap_create");
+			uvm_wait("segtab");
 			goto again;
 		}
 		SEGTAB_ADD(npage, 1);
