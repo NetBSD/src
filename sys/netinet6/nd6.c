3208249fce96430e2d74f1aa234cc32098545ff8
ozaki-r <ozaki-r@NetBSD.org>
2019-09-25 09:52:32 +0000

    Initialize DAD components properly
    The original code initialized each component in non-init functions such as
    arp_dad_start and nd6_dad_find, conditionally based on a global flag for each.
    However, it was racy because the flag and the code around it were not
    protected by a lock and could cause a kernel panic at worst.
    
    Fix the issue by initializing the components in bootup as usual.

--- sys/netinet6/nd6.c
+++ sys/netinet6/nd6.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: nd6.c,v 1.263 2019/09/01 19:26:21 roy Exp $	*/
+/*	$NetBSD: nd6.c,v 1.264 2019/09/25 09:52:32 ozaki-r Exp $	*/
 /*	$KAME: nd6.c,v 1.279 2002/06/08 11:16:51 itojun Exp $	*/
 
 /*
@@ -31,7 +31,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: nd6.c,v 1.263 2019/09/01 19:26:21 roy Exp $");
+__KERNEL_RCSID(0, "$NetBSD: nd6.c,v 1.264 2019/09/25 09:52:32 ozaki-r Exp $");
 
 #ifdef _KERNEL_OPT
 #include "opt_net_mpsafe.h"
@@ -136,6 +136,8 @@ void
 nd6_init(void)
 {
 	int error;
+
+	nd6_nbr_init();
 
 	rw_init(&nd6_lock);
 
