abd8277150b00558faf97cc122f92155a571d5e7
martin <martin@NetBSD.org>
2019-09-26 18:50:18 +0000

    Pull up following revision(s) (requested by msaitoh in ticket #242):
    	usr.sbin/cpuctl/arch/i386.c: revision 1.106
    	sys/arch/x86/x86/identcpu.c: revision 1.94
    
    Call cpu_dcp_cacheinfo() only when the cpuid Topology Extension flag is set
    on AMD processor.

--- usr.sbin/cpuctl/arch/i386.c
+++ usr.sbin/cpuctl/arch/i386.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: i386.c,v 1.104.2.1 2019/09/26 18:47:14 martin Exp $	*/
+/*	$NetBSD: i386.c,v 1.104.2.2 2019/09/26 18:50:18 martin Exp $	*/
 
 /*-
  * Copyright (c) 1999, 2000, 2001, 2006, 2007, 2008 The NetBSD Foundation, Inc.
@@ -57,7 +57,7 @@
 
 #include <sys/cdefs.h>
 #ifndef lint
-__RCSID("$NetBSD: i386.c,v 1.104.2.1 2019/09/26 18:47:14 martin Exp $");
+__RCSID("$NetBSD: i386.c,v 1.104.2.2 2019/09/26 18:50:18 martin Exp $");
 #endif /* not lint */
 
 #include <sys/types.h>
@@ -1433,7 +1433,8 @@ amd_cpu_cacheinfo(struct cpu_info *ci)
 	if (lfunc < 0x8000001d)
 		return;
 
-	cpu_dcp_cacheinfo(ci, 0x8000001d);
+	if (ci->ci_feat_val[3] & CPUID_TOPOEXT)
+		cpu_dcp_cacheinfo(ci, 0x8000001d);
 }
 
 static void
