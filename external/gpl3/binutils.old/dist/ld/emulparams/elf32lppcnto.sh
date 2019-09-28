1184fa5a9815fd5eaf5929d523cc4309243d1f58
martin <martin@NetBSD.org>
2019-09-28 12:33:53 +0000

    Remove unused trees at external/gpl3/binutils.old/, external/gpl3/gdb.old/ and crypto/external/bsd/openssl.old/, requested by skrll in ticket #264.

--- external/gpl3/binutils.old/dist/ld/emulparams/elf32lppcnto.sh
+++
@@ -1,5 +0,0 @@
-. ${srcdir}/emulparams/elf32ppc.sh
-OUTPUT_FORMAT="elf32-powerpcle"
-MAXPAGESIZE="CONSTANT (MAXPAGESIZE)"
-TEXT_START_ADDR=0x48040000
-
--- external/gpl3/gdb.old/dist/gdb/testsuite/gdb.base/solib1.c
+++
@@ -1,8 +0,0 @@
-#if defined(__cplusplus) || defined(__STDCPP__)
-extern "C"
-#endif
-int
-solib_main (int arg)
-{
-  return arg*arg;		/* HERE */
-}				/* STEP */
