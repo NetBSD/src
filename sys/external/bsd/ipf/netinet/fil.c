b076228a2e33ff72e804148b314cb51e59fe84b9
bouyer <bouyer@NetBSD.org>
2019-09-30 16:58:06 +0000

    Fix 2 bugs, reported by Edgar Fuß on tech-net@ - pfil_run_hooks() can be called recursively, so we have to   #define FASTROUTE_RECURSION in fil.c - ip6_if_output()/nd6_output() will free the mbuf on error, to make sure   to set *mpp to NULL so the caller won't try to free it again.

--- sys/external/bsd/ipf/netinet/fil.c
+++ sys/external/bsd/ipf/netinet/fil.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: fil.c,v 1.30 2019/08/08 14:38:53 christos Exp $	*/
+/*	$NetBSD: fil.c,v 1.31 2019/09/30 16:58:06 bouyer Exp $	*/
 
 /*
  * Copyright (C) 2012 by Darren Reed.
@@ -141,7 +141,7 @@ extern struct timeout ipf_slowtimer_ch;
 #if !defined(lint)
 #if defined(__NetBSD__)
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: fil.c,v 1.30 2019/08/08 14:38:53 christos Exp $");
+__KERNEL_RCSID(0, "$NetBSD: fil.c,v 1.31 2019/09/30 16:58:06 bouyer Exp $");
 #else
 static const char sccsid[] = "@(#)fil.c	1.36 6/5/96 (C) 1993-2000 Darren Reed";
 static const char rcsid[] = "@(#)Id: fil.c,v 1.1.1.2 2012/07/22 13:45:07 darrenr Exp $";
@@ -154,6 +154,8 @@ static const char rcsid[] = "@(#)Id: fil.c,v 1.1.1.2 2012/07/22 13:45:07 darrenr
 extern	int	opts;
 extern	int	blockreason;
 #endif /* _KERNEL */
+
+#define FASTROUTE_RECURSION
 
 #define	LBUMP(x)	softc->x++
 #define	LBUMPD(x, y)	do { softc->x.y++; DT(y); } while (0)
