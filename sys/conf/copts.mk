df25d61830940cda796be2c46bf3962529df9f34
mrg <mrg@NetBSD.org>
2019-09-30 00:06:02 +0000

    convert HAVE_GCC == 7 to HAVE_GCC >= 7.

--- sys/conf/copts.mk
+++ sys/conf/copts.mk
@@ -1,11 +1,11 @@
-#	$NetBSD: copts.mk,v 1.3 2019/02/10 05:01:59 mrg Exp $
+#	$NetBSD: copts.mk,v 1.4 2019/09/30 00:06:02 mrg Exp $
 
 # MI per-file compiler options required.
 
 .ifndef _SYS_CONF_COPTS_MK_
 _SYS_CONF_COPTS_MK_=1
 
-.if defined(HAVE_GCC) && ${HAVE_GCC} == 7 && ${ACTIVE_CC} == "gcc"
+.if defined(HAVE_GCC) && ${HAVE_GCC} >= 7 && ${ACTIVE_CC} == "gcc"
 COPTS.zlib.c+=		-Wno-error=implicit-fallthrough
 COPTS.pf.c+=		-Wno-error=implicit-fallthrough
 COPTS.radeon_cs.c+=	-Wno-error=implicit-fallthrough
