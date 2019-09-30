938ca14cd1ac4db74c097978caf7351548be20cd
mrg <mrg@NetBSD.org>
2019-09-30 08:40:20 +0000

    remove HAVE_GCC == 6 support, add HAVE_GCC == 8 support

--- tools/gcc/gcc-version.mk
+++ tools/gcc/gcc-version.mk
@@ -1,9 +1,9 @@
-#	$NetBSD: gcc-version.mk,v 1.16 2019/03/20 05:09:26 mrg Exp $
+#	$NetBSD: gcc-version.mk,v 1.17 2019/09/30 08:40:20 mrg Exp $
 
 # common location for tools and native build
 
-.if ${HAVE_GCC} == 6
-NETBSD_GCC_VERSION=nb4 20181109
-.elif ${HAVE_GCC} == 7
+.if ${HAVE_GCC} == 7
 NETBSD_GCC_VERSION=nb3 20190319
+.elif ${HAVE_GCC} == 8
+NETBSD_GCC_VERSION=nb1 20190930
 .endif
