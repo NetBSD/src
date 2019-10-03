885f39ac521750c5e1dbd5fc8890e06a69680ed2
kamil <kamil@NetBSD.org>
2019-10-03 23:11:11 +0000

    Add two KASSERTS in the ptrace(2) kernel code
    Verify that we will never return empty ptrace_state for CHILD/LWP event.

--- sys/kern/sys_ptrace_common.c
+++ sys/kern/sys_ptrace_common.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: sys_ptrace_common.c,v 1.62 2019/10/03 22:48:44 kamil Exp $	*/
+/*	$NetBSD: sys_ptrace_common.c,v 1.63 2019/10/03 23:11:11 kamil Exp $	*/
 
 /*-
  * Copyright (c) 2008, 2009 The NetBSD Foundation, Inc.
@@ -118,7 +118,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: sys_ptrace_common.c,v 1.62 2019/10/03 22:48:44 kamil Exp $");
+__KERNEL_RCSID(0, "$NetBSD: sys_ptrace_common.c,v 1.63 2019/10/03 23:11:11 kamil Exp $");
 
 #ifdef _KERNEL_OPT
 #include "opt_ptrace.h"
@@ -702,6 +702,10 @@ ptrace_get_process_state(struct proc *t, void *addr, size_t data)
 		memset(&ps, 0, sizeof(ps));
 	} else {
 		si = &t->p_sigctx.ps_info;
+
+		KASSERT(si->_reason._ptrace_state._pe_report_event > 0);
+		KASSERT(si->_reason._ptrace_state._option._pe_other_pid > 0);
+
 		ps.pe_report_event = si->_reason._ptrace_state._pe_report_event;
 
 		CTASSERT(sizeof(ps.pe_other_pid) == sizeof(ps.pe_lwp));
