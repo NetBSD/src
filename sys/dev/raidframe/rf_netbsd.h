738c2c54904dc54c18c6c80eb5b905e774a38d41
christos <christos@NetBSD.org>
2019-10-10 03:43:59 +0000

    fix the function pointer and callback mess: - callback functions return 0 and their result is not checked; make them void. - there are two types of callbacks and they used to overload their parameters   and the callback structure; separate them into "function" and "value"   callbacks. - make the wait function signature consistent.

--- sys/dev/raidframe/rf_netbsd.h
+++ sys/dev/raidframe/rf_netbsd.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: rf_netbsd.h,v 1.33 2019/02/06 02:49:09 oster Exp $	*/
+/*	$NetBSD: rf_netbsd.h,v 1.34 2019/10/10 03:43:59 christos Exp $	*/
 
 /*-
  * Copyright (c) 1996, 1997, 1998 The NetBSD Foundation, Inc.
@@ -61,7 +61,8 @@ struct RF_Pools_s {
 	struct pool asm_hdr;     /* Access Stripe Map Header */
 	struct pool asmap;       /* Access Stripe Map */
 	struct pool asmhle;      /* Access Stripe Map Header List Elements */
-	struct pool callback;    /* Callback descriptors */
+	struct pool callbackf;   /* Callback function descriptors */
+	struct pool callbackv;   /* Callback value descriptors */
 	struct pool dagh;        /* DAG headers */
 	struct pool dagnode;     /* DAG nodes */
 	struct pool daglist;     /* DAG lists */
