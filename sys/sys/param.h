9ac229fe321e94d15374ba59017c2d064365e63f
kamil <kamil@NetBSD.org>
2019-10-12 19:59:57 +0000

    Welcome to 9.99.17!
    struct proc change
    sigswitch() removed from public API

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.617 2019/10/12 06:31:04 maxv Exp $	*/
+/*	$NetBSD: param.h,v 1.618 2019/10/12 19:59:57 kamil Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -67,7 +67,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	999001600	/* NetBSD 9.99.16 */
+#define	__NetBSD_Version__	999001700	/* NetBSD 9.99.17 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
