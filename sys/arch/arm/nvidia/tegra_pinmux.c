4b890b4402714297cd2a6c0b4e9a541449daf65b
skrll <skrll@NetBSD.org>
2019-10-13 05:57:14 +0000

    Use PRIxBUSADDR

--- sys/arch/arm/nvidia/tegra_pinmux.c
+++ sys/arch/arm/nvidia/tegra_pinmux.c
@@ -1,4 +1,4 @@
-/* $NetBSD: tegra_pinmux.c,v 1.4 2019/10/01 23:32:52 jmcneill Exp $ */
+/* $NetBSD: tegra_pinmux.c,v 1.5 2019/10/13 05:57:14 skrll Exp $ */
 
 /*-
  * Copyright (c) 2015-2017 Jared McNeill <jmcneill@invisible.ca>
@@ -29,7 +29,7 @@
 #include "opt_tegra.h"
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: tegra_pinmux.c,v 1.4 2019/10/01 23:32:52 jmcneill Exp $");
+__KERNEL_RCSID(0, "$NetBSD: tegra_pinmux.c,v 1.5 2019/10/13 05:57:14 skrll Exp $");
 
 #include <sys/param.h>
 #include <sys/types.h>
@@ -273,8 +273,7 @@ tegra_pinmux_attach(device_t parent, device_t self, void *aux)
 		}
 		error = bus_space_map(sc->sc_bst, addr, size, res, &sc->sc_bsh[res]);
 		if (error) {
-			aprint_error(": couldn't map %#" PRIx64 ": %d",
-			    (uint64_t)addr, error);
+			aprint_error(": couldn't map %" PRIxBUSADDR ": %d", addr, error);
 			return;
 		}
 	}
