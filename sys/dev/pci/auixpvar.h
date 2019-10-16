4885c963b8824a366411a1e21a961a5d63c38efc
maya <maya@NetBSD.org>
2019-10-16 21:52:22 +0000

    Switch files copyright Reinoud Zandijk from BSD 4 clause to BSD 2 clause.
    OK'd by reinoud in email (from 4 Feb 2019).

--- sys/dev/pci/auixpvar.h
+++ sys/dev/pci/auixpvar.h
@@ -1,4 +1,4 @@
-/* $NetBSD: auixpvar.h,v 1.9 2019/05/08 13:40:18 isaki Exp $*/
+/* $NetBSD: auixpvar.h,v 1.10 2019/10/16 21:52:22 maya Exp $*/
 
 /*
  * Copyright (c) 2004, 2005 Reinoud Zandijk <reinoud@netbsd.org>
@@ -11,13 +11,6 @@
  *    notice, this list of conditions and the following disclaimer.
  * 2. The name of the author may not be used to endorse or promote products
  *    derived from this software without specific prior written permission.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed by the NetBSD
- *	Foundation, Inc. and its contributors.
- * 4. Neither the name of The NetBSD Foundation nor the names of its
- *    contributors may be used to endorse or promote products derived
- *    from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
