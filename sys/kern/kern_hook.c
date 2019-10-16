d2348edc56f9b1eeb3e90bfaac4dbbffff39b4a3
christos <christos@NetBSD.org>
2019-10-16 15:27:38 +0000

    Add void * function pointer casts. There are different ways to "fix" those warnings:     1. this one: add a void * cast (which I think is the least intrusive)     2. add pragmas to elide the warning     3. add intermediate inline conversion functions     4. change the called function prototypes, adding unused arguments and        converting some of the pointer arguments to void *.     5. make the functions varyadic (which defeats the purpose of checking)     6. pass command line flags to elide the warning I did try 3 and 4 and I was not pleased with the result (sys_ptrace_common.c) (3) added too much code and defines, and (4) made the regular use clumsy.

--- sys/kern/kern_hook.c
+++ sys/kern/kern_hook.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: kern_hook.c,v 1.6 2013/11/22 21:04:11 christos Exp $	*/
+/*	$NetBSD: kern_hook.c,v 1.7 2019/10/16 15:27:38 christos Exp $	*/
 
 /*-
  * Copyright (c) 1997, 1998, 1999, 2002, 2007, 2008 The NetBSD Foundation, Inc.
@@ -31,7 +31,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: kern_hook.c,v 1.6 2013/11/22 21:04:11 christos Exp $");
+__KERNEL_RCSID(0, "$NetBSD: kern_hook.c,v 1.7 2019/10/16 15:27:38 christos Exp $");
 
 #include <sys/param.h>
 #include <sys/malloc.h>
@@ -102,7 +102,7 @@ hook_proc_run(hook_list_t *list, struct proc *p)
 	struct hook_desc *hd;
 
 	LIST_FOREACH(hd, list, hk_list)
-		((void (*)(struct proc *, void *))*hd->hk_fn)(p, hd->hk_arg);
+		((void (*)(struct proc *, void *))(void *)*hd->hk_fn)(p, hd->hk_arg);
 }
 
 /*
@@ -201,7 +201,7 @@ static hook_list_t exechook_list = LIST_HEAD_INITIALIZER(exechook_list);
 void *
 exechook_establish(void (*fn)(struct proc *, void *), void *arg)
 {
-	return hook_establish(&exechook_list, (void (*)(void *))fn, arg);
+	return hook_establish(&exechook_list, (void (*)(void *))(void *)fn, arg);
 }
 
 void
@@ -228,7 +228,7 @@ exithook_establish(void (*fn)(struct proc *, void *), void *arg)
 	void *rv;
 
 	rw_enter(&exec_lock, RW_WRITER);
-	rv = hook_establish(&exithook_list, (void (*)(void *))fn, arg);
+	rv = hook_establish(&exithook_list, (void (*)(void *))(void *)fn, arg);
 	rw_exit(&exec_lock);
 	return rv;
 }
@@ -256,7 +256,7 @@ static hook_list_t forkhook_list = LIST_HEAD_INITIALIZER(forkhook_list);
 void *
 forkhook_establish(void (*fn)(struct proc *, struct proc *))
 {
-	return hook_establish(&forkhook_list, (void (*)(void *))fn, NULL);
+	return hook_establish(&forkhook_list, (void (*)(void *))(void *)fn, NULL);
 }
 
 void
@@ -274,7 +274,7 @@ doforkhooks(struct proc *p2, struct proc *p1)
 	struct hook_desc *hd;
 
 	LIST_FOREACH(hd, &forkhook_list, hk_list) {
-		((void (*)(struct proc *, struct proc *))*hd->hk_fn)
+		((void (*)(struct proc *, struct proc *))(void *)*hd->hk_fn)
 		    (p2, p1);
 	}
 }
