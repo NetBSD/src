09c48b6e7c4d8397ff597a42066d6fe0c4b31000
rin <rin@NetBSD.org>
2019-11-04 11:33:01 +0000

    regen

--- sys/compat/netbsd32/netbsd32_systrace_args.c
+++ sys/compat/netbsd32/netbsd32_systrace_args.c
@@ -1,4 +1,4 @@
-/* $NetBSD: netbsd32_systrace_args.c,v 1.35 2019/11/04 11:21:36 rin Exp $ */
+/* $NetBSD: netbsd32_systrace_args.c,v 1.36 2019/11/04 11:33:01 rin Exp $ */
 
 /*
  * System call argument to DTrace register array converstion.
@@ -3496,7 +3496,7 @@ systrace_args(register_t sysnum, const void *params, uintptr_t *uarg, size_t *n_
 	case 483: {
 		const struct netbsd32___getvfsstat90_args *p = params;
 		uarg[0] = (intptr_t) SCARG(p, buf).i32; /* netbsd32_statvfsp_t */
-		uarg[1] = SCARG(p, bufsize); /* size_t */
+		iarg[1] = SCARG(p, bufsize); /* netbsd32_size_t */
 		iarg[2] = SCARG(p, flags); /* int */
 		*n_args = 3;
 		break;
@@ -3523,7 +3523,7 @@ systrace_args(register_t sysnum, const void *params, uintptr_t *uarg, size_t *n_
 	case 486: {
 		const struct netbsd32___fhstatvfs190_args *p = params;
 		uarg[0] = (intptr_t) SCARG(p, fhp).i32; /* netbsd32_voidp */
-		uarg[1] = SCARG(p, fh_size); /* size_t */
+		iarg[1] = SCARG(p, fh_size); /* netbsd32_size_t */
 		uarg[2] = (intptr_t) SCARG(p, buf).i32; /* netbsd32_statvfsp_t */
 		iarg[3] = SCARG(p, flags); /* int */
 		*n_args = 4;
@@ -9470,7 +9470,7 @@ systrace_entry_setargdesc(int sysnum, int ndx, char *desc, size_t descsz)
 			p = "netbsd32_statvfsp_t";
 			break;
 		case 1:
-			p = "size_t";
+			p = "netbsd32_size_t";
 			break;
 		case 2:
 			p = "int";
@@ -9518,7 +9518,7 @@ systrace_entry_setargdesc(int sysnum, int ndx, char *desc, size_t descsz)
 			p = "netbsd32_voidp";
 			break;
 		case 1:
-			p = "size_t";
+			p = "netbsd32_size_t";
 			break;
 		case 2:
 			p = "netbsd32_statvfsp_t";
