f5398c898849a6c5565ffc6d7b969d5942f3adce
hannken <hannken@NetBSD.org>
2019-11-08 11:06:21 +0000

    dtrace_trap() gets called from alltraps() -> trap() with interrupts enabled so we cannot assert for interrupts disabled here.
    Should fix PR kern/54603: kernel panic when running dtruss

--- external/cddl/osnet/dev/dtrace/amd64/dtrace_subr.c
+++ external/cddl/osnet/dev/dtrace/amd64/dtrace_subr.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: dtrace_subr.c,v 1.13 2019/07/05 08:29:18 hannken Exp $	*/
+/*	$NetBSD: dtrace_subr.c,v 1.14 2019/11/08 11:06:21 hannken Exp $	*/
 
 /*
  * CDDL HEADER START
@@ -412,7 +412,11 @@ dtrace_trap(struct trapframe *frame, u_int type)
 	 */
 	nofault = (cpu_core[cpuid].cpuc_dtrace_flags & CPU_DTRACE_NOFAULT) != 0;
 	if (nofault) {
+#if 0
+		This assertion would always fire, we get called from
+		alltraps() -> trap() with interrupts enabled.
 		KASSERTMSG((x86_read_flags() & PSL_I) == 0, "interrupts enabled");
+#endif
 
 		/*
 		 * There are only a couple of trap types that are expected.
