51053250d3e5879674f81413b4cd0f13dd7444fd
tnn <tnn@NetBSD.org>
2019-11-11 09:24:56 +0000

    silence sign-conversion warning from clang

--- external/bsd/blacklist/lib/blacklist.c
+++ external/bsd/blacklist/lib/blacklist.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: blacklist.c,v 1.6 2019/11/06 20:50:01 christos Exp $	*/
+/*	$NetBSD: blacklist.c,v 1.7 2019/11/11 09:24:56 tnn Exp $	*/
 
 /*-
  * Copyright (c) 2014 The NetBSD Foundation, Inc.
@@ -33,7 +33,7 @@
 #endif
 
 #include <sys/cdefs.h>
-__RCSID("$NetBSD: blacklist.c,v 1.6 2019/11/06 20:50:01 christos Exp $");
+__RCSID("$NetBSD: blacklist.c,v 1.7 2019/11/11 09:24:56 tnn Exp $");
 
 #include <stdio.h>
 #include <bl.h>
@@ -61,7 +61,7 @@ int
 blacklist_sa_r(struct blacklist *bl, int action, int rfd,
 	const struct sockaddr *sa, socklen_t slen, const char *msg)
 {
-	int internal_action;
+	bl_type_t internal_action;
 
 	/* internal values are not the same as user application values */
 	switch (action) {
