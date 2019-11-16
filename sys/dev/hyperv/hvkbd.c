dddd08be75082cc0ce79c566bc6aae7c1f14e033
martin <martin@NetBSD.org>
2019-11-16 16:23:31 +0000

    Pull up following revision(s) (requested by nonaka in ticket #1441):
    	sys/dev/hyperv/if_hvn.c: revision 1.6
    	sys/dev/hyperv/hvkbd.c: revision 1.4
    
    hvn(4), hvkbd(4): No need to call vmbus_channel_setdeferred().
    
    These devices do not perform batch reading.

--- sys/dev/hyperv/hvkbd.c
+++ sys/dev/hyperv/hvkbd.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: hvkbd.c,v 1.1.4.4 2019/06/13 06:12:55 martin Exp $	*/
+/*	$NetBSD: hvkbd.c,v 1.1.4.5 2019/11/16 16:23:31 martin Exp $	*/
 
 /*-
  * Copyright (c) 2017 Microsoft Corp.
@@ -36,7 +36,7 @@
 #endif /* _KERNEL_OPT */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: hvkbd.c,v 1.1.4.4 2019/06/13 06:12:55 martin Exp $");
+__KERNEL_RCSID(0, "$NetBSD: hvkbd.c,v 1.1.4.5 2019/11/16 16:23:31 martin Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -214,12 +214,6 @@ hvkbd_attach(device_t parent, device_t self, void *aux)
 		aprint_error_dev(self,
 		    "failed to allocate channel data buffer\n");
 		return;
-	}
-
-	if (vmbus_channel_setdeferred(sc->sc_chan, device_xname(self))) {
-		aprint_error_dev(self,
-		    "failed to create the interrupt thread\n");
-		goto free_buf;
 	}
 
 	sc->sc_chan->ch_flags &= ~CHF_BATCHED;
--- sys/dev/hyperv/if_hvn.c
+++ sys/dev/hyperv/if_hvn.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: if_hvn.c,v 1.2.2.2 2019/03/09 17:10:19 martin Exp $	*/
+/*	$NetBSD: if_hvn.c,v 1.2.2.3 2019/11/16 16:23:31 martin Exp $	*/
 /*	$OpenBSD: if_hvn.c,v 1.39 2018/03/11 14:31:34 mikeb Exp $	*/
 
 /*-
@@ -35,7 +35,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: if_hvn.c,v 1.2.2.2 2019/03/09 17:10:19 martin Exp $");
+__KERNEL_RCSID(0, "$NetBSD: if_hvn.c,v 1.2.2.3 2019/11/16 16:23:31 martin Exp $");
 
 #ifdef _KERNEL_OPT
 #include "opt_inet.h"
@@ -933,12 +933,6 @@ hvn_nvs_attach(struct hvn_softc *sc)
 	    (HVN_TX_FRAGS + 1) * sizeof(struct vmbus_gpa));
 
 	sc->sc_chan->ch_flags &= ~CHF_BATCHED;
-
-	if (vmbus_channel_setdeferred(sc->sc_chan, device_xname(sc->sc_dev))) {
-		aprint_error_dev(sc->sc_dev,
-		    "failed to create the interrupt thread\n");
-		return -1;
-	}
 
 	/* Associate our interrupt handler with the channel */
 	if (vmbus_channel_open(sc->sc_chan, ringsize, NULL, 0,
