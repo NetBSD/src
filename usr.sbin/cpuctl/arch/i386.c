de6a10c185b5d5518b14f9096f36848db333b6e5
msaitoh <msaitoh@NetBSD.org>
2019-11-17 15:32:00 +0000

    0x7d and 0x7e are for 10th generation Core (Ice Lake).

--- usr.sbin/cpuctl/arch/i386.c
+++ usr.sbin/cpuctl/arch/i386.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: i386.c,v 1.107 2019/10/03 15:21:44 msaitoh Exp $	*/
+/*	$NetBSD: i386.c,v 1.108 2019/11/17 15:32:00 msaitoh Exp $	*/
 
 /*-
  * Copyright (c) 1999, 2000, 2001, 2006, 2007, 2008 The NetBSD Foundation, Inc.
@@ -57,7 +57,7 @@
 
 #include <sys/cdefs.h>
 #ifndef lint
-__RCSID("$NetBSD: i386.c,v 1.107 2019/10/03 15:21:44 msaitoh Exp $");
+__RCSID("$NetBSD: i386.c,v 1.108 2019/11/17 15:32:00 msaitoh Exp $");
 #endif /* not lint */
 
 #include <sys/types.h>
@@ -386,8 +386,8 @@ const struct cpu_cpuid_nameclass i386_cpuid_cpus[] = {
 				[0x6a] = "Future Xeon (Ice Lake)",
 				[0x6c] = "Future Xeon (Ice Lake)",
 				[0x7a] = "Atom (Goldmont Plus)",
-				[0x7d] = "Future Core (Ice Lake)",
-				[0x7e] = "Future Core (Ice Lake)",
+				[0x7d] = "10th gen Core (Ice Lake)",
+				[0x7e] = "10th gen Core (Ice Lake)",
 				[0x85] = "Xeon Phi 7215, 7285, 7295 (Knights Mill)",
 				[0x86] = "Atom (Tremont)",
 				[0x8e] = "7th or 8th gen Core (Kaby Lake, Coffee Lake) or Xeon E (Coffee Lake)",
