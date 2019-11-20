1d577fe3790e662ec592913bd86905a501cfc612
pgoyette <pgoyette@NetBSD.org>
2019-11-20 19:37:51 +0000

    Move all non-emulation-specific coredump code into the coredump module, and remove all #ifdef COREDUMP conditional compilation.  Now, the coredump module is completely separated from the emulation modules, and they can all be independently loaded and unloaded.
    Welcome to 9.99.18 !

--- sys/kern/core_elf32.c
+++ sys/kern/core_elf32.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: core_elf32.c,v 1.58 2019/01/22 03:44:44 kamil Exp $	*/
+/*	$NetBSD: core_elf32.c,v 1.59 2019/11/20 19:37:53 pgoyette Exp $	*/
 
 /*
  * Copyright (c) 2001 Wasabi Systems, Inc.
@@ -40,10 +40,9 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(1, "$NetBSD: core_elf32.c,v 1.58 2019/01/22 03:44:44 kamil Exp $");
+__KERNEL_RCSID(1, "$NetBSD: core_elf32.c,v 1.59 2019/11/20 19:37:53 pgoyette Exp $");
 
 #ifdef _KERNEL_OPT
-#include "opt_coredump.h"
 #include "opt_compat_netbsd32.h"
 #endif
 
@@ -60,12 +59,11 @@ __KERNEL_RCSID(1, "$NetBSD: core_elf32.c,v 1.58 2019/01/22 03:44:44 kamil Exp $"
 #include <sys/ptrace.h>
 #include <sys/kmem.h>
 #include <sys/kauth.h>
+#include <sys/compat_stub.h>
 
 #include <machine/reg.h>
 
 #include <uvm/uvm_extern.h>
-
-#ifdef COREDUMP
 
 struct writesegs_state {
 	Elf_Phdr *psections;
@@ -115,6 +113,9 @@ ELFNAMEEND(coredump)(struct lwp *l, struct coredump_iostate *cookie)
 	off_t notestart;
 	size_t notesize;
 	int error, i;
+#ifdef DIAGNOSTIC
+	off_t offset;
+#endif
 
 	struct note_state ns;
 	struct note_buf *nb;
@@ -144,7 +145,8 @@ ELFNAMEEND(coredump)(struct lwp *l, struct coredump_iostate *cookie)
 	 */
 
 	/* Pass 1: count the entries. */
-	npsections = uvm_coredump_count_segs(l->l_proc);
+	MODULE_HOOK_CALL(uvm_coredump_count_segs_hook,
+	    (l->l_proc), 0, npsections);
 	/* Allow for the PT_NOTE section. */
 	npsections++;
 
@@ -193,7 +195,8 @@ ELFNAMEEND(coredump)(struct lwp *l, struct coredump_iostate *cookie)
 #endif
 
 	/* Write out the ELF header. */
-	error = coredump_write(cookie, UIO_SYSSPACE, &ehdr, sizeof(ehdr));
+	MODULE_HOOK_CALL(coredump_write_hook, (cookie, UIO_SYSSPACE, &ehdr,
+	    sizeof(ehdr)), ENOSYS, error);
 	if (error)
 		goto out;
 
@@ -202,8 +205,8 @@ ELFNAMEEND(coredump)(struct lwp *l, struct coredump_iostate *cookie)
 		memset(&shdr, 0, sizeof(shdr));
 		shdr.sh_type = SHT_NULL;
 		shdr.sh_info = npsections;
-		error = coredump_write(cookie, UIO_SYSSPACE, &shdr,
-		    sizeof(shdr));
+		MODULE_HOOK_CALL(coredump_write_hook, (cookie, UIO_SYSSPACE,
+		    &shdr, sizeof(shdr)), ENOSYS, error);
 		if (error)
 			goto out;
 	}
@@ -218,8 +221,8 @@ ELFNAMEEND(coredump)(struct lwp *l, struct coredump_iostate *cookie)
 	ws.psections = psections;
 	ws.npsections = npsections - 1;
 	ws.p = l->l_proc;
-	error = uvm_coredump_walkmap(l->l_proc, ELFNAMEEND(coredump_getseghdrs),
-	    &ws);
+	MODULE_HOOK_CALL(uvm_coredump_walkmap_hook,
+	    (l->l_proc, ELFNAMEEND(coredump_getseghdrs), &ws), ENOSYS, error);
 	if (error)
 		goto out;
 	if (ws.npsections != 0) {
@@ -239,21 +242,25 @@ ELFNAMEEND(coredump)(struct lwp *l, struct coredump_iostate *cookie)
 	ws.psections->p_align = ELFROUNDSIZE;
 
 	/* Write the P-section headers followed by the PT_NOTE header */
-	error = coredump_write(cookie, UIO_SYSSPACE, psections, psectionssize);
+	MODULE_HOOK_CALL(coredump_write_hook, (cookie, UIO_SYSSPACE, psections,
+	    psectionssize), ENOSYS, error);
 	if (error)
 		goto out;
 
 #ifdef DIAGNOSTIC
-	if (coredump_offset(cookie) != notestart)
+	MODULE_HOOK_CALL(coredump_offset_hook, (cookie), 0, offset);
+	if (offset != notestart)
 		panic("coredump: offset %lld != notestart %lld",
-		    (long long) coredump_offset(cookie),
+		    (long long) offset,
 		    (long long) notestart);
 #endif
 
 	/* Write out the notes. */
 	for (nb = ns.ns_first; nb != NULL; nb = nb->nb_next) {
-		error = coredump_write(cookie, UIO_SYSSPACE, nb->nb_data,
-		    nb->nb_next == NULL ? ns.ns_offset : sizeof nb->nb_data);
+		MODULE_HOOK_CALL(coredump_write_hook, (cookie, UIO_SYSSPACE,
+		    nb->nb_data,
+		    nb->nb_next == NULL ? ns.ns_offset : sizeof nb->nb_data),
+		    ENOSYS, error);
 		if (error)
 			goto out;
 	}
@@ -264,15 +271,16 @@ ELFNAMEEND(coredump)(struct lwp *l, struct coredump_iostate *cookie)
 			continue;
 
 #ifdef DIAGNOSTIC
-		if (coredump_offset(cookie) != psections[i].p_offset)
+		MODULE_HOOK_CALL(coredump_offset_hook, (cookie), 0, offset);
+		if (offset != psections[i].p_offset)
 			panic("coredump: offset %lld != p_offset[%d] %lld",
-			    (long long) coredump_offset(cookie), i,
+			    (long long) offset, i,
 			    (long long) psections[i].p_filesz);
 #endif
 
-		error = coredump_write(cookie, UIO_USERSPACE,
+		MODULE_HOOK_CALL(coredump_write_hook, (cookie, UIO_USERSPACE,
 		    (void *)(vaddr_t)psections[i].p_vaddr,
-		    psections[i].p_filesz);
+		    psections[i].p_filesz), ENOSYS, error);
 		if (error)
 			goto out;
 	}
@@ -551,14 +559,3 @@ ELFNAMEEND(coredump_savenote)(struct note_state *ns, unsigned int type,
 	save_note_bytes(ns, name, nhdr.n_namesz);
 	save_note_bytes(ns, data, data_len);
 }
-
-#else	/* COREDUMP */
-
-int
-ELFNAMEEND(coredump)(struct lwp *l, struct coredump_iostate *cookie)
-{
-
-	return ENOSYS;
-}
-
-#endif	/* COREDUMP */
