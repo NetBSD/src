dcfcf77388a13d60da249b0c677e6b2561cf07da
christos <christos@NetBSD.org>
2019-11-24 19:56:50 +0000

    Import bind 9.14.8 (security fix -- limits on concurrent TCP queries)
            --- 9.14.8 released ---
    
    5315.	[bug]		Apply the inital RRSIG expiration spread fixed
    			to all dynamically created records in the zone
    			including NSEC3. Also fix the signature clusters
    			when the server has been offline for prolonged
    			period of times. [GL #1256]
    
    5314.	[func]		Added a new statistics variable "tcp-highwater"
    			that reports the maximum number of simultaneous TCP
    			clients BIND has handled while running. [GL #1206]
    
    5313.	[bug]		The default GeoIP2 database location did not match
    			the ARM.  'named -V' now reports the default
    			location. [GL #1301]
    
    5310.	[bug]		TCP failures were affecting EDNS statistics. [GL #1059]
    
    5308.	[bug]		Don't log DNS_R_UNCHANGED from sync_secure_journal()
    			at ERROR level in receive_secure_serial(). [GL #1288]
    
    5307.	[bug]		Fix hang when named-compilezone output is sent to pipe.
    			Thanks to Tony Finch. [GL !2481]
    
    5306.	[security]	Set a limit on the number of concurrently served
    			pipelined TCP queries. (CVE-2019-6477) [GL #1264]
    
    5305.	[bug]		NSEC Aggressive Cache ("synth-from-dnssec") has been
    			disabled by default because it was found to have
    			a significant performance impact on the recursive
    			service. [GL #1265]
    
    5304.	[bug]		"dnskey-sig-validity 0;" was not being accepted.
    			[GL #876]
    
    5302.	[bug]		Fix checking that "dnstap-output" is defined when
    			"dnstap" is specified in a view. [GL #1281]
    
    5301.	[bug]		Detect partial prefixes / incomplete IPv4 address in
    			acls. [GL #1143]

--- external/mpl/bind/dist/bin/tests/system/filter-aaaa/tests.sh
+++ external/mpl/bind/dist/bin/tests/system/filter-aaaa/tests.sh
@@ -166,7 +166,7 @@ status=`expr $status + $ret`
 
 n=`expr $n + 1`
 echo_i "checking that AAAA is returned when both AAAA and A record exists, unsigned over IPv6 ($n)"
-if $TESTSOCK6 fd92:7065:b8e:ffff::1
+if testsock6 fd92:7065:b8e:ffff::1
 then
 ret=0
 $DIG $DIGOPTS aaaa dual.unsigned -b fd92:7065:b8e:ffff::1 @fd92:7065:b8e:ffff::1 > dig.out.ns1.test$n || ret=1
@@ -207,7 +207,7 @@ status=`expr $status + $ret`
 
 n=`expr $n + 1`
 echo_i "checking that AAAA is included in additional section, qtype=MX, unsigned, over IPv6 ($n)"
-if $TESTSOCK6 fd92:7065:b8e:ffff::1
+if testsock6 fd92:7065:b8e:ffff::1
 then
 ret=0
 $DIG $DIGOPTS +add +dnssec mx unsigned -b fd92:7065:b8e:ffff::1 @fd92:7065:b8e:ffff::1 > dig.out.ns1.test$n || ret=1
@@ -340,7 +340,7 @@ status=`expr $status + $ret`
 
 n=`expr $n + 1`
 echo_i "checking that AAAA is returned when both AAAA and A record exists, unsigned over IPv6 with break-dnssec ($n)"
-if $TESTSOCK6 fd92:7065:b8e:ffff::4
+if testsock6 fd92:7065:b8e:ffff::4
 then
 ret=0
 $DIG $DIGOPTS aaaa dual.unsigned -b fd92:7065:b8e:ffff::4 @fd92:7065:b8e:ffff::4 > dig.out.ns4.test$n || ret=1
@@ -378,7 +378,7 @@ status=`expr $status + $ret`
 
 n=`expr $n + 1`
 echo_i "checking that AAAA is included in additional section, qtype=MX, unsigned, over IPv6, with break-dnssec ($n)"
-if $TESTSOCK6 fd92:7065:b8e:ffff::4
+if testsock6 fd92:7065:b8e:ffff::4
 then
 ret=0
 $DIG $DIGOPTS +add +dnssec mx unsigned -b fd92:7065:b8e:ffff::4 @fd92:7065:b8e:ffff::4 > dig.out.ns4.test$n || ret=1
@@ -513,7 +513,7 @@ status=`expr $status + $ret`
 
 n=`expr $n + 1`
 echo_i "checking that AAAA is returned when both AAAA and A record exists, unsigned over IPv6, recursive ($n)"
-if $TESTSOCK6 fd92:7065:b8e:ffff::2
+if testsock6 fd92:7065:b8e:ffff::2
 then
 ret=0
 $DIG $DIGOPTS aaaa dual.unsigned -b fd92:7065:b8e:ffff::2 @fd92:7065:b8e:ffff::2 > dig.out.ns2.test$n || ret=1
@@ -557,7 +557,7 @@ status=`expr $status + $ret`
 
 n=`expr $n + 1`
 echo_i "checking that AAAA is included in additional section, qtype=MX, unsigned, recursive, over IPv6 ($n)"
-if $TESTSOCK6 fd92:7065:b8e:ffff::2
+if testsock6 fd92:7065:b8e:ffff::2
 then
 ret=0
 $DIG $DIGOPTS +add +dnssec mx unsigned -b fd92:7065:b8e:ffff::2 @fd92:7065:b8e:ffff::2 > dig.out.ns2.test$n || ret=1
@@ -690,7 +690,7 @@ status=`expr $status + $ret`
 
 n=`expr $n + 1`
 echo_i "checking that AAAA is returned when both AAAA and A record exists, unsigned over IPv6, recursive with break-dnssec ($n)"
-if $TESTSOCK6 fd92:7065:b8e:ffff::3
+if testsock6 fd92:7065:b8e:ffff::3
 then
 ret=0
 $DIG $DIGOPTS aaaa dual.unsigned -b fd92:7065:b8e:ffff::3 @fd92:7065:b8e:ffff::3 > dig.out.ns3.test$n || ret=1
@@ -729,7 +729,7 @@ status=`expr $status + $ret`
 
 n=`expr $n + 1`
 echo_i "checking that AAAA is included in additional section, qtype=MX, unsigned, over IPv6, recursive with break-dnssec ($n)"
-if $TESTSOCK6 fd92:7065:b8e:ffff::3
+if testsock6 fd92:7065:b8e:ffff::3
 then
 ret=0
 $DIG $DIGOPTS +add +dnssec mx unsigned -b fd92:7065:b8e:ffff::3 @fd92:7065:b8e:ffff::3 > dig.out.ns3.test$n || ret=1
@@ -740,11 +740,12 @@ else
 echo_i "skipped."
 fi
 
-$TESTSOCK6 fd92:7065:b8e:ffff::1 || {
-        echo_i "IPv6 address not configured; skipping IPv6 query tests"
-        echo_i "exit status: $status"
-        exit $status
-}
+if ! testsock6 fd92:7065:b8e:ffff::1
+then
+	echo_i "IPv6 address not configured; skipping IPv6 query tests"
+	echo_i "exit status: $status"
+	exit $status
+fi
 
 # Reconfiguring for IPv6 tests
 echo_i "reconfiguring servers"
