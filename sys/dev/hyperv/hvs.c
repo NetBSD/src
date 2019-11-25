9681800d69fa0f371005ec29da0d6ac58cf5f04a
martin <martin@NetBSD.org>
2019-11-25 16:01:25 +0000

    Pull up following revision(s) (requested by nonaka in ticket #1458):
    	sys/dev/hyperv/hvs.c: revision 1.3
    	sys/dev/hyperv/if_hvn.c: revision 1.10
    
    hvs(4), hvn(4): Set correct size for bus_dmamap_sync(9).

--- sys/dev/hyperv/hvs.c
+++ sys/dev/hyperv/hvs.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: hvs.c,v 1.1.2.2 2019/03/09 17:10:19 martin Exp $	*/
+/*	$NetBSD: hvs.c,v 1.1.2.3 2019/11/25 16:01:25 martin Exp $	*/
 /*	$OpenBSD: hvs.c,v 1.17 2017/08/10 17:22:48 mikeb Exp $	*/
 
 /*-
@@ -37,7 +37,7 @@
 /* #define HVS_DEBUG_IO */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: hvs.c,v 1.1.2.2 2019/03/09 17:10:19 martin Exp $");
+__KERNEL_RCSID(0, "$NetBSD: hvs.c,v 1.1.2.3 2019/11/25 16:01:25 martin Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -1095,7 +1095,8 @@ hvs_free_ccbs(struct hvs_softc *sc)
 		if (ccb->ccb_dmap == NULL)
 			continue;
 
-		bus_dmamap_sync(sc->sc_dmat, ccb->ccb_dmap, 0, 0,
+		bus_dmamap_sync(sc->sc_dmat, ccb->ccb_dmap,
+		    0, ccb->ccb_dmap->dm_mapsize,
 		    BUS_DMASYNC_POSTREAD | BUS_DMASYNC_POSTWRITE);
 		bus_dmamap_unload(sc->sc_dmat, ccb->ccb_dmap);
 		bus_dmamap_destroy(sc->sc_dmat, ccb->ccb_dmap);
