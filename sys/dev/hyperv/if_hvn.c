539863adfc4dacac12578ed8a32c0008f77b556b
martin <martin@NetBSD.org>
2019-11-26 08:22:51 +0000

    Pull up following revision(s) (requested by nonaka in ticket #484):
    	sys/dev/hyperv/if_hvn.c: revision 1.11
    
    hvn(4): Fix incorrect ident when waiting for NVS command response.

--- sys/dev/hyperv/if_hvn.c
+++ sys/dev/hyperv/if_hvn.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: if_hvn.c,v 1.4.2.4 2019/11/25 16:58:54 martin Exp $	*/
+/*	$NetBSD: if_hvn.c,v 1.4.2.5 2019/11/26 08:22:51 martin Exp $	*/
 /*	$OpenBSD: if_hvn.c,v 1.39 2018/03/11 14:31:34 mikeb Exp $	*/
 
 /*-
@@ -35,7 +35,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: if_hvn.c,v 1.4.2.4 2019/11/25 16:58:54 martin Exp $");
+__KERNEL_RCSID(0, "$NetBSD: if_hvn.c,v 1.4.2.5 2019/11/26 08:22:51 martin Exp $");
 
 #ifdef _KERNEL_OPT
 #include "opt_inet.h"
@@ -1097,7 +1097,8 @@ hvn_nvs_cmd(struct hvn_softc *sc, void *cmd, size_t cmdsize, uint64_t tid,
 			hvn_nvs_intr(sc);
 			splx(s);
 		} else
-			tsleep(sc, PRIBIO | PCATCH, "nvscmd", mstohz(1));
+			tsleep(sc->sc_nvsrsp, PRIBIO | PCATCH, "nvscmd",
+			    mstohz(1));
 	} while (--timo > 0 && sc->sc_nvsdone != 1);
 
 	if (timo == 0 && sc->sc_nvsdone != 1) {
