37ebfa176c1557eb44da114c0fa31244a0b3f661
martin <martin@NetBSD.org>
2019-11-26 08:12:26 +0000

    Pull up following revision(s) (requested by nakayama in ticket #469):
    	lib/libc/tls/tls.c: revision 1.10
    	lib/libc/tls/tls.c: revision 1.11
    	lib/libc/tls/tls.c: revision 1.12
    	lib/libc/tls/tls.c: revision 1.13
    	libexec/ld.elf_so/tls.c: revision 1.13
    	libexec/ld.elf_so/tls.c: revision 1.14
    	libexec/ld.elf_so/Makefile: revision 1.142
    	lib/libc/tls/Makefile.inc: revision 1.3
    	usr.bin/ldd/Makefile.elf: revision 1.6
    
    PR 54093: Align static TLS area to max_align_t.
    Use alignof and not size_t for platforms with non-natural base
    alignments.
    
    Mirror the ld.elf_so logic for handling aligning the TLS size.
    Most noticable, recompute the start of the TLS area for variant I
    relative to the TCB. This makes a difference when the segment size and
    base alignment don't agree.
    
    Fix PR/54074 and PR/54093 completely.
    More similar to the ld.elf_so logic, it is necessary to align with
    p_align first.  Also, invert the #ifdef condition for consistency.
    
    Should fix regression for static linking binaries:
    http://releng.netbsd.org/b5reports/sparc/commits-2019.11.html#2019.11.10.23.39.03
    http://releng.netbsd.org/b5reports/sparc64/commits-2019.11.html#2019.11.16.04.10.33

--- libexec/ld.elf_so/tls.c
+++ libexec/ld.elf_so/tls.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: tls.c,v 1.12 2019/04/13 00:23:32 rin Exp $	*/
+/*	$NetBSD: tls.c,v 1.12.2.1 2019/11/26 08:12:26 martin Exp $	*/
 /*-
  * Copyright (c) 2011 The NetBSD Foundation, Inc.
  * All rights reserved.
@@ -29,11 +29,13 @@
  */
 
 #include <sys/cdefs.h>
-__RCSID("$NetBSD: tls.c,v 1.12 2019/04/13 00:23:32 rin Exp $");
+__RCSID("$NetBSD: tls.c,v 1.12.2.1 2019/11/26 08:12:26 martin Exp $");
 
 #include <sys/param.h>
 #include <sys/ucontext.h>
 #include <lwp.h>
+#include <stdalign.h>
+#include <stddef.h>
 #include <string.h>
 #include "debug.h"
 #include "rtld.h"
@@ -99,7 +101,7 @@ _rtld_tls_initial_allocation(void)
 
 #ifndef __HAVE_TLS_VARIANT_I
 	_rtld_tls_static_space = roundup2(_rtld_tls_static_space,
-	    sizeof(void *));
+	    alignof(max_align_t));
 #endif
 	dbg(("_rtld_tls_static_space %zu", _rtld_tls_static_space));
 
