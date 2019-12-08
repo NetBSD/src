5dbe579cadee6a1cf1f327093a7faef6d2c74940
ad <ad@NetBSD.org>
2019-12-08 19:52:37 +0000

    Revert previous.  No performance gain worth the potential headaches with buffers in these contexts.

--- sys/ufs/lfs/lfs_segment.c
+++ sys/ufs/lfs/lfs_segment.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: lfs_segment.c,v 1.279 2019/12/08 19:24:26 ad Exp $	*/
+/*	$NetBSD: lfs_segment.c,v 1.280 2019/12/08 19:52:37 ad Exp $	*/
 
 /*-
  * Copyright (c) 1999, 2000, 2001, 2002, 2003 The NetBSD Foundation, Inc.
@@ -60,7 +60,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: lfs_segment.c,v 1.279 2019/12/08 19:24:26 ad Exp $");
+__KERNEL_RCSID(0, "$NetBSD: lfs_segment.c,v 1.280 2019/12/08 19:52:37 ad Exp $");
 
 #ifdef DEBUG
 # define vndebug(vp, str) do {						\
@@ -2197,7 +2197,7 @@ lfs_writeseg(struct lfs *fs, struct segment *sp)
 		if (unbusybp != NULL) {
 			unbusybp->b_cflags &= ~BC_BUSY;
 			if (unbusybp->b_cflags & BC_WANTED)
-				cv_signal(&bp->b_busy);
+				cv_broadcast(&bp->b_busy);
 		}
 	}
 	mutex_exit(&bufcache_lock);
