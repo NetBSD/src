725f92dd4345afce52cde4b8c4e3df5ba54de673
martin <martin@NetBSD.org>
2019-12-09 15:19:30 +0000

    Pull up following revision(s) (requested by jmcneill in ticket #525):
    	external/cddl/osnet/dev/dtrace/aarch64/dtrace_isa.c: revision 1.1
    	distrib/sets/lists/modules/md.i386: revision 1.83
    	share/mk/bsd.own.mk: revision 1.1168
    	usr.bin/mkubootimage/mkubootimage.c: revision 1.25
    	sys/modules/dtrace/Makefile: revision 1.7
    	usr.bin/mkubootimage/mkubootimage.c: revision 1.26
    	sys/modules/dtrace/Makefile: revision 1.8
    	external/cddl/osnet/dist/lib/libdtrace/aarch64/dt_isadep.c: revision 1.2
    	distrib/sets/lists/modules/mi: revision 1.128
    	sys/arch/aarch64/include/frame.h: revision 1.3
    	sys/arch/evbarm/conf/mk.generic64: revision 1.4
    	external/cddl/osnet/dist/lib/libdtrace/common/dt_link.c: revision 1.12
    	sys/modules/cyclic/Makefile: revision 1.4
    	sys/arch/aarch64/conf/Makefile.aarch64: revision 1.16
    	external/cddl/osnet/dev/dtrace/aarch64/dtrace_subr.c: revision 1.1
    	sys/arch/aarch64/aarch64/start.S: revision 1.3
    	sys/arch/aarch64/aarch64/trap.c: revision 1.22
    	external/cddl/osnet/dev/fbt/aarch64/fbt_isa.c: revision 1.1
    	external/cddl/osnet/dev/dtrace/aarch64/dtrace_asm.S: revision 1.1
    	external/cddl/osnet/dev/fbt/aarch64/fbt_isa.h: revision 1.1
    	external/cddl/osnet/dev/dtrace/aarch64/regset.h: revision 1.1
    	external/cddl/osnet/lib/libdtrace/Makefile: revision 1.26
    	distrib/sets/lists/modules/md.amd64: revision 1.82
    	usr.bin/mkubootimage/mkubootimage.1: revision 1.13
    	distrib/sets/lists/modules/ad.arm: revision 1.14
    
    Add KDTRACE_HOOKS support.
    
    Define lwp_trapframe() macro
    
    dtrace: add support for aarch64
    
    Add syscall_linux back for other arm architectures (accidently removed
    in previous)
    
    Add -u flag for updating headers in place.
    
    Fix alignment of .text section by changing load address to
    0xffffffc000000000 and adding 64 bytes of padding before the entry point.
    
    Update arm64 image header in place
    
    Move dtrace_syscall_linux out of mi set list
    
    Enable DTrace on aarch64
    
    Fix signed/unsigned comparison

--- share/mk/bsd.own.mk
+++ share/mk/bsd.own.mk
@@ -1,4 +1,4 @@
-#	$NetBSD: bsd.own.mk,v 1.1149.2.2 2019/12/08 15:54:34 martin Exp $
+#	$NetBSD: bsd.own.mk,v 1.1149.2.3 2019/12/09 15:19:30 martin Exp $
 
 # This needs to be before bsd.init.mk
 .if defined(BSD_MK_COMPAT_FILE)
@@ -1009,6 +1009,7 @@ MKZFS?=		yes
 #
 .if ${MACHINE_ARCH} == "i386" || \
     ${MACHINE_ARCH} == "x86_64" || \
+    ${MACHINE_ARCH} == "aarch64" || \
     !empty(MACHINE_ARCH:Mearm*)
 MKDTRACE?=	yes
 MKCTF?=		yes
