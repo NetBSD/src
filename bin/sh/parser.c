173f0901c1dde43d025e704e04a972286bef4770
kre <kre@NetBSD.org>
2019-12-10 09:18:37 +0000

    Correct a typo in a comment, 08x0 was meant to be 0x80 (duh!).  NFC.

--- bin/sh/parser.c
+++ bin/sh/parser.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: parser.c,v 1.168 2019/05/04 02:52:22 kre Exp $	*/
+/*	$NetBSD: parser.c,v 1.169 2019/12/10 09:18:37 kre Exp $	*/
 
 /*-
  * Copyright (c) 1991, 1993
@@ -37,7 +37,7 @@
 #if 0
 static char sccsid[] = "@(#)parser.c	8.7 (Berkeley) 5/16/95";
 #else
-__RCSID("$NetBSD: parser.c,v 1.168 2019/05/04 02:52:22 kre Exp $");
+__RCSID("$NetBSD: parser.c,v 1.169 2019/12/10 09:18:37 kre Exp $");
 #endif
 #endif /* not lint */
 
@@ -1749,7 +1749,7 @@ readcstyleesc(char *out)
  *
  * The current definition of ISCTL() allows the compiler to
  * optimise away either half, or all, of the test in most of
- * the cases here (0xc0 | anything) cannot be between 08x0 and 0x9f
+ * the cases here (0xc0 | anything) cannot be between 0x80 and 0x9f
  * for example, so there a test is not needed).
  *
  * Which tests can be removed depends upon the actual values
