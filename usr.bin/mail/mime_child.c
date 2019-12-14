3b3cbdd9715dad1eb5620bcd1cb1ab5d7d8cd759
christos <christos@NetBSD.org>
2019-12-14 20:21:43 +0000

    PR/54766: elo: Broken mime-hooks handling in mail(1) pullup-9.

--- usr.bin/mail/mime_child.c
+++ usr.bin/mail/mime_child.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: mime_child.c,v 1.9 2017/11/09 20:27:50 christos Exp $	*/
+/*	$NetBSD: mime_child.c,v 1.10 2019/12/14 20:21:43 christos Exp $	*/
 
 /*-
  * Copyright (c) 2006 The NetBSD Foundation, Inc.
@@ -34,7 +34,7 @@
 
 #include <sys/cdefs.h>
 #ifndef __lint__
-__RCSID("$NetBSD: mime_child.c,v 1.9 2017/11/09 20:27:50 christos Exp $");
+__RCSID("$NetBSD: mime_child.c,v 1.10 2019/12/14 20:21:43 christos Exp $");
 #endif /* not __lint__ */
 
 #include <assert.h>
@@ -159,7 +159,7 @@ mime_run_command(const char *cmd, FILE *fo)
 	default:	/* parent */
 		(void)close(p[READ]);
 
-		nfo = fdopen(p[WRITE], "wef");
+		nfo = fdopen(p[WRITE], "we");
 		if (nfo == NULL) {
 			warn("mime_run_command: fdopen");
 			(void)close(p[WRITE]);
@@ -203,7 +203,7 @@ mime_run_function(void (*fn)(FILE *, FILE *, void *), FILE *fo, void *cookie)
 
 	default:	/* parent */
 		(void)close(p[READ]);
-		nfo = fdopen(p[WRITE], "wef");
+		nfo = fdopen(p[WRITE], "we");
 		if (nfo == NULL) {
 			warn("run_function: fdopen");
 			(void)close(p[WRITE]);
