1e72df6a037fdd3c6d3014a2679ffff7daab84ca
tsutsui <tsutsui@NetBSD.org>
2019-12-15 16:48:25 +0000

    Remove clause 3 and 4 leftovers from TNF licenses in more sources.
    Confirmed by martin@ in PR/54760.

--- sys/dev/ic/advlib.c
+++ sys/dev/ic/advlib.c
@@ -1,4 +1,4 @@
-/*      $NetBSD: advlib.c,v 1.28 2019/10/06 00:48:19 uwe Exp $        */
+/*      $NetBSD: advlib.c,v 1.29 2019/12/15 16:48:27 tsutsui Exp $        */
 
 /*
  * Low level routines for the Advanced Systems Inc. SCSI controllers chips
@@ -16,13 +16,6 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *        This product includes software developed by the NetBSD
- *        Foundation, Inc. and its contributors.
- * 4. Neither the name of The NetBSD Foundation nor the names of its
- *    contributors may be used to endorse or promote products derived
- *    from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
  * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
@@ -53,7 +46,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: advlib.c,v 1.28 2019/10/06 00:48:19 uwe Exp $");
+__KERNEL_RCSID(0, "$NetBSD: advlib.c,v 1.29 2019/12/15 16:48:27 tsutsui Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
--- sys/dev/ic/advlib.h
+++ sys/dev/ic/advlib.h
@@ -1,4 +1,4 @@
-/*      $NetBSD: advlib.h,v 1.20 2019/10/06 00:48:19 uwe Exp $        */
+/*      $NetBSD: advlib.h,v 1.21 2019/12/15 16:48:27 tsutsui Exp $        */
 
 /*
  * Definitions for low level routines and data structures
@@ -17,13 +17,6 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *        This product includes software developed by the NetBSD
- *        Foundation, Inc. and its contributors.
- * 4. Neither the name of The NetBSD Foundation nor the names of its
- *    contributors may be used to endorse or promote products derived
- *    from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
  * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
