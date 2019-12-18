5d209a4a0f734ffd836bffdabbd17f0773132791
martin <martin@NetBSD.org>
2019-12-18 20:04:32 +0000

    Pull up following revision(s) (requested by tsutsui in ticket #570):
    	sys/compat/netbsd32/netbsd32_compat_50_sysv.c: revision 1.3
    	sys/dev/raidframe/rf_compat50.h: revision 1.6
    	sys/arch/emips/emips/bus_space.c: revision 1.3
    	sys/compat/net/if.h: revision 1.5
    	sys/arch/emips/stand/common/bootinfo.c: revision 1.2
    	sys/compat/common/sysv_msg_50.c: revision 1.5
    	sys/compat/common/kern_time_30.c: revision 1.8
    	sys/arch/emips/stand/common/bootinfo.h: revision 1.2
    	sys/arch/ia64/include/bus.h: revision 1.4
    	sys/arch/ia64/ia64/bus_space.c: revision 1.2
    	sys/compat/common/sysv_shm_50.c: revision 1.5
    	sys/dev/ic/adw.h: revision 1.15
    	sys/compat/common/uipc_syscalls_50.c: revision 1.10
    	sys/arch/emips/ebus/flash_ebus.c: revision 1.22
    	sys/dev/ic/adv.h: revision 1.15
    	sys/dev/ic/adwmcode.c: revision 1.18
    	sys/dev/ic/advlib.c: revision 1.29
    	sys/arch/hpcarm/include/kloader.h: revision 1.3
    	sys/dev/usb/uberry.c: revision 1.16
    	sys/compat/common/sysv_sem_50.c: revision 1.5
    	sys/compat/netbsd32/netbsd32_compat_50.c: revision 1.43
    	sys/dev/ic/advlib.h: revision 1.21
    	sys/dev/ic/adv.c: revision 1.50
    	sys/compat/netinet6/in6_var.h: revision 1.5
    	sys/arch/hpc/stand/hpcboot/arm/arm_sa1100_asm.asm: revision 1.2
    	sys/arch/emips/include/loadfile_machdep.h: revision 1.3
    	sys/arch/emips/stand/common/prom_iface.c: revision 1.7
    	sys/dev/ic/adw.c: revision 1.56
    	sys/dev/ic/adwmcode.h: revision 1.12
    	sys/dev/ic/advmcode.c: revision 1.10
    	sys/arch/emips/ebus/ace_ebus.c: revision 1.22
    	sys/compat/netbsd32/netbsd32_compat_60.c: revision 1.5
    	sys/dev/raidframe/rf_compat50.c: revision 1.13
    	sys/arch/x68k/dev/intiovar.h: revision 1.15
    	sys/dev/usb/uipad.c: revision 1.8
    	sys/arch/zaurus/include/kloader.h: revision 1.3
    	sys/arch/emips/stand/common/bootxx.c: revision 1.2
    	sys/dev/ic/adwlib.h: revision 1.23
    	sys/dev/ic/adwlib.c: revision 1.44
    	sys/compat/netbsd32/netbsd32_compat_16.c: revision 1.3
    	sys/arch/amigappc/include/intr.h: revision 1.27
    	sys/arch/x68k/dev/mfp.c: revision 1.27
    	sys/arch/arm/at91/at91dbgu.c: revision 1.17
    	sys/dev/ic/advmcode.h: revision 1.7
    	sys/compat/ultrix/ultrix_exec.h: revision 1.7
    	sys/compat/common/vfs_syscalls_50.c: revision 1.24
    	sys/arch/mips/cavium/octeon_dma.c: revision 1.3
    	sys/arch/hpc/stand/hpcboot/arm/arm_pxa2x0_asm.asm: revision 1.2
    
    Remove clause 3 and 4 from TNF licenses.
    Ok'ed by martin@ in PR/54760.
    
    Remove clause 3 and 4 leftovers from TNF licenses in more sources.
    Confirmed by martin@ in PR/54760.

--- sys/compat/netbsd32/netbsd32_compat_50_sysv.c
+++ sys/compat/netbsd32/netbsd32_compat_50_sysv.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: netbsd32_compat_50_sysv.c,v 1.2 2019/01/27 02:08:40 pgoyette Exp $	*/
+/*	$NetBSD: netbsd32_compat_50_sysv.c,v 1.2.4.1 2019/12/18 20:04:32 martin Exp $	*/
 
 /*-
  * Copyright (c) 2008 The NetBSD Foundation, Inc.
@@ -15,13 +15,6 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *        This product includes software developed by the NetBSD
- *        Foundation, Inc. and its contributors.
- * 4. Neither the name of The NetBSD Foundation nor the names of its
- *    contributors may be used to endorse or promote products derived
- *    from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
  * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
@@ -36,7 +29,7 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: netbsd32_compat_50_sysv.c,v 1.2 2019/01/27 02:08:40 pgoyette Exp $");
+__KERNEL_RCSID(0, "$NetBSD: netbsd32_compat_50_sysv.c,v 1.2.4.1 2019/12/18 20:04:32 martin Exp $");
 
 #if defined(_KERNEL_OPT)
 #include "opt_sysv.h"
--- sys/dev/ic/adv.c
+++ sys/dev/ic/adv.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: adv.c,v 1.48 2018/08/29 16:51:51 rin Exp $	*/
+/*	$NetBSD: adv.c,v 1.48.6.1 2019/12/18 20:04:33 martin Exp $	*/
 
 /*
  * Generic driver for the Advanced Systems Inc. Narrow SCSI controllers
@@ -16,13 +16,6 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *        This product includes software developed by the NetBSD
- *        Foundation, Inc. and its contributors.
- * 4. Neither the name of The NetBSD Foundation nor the names of its
- *    contributors may be used to endorse or promote products derived
- *    from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
  * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
@@ -38,7 +31,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: adv.c,v 1.48 2018/08/29 16:51:51 rin Exp $");
+__KERNEL_RCSID(0, "$NetBSD: adv.c,v 1.48.6.1 2019/12/18 20:04:33 martin Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
