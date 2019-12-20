f3db7741b1e5c26532afd4ce5cc48491298a5249
ad <ad@NetBSD.org>
2019-12-20 21:13:48 +0000

    NetBSD 9.99.27 - cpu_data changes for topology

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.627 2019/12/16 22:48:25 ad Exp $	*/
+/*	$NetBSD: param.h,v 1.628 2019/12/20 21:13:48 ad Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -67,7 +67,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	999002600	/* NetBSD 9.99.26 */
+#define	__NetBSD_Version__	999002700	/* NetBSD 9.99.27 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
