4dd47e48b3194236fc9a4d4320ddffb18b487497
ad <ad@NetBSD.org>
2019-12-22 22:24:37 +0000

    NetBSD 9.99.29 - struct mount changed.

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.629 2019/12/21 15:12:34 ad Exp $	*/
+/*	$NetBSD: param.h,v 1.630 2019/12/22 22:24:37 ad Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -67,7 +67,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	999002800	/* NetBSD 9.99.28 */
+#define	__NetBSD_Version__	999002900	/* NetBSD 9.99.29 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
