c27ed63bf1d8924af2db34f3d257760243baa2dd
martin <martin@NetBSD.org>
2019-12-24 17:30:28 +0000

    Pull up following revision(s) (requested by ryo in ticket #574):
    	sys/arch/aarch64/include/asm.h: revision 1.5
    	sys/arch/aarch64/aarch64/cpuswitch.S: revision 1.13
    
    Add a speculation barrier after the 'eret'.
    
    Some aarch64 cpus speculatively execute instructions after 'eret',
    and this potentiates side-channel attack.
    
    from
     https://github.com/torvalds/linux/commit/679db70801da9fda91d26caf13bf5b5ccc74e8e8

--- sys/arch/aarch64/include/asm.h
+++ sys/arch/aarch64/include/asm.h
@@ -1,4 +1,4 @@
-/* $NetBSD: asm.h,v 1.3 2018/07/17 18:08:37 christos Exp $ */
+/* $NetBSD: asm.h,v 1.3.4.1 2019/12/24 17:30:28 martin Exp $ */
 
 #ifndef _AARCH64_ASM_H_
 #define _AARCH64_ASM_H_
@@ -7,5 +7,13 @@
 
 #define	fp	x29
 #define	lr	x30
+
+/*
+ * Add a speculation barrier after the 'eret'.
+ * Some aarch64 cpus speculatively execute instructions after 'eret',
+ * and this potentiates side-channel attacks.
+ */
+#define	ERET	\
+	eret; dsb sy; isb
 
 #endif /* !_AARCH64_ASM_H_ */
