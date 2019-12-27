2ea75ea34e8ce293d27ff21f1991aa186fbc23a8
ad <ad@NetBSD.org>
2019-12-27 12:53:40 +0000

    NetBSD 9.99.31 - page allocator changes, struct vm_page changed

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.631 2019/12/24 15:20:34 kamil Exp $	*/
+/*	$NetBSD: param.h,v 1.632 2019/12/27 12:53:40 ad Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -67,7 +67,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	999003000	/* NetBSD 9.99.30 */
+#define	__NetBSD_Version__	999003100	/* NetBSD 9.99.31 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
