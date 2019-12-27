416c285388d33f40ce22879823fa7d81c2dc1c23
msaitoh <msaitoh@NetBSD.org>
2019-12-27 07:41:22 +0000

    s/opration/operation/

--- sbin/restore/restore.h
+++ sbin/restore/restore.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: restore.h,v 1.20 2011/08/06 20:46:42 dholland Exp $	*/
+/*	$NetBSD: restore.h,v 1.21 2019/12/27 07:41:23 msaitoh Exp $	*/
 
 /*
  * Copyright (c) 1983, 1993
@@ -61,7 +61,7 @@ extern int32_t	volno;		/* current volume being read */
 extern int32_t	ntrec;		/* number of TP_BSIZE records per tape block */
 extern time_t	dumptime;	/* time that this dump begins */
 extern time_t	dumpdate;	/* time that this dump was made */
-extern char	command;	/* opration being performed */
+extern char	command;	/* operation being performed */
 extern size_t	pagesize;	/* system page size */
 extern FILE	*terminal;	/* file descriptor for the terminal input */
 extern const char *tmpdir;	/* where to store temporary files */
