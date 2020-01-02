26a7276626f18f63a6c47ca48c367beea7ce2cec
martin <martin@NetBSD.org>
2020-01-02 09:43:56 +0000

    Pull up following revision(s) (requested by maxv in ticket #1480):
    	sys/dev/usb/uthum.c: revision 1.18
    	sys/dev/usb/ucycom.c: revision 1.49
    	sys/dev/usb/uhid.c: revision 1.111
    
    Fix buffer overflows. sc_{o,f}len are controlled by the USB device. By
    crafting the former the device can leak stack data. By crafting the latter
    the device can overwrite the stack. The combination of the two means the
    device can ROP the kernel and obtain code execution (demonstrated with an
    actual exploit over vHCI).
    
    Truncate the lengths to the size of the buffers, and also drop sc_ilen
    since it is unused. Patch tested with vHCI+kASan.
    
     -
    
    Fix buffer overflows. Also add missing mutex_exit.
    
     -
    
    Fix buffer overflows: validate the lengths at attach time, given that they
    are apparently not supposed to be variable. Drop sc_ilen since it is
    unused.

--- sys/dev/usb/uhid.c
+++ sys/dev/usb/uhid.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: uhid.c,v 1.99 2017/03/11 12:41:14 maya Exp $	*/
+/*	$NetBSD: uhid.c,v 1.99.6.1 2020/01/02 09:43:56 martin Exp $	*/
 
 /*
  * Copyright (c) 1998, 2004, 2008, 2012 The NetBSD Foundation, Inc.
@@ -35,7 +35,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: uhid.c,v 1.99 2017/03/11 12:41:14 maya Exp $");
+__KERNEL_RCSID(0, "$NetBSD: uhid.c,v 1.99.6.1 2020/01/02 09:43:56 martin Exp $");
 
 #ifdef _KERNEL_OPT
 #include "opt_compat_netbsd.h"
@@ -393,6 +393,8 @@ uhid_do_read(struct uhid_softc *sc, struct uio *uio, int flag)
 	if (sc->sc_state & UHID_IMMED) {
 		DPRINTFN(1, ("uhidread immed\n"));
 		extra = sc->sc_hdev.sc_report_id != 0;
+		if (sc->sc_isize + extra > sizeof(buffer))
+			return ENOBUFS;
 		err = uhidev_get_report(&sc->sc_hdev, UHID_INPUT_REPORT,
 					buffer, sc->sc_isize + extra);
 		if (err)
@@ -536,8 +538,10 @@ uhid_do_ioctl(struct uhid_softc *sc, u_long cmd, void *addr,
 	case FIOASYNC:
 		mutex_enter(proc_lock);
 		if (*(int *)addr) {
-			if (sc->sc_async != NULL)
+			if (sc->sc_async != NULL) {
+				mutex_exit(proc_lock);
 				return EBUSY;
+			}
 			sc->sc_async = l->l_proc;
 			DPRINTF(("uhid_do_ioctl: FIOASYNC %p\n", l->l_proc));
 		} else
@@ -584,6 +588,8 @@ uhid_do_ioctl(struct uhid_softc *sc, u_long cmd, void *addr,
 	case USB_SET_IMMED:
 		if (*(int *)addr) {
 			extra = sc->sc_hdev.sc_report_id != 0;
+			if (sc->sc_isize + extra > sizeof(buffer))
+				return ENOBUFS;
 			err = uhidev_get_report(&sc->sc_hdev, UHID_INPUT_REPORT,
 						buffer, sc->sc_isize + extra);
 			if (err)
@@ -610,6 +616,8 @@ uhid_do_ioctl(struct uhid_softc *sc, u_long cmd, void *addr,
 			return EINVAL;
 		}
 		extra = sc->sc_hdev.sc_report_id != 0;
+		if (size + extra > sizeof(re->ucr_data))
+			return ENOBUFS;
 		err = uhidev_get_report(&sc->sc_hdev, re->ucr_report,
 		    re->ucr_data, size + extra);
 		if (extra)
@@ -633,6 +641,8 @@ uhid_do_ioctl(struct uhid_softc *sc, u_long cmd, void *addr,
 		default:
 			return EINVAL;
 		}
+		if (size > sizeof(re->ucr_data))
+			return ENOBUFS;
 		err = uhidev_set_report(&sc->sc_hdev, re->ucr_report,
 		    re->ucr_data, size);
 		if (err)
