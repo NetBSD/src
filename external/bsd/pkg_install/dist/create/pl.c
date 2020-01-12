75d16b030ddea1e6d509147dfc70a85bc1528836
christos <christos@NetBSD.org>
2020-01-12 21:31:03 +0000

    Handle HAVE_NBTOOL_CONFIG_H

--- external/bsd/pkg_install/dist/create/pl.c
+++ external/bsd/pkg_install/dist/create/pl.c
@@ -1,5 +1,8 @@
-/*	$NetBSD: pl.c,v 1.2 2017/04/20 13:18:23 joerg Exp $	*/
+/*	$NetBSD: pl.c,v 1.3 2020/01/12 21:31:03 christos Exp $	*/
 
+#ifdef HAVE_NBTOOL_CONFIG_H
+#include "nbtool_config.h"
+#else
 #if HAVE_CONFIG_H
 #include "config.h"
 #endif
@@ -7,7 +10,8 @@
 #if HAVE_SYS_CDEFS_H
 #include <sys/cdefs.h>
 #endif
-__RCSID("$NetBSD: pl.c,v 1.2 2017/04/20 13:18:23 joerg Exp $");
+#endif
+__RCSID("$NetBSD: pl.c,v 1.3 2020/01/12 21:31:03 christos Exp $");
 
 /*
  * FreeBSD install - a package for the installation and maintainance
