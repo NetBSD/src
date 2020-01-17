b8f8bc2e89d3565b88de086abe0417699443a2fe
ad <ad@NetBSD.org>
2020-01-17 20:09:47 +0000

    set_page_dirty: take the vmobjlock if present

--- sys/external/bsd/drm2/include/linux/mm.h
+++ sys/external/bsd/drm2/include/linux/mm.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: mm.h,v 1.10 2020/01/15 17:55:44 ad Exp $	*/
+/*	$NetBSD: mm.h,v 1.11 2020/01/17 20:09:47 ad Exp $	*/
 
 /*-
  * Copyright (c) 2013 The NetBSD Foundation, Inc.
@@ -95,8 +95,16 @@ kvfree(void *ptr)
 static inline void
 set_page_dirty(struct page *page)
 {
+	struct vm_page *pg = &page->p_vmp;
 
-	uvm_pagemarkdirty(&page->p_vmp, UVM_PAGE_STATUS_DIRTY);
+	/* XXX */
+	if (pg->uobject != NULL) {
+		mutex_enter(pg->uobject->vmobjlock);
+		uvm_pagemarkdirty(pg, UVM_PAGE_STATUS_DIRTY);
+		mutex_exit(pg->uobject->vmobjlock);
+	} else {
+		uvm_pagemarkdirty(pg, UVM_PAGE_STATUS_DIRTY);
+	}
 }
 
 #endif  /* _LINUX_MM_H_ */
