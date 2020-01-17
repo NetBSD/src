3ad405da5bd61945887949ea707c0ca3bf984c29
christos <christos@NetBSD.org>
2020-01-17 00:39:27 +0000

    Don't remove the test harness utilities (the tests were not removed already which is the majority of the files), we can use them for atf now.

--- external/bsd/libarchive/prepare-import.sh
+++ external/bsd/libarchive/prepare-import.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# $NetBSD: prepare-import.sh,v 1.4 2019/07/24 14:07:17 joerg Exp $
+# $NetBSD: prepare-import.sh,v 1.5 2020/01/17 00:39:27 christos Exp $
 #
 # Extract the new tarball and rename the libarchive-X.Y.Z directory
 # to dist.  Run this script and check for additional files and
@@ -9,7 +9,7 @@ set -e
 
 cd dist
 
-rm -rf build contrib doc examples test_utils autom4te.cache
+rm -rf build contrib doc examples autom4te.cache
 rm -f INSTALL Makefile.am Makefile.in aclocal.m4 config.h.in
 rm -f configure configure.ac CMakeLists.txt */CMakeLists.txt */*/CMakeLists.txt
 rm -f */config_freebsd.h  CTestConfig.cmake .gitattributes .gitignore
