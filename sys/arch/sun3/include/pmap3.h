0c4ecdae819fe5bef51678052940316c04c7d64e
tsutsui <tsutsui@NetBSD.org>
2020-01-18 20:47:15 +0000

    Fix "modload nfsserver" failure on sun3.  PR/54869
    sun3 port has two definitions for different pmaps for sun3 (pmap3.h)
    and sun3x (pmap3x.h) and they are selected by _SUN3_ or _SUN3X_ macro,
    but neither of them is defined on module builds so common exported
    definitions required by module(7) should be in a common header,
    i.e. pmap.h.
    
    Should be pulled up to netbsd-9.

--- sys/arch/sun3/include/pmap3.h
+++ sys/arch/sun3/include/pmap3.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: pmap3.h,v 1.49 2013/01/07 16:58:09 chs Exp $	*/
+/*	$NetBSD: pmap3.h,v 1.50 2020/01/18 20:47:15 tsutsui Exp $	*/
 
 /*-
  * Copyright (c) 1996 The NetBSD Foundation, Inc.
@@ -63,8 +63,6 @@ extern segsz_t pmap_wired_pages(pmap_t);
 
 /* We use the PA plus some low bits for device mmap. */
 #define pmap_phys_address(addr) 	(addr)
-
-#define	pmap_update(pmap)		/* nothing (yet) */
 
 /* Map a given physical region to a virtual region */
 extern vaddr_t pmap_map(vaddr_t, paddr_t, paddr_t, int);
