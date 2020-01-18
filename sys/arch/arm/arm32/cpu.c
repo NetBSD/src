340bb0f1358348857cbb8ceac725b7340b276a12
skrll <skrll@NetBSD.org>
2020-01-18 14:40:03 +0000

    Use 4K pages on ARM_MMU_EXTENDED platforms (all armv[67] except RPI) by creating a new pool l1ttpl for the userland L1 translation table which needs to be 8KB and 8KB aligned.
    Limit the pool to maxproc and add hooks to allow the sysctl changing of
    maxproc to adjust the pool.
    
    This comes at a 5% performance penalty for build.sh -j8 kernel on a
    Tegra TK1.

--- sys/arch/arm/arm32/cpu.c
+++ sys/arch/arm/arm32/cpu.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: cpu.c,v 1.140 2020/01/15 08:34:04 mrg Exp $	*/
+/*	$NetBSD: cpu.c,v 1.141 2020/01/18 14:40:04 skrll Exp $	*/
 
 /*
  * Copyright (c) 1995 Mark Brinicombe.
@@ -46,7 +46,7 @@
 #include "opt_multiprocessor.h"
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: cpu.c,v 1.140 2020/01/15 08:34:04 mrg Exp $");
+__KERNEL_RCSID(0, "$NetBSD: cpu.c,v 1.141 2020/01/18 14:40:04 skrll Exp $");
 
 #include <sys/param.h>
 #include <sys/conf.h>
@@ -886,3 +886,12 @@ identify_features(device_t dv)
 	    "pfr: [0]=%#x [1]=%#x\n",
 	    cpu_processor_features[0], cpu_processor_features[1]);
 }
+
+#ifdef _ARM_ARCH_6
+int
+cpu_maxproc_hook(int nmaxproc)
+{
+
+	return pmap_maxproc_set(nmaxproc);
+}
+#endif
