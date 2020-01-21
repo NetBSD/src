08157cbc1e043d434c4bda6a73738c7b34f82887
martin <martin@NetBSD.org>
2020-01-21 15:19:51 +0000

    Pull up following revision(s) (requested by simonb in ticket #630):
    	sys/dev/ic/ahcisatavar.h: revision 1.24
    	sys/dev/pci/ahcisata_pci.c: revision 1.57
    	sys/dev/ic/ahcisata_core.c: revision 1.81
    
    Add new AHCI_QUIRK_BADNCQ quick for controllers that have issues with
    NCQ on (some) drives.  Enable this quirk for ATI (AMD) SB600/SB700
    controllers.  Alternate fix for kern/54790 and kern/54855.
    ok jdolecek@, tested on my SB700 chipset and tsutsui's SB600 chipset.

--- sys/dev/ic/ahcisatavar.h
+++ sys/dev/ic/ahcisatavar.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: ahcisatavar.h,v 1.22 2019/01/14 21:29:56 jdolecek Exp $	*/
+/*	$NetBSD: ahcisatavar.h,v 1.22.4.1 2020/01/21 15:19:51 martin Exp $	*/
 
 /*
  * Copyright (c) 2006 Manuel Bouyer.
@@ -59,6 +59,7 @@ struct ahci_softc {
 #define AHCI_PCI_QUIRK_BAD64	__BIT(1)  /* broken 64-bit DMA */
 #define AHCI_QUIRK_BADPMP	__BIT(2)  /* broken PMP support, ignore */
 #define AHCI_QUIRK_SKIP_RESET	__BIT(4)  /* skip drive reset sequence */
+#define AHCI_QUIRK_BADNCQ	__BIT(5)  /* possibly broken NCQ support, ignore */
 
 	uint32_t sc_ahci_cap;	/* copy of AHCI_CAP */
 	int sc_ncmds; /* number of command slots */
