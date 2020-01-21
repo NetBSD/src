9c54512d3b754cc27e53ebe0d39031fe0f59ee64
martin <martin@NetBSD.org>
2020-01-21 15:22:21 +0000

    Pull up following revision(s) (requested by nonaka in ticket #631):
    	sys/arch/i386/stand/efiboot/devopen.c: revision 1.10
    	sys/arch/i386/stand/efiboot/devopen.c: revision 1.11
    
    fix indent.
    
    x86 efiboot: Fixed the problem that /EFI/NetBSD/boot.cfg could not be loaded.

--- sys/arch/i386/stand/efiboot/devopen.c
+++ sys/arch/i386/stand/efiboot/devopen.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: devopen.c,v 1.5.6.3 2019/12/17 12:59:52 martin Exp $	 */
+/*	$NetBSD: devopen.c,v 1.5.6.4 2020/01/21 15:22:21 martin Exp $	 */
 
 /*-
  * Copyright (c) 2005 The NetBSD Foundation, Inc.
@@ -100,10 +100,10 @@ bios2dev(int biosdev, daddr_t sector, char **devname, int *unit,
 		*devname = "hd";
 
 	(void)biosdisk_findpartition(biosdev, sector, partition, part_name);
-	if (*part_name != NULL) {
+	if (part_name != NULL && *part_name != NULL) {
 		snprintf(savedevname, sizeof(savedevname),
 		    "NAME=%s", *part_name);
-			*devname = savedevname;
+		*devname = savedevname;
 	}
 }
 
