fcb8be35946605c68e69f0e0188c13f5173604bc
skrll <skrll@NetBSD.org>
2020-01-22 12:23:12 +0000

    #ifdef whack-a-mole for the pmap_maxproc_set functionality

--- sys/arch/arm/arm32/cpu.c
+++ sys/arch/arm/arm32/cpu.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: cpu.c,v 1.142 2020/01/20 22:13:57 skrll Exp $	*/
+/*	$NetBSD: cpu.c,v 1.143 2020/01/22 12:23:12 skrll Exp $	*/
 
 /*
  * Copyright (c) 1995 Mark Brinicombe.
@@ -46,7 +46,7 @@
 #include "opt_multiprocessor.h"
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: cpu.c,v 1.142 2020/01/20 22:13:57 skrll Exp $");
+__KERNEL_RCSID(0, "$NetBSD: cpu.c,v 1.143 2020/01/22 12:23:12 skrll Exp $");
 
 #include <sys/param.h>
 #include <sys/conf.h>
@@ -887,11 +887,15 @@ identify_features(device_t dv)
 	    cpu_processor_features[0], cpu_processor_features[1]);
 }
 
-#ifdef ARM_MMU_EXTENDED
+#ifdef _ARM_ARCH_6
 int
 cpu_maxproc_hook(int nmaxproc)
 {
 
+#ifdef ARM_MMU_EXTENDED
 	return pmap_maxproc_set(nmaxproc);
+#else
+	return 0;
+#endif
 }
 #endif
