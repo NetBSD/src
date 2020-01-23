5a62cdf921859274a04b6b7e3ae794202ebd6e2c
ad <ad@NetBSD.org>
2020-01-23 17:03:20 +0000

    NetBSD 9.99.42 - struct vnode changed.

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.644 2020/01/21 22:08:41 ad Exp $	*/
+/*	$NetBSD: param.h,v 1.645 2020/01/23 17:03:20 ad Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -67,7 +67,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	999004100	/* NetBSD 9.99.41 */
+#define	__NetBSD_Version__	999004200	/* NetBSD 9.99.42 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
