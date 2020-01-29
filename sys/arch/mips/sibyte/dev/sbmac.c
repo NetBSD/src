be348edd02f871379450231cdb92b91c3d7afe34
thorpej <thorpej@NetBSD.org>
2020-01-29 05:30:14 +0000

    Adopt <net/if_stats.h>.

--- sys/arch/mips/sibyte/dev/sbmac.c
+++ sys/arch/mips/sibyte/dev/sbmac.c
@@ -1,4 +1,4 @@
-/* $NetBSD: sbmac.c,v 1.61 2019/09/13 07:55:06 msaitoh Exp $ */
+/* $NetBSD: sbmac.c,v 1.62 2020/01/29 05:30:14 thorpej Exp $ */
 
 /*
  * Copyright 2000, 2001, 2004
@@ -33,7 +33,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: sbmac.c,v 1.61 2019/09/13 07:55:06 msaitoh Exp $");
+__KERNEL_RCSID(0, "$NetBSD: sbmac.c,v 1.62 2020/01/29 05:30:14 thorpej Exp $");
 
 #include "opt_inet.h"
 #include "opt_ns.h"
@@ -1016,7 +1016,7 @@ sbdma_tx_process(struct sbmac_softc *sc, sbmacdma_t *d)
 		/*
 		 * for transmits we just free buffers and count packets.
 		 */
-		ifp->if_opackets++;
+		if_statinc(ifp, if_opackets);
 		m_freem(m);
 
 		/*
