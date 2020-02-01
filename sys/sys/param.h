a5d99e7e2b1b32d5a8ffab704574a719ed8c3803
thorpej <thorpej@NetBSD.org>
2020-02-01 21:12:13 +0000

    NetBSD 9.99.45 -- struct ifnet changed, per-cpu ifnet stats.

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.647 2020/01/29 05:50:34 thorpej Exp $	*/
+/*	$NetBSD: param.h,v 1.648 2020/02/01 21:12:13 thorpej Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -67,7 +67,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	999004400	/* NetBSD 9.99.44 */
+#define	__NetBSD_Version__	999004500	/* NetBSD 9.99.45 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
