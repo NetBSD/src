c57f707c75a6ee7c5af76e3d3926d0ea0d03360f
jmcneill <jmcneill@NetBSD.org>
2020-02-02 16:31:25 +0000

    Add acpimcfg_conf_valid helper for pcie controllers with nonstandard config space layouts.

--- sys/dev/acpi/acpi_mcfg.h
+++ sys/dev/acpi/acpi_mcfg.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: acpi_mcfg.h,v 1.2 2018/10/15 10:01:32 jmcneill Exp $	*/
+/*	$NetBSD: acpi_mcfg.h,v 1.3 2020/02/02 16:31:25 jmcneill Exp $	*/
 
 /*-
  * Copyright (C) 2015 NONAKA Kimihiro <nonaka@NetBSD.org>
@@ -36,6 +36,7 @@ int	acpimcfg_configure_bus(device_t, pci_chipset_tag_t, ACPI_HANDLE, int, int);
 
 int	acpimcfg_conf_read(pci_chipset_tag_t, pcitag_t, int, pcireg_t *);
 int	acpimcfg_conf_write(pci_chipset_tag_t, pcitag_t, int, pcireg_t);
+bool	acpimcfg_conf_valid(pci_chipset_tag_t, pcitag_t, int);
 
 struct acpimcfg_ops {
 	/* validate MCFG memory region */
