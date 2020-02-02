398fa00409e57fb1f7bdebfa0c559061018c40ae
skrll <skrll@NetBSD.org>
2020-02-02 08:16:40 +0000

    Always pmap_kenter_pa the page in pmap_grow_map regardless of how we got it.

--- sys/arch/arm/arm32/pmap.c
+++ sys/arch/arm/arm32/pmap.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: pmap.c,v 1.384 2020/02/02 07:55:11 skrll Exp $	*/
+/*	$NetBSD: pmap.c,v 1.385 2020/02/02 08:16:40 skrll Exp $	*/
 
 /*
  * Copyright 2003 Wasabi Systems, Inc.
@@ -198,7 +198,7 @@
 #endif
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: pmap.c,v 1.384 2020/02/02 07:55:11 skrll Exp $");
+__KERNEL_RCSID(0, "$NetBSD: pmap.c,v 1.385 2020/02/02 08:16:40 skrll Exp $");
 
 #include <sys/param.h>
 #include <sys/types.h>
@@ -5815,9 +5815,6 @@ pmap_grow_map(vaddr_t va, paddr_t *pap)
 #else
 		if (uvm_page_physget(&pa) == false)
 			return (1);
-
-		pmap_kenter_pa(va, pa,
-		    VM_PROT_READ|VM_PROT_WRITE, PMAP_KMPAGE|PMAP_PTE);
 #endif	/* PMAP_STEAL_MEMORY */
 	} else {
 		struct vm_page *pg;
@@ -5831,9 +5828,10 @@ pmap_grow_map(vaddr_t va, paddr_t *pap)
 		 */
 		struct vm_page_md *md __diagused = VM_PAGE_TO_MD(pg);
 		KASSERT(SLIST_EMPTY(&md->pvh_list));
-		pmap_kenter_pa(va, pa,
-		    VM_PROT_READ|VM_PROT_WRITE, PMAP_KMPAGE|PMAP_PTE);
 	}
+
+	pmap_kenter_pa(va, pa,
+	    VM_PROT_READ|VM_PROT_WRITE, PMAP_KMPAGE|PMAP_PTE);
 
 	if (pap)
 		*pap = pa;
