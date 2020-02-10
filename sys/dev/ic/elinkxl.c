ed4fae0bb934d647f94eb5351e7ab4fb0d00df5c
msaitoh <msaitoh@NetBSD.org>
2020-02-10 07:48:02 +0000

    Restore an register read for RX_FRAMES_OK which was removed in rev. 1.133. All statistics registers should be read to ACK the interrupt. Fixes PR#54920.
    XXX pullup-[89]

--- sys/dev/ic/elinkxl.c
+++ sys/dev/ic/elinkxl.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: elinkxl.c,v 1.136 2020/02/07 00:56:48 thorpej Exp $	*/
+/*	$NetBSD: elinkxl.c,v 1.137 2020/02/10 07:48:02 msaitoh Exp $	*/
 
 /*-
  * Copyright (c) 1998 The NetBSD Foundation, Inc.
@@ -30,7 +30,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: elinkxl.c,v 1.136 2020/02/07 00:56:48 thorpej Exp $");
+__KERNEL_RCSID(0, "$NetBSD: elinkxl.c,v 1.137 2020/02/10 07:48:02 msaitoh Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -1522,6 +1522,7 @@ ex_getstats(struct ex_softc *sc)
 	(void)bus_space_read_1(iot, ioh, TX_AFTER_1_COLLISION);
 	(void)bus_space_read_1(iot, ioh, TX_NO_SQE);
 	(void)bus_space_read_1(iot, ioh, TX_CD_LOST);
+	(void)bus_space_read_1(iot, ioh, RX_FRAMES_OK);
 	GO_WINDOW(4);
 	(void)bus_space_read_1(iot, ioh, ELINK_W4_BADSSD);
 	GO_WINDOW(1);
