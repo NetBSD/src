5140997571a6cafc9a3c306999a869563fa2f512
riastradh <riastradh@NetBSD.org>
2020-02-14 04:36:33 +0000

    Provide necessary forward declarations or includes.

--- sys/sys/kauth.h
+++ sys/sys/kauth.h
@@ -1,4 +1,4 @@
-/* $NetBSD: kauth.h,v 1.82 2019/04/10 18:49:04 maxv Exp $ */
+/* $NetBSD: kauth.h,v 1.83 2020/02/14 04:36:33 riastradh Exp $ */
 
 /*-
  * Copyright (c) 2005, 2006 Elad Efrat <elad@NetBSD.org>  
@@ -45,6 +45,8 @@ struct proc;
 struct tty;
 struct vnode;
 struct cwdinfo;
+
+enum uio_seg;
 
 /* Types. */
 typedef struct kauth_scope     *kauth_scope_t;
