987938736aafb6a9b6375d22edba9e8458523484
ad <ad@NetBSD.org>
2020-02-15 17:14:30 +0000

    NetBSD 9.99.47 - added lwp_renumber()

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.649 2020/02/07 13:37:20 thorpej Exp $	*/
+/*	$NetBSD: param.h,v 1.650 2020/02/15 17:14:30 ad Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -67,7 +67,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	999004600	/* NetBSD 9.99.46 */
+#define	__NetBSD_Version__	999004700	/* NetBSD 9.99.47 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
