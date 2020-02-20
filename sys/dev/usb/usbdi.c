b10839eedc74dc16980bf9045a8ed059e3581ff0
martin <martin@NetBSD.org>
2020-02-20 14:53:09 +0000

    Pull up following revision(s) (requested by riastradh in ticket #716):
    	sys/dev/usb/usbdi.c: revision 1.194
    
    When aborting a pipe, set pipe->up_running = 0.
    
    This prevents any concurrent usb_transfer_complete from restarting
    the pipe with usbd_start_next.

--- sys/dev/usb/usbdi.c
+++ sys/dev/usb/usbdi.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: usbdi.c,v 1.182.4.1 2019/09/01 13:00:37 martin Exp $	*/
+/*	$NetBSD: usbdi.c,v 1.182.4.2 2020/02/20 14:53:09 martin Exp $	*/
 
 /*
  * Copyright (c) 1998, 2012, 2015 The NetBSD Foundation, Inc.
@@ -32,7 +32,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: usbdi.c,v 1.182.4.1 2019/09/01 13:00:37 martin Exp $");
+__KERNEL_RCSID(0, "$NetBSD: usbdi.c,v 1.182.4.2 2020/02/20 14:53:09 martin Exp $");
 
 #ifdef _KERNEL_OPT
 #include "opt_usb.h"
@@ -876,6 +876,7 @@ usbd_ar_pipe(struct usbd_pipe *pipe)
 		usbd_dump_queue(pipe);
 #endif
 	pipe->up_repeat = 0;
+	pipe->up_running = 0;
 	pipe->up_aborting = 1;
 	while ((xfer = SIMPLEQ_FIRST(&pipe->up_queue)) != NULL) {
 		USBHIST_LOG(usbdebug, "pipe = %#jx xfer = %#jx "
