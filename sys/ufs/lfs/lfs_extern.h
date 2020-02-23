8f7f3547875bface48d13762c871851af9db42a3
riastradh <riastradh@NetBSD.org>
2020-02-23 08:40:37 +0000

    Break deadlock in PR kern/52301.
    The lock order is lfs_writer -> lfs_seglock.  The problem in 52301 is
    that lfs_segwrite violates this lock order by sometimes doing
    lfs_seglock -> lfs_writer, either (a) when doing a checkpoint or (b),
    opportunistically, when there are no dirops pending.  Both cases can
    deadlock, because dirops sometimes take the seglock (lfs_truncate,
    lfs_valloc, lfs_vfree):
    
    (a) There may be dirops pending, and they may be waiting for the
    seglock, so we can't wait for them to complete while holding the
    seglock.
    
    (b) The test for fs->lfs_dirops == 0 happens unlocked, and the state
    may change by the time lfs_writer_enter acquires lfs_lock.
    
    To resolve this in each case:
    
    (a) Do lfs_writer_enter before lfs_seglock, since we will need it
    unconditionally anyway.  The worst performance impact of this should
    be that some dirops get delayed a little bit.
    
    (b) Create a new lfs_writer_tryenter to use at this point so that the
    test for fs->lfs_dirops == 0 and the acquisition of lfs_writer happen
    atomically under lfs_lock.

--- sys/ufs/lfs/lfs_extern.h
+++ sys/ufs/lfs/lfs_extern.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: lfs_extern.h,v 1.115 2020/02/18 20:23:17 chs Exp $	*/
+/*	$NetBSD: lfs_extern.h,v 1.116 2020/02/23 08:40:37 riastradh Exp $	*/
 
 /*-
  * Copyright (c) 1999, 2000, 2001, 2002, 2003 The NetBSD Foundation, Inc.
@@ -211,6 +211,7 @@ int lfs_seglock(struct lfs *, unsigned long);
 void lfs_segunlock(struct lfs *);
 void lfs_segunlock_relock(struct lfs *);
 int lfs_writer_enter(struct lfs *, const char *);
+int lfs_writer_tryenter(struct lfs *);
 void lfs_writer_leave(struct lfs *);
 void lfs_wakeup_cleaner(struct lfs *);
 
