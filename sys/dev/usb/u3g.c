f3098d3a388db516bef103a1beb9bc37f0791aa5
martin <martin@NetBSD.org>
2020-02-27 14:32:31 +0000

    Pull up following revision(s) (requested by manu in ticket #1508):
    	sys/dev/usb/usbdevs: revision 1.776
    	sys/dev/usb/u3g.c: revision 1.40
    	sys/dev/usb/umodeswitch.c: revision 1.5
    
    Add UE version of D-Link DWM-157
     -
    Add support for D-Link DWM-157 3G USB modem

--- sys/dev/usb/u3g.c
+++ sys/dev/usb/u3g.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: u3g.c,v 1.34 2017/05/24 20:23:58 christos Exp $	*/
+/*	$NetBSD: u3g.c,v 1.34.2.1 2020/02/27 14:32:31 martin Exp $	*/
 
 /*-
  * Copyright (c) 2009 The NetBSD Foundation, Inc.
@@ -50,7 +50,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: u3g.c,v 1.34 2017/05/24 20:23:58 christos Exp $");
+__KERNEL_RCSID(0, "$NetBSD: u3g.c,v 1.34.2.1 2020/02/27 14:32:31 martin Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -258,6 +258,10 @@ static const struct usb_devno u3g_devs[] = {
 	/* 4G Systems */
 	{ USB_VENDOR_4GSYSTEMS, USB_PRODUCT_4GSYSTEMS_XSSTICK_P14 },
 	{ USB_VENDOR_4GSYSTEMS, USB_PRODUCT_4GSYSTEMS_XSSTICK_W14 },
+
+	/* DLink */
+	{ USB_VENDOR_DLINK, USB_PRODUCT_DLINK_DWM157 },
+	{ USB_VENDOR_DLINK, USB_PRODUCT_DLINK_DWM157E },
 };
 
 /*
