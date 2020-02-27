37fcfad72ad09c97e2f10f7303284412a27d75f6
christos <christos@NetBSD.org>
2020-02-27 03:25:08 +0000

    This takes a provider now

--- lib/libpam/modules/pam_ssh/pam_ssh.c
+++ lib/libpam/modules/pam_ssh/pam_ssh.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: pam_ssh.c,v 1.27 2019/06/01 07:15:39 mlelstv Exp $	*/
+/*	$NetBSD: pam_ssh.c,v 1.28 2020/02/27 03:25:08 christos Exp $	*/
 
 /*-
  * Copyright (c) 2003 Networks Associates Technology, Inc.
@@ -38,7 +38,7 @@
 #ifdef __FreeBSD__
 __FBSDID("$FreeBSD: src/lib/libpam/modules/pam_ssh/pam_ssh.c,v 1.40 2004/02/10 10:13:21 des Exp $");
 #else
-__RCSID("$NetBSD: pam_ssh.c,v 1.27 2019/06/01 07:15:39 mlelstv Exp $");
+__RCSID("$NetBSD: pam_ssh.c,v 1.28 2020/02/27 03:25:08 christos Exp $");
 #endif
 
 #include <sys/param.h>
@@ -68,7 +68,7 @@ __RCSID("$NetBSD: pam_ssh.c,v 1.27 2019/06/01 07:15:39 mlelstv Exp $");
 #include "authfile.h"
 
 #define ssh_add_identity(auth, key, comment) \
-	ssh_add_identity_constrained(auth, key, comment, 0, 0, 0)
+	ssh_add_identity_constrained(auth, key, comment, 0, 0, 0, "pam")
 
 extern char **environ;
 
