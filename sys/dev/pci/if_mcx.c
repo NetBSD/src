538118214bc639145f5620d833e97b97782b6e62
thorpej <thorpej@NetBSD.org>
2020-02-29 18:07:57 +0000

    Use the 64-bit PCI DMA tag if available.  Otherwise, we are needlessly allocating (and using) bounce buffers on sytems with >4GB of RAM.
    XXX pullup-9

--- sys/dev/pci/if_mcx.c
+++ sys/dev/pci/if_mcx.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: if_mcx.c,v 1.10 2020/01/30 14:02:14 thorpej Exp $ */
+/*	$NetBSD: if_mcx.c,v 1.11 2020/02/29 18:07:57 thorpej Exp $ */
 /*	$OpenBSD: if_mcx.c,v 1.33 2019/09/12 04:23:59 jmatthew Exp $ */
 
 /*
@@ -2215,7 +2215,10 @@ mcx_attach(device_t parent, device_t self, void *aux)
 	sc->sc_dev = self;
 	sc->sc_pc = pa->pa_pc;
 	sc->sc_tag = pa->pa_tag;
-	sc->sc_dmat = pa->pa_dmat;
+	if (pci_dma64_available(pa))
+		sc->sc_dmat = pa->pa_dmat64;
+	else
+		sc->sc_dmat = pa->pa_dmat;
 
 	/* Map the PCI memory space */
 	memtype = pci_mapreg_type(sc->sc_pc, sc->sc_tag, MCX_HCA_BAR);
