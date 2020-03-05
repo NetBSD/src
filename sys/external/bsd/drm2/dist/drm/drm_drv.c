b59f353f78caed471f26fa3bd680d76f80005e62
riastradh <riastradh@NetBSD.org>
2020-03-05 07:46:59 +0000

    Avoid struct inode.
    This is an fs-independent structure in Linux.  We don't actually use
    it as such; it's just a dummy struct tag.  But we do have an actual
    struct inode in ufs and in lfs, and using the same struct tag here
    confuses ctf leading to four copies of pretty much every drm data
    structure.

--- sys/external/bsd/drm2/dist/drm/drm_drv.c
+++ sys/external/bsd/drm2/dist/drm/drm_drv.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: drm_drv.c,v 1.12 2020/02/14 14:34:57 maya Exp $	*/
+/*	$NetBSD: drm_drv.c,v 1.13 2020/03/05 07:46:59 riastradh Exp $	*/
 
 /*
  * Created: Fri Jan 19 10:48:35 2001 by faith@acm.org
@@ -29,7 +29,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: drm_drv.c,v 1.12 2020/02/14 14:34:57 maya Exp $");
+__KERNEL_RCSID(0, "$NetBSD: drm_drv.c,v 1.13 2020/03/05 07:46:59 riastradh Exp $");
 
 #include <linux/debugfs.h>
 #include <linux/fs.h>
@@ -540,16 +540,14 @@ EXPORT_SYMBOL(drm_unplug_dev);
 
 #ifdef __NetBSD__
 
-struct inode;
-
-static struct inode *
+static void *
 drm_fs_inode_new(void)
 {
 	return NULL;
 }
 
 static void
-drm_fs_inode_free(struct inode *inode)
+drm_fs_inode_free(void *inode)
 {
 	KASSERT(inode == NULL);
 }
