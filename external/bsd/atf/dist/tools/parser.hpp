6c725c7a20b078e86bb27bf3e3a1deb6fb240720
christos <christos@NetBSD.org>
2020-03-09 20:34:52 +0000

    remove -std=gnu++98

--- external/bsd/atf/dist/tools/parser.hpp
+++ external/bsd/atf/dist/tools/parser.hpp
@@ -324,7 +324,7 @@ class parser {
 
 public:
     parser(TKZ& tkz);
-    ~parser(void);
+    ~parser(void) noexcept(false);
 
     bool good(void) const;
     void add_error(const parse_error&);
@@ -386,7 +386,7 @@ parser< TKZ >::parser(TKZ& tkz) :
 }
 
 template< class TKZ >
-parser< TKZ >::~parser(void)
+parser< TKZ >::~parser(void) noexcept(false)
 {
     if (!m_errors.empty() && !m_thrown)
         throw m_errors;
