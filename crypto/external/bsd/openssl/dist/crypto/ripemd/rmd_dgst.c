864ade6957d015d9e27603a6ff1bc413ea03c3be
christos <christos@NetBSD.org>
2020-03-22 20:13:15 +0000

    reduce diff with upstream

--- crypto/external/bsd/openssl/dist/crypto/ripemd/rmd_dgst.c
+++ crypto/external/bsd/openssl/dist/crypto/ripemd/rmd_dgst.c
@@ -36,8 +36,8 @@ int RIPEMD160_Init(RIPEMD160_CTX *c)
 void ripemd160_block_data_order(RIPEMD160_CTX *ctx, const void *p, size_t num)
 {
     const unsigned char *data = p;
-    register MD32_REG_T A, B, C, D, E;
-    MD32_REG_T a, b, c, d, e, l;
+    register unsigned MD32_REG_T A, B, C, D, E;
+    unsigned MD32_REG_T a, b, c, d, e, l;
 # ifndef MD32_XARRAY
     /* See comment in crypto/sha/sha_local.h for details. */
     unsigned MD32_REG_T XX0, XX1, XX2, XX3, XX4, XX5, XX6, XX7,
