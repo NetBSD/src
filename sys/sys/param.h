47a37260d1d411778338ec82af4b151550570521
ad <ad@NetBSD.org>
2020-03-26 19:47:23 +0000

    NetBSD 9.99.52 - sleepq_t & kcondvar_t shrunk

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.654 2020/03/22 14:43:05 ad Exp $	*/
+/*	$NetBSD: param.h,v 1.655 2020/03/26 19:47:23 ad Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -67,7 +67,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	999005100	/* NetBSD 9.99.51 */
+#define	__NetBSD_Version__	999005200	/* NetBSD 9.99.52 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
