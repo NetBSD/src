bd973ebcc1ffcc450317a2bc6bc5ba7fbd28c80b
christos <christos@NetBSD.org>
2020-03-28 19:49:25 +0000

    merge conflicts

--- sys/external/bsd/acpica/dist/compiler/dtutils.c
+++ sys/external/bsd/acpica/dist/compiler/dtutils.c
@@ -5,7 +5,7 @@
  *****************************************************************************/
 
 /*
- * Copyright (C) 2000 - 2019, Intel Corp.
+ * Copyright (C) 2000 - 2020, Intel Corp.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -81,7 +81,8 @@ DtError (
 
     /* Check if user wants to ignore this exception */
 
-    if (AslIsExceptionIgnored (Level, MessageId))
+    if (AslIsExceptionIgnored (AslGbl_Files[ASL_FILE_INPUT].Filename,
+        FieldObject->Line, Level, MessageId))
     {
         return;
     }
