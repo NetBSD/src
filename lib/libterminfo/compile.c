9add22a6eed4e6d3a34c080ceeed71e9cc9e08db
roy <roy@NetBSD.org>
2020-03-28 15:27:54 +0000

    terminfo: Add a comment for prior change.

--- lib/libterminfo/compile.c
+++ lib/libterminfo/compile.c
@@ -1,4 +1,4 @@
-/* $NetBSD: compile.c,v 1.18 2020/03/28 15:25:53 roy Exp $ */
+/* $NetBSD: compile.c,v 1.19 2020/03/28 15:27:54 roy Exp $ */
 
 /*
  * Copyright (c) 2009, 2010, 2011, 2020 The NetBSD Foundation, Inc.
@@ -32,7 +32,7 @@
 #endif
 
 #include <sys/cdefs.h>
-__RCSID("$NetBSD: compile.c,v 1.18 2020/03/28 15:25:53 roy Exp $");
+__RCSID("$NetBSD: compile.c,v 1.19 2020/03/28 15:27:54 roy Exp $");
 
 #if !HAVE_NBTOOL_CONFIG_H || HAVE_SYS_ENDIAN_H
 #include <sys/endian.h>
@@ -158,6 +158,10 @@ _ti_getname(int rtype, const char *orig)
 	char *name;
 
 	if (rtype == TERMINFO_RTYPE) {
+		/* , and | are the two print characters now allowed
+		 * in terminfo aliases or long descriptions.
+		 * As | is generally used to delimit aliases inside the
+		 * description, we use a comma. */
 		if (asprintf(&name, "%s,v3", orig) < 0)
 			name = NULL;
 	} else {
