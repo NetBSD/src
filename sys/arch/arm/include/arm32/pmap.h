f7c02910c11fd2adeaa4aafc53425c3b09be3004
skrll <skrll@NetBSD.org>
2020-03-29 09:01:32 +0000

    Provide a pt_entry_t and PRIxPTE

--- sys/arch/arm/include/arm32/pmap.h
+++ sys/arch/arm/include/arm32/pmap.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: pmap.h,v 1.164 2020/03/14 14:05:42 ad Exp $	*/
+/*	$NetBSD: pmap.h,v 1.165 2020/03/29 09:01:32 skrll Exp $	*/
 
 /*
  * Copyright (c) 2002, 2003 Wasabi Systems, Inc.
@@ -1185,6 +1185,12 @@ do {									\
 } while (/*CONSTCOND*/0)
 
 #endif /* !_LOCORE */
+
+#ifndef	__BSD_PTENTRY_T__
+#define	__BSD_PTENTRY_T__
+typedef uint32_t pt_entry_t;
+#define PRIxPTE		PRIx32
+#endif
 
 #endif /* _KERNEL */
 
