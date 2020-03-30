678f5c5b54da57de307c386008f6c31fb4c1d50e
wiz <wiz@NetBSD.org>
2020-03-30 09:32:46 +0000

    Match indentation of previous entries.

--- sys/sys/disklabel.h
+++ sys/sys/disklabel.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: disklabel.h,v 1.123 2020/03/30 09:19:06 martin Exp $	*/
+/*	$NetBSD: disklabel.h,v 1.124 2020/03/30 09:32:46 wiz Exp $	*/
 
 /*
  * Copyright (c) 1987, 1988, 1993
@@ -399,7 +399,7 @@ x(MINIXFS3,29, "MINIX FSv3", NULL,   NULL)    /* MINIX file system v3 */ \
 x(VMKCORE, 30, "VMware vmkcore", NULL, NULL)  /* VMware vmkcore */ \
 x(VMFS,    31, "VMware VMFS", NULL,  NULL)    /* VMware VMFS */ \
 x(VMWRESV, 32, "VMware Reserved", NULL, NULL) /* VMware reserved */ \
-x(ZFS,	   33, "ZFS", NULL, NULL)	      /* ZFS */
+x(ZFS,     33, "ZFS",        NULL,   NULL)    /* ZFS */
 
 
 #ifndef _LOCORE
