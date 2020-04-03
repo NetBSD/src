08716c57839e809214072a8bd9ff536968ce91eb
skrll <skrll@NetBSD.org>
2020-04-03 06:05:00 +0000

    Avoid crashes if we fail to allocate a transfer buffer and avoid an unnecessary usbd_free_buffer before usbd_free_xfer
    Worked out by Riastradh

--- sys/dev/usb/usbdi.c
+++ sys/dev/usb/usbdi.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: usbdi.c,v 1.198 2020/03/14 02:35:33 christos Exp $	*/
+/*	$NetBSD: usbdi.c,v 1.199 2020/04/03 06:05:00 skrll Exp $	*/
 
 /*
  * Copyright (c) 1998, 2012, 2015 The NetBSD Foundation, Inc.
@@ -32,7 +32,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: usbdi.c,v 1.198 2020/03/14 02:35:33 christos Exp $");
+__KERNEL_RCSID(0, "$NetBSD: usbdi.c,v 1.199 2020/04/03 06:05:00 skrll Exp $");
 
 #ifdef _KERNEL_OPT
 #include "opt_usb.h"
@@ -593,6 +593,11 @@ usbd_create_xfer(struct usbd_pipe *pipe, size_t len, unsigned int flags,
 	if (xfer == NULL)
 		return ENOMEM;
 
+	xfer->ux_pipe = pipe;
+	xfer->ux_flags = flags;
+	xfer->ux_nframes = nframes;
+	xfer->ux_methods = pipe->up_methods;
+
 	if (len) {
 		buf = usbd_alloc_buffer(xfer, len);
 		if (!buf) {
@@ -600,16 +605,10 @@ usbd_create_xfer(struct usbd_pipe *pipe, size_t len, unsigned int flags,
 			return ENOMEM;
 		}
 	}
-	xfer->ux_pipe = pipe;
-	xfer->ux_flags = flags;
-	xfer->ux_nframes = nframes;
-	xfer->ux_methods = pipe->up_methods;
 
 	if (xfer->ux_methods->upm_init) {
 		int err = xfer->ux_methods->upm_init(xfer);
 		if (err) {
-			if (buf)
-				usbd_free_buffer(xfer);
 			usbd_free_xfer(xfer);
 			return err;
 		}
