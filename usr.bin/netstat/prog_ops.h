e9a9a050bc169b0ba7629a6ed0aac1c5ffc35fb7
martin <martin@NetBSD.org>
2020-04-03 16:20:51 +0000

    Make route and netstat share the same struct progops (and initialization code)

--- usr.bin/netstat/prog_ops.h
+++ usr.bin/netstat/prog_ops.h
@@ -1,4 +1,4 @@
-/*      $NetBSD: prog_ops.h,v 1.3 2019/08/18 04:14:40 kamil Exp $	*/
+/*      $NetBSD: prog_ops.h,v 1.4 2020/04/03 16:20:51 martin Exp $	*/
 
 /*
  * Copyright (c) 2010 The NetBSD Foundation, Inc.
@@ -26,43 +26,5 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifndef _PROG_OPS_H_
-#define _PROG_OPS_H_
 
-#include <sys/types.h>
-
-#ifndef CRUNCHOPS
-struct sysctlnode;
-
-struct prog_ops {
-	int (*op_init)(void);
-
-	int (*op_sysctl)(const int *, u_int, void *, size_t *,
-			 const void *, size_t);
-
-	/* Indirection needed for sanitizers. */
-
-	int (*op_sysctlbyname)(const char *, void *, size_t *,
-			 const void *, size_t);
-
-	int (*op_sysctlgetmibinfo)(const char *, int *, u_int *,
-			 char *, size_t *, struct sysctlnode **, int);
-
-	int (*op_sysctlnametomib)(const char *, int *, size_t *);
-};
-extern const struct prog_ops prog_ops;
-
-#define prog_init prog_ops.op_init
-#define prog_sysctl prog_ops.op_sysctl
-#define prog_sysctlbyname prog_ops.op_sysctlbyname
-#define prog_sysctlgetmibinfo prog_ops.op_sysctlgetmibinfo
-#define prog_sysctlnametomib prog_ops.op_sysctlnametomib
-#else
-#define prog_init ((int (*)(void))NULL)
-#define prog_sysctl sysctl
-#define prog_sysctlbyname sysctlbyname
-#define prog_sysctlgetmibinfo sysctlgetmibinfo
-#define prog_sysctlnametomib sysctlnametomib
-#endif
-
-#endif /* _PROG_OPS_H_ */
+#include "../../sbin/route/prog_ops.h"
