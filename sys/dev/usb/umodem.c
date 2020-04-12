8ff3e959f9941d5fa449041d1665af43f37532dc
martin <martin@NetBSD.org>
2020-04-12 08:44:42 +0000

    Pull up following revision(s) (requested by simonb in ticket #828):
    	share/man/man4/uxrcom.4: revision 1.1
    	distrib/sets/lists/man/mi: revision 1.1687
    	share/man/man4/uxrcom.4: revision 1.2
    	share/man/man4/ucom.4: revision 1.28
    	sys/dev/usb/uxrcom.c: revision 1.1
    	sys/dev/usb/umodem.c: revision 1.74
    	sys/dev/usb/umodem_common.c: revision 1.33
    	sys/dev/usb/ukyopon.c: revision 1.26
    	sys/dev/usb/files.usb: revision 1.173
    	share/man/man4/Makefile: revision 1.704
    	sys/dev/usb/usbdevs: revision 1.781
    	sys/dev/usb/usbdevices.config: revision 1.38
    
    Add uxrcom driver for Exar XR21V141x USB serial adapters.  Based in part
    on the OpenBSD single-port XR21V1410 uxrcom driver, but adds support
    for multi-port chipsets and uses the common umodem framework instead of
    being a standalone driver.
    
    Thanks to skrll@ for much USB clue and mrg@ for financing the
    development of this driver.
    
    Add NetBSD CVS tag.

--- sys/dev/usb/umodem.c
+++ sys/dev/usb/umodem.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: umodem.c,v 1.72 2019/05/09 02:43:35 mrg Exp $	*/
+/*	$NetBSD: umodem.c,v 1.72.2.1 2020/04/12 08:44:42 martin Exp $	*/
 
 /*
  * Copyright (c) 1998 The NetBSD Foundation, Inc.
@@ -44,7 +44,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: umodem.c,v 1.72 2019/05/09 02:43:35 mrg Exp $");
+__KERNEL_RCSID(0, "$NetBSD: umodem.c,v 1.72.2.1 2020/04/12 08:44:42 martin Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -113,6 +113,8 @@ umodem_attach(device_t parent, device_t self, void *aux)
 	struct umodem_softc *sc = device_private(self);
 	struct usbif_attach_arg *uiaa = aux;
 	struct ucom_attach_args ucaa;
+
+	memset(&ucaa, 0, sizeof(ucaa));
 
 	ucaa.ucaa_portno = UCOM_UNK_PORTNO;
 	ucaa.ucaa_methods = &umodem_methods;
