bd4d0cb8a950bf6151ec9200809bc30649e76273
bouyer <bouyer@NetBSD.org>
2020-04-16 17:47:37 +0000

    fail silently if hypervisor is not found.

--- sys/arch/xen/xen/hypervisor.c
+++ sys/arch/xen/xen/hypervisor.c
@@ -1,4 +1,4 @@
-/* $NetBSD: hypervisor.c,v 1.73.2.2 2020/04/11 21:21:16 bouyer Exp $ */
+/* $NetBSD: hypervisor.c,v 1.73.2.3 2020/04/16 17:47:37 bouyer Exp $ */
 
 /*
  * Copyright (c) 2005 Manuel Bouyer.
@@ -53,7 +53,7 @@
 
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: hypervisor.c,v 1.73.2.2 2020/04/11 21:21:16 bouyer Exp $");
+__KERNEL_RCSID(0, "$NetBSD: hypervisor.c,v 1.73.2.3 2020/04/16 17:47:37 bouyer Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -250,8 +250,6 @@ hypervisor_match(device_t parent, cfdata_t match, void *aux)
 		vm_guest = VM_GUEST_XENPVHVM; /* Be more specific */
 
 	} else {
-		aprint_normal("%s: Xen HVM mode not identified. Exiting.\n",
-		    haa->haa_busname);
 		return 0;
 	}
 
@@ -425,7 +423,7 @@ hypervisor_match(device_t parent, cfdata_t match, void *aux)
 	bi.common.len = sizeof(struct btinfo_rootdevice);
 
 	/* From i386/multiboot.c */
-	/*	$NetBSD: hypervisor.c,v 1.73.2.2 2020/04/11 21:21:16 bouyer Exp $	*/
+	/*	$NetBSD: hypervisor.c,v 1.73.2.3 2020/04/16 17:47:37 bouyer Exp $	*/
 	int i, len;
 	vaddr_t data;
 	extern struct bootinfo	bootinfo;
