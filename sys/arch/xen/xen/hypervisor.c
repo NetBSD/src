7da2b489c4d2eaf66956d57d5c1cb63ef764cb62
jdolecek <jdolecek@NetBSD.org>
2020-04-19 18:47:40 +0000

    change interface for xen_shm_map() so that caller always supplies the VA, it now fails only if the Xen hypercall fails, in which case the failure is final
    change xbdback to pre-allocate KVA on xbdback attach (and free on detach),
    so it has always KVA to map the request pages
    
    remove no longer needed KVA allocation failure handling

--- sys/arch/xen/xen/hypervisor.c
+++ sys/arch/xen/xen/hypervisor.c
@@ -1,4 +1,4 @@
-/* $NetBSD: hypervisor.c,v 1.74 2020/04/10 14:54:33 jdolecek Exp $ */
+/* $NetBSD: hypervisor.c,v 1.75 2020/04/19 18:47:40 jdolecek Exp $ */
 
 /*
  * Copyright (c) 2005 Manuel Bouyer.
@@ -53,7 +53,7 @@
 
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: hypervisor.c,v 1.74 2020/04/10 14:54:33 jdolecek Exp $");
+__KERNEL_RCSID(0, "$NetBSD: hypervisor.c,v 1.75 2020/04/19 18:47:40 jdolecek Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -415,7 +415,7 @@ hypervisor_match(device_t parent, cfdata_t match, void *aux)
 	bi.common.len = sizeof(struct btinfo_rootdevice);
 
 	/* From i386/multiboot.c */
-	/*	$NetBSD: hypervisor.c,v 1.74 2020/04/10 14:54:33 jdolecek Exp $	*/
+	/*	$NetBSD: hypervisor.c,v 1.75 2020/04/19 18:47:40 jdolecek Exp $	*/
 	int i, len;
 	vaddr_t data;
 	extern struct bootinfo	bootinfo;
@@ -620,7 +620,6 @@ hypervisor_attach(device_t parent, device_t self, void *aux)
 
 	if (xendomain_is_privileged()) {
 		xenprivcmd_init();
-		xen_shm_init();
 	}
 #endif /* DOM0OPS */
 
