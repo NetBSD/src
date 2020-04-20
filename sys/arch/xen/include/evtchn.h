ed454bdec3a00fb1db980695041227ade91a4c25
bouyer <bouyer@NetBSD.org>
2020-04-20 19:46:44 +0000

    Misc fixes after merge

--- sys/arch/xen/include/evtchn.h
+++ sys/arch/xen/include/evtchn.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: evtchn.h,v 1.28.2.2 2020/04/20 11:29:00 bouyer Exp $	*/
+/*	$NetBSD: evtchn.h,v 1.28.2.3 2020/04/20 19:46:44 bouyer Exp $	*/
 
 /*
  *
@@ -41,8 +41,8 @@ bool events_resume(void);
 unsigned int evtchn_do_event(int, struct intrframe *);
 void call_evtchn_do_event(int, struct intrframe *);
 void call_xenevt_event(int);
-int event_set_handler(int, int (*func)(void *), void *, int, const char *,
-    const char *, bool, bool);
+struct intrhand *event_set_handler(int, int (*func)(void *), void *,
+    int, const char *, const char *, bool, bool);
 int event_remove_handler(int, int (*func)(void *), void *);
 
 struct cpu_info;
