31ea95722e22f76dc9c694cb52271ec280da5cbb
martin <martin@NetBSD.org>
2020-04-22 18:18:26 +0000

    Pull up following revision(s) (requested by gdt in ticket #1534):
    	sys/kern/vfs_syscalls.c: revision 1.544
    	lib/libc/sys/fdatasync.2: revision 1.17
    
    Relax fdatasync restriction that fd be writable
    
    The restriction that a fd passed to fdatasync(2) must be writable was
    added in 2003 in order to comply with POSIX.  Since then, POSIX has
    removed that requirement, and POSIX-valid programs have been therefore
    encountering errors on NetBSD.
    
    Patch by Paul Ripke after discussion on netbsd-users.  Issue
    discovered with pkgsrc/databases/mongodb3 as used by pkgsrc/net/unifi.

--- sys/kern/vfs_syscalls.c
+++ sys/kern/vfs_syscalls.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: vfs_syscalls.c,v 1.516 2017/06/01 02:45:13 chs Exp $	*/
+/*	$NetBSD: vfs_syscalls.c,v 1.516.2.1 2020/04/22 18:18:26 martin Exp $	*/
 
 /*-
  * Copyright (c) 2008, 2009 The NetBSD Foundation, Inc.
@@ -70,7 +70,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: vfs_syscalls.c,v 1.516 2017/06/01 02:45:13 chs Exp $");
+__KERNEL_RCSID(0, "$NetBSD: vfs_syscalls.c,v 1.516.2.1 2020/04/22 18:18:26 martin Exp $");
 
 #ifdef _KERNEL_OPT
 #include "opt_fileassoc.h"
@@ -4004,8 +4004,7 @@ sys_fsync(struct lwp *l, const struct sys_fsync_args *uap, register_t *retval)
  * Sync a range of file data.  API modeled after that found in AIX.
  *
  * FDATASYNC indicates that we need only save enough metadata to be able
- * to re-read the written data.  Note we duplicate AIX's requirement that
- * the file be open for writing.
+ * to re-read the written data.
  */
 /* ARGSUSED */
 int
@@ -4086,10 +4085,6 @@ sys_fdatasync(struct lwp *l, const struct sys_fdatasync_args *uap, register_t *r
 	/* fd_getvnode() will use the descriptor for us */
 	if ((error = fd_getvnode(SCARG(uap, fd), &fp)) != 0)
 		return (error);
-	if ((fp->f_flag & FWRITE) == 0) {
-		fd_putfile(SCARG(uap, fd));
-		return (EBADF);
-	}
 	vp = fp->f_vnode;
 	vn_lock(vp, LK_EXCLUSIVE | LK_RETRY);
 	error = VOP_FSYNC(vp, fp->f_cred, FSYNC_WAIT|FSYNC_DATAONLY, 0, 0);
