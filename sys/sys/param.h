ede4389518fdda692f0cbd11df909d7cfacb68fe
ad <ad@NetBSD.org>
2020-04-23 23:40:04 +0000

    NetBSD 9.99.58 - ubc_uiomove needs to see new flag

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.660 2020/04/21 21:53:53 ad Exp $	*/
+/*	$NetBSD: param.h,v 1.661 2020/04/23 23:40:04 ad Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -67,7 +67,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	999005700	/* NetBSD 9.99.57 */
+#define	__NetBSD_Version__	999005800	/* NetBSD 9.99.58 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
