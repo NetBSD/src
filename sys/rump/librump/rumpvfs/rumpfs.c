6c7e5f2a8c69e3bc0f505a410966e139227a92a2
ad <ad@NetBSD.org>
2020-04-23 21:47:07 +0000

    PR kern/54759 (vm.ubc_direct deadlock when read()/write() into mapping of itself)
    - Add new flag UBC_ISMAPPED which tells ubc_uiomove() the object is mmap()ed
      somewhere.  Use it to decide whether to do direct-mapped copy, rather than
      poking around directly in the vnode in ubc_uiomove(), which is ugly and
      doesn't work for tmpfs.  It would be nicer to contain all this in UVM but
      the filesystem provides the needed locking here (VV_MAPPED) and to
      reinvent that would suck more.
    
    - Rename UBC_UNMAP_FLAG() to UBC_VNODE_FLAGS().  Pass in UBC_ISMAPPED where
      appropriate.

--- sys/rump/librump/rumpvfs/rumpfs.c
+++ sys/rump/librump/rumpvfs/rumpfs.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: rumpfs.c,v 1.156 2020/04/13 19:23:20 ad Exp $	*/
+/*	$NetBSD: rumpfs.c,v 1.157 2020/04/23 21:47:08 ad Exp $	*/
 
 /*
  * Copyright (c) 2009, 2010, 2011 Antti Kantee.  All Rights Reserved.
@@ -26,7 +26,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: rumpfs.c,v 1.156 2020/04/13 19:23:20 ad Exp $");
+__KERNEL_RCSID(0, "$NetBSD: rumpfs.c,v 1.157 2020/04/23 21:47:08 ad Exp $");
 
 #include <sys/param.h>
 #include <sys/atomic.h>
@@ -1403,7 +1403,7 @@ rump_vop_read(void *v)
 		if (chunk == 0)
 			break;
 		error = ubc_uiomove(&vp->v_uobj, uio, chunk, advice,
-		    UBC_READ | UBC_PARTIALOK | UBC_UNMAP_FLAG(vp));
+		    UBC_READ | UBC_PARTIALOK | UBC_VNODE_FLAGS(vp));
 		if (error)
 			break;
 	}
@@ -1505,7 +1505,7 @@ rump_vop_write(void *v)
 		if (chunk == 0)
 			break;
 		error = ubc_uiomove(&vp->v_uobj, uio, chunk, advice,
-		    UBC_WRITE | UBC_PARTIALOK | UBC_UNMAP_FLAG(vp));
+		    UBC_WRITE | UBC_PARTIALOK | UBC_VNODE_FLAGS(vp));
 		if (error)
 			break;
 	}
