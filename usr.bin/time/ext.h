8a81d65da829d6c033281453570d22039c1a5506
martin <martin@NetBSD.org>
2020-04-25 10:50:47 +0000

    Pull up following revision(s) (requested by simonb in ticket #853):
    	bin/csh/time.c: revision 1.22
    	usr.bin/time/time.c: revision 1.24
    	bin/csh/extern.h: revision 1.32
    	usr.bin/time/time.1: revision 1.29
    	usr.bin/time/ext.h: revision 1.4
    
    Add '-t' option for tcsh-style time output.

--- usr.bin/time/ext.h
+++ usr.bin/time/ext.h
@@ -1,5 +1,5 @@
-/*	$NetBSD: ext.h,v 1.3 2017/07/15 14:34:08 christos Exp $	*/
+/*	$NetBSD: ext.h,v 1.3.8.1 2020/04/25 10:50:47 martin Exp $	*/
 
 /* borrowed from ../../bin/csh/extern.h */
-void prusage1(FILE *, const char *fmt, struct rusage *, struct rusage *,
+void prusage1(FILE *, const char *fmt, int, struct rusage *, struct rusage *,
     struct timespec *, struct timespec *);
