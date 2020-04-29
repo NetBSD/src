a00836b0fd33efc5c8169e0c8fde6b5ceed07a4a
thorpej <thorpej@NetBSD.org>
2020-04-29 01:56:54 +0000

    If the procfs mount is marked as linux-compat, then allow proc lookup by any LWP ID in the proc, not just the canonical PID.

--- sys/miscfs/procfs/procfs.h
+++ sys/miscfs/procfs/procfs.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: procfs.h,v 1.78 2020/01/17 20:08:09 ad Exp $	*/
+/*	$NetBSD: procfs.h,v 1.79 2020/04/29 01:56:54 thorpej Exp $	*/
 
 /*
  * Copyright (c) 1993
@@ -209,7 +209,9 @@ struct vfs_namemap {
 int vfs_getuserstr(struct uio *, char *, int *);
 const vfs_namemap_t *vfs_findname(const vfs_namemap_t *, const char *, int);
 
-int procfs_proc_lock(int, struct proc **, int);
+struct proc *procfs_proc_find(struct mount *, pid_t);
+bool procfs_use_linux_compat(struct mount *);
+int procfs_proc_lock(struct mount *, int, struct proc **, int);
 void procfs_proc_unlock(struct proc *);
 struct mount;
 int procfs_allocvp(struct mount *, struct vnode **, pid_t, pfstype, int);
