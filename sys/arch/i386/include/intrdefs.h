dcccbbb7d2bc78a878b1c16031c6dd7ed2f7e69e
bouyer <bouyer@NetBSD.org>
2020-04-30 22:05:17 +0000

    Don't #include xen/intrdefs.h is !XEN. Should fix third-party module builds (e.g. virtualbox)

--- sys/arch/i386/include/intrdefs.h
+++ sys/arch/i386/include/intrdefs.h
@@ -1,4 +1,7 @@
-/*	$NetBSD: intrdefs.h,v 1.5 2020/04/25 15:26:17 bouyer Exp $	*/
+/*	$NetBSD: intrdefs.h,v 1.6 2020/04/30 22:05:17 bouyer Exp $	*/
 
 #include <x86/intrdefs.h>
+
+#ifdef XEN
 #include <xen/intrdefs.h>
+#endif
