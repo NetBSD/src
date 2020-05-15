d4f3ba3e8ce602253b5629cedb59f114a3110828
martin <martin@NetBSD.org>
2020-05-15 15:20:40 +0000

    PR 55239: initialize all RAS sections for non-MP configurations

--- common/lib/libc/atomic/atomic_init_testset.c
+++ common/lib/libc/atomic/atomic_init_testset.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: atomic_init_testset.c,v 1.16 2019/02/18 11:22:56 martin Exp $	*/
+/*	$NetBSD: atomic_init_testset.c,v 1.17 2020/05/15 15:20:40 martin Exp $	*/
 
 /*-
  * Copyright (c) 2008 The NetBSD Foundation, Inc.
@@ -36,7 +36,7 @@
  */
 
 #include <sys/cdefs.h>
-__RCSID("$NetBSD: atomic_init_testset.c,v 1.16 2019/02/18 11:22:56 martin Exp $");
+__RCSID("$NetBSD: atomic_init_testset.c,v 1.17 2020/05/15 15:20:40 martin Exp $");
 
 #include "atomic_op_namespace.h"
 
@@ -296,30 +296,28 @@ __libc_atomic_init(void)
 		return;
 	if (ncpu > 1)
 		return;
+
 	if (rasctl(RAS_ADDR(_atomic_cas), RAS_SIZE(_atomic_cas),
 	    RAS_INSTALL) == 0) {
 		_atomic_cas_fn = _atomic_cas_up;
-		return;
 	}
+
 
 #ifdef	__HAVE_ATOMIC_CAS_64_UP
 	if (rasctl(RAS_ADDR(_atomic_cas_64), RAS_SIZE(_atomic_cas_64),
 	    RAS_INSTALL) == 0) {
 		_atomic_cas_64_fn = _atomic_cas_64_up;
-		return;
 	}
 #endif
 
 	if (rasctl(RAS_ADDR(_atomic_cas_16), RAS_SIZE(_atomic_cas_16),
 	    RAS_INSTALL) == 0) {
 		_atomic_cas_16_fn = _atomic_cas_16_up;
-		return;
 	}
 
 	if (rasctl(RAS_ADDR(_atomic_cas_8), RAS_SIZE(_atomic_cas_8),
 	    RAS_INSTALL) == 0) {
 		_atomic_cas_8_fn = _atomic_cas_8_up;
-		return;
 	}
 }
 
