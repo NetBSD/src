85ef913582e172c1573019bd1df1be4f3b2edce4
christos <christos@NetBSD.org>
2020-05-16 18:58:56 +0000

    Welcome to 9.99.63 (ACLS)

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.665 2020/05/14 13:34:46 kamil Exp $	*/
+/*	$NetBSD: param.h,v 1.666 2020/05/16 18:58:56 christos Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -67,7 +67,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	999006200	/* NetBSD 9.99.62 */
+#define	__NetBSD_Version__	999006300	/* NetBSD 9.99.63 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
