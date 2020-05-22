0ae2b2bb5ea9fe61d135c353ae5de414dd32e3f7
ryo <ryo@NetBSD.org>
2020-05-22 19:29:26 +0000

    fix to do backtrace properly for running LWPs and cpu_lwp_fork(). when dump of pcb_tf, only the switchframe part is now displayed instead of the whole trapframe.

--- sys/arch/aarch64/include/db_machdep.h
+++ sys/arch/aarch64/include/db_machdep.h
@@ -1,4 +1,4 @@
-/* $NetBSD: db_machdep.h,v 1.8 2018/10/12 01:28:58 ryo Exp $ */
+/* $NetBSD: db_machdep.h,v 1.9 2020/05/22 19:29:26 ryo Exp $ */
 
 /*-
  * Copyright (c) 2014 The NetBSD Foundation, Inc.
@@ -202,6 +202,7 @@ db_addr_t db_branch_taken(db_expr_t, db_addr_t, db_regs_t *);
 
 #define DB_MACHINE_COMMANDS
 void dump_trapframe(struct trapframe *, void (*)(const char *, ...));
+void dump_switchframe(struct trapframe *, void (*)(const char *, ...));
 const char *strdisasm(vaddr_t);
 const char *strdisasm_aarch32(vaddr_t);
 void db_machdep_init(void);
