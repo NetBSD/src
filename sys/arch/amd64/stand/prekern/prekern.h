cc47e119b88486474e29f394de32862111192bf0
maxv <maxv@NetBSD.org>
2020-05-23 08:25:32 +0000

    Bump copyrights.

--- sys/arch/amd64/stand/prekern/prekern.h
+++ sys/arch/amd64/stand/prekern/prekern.h
@@ -1,7 +1,7 @@
-/*	$NetBSD: prekern.h,v 1.22 2020/05/07 21:05:37 maxv Exp $	*/
+/*	$NetBSD: prekern.h,v 1.23 2020/05/23 08:25:32 maxv Exp $	*/
 
 /*
- * Copyright (c) 2017 The NetBSD Foundation, Inc. All rights reserved.
+ * Copyright (c) 2017-2020 The NetBSD Foundation, Inc. All rights reserved.
  *
  * This code is derived from software contributed to The NetBSD Foundation
  * by Maxime Villard.
