c2506c42f6f94f25ab6ef91d05c7e77677b19a2a
riastradh <riastradh@NetBSD.org>
2020-05-24 17:26:18 +0000

    Fix races in sdmmc tasks and teach ld@sdmmc to abort xfers on detach.
    - Teach sdmmc_add_task to queue it only if not already queued.
    - Remove now-redundant logic to avoid repeated queueing elsewhere.
    - Teach sdmmc_del_task to wait until task has completed.
    - Call sdmmc_del_task in various needful places.
    - Replace abuse of pcq by a lock and a tailq.
      (pcq is multi-producer, _single_-consumer, but there are potentially
      multiple consumers here and really only one producer.)
    - Teach ld_sdmmc to abort xfers on detach.
      (Mechanism is kinda kludgey but it'll do for now; any effort one is
      tempted to spend overhauling this should be spent overhauling sdmmc
      to support proper asynchronous commands.)
    - Make sure ld_sdmmc_discard either returns failure or eventually calls
      ldenddiscard.
    
    XXX Currently ld_sdmmc_detach aborts xfers _before_ ldbegindetach has
    has committed to detaching or not.  This is currently necessary to
    avoid a deadlock because ldbegindetach waits for xfers to drain --
    which strikes me as wrong; ldbegindetach shouldn't wait for anything,
    and should only make the decision to commit to detaching or not so
    the caller can decide whether to abort xfers before we actually wait
    for them in ldenddetach.
    
    XXX pullup -- although this changes some kernel symbols (sdmmc_add_task
    and sdmmc_del_task), it shouldn't affect any existing modules; the only
    module that uses sdmmc is ld_sdmmc.kmod, which is `.if 0' in the build
    so there shouldn't be any of them floating around.

--- sys/dev/sdmmc/if_bwfm_sdio.c
+++ sys/dev/sdmmc/if_bwfm_sdio.c
@@ -1,4 +1,4 @@
-/* $NetBSD: if_bwfm_sdio.c,v 1.15 2020/05/07 11:46:27 macallan Exp $ */
+/* $NetBSD: if_bwfm_sdio.c,v 1.16 2020/05/24 17:26:18 riastradh Exp $ */
 /* $OpenBSD: if_bwfm_sdio.c,v 1.1 2017/10/11 17:19:50 patrick Exp $ */
 /*
  * Copyright (c) 2010-2016 Broadcom Corporation
@@ -69,7 +69,6 @@ enum bwfm_sdio_clkstate {
 struct bwfm_sdio_softc {
 	struct bwfm_softc	sc_sc;
 	kmutex_t		sc_lock;
-	kmutex_t		sc_intr_lock;
 
 	bool			sc_bwfm_attached;
 
@@ -361,10 +360,8 @@ bwfm_sdio_attach(device_t parent, device_t self, void *aux)
 
 	mutex_init(&sc->sc_lock, MUTEX_DEFAULT, IPL_NONE);
 	cv_init(&sc->sc_rxctl_cv, "bwfmctl");
-	mutex_init(&sc->sc_intr_lock, MUTEX_DEFAULT, IPL_NONE);
 
 	sdmmc_init_task(&sc->sc_task, bwfm_sdio_task, sc);
-	sc->sc_task_queued = false;
 
 	sc->sc_bounce_size = 64 * 1024;
 	sc->sc_bounce_buf = kmem_alloc(sc->sc_bounce_size, KM_SLEEP);
@@ -620,10 +617,11 @@ bwfm_sdio_detach(device_t self, int flags)
 	if (sc->sc_bwfm_attached)
 		bwfm_detach(&sc->sc_sc, flags);
 
+	sdmmc_del_task(sc->sc_sf[1]->sc, &sc->sc_task, NULL);
+
 	kmem_free(sc->sc_sf, sc->sc_sf_size);
 	kmem_free(sc->sc_bounce_buf, sc->sc_bounce_size);
 
-	mutex_destroy(&sc->sc_intr_lock);
 	cv_destroy(&sc->sc_rxctl_cv);
 	mutex_destroy(&sc->sc_lock);
 
@@ -1426,11 +1424,7 @@ bwfm_sdio_intr1(void *v, const char *name)
 
 	DPRINTF(("%s: %s\n", DEVNAME(sc), name));
 
-	mutex_enter(&sc->sc_intr_lock);
-	if (!sdmmc_task_pending(&sc->sc_task))
-		sdmmc_add_task(sc->sc_sf[1]->sc, &sc->sc_task);
-	sc->sc_task_queued = true;
-	mutex_exit(&sc->sc_intr_lock);
+	sdmmc_add_task(sc->sc_sf[1]->sc, &sc->sc_task);
 	return 1;
 }
 
@@ -1444,33 +1438,13 @@ static void
 bwfm_sdio_task(void *v)
 {
 	struct bwfm_sdio_softc *sc = (void *)v;
-#ifdef BWFM_DEBUG
-	unsigned count = 0;
-#endif
 
-	mutex_enter(&sc->sc_intr_lock);
-	while (sc->sc_task_queued) {
+	mutex_enter(&sc->sc_lock);
+	bwfm_sdio_task1(sc);
 #ifdef BWFM_DEBUG
-		++count;
+	bwfm_sdio_debug_console(sc);
 #endif
-		sc->sc_task_queued = false;
-		mutex_exit(&sc->sc_intr_lock);
-
-		mutex_enter(&sc->sc_lock);
-		bwfm_sdio_task1(sc);
-#ifdef BWFM_DEBUG
-		bwfm_sdio_debug_console(sc);
-#endif
-		mutex_exit(&sc->sc_lock);
-
-		mutex_enter(&sc->sc_intr_lock);
-	}
-	mutex_exit(&sc->sc_intr_lock);
-
-#ifdef BWFM_DEBUG
-	if (count > 1)
-		DPRINTF(("%s: finished %u tasks\n", DEVNAME(sc), count));
-#endif
+	mutex_exit(&sc->sc_lock);
 }
 
 static void
