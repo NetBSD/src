2b088a4946d4e73f56f000158a8b1d901c87c797
thorpej <thorpej@NetBSD.org>
2020-06-07 06:08:20 +0000

    NetBSD 9.99.65 -- proplib(3) API additions also impact the kernel ABI.

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.667 2020/05/23 21:49:43 ad Exp $	*/
+/*	$NetBSD: param.h,v 1.668 2020/06/07 06:08:20 thorpej Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -67,7 +67,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	999006400	/* NetBSD 9.99.64 */
+#define	__NetBSD_Version__	999006500	/* NetBSD 9.99.65 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
