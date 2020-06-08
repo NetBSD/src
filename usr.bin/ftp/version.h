de744285fd4aec7ba9e442d301e876e4c18c9058
lukem <lukem@NetBSD.org>
2020-06-08 01:33:27 +0000

    ftp: exit if lostpeer invoked by a signal
    lostpeer() calls too many async-unsafe functions (both directly
    and indirectly) to close and cleanup the remote connections,
    so just exit after the cleanup if invoked by a signal.
    
    Reported in private mail by Qi Hou.
    May also resolve a crash reported by Thomas Klausner.

--- usr.bin/ftp/version.h
+++ usr.bin/ftp/version.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: version.h,v 1.88 2020/02/26 05:55:27 lukem Exp $	*/
+/*	$NetBSD: version.h,v 1.89 2020/06/08 01:33:27 lukem Exp $	*/
 
 /*-
  * Copyright (c) 1999-2020 The NetBSD Foundation, Inc.
@@ -34,5 +34,5 @@
 #endif
 
 #ifndef FTP_VERSION
-#define	FTP_VERSION	"20190622"
+#define	FTP_VERSION	"20200608"
 #endif
