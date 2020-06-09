81af860e7ba1dfac564b80c527bc29fde887965b
msaitoh <msaitoh@NetBSD.org>
2020-06-09 05:07:13 +0000

    Add braces.

--- sys/arch/x86/x86/identcpu_subr.c
+++ sys/arch/x86/x86/identcpu_subr.c
@@ -33,7 +33,7 @@
  * See src/usr.sbin/cpuctl/{Makefile, arch/i386.c}).
  */
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: identcpu_subr.c,v 1.5 2020/06/09 05:06:27 msaitoh Exp $");
+__KERNEL_RCSID(0, "$NetBSD: identcpu_subr.c,v 1.6 2020/06/09 05:07:13 msaitoh Exp $");
 
 #ifdef _KERNEL_OPT
 #include "lapic.h"
@@ -71,7 +71,7 @@ cpu_tsc_freq_cpuid(struct cpu_info *ci)
 	x86_cpuid(0x15, descs);
 	denominator = descs[0];
 	numerator = descs[1];
-	if ((denominator != 0) && numerator != 0) {
+	if ((denominator != 0) && (numerator != 0)) {
 		khz = 0;
 		if (descs[2] != 0)
 			khz = descs[2] / 1000;
