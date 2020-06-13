ed0a49d53d35f84da3270e2f06d995be39255529
kamil <kamil@NetBSD.org>
2020-06-13 16:51:59 +0000

    Fix incompatible function pointer casts

--- lib/librumpuser/sp_common.c
+++ lib/librumpuser/sp_common.c
@@ -1,4 +1,4 @@
-/*      $NetBSD: sp_common.c,v 1.41 2020/05/06 12:44:36 christos Exp $	*/
+/*      $NetBSD: sp_common.c,v 1.42 2020/06/13 16:51:59 kamil Exp $	*/
 
 /*
  * Copyright (c) 2010, 2011 Antti Kantee.  All Rights Reserved.
@@ -705,15 +705,21 @@ unix_cleanup(struct sockaddr *sa)
 
 /*ARGSUSED*/
 static int
-notsupp(void)
+addrparse_notsupp(const char *addr __unused, struct sockaddr **sa __unused,
+		  int allow_wildcard __unused)
 {
 
 	fprintf(stderr, "rump_sp: support not yet implemented\n");
 	return EOPNOTSUPP;
 }
 
+static void
+cleanup_success(struct sockaddr *sa __unused)
+{
+}
+
 static int
-success(void)
+connecthook_success(int s __unused)
 {
 
 	return 0;
@@ -728,12 +734,12 @@ static struct {
 	cleanup_fn cleanup;
 } parsetab[] = {
 	{ "tcp", PF_INET, sizeof(struct sockaddr_in),
-	    tcp_parse, tcp_connecthook, (cleanup_fn)success },
+	    tcp_parse, tcp_connecthook, cleanup_success },
 	{ "unix", PF_LOCAL, sizeof(struct sockaddr_un),
-	    unix_parse, (connecthook_fn)success, unix_cleanup },
+	    unix_parse, connecthook_success, unix_cleanup },
 	{ "tcp6", PF_INET6, sizeof(struct sockaddr_in6),
-	    (addrparse_fn)notsupp, (connecthook_fn)success,
-	    (cleanup_fn)success },
+	    addrparse_notsupp, connecthook_success,
+	    cleanup_success },
 };
 #define NPARSE (sizeof(parsetab)/sizeof(parsetab[0]))
 
