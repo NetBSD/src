86d5f123f9eea2ab20fa28524a4b9758182d7614
thorpej <thorpej@NetBSD.org>
2020-06-16 06:42:36 +0000

    Tidy up the function prototypes.  NFCI.

--- sys/sys/vmem.h
+++ sys/sys/vmem.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: vmem.h,v 1.21 2020/06/16 01:29:00 thorpej Exp $	*/
+/*	$NetBSD: vmem.h,v 1.22 2020/06/16 06:42:36 thorpej Exp $	*/
 
 /*-
  * Copyright (c)2006 YAMAMOTO Takashi,
@@ -45,43 +45,46 @@ typedef size_t vmem_size_t;
 #define	VMEM_ADDR_MIN	0
 #define	VMEM_ADDR_MAX	(~(vmem_addr_t)0)
 
-typedef int (vmem_import_t)(vmem_t *, vmem_size_t, vm_flag_t, vmem_addr_t *);
-typedef void (vmem_release_t)(vmem_t *, vmem_addr_t, vmem_size_t);
+typedef int	(vmem_import_t)(vmem_t *, vmem_size_t, vm_flag_t,
+		    vmem_addr_t *);
+typedef void	(vmem_release_t)(vmem_t *, vmem_addr_t, vmem_size_t);
 
-typedef int (vmem_ximport_t)(vmem_t *, vmem_size_t, vmem_size_t *,
-    vm_flag_t, vmem_addr_t *);
+typedef int	(vmem_ximport_t)(vmem_t *, vmem_size_t, vmem_size_t *,
+		    vm_flag_t, vmem_addr_t *);
 
 extern vmem_t *kmem_arena;
 extern vmem_t *kmem_meta_arena;
 extern vmem_t *kmem_va_arena;
 
-void vmem_subsystem_init(vmem_t *vm);
+void		vmem_subsystem_init(vmem_t *vm);
 
-vmem_t *vmem_create(const char *, vmem_addr_t, vmem_size_t, vmem_size_t,
-    vmem_import_t *, vmem_release_t *, vmem_t *, vmem_size_t,
-    vm_flag_t, int);
-vmem_t *vmem_xcreate(const char *, vmem_addr_t, vmem_size_t, vmem_size_t,
-    vmem_ximport_t *, vmem_release_t *, vmem_t *, vmem_size_t,
-    vm_flag_t, int);
-vmem_t *vmem_init(vmem_t *, const char *, vmem_addr_t, vmem_size_t, vmem_size_t,
-    vmem_import_t *, vmem_release_t *, vmem_t *, vmem_size_t,
-    vm_flag_t, int);
-void vmem_destroy(vmem_t *);
-int vmem_alloc(vmem_t *, vmem_size_t, vm_flag_t, vmem_addr_t *);
-void vmem_free(vmem_t *, vmem_addr_t, vmem_size_t);
-int vmem_xalloc(vmem_t *, vmem_size_t, vmem_size_t, vmem_size_t,
-    vmem_size_t, vmem_addr_t, vmem_addr_t, vm_flag_t, vmem_addr_t *);
-void vmem_xfree(vmem_t *, vmem_addr_t, vmem_size_t);
-void vmem_xfreeall(vmem_t *);
-int vmem_add(vmem_t *, vmem_addr_t, vmem_size_t, vm_flag_t);
-vmem_size_t vmem_roundup_size(vmem_t *, vmem_size_t);
-vmem_size_t vmem_size(vmem_t *, int typemask);
-void vmem_rehash_start(void);
-void vmem_whatis(uintptr_t, void (*)(const char *, ...) __printflike(1, 2));
-void vmem_print(uintptr_t, const char *, void (*)(const char *, ...)
-    __printflike(1, 2));
-void vmem_printall(const char *, void (*)(const char *, ...)
-    __printflike(1, 2));
+vmem_t *	vmem_create(const char *, vmem_addr_t, vmem_size_t, vmem_size_t,
+		    vmem_import_t *, vmem_release_t *, vmem_t *, vmem_size_t,
+		    vm_flag_t, int);
+vmem_t *	vmem_xcreate(const char *, vmem_addr_t, vmem_size_t,
+		    vmem_size_t, vmem_ximport_t *, vmem_release_t *, vmem_t *,
+		    vmem_size_t, vm_flag_t, int);
+vmem_t *	vmem_init(vmem_t *, const char *, vmem_addr_t, vmem_size_t,
+		    vmem_size_t, vmem_import_t *, vmem_release_t *, vmem_t *,
+		    vmem_size_t, vm_flag_t, int);
+void		vmem_destroy(vmem_t *);
+int		vmem_alloc(vmem_t *, vmem_size_t, vm_flag_t, vmem_addr_t *);
+void		vmem_free(vmem_t *, vmem_addr_t, vmem_size_t);
+int		vmem_xalloc(vmem_t *, vmem_size_t, vmem_size_t, vmem_size_t,
+		    vmem_size_t, vmem_addr_t, vmem_addr_t, vm_flag_t,
+		    vmem_addr_t *);
+void		vmem_xfree(vmem_t *, vmem_addr_t, vmem_size_t);
+void		vmem_xfreeall(vmem_t *);
+int		vmem_add(vmem_t *, vmem_addr_t, vmem_size_t, vm_flag_t);
+vmem_size_t	vmem_roundup_size(vmem_t *, vmem_size_t);
+vmem_size_t	vmem_size(vmem_t *, int typemask);
+void		vmem_rehash_start(void);
+void		vmem_whatis(uintptr_t, void (*)(const char *, ...)
+		    __printflike(1, 2));
+void		vmem_print(uintptr_t, const char *, void (*)(const char *, ...)
+		    __printflike(1, 2));
+void		vmem_printall(const char *, void (*)(const char *, ...)
+		    __printflike(1, 2));
 
 /* vm_flag_t */
 #define	VM_SLEEP	0x00000001
