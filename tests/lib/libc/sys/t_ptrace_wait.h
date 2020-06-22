e62c8f29455233048e397cc5ba11af236ab4a6db
rin <rin@NetBSD.org>
2020-06-22 12:21:02 +0000

    Work around QEMU bug #1668041 differently, by which floating-point division by zero is not correctly trapped for i386 and amd64:
    https://bugs.launchpad.net/qemu/+bug/1668041
    
    Make trigger_fpe() cause integer division by zero for x86, and
    floating-point one for other architectures. Also, assertions for
    si_code in *_crash_fpe tests are commented out for now. They
    should be cleaned up after the bug is fixed.
    
    Now, *_crash_fpe tests are working also on powerpc.
    
    Suggested by kamil.

--- tests/lib/libc/sys/t_ptrace_wait.h
+++ tests/lib/libc/sys/t_ptrace_wait.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: t_ptrace_wait.h,v 1.31 2020/06/22 02:51:06 rin Exp $	*/
+/*	$NetBSD: t_ptrace_wait.h,v 1.32 2020/06/22 12:21:02 rin Exp $	*/
 
 /*-
  * Copyright (c) 2016, 2017, 2018, 2019 The NetBSD Foundation, Inc.
@@ -671,9 +671,6 @@ are_fpu_exceptions_supported(void)
 		return false;
 	return true;
 }
-#elif __powerpc__
-/* Integer division by zero do not trap on powerpc. */
-#define are_fpu_exceptions_supported() 0
 #else
 #define are_fpu_exceptions_supported() 1
 #endif
@@ -681,15 +678,28 @@ are_fpu_exceptions_supported(void)
 static void __used
 trigger_fpe(void)
 {
-	volatile int a = getpid();
-	volatile int b = atoi("0");
+#if __i386__ || __x86_64__
+	/*
+	 * XXX
+	 * Hack for QEMU bug #1668041, by which floating-point division by
+	 * zero is not trapped correctly. Also, assertions for si_code in
+	 * ptrace_signal_wait.h are commented out. Clean them up after the
+	 * bug is fixed.
+	 */
+	volatile int a, b;
+#else
+	volatile double a, b;
+#endif
+
+	a = getpid();
+	b = atoi("0");
 
 #ifdef __HAVE_FENV
 	feenableexcept(FE_ALL_EXCEPT);
 #endif
 
 	/* Division by zero causes CPU trap, translated to SIGFPE */
-	usleep(a / b);
+	usleep((int)(a / b));
 }
 
 static void __used
