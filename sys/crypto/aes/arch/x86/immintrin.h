d395599aa3beb75e728e7517fab59c1a9a8d951f
riastradh <riastradh@NetBSD.org>
2020-06-29 23:47:54 +0000

    New SSE2-based bitsliced AES implementation.
    This should work on essentially all x86 CPUs of the last two decades,
    and may improve throughput over the portable C aes_ct implementation
    from BearSSL by
    
    (a) reducing the number of vector operations in sequence, and
    (b) batching four rather than two blocks in parallel.
    
    Derived from BearSSL'S aes_ct64 implementation adjusted so that where
    aes_ct64 uses 64-bit q[0],...,q[7], aes_sse2 uses (q[0], q[4]), ...,
    (q[3], q[7]), each tuple representing a pair of 64-bit quantities
    stacked in a single 128-bit register.  This translation was done very
    naively, and mostly reduces the cost of ShiftRows and data movement
    without doing anything to address the S-box or (Inv)MixColumns, which
    spread all 64-bit quantities across separate registers and ignore the
    upper halves.
    
    Unfortunately, SSE2 -- which is all that is guaranteed on all amd64
    CPUs -- doesn't have PSHUFB, which would help out a lot more.  For
    example, vpaes relies on that.  Perhaps there are enough CPUs out
    there with PSHUFB but not AES-NI to make it worthwhile to import or
    adapt vpaes too.
    
    Note: This includes local definitions of various Intel compiler
    intrinsics for gcc and clang in terms of their __builtin_* &c.,
    because the necessary header files are not available during the
    kernel build.  This is a kludge -- we should fix it properly; the
    present approach is expedient but not ideal.

---
+++ sys/crypto/aes/arch/x86/immintrin.h
@@ -0,0 +1,216 @@
+/*	$NetBSD: immintrin.h,v 1.1 2020/06/29 23:47:54 riastradh Exp $	*/
+
+/*-
+ * Copyright (c) 2020 The NetBSD Foundation, Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef	_SYS_CRYPTO_AES_ARCH_X86_IMMINTRIN_H
+#define	_SYS_CRYPTO_AES_ARCH_X86_IMMINTRIN_H
+
+#include <sys/types.h>
+
+/*
+ * This kludgerous header file provides definitions for the Intel
+ * intrinsics that work with GCC and Clang, because <immintrin.h> is
+ * not available during the kernel build and arranging to make it
+ * available is complicated.  Please fix this properly!
+ */
+
+#if defined(__GNUC__) && !defined(__clang__)
+
+#define	_INTRINSATTR							      \
+	__attribute__((__gnu_inline__, __always_inline__, __artificial__))
+#define	_PACKALIAS
+
+typedef float __m128 __attribute__((__vector_size__(16), __may_alias__));
+typedef long long __m128i __attribute__((__vector_size__(16), __may_alias__));
+typedef long long __m128i_u
+    __attribute__((__vector_size__(16), __may_alias__, __aligned__(1)));
+typedef long long __v2di __attribute__((__vector_size__(16)));
+typedef unsigned long long __v2du __attribute__((__vector_size__(16)));
+typedef int __v4si __attribute__((__vector_size__(16)));
+typedef float __v4sf __attribute__((__vector_size__(16)));
+typedef short __v8hi __attribute__((__vector_size__(16)));
+
+#elif defined(__clang__)
+
+typedef float __m128 __attribute__((__vector_size__(16), __aligned__(16)));
+typedef long long __m128i
+    __attribute__((__vector_size__(16), __aligned__(16)));
+typedef long long __m128i_u
+    __attribute__((__vector_size__(16), __may_alias__, __aligned__(1)));
+typedef long long __v2di __attribute__((__vector_size__(16)));
+typedef unsigned long long __v2du __attribute__((__vector_size__(16)));
+typedef int __v4si __attribute__((__vector_size__(16)));
+typedef float __v4sf __attribute__((__vector_size__(16)));
+typedef short __v8hi __attribute__((__vector_size__(16)));
+
+#define	_INTRINSATTR							      \
+	__attribute__((__always_inline__, __nodebug__, __target__("sse2"),    \
+		__min_vector_width__(128)))
+#define	_PACKALIAS							      \
+	__attribute__((__packed__, __may_alias__))
+
+#else
+
+#error Please teach me how to do Intel intrinsics for your compiler!
+
+#endif
+
+_INTRINSATTR
+static __inline __m128i
+_mm_loadu_si32(const void *__p)
+{
+	int32_t __v = ((const struct { int32_t __v; } _PACKALIAS *)__p)->__v;
+	return __extension__ (__m128i)(__v4si){ __v, 0, 0, 0 };
+}
+
+_INTRINSATTR
+static __inline __m128i
+_mm_loadu_si64(const void *__p)
+{
+	int64_t __v = ((const struct { int64_t __v; } _PACKALIAS *)__p)->__v;
+	return __extension__ (__m128i)(__v2di){ __v, 0 };
+}
+
+_INTRINSATTR
+static __inline __m128i
+_mm_set1_epi16(int16_t __v)
+{
+	return __extension__ (__m128i)(__v8hi){
+	    __v, __v, __v, __v, __v, __v, __v, __v
+	};
+}
+
+_INTRINSATTR
+static __inline __m128i
+_mm_set1_epi32(int32_t __v)
+{
+	return __extension__ (__m128i)(__v4si){ __v, __v, __v, __v };
+}
+
+_INTRINSATTR
+static __inline __m128i
+_mm_set1_epi64x(int64_t __v)
+{
+	return __extension__ (__m128i)(__v2di){ __v, __v };
+}
+
+_INTRINSATTR
+static __inline __m128i
+_mm_set_epi32(int32_t __v3, int32_t __v2, int32_t __v1, int32_t __v0)
+{
+	return __extension__ (__m128i)(__v4si){ __v0, __v1, __v2, __v3 };
+}
+
+_INTRINSATTR
+static __inline __m128i
+_mm_set_epi64x(int64_t __v1, int64_t __v0)
+{
+	return __extension__ (__m128i)(__v2di){ __v0, __v1 };
+}
+
+_INTRINSATTR
+static __inline __m128i
+_mm_setzero_si128(void)
+{
+	return _mm_set1_epi64x(0);
+}
+
+#define	_mm_shuffle_epi32(v,m)						      \
+	(__m128i)__builtin_ia32_pshufd((__v4si)(__m128i)(v), (int)(m))
+
+#define	_mm_shuffle_ps(x,y,m)						      \
+	(__m128)__builtin_ia32_shufps((__v4sf)(__m128)(x),		      \
+	    (__v4sf)(__m128)(y), (int)(m))				      \
+
+_INTRINSATTR
+static __inline __m128i
+_mm_slli_epi64(__m128i __v, uint8_t __bits)
+{
+	return (__m128i)__builtin_ia32_psllqi128((__v2di)__v, (int)__bits);
+}
+
+#if defined(__GNUC__) && !defined(__clang__)
+#define	_mm_slli_si128(v,bytes)						      \
+	(__m128i)__builtin_ia32_pslldqi128((__v2di)(__m128i)(v),	      \
+	    8*(int)(bytes))
+#elif defined(__clang__)
+#define	_mm_slli_si128(v,bytes)						      \
+	(__m128i)__builtin_ia32_pslldqi128_byteshift((__v2di)(__m128i)(v),    \
+	    (int)(bytes))
+#endif
+
+_INTRINSATTR
+static __inline __m128i
+_mm_srli_epi64(__m128i __v, uint8_t __bits)
+{
+	return (__m128i)__builtin_ia32_psrlqi128((__v2di)__v, (int)__bits);
+}
+
+#if defined(__GNUC__) && !defined(__clang__)
+#define	_mm_srli_si128(v,bytes)						      \
+	(__m128i)__builtin_ia32_psrldqi128((__m128i)(v), 8*(int)(bytes))
+#elif defined(__clang__)
+#define	_mm_srli_si128(v,bytes)						      \
+	(__m128i)__builtin_ia32_psrldqi128_byteshift((__v2di)(__m128i)(v),    \
+	    (int)(bytes));
+#endif
+
+_INTRINSATTR
+static __inline void
+_mm_storeu_si32(void *__p, __m128i __v)
+{
+	((struct { int32_t __v; } _PACKALIAS *)__p)->__v = ((__v4si)__v)[0];
+}
+
+_INTRINSATTR
+static __inline void
+_mm_storeu_si64(void *__p, __m128i __v)
+{
+	((struct { int64_t __v; } _PACKALIAS *)__p)->__v = ((__v2di)__v)[0];
+}
+
+_INTRINSATTR
+static __inline __m128i
+_mm_sub_epi64(__m128i __x, __m128i __y)
+{
+	return (__m128i)((__v2du)__x - (__v2du)__y);
+}
+
+_INTRINSATTR
+static __inline __m128i
+_mm_unpacklo_epi64(__m128i __lo, __m128i __hi)
+{
+#if defined(__GNUC__) && !defined(__clang__)
+	return (__m128i)__builtin_ia32_punpcklqdq128((__v2di)__lo,
+	    (__v2di)__hi);
+#elif defined(__clang__)
+	return (__m128i)__builtin_shufflevector((__v4si)__lo, (__v4si)__hi,
+	    0, 4, 1, 5);
+#endif
+}
+
+#endif	/* _SYS_CRYPTO_AES_ARCH_X86_IMMINTRIN_H */
