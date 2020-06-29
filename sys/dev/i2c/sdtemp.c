87da8e62236d8ab707fbf5fc01adff4ab9948d90
msaitoh <msaitoh@NetBSD.org>
2020-06-29 09:24:07 +0000

    Check the return value of iic_acquire_bus(). This function may fail.
     One of the case is driver's detaching phase on shutdown. mutex_tryenter()
    might fail and return with EBUSY. To avoid calling iic_release_bus() without
    taking lock, check the return value of iic_acquire_bus().

--- sys/dev/i2c/sdtemp.c
+++ sys/dev/i2c/sdtemp.c
@@ -1,4 +1,4 @@
-/*      $NetBSD: sdtemp.c,v 1.37 2020/06/29 06:01:30 msaitoh Exp $        */
+/*      $NetBSD: sdtemp.c,v 1.38 2020/06/29 09:24:07 msaitoh Exp $        */
 
 /*
  * Copyright (c) 2009 The NetBSD Foundation, Inc.
@@ -30,7 +30,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: sdtemp.c,v 1.37 2020/06/29 06:01:30 msaitoh Exp $");
+__KERNEL_RCSID(0, "$NetBSD: sdtemp.c,v 1.38 2020/06/29 09:24:07 msaitoh Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -218,7 +218,9 @@ sdtemp_match(device_t parent, cfdata_t cf, void *aux)
 	 * Verify that we can read the manufacturer ID, Device ID and the
 	 * capability
 	 */
-	iic_acquire_bus(sc.sc_tag, 0);
+	error = iic_acquire_bus(sc.sc_tag, 0);
+	if (error)
+		return 0;
 	error = sdtemp_read_16(&sc, SDTEMP_REG_MFG_ID,  &mfgid) |
 		sdtemp_read_16(&sc, SDTEMP_REG_DEV_REV, &devid) |
 		sdtemp_read_16(&sc, SDTEMP_REG_CAPABILITY, &cap);
@@ -258,7 +260,10 @@ sdtemp_attach(device_t parent, device_t self, void *aux)
 	sc->sc_address = ia->ia_addr;
 	sc->sc_dev = self;
 
-	iic_acquire_bus(sc->sc_tag, 0);
+	error = iic_acquire_bus(sc->sc_tag, 0);
+	if (error)
+		return;
+
 	if ((error = sdtemp_read_16(sc, SDTEMP_REG_MFG_ID,  &mfgid)) != 0 ||
 	    (error = sdtemp_read_16(sc, SDTEMP_REG_DEV_REV, &devid)) != 0) {
 		iic_release_bus(sc->sc_tag, 0);
@@ -426,7 +431,9 @@ sdtemp_get_limits(struct sysmon_envsys *sme, envsys_data_t *edata,
 	uint16_t lim;
 
 	*props = 0;
-	iic_acquire_bus(sc->sc_tag, 0);
+	if (iic_acquire_bus(sc->sc_tag, 0) != 0)
+		return;
+
 	if (sdtemp_read_16(sc, SDTEMP_REG_LOWER_LIM, &lim) == 0 && lim != 0) {
 		limits->sel_warnmin = sdtemp_decode_temp(sc, lim);
 		*props |= PROP_WARNMIN;
@@ -456,7 +463,9 @@ sdtemp_set_limits(struct sysmon_envsys *sme, envsys_data_t *edata,
 		limits = &sc->sc_deflims;
 		props  = &sc->sc_defprops;
 	}
-	iic_acquire_bus(sc->sc_tag, 0);
+	if (iic_acquire_bus(sc->sc_tag, 0) != 0)
+		return;
+
 	if (*props & PROP_WARNMIN) {
 		val = __UK2C(limits->sel_warnmin);
 		(void)sdtemp_write_16(sc, SDTEMP_REG_LOWER_LIM,
@@ -568,7 +577,10 @@ sdtemp_refresh(struct sysmon_envsys *sme, envsys_data_t *edata)
 	uint16_t val;
 	int error;
 
-	iic_acquire_bus(sc->sc_tag, 0);
+	error = iic_acquire_bus(sc->sc_tag, 0);
+	if (error)
+		return;
+
 	error = sdtemp_read_16(sc, SDTEMP_REG_AMBIENT_TEMP, &val);
 	iic_release_bus(sc->sc_tag, 0);
 
@@ -610,7 +622,10 @@ sdtemp_pmf_suspend(device_t dev, const pmf_qual_t *qual)
 	int error;
 	uint16_t config;
 
-	iic_acquire_bus(sc->sc_tag, 0);
+	error = iic_acquire_bus(sc->sc_tag, 0);
+	if (error != 0)
+		return false;
+
 	error = sdtemp_read_16(sc, SDTEMP_REG_CONFIG, &config);
 	if (error == 0) {
 		config |= SDTEMP_CONFIG_SHUTDOWN_MODE;
@@ -627,7 +642,10 @@ sdtemp_pmf_resume(device_t dev, const pmf_qual_t *qual)
 	int error;
 	uint16_t config;
 
-	iic_acquire_bus(sc->sc_tag, 0);
+	error = iic_acquire_bus(sc->sc_tag, 0);
+	if (error != 0)
+		return false;
+
 	error = sdtemp_read_16(sc, SDTEMP_REG_CONFIG, &config);
 	if (error == 0) {
 		config &= ~SDTEMP_CONFIG_SHUTDOWN_MODE;
