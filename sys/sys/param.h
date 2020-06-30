1d6d6bfbdc5284387e583fc8b0cf219a5a43dee0
riastradh <riastradh@NetBSD.org>
2020-06-30 06:25:15 +0000

    NetBSD 6.99.69 welcomes you, and hopes you enjoy your new AES API.

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.671 2020/06/19 13:52:40 jdolecek Exp $	*/
+/*	$NetBSD: param.h,v 1.672 2020/06/30 06:25:15 riastradh Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -67,7 +67,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	999006800	/* NetBSD 9.99.68 */
+#define	__NetBSD_Version__	999006900	/* NetBSD 9.99.69 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
