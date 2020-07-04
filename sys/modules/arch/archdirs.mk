14b4112f58bc3cdf57a03f2523d6407ba8622895
chs <chs@NetBSD.org>
2020-07-04 21:02:15 +0000

    the x86 xen and non-xen modules are identical, so remove the unneeded extra copies. Xen kernels now use the same modules as native kernels.

--- sys/modules/arch/archdirs.mk
+++ sys/modules/arch/archdirs.mk
@@ -1,14 +1,6 @@
-#	$NetBSD: archdirs.mk,v 1.5 2020/06/27 06:50:00 rin Exp $
+#	$NetBSD: archdirs.mk,v 1.6 2020/07/04 21:02:16 chs Exp $
 
 # list of subdirs used per-platform
-
-.if ${MACHINE} == "amd64"
-ARCHDIR_SUBDIR=	x86/amd64-xen
-.endif
-
-.if ${MACHINE} == "i386"
-ARCHDIR_SUBDIR=	x86/i386pae-xen
-.endif
 
 .if ${MACHINE_ARCH} == "powerpc"
 ARCHDIR_SUBDIR=	powerpc/powerpc-ibm4xx powerpc/powerpc-booke
