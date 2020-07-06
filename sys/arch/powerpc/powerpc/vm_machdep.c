a8ea2f981950be5135ccbc2958bba4221dc4d67b
rin <rin@NetBSD.org>
2020-07-06 10:52:12 +0000

    Drop unused opt_multiprocessor.h includes.

--- sys/arch/powerpc/powerpc/vm_machdep.c
+++ sys/arch/powerpc/powerpc/vm_machdep.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: vm_machdep.c,v 1.103 2020/07/06 09:34:18 rin Exp $	*/
+/*	$NetBSD: vm_machdep.c,v 1.104 2020/07/06 10:52:12 rin Exp $	*/
 
 /*
  * Copyright (C) 1995, 1996 Wolfgang Solfrank.
@@ -32,11 +32,10 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: vm_machdep.c,v 1.103 2020/07/06 09:34:18 rin Exp $");
+__KERNEL_RCSID(0, "$NetBSD: vm_machdep.c,v 1.104 2020/07/06 10:52:12 rin Exp $");
 
 #ifdef _KERNEL_OPT
 #include "opt_altivec.h"
-#include "opt_multiprocessor.h"
 #include "opt_ppcarch.h"
 #include "opt_ppccache.h"
 #endif
