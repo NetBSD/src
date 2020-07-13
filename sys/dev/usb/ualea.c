410a1f20ff384165037b9c02f82d6a2402448a36
martin <martin@NetBSD.org>
2020-07-13 14:32:20 +0000

    Pull up following revision(s) (requested by simonb in ticket #1003):
    	sys/dev/usb/ualea.c: revision 1.12
    
    Set up the USB xfer for every transfer, not just once in the attach
    function.  Caught by DIAGNOSTIC on a similar driver.  Thanks to mrg@
    for USB xfer clue and martin@ for testing.

--- sys/dev/usb/ualea.c
+++ sys/dev/usb/ualea.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: ualea.c,v 1.9 2018/01/21 13:57:12 skrll Exp $	*/
+/*	$NetBSD: ualea.c,v 1.9.10.1 2020/07/13 14:32:20 martin Exp $	*/
 
 /*-
  * Copyright (c) 2017 The NetBSD Foundation, Inc.
@@ -30,7 +30,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: ualea.c,v 1.9 2018/01/21 13:57:12 skrll Exp $");
+__KERNEL_RCSID(0, "$NetBSD: ualea.c,v 1.9.10.1 2020/07/13 14:32:20 martin Exp $");
 
 #include <sys/types.h>
 #include <sys/atomic.h>
@@ -144,11 +144,6 @@ ualea_attach(device_t parent, device_t self, void *aux)
 		return;
 	}
 
-	/* Setup the xfer to call ualea_xfer_done with sc.  */
-	usbd_setup_xfer(sc->sc_xfer, sc, usbd_get_buffer(sc->sc_xfer),
-	    sc->sc_maxpktsize, USBD_SHORT_XFER_OK, USBD_NO_TIMEOUT,
-	    ualea_xfer_done);
-
 	/* Success!  We are ready to run.  */
 	mutex_enter(&sc->sc_lock);
 	sc->sc_attached = true;
@@ -196,6 +191,11 @@ ualea_xfer(struct ualea_softc *sc)
 	/* Do nothing if we need nothing.  */
 	if (sc->sc_needed == 0)
 		return;
+
+	/* Setup the xfer to call ualea_xfer_done with sc.  */
+	usbd_setup_xfer(sc->sc_xfer, sc, usbd_get_buffer(sc->sc_xfer),
+	    sc->sc_maxpktsize, USBD_SHORT_XFER_OK, USBD_NO_TIMEOUT,
+	    ualea_xfer_done);
 
 	/* Issue xfer or complain if we can't.  */
 	/*
