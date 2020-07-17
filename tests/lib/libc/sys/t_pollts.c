a44e468f9f98e98186494a902f75b8ae07a03a9a
kamil <kamil@NetBSD.org>
2020-07-17 15:34:16 +0000

    Add ppoll() a compatibility wrapper around pollts(2)
    Submitted by Apurva Nandan.

--- tests/lib/libc/sys/t_pollts.c
+++ tests/lib/libc/sys/t_pollts.c
@@ -1,7 +1,7 @@
-/*	$NetBSD: t_poll.c,v 1.4 2020/07/17 15:34:16 kamil Exp $	*/
+/*	$NetBSD: t_pollts.c,v 1.1 2020/07/17 15:34:17 kamil Exp $	*/
 
 /*-
- * Copyright (c) 2011 The NetBSD Foundation, Inc.
+ * Copyright (c) 2011, 2020 The NetBSD Foundation, Inc.
  * All rights reserved.
  *
  * This code is derived from software contributed to The NetBSD Foundation
@@ -41,122 +41,23 @@
 #include <signal.h>
 #include <unistd.h>
 
-static int desc;
-
-static void
-child1(void)
-{
-	struct pollfd pfd;
-
-	pfd.fd = desc;
-	pfd.events = POLLIN | POLLHUP | POLLOUT;
-
-	(void)poll(&pfd, 1, 2000);
-	(void)printf("child1 exit\n");
-}
-
-static void
-child2(void)
-{
-	struct pollfd pfd;
-
-	pfd.fd = desc;
-	pfd.events = POLLIN | POLLHUP | POLLOUT;
-
-	(void)sleep(1);
-	(void)poll(&pfd, 1, INFTIM);
-	(void)printf("child2 exit\n");
-}
-
-static void
-child3(void)
-{
-	struct pollfd pfd;
-
-	(void)sleep(5);
-
-	pfd.fd = desc;
-	pfd.events = POLLIN | POLLHUP | POLLOUT;
-
-	(void)poll(&pfd, 1, INFTIM);
-	(void)printf("child3 exit\n");
-}
-
-ATF_TC(3way);
-ATF_TC_HEAD(3way, tc)
-{
-	atf_tc_set_md_var(tc, "timeout", "15");
-	atf_tc_set_md_var(tc, "descr",
-	    "Check for 3-way collision for descriptor. First child comes "
-	    "and polls on descriptor, second child comes and polls, first "
-	    "child times out and exits, third child comes and polls. When "
-	    "the wakeup event happens, the two remaining children should "
-	    "both be awaken. (kern/17517)");
-}
-
-ATF_TC_BODY(3way, tc)
-{
-	int pf[2];
-	int status, i;
-	pid_t pid;
-
-	pipe(pf);
-	desc = pf[0];
-
-	pid = fork();
-	ATF_REQUIRE(pid >= 0);
-
-	if (pid == 0) {
-		(void)close(pf[1]);
-		child1();
-		_exit(0);
-		/* NOTREACHED */
-	}
-
-	pid = fork();
-	ATF_REQUIRE(pid >= 0);
-
-	if (pid == 0) {
-		(void)close(pf[1]);
-		child2();
-		_exit(0);
-		/* NOTREACHED */
-	}
-
-	pid = fork();
-	ATF_REQUIRE( pid >= 0);
-
-	if (pid == 0) {
-		(void)close(pf[1]);
-		child3();
-		_exit(0);
-		/* NOTREACHED */
-	}
-
-	(void)sleep(10);
-
-	(void)printf("parent write\n");
-
-	ATF_REQUIRE(write(pf[1], "konec\n", 6) == 6);
-
-	for(i = 0; i < 3; ++i)
-		(void)wait(&status);
-
-	(void)printf("parent terminated\n");
-}
+#ifndef POLLTS
+#define POLLTS pollts
+#endif
 
 ATF_TC(basic);
 ATF_TC_HEAD(basic, tc)
 {
 	atf_tc_set_md_var(tc, "timeout", "10");
 	atf_tc_set_md_var(tc, "descr",
-	    "Basis functionality test for poll(2)");
+	    "Basis functionality test for ppoll(2)/pollts(2)");
 }
 
 ATF_TC_BODY(basic, tc)
 {
 	int fds[2];
 	struct pollfd pfds[2];
+	struct timespec timeout;
 	int ret;
 
 	ATF_REQUIRE_EQ(pipe(fds), 0);
@@ -166,13 +67,17 @@ ATF_TC_BODY(basic, tc)
 	pfds[1].fd = fds[1];
 	pfds[1].events = POLLOUT;
 
+	/* Use a timeout of 1 second. */
+	timeout.tv_sec = 1;
+	timeout.tv_nsec = 0;
+
 	/*
 	 * Check that we get a timeout waiting for data on the read end
 	 * of our pipe.
 	 */
 	pfds[0].revents = -1;
 	pfds[1].revents = -1;
-	ATF_REQUIRE_EQ_MSG(ret = poll(&pfds[0], 1, 1), 0,
+	ATF_REQUIRE_EQ_MSG(ret = POLLTS(&pfds[0], 1, &timeout, NULL), 0,
 	    "got: %d", ret);
 	ATF_REQUIRE_EQ_MSG(pfds[0].revents, 0, "got: %d", pfds[0].revents);
 	ATF_REQUIRE_EQ_MSG(pfds[1].revents, -1, "got: %d", pfds[1].revents);
@@ -180,7 +85,7 @@ ATF_TC_BODY(basic, tc)
 	/* Check that the write end of the pipe as reported as ready. */
 	pfds[0].revents = -1;
 	pfds[1].revents = -1;
-	ATF_REQUIRE_EQ_MSG(ret = poll(&pfds[1], 1, 1), 1,
+	ATF_REQUIRE_EQ_MSG(ret = POLLTS(&pfds[1], 1, &timeout, NULL), 1,
 	    "got: %d", ret);
 	ATF_REQUIRE_EQ_MSG(pfds[0].revents, -1, "got: %d", pfds[0].revents);
 	ATF_REQUIRE_EQ_MSG(pfds[1].revents, POLLOUT, "got: %d",\
@@ -189,7 +94,7 @@ ATF_TC_BODY(basic, tc)
 	/* Check that only the write end of the pipe as reported as ready. */
 	pfds[0].revents = -1;
 	pfds[1].revents = -1;
-	ATF_REQUIRE_EQ_MSG(ret = poll(pfds, 2, 1), 1,
+	ATF_REQUIRE_EQ_MSG(ret = POLLTS(pfds, 2, &timeout, NULL), 1,
 	    "got: %d", ret);
 	ATF_REQUIRE_EQ_MSG(pfds[0].revents, 0, "got: %d", pfds[0].revents);
 	ATF_REQUIRE_EQ_MSG(pfds[1].revents, POLLOUT, "got: %d",
@@ -201,7 +106,7 @@ ATF_TC_BODY(basic, tc)
 	/* Check that both ends of our pipe are reported as ready. */
 	pfds[0].revents = -1;
 	pfds[1].revents = -1;
-	ATF_REQUIRE_EQ_MSG(ret = poll(pfds, 2, 1), 2,
+	ATF_REQUIRE_EQ_MSG(ret = POLLTS(pfds, 2, &timeout, NULL), 2,
 	    "got: %d", ret);
 	ATF_REQUIRE_EQ_MSG(pfds[0].revents, POLLIN, "got: %d",
 	    pfds[0].revents);
@@ -215,30 +120,82 @@ ATF_TC_BODY(basic, tc)
 ATF_TC(err);
 ATF_TC_HEAD(err, tc)
 {
-	atf_tc_set_md_var(tc, "descr", "Check errors from poll(2)");
+	atf_tc_set_md_var(tc, "descr", "Check errors from ppoll(2)/pollts(2)");
 }
 
 ATF_TC_BODY(err, tc)
 {
+	struct timespec timeout;
 	struct pollfd pfd;
 	int fd = 0;
 
 	pfd.fd = fd;
 	pfd.events = POLLIN;
 
-	errno = 0;
-	ATF_REQUIRE_ERRNO(EFAULT, poll((struct pollfd *)-1, 1, -1) == -1);
+	timeout.tv_sec = 1;
+	timeout.tv_nsec = 0;
 
 	errno = 0;
-	ATF_REQUIRE_ERRNO(EINVAL, poll(&pfd, 1, -2) == -1);
+	ATF_REQUIRE_ERRNO(EFAULT, POLLTS((void *)-1, 1, &timeout, NULL) == -1);
+
+	timeout.tv_sec = -1;
+	timeout.tv_nsec = -1;
+
+	errno = 0;
+	ATF_REQUIRE_ERRNO(EINVAL, POLLTS(&pfd, 1, &timeout, NULL) == -1);
+}
+
+ATF_TC(sigmask);
+ATF_TC_HEAD(sigmask, tc)
+{
+	atf_tc_set_md_var(tc, "timeout", "10");
+	atf_tc_set_md_var(tc, "descr",
+	    "Check that ppoll(2)/pollts(2) restores the signal mask (PR kern/44986)");
+}
+
+ATF_TC_BODY(sigmask, tc)
+{
+	int fd;
+	struct pollfd pfd;
+	struct timespec timeout;
+	sigset_t mask;
+	int ret;
+
+	fd = open(_PATH_DEVNULL, O_RDONLY);
+	ATF_REQUIRE(fd >= 0);
+
+	pfd.fd = fd;
+	pfd.events = POLLIN;
+
+	/* Use a timeout of 1 second. */
+	timeout.tv_sec = 1;
+	timeout.tv_nsec = 0;
+
+	/* Unblock all signals. */
+	ATF_REQUIRE_EQ(sigfillset(&mask), 0);
+	ATF_REQUIRE_EQ(sigprocmask(SIG_UNBLOCK, &mask, NULL), 0);
+
+	/*
+	 * Check that ppoll(2)/pollts(2) immediately returns. We block *all*
+	 * signals during ppoll(2)/pollts(2).
+	 */
+	ATF_REQUIRE_EQ_MSG(ret = POLLTS(&pfd, 1, &timeout, &mask), 1,
+	    "got: %d", ret);
+
+	/* Check that signals are now longer blocked. */
+	ATF_REQUIRE_EQ(sigprocmask(SIG_SETMASK, NULL, &mask), 0);
+	ATF_REQUIRE_EQ_MSG(sigismember(&mask, SIGUSR1), 0,
+	    "signal mask was changed.");
+
+	ATF_REQUIRE_EQ(close(fd), 0);
 }
 
 ATF_TP_ADD_TCS(tp)
 {
 
-	ATF_TP_ADD_TC(tp, 3way);
 	ATF_TP_ADD_TC(tp, basic);
 	ATF_TP_ADD_TC(tp, err);
+	ATF_TP_ADD_TC(tp, sigmask);
 
 	return atf_no_error();
 }
