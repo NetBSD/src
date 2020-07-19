f8fbbfbea8c2573d8d3cad1878e03eafdb6d3f18
rillig <rillig@NetBSD.org>
2020-07-19 07:08:09 +0000

    make(1): demonstrate inconsistent :ts behavior among modifiers

--- usr.bin/make/unit-tests/modts.mk
+++ usr.bin/make/unit-tests/modts.mk
@@ -7,7 +7,7 @@ FU_mod-ts = a / b / cool
 AAA= a a a
 B.aaa= Baaa
 
-all:   mod-ts
+all:   mod-ts mod-ts-space
 
 # Use print or printf iff they are builtin.
 # XXX note that this causes problems, when make decides 
@@ -42,3 +42,19 @@ mod-ts:
 	@${PRINT} 'FU_$@="${FU_${@:ts}:ts}"'
 	@${PRINT} 'FU_$@:ts:T="${FU_${@:ts}:ts:T}" == cool?'
 	@${PRINT} 'B.$${AAA:ts}="${B.${AAA:ts}}" == Baaa?'
+
+mod-ts-space:
+	# After the :ts modifier, the whole string is interpreted as a single
+	# word since all spaces have been replaced with x.
+	@${PRINT} ':ts :S => '${aa bb aa bb aa bb:L:tsx:S,b,B,:Q}
+
+	# The :ts modifier also applies to word separators that are added
+	# afterwards.
+	@${PRINT} ':ts :S space    => '${a ababa c:L:tsx:S,b, ,g:Q}
+	@${PRINT} ':ts :S space :M => '${a ababa c:L:tsx:S,b, ,g:M*:Q}
+
+	# Not all modifiers behave this way though.  Some of them always use
+	# a space as word separator instead of the :ts separator.
+	# This seems like an oversight during implementation.
+	@${PRINT} ':ts :S    => '${a ababa c:L:tsx:S,b, ,g:Q}
+	@${PRINT} ':ts :S :@ => '${a ababa c:L:tsx:S,b, ,g:@v@${v}@:Q}
