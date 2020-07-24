4c97804472bb40ab277c0a6d1ae0f88e7d4bde3e
rillig <rillig@NetBSD.org>
2020-07-24 08:20:24 +0000

    make(1): make condition in ModifyWord_SubstRegex simpler
    The generated code is still the same, at least with Clang.

--- usr.bin/make/var.c
+++ usr.bin/make/var.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: var.c,v 1.305 2020/07/24 08:12:43 rillig Exp $	*/
+/*	$NetBSD: var.c,v 1.306 2020/07/24 08:20:24 rillig Exp $	*/
 
 /*
  * Copyright (c) 1988, 1989, 1990, 1993
@@ -69,14 +69,14 @@
  */
 
 #ifndef MAKE_NATIVE
-static char rcsid[] = "$NetBSD: var.c,v 1.305 2020/07/24 08:12:43 rillig Exp $";
+static char rcsid[] = "$NetBSD: var.c,v 1.306 2020/07/24 08:20:24 rillig Exp $";
 #else
 #include <sys/cdefs.h>
 #ifndef lint
 #if 0
 static char sccsid[] = "@(#)var.c	8.3 (Berkeley) 3/19/94";
 #else
-__RCSID("$NetBSD: var.c,v 1.305 2020/07/24 08:12:43 rillig Exp $");
+__RCSID("$NetBSD: var.c,v 1.306 2020/07/24 08:20:24 rillig Exp $");
 #endif
 #endif /* not lint */
 #endif
@@ -1392,8 +1392,7 @@ ModifyWord_SubstRegex(const char *word, SepBuf *buf, void *data)
     int flags = 0;
     regmatch_t m[10];
 
-    if ((args->pflags & (VARP_SUB_ONE | VARP_SUB_MATCHED)) ==
-	(VARP_SUB_ONE | VARP_SUB_MATCHED))
+    if ((args->pflags & VARP_SUB_ONE) && (args->pflags & VARP_SUB_MATCHED))
 	xrv = REG_NOMATCH;
     else {
     tryagain:
