51f85f64b4e61f6408bdb4f4a3c06c7ece3705eb
rillig <rillig@NetBSD.org>
2020-07-26 10:11:04 +0000

    make(1): fix missing error message about missing @ delimiter
    This had been broken since 2020-07-03, during a "refactoring".

--- usr.bin/make/var.c
+++ usr.bin/make/var.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: var.c,v 1.308 2020/07/24 08:24:23 rillig Exp $	*/
+/*	$NetBSD: var.c,v 1.309 2020/07/26 10:11:04 rillig Exp $	*/
 
 /*
  * Copyright (c) 1988, 1989, 1990, 1993
@@ -69,14 +69,14 @@
  */
 
 #ifndef MAKE_NATIVE
-static char rcsid[] = "$NetBSD: var.c,v 1.308 2020/07/24 08:24:23 rillig Exp $";
+static char rcsid[] = "$NetBSD: var.c,v 1.309 2020/07/26 10:11:04 rillig Exp $";
 #else
 #include <sys/cdefs.h>
 #ifndef lint
 #if 0
 static char sccsid[] = "@(#)var.c	8.3 (Berkeley) 3/19/94";
 #else
-__RCSID("$NetBSD: var.c,v 1.308 2020/07/24 08:24:23 rillig Exp $");
+__RCSID("$NetBSD: var.c,v 1.309 2020/07/26 10:11:04 rillig Exp $");
 #endif
 #endif /* not lint */
 #endif
@@ -3171,7 +3171,8 @@ ApplyModifiers(char *nstr, const char *tstr,
 		break;
 	    }
 	case '@':
-	    ApplyModifier_Loop(p, &st);
+	    if (!ApplyModifier_Loop(p, &st))
+		goto cleanup;
 	    break;
 	case '_':
 	    if (!ApplyModifier_Remember(p, &st))
