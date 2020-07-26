fd4b39b5bd24e7f6cb1716a53234eca23d6cb062
martin <martin@NetBSD.org>
2020-07-26 10:59:47 +0000

    Pull up following revision(s) (requested by tsutsui in ticket #1028):
    	share/mk/bsd.x11.mk: revision 1.135
    	external/mit/xorg/server/xorg-server/hw/xnest/Makefile: revision 1.22
    	external/mit/xorg/server/xorg-server.old/hw/xnest/Makefile: revision 1.4
    
    Fix typos in ${X11INCS.DIX} include paths. PR/55500
    Remove extra "-I" for ${X11INCS.DIX} macro.

--- share/mk/bsd.x11.mk
+++ share/mk/bsd.x11.mk
@@ -1,4 +1,4 @@
-#	$NetBSD: bsd.x11.mk,v 1.132 2019/07/10 21:53:35 mrg Exp $
+#	$NetBSD: bsd.x11.mk,v 1.132.2.1 2020/07/26 10:59:47 martin Exp $
 
 .include <bsd.init.mk>
 
@@ -46,8 +46,8 @@ X11FLAGS.EXTENSION=	${X11FLAGS.BASE_EXTENSION} \
 X11FLAGS.DIX=		-DHAVE_DIX_CONFIG_H -D_BSD_SOURCE -DHAS_FCHOWN \
 			-DHAS_STICKY_DIR_BIT -D_POSIX_THREAD_SAFE_FUNCTIONS=200112L \
 			-DHAVE_XORG_CONFIG_H
-X11INCS.DIX=		-I${X11INCSDIR}/freetype2  \
-			-I${X11INCSDIR}/pixman-1 \
+X11INCS.DIX=		-I${X11INCDIR}/freetype2  \
+			-I${X11INCDIR}/pixman-1 \
 			-I$(X11SRCDIR.xorg-server)/include \
 			-I$(X11SRCDIR.xorg-server)/Xext \
 			-I$(X11SRCDIR.xorg-server)/composite \
