3cfc2bc1145ec24dd50e59c2a69c48528a0c77f4
rillig <rillig@NetBSD.org>
2020-07-27 18:51:03 +0000

    make(1): add very basic test for archive handling
    The whole code in arch.c had been uncovered by tests before.
    
    The code coverage shows that neither the archive name nor the member
    contain any $, even though archive.mk looks like it.  It could be
    necessary to place the variable assignments below the dependency line,
    to force late evaluation.

---
+++ usr.bin/make/unit-tests/archive.mk
@@ -0,0 +1,37 @@
+# $NetBSD: archive.mk,v 1.1 2020/07/27 18:51:03 rillig Exp $
+#
+# Very basic demonstration of handling archives, based on the description
+# in PSD.doc/tutorial.ms.
+
+ARCHIVE=	libprog.${EXT.a}
+FILES=		archive.${EXT.mk} modmisc.${EXT.mk} varmisc.mk
+
+EXT.a=		a
+EXT.mk=		mk
+
+MAKE_CMD=	${.MAKE} -f ${MAKEFILE}
+RUN?=		@set -eu;
+
+all:
+	${RUN} ${MAKE_CMD} create-archive
+	${RUN} ${MAKE_CMD} list-archive
+	${RUN} ${MAKE_CMD} depend-on-existing-member
+	${RUN} ${MAKE_CMD} depend-on-nonexistent-member
+	${RUN} ${MAKE_CMD} remove-archive
+
+create-archive: ${ARCHIVE}
+${ARCHIVE}: ${ARCHIVE}(${FILES})
+	ar cru ${.TARGET} ${.OODATE}
+	ranlib ${.TARGET}
+
+list-archive: ${ARCHIVE}
+	ar t ${.ALLSRC}
+
+depend-on-existing-member: ${ARCHIVE}(archive.mk)
+	${RUN} echo $@
+
+depend-on-nonexistent-member: ${ARCHIVE}(nonexistent.mk)
+	${RUN} echo $@
+
+remove-archive: ${ARCHIVE}
+	rm ${ARCHIVE}
