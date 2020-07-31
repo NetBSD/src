d3f3c9fd70d2312bf058f3112e3bc70e7ef7c47d
rillig <rillig@NetBSD.org>
2020-07-31 07:29:21 +0000

    make(1): fix undefined behavior in malformed :S modifier
    The unit tests failed on Ubuntu, and by looking closely at the code, it
    was trivial to see the out-of-bounds memory read.
    
    Other modifiers may have the same problem and will be inspected later.

--- usr.bin/make/var.c
+++ usr.bin/make/var.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: var.c,v 1.358 2020/07/29 21:35:35 rillig Exp $	*/
+/*	$NetBSD: var.c,v 1.359 2020/07/31 07:29:21 rillig Exp $	*/
 
 /*
  * Copyright (c) 1988, 1989, 1990, 1993
@@ -69,14 +69,14 @@
  */
 
 #ifndef MAKE_NATIVE
-static char rcsid[] = "$NetBSD: var.c,v 1.358 2020/07/29 21:35:35 rillig Exp $";
+static char rcsid[] = "$NetBSD: var.c,v 1.359 2020/07/31 07:29:21 rillig Exp $";
 #else
 #include <sys/cdefs.h>
 #ifndef lint
 #if 0
 static char sccsid[] = "@(#)var.c	8.3 (Berkeley) 3/19/94";
 #else
-__RCSID("$NetBSD: var.c,v 1.358 2020/07/29 21:35:35 rillig Exp $");
+__RCSID("$NetBSD: var.c,v 1.359 2020/07/31 07:29:21 rillig Exp $");
 #endif
 #endif /* not lint */
 #endif
@@ -2395,6 +2395,11 @@ ApplyModifier_Subst(const char * const mod, ApplyModifiersState *st)
     ModifyWord_SubstArgs args;
     Boolean oneBigWord = st->oneBigWord;
     char delim = mod[1];
+    if (delim == '\0') {
+	Error("Missing delimiter for :S modifier");
+	st->next = mod + 1;
+	return AMR_CLEANUP;
+    }
 
     st->next = mod + 2;
 
