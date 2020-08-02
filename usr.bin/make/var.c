51ef6f0e63f81dc538971b4a71756c9ada8294e1
rillig <rillig@NetBSD.org>
2020-08-02 15:26:49 +0000

    make(1): remove unnecessary assignment in Var_Parse
    In a variable expression without braces or parentheses, it is not
    possible to have modifiers.  Therefore the assigned endc could not have
    been used anywhere.

--- usr.bin/make/var.c
+++ usr.bin/make/var.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: var.c,v 1.397 2020/08/02 12:43:40 rillig Exp $	*/
+/*	$NetBSD: var.c,v 1.398 2020/08/02 15:26:49 rillig Exp $	*/
 
 /*
  * Copyright (c) 1988, 1989, 1990, 1993
@@ -69,14 +69,14 @@
  */
 
 #ifndef MAKE_NATIVE
-static char rcsid[] = "$NetBSD: var.c,v 1.397 2020/08/02 12:43:40 rillig Exp $";
+static char rcsid[] = "$NetBSD: var.c,v 1.398 2020/08/02 15:26:49 rillig Exp $";
 #else
 #include <sys/cdefs.h>
 #ifndef lint
 #if 0
 static char sccsid[] = "@(#)var.c	8.3 (Berkeley) 3/19/94";
 #else
-__RCSID("$NetBSD: var.c,v 1.397 2020/08/02 12:43:40 rillig Exp $");
+__RCSID("$NetBSD: var.c,v 1.398 2020/08/02 15:26:49 rillig Exp $");
 #endif
 #endif /* not lint */
 #endif
@@ -3427,7 +3427,6 @@ Var_Parse(const char * const str, GNode *ctxt, VarEvalFlags eflags,
 	} else {
 	    haveModifier = FALSE;
 	    tstr = str + 1;
-	    endc = str[1];
 	}
     } else {
 	endc = startc == PROPEN ? PRCLOSE : BRCLOSE;
