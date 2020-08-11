8da139c641f5fa319b1fd17184b27fd8e82c5d92
mrg <mrg@NetBSD.org>
2020-08-11 09:51:57 +0000

    update GCC 7 version to nb4 20200810, and fix the unknown GCC version error assignment from "=?" to "?=" so it works as designed.

--- tools/gcc/gcc-version.mk
+++ tools/gcc/gcc-version.mk
@@ -1,9 +1,9 @@
-#	$NetBSD: gcc-version.mk,v 1.18 2020/03/11 10:07:01 mrg Exp $
+#	$NetBSD: gcc-version.mk,v 1.19 2020/08/11 09:51:57 mrg Exp $
 
 # common location for tools and native build
 
 .if ${HAVE_GCC} == 7
-NETBSD_GCC_VERSION=nb3 20190319
+NETBSD_GCC_VERSION=nb4 20200810
 .elif ${HAVE_GCC} == 8
 NETBSD_GCC_VERSION=nb1 20200311
 .endif
