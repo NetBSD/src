f0286e4fb4d4092f7f76cce0a2abc175b4dfd968
skrll <skrll@NetBSD.org>
2020-08-14 16:18:36 +0000

    Mirror the changes to aarch64 and
    - Switch to TPIDRPRW_IS_CURLWP, because curlwp is accessed much more often
      by MI code.  It also makes curlwp preemption safe,
    
    - Make ASTs operate per-LWP rather than per-CPU, otherwise sometimes LWPs
      can see spurious ASTs (which doesn't cause a problem, it just means some
      time may be wasted).
    
    - Make sure ASTs are always set on the same CPU as the target LWP, and
      delivered via IPI if posted from a remote CPU so that they are resolved
      quickly.
    
    - Add some cache line padding to struct cpu_info.
    
    - Add a memory barrier in a couple of places where ci_curlwp is set.  This
      is needed whenever an LWP that is resuming on the CPU could hold an
      adaptive mutex.  The barrier needs to drain the CPU's store buffer, so
      that the update to ci_curlwp becomes globally visible before the LWP can
      resume and call mutex_exit().

--- sys/arch/arm/include/locore.h
+++ sys/arch/arm/include/locore.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: locore.h,v 1.32 2020/02/15 08:16:11 skrll Exp $	*/
+/*	$NetBSD: locore.h,v 1.33 2020/08/14 16:18:36 skrll Exp $	*/
 
 /*
  * Copyright (c) 1994-1996 Mark Brinicombe.
@@ -87,18 +87,23 @@
 #if defined (TPIDRPRW_IS_CURCPU)
 #define GET_CURCPU(rX)		mrc	p15, 0, rX, c13, c0, 4
 #define GET_CURLWP(rX)		GET_CURCPU(rX); ldr rX, [rX, #CI_CURLWP]
+#define GET_CURX(rCPU, rLWP)	GET_CURCPU(rCPU); ldr rLWP, [rCPU, #CI_CURLWP]
 #elif defined (TPIDRPRW_IS_CURLWP)
 #define GET_CURLWP(rX)		mrc	p15, 0, rX, c13, c0, 4
 #if defined (MULTIPROCESSOR)
 #define GET_CURCPU(rX)		GET_CURLWP(rX); ldr rX, [rX, #L_CPU]
+#define GET_CURX(rCPU, rLWP)	GET_CURLWP(rLWP); ldr rCPU, [rLWP, #L_CPU]
 #elif defined(_ARM_ARCH_7)
 #define GET_CURCPU(rX)		movw rX, #:lower16:cpu_info_store; movt rX, #:upper16:cpu_info_store
+#define GET_CURX(rCPU, rLWP)	GET_CURLWP(rLWP); GET_CURCPU(rCPU)
 #else
 #define GET_CURCPU(rX)		ldr rX, =_C_LABEL(cpu_info_store)
+#define GET_CURX(rCPU, rLWP)	GET_CURLWP(rLWP); ldr rCPU, [rLWP, #L_CPU]
 #endif
 #elif !defined(MULTIPROCESSOR)
 #define GET_CURCPU(rX)		ldr rX, =_C_LABEL(cpu_info_store)
 #define GET_CURLWP(rX)		GET_CURCPU(rX); ldr rX, [rX, #CI_CURLWP]
+#define GET_CURX(rCPU, rLWP)	GET_CURCPU(rCPU); ldr rLWP, [rCPU, #CI_CURLWP]
 #endif
 #define GET_CURPCB(rX)		GET_CURLWP(rX); ldr rX, [rX, #L_PCB]
 
