61268a3cf362894a3880aebd2eb8f58a15997355
martin <martin@NetBSD.org>
2020-08-14 11:02:42 +0000

    Pull up following revision(s) (requested by mrg in ticket #1049):
    	tools/gcc/mknative-gcc: revision 1.104
    	tools/gcc/mknative-gcc: revision 1.105
    	tools/gcc/gcc-version.mk: revision 1.19
    	share/mk/bsd.own.mk: revision 1.1203
    	tools/gcc/Makefile: revision 1.98
    	tools/gcc/mknative-gcc.old: revision 1.7
    	tools/gcc/mknative-gcc.old: revision 1.8
    
    First pass at mknative for GCC 7.5.0

--- tools/gcc/gcc-version.mk
+++ tools/gcc/gcc-version.mk
@@ -1,9 +1,7 @@
-#	$NetBSD: gcc-version.mk,v 1.16 2019/03/20 05:09:26 mrg Exp $
+#	$NetBSD: gcc-version.mk,v 1.16.2.1 2020/08/14 11:02:42 martin Exp $
 
 # common location for tools and native build
 
-.if ${HAVE_GCC} == 6
-NETBSD_GCC_VERSION=nb4 20181109
-.elif ${HAVE_GCC} == 7
-NETBSD_GCC_VERSION=nb3 20190319
+.if ${HAVE_GCC} == 7
+NETBSD_GCC_VERSION=nb4 20200810
 .endif
