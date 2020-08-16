1fb97e4ba3048299e1e9aed40af6de0d33ba327f
rillig <rillig@NetBSD.org>
2020-08-16 12:07:50 +0000

    make(1): add dummies for fine-grained tests, one per single feature
    The test names have been derived from the current manual page.
    
    All these tests are dummies right now, and the code from the existing
    tests will be moved into the new tests step by step.
    
    This is done to prevent modmisc, escape, varmod-edge and varmisc from
    growing without any bounds, and to reduce the side-effects of one test
    to the others.

---
+++ usr.bin/make/unit-tests/cond-cmp-string.mk
@@ -0,0 +1,8 @@
+# $NetBSD: cond-cmp-string.mk,v 1.1 2020/08/16 12:07:51 rillig Exp $
+#
+# TODO: Description
+
+# TODO: Implementation
+
+all:
+	@:;
---
+++ usr.bin/make/unit-tests/varname-vpath.mk
@@ -0,0 +1,8 @@
+# $NetBSD: varname-vpath.mk,v 1.1 2020/08/16 12:07:52 rillig Exp $
+#
+# TODO: Description
+
+# TODO: Implementation
+
+all:
+	@:;
