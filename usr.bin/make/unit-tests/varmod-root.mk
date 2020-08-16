d126d9d551c983605e42f0a93514a6e1793f7427
rillig <rillig@NetBSD.org>
2020-08-16 12:07:50 +0000

    make(1): add dummies for fine-grained tests, one per single feature
    The test names have been derived from the current manual page.
    
    All these tests are dummies right now, and the code from the existing
    tests will be moved into the new tests step by step.
    
    This is done to prevent modmisc, escape, varmod-edge and varmisc from
    growing without any bounds, and to reduce the side-effects of one test
    to the others.

---
+++ usr.bin/make/unit-tests/varmod-root.mk
@@ -0,0 +1,8 @@
+# $NetBSD: varmod-root.mk,v 1.1 2020/08/16 12:07:51 rillig Exp $
+#
+# TODO: Description
+
+# TODO: Implementation
+
+all:
+	@:;
---
+++ usr.bin/make/unit-tests/varmod-to-abs.mk
@@ -0,0 +1,8 @@
+# $NetBSD: varmod-to-abs.mk,v 1.1 2020/08/16 12:07:51 rillig Exp $
+#
+# TODO: Description
+
+# TODO: Implementation
+
+all:
+	@:;
