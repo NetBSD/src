d761ba2df61b634352b4023dd19fad9adaac40e3
rillig <rillig@NetBSD.org>
2020-08-16 20:03:52 +0000

    make(1): move tests for the :M modifier into separate files
    The test for the different escaping has been adjusted to actually show
    the different parsing results in the test output.  To do this, it had to
    get its own file since it needs the -dv debug flag and specialized
    post-processing.

--- usr.bin/make/unit-tests/varmod-match.mk
+++ usr.bin/make/unit-tests/varmod-match.mk
@@ -1,9 +1,22 @@
-# $NetBSD: varmod-match.mk,v 1.2 2020/08/16 14:25:16 rillig Exp $
+# $NetBSD: varmod-match.mk,v 1.3 2020/08/16 20:03:53 rillig Exp $
 #
 # Tests for the :M variable modifier, which filters words that match the
 # given pattern.
 
-# TODO: Implementation
+all: match-char-class
+all: slow
 
-all:
-	@:;
+
+NUMBERS=	One Two Three Four five six seven
+
+match-char-class:
+	@echo '$@:'
+	@echo '  uppercase numbers: ${NUMBERS:M[A-Z]*}'
+	@echo '  all the others: ${NUMBERS:M[^A-Z]*}'
+	@echo '  starts with non-s, ends with [ex]: ${NUMBERS:M[^s]*[ex]}'
+
+
+# Before 2020-06-13, this expression took quite a long time in Str_Match,
+# calling itself 601080390 times for 16 asterisks.
+slow:
+	@: ${:U****************:M****************b}
