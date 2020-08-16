96c2cc6cd245dec4d7deaf13b2b7d7beea2a0169
rillig <rillig@NetBSD.org>
2020-08-16 20:43:01 +0000

    make(1): move tests for the :Ox modifier into separate file
    The test has been extended by ensuring that the shuffled words are still
    the same.  Comparing two shuffled lists is probabilistic, but comparing
    their sorted results is not, therefore that's completely sensible to do.
    
    When writing this test, by coincidence I discovered how to generate the
    "Undefined variable" error message.  Unfortunately, the error message is
    wrong since the variable NUMBERS is defined at that point.  In summary,
    that error message is shown when it shouldn't, and when it should it is
    not shown.  Still, I'm glad that I finally found it.

--- usr.bin/make/unit-tests/varmod-order.mk
+++ usr.bin/make/unit-tests/varmod-order.mk
@@ -1,4 +1,4 @@
-# $NetBSD: varmod-order.mk,v 1.3 2020/08/16 20:13:10 rillig Exp $
+# $NetBSD: varmod-order.mk,v 1.4 2020/08/16 20:43:01 rillig Exp $
 #
 # Tests for the :O variable modifier, which returns the words, sorted in
 # ascending order.
@@ -8,6 +8,12 @@ NUMBERS=	one two three four five six seven eight nine ten
 .if ${NUMBERS:O} != "eight five four nine one seven six ten three two"
 .error ${NUMBERS:O}
 .endif
+
+# Unknown modifier "OX"
+_:=	${NUMBERS:OX}
+
+# Unknown modifier "OxXX"
+_:=	${NUMBERS:OxXX}
 
 all:
 	@:;
