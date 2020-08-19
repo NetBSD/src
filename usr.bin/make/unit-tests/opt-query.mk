e2eaadf3b7655a16f55a4f93fb85588b85a4e065
rillig <rillig@NetBSD.org>
2020-08-19 05:13:18 +0000

    make(1): add test for the -q option

--- usr.bin/make/unit-tests/opt-query.mk
+++ usr.bin/make/unit-tests/opt-query.mk
@@ -1,8 +1,24 @@
-# $NetBSD: opt-query.mk,v 1.2 2020/08/16 14:25:16 rillig Exp $
+# $NetBSD: opt-query.mk,v 1.3 2020/08/19 05:13:18 rillig Exp $
 #
 # Tests for the -q command line option.
+#
+# The -q option only looks at the dependencies between the targets.
+# None of the commands in the targets are run, not even those that are
+# prefixed with '+'.
 
-# TODO: Implementation
+# This command cannot be prevented from being run since it is used at parse
+# time, and any later variable assignments may depend on its result.
+!=	echo 'command during parsing' 1>&2; echo
 
+# None of these commands are run.
+.BEGIN:
+	@echo '$@: hidden command'
+	@+echo '$@: run always'
+
+# None of these commands are run.
 all:
-	@:;
+	@echo '$@: hidden command'
+	@+echo '$@: run always'
+
+# The exit status 1 is because the "all" target has to be made, that is,
+# it is not up-to-date.
