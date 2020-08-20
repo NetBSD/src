2da00e76ce7ad0ee06a77c33c193b3ea41d98bb3
riastradh <riastradh@NetBSD.org>
2020-08-20 21:30:56 +0000

    Use cprng_strong, not cprng_fast, for ephemeral key.

--- sys/external/isc/libsodium/include/randombytes.h
+++ sys/external/isc/libsodium/include/randombytes.h
@@ -1,11 +1,12 @@
 /* This overwrites dist/src/libsodium/include/sodium/randombytes.h */
 
+#include <sys/cprng.h>
+
 static inline void
 randombytes_buf(void * const buf, const size_t size)
 {
 
-	extern size_t cprng_fast(void *, size_t);
-	cprng_fast(buf, size);
+	cprng_strong(kern_cprng, buf, size, 0);
 }
 
 static inline void
