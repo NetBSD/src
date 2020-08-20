569512438e0b7acb9be37d84769ce527d190c3ff
riastradh <riastradh@NetBSD.org>
2020-08-20 21:20:37 +0000

    Reuse temporaries in ge25519_scalarmult to reduce stack usage.

--- sys/external/isc/libsodium/dist/src/libsodium/crypto_core/ed25519/ref10/ed25519_ref10.c
+++ sys/external/isc/libsodium/dist/src/libsodium/crypto_core/ed25519/ref10/ed25519_ref10.c
@@ -755,41 +755,41 @@ ge25519_scalarmult(ge25519_p3 *h, const unsigned char *a, const ge25519_p3 *p)
     signed char     carry;
     ge25519_p1p1    r;
     ge25519_p2      s;
-    ge25519_p1p1    t2, t3, t4, t5, t6, t7, t8;
-    ge25519_p3      p2, p3, p4, p5, p6, p7, p8;
+    ge25519_p1p1    t1;
+    ge25519_p3      p2, p3, p4, pt;
     ge25519_cached  pi[8];
     ge25519_cached  t;
     int             i;
 
     ge25519_p3_to_cached(&pi[1 - 1], p);   /* p */
 
-    ge25519_p3_dbl(&t2, p);
-    ge25519_p1p1_to_p3(&p2, &t2);
+    ge25519_p3_dbl(&t1, p);
+    ge25519_p1p1_to_p3(&p2, &t1);
     ge25519_p3_to_cached(&pi[2 - 1], &p2); /* 2p = 2*p */
 
-    ge25519_add(&t3, p, &pi[2 - 1]);
-    ge25519_p1p1_to_p3(&p3, &t3);
+    ge25519_add(&t1, p, &pi[2 - 1]);
+    ge25519_p1p1_to_p3(&p3, &t1);
     ge25519_p3_to_cached(&pi[3 - 1], &p3); /* 3p = 2p+p */
 
-    ge25519_p3_dbl(&t4, &p2);
-    ge25519_p1p1_to_p3(&p4, &t4);
+    ge25519_p3_dbl(&t1, &p2);
+    ge25519_p1p1_to_p3(&p4, &t1);
     ge25519_p3_to_cached(&pi[4 - 1], &p4); /* 4p = 2*2p */
 
-    ge25519_add(&t5, p, &pi[4 - 1]);
-    ge25519_p1p1_to_p3(&p5, &t5);
-    ge25519_p3_to_cached(&pi[5 - 1], &p5); /* 5p = 4p+p */
+    ge25519_add(&t1, p, &pi[4 - 1]);
+    ge25519_p1p1_to_p3(&pt, &t1);
+    ge25519_p3_to_cached(&pi[5 - 1], &pt); /* 5p = 4p+p */
 
-    ge25519_p3_dbl(&t6, &p3);
-    ge25519_p1p1_to_p3(&p6, &t6);
-    ge25519_p3_to_cached(&pi[6 - 1], &p6); /* 6p = 2*3p */
+    ge25519_p3_dbl(&t1, &p3);
+    ge25519_p1p1_to_p3(&pt, &t1);
+    ge25519_p3_to_cached(&pi[6 - 1], &pt); /* 6p = 2*3p */
 
-    ge25519_add(&t7, p, &pi[6 - 1]);
-    ge25519_p1p1_to_p3(&p7, &t7);
-    ge25519_p3_to_cached(&pi[7 - 1], &p7); /* 7p = 6p+p */
+    ge25519_add(&t1, p, &pi[6 - 1]);
+    ge25519_p1p1_to_p3(&pt, &t1);
+    ge25519_p3_to_cached(&pi[7 - 1], &pt); /* 7p = 6p+p */
 
-    ge25519_p3_dbl(&t8, &p4);
-    ge25519_p1p1_to_p3(&p8, &t8);
-    ge25519_p3_to_cached(&pi[8 - 1], &p8); /* 8p = 2*4p */
+    ge25519_p3_dbl(&t1, &p4);
+    ge25519_p1p1_to_p3(&pt, &t1);
+    ge25519_p3_to_cached(&pi[8 - 1], &pt); /* 8p = 2*4p */
 
     for (i = 0; i < 32; ++i) {
         e[2 * i + 0] = (a[i] >> 0) & 15;
