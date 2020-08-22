d4fba315db1e94f8c2c4e1b8ec9a65a486c07b98
rillig <rillig@NetBSD.org>
2020-08-22 12:30:57 +0000

    make(1): add test for correct command order in .USE sources

--- usr.bin/make/unit-tests/depsrc-use.mk
+++ usr.bin/make/unit-tests/depsrc-use.mk
@@ -1,4 +1,4 @@
-# $NetBSD: depsrc-use.mk,v 1.3 2020/08/22 07:49:44 rillig Exp $
+# $NetBSD: depsrc-use.mk,v 1.4 2020/08/22 12:30:57 rillig Exp $
 #
 # Tests for the special source .USE in dependency declarations,
 # which allows to append common commands to other targets.
@@ -6,10 +6,12 @@
 all: action directly
 
 first: .USE
-	@echo first		# Using ${.TARGET} here would expand to "action"
+	@echo first 1		# Using ${.TARGET} here would expand to "action"
+	@echo first 2
 
 second: .USE
-	@echo second
+	@echo second 1
+	@echo second 2
 
 # It's possible but uncommon to have a .USE target with no commands.
 # This may happen as the result of expanding a .for loop.
