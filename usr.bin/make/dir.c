df4f3f9e187753e7bf9588d0e74f9ff59ad781cd
rillig <rillig@NetBSD.org>
2020-08-22 19:57:43 +0000

    make(1): use Lst_OpenS in Dir_SetPATH
    Since dirSearchPath is initialized in Dir_Init, opening the list can
    never fail.

--- usr.bin/make/dir.c
+++ usr.bin/make/dir.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: dir.c,v 1.106 2020/08/22 17:34:25 rillig Exp $	*/
+/*	$NetBSD: dir.c,v 1.107 2020/08/22 19:57:43 rillig Exp $	*/
 
 /*
  * Copyright (c) 1988, 1989, 1990 The Regents of the University of California.
@@ -70,14 +70,14 @@
  */
 
 #ifndef MAKE_NATIVE
-static char rcsid[] = "$NetBSD: dir.c,v 1.106 2020/08/22 17:34:25 rillig Exp $";
+static char rcsid[] = "$NetBSD: dir.c,v 1.107 2020/08/22 19:57:43 rillig Exp $";
 #else
 #include <sys/cdefs.h>
 #ifndef lint
 #if 0
 static char sccsid[] = "@(#)dir.c	8.2 (Berkeley) 1/2/94";
 #else
-__RCSID("$NetBSD: dir.c,v 1.106 2020/08/22 17:34:25 rillig Exp $");
+__RCSID("$NetBSD: dir.c,v 1.107 2020/08/22 19:57:43 rillig Exp $");
 #endif
 #endif /* not lint */
 #endif
@@ -478,43 +478,42 @@ Dir_SetPATH(void)
 {
     LstNode ln;			/* a list element */
     Path *p;
-    Boolean hasLastDot = FALSE;	/* true we should search dot last */
+    Boolean hasLastDot = FALSE;	/* true if we should search dot last */
 
     Var_Delete(".PATH", VAR_GLOBAL);
 
-    if (Lst_Open(dirSearchPath) == SUCCESS) {
-	if ((ln = Lst_First(dirSearchPath)) != NULL) {
-	    p = Lst_DatumS(ln);
-	    if (p == dotLast) {
-		hasLastDot = TRUE;
-		Var_Append(".PATH", dotLast->name, VAR_GLOBAL);
-	    }
+    Lst_OpenS(dirSearchPath);
+    if ((ln = Lst_First(dirSearchPath)) != NULL) {
+	p = Lst_DatumS(ln);
+	if (p == dotLast) {
+	    hasLastDot = TRUE;
+	    Var_Append(".PATH", dotLast->name, VAR_GLOBAL);
 	}
-
-	if (!hasLastDot) {
-	    if (dot)
-		Var_Append(".PATH", dot->name, VAR_GLOBAL);
-	    if (cur)
-		Var_Append(".PATH", cur->name, VAR_GLOBAL);
-	}
-
-	while ((ln = Lst_NextS(dirSearchPath)) != NULL) {
-	    p = Lst_DatumS(ln);
-	    if (p == dotLast)
-		continue;
-	    if (p == dot && hasLastDot)
-		continue;
-	    Var_Append(".PATH", p->name, VAR_GLOBAL);
-	}
-
-	if (hasLastDot) {
-	    if (dot)
-		Var_Append(".PATH", dot->name, VAR_GLOBAL);
-	    if (cur)
-		Var_Append(".PATH", cur->name, VAR_GLOBAL);
-	}
-	Lst_CloseS(dirSearchPath);
     }
+
+    if (!hasLastDot) {
+	if (dot)
+	    Var_Append(".PATH", dot->name, VAR_GLOBAL);
+	if (cur)
+	    Var_Append(".PATH", cur->name, VAR_GLOBAL);
+    }
+
+    while ((ln = Lst_NextS(dirSearchPath)) != NULL) {
+	p = Lst_DatumS(ln);
+	if (p == dotLast)
+	    continue;
+	if (p == dot && hasLastDot)
+	    continue;
+	Var_Append(".PATH", p->name, VAR_GLOBAL);
+    }
+
+    if (hasLastDot) {
+	if (dot)
+	    Var_Append(".PATH", dot->name, VAR_GLOBAL);
+	if (cur)
+	    Var_Append(".PATH", cur->name, VAR_GLOBAL);
+    }
+    Lst_CloseS(dirSearchPath);
 }
 
 /*-
