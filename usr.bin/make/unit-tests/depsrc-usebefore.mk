964247a312698ac13171fed39fa1eb6bceef00a5
rillig <rillig@NetBSD.org>
2020-08-22 07:46:18 +0000

    make(1): add test for the special source .USEBEFORE

--- usr.bin/make/unit-tests/depsrc-usebefore.mk
+++ usr.bin/make/unit-tests/depsrc-usebefore.mk
@@ -1,8 +1,20 @@
-# $NetBSD: depsrc-usebefore.mk,v 1.2 2020/08/16 14:25:16 rillig Exp $
+# $NetBSD: depsrc-usebefore.mk,v 1.3 2020/08/22 07:46:18 rillig Exp $
 #
-# Tests for the special source .USEBEFORE in dependency declarations.
+# Tests for the special source .USEBEFORE in dependency declarations,
+# which allows to prepend common commands to other targets.
 
-# TODO: Implementation
+all: action directly
 
-all:
-	@:;
+first: .USEBEFORE
+	@echo first		# Using ${.TARGET} here would expand to "action"
+
+second: .USEBEFORE
+	@echo second
+
+empty: .USEBEFORE
+
+# It's possible but uncommon to directly make a .USEBEFORE target.
+directly: .USEBEFORE
+	@echo directly
+
+action: second first empty
