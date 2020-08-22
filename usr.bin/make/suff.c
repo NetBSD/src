8274093b37eac6ba9d0b3d1714aa975856ba5fa3
rillig <rillig@NetBSD.org>
2020-08-22 22:00:50 +0000

    make(1): make Lst_Prev stricter regarding null pointers

--- usr.bin/make/suff.c
+++ usr.bin/make/suff.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: suff.c,v 1.109 2020/08/22 21:42:38 rillig Exp $	*/
+/*	$NetBSD: suff.c,v 1.110 2020/08/22 22:00:50 rillig Exp $	*/
 
 /*
  * Copyright (c) 1988, 1989, 1990, 1993
@@ -69,14 +69,14 @@
  */
 
 #ifndef MAKE_NATIVE
-static char rcsid[] = "$NetBSD: suff.c,v 1.109 2020/08/22 21:42:38 rillig Exp $";
+static char rcsid[] = "$NetBSD: suff.c,v 1.110 2020/08/22 22:00:50 rillig Exp $";
 #else
 #include <sys/cdefs.h>
 #ifndef lint
 #if 0
 static char sccsid[] = "@(#)suff.c	8.4 (Berkeley) 3/21/94";
 #else
-__RCSID("$NetBSD: suff.c,v 1.109 2020/08/22 21:42:38 rillig Exp $");
+__RCSID("$NetBSD: suff.c,v 1.110 2020/08/22 22:00:50 rillig Exp $");
 #endif
 #endif /* not lint */
 #endif
@@ -1649,7 +1649,7 @@ SuffExpandChildren(LstNode cln, GNode *pgn)
 	    Lst_AppendS(gn->parents, pgn);
 	    pgn->unmade++;
 	    /* Expand wildcards on new node */
-	    SuffExpandWildcards(Lst_Prev(cln), pgn);
+	    SuffExpandWildcards(Lst_PrevS(cln), pgn);
 	}
 	Lst_Destroy(members, NULL);
 
