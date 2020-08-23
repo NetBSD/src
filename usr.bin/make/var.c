4b050f635bdb4c06eea8c8dc5968ed50f15115ec
rillig <rillig@NetBSD.org>
2020-08-23 22:49:45 +0000

    make(1): remove redundant range restriction
    re.re_nsub is at least 0, and (at least 0) + 1 cannot be 0.

--- usr.bin/make/var.c
+++ usr.bin/make/var.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: var.c,v 1.468 2020/08/23 22:13:38 rillig Exp $	*/
+/*	$NetBSD: var.c,v 1.469 2020/08/23 22:49:45 rillig Exp $	*/
 
 /*
  * Copyright (c) 1988, 1989, 1990, 1993
@@ -69,14 +69,14 @@
  */
 
 #ifndef MAKE_NATIVE
-static char rcsid[] = "$NetBSD: var.c,v 1.468 2020/08/23 22:13:38 rillig Exp $";
+static char rcsid[] = "$NetBSD: var.c,v 1.469 2020/08/23 22:49:45 rillig Exp $";
 #else
 #include <sys/cdefs.h>
 #ifndef lint
 #if 0
 static char sccsid[] = "@(#)var.c	8.3 (Berkeley) 3/19/94";
 #else
-__RCSID("$NetBSD: var.c,v 1.468 2020/08/23 22:13:38 rillig Exp $");
+__RCSID("$NetBSD: var.c,v 1.469 2020/08/23 22:49:45 rillig Exp $");
 #endif
 #endif /* not lint */
 #endif
@@ -2473,8 +2473,6 @@ ApplyModifier_Regex(const char **pp, ApplyModifiersState *st)
     }
 
     args.nsub = args.re.re_nsub + 1;
-    if (args.nsub < 1)
-	args.nsub = 1;
     if (args.nsub > 10)
 	args.nsub = 10;
     st->newVal = ModifyWords(st->ctxt, st->sep, oneBigWord, st->val,
