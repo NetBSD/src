33c205ea770dbff2ab57d77c46a192d4efa77cb0
rillig <rillig@NetBSD.org>
2020-08-23 14:28:04 +0000

    make(1): add tests for the -i -k -v -V -W options

--- usr.bin/make/unit-tests/opt-warnings-as-errors.mk
+++ usr.bin/make/unit-tests/opt-warnings-as-errors.mk
@@ -1,8 +1,11 @@
-# $NetBSD: opt-warnings-as-errors.mk,v 1.2 2020/08/16 14:25:16 rillig Exp $
+# $NetBSD: opt-warnings-as-errors.mk,v 1.3 2020/08/23 14:28:04 rillig Exp $
 #
-# Tests for the -W command line option.
+# Tests for the -W command line option, which turns warnings into errors.
 
-# TODO: Implementation
+.warning message 1
+.warning message 2
+
+_!=	echo 'parsing continues' 1>&2
 
 all:
 	@:;
