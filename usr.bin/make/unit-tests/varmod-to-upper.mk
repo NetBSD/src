24bce02441cba1a38aa4ab52186552600dc9dd95
rillig <rillig@NetBSD.org>
2020-08-28 17:21:02 +0000

    make(1): add tests for :tl and :tu modifiers

--- usr.bin/make/unit-tests/varmod-to-upper.mk
+++ usr.bin/make/unit-tests/varmod-to-upper.mk
@@ -1,7 +1,19 @@
-# $NetBSD: varmod-to-upper.mk,v 1.3 2020/08/23 15:18:43 rillig Exp $
+# $NetBSD: varmod-to-upper.mk,v 1.4 2020/08/28 17:21:02 rillig Exp $
 #
 # Tests for the :tu variable modifier, which returns the words in the
 # variable value, converted to uppercase.
+
+.if ${:UUPPER:tu} != "UPPER"
+.error
+.endif
+
+.if ${:Ulower:tu} != "LOWER"
+.error
+.endif
+
+.if ${:UMixeD case.:tu} != "MIXED CASE."
+.error
+.endif
 
 # The :tu and :tl modifiers operate on the variable value as a single string,
 # not as a list of words. Therefore, the adjacent spaces are preserved.
