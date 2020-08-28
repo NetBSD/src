5cdd36a0e7c8043a5b4e7030e8455020fc15a568
rillig <rillig@NetBSD.org>
2020-08-28 04:05:35 +0000

    make(1): add test for the undocumented .NULL special dependency target

--- usr.bin/make/unit-tests/deptgt-suffixes.mk
+++ usr.bin/make/unit-tests/deptgt-suffixes.mk
@@ -1,8 +1,18 @@
-# $NetBSD: deptgt-suffixes.mk,v 1.2 2020/08/16 14:25:16 rillig Exp $
+# $NetBSD: deptgt-suffixes.mk,v 1.3 2020/08/28 04:05:35 rillig Exp $
 #
 # Tests for the special target .SUFFIXES in dependency declarations.
+#
+# See also:
+#	varname-dot-includes.mk
+#	varname-dot-libs.mk
 
-# TODO: Implementation
+.MAKEFLAGS: -dg1
+
+.SUFFIXES: .custom-null
+
+# TODO: What is the effect of this? How is it useful?
+.NULL: .custom-null
+.PATH.custom-null: . ..
 
 all:
 	@:;
