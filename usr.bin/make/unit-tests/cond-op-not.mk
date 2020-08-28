e1e75616317c5248776e529c702e590012efa97b
rillig <rillig@NetBSD.org>
2020-08-28 14:48:37 +0000

    make(1): add tests for the &&, || and ! operators

--- usr.bin/make/unit-tests/cond-op-not.mk
+++ usr.bin/make/unit-tests/cond-op-not.mk
@@ -1,8 +1,22 @@
-# $NetBSD: cond-op-not.mk,v 1.2 2020/08/16 14:25:16 rillig Exp $
+# $NetBSD: cond-op-not.mk,v 1.3 2020/08/28 14:48:37 rillig Exp $
 #
 # Tests for the ! operator in .if conditions.
 
-# TODO: Implementation
+# The exclamation mark negates its operand.
+.if !1
+.error
+.endif
+
+# Exclamation marks can be chained.
+# This doesn't happen in practice though.
+.if !!!1
+.error
+.endif
+
+# The ! binds more tightly than the &&.
+.if !!0 && 1
+.error
+.endif
 
 all:
 	@:;
