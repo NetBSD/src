a5ad5ded72256f6f8c92fe62531d55b54668c52b
rillig <rillig@NetBSD.org>
2020-08-31 17:41:38 +0000

    make(1): fix the :u modifier, which was broken for almost a day
    Big thanks go to sjg, who discovered the bug and did the main work to
    track it down.
    
    In the unit tests for the :u modifier from the previous commit, I had
    forgotten to actually add the :u modifier at the end.  I added it now
    and also added a few other tests.  It's better to have a few more tests
    than too few.

--- usr.bin/make/unit-tests/varmod-unique.mk
+++ usr.bin/make/unit-tests/varmod-unique.mk
@@ -1,39 +1,38 @@
-# $NetBSD: varmod-unique.mk,v 1.3 2020/08/31 17:32:13 rillig Exp $
+# $NetBSD: varmod-unique.mk,v 1.4 2020/08/31 17:41:38 rillig Exp $
 #
 # Tests for the :u variable modifier, which discards adjacent duplicate
 # words.
 
 .if ${:U1 2 1:u} != "1 2 1"
-.  error The :u modifier only merges _adjacent_ duplicate words.
+.  warning The :u modifier only merges _adjacent_ duplicate words.
 .endif
 
 .if ${:U1 2 2 3:u} != "1 2 3"
 .  warning The :u modifier must merge adjacent duplicate words.
-.  if ${:U1 2 2 3:u} == "1 2 3 3" # not the desired result
-.    warning FIXME
-.  endif
 .endif
 
 .if ${:U:u} != ""
-.  error The :u modifier must do nothing with an empty word list.
+.  warning The :u modifier must do nothing with an empty word list.
 .endif
 
 .if ${:U1:u} != "1"
-.  error The :u modifier must do nothing with a single-element word list.
+.  warning The :u modifier must do nothing with a single-element word list.
 .endif
 
-.if ${:U1 1 1 1 1 1 1 1} != "1"
+.if ${:U1 1 1 1 1 1 1 1:u} != "1"
 .  warning The :u modifier must merge _all_ adjacent duplicate words.
-.  if ${:U1 1 1 1 1 1 1 1} == "1 1 1 1 1 1 1 1"
-.    warning FIXME
-.  endif
 .endif
 
-.if ${:U   1    2    1 1  } != "1 2 1"
+.if ${:U   1    2    1 1  :u} != "1 2 1"
 .  warning The :u modifier must normalize whitespace between the words.
-.  if ${:U   1    2    1 1  } != "1 2 1 1"
-.    warning FIXME
-.  endif
+.endif
+
+.if ${:U1 1 1 1 2:u} != "1 2"
+.  warning Duplicate words at the beginning must be merged.
+.endif
+
+.if ${:U1 2 2 2 2:u} != "1 2"
+.  warning Duplicate words at the end must be merged.
 .endif
 
 all:
