bdbd23143a2d7bbdc1aa9f7f3d29ccd41d8a89c4
rillig <rillig@NetBSD.org>
2020-09-01 19:17:58 +0000

    make(1): add test for -m option with special argument .../

--- usr.bin/make/unit-tests/opt-m-include-dir.mk
+++ usr.bin/make/unit-tests/opt-m-include-dir.mk
@@ -1,8 +1,25 @@
-# $NetBSD: opt-m-include-dir.mk,v 1.2 2020/08/16 14:25:16 rillig Exp $
+# $NetBSD: opt-m-include-dir.mk,v 1.3 2020/09/01 19:17:58 rillig Exp $
 #
 # Tests for the -m command line option.
 
-# TODO: Implementation
+.MAKEFLAGS: -m .../buf.c
+.MAKEFLAGS: -m .../does-not-exist
+.MAKEFLAGS: -m .../${.PARSEFILE:T}
+
+# Whether or not buf.c exists depends on whether the source code of make
+# is available.  When running the tests in src/usr.bin/make, it succeeds,
+# and when running the tests in src/tests/usr.bin/make, it fails.
+
+# This file should never exist.
+.if exists(does-not-exist)
+.  error
+.endif
+
+# This test assumes that this test is run in the same directory as the
+# test file.
+.if !exists(${.PARSEFILE})
+.  error
+.endif
 
 all:
 	@:;
