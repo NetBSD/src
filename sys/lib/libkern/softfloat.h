2adc30b773c21083b83d00cc98687dda7cca1cb5
thorpej <thorpej@NetBSD.org>
2020-09-02 03:41:56 +0000

    Update to the SoftFloat-2c version of softfloat-specialize.h and softfloat.h. This includes a simplification of the license and updates the comments.  NFC.

--- sys/lib/libkern/softfloat.h
+++ sys/lib/libkern/softfloat.h
@@ -1,4 +1,4 @@
-/* $NetBSD: softfloat.h,v 1.5 2017/12/31 11:43:42 martin Exp $ */
+/* $NetBSD: softfloat.h,v 1.6 2020/09/02 03:41:56 thorpej Exp $ */
 
 /* This is a derivative work. */
 
@@ -31,35 +31,29 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
-/*
-===============================================================================
+/*============================================================================
 
-This C header file is part of the SoftFloat IEC/IEEE Floating-point
-Arithmetic Package, Release 2a.
+This C header file template is part of the Berkeley SoftFloat IEEE Floating-
+Point Arithmetic Package, Release 2c, by John R. Hauser.
 
-Written by John R. Hauser.  This work was made possible in part by the
-International Computer Science Institute, located at Suite 600, 1947 Center
-Street, Berkeley, California 94704.  Funding was partially provided by the
-National Science Foundation under grant MIP-9311980.  The original version
-of this code was written as part of a project to build a fixed-point vector
-processor in collaboration with the University of California at Berkeley,
-overseen by Profs. Nelson Morgan and John Wawrzynek.  More information
-is available through the Web page `http://HTTP.CS.Berkeley.EDU/~jhauser/
-arithmetic/SoftFloat.html'.
+THIS SOFTWARE IS DISTRIBUTED AS IS, FOR FREE.  Although reasonable effort has
+been made to avoid it, THIS SOFTWARE MAY CONTAIN FAULTS THAT WILL AT TIMES
+RESULT IN INCORRECT BEHAVIOR.  USE OF THIS SOFTWARE IS RESTRICTED TO PERSONS
+AND ORGANIZATIONS WHO CAN AND WILL TOLERATE ALL LOSSES, COSTS, OR OTHER
+PROBLEMS THEY INCUR DUE TO THE SOFTWARE WITHOUT RECOMPENSE FROM JOHN HAUSER OR
+THE INTERNATIONAL COMPUTER SCIENCE INSTITUTE, AND WHO FURTHERMORE EFFECTIVELY
+INDEMNIFY JOHN HAUSER AND THE INTERNATIONAL COMPUTER SCIENCE INSTITUTE
+(possibly via similar legal notice) AGAINST ALL LOSSES, COSTS, OR OTHER
+PROBLEMS INCURRED BY THEIR CUSTOMERS AND CLIENTS DUE TO THE SOFTWARE, OR
+INCURRED BY ANYONE DUE TO A DERIVATIVE WORK THEY CREATE USING ANY PART OF THE
+SOFTWARE.
 
-THIS SOFTWARE IS DISTRIBUTED AS IS, FOR FREE.  Although reasonable effort
-has been made to avoid it, THIS SOFTWARE MAY CONTAIN FAULTS THAT WILL AT
-TIMES RESULT IN INCORRECT BEHAVIOR.  USE OF THIS SOFTWARE IS RESTRICTED TO
-PERSONS AND ORGANIZATIONS WHO CAN AND WILL TAKE FULL RESPONSIBILITY FOR ANY
-AND ALL LOSSES, COSTS, OR OTHER PROBLEMS ARISING FROM ITS USE.
+Derivative works require also that (1) the source code for the derivative work
+includes prominent notice that the work is derivative, and (2) the source code
+includes prominent notice of these three paragraphs for those parts of this
+code that are retained.
 
-Derivative works are acceptable, even for commercial purposes, so long as
-(1) they include prominent notice that the work is derivative, and (2) they
-include prominent notice akin to these four paragraphs for those parts of
-this code that are retained.
-
-===============================================================================
-*/
+=============================================================================*/
 
 #include <sys/types.h>
 
@@ -72,23 +66,19 @@ this code that are retained.
 #endif
 #include <sys/endian.h>
 
-/*
--------------------------------------------------------------------------------
-The macro `FLOATX80' must be defined to enable the extended double-precision
-floating-point format `floatx80'.  If this macro is not defined, the
-`floatx80' type will not be defined, and none of the functions that either
-input or output the `floatx80' type will be defined.  The same applies to
-the `FLOAT128' macro and the quadruple-precision format `float128'.
--------------------------------------------------------------------------------
-*/
+/*----------------------------------------------------------------------------
+| The macro `FLOATX80' must be defined to enable the double-extended-precision
+| floating-point format `floatx80'.  If this macro is not defined, the
+| `floatx80' type will not be defined, and none of the functions that either
+| input or output the `floatx80' type will be defined.  The same applies to
+| the `FLOAT128' macro and the quadruple-precision format `float128'.
+*----------------------------------------------------------------------------*/
 /* #define FLOATX80 */
 /* #define FLOAT128 */
 
-/*
--------------------------------------------------------------------------------
-Software IEC/IEEE floating-point types.
--------------------------------------------------------------------------------
-*/
+/*----------------------------------------------------------------------------
+| Software IEEE floating-point types.
+*----------------------------------------------------------------------------*/
 typedef u_int32_t float32;
 typedef u_int64_t float64;
 #ifdef FLOATX80
@@ -116,24 +106,18 @@ typedef struct {
 
 #define float_rounding_mode() fpgetround()
 
-/*
--------------------------------------------------------------------------------
-Software IEC/IEEE floating-point underflow tininess-detection mode.
--------------------------------------------------------------------------------
-*/
-
+/*----------------------------------------------------------------------------
+| Software IEEE floating-point underflow tininess-detection mode.
+*----------------------------------------------------------------------------*/
 extern int float_detect_tininess;
 enum {
     float_tininess_after_rounding  = 1,
     float_tininess_before_rounding = 0
 };
 
-/*
--------------------------------------------------------------------------------
-Software IEC/IEEE floating-point rounding mode.
--------------------------------------------------------------------------------
-*/
-
+/*----------------------------------------------------------------------------
+| Software IEEE floating-point rounding mode.
+*----------------------------------------------------------------------------*/
 enum {
     float_round_nearest_even = FP_RN,
     float_round_to_zero      = FP_RZ,
@@ -141,12 +125,9 @@ enum {
     float_round_up           = FP_RP
 };
 
-/*
--------------------------------------------------------------------------------
-Software IEC/IEEE floating-point exception flags.
--------------------------------------------------------------------------------
-*/
-
+/*----------------------------------------------------------------------------
+| Software IEEE floating-point exception flags.
+*----------------------------------------------------------------------------*/
 enum {
     float_flag_inexact   =  FP_X_IMP,
     float_flag_underflow =  FP_X_UFL,
@@ -155,11 +136,21 @@ enum {
     float_flag_invalid   =  FP_X_INV
 };
 
+/*----------------------------------------------------------------------------
+| Routine to raise any or all of the software IEEE floating-point exception
+| flags.
+*----------------------------------------------------------------------------*/
 /*
--------------------------------------------------------------------------------
-Software IEC/IEEE integer-to-floating-point conversion routines.
--------------------------------------------------------------------------------
-*/
+ * Routines provided by <machine/ieeefp.h>:
+ *
+ * float_raise()
+ * float_set_inexact()
+ * float_set_invalid()
+ */
+
+/*----------------------------------------------------------------------------
+| Software IEEE integer-to-floating-point conversion routines.
+*----------------------------------------------------------------------------*/
 float32 int32_to_float32( int );
 float64 int32_to_float64( int );
 #ifdef FLOATX80
@@ -179,11 +170,9 @@ float128 int64_to_float128( int64_t );
 #endif
 #endif
 
-/*
--------------------------------------------------------------------------------
-Software IEC/IEEE single-precision conversion routines.
--------------------------------------------------------------------------------
-*/
+/*----------------------------------------------------------------------------
+| Software IEEE single-precision conversion routines.
+*----------------------------------------------------------------------------*/
 int float32_to_int32( float32 );
 int float32_to_int32_round_to_zero( float32 );
 #ifndef SOFTFLOAT_FOR_GCC /* __fix?fdi provided by libgcc2.c */
@@ -198,11 +187,9 @@ floatx80 float32_to_floatx80( float32 );
 float128 float32_to_float128( float32 );
 #endif
 
-/*
--------------------------------------------------------------------------------
-Software IEC/IEEE single-precision operations.
--------------------------------------------------------------------------------
-*/
+/*----------------------------------------------------------------------------
+| Software IEEE single-precision operations.
+*----------------------------------------------------------------------------*/
 float32 float32_round_to_int( float32 );
 float32 float32_add( float32, float32 );
 float32 float32_sub( float32, float32 );
@@ -220,11 +207,9 @@ int float32_lt_quiet( float32, float32 );
 int float32_is_signaling_nan( float32 );
 #endif
 
-/*
--------------------------------------------------------------------------------
-Software IEC/IEEE double-precision conversion routines.
--------------------------------------------------------------------------------
-*/
+/*----------------------------------------------------------------------------
+| Software IEEE double-precision conversion routines.
+*----------------------------------------------------------------------------*/
 int float64_to_int32( float64 );
 int float64_to_int32_round_to_zero( float64 );
 #ifndef SOFTFLOAT_FOR_GCC /* __fix?fdi provided by libgcc2.c */
@@ -240,18 +225,30 @@ floatx80 float64_to_floatx80( float64 );
 float128 float64_to_float128( float64 );
 #endif
 
-/*
--------------------------------------------------------------------------------
-Software IEC/IEEE double-precision operations.
--------------------------------------------------------------------------------
-*/
+/*----------------------------------------------------------------------------
+| Software IEEE double-precision operations.
+*----------------------------------------------------------------------------*/
+
+/*----------------------------------------------------------------------------
+| The pattern for a default generated double-precision NaN.
+*----------------------------------------------------------------------------*/
 #define float64_default_nan 0xFFF8000000000000LL
+
+/*----------------------------------------------------------------------------
+| Returns 1 if the double-precision floating-point value `a' is a NaN;
+| otherwise returns 0.
+*----------------------------------------------------------------------------*/
 
 static __inline int
 float64_is_nan(float64 a)
 {
 	return 0xFFE0000000000000LL < a << 1;
 }
+
+/*----------------------------------------------------------------------------
+| Returns 1 if the double-precision floating-point value `a' is a signaling
+| NaN; otherwise returns 0.
+*----------------------------------------------------------------------------*/
 
 static __inline int
 float64_is_signaling_nan(float64 a)
@@ -278,11 +275,9 @@ int float64_is_signaling_nan( float64 );
 
 #ifdef FLOATX80
 
-/*
--------------------------------------------------------------------------------
-Software IEC/IEEE extended double-precision conversion routines.
--------------------------------------------------------------------------------
-*/
+/*----------------------------------------------------------------------------
+| Software IEEE double-extended-precision conversion routines.
+*----------------------------------------------------------------------------*/
 int floatx80_to_int32( floatx80 );
 int floatx80_to_int32_round_to_zero( floatx80 );
 int64_t floatx80_to_int64( floatx80 );
@@ -293,19 +288,15 @@ float64 floatx80_to_float64( floatx80 );
 float128 floatx80_to_float128( floatx80 );
 #endif
 
-/*
--------------------------------------------------------------------------------
-Software IEC/IEEE extended double-precision rounding precision.  Valid
-values are 32, 64, and 80.
--------------------------------------------------------------------------------
-*/
+/*----------------------------------------------------------------------------
+| Software IEEE double-extended-precision rounding precision.  Valid values
+| are 32, 64, and 80.
+*----------------------------------------------------------------------------*/
 extern int floatx80_rounding_precision;
 
-/*
--------------------------------------------------------------------------------
-Software IEC/IEEE extended double-precision operations.
--------------------------------------------------------------------------------
-*/
+/*----------------------------------------------------------------------------
+| Software IEEE double-extended-precision operations.
+*----------------------------------------------------------------------------*/
 floatx80 floatx80_round_to_int( floatx80 );
 floatx80 floatx80_add( floatx80, floatx80 );
 floatx80 floatx80_sub( floatx80, floatx80 );
@@ -325,11 +316,9 @@ int floatx80_is_signaling_nan( floatx80 );
 
 #ifdef FLOAT128
 
-/*
--------------------------------------------------------------------------------
-Software IEC/IEEE quadruple-precision conversion routines.
--------------------------------------------------------------------------------
-*/
+/*----------------------------------------------------------------------------
+| Software IEEE quadruple-precision conversion routines.
+*----------------------------------------------------------------------------*/
 int float128_to_int32( float128 );
 int float128_to_int32_round_to_zero( float128 );
 int64_t float128_to_int64( float128 );
@@ -340,11 +329,9 @@ float64 float128_to_float64( float128 );
 floatx80 float128_to_floatx80( float128 );
 #endif
 
-/*
--------------------------------------------------------------------------------
-Software IEC/IEEE quadruple-precision operations.
--------------------------------------------------------------------------------
-*/
+/*----------------------------------------------------------------------------
+| Software IEEE quadruple-precision operations.
+*----------------------------------------------------------------------------*/
 float128 float128_round_to_int( float128 );
 float128 float128_add( float128, float128 );
 float128 float128_sub( float128, float128 );
