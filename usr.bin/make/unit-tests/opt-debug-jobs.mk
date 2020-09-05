0283a210962eda078ec6c7c914b75628f420272d
rillig <rillig@NetBSD.org>
2020-09-05 06:20:50 +0000

    make(1): add tests for each debug option

---
+++ usr.bin/make/unit-tests/opt-debug-jobs.mk
@@ -0,0 +1,9 @@
+# $NetBSD: opt-debug-jobs.mk,v 1.1 2020/09/05 06:20:51 rillig Exp $
+#
+# Tests for the -da command line option, which adds debug logging about
+# running jobs in multiple shells.
+
+# TODO: Implementation
+
+all:
+	@:;
