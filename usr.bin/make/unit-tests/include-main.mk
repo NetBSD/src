b8e87683c4c7b8604ed8a6ef1a3bb72bad551217
rillig <rillig@NetBSD.org>
2020-09-05 18:13:47 +0000

    make(1): add test for .INCLUDEDFILE combined with .for loops
    The .for loops are implemented as a special kind of .include, therefore
    they affect the .INCLUDEDFROM variable.

--- usr.bin/make/unit-tests/include-main.mk
+++ usr.bin/make/unit-tests/include-main.mk
@@ -1,4 +1,4 @@
-# $NetBSD: include-main.mk,v 1.3 2020/09/05 16:59:19 rillig Exp $
+# $NetBSD: include-main.mk,v 1.4 2020/09/05 18:13:47 rillig Exp $
 #
 # Demonstrates that the .INCLUDEDFROMFILE magic variable does not behave
 # as described in the manual page.
@@ -14,6 +14,14 @@
 .  warning main-before-fail(${.INCLUDEDFROMFILE})
 .endif
 
+.for i in once
+.  if !defined(${.INCLUDEDFROMFILE})
+.    info main-before-for-ok
+.  else
+.    warning main-before-for-fail(${.INCLUDEDFROMFILE})
+.  endif
+.endfor
+
 .include "include-sub.mk"
 
 .if !defined(.INCLUDEDFROMFILE)
@@ -21,5 +29,13 @@
 .else
 .  warning main-after-fail(${.INCLUDEDFROMFILE})
 .endif
+
+.for i in once
+.  if !defined(${.INCLUDEDFROMFILE})
+.    info main-after-for-ok
+.  else
+.    warning main-after-for-fail(${.INCLUDEDFROMFILE})
+.  endif
+.endfor
 
 all:	# nothing
--- usr.bin/make/unit-tests/include-sub.mk
+++ usr.bin/make/unit-tests/include-sub.mk
@@ -1,10 +1,21 @@
-# $NetBSD: include-sub.mk,v 1.2 2020/09/05 16:59:19 rillig Exp $
+# $NetBSD: include-sub.mk,v 1.3 2020/09/05 18:13:47 rillig Exp $
 
 .if ${.INCLUDEDFROMFILE} == "include-main.mk"
 .  info sub-before-ok
 .else
-.  warning sub-before-fail
+.  warning sub-before-fail(${.INCLUDEDFROMFILE})
 .endif
+
+# As of 2020-09-05, the .for loop is implemented as "including a file"
+# with a custom buffer.  Therefore this loop has side effects on these
+# variables.
+.for i in once
+.  if ${.INCLUDEDFROMFILE} == "include-main.mk"
+.    info sub-before-for-ok
+.  else
+.    warning sub-before-for-fail(${.INCLUDEDFROMFILE})
+.  endif
+.endfor
 
 .include "include-subsub.mk"
 
@@ -13,3 +24,11 @@
 .else
 .  warning sub-after-fail(${.INCLUDEDFROMFILE})
 .endif
+
+.for i in once
+.  if ${.INCLUDEDFROMFILE} == "include-main.mk"
+.    info sub-after-for-ok
+.  else
+.    warning sub-after-for-fail(${.INCLUDEDFROMFILE})
+.  endif
+.endfor
