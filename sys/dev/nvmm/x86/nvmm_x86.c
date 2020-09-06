0b8d836aa775b211a3671eba99bd99e75f20e7fc
riastradh <riastradh@NetBSD.org>
2020-09-06 02:18:53 +0000

    Fix fallout from previous uvm.h cleanup.
    - pmap(9) needs uvm/uvm_extern.h.
    
    - x86/pmap.h is not usable on its own; it is only usable if included
      via uvm/uvm_extern.h (-> uvm/uvm_pmap.h -> machine/pmap.h).
    
    - Make nvmm.h and nvmm_internal.h standalone.

--- sys/dev/nvmm/x86/nvmm_x86.c
+++ sys/dev/nvmm/x86/nvmm_x86.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: nvmm_x86.c,v 1.19 2020/09/05 16:30:11 riastradh Exp $	*/
+/*	$NetBSD: nvmm_x86.c,v 1.20 2020/09/06 02:18:53 riastradh Exp $	*/
 
 /*
  * Copyright (c) 2018-2020 Maxime Villard, m00nbsd.net
@@ -29,16 +29,17 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: nvmm_x86.c,v 1.19 2020/09/05 16:30:11 riastradh Exp $");
+__KERNEL_RCSID(0, "$NetBSD: nvmm_x86.c,v 1.20 2020/09/06 02:18:53 riastradh Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
 #include <sys/kernel.h>
 #include <sys/cpu.h>
 
+#include <uvm/uvm_extern.h>
+
 #include <x86/cputypes.h>
 #include <x86/specialreg.h>
-#include <x86/pmap.h>
 
 #include <dev/nvmm/nvmm.h>
 #include <dev/nvmm/nvmm_internal.h>
