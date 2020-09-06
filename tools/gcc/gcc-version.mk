f82cdb28416685334a632c3a810983423b8a6f09
mrg <mrg@NetBSD.org>
2020-09-06 21:59:33 +0000

    mark GCC 9 as (so far) netbsd 20200907 version.  this should fix the compiler saying it is "gcc version 9.3.0 ()".
    (needs mknative redo.)

--- tools/gcc/gcc-version.mk
+++ tools/gcc/gcc-version.mk
@@ -1,9 +1,9 @@
-#	$NetBSD: gcc-version.mk,v 1.19 2020/08/11 09:51:57 mrg Exp $
+#	$NetBSD: gcc-version.mk,v 1.20 2020/09/06 21:59:33 mrg Exp $
 
 # common location for tools and native build
 
-.if ${HAVE_GCC} == 7
-NETBSD_GCC_VERSION=nb4 20200810
-.elif ${HAVE_GCC} == 8
+.if ${HAVE_GCC} == 8
 NETBSD_GCC_VERSION=nb1 20200311
+.elif ${HAVE_GCC} == 9
+NETBSD_GCC_VERSION=nb1 20200907
 .endif
