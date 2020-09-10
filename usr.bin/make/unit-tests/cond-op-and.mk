7d80e1181eb45df60dee99e9d1ef25bdbfd6c43a
rillig <rillig@NetBSD.org>
2020-09-10 22:38:57 +0000

    make(1): add test for the &, && and &&& operators

--- usr.bin/make/unit-tests/cond-op-and.mk
+++ usr.bin/make/unit-tests/cond-op-and.mk
@@ -1,4 +1,4 @@
-# $NetBSD: cond-op-and.mk,v 1.3 2020/08/28 14:48:37 rillig Exp $
+# $NetBSD: cond-op-and.mk,v 1.4 2020/09/10 22:38:57 rillig Exp $
 #
 # Tests for the && operator in .if conditions.
 
@@ -21,6 +21,27 @@
 # The right-hand side is not evaluated since the left-hand side is already
 # false.
 .if 0 && ${UNDEF}
+.endif
+
+# The && operator may be abbreviated as &.  This is not widely known though
+# and is also not documented in the manual page.
+
+.if 0 & 0
+.  error
+.endif
+.if 1 & 0
+.  error
+.endif
+.if 0 & 1
+.  error
+.endif
+.if !(1 & 1)
+.  error
+.endif
+
+# There is no operator &&&.
+.if 0 &&& 0
+.  error
 .endif
 
 all:
