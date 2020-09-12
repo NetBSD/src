227aa15eed7e81005bc9aef182ef2253c390c3a6
isaki <isaki@NetBSD.org>
2020-09-12 05:19:15 +0000

    Improve am7930 family drivers to share more code. audioamd(4) on sparc, vsaudio(4) on vax, and bba(4) are. - Remove complex and useless callbacks: onopen, onclose, and   indirect_{read,write}.  This makes audioamd and vsaudio almost the same. - Remove (already disabled) assembly fast interrupt path from audioamd(4).   cf. http://mail-index.netbsd.org/source-changes/2009/12/19/msg004585.html - Use trigger_* method rather than start_* method.  It's more suitable. vsaudio(4) was tested by naru@, bba(4) was tested by tsutsui@.

--- sys/dev/ic/am7930var.h
+++ sys/dev/ic/am7930var.h
@@ -1,14 +1,22 @@
-/*	$NetBSD: am7930var.h,v 1.14 2019/05/08 13:40:18 isaki Exp $	*/
+/*	$NetBSD: am7930var.h,v 1.15 2020/09/12 05:19:16 isaki Exp $	*/
 
 struct am7930_softc;
 
 struct am7930_glue {
-	uint8_t	(*codec_iread)(struct am7930_softc *sc, int);
-	void	(*codec_iwrite)(struct am7930_softc *sc, int, uint8_t);
-	uint16_t	(*codec_iread16)(struct am7930_softc *sc, int);
-	void	(*codec_iwrite16)(struct am7930_softc *sc, int, uint16_t);
-	void	(*onopen)(struct am7930_softc *sc);
-	void	(*onclose)(struct am7930_softc *sc);
+	uint8_t	(*codec_dread)(struct am7930_softc *sc, int);
+	void	(*codec_dwrite)(struct am7930_softc *sc, int, uint8_t);
+};
+
+struct am7930_buf {
+	uint8_t *start;
+	uint8_t *end;
+	uint8_t *data;
+	uint8_t *blkend;
+	uint blksize;
+
+	void (*intr)(void *);
+	void *arg;
+	uint intr_pending;
 };
 
 struct am7930_softc {
@@ -21,19 +29,25 @@ struct am7930_softc {
 	uint8_t	sc_mic_mute;
 
 	struct am7930_glue *sc_glue;
+	struct am7930_buf sc_p;	/* for play */
+	struct am7930_buf sc_r;	/* for rec */
 
 	kmutex_t sc_lock;
 	kmutex_t sc_intr_lock;
+	void *sc_sicookie;		/* softint(9) cookie */
+	struct evcnt sc_intrcnt;	/* statistics */
 };
 
 extern int     am7930debug;
 
 void	am7930_init(struct am7930_softc *, int);
+int	am7930_hwintr(void *);
+void	am7930_swintr(void *);
 
-#define AM7930_IWRITE(x,y,z)	(*(x)->sc_glue->codec_iwrite)((x),(y),(z))
-#define AM7930_IREAD(x,y)	(*(x)->sc_glue->codec_iread)((x),(y))
-#define AM7930_IWRITE16(x,y,z)	(*(x)->sc_glue->codec_iwrite16)((x),(y),(z))
-#define AM7930_IREAD16(x,y)	(*(x)->sc_glue->codec_iread16)((x),(y))
+/* direct access functions */
+#define AM7930_DWRITE(x,y,z)	(*(x)->sc_glue->codec_dwrite)((x),(y),(z))
+#define AM7930_DREAD(x,y)	(*(x)->sc_glue->codec_dread)((x),(y))
+
 
 #define AUDIOAMD_POLL_MODE	0
 #define AUDIOAMD_DMA_MODE	1
@@ -61,17 +75,15 @@ void	am7930_init(struct am7930_softc *, int);
  * audio(9) MI callbacks from upper-level audio layer.
  */
 
-struct audio_device;
-struct audio_encoding;
-struct audio_params;
-
-int	am7930_open(void *, int);
-void	am7930_close(void *);
 int	am7930_query_format(void *, audio_format_query_t *);
 int	am7930_set_format(void *, int,
 	    const audio_params_t *, const audio_params_t *,
 	    audio_filter_reg_t *, audio_filter_reg_t *);
 int	am7930_commit_settings(void *);
+int	am7930_trigger_output(void *, void *, void *, int, void (*)(void *),
+	    void *, const audio_params_t *);
+int	am7930_trigger_input(void *, void *, void *, int, void (*)(void *),
+	    void *, const audio_params_t *);
 int	am7930_halt_output(void *);
 int	am7930_halt_input(void *);
 int	am7930_getdev(void *, struct audio_device *);
@@ -79,3 +91,4 @@ int	am7930_get_props(void *);
 int	am7930_set_port(void *, mixer_ctrl_t *);
 int	am7930_get_port(void *, mixer_ctrl_t *);
 int	am7930_query_devinfo(void *, mixer_devinfo_t *);
+void	am7930_get_locks(void *, kmutex_t **, kmutex_t **);
