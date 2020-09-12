188e8ec9bdc6bac6c7c215719072ca2c3450c0ac
rillig <rillig@NetBSD.org>
2020-09-12 11:55:28 +0000

    make(1): add test for .PARSEFILE

--- usr.bin/make/unit-tests/varname-dot-parsefile.mk
+++ usr.bin/make/unit-tests/varname-dot-parsefile.mk
@@ -1,8 +1,35 @@
-# $NetBSD: varname-dot-parsefile.mk,v 1.2 2020/08/16 14:25:16 rillig Exp $
+# $NetBSD: varname-dot-parsefile.mk,v 1.3 2020/09/12 11:55:28 rillig Exp $
 #
-# Tests for the special .PARSEFILE variable.
+# Tests for the special .PARSEFILE variable, which contains the basename part
+# of the file that is currently parsed.
 
-# TODO: Implementation
+.if ${.PARSEFILE} != "varname-dot-parsefile.mk"
+.  error
+.endif
+
+# During parsing, it is possible to undefine .PARSEFILE.
+# Not that anyone would ever want to do this, but there's code in parse.c,
+# function PrintLocation, that explicitly handles this situation.
+.if !defined(.PARSEFILE)
+.  error
+.endif
+.undef .PARSEFILE
+.if defined(.PARSEFILE)
+.  error
+.endif
+
+# The variable .PARSEFILE is indirectly used by the .info directive,
+# via PrintLocation.
+.info At this point, .PARSEFILE is undefined.
+
+# There is absolutely no point in faking the location of the file that is
+# being parsed.  Technically, it's possible though.
+.PARSEFILE = fake-parsefile
+.info The location can be faked.
+
+# After including another file, .PARSEFILE is reset.
+.include "/dev/null"
+.info The location is no longer fake.
 
 all:
-	@:;
+	@echo At run time, .PARSEFILE is ${.PARSEFILE:Uundefined}.
