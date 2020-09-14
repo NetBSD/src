75e5886cf1a068ca6a2bdd3008432d8adb0f5c6f
rillig <rillig@NetBSD.org>
2020-09-14 18:21:26 +0000

    make(1): add test for the global variable 'targets' in parse.c

--- usr.bin/make/unit-tests/deptgt.mk
+++ usr.bin/make/unit-tests/deptgt.mk
@@ -1,4 +1,4 @@
-# $NetBSD: deptgt.mk,v 1.3 2020/09/14 17:43:36 rillig Exp $
+# $NetBSD: deptgt.mk,v 1.4 2020/09/14 18:21:26 rillig Exp $
 #
 # Tests for special targets like .BEGIN or .SUFFIXES in dependency
 # declarations.
@@ -8,6 +8,23 @@
 # Just in case anyone tries to compile several special targets in a single
 # dependency line: That doesn't work, and make immediately rejects it.
 .SUFFIXES .PHONY: .c.o
+
+# Keyword "parse.c:targets"
+#
+# The following lines demonstrate how 'target' is set and reset during
+# parsing of dependencies.  To see it in action, set breakpoints in:
+#
+#	ParseDoDependency	at the beginning
+#	ParseFinishLine		at "targets = NULL"
+#	Parse_File		at "Lst_Free(targets)"
+#	Parse_File		at "targets = Lst_Init()"
+#	Parse_File		at "!inLine"
+#
+target1 target2: sources
+	: command1
+	: command2
+VAR=value
+	: command3
 
 all:
 	@:;
