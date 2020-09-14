f199ad3e4aaf8e23d6b528b17cb8f8ba07a9a305
rillig <rillig@NetBSD.org>
2020-09-14 17:43:36 +0000

    make(1): add test for combining several special targets

--- usr.bin/make/unit-tests/deptgt.mk
+++ usr.bin/make/unit-tests/deptgt.mk
@@ -1,9 +1,13 @@
-# $NetBSD: deptgt.mk,v 1.2 2020/08/16 14:25:16 rillig Exp $
+# $NetBSD: deptgt.mk,v 1.3 2020/09/14 17:43:36 rillig Exp $
 #
 # Tests for special targets like .BEGIN or .SUFFIXES in dependency
 # declarations.
 
 # TODO: Implementation
+
+# Just in case anyone tries to compile several special targets in a single
+# dependency line: That doesn't work, and make immediately rejects it.
+.SUFFIXES .PHONY: .c.o
 
 all:
 	@:;
