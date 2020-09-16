f1a01651b487f70ddea8ca02469de122f0832df4
msaitoh <msaitoh@NetBSD.org>
2020-09-16 15:04:01 +0000

    Add a workaround for jumbo frame on PCH2 and newer. Tested by chs@.
    - Add wm_lv_jumbo_workaround_ich8lan() and use it. From FreeBSD.
    
      XXX For KUMCTRLSTA_OFFSET_HD_CTRL register modification, it's doubtful.
      FreeBSD and Linux do the same thing that they set the same value on both
      jumbo frame's enable case and the disable case. It seems the default value
      is 0x0b0c and it's not changed on the enable case, so it might be a bug
      on the enable case or the modification is not required.
    
    - Rename I219_UNKNOWN1 to I82579_UNKNOWN1.

--- sys/dev/pci/if_wmreg.h
+++ sys/dev/pci/if_wmreg.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: if_wmreg.h,v 1.119 2020/01/08 03:51:37 msaitoh Exp $	*/
+/*	$NetBSD: if_wmreg.h,v 1.120 2020/09/16 15:04:02 msaitoh Exp $	*/
 
 /*
  * Copyright (c) 2001 Wasabi Systems, Inc.
@@ -1379,7 +1379,10 @@ struct livengood_tcpip_ctxdesc {
 #define WMREG_FEXTNVM11	0x5bbc	/* Future Extended NVM 11 */
 #define FEXTNVM11_DIS_MULRFIX	__BIT(13)	/* Disable MULR fix */
 
+#define WMREG_FFLT_DBG	0x05F04 /* Debug Register */
+
 #define WMREG_CRC_OFFSET 0x5f50
+#define WMREG_PCH_RAICC(x)	(WMREG_CRC_OFFSET + (x) * 4)
 
 #define WMREG_EEC	0x12010
 #define EEC_FLASH_DETECTED (1U << 19)	/* FLASH */
