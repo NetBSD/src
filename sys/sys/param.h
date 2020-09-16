789e2ab871d75f3127e93f4bbefc985e526bfafc
roy <roy@NetBSD.org>
2020-09-16 13:42:05 +0000

    9.99.73 welcomes SIOCGNBRINFO

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.675 2020/08/28 07:29:12 ozaki-r Exp $	*/
+/*	$NetBSD: param.h,v 1.676 2020/09/16 13:42:05 roy Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -67,7 +67,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	999007200	/* NetBSD 9.99.72 */
+#define	__NetBSD_Version__	999007300	/* NetBSD 9.99.73 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
