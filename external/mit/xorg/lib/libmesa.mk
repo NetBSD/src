4ca4175de18d17b22fcb45ed2356ea8a99a7a41c
nia <nia@NetBSD.org>
2020-09-16 18:19:23 +0000

    disable use of ELF TLS in Mesa/libGL
    this is an optimization that primarily benefits linux/glibc -
    most other systems have this disabled. in netbsd we've tried to
    patch around it to make things work, but there still appears to be
    some edge cases where libGL mysteriously crashes.
    
    discussed on tech-x11 some time ago. already in place in pkgsrc.

--- external/mit/xorg/lib/libmesa.mk
+++ external/mit/xorg/lib/libmesa.mk
@@ -1,4 +1,4 @@
-#	$NetBSD: libmesa.mk,v 1.11 2020/02/23 02:03:19 joerg Exp $
+#	$NetBSD: libmesa.mk,v 1.12 2020/09/16 18:19:23 nia Exp $
 #
 # Consumer of this Makefile should set MESA_SRC_MODULES.
 
@@ -526,7 +526,6 @@ CPPFLAGS+=	\
 	-DHAVE_LIBDRM -DGLX_USE_DRM \
 	-DGLX_INDIRECT_RENDERING \
 	-DGLX_DIRECT_RENDERING \
-	-DGLX_USE_TLS \
 	-DHAVE_X11_PLATFORM \
 	-DHAVE_DRM_PLATFORM \
 	-DENABLE_SHADER_CACHE \
