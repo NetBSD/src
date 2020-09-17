dfdc4437907e83301545fb897942eaa8763847c2
christos <christos@NetBSD.org>
2020-09-17 02:17:51 +0000

    regen for x86_64 and start fixing the Makefiles (does not link yet)

--- external/gpl3/gdb/lib/libdecnumber/arch/x86_64/gstdint.h
+++ external/gpl3/gdb/lib/libdecnumber/arch/x86_64/gstdint.h
@@ -2,7 +2,7 @@
 /* Generated from: NetBSD: mknative-gdb,v 1.8 2019/05/29 01:56:06 christos Exp  */
 /* Generated from: NetBSD: mknative.common,v 1.16 2018/04/15 15:13:37 christos Exp  */
 
-/* generated for  x86_64--netbsd-gcc (NetBSD nb1 20190930) 8.3.0 */
+/* generated for  x86_64--netbsd-gcc (NetBSD ) 9.3.0 */
 
 #ifndef GCC_GENERATED_STDINT_H
 #define GCC_GENERATED_STDINT_H 1
