625869f3298dfc285feec8d134f55c40377274ed
rillig <rillig@NetBSD.org>
2020-09-22 19:41:09 +0000

    make(1): add test for unintended double expansion of deferred commands

--- usr.bin/make/unit-tests/deptgt-end.mk
+++ usr.bin/make/unit-tests/deptgt-end.mk
@@ -1,13 +1,36 @@
-# $NetBSD: deptgt-end.mk,v 1.3 2020/08/29 17:34:21 rillig Exp $
+# $NetBSD: deptgt-end.mk,v 1.4 2020/09/22 19:41:09 rillig Exp $
 #
 # Tests for the special target .END in dependency declarations,
 # which is run after making the desired targets.
 
+VAR=	Should not be expanded.
+
 .BEGIN:
-	: $@
+	: $@ '$${VAR}'
+	...
+	: $@ '$${VAR}' deferred
+# Oops: The deferred command must not be expanded twice.
+# The Var_Subst in Compat_RunCommand looks suspicious.
 
 .END:
-	: $@
+	: $@ '$${VAR}'
+	...
+	: $@ '$${VAR}' deferred
 
 all:
-	: $@
+	: $@ '$${VAR}'
+	...
+	: $@ '$${VAR}' deferred
+# Oops: The deferred command must not be expanded twice.
+# The Var_Subst in Compat_RunCommand looks suspicious.
+
+# The deferred commands are run in the order '.END .BEGIN all'.
+# This may be unexpected at first since the natural order would be
+# '.BEGIN all .END', but it is implemented correctly.
+#
+# At the point where the commands of a node with deferred commands are run,
+# the deferred commands are appended to the commands of the .END node.
+# This happens in Compat_RunCommand, and to prevent an endless loop, the
+# deferred commands of the .END node itself are not appended to itself.
+# Instead, the deferred commands of the .END node are run as if they were
+# immediate commands.
