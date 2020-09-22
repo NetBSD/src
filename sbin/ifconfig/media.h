bfeafc760c2b507b97bfea23b75d9003e1ba1dc5
roy <roy@NetBSD.org>
2020-09-22 14:14:17 +0000

    ifconfig: Report link state even if media is not supported
    For AF_LINK addrs from getifaddrs(2), ifa_data is struct if_data.
    This in turn holds ifi_link_state which we can use to report
    link status if the interface does not support media where it's normally
    reported.
    
    Based on OpenBSD.

--- sbin/ifconfig/media.h
+++ sbin/ifconfig/media.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: media.h,v 1.1 2008/07/02 07:44:15 dyoung Exp $	*/
+/*	$NetBSD: media.h,v 1.2 2020/09/22 14:14:17 roy Exp $	*/
 
 #ifndef	_IFCONFIG_MEDIA_H
 #define	_IFCONFIG_MEDIA_H
@@ -11,6 +11,6 @@ extern struct pkw kwmedia;
 
 void	print_media_word(int, const char *);
 void	process_media_commands(prop_dictionary_t);
-void	media_status(prop_dictionary_t, prop_dictionary_t);
+void	media_status(int, int, prop_dictionary_t, prop_dictionary_t);
 
 #endif	/* _IFCONFIG_MEDIA_H */
