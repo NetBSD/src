a0a8375076fe08abc41e0bf163ac07c84eec0ed4
rillig <rillig@NetBSD.org>
2020-09-30 05:42:06 +0000

    make(1): demonstrate bug in evaluation of indirect variable modifiers
    Reported by sjg.

--- usr.bin/make/unit-tests/varmod-l-name-to-value.mk
+++ usr.bin/make/unit-tests/varmod-l-name-to-value.mk
@@ -1,4 +1,4 @@
-# $NetBSD: varmod-l-name-to-value.mk,v 1.3 2020/08/25 22:25:05 rillig Exp $
+# $NetBSD: varmod-l-name-to-value.mk,v 1.4 2020/09/30 05:42:06 rillig Exp $
 #
 # Tests for the :L modifier, which returns the variable name as the new value.
 
@@ -25,6 +25,13 @@
 # Probably unintended, but maybe useful.
 .if ${VARNAME:L:S,VAR,,:L} != "VARNAME"
 .error
+.endif
+
+# Between 2020-09-22 (var.c 1.527) and 2020-09-30 (var.c 1.554), there was
+# a bug in the evaluation of variable expression.  Indirect modifiers like
+# this :L did not update the definedness of the enclosing expression.
+# This resulted in a wrong "Malformed variable".
+.if ${value:${:UL}} == ""
 .endif
 
 all:
