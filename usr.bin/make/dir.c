ef0b5be0ac81e41a53cb18d017ab854fbe4f56ed
rillig <rillig@NetBSD.org>
2020-10-01 22:42:00 +0000

    make(1): remove redundant function prototypes

--- usr.bin/make/dir.c
+++ usr.bin/make/dir.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: dir.c,v 1.153 2020/09/28 23:13:57 rillig Exp $	*/
+/*	$NetBSD: dir.c,v 1.154 2020/10/01 22:42:00 rillig Exp $	*/
 
 /*
  * Copyright (c) 1988, 1989, 1990 The Regents of the University of California.
@@ -136,7 +136,7 @@
 #include "job.h"
 
 /*	"@(#)dir.c	8.2 (Berkeley) 1/2/94"	*/
-MAKE_RCSID("$NetBSD: dir.c,v 1.153 2020/09/28 23:13:57 rillig Exp $");
+MAKE_RCSID("$NetBSD: dir.c,v 1.154 2020/10/01 22:42:00 rillig Exp $");
 
 #define DIR_DEBUG0(text) DEBUG0(DIR, text)
 #define DIR_DEBUG1(fmt, arg1) DEBUG1(DIR, fmt, arg1)
@@ -245,13 +245,6 @@ static CachedDir *dotLast;	/* a fake path entry indicating we need to
 static Hash_Table mtimes;
 
 static Hash_Table lmtimes;	/* same as mtimes but for lstat */
-
-static void DirExpandInt(const char *, SearchPath *, StringList *);
-static char *DirLookup(CachedDir *, const char *, const char *, Boolean);
-static char *DirLookupSubdir(CachedDir *, const char *);
-static char *DirFindDot(Boolean, const char *, const char *);
-static char *DirLookupAbs(CachedDir *, const char *, const char *);
-
 
 /*
  * We use stat(2) a lot, cache the results.
