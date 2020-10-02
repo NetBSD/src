e0d0e6ed5c0072639eb6000a22b1271c8fb5d2a8
rillig <rillig@NetBSD.org>
2020-10-02 20:34:59 +0000

    make(1): add tests for parsing and exporting variables
    Once again, there are a few surprises deeply hidden inside the edge
    cases.

--- usr.bin/make/unit-tests/varmod-ifelse.mk
+++ usr.bin/make/unit-tests/varmod-ifelse.mk
@@ -1,9 +1,12 @@
-# $NetBSD: varmod-ifelse.mk,v 1.2 2020/08/16 14:25:16 rillig Exp $
+# $NetBSD: varmod-ifelse.mk,v 1.3 2020/10/02 20:34:59 rillig Exp $
 #
 # Tests for the ${cond:?then:else} variable modifier, which evaluates either
 # the then-expression or the else-expression, depending on the condition.
 
 # TODO: Implementation
+
+# TODO: Test another modifier after ifelse; does not work, it becomes part
+# of the else branch.
 
 all:
 	@:;
