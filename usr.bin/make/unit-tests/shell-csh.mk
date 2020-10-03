061864d82eeec1a912a189fd31ac7f3bcacfed1e
rillig <rillig@NetBSD.org>
2020-10-03 15:38:13 +0000

    make(1): demonstrate naive output filtering with csh in parallel mode

--- usr.bin/make/unit-tests/shell-csh.mk
+++ usr.bin/make/unit-tests/shell-csh.mk
@@ -1,12 +1,17 @@
-# $NetBSD: shell-csh.mk,v 1.3 2020/10/03 15:23:42 rillig Exp $
+# $NetBSD: shell-csh.mk,v 1.4 2020/10/03 15:38:13 rillig Exp $
 #
 # Tests for using a C shell for running the commands.
 
+# The shell path must be an absolute path.
+# This is only obvious in parallel mode since in compat mode,
+# simple commands are executed via execve directly.
 .SHELL: name="csh" path="${:!which csh!}"
 
-# The -j option is needed to cover the code in JobOutput.
+# In parallel mode, the commandShell->noPrint command is filtered from
+# the output, rather naively (in JobOutput).
 #
-# FIXME: The "se " does not belong in the output.
+# Until 2020-10-03, the output in parallel mode was garbled because
+# the definition of the csh had been wrong since 1993 at least.
 .MAKEFLAGS: -j1
 
 all:
@@ -21,3 +26,7 @@ all:
 
 	# This command is both printed and executed.
 	-echo ignore errors
+
+	# In the C shell, "unset verbose" is set as the noPrint command.
+	# Therefore it is filtered from the output, rather naively.
+	@echo 'They chatted in the sunset verbosely.'
