390d0c96c49a432160b08f647286be6f6e747ed4
rillig <rillig@NetBSD.org>
2020-10-03 15:21:12 +0000

    make(1): refine test for printing commands using csh

--- usr.bin/make/unit-tests/shell-csh.mk
+++ usr.bin/make/unit-tests/shell-csh.mk
@@ -1,13 +1,18 @@
-# $NetBSD: shell-csh.mk,v 1.1 2020/10/03 14:39:36 rillig Exp $
+# $NetBSD: shell-csh.mk,v 1.2 2020/10/03 15:21:12 rillig Exp $
 #
 # Tests for using a C shell for running the commands.
 
 .SHELL: name="csh" path="csh"
 
-# Contrary to sh and ksh, the csh does not know the ':' command.
-# Therefore this test uses 'true' instead.
 all:
-	true normal
-	@true hidden
-	+true always
-	-true ignore errors
+	# This command is both printed and executed.
+	echo normal
+
+	# This command is only executed.
+	@echo hidden
+
+	# This command is both printed and executed.
+	+echo always
+
+	# This command is both printed and executed.
+	-echo ignore errors
