925caa68e1e73b955891b25b131db891036c04c3
rillig <rillig@NetBSD.org>
2020-10-03 15:23:42 +0000

    make(1): demonstrate wrong output when using csh in parallel mode

--- usr.bin/make/unit-tests/shell-csh.mk
+++ usr.bin/make/unit-tests/shell-csh.mk
@@ -1,8 +1,13 @@
-# $NetBSD: shell-csh.mk,v 1.2 2020/10/03 15:21:12 rillig Exp $
+# $NetBSD: shell-csh.mk,v 1.3 2020/10/03 15:23:42 rillig Exp $
 #
 # Tests for using a C shell for running the commands.
 
-.SHELL: name="csh" path="csh"
+.SHELL: name="csh" path="${:!which csh!}"
+
+# The -j option is needed to cover the code in JobOutput.
+#
+# FIXME: The "se " does not belong in the output.
+.MAKEFLAGS: -j1
 
 all:
 	# This command is both printed and executed.
