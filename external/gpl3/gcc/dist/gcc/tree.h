6a1e1a2bea1df8c9c69f373144be9ec0359a561d
christos <christos@NetBSD.org>
2020-10-04 14:44:47 +0000

    Apply by hand a patch to fix http://r6.ca/blog/20200929T023701Z.html aka "the memcmp bug", from a patch for gcc-10 in https://gcc.gnu.org/bugzilla/show_bug.cgi?id=95189 Tested on x86_64 by running a full build, install and running it. Testing that the problem has been fixed with one of the unit tests provided.

--- external/gpl3/gcc/dist/gcc/tree.h
+++ external/gpl3/gcc/dist/gcc/tree.h
@@ -4227,7 +4227,7 @@ extern tree build_one_cst (tree);
 extern tree build_minus_one_cst (tree);
 extern tree build_all_ones_cst (tree);
 extern tree build_zero_cst (tree);
-extern tree build_string (int, const char *);
+extern tree build_string (unsigned, const char * = NULL);
 extern tree build_poly_int_cst (tree, const poly_wide_int_ref &);
 extern tree build_tree_list (tree, tree CXX_MEM_STAT_INFO);
 extern tree build_tree_list_vec (const vec<tree, va_gc> * CXX_MEM_STAT_INFO);
