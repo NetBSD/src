9234ef2dbed5bddba1491ebaa4767d2fbbbf231e
rillig <rillig@NetBSD.org>
2020-10-04 22:41:18 +0000

    make(1): add tests for .ifndef and .ifnmake

--- usr.bin/make/unit-tests/directive-ifndef.mk
+++ usr.bin/make/unit-tests/directive-ifndef.mk
@@ -1,8 +1,24 @@
-# $NetBSD: directive-ifndef.mk,v 1.2 2020/08/16 14:25:16 rillig Exp $
+# $NetBSD: directive-ifndef.mk,v 1.3 2020/10/04 22:41:18 rillig Exp $
 #
-# Tests for the .ifndef directive.
+# Tests for the .ifndef directive, which can be used for multiple-inclusion
+# guards.  In contrast to C, where #ifndef and #define nicely line up the
+# macro name, there is no such syntax in make.  Therefore, it is more
+# common to use .if !defined(GUARD) instead.
 
-# TODO: Implementation
+.ifndef GUARD
+GUARD=	# defined
+.info guarded section
+.endif
+
+.ifndef GUARD
+GUARD=	# defined
+.info guarded section
+.endif
+
+.if !defined(GUARD)
+GUARD=	# defined
+.info guarded section
+.endif
 
 all:
 	@:;
