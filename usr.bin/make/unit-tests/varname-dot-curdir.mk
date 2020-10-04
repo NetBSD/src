2ef9e8d68e84f34454ed9fc16adecc49abc33652
rillig <rillig@NetBSD.org>
2020-10-04 21:53:28 +0000

    make(1): fix assignment to .CURDIR via the shell assignment operator
    This is probably an edge case that nobody will ever stumble upon, since
    .CURDIR is usually regarded as a read-only variable.
    
    The other variable that is affected by this code path is .MAKE.EXPORTED,
    and for this variable as well, it would be unusual to assign it a value
    from a shell command.

--- usr.bin/make/unit-tests/varname-dot-curdir.mk
+++ usr.bin/make/unit-tests/varname-dot-curdir.mk
@@ -1,15 +1,17 @@
-# $NetBSD: varname-dot-curdir.mk,v 1.3 2020/10/04 20:06:48 rillig Exp $
+# $NetBSD: varname-dot-curdir.mk,v 1.4 2020/10/04 21:53:28 rillig Exp $
 #
 # Tests for the special .CURDIR variable.
 
 # TODO: Implementation
 
-# As of 2020-10-04, assigning the result of a shell command to .CURDIR tries
-# to add the shell command to the .PATH instead of the output of the shell
-# command.  Since "echo /" does not exist, the .PATH is left unmodified.
-# See Parse_DoVar at the very bottom.
+# Until 2020-10-04, assigning the result of a shell assignment to .CURDIR
+# tried to add the shell command ("echo /") to the .PATH instead of the
+# output of the shell command ("/").  Since "echo /" does not exist, the
+# .PATH was left unmodified.  See VarAssign_Eval.
+#
+# Since 2020-10-04, the output of the shell command is added to .PATH.
 .CURDIR!=	echo /
-.if ${.PATH:M/}
+.if ${.PATH:M/} != "/"
 .  error
 .endif
 
@@ -17,7 +19,7 @@
 # Appending to .CURDIR does not make sense, therefore it doesn't matter that
 # this code path is buggy as well.
 .CURDIR=	/
-.if !${.PATH:M/}
+.if ${.PATH:M/} != "/"
 .  error
 .endif
 
