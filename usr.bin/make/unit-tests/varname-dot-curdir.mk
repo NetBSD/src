c2446a819f9d0f80a7de001be3766ae75fea7bf1
rillig <rillig@NetBSD.org>
2020-10-04 20:06:48 +0000

    make(1): add test for assigning to .CURDIR

--- usr.bin/make/unit-tests/varname-dot-curdir.mk
+++ usr.bin/make/unit-tests/varname-dot-curdir.mk
@@ -1,8 +1,25 @@
-# $NetBSD: varname-dot-curdir.mk,v 1.2 2020/08/16 14:25:16 rillig Exp $
+# $NetBSD: varname-dot-curdir.mk,v 1.3 2020/10/04 20:06:48 rillig Exp $
 #
 # Tests for the special .CURDIR variable.
 
 # TODO: Implementation
+
+# As of 2020-10-04, assigning the result of a shell command to .CURDIR tries
+# to add the shell command to the .PATH instead of the output of the shell
+# command.  Since "echo /" does not exist, the .PATH is left unmodified.
+# See Parse_DoVar at the very bottom.
+.CURDIR!=	echo /
+.if ${.PATH:M/}
+.  error
+.endif
+
+# A normal assignment works fine, as does a substitution assignment.
+# Appending to .CURDIR does not make sense, therefore it doesn't matter that
+# this code path is buggy as well.
+.CURDIR=	/
+.if !${.PATH:M/}
+.  error
+.endif
 
 all:
 	@:;
