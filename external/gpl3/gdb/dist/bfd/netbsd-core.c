c076fdf5ec978e3db7c6179b60437c13824f06b0
kamil <kamil@NetBSD.org>
2020-10-06 21:39:53 +0000

    Undo local NFC patch

--- external/gpl3/gdb/dist/bfd/netbsd-core.c
+++ external/gpl3/gdb/dist/bfd/netbsd-core.c
@@ -275,7 +275,7 @@ const bfd_target core_netbsd_vec =
     0,				/* Symbol prefix.  */
     ' ',			/* ar_pad_char.  */
     16,				/* ar_max_namelen.  */
-    0,				/* match priority.  */
+    0,				/* Match priority.  */
     NO_GET64, NO_GETS64, NO_PUT64,	/* 64 bit data.  */
     NO_GET, NO_GETS, NO_PUT,		/* 32 bit data.  */
     NO_GET, NO_GETS, NO_PUT,		/* 16 bit data.  */
