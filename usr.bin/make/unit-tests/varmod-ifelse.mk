245b875e3f3498d6087aebf5f816e88a166f9173
rillig <rillig@NetBSD.org>
2020-10-09 07:03:20 +0000

    make(1): add test for parsing the :? variable modifier

--- usr.bin/make/unit-tests/varmod-ifelse.mk
+++ usr.bin/make/unit-tests/varmod-ifelse.mk
@@ -1,12 +1,19 @@
-# $NetBSD: varmod-ifelse.mk,v 1.3 2020/10/02 20:34:59 rillig Exp $
+# $NetBSD: varmod-ifelse.mk,v 1.4 2020/10/09 07:03:20 rillig Exp $
 #
 # Tests for the ${cond:?then:else} variable modifier, which evaluates either
 # the then-expression or the else-expression, depending on the condition.
 
 # TODO: Implementation
 
-# TODO: Test another modifier after ifelse; does not work, it becomes part
-# of the else branch.
+# When the :? is parsed, it is greedy.  The else branch spans all the
+# text, up until the closing character '}', even if the text looks like
+# another modifier.
+.if ${1:?then:else:Q} != "then"
+.  error
+.endif
+.if ${0:?then:else:Q} != "else:Q"
+.  error
+.endif
 
 all:
 	@:;
