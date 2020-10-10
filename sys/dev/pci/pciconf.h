11c18d7742daadabe0a3bd56a211bd5f6dbdee77
jmcneill <jmcneill@NetBSD.org>
2020-10-10 15:22:15 +0000

    Add pciconf_resource_reserve. This allows MD code to mark specific memory and I/O regions as in use. When pciconf finds a device already configured to use one of these regions, the device config is left as-is.

--- sys/dev/pci/pciconf.h
+++ sys/dev/pci/pciconf.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: pciconf.h,v 1.14 2020/07/07 03:38:49 thorpej Exp $	*/
+/*	$NetBSD: pciconf.h,v 1.15 2020/10/10 15:22:15 jmcneill Exp $	*/
 
 /*
  * Copyright 2001 Wasabi Systems, Inc.
@@ -51,6 +51,7 @@ struct pciconf_resources *
 void	pciconf_resource_fini(struct pciconf_resources *);
 int	pciconf_resource_add(struct pciconf_resources *, int,
 	    bus_addr_t, bus_size_t);
+void	pciconf_resource_reserve(int, bus_addr_t, bus_size_t);
 
 /*
  * args: pci_chipset_tag_t, resources, firstbus, cacheline_size
