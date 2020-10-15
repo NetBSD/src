4400c5d0b5380d9c62d67bdc0194e2c9ba2cf23c
mgorny <mgorny@NetBSD.org>
2020-10-15 17:38:41 +0000

    9.99.74 changes ptrace_machdep_dorequest() to fix LWP support

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.676 2020/09/16 13:42:05 roy Exp $	*/
+/*	$NetBSD: param.h,v 1.677 2020/10/15 17:38:41 mgorny Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -67,7 +67,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	999007300	/* NetBSD 9.99.73 */
+#define	__NetBSD_Version__	999007400	/* NetBSD 9.99.74 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
