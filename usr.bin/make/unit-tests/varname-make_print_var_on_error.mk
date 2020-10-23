9bfe75b9f5a39b0a6418afaa811b5ffd9a3e8a15
rillig <rillig@NetBSD.org>
2020-10-23 05:44:02 +0000

    make(1): add test for MAKE_PRINT_VAR_ON_ERROR

--- usr.bin/make/unit-tests/varname-make_print_var_on_error.mk
+++ usr.bin/make/unit-tests/varname-make_print_var_on_error.mk
@@ -1,8 +1,15 @@
-# $NetBSD: varname-make_print_var_on_error.mk,v 1.2 2020/08/16 14:25:16 rillig Exp $
+# $NetBSD: varname-make_print_var_on_error.mk,v 1.3 2020/10/23 05:44:02 rillig Exp $
 #
-# Tests for the special .MAKE.PRINT_VAR_ON_ERROR variable.
+# Tests for the special MAKE_PRINT_VAR_ON_ERROR variable, which prints the
+# values of selected variables on error.
 
-# TODO: Implementation
+MAKE_PRINT_VAR_ON_ERROR=	.ERROR_TARGET .ERROR_CMD
 
 all:
-	@:;
+	@: command before
+	@echo fail; false
+	@: command after
+
+# XXX: As of 2020-10-23, the .ERROR_CMD variable seems pointless since at
+# that point, the first command in gn->commands has been set to NULL already.
+# And because of this, .ERROR_CMD stays an empty list.
