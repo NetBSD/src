939aea5488d679d4cbeed12a6d19074524f60bb4
rillig <rillig@NetBSD.org>
2020-10-24 03:18:22 +0000

    make(1): extend test suff-lookup.mk
    This test ensures that FindSuffByName behaves correctly.
    
    In order to convert some char pointers to const char pointers, there
    needs to be FindSuffByNameLen, and to make sure that this function
    actually uses the given length, this test has the suffixes ".sho" and
    ".short".  Without the proper string comparison in FindSuffByNameLen,
    the suffix ".short" might be found when searching for ".sho".

--- usr.bin/make/unit-tests/suff-lookup.mk
+++ usr.bin/make/unit-tests/suff-lookup.mk
@@ -1,4 +1,4 @@
-# $NetBSD: suff-lookup.mk,v 1.1 2020/10/24 02:25:02 rillig Exp $
+# $NetBSD: suff-lookup.mk,v 1.2 2020/10/24 03:18:22 rillig Exp $
 #
 # Demonstrate name resolution for suffixes.
 #
@@ -7,14 +7,14 @@
 
 .MAKEFLAGS: -ds
 
-all: suff-main.cc
+all: suff-lookup.cc
 
 .SUFFIXES: .c .cc .ccc
 
 # Register '.short' before '.sho'.  When searching for the transformation
 # '.sho.c', the suffix '.short' must not be found even though it starts with
 # the correct characters.
-.SUFFIXES: .short .sho
+.SUFFIXES: .short .sho .dead-end
 
 # From long to short suffix.
 .ccc.cc:
@@ -28,7 +28,13 @@ all: suff-main.cc
 	: 'Making ${.TARGET} from ${.IMPSRC}.'
 .sho.c:
 	: 'Making ${.TARGET} from ${.IMPSRC}.'
+.dead-end.short:
+	: 'Making ${.TARGET} from ${.IMPSRC}.'
 
-suff-main.sho:
+suff-lookup.sho:
 	: 'Making ${.TARGET} out of nothing.'
 
+# Deleting all suffixes and adding them again rebuilds all of the above
+# transformation rules.
+.SUFFIXES:
+.SUFFIXES: .c .cc .ccc .short .sho .dead-end
