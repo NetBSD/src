ac865510fa473670ac3ca5be3fed60819e3a2091
rillig <rillig@NetBSD.org>
2020-10-25 22:04:24 +0000

    make(1): add tests for generating the ... command indirectly

--- usr.bin/make/unit-tests/sh-dots.mk
+++ usr.bin/make/unit-tests/sh-dots.mk
@@ -1,4 +1,4 @@
-# $NetBSD: sh-dots.mk,v 1.2 2020/10/18 18:12:42 rillig Exp $
+# $NetBSD: sh-dots.mk,v 1.3 2020/10/25 22:04:24 rillig Exp $
 #
 # Tests for the special shell command line "...", which does not run the
 # commands below it but appends them to the list of commands that are run
@@ -6,7 +6,7 @@
 
 .MAKEFLAGS: -d0			# switch stdout to being line-buffered
 
-all: first hidden repeated commented
+all: first hidden repeated commented indirect indirect-space
 
 # The ${.TARGET} correctly expands to the target name, even though the
 # commands are run separately from the main commands.
@@ -28,6 +28,21 @@ commented: .IGNORE
 	@echo commented ${.TARGET}
 	...	# Run the below commands later
 	@echo commented delayed ${.TARGET}
+
+# The dots don't have to be written literally, they can also come from a
+# variable expression.
+indirect:
+	@echo indirect regular
+	${:U...}
+	@echo indirect deferred
+
+# If the dots are followed by a space, that space is part of the command and
+# thus does not defer the command below it.
+indirect-space: .IGNORE
+	@echo indirect-space regular
+	${:U... }
+	@echo indirect-space deferred
+
 
 # The "..." can appear more than once, even though that doesn't make sense.
 # The second "..." is a no-op.
