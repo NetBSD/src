73b16c6156cc67cc7afbc4cbe8dba9f95717589e
rillig <rillig@NetBSD.org>
2020-10-26 21:34:10 +0000

    make(1): group the command line options and arguments
    By having a single struct that holds all command line options and
    arguments, it is easy to see in the code when such a command line
    argument is modified.  It also cleans up the namespace since the command
    line options don't follow a common naming style.  Having them in a
    struct also means that there is a single place for putting the
    documentation, not two as before.
    
    The struct also suggests to extract the initialization code out of main,
    which is still too large, having more than 400 lines of code and
    covering far too many topics.

--- usr.bin/make/cond.c
+++ usr.bin/make/cond.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: cond.c,v 1.168 2020/10/24 04:51:19 rillig Exp $	*/
+/*	$NetBSD: cond.c,v 1.169 2020/10/26 21:34:10 rillig Exp $	*/
 
 /*
  * Copyright (c) 1988, 1989, 1990 The Regents of the University of California.
@@ -93,7 +93,7 @@
 #include "dir.h"
 
 /*	"@(#)cond.c	8.2 (Berkeley) 1/2/94"	*/
-MAKE_RCSID("$NetBSD: cond.c,v 1.168 2020/10/24 04:51:19 rillig Exp $");
+MAKE_RCSID("$NetBSD: cond.c,v 1.169 2020/10/26 21:34:10 rillig Exp $");
 
 /*
  * The parsing of conditional expressions is based on this grammar:
@@ -294,7 +294,7 @@ FuncMake(size_t argLen MAKE_ATTR_UNUSED, const char *arg)
 {
     StringListNode *ln;
 
-    for (ln = create->first; ln != NULL; ln = ln->next)
+    for (ln = opts.create->first; ln != NULL; ln = ln->next)
 	if (Str_Match(ln->datum, arg))
 	    return TRUE;
     return FALSE;
