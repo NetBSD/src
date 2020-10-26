0c169c27be944e5ac46b1e6cabde2e196355a05c
christos <christos@NetBSD.org>
2020-10-26 17:35:39 +0000

    Depend directly on EXEC_ELF{32,64} to determine which versions of the coredump code are available.

--- sys/kern/kern_sig.c
+++ sys/kern/kern_sig.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: kern_sig.c,v 1.392 2020/10/20 13:16:26 christos Exp $	*/
+/*	$NetBSD: kern_sig.c,v 1.393 2020/10/26 17:35:39 christos Exp $	*/
 
 /*-
  * Copyright (c) 2006, 2007, 2008, 2019 The NetBSD Foundation, Inc.
@@ -70,8 +70,9 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: kern_sig.c,v 1.392 2020/10/20 13:16:26 christos Exp $");
+__KERNEL_RCSID(0, "$NetBSD: kern_sig.c,v 1.393 2020/10/26 17:35:39 christos Exp $");
 
+#include "opt_execfmt.h"
 #include "opt_ptrace.h"
 #include "opt_dtrace.h"
 #include "opt_compat_sunos.h"
@@ -2356,7 +2357,7 @@ coredump_netbsd(struct lwp *l, struct coredump_iostate *iocookie)
 	return retval;
 }
 
-#if !defined(_LP64) || defined(COMPAT_NETBSD32)
+#ifdef EXEC_ELF32
 int
 coredump_elf32(struct lwp *l, struct coredump_iostate *iocookie)
 {
@@ -2367,7 +2368,7 @@ coredump_elf32(struct lwp *l, struct coredump_iostate *iocookie)
 }
 #endif
 
-#ifdef _LP64
+#ifdef EXEC_ELF64
 int
 coredump_elf64(struct lwp *l, struct coredump_iostate *iocookie)
 {
