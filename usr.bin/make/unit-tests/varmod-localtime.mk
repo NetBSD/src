3e580fab68b1196f87545adecd729c6c01975196
rillig <rillig@NetBSD.org>
2020-10-29 19:01:10 +0000

    make(1): add test for default format of the :localtime modifier

--- usr.bin/make/unit-tests/varmod-localtime.mk
+++ usr.bin/make/unit-tests/varmod-localtime.mk
@@ -1,7 +1,15 @@
-# $NetBSD: varmod-localtime.mk,v 1.3 2020/08/23 15:13:21 rillig Exp $
+# $NetBSD: varmod-localtime.mk,v 1.4 2020/10/29 19:01:10 rillig Exp $
 #
 # Tests for the :localtime variable modifier, which returns the given time,
 # formatted as a local timestamp.
+
+# Test for the default time format, %c.  Since the time always varies, it's
+# only possible to check for the general format here.  The names of the
+# month and weekday are always in English, independent from the locale.
+# Example: Thu Oct 29 18:56:41 2020
+.if ${:U:localtime:tW:M??? ??? ?? ??\:??\:?? ????} == ""
+.  error
+.endif
 
 all:
 	@echo ${%Y:L:localtim=1593536400}	# modifier name too short
