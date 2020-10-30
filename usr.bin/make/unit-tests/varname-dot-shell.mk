bbfd7f457d4e4bb25bcef64b0d7b264d3526c128
rillig <rillig@NetBSD.org>
2020-10-30 15:03:58 +0000

    make(1): move test flags for varname-dot-shell to the test itself

--- usr.bin/make/unit-tests/varname-dot-shell.mk
+++ usr.bin/make/unit-tests/varname-dot-shell.mk
@@ -1,9 +1,11 @@
-# $NetBSD: varname-dot-shell.mk,v 1.3 2020/10/24 08:46:08 rillig Exp $
+# $NetBSD: varname-dot-shell.mk,v 1.4 2020/10/30 15:03:58 rillig Exp $
 #
 # Tests for the special .SHELL variable, which contains the shell used for
 # running the commands.
 #
 # This variable is read-only.
+
+.MAKEFLAGS: -dcpv
 
 ORIG_SHELL:=	${.SHELL}
 
@@ -21,5 +23,7 @@ ORIG_SHELL:=	${.SHELL}
 .  error
 .endif
 
+.MAKEFLAGS: -d0
+
 all:
-	@echo ${.SHELL:M*}
+	@:;
