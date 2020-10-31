759a4249daae93294e9ef339848f73484549bfb4
rillig <rillig@NetBSD.org>
2020-10-31 21:30:03 +0000

    make(1): add test for debug log for expansion of curly braces

--- usr.bin/make/unit-tests/dir.mk
+++ usr.bin/make/unit-tests/dir.mk
@@ -1,10 +1,27 @@
-# $NetBSD: dir.mk,v 1.6 2020/10/31 21:12:36 rillig Exp $
+# $NetBSD: dir.mk,v 1.7 2020/10/31 21:30:03 rillig Exp $
 #
 # Tests for dir.c.
+
+.MAKEFLAGS: -m /		# hide /usr/share/mk from the debug log
 
 # Dependency lines may use braces for expansion.
 # See DirExpandCurly for the implementation.
 all: {one,two,three}
+
+# XXX: The above dependency line is parsed as a single node named
+# "{one,two,three}".  There are no individual targets "one", "two", "three"
+# yet.  The node exists but is not a target since it never appeared
+# on the left-hand side of a dependency operator.  However, it is listed
+# in .ALLTARGETS (which lists all nodes, not only targets).
+.if target(one)
+.  error
+.endif
+.if target({one,two,three})
+.  error
+.endif
+.if ${.ALLTARGETS:M{one,two,three}} != "{one,two,three}"
+.  error
+.endif
 
 one:
 	: 1
@@ -58,3 +75,19 @@ all: {{{{{{{{{{single-word}}}}}}}}}}
 
 single-word:
 	: $@
+
+# Demonstrate debug logging for filename expansion, especially curly braces.
+.MAKEFLAGS: -dd
+# The below line does not call Dir_Expand yet.
+# It is expanded only when necessary, that is, when the 'debug' target is
+# indeed made.
+debug: {{thi,fou}r,fif}twen
+# Therefore, keep the debug logging active.
+
+.PHONY: one two three four five six
+.PHONY: thirteen fourteen fifteen
+.PHONY: single-word
+.PHONY: pre-patch patch pre-configure configure
+.PHONY: fetch fetch-post extract extract-post
+.PHONY: dup-1 single-word
+.PHONY: all
