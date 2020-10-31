e6f6aa446a561cb70786cc997f435047720fbb28
rillig <rillig@NetBSD.org>
2020-10-31 21:12:36 +0000

    make(1): mark the output in test dir.mk with a prefix
    This is needed to distinguish it from the debug log, which will be added
    in the next commit.

--- usr.bin/make/unit-tests/dir.mk
+++ usr.bin/make/unit-tests/dir.mk
@@ -1,4 +1,4 @@
-# $NetBSD: dir.mk,v 1.5 2020/09/07 19:48:08 rillig Exp $
+# $NetBSD: dir.mk,v 1.6 2020/10/31 21:12:36 rillig Exp $
 #
 # Tests for dir.c.
 
@@ -7,54 +7,54 @@
 all: {one,two,three}
 
 one:
-	@echo 1
+	: 1
 two:
-	@echo 2
+	: 2
 three:
-	@echo 3
+	: 3
 
 # The braces may start in the middle of a word.
 all: f{our,ive}
 
 four:
-	@echo 4
+	: 4
 five:
-	@echo 5
+	: 5
 six:
-	@echo 6
+	: 6
 
 # Nested braces work as expected since 2020-07-31 19:06 UTC.
 # They had been broken at least since 2003-01-01, probably even longer.
 all: {{thi,fou}r,fif}teen
 
 thirteen:
-	@echo 13
+	: 13
 fourteen:
-	@echo 14
+	: 14
 fifteen:
-	@echo 15
+	: 15
 
 # There may be multiple brace groups side by side.
 all: {pre-,}{patch,configure}
 
 pre-patch patch pre-configure configure:
-	@echo $@
+	: $@
 
 # Empty pieces are allowed in the braces.
 all: {fetch,extract}{,-post}
 
 fetch fetch-post extract extract-post:
-	@echo $@
+	: $@
 
 # The expansions may have duplicates.
 # These are merged together because of the dependency line.
 all: dup-{1,1,1,1,1,1,1}
 
 dup-1:
-	@echo $@
+	: $@
 
 # Other than in Bash, the braces are also expanded if there is no comma.
 all: {{{{{{{{{{single-word}}}}}}}}}}
 
 single-word:
-	@echo $@
+	: $@
