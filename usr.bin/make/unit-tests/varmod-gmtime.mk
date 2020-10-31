a076f4f25e884d460987a3450ed59f477405f889
rillig <rillig@NetBSD.org>
2020-10-31 19:48:23 +0000

    make(1): add tests for error handling in the :gmtime variable modifier

--- usr.bin/make/unit-tests/varmod-gmtime.mk
+++ usr.bin/make/unit-tests/varmod-gmtime.mk
@@ -1,10 +1,11 @@
-# $NetBSD: varmod-gmtime.mk,v 1.3 2020/10/29 18:59:43 rillig Exp $
+# $NetBSD: varmod-gmtime.mk,v 1.4 2020/10/31 19:48:23 rillig Exp $
 #
 # Tests for the :gmtime variable modifier, which formats a timestamp
 # using strftime(3).
 
 all:	mod-gmtime
 all:	mod-gmtime-indirect
+all:	parse-errors
 
 # Test for the default time format, %c.  Since the time always varies, it's
 # only possible to check for the general format here.  The names of the
@@ -38,6 +39,39 @@ mod-gmtime-indirect:
 	# If ApplyModifier_Gmtime were to pass its argument through
 	# ParseModifierPart, this would work.
 	@echo ${%Y:L:gmtime=${:U1593536400}}
+
+parse-errors:
+	@echo $@:
+
+	# As of 2020-10-31, it is possible to pass negative time stamps
+	# to the :gmtime modifier, resulting in dates before 1970.
+	# Going back 50 years in the past is not a practical use case for
+	# make.
+	: -1 becomes ${:L:gmtime=-1}.
+
+	# 0 means now; to get consistent test results, the actual value has
+	# to be normalized.
+	: 0 becomes ${:L:gmtime=0:C,^... ... .. ..:..:.. 20..$,ok,W}.
+
+	: 1 becomes ${:L:gmtime=1}.
+
+	: INT32_MAX becomes ${:L:gmtime=2147483647}.
+
+	# This may be different if time_t is still a 32-bit signed integer.
+	: INT32_MAX + 1 becomes ${:L:gmtime=2147483648}.
+
+	# Integer overflow.
+	# Because this modifier is implemented using strtoul, the parsed
+	# time is ULONG_MAX, which gets converted to -1.  This results
+	# in a time stamp of the second before 1970.
+	: overflow becomes ${:L:gmtime=10000000000000000000000000000000}.
+
+	# As of 2020-10-31, there is no error handling while parsing the
+	# :gmtime modifier, thus no error message is printed.  Parsing
+	# stops after the '=', and the remaining string is parsed for
+	# more variable modifiers.  Because of the unknown modifier 'e',
+	# the whole variable value is discarded and thus not printed.
+	: letter becomes ${:L:gmtime=error}.
 
 all:
 	@:;
