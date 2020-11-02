3f600bbee8247a3086dcafd9853d0f819e91ee8f
rillig <rillig@NetBSD.org>
2020-11-02 22:44:29 +0000

    make(1): document undefined behavior in Parse_IsVar
    Sigh.  If only C could be compiled in strict mode that detects these
    out-of-bounds memory accesses.

--- usr.bin/make/unit-tests/varname.mk
+++ usr.bin/make/unit-tests/varname.mk
@@ -1,4 +1,4 @@
-# $NetBSD: varname.mk,v 1.6 2020/11/02 22:29:48 rillig Exp $
+# $NetBSD: varname.mk,v 1.7 2020/11/02 22:44:29 rillig Exp $
 #
 # Tests for special variables, such as .MAKE or .PARSEDIR.
 # And for variable names in general.
@@ -23,6 +23,10 @@ ${VARNAME}=	3 open parentheses
 
 # In the above test, the variable name is constructed indirectly.  Neither
 # of the following expressions produces the intended effect.
+#
+# This is not a variable assignment since the parentheses and braces are not
+# balanced.  At the end of the line, there are still 3 levels open, which
+# means the variable name is not finished.
 ${:UVAR(((}=	try1
 ${:UVAR\(\(\(}=	try2
 ${:UVAR\(\(\(}=	try3
