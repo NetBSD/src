1eb6502ab8cdb2d6284969444f19cd5e3fb9f560
rillig <rillig@NetBSD.org>
2020-11-03 17:17:31 +0000

    make(1): move tests from directives.mk to separate tests

--- usr.bin/make/unit-tests/directive-info.mk
+++ usr.bin/make/unit-tests/directive-info.mk
@@ -1,8 +1,17 @@
-# $NetBSD: directive-info.mk,v 1.2 2020/08/16 14:25:16 rillig Exp $
+# $NetBSD: directive-info.mk,v 1.3 2020/11/03 17:17:31 rillig Exp $
 #
 # Tests for the .info directive.
 
 # TODO: Implementation
+
+.info begin .info tests
+.inf				# misspelled
+.info				# oops: message should be "missing parameter"
+.info message
+.info		indented message
+.information
+.information message		# oops: misspelled
+.info.man:			# not a message, but possibly a suffix rule
 
 all:
 	@:;
--- usr.bin/make/unit-tests/directive.mk
+++ usr.bin/make/unit-tests/directive.mk
@@ -1,8 +1,18 @@
-# $NetBSD: directive.mk,v 1.2 2020/08/16 14:25:16 rillig Exp $
+# $NetBSD: directive.mk,v 1.3 2020/11/03 17:17:31 rillig Exp $
 #
 # Tests for the preprocessing directives, such as .if or .info.
 
 # TODO: Implementation
+
+# Unknown directives are correctly named in the error messages,
+# even if they are indented.
+.indented none
+.  indented 2 spaces
+.	indented tab
+
+# Directives must be written directly, not indirectly via variable
+# expressions.
+.${:Uinfo} directives cannot be indirect
 
 all:
 	@:;
