aca0420f935398b6c29b19f300deb00d4d2debcb
mlelstv <mlelstv@NetBSD.org>
2020-11-03 12:04:56 +0000

    state struct should use fixed size types. NFCI.

--- sys/external/bsd/vchiq/dist/interface/vchiq_arm/vchiq_core.h
+++ sys/external/bsd/vchiq/dist/interface/vchiq_arm/vchiq_core.h
@@ -371,7 +371,7 @@ typedef struct vchiq_shared_state_struct {
 	REMOTE_EVENT_T recycle;
 
 	/* The slot_queue index where the next recycled slot will be written. */
-	int slot_queue_recycle;
+	int32_t slot_queue_recycle;
 
 	/* This event should be signalled when a synchronous message is sent. */
 	REMOTE_EVENT_T sync_trigger;
