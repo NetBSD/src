70c2d72ac2cd17ae94049566814b73ce85ae4bf9
skrll <skrll@NetBSD.org>
2020-11-04 07:09:45 +0000

    RCSID and whitespace police...

--- sys/arch/riscv/riscv/cpu_mainbus.c
+++ sys/arch/riscv/riscv/cpu_mainbus.c
@@ -1,3 +1,5 @@
+/*	$NetBSD: cpu_mainbus.c,v 1.2 2020/11/04 07:09:46 skrll Exp $	*/
+
 /*-
  * Copyright (c) 2014 The NetBSD Foundation, Inc.
  * All rights reserved.
@@ -29,7 +31,7 @@
 
 #include <sys/cdefs.h>
 
-__RCSID("$NetBSD: cpu_mainbus.c,v 1.1 2015/03/28 16:13:56 matt Exp $");
+__RCSID("$NetBSD: cpu_mainbus.c,v 1.2 2020/11/04 07:09:46 skrll Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
