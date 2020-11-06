8060eaa2ce677e8a6a887f31dffffa8fb315b438
christos <christos@NetBSD.org>
2020-11-06 14:50:13 +0000

    PR/55777: Ruslan Nikolaev: Move the unp_sysctl_create to uipc_usrreq.c to facilitate splitting rump modules and does not require a dummy function.

--- sys/kern/uipc_domain.c
+++ sys/kern/uipc_domain.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: uipc_domain.c,v 1.107 2020/10/17 09:45:20 mlelstv Exp $	*/
+/*	$NetBSD: uipc_domain.c,v 1.108 2020/11/06 14:50:13 christos Exp $	*/
 
 /*
  * Copyright (c) 1982, 1986, 1993
@@ -32,7 +32,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: uipc_domain.c,v 1.107 2020/10/17 09:45:20 mlelstv Exp $");
+__KERNEL_RCSID(0, "$NetBSD: uipc_domain.c,v 1.108 2020/11/06 14:50:13 christos Exp $");
 
 #include <sys/param.h>
 #include <sys/socket.h>
@@ -692,7 +692,6 @@ sysctl_net_setup(void)
 		       SYSCTL_DESCR("SOCK_DGRAM protocol control block list"),
 		       sysctl_unpcblist, 0, NULL, 0,
 		       CTL_NET, PF_LOCAL, SOCK_DGRAM, CTL_CREATE, CTL_EOL);
-	unp_sysctl_create(&domain_sysctllog);
 }
 
 void
