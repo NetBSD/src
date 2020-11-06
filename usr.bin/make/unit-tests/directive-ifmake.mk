5909008f0e559bb8acf8befffe2778bd3d786ea4
rillig <rillig@NetBSD.org>
2020-11-06 20:29:35 +0000

    make(1): fix indentation in test directive-ifmake

--- usr.bin/make/unit-tests/directive-ifmake.mk
+++ usr.bin/make/unit-tests/directive-ifmake.mk
@@ -1,44 +1,44 @@
-# $NetBSD: directive-ifmake.mk,v 1.4 2020/08/30 14:25:45 rillig Exp $
+# $NetBSD: directive-ifmake.mk,v 1.5 2020/11/06 20:29:35 rillig Exp $
 #
 # Tests for the .ifmake directive, which provides a shortcut for asking
 # whether a certain target is requested to be made from the command line.
 
 # This is the most basic form.
 .ifmake first
-.info ok: positive condition works
+.  info ok: positive condition works
 .else
-.warning positive condition fails
+.  warning positive condition fails
 .endif
 
 # The not operator works as expected.
 # An alternative interpretation were that this condition is asking whether
 # the target "!first" was requested.  To distinguish this, see the next test.
 .ifmake !first
-.warning unexpected
+.  warning unexpected
 .else
-.info ok: negation works
+.  info ok: negation works
 .endif
 
 # See if the exclamation mark really means "not", or if it is just part of
 # the target name.
 .ifmake !!first
-.info ok: double negation works
+.  info ok: double negation works
 .else
-.warning double negation fails
+.  warning double negation fails
 .endif
 
 # Multiple targets can be combined using the && and || operators.
 .ifmake first && second
-.info ok: both mentioned
+.  info ok: both mentioned
 .else
-.warning && does not work as expected
+.  warning && does not work as expected
 .endif
 
 # Negation also works in complex conditions.
 .ifmake first && !unmentioned
-.info ok: only those mentioned
+.  info ok: only those mentioned
 .else
-.warning && with ! does not work as expected
+.  warning && with ! does not work as expected
 .endif
 
 # Using the .MAKEFLAGS special dependency target, arbitrary command
@@ -46,9 +46,9 @@
 # possible to extend the targets to be made.
 .MAKEFLAGS: late-target
 .ifmake late-target
-.info Targets can even be added at parse time.
+.  info Targets can even be added at parse time.
 .else
-.info No, targets cannot be added at parse time anymore.
+.  info No, targets cannot be added at parse time anymore.
 .endif
 
 first second unmentioned late-target:
