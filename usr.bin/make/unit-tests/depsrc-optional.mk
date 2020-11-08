783d539a39d0fda766b3e4ab798e4b4b773319af
rillig <rillig@NetBSD.org>
2020-11-08 10:33:47 +0000

    make(1): fix debug output in out-of-date for cohorts (since 2003-11-14)
    Before, a wrong cause for being out-of-date was printed in the debug log,
    for optional cohorts.  This was caused by having the same conditions
    duplicated in the code, instead of putting them in a separate function.
    
    Now the optional cohort is correctly identified as using the '::'
    dependency operator.

--- usr.bin/make/unit-tests/depsrc-optional.mk
+++ usr.bin/make/unit-tests/depsrc-optional.mk
@@ -1,12 +1,9 @@
-# $NetBSD: depsrc-optional.mk,v 1.4 2020/11/08 10:17:55 rillig Exp $
+# $NetBSD: depsrc-optional.mk,v 1.5 2020/11/08 10:33:47 rillig Exp $
 #
 # Tests for the special source .OPTIONAL in dependency declarations,
 # which ignores the target if make cannot find out how to create it.
 #
 # TODO: Describe practical use cases for this feature.
-
-# TODO: Explain why the commands for "important" are not executed.
-# I had thought that only the "optional" commands were skipped.
 
 all: important
 	: ${.TARGET} is made.
@@ -15,11 +12,10 @@ important: optional optional-cohort
 	: ${.TARGET} is made.
 
 optional: .OPTIONAL
-	: This is not executed.
+	: An optional leaf node is not executed.
 
-# XXX: "non-existent and no sources" is wrong, should be ":: operator and
-# no sources..." instead.
+# See IsOODateRegular.
 optional-cohort:: .OPTIONAL
-	: This is not executed.
+	: A leaf node using '::' is considered out-of-date.
 
 .MAKEFLAGS: -dm
