a1237b73d493c29a3c9419b64402b9bc06b2a125
rillig <rillig@NetBSD.org>
2020-11-08 10:17:55 +0000

    make(1): add test for wrong debug message in GNode_IsOODate

--- usr.bin/make/unit-tests/depsrc-optional.mk
+++ usr.bin/make/unit-tests/depsrc-optional.mk
@@ -1,4 +1,4 @@
-# $NetBSD: depsrc-optional.mk,v 1.3 2020/09/05 15:57:12 rillig Exp $
+# $NetBSD: depsrc-optional.mk,v 1.4 2020/11/08 10:17:55 rillig Exp $
 #
 # Tests for the special source .OPTIONAL in dependency declarations,
 # which ignores the target if make cannot find out how to create it.
@@ -11,8 +11,15 @@
 all: important
 	: ${.TARGET} is made.
 
-important: optional
+important: optional optional-cohort
 	: ${.TARGET} is made.
 
 optional: .OPTIONAL
 	: This is not executed.
+
+# XXX: "non-existent and no sources" is wrong, should be ":: operator and
+# no sources..." instead.
+optional-cohort:: .OPTIONAL
+	: This is not executed.
+
+.MAKEFLAGS: -dm
