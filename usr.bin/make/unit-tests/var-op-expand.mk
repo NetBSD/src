eabaae1707227e39bfe0ce111e8668b1fa22975c
rillig <rillig@NetBSD.org>
2020-11-08 14:00:52 +0000

    make(1): fix bug description of using := with undefined variable
    In 1993, the variable names could not refer to other variables yet.
    This has been made possible on 2000-05-11, when the "cool magic" was
    added that allows assigning to VAR.${param}.

--- usr.bin/make/unit-tests/var-op-expand.mk
+++ usr.bin/make/unit-tests/var-op-expand.mk
@@ -1,4 +1,4 @@
-# $NetBSD: var-op-expand.mk,v 1.3 2020/11/08 13:46:15 rillig Exp $
+# $NetBSD: var-op-expand.mk,v 1.4 2020/11/08 14:00:52 rillig Exp $
 #
 # Tests for the := variable assignment operator, which expands its
 # right-hand side.
@@ -7,7 +7,7 @@
 
 # XXX: edge case: When a variable name refers to an undefined variable, the
 # behavior differs between the '=' and the ':=' assignment operators.
-# This bug exists since at least 1993-03-21.
+# This bug exists since var.c 1.42 from 2000-05-11.
 #
 # The '=' operator expands the undefined variable to an empty string, thus
 # assigning to VAR_ASSIGN_.  In the name of variables to be set, it should
