55f102493adac107655694cb5afc33949882be54
martin <martin@NetBSD.org>
2020-11-08 08:54:50 +0000

    PR 55338: re-enable threxec test for more test coverage.

--- tests/lib/librumpclient/t_exec.sh
+++ tests/lib/librumpclient/t_exec.sh
@@ -1,4 +1,4 @@
-#       $NetBSD: t_exec.sh,v 1.11 2020/08/29 05:46:34 martin Exp $
+#       $NetBSD: t_exec.sh,v 1.12 2020/11/08 08:54:50 martin Exp $
 #
 # Copyright (c) 2011 The NetBSD Foundation, Inc.
 # All rights reserved.
@@ -135,7 +135,7 @@ threxec_head()
 
 threxec_body()
 {
-	atf_skip "PR 55338: triggers a scheduler bug"
+	# atf_skip "PR 55338: triggers a scheduler bug"
 	atf_check -s exit:0 rump_server ${RUMP_SERVER}
 	atf_check -s exit:0 $(atf_get_srcdir)/h_execthr
 }
