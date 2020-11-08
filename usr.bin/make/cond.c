d6e34e9efe4b100178cebb0055885ced96e434e1
rillig <rillig@NetBSD.org>
2020-11-08 22:44:05 +0000

    make(1): improve local variable name in EvalNotEmpty

--- usr.bin/make/cond.c
+++ usr.bin/make/cond.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: cond.c,v 1.191 2020/11/08 22:41:40 rillig Exp $	*/
+/*	$NetBSD: cond.c,v 1.192 2020/11/08 22:44:05 rillig Exp $	*/
 
 /*
  * Copyright (c) 1988, 1989, 1990 The Regents of the University of California.
@@ -93,7 +93,7 @@
 #include "dir.h"
 
 /*	"@(#)cond.c	8.2 (Berkeley) 1/2/94"	*/
-MAKE_RCSID("$NetBSD: cond.c,v 1.191 2020/11/08 22:41:40 rillig Exp $");
+MAKE_RCSID("$NetBSD: cond.c,v 1.192 2020/11/08 22:44:05 rillig Exp $");
 
 /*
  * The parsing of conditional expressions is based on this grammar:
@@ -534,15 +534,15 @@ If_Eval(const struct If *if_info, const char *arg, size_t arglen)
 static Boolean
 EvalNotEmpty(CondParser *par, const char *value, Boolean quoted)
 {
-    double left;
+    double num;
 
     /* For .ifxxx "...", check for non-empty string. */
     if (quoted)
 	return value[0] != '\0';
 
     /* For .ifxxx <number>, compare against zero */
-    if (TryParseNumber(value, &left))
-	return left != 0.0;
+    if (TryParseNumber(value, &num))
+	return num != 0.0;
 
     /* For .if ${...}, check for non-empty string (defProc is ifdef). */
     if (par->if_info->form[0] == '\0')
