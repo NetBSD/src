d8c7eb6deb5138bde8cecd9bd5c6970018d9b993
rillig <rillig@NetBSD.org>
2020-11-09 20:57:36 +0000

    make(1): add test for repeating the leading '@' in shell commands

--- usr.bin/make/unit-tests/sh-leading-at.mk
+++ usr.bin/make/unit-tests/sh-leading-at.mk
@@ -1,4 +1,4 @@
-# $NetBSD: sh-leading-at.mk,v 1.3 2020/08/22 09:16:08 rillig Exp $
+# $NetBSD: sh-leading-at.mk,v 1.4 2020/11/09 20:57:36 rillig Exp $
 #
 # Tests for shell commands preceded by an '@', to suppress printing
 # the command to stdout.
@@ -8,3 +8,6 @@ all:
 	@echo 'ok'
 	@ echo 'space after @'
 	echo 'echoed'
+	# The leading '@' can be repeated.
+	@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+	@@@echo '3'
