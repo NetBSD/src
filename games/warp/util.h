653de20730e53e54359e8993f9e392607aa0c475
kamil <kamil@NetBSD.org>
2020-11-10 00:02:11 +0000

    Reset CVS keywords

--- games/warp/util.h
+++ games/warp/util.h
@@ -1,24 +1,6 @@
-/* $Header: /cvsroot/src/games/warp/util.h,v 1.1 2020/11/09 23:37:05 kamil Exp $ */
+/* Header: util.h,v 7.0 86/10/08 15:14:37 lwall Exp */
 
-/* $Log: util.h,v $
-/* Revision 1.1  2020/11/09 23:37:05  kamil
-/* Add Warp Kit, Version 7.0 by Larry Wall
-/*
-/* Warp is a real-time space war game that doesn't get boring very quickly.
-/* Read warp.doc and the manual page for more information.
-/*
-/* games/warp originally distributed with 4.3BSD-Reno, is back to the BSD
-/* world via NetBSD. Its remnants were still mentioned in games/Makefile.
-/*
-/* Larry Wall, the original author and the copyright holder, generously
-/* donated the game and copyright to The NetBSD Foundation, Inc.
-/*
-/* Import the game sources as-is from 4.3BSD-Reno, with the cession
-/* of the copyright and license to BSD-2-clause NetBSD-style.
-/*
-/* Signed-off-by: Larry Wall <larry@wall.org>
-/* Signed-off-by: Kamil Rytarowski <kamil@netbsd.org>
-/*
+/* Log:	util.h,v
  * Revision 7.0  86/10/08  15:14:37  lwall
  * Split into separate files.  Added amoebas and pirates.
  * 
