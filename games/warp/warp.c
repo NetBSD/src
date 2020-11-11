286a3ee2f8d4f814f8287e925e65b72ee6fbb9b4
kamil <kamil@NetBSD.org>
2020-11-11 11:54:42 +0000

    Fix the old leftover license note
    Larry Wall: "I outgrew commercial-free licensing about 30 years ago."

--- games/warp/warp.c
+++ games/warp/warp.c
@@ -5,17 +5,33 @@ char rcsid[] = "@(#)Header: warp.c,v 7.0.1.3 86/12/12 17:07:44 lwall Exp";
  *	helpers: Jonathan and Mark Biggar, and Dan Faigin
  *	special thanks to my sweetie Gloria who suggested the Planet Crusher
  *
- *	Copyright (C) 1986, Larry Wall
- *
- *	This program may be copied as long as this copyright notice is
- *	included, and as long as it is not being copied for purposes
- *	of profit.  If you want to modify this program in any way other
- *	than normal configuration changes, common decency would suggest
- *	that you also modify the name of the program so that my good name
- *	(what there is of it) is not impugned.  (Calling it something like
- *	"warpx" or "superwarp" would be fine.)  Also, give it another
- *	WARPDIR so that the scoreboards don't get confused.
- *
+ * Copyright (c) 2020 The NetBSD Foundation, Inc.
+ * All rights reserved.
+ * 
+ * This code is derived from software contributed to The NetBSD Foundation
+ * by Larry Wall.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * 
  * version 5.0  04/20/83
  *         5.1  05/05/83	various tidbits
  *	   5.2  05/12/83	VAX -> vax, ifdef'ed a SIGCONT
