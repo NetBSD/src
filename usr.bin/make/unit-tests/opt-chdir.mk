fca591a0a134a09471005a66f33cea29ba7ade34
rillig <rillig@NetBSD.org>
2020-11-14 22:17:29 +0000

    make(1): document since when the -C option is available

--- usr.bin/make/unit-tests/opt-chdir.mk
+++ usr.bin/make/unit-tests/opt-chdir.mk
@@ -1,6 +1,9 @@
-# $NetBSD: opt-chdir.mk,v 1.2 2020/08/16 14:25:16 rillig Exp $
+# $NetBSD: opt-chdir.mk,v 1.3 2020/11/14 22:17:29 rillig Exp $
 #
-# Tests for the -C command line option.
+# Tests for the -C command line option, which changes the directory at the
+# beginning.
+#
+# This option is available since 2009-08-27.
 
 # TODO: Implementation
 
