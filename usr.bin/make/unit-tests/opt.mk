6631621039ba98f11c13346a7b5634a75996c05a
rillig <rillig@NetBSD.org>
2020-11-15 06:19:34 +0000

    make(1): add test for passing MAKEFLAGS to sub-makes

--- usr.bin/make/unit-tests/opt.mk
+++ usr.bin/make/unit-tests/opt.mk
@@ -1,12 +1,16 @@
-# $NetBSD: opt.mk,v 1.4 2020/11/15 06:06:19 sjg Exp $
+# $NetBSD: opt.mk,v 1.5 2020/11/15 06:19:34 rillig Exp $
 #
 # Tests for the command line options.
-
-# TODO: Implementation
 
 .MAKEFLAGS: -d0			# make stdout line-buffered
 
 all: .IGNORE
+	# The options from the top-level make are passed to the sub-makes via
+	# the environment variable MAKEFLAGS.  This is where the " -r -k -d 0"
+	# comes from.  See MainParseArg.
+	${MAKE} -V MAKEFLAGS
+	@echo
+
 	# Just to see how the custom argument parsing code reacts to a syntax
 	# error.  The colon is used in the options string, marking an option
 	# that takes arguments.  It is not an option by itself, though.
