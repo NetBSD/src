68a89b2ecbf0279117b9867db3d88c22cd0badcd
knakahara <knakahara@NetBSD.org>
2020-11-17 04:50:29 +0000

    Add new spin mutex to avoid race between ixgbe_msix_admin() and ixgbe_handle_admin().
    At first, it seems "IXGBE_WRITE_REG(hw, IXGBE_EIMC, IXGBE_EIMC_OTHER)"
    cannot stop interrupts, because 31th bit is reserved for 82598, 82599,
    X540 and X550.  So, the current following design
        (1) ixgbe_msix_admin() disables interrupts
        (2) ixgbe_msix_admin() calls workqueue_enqueue() for ixgbe_handle_admin()
        (3) ixgbe_handle_admin() does interrupt processing
        (4) after ixgbe_handle_admin() has done all interrupt processings,
            ixgbe_handle_admin() enables interrupts
    does not work correctly, that is, interrupts can be lost while
    ixgbe_handle_admin() is running.
    
    To fix that, add new spin mutex(adapter->admmin_mtx) which protects
    atomically the following two members.
        - adapter->admin_pending
        - adapter->task_requests
    
    The unnecessary "IXGBE_WRITE_REG(hw, IXGBE_EIMC, IXGBE_EIMC_OTHER)"
    code will be removed later.
    
    Reviewed and tested by hikaru@n.o and msaitoh@n.o, thanks.

--- sys/dev/pci/ixgbe/ixgbe.h
+++ sys/dev/pci/ixgbe/ixgbe.h
@@ -1,4 +1,4 @@
-/* $NetBSD: ixgbe.h,v 1.71 2020/08/31 11:19:54 msaitoh Exp $ */
+/* $NetBSD: ixgbe.h,v 1.72 2020/11/17 04:50:29 knakahara Exp $ */
 
 /******************************************************************************
   SPDX-License-Identifier: BSD-3-Clause
@@ -523,6 +523,12 @@ struct adapter {
 	struct work		admin_wc;
 	u_int			admin_pending;
 	volatile u32		task_requests;
+	kmutex_t		admin_mtx; /* lock for admin_pending, task_request */
+					   /*
+					    * Don't acquire this mutex while
+					    * holding rx_mtx or tx_mtx, and
+					    * vice versa.
+					    */
 
 	bool			txrx_use_workqueue;
 
