fced1acd2a26faabbbe22284186339cf5f3c4fca
rillig <rillig@NetBSD.org>
2020-11-17 20:11:02 +0000

    make(1): fix error message of failed :!cmd! modifier (since 2000-04-29)

--- usr.bin/make/unit-tests/varmod-shell.mk
+++ usr.bin/make/unit-tests/varmod-shell.mk
@@ -1,4 +1,4 @@
-# $NetBSD: varmod-shell.mk,v 1.4 2020/11/17 20:08:09 rillig Exp $
+# $NetBSD: varmod-shell.mk,v 1.5 2020/11/17 20:11:02 rillig Exp $
 #
 # Tests for the :sh variable modifier, which runs the shell command
 # given by the variable value and returns its output.
@@ -20,7 +20,7 @@
 # If the command exits with non-zero, an error message is printed.
 # XXX: Processing continues as usual though.
 #
-# Since 2000-04-29, the error message mentions the previous
+# Between 2000-04-29 and 2020-11-17, the error message mentioned the previous
 # value of the expression (which is usually an empty string) instead of the
 # command that was executed.  It's strange that such a simple bug could
 # survive such a long time.
