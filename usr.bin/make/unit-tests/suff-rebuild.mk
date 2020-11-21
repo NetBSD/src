23b3edf59341517a96dc5440b6cc0c87705f9af6
rillig <rillig@NetBSD.org>
2020-11-21 08:23:36 +0000

    make(1): rename SuffScanTargets to SuffUpdateTarget
    The word "scan" no longer applies to this function since that is done by
    SuffUpdateTargets instead.

--- usr.bin/make/unit-tests/suff-rebuild.mk
+++ usr.bin/make/unit-tests/suff-rebuild.mk
@@ -1,4 +1,4 @@
-# $NetBSD: suff-rebuild.mk,v 1.2 2020/10/18 16:12:39 rillig Exp $
+# $NetBSD: suff-rebuild.mk,v 1.3 2020/11/21 08:23:36 rillig Exp $
 #
 # Demonstrates what happens to transformation rules (called inference rules
 # by POSIX) when all suffixes are deleted.
@@ -19,7 +19,7 @@ suff-rebuild-example.a:
 .c:
 	: from c to nothing
 
-# XXX: At a quick glance, the code in SuffScanTargets looks as if it were
+# XXX: At a quick glance, the code in SuffUpdateTarget looks as if it were
 # possible to delete the suffixes in the middle of the makefile, add back
 # the suffixes from before, and have the transformation rules preserved.
 #
