4afbfe91beec32dce074c79c7fb64049d6875a3a
rillig <rillig@NetBSD.org>
2020-11-22 11:11:43 +0000

    make(1): fix type of local variable in FindDepsRegular
    The compiler cannot check these since all lists and list nodes are
    aliases to each other.
    
    Maybe it's time to add type-generic lists to the code, to delegate these
    checks to the compiler.

--- usr.bin/make/suff.c
+++ usr.bin/make/suff.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: suff.c,v 1.289 2020/11/22 10:27:56 rillig Exp $	*/
+/*	$NetBSD: suff.c,v 1.290 2020/11/22 11:11:43 rillig Exp $	*/
 
 /*
  * Copyright (c) 1988, 1989, 1990, 1993
@@ -114,7 +114,7 @@
 #include "dir.h"
 
 /*	"@(#)suff.c	8.4 (Berkeley) 3/21/94"	*/
-MAKE_RCSID("$NetBSD: suff.c,v 1.289 2020/11/22 10:27:56 rillig Exp $");
+MAKE_RCSID("$NetBSD: suff.c,v 1.290 2020/11/22 11:11:43 rillig Exp $");
 
 #define SUFF_DEBUG0(text) DEBUG0(SUFF, text)
 #define SUFF_DEBUG1(fmt, arg1) DEBUG1(SUFF, fmt, arg1)
@@ -1748,7 +1748,7 @@ FindDepsRegular(GNode *gn, CandidateList *slst)
      * that still contain variables or wildcards in their names.
      */
     {
-	SuffixListNode *ln, *nln;
+	GNodeListNode *ln, *nln;
 	for (ln = gn->children->first; ln != NULL; ln = nln) {
 	    nln = ln->next;
 	    ExpandChildren(ln, gn);
