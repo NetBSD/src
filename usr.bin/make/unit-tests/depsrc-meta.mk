17ff3ec0536279f6e6b2574933c6484db2722fcc
rillig <rillig@NetBSD.org>
2020-11-27 08:36:10 +0000

    make(1): add very basic test about meta mode
    This test is just meant to cover the existing code, it still needs to be
    cleaned up to serve as a tutorial and to highlight the really
    interesting points.

--- usr.bin/make/unit-tests/depsrc-meta.mk
+++ usr.bin/make/unit-tests/depsrc-meta.mk
@@ -1,8 +1,30 @@
-# $NetBSD: depsrc-meta.mk,v 1.2 2020/08/16 14:25:16 rillig Exp $
+# $NetBSD: depsrc-meta.mk,v 1.3 2020/11/27 08:36:11 rillig Exp $
 #
 # Tests for the special source .META in dependency declarations.
 
 # TODO: Implementation
+# TODO: Explanation
+
+.if make(actual-test)
+
+.MAKEFLAGS: -dM
+.MAKE.MODE=	meta curDirOk=true
+
+actual-test: depsrc-meta-target
+depsrc-meta-target: .META
+	@> ${.TARGET}-file
+	@rm -f ${.TARGET}-file
+
+.elif make(check-results)
+
+check-results:
+	@echo 'Targets from meta mode:'
+	@awk '/^TARGET/ { print "| " $$0 }' depsrc-meta-target.meta
+
+.else
 
 all:
-	@:;
+	@${MAKE} -f ${MAKEFILE} actual-test
+	@${MAKE} -f ${MAKEFILE} check-results
+
+.endif
