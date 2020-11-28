3161e8f86fb51ee650a4258b6120ddad6da02e96
rillig <rillig@NetBSD.org>
2020-11-28 19:22:32 +0000

    make(1): reduce pointer indirection for GNode.implicitParents

--- usr.bin/make/suff.c
+++ usr.bin/make/suff.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: suff.c,v 1.312 2020/11/28 19:20:03 rillig Exp $	*/
+/*	$NetBSD: suff.c,v 1.313 2020/11/28 19:22:32 rillig Exp $	*/
 
 /*
  * Copyright (c) 1988, 1989, 1990, 1993
@@ -114,7 +114,7 @@
 #include "dir.h"
 
 /*	"@(#)suff.c	8.4 (Berkeley) 3/21/94"	*/
-MAKE_RCSID("$NetBSD: suff.c,v 1.312 2020/11/28 19:20:03 rillig Exp $");
+MAKE_RCSID("$NetBSD: suff.c,v 1.313 2020/11/28 19:22:32 rillig Exp $");
 
 #define SUFF_DEBUG0(text) DEBUG0(SUFF, text)
 #define SUFF_DEBUG1(fmt, arg1) DEBUG1(SUFF, fmt, arg1)
@@ -1484,7 +1484,7 @@ ApplyTransform(GNode *tgn, GNode *sgn, Suffix *tsuff, Suffix *ssuff)
      * Keep track of another parent to which this node is transformed so
      * the .IMPSRC variable can be set correctly for the parent.
      */
-    Lst_Append(sgn->implicitParents, tgn);
+    Lst_Append(&sgn->implicitParents, tgn);
 
     return TRUE;
 }
