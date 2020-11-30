ff4561985eb53fcaa54e3aa9e638a15b338e0047
bouyer <bouyer@NetBSD.org>
2020-11-30 17:02:27 +0000

    Introduce smap_enable()/smap_disable() functions, to be used from C code.

--- sys/arch/amd64/include/cpufunc.h
+++ sys/arch/amd64/include/cpufunc.h
@@ -1,3 +1,6 @@
-/*	$NetBSD: cpufunc.h,v 1.19 2007/10/17 19:53:03 garbled Exp $	*/
+/*	$NetBSD: cpufunc.h,v 1.20 2020/11/30 17:02:27 bouyer Exp $	*/
 
 #include <x86/cpufunc.h>
+
+void smap_enable(void);
+void smap_disable(void);
