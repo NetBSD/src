ec050d0a6c07fddf9d79cb5bb1a085eacf35b9cd
rillig <rillig@NetBSD.org>
2020-12-06 19:00:48 +0000

    make(1): test parsing a makefile that ends in a backslash

--- usr.bin/make/unit-tests/opt-file.mk
+++ usr.bin/make/unit-tests/opt-file.mk
@@ -1,8 +1,17 @@
-# $NetBSD: opt-file.mk,v 1.2 2020/08/16 14:25:16 rillig Exp $
+# $NetBSD: opt-file.mk,v 1.3 2020/12/06 19:00:48 rillig Exp $
 #
 # Tests for the -f command line option.
 
 # TODO: Implementation
+
+all: .PHONY file-ending-in-backslash
+
+# Passing '-' as the filename reads from stdin.  This is unusual but possible.
+#
+# In the unlikely case where a file ends in a backslash instead of a newline,
+# that backslash is trimmed.  See ParseGetLine.
+file-ending-in-backslash: .PHONY
+	@printf '%s' 'VAR=value\' | ${MAKE} -r -f - -v VAR
 
 all:
 	@:;
