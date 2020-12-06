d179c16de412efcc65c12f710359ad4f24fd70f0
rillig <rillig@NetBSD.org>
2020-12-06 16:06:11 +0000

    make(1): add test for directive unexport-env

--- usr.bin/make/unit-tests/directive-unexport-env.mk
+++ usr.bin/make/unit-tests/directive-unexport-env.mk
@@ -1,4 +1,4 @@
-# $NetBSD: directive-unexport-env.mk,v 1.3 2020/11/03 17:17:31 rillig Exp $
+# $NetBSD: directive-unexport-env.mk,v 1.4 2020/12/06 16:06:11 rillig Exp $
 #
 # Tests for the .unexport-env directive.
 
@@ -7,6 +7,18 @@
 .unexport-en			# oops: misspelled
 .unexport-env			# ok
 .unexport-environment		# oops: misspelled
+
+# Before 2020-12-06, the directive unexport-env was implemented strangely.
+# According to its documentation, it does not take any arguments, but the
+# Implementation accepted variable names as arguments and produced wrong debug
+# logging for them, saying "Unexporting" for variables that at this point were
+# not exported anymore.
+.MAKEFLAGS: -dv
+UT_EXPORTED=	value
+UT_UNEXPORTED=	value
+.export UT_EXPORTED
+.unexport-env UT_EXPORTED UT_UNEXPORTED
+.MAKEFLAGS: -d0
 
 all:
 	@:;
