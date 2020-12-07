75bc909397ac0ea42e27ed41b3ea6f99a0a13d3a
martin <martin@NetBSD.org>
2020-12-07 19:52:52 +0000

    Pull up the following, requested by kre in ticket #1631:
    	external/public-domain/tz/dist/systemv          delete
    	external/public-domain/tz/dist/pacificnew       delete
    	external/public-domain/tz/dist/yearistype.sh    delete
    	external/public-domain/tz/dist/Makefile         up to 1.1.1.29
    	external/public-domain/tz/dist/NEWS             up to 1.1.1.32
    	external/public-domain/tz/dist/README           up to 1.1.1.9
    	external/public-domain/tz/dist/TZDATA_VERSION   up to 1.22
    	external/public-domain/tz/dist/africa           up to 1.1.1.23
    	external/public-domain/tz/dist/antarctica       up to 1.1.1.13
    	external/public-domain/tz/dist/asia             up to 1.1.1.27
    	external/public-domain/tz/dist/australasia      up to 1.1.1.20
    	external/public-domain/tz/dist/backward         up to 1.1.1.11
    	external/public-domain/tz/dist/backzone         up to 1.1.1.19
    	external/public-domain/tz/dist/checktab.awk     up to 1.1.1.10
    	external/public-domain/tz/dist/etcetera         up to 1.1.1.4
    	external/public-domain/tz/dist/europe           up to 1.1.1.29
    	external/public-domain/tz/dist/factory          up to 1.1.1.4
    	external/public-domain/tz/dist/iso3166.tab      up to 1.1.1.6
    	external/public-domain/tz/dist/leap-seconds.list up to 1.1.1.14
    	external/public-domain/tz/dist/leapseconds      up to 1.1.1.16
    	external/public-domain/tz/dist/leapseconds.awk  up to 1.1.1.11
    	external/public-domain/tz/dist/northamerica     up to 1.1.1.26
    	external/public-domain/tz/dist/southamerica     up to 1.1.1.17
    	external/public-domain/tz/dist/theory.html      up to 1.1.1.11
    	external/public-domain/tz/dist/version          up to 1.1.1.19
    	external/public-domain/tz/dist/ziguard.awk      up to 1.1.1.5
    	external/public-domain/tz/dist/zishrink.awk     up to 1.1.1.7
    	external/public-domain/tz/dist/zone.tab         up to 1.1.1.18
    	external/public-domain/tz/dist/zone1970.tab     up to 1.1.1.20
    	external/public-domain/tz/dist/zoneinfo2tdf.pl  up to 1.1.1.4
    	distrib/sets/lists/base/mi			(apply patch)
    	doc/3RDPARTY					(apply patch)
    	external/public-domain/tz/share/zoneinfo/Makefile	(apply patch)
    
    Update tzdate from 2018i to 2020d

--- external/public-domain/tz/dist/zoneinfo2tdf.pl
+++ external/public-domain/tz/dist/zoneinfo2tdf.pl
@@ -28,7 +28,7 @@ while (<>) {
 
   $contZone = '';
   if ($type eq 'zone') {
-    # Zone  NAME  GMTOFF  RULES/SAVE  FORMAT  [UNTIL]
+    # Zone  NAME  STDOFF  RULES/SAVE  FORMAT  [UNTIL]
     my $nfields = @fields;
     $nfields >= 5 or warn "bad zone line";
     if ($nfields > 6) {
@@ -38,7 +38,7 @@ while (<>) {
     }
     $contZone = $fields[1] if @fields > 5;
   } elsif ($type eq 'rule') {
-    # Rule  NAME  FROM  TO  TYPE  IN  ON  AT  SAVE  LETTER/S
+    # Rule  NAME  FROM  TO  -  IN  ON  AT  SAVE  LETTER/S
     @fields == 10 or warn "bad rule line";
   } elsif ($type eq 'link') {
     # Link  TARGET  LINK-NAME
