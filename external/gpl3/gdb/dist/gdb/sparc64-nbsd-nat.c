55c4b3fafb7ee2d572a177f55ccc13c97a7c0641
christos <christos@NetBSD.org>
2020-12-08 13:56:38 +0000

    include gdbarch.h

--- external/gpl3/gdb/dist/gdb/sparc64-nbsd-nat.c
+++ external/gpl3/gdb/dist/gdb/sparc64-nbsd-nat.c
@@ -18,6 +18,7 @@
    along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
 
 #include "defs.h"
+#include "gdbarch.h"
 #include "gdbcore.h"
 #include "regcache.h"
 #include "inferior.h"
