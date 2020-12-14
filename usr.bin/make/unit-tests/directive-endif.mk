5e223303e336b1fdb4788b49243361c68a7c11de
rillig <rillig@NetBSD.org>
2020-12-14 20:57:31 +0000

    make(1): add more tests for parsing .endif

--- usr.bin/make/unit-tests/directive-endif.mk
+++ usr.bin/make/unit-tests/directive-endif.mk
@@ -1,4 +1,4 @@
-# $NetBSD: directive-endif.mk,v 1.3 2020/11/12 22:40:11 rillig Exp $
+# $NetBSD: directive-endif.mk,v 1.4 2020/12/14 20:57:31 rillig Exp $
 #
 # Tests for the .endif directive.
 #
@@ -22,6 +22,25 @@
 # Comments are allowed after an '.endif'.
 .if 2
 .endif # comment
+
+# Only whitespace and comments are allowed after an '.endif', but nothing
+# else.
+# XXX: Missing error message
+.if 1
+.endif0
+
+# Only whitespace and comments are allowed after an '.endif', but nothing
+# else.
+# XXX: Missing error message
+.if 1
+.endif/
+
+# After an '.endif', no other letter must occur.  This 'endifx' is not
+# parsed as an 'endif', therefore another '.endif' must follow to balance
+# the directives.
+.if 1
+.endifx
+.endif # to close the preceding '.if'
 
 all:
 	@:;
