1d387b62c6bf39bff94fa69fa3937ffd14f4a293
rillig <rillig@NetBSD.org>
2020-12-14 20:28:09 +0000

    make(1): add test for variable expressions after a parse error
    These variable expressions don't need to be expanded, and they aren't.

--- usr.bin/make/unit-tests/cond-eof.mk
+++ usr.bin/make/unit-tests/cond-eof.mk
@@ -1,9 +1,10 @@
-# $NetBSD: cond-eof.mk,v 1.1 2020/12/14 20:23:50 rillig Exp $
+# $NetBSD: cond-eof.mk,v 1.2 2020/12/14 20:28:09 rillig Exp $
 #
 # Tests for parsing conditions, especially the end of such conditions, which
 # are represented as the token TOK_EOF.
 
 SIDE_EFFECT=	${:!echo 'side effect' 1>&2!}
+SIDE_EFFECT2=	${:!echo 'side effect 2' 1>&2!}
 
 # In the following conditions, ${SIDE_EFFECT} is the position of the first
 # parse error.  It is always fully evaluated, even if it were not necessary
@@ -11,9 +12,9 @@ SIDE_EFFECT=	${:!echo 'side effect' 1>&2!}
 # an edge case that does not occur during normal operation, therefore there
 # is no need to optimize for this case, and it would slow down the common
 # case as well.
-.if 0 ${SIDE_EFFECT}
+.if 0 ${SIDE_EFFECT} ${SIDE_EFFECT2}
 .endif
-.if 1 ${SIDE_EFFECT}
+.if 1 ${SIDE_EFFECT} ${SIDE_EFFECT2}
 .endif
-.if (0) ${SIDE_EFFECT}
+.if (0) ${SIDE_EFFECT} ${SIDE_EFFECT2}
 .endif
