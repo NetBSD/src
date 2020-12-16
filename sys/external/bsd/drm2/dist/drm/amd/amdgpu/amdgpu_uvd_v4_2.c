c2c936d38e4038489f7477ce044a9e5cc3c2d120
christos <christos@NetBSD.org>
2020-12-16 19:49:04 +0000

    interupt -> interrupt

--- sys/external/bsd/drm2/dist/drm/amd/amdgpu/amdgpu_uvd_v4_2.c
+++ sys/external/bsd/drm2/dist/drm/amd/amdgpu/amdgpu_uvd_v4_2.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: amdgpu_uvd_v4_2.c,v 1.3 2020/02/14 14:34:58 maya Exp $	*/
+/*	$NetBSD: amdgpu_uvd_v4_2.c,v 1.4 2020/12/16 19:49:05 christos Exp $	*/
 
 /*
  * Copyright 2013 Advanced Micro Devices, Inc.
@@ -25,7 +25,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: amdgpu_uvd_v4_2.c,v 1.3 2020/02/14 14:34:58 maya Exp $");
+__KERNEL_RCSID(0, "$NetBSD: amdgpu_uvd_v4_2.c,v 1.4 2020/12/16 19:49:05 christos Exp $");
 
 #include <linux/firmware.h>
 #include <drm/drmP.h>
@@ -278,7 +278,7 @@ static int uvd_v4_2_start(struct amdgpu_device *adev)
 	/* disable clock gating */
 	WREG32(mmUVD_CGC_GATE, 0);
 
-	/* disable interupt */
+	/* disable interrupt */
 	WREG32_P(mmUVD_MASTINT_EN, 0, ~(1 << 1));
 
 	/* Stall UMC and register bus before resetting VCPU */
@@ -356,7 +356,7 @@ static int uvd_v4_2_start(struct amdgpu_device *adev)
 		return r;
 	}
 
-	/* enable interupt */
+	/* enable interrupt */
 	WREG32_P(mmUVD_MASTINT_EN, 3<<1, ~(3 << 1));
 
 	/* force RBC into idle state */
