61f96bc3b0a28ee789b2c8f810c2248944d6574e
maya <maya@NetBSD.org>
2020-12-18 17:32:41 +0000

    Merge pkg-install-20201218

--- external/bsd/pkg_install/dist/lib/version.h
+++ external/bsd/pkg_install/dist/lib/version.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: version.h,v 1.18 2020/12/12 11:00:57 wiz Exp $	*/
+/*	$NetBSD: version.h,v 1.19 2020/12/18 17:32:42 maya Exp $	*/
 
 /*
  * Copyright (c) 2001 Thomas Klausner.  All rights reserved.
@@ -27,6 +27,6 @@
 #ifndef _INST_LIB_VERSION_H_
 #define _INST_LIB_VERSION_H_
 
-#define PKGTOOLS_VERSION 20201212
+#define PKGTOOLS_VERSION 20201218
 
 #endif /* _INST_LIB_VERSION_H_ */
