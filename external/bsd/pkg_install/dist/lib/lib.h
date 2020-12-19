5726130b5c863a436a9be805a50d6aab8fa72e5b
martin <martin@NetBSD.org>
2020-12-19 13:34:41 +0000

    Pull up the following, requested by maya in ticket #1155:
    	external/bsd/pkg_install/dist/add/perform.c     up to 1.8
    	external/bsd/pkg_install/dist/lib/lib.h         up to 1.11
    	external/bsd/pkg_install/dist/lib/parse-config.c up to 1.4
    	external/bsd/pkg_install/dist/lib/pkg_install.conf.5.in up to 1.4
    	external/bsd/pkg_install/dist/lib/pkgdb.c       up to 1.5
    	external/bsd/pkg_install/dist/lib/plist.c       up to 1.6
    	external/bsd/pkg_install/dist/lib/version.h     up to 1.19
    	doc/3RDPARTY					(manually modified)
    
    Merge pkg_install 20201218.
    
    Provide silent backwards compatibility for existing package installs
    using /var/db/pkg.

--- external/bsd/pkg_install/dist/lib/lib.h
+++ external/bsd/pkg_install/dist/lib/lib.h
@@ -1,4 +1,4 @@
-/* $NetBSD: lib.h,v 1.9.4.1 2020/12/08 18:45:58 martin Exp $ */
+/* $NetBSD: lib.h,v 1.9.4.2 2020/12/19 13:34:42 martin Exp $ */
 
 /* from FreeBSD Id: lib.h,v 1.25 1997/10/08 07:48:03 charnier Exp */
 
@@ -447,6 +447,7 @@ extern const char *cert_chain_file;
 extern const char *certs_packages;
 extern const char *certs_pkg_vulnerabilities;
 extern const char *check_eol;
+extern const char *check_os_version;
 extern const char *check_vulnerabilities;
 extern const char *config_file;
 extern const char *config_pkg_dbdir;
