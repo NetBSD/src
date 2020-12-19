d0a1cd7c0d87e4787b0cc8a548881c40f4a08d74
rillig <rillig@NetBSD.org>
2020-12-19 22:33:11 +0000

    make(1): fix error message for .info/.warning/.error without argument
    Previously, the error message was "Unknown directive", which was
    obviously wrong.  The new error message is "Missing argument".

--- usr.bin/make/unit-tests/directive-info.mk
+++ usr.bin/make/unit-tests/directive-info.mk
@@ -1,12 +1,16 @@
-# $NetBSD: directive-info.mk,v 1.7 2020/12/19 12:40:00 rillig Exp $
+# $NetBSD: directive-info.mk,v 1.8 2020/12/19 22:33:11 rillig Exp $
 #
 # Tests for the .info directive.
+#
+# Until parse.c 1.502 from 2020-12-19, a missing argument to the directive
+# produced the wrong error message "Unknown directive".  Since parse.c 1.503
+# from 2020-12-19, the correct "Missing argument" is produced.
 
 # TODO: Implementation
 
 .info begin .info tests
 .inf				# misspelled
-.info				# oops: message should be "missing parameter"
+.info				# "Missing argument"
 .info message
 .info		indented message
 .information
