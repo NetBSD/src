ee77195b6a8e56c0e287e792fd30b116770f447b
rillig <rillig@NetBSD.org>
2020-12-22 20:10:21 +0000

    make(1): allow .undef to undefine multiple variables at once
    Since make doesn't support variable names containing spaces, this edge
    case is not enough reason to stop this feature.  Having multiple
    variable names as arguments nicely aligns with other directives such as
    .for and .export.

--- usr.bin/make/unit-tests/directive-undef.mk
+++ usr.bin/make/unit-tests/directive-undef.mk
@@ -1,18 +1,23 @@
-# $NetBSD: directive-undef.mk,v 1.8 2020/12/19 22:10:18 rillig Exp $
+# $NetBSD: directive-undef.mk,v 1.9 2020/12/22 20:10:21 rillig Exp $
 #
 # Tests for the .undef directive.
 #
 # See also:
 #	directive-misspellings.mk
 
-# As of 2020-07-28, .undef only undefines the first variable.
-# All further variable names are silently ignored.
-# See parse.c, string literal "undef".
+# Before var.c 1.737 from 2020-12-19, .undef only undefined the first
+# variable, silently skipping all further variable names.
+#
+# Before var.c 1.761 from 2020-12-22, .undef complained about too many
+# arguments.
+#
+# Since var.c 1.761 from 2020-12-22, .undef handles multiple variable names
+# just like the .export directive.
 1=		1
 2=		2
 3=		3
 .undef 1 2 3
-.if ${1:U_}${2:U_}${3:U_} != _23
+.if ${1:U_}${2:U_}${3:U_} != ___
 .  warning $1$2$3
 .endif
 
@@ -29,41 +34,39 @@
 .undef ${:U}
 
 
-# The argument of .undef is a single word, delimited by whitespace, without
-# any possibility of escaping or having variable expressions containing
-# spaces.  This word is then expanded exactly once, and the expanded string
-# is the single variable name.  This allows variable names to contain spaces,
-# as well as unbalanced single and double quotes.
+# The argument of .undef is first expanded exactly once and then split into
+# words, just like everywhere else.  This prevents variables whose names
+# contain spaces or unbalanced 'single' or "double" quotes from being
+# undefined, but these characters do not appear in variables names anyway.
 1=		1
 2=		2
 3=		3
 ${:U1 2 3}=	one two three
 VARNAMES=	1 2 3
 .undef ${VARNAMES}		# undefines the variable "1 2 3"
-.if defined(${:U1 2 3})
+.if !defined(${:U1 2 3})
 .  error
 .endif
-.if ${1}${2}${3} != "123"	# these are still defined
+.if ${1:U_}${2:U_}${3:U_} != "___"	# these are still defined
 .  error
 .endif
-.undef 1
-.undef 2
-.undef 3
 
 
-# It must be possible to undefine variables whose name includes spaces.
+# A variable named " " cannot be undefined.  There's no practical use case
+# for such variables anyway.
 SPACE=		${:U }
 ${SPACE}=	space
 .if !defined(${SPACE})
 .  error
 .endif
 .undef ${SPACE}
-.if defined(${SPACE})
+.if !defined(${SPACE})
 .  error
 .endif
 
 
-# It must be possible to undefine variables whose name includes dollars.
+# A variable named "$" can be undefined since the argument to .undef is
+# expanded exactly once, before being split into words.
 DOLLAR=		$$
 ${DOLLAR}=	dollar
 .if !defined(${DOLLAR})
@@ -73,6 +76,14 @@ ${DOLLAR}=	dollar
 .if defined(${DOLLAR})
 .  error
 .endif
+
+
+# Since var.c 1.762 from 2020-12-22, parse errors in the argument should be
+# properly detected and should stop the .undef directive from doing any work.
+#
+# As of var.c 1.762, this doesn't happen though because the error handling
+# in Var_Parse and Var_Subst is not done properly.
+.undef ${VARNAMES:L:Z}
 
 
 all:
