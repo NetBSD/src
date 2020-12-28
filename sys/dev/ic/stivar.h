cbcf134c84ee702df913a93b67be814baf831d14
martin <martin@NetBSD.org>
2020-12-28 20:10:04 +0000

    Pull up following revision(s) (requested by tsutsui in ticket #1162):
    	sys/dev/ic/stivar.h: revision 1.11
    	sys/dev/ic/sti.c: revision 1.23
    	sys/dev/ic/sti.c: revision 1.24
    	sys/dev/ic/sti.c: revision 1.25
    	sys/arch/hp300/dev/sti_sgc.c: revision 1.4
    
    Pull the latest OpenBSD sti(4) changes for bitmap framebuffer support.
    - bitmap and colormap ops based on old HP ngle X11 driver:
      http://cvsweb.openbsd.org/cgi-bin/cvsweb/src/sys/dev/ic/sti.c#rev1.76
      > Work-in-progress support for non-accelerated X11 on *some* sti(4)
      > frame buffers; based upon the old HP ngle X11 driver.
      > Currently limited to CRX (720/735/750), Timber (710, old 715),
      > Artist (712, 715) and EG (B-series), however the
      > colormap isn't set up correctly on Timber and EG yet.
      >
      > Joint work with Artem Falcon, now in good enough shape to be worked further
      > in the tree.
    - misc other cosmetic changes to reduce diffs
    No particular comments on port-hp300@ and port-hppa@:
     https://mail-index.netbsd.org/port-hp300/2020/12/19/msg000184.html
     https://mail-index.netbsd.org/port-hp300/2020/12/20/msg000185.html
    
    The MD hp300 attachment for SGC CRX (A1659-66001) will be committed
    separately.
    
    Add bitmap access ops support for SGC CRX (A1659-66001) framebuffer.
    Also modify existing 425e EVRX attachment to use updated MI sti(4) ops
    more efficiently.
    
    The Xorg server and mlterm-wscons (that support wsdisplay bitmap) work
    fine on SGC hp425t.
    
    No particular comments on port-hp300@ and port-hppa@:
     https://mail-index.netbsd.org/port-hp300/2020/12/19/msg000184.html
     https://mail-index.netbsd.org/port-hp300/2020/12/20/msg000185.html
    
    Special thanks to Miod Vallat again, for contributing the SGC CRX
    framebuffer with the SGC connector and flexible cable for HP9000/425t.
    
    He also contributed DIO-II "Hyperion" monochrome framebuffer and
    1 plane grayscale SGC GRX (A1924-66001), and I've confirmed hyper(4)
    just works even with Xorg server.  I will try GRX as the next project.
    
    Handle WSSCREEN_REVERSE properly.  Based on OpenBSD's sti(4).
    
    Also remove WSSCREEN_UNDERLINE from capabilities that is not handled
    by sti(4) ROM routines.
    
    Tested on HP9000/425t with CRX.
    
    Ignore WSDISPLAYIO_PUTCMAP in WSDISPLAYIO_MODE_EMUL, i.e. text mode.
    
    The hardware palette settings are handled by the STI ROM in STI_TEXTMODE
    and changing cmap could cause mangled text colors at least on CRX on 425t.
    
    Updating CMAP in EMUL mode isn't expected anyway.
    
    Fixes "red or invisible text" after exiting mlterm-wscons on A1659 CRX.

--- sys/dev/ic/stivar.h
+++ sys/dev/ic/stivar.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: stivar.h,v 1.9.34.1 2020/05/18 17:52:22 martin Exp $	*/
+/*	$NetBSD: stivar.h,v 1.9.34.2 2020/12/28 20:10:04 martin Exp $	*/
 
 /*	$OpenBSD: stivar.h,v 1.24 2009/02/06 22:51:04 miod Exp $	*/
 
@@ -42,11 +42,15 @@ struct sti_rom {
 
 	bus_space_tag_t		 iot, memt;	/* XXX iot unused */
 	bus_space_handle_t	 romh;
+	bus_space_handle_t	 regh[STI_REGION_MAX];
 	bus_addr_t		*bases;
 
 	struct sti_dd		 rom_dd;	/* in word format */
-
 	vaddr_t			 rom_code;
+
+	/*
+	 * ROM-provided function pointers
+	 */
 	sti_init_t		 init;
 	sti_mgmt_t		 mgmt;
 	sti_unpmv_t		 unpmv;
@@ -98,10 +102,20 @@ struct sti_screen {
 	struct	wsscreen_descr	 scr_wsd;
 	const struct wsscreen_descr	*scr_scrlist[1];
 	struct	wsscreen_list	 scr_screenlist;
+
+	/*
+	 * Board-specific function data and pointers
+	 */
+	void			(*setupfb)(struct sti_screen *);
+	int			(*putcmap)(struct sti_screen *, u_int, u_int);
+
+	uint32_t		reg10_value;
+	uint32_t		reg12_value;
+	bus_addr_t		cmap_finish_register;
 };
 
 /*
- * STI Device state
+ * STI device state
  */
 struct sti_softc {
 	device_t sc_dev;
@@ -134,6 +148,7 @@ u_int	sti_rom_size(bus_space_tag_t, bus_space_handle_t);
 int	sti_init(struct sti_screen *, int);
 #define	STI_TEXTMODE	0x01
 #define	STI_CLEARSCR	0x02
+#define	STI_FBMODE	0x04
 int	sti_ioctl(void *, void *, u_long, void *, int, struct lwp *);
 paddr_t	sti_mmap(void *, void *, off_t, int);
 int	sti_alloc_screen(void *, const struct wsscreen_descr *,
