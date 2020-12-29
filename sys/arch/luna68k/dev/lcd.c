25f9f3c34d33801b0c1c43bb79841169cc28da85
tsutsui <tsutsui@NetBSD.org>
2020-12-29 17:17:14 +0000

    Make local functions static.

--- sys/arch/luna68k/dev/lcd.c
+++ sys/arch/luna68k/dev/lcd.c
@@ -1,4 +1,4 @@
-/* $NetBSD: lcd.c,v 1.11 2019/06/30 05:04:48 tsutsui Exp $ */
+/* $NetBSD: lcd.c,v 1.12 2020/12/29 17:17:14 tsutsui Exp $ */
 /* $OpenBSD: lcd.c,v 1.7 2015/02/10 22:42:35 miod Exp $ */
 
 /*-
@@ -32,7 +32,7 @@
 
 #include <sys/cdefs.h>		/* RCS ID & Copyright macro defns */
 
-__KERNEL_RCSID(0, "$NetBSD: lcd.c,v 1.11 2019/06/30 05:04:48 tsutsui Exp $");
+__KERNEL_RCSID(0, "$NetBSD: lcd.c,v 1.12 2020/12/29 17:17:14 tsutsui Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -82,10 +82,10 @@ struct pio {
 static int  lcd_match(device_t, cfdata_t, void *);
 static void lcd_attach(device_t, device_t, void *);
 
-dev_type_open(lcdopen);
-dev_type_close(lcdclose);
-dev_type_write(lcdwrite);
-dev_type_ioctl(lcdioctl);
+static dev_type_open(lcdopen);
+static dev_type_close(lcdclose);
+static dev_type_write(lcdwrite);
+static dev_type_ioctl(lcdioctl);
 
 const struct cdevsw lcd_cdevsw = {
 	.d_open     = lcdopen,
@@ -111,11 +111,11 @@ struct lcd_softc {
 CFATTACH_DECL_NEW(lcd, sizeof(struct lcd_softc),
     lcd_match, lcd_attach, NULL, NULL);
 
-void lcdbusywait(void);
-void lcdput(int);
-void lcdctrl(int);
-void lcdshow(char *);
-void greeting(void);
+static void lcdbusywait(void);
+static void lcdput(int);
+static void lcdctrl(int);
+static void lcdshow(char *);
+static void greeting(void);
 			       /* "1234567890123456" */
 static char lcd_boot_message1[] = " NetBSD/luna68k ";
 static char lcd_boot_message2[] = "   SX-9100/DT   ";
@@ -148,7 +148,7 @@ lcd_attach(device_t parent, device_t self, void *aux)
 /*
  * open/close/write/ioctl
  */
-int
+static int
 lcdopen(dev_t dev, int flags, int fmt, struct lwp *l)
 {
 	int unit;
@@ -165,7 +165,7 @@ lcdopen(dev_t dev, int flags, int fmt, struct lwp *l)
 	return 0;
 }
 
-int
+static int
 lcdclose(dev_t dev, int flags, int fmt, struct lwp *l)
 {
 	int unit;
@@ -178,7 +178,7 @@ lcdclose(dev_t dev, int flags, int fmt, struct lwp *l)
 	return 0;
 }
 
-int
+static int
 lcdwrite(dev_t dev, struct uio *uio, int flag)
 {
 	int error;
@@ -201,7 +201,7 @@ lcdwrite(dev_t dev, struct uio *uio, int flag)
 	return 0;
 }
 
-int
+static int
 lcdioctl(dev_t dev, u_long cmd, void *addr, int flag, struct lwp *l)
 {
 	int val;
@@ -278,7 +278,7 @@ lcdioctl(dev_t dev, u_long cmd, void *addr, int flag, struct lwp *l)
 	return EPASSTHROUGH;
 }
 
-void
+static void
 lcdbusywait(void)
 {
 	struct pio *p1 = (struct pio *)OBIO_PIO1_BASE;
@@ -299,7 +299,7 @@ lcdbusywait(void)
 	splx(s);
 }	
 
-void
+static void
 lcdput(int cc)
 {
 	struct pio *p1 = (struct pio *)OBIO_PIO1_BASE;
@@ -316,7 +316,7 @@ lcdput(int cc)
 	splx(s);
 }
 
-void
+static void
 lcdctrl(int cc)
 {
 	struct pio *p1 = (struct pio *)OBIO_PIO1_BASE;
@@ -333,7 +333,7 @@ lcdctrl(int cc)
 	splx(s);
 }
 
-void
+static void
 lcdshow(char *s)
 {
 	int cc;
@@ -342,7 +342,7 @@ lcdshow(char *s)
 		lcdput(cc);
 }
 
-void
+static void
 greeting(void)
 {
 
