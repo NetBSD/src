c5851e5bb68b73c1dd12ce44f9019e467691126e
rillig <rillig@NetBSD.org>
2021-01-02 16:55:45 +0000

    lint: add test for message 308 "Invalid type %s for _Complex"

--- tests/usr.bin/xlint/lint1/msg_308.c
+++ tests/usr.bin/xlint/lint1/msg_308.c
@@ -1,7 +1,13 @@
-/*	$NetBSD: msg_308.c,v 1.1 2021/01/02 10:22:44 rillig Exp $	*/
+/*	$NetBSD: msg_308.c,v 1.2 2021/01/02 16:55:45 rillig Exp $	*/
 # 3 "msg_308.c"
 
 // Test for message: Invalid type %s for _Complex [308]
+
+float _Complex float_complex;
+double _Complex double_complex;
+long double _Complex long_double_complex;
+// FIXME: aborts: _Complex plain_complex;
+// FIXME: aborts: int _Complex int_complex;
 
 TODO: "Add example code that triggers the above message."
 TODO: "Add example code that almost triggers the above message."
