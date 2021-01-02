70d3a77d9bda7bee3f817f0a33b0bdcff9795d23
rillig <rillig@NetBSD.org>
2021-01-02 18:06:01 +0000

    lint: add a few more tests
    No serious bugs found this time.

--- tests/usr.bin/xlint/lint1/msg_001.c
+++ tests/usr.bin/xlint/lint1/msg_001.c
@@ -1,7 +1,8 @@
-/*	$NetBSD: msg_001.c,v 1.1 2021/01/02 10:22:42 rillig Exp $	*/
+/*	$NetBSD: msg_001.c,v 1.2 2021/01/02 18:06:01 rillig Exp $	*/
 # 3 "msg_001.c"
 
 // Test for message: old style declaration; add int [1]
 
-TODO: "Add example code that triggers the above message."
-TODO: "Add example code that almost triggers the above message."
+old_style = 1;
+
+int new_style = 1;
