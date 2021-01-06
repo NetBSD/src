04d12c4699977097d8b9ec70aefd151a3ff94bed
rin <rin@NetBSD.org>
2021-01-06 08:07:36 +0000

    Sort headers. No functional changes intended.

--- sys/arch/powerpc/ibm4xx/ibm4xx_machdep.c
+++ sys/arch/powerpc/ibm4xx/ibm4xx_machdep.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: ibm4xx_machdep.c,v 1.32 2020/07/06 13:10:19 rin Exp $	*/
+/*	$NetBSD: ibm4xx_machdep.c,v 1.33 2021/01/06 08:07:36 rin Exp $	*/
 /*	Original: ibm40x_machdep.c,v 1.3 2005/01/17 17:19:36 shige Exp $ */
 
 /*
@@ -68,7 +68,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: ibm4xx_machdep.c,v 1.32 2020/07/06 13:10:19 rin Exp $");
+__KERNEL_RCSID(0, "$NetBSD: ibm4xx_machdep.c,v 1.33 2021/01/06 08:07:36 rin Exp $");
 
 #include "ksyms.h"
 
@@ -79,10 +79,10 @@ __KERNEL_RCSID(0, "$NetBSD: ibm4xx_machdep.c,v 1.32 2020/07/06 13:10:19 rin Exp 
 #endif
 
 #include <sys/param.h>
-#include <sys/msgbuf.h>
-#include <sys/proc.h>
 #include <sys/cpu.h>
 #include <sys/ksyms.h>
+#include <sys/msgbuf.h>
+#include <sys/proc.h>
 
 #include <uvm/uvm_extern.h>
 
@@ -96,13 +96,13 @@ __KERNEL_RCSID(0, "$NetBSD: ibm4xx_machdep.c,v 1.32 2020/07/06 13:10:19 rin Exp 
 #endif
 
 #include <machine/powerpc.h>
-#include <powerpc/pcb.h>
 #include <machine/trap.h>
 
+#include <powerpc/pcb.h>
 #include <powerpc/spr.h>
-#include <powerpc/ibm4xx/spr.h>
 
 #include <powerpc/ibm4xx/cpu.h>
+#include <powerpc/ibm4xx/spr.h>
 
 /*
  * Global variables used here and there
