e8f7d0da27c01ef46bdbbbfa518577da1623bab5
rillig <rillig@NetBSD.org>
2021-01-07 18:37:41 +0000

    lint: add tests for some messages

--- tests/usr.bin/xlint/lint1/msg_006.c
+++ tests/usr.bin/xlint/lint1/msg_006.c
@@ -1,7 +1,7 @@
-/*	$NetBSD: msg_006.c,v 1.1 2021/01/02 10:22:42 rillig Exp $	*/
+/*	$NetBSD: msg_006.c,v 1.2 2021/01/07 18:37:41 rillig Exp $	*/
 # 3 "msg_006.c"
 
 // Test for message: use 'double' instead of 'long float' [6]
 
-TODO: "Add example code that triggers the above message."
-TODO: "Add example code that almost triggers the above message."
+long float x;
+double x;
