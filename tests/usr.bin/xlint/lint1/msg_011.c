d253c54c1469d7ebd7f42d3194fa21731cda5086
rillig <rillig@NetBSD.org>
2021-01-07 19:11:58 +0000

    lint: revert test for message 11
    That test only produced other messages, but not message 11.

--- tests/usr.bin/xlint/lint1/msg_011.c
+++ tests/usr.bin/xlint/lint1/msg_011.c
@@ -1,24 +1,7 @@
-/*	$NetBSD: msg_011.c,v 1.2 2021/01/07 18:37:41 rillig Exp $	*/
+/*	$NetBSD: msg_011.c,v 1.3 2021/01/07 19:11:58 rillig Exp $	*/
 # 3 "msg_011.c"
 
 // Test for message: bit-field initializer out of range [11]
 
-struct example {
-	int bit_field: 4;
-} example_var[] = {
-    { 2 },
-    { 1 + 1 },
-    { 1ULL << 40 },
-    { 1LL << 40 },
-    { -16 - 1 },
-};
-
-struct example_u {
-	unsigned int bit_field: 4;
-} example_var_u[] = {
-    { 2 },
-    { 1 + 1 },
-    { 1ULL << 40 },
-    { 1LL << 40 },
-    { -16 - 1 },
-};
+TODO: "Add example code that triggers the above message."
+TODO: "Add example code that almost triggers the above message."
