36dc3770fc8bc0bfa1ff44aeab4d2bc7a4a7b010
rillig <rillig@NetBSD.org>
2021-01-09 14:37:16 +0000

    lint: add tests for messages 117, 120, 125, 127

--- tests/usr.bin/xlint/lint1/msg_120.c
+++ tests/usr.bin/xlint/lint1/msg_120.c
@@ -1,7 +1,36 @@
-/*	$NetBSD: msg_120.c,v 1.1 2021/01/02 10:22:43 rillig Exp $	*/
+/*	$NetBSD: msg_120.c,v 1.2 2021/01/09 14:37:16 rillig Exp $	*/
 # 3 "msg_120.c"
 
 // Test for message: bitwise operation on signed value nonportable [120]
 
-TODO: "Add example code that triggers the above message."
-TODO: "Add example code that almost triggers the above message."
+/* lint1-extra-flags: -p */
+
+int
+shr(int a, int b)
+{
+	return a >> b;
+}
+
+int
+shr_lhs_constant_positive(int a)
+{
+	return 0x1234 >> a;
+}
+
+int
+shr_lhs_constant_negative(int a)
+{
+	return -0x1234 >> a;
+}
+
+int
+shr_rhs_constant_positive(int a)
+{
+	return a >> 0x1234;
+}
+
+int
+shr_rhs_constant_negative(int a)
+{
+	return a >> -0x1234;
+}
