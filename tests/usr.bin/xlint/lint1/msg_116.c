71043049ce74b3702d7bcbffe0df47810160d8e4
rillig <rillig@NetBSD.org>
2021-01-09 22:55:36 +0000

    lint: add test for 116 "illegal pointer subtraction"

--- tests/usr.bin/xlint/lint1/msg_116.c
+++ tests/usr.bin/xlint/lint1/msg_116.c
@@ -1,10 +1,34 @@
-/*	$NetBSD: msg_116.c,v 1.2 2021/01/08 21:25:03 rillig Exp $	*/
+/*	$NetBSD: msg_116.c,v 1.3 2021/01/09 22:55:36 rillig Exp $	*/
 # 3 "msg_116.c"
 
 // Test for message: illegal pointer subtraction [116]
 
-unsigned long
+/*
+ * Subtracting an int pointer from a double pointer does not make sense.
+ * The result cannot be reasonably defined since it is "the difference of
+ * the subscripts of the two array elements" (C99 6.5.5p9), and these two
+ * pointers cannot point to the same array.
+ */
+_Bool
 example(int *a, double *b)
 {
-	return a - b;
+	return a - b > 0;
+}
+
+/*
+ * Even though signed char and unsigned char have the same size,
+ * their pointer types are still considered incompatible.
+ *
+ * C99 6.5.5p9
+ */
+_Bool
+subtract_character_pointers(signed char *scp, unsigned char *ucp)
+{
+	return scp - ucp > 0;
+}
+
+_Bool
+subtract_const_pointer(const char *ccp, char *cp)
+{
+	return ccp - cp > 0;
 }
