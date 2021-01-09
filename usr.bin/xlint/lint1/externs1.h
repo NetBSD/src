4046862eac8b38d8b56e5fe345d048c198fb08d1
rillig <rillig@NetBSD.org>
2021-01-09 19:13:17 +0000

    lint: change return type of typeok to bool
    No functional change.

--- usr.bin/xlint/lint1/externs1.h
+++ usr.bin/xlint/lint1/externs1.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: externs1.h,v 1.49 2021/01/09 13:12:13 rillig Exp $	*/
+/*	$NetBSD: externs1.h,v 1.50 2021/01/09 19:13:17 rillig Exp $	*/
 
 /*
  * Copyright (c) 1994, 1995 Jochen Pohl
@@ -204,7 +204,7 @@ extern	tnode_t	*new_string_node(strg_t *);
 extern	sym_t	*struct_or_union_member(tnode_t *, op_t, sym_t *);
 extern	tnode_t	*build(op_t, tnode_t *, tnode_t *);
 extern	tnode_t	*cconv(tnode_t *);
-extern	int	typeok(op_t, int, tnode_t *, tnode_t *);
+extern	bool	typeok(op_t, int, tnode_t *, tnode_t *);
 extern	tnode_t	*promote(op_t, int, tnode_t *);
 extern	tnode_t	*convert(op_t, int, type_t *, tnode_t *);
 extern	void	cvtcon(op_t, int, type_t *, val_t *, val_t *);
