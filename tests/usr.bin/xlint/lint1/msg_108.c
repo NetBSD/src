a4a5a880c161ca301eb0e6b765cf38d39d52e157
rillig <rillig@NetBSD.org>
2021-01-09 17:02:19 +0000

    lint: add test for message 108, which currently crashes

--- tests/usr.bin/xlint/lint1/msg_108.c
+++ tests/usr.bin/xlint/lint1/msg_108.c
@@ -1,7 +1,22 @@
-/*	$NetBSD: msg_108.c,v 1.1 2021/01/02 10:22:43 rillig Exp $	*/
+/*	$NetBSD: msg_108.c,v 1.2 2021/01/09 17:02:19 rillig Exp $	*/
 # 3 "msg_108.c"
 
 // Test for message: operand of '%s' has incompatible type (%s != %s) [108]
 
 TODO: "Add example code that triggers the above message."
-TODO: "Add example code that almost triggers the above message."
+TODO: "Add example code that almost triggers the above message.";
+
+struct s {
+	int member;
+};
+
+void
+example(void)
+{
+	struct s s;
+
+	// FIXME: msg_108.c(14): lint error: common/tyname.c, 190: tspec_name(0)
+	// basic_type_name (t=NOTSPEC)
+	// warn_incompatible_types (op=COMPL, lt=STRUCT, rt=NOTSPEC)
+	//s = ~s;
+}
