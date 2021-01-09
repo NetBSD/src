cf455f94667191cc786c441fe034c76bf5c61e61
rillig <rillig@NetBSD.org>
2021-01-09 15:32:06 +0000

    lint: add test for message 160

--- tests/usr.bin/xlint/lint1/msg_160.c
+++ tests/usr.bin/xlint/lint1/msg_160.c
@@ -1,7 +1,13 @@
-/*	$NetBSD: msg_160.c,v 1.1 2021/01/02 10:22:43 rillig Exp $	*/
+/*	$NetBSD: msg_160.c,v 1.2 2021/01/09 15:32:06 rillig Exp $	*/
 # 3 "msg_160.c"
 
 // Test for message: operator '==' found where '=' was expected [160]
 
-TODO: "Add example code that triggers the above message."
-TODO: "Add example code that almost triggers the above message."
+/* lint1-extra-flags: -h */
+
+_Bool
+both_equal_or_unequal(int a, int b, int c, int d)
+{
+	/* XXX: Why shouldn't this be legitimate? */
+	return (a == b) == (c == d);
+}
