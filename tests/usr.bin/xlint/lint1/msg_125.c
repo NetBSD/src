3a4c80c2793dc01deb7d502fd4b68fc6a91be76f
rillig <rillig@NetBSD.org>
2021-01-09 14:37:16 +0000

    lint: add tests for messages 117, 120, 125, 127

--- tests/usr.bin/xlint/lint1/msg_125.c
+++ tests/usr.bin/xlint/lint1/msg_125.c
@@ -1,7 +1,14 @@
-/*	$NetBSD: msg_125.c,v 1.1 2021/01/02 10:22:43 rillig Exp $	*/
+/*	$NetBSD: msg_125.c,v 1.2 2021/01/09 14:37:16 rillig Exp $	*/
 # 3 "msg_125.c"
 
 // Test for message: ANSI C forbids ordered comparisons of pointers to functions [125]
 
-TODO: "Add example code that triggers the above message."
-TODO: "Add example code that almost triggers the above message."
+/* lint1-extra-flags: -s */
+
+typedef void (*action)(void);
+
+int
+less(action a, action b)
+{
+	return a < b;
+}
--- tests/usr.bin/xlint/lint1/msg_127.c
+++ tests/usr.bin/xlint/lint1/msg_127.c
@@ -1,7 +1,13 @@
-/*	$NetBSD: msg_127.c,v 1.1 2021/01/02 10:22:43 rillig Exp $	*/
+/*	$NetBSD: msg_127.c,v 1.2 2021/01/09 14:37:16 rillig Exp $	*/
 # 3 "msg_127.c"
 
-// Test for message: '&' before array or function: ignored [127]
+/* Test for message: '&' before array or function: ignored [127] */
 
-TODO: "Add example code that triggers the above message."
-TODO: "Add example code that almost triggers the above message."
+/* lint1-extra-flags: -t */
+
+void
+example(void)
+{
+	if (&example != (void *)0)
+		return;
+}
