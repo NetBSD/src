60d85130490efcf52f760869ac4be3dbfbdfe236
rillig <rillig@NetBSD.org>
2021-01-10 13:54:13 +0000

    lint: use message 232 for unused label in function
    This message had been defined but not used before.

--- tests/usr.bin/xlint/lint1/msg_232.c
+++ tests/usr.bin/xlint/lint1/msg_232.c
@@ -1,7 +1,14 @@
-/*	$NetBSD: msg_232.c,v 1.1 2021/01/02 10:22:44 rillig Exp $	*/
+/*	$NetBSD: msg_232.c,v 1.2 2021/01/10 13:54:13 rillig Exp $	*/
 # 3 "msg_232.c"
 
 // Test for message: label %s unused in function %s [232]
 
-TODO: "Add example code that triggers the above message."
-TODO: "Add example code that almost triggers the above message."
+void
+example(void)
+{
+	goto used_label;
+unused_label:
+	return;
+used_label:
+	return;
+}
