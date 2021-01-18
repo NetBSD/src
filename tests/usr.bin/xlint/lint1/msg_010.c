4329e41473ad82d9ac648acb085d509f0fe00642
rillig <rillig@NetBSD.org>
2021-01-18 17:41:04 +0000

    lint: add more test cases for duplicate type qualifiers

--- tests/usr.bin/xlint/lint1/msg_010.c
+++ tests/usr.bin/xlint/lint1/msg_010.c
@@ -1,21 +1,36 @@
-/*	$NetBSD: msg_010.c,v 1.2 2021/01/07 18:37:41 rillig Exp $	*/
+/*	$NetBSD: msg_010.c,v 1.3 2021/01/18 17:41:04 rillig Exp $	*/
 # 3 "msg_010.c"
 
 // Test for message: duplicate '%s' [10]
 
-inline inline void
+inline inline void		/* expect: [10] */
 double_inline(void)
 {
 }
 
-const const int
+const const int			/* expect: [10] */
 double_const(void)
 {
 	return 0;
 }
 
-volatile volatile int
+volatile volatile int		/* expect: [10] */
 double_volatile(void)
 {
 	return 0;
+}
+
+int
+restrict_pointer(const int * /*restrict*/ p) // FIXME: assertion "== VOLATILE" failed
+{
+	return *p;
+}
+
+_Thread_local int thread_local_int;
+_Thread_local int *pointer_to_thread_local;
+
+int
+thread_local_parameter(_Thread_local int i) /* caught by the compiler */
+{
+	return i;
 }
