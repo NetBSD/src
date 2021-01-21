ceda4d826d3f4aea894db316e45344efbf96b37e
rillig <rillig@NetBSD.org>
2021-01-21 00:38:28 +0000

    make(1): add more tests for tokens in conditionals

--- usr.bin/make/unit-tests/cond-token-plain.mk
+++ usr.bin/make/unit-tests/cond-token-plain.mk
@@ -1,4 +1,4 @@
-# $NetBSD: cond-token-plain.mk,v 1.6 2020/11/15 14:58:14 rillig Exp $
+# $NetBSD: cond-token-plain.mk,v 1.7 2021/01/21 00:38:28 rillig Exp $
 #
 # Tests for plain tokens (that is, string literals without quotes)
 # in .if conditions.
@@ -92,6 +92,52 @@
 .if ${:Uvar}||name != "var||name"
 .  error
 .endif
+
+# A bare word may appear alone in a condition, without any comparison
+# operator.  It is implicitly converted into defined(bare).
+.if bare
+.  error
+.else
+.  info A bare word is treated like defined(...), and the variable $\
+	'bare' is not defined.
+.endif
+
+VAR=	defined
+.if VAR
+.  info A bare word is treated like defined(...).
+.else
+.  error
+.endif
+
+# Bare words may be intermixed with variable expressions.
+.if V${:UA}R
+.  info ok
+.else
+.  error
+.endif
+
+# In bare words, even undefined variables are allowed.  Without the bare
+# words, undefined variables are not allowed.  That feels inconsistent.
+.if V${UNDEF}AR
+.  info Undefined variables in bare words expand to an empty string.
+.else
+.  error
+.endif
+
+.if 0${:Ux00}
+.  error
+.else
+.  info Numbers can be composed from literals and variable expressions.
+.endif
+
+.if 0${:Ux01}
+.  info Numbers can be composed from literals and variable expressions.
+.else
+.  error
+.endif
+
+# See cond-token-string.mk for similar tests where the condition is enclosed
+# in "quotes".
 
 all:
 	@:;
