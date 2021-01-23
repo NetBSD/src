7acde7fc1092eb3e096d677918e7bbd21c293abb
christos <christos@NetBSD.org>
2021-01-23 19:38:51 +0000

    Document via __HAVE_BUS_SPACE_8 platforms that implement bus_space_*_8

--- sys/arch/ofppc/include/types.h
+++ sys/arch/ofppc/include/types.h
@@ -1,10 +1,11 @@
-/*	$NetBSD: types.h,v 1.12 2017/01/26 15:55:10 christos Exp $	*/
+/*	$NetBSD: types.h,v 1.13 2021/01/23 19:38:53 christos Exp $	*/
 
 #ifndef _OFPPC_TYPES_H_
 #define	_OFPPC_TYPES_H_
 
 #define	__HAVE_NEW_STYLE_BUS_H
 #define	__HAVE_COMPAT_NETBSD32
+#define	__HAVE_BUS_SPACE_8
 
 #include <powerpc/types.h>
 
