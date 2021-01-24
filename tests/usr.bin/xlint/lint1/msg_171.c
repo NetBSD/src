fac45fc6d95a062da92e58a25fa6fb82fb863325
rillig <rillig@NetBSD.org>
2021-01-24 16:12:45 +0000

    lint: add tests for a few messages

--- tests/usr.bin/xlint/lint1/msg_171.c
+++ tests/usr.bin/xlint/lint1/msg_171.c
@@ -1,7 +1,19 @@
-/*	$NetBSD: msg_171.c,v 1.1 2021/01/02 10:22:43 rillig Exp $	*/
+/*	$NetBSD: msg_171.c,v 1.2 2021/01/24 16:12:45 rillig Exp $	*/
 # 3 "msg_171.c"
 
 // Test for message: assignment type mismatch (%s != %s) [171]
 
-TODO: "Add example code that triggers the above message."
-TODO: "Add example code that almost triggers the above message."
+struct s {
+	int member;
+};
+
+/*ARGSUSED*/
+void
+example(int i, void *vp, struct s *s)
+{
+	i = *s;			/* expect: 171 */
+	*s = i;			/* expect: 171 */
+
+	vp = *s;		/* expect: 171 */
+	*s = vp;		/* expect: 171 */
+}
