91e76d5ca4af8895b9a85ce8d86e8d1762c86ee4
rillig <rillig@NetBSD.org>
2021-01-24 10:50:42 +0000

    lint: document bug in typeok_eq
    Since rt is an alias for rn->tn_type->t_tspec, it cannot be PTR and VOID
    at the same time.  This makes the condition unsatisfiable.  Removing
    that part of the code didn't show any change in behavior, as expected.
    
    It may even be that fixing this obvious bug doesn't show any change in
    behavior since that function is only used in a single place and
    check_pointer_comparison performs its own checks before issuing any
    warning.
    
    At least the test cases added to msg_124.c all run as expected.

--- tests/usr.bin/xlint/lint1/msg_124.c
+++ tests/usr.bin/xlint/lint1/msg_124.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: msg_124.c,v 1.2 2021/01/03 15:44:35 rillig Exp $	*/
+/*	$NetBSD: msg_124.c,v 1.3 2021/01/24 10:50:42 rillig Exp $	*/
 # 3 "msg_124.c"
 
 // Test for message: illegal pointer combination, op %s [124]
@@ -13,7 +13,33 @@ typedef int(*printflike)(const char *, ...)
 void
 example(int *ptr)
 {
-	signal_handler handler = ptr;
-	sys_signal signal = ptr;
-	printflike printf = ptr;
+	signal_handler handler = ptr;	/* expect: 124 */
+	sys_signal signal = ptr;	/* expect: 124 */
+	printflike printf = ptr;	/* expect: 124 */
+}
+
+void ok(_Bool);
+void not_ok(_Bool);
+
+void
+compare_pointers(const void *vp, const char *cp, const int *ip,
+		 signal_handler fp)
+{
+	ok(vp == cp);
+	ok(vp == ip);
+	ok(vp == fp);
+	not_ok(cp == ip);	/* expect: 124 */
+	not_ok(cp == fp);	/* expect: 124 */
+	ok(vp == (void *)0);
+	ok(cp == (void *)0);
+	ok(ip == (void *)0);
+	ok(fp == (void *)0);
+	ok(vp == 0);
+	ok(cp == 0);
+	ok(ip == 0);
+	ok(fp == 0);
+	ok(vp == 0L);
+	ok(cp == 0L);
+	ok(ip == 0L);
+	ok(fp == 0L);
 }
