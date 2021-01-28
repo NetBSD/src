f5842dc3410e9512218b6bb34af8327a4d6e4a78
jmcneill <jmcneill@NetBSD.org>
2021-01-28 11:45:31 +0000

    port-arm/55957: Odroid C2 can not access eMMC card
    A few changes to clock setup:
     - Disable clock while changing dividers
     - Set / clear DDR flag before changing clock
     - Adjust TX/RX/core phases

--- sys/arch/arm/amlogic/mesongx_mmc.c
+++ sys/arch/arm/amlogic/mesongx_mmc.c
@@ -1,4 +1,4 @@
-/* $NetBSD: mesongx_mmc.c,v 1.13 2021/01/27 03:10:18 thorpej Exp $ */
+/* $NetBSD: mesongx_mmc.c,v 1.14 2021/01/28 11:45:31 jmcneill Exp $ */
 
 /*-
  * Copyright (c) 2019 Jared McNeill <jmcneill@invisible.ca>
@@ -27,7 +27,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: mesongx_mmc.c,v 1.13 2021/01/27 03:10:18 thorpej Exp $");
+__KERNEL_RCSID(0, "$NetBSD: mesongx_mmc.c,v 1.14 2021/01/28 11:45:31 jmcneill Exp $");
 
 #include <sys/param.h>
 #include <sys/bus.h>
@@ -496,6 +496,17 @@ mesongx_mmc_set_clock(struct mesongx_mmc_softc *sc, u_int freq, bool ddr)
 	if (best_diff == INT_MAX)
 		return ERANGE;
 
+	val = MMC_READ(sc, SD_EMMC_CFG);
+	val |= CFG_STOP_CLK;
+	MMC_WRITE(sc, SD_EMMC_CFG, val);
+
+	val = MMC_READ(sc, SD_EMMC_CFG);
+	if (ddr)
+		val |= CFG_DDR;
+	else
+		val &= ~CFG_DDR;
+	MMC_WRITE(sc, SD_EMMC_CFG, val);
+
 	val = MMC_READ(sc, SD_EMMC_CLOCK);
 	if (sc->sc_hwtype == MESONGX_MMC_V3)
 		val |= CLOCK_CFG_V3_ALWAYS_ON;
@@ -504,14 +515,18 @@ mesongx_mmc_set_clock(struct mesongx_mmc_softc *sc, u_int freq, bool ddr)
 	val &= ~CLOCK_CFG_RX_PHASE;
 	val |= __SHIFTIN(0, CLOCK_CFG_RX_PHASE);
 	val &= ~CLOCK_CFG_TX_PHASE;
-	val |= __SHIFTIN(2, CLOCK_CFG_TX_PHASE);
+	val |= __SHIFTIN(0, CLOCK_CFG_TX_PHASE);
 	val &= ~CLOCK_CFG_CO_PHASE;
-	val |= __SHIFTIN(3, CLOCK_CFG_CO_PHASE);
+	val |= __SHIFTIN(2, CLOCK_CFG_CO_PHASE);
 	val &= ~CLOCK_CFG_SRC;
 	val |= __SHIFTIN(best_sel, CLOCK_CFG_SRC);
 	val &= ~CLOCK_CFG_DIV;
 	val |= __SHIFTIN(best_div, CLOCK_CFG_DIV);
 	MMC_WRITE(sc, SD_EMMC_CLOCK, val);
+
+	val = MMC_READ(sc, SD_EMMC_CFG);
+	val &= ~CFG_STOP_CLK;
+	MMC_WRITE(sc, SD_EMMC_CFG, val);
 
 	return 0;
 }
@@ -733,21 +748,8 @@ static int
 mesongx_mmc_bus_clock(sdmmc_chipset_handle_t sch, int freq, bool ddr)
 {
 	struct mesongx_mmc_softc * const sc = sch;
-	uint32_t val;
-	int error;
 
-	error = mesongx_mmc_set_clock(sc, freq, ddr);
-	if (error != 0)
-		return error;
-
-	val = MMC_READ(sc, SD_EMMC_CFG);
-	if (ddr)
-		val |= CFG_DDR; 
-	else
-		val &= ~CFG_DDR;
-	MMC_WRITE(sc, SD_EMMC_CFG, val);
-
-	return 0;
+	return mesongx_mmc_set_clock(sc, freq, ddr);
 }
 
 static int
