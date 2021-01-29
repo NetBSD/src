e80eee50ce8536a6182094cf1b1ce7a14e96f094
thorpej <thorpej@NetBSD.org>
2021-01-29 15:24:00 +0000

    Use acpi_compatible_match() / acpi_compatible_lookup().

--- sys/dev/acpi/tpm_acpi.c
+++ sys/dev/acpi/tpm_acpi.c
@@ -1,4 +1,4 @@
-/* $NetBSD: tpm_acpi.c,v 1.12 2021/01/16 01:23:04 thorpej Exp $ */
+/* $NetBSD: tpm_acpi.c,v 1.13 2021/01/29 15:24:00 thorpej Exp $ */
 
 /*
  * Copyright (c) 2012, 2019 The NetBSD Foundation, Inc.
@@ -30,7 +30,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: tpm_acpi.c,v 1.12 2021/01/16 01:23:04 thorpej Exp $");
+__KERNEL_RCSID(0, "$NetBSD: tpm_acpi.c,v 1.13 2021/01/29 15:24:00 thorpej Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -55,43 +55,36 @@ static void	tpm_acpi_attach(device_t, device_t, void *);
 CFATTACH_DECL_NEW(tpm_acpi, sizeof(struct tpm_softc), tpm_acpi_match,
     tpm_acpi_attach, NULL, NULL);
 
-/*
- * Supported TPM 1.2 devices.
- */
-static const char * const tpm_1_2_acpi_ids[] = {
-	"PNP0C31",
-	NULL
-};
-
-/*
- * Supported TPM 2.0 devices.
- */
-static const char * const tpm2_acpi_ids[] = {
-	"MSFT0101",
-	NULL
+static const struct device_compatible_entry compat_data[] = {
+	{ .compat = "PNP0C31",		.value = TPM_1_2 },
+	{ .compat = "MSFT0101",		.value = TPM_2_0 },
+	DEVICE_COMPAT_EOL
 };
 
 static int
 tpm_acpi_match(device_t parent, cfdata_t match, void *aux)
 {
 	struct acpi_attach_args *aa = aux;
+	const struct device_compatible_entry *dce;
 	ACPI_TABLE_TPM2 *tpm2;
 	ACPI_STATUS rv;
-
-	if (aa->aa_node->ad_type != ACPI_TYPE_DEVICE)
-		return 0;
+	int ret;
 
 	/* We support only one TPM. */
 	if (tpm_cd.cd_devs && tpm_cd.cd_devs[0])
 		return 0;
 
-	if (acpi_match_hid(aa->aa_node->ad_devinfo, tpm_1_2_acpi_ids)) {
-		/* XXX assume TPM 1.2 devices are memory-mapped. */
-		return 1;
-	}
-
-	if (!acpi_match_hid(aa->aa_node->ad_devinfo, tpm2_acpi_ids))
+	ret = acpi_compatible_match(aa, compat_data);
+	if (ret == 0)
 		return 0;
+
+	dce = acpi_compatible_lookup(aa, compat_data);
+	KASSERT(dce != NULL);
+
+	if (dce->value == TPM_1_2) {
+		/* XXX assume TPM 1.2 devices are memory-mapped. */
+		return ret;
+	}
 
 	/* Make sure it uses TIS, and not CRB. */
 	rv = AcpiGetTable(ACPI_SIG_TPM2, 1, (ACPI_TABLE_HEADER **)&tpm2);
@@ -100,7 +93,7 @@ tpm_acpi_match(device_t parent, cfdata_t match, void *aux)
 	if (tpm2->StartMethod != ACPI_TPM2_MEMORY_MAPPED)
 		return 0;
 
-	return 1;
+	return ret;
 }
 
 static void
@@ -108,6 +101,7 @@ tpm_acpi_attach(device_t parent, device_t self, void *aux)
 {
 	struct tpm_softc *sc = device_private(self);
 	struct acpi_attach_args *aa = aux;
+	const struct device_compatible_entry *dce;
 	struct acpi_resources res;
 	struct acpi_mem *mem;
 	bus_addr_t base;
@@ -134,12 +128,11 @@ tpm_acpi_attach(device_t parent, device_t self, void *aux)
 	base = mem->ar_base;
 	size = mem->ar_length;
 
+	dce = acpi_compatible_lookup(aa, compat_data);
+	KASSERT(dce != NULL);
+
 	sc->sc_dev = self;
-	if (acpi_match_hid(aa->aa_node->ad_devinfo, tpm_1_2_acpi_ids)) {
-		sc->sc_ver = TPM_1_2;
-	} else {
-		sc->sc_ver = TPM_2_0;
-	}
+	sc->sc_ver = dce->value;
 	mutex_init(&sc->sc_lock, MUTEX_DEFAULT, IPL_NONE);
 	sc->sc_busy = false;
 	sc->sc_intf = &tpm_intf_tis12;
