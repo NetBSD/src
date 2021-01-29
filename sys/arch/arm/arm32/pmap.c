de43ae6133b09120e2518492bb387a2e833f642f
skrll <skrll@NetBSD.org>
2021-01-29 07:00:28 +0000

    More debug

--- sys/arch/arm/arm32/pmap.c
+++ sys/arch/arm/arm32/pmap.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: pmap.c,v 1.423 2021/01/24 14:51:01 skrll Exp $	*/
+/*	$NetBSD: pmap.c,v 1.424 2021/01/29 07:00:28 skrll Exp $	*/
 
 /*
  * Copyright 2003 Wasabi Systems, Inc.
@@ -192,7 +192,7 @@
 #endif
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: pmap.c,v 1.423 2021/01/24 14:51:01 skrll Exp $");
+__KERNEL_RCSID(0, "$NetBSD: pmap.c,v 1.424 2021/01/29 07:00:28 skrll Exp $");
 
 #include <sys/param.h>
 #include <sys/types.h>
@@ -4423,8 +4423,8 @@ pmap_fault_fixup(pmap_t pm, vaddr_t va, vm_prot_t ftype, int user)
 	pt_entry_t * const ptep = &l2b->l2b_kva[l2pte_index(va)];
 	pt_entry_t const opte = *ptep;
 	if (opte == 0 || (opte & L2_TYPE_MASK) == L2_TYPE_L) {
-		UVMHIST_LOG(maphist, " <-- done (empty pde for l1slot %#jx)",
-		    l1slot, 0, 0, 0);
+		UVMHIST_LOG(maphist, " <-- done (empty pte)",
+		    0, 0, 0, 0);
 		goto out;
 	}
 
@@ -4439,6 +4439,7 @@ pmap_fault_fixup(pmap_t pm, vaddr_t va, vm_prot_t ftype, int user)
 #endif
 
 	pa = l2pte_pa(opte);
+	UVMHIST_LOG(maphist, " pa %#jx opte %#jx ", pa, opte, 0, 0);
 
 	if ((ftype & VM_PROT_WRITE) && !l2pte_writable_p(opte)) {
 		/*
@@ -4474,6 +4475,7 @@ pmap_fault_fixup(pmap_t pm, vaddr_t va, vm_prot_t ftype, int user)
 		 */
 		if ((pv->pv_flags & PVF_WRITE) == 0) {
 			pmap_release_page_lock(md);
+			UVMHIST_LOG(maphist, " <-- done (write fault)", 0, 0, 0, 0);
 			goto out;
 		}
 
