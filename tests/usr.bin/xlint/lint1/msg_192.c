ddbcd44dcd76e5e5772c833afd8d257fb2178737
rillig <rillig@NetBSD.org>
2021-01-30 17:56:29 +0000

    lint: remove message 189, add tests for a few other messages
    Message 189 would have applied to traditional C and was supposed to
    detect assignments between struct and union types.  The corresponding
    check had never been implemented though.
    
    Traditional C has been superseded for 30 years now, therefore there is no
    point in adding this check retroactively.

--- tests/usr.bin/xlint/lint1/msg_192.c
+++ tests/usr.bin/xlint/lint1/msg_192.c
@@ -1,7 +1,10 @@
-/*	$NetBSD: msg_192.c,v 1.1 2021/01/02 10:22:43 rillig Exp $	*/
+/*	$NetBSD: msg_192.c,v 1.2 2021/01/30 17:56:29 rillig Exp $	*/
 # 3 "msg_192.c"
 
 // Test for message: %s unused in function %s [192]
 
-TODO: "Add example code that triggers the above message."
-TODO: "Add example code that almost triggers the above message."
+void
+example(int param)		/* expect: 231 */
+{
+	int local;		/* expect: 192 */
+}
--- tests/usr.bin/xlint/lint1/msg_194.c
+++ tests/usr.bin/xlint/lint1/msg_194.c
@@ -1,7 +1,13 @@
-/*	$NetBSD: msg_194.c,v 1.1 2021/01/02 10:22:43 rillig Exp $	*/
+/*	$NetBSD: msg_194.c,v 1.2 2021/01/30 17:56:29 rillig Exp $	*/
 # 3 "msg_194.c"
 
 // Test for message: label %s redefined [194]
 
-TODO: "Add example code that triggers the above message."
-TODO: "Add example code that almost triggers the above message."
+void example(void)
+{
+	int i = 0;
+label:				/* expect: 232 */
+	i = 1;
+label:				/* expect: 194 */
+	i = 2;
+}
