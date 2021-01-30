8a360ee5801a451c6c298e107b29c987a4874fa8
rillig <rillig@NetBSD.org>
2021-01-30 21:49:08 +0000

    lint: add type information to warning about troublesome casts
    The previous warning text did not mention the actual types that are
    involved in the type conversion.  These types can be hard to see from
    the source code as soon as macros are involved, and even in plain code,
    one would have to follow the declarations, which is an unnecessary
    burden.  Lint already has all information about the involved types, so
    there is no reason for omitting this crucial information.
    
    Seen in external/mit/lua/dist/src/lvm.c and several other files.
    Including the type information in the message immediately makes the
    message scarier.
    
    Before:	pointer casts may be troublesome
    After:	pointer cast from 'pointer to struct TString' to 'pointer to
    	union GCUnion' may be troublesome

--- usr.bin/xlint/lint1/tree.c
+++ usr.bin/xlint/lint1/tree.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: tree.c,v 1.194 2021/01/30 18:16:45 rillig Exp $	*/
+/*	$NetBSD: tree.c,v 1.195 2021/01/30 21:49:08 rillig Exp $	*/
 
 /*
  * Copyright (c) 1994, 1995 Jochen Pohl
@@ -37,7 +37,7 @@
 
 #include <sys/cdefs.h>
 #if defined(__RCSID) && !defined(lint)
-__RCSID("$NetBSD: tree.c,v 1.194 2021/01/30 18:16:45 rillig Exp $");
+__RCSID("$NetBSD: tree.c,v 1.195 2021/01/30 21:49:08 rillig Exp $");
 #endif
 
 #include <float.h>
@@ -2253,8 +2253,8 @@ check_pointer_conversion(op_t op, tnode_t *tn, type_t *tp)
 	     tp->t_subt->t_str != tn->tn_type->t_subt->t_str) ||
 	    psize(nt) != psize(ot)) {
 		if (cflag) {
-			/* pointer casts may be troublesome */
-			warning(247);
+			/* pointer cast from '%s' to '%s' may be troublesome */
+			warning(247, type_name(tn->tn_type), type_name(tp));
 		}
 	}
 }
