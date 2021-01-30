3029566535760fe808400f054e34002b8a579f4e
rillig <rillig@NetBSD.org>
2021-01-30 17:02:58 +0000

    lint: add tests that trigger a few more messages

--- tests/usr.bin/xlint/lint1/msg_220.c
+++ tests/usr.bin/xlint/lint1/msg_220.c
@@ -1,7 +1,25 @@
-/*	$NetBSD: msg_220.c,v 1.1 2021/01/02 10:22:44 rillig Exp $	*/
+/*	$NetBSD: msg_220.c,v 1.2 2021/01/30 17:02:58 rillig Exp $	*/
 # 3 "msg_220.c"
 
 // Test for message: fallthrough on case statement [220]
 
-TODO: "Add example code that triggers the above message."
-TODO: "Add example code that almost triggers the above message."
+/* lint1-extra-flags: -h */
+
+extern void
+println(const char *);
+
+void
+example(int n)
+{
+	switch (n) {
+	case 1:
+	case 3:
+	case 5:
+		println("odd");
+	case 2:			/* expect: 220 */
+	case 7:
+		println("prime");
+	default:		/* expect: 284 */
+		println("number");
+	}
+}
