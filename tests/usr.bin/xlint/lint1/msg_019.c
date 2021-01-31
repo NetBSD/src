f28c1d6120e47934860acbc306efdfb0d753de53
rillig <rillig@NetBSD.org>
2021-01-31 09:48:47 +0000

    lint: test messages 019 and 021

--- tests/usr.bin/xlint/lint1/msg_019.c
+++ tests/usr.bin/xlint/lint1/msg_019.c
@@ -1,7 +1,14 @@
-/*	$NetBSD: msg_019.c,v 1.1 2021/01/02 10:22:43 rillig Exp $	*/
+/*	$NetBSD: msg_019.c,v 1.2 2021/01/31 09:48:47 rillig Exp $	*/
 # 3 "msg_019.c"
 
 // Test for message: void type for %s [19]
 
-TODO: "Add example code that triggers the above message."
-TODO: "Add example code that almost triggers the above message."
+void global_variable;		/* expect: 19 */
+
+static void unit_variable;	/* expect: 19, 226 */
+
+void
+function(void parameter)	/* expect: 61, 231 */
+{
+	void local_variable;	/* expect: 19 */
+}
--- tests/usr.bin/xlint/lint1/msg_021.c
+++ tests/usr.bin/xlint/lint1/msg_021.c
@@ -1,7 +1,37 @@
-/*	$NetBSD: msg_021.c,v 1.1 2021/01/02 10:22:43 rillig Exp $	*/
+/*	$NetBSD: msg_021.c,v 1.2 2021/01/31 09:48:47 rillig Exp $	*/
 # 3 "msg_021.c"
 
 // Test for message: redeclaration of formal parameter %s [21]
 
-TODO: "Add example code that triggers the above message."
-TODO: "Add example code that almost triggers the above message."
+/*ARGSUSED*/
+void
+old_style_with_duplicate_parameter(parameter, parameter) /* expect: 21 */
+    int parameter;
+{				/* expect: 32 */
+}
+
+void
+old_style_with_duplicate_parameter_declaration(parameter)
+    int parameter;
+    int parameter;		/* expect: 237 */
+{
+}
+
+void old_style_with_local_variable(parameter)
+    int parameter;
+{
+	int parameter;		/* expect: 27 */
+}
+
+/*ARGSUSED*/
+void
+prototype_with_duplicate_parameter(int param, int param) /* expect: 237 */
+{
+
+}
+
+void
+prototype_with_local_variable(int parameter)
+{
+	int parameter;		/* expect: 27 */
+}
