3cef4b4f943a770761c0226595c672c3273ae6db
rillig <rillig@NetBSD.org>
2021-01-31 09:21:24 +0000

    lint: add a few more tests

--- tests/usr.bin/xlint/lint1/msg_243.c
+++ tests/usr.bin/xlint/lint1/msg_243.c
@@ -1,7 +1,25 @@
-/*	$NetBSD: msg_243.c,v 1.1 2021/01/02 10:22:44 rillig Exp $	*/
+/*	$NetBSD: msg_243.c,v 1.2 2021/01/31 09:21:24 rillig Exp $	*/
 # 3 "msg_243.c"
 
 // Test for message: dubious comparison of enums, op %s [243]
 
-TODO: "Add example code that triggers the above message."
-TODO: "Add example code that almost triggers the above message."
+/* lint1-extra-flags: -eP */
+
+enum color {
+	RED, GREEN, BLUE
+};
+
+void eval(_Bool);
+
+/* TODO: There should be a way to declare an enum type as "ordered ok". */
+
+void
+example(enum color a, enum color b)
+{
+	eval(a < b);		/* expect: 243 */
+	eval(a <= b);		/* expect: 243 */
+	eval(a > b);		/* expect: 243 */
+	eval(a >= b);		/* expect: 243 */
+	eval(a == b);
+	eval(a != b);
+}
