5b84e10a1af84279c33c1529646b85d793dbc402
rillig <rillig@NetBSD.org>
2021-01-31 09:21:24 +0000

    lint: add a few more tests

--- tests/usr.bin/xlint/lint1/msg_003.c
+++ tests/usr.bin/xlint/lint1/msg_003.c
@@ -1,7 +1,13 @@
-/*	$NetBSD: msg_003.c,v 1.1 2021/01/02 10:22:42 rillig Exp $	*/
+/*	$NetBSD: msg_003.c,v 1.2 2021/01/31 09:21:24 rillig Exp $	*/
 # 3 "msg_003.c"
 
 // Test for message: %s declared in argument declaration list [3]
 
-TODO: "Add example code that triggers the above message."
-TODO: "Add example code that almost triggers the above message."
+/*ARGSUSED*/
+void
+example(declare_struct, declare_union, declare_enum)
+    struct struct_in_argument *declare_struct;
+    union union_in_argument *declare_union;
+    enum enum_in_argument *declare_enum;
+{
+}
--- tests/usr.bin/xlint/lint1/msg_085.c
+++ tests/usr.bin/xlint/lint1/msg_085.c
@@ -1,9 +1,11 @@
-/*	$NetBSD: msg_085.c,v 1.2 2021/01/02 18:06:01 rillig Exp $	*/
+/*	$NetBSD: msg_085.c,v 1.3 2021/01/31 09:21:24 rillig Exp $	*/
 # 3 "msg_085.c"
 
 // Test for message: dubious tag declaration: %s %s [85]
 
-extern int stat(struct stat *);
+void declare_struct(struct in_argument *);	/* expect: 85 */
+void declare_union(union in_argument *);	/* expect: 85 */
+void declare_enum(enum in_argument *);		/* expect: 85 */
 
-struct ok;
+struct ok;					/* expect: 233 */
 extern int ok(struct ok *);
