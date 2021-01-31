9ff24546eef77342f861193c27fd5a2aebbaecf2
rillig <rillig@NetBSD.org>
2021-01-31 11:59:56 +0000

    lint: add more test cases for strange '==' warning 160

--- tests/usr.bin/xlint/lint1/msg_160.c
+++ tests/usr.bin/xlint/lint1/msg_160.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: msg_160.c,v 1.3 2021/01/31 11:12:07 rillig Exp $	*/
+/*	$NetBSD: msg_160.c,v 1.4 2021/01/31 11:59:56 rillig Exp $	*/
 # 3 "msg_160.c"
 
 // Test for message: operator '==' found where '=' was expected [160]
@@ -9,5 +9,31 @@ _Bool
 both_equal_or_unequal(int a, int b, int c, int d)
 {
 	/* XXX: Why shouldn't this be legitimate? */
-	return (a == b) == (c == d);		/* expect: 160, 160 */
+	return (a == b) == (c == d);	/* expect: 160, 160 */
+}
+
+void
+eval(_Bool);
+
+void
+unparenthesized(int a, int b, int c, _Bool z)
+{
+	/*
+	 * This one might be legitimate since the second '==' has _Bool
+	 * on both sides.  Parenthesizing its left-hand operand doesn't
+	 * hurt though.
+	 */
+	eval(a == b == z);		/* expect: 160 */
+
+	eval((a == b) == z);		/*FIXME*//* expect: 160 */
+
+	/*
+	 * This one is definitely wrong.  C, unlike Python, does not chain
+	 * comparison operators in the way mathematicians are used to.
+	 */
+	eval(a == b == c);		/* expect: 160 */
+
+	/* Parenthesizing one of the operands makes it obvious enough. */
+	eval((a == b) == c);		/*FIXME*//* expect: 160 */
+	eval(a == (b == c));		/*FIXME*//* expect: 160 */
 }
