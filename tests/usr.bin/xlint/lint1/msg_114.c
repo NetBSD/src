1c1bd1bbee326626e35937a786e9cc806be029ee
rillig <rillig@NetBSD.org>
2021-01-31 16:00:05 +0000

    lint: document that message 108 doesn't abort anymore

--- tests/usr.bin/xlint/lint1/msg_114.c
+++ tests/usr.bin/xlint/lint1/msg_114.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: msg_114.c,v 1.3 2021/01/31 11:12:07 rillig Exp $	*/
+/*	$NetBSD: msg_114.c,v 1.4 2021/01/31 16:00:05 rillig Exp $	*/
 # 3 "msg_114.c"
 
 // Test for message: %soperand of '%s' must be lvalue [114]
@@ -7,7 +7,14 @@ void
 example(int a)			/* expect: 231 */
 {
 	3++;			/* expect: 114 */
-	// FIXME: lint error: ../common/tyname.c, 190: tspec_name(0)
-	// "string"++;
+
+	/*
+	 * Before tree.c 1.137 from 2021-01-09, trying to increment an array
+	 * aborted lint with 'common/tyname.c, 190: tspec_name(0)'.
+	 *
+	 * See msg_108.c for more details.
+	 */
+	"string"++;		/* expect: 108 */
+
 	(a + a)++;		/* expect: 114 */
 }
