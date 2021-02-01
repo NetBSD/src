53f49756f406050b73fa70c41fb89e310d9c32be
rillig <rillig@NetBSD.org>
2021-02-01 21:38:20 +0000

    make: remove outdated comment about VarEvalFlags
    The previous variable preserveUndefined is now the flag VARE_KEEP_UNDEF.

--- usr.bin/make/nonints.h
+++ usr.bin/make/nonints.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: nonints.h,v 1.188 2021/01/24 20:11:55 rillig Exp $	*/
+/*	$NetBSD: nonints.h,v 1.189 2021/02/01 21:38:20 rillig Exp $	*/
 
 /*
  * Copyright (c) 1988, 1989, 1990, 1993
@@ -286,26 +286,28 @@ void Var_End(void);
 typedef enum VarEvalFlags {
 	VARE_NONE		= 0,
 
-	/* Expand and evaluate variables during parsing.
+	/*
+	 * Expand and evaluate variables during parsing.
 	 *
 	 * TODO: Document what Var_Parse and Var_Subst return when this flag
-	 * is not set. */
+	 * is not set.
+	 */
 	VARE_WANTRES		= 1 << 0,
 
-	/* Treat undefined variables as errors.
-	 * Must only be used in combination with VARE_WANTRES. */
+	/*
+	 * Treat undefined variables as errors.
+	 * Must only be used in combination with VARE_WANTRES.
+	 */
 	VARE_UNDEFERR		= 1 << 1,
 
-	/* Keep '$$' as '$$' instead of reducing it to a single '$'.
+	/*
+	 * Keep '$$' as '$$' instead of reducing it to a single '$'.
 	 *
 	 * Used in variable assignments using the ':=' operator.  It allows
 	 * multiple such assignments to be chained without accidentally
 	 * expanding '$$file' to '$file' in the first assignment and
 	 * interpreting it as '${f}' followed by 'ile' in the next assignment.
-	 *
-	 * See also preserveUndefined, which preserves subexpressions that are
-	 * based on undefined variables; maybe that can be converted to a flag
-	 * as well. */
+	 */
 	VARE_KEEP_DOLLAR	= 1 << 2,
 
 	/*
