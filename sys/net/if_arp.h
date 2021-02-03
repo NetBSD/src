2f19df027b7270b49e69acf94fc8a6a6cf9c163a
roy <roy@NetBSD.org>
2021-02-03 10:31:10 +0000

    Whitespace

--- sys/net/if_arp.h
+++ sys/net/if_arp.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: if_arp.h,v 1.34 2021/02/03 05:51:40 roy Exp $	*/
+/*	$NetBSD: if_arp.h,v 1.35 2021/02/03 10:31:10 roy Exp $	*/
 
 /*
  * Copyright (c) 1986, 1993
@@ -45,22 +45,22 @@
  */
 struct	arphdr {
 	uint16_t ar_hrd;	/* format of hardware address */
-#define ARPHRD_ETHER 	1	/* ethernet hardware format */
-#define ARPHRD_IEEE802 	6	/* IEEE 802 hardware format */
-#define ARPHRD_ARCNET 	7	/* ethernet hardware format */
-#define ARPHRD_FRELAY 	15	/* frame relay hardware format */
-#define ARPHRD_STRIP 	23	/* Ricochet Starmode Radio hardware format */
-#define	ARPHRD_IEEE1394	24	/* IEEE 1394 (FireWire) hardware format */
-	uint16_t ar_pro;	/* format of protocol address */
-	uint8_t  ar_hln;	/* length of hardware address */
-	uint8_t  ar_pln;	/* length of protocol address */
-	uint16_t ar_op;		/* one of: */
-#define	ARPOP_REQUEST	1	/* request to resolve address */
-#define	ARPOP_REPLY	2	/* response to previous request */
-#define	ARPOP_REVREQUEST 3	/* request protocol address given hardware */
-#define	ARPOP_REVREPLY	4	/* response giving protocol address */
-#define	ARPOP_INVREQUEST 8 	/* request to identify peer */
-#define	ARPOP_INVREPLY	9	/* response identifying peer */
+#define ARPHRD_ETHER		1  /* ethernet hardware format */
+#define ARPHRD_IEEE802		6  /* IEEE 802 hardware format */
+#define ARPHRD_ARCNET		7  /* ethernet hardware format */
+#define ARPHRD_FRELAY		15 /* frame relay hardware format */
+#define ARPHRD_STRIP		23 /* Ricochet Starmode Radio hardware format */
+#define	ARPHRD_IEEE1394		24 /* IEEE 1394 (FireWire) hardware format */
+	uint16_t ar_pro;	   /* format of protocol address */
+	uint8_t  ar_hln;	   /* length of hardware address */
+	uint8_t  ar_pln;	   /* length of protocol address */
+	uint16_t ar_op;		   /* one of: */
+#define	ARPOP_REQUEST		1  /* request to resolve address */
+#define	ARPOP_REPLY		2  /* response to previous request */
+#define	ARPOP_REVREQUEST	3  /* request protocol address given hardware */
+#define	ARPOP_REVREPLY		4  /* response giving protocol address */
+#define	ARPOP_INVREQUEST	8  /* request to identify peer */
+#define	ARPOP_INVREPLY		9  /* response identifying peer */
 /*
  * The remaining fields are variable in size,
  * according to the sizes above.
