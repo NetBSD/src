306b6489c302739f5c780ce4756d2c61560d093a
thorpej <thorpej@NetBSD.org>
2021-02-04 22:36:52 +0000

    Call acpi_device_register() / fdtbus_device_register() as approrpriate.

--- sys/arch/arm/ti/omap3_platform.c
+++ sys/arch/arm/ti/omap3_platform.c
@@ -1,4 +1,4 @@
-/* $NetBSD: omap3_platform.c,v 1.4 2020/09/28 11:54:23 jmcneill Exp $ */
+/* $NetBSD: omap3_platform.c,v 1.5 2021/02/04 22:36:53 thorpej Exp $ */
 
 /*-
  * Copyright (c) 2019 Jared McNeill <jmcneill@invisible.ca>
@@ -30,7 +30,7 @@
 #include "opt_console.h"
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: omap3_platform.c,v 1.4 2020/09/28 11:54:23 jmcneill Exp $");
+__KERNEL_RCSID(0, "$NetBSD: omap3_platform.c,v 1.5 2021/02/04 22:36:53 thorpej Exp $");
 
 #include <sys/param.h>
 #include <sys/bus.h>
@@ -146,6 +146,7 @@ omap3_platform_early_putchar(char c)
 static void
 omap3_platform_device_register(device_t self, void *aux)
 {
+	fdtbus_device_register(dev, aux);
 }
 
 static u_int
