35f5b664b59bfbdc8fe7c389458a9dad9cb07c63
jdolecek <jdolecek@NetBSD.org>
2021-02-04 21:07:06 +0000

    introduce vfs.generic.timestamp_precision sysctl to control precision of the timer used for vfs_timestamp(); default stays the same to use nanotime(9), but option is there to use the faster, albeit less precise methods
    code taken from FreeBSD
    
    suggested by Mateusz Guzik in:
    http://mail-index.netbsd.org/tech-kern/2020/07/19/msg026620.html

--- sys/sys/mount.h
+++ sys/sys/mount.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: mount.h,v 1.236 2020/01/17 20:08:10 ad Exp $	*/
+/*	$NetBSD: mount.h,v 1.237 2021/02/04 21:07:06 jdolecek Exp $	*/
 
 /*
  * Copyright (c) 1989, 1991, 1993
@@ -115,6 +115,7 @@
 					   as next argument */
 #define VFS_USERMOUNT	3		/* enable/disable fs mnt by non-root */
 #define	VFS_MAGICLINKS  4		/* expand 'magic' symlinks */
+#define	VFS_TIMESTAMP_PRECISION  5	/* file timestamp precision */
 
 /* vfsquery flags for kqueue(2) */
 #define VQ_MOUNT	0x0001	/* new filesystem arrived */
