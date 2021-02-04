14b9b22c12f5cf9087720996da3dea2656ce3eb1
thorpej <thorpej@NetBSD.org>
2021-02-04 21:39:00 +0000

    Add acpi_device_register().  Just a placeholder for now.

--- sys/dev/acpi/acpivar.h
+++ sys/dev/acpi/acpivar.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: acpivar.h,v 1.84 2021/01/27 05:11:54 thorpej Exp $	*/
+/*	$NetBSD: acpivar.h,v 1.85 2021/02/04 21:39:00 thorpej Exp $	*/
 
 /*
  * Copyright 2001 Wasabi Systems, Inc.
@@ -319,6 +319,7 @@ const struct device_compatible_entry *
 		    const struct device_compatible_entry *);
 
 bool    	acpi_device_present(ACPI_HANDLE);
+void		acpi_device_register(device_t, void *);
 
 int		acpi_reset(void);
 
