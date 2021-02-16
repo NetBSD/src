458f6744b0a2755e3d417341f997a705290de98d
rillig <rillig@NetBSD.org>
2021-02-16 19:01:18 +0000

    make: test exporting a variable that itself depends on a subprocess
    When the point that "isn't going to end well" is reached, the stacktrace
    is quite long but still reasonable:
    
    main
    main_ReadFiles
    ReadAllMakefiles
    ReadMakefile		directive-export.mk
    Parse_File
    ParseLine				# line 3 has: _!= :;:
    ParseVarassign
    Parse_DoVar
    VarAssign_Eval
    VarAssign_EvalShell			# because of the '!='
    Cmd_Exec		:;:
    Var_ReexportVars			# before starting the subprocess
    ExportVar		EMPTY_SHELL
    ExportVarEnv				# was only marked for export
    Var_Subst		${EMPTY_SHELL}	# to get the value to export
    VarSubstExpr		${EMPTY_SHELL}
    Var_Parse		${EMPTY_SHELL}
    Var_Subst		${:sh}		# since EMPTY_SHELL= ${:sh}
    VarSubstExpr		${:sh}
    Var_Parse		${:sh}
    ApplyModifiers
    ApplySingleModifier	:sh
    ApplyModifier
    ApplyModifier_SunShell	:sh
    Cmd_Exec		""		# empty command
    Var_ReexportVars
    ExportVar		EMPTY_SHELL
    ExportVarEnv				# skipping this edge case

--- usr.bin/make/unit-tests/directive-export.mk
+++ usr.bin/make/unit-tests/directive-export.mk
@@ -1,4 +1,4 @@
-# $NetBSD: directive-export.mk,v 1.7 2021/02/16 18:12:46 rillig Exp $
+# $NetBSD: directive-export.mk,v 1.8 2021/02/16 19:01:18 rillig Exp $
 #
 # Tests for the .export directive.
 #
@@ -34,5 +34,11 @@ VAR=		value $$ ${INDIRECT}
 # An empty argument means no additional variables to export.
 .export ${:U}
 
+
+# Trigger the "This isn't going to end well" in ExportVarEnv.
+EMPTY_SHELL=	${:sh}
+.export EMPTY_SHELL	# only marked for export at this point
+_!=		:;:	# Force the variable to be actually exported.
+
+
 all:
-	@:;
