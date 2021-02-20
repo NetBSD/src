1d161cfc8a17ac239e7a519247a8207129086d3e
nia <nia@NetBSD.org>
2021-02-20 08:59:24 +0000

    patch(1): add test cases for various backup file formats

--- tests/usr.bin/patch/t_patch.sh
+++ tests/usr.bin/patch/t_patch.sh
@@ -1,6 +1,6 @@
-# $NetBSD: t_patch.sh,v 1.1 2020/06/24 09:21:43 jruoho Exp $
+# $NetBSD: t_patch.sh,v 1.2 2021/02/20 08:59:24 nia Exp $
 #
-# Copyright (c) 2020 The NetBSD Foundation, Inc.
+# Copyright (c) 2020, 2021 The NetBSD Foundation, Inc.
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -71,8 +71,116 @@ long_lines_body()
 	patch_lines 10000
 }
 
+atf_test_case backup_simple
+backup_simple_head()
+{
+	atf_set "descr" "Test backup type 'simple' (-V simple)"
+}
+
+backup_simple_body()
+{
+	# Create the backup file so it's overwritten.
+	touch to_patch.orig
+	printf '%s\n' 'original file' > to_patch
+
+	cat << EOF > test_diff.patch
+--- original_file       2021-02-20 09:21:07.100869692 +0100
++++ new_file    2021-02-20 09:21:10.912906887 +0100
+@@ -1 +1 @@
+-original text
++new text
+EOF
+	cksum=$(sha256 -n to_patch | cut -d' ' -f1)
+	patch -V simple to_patch < test_diff.patch
+	atf_check [ -f to_patch.orig ]
+	origfile_cksum=$(sha256 -n to_patch.orig | cut -d' ' -f1)
+	atf_check_equal "$cksum" "$origfile_cksum"
+	atf_check grep -q -m 1 '^new text$' to_patch
+}
+
+atf_test_case backup_none
+backup_none_head()
+{
+	atf_set "descr" "Test backup type 'none' (-V none)"
+}
+
+backup_none_body()
+{
+	printf '%s\n' 'original file' > to_patch
+
+	cat << EOF > test_diff.patch
+--- original_file       2021-02-20 09:21:07.100869692 +0100
++++ new_file    2021-02-20 09:21:10.912906887 +0100
+@@ -1 +1 @@
+-original text
++new text
+EOF
+	# Patch would mistakenly create 'simple' backup files when unwanted:
+	# http://mail-index.netbsd.org/tech-userlevel/2021/02/19/msg012901.html
+	patch -V none to_patch < test_diff.patch
+	atf_check [ ! -f to_patch.orig ]
+	atf_check grep -q -m 1 '^new text$' to_patch
+
+	# Environment variables should be overriden.
+	printf '%s\n' 'original file' > to_patch
+	VERSION_CONTROL=existing patch -V none to_patch \
+	    < test_diff.patch
+	atf_check [ ! -f to_patch.orig ]
+	atf_check grep -q -m 1 '^new text$' to_patch
+
+	# --posix should imply -V none.
+	printf '%s\n' 'original file' > to_patch
+	patch --posix to_patch < test_diff.patch
+	atf_check [ ! -f to_patch.orig ]
+	atf_check grep -q -m 1 '^new text$' to_patch
+}
+
+atf_test_case backup_numbered
+backup_numbered_head()
+{
+	atf_set "descr" "Test backup type 'numbered' (-V numbered)"
+}
+
+backup_numbered_body()
+{
+	printf '%s\n' 'original file' > to_patch
+
+	cat << EOF > test_diff.patch
+--- original_file       2021-02-20 09:21:07.100869692 +0100
++++ new_file    2021-02-20 09:21:10.912906887 +0100
+@@ -1 +1 @@
+-original text
++new text
+EOF
+	cksum1=$(sha256 -n to_patch | cut -d' ' -f1)
+	patch -V numbered to_patch < test_diff.patch
+	atf_check grep -q -m 1 '^new text$' to_patch
+
+	cat << EOF > test_diff2.patch
+--- new_file	2021-02-20 09:44:52.363230019 +0100
++++ newer_file	2021-02-20 09:43:54.592863401 +0100
+@@ -1 +1 @@
+-new text
++newer text
+EOF
+	cksum2=$(sha256 -n to_patch | cut -d' ' -f1)
+	patch -V numbered to_patch < test_diff2.patch
+	atf_check grep -q -m 1 '^newer text$' to_patch
+
+	# Make sure the backup files match the original files.
+	origfile_cksum1=$(sha256 -n to_patch.~1~ | cut -d' ' -f1)
+	origfile_cksum2=$(sha256 -n to_patch.~2~ | cut -d' ' -f1)
+	atf_check [ -f to_patch.~1~ ]
+	atf_check_equal "$cksum1" "$origfile_cksum1"
+	atf_check [ -f to_patch.~2~ ]
+	atf_check_equal "$cksum2" "$origfile_cksum2"
+}
+
 atf_init_test_cases()
 {
 	atf_add_test_case lines
 	atf_add_test_case long_lines
+	atf_add_test_case backup_simple
+	atf_add_test_case backup_none
+	atf_add_test_case backup_numbered
 }
