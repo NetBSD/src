0826316a449ad956554684c8c2402648894d5575
rillig <rillig@NetBSD.org>
2021-02-21 14:49:23 +0000

    lint: add test for bit-field initialisation in traditional C

--- tests/usr.bin/xlint/lint1/msg_186.c
+++ tests/usr.bin/xlint/lint1/msg_186.c
@@ -1,7 +1,19 @@
-/*	$NetBSD: msg_186.c,v 1.2 2021/02/21 09:07:58 rillig Exp $	*/
+/*	$NetBSD: msg_186.c,v 1.3 2021/02/21 14:49:23 rillig Exp $	*/
 # 3 "msg_186.c"
 
-// Test for message: bit-field initialisation is illegal in traditional C [186]
+/* Test for message: bit-field initialisation is illegal in traditional C [186] */
 
-TODO: "Add example code that triggers the above message." /* expect: 249 */
-TODO: "Add example code that almost triggers the above message."
+/* lint1-flags: -tw */
+
+struct bit_field {
+	unsigned one: 1;
+	unsigned three: 3;
+	unsigned two: 2;
+};
+
+struct bit_field bit_field = {
+	1,
+	3.0,			/* expect: 186 */
+	2
+};
+/* XXX: The message is misleading.  Initialisation using integers is ok. */
