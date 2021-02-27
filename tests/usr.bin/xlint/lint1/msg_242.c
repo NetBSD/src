e5e97593ee0d14137b49a8c9f5586f549c63e1b6
rillig <rillig@NetBSD.org>
2021-02-27 18:01:29 +0000

    tests/lint: add tests for enum/int mismatch

--- tests/usr.bin/xlint/lint1/msg_242.c
+++ tests/usr.bin/xlint/lint1/msg_242.c
@@ -1,7 +1,27 @@
-/*	$NetBSD: msg_242.c,v 1.2 2021/02/21 09:07:58 rillig Exp $	*/
+/*	$NetBSD: msg_242.c,v 1.3 2021/02/27 18:01:29 rillig Exp $	*/
 # 3 "msg_242.c"
 
 // Test for message: combination of '%s' and '%s', op %s [242]
 
-TODO: "Add example code that triggers the above message." /* expect: 249 */
-TODO: "Add example code that almost triggers the above message."
+/* lint1-extra-flags: -e */
+
+enum E {
+	E1
+};
+
+void sink_enum(enum E);
+void sink_int(int);
+
+void
+example(enum E e, int i)
+{
+	enum E e2 = e;
+	enum E e3 = i;		/* expect: 242 */
+	int i2 = e;		/* expect: 242 */
+	int i3 = i;
+
+	sink_enum(e2);
+	sink_enum(e3);
+	sink_int(i2);
+	sink_int(i3);
+}
