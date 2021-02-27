f116cba5f225e7a1f073cf6859d943be045e9887
mrg <mrg@NetBSD.org>
2021-02-27 12:36:46 +0000

    belated call version 20210211 after previous memory leak fix.

--- libexec/httpd/bozohttpd.c
+++ libexec/httpd/bozohttpd.c
@@ -1,9 +1,9 @@
-/*	$NetBSD: bozohttpd.c,v 1.126 2021/02/11 09:57:52 mrg Exp $	*/
+/*	$NetBSD: bozohttpd.c,v 1.127 2021/02/27 12:36:46 mrg Exp $	*/
 
 /*	$eterna: bozohttpd.c,v 1.178 2011/11/18 09:21:15 mrg Exp $	*/
 
 /*
- * Copyright (c) 1997-2020 Matthew R. Green
+ * Copyright (c) 1997-2021 Matthew R. Green
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -108,7 +108,7 @@
 #define INDEX_HTML		"index.html"
 #endif
 #ifndef SERVER_SOFTWARE
-#define SERVER_SOFTWARE		"bozohttpd/20210210"
+#define SERVER_SOFTWARE		"bozohttpd/20210211"
 #endif
 #ifndef PUBLIC_HTML
 #define PUBLIC_HTML		"public_html"
