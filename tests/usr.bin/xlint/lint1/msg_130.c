853f9b5a0c195d13c8fa56298577c3320aa4f5c4
rillig <rillig@NetBSD.org>
2021-02-27 13:43:36 +0000

    tests/lint: add more tests for enum mismatch

--- tests/usr.bin/xlint/lint1/msg_130.c
+++ tests/usr.bin/xlint/lint1/msg_130.c
@@ -1,18 +1,39 @@
-/*	$NetBSD: msg_130.c,v 1.3 2021/01/31 11:12:07 rillig Exp $	*/
+/*	$NetBSD: msg_130.c,v 1.4 2021/02/27 13:43:36 rillig Exp $	*/
 # 3 "msg_130.c"
 
 // Test for message: enum type mismatch, op %s [130]
 
 enum color {
-	RED, GREEN, BLUE
+	RED	= 1 << 0,
+	GREEN	= 1 << 1,
+	BLUE	= 1 << 2
+};
+
+enum size {
+	SMALL,
+	MEDIUM,
+	LARGE
 };
 
 enum daytime {
 	NIGHT, MORNING, NOON, EVENING
 };
 
-int
-example(_Bool cond)
+void sink(_Bool);
+
+void
+example(_Bool cond, enum color c, enum size s)
 {
-	return cond ? GREEN : MORNING;	/* expect: 130 */
+	sink(cond ? GREEN : MORNING);	/* expect: 130 */
+
+	sink(c != s);			/* expect: 130 */
+	sink(c == s);			/* expect: 130 */
+	sink((c & MEDIUM) != 0);	/* might be useful to warn about */
+	sink((c | MEDIUM) != 0);	/* might be useful to warn about */
+
+	c |= MEDIUM;			/* might be useful to warn about */
+	c &= MEDIUM;			/* might be useful to warn about */
+
+	/* The cast to unsigned is required by GCC at WARNS=6. */
+	c &= ~(unsigned)MEDIUM;		/* might be useful to warn about */
 }
