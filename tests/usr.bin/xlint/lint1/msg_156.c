b8a3fbe77a299f3b3dcd36bcad302562f54fd16e
rillig <rillig@NetBSD.org>
2021-02-27 13:43:36 +0000

    tests/lint: add more tests for enum mismatch

--- tests/usr.bin/xlint/lint1/msg_156.c
+++ tests/usr.bin/xlint/lint1/msg_156.c
@@ -1,12 +1,12 @@
-/*	$NetBSD: msg_156.c,v 1.3 2021/01/31 11:12:07 rillig Exp $	*/
+/*	$NetBSD: msg_156.c,v 1.4 2021/02/27 13:43:36 rillig Exp $	*/
 # 3 "msg_156.c"
 
 // Test for message: enum type mismatch, arg #%d [156]
 
 enum color {
-	RED,
-	GREEN,
-	BLUE
+	RED	= 1 << 0,
+	GREEN	= 1 << 1,
+	BLUE	= 1 << 2
 };
 
 enum size {
@@ -15,11 +15,14 @@ enum size {
 	LARGE
 };
 
-void
-print_color(enum color);
+void print_color(enum color);
 
 void
-example(void)
+example(enum color c, enum size s)
 {
+	print_color(GREEN);
+	print_color(c);
+
 	print_color(MEDIUM);		/* expect: 156 */
+	print_color(s);			/* expect: 156 */
 }
