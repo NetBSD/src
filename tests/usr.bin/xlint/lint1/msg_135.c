021f1be0b7715a20e340ead283c2dd12de780ff6
rillig <rillig@NetBSD.org>
2021-02-28 00:20:19 +0000

    tests/lint: add test for 'possible pointer alignment problem [135]'

--- tests/usr.bin/xlint/lint1/msg_135.c
+++ tests/usr.bin/xlint/lint1/msg_135.c
@@ -1,7 +1,16 @@
-/*	$NetBSD: msg_135.c,v 1.2 2021/02/21 09:07:58 rillig Exp $	*/
+/*	$NetBSD: msg_135.c,v 1.3 2021/02/28 00:20:19 rillig Exp $	*/
 # 3 "msg_135.c"
 
 // Test for message: possible pointer alignment problem [135]
 
-TODO: "Add example code that triggers the above message." /* expect: 249 */
-TODO: "Add example code that almost triggers the above message."
+/* lint1-extra-flags: -h */
+
+unsigned
+read_uint(const unsigned char **pp)
+{
+	unsigned val;
+
+	val = *(const unsigned *)(*pp);
+	pp += sizeof(unsigned);
+	return val;
+}
