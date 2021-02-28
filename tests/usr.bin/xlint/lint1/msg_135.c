f3deda94d83021aca8d16791c2884242790d5b71
rillig <rillig@NetBSD.org>
2021-02-28 01:20:54 +0000

    tests/lint: force 'expect' annotations

--- tests/usr.bin/xlint/lint1/msg_135.c
+++ tests/usr.bin/xlint/lint1/msg_135.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: msg_135.c,v 1.4 2021/02/28 00:40:22 rillig Exp $	*/
+/*	$NetBSD: msg_135.c,v 1.5 2021/02/28 01:20:54 rillig Exp $	*/
 # 3 "msg_135.c"
 
 // Test for message: converting '%s' to '%s' may cause alignment problem [135]
@@ -10,7 +10,7 @@ read_uint(const unsigned char **pp)
 {
 	unsigned val;
 
-	val = *(const unsigned *)(*pp);
+	val = *(const unsigned *)(*pp);	/* expect: 135 */
 	pp += sizeof(unsigned);
 	return val;
 }
