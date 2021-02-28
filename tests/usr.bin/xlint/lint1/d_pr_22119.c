b19e283099975d8ed24a898d56a0fa1c2ca7280d
rillig <rillig@NetBSD.org>
2021-02-28 22:12:16 +0000

    lint: fix null pointer dereference on parse error
    Fixes PR bin/22119.

---
+++ tests/usr.bin/xlint/lint1/d_pr_22119.c
@@ -0,0 +1,18 @@
+/*	$NetBSD: d_pr_22119.c,v 1.1 2021/02/28 22:12:16 rillig Exp $	*/
+# 3 "d_pr_22119.c"
+
+/*
+ * https://gnats.netbsd.org/22119
+ *
+ * Before 2021-02-28, lint crashed in cast() since the target type of the
+ * cast is NULL.
+*/
+
+void
+func1(void)
+{
+	void (*f1)(void);
+
+	f1 = (void (*)(void))p;		/* expect: p undefined [99] */
+	f1 = (void *()(void))p;		/* crash before 2021-02-28 */
+}
