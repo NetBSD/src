3c4f693c91e1ed0a43f748f07bdba261326dc009
rillig <rillig@NetBSD.org>
2021-02-28 01:53:49 +0000

    tests/lint: add test for 'illegal structure pointer combination'

--- tests/usr.bin/xlint/lint1/msg_245.c
+++ tests/usr.bin/xlint/lint1/msg_245.c
@@ -1,7 +1,32 @@
-/*	$NetBSD: msg_245.c,v 1.2 2021/02/21 09:07:58 rillig Exp $	*/
+/*	$NetBSD: msg_245.c,v 1.3 2021/02/28 01:53:49 rillig Exp $	*/
 # 3 "msg_245.c"
 
 // Test for message: illegal structure pointer combination, op %s [245]
 
-TODO: "Add example code that triggers the above message." /* expect: 249 */
-TODO: "Add example code that almost triggers the above message."
+typedef struct tag_and_typedef_tag {
+	int member;
+} tag_and_typedef_typedef;
+
+struct only_tag {
+	int member;
+};
+
+typedef struct {
+	int member;
+} only_typedef;
+
+struct {
+	int member;
+} unnamed;
+
+void sink_bool(_Bool);
+
+void
+example(tag_and_typedef_typedef both,
+	only_typedef only_typedef,
+	struct only_tag only_tag)
+{
+	sink_bool(&both == &only_tag);		/* expect: 245 */
+	sink_bool(&both == &only_typedef);	/* expect: 245 */
+	sink_bool(&both == &unnamed);		/* expect: 245 */
+}
