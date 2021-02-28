4b52b116274f57b297758a16e4cf2551ed03c431
rillig <rillig@NetBSD.org>
2021-02-28 01:06:57 +0000

    lint: make messages for incompatible pointers more specific
    Message 153 didn't state obviously which of the pointer types was the
    one before conversion (or cast) and which was the resulting type.
    
    Message 229 didn't have any type information at all.

--- tests/usr.bin/xlint/lint1/msg_229.c
+++ tests/usr.bin/xlint/lint1/msg_229.c
@@ -1,12 +1,18 @@
-/*	$NetBSD: msg_229.c,v 1.3 2021/02/28 00:52:16 rillig Exp $	*/
+/*	$NetBSD: msg_229.c,v 1.4 2021/02/28 01:06:57 rillig Exp $	*/
 # 3 "msg_229.c"
 
-// Test for message: questionable conversion of function pointer [229]
+// Test for message: converting '%s' to '%s' is questionable [229]
 
 typedef double (*unary_operator)(double);
 
 int *
-example(unary_operator op)
+to_int_pointer(unary_operator op)
 {
 	return (int *)op;
+}
+
+unary_operator
+to_function_pointer(int *p)
+{
+	return (unary_operator)p;
 }
