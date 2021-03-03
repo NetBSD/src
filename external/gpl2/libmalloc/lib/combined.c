38630e8cdb72ca58d3bc84331b90dfb56e9a3db8
christos <christos@NetBSD.org>
2021-03-03 21:46:43 +0000

    Add some jemalloc stubs so we can link as a jemalloc replacement

--- external/gpl2/libmalloc/lib/combined.c
+++ external/gpl2/libmalloc/lib/combined.c
@@ -4,9 +4,17 @@
  * and libc sometimes invokes realloc, which can greatly confuse things
  * in the linking process...
  *
- *	$Id: combined.c,v 1.1 2016/01/13 21:56:38 christos Exp $
+ *	$Id: combined.c,v 1.2 2021/03/03 21:46:43 christos Exp $
  */
 
 #include "malloc.c"
 #include "free.c"
 #include "realloc.c"
+
+/* jemalloc defines these */
+void _malloc_prefork(void);
+void _malloc_postfork(void); 
+void _malloc_postfork_child(void);
+void _malloc_prefork(void) {}
+void _malloc_postfork(void) {}
+void _malloc_postfork_child(void) {}
