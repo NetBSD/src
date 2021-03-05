72d331ef12a6a3373e8eb9f670f9c0d8a99081b0
martin <martin@NetBSD.org>
2021-03-05 13:34:19 +0000

    Pull up the following (all via patch), requested by mrg in ticket #1221:
    	lib/lua/bozohttpd/Makefile			(apply patch)
    	libexec/httpd/Makefile				1.30-1.31
    	libexec/httpd/Makefile.boot			1.7-1.9
    	libexec/httpd/auth-bozo.c			1.25-1.26
    	libexec/httpd/bozohttpd.8			1.80-1.87
    	libexec/httpd/bozohttpd.c			1.114-1.123,1.125-1.128
    	libexec/httpd/bozohttpd.h			1.61-1.68
    	libexec/httpd/cgi-bozo.c			1.49-1.53
    	libexec/httpd/content-bozo.c			1.17-1.20
    	libexec/httpd/daemon-bozo.c			1-.22
    	libexec/httpd/dir-index-bozo.c			1.33-1.34
    	libexec/httpd/main.c				1.23-1.27
    	libexec/httpd/printenv.lua			1.4-1.5
    	libexec/httpd/ssl-bozo.c			1.27-1.29
    	libexec/httpd/libbozohttpd/libbozohttpd.3	1.5-1.6
    	libexec/httpd/small/Makefile			1.4
    	libexec/httpd/testsuite/Makefile		1.14
    	libexec/httpd/testsuite/t16.in			1.1
    	libexec/httpd/testsuite/t16.out			1.1
    	libexec/httpd/testsuite/t17.in			1.1
    	libexec/httpd/testsuite/t17.out			1.1
    	libexec/httpd/testsuite/t18.in			1.1
    	libexec/httpd/testsuite/t18.out			1.1
    
    
    Update to bozohttpd 20210227.
    Apply lua build fix (no blocklist support on this branch).
    
    changes in bozohttpd 20210227:
    	o  new support for content types: .tar.bz2, .tar.xz, .tar.lz,
    	   .tar.zst, .tbz2, .txz, .tlz, .zipx, .xz, .zst, .sz, .lz, .lzma,
    	   .lzo, .7z, .lzo, .cab, .dmg, .jar, and .rar.  should fix
    	   netbsd PR#56026:
    	   MIME type of .tar.xz file on ny{cdn,ftp}.NetBSD.org is invalid
    
    changes in bozohttpd 20210211:
    	o  fix various NULL derefs from malformed headers.  mostly from
    	   <emily@ingalls.rocks>.
    	o  fix memory leaks in library interface: add bozo_cleanup().
    
    changes in bozohttpd 20201014:
    	o  also set -D_GNU_SOURCE in Makefile.boot.  from
    	   hadrien.lacour@posteo.net.
    	o  fix array size botch (assertion, not exploitable.)  from
    	   martin@netbsd.org.
    	o  also match %2F as well as %2f.  from leah@vuxu.org.
    	o  many manual and help fixes.  clean ups for higher lint levels,
    	   consistency/style clean ups.  various option fixes including made
    	   -f imply -b.  from <henrik@gulbra.net> for freebsd.
    
    changes in bozohttpd 20200912:
    	o  add .m4a and .m4v file extensions.
    
    changes in bozohttpd 20200820:
    	o  make this work on sun2 by reducing mmap window there.
    	o  fix SSL shutdown sequence.  from spz@netbsd.org.
    	o  add readme support to directory indexing.  from jmcneill@netbsd.org
    	o  add blocklist(8) support.  from jruoho@netbsd.org.

--- libexec/httpd/auth-bozo.c
+++ libexec/httpd/auth-bozo.c
@@ -1,9 +1,9 @@
-/*	$NetBSD: auth-bozo.c,v 1.24 2019/02/28 08:28:21 mrg Exp $	*/
+/*	$NetBSD: auth-bozo.c,v 1.24.2.1 2021/03/05 13:34:19 martin Exp $	*/
 
 /*	$eterna: auth-bozo.c,v 1.17 2011/11/18 09:21:15 mrg Exp $	*/
 
 /*
- * Copyright (c) 1997-2019 Matthew R. Green
+ * Copyright (c) 1997-2020 Matthew R. Green
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -105,6 +105,11 @@ bozo_auth_check(bozo_httpreq_t *request, const char *file)
 					pass) != 0)
 				break;
 			fclose(fp);
+
+#ifndef NO_BLOCKLIST_SUPPORT
+			pfilter_notify(BLOCKLIST_AUTH_OK, 200);
+#endif /* !NO_BLOCKLIST_SUPPORT */
+
 			return 0;
 		}
 	}
--- libexec/httpd/cgi-bozo.c
+++ libexec/httpd/cgi-bozo.c
@@ -1,9 +1,9 @@
-/*	$NetBSD: cgi-bozo.c,v 1.48 2019/03/29 14:46:44 martin Exp $	*/
+/*	$NetBSD: cgi-bozo.c,v 1.48.2.1 2021/03/05 13:34:19 martin Exp $	*/
 
 /*	$eterna: cgi-bozo.c,v 1.40 2011/11/18 09:21:15 mrg Exp $	*/
 
 /*
- * Copyright (c) 1997-2019 Matthew R. Green
+ * Copyright (c) 1997-2021 Matthew R. Green
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -137,7 +137,7 @@ finish_cgi_output(bozohttpd_t *httpd, bozo_httpreq_t *request, int in, int nph)
 		 * return it in the response.  If the cgi program does
 		 * not return any `Status:' header then the server should
 		 * respond with 200 OK.
-		 * XXX The CGI 1.1 and 1.2 specification differ slightly on
+		 * The CGI 1.1 and 1.2 specification differ slightly on
 		 * this in that v1.2 says that the script MUST NOT return a
 		 * `Status:' header if it is returning a `Location:' header.
 		 * For compatibility we are going with the CGI 1.1 behavior.
@@ -182,8 +182,7 @@ finish_cgi_output(bozohttpd_t *httpd, bozo_httpreq_t *request, int in, int nph)
 		bozo_flush(httpd, stdout);
 	}
 
-	/* XXX we should have some goo that times us out
-	 */
+	/* CGI programs should perform their own timeouts */
 	while ((rbytes = read(in, buf, sizeof buf)) > 0) {
 		ssize_t wbytes;
 		char *bp = buf;
@@ -611,10 +610,16 @@ bozo_process_cgi(bozo_httpreq_t *request)
 		bozo_daemon_closefds(httpd);
 
 		if (-1 == execve(path, argv, envp)) {
+			int saveerrno = errno;
 			bozo_http_error(httpd, 404, request,
 				"Cannot execute CGI");
-			bozoerr(httpd, 1, "child exec failed: %s: %s",
-			      path, strerror(errno));
+			/* don't log easy to trigger events */
+			if (saveerrno != ENOENT &&
+			    saveerrno != EISDIR &&
+			    saveerrno != EACCES)
+				bozoerr(httpd, 1, "child exec failed: %s: %s",
+				      path, strerror(saveerrno));
+			_exit(1);
 		}
 		/* NOT REACHED */
 		bozoerr(httpd, 1, "child execve returned?!");
@@ -638,6 +643,8 @@ bozo_process_cgi(bozo_httpreq_t *request)
 		/* child reader/writer */
 		close(STDIN_FILENO);
 		finish_cgi_output(httpd, request, sv[0], nph);
+		/* if we do SSL, send a SSL_shutdown now */
+		bozo_ssl_shutdown(request->hr_httpd);
 		/* if we're done output, our parent is useless... */
 		kill(getppid(), SIGKILL);
 		debug((httpd, DEBUG_FAT, "done processing cgi output"));
@@ -645,8 +652,7 @@ bozo_process_cgi(bozo_httpreq_t *request)
 	}
 	close(STDOUT_FILENO);
 
-	/* XXX we should have some goo that times us out
-	 */
+	/* CGI programs should perform their own timeouts */
 	while ((rbytes = bozo_read(httpd, STDIN_FILENO, buf, sizeof buf)) > 0) {
 		ssize_t wbytes;
 		char *bp = buf;
