bc819e85e68f114190841371a4782e06476f072e
thorpej <thorpej@NetBSD.org>
2021-03-05 18:10:06 +0000

    Separate probing for the console device and initializing it, so that ofwoea_initppc() can have more control over which of those steps are performed during initialization.  Probing happens before setting up the exception vectors, initializing happens after.

--- sys/arch/powerpc/include/ofw_cons.h
+++ sys/arch/powerpc/include/ofw_cons.h
@@ -1,9 +1,10 @@
-/* $NetBSD: ofw_cons.h,v 1.2 2007/10/17 19:56:41 garbled Exp $ */
+/* $NetBSD: ofw_cons.h,v 1.3 2021/03/05 18:10:06 thorpej Exp $ */
 
 #ifndef _POWERPC_OFW_CONS_H_
 #define _POWERPC_OFW_CONS_H_
 
-void ofwoea_consinit(void);
-int ofkbd_cngetc(dev_t dev);
+void	ofwoea_cnprobe(void);
+void	ofwoea_consinit(void);
+int	ofkbd_cngetc(dev_t dev);
 
 #endif /* _POWERPC_OFW_CONS_H_ */
