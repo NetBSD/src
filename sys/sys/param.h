36956c1902bef988c9a08f5de40ca143a6da5826
christos <christos@NetBSD.org>
2021-03-08 20:02:47 +0000

    welcome to 9.99.81, ip_randomid() signature change.

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.690 2021/02/19 14:51:59 christos Exp $	*/
+/*	$NetBSD: param.h,v 1.691 2021/03/08 20:02:47 christos Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -67,7 +67,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	999008000	/* NetBSD 9.99.80 */
+#define	__NetBSD_Version__	999008100	/* NetBSD 9.99.81 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
