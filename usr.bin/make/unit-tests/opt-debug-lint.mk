db1ca772542bf9d977de4004ab2f64eea47b89f9
rillig <rillig@NetBSD.org>
2021-03-14 10:57:12 +0000

    make: fix wrong expression evaluation in -dL mode
    The modifier ':C' now only compiles the regular expression if the result
    of the expression is actually needed.
    
    Several other modifiers have the same bug of evaluating the expression
    in cases where this is not needed.  It just doesn't show up because they
    don't have any noticeable side effects, other than wasting CPU time.
    This affects irrelevant conditions as well.

--- usr.bin/make/unit-tests/opt-debug-lint.mk
+++ usr.bin/make/unit-tests/opt-debug-lint.mk
@@ -1,4 +1,4 @@
-# $NetBSD: opt-debug-lint.mk,v 1.13 2021/03/14 10:45:51 rillig Exp $
+# $NetBSD: opt-debug-lint.mk,v 1.14 2021/03/14 10:57:12 rillig Exp $
 #
 # Tests for the -dL command line option, which runs additional checks
 # to catch common mistakes, such as unclosed variable expressions.
@@ -77,12 +77,15 @@ ${UNDEF}: ${UNDEF}
 .  error
 .endif
 
-# Before var.c 1.XXX from 2021-03-14, the whole variable text was evaluated
-# to check for unclosed expressions and unknown operators.  During this check,
-# the subexpression '${:U2}' was not expanded, instead it was copied verbatim
-# into the regular expression, which was '.*=.{1,${:U2}}$'.  This regular
-# expression was then compiled (despite VARE_WANTRES being unset), which
-# resulted in a wrong error message.
+# In lint mode, the whole variable text is evaluated to check for unclosed
+# expressions and unknown operators.  During this check, the subexpression
+# '${:U2}' is not expanded, instead it is copied verbatim into the regular
+# expression, leading to '.*=.{1,${:U2}}$'.
+#
+# Before var.c 1.856 from 2021-03-14, this regular expression was then
+# compiled even though that was not necessary for checking the syntax at the
+# level of variable expressions.  The unexpanded '$' then resulted in a wrong
+# error message.
 #
 # This only happened in lint mode since in default mode the early check for
 # unclosed expressions and unknown modifiers is skipped.
