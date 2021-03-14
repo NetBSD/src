d5b354c4f244b18844bff1e496c77c241f3a046c
rillig <rillig@NetBSD.org>
2021-03-14 21:44:35 +0000

    tests/lint: add test for message 247 for pointer to unnamed struct

--- tests/usr.bin/xlint/lint1/msg_247.c
+++ tests/usr.bin/xlint/lint1/msg_247.c
@@ -1,7 +1,21 @@
-/*	$NetBSD: msg_247.c,v 1.3 2021/02/28 12:40:00 rillig Exp $	*/
+/*	$NetBSD: msg_247.c,v 1.4 2021/03/14 21:44:35 rillig Exp $	*/
 # 3 "msg_247.c"
 
 // Test for message: pointer cast from '%s' to '%s' may be troublesome [247]
 
-TODO: "Add example code that triggers the above message." /* expect: 249 */
-TODO: "Add example code that almost triggers the above message."
+/* lint1-extra-flags: -c */
+
+/* example taken from Xlib.h */
+typedef struct {
+	int id;
+} *PDisplay;
+
+struct Other {
+	int id;
+};
+
+void
+example(struct Other *arg)
+{
+	PDisplay display = (PDisplay)arg;	/* expect: 247 */
+}
