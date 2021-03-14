0c8fc40bd03ffd218d9874aecba8d42e59dc1281
rillig <rillig@NetBSD.org>
2021-03-14 22:24:24 +0000

    tests/lint: document how lint discards the typedef name in 247

--- tests/usr.bin/xlint/lint1/msg_247.c
+++ tests/usr.bin/xlint/lint1/msg_247.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: msg_247.c,v 1.4 2021/03/14 21:44:35 rillig Exp $	*/
+/*	$NetBSD: msg_247.c,v 1.5 2021/03/14 22:24:24 rillig Exp $	*/
 # 3 "msg_247.c"
 
 // Test for message: pointer cast from '%s' to '%s' may be troublesome [247]
@@ -17,5 +17,16 @@ struct Other {
 void
 example(struct Other *arg)
 {
-	PDisplay display = (PDisplay)arg;	/* expect: 247 */
+	PDisplay display;
+
+	/*
+	 * XXX: The target type is reported as 'struct <unnamed>'.  In cases
+	 *  like these, it would be helpful to print at least the type name
+	 *  of the pointer.  This type name though is discarded immediately
+	 *  when the parser reduces 'T_TYPENAME clrtyp' to 'clrtyp_typespec'.
+	 *  After that, the target type of the cast is just an unnamed struct,
+	 *  with no hint at all that there is a typedef for a pointer to the
+	 *  struct.
+	 */
+	display = (PDisplay)arg;	/* expect: 247 */
 }
