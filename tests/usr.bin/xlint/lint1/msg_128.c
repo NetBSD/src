1fb09ddd8c9f43aa80ea15df05fe34f332907332
rillig <rillig@NetBSD.org>
2021-03-16 23:12:30 +0000

    tests/lint: add test for message 128 about incompatible pointers

--- tests/usr.bin/xlint/lint1/msg_128.c
+++ tests/usr.bin/xlint/lint1/msg_128.c
@@ -1,7 +1,11 @@
-/*	$NetBSD: msg_128.c,v 1.2 2021/02/21 09:07:58 rillig Exp $	*/
+/*	$NetBSD: msg_128.c,v 1.3 2021/03/16 23:12:30 rillig Exp $	*/
 # 3 "msg_128.c"
 
 // Test for message: operands have incompatible pointer types, op %s (%s != %s) [128]
 
-TODO: "Add example code that triggers the above message." /* expect: 249 */
-TODO: "Add example code that almost triggers the above message."
+void
+conversion_to_unconst(const char *cstr)
+{
+	char *str;
+	str = cstr;		/* expect: 128 */
+}
