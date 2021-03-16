b5f9ba124b694edbfd84721a69470e8aeddb9b69
rillig <rillig@NetBSD.org>
2021-03-16 23:18:49 +0000

    tests/lint: add test for message 139 about division by zero

--- tests/usr.bin/xlint/lint1/msg_139.c
+++ tests/usr.bin/xlint/lint1/msg_139.c
@@ -1,7 +1,24 @@
-/*	$NetBSD: msg_139.c,v 1.2 2021/02/21 09:07:58 rillig Exp $	*/
+/*	$NetBSD: msg_139.c,v 1.3 2021/03/16 23:18:49 rillig Exp $	*/
 # 3 "msg_139.c"
 
 // Test for message: division by 0 [139]
 
-TODO: "Add example code that triggers the above message." /* expect: 249 */
-TODO: "Add example code that almost triggers the above message."
+void sink_int(int);
+void sink_double(double);
+
+void
+example(int i)
+{
+	enum {
+		zero = 0
+	};
+
+	sink_int(i / 0);	/* only triggers in constant expressions */
+	sink_int(i / zero);	/* only triggers in constant expressions */
+	sink_double(i / 0.0);
+
+	sink_int(13 / 0);	/* expect: 139 */
+	sink_int(13 / zero);	/* expect: 139 */
+	sink_double(13 / 0.0);	/* expect: 139 *//* XXX: Clang doesn't warn */
+	sink_double(13 / -0.0);	/* expect: 139 *//* XXX: Clang doesn't warn */
+}
