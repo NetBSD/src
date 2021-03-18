73bec62fd8a55d03b33ab77c11c93dffebc12bb6
rillig <rillig@NetBSD.org>
2021-03-18 21:20:21 +0000

    tests/lint: add test for type mismatch in initialization
    Copied and adapted from d_c99_init.c.

--- tests/usr.bin/xlint/lint1/msg_185.c
+++ tests/usr.bin/xlint/lint1/msg_185.c
@@ -1,7 +1,19 @@
-/*	$NetBSD: msg_185.c,v 1.3 2021/02/22 15:09:50 rillig Exp $	*/
+/*	$NetBSD: msg_185.c,v 1.4 2021/03/18 21:20:21 rillig Exp $	*/
 # 3 "msg_185.c"
 
 // Test for message: initialization type mismatch (%s) and (%s) [185]
 
-TODO: "Add example code that triggers the above message." /* expect: 249 */
-TODO: "Add example code that almost triggers the above message."
+typedef struct any {
+	const void *value;
+} any;
+
+void use(const void *);
+
+void
+initialization_with_redundant_braces(any arg)
+{
+	any local = { 3.0 };	/* expect: 185 */
+	use(&arg);
+}
+
+// TODO: message 185 needs to be reworded to "cannot initialize '%s' from '%s'".
