812baf4f2abbdedc31102383ddee769f722e4432
martin <martin@NetBSD.org>
2021-03-22 18:06:17 +0000

    Pull up following revision(s) (requested by knakahara in ticket #1233):
    	sys/net/if_l2tp.h: revision 1.10
    
    Fix l2tp(4) ioctl type. Pointed out by yamaguchi@n.o, thanks.
    XXX pullup-[89]

--- sys/net/if_l2tp.h
+++ sys/net/if_l2tp.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: if_l2tp.h,v 1.6.4.1 2019/09/24 03:10:35 martin Exp $	*/
+/*	$NetBSD: if_l2tp.h,v 1.6.4.2 2021/03/22 18:06:17 martin Exp $	*/
 
 /*
  * Copyright (c) 2017 Internet Initiative Japan Inc.
@@ -44,11 +44,11 @@
 #include <net/if_ether.h>
 #include <netinet/in.h>
 
-#define	SIOCSL2TPSESSION	_IOW('i', 151, struct l2tp_req)
-#define	SIOCDL2TPSESSION	_IOW('i', 152, struct l2tp_req)
-#define	SIOCSL2TPCOOKIE		_IOW('i', 153, struct l2tp_req)
-#define	SIOCDL2TPCOOKIE		_IOW('i', 154, struct l2tp_req)
-#define	SIOCSL2TPSTATE		_IOW('i', 155, struct l2tp_req)
+#define	SIOCSL2TPSESSION	_IOW('i', 151, struct ifreq)
+#define	SIOCDL2TPSESSION	_IOW('i', 152, struct ifreq)
+#define	SIOCSL2TPCOOKIE		_IOW('i', 153, struct ifreq)
+#define	SIOCDL2TPCOOKIE		_IOW('i', 154, struct ifreq)
+#define	SIOCSL2TPSTATE		_IOW('i', 155, struct ifreq)
 #define	SIOCGL2TP		SIOCGIFGENERIC
 
 struct l2tp_req {
