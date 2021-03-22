e5cdbbff735f7e04bcaf617778463d94c4b2cad6
rillig <rillig@NetBSD.org>
2021-03-22 15:05:00 +0000

    lint: demonstrate bug in assignment of compound literal
    Seen in external/mpl/bind/dist/lib/dns/rbtdb.c, update_rrsetstats.

--- tests/usr.bin/xlint/lint1/msg_171.c
+++ tests/usr.bin/xlint/lint1/msg_171.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: msg_171.c,v 1.2 2021/01/24 16:12:45 rillig Exp $	*/
+/*	$NetBSD: msg_171.c,v 1.3 2021/03/22 15:05:00 rillig Exp $	*/
 # 3 "msg_171.c"
 
 // Test for message: assignment type mismatch (%s != %s) [171]
@@ -16,4 +16,22 @@ example(int i, void *vp, struct s *s)
 
 	vp = *s;		/* expect: 171 */
 	*s = vp;		/* expect: 171 */
+}
+
+/*
+ * C99 6.5.2.5 says that a compound literal evaluates to an unnamed object
+ * with automatic storage duration, like any normal named object.  It is an
+ * lvalue, which means that it is possible to take the address of the object.
+ * Seen in external/mpl/bind/dist/lib/dns/rbtdb.c, update_rrsetstats.
+ */
+void
+pointer_to_compound_literal(void)
+{
+	struct point {
+		int x;
+		int y;
+	};
+	struct point *p = &(struct point){
+	    12, 5,
+	};			/* expect: 171 *//*FIXME*/
 }
