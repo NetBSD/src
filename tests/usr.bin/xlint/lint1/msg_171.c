56f5eec48a1c195edb7bdb87b96110db013e836e
rillig <rillig@NetBSD.org>
2021-03-22 16:51:24 +0000

    tests/lint: explain wrong type mismatch in compound literal
    When a pointer to a compound literal is used as an initializer, lint
    reports a wrong type mismatch.  The details of what happens are now
    documented, which allows this problem to be fixed properly.

--- tests/usr.bin/xlint/lint1/msg_171.c
+++ tests/usr.bin/xlint/lint1/msg_171.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: msg_171.c,v 1.4 2021/03/22 15:29:43 rillig Exp $	*/
+/*	$NetBSD: msg_171.c,v 1.5 2021/03/22 16:51:24 rillig Exp $	*/
 # 3 "msg_171.c"
 
 // Test for message: cannot assign to '%s' from '%s' [171]
@@ -34,4 +34,20 @@ pointer_to_compound_literal(void)
 	struct point *p = &(struct point){
 	    12, 5,
 	};			/* expect: 171 *//*FIXME*/
+	/*
+	 * FIXME: The type mismatch in the above line occurs because lint
+	 * wrongly assumes that there is only ever a single initialization
+	 * going on, which takes place in initstk.
+	 *
+	 * In the debug log, this is marked by the two calls to initstack_init
+	 * that are happily intermixed with 'begin initialization' and 'end
+	 * initialization'.  This was not planned for, and it worked well
+	 * before C99, since compound literals are a new feature from C99.
+	 *
+	 * The proper fix, as for so many similar problems is to not use
+	 * global variables for things that have a limited lifetime, but
+	 * instead let the grammar determine the lifetime and scope of these
+	 * objects, which makes them only accessible when they can actually be
+	 * used.
+	 */
 }
