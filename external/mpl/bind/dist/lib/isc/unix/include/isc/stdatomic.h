14934156165f537048e394787e781447c41c6146
rillig <rillig@NetBSD.org>
2021-03-27 00:06:27 +0000

    bind: fix typo in stdatomic to support lint + Clang
    Before this fix, lint complained:
    > dist/lib/isc/queue.c(203): error: expected undefined [99]
    
    https://gitlab.isc.org/isc-projects/bind9/-/issues/2601

--- external/mpl/bind/dist/lib/isc/unix/include/isc/stdatomic.h
+++ external/mpl/bind/dist/lib/isc/unix/include/isc/stdatomic.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: stdatomic.h,v 1.3 2021/02/19 16:42:20 christos Exp $	*/
+/*	$NetBSD: stdatomic.h,v 1.4 2021/03/27 00:06:27 rillig Exp $	*/
 
 /*
  * Copyright (C) Internet Systems Consortium, Inc. ("ISC")
@@ -144,7 +144,7 @@ typedef uintmax_t	   atomic_uintmax_t;
 	__c11_atomic_compare_exchange_weak_explicit(obj, expected, desired, \
 						    succ, fail)
 #define atomic_exchange_explicit(obj, desired, order) \
-	__c11_atomic_exchange_explicit(obj, expected, order)
+	__c11_atomic_exchange_explicit(obj, desired, order)
 #elif defined(__GNUC_ATOMICS) /* __atomic builtins */
 #define atomic_init(obj, desired)	 (*obj = desired)
 #define atomic_load_explicit(obj, order) __atomic_load_n(obj, order)
