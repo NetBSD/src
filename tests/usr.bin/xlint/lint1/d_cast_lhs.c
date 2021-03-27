8240c2b3589962ff86a49b69d5953471cc1f4a41
rillig <rillig@NetBSD.org>
2021-03-27 13:59:18 +0000

    tests/lint: move description of tests into the tests themselves
    In most cases the descriptions didn't add much to the test name anyway.

--- tests/usr.bin/xlint/lint1/d_cast_lhs.c
+++ tests/usr.bin/xlint/lint1/d_cast_lhs.c
@@ -1,7 +1,12 @@
-/*	$NetBSD: d_cast_lhs.c,v 1.3 2021/01/31 14:57:28 rillig Exp $	*/
+/*	$NetBSD: d_cast_lhs.c,v 1.4 2021/03/27 13:59:18 rillig Exp $	*/
 # 3 "d_cast_lhs.c"
 
-/* pointer casts are valid lhs lvalues */
+/*
+ * pointer casts are valid lhs lvalues
+ *
+ * XXX: C99 6.5.4 "Cast operators" footnote 85 says "A cast does not yield an
+ * lvalue".  It does not mention any exceptional rule for pointers.
+ */
 struct sockaddr {
 };
 
