49478c0137aa2cf97be912001f7c7691f8fc6e06
rillig <rillig@NetBSD.org>
2021-03-29 22:59:03 +0000

    tests/lint: test initialization without braces

--- tests/usr.bin/xlint/lint1/msg_181.c
+++ tests/usr.bin/xlint/lint1/msg_181.c
@@ -1,7 +1,7 @@
-/*	$NetBSD: msg_181.c,v 1.2 2021/02/21 09:07:58 rillig Exp $	*/
+/*	$NetBSD: msg_181.c,v 1.3 2021/03/29 22:59:03 rillig Exp $	*/
 # 3 "msg_181.c"
 
 // Test for message: {}-enclosed initializer required [181]
 
-TODO: "Add example code that triggers the above message." /* expect: 249 */
-TODO: "Add example code that almost triggers the above message."
+struct { int x; } missing_braces = 3;		/* expect: 181 */
+struct { int x; } including_braces = { 3 };
