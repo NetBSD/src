ccfd8cfeff3cbaf9be5dd51aa44ac0424c9dd446
rin <rin@NetBSD.org>
2021-03-30 02:25:24 +0000

    Support OPB running @ 33MHz for 405GP based boards. No need to modify EMAC_MR1 register this case (STACR_OPBC fields are reserved for 405GP).

--- sys/arch/powerpc/ibm4xx/dev/emacreg.h
+++ sys/arch/powerpc/ibm4xx/dev/emacreg.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: emacreg.h,v 1.4 2020/07/06 09:34:17 rin Exp $	*/
+/*	$NetBSD: emacreg.h,v 1.5 2021/03/30 02:25:24 rin Exp $	*/
 
 /*
  * Copyright 2001 Wasabi Systems, Inc.
@@ -215,6 +215,7 @@
 #define	  STACR_PHYE		  0x00004000	/* PHY error */
 #define	  STACR_WRITE		  0x00002000	/* STA command - write */
 #define	  STACR_READ		  0x00001000	/* STA command - read */
+#define	  STACR_OPBC_33MHZ	  0x0		/*   -  33MHz */
 #define	  STACR_OPBC_50MHZ	  0x0		/*   -  50MHz */
 #define	  STACR_OPBC_66MHZ	  0x1		/*   -  66MHz */
 #define	  STACR_OPBC_83MHZ	  0x2		/*   -  83MHz */
