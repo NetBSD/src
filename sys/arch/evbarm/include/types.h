28b83fe374ae25c1268a59dff56f55c3a4f859c8
simonb <simonb@NetBSD.org>
2021-04-01 04:35:45 +0000

    Whitespace: #define<tab>

--- sys/arch/evbarm/include/types.h
+++ sys/arch/evbarm/include/types.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: types.h,v 1.14 2018/04/01 04:35:05 ryo Exp $	*/
+/*	$NetBSD: types.h,v 1.15 2021/04/01 04:35:45 simonb Exp $	*/
 
 #ifndef _EVBARM_TYPES_H_
 #define	_EVBARM_TYPES_H_
@@ -6,7 +6,7 @@
 #ifdef __aarch64__
 #include <aarch64/types.h>
 
-#define __HAVE_DEVICE_REGISTER
+#define	__HAVE_DEVICE_REGISTER
 #else
 #include <arm/arm32/types.h>
 
--- sys/arch/netwinder/include/types.h
+++ sys/arch/netwinder/include/types.h
@@ -1,11 +1,11 @@
-/* $NetBSD: types.h,v 1.12 2017/01/26 15:55:10 christos Exp $ */
+/* $NetBSD: types.h,v 1.13 2021/04/01 04:35:46 simonb Exp $ */
 
 #ifndef _NETWINDER_TYPES_H_
 #define	_NETWINDER_TYPES_H_
 
 #include <arm/arm32/types.h>
 
-#define __HAVE_NEW_STYLE_BUS_H
+#define	__HAVE_NEW_STYLE_BUS_H
 #define	__HAVE_COMPAT_NETBSD32
 
 #endif
