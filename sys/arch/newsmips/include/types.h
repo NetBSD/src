fa2e10609e824c35160ff913ffc4aa5ada81cf6b
simonb <simonb@NetBSD.org>
2021-04-01 04:42:59 +0000

    Define __HAVE_LEGACY_INTRCNT to indicate this port uses legacy intrcnt interrupt accounting.

--- sys/arch/newsmips/include/types.h
+++ sys/arch/newsmips/include/types.h
@@ -1,7 +1,9 @@
-/*	$NetBSD: types.h,v 1.12 2008/01/20 18:09:08 joerg Exp $	*/
+/*	$NetBSD: types.h,v 1.13 2021/04/01 04:43:00 simonb Exp $	*/
 
 #include <mips/types.h>
 
 /* MIPS specific options */
+
 #define	__HAVE_BOOTINFO_H
+#define	__HAVE_LEGACY_INTRCNT
 #define	__HAVE_MIPS_MACHDEP_CACHE_CONFIG
