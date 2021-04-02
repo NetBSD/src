30eb7039ac55b366d6fc74b8a03e1c0ee0eea9ea
rin <rin@NetBSD.org>
2021-04-02 07:00:33 +0000

    evbppc: Add support for DHT Walnut 405GP evaluation board. (Digital Home Technologies PCB 01070201 Rev. 1.1)
    Official web page seems gone, but analyses by Linux people are at:
    
    - https://elinux.org/DHT-Walnut
    - http://web.archive.org/web/20070225171826/http://supernova.stanford.edu/dingdong/
    
    The board features:
    
    - 266MHz PowerPC 405GP processor
    - one PC133 SDRAM slot
    - two 32-bit 5V PCI slot
    - on-chip ethernet controller with DP83843 PHY
    - on-chip serial port / GPIO controller
    - on-board PDC20265 IDE controller
    
    Hardware limitations:
    
    - no MAC address assigned
    - no RTC present
    
    Known problem:
    
    - atabus(4) channels cannot be attached to pdcide(4) for cold boot
    
    dmesg:
    
    - https://dmesgd.nycbug.org/index.cgi?do=view&id=5997
    
    Have fun!

---
+++ sys/arch/evbppc/dht/autoconf.c
@@ -0,0 +1,79 @@
+/*	$NetBSD: autoconf.c,v 1.1 2021/04/02 07:00:33 rin Exp $	*/
+
+/*
+ * Taken from src/sys/arch/evbppc/walnut/autoconf.c:
+ *	NetBSD: autoconf.c,v 1.26 2021/03/30 05:14:00 rin Exp
+ */
+
+/*
+ * Copyright (C) 1995, 1996 Wolfgang Solfrank.
+ * Copyright (C) 1995, 1996 TooLs GmbH.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed by TooLs GmbH.
+ * 4. The name of TooLs GmbH may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY TOOLS GMBH ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL TOOLS GMBH BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <sys/cdefs.h>
+__KERNEL_RCSID(0, "$NetBSD: autoconf.c,v 1.1 2021/04/02 07:00:33 rin Exp $");
+
+#include <sys/param.h>
+#include <sys/device.h>
+#include <sys/systm.h>
+
+#include <machine/dht.h>
+
+#include <powerpc/spr.h>
+#include <powerpc/ibm4xx/spr.h>
+
+#include <powerpc/ibm4xx/cpu.h>
+#include <powerpc/ibm4xx/dcr4xx.h>
+#include <powerpc/ibm4xx/dev/plbvar.h>
+
+/*
+ * Determine device configuration for a machine.
+ */
+void
+cpu_configure(void)
+{
+
+	intr_init();
+	calc_delayconst();
+
+	/* Make sure that timers run at CPU frequency */
+	mtdcr(DCR_CPC0_CR1, mfdcr(DCR_CPC0_CR1) & ~CPC0_CR1_CETE);
+
+	if (config_rootfound("plb", NULL) == NULL)
+		panic("%s: plb not configured", __func__);
+
+	genppc_cpu_configure();
+}
+
+void
+device_register(device_t dev, void *aux)
+{
+
+	ibm4xx_device_register(dev, aux, DHT_COM_FREQ);
+}
