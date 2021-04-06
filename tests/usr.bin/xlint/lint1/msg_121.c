6e93208e35e6953f8954b0e06522d2c9370aa9d0
rillig <rillig@NetBSD.org>
2021-04-06 21:44:12 +0000

    lint: test what happens for 'int << double'
    I just wanted to see whether typeok_shift would be reached in this case,
    to ensure that is_uinteger applies as expected.  It does.

--- tests/usr.bin/xlint/lint1/msg_121.c
+++ tests/usr.bin/xlint/lint1/msg_121.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: msg_121.c,v 1.3 2021/01/31 11:12:07 rillig Exp $	*/
+/*	$NetBSD: msg_121.c,v 1.4 2021/04/06 21:44:12 rillig Exp $	*/
 # 3 "msg_121.c"
 
 // Test for message: negative shift [121]
@@ -7,4 +7,14 @@ int
 example(int x)
 {
 	return x << (3 - 5);		/* expect: 121 */
+}
+
+void /*ARGSUSED*/
+shift_by_double(int x, double amount)
+{
+	/*
+	 * This is already caught by typeok_scalar, so it doesn't reach
+	 * typeok_shift via typeok_op.
+	 */
+	return x << amount;		/* expect: incompatible types */
 }
