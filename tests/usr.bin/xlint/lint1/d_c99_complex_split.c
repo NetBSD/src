80108d8526c2a3b6a401f99f4fa57d05a53243ca
rillig <rillig@NetBSD.org>
2021-04-09 21:07:39 +0000

    tests/lint: demonstrate wrong lint warning about complex variables

--- tests/usr.bin/xlint/lint1/d_c99_complex_split.c
+++ tests/usr.bin/xlint/lint1/d_c99_complex_split.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: d_c99_complex_split.c,v 1.5 2021/03/27 13:59:18 rillig Exp $	*/
+/*	$NetBSD: d_c99_complex_split.c,v 1.6 2021/04/09 21:07:39 rillig Exp $	*/
 # 3 "d_c99_complex_split.c"
 
 /*
@@ -18,4 +18,36 @@ a(void)
 	double _Complex z = 0;
 	if (b(__real__ z) && b(__imag__ z))
 		return;
+}
+
+void sink(double _Complex);
+
+void
+set_complex_complete(double re, double im)
+{
+	double _Complex c;
+
+	__real__ c = re; /* FIXME *//* expect: may be used before set */
+	__imag__ c = im;
+	sink(c);
+}
+
+void
+set_complex_only_real(double re)
+{
+	double _Complex c;
+
+	__real__ c = re; /* FIXME *//* expect: may be used before set */
+	/* __imag__ c is left uninitialized */
+	sink(c);		/* XXX: may be used before set */
+}
+
+void
+set_complex_only_imag(double im)
+{
+	double _Complex c;
+
+	/* __real__ c is left uninitialized */
+	__imag__ c = im; /* FIXME *//* expect: may be used before set */
+	sink(c);		/* XXX: may be used before set */
 }
