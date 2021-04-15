488a24ca8d334151373c1871ed2739f67f18efd0
rin <rin@NetBSD.org>
2021-04-15 00:04:07 +0000

    sandpoint has its own module_init_md(). So, do not compile in that function in powerpc_machdep.c.

--- sys/arch/sandpoint/include/types.h
+++ sys/arch/sandpoint/include/types.h
@@ -1,6 +1,7 @@
-/*	$NetBSD: types.h,v 1.9 2021/04/01 04:35:47 simonb Exp $	*/
+/*	$NetBSD: types.h,v 1.10 2021/04/15 00:04:07 rin Exp $	*/
 
 #define	__HAVE_NEW_STYLE_BUS_H
+#define	__PPC_HAVE_MODULE_INIT_MD
 
 #include <powerpc/types.h>
 
