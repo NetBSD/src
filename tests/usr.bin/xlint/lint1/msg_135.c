814d2d428aa825aff1609ec976e7770fb34684b2
rillig <rillig@NetBSD.org>
2021-04-17 16:52:09 +0000

    tests/lint: add test for pointer cast from incomplete struct

--- tests/usr.bin/xlint/lint1/msg_135.c
+++ tests/usr.bin/xlint/lint1/msg_135.c
@@ -1,9 +1,11 @@
-/*	$NetBSD: msg_135.c,v 1.5 2021/02/28 01:20:54 rillig Exp $	*/
+/*	$NetBSD: msg_135.c,v 1.6 2021/04/17 16:52:09 rillig Exp $	*/
 # 3 "msg_135.c"
 
 // Test for message: converting '%s' to '%s' may cause alignment problem [135]
 
 /* lint1-extra-flags: -h */
+
+void sink(const void *);
 
 unsigned
 read_uint(const unsigned char **pp)
@@ -13,4 +15,26 @@ read_uint(const unsigned char **pp)
 	val = *(const unsigned *)(*pp);	/* expect: 135 */
 	pp += sizeof(unsigned);
 	return val;
+}
+
+struct incomplete;	/* expect: never defined */
+
+struct complete {
+    int member;
+};
+
+/*
+ * These types of conversions are typically seen in OpenSSL, when converting
+ * from the publicly visible, incomplete 'struct lhash_st' to a private
+ * implementation type such as 'struct lhash_st_OPENSSL_STRING'.
+ *
+ * See openssl/lhash.h.
+ */
+void
+pointer_to_structs(struct incomplete *incomplete)
+{
+	struct complete *complete;
+
+	complete = (struct complete *)incomplete; /* expect: 135 */
+	sink(complete);
 }
