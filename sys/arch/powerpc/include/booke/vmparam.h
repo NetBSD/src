20d045e219282392350f8ad0ee48f3445ed3b733
rin <rin@NetBSD.org>
2021-04-17 13:23:24 +0000

    PR port-powerpc/56107
    Decrease MAXSSIZ from ~256MB to 32MB (same as oea).
    
    This fixes tests in /usr/tests/usr.bin/make, that run with "ulimit -v 200000",
    fail with "Cannot map anonymous memory".
    
    Although I'm not fully convinced whether this limit is reasonable or not,
    old MAXSSIZ of ~256MB is too much anyway.

--- sys/arch/powerpc/include/booke/vmparam.h
+++ sys/arch/powerpc/include/booke/vmparam.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: vmparam.h,v 1.7 2012/10/02 23:51:39 christos Exp $	*/
+/*	$NetBSD: vmparam.h,v 1.8 2021/04/17 13:23:24 rin Exp $	*/
 /*-
  * Copyright (c) 2010, 2011 The NetBSD Foundation, Inc.
  * All rights reserved.
@@ -64,7 +64,7 @@
 #endif
 
 #ifndef	MAXSSIZ
-#define	MAXSSIZ		(1*256*1024*1024-PAGE_SIZE) /* maximum stack size */
+#define	MAXSSIZ		(32*1024*1024)		/* maximum stack size */
 #endif
 
 #ifndef	DFLDSIZ
