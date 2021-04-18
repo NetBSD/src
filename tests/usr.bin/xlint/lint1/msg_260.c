682ea3d6f447b7bdfef2128a7757ab5bc57d5348
rillig <rillig@NetBSD.org>
2021-04-18 07:31:47 +0000

    lint: document wrong location information in diagnostics

--- tests/usr.bin/xlint/lint1/msg_260.c
+++ tests/usr.bin/xlint/lint1/msg_260.c
@@ -1,7 +1,29 @@
-/*	$NetBSD: msg_260.c,v 1.2 2021/02/21 09:07:58 rillig Exp $	*/
+/*	$NetBSD: msg_260.c,v 1.3 2021/04/18 07:31:47 rillig Exp $	*/
 # 3 "msg_260.c"
 
 // Test for message: previous declaration of %s [260]
 
-TODO: "Add example code that triggers the above message." /* expect: 249 */
-TODO: "Add example code that almost triggers the above message."
+/* lint1-extra-flags: -r */
+
+# 100 "header.h" 1
+struct s {		/* expect: 260 */
+    int member;
+};
+# 13 "msg_260.c" 2
+
+# 200 "header.h" 1
+union s {		/* expect: tag redeclared *//* expect: 260 */
+    int member;
+};
+/*
+ * FIXME: the stack trace for the 260 is wrong, as the 260 is included from
+ * line 8, not from line 14.
+ */
+# 19 "msg_160.c" 2
+
+union s {		/* expect: tag redeclared */
+    int member;
+};
+/*
+ * FIXME: the stack trace for the 260 is missing.
+ */
