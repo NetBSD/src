2b59c53498d79abe7d0107ceedeec9373d41d523
christos <christos@NetBSD.org>
2021-04-25 22:27:08 +0000

    All mips64 has compat_netbsd32

--- sys/compat/netbsd32/netbsd32.mk
+++ sys/compat/netbsd32/netbsd32.mk
@@ -1,4 +1,4 @@
-#	$NetBSD: netbsd32.mk,v 1.2 2020/03/06 04:54:08 pgoyette Exp $
+#	$NetBSD: netbsd32.mk,v 1.3 2021/04/25 22:27:08 christos Exp $
 
 # makefile fragment that tells you if you should support netbsd32 or not.
 # include this and check ${COMPAT_USE_NETBSD32} != "no".
@@ -7,7 +7,7 @@
     || ${MACHINE_CPU} == "arm" \
     || ${MACHINE_CPU} == "aarch64" \
     || ${MACHINE_ARCH} == "sparc64" \
-    || (!empty(MACHINE_ARCH:Mmips64*) && !defined(BSD_MK_COMPAT_FILE))
+    || (${MACHINE_MIPS64} && !defined(BSD_MK_COMPAT_FILE))
 COMPAT_USE_NETBSD32?=yes
 .else
 COMPAT_USE_NETBSD32?=no
