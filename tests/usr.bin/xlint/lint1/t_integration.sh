1a28a78369dc81be620b3a49bfaa96a78757dcdd
rillig <rillig@NetBSD.org>
2021-04-30 23:49:36 +0000

    tests/lint: add very basic tests for GCC __attribute__

--- tests/usr.bin/xlint/lint1/t_integration.sh
+++ tests/usr.bin/xlint/lint1/t_integration.sh
@@ -1,4 +1,4 @@
-# $NetBSD: t_integration.sh,v 1.44 2021/04/22 22:43:26 rillig Exp $
+# $NetBSD: t_integration.sh,v 1.45 2021/04/30 23:49:36 rillig Exp $
 #
 # Copyright (c) 2008, 2010 The NetBSD Foundation, Inc.
 # All rights reserved.
@@ -179,6 +179,7 @@ test_case d_long_double_int
 
 test_case emit
 
+test_case gcc_attribute
 test_case gcc_init_compound_literal
 test_case gcc_typeof_after_statement
 
