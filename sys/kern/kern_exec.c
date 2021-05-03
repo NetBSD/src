4d4c4c3cfc84852a37a615fb7b3fb69fac8e1df7
bouyer <bouyer@NetBSD.org>
2021-05-03 09:12:50 +0000

    Pull up following revision(s) (requested by martin in ticket #1265): 	sys/kern/kern_exec.c: revision 1.505 via patch Fix copy&pasto in handling of POSIX_SPAWN_RESETIDS in posix_spawn(3)

--- sys/kern/kern_exec.c
+++ sys/kern/kern_exec.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: kern_exec.c,v 1.478.2.1 2019/10/15 18:32:13 martin Exp $	*/
+/*	$NetBSD: kern_exec.c,v 1.478.2.2 2021/05/03 09:12:50 bouyer Exp $	*/
 
 /*-
  * Copyright (c) 2008 The NetBSD Foundation, Inc.
@@ -59,7 +59,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: kern_exec.c,v 1.478.2.1 2019/10/15 18:32:13 martin Exp $");
+__KERNEL_RCSID(0, "$NetBSD: kern_exec.c,v 1.478.2.2 2021/05/03 09:12:50 bouyer Exp $");
 
 #include "opt_exec.h"
 #include "opt_execfmt.h"
@@ -2124,7 +2124,7 @@ spawn_return(void *arg)
 
 		/* Reset user ID's */
 		if (spawn_data->sed_attrs->sa_flags & POSIX_SPAWN_RESETIDS) {
-			error = do_setresuid(l, -1,
+			error = do_setresgid(l, -1,
 			     kauth_cred_getgid(l->l_cred), -1,
 			     ID_E_EQ_R | ID_E_EQ_S);
 			if (error)
