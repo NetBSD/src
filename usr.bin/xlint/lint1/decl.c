642772b897c33d6ea9cc038d2c1463961c0be1e9
rillig <rillig@NetBSD.org>
2021-05-16 10:34:19 +0000

    lint: add type information to unportable bit-field type
    Seeing the message "unportable bit-field type 'int'" may sound strange
    at first, but that's a strict interpretation of the wording in C99
    6.7.2.1p4, which requires that the bit-field type is "'_Bool', 'unsigned
    int' or 'signed int', or some other implementation-defined type".
    
    The rationale for C99 6.7.2.1 explicitly lists plain 'int' among the
    allowed types for bit-fields, regardless of any additional
    implementation-defined types.  This means that lint had interpreted this
    paragraph wrong, and it should be fixed to allow plain int as well.

--- usr.bin/xlint/lint1/decl.c
+++ usr.bin/xlint/lint1/decl.c
@@ -1,4 +1,4 @@
-/* $NetBSD: decl.c,v 1.180 2021/05/02 22:07:49 rillig Exp $ */
+/* $NetBSD: decl.c,v 1.181 2021/05/16 10:34:19 rillig Exp $ */
 
 /*
  * Copyright (c) 1996 Christopher G. Demetriou.  All Rights Reserved.
@@ -38,7 +38,7 @@
 
 #include <sys/cdefs.h>
 #if defined(__RCSID) && !defined(lint)
-__RCSID("$NetBSD: decl.c,v 1.180 2021/05/02 22:07:49 rillig Exp $");
+__RCSID("$NetBSD: decl.c,v 1.181 2021/05/16 10:34:19 rillig Exp $");
 #endif
 
 #include <sys/param.h>
@@ -1106,14 +1106,14 @@ check_bit_field_type(sym_t *dsym,  type_t **const inout_tp, tspec_t *inout_t)
 				/* bit-field type '%s' invalid in ANSI C */
 				warning(273, type_name(tp));
 			} else if (pflag) {
-				/* nonportable bit-field type */
-				warning(34);
+				/* nonportable bit-field type '%s' */
+				warning(34, type_name(tp));
 			}
 		}
 	} else if (t == INT && dcs->d_sign_mod == NOTSPEC) {
 		if (pflag && !bitfieldtype_ok) {
-			/* nonportable bit-field type */
-			warning(34);
+			/* nonportable bit-field type '%s' */
+			warning(34, type_name(tp));
 		}
 	} else if (t != INT && t != UINT && t != BOOL) {
 		/*
