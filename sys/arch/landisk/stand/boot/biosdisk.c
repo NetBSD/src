98934d571891189396c8a4415a22648070c55ba4
mrg <mrg@NetBSD.org>
2021-05-17 19:31:38 +0000

    include libkern.h to get moved dkcksum() definition.

--- sys/arch/landisk/stand/boot/biosdisk.c
+++ sys/arch/landisk/stand/boot/biosdisk.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: biosdisk.c,v 1.3 2011/07/17 20:54:43 joerg Exp $	*/
+/*	$NetBSD: biosdisk.c,v 1.4 2021/05/17 19:31:38 mrg Exp $	*/
 
 /*
  * Copyright (c) 1996, 1998
@@ -66,6 +66,7 @@
 #include <sys/types.h>
 #include <sys/disklabel.h>
 
+#include <lib/libkern/libkern.h>
 #include <lib/libsa/stand.h>
 #include <lib/libsa/saerrno.h>
 #include <lib/libsa/loadfile.h>
