cb2e4330b0652e5369e98c25cc675ded999d6820
dholland <dholland@NetBSD.org>
2021-05-23 23:22:55 +0000

    Improve ddb disassembly for mips (and riscv, cribbed from mips).
    - use db_read_bytes to get instructions
    - move the address check logic previously attached only to fetching
      instructions for disassembly to db_read_bytes (and db_write_bytes)
    
    Motivated by related x86 changes this afternoon.
    
    Note that the address check logic is not as sophisticated as what the
    x86 code does, but it's what we had before. (Except that riscv will
    now also try to fetch usermode instructions instead of just failing.)

--- sys/arch/riscv/riscv/db_disasm.c
+++ sys/arch/riscv/riscv/db_disasm.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: db_disasm.c,v 1.7 2021/05/01 06:48:51 skrll Exp $	*/
+/*	$NetBSD: db_disasm.c,v 1.8 2021/05/23 23:22:55 dholland Exp $	*/
 
 /*-
  * Copyright (c) 2014 The NetBSD Foundation, Inc.
@@ -31,7 +31,7 @@
 
 #include <sys/cdefs.h>
 
-__RCSID("$NetBSD: db_disasm.c,v 1.7 2021/05/01 06:48:51 skrll Exp $");
+__RCSID("$NetBSD: db_disasm.c,v 1.8 2021/05/23 23:22:55 dholland Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -39,6 +39,7 @@ __RCSID("$NetBSD: db_disasm.c,v 1.7 2021/05/01 06:48:51 skrll Exp $");
 #include <riscv/db_machdep.h>
 #include <riscv/insn.h>
 
+#include <ddb/db_access.h>
 #include <ddb/db_user.h>
 #include <ddb/db_interface.h>
 #include <ddb/db_output.h>
@@ -1463,23 +1464,16 @@ db_disasm(db_addr_t loc, bool altfmt)
 	unsigned n, i;
 	uint32_t insn32;
 
-#ifdef _KERNEL
-	if ((intptr_t) loc >= 0) {
-		db_printf("%s: %#"PRIxVADDR" is not a kernel address\n",
-		    __func__, loc);
-		return loc;
-	}
-#endif
-
 	/*
 	 * Fetch the instruction. The first halfword tells us how many
 	 * more there are, and they're always in little-endian order.
 	 */
-	insn[0] = ((const uint16_t *)loc)[0];
+	db_read_bytes(loc, sizeof(insn[0]), (void *)&insn[0]);
 	n = INSN_HALFWORDS(insn[0]);
 	KASSERT(n > 0 && n <= 5);
 	for (i = 1; i < n; i++) {
-		insn[i] = ((const uint16_t *)loc)[i];
+		db_read_bytes(loc + i * sizeof(insn[i]), sizeof(insn[i]),
+			      (void *)&insn[i]);
 	}
 
 	switch (n) {
