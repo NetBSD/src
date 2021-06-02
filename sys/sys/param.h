06c4a00fb31a21447573a97e207e95d26d82d6f1
yamaguchi <yamaguchi@NetBSD.org>
2021-06-02 01:48:08 +0000

    NetBSD 9.99.83 - struct ifnet adds 'if_lagg' field

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.692 2021/04/24 23:40:16 thorpej Exp $	*/
+/*	$NetBSD: param.h,v 1.693 2021/06/02 01:48:08 yamaguchi Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -67,7 +67,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	999008200	/* NetBSD 9.99.82 */
+#define	__NetBSD_Version__	999008300	/* NetBSD 9.99.83 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
