61b363bc0366d6413d04a3e59f0264c7b39690a7
rin <rin@NetBSD.org>
2021-06-04 01:42:14 +0000

    Add missing nbsd_init_abi() call to aarch64_nbsd_init_abi().
    Fix tests/usr.bin/gdb/t_regress:pie on aarch64eb (types of auxv was
    misinterpreted), and hopefully other strange behaviors on aarch64{,eb}.
    
    This is a regression introduced in GDB11. Neither gdb.old nor release
    branches are affected.

--- external/gpl3/gdb/dist/gdb/aarch64-nbsd-tdep.c
+++ external/gpl3/gdb/dist/gdb/aarch64-nbsd-tdep.c
@@ -182,6 +182,8 @@ aarch64_nbsd_init_abi (struct gdbarch_info info, struct gdbarch *gdbarch)
 {
   struct gdbarch_tdep *tdep = gdbarch_tdep (gdbarch);
 
+  nbsd_init_abi (info, gdbarch);
+
   set_solib_svr4_fetch_link_map_offsets (gdbarch,
 					 svr4_lp64_fetch_link_map_offsets);
 
