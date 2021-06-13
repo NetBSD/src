1ad700643c8d081922fe388a2670f581fbacdaf6
christos <christos@NetBSD.org>
2021-06-13 03:07:57 +0000

    bump, something changed and the kernel does not boot with .84 modules.

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.694 2021/06/09 15:49:57 martin Exp $	*/
+/*	$NetBSD: param.h,v 1.695 2021/06/13 03:07:57 christos Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -67,7 +67,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	999008400	/* NetBSD 9.99.84 */
+#define	__NetBSD_Version__	999008500	/* NetBSD 9.99.84 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
