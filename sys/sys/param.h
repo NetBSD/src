002d9cc8da67e20c06392af8799380714d2d20cd
martin <martin@NetBSD.org>
2021-06-13 15:11:20 +0000

    Fix the last bump - now for real: welcome to 9.99.85

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.695 2021/06/13 03:07:57 christos Exp $	*/
+/*	$NetBSD: param.h,v 1.696 2021/06/13 15:11:20 martin Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -67,7 +67,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	999008500	/* NetBSD 9.99.84 */
+#define	__NetBSD_Version__	999008500	/* NetBSD 9.99.85 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
