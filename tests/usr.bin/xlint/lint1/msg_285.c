1ceb9c821ca47ca6b8304a3f9b0015a437ce22b5
rillig <rillig@NetBSD.org>
2021-06-15 08:48:49 +0000

    tests/lint: add test for previous prototype declaration

--- tests/usr.bin/xlint/lint1/msg_285.c
+++ tests/usr.bin/xlint/lint1/msg_285.c
@@ -1,7 +1,15 @@
-/*	$NetBSD: msg_285.c,v 1.2 2021/02/21 09:07:58 rillig Exp $	*/
+/*	$NetBSD: msg_285.c,v 1.3 2021/06/15 08:48:49 rillig Exp $	*/
 # 3 "msg_285.c"
 
 // Test for message: prototype declaration [285]
 
-TODO: "Add example code that triggers the above message." /* expect: 249 */
-TODO: "Add example code that almost triggers the above message."
+/* lint1-extra-flags: -r */
+
+void function(int, int, int);	/* expect: 285 */
+
+/* ARGSUSED */
+extern void
+function(a, b)
+    int a, b;
+{				/* expect: 3 declared, 2 defined */
+}
