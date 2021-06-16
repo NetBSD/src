68bcbf010861953e0669b007697e66796ede9449
rillig <rillig@NetBSD.org>
2021-06-16 10:21:51 +0000

    tests/lint: sort the list of test cases
    No functional change.

--- tests/usr.bin/xlint/lint1/t_integration.sh
+++ tests/usr.bin/xlint/lint1/t_integration.sh
@@ -1,4 +1,4 @@
-# $NetBSD: t_integration.sh,v 1.51 2021/06/16 10:19:23 rillig Exp $
+# $NetBSD: t_integration.sh,v 1.52 2021/06/16 10:21:51 rillig Exp $
 #
 # Copyright (c) 2008, 2010 The NetBSD Foundation, Inc.
 # All rights reserved.
@@ -98,73 +98,74 @@ test_case()
 }
 
 
+test_case all_messages
+test_case d_alignof
 test_case d_bltinoffsetof
 test_case d_c99_anon_struct
 test_case d_c99_anon_union
 test_case d_c99_bool
 test_case d_c99_bool_strict
 test_case d_c99_bool_strict_syshdr
+test_case d_c99_complex_num
+test_case d_c99_complex_split
 test_case d_c99_compound_literal_comma
+test_case d_c99_decls_after_stmt
 test_case d_c99_decls_after_stmt2
+test_case d_c99_decls_after_stmt3
 test_case d_c99_flex_array_packed
+test_case d_c99_for_loops
+test_case d_c99_func
 test_case d_c99_init
 test_case d_c99_nested_struct
-test_case d_c99_union_cast
-test_case d_c99_union_init4
-test_case d_c99_union_init5
-test_case d_cast_fun_array_param
-test_case d_cast_typeof
-test_case d_decl_old_style_arguments
-test_case d_fold_test
-test_case d_gcc_extension
-test_case d_init_array_using_string
-test_case d_init_pop_member
-test_case d_lint_assert
-test_case d_return_type
-test_case d_type_question_colon
-test_case d_typefun
-test_case d_typename_as_var
+test_case d_c99_recursive_init
 test_case d_c99_struct_init
+test_case d_c99_union_cast
 test_case d_c99_union_init1
 test_case d_c99_union_init2
 test_case d_c99_union_init3
-test_case d_c99_recursive_init
+test_case d_c99_union_init4
+test_case d_c99_union_init5
+test_case d_c9x_array_init
 test_case d_c9x_recursive_init
-test_case d_nested_structs
-test_case d_packed_structs
-test_case d_pr_22119
-test_case d_struct_init_nested
+test_case d_cast_fun_array_param
 test_case d_cast_init
 test_case d_cast_init2
 test_case d_cast_lhs
-test_case d_gcc_func
-test_case d_c99_func
-test_case d_gcc_variable_array_init
-test_case d_c9x_array_init
-test_case d_c99_decls_after_stmt
-test_case d_c99_decls_after_stmt3
-test_case d_nolimit_init
-test_case d_zero_sized_arrays
+test_case d_cast_typeof
 test_case d_compound_literals1
 test_case d_compound_literals2
+test_case d_constant_conv1
+test_case d_constant_conv2
+test_case d_cvt_constant
+test_case d_cvt_in_ternary
+test_case d_decl_old_style_arguments
+test_case d_ellipsis_in_switch
+test_case d_fold_test
 test_case d_gcc_compound_statements1
 test_case d_gcc_compound_statements2
 test_case d_gcc_compound_statements3
-test_case d_cvt_in_ternary
-test_case d_cvt_constant
-test_case d_ellipsis_in_switch
-test_case d_c99_complex_num
-test_case d_c99_complex_split
-test_case d_c99_for_loops
-test_case d_alignof
+test_case d_gcc_extension
+test_case d_gcc_func
+test_case d_gcc_variable_array_init
+test_case d_incorrect_array_size
+test_case d_init_array_using_string
+test_case d_init_pop_member
+test_case d_lint_assert
+test_case d_long_double_int
+test_case d_nested_structs
+test_case d_nolimit_init
+test_case d_packed_structs
+test_case d_pr_22119
+test_case d_return_type
 test_case d_shift_to_narrower_type
-test_case d_constant_conv1
-test_case d_constant_conv2
+test_case d_struct_init_nested
 test_case d_type_conv1
 test_case d_type_conv2
 test_case d_type_conv3
-test_case d_incorrect_array_size
-test_case d_long_double_int
+test_case d_type_question_colon
+test_case d_typefun
+test_case d_typename_as_var
+test_case d_zero_sized_arrays
 test_case emit
 test_case expr_range
 test_case feat_stacktrace
@@ -175,8 +176,6 @@ test_case gcc_init_compound_literal
 test_case gcc_typeof_after_statement
 test_case op_colon
 
-
-test_case all_messages
 all_messages_body()
 {
 	local failed msg
