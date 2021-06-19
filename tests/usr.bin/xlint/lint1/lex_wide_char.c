61e55b3b2393102a61f3492c2374341bf168177b
rillig <rillig@NetBSD.org>
2021-06-19 08:30:08 +0000

    tests/lint: add test cases for lexical analysis

---
+++ tests/usr.bin/xlint/lint1/lex_wide_char.c
@@ -0,0 +1,42 @@
+/*	$NetBSD: lex_wide_char.c,v 1.1 2021/06/19 08:30:08 rillig Exp $	*/
+# 3 "lex_wide_char.c"
+
+/*
+ * Tests for lexical analysis of character constants.
+ *
+ * C99 6.4.4.4 "Character constants"
+ */
+
+void sink(int);
+
+void
+test(void)
+{
+	/* TODO: expect+1: empty character constant */
+	sink(L'');
+
+	sink(L'a');
+
+	sink(L'\0');
+
+	/* UTF-8 */
+	/* expect+1: too many characters in character constant */
+	sink(L'ä');
+	/* rescue the parser: ' */
+
+	/* GCC extension */
+	/* expect+1: dubious escape \e */
+	sink(L'\e');
+
+	/* since C99 */
+	sink(L'\x12');
+
+	/* octal */
+	sink(L'\177');
+
+	/* newline */
+	sink(L'\n');
+
+	/* TODO: expect+1: empty character constant */
+	sink(L'');
+}
