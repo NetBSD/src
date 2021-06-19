a899d3aef1ab94bd765bf80e96b135a842fdb41d
rillig <rillig@NetBSD.org>
2021-06-19 08:37:18 +0000

    lint: revert fix for endless loop in lexer for string literals
    String literals may contain null bytes, and these must be passed further
    on.
    
    This reintroduces the endless loop in the lexer, but that must be fixed
    in another way that doesn't destroy the error handling.

--- tests/usr.bin/xlint/lint1/lex_string.c
+++ tests/usr.bin/xlint/lint1/lex_string.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: lex_string.c,v 1.1 2021/06/19 08:30:08 rillig Exp $	*/
+/*	$NetBSD: lex_string.c,v 1.2 2021/06/19 08:37:18 rillig Exp $	*/
 # 3 "lex_string.c"
 
 /*
@@ -18,18 +18,11 @@ test(void)
 
 	sink("\0");
 
-	/* expect+5: unknown character \134 */
-	/* expect+4: syntax error '0' */
-	/* expect+3: unknown character \134 */
-	/* expect+2: unknown character \134 */
-	/* expect+1: unknown character \134 */
 	sink("\0\0\0\0");
 
-	/* expect+1: unknown character \134 */
+	/* expect+1: no hex digits follow \x [74] */
 	sink("\x");		/* unfinished */
 
-	/* expect+1: unknown character \134 */
+	/* expect+1: dubious escape \y [79] */
 	sink("\y");		/* unknown escape sequence */
 }
-
-/* expect+1: cannot recover from previous errors */
