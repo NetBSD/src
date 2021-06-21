0ea19b48ddf589bbdb9f80be6105215a6a813e86
martin <martin@NetBSD.org>
2021-06-21 17:27:57 +0000

    Pull up following revision(s) (requested by riastradh in ticket #1306):
    	sys/dev/usb/umass.c: revision 1.185
    	sys/dev/usb/umass_scsipi.c: revision 1.68
    
    umass(4): Use an empty function callback, not null pointer.
    
    This stupid bug, with an `XXX Broken!' comment right above, has been
    preventing NetBSD from suspend/resume with a USB drive plugged in for
    longer than I want to even think about admitting.  *sigh*
    umass(4): Assert that we got a cb up front.
    
    Avoids jump to zero waaaaaaay down the line where we've forgotten why
    we wanted to jump into oblivion.

--- sys/dev/usb/umass_scsipi.c
+++ sys/dev/usb/umass_scsipi.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: umass_scsipi.c,v 1.62 2019/05/30 21:44:49 mlelstv Exp $	*/
+/*	$NetBSD: umass_scsipi.c,v 1.62.2.1 2021/06/21 17:27:57 martin Exp $	*/
 
 /*
  * Copyright (c) 2001, 2003, 2012 The NetBSD Foundation, Inc.
@@ -31,7 +31,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: umass_scsipi.c,v 1.62 2019/05/30 21:44:49 mlelstv Exp $");
+__KERNEL_RCSID(0, "$NetBSD: umass_scsipi.c,v 1.62.2.1 2021/06/21 17:27:57 martin Exp $");
 
 #ifdef _KERNEL_OPT
 #include "opt_usb.h"
@@ -101,6 +101,8 @@ Static int umass_scsipi_ioctl(struct scsipi_channel *, u_long,
 Static int umass_scsipi_getgeom(struct scsipi_periph *,
 				struct disk_parms *, u_long);
 
+Static void umass_null_cb(struct umass_softc *, void *,
+			  int, int);
 Static void umass_scsipi_cb(struct umass_softc *, void *,
 			    int, int);
 Static void umass_scsipi_sense_cb(struct umass_softc *, void *,
@@ -319,7 +321,7 @@ umass_scsipi_request(struct scsipi_channel *chan,
 						  cmdlen, xs->data,
 						  xs->datalen, dir,
 						  xs->timeout, USBD_SYNCHRONOUS,
-						  0, xs);
+						  umass_null_cb, xs);
 			DPRINTFM(UDMASS_SCSI, "done err=%jd",
 			    scbus->sc_sync_status, 0, 0, 0);
 			switch (scbus->sc_sync_status) {
@@ -416,6 +418,12 @@ umass_scsipi_getgeom(struct scsipi_periph *periph, struct disk_parms *dp,
 	default:
 		return 0;
 	}
+}
+
+Static void
+umass_null_cb(struct umass_softc *sc, void *priv, int residue, int status)
+{
+	UMASSHIST_FUNC(); UMASSHIST_CALLED();
 }
 
 Static void
