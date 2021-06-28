b7e7673fbae17a35979c6ad91822ebd2b0bb8159
rillig <rillig@NetBSD.org>
2021-06-28 11:09:35 +0000

    lint: fix parse error for type 'void (*)[*]'

--- tests/usr.bin/xlint/lint1/msg_155.c
+++ tests/usr.bin/xlint/lint1/msg_155.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: msg_155.c,v 1.5 2021/06/28 10:29:05 rillig Exp $	*/
+/*	$NetBSD: msg_155.c,v 1.6 2021/06/28 11:09:35 rillig Exp $	*/
 # 3 "msg_155.c"
 
 // Test for message: passing '%s' to incompatible '%s', arg #%d [155]
@@ -8,7 +8,7 @@ void c99_6_7_6_example_a(int);
 void c99_6_7_6_example_b(int *);
 void c99_6_7_6_example_c(int *[3]);
 void c99_6_7_6_example_d(int (*)[3]);
-void c99_6_7_6_example_e(int (*)[*]);	/* expect: syntax error ']' *//* FIXME */
+void c99_6_7_6_example_e(int (*)[*]);
 // FIXME: assertion "sym->s_type != NULL" failed in declare_argument at decl.c:2436
 // void c99_6_7_6_example_f(int *());
 void c99_6_7_6_example_g(int (*)(void));
@@ -34,8 +34,7 @@ provoke_error_messages(struct incompatible arg)
 	/* expect+1: 'pointer to array[3] of int' */
 	c99_6_7_6_example_d(arg);
 
-	/* TODO: C99 says 'pointer to a variable length array of an unspecified number of ints' */
-	/* FIXME: no warning or error at all for an undefined function? */
+	/* expect+1: 'pointer to array[unknown_size] of int' */
 	c99_6_7_6_example_e(arg);
 
 	/* TODO: C99 says 'function with no parameter specification returning a pointer to int' */
