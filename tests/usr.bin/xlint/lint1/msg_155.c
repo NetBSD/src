cabaeef1bd6ec2ffce3a4723c0c8f615c711382a
rillig <rillig@NetBSD.org>
2021-06-28 10:07:43 +0000

    lint: turn null pointer dereference into assertion failure
    Originally I only needed a message that would output the type name from
    an abstract-declarator (C99 6.7.6), to see whether lint interprets the
    types correctly.
    
    Message 155 looked like a good candidate, but it only revealed more
    incomplete and untested code in lint.

--- tests/usr.bin/xlint/lint1/msg_155.c
+++ tests/usr.bin/xlint/lint1/msg_155.c
@@ -1,7 +1,47 @@
-/*	$NetBSD: msg_155.c,v 1.2 2021/02/21 09:07:58 rillig Exp $	*/
+/*	$NetBSD: msg_155.c,v 1.3 2021/06/28 10:07:43 rillig Exp $	*/
 # 3 "msg_155.c"
 
 // Test for message: argument is incompatible with prototype, arg #%d [155]
+// TODO: Add type information to the message
 
-TODO: "Add example code that triggers the above message." /* expect: 249 */
-TODO: "Add example code that almost triggers the above message."
+void c99_6_7_6_example_a(int);
+void c99_6_7_6_example_b(int *);
+void c99_6_7_6_example_c(int *[3]);
+void c99_6_7_6_example_d(int (*)[3]);
+void c99_6_7_6_example_e(int (*)[*]);	/* expect: syntax error ']' *//* FIXME */
+// FIXME: assertion "sym->s_type != NULL" failed in declare_argument at decl.c:2436
+// void c99_6_7_6_example_f(int *());
+void c99_6_7_6_example_g(int (*)(void));
+void c99_6_7_6_example_h(int (*const[])(unsigned int, ...));
+
+struct incompatible {
+	int member;
+};
+
+void
+provoke_error_messages(struct incompatible arg)
+{
+	/* expect+1: argument is incompatible with prototype, arg #1 */
+	c99_6_7_6_example_a(arg);
+
+	/* expect+1: argument is incompatible with prototype, arg #1 */
+	c99_6_7_6_example_b(arg);
+
+	/* expect+1: argument is incompatible with prototype, arg #1 */
+	c99_6_7_6_example_c(arg);
+
+	/* expect+1: argument is incompatible with prototype, arg #1 */
+	c99_6_7_6_example_d(arg);
+
+	/* FIXME: no warning or error at all for an undefined function? */
+	c99_6_7_6_example_e(arg);
+
+	/* FIXME: no warning or error at all for an undefined function? */
+	c99_6_7_6_example_f(arg);
+
+	/* expect+1: argument is incompatible with prototype, arg #1 */
+	c99_6_7_6_example_g(arg);
+
+	/* expect+1: argument is incompatible with prototype, arg #1 */
+	c99_6_7_6_example_h(arg);
+}
