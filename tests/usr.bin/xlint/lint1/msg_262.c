0b52e712a643c6ab10a12af6309a710a0aee60d4
rillig <rillig@NetBSD.org>
2021-06-29 07:17:43 +0000

    tests/lint: add tests for the lexical analysis

--- tests/usr.bin/xlint/lint1/msg_262.c
+++ tests/usr.bin/xlint/lint1/msg_262.c
@@ -1,7 +1,8 @@
-/*	$NetBSD: msg_262.c,v 1.2 2021/02/21 09:07:58 rillig Exp $	*/
+/*	$NetBSD: msg_262.c,v 1.3 2021/06/29 07:17:43 rillig Exp $	*/
 # 3 "msg_262.c"
 
-// Test for message: \" inside character constants undefined in traditional C [262]
+/* Test for message: \" inside character constants undefined in traditional C [262] */
 
-TODO: "Add example code that triggers the above message." /* expect: 249 */
-TODO: "Add example code that almost triggers the above message."
+/* lint1-flags: -tw */
+
+char msg = '\"';		/* expect: [262] */
