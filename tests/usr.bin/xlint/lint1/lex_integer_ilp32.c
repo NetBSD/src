5f9106a664e670d1a03c03f49899b9a4c46224ec
rillig <rillig@NetBSD.org>
2021-06-29 09:19:17 +0000

    tests/lint: add tests for ILP32 platforms
    Previously, all tests for lint had to produce the exact same output, no
    matter which platform they ran on.  This differs from practical needs
    since lint is intended to produce different results depending on whether
    the platform is ILP32 or LP64.
    
    Examples for these are type conversions and the widths of the integer
    types during lexical analysis.

---
+++ tests/usr.bin/xlint/lint1/lex_integer_ilp32.c
@@ -0,0 +1,44 @@
+/*	$NetBSD: lex_integer_ilp32.c,v 1.1 2021/06/29 09:19:17 rillig Exp $	*/
+# 3 "lex_integer_ilp32.c"
+
+/*
+ * Tests for lexical analysis of integer constants.
+ *
+ * C99 6.4.4.1 "Integer constants"
+ */
+
+/* lint1-only-on-ilp32 */
+
+void sinki(int);
+void sinku(unsigned int);
+
+/* All platforms supported by lint have 32-bit int in two's complement. */
+void
+test_signed_int(void)
+{
+	sinki(0);
+
+	sinki(-1);
+
+	sinki(2147483647);
+
+	/* expect+1: 'unsigned long' to 'int' is out of range, arg #1 [295] */
+	sinki(2147483648);
+
+	sinki(-2147483647);
+
+	/* expect+2: ANSI C treats constant as unsigned, op - [218] */
+	/* expect+1: 'unsigned long' to 'int' is out of range, arg #1 [295] */
+	sinki(-2147483648);
+}
+
+void
+test_unsigned_int(void)
+{
+	sinku(0);
+
+	sinku(4294967295U);
+
+	/* expect+1: integer constant out of range [252] */
+	sinku(4294967296U);
+}
