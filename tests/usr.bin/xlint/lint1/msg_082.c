e92c22d95d59c78e8da8372e2a3406f44c13cda1
rillig <rillig@NetBSD.org>
2021-06-29 07:17:43 +0000

    tests/lint: add tests for the lexical analysis

--- tests/usr.bin/xlint/lint1/msg_082.c
+++ tests/usr.bin/xlint/lint1/msg_082.c
@@ -1,8 +1,8 @@
-/*	$NetBSD: msg_082.c,v 1.3 2021/01/31 11:12:07 rillig Exp $	*/
+/*	$NetBSD: msg_082.c,v 1.4 2021/06/29 07:17:43 rillig Exp $	*/
 # 3 "msg_082.c"
 
 /* Test for message: \x undefined in traditional C [82] */
 
 /* lint1-flags: -Stw */
 
-char str[] = "A he\x78 escape";		/* expect: 82 */
+char str[] = "A he\x78 escape";		/* expect: [82] */
--- tests/usr.bin/xlint/lint1/msg_250.c
+++ tests/usr.bin/xlint/lint1/msg_250.c
@@ -1,7 +1,7 @@
-/*	$NetBSD: msg_250.c,v 1.2 2021/02/21 09:07:58 rillig Exp $	*/
+/*	$NetBSD: msg_250.c,v 1.3 2021/06/29 07:17:43 rillig Exp $	*/
 # 3 "msg_250.c"
 
 // Test for message: unknown character \%o [250]
 
-TODO: "Add example code that triggers the above message." /* expect: 249 */
-TODO: "Add example code that almost triggers the above message."
+@deprecated			/* expect: unknown character \100 [250] */
+char *gets(void);		/* expect: syntax error 'char' [249] */
