e6e831efb166660ffaadecc3c4bd91ae5066a1f5
rillig <rillig@NetBSD.org>
2021-07-02 23:29:54 +0000

    tests/lint: encode the array length in the diagnostic

--- tests/usr.bin/xlint/lint1/c99_init_array.c
+++ tests/usr.bin/xlint/lint1/c99_init_array.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: c99_init_array.c,v 1.2 2021/07/02 22:46:43 rillig Exp $	*/
+/*	$NetBSD: c99_init_array.c,v 1.3 2021/07/02 23:29:54 rillig Exp $	*/
 # 3 "c99_init_array.c"
 
 /*
@@ -10,10 +10,10 @@
 // The size of the array is determined by the maximum index, not by the last
 // one mentioned.
 int arr_11[] = { [10] = 10, [0] = 0 };
-typedef int ctassert_11[sizeof(arr_11) / sizeof(arr_11[0]) == 11 ? -1 : 1];
-/* expect-1: error: negative array dimension (-1) [20] */
+typedef int ctassert_11[-(int)(sizeof(arr_11) / sizeof(arr_11[0]))];
+/* expect-1: error: negative array dimension (-11) [20] */
 
 // Without an explicit subscript designator, the subscript counts up.
 int arr_3[] = { [1] = 1, [0] = 0, 1, 2 };
-typedef int ctassert_3[sizeof(arr_3) / sizeof(arr_3[0]) == 3 ? -1 : 1];
-/* expect-1: error: negative array dimension (-1) [20] */
+typedef int ctassert_3[-(int)(sizeof(arr_3) / sizeof(arr_3[0]))];
+/* expect-1: error: negative array dimension (-3) [20] */
