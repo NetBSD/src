933a6ade0164fffa82e8b47c3665716e46a003f5
rillig <rillig@NetBSD.org>
2021-07-02 21:52:36 +0000

    tests/lint: add test for array subscripts in C99 initialization

---
+++ tests/usr.bin/xlint/lint1/c99_init_array.c
@@ -0,0 +1,19 @@
+/*	$NetBSD: c99_init_array.c,v 1.1 2021/07/02 21:52:36 rillig Exp $	*/
+# 3 "c99_init_array.c"
+
+/*
+ * Test C99-style initialization of arrays.
+ */
+
+/* lint1-extra-flags: -p */
+
+// The size of the array is determined by the maximum index, not by the last
+// one mentioned.
+int arr_11[] = { [10] = 10, [0] = 0 };
+typedef int ctassert_11[sizeof(arr_11) / sizeof(arr_11[0]) == 11 ? -1 : 1];
+/* TODO: expect-1: error: negative array dimension (-1) [20] */
+
+// Without an explicit subscript designator, the subscript counts up.
+int arr_3[] = { [1] = 1, [0] = 0, 1, 2 };
+typedef int ctassert_3[sizeof(arr_3) / sizeof(arr_3[0]) == 3 ? -1 : 1];
+/* expect-1: error: negative array dimension (-1) [20] */
