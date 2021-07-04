1f1aea66d6f4334e4007617438faa45c91f45dc5
rillig <rillig@NetBSD.org>
2021-07-04 13:44:43 +0000

    tests/lint: fix unintended character constant in test
    I originally intended to type ASCII DEL as an example of a nonprintable
    character code.  The actual character that landed in the code was
    instead U+2303 "house", which looks similar to the typical
    representation of the DEL character.  The UTF-8 byte sequence for that
    code point started with \xE2, or in octal \342.

--- tests/usr.bin/xlint/lint1/msg_080.c
+++ tests/usr.bin/xlint/lint1/msg_080.c
@@ -1,9 +1,7 @@
-/*	$NetBSD: msg_080.c,v 1.3 2021/06/29 07:17:43 rillig Exp $	*/
+/*	$NetBSD: msg_080.c,v 1.4 2021/07/04 13:44:43 rillig Exp $	*/
 # 3 "msg_080.c"
 
 // Test for message: dubious escape \%o [80]
 
-/* expect+3: dubious escape \342 [80] */ /* FIXME: Why 342? */
-/* expect+2: multi-character character constant [294] */
-/* expect+1: initializer does not fit */
-char backslash_delete = '\⌂';
+/* expect+1: dubious escape \177 [80] */
+char backslash_delete = '\';
