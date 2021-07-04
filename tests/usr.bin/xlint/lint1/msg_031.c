d87e0b65b25c9e7856eb01209723f23ac60eb86d
rillig <rillig@NetBSD.org>
2021-07-04 13:31:10 +0000

    lint: improve wording for incomplete struct or union

--- tests/usr.bin/xlint/lint1/msg_031.c
+++ tests/usr.bin/xlint/lint1/msg_031.c
@@ -1,7 +1,7 @@
-/*	$NetBSD: msg_031.c,v 1.4 2021/03/16 23:39:41 rillig Exp $	*/
+/*	$NetBSD: msg_031.c,v 1.5 2021/07/04 13:31:10 rillig Exp $	*/
 # 3 "msg_031.c"
 
-// Test for message: incomplete structure or union %s: %s [31]
+// Test for message: argument '%s' has type '%s' [31]
 
 struct complete {
 	int dummy;
@@ -14,5 +14,6 @@ struct complete complete_var;
 
 struct incomplete incomplete_var;	/* expect: 31 */
 
-/* XXX: the 'incomplete: <unnamed>' in the diagnostic looks strange */
-void function(struct incomplete);	/* expect: incomplete: <unnamed> [31] */
+
+/* expect+1: error: argument '<unnamed>' has type 'incomplete struct incomplete' [31] */
+void function(struct incomplete);
