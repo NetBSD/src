a70e7bc559c76f2e5d4434516cc9e2029ccc1619
rillig <rillig@NetBSD.org>
2021-07-06 17:33:07 +0000

    tests/lint: add tests for GCC __attribute__
    Before fixing the wrong handling of __attribute__ that is demonstrated
    at the end of gcc_attribute.c, ensure that the attribute handling works
    in the most basic cases.
    
    Lint currently accepts __attribute__ in more places than it should.
    This leads to some ambiguities in the grammar.

---
+++ tests/usr.bin/xlint/lint1/gcc_attribute_stmt.c
@@ -0,0 +1,36 @@
+/*	$NetBSD: gcc_attribute_stmt.c,v 1.1 2021/07/06 17:33:07 rillig Exp $	*/
+# 3 "gcc_attribute_stmt.c"
+
+/*
+ * Tests for the GCC __attribute__ for statements.
+ *
+ * https://gcc.gnu.org/onlinedocs/gcc/Statement-Attributes.html
+ */
+
+void println(const char *);
+
+void
+attribute_fallthrough(int i)
+{
+	switch (i) {
+	case 5:
+		/*
+		 * The attribute 'fallthrough' is only valid after a
+		 * preceding statement. This is already caught by GCC, so
+		 * lint does not need to care.
+		 */
+		__attribute__((__fallthrough__));
+	case 3:
+		println("odd");
+		__attribute__((__fallthrough__));
+	case 2:
+		/*
+		 * Only the null statement can have the attribute
+		 * 'fallthrough'. This is already caught by GCC, so
+		 * lint does not need to care.
+		 */
+		/* expect+2: error: syntax error '__attribute__' [249] */
+		println("prime")
+		    __attribute__((__fallthrough__));
+	}
+}
