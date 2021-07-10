c417019bf57c68eb5286937f0537c24266e73799
rillig <rillig@NetBSD.org>
2021-07-10 18:34:03 +0000

    tests/lint: add another example for storage class in declaration

--- tests/usr.bin/xlint/lint1/msg_083.c
+++ tests/usr.bin/xlint/lint1/msg_083.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: msg_083.c,v 1.3 2021/01/31 11:12:07 rillig Exp $	*/
+/*	$NetBSD: msg_083.c,v 1.4 2021/07/10 18:34:03 rillig Exp $	*/
 # 3 "msg_083.c"
 
 // Test for message: storage class after type is obsolescent [83]
@@ -8,3 +8,8 @@ example(void)
 {
 	int register x;		/* expect: 83 */
 }
+
+struct {
+	int member;
+} typedef s;
+/* expect-1: warning: storage class after type is obsolescent [83] */
