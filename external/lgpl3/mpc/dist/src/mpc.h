e20a6b3e047c9fd92b1e98790b49efe292e46d56
mrg <mrg@NetBSD.org>
2021-07-11 20:13:00 +0000

    initial import of MPC 1.2.1.  from their NEWS:
    Changes in version 1.2.1, released in October 2020:
      - Bug fixes:
        - Fix an incompatibility problem with GMP 6.0 and before.
        - Fix an intermediate overflow in asin.

--- external/lgpl3/mpc/dist/src/mpc.h
+++ external/lgpl3/mpc/dist/src/mpc.h
@@ -27,8 +27,8 @@ along with this program. If not, see http://www.gnu.org/licenses/ .
 /* Define MPC version number */
 #define MPC_VERSION_MAJOR 1
 #define MPC_VERSION_MINOR 2
-#define MPC_VERSION_PATCHLEVEL 0
-#define MPC_VERSION_STRING "1.2.0"
+#define MPC_VERSION_PATCHLEVEL 1
+#define MPC_VERSION_STRING "1.2.1"
 
 /* Macros dealing with MPC VERSION */
 #define MPC_VERSION_NUM(a,b,c) (((a) << 16L) | ((b) << 8) | (c))
