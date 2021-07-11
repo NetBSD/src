88c97df113f9b09a2e464c4027ae006a0416ed28
rillig <rillig@NetBSD.org>
2021-07-11 19:30:56 +0000

    lint: add quotes around placeholders in message 23 and 232

--- tests/usr.bin/xlint/lint1/msg_232.c
+++ tests/usr.bin/xlint/lint1/msg_232.c
@@ -1,16 +1,15 @@
-/*	$NetBSD: msg_232.c,v 1.4 2021/07/11 19:24:42 rillig Exp $	*/
+/*	$NetBSD: msg_232.c,v 1.5 2021/07/11 19:30:56 rillig Exp $	*/
 # 3 "msg_232.c"
 
-// Test for message: label %s unused in function %s [232]
+// Test for message: label '%s' unused in function '%s' [232]
 
 void
 example(void)
 {
 	goto used_label;
-unused_label:			/* expect: 232 */
+	/* expect+1: label 'unused_label' unused in function 'example' [232] */
+unused_label:
 	return;
 used_label:
 	return;
 }
-
-/* TODO: add quotes around '%s' */
