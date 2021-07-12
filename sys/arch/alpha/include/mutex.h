8a2f8e9aff5915fe1219437544ebe98db7cf1e32
thorpej <thorpej@NetBSD.org>
2021-07-12 15:21:51 +0000

    Optimized fast-paths for mutex_spin_enter() and mutex_spin_exit().

--- sys/arch/alpha/include/mutex.h
+++ sys/arch/alpha/include/mutex.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: mutex.h,v 1.8 2020/09/23 00:52:49 thorpej Exp $	*/
+/*	$NetBSD: mutex.h,v 1.9 2021/07/12 15:21:51 thorpej Exp $	*/
 
 /*-
  * Copyright (c) 2002, 2006, 2007 The NetBSD Foundation, Inc.
@@ -59,6 +59,7 @@ struct kmutex {
 
 #define	__HAVE_SIMPLE_MUTEXES		1
 #define	__HAVE_MUTEX_STUBS		1
+#define	__HAVE_SPIN_MUTEX_STUBS		1
 
 #define	MUTEX_CAS(p, o, n)		_lock_cas((p), (o), (n))
 
