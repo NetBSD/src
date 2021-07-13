0e34d175f8d82ac0b744d118d2a20b0d096e1615
rillig <rillig@NetBSD.org>
2021-07-13 19:11:35 +0000

    tests/lint: skip test for floating point overflow on alpha
    On alpha and a few other platforms (see t_integration.sh), 'long double'
    has 128 bit, which under IEEE 754 rules means a decimal exponent of up
    to 4932.

--- tests/usr.bin/xlint/lint1/msg_142.c
+++ tests/usr.bin/xlint/lint1/msg_142.c
@@ -1,6 +1,14 @@
-/*	$NetBSD: msg_142.c,v 1.4 2021/04/05 01:35:34 rillig Exp $	*/
+/*	$NetBSD: msg_142.c,v 1.5 2021/07/13 19:11:35 rillig Exp $	*/
 # 3 "msg_142.c"
 
 // Test for message: floating point overflow detected, op %s [142]
 
-double dbl = 1e100 * 1e100 * 1e100 * 1e100 * 1e100;	/* expect: 142 *//* expect: 142 */
+/* lint1-only-if ldbl-64 */
+/*
+ * For 96-bit and 128-bit floating point numbers, a different number of
+ * multipliers is needed to produce an overflow.
+ */
+
+/* expect+2: warning: floating point overflow detected, op * [142] */
+/* expect+1: warning: floating point overflow detected, op * [142] */
+double dbl = 1e100 * 1e100 * 1e100 * 1e100 * 1e100;
