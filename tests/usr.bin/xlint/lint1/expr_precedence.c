dc3f79878def2e97db048087ba5283815c82c04b
rillig <rillig@NetBSD.org>
2021-07-15 17:48:10 +0000

    tests/lint: explain global variables in __attribute__

--- tests/usr.bin/xlint/lint1/expr_precedence.c
+++ tests/usr.bin/xlint/lint1/expr_precedence.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: expr_precedence.c,v 1.2 2021/07/15 17:20:58 rillig Exp $	*/
+/*	$NetBSD: expr_precedence.c,v 1.3 2021/07/15 17:48:10 rillig Exp $	*/
 # 3 "expr_precedence.c"
 
 /*
@@ -22,7 +22,15 @@ int init_syntactically_ok = var = 1 ? 2 : 3;
  * don't make sense at that point.
  */
 void __attribute__((format(printf,
-    /* expect+2: error: 'var' undefined [99] */ /* XXX: why? */
+    /*
+     * Inside of __attribute__((...)), symbol lookup works differently.  For
+     * example, 'printf' is a keyword, and since all arguments to
+     * __attribute__ are constant expressions, looking up global variables
+     * would not make sense.  Therefore, 'var' is undefined.
+     *
+     * See lex.c, function 'search', keyword 'attron'.
+     */
+    /* expect+2: error: 'var' undefined [99] */
     /* expect+1: syntax error '=' [249] */
     var = 1,
     /* Syntactically ok, must be a constant expression though. */
