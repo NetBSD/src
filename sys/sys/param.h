32817ac9d7ddefa429ddbf6874f29177f629604e
ozaki-r <ozaki-r@NetBSD.org>
2021-07-21 07:39:58 +0000

    Bump the version for ALTQ/CBQ changes
    Welcome to 9.99.87

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.698 2021/07/19 19:13:33 christos Exp $	*/
+/*	$NetBSD: param.h,v 1.699 2021/07/21 07:39:58 ozaki-r Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -67,7 +67,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	999008600	/* NetBSD 9.99.86 */
+#define	__NetBSD_Version__	999008700	/* NetBSD 9.99.87 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
