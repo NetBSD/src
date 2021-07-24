b80a79792f2c7fd48bc6d90c9caa49eea357c328
andvar <andvar@NetBSD.org>
2021-07-24 22:30:59 +0000

    s/structres/structures + s/whenver/whenever

--- sys/arch/acorn32/podulebus/if_ie.c
+++ sys/arch/acorn32/podulebus/if_ie.c
@@ -1,4 +1,4 @@
-/* $NetBSD: if_ie.c,v 1.48 2020/02/05 08:33:19 skrll Exp $ */
+/* $NetBSD: if_ie.c,v 1.49 2021/07/24 22:30:59 andvar Exp $ */
 
 /*
  * Copyright (c) 1995 Melvin Tang-Richardson.
@@ -53,7 +53,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: if_ie.c,v 1.48 2020/02/05 08:33:19 skrll Exp $");
+__KERNEL_RCSID(0, "$NetBSD: if_ie.c,v 1.49 2021/07/24 22:30:59 andvar Exp $");
 
 #define IGNORE_ETHER1_IDROM_CHECKSUM
 
@@ -94,7 +94,7 @@ __KERNEL_RCSID(0, "$NetBSD: if_ie.c,v 1.48 2020/02/05 08:33:19 skrll Exp $");
 #include <netinet/if_inarp.h>
 #endif
 
-/* Import our data structres */
+/* Import our data structures */
 
 #include "if_iereg.h"
 
