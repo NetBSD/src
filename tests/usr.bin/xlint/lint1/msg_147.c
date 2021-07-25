e82b7c6a42449f90e3883d935fc551a341eab88e
rillig <rillig@NetBSD.org>
2021-07-25 10:26:46 +0000

    tests/lint: test function types and pointer arithmetic

--- tests/usr.bin/xlint/lint1/msg_147.c
+++ tests/usr.bin/xlint/lint1/msg_147.c
@@ -1,7 +1,16 @@
-/*	$NetBSD: msg_147.c,v 1.2 2021/02/21 09:07:58 rillig Exp $	*/
+/*	$NetBSD: msg_147.c,v 1.3 2021/07/25 10:26:46 rillig Exp $	*/
 # 3 "msg_147.c"
 
 // Test for message: invalid cast expression [147]
 
-TODO: "Add example code that triggers the above message." /* expect: 249 */
-TODO: "Add example code that almost triggers the above message."
+// The type name 'int(int)' is a 'function(int) returning int'.
+void take(int(int));
+
+/* ARGSUSED */
+void
+call_take(int (*ptr)(int))
+{
+	/* XXX: That's a little too unspecific. */
+	/* expect+1: error: invalid cast expression [147] */
+	take((int(int))ptr);
+}
