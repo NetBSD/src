945ef5c2971ab884d68982f53c2bfca4024c2652
rillig <rillig@NetBSD.org>
2021-07-25 10:26:46 +0000

    tests/lint: test function types and pointer arithmetic

--- tests/usr.bin/xlint/lint1/msg_110.c
+++ tests/usr.bin/xlint/lint1/msg_110.c
@@ -1,7 +1,15 @@
-/*	$NetBSD: msg_110.c,v 1.2 2021/02/21 09:07:58 rillig Exp $	*/
+/*	$NetBSD: msg_110.c,v 1.3 2021/07/25 10:26:46 rillig Exp $	*/
 # 3 "msg_110.c"
 
 // Test for message: pointer to function is not allowed here [110]
 
-TODO: "Add example code that triggers the above message." /* expect: 249 */
-TODO: "Add example code that almost triggers the above message."
+/* ARGSUSED */
+void
+call_take(int (*ptr)(int))
+{
+	/* expect+1: error: pointer to function is not allowed here [110] */
+	ptr++;
+
+	/* expect+1: error: pointer to function is not allowed here [110] */
+	ptr + 1;
+}
