75b7761af5b5964e42fb0abaff2f3de8c2af3fa7
rillig <rillig@NetBSD.org>
2021-07-25 15:58:24 +0000

    lint: do not parse 'typeof(0)(void)' as function call
    Previously, lint tried to parse 'typeof(0)(void)' as 'typeof'
    '(0)(void)', which tries to call 0 as a function.

--- tests/usr.bin/xlint/lint1/gcc_typeof.c
+++ tests/usr.bin/xlint/lint1/gcc_typeof.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: gcc_typeof.c,v 1.2 2021/07/25 11:19:51 rillig Exp $	*/
+/*	$NetBSD: gcc_typeof.c,v 1.3 2021/07/25 15:58:24 rillig Exp $	*/
 # 3 "gcc_typeof.c"
 
 /*
@@ -10,16 +10,7 @@
 void take_double(typeof(0.0));
 
 void take_function_double_returning_double(
-    /*
-     * FIXME: lint's grammar uses 'typeof cast_expression', while GCC's
-     *  c_parser_typeof_specifier uses 'typeof ( expression )'.  The crucial
-     *  difference is that lint parses the following expression as 'typeof
-     *  ((0.0)(typeof(0.0))', that is, it tries to call the function 0.0,
-     *  which of course is nonsense.
-     */
     typeof(0.0)(
-	/* FIXME: GCC can parse this */
-	/* expect+1: error: syntax error 'typeof' [249] */
 	typeof(0.0)
     )
 );
@@ -32,7 +23,7 @@ cast(double(*fn)(double))
 	/* expect+1: warning: passing 'pointer to function(double) returning double' to incompatible 'double', arg #1 [155] */
 	take_double(fn);
 
-	/* XXX: oops; GCC detects this type mismatch. probably due to the parse error. */
+	/* expect+1: warning: passing 'double' to incompatible 'pointer to function(double) returning double', arg #1 [155] */
 	take_function_double_returning_double(0.0);
 
 	take_function_double_returning_double(fn);
