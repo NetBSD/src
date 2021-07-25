5eaee1613fc5ee9bc167a06092c66bc96ee2ccb3
rillig <rillig@NetBSD.org>
2021-07-25 17:27:07 +0000

    tests/lint: test declarators with attributes

---
+++ tests/usr.bin/xlint/lint1/parse_init_declarator.c
@@ -0,0 +1,34 @@
+/*	$NetBSD: parse_init_declarator.c,v 1.1 2021/07/25 17:27:07 rillig Exp $	*/
+# 3 "parse_init_declarator.c"
+
+/*
+ * Test parsing of init-declarator, which occurs as part of a top-level
+ * declaration.
+ *
+ * See also: GCC, c-parser.cpp, function c_parser_declaration_or_fndef.
+ */
+
+int global_var;
+
+int *init_declarator_without_initializer
+    __asm("") __attribute__((deprecated));
+/* expect-1: error: syntax error '__attribute__' [249] */
+/* FIXME */
+
+/* XXX: GCC does not accept this, neither should lint. */
+int *init_declarator_without_initializer_wrong_order
+    __attribute__((deprecated)) __asm("");
+
+int *init_declarator_with_initializer
+    __asm("") __attribute__((deprecated)) = &global_var;
+/* expect-1: error: syntax error '__attribute__' [249] */
+/* FIXME */
+
+/* XXX: GCC does not accept this, neither should lint. */
+int *init_declarator_with_initializer_wrong_order
+    __attribute__((deprecated)) __asm("") = &global_var;
+
+/* The attributes may only occur before the initializer, not after it. */
+int *init_declarator_with_initializer_attribute_too_late
+    __asm("") = &global_var __attribute__((deprecated));
+/* expect-1: error: syntax error '__attribute__' [249] */
