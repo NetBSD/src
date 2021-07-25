b538fc2d6ad90504808c041b260d9a89c02d7e50
rillig <rillig@NetBSD.org>
2021-07-25 18:34:44 +0000

    tests/lint: test __attribute__ with enum

--- tests/usr.bin/xlint/lint1/gcc_attribute_enum.c
+++ tests/usr.bin/xlint/lint1/gcc_attribute_enum.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: gcc_attribute_enum.c,v 1.1 2021/07/06 17:33:07 rillig Exp $	*/
+/*	$NetBSD: gcc_attribute_enum.c,v 1.2 2021/07/25 18:34:44 rillig Exp $	*/
 # 3 "gcc_attribute_enum.c"
 
 /*
@@ -7,16 +7,41 @@
  * https://gcc.gnu.org/onlinedocs/gcc/Enumerator-Attributes.html
  */
 
-enum Planet {
-	Mercury,
-	Venus,
-	Earth,
-	Mars,
-	Jupiter,
-	Saturn,
-	Uranus,
-	Neptune,
-	/* https://en.wikipedia.org/wiki/Pluto_(planet) */
-	/*FIXME*//* expect+1: error: syntax error '__attribute__' [249] */
-	Pluto __attribute__((__deprecated__ /* since August 2006 */))
+/*
+ * Attributes in enum-specifier.
+ *
+ * See GCC, c-parser.c, function c_parser_enum_specifier.
+ */
+
+/* expect+1: syntax error '__attribute__' [249] */
+enum __attribute__(()) tag;
+
+/* expect+2: syntax error '__attribute__' [249] */
+/* expect+1: syntax error '{' [249] */
+enum __attribute__(()) tag_with_declaration {
+	TAG_WITH_DECL
+} __attribute__(());
+/* expect-1: syntax error ';' [249] */
+
+/* expect+1: syntax error '{' [249] */
+enum __attribute__(()) {
+	ONLY_DECL
+} __attribute__(());
+/* expect-1: syntax error ';' [249] */
+/* expect-2: error: cannot recover from previous errors [224] */
+
+/*
+ * Attributes in enumerator.
+ *
+ * See GCC, c-parser.c, function c_parser_enum_specifier.
+ */
+
+enum {
+	NO_INIT_FIRST __attribute__(()),
+	NO_INIT__LAST __attribute__(())
+};
+
+enum {
+	INIT_FIRST __attribute__(()) = 1,
+	INIT__LAST __attribute__(()) = 2
 };
