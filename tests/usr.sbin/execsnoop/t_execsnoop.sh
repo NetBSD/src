07b952266ae5a8159fe3de516494f9b16cedadf0
skrll <skrll@NetBSD.org>
2021-07-27 11:05:39 +0000

    PR/53417 appears fixed now so remove atf_expect_fail (for Arm).

--- tests/usr.sbin/execsnoop/t_execsnoop.sh
+++ tests/usr.sbin/execsnoop/t_execsnoop.sh
@@ -1,4 +1,4 @@
-# $NetBSD: t_execsnoop.sh,v 1.8 2021/07/25 15:51:32 gson Exp $
+# $NetBSD: t_execsnoop.sh,v 1.9 2021/07/27 11:05:39 skrll Exp $
 #
 # Copyright (c) 2020 The NetBSD Foundation, Inc.
 # All rights reserved.
@@ -37,11 +37,6 @@ basic_head() {
 }
 
 basic_body() {
-	case `uname -p` in
-	*arm*)
-		atf_expect_fail "PR kern/53417" ;;
-	esac
-
 	n=10
 	atf_check -s exit:0 -o ignore -e empty -x "execsnoop > $tmp &"
 	sleep 5
