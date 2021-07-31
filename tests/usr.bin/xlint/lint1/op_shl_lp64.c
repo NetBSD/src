2101b0e9d09558243e96fe3edb18ef36ecbfbb10
rillig <rillig@NetBSD.org>
2021-07-31 19:52:44 +0000

    lint: improve support for __int128_t and __uint128_t
    For the .ln files, I chose the letter 'J' to represent the 128-bit
    integer types since it is close to 'I' for int.  The naming of 'L' for
    'long' is obvious, but 'Q' for 64-bit integers is a quad-16-bit word,
    which is an unusual measurement unit nowadays.  One benefit of choosing
    'J' is that the next letter, 'K' can then be used for 256-bit integer
    types.
    
    Support for 128-bit integer types is still very basic.  Plus, it is only
    supported on LP64 platforms, which means that lint cannot be
    cross-compiled to check for an LP64 platform while running on an ILP32
    platform.

--- tests/usr.bin/xlint/lint1/op_shl_lp64.c
+++ tests/usr.bin/xlint/lint1/op_shl_lp64.c
@@ -1,18 +1,25 @@
-/*	$NetBSD: op_shl_lp64.c,v 1.1 2021/07/04 20:22:31 rillig Exp $	*/
+/*	$NetBSD: op_shl_lp64.c,v 1.2 2021/07/31 19:52:44 rillig Exp $	*/
 # 3 "op_shl_lp64.c"
 
 /*
- * Test overflow on shl of 128-bit integers, as seen in
- * ecp_nistp256.c(296).
+ * Before decl.c 1.215 from 2021-07-31, lint wrongly treated __uint128_t and
+ * __int128_t as being equivalent to a missing type specifier, thereby
+ * defaulting to int.  This led to warnings like:
+ *
+ *	shift amount 105 is greater than bit-size 32 of 'int' [122]
+ *
+ * These warnings had been discovered in ecp_nistp256.c(296).
  */
 
 /* lint1-only-if lp64 */
 
 const __uint128_t zero105 =
-    /* FIXME: 105 is ok for __uint128_t */
-    /* expect+1: warning: shift amount 105 is greater than bit-size 32 of 'int' [122] */
     (((__uint128_t)1) << 105)
-    /* FIXME: 41 is ok for __uint128_t */
-    /* expect+1: warning: shift amount 41 is greater than bit-size 32 of 'int' [122] */
     - (((__uint128_t)1) << 41)
     - (((__uint128_t)1) << 9);
+
+const __uint128_t shl_128_129 =
+    /* expect+1: warning: shift equal to size of object [267] */
+    (((__uint128_t)1) << 128)
+    /* expect+1: warning: shift amount 129 is greater than bit-size 128 of '__uint128_t' [122] */
+    - (((__uint128_t)1) << 129);
