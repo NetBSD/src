1f909102e5c8ff4e2ed3352b0f0047bef4428f1e
rillig <rillig@NetBSD.org>
2021-07-31 09:14:47 +0000

    tests/lint: demonstrate wrong error message for initialization
    Seen in usr.bin/make/var.c:4022 in C99 mode, where a ModChain variable
    is initialized and two of the members are const-qualified.

--- tests/usr.bin/xlint/lint1/msg_115.c
+++ tests/usr.bin/xlint/lint1/msg_115.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: msg_115.c,v 1.4 2021/01/31 11:12:07 rillig Exp $	*/
+/*	$NetBSD: msg_115.c,v 1.5 2021/07/31 09:14:47 rillig Exp $	*/
 # 3 "msg_115.c"
 
 // Test for message: %soperand of '%s' must be modifiable lvalue [115]
@@ -14,4 +14,22 @@ example(const int *const_ptr)
 	*const_ptr /= 5;	/* expect: 115 */
 	*const_ptr %= 9;	/* expect: 115 */
 	(*const_ptr)++;		/* expect: 115 */
+}
+
+void
+initialize_const_struct_member(void)
+{
+	struct S {
+		int const member;
+	};
+
+	/* FIXME: In an initialization, const members can be assigned. */
+	/* expect+1: warning: left operand of '=' must be modifiable lvalue [115] */
+	struct S s1 = (struct S) { 12345 };
+	if (s1.member != 0)
+		return;
+
+	struct S s2 = { 12345 };
+	if (s2.member != 0)
+		return;
 }
