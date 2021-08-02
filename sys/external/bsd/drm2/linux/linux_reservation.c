92c112b5ac0804858117dd5fda69c5def65ab663
riastradh <riastradh@NetBSD.org>
2021-08-02 20:20:54 +0000

    drm: Release fence if we're about to return too.
    Should fix another fence leak.
    
    XXX pullup-9

--- sys/external/bsd/drm2/linux/linux_reservation.c
+++ sys/external/bsd/drm2/linux/linux_reservation.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: linux_reservation.c,v 1.12 2021/06/27 08:10:36 riastradh Exp $	*/
+/*	$NetBSD: linux_reservation.c,v 1.13 2021/08/02 20:20:54 riastradh Exp $	*/
 
 /*-
  * Copyright (c) 2018 The NetBSD Foundation, Inc.
@@ -30,7 +30,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: linux_reservation.c,v 1.12 2021/06/27 08:10:36 riastradh Exp $");
+__KERNEL_RCSID(0, "$NetBSD: linux_reservation.c,v 1.13 2021/08/02 20:20:54 riastradh Exp $");
 
 #include <sys/param.h>
 #include <sys/poll.h>
@@ -794,9 +794,9 @@ wait:
 	KASSERT(fence != NULL);
 	rcu_read_unlock();
 	ret = fence_wait_timeout(fence, intr, timeout);
+	fence_put(fence);
 	if (ret <= 0)
 		return ret;
-	fence_put(fence);
 	KASSERT(ret <= timeout);
 	timeout = ret;
 	goto top;
