65f8d10f54ce770806741d1dc2c45e5be5d38f22
rillig <rillig@NetBSD.org>
2021-08-03 18:44:33 +0000

    lint: add quotes around placeholder in message 214
    function '%s' expects to return value

--- tests/usr.bin/xlint/lint1/msg_214.c
+++ tests/usr.bin/xlint/lint1/msg_214.c
@@ -1,12 +1,11 @@
-/*	$NetBSD: msg_214.c,v 1.3 2021/08/03 18:38:02 rillig Exp $	*/
+/*	$NetBSD: msg_214.c,v 1.4 2021/08/03 18:44:33 rillig Exp $	*/
 # 3 "msg_214.c"
 
-// Test for message: function %s expects to return value [214]
+// Test for message: function '%s' expects to return value [214]
 
 int
 int_function(void)
 {
-	/* TODO: add quotes around '%s' */
-	/* expect+1: warning: function int_function expects to return value [214] */
+	/* expect+1: warning: function 'int_function' expects to return value [214] */
 	return;
 }
