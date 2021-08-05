2e1c5b53fe93768fec07da97645d39f2374e9062
rillig <rillig@NetBSD.org>
2021-08-05 22:36:07 +0000

    tests/lint: add test skeletons for messages from lint2

---
+++ tests/usr.bin/xlint/lint2/t_lint2.sh
@@ -0,0 +1,54 @@
+# $NetBSD: t_lint2.sh,v 1.1 2021/08/05 22:36:08 rillig Exp $
+#
+# Copyright (c) 2021 The NetBSD Foundation, Inc.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
+# ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+# TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS
+# BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#
+
+lint2=/usr/libexec/lint2
+
+do_msg_head()
+{
+	atf_set 'require.progs' "$lint2"
+}
+
+do_msg_body()
+{
+	# shellcheck disable=SC2155
+	local srcdir="$(atf_get_srcdir)"
+
+	# remove comments and empty lines from the .ln file
+	sed -e '/^#/d' -e '/^$/d' < "$srcdir/msg_$1.ln" > "msg_$1.ln"
+
+	atf_check -o "file:$srcdir/msg_$1.exp" \
+	    "$lint2" -h -p -x "msg_$1.ln"
+}
+
+atf_init_test_cases()
+{
+	for i in $(printf '%03d\n' $(seq 0 18)); do
+		eval "msg_${i}_head() { do_msg_head; }"
+		eval "msg_${i}_body() { do_msg_body '$i'; }"
+		atf_add_test_case "msg_$i"
+	done
+}
