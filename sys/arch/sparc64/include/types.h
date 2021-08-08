963aeb7329eecb948394cb96236c6378345d7280
thorpej <thorpej@NetBSD.org>
2021-08-08 00:53:39 +0000

    Define preprocessor symbols (__HAVE_OPENFIRMWARE_VARIANT_AAPL and __HAVE_OPENFIRMWARE_VARIANT_SUNW) that lets generic OpenFirmware code deal with differences / quirks among implementation variants.

--- sys/arch/sparc64/include/types.h
+++ sys/arch/sparc64/include/types.h
@@ -1,4 +1,4 @@
-/*       $NetBSD: types.h,v 1.28 2019/04/06 03:06:27 thorpej Exp $        */
+/*       $NetBSD: types.h,v 1.29 2021/08/08 00:53:39 thorpej Exp $        */
 
 #ifndef _SPARC64_TYPES_H_
 #define	_SPARC64_TYPES_H_
@@ -11,5 +11,6 @@
 
 #define	__HAVE_COMPAT_NETBSD32
 #define	__HAVE_UCAS_FULL
+#define	__HAVE_OPENFIRMWARE_VARIANT_SUNW
 
 #endif
