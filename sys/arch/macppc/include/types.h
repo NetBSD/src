3e3183ff5897383f589e5cdc6e8786d4ed33899e
thorpej <thorpej@NetBSD.org>
2021-08-08 00:53:39 +0000

    Define preprocessor symbols (__HAVE_OPENFIRMWARE_VARIANT_AAPL and __HAVE_OPENFIRMWARE_VARIANT_SUNW) that lets generic OpenFirmware code deal with differences / quirks among implementation variants.

--- sys/arch/macppc/include/types.h
+++ sys/arch/macppc/include/types.h
@@ -1,5 +1,6 @@
-/*	$NetBSD: types.h,v 1.14 2011/07/17 23:23:53 dyoung Exp $	*/
+/*	$NetBSD: types.h,v 1.15 2021/08/08 00:53:39 thorpej Exp $	*/
 
 #include <powerpc/types.h>
 
 #define	__HAVE_NEW_STYLE_BUS_H
+#define	__HAVE_OPENFIRMWARE_VARIANT_AAPL
