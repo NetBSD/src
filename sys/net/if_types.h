2c808783d8cbc353b3f6b489cc7168731a865118
andvar <andvar@NetBSD.org>
2021-08-09 19:57:57 +0000

    fix typos in asymmetry, asymmetric(al), symmetrical.

--- sys/net/if_types.h
+++ sys/net/if_types.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: if_types.h,v 1.31 2020/08/26 16:03:41 riastradh Exp $	*/
+/*	$NetBSD: if_types.h,v 1.32 2021/08/09 19:57:58 andvar Exp $	*/
 
 /*
  * Copyright (c) 1989, 1993, 1994
@@ -248,8 +248,8 @@
 #define IFT_SIPSIG		   0xcc /* SIP Signaling */
 #define IFT_DOCSCABLEUPSTREAMCHANNEL 0xcd /* CATV Upstream Channel */
 #define IFT_ECONET		   0xce /* Acorn Econet */
-#define IFT_PON155		   0xcf /* FSAN 155Mb Symetrical PON interface */
-#define IFT_PON622		   0xd0 /* FSAN 622Mb Symetrical PON interface */
+#define IFT_PON155		   0xcf /* FSAN 155Mb Symmetrical PON interface */
+#define IFT_PON622		   0xd0 /* FSAN 622Mb Symmetrical PON interface */
 #define IFT_BRIDGE		   0xd1 /* Transparent bridge interface */
 #define IFT_LINEGROUP		   0xd2 /* Interface common to multiple lines */
 #define IFT_VOICEEMFGD		   0xd3 /* voice E&M Feature Group D */
