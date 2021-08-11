0c1a6d77776de349371bef5dde256953d48b5af8
christos <christos@NetBSD.org>
2021-08-11 11:16:49 +0000

    PR/56355: Brad Harder: dtrace triggers double-fault in supervisor mode

--- external/cddl/osnet/dev/fbt/x86/fbt_isa.c
+++ external/cddl/osnet/dev/fbt/x86/fbt_isa.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: fbt_isa.c,v 1.3 2020/05/02 11:37:17 maxv Exp $	*/
+/*	$NetBSD: fbt_isa.c,v 1.4 2021/08/11 11:16:49 christos Exp $	*/
 
 /*
  * CDDL HEADER START
@@ -251,8 +251,9 @@ fbt_provide_module_cb(const char *name, int symindx, void *value,
 	/*
 	 * Exclude some more symbols which can be called from probe context.
 	 */
-	if (strcmp(name, "x86_curcpu") == 0 ||
-	    strcmp(name, "x86_curlwp") == 0) {
+	if (strncmp(name, "trap", 4) ||
+	    strncmp(name, "x86_curcpu", 10) == 0 ||
+	    strncmp(name, "x86_curlwp", 10) == 0) {
 		return 0;
 	}
 #endif
