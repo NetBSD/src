c8a8302d889cea15f5b89de4769891cb04b73669
rillig <rillig@NetBSD.org>
2021-08-14 12:46:23 +0000

    lint: allow initialization of struct with constant member
    The operator INIT, just like RETURN and FARG, initializes an object with
    an expression.  The target object of such an initialization may be a
    struct with constant members.
    
    The operator ASSIGN, on the other hand, is entirely different.  It
    overwrites the existing value of the object, and this is not allowed for
    structs that have a constant member.  Therefore it was wrong to use the
    operator ASSIGN for initialization.

--- tests/usr.bin/xlint/lint1/msg_164.c
+++ tests/usr.bin/xlint/lint1/msg_164.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: msg_164.c,v 1.3 2021/01/31 11:12:07 rillig Exp $	*/
+/*	$NetBSD: msg_164.c,v 1.4 2021/08/14 12:46:24 rillig Exp $	*/
 # 3 "msg_164.c"
 
 // Test for message: assignment of negative constant to unsigned type [164]
@@ -6,7 +6,8 @@
 void
 example(void)
 {
-	unsigned char uch = -3;		/* expect: 164 */
+	/* expect+1: warning: initialization of unsigned with negative constant [221] */
+	unsigned char uch = -3;
 
 	uch = -5;			/* expect: 164 */
 	uch += -7;			/* expect: 222 */
