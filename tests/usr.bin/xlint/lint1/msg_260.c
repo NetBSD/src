39bf493754ba24b7b2e9d0d71046a4b7f0a50d15
rillig <rillig@NetBSD.org>
2021-08-16 06:49:56 +0000

    lint: add more details to message about redeclared tag

--- tests/usr.bin/xlint/lint1/msg_260.c
+++ tests/usr.bin/xlint/lint1/msg_260.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: msg_260.c,v 1.3 2021/04/18 07:31:47 rillig Exp $	*/
+/*	$NetBSD: msg_260.c,v 1.4 2021/08/16 06:49:57 rillig Exp $	*/
 # 3 "msg_260.c"
 
 // Test for message: previous declaration of %s [260]
@@ -6,22 +6,26 @@
 /* lint1-extra-flags: -r */
 
 # 100 "header.h" 1
-struct s {		/* expect: 260 */
+/* expect+1: previous declaration of s [260] */
+struct s {
     int member;
 };
-# 13 "msg_260.c" 2
+# 14 "msg_260.c" 2
 
 # 200 "header.h" 1
-union s {		/* expect: tag redeclared *//* expect: 260 */
+/* expect+2: error: struct tag 's' redeclared as union [46] */
+/* expect+1: previous declaration of s [260] */
+union s {
     int member;
 };
 /*
- * FIXME: the stack trace for the 260 is wrong, as the 260 is included from
- * line 8, not from line 14.
+ * FIXME: the stack trace for the message 260 is wrong, as the previous
+ * declaration is included from logical line msg_260.c:8, not from
+ * msg_260.c:15.
  */
-# 19 "msg_160.c" 2
-
-union s {		/* expect: tag redeclared */
+# 27 "msg_260.c" 2
+/* expect+1: error: union tag 's' redeclared as union [46] */
+union s {
     int member;
 };
 /*
