1678a18aca90a0837a1dba1579c541bdf4fbece1
rillig <rillig@NetBSD.org>
2021-08-16 18:51:58 +0000

    tests/lint: add tests for several messages about type mismatch

--- tests/usr.bin/xlint/lint1/msg_211.c
+++ tests/usr.bin/xlint/lint1/msg_211.c
@@ -1,7 +1,22 @@
-/*	$NetBSD: msg_211.c,v 1.2 2021/02/21 09:07:58 rillig Exp $	*/
+/*	$NetBSD: msg_211.c,v 1.3 2021/08/16 18:51:58 rillig Exp $	*/
 # 3 "msg_211.c"
 
 // Test for message: return value type mismatch (%s) and (%s) [211]
 
-TODO: "Add example code that triggers the above message." /* expect: 249 */
-TODO: "Add example code that almost triggers the above message."
+struct str {
+	int member;
+};
+
+int
+return_int(double dbl, void *ptr, struct str str)
+{
+	if (dbl > 0.0)
+		return dbl;
+	if (ptr != (void *)0)
+		/* expect+1: warning: illegal combination of integer (int) and pointer (pointer to void) [183] */
+		return ptr;
+	if (str.member > 0)
+		/* expect+1: error: return value type mismatch (int) and (struct str) [211 */
+		return str;
+	return 3;
+}
