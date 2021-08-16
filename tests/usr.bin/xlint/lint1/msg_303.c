50c44181a348f767bd9503bb6a46816443bfc5be
rillig <rillig@NetBSD.org>
2021-08-16 18:51:58 +0000

    tests/lint: add tests for several messages about type mismatch

--- tests/usr.bin/xlint/lint1/msg_303.c
+++ tests/usr.bin/xlint/lint1/msg_303.c
@@ -1,7 +1,21 @@
-/*	$NetBSD: msg_303.c,v 1.2 2021/02/21 09:07:58 rillig Exp $	*/
+/*	$NetBSD: msg_303.c,v 1.3 2021/08/16 18:51:58 rillig Exp $	*/
 # 3 "msg_303.c"
 
-// Test for message: ANSI C forbids conversion of %s to %s [303]
+/* Test for message: ANSI C forbids conversion of %s to %s [303] */
 
-TODO: "Add example code that triggers the above message." /* expect: 249 */
-TODO: "Add example code that almost triggers the above message."
+/* lint1-flags: -sw */
+
+void take_void_pointer(void *);
+
+void *
+to_void_pointer(void)
+{
+	/* expect+1: warning: ANSI C forbids conversion of function pointer to 'void *' [303] */
+	return to_void_pointer;
+}
+
+void (*to_function_pointer(void *arg))(void)
+{
+	/* expect+1: warning: ANSI C forbids conversion of 'void *' to function pointer [303] */
+	return arg;
+}
