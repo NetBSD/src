ebbe94097593925d4c3dd8e3d4f4c89f11d2a585
rillig <rillig@NetBSD.org>
2021-08-22 13:45:56 +0000

    tests/lint: test messages 268 to 274

--- tests/usr.bin/xlint/lint1/msg_273.c
+++ tests/usr.bin/xlint/lint1/msg_273.c
@@ -1,7 +1,14 @@
-/*	$NetBSD: msg_273.c,v 1.2 2021/02/21 09:07:58 rillig Exp $	*/
+/*	$NetBSD: msg_273.c,v 1.3 2021/08/22 13:45:56 rillig Exp $	*/
 # 3 "msg_273.c"
 
-// Test for message: bit-field type '%s' invalid in ANSI C [273]
+/* Test for message: bit-field type '%s' invalid in ANSI C [273] */
 
-TODO: "Add example code that triggers the above message." /* expect: 249 */
-TODO: "Add example code that almost triggers the above message."
+/* lint1-flags: -sw */
+
+struct bit_fields {
+	int plain_int: 3;
+	unsigned int unsigned_int: 3;
+	signed int signed_int: 3;
+	/* expect+1: warning: bit-field type 'unsigned char' invalid in ANSI C [273] */
+	unsigned char unsigned_char: 3;
+};
