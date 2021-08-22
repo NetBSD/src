0eedaefa0b91e39f24d97248427f3cc57a457519
rillig <rillig@NetBSD.org>
2021-08-22 13:52:19 +0000

    lint: use standard quotes in messages 203 and 271

--- tests/usr.bin/xlint/lint1/msg_203.c
+++ tests/usr.bin/xlint/lint1/msg_203.c
@@ -1,7 +1,18 @@
-/*	$NetBSD: msg_203.c,v 1.2 2021/02/21 09:07:58 rillig Exp $	*/
+/*	$NetBSD: msg_203.c,v 1.3 2021/08/22 13:52:19 rillig Exp $	*/
 # 3 "msg_203.c"
 
-// Test for message: case label must be of type `int' in traditional C [203]
+/* Test for message: case label must be of type 'int' in traditional C [203] */
 
-TODO: "Add example code that triggers the above message." /* expect: 249 */
-TODO: "Add example code that almost triggers the above message."
+/* lint1-flags: -tw */
+
+example(x)
+	int x;
+{
+	switch (x) {
+	case (char)3:
+		break;
+	/* expect+1: warning: case label must be of type 'int' in traditional C [203] */
+	case 4L:
+		break;
+	}
+}
