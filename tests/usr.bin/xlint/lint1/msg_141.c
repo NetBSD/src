9962502cebc3df0c44d1cc1cfdf4f28fd79a648a
rillig <rillig@NetBSD.org>
2021-08-23 05:52:04 +0000

    tests/lint: demonstrate wrong warnings for integer overflow

--- tests/usr.bin/xlint/lint1/msg_141.c
+++ tests/usr.bin/xlint/lint1/msg_141.c
@@ -1,7 +1,23 @@
-/*	$NetBSD: msg_141.c,v 1.2 2021/02/21 09:07:58 rillig Exp $	*/
+/*	$NetBSD: msg_141.c,v 1.3 2021/08/23 05:52:04 rillig Exp $	*/
 # 3 "msg_141.c"
 
 // Test for message: integer overflow detected, op %s [141]
 
-TODO: "Add example code that triggers the above message." /* expect: 249 */
-TODO: "Add example code that almost triggers the above message."
+/* lint1-extra-flags: -h */
+
+/* FIXME */
+/* expect+1: warning: integer overflow detected, op - [141] */
+int signed_int_max = (1u << 31) - 1;
+
+/* FIXME */
+/* expect+1: warning: integer overflow detected, op - [141] */
+unsigned int unsigned_int_max = (1u << 31) - 1;
+
+/* expect+1: warning: integer overflow detected, op + [141] */
+int int_overflow = (1 << 30) + (1 << 30);
+
+/* expect+2: warning: integer overflow detected, op + [141] */
+/* expect+1: warning: initialization of unsigned with negative constant [221] */
+unsigned int intermediate_overflow = (1 << 30) + (1 << 30);
+
+unsigned int no_overflow = (1U << 30) + (1 << 30);
