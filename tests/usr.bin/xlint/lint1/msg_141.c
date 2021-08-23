d0949d3728cc118c2383a25f6d688099286673ad
rillig <rillig@NetBSD.org>
2021-08-23 06:50:01 +0000

    lint: add quotes around placeholder in message 141

--- tests/usr.bin/xlint/lint1/msg_141.c
+++ tests/usr.bin/xlint/lint1/msg_141.c
@@ -1,7 +1,7 @@
-/*	$NetBSD: msg_141.c,v 1.4 2021/08/23 06:10:27 rillig Exp $	*/
+/*	$NetBSD: msg_141.c,v 1.5 2021/08/23 06:50:01 rillig Exp $	*/
 # 3 "msg_141.c"
 
-// Test for message: integer overflow detected, op %s [141]
+// Test for message: integer overflow detected, op '%s' [141]
 
 /* lint1-extra-flags: -h */
 
@@ -17,17 +17,17 @@ int signed_int_max = (1u << 31) - 1;
  */
 unsigned int unsigned_int_max = (1u << 31) - 1;
 
-/* expect+1: warning: integer overflow detected, op + [141] */
+/* expect+1: warning: integer overflow detected, op '+' [141] */
 int int_overflow = (1 << 30) + (1 << 30);
 
-/* expect+2: warning: integer overflow detected, op + [141] */
+/* expect+2: warning: integer overflow detected, op '+' [141] */
 /* expect+1: warning: initialization of unsigned with negative constant [221] */
 unsigned int intermediate_overflow = (1 << 30) + (1 << 30);
 
 unsigned int no_overflow = (1U << 30) + (1 << 30);
 
-/* expect+1: warning: integer overflow detected, op - [141] */
+/* expect+1: warning: integer overflow detected, op '-' [141] */
 unsigned int unsigned_int_min = 0u - (1u << 31);
 
-/* expect+1: warning: integer overflow detected, op - [141] */
+/* expect+1: warning: integer overflow detected, op '-' [141] */
 unsigned int unsigned_int_min_unary = -(1u << 31);
