ec3b59f59db097093d81243987543561615647fd
rillig <rillig@NetBSD.org>
2021-08-27 19:50:44 +0000

    lint: remove unused message for struct without named members
    Lint has not produced this message since the rewrite of the
    initializations in March 2021.  Structures that consist of only unnamed
    bit-fields probably don't occur in practice.

--- tests/usr.bin/xlint/lint1/msg_179.c
+++ tests/usr.bin/xlint/lint1/msg_179.c
@@ -1,7 +1,24 @@
-/*	$NetBSD: msg_179.c,v 1.2 2021/02/21 09:07:58 rillig Exp $	*/
+/*	$NetBSD: msg_179.c,v 1.3 2021/08/27 19:50:44 rillig Exp $	*/
 # 3 "msg_179.c"
 
 // Test for message: cannot initialize struct/union with no named member [179]
+/* This message is not used. */
 
-TODO: "Add example code that triggers the above message." /* expect: 249 */
-TODO: "Add example code that almost triggers the above message."
+struct {
+	unsigned int :0;
+} no_named_member = {
+	/* no named member, therefore no initializer expression */
+};
+
+struct {
+	/* no members */
+} empty = {
+	/* no initializer expressions */
+};
+
+struct {
+	unsigned int: 0;
+} no_named_member_init = {
+	/* expect+1: error: too many struct/union initializers [172] */
+	3,
+};
