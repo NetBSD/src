cfc11c762c687aee26689f95a02f49145e8670ba
rillig <rillig@NetBSD.org>
2021-08-27 20:19:45 +0000

    lint: remove unused message about nonportable character escape
    It has not been used since at least 2001.

--- tests/usr.bin/xlint/lint1/msg_078.c
+++ tests/usr.bin/xlint/lint1/msg_078.c
@@ -1,7 +1,8 @@
-/*	$NetBSD: msg_078.c,v 1.3 2021/08/27 20:16:50 rillig Exp $	*/
+/*	$NetBSD: msg_078.c,v 1.4 2021/08/27 20:19:45 rillig Exp $	*/
 # 3 "msg_078.c"
 
 // Test for message: nonportable character escape [78]
+/* This message is not used. */
 
 char either_255_or_minus_1 = '\377';
 /* expect+1: warning: dubious escape \y [79] */
--- usr.bin/xlint/lint1/err.c
+++ usr.bin/xlint/lint1/err.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: err.c,v 1.140 2021/08/27 19:50:44 rillig Exp $	*/
+/*	$NetBSD: err.c,v 1.141 2021/08/27 20:19:45 rillig Exp $	*/
 
 /*
  * Copyright (c) 1994, 1995 Jochen Pohl
@@ -37,7 +37,7 @@
 
 #include <sys/cdefs.h>
 #if defined(__RCSID) && !defined(lint)
-__RCSID("$NetBSD: err.c,v 1.140 2021/08/27 19:50:44 rillig Exp $");
+__RCSID("$NetBSD: err.c,v 1.141 2021/08/27 20:19:45 rillig Exp $");
 #endif
 
 #include <sys/types.h>
@@ -132,7 +132,7 @@ const char *const msgs[] = {
 	"overflow in hex escape",				      /* 75 */
 	"character escape does not fit in character",		      /* 76 */
 	"bad octal digit %c",					      /* 77 */
-	"nonportable character escape",				      /* 78 */
+	"",			/* unused */			      /* 78 */
 	"dubious escape \\%c",					      /* 79 */
 	"dubious escape \\%o",					      /* 80 */
 	"\\a undefined in traditional C",			      /* 81 */
