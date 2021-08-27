df59880a05de628744ac10ca78427e2bde940d9e
rillig <rillig@NetBSD.org>
2021-08-27 20:49:25 +0000

    tests/lint: test some more messages

--- tests/usr.bin/xlint/lint1/msg_226.c
+++ tests/usr.bin/xlint/lint1/msg_226.c
@@ -1,7 +1,7 @@
-/*	$NetBSD: msg_226.c,v 1.2 2021/02/21 09:07:58 rillig Exp $	*/
+/*	$NetBSD: msg_226.c,v 1.3 2021/08/27 20:49:25 rillig Exp $	*/
 # 3 "msg_226.c"
 
 // Test for message: static variable %s unused [226]
 
-TODO: "Add example code that triggers the above message." /* expect: 249 */
-TODO: "Add example code that almost triggers the above message."
+/* expect+1: warning: static variable unused unused [226] */
+static int unused;
--- tests/usr.bin/xlint/lint1/msg_289.c
+++ tests/usr.bin/xlint/lint1/msg_289.c
@@ -1,7 +1,11 @@
-/*	$NetBSD: msg_289.c,v 1.2 2021/02/21 09:07:58 rillig Exp $	*/
+/*	$NetBSD: msg_289.c,v 1.3 2021/08/27 20:49:25 rillig Exp $	*/
 # 3 "msg_289.c"
 
 // Test for message: can't be used together: /* PRINTFLIKE */ /* SCANFLIKE */ [289]
 
-TODO: "Add example code that triggers the above message." /* expect: 249 */
-TODO: "Add example code that almost triggers the above message."
+/* PRINTFLIKE */ /* SCANFLIKE */
+void
+both(void)
+/* expect+1: warning: can't be used together */
+{
+}
