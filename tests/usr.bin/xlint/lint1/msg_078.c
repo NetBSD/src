871053f6e2d9dc2aed3c28f666f84c502809d808
rillig <rillig@NetBSD.org>
2021-08-27 20:16:50 +0000

    tests/lint: add tests for a few more messages

--- tests/usr.bin/xlint/lint1/msg_078.c
+++ tests/usr.bin/xlint/lint1/msg_078.c
@@ -1,7 +1,8 @@
-/*	$NetBSD: msg_078.c,v 1.2 2021/02/21 09:07:58 rillig Exp $	*/
+/*	$NetBSD: msg_078.c,v 1.3 2021/08/27 20:16:50 rillig Exp $	*/
 # 3 "msg_078.c"
 
 // Test for message: nonportable character escape [78]
 
-TODO: "Add example code that triggers the above message." /* expect: 249 */
-TODO: "Add example code that almost triggers the above message."
+char either_255_or_minus_1 = '\377';
+/* expect+1: warning: dubious escape \y [79] */
+char dubious_escape = '\y';
