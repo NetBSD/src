e593d3126cc5fae8bfc2a6dda3e5fe8f24c9fccc
rillig <rillig@NetBSD.org>
2021-08-27 20:49:25 +0000

    tests/lint: test some more messages

--- tests/usr.bin/xlint/lint1/msg_257.c
+++ tests/usr.bin/xlint/lint1/msg_257.c
@@ -1,7 +1,28 @@
-/*	$NetBSD: msg_257.c,v 1.2 2021/02/21 09:07:58 rillig Exp $	*/
+/*	$NetBSD: msg_257.c,v 1.3 2021/08/27 20:49:25 rillig Exp $	*/
 # 3 "msg_257.c"
 
 // Test for message: extra characters in lint comment [257]
 
-TODO: "Add example code that triggers the above message." /* expect: 249 */
-TODO: "Add example code that almost triggers the above message."
+void take(const void *);
+
+/* expect+1: warning: extra characters in lint comment [257] */
+/* FALLTHROUGH 3 */
+
+/* expect+1: warning: extra characters in lint comment [257] */
+/* FALLTHROUGH extra */
+
+/* PRINTFLIKE 7 */
+void
+my_printf(const char *fmt)
+/* expect+1: warning: argument number mismatch with directive: */
+{
+	take(fmt);
+}
+
+/* expect+1: warning: extra characters in lint comment [257] */
+/* SCANFLIKE extra */
+void
+my_scanf(const char *fmt)
+{
+	take(fmt);
+}
