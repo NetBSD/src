0f9bb1ebb7bed2cbb51d4b168509e83ab597b1a4
christos <christos@NetBSD.org>
2021-08-29 09:54:18 +0000

    Inetd enhancements by James Browning, Gabe Coffland, Alex Gavin, Solomon Ritzow Described in:     https://www.mail-archive.com/tech-userlevel@netbsd.org/msg03114.html And developed in:     https://github.com/ritzow/src/pull/1
    From their notes:
    
    All new functionality should be explained by the updated manpage.
    
    The manpage has been refactored a bit: A new section "Directives"
    has been added and the information about default hostnames and
    IPsec directives has been moved there, and the new file include
    directive information is also there.
    
    getconfigent has the most major changes. A newline is no longer
    read immediately, but is called only by a "goto more" (inside an
    if(false) block). This allows multiple definitions or directives
    to exist on a single line for anything that doesn't terminate using
    a newline. This means a key-values service definition can be followed
    by another key-values service definition, a positional definition,
    or an ipsec, hostname, or .include directive on the same line.
    
    memset is no longer used explicitly to clear the servtab structure,
    a function init_servtab() is used instead, which uses a C struct
    initializer.
    
    The servtab se_group field is its own allocation now, and not just
    a pointer into the user:group string.
    
    Refactored some stuff out of getconfigent to separate functions
    for use by parse_v2.c. These functions in inetd.c are named with
    the form parse_*()
    
    parse_v2.c only has code for parsing a key-values service definition
    into a provided servtab. It should not have anything that affects
    global state other than line and line_number.
    
    Some function prototypes, structures, and #defines have been moved
    from inetd.c to inetd.h.
    
    The function config_root replaces config as the function called on
    a config file load/reload. The code removed from the end of
    config(void) is now called in config_root, so it is not run on each
    recursive config call.
    
    setconfig(void) was removed and its code added into config_root
    because that is the only place it is called, and redundant checks
    for non-null globals were removed because they are always freed by
    endconfig. The fseek code was also removed because the config files
    are always closed by endconfig.
    
    Rate limiting code was updated to add a per-service per-IP rate
    limiting form. Some of that code was refactored out of other places
    into functions with names in the form rl_*()
    
    We have not added any of the license or version information to the
    new files parse_v2.c, parse_v2.h, and inetd.h and we have not
    updated the license or version info for inetd.c.
    
    Security related:
    
    The behavior when reading invalid IPsec strings has changed. Inetd
    no longer exits, it quits reading the current config file instead.
    Could this impact program security?
    
    We have not checked for memory leaks. Solomon tried to use dmalloc
    without success. getconfigent seemed to have a memory leak at each
    "goto more". It seems like inetd has never free'd allocated strings
    when throwing away erroneous service definitions during parsing
    (i.e. when "goto more" is called when parsing fields). OpenBSD's
    version calls freeconfig on "goto more"
    (https://github.com/openbsd/src/blob/c5eae130d6c937080c3d30d124e8c8b86db7d625/usr.sbin/inetd/inetd.c#L1049)
    but NetBSD only calls it when service definitions are no longer
    needed. This has been fixed. freeconfig is called immediately before
    any "goto more". There shouldn't be any time when a servtab is in
    an invalid state where freeconfig would break.

---
+++ tests/usr.sbin/inetd/test_server.c
@@ -0,0 +1,160 @@
+/*	$NetBSD: test_server.c,v 1.1 2021/08/29 09:54:18 christos Exp $	*/
+
+/*-
+ * Copyright (c) 2021 The NetBSD Foundation, Inc.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to The NetBSD Foundation
+ * by James Browning, Gabe Coffland, Alex Gavin, and Solomon Ritzow.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <sys/cdefs.h>
+__RCSID("$NetBSD: test_server.c,v 1.1 2021/08/29 09:54:18 christos Exp $");
+
+#include <sys/socket.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <syslog.h>
+
+#define CHECK(expr) do {\
+	if ((expr) == -1) {\
+		syslog(LOG_ERR, "Error at %s:%d: %s", \
+		    __FILE__, __LINE__, \
+		    strerror(errno));\
+		exit(EXIT_FAILURE);\
+	}\
+} while(0);
+
+static void stream_nowait_service(void);
+static void stream_wait_service(void);
+static void dgram_wait_service(void);
+
+int
+main(int argc, char **argv)
+{
+
+	openlog("inetd_test_server", LOG_PID | LOG_NOWAIT, LOG_DAEMON);
+
+	if (argc < 3) {
+		syslog(LOG_ERR, "Invalid arg count");
+		exit(EXIT_FAILURE);
+	}
+
+	/* Run the correct service according to the args */
+	if (strcmp(argv[1], "dgram") == 0) {
+		if (strcmp(argv[2], "wait") == 0) {
+			dgram_wait_service();
+		} else {
+			syslog(LOG_ERR, "Invalid arg %s", argv[2]);
+			exit(EXIT_FAILURE);
+		}
+	} else if (strcmp(argv[1], "stream") == 0) {
+		if (strcmp(argv[2], "wait") == 0) {
+			stream_wait_service();
+		} else if (strcmp(argv[2], "nowait") == 0) { 
+			stream_nowait_service();
+		} else {
+			syslog(LOG_ERR, "Invalid arg %s", argv[2]);
+			exit(EXIT_FAILURE);
+		}
+	} else {
+		syslog(LOG_ERR, "Invalid args %s %s", argv[1], argv[2]);
+		exit(EXIT_FAILURE);
+	}
+	return 0;
+}
+
+static void
+stream_nowait_service()
+{
+	ssize_t count;
+	char buffer[10];
+	CHECK(count = recv(0, buffer, sizeof(buffer), 0));
+	syslog(LOG_WARNING, "Received stream/nowait message \"%.*s\"\n",
+	    count, buffer);
+	CHECK(send(1, buffer, count, 0));
+}
+
+static void
+stream_wait_service()
+{
+	struct sockaddr_storage addr;
+	ssize_t count;
+	int fd;
+	socklen_t addr_len;
+	char buffer[10];
+
+	CHECK(fd = accept(0, (struct sockaddr*)&addr, &addr_len));
+	CHECK(count = recv(fd, buffer, sizeof(buffer), 0));
+	syslog(LOG_WARNING, "Received stream/wait message \"%.*s\"\n",
+	    count, buffer);
+	CHECK(send(fd, buffer, count, 0));
+	CHECK(shutdown(fd, SHUT_RDWR));
+	CHECK(close(fd));
+}
+
+static void
+dgram_wait_service()
+{
+	char buffer[256];
+	char name[NI_MAXHOST];
+	socklen_t source_size;
+	struct sockaddr_storage addr;
+
+	struct iovec store = {
+		.iov_base = &buffer,
+		.iov_len = sizeof(buffer)
+	};
+	struct msghdr header = {
+		.msg_name = &addr,
+		.msg_namelen = sizeof(struct sockaddr_storage),
+		.msg_iov = &store,
+		.msg_iovlen = 1
+		/* scatter/gather and control info is null */
+	};
+	int count;
+
+	/* Peek so service can still get the packet */
+	CHECK(count = recvmsg(0, &header, 0));
+
+	CHECK(sendto(1, buffer, count, 0, 
+	    (struct sockaddr*)(&addr), 
+	    addr.ss_len));
+	
+	int error = getnameinfo((struct sockaddr*)&addr, 
+	    addr.ss_len, name, NI_MAXHOST,
+	    NULL, 0, NI_NUMERICHOST);
+	
+	if (error) {
+		syslog(LOG_ERR, "getnameinfo error: %s\n", gai_strerror(error));
+		exit(EXIT_FAILURE);
+	}
+	syslog(LOG_WARNING, "Received dgram/wait message \"%.*s\" from %s\n", 
+	    count, buffer, name);
+}
---
+++ usr.sbin/inetd/inetd.h
@@ -0,0 +1,223 @@
+/*	$NetBSD: inetd.h,v 1.1 2021/08/29 09:54:18 christos Exp $	*/
+
+/*-
+ * Copyright (c) 1998, 2003 The NetBSD Foundation, Inc.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to The NetBSD Foundation
+ * by Jason R. Thorpe of the Numerical Aerospace Simulation Facility,
+ * NASA Ames Research Center and by Matthias Scheler.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/*
+ * Copyright (c) 1983, 1991, 1993, 1994
+ *	The Regents of the University of California.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the University nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#ifndef _INETD_H
+#define _INETD_H
+
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <sys/un.h>
+
+#include <arpa/inet.h>
+
+#include <netdb.h>
+
+#include "pathnames.h"
+
+#ifdef IPSEC
+#include <netipsec/ipsec.h>
+#ifndef IPSEC_POLICY_IPSEC	/* no ipsec support on old ipsec */
+#undef IPSEC
+#endif
+#include "ipsec.h"
+#endif
+
+typedef enum service_type {
+	NORM_TYPE = 0, 
+	MUX_TYPE = 1, 
+	MUXPLUS_TYPE = 2, 
+	FAITH_TYPE = 3
+} service_type;
+
+#define ISMUXPLUS(sep)	((sep)->se_type == MUXPLUS_TYPE)
+#define ISMUX(sep)	(((sep)->se_type == MUX_TYPE) || ISMUXPLUS(sep))
+
+#define	TOOMANY		40		/* don't start more than TOOMANY */
+
+#define CONF_ERROR_FMT "%s line %zu: "
+
+/* Log warning/error with 0 or variadic args with line number and file name */
+
+#define ILV(prio, msg, ...) syslog(prio, CONF_ERROR_FMT msg ".", \
+    CONFIG, line_number __VA_OPT__(,) __VA_ARGS__)
+
+#define WRN(msg, ...) ILV(LOG_WARNING, msg __VA_OPT__(,) __VA_ARGS__)
+#define ERR(msg, ...) ILV(LOG_ERR, msg __VA_OPT__(,) __VA_ARGS__)
+
+/* Debug logging */
+#ifdef DEBUG_ENABLE
+#define DPRINTF(fmt, ...) do {\
+	if (debug) {\
+		fprintf(stderr, fmt "\n" __VA_OPT__(,) __VA_ARGS__);\
+	}\
+} while (0)
+#else
+#define DPRINTF(fmt, ...) __nothing
+#endif
+
+#define DPRINTCONF(fmt, ...) DPRINTF(CONF_ERROR_FMT fmt,\
+	CONFIG, line_number __VA_OPT__(,) __VA_ARGS__)
+
+#define STRINGIFY(x) #x
+#define TOSTRING(x) STRINGIFY(x)
+
+struct	servtab {
+	char	*se_hostaddr;		/* host address to listen on */
+	char	*se_service;		/* name of service */
+	int	se_socktype;		/* type of socket to use */
+	sa_family_t	se_family;	/* address family */
+	char	*se_proto;		/* protocol used */
+	int	se_sndbuf;		/* sndbuf size */
+	int	se_rcvbuf;		/* rcvbuf size */
+	int	se_rpcprog;		/* rpc program number */
+	int	se_rpcversl;		/* rpc program lowest version */
+	int	se_rpcversh;		/* rpc program highest version */
+#define isrpcservice(sep)	((sep)->se_rpcversl != 0)
+	pid_t	se_wait;		/* single threaded server */
+	short	se_checked;		/* looked at during merge */
+	char	*se_user;		/* user name to run as */
+	char	*se_group;		/* group name to run as */
+	struct	biltin *se_bi;		/* if built-in, description */
+	char	*se_server;		/* server program */
+#define	MAXARGV 64
+	char	*se_argv[MAXARGV+1];	/* program arguments */
+#ifdef IPSEC
+	char	*se_policy;		/* IPsec poilcy string */
+#endif
+	struct accept_filter_arg se_accf; /* accept filter for stream service */
+	int	se_fd;			/* open descriptor */
+	service_type	se_type;	/* type */
+	union {
+		struct sockaddr_storage	se_ctrladdr_storage; /* ensure correctness of C struct initializer */
+		struct sockaddr	se_ctrladdr;
+		struct sockaddr_in	se_ctrladdr_in;
+		struct sockaddr_in6	se_ctrladdr_in6; /* in6 is used by bind()/getaddrinfo */
+		struct sockaddr_un	se_ctrladdr_un;
+	};				/* bound address */
+	socklen_t	se_ctrladdr_size;
+	size_t	se_service_max;		/* max # of instances of this service */
+	size_t	se_count;		/* number of instances of this service started since se_time */
+	size_t	se_ip_max;  		/* max # of instances of this service per ip per minute */
+	struct se_ip_list_node {
+		struct se_ip_list_node	*next;
+		size_t count;		/* 
+					 * number of instances of this service started from
+					 * this ip address since se_time (includes 
+					 * attempted starts if greater than se_ip_max)
+					 */
+		char address[NI_MAXHOST];
+	} *se_ip_list_head; 		/* linked list of number of requests per ip */
+	time_t se_time;	/* start of se_count and ip_max counts, in seconds from arbitrary point */
+	struct	servtab *se_next;
+};
+
+/* From inetd.c */
+int 	parse_protocol(struct servtab *);
+int 	parse_wait(struct servtab *, int);
+int 	parse_server(struct servtab *, const char *);
+void 	parse_socktype(char *, struct servtab *);
+void 	parse_accept_filter(char *, struct servtab *);
+char 	*nextline(FILE *);
+char 	*newstr(const char *);
+void	freeconfig(struct servtab *);
+
+/* Global debug mode boolean, enabled with -d */
+extern int debug;
+
+/* Current config file path */
+extern const char *CONFIG;
+
+/* Open config file */
+extern FILE	*fconfig;
+
+/* Current line number in current config file */
+extern size_t line_number;
+
+/* Default listening hostname/IP for current config file */
+extern char *defhost;
+
+/* Default IPsec policy for current config file */
+extern char *policy;
+
+/* From parse_v2.c */
+
+typedef enum parse_v2_result {V2_SUCCESS, V2_SKIP, V2_ERROR} parse_v2_result;
+
+/* 
+ * Parse a key-values service definition, starting at the token after
+ * on/off (i.e. parse a series of key-values pairs terminated by a semicolon).
+ * Fills the provided servtab structure. Does not call freeconfig on error.
+ */
+parse_v2_result	parse_syntax_v2(struct servtab *, char **);
+
+/* "Unspecified" indicator value for servtabs (mainly used by v2 syntax) */
+#define SERVTAB_UNSPEC_VAL -1
+
+#define SERVTAB_UNSPEC_SIZE_T SIZE_MAX
+
+#define SERVTAB_COUNT_MAX (SIZE_MAX - (size_t)1)
+
+/* Standard logging and debug print format for a servtab */
+#define SERV_FMT "%s/%s"
+#define SERV_PARAMS(sep) sep->se_service,sep->se_proto
+
+#endif
