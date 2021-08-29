5c4f1f3308e959f9c550055c7bbab3171b78263f
rillig <rillig@NetBSD.org>
2021-08-29 08:57:50 +0000

    tests/lint: test variants of the FALLTHROUGH comment

--- tests/usr.bin/xlint/lint1/msg_220.c
+++ tests/usr.bin/xlint/lint1/msg_220.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: msg_220.c,v 1.3 2021/04/12 15:54:55 christos Exp $	*/
+/*	$NetBSD: msg_220.c,v 1.4 2021/08/29 08:57:50 rillig Exp $	*/
 # 3 "msg_220.c"
 
 // Test for message: fallthrough on case statement [220]
@@ -39,5 +39,34 @@ example1(int n)
 		__attribute__((__fallthrough__));
 	default:
 		println("number");
+	}
+}
+
+/* https://gcc.gnu.org/onlinedocs/gcc/Warning-Options.html#index-Wimplicit-fallthrough */
+void
+annotation_comment_variations(int n)
+{
+	switch (n) {
+	case 0:
+		println("0");
+		/* FALLTHROUGH */
+	case 1:
+		println("1");
+		/* FALL THROUGH */
+		/* expect+1: warning: fallthrough on case statement [220] */
+	case 2:
+		println("2");
+		/* FALLS THROUGH */
+		/* expect+1: warning: fallthrough on case statement [220] */
+	case 3:
+		println("3");
+		/* intentionally falls through */
+		/* expect+1: warning: fallthrough on case statement [220] */
+	case 4:
+		println("4");
+		/* @fallthrough@ */
+		/* expect+1: warning: fallthrough on case statement [220] */
+	case 5:
+		println("5");
 	}
 }
