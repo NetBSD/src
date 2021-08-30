ca401a08a43c3fccfedcc60e796b3ed18d86b8aa
rillig <rillig@NetBSD.org>
2021-08-30 19:48:21 +0000

    tests/lint: test varargs, printflike, scanflike

--- tests/usr.bin/xlint/lint1/msg_280.c
+++ tests/usr.bin/xlint/lint1/msg_280.c
@@ -1,7 +1,60 @@
-/*	$NetBSD: msg_280.c,v 1.2 2021/02/21 09:07:58 rillig Exp $	*/
+/*	$NetBSD: msg_280.c,v 1.3 2021/08/30 19:48:21 rillig Exp $	*/
 # 3 "msg_280.c"
 
 // Test for message: must be outside function: /* %s */ [280]
 
-TODO: "Add example code that triggers the above message." /* expect: 249 */
-TODO: "Add example code that almost triggers the above message."
+/* VARARGS */
+void
+varargs_ok(const char *str, ...)
+{
+	(void)str;
+}
+
+void
+/* XXX: Why is this comment considered 'outside' enough? */
+varargs_bad_param(/* VARARGS */ const char *str, ...)
+{
+	/* expect+1: warning: must be outside function: */
+	/* VARARGS */
+	(void)str;
+}
+
+void
+/* expect+1: warning: must be outside function: */
+varargs_bad_ellipsis(const char *str, /* VARARGS */ ...)
+{
+	(void)str;
+}
+
+void
+/* XXX: Why is this comment considered 'outside' enough? */
+varargs_bad_body(const char *str, ...)
+{
+	/* expect+1: warning: must be outside function */
+	/* VARARGS */
+	(void)str;
+}
+
+void
+/* expect+1: warning: argument 'str' unused in function 'argsused_bad_body' [231] */
+argsused_bad_body(const char *str)
+{
+	/* expect+1: warning: must be outside function */
+	/* ARGSUSED */
+}
+
+void
+printflike_bad_body(const char *fmt, ...)
+{
+	/* expect+1: warning: must be outside function */
+	/* PRINTFLIKE */
+	(void)fmt;
+}
+
+void
+scanflike_bad_body(const char *fmt, ...)
+{
+	/* expect+1: warning: must be outside function */
+	/* SCANFLIKE */
+	(void)fmt;
+}
