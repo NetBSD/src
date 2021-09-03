33b42e1588f93ef4b8d813a74afb6a8cf22eb6ae
rillig <rillig@NetBSD.org>
2021-09-03 22:44:08 +0000

    tests/lint: test GCC builtins for overflow in strict bool mode
    Seen in inetd.c.

---
+++ tests/usr.bin/xlint/lint1/gcc_builtin_overflow.c
@@ -0,0 +1,25 @@
+/*	$NetBSD: gcc_builtin_overflow.c,v 1.1 2021/09/03 22:44:09 rillig Exp $	*/
+# 3 "gcc_builtin_overflow.c"
+
+/*
+ * Some GCC builtin functions return bool, and in lint's strict bool mode,
+ * that makes a difference.
+ *
+ * https://gcc.gnu.org/onlinedocs/gcc/Integer-Overflow-Builtins.html
+ */
+
+/* lint1-extra-flags: -T */
+
+void
+is_overflow(void)
+{
+	int sum;
+
+	/* expect+1: error: controlling expression must be bool, not 'int' [333] */
+	if (__builtin_add_overflow(1, 2, &sum))
+		return;
+
+	/* expect+1: error: controlling expression must be bool, not 'int' [333] */
+	if (__builtin_add_overflow_p(1, 2, 12345))
+		return;
+}
