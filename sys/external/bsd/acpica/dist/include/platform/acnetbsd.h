243b8cc7d7b3bdb5c7d80264f77759b87afdb119
jmcneill <jmcneill@NetBSD.org>
2021-09-03 20:44:23 +0000

    Only define ACPI_QUIET_BOOT for kernel builds

--- sys/external/bsd/acpica/dist/include/platform/acnetbsd.h
+++ sys/external/bsd/acpica/dist/include/platform/acnetbsd.h
@@ -1,7 +1,7 @@
 /******************************************************************************
  *
  * Name: acnetbsd.h - OS specific defines, etc.
- *       $Revision: 1.23 $
+ *       $Revision: 1.24 $
  *
  *****************************************************************************/
 
@@ -85,6 +85,12 @@
 #define ACPI_INTERNAL_XFACE
 #define ACPI_INTERNAL_VAR_XFACE
 
+#if defined(_KERNEL)
+/* Suppress ACPI_INFO level log messages in the kernel when this is true */
+#define ACPI_QUIET_BOOT	\
+	((boothowto & (AB_QUIET|AB_SILENT)) != 0)
+#endif
+
 /*
  * XXX: The internal memory tracking of ACPICA, available when
  *      ACPI_DBG_TRACK_ALLOCATIONS is defined, has been removed
@@ -144,9 +150,5 @@
 #define ACPI_USE_SYSTEM_CLIBRARY
 #define ACPI_USE_NATIVE_DIVIDE
 #define ACPI_USE_NATIVE_MATH64
-
-/* Suppress ACPI_INFO level log messages when this is true */
-#define	ACPI_QUIET_BOOT	\
-	((boothowto & (AB_QUIET|AB_SILENT)) != 0)
 
 #endif /* __ACNETBSD_H__ */
