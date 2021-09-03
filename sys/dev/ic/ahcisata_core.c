71ef55d029afbef386662fc9abb88611f1a46ca7
mrg <mrg@NetBSD.org>
2021-09-03 01:23:33 +0000

    revert "make ahcisata(4) work on rk3399 (rockpro64)" from 23-06-2021.
    this problem is known to affect more than AHCI, and a more general
    solution has been commited now.

--- sys/dev/ic/ahcisata_core.c
+++ sys/dev/ic/ahcisata_core.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: ahcisata_core.c,v 1.100 2021/08/07 16:19:11 thorpej Exp $	*/
+/*	$NetBSD: ahcisata_core.c,v 1.101 2021/09/03 01:23:33 mrg Exp $	*/
 
 /*
  * Copyright (c) 2006 Manuel Bouyer.
@@ -26,7 +26,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: ahcisata_core.c,v 1.100 2021/08/07 16:19:11 thorpej Exp $");
+__KERNEL_RCSID(0, "$NetBSD: ahcisata_core.c,v 1.101 2021/09/03 01:23:33 mrg Exp $");
 
 #include <sys/types.h>
 #include <sys/malloc.h>
@@ -144,30 +144,18 @@ static int
 ahci_reset(struct ahci_softc *sc)
 {
 	int i;
-	uint32_t timeout_ms = 1000;	/* default to 1s timeout */
-	prop_dictionary_t dict;
 
 	/* reset controller */
 	AHCI_WRITE(sc, AHCI_GHC, AHCI_GHC_HR);
-
-	/* some systems (rockchip rk3399) need extra reset time for ahcisata. */
-	dict = device_properties(sc->sc_atac.atac_dev);
-	if (dict)
-		prop_dictionary_get_uint32(dict, "ahci-reset-ms", &timeout_ms);
-
-	/* wait for reset to complete */
-	for (i = 0; i < timeout_ms; i++) {
+	/* wait up to 1s for reset to complete */
+	for (i = 0; i < 1000; i++) {
 		delay(1000);
 		if ((AHCI_READ(sc, AHCI_GHC) & AHCI_GHC_HR) == 0)
 			break;
 	}
-	if ((AHCI_READ(sc, AHCI_GHC) & AHCI_GHC_HR) != 0) {
-		aprint_error_dev(sc->sc_atac.atac_dev, "reset failed\n");
+	if ((AHCI_READ(sc, AHCI_GHC) & AHCI_GHC_HR)) {
+		aprint_error("%s: reset failed\n", AHCINAME(sc));
 		return -1;
-	}
-	if (i > 1000) {
-		aprint_normal_dev(sc->sc_atac.atac_dev,
-		    "reset took %d milliseconds\n", i);
 	}
 	/* enable ahci mode */
 	ahci_enable(sc);
