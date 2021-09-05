8b4a1311f0a491ad0d54b2a8b72543f56e45daaa
rillig <rillig@NetBSD.org>
2021-09-05 10:18:31 +0000

    osnet: fix lint warning about empty declaration
    proc_bkpt.c(32): warning: empty declaration [0]
    
    Use the same definition as in libarchive.

--- external/cddl/osnet/sys/sys/cdefs.h
+++ external/cddl/osnet/sys/sys/cdefs.h
@@ -1,10 +1,10 @@
-/*	$NetBSD: cdefs.h,v 1.1 2018/05/28 21:05:10 chs Exp $	*/
+/*	$NetBSD: cdefs.h,v 1.2 2021/09/05 10:18:31 rillig Exp $	*/
 
 #ifndef _FREEBSD_SYS_CDEFS_H_
 #define _FREEBSD_SYS_CDEFS_H_
 
 #include_next <sys/cdefs.h>
 
-#define __FBSDID(x) /* nothing */
+#define __FBSDID(x) struct _undefined_hack
 
 #endif /* !_FREEBSD_SYS_CDEFS_H_ */
