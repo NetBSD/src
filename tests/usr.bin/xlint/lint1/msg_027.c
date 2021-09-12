caff317e2947ca8ec753fed8d1b8da02e8d2118f
rillig <rillig@NetBSD.org>
2021-09-12 16:08:14 +0000

    tests/lint: demonstrate parse error for function-type parameters
    Seen in sbin/fsck_lfs/vnode.c(104), where lint wrongly complains:
    
    	error: redeclaration of register_vget [27]

--- tests/usr.bin/xlint/lint1/msg_027.c
+++ tests/usr.bin/xlint/lint1/msg_027.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: msg_027.c,v 1.3 2021/01/31 11:12:07 rillig Exp $	*/
+/*	$NetBSD: msg_027.c,v 1.4 2021/09/12 16:08:14 rillig Exp $	*/
 # 3 "msg_027.c"
 
 // Test for message: redeclaration of %s [27]
@@ -6,3 +6,19 @@
 extern int identifier(void);
 
 extern double identifier(void);		/* expect: 27 */
+
+/*
+ * As of 2021-09-12, lint complains about mismatched types.
+ * GCC and Clang accept this.
+ *
+ * Above:
+ *     function(pointer to void, int) returning void
+ *
+ * Below: function(
+ *     pointer to void,
+ *     pointer to function(pointer to void, int) returning pointer to double
+ * ) returning void
+ */
+void function_parameter(void *, double *(void *, int));
+/* expect+1: error: redeclaration of function_parameter [27] */
+void function_parameter(void *fs, double *func(void *, int));
