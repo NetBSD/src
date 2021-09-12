a7fb16a9f67d289f5c3cdaca58f033fae61ad056
rillig <rillig@NetBSD.org>
2021-09-12 16:28:44 +0000

    lint: add more details to error about redeclaration
    Message 27 is triggered by several conditions.  The one triggered by
    register_vget in sbin/fsck_lfs/vnode.c needs more details than the
    others.

--- tests/usr.bin/xlint/lint1/msg_027.c
+++ tests/usr.bin/xlint/lint1/msg_027.c
@@ -1,24 +1,9 @@
-/*	$NetBSD: msg_027.c,v 1.4 2021/09/12 16:08:14 rillig Exp $	*/
+/*	$NetBSD: msg_027.c,v 1.5 2021/09/12 16:28:45 rillig Exp $	*/
 # 3 "msg_027.c"
 
 // Test for message: redeclaration of %s [27]
 
 extern int identifier(void);
 
-extern double identifier(void);		/* expect: 27 */
-
-/*
- * As of 2021-09-12, lint complains about mismatched types.
- * GCC and Clang accept this.
- *
- * Above:
- *     function(pointer to void, int) returning void
- *
- * Below: function(
- *     pointer to void,
- *     pointer to function(pointer to void, int) returning pointer to double
- * ) returning void
- */
-void function_parameter(void *, double *(void *, int));
-/* expect+1: error: redeclaration of function_parameter [27] */
-void function_parameter(void *fs, double *func(void *, int));
+/* expect+1: error: redeclaration of 'identifier' with type 'function(void) returning double', expected 'function(void) returning int' [347] */
+extern double identifier(void);
