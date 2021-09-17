c49c3ec33fab7c2337485b760ce159f249f87b17
christos <christos@NetBSD.org>
2021-09-17 02:18:01 +0000

    - get the version dynamically - move more common stuff into common.mk

--- external/bsd/compiler_rt/lib/clang/lib/netbsd/common.mk
+++ external/bsd/compiler_rt/lib/clang/lib/netbsd/common.mk
@@ -1,3 +1,12 @@
-CLANG_VER=	13.0.0
+# $NetBSD: common.mk,v 1.6 2021/09/17 02:18:01 christos Exp $
+
+NOSANITIZER=	# defined
 NODEBUG=	# defined
 NOLIBCSANITIZER=# defined
+
+.include <bsd.own.mk>
+
+TOPDIR= ${NETBSDSRCDIR}/sys/external/bsd/compiler_rt/dist
+
+CLANG_VER!=	cd ${NETBSDSRCDIR}/external/apache2/llvm && \
+    ${MAKE} -V LLVM_VERSION
