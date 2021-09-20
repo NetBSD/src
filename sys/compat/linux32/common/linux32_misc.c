01b474c9636e7c8a5fde9f2cd081e9979e7ebba9
thorpej <thorpej@NetBSD.org>
2021-09-20 02:20:02 +0000

    Add preadv(2) and pwritev(2) system calls to COMPAT_LINUX and COMPAT_LINUX32.

--- sys/compat/linux32/common/linux32_misc.c
+++ sys/compat/linux32/common/linux32_misc.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: linux32_misc.c,v 1.32 2021/09/20 00:09:02 thorpej Exp $	*/
+/*	$NetBSD: linux32_misc.c,v 1.33 2021/09/20 02:20:03 thorpej Exp $	*/
 
 /*-
  * Copyright (c) 1995, 1998, 1999 The NetBSD Foundation, Inc.
@@ -32,7 +32,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: linux32_misc.c,v 1.32 2021/09/20 00:09:02 thorpej Exp $");
+__KERNEL_RCSID(0, "$NetBSD: linux32_misc.c,v 1.33 2021/09/20 02:20:03 thorpej Exp $");
 
 #include <sys/param.h>
 #include <sys/proc.h>
@@ -379,4 +379,54 @@ linux32_sys_eventfd2(struct lwp *l, const struct linux32_sys_eventfd2_args *uap,
 	NETBSD32TO64_UAP(flags);
 
 	return linux_sys_eventfd2(l, &ua, retval);
+}
+
+static inline off_t
+linux32_hilo_to_off_t(unsigned long hi, unsigned long lo)
+{
+	return (((off_t)hi) << 32) | lo;
+}
+
+int
+linux32_sys_preadv(struct lwp *l, const struct linux32_sys_preadv_args *uap,
+    register_t *retval)
+{
+	/* {
+		syscallarg(int) fd;
+		syscallarg(const netbsd32_iovecp_t) iovp;
+		syscallarg(int) iovcnt;
+		syscallarg(netbsd32_u_long) off_lo;
+		syscallarg(netbsd32_u_long) off_hi;
+	} */
+	struct netbsd32_preadv_args ua;
+
+	SCARG(&ua, fd) = SCARG(uap, fd);
+	SCARG(&ua, iovp) = SCARG(uap, iovp);
+	SCARG(&ua, iovcnt) = SCARG(uap, iovcnt);
+	SCARG(&ua, PAD) = 0;
+	SCARG(&ua, offset) = linux32_hilo_to_off_t(SCARG(uap, off_hi),
+						   SCARG(uap, off_lo));
+	return netbsd32_preadv(l, &ua, retval);
+}
+
+int
+linux32_sys_pwritev(struct lwp *l, const struct linux32_sys_pwritev_args *uap,
+    register_t *retval)
+{
+	/* {
+		syscallarg(int) fd;
+		syscallarg(const netbsd32_iovecp_t) iovp;
+		syscallarg(int) iovcnt;
+		syscallarg(netbsd32_u_long) off_lo;
+		syscallarg(netbsd32_u_long) off_hi;
+	} */
+	struct netbsd32_pwritev_args ua;
+
+	SCARG(&ua, fd) = SCARG(uap, fd);
+	SCARG(&ua, iovp) = SCARG(uap, iovp);
+	SCARG(&ua, iovcnt) = SCARG(uap, iovcnt);
+	SCARG(&ua, PAD) = 0;
+	SCARG(&ua, offset) = linux32_hilo_to_off_t(SCARG(uap, off_hi),
+						   SCARG(uap, off_lo));
+	return netbsd32_pwritev(l, &ua, retval);
 }
