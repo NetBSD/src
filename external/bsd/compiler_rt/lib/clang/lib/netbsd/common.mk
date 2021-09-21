417ec58da522ea2f0a68ac5e1447c96e00903cff
christos <christos@NetBSD.org>
2021-09-21 20:34:37 +0000

    centralize version handling

--- external/bsd/compiler_rt/lib/clang/lib/netbsd/common.mk
+++ external/bsd/compiler_rt/lib/clang/lib/netbsd/common.mk
@@ -1,12 +1,7 @@
-# $NetBSD: common.mk,v 1.6 2021/09/17 02:18:01 christos Exp $
+# $NetBSD: common.mk,v 1.7 2021/09/21 20:34:38 christos Exp $
 
 NOSANITIZER=	# defined
 NODEBUG=	# defined
 NOLIBCSANITIZER=# defined
 
-.include <bsd.own.mk>
-
-TOPDIR= ${NETBSDSRCDIR}/sys/external/bsd/compiler_rt/dist
-
-CLANG_VER!=	cd ${NETBSDSRCDIR}/external/apache2/llvm && \
-    ${MAKE} -V LLVM_VERSION
+.include <bsd.init.mk>
