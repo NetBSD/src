c87ed1b20f6a646a0ba7a08976e7fe9b07699703
knakahara <knakahara@NetBSD.org>
2021-09-24 04:09:32 +0000

    Import asymmetric toeplitz hash without memcpy implemented by ryo@n.o.
    This implementation has better performance than memcpy'ed one.
    (30%-60% improvement in micro benchmark)
    
    import from
        https://github.com/ryo/l2pkt/blob/master/l2pkt/toeplitz_hash.c

--- sys/net/toeplitz.c
+++ sys/net/toeplitz.c
@@ -202,3 +202,87 @@ stoeplitz_to_key(void *key, size_t klen)
 		k[i + 1] = skey;
 	}
 }
+
+/*
+ * e.g.)
+ *
+ * struct in_addr src, dst;
+ * uint16_t srcport, dstport;
+ * toeplitz_vhash(rsskey[], sizeof(rsskey),
+ *                    &src, sizeof(src),
+ *                    &dst, sizeof(dst),
+ *                    &srcport, sizeof(srcport),
+ *                    &dstport, sizeof(dstport),
+ *                    NULL);
+ *
+ * struct in6_addr src6, dst6;
+ * toeplitz_vhash(rsskey[], sizeof(rsskey),
+ *                    &src6, sizeof(src6),
+ *                    &dst6, sizeof(dst6),
+ *                    NULL);
+ *
+ * struct ip *ip;
+ * struct tcphdr *tcp;
+ * toeplitz_vhash(rsskey[], sizeof(rsskey),
+ *                    &ip->ip_src, sizeof(ip->ip_src),
+ *                    &ip->ip_dst, sizeof(ip->ip_dst),
+ *                    &tcp->th_sport, sizeof(tcp->th_sport),
+ *                    &tcp->th_dport, sizeof(tcp->th_dport),
+ *                    NULL);
+ *
+ */
+uint32_t
+toeplitz_vhash(const uint8_t *keyp, size_t keylen, ...)
+{
+	va_list ap;
+	uint32_t hash, v;
+	size_t datalen;
+	uint8_t *datap, key, data;
+	const uint8_t *keyend;
+
+	keyend = keyp + keylen;
+
+	/* first 32bit is initial vector */
+	v = *keyp++;
+	v <<= 8;
+	v |= *keyp++;
+	v <<= 8;
+	v |= *keyp++;
+	v <<= 8;
+	v |= *keyp++;
+
+	hash = 0;
+	va_start(ap, keylen);
+
+	while ((datap = va_arg(ap, uint8_t *)) != NULL) {
+		for (datalen = va_arg(ap, size_t); datalen > 0; datalen--) {
+			/* fetch key and input data by 8bit */
+			if (keyp < keyend)
+				key = *keyp++;
+			else
+				key = 0;
+			data = *datap++;
+
+#define XOR_AND_FETCH_BIT(x)			\
+			if (data & __BIT(x))		\
+				hash ^= v;		\
+			v <<= 1;			\
+			if (key & __BIT(x))		\
+				v |= 1;
+
+			XOR_AND_FETCH_BIT(7);
+			XOR_AND_FETCH_BIT(6);
+			XOR_AND_FETCH_BIT(5);
+			XOR_AND_FETCH_BIT(4);
+			XOR_AND_FETCH_BIT(3);
+			XOR_AND_FETCH_BIT(2);
+			XOR_AND_FETCH_BIT(1);
+			XOR_AND_FETCH_BIT(0);
+
+#undef XOR_AND_FETCH_BIT
+		}
+	}
+	va_end(ap);
+
+	return hash;
+}
