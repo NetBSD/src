1d9f14291fb6f77dfb36454783acc743558373aa
jmcneill <jmcneill@NetBSD.org>
2021-09-26 20:15:04 +0000

    Use the yield instruction as SPINLOCK_BACKOFF_HOOK for aarch64.

--- sys/arch/aarch64/include/lock.h
+++ sys/arch/aarch64/include/lock.h
@@ -1,6 +1,12 @@
-/* $NetBSD: lock.h,v 1.3 2015/06/26 18:27:52 matt Exp $ */
+/* $NetBSD: lock.h,v 1.4 2021/09/26 20:15:04 jmcneill Exp $ */
 
 #ifdef __aarch64__
+# ifdef _HARDKERNEL
+#  ifdef SPINLOCK_BACKOFF_HOOK
+#   undef SPINLOCK_BACKOFF_HOOK
+#  endif
+#  define SPINLOCK_BACKOFF_HOOK		asm volatile("yield" ::: "memory")
+# endif
 # include <sys/common_lock.h>
 #elif defined(__arm__)
 # include <arm/lock.h>
