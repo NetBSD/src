f525a4a4340b188e93b8639b78d24daf2af45480
rillig <rillig@NetBSD.org>
2021-09-26 14:28:22 +0000

    tests/lint: run all platform tests with the same options

--- tests/usr.bin/xlint/lint1/platform_schar.c
+++ tests/usr.bin/xlint/lint1/platform_schar.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: platform_schar.c,v 1.1 2021/09/26 03:17:59 rillig Exp $	*/
+/*	$NetBSD: platform_schar.c,v 1.2 2021/09/26 14:28:22 rillig Exp $	*/
 # 3 "platform_schar.c"
 
 /*
@@ -6,8 +6,10 @@
  * representation as signed char.
  */
 
+/* lint1-extra-flags: -c -h -a -p -b -r -z */
 /* lint1-only-if: schar */
 
 // TODO: Add some code that passes.
 // TODO: Add some code that fails.
+
 /* expect+1: warning: empty translation unit [272] */
