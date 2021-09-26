323bcd7a79520484a4ad5deb655df7e6d33752f3
rillig <rillig@NetBSD.org>
2021-09-26 14:28:22 +0000

    tests/lint: run all platform tests with the same options

--- tests/usr.bin/xlint/lint1/platform_ilp32.c
+++ tests/usr.bin/xlint/lint1/platform_ilp32.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: platform_ilp32.c,v 1.1 2021/09/26 03:17:59 rillig Exp $	*/
+/*	$NetBSD: platform_ilp32.c,v 1.2 2021/09/26 14:28:22 rillig Exp $	*/
 # 3 "platform_ilp32.c"
 
 /*
@@ -6,8 +6,10 @@
  * pointer types.
  */
 
+/* lint1-extra-flags: -c -h -a -p -b -r -z */
 /* lint1-only-if: ilp32 */
 
 // TODO: Add some code that passes.
 // TODO: Add some code that fails.
+
 /* expect+1: warning: empty translation unit [272] */
