cda237786fb435ed430c6a92b0bb547107dae0e4
rillig <rillig@NetBSD.org>
2021-10-09 21:25:39 +0000

    tests/lint: add examples for bogus warning 160 and for error 323

--- tests/usr.bin/xlint/lint1/msg_323.c
+++ tests/usr.bin/xlint/lint1/msg_323.c
@@ -1,7 +1,21 @@
-/*	$NetBSD: msg_323.c,v 1.2 2021/02/21 09:07:58 rillig Exp $	*/
+/*	$NetBSD: msg_323.c,v 1.3 2021/10/09 21:25:39 rillig Exp $	*/
 # 3 "msg_323.c"
 
 // Test for message: continue in 'do ... while (0)' loop [323]
+void println(const char *);
 
-TODO: "Add example code that triggers the above message." /* expect: 249 */
-TODO: "Add example code that almost triggers the above message."
+void
+example(const char *p)
+{
+	do {
+		switch (*p) {
+		case 'a':
+			continue;	/* leaves the 'do while 0' */
+		case 'b':
+			break;		/* leaves the 'switch' */
+		}
+		println("b");
+	/* XXX: Is that really worth an error? */
+	/* expect+1: error: continue in 'do ... while (0)' loop [323] */
+	} while (0);
+}
