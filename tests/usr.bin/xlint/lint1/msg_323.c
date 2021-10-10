9545b1c9835dd5f38001965b66e4ee4084f5ac43
rillig <rillig@NetBSD.org>
2021-10-10 09:17:24 +0000

    tests/lint: add more examples for continue in do-while-0

--- tests/usr.bin/xlint/lint1/msg_323.c
+++ tests/usr.bin/xlint/lint1/msg_323.c
@@ -1,11 +1,35 @@
-/*	$NetBSD: msg_323.c,v 1.3 2021/10/09 21:25:39 rillig Exp $	*/
+/*	$NetBSD: msg_323.c,v 1.4 2021/10/10 09:17:24 rillig Exp $	*/
 # 3 "msg_323.c"
 
 // Test for message: continue in 'do ... while (0)' loop [323]
+
 void println(const char *);
 
+/*
+ * In simple cases of a do-while-0 loop, the statements 'break' and
+ * 'continue' have the same effect, and 'break' is much more common.
+ *
+ * This is also covered by Clang-Tidy.
+ */
 void
-example(const char *p)
+simple_case(const char *p)
+{
+	do {
+		if (p[0] == '+')
+			break;
+		if (p[1] == '-')
+			continue;
+		println("no sign");
+	/* expect+1: error: continue in 'do ... while (0)' loop [323] */
+	} while (0);
+}
+
+/*
+ * If there is a 'switch' statement inside the do-while-0 loop, the 'break'
+ * statement is tied to the 'switch' statement instead of the loop.
+ */
+void
+nested_switch(const char *p)
 {
 	do {
 		switch (*p) {
@@ -17,5 +41,20 @@ example(const char *p)
 		println("b");
 	/* XXX: Is that really worth an error? */
 	/* expect+1: error: continue in 'do ... while (0)' loop [323] */
+	} while (0);
+}
+
+/*
+ * In a nested loop, the 'continue' statement is bound to the inner loop,
+ * thus no warning.
+ */
+void
+nested_for(void)
+{
+	do {
+		for (int i = 0; i < 6; i++) {
+			if (i < 3)
+				continue;
+		}
 	} while (0);
 }
