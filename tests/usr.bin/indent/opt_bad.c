f4321f9766957ec1bf97f375da1e896b8b9e25e8
rillig <rillig@NetBSD.org>
2021-10-16 09:39:21 +0000

    tests/indent: clean up tests for '-bad' and '-badp'

--- tests/usr.bin/indent/opt_bad.c
+++ tests/usr.bin/indent/opt_bad.c
@@ -1,57 +1,31 @@
-/* $NetBSD: opt_bad.c,v 1.1 2021/10/16 03:20:13 rillig Exp $ */
+/* $NetBSD: opt_bad.c,v 1.2 2021/10/16 09:39:21 rillig Exp $ */
 /* $FreeBSD$ */
 
-#indent input
 /*
- * The option -bad only affects declarations of local variables.  It does not
- * affect file-scoped declarations or definitions.
+ * Tests for the options '-bad' and '-nbad'.
+ *
+ * The option '-bad' forces a blank line after every block of declarations.
+ * It only affects declarations of local variables.  It does not affect
+ * file-scoped declarations or definitions.
+ *
+ * The option '-nbad' leaves everything as is.
  */
 
+/* Test global declarations. */
+#indent input
 int global_variable;
 void function_declaration(void);
 #if 0
 #endif
-void function_definition(void) {
-	int local_variable;
-	function_call();
-	int local_variable_after_statement;
-	function_call();
-}
+/* comment */
 #indent end
 
 #indent run -bad
-/*
- * The option -bad only affects declarations of local variables.  It does not
- * affect file-scoped declarations or definitions.
- */
-
 int		global_variable;
 void		function_declaration(void);
 #if 0
 #endif
-void
-function_definition(void)
-{
-	int		local_variable;
-
-	function_call();
-	int		local_variable_after_statement;
-
-	function_call();
-}
-#indent end
-
-#indent input
-int global_variable;
-void function_declaration(void);
-#if 0
-#endif
-void function_definition(void) {
-	int local_variable;
-	function_call();
-	int local_variable_after_statement;
-	function_call();
-}
+/* comment */
 #indent end
 
 #indent run -nbad
@@ -59,12 +33,41 @@ int		global_variable;
 void		function_declaration(void);
 #if 0
 #endif
+/* comment */
+#indent end
+
+/* Test local declarations. */
+#indent input
+void function_definition(void) {
+    int local_variable;
+    function_call();
+    int local_variable_after_statement;
+    function_call();
+}
+#indent end
+
+#indent run -bad
 void
 function_definition(void)
 {
 	int		local_variable;
+
 	function_call();
 	int		local_variable_after_statement;
+
+	function_call();
+}
+#indent end
+
+#indent run -nbad
+void
+function_definition(void)
+{
+	int		local_variable;
+	/* $ No blank line here. */
+	function_call();
+	int		local_variable_after_statement;
+	/* $ No blank line here. */
 	function_call();
 }
 #indent end
