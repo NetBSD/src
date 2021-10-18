a2af78d188be3ec5fad0e2802a41930cabf0ff37
rillig <rillig@NetBSD.org>
2021-10-18 20:18:00 +0000

    tests/indent: extend tests for labels, extract test driver
    Having the test driver in a separate file allows to run it on its own,
    as 'awk -f t_options.awk label.c'. When the test driver warns, exit 1;
    this is only needed for the standalone version, when run from inside ATF
    the warnings on stderr already suffice to make the test fail in the end.

---
+++ tests/usr.bin/indent/label.c
@@ -0,0 +1,124 @@
+/*	$NetBSD: label.c,v 1.1 2021/10/18 20:18:00 rillig Exp $	*/
+/* $FreeBSD: head/usr.bin/indent/tests/label.0 313544 2017-02-10 09:31:39Z pstef $ */
+
+/* See FreeBSD r303489 */
+
+#indent input
+void t(void) {
+	switch (1)
+	{
+		case 1:	/* test */
+		case 2:		/* test */
+	}
+CLEANUP:
+	;
+V: ;
+U: ;
+}
+#indent end
+
+#indent run
+void
+t(void)
+{
+	switch (1) {
+	case 1:			/* test */
+	case 2:			/* test */
+	}
+CLEANUP:
+	;
+V:	;
+U:	;
+}
+#indent end
+
+#indent input
+void
+label_width(void)
+{
+L:;
+L2:;
+L_3:;
+L__4:;
+L___5:;
+L____6:;
+L_____7:;
+L______8:;
+}
+#indent end
+
+#indent run
+void
+label_width(void)
+{
+L:	;
+L2:	;
+L_3:	;
+L__4:	;
+L___5:	;
+L____6:	;
+L_____7:;
+L______8:;
+}
+#indent end
+
+/*
+ * The indentation of statement labels is fixed to -2, it is not configurable.
+ */
+#indent input
+void
+label_indentation(void)
+{
+	if (level1) {
+	if (level2) {
+	if (level3) {
+	if (level4) {
+	if (level5) {
+	label5:
+	statement();
+	}
+	label4:
+	statement();
+	}
+	label3:
+	statement();
+	}
+	label2:
+	statement();
+	}
+	label1:
+	statement();
+	}
+	label0:
+	statement();
+}
+#indent end
+
+#indent run
+void
+label_indentation(void)
+{
+	if (level1) {
+		if (level2) {
+			if (level3) {
+				if (level4) {
+					if (level5) {
+				label5:
+						statement();
+					}
+			label4:
+					statement();
+				}
+		label3:
+				statement();
+			}
+	label2:
+			statement();
+		}
+label1:
+		statement();
+	}
+label0:
+	statement();
+}
+#indent end
--- tests/usr.bin/indent/t_options.sh
+++ tests/usr.bin/indent/t_options.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
-# $NetBSD: t_options.sh,v 1.7 2021/10/18 19:36:30 rillig Exp $
+# $NetBSD: t_options.sh,v 1.8 2021/10/18 20:18:00 rillig Exp $
 #
 # Copyright (c) 2021 The NetBSD Foundation, Inc.
 # All rights reserved.
@@ -51,149 +51,10 @@
 srcdir=$(atf_get_srcdir)
 indent=$(atf_config_get usr.bin.indent.test_indent /usr/bin/indent)
 
-# Read the test specification from stdin, output the actual test output on
-# stdout, write the expected test output to 'expected.out'.
-#
-# shellcheck disable=SC2016
-check_awk='
-function die(lineno, msg)
-{
-	if (!died) {
-		died = 1
-		print FILENAME ":" lineno ": error: " msg > "/dev/stderr"
-		exit(1)
-	}
-}
-
-function warn(lineno, msg)
-{
-	print FILENAME ":" lineno ": warning: " msg > "/dev/stderr"
-}
-
-function quote(s)
-{
-	return "'\''" s "'\''"
-}
-
-BEGIN {
-	section = ""		# "", "input" or "run"
-	section_excl_comm = ""	# without dollar comments
-	section_incl_comm = ""	# with dollar comments
-
-	input_excl_comm = ""	# stdin for indent
-	input_incl_comm = ""	# used for duplicate checks
-	unused_input_lineno = 0
-
-	output_excl_comm = ""	# expected output
-	output_incl_comm = ""	# used for duplicate checks
-	output_lineno = 0
-}
-
-# Hide comments starting with dollar from indent; they are used for marking
-# bugs and adding other remarks directly in the input or output sections.
-/^[[:space:]]*\/[*][[:space:]]*[$].*[*]\/$/ ||
-    /^[[:space:]]*\/\/[[:space:]]*[$]/ {
-	if (section != "")
-		section_incl_comm = section_incl_comm $0 "\n"
-	next
-}
-
-function check_unused_input()
-{
-	if (unused_input_lineno != 0)
-		warn(unused_input_lineno, "input is not used")
-}
-
-function run_indent(inp,   i, cmd)
-{
-	cmd = ENVIRON["INDENT"]
-	for (i = 3; i <= NF; i++)
-		cmd = cmd " " $i
-	printf("%s", inp) | cmd
-	close(cmd)
-}
-
-/^#/ && $1 == "#indent" {
-	print $0
-	print $0 > "expected.out"
-
-	if ($2 == "input") {
-		check_unused_input()
-		section = "input"
-		section_excl_comm = ""
-		section_incl_comm = ""
-		unused_input_lineno = NR
-
-	} else if ($2 == "run") {
-		section = "run"
-		output_lineno = NR
-		section_excl_comm = ""
-		section_incl_comm = ""
-
-		run_indent(input_excl_comm)
-		unused_input_lineno = 0
-
-	} else if ($2 == "run-equals-input") {
-		run_indent(input_excl_comm)
-		printf("%s", input_excl_comm) > "expected.out"
-		unused_input_lineno = 0
-
-	} else if ($2 == "run-equals-prev-output") {
-		run_indent(input_excl_comm)
-		printf("%s", output_excl_comm) > "expected.out"
-
-	} else if ($2 == "end" && section == "input") {
-		if (section_incl_comm == input_incl_comm)
-			warn(NR, "duplicate input; remove this section")
-
-		input_excl_comm = section_excl_comm
-		input_incl_comm = section_incl_comm
-		section = ""
-
-	} else if ($2 == "end" && section == "run") {
-		if (section_incl_comm == input_incl_comm)
-			warn(output_lineno,
-			    "output == input; use run-equals-input")
-		if (section_incl_comm == output_incl_comm)
-			warn(output_lineno,
-			    "duplicate output; use run-equals-prev-output")
-
-		output_excl_comm = section_excl_comm
-		output_incl_comm = section_incl_comm
-		section = ""
-
-	} else if ($2 == "end") {
-		warn(NR, "misplaced " quote("#indent end"))
-
-	} else {
-		die(NR, "invalid line " quote($0))
-	}
-
-	next
-}
-
-section == "input" || section == "run" {
-	section_excl_comm = section_excl_comm $0 "\n"
-	section_incl_comm = section_incl_comm $0 "\n"
-}
-
-section == "run" {
-	print $0 > "expected.out"
-}
-
-END {
-	if (section != "")
-		die(NR, "still in section " quote(section))
-	check_unused_input()
-}
-'
-
 check()
 {
-	printf '%s\n' "$check_awk" > check.awk
-
 	atf_check -o "file:expected.out" \
-	    env INDENT="$indent" awk -f check.awk "$srcdir/$1.c"
+	    env INDENT="$indent" awk -f "$srcdir/t_options.awk" "$srcdir/$1.c"
 }
 
 atf_init_test_cases()
