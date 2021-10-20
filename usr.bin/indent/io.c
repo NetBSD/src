f9a1f504d50fdbcec0c2d2e3cb3ee2457806da37
rillig <rillig@NetBSD.org>
2021-10-20 05:00:37 +0000

    indent: rename next_blank_lines to blank_lines_to_output
    The previous name was already an improvement over the name before that
    (n_real_blanklines), but didn't express the intended purpose clearly
    enough, so try another name.
    
    No functional change.

--- usr.bin/indent/io.c
+++ usr.bin/indent/io.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: io.c,v 1.97 2021/10/19 21:39:19 rillig Exp $	*/
+/*	$NetBSD: io.c,v 1.98 2021/10/20 05:00:37 rillig Exp $	*/
 
 /*-
  * SPDX-License-Identifier: BSD-4-Clause
@@ -43,7 +43,7 @@ static char sccsid[] = "@(#)io.c	8.1 (Berkeley) 6/6/93";
 
 #include <sys/cdefs.h>
 #if defined(__NetBSD__)
-__RCSID("$NetBSD: io.c,v 1.97 2021/10/19 21:39:19 rillig Exp $");
+__RCSID("$NetBSD: io.c,v 1.98 2021/10/20 05:00:37 rillig Exp $");
 #elif defined(__FreeBSD__)
 __FBSDID("$FreeBSD: head/usr.bin/indent/io.c 334927 2018-06-10 16:44:18Z pstef $");
 #endif
@@ -221,21 +221,21 @@ dump_line(void)
 	if (suppress_blanklines)
 	    suppress_blanklines = false;
 	else
-	    next_blank_lines++;
+	    blank_lines_to_output++;
 
     } else if (!inhibit_formatting) {
 	suppress_blanklines = false;
 	if (prefix_blankline_requested && !first_line) {
 	    if (opt.swallow_optional_blanklines) {
-		if (next_blank_lines == 1)
-		    next_blank_lines = 0;
+		if (blank_lines_to_output == 1)
+		    blank_lines_to_output = 0;
 	    } else {
-		if (next_blank_lines == 0)
-		    next_blank_lines = 1;
+		if (blank_lines_to_output == 0)
+		    blank_lines_to_output = 1;
 	    }
 	}
 
-	for (; next_blank_lines > 0; next_blank_lines--)
+	for (; blank_lines_to_output > 0; blank_lines_to_output--)
 	    output_char('\n');
 
 	if (ps.ind_level == 0)
@@ -387,7 +387,7 @@ parse_indent_comment(void)
 
     inhibit_formatting = !on;
     if (on) {
-	next_blank_lines = 0;
+	blank_lines_to_output = 0;
 	postfix_blankline_requested = false;
 	prefix_blankline_requested = false;
 	suppress_blanklines = true;
