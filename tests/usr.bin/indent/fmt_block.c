8c1e5e0299e96ca2aeb2e79be074046198f4efa0
rillig <rillig@NetBSD.org>
2021-10-22 19:27:53 +0000

    tests/indent: migrate a few more tests to t_options

--- tests/usr.bin/indent/fmt_block.c
+++ tests/usr.bin/indent/fmt_block.c
@@ -1,6 +1,21 @@
-/* $NetBSD: block.0.stdout,v 1.1 2021/03/08 20:55:34 rillig Exp $ */
+/* $NetBSD: fmt_block.c,v 1.1 2021/10/22 19:27:53 rillig Exp $ */
 /* $FreeBSD$ */
 
+#indent input
+void
+function(void)
+{
+	if (true) {
+
+	}
+
+	{
+		print("block");
+	}
+}
+#indent end
+
+#indent run
 void
 function(void)
 {
@@ -13,3 +28,4 @@ function(void)
 		print("block");
 	}
 }
+#indent end
--- tests/usr.bin/indent/lineno.0.pro
+++
@@ -1,4 +0,0 @@
-/* $NetBSD: lineno.0.pro,v 1.1 2021/03/08 20:12:04 rillig Exp $ */
-/* $FreeBSD$ */
-
--v
