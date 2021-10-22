7f26aefc550a833109be07fcbb7a285950b2bc5e
rillig <rillig@NetBSD.org>
2021-10-22 19:46:41 +0000

    tests/indent: migrate tests for parentheses, struct, #if, wide char

--- tests/usr.bin/indent/token_keyword_struct_union_enum.c
+++ tests/usr.bin/indent/token_keyword_struct_union_enum.c
@@ -1,4 +1,4 @@
-/* $NetBSD: token_keyword_struct_union_enum.c,v 1.1 2021/10/18 22:30:34 rillig Exp $ */
+/* $NetBSD: token_keyword_struct_union_enum.c,v 1.2 2021/10/22 19:46:41 rillig Exp $ */
 /* $FreeBSD$ */
 
 /*
@@ -21,3 +21,50 @@ union variant {
 #indent end
 
 #indent run-equals-input
+
+
+/* See FreeBSD r303485. */
+/* $FreeBSD: head/usr.bin/indent/tests/struct.0 334564 2018-06-03 16:21:15Z pstef $ */
+#indent input
+int f(struct x *a);
+
+void
+t(void)
+{
+	static const struct {
+		int	a;
+		int	b;
+	} c[] = {
+		{ D, E },
+		{ F, G }
+	};
+}
+
+void u(struct x a) {
+	int b;
+	struct y c = (struct y *)&a;
+}
+#indent end
+
+#indent run
+int		f(struct x *a);
+
+void
+t(void)
+{
+	static const struct {
+		int		a;
+		int		b;
+	}		c[] = {
+		{D, E},
+		{F, G}
+	};
+}
+
+void
+u(struct x a)
+{
+	int		b;
+	struct y	c = (struct y *)&a;
+}
+#indent end
--- tests/usr.bin/indent/token_string_prefix.c
+++ tests/usr.bin/indent/token_string_prefix.c
@@ -1,8 +1,10 @@
-/* $NetBSD: token_string_prefix.c,v 1.1 2021/10/18 22:30:34 rillig Exp $ */
+/* $NetBSD: token_string_prefix.c,v 1.2 2021/10/22 19:46:41 rillig Exp $ */
 /* $FreeBSD$ */
 
 /*
  * Tests for strings of wide characters, which are prefixed with 'L'.
+ *
+ * See FreeBSD r309220.
  */
 
 #indent input
@@ -16,3 +18,10 @@ wchar_t wide_string[] = L"wide string";
 #indent run-equals-input -di0
 #indent run-equals-input -di0 -l25
 #indent run-equals-input -di0 -l1
+
+
+#indent input
+wchar_t wide_char[] = L'w';
+#indent end
+
+#indent run-equals-input -di0
