9d788be610e84b6aaab7033469798aeecf0dba19
martin <martin@NetBSD.org>
2021-10-25 18:11:02 +0000

    Pull up following revision(s) (requested by ryo in ticket #1369):
    	sys/net/if_ethersubr.c: revision 1.302
    
    frame's vlan tag must be ntohs()'ed.
    
    VLAN 0 Priority tag was misrecognized on non vlan-hwtagging interfaces.

--- sys/net/if_ethersubr.c
+++ sys/net/if_ethersubr.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: if_ethersubr.c,v 1.276.2.1 2019/10/08 17:02:24 martin Exp $	*/
+/*	$NetBSD: if_ethersubr.c,v 1.276.2.2 2021/10/25 18:11:02 martin Exp $	*/
 
 /*
  * Copyright (C) 1995, 1996, 1997, and 1998 WIDE Project.
@@ -61,7 +61,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: if_ethersubr.c,v 1.276.2.1 2019/10/08 17:02:24 martin Exp $");
+__KERNEL_RCSID(0, "$NetBSD: if_ethersubr.c,v 1.276.2.2 2021/10/25 18:11:02 martin Exp $");
 
 #ifdef _KERNEL_OPT
 #include "opt_inet.h"
@@ -727,7 +727,7 @@ ether_input(struct ifnet *ifp, struct mbuf *m)
 		 * type, and if IP or IPV6, let them deal with it.
 		 */
 		if (m->m_len >= sizeof(*evl) &&
-		    EVL_VLANOFTAG(evl->evl_tag) == 0) {
+		    EVL_VLANOFTAG(ntohs(evl->evl_tag)) == 0) {
 			etype = ntohs(evl->evl_proto);
 			ehlen = sizeof(*evl);
 			if ((m->m_flags & M_PROMISC) == 0 &&
