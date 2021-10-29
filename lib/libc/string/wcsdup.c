2ba085037b180407513c24b56654305d53057cb0
nia <nia@NetBSD.org>
2021-10-29 11:03:46 +0000

    reallocarr does not set errno.

--- lib/libc/string/wcsdup.c
+++ lib/libc/string/wcsdup.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: wcsdup.c,v 1.4 2021/10/29 10:11:57 nia Exp $	*/
+/*	$NetBSD: wcsdup.c,v 1.5 2021/10/29 11:03:46 nia Exp $	*/
 
 /*
  * Copyright (C) 2006 Aleksey Cheusov
@@ -14,12 +14,13 @@
 #include <sys/cdefs.h>
 
 #if defined(LIBC_SCCS) && !defined(lint) 
-__RCSID("$NetBSD: wcsdup.c,v 1.4 2021/10/29 10:11:57 nia Exp $"); 
+__RCSID("$NetBSD: wcsdup.c,v 1.5 2021/10/29 11:03:46 nia Exp $"); 
 #endif /* LIBC_SCCS and not lint */ 
 
 #include "namespace.h"
 #include <stdlib.h>
 #include <assert.h>
+#include <errno.h>
 #include <wchar.h>
 
 __weak_alias(wcsdup,_wcsdup)
@@ -35,8 +36,10 @@ wcsdup(const wchar_t *str)
 	len = wcslen(str) + 1;
 
 	copy = NULL;
-	if (reallocarr(&copy, len, sizeof(wchar_t)) != 0)
+	if (reallocarr(&copy, len, sizeof(wchar_t)) != 0) {
+		errno = ENOMEM;
 		return NULL;
+	}
 
 	return wmemcpy(copy, str, len);
 }
