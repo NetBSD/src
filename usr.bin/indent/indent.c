ca7f767137bcfdb75f362fbfd509ce2b1c12346b
rillig <rillig@NetBSD.org>
2021-11-07 18:49:02 +0000

    indent: demonstrate disappearing form feed

--- usr.bin/indent/indent.c
+++ usr.bin/indent/indent.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: indent.c,v 1.216 2021/11/07 18:26:17 rillig Exp $	*/
+/*	$NetBSD: indent.c,v 1.217 2021/11/07 18:49:02 rillig Exp $	*/
 
 /*-
  * SPDX-License-Identifier: BSD-4-Clause
@@ -43,7 +43,7 @@ static char sccsid[] = "@(#)indent.c	5.17 (Berkeley) 6/7/93";
 
 #include <sys/cdefs.h>
 #if defined(__NetBSD__)
-__RCSID("$NetBSD: indent.c,v 1.216 2021/11/07 18:26:17 rillig Exp $");
+__RCSID("$NetBSD: indent.c,v 1.217 2021/11/07 18:49:02 rillig Exp $");
 #elif defined(__FreeBSD__)
 __FBSDID("$FreeBSD: head/usr.bin/indent/indent.c 340138 2018-11-04 19:24:49Z oshogbo $");
 #endif
@@ -475,6 +475,7 @@ search_stmt(lexer_symbol *lsym, bool *force_nl, bool *last_else)
 	    search_stmt_newline(force_nl);
 	    break;
 	case lsym_form_feed:
+	    /* XXX: Is simply removed from the source code. */
 	    break;
 	case lsym_comment:
 	    search_stmt_comment();
