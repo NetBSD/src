8f09a92475466d944c9f75c2a4d53abf3bdf77fe
uwe <uwe@NetBSD.org>
2021-11-24 14:34:51 +0000

    wsmoused: support absolute mouse position events
    Tested with VirtualBox Guest Addtions.

--- usr.sbin/wsmoused/wsmoused.c
+++ usr.sbin/wsmoused/wsmoused.c
@@ -1,4 +1,4 @@
-/* $NetBSD: wsmoused.c,v 1.27 2021/09/01 06:10:06 mlelstv Exp $ */
+/* $NetBSD: wsmoused.c,v 1.28 2021/11/24 14:34:51 uwe Exp $ */
 
 /*
  * Copyright (c) 2002, 2003, 2004 The NetBSD Foundation, Inc.
@@ -34,7 +34,7 @@
 #ifndef lint
 __COPYRIGHT("@(#) Copyright (c) 2002, 2003\
  The NetBSD Foundation, Inc.  All rights reserved.");
-__RCSID("$NetBSD: wsmoused.c,v 1.27 2021/09/01 06:10:06 mlelstv Exp $");
+__RCSID("$NetBSD: wsmoused.c,v 1.28 2021/11/24 14:34:51 uwe Exp $");
 #endif /* not lint */
 
 #include <sys/ioctl.h>
@@ -250,7 +250,7 @@ init_mouse(void)
 static void
 open_device(unsigned int secs)
 {
-	int version = WSMOUSE_EVENT_VERSION;
+	int status;
 
 	if (Mouse.m_devfd != -1)
 		return;
@@ -262,9 +262,37 @@ open_device(unsigned int secs)
 	if (Mouse.m_devfd == -1)
 		log_err(EXIT_FAILURE, "cannot open %s", Mouse.m_devname);
 
-	if (ioctl(Mouse.m_devfd, WSMOUSEIO_SETVERSION, &version) == -1)
+	const int version = WSMOUSE_EVENT_VERSION;
+	status = ioctl(Mouse.m_devfd, WSMOUSEIO_SETVERSION, &version);
+	if (status == -1)
 		log_err(EXIT_FAILURE, "cannot set version %s", Mouse.m_devname);
+
+
+	/*
+	 * Get calibration data for touch panel.  Not fatal if we can't.
+	 */
+	Mouse.m_doabs = 0;
+
+	unsigned int mouse_type = 0; /* defined WSMOUSE_TYPE_* start at 1 */
+	status = ioctl(Mouse.m_devfd, WSMOUSEIO_GTYPE, &mouse_type);
+	if (status == -1) {
+		log_warn("WSMOUSEIO_GTYPE");
+		return;
+	}
+
+	/* absolute position events make no sense for free-ranging mice */
+	if (mouse_type != WSMOUSE_TYPE_TPANEL)
+		return;
+
+	status = ioctl(Mouse.m_devfd, WSMOUSEIO_GCALIBCOORDS, &Mouse.m_calib);
+	if (status == -1) {
+		log_warn("WSMOUSEIO_GCALIBCOORDS");
+		return;
+	}
+
+	Mouse.m_doabs = 1;
 }
+
 
 /* --------------------------------------------------------------------- */
 
