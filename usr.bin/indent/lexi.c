438ce16ba34432b52d6ca93afbd6ac53d21da5bc
rillig <rillig@NetBSD.org>
2021-11-27 20:58:16 +0000

    indent: illustrate probably_looking_at_definition with examples
    No functional change.

--- usr.bin/indent/lexi.c
+++ usr.bin/indent/lexi.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: lexi.c,v 1.165 2021/11/27 20:33:39 rillig Exp $	*/
+/*	$NetBSD: lexi.c,v 1.166 2021/11/27 20:58:16 rillig Exp $	*/
 
 /*-
  * SPDX-License-Identifier: BSD-4-Clause
@@ -43,7 +43,7 @@ static char sccsid[] = "@(#)lexi.c	8.1 (Berkeley) 6/6/93";
 
 #include <sys/cdefs.h>
 #if defined(__NetBSD__)
-__RCSID("$NetBSD: lexi.c,v 1.165 2021/11/27 20:33:39 rillig Exp $");
+__RCSID("$NetBSD: lexi.c,v 1.166 2021/11/27 20:58:16 rillig Exp $");
 #elif defined(__FreeBSD__)
 __FBSDID("$FreeBSD: head/usr.bin/indent/lexi.c 337862 2018-08-15 18:19:45Z pstef $");
 #endif
@@ -451,7 +451,7 @@ cmp_keyword_by_name(const void *key, const void *elem)
 }
 
 /*
- * Looking at a line starting with 'function_name(something)', guess whether
+ * Looking at something like 'function_name(...)' in a line, guess whether
  * this starts a function definition or a declaration.
  */
 static bool
@@ -463,14 +463,20 @@ probably_looking_at_definition(void)
 	    paren_level++;
 	if (*p == ')' && --paren_level == 0) {
 	    p++;
+
 	    while (p < e && (ch_isspace(*p) || is_identifier_part(*p)))
-		p++;
-	    if (p < e && (*p == ';' || *p == ','))
-		return false;
-	    if (p < e && *p == '(')
-		paren_level++;
-	    else
+		p++;		/* '__dead' or '__unused' */
+
+	    if (p == e)		/* func(...) */
 		break;
+	    if (*p == ';')	/* func(...); */
+		return false;
+	    if (*p == ',')	/* double abs(), pi; */
+		return false;
+	    if (*p == '(')	/* func(...) __attribute__((...)) */
+		paren_level++;	/* func(...) __printflike(...) */
+	    else
+		break;		/* func(...) { ... */
 	}
     }
 
