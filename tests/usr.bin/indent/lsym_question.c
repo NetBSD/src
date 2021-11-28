599828102ccd416b81f1616beceb839fe40fa382
rillig <rillig@NetBSD.org>
2021-11-28 16:20:13 +0000

    tests/indent: migrate token_question to lsym_question and extend it

--- tests/usr.bin/indent/lsym_question.c
+++ tests/usr.bin/indent/lsym_question.c
@@ -1,4 +1,4 @@
-/* $NetBSD: lsym_question.c,v 1.1 2021/11/18 21:19:19 rillig Exp $ */
+/* $NetBSD: lsym_question.c,v 1.2 2021/11/28 16:20:13 rillig Exp $ */
 /* $FreeBSD$ */
 
 /*
@@ -7,7 +7,62 @@
  */
 
 #indent input
-// TODO: add input
+const char *result = cond ? "then" : "else";
+
+const char *multi = cond1 ? "cond1" : cond2 ? "cond2" : cond3 ? "cond3" : "";
+#indent end
+
+#indent run-equals-input -di0
+
+
+/*
+ * To make them easier to read, conditional expressions can be split into
+ * multiple lines.
+ */
+#indent input
+const char *separate_lines = cond
+	? "then"
+	: "else";
+#indent end
+
+#indent run -di0
+const char *separate_lines = cond
+// $ XXX: Continuation lines in expressions should be indented, even in column 1.
+? "then"
+: "else";
+#indent end
+
+
+/*
+ * In functions, conditional expressions are indented as intended.
+ */
+#indent input
+void
+function(void)
+{
+	return cond
+		? "then"
+		: "else";
+}
 #indent end
 
 #indent run-equals-input
+
+
+/*
+ * In functions, conditional expressions are indented as intended.
+ */
+#indent input
+void
+function(void)
+{
+	const char *branch = cond
+	// $ TODO: Indent these continuation lines as they are part of the
+	// $ TODO: initializer expression, not of the declarator part to the
+	// $ TODO: left of the '='.
+	? "then"
+	: "else";
+}
+#indent end
+
+#indent run-equals-input -di0
