58a626dd05252d1b96d5c9eeadb8ad54d9c14293
rillig <rillig@NetBSD.org>
2021-11-28 14:49:28 +0000

    tests/indent: move test for ':' to lsym_token.c

--- tests/usr.bin/indent/lsym_colon.c
+++ tests/usr.bin/indent/lsym_colon.c
@@ -1,4 +1,4 @@
-/* $NetBSD: lsym_colon.c,v 1.1 2021/11/18 21:19:19 rillig Exp $ */
+/* $NetBSD: lsym_colon.c,v 1.2 2021/11/28 14:49:28 rillig Exp $ */
 /* $FreeBSD$ */
 
 /*
@@ -11,10 +11,96 @@
  * As part of the conditional operator '?:'.
  *
  * In the declaration of a struct member that is a bit-field.
+ *
+ * See also:
+ *	label.c
+ *	lsym_question.c
  */
 
+/*
+ * The ':' marks a label that can be used in a 'goto' statement.
+ */
 #indent input
-// TODO: add input
+void endless(void)
+{
+label1:
+goto label2;
+
+    if (true)if (true)if (true)if (true)label2 :goto label1;
+}
+#indent end
+
+#indent run
+void
+endless(void)
+{
+label1:
+	goto label2;
+
+	if (true)
+		if (true)
+			if (true)
+				if (true)
+			label2:		goto label1;
+}
+#indent end
+
+
+/*
+ * The ':' is used in a 'switch' statement, after a 'case' label or a
+ * 'default' label.
+ */
+#indent input
+void
+example(void)
+{
+	switch (expr) {
+	case 'x':
+		return;
+	default:
+		return;
+	}
+}
 #indent end
 
 #indent run-equals-input
+
+
+/*
+ * The ':' is used as part of the conditional operator '?:'.
+ */
+#indent input
+int constant_expression = true?4:12345;
+#indent end
+
+#indent run
+int		constant_expression = true ? 4 : 12345;
+#indent end
+
+
+/*
+ * The ':' is used in the declaration of a struct member that is a bit-field.
+ */
+#indent input
+struct bit_field {
+	bool flag:1;
+	int maybe_signed : 4;
+	signed int definitely_signed:3;
+	signed int : 0;/* padding */
+	unsigned int definitely_unsigned:3;
+	unsigned int:0;/* padding */
+};
+#indent end
+
+#indent run
+struct bit_field {
+	bool		flag:1;
+	int		maybe_signed:4;
+	signed int	definitely_signed:3;
+/* $ XXX: Placing the colon directly at the type looks inconsistent. */
+	signed int:	0;	/* padding */
+	unsigned int	definitely_unsigned:3;
+/* $ XXX: Placing the colon directly at the type looks inconsistent. */
+	unsigned int:	0;	/* padding */
+};
+#indent end
