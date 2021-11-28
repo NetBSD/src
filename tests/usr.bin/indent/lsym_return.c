eac32271c18f833951de695f47b7bc3867a54d6b
rillig <rillig@NetBSD.org>
2021-11-28 16:31:39 +0000

    tests/indent: test variants of the 'return' statement

--- tests/usr.bin/indent/lsym_return.c
+++ tests/usr.bin/indent/lsym_return.c
@@ -1,4 +1,4 @@
-/* $NetBSD: lsym_return.c,v 1.1 2021/11/18 21:19:19 rillig Exp $ */
+/* $NetBSD: lsym_return.c,v 1.2 2021/11/28 16:31:39 rillig Exp $ */
 /* $FreeBSD$ */
 
 /*
@@ -6,8 +6,66 @@
  * starts a 'return' statement for leaving the execution of a function.
  */
 
+/*
+ * Return statements having a single-line expression are simple to format.
+ * Since 'return' is not a function name, there is a space between the
+ * 'return' and the '('.
+ */
 #indent input
-// TODO: add input
+void
+function(bool cond)
+{
+	if (cond)
+		return;
+}
+
+int
+calculate(int a, int b)
+{
+	return a;
+	return (b);
+	return (((a))) + b;
+	return calculate(b, a);
+}
 #indent end
 
 #indent run-equals-input
+
+
+/*
+ * Returning complex expressions may spread the expression over several lines.
+ * The exact formatting depends on the option '-lp'.
+ */
+#indent input
+int
+multi_line(int a)
+{
+	return calculate(3,
+			 4);
+	return calculate(
+			 3,
+			 4);
+	return calculate(
+			 3,
+			 4
+		);
+}
+#indent end
+
+#indent run-equals-input
+
+#indent run -nlp
+int
+multi_line(int a)
+{
+	return calculate(3,
+		4);
+	return calculate(
+		3,
+		4);
+	return calculate(
+		3,
+		4
+		);
+}
+#indent end
