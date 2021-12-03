84590f977b8c00f5ea4d8f48dff782977dce7a37
skrll <skrll@NetBSD.org>
2021-12-03 08:02:52 +0000

    Add a comment and simplify the code ever so slightly.

--- sys/dev/tprof/tprof_armv8.c
+++ sys/dev/tprof/tprof_armv8.c
@@ -1,4 +1,4 @@
-/* $NetBSD: tprof_armv8.c,v 1.11 2021/12/03 08:00:13 skrll Exp $ */
+/* $NetBSD: tprof_armv8.c,v 1.12 2021/12/03 08:02:52 skrll Exp $ */
 
 /*-
  * Copyright (c) 2018 Jared McNeill <jmcneill@invisible.ca>
@@ -27,7 +27,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: tprof_armv8.c,v 1.11 2021/12/03 08:00:13 skrll Exp $");
+__KERNEL_RCSID(0, "$NetBSD: tprof_armv8.c,v 1.12 2021/12/03 08:02:52 skrll Exp $");
 
 #include <sys/param.h>
 #include <sys/bus.h>
@@ -57,6 +57,7 @@ armv8_pmu_event_implemented(uint16_t event)
 	eid[0] = reg_pmceid0_el0_read();
 	eid[1] = reg_pmceid1_el0_read();
 
+	/* The low 32bits of PMCEID[01]_EL0 contain the commmon events 0 to n */
 	const u_int idx = event / 32;
 	const u_int bit = event % 32;
 
@@ -222,9 +223,7 @@ armv8_pmu_init_cpu(void *arg1, void *arg2)
 int
 armv8_pmu_init(void)
 {
-	uint64_t xc;
-
-	xc = xc_broadcast(0, armv8_pmu_init_cpu, NULL, NULL);
+	uint64_t xc = xc_broadcast(0, armv8_pmu_init_cpu, NULL, NULL);
 	xc_wait(xc);
 
 	return tprof_backend_register("tprof_armv8", &tprof_armv8_pmu_ops,
