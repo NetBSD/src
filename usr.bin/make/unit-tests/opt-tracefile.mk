7a9e0f2cd571d00f97be450bb7fb7d415b3c5f12
rillig <rillig@NetBSD.org>
2021-12-06 22:35:20 +0000

    tests/make: clean up after testing the option '-T'

--- usr.bin/make/unit-tests/opt-tracefile.mk
+++ usr.bin/make/unit-tests/opt-tracefile.mk
@@ -1,4 +1,4 @@
-# $NetBSD: opt-tracefile.mk,v 1.4 2021/12/04 07:23:43 rillig Exp $
+# $NetBSD: opt-tracefile.mk,v 1.5 2021/12/06 22:35:20 rillig Exp $
 #
 # Tests for the command line option '-T', which in jobs mode appends a trace
 # record to a trace log whenever a job is started or completed.
@@ -8,6 +8,7 @@ all: .PHONY
 	@${MAKE} -f ${MAKEFILE} -j1 -Topt-tracefile.log trace
 	# Remove timestamps, process IDs and directory paths.
 	@awk '{ print $$2, $$3 }' opt-tracefile.log
+	@rm opt-tracefile.log
 
 trace dependency1 dependency2: .PHONY
 	@echo 'Making ${.TARGET} from ${.ALLSRC:S,^$,<nothing>,W}.'
