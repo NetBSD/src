7553d5aecc915d475627cc54cd791a34fd4895ed
msaitoh <msaitoh@NetBSD.org>
2021-12-10 11:22:41 +0000

    Remove debug error message.

--- sys/dev/pci/ixgbe/ixgbe_common.c
+++ sys/dev/pci/ixgbe/ixgbe_common.c
@@ -1,4 +1,4 @@
-/* $NetBSD: ixgbe_common.c,v 1.35 2021/12/10 11:20:13 msaitoh Exp $ */
+/* $NetBSD: ixgbe_common.c,v 1.36 2021/12/10 11:22:41 msaitoh Exp $ */
 
 /******************************************************************************
   SPDX-License-Identifier: BSD-3-Clause
@@ -36,7 +36,7 @@
 /*$FreeBSD: head/sys/dev/ixgbe/ixgbe_common.c 331224 2018-03-19 20:55:05Z erj $*/
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: ixgbe_common.c,v 1.35 2021/12/10 11:20:13 msaitoh Exp $");
+__KERNEL_RCSID(0, "$NetBSD: ixgbe_common.c,v 1.36 2021/12/10 11:22:41 msaitoh Exp $");
 
 #include "ixgbe_common.h"
 #include "ixgbe_phy.h"
@@ -224,11 +224,6 @@ bool ixgbe_device_supports_autoneg_fc(struct ixgbe_hw *hw)
 	default:
 		break;
 	}
-
-	if (!supported)
-		ERROR_REPORT2(IXGBE_ERROR_UNSUPPORTED,
-			      "Device %x does not support flow control autoneg",
-			      hw->device_id);
 
 	return supported;
 }
