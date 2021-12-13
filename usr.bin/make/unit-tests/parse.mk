d37de80bd6b8ada432163653f9f029adde14f26b
rillig <rillig@NetBSD.org>
2021-12-13 23:38:54 +0000

    tests/make: extend tests for parsing makefiles

---
+++ usr.bin/make/unit-tests/parse.mk
@@ -0,0 +1,7 @@
+# $NetBSD: parse.mk,v 1.1 2021/12/13 23:38:54 rillig Exp $
+#
+# Test those parts of the parsing that do not belong in any of the other
+# categories.
+
+# expect+1: Makefile appears to contain unresolved CVS/RCS/??? merge conflicts
+<<<<<< old
