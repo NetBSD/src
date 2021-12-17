30b9078126f92cfa6e92c5c44c819ee1c8f3b9d1
rillig <rillig@NetBSD.org>
2021-12-17 00:05:24 +0000

    lint: in GCC mode, declare alloca and variants
    The prototype declarations define the correct parameter types of these
    functions so that they are no longer subject to the default argument
    promotions (C11 6.5.2.2p6).
    
    The GCC builtins are only recognized in GCC mode (-g).

--- usr.bin/xlint/lint1/tree.c
+++ usr.bin/xlint/lint1/tree.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: tree.c,v 1.399 2021/12/16 23:46:21 rillig Exp $	*/
+/*	$NetBSD: tree.c,v 1.400 2021/12/17 00:05:24 rillig Exp $	*/
 
 /*
  * Copyright (c) 1994, 1995 Jochen Pohl
@@ -37,7 +37,7 @@
 
 #include <sys/cdefs.h>
 #if defined(__RCSID) && !defined(lint)
-__RCSID("$NetBSD: tree.c,v 1.399 2021/12/16 23:46:21 rillig Exp $");
+__RCSID("$NetBSD: tree.c,v 1.400 2021/12/17 00:05:24 rillig Exp $");
 #endif
 
 #include <float.h>
@@ -203,6 +203,7 @@ is_compiler_builtin(const char *name)
 	if (gflag) {
 		if (strncmp(name, "__atomic_", 9) == 0 ||
 		    strncmp(name, "__builtin_", 10) == 0 ||
+		    strcmp(name, "alloca") == 0 ||
 		    /* obsolete but still in use, as of 2021 */
 		    strncmp(name, "__sync_", 7) == 0)
 			return true;
@@ -234,14 +235,6 @@ is_gcc_bool_builtin(const char *name)
 		str_endswith(name, "_overflow_p"));
 }
 
-/* https://gcc.gnu.org/onlinedocs/gcc/Other-Builtins.html */
-static bool
-is_gcc_void_pointer_builtin(const char *name)
-{
-	return strcmp(name, "__builtin_alloca") == 0 ||
-	       strncmp(name, "__builtin_alloca_", 17) == 0;
-}
-
 static void
 build_name_call(sym_t *sym)
 {
@@ -252,11 +245,8 @@ build_name_call(sym_t *sym)
 		 * they are regular functions compatible with
 		 * non-prototype calling conventions.
 		 */
-
-		if (is_gcc_bool_builtin(sym->s_name))
+		if (gflag && is_gcc_bool_builtin(sym->s_name))
 			sym->s_type = gettyp(BOOL);
-		else if (is_gcc_void_pointer_builtin(sym->s_name))
-			sym->s_type = derive_type(gettyp(VOID), PTR);
 
 	} else if (Sflag) {
 		/* function '%s' implicitly declared to return int */
