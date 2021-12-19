0b1f7fd38df69793c6670ff9615c899653458edc
riastradh <riastradh@NetBSD.org>
2021-12-19 00:27:17 +0000

    Rename fence -> dma_fence, step 3 of 3: code.

--- sys/external/bsd/drm2/include/linux/dma-fence.h
+++ sys/external/bsd/drm2/include/linux/dma-fence.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: dma-fence.h,v 1.1 2021/12/19 00:27:01 riastradh Exp $	*/
+/*	$NetBSD: dma-fence.h,v 1.2 2021/12/19 00:27:17 riastradh Exp $	*/
 
 /*-
  * Copyright (c) 2018 The NetBSD Foundation, Inc.
@@ -29,8 +29,8 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifndef	_LINUX_FENCE_H_
-#define	_LINUX_FENCE_H_
+#ifndef	_LINUX_DMA_FENCE_H_
+#define	_LINUX_DMA_FENCE_H_
 
 #include <sys/types.h>
 #include <sys/condvar.h>
@@ -41,98 +41,99 @@
 #include <linux/rcupdate.h>
 #include <linux/spinlock.h>
 
-struct fence_cb;
+struct dma_fence_cb;
 
-struct fence {
-	struct kref		refcount;
-	spinlock_t		*lock;
-	volatile unsigned long	flags;
-	unsigned		context;
-	unsigned		seqno;
-	const struct fence_ops	*ops;
+struct dma_fence {
+	struct kref			refcount;
+	spinlock_t			*lock;
+	volatile unsigned long		flags;
+	unsigned			context;
+	unsigned			seqno;
+	const struct dma_fence_ops	*ops;
 
-	TAILQ_HEAD(, fence_cb)	f_callbacks;
-	kcondvar_t		f_cv;
-	struct rcu_head		f_rcu;
+	TAILQ_HEAD(, dma_fence_cb)	f_callbacks;
+	kcondvar_t			f_cv;
+	struct rcu_head			f_rcu;
 };
 
-#define	FENCE_FLAG_ENABLE_SIGNAL_BIT	0
-#define	FENCE_FLAG_SIGNALED_BIT		1
-#define	FENCE_FLAG_USER_BITS		2
+#define	DMA_FENCE_FLAG_ENABLE_SIGNAL_BIT	0
+#define	DMA_FENCE_FLAG_SIGNALED_BIT		1
+#define	DMA_FENCE_FLAG_USER_BITS		2
 
-struct fence_ops {
-	const char	*(*get_driver_name)(struct fence *);
-	const char	*(*get_timeline_name)(struct fence *);
-	bool		(*enable_signaling)(struct fence *);
-	bool		(*signaled)(struct fence *);
-	long		(*wait)(struct fence *, bool, long);
-	void		(*release)(struct fence *);
+struct dma_fence_ops {
+	const char	*(*get_driver_name)(struct dma_fence *);
+	const char	*(*get_timeline_name)(struct dma_fence *);
+	bool		(*enable_signaling)(struct dma_fence *);
+	bool		(*signaled)(struct dma_fence *);
+	long		(*wait)(struct dma_fence *, bool, long);
+	void		(*release)(struct dma_fence *);
 };
 
-typedef void (*fence_func_t)(struct fence *, struct fence_cb *);
+typedef void (*dma_fence_func_t)(struct dma_fence *, struct dma_fence_cb *);
 
-struct fence_cb {
-	fence_func_t		fcb_func;
-	TAILQ_ENTRY(fence_cb)	fcb_entry;
-	bool			fcb_onqueue;
+struct dma_fence_cb {
+	dma_fence_func_t		fcb_func;
+	TAILQ_ENTRY(dma_fence_cb)	fcb_entry;
+	bool				fcb_onqueue;
 };
 
-#define	fence_add_callback	linux_fence_add_callback
-#define	fence_context_alloc	linux_fence_context_alloc
-#define	fence_default_wait	linux_fence_default_wait
-#define	fence_destroy		linux_fence_destroy
-#define	fence_enable_sw_signaling linux_fence_enable_sw_signaling
-#define	fence_free		linux_fence_free
-#define	fence_get		linux_fence_get
-#define	fence_get_rcu		linux_fence_get_rcu
-#define	fence_init		linux_fence_init
-#define	fence_is_later		linux_fence_is_later
-#define	fence_is_signaled	linux_fence_is_signaled
-#define	fence_is_signaled_locked linux_fence_is_signaled_locked
-#define	fence_put		linux_fence_put
-#define	fence_remove_callback	linux_fence_remove_callback
-#define	fence_signal		linux_fence_signal
-#define	fence_signal_locked	linux_fence_signal_locked
-#define	fence_wait		linux_fence_wait
-#define	fence_wait_any_timeout	linux_fence_wait_any_timeout
-#define	fence_wait_timeout	linux_fence_wait_timeout
+#define	dma_fence_add_callback		linux_dma_fence_add_callback
+#define	dma_fence_context_alloc		linux_dma_fence_context_alloc
+#define	dma_fence_default_wait		linux_dma_fence_default_wait
+#define	dma_fence_destroy		linux_dma_fence_destroy
+#define	dma_fence_enable_sw_signaling	linux_dma_fence_enable_sw_signaling
+#define	dma_fence_free			linux_dma_fence_free
+#define	dma_fence_get			linux_dma_fence_get
+#define	dma_fence_get_rcu		linux_dma_fence_get_rcu
+#define	dma_fence_init			linux_dma_fence_init
+#define	dma_fence_is_later		linux_dma_fence_is_later
+#define	dma_fence_is_signaled		linux_dma_fence_is_signaled
+#define	dma_fence_is_signaled_locked	linux_dma_fence_is_signaled_locked
+#define	dma_fence_put			linux_dma_fence_put
+#define	dma_fence_remove_callback	linux_dma_fence_remove_callback
+#define	dma_fence_signal		linux_dma_fence_signal
+#define	dma_fence_signal_locked		linux_dma_fence_signal_locked
+#define	dma_fence_wait			linux_dma_fence_wait
+#define	dma_fence_wait_any_timeout	linux_dma_fence_wait_any_timeout
+#define	dma_fence_wait_timeout		linux_dma_fence_wait_timeout
 
-extern int	linux_fence_trace;
+extern int	linux_dma_fence_trace;
 
-void	fence_init(struct fence *, const struct fence_ops *, spinlock_t *,
-	    unsigned, unsigned);
-void	fence_destroy(struct fence *);
-void	fence_free(struct fence *);
+void	dma_fence_init(struct dma_fence *, const struct dma_fence_ops *,
+	    spinlock_t *, unsigned, unsigned);
+void	dma_fence_destroy(struct dma_fence *);
+void	dma_fence_free(struct dma_fence *);
 
 unsigned
-	fence_context_alloc(unsigned);
-bool	fence_is_later(struct fence *, struct fence *);
+	dma_fence_context_alloc(unsigned);
+bool	dma_fence_is_later(struct dma_fence *, struct dma_fence *);
 
-struct fence *
-	fence_get(struct fence *);
-struct fence *
-	fence_get_rcu(struct fence *);
-void	fence_put(struct fence *);
+struct dma_fence *
+	dma_fence_get(struct dma_fence *);
+struct dma_fence *
+	dma_fence_get_rcu(struct dma_fence *);
+void	dma_fence_put(struct dma_fence *);
 
-int	fence_add_callback(struct fence *, struct fence_cb *, fence_func_t);
-bool	fence_remove_callback(struct fence *, struct fence_cb *);
-void	fence_enable_sw_signaling(struct fence *);
+int	dma_fence_add_callback(struct dma_fence *, struct dma_fence_cb *,
+	    dma_fence_func_t);
+bool	dma_fence_remove_callback(struct dma_fence *, struct dma_fence_cb *);
+void	dma_fence_enable_sw_signaling(struct dma_fence *);
 
-bool	fence_is_signaled(struct fence *);
-bool	fence_is_signaled_locked(struct fence *);
-int	fence_signal(struct fence *);
-int	fence_signal_locked(struct fence *);
-long	fence_default_wait(struct fence *, bool, long);
-long	fence_wait(struct fence *, bool);
-long	fence_wait_any_timeout(struct fence **, uint32_t, bool, long);
-long	fence_wait_timeout(struct fence *, bool, long);
+bool	dma_fence_is_signaled(struct dma_fence *);
+bool	dma_fence_is_signaled_locked(struct dma_fence *);
+int	dma_fence_signal(struct dma_fence *);
+int	dma_fence_signal_locked(struct dma_fence *);
+long	dma_fence_default_wait(struct dma_fence *, bool, long);
+long	dma_fence_wait(struct dma_fence *, bool);
+long	dma_fence_wait_any_timeout(struct dma_fence **, uint32_t, bool, long);
+long	dma_fence_wait_timeout(struct dma_fence *, bool, long);
 
 static inline void __printflike(2, 3)
-FENCE_TRACE(struct fence *f, const char *fmt, ...)
+DMA_FENCE_TRACE(struct dma_fence *f, const char *fmt, ...)
 {
 	va_list va;
 
-	if (__predict_false(linux_fence_trace)) {
+	if (__predict_false(linux_dma_fence_trace)) {
 		va_start(va, fmt);
 		printf("fence %u@%u: ", f->context, f->seqno);
 		vprintf(fmt, va);
@@ -140,4 +141,4 @@ FENCE_TRACE(struct fence *f, const char *fmt, ...)
 	}
 }
 
-#endif	/* _LINUX_FENCE_H_ */
+#endif	/* _LINUX_DMA_FENCE_H_ */
