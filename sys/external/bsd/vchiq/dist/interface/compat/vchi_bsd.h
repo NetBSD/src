63535b45b94dd9cd38130f17625a1870c8565693
riastradh <riastradh@NetBSD.org>
2021-12-19 11:26:04 +0000

    Don't redefine stuff that common now has
    Preferring common to avoid licensing mish-mash
    
    
    Author: Maya Rashish <maya@NetBSD.org>
    Committer: Taylor R Campbell <riastradh@NetBSD.org>

--- sys/external/bsd/vchiq/dist/interface/compat/vchi_bsd.h
+++ sys/external/bsd/vchiq/dist/interface/compat/vchi_bsd.h
@@ -173,47 +173,6 @@ void
 device_rlprintf(int pps, device_t dev, const char *fmt, ...)
 	__printflike(3, 4);
 
-#define might_sleep()
-
-#define WARN(condition, msg)				\
-({							\
-	int __ret_warn_on = !!(condition);		\
-	if (unlikely(__ret_warn_on))			\
-		printf((msg));				\
-	unlikely(__ret_warn_on);			\
-})
-
-
-
-#define WARN_ON(condition)				\
-({							\
-	int __ret_warn_on = !!(condition);		\
-	if (unlikely(__ret_warn_on))			\
-		printf("WARN_ON: " #condition "\n");	\
-	unlikely(__ret_warn_on);			\
-})
-
-#define WARN_ON_ONCE(condition) ({			\
-	static int __warned;				\
-	int __ret_warn_once = !!(condition);		\
-							\
-	if (unlikely(__ret_warn_once))			\
-		if (WARN_ON(!__warned))			\
-			__warned = 1;			\
-	unlikely(__ret_warn_once);			\
-})
-
-#define BUG_ON(cond)					\
-	do {						\
-		if (cond)				\
-			panic("BUG_ON: " #cond);	\
-	} while (0)
-
-#define BUG()						\
-	do {						\
-		panic("BUG: %s:%d", __FILE__, __LINE__);	\
-	} while (0)
-
 #define vchiq_static_assert(cond) CTASSERT(cond)
 
 /*
@@ -293,7 +252,6 @@ int fatal_signal_pending(VCHIQ_THREAD_T);
 #define __user
 
 #define	current			curlwp
-#define EXPORT_SYMBOL(x)
 #define PAGE_ALIGN(addr)	round_page(addr)
 
 typedef	void	irqreturn_t;
