73a282f0f4286184703725c98520ef60f9b8eba8
riastradh <riastradh@NetBSD.org>
2021-12-19 11:12:51 +0000

    i915: drm_waitqueue_t

--- sys/external/bsd/drm2/dist/drm/i915/gt/intel_reset_types.h
+++ sys/external/bsd/drm2/dist/drm/i915/gt/intel_reset_types.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: intel_reset_types.h,v 1.2 2021/12/18 23:45:30 riastradh Exp $	*/
+/*	$NetBSD: intel_reset_types.h,v 1.3 2021/12/19 11:12:51 riastradh Exp $	*/
 
 /* SPDX-License-Identifier: MIT */
 /*
@@ -11,6 +11,7 @@
 #include <linux/mutex.h>
 #include <linux/wait.h>
 #include <linux/srcu.h>
+#include <drm/drm_wait_netbsd.h> /* XXX */
 
 struct intel_reset {
 	/**
@@ -50,7 +51,11 @@ struct intel_reset {
 	 * Waitqueue to signal when the reset has completed. Used by clients
 	 * that wait for dev_priv->mm.wedged to settle.
 	 */
+#ifdef __NetBSD__
+	drm_waitqueue_t queue;
+#else
 	wait_queue_head_t queue;
+#endif
 
 	struct srcu_struct backoff_srcu;
 };
