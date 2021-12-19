be20f67e2cbac6d673537b8ae28470d600e5a9e5
riastradh <riastradh@NetBSD.org>
2021-12-19 01:48:38 +0000

    Side load <linux/types.h> here, because why not?

--- sys/external/bsd/drm2/include/linux/scatterlist.h
+++ sys/external/bsd/drm2/include/linux/scatterlist.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: scatterlist.h,v 1.1 2021/12/19 00:28:12 riastradh Exp $	*/
+/*	$NetBSD: scatterlist.h,v 1.2 2021/12/19 01:48:38 riastradh Exp $	*/
 
 /*-
  * Copyright (c) 2018 The NetBSD Foundation, Inc.
@@ -31,5 +31,7 @@
 
 #ifndef	_LINUX_SCATTERLIST_H_
 #define	_LINUX_SCATTERLIST_H_
+
+#include <linux/types.h>
 
 #endif	/* _LINUX_SCATTERLIST_H_ */
