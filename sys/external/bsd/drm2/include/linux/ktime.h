2cddb6fbe53f17ffefd66dc5604774ae06ff466c
riastradh <riastradh@NetBSD.org>
2021-12-19 01:16:28 +0000

    struct timespec serves as a perfectly adequate struct timespec64.

--- sys/external/bsd/drm2/include/linux/ktime.h
+++ sys/external/bsd/drm2/include/linux/ktime.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: ktime.h,v 1.11 2021/12/19 01:15:35 riastradh Exp $	*/
+/*	$NetBSD: ktime.h,v 1.12 2021/12/19 01:16:28 riastradh Exp $	*/
 
 /*-
  * Copyright (c) 2013 The NetBSD Foundation, Inc.
@@ -83,6 +83,7 @@ ktime_to_timespec(ktime_t kt)
 {
 	return ns_to_timespec(ktime_to_ns(kt));
 }
+#define	ktime_to_timespec64	ktime_to_timespec
 
 static inline struct timeval
 ktime_to_timeval(ktime_t kt)
@@ -96,6 +97,7 @@ timespec_to_ktime(struct timespec ts)
 	/* XXX Silently truncate?  */
 	return ns_to_ktime(1000000000*(int64_t)ts.tv_sec + ts.tv_nsec);
 }
+#define	timespec64_to_ktime	timespec_to_ktime
 
 static inline ktime_t
 ktime_get(void)
