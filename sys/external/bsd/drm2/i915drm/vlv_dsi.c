c62c2a0f379d45ab2c52a31528e3865bfc795a86
riastradh <riastradh@NetBSD.org>
2021-12-19 00:57:42 +0000

    Rename intel_dsi.c -> vlv_dsi.c

--- sys/external/bsd/drm2/i915drm/vlv_dsi.c
+++ sys/external/bsd/drm2/i915drm/vlv_dsi.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: intel_dsi.c,v 1.2 2018/08/27 15:09:35 riastradh Exp $	*/
+/*	$NetBSD: vlv_dsi.c,v 1.1 2021/12/19 00:57:42 riastradh Exp $	*/
 
 /*-
  * Copyright (c) 2018 The NetBSD Foundation, Inc.
@@ -30,11 +30,11 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: intel_dsi.c,v 1.2 2018/08/27 15:09:35 riastradh Exp $");
+__KERNEL_RCSID(0, "$NetBSD: vlv_dsi.c,v 1.1 2021/12/19 00:57:42 riastradh Exp $");
 
 #include "intel_drv.h"
 
 void
-intel_dsi_init(struct drm_device *dev)
+vlv_dsi_init(struct drm_i915_private *dev_priv)
 {
 }
