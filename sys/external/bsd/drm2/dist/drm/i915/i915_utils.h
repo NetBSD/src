a8d9e3e876c6dc7c0dea19547f978c9fbe332254
riastradh <riastradh@NetBSD.org>
2021-12-19 11:02:22 +0000

    (XXX unsure) attempt to match linux with additional parameter OP
    Author: Maya Rashish <maya@NetBSD.org>
    Committer: Taylor R Campbell <riastradh@NetBSD.org>

--- sys/external/bsd/drm2/dist/drm/i915/i915_utils.h
+++ sys/external/bsd/drm2/dist/drm/i915/i915_utils.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: i915_utils.h,v 1.3 2021/12/19 09:59:13 riastradh Exp $	*/
+/*	$NetBSD: i915_utils.h,v 1.4 2021/12/19 11:02:22 riastradh Exp $	*/
 
 /*
  * Copyright © 2016 Intel Corporation
@@ -299,11 +299,14 @@ wait_remaining_ms_from_jiffies(unsigned long timestamp_jiffies, int to_wait_ms)
  * check the condition before the timeout.
  */
 #ifdef __NetBSD__
-#define _wait_for(COND, MS, Wmin, Wmax) ({ \
+#define __wait_for(OP, COND, MS, Wmin, Wmax) ({ \
 	int ret__ = 0;							\
 	if (cold) {							\
 		int ms = (MS);						\
 		while (!(COND)) {					\
+			OP;						\
+			/* Guarantee COND check prior to timeout */	\
+			barrier();					\
 			if (--ms < 0) {					\
 				DELAY(1000);				\
 				if (!(COND))				\
@@ -315,6 +318,9 @@ wait_remaining_ms_from_jiffies(unsigned long timestamp_jiffies, int to_wait_ms)
 	} else {							\
 		unsigned long timeout__ = jiffies + msecs_to_jiffies(MS); \
 		while (!(COND)) {					\
+			OP;						\
+			/* Guarantee COND check prior to timeout */	\
+			barrier();					\
 			if (time_after(jiffies, timeout__)) {		\
 				if (!(COND))				\
 					ret__ = -ETIMEDOUT;		\
