e133e8388b5cf6cb0323e123ede403d5a5a5db82
riastradh <riastradh@NetBSD.org>
2021-12-19 09:57:17 +0000

    More obvious part of TTM API changes:
    - ref/unref to get/put
    - some global TTM things are now global
    - More indirection to get vma_node
    
    
    Author: Maya Rashish <maya@NetBSD.org>

--- sys/external/bsd/drm2/dist/drm/ttm/ttm_bo.c
+++ sys/external/bsd/drm2/dist/drm/ttm/ttm_bo.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: ttm_bo.c,v 1.21 2021/12/18 23:45:44 riastradh Exp $	*/
+/*	$NetBSD: ttm_bo.c,v 1.22 2021/12/19 09:57:17 riastradh Exp $	*/
 
 /* SPDX-License-Identifier: GPL-2.0 OR MIT */
 /**************************************************************************
@@ -32,7 +32,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: ttm_bo.c,v 1.21 2021/12/18 23:45:44 riastradh Exp $");
+__KERNEL_RCSID(0, "$NetBSD: ttm_bo.c,v 1.22 2021/12/19 09:57:17 riastradh Exp $");
 
 #define pr_fmt(fmt) "[TTM] " fmt
 
@@ -681,7 +681,7 @@ static void ttm_bo_release(struct kref *kref)
 #endif
 	drm_vma_offset_remove(bdev->vma_manager, &bo->base.vma_node);
 #ifdef __NetBSD__
-	drm_vma_node_destroy(&bo->vma_node);
+	drm_vma_node_destroy(&bo->base.vma_node);
 #endif
 	ttm_mem_io_lock(man, false);
 	ttm_mem_io_free_vm(bo);
@@ -1357,7 +1357,7 @@ int ttm_bo_init_reserved(struct ttm_bo_device *bdev,
 		 */
 		dma_resv_init(&bo->base._resv);
 #ifdef __NetBSD__
-		drm_vma_node_init(&bo->vma_node);
+		drm_vma_node_init(&bo->base.vma_node);
 		uvm_obj_init(&bo->uvmobj, bdev->driver->ttm_uvm_ops, true, 1);
 #else
 		drm_vma_node_reset(&bo->base.vma_node);
@@ -1645,7 +1645,7 @@ static void ttm_bo_global_release(void)
 	ttm_mem_unregister_shrink(glob->mem_glob, &glob->shrink);
 	BUG_ON(glob->dummy_read_page != NULL);
 	spin_lock_destroy(&glob->lru_lock);
-	mutex_destroy(&glob->device_list_mutex);
+	mutex_destroy(&ttm_global_mutex);
 	kfree(glob);
 #else
 	kobject_del(&glob->kobj);
@@ -1663,6 +1663,7 @@ static int ttm_bo_global_init(void)
 	int ret = 0;
 	unsigned i;
 
+	mutex_init(&ttm_global_mutex);
 	mutex_lock(&ttm_global_mutex);
 	if (++ttm_bo_glob_use_count > 1)
 		goto out;
