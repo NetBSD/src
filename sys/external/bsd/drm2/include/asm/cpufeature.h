fb9d4d8409f627e8f883ad4ca7d0aaeca7ff0d05
riastradh <riastradh@NetBSD.org>
2021-12-19 01:18:39 +0000

    Define x86 static_cpu_has(X86_FEATURE_CLFLUSH), clflush.

--- sys/external/bsd/drm2/include/asm/cpufeature.h
+++ sys/external/bsd/drm2/include/asm/cpufeature.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: cpufeature.h,v 1.5 2020/09/05 07:45:44 maxv Exp $	*/
+/*	$NetBSD: cpufeature.h,v 1.6 2021/12/19 01:18:39 riastradh Exp $	*/
 
 /*-
  * Copyright (c) 2018 The NetBSD Foundation, Inc.
@@ -39,10 +39,29 @@
 #define	cpu_has_clflush	((cpu_info_primary.ci_feat_val[0] & CPUID_CLFSH) != 0)
 #define	cpu_has_pat	((cpu_info_primary.ci_feat_val[0] & CPUID_PAT) != 0)
 
+#define	X86_FEATURE_CLFLUSH	0
+
+static inline bool
+static_cpu_has(int feature)
+{
+	switch (feature) {
+	case X86_FEATURE_CLFLUSH:
+		return cpu_has_clflush;
+	default:
+		return false;
+	}
+}
+
 static inline size_t
 cache_line_size(void)
 {
 	return cpu_info_primary.ci_cflush_lsize;
+}
+
+static inline void
+clflush(const void *p)
+{
+	asm volatile ("clflush %0" : : "m" (*(const char *)p));
 }
 
 #endif	/* x86 */
