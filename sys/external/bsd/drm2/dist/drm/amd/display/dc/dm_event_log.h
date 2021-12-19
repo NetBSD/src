98a24c08c795c8c6cdeed5d4e4755ae7048079a3
riastradh <riastradh@NetBSD.org>
2021-12-19 11:23:26 +0000

    amdgpu: Mark unused variables as such.

--- sys/external/bsd/drm2/dist/drm/amd/display/dc/dm_event_log.h
+++ sys/external/bsd/drm2/dist/drm/amd/display/dc/dm_event_log.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: dm_event_log.h,v 1.2 2021/12/18 23:45:00 riastradh Exp $	*/
+/*	$NetBSD: dm_event_log.h,v 1.3 2021/12/19 11:23:26 riastradh Exp $	*/
 
 /*
  * Copyright 2018 Advanced Micro Devices, Inc.
@@ -33,9 +33,18 @@
 
 #define __DM_EVENT_LOG_H__
 
-#define EVENT_LOG_AUX_REQ(ddc, type, action, address, len, data)
-#define EVENT_LOG_AUX_REP(ddc, type, replyStatus, len, data)
-#define EVENT_LOG_CUST_MSG(tag, a, ...)
+enum event_log_type {
+	EVENT_LOG_AUX_ORIGIN_NATIVE,
+};
+
+#define EVENT_LOG_AUX_REQ(ddc, type, action, address, len, data)	      \
+	(void)((void)(ddc), (void)(type), (void)(action), (void)(address),    \
+	    (void)(len), (void)(data))
+#define EVENT_LOG_AUX_REP(ddc, type, replyStatus, len, data)		      \
+	(void)((void)(ddc), (void)(type), (void)(replyStatus), (void)(len),   \
+	    (void)(data))
+#define EVENT_LOG_CUST_MSG(tag, a, ...)					      \
+	(void)((void)(tag), (void)(a))
 
 #endif
 
