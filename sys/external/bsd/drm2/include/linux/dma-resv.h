4925e2e903c51ebf5ae7626820d5e032bd94ea85
riastradh <riastradh@NetBSD.org>
2021-12-19 01:54:50 +0000

    stub xarray.h and dma-resv.h
    Add some upstream side-loads and empty structs.
    
    
    Author: Maya Rashish <maya@NetBSD.org>

---
+++ sys/external/bsd/drm2/include/linux/dma-resv.h
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+/*
+ * Copyright © 2020 The NetBSD Foundation, Inc.
+ */
+
+#ifndef	_LINUX_DMA_RESV_H_
+#define	_LINUX_DMA_RESV_H_
+
+#include <linux/ww_mutex.h>
+#include <linux/dma-fence.h>
+
+struct dma_resv {};
+
+#endif	/* _LINUX_DMA_RESV_H_ */
---
+++ sys/external/bsd/drm2/include/linux/xarray.h
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+/*
+ * Copyright © 2020 The NetBSD Foundation, Inc.
+ */
+
+#ifndef	_LINUX_XARRAY_H_
+#define	_LINUX_XARRAY_H_
+
+struct xarray;
+
+#endif	/* _LINUX_XARRAY_H_ */
