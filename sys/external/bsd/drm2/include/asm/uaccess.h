c68d42e0e215556be832c8b11b98608b410e188b
riastradh <riastradh@NetBSD.org>
2021-12-19 11:24:14 +0000

    Update to new access_ok prototype
    also... this function should probably do something
    
    
    Author: Maya Rashish <maya@NetBSD.org>
    Committer: Taylor R Campbell <riastradh@NetBSD.org>

--- sys/external/bsd/drm2/include/asm/uaccess.h
+++ sys/external/bsd/drm2/include/asm/uaccess.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: uaccess.h,v 1.8 2021/12/19 01:59:41 riastradh Exp $	*/
+/*	$NetBSD: uaccess.h,v 1.9 2021/12/19 11:24:14 riastradh Exp $	*/
 
 /*-
  * Copyright (c) 2013 The NetBSD Foundation, Inc.
@@ -42,8 +42,7 @@
 #define	VERIFY_READ	0
 #define	VERIFY_WRITE	1
 static inline bool
-access_ok(int verify_op __unused, const void *uaddr __unused,
-    size_t nbytes __unused)
+access_ok(const void *uaddr __unused, size_t nbytes __unused)
 {
 	return true;
 }
