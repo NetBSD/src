d32d666de4beb0f54ea5a5811bf137e0ddf6693e
riastradh <riastradh@NetBSD.org>
2021-12-19 09:57:51 +0000

    Follow up with TTM API changes
    Author: Maya Rashish <maya@NetBSD.org>

--- sys/external/bsd/drm2/dist/drm/ttm/ttm_bo_util.c
+++ sys/external/bsd/drm2/dist/drm/ttm/ttm_bo_util.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: ttm_bo_util.c,v 1.23 2021/12/19 09:57:42 riastradh Exp $	*/
+/*	$NetBSD: ttm_bo_util.c,v 1.24 2021/12/19 09:57:51 riastradh Exp $	*/
 
 /* SPDX-License-Identifier: GPL-2.0 OR MIT */
 /**************************************************************************
@@ -32,7 +32,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: ttm_bo_util.c,v 1.23 2021/12/19 09:57:42 riastradh Exp $");
+__KERNEL_RCSID(0, "$NetBSD: ttm_bo_util.c,v 1.24 2021/12/19 09:57:51 riastradh Exp $");
 
 #include <drm/ttm/ttm_bo_driver.h>
 #include <drm/ttm/ttm_placement.h>
@@ -568,7 +568,7 @@ static int ttm_buffer_object_transfer(struct ttm_buffer_object *bo,
 	INIT_LIST_HEAD(&fbo->base.io_reserve_lru);
 	fbo->base.moving = NULL;
 #ifdef __NetBSD__
-	drm_vma_node_init(&fbo->vma_node);
+	drm_vma_node_init(&fbo->base.base.vma_node);
 	uvm_obj_init(&fbo->base.uvmobj, bo->bdev->driver->ttm_uvm_ops, true, 1);
 	rw_obj_hold(bo->uvmobj.vmobjlock);
 	uvm_obj_setlock(&fbo->base.uvmobj, bo->uvmobj.vmobjlock);
