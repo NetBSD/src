ffec59cdef5e0af5c31efe844fe351acc3b72164
riastradh <riastradh@NetBSD.org>
2021-12-19 12:36:41 +0000

    amdgpu: Fix indexing for 64-bit doorbells.

--- sys/external/bsd/drm2/dist/drm/amd/amdgpu/amdgpu_device.c
+++ sys/external/bsd/drm2/dist/drm/amd/amdgpu/amdgpu_device.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: amdgpu_device.c,v 1.11 2021/12/19 12:31:45 riastradh Exp $	*/
+/*	$NetBSD: amdgpu_device.c,v 1.12 2021/12/19 12:36:41 riastradh Exp $	*/
 
 /*
  * Copyright 2008 Advanced Micro Devices, Inc.
@@ -28,7 +28,7 @@
  *          Jerome Glisse
  */
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: amdgpu_device.c,v 1.11 2021/12/19 12:31:45 riastradh Exp $");
+__KERNEL_RCSID(0, "$NetBSD: amdgpu_device.c,v 1.12 2021/12/19 12:36:41 riastradh Exp $");
 
 #include <linux/power_supply.h>
 #include <linux/kthread.h>
@@ -484,7 +484,7 @@ u64 amdgpu_mm_rdoorbell64(struct amdgpu_device *adev, u32 index)
 	if (index < adev->doorbell.num_doorbells) {
 #ifdef __NetBSD__
 		return bus_space_read_8(adev->doorbell.bst, adev->doorbell.bsh,
-		    4*index);
+		    8*index);
 #else
 		return atomic64_read((atomic64_t *)(adev->doorbell.ptr + index));
 #endif
@@ -509,7 +509,7 @@ void amdgpu_mm_wdoorbell64(struct amdgpu_device *adev, u32 index, u64 v)
 	if (index < adev->doorbell.num_doorbells) {
 #ifdef __NetBSD__
 		bus_space_write_8(adev->doorbell.bst, adev->doorbell.bsh,
-		    4*index, v);
+		    8*index, v);
 #else
 		atomic64_set((atomic64_t *)(adev->doorbell.ptr + index), v);
 #endif
