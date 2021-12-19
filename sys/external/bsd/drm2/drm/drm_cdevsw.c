b56292034fe815189566cc38c60c21c02281e910
riastradh <riastradh@NetBSD.org>
2021-12-19 10:35:59 +0000

    drm_agpsupport.h for drm_legacy_agp_clear.

--- sys/external/bsd/drm2/drm/drm_cdevsw.c
+++ sys/external/bsd/drm2/drm/drm_cdevsw.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: drm_cdevsw.c,v 1.24 2021/12/19 10:32:59 riastradh Exp $	*/
+/*	$NetBSD: drm_cdevsw.c,v 1.25 2021/12/19 10:35:59 riastradh Exp $	*/
 
 /*-
  * Copyright (c) 2013 The NetBSD Foundation, Inc.
@@ -30,7 +30,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: drm_cdevsw.c,v 1.24 2021/12/19 10:32:59 riastradh Exp $");
+__KERNEL_RCSID(0, "$NetBSD: drm_cdevsw.c,v 1.25 2021/12/19 10:35:59 riastradh Exp $");
 
 #include <sys/param.h>
 #include <sys/types.h>
@@ -57,6 +57,7 @@ __KERNEL_RCSID(0, "$NetBSD: drm_cdevsw.c,v 1.24 2021/12/19 10:32:59 riastradh Ex
 
 #include <linux/pm.h>
 
+#include <drm/drm_agpsupport.h>
 #include <drm/drm_device.h>
 #include <drm/drm_drv.h>
 #include <drm/drm_file.h>
