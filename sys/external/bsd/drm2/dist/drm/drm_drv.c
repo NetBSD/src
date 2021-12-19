df9751b2cda3aa4e2aadc13d1ccc88de381dad44
riastradh <riastradh@NetBSD.org>
2021-12-19 11:01:29 +0000

    Initialize drm_core_init_complete in drm_module.c
    Linux achieves this with a module_init(drm_core_init);
    
    
    Author: Maya Rashish <maya@NetBSD.org>
    Committer: Taylor R Campbell <riastradh@NetBSD.org>

--- sys/external/bsd/drm2/dist/drm/drm_drv.c
+++ sys/external/bsd/drm2/dist/drm/drm_drv.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: drm_drv.c,v 1.19 2021/12/19 09:45:25 riastradh Exp $	*/
+/*	$NetBSD: drm_drv.c,v 1.20 2021/12/19 11:01:29 riastradh Exp $	*/
 
 /*
  * Created: Fri Jan 19 10:48:35 2001 by faith@acm.org
@@ -29,7 +29,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: drm_drv.c,v 1.19 2021/12/19 09:45:25 riastradh Exp $");
+__KERNEL_RCSID(0, "$NetBSD: drm_drv.c,v 1.20 2021/12/19 11:01:29 riastradh Exp $");
 
 #include <linux/debugfs.h>
 #include <linux/fs.h>
@@ -72,7 +72,7 @@ static struct idr drm_minors_idr;
  * prefer to embed struct drm_device into their own device
  * structure and call drm_dev_init() themselves.
  */
-static bool drm_core_init_complete = false;
+bool drm_core_init_complete = false;
 
 #ifndef __NetBSD__
 static struct dentry *drm_debugfs_root;
