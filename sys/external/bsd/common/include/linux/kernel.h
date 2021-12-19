b112dc139f69cf90cd36c8ec99e17bf5a2eb329d
riastradh <riastradh@NetBSD.org>
2021-12-19 01:39:12 +0000

    __maybe_unused, typecheck

--- sys/external/bsd/common/include/linux/kernel.h
+++ sys/external/bsd/common/include/linux/kernel.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: kernel.h,v 1.35 2021/12/19 01:24:57 riastradh Exp $	*/
+/*	$NetBSD: kernel.h,v 1.36 2021/12/19 01:39:12 riastradh Exp $	*/
 
 /*-
  * Copyright (c) 2013 The NetBSD Foundation, Inc.
@@ -73,6 +73,7 @@
 #define	__must_check	/* nothing */
 #endif
 #define	__always_unused	__unused
+#define	__maybe_unused	__unused
 #define	noinline	__noinline
 #define	__deprecated	/* nothing */
 
@@ -87,6 +88,8 @@
  * Linux does `x = x', which is bollocks.
  */
 #define	uninitialized_var(x)	x = 0
+
+#define	typecheck(T, X)	({(1 + 0*sizeof((T *)0 - &(X)));})
 
 /* XXX These will multiply evaluate their arguments.  */
 #define	min(X, Y)	MIN(X, Y)
