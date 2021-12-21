cf22dfa87e4bfe1b5f4096dd0c3e2f35c1812d4e
knakahara <knakahara@NetBSD.org>
2021-12-21 04:09:32 +0000

    Fix net.*.rps_hash=toeplitz-othercpus on one CPU systems.

--- sys/net/pktqueue.c
+++ sys/net/pktqueue.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: pktqueue.c,v 1.15 2021/12/15 07:47:22 knakahara Exp $	*/
+/*	$NetBSD: pktqueue.c,v 1.16 2021/12/21 04:09:32 knakahara Exp $	*/
 
 /*-
  * Copyright (c) 2014 The NetBSD Foundation, Inc.
@@ -36,7 +36,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: pktqueue.c,v 1.15 2021/12/15 07:47:22 knakahara Exp $");
+__KERNEL_RCSID(0, "$NetBSD: pktqueue.c,v 1.16 2021/12/21 04:09:32 knakahara Exp $");
 
 #ifdef _KERNEL_OPT
 #include "opt_net_mpsafe.h"
@@ -283,6 +283,9 @@ static uint32_t
 pktq_rps_hash_toeplitz_othercpus(const struct mbuf *m)
 {
 	uint32_t hash;
+
+	if (ncpu == 1)
+		return 0;
 
 	hash = pktq_rps_hash_toeplitz(m);
 	hash %= ncpu - 1;
