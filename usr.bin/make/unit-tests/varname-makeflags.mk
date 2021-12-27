e35fc94a39713e6b38ee35864db9db5ce93a65d0
rillig <rillig@NetBSD.org>
2021-12-27 20:17:35 +0000

    tests/make: test MAKEFLAGS in POSIX mode
    This test covers the function 'explode' in main.c, the case in which the
    flags are actually expanded.

--- usr.bin/make/unit-tests/varname-makeflags.mk
+++ usr.bin/make/unit-tests/varname-makeflags.mk
@@ -1,4 +1,4 @@
-# $NetBSD: varname-makeflags.mk,v 1.3 2020/12/01 20:37:30 rillig Exp $
+# $NetBSD: varname-makeflags.mk,v 1.4 2021/12/27 20:17:35 rillig Exp $
 #
 # Tests for the special MAKEFLAGS variable, which is basically just a normal
 # environment variable.  It is closely related to .MAKEFLAGS but captures the
@@ -22,5 +22,23 @@
 .if ${.MAKEFLAGS} != " -r -k -d 0"
 .  error
 .endif
+
+
+# In POSIX mode, the environment variable MAKEFLAGS can contain letters only,
+# for compatibility.  These letters are exploded to form regular options.
+OUTPUT!=	env MAKEFLAGS=ikrs ${MAKE} -f /dev/null -v .MAKEFLAGS
+.if ${OUTPUT} != " -i -k -r -s -V .MAKEFLAGS"
+.  error
+.endif
+
+# As soon as there is a single non-alphabetic character in the environment
+# variable MAKEFLAGS, it is no longer split.  In this example, the word
+# "d0ikrs" is treated as a target, but the option '-v' prevents any targets
+# from being built.
+OUTPUT!=	env MAKEFLAGS=d0ikrs ${MAKE} -f /dev/null -v .MAKEFLAGS
+.if ${OUTPUT} != " -V .MAKEFLAGS"
+.  error ${OUTPUT}
+.endif
+
 
 all:
