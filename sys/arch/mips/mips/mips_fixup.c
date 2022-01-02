c324a9d17897fcae5d3fd18214d793bb8a81e44d
christos <christos@NetBSD.org>
2022-01-02 16:03:46 +0000

    fix KASSERT issue

--- sys/arch/mips/mips/mips_fixup.c
+++ sys/arch/mips/mips/mips_fixup.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: mips_fixup.c,v 1.22 2021/10/02 14:28:04 skrll Exp $	*/
+/*	$NetBSD: mips_fixup.c,v 1.23 2022/01/02 16:03:46 christos Exp $	*/
 
 /*-
  * Copyright (c) 2010 The NetBSD Foundation, Inc.
@@ -30,7 +30,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: mips_fixup.c,v 1.22 2021/10/02 14:28:04 skrll Exp $");
+__KERNEL_RCSID(0, "$NetBSD: mips_fixup.c,v 1.23 2022/01/02 16:03:46 christos Exp $");
 
 #include "opt_mips3_wired.h"
 #include "opt_multiprocessor.h"
@@ -120,6 +120,7 @@ mips_fixup_exceptions(mips_fixup_callback_t callback, void *arg)
 			if (addr <= load_addr
 			    && load_addr < addr + size
 			    && base == lui_reg) {
+#if defined(DIAGNOSTIC) || defined(DEBUG_VERBOSE)
 				KASSERT(rt == _R_K0 || rt == _R_K1);
 #ifdef DEBUG_VERBOSE
 				printf("%s: %#x: insn %08x: %s r%zu, %%lo(%08x)(r%zu)\n",
@@ -129,6 +130,7 @@ mips_fixup_exceptions(mips_fixup_callback_t callback, void *arg)
 					? INSN_LW_P(insn) ? "lw" : "ld"
 					: INSN_SW_P(insn) ? "sw" : "sd",
 				    rt, load_addr, base);
+#endif
 #endif
 				new_insns[0] = lui_insn;
 				new_insns[1] = *insnp;
