958fd6bb0c5423f8891c2095b25fa14890f5343e
rillig <rillig@NetBSD.org>
2022-01-02 03:23:55 +0000

    tests/make: demonstrate bugs in PrintStackTrace

--- usr.bin/make/unit-tests/opt-debug-parse.mk
+++ usr.bin/make/unit-tests/opt-debug-parse.mk
@@ -1,9 +1,24 @@
-# $NetBSD: opt-debug-parse.mk,v 1.1 2020/09/05 06:20:51 rillig Exp $
+# $NetBSD: opt-debug-parse.mk,v 1.2 2022/01/02 03:23:55 rillig Exp $
 #
 # Tests for the -dp command line option, which adds debug logging about
 # makefile parsing.
 
+.MAKEFLAGS: -dp
+
 # TODO: Implementation
 
-all:
-	@:;
+# In PrintStackTrace, the line number of the .for loop is wrong.  The actual
+# line number printed is the last line before the loop body, while it should
+# rather be the line number where the .for loop starts.
+.for \
+    var \
+    in \
+    value
+.info trace with multi-line .for loop head
+.endfor
+# FIXME: The .exp file says 'in .include from opt-debug-parse.mk:18', which is
+# completely wrong.  It should rather say 'in .for loop from :13'.
+
+.MAKEFLAGS: -d0
+
+all: .PHONY
