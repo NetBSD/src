a05fe36ad6a5086e9680f4514e53a8ccfa6cc87a
sjg <sjg@NetBSD.org>
2022-01-14 19:31:44 +0000

    Add unit-test for .MAKE.META.CMP_FILTER

--- usr.bin/make/unit-tests/meta-cmd-cmp.mk
+++ usr.bin/make/unit-tests/meta-cmd-cmp.mk
@@ -1,4 +1,4 @@
-# $NetBSD: meta-cmd-cmp.mk,v 1.2 2020/12/05 22:51:34 sjg Exp $
+# $NetBSD: meta-cmd-cmp.mk,v 1.3 2022/01/14 19:31:44 sjg Exp $
 #
 # Tests META_MODE command line comparison
 #
@@ -9,7 +9,7 @@
 tf:= .${.PARSEFILE:R}
 
 .if ${.TARGETS:Nall} == ""
-all: prep one two change1 change2 post
+all: prep one two change1 change2 filter0 filter1 filter2 post
 
 CLEANFILES= ${tf}*
 
@@ -22,6 +22,7 @@ FLAGS?=
 FLAGS2?=
 
 tests= ${tf}.cmp ${tf}.nocmp ${tf}.cmp2
+filter_tests= ${tf}.filter
 
 ${tf}.cmp:
 	@echo FLAGS=${FLAGS:Uempty} > $@
@@ -35,6 +36,14 @@ ${tf}.cmp2:
 	@echo FLAGS2=${FLAGS2:Uempty} > $@
 	@echo This line not compared FLAGS=${FLAGS:Uempty} ${.OODATE:MNOMETA_CMP}
 
+CCACHE= ccache
+.ifdef WITH_CMP_FILTER
+.MAKE.META.CMP_FILTER += Nccache Ndistcc
+.endif
+
+${tf}.filter:
+	@echo ${CCACHE} cc -c foo.c > $@
+
 # these do the same 
 one two: .PHONY
 	@echo $@:
@@ -47,6 +56,19 @@ change1: .PHONY
 change2: .PHONY
 	@echo $@:
 	@${.MAKE} -dM -r -C ${.CURDIR} -f ${MAKEFILE} FLAGS2=changed ${tests}
+
+filter0: .PHONY
+	@echo $@:
+	@${.MAKE} -dM -r -C ${.CURDIR} -f ${MAKEFILE} ${filter_tests}
+
+filter1: .PHONY
+	@echo $@:
+	@${.MAKE} -dM -r -C ${.CURDIR} -f ${MAKEFILE} CCACHE= ${filter_tests}
+
+filter2: .PHONY
+	@echo $@:
+	@${.MAKE} -dM -r -C ${.CURDIR} -f ${MAKEFILE} -DWITH_CMP_FILTER \
+		CCACHE=distcc ${filter_tests}
 
 # don't let gcov mess up the results
 .MAKE.META.IGNORE_PATTERNS+= *.gcda
