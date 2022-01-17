dd5578ea31da39b758eb76867bca039803e1f73e
bouyer <bouyer@NetBSD.org>
2022-01-17 11:12:28 +0000

    Add UQ_HID_IGNORE for the Microchip PICkit2 and 3 programmers.

--- sys/dev/usb/usb_quirks.c
+++ sys/dev/usb/usb_quirks.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: usb_quirks.c,v 1.102 2022/01/12 12:55:46 martin Exp $	*/
+/*	$NetBSD: usb_quirks.c,v 1.103 2022/01/17 11:12:28 bouyer Exp $	*/
 /*	$FreeBSD: src/sys/dev/usb/usb_quirks.c,v 1.30 2003/01/02 04:15:55 imp Exp $	*/
 
 /*
@@ -32,7 +32,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: usb_quirks.c,v 1.102 2022/01/12 12:55:46 martin Exp $");
+__KERNEL_RCSID(0, "$NetBSD: usb_quirks.c,v 1.103 2022/01/17 11:12:28 bouyer Exp $");
 
 #ifdef _KERNEL_OPT
 #include "opt_usb.h"
@@ -199,6 +199,10 @@ Static const struct usbd_quirk_entry {
  { USB_VENDOR_MGE,		USB_PRODUCT_MGE_UPS2,			ANY,
 	{ UQ_HID_IGNORE, NULL }},
  { USB_VENDOR_MICROCHIP,	USB_PRODUCT_MICROCHIP_PICKIT1,		ANY,
+	{ UQ_HID_IGNORE, NULL }},
+ { USB_VENDOR_MICROCHIP,	USB_PRODUCT_MICROCHIP_PICKIT2,		ANY,
+	{ UQ_HID_IGNORE, NULL }},
+ { USB_VENDOR_MICROCHIP,	USB_PRODUCT_MICROCHIP_PICKIT3,		ANY,
 	{ UQ_HID_IGNORE, NULL }},
  { USB_VENDOR_TRIPPLITE2,	ANY,					ANY,
 	{ UQ_HID_IGNORE, NULL }},
