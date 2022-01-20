d910fbb7395a3b1119aa76afe99181e212119ffe
rillig <rillig@NetBSD.org>
2022-01-20 19:24:53 +0000

    make: fix filename in warning about duplicate script (since 2018-12-22)

--- usr.bin/make/unit-tests/dep-duplicate.mk
+++ usr.bin/make/unit-tests/dep-duplicate.mk
@@ -1,4 +1,4 @@
-# $NetBSD: dep-duplicate.mk,v 1.2 2022/01/20 19:16:25 rillig Exp $
+# $NetBSD: dep-duplicate.mk,v 1.3 2022/01/20 19:24:53 rillig Exp $
 #
 # Test for a target whose commands are defined twice.  This generates a
 # warning, not an error, so ensure that the correct commands are kept.
@@ -6,11 +6,7 @@
 # Also ensure that the diagnostics mention the correct file in case of
 # included files.  Since parse.c 1.231 from 2018-12-22 and before parse.c
 # 1.653 from 2022-01-20, the wrong filename had been printed if the file of
-# the first commands section was in a file that was included by its relative
-# path.
-
-# expect: make: "dep-duplicate.inc" line 3: warning: using previous script for "all" defined here
-# FIXME: The file "dep-duplicate.inc" has no line 3.
+# the first commands section was included by its relative path.
 
 all: .PHONY
 	@exec > dep-duplicate.main; \
