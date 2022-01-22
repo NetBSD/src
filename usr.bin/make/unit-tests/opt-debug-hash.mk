c7d1780dbb9e477ec3bc8115e8c6b2f193f6f690
rillig <rillig@NetBSD.org>
2022-01-22 17:10:51 +0000

    tests/make: add a few more tests

--- usr.bin/make/unit-tests/opt-debug-hash.mk
+++ usr.bin/make/unit-tests/opt-debug-hash.mk
@@ -1,10 +1,12 @@
-# $NetBSD: opt-debug-hash.mk,v 1.1 2020/09/05 06:20:51 rillig Exp $
+# $NetBSD: opt-debug-hash.mk,v 1.2 2022/01/22 17:10:51 rillig Exp $
 #
 # Tests for the -dh command line option, which adds debug logging for
 # hash tables.  Even more detailed logging is available by compiling
 # make with -DDEBUG_HASH_LOOKUP.
 
-# TODO: Implementation
+.MAKEFLAGS: -dh
 
-all:
-	@:;
+.error
+
+# FIXME: There is a newline missing between 'continueHashTable'.
+# expect: make: Fatal errors encountered -- cannot continueHashTable targets: size=16 numEntries=0 maxchain=0
