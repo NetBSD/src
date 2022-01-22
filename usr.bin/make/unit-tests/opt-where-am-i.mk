6c3a1b34597b0ef3e2bd43cc87a827fe510551dc
rillig <rillig@NetBSD.org>
2022-01-22 17:10:51 +0000

    tests/make: add a few more tests

--- usr.bin/make/unit-tests/opt-where-am-i.mk
+++ usr.bin/make/unit-tests/opt-where-am-i.mk
@@ -1,8 +1,14 @@
-# $NetBSD: opt-where-am-i.mk,v 1.2 2020/08/16 14:25:16 rillig Exp $
+# $NetBSD: opt-where-am-i.mk,v 1.3 2022/01/22 17:10:51 rillig Exp $
 #
-# Tests for the -w command line option.
+# Tests for the -w command line option, which outputs the current directory
+# at the beginning and end of running make.  This is useful when building
+# large source trees that involve several nested make calls.
 
-# TODO: Implementation
+# The first "Entering directory" is missing since the below .MAKEFLAGS comes
+# too late for it.
+.MAKEFLAGS: -w
 
 all:
-	@:;
+.if ${.CURDIR} != "/"
+	@${MAKE} -r -f ${MAKEFILE:tA} -C /
+.endif
