6553c43d1703ca3dfee08e282837b4b7a2082eb6
rillig <rillig@NetBSD.org>
2022-01-23 16:09:38 +0000

    tests/make: extend test suite, move old tests to 2020 scheme
    The tests from envfirst.mk are now in opt-env.mk.
    The tests from modword.mk are now in varmod-select-words.mk.

--- usr.bin/make/unit-tests/var-class-global.mk
+++ usr.bin/make/unit-tests/var-class-global.mk
@@ -1,8 +1,18 @@
-# $NetBSD: var-class-global.mk,v 1.2 2020/08/16 14:25:16 rillig Exp $
+# $NetBSD: var-class-global.mk,v 1.3 2022/01/23 16:09:38 rillig Exp $
 #
 # Tests for global variables, which are the most common variables.
 
-# TODO: Implementation
+# Global variables can be assigned and appended to.
+GLOBAL=		value
+GLOBAL+=	addition
+.if ${GLOBAL} != "value addition"
+.  error
+.endif
 
-all:
-	@:;
+# Global variables can be removed from their scope.
+.undef GLOBAL
+.if defined(GLOBAL)
+.  error
+.endif
+
+all: .PHONY
