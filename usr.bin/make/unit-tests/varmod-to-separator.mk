06a4cfdde2b56310e13c37ffb642ef8147e1d371
rillig <rillig@NetBSD.org>
2022-01-23 18:00:53 +0000

    tests/make: migrate modts to varmod-to-separator and explain

--- usr.bin/make/unit-tests/varmod-to-separator.mk
+++ usr.bin/make/unit-tests/varmod-to-separator.mk
@@ -1,4 +1,4 @@
-# $NetBSD: varmod-to-separator.mk,v 1.7 2020/11/15 20:20:58 rillig Exp $
+# $NetBSD: varmod-to-separator.mk,v 1.8 2022/01/23 18:00:53 rillig Exp $
 #
 # Tests for the :ts variable modifier, which joins the words of the variable
 # using an arbitrary character as word separator.
@@ -80,6 +80,52 @@ WORDS=	one two three four five six
 .  warning The :ts modifier followed by an :S modifier does not work.
 .endif
 
+# After the modifier ':ts/', the expression value is a single word since all
+# spaces have been replaced with '/'.  This single word does not start with
+# 'two', which makes the modifier ':S' a no-op.
+.if ${WORDS:ts/:S/^two/2/} != "one/two/three/four/five/six"
+.  error
+.endif
+
+# After the :ts modifier, the whole string is interpreted as a single
+# word since all spaces have been replaced with x.  Because of this single
+# word, only the first 'b' is replaced with 'B'.
+.if ${aa bb aa bb aa bb:L:tsx:S,b,B,} != "aaxBbxaaxbbxaaxbb"
+.  error
+.endif
+
+# The :ts modifier also applies to word separators that are added
+# afterwards.  First, the modifier ':tsx' joins the 3 words, then the modifier
+# ':S' replaces the 2 'b's with spaces.  These spaces are part of the word,
+# so when the words are joined at the end of the modifier ':S', there is only
+# a single word, and the custom separator from the modifier ':tsx' has no
+# effect.
+.if ${a ababa c:L:tsx:S,b, ,g} != "axa a axc"
+.  error
+.endif
+
+# Adding the modifier ':M*' at the end of the above chain splits the
+# expression value and then joins it again.  At this point of splitting, the
+# newly added spaces are treated as word separators, resulting in 3 words.
+# When these 3 words are joined, the separator from the modifier ':tsx' is
+# used.
+.if ${a ababa c:L:tsx:S,b, ,g:M*} != "axaxaxaxc"
+.  error
+.endif
+
+# Not all modifiers use the separator from the previous modifier ':ts' though.
+# The modifier ':@' always uses a space as word separator instead.  This has
+# probably been an oversight during implementation.  For consistency, the
+# result should rather be "axaxaxaxc", as in the previous example.
+.if ${a ababa c:L:tsx:S,b, ,g:@v@$v@} != "axa a axc"
+.  error
+.endif
+
+# Adding a final :M* modifier applies the :ts separator again, though.
+.if ${a ababa c:L:tsx:S,b, ,g:@v@${v}@:M*} != "axaxaxaxc"
+.  error
+.endif
+
 # The separator can be \n, which is a newline.
 .if ${WORDS:[1..3]:ts\n} != "one${.newline}two${.newline}three"
 .  warning The separator \n does not produce a newline.
@@ -155,9 +201,19 @@ WORDS=	one two three four five six
 
 
 # In the :t modifier, the :t must be followed by any of A, l, s, u.
-.if ${WORDS:tx} != "anything"
-.  info This line is not reached because of the malformed condition.
-.  info If this line were reached, it would be visible in the -dcpv log.
+# expect: Bad modifier ":tx" for variable "WORDS"
+.if ${WORDS:tx}
+.  error
+.else
+.  error
+.endif
+
+# The word separator must be can only be a single character.
+# expect: Bad modifier ":ts\X" for variable "WORDS"
+.if ${WORDS:ts\X}
+.  error
+.else
+.  error
 .endif
 
 # After the backslash, only n, t, an octal number, or x and a hexadecimal
@@ -171,5 +227,3 @@ WORDS=	one two three four five six
 # for ':ts\90' ('Z' in decimal ASCII, undefined in octal)?
 
 # TODO: :ts\x1F600
-
-all:
