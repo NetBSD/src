a9de6c66643a4fbb5b891d8f9774bfd2c7eac116
rillig <rillig@NetBSD.org>
2022-01-29 09:38:26 +0000

    make: print stack trace on fatal errors
    The only fatal error that occurs while the makefiles are read in is the
    one about recursive variables, which didn't give any hint about the
    location before.
    
    If a recursive variable is detected while evaluating the commands of a
    target to be made, there is no location information, as before.

--- usr.bin/make/main.c
+++ usr.bin/make/main.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: main.c,v 1.576 2022/01/27 06:02:59 sjg Exp $	*/
+/*	$NetBSD: main.c,v 1.577 2022/01/29 09:38:26 rillig Exp $	*/
 
 /*
  * Copyright (c) 1988, 1989, 1990, 1993
@@ -111,7 +111,7 @@
 #include "trace.h"
 
 /*	"@(#)main.c	8.3 (Berkeley) 3/19/94"	*/
-MAKE_RCSID("$NetBSD: main.c,v 1.576 2022/01/27 06:02:59 sjg Exp $");
+MAKE_RCSID("$NetBSD: main.c,v 1.577 2022/01/29 09:38:26 rillig Exp $");
 #if defined(MAKE_NATIVE) && !defined(lint)
 __COPYRIGHT("@(#) Copyright (c) 1988, 1989, 1990, 1993 "
 	    "The Regents of the University of California.  "
@@ -1828,6 +1828,7 @@ Fatal(const char *fmt, ...)
 	va_end(ap);
 	(void)fprintf(stderr, "\n");
 	(void)fflush(stderr);
+	PrintStackTrace(true);
 
 	PrintOnError(NULL, "\n");
 
