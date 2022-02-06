766b864626ea8b7d9aee53860f60e517ade41a4e
jmcneill <jmcneill@NetBSD.org>
2022-02-06 15:52:20 +0000

    sdhc: Retire SDHC_FLAG_USE_ADMA2 flag.
    ADMA2 support in sdhc is mature now, so no need for it to be opt-in.

--- sys/dev/sdmmc/sdhcvar.h
+++ sys/dev/sdmmc/sdhcvar.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: sdhcvar.h,v 1.31 2019/10/23 05:20:52 hkenken Exp $	*/
+/*	$NetBSD: sdhcvar.h,v 1.32 2022/02/06 15:52:20 jmcneill Exp $	*/
 /*	$OpenBSD: sdhcvar.h,v 1.3 2007/09/06 08:01:01 jsg Exp $	*/
 
 /*
@@ -55,18 +55,17 @@ struct sdhc_softc {
 #define	SDHC_FLAG_NO_CLKBASE	0x00020000 /* ignore clkbase register */
 #define	SDHC_FLAG_SINGLE_POWER_WRITE 0x00040000
 #define	SDHC_FLAG_NO_TIMEOUT	0x00080000 /* ignore timeout interrupts */
-#define	SDHC_FLAG_USE_ADMA2	0x00100000
-#define	SDHC_FLAG_POLL_CARD_DET	0x00200000 /* polling card detect */
-#define	SDHC_FLAG_SLOW_SDR50  	0x00400000 /* reduce SDR50 speed */
-#define	SDHC_FLAG_USDHC		0x00800000 /* Freescale uSDHC */
-#define	SDHC_FLAG_NO_AUTO_STOP	0x01000000 /* No auto CMD12 */
-#define	SDHC_FLAG_NO_BUSY_INTR	0x02000000 /* No intr when RESP_BUSY */
-#define	SDHC_FLAG_STOP_WITH_TC	0x04000000 /* CMD12 can set xfer complete w/o SCF_RSP_BSY */
-#define	SDHC_FLAG_BROKEN_ADMA2_ZEROLEN 0x08000000 /*
+#define	SDHC_FLAG_POLL_CARD_DET	0x00100000 /* polling card detect */
+#define	SDHC_FLAG_SLOW_SDR50  	0x00200000 /* reduce SDR50 speed */
+#define	SDHC_FLAG_USDHC		0x00400000 /* Freescale uSDHC */
+#define	SDHC_FLAG_NO_AUTO_STOP	0x00800000 /* No auto CMD12 */
+#define	SDHC_FLAG_NO_BUSY_INTR	0x01000000 /* No intr when RESP_BUSY */
+#define	SDHC_FLAG_STOP_WITH_TC	0x02000000 /* CMD12 can set xfer complete w/o SCF_RSP_BSY */
+#define	SDHC_FLAG_BROKEN_ADMA2_ZEROLEN 0x04000000 /*
 						   * Broken ADMA2 zero length descriptor
 						   * Can't 64K Byte data transfer
 						   */
-#define	SDHC_FLAG_NO_1_8_V	0x10000000 /* No 1.8V supply */
+#define	SDHC_FLAG_NO_1_8_V	0x08000000 /* No 1.8V supply */
 
 	uint32_t		sc_clkbase;
 	int			sc_clkmsk;	/* Mask for SDCLK */
