cdc3b2d88eeaeb83d5b1118bf9db78ade52ce6f3
rillig <rillig@NetBSD.org>
2022-02-07 02:39:10 +0000

    tests/lint: add test for message 41 about bit-field in union
    See PR toolchain/53219.

--- tests/usr.bin/xlint/lint1/msg_041.c
+++ tests/usr.bin/xlint/lint1/msg_041.c
@@ -1,7 +1,10 @@
-/*	$NetBSD: msg_041.c,v 1.2 2021/02/21 09:07:58 rillig Exp $	*/
+/*	$NetBSD: msg_041.c,v 1.3 2022/02/07 02:39:10 rillig Exp $	*/
 # 3 "msg_041.c"
 
 // Test for message: illegal use of bit-field [41]
 
-TODO: "Add example code that triggers the above message." /* expect: 249 */
-TODO: "Add example code that almost triggers the above message."
+union u {
+	int member;
+	/* expect+1: illegal use of bit-field [41] */
+	unsigned bit_field : 7;
+};
