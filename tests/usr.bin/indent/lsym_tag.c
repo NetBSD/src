c0d6a3ebd14be41bf3c7e1eb0dfaf7ac0012e5a6
rillig <rillig@NetBSD.org>
2022-02-12 19:46:56 +0000

    tests/indent: add another test for indentation of enum constants
    To prevent an overzealous quick hack for indenting enum constants in a
    typedef, as part of fixing PR#55453.

--- tests/usr.bin/indent/lsym_tag.c
+++ tests/usr.bin/indent/lsym_tag.c
@@ -1,4 +1,4 @@
-/* $NetBSD: lsym_tag.c,v 1.1 2021/11/18 21:19:19 rillig Exp $ */
+/* $NetBSD: lsym_tag.c,v 1.2 2022/02/12 19:46:56 rillig Exp $ */
 /* $FreeBSD$ */
 
 /*
@@ -6,8 +6,38 @@
  * 'struct', 'union' or 'enum' that declare, define or use a tagged type.
  */
 
+/* TODO: Add systematic tests for 'struct'. */
+/* TODO: Add systematic tests for 'union'. */
+/* TODO: Add systematic tests for 'enum'. */
+
+
 #indent input
-// TODO: add input
+int
+indent_enum_constants(void)
+{
+	enum color {
+		red,
+		green
+	};
+	enum color	colors[] = {
+		red,
+		red,
+		red,
+	};
+	/*
+	 * Ensure that the token sequence 'enum type {' only matches if there
+	 * are no other tokens in between, to prevent statement continuations
+	 * from being indented like enum constant declarations.
+	 *
+	 * See https://gnats.netbsd.org/55453.
+	 */
+	if (colors[0] == (enum color)1) {
+		return 1
+		  + 2
+		  + 3;
+	}
+	return 0;
+}
 #indent end
 
-#indent run-equals-input
+#indent run-equals-input -ci2
