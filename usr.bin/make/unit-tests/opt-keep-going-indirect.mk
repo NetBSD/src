ae38fc6c2f583fc93ce15edc6323c4cd56542094
rillig <rillig@NetBSD.org>
2022-02-12 20:05:36 +0000

    tests/make: clean up comments in test for indirect -k failures
    See PR#49720.

--- usr.bin/make/unit-tests/opt-keep-going-indirect.mk
+++ usr.bin/make/unit-tests/opt-keep-going-indirect.mk
@@ -1,4 +1,4 @@
-# $NetBSD: opt-keep-going-indirect.mk,v 1.1 2022/02/12 13:17:57 rillig Exp $
+# $NetBSD: opt-keep-going-indirect.mk,v 1.2 2022/02/12 20:05:36 rillig Exp $
 #
 # Tests for the -k command line option, which stops building a target as soon
 # as an error is detected, but continues building the other, independent
@@ -6,27 +6,28 @@
 #
 # History:
 #	In 1993, the exit status for the option '-k' was always 0, even if a
-#	a direct or an indirect target failed.
+#	direct or an indirect target failed.
 #
 #	Since 2000.12.30.02.05.21, the word '(continuing)' is missing in jobs
 #	mode, both for direct as well as indirect targets.
 #
 #	Since 2001.10.16.18.50.12, the exit status for a direct failure in
-#	compat mode is 1, while jobs mode and indirect failures still return
-#	exit status 0.  The number of empty lines between the various error
-#	messages differs between the modes, for no reason.
+#	compat mode is the correct 1, while jobs mode and indirect failures
+#	still return the wrong exit status 0.  The number of empty lines
+#	between the various error messages differs between the modes, for no
+#	reason.
 #
 #	At 2006.11.17.22.07.39, the exit status for direct failures in both
-#	modes and for indirect failures in jobs mode was corrected, leaving
-#	only indirect failures in compat mode wrong.  On the downside, a
-#	failed indirect target in jobs mode was no longer listed as "not
-#	remade because of errors".
+#	modes and for indirect failures in jobs mode was fixed to the correct
+#	1.  The exit status for indirect failures in compat mode is still the
+#	wrong 0.  On the downside, a failed indirect target in jobs mode is no
+#	longer listed as "not remade because of errors".
 #
-#	At 2016.08.26.23.28.39, the additional empty lines for a direct
-#	failure in compat mode was removed, making it consistent with a direct
-#	failure in jobs mode.  This left only one inconsistency, in that
-#	indirect failures in jobs mode (by far the most common when building
-#	large projects) did not produce any empty line.
+#	At 2016.08.26.23.28.39, the additional empty line for a direct failure
+#	in compat mode was removed, making it consistent with a direct failure
+#	in jobs mode.  This left only one inconsistency, in that indirect
+#	failures in jobs mode (by far the most common when building large
+#	projects) did not produce any empty line.
 #
 #	Since 2020.12.07.00.53.30, the exit status is consistently 1 for
 #	failures in all 4 modes.
@@ -83,6 +84,7 @@ direct:
 # expect: exited 1
 
 # TODO: Add '(continuing)'.
+# TODO: Add 'not remade because of errors'.
 # expect: indirect jobs
 # expect: *** [direct] Error code 1
 # expect: exited 1
