ecb4d302b689f5687ec2636501d0b8ce979f57ae
rillig <rillig@NetBSD.org>
2022-02-12 01:23:44 +0000

    tests/lint: document where some C99 tests come from
    These tests were added due to PR#45417.

--- tests/usr.bin/xlint/lint1/d_c99_decls_after_stmt.c
+++ tests/usr.bin/xlint/lint1/d_c99_decls_after_stmt.c
@@ -1,5 +1,12 @@
-/*	$NetBSD: d_c99_decls_after_stmt.c,v 1.3 2021/01/31 14:57:28 rillig Exp $	*/
+/*	$NetBSD: d_c99_decls_after_stmt.c,v 1.4 2022/02/12 01:23:44 rillig Exp $	*/
 # 3 "d_c99_decls_after_stmt.c"
+
+/*
+ * Before cgram.y 1.50 from 2011-10-04, lint complained about syntax errors
+ * at the second 'int'.
+ *
+ * https://gnats.netbsd.org/45417
+ */
 
 void
 sample(void)
