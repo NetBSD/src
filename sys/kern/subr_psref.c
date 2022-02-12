7e9cceaba660b75a86fcb1f49f85f3c736449600
macallan <macallan@NetBSD.org>
2022-02-12 16:31:06 +0000

    more __diagused to appease clang

--- sys/kern/subr_psref.c
+++ sys/kern/subr_psref.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: subr_psref.c,v 1.17 2022/02/08 12:59:16 riastradh Exp $	*/
+/*	$NetBSD: subr_psref.c,v 1.18 2022/02/12 16:31:06 macallan Exp $	*/
 
 /*-
  * Copyright (c) 2016 The NetBSD Foundation, Inc.
@@ -64,7 +64,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: subr_psref.c,v 1.17 2022/02/08 12:59:16 riastradh Exp $");
+__KERNEL_RCSID(0, "$NetBSD: subr_psref.c,v 1.18 2022/02/12 16:31:06 macallan Exp $");
 
 #include <sys/param.h>
 #include <sys/types.h>
@@ -180,7 +180,7 @@ psref_cpu_drained_p(void *p, void *cookie, struct cpu_info *ci __unused)
 		*retp = false;
 }
 
-static bool
+static bool __diagused
 psref_class_drained_p(const struct psref_class *prc)
 {
 	bool ret = true;
