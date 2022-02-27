dfaaa573b0bc0fbe826a97d4ffaed9091f5dda27
riastradh <riastradh@NetBSD.org>
2022-02-27 14:22:21 +0000

    drm: Move acpi_check_dsm &c. from intel_acpi.c to new linux_acpi.c.

--- sys/external/bsd/drm2/dist/drm/i915/display/intel_acpi.c
+++ sys/external/bsd/drm2/dist/drm/i915/display/intel_acpi.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: intel_acpi.c,v 1.6 2022/02/27 14:20:30 riastradh Exp $	*/
+/*	$NetBSD: intel_acpi.c,v 1.7 2022/02/27 14:22:21 riastradh Exp $	*/
 
 // SPDX-License-Identifier: GPL-2.0
 /*
@@ -8,7 +8,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: intel_acpi.c,v 1.6 2022/02/27 14:20:30 riastradh Exp $");
+__KERNEL_RCSID(0, "$NetBSD: intel_acpi.c,v 1.7 2022/02/27 14:22:21 riastradh Exp $");
 
 #include <linux/pci.h>
 #include <linux/acpi.h>
@@ -32,91 +32,6 @@ ACPI_MODULE_NAME("acpi_intel_brightness")
 #define	package		Package
 #define	pointer		Pointer
 #define	value		Value
-
-static ACPI_OBJECT *
-acpi_evaluate_dsm(ACPI_HANDLE handle, const guid_t *uuid, int rev, int func,
-    ACPI_OBJECT *argv4)
-{
-	ACPI_OBJECT_LIST arg;
-	ACPI_OBJECT params[4];
-	ACPI_BUFFER buf;
-	ACPI_STATUS rv;
-
-	if (handle == NULL)
-		handle = ACPI_ROOT_OBJECT;
-
-	arg.Count = 4;
-	arg.Pointer = params;
-	params[0].Type = ACPI_TYPE_BUFFER;
-	params[0].Buffer.Length = 16;
-	params[0].Buffer.Pointer = (char *)__UNCONST(uuid);
-	params[1].Type = ACPI_TYPE_INTEGER;
-	params[1].Integer.Value = rev;
-	params[2].Type = ACPI_TYPE_INTEGER;
-	params[2].Integer.Value = func;
-	if (argv4 != NULL) {
-		params[3] = *argv4;
-	} else {
-		params[3].Type = ACPI_TYPE_PACKAGE;
-		params[3].Package.Count = 0;
-		params[3].Package.Elements = NULL;
-	}
-
-	buf.Pointer = NULL;
-	buf.Length = ACPI_ALLOCATE_LOCAL_BUFFER;
-
-	rv = AcpiEvaluateObject(handle, "_DSM", &arg, &buf);
-	if (ACPI_SUCCESS(rv))
-		return (ACPI_OBJECT *)buf.Pointer;
-	return NULL;
-}
-
-static inline ACPI_OBJECT *
-acpi_evaluate_dsm_typed(ACPI_HANDLE handle, const guid_t *uuid, int rev,
-    int func, ACPI_OBJECT *argv4, ACPI_OBJECT_TYPE type)
-{
-	ACPI_OBJECT *obj;
-
-	obj = acpi_evaluate_dsm(handle, uuid, rev, func, argv4);
-	if (obj != NULL && obj->Type != type) {
-		ACPI_FREE(obj);
-		obj = NULL;
-	}
-	return obj;
-}
-
-#define	ACPI_INIT_DSM_ARGV4(cnt, eles)		\
-{						\
-	.Package.Type = ACPI_TYPE_PACKAGE,	\
-	.Package.Count = (cnt),			\
-	.Package.Elements = (eles)		\
-}
-
-static bool
-acpi_check_dsm(ACPI_HANDLE handle, const guid_t *uuid, int rev, uint64_t funcs)
-{
-	ACPI_OBJECT *obj;
-	uint64_t mask = 0;
-	int i;
-
-	if (funcs == 0)
-		return false;
-
-	obj = acpi_evaluate_dsm(handle, uuid, rev, 0, NULL);
-	if (obj == NULL)
-		return false;
-
-	if (obj->Type == ACPI_TYPE_INTEGER)
-		mask = obj->Integer.Value;
-	else if (obj->Type == ACPI_TYPE_BUFFER)
-		for (i = 0; i < obj->Buffer.Length && i < 8; i++)
-			mask |= (uint64_t)obj->Buffer.Pointer[i] << (i * 8);
-	ACPI_FREE(obj);
-
-	if ((mask & 0x1) == 0x1 && (mask & funcs) == funcs)
-		return true;
-	return false;
-}
 #endif
 
 #define INTEL_DSM_REVISION_ID 1 /* For Calpella anyway... */
