ca03808ba6f10726a2247f1d04371eac0ec4231c
rillig <rillig@NetBSD.org>
2022-02-27 19:32:51 +0000

    lint: concatenate string literals from left to right
    Previously, the string literals "1" "2" "3" "4" were concatenated in the
    order "23", "234", "1234".  This influenced the location of the
    diagnostics for traditional C (which doesn't know concatenation at all)
    and for mixing regular strings and wide strings.
    
    Now the diagnostics occur exactly where they are expected.  The first
    string literal defines whether the whole string is regular or wide, and
    any further string literals must match it.
    
    In traditional C mode, there are more diagnostics than before, but that
    doesn't hurt since they are still correct and nobody uses lint in
    traditional C mode anyway.

--- tests/usr.bin/xlint/lint1/msg_292.c
+++ tests/usr.bin/xlint/lint1/msg_292.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: msg_292.c,v 1.3 2022/02/27 18:51:21 rillig Exp $	*/
+/*	$NetBSD: msg_292.c,v 1.4 2022/02/27 19:32:51 rillig Exp $	*/
 # 3 "msg_292.c"
 
 // Test for message: cannot concatenate wide and regular string literals [292]
@@ -19,21 +19,13 @@ typedef int reveal_sizeof_c_c_c_w_w_w[-(int)sizeof(c_c_c_w_w_w)];
 
 const char c_w_c_w_c_w[] =
 	"c2"
+	/* expect+1: error: cannot concatenate wide and regular string literals [292] */
 	L"w2"
-	/* expect+1: error: cannot concatenate wide and regular string literals [292] */
 	"c  4"
-	L"w  4"
 	/* expect+1: error: cannot concatenate wide and regular string literals [292] */
+	L"w  4"
 	"c      8"
 	/* expect+1: error: cannot concatenate wide and regular string literals [292] */
 	L"w      8";
-/*
- * Concatenating L"w2" with "c4" fails, keeping L"w2".
- * Concatenating L"w2" with L"w4" succeeds, resulting in L"w2w4".
- * Concatenating L"w2w4" with "c8" fails, keeping L"w2w4".
- * Concatenating L"w2w4" with L"w8" succeeds, resulting in L"w2w4w8".
- * Concatenating "c2" with L"w2w4w8" fails, keeping "c2".
- * The size of "c2" is 3.
- */
-/* expect+1: error: negative array dimension (-3) [20] */
+/* expect+1: error: negative array dimension (-15) [20] */
 typedef int reveal_sizeof_c_w_c_w_c_w[-(int)sizeof(c_w_c_w_c_w)];
