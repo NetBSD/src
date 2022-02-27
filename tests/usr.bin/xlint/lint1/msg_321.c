b925e37d2a8c001af44b1bf7879e5024e3f6af9a
rillig <rillig@NetBSD.org>
2022-02-27 12:00:27 +0000

    tests/lint: add tests for messages referring to C99

--- tests/usr.bin/xlint/lint1/msg_321.c
+++ tests/usr.bin/xlint/lint1/msg_321.c
@@ -1,7 +1,13 @@
-/*	$NetBSD: msg_321.c,v 1.3 2022/02/27 11:40:30 rillig Exp $	*/
+/*	$NetBSD: msg_321.c,v 1.4 2022/02/27 12:00:27 rillig Exp $	*/
 # 3 "msg_321.c"
 
-// Test for message: array initializer with designators is a C99 feature [321]
+/* Test for message: array initializer with designators is a C99 feature [321] */
 
-TODO: "Add example code that triggers the above message." /* expect: 249 */
-TODO: "Add example code that almost triggers the above message."
+/* lint1-flags: -sw */
+
+int vector[3] = {
+	/* expect+1: warning: array initializer with designators is a C99 feature [321] */
+	[0] = 3,
+	/* expect+1: warning: array initializer with designators is a C99 feature [321] */
+	[1] = 5,
+};
