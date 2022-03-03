c499beaef19e616f368dd3d198e8f34ddfbd41e4
riastradh <riastradh@NetBSD.org>
2022-03-03 05:52:46 +0000

    usbnet: Take the core lock around uno_mcast.
    Every driver does this already.  This will enable us to change the
    lock that serializes access to the registers so we can go back to
    doing this synchronously in SIOCADDMULTI/SIOCDELMULTI.

--- sys/dev/usb/if_smsc.c
+++ sys/dev/usb/if_smsc.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: if_smsc.c,v 1.79 2022/03/03 05:52:35 riastradh Exp $	*/
+/*	$NetBSD: if_smsc.c,v 1.80 2022/03/03 05:52:46 riastradh Exp $	*/
 
 /*	$OpenBSD: if_smsc.c,v 1.4 2012/09/27 12:38:11 jsg Exp $	*/
 /*	$FreeBSD: src/sys/dev/usb/net/if_smsc.c,v 1.1 2012/08/15 04:03:55 gonzo Exp $ */
@@ -61,7 +61,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: if_smsc.c,v 1.79 2022/03/03 05:52:35 riastradh Exp $");
+__KERNEL_RCSID(0, "$NetBSD: if_smsc.c,v 1.80 2022/03/03 05:52:46 riastradh Exp $");
 
 #ifdef _KERNEL_OPT
 #include "opt_usb.h"
@@ -762,11 +762,7 @@ smsc_uno_mcast(struct ifnet *ifp)
 {
 	struct usbnet * const un = ifp->if_softc;
 
-	usbnet_lock_core(un);
-
 	smsc_setiff_locked(un);
-
-	usbnet_unlock_core(un);
 }
 
 static int
