a8399c055353da922143d69e40a3b29fc1d62951
riastradh <riastradh@NetBSD.org>
2022-03-03 06:26:28 +0000

    arm: Use device_set_private for various drivers.

--- sys/arch/arm/broadcom/bcm53xx_ccb.c
+++ sys/arch/arm/broadcom/bcm53xx_ccb.c
@@ -34,7 +34,7 @@
 
 #include <sys/cdefs.h>
 
-__KERNEL_RCSID(1, "$NetBSD: bcm53xx_ccb.c,v 1.8 2021/08/07 16:18:43 thorpej Exp $");
+__KERNEL_RCSID(1, "$NetBSD: bcm53xx_ccb.c,v 1.9 2022/03/03 06:26:28 riastradh Exp $");
 
 #include <sys/param.h>
 #include <sys/bus.h>
@@ -168,7 +168,7 @@ bcmccb_mainbus_attach(device_t parent, device_t self, void *aux)
 	struct bcmccb_softc * const sc = &bcmccb_sc;
 
 	sc->sc_dev = self;
-	self->dv_private = sc;
+	device_set_private(self, sc);
 
 	sc->sc_bst = bcm53xx_ioreg_bst;
 
