226a9ab6b851b5ffdf68407526b991c293fd8ed6
riastradh <riastradh@NetBSD.org>
2022-03-03 06:27:03 +0000

    mips: Use device_set_private for cpuN on older ports.
    Specifically, for those that don't use cpu_attach_common of
    mips_subr.c.

--- sys/arch/cobalt/cobalt/cpu.c
+++ sys/arch/cobalt/cobalt/cpu.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: cpu.c,v 1.11 2011/02/20 07:54:10 matt Exp $	*/
+/*	$NetBSD: cpu.c,v 1.12 2022/03/03 06:27:03 riastradh Exp $	*/
 
 /*
  * Copyright (c) 1994, 1995 Carnegie-Mellon University.
@@ -28,7 +28,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: cpu.c,v 1.11 2011/02/20 07:54:10 matt Exp $");
+__KERNEL_RCSID(0, "$NetBSD: cpu.c,v 1.12 2022/03/03 06:27:03 riastradh Exp $");
 
 #include <sys/param.h>
 #include <sys/device.h>
@@ -59,7 +59,7 @@ cpu_attach(device_t parent, device_t self, void *aux)
 	struct cpu_info * const ci = curcpu();
 
 	ci->ci_dev = self;
-	self->dv_private = ci;
+	device_set_private(self, ci);
 
 	aprint_normal(": ");
 	cpu_identify(self);
