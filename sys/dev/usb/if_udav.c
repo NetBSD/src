3485fa95907a3097039f0fcef7460a8be2aa573a
riastradh <riastradh@NetBSD.org>
2022-03-03 05:56:00 +0000

    usbnet drivers: Simplify return of usbnet_init_rx_tx.

--- sys/dev/usb/if_udav.c
+++ sys/dev/usb/if_udav.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: if_udav.c,v 1.94 2022/03/03 05:55:19 riastradh Exp $	*/
+/*	$NetBSD: if_udav.c,v 1.95 2022/03/03 05:56:00 riastradh Exp $	*/
 /*	$nabe: if_udav.c,v 1.3 2003/08/21 16:57:19 nabe Exp $	*/
 
 /*
@@ -45,7 +45,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: if_udav.c,v 1.94 2022/03/03 05:55:19 riastradh Exp $");
+__KERNEL_RCSID(0, "$NetBSD: if_udav.c,v 1.95 2022/03/03 05:56:00 riastradh Exp $");
 
 #ifdef _KERNEL_OPT
 #include "opt_usb.h"
@@ -502,11 +502,9 @@ udav_uno_init(struct ifnet *ifp)
 	}
 
 	if (usbnet_isdying(un))
-		rc = EIO;
-	else
-		rc = usbnet_init_rx_tx(un);
+		return EIO;
 
-	return rc;
+	return usbnet_init_rx_tx(un);
 }
 
 static void
