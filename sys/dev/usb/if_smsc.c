a0a4ae2e888be3ffd00291b55a6fd26de5dee955
riastradh <riastradh@NetBSD.org>
2022-03-03 05:55:38 +0000

    usbnet drivers: Omit redundant device reset via *_uno_stop on init.
    Only those drivers where *_uno_stop is just *_reset, and *_uno_init
    immediately calls *_reset afterward, are affected.

--- sys/dev/usb/if_smsc.c
+++ sys/dev/usb/if_smsc.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: if_smsc.c,v 1.89 2022/03/03 05:55:29 riastradh Exp $	*/
+/*	$NetBSD: if_smsc.c,v 1.90 2022/03/03 05:55:38 riastradh Exp $	*/
 
 /*	$OpenBSD: if_smsc.c,v 1.4 2012/09/27 12:38:11 jsg Exp $	*/
 /*	$FreeBSD: src/sys/dev/usb/net/if_smsc.c,v 1.1 2012/08/15 04:03:55 gonzo Exp $ */
@@ -61,7 +61,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: if_smsc.c,v 1.89 2022/03/03 05:55:29 riastradh Exp $");
+__KERNEL_RCSID(0, "$NetBSD: if_smsc.c,v 1.90 2022/03/03 05:55:38 riastradh Exp $");
 
 #ifdef _KERNEL_OPT
 #include "opt_usb.h"
@@ -554,9 +554,6 @@ smsc_uno_init(struct ifnet *ifp)
 {
 	struct usbnet * const un = ifp->if_softc;
 	struct smsc_softc * const sc = usbnet_softc(un);
-
-	/* Cancel pending I/O */
-	smsc_uno_stop(ifp, 1);
 
 	/* Reset the ethernet interface. */
 	smsc_reset(sc);
