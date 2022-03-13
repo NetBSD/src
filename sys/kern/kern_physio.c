a0b28ef05deae533516cf677a182cd3f7e4acfec
riastradh <riastradh@NetBSD.org>
2022-03-13 18:37:10 +0000

    kern: Use harmless, not harmful, integer truncation in physio.
    Reported-by: syzbot+13abd9bc700f7ceac337@syzkaller.appspotmail.com

--- sys/kern/kern_physio.c
+++ sys/kern/kern_physio.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: kern_physio.c,v 1.99 2021/09/16 22:19:11 andvar Exp $	*/
+/*	$NetBSD: kern_physio.c,v 1.100 2022/03/13 18:37:10 riastradh Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1990, 1993
@@ -71,7 +71,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: kern_physio.c,v 1.99 2021/09/16 22:19:11 andvar Exp $");
+__KERNEL_RCSID(0, "$NetBSD: kern_physio.c,v 1.100 2022/03/13 18:37:10 riastradh Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -317,7 +317,7 @@ physio(void (*strategy)(struct buf *), struct buf *obp, dev_t dev, int flags,
 				 */
 				bp->b_bcount = MIN(MAXPHYS, iovp->iov_len);
 			} else {
-				bp->b_bcount = iovp->iov_len;
+				bp->b_bcount = MIN(INT_MAX, iovp->iov_len);
 			}
 			bp->b_data = iovp->iov_base;
 
