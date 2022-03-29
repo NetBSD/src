464da700f9b246e83585700c4e5f957bfc8acb1d
riastradh <riastradh@NetBSD.org>
2022-03-29 09:08:44 +0000

    emdtv(4): Fix issues in detach.
    - Use config_detach_children, and do it up front, and handle failure
      (not relevant for yanking usb but relevant for drvctl which doesn't
      pass DETACH_FORCE).
    
    - Fix teardown order: stop interrupts so we stop issuing new work,
      and _then_ wait for pending work to drain and destroy the
      workqueue.
    
    - Omit needless empty  mutex_enter(lock); mutex_exit(lock)  dance
      which probably only appeared necessary because of the wrong
      teardown order.

--- sys/dev/usb/emdtv_dtv.c
+++ sys/dev/usb/emdtv_dtv.c
@@ -1,4 +1,4 @@
-/* $NetBSD: emdtv_dtv.c,v 1.16 2021/08/07 16:19:16 thorpej Exp $ */
+/* $NetBSD: emdtv_dtv.c,v 1.17 2022/03/29 09:08:44 riastradh Exp $ */
 
 /*-
  * Copyright (c) 2008, 2011 Jared D. McNeill <jmcneill@invisible.ca>
@@ -27,7 +27,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: emdtv_dtv.c,v 1.16 2021/08/07 16:19:16 thorpej Exp $");
+__KERNEL_RCSID(0, "$NetBSD: emdtv_dtv.c,v 1.17 2022/03/29 09:08:44 riastradh Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -142,12 +142,8 @@ emdtv_dtv_free_xfers(struct emdtv_softc *sc)
 void
 emdtv_dtv_detach(struct emdtv_softc *sc, int flags)
 {
-	sc->sc_streaming = false;
 
-	if (sc->sc_dtvdev != NULL) {
-		config_detach(sc->sc_dtvdev, flags);
-		sc->sc_dtvdev = NULL;
-	}
+	sc->sc_streaming = false;
 
 	if (sc->sc_xc3028)
 		xc3028_close(sc->sc_xc3028);
