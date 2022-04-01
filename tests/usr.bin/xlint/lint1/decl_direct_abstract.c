862c3a89c26418bf65eefae39a8b862a3191dfd8
rillig <rillig@NetBSD.org>
2022-04-01 23:16:31 +0000

    lint: add type details to message about 'sizeof(function)'
    The code in add_function is severely broken, it mixes up the return type
    of the function with the argument types.  For now, at least show the
    guessed type in the diagnostic, to allow human readers quickly spot the
    bug.
    
    Extend the test cases in decl_direct_abstract.c to show that the
    behavior differs unreasonably if the first parameter of the function is
    equal to its return type.

--- tests/usr.bin/xlint/lint1/decl_direct_abstract.c
+++ tests/usr.bin/xlint/lint1/decl_direct_abstract.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: decl_direct_abstract.c,v 1.5 2022/04/01 22:28:21 rillig Exp $	*/
+/*	$NetBSD: decl_direct_abstract.c,v 1.6 2022/04/01 23:16:32 rillig Exp $	*/
 # 3 "decl_direct_abstract.c"
 
 /*
@@ -77,12 +77,119 @@ void int_array_3_array(int[3][7]);
 /* supported since cgram.y 1.363 from 2021-09-14 */
 void int_array_ast_array(int[*][7]);
 
-/* expect+1: error: cannot take size/alignment of function [144] */
+/* expect+1: error: cannot take size/alignment of function type 'function() returning int' [144] */
 unsigned long size_unspecified_args = sizeof(int());
 /* FIXME: Must be 'of function', not 'of void'. */
 /* expect+1: error: cannot take size/alignment of void [146] */
 unsigned long size_prototype_void = sizeof(int(void));
-/* TODO: error: cannot take size/alignment of function [144] */
+/* TODO: error: cannot take size/alignment of function type 'function(double) returning int' [144] */
 unsigned long size_prototype_unnamed = sizeof(int(double));
-/* TODO: error: cannot take size/alignment of function [144] */
+/* TODO: error: cannot take size/alignment of function type 'function(double) returning int' [144] */
 unsigned long size_prototype_named = sizeof(int(double dbl));
+
+/* expect+2: error: cannot take size/alignment of function type 'function() returning int' [144] */
+/* expect+1: error: negative array dimension (-1000) [20] */
+int size_unspecified_args_return_int[-1000 - (int)sizeof(int())];
+
+/* expect+2: error: cannot take size/alignment of function type 'function() returning char' [144] */
+/* expect+1: error: negative array dimension (-1000) [20] */
+int size_unspecified_args_return_char[-1000 - (int)sizeof(char())];
+
+/* FIXME: 'of void' must be 'of function'. */
+/* expect+2: error: cannot take size/alignment of void [146] */
+/* expect+1: error: negative array dimension (-1000) [20] */
+int size_prototype_void_return_int[-1000 - (int)sizeof(int(void))];
+
+/* FIXME: 'of void' must be 'of function'. */
+/* expect+2: error: cannot take size/alignment of void [146] */
+/* expect+1: error: negative array dimension (-1000) [20] */
+int size_prototype_void_return_double[-1000 - (int)sizeof(double(void))];
+
+/* expect+1: error: negative array dimension (-1008) [20] */
+int size_prototype_unnamed_return_int[-1000 - (int)sizeof(int(double))];
+
+/* expect+1: error: negative array dimension (-1008) [20] */
+int size_prototype_unnamed_return_pchar[-1000 - (int)sizeof(char *(double))];
+
+/* expect+1: error: negative array dimension (-1008) [20] */
+int size_prototype_named_return_int[-1000 - (int)sizeof(int(double dbl))];
+
+/* expect+1: error: negative array dimension (-1008) [20] */
+int size_prototype_named_return_pppchar[-1000 - (int)sizeof(char ***(double dbl))];
+
+
+typedef struct {
+	char a[1];
+} a01;
+
+typedef struct {
+	char a[4];
+} a04;
+
+typedef struct {
+	char a[8];
+} a08;
+
+typedef struct {
+	char a[32];
+} a32;
+
+/* expect+2: error: cannot take size/alignment of function type 'function() returning struct typedef a01' [144] */
+/* expect+1: error: negative array dimension (-1000) [20] */
+int unspecified_args_return_01[-1000 - (int)sizeof(a01())];
+/* expect+2: error: cannot take size/alignment of function type 'function() returning struct typedef a04' [144] */
+/* expect+1: error: negative array dimension (-1000) [20] */
+int unspecified_args_return_04[-1000 - (int)sizeof(a04())];
+/* expect+2: error: cannot take size/alignment of function type 'function() returning struct typedef a08' [144] */
+/* expect+1: error: negative array dimension (-1000) [20] */
+int unspecified_args_return_08[-1000 - (int)sizeof(a08())];
+/* expect+2: error: cannot take size/alignment of function type 'function() returning struct typedef a32' [144] */
+/* expect+1: error: negative array dimension (-1000) [20] */
+int unspecified_args_return_32[-1000 - (int)sizeof(a32())];
+
+/* expect+2: error: cannot take size/alignment of void [146] */
+/* expect+1: error: negative array dimension (-1000) [20] */
+int prototype_void_return_01[-1000 - (int)sizeof(a01(void))];
+/* expect+2: error: cannot take size/alignment of void [146] */
+/* expect+1: error: negative array dimension (-1000) [20] */
+int prototype_void_return_04[-1000 - (int)sizeof(a04(void))];
+/* expect+2: error: cannot take size/alignment of void [146] */
+/* expect+1: error: negative array dimension (-1000) [20] */
+int prototype_void_return_08[-1000 - (int)sizeof(a08(void))];
+/* expect+2: error: cannot take size/alignment of void [146] */
+/* expect+1: error: negative array dimension (-1000) [20] */
+int prototype_void_return_32[-1000 - (int)sizeof(a32(void))];
+
+/* expect+1: error: negative array dimension (-1001) [20] */
+int prototype_unnamed_01_return_32[-1000 - (int)sizeof(a32(a01))];
+/* expect+1: error: negative array dimension (-1004) [20] */
+int prototype_unnamed_04_return_32[-1000 - (int)sizeof(a32(a04))];
+/* expect+1: error: negative array dimension (-1008) [20] */
+int prototype_unnamed_08_return_32[-1000 - (int)sizeof(a32(a08))];
+/* expect+2: error: cannot take size/alignment of function type 'function(struct typedef a32) returning struct typedef a32' [144] */
+/* expect+1: error: negative array dimension (-1000) [20] */
+int prototype_unnamed_32_return_32[-1000 - (int)sizeof(a32(a32))];
+
+/* expect+1: error: negative array dimension (-1032) [20] */
+int prototype_unnamed_32_return_01[-1000 - (int)sizeof(a01(a32))];
+/* expect+1: error: negative array dimension (-1032) [20] */
+int prototype_unnamed_32_return_04[-1000 - (int)sizeof(a04(a32))];
+/* expect+1: error: negative array dimension (-1032) [20] */
+int prototype_unnamed_32_return_08[-1000 - (int)sizeof(a08(a32))];
+
+/* expect+1: error: negative array dimension (-1001) [20] */
+int prototype_named_01_return_32[-1000 - (int)sizeof(a32(a01 arg))];
+/* expect+1: error: negative array dimension (-1004) [20] */
+int prototype_named_04_return_32[-1000 - (int)sizeof(a32(a04 arg))];
+/* expect+1: error: negative array dimension (-1008) [20] */
+int prototype_named_08_return_32[-1000 - (int)sizeof(a32(a08 arg))];
+/* expect+2: error: cannot take size/alignment of function type 'function(struct typedef a32) returning struct typedef a32' [144] */
+/* expect+1: error: negative array dimension (-1000) [20] */
+int prototype_named_32_return_32[-1000 - (int)sizeof(a32(a32 arg))];
+
+/* expect+1: error: negative array dimension (-1032) [20] */
+int prototype_named_32_return_01[-1000 - (int)sizeof(a01(a32 arg))];
+/* expect+1: error: negative array dimension (-1032) [20] */
+int prototype_named_32_return_04[-1000 - (int)sizeof(a04(a32 arg))];
+/* expect+1: error: negative array dimension (-1032) [20] */
+int prototype_named_32_return_08[-1000 - (int)sizeof(a08(a32 arg))];
--- tests/usr.bin/xlint/lint1/msg_014.c
+++ tests/usr.bin/xlint/lint1/msg_014.c
@@ -1,11 +1,11 @@
-/*	$NetBSD: msg_014.c,v 1.3 2021/08/26 19:23:25 rillig Exp $	*/
+/*	$NetBSD: msg_014.c,v 1.4 2022/04/01 23:16:32 rillig Exp $	*/
 # 3 "msg_014.c"
 
 // Test for message: compiler takes alignment of function [14]
 
 typedef void function(void);
 
-/* expect+1: error: cannot take size/alignment of function [144] */
+/* expect+1: error: cannot take size/alignment of function type 'function(void) returning void' [144] */
 unsigned long alignof_function = __alignof__(function);
 
 TODO: "Add example code that triggers the above message." /* expect: 249 */
