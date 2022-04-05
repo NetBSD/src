e6d4541762788874639f480d5f8e4c726b18f0be
rillig <rillig@NetBSD.org>
2022-04-05 23:09:19 +0000

    tests/lint: add tests for a few early messages

--- tests/usr.bin/xlint/lint1/msg_026.c
+++ tests/usr.bin/xlint/lint1/msg_026.c
@@ -1,7 +1,8 @@
-/*	$NetBSD: msg_026.c,v 1.2 2021/02/21 09:07:58 rillig Exp $	*/
+/*	$NetBSD: msg_026.c,v 1.3 2022/04/05 23:09:19 rillig Exp $	*/
 # 3 "msg_026.c"
 
 // Test for message: cannot initialize extern declaration: %s [26]
 
-TODO: "Add example code that triggers the above message." /* expect: 249 */
-TODO: "Add example code that almost triggers the above message."
+/* expect+1: warning: cannot initialize extern declaration: used [26] */
+extern int used = 1;
+int defined = 1;
