9e1ddc550b01c74bbc8d1b3f79e466f1f39e2a66
rillig <rillig@NetBSD.org>
2022-04-08 21:48:19 +0000

    lint: fix undefined behavior on enum overflow (since 1995)
    GCC had optimized the check away, due to the obvious integer overflow 'x
    - 1 == INT_MAX'.  To prevent further bugs like this, compile with
    -ftrapv.

--- tests/usr.bin/xlint/lint1/msg_048.c
+++ tests/usr.bin/xlint/lint1/msg_048.c
@@ -1,10 +1,10 @@
-/*	$NetBSD: msg_048.c,v 1.3 2022/04/08 21:29:29 rillig Exp $	*/
+/*	$NetBSD: msg_048.c,v 1.4 2022/04/08 21:48:19 rillig Exp $	*/
 # 3 "msg_048.c"
 
 // Test for message: overflow in enumeration values: %s [48]
 
 /*
- * Before decl.c 1.TODO from 2022-04-TODO, the comparison for enum constant
+ * Before decl.c 1.269 from 2022-04-08, the comparison for enum constant
  * overflow was done in signed arithmetic, and since 'enumval' wrapped
  * around, its value became INT_MIN, at least on platforms where integer
  * overflow was defined as 2-complements wrap-around.  When comparing
@@ -16,9 +16,7 @@ enum int_limits {
 	MAX_MINUS_2 = 0x7ffffffd,
 	MAX_MINUS_1,
 	MAX,
-	/* TODO: expect: overflow in enumeration values: MIN */
-	MIN
+	/* expect+1: overflow in enumeration values: MIN [48] */
+	MIN,
+	MIN_PLUS_1
 };
-
-TODO: "Add example code that triggers the above message." /* expect: 249 */
-TODO: "Add example code that almost triggers the above message."
