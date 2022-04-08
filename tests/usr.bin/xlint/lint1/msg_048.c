a9c16fb6530ed46d28e8755e916e1e5141964700
rillig <rillig@NetBSD.org>
2022-04-08 21:29:29 +0000

    lint: remove unused message 70, add some more tests

--- tests/usr.bin/xlint/lint1/msg_048.c
+++ tests/usr.bin/xlint/lint1/msg_048.c
@@ -1,7 +1,24 @@
-/*	$NetBSD: msg_048.c,v 1.2 2021/02/21 09:07:58 rillig Exp $	*/
+/*	$NetBSD: msg_048.c,v 1.3 2022/04/08 21:29:29 rillig Exp $	*/
 # 3 "msg_048.c"
 
 // Test for message: overflow in enumeration values: %s [48]
+
+/*
+ * Before decl.c 1.TODO from 2022-04-TODO, the comparison for enum constant
+ * overflow was done in signed arithmetic, and since 'enumval' wrapped
+ * around, its value became INT_MIN, at least on platforms where integer
+ * overflow was defined as 2-complements wrap-around.  When comparing
+ * 'enumval - 1 == TARG_INT_MAX', there was another integer overflow, and
+ * this one was optimized away by GCC, taking advantage of the undefined
+ * behavior.
+ */
+enum int_limits {
+	MAX_MINUS_2 = 0x7ffffffd,
+	MAX_MINUS_1,
+	MAX,
+	/* TODO: expect: overflow in enumeration values: MIN */
+	MIN
+};
 
 TODO: "Add example code that triggers the above message." /* expect: 249 */
 TODO: "Add example code that almost triggers the above message."
