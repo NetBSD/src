c91f4bf8ddb1d85437cc035dbc9bae9d84f5ce81
rillig <rillig@NetBSD.org>
2022-04-23 09:01:03 +0000

    tests/indent: migrate a few token tests to psym tests

--- tests/usr.bin/indent/psym_if_expr_stmt.c
+++ tests/usr.bin/indent/psym_if_expr_stmt.c
@@ -1,14 +1,27 @@
-/* $NetBSD: psym_if_expr_stmt.c,v 1.2 2022/04/22 21:21:20 rillig Exp $ */
+/* $NetBSD: psym_if_expr_stmt.c,v 1.3 2022/04/23 09:01:03 rillig Exp $ */
 
 /*
  * Tests for the parser symbol psym_if_expr_stmt, which represents the state
  * after reading the keyword 'if', the controlling expression and the
- * statement for the 'then' branch.  If the next token is 'else', another
- * statement will follow, otherwise the 'if' statement is finished already.
+ * statement for the 'then' branch.
+ *
+ * At this point, the 'if' statement is not necessarily complete, it can be
+ * completed with the keyword 'else' followed by a statement.
+ *
+ * Any token other than 'else' completes the 'if' statement.
  */
 
 #indent input
-// TODO: add input
+void
+function(void)
+{
+	if (cond)
+		stmt();
+	if (cond)
+		stmt();
+	else			/* belongs to the second 'if' */
+		stmt();
+}
 #indent end
 
 #indent run-equals-input
