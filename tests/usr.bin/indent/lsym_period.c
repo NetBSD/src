61c903eeb04c0309884dd6c06a0ac6c3d3ba1cce
rillig <rillig@NetBSD.org>
2022-04-23 09:59:13 +0000

    tests/indent: migrate tests for the tokens '{' and '.'

--- tests/usr.bin/indent/lsym_period.c
+++ tests/usr.bin/indent/lsym_period.c
@@ -1,4 +1,4 @@
-/* $NetBSD: lsym_period.c,v 1.2 2022/04/22 21:21:20 rillig Exp $ */
+/* $NetBSD: lsym_period.c,v 1.3 2022/04/23 09:59:14 rillig Exp $ */
 
 /*
  * Tests for the token lsym_period, which represents '.' in these contexts:
@@ -9,14 +9,57 @@
  * or union 'sou'.
  *
  * In a function prototype declaration, the sequence '.' '.' '.' marks the
- * start of a variable number of arguments.
+ * start of a variable number of arguments.  It would have been more intuitive
+ * to model them as a single token, but it doesn't make any difference for
+ * formatting the code.
  *
  * See also:
  *	lsym_word.c		for '.' inside numeric constants
  */
 
+/* Designators in an initialization */
 #indent input
-// TODO: add input
+struct point {
+	int x;
+	int y;
+} p = {
+	.x = 3,
+	.y = 4,
+};
 #indent end
 
-#indent run-equals-input
+#indent run-equals-input -di0
+
+
+/* Accessing struct members */
+#indent input
+time_t
+get_time(struct stat st)
+{
+	return st.st_mtime > 0 ? st . st_atime : st.st_ctime;
+}
+#indent end
+
+#indent run
+time_t
+/* $ FIXME: The '{' must be in the next line. */
+get_time(struct stat st){
+	return st.st_mtime > 0 ? st.st_atime : st.st_ctime;
+}
+#indent end
+
+#indent run -Ttime_t
+time_t
+get_time(struct stat st)
+{
+	return st.st_mtime > 0 ? st.st_atime : st.st_ctime;
+}
+#indent end
+
+
+/* Varargs in a function declaration */
+#indent input
+void my_printf(const char *, ...);
+#indent end
+
+#indent run-equals-input -di0
