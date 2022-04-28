8ab6d3ca9321866e5b65f9b52e04bddbddd7b76e
rillig <rillig@NetBSD.org>
2022-04-28 21:38:38 +0000

    lint: revert resolving grammar conflicts for labeled statements
    Restore the grammar rule for labeled_statement as it was before cgram.y
    1.400 from 2022-04-24.  This allows labels with attributes again.  Fix
    the wrong interpretation in the tests; the attributes belong to the
    label, not to the statement.
    
    Today in the morning, when I thought that the change in cgram.y 1.400
    were innocent, I accidentally ran lint only with the options '-Sw' but
    forgot the option '-g' for GNU mode.  Without that option, the token
    '__attribute__' is unknown, which unsurprisingly leads to lots of syntax
    errors, and these didn't change with that commit.  The actual change was
    only visible in GNU mode.

--- tests/usr.bin/xlint/lint1/gcc_attribute_label.c
+++ tests/usr.bin/xlint/lint1/gcc_attribute_label.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: gcc_attribute_label.c,v 1.2 2021/07/11 19:24:42 rillig Exp $	*/
+/*	$NetBSD: gcc_attribute_label.c,v 1.3 2022/04/28 21:38:38 rillig Exp $	*/
 # 3 "gcc_attribute_label.c"
 
 /*
@@ -22,4 +22,28 @@ hot:
 	__attribute__((__hot__));
 	if (i < 0)
 		goto error;
+}
+
+/* GCC allows a label to be marked as (possibly) unused. */
+void
+unused_labels(int x)
+{
+	switch (x) {
+	case 3:
+		__attribute__((__unused__))
+		break;
+	case 4:
+		goto label;
+	label:
+		__attribute__((__unused__))
+		return;
+	}
+
+	/*
+	 * The GCC attributes may only occur after a label; they cannot occur
+	 * before an arbitrary statement.
+	 */
+	__attribute__((__unused__))
+	/* expect+1: error: syntax error 'return' [249] */
+	return;
 }
