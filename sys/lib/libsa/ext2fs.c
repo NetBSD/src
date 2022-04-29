c87085357cd6b552fbc5eaa1a747449e77503c61
rin <rin@NetBSD.org>
2022-04-29 07:42:07 +0000

    Re-introduce SA_HARDCODED_SECSIZE hack, by which hardcoded DEV_BSIZE is used instead of secsize obtained by SAIOSECSIZE ioctl.
    This hack avoids divdi3 and friends from being linked, in order to
    support variable secsize for some archs.
    
    Should be useful for ancient archs, for which secsize is fixed.
    
    Thanks christos@ for comment.

--- sys/lib/libsa/ext2fs.c
+++ sys/lib/libsa/ext2fs.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: ext2fs.c,v 1.33 2022/04/27 14:48:50 rin Exp $	*/
+/*	$NetBSD: ext2fs.c,v 1.34 2022/04/29 07:42:07 rin Exp $	*/
 
 /*
  * Copyright (c) 1997 Manuel Bouyer.
@@ -415,15 +415,9 @@ read_sblock(struct open_file *f, struct m_ext2fs *fs)
 	struct ext2fs ext2fs;
 	size_t buf_size;
 	int rc;
-	u_int secsize;
-
-	secsize = 0;
-	rc = DEV_IOCTL(f->f_dev)(f, SAIOSECSIZE, &secsize);
-	if (rc != 0 || secsize == 0)
-		secsize = DEV_BSIZE;
 
 	rc = DEV_STRATEGY(f->f_dev)(f->f_devdata, F_READ,
-	    SBOFF / secsize, SBSIZE, sbbuf, &buf_size);
+	    SBOFF / GETSECSIZE(f), SBSIZE, sbbuf, &buf_size);
 	if (rc)
 		return rc;
 
