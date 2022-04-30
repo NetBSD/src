1977c5d80ed847eeaea7c25681e6d1720871c6bd
pgoyette <pgoyette@NetBSD.org>
2022-04-30 17:09:44 +0000

    Don't put temporary files and directories into the atf_srcdir (that might be read-only!).  Instead, put them in the test's working directory what the atf framework will automatically clean up after the test completes.

--- tests/lib/libc/kevent_nullmnt/t_nullmnt.sh
+++ tests/lib/libc/kevent_nullmnt/t_nullmnt.sh
@@ -6,27 +6,27 @@ nullmnt_head()
 
 nullmnt_body()
 {    
-	srcdir=$(atf_get_srcdir)
-	helper=${srcdir}/h_nullmnt
+	curdir=$(pwd)
+	helper=$(atf_get_srcdir)/h_nullmnt
 
-	mkdir ${srcdir}/realdir
-	mkdir ${srcdir}/nulldir
-	mount -t null ${srcdir}/realdir ${srcdir}/nulldir
-	rm -f ${srcdir}/realdir/afile
-	touch ${srcdir}/realdir/afile
+	mkdir ${curdir}/realdir
+	mkdir ${curdir}/nulldir
+	mount -t null ${curdir}/realdir ${curdir}/nulldir
+	rm -f ${curdir}/realdir/afile
+	touch ${curdir}/realdir/afile
 
 	atf_expect_fail "PR kern/56713"
 
 	atf_check -e ignore -o ignore -s exit:0 \
-		${helper} ${srcdir}/realdir/afile ${srcdir}/nulldir/afile
+		${helper} ${curdir}/realdir/afile ${curdir}/nulldir/afile
 
 } 
 
 nullmnt_cleanup()
 {
-	srcdir=$(atf_get_srcdir)
-	umount ${srcdir}/nulldir
-	rm -rf ${srcdir}/realdir ${srcdir}/nulldir
+	curdir=$(pwd)
+	umount ${curdir}/nulldir
+	rm -rf ${curdir}/realdir ${curdir}/nulldir
 }
 
 atf_init_test_cases()
