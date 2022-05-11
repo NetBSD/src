86e567be2eec5216b0b5188683adc8421f8a19d7
christos <christos@NetBSD.org>
2022-05-11 15:46:25 +0000

    gcc also allows __alignof

--- usr.bin/xlint/lint1/lex.c
+++ usr.bin/xlint/lint1/lex.c
@@ -1,4 +1,4 @@
-/* $NetBSD: lex.c,v 1.128 2022/04/30 22:31:23 rillig Exp $ */
+/* $NetBSD: lex.c,v 1.129 2022/05/11 15:46:25 christos Exp $ */
 
 /*
  * Copyright (c) 1996 Christopher G. Demetriou.  All Rights Reserved.
@@ -38,7 +38,7 @@
 
 #include <sys/cdefs.h>
 #if defined(__RCSID) && !defined(lint)
-__RCSID("$NetBSD: lex.c,v 1.128 2022/04/30 22:31:23 rillig Exp $");
+__RCSID("$NetBSD: lex.c,v 1.129 2022/05/11 15:46:25 christos Exp $");
 #endif
 
 #include <ctype.h>
@@ -114,7 +114,7 @@ static const struct keyword {
 } keywords[] = {
 	kwdef_keyword(	"_Alignas",	T_ALIGNAS),
 	kwdef_keyword(	"_Alignof",	T_ALIGNOF),
-	kwdef_token(	"__alignof__",	T_ALIGNOF,		78,0,1),
+	kwdef_token(	"alignof",	T_ALIGNOF,		78,0,3),
 	kwdef_token(	"asm",		T_ASM,			78,1,7),
 	kwdef_token(	"attribute",	T_ATTRIBUTE,		78,1,6),
 	kwdef_sclass(	"auto",		AUTO,			78,0,1),
