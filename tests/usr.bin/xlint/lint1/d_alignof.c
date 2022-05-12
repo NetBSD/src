6300d02a6363652b2951831781123b873b2140f2
rillig <rillig@NetBSD.org>
2022-05-12 00:18:35 +0000

    lint: fix support for __alignof__, add support for __alignof
    Broken since lex.c 1.129 from yesterday.

--- tests/usr.bin/xlint/lint1/d_alignof.c
+++ tests/usr.bin/xlint/lint1/d_alignof.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: d_alignof.c,v 1.3 2022/05/12 00:09:44 rillig Exp $	*/
+/*	$NetBSD: d_alignof.c,v 1.4 2022/05/12 00:18:35 rillig Exp $	*/
 # 3 "d_alignof.c"
 
 /* https://gcc.gnu.org/onlinedocs/gcc/Alignment.html */
@@ -6,12 +6,8 @@
 unsigned long
 leading_and_trailing_alignof_type(void)
 {
-	/* FIXME: '__alignof__' must be recognized. */
-	/* expect+2: error: function '__alignof__' implicitly declared to return int [215] */
-	/* expect+1: error: syntax error 'short' [249] */
 	return __alignof__(short);
 }
-/* expect-1: warning: function leading_and_trailing_alignof_type falls off bottom without returning value [217] */
 
 unsigned long
 leading_alignof_type(void)
@@ -23,16 +19,17 @@ unsigned long
 plain_alignof_type(void)
 {
 	/* The plain word 'alignof' is not recognized by GCC. */
-	/* FIXME: plain 'alignof' is not defined by GCC. */
+	/* expect+2: error: function 'alignof' implicitly declared to return int [215] */
+	/* expect+1: error: syntax error 'short' [249] */
 	return alignof(short);
 }
+/* expect-1: warning: function plain_alignof_type falls off bottom without returning value [217] */
 
 unsigned long
 leading_and_trailing_alignof_expr(void)
 {
 	/* FIXME: '__alignof__' must be recognized. */
 	/* FIXME: '__alignof__ expr' must be recognized. */
-	/* expect+2: error: '__alignof__' undefined [99] */
 	/* expect+1: error: syntax error '3' [249] */
 	return __alignof__ 3;
 }
@@ -51,6 +48,7 @@ unsigned long
 plain_alignof_expr(void)
 {
 	/* The plain word 'alignof' is not recognized by GCC. */
+	/* expect+2: error: 'alignof' undefined [99] */
 	/* expect+1: error: syntax error '3' [249] */
 	return alignof 3;
 }
