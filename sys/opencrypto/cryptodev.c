9f36b9ed4c18984051dcd3ec3a0cccbcf578fbde
riastradh <riastradh@NetBSD.org>
2022-05-18 20:02:49 +0000

    crypto(4): Use IPL_NONE, not IPL_NET, for /dev/crypto pools.
    These are used (pool_get/put) only from thread context, never from
    interrupt or even soft interrupt context.

--- sys/opencrypto/cryptodev.c
+++ sys/opencrypto/cryptodev.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: cryptodev.c,v 1.108 2022/05/17 09:53:09 riastradh Exp $ */
+/*	$NetBSD: cryptodev.c,v 1.109 2022/05/18 20:02:49 riastradh Exp $ */
 /*	$FreeBSD: src/sys/opencrypto/cryptodev.c,v 1.4.2.4 2003/06/03 00:09:02 sam Exp $	*/
 /*	$OpenBSD: cryptodev.c,v 1.53 2002/07/10 22:21:30 mickey Exp $	*/
 
@@ -64,7 +64,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: cryptodev.c,v 1.108 2022/05/17 09:53:09 riastradh Exp $");
+__KERNEL_RCSID(0, "$NetBSD: cryptodev.c,v 1.109 2022/05/18 20:02:49 riastradh Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -2118,9 +2118,9 @@ cryptoattach(int num)
 	mutex_init(&cryptodev_mtx, MUTEX_DEFAULT, IPL_NONE);
 
 	pool_init(&fcrpl, sizeof(struct fcrypt), 0, 0, 0, "fcrpl",
-	    NULL, IPL_NET);	/* XXX IPL_NET ("splcrypto") */
+	    NULL, IPL_NONE);
 	pool_init(&csepl, sizeof(struct csession), 0, 0, 0, "csepl",
-	    NULL, IPL_NET);	/* XXX IPL_NET ("splcrypto") */
+	    NULL, IPL_NONE);
 
 	/*
 	 * Preallocate space for 64 users, with 5 sessions each.
