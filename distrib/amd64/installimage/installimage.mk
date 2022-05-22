d3cd82a80036b32f2a71ccbbf93cd0cedab56e31
jmcneill <jmcneill@NetBSD.org>
2022-05-22 19:32:41 +0000

    Restore Makefile.installimage include after variables are set. Makes amd64 USB install images bootable again.

--- distrib/amd64/installimage/installimage.mk
+++ distrib/amd64/installimage/installimage.mk
@@ -1,4 +1,4 @@
-#	$NetBSD: installimage.mk,v 1.1 2022/05/08 08:12:45 mrg Exp $
+#	$NetBSD: installimage.mk,v 1.2 2022/05/22 19:32:41 jmcneill Exp $
 
 # common code between distrib/amd64/installimage/Makefile and
 # distrib/amd64/installimage-bios/Makefile.
@@ -29,5 +29,3 @@ IMGFILE_EXTRA=								\
 	${.CURDIR}/etc.rc		etc/rc				\
 	${.CURDIR}/install.sh		.				\
 	${.OBJDIR}/boot.cfg		.
-
-.include "${NETBSDSRCDIR}/distrib/common/bootimage/Makefile.installimage"
