4c5c3727fe33ba81ab0947d9209f2c6e11b19eb2
rillig <rillig@NetBSD.org>
2022-05-22 13:53:39 +0000

    tests/lint: demonstrate wrong warning for switch on enum

--- tests/usr.bin/xlint/lint1/msg_206.c
+++ tests/usr.bin/xlint/lint1/msg_206.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: msg_206.c,v 1.4 2021/07/08 18:53:57 rillig Exp $	*/
+/*	$NetBSD: msg_206.c,v 1.5 2022/05/22 13:53:39 rillig Exp $	*/
 # 3 "msg_206.c"
 
 // Test for message: enumeration value(s) not handled in switch [206]
@@ -25,4 +25,22 @@ test(enum number num)
 	case THREE:
 		break;
 	}
+}
+
+int
+too_many(enum number num)
+{
+	switch (num) {
+	case ONE:
+		return 1;
+	case TWO:
+		return 2;
+	case THREE:
+		return 3;
+	case 3:
+		return -1;
+	}
+	/* FIXME: There are _too many_ branches, not _too few_. */
+	/* expect-2: warning: enumeration value(s) not handled in switch [206] */
+	return 3;
 }
