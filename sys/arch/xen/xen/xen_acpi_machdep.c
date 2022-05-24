021b40005718fcb9e987e18fcaf14a999a222942
bouyer <bouyer@NetBSD.org>
2022-05-24 17:06:08 +0000

    PR kern/56853: add a dummy acpi_md_vbios_reset variable so that we can again enable vga at pci for Xen. Note that this only works in BIOS mode, not UEFI mode.

--- sys/arch/xen/xen/xen_acpi_machdep.c
+++ sys/arch/xen/xen/xen_acpi_machdep.c
@@ -1,14 +1,16 @@
-/*	$NetBSD: xen_acpi_machdep.c,v 1.5 2009/08/18 16:41:03 jmcneill Exp $	*/
+/*	$NetBSD: xen_acpi_machdep.c,v 1.6 2022/05/24 17:06:08 bouyer Exp $	*/
 
 #include "acpica.h"
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: xen_acpi_machdep.c,v 1.5 2009/08/18 16:41:03 jmcneill Exp $");
+__KERNEL_RCSID(0, "$NetBSD: xen_acpi_machdep.c,v 1.6 2022/05/24 17:06:08 bouyer Exp $");
 
 #include <dev/acpi/acpica.h>
 #include <dev/acpi/acpivar.h>
 #define ACPI_MACHDEP_PRIVATE
 #include <machine/acpi_machdep.h>
+
+int acpi_md_vbios_reset = 0;
 
 int
 acpi_md_sleep(int state)
