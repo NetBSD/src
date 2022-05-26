5fcdccd07445f9a17e12c8979b530b45bbee6b4c
tsutsui <tsutsui@NetBSD.org>
2022-05-26 14:28:56 +0000

    Make readonly keymap data const.

--- sys/arch/x68k/dev/kbdmap.h
+++ sys/arch/x68k/dev/kbdmap.h
@@ -1,9 +1,10 @@
-/*	$NetBSD: kbdmap.h,v 1.4 2005/12/11 12:19:37 christos Exp $	*/
+/*	$NetBSD: kbdmap.h,v 1.5 2022/05/26 14:28:56 tsutsui Exp $	*/
 
 #include <machine/kbdmap.h>
 
 #ifdef _KERNEL
 /* XXX: ITE interface */
-extern struct kbdmap kbdmap, ascii_kbdmap;
+extern struct kbdmap kbdmap;
+extern const struct kbdmap ascii_kbdmap;
 extern unsigned char acctable[KBD_NUM_ACC][64];
 #endif
