16d1b77db70db66bc68f591ae757fa904761712f
martin <martin@NetBSD.org>
2022-06-10 17:34:22 +0000

    Pull up following revision(s) (requested by skrll in ticket #1468):
    	sys/arch/hppa/hppa/trap.c: revision 1.120
    	sys/arch/hppa/include/ptrace.h: revision 1.11-1.12 (via patch)
    
    Define a PTRACE_ILLEGAL_ASM
    
    Match up PTRACE_BREAKPOINT_ASM with PTRACE_BREAKPOINT which is the
    gdb breakpoint instruction.
    
    Only report the SSBREAKPOINT break instruction as SIGTRAP/TRAP_TRACE. All
    other break instructions will be reported as SIGTRAP/TRAP_BRKPT
    This fixes a mistake I made back in 2008.
    
    PR/56866: hppa: kernel gets confused between actual breakpoints and
    single-step breakpoints

--- sys/arch/hppa/include/ptrace.h
+++ sys/arch/hppa/include/ptrace.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: ptrace.h,v 1.9 2019/06/18 21:18:12 kamil Exp $	*/
+/*	$NetBSD: ptrace.h,v 1.9.2.1 2022/06/10 17:34:22 martin Exp $	*/
 
 /*	$OpenBSD: ptrace.h,v 1.2 1998/12/01 03:05:44 mickey Exp $	*/
 
@@ -58,6 +58,11 @@
 #define PTRACE_REG_SP(r)	(r)->r_regs[30]
 #define PTRACE_REG_INTRV(r)	(r)->r_regs[28]
 
+#define HPPA_BREAK_GDB		4
+#define HPPA_BREAK_GDB_SS	8
+
 #define PTRACE_BREAKPOINT	((const uint8_t[]) { 0x00, 0x01, 0x00, 0x04 })
-#define PTRACE_BREAKPOINT_ASM	__asm __volatile("break	%0, %1" :: "i" (HPPA_BREAK_KERNEL), "i" (HPPA_BREAK_SS) : "memory")
+#define PTRACE_BREAKPOINT_ASM	__asm __volatile("break	%0, %1" :: "i" (HPPA_BREAK_GDB), "i" (HPPA_BREAK_GDB_SS) : "memory")
 #define PTRACE_BREAKPOINT_SIZE	4
+
+#define PTRACE_ILLEGAL_ASM	__asm __volatile (".word 0x000ff000" : : : "memory")
