b046ace8902fa84322e351c4a5299d3f2420fc22
rillig <rillig@NetBSD.org>
2022-06-11 11:20:40 +0000

    tests/lint: add tests for messages that lack quotes

--- tests/usr.bin/xlint/lint1/msg_307.c
+++ tests/usr.bin/xlint/lint1/msg_307.c
@@ -1,7 +1,15 @@
-/*	$NetBSD: msg_307.c,v 1.2 2021/02/21 09:07:58 rillig Exp $	*/
+/*	$NetBSD: msg_307.c,v 1.3 2022/06/11 11:20:40 rillig Exp $	*/
 # 3 "msg_307.c"
 
 // Test for message: static variable %s set but not used [307]
 
-TODO: "Add example code that triggers the above message." /* expect: 249 */
-TODO: "Add example code that almost triggers the above message."
+/* expect+1: warning: static variable set_but_not_used set but not used [307] */
+static int set_but_not_used;
+
+static int only_incremented;
+
+void function(void)
+{
+	set_but_not_used = 3;
+	only_incremented++;
+}
