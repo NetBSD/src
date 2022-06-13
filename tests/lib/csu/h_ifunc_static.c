498e658a25d14650300362476ebdc854f685c304
martin <martin@NetBSD.org>
2022-06-13 12:55:06 +0000

    Restore arm in the list of architectures supporting ifunc, accidently lost in the OABI removal.

--- tests/lib/csu/h_ifunc_static.c
+++ tests/lib/csu/h_ifunc_static.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: h_ifunc_static.c,v 1.4 2021/06/07 17:11:17 christos Exp $	*/
+/*	$NetBSD: h_ifunc_static.c,v 1.5 2022/06/13 12:55:06 martin Exp $	*/
 
 /*-
  * Copyright (c) 2018 The NetBSD Foundation, Inc.
@@ -29,7 +29,8 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
-#if defined(__i386__) || defined(__x86_64__) || defined(__powerpc__) || defined(__sparc__)
+#if defined(__arm__) || defined(__i386__) || \
+	defined(__x86_64__) || defined(__powerpc__) || defined(__sparc__)
 #include <stdlib.h>
 #include <string.h>
 
