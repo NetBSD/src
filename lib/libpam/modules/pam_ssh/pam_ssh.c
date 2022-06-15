53bf217fde05f5a5bc96672a2abffa534923fef0
hannken <hannken@NetBSD.org>
2022-06-15 08:31:34 +0000

    Set provider to NULL -- "pam" is not a valid security key helper library. Now ssh-agent no longer fails key addition with
        error: Cannot add provider: RSA is not an authenticator-hosted key

--- lib/libpam/modules/pam_ssh/pam_ssh.c
+++ lib/libpam/modules/pam_ssh/pam_ssh.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: pam_ssh.c,v 1.29 2022/02/24 00:26:26 christos Exp $	*/
+/*	$NetBSD: pam_ssh.c,v 1.30 2022/06/15 08:31:34 hannken Exp $	*/
 
 /*-
  * Copyright (c) 2003 Networks Associates Technology, Inc.
@@ -38,7 +38,7 @@
 #ifdef __FreeBSD__
 __FBSDID("$FreeBSD: src/lib/libpam/modules/pam_ssh/pam_ssh.c,v 1.40 2004/02/10 10:13:21 des Exp $");
 #else
-__RCSID("$NetBSD: pam_ssh.c,v 1.29 2022/02/24 00:26:26 christos Exp $");
+__RCSID("$NetBSD: pam_ssh.c,v 1.30 2022/06/15 08:31:34 hannken Exp $");
 #endif
 
 #include <sys/param.h>
@@ -68,7 +68,7 @@ __RCSID("$NetBSD: pam_ssh.c,v 1.29 2022/02/24 00:26:26 christos Exp $");
 #include "authfile.h"
 
 #define ssh_add_identity(auth, key, comment) \
-    ssh_add_identity_constrained(auth, key, comment, 0, 0, 0, "pam", NULL, 00)
+    ssh_add_identity_constrained(auth, key, comment, 0, 0, 0, NULL, NULL, 00)
 
 extern char **environ;
 
