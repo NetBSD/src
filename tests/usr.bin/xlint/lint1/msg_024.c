908cc94ba14cd90d2d3a2102f26b7c86828198c0
rillig <rillig@NetBSD.org>
2022-06-19 11:50:42 +0000

    tests/lint: add quotes around placeholders in a few messages

--- tests/usr.bin/xlint/lint1/msg_024.c
+++ tests/usr.bin/xlint/lint1/msg_024.c
@@ -1,7 +1,7 @@
-/*	$NetBSD: msg_024.c,v 1.3 2021/08/27 20:16:50 rillig Exp $	*/
+/*	$NetBSD: msg_024.c,v 1.4 2022/06/19 11:50:42 rillig Exp $	*/
 # 3 "msg_024.c"
 
-// Test for message: cannot initialize function: %s [24]
+// Test for message: cannot initialize function '%s' [24]
 
 typedef void (function)(void);
 
@@ -10,7 +10,7 @@ definition(void)
 {
 }
 
-/* expect+3: error: cannot initialize function: fn [24] */
+/* expect+3: error: cannot initialize function 'fn' [24] */
 /* The following message is strange but does not occur in practice. */
 /* expect+1: error: {}-enclosed initializer required [181] */
 function fn = definition;
