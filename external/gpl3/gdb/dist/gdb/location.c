37d124bb90e55e6ca2a31d88d40f6b43be97ebfb
hgutch <hgutch@NetBSD.org>
2022-06-25 17:46:05 +0000

    Cherry-picking two upstream commits[1,2] to fix tools build under gcc 12 if MKCROSSGDB is set.  Without these, gcc 12 correctly points out that certain checkr for pointers being NULL are always false and errors out due to -Werror=nonnull-compare/-Werror=address (implicitly set by -Wall).
    Build failure reported by Piyush Sachdeva.
    
    [1] https://sourceware.org/git/?p=binutils-gdb.git;a=commitdiff;h=fb6262e8534e0148a4a424e9e5138159af19faf1;hp=f681e5867de63f1c8ca692023cf86e4c884fdae7
    [2] https://sourceware.org/git/?p=binutils-gdb.git;a=commitdiff;h=da7ee7f9ce2fc8c278a46e0b360d44319a5a1e7a;hp=cb2e519a5e41052a4dd55be4f1c4d818d2e8af9d

--- external/gpl3/gdb/dist/gdb/location.c
+++ external/gpl3/gdb/dist/gdb/location.c
@@ -960,12 +960,11 @@ event_location_empty_p (const struct event_location *location)
       return 0;
 
     case EXPLICIT_LOCATION:
-      return (EL_EXPLICIT (location) == NULL
-	      || (EL_EXPLICIT (location)->source_filename == NULL
-		  && EL_EXPLICIT (location)->function_name == NULL
-		  && EL_EXPLICIT (location)->label_name == NULL
-		  && (EL_EXPLICIT (location)->line_offset.sign
-		      == LINE_OFFSET_UNKNOWN)));
+      return (EL_EXPLICIT (location)->source_filename == NULL
+	      && EL_EXPLICIT (location)->function_name == NULL
+	      && EL_EXPLICIT (location)->label_name == NULL
+	      && (EL_EXPLICIT (location)->line_offset.sign
+		  == LINE_OFFSET_UNKNOWN));
 
     case PROBE_LOCATION:
       return EL_PROBE (location) == NULL;
