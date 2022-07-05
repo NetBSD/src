aa24dc9f8b7a64d471581c07461fb408a23dd96d
tsutsui <tsutsui@NetBSD.org>
2022-07-05 15:32:45 +0000

    Remove unnecessary and incorrect mulitple inclusion protection.

--- sys/arch/luna68k/include/aout_machdep.h
+++ sys/arch/luna68k/include/aout_machdep.h
@@ -1,8 +1,3 @@
-/*	$NetBSD: aout_machdep.h,v 1.1 2000/01/05 08:49:00 nisimura Exp $	*/
-
-#ifndef _MACHINE_EXEC_H_
-#define _MACHINE_EXEC_H_
+/*	$NetBSD: aout_machdep.h,v 1.2 2022/07/05 15:32:45 tsutsui Exp $	*/
 
 #include <m68k/aout_machdep.h>
-
-#endif  /* _MACHINE_EXEC_H_ */
