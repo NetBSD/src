a1623e3d2156c716fc4096f0c846277fa4b29c39
martin <martin@NetBSD.org>
2022-08-03 10:58:58 +0000

    Pull up following revision(s) (requested by riastradh in ticket #1486):
    	sys/compat/netbsd32/netbsd32_nfssvc.c: revision 1.7
    
    compat_netbsd32: Copy out 32-bit version in nfssvc32_nsd_out.

--- sys/compat/netbsd32/netbsd32_nfssvc.c
+++ sys/compat/netbsd32/netbsd32_nfssvc.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: netbsd32_nfssvc.c,v 1.6 2019/01/27 02:08:40 pgoyette Exp $	*/
+/*	$NetBSD: netbsd32_nfssvc.c,v 1.6.4.1 2022/08/03 10:58:58 martin Exp $	*/
 
 /*
  * Copyright (c) 2015 Matthew R. Green
@@ -29,7 +29,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: netbsd32_nfssvc.c,v 1.6 2019/01/27 02:08:40 pgoyette Exp $");
+__KERNEL_RCSID(0, "$NetBSD: netbsd32_nfssvc.c,v 1.6.4.1 2022/08/03 10:58:58 martin Exp $");
 
 #if defined(_KERNEL_OPT)
 #include "opt_nfs.h"
@@ -134,7 +134,7 @@ nfssvc32_nsd_out(void *argp, const struct nfsd_srvargs *nsd)
 	args32.nsd_key[0] = nsd->nsd_key[0];
 	args32.nsd_key[1] = nsd->nsd_key[1];
 
-	return copyout(nsd, argp, sizeof *nsd);
+	return copyout(&args32, argp, sizeof args32);
 }
 
 static int
