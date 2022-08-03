036dd73a6b12167bafa27f39f143b3fcedec30d7
martin <martin@NetBSD.org>
2022-08-03 10:42:02 +0000

    Pull up following revision(s) (requested by wiz in ticket #1484):
    	lib/libc/sys/clone.2: revision 1.14
    	lib/libc/sys/clone.2: revision 1.15
    	include/sched.h: revision 1.13
    	include/sched.h: revision 1.14
    	include/sched.h: revision 1.15
    
    Remove mentions of CLONE_PID and CLONE_STOPPED
    
    CLONE_PID was removed in Linux 2.5.15 and recycled for
    CLONE_PIDFD since Linux 5.2.
    CLONE_STOPPED was removed in Linux 2.6.38 and recycled
    for CLONE_NEWCGROUP since Linux 4.6.
    
    sched.h: Linux documents clone and __clone as only available
    when_GNU_SOURCE is defined - follow suit.
    Ok martin@
    
    clone(2): document that _GNU_SOURCE must be defined for the prototypes
    sched.h: format comment to follow KNF
    requested by thorpej@
    sched.h: keep __clone() visible under _NETBSD_SOURCE

--- include/sched.h
+++ include/sched.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: sched.h,v 1.12 2009/01/11 03:04:12 christos Exp $	*/
+/*	$NetBSD: sched.h,v 1.12.52.1 2022/08/03 10:42:02 martin Exp $	*/
 
 /*-
  * Copyright (c) 2001 The NetBSD Foundation, Inc.
@@ -59,20 +59,32 @@ __END_DECLS
 #define sched_yield		__libc_thr_yield
 #endif /* __LIBPTHREAD_SOURCE__ */
 
-#if defined(_NETBSD_SOURCE)
-
 __BEGIN_DECLS
+
+#if defined(_NETBSD_SOURCE)
 
 /* Process affinity functions (not portable) */
 int	sched_getaffinity_np(pid_t, size_t, cpuset_t *);
 int	sched_setaffinity_np(pid_t, size_t, cpuset_t *);
 
-/* Historical functions, not defined in standard */
+#endif /* _NETBSD_SOURCE */
+
+#if defined(_GNU_SOURCE)
+
+/*
+ * Historical functions, not defined in standard
+ * Linux man page documents clone() as only available when
+ * _GNU_SOURCE is defined
+ */
 pid_t	 clone(int (*)(void *), void *, int, void *);
+#endif /* _GNU_SOURCE */
+
+#if defined(_NETBSD_SOURCE)
+
 pid_t	__clone(int (*)(void *), void *, int, void *);
 
-__END_DECLS
-
 #endif /* _NETBSD_SOURCE */
+
+__END_DECLS
 
 #endif /* _SCHED_H_ */
