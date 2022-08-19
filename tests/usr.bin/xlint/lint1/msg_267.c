9b3d4b52677c57a7ef2ee840ee58825e482b6f6b
rillig <rillig@NetBSD.org>
2022-08-19 19:13:04 +0000

    tests/lint: demonstrate wrong warning about bit-shift

--- tests/usr.bin/xlint/lint1/msg_267.c
+++ tests/usr.bin/xlint/lint1/msg_267.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: msg_267.c,v 1.4 2022/06/16 21:24:41 rillig Exp $	*/
+/*	$NetBSD: msg_267.c,v 1.5 2022/08/19 19:13:04 rillig Exp $	*/
 # 3 "msg_267.c"
 
 // Test for message: shift equal to size of object [267]
@@ -15,4 +15,18 @@ shl32(unsigned int x)
 {
 	/* expect+1: warning: shift equal to size of object [267] */
 	return x << 32;
+}
+
+/*
+ * As of 2022-08-19, lint ignores the GCC-specific 'mode' attribute, treating
+ * the tetra-int as a plain single-int, thus having width 32.
+ *
+ * https://gcc.gnu.org/onlinedocs/gccint/Machine-Modes.html
+ */
+unsigned
+function(unsigned __attribute__((mode(TI))) arg)
+{
+	/* XXX: The 'size' usually means the size in bytes, not in bits. */
+	/* expect+1: warning: shift equal to size of object [267] */
+	return (arg >> 32) & 3;
 }
