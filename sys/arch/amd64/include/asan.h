448b1ee94e48df35e9eda28488b13d226085141f
riastradh <riastradh@NetBSD.org>
2022-08-20 23:15:36 +0000

    x86: Split bootspace out of x86/pmap.h into new x86/bootspace.h.

--- sys/arch/amd64/include/asan.h
+++ sys/arch/amd64/include/asan.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: asan.h,v 1.9 2020/09/10 14:10:46 maxv Exp $	*/
+/*	$NetBSD: asan.h,v 1.10 2022/08/20 23:15:36 riastradh Exp $	*/
 
 /*
  * Copyright (c) 2018-2020 Maxime Villard, m00nbsd.net
@@ -34,6 +34,8 @@
 
 #include <amd64/pmap.h>
 #include <amd64/vmparam.h>
+
+#include <x86/bootspace.h>
 
 #ifdef __HAVE_PCPU_AREA
 #error "PCPU area not allowed with KASAN"
