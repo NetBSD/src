5b139df6bf01f17dad3906a856903ac2955e1196
christos <christos@NetBSD.org>
2022-08-27 14:01:59 +0000

    Import acpica-2022-03-31 ---------------------------------------- 31 March 2022. Summary of changes for version 20220331:
    0) Global changes:
    
    Update all copyright notices to the year 2022. This effects all source
    modules, as well as utility signons.
    
    
    1) ACPICA kernel-resident subsystem:
    
    For the ASL Sleep() operator, issue a warning if the sleep value is
    greater than 10 Milliseconds. Quick boottime is important, so warn about
    sleeps greater than 10 ms. Distribution Linux kernels reach initrd in 350
    ms, so excessive delays should be called out. 10 ms is chosen randomly,
    but three of such delays would already make up ten percent of the
    boottime.
    
    Namespace: Avoid attempting to walk the Namespace if the Namespace does
    not exist.
    
    AML interpreter/iASL compiler: Add new Acpi 6.4 semantics for the
    LoadTable and Load operators. DDB_HANDLE is gone, now loadtable returns a
    pass/fail integer. Now load returns a pass/fail integer, as well as
    storing the return value in an optional 2nd argument.
    
    Headers: Use uintptr_t and offsetof() in Linux kernel builds. To avoid
    "performing pointer subtraction with a null pointer has undefined
    behavior" compiler warnings, use uintptr_t and offsetof() that are always
    available during Linux kernel builds to define ACPI_UINTPTR_T and the
    ACPI_TO_INTEGER() and ACPI_OFFSET() macros when building the ACPICA code
    in the Linux kernel.
    
    Added support for the Windows 11 _OSI string ("Windows 2021"). Submitted
    by superm1.
    
    executer/exsystem: Inform users about ACPI spec violation for the Stall()
    operator. Values greater than 100 microseconds violate the ACPI
    specification, so warn users about it. From the ACPI Specification
    version 6.2 Errata A, 19.6.128 *Stall (Stall for a Short Time)*:
    > The implementation of Stall is OS-specific, but must not relinquish
    > control of the processor. Because of this, delays longer than 100
    > microseconds must use Sleep instead of Stall.
    
    
    2) iASL Compiler/Disassembler and ACPICA tools:
    
    Data Table Compiler/Disassembler: Add support for the APMT table - ARM
    Performance Monitoring Unit table. Submitted by @bwicaksononv.
    
    Data Table Compiler/Disassembler: For MADT, add support for the OEM-
    defined subtables (Types 0x80-0x7F).
    
    Data Table Compiler: Fixed a problem with support for the SDEV table,
    where a subtable Length was not computed correctly.
    
    Data Table Compiler/Disassembler: Add/fix the CFMWS subtable to the CEDT
    Acpi table support.
    
    Data Table Compiler/Disassembler: Fix a compile issue with the CEDT and
    add template. Submitted by MasterDrogo.
    
    Data Table Compiler/Disassembler: NHLT Changes provided by Piotr Maziarz:
    iASL/NHLT: Rename linux specific structures to DeviceInfo to improve
    readability of the code.
    iASL/NHLT: Fix parsing undocumented bytes at the end of Endpoint.
    Undocumented bytes at the end of Endpoint Descriptor can be present
    independently of Linux-specific structures. Their size can also vary.
    iASL/NHLT: Treat TableTerminator as SpecificConfig. SpecificConfig has 4
    bytes of size and then an amount of bytes specified by size. All of the
    terminators that I've seen had a size equal to 4, but theoretically it
    can vary.
    
    iASL/AcpiExec: Use _exit instead of exit in signal handers (ctrl-C).
    
    iASL: Remove a remark due to excessive output. Removed a remark for
    duplicate Offset() operators, due to a user complaint.

--- sys/external/bsd/acpica/dist/common/dmtbinfo1.c
+++ sys/external/bsd/acpica/dist/common/dmtbinfo1.c
@@ -5,7 +5,7 @@
  *****************************************************************************/
 
 /*
- * Copyright (C) 2000 - 2021, Intel Corp.
+ * Copyright (C) 2000 - 2022, Intel Corp.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -413,6 +413,28 @@ ACPI_DMTABLE_INFO           AcpiDmTableInfoCedt0[] =
     ACPI_DMT_TERMINATOR
 };
 
+/* 1: CXL Fixed Memory Window Structure */
+
+ACPI_DMTABLE_INFO           AcpiDmTableInfoCedt1[] =
+{
+    {ACPI_DMT_UINT32,   ACPI_CEDT1_OFFSET (Reserved1),            "Reserved", 0},
+    {ACPI_DMT_UINT64,   ACPI_CEDT1_OFFSET (BaseHpa),              "Window base address", 0},
+    {ACPI_DMT_UINT64,   ACPI_CEDT1_OFFSET (WindowSize),           "Window size", 0},
+    {ACPI_DMT_UINT8,    ACPI_CEDT1_OFFSET (InterleaveWays),       "Interleave Members (2^n)", 0},
+    {ACPI_DMT_UINT8,    ACPI_CEDT1_OFFSET (InterleaveArithmetic), "Interleave Arithmetic", 0},
+    {ACPI_DMT_UINT16,   ACPI_CEDT1_OFFSET (Reserved2),            "Reserved", 0},
+    {ACPI_DMT_UINT32,   ACPI_CEDT1_OFFSET (Granularity),          "Granularity", 0},
+    {ACPI_DMT_UINT16,   ACPI_CEDT1_OFFSET (Restrictions),         "Restrictions", 0},
+    {ACPI_DMT_UINT16,   ACPI_CEDT1_OFFSET (QtgId),                "QtgId", 0},
+    {ACPI_DMT_UINT32,   ACPI_CEDT1_OFFSET (InterleaveTargets),    "First Target", 0},
+    ACPI_DMT_TERMINATOR
+};
+
+ACPI_DMTABLE_INFO           AcpiDmTableInfoCedt1_te[] =
+{
+    {ACPI_DMT_UINT32,   ACPI_CEDT1_TE_OFFSET (InterleaveTarget),  "Next Target", 0},
+    ACPI_DMT_TERMINATOR
+};
 
 /*******************************************************************************
  *
@@ -660,6 +682,15 @@ ACPI_DMTABLE_INFO           AcpiDmTableInfoDmar4[] =
     ACPI_DMT_TERMINATOR
 };
 
+/* 5: Hardware Unit Definition */
+
+ACPI_DMTABLE_INFO           AcpiDmTableInfoDmar5[] =
+{
+    {ACPI_DMT_UINT8,    ACPI_DMAR5_OFFSET (Flags),                  "Flags", 0},
+    {ACPI_DMT_UINT8,    ACPI_DMAR5_OFFSET (Reserved),               "Reserved", 0},
+    {ACPI_DMT_UINT16,   ACPI_DMAR5_OFFSET (Segment),                "PCI Segment Number", 0},
+    ACPI_DMT_TERMINATOR
+};
 
 /*******************************************************************************
  *
