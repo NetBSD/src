a3dc47105a4ce594db04bf96d49bb1ba1a273ffa
msaitoh <msaitoh@NetBSD.org>
2022-09-28 08:12:55 +0000

    s/u_quad_t/uint64_t/. No functional change.

--- sys/compat/sys/sockio.h
+++ sys/compat/sys/sockio.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: sockio.h,v 1.19 2019/05/17 07:37:12 msaitoh Exp $	*/
+/*	$NetBSD: sockio.h,v 1.20 2022/09/28 08:12:56 msaitoh Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1990, 1993, 1994
@@ -75,21 +75,21 @@ struct oif_data {
 	u_char	ifi_addrlen;		/* media address length */
 	u_char	ifi_hdrlen;		/* media header length */
 	int	ifi_link_state;		/* current link state */
-	u_quad_t ifi_mtu;		/* maximum transmission unit */
-	u_quad_t ifi_metric;		/* routing metric (external only) */
-	u_quad_t ifi_baudrate;		/* linespeed */
+	uint64_t ifi_mtu;		/* maximum transmission unit */
+	uint64_t ifi_metric;		/* routing metric (external only) */
+	uint64_t ifi_baudrate;		/* linespeed */
 	/* volatile statistics */
-	u_quad_t ifi_ipackets;		/* packets received on interface */
-	u_quad_t ifi_ierrors;		/* input errors on interface */
-	u_quad_t ifi_opackets;		/* packets sent on interface */
-	u_quad_t ifi_oerrors;		/* output errors on interface */
-	u_quad_t ifi_collisions;	/* collisions on csma interfaces */
-	u_quad_t ifi_ibytes;		/* total number of octets received */
-	u_quad_t ifi_obytes;		/* total number of octets sent */
-	u_quad_t ifi_imcasts;		/* packets received via multicast */
-	u_quad_t ifi_omcasts;		/* packets sent via multicast */
-	u_quad_t ifi_iqdrops;		/* dropped on input, this interface */
-	u_quad_t ifi_noproto;		/* destined for unsupported protocol */
+	uint64_t ifi_ipackets;		/* packets received on interface */
+	uint64_t ifi_ierrors;		/* input errors on interface */
+	uint64_t ifi_opackets;		/* packets sent on interface */
+	uint64_t ifi_oerrors;		/* output errors on interface */
+	uint64_t ifi_collisions;	/* collisions on csma interfaces */
+	uint64_t ifi_ibytes;		/* total number of octets received */
+	uint64_t ifi_obytes;		/* total number of octets sent */
+	uint64_t ifi_imcasts;		/* packets received via multicast */
+	uint64_t ifi_omcasts;		/* packets sent via multicast */
+	uint64_t ifi_iqdrops;		/* dropped on input, this interface */
+	uint64_t ifi_noproto;		/* destined for unsupported protocol */
 	struct	timeval50 ifi_lastchange;/* last operational state change */
 };
 
