4bba9887b7559b2c6fb857a14630d8abe7e6dca4
msaitoh <msaitoh@NetBSD.org>
2022-10-01 07:22:55 +0000

    Add Nuvoton NCT6799D support.

--- sys/dev/isa/wbsioreg.h
+++ sys/dev/isa/wbsioreg.h
@@ -1,4 +1,4 @@
-/* $NetBSD: wbsioreg.h,v 1.9 2019/07/10 16:23:55 msaitoh Exp $ */
+/* $NetBSD: wbsioreg.h,v 1.10 2022/10/01 07:22:55 msaitoh Exp $ */
 
 /* $OpenBSD: wbsioreg.h,v 1.4 2015/01/02 23:02:54 chris Exp $ */
 /*
@@ -72,6 +72,7 @@
 #define WBSIO_ID_NCT6795D	0xd35
 #define WBSIO_ID_NCT6796D	0xd420	/* 13bits */
 #define WBSIO_ID_NCT6798D	0xd428
+#define WBSIO_ID_NCT6799D	0xd800
 
 /* Make the above WBSIO_ID_* vaue from WBSIO_ID, WBSIO_REV and IDbits */
 #define WBSIO_MAKEID(id, rev, bits)					\
