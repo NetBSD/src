05bd0853213659445923c0db85c89d90379d7d36
knakahara <knakahara@NetBSD.org>
2022-10-11 09:52:49 +0000

    Welcome to 9.99.101
        - changed struct sadb_x_policy in pfkeyv2.h

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.713 2022/09/20 02:24:18 knakahara Exp $	*/
+/*	$NetBSD: param.h,v 1.714 2022/10/11 09:52:49 knakahara Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -67,7 +67,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	999010000	/* NetBSD 9.99.100 */
+#define	__NetBSD_Version__	999010100	/* NetBSD 9.99.101 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
