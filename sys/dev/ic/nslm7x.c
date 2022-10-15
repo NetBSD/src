58b7d74a7673813fbc4ee75337efd45b3d344499
martin <martin@NetBSD.org>
2022-10-15 09:45:54 +0000

    Pull up following revision(s) (requested by msaitoh in ticket #1541):
    	share/man/man4/lm.4: revision 1.37
    	sys/dev/isa/wbsio.c: revision 1.28
    	sys/dev/ic/nslm7x.c: revision 1.78
    	sys/dev/isa/wbsioreg.h: revision 1.10
    
    Add Nuvoton NCT6799D support.
    
    Add NCT679[89] to the supported list.

--- sys/dev/ic/nslm7x.c
+++ sys/dev/ic/nslm7x.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: nslm7x.c,v 1.73 2019/07/10 16:23:55 msaitoh Exp $ */
+/*	$NetBSD: nslm7x.c,v 1.73.2.1 2022/10/15 09:45:54 martin Exp $ */
 
 /*-
  * Copyright (c) 2000 The NetBSD Foundation, Inc.
@@ -30,7 +30,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: nslm7x.c,v 1.73 2019/07/10 16:23:55 msaitoh Exp $");
+__KERNEL_RCSID(0, "$NetBSD: nslm7x.c,v 1.73.2.1 2022/10/15 09:45:54 martin Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -2157,6 +2157,7 @@ static const struct wb_product wbsio_products[] = {
     { WBSIO_ID_NCT6795D,    "NCT6795D", nct6779d_sensors, NULL },
     { WBSIO_ID_NCT6796D,    "NCT6796D", nct6779d_sensors, NULL },
     { WBSIO_ID_NCT6798D,    "NCT6798D", nct6779d_sensors, NULL },
+    { WBSIO_ID_NCT6799D,    "NCT6799D", nct6779d_sensors, NULL },
     { 0, NULL, NULL, NULL }
 };
 
