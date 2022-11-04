bb38679ddb6a499aaa637815a359baf1fdbc0c6d
ozaki-r <ozaki-r@NetBSD.org>
2022-11-04 09:06:25 +0000

    Bump the version for function renames of inpcb
    Welcome to 9.99.105

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.717 2022/10/28 08:16:57 ozaki-r Exp $	*/
+/*	$NetBSD: param.h,v 1.718 2022/11/04 09:06:25 ozaki-r Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -67,7 +67,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	999010400	/* NetBSD 9.99.104 */
+#define	__NetBSD_Version__	999010500	/* NetBSD 9.99.105 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
