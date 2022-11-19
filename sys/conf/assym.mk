5877649137069a66199aa5843547e0bd9ec5d5d3
yamt <yamt@NetBSD.org>
2022-11-19 07:54:25 +0000

    assym.mk: Fix assym.d generation
    It seems that the genassym/mkdep output has been changed.
    This commit adapts the assym.d generation rule to the new output.

--- sys/conf/assym.mk
+++ sys/conf/assym.mk
@@ -1,4 +1,4 @@
-# $NetBSD: assym.mk,v 1.7 2020/07/09 02:13:58 christos Exp $
+# $NetBSD: assym.mk,v 1.8 2022/11/19 07:54:25 yamt Exp $
 
 GENASSYM_FLAGS=${CFLAGS:N-Wa,*:N-fstack-usage*} ${CPPFLAGS} ${GENASSYM_CPPFLAGS} 
 
@@ -16,11 +16,26 @@ ${SRCS:M*.[sS]:C|\.[Ss]|.o|}: assym.h
 ${SRCS:M*.[sS]:C|\.[Ss]|.d|}: assym.h
 .endif
 
+# assym.dep in the below target looks like:
+#
+#   assym.o: \
+#    /var/folders/74/hw1sphgx0lv63q6pq_n5grw00000gn/T//genassym.BCtq6a/assym.c \
+#    opt_arm_start.h opt_execfmt.h opt_multiprocessor.h \
+#      :
+#      :
+#
+# The following sed modifies it to:
+#
+#   assym.h: \
+#    opt_arm_start.h opt_execfmt.h opt_multiprocessor.h \
+#      :
+#      :
+
 assym.d: assym.h
 	${_MKTARGET_CREATE}
 	cat ${GENASSYM_CONF} ${GENASSYM_EXTRAS} | \
 	    ${GENASSYM} -- ${MKDEP} -f assym.dep -- ${GENASSYM_FLAGS}
-	${TOOL_SED} -e 's/.*\.o:.*\.c/assym.h:/' < assym.dep >${.TARGET}
+	${TOOL_SED} -e '1{N;s/\\\n//;}' -e 's/.*\.o:.*\.c/assym.h:/' < assym.dep >${.TARGET}
 	rm -f assym.dep
 
 DEPS+=	assym.d
