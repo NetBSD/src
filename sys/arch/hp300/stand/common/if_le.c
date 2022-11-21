8428850da2d6fec83d3a1d2adee3e5f1c07acdc7
tsutsui <tsutsui@NetBSD.org>
2022-11-21 15:36:29 +0000

    Make local functions and variables static.

--- sys/arch/hp300/stand/common/if_le.c
+++ sys/arch/hp300/stand/common/if_le.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: if_le.c,v 1.13 2014/06/21 02:02:40 tsutsui Exp $	*/
+/*	$NetBSD: if_le.c,v 1.14 2022/11/21 15:36:29 tsutsui Exp $	*/
 
 /*
  * Copyright (c) 1993 Adam Glass
@@ -72,12 +72,12 @@ struct le_sel {
 	int	le_bonus;
 };
 
-int le_probe(struct netif *, void *);
-int le_match(struct netif *, void *);
-void le_init(struct iodesc *, void *);
-int le_get(struct iodesc *, void *, size_t, saseconds_t);
-int le_put(struct iodesc *, void *, size_t);
-void le_end(struct netif *);
+static int le_probe(struct netif *, void *);
+static int le_match(struct netif *, void *);
+static void le_init(struct iodesc *, void *);
+static int le_get(struct iodesc *, void *, size_t, saseconds_t);
+static int le_put(struct iodesc *, void *, size_t);
+static void le_end(struct netif *);
 
 static inline void lewrcsr(struct le_softc *, uint16_t, uint16_t);
 static inline uint16_t lerdcsr(struct le_softc *, uint16_t);
@@ -92,7 +92,7 @@ static int le_poll(struct iodesc *, void *, int);
 int le_debug = 0;
 #endif
 
-struct le_sel le0conf[] = {
+static struct le_sel le0conf[] = {
 /* offsets for:	   ID   REGS     MEM   NVRAM	le_heat	le_bonus*/
 {		    0,	0x4000, 0x8000, 0xC008,	1,	10   }
 };
@@ -100,7 +100,7 @@ struct le_sel le0conf[] = {
 
 extern struct netif_stats	le_stats[];
 
-struct netif_dif le_ifs[] = {
+static struct netif_dif le_ifs[] = {
 /*	dif_unit	dif_nsel	dif_stats	dif_private	*/
 {	0,		NLE0CONF,	&le_stats[0],	le0conf,	},
 };
@@ -120,7 +120,7 @@ struct netif_driver le_driver = {
 	NLE_IFS			/* netif_nifs */
 };
 
-struct le_softc le_softc[NLE];
+static struct le_softc le_softc[NLE];
 
 static inline void
 lewrcsr(struct le_softc *sc, uint16_t port, uint16_t val)
@@ -211,7 +211,7 @@ leinit(void)
 	}
 }
 
-int
+static int
 le_match(struct netif *nif, void *machdep_hint)
 {
 	struct le_sel *sels;
@@ -232,7 +232,7 @@ le_match(struct netif *nif, void *machdep_hint)
 	return rv;
 }
 
-int
+static int
 le_probe(struct netif *nif, void *machdep_hint)
 {
 #if 0
@@ -307,7 +307,7 @@ le_mem_summary(int unit)
 #define le_mem_summary(u)
 #endif
 
-void
+static void
 le_error(int unit, char *str, uint16_t stat)
 {
 
@@ -328,7 +328,7 @@ le_error(int unit, char *str, uint16_t stat)
 	((u_long)(a) - (u_long)sc->sc_mem)
 
 /* LANCE initialization block set up. */
-void
+static void
 lememinit(struct le_softc *sc)
 {
 	int i;
@@ -388,7 +388,7 @@ lememinit(struct le_softc *sc)
 	}
 }
 
-void
+static void
 le_reset(int unit, u_char *myea)
 {
 	struct le_softc *sc = &le_softc[unit];
@@ -450,7 +450,7 @@ le_reset(int unit, u_char *myea)
 	le_mem_summary(unit);
 }
 
-int
+static int
 le_poll(struct iodesc *desc, void *pkt, int len)
 {
 	int unit = /*nif->nif_unit*/0;
@@ -525,7 +525,7 @@ cleanup:
 	return length;
 }
 
-int
+static int
 le_put(struct iodesc *desc, void *pkt, size_t len)
 {
 	int unit = /*nif->nif_unit*/0;
@@ -627,7 +627,7 @@ le_put(struct iodesc *desc, void *pkt, size_t len)
 }
 
 
-int
+static int
 le_get(struct iodesc *desc, void *pkt, size_t len, saseconds_t timeout)
 {
 	satime_t t;
@@ -640,7 +640,7 @@ le_get(struct iodesc *desc, void *pkt, size_t len, saseconds_t timeout)
 	return cc;
 }
 
-void
+static void
 le_init(struct iodesc *desc, void *machdep_hint)
 {
 	struct netif *nif = desc->io_netif;
@@ -658,7 +658,7 @@ le_init(struct iodesc *desc, void *machdep_hint)
 	le_reset(unit, desc->myea);
 }
 
-void
+static void
 le_end(struct netif *nif)
 {
 	int unit = nif->nif_unit;
