a4ee3ecfb261213f095b167a933ef16c2c0c04c7
ryo <ryo@NetBSD.org>
2022-12-01 00:48:05 +0000

    tprof module revamped. Welcome to 9.99.108

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.720 2022/11/25 08:40:15 knakahara Exp $	*/
+/*	$NetBSD: param.h,v 1.721 2022/12/01 00:48:05 ryo Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -67,7 +67,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	999010700	/* NetBSD 9.99.107 */
+#define	__NetBSD_Version__	999010800	/* NetBSD 9.99.108 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
