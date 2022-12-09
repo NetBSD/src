99162e57733f79f4b8ca5caef96131eeab3d2fe2
martin <martin@NetBSD.org>
2022-12-09 17:02:13 +0000

    Add set_default_sizemult() calls to some architectures obviously lacking it.

--- usr.sbin/sysinst/arch/sparc64/md.c
+++ usr.sbin/sysinst/arch/sparc64/md.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: md.c,v 1.7 2022/01/29 16:01:20 martin Exp $	*/
+/*	$NetBSD: md.c,v 1.8 2022/12/09 17:02:14 martin Exp $	*/
 
 /*
  * Copyright 1997 Piermont Information Systems Inc.
@@ -68,6 +68,8 @@ md_init_set_status(int flags)
 bool
 md_get_info(struct install_partition_desc *install)
 {
+
+	set_default_sizemult(pm->diskdev, MEG, pm->sectorsize);
 	return true;
 }
 
