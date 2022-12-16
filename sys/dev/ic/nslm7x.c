5e7dd0391c9d3858a919d064cd87cf466431a7bd
msaitoh <msaitoh@NetBSD.org>
2022-12-16 00:02:28 +0000

    lm(4): Add Nuvoton NCT6797D support.

--- sys/dev/ic/nslm7x.c
+++ sys/dev/ic/nslm7x.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: nslm7x.c,v 1.78 2022/10/01 07:22:56 msaitoh Exp $ */
+/*	$NetBSD: nslm7x.c,v 1.79 2022/12/16 00:02:28 msaitoh Exp $ */
 
 /*-
  * Copyright (c) 2000 The NetBSD Foundation, Inc.
@@ -30,7 +30,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: nslm7x.c,v 1.78 2022/10/01 07:22:56 msaitoh Exp $");
+__KERNEL_RCSID(0, "$NetBSD: nslm7x.c,v 1.79 2022/12/16 00:02:28 msaitoh Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -2156,6 +2156,7 @@ static const struct wb_product wbsio_products[] = {
     { WBSIO_ID_NCT6793D,    "NCT6793D", nct6779d_sensors, NULL },
     { WBSIO_ID_NCT6795D,    "NCT6795D", nct6779d_sensors, NULL },
     { WBSIO_ID_NCT6796D,    "NCT6796D", nct6779d_sensors, NULL },
+    { WBSIO_ID_NCT6797D,    "NCT6797D", nct6779d_sensors, NULL },
     { WBSIO_ID_NCT6798D,    "NCT6798D", nct6779d_sensors, NULL },
     { WBSIO_ID_NCT6799D,    "NCT6799D", nct6779d_sensors, NULL },
     { 0, NULL, NULL, NULL }
