4638cdf24bd97c4c0ab45639006fca6a07dbd558
ryo <ryo@NetBSD.org>
2022-12-22 06:58:47 +0000

    PMCR_EL0.LC should be set. ARM deprecates use of PMCR_EL0.LC=0

--- sys/arch/aarch64/aarch64/cpu.c
+++ sys/arch/aarch64/aarch64/cpu.c
@@ -1,4 +1,4 @@
-/* $NetBSD: cpu.c,v 1.71 2022/12/22 06:58:07 ryo Exp $ */
+/* $NetBSD: cpu.c,v 1.72 2022/12/22 06:58:47 ryo Exp $ */
 
 /*
  * Copyright (c) 2017 Ryo Shimizu <ryo@nerv.org>
@@ -27,7 +27,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(1, "$NetBSD: cpu.c,v 1.71 2022/12/22 06:58:07 ryo Exp $");
+__KERNEL_RCSID(1, "$NetBSD: cpu.c,v 1.72 2022/12/22 06:58:47 ryo Exp $");
 
 #include "locators.h"
 #include "opt_arm_debug.h"
@@ -498,7 +498,7 @@ cpu_init_counter(struct cpu_info *ci)
 		return;
 	}
 
-	reg_pmcr_el0_write(PMCR_E | PMCR_C);
+	reg_pmcr_el0_write(PMCR_E | PMCR_C | PMCR_LC);
 	reg_pmintenclr_el1_write(PMINTEN_C | PMINTEN_P);
 	reg_pmcntenset_el0_write(PMCNTEN_C);
 
