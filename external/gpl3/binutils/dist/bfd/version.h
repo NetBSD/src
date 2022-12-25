cfb50c1c66fb3c71a9d4259691298e70df88edb9
christos <christos@NetBSD.org>
2022-12-25 20:04:03 +0000

    Add a space between the version package and the version string because gcc configure needs it to parse the ld version to detect if support is there for symbol visibility. Yes we pass '(NetBSD binutils nb1) ' in the Makefiles, but this is fragile with all the automake massaging, and this is much easier to enforce.

--- external/gpl3/binutils/dist/bfd/version.h
+++ external/gpl3/binutils/dist/bfd/version.h
@@ -18,5 +18,5 @@
    sonames.  */
 #define BFD_VERSION_DATE 20220805
 #define BFD_VERSION @bfd_version@
-#define BFD_VERSION_STRING  @bfd_version_package@ @bfd_version_string@
+#define BFD_VERSION_STRING  @bfd_version_package@ " " @bfd_version_string@
 #define REPORT_BUGS_TO @report_bugs_to@
