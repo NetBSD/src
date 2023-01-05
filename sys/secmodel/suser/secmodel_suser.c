3f9c8a4e8da566d2e4b5805212ecf73bef702644
jakllsch <jakllsch@NetBSD.org>
2023-01-05 18:29:45 +0000

    wg(4): Allow non-root to retrieve information other than the private key and the peer preshared key.
    Add kauth(9) enums for wg(4) and add use them in suser secmodel.
    
    Refines fix for PR 57161.

--- sys/secmodel/suser/secmodel_suser.c
+++ sys/secmodel/suser/secmodel_suser.c
@@ -1,4 +1,4 @@
-/* $NetBSD: secmodel_suser.c,v 1.56 2023/01/05 17:36:53 jakllsch Exp $ */
+/* $NetBSD: secmodel_suser.c,v 1.57 2023/01/05 18:29:45 jakllsch Exp $ */
 /*-
  * Copyright (c) 2006 Elad Efrat <elad@NetBSD.org>
  * All rights reserved.
@@ -38,7 +38,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: secmodel_suser.c,v 1.56 2023/01/05 17:36:53 jakllsch Exp $");
+__KERNEL_RCSID(0, "$NetBSD: secmodel_suser.c,v 1.57 2023/01/05 18:29:45 jakllsch Exp $");
 
 #include <sys/types.h>
 #include <sys/param.h>
@@ -751,6 +751,20 @@ secmodel_suser_network_cb(kauth_cred_t cred, kauth_action_t action,
 			if (isroot)
 				result = KAUTH_RESULT_ALLOW;
 
+			break;
+
+		default:
+			break;
+		}
+
+		break;
+
+	case KAUTH_NETWORK_INTERFACE_WG:
+		switch (req) {
+		case KAUTH_REQ_NETWORK_INTERFACE_WG_GETPRIV:
+		case KAUTH_REQ_NETWORK_INTERFACE_WG_SETPRIV:
+			if (isroot)
+				result = KAUTH_RESULT_ALLOW;
 			break;
 
 		default:
