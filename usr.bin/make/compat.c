dad38d118440e2c4a5903e7cfa4e2b04a75b8c02
christos <christos@NetBSD.org>
2023-01-17 21:35:19 +0000

    Accept whitespace between command specifiers @+- like gmake does. New binutils does this.

--- usr.bin/make/compat.c
+++ usr.bin/make/compat.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: compat.c,v 1.243 2022/12/07 10:28:48 rillig Exp $	*/
+/*	$NetBSD: compat.c,v 1.244 2023/01/17 21:35:19 christos Exp $	*/
 
 /*
  * Copyright (c) 1988, 1989, 1990 The Regents of the University of California.
@@ -91,7 +91,7 @@
 #include "pathnames.h"
 
 /*	"@(#)compat.c	8.2 (Berkeley) 3/19/94"	*/
-MAKE_RCSID("$NetBSD: compat.c,v 1.243 2022/12/07 10:28:48 rillig Exp $");
+MAKE_RCSID("$NetBSD: compat.c,v 1.244 2023/01/17 21:35:19 christos Exp $");
 
 static GNode *curTarg = NULL;
 static pid_t compatChild;
@@ -280,7 +280,8 @@ Compat_RunCommand(const char *cmdp, GNode *gn, StringListNode *ln)
 			doIt = true;
 			if (shellName == NULL)	/* we came here from jobs */
 				Shell_Init();
-		} else
+		} else if (!ch_isspace(*cmd))
+			/* Ignore whitespace for compatibility with gnu make */
 			break;
 		cmd++;
 	}
