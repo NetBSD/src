5732ef2398b4e0b59a20b9f50af7650e1f422220
rillig <rillig@NetBSD.org>
2023-01-17 19:42:47 +0000

    tests/make: test backslash-newline after macro expansion in command

--- usr.bin/make/unit-tests/varname-dot-newline.mk
+++ usr.bin/make/unit-tests/varname-dot-newline.mk
@@ -1,7 +1,21 @@
-# $NetBSD: varname-dot-newline.mk,v 1.4 2020/10/24 08:46:08 rillig Exp $
+# $NetBSD: varname-dot-newline.mk,v 1.5 2023/01/17 19:42:47 rillig Exp $
 #
-# Tests for the special .newline variable.
+# Tests for the special .newline variable, which contains a single newline
+# character (U+000A).
+
+
+# https://austingroupbugs.net/view.php?id=1549 proposes:
+# > After all macro expansion is complete, when an escaped <newline> is
+# > found in a command line in a makefile, the command line that is executed
+# > shall contain the <backslash>, the <newline>, and the next line, except
+# > that the first character of the next line shall not be included if it is
+# > a <tab>.
 #
+# The above quote assumes that each resulting <newline> character has a "next
+# line", but that's not how the .newline variable works.
+BACKSLASH_NEWLINE:=	\${.newline}
+
+
 # Contrary to the special variable named "" that is used in expressions like
 # ${:Usome-value}, the variable ".newline" is not protected against
 # modification.  Nobody exploits that though.
@@ -21,3 +35,4 @@ NEWLINE:=	${.newline}
 
 all:
 	@echo 'first${.newline}second'
+	@echo 'backslash newline: <${BACKSLASH_NEWLINE}>'
