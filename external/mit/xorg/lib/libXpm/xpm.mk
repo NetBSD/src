1221c3b5ba7d5b8e5594a53a6a4c5b82246c960e
mrg <mrg@NetBSD.org>
2023-01-19 05:31:34 +0000

    updates for libXpm 3.5.15.

---
+++ external/mit/xorg/lib/libXpm/xpm.mk
@@ -0,0 +1,6 @@
+
+CPPFLAGS+=	-DHAS_SNPRINTF
+CPPFLAGS+=	-DXPM_PATH_COMPRESS=\"/usr/bin/compress\"
+CPPFLAGS+=	-DXPM_PATH_GZIP=\"/usr/bin/gzip\"
+CPPFLAGS+=	-DXPM_PATH_UNCOMPRESS=\"/usr/bin/uncompress\"
+CPPFLAGS+=	-I${DESTDIR}${X11INCDIR}/X11
