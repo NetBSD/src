a695cd72f689017d94f5f936ce22cff59d4c37e9
rillig <rillig@NetBSD.org>
2023-01-21 17:48:29 +0000

    lint: add test for floating constant overflow

--- tests/usr.bin/xlint/lint1/msg_248.c
+++ tests/usr.bin/xlint/lint1/msg_248.c
@@ -1,8 +1,12 @@
-/*	$NetBSD: msg_248.c,v 1.3 2022/06/16 21:24:41 rillig Exp $	*/
+/*	$NetBSD: msg_248.c,v 1.4 2023/01/21 17:48:29 rillig Exp $	*/
 # 3 "msg_248.c"
 
 // Test for message: floating-point constant out of range [248]
 
-/* expect+1: error: syntax error ':' [249] */
-TODO: "Add example code that triggers the above message."
-TODO: "Add example code that almost triggers the above message."
+float fits_flt = 1e37f;
+/* expect+1: warning: floating-point constant out of range [248] */
+float too_large_flt = 1e40f;
+
+double fits_dbl = 1e300;
+/* expect+1: warning: floating-point constant out of range [248] */
+double too_large_dbl = 1e310;
