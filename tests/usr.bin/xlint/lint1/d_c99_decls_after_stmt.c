29dbeebd92d4aaf6afebcd139baa877b9e11dfe5
rillig <rillig@NetBSD.org>
2023-01-22 17:17:24 +0000

    tests/lint: merge tests for declaration after statement

--- tests/usr.bin/xlint/lint1/d_c99_decls_after_stmt.c
+++ tests/usr.bin/xlint/lint1/d_c99_decls_after_stmt.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: d_c99_decls_after_stmt.c,v 1.4 2022/02/12 01:23:44 rillig Exp $	*/
+/*	$NetBSD: d_c99_decls_after_stmt.c,v 1.5 2023/01/22 17:17:25 rillig Exp $	*/
 # 3 "d_c99_decls_after_stmt.c"
 
 /*
@@ -9,9 +9,32 @@
  */
 
 void
-sample(void)
+two_groups_of_decl_plus_stmt(void)
 {
 	int i = 0;
+	i += 1;
+
+	int j = 0;
+	j += 1;
+}
+
+typedef int int_t;
+
+int
+second_decl_stmt_uses_a_typedef(void)
+{
+	int i = 0;
+	i += 1;
+
+	int_t j = 0;
+	j += 1;
+
+	return 0;
+}
+
+void
+function_with_argument(int i)
+{
 	i += 1;
 
 	int j = 0;
