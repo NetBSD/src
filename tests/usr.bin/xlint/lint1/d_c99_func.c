b5b35fa5a0b72812bb20e54740772c65cf61edf8
rillig <rillig@NetBSD.org>
2023-01-29 18:16:48 +0000

    lint: fix definition of __func__

--- tests/usr.bin/xlint/lint1/d_c99_func.c
+++ tests/usr.bin/xlint/lint1/d_c99_func.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: d_c99_func.c,v 1.4 2023/01/29 17:36:26 rillig Exp $	*/
+/*	$NetBSD: d_c99_func.c,v 1.5 2023/01/29 18:16:48 rillig Exp $	*/
 # 3 "d_c99_func.c"
 
 /* C99 __func__ */
@@ -8,8 +8,7 @@ const char *str;
 const char *
 function_name(void)
 {
-	/* FIXME: -14 for the array, not -8 for a pointer. */
-	/* expect+1: error: negative array dimension (-8) [20] */
+	/* expect+1: error: negative array dimension (-14) [20] */
 	typedef int reveal_size[-(int)sizeof(__func__)];
 	return __func__;
 }
