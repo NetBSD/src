932cf2b587e668f55f678c88e67dcc1667492dfc
mrg <mrg@NetBSD.org>
2023-02-05 22:42:39 +0000

    fix root detection on evbarm when raid is involved
    there are several problems solved in this change:
    - lots of work was re-done when we already have determined the
      device booted from, so several new early returns introduced
      if booted_device has been set
    - due to the lack of cpu_bootconf(), raidframe softroot would
      override "root=xxx" on the boot command line (note that
      platforms that use eg, device_register() to detect the boot
      device are not affected by this issue as they find the
      boot device much earlier.)
    - in the new cpu_bootconf(), switch the order of the platform
      boot-config with the set_root_device() call.  this avoids a
      problem where "root=xxx" is checked after automated methods,
      and is thus ignored.
    - in fdt_detect_root_device(), remove the code to add "root=xxx""
      string to the boot_args[] that would be later parsed by the
      set_root_device() call, and simply set booted_device and, for
      mbr installs, booted_partition directly.  also, for any
      successful call, perform an early return.
    - define __HAVE_CPU_BOOTCONF so early boot calls cpu_bootconf().
    
    
    tested on:
    - rockpro64 booting from emmc, sata (big, and little endian)
    - rockpro64 loading kernel from msdos partition
    - rockpro64 booting from network (fails to auto-detect, with or
      without this change)
    - quartz64 booting from nvme
    - lx2k booting from nvme
    
    XXX: pullup-10

--- sys/arch/evbarm/fdt/fdt_machdep.c
+++ sys/arch/evbarm/fdt/fdt_machdep.c
@@ -1,4 +1,4 @@
-/* $NetBSD: fdt_machdep.c,v 1.99 2022/11/04 10:51:17 jmcneill Exp $ */
+/* $NetBSD: fdt_machdep.c,v 1.100 2023/02/05 22:42:39 mrg Exp $ */
 
 /*-
  * Copyright (c) 2015-2017 Jared McNeill <jmcneill@invisible.ca>
@@ -27,7 +27,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: fdt_machdep.c,v 1.99 2022/11/04 10:51:17 jmcneill Exp $");
+__KERNEL_RCSID(0, "$NetBSD: fdt_machdep.c,v 1.100 2023/02/05 22:42:39 mrg Exp $");
 
 #include "opt_arm_debug.h"
 #include "opt_bootconfig.h"
@@ -741,16 +741,10 @@ delay(u_int us)
 static void
 fdt_detect_root_device(device_t dev)
 {
-	struct mbr_sector mbr;
-	uint8_t buf[DEV_BSIZE];
-	uint8_t hash[16];
-	const uint8_t *rhash;
-	char rootarg[64];
-	struct vnode *vp;
-	MD5_CTX md5ctx;
 	int error, len;
-	size_t resid;
-	u_int part;
+
+	if (booted_device)
+		return;
 
 	const int chosen = OF_finddevice("/chosen");
 	if (chosen < 0)
@@ -758,6 +752,14 @@ fdt_detect_root_device(device_t dev)
 
 	if (of_hasprop(chosen, "netbsd,mbr") &&
 	    of_hasprop(chosen, "netbsd,partition")) {
+		struct mbr_sector mbr;
+		uint8_t buf[DEV_BSIZE];
+		uint8_t hash[16];
+		const uint8_t *rhash;
+		struct vnode *vp;
+		MD5_CTX md5ctx;
+		size_t resid;
+		u_int part;
 
 		/*
 		 * The bootloader has passed in a partition index and MD5 hash
@@ -787,22 +789,22 @@ fdt_detect_root_device(device_t dev)
 		MD5Update(&md5ctx, (void *)&mbr, sizeof(mbr));
 		MD5Final(hash, &md5ctx);
 
-		if (memcmp(rhash, hash, 16) != 0)
-			return;
+		if (memcmp(rhash, hash, 16) == 0) {
+			booted_device = dev;
+			booted_partition = part;
+		}
 
-		snprintf(rootarg, sizeof(rootarg), " root=%s%c", device_xname(dev), part + 'a');
-		strcat(boot_args, rootarg);
+		return;
 	}
 
 	if (of_hasprop(chosen, "netbsd,gpt-guid")) {
-		char guidbuf[UUID_STR_LEN];
-		const struct uuid *guid = fdtbus_get_prop(chosen, "netbsd,gpt-guid", &len);
-		if (guid == NULL || len != 16)
-			return;
+		const struct uuid *guid =
+		    fdtbus_get_prop(chosen, "netbsd,gpt-guid", &len);
 
-		uuid_snprintf(guidbuf, sizeof(guidbuf), guid);
-		snprintf(rootarg, sizeof(rootarg), " root=wedge:%s", guidbuf);
-		strcat(boot_args, rootarg);
+		if (guid != NULL && len == 16)
+			booted_device = dev;
+
+		return;
 	}
 
 	if (of_hasprop(chosen, "netbsd,gpt-label")) {
@@ -813,14 +815,19 @@ fdt_detect_root_device(device_t dev)
 		device_t dv = dkwedge_find_by_wname(label);
 		if (dv != NULL)
 			booted_device = dv;
+
+		return;
 	}
 
 	if (of_hasprop(chosen, "netbsd,booted-mac-address")) {
-		const uint8_t *macaddr = fdtbus_get_prop(chosen, "netbsd,booted-mac-address", &len);
+		const uint8_t *macaddr =
+		    fdtbus_get_prop(chosen, "netbsd,booted-mac-address", &len);
+		struct ifnet *ifp;
+
 		if (macaddr == NULL || len != 6)
 			return;
+
 		int s = pserialize_read_enter();
-		struct ifnet *ifp;
 		IFNET_READER_FOREACH(ifp) {
 			if (memcmp(macaddr, CLLADDR(ifp->if_sadl), len) == 0) {
 				device_t dv = device_find_by_xname(ifp->if_xname);
@@ -830,6 +837,8 @@ fdt_detect_root_device(device_t dev)
 			}
 		}
 		pserialize_read_exit(s);
+
+		return;
 	}
 }
 
@@ -878,7 +887,6 @@ fdt_cpu_rootconf(void)
 {
 	device_t dev;
 	deviter_t di;
-	char *ptr;
 
 	if (booted_device != NULL)
 		return;
@@ -887,11 +895,11 @@ fdt_cpu_rootconf(void)
 		if (device_class(dev) != DV_DISK)
 			continue;
 
-		if (get_bootconf_option(boot_args, "root", BOOTOPT_TYPE_STRING, &ptr) != 0)
-			break;
-
 		if (device_is_a(dev, "ld") || device_is_a(dev, "sd") || device_is_a(dev, "wd"))
 			fdt_detect_root_device(dev);
+
+		if (booted_device != NULL)
+			break;
 	}
 	deviter_release(&di);
 }
