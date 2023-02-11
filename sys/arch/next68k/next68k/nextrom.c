ff97794b11cd2efd6cd8c44e981bae23e0101259
tsutsui <tsutsui@NetBSD.org>
2023-02-11 02:33:27 +0000

    Add and check machine type NeXT_CUBE_TURBO (type 8).
    Info from Andreas Grabher on port-next68k@.

--- sys/arch/next68k/next68k/nextrom.c
+++ sys/arch/next68k/next68k/nextrom.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: nextrom.c,v 1.28 2023/02/11 02:31:34 tsutsui Exp $	*/
+/*	$NetBSD: nextrom.c,v 1.29 2023/02/11 02:33:27 tsutsui Exp $	*/
 /*
  * Copyright (c) 1998 Darrin B. Jewell
  * All rights reserved.
@@ -25,7 +25,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: nextrom.c,v 1.28 2023/02/11 02:31:34 tsutsui Exp $");
+__KERNEL_RCSID(0, "$NetBSD: nextrom.c,v 1.29 2023/02/11 02:33:27 tsutsui Exp $");
 
 #include "opt_ddb.h"
 #include "opt_serial.h"
@@ -261,6 +261,11 @@ next68k_bootargs(unsigned char **args)
 			msize4  =  0x800000;
 			msize1  =  0x200000;
 			ROM_PUTS("Looks like a NeXT_TURBO_MONO\r\n");
+		} else if (MONRELOC(char, MG_machine_type) == NeXT_CUBE_TURBO) {
+			msize16 = 0x2000000;
+			msize4  =  0x800000;
+			msize1  =  0x200000;
+			ROM_PUTS("Looks like a NeXT_CUBE_TURBO\r\n");
 		} else {
 			msize16 = 0x100000;
 			msize4  = 0x100000;
@@ -270,7 +275,9 @@ next68k_bootargs(unsigned char **args)
 
 		mach = MONRELOC(char, MG_machine_type);
 		RELOC(rom_machine_type, char) = mach;
-		if (mach == NeXT_TURBO_MONO || mach == NeXT_TURBO_COLOR)
+		if (mach == NeXT_TURBO_MONO ||
+		    mach == NeXT_TURBO_COLOR ||
+		    mach == NeXT_CUBE_TURBO)
 			turbo_l = 1;
 		else
 			turbo_l = 0;
