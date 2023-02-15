de6829a2cc34b49c5705dfecde3ce00c0433433b
riastradh <riastradh@NetBSD.org>
2023-02-15 13:12:45 +0000

    kern/subr_devsw.c: Nix trailing whitespace.
    No functional change intended.
    
    Please do (setq show-trailing-whitespace t) if you use Emacs!
    
    Maybe someone should teach nvi to do something similar.

--- sys/kern/subr_devsw.c
+++ sys/kern/subr_devsw.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: subr_devsw.c,v 1.50 2023/02/13 19:07:14 buhrow Exp $	*/
+/*	$NetBSD: subr_devsw.c,v 1.51 2023/02/15 13:12:45 riastradh Exp $	*/
 
 /*-
  * Copyright (c) 2001, 2002, 2007, 2008 The NetBSD Foundation, Inc.
@@ -69,7 +69,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: subr_devsw.c,v 1.50 2023/02/13 19:07:14 buhrow Exp $");
+__KERNEL_RCSID(0, "$NetBSD: subr_devsw.c,v 1.51 2023/02/15 13:12:45 riastradh Exp $");
 
 #ifdef _KERNEL_OPT
 #include "opt_dtrace.h"
@@ -397,7 +397,7 @@ devsw_attach(const char *devname,
 		if (conv->d_name == NULL || strcmp(devname, conv->d_name) != 0)
 			continue;
 
-		if ((bdev != NULL) && (*bmajor < 0)) 
+		if ((bdev != NULL) && (*bmajor < 0))
 			*bmajor = conv->d_bmajor;
 		if (*cmajor < 0)
 			*cmajor = conv->d_cmajor;
