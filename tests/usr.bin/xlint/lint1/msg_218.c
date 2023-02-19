098f26b96b857a434907abf8fabcbb6c65966530
rillig <rillig@NetBSD.org>
2023-02-19 11:50:29 +0000

    tests/lint: fix and extend tests for C90 migration warning

--- tests/usr.bin/xlint/lint1/msg_218.c
+++ tests/usr.bin/xlint/lint1/msg_218.c
@@ -1,9 +1,15 @@
-/*	$NetBSD: msg_218.c,v 1.4 2022/06/22 19:23:18 rillig Exp $	*/
+/*	$NetBSD: msg_218.c,v 1.5 2023/02/19 11:50:29 rillig Exp $	*/
 # 3 "msg_218.c"
 
 // Test for message: ANSI C treats constant as unsigned, op '%s' [218]
 
 /* lint1-only-if: ilp32 */
+
+_Bool cond;
+signed int s32;
+unsigned int u32;
+signed long long s64;
+unsigned long long u64;
 
 void sink_int(int);
 
@@ -11,7 +17,39 @@ void sink_int(int);
 void
 test_signed_int(void)
 {
-	/* expect+2: warning: ANSI C treats constant as unsigned, op '-' [218] */
 	/* expect+1: warning: conversion of 'unsigned long' to 'int' is out of range, arg #1 [295] */
 	sink_int(-2147483648);
+}
+
+/*
+ * In traditional C, integer constants with an 'L' suffix that didn't fit
+ * into 'long' were promoted to the next larger integer type, if that existed
+ * at all, as the suffix 'LL' was introduced by C90.
+ *
+ * Starting with C90, integer constants with an 'L' suffix that didn't fit
+ * into 'long' were promoted to 'unsigned long' first, before trying 'long
+ * long'.
+ *
+ * In C99 mode, this distinction is no longer necessary since it is far
+ * enough from traditional C.
+ */
+void
+compare_large_constant(void)
+{
+	/* expect+1: warning: ANSI C treats constant as unsigned, op '<' [218] */
+	cond = s32 < 3000000000L;
+	/* expect+1: warning: ANSI C treats constant as unsigned, op '<' [218] */
+	cond = 3000000000L < s32;
+	/* expect+1: warning: ANSI C treats constant as unsigned, op '<' [218] */
+	cond = u32 < 3000000000L;
+	/* expect+1: warning: ANSI C treats constant as unsigned, op '<' [218] */
+	cond = 3000000000L < u32;
+	/* expect+1: warning: ANSI C treats constant as unsigned, op '<' [218] */
+	cond = s64 < 3000000000L;
+	/* expect+1: warning: ANSI C treats constant as unsigned, op '<' [218] */
+	cond = 3000000000L < s64;
+	/* expect+1: warning: ANSI C treats constant as unsigned, op '<' [218] */
+	cond = u64 < 3000000000L;
+	/* expect+1: warning: ANSI C treats constant as unsigned, op '<' [218] */
+	cond = 3000000000L < u64;
 }
