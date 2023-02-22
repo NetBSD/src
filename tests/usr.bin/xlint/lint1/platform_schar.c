f29e29e21929d5568671e6663fbf9692ab55293d
rillig <rillig@NetBSD.org>
2023-02-22 22:12:35 +0000

    tests/lint: add platform-specific tests

--- tests/usr.bin/xlint/lint1/platform_schar.c
+++ tests/usr.bin/xlint/lint1/platform_schar.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: platform_schar.c,v 1.2 2021/09/26 14:28:22 rillig Exp $	*/
+/*	$NetBSD: platform_schar.c,v 1.3 2023/02/22 22:12:35 rillig Exp $	*/
 # 3 "platform_schar.c"
 
 /*
@@ -9,7 +9,7 @@
 /* lint1-extra-flags: -c -h -a -p -b -r -z */
 /* lint1-only-if: schar */
 
-// TODO: Add some code that passes.
-// TODO: Add some code that fails.
-
-/* expect+1: warning: empty translation unit [272] */
+/* CONSTCOND */
+/* FIXME: Wrong order in the diagnostic, should be '-128 <' instead. */
+/* expect+1: warning: nonportable character comparison '< -128' [230] */
+typedef int is_signed[(char)'\200' < (char)'\177' ? 1 : -1];
