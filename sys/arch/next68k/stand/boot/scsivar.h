be132a8028345ebaf9f0a867eb5925a0c6dc9c66
martin <martin@NetBSD.org>
2023-02-22 12:09:16 +0000

    Pull up following revision(s) (requested by tsutsui in ticket #1600):
    	sys/arch/next68k/stand/boot/en.c: revision 1.20
    	sys/arch/next68k/stand/boot/scsi.c: revision 1.13
    	sys/arch/next68k/stand/boot/scsi.c: revision 1.14
    	sys/arch/next68k/stand/boot/conf.c: revision 1.8
    	sys/arch/next68k/stand/boot/sd.c: revision 1.17
    	sys/arch/next68k/stand/boot/samachdep.h: revision 1.1
    	sys/arch/next68k/stand/boot/samachdep.h: revision 1.2
    	sys/arch/next68k/stand/boot/rtc.c: revision 1.8
    	sys/arch/next68k/stand/boot/machdep.c: revision 1.9
    	sys/arch/next68k/stand/boot/boot.c: revision 1.14
    	sys/arch/next68k/stand/boot/boot.c: revision 1.15
    	sys/arch/next68k/stand/boot/scsivar.h: revision 1.2
    	sys/arch/next68k/stand/boot/devopen.c: revision 1.8
    	sys/arch/next68k/stand/boot/version: revision 1.7
    
    Use common declarations and macros in proper headers.
    
    Also fix inconsistent sdopen() and sdstrategy() args and
    remove useless #if 0'ed out code.
    No functional change.
    
    Replace DELAY() with one in hp300 bootloader and adjust cpuspeed counts.
    
    Fix boot failure on my ancient Seagate ST52160N drive.
    It looks some of such old drives can't respond to SCSI
    test-unit-ready command without proper wait after SCSI bus reset.
    
    Bump version again to denote a fix.
    
    XXX we should re-evaluate cpuspeed counts for DELAY() in bootloaders
        (where cache is disabled) on other m68k ports, hp300 and luna68k etc.
    
    Actually bump version (missed in the previous commit).

--- sys/arch/next68k/stand/boot/scsivar.h
+++ sys/arch/next68k/stand/boot/scsivar.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: scsivar.h,v 1.1.1.1 1998/06/09 07:53:06 dbj Exp $	*/
+/*	$NetBSD: scsivar.h,v 1.1.1.1.242.1 2023/02/22 12:09:16 martin Exp $	*/
 /*
  * Copyright (c) 1994 Rolf Grossmann
  * All rights reserved.
@@ -69,4 +69,5 @@ struct	scsi_softc {
 
 extern	struct scsi_softc scsi_softc;
 
-#define DELAY(n)        { register int N = (n); while (--N > 0); }
+void scsi_init(void);
+int scsiicmd(char, char, u_char *, int, char *, int *);
