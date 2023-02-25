e1c01bd816b27e18dc9946bb42bf4addfd231b05
rillig <rillig@NetBSD.org>
2023-02-25 00:09:52 +0000

    tests/make: test how '$' behaves in .MAKEOVERRIDES and MAKEFLAGS

--- usr.bin/make/unit-tests/varname-dot-makeoverrides.mk
+++ usr.bin/make/unit-tests/varname-dot-makeoverrides.mk
@@ -1,8 +1,18 @@
-# $NetBSD: varname-dot-makeoverrides.mk,v 1.3 2023/02/15 06:52:59 rillig Exp $
+# $NetBSD: varname-dot-makeoverrides.mk,v 1.4 2023/02/25 00:09:52 rillig Exp $
 #
 # Tests for the special .MAKEOVERRIDES variable.
 
-# TODO: Implementation
-
 all:
-	@:;
+	@${MAKE} -r -f ${MAKEFILE} dollars_stage_1
+
+# Demonstrate that '$' characters are altered when they are passed on to child
+# make processes via .MAKEOVERRIDES and MAKEFLAGS.
+dollars_stage_1:
+	${MAKE} -r -f ${MAKEFILE} dollars_stage_2 DOLLARS='$$$${varname}'
+
+dollars_stage_2:
+	@echo 'stage 2: dollars=<${DOLLARS}>'
+	${MAKE} -r -f ${MAKEFILE} dollars_stage_3
+
+dollars_stage_3:
+	@echo 'stage 3: dollars=<${DOLLARS}>'
