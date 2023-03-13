7739de6469b23dcc5a719eaab46fafe8fec762e3
riastradh <riastradh@NetBSD.org>
2023-03-13 18:26:32 +0000

    libm: Need <math.h> for __HAVE_LONG_DOUBLE.
    Fixes missing definitions of lrintl, llrintl on various platforms.

--- lib/libm/src/s_llrintl.c
+++ lib/libm/src/s_llrintl.c
@@ -2,8 +2,10 @@
 #ifdef __FreeBSD__
 __FBSDID("$FreeBSD: head/lib/msun/src/s_llrintl.c 175309 2008-01-14 02:12:07Z das $");
 #else
-__RCSID("$NetBSD: s_llrintl.c,v 1.2 2017/05/07 11:29:21 martin Exp $");
+__RCSID("$NetBSD: s_llrintl.c,v 1.3 2023/03/13 18:26:32 riastradh Exp $");
 #endif
+
+#include <math.h>
 
 #ifdef __HAVE_LONG_DOUBLE
 #define stype		long double
