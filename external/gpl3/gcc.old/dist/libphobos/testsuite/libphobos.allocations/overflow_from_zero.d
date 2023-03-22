0eac2ef93757d93f111ad2e5623f999920d62dab
martin <martin@NetBSD.org>
2023-03-22 17:43:52 +0000

    Remove directory, requested by mrg in ticket #123:
    	external/gpl3/gcc.old		(remove)
    
    This directory is not used on this branch.

--- external/gpl3/gcc.old/dist/libphobos/testsuite/libphobos.allocations/overflow_from_zero.d
+++
@@ -1,8 +0,0 @@
-// { dg-shouldfail "Memory allocation failed" }
-// { dg-output "core.exception.OutOfMemoryError@.*: Memory allocation failed" }
-void main()
-{
-    void[] buffer;
-    buffer.length = 0;
-    buffer.length = size_t.max;
-}
