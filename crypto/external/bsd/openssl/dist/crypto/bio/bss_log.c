b70acc17d81021ba22a59ec13a60f620a1e90882
riastradh <riastradh@NetBSD.org>
2023-03-29 10:25:11 +0000

    openssl: Omit needless sprintf->snprintf under OPENSSL_SYS_WIN32.
    Much as I'm happy to eliminate sprintf, there's very little value to
    maintaining a local change under an #ifdef that will never, ever be
    taken on NetBSD.
    
    Verified libcrypto.so does not sprout any references to sprintf as a
    result.

--- crypto/external/bsd/openssl/dist/crypto/bio/bss_log.c
+++ crypto/external/bsd/openssl/dist/crypto/bio/bss_log.c
@@ -280,7 +280,7 @@ static void xsyslog(BIO *bp, int priority, const char *string)
         break;
     }
 
-    snprintf(pidbuf, sizeof(pidbuf), "[%u] ", GetCurrentProcessId());
+    sprintf(pidbuf, "[%u] ", GetCurrentProcessId());
     lpszStrings[0] = pidbuf;
     lpszStrings[1] = string;
 
