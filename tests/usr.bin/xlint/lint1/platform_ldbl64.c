52b9bc5987f94e4e2bd871fe7a37714c97a8f6ba
rillig <rillig@NetBSD.org>
2023-04-05 20:13:01 +0000

    tests/lint: demonstrate duplicate warning when parsing big floats

--- tests/usr.bin/xlint/lint1/platform_ldbl64.c
+++ tests/usr.bin/xlint/lint1/platform_ldbl64.c
@@ -1,13 +1,22 @@
-/*	$NetBSD: platform_ldbl64.c,v 1.3 2023/02/22 22:12:35 rillig Exp $	*/
+/*	$NetBSD: platform_ldbl64.c,v 1.4 2023/04/05 20:13:01 rillig Exp $	*/
 # 3 "platform_ldbl64.c"
 
 /*
  * Test features that only apply to platforms that have 64-bit long double.
  */
 
-/* lint1-extra-flags: -c -h -a -p -b -r -z */
+/* lint1-extra-flags: -c -h -a -p -b -r -z -X 351 */
 /* lint1-only-if: ldbl-64 */
 
 /* CONSTCOND */
 typedef int bits_per_byte[((unsigned char)-1) == 255 ? 1 : -1];
 typedef int bytes_per_long_double[sizeof(long double) == 8 ? 1 : -1];
+
+/* expect+1: warning: floating-point constant out of range [248] */
+double larger_than_ldbl = 1e310;
+/*
+ * Since 'long double' has the same size as 'double', there is no floating
+ * point constant that fits in 'long double' but not in 'double'.
+ */
+/* expect+1: warning: floating-point constant out of range [248] */
+long double larger_than_ldbl_l = 1e310L;
