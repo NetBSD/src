561c0944e113575a10510c4ff6f5b68cff606cce
rillig <rillig@NetBSD.org>
2023-04-10 23:52:49 +0000

    tests/lint: demonstrate wrong warning with complex numbers

--- tests/usr.bin/xlint/lint1/msg_142.c
+++ tests/usr.bin/xlint/lint1/msg_142.c
@@ -1,10 +1,15 @@
-/*	$NetBSD: msg_142.c,v 1.8 2023/03/28 14:44:34 rillig Exp $	*/
+/*	$NetBSD: msg_142.c,v 1.9 2023/04/10 23:52:49 rillig Exp $	*/
 # 3 "msg_142.c"
 
 // Test for message: floating point overflow on operator '%s' [142]
 
-/* lint1-only-if: ldbl-64 */
 /* lint1-extra-flags: -X 351 */
+
+/*
+ * VAX has floating point formats with different limits than the other
+ * platforms, which all implement IEEE 754.
+ */
+/* xlint1-skip-if: vax */
 
 /*
  * For 96-bit and 128-bit floating point numbers, a different number of
@@ -14,3 +19,6 @@
 /* expect+2: warning: floating point overflow on operator '*' [142] */
 /* expect+1: warning: floating point overflow on operator '*' [142] */
 double dbl = 1e100 * 1e100 * 1e100 * 1e100 * 1e100;
+
+/* expect+1: warning: floating point overflow on operator '+' [142] */
+double _Complex complex_sum = 1e308 + 1e308i;
