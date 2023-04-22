6fc4b018f8f0e3f3b44c5a45775a16c3a343eb8e
riastradh <riastradh@NetBSD.org>
2023-04-22 14:05:36 +0000

    Welcome to 10.99.4, courtesy of struct fileops additions!

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.725 2023/04/12 06:49:17 riastradh Exp $	*/
+/*	$NetBSD: param.h,v 1.726 2023/04/22 14:05:36 riastradh Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -67,7 +67,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	1099000300	/* NetBSD 10.99.3 */
+#define	__NetBSD_Version__	1099000400	/* NetBSD 10.99.4 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
