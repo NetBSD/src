976dee0a9f9389591a5ca3d6669da55c93085252
bouyer <bouyer@NetBSD.org>
2023-05-04 17:09:44 +0000

    The i.mx6sx has 2 OTG and one host-only USB controller, while the 6q has only one OTG. Add a "uintptr_t data" argument to all sc_*_md_hook callbacks, which gets the sc_md_hook_data value when called. In imx6_usb.c use this to pass the number of OTG controllers to the callbacks. imx6_usb_init() can then properly call init_otg() or init_h1() for unit 1.
    In imx6_usb_attach(), test if there is a vbus-supply property in the fdt,
    and enable the regulator if present.
    
    Now the USB port of the UDOO Neo works.

--- sys/arch/arm/imx/imx23_usb.c
+++ sys/arch/arm/imx/imx23_usb.c
@@ -1,4 +1,4 @@
-/* $Id: imx23_usb.c,v 1.5 2021/08/07 16:18:44 thorpej Exp $ */
+/* $Id: imx23_usb.c,v 1.6 2023/05/04 17:09:44 bouyer Exp $ */
 
 /*
  * Copyright (c) 2013 The NetBSD Foundation, Inc.
@@ -152,7 +152,7 @@ imxusbc_search(device_t parent, cfdata_t cf, const int *ldesc, void *aux)
 }
 
 static
-void imx23_usb_init(struct imxehci_softc *sc)
+void imx23_usb_init(struct imxehci_softc *sc, uintptr_t data)
 {
 
 	sc->sc_iftype = IMXUSBC_IF_UTMI;
