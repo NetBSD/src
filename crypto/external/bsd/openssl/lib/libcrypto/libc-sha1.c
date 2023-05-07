f4748aaa01faf324805f9747191535eb6600f82c
christos <christos@NetBSD.org>
2023-05-07 16:22:10 +0000

    more checkpointing

--- crypto/external/bsd/openssl/lib/libcrypto/libc-sha1.c
+++ crypto/external/bsd/openssl/lib/libcrypto/libc-sha1.c
@@ -20,6 +20,21 @@
 #include <openssl/evp.h>
 #include "crypto/sha.h"
 
+unsigned char *ossl_sha1(const unsigned char *d, size_t n, unsigned char *md)
+{
+    SHA_CTX c;
+    static unsigned char m[SHA_DIGEST_LENGTH];
+
+    if (md == NULL)
+        md = m;
+    if (!SHA1_Init(&c))
+        return NULL;
+    SHA1_Update(&c, d, n);
+    SHA1_Final(md, &c);
+    OPENSSL_cleanse(&c, sizeof(c));
+    return md;
+}
+
 unsigned char *SHA1(const unsigned char *d, size_t n, unsigned char *md)
 {
     static unsigned char m[SHA_DIGEST_LENGTH];
