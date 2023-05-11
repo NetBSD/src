956e356f51a265b4e41d4699c25b5a9fe8206577
gutteridge <gutteridge@NetBSD.org>
2023-05-11 01:56:31 +0000

    t_fss.sh: add missing test case head

--- tests/dev/fss/t_fss.sh
+++ tests/dev/fss/t_fss.sh
@@ -1,4 +1,4 @@
-# $NetBSD: t_fss.sh,v 1.5 2022/11/30 17:49:59 martin Exp $
+# $NetBSD: t_fss.sh,v 1.6 2023/05/11 01:56:31 gutteridge Exp $
 #
 # Copyright (c) 2006, 2007, 2008 The NetBSD Foundation, Inc.
 # All rights reserved.
@@ -35,6 +35,11 @@ orig_data="Original data"
 repl_data="Replacement data"
 
 atf_test_case basic cleanup
+basic_head() {
+	atf_set "descr" "Verify basic operation of fss(4) file system " \
+		"snapshot device"
+}
+
 basic_body() {
 
 # verify fss is available (or loadable as a module)
@@ -81,5 +86,5 @@ basic_cleanup() {
 
 atf_init_test_cases()
 {
-        atf_add_test_case basic
+	atf_add_test_case basic
 }
