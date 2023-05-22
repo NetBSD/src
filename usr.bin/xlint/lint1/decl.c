960cfb1b4ef2f541028ec6642c91647aa08a0a5e
rillig <rillig@NetBSD.org>
2023-05-22 17:53:27 +0000

    lint: fix emitted type for arrays of unknown size

--- usr.bin/xlint/lint1/decl.c
+++ usr.bin/xlint/lint1/decl.c
@@ -1,4 +1,4 @@
-/* $NetBSD: decl.c,v 1.312 2023/05/22 17:47:27 rillig Exp $ */
+/* $NetBSD: decl.c,v 1.313 2023/05/22 17:53:27 rillig Exp $ */
 
 /*
  * Copyright (c) 1996 Christopher G. Demetriou.  All Rights Reserved.
@@ -38,7 +38,7 @@
 
 #include <sys/cdefs.h>
 #if defined(__RCSID)
-__RCSID("$NetBSD: decl.c,v 1.312 2023/05/22 17:47:27 rillig Exp $");
+__RCSID("$NetBSD: decl.c,v 1.313 2023/05/22 17:53:27 rillig Exp $");
 #endif
 
 #include <sys/param.h>
@@ -1992,7 +1992,8 @@ declare_extern(sym_t *dsym, bool has_initializer, sbuf_t *renaming)
 		 */
 		bool rval = dsym->s_type->t_subt->t_tspec != VOID;
 		outfdef(dsym, &dsym->s_def_pos, rval, false, NULL);
-	} else if (!is_compiler_builtin(dsym->s_name)) {
+	} else if (!is_compiler_builtin(dsym->s_name)
+	    && !(has_initializer && dsym->s_type->t_incomplete_array)) {
 		outsym(dsym, dsym->s_scl, dsym->s_def);
 	}
 
