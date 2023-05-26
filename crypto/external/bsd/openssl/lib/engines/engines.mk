1bca1b82cb6c049268b4255ef6f597e478b90d7a
riastradh <riastradh@NetBSD.org>
2023-05-26 22:26:10 +0000

    Revert "openssl: Nix build goo for dynamically loaded engines."
    Turns out there is a regression lurking here: the devcrypto engine
    was made dynamic-only (https://github.com/openssl/openssl/pull/7859),
    so to maintain that functionality, we need to build dynamic engines.
    Will fix the aarch64 build issue and wire things up separately.

---
+++ crypto/external/bsd/openssl/lib/engines/engines.mk
@@ -0,0 +1,34 @@
+#	$NetBSD: engines.mk,v 1.5 2023/05/26 22:26:10 riastradh Exp $
+
+NOLINT=		# don't build a lint library
+NOPROFILE=	# don't build a profile library
+NOPICINSTALL=	# don't install _pic.a library
+
+.include <bsd.own.mk>
+
+SHLIB_MAJOR=0
+SHLIB_MINOR=0
+
+CRYPTODIST=     ${NETBSDSRCDIR}/crypto
+.include "${NETBSDSRCDIR}/crypto/Makefile.openssl"
+.PATH: ${OPENSSLSRC}/engines
+
+CPPFLAGS+= -I${OPENSSLSRC}/include -I${OPENSSLSRC}/../include
+
+LIBDIR=${OSSL_ENGINESDIR}
+
+.if ${MKPIC} != "no"
+.PRECIOUS: ${DESTDIR}${LIBDIR}/${LIB}.so
+libinstall:: ${DESTDIR}${LIBDIR}/${LIB}.so
+.else
+libinstall::
+.endif
+
+LDFLAGS+=-Wl,--version-script=${.CURDIR}/${LIB}.map
+
+.include <bsd.lib.mk>
+
+${DESTDIR}${LIBDIR}/${LIB}.so: lib${LIB}.so.${SHLIB_FULLVERSION}
+	${_MKTARGET_INSTALL}
+	${INSTALL_FILE} -o ${LIBOWN} -g ${LIBGRP} -m ${LIBMODE} \
+	    ${.ALLSRC} ${.TARGET}
