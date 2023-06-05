c55e65d7ff9baa8d77aaf8cdd762a1957e5c871f
rillig <rillig@NetBSD.org>
2023-06-05 08:10:25 +0000

    tests/indent: add tests for 'do-while' loops

--- tests/usr.bin/indent/psym_do.c
+++ tests/usr.bin/indent/psym_do.c
@@ -1,8 +1,12 @@
-/* $NetBSD: psym_do.c,v 1.4 2022/04/24 10:36:37 rillig Exp $ */
+/* $NetBSD: psym_do.c,v 1.5 2023/06/05 08:10:25 rillig Exp $ */
 
 /*
  * Tests for the parser symbol psym_do, which represents the state after
  * reading the token 'do', now waiting for the statement of the loop body.
+ *
+ * See also:
+ *	lsym_do.c
+ *	psym_do_stmt.c
  */
 
 //indent input
