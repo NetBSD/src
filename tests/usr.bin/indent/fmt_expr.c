c2db4af95dd9e890b009c1ac72e6dd53423b669a
rillig <rillig@NetBSD.org>
2023-06-09 09:45:55 +0000

    tests/indent: add real-life test cases from make(1) and lint(1)

--- tests/usr.bin/indent/fmt_expr.c
+++ tests/usr.bin/indent/fmt_expr.c
@@ -1,4 +1,4 @@
-/* $NetBSD: fmt_expr.c,v 1.4 2022/04/24 09:04:12 rillig Exp $ */
+/* $NetBSD: fmt_expr.c,v 1.5 2023/06/09 09:45:55 rillig Exp $ */
 
 /*
  * Tests for all kinds of expressions that are not directly related to unary
@@ -23,3 +23,14 @@ t(void)
 	int		n = malloc(offsetof(struct s, f) + 1);
 }
 //indent end
+
+
+//indent input
+{
+	for (ln = gnodes->first; ln != NULL; ln = ln->next)
+// $ FIXME: No space after the cast.
+		*(GNode **) Vector_Push(&vec) = ln->datum;
+}
+//indent end
+
+//indent run-equals-input
