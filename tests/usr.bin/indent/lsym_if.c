15cbd748df0b9e870316bc95643e4cc22996112b
rillig <rillig@NetBSD.org>
2023-06-10 16:43:55 +0000

    indent: miscellaneous cleanups

--- tests/usr.bin/indent/lsym_if.c
+++ tests/usr.bin/indent/lsym_if.c
@@ -1,4 +1,4 @@
-/* $NetBSD: lsym_if.c,v 1.4 2022/04/24 10:36:37 rillig Exp $ */
+/* $NetBSD: lsym_if.c,v 1.5 2023/06/10 16:43:56 rillig Exp $ */
 
 /*
  * Tests for the token lsym_if, which represents the keyword 'if' that starts
@@ -19,5 +19,27 @@ function(void)
 {
 	if (cond)
 		stmt();
+}
+//indent end
+
+
+/*
+ * After an 'if' statement without an 'else' branch, braces start a separate
+ * block.
+ */
+//indent input
+{
+	if(0)if(1)if(2)stmt();{}
+}
+//indent end
+
+//indent run
+{
+	if (0)
+		if (1)
+			if (2)
+// $ FIXME: The '{' must be on a separate line, with indentation 8.
+				stmt(); {
+				}
 }
 //indent end
