4bcf3729408188a7f90d8cf7eb873b9ea855ca61
martin <martin@NetBSD.org>
2023-06-21 20:48:06 +0000

    Pull up following revision(s) (requested by riastradh in ticket #1835):
    	sys/dev/pci/if_iwi.c: revision 1.117
    	sys/dev/raidframe/rf_netbsdkintf.c: revision 1.401
    	sys/dev/scsipi/ses.c: revision 1.52
    	sys/dev/isa/mcd.c: revision 1.121
    	(all via patch)
    
    sys/dev: Memset zero before copyout.
    
    Just in case of uninitialized padding which would lead to kernel
    stack disclosure.  If the compiler can prove the memset redundant
    then it can optimize it away; otherwise better safe than sorry.
    
    I think the iwi(4), mcd(4), and ses(4) changes actually plug leaks;
    the raidframe(4) change probably doesn't (but doesn't hurt).

--- sys/dev/scsipi/ses.c
+++ sys/dev/scsipi/ses.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: ses.c,v 1.50 2016/11/20 15:37:19 mlelstv Exp $ */
+/*	$NetBSD: ses.c,v 1.50.8.1 2023/06/21 20:48:07 martin Exp $ */
 /*
  * Copyright (C) 2000 National Aeronautics & Space Administration
  * All rights reserved.
@@ -26,7 +26,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: ses.c,v 1.50 2016/11/20 15:37:19 mlelstv Exp $");
+__KERNEL_RCSID(0, "$NetBSD: ses.c,v 1.50.8.1 2023/06/21 20:48:07 martin Exp $");
 
 #ifdef _KERNEL_OPT
 #include "opt_scsi.h"
@@ -415,6 +415,7 @@ sesioctl(dev_t dev, u_long cmd, void *arg_addr, int flag, struct lwp *l)
 	case SESIOC_GETOBJMAP:
 		if (addr == NULL)
 			return EINVAL;
+		memset(&obj, 0, sizeof(obj));
 		for (uobj = addr, i = 0; i != ssc->ses_nobjects; i++, uobj++) {
 			obj.obj_id = i;
 			obj.subencid = ssc->ses_objmap[i].subenclosure;
