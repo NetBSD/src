10bd4ec80470904202f408e3b7a1bb4a48d6f6e6
rillig <rillig@NetBSD.org>
2023-07-03 09:33:07 +0000

    lint: fix C11 mode to not allow C23 features (since yesterday)

--- tests/usr.bin/xlint/lint1/msg_353.c
+++ tests/usr.bin/xlint/lint1/msg_353.c
@@ -1,10 +1,12 @@
-/*	$NetBSD: msg_353.c,v 1.1 2023/07/02 23:45:10 rillig Exp $	*/
+/*	$NetBSD: msg_353.c,v 1.2 2023/07/03 09:33:07 rillig Exp $	*/
 # 3 "msg_353.c"
 
 // Test for message 353: empty initializer braces require C23 or later [353]
 //
 // See also:
 //	c23.c
+
+/* lint1-extra-flags: -Ac11 */
 
 void
 c23(void)
