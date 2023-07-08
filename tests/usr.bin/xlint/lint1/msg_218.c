5880b286cc99d7de8243d645f790dfd2abc8af94
rillig <rillig@NetBSD.org>
2023-07-08 11:03:00 +0000

    tests/lint: fix a few ilp32 tests

--- tests/usr.bin/xlint/lint1/msg_218.c
+++ tests/usr.bin/xlint/lint1/msg_218.c
@@ -1,15 +1,17 @@
-/*	$NetBSD: msg_218.c,v 1.7 2023/03/28 14:44:35 rillig Exp $	*/
+/*	$NetBSD: msg_218.c,v 1.8 2023/07/08 11:03:00 rillig Exp $	*/
 # 3 "msg_218.c"
 
-// Test for message: ANSI C treats constant as unsigned, op '%s' [218]
+/* Test for message: ANSI C treats constant as unsigned, op '%s' [218] */
 
 /* lint1-only-if: ilp32 */
-/* lint1-extra-flags: -X 351 */
+/* lint1-flags: -w -X 351 */
 
-_Bool cond;
+int cond;
 signed int s32;
 unsigned int u32;
+/* expect+1: warning: C90 does not support 'long long' [265] */
 signed long long s64;
+/* expect+1: warning: C90 does not support 'long long' [265] */
 unsigned long long u64;
 
 void sink_int(int);
@@ -18,6 +20,7 @@ void sink_int(int);
 void
 test_signed_int(void)
 {
+	/* expect+2: warning: ANSI C treats constant as unsigned, op '-' [218] */
 	/* expect+1: warning: conversion of 'unsigned long' to 'int' is out of range, arg #1 [295] */
 	sink_int(-2147483648);
 }
@@ -37,12 +40,20 @@ test_signed_int(void)
 void
 compare_large_constant(void)
 {
+	/* expect+1: warning: ANSI C treats constant as unsigned, op '<' [218] */
 	cond = s32 < 3000000000L;
+	/* expect+1: warning: ANSI C treats constant as unsigned, op '<' [218] */
 	cond = 3000000000L < s32;
+	/* expect+1: warning: ANSI C treats constant as unsigned, op '<' [218] */
 	cond = u32 < 3000000000L;
+	/* expect+1: warning: ANSI C treats constant as unsigned, op '<' [218] */
 	cond = 3000000000L < u32;
+	/* expect+1: warning: ANSI C treats constant as unsigned, op '<' [218] */
 	cond = s64 < 3000000000L;
+	/* expect+1: warning: ANSI C treats constant as unsigned, op '<' [218] */
 	cond = 3000000000L < s64;
+	/* expect+1: warning: ANSI C treats constant as unsigned, op '<' [218] */
 	cond = u64 < 3000000000L;
+	/* expect+1: warning: ANSI C treats constant as unsigned, op '<' [218] */
 	cond = 3000000000L < u64;
 }
