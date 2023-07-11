02e5feec3ee8aa57760a4b6b4702ad82c566425f
mrg <mrg@NetBSD.org>
2023-07-11 18:13:27 +0000

    bump gcc 10 version.

--- tools/gcc/gcc-version.mk
+++ tools/gcc/gcc-version.mk
@@ -1,11 +1,7 @@
-#	$NetBSD: gcc-version.mk,v 1.22 2022/07/23 19:01:18 mrg Exp $
+#	$NetBSD: gcc-version.mk,v 1.23 2023/07/11 18:13:27 mrg Exp $
 
 # common location for tools and native build
 
-.if ${HAVE_GCC} == 8
-NETBSD_GCC_VERSION=nb1 20200311
-.elif ${HAVE_GCC} == 9
-NETBSD_GCC_VERSION=nb1 20200907
-.elif ${HAVE_GCC} == 10
-NETBSD_GCC_VERSION=nb1 20220722
+.if ${HAVE_GCC} == 10
+NETBSD_GCC_VERSION=nb2 20230710
 .endif
