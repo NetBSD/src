1daee1fee22c4527f54943c2083d87fe41934831
rillig <rillig@NetBSD.org>
2023-07-13 20:30:21 +0000

    lint: in C23 mode, recognize the keyword 'thread_local'

--- usr.bin/xlint/lint1/decl.c
+++ usr.bin/xlint/lint1/decl.c
@@ -1,4 +1,4 @@
-/* $NetBSD: decl.c,v 1.352 2023/07/13 19:59:08 rillig Exp $ */
+/* $NetBSD: decl.c,v 1.353 2023/07/13 20:30:21 rillig Exp $ */
 
 /*
  * Copyright (c) 1996 Christopher G. Demetriou.  All Rights Reserved.
@@ -38,7 +38,7 @@
 
 #include <sys/cdefs.h>
 #if defined(__RCSID)
-__RCSID("$NetBSD: decl.c,v 1.352 2023/07/13 19:59:08 rillig Exp $");
+__RCSID("$NetBSD: decl.c,v 1.353 2023/07/13 20:30:21 rillig Exp $");
 #endif
 
 #include <sys/param.h>
@@ -1514,7 +1514,8 @@ declarator_name(sym_t *sym)
 			 */
 			sc = AUTO;
 			sym->s_def = DEF;
-		} else if (sc == AUTO || sc == STATIC || sc == TYPEDEF)
+		} else if (sc == AUTO || sc == STATIC || sc == TYPEDEF
+		    || sc == THREAD_LOCAL)
 			sym->s_def = DEF;
 		else if (sc == REG) {
 			sym->s_register = true;
