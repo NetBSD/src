3a0e83faf677554046b20c462f2f557733e024bc
rillig <rillig@NetBSD.org>
2023-07-13 08:40:38 +0000

    lint: indent copyright lines consistently

--- usr.bin/xlint/lint1/op.h
+++ usr.bin/xlint/lint1/op.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: op.h,v 1.20 2022/06/15 18:29:21 rillig Exp $	*/
+/*	$NetBSD: op.h,v 1.21 2023/07/13 08:40:38 rillig Exp $	*/
 
 /*
  * Copyright (c) 1994, 1995 Jochen Pohl
@@ -14,7 +14,7 @@
  *    documentation and/or other materials provided with the distribution.
  * 3. All advertising materials mentioning features or use of this software
  *    must display the following acknowledgement:
- *      This product includes software developed by Jochen Pohl for
+ *	This product includes software developed by Jochen Pohl for
  *	The NetBSD Project.
  * 4. The name of the author may not be used to endorse or promote products
  *    derived from this software without specific prior written permission.
