1595239a2c9ae31744bda6b9f49263b5793768c9
riastradh <riastradh@NetBSD.org>
2023-07-13 18:27:46 +0000

    ia64: Need sys/types.h for u_int, vaddr_t; sys/mutex.h for kmutex_t.

--- sys/arch/ia64/include/sapicvar.h
+++ sys/arch/ia64/include/sapicvar.h
@@ -29,6 +29,10 @@
 #ifndef _MACHINE_SAPICVAR_H_
 #define _MACHINE_SAPICVAR_H_
 
+#include <sys/types.h>
+
+#include <sys/mutex.h>
+
 struct sapic {
 	kmutex_t	sa_mtx;
 	vaddr_t		sa_registers;	/* virtual address of sapic */
