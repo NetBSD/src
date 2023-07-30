e791292b6c2135953d4b86a7bad800de58260d5f
mrg <mrg@NetBSD.org>
2023-07-30 06:36:21 +0000

    mark our GCC 12 as "nb1 20230729".

--- tools/gcc/gcc-version.mk
+++ tools/gcc/gcc-version.mk
@@ -1,7 +1,10 @@
-#	$NetBSD: gcc-version.mk,v 1.23 2023/07/11 18:13:27 mrg Exp $
+#	$NetBSD: gcc-version.mk,v 1.24 2023/07/30 06:36:21 mrg Exp $
 
 # common location for tools and native build
 
 .if ${HAVE_GCC} == 10
 NETBSD_GCC_VERSION=nb2 20230710
+.endif
+.if ${HAVE_GCC} == 12
+NETBSD_GCC_VERSION=nb1 20230729
 .endif
