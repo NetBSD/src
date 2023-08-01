b86bdcb2e1faa198ab063f8407babf280e516a8e
martin <martin@NetBSD.org>
2023-08-01 16:55:03 +0000

    Pull up following revision(s) (requested by riastradh in ticket #299):
    	sys/external/bsd/drm2/linux/linux_ww_mutex.c: revision 1.16
    
    drm/linux_ww_mutex: Fix ww acquire context ordering.

--- sys/external/bsd/drm2/linux/linux_ww_mutex.c
+++ sys/external/bsd/drm2/linux/linux_ww_mutex.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: linux_ww_mutex.c,v 1.14.4.1 2023/08/01 16:53:18 martin Exp $	*/
+/*	$NetBSD: linux_ww_mutex.c,v 1.14.4.2 2023/08/01 16:55:03 martin Exp $	*/
 
 /*-
  * Copyright (c) 2014 The NetBSD Foundation, Inc.
@@ -30,7 +30,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: linux_ww_mutex.c,v 1.14.4.1 2023/08/01 16:53:18 martin Exp $");
+__KERNEL_RCSID(0, "$NetBSD: linux_ww_mutex.c,v 1.14.4.2 2023/08/01 16:55:03 martin Exp $");
 
 #include <sys/types.h>
 #include <sys/atomic.h>
@@ -62,7 +62,7 @@ ww_acquire_ctx_compare(void *cookie __unused, const void *va, const void *vb)
 	if (ctx_a->wwx_ticket < ctx_b->wwx_ticket)
 		return -1;
 	if (ctx_a->wwx_ticket > ctx_b->wwx_ticket)
-		return -1;
+		return +1;
 	return 0;
 }
 
@@ -76,7 +76,7 @@ ww_acquire_ctx_compare_key(void *cookie __unused, const void *vn,
 	if (ctx->wwx_ticket < ticket)
 		return -1;
 	if (ctx->wwx_ticket > ticket)
-		return -1;
+		return +1;
 	return 0;
 }
 
