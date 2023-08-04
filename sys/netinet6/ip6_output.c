109b57ebd016383fc8361e5b9d04693cab4b1181
martin <martin@NetBSD.org>
2023-08-04 14:28:01 +0000

    Pull up following revision(s) (requested by ozaki-r in ticket #310):
    	sys/netinet6/in6.c: revision 1.289
    	sys/netinet6/ip6_output.c: revision 1.234
    
    in6: clear ND6_IFF_IFDISABLED to allow DAD again on link-up
    
    in6: don't send any IPv6 packets over a disabled interface

--- sys/netinet6/ip6_output.c
+++ sys/netinet6/ip6_output.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: ip6_output.c,v 1.231.2.1 2023/03/23 12:03:04 martin Exp $	*/
+/*	$NetBSD: ip6_output.c,v 1.231.2.2 2023/08/04 14:28:01 martin Exp $	*/
 /*	$KAME: ip6_output.c,v 1.172 2001/03/25 09:55:56 itojun Exp $	*/
 
 /*
@@ -62,7 +62,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: ip6_output.c,v 1.231.2.1 2023/03/23 12:03:04 martin Exp $");
+__KERNEL_RCSID(0, "$NetBSD: ip6_output.c,v 1.231.2.2 2023/08/04 14:28:01 martin Exp $");
 
 #ifdef _KERNEL_OPT
 #include "opt_inet.h"
@@ -169,6 +169,12 @@ ip6_if_output(struct ifnet * const ifp, struct ifnet * const origifp,
 			m_freem(m);
 			return error;
 		}
+	}
+
+	/* discard the packet if IPv6 operation is disabled on the interface */
+	if ((ND_IFINFO(ifp)->flags & ND6_IFF_IFDISABLED)) {
+		m_freem(m);
+		return ENETDOWN; /* better error? */
 	}
 
 	if ((ifp->if_flags & IFF_LOOPBACK) != 0)
