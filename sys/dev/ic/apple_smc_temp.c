df4d49549c23968fb3f4045c8429ffd8f5b9965b
mrg <mrg@NetBSD.org>
2023-08-08 05:20:14 +0000

    fix array vs pointer function declaration.
    also, in some callers, and make sure to provide all the accessed
    backing for a string array.
    
    found by GCC 12.

--- sys/dev/ic/apple_smc_temp.c
+++ sys/dev/ic/apple_smc_temp.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: apple_smc_temp.c,v 1.6 2022/06/29 15:58:12 mlelstv Exp $	*/
+/*	$NetBSD: apple_smc_temp.c,v 1.7 2023/08/08 05:20:14 mrg Exp $	*/
 
 /*
  * Apple System Management Controller: Temperature Sensors
@@ -34,7 +34,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: apple_smc_temp.c,v 1.6 2022/06/29 15:58:12 mlelstv Exp $");
+__KERNEL_RCSID(0, "$NetBSD: apple_smc_temp.c,v 1.7 2023/08/08 05:20:14 mrg Exp $");
 
 #include <sys/types.h>
 #include <sys/param.h>
@@ -425,12 +425,12 @@ apple_smc_bound_temp_sensors(struct apple_smc_tag *smc, uint32_t *tstart,
 	int error;
 
 	/* Find the first `T...' key.  */
-	error = apple_smc_key_search(smc, "T", tstart);
+	error = apple_smc_key_search(smc, "T\0\0\0", tstart);
 	if (error)
 		return error;
 
 	/* Find the first `U...' key.  */
-	error = apple_smc_key_search(smc, "U", ustart);
+	error = apple_smc_key_search(smc, "U\0\0\0", ustart);
 	if (error)
 		return error;
 
