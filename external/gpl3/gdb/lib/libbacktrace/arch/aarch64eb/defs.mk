3981945464918dc9f9d47cd8bdc4b062296d6de7
christos <christos@NetBSD.org>
2023-08-09 22:14:23 +0000

    do aarch64eb

--- external/gpl3/gdb/lib/libbacktrace/arch/aarch64eb/defs.mk
+++ external/gpl3/gdb/lib/libbacktrace/arch/aarch64eb/defs.mk
@@ -2,4 +2,5 @@
 # Generated from: NetBSD: mknative-gdb,v 1.16 2023/07/31 17:09:59 christos Exp 
 # Generated from: NetBSD: mknative.common,v 1.16 2018/04/15 15:13:37 christos Exp 
 #
-G_OBJS=decNumber.o decContext.o decimal32.o decimal64.o decimal128.o bid2dpd_dpd2bid.o host-ieee32.o host-ieee64.o host-ieee128.o
+G_OBJECTS=atomic.o dwarf.o fileline.o posix.o print.o sort.o state.o
+G_LIBADD=backtrace.o simple.o elf.o mmapio.o mmap.o
--- external/gpl3/gdb/lib/libsframe/arch/aarch64eb/defs.mk
+++ external/gpl3/gdb/lib/libsframe/arch/aarch64eb/defs.mk
@@ -2,4 +2,4 @@
 # Generated from: NetBSD: mknative-gdb,v 1.16 2023/07/31 17:09:59 christos Exp 
 # Generated from: NetBSD: mknative.common,v 1.16 2018/04/15 15:13:37 christos Exp 
 #
-G_OBJS=decNumber.o decContext.o decimal32.o decimal64.o decimal128.o bid2dpd_dpd2bid.o host-ieee32.o host-ieee64.o host-ieee128.o
+G_OBJECTS=sframe.o sframe-dump.o sframe-error.o
