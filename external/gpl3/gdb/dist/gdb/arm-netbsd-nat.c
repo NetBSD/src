1af58d72695d1ccf605a3aaf8cef99899b90d082
christos <christos@NetBSD.org>
2023-08-11 20:28:31 +0000

    Add arm, vax

--- external/gpl3/gdb/dist/gdb/arm-netbsd-nat.c
+++ external/gpl3/gdb/dist/gdb/arm-netbsd-nat.c
@@ -17,8 +17,6 @@
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
 
-/* We define this to get types like register_t.  */
-#define _KERNTYPES
 #include "defs.h"
 #include "gdbcore.h"
 #include "inferior.h"
