fdd1c406901aedc783574ca29f4b7e6c699d4d15
rillig <rillig@NetBSD.org>
2023-08-11 04:27:49 +0000

    tests/lint: test parsing of preprocessing directives

--- tests/usr.bin/xlint/lint1/msg_255.c
+++ tests/usr.bin/xlint/lint1/msg_255.c
@@ -1,13 +1,45 @@
-/*	$NetBSD: msg_255.c,v 1.5 2023/07/09 11:01:27 rillig Exp $	*/
+/*	$NetBSD: msg_255.c,v 1.6 2023/08/11 04:27:49 rillig Exp $	*/
 # 3 "msg_255.c"
 
 // Test for message: undefined or invalid '#' directive [255]
 
 /* lint1-extra-flags: -X 351 */
 
+/* expect+1: warning: undefined or invalid '#' directive [255] */
+#
+
+/* expect+1: warning: undefined or invalid '#' directive [255] */
+#pragma
+
 #pragma once
 
 /* expect+1: warning: undefined or invalid '#' directive [255] */
 #fatal_error
+
+/* expect+1: warning: undefined or invalid '#' directive [255] */
+#    ident "obsolete"
+
+/* expect+1: warning: undefined or invalid '#' directive [255] */
+#1
+
+// Sets the line number of the current file.
+# 2
+
+// Switch back to the main file.
+# 30 "msg_255.c"
+
+/* expect+1: warning: undefined or invalid '#' directive [255] */
+# 3/
+
+/* expect+1: warning: undefined or invalid '#' directive [255] */
+# 4 /
+
+/* expect+1: warning: undefined or invalid '#' directive [255] */
+# 5 "unfinished
+
+// An empty string means standard input; tabs may be used for spacing.
+#	6	""
+
+# 44 "msg_255.c"
 
 int dummy;
