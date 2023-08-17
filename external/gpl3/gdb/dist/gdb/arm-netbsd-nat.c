63c3f15604925e8c3170fc796a1eb1168e955f4c
rin <rin@NetBSD.org>
2023-08-17 04:52:38 +0000

    gdb/arm: Restore upstream comment, accidentally lost during merge
    No binary changes.

--- external/gpl3/gdb/dist/gdb/arm-netbsd-nat.c
+++ external/gpl3/gdb/dist/gdb/arm-netbsd-nat.c
@@ -17,6 +17,7 @@
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
 
+/* We define this to get types like register_t.  */
 #include "defs.h"
 #include "gdbcore.h"
 #include "inferior.h"
