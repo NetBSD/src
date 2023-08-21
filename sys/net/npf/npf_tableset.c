86d6725cb8519c806fd838d79c09347e0c0bf63b
martin <martin@NetBSD.org>
2023-08-21 12:18:17 +0000

    Pull up following revision(s) (requested by riastradh in ticket #332):
    	sys/net/npf/npf_tableset.c: revision 1.41
    
    npf(9): Drop table lock around copyout.
    
    It is forbidden to hold a spin lock around copyout, and t_lock is a
    spin lock.
    
    We need t_lock in order to iterate over the list of entries.
    However, during copyout itself, we only need to ensure that the
    object we're copying out isn't freed by npf_table_remove or
    npf_table_gc.
    
    Fortunately, the only caller of npf_table_list, npf_table_remove, and
    npf_table_gc is npfctl_table, and it serializes all of them by the
    npf config lock.  So we can safely drop t_lock across copyout.
    
    PR kern/57136
    PR kern/57181

--- sys/net/npf/npf_tableset.c
+++ sys/net/npf/npf_tableset.c
@@ -46,7 +46,7 @@
 
 #ifdef _KERNEL
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: npf_tableset.c,v 1.38 2022/04/09 23:38:33 riastradh Exp $");
+__KERNEL_RCSID(0, "$NetBSD: npf_tableset.c,v 1.38.4.1 2023/08/21 12:18:17 martin Exp $");
 
 #include <sys/param.h>
 #include <sys/types.h>
@@ -762,15 +762,17 @@ table_ent_copyout(const npf_addr_t *addr, const int alen, npf_netmask_t mask,
 }
 
 static int
-table_generic_list(const npf_table_t *t, void *ubuf, size_t len)
+table_generic_list(npf_table_t *t, void *ubuf, size_t len)
 {
 	npf_tblent_t *ent;
 	size_t off = 0;
 	int error = 0;
 
 	LIST_FOREACH(ent, &t->t_list, te_listent) {
+		mutex_exit(&t->t_lock);
 		error = table_ent_copyout(&ent->te_addr,
 		    ent->te_alen, ent->te_preflen, ubuf, len, &off);
+		mutex_enter(&t->t_lock);
 		if (error)
 			break;
 	}
