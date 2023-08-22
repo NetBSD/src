c8a3215b40f64ccd21c868a639bbf03e417b97ac
martin <martin@NetBSD.org>
2023-08-22 16:10:21 +0000

    Pull up following revision(s) (requested by msaitoh in ticket #1720):
    	sys/arch/x86/pci/amdzentemp.c: revision 1.20
    	sys/arch/x86/pci/amdsmn.c: revision 1.17
    	sys/arch/x86/pci/amdzentemp.c: revision 1.19
    
    Add Zen4 Ryzen "Phoenix" support.
    Add Zen2 Mendocino APU support.
    Add Zen4 Phoenix support.

--- sys/arch/x86/pci/amdsmn.c
+++ sys/arch/x86/pci/amdsmn.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: amdsmn.c,v 1.5.2.4 2023/06/21 18:56:58 martin Exp $	*/
+/*	$NetBSD: amdsmn.c,v 1.5.2.5 2023/08/22 16:10:21 martin Exp $	*/
 
 /*-
  * Copyright (c) 2017, 2019 Conrad Meyer <cem@FreeBSD.org>
@@ -29,7 +29,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: amdsmn.c,v 1.5.2.4 2023/06/21 18:56:58 martin Exp $ ");
+__KERNEL_RCSID(0, "$NetBSD: amdsmn.c,v 1.5.2.5 2023/08/22 16:10:21 martin Exp $ ");
 
 /*
  * Driver for the AMD Family 15h (model 60+) and 17h CPU
@@ -109,6 +109,11 @@ static const struct pciid {
 	},
 	{
 		.amdsmn_deviceid = PCI_PRODUCT_AMD_F19_6X_RC,
+		.amdsmn_addr_reg = F17H_SMN_ADDR_REG,
+		.amdsmn_data_reg = F17H_SMN_DATA_REG,
+	},
+	{
+		.amdsmn_deviceid = PCI_PRODUCT_AMD_F19_7X_RC,
 		.amdsmn_addr_reg = F17H_SMN_ADDR_REG,
 		.amdsmn_data_reg = F17H_SMN_DATA_REG,
 	},
