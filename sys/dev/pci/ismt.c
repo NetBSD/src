6e85c1c9bc5f34a4a92d18bc6b6505d5f04e3ca4
martin <martin@NetBSD.org>
2023-08-23 17:13:08 +0000

    Pull up following revision(s) (requested by msaitoh in ticket #338):
    	sys/dev/pci/ichsmb.c: revision 1.85
    	sys/dev/pci/ismt.c: revision 1.11
    
    ichsmb(4),ismt(4): Add Snow Ridge support.

--- sys/dev/pci/ismt.c
+++ sys/dev/pci/ismt.c
@@ -60,7 +60,7 @@
 #if 0
 __FBSDID("$FreeBSD: head/sys/dev/ismt/ismt.c 266474 2014-05-20 19:55:06Z jimharris $");
 #endif
-__KERNEL_RCSID(0, "$NetBSD: ismt.c,v 1.9 2021/08/07 16:19:14 thorpej Exp $");
+__KERNEL_RCSID(0, "$NetBSD: ismt.c,v 1.9.6.1 2023/08/23 17:13:08 martin Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -778,6 +778,7 @@ ismt_match(device_t parent, cfdata_t match, void *aux)
 	case PCI_PRODUCT_INTEL_S1200_SMBUS_1:
 	case PCI_PRODUCT_INTEL_C2000_SMBUS:
 	case PCI_PRODUCT_INTEL_C3K_SMBUS:
+	case PCI_PRODUCT_INTEL_SNR_SMB_HOST:
 		break;
 	default:
 		return 0;
