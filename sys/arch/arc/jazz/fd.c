b523b3bec9b7c0de4c3807e3b3ef9b20ce27b96a
andvar <andvar@NetBSD.org>
2023-08-29 21:55:11 +0000

    arc/fdc(4): fix printf specifiers for FD_DEBUG build.

--- sys/arch/arc/jazz/fd.c
+++ sys/arch/arc/jazz/fd.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: fd.c,v 1.51 2021/08/07 16:18:42 thorpej Exp $	*/
+/*	$NetBSD: fd.c,v 1.52 2023/08/29 21:55:11 andvar Exp $	*/
 /*	$OpenBSD: fd.c,v 1.6 1998/10/03 21:18:57 millert Exp $	*/
 /*	NetBSD: fd.c,v 1.78 1995/07/04 07:23:09 mycroft Exp 	*/
 
@@ -66,7 +66,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: fd.c,v 1.51 2021/08/07 16:18:42 thorpej Exp $");
+__KERNEL_RCSID(0, "$NetBSD: fd.c,v 1.52 2023/08/29 21:55:11 andvar Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -470,8 +470,8 @@ fdstrategy(struct buf *bp)
 	    bp->b_blkno / (FDC_BSIZE / DEV_BSIZE) / fd->sc_type->seccyl;
 
 #ifdef FD_DEBUG
-	printf("%s: b_blkno %" PRId64 " b_bcount %ld blkno %" PRId64
-	    " cylin %ld sz %d\n", __func__,
+	printf("%s: b_blkno %" PRId64 " b_bcount %d blkno %" PRId64
+	    " cylin %d sz %d\n", __func__,
 	    bp->b_blkno, bp->b_bcount, fd->sc_blkno, bp->b_cylinder, sz);
 #endif
 
