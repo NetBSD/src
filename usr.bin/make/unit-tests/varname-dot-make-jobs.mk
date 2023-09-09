859d4f0b97f6a98ed7a10ad4b35d9d541486dd17
sjg <sjg@NetBSD.org>
2023-09-09 01:30:59 +0000

    make: allow -j to compute a multiple of ncpu
    If _SC_NPROCESSORS_ONLN is supported; and -j arg is a floating point
    number or ends in 'C' compute .MAKE.JOBS as a multiple of _SC_NPROCESSORS_ONLN
    
    Based on a suggestion from des at freebsd.org
    Discussed with: rillig, christos

--- usr.bin/make/unit-tests/varname-dot-make-jobs.mk
+++ usr.bin/make/unit-tests/varname-dot-make-jobs.mk
@@ -1,4 +1,4 @@
-# $NetBSD: varname-dot-make-jobs.mk,v 1.3 2022/01/26 22:47:03 rillig Exp $
+# $NetBSD: varname-dot-make-jobs.mk,v 1.4 2023/09/09 01:30:59 sjg Exp $
 #
 # Tests for the special .MAKE.JOBS variable, which is defined in jobs mode
 # only.  There it contains the number of jobs that may run in parallel.
@@ -15,10 +15,33 @@ all:
 	@${MAKE} -r -f ${MAKEFILE} echo -j20
 	@${MAKE} -r -f ${MAKEFILE} echo -j00000000000000000000000000000001
 
+.if !make(echo)
+# These results will not be static, we need NCPU
+# to compute expected results.
+# We only support -jC if _SC_NPROCESSORS_ONLN is defined,
+# otherwise we will get NCPU=0
+NCPU!= ${MAKE} -r -f /dev/null -jC -V .MAKE.JOBS 2> /dev/null || echo 0
+
+.if ${NCPU} > 0
+all:	jC
+
+# If -j arg is floating point or ends in C;
+# .MAKE.JOBS is a multiple of _SC_NPROCESSORS_ONLN
+# No news is good news here.
+jCvals ?= 1 1.2 2
+
+jC:
+	@for j in ${jCvals}; do \
+	e=`echo "${NCPU} * $$j" | bc | sed 's/\.[0-9]*//'`; \
+	g=`${MAKE} -r -f /dev/null -V .MAKE.JOBS -j$${j}C`; \
+	test $$g = $$e || echo "$$g != $$e"; \
+	done
+
+.endif
+.endif
+
 # expect: undefined
 # expect: 1
 # expect: 5
 # expect: 20
-# The value of .MAKE.JOBS is the exact text given in the command line, not the
-# canonical number.  This doesn't have practical consequences though.
-# expect: 00000000000000000000000000000001
+# expect: 1
