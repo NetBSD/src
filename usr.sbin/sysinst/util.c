0f98007e825ed36cd28af88dacfd1460caa22619
martin <martin@NetBSD.org>
2023-09-09 14:50:15 +0000

    Pull up following revision(s) (requested by riastradh in ticket #358):
    	usr.sbin/sysinst/util.c: revision 1.73
    
    sysinst(1): Run `certctl rehash' on fresh installs.
    
    It has come to my attention that sysinst does not, in fact, run
    postinstall(8) post-install -- only post-upgrade.
    
    Perhaps we should change this so that postinstall serves the purpose
    it says on the box -- make postinstall mandatory for new
    installations.

--- usr.sbin/sysinst/util.c
+++ usr.sbin/sysinst/util.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: util.c,v 1.71 2022/12/15 20:34:46 martin Exp $	*/
+/*	$NetBSD: util.c,v 1.71.2.1 2023/09/09 14:50:15 martin Exp $	*/
 
 /*
  * Copyright 1997 Piermont Information Systems Inc.
@@ -1524,8 +1524,13 @@ get_and_unpack_sets(int update, msg setupdone_msg, msg success_msg, msg failure_
 	}
 
 	/* Configure the system */
-	if (set_status[SET_BASE] & SET_INSTALLED)
+	if (set_status[SET_BASE] & SET_INSTALLED) {
 		run_makedev();
+		if (!update) {
+			run_program(RUN_CHROOT|RUN_DISPLAY,
+			    "/usr/sbin/certctl rehash");
+		}
+	}
 
 	if (!update) {
 		struct stat sb1, sb2;
