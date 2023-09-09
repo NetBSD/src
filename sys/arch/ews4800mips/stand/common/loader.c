4d29d221e2ecd8020e5dc38a6f18275f7e5f3386
andvar <andvar@NetBSD.org>
2023-09-09 21:04:26 +0000

    s/reagion/region/ in comment.

--- sys/arch/ews4800mips/stand/common/loader.c
+++ sys/arch/ews4800mips/stand/common/loader.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: loader.c,v 1.5 2021/10/06 20:36:58 andvar Exp $	*/
+/*	$NetBSD: loader.c,v 1.6 2023/09/09 21:04:26 andvar Exp $	*/
 
 /*-
  * Copyright (c) 2004 The NetBSD Foundation, Inc.
@@ -98,7 +98,7 @@ cmd_load_binary(int argc, char *argp[], int interactive)
 	q = (uint8_t *)strtoul(argp[1], 0, 0);
 	p = kernel_binary;
 
-	/* check load reagion */
+	/* check load region */
 	printf("load end=%p loader start=%p\n",
 	    q + kernel_binary_size, start);
 	if ((uint32_t)(q + kernel_binary_size) >= (uint32_t)start) {
