41bc0f7db13c7f785b3febbc7a22bb66f45abcc6
rillig <rillig@NetBSD.org>
2023-09-12 06:51:02 +0000

    tests/lint: show that '?:' skips untaken branches early
    This is probably done to avoid wrong warnings from branches that are
    only taken on other platforms, but it also generates a wrong 'unused'
    warning.

--- tests/usr.bin/xlint/lint1/msg_192.c
+++ tests/usr.bin/xlint/lint1/msg_192.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: msg_192.c,v 1.6 2023/07/09 11:18:55 rillig Exp $	*/
+/*	$NetBSD: msg_192.c,v 1.7 2023/09/12 06:51:02 rillig Exp $	*/
 # 3 "msg_192.c"
 
 // Test for message: '%s' unused in function '%s' [192]
@@ -11,4 +11,33 @@ example(int param)
 {
 	/* expect+1: warning: 'local' unused in function 'example' [192] */
 	int local;
+}
+
+
+void assertion_failed(const char *);
+
+void
+assert_true(void)
+{
+	sizeof(char) == 1
+	    ? (void)0
+/* FIXME: '__func__' is used, the code is just not reachable. */
+/* expect+1: warning: '__func__' unused in function 'assert_true' [192] */
+	    : assertion_failed(__func__);
+}
+
+void
+assert_false(void)
+{
+	sizeof(char) == 0
+	    ? (void)0
+	    : assertion_failed(__func__);
+}
+
+void
+assert_unknown(_Bool cond)
+{
+	cond
+	    ? (void)0
+	    : assertion_failed(__func__);
 }
