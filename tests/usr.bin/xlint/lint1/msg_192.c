6bb0d3704194ef86b28c0c86683cb558962da43a
rillig <rillig@NetBSD.org>
2023-09-12 22:01:05 +0000

    lint: mark unreachable function call arguments as used as well
    Previously, in a '?:' expression with a constant condition, the branch
    that is not taken was skipped but any identifiers in there were intended
    to be marked as used.  In function call expressions, this only worked
    for the last argument, as the PUSH operator is not a binary operator
    (see ops.def).  Cover this case as well.

--- tests/usr.bin/xlint/lint1/msg_192.c
+++ tests/usr.bin/xlint/lint1/msg_192.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: msg_192.c,v 1.8 2023/09/12 07:23:27 rillig Exp $	*/
+/*	$NetBSD: msg_192.c,v 1.9 2023/09/12 22:01:05 rillig Exp $	*/
 # 3 "msg_192.c"
 
 // Test for message: '%s' unused in function '%s' [192]
@@ -14,7 +14,7 @@ example(int param)
 }
 
 
-void assertion_failed(const char *);
+void assertion_failed(const char *, int, const char *, const char *);
 
 /*
  * The symbol '__func__' only occurs in an unreachable branch.  It is
@@ -25,7 +25,7 @@ assert_true(void)
 {
 	sizeof(char) == 1
 	    ? (void)0
-	    : assertion_failed(__func__);
+	    : assertion_failed("file", 26, __func__, "sizeof(char) == 1");
 }
 
 void
@@ -33,7 +33,7 @@ assert_false(void)
 {
 	sizeof(char) == 0
 	    ? (void)0
-	    : assertion_failed(__func__);
+	    : assertion_failed("file", 34, __func__, "sizeof(char) == 0");
 }
 
 void
@@ -41,5 +41,5 @@ assert_unknown(_Bool cond)
 {
 	cond
 	    ? (void)0
-	    : assertion_failed(__func__);
+	    : assertion_failed("file", 42, __func__, "cond");
 }
