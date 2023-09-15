5fb7a5f9aa0297006c934f47e702898ba6546e25
andvar <andvar@NetBSD.org>
2023-09-15 20:59:56 +0000

    sh3/scif(4): replace undefined scifstatus with DPRINTF.
    fixes the build with SCIF_DEBUG enabled.

--- sys/arch/sh3/dev/scif.c
+++ sys/arch/sh3/dev/scif.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: scif.c,v 1.68 2020/11/21 17:25:52 thorpej Exp $ */
+/*	$NetBSD: scif.c,v 1.69 2023/09/15 20:59:56 andvar Exp $ */
 
 /*-
  * Copyright (C) 1999 T.Horiuchi and SAITOH Masanobu.  All rights reserved.
@@ -93,7 +93,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: scif.c,v 1.68 2020/11/21 17:25:52 thorpej Exp $");
+__KERNEL_RCSID(0, "$NetBSD: scif.c,v 1.69 2023/09/15 20:59:56 andvar Exp $");
 
 #include "opt_kgdb.h"
 #include "opt_scif.h"
@@ -176,6 +176,11 @@ struct scif_softc {
 	volatile u_char sc_heldchange;
 };
 
+#ifdef SCIF_DEBUG
+#define DPRINTF_ENABLE
+#define DPRINTF_DEBUG	scif_debug
+#endif
+#include <machine/debug.h>
 
 static int scif_match(device_t, cfdata_t, void *);
 static void scif_attach(device_t, device_t, void *);
@@ -683,10 +688,7 @@ scifparam(struct tty *tp, struct termios *t)
 
 	splx(s);
 
-#ifdef SCIF_DEBUG
-	if (scif_debug)
-		scifstatus(sc, "scifparam ");
-#endif
+	DPRINTF("%s: scifparam\n", device_xname(sc->sc_dev));
 
 	if (!ISSET(t->c_cflag, CHWFLOW)) {
 		if (sc->sc_tx_stopped) {
@@ -797,10 +799,7 @@ scifopen(dev_t dev, int flag, int mode, struct lwp *l)
 		scif_hwiflow(sc);
 #endif
 
-#ifdef SCIF_DEBUG
-		if (scif_debug)
-			scifstatus(sc, "scifopen  ");
-#endif
+		DPRINTF("%s: scifopen\n", device_xname(sc->sc_dev));
 
 		splx(s2);
 	}
@@ -1148,10 +1147,7 @@ scif_stsoft(struct scif_softc *sc, struct tty *tp)
 		}
 	}
 
-#ifdef SCIF_DEBUG
-	if (scif_debug)
-		scifstatus(sc, "scif_stsoft");
-#endif
+	DPRINTF("%s: scif_stsoft\n", device_xname(sc->sc_dev));
 }
 #endif /* 0 */
 
@@ -1347,10 +1343,7 @@ scifintr(void *arg)
 		if (ISSET(~msr, sc->sc_msr_mask)) {
 			sc->sc_tbc = 0;
 			sc->sc_heldtbc = 0;
-#ifdef SCIF_DEBUG
-			if (scif_debug)
-				scifstatus(sc, "scifintr  ");
-#endif
+			DPRINTF("%s: scifintr\n", device_xname(sc->sc_dev));
 		}
 
 		sc->sc_st_check = 1;
