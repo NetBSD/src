8627aec9f444b1c2c016eb0f048cb681e3ed8f42
mrg <mrg@NetBSD.org>
2023-10-08 21:52:34 +0000

    bump the netbsd gcc 10 version to "nb3 20231008" for all the vax updates.
    XXX: check if mknative needed?
    XXX: pullup-10

--- tools/gcc/gcc-version.mk
+++ tools/gcc/gcc-version.mk
@@ -1,9 +1,9 @@
-#	$NetBSD: gcc-version.mk,v 1.24 2023/07/30 06:36:21 mrg Exp $
+#	$NetBSD: gcc-version.mk,v 1.25 2023/10/08 21:52:34 mrg Exp $
 
 # common location for tools and native build
 
 .if ${HAVE_GCC} == 10
-NETBSD_GCC_VERSION=nb2 20230710
+NETBSD_GCC_VERSION=nb3 20231008
 .endif
 .if ${HAVE_GCC} == 12
 NETBSD_GCC_VERSION=nb1 20230729
