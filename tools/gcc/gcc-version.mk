a0f6c1396a0284c8f5ba34e23d7d2a670b380212
martin <martin@NetBSD.org>
2023-10-09 07:38:16 +0000

    Pull up following revision(s) (requested by mrg in ticket #398):
    	tools/gcc/gcc-version.mk: revision 1.25
    
    bump the netbsd gcc 10 version to "nb3 20231008" for all the vax updates.

--- tools/gcc/gcc-version.mk
+++ tools/gcc/gcc-version.mk
@@ -1,7 +1,7 @@
-#	$NetBSD: gcc-version.mk,v 1.22.2.1 2023/07/14 08:21:45 martin Exp $
+#	$NetBSD: gcc-version.mk,v 1.22.2.2 2023/10/09 07:38:16 martin Exp $
 
 # common location for tools and native build
 
 .if ${HAVE_GCC} == 10
-NETBSD_GCC_VERSION=nb2 20230710
+NETBSD_GCC_VERSION=nb3 20231008
 .endif
