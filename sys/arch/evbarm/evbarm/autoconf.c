b7e0f113b55d45a934cd8ffe387483b3ba07fd75
martin <martin@NetBSD.org>
2023-10-20 16:13:03 +0000

    Pull up following revision(s) (requested by mrg in ticket #431):
    	sys/arch/evbarm/evbarm/autoconf.c: revision 1.24
    	sys/arch/evbarm/include/types.h: revision 1.16
    	sys/arch/evbarm/fdt/fdt_machdep.c: revision 1.100
    	sys/arch/evbarm/fdt/fdt_machdep.c: revision 1.101
    
    fix root detection on evbarm when raid is involved
    
    there are several problems solved in this change:
    - - lots of work was re-done when we already have determined the
      device booted from, so several new early returns introduced
      if booted_device has been set
    - - due to the lack of cpu_bootconf(), raidframe softroot would
      override "root=xxx" on the boot command line (note that
      platforms that use eg, device_register() to detect the boot
      device are not affected by this issue as they find the
      boot device much earlier.)
    - - in the new cpu_bootconf(), switch the order of the platform
      boot-config with the set_root_device() call.  this avoids a
      problem where "root=xxx" is checked after automated methods,
      and is thus ignored.
    - - in fdt_detect_root_device(), remove the code to add "root=xxx""
      string to the boot_args[] that would be later parsed by the
      set_root_device() call, and simply set booted_device and, for
      mbr installs, booted_partition directly.  also, for any
      successful call, perform an early return.
    - - define __HAVE_CPU_BOOTCONF so early boot calls cpu_bootconf().
    
    tested on:
    - - rockpro64 booting from emmc, sata (big, and little endian)
    - - rockpro64 loading kernel from msdos partition
    - - rockpro64 booting from network (fails to auto-detect, with or
      without this change)
    - - quartz64 booting from nvme
    - - lx2k booting from nvme
    
    Locate wedges as boot device and also match a partition GUID.

--- sys/arch/evbarm/evbarm/autoconf.c
+++ sys/arch/evbarm/evbarm/autoconf.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: autoconf.c,v 1.23 2020/12/19 21:54:00 mrg Exp $	*/
+/*	$NetBSD: autoconf.c,v 1.23.18.1 2023/10/20 16:13:03 martin Exp $	*/
 
 /*-
  * Copyright (c) 2001 The NetBSD Foundation, Inc.
@@ -30,7 +30,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: autoconf.c,v 1.23 2020/12/19 21:54:00 mrg Exp $");
+__KERNEL_RCSID(0, "$NetBSD: autoconf.c,v 1.23.18.1 2023/10/20 16:13:03 martin Exp $");
 
 #include "opt_md.h"
 #include "opt_ddb.h"
@@ -108,6 +108,9 @@ set_root_device(void)
 	char *ptr, *end, *buf;
 	size_t len;
 
+	if (booted_device)
+		return;
+
 	if (boot_args == NULL)
 		return;
 
@@ -118,7 +121,7 @@ set_root_device(void)
 		return;
 
 	/* NUL-terminate string, get_bootconf_option doesn't */
-	for (end=ptr; *end != '\0'; ++end) {
+	for (end = ptr; *end != '\0'; ++end) {
 		if (*end == ' ' || *end == '\t') {
 			break;
 		}
@@ -144,16 +147,25 @@ set_root_device(void)
 #endif
 
 /*
- * Set up the root device from the boot args
+ * Set up the root device from the boot args.
+ *
+ * cpu_bootconf() is called before RAIDframe root detection,
+ * and cpu_rootconf() is called after.
  */
+void
+cpu_bootconf(void)
+{
+#ifndef MEMORY_DISK_IS_ROOT
+	set_root_device();
+	if (evbarm_cpu_rootconf)
+		(*evbarm_cpu_rootconf)();
+#endif
+}
+
 void
 cpu_rootconf(void)
 {
-#ifndef MEMORY_DISK_IS_ROOT
-	if (evbarm_cpu_rootconf)
-		(*evbarm_cpu_rootconf)();
-	set_root_device();
-#endif
+	cpu_bootconf();
 	aprint_normal("boot device: %s\n",
 	    booted_device != NULL ? device_xname(booted_device) : "<unknown>");
 	rootconf();
