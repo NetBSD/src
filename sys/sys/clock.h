74ed61d97c9c5744a7099490811d683a3dad937b
jschauma <jschauma@NetBSD.org>
2023-10-26 00:59:16 +0000

    include stdint.h if not in kernel/standalone mode
    This fixes problems with including sys/clock.h by itself and avoids
    having to order headers counter to share/style.
    
    ifdefs suggested by tnn@ on netbsd-users@.

--- sys/sys/clock.h
+++ sys/sys/clock.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: clock.h,v 1.4 2018/04/19 21:19:07 christos Exp $	*/
+/*	$NetBSD: clock.h,v 1.5 2023/10/26 00:59:16 jschauma Exp $	*/
 
 /*-
  * Copyright (c) 1996 The NetBSD Foundation, Inc.
@@ -31,6 +31,10 @@
 
 #ifndef _SYS_CLOCK_H_
 #define _SYS_CLOCK_H_
+
+#if !defined(_KERNEL) && !defined(_STANDALONE)
+#include <stdint.h>
+#endif
 
 /* Some handy constants. */
 #define SECS_PER_MINUTE		60
