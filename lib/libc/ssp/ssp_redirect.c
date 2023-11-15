665d6d6a4c105a686b997b5a902cea4fcacef7e1
christos <christos@NetBSD.org>
2023-11-15 03:15:28 +0000

    provide materialized functions for the ssp overriden inlines

---
+++ lib/libc/ssp/ssp_redirect.c
@@ -0,0 +1,56 @@
+/*	$NetBSD: ssp_redirect.c,v 1.1 2023/11/15 03:15:28 christos Exp $	*/
+
+/*-
+ * Copyright (c) 2023 The NetBSD Foundation, Inc.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to The NetBSD Foundation
+ * by Christos Zoulas.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#undef _FORTIFY_SOURCE
+#define _FORTIFY_SOURCE 2
+#define __ssp_inline
+
+#include <sys/cdefs.h>
+__RCSID("$NetBSD: ssp_redirect.c,v 1.1 2023/11/15 03:15:28 christos Exp $");
+
+#include <unistd.h>
+
+int ssp_use(void);
+
+/*
+ * Provide definitions of the redirect functions in libc.
+ */
+int
+ssp_use(void)
+{
+	if (getcwd(NULL, 0) == NULL)
+		return -1;
+	if (read(-1, NULL, 0) == -1)
+		return -1;
+	if (readlink(NULL, NULL, 0) == -1)
+		return -1;
+	return 0;
+}
