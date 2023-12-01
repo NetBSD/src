4dca0d2c9973bbd34d56cd1882640ef6156fb725
yamaguchi <yamaguchi@NetBSD.org>
2023-12-01 09:27:17 +0000

    lagg(4): eliminate unnecessary reset by the change of if_flags

--- sys/net/lagg/if_lagg.c
+++ sys/net/lagg/if_lagg.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: if_lagg.c,v 1.56 2023/12/01 06:18:02 yamaguchi Exp $	*/
+/*	$NetBSD: if_lagg.c,v 1.57 2023/12/01 09:27:17 yamaguchi Exp $	*/
 
 /*
  * Copyright (c) 2005, 2006 Reyk Floeter <reyk@openbsd.org>
@@ -20,7 +20,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: if_lagg.c,v 1.56 2023/12/01 06:18:02 yamaguchi Exp $");
+__KERNEL_RCSID(0, "$NetBSD: if_lagg.c,v 1.57 2023/12/01 09:27:17 yamaguchi Exp $");
 
 #ifdef _KERNEL_OPT
 #include "opt_inet.h"
@@ -722,7 +722,6 @@ lagg_ioctl(struct ifnet *ifp, u_long cmd, void *data)
 			if_stop(ifp, 1);
 			break;
 		case IFF_UP:
-		case IFF_UP | IFF_RUNNING:
 			error = if_init(ifp);
 			break;
 		}
