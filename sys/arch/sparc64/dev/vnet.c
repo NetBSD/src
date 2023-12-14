6d47421e3f13c67c44dae0615b71dd5cd6994839
andvar <andvar@NetBSD.org>
2023-12-14 20:17:18 +0000

    Use console_debugger() vs Debugger() which allows to avoid #ifdef DDB blocks.
    From uwe@

--- sys/arch/sparc64/dev/vnet.c
+++ sys/arch/sparc64/dev/vnet.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: vnet.c,v 1.9 2023/12/13 23:11:35 andvar Exp $	*/
+/*	$NetBSD: vnet.c,v 1.10 2023/12/14 20:17:18 andvar Exp $	*/
 /*	$OpenBSD: vnet.c,v 1.62 2020/07/10 13:26:36 patrick Exp $	*/
 /*
  * Copyright (c) 2009, 2015 Mark Kettenis
@@ -441,9 +441,7 @@ vnet_rx_intr(void *arg)
 	default:
 		DPRINTF(("%s: unhandled type %0x02/%0x02/%0x02\n",
 				 __func__, lp->type, lp->stype, lp->ctrl));
-#ifdef DDB
-		Debugger();
-#endif
+		console_debugger();
 		ldc_reset(lc);
 		break;
 	}
