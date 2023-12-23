c450fdf997a113a4d3161ccb645582be3579986d
martin <martin@NetBSD.org>
2023-12-23 13:06:43 +0000

    Pull up following revision(s) (requested by thorpej in ticket #515):
    	sys/arch/vax/vax/pmap.c: revision 1.200
    
    rmspace(): when a PT page is put back onto the free list, invalidate
    it's VA in the TB.
    
    rmptep(): Add a comment that the no TIBS is needed for newly-freed PT
    pages here because all of the callers perform a TBIA.

--- sys/arch/vax/vax/pmap.c
+++ sys/arch/vax/vax/pmap.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: pmap.c,v 1.195.2.1 2023/06/21 19:09:07 martin Exp $	   */
+/*	$NetBSD: pmap.c,v 1.195.2.2 2023/12/23 13:06:43 martin Exp $	   */
 /*
  * Copyright (c) 1994, 1998, 1999, 2003 Ludd, University of Lule}, Sweden.
  * All rights reserved.
@@ -25,7 +25,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: pmap.c,v 1.195.2.1 2023/06/21 19:09:07 martin Exp $");
+__KERNEL_RCSID(0, "$NetBSD: pmap.c,v 1.195.2.2 2023/12/23 13:06:43 martin Exp $");
 
 #include "opt_ddb.h"
 #include "opt_cputype.h"
@@ -631,6 +631,7 @@ rmspace(struct pmap *pm)
 		}
 		free_ptp((((struct pte *)ptpp)->pg_pfn << VAX_PGSHIFT));
 		*ptpp = 0;
+		mtpr((vaddr_t)br, PR_TBIS);
 	}
 	lr = pm->pm_p1lr/NPTEPG;
 	for (i = lr; i < NPTEPERREG/NPTEPG; i++) {
@@ -645,6 +646,7 @@ rmspace(struct pmap *pm)
 		}
 		free_ptp((((struct pte *)ptpp)->pg_pfn << VAX_PGSHIFT));
 		*ptpp = 0;
+		mtpr((vaddr_t)br, PR_TBIS);
 	}
 
 	if (pm->pm_p0lr != 0)
@@ -770,6 +772,7 @@ rmptep(struct pte *pte)
 #endif
 	free_ptp((((struct pte *)ptpp)->pg_pfn << VAX_PGSHIFT));
 	*ptpp = 0;
+	/* N.B. callers all do a TBIA, so TBIS not needed here. */
 }
 
 static int 
