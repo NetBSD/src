a6771b5ec5d0027e2a227a4f62d7374af6a98eb6
thorpej <thorpej@NetBSD.org>
2023-12-27 19:26:29 +0000

    Define values for the 68030 TT registers in terms of the definitions in <m68k/mmu_30.h> rather than using magic numbers.

--- sys/arch/mac68k/include/pmap.h
+++ sys/arch/mac68k/include/pmap.h
@@ -1,3 +1,21 @@
-/*	$NetBSD: pmap.h,v 1.37 2002/11/03 19:56:30 chs Exp $	*/
+/*	$NetBSD: pmap.h,v 1.38 2023/12/27 19:26:29 thorpej Exp $	*/
+
+#ifndef _MAC68K_PMAP_H_
+#define	_MAC68K_PMAP_H_
 
 #include <m68k/pmap_motorola.h>
+#include <m68k/mmu_30.h>
+
+/*
+ * Tranparent translation register used in locore.s:get_pte().
+ * User Data set up for R/W access of the entire address space.
+ *
+ * (XXX TT30_RW isn't actually needed because of TT30_RWM, but
+ * this the value historically used.)
+ */
+#define	MAC68K_TT_GET_PTE	(0x00000000 |				\
+				 __SHIFTIN(0xff,TT30_LAM) |		\
+				 TT30_E | TT30_CI | TT30_RW | TT30_RWM |\
+				 TT30_USERD)
+
+#endif /* _MAC68K_PMAP_H_ */
--- sys/arch/news68k/include/pmap.h
+++ sys/arch/news68k/include/pmap.h
@@ -1,3 +1,30 @@
-/*	$NetBSD: pmap.h,v 1.14 2002/11/03 19:56:32 chs Exp $	*/
+/*	$NetBSD: pmap.h,v 1.15 2023/12/27 19:26:29 thorpej Exp $	*/
+
+#ifndef _NEWS68K_PMAP_H_
+#define	_NEWS68K_PMAP_H_
 
 #include <m68k/pmap_motorola.h>
+#include <m68k/mmu_30.h>
+
+/*
+ * Transparent translation register values for:
+ *
+ * I/O space: 0xe0000000-0xffffffff
+ * RAM to use PROM calls: 0xc0000000-0xdfffffff
+ *
+ * Both use Function Codes 4-7 (to get SUPERD and SUPERP).
+ * XXX I/O space can probably just use SUPERD.
+ */
+#define	NEWS68K_TT_IO		(0xe0000000 |				\
+				 __SHIFTIN(0x1f,TT30_LAM) |		\
+				 TT30_E | TT30_CI | TT30_RWM |		\
+				 __SHIFTIN(4,TT30_FCBASE) |		\
+				 __SHIFTIN(3,TT30_FCMASK))
+
+#define	NEWS68K_TT_PROM		(0xc0000000 |				\
+				 __SHIFTIN(0x1f,TT30_LAM) |		\
+				 TT30_E | TT30_RWM |			\
+				 __SHIFTIN(4,TT30_FCBASE) |		\
+				 __SHIFTIN(3,TT30_FCMASK))
+
+#endif /* _NEWS68K_PMAP_H_ */
