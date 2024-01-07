cc3c7d5b6a6d896c4703f47bb421c40c9600a9b0
rillig <rillig@NetBSD.org>
2024-01-07 21:19:42 +0000

    tests/lint: test operators, integer overflow, conversions

--- tests/usr.bin/xlint/lint1/msg_107.c
+++ tests/usr.bin/xlint/lint1/msg_107.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: msg_107.c,v 1.6 2023/07/07 19:45:22 rillig Exp $	*/
+/*	$NetBSD: msg_107.c,v 1.7 2024/01/07 21:19:42 rillig Exp $	*/
 # 3 "msg_107.c"
 
 // Test for message: operands of '%s' have incompatible types '%s' and '%s' [107]
@@ -11,4 +11,13 @@ compare(double d, void *ptr)
 {
 	/* expect+1: error: operands of '==' have incompatible types 'double' and 'pointer to void' [107] */
 	return d == ptr;
+}
+
+static inline void
+typeok_minus(const char *p, int i, double d)
+{
+	if (p[i] != p[(int)d])
+		return;
+	/* expect+1: error: operands of '-' have incompatible types 'pointer to const char' and 'double' [107] */
+	return (p - i) - (p - d);
 }
