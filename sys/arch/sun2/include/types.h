7f61c3050a17f24fd6d76cacad6f1042eb58a443
thorpej <thorpej@NetBSD.org>
2024-01-14 18:22:02 +0000

    Merge common sun68k types defns into <sun68k/types.h>.

--- sys/arch/sun2/include/types.h
+++ sys/arch/sun2/include/types.h
@@ -1,11 +1,5 @@
-/*	$NetBSD: types.h,v 1.14 2021/04/01 04:43:01 simonb Exp $	*/
+/*	$NetBSD: types.h,v 1.15 2024/01/14 18:22:02 thorpej Exp $	*/
 
-#include <m68k/types.h>
+#include <sun68k/types.h>
 
-#define	__HAVE_LEGACY_INTRCNT
-#define	__HAVE_MM_MD_DIRECT_MAPPED_PHYS
-#define	__HAVE_MM_MD_KERNACC
-#define	__HAVE_MM_MD_MMAP
-#define	__HAVE_MM_MD_READWRITE
 #define	__HAVE_NO___THREAD
-#define	__HAVE_NO_PMAP_STATS
---
+++ sys/arch/sun68k/include/types.h
@@ -0,0 +1,10 @@
+/*	$NetBSD: types.h,v 1.1 2024/01/14 18:22:02 thorpej Exp $	*/
+
+#include <m68k/types.h>
+
+#define	__HAVE_LEGACY_INTRCNT
+#define	__HAVE_MM_MD_DIRECT_MAPPED_PHYS
+#define	__HAVE_MM_MD_KERNACC
+#define	__HAVE_MM_MD_MMAP
+#define	__HAVE_MM_MD_READWRITE
+#define	__HAVE_NO_PMAP_STATS
