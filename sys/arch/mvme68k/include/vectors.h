80acf3e76d3c224f5d7842bdbca0853ceb3cd0fc
thorpej <thorpej@NetBSD.org>
2024-01-16 01:26:34 +0000

    Switch mvme68k over to the common interrupt dispatch code and G/C __HAVE_LEGACY_INTRCNT.

--- sys/arch/mvme68k/include/vectors.h
+++ sys/arch/mvme68k/include/vectors.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: vectors.h,v 1.1 2024/01/13 20:18:46 thorpej Exp $	*/
+/*	$NetBSD: vectors.h,v 1.2 2024/01/16 01:26:34 thorpej Exp $	*/
 
 /*-
  * Copyright (c) 2024 The NetBSD Foundation, Inc.
@@ -36,14 +36,14 @@
 
 #include <m68k/vectors.h>
 
-#define	MACHINE_AV0_HANDLER	intrhand_autovec
-#define	MACHINE_AV1_HANDLER	intrhand_autovec
-#define	MACHINE_AV2_HANDLER	intrhand_autovec
-#define	MACHINE_AV3_HANDLER	intrhand_autovec
-#define	MACHINE_AV4_HANDLER	intrhand_autovec
-#define	MACHINE_AV5_HANDLER	intrhand_autovec
-#define	MACHINE_AV6_HANDLER	intrhand_autovec
-#define	MACHINE_AV7_HANDLER	intrhand_autovec
+#define	MACHINE_AV0_HANDLER	intrstub_autovec
+#define	MACHINE_AV1_HANDLER	intrstub_autovec
+#define	MACHINE_AV2_HANDLER	intrstub_autovec
+#define	MACHINE_AV3_HANDLER	intrstub_autovec
+#define	MACHINE_AV4_HANDLER	intrstub_autovec
+#define	MACHINE_AV5_HANDLER	intrstub_autovec
+#define	MACHINE_AV6_HANDLER	intrstub_autovec
+#define	MACHINE_AV7_HANDLER	intrstub_autovec
 
 #endif /* _KERNEL */
 
