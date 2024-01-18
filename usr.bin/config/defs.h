afc4571bb4168e154df9f0e85c60d6f29ad65dd0
thorpej <thorpej@NetBSD.org>
2024-01-18 05:41:38 +0000

    Bump version.

--- usr.bin/config/defs.h
+++ usr.bin/config/defs.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: defs.h,v 1.107 2024/01/18 04:41:37 thorpej Exp $	*/
+/*	$NetBSD: defs.h,v 1.108 2024/01/18 05:41:38 thorpej Exp $	*/
 
 /*
  * Copyright (c) 1992, 1993
@@ -107,7 +107,7 @@ extern const char *progname;
  * The next two lines define the current version of the config(1) binary,
  * and the minimum version of the configuration files it supports.
  */
-#define CONFIG_VERSION		20180827
+#define CONFIG_VERSION		20240118
 #define CONFIG_MINVERSION	0
 
 struct where {
