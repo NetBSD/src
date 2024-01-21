c321ba6a5e21f6088de6713a5028142c6f137e71
christos <christos@NetBSD.org>
2024-01-21 00:35:57 +0000

    fix alpha, grr.

--- lib/libc/sys/Lint_ptrace.c
+++ lib/libc/sys/Lint_ptrace.c
@@ -1,10 +1,11 @@
-/* $NetBSD: Lint_ptrace.c,v 1.3 2024/01/20 14:52:49 christos Exp $ */
+/* $NetBSD: Lint_ptrace.c,v 1.4 2024/01/21 00:35:57 christos Exp $ */
 
 /*
  * This file placed in the public domain.
  * Chris Demetriou, November 5, 1997.
  */
 
+#include <signal.h>	/* XXX: alpha <machine/signal.h> needs sigset_t! */
 #include <sys/types.h>
 #include <sys/ptrace.h>
 
