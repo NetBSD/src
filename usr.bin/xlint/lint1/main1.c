d3156b2b6773b691bf7e50ec8ca17d2644e9012a
rillig <rillig@NetBSD.org>
2024-02-02 23:36:01 +0000

    lint: use the locale for interpreting wide character strings

--- usr.bin/xlint/lint1/main1.c
+++ usr.bin/xlint/lint1/main1.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: main1.c,v 1.79 2024/01/21 14:11:52 rillig Exp $	*/
+/*	$NetBSD: main1.c,v 1.80 2024/02/02 23:36:01 rillig Exp $	*/
 
 /*
  * Copyright (c) 1994, 1995 Jochen Pohl
@@ -37,10 +37,11 @@
 
 #include <sys/cdefs.h>
 #if defined(__RCSID)
-__RCSID("$NetBSD: main1.c,v 1.79 2024/01/21 14:11:52 rillig Exp $");
+__RCSID("$NetBSD: main1.c,v 1.80 2024/02/02 23:36:01 rillig Exp $");
 #endif
 
 #include <sys/types.h>
+#include <locale.h>
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -134,6 +135,7 @@ main(int argc, char *argv[])
 {
 	int c;
 
+	setlocale(LC_ALL, "");
 	setprogname(argv[0]);
 
 	while ((c = getopt(argc, argv, "abceghpq:rstuvwyzA:FPR:STX:")) != -1) {
