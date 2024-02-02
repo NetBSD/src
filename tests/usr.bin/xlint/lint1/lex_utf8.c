03951da31ed81dc69b0424e7ef8e04da88c40d15
rillig <rillig@NetBSD.org>
2024-02-02 23:36:01 +0000

    lint: use the locale for interpreting wide character strings

--- tests/usr.bin/xlint/lint1/lex_utf8.c
+++ tests/usr.bin/xlint/lint1/lex_utf8.c
@@ -1,15 +1,12 @@
-/*	$NetBSD: lex_utf8.c,v 1.1 2024/02/02 23:30:39 rillig Exp $	*/
+/*	$NetBSD: lex_utf8.c,v 1.2 2024/02/02 23:36:01 rillig Exp $	*/
 # 3 "lex_utf8.c"
 
 /*
  * Test lexing of multibyte characters and strings in an UTF-8 locale.
+ *
+ * See also:
+ *	lex_wide_string.c	(runs in the C locale)
  */
 
-/*
- * Since lint always runs in the default "C" locale, it does not support any
- * multibyte character encoding, thus treating each byte as a separate
- * character. If lint were to support UTF-8, the array dimension would be 3
- * instead of 7.
- */
-/* expect+1: error: negative array dimension (-7) [20] */
+/* expect+1: error: negative array dimension (-3) [20] */
 typedef int mblen[-(int)(sizeof(L"Ä😄") / sizeof(L""))];
