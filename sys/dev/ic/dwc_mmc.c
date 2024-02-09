48a60f0591130fecf9edc30cab53660c6de80a32
skrll <skrll@NetBSD.org>
2024-02-09 17:16:42 +0000

    Restrict dwcmmc to 32bit DMA (<4GB) regardless of attachment

--- sys/dev/ic/dwc_mmc.c
+++ sys/dev/ic/dwc_mmc.c
@@ -1,4 +1,4 @@
-/* $NetBSD: dwc_mmc.c,v 1.30 2024/02/09 16:57:11 skrll Exp $ */
+/* $NetBSD: dwc_mmc.c,v 1.31 2024/02/09 17:16:42 skrll Exp $ */
 
 /*-
  * Copyright (c) 2014-2017 Jared McNeill <jmcneill@invisible.ca>
@@ -27,7 +27,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: dwc_mmc.c,v 1.30 2024/02/09 16:57:11 skrll Exp $");
+__KERNEL_RCSID(0, "$NetBSD: dwc_mmc.c,v 1.31 2024/02/09 17:16:42 skrll Exp $");
 
 #include <sys/param.h>
 #include <sys/bus.h>
@@ -836,6 +836,16 @@ dwc_mmc_init(struct dwc_mmc_softc *sc)
 
 	val = MMC_READ(sc, DWC_MMC_VERID);
 	sc->sc_verid = __SHIFTOUT(val, DWC_MMC_VERID_ID);
+
+	if (sizeof(bus_addr_t) > 4) {
+		int error = bus_dmatag_subregion(sc->sc_dmat, 0, __MASK(32),
+		    &sc->sc_dmat, BUS_DMA_WAITOK);
+		if (error != 0) {
+			aprint_error_dev(sc->sc_dev,
+			    "failed to create DMA subregion\n");
+			return ENOMEM;
+		}
+	}
 
 	if (sc->sc_fifo_reg == 0) {
 		if (sc->sc_verid < DWC_MMC_VERID_240A)
