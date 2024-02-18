4afad4b7fa6d4a0d3dedf41d1587a7250710ae54
christos <christos@NetBSD.org>
2024-02-18 20:57:30 +0000

    Disentangle dhcp from bind by putting enough of bind 9.16.42 for it to build here. Bind keeps removing the bits that dhcp uses to build and keeping bind up-to-date and dhcp building is a losing battle.

---
+++ external/mpl/dhcp/bind/dist/lib/dns/rdata/generic/rt_21.h
@@ -0,0 +1,29 @@
+/*	$NetBSD: rt_21.h,v 1.1 2024/02/18 20:57:44 christos Exp $	*/
+
+/*
+ * Copyright (C) Internet Systems Consortium, Inc. ("ISC")
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, you can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * See the COPYRIGHT file distributed with this work for additional
+ * information regarding copyright ownership.
+ */
+
+#ifndef GENERIC_RT_21_H
+#define GENERIC_RT_21_H 1
+
+/*!
+ *  \brief Per RFC1183 */
+
+typedef struct dns_rdata_rt {
+	dns_rdatacommon_t common;
+	isc_mem_t *mctx;
+	uint16_t preference;
+	dns_name_t host;
+} dns_rdata_rt_t;
+
+#endif /* GENERIC_RT_21_H */
