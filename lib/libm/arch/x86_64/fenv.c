232bde9a3122407659e11a405e6d77a1b0dd88e2
martin <martin@NetBSD.org>
2024-02-23 18:15:53 +0000

    Pull up following revision(s) (requested by riastradh in ticket #1937):
    	tests/lib/libm/t_fenv.c: revision 1.14
    	tests/lib/libm/t_fenv.c: revision 1.15
    	lib/libm/arch/x86_64/fenv.c: revision 1.11
    
    fenv(3): Add test for PR port-amd64/57949.
    
    fenv(3): Fix fetestexcept to avoid side effects on trap state.
    
    PR port-amd64/57949

--- lib/libm/arch/x86_64/fenv.c
+++ lib/libm/arch/x86_64/fenv.c
@@ -1,4 +1,4 @@
-/* $NetBSD: fenv.c,v 1.7 2017/03/22 23:11:09 chs Exp $ */
+/* $NetBSD: fenv.c,v 1.7.4.1 2024/02/23 18:15:53 martin Exp $ */
 
 /*-
  * Copyright (c) 2004-2005 David Schultz <das (at) FreeBSD.ORG>
@@ -27,7 +27,7 @@
  */
 
 #include <sys/cdefs.h>
-__RCSID("$NetBSD: fenv.c,v 1.7 2017/03/22 23:11:09 chs Exp $");
+__RCSID("$NetBSD: fenv.c,v 1.7.4.1 2024/02/23 18:15:53 martin Exp $");
 
 #include "namespace.h"
 
@@ -259,7 +259,6 @@ fesetexceptflag(const fexcept_t *flagp, int excepts)
 int
 fetestexcept(int excepts)
 {
-	fenv_t fenv;
 	uint32_t mxcsr;
 	uint16_t status;
 	int ex;
@@ -268,17 +267,10 @@ fetestexcept(int excepts)
 
 	ex = excepts & FE_ALL_EXCEPT;
 
-	/* Store the current x87 floating-point environment */
-	memset(&fenv, 0, sizeof(fenv));
-
-	__fnstenv(&fenv);
 	__fnstsw(&status);
-
-	/* Store the MXCSR register state */
-	__stmxcsr(&fenv.mxcsr);
 	__stmxcsr(&mxcsr);
 
-	return ((fenv.x87.status | fenv.mxcsr) & ex);
+	return ((status | mxcsr) & ex);
 }
 
 /*
