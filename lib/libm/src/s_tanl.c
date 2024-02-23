4851547888f14a09c64545831c61ea8fb3b9b92b
christos <christos@NetBSD.org>
2024-02-23 13:42:01 +0000

    fix the 32 bit tanl symbol.

--- lib/libm/src/s_tanl.c
+++ lib/libm/src/s_tanl.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: s_tanl.c,v 1.1 2024/01/21 18:53:19 christos Exp $	*/
+/*	$NetBSD: s_tanl.c,v 1.2 2024/02/23 13:42:01 christos Exp $	*/
 
 /*-
  * SPDX-License-Identifier: BSD-2-Clause
@@ -29,7 +29,7 @@
  */
 
 #include <sys/cdefs.h>
-__RCSID("$NetBSD: s_tanl.c,v 1.1 2024/01/21 18:53:19 christos Exp $");
+__RCSID("$NetBSD: s_tanl.c,v 1.2 2024/02/23 13:42:01 christos Exp $");
 
 /*
  * Limited testing on pseudorandom numbers drawn within [0:4e8] shows
@@ -45,11 +45,11 @@ __RCSID("$NetBSD: s_tanl.c,v 1.1 2024/01/21 18:53:19 christos Exp $");
 #include "math.h"
 #include "math_private.h"
 
-#ifdef __HAVE_LONG_DOUBLE
-
 #ifdef __weak_alias
 __weak_alias(tanl, _tanl)
 #endif
+
+#ifdef __HAVE_LONG_DOUBLE
 
 #if LDBL_MANT_DIG == 64
 #include "../ld80/e_rem_pio2l.h"
@@ -109,8 +109,10 @@ tanl(long double x)
 
 #else
 
-#ifdef __weak_alias
-__weak_alias(tanl, tan)
-#endif
+long double
+tanl(long double x)
+{
+	return tan(x);
+}
 
 #endif
