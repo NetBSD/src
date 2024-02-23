dde872423e2e75456ab92a24f6048f78e64549ac
mrg <mrg@NetBSD.org>
2024-02-23 05:33:03 +0000

    build infrastructure for isl.

---
+++ external/mit/isl/include/gitversion.h
@@ -0,0 +1,1 @@
+#define GIT_HEAD_ID "isl-0.26"
---
+++ external/mit/isl/include/isl/stdint.h
@@ -0,0 +1,9 @@
+#ifndef _ISL_INCLUDE_ISL_STDINT_H
+#define _ISL_INCLUDE_ISL_STDINT_H 1
+#ifndef _GENERATED_STDINT_H
+#define _GENERATED_STDINT_H "isl 0.26"
+/* generated using gnu compiler gcc (nb2 20230710) 10.5.0 */
+#define _STDINT_HAVE_STDINT_H 1
+#include <stdint.h>
+#endif
+#endif
