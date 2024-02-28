5426de247dee0370a3b5b43b9723bd2d942b73f2
thorpej <thorpej@NetBSD.org>
2024-02-28 13:07:07 +0000

    Revert the CLOCK_FORMAT0 work-around.

--- sys/arch/evbcf/include/cpu.h
+++ sys/arch/evbcf/include/cpu.h
@@ -1,7 +1,3 @@
-/*	$NetBSD: cpu.h,v 1.10 2024/02/27 16:07:02 nat Exp $	*/
-
-#ifndef CLOCK_FORMAT0
-#define CLOCK_FORMAT0
-#endif
+/*	$NetBSD: cpu.h,v 1.11 2024/02/28 13:07:07 thorpej Exp $	*/
 
 #include <m68k/cpu.h>
