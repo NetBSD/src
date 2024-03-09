d6cf182bfea40bcb772bec692f59539c7e17ad26
rillig <rillig@NetBSD.org>
2024-03-09 16:47:09 +0000

    tests/lint: test check for out-of-bounds array index

--- tests/usr.bin/xlint/lint1/platform_ilp32_int.c
+++ tests/usr.bin/xlint/lint1/platform_ilp32_int.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: platform_ilp32_int.c,v 1.2 2023/03/28 14:44:35 rillig Exp $	*/
+/*	$NetBSD: platform_ilp32_int.c,v 1.3 2024/03/09 16:47:09 rillig Exp $	*/
 # 3 "platform_ilp32_int.c"
 
 /*
@@ -26,4 +26,26 @@ convert_between_int_and_long(void)
 	sl32 = s32;
 	u32 = ul32;
 	ul32 = u32;
+}
+
+char ch;
+
+void
+array_index(void)
+{
+	static char buf[20];
+
+	/* expect+1: warning: array subscript cannot be > 19: 2147483647 [168] */
+	ch += buf[2147483647];
+	/* expect+2: warning: conversion of 'long long' to 'int' is out of range [119] */
+	/* expect+1: warning: array subscript cannot be negative: -2147483648 [167] */
+	ch += buf[2147483648];
+	/* expect+2: warning: conversion of 'unsigned int' to 'int' is out of range [119] */
+	/* expect+1: warning: array subscript cannot be negative: -2147483648 [167] */
+	ch += buf[0x80000000];
+	/* expect+2: warning: conversion of 'unsigned int' to 'int' is out of range [119] */
+	/* expect+1: warning: array subscript cannot be negative: -1 [167] */
+	ch += buf[0xffffffff];
+	/* expect+1: warning: array subscript cannot be negative: -1 [167] */
+	ch += buf[0xffffffffffffffff];
 }
