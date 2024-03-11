d31492db19a097a547f780bad9cf641793697fd0
martin <martin@NetBSD.org>
2024-03-11 20:08:15 +0000

    Pull up following revision(s) (requested by riastradh in ticket #631):
    	sys/dev/pci/ehci_pci.c: revision 1.76
    
    Consistently use cached chipset tag value.

--- sys/dev/pci/ehci_pci.c
+++ sys/dev/pci/ehci_pci.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: ehci_pci.c,v 1.75 2022/10/28 21:56:44 riastradh Exp $	*/
+/*	$NetBSD: ehci_pci.c,v 1.75.2.1 2024/03/11 20:08:15 martin Exp $	*/
 
 /*
  * Copyright (c) 2001, 2002 The NetBSD Foundation, Inc.
@@ -30,7 +30,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: ehci_pci.c,v 1.75 2022/10/28 21:56:44 riastradh Exp $");
+__KERNEL_RCSID(0, "$NetBSD: ehci_pci.c,v 1.75.2.1 2024/03/11 20:08:15 martin Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -196,7 +196,7 @@ ehci_pci_attach(device_t parent, device_t self, void *aux)
 	 * Allocate IRQ
 	 */
 	intrstr = pci_intr_string(pc, sc->sc_pihp[0], intrbuf, sizeof(intrbuf));
-	pci_intr_setattr(pa->pa_pc, &sc->sc_pihp[0], PCI_INTR_MPSAFE, true);
+	pci_intr_setattr(pc, &sc->sc_pihp[0], PCI_INTR_MPSAFE, true);
 	sc->sc_ih = pci_intr_establish_xname(pc, sc->sc_pihp[0], IPL_USB,
 	    ehci_intr, sc, device_xname(self));
 	if (sc->sc_ih == NULL) {
