16095d554f53e10839d981e5dd86acbdb63acb95
christos <christos@NetBSD.org>
2024-04-02 18:40:50 +0000

    undo accidental commit.

--- lib/libm/src/s_fabsl.c
+++ lib/libm/src/s_fabsl.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: s_fabsl.c,v 1.7 2024/04/02 18:39:51 christos Exp $	*/
+/*	$NetBSD: s_fabsl.c,v 1.8 2024/04/02 18:40:50 christos Exp $	*/
 
 /*-
  * Copyright (c) 2010 The NetBSD Foundation, Inc.
@@ -26,7 +26,7 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 #include <sys/cdefs.h>
-__RCSID("$NetBSD: s_fabsl.c,v 1.7 2024/04/02 18:39:51 christos Exp $");
+__RCSID("$NetBSD: s_fabsl.c,v 1.8 2024/04/02 18:40:50 christos Exp $");
 
 #include <math.h>
 #include <machine/ieee.h>
@@ -47,10 +47,12 @@ fabsl(long double x)
 	return (ux.extu_ld);
 }
 #else
+#if 0
 /* defined in libc */
 long double
 fabsl(long double x)
 {
 	return fabs(x);
 }
+#endif
 #endif
