d2731ebc4dec096fa67d4583a3fa39512895fcc2
yamaguchi <yamaguchi@NetBSD.org>
2024-04-04 08:36:03 +0000

    lagg(4): change errno
    suggested by ozaki-r@, thanks.

--- sys/net/lagg/if_lagg.c
+++ sys/net/lagg/if_lagg.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: if_lagg.c,v 1.63 2024/04/04 08:31:58 yamaguchi Exp $	*/
+/*	$NetBSD: if_lagg.c,v 1.64 2024/04/04 08:36:03 yamaguchi Exp $	*/
 
 /*
  * Copyright (c) 2005, 2006 Reyk Floeter <reyk@openbsd.org>
@@ -20,7 +20,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: if_lagg.c,v 1.63 2024/04/04 08:31:58 yamaguchi Exp $");
+__KERNEL_RCSID(0, "$NetBSD: if_lagg.c,v 1.64 2024/04/04 08:36:03 yamaguchi Exp $");
 
 #ifdef _KERNEL_OPT
 #include "opt_inet.h"
@@ -1027,7 +1027,7 @@ lagg_tx_common(struct ifnet *ifp, struct mbuf *m)
 	} else {
 		m_freem(m);
 		if_statinc(ifp, if_oerrors);
-		error = ENOBUFS;
+		error = EIO;
 	}
 
 	lagg_variant_putref(var, &psref);
