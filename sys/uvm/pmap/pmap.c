59d7e1a78d69219ceee811a95052aa184922b699
martin <martin@NetBSD.org>
2024-04-19 09:18:28 +0000

    Pull up following revision(s) (requested by skrll in ticket #671):
    	sys/uvm/pmap/pmap.c: revision 1.78
    	sys/uvm/pmap/pmap.h: revision 1.27
    
    Fix types in pmap_page_clear_attributes so that the top bits of
    the u_long mdpg_attrs aren't dropped giving atomic_cas_ulong no
    chance of completing if any of the top bits is set.
    
    Update pmap_page_set_attributes for consistency.
    
    An ATF test run completed for me with this fix.
    
    port-riscv/58006: ATF tests no longer complete on riscv-riscv64

--- sys/uvm/pmap/pmap.c
+++ sys/uvm/pmap/pmap.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: pmap.c,v 1.74 2022/11/03 09:04:57 skrll Exp $	*/
+/*	$NetBSD: pmap.c,v 1.74.2.1 2024/04/19 09:18:28 martin Exp $	*/
 
 /*-
  * Copyright (c) 1998, 2001 The NetBSD Foundation, Inc.
@@ -67,7 +67,7 @@
 
 #include <sys/cdefs.h>
 
-__KERNEL_RCSID(0, "$NetBSD: pmap.c,v 1.74 2022/11/03 09:04:57 skrll Exp $");
+__KERNEL_RCSID(0, "$NetBSD: pmap.c,v 1.74.2.1 2024/04/19 09:18:28 martin Exp $");
 
 /*
  *	Manages physical address maps.
@@ -405,21 +405,21 @@ pmap_addr_range_check(pmap_t pmap, vaddr_t sva, vaddr_t eva, const char *func)
  */
 
 bool
-pmap_page_clear_attributes(struct vm_page_md *mdpg, u_int clear_attributes)
+pmap_page_clear_attributes(struct vm_page_md *mdpg, u_long clear_attributes)
 {
-	volatile unsigned long * const attrp = &mdpg->mdpg_attrs;
+	volatile u_long * const attrp = &mdpg->mdpg_attrs;
 
 #ifdef MULTIPROCESSOR
 	for (;;) {
-		u_int old_attr = *attrp;
+		u_long old_attr = *attrp;
 		if ((old_attr & clear_attributes) == 0)
 			return false;
-		u_int new_attr = old_attr & ~clear_attributes;
+		u_long new_attr = old_attr & ~clear_attributes;
 		if (old_attr == atomic_cas_ulong(attrp, old_attr, new_attr))
 			return true;
 	}
 #else
-	unsigned long old_attr = *attrp;
+	u_long old_attr = *attrp;
 	if ((old_attr & clear_attributes) == 0)
 		return false;
 	*attrp &= ~clear_attributes;
@@ -428,7 +428,7 @@ pmap_page_clear_attributes(struct vm_page_md *mdpg, u_int clear_attributes)
 }
 
 void
-pmap_page_set_attributes(struct vm_page_md *mdpg, u_int set_attributes)
+pmap_page_set_attributes(struct vm_page_md *mdpg, u_long set_attributes)
 {
 #ifdef MULTIPROCESSOR
 	atomic_or_ulong(&mdpg->mdpg_attrs, set_attributes);
