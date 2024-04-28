76b1e4b5b311ba8a632c44bd324426ac28f3cfda
martin <martin@NetBSD.org>
2024-04-28 10:11:21 +0000

    Welcome to 9.4_STABLE

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.599.2.14 2024/04/20 13:32:21 martin Exp $	*/
+/*	$NetBSD: param.h,v 1.599.2.15 2024/04/28 10:11:21 martin Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -67,7 +67,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	904000000	/* NetBSD 9.4 */
+#define	__NetBSD_Version__	904000000	/* NetBSD 9.4_STABLE */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
