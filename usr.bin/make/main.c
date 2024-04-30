7e10b2067b083b6a836a0c6e3993b9208741d155
sjg <sjg@NetBSD.org>
2024-04-30 16:13:33 +0000

    make: ensure '.include <makefile>' respects MAKESYSPATH
    Since Dir_FindFile is used by '.include' and its variants,
    and will first search .CURDIR unless the give path starts with
    ".DOTLAST".
    
    Update unit-tests/opt-m-include-dir to test this.

--- usr.bin/make/main.c
+++ usr.bin/make/main.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: main.c,v 1.613 2024/04/27 17:33:46 rillig Exp $	*/
+/*	$NetBSD: main.c,v 1.614 2024/04/30 16:13:33 sjg Exp $	*/
 
 /*
  * Copyright (c) 1988, 1989, 1990, 1993
@@ -111,7 +111,7 @@
 #include "trace.h"
 
 /*	"@(#)main.c	8.3 (Berkeley) 3/19/94"	*/
-MAKE_RCSID("$NetBSD: main.c,v 1.613 2024/04/27 17:33:46 rillig Exp $");
+MAKE_RCSID("$NetBSD: main.c,v 1.614 2024/04/30 16:13:33 sjg Exp $");
 #if defined(MAKE_NATIVE)
 __COPYRIGHT("@(#) Copyright (c) 1988, 1989, 1990, 1993 "
 	    "The Regents of the University of California.  "
@@ -1156,6 +1156,8 @@ InitDefSysIncPath(char *syspath)
 	else
 		syspath = bmake_strdup(syspath);
 
+	/* do NOT search .CURDIR first for .include <makefile> */
+	SearchPath_Add(defSysIncPath, ".DOTLAST");
 	for (start = syspath; *start != '\0'; start = p) {
 		for (p = start; *p != '\0' && *p != ':'; p++)
 			continue;
