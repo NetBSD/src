d2c36e3e8660b7bf2a08df287970689f3d38fbd1
skrll <skrll@NetBSD.org>
2024-05-06 06:58:00 +0000

    arm has been fixed

--- tests/lib/libc/setjmp/t_sigstack.c
+++ tests/lib/libc/setjmp/t_sigstack.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: t_sigstack.c,v 1.9 2024/04/04 00:46:42 riastradh Exp $	*/
+/*	$NetBSD: t_sigstack.c,v 1.10 2024/05/06 06:58:00 skrll Exp $	*/
 
 /*-
  * Copyright (c) 2024 The NetBSD Foundation, Inc.
@@ -27,7 +27,7 @@
  */
 
 #include <sys/cdefs.h>
-__RCSID("$NetBSD: t_sigstack.c,v 1.9 2024/04/04 00:46:42 riastradh Exp $");
+__RCSID("$NetBSD: t_sigstack.c,v 1.10 2024/05/06 06:58:00 skrll Exp $");
 
 #include <setjmp.h>
 #include <signal.h>
@@ -92,7 +92,7 @@ on_sigusr1(int signo, siginfo_t *si, void *ctx)
 	 *	vax
 	 *	x86_64
 	 */
-#if defined __arm__ || defined __hppa__ || \
+#if defined __hppa__ || \
     defined __ia64__ || defined __mips__ || defined __sh3__ || \
     defined __sparc__ || defined __sparc64__
 	if (nentries > 0)
