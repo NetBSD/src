c9f875b08c7caf1ee0837935d47d8bace6a12ac1
riastradh <riastradh@NetBSD.org>
2024-05-08 20:19:37 +0000

    tests/lib/libc/stdio/t_printf: Fix %a test the same way.

--- tests/lib/libc/stdio/t_printf.c
+++ tests/lib/libc/stdio/t_printf.c
@@ -1,4 +1,4 @@
-/* $NetBSD: t_printf.c,v 1.12 2024/05/08 20:04:33 riastradh Exp $ */
+/* $NetBSD: t_printf.c,v 1.13 2024/05/08 20:19:37 riastradh Exp $ */
 
 /*-
  * Copyright (c) 2010 The NetBSD Foundation, Inc.
@@ -196,7 +196,7 @@ ATF_TC_BODY(snprintf_double_a, tc)
 	ATF_CHECK_MSG((strcmp(buf, "0x1.533p+3") == 0 ||
 		strcmp(buf, "0x2.a66p+2") == 0 ||
 		strcmp(buf, "0x5.4ccp+1") == 0 ||
-		strcmp(buf, "0xa.998p+0") == 0),
+		strcmp(buf, "0xa.99ap+0") == 0),
 	    "buf=%s", buf);
 
 	snprintf(buf, sizeof buf, "%a", (double)0.125);
