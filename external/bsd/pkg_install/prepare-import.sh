ded410dd5dcc9507bd72189d9b894fdcc63aedbb
wiz <wiz@NetBSD.org>
2024-06-11 10:18:11 +0000

    Add new file for pkg_install-20240307, add import instructions.

--- external/bsd/pkg_install/prepare-import.sh
+++ external/bsd/pkg_install/prepare-import.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
-# $NetBSD: prepare-import.sh,v 1.4 2013/04/20 15:30:34 wiz Exp $
+# $NetBSD: prepare-import.sh,v 1.5 2024/06/11 10:18:11 wiz Exp $
 #
 # Copy new pkgsrc/pkgtools/pkg_install/files to dist.
 # Run this script and check for additional files and
 # directories to prune, only relevant content is included.
+#
+# Import with:
+#
+# cd dist
+# cvs -d cvs.netbsd.org:/cvsroot import -m "Import pkg_install 20xxxxxx from pkgsrc" src/external/bsd/pkg_install/dist PKGSRC pkg_install-20xxxxxx
 
 set -e
 
