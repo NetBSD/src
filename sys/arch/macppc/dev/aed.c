02b776129b7c0dfa5abb4eea2d30a494cd872497
martin <martin@NetBSD.org>
2024-06-20 18:15:15 +0000

    Pull up following revision(s) (requested by nat in ticket #708):
    	sys/arch/macppc/dev/aed.c: revision 1.35
    	sys/arch/mac68k/dev/aed.c: revision 1.39
    
    Pass event up to adb device.
    
    While here do the same for macppc.
    
    Addresses PR/58303

--- sys/arch/macppc/dev/aed.c
+++ sys/arch/macppc/dev/aed.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: aed.c,v 1.34 2021/09/26 16:36:19 thorpej Exp $	*/
+/*	$NetBSD: aed.c,v 1.34.4.1 2024/06/20 18:15:15 martin Exp $	*/
 
 /*
  * Copyright (C) 1994	Bradley A. Grantham
@@ -26,7 +26,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: aed.c,v 1.34 2021/09/26 16:36:19 thorpej Exp $");
+__KERNEL_RCSID(0, "$NetBSD: aed.c,v 1.34.4.1 2024/06/20 18:15:15 martin Exp $");
 
 #include <sys/param.h>
 #include <sys/device.h>
@@ -167,6 +167,7 @@ aed_input(adb_event_t *event)
 			aed_dokeyupdown(&new_event);
 		break;
 	case ADBADDR_MS:
+		event->u.m.buttons |= aed_sc->sc_buttons;
 		new_event.u.m.buttons |= aed_sc->sc_buttons;
 		aed_handoff(&new_event);
 		break;
