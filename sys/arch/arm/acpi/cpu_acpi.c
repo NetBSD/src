7520b0a97e6098433cb3044b2d0cae1547719dd5
jmcneill <jmcneill@NetBSD.org>
2024-06-30 17:58:08 +0000

    aarch64: Add NUMA awareness for ACPI systems with SRAT tables.
    On an Ampere Altra w/ hemisphere mode enabled:
    
    [   1.0000000] cpu0: package 16, core 0, smt 0, numa 0
    [...]
    [   1.0000000] cpu32: package 22, core 0, smt 0, numa 1
    
    [     1.000004] SRAT: 2 NUMA nodes
    [     1.000004] SRAT: node 0 memory range 0 (0x88300000 - 0x88400000 flags 1)
    [     1.000004] SRAT: node 0 memory range 1 (0x90000000 - 0x100000000 flags 1)
    [     1.000004] SRAT: node 0 memory range 2 (0x80000000000 - 0x80080000000 flags 1)
    [     1.000004] SRAT: node 0 memory range 3 (0x80100000000 - 0x81000000000 flags 1)
    [     1.000004] SRAT: node 1 memory range 0 (0xc0000000000 - 0xc1000000000 flags 1)

--- sys/arch/arm/acpi/cpu_acpi.c
+++ sys/arch/arm/acpi/cpu_acpi.c
@@ -1,4 +1,4 @@
-/* $NetBSD: cpu_acpi.c,v 1.15 2024/05/09 12:41:08 pho Exp $ */
+/* $NetBSD: cpu_acpi.c,v 1.16 2024/06/30 17:58:08 jmcneill Exp $ */
 
 /*-
  * Copyright (c) 2018 The NetBSD Foundation, Inc.
@@ -33,7 +33,7 @@
 #include "opt_multiprocessor.h"
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: cpu_acpi.c,v 1.15 2024/05/09 12:41:08 pho Exp $");
+__KERNEL_RCSID(0, "$NetBSD: cpu_acpi.c,v 1.16 2024/06/30 17:58:08 jmcneill Exp $");
 
 #include <sys/param.h>
 #include <sys/bus.h>
@@ -45,6 +45,7 @@ __KERNEL_RCSID(0, "$NetBSD: cpu_acpi.c,v 1.15 2024/05/09 12:41:08 pho Exp $");
 
 #include <dev/acpi/acpireg.h>
 #include <dev/acpi/acpivar.h>
+#include <dev/acpi/acpi_srat.h>
 
 #include <arm/armreg.h>
 #include <arm/cpu.h>
@@ -100,6 +101,7 @@ cpu_acpi_attach(device_t parent, device_t self, void *aux)
 	const uint64_t mpidr = gicc->ArmMpidr;
 	const int unit = device_unit(self);
 	struct cpu_info *ci = &cpu_info_store[unit];
+	struct acpisrat_node *node;
 
 #ifdef MULTIPROCESSOR
 	if (cpu_mpidr_aff_read() != mpidr && (boothowto & RB_MD1) == 0) {
@@ -132,6 +134,15 @@ cpu_acpi_attach(device_t parent, device_t self, void *aux)
 
 	/* Store the ACPI Processor UID in cpu_info */
 	ci->ci_acpiid = gicc->Uid;
+
+	/* Scan SRAT for NUMA info. */
+	if (cpu_mpidr_aff_read() == mpidr) {
+		acpisrat_init();
+	}
+	node = acpisrat_get_node(gicc->Uid);
+	if (node != NULL) {
+		ci->ci_numa_id = node->nodeid;
+	}
 
 	/* Attach the CPU */
 	cpu_attach(self, mpidr);
