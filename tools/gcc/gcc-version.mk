0b4652e5969359505c08e54a368a9a93ea5fd985
mrg <mrg@NetBSD.org>
2024-06-30 09:41:47 +0000

    update netbsd gcc version/date.

--- tools/gcc/gcc-version.mk
+++ tools/gcc/gcc-version.mk
@@ -1,4 +1,4 @@
-#	$NetBSD: gcc-version.mk,v 1.26 2024/02/25 02:24:19 mrg Exp $
+#	$NetBSD: gcc-version.mk,v 1.27 2024/06/30 09:41:47 mrg Exp $
 
 # common location for tools and native build
 
@@ -6,5 +6,5 @@
 NETBSD_GCC_VERSION=nb3 20231008
 .endif
 .if ${HAVE_GCC} == 12
-NETBSD_GCC_VERSION=nb2 20240221
+NETBSD_GCC_VERSION=nb1 20240630
 .endif
