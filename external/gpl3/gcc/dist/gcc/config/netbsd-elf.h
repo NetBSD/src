96a1716913885d5bec20b9c2c154dcffaa5d5636
rin <rin@NetBSD.org>
2024-07-01 05:12:11 +0000

    gcc: netbsd-elf.h: Remove duplicate `SUBTARGET_EXTRA_SPECS` definition

--- external/gpl3/gcc/dist/gcc/config/netbsd-elf.h
+++ external/gpl3/gcc/dist/gcc/config/netbsd-elf.h
@@ -106,18 +106,6 @@ along with GCC; see the file COPYING3.  If not see
 #undef SUBTARGET_EXTRA_SPECS
 #define SUBTARGET_EXTRA_SPECS   NETBSD_SUBTARGET_EXTRA_SPECS
 
-
-/* Provide the standard list of subtarget extra specs for NetBSD targets.  */
-#define NETBSD_SUBTARGET_EXTRA_SPECS \
-  { "netbsd_link_ld_elf_so",    NETBSD_LINK_LD_ELF_SO_SPEC }, \
-  { "netbsd_cpp_spec",          NETBSD_CPP_SPEC }, \
-  { "netbsd_link_spec",         NETBSD_LINK_SPEC_ELF }, \
-  { "netbsd_entry_point",       NETBSD_ENTRY_POINT }, \
-  { "netbsd_endfile_spec",      NETBSD_ENDFILE_SPEC },
-
-#undef SUBTARGET_EXTRA_SPECS
-#define SUBTARGET_EXTRA_SPECS   NETBSD_SUBTARGET_EXTRA_SPECS
-
 /* Use --as-needed -lgcc_s for eh support.  */
 #ifdef HAVE_LD_AS_NEEDED
 #define USE_LD_AS_NEEDED 1
