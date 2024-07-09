b2c35e17b976cf7ccd7250c86c6f5e95090ed636
christos <christos@NetBSD.org>
2024-07-09 04:29:09 +0000

    regen x86, adjust Makefiles.

--- external/gpl3/binutils/lib/libsframe/arch/i386/defs.mk
+++ external/gpl3/binutils/lib/libsframe/arch/i386/defs.mk
@@ -2,6 +2,4 @@
 # Generated from: NetBSD: mknative-binutils,v 1.16 2024/06/30 16:11:11 christos Exp 
 # Generated from: NetBSD: mknative.common,v 1.16 2018/04/15 15:13:37 christos Exp 
 #
-G_archdefs=-DARCH_i386 -DARCH_iamcu
-G_BFD_MACHINES=i386-dis.lo
-G_libopcodes_la_SOURCES=dis-buf.c disassemble.c dis-init.c
+G_libsframe_la_SOURCES=sframe.c sframe-dump.c sframe-error.c
