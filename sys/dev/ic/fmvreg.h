aed94bfe36feb4a2ce8694e424bce09c467ac1c1
andvar <andvar@NetBSD.org>
2024-07-20 20:53:53 +0000

    s/hardwar-cursor/hardware-cursor/, s/hardrare/hardware/ in comments.

--- sys/dev/ic/fmvreg.h
+++ sys/dev/ic/fmvreg.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: fmvreg.h,v 1.1 2002/10/05 15:16:11 tsutsui Exp $	*/
+/*	$NetBSD: fmvreg.h,v 1.2 2024/07/20 20:53:53 andvar Exp $	*/
 
 /*
  * All Rights Reserved, Copyright (C) Fujitsu Limited 1995
@@ -35,7 +35,7 @@
  */
 
 #define FE_FMV0		16	/* Hardware status.		*/
-#define FE_FMV1		17	/* Hardrare type?  Always 0	*/
+#define FE_FMV1		17	/* Hardware type?  Always 0	*/
 #define FE_FMV2		18	/* Hardware configuration.	*/
 #define FE_FMV3		19	/* Hardware enable.		*/
 #define FE_FMV4		20	/* Station address #1		*/
