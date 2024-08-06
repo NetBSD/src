ca4562dc2691840e224ee60f5103217ffceb3a4c
macallan <macallan@NetBSD.org>
2024-08-06 07:26:56 +0000

    moar bits & pieces

--- sys/dev/ic/stireg.h
+++ sys/dev/ic/stireg.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: stireg.h,v 1.11 2024/08/05 09:43:37 macallan Exp $	*/
+/*	$NetBSD: stireg.h,v 1.12 2024/08/06 07:26:56 macallan Exp $	*/
 
 /*	$OpenBSD: stireg.h,v 1.14 2015/04/05 23:25:57 miod Exp $	*/
 
@@ -644,23 +644,28 @@ STI_DEP(util);
 
 #define BA(F,C,S,A,J,B,I)						\
 	(((F)<<31)|((C)<<27)|((S)<<24)|((A)<<21)|((J)<<16)|((B)<<12)|(I))
-	/* FCCCCSSSAAAJJJJJBBBBIIIIIIIIIIII */
+	/* FCCC CSSS AAAJ JJJJ BBBB IIII IIII IIII */
 
 #define IBOvals(R,M,X,S,D,L,B,F)					\
 	(((R)<<8)|((M)<<16)|((X)<<24)|((S)<<29)|((D)<<28)|((L)<<31)|((B)<<1)|(F))
-	/* LSSDXXXXMMMMMMMMRRRRRRRR??????BF */
+	/* LSSD XXXX MMMM MMMM RRRR RRRR ???? ??BF */
 
 #define	    IndexedDcd	0	/* Pixel data is indexed (pseudo) color */
+#define	    FractDcd	1	/* Pixel data is Fractional 8-8-8 */
 #define	    Otc04	2	/* Pixels in each longword transfer (4) */
 #define	    Otc32	5	/* Pixels in each longword transfer (32) */
+#define	    Otc24	7	/* NGLE uses this for 24bit blits */
 #define	    Ots08	3	/* Each pixel is size (8)d transfer (1) */
 #define	    OtsIndirect	6	/* Each bit goes through FG/BG color(8) */
+#define	    AddrByte	3	/* byte access? Used by NGLE for direct fb */
 #define	    AddrLong	5	/* FB address is Long aligned (pixel) */
-#define	    BINovly	0x2	/* 8 bit overlay */
 #define	    BINapp0I	0x0	/* Application Buffer 0, Indexed */
 #define	    BINapp1I	0x1	/* Application Buffer 1, Indexed */
+#define	    BINovly	0x2	/* 8 bit overlay */
+#define	    BINcursor	0x7	/* cursor bitmap on EG */
 #define	    BINapp0F8	0xa	/* Application Buffer 0, Fractional 8-8-8 */
 #define	    BINattr	0xd	/* Attribute Bitmap */
+#define	    BINcmap	0xf	/* colour map(s) */
 #define	    RopClr 	0x0
 #define	    RopSrc 	0x3
 #define	    RopInv 	0xc
