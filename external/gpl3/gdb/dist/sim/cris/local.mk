c8aa0b40a4f1749093e5a69cc0a7c5e910426525
christos <christos@NetBSD.org>
2024-08-12 21:37:18 +0000

    Import gdb-15.1, previous was 13.2
    ChangeLog:
    
    2024-03-20  Simon Marchi  <simon.marchi@efficios.com>
    
    	* .pre-commit-config.yaml: Bump black hook to 24.3.0
    
    2024-03-20  Simon Marchi  <simon.marchi@efficios.com>
    
    	* .pre-commit-config.yaml: New.
    
    2024-03-14  Simon Marchi  <simon.marchi@efficios.com>
    
    	* Makefile.def: Add configure-gdbserver and all-gdbserver
    	dependencies on all-libiconv.
    	* Makefile.in: Re-generate.
    
    2024-01-15  Nick Clifton  <nickc@redhat.com>
    
    	* 2.42 branch point.
    
    2023-11-15  Arsen Arsenović  <arsen@aarsen.me>
    
    	* intl: Remove directory.  Replaced with out-of-tree GNU
    	gettext.
    	* .gitignore: Add '/gettext*'.
    	* configure.ac (host_libs): Replace intl with gettext.
    	(hbaseargs, bbaseargs, baseargs): Split baseargs into
    	{h,b}baseargs.
    	(skip_barg): New flag.  Skips appending current flag to
    	bbaseargs.
    	<library exemptions>: Exempt --with-libintl-{type,prefix} from
    	target and build machine argument passing.
    	* configure: Regenerate.
    	* Makefile.def (host_modules): Replace intl module with gettext
    	module.
    	(configure-ld): Depend on configure-gettext.
    	* Makefile.in: Regenerate.
    	* src-release.sh: Remove references to the intl/ directory.
    
    2023-07-03  Nick Clifton  <nickc@redhat.com>
    
    	2.41 Branch Point.
    
    2023-06-26  Nick Clifton  <nickc@redhat.com>
    
    	* Import these updates to the config scripts
    
    	commit 4ad4bb7c30aca1e705448ba8d51a210bbd47bb52
    	Author: Paul Eggert <eggert@cs.ucla.edu>
    	Date:   Fri Jun 23 09:55:10 2023 -0700
    
    		Quote 'like this', not `like this'.
    
    	commit 63acb96f92473ceb5e21d873d7c0aee266b3d6d3
    	Author: Paul Eggert <eggert@cs.ucla.edu>
    	Date:   Sat Jan 21 00:15:01 2023 -0600
    
    		Fix config.sub spelling typo for "athlon"
    
    	commit 4ce12a5c9125cedc0d0ba584444a6865396923ec
    	Author: Dmitry V. Levin <ldv@altlinux.org>
    	Date:   Sun Jan 1 08:00:00 2023 +0000
    
    		Update copyright years
    
    	commit c397e2c040bce50bcdccb131f90115ba7e8bfc19
    	Author: Arsen Arsenovi <arsen@aarsen.me>
    	Date:   Sat Sep 17 23:34:48 2022 +0200
    
    		config.sub: add linux-mlibc targets
    
    	commit 9f9f9b0b13197269848c76e3e057a3ed0680b4bf
    	Author: Arsen Arsenovi <arsen@aarsen.me>
    	Date:   Sat Sep 17 23:34:47 2022 +0200
    
    		config.guess: support running on Managarm systems
    
    	commit 87e6687749da7bb2ab158a79fa83721c19ed9246
    	Author: Arsen Arsenovi <arsen@aarsen.me>
    	Date:   Sat Sep 17 23:34:46 2022 +0200
    
    		config.sub: add managarm-{mlibc,kernel} targets
    
    	commit 20403c5701973a4cbd7e0b4bbeb627fcd424a0f1
    	Author: Xiaotian Wu <wuxiaotian@loongson.cn>
    	Date:   Mon Aug 1 16:05:29 2022 +0800
    
    		Remove loongarchx32
    
    	commit 02ba26b218d3d3db6c56e014655faf463cefa983
    	Author: Alexander von Gluck IV <kallisti5@unixzen.com>
    	Date:   Wed May 25 15:43:13 2022 -0500
    
    		config.guess: Update Haiku guesses
    
    	commit f56a7140386d08a531bcfd444d632b28c61a6329
    	Author: Bruno Haible <bruno@clisp.org>
    	Date:   Sun May 8 19:08:08 2022 +0200
    
    		config.guess (x86_64:Linux:*:*): Detect 32-bit ABI.
    
    2023-04-20  Nick Clifton  <nickc@redhat.com>
    
    	* SECURITY.txt: New file.
    	* src-release.sh (DEVO_SUPPORT): Add SECURITY.txt.
    
    2022-12-31  Nick Clifton  <nickc@redhat.com>
    
    	* 2.40 binutils branch created.

--- external/gpl3/gdb/dist/sim/cris/local.mk
+++ external/gpl3/gdb/dist/sim/cris/local.mk
@@ -1,6 +1,6 @@
 ## See sim/Makefile.am
 ##
-## Copyright (C) 2004-2023 Free Software Foundation, Inc.
+## Copyright (C) 2004-2024 Free Software Foundation, Inc.
 ## Contributed by Axis Communications.
 ##
 ## This program is free software; you can redistribute it and/or modify
@@ -16,6 +16,50 @@
 ## You should have received a copy of the GNU General Public License
 ## along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+## Some CGEN kludges are causing build-time warnings.  See cris.cpu for details.
+AM_CFLAGS_%C%_mloopv10f.o = $(SIM_CFLAG_WNO_UNUSED_BUT_SET_VARIABLE)
+AM_CFLAGS_%C%_mloopv32f.o = $(SIM_CFLAG_WNO_UNUSED_BUT_SET_VARIABLE)
+## Some CGEN assignments use variable names that are nested & repeated.
+AM_CFLAGS_%C%_mloopv10f.o += $(SIM_CFLAG_WNO_SHADOW_LOCAL)
+
+nodist_%C%_libsim_a_SOURCES = \
+	%D%/modules.c
+%C%_libsim_a_SOURCES = \
+	$(common_libcommon_a_SOURCES)
+%C%_libsim_a_LIBADD = \
+	$(patsubst %,%D%/%,$(SIM_NEW_COMMON_OBJS)) \
+	$(patsubst %,%D%/dv-%.o,$(SIM_HW_DEVICES)) \
+	$(patsubst %,%D%/dv-%.o,$(%C%_SIM_EXTRA_HW_DEVICES)) \
+	\
+	%D%/cgen-run.o \
+	%D%/cgen-scache.o \
+	%D%/cgen-trace.o \
+	%D%/cgen-utils.o \
+	\
+	%D%/arch.o \
+	%D%/crisv10f.o \
+	%D%/cpuv10.o \
+	%D%/decodev10.o \
+	%D%/modelv10.o \
+	%D%/mloopv10f.o \
+	%D%/crisv32f.o \
+	%D%/cpuv32.o \
+	%D%/decodev32.o \
+	%D%/modelv32.o \
+	%D%/mloopv32f.o \
+	\
+	%D%/sim-if.o \
+	%D%/traps.o
+$(%C%_libsim_a_OBJECTS) $(%C%_libsim_a_LIBADD): %D%/hw-config.h
+
+noinst_LIBRARIES += %D%/libsim.a
+
+## Override wildcards that trigger common/modules.c to be (incorrectly) used.
+%D%/modules.o: %D%/modules.c
+
+%D%/%.o: common/%.c ; $(SIM_COMPILE)
+-@am__include@ %D%/$(DEPDIR)/*.Po
+
 %C%_run_SOURCES =
 %C%_run_LDADD = \
 	%D%/nrun.o \
@@ -24,6 +68,8 @@
 
 noinst_PROGRAMS += %D%/run
 
+%C%_SIM_EXTRA_HW_DEVICES = rv cris cris_900000xx
+
 ## rvdummy is just used for testing -- it runs on the same host as `run`.
 ## It does nothing if --enable-sim-hardware isn't active.
 %C%_rvdummy_SOURCES = %D%/rvdummy.c
@@ -31,25 +77,26 @@ noinst_PROGRAMS += %D%/run
 
 check_PROGRAMS += %D%/rvdummy
 
-%C%_BUILD_OUTPUTS = \
+## List all generated headers to help Automake dependency tracking.
+BUILT_SOURCES += \
 	%D%/engv10.h \
+	%D%/engv32.h
+%C%_BUILD_OUTPUTS = \
 	%D%/mloopv10f.c \
 	%D%/stamp-mloop-v10f \
-	%D%/engv32.h \
 	%D%/mloopv32f.c \
 	%D%/stamp-mloop-v32f
 
-## This makes sure build tools are available before building the arch-subdirs.
-SIM_ALL_RECURSIVE_DEPS += $(%C%_BUILD_OUTPUTS)
+## Generating modules.c requires all sources to scan.
+%D%/modules.c: | $(%C%_BUILD_OUTPUTS)
 
 ## FIXME: What is mono and what does "Use of `mono' is wip" mean (other
 ## than the apparent; some "mono" feature is work in progress)?
 %D%/mloopv10f.c %D%/engv10.h: %D%/stamp-mloop-v10f ; @true
-%D%/stamp-mloop-v10f: $(srccom)/genmloop.sh %D%/mloop.in
-	$(AM_V_GEN)$(SHELL) $(srccom)/genmloop.sh -shell $(SHELL) \
+%D%/stamp-mloop-v10f: %D%/mloop.in $(srccom)/genmloop.sh
+	$(AM_V_GEN)$(CGEN_GEN_MLOOP) \
 		-mono -no-fast -pbb -switch semcrisv10f-switch.c \
-		-cpu crisv10f \
-		-infile $(srcdir)/%D%/mloop.in -outfile-prefix %D%/ -outfile-suffix -v10f
+		-cpu crisv10f -outfile-suffix -v10f
 	$(AM_V_at)$(SHELL) $(srcroot)/move-if-change %D%/eng-v10f.hin %D%/engv10.h
 	$(AM_V_at)$(SHELL) $(srcroot)/move-if-change %D%/mloop-v10f.cin %D%/mloopv10f.c
 	$(AM_V_at)touch $@
@@ -57,13 +104,30 @@ SIM_ALL_RECURSIVE_DEPS += $(%C%_BUILD_OUTPUTS)
 ## FIXME: What is mono and what does "Use of `mono' is wip" mean (other
 ## than the apparent; some "mono" feature is work in progress)?
 %D%/mloopv32f.c %D%/engv32.h: %D%/stamp-mloop-v32f ; @true
-%D%/stamp-mloop-v32f: $(srccom)/genmloop.sh %D%/mloop.in
-	$(AM_V_GEN)$(SHELL) $(srccom)/genmloop.sh -shell $(SHELL) \
+%D%/stamp-mloop-v32f: %D%/mloop.in $(srccom)/genmloop.sh
+	$(AM_V_GEN)$(CGEN_GEN_MLOOP) \
 		-mono -no-fast -pbb -switch semcrisv32f-switch.c \
-		-cpu crisv32f \
-		-infile $(srcdir)/%D%/mloop.in -outfile-prefix %D%/ -outfile-suffix -v32f
+		-cpu crisv32f -outfile-suffix -v32f
 	$(AM_V_at)$(SHELL) $(srcroot)/move-if-change %D%/eng-v32f.hin %D%/engv32.h
 	$(AM_V_at)$(SHELL) $(srcroot)/move-if-change %D%/mloop-v32f.cin %D%/mloopv32f.c
 	$(AM_V_at)touch $@
 
+CLEANFILES += %D%/engv10.h %D%/engv32.h
 MOSTLYCLEANFILES += $(%C%_BUILD_OUTPUTS)
+
+## Target that triggers all cgen targets that works when --disable-cgen-maint.
+%D%/cgen: %D%/cgen-arch %D%/cgen-cpu-decode-v10f %D%/cgen-cpu-decode-v32f
+
+%D%/cgen-arch:
+	$(AM_V_GEN)mach=crisv10,crisv32 FLAGS="with-scache with-profile=fn"; $(CGEN_GEN_ARCH)
+$(srcdir)/%D%/arch.h $(srcdir)/%D%/arch.c $(srcdir)/%D%/cpuall.h: @CGEN_MAINT@ %D%/cgen-arch
+
+%D%/cgen-cpu-decode-v10f:
+	$(AM_V_GEN)cpu=crisv10f mach=crisv10 SUFFIX=v10 FLAGS="with-scache with-profile=fn" EXTRAFILES="$(CGEN_CPU_SEMSW)"; $(CGEN_GEN_CPU_DECODE)
+	$(AM_V_at)$(SHELL) $(srcroot)/move-if-change $(srcdir)/%D%/semv10-switch.c $(srcdir)/%D%/semcrisv10f-switch.c
+$(srcdir)/%D%/cpuv10.h $(srcdir)/%D%/cpuv10.c $(srcdir)/%D%/semcrisv10f-switch.c $(srcdir)/%D%/modelv10.c $(srcdir)/%D%/decodev10.c $(srcdir)/%D%/decodev10.h: @CGEN_MAINT@ %D%/cgen-cpu-decode-v10f
+
+%D%/cgen-cpu-decode-v32f:
+	$(AM_V_GEN)cpu=crisv32f mach=crisv32 SUFFIX=v32 FLAGS="with-scache with-profile=fn" EXTRAFILES="$(CGEN_CPU_SEMSW)"; $(CGEN_GEN_CPU_DECODE)
+	$(AM_V_at)$(SHELL) $(srcroot)/move-if-change $(srcdir)/%D%/semv32-switch.c $(srcdir)/%D%/semcrisv32f-switch.c
+$(srcdir)/%D%/cpuv32.h $(srcdir)/%D%/cpuv32.c $(srcdir)/%D%/semcrisv32f-switch.c $(srcdir)/%D%/modelv32.c $(srcdir)/%D%/decodev32.c $(srcdir)/%D%/decodev32.h: @CGEN_MAINT@ %D%/cgen-cpu-decode-v32f
