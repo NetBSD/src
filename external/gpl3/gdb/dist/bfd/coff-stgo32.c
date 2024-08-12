8c80fa904c75c8dd261d11630cdd98d2613a9ac5
christos <christos@NetBSD.org>
2024-08-12 21:38:54 +0000

    Import gdb-15.1, previous was 13.2
    ChangeLog:
    
    2024-03-20  Simon Marchi  <simon.marchi@efficios.com>
    
    	* .pre-commit-config.yaml: Bump black hook to 24.3.0
    
    2024-03-20  Simon Marchi  <simon.marchi@efficios.com>
    
    	* .pre-commit-config.yaml: New.
    
    2024-03-14  Simon Marchi  <simon.marchi@efficios.com>
    
    	* Makefile.def: Add configure-gdbserver and all-gdbserver
    	dependencies on all-libiconv.
    	* Makefile.in: Re-generate.
    
    2024-01-15  Nick Clifton  <nickc@redhat.com>
    
    	* 2.42 branch point.
    
    2023-11-15  Arsen Arsenović  <arsen@aarsen.me>
    
    	* intl: Remove directory.  Replaced with out-of-tree GNU
    	gettext.
    	* .gitignore: Add '/gettext*'.
    	* configure.ac (host_libs): Replace intl with gettext.
    	(hbaseargs, bbaseargs, baseargs): Split baseargs into
    	{h,b}baseargs.
    	(skip_barg): New flag.  Skips appending current flag to
    	bbaseargs.
    	<library exemptions>: Exempt --with-libintl-{type,prefix} from
    	target and build machine argument passing.
    	* configure: Regenerate.
    	* Makefile.def (host_modules): Replace intl module with gettext
    	module.
    	(configure-ld): Depend on configure-gettext.
    	* Makefile.in: Regenerate.
    	* src-release.sh: Remove references to the intl/ directory.
    
    2023-07-03  Nick Clifton  <nickc@redhat.com>
    
    	2.41 Branch Point.
    
    2023-06-26  Nick Clifton  <nickc@redhat.com>
    
    	* Import these updates to the config scripts
    
    	commit 4ad4bb7c30aca1e705448ba8d51a210bbd47bb52
    	Author: Paul Eggert <eggert@cs.ucla.edu>
    	Date:   Fri Jun 23 09:55:10 2023 -0700
    
    		Quote 'like this', not `like this'.
    
    	commit 63acb96f92473ceb5e21d873d7c0aee266b3d6d3
    	Author: Paul Eggert <eggert@cs.ucla.edu>
    	Date:   Sat Jan 21 00:15:01 2023 -0600
    
    		Fix config.sub spelling typo for "athlon"
    
    	commit 4ce12a5c9125cedc0d0ba584444a6865396923ec
    	Author: Dmitry V. Levin <ldv@altlinux.org>
    	Date:   Sun Jan 1 08:00:00 2023 +0000
    
    		Update copyright years
    
    	commit c397e2c040bce50bcdccb131f90115ba7e8bfc19
    	Author: Arsen Arsenovi <arsen@aarsen.me>
    	Date:   Sat Sep 17 23:34:48 2022 +0200
    
    		config.sub: add linux-mlibc targets
    
    	commit 9f9f9b0b13197269848c76e3e057a3ed0680b4bf
    	Author: Arsen Arsenovi <arsen@aarsen.me>
    	Date:   Sat Sep 17 23:34:47 2022 +0200
    
    		config.guess: support running on Managarm systems
    
    	commit 87e6687749da7bb2ab158a79fa83721c19ed9246
    	Author: Arsen Arsenovi <arsen@aarsen.me>
    	Date:   Sat Sep 17 23:34:46 2022 +0200
    
    		config.sub: add managarm-{mlibc,kernel} targets
    
    	commit 20403c5701973a4cbd7e0b4bbeb627fcd424a0f1
    	Author: Xiaotian Wu <wuxiaotian@loongson.cn>
    	Date:   Mon Aug 1 16:05:29 2022 +0800
    
    		Remove loongarchx32
    
    	commit 02ba26b218d3d3db6c56e014655faf463cefa983
    	Author: Alexander von Gluck IV <kallisti5@unixzen.com>
    	Date:   Wed May 25 15:43:13 2022 -0500
    
    		config.guess: Update Haiku guesses
    
    	commit f56a7140386d08a531bcfd444d632b28c61a6329
    	Author: Bruno Haible <bruno@clisp.org>
    	Date:   Sun May 8 19:08:08 2022 +0200
    
    		config.guess (x86_64:Linux:*:*): Detect 32-bit ABI.
    
    2023-04-20  Nick Clifton  <nickc@redhat.com>
    
    	* SECURITY.txt: New file.
    	* src-release.sh (DEVO_SUPPORT): Add SECURITY.txt.
    
    2022-12-31  Nick Clifton  <nickc@redhat.com>
    
    	* 2.40 binutils branch created.

--- external/gpl3/gdb/dist/bfd/coff-stgo32.c
+++ external/gpl3/gdb/dist/bfd/coff-stgo32.c
@@ -1,5 +1,5 @@
 /* BFD back-end for Intel 386 COFF files (DJGPP variant with a stub).
-   Copyright (C) 1997-2022 Free Software Foundation, Inc.
+   Copyright (C) 1997-2024 Free Software Foundation, Inc.
    Written by Robert Hoehne.
 
    This file is part of BFD, the Binary File Descriptor library.
@@ -248,6 +248,7 @@ static void
 go32exe_cleanup (bfd *abfd)
 {
   abfd->origin = 0;
+  coff_object_cleanup (abfd);
 
   free (go32exe_temp_stub);
   go32exe_temp_stub = NULL;
@@ -277,7 +278,7 @@ go32exe_check_format (bfd *abfd)
   bfd_set_error (bfd_error_system_call);
 
   /* Read in the stub file header, which is a DOS MZ executable.  */
-  if (bfd_bread (&filehdr_dos, DOS_HDR_SIZE, abfd) != DOS_HDR_SIZE)
+  if (bfd_read (&filehdr_dos, DOS_HDR_SIZE, abfd) != DOS_HDR_SIZE)
     goto fail;
 
   /* Make sure that this is an MZ executable.  */
@@ -298,11 +299,12 @@ go32exe_check_format (bfd *abfd)
   /* Save now the stub to be used later.  Put the stub data to a temporary
      location first as tdata still does not exist.  It may not even
      be ever created if we are just checking the file format of ABFD.  */
-  bfd_seek (abfd, 0, SEEK_SET);
+  if (bfd_seek (abfd, 0, SEEK_SET) != 0)
+    goto fail;
   go32exe_temp_stub = bfd_malloc (stubsize);
   if (go32exe_temp_stub == NULL)
     goto fail;
-  if (bfd_bread (go32exe_temp_stub, stubsize, abfd) != stubsize)
+  if (bfd_read (go32exe_temp_stub, stubsize, abfd) != stubsize)
     goto fail;
   go32exe_temp_stub_size = stubsize;
 
@@ -323,7 +325,7 @@ go32exe_check_format (bfd *abfd)
   bfd_cleanup cleanup = coff_object_p (abfd);
   if (cleanup == NULL)
     goto fail;
-  BFD_ASSERT (cleanup == _bfd_no_cleanup);
+  BFD_ASSERT (cleanup == coff_object_cleanup);
 
   return go32exe_cleanup;
 
@@ -350,7 +352,7 @@ go32exe_write_object_contents (bfd *abfd)
   abfd->origin = 0;
   if (bfd_seek (abfd, 0, SEEK_SET) != 0)
     return false;
-  if (bfd_bwrite (coff_data (abfd)->stub, stubsize, abfd) != stubsize)
+  if (bfd_write (coff_data (abfd)->stub, stubsize, abfd) != stubsize)
     return false;
 
   /* Seek back to where we were.  */
