89e534c4b53970b3404d95ff3b28a9afaf771512
christos <christos@NetBSD.org>
2024-08-12 21:37:17 +0000

    Import gdb-15.1, previous was 13.2
    ChangeLog:
    
    2024-03-20  Simon Marchi  <simon.marchi@efficios.com>
    
    	* .pre-commit-config.yaml: Bump black hook to 24.3.0
    
    2024-03-20  Simon Marchi  <simon.marchi@efficios.com>
    
    	* .pre-commit-config.yaml: New.
    
    2024-03-14  Simon Marchi  <simon.marchi@efficios.com>
    
    	* Makefile.def: Add configure-gdbserver and all-gdbserver
    	dependencies on all-libiconv.
    	* Makefile.in: Re-generate.
    
    2024-01-15  Nick Clifton  <nickc@redhat.com>
    
    	* 2.42 branch point.
    
    2023-11-15  Arsen Arsenović  <arsen@aarsen.me>
    
    	* intl: Remove directory.  Replaced with out-of-tree GNU
    	gettext.
    	* .gitignore: Add '/gettext*'.
    	* configure.ac (host_libs): Replace intl with gettext.
    	(hbaseargs, bbaseargs, baseargs): Split baseargs into
    	{h,b}baseargs.
    	(skip_barg): New flag.  Skips appending current flag to
    	bbaseargs.
    	<library exemptions>: Exempt --with-libintl-{type,prefix} from
    	target and build machine argument passing.
    	* configure: Regenerate.
    	* Makefile.def (host_modules): Replace intl module with gettext
    	module.
    	(configure-ld): Depend on configure-gettext.
    	* Makefile.in: Regenerate.
    	* src-release.sh: Remove references to the intl/ directory.
    
    2023-07-03  Nick Clifton  <nickc@redhat.com>
    
    	2.41 Branch Point.
    
    2023-06-26  Nick Clifton  <nickc@redhat.com>
    
    	* Import these updates to the config scripts
    
    	commit 4ad4bb7c30aca1e705448ba8d51a210bbd47bb52
    	Author: Paul Eggert <eggert@cs.ucla.edu>
    	Date:   Fri Jun 23 09:55:10 2023 -0700
    
    		Quote 'like this', not `like this'.
    
    	commit 63acb96f92473ceb5e21d873d7c0aee266b3d6d3
    	Author: Paul Eggert <eggert@cs.ucla.edu>
    	Date:   Sat Jan 21 00:15:01 2023 -0600
    
    		Fix config.sub spelling typo for "athlon"
    
    	commit 4ce12a5c9125cedc0d0ba584444a6865396923ec
    	Author: Dmitry V. Levin <ldv@altlinux.org>
    	Date:   Sun Jan 1 08:00:00 2023 +0000
    
    		Update copyright years
    
    	commit c397e2c040bce50bcdccb131f90115ba7e8bfc19
    	Author: Arsen Arsenovi <arsen@aarsen.me>
    	Date:   Sat Sep 17 23:34:48 2022 +0200
    
    		config.sub: add linux-mlibc targets
    
    	commit 9f9f9b0b13197269848c76e3e057a3ed0680b4bf
    	Author: Arsen Arsenovi <arsen@aarsen.me>
    	Date:   Sat Sep 17 23:34:47 2022 +0200
    
    		config.guess: support running on Managarm systems
    
    	commit 87e6687749da7bb2ab158a79fa83721c19ed9246
    	Author: Arsen Arsenovi <arsen@aarsen.me>
    	Date:   Sat Sep 17 23:34:46 2022 +0200
    
    		config.sub: add managarm-{mlibc,kernel} targets
    
    	commit 20403c5701973a4cbd7e0b4bbeb627fcd424a0f1
    	Author: Xiaotian Wu <wuxiaotian@loongson.cn>
    	Date:   Mon Aug 1 16:05:29 2022 +0800
    
    		Remove loongarchx32
    
    	commit 02ba26b218d3d3db6c56e014655faf463cefa983
    	Author: Alexander von Gluck IV <kallisti5@unixzen.com>
    	Date:   Wed May 25 15:43:13 2022 -0500
    
    		config.guess: Update Haiku guesses
    
    	commit f56a7140386d08a531bcfd444d632b28c61a6329
    	Author: Bruno Haible <bruno@clisp.org>
    	Date:   Sun May 8 19:08:08 2022 +0200
    
    		config.guess (x86_64:Linux:*:*): Detect 32-bit ABI.
    
    2023-04-20  Nick Clifton  <nickc@redhat.com>
    
    	* SECURITY.txt: New file.
    	* src-release.sh (DEVO_SUPPORT): Add SECURITY.txt.
    
    2022-12-31  Nick Clifton  <nickc@redhat.com>
    
    	* 2.40 binutils branch created.

--- external/gpl3/gdb/dist/libbacktrace/zstdtest.c
+++ external/gpl3/gdb/dist/libbacktrace/zstdtest.c
@@ -1,5 +1,5 @@
-/* ztest.c -- Test for libbacktrace inflate code.
-   Copyright (C) 2017-2024 Free Software Foundation, Inc.
+/* ztest.c -- Test for libbacktrace zstd code.
+   Copyright (C) 2022-2024 Free Software Foundation, Inc.
    Written by Ian Lance Taylor, Google.
 
 Redistribution and use in source and binary forms, with or without
@@ -40,8 +40,8 @@ POSSIBILITY OF SUCH DAMAGE.  */
 #include <sys/types.h>
 #include <sys/stat.h>
 
-#ifdef HAVE_ZLIB
-#include <zlib.h>
+#ifdef HAVE_ZSTD
+#include <zstd.h>
 #endif
 
 #include "backtrace.h"
@@ -70,14 +70,14 @@ xclock_gettime (xclockid_t id ATTRIBUTE_UNUSED,
 #endif /* !defined(HAVE_CLOCK_GETTIME) */
 
 #ifdef CLOCK_PROCESS_CPUTIME_ID
-#define ZLIB_CLOCK_GETTIME_ARG CLOCK_PROCESS_CPUTIME_ID
+#define ZSTD_CLOCK_GETTIME_ARG CLOCK_PROCESS_CPUTIME_ID
 #else
-#define ZLIB_CLOCK_GETTIME_ARG CLOCK_REALTIME
+#define ZSTD_CLOCK_GETTIME_ARG CLOCK_REALTIME
 #endif
 
-/* Some tests for the local zlib inflation code.  */
+/* Some tests for the local zstd inflation code.  */
 
-struct zlib_test
+struct zstd_test
 {
   const char *name;
   const char *uncompressed;
@@ -99,31 +99,30 @@ error_callback_compress (void *vdata ATTRIBUTE_UNUSED, const char *msg,
   exit (EXIT_FAILURE);
 }
 
-static const struct zlib_test tests[] =
+static const struct zstd_test tests[] =
 {
   {
     "empty",
     "",
     0,
-    "\x78\x9c\x03\x00\x00\x00\x00\x01",
-    8,
+    "\x28\xb5\x2f\xfd\x24\x00\x01\x00\x00\x99\xe9\xd8\x51",
+    13,
   },
   {
     "hello",
     "hello, world\n",
     0,
-    ("\x78\x9c\xca\x48\xcd\xc9\xc9\xd7\x51\x28\xcf"
-     "\x2f\xca\x49\xe1\x02\x04\x00\x00\xff\xff\x21\xe7\x04\x93"),
-    25,
+    ("\x28\xb5\x2f\xfd\x24\x0d\x69\x00\x00\x68\x65\x6c\x6c\x6f\x2c\x20"
+     "\x77\x6f\x72\x6c\x64\x0a\x4c\x1f\xf9\xf1"),
+    26,
   },
   {
     "goodbye",
     "goodbye, world",
     0,
-    ("\x78\x9c\x4b\xcf\xcf\x4f\x49\xaa"
-     "\x4c\xd5\x51\x28\xcf\x2f\xca\x49"
-     "\x01\x00\x28\xa5\x05\x5e"),
-    22,
+    ("\x28\xb5\x2f\xfd\x24\x0e\x71\x00\x00\x67\x6f\x6f\x64\x62\x79\x65"
+     "\x2c\x20\x77\x6f\x72\x6c\x64\x61\x7b\x4b\x83"),
+    27,
   },
   {
     "ranges",
@@ -170,17 +169,19 @@ static const struct zlib_test tests[] =
      "\x9f\x01\x00\x00\x00\x00\x00\x00\xa7\x01\x00\x00\x00\x00\x00\x00"
      "\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"),
     672,
-    ("\x78\x9c\x3b\x23\xc8\x00\x06\x57\x85\x21\xb4\x8c\x08\x84\x2e\x82"
-     "\xd2\x73\xa1\xf4\x55\x28\x8d\x0e\x7e\x0b\x41\x68\x4e\xa8\x7e\x1e"
-     "\x28\x7d\x1a\x4a\x6b\x42\xf5\xf9\x91\x69\x5e\x3a\x9a\x79\x84\xf4"
-     "\xc7\x73\x43\xe8\x1c\x28\x5d\x0b\xa5\xeb\x78\x20\xb4\x05\x3f\x84"
-     "\x8e\xe1\xc7\xae\xbf\x19\xaa\xee\x17\x94\xfe\xcb\x0b\xa1\xdf\xf3"
-     "\x41\x68\x11\x7e\x54\x73\xe6\x43\xe9\x35\x50\xfa\x36\x94\xfe\x8f"
-     "\xc3\x7c\x98\x79\x37\xf8\xc8\xd3\x0f\x73\xd7\x2b\x1c\xee\x8a\x21"
-     "\xd2\x5d\x3a\x02\xd8\xcd\x4f\x80\xa6\x87\x8b\x62\x10\xda\x81\x1b"
-     "\xbf\xfa\x2a\x28\xbd\x0d\x4a\xcf\x67\x84\xd0\xcb\x19\xf1\xab\x5f"
-     "\x49\xa4\x7a\x00\x48\x97\x29\xd4"),
-    152,
+    ("\x28\xb5\x2f\xfd\x64\xa0\x01\x2d\x05\x00\xc4\x04\xcc\x11\x00\xd5"
+     "\x13\x00\x1c\x14\x00\x72\x9d\xd5\xfb\x12\x00\x09\x0c\x13\xcb\x13"
+     "\x29\x4e\x67\x5f\x0b\x6c\x0b\x7d\x0b\x7e\x0c\x38\x0f\x5c\x0f\x83"
+     "\x0c\xfa\x0c\xfd\x0d\xef\x0e\x14\x38\x9f\x0f\xac\x0f\xdb\x0f\xff"
+     "\x0f\xd8\x9f\xac\xdb\xff\xea\x5c\x2c\x10\x60\xd1\x16\x40\x0b\x7a"
+     "\x00\xb6\x00\x9f\x01\xa7\x01\xa9\x36\x20\xa0\x83\x14\x34\x63\x4a"
+     "\x21\x70\x8c\x07\x46\x03\x4e\x10\x62\x3c\x06\x4e\xc8\x8c\xb0\x32"
+     "\x2a\x59\xad\xb2\xf1\x02\x82\x7c\x33\xcb\x92\x6f\x32\x4f\x9b\xb0"
+     "\xa2\x30\xf0\xc0\x06\x1e\x98\x99\x2c\x06\x1e\xd8\xc0\x03\x56\xd8"
+     "\xc0\x03\x0f\x6c\xe0\x01\xf1\xf0\xee\x9a\xc6\xc8\x97\x99\xd1\x6c"
+     "\xb4\x21\x45\x3b\x10\xe4\x7b\x99\x4d\x8a\x36\x64\x5c\x77\x08\x02"
+     "\xcb\xe0\xce"),
+    179,
   }
 };
 
@@ -193,61 +194,56 @@ test_samples (struct backtrace_state *state)
 
   for (i = 0; i < sizeof tests / sizeof tests[0]; ++i)
     {
-      char *p;
-      size_t v;
-      size_t j;
       unsigned char *uncompressed;
       size_t uncompressed_len;
 
-      p = malloc (12 + tests[i].compressed_len);
-      memcpy (p, "ZLIB", 4);
-      v = tests[i].uncompressed_len;
-      if (v == 0)
-	v = strlen (tests[i].uncompressed);
-      for (j = 0; j < 8; ++j)
-	p[j + 4] = (v >> ((7 - j) * 8)) & 0xff;
-      memcpy (p + 12, tests[i].compressed, tests[i].compressed_len);
-      uncompressed = NULL;
-      uncompressed_len = 0;
-      if (!backtrace_uncompress_zdebug (state, (unsigned char *) p,
-					tests[i].compressed_len + 12,
-					error_callback_compress, NULL,
-					&uncompressed, &uncompressed_len))
+      uncompressed_len = tests[i].uncompressed_len;
+      if (uncompressed_len == 0)
+	uncompressed_len = strlen (tests[i].uncompressed);
+
+      uncompressed = (unsigned char *) malloc (uncompressed_len);
+      if (uncompressed == NULL)
+	{
+	  perror ("malloc");
+	  fprintf (stderr, "test %s: uncompress failed\n", tests[i].name);
+	  ++failures;
+	  continue;
+	}
+
+      if (!backtrace_uncompress_zstd (state,
+				      ((const unsigned char *)
+				       tests[i].compressed),
+				      tests[i].compressed_len,
+				      error_callback_compress, NULL,
+				      uncompressed, uncompressed_len))
 	{
 	  fprintf (stderr, "test %s: uncompress failed\n", tests[i].name);
 	  ++failures;
 	}
       else
 	{
-	  if (uncompressed_len != v)
-	    {
-	      fprintf (stderr,
-		       "test %s: got uncompressed length %zu, want %zu\n",
-		       tests[i].name, uncompressed_len, v);
-	      ++failures;
-	    }
-	  else if (memcmp (tests[i].uncompressed, uncompressed, v) != 0)
+	  if (memcmp (tests[i].uncompressed, uncompressed, uncompressed_len)
+	      != 0)
 	    {
 	      size_t j;
 
 	      fprintf (stderr, "test %s: uncompressed data mismatch\n",
 		       tests[i].name);
-	      for (j = 0; j < v; ++j)
+	      for (j = 0; j < uncompressed_len; ++j)
 		if (tests[i].uncompressed[j] != uncompressed[j])
 		  fprintf (stderr, "  %zu: got %#x want %#x\n", j,
 			   uncompressed[j], tests[i].uncompressed[j]);
 	      ++failures;
 	    }
 	  else
-	    printf ("PASS: inflate %s\n", tests[i].name);
-
-	  backtrace_free (state, uncompressed, uncompressed_len,
-			  error_callback_compress, NULL);
+	    printf ("PASS: uncompress %s\n", tests[i].name);
 	}
+
+      free (uncompressed);
     }
 }
 
-#ifdef HAVE_ZLIB
+#ifdef HAVE_ZSTD
 
 /* Given a set of TRIALS timings, discard the lowest and highest
    values and return the mean average of the rest.  */
@@ -296,16 +292,15 @@ average_time (const size_t *times, size_t trials)
 static void
 test_large (struct backtrace_state *state ATTRIBUTE_UNUSED)
 {
-#ifdef HAVE_ZLIB
+#ifdef HAVE_ZSTD
   unsigned char *orig_buf;
   size_t orig_bufsize;
   size_t i;
   char *compressed_buf;
   size_t compressed_bufsize;
-  unsigned long compress_sizearg;
+  size_t compressed_size;
   unsigned char *uncompressed_buf;
-  size_t uncompressed_bufsize;
-  int r;
+  size_t r;
   clockid_t cid;
   struct timespec ts1;
   struct timespec ts2;
@@ -371,11 +366,11 @@ test_large (struct backtrace_state *state ATTRIBUTE_UNUSED)
   if (orig_buf == NULL)
     {
       /* We couldn't find an input file.  */
-      printf ("UNSUPPORTED: inflate large\n");
+      printf ("UNSUPPORTED: zstd large\n");
       return;
     }
 
-  compressed_bufsize = compressBound (orig_bufsize) + 12;
+  compressed_bufsize = ZSTD_compressBound (orig_bufsize);
   compressed_buf = malloc (compressed_bufsize);
   if (compressed_buf == NULL)
     {
@@ -383,21 +378,15 @@ test_large (struct backtrace_state *state ATTRIBUTE_UNUSED)
       goto fail;
     }
 
-  compress_sizearg = compressed_bufsize - 12;
-  r = compress ((unsigned char *) compressed_buf + 12, &compress_sizearg,
-		orig_buf, orig_bufsize);
-  if (r != Z_OK)
+  r = ZSTD_compress (compressed_buf, compressed_bufsize,
+		     orig_buf, orig_bufsize,
+		     ZSTD_CLEVEL_DEFAULT);
+  if (ZSTD_isError (r))
     {
-      fprintf (stderr, "zlib compress failed: %d\n", r);
+      fprintf (stderr, "zstd compress failed: %s\n", ZSTD_getErrorName (r));
       goto fail;
     }
-
-  compressed_bufsize = compress_sizearg + 12;
-
-  /* Prepare the header that our library expects.  */
-  memcpy (compressed_buf, "ZLIB", 4);
-  for (i = 0; i < 8; ++i)
-    compressed_buf[i + 4] = (orig_bufsize >> ((7 - i) * 8)) & 0xff;
+  compressed_size = r;
 
   uncompressed_buf = malloc (orig_bufsize);
   if (uncompressed_buf == NULL)
@@ -405,38 +394,33 @@ test_large (struct backtrace_state *state ATTRIBUTE_UNUSED)
       perror ("malloc");
       goto fail;
     }
-  uncompressed_bufsize = orig_bufsize;
 
-  if (!backtrace_uncompress_zdebug (state, (unsigned char *) compressed_buf,
-				    compressed_bufsize,
-				    error_callback_compress, NULL,
-				    &uncompressed_buf, &uncompressed_bufsize))
+  if (!backtrace_uncompress_zstd (state, (unsigned char *) compressed_buf,
+				  compressed_size,
+				  error_callback_compress, NULL,
+				  uncompressed_buf, orig_bufsize))
     {
-      fprintf (stderr, "inflate large: backtrace_uncompress_zdebug failed\n");
+      fprintf (stderr, "zstd large: backtrace_uncompress_zstd failed\n");
       goto fail;
     }
 
-  if (uncompressed_bufsize != orig_bufsize)
+  if (memcmp (uncompressed_buf, orig_buf, orig_bufsize) != 0)
     {
-      fprintf (stderr,
-	       "inflate large: got uncompressed length %zu, want %zu\n",
-	       uncompressed_bufsize, orig_bufsize);
+      size_t j;
+
+      fprintf (stderr, "zstd large: uncompressed data mismatch\n");
+      for (j = 0; j < orig_bufsize; ++j)
+	if (orig_buf[j] != uncompressed_buf[j])
+	  fprintf (stderr, "  %zu: got %#x want %#x\n", j,
+		   uncompressed_buf[j], orig_buf[j]);
       goto fail;
     }
 
-  if (memcmp (uncompressed_buf, orig_buf, uncompressed_bufsize) != 0)
-    {
-      fprintf (stderr, "inflate large: uncompressed data mismatch\n");
-      goto fail;
-    }
-
-  printf ("PASS: inflate large\n");
+  printf ("PASS: zstd large\n");
 
   for (i = 0; i < trials; ++i)
     {
-      unsigned long uncompress_sizearg;
-
-      cid = ZLIB_CLOCK_GETTIME_ARG;
+      cid = ZSTD_CLOCK_GETTIME_ARG;
       if (clock_gettime (cid, &ts1) < 0)
 	{
 	  if (errno == EINVAL)
@@ -445,16 +429,16 @@ test_large (struct backtrace_state *state ATTRIBUTE_UNUSED)
 	  return;
 	}
 
-      if (!backtrace_uncompress_zdebug (state,
-					(unsigned char *) compressed_buf,
-					compressed_bufsize,
-					error_callback_compress, NULL,
-					&uncompressed_buf,
-					&uncompressed_bufsize))
+      if (!backtrace_uncompress_zstd (state,
+				      (unsigned char *) compressed_buf,
+				      compressed_size,
+				      error_callback_compress, NULL,
+				      uncompressed_buf,
+				      orig_bufsize))
 	{
 	  fprintf (stderr,
-		   ("inflate large: "
-		    "benchmark backtrace_uncompress_zdebug failed\n"));
+		   ("zstd large: "
+		    "benchmark backtrace_uncompress_zstd failed\n"));
 	  return;
 	}
 
@@ -474,10 +458,8 @@ test_large (struct backtrace_state *state ATTRIBUTE_UNUSED)
 	  return;
 	}
 
-      uncompress_sizearg = uncompressed_bufsize;
-      r = uncompress ((unsigned char *) uncompressed_buf, &uncompress_sizearg,
-		      (unsigned char *) compressed_buf + 12,
-		      compressed_bufsize - 12);
+      r = ZSTD_decompress (uncompressed_buf, orig_bufsize,
+			   compressed_buf, compressed_size);
 
       if (clock_gettime (cid, &ts2) < 0)
 	{
@@ -485,11 +467,11 @@ test_large (struct backtrace_state *state ATTRIBUTE_UNUSED)
 	  return;
 	}
 
-      if (r != Z_OK)
+      if (ZSTD_isError (r))
 	{
 	  fprintf (stderr,
-		   "inflate large: benchmark zlib uncompress failed: %d\n",
-		   r);
+		   "zstd large: benchmark zlib uncompress failed: %s\n",
+		   ZSTD_getErrorName (r));
 	  return;
 	}
 
@@ -503,13 +485,13 @@ test_large (struct backtrace_state *state ATTRIBUTE_UNUSED)
   ztime = average_time (ztimes, trials);
 
   printf ("backtrace: %zu ns\n", ctime);
-  printf ("zlib     : %zu ns\n", ztime);
+  printf ("zstd     : %zu ns\n", ztime);
   printf ("ratio    : %g\n", (double) ztime / (double) ctime);
 
   return;
 
  fail:
-  printf ("FAIL: inflate large\n");
+  printf ("FAIL: zstd large\n");
   ++failures;
 
   if (orig_buf != NULL)
@@ -519,11 +501,11 @@ test_large (struct backtrace_state *state ATTRIBUTE_UNUSED)
   if (uncompressed_buf != NULL)
     free (uncompressed_buf);
 
-#else /* !HAVE_ZLIB */
+#else /* !HAVE_ZSTD */
 
- printf ("UNSUPPORTED: inflate large\n");
+ printf ("UNSUPPORTED: zstd large\n");
 
-#endif /* !HAVE_ZLIB */
+#endif /* !HAVE_ZSTD */
 }
 
 int
