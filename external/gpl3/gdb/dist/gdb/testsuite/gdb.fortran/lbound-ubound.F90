2a1c97808946e9207c01bcd7f0d669b8d9cc6ee9
christos <christos@NetBSD.org>
2024-08-12 21:38:29 +0000

    Import gdb-15.1, previous was 13.2
    ChangeLog:
    
    2024-03-20  Simon Marchi  <simon.marchi@efficios.com>
    
    	* .pre-commit-config.yaml: Bump black hook to 24.3.0
    
    2024-03-20  Simon Marchi  <simon.marchi@efficios.com>
    
    	* .pre-commit-config.yaml: New.
    
    2024-03-14  Simon Marchi  <simon.marchi@efficios.com>
    
    	* Makefile.def: Add configure-gdbserver and all-gdbserver
    	dependencies on all-libiconv.
    	* Makefile.in: Re-generate.
    
    2024-01-15  Nick Clifton  <nickc@redhat.com>
    
    	* 2.42 branch point.
    
    2023-11-15  Arsen Arsenović  <arsen@aarsen.me>
    
    	* intl: Remove directory.  Replaced with out-of-tree GNU
    	gettext.
    	* .gitignore: Add '/gettext*'.
    	* configure.ac (host_libs): Replace intl with gettext.
    	(hbaseargs, bbaseargs, baseargs): Split baseargs into
    	{h,b}baseargs.
    	(skip_barg): New flag.  Skips appending current flag to
    	bbaseargs.
    	<library exemptions>: Exempt --with-libintl-{type,prefix} from
    	target and build machine argument passing.
    	* configure: Regenerate.
    	* Makefile.def (host_modules): Replace intl module with gettext
    	module.
    	(configure-ld): Depend on configure-gettext.
    	* Makefile.in: Regenerate.
    	* src-release.sh: Remove references to the intl/ directory.
    
    2023-07-03  Nick Clifton  <nickc@redhat.com>
    
    	2.41 Branch Point.
    
    2023-06-26  Nick Clifton  <nickc@redhat.com>
    
    	* Import these updates to the config scripts
    
    	commit 4ad4bb7c30aca1e705448ba8d51a210bbd47bb52
    	Author: Paul Eggert <eggert@cs.ucla.edu>
    	Date:   Fri Jun 23 09:55:10 2023 -0700
    
    		Quote 'like this', not `like this'.
    
    	commit 63acb96f92473ceb5e21d873d7c0aee266b3d6d3
    	Author: Paul Eggert <eggert@cs.ucla.edu>
    	Date:   Sat Jan 21 00:15:01 2023 -0600
    
    		Fix config.sub spelling typo for "athlon"
    
    	commit 4ce12a5c9125cedc0d0ba584444a6865396923ec
    	Author: Dmitry V. Levin <ldv@altlinux.org>
    	Date:   Sun Jan 1 08:00:00 2023 +0000
    
    		Update copyright years
    
    	commit c397e2c040bce50bcdccb131f90115ba7e8bfc19
    	Author: Arsen Arsenovi <arsen@aarsen.me>
    	Date:   Sat Sep 17 23:34:48 2022 +0200
    
    		config.sub: add linux-mlibc targets
    
    	commit 9f9f9b0b13197269848c76e3e057a3ed0680b4bf
    	Author: Arsen Arsenovi <arsen@aarsen.me>
    	Date:   Sat Sep 17 23:34:47 2022 +0200
    
    		config.guess: support running on Managarm systems
    
    	commit 87e6687749da7bb2ab158a79fa83721c19ed9246
    	Author: Arsen Arsenovi <arsen@aarsen.me>
    	Date:   Sat Sep 17 23:34:46 2022 +0200
    
    		config.sub: add managarm-{mlibc,kernel} targets
    
    	commit 20403c5701973a4cbd7e0b4bbeb627fcd424a0f1
    	Author: Xiaotian Wu <wuxiaotian@loongson.cn>
    	Date:   Mon Aug 1 16:05:29 2022 +0800
    
    		Remove loongarchx32
    
    	commit 02ba26b218d3d3db6c56e014655faf463cefa983
    	Author: Alexander von Gluck IV <kallisti5@unixzen.com>
    	Date:   Wed May 25 15:43:13 2022 -0500
    
    		config.guess: Update Haiku guesses
    
    	commit f56a7140386d08a531bcfd444d632b28c61a6329
    	Author: Bruno Haible <bruno@clisp.org>
    	Date:   Sun May 8 19:08:08 2022 +0200
    
    		config.guess (x86_64:Linux:*:*): Detect 32-bit ABI.
    
    2023-04-20  Nick Clifton  <nickc@redhat.com>
    
    	* SECURITY.txt: New file.
    	* src-release.sh (DEVO_SUPPORT): Add SECURITY.txt.
    
    2022-12-31  Nick Clifton  <nickc@redhat.com>
    
    	* 2.40 binutils branch created.

--- external/gpl3/gdb/dist/gdb/testsuite/gdb.fortran/lbound-ubound.F90
+++ external/gpl3/gdb/dist/gdb/testsuite/gdb.fortran/lbound-ubound.F90
@@ -1,4 +1,4 @@
-! Copyright 2021-2023 Free Software Foundation, Inc.
+! Copyright 2021-2024 Free Software Foundation, Inc.
 !
 ! This program is free software; you can redistribute it and/or modify
 ! it under the terms of the GNU General Public License as published by
@@ -19,28 +19,6 @@
 subroutine do_test (lb, ub)
   integer*4, dimension (:) :: lb
   integer*4, dimension (:) :: ub
-
-  print *, ""
-  print *, "Expected GDB Output:"
-  print *, ""
-
-  write(*, fmt="(A)", advance="no") "LBOUND = ("
-  do i=LBOUND (lb, 1), UBOUND (lb, 1), 1
-     if (i > LBOUND (lb, 1)) then
-        write(*, fmt="(A)", advance="no") ", "
-     end if
-     write(*, fmt="(I0)", advance="no") lb (i)
-  end do
-  write(*, fmt="(A)", advance="yes") ")"
-
-  write(*, fmt="(A)", advance="no") "UBOUND = ("
-  do i=LBOUND (ub, 1), UBOUND (ub, 1), 1
-     if (i > LBOUND (ub, 1)) then
-        write(*, fmt="(A)", advance="no") ", "
-     end if
-     write(*, fmt="(I0)", advance="no") ub (i)
-  end do
-  write(*, fmt="(A)", advance="yes") ")"
 
   print *, ""	! Test Breakpoint
 end subroutine do_test
--- external/gpl3/gdb/dist/gdb/testsuite/gdb.fortran/library-module-lib.f90
+++ external/gpl3/gdb/dist/gdb/testsuite/gdb.fortran/library-module-lib.f90
@@ -1,4 +1,4 @@
-! Copyright 2010-2023 Free Software Foundation, Inc.
+! Copyright 2010-2024 Free Software Foundation, Inc.
 ! 
 ! This program is free software; you can redistribute it and/or modify
 ! it under the terms of the GNU General Public License as published by
@@ -19,7 +19,7 @@ contains
         subroutine lib_func
         if (var_i .ne. 1) call abort
         var_i = 2
-        var_i = var_i                 ! i-is-2-in-lib
+        var_i = 2                 ! i-is-2-in-lib
         end subroutine lib_func
 end module lib
 
