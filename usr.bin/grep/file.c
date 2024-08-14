afd3b88943aea09d125b3b2f7d932f7bfab3f425
rin <rin@NetBSD.org>
2024-08-14 04:59:51 +0000

    grep: grep_refill: Fix -Wunused-variable for WITHOUT_GZIP
    NFC otherwise.

--- usr.bin/grep/file.c
+++ usr.bin/grep/file.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: file.c,v 1.11 2019/04/05 13:34:41 christos Exp $	*/
+/*	$NetBSD: file.c,v 1.12 2024/08/14 04:59:51 rin Exp $	*/
 /*	$FreeBSD: head/usr.bin/grep/file.c 211496 2010-08-19 09:28:59Z des $	*/
 /*	$OpenBSD: file.c,v 1.11 2010/07/02 20:48:48 nicm Exp $	*/
 
@@ -35,7 +35,7 @@
 #endif
 
 #include <sys/cdefs.h>
-__RCSID("$NetBSD: file.c,v 1.11 2019/04/05 13:34:41 christos Exp $");
+__RCSID("$NetBSD: file.c,v 1.12 2024/08/14 04:59:51 rin Exp $");
 
 #include <sys/param.h>
 #include <sys/types.h>
@@ -74,7 +74,9 @@ static inline int
 grep_refill(struct file *f)
 {
 	ssize_t nr = -1;
+#ifndef WITHOUT_GZIP
 	int bzerr;
+#endif
 
 	bufpos = buffer;
 	bufrem = 0;
