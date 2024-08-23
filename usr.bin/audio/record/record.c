b0379f770f6ec492f3461c8042100db4e7297a4e
martin <martin@NetBSD.org>
2024-08-23 17:16:27 +0000

    Pull up following revision(s) (requested by mrg in ticket #1874):
    	usr.bin/audio/common/wav.c: revision 1.23
    	usr.bin/audio/record/record.c: revision 1.59
    
    fix some sizeof() confusion.
    
    using "const char search[4]" as a function parameter means that
    "search" is actually a pointer type so "sizeof search" returns
    8 on 64-bit platforms.  i mis-read this and used "sizeof *search"
    which is always 1, noted by rillig.
    
    instead of trying to avoid writing "4" twice, put it in a define
    and use that in various places instead.  annoying.
    match message & conversion function name properly.
    
    probably should make this into an array with two members.

--- usr.bin/audio/record/record.c
+++ usr.bin/audio/record/record.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: record.c,v 1.54.18.1 2024/03/12 12:41:39 martin Exp $	*/
+/*	$NetBSD: record.c,v 1.54.18.2 2024/08/23 17:16:27 martin Exp $	*/
 
 /*
  * Copyright (c) 1999, 2002, 2003, 2005, 2010 Matthew R. Green
@@ -32,7 +32,7 @@
 #include <sys/cdefs.h>
 
 #ifndef lint
-__RCSID("$NetBSD: record.c,v 1.54.18.1 2024/03/12 12:41:39 martin Exp $");
+__RCSID("$NetBSD: record.c,v 1.54.18.2 2024/08/23 17:16:27 martin Exp $");
 #endif
 
 
@@ -322,9 +322,9 @@ main(int argc, char *argv[])
 			s = "change sign (little-endian, 32 bit)";
 		else if (conv_func == change_sign16_swap_bytes_be)
 			s = "change sign & swap bytes (big-endian, 16 bit)";
-		else if (conv_func == change_sign24_swap_bytes_le)
+		else if (conv_func == change_sign16_swap_bytes_le)
 			s = "change sign & swap bytes (little-endian, 16 bit)";
-		else if (conv_func == change_sign16_swap_bytes_be)
+		else if (conv_func == change_sign24_swap_bytes_be)
 			s = "change sign & swap bytes (big-endian, 24 bit)";
 		else if (conv_func == change_sign24_swap_bytes_le)
 			s = "change sign & swap bytes (little-endian, 24 bit)";
