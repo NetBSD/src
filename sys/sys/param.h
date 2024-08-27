4422e45706b46901919522c11ad12d5dacac6df5
riastradh <riastradh@NetBSD.org>
2024-08-27 00:57:35 +0000

    sys/param.h: Welcome to 10.99.12!
    Courtesy of entropy_consolidate's new return value, telling the
    caller whether it was interrupted by a signal or not.

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.734 2024/06/29 13:04:20 riastradh Exp $	*/
+/*	$NetBSD: param.h,v 1.735 2024/08/27 00:57:35 riastradh Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -67,7 +67,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	1099001100	/* NetBSD 10.99.11 */
+#define	__NetBSD_Version__	1099001200	/* NetBSD 10.99.12 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
