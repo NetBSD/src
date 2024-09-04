2977ca9681bed2ac01afc980ef5e16228880c5c2
rin <rin@NetBSD.org>
2024-09-04 03:33:29 +0000

    t_siginfo:adraln: Brush up skip message for x86
    Note that:
    
    1. No data address is reported back for alignment check exception, as per
       Intel SDM (June 2024).
    
    2. Kernel sets faulting PC to si_addr.
    
    3. 2. is compliant to siginfo(2), at least:
    
    > For SIGILL, SIGFPE, SIGBUS and SIGSEGV ... si_addr contains
    > the address of the faulting instruction or data and ...
    
    4. IEEE Std 1003.1-2024 does not require si_addr for SIGBUS. For SIGILL
       and SIGSEGV, e.g., it is required (SHALL) to store faulting PC and
       data address, respectively, on the other hand.

--- tests/lib/libc/gen/t_siginfo.c
+++ tests/lib/libc/gen/t_siginfo.c
@@ -1,4 +1,4 @@
-/* $NetBSD: t_siginfo.c,v 1.54 2024/09/04 02:20:49 rin Exp $ */
+/* $NetBSD: t_siginfo.c,v 1.55 2024/09/04 03:33:29 rin Exp $ */
 
 /*-
  * Copyright (c) 2010 The NetBSD Foundation, Inc.
@@ -442,8 +442,9 @@ sigbus_action(int signo, siginfo_t *info, void *ptr)
 	ATF_REQUIRE_EQ(info->si_code, BUS_ADRALN);
 
 #if defined(__i386__) || defined(__x86_64__)
-	atf_tc_skip("x86 architecture does not correctly "
-	    "report the address where the unaligned access occurred");
+	atf_tc_skip("Data address is not provided for "
+	    "x86 alignment check exception, and NetBSD/x86 reports "
+	    "faulting PC instead");
 #endif
 	ATF_REQUIRE_EQ(info->si_addr, (volatile void *)addr);
 
