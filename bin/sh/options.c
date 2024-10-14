2ecc8e9937ae0a756dca31f316ed26a4bc28d0fe
kre <kre@NetBSD.org>
2024-10-14 08:27:53 +0000

    Add a -r (version) option to sh
    This new -r (or +r) option is for command line use only.
    When encountered, the shell simply prints its version info
    (such as it has, which isn't much) and exits (immediately).
    
    This allows "funny" uses like
    	sh -version
    the -v and -e options are standard, and processed as normal
    (changing nothing, yet, except setting the option).  Then the
    'r' option is seen, the version info is printed, and the shell
    exits.  Any remaining "options" (there is no "-o n" option) are
    ignored, as are any other args given to the shell.
    
    The string printed (currently) is just "NetBSD shell:" followed
    by the value of the NETBSD_SHELL variable (which has been established
    already by the time options are processed).

--- bin/sh/options.c
+++ bin/sh/options.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: options.c,v 1.60 2024/09/21 20:48:50 kre Exp $	*/
+/*	$NetBSD: options.c,v 1.61 2024/10/14 08:27:53 kre Exp $	*/
 
 /*-
  * Copyright (c) 1991, 1993
@@ -37,7 +37,7 @@
 #if 0
 static char sccsid[] = "@(#)options.c	8.2 (Berkeley) 5/4/95";
 #else
-__RCSID("$NetBSD: options.c,v 1.60 2024/09/21 20:48:50 kre Exp $");
+__RCSID("$NetBSD: options.c,v 1.61 2024/10/14 08:27:53 kre Exp $");
 #endif
 #endif /* not lint */
 
@@ -244,6 +244,9 @@ options(int cmdline)
 				else
 					set_debug("*$", val);
 #endif
+			} else if (cmdline && c == 'r') {
+				out1fmt("NetBSD shell: %s\n", lookupvar("NETBSD_SHELL"));
+				sh_exit(0);
 			} else {
 				setoption(c, val);
 			}
