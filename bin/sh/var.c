470d6e5e934dd64941c5ca4a159fc018b25193ec
kre <kre@NetBSD.org>
2024-10-14 08:15:43 +0000

    Apologies for that commit message ... it should have been the same as for revision 1.124 of Makefile (and as below).
    (This change changes nothing).
    
    Reject nul characters in shell input.
    
    At the request of Thomas Klausner (wiz@) copy an idea from OpenBSD,
    and have the shell simply reject any (sh) input containing a \0 (nul)
    character.   Previously nul characters were simply ignored (removed
    from the input before it was examined in any other way).
    
    Note this affects data read by the shell itself only, and has no
    impact on other utilities, including those that are built into
    the shell (so 'read' can still use -d '' to process output using
    \0 as the record separator).
    
    While I have tested that this works, there are so many places
    where a nul might appear, that I cannot possibly test them all
    (or even imagine all the possible places), so if this change
    causes any problems, let me know (if from a script, send me the
    script).
    
    To undo this change, simply comment out (or delete) the line
    	CPPFLAGS+= -DREJECT_NULS
    from the Makefile, and build again.   Eventually if this causes
    no problems, that option (conditional compilation) will probably
    just go away, and this change would be permanent.
    
    While the conditional compilation (on or off) persists, the
    NETBSD_SHELL variable value will contain the word REJECT_NULLS
    so you can easily verify if you are running a shell with this
    included or not.
    
    No pullups planned.

--- bin/sh/var.c
+++ bin/sh/var.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: var.c,v 1.85 2024/10/14 08:11:57 kre Exp $	*/
+/*	$NetBSD: var.c,v 1.86 2024/10/14 08:15:43 kre Exp $	*/
 
 /*-
  * Copyright (c) 1991, 1993
@@ -37,7 +37,7 @@
 #if 0
 static char sccsid[] = "@(#)var.c	8.3 (Berkeley) 5/4/95";
 #else
-__RCSID("$NetBSD: var.c,v 1.85 2024/10/14 08:11:57 kre Exp $");
+__RCSID("$NetBSD: var.c,v 1.86 2024/10/14 08:15:43 kre Exp $");
 #endif
 #endif /* not lint */
 
