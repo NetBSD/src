26bb3c5a667cfd9bfae466baaf840171e1b0191c
rin <rin@NetBSD.org>
2024-11-07 09:49:49 +0000

    vchiq: Adjust newlines in arguments of sysctl_createv(9)
    No binary changes.

--- sys/external/bsd/vchiq/dist/interface/vchiq_arm/vchiq_kmod_netbsd.c
+++ sys/external/bsd/vchiq/dist/interface/vchiq_arm/vchiq_kmod_netbsd.c
@@ -1,4 +1,4 @@
-/* $NetBSD: vchiq_kmod_netbsd.c,v 1.12 2020/09/26 12:58:23 skrll Exp $ */
+/* $NetBSD: vchiq_kmod_netbsd.c,v 1.13 2024/11/07 09:49:49 rin Exp $ */
 
 /*-
  * Copyright (c) 2013 The NetBSD Foundation, Inc.
@@ -30,7 +30,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: vchiq_kmod_netbsd.c,v 1.12 2020/09/26 12:58:23 skrll Exp $");
+__KERNEL_RCSID(0, "$NetBSD: vchiq_kmod_netbsd.c,v 1.13 2024/11/07 09:49:49 rin Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -114,25 +114,27 @@ SYSCTL_SETUP(sysctl_hw_vchiq_setup, "sysctl hw.vchiq setup")
 	const struct sysctlnode *cnode = NULL;
 
 	sysctl_createv(clog, 0, NULL, &rnode,
-	    CTLFLAG_PERMANENT, CTLTYPE_NODE, "vchiq", NULL,
-	    NULL, 0, NULL, 0, CTL_HW, CTL_CREATE, CTL_EOL);
+	    CTLFLAG_PERMANENT, CTLTYPE_NODE, "vchiq",
+	    NULL, NULL, 0,
+	    NULL, 0, CTL_HW, CTL_CREATE, CTL_EOL);
 
 	sysctl_createv(clog, 0, &rnode, &cnode,
-	    CTLFLAG_PERMANENT, CTLTYPE_NODE, "loglevel", NULL,
-	    NULL, 0, NULL, 0, CTL_CREATE, CTL_EOL);
+	    CTLFLAG_PERMANENT, CTLTYPE_NODE, "loglevel",
+	    NULL, NULL, 0,
+	    NULL, 0, CTL_CREATE, CTL_EOL);
 
 	sysctl_createv(clog, 0, &cnode, NULL,
-	    CTLFLAG_PERMANENT|CTLFLAG_READWRITE,
-	    CTLTYPE_INT, "core", "VChiq Core Loglevel", NULL, 0,
+	    CTLFLAG_PERMANENT|CTLFLAG_READWRITE, CTLTYPE_INT, "core",
+	    "VChiq Core Loglevel", NULL, 0,
 	    &vchiq_core_log_level, 0, CTL_CREATE, CTL_EOL);
 
 	sysctl_createv(clog, 0, &cnode, NULL,
-	    CTLFLAG_PERMANENT|CTLFLAG_READWRITE,
-	    CTLTYPE_INT, "coremsg", "VChiq Core Message Loglevel", NULL, 0,
+	    CTLFLAG_PERMANENT|CTLFLAG_READWRITE, CTLTYPE_INT, "coremsg",
+	    "VChiq Core Message Loglevel", NULL, 0,
 	    &vchiq_core_msg_log_level, 0, CTL_CREATE, CTL_EOL);
 
 	sysctl_createv(clog, 0, &cnode, NULL,
-	    CTLFLAG_PERMANENT|CTLFLAG_READWRITE,
-	    CTLTYPE_INT, "sync", "VChiq Sync Loglevel", NULL, 0,
+	    CTLFLAG_PERMANENT|CTLFLAG_READWRITE, CTLTYPE_INT, "sync",
+	    "VChiq Sync Loglevel", NULL, 0,
 	    &vchiq_sync_log_level, 0, CTL_CREATE, CTL_EOL);
 }
