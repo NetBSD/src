f88325654bcd07043f4b90b5dd7b640a7d53fec0
rillig <rillig@NetBSD.org>
2024-12-15 07:43:53 +0000

    tests/lint: test negative constant and unsigned type for all operators

--- tests/usr.bin/xlint/lint1/msg_222.c
+++ tests/usr.bin/xlint/lint1/msg_222.c
@@ -1,43 +1,105 @@
-/*	$NetBSD: msg_222.c,v 1.7 2024/12/15 05:56:18 rillig Exp $	*/
+/*	$NetBSD: msg_222.c,v 1.8 2024/12/15 07:43:53 rillig Exp $	*/
 # 3 "msg_222.c"
 
 // Test for message: conversion of negative constant %lld to unsigned type '%s' [222]
+//
+// See also:
+//	msg_162.c: comparison of unsigned type with negative constant
+//	msg_164.c: assignment of negative constant to unsigned type
+//	msg_221.c: initialization of unsigned type with negative constant
+//	msg_296.c: conversion of negative constant to unsigned type in call
 
 /* lint1-extra-flags: -X 351 */
 
-/* expect+1: warning: initialization of unsigned type 'unsigned int' with negative constant -1 [221] */
-unsigned int global = -1;
+unsigned int u32;
+signed char sc;
+unsigned char uc;
+_Bool b;
 
-void take_unsigned_int(unsigned int);
 
 void
-function(signed char *scp, unsigned char *ucp)
+convert_negative_constant(void)
 {
-	/* expect+1: warning: initialization of unsigned type 'unsigned int' with negative constant -1 [221] */
-	unsigned int local = -1;
+	u32 = !-8;
+	u32 = ~-8;
+	/* expect+1: warning: assignment of negative constant -8 to unsigned type 'unsigned int' [164] */
+	u32 = +-8;
+	u32 = - -8;
 
-	/* expect+1: warning: conversion of negative constant -1 to unsigned type 'unsigned int', arg #1 [296] */
-	take_unsigned_int(-1);
-
-	if (local & -1)
-		return;
+	/* expect+1: warning: conversion of negative constant -8 to unsigned type 'unsigned int' [222] */
+	u32 = u32 * -8;
+	/* expect+1: warning: conversion of negative constant -8 to unsigned type 'unsigned int' [222] */
+	u32 = -8 * u32;
+	/* expect+1: warning: conversion of negative constant -8 to unsigned type 'unsigned int' [222] */
+	u32 = u32 / -8;
+	/* expect+1: warning: conversion of negative constant -8 to unsigned type 'unsigned int' [222] */
+	u32 = -8 / u32;
+	/* expect+1: warning: conversion of negative constant -8 to unsigned type 'unsigned int' [222] */
+	u32 = u32 % -8;
+	/* expect+1: warning: conversion of negative constant -8 to unsigned type 'unsigned int' [222] */
+	u32 = -8 / u32;
+	/* expect+1: warning: conversion of negative constant -8 to unsigned type 'unsigned int' [222] */
+	u32 = u32 + -8;
+	/* expect+1: warning: conversion of negative constant -8 to unsigned type 'unsigned int' [222] */
+	u32 = -8 + u32;
+	/* expect+1: warning: conversion of negative constant -8 to unsigned type 'unsigned int' [222] */
+	u32 = u32 - -8;
+	/* expect+1: warning: conversion of negative constant -8 to unsigned type 'unsigned int' [222] */
+	u32 = -8 - u32;
+	/* expect+1: warning: negative shift [121] */
+	u32 = u32 << -8;
+	u32 = -8 << u32;
+	/* expect+1: warning: negative shift [121] */
+	u32 = u32 >> -8;
+	u32 = -8 >> u32;
 
 	/* expect+1: warning: operator '<' compares 'unsigned int' with 'negative constant' [162] */
-	if (local < -1)
-		return;
+	b = u32 < -8;
+	/* expect+1: warning: operator '<=' compares 'unsigned int' with 'negative constant' [162] */
+	b = u32 <= -8;
+	/* expect+1: warning: operator '>' compares 'unsigned int' with 'negative constant' [162] */
+	b = u32 > -8;
+	/* expect+1: warning: operator '>=' compares 'unsigned int' with 'negative constant' [162] */
+	b = u32 >= -8;
+	/* expect+1: warning: operator '==' compares 'unsigned int' with 'negative constant' [162] */
+	b = u32 == -8;
+	/* expect+1: warning: operator '!=' compares 'unsigned int' with 'negative constant' [162] */
+	b = u32 != -8;
 
-	local &= -1;
+	u32 = u32 & -8;
+	u32 = u32 ^ -8;
+	u32 = u32 | -8;
+	b = u32 && -8;
+	b = u32 || -8;
 
-	/* expect+1: warning: conversion of negative constant -1 to unsigned type 'unsigned int' [222] */
-	local += -1;
+	/* expect+1: warning: assignment of negative constant -8 to unsigned type 'unsigned int' [164] */
+	u32 = -8;
+	/* expect+1: warning: conversion of negative constant -8 to unsigned type 'unsigned int' [222] */
+	u32 *= -8;
+	/* expect+1: warning: conversion of negative constant -8 to unsigned type 'unsigned int' [222] */
+	u32 /= -8;
+	/* expect+1: warning: conversion of negative constant -8 to unsigned type 'unsigned int' [222] */
+	u32 %= -8;
+	/* expect+1: warning: conversion of negative constant -8 to unsigned type 'unsigned int' [222] */
+	u32 += -8;
+	/* expect+1: warning: conversion of negative constant -8 to unsigned type 'unsigned int' [222] */
+	u32 -= -8;
+	// XXX: missing 'negative shift' warning
+	u32 <<= -8;
+	// XXX: missing 'negative shift' warning
+	u32 >>= -8;
+	u32 &= -8;
+	/* expect+1: warning: conversion of negative constant -8 to unsigned type 'unsigned int' [222] */
+	u32 ^= -8;
+	u32 |= -8;
 
-	*scp += 'A' - 'a';
-	*scp -= 'A' - 'a';
+	sc += 'A' - 'a';
+	sc -= 'A' - 'a';
 
 	// XXX: It's perfectly fine to effectively subtract a constant from
 	// XXX: an unsigned type.
 	/* expect+1: warning: conversion of negative constant -32 to unsigned type 'unsigned char' [222] */
-	*ucp += 'A' - 'a';
+	uc += 'A' - 'a';
 	/* expect+1: warning: conversion of negative constant -32 to unsigned type 'unsigned char' [222] */
-	*ucp -= 'A' - 'a';
+	uc -= 'A' - 'a';
 }
