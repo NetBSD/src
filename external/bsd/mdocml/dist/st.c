e8c4543c7511a29f77787ec52db6b4a471e808b8
wiz <wiz@NetBSD.org>
2025-01-11 11:47:17 +0000

    Remove .St -isoC-2018 support.
    Not accepted by upstream (seen as a corrigendum, not a proper new
    version), and no users in the source tree.

--- external/bsd/mdocml/dist/st.c
+++ external/bsd/mdocml/dist/st.c
@@ -54,7 +54,6 @@ LINE("-isoC-tcor1",	"ISO/IEC 9899/TCOR1:1994 (\\(lqISO\\~C90, Technical Corrigen
 LINE("-isoC-tcor2",	"ISO/IEC 9899/TCOR2:1995 (\\(lqISO\\~C90, Technical Corrigendum 2\\(rq)")
 LINE("-isoC-99",	"ISO/IEC 9899:1999 (\\(lqISO\\~C99\\(rq)")
 LINE("-isoC-2011",	"ISO/IEC 9899:2011 (\\(lqISO\\~C11\\(rq)")
-LINE("-isoC-2018",	"ISO/IEC 9899:2018 (\\(lqISO\\~C18\\(rq)")
 LINE("-isoC-2023",	"ISO/IEC 9899:2024 (\\(lqISO\\~C23\\(rq)")
 LINE("-iso9945-1-90",	"ISO/IEC 9945-1:1990 (\\(lqPOSIX.1\\(rq)")
 LINE("-iso9945-1-96",	"ISO/IEC 9945-1:1996 (\\(lqPOSIX.1\\(rq)")
