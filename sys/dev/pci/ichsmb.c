d48e8040b465f5f321070ac0c76eb57a0f3bacc4
mrg <mrg@NetBSD.org>
2025-01-18 08:12:13 +0000

    match meteor lake's smbus.

--- sys/dev/pci/ichsmb.c
+++ sys/dev/pci/ichsmb.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: ichsmb.c,v 1.85 2023/08/07 06:24:35 msaitoh Exp $	*/
+/*	$NetBSD: ichsmb.c,v 1.86 2025/01/18 08:12:13 mrg Exp $	*/
 /*	$OpenBSD: ichiic.c,v 1.44 2020/10/07 11:23:05 jsg Exp $	*/
 
 /*
@@ -22,7 +22,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: ichsmb.c,v 1.85 2023/08/07 06:24:35 msaitoh Exp $");
+__KERNEL_RCSID(0, "$NetBSD: ichsmb.c,v 1.86 2025/01/18 08:12:13 mrg Exp $");
 
 #include <sys/param.h>
 #include <sys/device.h>
@@ -175,6 +175,7 @@ ichsmb_match(device_t parent, cfdata_t match, void *aux)
 		case PCI_PRODUCT_INTEL_6HS_H_SMB:
 		case PCI_PRODUCT_INTEL_6HS_LP_SMB:
 		case PCI_PRODUCT_INTEL_7HS_SMB:
+		case PCI_PRODUCT_INTEL_SMBUS_MTL:
 			return 1;
 		}
 	}
