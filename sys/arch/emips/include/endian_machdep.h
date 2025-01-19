84ccf14da58b29475215ddaf17018715a75c623f
skrll <skrll@NetBSD.org>
2025-01-19 10:09:23 +0000

    Delete empty lines and put the RSCId on the first line

--- sys/arch/emips/include/endian_machdep.h
+++ sys/arch/emips/include/endian_machdep.h
@@ -1,6 +1,4 @@
-
-
-/*	$NetBSD: endian_machdep.h,v 1.1 2011/01/26 01:18:51 pooka Exp $	*/
+/*	$NetBSD: endian_machdep.h,v 1.2 2025/01/19 10:09:23 skrll Exp $	*/
 
 #define _BYTE_ORDER _BIG_ENDIAN
 #include <mips/endian_machdep.h>
