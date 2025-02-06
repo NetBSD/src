672a38c97ead20c786e26ff59866e38bd2c41859
sborrill <sborrill@NetBSD.org>
2025-02-06 13:32:42 +0000

    cdroms as xbd devices are skipped in PVHVM mode to avoid hangs if they are not ready, i.e. do not contain an ISO image (that they hang is a bug that should be fixed in itself). They are instead detected as an emulated cdX which does deal with not being ready.
    This leads to a regression in pure PV mode where xbd devices are hot-plugged
    when an ISO is mounted and there are no emulated block devices such as cdX.
    The xbd devices never appear and thus ISO images are not accessible.
    
    Revert to pre-10 behaviour on pure PV kernels.

--- sys/arch/xen/xenbus/xenbus_probe.c
+++ sys/arch/xen/xenbus/xenbus_probe.c
@@ -1,4 +1,4 @@
-/* $NetBSD: xenbus_probe.c,v 1.60 2023/10/17 11:52:45 bouyer Exp $ */
+/* $NetBSD: xenbus_probe.c,v 1.61 2025/02/06 13:32:42 sborrill Exp $ */
 /******************************************************************************
  * Talks to Xen Store to figure out what devices we have.
  *
@@ -29,7 +29,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: xenbus_probe.c,v 1.60 2023/10/17 11:52:45 bouyer Exp $");
+__KERNEL_RCSID(0, "$NetBSD: xenbus_probe.c,v 1.61 2025/02/06 13:32:42 sborrill Exp $");
 
 #if 0
 #define DPRINTK(fmt, args...) \
@@ -452,6 +452,7 @@ xenbus_probe_device_type(const char *path, const char *type,
 					kmem_free(xbusd, xbusd->xbusd_sz);
 					break;
 				}
+#ifndef XENPV
 				if (strcmp(dtype, "cdrom") == 0) {
 					aprint_verbose_dev(xenbus_dev,
 					    "ignoring %s type cdrom\n",
@@ -459,6 +460,7 @@ xenbus_probe_device_type(const char *path, const char *type,
 					kmem_free(xbusd, xbusd->xbusd_sz);
 					continue;
 				}
+#endif
 			}
 			err = read_backend_details(xbusd);
 			if (err != 0) {
