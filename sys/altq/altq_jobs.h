a66d868c77b728b6ff727e912b74203eae2d4b9a
joe <joe@NetBSD.org>
2025-02-10 19:12:49 +0000

    whitespace: clear trailing whitespaces

--- sys/altq/altq_jobs.h
+++ sys/altq/altq_jobs.h
@@ -1,58 +1,58 @@
-/*	$NetBSD: altq_jobs.h,v 1.5 2010/04/09 19:32:45 plunky Exp $	*/
+/*	$NetBSD: altq_jobs.h,v 1.6 2025/02/10 19:12:49 joe Exp $	*/
 /*	$KAME: altq_jobs.h,v 1.6 2003/07/10 12:07:48 kjc Exp $	*/
 /*
- * Copyright (c) 2001, Rector and Visitors of the University of 
+ * Copyright (c) 2001, Rector and Visitors of the University of
  * Virginia.
  * All rights reserved.
  *
- * Redistribution and use in source and binary forms, 
- * with or without modification, are permitted provided 
+ * Redistribution and use in source and binary forms,
+ * with or without modification, are permitted provided
  * that the following conditions are met:
  *
- * Redistributions of source code must retain the above 
- * copyright notice, this list of conditions and the following 
- * disclaimer. 
+ * Redistributions of source code must retain the above
+ * copyright notice, this list of conditions and the following
+ * disclaimer.
  *
- * Redistributions in binary form must reproduce the above 
- * copyright notice, this list of conditions and the following 
- * disclaimer in the documentation and/or other materials provided 
- * with the distribution. 
+ * Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following
+ * disclaimer in the documentation and/or other materials provided
+ * with the distribution.
  *
- * Neither the name of the University of Virginia nor the names 
- * of its contributors may be used to endorse or promote products 
- * derived from this software without specific prior written 
- * permission. 
+ * Neither the name of the University of Virginia nor the names
+ * of its contributors may be used to endorse or promote products
+ * derived from this software without specific prior written
+ * permission.
  *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND 
- * CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, 
- * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF 
- * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
- * DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE 
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, 
- * OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, 
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, 
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND 
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT 
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING 
- * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ * CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+ * OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+ * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
  * THE POSSIBILITY OF SUCH DAMAGE.
  */
-/*                                                                     
- * JoBS - altq prototype implementation                                
- *                                                                     
+/*
+ * JoBS - altq prototype implementation
+ *
  * Author: Nicolas Christin <nicolas@cs.virginia.edu>
  *
- * JoBS algorithms originally devised and proposed by		       
+ * JoBS algorithms originally devised and proposed by
  * Nicolas Christin and Jorg Liebeherr.
- * Grateful Acknowledgments to Tarek Abdelzaher for his help and       
+ * Grateful Acknowledgments to Tarek Abdelzaher for his help and
  * comments, and to Kenjiro Cho for some helpful advice.
  * Contributed by the Multimedia Networks Group at the University
- * of Virginia. 
+ * of Virginia.
  *
- * Papers and additional info can be found at 
+ * Papers and additional info can be found at
  * http://qosbox.cs.virginia.edu
- *                                                                      
- */ 							               
+ *
+ */
 
 #ifndef _ALTQ_ALTQ_JOBS_H_
 #define	_ALTQ_ALTQ_JOBS_H_
@@ -73,7 +73,7 @@ extern "C" {
 
 /* list of packet arrival times */
 struct _tsentry;
-typedef TAILQ_HEAD(_timestamps, _tsentry) TSLIST;  
+typedef TAILQ_HEAD(_timestamps, _tsentry) TSLIST;
 typedef struct _tsentry {
 	TAILQ_ENTRY(_tsentry) ts_list;
 	uint64_t	timestamp;
