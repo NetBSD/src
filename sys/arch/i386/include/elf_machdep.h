c16fa967cda43238e3424b51540268adf0d989ed
jkoshy <jkoshy@NetBSD.org>
2025-02-11 12:27:58 +0000

    Add an alternate spelling for an i386 relocation type.

--- sys/arch/i386/include/elf_machdep.h
+++ sys/arch/i386/include/elf_machdep.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: elf_machdep.h,v 1.13 2017/11/06 03:47:46 christos Exp $	*/
+/*	$NetBSD: elf_machdep.h,v 1.14 2025/02/11 12:27:58 jkoshy Exp $	*/
 
 #define	ELF32_MACHDEP_ENDIANNESS	ELFDATA2LSB
 #define	ELF32_MACHDEP_ID_CASES						\
@@ -24,6 +24,7 @@
 #define	R_386_COPY	5
 #define	R_386_GLOB_DAT	6
 #define	R_386_JMP_SLOT	7
+#define	R_386_JUMP_SLOT	7 /* psABI spelling. */
 #define	R_386_RELATIVE	8
 #define	R_386_GOTOFF	9
 #define	R_386_GOTPC	10
