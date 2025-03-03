f3e0bc8cdc989b7713e5d9748b11ea632d46e431
Thomas Dettbarn <dettus@dettus.net>
2025-03-03 19:14:44 +0100

    sub directory display/ was missing for 5.6.19

--- sys/external/bsd/drm2/dist/drm/i915/display/intel_dp.c
+++ sys/external/bsd/drm2/dist/drm/i915/display/intel_dp.c
@@ -1,4 +1,5 @@
 /*	$NetBSD: intel_dp.c,v 1.7 2021/12/19 12:41:54 riastradh Exp $	*/
+		.ops = &icl_tc_phy_aux_power_well_ops,
 
 /*
  * Copyright © 2008 Intel Corporation
@@ -2518,9 +2519,6 @@ static void intel_dp_prepare(struct intel_encoder *encoder,
 				 pipe_config->lane_count,
 				 intel_crtc_has_type(pipe_config,
 						     INTEL_OUTPUT_DP_MST));
-
-	intel_dp->regs.dp_tp_ctl = DP_TP_CTL(port);
-	intel_dp->regs.dp_tp_status = DP_TP_STATUS(port);
 
 	/*
 	 * There are four kinds of DP registers:
@@ -7651,6 +7649,8 @@ bool intel_dp_init(struct drm_i915_private *dev_priv,
 
 	intel_dig_port->dp.output_reg = output_reg;
 	intel_dig_port->max_lanes = 4;
+	intel_dig_port->dp.regs.dp_tp_ctl = DP_TP_CTL(port);
+	intel_dig_port->dp.regs.dp_tp_status = DP_TP_STATUS(port);
 
 	intel_encoder->type = INTEL_OUTPUT_DP;
 	intel_encoder->power_domain = intel_port_to_power_domain(port);
