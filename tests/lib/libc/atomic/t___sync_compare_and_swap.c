eba06bbe9b3b942c8ab6ff7fc7537e783331eb12
riastradh <riastradh@NetBSD.org>
2025-03-13 14:00:50 +0000

    tests/lib/libc/atomic: Test subword compare-and-swap explicitly.
    Make sure the sign doesn't bleed into an adjacent word.
    
    We already had failing tests, but this should make the failures a
    little more obvious and perhaps check for more adjacent problems.
    
    PR port-arm/56839: GCC emits wrong codes for compare_and_swap_1
    bultins on armv5 (el & eb)

--- tests/lib/libc/atomic/t___sync_compare_and_swap.c
+++ tests/lib/libc/atomic/t___sync_compare_and_swap.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: t___sync_compare_and_swap.c,v 1.1 2019/02/26 10:01:41 isaki Exp $	*/
+/*	$NetBSD: t___sync_compare_and_swap.c,v 1.2 2025/03/13 14:00:50 riastradh Exp $	*/
 
 /*
  * Copyright (C) 2019 Tetsuya Isaki. All rights reserved.
@@ -26,7 +26,7 @@
  */
 
 #include <sys/cdefs.h>
-__RCSID("$NetBSD: t___sync_compare_and_swap.c,v 1.1 2019/02/26 10:01:41 isaki Exp $");
+__RCSID("$NetBSD: t___sync_compare_and_swap.c,v 1.2 2025/03/13 14:00:50 riastradh Exp $");
 
 #include <atf-c.h>
 #include <inttypes.h>
@@ -92,6 +92,59 @@ atf_sync_bool(__sync_bool_compare_and_swap_4, uint32_t, PRIx32);
 atf_sync_bool(__sync_bool_compare_and_swap_8, uint64_t, PRIx64);
 #endif
 
+#if _BYTE_ORDER == _LITTLE_ENDIAN
+#  define LSB	0
+#  define MSB	1
+#elif _BYTE_ORDER == _BIG_ENDIAN
+#  define LSB	1
+#  define MSB	0
+#else
+#  error Unknown byte order!
+#endif
+
+#define atf_sync_bool_subword(NAME, SUBWIDTH, SUBTYPE, TYPE, SUBFMT, FMT) \
+ATF_TC(NAME##_subword); \
+ATF_TC_HEAD(NAME##_subword, tc) \
+{ \
+	atf_tc_set_md_var(tc, "descr", "subword " #NAME); \
+} \
+ATF_TC_BODY(NAME##_subword, tc) \
+{ \
+	volatile union { \
+		TYPE word; \
+		SUBTYPE subword[2]; \
+	} val; \
+	/* If successful */ \
+	val.subword[LSB] = -1; \
+	val.subword[MSB] = 123; \
+	ATF_CHECK(NAME(&val.subword[LSB], /*old*/-1, /*new*/-2)); \
+	ATF_CHECK_EQ_MSG(val.subword[LSB], (SUBTYPE)-2, \
+	    "val.subword[LSB] = 0x%" SUBFMT, val.subword[LSB]); \
+	ATF_CHECK_EQ_MSG(val.subword[MSB], 123, \
+	    "val.subword[MSB] = 0x%" SUBFMT, val.subword[MSB]); \
+	ATF_CHECK_EQ_MSG(val.word, \
+	    ((TYPE)123 << (SUBWIDTH)) | (TYPE)(SUBTYPE)-2, \
+	    "val.word = 0x%" FMT, val.word); \
+	/* If failed */ \
+	val.subword[LSB] = -3; \
+	val.subword[MSB] = 45; \
+	ATF_CHECK(!NAME(&val.subword[LSB], /*old*/-1, /*new*/-2)); \
+	ATF_CHECK_EQ_MSG(val.subword[LSB], (SUBTYPE)-3, \
+	    "val.subword[LSB] = 0x%" SUBFMT, val.subword[LSB]); \
+	ATF_CHECK_EQ_MSG(val.subword[MSB], 45, \
+	    "val.subword[MSB] = 0x%" SUBFMT, val.subword[MSB]); \
+	ATF_CHECK_EQ_MSG(val.word, \
+	    ((TYPE)45 << (SUBWIDTH)) | (TYPE)(SUBTYPE)-3, \
+	    "val.word = 0x%" FMT, val.word); \
+}
+
+atf_sync_bool_subword(__sync_bool_compare_and_swap_1, 8, uint8_t, uint16_t,
+    PRIx8, PRIx16);
+atf_sync_bool_subword(__sync_bool_compare_and_swap_2, 16, uint16_t, uint32_t,
+    PRIx16, PRIx32);
+atf_sync_bool_subword(__sync_bool_compare_and_swap_4, 32, uint32_t, uint64_t,
+    PRIx32, PRIx64);
+
 #define atf_sync_val(NAME, TYPE, FMT) \
 ATF_TC(NAME); \
 ATF_TC_HEAD(NAME, tc) \
@@ -137,6 +190,56 @@ atf_sync_val(__sync_val_compare_and_swap_4, uint32_t, PRIx32);
 atf_sync_val(__sync_val_compare_and_swap_8, uint64_t, PRIx64);
 #endif
 
+#define atf_sync_val_subword(NAME, SUBWIDTH, SUBTYPE, TYPE, SUBFMT, FMT) \
+ATF_TC(NAME##_subword); \
+ATF_TC_HEAD(NAME##_subword, tc) \
+{ \
+	atf_tc_set_md_var(tc, "descr", "subword " #NAME); \
+} \
+ATF_TC_BODY(NAME##_subword, tc) \
+{ \
+	volatile union { \
+		TYPE word; \
+		SUBTYPE subword[2]; \
+	} val; \
+	SUBTYPE rval; \
+	/* If successful */ \
+	val.subword[LSB] = -1; \
+	val.subword[MSB] = 123; \
+	ATF_CHECK_EQ_MSG((rval = NAME(&val.subword[LSB], \
+		/*old*/-1, /*new*/-2)), (SUBTYPE)-1, \
+	    "expected 0x%" SUBFMT ", got 0x%" SUBFMT, \
+	    (SUBTYPE)-1, rval); \
+	ATF_CHECK_EQ_MSG(val.subword[LSB], (SUBTYPE)-2, \
+	    "val.subword[LSB] = 0x%" SUBFMT, val.subword[LSB]); \
+	ATF_CHECK_EQ_MSG(val.subword[MSB], 123, \
+	    "val.subword[MSB] = 0x%" SUBFMT, val.subword[MSB]); \
+	ATF_CHECK_EQ_MSG(val.word, \
+	    ((TYPE)123 << (SUBWIDTH)) | (TYPE)(SUBTYPE)-2, \
+	    "val.word = 0x%" FMT, val.word); \
+	/* If failed */ \
+	val.subword[LSB] = -3; \
+	val.subword[MSB] = 45; \
+	ATF_CHECK_EQ_MSG((rval = NAME(&val.subword[LSB], \
+		/*old*/-1, /*new*/-2)), (SUBTYPE)-3, \
+	    "expected 0x%" SUBFMT ", got 0x%" SUBFMT, \
+	    (SUBTYPE)-3, rval); \
+	ATF_CHECK_EQ_MSG(val.subword[LSB], (SUBTYPE)-3, \
+	    "val.subword[LSB] = 0x%" SUBFMT, val.subword[LSB]); \
+	ATF_CHECK_EQ_MSG(val.subword[MSB], 45, \
+	    "val.subword[MSB] = 0x%" SUBFMT, val.subword[MSB]); \
+	ATF_CHECK_EQ_MSG(val.word, \
+	    ((TYPE)45 << (SUBWIDTH)) | (TYPE)(SUBTYPE)-3, \
+	    "val.word = 0x%" FMT, val.word); \
+}
+
+atf_sync_val_subword(__sync_val_compare_and_swap_1, 8, uint8_t, uint16_t,
+    PRIx8, PRIx16);
+atf_sync_val_subword(__sync_val_compare_and_swap_2, 16, uint16_t, uint32_t,
+    PRIx16, PRIx32);
+atf_sync_val_subword(__sync_val_compare_and_swap_4, 32, uint32_t, uint64_t,
+    PRIx32, PRIx64);
+
 ATF_TP_ADD_TCS(tp)
 {
 	ATF_TP_ADD_TC(tp, __sync_bool_compare_and_swap_1);
@@ -146,12 +249,20 @@ ATF_TP_ADD_TCS(tp)
 	ATF_TP_ADD_TC(tp, __sync_bool_compare_and_swap_8);
 #endif
 
+	ATF_TP_ADD_TC(tp, __sync_bool_compare_and_swap_1_subword);
+	ATF_TP_ADD_TC(tp, __sync_bool_compare_and_swap_2_subword);
+	ATF_TP_ADD_TC(tp, __sync_bool_compare_and_swap_4_subword);
+
 	ATF_TP_ADD_TC(tp, __sync_val_compare_and_swap_1);
 	ATF_TP_ADD_TC(tp, __sync_val_compare_and_swap_2);
 	ATF_TP_ADD_TC(tp, __sync_val_compare_and_swap_4);
 #ifdef __HAVE_ATOMIC64_OPS
 	ATF_TP_ADD_TC(tp, __sync_val_compare_and_swap_8);
 #endif
+
+	ATF_TP_ADD_TC(tp, __sync_val_compare_and_swap_1_subword);
+	ATF_TP_ADD_TC(tp, __sync_val_compare_and_swap_2_subword);
+	ATF_TP_ADD_TC(tp, __sync_val_compare_and_swap_4_subword);
 
 	return atf_no_error();
 }
