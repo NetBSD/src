a14dc95f317981720d1662e5e874d8a76ee20d44
nat <nat@NetBSD.org>
2025-04-03 13:07:39 +0000

    Revert to previous.
    Based on information and discussion on source-change-d from isaki@
    
    Many thanks for the explination.
    
    (currently compile tested only).

--- sys/arch/m68k/m68k/switch_subr.s
+++ sys/arch/m68k/m68k/switch_subr.s
@@ -1,4 +1,4 @@
-/*	$NetBSD: switch_subr.s,v 1.40 2025/03/31 13:03:23 nat Exp $	*/
+/*	$NetBSD: switch_subr.s,v 1.41 2025/04/03 13:07:39 nat Exp $	*/
 
 /*
  * Copyright (c) 2001 The NetBSD Foundation.
@@ -68,21 +68,6 @@
  *        the call.
  */
 
-/*
- * LC_NOP is for compatabiltiy with buggy lc040 cpus where by using fpu
- * emulation the system would become unstable.
- *
- * These insertions are necessary as we might be switching to/from an f-line
- * instruction.
- *
- *  Refer to: NetBSD PR #13078.
- */
-#if 1
-#define LC_NOP 	nop
-#else
-#define LC_NOP
-#endif
-
 	.data
 GLOBAL(curpcb)
 GLOBAL(masterpaddr)		| XXXcompatibility (debuggers)
@@ -95,7 +80,7 @@ GLOBAL(masterpaddr)		| XXXcompatibility (debuggers)
 ASENTRY_NOPROFILE(cpu_idle)
 	stop	#PSL_LOWIPL
 GLOBAL(_Idle)				/* For sun2/sun3's clock.c ... */
-	LC_NOP
+	nop
 	rts
 
 /*
@@ -223,7 +208,7 @@ ENTRY(cpu_switchto)
 .Lcpu_switch_nofprest:
 	movl	%d1,%d0			| return outgoing lwp
 	movl	%d0,%a0			| (in a0, too)
-	LC_NOP
+	nop
 	rts
 
 /*
@@ -272,7 +257,7 @@ ENTRY(savectx)
 #endif /* FPCOPROC */
 #endif /* !_M68K_CUSTOM_FPU_CTX */
 	moveq	#0,%d0			| return 0
-	LC_NOP
+	nop
 	rts
 
 #if !defined(M68010)
@@ -304,7 +289,7 @@ ENTRY(m68k_make_fpu_idle_frame)
 	frestore (%sp)
 	fnop
 	addql	#4,%sp
-	LC_NOP
+	nop
 	rts
 #endif
 
@@ -326,7 +311,7 @@ ENTRY(m68881_save)
 	fmovem	%fp0-%fp7,FPF_REGS(%a0)	| save FP general registers
 	fmovem	%fpcr/%fpsr/%fpi,FPF_FPCR(%a0) | save FP control registers
 .Lm68881sdone:
-	LC_NOP
+	nop
 	rts
 #endif
 #if defined(M68060)
@@ -338,7 +323,7 @@ ENTRY(m68881_save)
 	fmovem	%fpsr,FPF_FPSR(%a0)
 	fmovem	%fpi,FPF_FPI(%a0)
 .Lm68060sdone:
-	LC_NOP
+	nop
         rts
 #endif
 
@@ -356,7 +341,7 @@ ENTRY(m68881_restore)
 	fmovem	FPF_REGS(%a0),%fp0-%fp7	| restore FP general registers
 .Lm68881rdone:
 	frestore (%a0)			| restore state
-	LC_NOP
+	nop
 	rts
 #endif
 #if defined(M68060)
@@ -369,7 +354,7 @@ ENTRY(m68881_restore)
 	fmovem	FPF_REGS(%a0),%fp0-%fp7 | restore FP general registers
 .Lm68060fprdone:
 	frestore (%a0)			| restore state
-	LC_NOP
+	nop
 	rts
 #endif
 #endif
