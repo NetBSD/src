7dc4d7c84f8dc4269e17b768fdaff97b33193a6a
martin <martin@NetBSD.org>
2025-04-04 16:19:39 +0000

    Pull up following revision(s) (requested by hans in ticket #1085):
    	sys/arch/vax/vax/db_disasm.c: revision 1.26
    	sys/arch/vax/vax/db_machdep.c: revision 1.61
    	sys/arch/vax/vax/trap.c: revision 1.140
    	sys/arch/vax/include/db_machdep.h: revision 1.21
    
    vax/ddb(4): clean up machine dependent code and improve usability
    
    First, let's garbage collect some dead code wrapped in #if 0/#endif
    that were introduced in back in 1999 in revision 1.17 of db_machdep.c,
    when VAX stack tracing was last reworked. There's also an unused
    argument "stackbase" in db_dump_stack() that can go away.
    
    Next, fix stack tracing on panic. The panicstr has already been printed
    by the time we get here from db_panic(), and at least on !MULTIPROCESSOR
    the panic stack trace caused a recursive panic immediately. While here,
    add tracing by lwp and proc addresses. The code for tracing a process or
    lwp should live in its own function, and we can rearrange the logic in
    db_stack_trace_print() to be a bit clearer.
    
    While here, add some basic memory access checks so we don't suffer
    from recursive panics all the time. For the same reason, get the
    process with db_find_proc() rather than proc_find_raw().

--- sys/arch/vax/include/db_machdep.h
+++ sys/arch/vax/include/db_machdep.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: db_machdep.h,v 1.20 2017/11/06 03:47:48 christos Exp $	*/
+/*	$NetBSD: db_machdep.h,v 1.20.34.1 2025/04/04 16:19:39 martin Exp $	*/
 
 /* 
  * Mach Operating System
@@ -34,6 +34,7 @@
  * Modified for vax out of i386 code.
  */
 
+#include <sys/stdbool.h>
 #include <sys/param.h>
 #include <uvm/uvm.h>
 #include <machine/trap.h>
@@ -77,6 +78,7 @@ extern	db_regs_t	ddb_regs;	/* register state */
 
 /* Prototypes */
 void	kdb_trap(struct trapframe *);
+bool	db_validate_address(vaddr_t);
 
 /*
  * We use a.out symbols in DDB (unless we are ELF then we use ELF symbols).
