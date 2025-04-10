6aa492bfbaf526be26b172d7c5f575d00829a36b
nat <nat@NetBSD.org>
2025-04-10 19:22:20 +0000

    Revet to previous.
    As pointed out to me on source-changes.

--- sys/arch/m68k/m68k/fpu.c
+++ sys/arch/m68k/m68k/fpu.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: fpu.c,v 1.2 2025/04/09 00:04:41 nat Exp $	*/
+/*	$NetBSD: fpu.c,v 1.3 2025/04/10 19:22:20 nat Exp $	*/
 
 /*-
  * Copyright (c) 1996 The NetBSD Foundation, Inc.
@@ -35,7 +35,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: fpu.c,v 1.2 2025/04/09 00:04:41 nat Exp $");
+__KERNEL_RCSID(0, "$NetBSD: fpu.c,v 1.3 2025/04/10 19:22:20 nat Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -72,9 +72,6 @@ fpu_probe(void)
 	 * state, so we can determine which we have by
 	 * examining the size of the FP state frame
 	 */
-#if defined(M68040)
-	__asm("nop");		/* buggy lc040 workaround. */
-#endif
 	__asm("fnop");
 
 	nofault = NULL;
