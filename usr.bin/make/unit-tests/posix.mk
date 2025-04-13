44e9df0808dfa2357176e4187eb0343d3e05b252
rillig <rillig@NetBSD.org>
2025-04-13 09:44:58 +0000

    tests/make: duplicate ".POSIX:" lines don't hurt anymore
    Fixed in parse.c 1.743 from a few minutes ago.

--- usr.bin/make/unit-tests/posix.mk
+++ usr.bin/make/unit-tests/posix.mk
@@ -1,4 +1,4 @@
-# $NetBSD: posix.mk,v 1.4 2025/04/13 09:29:33 rillig Exp $
+# $NetBSD: posix.mk,v 1.5 2025/04/13 09:44:58 rillig Exp $
 #
 # This file is included in all tests that start with a ".POSIX:" line,
 # even when the "-r" option is given.
@@ -6,11 +6,7 @@
 # The makefile containing the POSIX definitions is not supposed to contain a
 # ".POSIX:" line, but even if it does, this must not lead to an endless loop
 # by including it over and over again.
-#
-# When being included from another file, this ".POSIX:" line occurs too late,
-# though, and would thus switch out of POSIX mode again.
-#
-#.POSIX:
+.POSIX:
 
 # The file <posix.mk> is not intended to be used as a top-level makefile, and
 # it is not supposed to define any targets, only rules.
