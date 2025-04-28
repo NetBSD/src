b26475db0d0287efa146e971c34a3e93d052998c
riastradh <riastradh@NetBSD.org>
2025-04-28 00:28:27 +0000

    t_sigstack: Don't do atf_tc_expect_fail then atf_tc_expect_signal.
    atf interprets this sequence, with no intervening atf_tc_fail, to
    mean the xfail failed to fail so it fails.  (Got that?)  That is, it
    is interpreted somewhat like
    
    	atf_tc_expect_fail(...);
    	... there is supposed to be a failure here ...
    	atf_tc_expect_pass();
    	... remaining checks are expected to pass ...
    
    where a lack of failures between the xfail and the xpass means the
    expectation was not met.
    
    But in this case, there are two compounding bugs:
    
    1. the compat 1.3 setjmp has botched its registers, as uwe@
       discovered, so it is expected to crash on SIGSEGV (PR 59351); and
    
    2. the compat 1.3 longjmp restores the stack poiter and signal mask
       in the wrong order, so it is expected to fail the test (PR 57946).
    
    Since the crash in setjmp triggers before the failure after longjmp,
    only do atf_tc_expect_signal; once that is fixed we can re-enable the
    atf_tc_expect_fail for longjmp.
    
    PR lib/57946: longjmp fails to restore stack first before restoring
    signal mask on most architectures
    
    PR port-arm/59351: compat_setjmp is busted

--- tests/lib/libc/setjmp/t_sigstack.c
+++ tests/lib/libc/setjmp/t_sigstack.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: t_sigstack.c,v 1.21 2025/04/25 01:56:52 riastradh Exp $	*/
+/*	$NetBSD: t_sigstack.c,v 1.22 2025/04/28 00:28:27 riastradh Exp $	*/
 
 /*-
  * Copyright (c) 2024 The NetBSD Foundation, Inc.
@@ -27,7 +27,7 @@
  */
 
 #include <sys/cdefs.h>
-__RCSID("$NetBSD: t_sigstack.c,v 1.21 2025/04/25 01:56:52 riastradh Exp $");
+__RCSID("$NetBSD: t_sigstack.c,v 1.22 2025/04/28 00:28:27 riastradh Exp $");
 
 #include <dlfcn.h>
 #include <setjmp.h>
@@ -273,9 +273,11 @@ ATF_TC_BODY(compat13_setjmp, tc)
 	compatsetup();
 
 #if defined __arm__ || defined __i386__ || defined __sh3__
+#ifndef __arm__			/* will be exposed once PR 59351 is fixed */
 	atf_tc_expect_fail("PR lib/57946:"
 	    " longjmp fails to restore stack first before"
 	    " restoring signal mask on most architectures");
+#endif
 #endif
 #ifdef __arm__
 	atf_tc_expect_signal(-1, "PR port-arm/59351: compat_setjmp is busted");
@@ -351,9 +353,11 @@ ATF_TC_BODY(compat13_sigsetjmp, tc)
 	compatsetup();
 
 #if defined __arm__ || defined __i386__ || defined __sh3__
+#ifndef __arm__			/* will be exposed once PR 59351 is fixed */
 	atf_tc_expect_fail("PR lib/57946:"
 	    " longjmp fails to restore stack first before"
 	    " restoring signal mask on most architectures");
+#endif
 #endif
 #ifdef __arm__
 	atf_tc_expect_signal(-1, "PR port-arm/59351: compat_setjmp is busted");
