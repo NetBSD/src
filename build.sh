efbc90c66118f14b4fd1e9a6abd9bcd0891ee3ca
bouyer <bouyer@NetBSD.org>
2025-05-10 18:06:49 +0000

    Pull up following revision(s) (requested by martin in ticket #1950): 	build.sh: revision 1.392 via patch Expert hack: if you know what you are doing, allow to avoid the tool build for some commands that might or might not need it, like: 	build.sh -VUSETOOLS=never -P mkrepro-timestamp show-revisionid This will not need any tools when run on a new-enough NetBSD host or if using some other VCS than CVS.

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env sh
-#	$NetBSD: build.sh,v 1.333.2.7 2025/04/24 14:17:05 snj Exp $
+#	$NetBSD: build.sh,v 1.333.2.8 2025/05/10 18:06:49 bouyer Exp $
 #
 # Copyright (c) 2001-2011 The NetBSD Foundation, Inc.
 # All rights reserved.
@@ -1463,7 +1463,11 @@ parseoptions()
 	MAKEFLAGS="-de -m ${TOP}/share/mk ${MAKEFLAGS}"
 	MAKEFLAGS="${MAKEFLAGS} MKOBJDIRS=${MKOBJDIRS-yes}"
 	export MAKEFLAGS MACHINE MACHINE_ARCH
-	setmakeenv USETOOLS "yes"
+	if [ -z "${USETOOLS}" ]; then
+		setmakeenv USETOOLS yes
+	else
+		setmakeenv USETOOLS "${USETOOLS}"
+	fi
 	setmakeenv MAKEWRAPPERMACHINE "${makewrappermachine:-${MACHINE}}"
 }
 
@@ -1520,6 +1524,13 @@ sanitycheck()
 	done
 }
 
+# find a command in PATH and print the full filename
+print_path_of()
+{
+	set -- $( type "$1" )
+	printf "${3}\n"
+}
+
 # print_tooldir_make --
 # Try to find and print a path to an existing
 # ${TOOLDIR}/bin/${toolprefix}program
@@ -1530,6 +1541,11 @@ print_tooldir_program()
 	local possible_program
 	local tooldir_program
 	local program=${1}
+
+	if [ "${USETOOLS-yes}" != "yes" ]; then
+		print_path_of "${program}"
+		return
+	fi
 
 	if [ -n "${TOOLDIR}" ]; then
 		echo "${TOOLDIR}/bin/${toolprefix}${program}"
@@ -1966,7 +1982,7 @@ createmakewrapper()
 	eval cat <<EOF ${makewrapout}
 #! ${HOST_SH}
 # Set proper variables to allow easy "make" building of a NetBSD subtree.
-# Generated from:  \$NetBSD: build.sh,v 1.333.2.7 2025/04/24 14:17:05 snj Exp $
+# Generated from:  \$NetBSD: build.sh,v 1.333.2.8 2025/05/10 18:06:49 bouyer Exp $
 # with these arguments: ${_args}
 #
 
