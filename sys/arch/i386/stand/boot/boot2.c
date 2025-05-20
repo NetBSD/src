555d26c71734d1d0a056c7085cfa1092c3a98b5a
pgoyette <pgoyette@NetBSD.org>
2025-05-20 12:05:42 +0000

    /home/paul/COMMIT.txt

--- sys/arch/i386/stand/boot/boot2.c
+++ sys/arch/i386/stand/boot/boot2.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: boot2.c,v 1.86 2025/05/06 18:16:12 pgoyette Exp $	*/
+/*	$NetBSD: boot2.c,v 1.87 2025/05/20 12:05:42 pgoyette Exp $	*/
 
 /*-
  * Copyright (c) 2008, 2009 The NetBSD Foundation, Inc.
@@ -489,14 +489,18 @@ command_boot(char *arg)
 	if (!parseboot(arg, &filename, &howto))
 		return;
 
-	if (filename != NULL) {
+	if (filename != NULL && filename[0] != '\0') {
 		/* try old locations first to assist atf test beds */
 		snprintf(path, sizeof(path) - 4, "%s", filename);
 		bootit2(path, sizeof(path), howto);
 
-		/* now treat given filename as a directory */
-		if (strchr(filename, '/') == NULL) {
-			snprintf(path, sizeof(path) - 4, "%s/kernel", filename);
+		/*
+		 * now treat given filename as a directory unless there
+		 * is already an embedded path-name separator '/' present
+		 */
+		if (strchr(filename + 1, '/') == NULL) {
+			snprintf(path, sizeof(path) - 4, "%s/kernel",
+			    filename);
 			bootit2(path, sizeof(path), howto);
 		}
 	} else {
