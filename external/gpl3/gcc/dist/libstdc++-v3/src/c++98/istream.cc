2a2dff506c93fde0564340c445c5e62ce6a925b4
mrg <mrg@NetBSD.org>
2025-07-21 16:41:53 +0000

    re-apply christos's fix from 2019 fully.

--- external/gpl3/gcc/dist/libstdc++-v3/src/c++98/istream.cc
+++ external/gpl3/gcc/dist/libstdc++-v3/src/c++98/istream.cc
@@ -112,7 +112,7 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
     basic_istream<char>::
     ignore(streamsize __n, int_type __delim)
     {
-//#ifdef _GLIBCXX_COMPAT_
+#ifdef _GLIBCXX_COMPAT_
       {
 	// If conversion to int_type changes the value then __delim does not
 	// correspond to a value of type char_type, and so will never match
@@ -124,7 +124,7 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
 	// Now we know that __delim is a valid char_type value, so it's safe
 	// for the code below to use traits_type::find to search for it.
       }
-//#endif
+#endif
 
       _M_gcount = 0;
       sentry __cerb(*this, true);
