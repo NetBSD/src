294588d3408f827aab259a87affbf15fab548be8
mrg <mrg@NetBSD.org>
2025-07-21 05:33:08 +0000

    mknative-gcc updates for GCC 12.5.0.

--- external/gpl3/gcc/usr.bin/gcc/arch/sh3eb/plugin-version.h
+++ external/gpl3/gcc/usr.bin/gcc/arch/sh3eb/plugin-version.h
@@ -5,12 +5,12 @@
 #include "configargs.h"
 
 #define GCCPLUGIN_VERSION_MAJOR   12
-#define GCCPLUGIN_VERSION_MINOR   4
+#define GCCPLUGIN_VERSION_MINOR   5
 #define GCCPLUGIN_VERSION_PATCHLEVEL   0
 #define GCCPLUGIN_VERSION  (GCCPLUGIN_VERSION_MAJOR*1000 + GCCPLUGIN_VERSION_MINOR)
 
-static char basever[] = "12.4.0";
-static char datestamp[] = "20240620";
+static char basever[] = "12.5.0";
+static char datestamp[] = "20250711";
 static char devphase[] = "";
 static char revision[] = "";
 
