031456b237012a1c9625d18cbdd940a4bdbb52d4
sjg <sjg@NetBSD.org>
2025-08-04 18:09:56 +0000

    meta-output.mk: Allow for echo that does not support -n
    On latest Darwin, /bin/sh builtin echo does not do -n
    
    There will be others, so test if echo -n works and adjust.
    
    Capture only the command output from the meta file so
    that results can be consistent whether echo -n works or not.

--- usr.bin/make/unit-tests/meta-output.mk
+++ usr.bin/make/unit-tests/meta-output.mk
@@ -4,6 +4,13 @@
 
 .if make(output)
 .MAKE.MODE= meta curDirOk=true nofilemon
+x!= echo -n ok; echo
+.if ${x:M-n} == ""
+ECHO_SCRIPT= Echo() { echo "$$@"; }
+.else
+ECHO_SCRIPT= Echo() { case "$$1" in -n) shift; echo "$$@\c";; \
+	*) echo "$$@";; esac; }
+.endif
 .else
 .MAKE.MODE= compat
 .endif
@@ -14,13 +21,13 @@ _mf := ${.PARSEDIR}/${.PARSEFILE}
 
 # this output should be accurately reflected in the .meta file
 output: .NOPATH
-	@{ echo Test ${tag} output; \
+	@${ECHO_SCRIPT}; { echo Test ${tag} output; \
 	for i in 1 2 3; do \
-	echo -n "test$$i:  "; sleep 0; echo " Done"; \
+	Echo -n "test$$i:  "; sleep 0; echo " Done"; \
 	done; }
 
 output.-B output.-j1:
 	@{ rm -f ${TMPDIR}/output; mkdir -p ${TMPDIR}/obj; \
 	MAKEFLAGS= ${.MAKE} -r -C ${TMPDIR} ${.TARGET:E} tag=${.TARGET:E} -f ${_mf} output; \
-	cat ${TMPDIR}/obj/output.meta; \
+	sed '1,/^TARGET/d' ${TMPDIR}/obj/output.meta; \
 	}
