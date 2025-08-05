ed60fc5f6d974509d0baebb6b2bec0e787d7b4d4
sjg <sjg@NetBSD.org>
2025-08-05 16:02:14 +0000

    meta-output.mk diff output
    We construct output such that it should match what we
    can extract from output.meta, so diff the two.
    This way the test cannot pass if meta.c is capturing
    something inaccurately - other than the trailing newline.

--- usr.bin/make/unit-tests/meta-output.mk
+++ usr.bin/make/unit-tests/meta-output.mk
@@ -12,15 +12,19 @@ all: output.-B output.-j1
 
 _mf := ${.PARSEDIR}/${.PARSEFILE}
 
-# this output should be accurately reflected in the .meta file
+# This output should be accurately reflected in the .meta file.
+# We append an extra newline to ${.TARGET} (after it has been
+# written to stdout) to match what meta_cmd_finish() will do.
 output: .NOPATH
 	@{ echo Test ${tag} output; \
 	for i in 1 2 3; do \
 	printf "test$$i:  "; sleep 0; echo " Done"; \
-	done; }
+	done; echo; } | tee ${.TARGET}; echo >> ${.TARGET}
 
+# The diff at the end should produce nothing.
 output.-B output.-j1:
 	@{ rm -f ${TMPDIR}/output; mkdir -p ${TMPDIR}/obj; \
 	MAKEFLAGS= ${.MAKE} -r -C ${TMPDIR} ${.TARGET:E} tag=${.TARGET:E} -f ${_mf} output; \
-	sed '1,/^TARGET/d' ${TMPDIR}/obj/output.meta; \
-	}
+	sed '1,/command output/d' ${TMPDIR}/obj/output.meta > ${TMPDIR}/obj/output-meta; \
+	diff -u ${TMPDIR}/obj/output ${TMPDIR}/obj/output-meta; }
+
