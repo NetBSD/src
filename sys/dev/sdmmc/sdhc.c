1804655e3724e1d65e249fe3f83ef56404bae7ed
jmcneill <jmcneill@NetBSD.org>
2025-09-02 22:22:14 +0000

    sdhc: Disable bus power for 32-bit register accesses.
    SDHC_FLAG_32BIT_ACCESS shouldn't prohibit clearing the SD Bus Power bit in
    the Power Control Register, that's what SDHC_FLAG_NO_PWR0 is for.
    
    Should help with card initialization issues at boot on controllers that
    set the 32-bit access flag.

--- sys/dev/sdmmc/sdhc.c
+++ sys/dev/sdmmc/sdhc.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: sdhc.c,v 1.121 2025/02/16 11:15:18 jmcneill Exp $	*/
+/*	$NetBSD: sdhc.c,v 1.122 2025/09/02 22:22:14 jmcneill Exp $	*/
 /*	$OpenBSD: sdhc.c,v 1.25 2009/01/13 19:44:20 grange Exp $	*/
 
 /*
@@ -23,7 +23,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: sdhc.c,v 1.121 2025/02/16 11:15:18 jmcneill Exp $");
+__KERNEL_RCSID(0, "$NetBSD: sdhc.c,v 1.122 2025/09/02 22:22:14 jmcneill Exp $");
 
 #ifdef _KERNEL_OPT
 #include "opt_sdmmc.h"
@@ -935,8 +935,7 @@ sdhc_bus_power(sdmmc_chipset_handle_t sch, uint32_t ocr)
 	/*
 	 * Disable bus power before voltage change.
 	 */
-	if (!ISSET(hp->sc->sc_flags, SDHC_FLAG_32BIT_ACCESS)
-	    && !ISSET(hp->sc->sc_flags, SDHC_FLAG_NO_PWR0)) {
+	if (!ISSET(hp->sc->sc_flags, SDHC_FLAG_NO_PWR0)) {
 		hp->vdd = 0;
 		HWRITE1(hp, SDHC_POWER_CTL, 0);
 	}
