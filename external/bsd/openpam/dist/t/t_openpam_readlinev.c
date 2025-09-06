19d727a294d4230ad0035e414f3384d2b8c186e3
riastradh <riastradh@NetBSD.org>
2025-09-06 12:38:20 +0000

    openpam: Nix duplicate definition in tests.
    We obviously don't build or run the tests (not even sure how,
    requires cryb/test.h which doesn't exist in src or pkgsrc), but let's
    not carry around a local patch that makes them obviously not compile
    even if we had the infrastructure to do so.

--- external/bsd/openpam/dist/t/t_openpam_readlinev.c
+++ external/bsd/openpam/dist/t/t_openpam_readlinev.c
@@ -147,23 +147,6 @@ static const char *numbers[] = {
 };
 
 
-static const char *numbers[] = {
-	"zero", "one", "two", "three", "four", "five", "six", "seven",
-	"eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen",
-	"fifteen", "sixteen", "seventeen", "nineteen", "twenty",
-	"twenty-one", "twenty-two", "twenty-three", "twenty-four",
-	"twenty-five", "twenty-six", "twenty-seven", "twenty-eight",
-	"twenty-nine", "thirty", "thirty-one", "thirty-two", "thirty-three",
-	"thirty-four", "thirty-five", "thirty-six", "thirty-seven",
-	"thirty-eight", "thirty-nine", "fourty", "fourty-one", "fourty-two",
-	"fourty-three", "fourty-four", "fourty-five", "fourty-six",
-	"fourty-seven", "fourty-eight", "fourty-nine", "fifty", "fifty-one",
-	"fifty-two", "fifty-three", "fifty-four", "fifty-five", "fifty-six",
-	"fifty-seven", "fifty-eight", "fifty-nine", "sixty", "sixty-one",
-	"sixty-two", "sixty-three",
-	NULL
-};
-
 /***************************************************************************
  * Lines without words
  */
