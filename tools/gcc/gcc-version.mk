1dd146e1b3c2ad6083b1e4a5deab7658a7f3bb68
mrg <mrg@NetBSD.org>
2025-09-16 02:08:01 +0000

    clean ups and gcc 14 support.
    - remove some obsolete code
    - derive MKNATIVE path from EXTERNAL_GCC_SUBDIR
    - add version for netbsd gcc 14

--- tools/gcc/gcc-version.mk
+++ tools/gcc/gcc-version.mk
@@ -1,10 +1,10 @@
-#	$NetBSD: gcc-version.mk,v 1.28 2025/07/21 05:27:09 mrg Exp $
+#	$NetBSD: gcc-version.mk,v 1.29 2025/09/16 02:08:01 mrg Exp $
 
 # common location for tools and native build
 
-.if ${HAVE_GCC} == 10
-NETBSD_GCC_VERSION=nb3 20231008
-.endif
 .if ${HAVE_GCC} == 12
 NETBSD_GCC_VERSION=nb1 20250721
+.endif
+.if ${HAVE_GCC} == 14
+NETBSD_GCC_VERSION=nb1 20250915
 .endif
