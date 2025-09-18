67e9acae606b2ab1162f7f765e254e8582441e83
mrg <mrg@NetBSD.org>
2025-09-18 06:45:09 +0000

    fix some issue GCC 14 found.
    include <sys/ioctl.h> when using ioctl.
    don't assign NULL to integers.

--- external/cddl/osnet/dist/common/zfs/zfs_ioctl_compat.c
+++ external/cddl/osnet/dist/common/zfs/zfs_ioctl_compat.c
@@ -29,6 +29,7 @@
 #include <sys/types.h>
 #include <sys/param.h>
 #include <sys/cred.h>
+#include <sys/ioctl.h>
 #include <sys/dmu.h>
 #include <sys/zio.h>
 #include <sys/nvpair.h>
--- external/cddl/osnet/dist/lib/libzfs/common/libzfs_import.c
+++ external/cddl/osnet/dist/lib/libzfs/common/libzfs_import.c
@@ -51,6 +51,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
+#include <sys/ioctl.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <thread_pool.h>
