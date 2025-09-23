a4f469f6e2e6a154f5d59fc0e3fd8d65bdef52e1
martin <martin@NetBSD.org>
2025-09-23 12:44:01 +0000

    Pull up following revision(s) (requested by nat in ticket #1159):
    	sys/dev/usb/xhci.c: revision 1.190
    
    Handle special case of USBD_FORCE_SHORT_XFER.
    
    This fixes devices such as urtwn(4) connected to an xhci port.
    
    It works by sending a zero length transfer at the end of the data transfer
    for the above mentioned flag.
    
    Similar artwork in this space already exists for ohci/uhci/ehci.
    
    OK jkallsch@.

--- sys/dev/usb/xhci.c
+++ sys/dev/usb/xhci.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: xhci.c,v 1.175.2.3 2024/09/20 10:52:21 martin Exp $	*/
+/*	$NetBSD: xhci.c,v 1.175.2.4 2025/09/23 12:44:01 martin Exp $	*/
 
 /*
  * Copyright (c) 2013 Jonathan A. Kollasch
@@ -34,7 +34,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: xhci.c,v 1.175.2.3 2024/09/20 10:52:21 martin Exp $");
+__KERNEL_RCSID(0, "$NetBSD: xhci.c,v 1.175.2.4 2025/09/23 12:44:01 martin Exp $");
 
 #ifdef _KERNEL_OPT
 #include "opt_usb.h"
@@ -4756,6 +4756,13 @@ xhci_device_bulk_start(struct usbd_xfer *xfer)
 	    (isread ? XHCI_TRB_3_ISP_BIT : 0) |
 	    XHCI_TRB_3_IOC_BIT;
 	xhci_xfer_put_trb(xx, i++, parameter, status, control);
+
+	if (!isread && (xfer->ux_flags & USBD_FORCE_SHORT_XFER)) {
+		status = XHCI_TRB_2_IRQ_SET(0) |
+		    XHCI_TRB_2_TDSZ_SET(0) |
+		    XHCI_TRB_2_BYTES_SET(0);
+		xhci_xfer_put_trb(xx, i++, parameter, status, control);
+	}
 
 	if (!polling)
 		mutex_enter(&tr->xr_lock);
