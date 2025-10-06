a2a5a030ef0f2b078dbff6d07527ef6bf61663f6
martin <martin@NetBSD.org>
2025-10-06 16:23:00 +0000

    Pull up following revision(s) (requested by kre in ticket #1171):
    	tests/lib/libc/kevent_nullmnt/t_nullmnt.sh: revision 1.7-1.9
    
    t_nullmnt: Need privileges for mount.
    (unless vfs.generic.usermount=1 but let's keep it simple)
    
    PR 59683: check for availability of null FS support via
    vfs.generic.fstypes, fail if the mount fails for other reasons.

--- tests/lib/libc/kevent_nullmnt/t_nullmnt.sh
+++ tests/lib/libc/kevent_nullmnt/t_nullmnt.sh
@@ -3,15 +3,33 @@
 curdir=$(pwd)
 helper=$(atf_get_srcdir)/h_nullmnt
 
+# helper function to check for kernel support
+have_nullfs_support()
+{
+	for t in $( sysctl -n vfs.generic.fstypes )
+	do
+		case "$t" in
+		  null)	return 0;;
+		esac
+	done
+
+	return 1
+}
+
 # common test body
 #    $1 = directory of file to monitor
 #    $2 = directory of file to update/modify
 
 nullmnt_common()
-{    
+{
+	if ! have_nullfs_support; then
+		atf_skip "nullfs not supported"
+	fi
+
 	mkdir ${curdir}/lower_dir
 	mkdir ${curdir}/upper_dir
-	mount -t null ${curdir}/lower_dir ${curdir}/upper_dir
+	mount -t null ${curdir}/lower_dir ${curdir}/upper_dir || \
+		atf_fail "could not mount nullfs"
 	rm -f ${curdir}/lower_dir/afile
 	touch ${curdir}/lower_dir/afile
 
@@ -30,6 +48,7 @@ atf_test_case nullmnt_upper_lower cleanup
 nullmnt_upper_lower_head()
 {
 	atf_set "descr" "ensure upper fs events seen on lower fs"
+	atf_set "require.user" "root"
 }
 nullmnt_upper_lower_body()
 {
@@ -44,6 +63,7 @@ atf_test_case nullmnt_upper_upper cleanup
 nullmnt_upper_upper_head()
 {
 	atf_set "descr" "ensure upper fs events seen on upper fs"
+	atf_set "require.user" "root"
 }
 nullmnt_upper_upper_body()
 {
@@ -57,6 +77,7 @@ atf_test_case nullmnt_lower_upper cleanup
 nullmnt_lower_upper_head()
 {
 	atf_set "descr" "ensure lower fs events seen on upper fs"
+	atf_set "require.user" "root"
 }
 nullmnt_lower_upper_body()
 {
@@ -71,6 +92,7 @@ atf_test_case nullmnt_lower_lower cleanup
 nullmnt_lower_lower_head()
 {
 	atf_set "descr" "ensure lower fs events seen on lower fs"
+	atf_set "require.user" "root"
 }
 nullmnt_lower_lower_body()
 {
