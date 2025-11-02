764d9b8e6455f134ce8def4fd55c12728d2f627f
nia <nia@NetBSD.org>
2025-11-02 22:16:24 +0000

    wscons(4): Add support for 256 colours to VT100 emulation
    Also implemented are CSI 90-97 and CSI 100-107 to set the bright
    versions of the regular eight colours directly.
    
    wscons already has full support of 8/16/24/32-bit displays (and
    can take advantage of them for antialiased fonts), but increasingly
    command-line applications are built to take advantage of colours beyond
    the standard 8 provided by the ANSI escape codes. These extensions are
    not generally standardized, but xterm serves as a de-facto standard.
    
    Patch from Crystal Kolipe, adjusted slightly to work on 16-bit and
    24-bit displays by me.
    
    PR kern/59544

--- sys/dev/wscons/wsdisplayvar.h
+++ sys/dev/wscons/wsdisplayvar.h
@@ -1,4 +1,4 @@
-/* $NetBSD: wsdisplayvar.h,v 1.57 2023/03/01 08:42:33 riastradh Exp $ */
+/* $NetBSD: wsdisplayvar.h,v 1.58 2025/11/02 22:16:24 nia Exp $ */
 
 /*
  * Copyright (c) 1996, 1997 Christopher G. Demetriou.  All rights reserved.
@@ -107,6 +107,7 @@ struct wsscreen_descr {
 #define WSSCREEN_RESIZE		32	/* can resize */
 #define WSSCREEN_FREE		64	/* free() this struct when deleting
 					 * internal only, do not set */
+#define WSSCREEN_256COL		128
 	void *modecookie;
 };
 
