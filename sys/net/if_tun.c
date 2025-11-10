199fe8984d85e627cc9b68079c1f77087ee517c9
knakahara <knakahara@NetBSD.org>
2025-11-10 06:28:13 +0000

    fix indent

--- sys/net/if_tun.c
+++ sys/net/if_tun.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: if_tun.c,v 1.177 2024/09/18 23:20:20 rin Exp $	*/
+/*	$NetBSD: if_tun.c,v 1.178 2025/11/10 06:28:13 knakahara Exp $	*/
 
 /*
  * Copyright (c) 1988, Julian Onions <jpo@cs.nott.ac.uk>
@@ -19,7 +19,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: if_tun.c,v 1.177 2024/09/18 23:20:20 rin Exp $");
+__KERNEL_RCSID(0, "$NetBSD: if_tun.c,v 1.178 2025/11/10 06:28:13 knakahara Exp $");
 
 #ifdef _KERNEL_OPT
 #include "opt_inet.h"
@@ -154,7 +154,7 @@ tundetach(void)
 #endif
 		if_clone_attach(&tun_cloner);
 		return EBUSY;
-}
+	}
 
 	mutex_destroy(&tun_softc_lock);
 
