561bc46d9749a5d74fbf96f7ba3e3b7da575685a
tsutsui <tsutsui@NetBSD.org>
2025-11-12 18:13:26 +0000

    Pull cpu_init_kcore_hdr() changes to sync with othen m68k ports.
    https://mail-index.netbsd.org/source-changes/2025/11/12/msg158921.html
    > cpu_init_kcore_hdr(): use pmap_init_kcore_hdr().

--- sys/arch/news68k/news68k/machdep.c
+++ sys/arch/news68k/news68k/machdep.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: machdep.c,v 1.117 2025/11/12 17:32:48 tsutsui Exp $	*/
+/*	$NetBSD: machdep.c,v 1.118 2025/11/12 18:13:26 tsutsui Exp $	*/
 
 /*
  * Copyright (c) 1988 University of Utah.
@@ -39,7 +39,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: machdep.c,v 1.117 2025/11/12 17:32:48 tsutsui Exp $");
+__KERNEL_RCSID(0, "$NetBSD: machdep.c,v 1.118 2025/11/12 18:13:26 tsutsui Exp $");
 
 #include "opt_ddb.h"
 #include "opt_compat_netbsd.h"
@@ -350,58 +350,13 @@ cpu_reboot(int howto, char *bootstr)
 static void
 cpu_init_kcore_hdr(void)
 {
-	cpu_kcore_hdr_t *h = &cpu_kcore_hdr;
-	struct m68k_kcore_hdr *m = &h->un._m68k;
-
-	memset(&cpu_kcore_hdr, 0, sizeof(cpu_kcore_hdr));
-
-	/*
-	 * Initialize the `dispatcher' portion of the header.
-	 */
-	strcpy(h->name, machine);
-	h->page_size = PAGE_SIZE;
-	h->kernbase = KERNBASE;
-
-	/*
-	 * Fill in information about our MMU configuration.
-	 */
-	m->mmutype	= mmutype;
-	m->sg_v		= SG_V;
-	m->sg_frame	= SG_FRAME;
-	m->sg_ishift	= SG_ISHIFT;
-	m->sg_pmask	= SG_PMASK;
-	m->sg40_shift1	= SG4_SHIFT1;
-	m->sg40_mask2	= SG4_MASK2;
-	m->sg40_shift2	= SG4_SHIFT2;
-	m->sg40_mask3	= SG4_MASK3;
-	m->sg40_shift3	= SG4_SHIFT3;
-	m->sg40_addr1	= SG4_ADDR1;
-	m->sg40_addr2	= SG4_ADDR2;
-	m->pg_v		= PG_V;
-	m->pg_frame	= PG_FRAME;
-
-	/*
-	 * Initialize pointer to kernel segment table.
-	 */
-	m->sysseg_pa = (uint32_t)(pmap_kernel()->pm_stpa);
-
-	/*
-	 * Initialize relocation value such that:
-	 *
-	 *	pa = (va - KERNBASE) + reloc
-	 */
-	m->reloc = lowram;
-
-	/*
-	 * Define the end of the relocatable range.
-	 */
-	m->relocend = (uint32_t)&end;
+	phys_ram_seg_t *ram_segs = pmap_init_kcore_hdr(&cpu_kcore_hdr);
 
 	/*
 	 * news68k has one contiguous memory segment.
 	 */
-	m->ram_segs[0].start = lowram;
-	m->ram_segs[0].size  = ctob(physmem);
+	ram_segs[0].start = lowram;
+	ram_segs[0].size  = ctob(physmem);
 }
 
 /*
