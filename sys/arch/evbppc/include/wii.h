b7f37d394ace141a7d417175ff6630070619869d
martin <martin@NetBSD.org>
2025-11-14 13:18:31 +0000

    Pull up following revision(s) (requested by jmcneill in ticket #1188):
    	sys/arch/evbppc/include/wii.h: revision 1.12
    	sys/arch/evbppc/conf/files.wii: revision 1.7
    	sys/arch/evbppc/conf/WII: revision 1.10
    	sys/arch/evbppc/wii/dev/hwaes.c: revision 1.1
    
    wii: Add support for hardware AES engine and cgd(4) to the WII kernel.
    
    Add a driver for the Nintendo Wii's AES engine. The driver registers an
    AES implementation for kernel use via aes_md_init(). AES-128 requests are
    accelerated by hardware and all other requests are passed through to the
    default (BearSSL aes_ct) implementation.
    
    Add cgd(4) to the Wii kernel to enable disk encryption support.
    
    Tested performance with cgd(4) and AES-128-CBC on a USB flash drive:
      hwaes:   33513984 bytes transferred in 2.245 secs (14928277 bytes/sec)
      bear:    33513984 bytes transferred in 7.581 secs (4420786 bytes/sec)
    
    Many thanks to Taylor Campbell who walked me through implementing support
    for non-CBC modes.

--- sys/arch/evbppc/include/wii.h
+++ sys/arch/evbppc/include/wii.h
@@ -1,4 +1,4 @@
-/* $NetBSD: wii.h,v 1.7.2.6 2025/10/26 13:12:29 martin Exp $ */
+/* $NetBSD: wii.h,v 1.7.2.7 2025/11/14 13:18:31 martin Exp $ */
 
 /*-
  * Copyright (c) 2024 Jared McNeill <jmcneill@invisible.ca>
@@ -135,6 +135,7 @@
 #define  IOPOH1EN			__BIT(22)
 #define  IOPOH0EN			__BIT(21)
 #define  IOPEHCEN			__BIT(20)
+#define  IOPAESEN			__BIT(18)
 #define HW_AIPPROT			(HOLLYWOOD_PRIV_BASE + 0x070)
 #define  ENAHBIOPI			__BIT(0)
 #define HW_GPIOB_OUT			(HOLLYWOOD_BASE + 0x0c0)
