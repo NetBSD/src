270f62bb93b83ad6740757d729c39070fed8d96f
martin <martin@NetBSD.org>
2025-11-20 19:17:39 +0000

    Pull up following revision(s) (requested by jmcneill in ticket #92):
    	sys/arch/evbppc/stand/wii/Makefile: revision 1.1
    	sys/arch/evbppc/stand/wii/gecko.c: revision 1.1
    	distrib/utils/embedded/conf/wii.conf: revision 1.6
    	sys/arch/evbppc/stand/wii/gecko.h: revision 1.1
    	sys/arch/evbppc/wii/wii_mmuinit.S: revision 1.3
    	sys/arch/evbppc/stand/wii/miniipc.c: revision 1.1
    	sys/arch/evbppc/stand/wii/devopen.c: revision 1.1
    	etc/etc.evbppc/Makefile.inc: revision 1.20
    	sys/arch/evbppc/stand/wii/miniipc.h: revision 1.1
    	sys/arch/evbppc/stand/wii/gpio.c: revision 1.1
    	sys/arch/evbppc/stand/wii/gpio.c: revision 1.2
    	sys/arch/evbppc/stand/wii/start.S: revision 1.1
    	sys/arch/evbppc/stand/wii/gpio.h: revision 1.1
    	sys/arch/evbppc/stand/wii/gpio.h: revision 1.2
    	sys/arch/evbppc/stand/wii/timer.h: revision 1.1
    	sys/arch/evbppc/stand/wii/timer.c: revision 1.1
    	sys/arch/evbppc/stand/wii/console.c: revision 1.1
    	sys/arch/evbppc/Makefile: revision 1.4
    	sys/arch/evbppc/stand/wii/sdmmc.c: revision 1.1
    	sys/arch/evbppc/stand/wii/console.h: revision 1.1
    	sys/arch/evbppc/stand/wii/version: revision 1.1
    	sys/arch/evbppc/stand/wii/cache.h: revision 1.1
    	sys/arch/evbppc/stand/wii/version: revision 1.2
    	sys/arch/evbppc/stand/wii/sdmmc.h: revision 1.1
    	sys/arch/evbppc/stand/wii/conf.c: revision 1.1
    	sys/arch/evbppc/stand/wii/boot.c: revision 1.1
    	sys/arch/evbppc/stand/wii/boot.c: revision 1.2
    	sys/arch/evbppc/stand/Makefile: revision 1.1
    	distrib/sets/lists/base/md.evbppc: revision 1.4
    
    wii: Add standalone bootloader.
    
    This adds "ppcboot.elf" to the Wii (evbppc) build, which can be placed on
    the FAT partition of an SD card in the bootmii directory. The bootloader
    doesn't have a user interface, it simply loads a kernel from the first
    FFS partition it finds on the SD card.
    
    Debug logs are printed to a USB Gecko device in either memory card slot
    if present.
    
      \\-__,------,___.
       \\        __,---`  NetBSD/evbppc Wii MINI boot
        \\       `---,_.  Revision 1.0 (Sun Nov 16 19:27:47 UTC 2025)
         \\-,_____,.---`
          \       \        booting netbsd 7828064/-|+130520 [315696+305916]=0x82f350
    [   1.0000000] Copyright (c) 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003,
    [   1.0000000]     2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013,
    [   1.0000000]     2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023,
    [   1.0000000]     2024, 2025
    [   1.0000000]     The NetBSD Foundation, Inc.  All rights reserved.
    [   1.0000000] Copyright (c) 1982, 1986, 1989, 1991, 1993
    [   1.0000000]     The Regents of the University of California.  All rights reserved.
    [   1.0000000] NetBSD 11.99.3 (WII) #207: Sun Nov 16 14:28:23 AST 2025
    [   1.0000000]  jmcneill@berserk:/home/jmcneill/netbsd/obj/sys/arch/evbppc/compile/WII
    [   1.0000000] total memory = 90112 KB
    [   1.0000000] avail memory = 77008 KB
    [   1.0000000] mainbus0 (root): Nintendo Wii
    ...
    
    wii: ppcboot.elf: Support booting backup kernel images.
    
    When the EJECT button is pressed during boot, the default kernel filenames
    "netbsd" and "netbsd.gz" are skipped. Backup kernels starting with
    "onetbsd" and "onetbsd.gz" are used instead.
    
    While here, ensure that the RESET button can reset the system when
    ppcboot.elf is in panic state.
    
    wii: install ppcboot.elf to releasedir (installation/misc)

---
+++ sys/arch/evbppc/stand/wii/boot.c
@@ -0,0 +1,138 @@
+/* $NetBSD: boot.c,v 1.2.4.2 2025/11/20 19:17:41 martin Exp $ */
+
+/*-
+ * Copyright (c) 2025 Jared McNeill <jmcneill@invisible.ca>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <lib/libsa/stand.h>
+#include <lib/libsa/loadfile.h>
+
+#include <powerpc/include/psl.h>
+#include <powerpc/include/spr.h>
+
+#include <machine/pio.h>
+
+#include "cache.h"
+#include "console.h"
+#include "gpio.h"
+#include "miniipc.h"
+#include "sdmmc.h"
+#include "timer.h"
+
+#define PI_INTERRUPT_CAUSE	0x0c003000
+#define  RESET_SWITCH_STATE	__BIT(16)
+
+#define HW_BASE			0x0d800000
+#define HW_RESETS		(HW_BASE + 0x194)
+#define  RSTBINB		__BIT(0)
+
+static const char * const names[] = {
+	"netbsd", "netbsd.gz",
+	"onetbsd", "onetbsd.gz",
+	"netbsd.old", "onetbsd.old.gz",
+};
+#define NUMNAMES	__arraycount(names)
+
+static int	exec_netbsd(const char *);
+
+int
+main(void)
+{
+	extern uint8_t edata[], end[];
+	int curname;
+
+	memset(&edata, 0, end - edata);	/* clear BSS */
+
+	gpio_enable_int(GPIO_EJECT_BTN);
+	gpio_ack_int(GPIO_EJECT_BTN);
+
+	console_init();
+
+	gpio_set(GPIO_SLOT_LED);
+
+	if (!miniipc_probe()) {
+		panic("MINI IPC not found!");
+	}
+
+	sdmmc_init();
+
+	curname = gpio_get_int(GPIO_EJECT_BTN) ? 2 : 0;
+	gpio_disable_int(GPIO_EJECT_BTN);
+	gpio_ack_int(GPIO_EJECT_BTN);
+
+	for (; curname < NUMNAMES; curname++) {
+		printf("booting %s ", names[curname]);
+		exec_netbsd(names[curname]);
+	}
+
+	panic("No bootable kernel found");
+
+	return 0;
+}
+
+static int
+exec_netbsd(const char *fname)
+{
+	u_long marks[MARK_MAX];
+	void (*entry)(void);
+	int fd;
+
+	memset(marks, 0, sizeof(marks));
+	fd = loadfile(fname, marks, LOAD_KERNEL);
+	if (fd == -1) {
+		return -1;
+	}
+
+	gpio_clear(GPIO_SLOT_LED);
+
+	entry = (void *)marks[MARK_ENTRY];
+	cache_dcbf((void *)marks[MARK_START],
+		   marks[MARK_END] - marks[MARK_START]);
+	cache_icbi((void *)marks[MARK_START],
+		   marks[MARK_END] - marks[MARK_START]);
+
+	entry();
+	panic("Unexpected return from kernel");
+}
+
+__dead void
+_rtt(void)
+{
+	int led = 0;
+	for (;;) {
+		if (led) {
+			gpio_set(GPIO_SLOT_LED);
+		} else {
+			gpio_clear(GPIO_SLOT_LED);
+		}
+		for (int i = 0; i < 1000; i++) {
+			if ((in32(PI_INTERRUPT_CAUSE) & RESET_SWITCH_STATE) == 0) {
+				out32(HW_RESETS, in32(HW_RESETS) & ~RSTBINB);
+			}
+			timer_udelay(1000);
+		}
+		led ^= 1;
+	}
+}
---
+++ sys/arch/evbppc/stand/wii/miniipc.c
@@ -0,0 +1,281 @@
+/* $NetBSD: miniipc.c,v 1.1.4.2 2025/11/20 19:17:39 martin Exp $ */
+
+/*-
+ * Copyright (c) 2025 Jared McNeill <jmcneill@invisible.ca>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <lib/libsa/stand.h>
+
+#include <machine/pio.h>
+
+#include "miniipc.h"
+#include "sdmmc.h"
+#include "cache.h"
+#include "timer.h"
+
+#define MINIIPC_INFO_PTR		0x13fffffc
+
+#define HW_BASE				0x0d800000
+#define	HW_IPC_PPCMSG			(HW_BASE + 0x00)
+#define  HW_IPC_PPCMSG_INDEX_OUT_MASK	__BITS(31, 16)
+#define  HW_IPC_PPCMSG_INDEX_IN_MASK	__BITS(15, 0)
+#define HW_IPC_PPCCTRL			(HW_BASE + 0x04)
+#define  HW_IPC_PPCCTRL_IN		__BIT(2)
+#define  HW_IPC_PPCCTRL_OUT		__BIT(0)
+#define HW_IPC_ARMMSG			(HW_BASE + 0x08)
+#define  HW_IPC_ARMMSG_INDEX_OUT_MASK	__BITS(15, 0)
+
+/* IPC request flags */
+#define IPC_SLOW			0x00
+#define IPC_FAST			0x01
+
+/* IPC devices and requests */
+#define IPC_DEV_SYS			0x00
+#define  IPC_SYS_PING			0x0000
+#define IPC_DEV_SDMMC			0x07
+#define  IPC_SDMMC_ACK			0x0000
+#define  IPC_SDMMC_READ			0x0001
+#define  IPC_SDMMC_WRITE		0x0002
+#define  IPC_SDMMC_STATE		0x0003
+#define  IPC_SDMMC_SIZE			0x0004
+
+static volatile ipc_info_header_t *infohdr;
+static uint32_t index_in;
+static uint32_t index_out;
+static uint32_t request_tag;
+
+static uint32_t
+miniipc_next_index_in(void)
+{
+	return (index_in + 1) % infohdr->ipc_in_size;
+}
+
+static uint32_t
+miniipc_next_index_out(void)
+{
+	return (index_out + 1) % infohdr->ipc_out_size;
+}
+
+bool
+miniipc_probe(void)
+{
+	paddr_t pa;
+
+	pa = in32(MINIIPC_INFO_PTR);
+	if (pa == 0 || pa == 0xffffffff) {
+		printf("No MINI IPC pointer at 0x%x\n", MINIIPC_INFO_PTR);
+		return false;
+	}
+	infohdr = (ipc_info_header_t *)pa;
+	cache_dcbi((void *)infohdr, sizeof(*infohdr));
+
+	if (memcmp((void *)infohdr->magic, "IPC", 3) != 0) {
+		return false;
+	}
+
+	index_in = __SHIFTOUT(in32(HW_IPC_PPCMSG), HW_IPC_PPCMSG_INDEX_IN_MASK);
+	index_out = __SHIFTOUT(in32(HW_IPC_PPCMSG), HW_IPC_PPCMSG_INDEX_OUT_MASK);
+
+	miniipc_ping();
+
+	return true;
+}
+
+static int
+miniipc_sendrecv(const ipc_request_t *req_in, ipc_request_t *req_out)
+{
+	volatile ipc_request_t *req;
+	int retry, error = 0;
+
+	req = &infohdr->ipc_in[index_in];
+
+	*req = *req_in;
+	req->tag = ++request_tag;
+
+#ifdef MINIIPC_DEBUG
+	printf("IPC[%u] -> req code 0x%x tag %u args %u %u %u %u %u %u\n",
+	    index_in, req->code, req->tag,
+	    req->args[0], req->args[1], req->args[2], req->args[3],
+	    req->args[4], req->args[5]);
+#endif
+
+	cache_dcbf((void *)req, sizeof(*req));
+
+	req = &infohdr->ipc_out[index_out];
+	cache_dcbf((void *)req, sizeof(*req));
+
+	index_in = miniipc_next_index_in();
+
+	out32(HW_IPC_PPCMSG,
+	    (in32(HW_IPC_PPCMSG) & ~HW_IPC_PPCMSG_INDEX_IN_MASK) |
+	     __SHIFTIN(index_in, HW_IPC_PPCMSG_INDEX_IN_MASK));
+	out32(HW_IPC_PPCCTRL, in32(HW_IPC_PPCCTRL) | HW_IPC_PPCCTRL_OUT);
+
+	retry = 10000000;
+	while (__SHIFTOUT(in32(HW_IPC_ARMMSG), HW_IPC_ARMMSG_INDEX_OUT_MASK) ==
+	       index_out) {
+		timer_udelay(1);
+		if (--retry == 0) {
+#ifdef MINIIPC_DEBUG
+			printf("HW_IPC_PPCMSG: 0x%x\n", in32(HW_IPC_PPCMSG));
+			printf("HW_IPC_ARMMSG: 0x%x\n", in32(HW_IPC_ARMMSG));
+#endif
+			error = ETIMEDOUT;
+			break;
+		}
+	}
+
+	req = &infohdr->ipc_out[index_out];
+	cache_dcbi((void *)req, sizeof(*req));
+
+#ifdef MINIIPC_DEBUG
+	printf("IPC[%u] <- req code 0x%x tag %u args %u %u %u %u %u %u\n",
+	    index_out, req->code, req->tag,
+	    req->args[0], req->args[1], req->args[2], req->args[3],
+	    req->args[4], req->args[5]);
+#endif
+
+	*req_out = *req;
+
+#ifdef MINIIPC_DEBUG
+	printf(" DUMP OUTPUT RING\n");
+	for (int n = 0; n < infohdr->ipc_out_size; n++) {
+		req = &infohdr->ipc_out[n];
+		cache_dcbi((void *)req, sizeof(*req));
+		printf(" debug IPC[%u] <- req code 0x%x tag %u args %u %u %u %u %u %u\n",
+		    n, req->code, req->tag,
+		    req->args[0], req->args[1], req->args[2], req->args[3],
+		    req->args[4], req->args[5]);
+	}
+	printf(" END OUTPUT RING\n");
+#endif
+
+	index_out = miniipc_next_index_out();
+	out32(HW_IPC_PPCMSG,
+	    (in32(HW_IPC_PPCMSG) & ~HW_IPC_PPCMSG_INDEX_OUT_MASK) |
+	     __SHIFTIN(index_out, HW_IPC_PPCMSG_INDEX_OUT_MASK));
+
+	return error;
+}
+
+int
+miniipc_ping(void)
+{
+	ipc_request_t req = {
+		.flags = IPC_FAST,
+		.device = IPC_DEV_SYS,
+		.req = IPC_SYS_PING,
+	};
+
+	return miniipc_sendrecv(&req, &req);
+}
+
+int
+miniipc_sdmmc_ack(uint32_t *ack)
+{
+	ipc_request_t req = {
+		.flags = IPC_SLOW,
+		.device = IPC_DEV_SDMMC,
+		.req = IPC_SDMMC_ACK,
+	};
+	int error;
+
+	error = miniipc_sendrecv(&req, &req);
+	if (error != 0) {
+		*ack = 0;
+		return error;
+	}
+
+	*ack = req.args[0];
+	return 0;
+}
+
+int
+miniipc_sdmmc_state(uint32_t *state)
+{
+	ipc_request_t req = {
+		.flags = IPC_SLOW,
+		.device = IPC_DEV_SDMMC,
+		.req = IPC_SDMMC_STATE,
+	};
+	int error;
+
+	error = miniipc_sendrecv(&req, &req);
+	if (error != 0) {
+		*state = 0;
+		return error;
+	}
+
+	*state = req.args[0];
+	return 0;
+}
+
+int
+miniipc_sdmmc_size(uint32_t *size)
+{
+	ipc_request_t req = {
+		.flags = IPC_SLOW,
+		.device = IPC_DEV_SDMMC,
+		.req = IPC_SDMMC_SIZE,
+	};
+	int error;
+
+	error = miniipc_sendrecv(&req, &req);
+	if (error != 0) {
+		*size = 0;
+		return error;
+	}
+
+	*size = req.args[0];
+	return 0;
+}
+
+int
+miniipc_sdmmc_read(uint32_t start_blkno, uint32_t nblks, void *buf)
+{
+	ipc_request_t req = {
+		.flags = IPC_SLOW,
+		.device = IPC_DEV_SDMMC,
+		.req = IPC_SDMMC_READ,
+		.args = { start_blkno, nblks, (uint32_t)buf },
+	};
+	int error;
+
+	if (((uint32_t)buf & (CACHE_LINE_SIZE - 1)) != 0) {
+		cache_dcbf(buf, nblks * SDMMC_BLOCK_SIZE);
+	}
+	error = miniipc_sendrecv(&req, &req);
+	if (error != 0) {
+		return error;
+	} else if (req.args[0] != 0) {
+		printf("read block %u (nblks = %u) failed: %d\n",
+		    start_blkno, nblks, (int)req.args[0]);
+		error = EIO;
+		return error;
+	}
+	cache_dcbi(buf, nblks * SDMMC_BLOCK_SIZE);
+
+	return 0;
+}
