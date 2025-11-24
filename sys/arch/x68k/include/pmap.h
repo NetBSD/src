406257e7ca00a242f9cf880742f0cc3f8e068a99
thorpej <thorpej@NetBSD.org>
2025-11-24 16:53:01 +0000

    No need to explicitly include mmu_{30,40}.h here; x68k does not use the TT register for anything.

--- sys/arch/x68k/include/pmap.h
+++ sys/arch/x68k/include/pmap.h
@@ -1,12 +1,9 @@
-/*	$NetBSD: pmap.h,v 1.28 2025/11/16 03:11:47 tsutsui Exp $	*/
+/*	$NetBSD: pmap.h,v 1.29 2025/11/24 16:53:01 thorpej Exp $	*/
 
 #ifdef __HAVE_NEW_PMAP_68K
 #include <m68k/pmap_68k.h>
 #else
 #include <m68k/pmap_motorola.h>
 #endif /* __HAVE_NEW_PMAP_68K */
-
-#include <m68k/mmu_30.h>
-#include <m68k/mmu_40.h>
 
 #define __HAVE_MACHINE_BOOTMAP
