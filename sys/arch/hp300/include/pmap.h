3fe1080c6bbd21b732e905498d121310a62b1096
thorpej <thorpej@NetBSD.org>
2025-11-24 16:36:14 +0000

    Enable pmap_prefer() on hp300 systems to handle the 320/350 VAC.

--- sys/arch/hp300/include/pmap.h
+++ sys/arch/hp300/include/pmap.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: pmap.h,v 1.38 2025/11/24 07:00:01 thorpej Exp $	*/
+/*	$NetBSD: pmap.h,v 1.39 2025/11/24 16:36:14 thorpej Exp $	*/
 
 #ifdef __HAVE_NEW_PMAP_68K
 #include <m68k/pmap_68k.h>
@@ -20,6 +20,12 @@
  * we might as well put it to good use as the NULL segment table.
  */
 #define	NULL_SEGTAB_PA	MAXADDR
+
+/*
+ * Some hp300 systems have a virtually-addressed cache.  Enable
+ * pmap_prefer() for cache alias avoidance on those machines.
+ */
+#define	PMAP_PREFER(h, vap, sz, td)	pmap_prefer((h), (vap), (td))
 #else
 #include <m68k/pmap_motorola.h>
 #endif /* __HAVE_NEW_PMAP_68K */
