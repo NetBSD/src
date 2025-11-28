2b2d6646e83a143a9db0e437f2211dbf421f4c30
martin <martin@NetBSD.org>
2025-11-28 10:58:02 +0000

    Pull up following revision(s) (requested by nia in ticket #108):
    	sys/arch/sparc64/include/bswap.h: revision 1.3
    	sys/arch/sparc/include/bswap.h: revision 1.3
    	sys/sys/bswap.h: revision 1.20
    	sys/sys/endian.h: revision 1.38
    
    sparc: Avoid using GCC builtins for byte-swapping and endian encoding.
    
    gcc on most netbsd architectures (mostly excluding sparc and vax) can
    generate fast MD byte-swapping code. On sparc it generates a function
    call, which is very slow. We have existing inline macros for byte
    swapping, so use those instead.
    
    bswap on sparc with gcc is still non-ideal, so also reactivate the
    portable endian encoding functions we apparently haven't been using
    since the gcc2 days, rather than using bswap for that.
    
    These changes improve the speed of disk encryption on ultrasparc ii
    by almost 40% (the endian.h change being more significant in making
    that number go high).
    clang does the right thing on sparc (or at least introduces different
    bottlenecks...), so avoid this there.
    
    Thanks joerg for analysis of the problem, uwe for reviewing the patch.
    
    PR port-sparc64/59789 bswap is slow
    PR kern/59774 bearssl 32-bit AES is too slow

--- sys/arch/sparc/include/bswap.h
+++ sys/arch/sparc/include/bswap.h
@@ -1,7 +1,15 @@
-/*      $NetBSD: bswap.h,v 1.2 1999/08/21 05:39:55 simonb Exp $      */
+/*      $NetBSD: bswap.h,v 1.2.264.1 2025/11/28 10:58:02 martin Exp $      */
 
 #ifndef _MACHINE_BSWAP_H_
 #define	_MACHINE_BSWAP_H_
+
+/*
+ * GCC doesn't generate inline calls to bswapX on sparc and instead
+ * generates function calls.
+ */
+#if !defined(__clang__)
+#define __HAVE_SLOW_BSWAP_BUILTIN
+#endif
 
 #include <sys/bswap.h>
 
