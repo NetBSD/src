932a7c96edd804952801203aa6334051e0a6414c
nia <nia@NetBSD.org>
2025-11-30 17:48:16 +0000

    powerpc: Use GCC builtins for bswap.
    This doubled disk encryption performance on an (emulated) G4. Further
    improvements are possible if the GCC 2 version of the endian functions
    is used, so we might want to consider doing that on more NetBSD ports.
    
    PR port-sparc64/59789 bswap is slow

--- sys/arch/powerpc/include/bswap.h
+++ sys/arch/powerpc/include/bswap.h
@@ -1,7 +1,13 @@
-/*	$NetBSD: bswap.h,v 1.6 2006/01/31 07:51:41 dsl Exp $	*/
+/*	$NetBSD: bswap.h,v 1.7 2025/11/30 17:48:16 nia Exp $	*/
 
 #ifndef _POWERPC_BSWAP_H_
 #define _POWERPC_BSWAP_H_
+
+#if defined(__GNUC__) && !defined(__lint__)
+#define __BYTE_SWAP_U64_VARIABLE __builtin_bswap64
+#define __BYTE_SWAP_U32_VARIABLE __builtin_bswap32
+#define __BYTE_SWAP_U16_VARIABLE __builtin_bswap16
+#endif
 
 #include <sys/bswap.h>
 
