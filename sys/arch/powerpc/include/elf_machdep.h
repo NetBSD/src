9f574da3d1dcb1a57af397ed0e7d3206fce8c5b2
jkoshy <jkoshy@NetBSD.org>
2025-12-01 16:39:57 +0000

    powerpc/elf_machdep.h: Use psABI spellings and group symbols into psABI/local groups.
    The prior spellings have been kept around for backward compatibility.
    
    Permit this file to be used alongside <sys/elfdefinitions.h>.
    
    PR lib/59564

--- sys/arch/powerpc/include/elf_machdep.h
+++ sys/arch/powerpc/include/elf_machdep.h
@@ -1,29 +1,13 @@
-/*	$NetBSD: elf_machdep.h,v 1.16 2019/12/08 21:46:03 uwe Exp $	*/
+/*	$NetBSD: elf_machdep.h,v 1.17 2025/12/01 16:39:57 jkoshy Exp $	*/
 
 #ifndef _POWERPC_ELF_MACHDEP_H_
 #define _POWERPC_ELF_MACHDEP_H_
 
-#define	ELF32_MACHDEP_ENDIANNESS	ELFDATA2MSB
-#define	ELF32_MACHDEP_ID_CASES						\
-		case EM_PPC:						\
-			break;
+#if !defined(_SYS_ELFDEFINITIONS_H_)
 
-#define	ELF64_MACHDEP_ENDIANNESS	ELFDATA2MSB
-#define	ELF64_MACHDEP_ID_CASES						\
-		case EM_PPC64:						\
-			break;
-
-#define	ELF32_MACHDEP_ID	EM_PPC
-#define	ELF64_MACHDEP_ID	EM_PPC64
-
-
-#ifdef _LP64
-#define KERN_ELFSIZE		64
-#define ARCH_ELFSIZE		64	/* MD native binary size */
-#else
-#define KERN_ELFSIZE		32
-#define ARCH_ELFSIZE		32	/* MD native binary size */
-#endif
+/*
+ * psABI symbols.
+ */
 
 /* Specify the value of _GLOBAL_OFFSET_TABLE_ */
 #define	DT_PPC_GOT		DT_LOPROC
@@ -88,12 +72,12 @@
 #define	R_PPC_ADDR16_HI 	5	// #hi(S + A)
 #define	R_PPC_ADDR16_HA 	6	// #ha(S + A)
 #define	R_PPC_ADDR14 		7	// (S + A) >> 2
-#define	R_PPC_ADDR14_TAKEN 	8	// (S + A) >> 2
-#define	R_PPC_ADDR14_NTAKEN 	9	// (S + A) >> 2
+#define	R_PPC_ADDR14_BRTAKEN  	8	// (S + A) >> 2
+#define	R_PPC_ADDR14_BRNTAKEN 	9	// (S + A) >> 2
 #define	R_PPC_REL24 		10 	// (S + A - P) >> 2
 #define	R_PPC_REL14 		11	// (S + A - P) >> 2
-#define	R_PPC_REL14_TAKEN 	12	// (S + A - P) >> 2
-#define	R_PPC_REL14_NTAKEN 	13	// (S + A - P) >> 2
+#define	R_PPC_REL14_BRTAKEN 	12	// (S + A - P) >> 2
+#define	R_PPC_REL14_BRNTAKEN 	13	// (S + A - P) >> 2
 #define	R_PPC_GOT16 		14	// G + A
 #define	R_PPC_GOT16_LO 		15	// #lo(G + A)
 #define	R_PPC_GOT16_HI 		16	// #hi(G + A)
@@ -108,7 +92,7 @@
 #define	R_PPC_UADDR16 		25	// S + A
 #define	R_PPC_REL32 		26	// S + A - P
 #define	R_PPC_PLT32 		27	// L
-#define	R_PPC_PLTREL 		28	// L - P
+#define	R_PPC_PLTREL32 		28	// L - P
 #define	R_PPC_PLT16_LO 		29	// #lo(L)
 #define	R_PPC_PLT16_HI 		30	// #hi(L)
 #define	R_PPC_PLT16_HA 		31	// #ha(L)
@@ -118,6 +102,136 @@
 #define	R_PPC_SECTOFF_HI	35	// #lo(R + A)
 #define	R_PPC_SECTOFF_HA	36	// #ha(R + A)
 #define	R_PPC_ADDR30 		37	// (S + A - P) >> 2
+
+/* PPC64 relocations */
+#define R_PPC64_ADDR64		38	// S + A
+#define R_PPC64_ADDR16_HIGHER	39	// #higher(S + A)
+#define R_PPC64_ADDR16_HIGHERA	40	// #highera(S + A)
+#define R_PPC64_ADDR16_HIGHEST	41	// #highest(S + A)
+#define R_PPC64_ADDR16_HIGHESTA	42	// #highesta(S + A)
+#define R_PPC64_UADDR64		43	// S + A
+#define R_PPC64_REL64		44	// S + A - P
+#define R_PPC64_PLT64		45	// L
+#define R_PPC64_PLTREL64	46	// L - P
+#define	R_PPC64_TOC16 		47	// S + A - .TOC.
+#define	R_PPC64_TOC16_LO	48	// #lo(S + A - .TOC.)
+#define	R_PPC64_TOC16_HI	49	// #lo(S + A - .TOC.)
+#define	R_PPC64_TOC16_HA	50	// #ha(S + A - .TOC.)
+#define R_PPC64_TOC		51	// .TOC.
+#define	R_PPC64_PLTGOT16 	52	// M
+#define	R_PPC64_PLTGOT16_LO 	53	// #lo(M)
+#define	R_PPC64_PLTGOT16_HI	54	// #lo(M)
+#define	R_PPC64_PLTGOT16_HA	55	// #ha(M)
+#define	R_PPC64_ADDR16_DS	56	// (S + A) >> 2
+#define	R_PPC64_ADDR16_LO_DS 	57	// #lo(S + A) >> 2
+#define	R_PPC64_GOT16_DS 	58	// G >> 2
+#define	R_PPC64_GOT16_LO_DS 	59	// #lo(G) >> 2
+#define	R_PPC64_PLT16_LO_DS 	60	// #lo(L) >> 2
+#define	R_PPC64_SECTOFF_DS	61	// (R + A) >> 2
+#define	R_PPC64_SECTOFF_LO_DS	62	// #lo(R + A) >> 2
+#define	R_PPC64_TOC16_DS	63	// (S + A - .TOC.) >> 2
+#define	R_PPC64_TOC16_LO_DS 	64	// #lo(S + A - .TOC.) >> 2
+#define	R_PPC64_PLTGOT16_DS	65	// M >> 2
+#define	R_PPC64_PLTGOT16_LO_DS 	66	// #lo(M) >> 2
+
+/* TLS relocations */
+#define	R_PPC_TLS		67	// none
+
+#define	R_PPC_DTPMOD32		68
+#define	R_PPC_TPREL16		69	// @tprel
+#define	R_PPC_TPREL16_LO	70	// #lo(@tprel)
+#define	R_PPC_TPREL16_HI	71	// #hi(@tprel)
+#define	R_PPC_TPREL16_HA	72	// #ha(@tprel)
+#define	R_PPC_TPREL32		73	// @tprel
+#define	R_PPC_DTPREL16		74	// @got@dtprel
+#define	R_PPC_DTPREL16_LO	75	// #lo(@dtprel)
+#define	R_PPC_DTPREL16_HI	76	// #hi(@dtprel)
+#define	R_PPC_DTPREL16_HA	77	// #ha(@dtprel)
+#define	R_PPC_DTPREL32		78	// @dtprel
+
+#define	R_PPC_GOT_TLSGD16	79	// @got@tlsgd
+#define	R_PPC_GOT_TLSGD16_LO	80	// #lo(@got@tlsgd)
+#define	R_PPC_GOT_TLSGD16_HI	81	// #hi(@got@tlsgd)
+#define	R_PPC_GOT_TLSGD16_HA	82	// #ha(@got@tlsgd)
+#define	R_PPC_GOT_TLSLD16	83	// @got@tlsld
+#define	R_PPC_GOT_TLSLD16_LO	84	// #lo(@got@tlsld)
+#define	R_PPC_GOT_TLSLD16_HI	85	// #hi(@got@tlsld)
+#define	R_PPC_GOT_TLSLD16_HA	86	// #ha(@got@tlsld)
+
+#define	R_PPC_GOT_TPREL16	87	// @got@tprel
+#define	R_PPC_GOT_TPREL16_LO	88	// #lo(@got@tprel)
+#define	R_PPC_GOT_TPREL16_HI	89	// #hi(@got@tprel)
+#define	R_PPC_GOT_TPREL16_HA	90	// #ha(@got@tprel)
+#define	R_PPC_GOT_DTPREL16	91	// @got@dtprel
+#define	R_PPC_GOT_DTPREL16_LO	92	// #lo(@got@dtprel)
+#define	R_PPC_GOT_DTPREL16_HI	93	// #hi(@got@dtprel)
+#define	R_PPC_GOT_DTPREL16_HA	94	// #ha(@got@dtprel)
+#define	R_PPC_TLSGD		95
+#define	R_PPC_TLSLD		96
+
+#define	R_PPC64_TPREL16_DS	95	// @tprel
+#define	R_PPC64_TPREL16_LO_DS	96	// #lo(@tprel)
+#define	R_PPC64_TPREL16_HIGHER	97	// #higher(@tprel)
+#define	R_PPC64_TPREL16_HIGHERA	98	// #highera(@tprel)
+#define	R_PPC64_TPREL16_HIGHEST	99	// #highest(@tprel)
+#define	R_PPC64_TPREL16_HIGHESTA 100	// #highesta(@tprel)
+
+#define	R_PPC64_DTPREL16_DS	101	// @dtprel
+#define	R_PPC64_DTPREL16_LO_DS	102	// #lo(@dtprel)
+#define	R_PPC64_DTPREL16_HIGHER	103	// #higher(@dtprel)
+#define	R_PPC64_DTPREL16_HIGHERA 104	// #highera(@dtprel)
+#define	R_PPC64_DTPREL16_HIGHEST 105	// #highest(@dtprel)
+#define	R_PPC64_DTPREL16_HIGHESTA 106	// #highesta(@dtprel)
+
+/* Indirect-function support */
+#define	R_PPC64_IRELATIVE	248
+#define	R_PPC_IRELATIVE		248     // GNU spelling.
+
+/* Used for the secure-plt PIC code sequences */
+#define	R_PPC64_REL16		249	// S + A - P
+#define	R_PPC64_REL16_LO	250	// #lo(S + A - P)
+#define	R_PPC64_REL16_HI	251	// #hi(S + A - P)
+#define	R_PPC64_REL16_HA	252	// #ha(S + A - P)
+
+#endif /* !defined(_SYS_ELFDEFINITIONS_H_) */
+
+/*
+ * Local symbols.
+ */
+
+#define	ELF32_MACHDEP_ENDIANNESS	ELFDATA2MSB
+#define	ELF32_MACHDEP_ID_CASES						\
+		case EM_PPC:						\
+			break;
+
+#define	ELF64_MACHDEP_ENDIANNESS	ELFDATA2MSB
+#define	ELF64_MACHDEP_ID_CASES						\
+		case EM_PPC64:						\
+			break;
+
+#define	ELF32_MACHDEP_ID	EM_PPC
+#define	ELF64_MACHDEP_ID	EM_PPC64
+
+
+#ifdef _LP64
+#define KERN_ELFSIZE		64
+#define ARCH_ELFSIZE		64	/* MD native binary size */
+#else
+#define KERN_ELFSIZE		32
+#define ARCH_ELFSIZE		32	/* MD native binary size */
+#endif
+
+/*
+ * Local spellings of psABI constants.
+ */
+#define	R_PPC_ADDR14_TAKEN 	8	// (S + A) >> 2
+#define	R_PPC_ADDR14_NTAKEN 	9	// (S + A) >> 2
+
+#define	R_PPC_REL14_TAKEN 	12	// (S + A - P) >> 2
+#define	R_PPC_REL14_NTAKEN 	13	// (S + A - P) >> 2
+
+#define	R_PPC_PLTREL 		28	// L - P
+
 /* PPC64 relocations */
 #define R_PPC_ADDR64		38	// S + A
 #define R_PPC_ADDR16_HIGHER	39	// #higher(S + A)
@@ -149,42 +263,12 @@
 #define	R_PPC_PLTGOT16_DS	65	// M >> 2
 #define	R_PPC_PLTGOT16_LO_DS 	66	// #lo(M) >> 2
 
-/* TLS relocations */
-#define	R_PPC_TLS		67	// none
-
 #define	R_PPC_DTPMOD		68
-#define	R_PPC_TPREL16		69	// @tprel
-#define	R_PPC_TPREL16_LO	70	// #lo(@tprel)
-#define	R_PPC_TPREL16_HI	71	// #hi(@tprel)
-#define	R_PPC_TPREL16_HA	72	// #ha(@tprel)
+
 #define	R_PPC_TPREL		73	// @tprel
-#define	R_PPC_DTPREL16		74	// @got@dtprel
-#define	R_PPC_DTPREL16_LO	75	// #lo(@dtprel)
-#define	R_PPC_DTPREL16_HI	76	// #hi(@dtprel)
-#define	R_PPC_DTPREL16_HA	77	// #ha(@dtprel)
+
 #define	R_PPC_DTPREL		78	// @dtprel
 
-#define	R_PPC_GOT_TLSGD16	79	// @got@tlsgd
-#define	R_PPC_GOT_TLSGD16_LO	80	// #lo(@got@tlsgd)
-#define	R_PPC_GOT_TLSGD16_HI	81	// #hi(@got@tlsgd)
-#define	R_PPC_GOT_TLSGD16_HA	82	// #ha(@got@tlsgd)
-#define	R_PPC_GOT_TLSLD16	83	// @got@tlsld
-#define	R_PPC_GOT_TLSLD16_LO	84	// #lo(@got@tlsld)
-#define	R_PPC_GOT_TLSLD16_HI	85	// #hi(@got@tlsld)
-#define	R_PPC_GOT_TLSLD16_HA	86	// #ha(@got@tlsld)
-
-#define	R_PPC_GOT_TPREL16	87	// @got@tprel
-#define	R_PPC_GOT_TPREL16_LO	88	// #lo(@got@tprel)
-#define	R_PPC_GOT_TPREL16_HI	89	// #hi(@got@tprel)
-#define	R_PPC_GOT_TPREL16_HA	90	// #ha(@got@tprel)
-#define	R_PPC_GOT_DTPREL16	91	// @got@dtprel
-#define	R_PPC_GOT_DTPREL16_LO	92	// #lo(@got@dtprel)
-#define	R_PPC_GOT_DTPREL16_HI	93	// #hi(@got@dtprel)
-#define	R_PPC_GOT_DTPREL16_HA	94	// #ha(@got@dtprel)
-#define	R_PPC_TLSGD		95
-#define	R_PPC_TLSLD		96
-
-/* PPC64 relocations */
 #define	R_PPC_TPREL16_DS	95	// @tprel
 #define	R_PPC_TPREL16_LO_DS	96	// #lo(@tprel)
 #define	R_PPC_TPREL16_HIGHER	97	// #higher(@tprel)
@@ -198,9 +282,6 @@
 #define	R_PPC_DTPREL16_HIGHERA	104	// #highera(@dtprel)
 #define	R_PPC_DTPREL16_HIGHEST	105	// #highest(@dtprel)
 #define	R_PPC_DTPREL16_HIGHESTA	106	// #highesta(@dtprel)
-
-/* Indirect-function support */
-#define	R_PPC_IRELATIVE		248
 
 /* Used for the secure-plt PIC code sequences */
 #define	R_PPC_REL16		249	// S + A - P
