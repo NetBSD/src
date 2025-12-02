ecf3e1f64ed36b201bb85b04ed8e0f6c460aa78f
nia <nia@NetBSD.org>
2025-12-02 15:40:19 +0000

    We can use __builtin_bswapX on sparc when using clang, so do so.
    PR port-sparc64/59789 bswap is slow

--- sys/arch/sparc/include/bswap.h
+++ sys/arch/sparc/include/bswap.h
@@ -1,4 +1,4 @@
-/*      $NetBSD: bswap.h,v 1.3 2025/11/26 22:25:10 nia Exp $      */
+/*      $NetBSD: bswap.h,v 1.4 2025/12/02 15:40:19 nia Exp $      */
 
 #ifndef _MACHINE_BSWAP_H_
 #define	_MACHINE_BSWAP_H_
@@ -9,6 +9,10 @@
  */
 #if !defined(__clang__)
 #define __HAVE_SLOW_BSWAP_BUILTIN
+#else
+#define __BYTE_SWAP_U64_VARIABLE __builtin_bswap64
+#define __BYTE_SWAP_U32_VARIABLE __builtin_bswap32
+#define __BYTE_SWAP_U16_VARIABLE __builtin_bswap16
 #endif
 
 #include <sys/bswap.h>
