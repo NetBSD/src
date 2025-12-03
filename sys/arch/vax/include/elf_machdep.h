7ec39284f935268e050e9058460e012ad3afe5be
jkoshy <jkoshy@NetBSD.org>
2025-12-03 21:36:14 +0000

    vax/elf_machdep.h: permit usage with <sys/elfdefinitions.h>.
    PR lib/59564

--- sys/arch/vax/include/elf_machdep.h
+++ sys/arch/vax/include/elf_machdep.h
@@ -1,5 +1,23 @@
-/*	$NetBSD: elf_machdep.h,v 1.6 2017/11/06 03:47:48 christos Exp $	*/
+/*	$NetBSD: elf_machdep.h,v 1.7 2025/12/03 21:36:14 jkoshy Exp $	*/
 
+#if !defined(_SYS_ELFDEFINITIONS_H_)
+/* VAX relocations */
+#define	R_VAX_NONE	0
+#define	R_VAX_32	1	/* S + A */
+#define	R_VAX_16	2
+#define	R_VAX_8		3
+#define	R_VAX_PC32	4	/* S + A - PC */
+#define	R_VAX_PC16	5
+#define	R_VAX_PC8	6
+#define	R_VAX_COPY	19
+#define	R_VAX_GLOB_DAT	20
+#define	R_VAX_JMP_SLOT	21
+#define R_VAX_RELATIVE	22	/* S + A + D */
+#endif /* !defined(_SYS_ELFDEFINITIONS_H_) */
+
+/*
+ * Local symbols.
+ */
 #define	ELF32_MACHDEP_ENDIANNESS	ELFDATA2LSB
 #define	ELF32_MACHDEP_ID_CASES						\
 		case EM_VAX:						\
@@ -13,18 +31,5 @@
 
 #define	KERN_ELFSIZE		32
 #define ARCH_ELFSIZE		32	/* MD native binary size */
-
-/* VAX relocations */
-#define	R_VAX_NONE	0
-#define	R_VAX_32	1	/* S + A */
-#define	R_VAX_16	2
-#define	R_VAX_8		3
-#define	R_VAX_PC32	4	/* S + A - PC */
-#define	R_VAX_PC16	5
-#define	R_VAX_PC8	6
-#define	R_VAX_COPY	19
-#define	R_VAX_GLOB_DAT	20
-#define	R_VAX_JMP_SLOT	21
-#define R_VAX_RELATIVE	22	/* S + A + D */
 
 #define	R_TYPE(name)	__CONCAT(R_VAX_,name)
