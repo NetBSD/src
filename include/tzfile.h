82169cdafa2a35b0344af8594da491812b67bebf
christos <christos@NetBSD.org>
2025-12-18 14:01:49 +0000

    reduce diff with upstream (2025c)

--- include/tzfile.h
+++ include/tzfile.h
@@ -1,4 +1,6 @@
-/*	$NetBSD: tzfile.h,v 1.14 2025/01/31 18:23:52 christos Exp $	*/
+/*	$NetBSD: tzfile.h,v 1.15 2025/12/18 14:01:49 christos Exp $	*/
+
+/* Layout and location of TZif files.  */
 
 #ifndef _TZFILE_H_
 #define _TZFILE_H_
@@ -21,15 +23,15 @@
 */
 
 #ifndef TZDIR		/* Time zone object file directory */
-# define TZDIR		"/usr/share/zoneinfo"
+# define TZDIR "/usr/share/zoneinfo"
 #endif /* !defined TZDIR */
 
 #ifndef TZDEFAULT
-# define TZDEFAULT	"/etc/localtime"
+# define TZDEFAULT "/etc/localtime"
 #endif /* !defined TZDEFAULT */
 
 #ifndef TZDEFRULES
-# define TZDEFRULES	"posixrules"
+# define TZDEFRULES "posixrules"
 #endif /* !defined TZDEFRULES */
 
 
@@ -39,11 +41,11 @@
 ** Each file begins with. . .
 */
 
-#define	TZ_MAGIC	"TZif"
+#define TZ_MAGIC "TZif"
 
 struct tzhead {
 	char	tzh_magic[4];		/* TZ_MAGIC */
-	char	tzh_version[1];		/* '\0' or '2' or '3' as of 2013 */
+	char	tzh_version[1];		/* '\0' or '2'-'4' as of 2021 */
 	char	tzh_reserved[15];	/* reserved; must be zero */
 	char	tzh_ttisutcnt[4];	/* coded number of trans. time flags */
 	char	tzh_ttisstdcnt[4];	/* coded number of trans. time flags */
@@ -83,14 +85,16 @@ struct tzhead {
 ** If tzh_version is '2' or greater, the above is followed by a second instance
 ** of tzhead and a second instance of the data in which each coded transition
 ** time uses 8 rather than 4 chars,
-** then a POSIX-TZ-environment-variable-style string for use in handling
+** then a POSIX.1-2017 proleptic TZ string for use in handling
 ** instants after the last transition time stored in the file
 ** (with nothing between the newlines if there is no POSIX.1-2017
 ** representation for such instants).
 **
-** If tz_version is '3' or greater, the above is extended as follows.
+** If tz_version is '3' or greater, the TZ string can be any POSIX.1-2024
+** proleptic TZ string, which means the above is extended as follows.
 ** First, the TZ string's hour offset may range from -167
-** through 167 as compared to the POSIX-required 0 through 24.
+** through 167 as compared to the range 0 through 24 required
+** by POSIX.1-2017 and earlier.
 ** Second, its DST start time may be January 1 at 00:00 and its stop
 ** time December 31 at 24:00 plus the difference between DST and
 ** standard time, indicating DST all year.
