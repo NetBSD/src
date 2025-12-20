87205b3075ee5cd76d93e93e884ca5b9a3740bc4
jkoshy <jkoshy@NetBSD.org>
2025-12-20 13:00:51 +0000

    usr.sbin/lockstat: Prefer <elf.h> in userland code.

--- usr.sbin/lockstat/elf32.c
+++ usr.sbin/lockstat/elf32.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: elf32.c,v 1.10 2013/10/19 17:10:17 christos Exp $	*/
+/*	$NetBSD: elf32.c,v 1.11 2025/12/20 13:00:51 jkoshy Exp $	*/
 
 /*-
  * Copyright (c) 2006 The NetBSD Foundation, Inc.
@@ -65,7 +65,7 @@
 
 #include <sys/cdefs.h>
 #if !defined(lint)
-__RCSID("$NetBSD: elf32.c,v 1.10 2013/10/19 17:10:17 christos Exp $");
+__RCSID("$NetBSD: elf32.c,v 1.11 2025/12/20 13:00:51 jkoshy Exp $");
 #endif
 
 #ifndef ELFSIZE
@@ -73,11 +73,11 @@ __RCSID("$NetBSD: elf32.c,v 1.10 2013/10/19 17:10:17 christos Exp $");
 #endif
 
 #include <sys/param.h>
-#include <sys/exec_elf.h>
 #include <sys/queue.h>
 
 #include <dev/lockstat.h>
 
+#include <elf.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
