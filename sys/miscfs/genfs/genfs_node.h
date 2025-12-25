cff47d6819f80c781c6ebc824ba663151bd42350
nia <nia@NetBSD.org>
2025-12-25 09:08:28 +0000

    Remove the advertising clause from Chuck's license.
    chs was ok with this, thanks!

--- sys/miscfs/genfs/genfs_node.h
+++ sys/miscfs/genfs/genfs_node.h
@@ -1,4 +1,4 @@
-/* $NetBSD: genfs_node.h,v 1.24 2020/03/14 21:47:41 ad Exp $ */
+/* $NetBSD: genfs_node.h,v 1.25 2025/12/25 09:08:28 nia Exp $ */
 
 /*
  * Copyright (c) 2001 Chuck Silvers.
@@ -12,10 +12,7 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *      This product includes software developed by Chuck Silvers.
- * 4. The name of the author may not be used to endorse or promote products
+ * 3. The name of the author may not be used to endorse or promote products
  *    derived from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
