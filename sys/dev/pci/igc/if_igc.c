a36344aba954a0ca04b0096dbd661168a3349dd5
riastradh <riastradh@NetBSD.org>
2026-01-03 20:17:16 +0000

    igc(4): Fix some attach error branches.
    Might explain the crash on igc(4) attach failure in:
    
    PR kern/59876: igc0: unable to initialize

--- sys/dev/pci/igc/if_igc.c
+++ sys/dev/pci/igc/if_igc.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: if_igc.c,v 1.23 2025/10/21 15:20:34 pgoyette Exp $	*/
+/*	$NetBSD: if_igc.c,v 1.24 2026/01/03 20:17:16 riastradh Exp $	*/
 /*	$OpenBSD: if_igc.c,v 1.13 2023/04/28 10:18:57 bluhm Exp $	*/
 /*-
  * SPDX-License-Identifier: BSD-2-Clause
@@ -30,7 +30,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: if_igc.c,v 1.23 2025/10/21 15:20:34 pgoyette Exp $");
+__KERNEL_RCSID(0, "$NetBSD: if_igc.c,v 1.24 2026/01/03 20:17:16 riastradh Exp $");
 
 #ifdef _KERNEL_OPT
 #include "opt_if_igc.h"
@@ -388,7 +388,7 @@ igc_attach(device_t parent, device_t self, void *aux)
 	if (igc_allocate_pci_resources(sc)) {
 		aprint_error_dev(sc->sc_dev,
 		    "unable to allocate PCI resources\n");
-		goto err_pci;
+		goto err;
 	}
 
 	if (igc_allocate_interrupts(sc)) {
@@ -489,11 +489,12 @@ igc_attach(device_t parent, device_t self, void *aux)
 
  err_late:
 	igc_release_hw_control(sc);
+	kmem_free(sc->mta, IGC_MTA_LEN);
  err_alloc_intr:
 	igc_free_interrupts(sc);
  err_pci:
 	igc_free_pci_resources(sc);
-	kmem_free(sc->mta, IGC_MTA_LEN);
+ err:	return;
 }
 
 /*********************************************************************
