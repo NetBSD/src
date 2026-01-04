c0c9c49543b0a96ca839b07a4ec674bb460772a3
riastradh <riastradh@NetBSD.org>
2026-01-04 03:16:22 +0000

    subr_disk_mbr.c: Sort includes.
    No functional change intended.  Preparation for:
    
    PR kern/58378: Kernel error code origination lacks dtrace probes

--- sys/kern/subr_disk_mbr.c
+++ sys/kern/subr_disk_mbr.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: subr_disk_mbr.c,v 1.59 2025/02/08 10:49:45 mlelstv Exp $	*/
+/*	$NetBSD: subr_disk_mbr.c,v 1.60 2026/01/04 03:16:22 riastradh Exp $	*/
 
 /*
  * Copyright (c) 1982, 1986, 1988 Regents of the University of California.
@@ -54,7 +54,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: subr_disk_mbr.c,v 1.59 2025/02/08 10:49:45 mlelstv Exp $");
+__KERNEL_RCSID(0, "$NetBSD: subr_disk_mbr.c,v 1.60 2026/01/04 03:16:22 riastradh Exp $");
 
 #ifdef _KERNEL_OPT
 #include "opt_mbr.h"
@@ -62,20 +62,22 @@ __KERNEL_RCSID(0, "$NetBSD: subr_disk_mbr.c,v 1.59 2025/02/08 10:49:45 mlelstv E
 #endif /* _KERNEL_OPT */
 
 #include <sys/param.h>
-#include <sys/systm.h>
-#include <sys/buf.h>
-#include <sys/bootblock.h>
-#include <sys/disklabel.h>
-#include <sys/disk.h>
-#include <sys/syslog.h>
-#include <sys/vnode.h>
-#include <sys/fcntl.h>
-#include <sys/conf.h>
-#include <sys/cdio.h>
-#include <sys/dkbad.h>
-#include <fs/udf/ecma167-udf.h>
+#include <sys/types.h>
 
+#include <sys/bootblock.h>
+#include <sys/buf.h>
+#include <sys/cdio.h>
+#include <sys/conf.h>
+#include <sys/disk.h>
+#include <sys/disklabel.h>
+#include <sys/dkbad.h>
+#include <sys/fcntl.h>
 #include <sys/kauth.h>
+#include <sys/syslog.h>
+#include <sys/systm.h>
+#include <sys/vnode.h>
+
+#include <fs/udf/ecma167-udf.h>
 
 typedef struct mbr_partition mbr_partition_t;
 
