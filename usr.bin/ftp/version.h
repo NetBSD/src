2a6d3f5e2ac646c230c4c56858469b22b62157e4
martin <martin@NetBSD.org>
2026-01-22 19:56:10 +0000

    Pull up following revision(s) (requested by lukem in ticket #151):
    	usr.bin/ftp/version.h: revision 1.100
    	usr.bin/ftp/cmdtab.c: revision 1.54
    	usr.bin/ftp/ftp.1: revision 1.161
    
    ftp: connect is a synonym for open
    
    As we have disconnect as a synonym for close,
    add connect as a synonym for open.
    
    Suggested by Colby Russell in email.

--- usr.bin/ftp/version.h
+++ usr.bin/ftp/version.h
@@ -1,7 +1,7 @@
-/*	$NetBSD: version.h,v 1.99 2024/11/29 04:31:57 lukem Exp $	*/
+/*	$NetBSD: version.h,v 1.99.2.1 2026/01/22 19:56:10 martin Exp $	*/
 
 /*-
- * Copyright (c) 1999-2024 The NetBSD Foundation, Inc.
+ * Copyright (c) 1999-2025 The NetBSD Foundation, Inc.
  * All rights reserved.
  *
  * This code is derived from software contributed to The NetBSD Foundation
@@ -34,5 +34,5 @@
 #endif
 
 #ifndef FTP_VERSION
-#define	FTP_VERSION	"20241129"
+#define	FTP_VERSION	"20251028"
 #endif
