507e926fd1066f0e9d2561b529b1d6167b5a90f3
martin <martin@NetBSD.org>
2026-01-22 18:57:51 +0000

    Pull up the following revisions, requested by mrg in ticket #1996
    	external/gpl2/gmake/dist/ar.c                   up to 1.1.1.1
    	external/gpl2/gmake/dist/arscan.c               up to 1.4
    	external/gpl2/gmake/dist/configure              up to 1.3
    	external/gpl2/gmake/dist/getopt.c               up to 1.1.1.1
    	external/gpl2/gmake/dist/getopt.h               up to 1.1.1.1
    	external/gpl2/gmake/dist/getopt1.c              up to 1.1.1.1
    	external/gpl2/gmake/dist/job.c                  up to 1.4
    	external/gpl2/gmake/dist/main.c                 up to 1.2
    	external/gpl2/gmake/dist/make.h                 up to 1.4
    	external/gpl2/gmake/dist/read.c                 up to 1.4
    	external/gpl2/gmake/dist/config/config.guess    up to 1.5
    	external/gpl2/gmake/dist/doc/Makefile.in        up to 1.2
    	external/gpl2/gmake/dist/glob/fnmatch.c         up to 1.1.1.1
    	external/gpl2/groff/groff2netbsd                up to 1.2
    	external/gpl2/groff/bin/troff/Makefile          up to 1.4
    	external/gpl2/groff/dist/src/libs/libgroff/getopt.c up to 1.1.1.1
    	external/gpl2/groff/dist/src/roff/groff/groff.cpp up to 1.2
    	external/gpl2/groff/dist/src/roff/troff/input.cpp up to 1.5
    	external/gpl2/groff/dist/tmac/doc-syms          up to 1.9
    	external/gpl2/groff/dist/tmac/groff_mdoc.man    up to 1.10
    	external/gpl2/groff/tmac/mdoc.local             up to 1.12
    
    Allow building with modern compilers

--- external/gpl2/gmake/dist/read.c
+++ external/gpl2/gmake/dist/read.c
@@ -2937,7 +2937,7 @@ tilde_expand (char *name)
 #ifndef VMS
   if (name[1] == '/' || name[1] == '\0')
     {
-      extern char *getenv ();
+      extern char *getenv (const char *);
       char *home_dir;
       int is_variable;
 
@@ -3015,7 +3015,7 @@ tilde_expand (char *name)
 struct nameseq *
 multi_glob (struct nameseq *chain, unsigned int size)
 {
-  extern void dir_setup_glob ();
+  extern void dir_setup_glob (glob_t *);
   register struct nameseq *new = 0;
   register struct nameseq *old;
   struct nameseq *nexto;
