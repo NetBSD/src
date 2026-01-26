0639a67d0ef032e3228cd3a13f35e2dc27137c43
kre <kre@NetBSD.org>
2026-01-26 05:41:05 +0000

    Clean up this test a bit.
    1. Use printf instead of echo
    2. Use $( ) instead of ``
    3. Check exit status of commands being tested
    4. Misc other stuff (use -ne rather than != when comparing numbers, ...)

--- tests/bin/dd/t_dd.sh
+++ tests/bin/dd/t_dd.sh
@@ -1,4 +1,4 @@
-# $NetBSD: t_dd.sh,v 1.3 2022/05/24 21:42:37 rillig Exp $
+# $NetBSD: t_dd.sh,v 1.4 2026/01/26 05:41:05 kre Exp $
 #
 # Copyright (c) 2007 The NetBSD Foundation, Inc.
 # All rights reserved.
@@ -28,9 +28,9 @@
 test_dd_length() {
 	result=$1
 	cmd=$2
-	set -- x `eval $cmd | wc -c`
-	res=$2
-	if [ x"$res" != x"$result" ]; then
+	res=$( eval "$cmd" | wc -c ) || atf_fail "Exit status $? from: $cmd"
+	res=$(( $res ))
+	if [ "$res" -ne "$result" ]; then
 		atf_fail "Expected $result bytes of output, got $res: $cmd"
 	fi
 }
@@ -53,8 +53,8 @@ length_body() {
 }
 
 test_dd_io() {
-	res="`echo -n "$2" | eval $1`"
-        if [ x"$res" != x"$3" ]; then
+	res=$(printf %s "$2" | eval "$1") || atf_fail "Non zero exit status ($?) from: $cmd"
+        if [ "$res" != "$3" ]; then
 		atf_fail "Expected \"$3\", got \"$res\": $1"
 	fi
 }
@@ -63,8 +63,8 @@ allbits1="\000\001\002\003\004\005\006\007\010\011\012\013\014\015\016\017\020\0
 
 ebcdicbits1="\000\001\002\003\067\055\056\057\026\005\045\013\014\015\016\017\020\021\022\023\074\075\062\046\030\031\077\047\034\035\036\037\100\132\177\173\133\154\120\175\115\135\134\116\153\140\113\141\360\361\362\363\364\365\366\367\370\371\172\136\114\176\156\157\174\301\302\303\304\305\306\307\310\311\321\322\323\324\325\326\327\330\331\342\343\344\345\346\347\350\351\255\340\275\232\155\171\201\202\203\204\205\206\207\210\211\221\222\223\224\225\226\227\230\231\242\243\244\245\246\247\250\251\300\117\320\137\007\040\041\042\043\044\025\006\027\050\051\052\053\054\011\012\033\060\061\032\063\064\065\066\010\070\071\072\073\004\024\076\341\101\102\103\104\105\106\107\110\111\121\122\123\124\125\126\127\130\131\142\143\144\145\146\147\150\151\160\161\162\163\164\165\166\167\170\200\212\213\214\215\216\217\220\152\233\234\235\236\237\240\252\253\254\112\256\257\260\261\262\263\264\265\266\267\270\271\272\273\274\241\276\277\312\313\314\315\316\317\332\333\334\335\336\337\352\353\354\355\356\357\372\373\374\375\376\377"
 
-allvisbits=`echo -n "$allbits1" | unvis | vis`
-ebcdicvisbits=`echo -n "$ebcdicbits1" | unvis | vis`
+allvisbits=$(printf %s "$allbits1" | unvis | vis)
+ebcdicvisbits=$(printf %s "$ebcdicbits1" | unvis | vis)
 
 atf_test_case io
 io_head() {
@@ -107,14 +107,14 @@ seek_head() {
 }
 
 seek_body() {
-	echo TEST1234 > testfile
+	printf TEST1234\\n > testfile
 	atf_check -s exit:0 -e ignore \
 	    dd if=/dev/zero of=testfile seek=1 bs=8k count=1
 	atf_check -s exit:0 -e ignore -o match:'^TEST1234$' dd if=testfile
 	eval $(stat -s testfile)
 	atf_check_equal $st_size $((2*8192))
 
-	echo -n TEST1234 > tf2
+	printf TEST1234 > tf2
 	atf_check -s exit:0 -e ignore -x \
 	    'dd bs=4 if=/dev/zero count=1 | tr \\0 \\n | dd of=tf2 bs=4 seek=1'
 	atf_check -s exit:0 -e ignore -o match:'^TEST$' dd if=tf2
@@ -128,7 +128,7 @@ swab_head() {
 }
 
 swab_body() {
-	echo -n abcdefgh > testfile
+	printf abcdefgh > testfile
 
 	test_swab() {
 		atf_check -o inline:"$1" \
