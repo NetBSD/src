e7913bf9291c6c406700fa776c61c8488c5fc065
christos <christos@NetBSD.org>
2026-01-28 13:58:36 +0000

    Can't use -1 for the recursion_guard thread id, because pthread_equal SEGV's trying to verify the thread magic.

--- crypto/external/apache2/openssl/dist/crypto/initthread.c
+++ crypto/external/apache2/openssl/dist/crypto/initthread.c
@@ -200,7 +200,7 @@ static void init_thread_destructor(void *hands)
 }
 
 static CRYPTO_ONCE ossl_init_thread_runonce = CRYPTO_ONCE_STATIC_INIT;
-static CRYPTO_THREAD_ID recursion_guard = (CRYPTO_THREAD_ID)-1;
+static CRYPTO_THREAD_ID recursion_guard = (CRYPTO_THREAD_ID)0;
 
 DEFINE_RUN_ONCE_STATIC(ossl_init_thread_once)
 {
