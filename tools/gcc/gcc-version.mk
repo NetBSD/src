fdc5d1b0e64bd6d3fbaf15f2196a4996dd3e56e4
mrg <mrg@NetBSD.org>
2026-02-02 08:36:48 +0000

    belated bump the GCC 12 version.
    should have done this closer to:
       https://mail-index.netbsd.org/source-changes/2026/01/19/msg160286.html

--- tools/gcc/gcc-version.mk
+++ tools/gcc/gcc-version.mk
@@ -1,9 +1,9 @@
-#	$NetBSD: gcc-version.mk,v 1.30 2026/01/19 22:02:23 mrg Exp $
+#	$NetBSD: gcc-version.mk,v 1.31 2026/02/02 08:36:48 mrg Exp $
 
 # common location for tools and native build
 
 .if ${HAVE_GCC} == 12
-NETBSD_GCC_VERSION=nb1 20250721
+NETBSD_GCC_VERSION=nb2 20260119
 .endif
 .if ${HAVE_GCC} == 14
 NETBSD_GCC_VERSION=nb2 20260118
