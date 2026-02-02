b4186afa08badfe44c9b3f5c792a26e4180190b8
martin <martin@NetBSD.org>
2026-02-02 20:31:18 +0000

    Apply patch, requests by mrg in ticket #181:
    	tools/gcc/gcc-version.mk	(apply patch)
    
    bump gcc version

--- tools/gcc/gcc-version.mk
+++ tools/gcc/gcc-version.mk
@@ -1,10 +1,7 @@
-#	$NetBSD: gcc-version.mk,v 1.28 2025/07/21 05:27:09 mrg Exp $
+#	$NetBSD: gcc-version.mk,v 1.28.2.1 2026/02/02 20:31:18 martin Exp $
 
 # common location for tools and native build
 
-.if ${HAVE_GCC} == 10
-NETBSD_GCC_VERSION=nb3 20231008
-.endif
 .if ${HAVE_GCC} == 12
-NETBSD_GCC_VERSION=nb1 20250721
+NETBSD_GCC_VERSION=nb1 20260119
 .endif
