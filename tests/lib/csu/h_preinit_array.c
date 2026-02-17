3a0938993df29fbfe62f37cd0c9755cd0403a18a
skrll <skrll@NetBSD.org>
2026-02-17 07:12:54 +0000

    Test more than one entry in .preinit_array

--- tests/lib/csu/h_preinit_array.c
+++ tests/lib/csu/h_preinit_array.c
@@ -1,4 +1,5 @@
 static int x = 1;
+static int y = 1;
 
 static void
 foo(void)
@@ -6,11 +7,19 @@ foo(void)
 	x--;
 }
 
+static void
+bar(void)
+{
+	y--;
+}
+
 static void (*fp) (void) __attribute__((__section__(".preinit_array"), __used__)) =
     foo;
+static void (*bp) (void) __attribute__((__section__(".preinit_array"), __used__)) =
+    bar;
 
 int
 main(void)
 {
-	return x;
+	return x + y;
 }
