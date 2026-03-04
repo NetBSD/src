9b0cc6488089c9e8df07793c59e5fcece83fe815
martin <martin@NetBSD.org>
2026-03-04 19:04:51 +0000

    Pull up following revision(s) (requested by lukem in ticket #1237):
    	usr.bin/ftp/ftp_var.h: revision 1.90
    	usr.bin/ftp/util.c: revision 1.170
    	usr.bin/ftp/version.h: revision 1.102
    	usr.bin/ftp/ftp.c: revision 1.180
    	usr.bin/ftp/fetch.c: revision 1.243
    
    ftp: limit I/O transfers to 128 KiB
    
    Change the default/maximum I/O transfer size to 128 KiB,
    instead of relying upon the socket buffer size.
    
    Reworked change proposed by Michael van Elst.
    
    Use more constants instead of magic values.
    
    Bump version to 20260115.
    
    Part of PR bin/59865.

--- usr.bin/ftp/version.h
+++ usr.bin/ftp/version.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: version.h,v 1.95.2.5 2026/01/22 20:01:32 martin Exp $	*/
+/*	$NetBSD: version.h,v 1.95.2.6 2026/03/04 19:04:51 martin Exp $	*/
 
 /*-
  * Copyright (c) 1999-2025 The NetBSD Foundation, Inc.
@@ -34,5 +34,5 @@
 #endif
 
 #ifndef FTP_VERSION
-#define	FTP_VERSION	"20251206"
+#define	FTP_VERSION	"20260115"
 #endif
