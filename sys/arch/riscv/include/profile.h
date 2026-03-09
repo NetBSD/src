ad5e3570437a03e8f283567bd2a44fa1351f373f
skrll <skrll@NetBSD.org>
2026-03-09 20:26:36 +0000

    risc-v: Add profiling support
    PR/59301 riscv: missing MKPROFILE=yes support
     CVS: ----------------------------------------------------------------------

--- sys/arch/riscv/include/profile.h
+++ sys/arch/riscv/include/profile.h
@@ -1,4 +1,4 @@
-/* $NetBSD: profile.h,v 1.1 2014/09/19 17:36:26 matt Exp $ */
+/* $NetBSD: profile.h,v 1.2 2026/03/09 20:26:36 skrll Exp $ */
 
 /*-
  * Copyright (c) 2014 The NetBSD Foundation, Inc.
@@ -32,60 +32,87 @@
 #ifndef _RISCV_PROFILE_H_
 #define _RISCV_PROFILE_H_
 
-#define	_MCOUNT_DECL void _mcount
+#include <machine/sysreg.h>
 
-/*
- * Cannot implement mcount in C as GCC will trash the ip register when it
- * pushes a trapframe. Pity we cannot insert assembly before the function
- * prologue.
- */
+#define	_MCOUNT_DECL void mcount
 
-#define MCOUNT_ASM_NAME "__mcount"
+#define MCOUNT_ASM_NAME "_mcount"
 #define	PLTSYM
 
-#if 0
-#define	MCOUNT								\
-	__asm(".text");							\
-	__asm(".align	0");						\
-	__asm(".type	" MCOUNT_ASM_NAME ",@function");		\
-	__asm(".global	" MCOUNT_ASM_NAME);				\
-	__asm(MCOUNT_ASM_NAME ":");					\
+#ifdef _LP64
+#define	MCOUNT	MCOUNT_ARCH("8", "sd", "ld")
+#else
+#define	MCOUNT	MCOUNT_ARCH("4", "sw", "lw")
+#endif
+
+#ifdef __PIC__
+#define	_PLT	"@plt"
+#else
+#define	_PLT	/* nothing */	
+#endif
+
+
+#define	MCOUNT_ARCH(_SZREG, _REG_S, _REG_L)	__asm(			\
+"	.text\n"							\
+"	.align	2\n"							\
+"	.type	" MCOUNT_ASM_NAME ",@function\n"			\
+"	.global	" MCOUNT_ASM_NAME "\n"					\
+MCOUNT_ASM_NAME ":\n"							\
 	/*								\
-	 * Preserve registers that are trashed during mcount		\
+	 * Preserve registers that could be trashed during mcount, i.e.	\
+	 * caller saved registers.					\
 	 */								\
-	__asm("sub	sp, sp, #80");					\
-	__asm("stp	x29, x30, [sp, #64]");				\
-	__asm("add	x29, sp, #64");					\
-	__asm("stp	x0, x1, [x29, #0]");				\
-	__asm("stp	x2, x3, [x29, #16]");				\
-	__asm("stp	x4, x5, [x29, #32]");				\
-	__asm("stp	x6, x7, [x29, #48]");				\
-	/*								\
-	 * find the return address for mcount,				\
-	 * and the return address for mcount's caller.			\
-	 *								\
-	 * frompcindex = pc pushed by call into self.			\
-	 */								\
-	__asm("mov	x0, x19");					\
-	/*								\
-	 * selfpc = pc pushed by mcount call				\
-	 */								\
-	__asm("mov	x1, x30");					\
-	/*								\
-	 * Call the real mcount code					\
-	 */								\
-	__asm("bl	" ___STRING(_C_LABEL(_mcount)));		\
-	/*								\
-	 * Restore registers that were trashed during mcount		\
-	 */								\
-	__asm("ldp	x0, x1, [x29, #0]");				\
-	__asm("ldp	x2, x3, [x29, #16]");				\
-	__asm("ldp	x4, x5, [x29, #32]");				\
-	__asm("ldp	x6, x7, [x29, #48]");				\
-	__asm("ldp	x29, x30, [x29, #64]");				\
-	__asm("add	sp, sp, #80");					\
-	__asm("ret");							\
-	__asm(".size	" MCOUNT_ASM_NAME ", .-" MCOUNT_ASM_NAME);
+"	addi	sp, sp, -(16 * " _SZREG ")\n"				\
+"	" _REG_S "	ra, ( 0 * " _SZREG ")(sp)\n" 			\
+"	" _REG_S "	t0, ( 1 * " _SZREG ")(sp)\n" 			\
+"	" _REG_S "	t1, ( 2 * " _SZREG ")(sp)\n" 			\
+"	" _REG_S "	t2, ( 3 * " _SZREG ")(sp)\n" 			\
+"	" _REG_S "	a0, ( 4 * " _SZREG ")(sp)\n" 			\
+"	" _REG_S "	a1, ( 5 * " _SZREG ")(sp)\n" 			\
+"	" _REG_S "	a2, ( 6 * " _SZREG ")(sp)\n" 			\
+"	" _REG_S "	a3, ( 7 * " _SZREG ")(sp)\n" 			\
+"	" _REG_S "	a4, ( 8 * " _SZREG ")(sp)\n" 			\
+"	" _REG_S "	a5, ( 9 * " _SZREG ")(sp)\n" 			\
+"	" _REG_S "	a6, (10 * " _SZREG ")(sp)\n" 			\
+"	" _REG_S "	a7, (11 * " _SZREG ")(sp)\n" 			\
+"	" _REG_S "	t3, (12 * " _SZREG ")(sp)\n" 			\
+"	" _REG_S "	t4, (13 * " _SZREG ")(sp)\n" 			\
+"	" _REG_S "	t5, (14 * " _SZREG ")(sp)\n" 			\
+"	" _REG_S "	t6, (15 * " _SZREG ")(sp)\n" 			\
+"	mv	a1, ra\n"						\
+"	call	mcount " _PLT "\n"					\
+	 /* restore caller saved registers */				\
+"	" _REG_L "	ra, ( 0 * " _SZREG ")(sp)\n"			\
+"	" _REG_L "	t0, ( 1 * " _SZREG ")(sp)\n"			\
+"	" _REG_L "	t1, ( 2 * " _SZREG ")(sp)\n"			\
+"	" _REG_L "	t2, ( 3 * " _SZREG ")(sp)\n"			\
+"	" _REG_L "	a0, ( 4 * " _SZREG ")(sp)\n"			\
+"	" _REG_L "	a1, ( 5 * " _SZREG ")(sp)\n"			\
+"	" _REG_L "	a2, ( 6 * " _SZREG ")(sp)\n"			\
+"	" _REG_L "	a3, ( 7 * " _SZREG ")(sp)\n"			\
+"	" _REG_L "	a4, ( 8 * " _SZREG ")(sp)\n"			\
+"	" _REG_L "	a5, ( 9 * " _SZREG ")(sp)\n"			\
+"	" _REG_L "	a6, (10 * " _SZREG ")(sp)\n"			\
+"	" _REG_L "	a7, (11 * " _SZREG ")(sp)\n"			\
+"	" _REG_L "	t3, (12 * " _SZREG ")(sp)\n"			\
+"	" _REG_L "	t4, (13 * " _SZREG ")(sp)\n"			\
+"	" _REG_L "	t5, (14 * " _SZREG ")(sp)\n"			\
+"	" _REG_L "	t6, (15 * " _SZREG ")(sp)\n"			\
+"	addi	sp, sp, (16 * " _SZREG ")\n"				\
+"	jr	ra\n");
+
+
+#ifdef _KERNEL
+
+#define MCOUNT_ENTER						\
+   s = (csr_sstatus_read() & SR_SIE) != 0;			\
+   csr_sstatus_clear(SR_SIE);	// DISABLE_INTERRUPTS
+
+#define	MCOUNT_EXIT						\
+   if (s)							\
+   	csr_sstatus_set(SR_SIE); // ENABLE_INTERRUPTS
+
 #endif
 
 #endif /* _RISCV_PROFILE_H_ */
+
