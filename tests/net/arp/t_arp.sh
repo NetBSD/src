81eb9d4523dbc9712679d8d0fa0e60b785245ceb
ozaki-r <ozaki-r@NetBSD.org>
2026-03-26 05:23:42 +0000

    tests, arp: add keep_sending test
    It tests whether ARP sends requests more than net.inet.arp.nd_bmaxtries
    times while ping is sending packets.

--- tests/net/arp/t_arp.sh
+++ tests/net/arp/t_arp.sh
@@ -1,4 +1,4 @@
-#	$NetBSD: t_arp.sh,v 1.49 2025/08/18 06:48:29 ozaki-r Exp $
+#	$NetBSD: t_arp.sh,v 1.50 2026/03/26 05:23:42 ozaki-r Exp $
 #
 # Copyright (c) 2015 The NetBSD Foundation, Inc.
 # All rights reserved.
@@ -907,6 +907,29 @@ test_resolution()
 	rump_server_destroy_ifaces
 }
 
+test_keep_sending()
+{
+
+	rump_server_start $SOCKSRC
+	setup_src_server
+
+	export RUMP_SERVER=$SOCKSRC
+
+	extract_new_packets bus1 > ./out
+
+	atf_check -s not-exit:0 -o ignore -e ignore \
+	    rump.ping -n -w 10 -c 10 $IP4DST
+
+	extract_new_packets bus1 > ./out
+	$DEBUG && cat ./out
+
+	pkt=$(make_pkt_str_arpreq $IP4DST $IP4SRC)
+	n=$(grep -E "$pkt" ./out |wc -l)
+	atf_check -s exit:0 test $n -ge 10
+
+	rump_server_destroy_ifaces
+}
+
 add_test()
 {
 	local name=$1
@@ -947,4 +970,5 @@ atf_init_test_cases()
 	add_test cache_creation        "Tests for ARP cache creation"
 	add_test cache_creation_nodad  "Tests for ARP cache creation without DAD"
 	add_test resolution            "Tests for ARP resolution"
+	add_test keep_sending          "Tests if ARP keeps sending requests"
 }
