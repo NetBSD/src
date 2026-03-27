37c332576bfb73890e2c36b698823fe978aafe6b
mrg <mrg@NetBSD.org>
2026-03-27 00:29:30 +0000

    genmatch.cc:  bring across several enhancements from mainline GCC.
    in my testing this reduces the size of the emitted "gimple-match.cc"
    by about 35%, reduces the code section sise about 15%, and reduces
    the compile time about 30%.  (this file is the largest single compile
    for all of GCC.)
    
    this makes it capable of compiling on vax and sparc again, it seems,
    where both were already capable for GCC 14 (which in addition to this
    set, splits the emitted file into 10 sub parts for compiling both
    separately and in parallel.)
    
    (this is part 1, part 2 will be about mergig the split of the output
    into multiple files, but may be abandoned as this portion helps
    enough.)
    
    tested on amd64, vax, sparc as only target, powerpc, with some arm64,
    arm, and m68k to come -- may be also very useful for m68k target.
    
    bump the netbsd gcc version.
    
    XXX: pullup-11, but maybe afer netbsd 11.0 ships (my testing has been
    largely against netbsd-11, so this option is well tested.)
    
    
    commit 75cda3be0232f745cda4e177d514f6900390af0b
    Author: Richard Biener <rguenther@suse.de>
    Date:   Tue Mar 28 12:42:14 2023 +0200
    
        bootstrap/84402 - improve (match ...) code generation
    
    commit 580cda3c2799b1f8323af770e52f1eb0fa204718
    Author: Tamar Christina <tamar.christina@arm.com>
    Date:   Fri May 5 13:35:17 2023 +0100
    
        match.pd: don't emit label if not needed
    
    commit e487fcc0f7466ea663a0fea52076337bebd42b8b
    Author: Tamar Christina <tamar.christina@arm.com>
    Date:   Fri May 5 13:36:01 2023 +0100
    
        match.pd: Remove commented out line pragmas unless -vv is used.
    
    commit c0ce29bc1ce329001b6c02bb3d34bcbb086e1b72
    Author: Tamar Christina <tamar.christina@arm.com>
    Date:   Fri May 5 13:36:43 2023 +0100
    
        match.pd: CSE the dump output check.

--- tools/gcc/gcc-version.mk
+++ tools/gcc/gcc-version.mk
@@ -1,9 +1,9 @@
-#	$NetBSD: gcc-version.mk,v 1.31 2026/02/02 08:36:48 mrg Exp $
+#	$NetBSD: gcc-version.mk,v 1.32 2026/03/27 00:29:30 mrg Exp $
 
 # common location for tools and native build
 
 .if ${HAVE_GCC} == 12
-NETBSD_GCC_VERSION=nb2 20260119
+NETBSD_GCC_VERSION=nb3 20260326
 .endif
 .if ${HAVE_GCC} == 14
 NETBSD_GCC_VERSION=nb2 20260118
