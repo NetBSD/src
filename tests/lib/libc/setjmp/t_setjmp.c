0ae0c2d5bad089ca26d4d28cf42d8d11c426c94d
martin <martin@NetBSD.org>
2026-04-02 18:34:39 +0000

    Pull up following revision(s) (requested by skrll in ticket #226):
    	lib/libc/compat/arch/arm/gen/compat_setjmp.S: revision 1.6
    	tests/lib/libc/setjmp/t_setjmp.c: revision 1.14
    
    PR/59351 compat_setjmp is busted
    
    Apply the patch in the PR from Valery Ushakov (thanks) which restores
    the restoration of r0, lr after the call to sigblock that was lost in
    revision 1.3
    
    Remove atf_tc_expect_signal for arm now that PR port-arm/59351 is fixed

--- tests/lib/libc/setjmp/t_setjmp.c
+++ tests/lib/libc/setjmp/t_setjmp.c
@@ -1,4 +1,4 @@
-/* $NetBSD: t_setjmp.c,v 1.13 2025/04/28 18:29:09 martin Exp $ */
+/* $NetBSD: t_setjmp.c,v 1.13.2.1 2026/04/02 18:34:39 martin Exp $ */
 
 /*-
  * Copyright (c) 2008 The NetBSD Foundation, Inc.
@@ -63,7 +63,7 @@
 #include <sys/cdefs.h>
 __COPYRIGHT("@(#) Copyright (c) 2008\
  The NetBSD Foundation, inc. All rights reserved.");
-__RCSID("$NetBSD: t_setjmp.c,v 1.13 2025/04/28 18:29:09 martin Exp $");
+__RCSID("$NetBSD: t_setjmp.c,v 1.13.2.1 2026/04/02 18:34:39 martin Exp $");
 
 #include <sys/types.h>
 
@@ -352,9 +352,6 @@ ATF_TC_HEAD(compat13_setjmp, tc)
 }
 ATF_TC_BODY(compat13_setjmp, tc)
 {
-#ifdef __arm__
-	atf_tc_expect_signal(-1, "PR port-arm/59351: compat_setjmp is busted");
-#endif
 	h_check(TEST_COMPAT13_SETJMP);
 }
 
@@ -366,9 +363,6 @@ ATF_TC_HEAD(compat13_sigsetjmp_save, tc)
 }
 ATF_TC_BODY(compat13_sigsetjmp_save, tc)
 {
-#ifdef __arm__
-	atf_tc_expect_signal(-1, "PR port-arm/59351: compat_setjmp is busted");
-#endif
 	h_check(TEST_COMPAT13_SIGSETJMP_SAVE);
 }
 
@@ -391,9 +385,6 @@ ATF_TC_HEAD(compat13_longjmp_zero, tc)
 }
 ATF_TC_BODY(compat13_longjmp_zero, tc)
 {
-#ifdef __arm__
-	atf_tc_expect_signal(-1, "PR port-arm/59351: compat_setjmp is busted");
-#endif
 	h_check(TEST_COMPAT13_LONGJMP_ZERO);
 }
 
