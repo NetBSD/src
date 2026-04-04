55958cb1c0ef14d96e36f9ad9cb04a353159e61e
rillig <rillig@NetBSD.org>
2026-04-04 06:02:59 +0000

    tests/libc: test long double operations more thoroughly
    For lint, the tests msg_380.c and msg_381.c fail, which is probably due
    to a bug in the softfloat implementation.
    
    The tests are intended to yield the same results on all platforms, as
    they do not exercise any tricky rounding details.

--- tests/lib/libc/t_long_double.c
+++ tests/lib/libc/t_long_double.c
@@ -27,12 +27,220 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 #include <sys/cdefs.h>
-__RCSID("$NetBSD: t_long_double.c,v 1.1 2026/04/01 21:00:53 rillig Exp $");
+__RCSID("$NetBSD: t_long_double.c,v 1.2 2026/04/04 06:02:59 rillig Exp $");
 
 #include <atf-c.h>
 
-#include <stdio.h>
-#include <stdlib.h>
+#include <float.h>
+#include <inttypes.h>
+#include <limits.h>
+#include <stdint.h>
+
+static void
+test_ldbl_cmp(void)
+{
+	static const long double ns[] = {
+		-0x1.0p63L,
+		-0x1.0p62L,
+		-0x1.0p60L,
+		-0x1.0p10L,
+		-0x1.0p1L,
+		-0x1.0p0L,
+		0.0L,
+		+0x1.0p0L,
+		+0x1.0p1L,
+		+0x1.0p10L,
+		+0x1.0p60L,
+		+0x1.0p62L,
+		+0x1.0p63L,
+	};
+
+	for (size_t i = 0; i < __arraycount(ns); i++) {
+		for (size_t j = 0; j < __arraycount(ns); j++) {
+			if (i < j)
+				ATF_CHECK_MSG(ns[i] < ns[j],
+				              "want %Lf (%La) < %Lf (%La)",
+				              ns[i], ns[i], ns[j], ns[j]);
+			if (i <= j)
+				ATF_CHECK_MSG(ns[i] <= ns[j],
+				              "want %Lf (%La) <= %Lf (%La)",
+				              ns[i], ns[i], ns[j], ns[j]);
+			if (i == j)
+				ATF_CHECK_MSG(ns[i] == ns[j],
+				              "want %Lf (%La) == %Lf (%La)",
+				              ns[i], ns[i], ns[j], ns[j]);
+			if (i != j)
+				ATF_CHECK_MSG(ns[i] != ns[j],
+				              "want %Lf (%La) != %Lf (%La)",
+				              ns[i], ns[i], ns[j], ns[j]);
+			if (i >= j)
+				ATF_CHECK_MSG(ns[i] >= ns[j],
+				              "want %Lf (%La) >= %Lf (%La)",
+				              ns[i], ns[i], ns[j], ns[j]);
+			if (i > j)
+				ATF_CHECK_MSG(ns[i] > ns[j],
+				              "want %Lf (%La) > %Lf (%La)",
+				              ns[i], ns[i], ns[j], ns[j]);
+		}
+	}
+}
+
+static void
+test_int64_to_ldbl(void)
+{
+	static const struct testcase {
+		int64_t arg;
+		long double want;
+	} testcases[] = {
+		{ INT64_MIN, -0x1.0p63L },
+		{ -(1LL << 62), -0x1.0p62L },
+		{ -(1LL << 60), -0x1.0p60L },
+		{ -(1LL << 40), -0x1.0p40L },
+		{ -(1LL << 20), -0x1.0p20L },
+		{ -(1LL << 10), -0x1.0p10L },
+		{ -1, -1.0L },
+		{ 0, +0.0L },
+		{ +1, +1.0L },
+		{ +(1LL << 10), +0x1.0p10L },
+		{ +(1LL << 20), +0x1.0p20L },
+		{ +(1LL << 40), +0x1.0p40L },
+		{ +(1LL << 60), +0x1.0p60L },
+		{ +(1LL << 62), +0x1.0p62L },
+		{ INT64_MAX - (INT64_MAX >> 1), +0x1.0p62L },
+	};
+
+	for (size_t i = 0; i < __arraycount(testcases); i++) {
+		const struct testcase *c = testcases + i;
+		long double have = (long double)c->arg;
+		ATF_CHECK_MSG(have == c->want,
+		    "want %Lf (%La) for %" PRId64 " (0x%016" PRIx64 "), "
+		    "have %Lf (%La)",
+		    c->want, c->want, c->arg, c->arg, have, have);
+	}
+}
+
+static void
+test_ldbl_to_int64(void)
+{
+	static const struct testcase {
+		long double arg;
+		int64_t want;
+	} testcases[] = {
+		{ -0x1.0p63L, INT64_MIN },
+		{ -1.0L, -1 },
+		{ -0.0L, 0 },
+		{ +0.0L, 0 },
+		{ +1.0L, 1 },
+		{  +0x1.0p62L, INT64_MAX - (INT64_MAX >> 1) },
+	};
+
+	for (size_t i = 0; i < __arraycount(testcases); i++) {
+		const struct testcase *c = testcases + i;
+		int64_t have = (int64_t)c->arg;
+		ATF_CHECK_MSG(have == c->want,
+		    "want %" PRId64 " (0x%016" PRIx64 ") for %Lf (%La), "
+		    "have %" PRId64 " (0x%016" PRIx64 ")",
+		    c->want, c->want, c->arg, c->arg, have, have);
+	}
+}
+
+static void
+test_uint64_to_ldbl(void)
+{
+	static const struct testcase {
+		uint64_t arg;
+		long double want;
+	} testcases[] = {
+		{ 0, +0.0L },
+		{ +1, +1.0L },
+		{ UINT64_MAX - (UINT64_MAX >> 1), +0x1.0p63L },
+	};
+
+	for (size_t i = 0; i < __arraycount(testcases); i++) {
+		const struct testcase *c = testcases + i;
+		long double have = (long double)c->arg;
+		ATF_CHECK_MSG(have == c->want,
+		    "want %Lf (%La) for %" PRIu64 " (0x%016" PRIx64 "), "
+		    "have %Lf (%La)",
+		    c->want, c->want, c->arg, c->arg, have, have);
+	}
+}
+
+static void
+test_ldbl_to_uint64(void)
+{
+	static const struct testcase {
+		long double arg;
+		uint64_t want;
+	} testcases[] = {
+		{ -1.0L, (uint64_t)-1 },
+		{ -0.0L, 0 },
+		{ +0.0L, 0 },
+		{ +1.0L, 1 },
+		{ +0x1.0p63L, UINT64_MAX - (UINT64_MAX >> 1) },
+	};
+
+	for (size_t i = 0; i < __arraycount(testcases); i++) {
+		const struct testcase *c = testcases + i;
+		uint64_t have = (uint64_t)c->arg;
+		ATF_CHECK_MSG(have == c->want,
+		    "want %" PRIu64 " (0x%016" PRIx64 ") for %Lf (%La), "
+		    "have %" PRIu64 " (0x%016" PRIx64 ")",
+		    c->want, c->want, c->arg, c->arg, have, have);
+	}
+}
+
+static void
+test_dbl_to_ldbl(void)
+{
+	static const struct testcase {
+		double arg;
+		long double want;
+	} testcases[] = {
+		{ -DBL_MAX, -DBL_MAX },
+		{ -1.0, -1.0L },
+		{ -DBL_MIN, -DBL_MIN },
+		{ -0.0, -0.0L },
+		{ +0.0, +0.0L },
+		{ +DBL_MIN, +DBL_MIN },
+		{ +1.0, +1.0L },
+		{ +DBL_MAX, +DBL_MAX },
+	};
+
+	for (size_t i = 0; i < __arraycount(testcases); i++) {
+		const struct testcase *c = testcases + i;
+		long double have = (long double)c->arg;
+		ATF_CHECK_MSG(have == c->want,
+		    "want %Lf (%La) for %f (%a), have %Lf (%La)",
+		    c->want, c->want, c->arg, c->arg, have, have);
+	}
+}
+
+static void
+test_ldbl_to_dbl(void)
+{
+	static const struct testcase {
+		long double arg;
+		double want;
+	} testcases[] = {
+		{ -DBL_MAX, -DBL_MAX },
+		{ -1.0L, -1.0 },
+		{ -DBL_MIN, -DBL_MIN },
+		{ -0.0L, -0.0 },
+		{ +0.0L, +0.0 },
+		{ +DBL_MIN, +DBL_MIN },
+		{ +1.0L, +1.0 },
+		{ +DBL_MAX, +DBL_MAX },
+	};
+
+	for (size_t i = 0; i < __arraycount(testcases); i++) {
+		const struct testcase *c = testcases + i;
+		double have = (double)c->arg;
+		ATF_CHECK_MSG(have == c->want,
+		    "want %f (%a) for %Lf (%La), have %f (%a)",
+		    c->want, c->want, c->arg, c->arg, have, have);
+	}
+}
 
 ATF_TC(long_double);
 
@@ -42,67 +250,15 @@ ATF_TC_HEAD(long_double, tc)
 	atf_tc_set_md_var(tc, "descr", "Test 'long double' operations");
 }
 
-#define	P_ldbl_m_1_63	"%La"
-#define	P_ldbl_p_1_63	"%La"
-#define	P_dbl_m_1_63	"%a"
-#define	P_dbl_p_1_63	"%a"
-#define	P_ll_m_1_63	"%#016llx"
-#define	P_ll_p_1_63	"%#016llx"
-#define	P_ull_1_63	"%#016llx"
-
 ATF_TC_BODY(long_double, tc)
 {
-	volatile long double ldbl_m_1_63 = -0x1.0p63L;
-	volatile long double ldbl_p_1_63 = +0x1.0p63L;
-	volatile double dbl_m_1_63 = -0x1.0p63;
-	volatile double dbl_p_1_63 = +0x1.0p63;
-	volatile long long ll_m_1_63 = (long long)(-1ULL << 63);
-	volatile long long ll_p_1_63 = (long long)(+1ULL << 63);
-	volatile unsigned long long ull_1_63 = +1ULL << 63;
-
-#define	EXPECT_CMP(lhs, op, rhs) \
-	ATF_CHECK_MSG(lhs op rhs, \
-	    P_##lhs " !" #op " " P_##rhs, \
-	    lhs, rhs)
-
-	// _Qp_fge
-	EXPECT_CMP(ldbl_m_1_63, >=, ldbl_m_1_63);
-	EXPECT_CMP(ldbl_p_1_63, >=, ldbl_p_1_63);
-
-	// _Qp_fle
-	EXPECT_CMP(ldbl_m_1_63, <=, ldbl_m_1_63);
-	EXPECT_CMP(ldbl_p_1_63, <=, ldbl_p_1_63);
-
-	// _Qp_feq
-	EXPECT_CMP(ldbl_m_1_63, ==, ldbl_m_1_63);
-	EXPECT_CMP(ldbl_p_1_63, ==, ldbl_p_1_63);
-
-	// _Qp_fne
-	EXPECT_CMP(ldbl_m_1_63, !=, ldbl_p_1_63);
-	EXPECT_CMP(ldbl_p_1_63, !=, ldbl_m_1_63);
-
-	// _Qp_xtoq
-	EXPECT_CMP(ldbl_m_1_63, ==, ll_m_1_63);
-	EXPECT_CMP(ll_m_1_63, ==, ldbl_m_1_63);
-	EXPECT_CMP(ldbl_p_1_63, !=, ll_p_1_63);
-	EXPECT_CMP(ll_p_1_63, !=, ldbl_p_1_63);
-
-	// ?
-	EXPECT_CMP(ldbl_m_1_63, ==, dbl_m_1_63);
-	EXPECT_CMP(dbl_m_1_63, ==, ldbl_m_1_63);
-	EXPECT_CMP(ldbl_p_1_63, ==, dbl_p_1_63);
-	EXPECT_CMP(dbl_p_1_63, ==, ldbl_p_1_63);
-
-	// _Qp_uxtoq
-	EXPECT_CMP(ldbl_p_1_63, ==, ull_1_63);
-
-	// _Qp_qtox
-	ATF_CHECK_MSG((long long)ldbl_m_1_63 == ll_m_1_63,
-	    "%#llx != %#llx", (long long)ldbl_m_1_63, ll_m_1_63);
-
-	// _Qp_qtoux
-	ATF_CHECK_MSG((unsigned long long)ldbl_m_1_63 == ull_1_63,
-	    "%#llx != %#llx", (unsigned long long)ldbl_m_1_63, ull_1_63);
+	test_ldbl_cmp();
+	test_int64_to_ldbl();
+	test_ldbl_to_int64();
+	test_uint64_to_ldbl();
+	test_ldbl_to_uint64();
+	test_dbl_to_ldbl();
+	test_ldbl_to_dbl();
 }
 
 ATF_TP_ADD_TCS(tp)
