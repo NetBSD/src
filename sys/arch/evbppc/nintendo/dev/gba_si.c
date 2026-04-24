52e4a95c50d387b8d77a48c313ad900e0ba0532f
zac brown <gummybuns@protonmail.com>
2026-04-24 22:03:49 +0000

    got a char driver for gba to work
    - moved common stuff to si.h
    - use the identity of the device in match for uhid/gba
    - very basic char driver for gba
      right now can be initialized with mknod /dev/gba0 c gba 0

--- sys/arch/evbppc/nintendo/dev/gba_si.c
+++ sys/arch/evbppc/nintendo/dev/gba_si.c
@@ -1,7 +1,5 @@
-/* $NetBSD: uhid_si.c,v 1.1 2026/01/09 22:54:30 jmcneill Exp $ */
-
 /*-
- * Copyright (c) 2025 Jared McNeill <jmcneill@invisible.ca>
+ * Copyright (c) 2025 ZacBrown <gummybuns@protonmail.com>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -27,11 +25,12 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: uhid_si.c,v 1.1 2026/01/09 22:54:30 jmcneill Exp $");
+__KERNEL_RCSID(0, "$NetBSD: gba_si.c,v 1.0 2026/04/24 15:07:30 gummybuns Exp $");
 
-#include <sys/param.h>
 #include <sys/bus.h>
+#include <sys/conf.h>
 #include <sys/device.h>
+#include <sys/param.h>
 #include <sys/systm.h>
 #include <sys/tty.h>
 
@@ -41,64 +40,87 @@ __KERNEL_RCSID(0, "$NetBSD: uhid_si.c,v 1.1 2026/01/09 22:54:30 jmcneill Exp $")
 #include "si.h"
 #include "joybus.h"
 
-#define UHID_SI_VENDOR	0x057e	/* Nintendo */
-#define UHID_SI_PRODUCT	0x0337	/* GameCube adapter */
+static int gba_si_match(device_t, cfdata_t, void *);
+static void gba_si_attach(device_t, device_t, void *);
 
-static int	uhid_si_match(device_t, cfdata_t, void *);
-static void	uhid_si_attach(device_t, device_t, void *);
+dev_type_open(gba_open);
+dev_type_close(gba_close);
+dev_type_ioctl(gba_ioctl);
 
-static int	uhid_si_ioctl(struct uhid_softc *, u_long, void *, int,
-			      struct lwp *);
+const struct cdevsw gba_cdevsw = {
+	.d_open = gba_open,
+	.d_close = gba_close,
+	.d_ioctl = noioctl,
+	.d_read = noread,
+	.d_write = nowrite,
+	.d_stop = nostop,
+	.d_tty = notty,
+	.d_poll = nopoll,
+	.d_mmap = nommap,
+	.d_kqfilter = nokqfilter,
+	.d_discard = nodiscard,
+	.d_flag = D_OTHER
+};
 
-CFATTACH_DECL_NEW(uhid_si, sizeof(struct uhid_softc),
-	uhid_si_match, uhid_si_attach, NULL, NULL);
+struct gba_softc {
+	device_t		sc_dev;
+	struct si_channel	*ch;
+	bus_space_tag_t		sc_bst;
+	bus_space_handle_t	sc_bsh;
+};
+
+CFATTACH_DECL_NEW(gba_si, sizeof(struct gba_softc),
+	gba_si_match, gba_si_attach, NULL, NULL);
 
 static int
-uhid_si_match(device_t parent, cfdata_t cf, void *aux)
+gba_si_match(device_t parent, cfdata_t cf, void *aux)
 {
 	struct si_softc * const sc = device_private(parent);
 	struct si_attach_args * const saa = aux;
 	struct si_channel *ch;
 
 	ch = &sc->sc_chan[saa->saa_index];
-	if (IS_GCPAD(ch->ch_id)) {
+	aprint_normal("gba: checking 0x%08X...", ch->ch_id);
+	if (IS_GBA(ch->ch_id)) {
+		aprint_normal(" is is a gba!\n");
 		return 1;
 	}
 
+	aprint_normal(" is not a gba\n");
 	return 0;
 }
 
 static void
-uhid_si_attach(device_t parent, device_t self, void *aux)
+gba_si_attach(device_t parent, device_t self, void *aux)
 {
-	struct uhid_softc * const sc = device_private(self);
+	struct si_softc * const psc = device_private(parent);
 	struct si_attach_args * const saa = aux;
+	struct gba_softc * const sc = device_private(self);
+	struct si_channel *ch = &psc->sc_chan[saa->saa_index];
 
+	aprint_normal("gba: inside attach\n");
 	sc->sc_dev = self;
-	sc->sc_report_id = saa->saa_index + 1;
-	sc->sc_ioctl = uhid_si_ioctl;
-	sc->sc_hidev = saa->saa_hidev;
-
-	uhid_attach_common(sc);
+	sc->ch = ch;
+	sc->sc_bst = psc->sc_bst;
+	sc->sc_bsh = psc->sc_bsh;
 }
 
-static int
-uhid_si_ioctl(struct uhid_softc *sc, u_long cmd, void *addr, int flag,
-    struct lwp *l)
+int
+gba_open(dev_t dev, int flag, int mode, struct lwp *l)
 {
-	struct usb_device_info *udi;
-
-	switch (cmd) {
-	case USB_GET_DEVICEINFO:
-		udi = addr;
-		udi->udi_vendorNo = UHID_SI_VENDOR;
-		udi->udi_productNo = UHID_SI_PRODUCT;
-		snprintf(udi->udi_vendor, sizeof(udi->udi_vendor), "Nintendo");
-		snprintf(udi->udi_product, sizeof(udi->udi_product),
-		    "GameCube Controller");
-		return 0;
-		break;
-	}
-
-	return EINVAL;
+	return 0;
 }
+
+int
+gba_close(dev_t dev, int flag, int mode, struct lwp *l)
+{
+	return 0;
+}
+
+/*
+int
+gba_read(dev_t dev, struct uio *uio, int flags)
+{
+	return 0;
+}
+*/
--- sys/arch/evbppc/nintendo/dev/si.c
+++ sys/arch/evbppc/nintendo/dev/si.c
@@ -49,99 +49,6 @@ __KERNEL_RCSID(0, "$NetBSD: si.c,v 1.2 2026/06/11 19:46:18 andvar Exp $");
 #include "gcpad_rdesc.h"
 #include "joybus.h"
 
-#define SI_NUM_CHAN		4
-
-#define SICOUTBUF(n)		((n) * 0xc + 0x00)
-#define   SIIDENTIFY		0x00000000
-#define   SIINIT		0x00400300
-#define SICINBUFH(n)		((n) * 0xc + 0x04)
-#define SICINBUFL(n)		((n) * 0xc + 0x08)
-#define SIPOLL			0x30
-#define  SIPOLL_X		__BITS(25, 16)
-#define  SIPOLL_Y		__BITS(15, 8)
-#define  SIPOLL_EN(n)		(__BIT(7 - n))
-#define SICOMCSR		0x34
-#define  SICOMCSR_TCINT		__BIT(31)
-#define  SICOMCSR_TCINTMSK	__BIT(30)
-#define  SICOMCSR_COMERR	__BIT(29)
-#define  SICOMCSR_RDSTINT	__BIT(28)
-#define  SICOMCSR_RDSTINTMSK	__BIT(27)
-#define  SICOMCSR_CH_EN    	__BIT(24)
-#define  SICOMCSR_OUTLNGTH	__BITS(22, 16)
-#define  SICOMCSR_INLNGTH	__BITS(14, 8)
-#define  SICOMCSR_CMD_EN   	__BIT(7)
-#define  SICOMCSR_CHANNEL	__BITS(2, 1)
-#define  SICOMCSR_TSTART	__BIT(0)
-#define SISR			0x38
-#define  SISR_OFF(n)		((3 - (n)) * 8)
-#define  SISR_WR(n)		__BIT(SISR_OFF(n) + 7)
-#define  SISR_RDST(n)		__BIT(SISR_OFF(n) + 5)
-#define  SISR_WRST(n)		__BIT(SISR_OFF(n) + 4)
-#define  SISR_NOREP(n)		__BIT(SISR_OFF(n) + 3)
-#define  SISR_COLL(n)		__BIT(SISR_OFF(n) + 2)
-#define  SISR_OVRUN(n)		__BIT(SISR_OFF(n) + 1)
-#define  SISR_UNRUN(n)		__BIT(SISR_OFF(n) + 0)
-#define  SISR_ERROR_MASK(n)	(SISR_NOREP(n) | SISR_COLL(n) | \
-				 SISR_OVRUN(n) | SISR_UNRUN(n))
-#define  SISR_ERROR_ACK_ALL	(SISR_ERROR_MASK(0) | SISR_ERROR_MASK(1) | \
-				 SISR_ERROR_MASK(2) | SISR_ERROR_MASK(3))
-#define SIEXILK			0x3c
-#define SIIOBUF			0x80
-
-#define GCPAD_REPORT_SIZE	9
-#define GCPAD_START(_buf)	ISSET((_buf)[0], 0x10)
-#define GCPAD_Y(_buf)		ISSET((_buf)[0], 0x08)
-#define GCPAD_X(_buf)		ISSET((_buf)[0], 0x04)
-#define GCPAD_B(_buf)		ISSET((_buf)[0], 0x02)
-#define GCPAD_A(_buf)		ISSET((_buf)[0], 0x01)
-#define GCPAD_LCLICK(_buf)	ISSET((_buf)[1], 0x40)
-#define GCPAD_RCLICK(_buf)	ISSET((_buf)[1], 0x20)
-#define GCPAD_Z(_buf)		ISSET((_buf)[1], 0x10)
-#define GCPAD_UP(_buf)		ISSET((_buf)[1], 0x08)
-#define GCPAD_DOWN(_buf)	ISSET((_buf)[1], 0x04)
-#define GCPAD_RIGHT(_buf)	ISSET((_buf)[1], 0x02)
-#define GCPAD_LEFT(_buf)	ISSET((_buf)[1], 0x01)
-
-struct si_softc;
-
-struct si_channel {
-	struct si_softc		*ch_sc;
-	device_t		ch_dev;
-	unsigned		ch_index;
-	struct hidev_tag	ch_hidev;
-	kmutex_t		ch_lock;
-	kcondvar_t		ch_cv;
-	uint8_t			ch_state;
-	uint16_t		ch_id;
-	uint16_t		ch_id_extra;
-#define SI_STATE_OPEN		__BIT(0)
-#define SI_STATE_STOPPED	__BIT(1)
-	void			(*ch_intr)(void *, void *, u_int);
-	void			*ch_intrarg;
-	uint8_t			ch_buf[GCPAD_REPORT_SIZE];
-	void			*ch_desc;
-	int			ch_descsize;
-	void			*ch_si;
-};
-
-struct si_softc {
-	device_t		sc_dev;
-	bus_space_tag_t		sc_bst;
-	bus_space_handle_t	sc_bsh;
-
-	struct si_channel	sc_chan[SI_NUM_CHAN];
-};
-
-#define RD4(sc, reg)							\
-	bus_space_read_4((sc)->sc_bst, (sc)->sc_bsh, (reg))
-#define WR4(sc, reg, val)						\
-	bus_space_write_4((sc)->sc_bst, (sc)->sc_bsh, (reg), (val))
-
-#define AWAIT_SISR(sc, chan)	 					\
-	do { while (RD4(sc, SISR) & SISR_WRST(chan)); } while (0)
-#define AWAIT_SICOMCSR(sc)	 					\
-	do { while (RD4(sc, SICOMCSR) & SICOMCSR_TSTART); } while (0)
-
 
 static int	si_match(device_t, cfdata_t, void *);
 static void	si_attach(device_t, device_t, void *);
@@ -241,7 +148,7 @@ si_rescan(device_t self, const char *ifattr, const int *locs)
 	for (chan = 0; chan < SI_NUM_CHAN; chan++) {
 		struct si_channel *ch = &sc->sc_chan[chan];
 
-		if (ch->ch_dev == NULL && (IS_GCPAD(ch->ch_id))) {
+		if (ch->ch_dev == NULL) {
 			saa.saa_hidev = &ch->ch_hidev;
 			saa.saa_index = ch->ch_index;
 
