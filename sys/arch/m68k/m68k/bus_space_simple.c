0d9efa0489b4c0dd152cd8370242f3c04dbfd51a
thorpej <thorpej@NetBSD.org>
2026-04-26 17:43:41 +0000

    Support non-static mappings, allocating space from kernel_map.
    Still TODO: platform-specific hook for a vmem arena that supplies
    the VAs for a given bus.

--- sys/arch/m68k/m68k/bus_space_simple.c
+++ sys/arch/m68k/m68k/bus_space_simple.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: bus_space_simple.c,v 1.3 2026/04/26 15:12:09 thorpej Exp $	*/
+/*	$NetBSD: bus_space_simple.c,v 1.4 2026/04/26 17:43:41 thorpej Exp $	*/
 
 /*-
  * Copyright (c) 1998, 2000 The NetBSD Foundation, Inc.
@@ -30,7 +30,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: bus_space_simple.c,v 1.3 2026/04/26 15:12:09 thorpej Exp $");
+__KERNEL_RCSID(0, "$NetBSD: bus_space_simple.c,v 1.4 2026/04/26 17:43:41 thorpej Exp $");
 
 #define _M68K_BUS_SPACE_PRIVATE
 
@@ -62,6 +62,7 @@ int
 _bus_space_map(void *cookie, bus_addr_t addr, bus_size_t size, int flags,
     bus_space_handle_t *bushp)
 {
+	vsize_t offset;
 	vaddr_t va;
 	int map_flags;
 
@@ -72,20 +73,57 @@ _bus_space_map(void *cookie, bus_addr_t addr, bus_size_t size, int flags,
 		return 0;
 	}
 
-	/* XXX dynamically allocate space for new mapping */
+	/*
+	 * Allocate virtual space from the kernel_map.
+	 *
+	 * XXX Provide a hook for the platform to supply a
+	 * XXX vmem arena for the space.  That would be needed
+	 * XXX in situations where non-static mappings are
+	 * XXX needed before UVM is initialized (e.g. frame
+	 * XXX buffer console devices).
+	 */
+	offset = m68k_page_offset(addr);
+	size = m68k_round_page(offset + size);
+	addr = m68k_trunc_page(addr);
 
-	return EINVAL;
+	va = uvm_km_alloc(kernel_map, size, 0, UVM_KMF_VAONLY);
+	if (va == 0) {
+		return ENOMEM;
+	}
+
+	*bushp = (bus_space_handle_t)(va + offset);
+
+	while (size != 0) {
+		pmap_kenter_pa(va, addr, VM_PROT_READ | VM_PROT_WRITE,
+		    PMAP_NOCACHE);
+		va += PAGE_SIZE;
+		addr += PAGE_SIZE;
+		size -= PAGE_SIZE;
+	}
+
+	return 0;
 }
 
 /* ARGSUSED */
 void
 _bus_space_unmap(void *cookie, bus_space_handle_t bush, bus_size_t size)
 {
+	vaddr_t va;
+	vsize_t offset;
+
 	if (pmap_va_is_static_mapping((vaddr_t)bush, size)) {
 		return;
 	}
 
-	/* XXX */
+	va = m68k_trunc_page(bush);
+	offset = m68k_page_offset(bush);
+	size = m68k_round_page(offset + size);
+
+	pmap_kremove(va, size);
+
+	/* XXX vmem arena hook */
+
+	uvm_km_free(kernel_map, va, size, UVM_KMF_VAONLY);
 }
 
 paddr_t
