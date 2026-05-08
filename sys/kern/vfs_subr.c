63a2f09595cfb7034b81b4c41a5f114a189adf43
perseant <perseant@NetBSD.org>
2026-05-08 04:18:32 +0000

    Correct reversed sense of IMNT_MPSAFE test in VFS_MOUNT(9).

--- sys/kern/vfs_subr.c
+++ sys/kern/vfs_subr.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: vfs_subr.c,v 1.502 2024/12/07 02:27:38 riastradh Exp $	*/
+/*	$NetBSD: vfs_subr.c,v 1.503 2026/05/08 04:18:32 perseant Exp $	*/
 
 /*-
  * Copyright (c) 1997, 1998, 2004, 2005, 2007, 2008, 2019, 2020
@@ -69,7 +69,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: vfs_subr.c,v 1.502 2024/12/07 02:27:38 riastradh Exp $");
+__KERNEL_RCSID(0, "$NetBSD: vfs_subr.c,v 1.503 2026/05/08 04:18:32 perseant Exp $");
 
 #ifdef _KERNEL_OPT
 #include "opt_compat_43.h"
@@ -1488,11 +1488,11 @@ VFS_MOUNT(struct mount *mp, const char *a, void *b, size_t *c)
 	 * XXX Maybe the MPSAFE bit should be set in struct vfsops and
 	 * not in struct mount.
 	 */
-	if (mpsafe) {
+	if (!mpsafe) {
 		KERNEL_LOCK(1, NULL);
 	}
 	error = (*(mp->mnt_op->vfs_mount))(mp, a, b, c);
-	if (mpsafe) {
+	if (!mpsafe) {
 		KERNEL_UNLOCK_ONE(NULL);
 	}
 
