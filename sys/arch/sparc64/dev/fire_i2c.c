a055c2a130749e72496ce0a1fa75f9f8a73e8cad
jdc <jdc@NetBSD.org>
2026-06-05 09:08:55 +0000

    Add a cast when printing ma->ma_reg[0].ur_len, so that it works on both sparc and sparc64.

--- sys/arch/sparc64/dev/fire_i2c.c
+++ sys/arch/sparc64/dev/fire_i2c.c
@@ -1,4 +1,4 @@
-/* $NetBSD: fire_i2c.c,v 1.2 2026/06/05 06:26:44 kre Exp $ */
+/* $NetBSD: fire_i2c.c,v 1.3 2026/06/05 09:08:55 jdc Exp $ */
 
 /*-
  * Copyright (c) 2026 The NetBSD Foundation, Inc.
@@ -30,7 +30,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: fire_i2c.c,v 1.2 2026/06/05 06:26:44 kre Exp $");
+__KERNEL_RCSID(0, "$NetBSD: fire_i2c.c,v 1.3 2026/06/05 09:08:55 jdc Exp $");
 
 #include <sys/param.h>
 #include <sys/condvar.h>
@@ -122,7 +122,7 @@ firei2c_attach(device_t parent, device_t self, void *aux)
 	}
 	if (ma->ma_reg[0].ur_len < FIREI2C_SRST) {
 		aprint_error(": register length error (%lld < %d\n",
-		    ma->ma_reg[0].ur_len, FIREI2C_SRST);
+		    (long long int) ma->ma_reg[0].ur_len, FIREI2C_SRST);
 		return;
 	}
 
