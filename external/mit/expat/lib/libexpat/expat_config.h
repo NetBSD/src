f0e3c1fd4275f262bc6f0b6d9f3764b91a3fce80
martin <martin@NetBSD.org>
2026-06-16 08:55:52 +0000

    Pull up following revision(s) (requested by jdc in ticket #311):
    	external/mit/expat/lib/libexpat/shlib_version: revision 1.7
    	distrib/sets/lists/base/shl.mi: revision 1.1043
    	distrib/sets/lists/debug/shl.mi: revision 1.404
    	external/mit/expat/lib/libexpat/expat_config.h: revision 1.18
    	doc/3RDPARTY: revision 1.2217
    	external/mit/expat/bin/xmlwf/Makefile: revision 1.2
    	external/mit/expat/lib/libexpat/Makefile: revision 1.6
    	doc/CHANGES: revision 1.3272
    	external/mit/expat/lib/libexpat/expat.expsym: revision 1.3
    	external/mit/xorg/tools/fc-cache/Makefile: revision 1.25
    
    Update for expat 2.8.1
    Note export 2.8.1 import
    Update for expat 2.8.1
    Update for libexpat minor bump

--- external/mit/expat/lib/libexpat/expat_config.h
+++ external/mit/expat/lib/libexpat/expat_config.h
@@ -1,5 +1,4 @@
-/* expat_config.h.  Generated from expat_config.h.in by configure.  */
-/* expat_config.h.in.  Generated from configure.ac by autoheader.  */
+/* expat_config.h.cmake.  Based upon generated expat_config.h.in.  */
 
 #ifndef EXPAT_CONFIG_H
 #define EXPAT_CONFIG_H 1
@@ -12,15 +11,10 @@
 #define BYTEORDER _BYTE_ORDER
 
 /* Define to 1 if you have the `arc4random' function. */
-/* #undef HAVE_ARC4RANDOM */
+#define HAVE_ARC4RANDOM 1
 
-#ifndef TOOL_FCCACHE
 /* Define to 1 if you have the `arc4random_buf' function. */
 #define HAVE_ARC4RANDOM_BUF 1
-#endif
-
-/* define if the compiler supports basic C++11 syntax */
-#define HAVE_CXX11 1
 
 /* Define to 1 if you have the <dlfcn.h> header file. */
 #define HAVE_DLFCN_H 1
@@ -31,17 +25,17 @@
 /* Define to 1 if you have the `getpagesize' function. */
 #define HAVE_GETPAGESIZE 1
 
-/* Host build might not have getrandom */
-#ifndef TOOL_FCCACHE
 /* Define to 1 if you have the `getrandom' function. */
 #define HAVE_GETRANDOM 1
-#endif
+
+/* Define to 1 if you have the `getentropy' function. */
+#define HAVE_GETENTROPY 1
 
 /* Define to 1 if you have the <inttypes.h> header file. */
 #define HAVE_INTTYPES_H 1
 
-/* Define to 1 if you have the `bsd' library (-lbsd). */
-/* #undef HAVE_LIBBSD */
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
 
 /* Define to 1 if you have a working `mmap' system call. */
 #define HAVE_MMAP 1
@@ -61,13 +55,8 @@
 /* Define to 1 if you have the <string.h> header file. */
 #define HAVE_STRING_H 1
 
-#ifndef TOOL_FCCACHE
 /* Define to 1 if you have `syscall' and `SYS_getrandom'. */
 #define HAVE_SYSCALL_GETRANDOM 1
-#endif
-
-/* Define to 1 if you have the <sys/param.h> header file. */
-#define HAVE_SYS_PARAM_H 1
 
 /* Define to 1 if you have the <sys/stat.h> header file. */
 #define HAVE_SYS_STAT_H 1
@@ -77,9 +66,6 @@
 
 /* Define to 1 if you have the <unistd.h> header file. */
 #define HAVE_UNISTD_H 1
-
-/* Define to the sub-directory where libtool stores uninstalled libraries. */
-#define LT_OBJDIR ".libs/"
 
 /* Name of package */
 #define PACKAGE "expat"
@@ -91,7 +77,7 @@
 #define PACKAGE_NAME "expat"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "expat 2.7.1"
+#define PACKAGE_STRING "expat 2.8.1"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "expat"
@@ -100,18 +86,15 @@
 #define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "2.7.1"
+#define PACKAGE_VERSION "2.8.1"
 
-/* Define to 1 if all of the C90 standard headers exist (not just the ones
-   required in a freestanding environment). This macro is provided for
-   backward compatibility; new code need not use it. */
+/* Define to 1 if you have the ANSI C header files. */
 #define STDC_HEADERS 1
 
 /* Version number of package */
-#define VERSION "2.7.1"
+#define VERSION "2.8.1"
 
-/* Define WORDS_BIGENDIAN to 1 if your processor stores words with the most
-   significant byte first (like Motorola and SPARC, unlike Intel). */
+/* whether byteorder is bigendian */
 #if defined AC_APPLE_UNIVERSAL_BUILD
 # if defined __BIG_ENDIAN__
 #  define WORDS_BIGENDIAN 1
@@ -130,8 +113,10 @@
    point, 0 to disable. */
 #define XML_CONTEXT_BYTES 1024
 
+#  if ! defined(_WIN32)
 /* Define to include code reading entropy from `/dev/urandom'. */
 #define XML_DEV_URANDOM 1
+#  endif
 
 /* Define to make parameter entity parsing functionality available. */
 #define XML_DTD 1
@@ -142,10 +127,12 @@
 /* Define to make XML Namespaces functionality available. */
 #define XML_NS 1
 
-/* Define to empty if `const' does not conform to ANSI C. */
-/* #undef const */
+/* Define to __FUNCTION__ or "" if `__func__' does not conform to ANSI C. */
+#  ifdef _MSC_VER
+#    define __func__ __FUNCTION__
+#  endif
 
-/* Define to `long int' if <sys/types.h> does not define. */
+/* Define to `long' if <sys/types.h> does not define. */
 /* #undef off_t */
 
 #endif // ndef EXPAT_CONFIG_H
