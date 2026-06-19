22fcffaa067853052328bda2a67073f70fd1c8d2
kre <kre@NetBSD.org>
2026-06-19 07:49:00 +0000

    tmux/prepare-import.sh - script cleanups
    A bunch of minor changes that really affect nothing, but
    be sure to never use "test ... -a ...", and avoid using
    	local "var=$(cmdsub)"
    or the status from the cmdsub is ignored, do it in 2 commands
    	local var
    	var=$(cmdsub)
    [And yes, the quotes are needed in the former version, not in the new.]
    
    Also, after setting a trap to cleanup in case of a signal, remove it
    again after the cleanup has been done.

--- external/bsd/tmux/prepare-import.sh
+++ external/bsd/tmux/prepare-import.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
-# $NetBSD: prepare-import.sh,v 1.7 2026/02/09 08:36:49 wiz Exp $
+# $NetBSD: prepare-import.sh,v 1.8 2026/06/19 07:49:00 kre Exp $
 #
 # Use this script to recreate the 'dist' subdirectory from a newly released
 # distfile.  The script takes care of unpacking the distfile, removing any
@@ -14,20 +14,23 @@ set -e
 ProgName=${0##*/}
 
 err() {
-	echo "${ProgName}:" "${@}" 1>&2
+	IFS=' '
+	printf '%s: %s\n' "${ProgName}" "${*}" 1>&2
 	exit 1
 }
 
 log() {
-	echo "${ProgName}:" "${@}"
+	IFS=' '
+	printf '%s: %s\n' "${ProgName}" "${*}"
+	unset IFS
 }
 
 backup_dist() {
 	if [ -d dist.old ]; then
-		log "Removing dist; dist.old exists"
+		log 'Removing dist; dist.old exists'
 		rm -rf dist
 	else
-		log "Backing up dist as dist.old"
+		log 'Backing up dist as dist.old'
 		mv dist dist.old
 	fi
 }
@@ -38,21 +41,28 @@ extract_distfile() {
 
 	log "Extracting ${distfile}"
 	tar -xzf "${distfile}"
-	[ -d "${distname}" ] || err "Distfile did not create ${distname}"
-	log "Renaming ${distname} to dist"
+	[ -d "${distname}" ] || err "Distfile did not create '${distname}'"
+	log "Renaming '${distname}' to dist"
 	mv "${distname}" dist
 }
 
 get_distname() {
 	local distfile="${1}"; shift
-	basename "${distfile}" | sed -e 's,\.tar.*,,'
+	basename "${distfile%.tar.*}"
 }
 
 cleanup_dist() {
 	log "Removing unnecessary files from dist"
+
 	find dist -name .deps -exec rm -fr {} +
 	find dist -name .dirstamp -exec rm -f {} +
+
+	# If any of these are no longer in the distfile
+	# the script will abort (-e ...) - remove the
+	# line that is no longer needed.
+
 	rm dist/cmd-parse.c
+
 	rm dist/compat/asprintf.c
 	rm dist/compat/bitstring.h
 	rm dist/compat/closefrom.c
@@ -79,34 +89,41 @@ diff_dirs() {
 	local old_dir="${1}"; shift
 	local new_dir="${1}"; shift
 
-	local old_list=$(mktemp -t tmux-import.XXXXXX)
-	local new_list=$(mktemp -t tmux-import.XXXXXX)
-	local diff=$(mktemp -t tmux-import.XXXXXX)
+	local old_list new_list diff
+
+	old_list=$(mktemp -t tmux-import-o.XXXXXX)
+	new_list=$(mktemp -t tmux-import-n.XXXXXX)
+	diff=$(    mktemp -t tmux-import-d.XXXXXX)
+
 	trap "rm -f '${old_list}' '${new_list}' '${diff}'; exit 1" \
 	    HUP INT QUIT TERM
 
-	( cd "${old_dir}" && find . | sort >>"${old_list}" )
-	( cd "${new_dir}" && find . | sort >>"${new_list}" )
+	( cd "${old_dir}" && find . | sort >"${old_list}" )
+	( cd "${new_dir}" && find . | sort >"${new_list}" )
 
-	diff -u "${old_list}" "${new_list}" | grep '^+\.' >>"${diff}" || true
+	diff -u "${old_list}" "${new_list}" | grep '^+\.' >>"${diff}" || :
 	if [ -s "${diff}" ]; then
-		log "New files found"
+		log 'New files found'
 		diff -u "${old_list}" "${new_list}" | grep '^+\.'
-		log "Check if any files have to be cleaned up and update" \
-		    "the prepare-import.sh script accordingly"
+		log 'Check if any files have to be cleaned up and update' \
+		    'the prepare-import.sh script accordingly'
 	else
-		log "No new files; all good!"
+		log 'No new files; all good!'
 	fi
 
+	# Should something be testing for removed files ?
+
 	rm -f "${old_list}" "${new_list}" "${diff}"
+	trap - HUP INT QUIT TERM
 }
 
 main() {
-	[ ${#} -eq 1 ] || err "Must provide a distfile name"
+	[ ${#} -eq 1 ] || err "Usage: ${ProgName} distfile"
+
 	local distfile="${1}"; shift
 
-	[ -f Makefile -a -f prepare-import.sh ] || \
-	    err "Must be run from the src/external/bsd/tmux subdirectory"
+	[ -f Makefile ] && [ -f prepare-import.sh ] || \
+	    err 'Must be run from the src/external/bsd/tmux subdirectory'
 
 	local distname="$(get_distname ${distfile})"
 
@@ -116,7 +133,7 @@ main() {
 	diff_dirs dist.old dist
 	cleantags dist
 	log "Don't forget to update the -D flags in usr.bin/tmux/Makefile" \
-	    "and to update the version in doc/3RDPARTY"
+	    'and to update the version in doc/3RDPARTY'
 }
 
 main "${@}"
