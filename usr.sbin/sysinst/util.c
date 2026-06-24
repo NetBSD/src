a44eba70587b0dbead6449c3eb7544e29944f59e
martin <martin@NetBSD.org>
2026-06-24 10:33:22 +0000

    PR 60360: make the default /boot partition on evbarm 64MB if we need the dtb set (the newer dtb files overflow the old 32mb default). In ACPI environments where we do not need dtb installed, do not select the set by default.

--- usr.sbin/sysinst/util.c
+++ usr.sbin/sysinst/util.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: util.c,v 1.80 2026/06/22 19:26:01 martin Exp $	*/
+/*	$NetBSD: util.c,v 1.81 2026/06/24 10:33:22 martin Exp $	*/
 
 /*
  * Copyright 1997 Piermont Information Systems Inc.
@@ -231,8 +231,14 @@ init_set_status(int flags)
 
 	for (i = 0; i < __arraycount(sets_valid); i++)
 		set_status[sets_valid[i]] = SET_VALID;
-	for (i = 0; i < nelem_selected; i++)
+	for (i = 0; i < nelem_selected; i++) {
+#ifdef MD_ALLOW_DEFAULT_SET
+		if (!MD_ALLOW_DEFAULT_SET(sets_selected[i]))
+			continue;
+#endif
+
 		set_status[sets_selected[i]] |= SET_SELECTED;
+	}
 
 	set_status[SET_GROUP] = SET_VALID;
 
@@ -816,6 +822,11 @@ set_all_none(menudesc *menu, void *arg, int set, int clr)
 			nested++;
 			continue;
 		}
+#ifdef MD_ALLOW_DEFAULT_SET
+		if (set)
+			if (!MD_ALLOW_DEFAULT_SET(set))
+				continue;
+#endif
 		set_status[dist->set] = (set_status[dist->set] & ~clr) | set;
 	}
 	return 0;
