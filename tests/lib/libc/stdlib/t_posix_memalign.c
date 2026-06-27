c5f879a520a1f853f026e0d50ceb72ea5436fb72
martin <martin@NetBSD.org>
2026-06-27 19:13:11 +0000

    Pull up following revision(s) (requested by riastradh in ticket #1286):
    	lib/libbsdmalloc/bsdmalloc.3: revision 1.1
    	lib/libbsdmalloc/bsdmalloc.3: revision 1.2
    	lib/libbsdmalloc/bsdmalloc.3: revision 1.3
    	distrib/sets/lists/base/shl.mi: revision 1.952
    	tests/lib/libc/stdlib/t_posix_memalign.c: revision 1.8
    	lib/libbsdmalloc/malloc.c: revision 1.3
    	lib/libbsdmalloc/malloc.c: revision 1.4
    	lib/libbsdmalloc/malloc.c: revision 1.5
    	lib/libbsdmalloc/malloc.c: revision 1.10
    	lib/libbsdmalloc/malloc.c: revision 1.6
    	lib/libbsdmalloc/malloc.c: revision 1.11
    	lib/libbsdmalloc/malloc.c: revision 1.7
    	lib/libbsdmalloc/malloc.c: revision 1.8
    	lib/libbsdmalloc/malloc.c: revision 1.9
    	lib/libbsdmalloc/shlib_version: revision 1.2
    	distrib/sets/lists/debug/shl.mi: revision 1.313
    	distrib/sets/lists/comp/mi: revision 1.2434
    	lib/libbsdmalloc/Makefile: revision 1.5
    	lib/libbsdmalloc/Makefile: revision 1.6
    	lib/libbsdmalloc/Makefile: revision 1.7
    	(all via patch)
    
    
    libbsdmalloc: Provide all allocator front ends and fork hooks.
    
    Front ends:
    - aligned_alloc
    - calloc
    - posix_memalign
    
    Fork hooks:
    - _malloc_prefork
    - _malloc_postfork
    - _malloc_postfork_child
    
    Otherwise these will pull in the jemalloc definitions from libc,
    which (a) defeats the purpose, and (b) won't work correctly with
    fork and threads.
    
    Thanks to kre@ and the thread on tech-userlevel for pointing me in
    the right direction to making this actually work to override
    jemalloc:
    https://mail-index.netbsd.org/tech-userlevel/2023/06/30/msg013957.html
    
    Note: libbsdmalloc doesn't set errno=ENOMEM on malloc failure, but
    these front ends do (even aligned_alloc, which is from C11, which
    doesn't define ENOMEM at all, but this pacifies our aligned_alloc
    tests in t_posix_memalign.c).  Might want to fix that.
    
    libbsdmalloc: Nix trailing whitespace.
    No functional change intended.
    
    libbsdmalloc: Bump shlib minor.
    New symbols, new minor.
    
    libbsdmalloc: New man page.
    
    libbsdmalloc: Nix K&R definitions.  Bump WARNS to 3.
    
    libbsdmalloc: Fix build with DEBUG.
    Nix __P while here.
    
    bsdmalloc.3: fix typos in function parameters
    libbsdmalloc: Lift C11 divisibility restriction on aligned_alloc.
    Restriction was lifted in C17.
    
    libbsdmalloc: Make aligned_alloc(A, S) actually align when A > S.
    
    t_posix_memalign: Simplify.
    No functional change intended.
    
    libbsdmalloc: Use a multiply-overflow that clang supports too.
    
    bsdmalloc(3): fix malloc() signature
    
    Make mstats() decl match prototype so this compiles with -DMSTATS.
    
    libbsdmalloc: Fix sense of assertion in aligned_alloc.

--- tests/lib/libc/stdlib/t_posix_memalign.c
+++ tests/lib/libc/stdlib/t_posix_memalign.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: t_posix_memalign.c,v 1.5.10.1 2024/08/24 16:24:04 martin Exp $ */
+/*	$NetBSD: t_posix_memalign.c,v 1.5.10.2 2026/06/27 19:13:11 martin Exp $ */
 
 /*-
  * Copyright (c) 2008 The NetBSD Foundation, Inc.
@@ -32,7 +32,7 @@
 #include <sys/cdefs.h>
 __COPYRIGHT("@(#) Copyright (c) 2008\
  The NetBSD Foundation, inc. All rights reserved.");
-__RCSID("$NetBSD: t_posix_memalign.c,v 1.5.10.1 2024/08/24 16:24:04 martin Exp $");
+__RCSID("$NetBSD: t_posix_memalign.c,v 1.5.10.2 2026/06/27 19:13:11 martin Exp $");
 
 #include <atf-c.h>
 
@@ -189,19 +189,11 @@ ATF_TC_BODY(aligned_alloc_basic, tc)
 				ATF_CHECK_EQ_MSG(p, NULL,
 				    "aligned_alloc(%zu, %zu): %p, %s",
 				    align[i], size[j], p, strerror(errno));
-				switch (errno) {
-				case EINVAL:
-				case ENOMEM:
-					break;
-				default:
-					atf_tc_fail_nonfatal(
-					    "%s:%d:"
-					    " aligned_alloc(%zu, %zu): %s",
-					    __FILE__, __LINE__,
-					    align[i], size[j],
-					    strerror(errno));
-					break;
-				}
+				ATF_CHECK_MSG((errno == EINVAL ||
+					errno == ENOMEM),
+				    "aligned_alloc(%zu, %zu): %s",
+				    align[i], size[j],
+				    strerror(errno));
 				continue;
 			}
 
@@ -219,15 +211,10 @@ ATF_TC_BODY(aligned_alloc_basic, tc)
 			    "aligned_alloc(%zu, %zu): %p",
 			    align[i], size[j], p);
 			if (size[j] != 0) {
-				if (p == NULL) {
-					atf_tc_fail_nonfatal(
-					    "%s:%d:"
-					    " aligned_alloc(&p, %zu, %zu):"
-					    " %p, %s",
-					    __FILE__, __LINE__,
-					    align[i], size[j], p,
-					    strerror(errno));
-				}
+				ATF_CHECK_MSG(p != NULL,
+				    "aligned_alloc(&p, %zu, %zu): %p, %s",
+				    align[i], size[j], p,
+				    strerror(errno));
 			} else {
 				/*
 				 * No guarantees about whether
