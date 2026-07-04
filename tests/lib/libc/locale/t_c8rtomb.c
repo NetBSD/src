377d1b953836c93e8bdf2a2c7656f373c8fea088
martin <martin@NetBSD.org>
2026-07-04 15:44:50 +0000

    Additionally pull up following revision(s) (requested by riastradh in ticket #339):
    	tests/lib/libc/locale/t_c8rtomb.c: revision 1.8
    	lib/libc/locale/c8rtomb.c: revision 1.10
    	lib/libc/citrus/modules/citrus_utf8.c: revision 1.20
    	lib/libc/citrus/modules/citrus_utf8.c: revision 1.21
    	lib/libc/citrus/modules/citrus_utf8.c: revision 1.22
    
    Be truly pedantic about UTF-8 encodings
    
    If we're not going to be accepting "legacy" UTF-8
    (5 and 6 byte encodings for code points >= 0x00200000 which the
    standards don't allow, as they won't fit in UTF-16) then we
    certainly should never be able to generate them, and even more
    should certainly be pedantic about not allowing the various
    forms of mis-coded strings for which there is no justification
    but have been known to be used to attempt to violate security.
    
    This, I believe, now enforces all the current restrictions, eg,
    it will no longer be possible to encode ascii in 2 bytes (0xc0 '.')
    and similar, the shortest legal encoding is all that will be
    accepted (and all that will be generated, but that was always true).
    
    It is quite possible that this will break things, probably many
    tests, as now random garbage won't be accepted as valid, things
    must be properly encodedd.
    mbrtowc() fix a stupid typo in the previous version.
    
    
    No idea how I managed to miss this previously.   This update should
    make at least some of the ATF tests (and other stuff) which failed
    after the previous change start working again.
    
    
    libc: Fix two bugs in UTF-8 decoding and add exhaustive tests.
    1. Despite the recent slew of changes, mbrtowc(3) in UTF-8 locales
       would still fail to return EILSEQ at the first byte where it can,
       because it would silently consume the first two bytes encoding a
       surrogate code point and only reject it after consuming the third
       byte.
    2. The byte classification table for the compressed DFA used by
       c8rtomb(3) was wrong for the bytes c0-c1 and f5-ff.  Somehow the
       program I used, over a decade ago, to generate the compressed DFA
       and classification table got those, and only those, wrong, and I
       can't find that program now, so I'll just have to correct this by
       hand.  Class 10 was missing from the table (but class 11 was not)
       and is obviously the right class for the always-invalid c0-c1 and
       f5-ff because all states transition to UTF8_REJECT (=96) on class
       10, and the same is not true for any other class.
    
    Now mbrtowc(3) in LC_CTYPE=C.UTF_8 is checked against c8rtomb(3)
    systematically to verify they agree on all possible inputs one byte
    at a time.  There are 4501261 distinct such inputs, including the
    valid encodings of all Unicode scalar values, the invalid encodings
    of Unicode surrogate code points, and other invalid encodings.  We
    stop at the first invalid byte, so it is not necessary to examine all
    ~four billion 4-byte strings.  I considered randomly subsampling to
    make this test take less time, but decided that would be too
    confusing.  For debugging purposes, you can run this new test with
    `atf-run -v c8rtomb_all_faliures=yes' to show all failures rather
    than just the first one; this produces megabytes of output with some
    of the bugs we've had, so it's off by default.
    
    PR standards/60369: mbrtowc, mbrlen have wrong return value for some
    invalid byte sequences

--- tests/lib/libc/locale/t_c8rtomb.c
+++ tests/lib/libc/locale/t_c8rtomb.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: t_c8rtomb.c,v 1.7 2024/08/19 16:22:10 riastradh Exp $	*/
+/*	$NetBSD: t_c8rtomb.c,v 1.7.4.1 2026/07/04 15:44:50 martin Exp $	*/
 
 /*-
  * Copyright (c) 2002 Tim J. Robbins
@@ -33,16 +33,38 @@
  */
 
 #include <sys/cdefs.h>
-__RCSID("$NetBSD: t_c8rtomb.c,v 1.7 2024/08/19 16:22:10 riastradh Exp $");
+__RCSID("$NetBSD: t_c8rtomb.c,v 1.7.4.1 2026/07/04 15:44:50 martin Exp $");
 
 #include <errno.h>
 #include <limits.h>
 #include <locale.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <uchar.h>
+#include <wchar.h>
 
 #include <atf-c.h>
+
+#define	MIN(a, b)	((a) < (b) ? (a) : (b))
+
+static void
+hexdump(FILE *fp, const char *title, const void *buffer, size_t len)
+{
+	const uint8_t *p = buffer;
+	size_t i;
+
+	fprintf(fp, "# %s (%zu bytes)\n", title, len);
+	for (i = 0; i < len; i++) {
+		if ((i % 8) == 0)
+			fprintf(fp, " ");
+		fprintf(fp, " %02hhx", p[i]);
+		if ((i % 16) == 15)
+			fprintf(fp, "\n");
+	}
+	if (i % 16)
+		fprintf(fp, "\n");
+}
 
 static void
 require_lc_ctype(const char *locale_name)
@@ -342,13 +364,153 @@ ATF_TC_BODY(c8rtomb_utf_8_test, tc)
 	    "buf=[%02x %02x]", buf[0], buf[1]);
 }
 
+ATF_TC_WITHOUT_HEAD(c8rtomb_exhaustive);
+ATF_TC_BODY(c8rtomb_exhaustive, tc)
+{
+	const char *all_failures_var =
+	    atf_tc_get_config_var_wd(tc, "c8rtomb_all_failures", "no");
+	void (*fail)(const char *, ...) __printflike(1,2) =
+	    strcasecmp(all_failures_var, "yes") == 0
+	    ? atf_tc_fail_nonfatal
+	    : atf_tc_fail;
+	mbstate_t in[5] = {0};
+	mbstate_t out[5] = {0};
+	unsigned d;
+
+	require_lc_ctype("C.UTF-8");
+
+	d = 0;
+	for (;;) {
+		char outbuf[MB_LEN_MAX];
+		wchar_t wc;
+		size_t n;
+
+		ATF_REQUIRE_MSG(d < 4, "d=%u", d);
+
+		in[d + 1] = in[d];
+		n = mbrtowc(&wc, (const char *)&buf[d], 1, &in[d + 1]);
+		switch (n) {
+		case (size_t)-2:	/* incomplete */
+			out[d + 1] = out[d];
+			n = c8rtomb(outbuf, buf[d], &out[d + 1]);
+			switch (n) {
+			case (size_t)-1: {
+				int error = errno;
+
+				hexdump(stderr, "buf", buf, d + 1);
+				(*fail)("c8rtomb: error on incomplete: %s",
+				    strerror(error));
+				break;
+			}
+			case 0:
+				if (d == 3) {
+					hexdump(stderr, "buf", buf, d + 1);
+					(*fail)("mbrtowc:"
+					    " overlong input incomplete");
+					break;
+				}
+				d++;		/* push */
+				break;
+			default:
+				hexdump(stderr, "buf", buf, d + 1);
+				(*fail)("c8rtomb: output on incomplete");
+				break;
+			}
+			break;
+		case (size_t)-1:	/* invalid */
+			out[d + 1] = out[d];
+			n = c8rtomb(outbuf, buf[d], &out[d + 1]);
+			switch (n) {
+			case (size_t)-1:
+				break;
+			case 0:
+				hexdump(stderr, "buf", buf, d + 1);
+				(*fail)("c8rtomb: incomplete on invalid");
+				break;
+			default:
+				hexdump(stderr, "buf", buf, d + 1);
+				hexdump(stderr, "outbuf", outbuf,
+				    MIN(n, sizeof(outbuf)));
+				(*fail)("c8rtomb: accepted invalid");
+				break;
+			}
+			break;
+		case 0:			/* NUL */
+			if (buf[d] != '\0') {
+				hexdump(stderr, "buf", buf, d + 1);
+				(*fail)("mbrtowc: bogus NUL");
+				break;
+			}
+			out[d + 1] = out[d];
+			n = c8rtomb(outbuf, buf[d], &out[d + 1]);
+			switch (n) {
+			case (size_t)-1: {
+				int error = errno;
+
+				hexdump(stderr, "buf", buf, d + 1);
+				(*fail)("c8rtomb: error on NUL: %s",
+				    strerror(error));
+				break;
+			}
+			case 0:
+				hexdump(stderr, "buf", buf, d + 1);
+				(*fail)("c8rtomb: incomplete on NUL");
+				break;
+			case 1:
+				if (outbuf[0] == '\0')
+					break;
+				/*FALLTHROUGH*/
+			default:
+				hexdump(stderr, "buf", buf, d + 1);
+				hexdump(stderr, "outbuf", outbuf,
+				    MIN(n, sizeof(outbuf)));
+				(*fail)("c8rtomb: bogus on NUL, n=%zu", n);
+			}
+			break;
+		case 1:			/* valid char after 1 byte */
+			out[d + 1] = out[d];
+			n = c8rtomb(outbuf, buf[d], &out[d + 1]);
+			switch (n) {
+			case (size_t)-1:
+				hexdump(stderr, "buf", buf, d + 1);
+				(*fail)("c8rtomb: rejected valid");
+				break;
+			case 0:
+				hexdump(stderr, "buf", buf, d + 1);
+				(*fail)("c8rtomb: incomplete valid");
+				break;
+			default:
+				if (n == d + 1 && memcmp(outbuf, buf, n) == 0)
+					break;
+				hexdump(stderr, "buf", buf, d + 1);
+				hexdump(stderr, "outbuf", outbuf,
+				    MIN(n, sizeof(outbuf)));
+				(*fail)("c8rtomb: bogus, n=%zu", n);
+			}
+			break;
+		default:
+			hexdump(stderr, "buf", buf, d + 1);
+			(*fail)("mbrtowc: bogus result: %zu", n);
+			break;
+		}
+
+		while (++buf[d] == 0) {
+			if (d-- == 0)		/* pop */
+				goto out;
+		}
+	}
+
+out:	return;
+}
+
 ATF_TP_ADD_TCS(tp)
 {
 
 	ATF_TP_ADD_TC(tp, c8rtomb_c_locale_test);
+	ATF_TP_ADD_TC(tp, c8rtomb_exhaustive);
 	ATF_TP_ADD_TC(tp, c8rtomb_iso2022jp_locale_test);
-	ATF_TP_ADD_TC(tp, c8rtomb_iso_8859_1_test);
 	ATF_TP_ADD_TC(tp, c8rtomb_iso_8859_15_test);
+	ATF_TP_ADD_TC(tp, c8rtomb_iso_8859_1_test);
 	ATF_TP_ADD_TC(tp, c8rtomb_utf_8_test);
 
 	return (atf_no_error());
