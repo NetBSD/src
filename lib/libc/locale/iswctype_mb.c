dfc6e7e660c9ed6975ad90404a35bd7fbfe4b26f
kre <kre@NetBSD.org>
2026-07-10 06:48:54 +0000

    PR lib/59067 (wctrans got error member)
    Patches from the OP (ru_j217) and from RVP - see the PR
    
    This looks to be just correcting what appear to be simple
    errors in the code.
    
    XXX pullup -11

--- lib/libc/locale/iswctype_mb.c
+++ lib/libc/locale/iswctype_mb.c
@@ -1,4 +1,4 @@
-/* $NetBSD: iswctype_mb.c,v 1.14 2024/06/07 13:53:23 riastradh Exp $ */
+/* $NetBSD: iswctype_mb.c,v 1.15 2026/07/10 06:48:54 kre Exp $ */
 
 /*-
  * Copyright (c)2008 Citrus Project,
@@ -28,7 +28,7 @@
 
 #include <sys/cdefs.h>
 #if defined(LIBC_SCCS) && !defined(lint)
-__RCSID("$NetBSD: iswctype_mb.c,v 1.14 2024/06/07 13:53:23 riastradh Exp $");
+__RCSID("$NetBSD: iswctype_mb.c,v 1.15 2026/07/10 06:48:54 kre Exp $");
 #endif /* LIBC_SCCS and not lint */
 
 #include "namespace.h"
@@ -129,7 +129,7 @@ wctrans_l(const char *charmap, locale_t loc)
 	for (i = 0; i < _WCTRANS_NINDEXES; ++i) {
 		_DIAGASSERT(rl->rl_wctrans[i].te_name != NULL);
 		if (!strcmp(rl->rl_wctrans[i].te_name, charmap))
-			return (wctrans_t)__UNCONST(&rl->rl_wctype[i]);
+			return (wctrans_t)__UNCONST(&rl->rl_wctrans[i]);
 	}
 	return (wctrans_t)NULL;
 }
