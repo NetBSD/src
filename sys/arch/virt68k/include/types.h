162bfd776a442ef7c56e0bc98d49070d88db72d2
thorpej <thorpej@NetBSD.org>
2026-07-21 14:35:28 +0000

    Hide some machine-specific items from _MODULE.

--- sys/arch/virt68k/include/types.h
+++ sys/arch/virt68k/include/types.h
@@ -1,10 +1,12 @@
-/*	$NetBSD: types.h,v 1.1 2024/01/02 07:41:01 thorpej Exp $	*/
+/*	$NetBSD: types.h,v 1.2 2026/07/21 14:35:28 thorpej Exp $	*/
 
-#ifndef _MACHINE_TYPES_H_
-#define	_MACHINE_TYPES_H_
+#ifndef _VIRT68K_TYPES_H_
+#define	_VIRT68K_TYPES_H_
 
 #include <m68k/types.h>
 
+#if defined(_KERNEL) && !defined(_MODULE)
 #define	__HAVE_DEVICE_REGISTER
-
 #endif
+
+#endif /* _VIRT68K_TYPES_H_ */
