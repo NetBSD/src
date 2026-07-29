f2b5bb188f078601fd6ce87ed8562b5ae65f6d69
sborrill <sborrill@NetBSD.org>
2026-07-29 10:16:44 +0000

    Pull up the following revisions(s) (requested by joe in ticket #1315): 	sys/dev/raidframe/rf_disks.c:	revision 1.96-1.97
    Use the right assertions to ensure that all IOs are suspended.
    zero indicates that IOs resume.
    PR kern/60477

--- sys/dev/raidframe/rf_disks.c
+++ sys/dev/raidframe/rf_disks.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: rf_disks.c,v 1.93.4.2 2024/05/04 12:04:56 martin Exp $	*/
+/*	$NetBSD: rf_disks.c,v 1.93.4.3 2026/07/29 10:16:44 sborrill Exp $	*/
 /*-
  * Copyright (c) 1999 The NetBSD Foundation, Inc.
  * All rights reserved.
@@ -60,7 +60,7 @@
  ***************************************************************/
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: rf_disks.c,v 1.93.4.2 2024/05/04 12:04:56 martin Exp $");
+__KERNEL_RCSID(0, "$NetBSD: rf_disks.c,v 1.93.4.3 2026/07/29 10:16:44 sborrill Exp $");
 
 #include <dev/raidframe/raidframevar.h>
 
@@ -1266,7 +1266,7 @@ rf_swap_components(RF_Raid_t *raidPtr, int a, int b)
 
 
 	/* This function *MUST* be called with all IO suspended. */
-	RF_ASSERT(raidPtr->accesses_suspended == 0);
+	RF_ASSERT(raidPtr->accesses_suspended > 0);
 	
 	/* Swap the component names... */
 	snprintf(tmpdevname, sizeof(tmpdevname), "%s", raidPtr->Disks[a].devname);
