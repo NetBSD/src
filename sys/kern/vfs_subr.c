74cba11df0117ba5c3ce12773ee9ff707d136a4e
riastradh <riastradh@NetBSD.org>
2026-08-06 22:28:23 +0000

    vflushbuf: Print `vflushbuf: dirty' warning only once per call.
    And do it only under DEBUG, and rate-limit it globally.
    
    This can happen when vflushbuf is competing with an onslaught of
    concurrent I/O on a snapshot or block device (which is done without
    holding the vnode lock, despite what the comment says).  That
    concurrent I/O might be happening indefinitely, so we are likely to
    have to print a _lot_ of warnings, potentially overwhelming the
    console, until the underlying problem is fixed by teaching vflushbuf
    to wait only for the writes that began before have completed, not
    also all writes that have begun since.
    
    This stop-gap measure intended to be low-risk for pullup to release
    branches.
    
    PR kern/60531: looping "vflushbuf: dirty" message

--- sys/kern/vfs_subr.c
+++ sys/kern/vfs_subr.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: vfs_subr.c,v 1.503 2026/05/08 04:18:32 perseant Exp $	*/
+/*	$NetBSD: vfs_subr.c,v 1.504 2026/08/06 22:28:23 riastradh Exp $	*/
 
 /*-
  * Copyright (c) 1997, 1998, 2004, 2005, 2007, 2008, 2019, 2020
@@ -69,7 +69,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: vfs_subr.c,v 1.503 2026/05/08 04:18:32 perseant Exp $");
+__KERNEL_RCSID(0, "$NetBSD: vfs_subr.c,v 1.504 2026/08/06 22:28:23 riastradh Exp $");
 
 #ifdef _KERNEL_OPT
 #include "opt_compat_43.h"
@@ -338,6 +338,9 @@ vflushbuf(struct vnode *vp, int flags)
 	struct buf *bp, *nbp;
 	int error, pflags;
 	bool dirty, sync;
+#ifdef DEBUG
+	bool warned = false;
+#endif
 
 	sync = (flags & FSYNC_WAIT) != 0;
 	pflags = PGO_CLEANIT | PGO_ALLPAGES |
@@ -382,7 +385,18 @@ loop:
 	mutex_exit(vp->v_interlock);
 
 	if (dirty) {
-		vprint("vflushbuf: dirty", vp);
+#ifdef DEBUG
+		if (!warned) {
+			static struct timeval vflushbuf_warntime;
+			const struct timeval interval = {60,0};
+
+			mutex_enter(&bufcache_lock);
+			if (ratecheck(&vflushbuf_warntime, &interval))
+				vprint("vflushbuf: dirty", vp);
+			mutex_exit(&bufcache_lock);
+			warned = true;
+		}
+#endif
 		goto loop;
 	}
 
