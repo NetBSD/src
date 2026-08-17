5b8e3be5d22c66214b554eca0be50c831cf38f84
martin <martin@NetBSD.org>
2026-08-17 15:44:49 +0000

    Pull up following revision(s) (requested by msaitoh in ticket #1324):
    	usr.sbin/syslogd/syslogd.c: revision 1.156
    	usr.sbin/syslogd/syslogd.c: revision 1.157
    
    syslogd: Fix buffering of partial klog lines
    
     When a /dev/klog read ends without a newline, the remaining data
    is buffered for the next read. The code mistakenly copied the data
    into linebuf instead of klog_linebuf, breaking reconstruction of
    split log messages.
    
    Found and fixed by nonaka@ and Kenichi Suzuki at IIJ.
    
    
    syslogd: Preserve full partial klog line when buffering
    
     Buffer incomplete /dev/klog lines from the start of the original
    line instead of the parse position. Otherwise, split messages
    lose the BSD syslog priority prefix when reconstructed.
    
    Fixed by Kenichi Suzuki at IIJ.

--- usr.sbin/syslogd/syslogd.c
+++ usr.sbin/syslogd/syslogd.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: syslogd.c,v 1.140.2.2 2026/07/07 14:49:16 sborrill Exp $	*/
+/*	$NetBSD: syslogd.c,v 1.140.2.3 2026/08/17 15:44:49 martin Exp $	*/
 
 /*
  * Copyright (c) 1983, 1988, 1993, 1994
@@ -39,7 +39,7 @@ __COPYRIGHT("@(#) Copyright (c) 1983, 1988, 1993, 1994\
 #if 0
 static char sccsid[] = "@(#)syslogd.c	8.3 (Berkeley) 4/4/94";
 #else
-__RCSID("$NetBSD: syslogd.c,v 1.140.2.2 2026/07/07 14:49:16 sborrill Exp $");
+__RCSID("$NetBSD: syslogd.c,v 1.140.2.3 2026/08/17 15:44:49 martin Exp $");
 #endif
 #endif /* not lint */
 
@@ -1580,6 +1580,7 @@ printsys(char *msg)
 	klog_linebufoff = 0;
 	for (p = msg; *p != '\0'; ) {
 		bool bsdsyslog = true;
+		char *line_start = p;
 
 		is_printf = 1;
 		flags = ISKERNEL | ADDDATE | BSDSYSLOG;
@@ -1610,7 +1611,7 @@ printsys(char *msg)
 		if (*q != '\0')
 			*q++ = '\0';
 		else {
-			memcpy(linebuf, p, klog_linebufoff = q - p);
+			memcpy(klog_linebuf, line_start, klog_linebufoff = q - line_start);
 			break;
 		}
 
