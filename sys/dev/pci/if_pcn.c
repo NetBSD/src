d2b56d208fa9697bd3359450021bd0eb3488543c
martin <martin@NetBSD.org>
2026-08-25 18:55:12 +0000

    Pull up following revision(s) (requested by tls in ticket #429):
    	sys/dev/pci/if_pcn.c: revision 1.82
    	sys/dev/pci/if_pcn.c: revision 1.83
    
    Fix pcn MAC address read on QEMU and possibly some other emulated hardware.
    
    Correct typo in previous (s/1/i/) and remove gratuitous wsp (build fix)

--- sys/dev/pci/if_pcn.c
+++ sys/dev/pci/if_pcn.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: if_pcn.c,v 1.80 2024/11/10 11:45:25 mlelstv Exp $	*/
+/*	$NetBSD: if_pcn.c,v 1.80.2.1 2026/08/25 18:55:12 martin Exp $	*/
 
 /*
  * Copyright (c) 2001 Wasabi Systems, Inc.
@@ -65,7 +65,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: if_pcn.c,v 1.80 2024/11/10 11:45:25 mlelstv Exp $");
+__KERNEL_RCSID(0, "$NetBSD: if_pcn.c,v 1.80.2.1 2026/08/25 18:55:12 martin Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -640,9 +640,26 @@ pcn_attach(device_t parent, device_t self, void *aux)
 			enaddr[2 * i + 1] = (val >> 8) & 0xff;
 		}
 	} else {
-		for (i = 0; i < ETHER_ADDR_LEN; i++) {
-			enaddr[i] = bus_space_read_1(sc->sc_st, sc->sc_sh,
+		/*
+		 * Read the address ROM 4 bytes at a time; works on
+		 * real hardware, and accommodates some emulators which
+		 * believe that after pcn_reset(), we are in DWIO mode
+		 * where byte access is not supported.
+		 */
+		for (i = 0; i < ETHER_ADDR_LEN; i+= 4) {
+			uint32_t word;
+			word = bus_space_read_4(sc->sc_st, sc->sc_sh,
 			    PCN32_APROM + i);
+			enaddr[i] = word & 0xff;
+			if (i + 1 < ETHER_ADDR_LEN) {
+				enaddr[i + 1] = (word >> 8) & 0xff;
+			}
+			if (i + 2 < ETHER_ADDR_LEN) {
+				enaddr[i + 2] = (word >> 16) & 0xff;
+			}
+			if (i + 3 < ETHER_ADDR_LEN) {
+				enaddr[i + 3] = (word >> 24) & 0xff;
+			}
 		}
 	}
 
