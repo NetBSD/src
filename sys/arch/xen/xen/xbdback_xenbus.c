c4a898c295324143b0b8f3a8252f2ce849cad9c2
martin <martin@NetBSD.org>
2026-09-13 13:55:42 +0000

    Pull up following revision(s) (requested by bouyer in ticket #1338):
    	sys/arch/xen/xen/xbdback_xenbus.c: revision 1.109
    
    Fix a brain-o in a KASSERT that could cause a guest to panic a DIAGNOSTIC
    kernel: to assert that an I/O is already in flight we want
    xbdi_pendingreqs > 0, not xbdi_pendingreqs > 1
    
    While there, fix an error path that could also cause a KASSERT to fire
    or a state corruption: reset xio_need_bounce to 0 before calling
    xbdback_io_error() when the bounce buffer is not fully set up.

--- sys/arch/xen/xen/xbdback_xenbus.c
+++ sys/arch/xen/xen/xbdback_xenbus.c
@@ -1,4 +1,4 @@
-/*      $NetBSD: xbdback_xenbus.c,v 1.101.4.2 2024/06/22 18:30:13 martin Exp $      */
+/*      $NetBSD: xbdback_xenbus.c,v 1.101.4.3 2026/09/13 13:55:42 martin Exp $      */
 
 /*
  * Copyright (c) 2006,2024 Manuel Bouyer.
@@ -26,7 +26,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: xbdback_xenbus.c,v 1.101.4.2 2024/06/22 18:30:13 martin Exp $");
+__KERNEL_RCSID(0, "$NetBSD: xbdback_xenbus.c,v 1.101.4.3 2026/09/13 13:55:42 martin Exp $");
 
 #include <sys/buf.h>
 #include <sys/condvar.h>
@@ -1353,6 +1353,7 @@ xbdback_co_io_gotio(struct xbdback_instance *xbdi, void *obj)
 				    seg->first_sect, seg->last_sect);
 			}
 			xbdi->xbdi_pendingreqs++; /* xbdback_io_error will -- */
+			xbd_io->xio_need_bounce = 0; /* bouncebuf not setup yet */
 			xbdback_io_error(xbd_io, EINVAL);
 			/* do not retry */
 			xbdi->xbdi_cont = xbdback_co_main_incr;
@@ -1395,7 +1396,7 @@ xbdback_co_io_gotio(struct xbdback_instance *xbdi, void *obj)
 	xbd_io->xio_buf.b_bcount = bcount;
 	if (__predict_false(xbd_io->xio_need_bounce)) {
 		if (__predict_false(xbdi->xbdi_bouncebuf_use)) {
-			KASSERT(xbdi->xbdi_pendingreqs > 1);
+			KASSERT(xbdi->xbdi_pendingreqs > 0);
 			/* retry later */
 			xbdi->xbdi_cont_restart = xbdback_co_io_gotio;
 			xbdi->xbdi_cont_restart_obj = xbd_io;
