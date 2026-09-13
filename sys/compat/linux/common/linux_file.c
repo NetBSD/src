f4848010b142a60da6002c4594cbcb979ad85c0a
martin <martin@NetBSD.org>
2026-09-13 10:12:35 +0000

    Pull up following revision(s) (requested by sborrill in ticket #454):
    	sys/compat/linux/common/linux_file.c: revision 1.135
    
    Fix edge-case panic in copy_file_range when off_in or off_out are not NULL.
    
    Found by Henrique Brito as part of GSoC 2024.

--- sys/compat/linux/common/linux_file.c
+++ sys/compat/linux/common/linux_file.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: linux_file.c,v 1.133 2024/10/01 17:46:51 riastradh Exp $	*/
+/*	$NetBSD: linux_file.c,v 1.133.2.1 2026/09/13 10:12:35 martin Exp $	*/
 
 /*-
  * Copyright (c) 1995, 1998, 2008 The NetBSD Foundation, Inc.
@@ -35,7 +35,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: linux_file.c,v 1.133 2024/10/01 17:46:51 riastradh Exp $");
+__KERNEL_RCSID(0, "$NetBSD: linux_file.c,v 1.133.2.1 2026/09/13 10:12:35 martin Exp $");
 
 #include <sys/types.h>
 #include <sys/param.h>
@@ -1123,11 +1123,9 @@ linux_sys_copy_file_range(lwp_t *l,
 		DPRINTF("%s: Input or output is a directory\n", __func__);
 		goto out;
 	}
-	if ((SCARG(uap, off_in) != NULL && *SCARG(uap, off_in) < 0) ||
-	    (SCARG(uap, off_out) != NULL && *SCARG(uap, off_out) < 0) ||
-	    vattr_in.va_type != VREG || vattr_out.va_type != VREG) {
+	if (vattr_in.va_type != VREG || vattr_out.va_type != VREG) {
 		error = EINVAL;
-		DPRINTF("%s: Invalid offset or file type\n", __func__);
+		DPRINTF("%s: Invalid file type\n", __func__);
 		goto out;
 	}
 
