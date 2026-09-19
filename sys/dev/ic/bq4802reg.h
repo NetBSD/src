deac6ea566403197ee482f2a4347af10c4f9d353
martin <martin@NetBSD.org>
2026-09-19 16:18:05 +0000

    Pull up following revision(s) (requested by jdc in ticket #479):
    	share/man/man4/bq4802rtc.4: revision 1.1
    	sys/arch/sparc64/conf/files.sparc64: revision 1.171
    	sys/dev/ic/bq4802reg.h: revision 1.3
    	distrib/sets/lists/man/mi: revision 1.1836
    	distrib/sets/lists/manhtml/mi: revision 1.54
    	sys/arch/sparc64/dev/bq4802_ebus.c: revision 1.3
    	share/man/man4/Makefile: revision 1.753
    
    Add a note about the validity of the battery-valid flag.
    
    Add a sysctl to control the clock oscillator.
    
    Only write to the chip at attach time if the configuration needs changing.
    
    Add a manual page for bq4802rtc(4) (Texas Instruments bq4802Y/bq4802LY).
    
    Add build glue (Makefile and set lists) for bq4802rtc.4 manual page.
    
    Add sysmon_envsys to bq4802rtc.

--- sys/dev/ic/bq4802reg.h
+++ sys/dev/ic/bq4802reg.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: bq4802reg.h,v 1.3.2.2 2026/09/13 10:39:46 martin Exp $	*/
+/*	$NetBSD: bq4802reg.h,v 1.3.2.3 2026/09/19 16:18:05 martin Exp $	*/
 
 /*-
  * Copyright (c) 2026 The NetBSD Foundation, Inc.
@@ -109,7 +109,7 @@
 #define	BQ4802_EN_AIE	0x08	/* Alarm interrupt enable */
 
 /* Flags */
-#define	BQ4802_FLG_BVF	0x01	/* Battery-valid flag */
+#define	BQ4802_FLG_BVF	0x01	/* Battery-valid flag (might not get unset) */
 #define	BQ4802_FLG_PWRF	0x02	/* Power-fail interrupt flag */
 #define	BQ4802_FLG_PF	0x04	/* Periodic interrupt flag */
 #define	BQ4802_FLG_AF	0x08	/* Alarm interrupt flag */
