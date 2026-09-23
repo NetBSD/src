8122b971781082591f6f2533dc13997dcefc5477
riastradh <riastradh@NetBSD.org>
2026-09-23 18:25:56 +0000

    t_fdrestart: Test restarting poll and select on close too.
    They must promptly report POLLNVAL/EBADF when a file descriptor they
    are listening for is closed, just like read and write.
    
    Note: kevent doesn't get restarted when an fd registered with it is
    closed; the registration is simply silently discarded instead.  Hence
    no corresponding kevent tests here.
    
    PR kern/57659: closing pipe writefd fails to wake concurrent write on
    same writefd

--- tests/kernel/t_fdrestart.c
+++ tests/kernel/t_fdrestart.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: t_fdrestart.c,v 1.4 2023/11/18 19:46:55 riastradh Exp $	*/
+/*	$NetBSD: t_fdrestart.c,v 1.5 2026/09/23 18:25:56 riastradh Exp $	*/
 
 /*-
  * Copyright (c) 2023 The NetBSD Foundation, Inc.
@@ -29,14 +29,16 @@
 #define	_KMEMUSER		/* ERESTART */
 
 #include <sys/cdefs.h>
-__RCSID("$NetBSD: t_fdrestart.c,v 1.4 2023/11/18 19:46:55 riastradh Exp $");
+__RCSID("$NetBSD: t_fdrestart.c,v 1.5 2026/09/23 18:25:56 riastradh Exp $");
 
 #include <sys/ioctl.h>
+#include <sys/select.h>
 #include <sys/socket.h>
 #include <sys/un.h>
 
 #include <atf-c.h>
 #include <errno.h>
+#include <poll.h>
 #include <pthread.h>
 #include <unistd.h>
 
@@ -86,7 +88,7 @@ doread(struct fdrestart *F)
 	nread = rump_sys_read(F->fd, &c, sizeof(c));
 	ATF_REQUIRE_EQ_MSG(nread, -1, "nread=%zd", nread);
 	error = errno;
-	ATF_REQUIRE_EQ_MSG(error, ERESTART, "errno=%d (%s)", error,
+	ATF_CHECK_EQ_MSG(error, ERESTART, "errno=%d (%s)", error,
 	    strerror(error));
 
 	/*
@@ -96,8 +98,102 @@ doread(struct fdrestart *F)
 	nread = rump_sys_read(F->fd, &c, sizeof(c));
 	ATF_REQUIRE_EQ_MSG(nread, -1, "nread=%zd", nread);
 	error = errno;
-	ATF_REQUIRE_EQ_MSG(error, EBADF, "errno=%d (%s)", error,
+	ATF_CHECK_EQ_MSG(error, EBADF, "errno=%d (%s)", error,
 	    strerror(error));
+}
+
+static void
+dopollread(struct fdrestart *F)
+{
+	struct pollfd pfd;
+	int nfds;
+
+	/*
+	 * Prepare poll inputs so we can start ASAP when we cross the
+	 * barrier.
+	 */
+	memset(&pfd, 0, sizeof(pfd));
+	pfd.fd = F->fd;
+	pfd.events = POLLIN;
+
+	/*
+	 * Wait for the other thread to be ready.
+	 */
+	waitforbarrier(F, "reader");
+
+	/*
+	 * Wait for readability.  This should block, and then, when the
+	 * other thread closes the fd, should be woken to fail with
+	 * ERESTART.
+	 */
+	nfds = rump_sys_poll(&pfd, 1, -1);
+	ATF_REQUIRE_EQ_MSG(nfds, 1, "nfds=%d", nfds);
+	ATF_CHECK_EQ_MSG(pfd.revents, POLLNVAL, "revents=0x%x", pfd.revents);
+
+	/*
+	 * Further attempts to poll should return POLLNVAL immediately
+	 * because the fd has been closed.
+	 */
+	memset(&pfd, 0, sizeof(pfd));
+	pfd.fd = F->fd;
+	pfd.events = POLLOUT;
+	nfds = rump_sys_poll(&pfd, 1, -1);
+	ATF_REQUIRE_EQ_MSG(nfds, 1, "nfds=%d", nfds);
+	ATF_CHECK_EQ_MSG(pfd.revents, POLLNVAL, "revents=0x%x", pfd.revents);
+}
+
+static void
+doselectread(struct fdrestart *F)
+{
+	fd_set rfd;
+	int nfds;
+
+	/*
+	 * Prepare select inputs so we can start ASAP when we cross the
+	 * barrier.
+	 */
+	FD_ZERO(&rfd);
+	FD_SET(F->fd, &rfd);
+
+	/*
+	 * Wait for the other thread to be ready.
+	 */
+	waitforbarrier(F, "reader");
+
+	/*
+	 * Wait for readability.  This should block, and then, when the
+	 * other thread closes the fd, should be woken to fail with
+	 * ERESTART.
+	 */
+	nfds = rump_sys_select(F->fd + 1, &rfd, /*wfd*/NULL, /*efd*/NULL,
+	    /*timeout*/NULL);
+	ATF_CHECK_ERRNO(EBADF, nfds == -1);
+
+	/*
+	 * Further attempts to select should fail with EBADF
+	 * immediately because the fd has been closed.
+	 */
+	FD_ZERO(&rfd);
+	FD_SET(F->fd, &rfd);
+	nfds = rump_sys_select(F->fd + 1, &rfd, /*wfd*/NULL, /*efd*/NULL,
+	    /*timeout*/NULL);
+	ATF_CHECK_ERRNO(EBADF, nfds == -1);
+}
+
+static void
+fillpipebuf(int fd)
+{
+	static const char buf[1024*1024]; /* XXX >BIG_PIPE_SIZE */
+
+	for (;;) {
+		int nspace;
+
+		RL(rump_sys_ioctl(fd, FIONSPACE, &nspace));
+		ATF_REQUIRE_MSG(nspace >= 0, "nspace=%d", nspace);
+		if (nspace == 0)
+			break;
+		RL(rump_sys_write(fd, buf, (size_t)nspace));
+	}
 }
 
 static void
@@ -110,15 +206,7 @@ dowrite(struct fdrestart *F)
 	/*
 	 * Make sure the pipe's buffer is full first.
 	 */
-	for (;;) {
-		int nspace;
-
-		RL(rump_sys_ioctl(F->fd, FIONSPACE, &nspace));
-		ATF_REQUIRE_MSG(nspace >= 0, "nspace=%d", nspace);
-		if (nspace == 0)
-			break;
-		RL(rump_sys_write(F->fd, buf, (size_t)nspace));
-	}
+	fillpipebuf(F->fd);
 
 	/*
 	 * Wait for the other thread to be ready.
@@ -132,7 +220,7 @@ dowrite(struct fdrestart *F)
 	nwrit = rump_sys_write(F->fd, buf, sizeof(buf));
 	ATF_REQUIRE_EQ_MSG(nwrit, -1, "nwrit=%zd", nwrit);
 	error = errno;
-	ATF_REQUIRE_EQ_MSG(error, ERESTART, "errno=%d (%s)", error,
+	ATF_CHECK_EQ_MSG(error, ERESTART, "errno=%d (%s)", error,
 	    strerror(error));
 
 	/*
@@ -142,8 +230,96 @@ dowrite(struct fdrestart *F)
 	nwrit = rump_sys_write(F->fd, buf, sizeof(buf));
 	ATF_REQUIRE_EQ_MSG(nwrit, -1, "nwrit=%zd", nwrit);
 	error = errno;
-	ATF_REQUIRE_EQ_MSG(error, EBADF, "errno=%d (%s)", error,
+	ATF_CHECK_EQ_MSG(error, EBADF, "errno=%d (%s)", error,
 	    strerror(error));
+}
+
+static void
+dopollwrite(struct fdrestart *F)
+{
+	struct pollfd pfd;
+	int nfds;
+
+	/*
+	 * Make sure the pipe's buffer is full first.
+	 */
+	fillpipebuf(F->fd);
+
+	/*
+	 * Prepare poll inputs so we can start ASAP when we cross the
+	 * barrier.
+	 */
+	memset(&pfd, 0, sizeof(pfd));
+	pfd.fd = F->fd;
+	pfd.events = POLLOUT;
+
+	/*
+	 * Wait for the other thread to be ready.
+	 */
+	waitforbarrier(F, "writer");
+
+	/*
+	 * Wait for writability.  This should block, and then, when the
+	 * other thread closes the fd, should be woken to fail with
+	 * ERESTART.
+	 */
+	nfds = rump_sys_poll(&pfd, 1, -1);
+	ATF_REQUIRE_EQ_MSG(nfds, 1, "nfds=%d", nfds);
+	ATF_CHECK_EQ_MSG(pfd.revents, POLLNVAL, "revents=0x%x", pfd.revents);
+
+	/*
+	 * Further attempts to poll should return POLLNVAL immediately
+	 * because the fd has been closed.
+	 */
+	memset(&pfd, 0, sizeof(pfd));
+	pfd.fd = F->fd;
+	pfd.events = POLLOUT;
+	nfds = rump_sys_poll(&pfd, 1, -1);
+	ATF_REQUIRE_EQ_MSG(nfds, 1, "nfds=%d", nfds);
+	ATF_CHECK_EQ_MSG(pfd.revents, POLLNVAL, "revents=0x%x", pfd.revents);
+}
+
+static void
+doselectwrite(struct fdrestart *F)
+{
+	fd_set wfd;
+	int nfds;
+
+	/*
+	 * Make sure the pipe's buffer is full first.
+	 */
+	fillpipebuf(F->fd);
+
+	/*
+	 * Prepare select inputs so we can start ASAP when we cross the
+	 * barrier.
+	 */
+	FD_ZERO(&wfd);
+	FD_SET(F->fd, &wfd);
+
+	/*
+	 * Wait for the other thread to be ready.
+	 */
+	waitforbarrier(F, "writer");
+
+	/*
+	 * Wait for writability.  This should block, and then, when the
+	 * other thread closes the fd, should be woken to fail with
+	 * ERESTART.
+	 */
+	nfds = rump_sys_select(F->fd + 1, /*rfd*/NULL, &wfd, /*efd*/NULL,
+	    /*timeout*/NULL);
+	ATF_CHECK_ERRNO(EBADF, nfds == -1);
+
+	/*
+	 * Further attempts to select should fail with EBADF
+	 * immediately because the fd has been closed.
+	 */
+	FD_ZERO(&wfd);
+	FD_SET(F->fd, &wfd);
+	nfds = rump_sys_select(F->fd + 1, /*rfd*/NULL, &wfd, /*efd*/NULL,
+	    /*timeout*/NULL);
+	ATF_CHECK_ERRNO(EBADF, nfds == -1);
 }
 
 static void *
@@ -168,8 +344,7 @@ testfdrestart(struct fdrestart *F)
 {
 	pthread_t t;
 
-	ATF_REQUIRE_MSG(signal(SIGALRM, &on_sigalrm) != SIG_ERR,
-	    "errno=%d (%s)", errno, strerror(errno));
+	REQUIRE_LIBC(signal(SIGALRM, &on_sigalrm), SIG_ERR);
 
 	RZ(pthread_barrier_init(&F->barrier, NULL, 2));
 	RZ(pthread_create(&t, NULL, &doit, F));
@@ -201,6 +376,48 @@ ATF_TC_BODY(pipe_read, tc)
 	testfdrestart(F);
 }
 
+ATF_TC(pipe_pollread);
+ATF_TC_HEAD(pipe_pollread, tc)
+{
+	atf_tc_set_md_var(tc, "descr",
+	    "Test poll waiting for pipe readability wakes on close");
+}
+ATF_TC_BODY(pipe_pollread, tc)
+{
+	struct fdrestart fdrestart, *F = &fdrestart;
+	int fd[2];
+
+	rump_init();
+
+	RL(rump_sys_pipe(fd));
+
+	memset(F, 0, sizeof(*F));
+	F->op = &dopollread;
+	F->fd = fd[0];
+	testfdrestart(F);
+}
+
+ATF_TC(pipe_selectread);
+ATF_TC_HEAD(pipe_selectread, tc)
+{
+	atf_tc_set_md_var(tc, "descr",
+	    "Test select waiting for pipe readability wakes on close");
+}
+ATF_TC_BODY(pipe_selectread, tc)
+{
+	struct fdrestart fdrestart, *F = &fdrestart;
+	int fd[2];
+
+	rump_init();
+
+	RL(rump_sys_pipe(fd));
+
+	memset(F, 0, sizeof(*F));
+	F->op = &doselectread;
+	F->fd = fd[0];
+	testfdrestart(F);
+}
+
 ATF_TC(pipe_write);
 ATF_TC_HEAD(pipe_write, tc)
 {
@@ -222,6 +439,48 @@ ATF_TC_BODY(pipe_write, tc)
 	testfdrestart(F);
 }
 
+ATF_TC(pipe_pollwrite);
+ATF_TC_HEAD(pipe_pollwrite, tc)
+{
+	atf_tc_set_md_var(tc, "descr",
+	    "Test poll waiting for pipe writability wakes on close");
+}
+ATF_TC_BODY(pipe_pollwrite, tc)
+{
+	struct fdrestart fdrestart, *F = &fdrestart;
+	int fd[2];
+
+	rump_init();
+
+	RL(rump_sys_pipe(fd));
+
+	memset(F, 0, sizeof(*F));
+	F->op = &dopollwrite;
+	F->fd = fd[1];
+	testfdrestart(F);
+}
+
+ATF_TC(pipe_selectwrite);
+ATF_TC_HEAD(pipe_selectwrite, tc)
+{
+	atf_tc_set_md_var(tc, "descr",
+	    "Test select waiting for pipe writability wakes on close");
+}
+ATF_TC_BODY(pipe_selectwrite, tc)
+{
+	struct fdrestart fdrestart, *F = &fdrestart;
+	int fd[2];
+
+	rump_init();
+
+	RL(rump_sys_pipe(fd));
+
+	memset(F, 0, sizeof(*F));
+	F->op = &doselectwrite;
+	F->fd = fd[1];
+	testfdrestart(F);
+}
+
 ATF_TC(socketpair_read);
 ATF_TC_HEAD(socketpair_read, tc)
 {
@@ -238,6 +497,48 @@ ATF_TC_BODY(socketpair_read, tc)
 
 	memset(F, 0, sizeof(*F));
 	F->op = &doread;
+	F->fd = fd[0];
+	testfdrestart(F);
+}
+
+ATF_TC(socketpair_pollread);
+ATF_TC_HEAD(socketpair_pollread, tc)
+{
+	atf_tc_set_md_var(tc, "descr",
+	    "Test socketpair poll for readability wakes on close");
+}
+ATF_TC_BODY(socketpair_pollread, tc)
+{
+	struct fdrestart fdrestart, *F = &fdrestart;
+	int fd[2];
+
+	rump_init();
+
+	RL(rump_sys_socketpair(AF_LOCAL, SOCK_STREAM, 0, fd));
+
+	memset(F, 0, sizeof(*F));
+	F->op = &dopollread;
+	F->fd = fd[0];
+	testfdrestart(F);
+}
+
+ATF_TC(socketpair_selectread);
+ATF_TC_HEAD(socketpair_selectread, tc)
+{
+	atf_tc_set_md_var(tc, "descr",
+	    "Test socketpair select for readability wakes on close");
+}
+ATF_TC_BODY(socketpair_selectread, tc)
+{
+	struct fdrestart fdrestart, *F = &fdrestart;
+	int fd[2];
+
+	rump_init();
+
+	RL(rump_sys_socketpair(AF_LOCAL, SOCK_STREAM, 0, fd));
+
+	memset(F, 0, sizeof(*F));
+	F->op = &doselectread;
 	F->fd = fd[0];
 	testfdrestart(F);
 }
@@ -262,12 +563,62 @@ ATF_TC_BODY(socketpair_write, tc)
 	testfdrestart(F);
 }
 
+ATF_TC(socketpair_pollwrite);
+ATF_TC_HEAD(socketpair_pollwrite, tc)
+{
+	atf_tc_set_md_var(tc, "descr",
+	    "Test poll waiting for socket writability wakes on close");
+}
+ATF_TC_BODY(socketpair_pollwrite, tc)
+{
+	struct fdrestart fdrestart, *F = &fdrestart;
+	int fd[2];
+
+	rump_init();
+
+	RL(rump_sys_socketpair(AF_LOCAL, SOCK_STREAM, 0, fd));
+
+	memset(F, 0, sizeof(*F));
+	F->op = &dopollwrite;
+	F->fd = fd[0];
+	testfdrestart(F);
+}
+
+ATF_TC(socketpair_selectwrite);
+ATF_TC_HEAD(socketpair_selectwrite, tc)
+{
+	atf_tc_set_md_var(tc, "descr",
+	    "Test select waiting for socket writability wakes on close");
+}
+ATF_TC_BODY(socketpair_selectwrite, tc)
+{
+	struct fdrestart fdrestart, *F = &fdrestart;
+	int fd[2];
+
+	rump_init();
+
+	RL(rump_sys_socketpair(AF_LOCAL, SOCK_STREAM, 0, fd));
+
+	memset(F, 0, sizeof(*F));
+	F->op = &doselectwrite;
+	F->fd = fd[0];
+	testfdrestart(F);
+}
+
 ATF_TP_ADD_TCS(tp)
 {
 
+	ATF_TP_ADD_TC(tp, pipe_pollread);
+	ATF_TP_ADD_TC(tp, pipe_pollwrite);
 	ATF_TP_ADD_TC(tp, pipe_read);
+	ATF_TP_ADD_TC(tp, pipe_selectread);
+	ATF_TP_ADD_TC(tp, pipe_selectwrite);
 	ATF_TP_ADD_TC(tp, pipe_write);
+	ATF_TP_ADD_TC(tp, socketpair_pollread);
+	ATF_TP_ADD_TC(tp, socketpair_pollwrite);
 	ATF_TP_ADD_TC(tp, socketpair_read);
+	ATF_TP_ADD_TC(tp, socketpair_selectread);
+	ATF_TP_ADD_TC(tp, socketpair_selectwrite);
 	ATF_TP_ADD_TC(tp, socketpair_write);
 
 	return atf_no_error();
