805df380f74ca122bee6b891287b0985adc7faa7
riastradh <riastradh@NetBSD.org>
2026-09-23 18:24:48 +0000

    t_poll: Extend err test to cover POLLNVAL too.
    Prompted by:
    
    PR kern/59056: poll POLLHUP bugs

--- tests/lib/libc/sys/t_poll.c
+++ tests/lib/libc/sys/t_poll.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: t_poll.c,v 1.12 2025/02/10 02:41:34 riastradh Exp $	*/
+/*	$NetBSD: t_poll.c,v 1.13 2026/09/23 18:24:48 riastradh Exp $	*/
 
 /*-
  * Copyright (c) 2011 The NetBSD Foundation, Inc.
@@ -230,16 +230,28 @@ ATF_TC_HEAD(err, tc)
 ATF_TC_BODY(err, tc)
 {
 	struct pollfd pfd;
-	int fd = 0;
+	int fd = 0, invalidfd;
+	int nfds;
 
 	pfd.fd = fd;
 	pfd.events = POLLIN;
 
 	errno = 0;
-	ATF_REQUIRE_ERRNO(EFAULT, poll((struct pollfd *)-1, 1, -1) == -1);
+	ATF_CHECK_ERRNO(EFAULT, poll((struct pollfd *)-1, 1, -1) == -1);
 
 	errno = 0;
-	ATF_REQUIRE_ERRNO(EINVAL, poll(&pfd, 1, -2) == -1);
+	ATF_CHECK_ERRNO(EINVAL, poll(&pfd, 1, -2) == -1);
+
+	RL(invalidfd = fcntl(0, F_MAXFD));
+	invalidfd++;
+	pfd.fd = invalidfd;
+	errno = 0;
+	RL(nfds = poll(&pfd, 1, 0));
+	ATF_CHECK_EQ_MSG(nfds, 1, "nfds=%d", nfds);
+	ATF_CHECK_EQ_MSG(pfd.fd, invalidfd,
+	    "pfd.fd=%d fd=%d", pfd.fd, invalidfd);
+	ATF_CHECK_EQ_MSG(pfd.revents, POLLNVAL,
+	    "pfd.revents=0x%x expected=0x%x", pfd.revents, POLLNVAL);
 }
 
 static const char	fifo_path[] = "pollhup_fifo";
