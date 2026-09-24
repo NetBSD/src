bbe3f7435b32e2c6bb9a955c042dc6021724803e
joe <joe@NetBSD.org>
2026-09-24 21:11:00 +0000

    improve routing code in NPF
    duplicate packet and end copy of the packet and locally manage original.
    return false always to keep it in procedures returns.

--- sys/net/npf/npf_ext_route.c
+++ sys/net/npf/npf_ext_route.c
@@ -33,7 +33,7 @@
 
  #ifdef _KERNEL
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: npf_ext_route.c,v 1.11 2026/09/22 16:29:11 joe Exp $");
+__KERNEL_RCSID(0, "$NetBSD: npf_ext_route.c,v 1.12 2026/09/24 21:11:00 joe Exp $");
 
 #include <sys/param.h>
 #include <sys/types.h>
@@ -309,20 +309,19 @@ npf_route(npf_cache_t *npc, void *meta, const npf_match_info_t __unused *mi,
     int *decision)
 {
 	struct mbuf *m = nbuf_head_mbuf(npc->npc_nbuf);
+	struct mbuf *m0 = NULL;
 	const npf_ext_route_t *route = meta;
 	npf_t *npf = npf_getkernctx();
 	struct ifnet *ifp;
 	int error, sw_csum;
-	bool consumed, rv;
+	bool consumed = false;
 	int stats;
 	union {
 		struct sockaddr_in v4;
 		struct sockaddr_in6 v6;
 	} dst;
 
-	consumed = false;
-	rv = true;
-	stats = NPF_STAT_NOREROUTE;
+	stats = NPF_STAT_REROUTE;
 
 	/* Skip, if already blocking.
 	 * also when routing is applied to a stateful rule, incoming packets
@@ -341,6 +340,11 @@ npf_route(npf_cache_t *npc, void *meta, const npf_match_info_t __unused *mi,
 		goto bad;
 	}
 
+	/* duplicate packet and send our version */
+	if ((m0 = m_dup(m, 0, M_COPYALL, M_NOWAIT)) == NULL) {
+		goto bad;
+	}
+
 	if (npf_iscached(npc, NPC_IP6)) {
 #if defined(INET6)
 		struct ip6_hdr *ip6 = npc->npc_ip.v6;
@@ -353,23 +357,22 @@ npf_route(npf_cache_t *npc, void *meta, const npf_match_info_t __unused *mi,
 			}
 		}
 
-		npf_validate_s6addr(m, ifp, &sw_csum);
+		npf_validate_s6addr(m0, ifp, &sw_csum);
 
 		if (m->m_pkthdr.len > ifp->if_mtu) {
 			/* router not allowed to fragment */
 			npf_stats_inc(npf, NPF_STAT_NOFRAGMENT);
 			goto bad;
 		}
-		consumed = true;
 		if (__predict_false(sw_csum & M_CSUM_TSOv6)) {
 			/*
 			 * TSO6 is required by a packet, but disabled for
 			 * the interface.
 			*/
-			error = ip6_tso_output(ifp, ifp, m, &dst.v6, NULL);
+			error = ip6_tso_output(ifp, ifp, m0, &dst.v6, NULL);
 		} else
-			error = ip6_if_output(ifp, ifp, m, &dst.v6, NULL);
-
+			error = ip6_if_output(ifp, ifp, m0, &dst.v6, NULL);
+		consumed = true;
 		if (error) {
 			goto bad;
 		}
@@ -387,11 +390,10 @@ npf_route(npf_cache_t *npc, void *meta, const npf_match_info_t __unused *mi,
 		if (error)
 			goto bad;
 
-		consumed = true;
 		if (ntohs(ip->ip_len) > ifp->if_mtu)
 			goto fragment;
 
-		npf_chcksum(ifp, m, ip, &sw_csum);
+		npf_chcksum(ifp, m0, ip, &sw_csum);
 
 		/* Send it */
 		if (__predict_false(sw_csum & M_CSUM_TSOv4)) {
@@ -399,36 +401,39 @@ npf_route(npf_cache_t *npc, void *meta, const npf_match_info_t __unused *mi,
 			 * TSO4 is required by a packet, but disabled for
 			 * the interface.
 			 */
-			error = ip_tso_output(ifp, m, sintocsa(&dst.v4), NULL);
+			error = ip_tso_output(ifp, m0, sintocsa(&dst.v4), NULL);
 		} else
-			error = ip_if_output(ifp, m, sintocsa(&dst.v4), NULL);
-
+			error = ip_if_output(ifp, m0, sintocsa(&dst.v4), NULL);
+		consumed = true;
 		if (error) {
 			goto bad;
 		}
 		goto done;
 
 fragment:
-		error = npf_fragment(npf, ifp, ip, &m, &dst.v4);
-		if (error) {
-			goto bad;
+		error = npf_fragment(npf, ifp, ip, &m0, &dst.v4);
+		consumed = true;
+		if (!error) {
+			goto done;
 		}
 	}
 
 /*
- * for routing procedures, we reverse the returns
- * because we need the kernel to stop processing the mbuf
- * after we leave the filtering context
+ * for routing procedures, we need processing of the moving mbuf stopped
+ * either in a failure or success path.(we now care about the copied which is to be sent)
+ * just return false and handle the freeing of the moving mbuf,
+ * leave the copied/unconsumed mbuf to the network stack.
  */
-done:
-	stats = NPF_STAT_REROUTE;
-	rv = false;
 bad:
+	if (!consumed)
+		m_freem(m0);
+	stats = NPF_STAT_NOREROUTE;
+done:
 	npf_stats_inc(npf, stats);
-	if (consumed)
-		memset(npc->npc_nbuf, 0, sizeof(*npc->npc_nbuf));
+	m_freem(m);
+	m = NULL;
 	KERNEL_UNLOCK_ONE(NULL);
-	return rv;
+	return false;
 }
 
 __dso_public int
